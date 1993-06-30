be449bcfcc01b5c6607908472ea1d1701ca7df59
nate <nate@FreeBSD.org>
1993-06-30 22:59:18 +0000

    Added (protection) around negative constants, in case a program wants to use the negative of that constant.
    #define NEG_NUM -3
    #define SAFE_NEG_NUM (-3)
    
    i = -NEG_NUM;		/* Error --3 */
    j = -SAFE_NEG_NUM	/* Okay -(-3) */

--- sys/i386/include/float.h
+++ sys/i386/include/float.h
@@ -39,9 +39,9 @@
 #define FLT_MANT_DIG	24		/* p */
 #define FLT_EPSILON	1.19209290E-07F	/* b**(1-p) */
 #define FLT_DIG		6		/* floor((p-1)*log10(b))+(b == 10) */
-#define FLT_MIN_EXP	-125		/* emin */
+#define FLT_MIN_EXP	(-125)		/* emin */
 #define FLT_MIN		1.17549435E-38F	/* b**(emin-1) */
-#define FLT_MIN_10_EXP	-37		/* ceil(log10(b**(emin-1))) */
+#define FLT_MIN_10_EXP	(-37)		/* ceil(log10(b**(emin-1))) */
 #define FLT_MAX_EXP	128		/* emax */
 #define FLT_MAX		3.40282347E+38F	/* (1-b**(-p))*b**emax */
 #define FLT_MAX_10_EXP	38		/* floor(log10((1-b**(-p))*b**emax)) */
@@ -49,9 +49,9 @@
 #define DBL_MANT_DIG	53
 #define DBL_EPSILON	2.2204460492503131E-16
 #define DBL_DIG		15
-#define DBL_MIN_EXP	-1021
+#define DBL_MIN_EXP	(-1021)
 #define DBL_MIN		2.2250738585072014E-308
-#define DBL_MIN_10_EXP	-307
+#define DBL_MIN_10_EXP	(-307)
 #define DBL_MAX_EXP	1024
 #define DBL_MAX		1.7976931348623157E+308
 #define DBL_MAX_10_EXP	308
