73606c73729da848d3173c01390dc2a78d2997e1
jkh <jkh@FreeBSD.org>
1993-07-07 23:07:02 +0000

    Newer new man page reader.

---
+++ gnu/usr.bin/man/man/version.h
@@ -0,0 +1,17 @@
+/*
+ * version.h
+ *
+ * Copyright (c) 1990, 1991, John W. Eaton.
+ *
+ * You may distribute under the terms of the GNU General Public
+ * License as specified in the file COPYING that comes with the man
+ * distribution.  
+ *
+ * John W. Eaton
+ * jwe@che.utexas.edu
+ * Department of Chemical Engineering
+ * The University of Texas at Austin
+ * Austin, Texas  78712
+ */
+
+static char version[] = "1.1";
