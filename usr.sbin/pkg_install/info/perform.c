9d2dd7e05047e0ad48700014adcc568fd35ab1c8
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1993-09-05 22:36:54 +0000

    Added -e flag to pkg_info.  Now you can test to see whether a package is installed or not rather easily (useful for install scripts that want to check for prerequisite packages).  See man page for details.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@392 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/pkg_install/info/perform.c
+++ usr.sbin/pkg_install/info/perform.c
@@ -37,7 +37,7 @@ pkg_perform(char **pkgs)
     signal(SIGINT, cleanup);
 
     /* Overriding action? */
-    if (AllInstalled) {
+    if (AllInstalled || CheckPkg) {
 	if (isdir(LOG_DIR)) {
 	    DIR *dirp;
 	    struct dirent *dp;
@@ -45,10 +45,18 @@ pkg_perform(char **pkgs)
 	    dirp = opendir(LOG_DIR);
 	    if (dirp) {
 		for (dp = readdir(dirp); dp != NULL; dp = readdir(dirp)) {
-		    if (strcmp(dp->d_name, ".") && strcmp(dp->d_name, ".."))
-			err_cnt += pkg_do(dp->d_name);
+		    if (strcmp(dp->d_name, ".") && strcmp(dp->d_name, "..")) {
+			if (CheckPkg) {
+			    if (!strcmp(dp->d_name, CheckPkg))
+				return 0;
+			}
+			else 
+			    err_cnt += pkg_do(dp->d_name);
+		    }
 		}
 		(void)closedir(dirp);
+		if (CheckPkg)
+		    return 1;
 	    }
 	    else
 		++err_cnt;
