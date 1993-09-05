2b40491738bdf7ef0fefb8e25e94ec655b67e10d
Rodney W. Grimes <rgrimes@FreeBSD.org>
1993-09-05 17:41:07 +0000

    Fixed printf if you are using SCSI_DELAY to allow the scsi bus to settle down after the reset.

--- sys/scsi/scsiconf.c
+++ sys/scsi/scsiconf.c
@@ -14,7 +14,7 @@
  *
  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992
  *
- *	$Id: scsiconf.c,v 1.6 93/08/26 21:09:39 julian Exp Locker: julian $
+ *	$Id: scsiconf.c,v 1.5 1993/08/28 03:08:53 rgrimes Exp $
  */
 
 #include <sys/types.h>
@@ -166,7 +166,8 @@ struct	scsi_switch	*scsi_switch;
 
 #ifdef	SCSI_DELAY
 #if 	SCSI_DELAY > 2
-	printf("waiting for scsi devices to settle\n");
+	printf("%s%d waiting for scsi devices to settle\n",
+		scsi_switch->name, unit);
 #else	SCSI_DELAY > 2
 #define	SCSI_DELAY 2
 #endif	SCSI_DELAY > 2
