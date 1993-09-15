af64f007ac7bb2c748251d1648355c21ca42f294
jtc <jtc@FreeBSD.org>
1993-09-15 20:27:25 +0000

    Include <rpc/rpc.h> instead of various individual <rpc/*.h> header files to compile with the newly prototyped rpc headers.

--- usr.sbin/tcpdump/tcpdump/print-nfs.c
+++ usr.sbin/tcpdump/tcpdump/print-nfs.c
@@ -21,7 +21,7 @@
 
 #ifndef lint
 static char rcsid[] =
-    "@(#) $Header: print-nfs.c,v 1.24 92/01/31 12:27:46 mccanne Exp $ (LBL)";
+    "@(#) $Header: /a/cvs/386BSD/src/contrib/tcpdump/tcpdump/print-nfs.c,v 1.1.1.1 1993/06/12 14:42:08 rgrimes Exp $ (LBL)";
 #endif
 
 #include <stdio.h>
@@ -37,12 +37,7 @@ static char rcsid[] =
 
 #include <sys/time.h>
 #include <errno.h>
-#include <rpc/types.h>
-#include <rpc/auth.h>
-#include <rpc/auth_unix.h>
-#include <rpc/svc.h>
-#include <rpc/xdr.h>
-#include <rpc/rpc_msg.h>
+#include <rpc/rpc.h>
 
 #include <ctype.h>
 
