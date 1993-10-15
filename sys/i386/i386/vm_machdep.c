98dbbc53998595e1b7b1ea3fef2e47953b042007
rgrimes <rgrimes@FreeBSD.org>
1993-10-15 10:34:29 +0000

    genassym.c: 	Remove NKMEMCLUSTERS, it is no longer define or used.
    locores.s:
    	Fix comment on PTDpde and APTDpde to be pde instead of pte
    	Add new equation for calculating location of Sysmap
    	Remove Bill's old #ifdef garbage for counting up memory,
    	that stuff will never be made to work and was just cluttering
    	up the file.
    
    	Add code that places the PTD, page table pages, and kernel
    	stack below the 640k ISA hole if there is room for it, otherwise
    	put this stuff all at 1MB.  This fixes the 28K bogusity in
    	the boot blocks, that can now go away!
    
    	Fix the caclulation of where first is to be dependent on
    	NKPDE so that we can skip over the above mentioned areas.
    	The 28K thing is now 44K in size due to the increase in
    	kernel virtual memory space, but since we no longer have
    	to worry about that this is no big deal.
    
    	Use if NNPX > 0 instead of ifdef NPX for floating point code.
    
    machdep.c
    	Change the calculation of for the buffer cache to be
    	20% of all memory above 2MB and add back the upper limit
    	of 2/5's of the VM_KMEM_SIZE so that we do not eat ALL
    	of the kernel memory space on large memory machines, note
    	that this will not even come into effect unless you have
    	more than 32MB.  The current buffer cache limit is 6.7MB
    	due to this caclulation.
    
    	It seems that we where erroniously allocating bufpages pages
    	for buffer_map.  buffer_map is UNUSED in this implementation
    	of the buffer cache, but since the map is referenced in
    	several if statements a quick fix was to simply allocate
    	1 vm page (but no real memory) to it.
    
    pmap.h
    	Remove rcsid, don't want them in the kernel files!
    
    	Removed some cruft inside an #ifdef DEBUGx that caused
    	compiler errors if you where compiling this for debug.
    
    	Use the #defines for PD_SHIFT and PG_SHIFT in place of
    	constants.
    
    trap.c:
    	Remove patch kit header and rcsid, fix $Id$.
    	Now include "npx.h" and use NNPX for controlling the
    	floating point code.
    
    	Remove a now completly invalid check for a maximum virtual
    	address, the virtual address now ends at 0xFFFFFFFF so
    	there is no more MAX!!  (Thanks David, I completly missed
    	that one!)
    
    vm_machdep.c
    	Remove patch kit header and rcsid, fix $Id$.
    	Now include "npx.h" and use NNPX for controlling the
    	floating point code.
    
    	Replace several 0xFE00000 constants with KERNBASE

--- sys/i386/i386/vm_machdep.c
+++ sys/i386/i386/vm_machdep.c
@@ -35,22 +35,12 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vm_machdep.c	7.3 (Berkeley) 5/13/91
- *
- * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE
- * --------------------         -----   ----------------------
- * CURRENT PATCH LEVEL:         1       00154
- * --------------------         -----   ----------------------
- *
- * 20 Apr 93	Bruce Evans		New npx-0.5 code
- *
- */
-
-/*
+ *	from: @(#)vm_machdep.c	7.3 (Berkeley) 5/13/91
  *	Utah $Hdr: vm_machdep.c 1.16.1.1 89/06/23$
+ *	$Id$
  */
-static char rcsid[] = "$Header: /usr/chroot/CVS/386BSD/src/sys/i386/i386/vm_machdep.c,v 1.3 1993/07/27 10:52:21 davidg Exp $";
 
+#include "npx.h"
 #include "param.h"
 #include "systm.h"
 #include "proc.h"
@@ -152,9 +142,9 @@ cpu_exit(p)
 {
 	static struct pcb nullpcb;	/* pcb to overwrite on last swtch */
 
-#ifdef NPX
+#if NNPX > 0
 	npxexit(p);
-#endif
+#endif	/* NNPX */
 
 	/* move to inactive space and stack, passing arg accross */
 	p = swtch_to_inactive(p);
@@ -174,9 +164,9 @@ cpu_exit(p)
 	register struct proc *p;
 {
 	
-#ifdef NPX
+#if NNPX > 0
 	npxexit(p);
-#endif
+#endif	/* NNPX */
 	splclock();
 	swtch();
 	/* 
@@ -308,8 +298,8 @@ kernacc(addr, count, rw)
 	for (pde += ix; cnt; cnt--, pde++)
 		if (pde->pd_v == 0)
 			return(0);
-	ix = btop(addr-0xfe000000);
-	cnt = btop(addr-0xfe000000+count+NBPG-1);
+	ix = btop(addr-KERNBASE);
+	cnt = btop(addr-KERNBASE+count+NBPG-1);
 	if (cnt > (int)&Syssize)
 		return(0);
 	cnt -= ix;
