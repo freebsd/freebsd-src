7b833e734d5d8e5d24f87e2de2e1d216b12dc402
rgrimes <rgrimes@FreeBSD.org>
1993-10-16 19:17:18 +0000

    Removed all patch kit headers, sccsid and rcsid strings, put $Id$ in, some minor cleanup.  Added $Id$ to files that did not have any version info, etc

--- sys/i386/boot/biosboot/sys.c
+++ sys/i386/boot/biosboot/sys.c
@@ -1,6 +1,4 @@
 /*
- * Ported to boot 386BSD by Julian Elischer (julian@tfs.com) Sept 1992
- *
  * Mach Operating System
  * Copyright (c) 1992, 1991 Carnegie Mellon University
  * All Rights Reserved.
@@ -24,18 +22,9 @@
  * 
  * any improvements or extensions that they make and grant Carnegie Mellon
  * the rights to redistribute these changes.
- */
-
-/*
- * HISTORY
- * $Log: sys.c,v $
- * Revision 1.1  1993/03/21  18:08:45  cgd
- * after 0.2.2 "stable" patches applied
  *
- * Revision 2.2  92/04/04  11:36:34  rpd
- * 	Fabricated from 3.0 bootstrap and scratch.
- * 	[92/03/30            mg32]
- * 
+ *	from: Mach, Revision 2.2  92/04/04  11:36:34  rpd
+ *	$Id$
  */
 
 #include "boot.h"
