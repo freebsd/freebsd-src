66fdbc00cfad3be604d07da78b916f4c04f96dbc
paul <paul@FreeBSD.org>
1993-11-03 23:41:59 +0000

    Imported NetBSD's ld for shared libs.

---
+++ gnu/usr.bin/ld/etc.c
@@ -0,0 +1,211 @@
+/*
+ * $Id: etc.c,v 1.2 1993/10/21 00:52:52 pk Exp $
+ */
+
+#include <sys/param.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <ar.h>
+#include <ranlib.h>
+#include <a.out.h>
+#include <stab.h>
+#include <string.h>
+#if __STDC__
+#include <stdarg.h>
+#else
+#include <varargs.h>
+#endif
+
+#include "ld.h"
+
+/*
+ * Report a nonfatal error.
+ */
+
+void
+#if __STDC__
+error(char *fmt, ...)
+#else
+error(fmt, va_alist)
+	char	*fmt;
+	va_dcl
+#endif
+{
+	va_list	ap;
+#if __STDC__
+	va_start(ap, fmt);
+#else
+	va_start(ap);
+#endif
+	(void)fprintf(stderr, "%s: ", progname);
+	(void)vfprintf(stderr, fmt, ap);
+	(void)fprintf(stderr, "\n");
+	va_end(ap);
+}
+
+/*
+ * Report a fatal error.
+ */
+
+void
+#if __STDC__
+fatal(char *fmt, ...)
+#else
+fatal(fmt, va_alist)
+	char	*fmt;
+	va_dcl
+#endif
+{
+	va_list	ap;
+#if __STDC__
+	va_start(ap, fmt);
+#else
+	va_start(ap);
+#endif
+	(void)fprintf(stderr, "%s: ", progname);
+	(void)vfprintf(stderr, fmt, ap);
+	(void)fprintf(stderr, "\n");
+	va_end(ap);
+
+	if (outdesc >= 0)
+		unlink(output_filename);
+	exit(1);
+}
+
+
+/*
+ * Return a newly-allocated string whose contents concatenate
+ * the strings S1, S2, S3.
+ */
+
+char *
+concat(s1, s2, s3)
+	char *s1, *s2, *s3;
+{
+	register int	len1 = strlen (s1),
+			len2 = strlen (s2),
+			len3 = strlen (s3);
+
+	register char *result = (char *) xmalloc (len1 + len2 + len3 + 1);
+
+	strcpy (result, s1);
+	strcpy (result + len1, s2);
+	strcpy (result + len1 + len2, s3);
+	result[len1 + len2 + len3] = 0;
+
+	return result;
+}
+
+/* Parse the string ARG using scanf format FORMAT, and return the result.
+   If it does not parse, report fatal error
+   generating the error message using format string ERROR and ARG as arg.  */
+
+int
+parse(arg, format, error)
+	char *arg, *format, *error;
+{
+	int x;
+	if (1 != sscanf (arg, format, &x))
+		fatal (error, arg);
+	return x;
+}
+
+/* Like malloc but get fatal error if memory is exhausted.  */
+
+void *
+xmalloc(size)
+	int size;
+{
+	register void	*result = (void *)malloc (size);
+
+	if (!result)
+		fatal ("virtual memory exhausted", 0);
+
+	return result;
+}
+
+/* Like realloc but get fatal error if memory is exhausted.  */
+
+void *
+xrealloc(ptr, size)
+	void *ptr;
+	int size;
+{
+	register void	*result;
+
+	if (ptr == NULL)
+		result = (void *)malloc (size);
+	else
+		result = (void *)realloc (ptr, size);
+
+	if (!result)
+		fatal ("virtual memory exhausted", 0);
+
+	return result;
+}
+
+
+#ifdef USG
+void
+bzero(p, n)
+	char *p;
+{
+	memset (p, 0, n);
+}
+
+void
+bcopy(from, to, n)
+	char *from, *to;
+{
+	memcpy (to, from, n);
+}
+#endif
+
+
+/* These must move */
+
+#ifndef RTLD
+/*
+ * Output COUNT*ELTSIZE bytes of data at BUF to the descriptor DESC.
+ */
+void
+mywrite (buf, count, eltsize, desc)
+     char *buf;
+     int count;
+     int eltsize;
+     int desc;
+{
+	register int val;
+	register int bytes = count * eltsize;
+
+	while (bytes > 0) {
+		val = write (desc, buf, bytes);
+		if (val <= 0)
+			perror(output_filename);
+		buf += val;
+		bytes -= val;
+	}
+}
+
+/* Output PADDING zero-bytes to descriptor OUTDESC.
+   PADDING may be negative; in that case, do nothing.  */
+
+void
+padfile (padding, outdesc)
+     int padding;
+     int outdesc;
+{
+	register char *buf;
+	if (padding <= 0)
+		return;
+
+	buf = (char *) alloca (padding);
+	bzero (buf, padding);
+	mywrite (buf, padding, 1, outdesc);
+}
+#endif
---
+++ gnu/usr.bin/ld/symbol.c
@@ -0,0 +1,148 @@
+/*
+ * $Id: symbol.c,v 1.3 1993/11/01 16:26:19 pk Exp $		- symbol table routines
+ */
+
+/* Create the symbol table entries for `etext', `edata' and `end'.  */
+
+#include <sys/param.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <a.out.h>
+#include <stab.h>
+#include <string.h>
+
+#include "ld.h"
+
+void
+symtab_init (relocatable_output)
+int	relocatable_output;
+{
+	/*
+	 * Put linker reserved symbols into symbol table.
+	 */
+
+	dynamic_symbol = getsym ("__DYNAMIC");
+	dynamic_symbol->defined = relocatable_output?N_UNDF:(N_DATA | N_EXT);
+	dynamic_symbol->referenced = 0;
+	dynamic_symbol->value = 0;
+
+	got_symbol = getsym ("__GLOBAL_OFFSET_TABLE_");
+	got_symbol->defined = N_DATA | N_EXT;
+	got_symbol->referenced = 0;
+	got_symbol->value = 0;
+
+	if (relocatable_output)
+		return;
+
+#ifndef nounderscore
+	edata_symbol = getsym ("_edata");
+	etext_symbol = getsym ("_etext");
+	end_symbol = getsym ("_end");
+#else
+	edata_symbol = getsym ("edata");
+	etext_symbol = getsym ("etext");
+	end_symbol = getsym ("end");
+#endif
+	edata_symbol->defined = N_DATA | N_EXT;
+	etext_symbol->defined = N_TEXT | N_EXT;
+	end_symbol->defined = N_BSS | N_EXT;
+
+	edata_symbol->referenced = 1;
+	etext_symbol->referenced = 1;
+	end_symbol->referenced = 1;
+}
+
+/* Compute the hash code for symbol name KEY.  */
+
+int
+hash_string (key)
+     char *key;
+{
+	register char *cp;
+	register int k;
+
+	cp = key;
+	k = 0;
+	while (*cp)
+		k = (((k << 1) + (k >> 14)) ^ (*cp++)) & 0x3fff;
+
+	return k;
+}
+
+/* Get the symbol table entry for the global symbol named KEY.
+   Create one if there is none.  */
+
+symbol *
+getsym(key)
+	char *key;
+{
+	register int hashval;
+	register symbol *bp;
+
+	/* Determine the proper bucket.  */
+	hashval = hash_string (key) % TABSIZE;
+
+	/* Search the bucket.  */
+	for (bp = symtab[hashval]; bp; bp = bp->link)
+		if (! strcmp (key, bp->name))
+			return bp;
+
+	/* Nothing was found; create a new symbol table entry.  */
+	bp = (symbol *) xmalloc (sizeof (symbol));
+	bp->refs = 0;
+	bp->name = (char *) xmalloc (strlen (key) + 1);
+	strcpy (bp->name, key);
+	bp->defined = 0;
+	bp->referenced = 0;
+	bp->trace = 0;
+	bp->value = 0;
+	bp->max_common_size = 0;
+	bp->warning = 0;
+	bp->undef_refs = 0;
+	bp->multiply_defined = 0;
+	bp->alias = 0;
+	bp->setv_count = 0;
+
+	bp->size = 0;
+	bp->sorefs = 0;
+	bp->so_defined = 0;
+	bp->def_nlist = 0;
+	bp->jmpslot_offset = -1;
+	bp->gotslot_offset = -1;
+	bp->jmpslot_claimed = 0;
+	bp->gotslot_claimed = 0;
+	bp->cpyreloc_reserved = 0;
+	bp->cpyreloc_claimed = 0;
+
+	/* Add the entry to the bucket.  */
+	bp->link = symtab[hashval];
+	symtab[hashval] = bp;
+
+	++num_hash_tab_syms;
+
+	return bp;
+}
+
+/* Like `getsym' but return 0 if the symbol is not already known.  */
+
+symbol *
+getsym_soft (key)
+     char *key;
+{
+	register int hashval;
+	register symbol *bp;
+
+	/* Determine which bucket.  */
+
+	hashval = hash_string (key) % TABSIZE;
+
+	/* Search the bucket.  */
+
+	for (bp = symtab[hashval]; bp; bp = bp->link)
+		if (! strcmp (key, bp->name))
+			return bp;
+
+	return 0;
+}
