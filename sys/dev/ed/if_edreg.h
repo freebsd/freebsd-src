39ad61a78db35d2ec329347fce760265c6340051
dg <dg@FreeBSD.org>
1993-11-22 11:08:16 +0000

    * Revision 2.14  1993/11/22  10:55:30  davidg  * change all splnet's to splimp's  *  * Revision 2.13  1993/11/22  10:53:52  davidg  * patch to add support for SMC8216 (Elite-Ultra) boards  * from Glen H. Lowe  *  * Revision 2.12  1993/11/07  18:04:13  davidg  * fix from Garrett Wollman:  * add a return(0) at the end of ed_probe so that if the various device  * specific probes fail that we just don't fall of the end of the function.

--- sys/dev/ed/if_edreg.h
+++ sys/dev/ed/if_edreg.h
@@ -1,32 +1,18 @@
 /*
  * National Semiconductor DS8390 NIC register definitions 
  *
- * $Id: if_edreg.h,v 2.0 93/09/29 00:37:15 davidg Exp Locker: davidg $
+ * $Id: if_edreg.h,v 2.1 1993/11/22 10:52:33 davidg Exp davidg $
  *
  * Modification history
  *
- * $Log:	if_edreg.h,v $
+ * Revision 2.1  1993/11/22  10:52:33  davidg
+ * patch to add support for SMC8216 (Elite-Ultra) boards
+ * from Glen H. Lowe
+ *
  * Revision 2.0  93/09/29  00:37:15  davidg
  * changed double buffering flag to multi buffering
  * made changes/additions for 3c503 multi-buffering
  * ...companion to Rev. 2.0 of 'ed' driver.
- * 
- * Revision 1.6  93/09/28  17:20:03  davidg
- * first cut at PIO (e.g. NE1000/2000) support
- * 
- * Revision 1.5  93/08/25  20:38:34  davidg
- * added define for card type WD8013WC (10BaseT)
- * 
- * Revision 1.4  93/08/14  20:07:55  davidg
- * fix board type definition for 8013EP
- * 
- * Revision 1.3  93/07/20  15:25:25  davidg
- * added config flags for forcing 8/16bit mode and disabling double
- * xmit buffers.
- * 
- * Revision 1.2  93/06/23  03:03:05  davidg
- * added some additional definitions for the 83C584 bus interface
- * chip (SMC/WD boards)
  * 
  * Revision 1.1  93/06/23  03:01:07  davidg
  * Initial revision
@@ -692,6 +678,8 @@ struct ed_ring	{
 #define ED_TYPE_WD8013WC	0x28
 #define ED_TYPE_WD8013EBP	0x2c
 #define ED_TYPE_WD8013EPC	0x29
+#define ED_TYPE_SMC8216T	0x2a
+#define ED_TYPE_SMC8216C	0x2b
 
 /* Bit definitions in card ID */
 #define	ED_WD_REV_MASK		0x1f		/* Revision mask */
