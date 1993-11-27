4a8e05dd599390dfa99dca98bed6a5106af3827d
Andrey A. Chernov <ache@FreeBSD.org>
1993-11-27 22:16:44 +0000

    Move soundcard.h & ultrasound.h to proper location <machine/...> to allow application access it.

--- sys/sys/soundcard.h
+++ sys/sys/soundcard.h
@@ -12,10 +12,10 @@
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -30,7 +30,7 @@
  /* 
   * If you make modifications to this file, please contact me before
   * distributing the modified version. There is already enough 
-  * divercity in the world.
+  * diversity in the world.
   *
   * Regards,
   * Hannu Savolainen
@@ -603,7 +603,7 @@ struct sb_mixer_params
  *	These macros define the API which should be used when possible.
  */
 
-void seqbuf_dump();	/* This function must be provided by programs */
+void seqbuf_dump(void);	/* This function must be provided by programs */
 
 /* Sample seqbuf_dump() implementation:
  *
