fa2f9983992b8f76f4876d186e7f838007698a3a
jkh <jkh@FreeBSD.org>
1993-12-11 12:02:10 +0000

    Sync up with Paul K's latest ld from cesium.

--- gnu/usr.bin/ld/sparc/md.c
+++ gnu/usr.bin/ld/sparc/md.c
@@ -1,5 +1,33 @@
 /*
- * $Id: md.c,v 1.3 1993/11/22 19:05:30 jkh Exp $
+ * Copyright (c) 1993 Paul Kranenburg
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Paul Kranenburg.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software withough specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *	$Id: md.c,v 1.6 1993/12/08 10:29:02 pk Exp $
  */
 
 #include <sys/param.h>
@@ -181,9 +209,14 @@ int			type;
 #if 1
 	/*
 	 * This wouldn't be strictly necessary - we could record the
-	 * location value "in situ" in stead of in the r_addend field -
+	 * relocation value "in situ" in stead of in the r_addend field -
 	 * but we are being Sun compatible here. Besides, Sun's ld.so
-	 * has a bug that prevents it from handling this alternate method
+	 * has a bug that prevents it from handling this alternate method.
+	 *
+	 * IT WOULD BE REALLY NICE TO HAVE CONSISTENCY THROUGHOUT THE ENTIRE
+	 * RELOCATION PROCESS, ie. using `r_addend' for storing all partially
+	 * completed relocations, in stead of mixing them in both relocation
+	 * records and in the segment data.
 	 */
 	if (RELOC_PCREL_P(rp))
 		r->r_addend -= pc_relocation;
