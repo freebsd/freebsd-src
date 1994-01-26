42fee8984fee44230fd9de8ee2bf7bae62f8f812
ache <ache@FreeBSD.org>
1994-01-26 20:08:30 +0000

    Replace old-style Joerg copyright to berkeley-style, per Joerg request.

--- sys/sys/fdcio.h
+++ sys/sys/fdcio.h
@@ -1,16 +1,27 @@
 /*
- * 386BSD:
- * format a floppy disk
+ * Copyright (C) 1992-1993 by Joerg Wunsch, Dresden
+ * All rights reserved.
  *
- * First, totally uncomfortable and unreliable version.
- * USE AT YOUR OWN RISK. NEITHER KIND OF WARRANTY OR LIABILITY.
- * No complaints about "lost data", "destroyed diskette", "blown
- * controllers" or so are accepted.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
- * Detailed bug reports, bug fixes, and suggestions are welcome.
- * Author & Copyright:
- *  (c) 1992, 1993 Joerg Wunsch, Dresden
- *  joerg_wunsch@uriah.sax.de
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 #ifndef _IOCTL_FD_H
