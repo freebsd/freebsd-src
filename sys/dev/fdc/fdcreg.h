0cd7882d741fe4edbe992b444fbee8e177f0c24c
alm <alm@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1994-02-07 04:27:13 +0000

    Add floppy tape driver - fd => fdc
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@1110 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/fdc/fdcreg.h
+++ sys/dev/fdc/fdcreg.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)fdreg.h	7.1 (Berkeley) 5/9/91
- *	$Id$
+ *	$Id: fdreg.h,v 1.2 1993/10/16 13:45:50 rgrimes Exp $
  */
 
 /*
@@ -63,4 +63,41 @@
 #define	fdin	7	/* Digital Input Register (R) */
 #define	FDI_DCHG	0x80	/* diskette has been changed */
 
+/***********************************************************************\
+* Per controller structure.						*
+\***********************************************************************/
+struct fdc_data
+{
+	int	fdcu;		/* our unit number */
+	int	baseport;
+	int	dmachan;
+	int	flags;
+#define FDC_ATTACHED	0x01
+#define FDC_HASFTAPE	0x02
+#define FDC_TAPE_BUSY	0x04
+	struct	fd_data *fd;
+	int fdu;		/* the active drive	*/
+	struct buf head;	/* Head of buf chain      */
+	struct buf rhead;	/* Raw head of buf chain  */
+	int state;
+	int retry;
+	int status[7];		/* copy of the registers */
+};
 
+/***********************************************************************\
+* Throughout this file the following conventions will be used:		*
+* fd is a pointer to the fd_data struct for the drive in question	*
+* fdc is a pointer to the fdc_data struct for the controller		*
+* fdu is the floppy drive unit number					*
+* fdcu is the floppy controller unit number				*
+* fdsu is the floppy drive unit number on that controller. (sub-unit)	*
+\***********************************************************************/
+typedef int	fdu_t;
+typedef int	fdcu_t;
+typedef int	fdsu_t;
+typedef	struct fd_data *fd_p;
+typedef struct fdc_data *fdc_p;
+
+
+#define FDUNIT(s)       (((s)>>6)&03)
+#define FDTYPE(s)       ((s)&077)
