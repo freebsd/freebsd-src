4f6253f0cbe642b54994491c5c148eb280c6a761
jkh <jkh@FreeBSD.org>
1994-03-06 03:10:58 +0000

    Beginning of change set for making more friendly laptop configurations. Changes _only_ take effect if `options LAPTOP' is set.
    Note:  This one is distinctly dodgy. When my IDE drive spins back up from sleep
    mode, it generates this `extra interrupt' condition by spinning back up and
    generating an intr, though without any particular action required.  This
    message coming out every time is rather annoying, and thus disabled.
    However, what I'm not at all sure of is whether or not all IDE drives will
    behave in the same way, or if perhaps it needs to be done in a more complicated
    fashion by detecting this more involved "I've spun up and am just saying hi"
    condition.  This is a simple change and easy to back out/ammend if anybody has
    any better ideas.

--- sys/i386/isa/wd.c
+++ sys/i386/isa/wd.c
@@ -37,7 +37,7 @@ static int wdtest = 0;
  * SUCH DAMAGE.
  *
  *	from: @(#)wd.c	7.2 (Berkeley) 5/9/91
- *	$Id: wd.c,v 1.34 1994/02/25 23:17:40 ache Exp $
+ *	$Id: wd.c,v 1.35 1994/03/04 16:43:07 ache Exp $
  */
 
 /* TODO:
@@ -619,7 +619,9 @@ wdintr(int unit)
 	if (wdtab[unit].b_active == 2)
 		return;		/* intr in wdflushirq() */
 	if (!wdtab[unit].b_active) {
+#ifndef LAPTOP
 		printf("wdc%d: extra interrupt\n", unit);
+#endif
 		return;
 	}
 
