452b5808d651ba0823eb9b96c64bab8d2050868e
ache <ache@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1994-04-23 04:16:53 +0000

    Additional changes for 2.5
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@1404 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/isa/sound/local.h
+++ sys/i386/isa/sound/local.h
@@ -7,8 +7,8 @@
 
 #define DSP_BUFFSIZE 65536
 #define SELECTED_SOUND_OPTIONS	0xffffffff
-#define SOUND_VERSION_STRING "2.4"
-#define SOUND_CONFIG_DATE "Mon Mar 7 23:54:09 PST 1994"
-#define SOUND_CONFIG_BY "swallace"
-#define SOUND_CONFIG_HOST "freefall.cdrom.com"
+#define SOUND_VERSION_STRING "2.5"
+#define SOUND_CONFIG_DATE "Sat Apr 23 07:45:17 MSD 1994"
+#define SOUND_CONFIG_BY "ache"
+#define SOUND_CONFIG_HOST "dream.demos.su"
 #define SOUND_CONFIG_DOMAIN ""
