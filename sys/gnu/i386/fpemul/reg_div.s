25e00a41009e5d46bae8f3974e082cd950318672
gclarkii <gclarkii@FreeBSD.org>
1994-04-29 21:34:30 +0000

    Added rcs Id's

--- sys/gnu/i386/fpemul/reg_div.s
+++ sys/gnu/i386/fpemul/reg_div.s
@@ -48,7 +48,10 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *                                    unsigned int control_word)
+ *
+ * 
+ *      $Id:$
+ *
  */
 
 #include "exception.h"
