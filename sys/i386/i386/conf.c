cbb33f0cf5083e7bf4cf2bb319d3d50b0a0cdc5f
Rodney W. Grimes <rgrimes@FreeBSD.org>
1994-05-04 08:24:02 +0000

    USL copyright

--- sys/i386/i386/conf.c
+++ sys/i386/i386/conf.c
@@ -1,4 +1,11 @@
 /*
+ * Copyright (c) UNIX System Laboratories, Inc.  All or some portions
+ * of this file are derived from material licensed to the
+ * University of California by American Telephone and Telegraph Co.
+ * or UNIX System Laboratories, Inc. and are reproduced herein with
+ * the permission of UNIX System Laboratories, Inc.
+ */
+/*
  * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
  *
@@ -34,7 +41,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)conf.c	5.8 (Berkeley) 5/12/91
- *	$Id: conf.c,v 1.23 1994/04/19 23:45:43 wollman Exp $
+ *	$Id: conf.c,v 1.24 1994/04/21 14:10:31 sos Exp $
  */
 
 #include "param.h"
