b0d61785cae024b1f44119446a940ee14c9ac959
rgrimes <rgrimes@FreeBSD.org>
1994-05-30 19:09:18 +0000

    BSD 4.4 Lite Share Sources

---
+++ share/mk/bsd.doc.mk
@@ -0,0 +1,53 @@
+#	@(#)bsd.doc.mk	8.1 (Berkeley) 8/14/93
+
+BIB?=		bib
+EQN?=		eqn
+GREMLIN?=	grn
+GRIND?=		vgrind -f
+INDXBIB?=	indxbib
+PIC?=		pic
+REFER?=		refer
+ROFF?=		groff -M/usr/share/tmac -M/usr/old/lib/tmac ${MACROS} ${PAGES}
+SOELIM?=	soelim
+TBL?=		tbl
+
+.PATH: ${.CURDIR}
+
+.if !target(all)
+.MAIN: all
+all: paper.ps
+.endif
+
+.if !target(paper.ps)
+paper.ps: ${SRCS}
+	${ROFF} ${SRCS} > ${.TARGET}
+.endif
+
+.if !target(print)
+print: paper.ps
+	lpr -P${PRINTER} paper.ps
+.endif
+
+.if !target(manpages)
+manpages:
+.endif
+
+.if !target(obj)
+obj:
+.endif
+
+clean cleandir:
+	rm -f paper.* [eE]rrs mklog ${CLEANFILES}
+
+FILES?=	${SRCS}
+install:
+	install -c -o ${BINOWN} -g ${BINGRP} -m 444 \
+	    Makefile ${FILES} ${EXTRA} ${DESTDIR}${BINDIR}/${DIR}
+
+spell: ${SRCS}
+	spell ${SRCS} | sort | comm -23 - spell.ok > paper.spell
+
+BINDIR?=	/usr/share/doc
+BINGRP?=	bin
+BINOWN?=	bin
+BINMODE?=	444
