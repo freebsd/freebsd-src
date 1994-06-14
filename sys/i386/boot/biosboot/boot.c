5618a2931b029b0adc5bc9053589405d751baf5f
rgrimes <rgrimes@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1994-06-14 07:31:42 +0000

    Touch the $Revision$ string to cause a cvs revision update of the boot code (ie, should now print Revision 1.13).
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@1717 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/boot/biosboot/boot.c
+++ sys/i386/boot/biosboot/boot.c
@@ -24,7 +24,7 @@
  * the rights to redistribute these changes.
  *
  *	from: Mach, [92/04/03  16:51:14  rvb]
- *	$Id: boot.c,v 1.11 1994/05/01 03:53:29 martin Exp $
+ *	$Id: boot.c,v 1.12 1994/05/30 05:23:53 ache Exp $
  */
 
 
@@ -75,7 +75,7 @@ int drive;
 		ouraddr,
 		argv[7] = memsize(0),
 		argv[8] = memsize(1),
-		"$Revision: 1.11 $");
+		"$Revision: 1.13 $");
 	printf("use hd(1,a)/386bsd to boot sd0 when wd0 is also installed\n");
 	gateA20();
 loadstart:
