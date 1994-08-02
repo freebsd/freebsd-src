b930be38b503c0622cbf2513cdeea209d34c6267
phk <phk@FreeBSD.org>
1994-08-02 20:15:59 +0000

    Here comes the right import of gcc-2.6.0.

---
+++ gnu/usr.bin/cc/include/insn-attr.h
@@ -0,0 +1,19 @@
+/* Generated automatically by the program `genattr'
+from the machine description file `md'.  */
+
+#ifndef PROTO
+#if defined (USE_PROTOTYPES) ? USE_PROTOTYPES : defined (__STDC__)
+#define PROTO(ARGS) ARGS
+#else
+#define PROTO(ARGS) ()
+#endif
+#endif
+#define HAVE_ATTR_alternative
+#define get_attr_alternative(insn) which_alternative
+
+#define ATTR_FLAG_forward	0x1
+#define ATTR_FLAG_backward	0x2
+#define ATTR_FLAG_likely	0x4
+#define ATTR_FLAG_very_likely	0x8
+#define ATTR_FLAG_unlikely	0x10
+#define ATTR_FLAG_very_unlikely	0x20
