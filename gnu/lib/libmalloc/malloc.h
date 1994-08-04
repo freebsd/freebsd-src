f53b3edeed8d6933c67419635b550bc0994f32ea
wollman <wollman@FreeBSD.org>
1994-08-04 20:53:14 +0000

    Use __GNU_P ratherthan __P.

--- gnu/lib/libmalloc/malloc.h
+++ gnu/lib/libmalloc/malloc.h
@@ -59,13 +59,13 @@ extern "C"
 #endif
 
 #if defined (__cplusplus) || (defined (__STDC__) && __STDC__)
-#undef	__P
-#define	__P(args)	args
+#undef	__GNU_P
+#define	__GNU_P(args)	args
 #undef	__ptr_t
 #define	__ptr_t		void *
 #else /* Not C++ or ANSI C.  */
-#undef	__P
-#define	__P(args)	()
+#undef	__GNU_P
+#define	__GNU_P(args)	()
 #undef	const
 #define	const
 #undef	__ptr_t
@@ -87,20 +87,20 @@ extern "C"
 
 
 /* Allocate SIZE bytes of memory.  */
-extern __ptr_t malloc __P ((size_t __size));
+extern __ptr_t malloc __GNU_P ((size_t __size));
 /* Re-allocate the previously allocated block
    in __ptr_t, making the new block SIZE bytes long.  */
-extern __ptr_t realloc __P ((__ptr_t __ptr, size_t __size));
+extern __ptr_t realloc __GNU_P ((__ptr_t __ptr, size_t __size));
 /* Allocate NMEMB elements of SIZE bytes each, all initialized to 0.  */
-extern __ptr_t calloc __P ((size_t __nmemb, size_t __size));
+extern __ptr_t calloc __GNU_P ((size_t __nmemb, size_t __size));
 /* Free a block allocated by `malloc', `realloc' or `calloc'.  */
-extern void free __P ((__ptr_t __ptr));
+extern void free __GNU_P ((__ptr_t __ptr));
 
 /* Allocate SIZE bytes allocated to ALIGNMENT bytes.  */
-extern __ptr_t memalign __P ((size_t __alignment, size_t __size));
+extern __ptr_t memalign __GNU_P ((size_t __alignment, size_t __size));
 
 /* Allocate SIZE bytes on a page boundary.  */
-extern __ptr_t valloc __P ((size_t __size));
+extern __ptr_t valloc __GNU_P ((size_t __size));
 
 
 #ifdef _MALLOC_INTERNAL
@@ -195,34 +195,34 @@ extern size_t _chunks_free;
 extern size_t _bytes_free;
 
 /* Internal version of `free' used in `morecore' (malloc.c). */
-extern void _free_internal __P ((__ptr_t __ptr));
+extern void _free_internal __GNU_P ((__ptr_t __ptr));
 
 #endif /* _MALLOC_INTERNAL.  */
 
 /* Underlying allocation function; successive calls should
    return contiguous pieces of memory.  */
-extern __ptr_t (*__morecore) __P ((ptrdiff_t __size));
+extern __ptr_t (*__morecore) __GNU_P ((ptrdiff_t __size));
 
 /* Default value of `__morecore'.  */
-extern __ptr_t __default_morecore __P ((ptrdiff_t __size));
+extern __ptr_t __default_morecore __GNU_P ((ptrdiff_t __size));
 
 /* If not NULL, this function is called after each time
    `__morecore' is called to increase the data size.  */
-extern void (*__after_morecore_hook) __P ((void));
+extern void (*__after_morecore_hook) __GNU_P ((void));
 
 /* Nonzero if `malloc' has been called and done its initialization.  */
 extern int __malloc_initialized;
 
 /* Hooks for debugging versions.  */
-extern void (*__free_hook) __P ((__ptr_t __ptr));
-extern __ptr_t (*__malloc_hook) __P ((size_t __size));
-extern __ptr_t (*__realloc_hook) __P ((__ptr_t __ptr, size_t __size));
+extern void (*__free_hook) __GNU_P ((__ptr_t __ptr));
+extern __ptr_t (*__malloc_hook) __GNU_P ((size_t __size));
+extern __ptr_t (*__realloc_hook) __GNU_P ((__ptr_t __ptr, size_t __size));
 
 /* Activate a standard collection of debugging hooks.  */
-extern int mcheck __P ((void (*__func) __P ((void))));
+extern int mcheck __GNU_P ((void (*__func) __GNU_P ((void))));
 
 /* Activate a standard collection of tracing hooks.  */
-extern void mtrace __P ((void));
+extern void mtrace __GNU_P ((void));
 
 /* Statistics available to the user.  */
 struct mstats
@@ -235,23 +235,23 @@ struct mstats
   };
 
 /* Pick up the current statistics. */
-extern struct mstats mstats __P ((void));
+extern struct mstats mstats __GNU_P ((void));
 
 /* Call WARNFUN with a warning message when memory usage is high.  */
-extern void memory_warnings __P ((__ptr_t __start,
-				  void (*__warnfun) __P ((__const char *))));
+extern void memory_warnings __GNU_P ((__ptr_t __start,
+				  void (*__warnfun) __GNU_P ((__const char *))));
 
 
 /* Relocating allocator.  */
 
 /* Allocate SIZE bytes, and store the address in *HANDLEPTR.  */
-extern __ptr_t r_alloc __P ((__ptr_t *__handleptr, size_t __size));
+extern __ptr_t r_alloc __GNU_P ((__ptr_t *__handleptr, size_t __size));
 
 /* Free the storage allocated in HANDLEPTR.  */
-extern void r_alloc_free __P ((__ptr_t *__handleptr));
+extern void r_alloc_free __GNU_P ((__ptr_t *__handleptr));
 
 /* Adjust the block at HANDLEPTR to be SIZE bytes long.  */
-extern __ptr_t r_re_alloc __P ((__ptr_t *__handleptr, size_t __size));
+extern __ptr_t r_re_alloc __GNU_P ((__ptr_t *__handleptr, size_t __size));
 
 
 #ifdef	__cplusplus
