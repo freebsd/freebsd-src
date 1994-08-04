3e7ddf301c924b4b846b632c851e3d0804dcd160
wollman <wollman@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1994-08-04 21:10:08 +0000

    Add a couple of files we created in 1.x.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@1845 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ share/mk/bsd.own.mk
@@ -0,0 +1,14 @@
+#	$Id: bsd.own.mk,v 1.5 1994/03/19 22:02:35 jkh Exp $
+
+BINGRP?=	bin
+BINOWN?=	bin
+BINMODE?=	555
+
+STRIP?=		-s
+
+COPY?=		-c
+
+MANDIR?=	/usr/share/man/man
+MANGRP?=	bin
+MANOWN?=	bin
+MANMODE?=	444
