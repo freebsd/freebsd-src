2e3b4b60108a0e2c9dd3fe496055b73761335c68
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1994-08-08 05:52:55 +0000

    This commit was generated by cvs2svn to compensate for changes in r1935, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@1936 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ gnu/lib/libg++/include/Random.h
@@ -0,0 +1,54 @@
+// This may look like C code, but it is really -*- C++ -*-
+/* 
+Copyright (C) 1988 Free Software Foundation
+    written by Dirk Grunwald (grunwald@cs.uiuc.edu)
+
+This file is part of the GNU C++ Library.  This library is free
+software; you can redistribute it and/or modify it under the terms of
+the GNU Library General Public License as published by the Free
+Software Foundation; either version 2 of the License, or (at your
+option) any later version.  This library is distributed in the hope
+that it will be useful, but WITHOUT ANY WARRANTY; without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the GNU Library General Public License for more details.
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the Free Software
+Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef _Random_h
+#define _Random_h 1
+#ifdef __GNUG__
+#pragma interface
+#endif
+
+#include <math.h>
+#include <RNG.h>
+
+class Random {
+protected:
+    RNG *pGenerator;
+public:
+    Random(RNG *generator);
+    virtual double operator()() = 0;
+
+    RNG *generator();
+    void generator(RNG *p);
+};
+
+
+inline Random::Random(RNG *gen)
+{
+    pGenerator = gen;
+}
+
+inline RNG *Random::generator()
+{
+    return(pGenerator);
+}
+
+inline void Random::generator(RNG *p)
+{
+    pGenerator = p;
+}
+
+#endif
