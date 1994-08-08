61d5ffaf10e8b89118dab351dc6117a08bee6887
Poul-Henning Kamp <phk@FreeBSD.org>
1994-08-08 05:52:55 +0000

    I belive this should be makeable under 2.0.  There may be outstanding issues in these areas:     .h's installed     .hP's installed     -lcurses interaction     files needed in ~/legal for copyleft reasons.

---
+++ gnu/lib/libg++/libio/cleanup.c
@@ -0,0 +1,14 @@
+#include "libioP.h"
+#if _G_HAVE_ATEXIT
+#include <stdlib.h>
+
+typedef void (*voidfunc) __P((void));
+
+static void _IO_register_cleanup ()
+{
+  atexit ((voidfunc)_IO_cleanup);
+  _IO_cleanup_registration_needed = 0;
+}
+
+void (*_IO_cleanup_registration_needed)() = _IO_register_cleanup;
+#endif /* _G_HAVE_ATEXIT */
