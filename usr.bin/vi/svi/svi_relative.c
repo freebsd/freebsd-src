d9334495576fcc6fc1f0599a250c3cac73128faf
sef <sef@FreeBSD.org>
1994-08-18 01:14:32 +0000

    Bringin in nvi 1.34.
    Reviewed by:	Sean Eric Fagan

--- usr.bin/vi/svi/svi_relative.c
+++ usr.bin/vi/svi/svi_relative.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static const char sccsid[] = "@(#)svi_relative.c	8.17 (Berkeley) 8/17/94";
+static char sccsid[] = "@(#)svi_relative.c	8.18 (Berkeley) 8/17/94";
 #endif /* not lint */
 
 #include <sys/types.h>
--- usr.bin/vi/svi/svi_screen.c
+++ usr.bin/vi/svi/svi_screen.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static const char sccsid[] = "@(#)svi_screen.c	8.93 (Berkeley) 8/17/94";
+static char sccsid[] = "@(#)svi_screen.c	8.94 (Berkeley) 8/17/94";
 #endif /* not lint */
 
 #include <sys/types.h>
