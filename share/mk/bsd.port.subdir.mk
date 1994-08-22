129f91e56b99443074f8b5a8b048e840770f88bd
jkh <jkh@FreeBSD.org>
1994-08-22 10:46:38 +0000

    I had to bite the bullet:  There's now a port.subdir.mk that does the right thing with recursive build, configure, bundle or extract targets. Reviewed by: Submitted by:

---
+++ share/mk/bsd.port.subdir.mk
@@ -0,0 +1,84 @@
+#	from: @(#)bsd.subdir.mk	5.9 (Berkeley) 2/1/91
+#	$Id$
+
+.MAIN: all
+
+STRIP?=	-s
+
+BINGRP?=	bin
+BINOWN?=	bin
+BINMODE?=	555
+
+_SUBDIRUSE: .USE
+	@for entry in ${SUBDIR}; do \
+		(if test -d ${.CURDIR}/$${entry}.${MACHINE}; then \
+			echo "===> ${DIRPRFX}$${entry}.${MACHINE}"; \
+			edir=$${entry}.${MACHINE}; \
+			cd ${.CURDIR}/$${edir}; \
+		else \
+			echo "===> ${DIRPRFX}$$entry"; \
+			edir=$${entry}; \
+			cd ${.CURDIR}/$${edir}; \
+		fi; \
+		${MAKE} ${.TARGET:realinstall=install} DIRPRFX=${DIRPRFX}$$edir/); \
+	done
+
+${SUBDIR}::
+	@if test -d ${.TARGET}.${MACHINE}; then \
+		cd ${.CURDIR}/${.TARGET}.${MACHINE}; \
+	else \
+		cd ${.CURDIR}/${.TARGET}; \
+	fi; \
+	${MAKE} all
+
+.if !target(all)
+all: _SUBDIRUSE
+.endif
+
+.if !target(extract)
+extract: _SUBDIRUSE
+.endif
+
+.if !target(configure)
+configure: _SUBDIRUSE
+.endif
+
+.if !target(build)
+build: _SUBDIRUSE
+.endif
+
+.if !target(clean)
+clean: _SUBDIRUSE
+.endif
+
+.if !target(depend)
+depend: _SUBDIRUSE
+.endif
+
+.if !target (maninstall)
+maninstall: _SUBDIRUSE
+.endif
+
+.if !target(install)
+.if !target(beforeinstall)
+beforeinstall:
+.endif
+.if !target(afterinstall)
+afterinstall:
+.endif
+install: afterinstall
+afterinstall: realinstall
+realinstall: beforeinstall _SUBDIRUSE
+.endif
+
+.if !target(lint)
+lint: _SUBDIRUSE
+.endif
+
+.if !target(obj)
+obj: _SUBDIRUSE
+.endif
+
+.if !target(tags)
+tags: _SUBDIRUSE
+.endif
