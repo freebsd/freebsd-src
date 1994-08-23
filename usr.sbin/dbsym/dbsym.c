995c83836e07de81d0df2f0775a50d88f698addd
jkh <jkh@FreeBSD.org>
1994-08-23 11:17:55 +0000

    Changes to adapt to 2.0. Submitted by:	jkh

--- usr.sbin/dbsym/dbsym.c
+++ usr.sbin/dbsym/dbsym.c
@@ -3,7 +3,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$Id: dbsym.c,v 1.2 1993/09/26 16:40:41 rgrimes Exp $";
+static char rcsid[] = "$Id: dbsym.c,v 1.3 1994/01/19 03:52:25 nate Exp $";
 #endif /* not lint */
 
 #include <stdio.h>
@@ -13,7 +13,7 @@ static char rcsid[] = "$Id: dbsym.c,v 1.2 1993/09/26 16:40:41 rgrimes Exp $";
 #include <machine/param.h>
 #include <vm/vm_param.h>
 #include <vm/lock.h>
-#include <vm/vm_statistics.h>
+#include <vm/pmap.h>
 #include <machine/pmap.h>
 #include <machine/vmparam.h>
 
