53e1aa653e755993a2b9e6c8026bf36b59b9b224
Andrey A. Chernov <ache@FreeBSD.org>
1994-09-07 16:52:33 +0000

    Bug fixed: when refreshing standouted line curses outputs SO for all characters Submitted by: ZW6T-KND@j.asahi-net.or.jp

--- lib/libcurses/refresh.c
+++ lib/libcurses/refresh.c
@@ -337,8 +337,8 @@ makech(win, wy)
 			}
 
 			/* Enter/exit standout mode as appropriate. */
-			if (SO && (nsp->attr & __STANDOUT) !=
-			    (curscr->flags & __WSTANDOUT)) {
+			if (SO && !(nsp->attr & __STANDOUT) !=
+			    !(curscr->flags & __WSTANDOUT)) {
 				if (nsp->attr & __STANDOUT) {
 					tputs(SO, 0, __cputchar);
 					curscr->flags |= __WSTANDOUT;
