07c3866a6e3d1bc84ae70e71b9dc5209d2c82321
dg <dg@FreeBSD.org>
1994-09-09 11:06:27 +0000

    Two fixes from the NetBSD group (Charles Hannum):
    1) dir.c: get byte order right in mkentry()
    2) pass1.c: When doing -c2 conversion, do secsize reads for a symlink -
       not doing so was causing the conversion to fail because the device
       driver can't deal with short reads.

--- sbin/fsck/dir.c
+++ sbin/fsck/dir.c
@@ -328,13 +328,20 @@ mkentry(idesc)
 	dirp->d_reclen = oldlen;
 	dirp = (struct direct *)(((char *)dirp) + oldlen);
 	dirp->d_ino = idesc->id_parent;	/* ino to be entered is in id_parent */
-	if (newinofmt)
+	if (newinofmt) {
 		dirp->d_type = typemap[idesc->id_parent];
-	else
-		dirp->d_type = 0;
+		dirp->d_namlen = newent.d_namlen;
+	} else {
+#		if (BYTE_ORDER == LITTLE_ENDIAN)
+			dirp->d_type = newent.d_namlen;
+			dirp->d_namlen = 0;
+#		else
+			dirp->d_type = 0;
+			dirp->d_namlen = newent.d_namlen;
+#		endif
+	}
 	dirp->d_reclen = newent.d_reclen;
-	dirp->d_namlen = newent.d_namlen;
-	bcopy(idesc->id_name, dirp->d_name, (size_t)dirp->d_namlen + 1);
+	bcopy(idesc->id_name, dirp->d_name, (size_t)newent.d_namlen + 1);
 	return (ALTERED|STOP);
 }
 
