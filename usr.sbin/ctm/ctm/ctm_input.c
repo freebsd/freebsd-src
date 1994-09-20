fa63aa6464cb231ea1d01c53e39c679f2c15a8e7
Poul-Henning Kamp <phk@FreeBSD.org>
1994-09-20 07:13:39 +0000

    A bunch of improvements.  Still far to go.

--- usr.sbin/ctm/ctm/ctm_input.c
+++ usr.sbin/ctm/ctm/ctm_input.c
@@ -1,3 +1,15 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@login.dknet.dk> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+ *
+ * $Id$
+ *
+ */
+
 #include "ctm.h"
 
 /*---------------------------------------------------------------------------*/
--- usr.sbin/ctm/ctm/ctm_pass3.c
+++ usr.sbin/ctm/ctm/ctm_pass3.c
@@ -1,4 +1,17 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@login.dknet.dk> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+ *
+ * $Id$
+ *
+ */
+
 #include "ctm.h"
+#define BADREAD 32
 
 /*---------------------------------------------------------------------------*/
 /* Pass3 -- Validate the incomming CTM-file.
@@ -113,6 +126,14 @@ Pass3(FILE *fd)
 		continue;
 	    }
 	    continue;
+	}
+	if(!strcmp(sp->Key,"FN")) {
+	    strcpy(buf,name);
+	    strcat(buf,".ctm");
+	    i = ctm_edit(trash,cnt,name,buf);
+	    if(i) {
+	    }
+	    rename(buf,name);
 	}
 	if(!strcmp(sp->Key,"DM")) {
 	    if(0 > mkdir(name,0755)) {
