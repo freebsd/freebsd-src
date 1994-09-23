32bff95b5576f71f7953fe0ae5cf1bb27c1a0e7e
David Greenman <dg@FreeBSD.org>
1994-09-23 08:28:43 +0000

    Fixed a couple of compiler warnings related to wrong prototypes.

--- gnu/usr.bin/awk/io.c
+++ gnu/usr.bin/awk/io.c
@@ -646,7 +646,8 @@ devopen(name, mode)
 const char *name, *mode;
 {
 	int openfd = INVALID_HANDLE;
-	const char *cp, *ptr;
+	const char *cp;
+	char *ptr;
 	int flag = 0;
 	struct stat buf;
 	extern double strtod();
@@ -806,7 +807,7 @@ const char *name, *mode;
 	char tbuf[BUFSIZ], *cp;
 	int i;
 #if defined(NGROUPS_MAX) && NGROUPS_MAX > 0
-#if defined(atarist) || defined(__svr4__) || defined(__osf__)
+#if defined(atarist) || defined(__svr4__) || defined(__osf__) || defined(__FreeBSD__)
 	gid_t groupset[NGROUPS_MAX];
 #else
 	int groupset[NGROUPS_MAX];
