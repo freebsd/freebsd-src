2f56b9ec91bb7b67388f817d0227d24eee7b5cf2
phk <phk@FreeBSD.org>
1994-09-27 20:35:55 +0000

    Added include of <machine/cons.h> so that cngetc() gets declared.

--- sys/ddb/db_ps.c
+++ sys/ddb/db_ps.c
@@ -30,12 +30,13 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id$
+ *	$Id: db_ps.c,v 1.1 1994/08/27 16:28:45 davidg Exp $
  */
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/proc.h>
 #include <ddb/db_output.h>
+#include <machine/cons.h>
 
 void
 db_ps() {
