e36707d539c6b062fadcbb6b203081b6a199cfe2
sos <sos@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1994-09-29 08:29:21 +0000

    Changed header slightly.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@3186 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/isa/kbdtables.h
+++ sys/i386/isa/kbdtables.h
@@ -1,20 +1,31 @@
-/*
- *  Copyright (C) 1992, 1993, 1994 Søren Schmidt
+/*-
+ * Copyright (c) 1992-1994 Søren Schmidt
+ * All rights reserved.
  *
- *  This program is free software; you may redistribute it and/or 
- *  modify it, provided that it retain the above copyright notice 
- *  and the following disclaimer.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer
+ *    in this position and unchanged.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software withough specific prior written permission
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *	Søren Schmidt 		Email:	sos@login.dkuug.dk
- *	Tritonvej 36		UUCP:	...uunet!dkuug!login!sos
- *	DK9210 Aalborg SO	Phone:  +45 9814 8076
- *
- *	@(#)kbdtables.h	1.3 940123
- *	$Id: kbdtables.h,v 1.12 1994/04/26 09:24:06 sos Exp $
+ *	$Id$
  */
 
 #define SET8 0x80       	/* eight bit for emacs SET8-key */
