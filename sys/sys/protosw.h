34a9d3d86955fe2df3dade7ffc4928c7ad81b896
phk <phk@FreeBSD.org>
1994-10-08 01:45:30 +0000

    Prototypes of today.  Brought to you by a 28 minute transit time on BART :-)
    (For the SF-unaware:  I ride the BART (The Bay-area subway) for half an hour
    each way to work.  I use the time to shut up gcc -Wall on my handbook).

--- sys/sys/protosw.h
+++ sys/sys/protosw.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)protosw.h	8.1 (Berkeley) 6/2/93
- * $Id: protosw.h,v 1.2 1994/08/02 07:53:22 davidg Exp $
+ * $Id: protosw.h,v 1.3 1994/08/21 04:41:56 paul Exp $
  */
 
 #ifndef _SYS_PROTOSW_H_
@@ -211,6 +211,7 @@ char	*prcorequests[] = {
 
 #ifdef KERNEL
 extern	struct protosw *pffindproto(), *pffindtype();
+void	pfctlinput __P((int,struct sockaddr *));
 #endif
 
 #endif
