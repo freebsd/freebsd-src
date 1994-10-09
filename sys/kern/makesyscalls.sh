dd90f6a35847af8c2952c38ab8eba655f3d5d4ad
sos <sos@FreeBSD.org>
1994-10-09 22:07:38 +0000

    Added errno conversion table (initialize to zero).

--- sys/kern/makesyscalls.sh
+++ sys/kern/makesyscalls.sh
@@ -1,6 +1,6 @@
 #! /bin/sh -
 #	@(#)makesyscalls.sh	8.1 (Berkeley) 6/10/93
-# $Id: makesyscalls.sh,v 1.7 1994/09/13 02:21:48 wollman Exp $
+# $Id: makesyscalls.sh,v 1.8 1994/09/26 03:27:22 phk Exp $
 
 set -e
 
@@ -180,7 +180,7 @@ awk < $1 "
 		printf ("struct sysentvec aout_sysvec = {\n") > sysent
 		printf ("\tsizeof (sysent) / sizeof (sysent[0]),\n") > sysent
 		printf ("\tsysent,\n") > sysent
-		printf ("\t0,\n\t0,\n\t0\n};\n") > sysent
+		printf ("\t0,\n\t0,\n\t0,\n\t0,\n\t0\n};\n") > sysent
 		printf("};\n") > sysnames
 	} '
 
