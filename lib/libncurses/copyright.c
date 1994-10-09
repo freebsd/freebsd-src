eb3b7db32ddd17f7ead15ccef9a7693b230256e4
ache <ache@FreeBSD.org>
1994-10-09 09:27:55 +0000

    Add copyright.c to be always linked in

---
+++ lib/libncurses/copyright.c
@@ -0,0 +1,19 @@
+char _ncurses_copyright[] = "\
+/***************************************************************************\n\
+*                         COPYRIGHT NOTICE                                 *\n\
+****************************************************************************\n\
+*              ncurses is copyright (C) 1992, 1993, 1994                   *\n\
+*                        by Zeyd M. Ben-Halim                              *\n\
+*                        zmbenhal@netcom.com                               *\n\
+*                                                                          *\n\
+*        Permission is hereby granted to reproduce and distribute ncurses  *\n\
+*        by any means and for any fee, whether alone or as part of a       *\n\
+*        larger distribution, in source or in binary form, PROVIDED        *\n\
+*        this notice remains in the sources and is reproduced in any       *\n\
+*        documentation accompanying it or the applications linked with it. *\n\
+*        Any other use of ncurses, including incorporating it in another   *\n\
+*        product is subject to written permission.                         *\n\
+*                                                                          *\n\
+*        ncurses comes AS IS with no warranty, implied or expressed.       *\n\
+*                                                                          *\n\
+***************************************************************************/\n";
