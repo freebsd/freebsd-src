f665d68d0423089a43747e22cc474e2a4dd5adff
Garrett Wollman <wollman@FreeBSD.org>
1994-10-11 02:13:21 +0000

    Make `id' and `groups' somewhat faster and less cpu-intensive :-)
    Really!

--- usr.bin/id/whoami.sh
+++ usr.bin/id/whoami.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -
+exec id -un
 #
 # Copyright (c) 1991, 1993
 #	The Regents of the University of California.  All rights reserved.
@@ -34,4 +35,3 @@
 #	@(#)whoami.sh	8.1 (Berkeley) 6/6/93
 #
 
-id -un
