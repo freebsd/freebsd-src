f6c7206456ee32ed5b4ec37bd87516a68c3bda58
wollman <wollman@FreeBSD.org>
1994-10-11 02:13:21 +0000

    Make `id' and `groups' somewhat faster and less cpu-intensive :-)
    Really!

--- usr.bin/id/groups.sh
+++ usr.bin/id/groups.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -
+exec id -Gn $*
 #
 # Copyright (c) 1991, 1993
 #	The Regents of the University of California.  All rights reserved.
@@ -34,4 +35,3 @@
 #	@(#)groups.sh	8.1 (Berkeley) 6/6/93
 #
 
-id -Gn $*
