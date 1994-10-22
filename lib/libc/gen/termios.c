586db7f5405ee5f51a10247aa40332e1bfbab15b
ache <ache@FreeBSD.org>
1994-10-22 18:12:17 +0000

    cfmakeraw(): set IGNBRK, clear IXOFF, INPCK per Bruce suggestion Set IGNPAR, clear NOFLSH, PENDIN, TOSTOP, ECHOE, ECHOK

--- lib/libc/gen/termios.c
+++ lib/libc/gen/termios.c
@@ -163,9 +163,10 @@ cfmakeraw(t)
 	struct termios *t;
 {
 
-	t->c_iflag &= ~(IMAXBEL|IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL|IXON);
+	t->c_iflag &= ~(IMAXBEL|IXOFF|INPCK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL|IXON);
+	t->c_iflag |= (IGNBRK|IGNPAR);
 	t->c_oflag &= ~OPOST;
-	t->c_lflag &= ~(ECHO|ECHONL|ICANON|ISIG|IEXTEN);
+	t->c_lflag &= ~(ECHO|ECHOE|ECHOK|ECHONL|ICANON|ISIG|IEXTEN|NOFLSH|TOSTOP|PENDIN);
 	t->c_cflag &= ~(CSIZE|PARENB);
 	t->c_cflag |= CS8;
 	t->c_cc[VMIN] = 1;
