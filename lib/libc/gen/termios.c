4ceeaa06a816ea093b81fd2465320e7e34ebed1f
Andrey A. Chernov <ache@FreeBSD.org>
1994-10-22 01:49:27 +0000

    makeraw(): forget to clear IMAXBEL, set VMIN/VTIME

--- lib/libc/gen/termios.c
+++ lib/libc/gen/termios.c
@@ -163,12 +163,13 @@ cfmakeraw(t)
 	struct termios *t;
 {
 
-	t->c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL|IXON);
+	t->c_iflag &= ~(IMAXBEL|IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL|IXON);
 	t->c_oflag &= ~OPOST;
 	t->c_lflag &= ~(ECHO|ECHONL|ICANON|ISIG|IEXTEN);
 	t->c_cflag &= ~(CSIZE|PARENB);
 	t->c_cflag |= CS8;
-	/* XXX set MIN/TIME */
+	t->c_cc[VMIN] = 1;
+	t->c_cc[VTIME] = 0;
 }
 
 tcsendbreak(fd, len)
