a82aaac407f739e7826e976f4516039c60486a84
ache <ache@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1994-11-04 05:34:16 +0000

    Prepare for 1.8.6 import
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@4134 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/ncftp/.cvsimport.sh
+++ usr.bin/ncftp/.cvsimport.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-tar zxvf ncftp185.tgz
-cd ncftp185
-mv Makefile Makefile.ORIG
-cvs import src/usr.bin/ncftp mgleason ncftp_1_8_5
+tar zxvf ncftp186.tgz
+cd ncftp186
+rm Makefile
+cvs import src/usr.bin/ncftp mgleason ncftp_1_8_6
