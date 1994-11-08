70201e10034a02af82b323e73bf4e192e35027af
Jordan K. Hubbard <jkh@FreeBSD.org>
1994-11-08 11:14:45 +0000

    11th hour fixes to get the install just right.  Fix some bugs in how ftp transfers were done, make sure all output goes to /dev/ttyv1 for debugging, add in the parallel-IP support so Poul will stop squeaking (for the moment, anyway).

--- release/extract.sh
+++ release/extract.sh
@@ -1,4 +1,7 @@
-:
+#!/bin/sh
 if [ -f bin_tgz.aa ] ; then
+	echo; echo "Extracting bindist, please wait.  Ignore any messages from"
+	echo "cpio saying \"No such file or directory\".  It doesn't know what"
+	echo "it's talking about.."; echo
 	cat bin_tgz.?? | zcat | ( cd / ; cpio -H tar -idumV )
 fi
