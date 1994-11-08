ab2858472b1b861827d4e45c2cc441a07620d4e1
jkh <jkh@FreeBSD.org>
1994-11-08 11:17:29 +0000

    Temporarily correct for Poul's insane serial-killer bindist (that wants to kill myname and wipe out my oh-so-carefully constructed hosts file).

--- release/extract.sh
+++ release/extract.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 if [ -f bin_tgz.aa ] ; then
+	# Temporary kludge for pathological bindist.
+	cp /etc/hosts /etc/myname /stand/etc
 	echo; echo "Extracting bindist, please wait.  Ignore any messages from"
 	echo "cpio saying \"No such file or directory\".  It doesn't know what"
 	echo "it's talking about.."; echo
 	cat bin_tgz.?? | zcat | ( cd / ; cpio -H tar -idumV )
+	# Add back what the bindist nuked.
+	cp /stand/etc/myname /etc
+	cat /stand/etc/hosts >> /etc/hosts
 fi
