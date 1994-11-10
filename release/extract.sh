7ddd763c09c5f1b4742039df76e8b83aeb3a12d3
phk <phk@FreeBSD.org>
1994-11-10 02:24:47 +0000

    Make it extract anything it finds...

--- release/extract.sh
+++ release/extract.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
+# $Id$
+
+DDIR=/
+
 if [ -f bin_tgz.aa ] ; then
 	# Temporary kludge for pathological bindist.
-	cp /etc/hosts /etc/myname /stand/etc
+	cp $DDIR/etc/hosts $DDIR/etc/myname $DDIR/stand/etc
 	echo; echo "Extracting bindist, please wait.  Ignore any messages from"
 	echo "cpio saying \"No such file or directory\".  It doesn't know what"
 	echo "it's talking about.."; echo
-	cat bin_tgz.?? | zcat | ( cd / ; cpio -H tar -idumV )
+	cat bin_tgz.?? | zcat | ( cd $DDIR ; cpio -H tar -idumV )
 	# Add back what the bindist nuked.
-	cp /stand/etc/myname /etc
-	cat /stand/etc/hosts >> /etc/hosts
+	cp $DDIR/stand/etc/myname $DDIR/etc
+	cat $DDIR/stand/etc/hosts >> $DDIR/etc/hosts
 fi
+
+for i in *.aa
+do
+	b=`basename $i .aa`
+	echo "Extracting $b"
+	if [ "$b" != bin_tgz ] ; then
+		cat $b.?? | zcat | ( cd $DDIR ; tar xf - )
+	fi
+done
