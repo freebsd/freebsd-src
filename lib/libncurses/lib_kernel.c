feae074176a7eb168667fdbf4751e32a4bda9a1d
ache <ache@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1994-11-10 13:10:21 +0000

    Add 57600, 115200 ro baudrate()
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@4342 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libncurses/lib_kernel.c
+++ lib/libncurses/lib_kernel.c
@@ -167,30 +167,40 @@ struct speed {
 };
 
 static struct speed speeds[] = {
-	{B0, 0,},
-	{B50, 50,},
-	{B75, 75,},
-	{B110, 110,},
-	{B134, 134,},
-	{B150, 150,},
-	{B200, 200,},
-	{B300, 300,},
-	{B600, 600,},
-	{B1200, 1200,},
-	{B1800, 1800,},
-	{B2400, 2400,},
-	{B4800, 4800,},
-	{B9600, 9600,},
+	{B0, 0},
+	{B50, 50},
+	{B75, 75},
+	{B110, 110},
+	{B134, 134},
+	{B150, 150},
+	{B200, 200},
+	{B300, 300},
+	{B600, 600},
+	{B1200, 1200},
+	{B1800, 1800},
+	{B2400, 2400},
+	{B4800, 4800},
+	{B9600, 9600}
 #define MAX_BAUD	B9600
 #ifdef B19200
 #undef MAX_BAUD
 #define MAX_BAUD	B19200
-	{B19200, 19200,},
+	,{B19200, 19200}
 #endif
 #ifdef B38400
 #undef MAX_BAUD
 #define MAX_BAUD	B38400
-	{B38400, 38000,}
+	,{B38400, 38400}
+#endif
+#ifdef B57600
+#undef MAX_BAUD
+#define MAX_BAUD        B57600
+	,{B57600, 57600}
+#endif
+#ifdef B115200
+#undef MAX_BAUD
+#define MAX_BAUD        B115200
+	,{B115200, 115200}
 #endif
 };
 
