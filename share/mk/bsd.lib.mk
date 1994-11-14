4feb253d9476f55aa7464775e43d0c4a6ef8027a
bde <bde@FreeBSD.org>
1994-11-14 06:28:20 +0000

    This change was actually made in the previous revision:
    Define SHLIBDIR?= ${LIBDIR} and install shared libraries in
    ${DESTDIR}${SHLIBDIR} instead of in ${DESTDIR}${LIBDIR}.
    SHLIBDIR may be defined in /etc/make.conf to override the
    default of /usr/lib (I use /lib).  Other changes are required
    for non-default shared library directories to actually work
    (ld* and crtso have too many hard-coded paths).
    
    Bruce

--- share/mk/bsd.lib.mk
+++ share/mk/bsd.lib.mk
@@ -1,5 +1,5 @@
 #	from: @(#)bsd.lib.mk	5.26 (Berkeley) 5/2/91
-#	$Id: bsd.lib.mk,v 1.11 1994/11/13 21:05:25 phk Exp $
+#	$Id: bsd.lib.mk,v 1.12 1994/11/14 04:55:40 bde Exp $
 #
 
 .if exists(${.CURDIR}/../Makefile.inc)
