4d10f8e3af54ae101566ca07c4e33c9ec73659a6
ache <ache@FreeBSD.org>
1994-11-16 14:37:37 +0000

    Full autosizing support, now you can pass -1, -1 for any string

--- gnu/lib/libdialog/msgbox.c
+++ gnu/lib/libdialog/msgbox.c
@@ -29,8 +29,16 @@
  */
 int dialog_msgbox(unsigned char *title, unsigned char *prompt, int height, int width, int pause)
 {
-  int i, x, y, key = 0;
+  int i, j, x, y, key = 0;
   WINDOW *dialog;
+
+  if (height < 0)
+	height = strheight(prompt)+2+2*(!!pause);
+  if (width < 0) {
+	i = strwidth(prompt);
+	j = strwidth(title);
+	width = MAX(i,j)+4;
+  }
 
   /* center dialog box on screen */
   x = (COLS - width)/2;
