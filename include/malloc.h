f6fda6cebcf89db1698d15c87b06a20b9936e03a
ache <ache@FreeBSD.org>
1994-11-17 11:04:49 +0000

    By Bruce and Joerg suggestions and  by looking into June version of NetBSD simple #include <stdlib.h> into malloc.h Put #warning that this file is obsoleted ( by Joerg suggestion)

--- include/malloc.h
+++ include/malloc.h
@@ -1,16 +1,5 @@
-/* This file is SYSV-ism (for compatibility only) */
-/* this declarations must match stdlib.h ones */
+#if __GNUC__
+#warning "this file includes <malloc.h> which is obsoleted, use <stdlib.h> instead"
+#endif
 
-#if !defined(_MALLOC_H_) && !defined(_STDLIB_H_)
-#define _MALLOC_H_
-
-#include <sys/cdefs.h>
-
-__BEGIN_DECLS
-void	*calloc __P((size_t, size_t));
-void	 free __P((void *));
-void	*malloc __P((size_t));
-void	*realloc __P((void *, size_t));
-__END_DECLS
-
-#endif /* _MALLOC_H_ */
+#include <stdlib.h>
