6eef72c55e5d22886232bdc6fbee7c6a274f505e
ats <ats@FreeBSD.org>
1994-11-18 22:40:11 +0000

    Reviewed by:	Roger Holst roger@first.gmd.de Add an initialization of the len parameter for the getsockname call. Now rpc.rusersd can run under inetd.

--- libexec/rpc.rusersd/rusersd.c
+++ libexec/rpc.rusersd/rusersd.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$Id: rusersd.c,v 1.1 1993/09/16 00:34:49 jtc Exp $";
+static char rcsid[] = "$Id: rusersd.c,v 1.1.1.1 1994/08/28 14:53:45 csgr Exp $";
 #endif /* not lint */
 
 #include <stdio.h>
@@ -68,6 +68,7 @@ main(argc, argv)
         /*
          * See if inetd started us
          */
+	fromlen = sizeof(from);
         if (getsockname(0, (struct sockaddr *)&from, &fromlen) < 0) {
                 from_inetd = 0;
                 sock = RPC_ANYSOCK;
