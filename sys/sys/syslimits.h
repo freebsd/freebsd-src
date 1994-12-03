5968eb8edbbfab6a4e2884b5eb0abc003ac9809d
smace <smace@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1994-12-03 17:36:37 +0000

    Make CHILD_MAX and OPEN_MAX a tunable parameter.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@4934 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/syslimits.h
+++ sys/sys/syslimits.h
@@ -31,20 +31,24 @@
  * SUCH DAMAGE.
  *
  *	@(#)syslimits.h	8.1 (Berkeley) 6/2/93
- * $Id: syslimits.h,v 1.4 1994/08/08 09:12:43 davidg Exp $
+ * $Id: syslimits.h,v 1.5 1994/08/21 04:41:59 paul Exp $
  */
 
 #ifndef _SYS_SYSLIMITS_H_
 #define _SYS_SYSLIMITS_H_
 
 #define	ARG_MAX			65536	/* max bytes for an exec function */
+#ifndef CHILD_MAX
 #define	CHILD_MAX		   40	/* max simultaneous processes */
+#endif
 #define	LINK_MAX		32767	/* max file link count */
 #define	MAX_CANON		  255	/* max bytes in term canon input line */
 #define	MAX_INPUT		  255	/* max bytes in terminal input */
 #define	NAME_MAX		  255	/* max bytes in a file name */
 #define	NGROUPS_MAX		   16	/* max supplemental group id's */
+#ifndef OPEN_MAX
 #define	OPEN_MAX		   64	/* max open files per process */
+#endif
 #define	PATH_MAX		 1024	/* max bytes in pathname */
 #define	PIPE_BUF		  512	/* max bytes for atomic pipe writes */
 
