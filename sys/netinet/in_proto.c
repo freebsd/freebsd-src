1db18e0fc4d2d30b61da7a24d1fd378fe4c2a8f0
Garrett Wollman <wollman@FreeBSD.org>
1994-12-11 21:36:10 +0000

    Advanced route cache management is now an official part of IP support.

--- sys/netinet/in_proto.c
+++ sys/netinet/in_proto.c
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)in_proto.c	8.1 (Berkeley) 6/10/93
- * $Id: in_proto.c,v 1.6 1994/09/15 10:36:52 davidg Exp $
+ * $Id: in_proto.c,v 1.7 1994/11/02 04:41:39 wollman Exp $
  */
 
 #include <sys/param.h>
@@ -154,18 +154,12 @@ struct protosw inetsw[] = {
 },
 };
 
-#ifdef IN_RMX
 extern int in_inithead(void **, int);
-#endif
 
 struct domain inetdomain =
     { AF_INET, "internet", 0, 0, 0, 
       inetsw, &inetsw[sizeof(inetsw)/sizeof(inetsw[0])], 0,
-#ifdef IN_RMX
       in_inithead, 32, sizeof(struct sockaddr_in)
-#else
-      rn_inithead, 32, sizeof(struct sockaddr_in)
-#endif
     };
 
 #include "imp.h"
