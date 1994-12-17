2a78ff483e4d22eb49f55f8ad890c9860a43c44f
gpalmer <gpalmer@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1994-12-17 20:50:44 +0000

    bsd.port.mk: missing semicolon added bsd.port.subdir.mk: added check-md5 as target
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@5138 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/bsd.port.subdir.mk
+++ share/mk/bsd.port.subdir.mk
@@ -1,5 +1,5 @@
 #	from: @(#)bsd.subdir.mk	5.9 (Berkeley) 2/1/91
-#	$Id: bsd.port.subdir.mk,v 1.5 1994/09/16 14:30:22 jkh Exp $
+#	$Id: bsd.port.subdir.mk,v 1.6 1994/11/17 16:02:56 jkh Exp $
 
 .MAIN: all
 
@@ -83,4 +83,8 @@ realinstall: beforeinstall _SUBDIRUSE
 
 .if !target(tags)
 tags: _SUBDIRUSE
+.endif
+
+.if !target(check-md5)
+check-md5: _SUBDIRUSE
 .endif
