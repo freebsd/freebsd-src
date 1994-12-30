7010a8abed78129f0b1199dd403af7981d5d4682
Bruce Evans <bde@FreeBSD.org>
1994-12-30 07:48:07 +0000

    Save 16 bytes of data by not explicitly initializing to 0.

--- sys/i386/boot/biosboot/sys.c
+++ sys/i386/boot/biosboot/sys.c
@@ -24,7 +24,7 @@
  * the rights to redistribute these changes.
  *
  *	from: Mach, Revision 2.2  92/04/04  11:36:34  rpd
- *	$Id: sys.c,v 1.5 1994/09/20 22:25:00 adam Exp $
+ *	$Id: sys.c,v 1.6 1994/11/07 11:26:30 davidg Exp $
  */
 
 #include "boot.h"
@@ -144,7 +144,7 @@ loop:	iodest = iobuf;
 }
 
 char mapbuf[MAPBUFSIZE];
-int mapblock = 0;
+int mapblock;
 
 block_map(file_block)
      int file_block;
