cde6c872cc37b1aff6c2674565fd7caa2d5be1e1
ache <ache@FreeBSD.org>
1995-01-03 17:25:13 +0000

    Upgrade

--- usr.bin/ncftp/.cvsimport.sh
+++ usr.bin/ncftp/.cvsimport.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-tar zxvf ncftp-1.9.0.tgz
-cd ncftp-1.9.0
+tar zxvf ncftp-1.9.1.tgz
+cd ncftp-1.9.1
 rm Makefile
-cvs import src/usr.bin/ncftp mgleason ncftp_1_9_0
+cvs import src/usr.bin/ncftp mgleason ncftp_1_9_1
