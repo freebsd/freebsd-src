2469776155428b0a2e080eee1220634fa68bb983
ache <ache@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1995-01-04 21:29:32 +0000

    Much improved variant
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@5380 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/bsd.info.mk
+++ share/mk/bsd.info.mk
@@ -1,16 +1,64 @@
+BINMODE?=	444
 BINDIR?=	/usr/share/info
 MAKEINFO?=	makeinfo
 MAKEINFOFLAGS?=	# --no-split would simplify some things, e.g., compression
 
-.SUFFIXES: .info .texi
+.SUFFIXES: .info .texi .texinfo
 .texi.info:
 	${MAKEINFO} ${MAKEINFOFLAGS} ${.IMPSRC} -o ${.TARGET}
+.texinfo.info:
+	${MAKEINFO} ${MAKEINFOFLAGS} ${.IMPSRC} -o ${.TARGET}
 
+.if defined(SRCS)
 all: ${INFO:S/$/.info/g}
+	${MAKEINFO} ${MAKEINFOFLAGS} ${SRCS} -o ${INFO:S/$/.info/g}
+.else
+all: ${INFO:S/$/.info/g}
+.endif
 
-# Hacks to interface to bsd.doc.mk.
-DOC=		${INFO}
-PRINTER=	texi
-depend:
+depend:;
 
-.include <bsd.doc.mk>
+.if !target(obj)
+.if defined(NOOBJ)
+obj:
+.else
+obj:
+	@cd ${.CURDIR}; rm -f obj > /dev/null 2>&1 || true; \
+	here=`pwd`; subdir=`echo $$here | sed 's,^/usr/src/,,'`; \
+	if test $$here != $$subdir ; then \
+		dest=/usr/obj/$$subdir ; \
+		${ECHO} "$$here -> $$dest"; ln -s $$dest obj; \
+		if test -d /usr/obj -a ! -d $$dest; then \
+			mkdir -p $$dest; \
+		else \
+			true; \
+		fi; \
+	else \
+		true ; \
+		dest=$$here/obj ; \
+		${ECHO} "making $$here/obj" ; \
+		if test ! -d obj ; then \
+			mkdir $$here/obj; \
+		fi ; \
+	fi;
+.endif
+.endif
+
+clean:
+	rm -f ${INFO:S/$/.info*/g} [eE]rrs mklog ${CLEANFILES}
+
+cleandir: clean
+	cd ${.CURDIR}; rm -rf obj
+
+install:
+	@if [ ! -d "${DESTDIR}${BINDIR}" ]; then \
+		/bin/rm -f ${DESTDIR}${BINDIR}  ; \
+		mkdir -p ${DESTDIR}${BINDIR}  ; \
+		chown root.wheel ${DESTDIR}${BINDIR}  ; \
+		chmod 755 ${DESTDIR}${BINDIR}  ; \
+        else \
+                true ; \
+        fi
+	${INSTALL} ${COPY} -o ${BINOWN} -g ${BINGRP} -m ${BINMODE} \
+		${INFO:S/$/.info*/g} ${DESTDIR}${BINDIR}
+
