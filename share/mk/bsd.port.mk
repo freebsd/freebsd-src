e541856002a0d71325385f981306c3ac5f6f2cc7
asami <asami@FreeBSD.org>
1995-01-11 08:50:48 +0000

    Add missing ! in front of pipeline for extract commands---otherwise it would exit on *successful* extraction.

--- share/mk/bsd.port.mk
+++ share/mk/bsd.port.mk
@@ -3,7 +3,7 @@
 #	bsd.port.mk - 940820 Jordan K. Hubbard.
 #	This file is in the public domain.
 #
-# $Id: bsd.port.mk,v 1.98 1995/01/06 22:14:12 ache Exp $
+# $Id: bsd.port.mk,v 1.99 1995/01/10 12:23:44 jkh Exp $
 #
 # Please view me with 4 column tabs!
 
@@ -549,13 +549,13 @@ ${EXTRACT_COOKIE}:
 	@mkdir -p ${WRKDIR}
 .if defined(EXTRACT_ONLY)
 	@for file in ${EXTRACT_ONLY}; do \
-		if ${EXTRACT_CMD} ${EXTRACT_ARGS} ${DISTDIR}/$$file; then \
+		! if ${EXTRACT_CMD} ${EXTRACT_ARGS} ${DISTDIR}/$$file; then \
 			exit 1; \
 		fi \
 	done
 .else
 	@for file in ${DISTFILES}; do \
-		if ${EXTRACT_CMD} ${EXTRACT_ARGS} ${DISTDIR}/$$file; then \
+		! if ${EXTRACT_CMD} ${EXTRACT_ARGS} ${DISTDIR}/$$file; then \
 			exit 1; \
 		fi \
 	done
