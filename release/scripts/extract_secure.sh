30d561922ba76535b3d2c17c080a2bab00d10d73
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-01-14 07:44:23 +0000

    Do the right thing with init for secrdist.

--- release/scripts/extract_secure.sh
+++ release/scripts/extract_secure.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-# $Id: extract.sh,v 1.17 1994/12/04 03:41:18 jkh Exp $
+# $Id: extract_secure.sh,v 1.1 1995/01/14 07:41:45 jkh Exp $
 PATH=/stand:$PATH
 DDIR=/
 
 DIST=secure
+# You can't write over the running init
+if [ -f /sbin/init ]; then mv /sbin/init /sbin/init.insecure; fi
+
 echo "Extracting ${DIST}"
 cat ${DIST}.?? | gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
