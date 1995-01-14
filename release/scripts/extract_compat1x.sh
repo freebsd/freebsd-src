b78bfc62ee368e62155700874a95d622e90b960c
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1995-01-14 07:41:52 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@5582 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ release/scripts/extract_compat1x.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# $Id: extract.sh,v 1.17 1994/12/04 03:41:18 jkh Exp $
+PATH=/stand:$PATH
+DDIR=/
+
+DIST=compat1x_tgz
+echo "Extracting ${DIST}"
+cat ${DIST}.?? | gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
