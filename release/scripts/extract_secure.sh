8d27b7fcda9fb2dd05616ae6a0e3eaa49f896c27
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-01-27 07:49:46 +0000

    Adjust some of these extract scripts.

--- release/scripts/extract_secure.sh
+++ release/scripts/extract_secure.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: extract_secure.sh,v 1.1 1995/01/14 07:41:45 jkh Exp $
+# $Id: extract_secure.sh,v 1.2 1995/01/14 07:44:23 jkh Exp $
 PATH=/stand:$PATH
 DDIR=/
 
@@ -9,3 +9,10 @@ if [ -f /sbin/init ]; then mv /sbin/init /sbin/init.insecure; fi
 
 echo "Extracting ${DIST}"
 cat ${DIST}.?? | gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
+
+for DIST in ebones secrsrc; do
+	if [ -f ${DIST}.aa ]; then
+		echo "Extracting ${DIST} sources"
+		cat ${DIST}.?? | gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
+	fi
+done
