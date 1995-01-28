c4f97fa185095db2dfac9405658100bf51b57850
jkh <jkh@FreeBSD.org>
1995-01-28 09:07:48 +0000

    Print message about ignoring stupid warnings in game dist Don't ask user to press return after every dist.

--- release/scripts/extract_games.sh
+++ release/scripts/extract_games.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# $Id: extract.sh,v 1.17 1994/12/04 03:41:18 jkh Exp $
+# $Id: extract_games.sh,v 1.1 1995/01/14 07:41:41 jkh Exp $
 PATH=/stand:$PATH
 DDIR=/
 
 DIST=games
-echo "Extracting ${DIST}"
+echo "Extracting ${DIST} - ignore any errors from cpio"
 cat ${DIST}.?? | gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
