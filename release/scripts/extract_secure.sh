1bdc0476020097e6a38c7d1dcb217bb2d6c8fc9f
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-01-28 09:11:39 +0000

    Make some of the extract scripts more explicit.

--- release/scripts/extract_secure.sh
+++ release/scripts/extract_secure.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: extract_secure.sh,v 1.2 1995/01/14 07:44:23 jkh Exp $
+# $Id: extract_secure.sh,v 1.3 1995/01/27 07:49:45 jkh Exp $
 PATH=/stand:$PATH
 DDIR=/
 
@@ -7,7 +7,6 @@ DIST=secure
 # You can't write over the running init
 if [ -f /sbin/init ]; then mv /sbin/init /sbin/init.insecure; fi
 
-echo "Extracting ${DIST}"
 cat ${DIST}.?? | gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
 
 for DIST in ebones secrsrc; do
