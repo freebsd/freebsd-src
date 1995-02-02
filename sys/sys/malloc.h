761c79bffef2212388f737488b0d632c0a9fef08
dg <dg@FreeBSD.org>
1995-02-02 08:49:45 +0000

    Calling semantics for kmem_malloc() have been changed...and the third argument is now more than just a single flag.
    Submitted by:	John Dyson

--- sys/sys/malloc.h
+++ sys/sys/malloc.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)malloc.h	8.3 (Berkeley) 1/12/94
- * $Id: malloc.h,v 1.4 1994/09/19 15:41:55 dfr Exp $
+ * $Id: malloc.h,v 1.5 1994/10/04 06:51:36 phk Exp $
  */
 
 #ifndef _SYS_MALLOC_H_
@@ -44,6 +44,7 @@
  */
 #define	M_WAITOK	0x0000
 #define	M_NOWAIT	0x0001
+#define M_KERNEL	0x0002
 
 /*
  * Types of memory to be allocated
