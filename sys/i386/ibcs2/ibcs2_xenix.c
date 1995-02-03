dd55809b65cafd12917ca99b9bf306a58131164a
bde <bde@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1995-02-03 21:31:35 +0000

    Don't depend on namespace pollution in <machine/limits.h> for the definition of CLK_TCK.  Use _BSD_CLK_TCK_ instead of CLK_TCK so that we don't have to include the user header <time.h>.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@6163 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/ibcs2/ibcs2_xenix.c
+++ sys/i386/ibcs2/ibcs2_xenix.c
@@ -26,7 +26,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *	$Id: ibcs2_xenix.c,v 1.10 1994/10/12 19:38:38 sos Exp $
+ *	$Id: ibcs2_xenix.c,v 1.1 1994/10/14 08:53:12 sos Exp $
  */
 
 #include <i386/ibcs2/ibcs2.h>
@@ -251,7 +251,7 @@ ibcs2_cxenix(struct proc *p, void *args, int *retval)
 			*retval = (CHILD_MAX); 
 			break;
 		case 2:		/* _SC_CLK_TCK */
-			*retval = (CLK_TCK);
+			*retval = (_BSD_CLK_TCK_);
 			break;
 		case 3:		/* _SC_NGROUPS_MAX */
 			*retval = (NGROUPS_MAX);
