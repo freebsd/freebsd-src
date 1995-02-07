c5c3febed7f4fa16edd03848f1580c9196409e70
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-02-07 01:01:21 +0000

    Extract the sources into the right place!  Can't believe I didn't notice this before.

--- release/scripts/extract_secure.sh
+++ release/scripts/extract_secure.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: extract_secure.sh,v 1.3 1995/01/27 07:49:45 jkh Exp $
+# $Id: extract_secure.sh,v 1.4 1995/01/28 09:11:39 jkh Exp $
 PATH=/stand:$PATH
 DDIR=/
 
@@ -9,6 +9,7 @@ if [ -f /sbin/init ]; then mv /sbin/init /sbin/init.insecure; fi
 
 cat ${DIST}.?? | gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
 
+DDIR=/usr/src
 for DIST in ebones secrsrc; do
 	if [ -f ${DIST}.aa ]; then
 		echo "Extracting ${DIST} sources"
