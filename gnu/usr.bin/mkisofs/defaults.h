2c427653c39c60269b3a6243237a5ee2deccdbfb
jkh <jkh@FreeBSD.org>
1995-02-12 08:00:42 +0000

    Original virgin sources for version 1.2 of mkisofs, by Eric Youngdale of Yggdrasil Computing Inc.  This is a useful concession to us CDROM mastering weenies out here. Obtained from: Yggdrasil Computing, Inc.

---
+++ gnu/usr.bin/mkisofs/defaults.h
@@ -0,0 +1,18 @@
+/*
+ * Header file defaults.h - assorted default values for character strings in
+ * the volume descriptor.
+ */
+
+#define  PREPARER_DEFAULT 	NULL
+#define  PUBLISHER_DEFAULT	NULL
+#define  APPID_DEFAULT 		NULL
+#define  COPYRIGHT_DEFAULT 	NULL
+#define  BIBLIO_DEFAULT 	NULL
+#define  ABSTRACT_DEFAULT 	NULL
+#define  VOLSET_ID_DEFAULT 	NULL
+#define  VOLUME_ID_DEFAULT 	"CDROM"
+#ifdef __QNX__
+#define  SYSTEM_ID_DEFAULT 	"QNX"
+#else
+#define  SYSTEM_ID_DEFAULT 	"LINUX"
+#endif
---
+++ gnu/usr.bin/mkisofs/exclude.h
@@ -0,0 +1,8 @@
+/*
+ * 9-Dec-93 R.-D. Marzusch, marzusch@odiehh.hanse.de:
+ * added 'exclude' option (-x) to specify pathnames NOT to be included in 
+ * CD image.
+ */
+
+void exclude();
+int is_excluded();
