f5ba2bbf6b66479f855dbc18ffa27bdf73b403c1
ache <ache@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1995-02-16 17:01:11 +0000

    Add 8bit collate stuff Submitted by: alex@elvisti.kiev.ua
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@6509 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/string/strcoll.c
+++ lib/libc/string/strcoll.c
@@ -1,9 +1,7 @@
 /*-
- * Copyright (c) 1990, 1993
- *	The Regents of the University of California.  All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * Chris Torek.
+ * Copyright (c) 1995 Alex Tatmanjants <alex@elvisti.kiev.ua>
+ *		at Electronni Visti IA, Kiev, Ukraine.
+ *			All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -13,18 +11,11 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -32,22 +23,58 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ * $Id: strcoll.c,v 1.4 1995/01/27 12:51:06 alex Exp alex $
  */
 
-#if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strcoll.c	8.1 (Berkeley) 6/4/93";
-#endif /* LIBC_SCCS and not lint */
-
-#include <sys/cdefs.h>
+#include <stdlib.h>
 #include <string.h>
+#include "collate.h"
 
-/*
- * Compare strings according to LC_COLLATE category of current locale.
- */
+/* Compare strings according to LC_COLLATE category of current locale */
+
 int
-strcoll(s1, s2)
-	const char *s1, *s2;
+strcoll(s, s2)
+	const char *s, *s2;
 {
-	/* LC_COLLATE is unimplemented, hence always "C" */
-	return (strcmp(s1, s2));
+	int len, len2, prim, prim2, sec, sec2, ret, ret2;
+	char *tt, *t, *tt2, *t2;
+
+	if (__collate_load_error)
+		return strcmp(s, s2);
+
+	len = len2 = 1;
+	ret = ret2 = 0;
+	tt = t = __collate_substitute(s);
+	tt2 = t2 = __collate_substitute(s2);
+	while(*t && *t2) {
+		prim = prim2 = 0;
+		while(*t && !prim) {
+			__collate_lookup(t, &len, &prim, &sec);
+			t += len;
+		}
+		while(*t2 && !prim2) {
+			__collate_lookup(t2, &len2, &prim2, &sec2);
+			t2 += len2;
+		}
+		if(!prim || !prim2)
+			break;
+		if(prim != prim2) {
+			ret = prim - prim2;
+			goto end;
+		}
+		if(!ret2)
+			ret2 = sec - sec2;
+	}
+	if(!*t && *t2)
+		ret = -(int)((u_char)*t2);
+	else if(*t && !*t2)
+		ret = (u_char)*t;
+	else if(!*t && !*t2)
+		ret = ret2;
+  end:
+	free(tt);
+	free(tt2);
+
+	return ret;
 }
