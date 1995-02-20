59a9ecebb5209f6797522e8593d78c05a20d21ae
David Greenman <dg@FreeBSD.org>
1995-02-20 15:53:33 +0000

    Make sure process isn't swapped when messing with it. Added missing newline to log() call.

--- sys/miscfs/procfs/procfs_vfsops.c
+++ sys/miscfs/procfs/procfs_vfsops.c
@@ -36,7 +36,7 @@
  *
  *	@(#)procfs_vfsops.c	8.4 (Berkeley) 1/21/94
  *
- *	$Id: procfs_vfsops.c,v 1.5 1994/09/21 03:47:06 wollman Exp $
+ *	$Id: procfs_vfsops.c,v 1.6 1994/10/10 07:55:39 phk Exp $
  */
 
 /*
@@ -73,7 +73,7 @@ procfs_mount(mp, path, data, ndp, p)
 	u_int size;
 
 	if (UIO_MX & (UIO_MX-1)) {
-		log(LOG_ERR, "procfs: invalid directory entry size");
+		log(LOG_ERR, "procfs: invalid directory entry size\n");
 		return (EINVAL);
 	}
 
