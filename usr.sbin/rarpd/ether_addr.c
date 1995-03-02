ec46f0f9283084d4e70f1348470c0479b502e274
wpaul <wpaul@FreeBSD.org>
1995-03-02 06:41:40 +0000

    Obtained from: An old BPF release packaged with the tcpdump-2.0 source code.
    "Yes Virginia, there is a rarpd."
    
    (Before anyone asks, this *not* the rarpd from NetBSD. It did come from
    the same place as theirs, however.)
    
    This is a port of the rarpd program included with the tcpdump-2.0
    source code (which I finally unearthed after scrounging around
    some of the darker corners of the Internet). It's as close to the
    original as I could keep it except for the following changes:
    
    - The original program was based on an older version of the Berkeley
      Packet Filter which used different filter programming instructions.
      Fortunately, an updated RARP packet filter is available right in the
      BPF man page so this was easy to fix.
    
    - The old code didn't know how to deal with variable length addresses
      in ifreq buffers. This has been fixed.
    
    - Some byte order weirdness had to be fixed. The sanity checks in
      rarp_check() needed some htons()es, and the rarp_reply() function
      needed to properly set the ether_type field in the ethernet header
      to ETHERTYPE_REVARP before transmitting the packet, otherwise
      the bytes in ether_type would wind up reversed. It is important to note
      that using htons(ETHERTYPE_REVARP) will not work. This is odd, because
      the NetBSD rarpd uses htons(ETHERTYPE_REVARP). (Praise be to tcpdump:
      I would never have been able to track this silliness down without it.)
    
    - The update_arptab() function has been castrated. It depends on
      SIOCSARP which has been deprecated in 4.4BSD. The NetBSD people
      don't seem to be using this function either. It wouldn't be too
      hard to replace this with equivalent code from arp.c, but it
      might not be necessary.
    
    - I put together an ether_ntohost() support function that allows
      both local (/etc/ethers) and NIS lookups. This stuff should go
      in libc at some point, but nothing else seems to need it for now,
      so it can wait a while.
    
    As you may have guessed, you need to have the Berkeley Packet Filter in
    your kernel in order to use this program. The good news is that together
    with the recently added bootparamd, you can use finally use a FreeBSD
    box to boot Sun boxes over the network. (This was my whole motivation
    for getting this stuff to work: I have this one subnet that has a whole
    bunch of Sun3 X-terminals on it with only two Sun4 workstations, both
    of which are locked in peoples' offices. If those two machines crash
    (and they do every so often) then none of the X-terms will boot. Now I
    can use a spare PC that I have as a boot server. :)

---
+++ usr.sbin/rarpd/ether_addr.c
@@ -0,0 +1,108 @@
+/*
+ * rarpd support routines
+ *
+ * Written by Bill Paul <wpaul@ctr.columbia.edu>
+ * Center for Telecommunications Research
+ * Columbia University, New York City
+ *
+ * This code is public domain. There is no copyright. There are no
+ * distribution or usage restrictions. There are no strings attached.
+ *
+ * Have a party.
+ *
+ *	$Id$
+ */
+
+
+#include <stdio.h>
+
+#ifndef _PATH_ETHERS
+#define _PATH_ETHERS "/etc/ethers"
+#endif
+
+/*
+ * This should be defined in <netinet/if_ether.h> but it isn't.
+ */
+struct ether_addr {
+	unsigned char octet[6];
+};
+
+/*
+ * Parse a string of text containing an ethernet address and hostname
+ * and separate it into its component parts.
+ */
+int ether_line(l, e, hostname)
+        char *l;
+	struct ether_addr *e;
+	char *hostname;
+{
+        int i, o[6];
+
+        i = sscanf(l, "%x:%x:%x:%x:%x:%x %s", &o[0], &o[1], &o[2],
+                                              &o[3], &o[4], &o[5],
+					      hostname);
+	if (i != 7)
+                return (-1);
+
+        for (i=0; i<6; i++)
+                e->octet[i] = o[i];
+        return (0);
+}
+
+/*
+ * Map an ethernet address to a hostname. Use either /etc/ethers or
+ * NIS/YP.
+ */
+
+int ether_ntohost(hostname, e)
+	char *hostname;
+	struct ether_addr *e;
+{
+	FILE *fp;
+	static char buf[BUFSIZ];
+	static struct ether_addr local_ether;
+	static char *local_host;
+	static char *result;
+	int resultlen, i;
+	extern int yp_get_default_domain();
+	extern int yp_match();
+	static char *yp_domain;
+	static char ether_a[BUFSIZ];
+
+	if ((fp = fopen(_PATH_ETHERS, "r")) == NULL) {
+                        perror(_PATH_ETHERS);
+                        return (-1);
+	}
+
+	while (fgets(buf,BUFSIZ,fp)) {
+		if (buf[0] == '+') {
+			if (yp_get_default_domain(&yp_domain))
+				return(-1);
+			sprintf(ether_a,"%x:%x:%x:%x:%x:%x",
+						e->octet[0], e->octet[1],
+						e->octet[2], e->octet[3],
+						e->octet[4], e->octet[5]);
+			if (yp_match(yp_domain, "ethers.byaddr", ether_a,
+				strlen(ether_a),&result, &resultlen))
+				return(-1);
+			if (ether_line(result, &local_ether,
+					&local_host) == 0) {
+				strcpy(hostname, (char *)&local_host);
+				return(0);
+			} else
+				return(-1);
+		}
+		if (ether_line(&buf, &local_ether, &local_host) == 0) {
+			for (i = 0; i < 6; i++)
+				if (local_ether.octet[i] != e->octet[i])
+					goto nomatch;
+			/* We have a match */
+			strcpy(hostname, (char *)&local_host);
+			fclose(fp);
+			return(0);
+		}
+nomatch:
+	}
+
+return (-1);
+}
