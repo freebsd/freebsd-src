f767c7b6b0b86ab23d66150ceaa6ce60c9cc2f3e
pst <pst@FreeBSD.org>
1995-03-10 05:12:47 +0000

    Remove Matt Day patch (handled elsewhere in 8.6.11)

--- usr.sbin/sendmail/src/daemon.c
+++ usr.sbin/sendmail/src/daemon.c
@@ -1417,9 +1417,6 @@ hostnamebyanyaddr(sap)
 	int saveretry;
 
 #if NAMED_BIND
-	/* need to make sure _res.retry is initialized before using it */
-	if ((_res.options & RES_INIT) == 0)
-		res_init();
 	/* shorten name server timeout to avoid higher level timeouts */
 	saveretry = _res.retry;
 	_res.retry = 3;
