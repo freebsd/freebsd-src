3b54d81e93bcaa74ea4b1a73c264c9a70c2823e3
pst <pst@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1995-03-10 04:50:28 +0000

    This commit was generated by cvs2svn to compensate for changes in r6973, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@6974 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	8.6.10.1 (Berkeley) 2/7/95";
+static char sccsid[] = "@(#)version.c	8.6.11.1 (Berkeley) 2/28/95";
 #endif /* not lint */
 
-char	Version[] = "8.6.10";
+char	Version[] = "8.6.11";
