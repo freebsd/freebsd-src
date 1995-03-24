a3bee0e414af1daf8795f29c45553337affa5a1a
phk <phk@FreeBSD.org>
1995-03-24 02:45:34 +0000

    A whole slew of changes relating to the generation of src-dists.

--- release/scripts/extract_src.sh
+++ release/scripts/extract_src.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-# $Id: extract_src.sh,v 1.7 1995/03/24 00:16:51 phk Exp $
+# $Id: extract_src.sh,v 1.8 1995/03/24 02:04:00 phk Exp $
 PATH=/stand:$PATH
 DDIR=/usr/src
 
-for DIST in base srcbin etc games gnu include lib libexec release sbin lkm \
-	release share sys usrbin usrsbin; do
-	if [ -f ${DIST}/${DIST}.aa ]; then
+for T in src*.aa ; do
+	DIST=`basename $T .aa`
+	if [ -f ${DIST}.aa ]; then
 		echo "Extracting ${DIST} sources"
-		cat ${DIST}/${DIST}.?? 
+		cat ${DIST}.?? 
 			| gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
 	elif [ -f ${DIST}.aa ]; then
 		echo "Extracting ${DIST} sources"
