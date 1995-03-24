d57a2bc39275f6d8ffdc8db1f056c73a9ea7670e
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1995-03-24 00:16:51 +0000

    Add a missing ${DIST}/
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@7297 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/scripts/extract_src.sh
+++ release/scripts/extract_src.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-# $Id: extract_src.sh,v 1.5 1995/02/10 07:22:39 jkh Exp $
+# $Id: extract_src.sh,v 1.6 1995/03/21 21:44:54 jkh Exp $
 PATH=/stand:$PATH
 DDIR=/usr/src
 
 for DIST in base srcbin etc games gnu include lib libexec release sbin lkm \
 	release share sys usrbin usrsbin; do
-	if [ -f ${DIST}.aa ]; then
+	if [ -f ${DIST}/${DIST}.aa ]; then
 		echo "Extracting ${DIST} sources"
-		cat ${DIST}.?? | gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
+		cat ${DIST}/${DIST}.?? 
+			| gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
 	fi
 done
 ln -fs /usr/src/sys /sys
