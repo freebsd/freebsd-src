8a094e122ae7d36f455ed2e7bc8ef7493fca996f
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1995-03-24 03:38:48 +0000

    Fix a bogon.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@7312 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/scripts/extract_src.sh
+++ release/scripts/extract_src.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: extract_src.sh,v 1.8 1995/03/24 02:04:00 phk Exp $
+# $Id: extract_src.sh,v 1.9 1995/03/24 02:45:34 phk Exp $
 PATH=/stand:$PATH
 DDIR=/usr/src
 
@@ -7,12 +7,8 @@ for T in src*.aa ; do
 	DIST=`basename $T .aa`
 	if [ -f ${DIST}.aa ]; then
 		echo "Extracting ${DIST} sources"
-		cat ${DIST}.?? 
-			| gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
-	elif [ -f ${DIST}.aa ]; then
-		echo "Extracting ${DIST} sources"
-		cat ${DIST}.?? 
-			| gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
+		cat ${DIST}.?? | 
+			gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
 	fi
 done
 ln -fs /usr/src/sys /sys
