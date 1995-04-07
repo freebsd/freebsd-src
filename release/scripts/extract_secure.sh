54a01fd96f89c43e7539bb421569de214c381548
phk <phk@FreeBSD.org>
1995-04-07 01:20:26 +0000

    Various changes: tip removed from cpio.flp (!)  crunch chokes on it after the latest changes. source for secure & eBones moved into securedist.

--- release/scripts/extract_secure.sh
+++ release/scripts/extract_secure.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: extract_secure.sh,v 1.4 1995/01/28 09:11:39 jkh Exp $
+# $Id: extract_secure.sh,v 1.5 1995/02/07 01:01:21 jkh Exp $
 PATH=/stand:$PATH
 DDIR=/
 
@@ -10,7 +10,7 @@ if [ -f /sbin/init ]; then mv /sbin/init /sbin/init.insecure; fi
 cat ${DIST}.?? | gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
 
 DDIR=/usr/src
-for DIST in ebones secrsrc; do
+for DIST in sebones ssecure ; do
 	if [ -f ${DIST}.aa ]; then
 		echo "Extracting ${DIST} sources"
 		cat ${DIST}.?? | gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
