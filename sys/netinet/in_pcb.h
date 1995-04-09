e2bcab34abbb8c90af836b4315eac0ea5fdcec8a
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1995-04-09 16:46:47 +0000

    #include <sys/queue.h> or die horribly.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@7720 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/in_pcb.h
+++ sys/netinet/in_pcb.h
@@ -31,11 +31,13 @@
  * SUCH DAMAGE.
  *
  *	@(#)in_pcb.h	8.1 (Berkeley) 6/10/93
- * $Id: in_pcb.h,v 1.5 1995/03/16 18:14:52 bde Exp $
+ * $Id: in_pcb.h,v 1.6 1995/04/09 01:29:19 davidg Exp $
  */
 
 #ifndef _NETINET_IN_PCB_H_
 #define _NETINET_IN_PCB_H_
+
+#include <sys/queue.h>
 
 /*
  * Common structure pcb for internet protocol implementation.
