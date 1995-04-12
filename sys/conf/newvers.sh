46ddef1c224cddac9dc3e40c32ce91b46bd72eab
jkh <jkh@FreeBSD.org>
1995-04-12 23:29:14 +0000

    DISTNAME="2.0-950412-SNAP"

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -32,12 +32,12 @@
 # SUCH DAMAGE.
 #
 #	@(#)newvers.sh	8.1 (Berkeley) 4/20/94
-# $Id: newvers.sh,v 1.9 1995/03/24 02:41:47 phk Exp $
+# $Id: newvers.sh,v 1.10 1995/04/09 07:17:45 jkh Exp $
 
 TYPE="FreeBSD"
 RELEASE="2.1.0-Development"
 RELDATE="199504"
-DISTNAME="2.0-950408-SNAP"
+DISTNAME="2.0-950412-SNAP"
 
 if [ "x$JUST_TELL_ME" = "x" ]
 then
