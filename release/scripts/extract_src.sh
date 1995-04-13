fc33f1783e35cd0b7c3b597b128c387c82ff3897
phk <phk@FreeBSD.org>
1995-04-13 09:06:15 +0000

    tell people what we do at the end, since it can take a little while. use "set -e" when extracting bindist make src dists extract.

--- release/scripts/extract_src.sh
+++ release/scripts/extract_src.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# $Id: extract_src.sh,v 1.11 1995/03/25 05:03:40 phk Exp $
+# $Id: extract_src.sh,v 1.12 1995/03/28 08:01:47 phk Exp $
 PATH=/stand:$PATH
 DDIR=/usr/src
 
 mkdir -p $DDIR
 
-for T in src*.aa ; do
+for T in s*.aa ; do
 	DIST=`basename $T .aa`
 	if [ -f ${DIST}.aa ]; then
 		echo "Extracting ${DIST} sources"
