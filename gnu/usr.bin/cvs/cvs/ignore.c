9eadc62a82dcbd174f0b5d831074b5b5679f8040
nate <nate@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1995-04-18 18:14:57 +0000

    Backed out the only FreeBSD specific change to the stock CVS sources now that it's taken care of in the CVSROOT/cvsignore file which is FreeBSD specific.
    This will make it easier to track changes in the *official* release.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@7928 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- gnu/usr.bin/cvs/cvs/ignore.c
+++ gnu/usr.bin/cvs/cvs/ignore.c
@@ -27,7 +27,7 @@ static int ign_size;			/* This many slots available (plus
 static int ign_hold;			/* Index where first "temporary" item
 					 * is held */
 
-char *ign_default = ". .. core RCSLOG tags TAGS RCS SCCS .make.state .nse_depinfo #* .#* cvslog.* ,* CVS* .del-* *.a *.o *.so *.Z *~ *.old *.elc *.ln *.bak *.BAK *.orig *.rej *.info *.db";
+char *ign_default = ". .. core RCSLOG tags TAGS RCS SCCS .make.state .nse_depinfo #* .#* cvslog.* ,* CVS* .del-* *.a *.o *.so *.Z *~ *.old *.elc *.ln *.bak *.BAK *.orig *.rej";
 
 #define IGN_GROW 16			/* grow the list by 16 elements at a
 					 * time */
