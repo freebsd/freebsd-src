55e97012f8e3747b1dccd3ddc98cb2d3ac2c5d6f
jkh <jkh@FreeBSD.org>
1995-04-21 10:05:46 +0000

    Bring this up to date with Marc van Kempen's latest version (1.0) now that the libdialog changes have gone in to support it. Submitted by:	Marc van Kempen <wmbfmk@urc.tue.nl>

--- usr.sbin/pkg_manage/pkg_manage.c
+++ usr.sbin/pkg_manage/pkg_manage.c
@@ -8,11 +8,7 @@
  *
  * 1. View installed packages
  * 2. Delete installed packages
- * 3. View package files
- * 4. Install packages files.
- *
- * Installation and deletion of packages should be previewable
- *
+ * 3. Install packages
  *
  * Copyright (c) 1995, Marc van Kempen
  *
@@ -32,54 +28,38 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/param.h>
 #include <sys/stat.h>
 #include <stdio.h>
 
 #include "pkg_manage.h"
+#include "ui_objects.h"
+#include "dialog.priv.h"
+#include "dir.h"
 
-static PKG_info		p_inf;
-static int got_info;
+PKG_info	p_inf = { 0, 0, NULL, NULL, NULL, NULL, NULL };
 
+/*******************************************************************
+ *
+ * 	Misc Functions
+ *
+ *******************************************************************/
+
+void 
+FreeInfo(void)
 /*
- * Local prototypes
+ * Desc: free the space allocated to p_inf
  */
-
-void	run_menu(void);
-void	get_pkginfo(void);
-void 	FreeMnu(unsigned char **mnu, int n);
-void    FreeInfo(void);
-
-/*
- * Main
- */
-
-void
-main(void)
 {
-    init_dialog();
-
-    get_pkginfo();
-    run_menu();
-    
-    if (got_info)
-	FreeInfo();
-
-    clear();
-    dialog_update();
-    end_dialog();
+    free(p_inf.buf);
+    free(p_inf.name);
+    free(p_inf.comment);
+    free(p_inf.description);
+    p_inf.Nitems = 0;
+    FreeMnu(p_inf.mnu, 2*p_inf.Nitems);
 
     return;
-} /* main() */
-
-void FreeInfo(void)
-{
-	free(p_inf.buf);
-	free(p_inf.name);
-	free(p_inf.comment);
-	free(p_inf.description);
-	FreeMnu(p_inf.mnu, 2*p_inf.Nitems);
-	got_info = FALSE;
-}
+} /* FreeInfo() */
 
 void
 FreeMnu(unsigned char **mnu, int n)
@@ -91,7 +71,7 @@ FreeMnu(unsigned char **mnu, int n)
     
     for (i=0; i<n; i++) {
 	free(mnu[i]);
-    }
+    }   
     free(mnu);
 
     return;
@@ -117,68 +97,135 @@ file_exists(char *fname)
 } /* file_exists() */
 
 
+int
+exec_catch_errors(char *prog, char *arg, char *fout)
+/*
+ * Desc: run the program <prog> with arguments <arg> and catch its output
+ * 	 in <fout> and display it in case of an error. specify NULL, 
+ *	 if you don't want output.
+ */
+{
+    char	*execstr, *tmp;
+    int		ret, yesno, unlink_fout;
+
+    execstr = (char *) malloc( strlen(prog) + strlen(arg) + 30 );
+    if (!execstr) {
+	fprintf(stderr, "exec_catch_errors: Error while mallocing memory\n");
+	exit(-1);
+    }
+
+    /* when fout == NULL, allocate a temporary file name and unlink it */
+    /* when done */
+    if (!fout) {
+	fout = tempnam(NULL, "pkg.");
+	if (!fout) {
+	    fprintf(stderr, "exec_catch_errors: Error allocating temp.name\n");
+	    exit(-1);
+	}
+	unlink_fout = TRUE;
+    } else {
+	unlink_fout = FALSE;
+    }
+
+    sprintf(execstr, "%s %s > %s 2>&1", prog, arg, fout);
+    ret = system(execstr);
+    if (ret) {	
+
+	yesno = dialog_yesno("Error", "An error occured, view output?", 8, 40);
+	if (yesno == 0) {
+	    /* disable helpline */
+	    tmp = get_helpline();
+	    use_helpline("use arrowkeys, PgUp, PgDn to move, press enter when done");
+	    dialog_textbox("Error output from pkg_add", fout, LINES-2, COLS-4);
+	    restore_helpline(tmp);
+	}
+    }
+
+    if (unlink_fout) {
+	unlink(fout);
+	free(fout);
+    }
+    free(execstr);
+    
+    return(ret);
+} /* exec_catch_errors() */
+
 void
 get_pkginfo(void)
+/*
+ * Desc: get info about installed packages
+ */
 {
     FILE	*f;
     char 	*p;
-    char 	prog[512];
     struct stat sb;
     int		i, j, n, lsize;
-    int		newline;
+    int		newline, ret;
     int		state;
     char	*tmp_file;
 #define	R_NAME 1
 #define R_COMMENT 2
 #define R_DESC 3
 
-    if (got_info)
+    if (p_inf.Nitems > 0) {
 	FreeInfo();
+    }
+    /* p_inf.Nitems == 0 */
 
     dialog_msgbox("PKG INFO", "Reading info, please wait ...", 4, 35, FALSE);
 
-    tmp_file = tempnam(NULL, "pkg.");
-
-    sprintf(prog, "%s -a > %s", PKG_INFO, tmp_file);
-    system(prog);
-    dialog_clear_norefresh();
-    
-    f = fopen(tmp_file, "r");
-    if (!f) {
-	dialog_notify("Could not open temporary file");
-	goto err1;
-    }
-    
-    if (stat(tmp_file, &sb)) {	/* stat file to get filesize */
-	dialog_notify("Could not stat temporary file");
-	goto err2;
-    }
-    
-    if (sb.st_size == 0) {
-	dialog_notify("No packages info available");
-	goto err2;
-    }
-
-    /* Allocate a buffer with sufficient space to hold the entire file */
-    p_inf.buf = (char *) malloc( sb.st_size );
-    p_inf.N = sb.st_size;
-    if (fread(p_inf.buf, 1, p_inf.N, f) != p_inf.N) {
-	dialog_notify("Could not read from temporary file");
-	free(p_inf.buf);
-err2:
-	fclose(f);
-err1:
-	got_info = FALSE;
+    tmp_file = tempnam(NULL, "pkg.");  
+    ret = exec_catch_errors(PKG_INFO, "-a", tmp_file);
+    if (ret) {
+	dialog_notify("Could not get package info\nexiting!");
 	unlink(tmp_file);
 	free(tmp_file);
 	return;
     }
+    dialog_clear_norefresh();
+
+    f = fopen(tmp_file, "r");
+    if (!f) {
+	dialog_notify("Could not open temporary file");
+	unlink(tmp_file);
+	free(tmp_file);
+	return;
+    }
+    
+    if (stat(tmp_file, &sb)) {	/* stat file to get filesize */
+	dialog_notify("Could not stat temporary file");
+	fclose(f);
+	unlink(tmp_file);
+	free(tmp_file);
+	return;
+    }
+    
+    if (sb.st_size == 0) {
+	dialog_notify("No packages installed or no info available");
+	fclose(f);
+	unlink(tmp_file);
+	free(tmp_file);
+	return;
+    }
+
+    /* Allocate a buffer with sufficient space to hold the entire file */
+    p_inf.buf = (char *) malloc( sb.st_size + 1);
+    p_inf.N = sb.st_size;
+    if (fread(p_inf.buf, 1, p_inf.N, f) != p_inf.N) {
+	dialog_notify("Could not read entire temporary file");
+	free(p_inf.buf);
+	fclose(f);
+	unlink(tmp_file);
+	free(tmp_file);
+	return;
+    }
+    p_inf.buf[p_inf.N] = 0;
     fclose(f);
     unlink(tmp_file);
     free(tmp_file);
+
     /* make one sweep through the buffer to determine the # of entries */
     /* Look for "Information for" in the first column */
-
     i = p_inf.N - strlen("Information for") - 1;
     p = p_inf.buf;
     if (strncmp(p_inf.buf, "Information for", 15) == 0) {
@@ -186,7 +233,7 @@ err1:
     } else {
 	n = 0;
     }
-    while (i--) {
+    while (i-- > 0) {
 	if (*p == '\n') {
 	    if (strncmp(p+1, "Information for", 15) == 0) {
 		n++;
@@ -210,6 +257,7 @@ err1:
     state = R_NAME;
     while (*p) {
 	if (newline) {
+	    newline = FALSE;
 	    switch(state) {
 	    case R_NAME:
 		if (strncmp(p, "Information for", 15) == 0) {
@@ -226,21 +274,25 @@ err1:
 		    if (*p) p_inf.comment[i] = (char *) p+1;
 		    p++;
 		    while (*p && *p != '\n') p++;
-		    if (*p) *p = '\0';
-		    p++;
+		    if (*p) {
+			*p = '\0';
+			newline = TRUE;
+		    }
 		    state = R_DESC;
 		}
 		break;
 	    case R_DESC:
 		if (strncmp(p, "Description:", 12) == 0) {
 		    while (*p && *p != '\n') p++;
-		    if (*p) p_inf.description[i] = (char *) p+1;
+		    if (*p) {
+			p_inf.description[i] = (char *) p+1;
+			newline = TRUE;
+		    }
 		    state = R_NAME;
 		    i++;
 		}
 		break;
 	    }
-	    newline = FALSE;
 	}
 	if (*p == '\n') newline = TRUE;
 	p++;
@@ -262,8 +314,7 @@ err1:
 	strncpy(p_inf.mnu[j], p_inf.comment[i], lsize-1); 
 	p_inf.mnu[j++][lsize-1] = 0; 
     }
-    
-    got_info = TRUE;
+
     return;
 } /* get_pkginfo() */
 
@@ -275,268 +326,220 @@ get_pkg_index(char *selection)
 {
     int i, found = FALSE, index = -1;
 
-    if (got_info)
-	for (i=0; i<p_inf.Nitems && !found; i++) {
-	    if (strcmp(selection, p_inf.name[i]) == 0) {
-		found = TRUE;
-		index = i;
-	    }
+    for (i=0; i<p_inf.Nitems && !found; i++) {
+	if (strcmp(selection, p_inf.name[i]) == 0) {
+	    found = TRUE;
+	    index = i;
 	}
+    }
     
     return(index);
 } /* get_pkg_index() */
 
 void
-view_installed(void)
-/*
- * Desc: View the installed packages
- */
-{
-    int		i, quit_view, sc=0, ch=0;
-    char	selection[1024];
-
-    if (!got_info) {
-	use_helpfile(NULL);
-	use_helpline(NULL);
-	dialog_notify("No packages info available");
-	return;
-    }
-    use_helpfile(VIEW_INST_HLP);
-    quit_view = FALSE;
-    while (!quit_view) {
-	use_helpline("use arrow-keys or character to select option and press enter");
-	if (dialog_menu("View installed packages", 
-			"Press enter to see the package descriptions",
-			LINES, COLS, LINES-5, p_inf.Nitems, 
-			p_inf.mnu, selection, &ch, &sc )) {
-	    quit_view = TRUE;
-	} else {
-	    i = get_pkg_index(selection);
-	    use_helpline("use PgUp and PgDn and arrow-keys to move throught the text");
-	    dialog_mesgbox(p_inf.comment[i], p_inf.description[i], LINES, COLS);
-	}
-    }
-    use_helpfile(NULL);
-    use_helpline(NULL);
-
-    return;
-} /* view_installed() */
-
-void
-delete_installed(void)
-/*
- * Desc: Delete an installed package
- */
-{
-    unsigned char *mnu[] = {
-	"1. Simulate delete", "Display commands that are going to be executed",
-	"2. Delete", "Execute commands to delete the package",
-	"3. Cancel", "Do NOT delete the package"
-    };
-    char	tmp[512], selection[512], *tmp_file;
-    int		quit_view, quit_del;
-    int		i, sel, ch=0, sc=0, ch0=0, sc0=0;
-
-    if (!got_info) {
-	use_helpfile(NULL);
-	use_helpline(NULL);
-	dialog_notify("No packages info available");
-	return;
-    }
-    quit_view = FALSE;
-    while (!quit_view) {
-	use_helpline("use arrow-keys or character to select option and press enter");
-	use_helpfile(DEL_INST_HLP);
-	if (dialog_menu("DELETE an installed package",
-			"Press enter to select a package",
-			LINES, COLS, LINES-5, p_inf.Nitems, 
-			p_inf.mnu, selection, &ch, &sc)) {
-	    quit_view = TRUE;
-	} else {
-	    quit_del = FALSE;
-	    i = get_pkg_index(selection);
-	    while (!quit_del) {
-		sprintf(tmp, "Delete <%s>", p_inf.name[i]);
-		use_helpline("use arrow-keys or digit to select option and press enter");
-		if (dialog_menu("Delete a package", tmp, 10, COLS-6, 
-				3, 3, mnu, selection, &ch0, &sc0)) {
-		    quit_del = TRUE;
-		} else {
-		    sel = atoi(selection);
-		    switch(sel) {
-		    case 1:
-			tmp_file = tempnam(NULL, "pkg.");
-			sprintf(tmp, "%s -n %s > %s", 
-				PKG_DELETE, p_inf.name[i], tmp_file);
-			system(tmp);
-			dialog_textbox("Package deletion commands",
-				       tmp_file, LINES, COLS);
-			unlink(tmp_file);
-			free(tmp_file);
-			break;
-		    case 2:
-			sprintf(tmp, "%s %s", PKG_DELETE, p_inf.name[i]);
-			system(tmp);
-			get_pkginfo();
-			quit_del = TRUE;
-			break;
-		    case 3:
-			quit_del = TRUE;
-			break;
-		    }
-		}
-	    }
-	}
-    }
-    use_helpfile(NULL);
-    use_helpline(NULL);
-
-    return;
-} /* delete_installed() */
-
-void 
-preview_pkg(void)
+install_package(char *fname)
 /* 
- * Desc: View the package description and comment before installation
+ * Desc: install the package <fname>
  */
 {
-    char	*fname;
-    char	*tmp_file;
-    char	prog[512], title[512];
+    char 	*tmp_file;
 
-    use_helpfile(PREVIEW_HLP);
-    fname = dialog_fselect(".", "*.tgz");
-    while (fname) {
-	use_helpfile(PREVIEW_HLP);
-	use_helpline("use PgUp and PgDn and arrow-keys to move through the text");
-	tmp_file = tempnam(NULL, "pkg.");
-	sprintf(prog, "%s zxOf %s +COMMENT +DESC > %s", TAR, fname, tmp_file);
-	system(prog);
-	sprintf(title, "Preview package <%s>", fname);
-	dialog_textbox(title, tmp_file, LINES, COLS);
-	unlink(tmp_file);
-	free(fname);
-	free(tmp_file);
-	fname = dialog_fselect(".", "*.tgz");
+    tmp_file = tempnam(NULL, "pkg.");
+    if (!tmp_file) {
+	fprintf(stderr, "install_package(): Error malloc'ing tmp_file\n");
+	exit(-1);
     }
-    if (fname) free(fname);
-    use_helpfile(NULL);
-    use_helpline(NULL);
+    exec_catch_errors(PKG_ADD, fname, tmp_file);
+
+    unlink(tmp_file);
+    free(tmp_file);
 
     return;
-} /* preview_pkg() */
+} /* install_package() */
 
-void
-install_new(void)
+
+
+int
+get_desc(char *fname, char **name, char **comment, 
+	 char **desc, long *size, char *tmp_dir)
 /*
- * Desc: Install a new package
+ * Desc: get the description and comment from the files
+ *	 DESC, CONTENT and COMMENT from fname
+ * Pre:  the current working directory is a temporary, 
+ *	 empty directory.
+ * Post: name = the name of the package
+ * 	 comment = the comment for the package
+ *	 desc = the description for the package
  */
 {
-    char		*fname;
-    char		*tmp_file;
-    char		tmp[512], selection[40];
-    unsigned char 	*mnu[] = {
-	"1. Simulate install", "Display commands that are going to be executed",
-	"2. Install", "Execute commands to install the package",
-	"3. Cancel", "Do NOT install the package"
-    };
-    int		sel, quit_inst, ch=0, sc=0;
-    
-    use_helpfile(INSTALL_HLP);
-    fname = dialog_fselect(".", "*.tgz");
-    if (!fname) {
-	quit_inst = TRUE;
+    char	msg[80], args[512], *buf, *p, tmp[MAXPATHLEN];
+    FILE	*f, *pf;
+    struct stat sb;
+    int		i, N, ret, found;
+
+    *comment = NULL;
+    *desc = NULL;
+    *name = NULL;
+
+    sprintf(args, "--fast-read -zxvf %s -C %s %s %s %s", fname, 
+	    tmp_dir, CONTENTS, DESC, COMMENT);
+    ret = exec_catch_errors(TAR, args, NULL);
+    if (ret) {
+	sprintf(msg, "Could not get info for <%s>", fname);
+	dialog_notify(msg);
+	return(FALSE);
+    }
+    /* Read CONTENTS */
+    sprintf(tmp, "%s/%s", tmp_dir, CONTENTS);
+    f = fopen(tmp, "r");
+    if (f == NULL) {
+	/* No contents file in package, propably not a package */
+	return(FALSE);
+    }
+    if (stat(tmp, &sb)) {	/* stat file to get filesize */
+	dialog_notify("Could not stat CONTENTS file");
+	fclose(f);
+	return(FALSE);
+    }
+    if (sb.st_size == 0) {
+	dialog_notify("CONTENTS file has zero length");
+	fclose(f);
+	return(FALSE);
+    }
+
+    /* Allocate a buffer with sufficient space to hold the entire file */
+    buf = (char *) malloc( sb.st_size + 1);
+    N = sb.st_size;
+    if (fread(buf, 1, N, f) != N) {
+	sprintf(msg, "Could not read CONTENT file for <%s>", fname);
+	dialog_notify(msg);
+	free(buf);
+	fclose(f);
+	return(FALSE);
+    }
+    fclose(f);
+    buf[N] = 0;
+
+    /* look for the name of the package */
+    p = buf;
+    found = FALSE;
+    while (*p && !found) {
+	if (strncmp(p, "@name ", 6) == 0) {
+	    i=0; 
+	    p += 6;
+	    while (*p && p[i] != '\n' && p[i] != '\r') i++;
+	    *name = (char *) malloc( i+1 );
+	    strncpy(*name, p, i);
+	    (*name)[i] = 0;
+	    found = TRUE;
+	} else {
+	    p++;
+	}
+    }
+    unlink(tmp);
+
+    /* Read COMMENT file */
+    sprintf(tmp, "%s/%s", tmp_dir, COMMENT);
+    f = fopen(tmp, "r");
+    if (f == NULL) {
+	/* No comment file in package, propably not a package */
+	return(FALSE);
+    }
+    if (stat(tmp, &sb)) {	/* stat file to get filesize */
+	dialog_notify("Could not stat COMMENT file");
+	fclose(f);
+	return(FALSE);
+    }
+    if (sb.st_size == 0) {
+	dialog_notify("COMMENT file has zero length");
+	fclose(f);
+	return(FALSE);
+    }
+
+    /* Allocate a buffer with sufficient space to hold the entire file */
+    *comment = (char *) malloc( sb.st_size + 1);
+    N = sb.st_size;
+    if (fread(*comment, 1, N, f) != N) {
+	sprintf(msg, "Could not read COMMENT file for <%s>", fname);
+	dialog_notify(msg);
+	free(*comment);
+	fclose(f);
+	return(FALSE);
+    }
+    fclose(f);
+    (*comment)[N] = 0;
+    unlink(tmp);
+
+    /* Read DESC */
+    sprintf(tmp, "%s/%s", tmp_dir, DESC);
+    f = fopen(tmp, "r");
+    if (f == NULL) {
+	/* No description file in package, propably not a package */
+	return(FALSE);
+    }
+    if (stat(tmp, &sb)) {	/* stat file to get filesize */
+	dialog_notify("Could not stat DESC file");
+	fclose(f);
+	return(FALSE);
+    }
+    if (sb.st_size == 0) {
+	dialog_notify("DESC file has zero length");
+	fclose(f);
+	return(FALSE);
+    }
+
+    /* Allocate a buffer with sufficient space to hold the entire file */
+    *desc = (char *) malloc( sb.st_size + 1);
+    N = sb.st_size;
+    if (fread(*desc, 1, N, f) != N) {
+	sprintf(msg, "Could not read CONTENT file for <%s>", fname);
+	dialog_notify(msg);
+	free(*desc);
+	fclose(f);
+	return(FALSE);
+    }
+    fclose(f);
+    (*desc)[N] = 0;
+    unlink(tmp);
+
+    /* get the size from the uncompressed package */
+    sprintf(tmp, "%s -l %s", GUNZIP, fname);
+    pf = popen(tmp, "r");
+    if (!pf) {
+	dialog_notify("Could not popen gunzip to get package size");
+	*size = 0;
     } else {
-	quit_inst = FALSE;
-    }
-    while (!quit_inst) {
-	use_helpfile(INSTALL_HLP);
-	use_helpline("use arrow-keys or digit to select option and press enter");
-	sprintf(tmp, "Install package <%s>", fname);
-	if (dialog_menu("Install a package", tmp, 10, COLS-5, 
-			3, 3, mnu, selection, &ch, &sc)) {
-	    quit_inst = TRUE;
-	} else {
-	    sel = atoi(selection);
-	    switch(sel) {
-	    case 1:
-		tmp_file = tempnam(NULL, "*.pkg");
-		sprintf(tmp, "%s -n %s 2>&1 | cat > %s", PKG_ADD, fname, tmp_file);
-		system(tmp);
-		dialog_textbox("Package installation commands",
-			       tmp_file, LINES, COLS);
-		unlink(tmp_file);
-		free(tmp_file);
-		break;
-	    case 2:
-		sprintf(tmp, "%s %s", PKG_ADD, fname);
-		system(tmp);
-		get_pkginfo();
-		quit_inst = TRUE;
-		break;
-	    case 3:
-		quit_inst = TRUE;
-		break;
-	    }
+	while (!feof(pf)) {
+	    fgets(tmp, 80, pf);
 	}
+	sscanf(tmp, "%*s %ld", size);
+	pclose(pf);
     }
-    if (fname) free(fname);
-    use_helpfile(NULL);
-    use_helpline(NULL);
+	
+    if (found) {
+	return(TRUE);
+    } else {
+	return(FALSE);
+    }
+} /* get_desc() */
 
-    return;
-} /* install_new() */
 
-void
-run_menu(void)
-/*
- * Desc: display/choose from menu
+int
+already_installed(char *name)
+/* 
+ * Desc: check if <name> is already installed as a package
  */
 {
-    int		quit_pkg, sel, ch=0, sc=0;
-    char	selection[30];
-    unsigned char *pkg_menu[] = {
-	"1. View installed", "Overview of the installed packages",
-	"2. Delete installed", "Delete an installed package",
-	"3. View pkg files", "Preview about to be installed packages",
-	"4. Install pkg files", "Install new packages",
-	"5. Quit", "Leave the program",
-    };
+    int	i, found;
 
-    quit_pkg = FALSE;
-    while (!quit_pkg) {
-	use_helpline("use arrow-keys or digit to select option and press enter");
-	use_helpfile(MAIN_HLP);
-	if (dialog_menu("Package Manager", "Choose one of the options",
-		    LINES, COLS, 5, 5, pkg_menu, selection, &ch, &sc)) {
-	    sel = 0;
-	    quit_pkg = TRUE;
-	} else {
-	    sel = atoi(selection);
-	}
-	
-	switch(sel) {
-	case 0: /* Quit */
-	    break;
-	case 1:	/* View installed packages */
-	    view_installed();
-	    break;
-	case 2: /* Delete installed package */
-	    delete_installed();
-	    break;
-	case 3: /* Preview new package file */
-	    preview_pkg();
-	    break;
-	case 4: /* Install new package */
-	    install_new();
-	    break;
-	case 5: /* Quit */
-	    quit_pkg = TRUE;
-	    break;
+    found = FALSE;
+    for (i=0; i<p_inf.Nitems && !found; i++) {
+	if (strcmp(name, p_inf.name[i]) == 0) {
+	    found = TRUE;
 	}
     }
 
-    return;
-} /* run_menu() */
-	    
+    return(found);
+} /* already_installed() */
     
+ 
