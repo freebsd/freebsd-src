02ace706173509b7d8549494277ff682b7cefb8a
jkh <jkh@FreeBSD.org>
1995-04-25 15:05:11 +0000

    A programmatic interface to ftp.  I need this for several other components of the system.
    The license is poorly worded, though I have an (email only) release
    from the author for unlimited FreeBSD use.  I will try to get something
    more concrete, though the author's remote location makes this difficult.
    Submitted by:	Oleg Orel <orel@oea.ihep.su>

---
+++ lib/libftp/FtpSyscalls.c
@@ -0,0 +1,34 @@
+/*
+		      Library for ftpd clients.(libftp)
+			Copyright by Oleg Orel
+			 All rights reserved.
+			
+This  library is desined  for  free,  non-commercial  software  creation. 
+It is changeable and can be improved. The author would greatly appreciate 
+any  advises, new  components  and  patches  of  the  existing  programs.
+Commercial  usage is  also  possible  with  participation of it's author.
+
+
+
+*/
+
+#include "FtpLibrary.h"
+#include <errno.h>
+
+#define DEF(syscal,name) name(a,b,c,d,e,f,g,h) \
+{\
+   register int status;\
+   while (((status=syscal(a,b,c,d,e,f,g,h))==-1) && (errno==EINTR));\
+   return status;\
+}
+
+DEF(open,nointr_open)
+DEF(close,nointr_close)
+DEF(select,nointr_select)
+DEF(read,nointr_read)
+DEF(write,nointr_write)
+DEF(dup,nointr_dup)
+DEF(wait,nointr_wait)
+DEF(connect,nointr_connect)
+DEF(listen,nointr_listen)
+DEF(accept,nointr_accept)
