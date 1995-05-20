8dc76129d9c58bd9080b3515b8b37b647049fbc8
gpalmer <gpalmer@FreeBSD.org>
1995-05-20 03:49:10 +0000

    Make Jordan's ``half baked'' code compile so that make release's aren't stopped dead in their tracks. Also add the beginnings of my distribution extraction code in media_strategy.c

--- release/sysinstall/media_strategy.c
+++ release/sysinstall/media_strategy.c
@@ -4,7 +4,7 @@
  * This is probably the last attempt in the `sysinstall' line, the next
  * generation being slated to essentially a complete rewrite.
  *
- * $Id: media.c,v 1.5 1995/05/16 11:37:18 jkh Exp $
+ * $Id: media_strategy.c,v 1.1 1995/05/17 14:39:53 jkh Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -43,12 +43,64 @@
 
 #include <stdio.h>
 #include "sysinstall.h"
+#include <ctype.h>
+#include <sys/stat.h>
+#include <sys/errno.h>
+#include <sys/file.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/param.h>
+#include <sys/dkbad.h>
+
+#define MSDOSFS
+#define CD9660
+#define NFS
+#include <sys/mount.h>
+#undef MSDOSFS
+#undef CD9660
+#undef NFS
+
+int
+genericGetDist(char *dist, char *source)
+{
+    return 0;
+}
 
 /* Various media "strategy" routines */
 
 Boolean
 mediaInitCDROM(Device *dev)
 {
+    struct iso_args	args;
+    struct stat		sb;
+
+    if (Mkdir("/mnt/cdrom", NULL))
+	return FALSE;
+
+    args.fspec = dev->devname;
+    if (mount(MOUNT_CD9660, "/mnt/cdrom", 0, (caddr_t) &args) == -1)
+    {
+	msgConfirm("Error mounting %s on /mnt/cdrom: %s\n",
+		   dev, strerror(errno));
+	return FALSE;
+    }
+
+    /* Do a very simple check to see if this looks roughly like a 2.0.5 CDROM
+       Unfortunately FreeBSD won't let us read the ``label'' AFAIK, which is one
+       sure way of telling the disc version :-( */
+    if (stat("/mnt/cdrom/dists", &sb))
+    {
+	if (errno == ENOENT)
+	{
+	    msgConfirm("Couldn't locate the directory `dists' on the cdrom\n\
+Is this a 2.0.5 CDROM?\n");
+	    return FALSE;
+	} else {
+	    msgConfirm("Couldn't stat directory %s: %s", "/mnt/cdrom/dists", strerror(errno));
+	    return FALSE;
+	}
+    }
+    
     return TRUE;
 }
 
