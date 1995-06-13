e01156de3b41b5ee86c359e9482b89719904ac1e
cvs2svn <cvs2svn@FreeBSD.org>
1995-06-13 18:05:17 +0000

    This commit was manufactured by cvs2svn to create branch 'RELENG_2_1_0'.

--- release/scripts/extract_manpages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# $Id: extract_manpages.sh,v 1.1 1995/01/14 07:41:43 jkh Exp $
-set -e
-PATH=/stand:$PATH
-DDIR=/
-
-DIST=manpages
-echo "Extracting ${DIST}"
-cat ${DIST}.?? | gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
--- release/scripts/extract_proflibs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# $Id: extract_proflibs.sh,v 1.2 1995/01/28 09:11:36 jkh Exp $
-set -e
-PATH=/stand:$PATH
-DDIR=/
-
-DIST=proflibs
-echo "Extracting ${DIST} - ignore any errors from cpio"
-cat ${DIST}.?? | gzip -c -d | ( cd $DDIR; cpio -H tar -imdu )
