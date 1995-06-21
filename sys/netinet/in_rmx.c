b127902179e722a606f3129d9946a9706718903a
wollman <wollman@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1995-06-21 19:48:53 +0000

    Fix an error in the comparison direction of the ap->updating case of in_rtqkill().
    Submitted by: W. Richard Stevens
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@9279 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/in_rmx.c
+++ sys/netinet/in_rmx.c
@@ -26,7 +26,7 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: in_rmx.c,v 1.12 1995/03/19 14:29:01 davidg Exp $
+ * $Id: in_rmx.c,v 1.13 1995/05/30 08:09:31 rgrimes Exp $
  */
 
 /*
@@ -181,7 +181,7 @@ in_rtqkill(struct radix_node *rn, void *rock)
 			}
 		} else {
 			if(ap->updating
-			   && (time.tv_sec - rt->rt_rmx.rmx_expire
+			   && (rt->rt_rmx.rmx_expire - time.tv_sec
 			       > rtq_reallyold)) {
 				rt->rt_rmx.rmx_expire = time.tv_sec
 					+ rtq_reallyold;
