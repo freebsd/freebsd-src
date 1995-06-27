5d28c984ccb9896069557771612be90abe5b1254
guido <guido@FreeBSD.org>
1995-06-27 17:26:27 +0000

    reject option in ip_fw used to panic the system. This fixes it.
    -Guido
    Reviewed by:
    Submitted by:
    Obtained from:

--- sys/netinet/ip_input.c
+++ sys/netinet/ip_input.c
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)ip_input.c	8.2 (Berkeley) 1/4/94
- * $Id: ip_input.c,v 1.21 1995/05/11 00:13:18 wollman Exp $
+ * $Id: ip_input.c,v 1.23 1995/06/13 17:51:10 wollman Exp $
  */
 
 #include <sys/param.h>
@@ -243,7 +243,7 @@ next:
 
         if (ip_fw_chk_ptr!=NULL)
                if (!(*ip_fw_chk_ptr)(ip,m->m_pkthdr.rcvif,ip_fw_chain) ) {
-                       goto bad;
+                       goto next;
                }
 
 	/*
