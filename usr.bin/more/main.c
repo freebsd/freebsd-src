605abcde5ef3cf0e6bf34e0f704b0d893f6f4810
Peter Wemm <peter@FreeBSD.org>
1995-07-17 12:37:50 +0000

    Make 'more' sensative to the current locale for it's ctype calls. Reviewed by: Submitted by:	Thomas Gellekum, with minor cosmetic changes from me. Obtained from:

--- usr.bin/more/main.c
+++ usr.bin/more/main.c
@@ -51,6 +51,7 @@ static char sccsid[] = "@(#)main.c	8.1 (Berkeley) 6/7/93";
 #include <sys/file.h>
 #include <stdio.h>
 #include <less.h>
+#include <locale.h>
 
 int	ispipe;
 int	new_file;
@@ -251,6 +252,8 @@ main(argc, argv)
 {
 	int envargc, argcnt;
 	char *envargv[2], *getenv();
+
+	setlocale(LC_CTYPE, "");
 
 	/*
 	 * Process command line arguments and MORE environment arguments.
