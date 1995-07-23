61037ba185c9ea7f6e3fcecf84b94d6f1e3644a9
dg <dg@FreeBSD.org>
1995-07-23 05:48:03 +0000

    Brought in changes from revs 1.21 and 1.22: Fix panic when no mbuf is allocated (when 0 length is passed in setsockopt()).

--- sys/netinet/ip_output.c
+++ sys/netinet/ip_output.c
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)ip_output.c	8.3 (Berkeley) 1/21/94
- * $Id: ip_output.c,v 1.18 1995/05/09 13:35:46 davidg Exp $
+ * $Id: ip_output.c,v 1.19 1995/05/30 08:09:49 rgrimes Exp $
  */
 
 #include <sys/param.h>
@@ -549,7 +549,7 @@ ip_ctloutput(op, so, level, optname, mp)
 		case IP_RECVOPTS:
 		case IP_RECVRETOPTS:
 		case IP_RECVDSTADDR:
-			if (m->m_len != sizeof(int))
+			if (m == 0 || m->m_len != sizeof(int))
 				error = EINVAL;
 			else {
 				optval = *mtod(m, int *);
