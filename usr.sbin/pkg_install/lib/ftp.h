e7a7980c25f5b1a58d1fcc7d302b25cc2e92524e
Satoshi Asami <asami@FreeBSD.org>
1995-07-31 04:00:01 +0000

    Sync this up with ftp.c.

--- usr.sbin/pkg_install/lib/ftp.h
+++ usr.sbin/pkg_install/lib/ftp.h
@@ -17,9 +17,6 @@ FTP_t		FtpInit();
 int		FtpOpen(FTP_t, char *host, char *user, char *passwd);
 #define 	FtpBinary(ftp,bool)	{ (ftp)->binary = (bool); }
 #define 	FtpPassive(ftp,bool)	{ (ftp)->passive = (bool); }
-#ifndef STANDALONE_FTP
-#define 	FtpDebug(ftp, bool)	{ (ftp)->fd_debug = (bool); }
-#endif
 int		FtpChdir(FTP_t, char *);
 int		FtpGet(FTP_t, char *);
 int		FtpEOF(FTP_t);
