de17dd4012ae4edd256278c5cffa0a3a3907ad20
peter <peter@FreeBSD.org>
1995-08-20 21:43:53 +0000

    Update 4.9.3.b9p1 tools to 4.9.3.b24 Note: new program, "ndc", similar to gated's gdc.  This provides a superset of named.reload and named.restart.

--- usr.sbin/named/tools/named.restart/named.restart.sh
+++ usr.sbin/named/tools/named.restart/named.restart.sh
@@ -1,23 +1,7 @@
 #!/bin/sh -
 #
 #	from named.restart	5.4 (Berkeley) 6/27/89
-#	$Id: named.restart.sh,v 1.2 1994/09/22 20:45:26 pst Exp $
+#	$Id: named.restart.sh,v 1.3 1995/05/03 03:26:59 rgrimes Exp $
 #
 
-# If there is a global system configuration file, suck it in.
-if [ -f /etc/sysconfig ]; then
-	. /etc/sysconfig
-fi
-
-PATH=%DESTSBIN%:/bin:/usr/bin
-
-if [ -f %PIDDIR%/named.pid ]; then
-	pid=`cat %PIDDIR%/named.pid`
-	kill $pid
-	sleep 5
-fi
-
-# $namedflags is imported from /etc/sysconfig
-if [ "X${namedflags}" != "XNO" ]; then 
-	exec %INDOT%named ${namedflags}
-fi
+exec %DESTSBIN%/%INDOT%ndc restart
