3b894f576160a2aecb7b14e0329113ed5f96b31a
peter <peter@FreeBSD.org>
1995-08-20 21:32:49 +0000

    Update 4.9.3.beta9p1 -> 4.9.3-beta24

--- usr.sbin/named/tools/named.reload/named.reload.sh
+++ usr.sbin/named/tools/named.reload/named.reload.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -
 #
 #	from named.reload	5.2 (Berkeley) 6/27/89
-#	$Id: named.reload.sh,v 4.9.1.2 1993/09/08 00:01:17 vixie Exp $
+#	$Id: named.reload.sh,v 8.1 1994/12/15 06:24:14 vixie Exp $
 #
-kill -HUP `cat %PIDDIR%/named.pid`
+
+exec %DESTSBIN%/%INDOT%ndc reload
--- usr.sbin/named/tools/named.restart/named.restart.sh
+++ usr.sbin/named/tools/named.restart/named.restart.sh
@@ -1,12 +1,7 @@
 #!/bin/sh -
 #
 #	from named.restart	5.4 (Berkeley) 6/27/89
-#	$Id: named.restart.sh,v 4.9.1.5 1994/04/09 03:43:17 vixie Exp $
+#	$Id: named.restart.sh,v 8.1 1994/12/15 06:24:14 vixie Exp $
 #
 
-PATH=%DESTSBIN%:/bin:/usr/bin
-
-pid=`cat %PIDDIR%/named.pid`
-kill $pid
-sleep 5
-exec %INDOT%named 
+exec %DESTSBIN%/%INDOT%ndc restart
