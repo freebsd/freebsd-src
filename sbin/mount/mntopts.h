fa17d89229c96c1a5fc5e4115fdf5dead35fa426
jkh <jkh@FreeBSD.org>
1995-08-23 12:59:27 +0000

    Add a "noauto" flag so that you can do things like prevent your system from not coming up multiuser just because you have a CD mount in fstab but no CD in the drive. Submitted by:	"Full Name Not Supplied" <simon@masi.ibp.fr>

--- sbin/mount/mntopts.h
+++ sbin/mount/mntopts.h
@@ -42,6 +42,7 @@ struct mntopt {
 
 /* User-visible MNT_ flags. */
 #define MOPT_ASYNC		{ "async",	0, MNT_ASYNC, 0 }
+#define MOPT_NOAUTO		{ "auto",	1, MNT_NOAUTO, 0 }
 #define MOPT_NODEV		{ "dev",	1, MNT_NODEV, 0 }
 #define MOPT_NOEXEC		{ "exec",	1, MNT_NOEXEC, 0 }
 #define MOPT_NOSUID		{ "suid",	1, MNT_NOSUID, 0 }
@@ -68,6 +69,7 @@ struct mntopt {
 /* Standard options which all mounts can understand. */
 #define MOPT_STDOPTS							\
 	MOPT_FSTAB_COMPAT,						\
+	MOPT_NOAUTO,							\
 	MOPT_NODEV,							\
 	MOPT_NOEXEC,							\
 	MOPT_NOSUID,							\
