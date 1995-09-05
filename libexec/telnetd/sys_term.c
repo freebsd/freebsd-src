e5e3ec3964d44cbdc2715171861c152e900ad8c6
pst <pst@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1995-09-05 17:38:31 +0000

    Properly set the erase character for the login prompt.
    Submitted by:	John Capo <jc@irbs.com> & Peter Wemm
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@10561 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- libexec/telnetd/sys_term.c
+++ libexec/telnetd/sys_term.c
@@ -1049,6 +1049,7 @@ extern void utmp_sig_notify P((int));
 getptyslave()
 {
 	register int t = -1;
+	char erase;
 
 #if	!defined(CRAY) || !defined(NEWINIT)
 # ifdef	LINEMODE
@@ -1065,12 +1066,13 @@ getptyslave()
 	 * 	if linemode was turned on
 	 *	terminal window size
 	 *	terminal speed
+	 *	erase character
 	 * so that we can re-set them if we need to.
 	 */
 # ifdef	LINEMODE
 	waslm = tty_linemode();
 # endif
-
+	erase = termbuf.c_cc[VERASE];
 
 	/*
 	 * Make sure that we don't have a controlling tty, and
@@ -1157,6 +1159,8 @@ getptyslave()
 	tty_rspeed((def_rspeed > 0) ? def_rspeed : 9600);
 	tty_tspeed((def_tspeed > 0) ? def_tspeed : 9600);
 # ifdef	LINEMODE
+	if (erase)
+		termbuf.c_cc[VERASE] = erase;
 	if (waslm)
 		tty_setlinemode(1);
 # endif	/* LINEMODE */
