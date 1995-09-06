360604bd5e58d0e0f85babaf59799f84bcdc80c7
David Greenman <dg@FreeBSD.org>
1995-09-06 10:34:19 +0000

    Brought in changes from main branch: update to mrouted v3.6.
    Reviewed by:	Bill Fenner <fenner@parc.xerox.com>, wollman

--- usr.sbin/mrouted/callout.c
+++ usr.sbin/mrouted/callout.c
@@ -7,7 +7,7 @@
  * Leland Stanford Junior University.
  *
  *
- * $Id: callout.c,v 1.3 1995/05/16 00:28:42 jkh Exp $
+ * $Id: callout.c,v 1.6 1995/06/28 17:58:25 wollman Exp $
  */
 
 #include "defs.h"
@@ -18,20 +18,22 @@ static struct timeout_q  *Q = 0; /* pointer to the beginning of timeout queue */
 
 static int in_callout= 0;
 
-typedef void (* cfunc_t)();
-
 struct timeout_q {
 	struct timeout_q *next;		/* next event */
-	int        	 id;
-	cfunc_t          func ;    	/* function to call */
+	int        	 id;  
+	cfunc_t          func;    	/* function to call */
 	char	   	 *data;		/* func's data */
 	int            	 time;		/* time offset to next event*/
 };
 
+#ifdef IGMP_DEBUG
+static void print_Q __P((void));
+#else
+#define	print_Q()	
+#endif
 
-static void print_Q();
-
-void callout_init()
+void
+callout_init()
 {
     Q = (struct timeout_q *) 0;
 }
@@ -40,23 +42,24 @@ void callout_init()
 /*
  * signal handler for SIGALARM that is called once every second
  */
-void age_callout_queue()
+void
+age_callout_queue()
 {
     struct timeout_q *ptr;
-
+    
     if (in_callout)
 	return;
 
     in_callout = 1;
     ptr = Q;
-
+    
     while (ptr){
 	if (!ptr->time ) {
 	    /* timeout has happened */
-	    if(ptr->func)
+	    if (ptr->func)
 		ptr->func(ptr->data);
 	    Q = Q->next;
-
+	    
 	    free(ptr);
 	    ptr = Q;
 	}
@@ -64,7 +67,7 @@ void age_callout_queue()
 	    ptr->time --;
 #ifdef IGMP_DEBUG
 	    log(LOG_DEBUG,0,"[callout, age_callout_queue] -- time (%d)", ptr->time);
-#endif IGMP_DEBUG
+#endif /* IGMP_DEBUG */
 	    in_callout = 0; return;
 	}
     }
@@ -73,48 +76,49 @@ void age_callout_queue()
 }
 
 
-/*
+/* 
  * sets the timer
  */
-int timer_setTimer(delay, action, data)
+int
+timer_setTimer(delay, action, data)
     int 	delay;  	/* number of units for timeout */
     cfunc_t	action; 	/* function to be called on timeout */
     char  	*data;  	/* what to call the timeout function with */
 {
     struct     timeout_q  *ptr, *node, *prev;
-
+    
     if (in_callout)
 	return -1;
 
     in_callout = 1;
-
-    /* create a node */
-    node   = (struct timeout_q *)malloc(sizeof(struct timeout_q));
-    if ((int) node <= 0) {
+    
+    /* create a node */	
+    node = (struct timeout_q *)malloc(sizeof(struct timeout_q));
+    if (node == 0) {
 	log(LOG_WARNING, 0, "Malloc Failed in timer_settimer\n");
 	in_callout = 0;
 	return -1;
     }
-    node->func = action;
+    node->func = action; 
     node->data = data;
-    node->time = delay;
-    node->next = 0;
+    node->time = delay; 
+    node->next = 0;	
     node->id   = ++id;
-
+    
     prev = ptr = Q;
-
+    
     /* insert node in the queue */
-
+    
     /* if the queue is empty, insert the node and return */
     if (!Q)
 	Q = node;
     else {
 	/* chase the pointer looking for the right place */
-	while (ptr){
-
-	    if (delay < ptr->time){
+	while (ptr) {
+	    
+	    if (delay < ptr->time) {
 		/* right place */
-
+		
 		node->next = ptr;
 		if (ptr == Q)
 		    Q = node;
@@ -124,10 +128,9 @@ int timer_setTimer(delay, action, data)
 		print_Q();
 		in_callout = 0;
 		return node->id;
-	    }
-	    else  {
+	    } else  {
 		/* keep moving */
-
+		
 		delay -= ptr->time; node->time = delay;
 		prev = ptr;
 		ptr = ptr->next;
@@ -142,39 +145,41 @@ int timer_setTimer(delay, action, data)
 
 
 /* clears the associated timer */
-void timer_clearTimer( id)
-    int  id;
+void
+timer_clearTimer(timer_id)
+    int  timer_id;
 {
     struct timeout_q  *ptr, *prev;
+    
+    if (in_callout)
+        return;
+    if (!timer_id)
+	return;
 
-    if (in_callout)  return;
     in_callout = 1;
-
-
-    if ( !id ) {in_callout = 0; return;}
-
+    
     prev = ptr = Q;
-
+    
     /*
      * find the right node, delete it. the subsequent node's time
      * gets bumped up
      */
-
+    
     print_Q();
-    while (ptr){
-	if (ptr->id == id){
+    while (ptr) {
+	if (ptr->id == timer_id) {
 	    /* got the right node */
-
+	    
 	    /* unlink it from the queue */
-	    if ( ptr == Q)
+	    if (ptr == Q)
 		Q = Q->next;
 	    else
 		prev->next = ptr->next;
-
+	    
 	    /* increment next node if any */
 	    if (ptr->next != 0)
 		(ptr->next)->time += ptr->time;
-
+	    
 	    free(ptr->data);
 	    free(ptr);
 	    print_Q();
@@ -188,16 +193,16 @@ void timer_clearTimer( id)
     in_callout = 0;
 }
 
+#ifdef IGMP_DEBUG
 /*
  * debugging utility
  */
-static void print_Q()
+static void
+print_Q()
 {
-#ifdef IGMP_DEBUG
     struct timeout_q  *ptr;
-
+    
     for(ptr = Q; ptr; ptr = ptr->next)
 	log(LOG_DEBUG,0,"(%d,%d) ", ptr->id, ptr->time);
-#endif IGMP_DEBUG
 }
-
+#endif /* IGMP_DEBUG */
--- usr.sbin/mrouted/igmp.c
+++ usr.sbin/mrouted/igmp.c
@@ -7,7 +7,7 @@
  * Leland Stanford Junior University.
  *
  *
- * igmp.c,v 1.2 1994/09/08 02:51:15 wollman Exp
+ * $Id: igmp.c,v 1.7 1995/06/28 17:58:32 wollman Exp $
  */
 
 
@@ -17,22 +17,34 @@
 /*
  * Exported variables.
  */
-char		recv_buf[MAX_IP_PACKET_LEN]; /* input packet buffer         */
-char		send_buf[MAX_IP_PACKET_LEN]; /* output packet buffer        */
+char		*recv_buf; 		     /* input packet buffer         */
+char		*send_buf; 		     /* output packet buffer        */
 int		igmp_socket;		     /* socket for all network I/O  */
-u_long		allhosts_group;		     /* allhosts  addr in net order */
-u_long		dvmrp_group;		     /* DVMRP grp addr in net order */
-u_long		dvmrp_genid;		     /* IGMP generation id          */
+u_int32		allhosts_group;		     /* All hosts addr in net order */
+u_int32		allrtrs_group;		     /* All-Routers "  in net order */
+u_int32		dvmrp_group;		     /* DVMRP grp addr in net order */
+u_int32		dvmrp_genid;		     /* IGMP generation id          */
+
+/*
+ * Local function definitions.
+ */
+/* u_char promoted to u_int */
+static char *	packet_kind __P((u_int type, u_int code));
+static int	igmp_log_level __P((u_int type, u_int code));
 
 /*
  * Open and initialize the igmp socket, and fill in the non-changing
  * IP header fields in the output packet buffer.
  */
-void init_igmp()
+void
+init_igmp()
 {
     struct ip *ip;
 
-    if ((igmp_socket = socket(AF_INET, SOCK_RAW, IPPROTO_IGMP)) < 0)
+    recv_buf = malloc(RECV_BUF_SIZE);
+    send_buf = malloc(RECV_BUF_SIZE);
+
+    if ((igmp_socket = socket(AF_INET, SOCK_RAW, IPPROTO_IGMP)) < 0) 
 	log(LOG_ERR, errno, "IGMP socket");
 
     k_hdr_include(TRUE);	/* include IP header when sending */
@@ -50,10 +62,9 @@ void init_igmp()
 
     allhosts_group = htonl(INADDR_ALLHOSTS_GROUP);
     dvmrp_group    = htonl(INADDR_DVMRP_GROUP);
+    allrtrs_group  = htonl(INADDR_ALLRTRS_GROUP);
 }
 
-/* %%% hack for PIM %%% */
-#define IGMP_PIM	0x14
 #define PIM_QUERY        0
 #define PIM_REGISTER     1
 #define PIM_REGISTER_STOP 	2
@@ -63,14 +74,15 @@ void init_igmp()
 #define PIM_GRAFT        6
 #define PIM_GRAFT_ACK    7
 
-static char *packet_kind(type, code)
-     u_char type, code;
+static char *
+packet_kind(type, code)
+     u_int type, code;
 {
     switch (type) {
 	case IGMP_HOST_MEMBERSHIP_QUERY:	return "membership query  ";
 	case IGMP_HOST_MEMBERSHIP_REPORT:	return "membership report ";
-	case IGMP_HOST_NEW_MEMBERSHIP_REPORT:	return "new membership report ";
-	case IGMP_HOST_LEAVE_MESSAGE:                return "leave message";
+	case IGMP_HOST_NEW_MEMBERSHIP_REPORT:	return "new member report ";
+	case IGMP_HOST_LEAVE_MESSAGE:           return "leave message     ";
 	case IGMP_DVMRP:
 	  switch (code) {
 	    case DVMRP_PROBE:	    		return "neighbor probe    ";
@@ -79,12 +91,12 @@ static char *packet_kind(type, code)
 	    case DVMRP_NEIGHBORS:	    	return "neighbor list     ";
 	    case DVMRP_ASK_NEIGHBORS2:   	return "neighbor request 2";
 	    case DVMRP_NEIGHBORS2:	    	return "neighbor list 2   ";
-	    case DVMRP_PRUNE:			return "prune message	  ";
-	    case DVMRP_GRAFT:			return "graft message	  ";
+	    case DVMRP_PRUNE:			return "prune message     ";
+	    case DVMRP_GRAFT:			return "graft message     ";
 	    case DVMRP_GRAFT_ACK:		return "graft message ack ";
 	    default:	    			return "unknown DVMRP msg ";
 	  }
- 	case IGMP_PIM:				/* %%% hack for PIM %%% */
+ 	case IGMP_PIM:
  	  switch (code) {
  	    case PIM_QUERY:			return "PIM Router-Query  ";
  	    case PIM_REGISTER:			return "PIM Register      ";
@@ -106,10 +118,11 @@ static char *packet_kind(type, code)
  * Process a newly received IGMP packet that is sitting in the input
  * packet buffer.
  */
-void accept_igmp(recvlen)
+void
+accept_igmp(recvlen)
     int recvlen;
 {
-    register u_long src, dst, group;
+    register u_int32 src, dst, group;
     struct ip *ip;
     struct igmp *igmp;
     int ipdatalen, iphdrlen, igmpdatalen;
@@ -124,9 +137,9 @@ void accept_igmp(recvlen)
     src       = ip->ip_src.s_addr;
     dst       = ip->ip_dst.s_addr;
 
-    /*
+    /* 
      * this is most likely a message from the kernel indicating that
-     * a new src grp pair message has arrived and so, it would be
+     * a new src grp pair message has arrived and so, it would be 
      * necessary to install a route into the kernel for this.
      */
     if (ip->ip_p == 0) {
@@ -163,29 +176,30 @@ void accept_igmp(recvlen)
     switch (igmp->igmp_type) {
 
 	case IGMP_HOST_MEMBERSHIP_QUERY:
-	    /* we have to do the determination of the querrier router here */
+	    accept_membership_query(src, dst, group, igmp->igmp_code);
 	    return;
 
 	case IGMP_HOST_MEMBERSHIP_REPORT:
 	case IGMP_HOST_NEW_MEMBERSHIP_REPORT:
-	    accept_group_report(src, dst, group,igmp->igmp_type);
+	    accept_group_report(src, dst, group, igmp->igmp_type);
 	    return;
-
+	    
 	case IGMP_HOST_LEAVE_MESSAGE:
-	    leave_group_message(src, dst, group);
+	    accept_leave_message(src, dst, group);
 	    return;
 
 	case IGMP_DVMRP:
-	    switch (igmp->igmp_code) {
+	    group = ntohl(group);
 
+	    switch (igmp->igmp_code) {
 		case DVMRP_PROBE:
 		    accept_probe(src, dst,
-                               (char *)(igmp+1), igmpdatalen, ntohl(group));
+				 (char *)(igmp+1), igmpdatalen, group);
 		    return;
 
 		case DVMRP_REPORT:
  		    accept_report(src, dst,
-                                (char *)(igmp+1), igmpdatalen, ntohl(group));
+				  (char *)(igmp+1), igmpdatalen, group);
 		    return;
 
 		case DVMRP_ASK_NEIGHBORS:
@@ -197,13 +211,13 @@ void accept_igmp(recvlen)
 		    return;
 
 		case DVMRP_NEIGHBORS:
-		    accept_neighbors(src, dst, (char *)(igmp+1), igmpdatalen,
-				     group);
+		    accept_neighbors(src, dst, (u_char *)(igmp+1), igmpdatalen,
+					     group);
 		    return;
 
 		case DVMRP_NEIGHBORS2:
-		    accept_neighbors2(src, dst, (char *)(igmp+1), igmpdatalen,
-				     group);
+		    accept_neighbors2(src, dst, (u_char *)(igmp+1), igmpdatalen,
+					     group);
 		    return;
 
 		case DVMRP_PRUNE:
@@ -226,37 +240,63 @@ void accept_igmp(recvlen)
 		    return;
 	    }
 
-
- 	case IGMP_PIM:		/* %%% hack for PIM  %%% */
+ 	case IGMP_PIM:
  	    return;
 
+	case IGMP_MTRACE_RESP:
+	    return;
+
 	case IGMP_MTRACE:
-	    mtrace(src, dst, group, (char *)(igmp+1),
+	    accept_mtrace(src, dst, group, (char *)(igmp+1),
 		   igmp->igmp_code, igmpdatalen);
 	    return;
 
 	default:
 	    log(LOG_INFO, 0,
-		"ignoring unknown IGMP message type %u from %s to %s",
+		"ignoring unknown IGMP message type %x from %s to %s",
 		igmp->igmp_type, inet_fmt(src, s1),
 		inet_fmt(dst, s2));
 	    return;
     }
 }
 
+/*
+ * Some IGMP messages are more important than others.  This routine
+ * determines the logging level at which to log a send error (often
+ * "No route to host").  This is important when there is asymmetric
+ * reachability and someone is trying to, i.e., mrinfo me periodically.
+ */
+static int
+igmp_log_level(type, code)
+    u_int type, code;
+{
+    switch (type) {
+	case IGMP_MTRACE_RESP:
+	    return LOG_INFO;
+
+	case IGMP_DVMRP:
+	  switch (code) {
+	    case DVMRP_NEIGHBORS:
+	    case DVMRP_NEIGHBORS2:
+		return LOG_INFO;
+	  }
+    }
+    return LOG_WARNING;
+}
 
 /*
  * Construct an IGMP message in the output packet buffer.  The caller may
  * have already placed data in that buffer, of length 'datalen'.  Then send
  * the message from the interface with IP address 'src' to destination 'dst'.
  */
-void send_igmp(src, dst, type, code, group, datalen)
-    u_long src, dst;
+void
+send_igmp(src, dst, type, code, group, datalen)
+    u_int32 src, dst;
     int type, code;
-    u_long group;
+    u_int32 group;
     int datalen;
 {
-    static struct sockaddr_in sdst = {AF_INET, sizeof sdst};
+    static struct sockaddr_in sdst;
     struct ip *ip;
     struct igmp *igmp;
 
@@ -276,13 +316,18 @@ void send_igmp(src, dst, type, code, group, datalen)
     if (IN_MULTICAST(ntohl(dst))) k_set_if(src);
     if (dst == allhosts_group) k_set_loop(TRUE);
 
+    bzero(&sdst, sizeof(sdst));
+    sdst.sin_family = AF_INET;
+#if (defined(BSD) && (BSD >= 199103))
+    sdst.sin_len = sizeof(sdst);
+#endif
     sdst.sin_addr.s_addr = dst;
     if (sendto(igmp_socket, send_buf, ip->ip_len, 0,
 			(struct sockaddr *)&sdst, sizeof(sdst)) < 0) {
 	if (errno == ENETDOWN)
 	    check_vif_state();
 	else
-	    log(LOG_WARNING, errno,
+	    log(igmp_log_level(type, code), errno,
 		"sendto to %s on %s",
 		inet_fmt(dst, s1), inet_fmt(src, s2));
     }
