de26dac73e741823c6709d241f04dc5b5cfe45ec
julian <julian@FreeBSD.org>
1995-09-09 12:51:56 +0000

    More hacking on devfs.. I can  now do an mv on devices and directories in devfs This was the hardest part.. link, delete and symlink will follow in short order. This code works but has definitly got vnode locking problems I am electing to get the structure of it working before spending too much time on the vnode confusion so it's probably not reliable at the moment.. never-the less it looks good.  :)

--- sys/miscfs/devfs/devfs_proto.h
+++ sys/miscfs/devfs/devfs_proto.h
@@ -1,20 +1,22 @@
 /* THIS FILE PRODUCED AUTOMATICALLY */
 void  devfs_sinit(caddr_t junk) /*proto*/;
-dn_p dev_findname(dn_p dir,char *name) /*proto*/;
+devnm_p dev_findname(dn_p dir,char *name) /*proto*/;
 int	dev_finddir(char *orig_path, dn_p dirnode, int create, dn_p *dn_pp) /*proto*/;
-int	dev_add_name(char *name, dn_p dirnode,devnm_p back, int entrytype, union typeinfo *by, devnm_p *devnm_pp) /*proto*/;
+int	dev_add_name(char *name, dn_p dirnode, devnm_p back, dn_p dnp, devnm_p *devnm_pp) /*proto*/;
+int	dev_add_node(int entrytype, union typeinfo *by, dn_p proto, dn_p *dn_pp) /*proto*/;
 int	dev_touch(devnm_p key)		/* update the node for this dev */ /*proto*/;
 void	devfs_dn_free(dn_p dnp) /*proto*/;
 int devfs_add_fronts(devnm_p parent,devnm_p child) /*proto*/;
 void	dev_remove_dev(devnm_p devnmp) /*proto*/;
 int dev_dup_plane(struct devfsmount *devfs_mp_p) /*proto*/;
 void  devfs_free_plane(struct devfsmount *devfs_mp_p) /*proto*/;
-int dev_dup_name(dn_p parent, devnm_p back, devnm_p *dnm_pp, struct devfsmount *dvm) /*proto*/;
+int dev_dup_entry(dn_p parent, devnm_p back, devnm_p *dnm_pp, struct devfsmount *dvm) /*proto*/;
 void dev_free_name(devnm_p devnmp) /*proto*/;
 int devfs_vntodn(struct vnode *vn_p, dn_p *dn_pp) /*proto*/;
 int devfs_dntovn(dn_p dnp, struct vnode **vn_pp) /*proto*/;
 int get_cdev_major_num(caddr_t addr)	/*proto*/;
 int get_bdev_major_num(caddr_t addr)	/*proto*/;
+int dev_add_entry(char *name, dn_p parent, int type, union typeinfo *by, devnm_p *nm_pp) /*proto*/ ;
 int devfs_init(void) /*proto*/;
 int devfs_mount( struct mount *mp, char *path, caddr_t data, struct nameidata *ndp, struct proc *p) /*proto*/;
 int mountdevfs( struct mount *mp, struct proc *p) /*proto*/;
