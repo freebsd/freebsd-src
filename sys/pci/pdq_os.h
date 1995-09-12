cd86ea59b47a4bbab896d33d62704b16564c7c54
David Greenman <dg@FreeBSD.org>
1995-09-12 09:19:23 +0000

    Brought in change from rev 1.3: include systm.h.

--- sys/pci/pdq_os.h
+++ sys/pci/pdq_os.h
@@ -21,9 +21,16 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: pdq_os.h,v 1.1 1995/03/14 09:16:07 davidg Exp $
+ * $Id: pdq_os.h,v 1.3 1995/07/16 10:07:20 bde Exp $
  *
  * $Log: pdq_os.h,v $
+ * Revision 1.3  1995/07/16  10:07:20  bde
+ * Fix compiler warnings (systm.h wasn't included).
+ *
+ * Revision 1.2  1995/03/25  22:40:49  bde
+ * Remove wrong redeclarations of printf() and bzero().  Include the correct
+ * header to declare DELAY().
+ *
  * Revision 1.1  1995/03/14  09:16:07  davidg
  * Added support for generic FDDI and the DEC DEFEA and DEFPA FDDI adapters.
  *
@@ -63,6 +70,7 @@
 #elif defined(__FreeBSD__) || defined(__bsdi__)
 
 #include <sys/param.h>
+#include <sys/systm.h>
 #ifndef M_MCAST
 #include <sys/mbuf.h>
 #endif
