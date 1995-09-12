e3716ece955756860ea433402d0d912b9058bc1e
dg <dg@FreeBSD.org>
1995-09-12 07:19:45 +0000

    Brought in change from main branch: initialize d_ncylinders.

--- sys/i386/isa/diskslice_machdep.c
+++ sys/i386/isa/diskslice_machdep.c
@@ -35,7 +35,7 @@
  *
  *	from: @(#)ufs_disksubr.c	7.16 (Berkeley) 5/4/91
  *	from: ufs_disksubr.c,v 1.8 1994/06/07 01:21:39 phk Exp $
- *	$Id: diskslice_machdep.c,v 1.13 1995/05/07 07:06:36 bde Exp $
+ *	$Id: diskslice_machdep.c,v 1.16 1995/08/23 11:43:45 jkh Exp $
  */
 
 #include <stddef.h>
@@ -304,6 +304,7 @@ reread_mbr:
 		secperunit = secpercyl * max_ncyls;
 		if (lp->d_secperunit < secperunit)
 			lp->d_secperunit = secperunit;
+		lp->d_ncylinders = lp->d_secperunit / secpercyl;
 	}
 
 	/*
