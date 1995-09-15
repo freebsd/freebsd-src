8517bf983f7da51ea824df7b60085794d4546609
Justin T. Gibbs <gibbs@FreeBSD.org>
1995-09-15 17:28:28 +0000

    Merge with the head.  Bring in fixes to deal with krb.h declaring all necessary prototypes.

--- usr.bin/rlogin/krb.h
+++ usr.bin/rlogin/krb.h
@@ -33,19 +33,6 @@
  *	@(#)krb.h	8.1 (Berkeley) 6/6/93
  */
 
-/*
- * XXX
- * These should be in a kerberos include file.
- */
-void	 des_clear_key __P(());
-int	 des_read __P((int, char *, int));
-void	 des_set_key __P((C_Block, Key_schedule));
-int	 des_write __P((int, char *, int));
-int	 krb_net_read __P((int, char *, int));
-char	*krb_realmofhost __P((char *));
-int	 krb_sendauth __P((long, int, KTEXT, char *, char *, char *,
-	    u_long, MSG_DAT *, CREDENTIALS *, Key_schedule,
-	    struct sockaddr_in *, struct sockaddr_in *, char *));
 int	 krcmd __P((char **, u_short, char *, char *, int *, char *));
 int	 krcmd_mutual __P((char **, u_short, char *, char *, int *,
 	    char *, CREDENTIALS *, Key_schedule));
