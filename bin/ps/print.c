c55931c759ae5cb50f960e6d7366a46a18d806d1
Peter Wemm <peter@FreeBSD.org>
1995-09-26 17:48:59 +0000

    Correct the alignment of the tty column, which was affected by my change to allow more than two tty characters.
    David Greenman pointed out that when a process that had been revoked from
    it's controlling tty, the "-" sign was detached from any two-character
    names.

--- bin/ps/print.c
+++ bin/ps/print.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: print.c,v 1.8 1995/08/07 19:17:39 wollman Exp $
+ *	$Id: print.c,v 1.9 1995/09/04 01:22:41 peter Exp $
  */
 
 #ifndef lint
@@ -322,12 +322,12 @@ tname(k, ve)
 	v = ve->var;
 	dev = KI_EPROC(k)->e_tdev;
 	if (dev == NODEV || (ttname = devname(dev, S_IFCHR)) == NULL)
-		(void)printf("%-*s", v->width, "??");
+		(void)printf("%*s ", v->width-1, "??");
 	else {
 		if (strncmp(ttname, "tty", 3) == 0 ||
 		    strncmp(ttname, "cua", 3) == 0)
 			ttname += 3;
-		(void)printf("%-*.*s%c", v->width-1, v->width-1, ttname,
+		(void)printf("%*.*s%c", v->width-1, v->width-1, ttname,
 			KI_EPROC(k)->e_flag & EPROC_CTTY ? ' ' : '-');
 	}
 }
