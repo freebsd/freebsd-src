52efcf9999439b77c929006cc239ac203a455ed8
jkh <jkh@FreeBSD.org>
1995-10-05 09:55:01 +0000

    wcd0a -> wcd0c

--- release/sysinstall/devices.c
+++ release/sysinstall/devices.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: devices.c,v 1.36 1995/06/11 19:29:45 rgrimes Exp $
+ * $Id: devices.c,v 1.36.2.1 1995/10/03 23:36:37 jkh Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -82,8 +82,7 @@ static struct {
     { DEVICE_TYPE_CDROM,	"scd1a",	"Sony CDROM drive - CDU31/33A type (2nd unit)"		},
     { DEVICE_TYPE_CDROM,	"matcd0a",	"Matsushita CDROM ('sound blaster' type)"		},
     { DEVICE_TYPE_CDROM,	"matcd1a",	"Matsushita CDROM (2nd unit)"				},
-    { DEVICE_TYPE_CDROM,	"wcd0a",	"IDE CDROM Drive"					},
-    { DEVICE_TYPE_CDROM,	"wcd1a",	"IDE CDROM Drive (2nd unit)"				},
+    { DEVICE_TYPE_CDROM,	"wcd0c",	"IDE CDROM Drive"					},
     { DEVICE_TYPE_TAPE, 	"rst0",		"SCSI tape drive"					},
     { DEVICE_TYPE_TAPE, 	"rst1",		"SCSI tape drive (2nd unit)"				},
     { DEVICE_TYPE_TAPE, 	"ft0",		"Floppy tape drive (QIC-02)"				},
