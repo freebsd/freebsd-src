69da6fae3cb8b89a01251ac3a714accc4a557b1c
David Greenman <dg@FreeBSD.org>
1995-10-10 01:04:38 +0000

    Brought in change from rev 1.6: bugfix for the bugfix.

--- bin/sh/options.c
+++ bin/sh/options.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: options.c,v 1.3.4.1 1995/08/25 03:19:17 davidg Exp $
+ *	$Id: options.c,v 1.3.4.2 1995/10/02 07:55:18 davidg Exp $
  */
 
 #ifndef lint
@@ -106,7 +106,7 @@ procargs(argc, argv)
 		commandname = arg0 = *argptr++;
 		setinputfile(commandname, 0);
 	}
-	if (minusc)
+	if (*argptr && minusc)
 		/* Posix.2: first arg after -c cmd is $0, remainder $1... */
 		arg0 = *argptr++;
 	shellparam.p = argptr;
