5e847383397df915ba70004a1d9a9893ad22281d
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1995-10-31 20:24:14 +0000

    Merge/update ppp-2.2 kernel parts onto mainline.
    Note that the old if_ppp.c has been split in half into if_ppp.c and
    ppp_tty.c
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@11970 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net/if_ppp.h
+++ sys/net/if_ppp.h
@@ -1,3 +1,6 @@
+/*	from Id: if_ppp.h,v 1.12 1995/04/24 02:40:23 paulus Exp	*/
+/*	$Id$ */
+
 /*
  * if_ppp.h - Point-to-Point Protocol definitions.
  *
@@ -15,118 +18,88 @@
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- *
- * $Id: if_ppp.h,v 1.3 1995/05/30 08:08:09 rgrimes Exp $
  */
 
 #ifndef _IF_PPP_H_
 #define _IF_PPP_H_
 
-/*
- * Standard PPP header.
- */
-struct ppp_header {
-	u_char	ph_address;	/* Address Field */
-	u_char	ph_control;	/* Control Field */
-	u_short	ph_protocol;	/* Protocol Field */
-};
-
-#define PPP_HDRLEN	4	/* sizeof(struct ppp_header) must be 4 */
-#define PPP_FCSLEN	2	/* octets for FCS */
-
-#define	PPP_ALLSTATIONS	0xff	/* All-Stations broadcast address */
-#define	PPP_UI		0x03	/* Unnumbered Information */
-#define	PPP_FLAG	0x7e	/* Flag Sequence */
-#define	PPP_ESCAPE	0x7d	/* Asynchronous Control Escape */
-#define	PPP_TRANS	0x20	/* Asynchronous transparency modifier */
-
-/*
- * Protocol field values.
- */
-#define PPP_IP		0x21	/* Internet Protocol */
-#define	PPP_XNS		0x25	/* Xerox NS */
-#define PPP_IPX		0x2b	/* IPX Datagram (RFC1552) */
-#define	PPP_VJC_COMP	0x2d	/* VJ compressed TCP */
-#define	PPP_VJC_UNCOMP	0x2f	/* VJ uncompressed TCP */
-#define PPP_COMP	0xfd	/* compressed packet */
-#define PPP_LCP		0xc021	/* Link Control Protocol */
-#define PPP_IPXCP	0x802b	/* IPX Control Protocol (RFC1552) */
-#define PPP_CCP		0x80fd	/* Compression Control Protocol */
-
-/*
- * Important FCS values.
- */
-#define PPP_INITFCS	0xffff	/* Initial FCS value */
-#define PPP_GOODFCS	0xf0b8	/* Good final FCS value */
-#define PPP_FCS(fcs, c)	(((fcs) >> 8) ^ fcstab[((fcs) ^ (c)) & 0xff])
+/* XXX this used to be self-contained. */
+#include <net/ppp_defs.h>
+#include <net/if.h>
 
 /*
  * Packet sizes
  */
 #define	PPP_MTU		1500	/* Default MTU (size of Info field) */
-#define PPP_MRU		1500	/* Default MRU (max receive unit) */
 #define PPP_MAXMRU	65000	/* Largest MRU we allow */
-
-/* Extended asyncmap - allows any character to be escaped. */
-typedef u_long	ext_accm[8];
+#define PPP_MAXMTU	16384	/* Largest MTU we allow */
 
 /*
- * Structure describing each ppp unit.
+ * Bit definitions for flags.
  */
-struct ppp_softc {
-	struct ifnet sc_if;	/* network-visible interface */
-	u_int	sc_flags;	/* see below */
-	void	*sc_devp;	/* pointer to device-dependent structure */
-	int	(*sc_start) __P((struct ppp_softc *));	/* start routine */
-	short	sc_mru;		/* max receive unit */
-	pid_t	sc_xfer;	/* used in xferring unit to another dev */
-	struct	ifqueue sc_inq;	/* TTY side input queue */
-	struct	ifqueue sc_fastq; /* IP interactive output packet queue */
-#ifdef	VJC
-	struct	slcompress sc_comp; /* vjc control buffer */
-#endif
-	u_int	sc_bytessent;	/* count of octets sent */
-	u_int	sc_bytesrcvd;	/* count of octets received */
-	caddr_t	sc_bpf;		/* hook for BPF */
-
-	/* Device-dependent part for async lines. */
-	ext_accm sc_asyncmap;	/* async control character map */
-	u_long	sc_rasyncmap;	/* receive async control char map */
-	struct	mbuf *sc_outm;	/* mbuf chain being output currently */
-	struct	mbuf *sc_m;	/* pointer to input mbuf chain */
-	struct	mbuf *sc_mc;	/* pointer to current input mbuf */
-	char	*sc_mp;		/* pointer to next char in input mbuf */
-	short	sc_ilen;	/* length of input-packet-so-far */
-	u_short	sc_fcs;		/* FCS so far (input) */
-	u_short	sc_outfcs;	/* FCS so far for output packet */
-	u_char	sc_rawin[16];	/* chars as received */
-	int	sc_rawin_count;	/* # in sc_rawin */
-};
-
-/* flags */
 #define SC_COMP_PROT	0x00000001	/* protocol compression (output) */
 #define SC_COMP_AC	0x00000002	/* header compression (output) */
 #define	SC_COMP_TCP	0x00000004	/* TCP (VJ) compression (output) */
 #define SC_NO_TCP_CCID	0x00000008	/* disable VJ connection-id comp. */
 #define SC_REJ_COMP_AC	0x00000010	/* reject adrs/ctrl comp. on input */
 #define SC_REJ_COMP_TCP	0x00000020	/* reject TCP (VJ) comp. on input */
-#define SC_ENABLE_IP	0x00000100	/* IP packets may be exchanged */
+#define SC_CCP_OPEN	0x00000040	/* Look at CCP packets */
+#define SC_CCP_UP	0x00000080	/* May send/recv compressed packets */
 #define SC_DEBUG	0x00010000	/* enable debug messages */
 #define SC_LOG_INPKT	0x00020000	/* log contents of good pkts recvd */
 #define SC_LOG_OUTPKT	0x00040000	/* log contents of pkts sent */
 #define SC_LOG_RAWIN	0x00080000	/* log all chars received */
 #define SC_LOG_FLUSH	0x00100000	/* log all chars flushed */
-#define	SC_MASK		0x0fffffff	/* bits that user can change */
-
-/* state bits */
-#define	SC_ESCAPED	0x80000000	/* saw a PPP_ESCAPE */
-#define	SC_FLUSH	0x40000000	/* flush input until next PPP_FLAG */
 #define SC_RCV_B7_0	0x01000000	/* have rcvd char with bit 7 = 0 */
-#define SC_RCV_B7_1	0x02000000	/* have rcvd char with bit 7 = 0 */
+#define SC_RCV_B7_1	0x02000000	/* have rcvd char with bit 7 = 1 */
 #define SC_RCV_EVNP	0x04000000	/* have rcvd char with even parity */
 #define SC_RCV_ODDP	0x08000000	/* have rcvd char with odd parity */
+#define	SC_MASK		0x0fff00ff	/* bits that user can change */
 
-/* this stuff doesn't belong here... */
+/*
+ * State bits in sc_flags, not changeable by user.
+ */
+#define SC_TIMEOUT	0x00000400	/* timeout is currently pending */
+#define SC_VJ_RESET	0x00000800	/* need to reset VJ decomp */
+#define SC_COMP_RUN	0x00001000	/* compressor has been inited */
+#define SC_DECOMP_RUN	0x00002000	/* decompressor has been inited */
+#define SC_DC_ERROR	0x00004000	/* non-fatal decomp error detected */
+#define SC_DC_FERROR	0x00008000	/* fatal decomp error detected */
+#define SC_TBUSY	0x10000000	/* xmitter doesn't need a packet yet */
+#define SC_PKTLOST	0x20000000	/* have lost or dropped a packet */
+#define	SC_FLUSH	0x40000000	/* flush input until next PPP_FLAG */
+#define	SC_ESCAPED	0x80000000	/* saw a PPP_ESCAPE */
+
+/*
+ * Ioctl definitions.
+ */
+
+struct npioctl {
+    int		protocol;	/* PPP procotol, e.g. PPP_IP */
+    enum NPmode	mode;
+};
+
+/* Structure describing a CCP configuration option, for PPPIOCSCOMPRESS */
+struct ppp_option_data {
+	u_char	*ptr;
+	u_int	length;
+	int	transmit;
+};
+
+struct ifpppstatsreq {
+    char ifr_name[IFNAMSIZ];
+    struct ppp_stats stats;
+};
+
+struct ifpppcstatsreq {
+    char ifr_name[IFNAMSIZ];
+    struct ppp_comp_stats stats;
+};
+
+/*
+ * Ioctl definitions.
+ */
+
 #define	PPPIOCGFLAGS	_IOR('t', 90, int)	/* get configuration flags */
 #define	PPPIOCSFLAGS	_IOW('t', 89, int)	/* set configuration flags */
 #define	PPPIOCGASYNCMAP	_IOR('t', 88, int)	/* get async map */
@@ -140,9 +113,24 @@ struct ppp_softc {
 #define PPPIOCGXASYNCMAP _IOR('t', 80, ext_accm) /* get extended ACCM */
 #define PPPIOCSXASYNCMAP _IOW('t', 79, ext_accm) /* set extended ACCM */
 #define PPPIOCXFERUNIT	_IO('t', 78)		/* transfer PPP unit */
+#define PPPIOCSCOMPRESS	_IOW('t', 77, struct ppp_option_data)
+#define PPPIOCGNPMODE	_IOWR('t', 76, struct npioctl) /* get NP mode */
+#define PPPIOCSNPMODE	_IOW('t', 75, struct npioctl)  /* set NP mode */
+#define PPPIOCGIDLE	_IOR('t', 74, struct ppp_idle) /* get idle time */
+
+/* PPPIOC[GS]MTU are alternatives to SIOC[GS]IFMTU, used under Ultrix */
+#define PPPIOCGMTU	_IOR('t', 73, int)	/* get interface MTU */
+#define PPPIOCSMTU	_IOW('t', 72, int)	/* set interface MTU */
+
+/*
+ * These two are interface ioctls so that pppstats can do them on
+ * a socket without having to open the serial device.
+ */
+#define SIOCGPPPSTATS	_IOWR('i', 123, struct ifpppstatsreq)
+#define SIOCGPPPCSTATS	_IOWR('i', 122, struct ifpppcstatsreq)
 
 #if !defined(ifr_mtu)
-#define ifr_mtu	ifr_metric
+#define ifr_mtu	ifr_ifru.ifru_metric
 #endif
 
 #endif /* _IF_PPP_H_ */
