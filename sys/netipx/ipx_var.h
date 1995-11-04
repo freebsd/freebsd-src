086c0271eed83e318fd98b71b2000b4082a86f73
julian <julian@FreeBSD.org>
1995-11-04 09:03:47 +0000

    Suggested by: bde clear up some confusion about Id: lines on behalf of the author

--- sys/netipx/ipx_var.h
+++ sys/netipx/ipx_var.h
@@ -31,7 +31,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)$Id$
+ *	@(#)ipx_var.h
+ *
+ * $Id: ipx_var.h,v 1.2 1995/10/31 23:36:40 julian Exp $
  */
 
 #ifndef _NETIPX_IPX_VAR_H_
