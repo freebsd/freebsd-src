1f51b31ec3978813dd910fca3dcde0399c4b1956
ache <ache@FreeBSD.org>
1995-11-23 20:19:56 +0000

    Merge with current. Back out all trailing spaces changes, they hits again and again.

--- usr.bin/ncftp/.cvsimport.sh
+++ usr.bin/ncftp/.cvsimport.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-tar zxvf ncftp-1.9.4.tgz
-cd ncftp-1.9.4
+tar zxvf ncftp-1.9.5.tgz
+cd ncftp-1.9.5
 rm Makefile
-cvs import src/usr.bin/ncftp mgleason ncftp_1_9_4
+cvs import src/usr.bin/ncftp mgleason ncftp_1_9_5
