1630f073cf7c100097ffefd2e33cb30b2951d1be
Bruce Evans <bde@FreeBSD.org>
1995-12-03 13:45:34 +0000

    Moved inline functions for insque() and remque() to <sys/queue.h>. Protected them with `#ifdef KERNEL' so that <sys/queue.h> is valid C++. Added the necessary #includes of <sys/queue.h>.
    These functions are bogus and should be replaced by the queue macros.

--- sys/netns/ns_pcb.c
+++ sys/netns/ns_pcb.c
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)ns_pcb.c	8.1 (Berkeley) 6/10/93
- * $Id: ns_pcb.c,v 1.2 1994/08/02 07:51:52 davidg Exp $
+ * $Id: ns_pcb.c,v 1.3 1995/05/30 08:12:28 rgrimes Exp $
  */
 
 #include <sys/param.h>
@@ -41,6 +41,7 @@
 #include <sys/socket.h>
 #include <sys/socketvar.h>
 #include <sys/protosw.h>
+#include <sys/queue.h>
 
 #include <net/if.h>
 #include <net/route.h>
