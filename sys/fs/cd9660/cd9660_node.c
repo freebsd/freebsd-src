10dd32cd874ebcf9b45c1a28d6309a94725866ad
Bruce Evans <bde@FreeBSD.org>
1995-12-03 17:14:38 +0000

    Added prototypes.
    cd9660_rrip.c:
    Added lots of bogus casts to hide type errors exposed by the prototypes.
    (Different structs are assumed to have a common prefix.)
    
    cd9660_vnops.c:
    Finished staticizing.

--- sys/fs/cd9660/cd9660_node.c
+++ sys/fs/cd9660/cd9660_node.c
@@ -36,7 +36,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)cd9660_node.c	8.2 (Berkeley) 1/23/94
- * $Id: cd9660_node.c,v 1.10 1995/03/16 18:12:15 bde Exp $
+ * $Id: cd9660_node.c,v 1.11 1995/05/30 08:04:59 rgrimes Exp $
  */
 
 #include <sys/param.h>
@@ -79,6 +79,8 @@ union iso_dhead {
 	struct iso_dnode *dh_chain[2];
 } iso_dhead[DNOHSZ];
 #endif
+
+static unsigned	cd9660_chars2ui __P((unsigned char *begin, int len));
 
 /*
  * Initialize hash links for inodes and dnodes.
