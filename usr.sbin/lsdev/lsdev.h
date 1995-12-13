a9b0efbc489928e8b1df5cb2c1b948a9128932f2
bde <bde@FreeBSD.org>
1995-12-13 15:10:56 +0000

    Unspammed #includes.
    Fixed printf format strings.

--- usr.sbin/lsdev/lsdev.h
+++ usr.sbin/lsdev/lsdev.h
@@ -1,17 +1,11 @@
 /*
  * Declarations for lsdev(8).
  */
-#include <sys/types.h>
-#include <sys/param.h>
-#include <sys/time.h>
-#include <sys/proc.h>
-#include <vm/vm.h>
-#include <sys/sysctl.h>
-#include <sys/devconf.h>
 
-extern const char *const devtypes[]; /* device type array */
-extern void print_config(struct devconf *); /* machine-specific print routine */
-extern void hprint_config(void); /* machine-specific header printer */
+extern const char *const devtypes[];	/* device type array */
 extern int vflag;
 
-extern int findtype(const char *); /* get device type by name */
+int findtype(const char *);		/* get device type by name */
+void hprint_config(void);		/* machine-specific header printer */
+struct devconf;
+void print_config(struct devconf *);	/* machine-specific print routine */
