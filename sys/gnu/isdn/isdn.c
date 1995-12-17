8c2b2403cd65e1dcf31e1c11240ba61a894298ef
phk <phk@FreeBSD.org>
1995-12-17 21:17:48 +0000

    Staticize and remove $Log$.

--- sys/gnu/isdn/isdn.c
+++ sys/gnu/isdn/isdn.c
@@ -1,6 +1,6 @@
-static char     _isdnid[] = "@(#)$Id: isdn.c,v 1.10 1995/12/08 23:19:44 phk Exp $";
+static char     _isdnid[] = "@(#)$Id: isdn.c,v 1.11 1995/12/10 15:54:17 bde Exp $";
 /*******************************************************************************
- *  II - Version 0.1 $Revision: 1.10 $   $State: Exp $
+ *  II - Version 0.1 $Revision: 1.11 $   $State: Exp $
  *
  * Copyright 1994 Dietmar Friede
  *******************************************************************************
@@ -9,82 +9,7 @@ static char     _isdnid[] = "@(#)$Id: isdn.c,v 1.10 1995/12/08 23:19:44 phk Exp 
  *	jkr@saarlink.de or jkrause@guug.de
  *
  *******************************************************************************
- * $Log: isdn.c,v $
- * Revision 1.10  1995/12/08  23:19:44  phk
- * Julian forgot to make the *devsw structures static.
- *
- * Revision 1.9  1995/12/08  11:13:01  julian
- * Pass 3 of the great devsw changes
- * most devsw referenced functions are now static, as they are
- * in the same file as their devsw structure. I've also added DEVFS
- * support for nearly every device in the system, however
- * many of the devices have 'incorrect' names under DEVFS
- * because I couldn't quickly work out the correct naming conventions.
- * (but devfs won't be coming on line for a month or so anyhow so that doesn't
- * matter)
- *
- * If you "OWN" a device which would normally have an entry in /dev
- * then search for the devfs_add_devsw() entries and munge to make them right..
- * check out similar devices to see what I might have done in them in you
- * can't see what's going on..
- * for a laugh compare conf.c conf.h defore and after... :)
- * I have not doen DEVFS entries for any DISKSLICE devices yet as that will be
- * a much more complicated job.. (pass 5 :)
- *
- * pass 4 will be to make the devsw tables of type (cdevsw * )
- * rather than (cdevsw)
- * seems to work here..
- * complaints to the usual places.. :)
- *
- * Revision 1.8  1995/11/29  14:39:12  julian
- * If you're going to mechanically replicate something in 50 files
- * it's best to not have a (compiles cleanly) typo in it! (sigh)
- *
- * Revision 1.7  1995/11/29  10:47:10  julian
- * OK, that's it..
- * That's EVERY SINGLE driver that has an entry in conf.c..
- * my next trick will be to define cdevsw[] and bdevsw[]
- * as empty arrays and remove all those DAMNED defines as well..
- *
- * Revision 1.6  1995/11/16  10:47:21  bde
- * Fixed a call to the listen function.  A trailing arg was missing.
- *
- * Fixed the type of isdn_check().  A trailing arg was missing.
- *
- * Included "conf.h" to get some prototypes.
- *
- * Completed function declarations.
- *
- * Added prototypes.
- *
- * Removed some useless includes.
- *
- * Revision 1.5  1995/09/08  11:06:58  bde
- * Fix benign type mismatches in devsw functions.  82 out of 299 devsw
- * functions were wrong.
- *
- * Revision 1.4  1995/05/30  07:58:02  rgrimes
- * Remove trailing whitespace.
- *
- * Revision 1.3  1995/03/28  07:54:44  bde
- * Add and move declarations to fix all of the warnings from `gcc -Wimplicit'
- * (except in netccitt, netiso and netns) that I didn't notice when I fixed
- * "all" such warnings before.
- *
- * Revision 1.2  1995/02/15  06:28:29  jkh
- * Fix up include paths, nuke some warnings.
- *
- * Revision 1.1  1995/02/14  15:00:33  jkh
- * An ISDN driver that supports the EDSS1 and the 1TR6 ISDN interfaces.
- * EDSS1 is the "Euro-ISDN", 1TR6 is the soon obsolete german ISDN Interface.
- * Obtained from: Dietmar Friede <dfriede@drnhh.neuhaus.de> and
- * 	Juergen Krause <jkr@saarlink.de>
- *
- * This is only one part - the rest to follow in a couple of hours.
- * This part is a benign import, since it doesn't affect anything else.
- *
- *
- ******************************************************************************/
+ */
 
 /*
  * Copyright (c) 1994 Dietmar Friede (dietmar@friede.de) All rights reserved.
@@ -119,14 +44,14 @@ static char     _isdnid[] = "@(#)$Id: isdn.c,v 1.10 1995/12/08 23:19:44 phk Exp 
 
 isdn_appl_t     isdn_appl[N_ISDN_APPL];
 isdn_ctrl_t     isdn_ctrl[N_ISDN_CTRL];
-int Isdn_Appl, Isdn_Ctrl, Isdn_Typ;
+static int Isdn_Appl, Isdn_Ctrl, Isdn_Typ;
 
-extern void	isdn_attach __P((void));
+static void	isdn_attach __P((void));
 static timeout_t isdn_check;
-extern char	*isdn_get_prot __P((int ap, int dir));
-extern int	isdn_get_prot_size __P((int ap));
-extern int	isdn_set_prot __P((int ap, int dir, char *p));
-extern int	isdn_stat __P((int cn));
+static char	*isdn_get_prot __P((int ap, int dir));
+static int	isdn_get_prot_size __P((int ap));
+static int	isdn_set_prot __P((int ap, int dir, char *p));
+static int	isdn_stat __P((int cn));
 static void	passout __P((int unit, int l, char *buf));
 
 static	d_open_t	isdnopen;
@@ -154,13 +79,13 @@ static prot     activ[6]     = {{0}, {1, 3}};
 u_short isdn_state= 0;
 static isdn_timeout= 0;
 
-int
+static int
 isdn_get_prot_size(int ap)
 {
 	return (prot_size[isdn_appl[ap].prot]);
 }
 
-char *
+static char *
 isdn_get_prot(int ap, int dir)
 {
 	if(dir)
@@ -168,7 +93,7 @@ isdn_get_prot(int ap, int dir)
 	return(passiv[isdn_appl[ap].prot]);
 }
 
-int
+static int
 isdn_set_prot(int ap, int dir, char *p)
 {
 	char           *pr;
@@ -184,7 +109,7 @@ isdn_set_prot(int ap, int dir, char *p)
 	return (l);
 }
 
-void
+static void
 isdn_attach()
 {
 	isdn_appl_t    *appl;
@@ -516,7 +441,7 @@ isdn_start_out(int cn)
 	splx(x);
 }
 
-int
+static int
 isdn_stat(int cn)
 {
 	isdn_ctrl_t    *ctrl = &isdn_ctrl[cn];
