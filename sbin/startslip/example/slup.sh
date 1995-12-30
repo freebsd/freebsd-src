639da796bdbec7320c3c404f008335f308357290
peter <peter@FreeBSD.org>
1995-12-30 19:02:48 +0000

    recording cvs-1.6 file death

--- sbin/startslip/example/slup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-myname=<my.ip.address>
-gateway=<gateway.ip.address>
-netmask=255.255.255.248
-tune="link0 -link2"     # links means force compression
-
-/sbin/ifconfig $1 $2 $tune
-/sbin/ifconfig $1 inet $myname $gateway netmask $netmask
-/sbin/route add default $gateway
