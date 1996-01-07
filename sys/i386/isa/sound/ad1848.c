76158e361c9e79d95ed8cb87a5015bbaade4dfb0
wpaul <wpaul@FreeBSD.org>
1996-01-07 08:12:13 +0000

    Add NMSS to the list of things that local.h checks for when decding whether of not to automatically #define EXCLUDE_AUDIO; MSS is a real audio device and we should not #define EXCLUDE_AUDIO if we have one. (And I want it because it's the only mixer-capable audio driver that I can use with my crummy Packard Bell (nee Aztech) audio board.)
    This fixes the very confusing condition where having all of this:
    
    mss0 at 0x530 irq 10 drq 1 on isa
    gus0: <MS Sound System (CS4231)>
    opl0 at 0x388 on isa
    opl0: <Yamaha OPL-3 FM>
    mpu0 at 0x300 irq 9 drq 0 on isa
    mpu0: <MPU-401 MIDI Interface 0.0 >
    
    will still give you this:
    
    % cat /dev/sndstat
    SoundCard Error: The soundcard system has not been configured
    
    Also remove an unnecessary newline in the printf() message for the
    'gus0' device shown above so that we don't wind up printing a blank
    line between mss0 and gus0.

--- sys/i386/isa/sound/ad1848.c
+++ sys/i386/isa/sound/ad1848.c
@@ -1192,7 +1192,7 @@ ad1848_init (char *name, int io_base, int irq, int dma_playback, int dma_capture
 	     "Generic audio codec (%s)", devc->chip_name);
 
 #if defined(__FreeBSD__)
-  printk ("\ngus0: <%s>", ad1848_pcm_operations[nr_ad1848_devs].name);
+  printk ("gus0: <%s>", ad1848_pcm_operations[nr_ad1848_devs].name);
 #else
   printk (" <%s>", ad1848_pcm_operations[nr_ad1848_devs].name);
 #endif
