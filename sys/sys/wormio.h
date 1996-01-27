da63e0a3df72f32471e5ec41ce8ef0d26834ae7e
joerg <joerg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1996-01-27 17:49:47 +0000

    Add <sys/wormio.h>, containing the declarations that are shared between kernel and userland.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@13651 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ sys/sys/wormio.h
@@ -0,0 +1,62 @@
+/* Shared between kernel & process */
+#ifndef _SYS_WORMIO_H_
+#define _SYS_WORMIO_H_
+
+/***************************************************************\
+* Ioctls for the WORM drive					*
+\***************************************************************/
+
+/*
+ * Quirk select: chose the set of quirk functions to use for this
+ * device.
+ */
+
+struct wormio_quirk_select
+{
+	const char *vendor;	/* vendor name */
+	const char *model;	/* model name */
+};
+
+#define WORMIOCQUIRKSELECT	_IOW('W', 10, struct wormio_quirk_select)
+
+/*
+ * Prepare disk-wide parameters.
+ */
+
+struct wormio_prepare_disk
+{
+	int dummy;		/* use dummy writes, laser turned off */
+	int speed;		/* drive speed selection */
+};
+
+#define WORMIOCPREPDISK		_IOW('W', 20, struct wormio_prepare_disk)
+
+/*
+ * Prepare track-specific parameters.
+ */
+
+struct wormio_prepare_track
+{
+	int audio;		/* audio track (data track if 0) */
+	int preemp;		/* audio with preemphasis */
+};
+
+#define WORMIOCPREPTRACK	_IOW('W', 21, struct wormio_prepare_track)
+
+/*
+ * Fixation: write leadins and leadouts.  Select table-of-contents
+ * type for this session.  If onp is != 0, another session will be
+ * opened.
+ */
+
+struct wormio_fixation
+{
+	int toc_type;		/* TOC type */
+	int onp;		/* open next program area */
+};
+
+#define WORMIOCFIXATION		_IOW('W', 22, struct wormio_fixation)
+
+
+#endif /* _SYS_WORMIO_H_ */
+
