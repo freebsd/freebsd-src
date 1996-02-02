7a065e6b3013c824cd36cd47da450eb6d1946531
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1996-02-02 23:53:48 +0000

    Add install script for doc, make bin install more intelligent.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@13873 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/scripts/doc-install.sh
+++ release/scripts/doc-install.sh
@@ -4,9 +4,9 @@ if [ "`id -u`" != "0" ]; then
 	echo "Sorry, this must be done as root."
 	exit 1
 fi
-echo "You are about to extract the base distribution into / - are you SURE"
+echo "You are about to extract the doc distribution into / - are you SURE"
 echo -n "you want to do this over your installed system (y/n)? "
-read ans 
+read ans
 if [ "$ans" = "y" ]; then
-	cat bin.?? | tar --unlink -xpzf - -C /
+	cat doc.?? | tar --unlink -xpzf - -C /
 fi
