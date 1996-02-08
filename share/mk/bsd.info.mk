1e87f8e1fa62612871d970951225edbb2757ca0c
mpp <mpp@FreeBSD.org>
1996-02-08 18:27:07 +0000

    Make sure that the --no-split flag gets passed to makeinfo. Fixes PR # 1003 (gdb info files don't work).

--- share/mk/bsd.info.mk
+++ share/mk/bsd.info.mk
@@ -1,9 +1,9 @@
-# $Id: bsd.info.mk,v 1.14 1995/07/26 13:51:46 wollman Exp $
+# $Id: bsd.info.mk,v 1.15 1995/07/27 15:03:27 wollman Exp $
 
 BINMODE=        444
 BINDIR?=	/usr/share/info
 MAKEINFO?=	makeinfo
-MAKEINFOFLAGS?=	--no-split # simplify some things, e.g., compression
+MAKEINFOFLAGS+=	--no-split # simplify some things, e.g., compression
 
 .MAIN: all
 
