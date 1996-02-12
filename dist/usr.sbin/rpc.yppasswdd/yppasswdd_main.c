fd226e97ab8436e8deae6ef82ba9ecfc0573f8fa
wpaul <wpaul@FreeBSD.org>
1996-02-12 15:09:01 +0000

    Import new rpc.yppasswdd. (Note: accompanying changes to passwd(1) and chpass(1) are on the way too.) This version supports all the features of the old one and adds several new ones:
    - Supports real multi-domain operation (optional, can be turned
      on with a command-line flag). This means you can actually have
      several different domains all served from one NIS server and
      allow users in any of the supported domains to change their passwords.
      The old yppasswdd only allowed changing passwords in the domain
      that was set as the system default domain name on the NIS master
      server. The new one can change passwords in any domain by trying
      to match the user information passed to it against all the passwd
      maps it can find. This is something of a hack, but the yppasswd.x
      protocol definiton does not allow for a domain to be passwd as an
      argument to rpc.yppasswdd, so the server has no choice but to
      grope around for a likely match. Since this method can fail if
      the same user exists in two domains, this feature is off by default.
      If the feature is turned on and the server becomes confused by
      duplicate entries, it will abort the update.
    
    - Does not require NIS client services to be available. NIS servers do
      _NOT_ necessarily have to be configured as NIS clients in order to
      function: the ypserv, ypxfr and yppush programs I've written recently
      will operate fine even if the system domain name isn't set, ypbind isn't
      running and there are no magic '+' entries in any of the /etc files.
      Now rpc.yppasswdd is the same way. The old yppasswdd would not work
      like this because it depended on getpwent(3) and friends to look up
      users: this will obviously only work if the system where yppasswdd is
      running is configured as an NIS client. The new rpc.yppasswdd doesn't
      use getpwent(3) at all: instead it searches through the master.passwd
      map databases directly. This also makes it easier for it to handle
      multiple domains.
    
    - Allows the superuser on the NIS master server to change any user's
      password without requiring password authentication. rpc.yppasswdd
      creates a UNIX domain socket (/var/run/ypsock) which it monitors
      using the same svc_run() loop used to handle incoming RPC requests.
      It also clears all the permission bits for /var/run/ypsock; since
      this socket is owned by root, this prevents anyone except root from
      successfully connect()ing to it. (Using a UNIX domain socket also
      prevents IP spoofing attacks.) By building code into passwd(1) and
      chpass(1) to take advantage of this 'trusted' channel, the superuser
      can use them to send private requests to rpc.yppasswdd.
    
    - Allows the superuser on the NIS master to use chpass(1) to update _all_
      of a user's master.passwd information. The UNIX domain access point
      accepts a full master.passwd style structure (along with a domain
      name and other information), which allows the superuser to update all
      of a user's master.passwd information in the NIS master.passwd maps.
      Normal users on NIS clients are still only allowed to change their full
      name and shell information with chpass.
    
    - Allows the superuser on the NIS master to _add_ records to the NIS
      master.passwd maps using chpass(1). This feature is also switchable
      with a command-line flag and is off by default.

---
+++ dist/usr.sbin/rpc.yppasswdd/yppasswdd_main.c
@@ -0,0 +1,362 @@
+/*
+ * Copyright (c) 1995, 1996
+ *	Bill Paul <wpaul@ctr.columbia.edu>.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Bill Paul.
+ * 4. Neither the name of the author nor the names of any co-contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	$Id: yppasswdd_main.c,v 1.8 1996/02/09 04:21:35 wpaul Exp $
+ */
+
+#include "yppasswd.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h> /* getenv, exit */
+#include <unistd.h>
+#include <string.h>
+#include <sys/param.h>
+#include <rpc/pmap_clnt.h> /* for pmap_unset */
+#include <string.h> /* strcmp */
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/ttycom.h> /* TIOCNOTTY */
+#ifdef __cplusplus
+#include <sysent.h> /* getdtablesize, open */
+#endif /* __cplusplus */
+#include <memory.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <syslog.h>
+#include <err.h>
+#include <errno.h>
+#include <rpcsvc/yp.h>
+struct dom_binding {};
+#include <rpcsvc/ypclnt.h>
+#include "yppasswdd_extern.h"
+#include "yppasswd_comm.h"
+#include "ypxfr_extern.h"
+
+#ifndef SIG_PF
+#define	SIG_PF void(*)(int)
+#endif
+
+#ifdef DEBUG
+#define	RPC_SVC_FG
+#endif
+
+#define	_RPCSVC_CLOSEDOWN 120
+#ifndef lint
+static const char rcsid[] = "$Id: yppasswdd_main.c,v 1.8 1996/02/09 04:21:35 wpaul Exp $";
+#endif /* not lint */
+int _rpcpmstart = 0;		/* Started by a port monitor ? */
+static int _rpcfdtype;
+		 /* Whether Stream or Datagram ? */
+	/* States a server can be in wrt request */
+
+#define	_IDLE 0
+#define	_SERVED 1
+#define	_SERVING 2
+
+extern int _rpcsvcstate;	 /* Set when a request is serviced */
+char *progname = "rpc.yppasswdd";
+char *passfile_default = "/var/yp/master.passwd";
+char *passfile;
+char *yppasswd_domain = NULL;
+int no_chsh = 0;
+int no_chfn = 0;
+int allow_additions = 0;
+int multidomain = 0;
+int verbose = 0;
+char *yp_dir = "/var/yp/";
+int yp_sock;
+
+
+static void
+my_svc_run()
+{
+#ifdef FD_SETSIZE
+	fd_set readfds;
+#else
+      int readfds;
+#endif /* def FD_SETSIZE */
+	extern int errno;
+
+	for (;;) {
+
+
+#ifdef FD_SETSIZE
+		readfds = svc_fdset;
+#else
+		readfds = svc_fds;
+#endif /* def FD_SETSIZE */
+		FD_SET(yp_sock, &readfds);
+
+		switch (select(_rpc_dtablesize(), &readfds, (fd_set *)0, (fd_set *)0,
+			       (struct timeval *)0)) {
+		case -1:
+			if (errno == EINTR) {
+				continue;
+			}
+			perror("svc_run: - select failed");
+			return;
+		case 0:
+			continue;
+		default:
+			if (FD_ISSET(yp_sock, &readfds)) {
+				do_master();
+				FD_CLR(yp_sock, &readfds);
+			}
+			svc_getreqset(&readfds);
+		}
+	}
+}
+
+static void terminate(sig)
+	int sig;
+{
+	svc_unregister(YPPASSWDPROG, YPPASSWDVERS);
+	close(yp_sock);
+	unlink(sockname);
+	exit(0);
+}
+	
+static void
+closedown(int sig)
+{
+	if (_rpcsvcstate == _IDLE) {
+		extern fd_set svc_fdset;
+		static int size;
+		int i, openfd;
+
+		if (_rpcfdtype == SOCK_DGRAM) {
+			close(yp_sock);
+			unlink(sockname);
+			exit(0);
+		}
+		if (size == 0) {
+			size = getdtablesize();
+		}
+		for (i = 0, openfd = 0; i < size && openfd < 2; i++)
+			if (FD_ISSET(i, &svc_fdset))
+				openfd++;
+		if (openfd <= 1) {
+			close(yp_sock);
+			unlink(sockname);
+			exit(0);
+		}
+	}
+	if (_rpcsvcstate == _SERVED)
+		_rpcsvcstate = _IDLE;
+
+	(void) signal(SIGALRM, (SIG_PF) closedown);
+	(void) alarm(_RPCSVC_CLOSEDOWN/2);
+}
+
+static void usage()
+{
+	fprintf(stderr, "Usage: %s [-t master.passwd file] [-d domain] \
+[-p path] [-s] [-f] [-m] [-a] [-v] [-h]\n",
+		progname);
+	exit(1);
+}
+
+main(argc, argv)
+	int argc;
+	char *argv[];
+{
+	register SVCXPRT *transp = NULL;
+	int sock;
+	int proto = 0;
+	struct sockaddr_in saddr;
+	int asize = sizeof (saddr);
+	int ch;
+	int rval;
+	char *mastername;
+	char myname[MAXHOSTNAMELEN + 2];
+	extern int errno;
+	extern int debug;
+
+	debug = 1;
+
+	while ((ch = getopt(argc, argv, "t:d:p:sfamvh")) != EOF) {
+		switch(ch) {
+		case 't':
+			passfile_default = optarg;
+			break;
+		case 'd':
+			yppasswd_domain = optarg;
+			break;
+		case 's':
+			no_chsh++;
+			break;
+		case 'f':
+			no_chfn++;
+			break;
+		case 'p':
+			yp_dir = optarg;
+			break;
+		case 'a':
+			allow_additions++;
+			break;
+		case 'm':
+			multidomain++;
+			break;
+		case 'v':
+			verbose++;
+			break;
+		default:
+		case 'h':
+			usage();
+			break;
+		}
+	}
+
+	if (yppasswd_domain == NULL) {
+		if (yp_get_default_domain(&yppasswd_domain)) {
+			yp_error("no domain specified and system domain \
+name isn't set -- aborting");
+		usage();
+		}
+	}
+
+	if (getrpcport("localhost", YPPROG, YPVERS, IPPROTO_UDP) <= 0) {
+		yp_error("this host is not an NIS server -- aborting");
+		exit(1);
+	}
+
+	if ((mastername = ypxfr_get_master(yppasswd_domain, "passwd.byname",
+						"localhost",0)) == NULL) {
+		yp_error("can't get name of NIS master server");
+		exit(1);
+	}
+
+	if (gethostname((char *)&myname, sizeof(myname)) == -1) {
+		yp_error("can't get local hostname: %s", strerror(errno));
+		exit(1);
+	}
+
+	if (strncmp(mastername, (char *)&myname, sizeof(myname))) {
+		yp_error("this host is not an NIS master server -- aborting");
+		exit(1);
+	}
+
+	if (getsockname(0, (struct sockaddr *)&saddr, &asize) == 0) {
+		int ssize = sizeof (int);
+
+		if (saddr.sin_family != AF_INET)
+			exit(1);
+		if (getsockopt(0, SOL_SOCKET, SO_TYPE,
+				(char *)&_rpcfdtype, &ssize) == -1)
+			exit(1);
+		sock = 0;
+		_rpcpmstart = 1;
+		proto = 0;
+		openlog(progname, LOG_PID, LOG_DAEMON);
+	} else {
+#ifndef RPC_SVC_FG
+		int size;
+		int pid, i;
+
+		pid = fork();
+		if (pid < 0) {
+			perror("cannot fork");
+			exit(1);
+		}
+		if (pid)
+			exit(0);
+		size = getdtablesize();
+		for (i = 0; i < size; i++)
+			(void) close(i);
+		i = open("/dev/console", 2);
+		(void) dup2(i, 1);
+		(void) dup2(i, 2);
+		i = open("/dev/tty", 2);
+		if (i >= 0) {
+			(void) ioctl(i, TIOCNOTTY, (char *)NULL);
+			(void) close(i);
+		}
+		openlog(progname, LOG_PID, LOG_DAEMON);
+#endif
+		sock = RPC_ANYSOCK;
+		(void) pmap_unset(YPPASSWDPROG, YPPASSWDVERS);
+	}
+
+	if ((_rpcfdtype == 0) || (_rpcfdtype == SOCK_DGRAM)) {
+		transp = svcudp_create(sock);
+		if (transp == NULL) {
+			yp_error("cannot create udp service.");
+			exit(1);
+		}
+		if (!_rpcpmstart)
+			proto = IPPROTO_UDP;
+		if (!svc_register(transp, YPPASSWDPROG, YPPASSWDVERS, yppasswdprog_1, proto)) {
+			yp_error("unable to register (YPPASSWDPROG, YPPASSWDVERS, udp).");
+			exit(1);
+		}
+	}
+
+	if ((_rpcfdtype == 0) || (_rpcfdtype == SOCK_STREAM)) {
+		transp = svctcp_create(sock, 0, 0);
+		if (transp == NULL) {
+			yp_error("cannot create tcp service.");
+			exit(1);
+		}
+		if (!_rpcpmstart)
+			proto = IPPROTO_TCP;
+		if (!svc_register(transp, YPPASSWDPROG, YPPASSWDVERS, yppasswdprog_1, proto)) {
+			yp_error("unable to register (YPPASSWDPROG, YPPASSWDVERS, tcp).");
+			exit(1);
+		}
+	}
+
+	if (transp == (SVCXPRT *)NULL) {
+		yp_error("could not create a handle");
+		exit(1);
+	}
+	if (_rpcpmstart) {
+		(void) signal(SIGALRM, (SIG_PF) closedown);
+		(void) alarm(_RPCSVC_CLOSEDOWN/2);
+	}
+	/* set up resporce limits and block signals */
+	pw_init();
+
+	/* except SIGCHLD, which we need to catch */
+	install_reaper(1);
+	signal(SIGTERM, (SIG_PF) terminate);
+
+	unlink(sockname);
+	yp_sock = makeservsock();
+	if (chmod(sockname, 0))
+		err(1, "chmod of %s failed", sockname);
+
+	my_svc_run();
+	yp_error("svc_run returned");
+	exit(1);
+	/* NOTREACHED */
+}
