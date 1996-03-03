d9c682305a8e50e5a1b420e1ecf743110094181f
Peter Wemm <peter@FreeBSD.org>
1996-03-03 18:51:33 +0000

    Add missing prototype for newly public vn_vmio_open function, next to vn_vmio_close.

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)vnode.h	8.7 (Berkeley) 2/4/94
- * $Id: vnode.h,v 1.29 1996/01/19 03:59:06 dyson Exp $
+ * $Id: vnode.h,v 1.30 1996/01/30 23:01:34 mpp Exp $
  */
 
 #ifndef _SYS_VNODE_H_
@@ -422,6 +422,8 @@ int 	vn_rdwr __P((enum uio_rw rw, struct vnode *vp, caddr_t base,
 	    struct ucred *cred, int *aresid, struct proc *p));
 int	vn_stat __P((struct vnode *vp, struct stat *sb, struct proc *p));
 void	vn_vmio_close __P((struct vnode *vp));
+int	vn_vmio_open __P((struct vnode *vp, struct proc *p,
+                struct ucred *cred));
 int 	vn_writechk __P((struct vnode *vp));
 void	vprint __P((char *label, struct vnode *vp));
 void 	vput __P((struct vnode *vp));
