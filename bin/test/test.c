1b88806d8c1baf4daa921b4fe59fbd128c019574
joerg <joerg@FreeBSD.org>
1996-03-11 11:04:23 +0000

    Update from HEAD (err-->errx)

--- bin/test/test.c
+++ bin/test/test.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: test.c,v 1.12 1995/10/28 11:54:42 ache Exp $
+ *	$Id: test.c,v 1.11.4.1 1996/02/20 09:48:53 jkh Exp $
  */
 
 #ifndef lint
@@ -578,12 +578,12 @@ static void
 syntax()
 {
 
-	err(2, "syntax error");
+	errx(2, "syntax error");
 }
 
 static void
 overflow()
 {
 
-	err(2, "expression is too complex");
+	errx(2, "expression is too complex");
 }
