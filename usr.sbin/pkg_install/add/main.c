b8029dbe28e4991e55c7c5b531e4e21c09ad859c
jkh <jkh@FreeBSD.org>
1996-03-12 06:12:43 +0000

    Fix a couple of miscellaneous bugs and make pkg_add also support reading from stdin.

--- usr.sbin/pkg_install/add/main.c
+++ usr.sbin/pkg_install/add/main.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *rcsid = "$Id: main.c,v 1.8 1995/10/25 15:37:47 jkh Exp $";
+static char *rcsid = "$Id: main.c,v 1.9 1995/11/12 04:55:19 jkh Exp $";
 #endif
 
 /*
@@ -113,7 +113,9 @@ main(int argc, char **argv)
 
 	/* Get all the remaining package names, if any */
 	for (ch = 0; *argv; ch++, argv++) {
-	    if (isURL(*argv))	/* preserve URLs */
+	    if (!strcmp(*argv, "-"))	/* stdin? */
+		pkgs[ch] = "-";
+	    else if (isURL(*argv))	/* preserve URLs */
 		pkgs[ch] = strcpy(pkgnames[ch], *argv);
 	    else {			/* expand all pathnames to fullnames */
 		if (fexists(*argv)) /* refers to a file directly */
