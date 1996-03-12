8a9da25ead273a1a9d8e8f7152426c7d80e0cadd
wosch <wosch@FreeBSD.org>
1996-03-12 00:07:28 +0000

    add DOCDIR, DOCOWN, DOCGRP, DOCMODE for documents add LIBDIR, LINTLIBDIR, SHLIBDIR, LIBOWN, LIBGRP, LIBMODE for libs add KMODOWN, KMODGRP, KMODMODE new variable KMODDIR similar to BINDIR

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,8 +1,4 @@
-#	$Id: bsd.own.mk,v 1.1 1994/08/04 21:10:08 wollman Exp $
-
-BINGRP?=	bin
-BINOWN?=	bin
-BINMODE?=	555
+#	$Id: bsd.own.mk,v 1.2 1994/09/16 14:30:21 jkh Exp $
 
 .if !defined(DEBUG_FLAGS)
 STRIP?=		-s
@@ -10,7 +6,29 @@ STRIP?=		-s
 
 COPY?=		-c
 
+
+BINOWN?=	bin
+BINGRP?=	bin
+BINMODE?=	555
+
 MANDIR?=	/usr/share/man/man
-MANGRP?=	bin
 MANOWN?=	bin
+MANGRP?=	bin
 MANMODE?=	444
+
+DOCDIR?=	/usr/share
+DOCOWN?=	bin
+DOCGRP?=	bin
+DOCMODE?=	444
+
+LIBDIR?=	/usr/lib
+LINTLIBDIR?=	/usr/libdata/lint
+SHLIBDIR?=	${LIBDIR}
+LIBOWN?=	bin
+LIBGRP?=	bin
+LIBMODE?=	444
+
+KMODDIR=	/lkm
+KMODOWN?=	bin
+KMODGRP?=	bin
+KMODMODE?=	555
