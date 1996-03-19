f88ce5b1d9e1f31052f0701d026e74270c8192c3
bde <bde@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1996-03-19 14:30:15 +0000

    sysinstall/Makefile: Fixed DPADD again.
    mk/bsd.README
    Don't list the LIBXXX identifiers here.  Describe them better.
    
    mk/bsd.prog.mk
    Updated the list of LIBXXX identifiers.
    - recently added library libdisk.a wasn't mentioned (required for sysinstall)
    - old objects kz*.o weren't mentioned
    - old libraries libc_pic.a, libcom_err.a, libf2c.a, libg++.a, libgcc_pic.a,
      libgmp.a, libipx.a, libkeycap.a, libss.a and libxpg4.a weren't mentioned
    - old libraries libgnumalloc.a and libftp.a no longer exist
    - old library libmp.a was said to not exist
    - deprecated links libfl.a and libln.a weren't mentioned
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@14701 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -1,5 +1,5 @@
 #	from: @(#)bsd.prog.mk	5.26 (Berkeley) 6/25/91
-#	$Id: bsd.prog.mk,v 1.27 1995/10/02 20:01:53 wollman Exp $
+#	$Id: bsd.prog.mk,v 1.28 1996/03/09 23:48:55 wosch Exp $
 
 .if exists(${.CURDIR}/../Makefile.inc)
 .include "${.CURDIR}/../Makefile.inc"
@@ -22,25 +22,37 @@ BINOWN?=	bin
 BINMODE?=	555
 
 LIBCRT0?=	${DESTDIR}/usr/lib/crt0.o
+LIBKZHEAD?=	${DESTDIR}/usr/lib/kzhead.o
+LIBKZTAIL?=	${DESTDIR}/usr/lib/kztail.o
+
 LIBC?=		${DESTDIR}/usr/lib/libc.a
+LIBC_PIC=	${DESTDIR}/usr/lib/libc_pic.a
+LIBCOM_ERR=	${DESTDIR}/usr/lib/libcom_err.a
 LIBCOMPAT?=	${DESTDIR}/usr/lib/libcompat.a
 LIBCRYPT?=	${DESTDIR}/usr/lib/libcrypt.a
 LIBCURSES?=	${DESTDIR}/usr/lib/libcurses.a
 LIBDES?=	${DESTDIR}/usr/lib/libdes.a	# XXX doesn't exist
 LIBDIALOG?=	${DESTDIR}/usr/lib/libdialog.a
+LIBDISK?=	${DESTDIR}/usr/lib/libdisk.a
 LIBEDIT?=	${DESTDIR}/usr/lib/libedit.a
+LIBF2C?=	${DESTDIR}/usr/lib/libf2c.a
+LIBFL?=		"don't use LIBFL, use LIBL"
 LIBFORMS?=	${DESTDIR}/usr/lib/libforms.a
-LIBFTP?=	${DESTDIR}/usr/lib/libftp.a
+LIBGPLUSPLUS?=	${DESTDIR}/usr/lib/libg++.a
 LIBGCC?=	${DESTDIR}/usr/lib/libgcc.a
-LIBGNUMALLOC?=	${DESTDIR}/usr/lib/libgnumalloc.a
+LIBGCC_PIC?=	${DESTDIR}/usr/lib/libgcc_pic.a
+LIBGMP?=	${DESTDIR}/usr/lib/libgmp.a
 LIBGNUREGEX?=	${DESTDIR}/usr/lib/libgnuregex.a
+LIBIPX?=	${DESTDIR}/usr/lib/libipx.a
 LIBKDB?=	${DESTDIR}/usr/lib/libkdb.a	# XXX doesn't exist
 LIBKRB?=	${DESTDIR}/usr/lib/libkrb.a	# XXX doesn't exist
+LIBKEYCAP?=	${DESTDIR}/usr/lib/libkeycap.a
 LIBKVM?=	${DESTDIR}/usr/lib/libkvm.a
 LIBL?=		${DESTDIR}/usr/lib/libl.a
+LIBLN?=		"don't use, LIBLN, use LIBL"
 LIBM?=		${DESTDIR}/usr/lib/libm.a
 LIBMD?=		${DESTDIR}/usr/lib/libmd.a
-LIBMP?=		${DESTDIR}/usr/lib/libmp.a	# XXX doesn't exist
+LIBMP?=		${DESTDIR}/usr/lib/libmp.a
 LIBMYTINFO?=	${DESTDIR}/usr/lib/libmytinfo.a
 LIBNCURSES?=	${DESTDIR}/usr/lib/libncurses.a
 LIBPC?=		${DESTDIR}/usr/lib/libpc.a	# XXX doesn't exist
@@ -49,12 +61,15 @@ LIBPLOT?=	${DESTDIR}/usr/lib/libplot.a	# XXX doesn't exist
 LIBREADLINE?=	${DESTDIR}/usr/lib/libreadline.a
 LIBRESOLV?=	${DESTDIR}/usr/lib/libresolv.a
 LIBRPCSVC?=	${DESTDIR}/usr/lib/librpcsvc.a
-LIBSCRYPT?=	${DESTDIR}/usr/lib/libscrypt.a	# XXX don't use, use LIBCRYPT
+LIBSCRYPT?=	"don't use LIBSCRYPT, use LIBCRYPT"
 LIBSCSI?=	${DESTDIR}/usr/lib/libscsi.a
 LIBSKEY?=	${DESTDIR}/usr/lib/libskey.a
+LIBSS?=		${DESTDIR}/usr/lib/libss.a
 LIBTELNET?=	${DESTDIR}/usr/lib/libtelnet.a
 LIBTERMCAP?=	${DESTDIR}/usr/lib/libtermcap.a
+LIBTERMLIB?=	"don't use LIBTERMLIB, use LIBTERMCAP"
 LIBUTIL?=	${DESTDIR}/usr/lib/libutil.a
+LIBXPG4?=	${DESTDIR}/usr/lib/libxpg4.a
 LIBY?=		${DESTDIR}/usr/lib/liby.a
 
 .if defined(NOSHARED)
