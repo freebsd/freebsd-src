04e28f155b1cfcb911b405bea2c951246a5a3fd8
wosch <wosch@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1996-03-31 23:15:17 +0000

    add SHAREDIR?=, SHAREOWN?=, SHAREGRP?=, SHAREMODE?=, INFOOWN?=, INFOGRP?=, INFOMODE?=
    set variables recursive, eg. change KMODOWN?=bin to KMODOWN?=${BINOWN}
    
    from NetBSD:
    	add NLSDIR?=, NLSGRP?=, NLSOWN?=, NLSMODE?=,
    	BSDSRCDIR?=, BSDOBJDIR?=, NOBINMODE?=
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@14968 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,36 +1,59 @@
-#	$Id: bsd.own.mk,v 1.3 1996/03/12 00:07:28 wosch Exp $
+#	$Id: bsd.own.mk,v 1.4 1996/03/24 00:31:56 wosch Exp $
 
+# where the system object and source trees are kept; can be configurable
+# by the user in case they want them in ~/foosrc and ~/fooobj, for example
+BSDSRCDIR?=	/usr/src
+BSDOBJDIR?=	/usr/obj
+
+
+# Binaries
+BINOWN?=	bin
+BINGRP?=	bin
+BINMODE?=	555
+NOBINMODE?=	444
+
+LIBDIR?=	/usr/lib
+LINTLIBDIR?=	/usr/libdata/lint
+SHLIBDIR?=	${LIBDIR}
+LIBOWN?=	${BINOWN}
+LIBGRP?=	${BINGRP}
+LIBMODE?=	${NOBINMODE}
+
+KMODDIR?=	/lkm
+KMODOWN?=	${BINOWN}
+KMODGRP?=	${BINGRP}
+KMODMODE?=	${BINMODE}
+
+
+# Share files
+SHAREDIR?=	/usr/share
+SHAREOWN?=	bin
+SHAREGRP?=	bin
+SHAREMODE?=	${NOBINMODE}
+
+MANDIR?=	${SHAREDIR}/man/man
+MANOWN?=	${SHAREOWN}
+MANGRP?=	${SHAREGRP}
+MANMODE?=	${NOBINMODE}
+
+DOCDIR?=	${SHAREDIR}/doc
+DOCOWN?=	${SHAREOWN}
+DOCGRP?=	${SHAREGRP}
+DOCMODE?=	${NOBINMODE}
+
+INFODIR?=	${SHAREDIR}/info
+INFOOWN?=	${SHAREOWN}
+INFOGRP?=	${SHAREGRP}
+INFOMODE?=	${NOBINMODE}
+
+NLSDIR?=	${SHAREDIR}/nls
+NLSGRP?=	${SHAREOWN}
+NLSOWN?=	${SHAREGRP}
+NLSMODE?=	${NONBINMODE}
+
+# Common variables
 .if !defined(DEBUG_FLAGS)
 STRIP?=		-s
 .endif
 
 COPY?=		-c
-
-
-BINOWN?=	bin
-BINGRP?=	bin
-BINMODE?=	555
-
-MANDIR?=	/usr/share/man/man
-MANOWN?=	bin
-MANGRP?=	bin
-MANMODE?=	444
-
-DOCDIR?=	/usr/share
-DOCOWN?=	bin
-DOCGRP?=	bin
-DOCMODE?=	444
-
-INFODIR?=	/usr/share/info
-
-LIBDIR?=	/usr/lib
-LINTLIBDIR?=	/usr/libdata/lint
-SHLIBDIR?=	${LIBDIR}
-LIBOWN?=	bin
-LIBGRP?=	bin
-LIBMODE?=	444
-
-KMODDIR?=	/lkm
-KMODOWN?=	bin
-KMODGRP?=	bin
-KMODMODE?=	555
