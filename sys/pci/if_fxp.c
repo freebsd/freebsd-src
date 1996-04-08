c36dcc378cf1d38aced8b7731150921206f3a3de
dg <dg@FreeBSD.org>
1996-04-08 01:32:00 +0000

    From main branch: updates to my copyright.

--- sys/pci/if_fxp.c
+++ sys/pci/if_fxp.c
@@ -11,11 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by David Greenman.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -29,7 +24,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: if_fxp.c,v 1.8 1996/01/15 10:12:41 davidg Exp $
+ *	$Id: if_fxp.c,v 1.8.2.1 1996/01/29 12:59:47 davidg Exp $
  */
 
 /*
