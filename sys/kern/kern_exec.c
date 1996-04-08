d1862b565569e48af7d5c68a0ddfb710f5601621
dg <dg@FreeBSD.org>
1996-04-08 01:22:00 +0000

    Killed sections 3 and 4 of my copyright as I don't agree with it (I believe it to be unnecessarily restrictive). For tty_subr.c, update to my standard copyright.

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -10,11 +10,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by David Greenman
- * 4. The name of the developer may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -28,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: kern_exec.c,v 1.36 1996/03/02 19:38:08 peter Exp $
+ *	$Id: kern_exec.c,v 1.37 1996/03/10 08:42:54 sos Exp $
  */
 
 #include <sys/param.h>
