972c3fb099c0c7892d08924d1c0d55e6771f196c
David Greenman <dg@FreeBSD.org>
1996-04-08 01:32:00 +0000

    From main branch: updates to my copyright.

--- sys/i386/isa/if_ed.c
+++ sys/i386/isa/if_ed.c
@@ -11,11 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by David Greenman.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -29,7 +24,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: if_ed.c,v 1.73.4.4 1996/01/03 17:51:32 davidg Exp $
+ *	$Id: if_ed.c,v 1.73.4.5 1996/03/31 15:55:01 joerg Exp $
  */
 
 /*
