58b248e2545ed4415d83f2a88839fb2ccd8b4bbb
David Greenman <dg@FreeBSD.org>
1996-04-08 01:31:42 +0000

    Removed sections 3 and 4 from my copyright.

--- sys/dev/fxp/if_fxpreg.h
+++ sys/dev/fxp/if_fxpreg.h
@@ -11,11 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by David Greenman.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -29,7 +24,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: if_fxpreg.h,v 1.1 1995/11/28 23:55:26 davidg Exp $
+ *	$Id: if_fxpreg.h,v 1.2 1995/12/05 11:49:55 davidg Exp $
  */
 
 #define FXP_VENDORID_INTEL	0x8086
