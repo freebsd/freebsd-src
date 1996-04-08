3c856887118e5ab3a2a868f2811ef55b6bf21dd7
dg <dg@FreeBSD.org>
1996-04-08 01:28:10 +0000

    From current: updates to my copyright.

--- sys/kern/imgact_shell.c
+++ sys/kern/imgact_shell.c
@@ -10,11 +10,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by David Greenman
- * 4. The name of the developer may be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -28,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: imgact_shell.c,v 1.6 1994/09/25 19:33:32 phk Exp $
+ *	$Id: imgact_shell.c,v 1.6.8.1 1995/09/08 13:25:46 davidg Exp $
  */
 
 #include <sys/param.h>
