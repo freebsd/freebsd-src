438d9b3a8b99c5c9ff7fdbd4ddebe8b7e4854770
nate <nate@FreeBSD.org>
1996-04-10 05:45:39 +0000

    Run indent on all these files to make them more readable.  (I also went through by hand and cleaned up some indent bogons.)

--- usr.sbin/pccard/pccardc/rdreg.c
+++ usr.sbin/pccard/pccardc/rdreg.c
@@ -8,47 +8,42 @@
 #include <pccard/card.h>
 void
 dumpslot(sl)
-int	sl;
+	int     sl;
 {
-char	name[64];
-int	fd;
-struct pcic_reg r;
+	char    name[64];
+	int     fd;
+	struct pcic_reg r;
 
 	sprintf(name, "/dev/card%d", sl);
 	fd = open(name, 2);
-	if (fd < 0)
-		{
+	if (fd < 0) {
 		perror(name);
 		return;
-		}
+	}
 	printf("Registers for slot %d\n", sl);
-	for (r.reg = 0; r.reg < 0x40; r.reg++)
-		{
-		if (ioctl(fd, PIOCGREG, &r))
-			{
+	for (r.reg = 0; r.reg < 0x40; r.reg++) {
+		if (ioctl(fd, PIOCGREG, &r)) {
 			perror("ioctl");
 			break;
-			}
-		if ((r.reg % 16)==0)
+		}
+		if ((r.reg % 16) == 0)
 			printf("%02x:", r.reg);
 		printf(" %02x", r.value);
-		if ((r.reg % 16)==15)
+		if ((r.reg % 16) == 15)
 			printf("\n");
-		}
+	}
 	close(fd);
 }
+
 int
 rdreg_main(argc, argv)
-int	argc;
-char	*argv[];
+	int     argc;
+	char   *argv[];
 {
-	if (argc != 2)
-		{
+	if (argc != 2) {
 		dumpslot(0);
 		dumpslot(1);
-		}
-	else
+	} else
 		dumpslot(atoi(argv[1]));
 	return 0;
 }
-
--- usr.sbin/pccard/pccardc/wrattr.c
+++ usr.sbin/pccard/pccardc/wrattr.c
@@ -9,38 +9,34 @@
 
 int
 wrattr_main(argc, argv)
-int	argc;
-char	*argv[];
+	int     argc;
+	char   *argv[];
 {
-int reg,value;
-char	name[64], c;
-int	fd;
-off_t offs;
+	int     reg, value;
+	char    name[64], c;
+	int     fd;
+	off_t   offs;
 
-	if (argc != 4)
-		{
+	if (argc != 4) {
 		fprintf(stderr, "usage: wrmem slot offs value\n");
 		exit(1);
-		}
+	}
 	sprintf(name, "/dev/card%d", atoi(argv[1]));
 	fd = open(name, 2);
-	if (fd < 0)
-		{
+	if (fd < 0) {
 		perror(name);
 		exit(1);
-		}
+	}
 	reg = MDF_ATTR;
-	if (ioctl(fd, PIOCRWFLAG, &reg))
-		{
+	if (ioctl(fd, PIOCRWFLAG, &reg)) {
 		perror("ioctl (PIOCRWFLAG)");
 		exit(1);
-		}
+	}
 	if (sscanf(argv[2], "%x", &reg) != 1 ||
-	    sscanf(argv[3], "%x", &value) != 1)
-		{
+	    sscanf(argv[3], "%x", &value) != 1) {
 		fprintf(stderr, "arg error\n");
 		exit(1);
-		}
+	}
 	offs = reg;
 	c = value;
 	lseek(fd, offs, SEEK_SET);
