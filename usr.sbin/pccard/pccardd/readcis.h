7f78c5620d351e28c1f4540531feda28e83df27f
nate <nate@FreeBSD.org>
1996-04-10 06:49:32 +0000

    Run indent on all these files to make them more readable.  (I also went through by hand and cleaned up some indent bogons.)

--- usr.sbin/pccard/pccardd/readcis.h
+++ usr.sbin/pccard/pccardd/readcis.h
@@ -1,67 +1,63 @@
-
 #define	MAXSTR	20
 /*
  *	Storage of one tuple.
  */
-struct tuple
-	{
+struct tuple {
 	struct tuple *next;
 	unsigned char code;
-	int length;
+	int     length;
 	unsigned char *data;
-	};
+};
 
-struct tuple_list
-	{
+struct tuple_list {
 	struct tuple_list *next;
 	struct tuple *tuples;
-	off_t	offs;
-	int	flags;
-	};
+	off_t   offs;
+	int     flags;
+};
 
-struct tuple_info
-	{
-	char	*name;
+struct tuple_info {
+	char   *name;
 	unsigned char code;
-	unsigned char length;			/* 255 means variable length */
-	};
+	unsigned char length;		/* 255 means variable length */
+};
+
 /*
  *	Memory device descriptor.
  */
-struct dev_mem
-	{
+struct dev_mem {
 	unsigned char valid;
 	unsigned char type;
 	unsigned char speed;
 	unsigned char wps;
 	unsigned char addr;
 	unsigned char units;
-	};
+};
+
 /*
  *	One I/O structure describing a possible I/O map
  *	of the card.
  */
-struct cis_ioblk
-	{
+struct cis_ioblk {
 	struct cis_ioblk *next;
 	unsigned int addr;
 	unsigned int size;
-	};
+};
+
 /*
  *	A structure storing a memory map for the card.
  */
-struct cis_memblk
-	{
+struct cis_memblk {
 	struct cis_memblk *next;
 	unsigned int address;
 	unsigned int length;
 	unsigned int host_address;
-	};
+};
+
 /*
  *	One configuration entry for the card.
  */
-struct cis_config
-	{
+struct cis_config {
 	struct cis_config *next;
 	unsigned int pwr:1;		/* Which values are defined. */
 	unsigned int timing:1;
@@ -80,20 +76,20 @@ struct cis_config
 	unsigned char memwins;
 	struct cis_memblk *mem;
 	unsigned char misc;
-	};
+};
+
 /*
  *	Structure holding all data retrieved from the
  *	CIS block on the card.
  *	The default configuration contains interface defaults
  *	not listed in each separate configuration.
  */
-struct cis
-	{
+struct cis {
 	struct tuple_list *tlist;
-	char	manuf[MAXSTR];
-	char	vers[MAXSTR];
-	char	add_info1[MAXSTR];
-	char	add_info2[MAXSTR];
+	char    manuf[MAXSTR];
+	char    vers[MAXSTR];
+	char    add_info1[MAXSTR];
+	char    add_info2[MAXSTR];
 	unsigned char maj_v, min_v;
 	unsigned char last_config;
 	unsigned char ccrs;
@@ -102,15 +98,15 @@ struct cis
 	struct dev_mem common_mem;
 	struct cis_config *def_config;
 	struct cis_config *conf;
-	};
+};
 
-void	*xmalloc(int);
+void   *xmalloc(int);
 struct cis *readcis(int);
-void dumpcis(struct cis *);
-void freecis(struct cis *);
+void    dumpcis(struct cis *);
+void    freecis(struct cis *);
 struct tuple_list *read_tuples(int);
 struct tuple *find_tuple(struct cis *, unsigned char);
 struct tuple *find_tuple_in_list(struct tuple_list *, unsigned char);
 
 struct tuple_info *get_tuple_info(unsigned char);
-char	*tuple_name(unsigned char);
+char   *tuple_name(unsigned char);
