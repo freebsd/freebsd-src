2e5912fe126c08734bf3d160c1983e32b5faf293
joerg <joerg@FreeBSD.org>
1996-04-13 15:33:16 +0000

    Import NetBSD's termios'ed getty into a vendor branch.
    Obtained from:	NetBSD 1.1R

--- libexec/getty/pathnames.h
+++ libexec/getty/pathnames.h
@@ -30,7 +30,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)pathnames.h	8.1 (Berkeley) 6/4/93
+ *	from: @(#)pathnames.h	8.1 (Berkeley) 6/4/93
+ *	$Id: pathnames.h,v 1.3 1994/08/17 20:10:39 pk Exp $
  */
 
 #include <paths.h>
