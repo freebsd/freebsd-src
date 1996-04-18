2c801ff09de285f4618f72fcc1cc7a007a495469
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1996-04-18 13:21:26 +0000

    Remove the special-case behavior for fire actions that return DITEM_FAILURE - formerly they would simply act as an implicit "continue", but this is wrong.  If you want this behavior, you should now return with the DITEM_CONTINUE flag set.
    Also make the semantics of DITEM_RESTORE quite a bit different - rather
    than restore the screen back to pre-menu state, we restore the menu
    itself.  This is more correct for a variety of reasons when dealing with
    nested menus (whoops!).
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@15289 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- gnu/lib/libdialog/TESTS/menu2.c
+++ gnu/lib/libdialog/TESTS/menu2.c
@@ -13,7 +13,7 @@
  * the author assume any responsibility for damages incurred with
  * its use.
  *
- * $Id: menu2.c,v 1.2 1996/04/07 03:20:54 jkh Exp $
+ * $Id: menu2.c,v 1.3 1996/04/16 12:17:23 jkh Exp $
  */
 
 #include <stdio.h>
@@ -29,61 +29,37 @@ static enum { nowhere, berlin, rome, ny } where;
 static int
 _menu1_berlin_action(dialogMenuItem *self)
 {
-    WINDOW *w;
-    int st = DITEM_FAILURE;
-    
-    w = dupwin(newscr);
-    if (where == berlin) {
+    if (where == berlin)
 	dialog_mesgbox("excuse me?", "But you're already *in* Berlin!", -1, -1);
-    }
     else {
 	where = berlin;
 	dialog_mesgbox("whoosh!", "Welcome to Berlin!  Have a beer!", -1, -1);
     }
-    touchwin(w);
-    wrefresh(w);
-    delwin(w);
-    return st;
+    return DITEM_SUCCESS | DITEM_RESTORE | DITEM_CONTINUE;
 }
 
 static int
 _menu1_rome_action(dialogMenuItem *self)
 {
-    WINDOW *w;
-    int st = DITEM_FAILURE;
-    
-    w = dupwin(newscr);
-    if (where == rome) {
+    if (where == rome)
 	dialog_mesgbox("The wine must be getting to you..", "You're already in Rome!", -1, -1);
-    }
     else {
 	where = rome;
 	dialog_mesgbox("whoosh!", "Welcome to Rome!  Have a coffee!", -1, -1);
     }
-    touchwin(w);
-    wrefresh(w);
-    delwin(w);
-    return st;
+    return DITEM_SUCCESS | DITEM_RESTORE | DITEM_CONTINUE;
 }
 
 static int
 _menu1_ny_action(dialogMenuItem *self)
 {
-    WINDOW *w;
-    int st = DITEM_FAILURE;
-    
-    w = dupwin(newscr);
-    if (where == ny) {
+    if (where == ny)
 	dialog_mesgbox("Say what?", "You're already there!", -1, -1);
-    }
     else {
 	where = ny;
 	dialog_mesgbox("whoosh!", "Welcome to New York!  Now go someplace else!", -1, -1);
     }
-    touchwin(w);
-    wrefresh(w);
-    delwin(w);
-    return st;
+    return DITEM_SUCCESS | DITEM_RESTORE | DITEM_CONTINUE;
 }
 
 /* menu1 - show off the "fire" action hook */
