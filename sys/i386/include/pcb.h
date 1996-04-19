0331c668f686f6d4c3699fa3f9082c9156346984
phk <phk@FreeBSD.org>
1996-04-19 07:28:04 +0000

    savectx returns through cpu_switch in case of the child, so it must return void just like cpu_switch.  Fix prototype and usage from machdep.c

--- sys/i386/include/pcb.h
+++ sys/i386/include/pcb.h
@@ -34,7 +34,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)pcb.h	5.10 (Berkeley) 5/12/91
- *	$Id: pcb.h,v 1.11 1996/03/02 19:37:45 peter Exp $
+ *	$Id: pcb.h,v 1.12 1996/04/18 21:34:53 phk Exp $
  */
 
 #ifndef _I386_PCB_H_
@@ -79,7 +79,7 @@ struct md_coredump {
 
 #ifdef KERNEL
 extern struct pcb *curpcb;		/* our current running pcb */
-int	savectx __P((struct pcb*, int *));
+void	savectx __P((struct pcb*, int *));
 #endif
 
 #endif /* _I386_PCB_H_ */
