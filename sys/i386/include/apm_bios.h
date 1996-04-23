a2f3bfdb37df368010baf83abc5ef4c00491cb71
nate <nate@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1996-04-23 16:03:08 +0000

    Adds the APM hooks into the generic pccard kernel files.  With this code in place device drivers can now register power-down/power-up routines so that we can use common routines to power-up/power-down cards for insert/removals, suspend/resume, etc..
    Reviewed by:	phk
    Submitted by:	the 'Nomads'
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@15360 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/include/apm_bios.h
+++ sys/i386/include/apm_bios.h
@@ -12,7 +12,7 @@
  *
  * Aug, 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)
  *
- *	$Id: apm_bios.h,v 1.8 1996/03/12 05:51:35 nate Exp $
+ *	$Id: apm_bios.h,v 1.9 1996/03/13 00:41:45 nate Exp $
  */
 
 #ifndef _MACHINE_APM_BIOS_H_
@@ -135,9 +135,7 @@ struct apmhook {
 
 void apm_suspend(void);
 struct apmhook *apm_hook_establish (int apmh, struct apmhook *);
-#ifdef notused
 void apm_hook_disestablish (int apmh, struct apmhook *);
-#endif
 void apm_cpu_idle(void);
 void apm_cpu_busy(void);
 
