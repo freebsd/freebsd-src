e2cc0782460bbcd8c4c45abbaa875fa4933f57dc
Jordan K. Hubbard <jkh@FreeBSD.org>
1996-06-03 06:57:52 +0000

    Whoops - this part should not have come across.

--- sys/sys/types.h
+++ sys/sys/types.h
@@ -36,7 +36,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)types.h	8.6 (Berkeley) 2/19/95
- * $Id: types.h,v 1.11 1996/04/30 23:35:57 bde Exp $
+ * $Id: types.h,v 1.4.8.1 1996/06/03 05:12:07 jkh Exp $
  */
 
 #ifndef _SYS_TYPES_H_
@@ -73,11 +73,6 @@ typedef	_BSD_PID_T_	pid_t;		/* process id */
 typedef	int32_t		segsz_t;	/* segment size */
 typedef	int32_t		swblk_t;	/* swap offset */
 typedef	u_int32_t	uid_t;		/* user id */
-
-#ifdef KERNEL
-typedef	int		boolean_t;
-typedef	struct vm_page	*vm_page_t;
-#endif
 
 /*
  * This belongs in unistd.h, but is placed here to ensure that programs
