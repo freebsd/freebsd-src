a590cd439ed718048618337e5f0436d87221857a
Jordan K. Hubbard <jkh@FreeBSD.org>
1996-06-07 11:46:19 +0000

    Various fixes, take power.uu out as it was causing overflows.  We'll need to fetch it dynamically rather than keep it in the source tree (subject of a follow-up commit).

--- release/info.sh
+++ release/info.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-ls $1.* | wc | awk '{ print "Pieces = ",$1 }'
-for FILE in $1.*; do \
-PIECE=`echo $FILE | cut -d . -f 2` ; \
-echo -n "cksum.$PIECE = "; \
-cksum $FILE | awk ' { print $1,$2 } '
+# $ANA: info.sh,v 1.3 1996/06/04 16:25:30 wollman Exp $
+ls $1.[a-z][a-z] | wc | awk '{ print "Pieces = ",$1 }'
+for FILE in $1.[a-z][a-z]; do
+       PIECE=`echo $FILE | cut -d . -f 2`
+       echo -n "cksum.$PIECE = "
+       cksum $FILE | awk ' { print $1,$2 } '
 done
