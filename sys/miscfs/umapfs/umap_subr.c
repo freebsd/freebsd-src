2a54936c68693838b2aec53a36d5ced816f79e9b
dg <dg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1996-06-12 03:37:57 +0000

    Moved the fsnode MALLOC to before the call to getnewvnode() so that the process won't possibly block before filling in the fsnode pointer (v_data) which might be dereferenced during a sync since the vnode is put on the mnt_vnodelist by getnewvnode.
    Pointed out by Matt Day <mday@artisoft.com>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@16312 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/miscfs/umapfs/umap_subr.c
+++ sys/miscfs/umapfs/umap_subr.c
@@ -35,7 +35,7 @@
  *
  *	@(#)umap_subr.c	8.6 (Berkeley) 1/26/94
  *
- * $Id: umap_subr.c,v 1.5 1995/12/03 14:38:57 bde Exp $
+ * $Id: umap_subr.c,v 1.6 1995/12/03 14:54:39 bde Exp $
  */
 
 #include <sys/param.h>
@@ -222,13 +222,23 @@ umap_node_alloc(mp, lowervp, vpp)
 	struct vnode *othervp, *vp;
 	int error;
 
-	error = getnewvnode(VT_UMAP, mp, umap_vnodeop_p, vpp);
-	if (error)
-		return (error);
-	vp = *vpp;
+	/* XXX This routine probably needs a node_alloc lock */
 
+	/*
+	 * Do the MALLOC before the getnewvnode since doing so afterward
+	 * might cause a bogus v_data pointer to get dereferenced
+	 * elsewhere if MALLOC should block.
+	 */
 	MALLOC(xp, struct umap_node *, sizeof(struct umap_node),
 	    M_TEMP, M_WAITOK);
+
+	error = getnewvnode(VT_UMAP, mp, umap_vnodeop_p, vpp);
+	if (error) {
+		FREE(xp, M_TEMP);
+		return (error);
+	}
+	vp = *vpp;
+
 	vp->v_type = lowervp->v_type;
 	xp->umap_vnode = vp;
 	vp->v_data = xp;
