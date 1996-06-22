def73a533807d24387229f9245bb0e63fcb128a9
joerg <joerg@FreeBSD.org>
1996-06-22 13:03:56 +0000

    Fix the major number of the `od' driver.
    Submitted by:	akiyama@kme.mei.co.jp (Shunsuke Akiyama)

--- sys/i386/i386/swapgeneric.c
+++ sys/i386/i386/swapgeneric.c
@@ -34,7 +34,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)swapgeneric.c	5.5 (Berkeley) 5/9/91
- *	$Id: swapgeneric.c,v 1.11 1995/08/19 15:59:35 joerg Exp $
+ *	$Id: swapgeneric.c,v 1.10.4.1 1995/09/30 13:50:10 davidg Exp $
  */
 
 #include <sys/param.h>
@@ -109,7 +109,7 @@ struct	genericconf {
 	{ "matcd",	makedev(17,0x00000000),	},
 #endif
 #if NOD > 0
-	{ "od",		makedev(19,0x00000000),	},
+	{ "od",		makedev(20,0x00000000),	},
 #endif
 	{ 0 },
 };
