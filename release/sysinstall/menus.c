446321c613747f69ff2bdbdd7c36ea5b19717d28
Jordan K. Hubbard <jkh@FreeBSD.org>
1996-06-27 07:03:44 +0000

    Add 3rd South Africa mirror.

--- release/sysinstall/menus.c
+++ release/sysinstall/menus.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: menus.c,v 1.71 1996/06/17 19:33:27 jkh Exp $
+ * $Id: menus.c,v 1.72 1996/06/26 09:09:30 jkh Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -520,6 +520,8 @@ guaranteed to carry the full range of possible distributions.",
     VAR_FTP_PATH "=ftp://ftp.za.freebsd.org/pub/FreeBSD/" },
   { "South Africa #2",	"ftp2.za.freebsd.org", NULL, dmenuSetVariable, NULL,
     VAR_FTP_PATH "=ftp://ftp2.za.freebsd.org/pub/FreeBSD/" },
+  { "South Africa #3",	"ftp3.za.freebsd.org", NULL, dmenuSetVariable, NULL,
+    VAR_FTP_PATH "=ftp://ftp3.za.freebsd.org/pub/FreeBSD/" },
   { "Sweden",		"ftp.luth.se", NULL, dmenuSetVariable, NULL,
     VAR_FTP_PATH "=ftp://ftp.luth.se/pub/FreeBSD/" },
   { "Taiwan",		"ftp.tw.freebsd.org", NULL, dmenuSetVariable, NULL,
