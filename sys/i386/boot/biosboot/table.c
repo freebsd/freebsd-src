1cc273db90ed49ba889c14669c8fc581679d3030
Bruce Evans <bde@FreeBSD.org>
1996-07-12 05:25:47 +0000

    Moved the definition of dflt_name to the correct file (table.c is only for explicitly initialized data) and made it conditional on NAMEBLOCK.

--- sys/i386/boot/biosboot/table.c
+++ sys/i386/boot/biosboot/table.c
@@ -24,7 +24,7 @@
  * the rights to redistribute these changes.
  *
  *	from: Mach, Revision 2.2  92/04/04  11:36:43  rpd
- *	$Id: table.c,v 1.10 1996/04/07 14:28:05 bde Exp $
+ *	$Id: table.c,v 1.11 1996/07/05 19:55:05 julian Exp $
  */
 
 /*
@@ -140,4 +140,3 @@ char *devs[] = { "wd", "dk", "fd", "wt", "sd", 0 };
 char dflname[] = "/kernel";
 char *name = dflname;
 unsigned long tw_chars = 0x5C2D2F7C;	/* "\-/|" */
-char *dflt_name;
