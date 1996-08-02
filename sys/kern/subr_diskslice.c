f2d0afb82e443578a2e1adfac2aeaa2ffc501601
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1996-08-02 06:14:25 +0000

    dscheck() was returning without setting bp->b_error when given a negative block number.. (assuming Debugger() returned).  The disk drivers assume that dscheck() sets both error markers (bp->b_error and set B_ERROR in bp->b_flags) if it fails.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@17391 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/subr_diskslice.c
+++ sys/kern/subr_diskslice.c
@@ -43,7 +43,7 @@
  *	from: wd.c,v 1.55 1994/10/22 01:57:12 phk Exp $
  *	from: @(#)ufs_disksubr.c	7.16 (Berkeley) 5/4/91
  *	from: ufs_disksubr.c,v 1.8 1994/06/07 01:21:39 phk Exp $
- *	$Id: subr_diskslice.c,v 1.26 1996/06/12 05:07:31 gpalmer Exp $
+ *	$Id: subr_diskslice.c,v 1.27 1996/06/17 14:43:45 bde Exp $
  */
 
 #include <sys/param.h>
@@ -121,6 +121,7 @@ dscheck(bp, ssp)
 
 	if (bp->b_blkno < 0) {
 		Debugger("Slice code got negative blocknumber");
+		bp->b_error = EINVAL;
 		goto bad;
 	}
 
