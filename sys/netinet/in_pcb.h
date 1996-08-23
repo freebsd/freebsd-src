321a2846251910690d28419c9870fafadef8d36d
Poul-Henning Kamp <phk@FreeBSD.org>
1996-08-23 18:59:07 +0000

    Mark sockets where the kernel chose the port# for. This can be used by netstat to behave more intelligently.

--- sys/netinet/in_pcb.h
+++ sys/netinet/in_pcb.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)in_pcb.h	8.1 (Berkeley) 6/10/93
- * $Id: in_pcb.h,v 1.10 1995/12/05 21:26:34 bde Exp $
+ * $Id: in_pcb.h,v 1.11 1996/02/22 21:32:22 peter Exp $
  */
 
 #ifndef _NETINET_IN_PCB_H_
@@ -80,6 +80,7 @@ struct inpcbinfo {
 #define	INP_HDRINCL		0x08	/* user supplies entire IP header */
 #define	INP_HIGHPORT		0x10	/* user wants "high" port binding */
 #define	INP_LOWPORT		0x20	/* user wants "low" port binding */
+#define	INP_ANONPORT		0x40	/* port chosen for user */
 
 #define	INPLOOKUP_WILDCARD	1
 
