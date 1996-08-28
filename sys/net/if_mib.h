5d0a1927463d118793aa92e4b17aaf58aabf8fd3
wollman <wollman@FreeBSD.org>
1996-08-28 18:32:19 +0000

    Add some padding to struct ifmibdata and move the `struct ifdata' to the end of that sstructure to make evolution easier.
    Add definitions for the 802.3/Ethernet MIB.  To implement this, simply
    add a `struct ifmib_iso_8802_3' somewhere in your interface's softc,
    point if_linkmib to it, set if_linkmiblen, and fill in the statistics
    with appropriate values.  (I didn't want to create Yet Another Ethernet-
    related header file, otherwise this would have been separated out.)

--- sys/net/if_mib.h
+++ sys/net/if_mib.h
@@ -26,7 +26,7 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id$
+ *	$Id: if_mib.h,v 1.1 1996/07/30 19:17:01 wollman Exp $
  */
 
 #ifndef _NET_IF_MIB_H
@@ -36,10 +36,11 @@ struct ifmibdata {
 	char	ifmd_name[IFNAMSIZ]; /* name of interface */
 	int	ifmd_pcount;	/* number of promiscuous listeners */
 	int	ifmd_flags;	/* interface flags */
-	struct	if_data ifmd_data; /* generic information and statistics */
 	int	ifmd_snd_len;	/* instantaneous length of send queue */
 	int	ifmd_snd_maxlen; /* maximum length of send queue */
 	int	ifmd_snd_drops;	/* number of drops in send queue */
+	int	ifmd_filler[4];	/* for future expansion */
+	struct	if_data ifmd_data; /* generic information and statistics */
 };
 
 /*
@@ -74,5 +75,96 @@ struct ifmibdata {
  *	<iftype> and a particular protocol family (e.g., net.link.ether.inet
  *	instruments ARP).  This does not really leave room for anything else
  *	that needs to have a well-known number.
+ */
+
+/*
+ * Link-specific MIB structures for various link types.
+ */
+
+/* For IFT_ETHER, IFT_ISO88023, and IFT_STARLAN, as used by RFC 1650 */
+struct ifmib_iso_8802_3 {
+	u_int32_t	dot3StatsAlignmentErrors;
+	u_int32_t	dot3StatsFCSErrors;
+	u_int32_t	dot3StatsSingleCollisionFrames;
+	u_int32_t	dot3StatsMultipleCollisionFrames;
+	u_int32_t	dot3StatsSQETestErrors;
+	u_int32_t	dot3StatsDeferredTransmissions;
+	u_int32_t	dot3StatsLateCollisions;
+	u_int32_t	dot3StatsExcessiveCollisions;
+	u_int32_t	dot3StatsInternalMacTransmitErrors;
+	u_int32_t	dot3StatsCarrierSenseErrors;
+	u_int32_t	dot3StatsFrameTooLongs;
+	u_int32_t	dot3StatsInternalMacReceiveErrors;
+	u_int32_t	dot3StatsEtherChipSet;
+	/* Matt Thomas wants this one, not included in RFC 1650: */
+	u_int32_t	dot3StatsMissedFrames;
+
+	u_int32_t	dot3StatsCollFrequencies[16]; /* NB: index origin */
+
+	u_int32_t	dot3Compliance;
+#define	DOT3COMPLIANCE_STATS	1
+#define	DOT3COMPLIANCE_COLLS	2
+};
+
+/*
+ * Chipset identifiers are normally part of the vendor's enterprise MIB.
+ * However, we don't want to be trying to represent arbitrary-length
+ * OBJECT IDENTIFIERs here (ick!), and the right value is not necessarily
+ * obvious to the driver implementor.  So, we define our own identification
+ * mechanism here, and let the agent writer deal with the translation.
+ */
+#define	DOT3CHIPSET_VENDOR(x)	((x) >> 16)
+#define	DOT3CHIPSET_PART(x)	((x) & 0xffff)
+#define	DOT3CHIPSET(v,p)	(((v) << 16) + ((p) & 0xffff))
+
+/* Driver writers!  Add your vendors here! */
+enum dot3Vendors {
+	dot3VendorAMD = 1,
+	dot3VendorIntel = 2,
+	dot3VendorNational = 4,
+	dot3VendorFujitsu = 5,
+	dot3VendorDigital = 6,
+	dot3VendorWesternDigital = 7,
+};
+
+/* Driver writers!  Add your chipsets here! */
+enum {
+	dot3ChipSetAMD7990 = 1,
+	dot3ChipSetAMD79900 = 2,
+	dot3ChipSetAMD79C940 = 3
+};
+
+enum {
+	dot3ChipSetIntel82586 = 1,
+	dot3ChipSetIntel82596 = 2,
+	dot3ChipSetIntel82557 = 3
+};
+
+enum {
+	dot3ChipSetNational8390 = 1,
+	dot3ChipSetNationalSonic = 2
+};
+
+enum {
+	dot3ChipSetFujitsu86950 = 1
+};
+
+enum {
+	dot3ChipSetDigitalDC21040 = 1,
+	dot3ChipSetDigitalDC21140 = 2,
+	dot3ChipSetDigitalDC21041 = 3,
+	dot3ChipSetDigitalDC21140A = 4,
+	dot3ChipSetDigitalDC21142 = 5
+};
+
+enum {
+	dot3ChipSetWesternDigital83C690 = 1,
+	dot3ChipSetWesternDigital83C790 = 2
+};
+/* END of Ethernet-link MIB stuff */
+
+/*
+ * Put other types of interface MIBs here, or in interface-specific
+ * header files if convenient ones already exist.
  */
 #endif /* _NET_IF_MIB_H */
