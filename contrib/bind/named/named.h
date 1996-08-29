2d3cf9fcaf1ca2528c5fe3ba683d1f6c1268dc41
peter <peter@FreeBSD.org>
1996-08-29 19:20:22 +0000

    Take #2.  Import bind-4.9.4-P1 into the intended directory! This has most of the non-essential stuff removed (ie: what is not built)
    bmake glue to follow.

---
+++ contrib/bind/named/named.h
@@ -0,0 +1,19 @@
+/* named.h - include the local definitions in the right order
+ * vix 28aug93 [original]
+ *
+ * $Id: named.h,v 8.1 1994/12/15 06:24:14 vixie Exp $
+ */
+
+#include "../conf/portability.h"
+#include "../conf/options.h"
+
+#include "pathnames.h"
+
+#include "ns_defs.h"
+#include "db_defs.h"
+
+#include "ns_glob.h"
+#include "db_glob.h"
+
+#include "ns_func.h"
+#include "db_func.h"
