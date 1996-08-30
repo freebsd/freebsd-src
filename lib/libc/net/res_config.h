2e0e3adbfdd71837c1010680bb5ec5718293f1da
peter <peter@FreeBSD.org>
1996-08-30 21:13:42 +0000

    Sigh, back out the last bright idea I had here about compiling the res_* routines from contrib/bind directly.  There were too many problems, including having to add -DUSE_OPTIONS_H to the entire libc source in order for the contrib code to pick up it's options, and so on.
    Instead, I've merged the changes, libc is now self contained again.

--- lib/libc/net/res_config.h
+++ lib/libc/net/res_config.h
@@ -4,5 +4,6 @@
 #define	RFC1535		/* comply with RFC1535 (STRONGLY reccomended by vixie)*/
 #undef	USELOOPBACK	/* res_init() bind to localhost */
 #undef	SUNSECURITY	/* verify gethostbyaddr() calls - WE DONT NEED IT  */
-#undef	ALLOW_UPDATES	/* compile support for update REQUESTS - harmless */
+#define	ALLOW_UPDATES	/* compile recognition for update REQUESTS - harmless */
 #define MULTI_PTRS_ARE_ALIASES 1 /* fold multiple PTR records into aliases */
+#define	CHECK_SRVR_ADDR 1 /* confirm that the server requested sent the reply */
