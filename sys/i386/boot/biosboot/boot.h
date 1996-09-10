fec607364516b9efdc246cc2d91cb38b09d39e67
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1996-09-10 21:18:40 +0000

    Rather than adding more gunk here, clean some of it up: 	devread() had a bogus interface, cleaned up. 	Bread() did an unneeded bcopy(), don't. Saves 80 bytes and some time.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@18228 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/boot/biosboot/boot.h
+++ sys/i386/boot/biosboot/boot.h
@@ -24,7 +24,7 @@
  * the rights to redistribute these changes.
  *
  *	from: Mach, Revision 2.2  92/04/04  11:35:03  rpd
- *	$Id: boot.h,v 1.10 1995/05/30 07:58:29 rgrimes Exp $
+ *	$Id: boot.h,v 1.11 1995/06/25 14:02:52 joerg Exp $
  */
 
 #include <sys/types.h>
@@ -69,8 +69,8 @@ void boot2(void);
 
 /* disk.c */
 int devopen(void);
-void devread(void);
-void Bread(int dosdev, int sector);
+void devread(char *iodest, int sector, int cnt);
+char * Bread(int dosdev, int sector);
 int badsect(int dosdev, int sector);
 
 /* io.c */
