6c6c2f34c21bfbf59826275ad204ccc77164237c
bde <bde@FreeBSD.org>
1996-09-14 07:38:14 +0000

    Removed declarations of recently deleted variables and cleaned up #includes.

--- sys/i386/boot/biosboot/boot.h
+++ sys/i386/boot/biosboot/boot.h
@@ -24,21 +24,21 @@
  * the rights to redistribute these changes.
  *
  *	from: Mach, Revision 2.2  92/04/04  11:35:03  rpd
- *	$Id: boot.h,v 1.11 1995/06/25 14:02:52 joerg Exp $
+ *	$Id: boot.h,v 1.12 1996/09/10 21:18:38 phk Exp $
  */
 
-#include <sys/types.h>
 #include <sys/param.h>
 #include <sys/time.h>
-#include <ufs/ufs/quota.h>
+
 #include <ufs/ffs/fs.h>
+#include <ufs/ufs/quota.h>
 #include <ufs/ufs/inode.h>
 
-extern char *devs[], *iodest;
-extern char *name, dflname[];
+extern char *devs[];
+extern char *name;
 extern struct fs *fs;
 extern struct inode inode;
-extern int dosdev, unit, slice, part, maj, boff, poff, bnum, cnt;
+extern int dosdev, unit, slice, part, maj, boff, poff;
 extern unsigned long tw_chars;
 extern int loadflags;
 extern struct disklabel disklabel;
