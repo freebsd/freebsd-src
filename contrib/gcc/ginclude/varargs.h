b69108b2831cdc92b5c6dfe373ebe07b1667caa5
peter <peter@FreeBSD.org>
1996-09-18 06:31:42 +0000

    Patches originally obtained from Bruce Evans <bde@freebsd.org> to make gcc "do the right thing" when running standalone on FreeBSD

--- contrib/gcc/ginclude/varargs.h
+++ contrib/gcc/ginclude/varargs.h
@@ -78,10 +78,20 @@
 
 #ifndef __GNUC_VA_LIST
 #define __GNUC_VA_LIST
+#if defined (__FreeBSD__)
+/* This is the correct way to handle all BSD NET2 and BSD 4.4 systems.  */
+#include <machine/ansi.h>
+#ifdef _BSD_VA_LIST_
+typedef _BSD_VA_LIST_ __gnuc_va_list;
+#else
+typedef _VA_LIST_ __gnuc_va_list;
+#endif
+#else
 #if defined(__svr4__) || defined(_AIX) || defined(_M_UNIX)
 typedef char *__gnuc_va_list;
 #else
 typedef void *__gnuc_va_list;
+#endif
 #endif
 #endif
 
@@ -185,6 +195,11 @@ typedef __gnuc_va_list va_list;
 
 /* The next BSD release (if there is one) wants this symbol to be
    undefined instead of _VA_LIST_.  */
+#if 0
+/* BSD 4.4 actually spells the name _BSD_VA_LIST_ and requires it to be
+ * defined and usable in place of va_list when the latter name is not
+ * allowed (e.g., in stdio.h - see ginclude/stdarg.h).  */
 #ifdef _BSD_VA_LIST
 #undef _BSD_VA_LIST
+#endif
 #endif
