97d25bbf44d06032b78810ad024c863534d4de42
Peter Wemm <peter@FreeBSD.org>
1996-09-18 06:31:42 +0000

    Patches originally obtained from Bruce Evans <bde@freebsd.org> to make gcc "do the right thing" when running standalone on FreeBSD

--- contrib/gcc/ginclude/stddef.h
+++ contrib/gcc/ginclude/stddef.h
@@ -257,7 +257,7 @@ typedef __WCHAR_TYPE__ wchar_t;
 
 /*  In 4.3bsd-net2, leave these undefined to indicate that size_t, etc.
     are already defined.  */
-#ifdef _ANSI_H_
+#if defined(_ANSI_H_) || defined(_MACHINE_ANSI_H_)
 /*  The references to _GCC_PTRDIFF_T_, _GCC_SIZE_T_, and _GCC_WCHAR_T_
     are probably typos and should be removed before 2.8 is released.  */
 #ifdef _GCC_PTRDIFF_T_
@@ -285,7 +285,7 @@ typedef __WCHAR_TYPE__ wchar_t;
 #undef _WCHAR_T_
 #undef _BSD_WCHAR_T_
 #endif
-#endif /* _ANSI_H_ */
+#endif /* defined(_ANSI_H_) || defined(_MACHINE_ANSI_H_) */
 
 #endif /* __sys_stdtypes_h */
 
