c4b4499526a4706ae4acceb78b13d744d0d46a90
steve <steve@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1996-10-06 16:45:32 +0000

    This commit was generated by cvs2svn to compensate for changes in r18764, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@18765 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/make/lst.lib/lstIsEmpty.c
+++ usr.bin/make/lst.lib/lstIsEmpty.c
@@ -1,3 +1,5 @@
+/*	$NetBSD: lstIsEmpty.c,v 1.4 1995/06/14 15:21:27 christos Exp $	*/
+
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -35,7 +37,11 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)lstIsEmpty.c	8.2 (Berkeley) 4/28/95";
+#if 0
+static char sccsid[] = "@(#)lstIsEmpty.c	8.1 (Berkeley) 6/6/93";
+#else
+static char rcsid[] = "$NetBSD: lstIsEmpty.c,v 1.4 1995/06/14 15:21:27 christos Exp $";
+#endif
 #endif /* not lint */
 
 /*-
