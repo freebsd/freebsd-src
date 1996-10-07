bbdfd53f015bd538d412bc4a83d345f0c2ff8909
pst <pst@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1996-10-07 04:32:42 +0000

    Increase robustness of FreeBSD against high-rate connection attempt denial of service attacks.
    Reviewed by:	bde,wollman,olah
    Inspired by:	vjs@sgi.com
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@18787 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/sysctl.h
+++ sys/sys/sysctl.h
@@ -34,7 +34,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)sysctl.h	8.1 (Berkeley) 6/2/93
- * $Id: sysctl.h,v 1.44 1996/09/10 23:31:13 bde Exp $
+ * $Id: sysctl.h,v 1.45 1996/09/19 00:54:29 pst Exp $
  */
 
 #ifndef _SYS_SYSCTL_H_
@@ -223,8 +223,7 @@ int sysctl_handle_opaque SYSCTL_HANDLER_ARGS;
 #define	KERN_MAXSOCKBUF		31	/* int: max size of a socket buffer */
 #define	KERN_PS_STRINGS		32	/* int: address of PS_STRINGS */
 #define	KERN_USRSTACK		33	/* int: address of USRSTACK */
-#define	KERN_SOMINQUEUE		34	/* int: override socket listen() */
-#define KERN_MAXID		35      /* number of valid kern ids */
+#define KERN_MAXID		34      /* number of valid kern ids */
 
 #define CTL_KERN_NAMES { \
 	{ 0, 0 }, \
@@ -261,7 +260,6 @@ int sysctl_handle_opaque SYSCTL_HANDLER_ARGS;
 	{ "maxsockbuf", CTLTYPE_INT }, \
 	{ "ps_strings", CTLTYPE_INT }, \
 	{ "usrstack", CTLTYPE_INT }, \
-	{ "sominqueue", CTLTYPE_INT }, \
 }
 
 /*
