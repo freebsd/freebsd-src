272937b07fd5cb2d521fb8d755f784c7cc5a894b
Bruce Evans <bde@FreeBSD.org>
1996-10-09 18:41:40 +0000

    Oops, this wasn't supposed to have -Wcast-qual yet.
    Also disabled -Wunused.  It caused too many warnings even for me.
    The sign mismatch warnings should be fixed first.  They are more
    important and harder to disable (they are controlled by -W, which
    controls too many things).

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -1,11 +1,11 @@
-#	$Id$
+#	$Id: bsd.kern.mk,v 1.1 1996/10/08 22:10:49 bde Exp $
 
 #
 # Warning flags for compiling the kernel and components of the kernel.
 #
 CWARNFLAGS?=	-W -Wreturn-type -Wcomment -Wredundant-decls -Wimplicit \
 		-Wnested-externs -Wstrict-prototypes -Wmissing-prototypes \
-		-Winline -Wunused -Wpointer-arith -Wcast-qual
+		-Winline -Wpointer-arith # -Wunused -Wcast-qual
 #
 # The following flags are next up for working on:
 #	-Wformat -Wall
