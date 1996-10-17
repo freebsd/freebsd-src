53410b6ff1ac5ed2ff0aa1a7fd0858dfbecf0e80
jkh <jkh@FreeBSD.org>
1996-10-17 18:42:33 +0000

    Bring back netns so that Netcon can take over support for it, as agreed. This was actually easier than I thought - CVS is smart enough to handle "resurrections" now. :-)

---
+++ sys/netns/idp_var.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 1984, 1985, 1986, 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)idp_var.h	8.1 (Berkeley) 6/10/93
+ * $Id: idp_var.h,v 1.3 1994/08/21 06:22:06 paul Exp $
+ */
+
+#ifndef _NETNS_IDP_VAR_H_
+#define _NETNS_IDP_VAR_H_
+
+/*
+ * IDP Kernel Structures and Variables
+ */
+struct	idpstat {
+	int	idps_badsum;		/* checksum bad */
+	int	idps_tooshort;		/* packet too short */
+	int	idps_toosmall;		/* not enough data */
+	int	idps_badhlen;		/* ip header length < data size */
+	int	idps_badlen;		/* ip length < ip header length */
+};
+
+#ifdef KERNEL
+struct	idpstat	idpstat;
+#endif
+
+#endif
