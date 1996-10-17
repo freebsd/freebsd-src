802f868bd54bc93434f2e2e1d8326e6292926a85
ache <ache@FreeBSD.org>
1996-10-17 17:06:04 +0000

    Don't ever ask for password if it is impossible to confirm it It happens if 1) regular passwords not allowed, 2) skey database not activated for given user. Under some rare circumstanes skey_challenge can return empty diagnostic or even previous buffer, fix it.

--- libexec/ftpd/skey-stuff.c
+++ libexec/ftpd/skey-stuff.c
@@ -1,6 +1,6 @@
 /* Author: Wietse Venema, Eindhoven University of Technology. 
  *
- *	$Id$
+ *	$Id: skey-stuff.c,v 1.3 1996/09/22 21:53:34 wosch Exp $
  */
 
 #include <stdio.h>
@@ -10,18 +10,20 @@
 
 /* skey_challenge - additional password prompt stuff */
 
-char   *skey_challenge(name, pwd, pwok)
+char   *skey_challenge(name, pwd, pwok, sflag)
 char   *name;
 struct passwd *pwd;
 int     pwok;
+int    *sflag;
 {
     static char buf[128];
     struct skey skey;
+    char *username = pwd ? pwd->pw_name : ":";
 
     /* Display s/key challenge where appropriate. */
 
-    if (pwd == 0 || skeychallenge(&skey, pwd->pw_name, buf) != 0)
-	sprintf(buf, "%s required for %s.",
-		pwok ? "Password" : "S/Key password", name);
+    *sflag = skeychallenge(&skey, username, buf);
+    sprintf(buf, "%s required for %s.",
+	pwok ? "Password" : "S/Key password", name);
     return (buf);
 }
