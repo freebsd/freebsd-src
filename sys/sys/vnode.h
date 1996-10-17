f1dbda5d8a28f78e8ff4c58cd19b512bd930c741
jkh <jkh@FreeBSD.org>
1996-10-17 17:12:04 +0000

    Some very small changes to support Netcon's TFS filesystem. These patches were formerly applied by the Netcon installer before rebuilding your kernel.

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)vnode.h	8.7 (Berkeley) 2/4/94
- * $Id: vnode.h,v 1.34 1996/08/21 21:56:01 dyson Exp $
+ * $Id: vnode.h,v 1.35 1996/10/15 19:22:46 bde Exp $
  */
 
 #ifndef _SYS_VNODE_H_
@@ -58,7 +58,7 @@ enum vtype	{ VNON, VREG, VDIR, VBLK, VCHR, VLNK, VSOCK, VFIFO, VBAD };
 enum vtagtype	{
 	VT_NON, VT_UFS, VT_NFS, VT_MFS, VT_PC, VT_LFS, VT_LOFS, VT_FDESC,
 	VT_PORTAL, VT_NULL, VT_UMAP, VT_KERNFS, VT_PROCFS, VT_AFS, VT_ISOFS,
-	VT_UNION, VT_MSDOSFS, VT_DEVFS
+	VT_UNION, VT_MSDOSFS, VT_DEVFS, VT_TFS
 };
 
 /*
