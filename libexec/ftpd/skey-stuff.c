43658eac6462232f0d9112fc9cddbf97456c05ca
Andrey A. Chernov <ache@FreeBSD.org>
1996-10-18 17:09:26 +0000

    Implement alternative strategy if it is impossible to confirm password: ask for it, but don't tell that S/key password required. It looks like non-s/key system from outside.
    Additionally tell that s/key required when it is so for normal case

--- libexec/ftpd/skey-stuff.c
+++ libexec/ftpd/skey-stuff.c
@@ -1,6 +1,6 @@
 /* Author: Wietse Venema, Eindhoven University of Technology. 
  *
- *	$Id: skey-stuff.c,v 1.4 1996/10/17 17:06:04 ache Exp $
+ *	$Id: skey-stuff.c,v 1.3 1996/09/22 21:53:34 wosch Exp $
  */
 
 #include <stdio.h>
@@ -10,21 +10,19 @@
 
 /* skey_challenge - additional password prompt stuff */
 
-char   *skey_challenge(name, pwd, pwok, sflag)
+char   *skey_challenge(name, pwd, pwok)
 char   *name;
 struct passwd *pwd;
-int     pwok;
-int    *sflag;
+int    pwok;
 {
     static char buf[128];
     struct skey skey;
-    char *username = pwd ? pwd->pw_name : ":";
 
     /* Display s/key challenge where appropriate. */
 
-    *sflag = skeychallenge(&skey, username, buf);
-    if (*sflag)
-	sprintf(buf, "%s required for %s.",
-	    pwok ? "Password" : "S/Key password", name);
+    if (pwd == NULL || skeychallenge(&skey, pwd->pw_name, buf))
+	sprintf(buf, "Password required for %s.", name);
+    else if (!pwok)
+	strcat(buf, " (s/key required)");
     return (buf);
 }
