5d35538545a3506a79a01c23fd00fcd6464d7620
jkh <jkh@FreeBSD.org>
1996-11-11 14:17:35 +0000

    Mr. Snake is 2.2-RELEASE in this branch.

--- lkm/syscons/snake/snake_saver.c
+++ lkm/syscons/snake/snake_saver.c
@@ -25,7 +25,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *	$Id: snake_saver.c,v 1.9 1995/11/29 20:10:31 wollman Exp $
+ *	$Id: snake_saver.c,v 1.10 1996/07/01 20:28:54 bde Exp $
  */
 
 #include <sys/param.h>
@@ -48,7 +48,7 @@ void (*old_saver)(int blank);
 static void
 snake_saver(int blank)
 {
-	const char	saves[] = {"FreeBSD-2.2-CURRENT"};
+	const char	saves[] = {"FreeBSD-2.2"};
 	static u_char	*savs[sizeof(saves)-1];
 	static int	dirx, diry;
 	int		f;
