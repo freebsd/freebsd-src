f014b7e69bbd6737e73d43099fdb4ba32e99b5ad
Bill Fenner <fenner@FreeBSD.org>
1996-11-11 03:50:15 +0000

    Update to the unreleased mrouted 3.8a .  This includes a minor endian-ness fix, Router Alert options on IGMP messages, and a new keyword, "advert_metric", for fine-tuning tunnel metrics.
    This also includes a new mtrace, which is also unreleased but
    builds significantly on the experiences of users' troubles with
    using and understanding mtrace in release 3.8 .
    
    (unreleased does not, of course, mean untested!)
    
    This is a candidate for both 2.2 and 2.1.6 .

--- usr.sbin/mrouted/config.c
+++ usr.sbin/mrouted/config.c
@@ -7,7 +7,7 @@
  * Leland Stanford Junior University.
  *
  *
- * $Id: config.c,v 3.8 1995/11/29 22:36:57 fenner Rel $
+ * $Id: config.c,v 3.8.1.1 1996/08/09 22:46:16 fenner Exp $
  */
 
 
@@ -116,6 +116,7 @@ config_vifs_from_kernel()
 	v  = &uvifs[numvifs];
 	v->uv_flags       = 0;
 	v->uv_metric      = DEFAULT_METRIC;
+	v->uv_admetric	  = 0;
 	v->uv_rate_limit  = DEFAULT_PHY_RATE_LIMIT;
 	v->uv_threshold   = DEFAULT_THRESHOLD;
 	v->uv_lcl_addr    = addr;
