be446406be244463f43bf8db262174ba503c5fa7
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1996-11-12 23:09:15 +0000

    Increase precision of duration to milliseconds. Some heuristics to avoid overflow in calculation attempted.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@19694 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- bin/dd/conv.c
+++ bin/dd/conv.c
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id$
+ *	$Id: conv.c,v 1.3 1994/09/24 02:54:44 davidg Exp $
  */
 
 #ifndef lint
@@ -45,6 +45,7 @@ static char sccsid[] = "@(#)conv.c	8.3 (Berkeley) 4/2/94";
 
 #include <err.h>
 #include <string.h>
+#include <sys/time.h>
 
 #include "dd.h"
 #include "extern.h"
