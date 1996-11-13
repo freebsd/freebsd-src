afd4757fab3ed66a33c9d4a8826eb623b0571391
Jordan K. Hubbard <jkh@FreeBSD.org>
1996-11-13 12:15:26 +0000

    Actually, it's over 600 packages now.

--- release/sysinstall/install.c
+++ release/sysinstall/install.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: install.c,v 1.134.2.6 1996/11/09 16:34:21 joerg Exp $
+ * $Id: install.c,v 1.134.2.7 1996/11/12 18:41:29 jkh Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -491,7 +491,7 @@ installNovice(dialogMenuItem *self)
     }
 
     dialog_clear_norefresh();
-    if (!msgYesNo("The FreeBSD package collection is a collection of over 550 ready-to-run\n"
+    if (!msgYesNo("The FreeBSD package collection is a collection of over 600 ready-to-run\n"
 		  "applications, from text editors to games to WEB servers.  Would you like\n"
 		  "to browse the collection now?"))
 	configPackages(self);
