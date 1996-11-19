037def9693b4a8fd76b4400b91eccf7ab68c602f
Garrett Wollman <wollman@FreeBSD.org>
1996-11-19 18:09:41 +0000

    Completely re-vamped `tzsetup':
    - It no longer attempts to fiddle wall-vs-UTC-in-RTC.  The results
      were just confusing most of the time.
    
    - The program no longer contains a pre-compiled list of timezones
      (compiled by groveling through the tzdata source files for comments
      starting with `ZONE-DESCR').  Now it uses the new `zone.tab' file
      supplied with current versions of the timezone data files, to determine
      the list at run time.  (It also requires the ISO 3166 table  I
      committed some months ago.)
    
    AS A RESULT, this program will NOT work until the new timezone data files
    are committed (should happen sometime soon).

---
+++ usr.sbin/tzsetup/paths.h
@@ -0,0 +1,4 @@
+#define	_PATH_ZONETAB	"/usr/share/zoneinfo/zone.tab"
+#define	_PATH_ISO3166	"/usr/share/misc/iso3166"
+#define	_PATH_ZONEINFO	"/usr/share/zoneinfo"
+#define	_PATH_LOCALTIME	"/etc/localtime"
