2596f6cfcadce74d72c67c1edafafe2a4a0214ce
nate <nate@FreeBSD.org>
1996-12-03 21:56:15 +0000

    Added support for '-T' and '-B' (text and binary) file recognition by peeking inside of Chris Torek's stdio library internals.  This is similar to the code used for other systems, but didn't work on CT's new implementation.
    Submitted by:	Gary Kline <kline@tera.com>

--- gnu/usr.bin/perl/perl/tdoio.c
+++ gnu/usr.bin/perl/perl/tdoio.c
@@ -1,4 +1,4 @@
-/* $RCSfile: tdoio.c,v $$Revision: 1.1.1.1 $$Date: 1994/09/10 06:27:36 $
+/* $RCSfile: tdoio.c,v $$Revision: 1.2 $$Date: 1995/05/30 05:03:23 $
  *
  *    Copyright (c) 1991, Larry Wall
  *
@@ -6,6 +6,9 @@
  *    License or the Artistic License, as specified in the README file.
  *
  * $Log: tdoio.c,v $
+ * Revision 1.2  1995/05/30 05:03:23  rgrimes
+ * Remove trailing whitespace.
+ *
  * Revision 1.1.1.1  1994/09/10  06:27:36  gclarkii
  * Initial import of Perl 4.046 bmaked
  *
@@ -1214,7 +1217,25 @@ STR *str;
 	    len = stio->ifp->_cnt + (stio->ifp->_ptr - stio->ifp->_base);
 	    s = stio->ifp->_base;
 #else
+#if (defined(BSD) && (BSD >= 199103))
+	    s = stio->ifp->_base;
+	    fstat(fileno(stio->ifp),&statcache);
+	    if (S_ISDIR(statcache.st_mode))     /* handle NFS glitch */
+		return arg->arg_type == O_FTTEXT ? &str_no : &str_yes;
+
+	    if (stio->ifp->_bf._size  <=  0) {
+		i = getc(stio->ifp);
+		if (i != EOF)
+		    (void)ungetc(i,stio->ifp);
+	    }
+
+	    if (stio->ifp->_bf._size  <=  0)
+		return &str_yes;
+	    len = stio->ifp->_bf._size+(stio->ifp->_p - stio->ifp->_bf._base);
+	    s = stio->ifp->_bf._base;
+#else
 	    fatal("-T and -B not implemented on filehandles");
+#endif
 #endif
 	}
 	else {
