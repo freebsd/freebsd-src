e198c2084e302357c492fb892ae18bb9c017bc03
dyson <dyson@FreeBSD.org>
1996-12-05 04:28:52 +0000

    Clean-up of the new buffer kva allocation code.  Also, there was an error in the !BOUNCE_BUFFERS case.

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -35,7 +35,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91
- *	$Id: machdep.c,v 1.215 1996/12/01 15:46:40 dyson Exp $
+ *	$Id: machdep.c,v 1.216 1996/12/01 16:34:05 bde Exp $
  */
 
 #include "npx.h"
@@ -282,7 +282,7 @@ again:
 	if (nbuf == 0) {
 		nbuf = 30;
 		if( physmem > 1024)
-			nbuf += min((physmem - 1024) / 6, 2048);
+			nbuf += min((physmem - 1024) / 4, 1536);
 	}
 	nswbuf = max(min(nbuf/4, 128), 16);
 
@@ -322,15 +322,15 @@ again:
 
 #ifdef BOUNCE_BUFFERS
 	clean_map = kmem_suballoc(kernel_map, &clean_sva, &clean_eva,
-			(3*nbuf*DFLTBSIZE/2) + (nswbuf*MAXPHYS) +
+			(nbuf*BKVASIZE) + (nswbuf*MAXPHYS) +
 				maxbkva + pager_map_size, TRUE);
 	io_map = kmem_suballoc(clean_map, &minaddr, &maxaddr, maxbkva, FALSE);
 #else
 	clean_map = kmem_suballoc(kernel_map, &clean_sva, &clean_eva,
-			(nbuf*MAXBSIZE) + (nswbuf*MAXPHYS) + pager_map_size, TRUE);
+			(nbuf*BKVASIZE) + (nswbuf*MAXPHYS) + pager_map_size, TRUE);
 #endif
 	buffer_map = kmem_suballoc(clean_map, &buffer_sva, &buffer_eva,
-				(3*nbuf*DFLTBSIZE/2), TRUE);
+				(nbuf*BKVASIZE), TRUE);
 	pager_map = kmem_suballoc(clean_map, &pager_sva, &pager_eva,
 				(nswbuf*MAXPHYS) + pager_map_size, TRUE);
 	exec_map = kmem_suballoc(kernel_map, &minaddr, &maxaddr,
