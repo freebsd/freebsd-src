1527a413962a710e7a0ff273fb6bf4a480649c4f
ache <ache@FreeBSD.org>
1996-12-07 00:16:04 +0000

    Disable seteuid/setegid back, I overlook one place where SETEUID chacked before SETREUID

--- gnu/usr.bin/perl/perl/config.h
+++ gnu/usr.bin/perl/perl/config.h
@@ -353,14 +353,14 @@
  *	to change the effective gid of the current program.
  *	Do not use on systems with _POSIX_SAVED_IDS support.
  */
-#define	HAS_SETEGID	/**/
+/*#undef	HAS_SETEGID		/**/
 
 /* HAS_SETEUID
  *	This symbol, if defined, indicates that the seteuid routine is available
  *	to change the effective uid of the current program.
  *	Do not use on systems with _POSIX_SAVED_IDS support.
  */
-#define	HAS_SETEUID	/**/
+/*#undef	HAS_SETEUID		/**/
 
 /* HAS_SETPGRP
  *	This symbol, if defined, indicates that the setpgrp() routine is
