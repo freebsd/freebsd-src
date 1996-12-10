c0621c83c9fbad7ac3362b2353655eaef67b8af7
Joerg Wunsch <joerg@FreeBSD.org>
1996-12-10 17:24:10 +0000

    YAMFC

--- include/fnmatch.h
+++ include/fnmatch.h
@@ -42,7 +42,7 @@
 #define	FNM_PATHNAME	0x02	/* Slash must be matched by slash. */
 #define	FNM_PERIOD	0x04	/* Period must be matched by period. */
 #ifndef _POSIX_SOURCE
-#define	FNM_LEADING_DIR	0x08	/* Ignore "/*" after match. */
+#define	FNM_LEADING_DIR	0x08	/* Ignore /<tail> after Imatch. */
 #define	FNM_CASEFOLD	0x10	/* Case insensitive search. */
 #endif
 
