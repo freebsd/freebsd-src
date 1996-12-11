42c3522ef4deed32e2cd76246c71160a79602bcb
dg <dg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1996-12-11 03:26:36 +0000

    Only pay attention to the offset and the IP_MF flag in ip_off. Pointed out by Nathaniel D. Daw (daw@panix.com), but fixed differently by me.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@20308 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/ip_input.c
+++ sys/netinet/ip_input.c
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)ip_input.c	8.2 (Berkeley) 1/4/94
- * $Id: ip_input.c,v 1.50 1996/10/25 17:57:45 fenner Exp $
+ * $Id: ip_input.c,v 1.51 1996/11/11 04:56:15 fenner Exp $
  *	$ANA: ip_input.c,v 1.5 1996/09/18 14:34:59 wollman Exp $
  */
 
@@ -439,7 +439,7 @@ ours:
 	 * if the packet was previously fragmented,
 	 * but it's not worth the time; just let them time out.)
 	 */
-	if (ip->ip_off &~ IP_DF) {
+	if (ip->ip_off & (IP_MF | IP_OFFMASK)) {
 		if (m->m_flags & M_EXT) {		/* XXX */
 			if ((m = m_pullup(m, sizeof (struct ip))) == 0) {
 				ipstat.ips_toosmall++;
