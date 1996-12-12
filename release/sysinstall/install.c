63c908f0c7009c24e82e7875d7cb9e09c0550ff6
jkh <jkh@FreeBSD.org>
1996-12-12 23:13:15 +0000

    OGIYAMFC

--- release/sysinstall/install.c
+++ release/sysinstall/install.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: install.c,v 1.134.2.12 1996/12/01 12:28:29 jkh Exp $
+ * $Id: install.c,v 1.145 1996/12/12 23:12:44 jkh Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -494,7 +494,7 @@ installNovice(dialogMenuItem *self)
     }
 
     dialog_clear_norefresh();
-    if (!msgYesNo("The FreeBSD package collection is a collection of over 600 ready-to-run\n"
+    if (!msgYesNo("The FreeBSD package collection is a collection of over 700 ready-to-run\n"
 		  "applications, from text editors to games to WEB servers.  Would you like\n"
 		  "to browse the collection now?"))
 	configPackages(self);
