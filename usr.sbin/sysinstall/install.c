f034d55f02eeb1e220cdab9ade449740e6117af2
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1996-12-12 23:12:44 +0000

    700 now. :)
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@20380 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/sysinstall/install.c
+++ usr.sbin/sysinstall/install.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: install.c,v 1.143 1996/12/01 12:27:36 jkh Exp $
+ * $Id: install.c,v 1.144 1996/12/02 05:01:02 jkh Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -494,7 +494,7 @@ installNovice(dialogMenuItem *self)
     }
 
     dialog_clear_norefresh();
-    if (!msgYesNo("The FreeBSD package collection is a collection of over 600 ready-to-run\n"
+    if (!msgYesNo("The FreeBSD package collection is a collection of over 700 ready-to-run\n"
 		  "applications, from text editors to games to WEB servers.  Would you like\n"
 		  "to browse the collection now?"))
 	configPackages(self);
