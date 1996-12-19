7b53451ad8534abb0a61d76e3203fbdf51a01b6d
bde <bde@FreeBSD.org>
1996-12-19 18:16:33 +0000

    Fixed errno for unsupported advisory locks.  The errno is now EINVAL fcntl() and EOPNOTSUPP for flock().  POSIX specifies the weaker EINVAL errno and the man page agrees.
    Not fixed:
    deadfs: always returns wrong EBADF
    devfs, msdosfs: always return sometimes-wrong EINVAL
    cd9660, fdesc, kernfs, portal: always return sometimes-wrong EOPNOTSUPP
    procfs: always returns wrong EIO
    mfs: panic?!
    nfs: fudged
    
    NetBSD uses a generic file system genfs to do return the sometimes-wrong
    EOPNOTSUPP more consistently :-)(.
    
    Found by:	NIST-PCTS

--- sys/miscfs/specfs/spec_vnops.c
+++ sys/miscfs/specfs/spec_vnops.c
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)spec_vnops.c	8.6 (Berkeley) 4/9/94
- * $Id: spec_vnops.c,v 1.32 1996/09/03 14:23:21 bde Exp $
+ * $Id: spec_vnops.c,v 1.33 1996/10/06 21:19:33 dyson Exp $
  */
 
 #include <sys/param.h>
@@ -712,7 +712,7 @@ spec_advlock(ap)
 	} */ *ap;
 {
 
-	return (EOPNOTSUPP);
+	return (ap->a_flags & F_FLOCK ? EOPNOTSUPP : EINVAL);
 }
 
 /*
