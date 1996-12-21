fb7638501a8f858b58b8ac27791c9cea104d4b43
bde <bde@FreeBSD.org>
1996-12-21 19:04:32 +0000

    YAMFC (fixed lseek() on named pipes by adding new descriptor type DTYPE_FIFO).

--- sys/kern/kern_descrip.c
+++ sys/kern/kern_descrip.c
@@ -36,7 +36,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)kern_descrip.c	8.6 (Berkeley) 4/19/94
- * $Id: kern_descrip.c,v 1.32 1996/09/28 16:33:21 bde Exp $
+ * $Id: kern_descrip.c,v 1.32.2.1 1996/12/21 18:44:04 bde Exp $
  */
 
 #include <sys/param.h>
@@ -420,6 +420,7 @@ ofstat(p, uap, retval)
 		return (EBADF);
 	switch (fp->f_type) {
 
+	case DTYPE_FIFO:
 	case DTYPE_VNODE:
 		error = vn_stat((struct vnode *)fp->f_data, &ub, p);
 		break;
@@ -471,6 +472,7 @@ fstat(p, uap, retval)
 		return (EBADF);
 	switch (fp->f_type) {
 
+	case DTYPE_FIFO:
 	case DTYPE_VNODE:
 		error = vn_stat((struct vnode *)fp->f_data, &ub, p);
 		break;
@@ -528,6 +530,7 @@ fpathconf(p, uap, retval)
 		*retval = PIPE_BUF;
 		return (0);
 
+	case DTYPE_FIFO:
 	case DTYPE_VNODE:
 		vp = (struct vnode *)fp->f_data;
 		return (VOP_PATHCONF(vp, uap->name, retval));
