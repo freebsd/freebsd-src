347feb399c728db4ab953f5bff2b5f0a611f4020
Bruce Evans <bde@FreeBSD.org>
1996-12-21 18:16:22 +0000

    YAMFC (moved printing of BIOS geometries to correct place).

--- sys/i386/i386/machdep.c
+++ sys/i386/i386/machdep.c
@@ -35,7 +35,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91
- *	$Id: machdep.c,v 1.209.2.5 1996/12/15 09:54:01 davidg Exp $
+ *	$Id: machdep.c,v 1.209.2.6 1996/12/15 19:25:41 bde Exp $
  */
 
 #include "npx.h"
@@ -389,38 +389,6 @@ again:
 	 */
 	bufinit();
 	vm_pager_bufferinit();
-
-	/*
-	 * In verbose mode, print out the BIOS's idea of the disk geometries.
-	 */
-	if (bootverbose) {
-		printf("BIOS Geometries:\n");
-		for (i = 0; i < N_BIOS_GEOM; i++) {
-			unsigned long bios_geom;
-			int max_cylinder, max_head, max_sector;
-
-			bios_geom = bootinfo.bi_bios_geom[i];
-
-			/*
-			 * XXX the bootstrap punts a 1200K floppy geometry
-			 * when the get-disk-geometry interrupt fails.  Skip
-			 * drives that have this geometry.
-			 */
-			if (bios_geom == 0x4f010f)
-				continue;
-
-			printf(" %x:%08lx ", i, bios_geom);
-			max_cylinder = bios_geom >> 16;
-			max_head = (bios_geom >> 8) & 0xff;
-			max_sector = bios_geom & 0xff;
-			printf(
-		"0..%d=%d cylinders, 0..%d=%d heads, 1..%d=%d sectors\n",
-			       max_cylinder, max_cylinder + 1,
-			       max_head, max_head + 1,
-			       max_sector, max_sector);
-		}
-		printf(" %d accounted for\n", bootinfo.bi_n_bios_used);
-	}
 }
 
 int
--- sys/pc98/i386/machdep.c
+++ sys/pc98/i386/machdep.c
@@ -35,7 +35,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91
- *	$Id: machdep.c,v 1.11.2.3 1996/12/04 16:00:37 phk Exp $
+ *	$Id: machdep.c,v 1.11.2.4 1996/12/17 04:37:25 davidg Exp $
  */
 
 #include "npx.h"
@@ -398,37 +398,6 @@ again:
 	 */
 	bufinit();
 	vm_pager_bufferinit();
-
-	/*
-	 * In verbose mode, print out the BIOS's idea of the disk geometries.
-	 */
-	if (bootverbose) {
-		printf("BIOS Geometries:\n");
-		for (i = 0; i < N_BIOS_GEOM; i++) {
-			unsigned long bios_geom;
-			int max_cylinder, max_head, max_sector;
-
-			bios_geom = bootinfo.bi_bios_geom[i];
-
-			/*
-			 * XXX the bootstrap punts a 1200K floppy geometry
-			 * when the get-disk-geometry interrupt fails.  Skip
-			 * drives that have this geometry.
-			 */
-			if (bios_geom == 0x4f010f)
-				continue;
-
-			printf(" %x:%08lx ", i, bios_geom);
-			max_cylinder = bios_geom >> 16;
-			max_head = (bios_geom >> 8) & 0xff;
-			max_sector = bios_geom & 0xff;
-			printf(
-		"0..%d=%d cylinders, 0..%d=%d heads, 1..%d=%d sectors\n",
-			       max_cylinder, max_cylinder + 1,
-			       max_head, max_head + 1,
-			       max_sector, max_sector);
-		}
-	}
 }
 
 int
