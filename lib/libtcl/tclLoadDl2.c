8664b3762ed057d55c5ff88baf68749d397e14f0
peter <peter@FreeBSD.org>
1996-12-28 17:15:10 +0000

    Remove these..  umm.. "interesting".. hacks now that they are not needed.

--- lib/libtcl/tclLoadDl2.c
+++
@@ -1,8 +0,0 @@
-/*
- * XXX: We don't have dlopen & friends in statically linked programs
- * XXX: so we avoid using them.
- */
-#ifdef PIC
-#include "../../../contrib/tcl/unix/tclLoadDl2.c"
-#endif
-
