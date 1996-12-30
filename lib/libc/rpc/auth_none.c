27b979b2d78084a3f620dc41ded80f4c3c465519
Peter Wemm <peter@FreeBSD.org>
1996-12-30 14:13:30 +0000

    - prototype now in include file, not here
    Obtained from: a diff of FreeBSD vs. OpenBSD/NetBSD rpc code.

--- lib/libc/rpc/auth_none.c
+++ lib/libc/rpc/auth_none.c
@@ -30,7 +30,7 @@
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)auth_none.c 1.19 87/08/11 Copyr 1984 Sun Micro";*/
 /*static char *sccsid = "from: @(#)auth_none.c	2.1 88/07/29 4.0 RPCSRC";*/
-static char *rcsid = "$Id: auth_none.c,v 1.3 1995/10/22 14:51:06 phk Exp $";
+static char *rcsid = "$Id: auth_none.c,v 1.4 1996/06/08 22:54:49 jraynard Exp $";
 #endif
 
 /*
@@ -46,8 +46,6 @@ static char *rcsid = "$Id: auth_none.c,v 1.3 1995/10/22 14:51:06 phk Exp $";
 #include <rpc/xdr.h>
 #include <rpc/auth.h>
 #define MAX_MARSHEL_SIZE 20
-
-bool_t xdr_opaque_auth(XDR *xdrs, struct opaque_auth *ap);
 
 /*
  * Authenticator operations routines
