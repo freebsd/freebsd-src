ccbc5de87d44fe7e30f931ae53f21419f2db2f9f
KATO Takenori <kato@FreeBSD.org>
1997-01-04 17:05:45 +0000

    YAMFC (use breakpoint instead of Debugger).

--- sys/pc98/pc98/sio.c
+++ sys/pc98/pc98/sio.c
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)com.c	7.5 (Berkeley) 5/16/91
- *	$Id: sio.c,v 1.8.2.2 1996/11/12 09:09:46 phk Exp $
+ *	$Id: sio.c,v 1.8.2.3 1996/12/04 16:01:35 phk Exp $
  */
 
 #include "opt_comconsole.h"
@@ -1840,8 +1840,7 @@ more_intr:
 				if (line_status & LSR_BI) {
 #if defined(DDB) && defined(BREAK_TO_DEBUGGER)
 					if (com->unit == comconsole) {
-						Debugger(
-						    "serial console break");
+						breakpoint();
 						goto cont;
 					}
 #endif
