10e7b9e12ec804040fb6cc115ce7b4e024afcd30
jmacd <jmacd@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-01-11 02:39:37 +0000

    Change the installation rules.  It now copies an empty dir file into the DESTDIR in the beforeinstall rule in src/share/info/Makefile. Then each info file that gets installed into the dir file using install-info.
    It has struck me that there's going to be a problem bootstrapping
    this change, since parts of install will fail until install-info
    is installed.  Maybe someone who knows best how to deal with this can
    fix it.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@21502 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/bsd.info.mk
+++ share/mk/bsd.info.mk
@@ -1,24 +1,31 @@
-# $Id: bsd.info.mk,v 1.20 1997/01/05 15:33:36 ache Exp $
+# $Id: bsd.info.mk,v 1.19 1996/09/03 15:14:45 bde Exp $
 
+BINMODE=        444
+BINDIR?=	/usr/share/info
 MAKEINFO?=	makeinfo
 MAKEINFOFLAGS+=	--no-split # simplify some things, e.g., compression
-SRCDIR?=	${.CURDIR}
+INFODIRFILE?=   dir
+INFOTMPL?=      /usr/share/info/dir-tmpl
+INSTALLINFO?=   install-info
+INFOSECTION?=   Miscellaneous
 
 .MAIN: all
 
 .SUFFIXES: .gz .info .texi .texinfo
 .texi.info:
-	${MAKEINFO} ${MAKEINFOFLAGS} -I ${.CURDIR} -I ${SRCDIR} ${.IMPSRC} -o ${.TARGET}
+	${MAKEINFO} ${MAKEINFOFLAGS} -I ${.CURDIR} ${.IMPSRC} -o ${.TARGET}
 .texinfo.info:
-	${MAKEINFO} ${MAKEINFOFLAGS} -I ${.CURDIR} -I ${SRCDIR} ${.IMPSRC} -o ${.TARGET}
+	${MAKEINFO} ${MAKEINFOFLAGS} -I ${.CURDIR} ${.IMPSRC} -o ${.TARGET}
 
-.PATH: ${.CURDIR} ${SRCDIR}
+.PATH: ${.CURDIR}
+
+IFILENS= ${INFO:S/$/.info/g}
 
 .if !defined(NOINFOCOMPRESS)
 IFILES=	${INFO:S/$/.info.gz/g}
 all: ${IFILES} _SUBDIR
 .else
-IFILES=	${INFO:S/$/.info/g}
+IFILES=	${IFILENS}
 all: ${IFILES} _SUBDIR
 .endif
 
@@ -29,6 +36,16 @@ ${x:S/$/.gz/}:	${x}
 	${GZIPCMD} -c ${.ALLSRC} > ${.TARGET}
 .endfor
 
+.for x in ${INFO}
+INSTALLINFODIRS+= ${x:S/$/-install/}
+${x:S/$/-install/}:
+	${INSTALLINFO} --defsection=${INFOSECTION} \
+		       --defentry=${INFOENTRY_${x}} \
+		       ${x}.info ${DESTDIR}/${BINDIR}/${INFODIRFILE}
+.endfor
+
+.PHONY: ${INSTALLINFODIRS}
+
 # The default is "info" and it can never be "bin"
 DISTRIBUTION?=	info
 .if ${DISTRIBUTION} == "bin"
@@ -37,12 +54,12 @@ DISTRIBUTION=	info
 
 .if !target(distribute)
 distribute: _SUBDIR
-	cd ${.CURDIR} ; $(MAKE) install DESTDIR=${DISTDIR}/${DISTRIBUTION} SHARED=copies     
+	cd ${.CURDIR} ; $(MAKE) install DESTDIR=${DISTDIR}/${DISTRIBUTION} SHARED=copies
 .endif
 
 .if defined(SRCS)
 ${INFO}.info: ${SRCS}
-	${MAKEINFO} ${MAKEINFOFLAGS} -I ${.CURDIR} -I ${SRCDIR} ${SRCS:S/^/${SRCDIR}\//g} -o ${INFO}.info
+	${MAKEINFO} ${MAKEINFOFLAGS} -I ${.CURDIR} ${SRCS:S/^/${.CURDIR}\//g} -o ${INFO}.info
 .endif
 
 depend: _SUBDIR
@@ -51,9 +68,9 @@ depend: _SUBDIR
 clean: _SUBDIR
 	rm -f ${INFO:S/$/.info*/g} Errs errs mklog ${CLEANFILES}
 
-install: _SUBDIR
-	${INSTALL} ${COPY} -o ${INFOOWN} -g ${INFOGRP} -m ${INFOMODE} \
-		${IFILES} ${DESTDIR}${INFODIR}
+install: ${INSTALLINFODIRS} _SUBDIR
+	${INSTALL} ${COPY} -o ${BINOWN} -g ${BINGRP} -m ${BINMODE} \
+		${IFILES} ${DESTDIR}${BINDIR}
 
 .if !target(maninstall)
 maninstall: _SUBDIR
