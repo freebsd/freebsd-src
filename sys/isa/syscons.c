5c8f8b80a3ad88f1a93890c3f75dcef4c9e65056
sos <sos@FreeBSD.org>
1997-01-20 08:05:15 +0000

    Fix the bug that caused CTRL & ALT keys to be hanging sometimes after an X seesion. Really stupid error of me, and I've been looking at this code SO many times. Thanks to Kazutaka YOKOTA for seeing this..
    Submitted by:	Kazutaka YOKOTA

--- sys/isa/syscons.c
+++ sys/isa/syscons.c
@@ -1196,7 +1196,7 @@ scioctl(dev_t dev, int cmd, caddr_t data, int flag, struct proc *p)
 	    return 0;
 
 	case K_XLATE:   	/* switch to XLT ascii mode */
-	    if (scp == cur_console && scp->status == KBD_RAW_MODE)
+	    if (scp == cur_console && scp->status & KBD_RAW_MODE)
 		shfts = ctls = alts = agrs = metas = 0;
 	    scp->status &= ~KBD_RAW_MODE;
 	    return 0;
