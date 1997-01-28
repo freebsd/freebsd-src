a24c90db32c2fc9ba4fefeddc6f5ec0e4a54492f
dg <dg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-01-28 07:06:54 +0000

    Fix signal handler race condition.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@22057 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- libexec/ftpd/ftpd.c
+++ libexec/ftpd/ftpd.c
@@ -1644,6 +1644,11 @@ void
 dologout(status)
 	int status;
 {
+	/*
+	 * Prevent reception of SIGURG from resulting in a resumption
+	 * back to the main program loop.
+	*/
+	transflag = 0;
 
 	if (logged_in) {
 		(void) seteuid((uid_t)0);
