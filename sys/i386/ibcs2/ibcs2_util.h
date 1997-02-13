57180617c4aee839cb5f43b70117ba41ecd479a8
Bruce Evans <bde@FreeBSD.org>
1997-02-13 07:16:56 +0000

    YAMFC (added a missing prototype).

--- sys/i386/ibcs2/ibcs2_util.h
+++ sys/i386/ibcs2/ibcs2_util.h
@@ -90,4 +90,8 @@ int ibcs2_emul_find __P((struct proc *, caddr_t *, const char *, char *,
 #define CHECKALTCREAT(p, sgp, path) \
     ibcs2_emul_find(p, sgp, ibcs2_emul_path, path, &(path), 1)
 
+#ifdef SPX_HACK
+int spx_open __P((struct proc *p, void *uap, int *retval));
+#endif
+
 #endif /* !_IBCS2_UTIL_H_ */
