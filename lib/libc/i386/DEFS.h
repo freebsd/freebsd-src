4fdb3162d1d877787d0419a0ad68e4e6527eeac2
bde <bde@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-02-14 10:57:07 +0000

    Moved definitions of PIC macros from SYS.h to DEFS.h so that SYS.h doesn't need to be included in files that have nothing to do with syscalls.
    Added missing `.text' to START_ENTRY so that ENTRY() works when
    invoked in the data section.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@22715 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/i386/DEFS.h
+++ lib/libc/i386/DEFS.h
@@ -40,6 +40,26 @@
 
 #include <sys/cdefs.h>
 
+#ifdef PIC
+#define	PIC_PROLOGUE	\
+	pushl	%ebx;	\
+	call	1f;	\
+1:			\
+	popl	%ebx;	\
+	addl	$_GLOBAL_OFFSET_TABLE_+[.-1b],%ebx
+#define	PIC_EPILOGUE	\
+	popl	%ebx
+#define	PIC_PLT(x)	x@PLT
+#define	PIC_GOT(x)	x@GOT(%ebx)
+#define	PIC_GOTOFF(x)	x@GOTOFF(%ebx)
+#else
+#define	PIC_PROLOGUE
+#define	PIC_EPILOGUE
+#define	PIC_PLT(x)	x
+#define	PIC_GOT(x)	x
+#define	PIC_GOTOFF(x)	x
+#endif
+
 /*
  * CNAME and HIDENAME manage the relationship between symbol names in C
  * and the equivalent assembly language names.  CNAME is given a name as
@@ -57,7 +77,7 @@
 
 
 /* XXX should use align 4,0x90 for -m486. */
-#define _START_ENTRY	.align 2,0x90;
+#define _START_ENTRY	.text; .align 2,0x90;
 #if 0
 /* Data is not used, except perhaps by non-g prof, which we don't support. */
 #define _MID_ENTRY	.data; .align 2; 8:; .long 0;		\
