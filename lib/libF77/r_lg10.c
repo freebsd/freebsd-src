319ad1705e37656d13112180083a4fe391d0eb41
Bruce Evans <bde@FreeBSD.org>
1997-02-16 17:55:41 +0000

    YAMFC (use log10() instead of a home made version).

--- lib/libF77/r_lg10.c
+++ lib/libF77/r_lg10.c
@@ -1,7 +1,5 @@
 #include "f2c.h"
 
-#define log10e 0.43429448190325182765
-
 #ifdef KR_headers
 double log();
 double r_lg10(x) real *x;
@@ -11,5 +9,5 @@ double r_lg10(x) real *x;
 double r_lg10(real *x)
 #endif
 {
-return( log10e * log(*x) );
+return( log10(*x) );
 }
