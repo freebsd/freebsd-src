7d79becfbdc6fb1f919b81292e31fd622e5417ee
Bruce Evans <bde@FreeBSD.org>
1997-02-16 17:23:01 +0000

    YAMFC (don't include <sys/dir.h>).

--- contrib/gdb/gdb/infptrace.c
+++ contrib/gdb/gdb/infptrace.c
@@ -30,7 +30,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #endif
 
 #include <sys/param.h>
-#include <sys/dir.h>
 #include <signal.h>
 #include <sys/ioctl.h>
 
