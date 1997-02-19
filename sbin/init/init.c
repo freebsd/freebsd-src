725728d3f182a76093d473c536a1a8e671b28cec
David Greenman <dg@FreeBSD.org>
1997-02-19 08:12:01 +0000

    Brought in fix from rev 1.15: protect from stack overrun.
    NOTE: I do not plan to move the RELENG_2_1_7_RELEASE tag after this
    commit since the release has presumably already been rolled. If Jordan
    wants to include this change, then he'll need to move the tag himself.

--- sbin/init/init.c
+++ sbin/init/init.c
@@ -1059,7 +1059,7 @@ start_window_system(sp)
 	if (sp->se_type) {
 		/* Don't use malloc after fork */
 		strcpy(term, "TERM=");
-		strcat(term, sp->se_type);
+		strncat(term, sp->se_type, sizeof(term) - 6);
 		env[0] = term;
 		env[1] = 0;
 	}
@@ -1121,7 +1121,7 @@ start_getty(sp)
 	if (sp->se_type) {
 		/* Don't use malloc after fork */
 		strcpy(term, "TERM=");
-		strcat(term, sp->se_type);
+		strncat(term, sp->se_type, sizeof(term) - 6);
 		env[0] = term;
 		env[1] = 0;
 	}
