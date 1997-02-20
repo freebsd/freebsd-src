dabe6fde8a6ea5c9d335f9bf16e3d85b1e0a198c
bde <bde@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-02-20 13:35:15 +0000

    Split up the Bessel function wrapper files so that most wrapper functions are in their own file.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@22948 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/msun/src/w_j0.c
+++ lib/msun/src/w_j0.c
@@ -15,7 +15,7 @@ static char rcsid[] = "$FreeBSD$";
 #endif
 
 /*
- * wrapper j0(double x), y0(double x)
+ * wrapper j0(double x)
  */
 
 #include "math.h"
@@ -35,34 +35,6 @@ static char rcsid[] = "$FreeBSD$";
 	if(_LIB_VERSION == _IEEE_ || isnan(x)) return z;
 	if(fabs(x)>X_TLOSS) {
 	        return __kernel_standard(x,x,34); /* j0(|x|>X_TLOSS) */
-	} else
-	    return z;
-#endif
-}
-
-#ifdef __STDC__
-	double y0(double x)		/* wrapper y0 */
-#else
-	double y0(x)			/* wrapper y0 */
-	double x;
-#endif
-{
-#ifdef _IEEE_LIBM
-	return __ieee754_y0(x);
-#else
-	double z;
-	z = __ieee754_y0(x);
-	if(_LIB_VERSION == _IEEE_ || isnan(x) ) return z;
-        if(x <= 0.0){
-                if(x==0.0)
-                    /* d= -one/(x-x); */
-                    return __kernel_standard(x,x,8);
-                else
-                    /* d = zero/(x-x); */
-                    return __kernel_standard(x,x,9);
-        }
-	if(x>X_TLOSS) {
-	        return __kernel_standard(x,x,35); /* y0(x>X_TLOSS) */
 	} else
 	    return z;
 #endif
--- lib/msun/src/w_jnf.c
+++ lib/msun/src/w_jnf.c
@@ -40,32 +40,3 @@ static char rcsid[] = "$FreeBSD$";
 	    return z;
 #endif
 }
-
-#ifdef __STDC__
-	float ynf(int n, float x)	/* wrapper ynf */
-#else
-	float ynf(n,x)			/* wrapper ynf */
-	float x; int n;
-#endif
-{
-#ifdef _IEEE_LIBM
-	return __ieee754_ynf(n,x);
-#else
-	float z;
-	z = __ieee754_ynf(n,x);
-	if(_LIB_VERSION == _IEEE_ || isnanf(x) ) return z;
-        if(x <= (float)0.0){
-                if(x==(float)0.0)
-                    /* d= -one/(x-x); */
-                    return (float)__kernel_standard((double)n,(double)x,112);
-                else
-                    /* d = zero/(x-x); */
-                    return (float)__kernel_standard((double)n,(double)x,113);
-        }
-	if(x>(float)X_TLOSS) {
-	    /* yn(x>X_TLOSS,n) */
-	    return (float)__kernel_standard((double)n,(double)x,139);
-	} else
-	    return z;
-#endif
-}
