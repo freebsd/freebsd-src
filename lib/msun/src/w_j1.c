a9570d9f942c778c099c84d08f34773b14f0d9c8
bde <bde@FreeBSD.org>
1997-02-20 13:35:15 +0000

    Split up the Bessel function wrapper files so that most wrapper functions are in their own file.

--- lib/msun/src/w_j1.c
+++ lib/msun/src/w_j1.c
@@ -15,7 +15,7 @@ static char rcsid[] = "$FreeBSD$";
 #endif
 
 /*
- * wrapper of j1,y1
+ * wrapper of j1
  */
 
 #include "math.h"
@@ -36,34 +36,6 @@ static char rcsid[] = "$FreeBSD$";
 	if(_LIB_VERSION == _IEEE_ || isnan(x) ) return z;
 	if(fabs(x)>X_TLOSS) {
 	        return __kernel_standard(x,x,36); /* j1(|x|>X_TLOSS) */
-	} else
-	    return z;
-#endif
-}
-
-#ifdef __STDC__
-	double y1(double x)		/* wrapper y1 */
-#else
-	double y1(x)			/* wrapper y1 */
-	double x;
-#endif
-{
-#ifdef _IEEE_LIBM
-	return __ieee754_y1(x);
-#else
-	double z;
-	z = __ieee754_y1(x);
-	if(_LIB_VERSION == _IEEE_ || isnan(x) ) return z;
-        if(x <= 0.0){
-                if(x==0.0)
-                    /* d= -one/(x-x); */
-                    return __kernel_standard(x,x,10);
-                else
-                    /* d = zero/(x-x); */
-                    return __kernel_standard(x,x,11);
-        }
-	if(x>X_TLOSS) {
-	        return __kernel_standard(x,x,37); /* y1(x>X_TLOSS) */
 	} else
 	    return z;
 #endif
