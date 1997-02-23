94b96be96b5665833efec91045a21e370bb1024b
peter <peter@FreeBSD.org>
1997-02-23 06:56:46 +0000

    Fix some $ Id$ in $ Log$ expansion problems. :-(  (added spaces just in case)
    Pointed out by: bde

--- gnu/libexec/uucp/contrib/uurate.c
+++ gnu/libexec/uucp/contrib/uurate.c
@@ -83,19 +83,7 @@
  *      - reapplied patch by Scott Boyd <scott@futures.com> that I did not
  *        get knowledge of
  */
-/* $Log: uurate.c,v $
- * Revision 1.5  1997/01/14 05:48:22  jkh
- * Make the long-awaited change from $Id$ to $Id$
- *
- * This will make a number of things easier in the future, as well as (finally!)
- * avoiding the Id-smashing problem which has plagued developers for so long.
- *
- * Boy, I'm glad we're not using sup anymore.  This update would have been
- * insane otherwise.
- *
- * Revision 1.4  1995/08/19 21:24:38  ache
- * Commit delta: current -> 1.06 + FreeBSD configuration
- *
+/* Log: uurate.c,v
  * Revision 1.15  1994/04/07  21:47:11  kdburg
  * printed 'no data avail' while there was data; layout chnaged
  * (cosmetic only)
@@ -151,7 +139,7 @@
  * */
 
 char version[] = "@(#) Taylor UUCP Log File Summary Filter, Version 1.2.2";
-static char rcsid[] = "$Id$";
+static char rcsid[] = "$Id: uurate.c,v 1.6 1997/02/22 15:27:02 peter Exp $";
 #include <ctype.h>            /* Character Classification      */
 #include <math.h>
 #include "uucp.h"
