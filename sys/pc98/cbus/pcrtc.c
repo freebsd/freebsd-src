ce81d8a6d266fed014d60d2515664daf0033cf92
kato <kato@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-03-05 16:19:48 +0000

    Synchronize with sys/i386/isa/clock.c revision 1.79.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@23407 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/pc98/cbus/pcrtc.c
+++ sys/pc98/cbus/pcrtc.c
@@ -34,7 +34,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)clock.c	7.2 (Berkeley) 5/12/91
- *	$Id$
+ *	$Id: clock.c,v 1.17 1997/02/22 09:43:32 peter Exp $
  */
 
 /*
@@ -655,7 +655,8 @@ calibrate_clocks(void)
 	u_int count, prev_count, tot_count;
 	int sec, start_sec, timeout;
 
-	printf("Calibrating clock(s) relative to mc146818A clock ... ");
+	if (bootverbose)
+	        printf("Calibrating clock(s) ... ");
 	if (!(rtcin(RTC_STATUSD) & RTCSD_PWR))
 		goto fail;
 	timeout = 100000000;
@@ -728,15 +729,19 @@ calibrate_clocks(void)
 	 */
 	if (cpu_class == CPUCLASS_586 || cpu_class == CPUCLASS_686) {
 		set_i586_ctr_freq((u_int)rdtsc(), tot_count);
-		printf("i586 clock: %u Hz, ", i586_ctr_freq);
+		if (bootverbose)
+		        printf("i586 clock: %u Hz, ", i586_ctr_freq);
 	}
 #endif
 
-	printf("i8254 clock: %u Hz\n", tot_count);
+	if (bootverbose)
+	        printf("i8254 clock: %u Hz\n", tot_count);
 	return (tot_count);
 
 fail:
-	printf("failed, using default i8254 clock of %u Hz\n", timer_freq);
+	if (bootverbose)
+	        printf("failed, using default i8254 clock of %u Hz\n",
+		       timer_freq);
 	return (timer_freq);
 }
 #endif	/* !PC98 */
@@ -818,8 +823,10 @@ startrtclock()
 #endif
 		timer_freq = freq;
 	} else {
-		printf("%d Hz differs from default of %d Hz by more than 1%%\n",
-		       freq, timer_freq);
+		if (bootverbose)
+			printf(
+		    "%d Hz differs from default of %d Hz by more than 1%%\n",
+			       freq, timer_freq);
 #if defined(I586_CPU) || defined(I686_CPU)
 		i586_ctr_freq = 0;
 #endif
@@ -848,7 +855,8 @@ startrtclock()
 		DELAY(1000000);
 		set_i586_ctr_freq((u_int)rdtsc(), timer_freq);
 #ifdef CLK_USE_I586_CALIBRATION
-		printf("i586 clock: %u Hz\n", i586_ctr_freq);
+		if (bootverbose)
+			printf("i586 clock: %u Hz\n", i586_ctr_freq);
 #endif
 	}
 #endif
