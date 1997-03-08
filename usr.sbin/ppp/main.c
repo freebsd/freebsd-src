b1f75f452e045ddd005bf9275ae0df0be8ec04a1
ache <ache@FreeBSD.org>
1997-03-08 10:04:21 +0000

    Unpend all killing signals to shut down this thing immediately, or it is impossible to kill it in some situations. Unpend yet one SIGARLM (see timer.c commit)
    Should go into 2.2

--- usr.sbin/ppp/main.c
+++ usr.sbin/ppp/main.c
@@ -17,7 +17,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: main.c,v 1.32 1997/02/22 16:10:32 peter Exp $
+ * $Id: main.c,v 1.33 1997/02/25 14:05:03 brian Exp $
  *
  *	TODO:
  *		o Add commands for traffic summary, version display, etc.
@@ -349,9 +349,9 @@ char **argv;
   tcgetattr(0, &oldtio);		/* Save original tty mode */
 
   pending_signal(SIGHUP, Hangup);
-  pending_signal(SIGTERM, CloseSession);
-  pending_signal(SIGINT, CloseSession);
-  pending_signal(SIGQUIT, CloseSession);
+  signal(SIGTERM, CloseSession);
+  signal(SIGINT, CloseSession);
+  signal(SIGQUIT, CloseSession);
 #ifdef SIGSEGV
   pending_signal(SIGSEGV, Hangup);
 #endif
@@ -359,7 +359,7 @@ char **argv;
   pending_signal(SIGPIPE, Hangup);
 #endif
 #ifdef SIGALRM
-  pending_signal(SIGALRM, SIG_IGN);
+  signal(SIGALRM, SIG_IGN);
 #endif
   if(mode & MODE_INTER)
     {
