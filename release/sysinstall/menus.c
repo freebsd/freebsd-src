110e0400c013f0aa069979eea0a0f7ab55a9b8e6
jkh <jkh@FreeBSD.org>
1997-03-10 21:10:45 +0000

    o Add alias for `register' so you can say `/stand/sysinstall register' to   go straight to the registration screen.
    o Add ports collection option a little more prominently to distribution
      choices.
    
    o Tell user how to register if they elect to say "No" the first time.

--- release/sysinstall/menus.c
+++ release/sysinstall/menus.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: menus.c,v 1.89.2.25 1997/03/09 22:37:56 jkh Exp $
+ * $Id: menus.c,v 1.89.2.26 1997/03/10 12:20:10 jkh Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -122,8 +122,10 @@ clearX11Fonts(dialogMenuItem *self)
     return DITEM_SUCCESS | DITEM_REDRAW;
 }
 
-#define IS_DEVELOPER(dist, extra) (((dist) == (_DIST_DEVELOPER | (extra))) || ((dist) == (_DIST_DEVELOPER | DIST_DES | (extra))))
-#define IS_USER(dist, extra) (((dist) == (_DIST_USER | (extra))) || ((dist) == (_DIST_USER | DIST_DES | (extra))))
+#define IS_DEVELOPER(dist, extra) ((((dist) & (_DIST_DEVELOPER | (extra))) == (_DIST_DEVELOPER | (extra))) || \
+				   (((dist) & (_DIST_DEVELOPER | DIST_DES | (extra))) == (_DIST_DEVELOPER | DIST_DES | (extra))))
+#define IS_USER(dist, extra) ((((dist) & (_DIST_USER | (extra))) == (_DIST_USER | (extra))) || \
+			      (((dist) & (_DIST_USER | DIST_DES | (extra))) == (_DIST_USER | DIST_DES | (extra))))
 
 static int
 checkDistDeveloper(dialogMenuItem *self)
