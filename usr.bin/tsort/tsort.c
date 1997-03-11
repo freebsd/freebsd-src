70630eac34910312275a4e78ce206bc9c3a3bb47
peter <peter@FreeBSD.org>
1997-03-11 14:47:11 +0000

    Import CSRG 4.4BSD-Lite2 tsort

--- usr.bin/tsort/tsort.c
+++ usr.bin/tsort/tsort.c
@@ -41,7 +41,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)tsort.c	8.2 (Berkeley) 3/30/94";
+static char sccsid[] = "@(#)tsort.c	8.3 (Berkeley) 5/4/95";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -54,6 +54,7 @@ static char sccsid[] = "@(#)tsort.c	8.2 (Berkeley) 3/30/94";
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 /*
  *  Topological sort.  Input is a list of pairs of strings separated by
