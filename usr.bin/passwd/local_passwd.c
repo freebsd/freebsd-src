25a75681875810cd650f496ad8f6d657424431f0
ache <ache@FreeBSD.org>
1997-03-11 14:06:52 +0000

    Fix srandom arg type according to Lite2

--- usr.bin/passwd/local_passwd.c
+++ usr.bin/passwd/local_passwd.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: local_passwd.c,v 1.13 1997/02/22 19:56:34 peter Exp $
+ * $Id: local_passwd.c,v 1.14 1997/03/10 07:46:03 ache Exp $
  */
 
 #ifndef lint
@@ -152,7 +152,7 @@ getnewpasswd(pw, nis)
 		(void)printf("Mismatch; try again, EOF to quit.\n");
 	}
 	/* grab a random printable character that isn't a colon */
-	(void)srandom((unsigned)(time(NULL) ^ getpid()));
+	(void)srandom((unsigned long)(time(NULL) ^ getpid()));
 #ifdef NEWSALT
 	salt[0] = _PASSWORD_EFMT1;
 	to64(&salt[1], (long)(29 * 25), 4);
