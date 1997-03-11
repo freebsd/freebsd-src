f1bfb9dde0b97018110712e1105291123e53f805
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-03-11 13:08:12 +0000

    This commit was generated by cvs2svn to compensate for changes in r23690, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@23691 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/finger/extern.h
+++ usr.bin/finger/extern.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)extern.h	8.1 (Berkeley) 6/6/93
+ *	@(#)extern.h	8.2 (Berkeley) 4/28/95
  */
 
 extern char tbuf[1024];			/* Temp buffer for anybody. */
@@ -40,7 +40,6 @@ extern DB *db;				/* Database. */
 void	 enter_lastlog __P((PERSON *));
 PERSON	*enter_person __P((struct passwd *));
 void	 enter_where __P((struct utmp *, PERSON *));
-void	 err __P((const char *, ...));
 PERSON	*find_person __P((char *));
 void	 lflag_print __P((void));
 int	 match __P((struct passwd *, char *));
