d595a2d0872984b79873b4152ae4550b61e185a6
peter <peter@FreeBSD.org>
1997-03-13 12:44:53 +0000

    Missing $Id$

--- sbin/fsdb/fsdb.h
+++ sbin/fsdb/fsdb.h
@@ -26,6 +26,8 @@
  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
  */
 
 extern int bread __P((int fd, char *buf, daddr_t blk, long size));
