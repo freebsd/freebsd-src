554f89a427daf44c9f014aa57872e41b09e6cafe
joerg <joerg@FreeBSD.org>
1997-03-14 01:27:02 +0000

    Save and restore the state of the variable Initialized inside die(). Otherwise, the shutdown message will print out with 15 NUL bytes in front, due to a missing timestamp.

--- usr.sbin/syslogd/syslogd.c
+++ usr.sbin/syslogd/syslogd.c
@@ -39,7 +39,7 @@ static const char copyright[] =
 static char sccsid[] = "@(#)syslogd.c	8.3 (Berkeley) 4/4/94";
 */
 static const char rcsid[] =
-	"$Id$";
+	"$Id: syslogd.c,v 1.20 1997/02/22 16:14:00 peter Exp $";
 #endif /* not lint */
 
 /*
@@ -1016,8 +1016,10 @@ die(signo)
 	int signo;
 {
 	struct filed *f;
+	int was_initialized;
 	char buf[100];
 
+	was_initialized = Initialized;
 	Initialized = 0;	/* Don't log SIGCHLDs. */
 	for (f = Files; f != NULL; f = f->f_next) {
 		/* flush any pending output */
@@ -1026,6 +1028,7 @@ die(signo)
 		if (f->f_type == F_PIPE)
 			(void)close(f->f_file);
 	}
+	Initialized = was_initialized;
 	if (signo) {
 		dprintf("syslogd: exiting on signal %d\n", signo);
 		(void)sprintf(buf, "exiting on signal %d", signo);
