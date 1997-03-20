0b6b60f9ae349ded9176031e2c151e4622f82826
mpp <mpp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-03-20 23:34:11 +0000

    I had to rename one additional file in the last file import, so update this script to reflect that.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@24070 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/file/cvsimport.sh
+++ usr.bin/file/cvsimport.sh
@@ -3,7 +3,7 @@
 # This shell script can be used in order to handle future imports
 # of newer versions of file(1)
 #
-# $Id$
+# $Id: cvsimport.sh,v 1.3 1997/02/22 19:29:10 peter Exp $
 if [ $# -ne 2 ] ; then 
 	echo "usage: $0 <major> <minor>" 1>&2
 	exit 1
@@ -14,4 +14,5 @@ cd file-$version
 mv file.man file.1
 mv magic.man magic.5
 rm Magdir/Makefile
+mv Magdir/msdos Magdir/ms-dos
 cvs -n import src/usr.bin/file DARWIN file_$1_$2
