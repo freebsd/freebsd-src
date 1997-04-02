21f25076eb411aa40610f1835b4d75b0cf441351
jkh <jkh@FreeBSD.org>
1997-04-02 07:05:16 +0000

    Close a security hole - anonymous FTP user was being created without a password.

--- release/sysinstall/anonFTP.c
+++ release/sysinstall/anonFTP.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: anonFTP.c,v 1.17.2.2 1997/01/15 04:49:59 jkh Exp $
+ * $Id: anonFTP.c,v 1.17.2.3 1997/03/09 22:37:47 jkh Exp $
  *
  * Copyright (c) 1995
  *	Coranth Gryphon.  All rights reserved.
@@ -168,7 +168,7 @@ createFtpUser(void)
 	return DITEM_SUCCESS; 	/* succeeds if already exists */
     }
     
-    sprintf(pwline, "%s::%s:%d::0:0:%s:%s:/bin/date\n", FTP_NAME, tconf.uid, gid, tconf.comment, tconf.homedir);
+    sprintf(pwline, "%s:*:%s:%d::0:0:%s:%s:/nonexistent\n", FTP_NAME, tconf.uid, gid, tconf.comment, tconf.homedir);
     
     fptr = fopen(_PATH_MASTERPASSWD,"a");
     if (! fptr) {
