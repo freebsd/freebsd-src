e4878f1a1937e3460e3a74385dcda04615352ed1
jmallett <jmallett@FreeBSD.org>
1997-04-04 18:41:29 +0000

    Import OpenBSD m4 as of today.

--- usr.bin/m4/pathnames.h
+++ usr.bin/m4/pathnames.h
@@ -1,3 +1,6 @@
+/*	$OpenBSD: pathnames.h,v 1.4 1997/04/04 18:41:29 deraadt Exp $	*/
+/*	$NetBSD: pathnames.h,v 1.6 1995/09/29 00:27:55 cgd Exp $	*/
+
 /*
  * Copyright (c) 1989, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -46,8 +49,8 @@
 #define	UNIQUE		3			/* unique char location */
 #endif
 
-#ifdef unix
-#define _PATH_DIVNAME	"/tmp/m4.0XXXXXX"	/* unix diversion files */
+#if defined(unix) || defined(__NetBSD__) || defined(__OpenBSD__)
+#define _PATH_DIVNAME	"/tmp/m4.0XXXXXXXXXX"	/* unix diversion files */
 #define UNIQUE		8			/* unique char location */
 #endif
 
