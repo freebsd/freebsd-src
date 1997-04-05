083af2b396ca230042067246e8a57587601f818b
KATO Takenori <kato@FreeBSD.org>
1997-04-05 15:05:16 +0000

    Synchronize with sys/i386/isa/sioreg.h revision 1.8.

--- sys/pc98/pc98/sioreg.h
+++ sys/pc98/pc98/sioreg.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)comreg.h	7.2 (Berkeley) 5/9/91
- *	$Id$
+ *	$Id: sioreg.h,v 1.4 1997/02/22 09:43:45 peter Exp $
  */
 
 
@@ -109,20 +109,6 @@
 #define	MSR_TERI	0x04
 #define	MSR_DDSR	0x02
 #define	MSR_DCTS	0x01
-
-/*
- * WARNING: Serial console is assumed to be at COM1 address
- * and CONUNIT must be 0.
- *
- * Well, maybe not...
- */
-#ifndef CONADDR
-#define	CONADDR	(0x3F8)
-#endif
-
-#ifndef CONUNIT
-#define	CONUNIT	(0)
-#endif
 
 #ifndef CONSPEED
 #define CONSPEED (9600)
