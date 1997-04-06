e62e4c624ae75b13f1812862aa5ffc194f1921a4
dufault <dufault@FreeBSD.org>
1997-04-06 11:14:13 +0000

    Make MOD_* macros almost consistent:
    Use the name argument almost the same in all LKM types.  Maintain
    the current behavior for the external (e.g., modstat) name for DEV,
    EXEC, and MISC types being #name ## "_mod" and SYCALL and VFS only
    #name.  This is a candidate for change and I vote just the name without
    the "_mod".
    
    Change the DISPATCH macro to MOD_DISPATCH for consistency with the
    other macros.
    
    Add an LKM_ANON #define to eliminate the magic -1 and associated
    signed/unsigned warnings.
    
    Add MOD_PRIVATE to support wcd.c's poking around in the lkm structure.
    
    Change source in tree to use the new interface.
    
    Reviewed by:	Bruce Evans

--- sys/i386/isa/atapi.c
+++ sys/i386/isa/atapi.c
@@ -1067,7 +1067,8 @@ static int atapi_unload (struct lkm_table *lkmtp, int cmd)
  */
 int atapi_mod (struct lkm_table *lkmtp, int cmd, int ver)
 {
-	DISPATCH (lkmtp, cmd, ver, atapi_load, atapi_unload, lkm_nullcmd);
+	MOD_DISPATCH (atapi, lkmtp, cmd, ver,
+		atapi_load, atapi_unload, lkm_nullcmd);
 }
 #endif /* ATAPI_MODULE */
 
