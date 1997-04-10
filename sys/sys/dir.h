d1f3e5b0fe31ca90ca665badcff0930933b33ca4
Bruce Evans <bde@FreeBSD.org>
1997-04-10 13:54:19 +0000

    Deprecated <sys/dir.h> some more - changed the comment saying that <dirent.h> should be used instead to a warning.  If this causes too many warnings in ports then it should be changed back after checking some ports for related configuration errors.
    Moved the definition of DIRSIZ() from <sys/dir.h> to <sys/dirent.h>
    so that it can be used in the kernel without including <sys/dir.h>.
    Renamed it in some cases to avoid new namespace pollution.

--- sys/sys/dir.h
+++ sys/sys/dir.h
@@ -31,32 +31,23 @@
  * SUCH DAMAGE.
  *
  *	@(#)dir.h	8.2 (Berkeley) 1/4/94
- * $Id$
- */
-
-/*
- * The information in this file should be obtained from <dirent.h>
- * and is provided solely (and temporarily) for backward compatibility.
+ * $Id: dir.h,v 1.4 1997/02/22 09:45:01 peter Exp $
  */
 
 #ifndef _SYS_DIR_H_
 #define	_SYS_DIR_H_
+
+#if __GNUC__
+#warning "The information in this file should be obtained from <dirent.h>"
+#warning "and is provided solely (and temporarily) for backward compatibility."
+#endif
 
 #include <dirent.h>
 
 /*
  * Backwards compatibility.
  */
-#define direct dirent
-
-/*
- * The DIRSIZ macro gives the minimum record length which will hold
- * the directory entry.  This requires the amount of space in struct direct
- * without the d_name field, plus enough space for the name with a terminating
- * null byte (dp->d_namlen+1), rounded up to a 4 byte boundary.
- */
-#undef DIRSIZ
-#define DIRSIZ(dp) \
-    ((sizeof (struct direct) - (MAXNAMLEN+1)) + (((dp)->d_namlen+1 + 3) &~ 3))
+#define	direct		dirent
+#define	DIRSIZ(dp)	_GENERIC_DIRSIZ(dp)
 
 #endif /* !_SYS_DIR_H_ */
