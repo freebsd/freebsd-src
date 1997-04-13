3aafa26aa4859a37620bed85f5306a1b1d1c822d
paul <paul@FreeBSD.org>
1997-04-13 11:33:17 +0000

    This file is deprecated not "obsoleted".

--- include/malloc.h
+++ include/malloc.h
@@ -1,5 +1,5 @@
 #if __GNUC__
-#warning "this file includes <malloc.h> which is obsoleted, use <stdlib.h> instead"
+#warning "this file includes <malloc.h> which is deprecated, use <stdlib.h> instead"
 #endif
 
 #include <stdlib.h>
