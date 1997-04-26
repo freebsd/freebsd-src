02518b1729ce479dfd28b44a0816a6fd104e6558
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-04-26 19:26:37 +0000

    change #if 0 to #ifdef KERNEL since this is pretty important for smp compiling.  It still leaves dset non-functional on smp kernels, but we've been doing that all along with the private smp tree anyway.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@25176 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/isa/isa_device.h
+++ sys/i386/isa/isa_device.h
@@ -31,15 +31,16 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)isa_device.h	7.1 (Berkeley) 5/9/91
- *	$Id: isa_device.h,v 1.36 1997/04/26 11:46:02 peter Exp $
+ *	$Id: isa_device.h,v 1.37 1997/04/26 19:07:36 fsmp Exp $
  */
 
 #ifndef _I386_ISA_ISA_DEVICE_H_
 #define	_I386_ISA_ISA_DEVICE_H_
 
-#if 0
+#ifdef KERNEL
 #include "opt_smp.h"
 #endif
+
 /*
  * ISA Bus Autoconfiguration
  */
