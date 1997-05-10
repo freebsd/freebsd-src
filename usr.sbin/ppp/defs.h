73e13c30f3f5c75f581b95f063c8e1437b55163e
brian <brian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-05-10 03:39:57 +0000

    Add a ttyXX.if file in /var/run that points to the tunX.pid file.  Change the ppp.tunX.pid name to tunX.pid
    Requested by:	Daniel O Callaghan <danny@panda.hilink.com.au>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@25634 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/ppp/defs.h
+++ usr.sbin/ppp/defs.h
@@ -15,7 +15,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: defs.h,v 1.12 1997/04/21 01:01:41 brian Exp $
+ * $Id: defs.h,v 1.13 1997/05/04 02:39:03 ache Exp $
  *
  *	TODO:
  */
@@ -39,8 +39,10 @@
 #define	LOGFILE		"/var/log/ppp.tun%d.log" /* Name of log	file */
 #ifdef __FreeBSD__
 #define	MODEM_DEV	"/dev/cuaa1"		/* name of tty device */
+#define	BASE_MODEM_DEV	"cuaa1"			/* name of base tty device */
 #else
 #define	MODEM_DEV	"/dev/tty01"		/* name of tty device */
+#define	BASE_MODEM_DEV	"tty01"			/* name of base tty device */
 #endif
 #define MODEM_SPEED	B38400			/* tty speed */
 #define	SERVER_PORT	3000			/* Base server port no. */
