a443e2f409b2476ae7956e927d961fa66a040c54
brian <brian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-05-14 01:14:32 +0000

    Don't attempt to ioctl(modem...) when modem is closed.  Just DownConnection().
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@25800 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/ppp/modem.c
+++ usr.sbin/ppp/modem.c
@@ -17,7 +17,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: modem.c,v 1.35 1997/05/10 01:22:17 brian Exp $
+ * $Id: modem.c,v 1.36 1997/05/10 03:39:54 brian Exp $
  *
  *  TODO:
  */
@@ -228,6 +228,7 @@ DownConnection()
 void
 ModemTimeout()
 {
+  static int waiting;
   int ombits = mbits;
   int change;
 
@@ -237,6 +238,13 @@ ModemTimeout()
   StartTimer(&ModemTimer);
 
   if (dev_is_modem) {
+    if (modem < 0) {
+      if (!waiting)
+        DownConnection();
+      waiting = 1;
+      return;
+    }
+    waiting = 0;
     if (ioctl(modem, TIOCMGET, &mbits) < 0) {
       LogPrintf(LOG_PHASE_BIT, "ioctl error (%s)!\n", strerror(errno));
       DownConnection();
