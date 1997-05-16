c3559f970f329207669e1394c9ccfaa712fa2de6
jkh <jkh@FreeBSD.org>
1997-05-16 09:21:47 +0000

    Bump reldate for 2.2.2

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -32,7 +32,7 @@
 # SUCH DAMAGE.
 #
 #	@(#)newvers.sh	8.1 (Berkeley) 4/20/94
-#	$Id: newvers.sh,v 1.26.2.11 1997/04/15 05:29:32 asami Exp $
+#	$Id: newvers.sh,v 1.26.2.12 1997/04/30 23:42:22 asami Exp $
 
 TYPE="FreeBSD"
 REVISION="2.2"
@@ -44,7 +44,7 @@ if [ "X${SNAPDATE}" != "X" ]; then
 fi
 VERSION="${TYPE} ${RELEASE}"
 
-RELDATE="221002"
+RELDATE="222000"
 
 b=share/examples/etc/bsd-style-copyright
 year=`date '+%Y'`
