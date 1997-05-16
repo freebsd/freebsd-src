6bcf8482922a0f10576fb98b0ad3ec18ff0be955
pst <pst@FreeBSD.org>
1997-05-16 20:37:44 +0000

    Allow blank lines and comments.

--- release/sysinstall/variable_load.c
+++ release/sysinstall/variable_load.c
@@ -4,7 +4,7 @@
  * This is probably the last attempt in the `sysinstall' line, the next
  * generation being slated for what's essentially a complete rewrite.
  *
- * $Id: variable_load.c,v 1.1.2.1 1997/03/28 02:25:15 jkh Exp $
+ * $Id: variable_load.c,v 1.1.2.2 1997/04/01 02:04:58 pst Exp $
  *
  * Copyright (c) 1997
  *	Paul Traina.  All rights reserved.
@@ -84,6 +84,10 @@ variableLoad(dialogMenuItem * self)
     msgNotify("Loading %s pre-configuration file", cp);
 
     while (fgets(buf, sizeof buf, fp)) {
+	if ((cp = strchr(buf, '\n')) != NULL)
+	    *cp = '\0';
+	if (*buf == '\0' || *buf == '#')
+	    continue;
 	if (DITEM_STATUS(dispatchCommand(buf)) != DITEM_SUCCESS) {
 	    msgConfirm("Command `%s' failed - rest of script aborted.\n", buf);
 	    what |= DITEM_FAILURE;
