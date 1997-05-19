1ad8b2cb849edcf4943deda44af5fe3f1f2dba4d
Jean-Marc Zucconi <jmz@FreeBSD.org>
1997-05-19 17:30:50 +0000

    A few improvements to the worm driver.
    - remove all calls to scsi_stop_unit(). Some drives refuse commands when
      stopped. This will fix the 'device not configured' message which was
      cleared after opening/closing the tray.
    - Never set the logical block address in the scsi_cmd struct when writing.
      The computation was bogus for block sizes not a multiple of DEV_BSIZE.
      (the bug is still there in the READ case)
    - reset the block size to the 2048 bytes in finalize_track() track to avoid
      an error when mounting a disk after an audio write.
    - remove the WORMIOCQUIRKSELECT ioctl. Quirks are now recorded at probe time
      (see scsiconf.c)
    - change and expand the argument to the WORMIOCPREPTRACK ioctl. It now possible
      to select more track options (copy bits, ISRC codes, track type,
      track number)
    - add an error handler to catch false errors (warnings in fact) and record
      the error type.
    - add an ioctl call (WORMIOERROR) to get more information on the nature of the
      error when a command or a write failed.
    - add an ioctl call (WORMIOCFINISHTRACK) to finalize a track without closing
      the device (closing the device still finalize the track if the command was
      not performed)
    
    Approved by:	joerg

--- sys/sys/wormio.h
+++ sys/sys/wormio.h
@@ -9,18 +9,6 @@
 * Ioctls for the WORM drive					*
 \***************************************************************/
 
-/*
- * Quirk select: chose the set of quirk functions to use for this
- * device.
- */
-
-struct wormio_quirk_select
-{
-	const char *vendor;	/* vendor name */
-	const char *model;	/* model name */
-};
-
-#define WORMIOCQUIRKSELECT	_IOW('W', 10, struct wormio_quirk_select)
 
 /*
  * Prepare disk-wide parameters.
@@ -42,9 +30,28 @@ struct wormio_prepare_track
 {
 	int audio;		/* audio track (data track if 0) */
 	int preemp;		/* audio with preemphasis */
+#define BLOCK_RAW             0 /* 2352 bytes, raw data */
+#define BLOCK_RAWPQ           1 /* 2368 bytes, raw data with P and Q subchannels */
+#define BLOCK_RAWPW           2 /* 2448 bytes, raw data with P-W subchannel appended */
+#define BLOCK_MODE_1          8 /* 2048 bytes, mode 1 (ISO/IEC 10149) */
+#define BLOCK_MODE_2          9 /* 2336 bytes, mode 2 (ISO/IEC 10149) */
+#define BLOCK_MODE_2_FORM_1  10 /* 2048 bytes, CD-ROM XA form 1 */
+#define BLOCK_MODE_2_FORM_1b 11 /* 2056 bytes, CD-ROM XA form 1 */
+#define BLOCK_MODE_2_FORM_2  12 /* 2324 bytes, CD-ROM XA form 2 */
+#define BLOCK_MODE_2_FORM_2b 13 /* 2332 bytes, CD-ROM XA form 2 */
+        int track_type;         /* defines the number of bytes in a block */
+#define COPY_INHIBIT    0       /* no copy allowed */
+#define COPY_PERMITTED  1       /* track can be copied */
+#define COPY_SCMS       2       /* alternate copy */
+        int copy_bits;          /* define the possibilities for copying */
+        int track_number;
+        char ISRC_country[2];   /* country code (2 chars) */
+        char ISRC_owner[3];     /* owner code (3 chars) */
+        int ISRC_year;          /* year of recording */
+        char ISRC_serial[5];    /* serial number */
 };
-
 #define WORMIOCPREPTRACK	_IOW('W', 21, struct wormio_prepare_track)
+
 
 /*
  * Fixation: write leadins and leadouts.  Select table-of-contents
@@ -59,5 +66,22 @@ struct wormio_fixation
 };
 
 #define WORMIOCFIXATION		_IOW('W', 22, struct wormio_fixation)
+
+/* 
+ * Finalize track
+ */
+#define WORMIOCFINISHTRACK      _IO('W', 23)
+
+/* Errors/warnings */
+#define WORM_SEQUENCE_ERROR                  1
+#define WORM_DUMMY_BLOCKS_ADDED              2
+#define WORM_CALIBRATION_AREA_ALMOST_FULL    3
+#define WORM_CALIBRATION_AREA_FULL           4
+#define WORM_BUFFER_UNDERRUN                 5
+#define WORM_ABSORPTION_CONTROL_ERROR        6
+#define WORM_END_OF_MEDIUM                   7
+#define WORM_OPTIMUM_POWER_CALIBRATION_ERROR 8
+
+#define WORMIOERROR            _IOR('W', 24, int)
 
 #endif /* !_SYS_WORMIO_H_ */
