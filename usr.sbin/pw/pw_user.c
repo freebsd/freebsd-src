ef2123895de7b3de01a584334b4c17a7574f0740
davidn <davidn@FreeBSD.org>
1997-05-24 10:43:09 +0000

    MFC: Bugfix for -k. PR:		3666 Reviewed by: Submitted by: Obtained from:

--- usr.sbin/pw/pw_user.c
+++ usr.sbin/pw/pw_user.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: pw_user.c,v 1.1.1.1.2.6 1997/01/03 06:33:15 davidn Exp $
+ *	$Id: pw_user.c,v 1.1.1.1.2.7 1997/02/07 11:21:46 davidn Exp $
  */
 
 #include <unistd.h>
@@ -229,7 +229,7 @@ pw_user(struct userconf * cnf, int mode, struct cargs * args)
 			cnf->groups[i++] = NULL;
 	}
 	if ((arg = getarg(args, 'k')) != NULL) {
-		if (stat(cnf->dotdir = arg->val, &st) == -1 || S_ISDIR(st.st_mode))
+		if (stat(cnf->dotdir = arg->val, &st) == -1 || !S_ISDIR(st.st_mode))
 			cmderr(EX_OSFILE, "skeleton `%s' is not a directory or does not exist\n", cnf->dotdir);
 	}
 	if ((arg = getarg(args, 's')) != NULL)
