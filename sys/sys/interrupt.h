a201b8ac68687823a59787419197aa1c621886d1
se <se@FreeBSD.org>
1997-05-26 14:37:43 +0000

    Add support for shared interrupts to the kernel. This code is meant be (eventually) architecture independent. It provides an emulation of the ISA interrupt registration function register_intr(), but that function does no longer manipulated the interrupt controller and interrupt descriptor table, but calls the architecture dependent function setup_icu() for that purpose.
    After theISA/EISA bus code has been modified to directly call the new
    interrupt registartion functions (intr_create() and intr_connect()),
    the emulation of register_intr() should be dropped.
    
    The C level interrupt handler function should take a (void*) argument,
    and the function pointer type (inthand2_t) should defined in some  other
    place than isa_device.h.
    
    This commit is a pre-requisite for the removal of the PCI specific shared
    interrupt code.
    
    Reviewed by:	dfr,bde

---
+++ sys/sys/interrupt.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 1997, Stefan Esser <se@freebsd.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ *
+ */
+
+#if ICU_LEN > 32
+#error adjust type of intrmask for ICU_LEN of more than 32
+#endif
+/* XXX adjust size if more than 32 interrupts are supported ! */
+typedef u_int32_t intrmask;
+
+/* XXX inthand2_typ should be defined here, not in isa_device.h */
+
+typedef struct intrec {
+	intrmask	mask;
+	inthand2_t	*handler;
+	void		*argument;
+	struct intrec	*next;
+	void		*devdata;
+	int		intr;
+	intrmask	*maskptr;
+	int		flags;
+#define	INTR_FAST		0x00000001 /* fast interrupt handler */
+#define INTR_EXCL		0x00010000 /* excl. intr, default is shared */
+} intrec;
+
+/* XXX currently dev_instance must be set to the ISA device_id or -1 for PCI */
+intrec *intr_create(void *dev_instance, int irq, inthand2_t handler,
+		    void *arg, intrmask *maskptr, int flags);
+int intr_destroy(intrec *idesc);
+
+int intr_connect(intrec *idesc);
+int intr_disconnect(intrec *idesc);
+
+/* XXX emulate old interface for now ... */
+int register_intr __P((int intr, int device_id, u_int flags,
+		       inthand2_t *handler, u_int *maskptr, int unit));
+int unregister_intr(int intr, inthand2_t handler);
