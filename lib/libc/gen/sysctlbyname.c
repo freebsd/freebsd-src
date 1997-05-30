340e598fffcd5a2304686739da400cc94e28dda7
phk <phk@FreeBSD.org>
1997-05-30 20:53:13 +0000

    sysctlbyname allows acces to sysctl variables by name.
    The manpage has been sent to linquistic decontamination and will arrive
    when released from the quarantine
    
    Reviewed by:	peter

---
+++ lib/libc/gen/sysctlbyname.c
@@ -0,0 +1,33 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@FreeBSD.org> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+ *
+ * $Id$
+ *
+ */
+#include <sys/types.h>
+#include <sys/sysctl.h>
+
+int
+sysctlbyname(char *name, void *oldp, size_t *oldlenp, void *newp, size_t newlen)
+{
+	int name2oid_oid[2];
+	int real_oid[CTL_MAXNAME+2];
+	int error, oidlen;
+
+	name2oid_oid[0] = 0;	/* This is magic & undocumented! */
+	name2oid_oid[1] = 3;
+
+	oidlen = sizeof(real_oid);
+	error = sysctl(name2oid_oid, 2, real_oid, &oidlen, name, strlen(name));
+	if (error < 0) 
+		return error;
+	oidlen /= sizeof (int);
+	error = sysctl(real_oid, oidlen, oldp, oldlenp, newp, newlen);
+	return (error);
+}
+
