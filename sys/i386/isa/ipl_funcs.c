47a48e6327c679ee83116e5ec6478dc5b757ef3d
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-05-31 08:57:05 +0000

    Bruce's original implementation of the splxxx() routines, but as C code rather than inlines.  These are compiled with -fomit-frame-pointer and work out pretty close to the original routines, but it might be a fraction slower.  The reason for doing this is to prevent the SWI_* and HWI_* values from being compiled into drivers and lkms etc which is one of the things that prevents the same lkm from being used on both SMP and UP kernels.
    This gives us a lot more scope for experimenting with the splxxx
    implementaton for SMP parallelism etc.
    
    Reviewed by: bde
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@26301 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ sys/i386/isa/ipl_funcs.c
@@ -0,0 +1,94 @@
+/*-
+ * Copyright (c) 1997 Bruce Evans.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	$Id$
+ */
+
+#include <sys/types.h>
+#include <sys/systm.h>
+#include <machine/ipl.h>
+
+/*
+ * The volatile bitmap variables must be set atomically.  This normally
+ * involves using a machine-dependent bit-set or `or' instruction.
+ */
+
+#define DO_SETBITS(name, var, bits) \
+void name(void)					\
+{						\
+	setbits(var, bits);			\
+}
+
+DO_SETBITS(setdelayed,   &ipending, loadandclear((unsigned *)&idelayed))
+DO_SETBITS(setsoftast,   &ipending, SWI_AST_PENDING)
+DO_SETBITS(setsoftclock, &ipending, SWI_CLOCK_PENDING)
+DO_SETBITS(setsoftnet,   &ipending, SWI_NET_PENDING)
+DO_SETBITS(setsofttty,   &ipending, SWI_TTY_PENDING)
+
+DO_SETBITS(schedsoftnet, &idelayed, SWI_NET_PENDING)
+DO_SETBITS(schedsofttty, &idelayed, SWI_TTY_PENDING)
+
+unsigned
+softclockpending(void)
+{
+	return (ipending & SWI_CLOCK_PENDING);
+}
+
+#define	GENSPL(name, set_cpl) \
+unsigned name(void)				\
+{						\
+	unsigned x;				\
+						\
+	x = cpl;				\
+	set_cpl;				\
+	return (x);				\
+}
+
+GENSPL(splbio, cpl |= bio_imask)
+GENSPL(splclock, cpl = HWI_MASK | SWI_MASK)
+GENSPL(splhigh, cpl = HWI_MASK | SWI_MASK)
+GENSPL(splimp, cpl |= net_imask)
+GENSPL(splnet, cpl |= SWI_NET_MASK)
+GENSPL(splsoftclock, cpl = SWI_CLOCK_MASK)
+GENSPL(splsofttty, cpl |= SWI_TTY_MASK)
+GENSPL(splstatclock, cpl |= stat_imask)
+GENSPL(spltty, cpl |= tty_imask)
+GENSPL(splvm, cpl |= net_imask | bio_imask)
+
+void
+spl0(void)
+{
+	cpl = SWI_AST_MASK;
+	if (ipending & ~SWI_AST_MASK)
+		splz();
+}
+
+void
+splx(unsigned ipl)
+{
+	cpl = ipl;
+	if (ipending & ~ipl)
+		splz();
+}
