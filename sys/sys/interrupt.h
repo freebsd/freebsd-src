a97e2d0d9b72ed1f7cba93f4ca96cdc45bff3730
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-06-01 16:05:14 +0000

    Move "typedef struct intrec {} intrec" from sys/interrupt.h to kern_intr.c since that's the only place that it's used.
    Submitted by: se  (apparently on suggestion from dfr)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@26349 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/interrupt.h
+++ sys/sys/interrupt.h
@@ -23,30 +23,17 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: interrupt.h,v 1.1 1997/05/26 14:37:31 se Exp $
- *
+ * $Id: interrupt.h,v 1.2 1997/05/31 09:33:29 peter Exp $
  */
 
-typedef struct intrec {
-	intrmask_t	mask;
-	inthand2_t	*handler;
-	void		*argument;
-	struct intrec	*next;
-	void		*devdata;
-	int		intr;
-	intrmask_t	*maskptr;
-	int		flags;
-#define	INTR_FAST		0x00000001 /* fast interrupt handler */
-#define INTR_EXCL		0x00010000 /* excl. intr, default is shared */
-} intrec;
-
 /* XXX currently dev_instance must be set to the ISA device_id or -1 for PCI */
-intrec *intr_create(void *dev_instance, int irq, inthand2_t handler,
-		    void *arg, intrmask_t *maskptr, int flags);
-int intr_destroy(intrec *idesc);
+struct intrec *intr_create(void *dev_instance, int irq, inthand2_t handler,
+			   void *arg, intrmask_t *maskptr, int flags);
 
-int intr_connect(intrec *idesc);
-int intr_disconnect(intrec *idesc);
+int intr_destroy(struct intrec *idesc);
+
+int intr_connect(struct intrec *idesc);
+int intr_disconnect(struct intrec *idesc);
 
 /* XXX emulate old interface for now ... */
 int register_intr __P((int intr, int device_id, u_int flags,
