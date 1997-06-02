10484b859e3de193953d5c9f900f410cb84cb2ad
Mike Smith <msmith@FreeBSD.org>
1997-06-02 06:45:17 +0000

    Grr.  Remove not-ready LDT modification fluff that also crept in with the last commit.

--- sys/alpha/linux/linux.h
+++ sys/alpha/linux/linux.h
@@ -25,7 +25,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *	$Id: linux.h,v 1.14 1997/02/22 09:38:19 peter Exp $
+ *	$Id: linux.h,v 1.15 1997/06/02 06:28:03 msmith Exp $
  */
 
 #ifndef _I386_LINUX_LINUX_H_
@@ -592,30 +592,5 @@ struct linux_ifreq
 #define LINUX_ASYNC_PGRP_LOCKOUT    	0x0200
 #define LINUX_ASYNC_CALLOUT_NOHUP   	0x0400
 #define LINUX_ASYNC_FLAGS     		0x0FFF
-
-/* structure and defines for i386_modify_ldt */
-
-/* Maximum number of LDT entries supported. */
-#define LDT_ENTRIES     8192
-/* The size of each LDT entry. */
-#define LDT_ENTRY_SIZE  8
-
-struct modify_ldt_ldt_s {
-        unsigned int  entry_number;
-        unsigned long base_addr;
-        unsigned int  limit;
-        unsigned int  seg_32bit:1;
-        unsigned int  contents:2;
-        unsigned int  read_exec_only:1;
-        unsigned int  limit_in_pages:1;
-        unsigned int  seg_not_present:1;
-};
-
-#define MODIFY_LDT_CONTENTS_DATA	0
-#define MODIFY_LDT_CONTENTS_STACK	1
-#define MODIFY_LDT_CONTENTS_CODE	2
-
-#define LINUX_MODIFY_LDT_GET		0
-#define LINUX_MODIFY_LDT_SET		1
 
 #endif /* !_I386_LINUX_LINUX_H_ */
