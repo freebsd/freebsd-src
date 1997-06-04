175efc06b9df352d0b8bfb7d8fcc5251674743ac
pst <pst@FreeBSD.org>
1997-06-04 04:52:40 +0000

    If the boot blocks were using the serial port, read the system console speed using the boot blocks, instead of a hardcoded value stuck in the kernel.  This way, you can have systems using the same kernel but different console speeds.
    Add a sysctl entry for changing the system console speed.
    Lock the user tty speed to match the system console speed.
    
    Nuke CONSPEED.
    Reviewed by:	bde

--- sys/isa/sioreg.h
+++ sys/isa/sioreg.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)comreg.h	7.2 (Berkeley) 5/9/91
- *	$Id: sioreg.h,v 1.7 1997/02/22 09:37:10 peter Exp $
+ *	$Id: sioreg.h,v 1.8 1997/04/05 13:15:50 bde Exp $
  */
 
 
@@ -106,6 +106,5 @@
 #define	MSR_DDSR	0x02
 #define	MSR_DCTS	0x01
 
-#ifndef CONSPEED
-#define CONSPEED (9600)
-#endif
+/* speed to initialize to during chip tests */
+#define	SIO_TEST_SPEED	9600
