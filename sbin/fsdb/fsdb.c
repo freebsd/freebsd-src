645259b64b739a05b39a77f3333021989e482d0b
Philippe Charnier <charnier@FreeBSD.org>
1997-06-12 07:01:23 +0000

    MFC: Add Id. Staticize usage function. Remove __progname. Make -f option a no-op according to the man page. Remove unused variables.

--- sbin/fsdb/fsdb.c
+++ sbin/fsdb/fsdb.c
@@ -26,6 +26,8 @@
  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
+ *
+ *		$Id$
  */
 
 #ifndef lint
@@ -47,6 +49,7 @@ static char rcsid[] = "$NetBSD: fsdb.c,v 1.2 1995/10/08 23:18:10 thorpej Exp $";
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <err.h>
 
 #include <ufs/ufs/dinode.h>
 #include <ufs/ufs/dir.h>
@@ -55,15 +58,14 @@ static char rcsid[] = "$NetBSD: fsdb.c,v 1.2 1995/10/08 23:18:10 thorpej Exp $";
 #include "fsdb.h"
 #include "fsck.h"
 
-extern char *__progname;	/* from crt0.o */
-
-void usage __P((void));
+static void usage __P((void));
 int cmdloop __P((void));
 
-void 
+static void 
 usage()
 {
-	errx(1, "usage: %s [-d] -f <fsname>", __progname);
+	fprintf(stderr, "usage: fsdb [-d] [-f] fsname\n");
+	exit(1);
 }
 
 int returntosingle = 0;
@@ -81,12 +83,13 @@ main(argc, argv)
 {
 	int ch, rval;
 	char *fsys = NULL;
-	struct stat stb;
 
-	while (-1 != (ch = getopt(argc, argv, "f:d"))) {
+	while (-1 != (ch = getopt(argc, argv, "fd"))) {
 		switch (ch) {
 		case 'f':
-			fsys = optarg;
+				/* The -f option is left for historical
+				 * reasons and has no meaning.
+				 */
 			break;
 		case 'd':
 			debug++;
@@ -97,12 +100,10 @@ main(argc, argv)
 	}
 	argc -= optind;
 	argv += optind;
-	if (fsys == NULL) { 
-		if (argc != 1)
-			usage();
-		else
-			fsys = argv[0];
-	}
+	if (argc != 1)
+		usage();
+	else
+		fsys = argv[0];
 
 	if (!setup(fsys))
 		errx(1, "cannot set up file system `%s'", fsys);
@@ -229,7 +230,7 @@ cmdloop()
     hist = history_init();
     history(hist, H_EVENT, 100);	/* 100 elt history buffer */
 
-    elptr = el_init(__progname, stdin, stdout);
+    elptr = el_init("fsdb", stdin, stdout);
     el_set(elptr, EL_EDITOR, "emacs");
     el_set(elptr, EL_PROMPT, prompt);
     el_set(elptr, EL_HIST, history, hist);
@@ -464,7 +465,6 @@ CMDFUNCSTART(focusname)
 CMDFUNCSTART(ln)
 {
     ino_t inum;
-    struct dinode *dp;
     int rval;
     char *cp;
 
@@ -514,7 +514,6 @@ chinumfunc(idesc)
 
 CMDFUNCSTART(chinum)
 {
-    int rval;
     char *cp;
     ino_t inum;
     struct inodesc idesc;
@@ -568,7 +567,6 @@ CMDFUNCSTART(chname)
 {
     int rval;
     char *cp;
-    ino_t inum;
     struct inodesc idesc;
     
     slotcount = 0;
@@ -611,7 +609,6 @@ struct typemap {
 
 CMDFUNCSTART(newtype)
 {
-    int rval = 1;
     int type;
     struct typemap *tp;
 
