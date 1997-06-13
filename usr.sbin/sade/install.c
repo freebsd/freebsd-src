9bfa3497f92cde6881ccddc133d5f124d80b7b64
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-06-13 07:11:57 +0000

    YAMF22
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@26598 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/sade/install.c
+++ usr.sbin/sade/install.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: install.c,v 1.186 1997/05/22 04:04:30 jkh Exp $
+ * $Id: install.c,v 1.187 1997/05/30 01:03:09 jkh Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -968,6 +968,7 @@ installVarDefaults(dialogMenuItem *self)
     variable_set2(VAR_CPIO_VERBOSITY,		"high");
     variable_set2(VAR_TAPE_BLOCKSIZE,		DEFAULT_TAPE_BLOCKSIZE);
     variable_set2(VAR_INSTALL_ROOT,		"/");
+    variable_set2(VAR_INSTALL_CFG,		"install.cfg");
     cp = getenv("EDITOR");
     if (!cp)
 	cp = "/usr/bin/ee";
