1f73d5d9e27f25fe02502ea4040d12374acd3a99
Alexander Langer <alex@FreeBSD.org>
1997-06-21 16:18:56 +0000

    Merge from current: block write ops to /proc/1/* when securelevel > 0.

--- sys/miscfs/procfs/procfs_subr.c
+++ sys/miscfs/procfs/procfs_subr.c
@@ -36,7 +36,7 @@
  *
  *	@(#)procfs_subr.c	8.4 (Berkeley) 1/27/94
  *
- *	$Id: procfs_subr.c,v 1.9 1996/07/02 13:38:10 dyson Exp $
+ *	$Id: procfs_subr.c,v 1.10 1996/08/31 16:52:39 bde Exp $
  */
 
 #include <sys/param.h>
@@ -236,6 +236,8 @@ procfs_rw(ap)
 	p = PFIND(pfs->pfs_pid);
 	if (p == 0)
 		return (EINVAL);
+	if (p->p_pid == 1 && securelevel > 0 && uio->uio_rw == UIO_WRITE)
+		return(EACCES);
 
 	while (pfs->pfs_lockowner) {
 		tsleep(&pfs->pfs_lockowner, PRIBIO, "pfslck", 0);
