4855e3f1007483fcb409a76c56db23b97ab05486
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-06-21 15:42:17 +0000

    New build scripts for these distributions.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@26762 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ release/scripts/manpages-make.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Move all the manpages out to their own dist, using the bin dist as a
+# starting point.
+if [ -d ${RD}/trees/bin/usr/share/man ]; then
+	( cd ${RD}/trees/bin/usr/share/man;
+	find . | cpio -dumpl ${RD}/trees/manpages/usr/share/man > /dev/null 2>&1) &&
+	rm -rf ${RD}/trees/bin/usr/share/man;
+fi
