bfa89ce6167d5a441857d74731b62966f91881d8
gibbs <gibbs@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-06-27 19:38:56 +0000

    Modify my copyright notice to allow the sequencer to be used with GPLed software (aka Linux).
    Fix a few bugs in the sequencer assembler.
    
    Make it easy to compiler the assembler with debugging turned on.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@26997 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/aic7xxx/aicasm.h
+++ sys/dev/aic7xxx/aicasm.h
@@ -8,8 +8,8 @@
  * modification, are permitted provided that the following conditions
  * are met:
  * 1. Redistributions of source code must retain the above copyright
- *    notice immediately at the beginning of the file, without modification,
- *    this list of conditions, and the following disclaimer.
+ *    notice, this list of conditions, and the following disclaimer,
+ *    without modification, immediately at the beginning of the file.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *      $Id: aic7xxx_asm.h,v 1.1.2.1 1997/03/16 07:21:32 gibbs Exp $
+ *      $Id: aic7xxx_asm.h,v 1.2 1997/03/16 07:28:30 gibbs Exp $
  */
 
 #include <sys/queue.h>
--- sys/dev/aic7xxx/aicasm/aicasm_symbol.h
+++ sys/dev/aic7xxx/aicasm/aicasm_symbol.h
@@ -8,8 +8,8 @@
  * modification, are permitted provided that the following conditions
  * are met:
  * 1. Redistributions of source code must retain the above copyright
- *    notice immediately at the beginning of the file, without modification,
- *    this list of conditions, and the following disclaimer.
+ *    notice, this list of conditions, and the following disclaimer,
+ *    without modification, immediately at the beginning of the file.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *      $Id$
+ *      $Id: symbol.h,v 1.1 1997/03/16 07:08:19 gibbs Exp $
  */
 
 #include <sys/queue.h>
