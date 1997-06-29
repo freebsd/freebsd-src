75fe8f2ef87e636e3d49e7b77ea387b603dbe2f5
Paul Traina <pst@FreeBSD.org>
1997-06-29 06:06:10 +0000

    Maybe this time's the charm?

--- release/scripts/dict-make.sh
+++ release/scripts/dict-make.sh
@@ -7,11 +7,9 @@ if [ -d ${RD}/trees/bin/usr/share/dict ]; then
 	rm -rf ${RD}/trees/bin/usr/share/dict;
 fi
 
-mkdir ${RD}/trees/dict/usr/share/misc
-
 for i in airport birthtoken flowers inter.phone iso3166 na.phone zipcodes; do
 	if [ -f ${RD}/trees/bin/usr/share/misc/$i ]; then
-		mv ${RD}/trees/bin/usr/share/misc/$i
+		mv ${RD}/trees/bin/usr/share/misc/$i \
 			${RD}/trees/dict/usr/share/misc;
 	fi;
 done
