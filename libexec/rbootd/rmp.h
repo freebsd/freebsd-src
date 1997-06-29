4adc504d63118b7c6fcce900ee44b35e9e55ae0b
steve <steve@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-06-29 18:50:34 +0000

    This commit was generated by cvs2svn to compensate for changes in r27074, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@27075 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- libexec/rbootd/rmp.h
+++ libexec/rbootd/rmp.h
@@ -1,3 +1,5 @@
+/*	$NetBSD: rmp.h,v 1.4 1995/10/06 05:12:18 thorpej Exp $	*/
+
 /*
  * Copyright (c) 1988, 1992 The University of Utah and the Center
  *	for Software Science (CSS).
@@ -38,9 +40,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)rmp.h	8.1 (Berkeley) 6/4/93
+ *	from: @(#)rmp.h	8.1 (Berkeley) 6/4/93
  *
- * Utah $Hdr: rmp.h 3.1 92/07/06$
+ * From: Utah Hdr: rmp.h 3.1 92/07/06
  * Author: Jeff Forys, University of Utah CSS
  */
 
@@ -75,21 +77,21 @@
  */
 
 struct hp_hdr {
-	u_char	daddr[RMP_ADDRLEN];
-	u_char	saddr[RMP_ADDRLEN];
-	u_short	len;
+	u_int8_t	daddr[RMP_ADDRLEN];
+	u_int8_t	saddr[RMP_ADDRLEN];
+	u_int16_t	len;
 };
 
 /*
  * HP uses 802.2 LLC with their own local extensions.  This struct makes
- * sence out of this data (encapsulated in the above 802.3 packet).
+ * sense out of this data (encapsulated in the above 802.3 packet).
  */
 
 struct hp_llc {
-	u_char	dsap;		/* 802.2 DSAP */
-	u_char	ssap;		/* 802.2 SSAP */
-	u_short	cntrl;		/* 802.2 control field */
-	u_short	filler;		/* HP filler (must be zero) */
-	u_short	dxsap;		/* HP extended DSAP */
-	u_short	sxsap;		/* HP extended SSAP */
+	u_int8_t	dsap;		/* 802.2 DSAP */
+	u_int8_t	ssap;		/* 802.2 SSAP */
+	u_int16_t	cntrl;		/* 802.2 control field */
+	u_int16_t	filler;		/* HP filler (must be zero) */
+	u_int16_t	dxsap;		/* HP extended DSAP */
+	u_int16_t	sxsap;		/* HP extended SSAP */
 };
