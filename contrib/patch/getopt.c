e6efd720f1962b60f37309c54e5869c433a2503b
steve <steve@FreeBSD.org>
1997-06-29 06:56:24 +0000

    -Wall clean.

--- contrib/patch/getopt.c
+++ contrib/patch/getopt.c
@@ -43,6 +43,7 @@ USA.  */
 #endif
 
 #include <stdio.h>
+#include <string.h>
 
 /* Comment out all this code if we are using the GNU C Library, and are not
    actually compiling the library itself.  This code is part of the GNU C
