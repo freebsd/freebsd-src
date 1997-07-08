5d20d6843cba6251a9a060a75543e956799e2eb2
Brian Somers <brian@FreeBSD.org>
1997-07-08 13:38:58 +0000

    Remove useless comparison.
    Suggested by:	Kazutaka YOKOTA <yokota@zodiac.mech.utsunomiya-u.ac.jp>

--- sys/isa/syscons.c
+++ sys/isa/syscons.c
@@ -25,7 +25,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *  $Id: syscons.c,v 1.220 1997/06/30 13:31:49 yokota Exp $
+ *  $Id: syscons.c,v 1.221 1997/07/07 13:42:42 brian Exp $
  */
 
 #include "sc.h"
@@ -3129,8 +3129,7 @@ next_code:
 				       (cur_console->xsize*i),
 				       cur_console->xsize * sizeof(u_short));
 				ptr += cur_console->xsize;
-				if (cur_console->history &&
-				    ptr + cur_console->xsize >
+				if (ptr + cur_console->xsize >
 				    cur_console->history +
 				    cur_console->history_size)
 				    ptr = cur_console->history;
