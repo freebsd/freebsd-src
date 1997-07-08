6df0d99c483fab08579bb152f2b668fa397ddc67
pst <pst@FreeBSD.org>
1997-07-08 00:46:32 +0000

    iso codes need to be in bin distribution for tzsetup

--- release/scripts/dict-make.sh
+++ release/scripts/dict-make.sh
@@ -7,7 +7,7 @@ if [ -d ${RD}/trees/bin/usr/share/dict ]; then
 	rm -rf ${RD}/trees/bin/usr/share/dict;
 fi
 
-for i in airport birthtoken flowers inter.phone iso3166 na.phone zipcodes; do
+for i in airport birthtoken flowers inter.phone na.phone zipcodes; do
 	if [ -f ${RD}/trees/bin/usr/share/misc/$i ]; then
 		mv ${RD}/trees/bin/usr/share/misc/$i \
 			${RD}/trees/dict/usr/share/misc;
