b01d93dc4cf4601f68a8a6a464a6db955329c733
Philippe Charnier <charnier@FreeBSD.org>
1997-07-11 06:23:33 +0000

    MFC: Use err(3) instead of local redefinition.

--- usr.bin/head/head.c
+++ usr.bin/head/head.c
@@ -32,21 +32,25 @@
  */
 
 #ifndef lint
-static char copyright[] =
+static const char copyright[] =
 "@(#) Copyright (c) 1980, 1987, 1992, 1993\n\
 	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
+#if 0
 static char sccsid[] = "@(#)head.c	8.1 (Berkeley) 6/6/93";
+#endif
+static const char rcsid[] =
+	"$Id$";
 #endif /* not lint */
 
 #include <sys/types.h>
-#include <errno.h>
+#include <ctype.h>
+#include <err.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <ctype.h>
 
 /*
  * head - give the first few lines of a stream or of each of a set of files
@@ -54,7 +58,6 @@ static char sccsid[] = "@(#)head.c	8.1 (Berkeley) 6/6/93";
  * Bill Joy UCB August 24, 1977
  */
 
-void err __P((int, const char *, ...));
 void head __P((FILE *, int));
 void obsolete __P((char *[]));
 void usage __P((void));
@@ -78,7 +81,7 @@ main(argc, argv)
 		case 'n':
 			linecnt = strtol(optarg, &ep, 10);
 			if (*ep || linecnt <= 0)
-				err(1, "illegal line count -- %s", optarg);
+				errx(1, "illegal line count -- %s", optarg);
 			break;
 		case '?':
 		default:
@@ -90,7 +93,8 @@ main(argc, argv)
 	if (*argv)
 		for (first = 1; *argv; ++argv) {
 			if ((fp = fopen(*argv, "r")) == NULL) {
-				err(0, "%s: %s", *argv, strerror(errno));
+				warn("%s", *argv);
+				eval = 1;
 				continue;
 			}
 			if (argc > 1) {
@@ -115,7 +119,7 @@ head(fp, cnt)
 
 	while (cnt && (ch = getc(fp)) != EOF) {
 			if (putchar(ch) == EOF)
-				err(1, "stdout: %s", strerror(errno));
+				err(1, "stdout");
 			if (ch == '\n')
 				cnt--;
 		}
@@ -132,7 +136,7 @@ obsolete(argv)
 		if (ap[0] != '-' || ap[1] == '-' || !isdigit(ap[1]))
 			return;
 		if ((ap = malloc(strlen(*argv) + 2)) == NULL)
-			err(1, "%s", strerror(errno));
+			err(1, NULL);
 		ap[0] = '-';
 		ap[1] = 'n';
 		(void)strcpy(ap + 2, *argv + 1);
@@ -145,35 +149,4 @@ usage()
 {
 	(void)fprintf(stderr, "usage: head [-n lines] [file ...]\n");
 	exit(1);
-}
-
-#if __STDC__
-#include <stdarg.h>
-#else
-#include <varargs.h>
-#endif
-
-void
-#if __STDC__
-err(int fatal, const char *fmt, ...)
-#else
-err(fatal, fmt, va_alist)
-	int fatal;
-	char *fmt;
-        va_dcl
-#endif
-{
-	va_list ap;
-#if __STDC__
-	va_start(ap, fmt);
-#else
-	va_start(ap);
-#endif
-	(void)fprintf(stderr, "head: ");
-	(void)vfprintf(stderr, fmt, ap);
-	va_end(ap);
-	(void)fprintf(stderr, "\n");
-	if (fatal)
-		exit(1);
-	eval = 1;
 }
