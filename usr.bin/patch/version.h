65b3003d2db57c7e7f521f5f091ed7adbc151768
Peter Wemm <peter@FreeBSD.org>
1997-07-13 14:26:00 +0000

    kill the undead

--- usr.bin/patch/version.h
+++
@@ -1,9 +0,0 @@
-/* $Header: version.h,v 2.0 86/09/17 15:40:14 lwall Exp $
- *
- * $Log:	version.h,v $
- * Revision 2.0  86/09/17  15:40:14  lwall
- * Baseline for netwide release.
- * 
- */
-
-void version();
