286654a823b1f247ecea71687004ba165874b9fd
jfieber <jfieber@FreeBSD.org>
1997-07-19 15:19:08 +0000

    Remove SGML tools from -current.

--- usr.bin/sgmls/sgmls/alloc.h
+++
@@ -1,8 +0,0 @@
-/* alloc.h */
-
-typedef unsigned SIZE_T;
-
-/* Like malloc and realloc, but don't return if no memory is available. */
-
-extern UNIV xmalloc P((SIZE_T));
-extern UNIV xrealloc P((UNIV, SIZE_T));
