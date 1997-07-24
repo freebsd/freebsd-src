9edce0605af4ded170746dd3581f482142acde20
Steve Passe <fsmp@FreeBSD.org>
1997-07-24 18:05:22 +0000

    Fix a difference between the declaration & definition of isa_dmastatus().
    Submitted by:	Chuck Robey <chuckr@glue.umd.edu>

--- sys/i386/isa/isa_device.h
+++ sys/i386/isa/isa_device.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)isa_device.h	7.1 (Berkeley) 5/9/91
- *	$Id: isa_device.h,v 1.43 1997/07/20 12:34:15 bde Exp $
+ *	$Id: isa_device.h,v 1.44 1997/07/24 05:27:40 msmith Exp $
  */
 
 #ifndef _I386_ISA_ISA_DEVICE_H_
@@ -117,7 +117,7 @@ void	isa_dmainit __P((int chan, u_int bouncebufsize));
 void	isa_dmastart __P((int flags, caddr_t addr, u_int nbytes, int chan));
 int	isa_dma_acquire __P((int chan));
 void	isa_dma_release __P((int chan));
-void	isa_dmastatus __P((int chan));
+int	isa_dmastatus __P((int chan));
 void	reconfig_isadev __P((struct isa_device *isdp, u_int *mp));
 
 #endif /* KERNEL */
