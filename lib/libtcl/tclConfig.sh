7d889d046ac4424ed43c21e48c2ad11872c3d7c8
phk <phk@FreeBSD.org>
1997-07-25 19:51:45 +0000

    Tada!, tcl is now at 8.0b2 I belive.

--- lib/libtcl/tclConfig.sh
+++ lib/libtcl/tclConfig.sh
@@ -9,21 +9,22 @@
 #
 # The information in this file is specific to a single platform.
 #
-# SCCS: @(#) tclConfig.sh.in 1.15 96/04/17 10:46:27
+# SCCS: @(#) tclConfig.sh.in 1.19 96/12/17 09:08:29
 
 # Tcl's version number.
-TCL_VERSION='7.5'
-TCL_MAJOR_VERSION='7'
-TCL_MINOR_VERSION='5'
+TCL_VERSION='8.0'
+TCL_MAJOR_VERSION='8'
+TCL_MINOR_VERSION='0'
+TCL_PATCH_LEVEL='b2'
 
 # C compiler to use for compilation.
 TCL_CC='cc'
 
 # -D flags for use with the C compiler.
-TCL_DEFS=' -DHAVE_UNISTD_H=1 -DHAVE_SYS_TIME_H=1 -DTIME_WITH_SYS_TIME=1 -DHAVE_TM_ZONE=1 -DHAVE_TM_GMTOFF=1 -DSTDC_HEADERS=1 -DNEED_MATHERR=1 '
+TCL_DEFS=' -DNO_VALUES_H=1 -DHAVE_UNISTD_H=1 -DUSE_TERMIOS=1 -DHAVE_SYS_TIME_H=1 -DTIME_WITH_SYS_TIME=1 -DHAVE_TM_ZONE=1 -DHAVE_TM_GMTOFF=1 -DSTDC_HEADERS=1 -DNEED_MATHERR=1 -DHAVE_SIGNED_CHAR=1 -DHAVE_SYS_IOCTL_H=1 -DHAVE_SYS_FILIO_H=1 '
 
 # The name of the Tcl library (may be either a .a file or a shared library):
-TCL_LIB_FILE=libtcl75.so.1.1
+TCL_LIB_FILE=libtcl80.so.1.2
 
 # Additional libraries to use when linking Tcl.
 TCL_LIBS='  -lm'
@@ -37,7 +38,7 @@ TCL_PREFIX='/usr'
 TCL_EXEC_PREFIX='/usr'
 
 # Flags to pass to cc when compiling the components of a shared library:
-TCL_SHLIB_CFLAGS='-fPIC'
+TCL_SHLIB_CFLAGS='-fpic'
 
 # Base command to use for combining object files into a shared library:
 TCL_SHLIB_LD='ld -Bshareable -x'
@@ -61,7 +62,7 @@ TCL_LD_FLAGS=''
 # Flags to pass to ld, such as "-R /usr/local/tcl/lib", that tell the
 # run-time dynamic linker where to look for shared libraries such as
 # libtcl.so.  Used when linking applications.  Only works if there
-# is a variable "LIB_INSTALL_DIR" defined in the Makefile.
+# is a variable "LIB_RUNTIME_DIR" defined in the Makefile.
 TCL_LD_SEARCH_FLAGS=''
 
 # Additional object files linked with Tcl to provide compatibility
@@ -73,11 +74,11 @@ TCL_RANLIB='ranlib'
 
 # String to pass to linker to pick up the Tcl library from its
 # build directory.
-TCL_BUILD_LIB_SPEC='-ltcl75'
+TCL_BUILD_LIB_SPEC='-ltcl80'
 
 # String to pass to linker to pick up the Tcl library from its
 # installed directory.
-TCL_LIB_SPEC='-ltcl75'
+TCL_LIB_SPEC='-ltcl80'
 
 # Indicates whether a version numbers should be used in -l switches
 # ("ok" means it's safe to use switches like -ltcl7.5;  "nodots" means
@@ -97,3 +98,16 @@ TCL_SHARED_LIB_SUFFIX='`echo ${VERSION} | tr -d .`.so.1.0'
 # extension, and anything else needed).  May depend on the variable
 # VERSION.  On most UNIX systems this is ${VERSION}.a.
 TCL_UNSHARED_LIB_SUFFIX='`echo ${VERSION} | tr -d .`.a'
+
+# Location of the top-level source directory from which Tcl was built.
+# This is the directory that contains a README file as well as
+# subdirectories such as generic, unix, etc.  If Tcl was compiled in a
+# different place than the directory containing the source files, this
+# points to the location of the sources, not the location where Tcl was
+# compiled.
+TCL_SRC_DIR='/c/phk/cvs/src/contrib/tcl'
+
+# List of standard directories in which to look for packages during
+# "package require" commands.  Contains the "prefix" directory plus also
+# the "exec_prefix" directory, if it is different.
+TCL_PACKAGE_PATH='/usr/local/lib/tclpkg'
