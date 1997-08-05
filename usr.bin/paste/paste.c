72c116fc1cc596a8341091306da0dbd9bf4810ac
asami <asami@FreeBSD.org>
1997-08-05 03:40:23 +0000

    Merge 1.4.

--- usr.bin/paste/paste.c
+++ usr.bin/paste/paste.c
@@ -45,7 +45,7 @@ static const char copyright[] =
 static char sccsid[] = "@(#)paste.c	8.1 (Berkeley) 6/6/93";
 #endif
 static const char rcsid[] =
-	"$Id$";
+	"$Id: paste.c,v 1.1.1.1.8.1 1997/08/01 06:44:23 charnier Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -116,7 +116,7 @@ parallel(argv)
 	register char ch, *p;
 	LIST *head, *tmp;
 	int opencnt, output;
-	char buf[_POSIX2_LINE_MAX + 1], *malloc();
+	char buf[_POSIX2_LINE_MAX + 1];
 
 	for (cnt = 0, head = NULL; (p = *argv); ++argv, ++cnt) {
 		if (!(lp = (LIST *)malloc((u_int)sizeof(LIST))))
