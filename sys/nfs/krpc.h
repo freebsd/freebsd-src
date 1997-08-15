46d277a7d01c7cc6926f6cac5ffba79e6766aafe
tegge <tegge@FreeBSD.org>
1997-08-15 13:33:21 +0000

    MFC: Remove unused routines.

--- sys/nfs/krpc.h
+++ sys/nfs/krpc.h
@@ -1,5 +1,5 @@
 /*	$NetBSD: krpc.h,v 1.4 1995/12/19 23:07:11 cgd Exp $	*/
-/*	$Id:$	*/
+/*	$Id: krpc.h,v 1.1.4.1 1997/05/11 18:01:25 tegge Exp $	*/
 
 #include <sys/cdefs.h>
 
@@ -11,10 +11,6 @@ int krpc_portmap __P((struct sockaddr_in *sin,
 	u_int prog, u_int vers, u_int16_t *portp, struct proc *procp));
 
 struct mbuf *xdr_string_encode __P((char *str, int len));
-struct mbuf *xdr_string_decode __P((struct mbuf *m, char *str, int *len_p));
-struct mbuf *xdr_inaddr_encode __P((struct in_addr *ia));
-struct mbuf *xdr_inaddr_decode __P((struct mbuf *m, struct in_addr *ia));
-
 
 /*
  * RPC definitions for the portmapper
