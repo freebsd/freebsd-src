1ba1e07209761ad45fbc92ca09b9aa3f09e51c95
peter <peter@FreeBSD.org>
1997-08-22 12:03:57 +0000

    Update to pppd-2.3.1, except for the CLOCAL changes while running the connect script to main.c that can cause problems on bsd-style tty systems.

--- usr.sbin/pppd/patchlevel.h
+++ usr.sbin/pppd/patchlevel.h
@@ -1,6 +1,6 @@
-/* $Id$ */
-#define	PATCHLEVEL	0
+/* $Id: patchlevel.h,v 1.6 1997/08/19 17:52:44 peter Exp $ */
+#define	PATCHLEVEL	1
 
 #define VERSION		"2.3"
 #define IMPLEMENTATION	""
-#define DATE		"22 May 97"
+#define DATE		"27 June 97"
