c7364ca6cceb97f48969841547f9640d6730bb6a
imp <imp@FreeBSD.org>
1997-08-25 05:02:54 +0000

    MFC: style change MAXPATHLEN->PATH_MAX and also buffer overflow fix.

--- usr.bin/tip/tip/tipout.c
+++ usr.bin/tip/tip/tipout.c
@@ -36,7 +36,7 @@
 static char sccsid[] = "@(#)tipout.c	8.1 (Berkeley) 6/6/93";
 #endif
 static const char rcsid[] =
-	"$Id$";
+	"$Id: tipout.c,v 1.3.2.1 1997/08/20 07:06:48 charnier Exp $";
 #endif /* not lint */
 
 #include "tip.h"
@@ -74,7 +74,7 @@ intEMT()
 	char reply;
 
 	read(fildes[0], &c, 1);
-	while (c != '\n') {
+	while (c != '\n' && pline - line < sizeof(line)) {
 		*pline++ = c;
 		read(fildes[0], &c, 1);
 	}
