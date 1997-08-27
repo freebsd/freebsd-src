3fa69daee8bd639012dbd9d6b15b9c93b4e1a79b
Warner Losh <imp@FreeBSD.org>
1997-08-27 05:27:53 +0000

    Don't getenv(HOME) when set[ug]od.  This can lead to a buffer overflow and elevated privs. Obtained from: OpenBSD

--- lib/libc/gen/glob.c
+++ lib/libc/gen/glob.c
@@ -358,10 +358,11 @@ globtilde(pattern, patbuf, patbuf_len, pglob)
 
 	if (((char *) patbuf)[0] == EOS) {
 		/*
-		 * handle a plain ~ or ~/ by expanding $HOME
-		 * first and then trying the password file
+		 * handle a plain ~ or ~/ by expanding $HOME first (iff
+		 * we're not running setuid or setgid) and then trying
+		 * the password file
 		 */
-		if ((h = getenv("HOME")) == NULL) {
+		if (issetugid() != 0 || (h = getenv("HOME")) == NULL) {
 			if ((pwd = getpwuid(getuid())) == NULL)
 				return pattern;
 			else
