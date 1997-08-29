908c7a28f99afdc925229cce29f01334f6c373a5
kato <kato@FreeBSD.org>
1997-08-29 14:53:18 +0000

    Synchronize with sys/i386/i386/userconfig.c revision 1.90.

--- sys/pc98/i386/userconfig.c
+++ sys/pc98/i386/userconfig.c
@@ -46,7 +46,7 @@
  ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **
- **      $Id: userconfig.c,v 1.32 1997/08/17 03:38:52 kato Exp $
+ **      $Id: userconfig.c,v 1.33 1997/08/22 08:12:06 kato Exp $
  **/
 
 /**
@@ -2374,7 +2374,7 @@ visuserconfig(void)
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *      $Id: userconfig.c,v 1.32 1997/08/17 03:38:52 kato Exp $
+ *      $Id: userconfig.c,v 1.33 1997/08/22 08:12:06 kato Exp $
  */
 
 #include "scbus.h"
@@ -3140,7 +3140,7 @@ lsscsi(void)
 
     for (i = 0; scsi_cinit[i].driver; i++)
     {
-	id_put("controller scbus", scsi_cinit[i].bus);
+	id_put("controller scbus", scsi_cinit[i].scbus);
 
 	if (scsi_cinit[i].unit != -1)
 	{
@@ -3161,7 +3161,7 @@ lsscsi(void)
 		id_put(" lun ", scsi_dinit[i].lun);
 
 		if (scsi_dinit[i].flags)
-	    	printf("flags 0x%x\n", scsi_dinit[i].flags);
+	    	printf(" flags 0x%x\n", scsi_dinit[i].flags);
 
 		printf("\n");
     }
