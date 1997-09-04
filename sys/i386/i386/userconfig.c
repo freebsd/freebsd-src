8b442691e8b3ad95adf0dd69a62d1a1d06fb9bdb
Jordan K. Hubbard <jkh@FreeBSD.org>
1997-09-04 09:01:39 +0000

    MFC: spelling bogon

--- sys/i386/i386/userconfig.c
+++ sys/i386/i386/userconfig.c
@@ -46,7 +46,7 @@
  ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **
- **      $Id: userconfig.c,v 1.63.2.18 1997/08/11 13:26:19 jkh Exp $
+ **      $Id: userconfig.c,v 1.63.2.19 1997/08/29 14:50:36 kato Exp $
  **/
 
 /**
@@ -1874,8 +1874,8 @@ static char *helptext[] =
     "Use the [!bTAB!n] key to toggle between the Active and Inactive list; if",
     "you need to move a driver from the Inactive list back to the Active",
     "one, select it in the Inactive list, using [!bTAB!n] to change lists if",
-    "necessary, and press [!bRETURN!n] -- the device will me moved to its",
-    "category in the Active list.",
+    "necessary, and press [!bRETURN!n] -- the device will be moved back to",
+    "its place in the Active list.",
     "",
     "- - Altering the list/parameters -",
     "",
@@ -2358,7 +2358,7 @@ visuserconfig(void)
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *      $Id: userconfig.c,v 1.63.2.18 1997/08/11 13:26:19 jkh Exp $
+ *      $Id: userconfig.c,v 1.63.2.19 1997/08/29 14:50:36 kato Exp $
  */
 
 #include "scbus.h"
