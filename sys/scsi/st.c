3073a9f59dc4c96b108f924a63173da02ec091c4
joerg <joerg@FreeBSD.org>
1997-09-07 10:08:23 +0000

    Silence the PREVENT ALLOW MEDIUM REMOVAL call.
    Submitted by:	jmattson@wco.com (Jim Mattson)

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -12,7 +12,7 @@
  * on the understanding that TFS is not responsible for the correct
  * functioning of this software in any circumstances.
  *
- * $Id: st.c,v 1.79 1997/08/23 21:05:32 steve Exp $
+ * $Id: st.c,v 1.80 1997/09/02 20:06:38 bde Exp $
  */
 
 /*
@@ -427,7 +427,8 @@ struct scsi_link *sc_link)
 	if ((flags & O_ACCMODE) == FWRITE)
 		st->flags |= ST_WRITTEN;
 
-	scsi_prevent(sc_link, PR_PREVENT, 0);	/* who cares if it fails? */
+	/* PREVENT ALLOW MEDIUM REMOVAL is optional per the SCSI-2 specs */
+	scsi_prevent(sc_link, PR_PREVENT, SCSI_SILENT | SCSI_ERR_OK);
 
 	SC_DEBUG(sc_link, SDEV_DB2, ("Open complete\n"));
 
