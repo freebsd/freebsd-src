188cbc1fc9561979233cfbdd493aa1f361f36101
jkh <jkh@FreeBSD.org>
1997-09-09 16:28:25 +0000

    MFC: XFree86 3.3.1 update and shorter message dialog title.

--- release/sysinstall/msg.c
+++ release/sysinstall/msg.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: msg.c,v 1.41.2.1 1996/12/09 09:29:05 jkh Exp $
+ * $Id: msg.c,v 1.41.2.2 1997/04/06 18:00:00 jkh Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -214,7 +214,7 @@ msgNotify(char *fmt, ...)
     if (isDebug())
 	msgDebug("Notify: %s\n", errstr);
     dialog_clear_norefresh();
-    dialog_msgbox("Information Dialog", errstr, -1, -1, 0);
+    dialog_msgbox(" ", errstr, -1, -1, 0);
 }
 
 /* Put up a message in a popup yes/no box and return 1 for YES, 0 for NO */
@@ -301,7 +301,7 @@ msgWeHaveOutput(char *fmt, ...)
     use_helpfile(NULL);
     msgDebug("Notify: %s\n", errstr);
     dialog_clear_norefresh();
-    dialog_msgbox("Information Dialog", errstr, -1, -1, 0);
+    dialog_msgbox(" ", errstr, -1, -1, 0);
 }
 
 /* Simple versions of msgConfirm() and msgNotify() for calling from scripts */
