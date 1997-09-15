c263309d70e97d23d459bb2ba4c1e2036f7784e2
ache <ache@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-09-15 02:03:13 +0000

    Sense *.ISO_8859-1 and *.KOI8-R locales from environment PR: 4290 Submitted by: "Anatoly A. Orehovsky" <tolik@www.tomsk.su>
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@29438 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/groff/eqn/neqn.sh
+++ contrib/groff/eqn/neqn.sh
@@ -2,4 +2,19 @@
 # Provision of this shell script should not be taken to imply that use of
 # GNU eqn with groff -Tascii|-Tlatin1 is supported.
 
-exec @g@eqn -Tascii ${1+"$@"}
+# Default device.
+if test `expr "$LC_CTYPE" : ".*\.ISO_8859-1"` -gt 0 || \
+   test `expr "$LANG" : ".*\.ISO_8859-1"` -gt 0
+then
+	T=-Tlatin1
+else
+if test `expr "$LC_CTYPE" : ".*\.KOI8-R"` -gt 0 || \
+   test `expr "$LANG" : ".*\.KOI8-R"` -gt 0
+then
+	T=-Tkoi8-r
+else
+	T=-Tascii
+fi
+fi
+
+exec @g@eqn -T${T} ${1+"$@"}
