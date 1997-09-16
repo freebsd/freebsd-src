53194477cee6503aef7a689552bc7244837ec715
dg <dg@FreeBSD.org>
1997-09-16 10:45:05 +0000

    Bring in changes from revs 1.262 & 1.263: speculative memory probe for machines with >64MB of RAM.

--- sys/i386/i386/machdep.c
+++ sys/i386/i386/machdep.c
@@ -35,7 +35,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91
- *	$Id: machdep.c,v 1.209.2.10 1997/04/03 06:37:31 davidg Exp $
+ *	$Id: machdep.c,v 1.209.2.11 1997/06/20 10:27:35 kato Exp $
  */
 
 #include "npx.h"
@@ -173,7 +173,6 @@ SYSCTL_PROC(_hw, HW_USERMEM, usermem, CTLTYPE_INT|CTLFLAG_RD,
 	0, 0, sysctl_hw_usermem, "I", "");
 
 int boothowto = 0, bootverbose = 0, Maxmem = 0;
-static int	badpages = 0;
 long dumplo;
 extern int bootdev;
 
@@ -220,17 +219,11 @@ cpu_startup(dummy)
 	/*
 	 * Display any holes after the first chunk of extended memory.
 	 */
-	if (badpages != 0) {
-		int indx = 1;
+	if (bootverbose) {
+		int indx;
 
-		/*
-		 * XXX skip reporting ISA hole & unmanaged kernel memory
-		 */
-		if (phys_avail[0] == PAGE_SIZE)
-			indx += 2;
-
-		printf("Physical memory hole(s):\n");
-		for (; phys_avail[indx + 1] != 0; indx += 2) {
+		printf("Physical memory chunk(s):\n");
+		for (indx = 0; phys_avail[indx + 1] != 0; indx += 2) {
 			int size = phys_avail[indx + 1] - phys_avail[indx];
 
 			printf("0x%08lx - 0x%08lx, %d bytes (%d pages)\n", phys_avail[indx],
@@ -960,9 +953,10 @@ init386(first)
 	struct isa_device *idp;
 	/* table descriptors - used to load tables by microp */
 	struct region_descriptor r_gdt, r_idt;
-	int	pagesinbase, pagesinext;
-	int	target_page, pa_indx;
-	int	off;
+	int pagesinbase, pagesinext;
+	int target_page, pa_indx;
+	int off;
+	int speculative_mprobe;
 
 	proc0.p_addr = proc0paddr;
 
@@ -1160,15 +1154,30 @@ init386(first)
 	 * called something like "Maxphyspage".
 	 */
 	Maxmem = pagesinext + 0x100000/PAGE_SIZE;
+	/*
+	 * Indicate that we wish to do a speculative search for memory beyond
+	 * the end of the reported size if the indicated amount is 64MB (0x4000
+	 * pages) - which is the largest amount that the BIOS/bootblocks can
+	 * currently report. If a specific amount of memory is indicated via
+	 * the MAXMEM option or the npx0 "msize", then don't do the speculative
+	 * memory probe.
+	 */
+	if (Maxmem == 0x4000)
+		speculative_mprobe = TRUE;
+	else
+		speculative_mprobe = FALSE;
 
 #ifdef MAXMEM
 	Maxmem = MAXMEM/4;
+	speculative_mprobe = FALSE;
 #endif
 
 #if NNPX > 0
 	idp = find_isadev(isa_devtab_null, &npxdriver, 0);
-	if (idp != NULL && idp->id_msize != 0)
+	if (idp != NULL && idp->id_msize != 0) {
 		Maxmem = idp->id_msize / 4;
+		speculative_mprobe = FALSE;
+	}
 #endif
 
 	/* call pmap initialization to make new kernel address space */
@@ -1183,7 +1192,6 @@ init386(first)
 	 * XXX  ...but we probably should.
 	 */
 	pa_indx = 0;
-	badpages = 0;
 	if (pagesinbase > 1) {
 		phys_avail[pa_indx++] = PAGE_SIZE;	/* skip first page of memory */
 		phys_avail[pa_indx] = ptoa(pagesinbase);/* memory up to the ISA hole */
@@ -1194,7 +1202,9 @@ init386(first)
 	}
 
 	for (target_page = avail_start; target_page < ptoa(Maxmem); target_page += PAGE_SIZE) {
-		int tmp, page_bad = FALSE;
+		int tmp, page_bad;
+
+		page_bad = FALSE;
 
 		/*
 		 * map page into kernel: valid, read/write, non-cacheable
@@ -1249,9 +1259,16 @@ init386(first)
 			 * the end pointer. Otherwise start a new chunk.
 			 * Note that "end" points one higher than end,
 			 * making the range >= start and < end.
+			 * If we're also doing a speculative memory
+			 * test and we at or past the end, bump up Maxmem
+			 * so that we keep going. The first bad page
+			 * will terminate the loop.
 			 */
 			if (phys_avail[pa_indx] == target_page) {
 				phys_avail[pa_indx] += PAGE_SIZE;
+				if (speculative_mprobe == TRUE &&
+				    phys_avail[pa_indx] >= (64*1024*1024))
+					Maxmem++;
 			} else {
 				pa_indx++;
 				if (pa_indx == PHYS_AVAIL_ARRAY_END) {
@@ -1263,9 +1280,6 @@ init386(first)
 				phys_avail[pa_indx] = target_page + PAGE_SIZE;	/* end */
 			}
 			physmem++;
-		} else {
-			badpages++;
-			page_bad = FALSE;
 		}
 	}
 
