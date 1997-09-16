faf89e983bbde37d6782b411763276507d747419
charnier <charnier@FreeBSD.org>
1997-09-16 06:57:11 +0000

    MFC: Use err(3), add usage(). -Wall clean. Police from style.9 and Bruce.

--- usr.sbin/config/mkheaders.c
+++ usr.sbin/config/mkheaders.c
@@ -32,18 +32,27 @@
  */
 
 #ifndef lint
+#if 0
 static char sccsid[] = "@(#)mkheaders.c	8.1 (Berkeley) 6/6/93";
+#endif
+static const char rcsid[] =
+	"$Id$";
 #endif /* not lint */
 
 /*
  * Make all the .h files for the optional entries
  */
 
-#include <stdio.h>
 #include <ctype.h>
+#include <err.h>
+#include <stdio.h>
 #include "config.h"
 #include "y.tab.h"
 
+void do_header __P((char *, char *, int));
+void do_count __P((char *, char *, int));
+
+void
 headers()
 {
 	register struct file_list *fl;
@@ -57,6 +66,7 @@ headers()
  * count all the devices of a certain type and recurse to count
  * whatever the device is connected to
  */
+void
 do_count(dev, hname, search)
 	register char *dev, *hname;
 	int search;
@@ -96,6 +106,7 @@ do_count(dev, hname, search)
 	do_header(dev, hname, count > hicount ? count : hicount);
 }
 
+void
 do_header(dev, hname, count)
 	char *dev, *hname;
 	int count;
@@ -111,10 +122,8 @@ do_header(dev, hname, count)
 	oldcount = -1;
 	if (inf == 0) {
 		outf = fopen(file, "w");
-		if (outf == 0) {
-			perror(file);
-			exit(1);
-		}
+		if (outf == 0)
+			err(1, "%s", file);
 		fprintf(outf, "#define %s %d\n", name, count);
 		(void) fclose(outf);
 		return;
@@ -163,10 +172,8 @@ do_header(dev, hname, count)
 		fl_head = fl;
 	}
 	outf = fopen(file, "w");
-	if (outf == 0) {
-		perror(file);
-		exit(1);
-	}
+	if (outf == 0)
+		err(1, "%s", file);
 	for (fl = fl_head; fl != NULL; fl = tflp) {
 		fprintf(outf,
 		    "#define %s %u\n", fl->f_fn, count ? fl->f_type : 0);
