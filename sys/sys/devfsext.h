418589bcca9dc426b3bf8bcda7bbf61b065e4f99
bde <bde@FreeBSD.org>
1997-09-16 14:23:35 +0000

    Cleaned up.

--- sys/sys/devfsext.h
+++ sys/sys/devfsext.h
@@ -1,27 +1,16 @@
 /* usual BSD style copyright here */
 /* Written by Julian Elischer (julian@dialix.oz.au)*/
 /*
- * $Id$
+ * $Id: devfsext.h,v 1.15 1997/02/22 09:44:59 peter Exp $
  */
 
 #ifndef _SYS_DEVFSECT_H_
-#define _SYS_DEVFSECT_H_ 1
+#define	_SYS_DEVFSECT_H_
 
-void *devfs_add_devswf(void *devsw,
-		int minor,
-		int chrblk,
-		uid_t uid,
-		gid_t gid,
-		int perms,
-		char *fmt,
-		...)  ; 
-
-void *devfs_link(void *original,  /* the result of a previous dev_link */
-		 char *fmt,	  /*		or dev_add operation */
-		 ...);
-
-/* remove the device the cookie represents */
-void devfs_remove_dev(void *devnmp);
+void	*devfs_add_devswf __P((void *devsw, int minor, int chrblk, uid_t uid,
+			       gid_t gid, int perms, char *fmt, ...)); 
+void	*devfs_link __P((void *original, char *fmt, ...));
+void	devfs_remove_dev __P((void *devnmp));
 
 #define DV_CHR 0
 #define DV_BLK 1
@@ -39,4 +28,4 @@ void devfs_remove_dev(void *devnmp);
 #define	GID_BIN		7
 #define	GID_DIALER	68
 
-#endif /*_SYS_DEVFSECT_H_*/
+#endif /* !_SYS_DEVFSECT_H_ */
