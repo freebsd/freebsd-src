dc9fb7214712036d21e7066d05c5ddd251d1ddd4
tegge <tegge@FreeBSD.org>
1997-09-17 20:16:17 +0000

    Enable the FIFO on enhanced floppy controllers. This reduces the number of dma overruns/underruns for systems under heavy dma load. As a side effect, broken enhanced floppy controllers that sometimes don't detect dma overruns/underruns will give less errors.
    Reviewed by:	j@uriah.heep.sax.de (J Wunsch)

--- sys/dev/ic/nec765.h
+++ sys/dev/ic/nec765.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)nec765.h	7.1 (Berkeley) 5/9/91
- *	$Id$
+ *	$Id: nec765.h,v 1.6 1997/02/22 09:38:04 peter Exp $
  */
 
 /*
@@ -126,6 +126,7 @@
  */
 #define NE7CMD_VERSION	0x10	/*  version (ok for all controllers) */
 
+#define I8207X_CONFIGURE 0x13   /*  configure enhanced features */
 
 /*
  * "specify" definitions
