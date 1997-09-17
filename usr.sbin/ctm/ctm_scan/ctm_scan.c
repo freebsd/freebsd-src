34a6b27326dc91b02abd2d076f78ebe1517503ae
charnier <charnier@FreeBSD.org>
1997-09-17 06:24:58 +0000

    Use err(3). Use An/Aq for author name.

--- usr.sbin/ctm/ctm_scan/ctm_scan.c
+++ usr.sbin/ctm/ctm_scan/ctm_scan.c
@@ -6,13 +6,14 @@
  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
  * ----------------------------------------------------------------------------
  *
- * $Id$
+ * $Id: ctm_scan.c,v 1.15 1997/02/22 16:05:40 peter Exp $
  *
  */
+#include <err.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/file.h>
@@ -45,7 +46,7 @@ Do(char *path)
     npde = 1;
     pde = malloc(sizeof *pde * (npde+1));
     d = opendir(path);
-    if(!d) { perror(path); return 2; }
+    if(!d) { warn("%s", path); return 2; }
     if(!strcmp(path,".")) {
 	*buf = 0;
     } else {
@@ -163,7 +164,7 @@ main(int argc, char **argv)
      */
     if (argc > 1) {
 	if(chdir(argv[1])) {
-	    perror(argv[1]);
+	    warn("%s", argv[1]);
 	    return 2;
 	}
 	argc--;
