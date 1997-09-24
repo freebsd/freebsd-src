293b43f7c244ce3085c66358d438efc625dc7281
ache <ache@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-09-24 21:29:58 +0000

    Revoke properly by setgid, not by setuid, overlooked in games uid->gid change
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@29825 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- games/atc/input.c
+++ games/atc/input.c
@@ -47,6 +47,8 @@
 static char sccsid[] = "@(#)input.c	8.1 (Berkeley) 5/31/93";
 #endif not lint
 
+#include <stdlib.h>
+#include <string.h>
 #include "include.h"
 #include "pathnames.h"
 
@@ -319,9 +321,10 @@ gettoken()
 #endif
 			if (fork() == 0)	/* child */
 			{
-				char *shell, *base, *getenv(), *strrchr();
+				char *shell, *base;
 
-				setuid(getuid()); /* turn off setuid bit */
+				/* revoke */
+				setgid(getgid());
 				done_screen();
 
 						 /* run user's favorite shell */
