ecf0a11fd25aab492cbca3bfb52beca131ab4550
ache <ache@FreeBSD.org>
1997-09-24 06:22:40 +0000

    Use srandomdev

--- games/fortune/strfile/strfile.c
+++ games/fortune/strfile/strfile.c
@@ -429,7 +429,7 @@ void randomize()
 	register long   tmp;
 	register long   *sp;
 
-	srandom((int)(time((time_t *) NULL) ^ getpid()));
+	srandomdev();
 
 	Tbl.str_flags |= STR_RANDOM;
 	cnt = Tbl.str_numstr;
