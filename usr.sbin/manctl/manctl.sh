4ecc64c0ad2f012f04ec9067ca9c63c469ca5114
Philippe Charnier <charnier@FreeBSD.org>
1997-09-29 06:32:24 +0000

    MFC: Stylistic changes in man page.

--- usr.sbin/manctl/manctl.sh
+++ usr.sbin/manctl/manctl.sh
@@ -30,7 +30,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $Id: manctl.sh,v 1.6 1996/01/23 23:59:36 wosch Exp $
+# $Id: manctl.sh,v 1.6.2.1 1996/11/06 09:39:45 phk Exp $
 #
 # manctl: 
 #	a utility for manipulating manual pages
@@ -345,10 +345,10 @@ do_compress_so()
 #
 ctl_usage()
 {
-	echo "usage : 	$1 -compress <path> ... " 1>&2
-	echo "	 	$1 -uncompress <path> ... " 1>&2
-	echo "	 	$1 -purge <days> <path> ... " 1>&2
-	echo "	 	$1 -purge expire <path> ... " 1>&2
+	echo "usage: $1 -compress <path> ... " 1>&2
+	echo "       $1 -uncompress <path> ... " 1>&2
+	echo "       $1 -purge <days> <path> ... " 1>&2
+	echo "       $1 -purge expire <path> ... " 1>&2
 	exit 1
 }
 
