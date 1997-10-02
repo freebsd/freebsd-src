59cfbfda33f03c557433e6dc9f808ed31515b88d
charnier <charnier@FreeBSD.org>
1997-10-02 11:40:14 +0000

    MFC: Use err(3) instead of local redefinition.

--- usr.sbin/mtree/extern.h
+++ usr.sbin/mtree/extern.h
@@ -36,7 +36,6 @@
 int	 compare __P((char *, NODE *, FTSENT *));
 int	 crc __P((int, u_long *, u_long *));
 void	 cwalk __P((void));
-void	 err __P((const char *, ...));
 char	*inotype __P((u_int));
 u_int	 parsekey __P((char *, int *));
 char	*rlink __P((char *));
