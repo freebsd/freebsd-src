18a6945684fd7ada8519c2ae15a2eff2b8882749
peter <peter@FreeBSD.org>
1997-10-10 12:53:27 +0000

    #include <machine/tss.h> explicitly

--- gnu/usr.bin/gdb/gdb/kvm-fbsd.c
+++ gnu/usr.bin/gdb/gdb/kvm-fbsd.c
@@ -41,6 +41,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #include <machine/vmparam.h>
 #include <machine/pcb.h>
+#include <machine/tss.h>
 #include <machine/frame.h>
 
 static void kcore_files_info PARAMS ((struct target_ops *));
