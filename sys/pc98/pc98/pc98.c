9a0963757a8afd4c9a8e3841ad4d33296c2fe2be
KATO Takenori <kato@FreeBSD.org>
1997-10-13 08:58:48 +0000

    Synchronize with sys/i386/isa/isa.c revision 1.74.2.2.
    Submitted by:	H. Nokubi <h-nokubi@nmit.tmg.nec.co.jp>

--- sys/pc98/pc98/pc98.c
+++ sys/pc98/pc98/pc98.c
@@ -34,7 +34,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)isa.c	7.2 (Berkeley) 5/13/91
- *	$Id: pc98.c,v 1.10.2.6 1997/02/13 12:26:42 kato Exp $
+ *	$Id: pc98.c,v 1.10.2.7 1997/06/20 10:28:13 kato Exp $
  */
 
 /*
@@ -919,20 +919,31 @@ isa_nmi(cd)
 #else /* IBM-PC */
 	int isa_port = inb(0x61);
 	int eisa_port = inb(0x461);
-	if(isa_port & NMI_PARITY) {
+	if (isa_port & NMI_PARITY)
 		panic("RAM parity error, likely hardware failure.");
-	} else if(isa_port & NMI_IOCHAN) {
+
+	if(isa_port & NMI_IOCHAN)
 		panic("I/O channel check, likely hardware failure.");
-	} else if(eisa_port & ENMI_WATCHDOG) {
-		panic("EISA watchdog timer expired, likely hardware failure.");
-	} else if(eisa_port & ENMI_BUSTIMER) {
-		panic("EISA bus timeout, likely hardware failure.");
-	} else if(eisa_port & ENMI_IOSTATUS) {
-		panic("EISA I/O port status error.");
-	} else {
-		printf("\nNMI ISA %x, EISA %x\n", isa_port, eisa_port);
+
+	/*
+	 * On a real EISA machine, this will never happen.  However it can
+	 * happen on ISA machines which implement XT style floating point
+	 * error handling (very rare).  Save them from a meaningless panic.
+	 */
+	if (eisa_port == 0xff)
 		return(0);
-	}
+
+	if (eisa_port & ENMI_WATCHDOG)
+		panic("EISA watchdog timer expired, likely hardware failure.");
+
+	if (eisa_port & ENMI_BUSTIMER)
+		panic("EISA bus timeout, likely hardware failure.");
+
+	if (eisa_port & ENMI_IOSTATUS)
+		panic("EISA I/O port status error.");
+
+	printf("\nNMI ISA %x, EISA %x\n", isa_port, eisa_port);
+	return(0);
 #endif
 }
 
