d9a238cb7964181febc117f0f579b9a8d91ea7ce
kato <kato@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-10-13 09:24:04 +0000

    Synchronize with sys/i386/isa/wd.c revision 1.141.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@30369 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/pc98/pc98/wd.c
+++ sys/pc98/pc98/wd.c
@@ -34,7 +34,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)wd.c	7.2 (Berkeley) 5/9/91
- *	$Id: wd.c,v 1.32 1997/09/14 01:44:17 kato Exp $
+ *	$Id: wd.c,v 1.33 1997/09/22 12:24:39 kato Exp $
  */
 
 /* TODO:
@@ -583,15 +583,15 @@ wdattach(struct isa_device *dvp)
 			wdtimeout(du);
 
 #ifdef DEVFS
-			mynor = dkmakeminor(unit, WHOLE_DISK_SLICE, RAW_PART);
+			mynor = dkmakeminor(lunit, WHOLE_DISK_SLICE, RAW_PART);
 			du->dk_bdev = devfs_add_devswf(&wd_bdevsw, mynor,
 						       DV_BLK, UID_ROOT,
 						       GID_OPERATOR, 0640,
-						       "wd%d", unit);
+						       "wd%d", lunit);
 			du->dk_cdev = devfs_add_devswf(&wd_cdevsw, mynor,
 						       DV_CHR, UID_ROOT,
 						       GID_OPERATOR, 0640,
-						       "rwd%d", unit);
+						       "rwd%d", lunit);
 #endif
 
 			if (dk_ndrive < DK_NDRIVE) {
