6e47de28bb1f6eadeab11bf6b4bf6d948c0df3f7
Bruce Evans <bde@FreeBSD.org>
1997-10-21 06:53:24 +0000

    Added -Wuninitialized to CWARNFLAGS.  Warnings about uninitialialized variables were lost when we removed -W, and 23 new ones including at least one serious one have crept in for LINT.
    Restored -Winline to CFLAGS.  This gives only 3 old warnings and 1 new
    for LINT.

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -1,11 +1,12 @@
-#	$Id$
+#	$Id: bsd.kern.mk,v 1.5 1997/02/22 13:56:10 peter Exp $
 
 #
 # Warning flags for compiling the kernel and components of the kernel.
 #
 CWARNFLAGS?=	-Wreturn-type -Wcomment -Wredundant-decls -Wimplicit \
 		-Wnested-externs -Wstrict-prototypes -Wmissing-prototypes \
-		-Wpointer-arith # -W -Winline -Wunused -Wcast-qual
+		-Wpointer-arith -Winline -Wuninitialized # -W -Wunused \
+		-Wcast-qual
 #
 # The following flags are next up for working on:
 #	-Wformat -Wall
