4f69c3d8cc9a7b164e62aba538de05ff8bc9567e
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-10-22 00:38:48 +0000

    Recognize a %VERSREQ=nnnnn string in the system Makefile.  Both config(8) and the kernel will have a 'config interface version number'.  If an incompatable change is made to the kernel that requires a rebuild of config(8) (such as the cam devtab stuff), then the version number would be bumped in both places.  If a user neglects to rebuild config, then they will get a nagging (but non-fatal) warning that they need to rebuild config.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@30638 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ usr.sbin/config/configvers.h
@@ -0,0 +1,11 @@
+/*
+ * 6 digits of version.  The most significant are branch indicators
+ * (eg: RELENG_2_2 = 22, -current presently = 30 etc). The least
+ * significant digits are incremented for each incompatable change.
+ *
+ * The numbering scheme is inspired by the sys/conf/newvers.sh RELDATE
+ * and <osreldate.h> system.
+ *
+ * $Id$
+ */
+#define CONFIGVERS 300002
