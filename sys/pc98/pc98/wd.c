e98af178cdfe6d00e0e50dc345db2617ab161cd3
kato <kato@FreeBSD.org>
1997-11-04 03:07:59 +0000

    Sync with sys/i386/isa/wd.c revision 1.119.2.9.
    Reviewed by:	H. Nokubi <h-nokubi@nmit.tmg.nec.co.jp>

--- sys/pc98/pc98/wd.c
+++ sys/pc98/pc98/wd.c
@@ -34,7 +34,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)wd.c	7.2 (Berkeley) 5/9/91
- *	$Id: wd.c,v 1.9.2.11 1997/08/12 08:09:56 kato Exp $
+ *	$Id: wd.c,v 1.9.2.12 1997/09/14 06:26:40 kato Exp $
  */
 
 /* TODO:
@@ -550,15 +550,15 @@ wdattach(struct isa_device *dvp)
 			wdtimeout(du);
 
 #ifdef DEVFS
-			mynor = dkmakeminor(unit, WHOLE_DISK_SLICE, RAW_PART);
+			mynor = dkmakeminor(lunit, WHOLE_DISK_SLICE, RAW_PART);
 			du->dk_bdev = devfs_add_devswf(&wd_bdevsw, mynor,
 						       DV_BLK, UID_ROOT,
 						       GID_OPERATOR, 0640,
-						       "wd%d", unit);
+						       "wd%d", lunit);
 			du->dk_cdev = devfs_add_devswf(&wd_cdevsw, mynor,
 						       DV_CHR, UID_ROOT,
 						       GID_OPERATOR, 0640,
-						       "rwd%d", unit);
+						       "rwd%d", lunit);
 #endif
 
 			if (dk_ndrive < DK_NDRIVE) {
