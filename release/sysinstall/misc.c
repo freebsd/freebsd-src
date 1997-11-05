eb98cfd8ddf86e68c3bf14b34047746207650239
obrien <obrien@FreeBSD.org>
1997-11-05 05:54:30 +0000

    1. If running multi-user and doing initializing a new disk, don't mount    async. 2. Try to be more consistant in our usage of partition when we really mean    slice.  (still needs more work, but hopefully closer to being consistant)
    Ok'ed by:	jkh

--- release/sysinstall/misc.c
+++ release/sysinstall/misc.c
@@ -1,7 +1,7 @@
 /*
  * Miscellaneous support routines..
  *
- * $Id: misc.c,v 1.22.2.9 1997/03/09 22:37:58 jkh Exp $
+ * $Id: misc.c,v 1.22.2.10 1997/06/13 07:11:21 jkh Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -337,7 +337,8 @@ Mount(char *mountp, void *dev)
 	msgDebug("mount %s %s\n", device, mountpoint);
 
     ufsargs.fspec = device;
-    if (mount(MOUNT_UFS, mountpoint, MNT_ASYNC, (caddr_t)&ufsargs) == -1) {
+    if (mount(MOUNT_UFS, mountpoint, RunningAsInit ? MNT_ASYNC : 0,
+	(caddr_t)&ufsargs) == -1) {
 	msgConfirm("Error mounting %s on %s : %s", device, mountpoint, strerror(errno));
 	return DITEM_FAILURE;
     }
