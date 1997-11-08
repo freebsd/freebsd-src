6151196976fc79a694916e12215a65980d7772e2
julian <julian@FreeBSD.org>
1997-11-08 19:02:28 +0000

    fix slight breakages from PHK's VFS work. also remove irrelevant copyright, now that all that code has gone away.

--- sys/miscfs/devfs/devfs_vnops.c
+++ sys/miscfs/devfs/devfs_vnops.c
@@ -1,7 +1,7 @@
 /*
  *  Written by Julian Elischer (julian@DIALix.oz.au)
  *
- *	$Header: /home/ncvs/src/sys/miscfs/devfs/devfs_vnops.c,v 1.47 1997/10/26 20:55:12 phk Exp $
+ *	$Header: /home/ncvs/src/sys/miscfs/devfs/devfs_vnops.c,v 1.48 1997/10/27 13:33:37 bde Exp $
  *
  * symlinks can wait 'til later.
  */
@@ -1439,7 +1439,7 @@ devfs_reclaim(struct vop_reclaim_args *ap)
 		struct vnode *a_vp;
         } */
 {
-	dn_p	file_node;
+	dn_p	file_node = NULL;
 	int	error;
 
 DBPRINT(("reclaim\n"));
@@ -1450,8 +1450,10 @@ DBPRINT(("reclaim\n"));
 	}
 
 	ap->a_vp->v_data = NULL;
-	file_node->vn = 0;
-	file_node->vn_id = 0;
+	if (file_node) {
+		file_node->vn = 0;
+		file_node->vn_id = 0;
+	}
 	return(0);
 }
 
@@ -1546,44 +1548,6 @@ static struct vnodeopv_desc devfs_vnodeop_opv_desc =
 
 VNODEOP_SET(devfs_vnodeop_opv_desc);
 
-
-/*copied in from specfs/spec_vnops.c.. (spot the changes )*/
-/* These are the operations used by special devices in a devfs */
-/*
- * Copyright (c) 1989, 1993
- *	The Regents of the University of California.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- *	@(#)spec_vnops.c	8.6 (Berkeley) 4/9/94
- * spec_vnops.c,v 1.9 1994/11/14 13:22:52 bde Exp
- */
 
 
 vop_t **dev_spec_vnodeop_p;
