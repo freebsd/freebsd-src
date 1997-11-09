36d59367b86a286bb178760e81c08ff88214748f
wosch <wosch@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-11-09 15:03:15 +0000

    Remove the comments hat just list the nested includes. The lists don't provide significantly more information than grep '\.include', and grep gives lists that are actually correct. Submitted by: Bruce
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@31074 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/conf/kmod.mk
+++ sys/conf/kmod.mk
@@ -1,10 +1,7 @@
 #	From: @(#)bsd.prog.mk	5.26 (Berkeley) 6/25/91
-#	$Id: bsd.kmod.mk,v 1.39 1997/06/30 23:37:54 bde Exp $
+#	$Id: bsd.kmod.mk,v 1.40 1997/07/21 16:04:41 bde Exp $
 #
 # The include file <bsd.kmod.mk> handles installing Loadable Kernel Modules.
-# <bsd.kmod.mk> includes the file named "../Makefile.inc" if it exists,
-# as well as the include file <bsd.obj.mk>, <bsd.dep.mk>, and
-# may be <bsd.man.mk>
 #
 #
 # +++ variables +++
