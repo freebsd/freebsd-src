e4474ce8bf7dc8107c11198ed6c53db0643f5fcd
Bruce Evans <bde@FreeBSD.org>
1997-11-18 12:43:41 +0000

    Removed an unused #include.  Ifdefed a conditionally used #include.

--- sys/kern/subr_autoconf.c
+++ sys/kern/subr_autoconf.c
@@ -41,15 +41,16 @@
  *
  *	@(#)subr_autoconf.c	8.1 (Berkeley) 6/10/93
  *
- * $Id: subr_autoconf.c,v 1.4 1997/02/22 09:39:15 peter Exp $
+ * $Id: subr_autoconf.c,v 1.5 1997/09/21 22:00:18 gibbs Exp $
  */
 
 #include <sys/param.h>
 #include <sys/kernel.h>
-#include <sys/queue.h>
 #include <sys/systm.h>
 #include <sys/device.h>
+#ifdef UNUSED
 #include <sys/malloc.h>
+#endif
 
 /*
  * Autoconfiguration subroutines.
