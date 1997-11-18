11b16e0b5f538e0ccde9b98e0dcbd0f250a4ac7b
John Polstra <jdp@FreeBSD.org>
1997-11-18 03:43:09 +0000

    Merge from main branch 1.18 -> 1.19: Fix incorrect format string.

--- usr.sbin/config/main.c
+++ usr.sbin/config/main.c
@@ -42,7 +42,7 @@ static const char copyright[] =
 static char sccsid[] = "@(#)main.c	8.1 (Berkeley) 6/6/93";
 #endif
 static const char rcsid[] =
-	"$Id$";
+	"$Id: main.c,v 1.14.2.3 1997/09/16 06:57:09 charnier Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -422,7 +422,7 @@ moveifchanged(const char *from_name, const char *to_name)
 			err(EX_OSERR, "rename(%s, %s)", from_name, to_name);
 	} else {
 		if (unlink(from_name) < 0)
-			err(EX_OSERR, "unlink(%s, %s)", from_name);
+			err(EX_OSERR, "unlink(%s)", from_name);
 	}
 
 #ifdef DIAG
