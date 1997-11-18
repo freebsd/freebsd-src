b910ff6e4e51da2280212386e69799d4c57440dc
brian <brian@FreeBSD.org>
1997-11-18 07:33:23 +0000

    DropClient() when read() returns 0 (as it did before).

--- usr.sbin/ppp/main.c
+++ usr.sbin/ppp/main.c
@@ -17,7 +17,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: main.c,v 1.101 1997/11/17 01:13:41 brian Exp $
+ * $Id: main.c,v 1.102 1997/11/18 00:19:34 brian Exp $
  *
  *	TODO:
  *		o Add commands for traffic summary, version display, etc.
@@ -600,7 +600,7 @@ ReadTty()
       if (n)
         DecodeCommand(linebuff, n, IsInteractive(0) ? NULL : "Client");
       Prompt();
-    } else if (n < 0)
+    } else if (n <= 0)
       DropClient();
     return;
   }
