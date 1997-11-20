7a938e0797ad81f6dc8bb67d164c830d5a75b09d
charnier <charnier@FreeBSD.org>
1997-11-20 07:26:04 +0000

    Add const to copyright string. Put sccsid under #if 0/#endif control.

--- libexec/fingerd/fingerd.c
+++ libexec/fingerd/fingerd.c
@@ -32,17 +32,17 @@
  */
 
 #ifndef lint
-static char copyright[] =
+static const char copyright[] =
 "@(#) Copyright (c) 1983, 1993\n\
 	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-/*
+#if 0
 static char sccsid[] = "@(#)fingerd.c	8.1 (Berkeley) 6/4/93";
-*/
+#endif
 static const char rcsid[] =
-	"$Id: fingerd.c,v 1.8 1997/03/28 15:48:09 imp Exp $";
+	"$Id$";
 #endif /* not lint */
 
 #include <sys/types.h>
