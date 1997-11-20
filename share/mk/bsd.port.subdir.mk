59ceaa0b0460ae5fe79c6a0981e8f8ab0db16747
Satoshi Asami <asami@FreeBSD.org>
1997-11-20 05:37:23 +0000

    Merge everything up to 1.23.

--- share/mk/bsd.port.subdir.mk
+++ share/mk/bsd.port.subdir.mk
@@ -1,5 +1,37 @@
 #	from: @(#)bsd.subdir.mk	5.9 (Berkeley) 2/1/91
-#	$Id: bsd.port.subdir.mk,v 1.11.4.3 1997/01/13 02:17:00 asami Exp $
+#	$Id: bsd.port.subdir.mk,v 1.11.4.4 1997/08/22 11:17:06 asami Exp $
+#
+# The include file <bsd.port.subdir.mk> contains the default targets
+# for building ports subdirectories. 
+#
+#
+# +++ variables +++
+#
+# STRIP		The flag passed to the install program to cause the binary
+#		to be stripped.  This is to be used when building your
+#		own install script so that the entire system can be made
+#		stripped/not-stripped using a single knob. [-s]
+#
+# ECHO_MSG	Used to print all the '===>' style prompts - override this
+#		to turn them off [echo].
+#
+# OPSYS		Get the operating system type [`uname -s`]
+#
+# SUBDIR	A list of subdirectories that should be built as well.
+#		Each of the targets will execute the same target in the
+#		subdirectories.
+#
+#
+# +++ targets +++
+#
+#	README.html:
+#		Creating README.html for package.
+#
+#	afterinstall, all, beforeinstall, build, checksum, clean,
+#	configure, depend, describe, extract, fetch, fetch-list,
+#	install, package, readmes, realinstall, reinstall, tags
+#
+
 
 .MAIN: all
 
@@ -48,7 +80,7 @@ ${SUBDIR}::
 
 .for __target in all fetch fetch-list package extract configure \
 		 build clean depend describe distclean reinstall tags checksum
-.if !target(__target)
+.if !target(${__target})
 ${__target}: _SUBDIRUSE
 .endif
 .endfor
@@ -63,6 +95,34 @@ afterinstall:
 install: afterinstall
 afterinstall: realinstall
 realinstall: beforeinstall _SUBDIRUSE
+.endif
+
+IGNOREDIR=	CVS distfiles packages pkg templates
+
+.if !target(checksubdirs)
+.if defined(PORTSTOP)
+checksubdirs: checksubdir _SUBDIRUSE
+.else
+checksubdirs: checksubdir
+.endif
+.endif
+
+.if !target(checksubdir)
+checksubdir:
+	@for d in *; do \
+	  if [ -d "$$d" ]; then \
+	    found=0; \
+	    for s in ${SUBDIR} ${IGNOREDIR}; do \
+	      if [ "x$$s" = "x$$d" ]; then \
+	        found=1; \
+	        break; \
+	      fi; \
+	    done; \
+	    if [ $$found = 0 ]; then \
+	      ${ECHO} "Warning: directory $$d not in SUBDIR"; \
+	    fi; \
+	  fi; \
+	done
 .endif
 
 .if !target(readmes)
@@ -87,27 +147,34 @@ README=	${TEMPLATES}/README.top
 README=	${TEMPLATES}/README.category
 .endif
 
+HTMLIFY=	sed -e 's/&/\&amp;/g' -e 's/>/\&gt;/g' -e 's/</\&lt;/g'
+
 README.html:
 	@echo "===>  Creating README.html"
 	@> $@.tmp
 .for entry in ${SUBDIR}
 .if defined(PORTSTOP)
-	@echo -n '<a href="'${entry}/README.html'">${entry}</a>: ' >> $@.tmp
+	@echo -n '<a href="'${entry}/README.html'">'"`echo ${entry} | ${HTMLIFY}`"'</a>: ' >> $@.tmp
 .else
-	@echo -n '<a href="'${entry}/README.html'">'"`cd ${entry}; make package-name`</a>: " >> $@.tmp
+	@echo -n '<a href="'${entry}/README.html'">'"`cd ${entry}; make package-name | ${HTMLIFY}`</a>: " >> $@.tmp
 .endif
 .if exists(${entry}/pkg/COMMENT)
-	@cat ${entry}/pkg/COMMENT >> $@.tmp
+	@${HTMLIFY} ${entry}/pkg/COMMENT >> $@.tmp
 .else
 	@echo "(no description)" >> $@.tmp
 .endif
 .endfor
 	@sort -t '>' +1 -2 $@.tmp > $@.tmp2
+.if exists(${.CURDIR}/pkg/DESCR)
+	@${HTMLIFY} ${.CURDIR}/pkg/DESCR > $@.tmp3
+.else
+	@> $@.tmp3
+.endif
 	@cat ${README} | \
-		sed -e 's%%CATEGORY%%'`echo ${.CURDIR} | sed -e 's.*/\([^/]*\)$$\1'`'g' \
-			-e '/%%DESCR%%/r${.CURDIR}/pkg/DESCR' \
+		sed -e 's/%%CATEGORY%%/'"`basename ${.CURDIR}`"'/g' \
+			-e '/%%DESCR%%/r$@.tmp3' \
 			-e '/%%DESCR%%/d' \
 			-e '/%%SUBDIR%%/r$@.tmp2' \
 			-e '/%%SUBDIR%%/d' \
 		> $@
-	@rm -f $@.tmp $@.tmp2
+	@rm -f $@.tmp $@.tmp2 $@.tmp3
