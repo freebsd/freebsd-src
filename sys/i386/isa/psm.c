358e851a59d646519ad9d0b0e4a8ef76e3d061a7
Kazutaka YOKOTA <yokota@FreeBSD.org>
1997-11-21 11:36:21 +0000

    Fix for a PS/2 mouse model from MouseSystems. It now appears that this mouse can correctly operate only in the high resolution mode.
    If the mouse pointer jumps to the top or left edge or the top-left
    corner of the screen, try defining PSM_MSCKLUDGE in the kernel
    configuration file. This option will put the mouse in the high
    resolution mode during device initialization.

--- sys/i386/isa/psm.c
+++ sys/i386/isa/psm.c
@@ -19,7 +19,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: psm.c,v 1.44 1997/11/07 08:52:41 phk Exp $
+ * $Id: psm.c,v 1.45 1997/11/18 12:21:32 bde Exp $
  */
 
 /*
@@ -765,6 +765,10 @@ psmprobe(struct isa_device *dvp)
     if (verbose >= 2)
 	log(LOG_DEBUG, "psm%d: SET_DEFAULTS return code:%04x\n",
 	    unit, i);
+#ifdef PSM_MSCKLUDGE
+    set_mouse_resolution(sc->kbdc, PSMD_RES_HIGH);
+#endif
+
 #if 0
     set_mouse_scaling(sc->kbdc); 	/* 1:1 scaling */
     set_mouse_mode(sc->kbdc);		/* stream mode */
