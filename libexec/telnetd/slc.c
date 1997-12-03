d16e315056c3d011370a8eb6c4a924d66525de5b
charnier <charnier@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-12-03 07:16:08 +0000

    Use err(3). Remove progname and trailing \n in syslog strings.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@31511 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- libexec/telnetd/slc.c
+++ libexec/telnetd/slc.c
@@ -29,12 +29,14 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- *	$Id$
  */
 
 #ifndef lint
+#if 0
 static char sccsid[] = "@(#)slc.c	8.1 (Berkeley) 6/4/93";
+#endif
+static const char rcsid[] =
+	"$Id$";
 #endif /* not lint */
 
 #include "telnetd.h"
