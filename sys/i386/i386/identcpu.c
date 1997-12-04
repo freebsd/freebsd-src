59139e27496a35e7d5a23b5cb8bc975d8a565857
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-12-04 14:35:40 +0000

    After consultation with David, change #ifndef NO_F00F_HACK to #if defined(I586_CPU) && !defined(NO_F00F_HACK)
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@31535 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/i386/identcpu.c
+++ sys/i386/i386/identcpu.c
@@ -36,7 +36,7 @@
  * SUCH DAMAGE.
  *
  *	from: Id: machdep.c,v 1.193 1996/06/18 01:22:04 bde Exp
- *	$Id: identcpu.c,v 1.33 1997/11/07 08:52:27 phk Exp $
+ *	$Id: identcpu.c,v 1.34 1997/12/03 02:45:42 sef Exp $
  */
 
 #include "opt_cpu.h"
@@ -107,7 +107,7 @@ do_cpuid(u_long ax, u_long *p)
 	);
 }
 
-#ifndef NO_F00F_HACK
+#if defined(I586_CPU) && !defined(NO_F00F_HACK)
 int has_f00f_bug = 0;
 #endif
 
@@ -140,7 +140,7 @@ printcpuinfo(void)
 				break;
 			case 0x500:
 				strcat(cpu_model, "Pentium"); /* nb no space */
-#ifndef NO_F00F_HACK
+#if defined(I586_CPU) && !defined(NO_F00F_HACK)
 				/*
 				 * XXX - If/when Intel fixes the bug, this
 				 * should also check the version of the
