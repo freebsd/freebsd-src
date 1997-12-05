51a673611066b0870283e141bdd3726450ab3313
kato <kato@FreeBSD.org>
1997-12-05 11:50:42 +0000

    Sync with sys/i386/boot/biosboot/boot.h and io.c revisions 1.23 and 1.25, respectively.

--- sys/pc98/boot/biosboot/boot.h
+++ sys/pc98/boot/biosboot/boot.h
@@ -24,11 +24,10 @@
  * the rights to redistribute these changes.
  *
  *	from: Mach, Revision 2.2  92/04/04  11:35:03  rpd
- *	$Id: boot.h,v 1.11 1997/07/13 12:13:02 kato Exp $
+ *	$Id: boot.h,v 1.12 1997/09/01 10:38:32 kato Exp $
  */
 
 #include <sys/param.h>
-#include <sys/lock.h>
 #include <sys/time.h>
 
 #include <ufs/ffs/fs.h>
@@ -80,10 +79,9 @@ void printf(const char *format, ...);
 void putchar(int c);
 void delay1ms(void);
 int gets(char *buf);
-#ifndef CDBOOT
 int strcmp(const char *s1, const char *s2);
-#else /* CDBOOT */
-int strncasecmp(const char *s1, const char *s2, size_t s);
+#ifdef CDBOOT
+int strcasecmp(const char *s1, const char *s2);
 #endif /* !CDBOOT */
 void bcopy(const void *from, void *to, size_t len);
 void twiddle(void);
