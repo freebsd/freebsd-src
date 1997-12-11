50f14fbd5e2280803b89cf6790ff1cc9c978424a
bde <bde@FreeBSD.org>
1997-12-11 07:12:10 +0000

    Merged from Lite2 (fix misformattings in copyright).

--- usr.sbin/bad144/bad144.c
+++ usr.sbin/bad144/bad144.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1993, 198019861988
+ * Copyright (c) 1980, 1986, 1988, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -33,16 +33,16 @@
 
 #ifndef lint
 static const char copyright[] =
-"@(#) Copyright (c) 1993, 198019861988\n\
+"@(#) Copyright (c) 1980, 1986, 1988, 1993\n\
 	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
 #if 0
-static const char sccsid[] = "@(#)bad144.c	8.1 (Berkeley) 6/6/93";
+static char sccsid[] = "@(#)bad144.c	8.2 (Berkeley) 4/27/95";
 #endif
 static const char rcsid[] =
-	"$Id$";
+	"$Id: bad144.c,v 1.13 1997/09/04 11:40:51 charnier Exp $";
 #endif /* not lint */
 
 /*
