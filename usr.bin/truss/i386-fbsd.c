dcc185ff639f63027e0c0bafbe97b22bed6507b3
sef <sef@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-12-20 18:40:43 +0000

    Add some copyright and license terms, at Jordan's request.  Note that syscall.h just refers to another file for the copyright notice.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@31899 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/truss/i386-fbsd.c
+++ usr.bin/truss/i386-fbsd.c
@@ -1,4 +1,36 @@
 /*
+ * Copryight 1997 Sean Eric Fagan
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Sean Eric Fagan
+ * 4. Neither the name of the author may be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+/*
  * FreeBSD/386-specific system call handling.  This is probably the most
  * complex part of the entire truss program, although I've got lots of
  * it handled relatively cleanly now.  The system call names are generated
@@ -6,7 +38,7 @@
  * names used for the various structures are confusing, I sadly admit.
  */
 /*
- * $Id$
+ * $Id: i386-fbsd.c,v 1.1 1997/12/06 05:22:50 sef Exp $
  */
 
 #include <stdio.h>
