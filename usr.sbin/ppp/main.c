4401489a4a67023dc7f6c7dfcc2b918f7fcdf41c
Brian Somers <brian@FreeBSD.org>
1997-12-22 02:28:18 +0000

    Don't forget to ntohs(sin.sin_port) (DUH!)

--- usr.sbin/ppp/main.c
+++ usr.sbin/ppp/main.c
@@ -17,7 +17,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: main.c,v 1.108 1997/12/17 21:21:47 brian Exp $
+ * $Id: main.c,v 1.109 1997/12/17 21:21:56 brian Exp $
  *
  *	TODO:
  *		o Add commands for traffic summary, version display, etc.
@@ -981,10 +981,10 @@ DoLoop(void)
           LogPrintf(LogPHASE, "Connected to local client.\n");
           break;
         case AF_INET:
-          if (sin->sin_port < 1024) {
+          if (ntohs(sin->sin_port) < 1024) {
             LogPrintf(LogALERT, "Rejected client connection from %s:%u"
                       "(invalid port number) !\n",
-                      inet_ntoa(sin->sin_addr), sin->sin_port);
+                      inet_ntoa(sin->sin_addr), ntohs(sin->sin_port));
 	    close(wfd);
 	    continue;
           }
