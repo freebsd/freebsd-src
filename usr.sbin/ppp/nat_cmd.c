4f5a21e539526306ad4a7caae0893f5e5dc46de7
brian <brian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-12-24 10:28:38 +0000

    Correct copyright.
    Requested by: Eivind Eklund <perhaps@yes.no>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@31964 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/ppp/nat_cmd.c
+++ usr.sbin/ppp/nat_cmd.c
@@ -1,29 +1,8 @@
 /*-
- * Copyright (c) 1997 Charles Mott <cmott@srv.net>
- * All rights reserved.
+ * The code in this file was written by Eivind Eklund <perhaps@yes.no>,
+ * who places it in the public domain without restriction.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- *	$Id: alias_cmd.c,v 1.9 1997/12/21 12:11:04 brian Exp $
+ *	$Id: alias_cmd.c,v 1.10 1997/12/21 14:28:23 brian Exp $
  */
 
 #include <sys/param.h>
