5ebfa8de69f7860ed332336eff6bc7978a05852d
Andrey A. Chernov <ache@FreeBSD.org>
1997-12-24 14:32:40 +0000

    Back out part related to "return 0 if n == 0" and return EOF as before.
    The main argument is that it is impossible to determine if %n evaluated or not
    when snprintf return 0, because it can happens for both n == 0 and n == 1.
    Although EOF here is good indication of the end of process, if n is
    decreased in the loop...
    Since it is already supposed in many places that EOF *is* negative, f.e.
    from Single Unix specs for snprintf
    "return ... a negative value if an output error was encountered"
    this not makes situation worse.

--- lib/libc/stdio/snprintf.c
+++ lib/libc/stdio/snprintf.c
@@ -39,7 +39,7 @@
 static char sccsid[] = "@(#)snprintf.c	8.1 (Berkeley) 6/4/93";
 #endif
 static const char rcsid[] =
-		"$Id: snprintf.c,v 1.5 1997/02/22 15:02:29 peter Exp $";
+		"$Id: snprintf.c,v 1.6 1997/12/24 12:31:31 ache Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdio.h>
@@ -65,8 +65,6 @@ snprintf(str, n, fmt, va_alist)
 	va_list ap;
 	FILE f;
 
-	if (n == 0)
-		return (0);
 	if ((int)n < 1)
 		return (EOF);
 	n--;
