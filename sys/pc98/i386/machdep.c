19d768b80b9b900d86bf0e35c000b48c40803e8b
Peter Wemm <peter@FreeBSD.org>
1997-12-27 02:28:28 +0000

    #include "opt_user_ldt.h" so that the #ifdef USER_LDT checks can work, as commented about at length in the PR audit trail.
    PR: 2412

--- sys/pc98/i386/machdep.c
+++ sys/pc98/i386/machdep.c
@@ -35,7 +35,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91
- *	$Id: machdep.c,v 1.65 1997/12/14 12:31:44 kato Exp $
+ *	$Id: machdep.c,v 1.66 1997/12/16 10:22:49 kato Exp $
  */
 
 #include "apm.h"
@@ -47,6 +47,7 @@
 #include "opt_perfmon.h"
 #include "opt_smp.h"
 #include "opt_sysvipc.h"
+#include "opt_user_ldt.h"
 #include "opt_userconfig.h"
 #include "opt_vm86.h"
 
