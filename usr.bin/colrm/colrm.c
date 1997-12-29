005e82d0d48c18bf9c8f7dd68fbdddaecd02fa38
alex <alex@FreeBSD.org>
1997-12-29 00:09:06 +0000

    -Wall cleanup.

--- usr.bin/colrm/colrm.c
+++ usr.bin/colrm/colrm.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: colrm.c,v 1.3 1997/06/26 11:26:20 charnier Exp $
+ *	$Id: colrm.c,v 1.4 1997/06/30 11:05:42 charnier Exp $
  */
 
 #ifndef lint
@@ -116,7 +116,7 @@ main(argc, argv)
 			break;
 		}
 
-		if ((!start || column < start || stop && column > stop) &&
+		if ((!start || column < start || (stop && column > stop)) &&
 		    putchar(ch) == EOF)
 			check(stdout);
 	}
