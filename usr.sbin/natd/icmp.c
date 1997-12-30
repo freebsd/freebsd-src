36113785c1a907b384f95cc652656c268075ab26
alex <alex@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1997-12-30 00:38:56 +0000

    Added copyright (taken from natd.c).
    Approved by:	Ari Suutari <ari@suutari.iki.fi>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@32107 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/natd/icmp.c
+++ usr.sbin/natd/icmp.c
@@ -1,5 +1,13 @@
 /*
- * $Id$
+ * This software is provided free of charge, with no 
+ * warranty of any kind, either expressed or implied.
+ * Use at your own risk.
+ * 
+ * You may copy, modify and distribute this software freely.
+ *
+ * Ari Suutari <suutari@iki.fi>
+ *
+ * $Id: icmp.c,v 1.3 1997/12/27 19:31:11 alex Exp $
  */
 
 #include <stdlib.h>
