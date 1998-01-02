3c85ff2d98c9ab5f64e731e135c28ef61978191a
Andrey A. Chernov <ache@FreeBSD.org>
1998-01-02 04:39:38 +0000

    MFC: insdel in last windows line fix

--- lib/libncurses/lib_insdel.c
+++ lib/libncurses/lib_insdel.c
@@ -12,38 +12,20 @@
 **
 */
 
-#include <stdlib.h>
 #include "curses.priv.h"
-#include "terminfo.h"
 
 int
 winsdelln(WINDOW *win, int n)
 {
-	int ret, sscroll, stop, sbot;
+int code = ERR;
 
 	T(("winsdel(%x,%d) called", win, n));
 
-	if (n == 0)
-		return OK;
-	if (n < 0 && win->_cury - n >= win->_maxy)
-		/* request to delete too many lines */
-		/* should we truncate to an appropriate number? */
-		return ERR;
-
-	sscroll = win->_scroll;
-	stop = win->_regtop;
-	sbot = win->_regbottom;
-
-	win->_scroll = TRUE;
-	win->_regtop = win->_cury;
-	if (win->_regtop > win->_regbottom)
-		win->_regbottom = win->_maxy;
-
-	ret = wscrl(win, -n);
-
-	win->_scroll = sscroll;
-	win->_regtop = stop;
-	win->_regbottom = sbot;
-
-	return ret;
+	if (win) {
+	  if (n != 0) {
+	    _nc_scroll_window(win, -n, win->_cury, win->_maxy, _nc_background(win));	  
+	  }
+	  code = OK;
+	}
+	return code;
 }
