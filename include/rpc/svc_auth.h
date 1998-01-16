71b8855ac8252563b55c053c44f1128e85c6a391
bde <bde@FreeBSD.org>
1998-01-16 11:47:51 +0000

    Null change to attach the correct log message for the previous commit:
    Added some forward struct declarations so that this file is less
    self-insufficent.
    Fixed an incomplete prototype.
    Fixed some misformattings.

--- include/rpc/svc_auth.h
+++ include/rpc/svc_auth.h
@@ -28,7 +28,7 @@
  *
  *	from: @(#)svc_auth.h 1.6 86/07/16 SMI
  *	from: @(#)svc_auth.h	2.1 88/07/29 4.0 RPCSRC
- *	$Id: svc_auth.h,v 1.9 1997/05/28 04:45:14 wpaul Exp $
+ *	$Id: svc_auth.h,v 1.10 1998/01/16 11:46:05 bde Exp $
  */
 
 /*
