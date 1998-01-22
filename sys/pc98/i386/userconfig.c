06d33a7ee08aaab4edd6bf8c52ef39cfc56c0233
kato <kato@FreeBSD.org>
1998-01-22 14:26:34 +0000

    Sync with sys/i386/i386/userconfig.c revision 1.100.

--- sys/pc98/i386/userconfig.c
+++ sys/pc98/i386/userconfig.c
@@ -46,7 +46,7 @@
  ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **
- **      $Id: userconfig.c,v 1.42 1997/12/09 11:55:25 kato Exp $
+ **      $Id: userconfig.c,v 1.43 1997/12/29 16:06:20 kato Exp $
  **/
 
 /**
@@ -267,6 +267,7 @@ static DEV_INFO device_info[] = {
 {"ix",          "Intel EtherExpress Ethernet adapter",	0,		CLS_NETWORK},
 {"le",          "DEC Etherworks 2 and 3 Ethernet adapters",	0,	CLS_NETWORK},
 {"lnc",         "Isolan, Novell NE2100/NE32-VL Ethernet adapters",	0,CLS_NETWORK},
+{"tx",          "SMC 9432TX Ethernet adapters",			0,	CLS_NETWORK},
 {"vx",          "3COM 3C590/3C595 Ethernet adapters",		0,	CLS_NETWORK},
 {"ze",          "IBM/National Semiconductor PCMCIA Ethernet adapter",0,	CLS_NETWORK},
 {"zp",          "3COM PCMCIA Etherlink III Ethernet adapter",	0,	CLS_NETWORK},
@@ -2385,7 +2386,7 @@ visuserconfig(void)
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *      $Id: userconfig.c,v 1.42 1997/12/09 11:55:25 kato Exp $
+ *      $Id: userconfig.c,v 1.43 1997/12/29 16:06:20 kato Exp $
  */
 
 #include "scbus.h"
