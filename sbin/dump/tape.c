f2f8f6cac19a8b637f8cf03d3f9f88dce8f4b71b
jb <jb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-01-25 09:15:10 +0000

    read() and write() return ssize_t not int, so the first arg to atomic() should be ssize_t too.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@32758 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/dump/tape.c
+++ sbin/dump/tape.c
@@ -77,7 +77,7 @@ extern	int cartridge;
 extern	char *host;
 char	*nexttape;
 
-static	int atomic __P((int (*)(), int, char *, int));
+static	int atomic __P((ssize_t (*)(), int, char *, int));
 static	void doslave __P((int, int));
 static	void enslave __P((void));
 static	void flushtape __P((void));
@@ -855,7 +855,8 @@ doslave(cmd, slave_number)
  */
 static int
 atomic(func, fd, buf, count)
-	int (*func)(), fd;
+	ssize_t (*func)();
+	int fd;
 	char *buf;
 	int count;
 {
