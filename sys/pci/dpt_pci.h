ce41fbb4b9c0f52507ea14e1494b8a092da1aff8
cvs2svn <cvs2svn@FreeBSD.org>
1998-01-26 06:26:42 +0000

    This commit was manufactured by cvs2svn to create branch 'RELENG_2_2'.

---
+++ sys/pci/dpt_pci.h
@@ -0,0 +1,207 @@
+/*
+ *       Copyright (c) 1997 by Simon Shapiro
+ *       All Rights Reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions, and the following disclaimer,
+ *    without modification, immediately at the beginning of the file.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#ident "$Id: dpt_pci.h,v 1.3 1997/10/02 04:25:30 ShimonR Exp $"
+
+#define DPT_VENDOR_ID             0x00001044
+#define DPT_DEVICE_ID             0x0000a400
+
+/* The following are taken, shamelessly from Linux include/linux/pci.h */
+
+/*
+ * Under PCI, each device has 256 bytes of configuration address space,
+ * of which the first 64 bytes are standardized as follows:
+ */
+#define PCI_VENDOR_ID		0x00	/* 16 bits */
+#define PCI_DEVICE_ID		0x02	/* 16 bits */
+#define PCI_COMMAND		0x04	/* 16 bits */
+#define  PCI_COMMAND_IO		0x1	/* Enable response in I/O space */
+#define  PCI_COMMAND_MEMORY	0x2	/* Enable response in Memory space */
+#define  PCI_COMMAND_MASTER	0x4	/* Enable bus mastering */
+#define  PCI_COMMAND_SPECIAL	0x8	/* Enable response to special cycles */
+#define  PCI_COMMAND_INVALIDATE	0x10	/* Use memory write and invalidate */
+#define  PCI_COMMAND_VGA_PALETTE 0x20	/* Enable palette snooping */
+#define  PCI_COMMAND_PARITY	0x40	/* Enable parity checking */
+#define  PCI_COMMAND_WAIT 	0x80	/* Enable address/data stepping */
+#define  PCI_COMMAND_SERR	0x100	/* Enable SERR */
+#define  PCI_COMMAND_FAST_BACK	0x200	/* Enable back-to-back writes */
+
+#define PCI_STATUS		0x06	/* 16 bits */
+#define  PCI_STATUS_66MHZ	0x20	/* Support 66 Mhz PCI 2.1 bus */
+#define  PCI_STATUS_UDF		0x40	/* Support User Definable Features */
+
+#define  PCI_STATUS_FAST_BACK	0x80	/* Accept fast-back to back */
+#define  PCI_STATUS_PARITY	0x100	/* Detected parity error */
+#ifndef PCI_STATUS_DEVSEL_MASK
+#define  PCI_STATUS_DEVSEL_MASK	0x600	/* DEVSEL timing */
+#define  PCI_STATUS_DEVSEL_FAST	0x000
+#define  PCI_STATUS_DEVSEL_MEDIUM 0x200
+#define  PCI_STATUS_DEVSEL_SLOW 0x400
+#endif
+#define  PCI_STATUS_SIG_TARGET_ABORT 0x800	/* Set on target abort */
+#define  PCI_STATUS_REC_TARGET_ABORT 0x1000	/* Master ack of " */
+#define  PCI_STATUS_REC_MASTER_ABORT 0x2000	/* Set on master abort */
+#define  PCI_STATUS_SIG_SYSTEM_ERROR 0x4000	/* Set when we drive SERR */
+#define  PCI_STATUS_DETECTED_PARITY 0x8000	/* Set on parity error */
+
+#define PCI_CLASS_REVISION	0x08	/* High 24 bits are class, low 8
+					 * revision */
+#define PCI_REVISION_ID         0x08	/* Revision ID */
+#define PCI_CLASS_PROG          0x09	/* Reg. Level Programming Interface */
+#define PCI_CLASS_DEVICE        0x0a	/* Device class */
+
+#define PCI_CACHE_LINE_SIZE	0x0c	/* 8 bits */
+#define PCI_LATENCY_TIMER	0x0d	/* 8 bits */
+#define PCI_HEADER_TYPE		0x0e	/* 8 bits */
+#define PCI_BIST		0x0f	/* 8 bits */
+#define PCI_BIST_CODE_MASK	0x0f	/* Return result */
+#define PCI_BIST_START		0x40	/* 1 to start BIST, 2 secs or less */
+#define PCI_BIST_CAPABLE	0x80	/* 1 if BIST capable */
+
+/*
+ * Base addresses specify locations in memory or I/O space.
+ * Decoded size can be determined by writing a value of
+ * 0xffffffff to the register, and reading it back.  Only
+ * 1 bits are decoded.
+ */
+#define PCI_BASE_ADDRESS_0	0x10	/* 32 bits */
+#define PCI_BASE_ADDRESS_1	0x14	/* 32 bits */
+#define PCI_BASE_ADDRESS_2	0x18	/* 32 bits */
+#define PCI_BASE_ADDRESS_3	0x1c	/* 32 bits */
+#define PCI_BASE_ADDRESS_4	0x20	/* 32 bits */
+#define PCI_BASE_ADDRESS_5	0x24	/* 32 bits */
+#define  PCI_BASE_ADDRESS_SPACE	0x01	/* 0 = memory, 1 = I/O */
+#define  PCI_BASE_ADDRESS_SPACE_IO 0x01
+#define  PCI_BASE_ADDRESS_SPACE_MEMORY 0x00
+#define  PCI_BASE_ADDRESS_MEM_TYPE_MASK 0x06
+#define  PCI_BASE_ADDRESS_MEM_TYPE_32	0x00	/* 32 bit address */
+#define  PCI_BASE_ADDRESS_MEM_TYPE_1M	0x02	/* Below 1M */
+#define  PCI_BASE_ADDRESS_MEM_TYPE_64	0x04	/* 64 bit address */
+#define  PCI_BASE_ADDRESS_MEM_PREFETCH	0x08	/* prefetchable? */
+#define  PCI_BASE_ADDRESS_MEM_MASK	(~0x0f)
+#define  PCI_BASE_ADDRESS_IO_MASK	(~0x03)
+/* bit 1 is reserved if address_space = 1 */
+
+#define PCI_CARDBUS_CIS		0x28
+#define PCI_SUBSYSTEM_ID	0x2c
+#define PCI_SUBSYSTEM_VENDOR_ID	0x2e
+#define PCI_ROM_ADDRESS		0x30	/* 32 bits */
+#define  PCI_ROM_ADDRESS_ENABLE	0x01	/* Write 1 to enable ROM, bits 31..11
+					 * are address, 10..2 are reserved */
+
+/* 0x34-0x3b are reserved */
+#define PCI_INTERRUPT_LINE	0x3c	/* 8 bits */
+#define PCI_INTERRUPT_PIN	0x3d	/* 8 bits */
+#define PCI_MIN_GNT		0x3e	/* 8 bits */
+#define PCI_MAX_LAT		0x3f	/* 8 bits */
+
+#define PCI_CLASS_NOT_DEFINED		0x0000
+#define PCI_CLASS_NOT_DEFINED_VGA	0x0001
+
+#define PCI_BASE_CLASS_STORAGE		0x01
+#define PCI_CLASS_STORAGE_SCSI		0x0100
+#define PCI_CLASS_STORAGE_IDE		0x0101
+#define PCI_CLASS_STORAGE_FLOPPY	0x0102
+#define PCI_CLASS_STORAGE_IPI		0x0103
+#define PCI_CLASS_STORAGE_RAID		0x0104
+#define PCI_CLASS_STORAGE_OTHER		0x0180
+
+#define PCI_BASE_CLASS_NETWORK		0x02
+#define PCI_CLASS_NETWORK_ETHERNET	0x0200
+#define PCI_CLASS_NETWORK_TOKEN_RING	0x0201
+#define PCI_CLASS_NETWORK_FDDI		0x0202
+#define PCI_CLASS_NETWORK_ATM		0x0203
+#define PCI_CLASS_NETWORK_OTHER		0x0280
+
+#define PCI_BASE_CLASS_DISPLAY		0x03
+#define PCI_CLASS_DISPLAY_VGA		0x0300
+#define PCI_CLASS_DISPLAY_XGA		0x0301
+#define PCI_CLASS_DISPLAY_OTHER		0x0380
+
+#define PCI_BASE_CLASS_MULTIMEDIA	0x04
+#define PCI_CLASS_MULTIMEDIA_VIDEO	0x0400
+#define PCI_CLASS_MULTIMEDIA_AUDIO	0x0401
+#define PCI_CLASS_MULTIMEDIA_OTHER	0x0480
+
+#define PCI_BASE_CLASS_MEMORY		0x05
+#define  PCI_CLASS_MEMORY_RAM		0x0500
+#define  PCI_CLASS_MEMORY_FLASH		0x0501
+#define  PCI_CLASS_MEMORY_OTHER		0x0580
+
+#define PCI_BASE_CLASS_BRIDGE		0x06
+#define  PCI_CLASS_BRIDGE_HOST		0x0600
+#define  PCI_CLASS_BRIDGE_ISA		0x0601
+#define  PCI_CLASS_BRIDGE_EISA		0x0602
+#define  PCI_CLASS_BRIDGE_MC		0x0603
+#define  PCI_CLASS_BRIDGE_PCI		0x0604
+#define  PCI_CLASS_BRIDGE_PCMCIA	0x0605
+#define  PCI_CLASS_BRIDGE_NUBUS		0x0606
+#define  PCI_CLASS_BRIDGE_CARDBUS	0x0607
+#define  PCI_CLASS_BRIDGE_OTHER		0x0680
+
+
+#define PCI_BASE_CLASS_COMMUNICATION	0x07
+#define PCI_CLASS_COMMUNICATION_SERIAL	0x0700
+#define PCI_CLASS_COMMUNICATION_PARALLEL 0x0701
+#define PCI_CLASS_COMMUNICATION_OTHER	0x0780
+
+#define PCI_BASE_CLASS_SYSTEM		0x08
+#define PCI_CLASS_SYSTEM_PIC		0x0800
+#define PCI_CLASS_SYSTEM_DMA		0x0801
+#define PCI_CLASS_SYSTEM_TIMER		0x0802
+#define PCI_CLASS_SYSTEM_RTC		0x0803
+#define PCI_CLASS_SYSTEM_OTHER		0x0880
+
+#define PCI_BASE_CLASS_INPUT		0x09
+#define PCI_CLASS_INPUT_KEYBOARD	0x0900
+#define PCI_CLASS_INPUT_PEN		0x0901
+#define PCI_CLASS_INPUT_MOUSE		0x0902
+#define PCI_CLASS_INPUT_OTHER		0x0980
+
+#define PCI_BASE_CLASS_DOCKING		0x0a
+#define PCI_CLASS_DOCKING_GENERIC	0x0a00
+#define PCI_CLASS_DOCKING_OTHER		0x0a01
+
+#define PCI_BASE_CLASS_PROCESSOR	0x0b
+#define PCI_CLASS_PROCESSOR_386		0x0b00
+#define PCI_CLASS_PROCESSOR_486		0x0b01
+#define PCI_CLASS_PROCESSOR_PENTIUM	0x0b02
+#define PCI_CLASS_PROCESSOR_ALPHA	0x0b10
+#define PCI_CLASS_PROCESSOR_POWERPC	0x0b20
+#define PCI_CLASS_PROCESSOR_CO		0x0b40
+
+#define PCI_BASE_CLASS_SERIAL		0x0c
+#define PCI_CLASS_SERIAL_FIREWIRE	0x0c00
+#define PCI_CLASS_SERIAL_ACCESS		0x0c01
+#define PCI_CLASS_SERIAL_SSA		0x0c02
+#define PCI_CLASS_SERIAL_USB		0x0c03
+#define PCI_CLASS_SERIAL_FIBER		0x0c04
+
+#define PCI_CLASS_OTHERS		0xff
---
+++ sys/pci/smc83c170.h
@@ -0,0 +1,272 @@
+/*-
+ * Copyright (c) 1997 Semen Ustimenko
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+/*
+ * smc83c170.h
+ */
+
+/*
+ * Configuration
+ */
+#define EPIC_MAX_DEVICES	4
+/*#define	RX_TO_MBUF	1*/		/* IT IS BUGGY */
+/*#define	EPIC_DEBUG	1*/
+#define TX_RING_SIZE	16
+#define RX_RING_SIZE	16
+
+/*
+ * Media control
+ */
+#ifndef	IFF_ALTPHYS
+#define	IFF_ALTPHYS	IFF_LINK2
+#endif
+#define IFF_100MB	IFF_ALTPHYS
+
+#ifndef	IFF_FULLDUPLEX
+#define	IFF_FULLDUPLEX	IFF_LINK1
+#endif
+
+#ifndef	IFF_NOAUTONEG
+#define	IFF_NOAUTONEG	IFF_LINK0
+#endif
+
+#define ETHER_MAX_FRAME_LEN	(ETHER_MAX_LEN + ETHER_CRC_LEN)
+
+/* Shall be moved to ../net/if_mib.h */
+#define	dot3VendorSMC		8
+#define	dot3ChipSetSMC83c170	1
+
+/* PCI identification */
+#define SMC_VENDORID		0x10B8
+#define CHIPID_83C170		0x0005
+#define	PCI_VENDORID(x)		((x) & 0xFFFF)
+#define	PCI_CHIPID(x)		(((x) >> 16) & 0xFFFF)
+
+/* PCI configuration */
+#define	PCI_CFID	0x00	/* Configuration ID */
+#define	PCI_CFCS	0x04	/* Configurtion Command/Status */
+#define	PCI_CFRV	0x08	/* Configuration Revision */
+#define	PCI_CFLT	0x0c	/* Configuration Latency Timer */
+#define	PCI_CBIO	0x10	/* Configuration Base IO Address */
+#define	PCI_CBMA	0x14	/* Configuration Base Memory Address */
+#define	PCI_CFIT	0x3c	/* Configuration Interrupt */
+#define	PCI_CFDA	0x40	/* Configuration Driver Area */
+
+#define	PCI_CONF_WRITE(r, v)	pci_conf_write(config_id, (r), (v))
+#define	PCI_CONF_READ(r)	pci_conf_read(config_id, (r))
+
+/* EPIC's registers */
+#define	COMMAND		0x0000
+#define	INTSTAT		0x0004		/* Interrupt status. See below */
+#define	INTMASK		0x0008		/* Interrupt mask. See below */
+#define	GENCTL		0x000C
+#define	NVCTL		0x0010
+#define	EECTL		0x0014		/* EEPROM control **/
+#define	TEST1		0x001C		/* XXXXX */
+#define	CRCCNT		0x0020		/* CRC error counter */
+#define	ALICNT		0x0024		/* FrameTooLang error counter */
+#define	MPCNT		0x0028		/* MissedFrames error counters */
+#define	MIICTL		0x0030
+#define	MIIDATA		0x0034
+#define	MIICFG		0x0038
+#define IPG		0x003C
+#define	LAN0		0x0040		/* MAC address */
+#define	LAN1		0x0044		/* MAC address */
+#define	LAN2		0x0048		/* MAC address */
+#define	ID_CHK		0x004C
+#define	MC0		0x0050		/* Multicast filter table */
+#define	MC1		0x0054		/* Multicast filter table */
+#define	MC2		0x0058		/* Multicast filter table */
+#define	MC3		0x005C		/* Multicast filter table */
+#define	RXCON		0x0060		/* Rx control register */
+#define	TXCON		0x0070		/* Tx control register */
+#define	TXSTAT		0x0074
+#define	PRCDAR		0x0084		/* RxRing bus address */
+#define	PRSTAT		0x00A4
+#define	PRCPTHR		0x00B0
+#define	PTCDAR		0x00C4		/* TxRing bus address */
+#define	ETXTHR		0x00DC
+
+#define	COMMAND_STOP_RX		0x01
+#define	COMMAND_START_RX	0x02
+#define	COMMAND_TXQUEUED	0x04
+#define	COMMAND_RXQUEUED	0x08
+#define	COMMAND_NEXTFRAME	0x10
+#define	COMMAND_STOP_TDMA	0x20
+#define	COMMAND_STOP_RDMA	0x40
+#define	COMMAND_TXUGO		0x80
+
+/* Tx threshold */
+#define TX_FIFO_THRESH	0x80		/* 0x40 or 0x10 */
+
+/* Interrupt register bits */
+#define INTSTAT_RCC	0x00000001
+#define INTSTAT_HCC	0x00000002
+#define INTSTAT_RQE	0x00000004
+#define INTSTAT_OVW	0x00000008	
+#define INTSTAT_RXE	0x00000010	
+#define INTSTAT_TXC	0x00000020
+#define INTSTAT_TCC	0x00000040	
+#define INTSTAT_TQE	0x00000080	
+#define INTSTAT_TXU	0x00000100
+#define INTSTAT_CNT	0x00000200
+#define INTSTAT_RCT	0x00000400
+#define INTSTAT_DPE	0x00000800	
+#define INTSTAT_APE	0x00001000	
+#define INTSTAT_PMA	0x00002000
+#define INTSTAT_PTA	0x00004000	
+#define INTSTAT_RSV	0x00008000	
+#define INTSTAT_INT_ACTV 0x00010000
+#define INTSTAT_RXIDLE	0x00020000
+#define INTSTAT_TXIDLE	0x00040000
+#define INTSTAT_RCIP	0x00080000	
+#define INTSTAT_TCIP	0x00100000	
+#define INTSTAT_RBE	0x00200000
+#define INTSTAT_RCTS	0x00400000	
+
+#define	GENCTL_SOFT_RESET		0x00000001
+#define	GENCTL_ENABLE_INTERRUPT		0x00000002
+#define	GENCTL_SOFTWARE_INTERRUPT	0x00000004
+#define	GENCTL_POWER_DOWN		0x00000008
+#define	GENCTL_ONECOPY			0x00000010
+#define	GENCTL_BIG_ENDIAN		0x00000020
+#define	GENCTL_RECEIVE_DMA_PRIORITY	0x00000040
+#define	GENCTL_TRANSMIT_DMA_PRIORITY	0x00000080
+#define	GENCTL_RECEIVE_FIFO_THRESHOLD128	0x00000300
+#define	GENCTL_RECEIVE_FIFO_THRESHOLD96	0x00000200
+#define	GENCTL_RECEIVE_FIFO_THRESHOLD64	0x00000100
+#define	GENCTL_RECEIVE_FIFO_THRESHOLD32	0x00000000
+#define	GENCTL_MEMORY_READ_LINE		0x00000400
+#define	GENCTL_MEMORY_READ_MULTIPLE	0x00000800
+#define	GENCTL_SOFTWARE1		0x00001000
+#define	GENCTL_SOFTWARE2		0x00002000
+#define	GENCTL_RESET_PHY		0x00004000
+
+#define	RXCON_SAVE_ERRORED_PACKETS	0x00000001
+#define	RXCON_RECEIVE_RUNT_FRAMES	0x00000002
+#define	RXCON_RECEIVE_BROADCAST_FRAMES	0x00000004
+#define	RXCON_RECEIVE_MULTICAST_FRAMES	0x00000008
+#define	RXCON_RECEIVE_INVERSE_INDIVIDUAL_ADDRESS_FRAMES	0x00000010
+#define	RXCON_PROMISCUOUS_MODE		0x00000020
+#define	RXCON_MONITOR_MODE		0x00000040
+#define	RXCON_EARLY_RECEIVE_ENABLE	0x00000080
+#define	RXCON_EXTERNAL_BUFFER_DISABLE	0x00000000
+#define	RXCON_EXTERNAL_BUFFER_16K	0x00000100
+#define	RXCON_EXTERNAL_BUFFER_32K	0x00000200
+#define	RXCON_EXTERNAL_BUFFER_128K	0x00000300
+
+#define TXCON_EARLY_TRANSMIT_ENABLE	0x00000001
+#define TXCON_LOOPBACK_DISABLE		0x00000000
+#define TXCON_LOOPBACK_MODE_INT		0x00000002
+#define TXCON_LOOPBACK_MODE_PHY		0x00000004
+#define TXCON_LOOPBACK_MODE_FULL_DUPLEX	0x00000006
+
+/*
+ * Structures definition and Functions prototypes
+ */
+
+/* EPIC's descriptors */
+struct epic_tx_desc {
+	u_int16_t	status;
+	u_int16_t	txlength;
+	u_int32_t	bufaddr;
+	u_int16_t	buflength;
+	u_int16_t	control;
+	u_int32_t	next;
+};
+struct epic_rx_desc {
+	u_int16_t	status;
+	u_int16_t	rxlength;
+	u_int32_t	bufaddr;
+	u_int32_t	buflength;
+	u_int32_t	next;
+};
+
+struct epic_rx_buffer {
+	struct epic_rx_desc	desc;		/* EPIC's descriptor */
+	caddr_t			data;		/* Rx buffer address */
+#if !defined(RX_TO_MBUF)
+	caddr_t			pool;		/* Pool, allocated for buffer */
+#else
+	struct mbuf *		mbuf;		/* Or mbuf structure */
+#endif
+};
+
+struct epic_tx_buffer {
+	struct epic_tx_desc	desc;		/* EPIC's descriptor */
+	caddr_t			data;		/* Tx buffer address */
+	caddr_t			pool;		/* Pool, allocated for buffer */
+};
+
+typedef struct {
+	int			unit;
+	struct arpcom		epic_ac;
+	struct epic_rx_buffer	rx_buffer[RX_RING_SIZE];
+	struct epic_tx_buffer	tx_buffer[TX_RING_SIZE];
+	u_int32_t		rxcon;
+	u_int32_t		media;
+	u_int32_t		txcon;
+	u_int32_t		genctl;
+	u_int32_t		cur_tx;
+	u_int32_t		cur_rx;
+	u_int32_t		dirty_tx;
+	u_int32_t		pending_txs;
+	u_int32_t		iobase;
+	u_int32_t		irq;
+	struct ifmib_iso_8802_3	dot3stats;
+} epic_softc_t;
+
+#define epic_if epic_ac.ac_if
+#define epic_macaddr epic_ac.ac_enaddr
+
+//extern epic_softc_t *epics[];
+//extern u_long epic_pci_count;
+
+static int epic_ifioctl(register struct ifnet *, int, caddr_t);
+static void epic_intr_normal(void *);
+static void epic_rx_done(epic_softc_t *);
+static void epic_tx_done(epic_softc_t *);
+static void epic_ifstart(struct ifnet *);
+static void epic_ifwatchdog(struct ifnet *);
+
+static char* epic_pci_probe(pcici_t, pcidi_t);
+static void epic_pci_attach(pcici_t, int);
+static void epic_update_if_media_flags(epic_softc_t *);
+static void epic_init(epic_softc_t *);
+static void epic_stop(epic_softc_t *);
+
+static void epic_init_rings(epic_softc_t *);
+
+static int epic_read_eeprom(u_int16_t,u_int16_t);
+static void epic_output_eepromw(u_int16_t, u_int16_t);
+static u_int16_t epic_input_eepromw(u_int16_t);
+static u_int8_t epic_eeprom_clock(u_int16_t,u_int8_t);
+static void epic_write_eepromreg(u_int16_t,u_int8_t);
+static u_int8_t epic_read_eepromreg(u_int16_t);
+
+static int epic_read_phy_register(u_int16_t, u_int16_t);
+static void epic_write_phy_register(u_int16_t, u_int16_t,u_int16_t);
