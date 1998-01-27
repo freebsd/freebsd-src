b7b37d2099aa20beccbd2135dcba9b260ea0fa4c
Brian Somers <brian@FreeBSD.org>
1998-01-27 23:16:24 +0000

    MFC: Cosmetic copyright changes.

--- usr.sbin/ppp/main.c
+++ usr.sbin/ppp/main.c
@@ -17,7 +17,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: main.c,v 1.22.2.43 1997/10/24 23:15:40 brian Exp $
+ * $Id: main.c,v 1.22.2.44 1998/01/26 20:04:58 brian Exp $
  *
  *	TODO:
  *		o Add commands for traffic summary, version display, etc.
@@ -342,15 +342,6 @@ ProcessArgs(int argc, char **argv)
   return argc == 1 ? *argv : NULL;	/* Don't SetLabel yet ! */
 }
 
-static void
-Greetings(void)
-{
-  if (VarTerm) {
-    fprintf(VarTerm, "User Process PPP. Written by Toshiharu OHNO.\n");
-    fflush(VarTerm);
-  }
-}
-
 int
 main(int argc, char **argv)
 {
@@ -409,7 +400,7 @@ main(int argc, char **argv)
 
   if (!GetShortHost())
     return 1;
-  Greetings();
+  IsInteractive(1);
   IpcpDefAddress();
 
   if (mode & MODE_INTER)
@@ -423,9 +414,7 @@ main(int argc, char **argv)
     return EX_START;
   }
   CleanInterface(IfDevName);
-  if (mode & MODE_INTER)
-    fprintf(VarTerm, "Interactive mode\n");
-  else if ((mode & MODE_OUTGOING_DAEMON) && !(mode & MODE_DEDICATED))
+  if ((mode & MODE_OUTGOING_DAEMON) && !(mode & MODE_DEDICATED))
     if (label == NULL) {
       if (VarTerm)
 	fprintf(VarTerm, "Destination system must be specified in"
@@ -1003,7 +992,6 @@ DoLoop(void)
       netfd = wfd;
       VarTerm = fdopen(netfd, "a+");
       LocalAuthInit();
-      Greetings();
       IsInteractive(1);
       Prompt();
     }
