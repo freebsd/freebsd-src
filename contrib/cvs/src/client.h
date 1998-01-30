c3a8ee0e80a59793349940056dfd14746ebd4905
peter <peter@FreeBSD.org>
1998-01-30 15:31:47 +0000

    Import cvs-1.9.24 since it came quite soon after the snapshot that was imported a few days ago.  There is very little difference, except that the remote protocol apparently supports wrappers and -k options better.

--- contrib/cvs/src/client.h
+++ contrib/cvs/src/client.h
@@ -139,6 +139,8 @@ struct response
      * Function to carry out the response.  ARGS is the text of the
      * command after name and, if present, a single space, have been
      * stripped off.  The function can scribble into ARGS if it wants.
+     * Note that although LEN is given, ARGS is also guaranteed to be
+     * '\0' terminated.
      */
     void (*func) PROTO((char *args, int len));
 
