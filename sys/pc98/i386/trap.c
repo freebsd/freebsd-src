3efe1bb939122a6e8330021cf6631db905c6bf6c
KATO Takenori <kato@FreeBSD.org>
1998-02-05 10:05:47 +0000

    Sync with sys/i386/i386/machdep.c and trap.c reivsions 1.287 and 1.121, respectively.

--- sys/pc98/i386/trap.c
+++ sys/pc98/i386/trap.c
@@ -35,7 +35,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)trap.c	7.4 (Berkeley) 5/13/91
- *	$Id: trap.c,v 1.42 1997/12/06 06:19:19 kato Exp $
+ *	$Id: trap.c,v 1.43 1998/01/31 15:16:27 kato Exp $
  */
 
 /*
@@ -44,6 +44,7 @@
 
 #include "opt_cpu.h"
 #include "opt_ddb.h"
+#include "opt_diagnostic.h"
 #include "opt_ktrace.h"
 #include "opt_trap.h"
 #include "opt_vm86.h"
