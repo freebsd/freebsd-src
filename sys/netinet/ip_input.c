9db44542ddce279cd4d9399c352f0e7d4b4f37a7
guido <guido@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-02-16 19:23:58 +0000

    Add new sysctl variable: net.inet.ip.accept_sourceroute It controls if the system is to accept source routed packets. It used to be such that, no matter if the setting of net.inet.ip.sourceroute, source routed packets destined at us would be accepted. Now it is controllable with eth default set to NOT accept those.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@33440 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/ip_input.c
+++ sys/netinet/ip_input.c
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)ip_input.c	8.2 (Berkeley) 1/4/94
- * $Id: ip_input.c,v 1.76 1998/02/11 18:43:42 guido Exp $
+ * $Id: ip_input.c,v 1.77 1998/02/12 03:37:45 ache Exp $
  *	$ANA: ip_input.c,v 1.5 1996/09/18 14:34:59 wollman Exp $
  */
 
@@ -94,6 +94,10 @@ SYSCTL_INT(_net_inet_ip, IPCTL_DEFTTL, ttl, CTLFLAG_RW,
 static int	ip_dosourceroute = 0;
 SYSCTL_INT(_net_inet_ip, IPCTL_SOURCEROUTE, sourceroute, CTLFLAG_RW,
 	&ip_dosourceroute, 0, "");
+
+static int	ip_acceptsourceroute = 0;
+SYSCTL_INT(_net_inet_ip, IPCTL_ACCEPTSOURCEROUTE, accept_sourceroute,
+	CTLFLAG_RW, &ip_acceptsourceroute, 0, "");
 #ifdef DIAGNOSTIC
 static int	ipprintfs = 0;
 #endif
@@ -965,6 +969,8 @@ ip_dooptions(m)
 				/*
 				 * End of source route.  Should be for us.
 				 */
+				if (!ip_acceptsourceroute)
+					goto nosourcerouting;
 				save_rte(cp, ip->ip_src);
 				break;
 			}
