975b45e5c370206aa87fec71de2176975e32d08b
dima <dima@FreeBSD.org>
1998-02-18 20:15:49 +0000

    MFC: NO_LKM option

--- sys/kern/kern_lkm.c
+++ sys/kern/kern_lkm.c
@@ -30,8 +30,10 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: kern_lkm.c,v 1.32 1996/09/03 22:52:14 bde Exp $
+ * $Id: kern_lkm.c,v 1.32.2.1 1997/02/22 20:50:43 joerg Exp $
  */
+
+#include "opt_no_lkm.h"
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -936,6 +938,6 @@ static void 	lkm_drvinit(void *unused)
     	}
 }
 
+#ifndef NO_LKM
 SYSINIT(lkmdev,SI_SUB_DRIVERS,SI_ORDER_MIDDLE+CDEV_MAJOR,lkm_drvinit,NULL)
-
-
+#endif
