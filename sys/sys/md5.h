52d420ba99614d85559e2adeca14106986f88dde
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-02-18 02:28:14 +0000

    MFC: constify

--- sys/sys/md5.h
+++ sys/sys/md5.h
@@ -1,5 +1,5 @@
 /* MD5.H - header file for MD5C.C
- * $Id$
+ * $Id: md5.h,v 1.6 1996/12/22 10:31:34 phk Exp $
  */
 
 /* Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
@@ -37,6 +37,6 @@ void   MD5Init (MD5_CTX *);
 void   MD5Update (MD5_CTX *, const unsigned char *, unsigned int);
 void   MD5Final (unsigned char [16], MD5_CTX *);
 char * MD5End(MD5_CTX *, char *);
-char * MD5File(char *, char *);
+char * MD5File(const char *, char *);
 char * MD5Data(const unsigned char *, unsigned int, char *);
 #endif /* _SYS_MD5_H_ */
