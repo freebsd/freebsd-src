f287ca6aaa41c2c357a474b6be0c3f06cc779b80
Brian Somers <brian@FreeBSD.org>
1998-02-18 20:41:35 +0000

    MFC: LogWARN rather than LogERROR when we can't create      our diagnostic socket.

--- usr.sbin/ppp/server.c
+++ usr.sbin/ppp/server.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id$
+ *	$Id: server.c,v 1.1.2.6 1998/01/26 20:05:17 brian Exp $
  */
 
 #include <sys/param.h>
@@ -59,13 +59,13 @@ ServerLocalOpen(const char *name, mode_t mask)
   int s;
 
   if (VarLocalAuth == LOCAL_DENY) {
-    LogPrintf(LogERROR, "Local: Can't open socket %s: No password "
+    LogPrintf(LogWARN, "Local: Can't open socket %s: No password "
 	      "in ppp.secret\n", name);
     return 1;
   }
 
   if (mode & MODE_INTER) {
-    LogPrintf(LogERROR, "Local: Can't open socket in interactive mode\n");
+    LogPrintf(LogWARN, "Local: Can't open socket in interactive mode\n");
     return 1;
   }
 
@@ -117,13 +117,13 @@ ServerTcpOpen(int port)
   int s;
 
   if (VarLocalAuth == LOCAL_DENY) {
-    LogPrintf(LogERROR, "Tcp: Can't open socket %d: No password "
+    LogPrintf(LogWARN, "Tcp: Can't open socket %d: No password "
 	      "in ppp.secret\n", port);
     return 6;
   }
 
   if (mode & MODE_INTER) {
-    LogPrintf(LogERROR, "Tcp: Can't open socket in interactive mode\n");
+    LogPrintf(LogWARN, "Tcp: Can't open socket in interactive mode\n");
     return 6;
   }
 
