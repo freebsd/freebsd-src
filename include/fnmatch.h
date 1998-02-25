18e07ad820bc1e693886ae0d3f16d3f3dd5436ef
bde <bde@FreeBSD.org>
1998-02-25 02:15:59 +0000

    Removed _POSIX_SOURCE ifdefs.  This is not a POSIX.1 header.

--- include/fnmatch.h
+++ include/fnmatch.h
@@ -41,11 +41,9 @@
 #define	FNM_NOESCAPE	0x01	/* Disable backslash escaping. */
 #define	FNM_PATHNAME	0x02	/* Slash must be matched by slash. */
 #define	FNM_PERIOD	0x04	/* Period must be matched by period. */
-#ifndef _POSIX_SOURCE
 #define	FNM_LEADING_DIR	0x08	/* Ignore /<tail> after Imatch. */
 #define	FNM_CASEFOLD	0x10	/* Case insensitive search. */
 #define	FNM_IGNORECASE	FNM_CASEFOLD
-#endif
 
 #include <sys/cdefs.h>
 
