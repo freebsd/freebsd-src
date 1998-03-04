540d82e3b41d64f8d080803fcfb1dbd05d9eeb99
Tor Egge <tegge@FreeBSD.org>
1998-03-04 23:45:12 +0000

    Disable redirection of clock interrupt to a higher priority vector. This does not work on most dual Pentium machines, due to setidt() being neutralized by f00f_hack().

--- sys/i386/include/smptests.h
+++ sys/i386/include/smptests.h
@@ -22,7 +22,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: smptests.h,v 1.28 1997/12/08 22:56:49 fsmp Exp $
+ *	$Id: smptests.h,v 1.29 1998/03/03 22:56:26 tegge Exp $
  */
 
 #ifndef _MACHINE_SMPTESTS_H_
@@ -278,7 +278,7 @@
  * while still using the slow interrupt handler. Only effective when 
  * APIC_INTR_REORDER is defined.
  */
-#define APIC_INTR_HIGHPRI_CLOCK
+/* #define APIC_INTR_HIGHPRI_CLOCK */
 
 #endif /* APIC_IO */
 
