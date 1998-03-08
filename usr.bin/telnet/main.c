e471fa317cfd28d3cc5859db8fc1e82bacb2b67b
jkh <jkh@FreeBSD.org>
1998-03-08 13:48:34 +0000

    MFC: update man page, cosmetic tweak.

--- usr.bin/telnet/main.c
+++ usr.bin/telnet/main.c
@@ -135,7 +135,7 @@ main(argc, argv)
 	rlogin = (strncmp(prompt, "rlog", 4) == 0) ? '~' : _POSIX_VDISABLE;
 	autologin = -1;
 
-	while ((ch = getopt(argc, argv, "8EKLS:X:acde:fFk:l:n:rt:x")) !=  -1) {
+	while ((ch = getopt(argc, argv, "8EKLS:X:acde:fFk:l:n:rt:x")) != -1) {
 		switch(ch) {
 		case '8':
 			eight = 3;	/* binary output and input */
