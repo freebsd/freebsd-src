cf313fc2130e122629cabda8c4985bdb04f1b111
jkh <jkh@FreeBSD.org>
1998-03-08 07:56:02 +0000

    MFC: proper parens

--- usr.bin/colrm/colrm.c
+++ usr.bin/colrm/colrm.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: colrm.c,v 1.1.1.1.8.3 1997/08/29 05:29:02 imp Exp $
+ *	$Id: colrm.c,v 1.1.1.1.8.4 1997/09/14 23:17:17 jkh Exp $
  */
 
 #ifndef lint
@@ -116,7 +116,7 @@ main(argc, argv)
 			break;
 		}
 
-		if ((!start || column < start || stop && column > stop) &&
+		if ((!start || column < start || (stop && column > stop)) &&
 		    putchar(ch) == EOF)
 			check(stdout);
 	}
