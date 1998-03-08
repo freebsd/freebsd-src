324320a0b2e525ba19d0d7196c5651704287125f
jkh <jkh@FreeBSD.org>
1998-03-08 01:52:31 +0000

    MFC: don't clobber flags.

--- sys/kern/subr_dkbad.c
+++ sys/kern/subr_dkbad.c
@@ -43,7 +43,7 @@
  *	from: wd.c,v 1.55 1994/10/22 01:57:12 phk Exp $
  *	from: @(#)ufs_disksubr.c	7.16 (Berkeley) 5/4/91
  *	from: ufs_disksubr.c,v 1.8 1994/06/07 01:21:39 phk Exp $
- *	$Id: subr_dkbad.c,v 1.3 1996/09/14 01:04:18 bde Exp $
+ *	$Id: subr_dkbad.c,v 1.4 1996/09/20 17:39:22 bde Exp $
  */
 
 #include <sys/param.h>
@@ -117,7 +117,8 @@ readbad144(dev, strat, lp, bdp)
 		else
 			bp->b_blkno /= DEV_BSIZE / lp->d_secsize;
 		bp->b_bcount = lp->d_secsize;
-		bp->b_flags = B_BUSY | B_READ;
+		bp->b_flags |= B_BUSY | B_READ;
+		bp->b_flags &= ~B_ERROR;
 		(*strat)(bp);
 
 		/* If successful, validate, otherwise try another. */
