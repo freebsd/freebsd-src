a987e5c779a322b866578a6ec20c633542028d28
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-03-08 05:29:49 +0000

    First round of changes to support generation of assembler for the old a.out gas and the binutils gas (elf or a.out) with a single compiler.
    This uses other infrastructure not yet committed, in order to support
    both a.out and elf it needs to be able to get to both a.out and elf
    gas, ld, libs, crt* etc.  So for now, the support is pretty much dormant.
    
    The new freebsd.h file is based on the old freebsd-elf.h file (which has a
    long lineage, right back through linux and svr4 files).  The change is
    pretty dramatic from a gcc internals standpoint as it overrides a lot of
    definitions in order to generate different output based on target mode.
    There is potential for screw-ups, so please be on the lookout - gcc's
    configuration mechanism wasn't really meant for this kind of thing.
    It's believed to compile world etc just fine under both a.out and elf, can
    handle global constructors and destructors, handles the differences in
    a.out and elf stabs, and what sections things like exceptions go in.
    
    The initial idea came from i386/osfrose.h which is a dual rose/elf format
    target.  These two are not as diverse as a.out and elf it would seem.
    
    The cc front-end uses external configuration to determine default object
    format (still being thrashed out, so read the source if you want to see
    it so far), and has a '-aout' and '-elf' override command line switch.
    There are some other internal switches that can be accessed, namely -maout,
    -mno-aout, -munderscores and -mnounderscores.  The underscore and local
    symbol prefixing rules are controllable seperately to the output format.
    (ie: it's possible to generate a.out without the _ prefixes on symbols and
    also to generate elf with the _ prefixes.  This isn't quite optimal, but
    does seem to work pretty well, except the linkers don't always recognise
    the local symbols without their normal names)
    
    The default format is a.out (still), nobody should see any major changes.
    
    With both elf and a.out tools and libraries installed:
    
    [1:26pm]/tmp-223> cc -elf -o hello hello.c
    peter@beast[1:27pm]/tmp-224> file hello
    hello: ELF 32-bit LSB executable, Intel 80386, version 1 (FreeBSD), dynamically linked, not stripped
    [1:27pm]/tmp-225> ./hello
    hello world!
    
    [1:27pm]/tmp-226> cc -aout -o hello hello.c
    [1:27pm]/tmp-227> file hello
    hello: FreeBSD/i386 compact demand paged dynamically linked executable not stripped
    1:27pm]/tmp-228> ./hello
    hello world!
    
    Since my co-conspirators put a lot of effort into this too, I'll add them
    so they can share the blame^H^H^H^H^Hglory. :-)
    
    Reviewed by: sos, jdp
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@34229 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/gcc/function.c
+++ contrib/gcc/function.c
@@ -4887,18 +4887,23 @@ mark_varargs ()
 #define HAS_INIT_SECTION
 #endif
 
+#ifndef GEN_CALL__MAIN
+#ifndef HAS_INIT_SECTION
+#define GEN_CALL__MAIN \
+  do {									\
+    emit_library_call (gen_rtx (SYMBOL_REF, Pmode, NAME__MAIN), 0,	\
+		       VOIDmode, 0);					\
+  } while (0)
+#endif
+#endif
+
 void
 expand_main_function ()
 {
+#ifdef GEN_CALL__MAIN
   if (!output_bytecode)
-    {
-      /* The zero below avoids a possible parse error */
-      0;
-#if !defined (HAS_INIT_SECTION)
-      emit_library_call (gen_rtx (SYMBOL_REF, Pmode, NAME__MAIN), 0,
-			 VOIDmode, 0);
-#endif /* not HAS_INIT_SECTION */
-    }
+    GEN_CALL__MAIN;
+#endif
 }
 
 extern struct obstack permanent_obstack;
