bbfbfec7a4df45caf8fd3218601f7003eda6c41a
jkh <jkh@FreeBSD.org>
1998-03-08 12:19:06 +0000

    MFC: cosmetic.

--- usr.bin/split/split.c
+++ usr.bin/split/split.c
@@ -75,7 +75,7 @@ main(argc, argv)
 	int ch;
 	char *ep, *p;
 
-	while ((ch = getopt(argc, argv, "-0123456789b:l:")) !=  -1)
+	while ((ch = getopt(argc, argv, "-0123456789b:l:")) != -1)
 		switch (ch) {
 		case '0': case '1': case '2': case '3': case '4':
 		case '5': case '6': case '7': case '8': case '9':
