a165a47d4a8d7b82926e1642053a599769143670
andreas <andreas@FreeBSD.org>
1998-03-09 21:54:20 +0000

    set timeout to 4 hours, large disks take a long time to low level format.

--- sbin/scsiformat/scsiformat.sh
+++ sbin/scsiformat/scsiformat.sh
@@ -26,7 +26,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # 
-# $Id: scsiformat.sh,v 1.5 1997/03/02 11:21:07 joerg Exp $
+# $Id: scsiformat.sh,v 1.6 1998/03/01 20:15:00 joerg Exp $
 #
 
 PATH="/sbin:/usr/sbin:/bin:/usr/bin"; export PATH
@@ -138,7 +138,7 @@ then
 		echo -n "Hit return to continue, or INTR (^C) to abort: "
 		read dummy
 	fi
-	# formatting may take a huge amount of time, set timeout to 2 hours
+	# formatting may take a huge amount of time, set timeout to 4 hours
 	echo "Formatting... this may take a while."
-	scsi -s 10800 -f $RAW -c "4 0 0 0 0 0"
+	scsi -s 14400 -f $RAW -c "4 0 0 0 0 0"
 fi
