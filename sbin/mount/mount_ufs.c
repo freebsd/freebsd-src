b92157c4c51b79dfab57bdf442f5b7612ee56eb5
msmith <msmith@FreeBSD.org>
1998-03-09 02:06:27 +0000

    Merge from -current: search for the right slice entry to match the one mounted on / when supplied with a possible compatability-slice devicename.

--- sbin/mount/mount_ufs.c
+++ sbin/mount/mount_ufs.c
@@ -76,6 +76,10 @@ mount_ufs(argc, argv)
 	int ch, mntflags;
 	char *fs_name;
 	struct vfsconf *vfc;
+#ifdef ROOTSLICE_HUNT
+	int unit, slice, result;
+	char part, devbuf[MAXPATHLEN], devpfx[MAXPATHLEN];
+#endif
 
 	mntflags = 0;
 	optind = optreset = 1;		/* Reset for parse of new argv. */
@@ -117,8 +121,37 @@ mount_ufs(argc, argv)
 			/*warnx("ufs: filesystem not found");*/
 		}
 	}
-
+#ifdef ROOTSLICE_HUNT
+	result = -1;
+	/*
+	 * If we are mounting root, and we have a mount of something that
+	 * might be the compatability slice, try mounting other slices
+	 * first.  If the kernel has done the right thing and mounted
+	 * the slice because the disk is really sliced, this will find
+	 * the real root filesystem.  If not, we'll try what was supplied.
+	 */
+	if (!strcmp(fs_name, "/") &&
+	    (sscanf(args.fspec, "%[^0-9]%d%c", devpfx, &unit, &part) == 3) &&
+	    (part >= 'a') &&
+	    (part <= 'h')) {
+		for (slice = 1; (slice < 32) && (result < 0); slice++) {
+			sprintf(devbuf, "%s%ds%d%c",
+				devpfx, unit, slice, part);
+			args.fspec = devbuf;
+			result = mount(vfc ? vfc->vfc_index : MOUNT_UFS,
+					fs_name, mntflags, &args);
+		}
+		args.fspec = argv[0];
+	}
+	if (result == 0)
+		warnx("*** update /etc/fstab entry for %s to use %s ***",
+			fs_name, devbuf);
+	/* Try the mount as originally planned */
+	if ((result < 0) &&
+		(mount(vfc ? vfc->vfc_index : MOUNT_UFS, fs_name, mntflags, &args) < 0)) {
+#else
 	if (mount(vfc ? vfc->vfc_index : MOUNT_UFS, fs_name, mntflags, &args) < 0) {
+#endif
 		(void)fprintf(stderr, "%s on %s: ", args.fspec, fs_name);
 		switch (errno) {
 		case EMFILE:
