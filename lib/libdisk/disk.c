534868f7e84e8b6e808c0beb9ab7ab127bc984fa
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-03-20 23:27:20 +0000

    Teach libdisk about wfd drives.

--- lib/libdisk/disk.c
+++ lib/libdisk/disk.c
@@ -6,7 +6,7 @@
  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
  * ----------------------------------------------------------------------------
  *
- * $Id: disk.c,v 1.22.2.5 1998/03/07 22:04:12 ache Exp $
+ * $Id: disk.c,v 1.22.2.6 1998/03/14 21:15:52 ache Exp $
  *
  */
 
@@ -289,7 +289,7 @@ Collapse_Disk(struct disk *d)
 }
 #endif
 
-static char * device_list[] = {"wd","sd","da","od",0};
+static char * device_list[] = {"wd","sd","wfd","od",0};
 
 char **
 Disk_Names()
