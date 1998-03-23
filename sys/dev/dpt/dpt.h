6ea157ac6d9228597f68c235a9d4c8e478a96931
jkh <jkh@FreeBSD.org>
1998-03-23 07:57:34 +0000

    MFC: update copyrights.
    Approved by:	Simon Shapiro <shimon@simon-shapiro.org>

--- sys/dev/dpt/dpt.h
+++ sys/dev/dpt/dpt.h
@@ -1,13 +1,31 @@
 /*
- *       Copyright (c) 1997 by Simon Shapiro
+ *
+ * Copyright (c) 1997 by Simon Shapiro
  *       All Rights Reserved
  *
- * This is a proprietary, unpublished source code.  No publishing, copying,
- * distribution or use permission is granted to anyone.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions, and the following disclaimer,
+ *    without modification, immediately at the beginning of the file.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
  *
- * If you want to use this product in any way, please contact the author by
- * sending email to shimon@i-connect.net
- *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 /*
@@ -22,7 +40,7 @@
  */
 
 
-#ident "$Id: dpt.h,v 1.1.2.1 1998/03/06 23:44:05 julian Exp $"
+#ident "$Id: dpt.h,v 1.1.2.2 1998/03/08 10:58:11 julian Exp $"
 
 #ifndef _DPT_H
 #define _DPT_H
