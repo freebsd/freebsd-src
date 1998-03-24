5e3be559df95bb3bf7a3b0f9f5e3343c72f3a797
asami <asami@FreeBSD.org>
1998-03-24 03:35:54 +0000

    Merge 1.273.

--- share/mk/bsd.port.mk
+++ share/mk/bsd.port.mk
@@ -1,7 +1,7 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4
 #
-#	$Id: bsd.port.mk,v 1.227.2.33 1998/02/19 06:51:32 asami Exp $
+#	$Id: bsd.port.mk,v 1.227.2.34 1998/03/07 04:41:14 asami Exp $
 #	$NetBSD: $
 #
 #	bsd.port.mk - 940820 Jordan K. Hubbard.
@@ -1147,7 +1147,10 @@ package-links:
 
 .if !target(delete-package-links)
 delete-package-links:
-	@${RM} -f ${PACKAGES}/[a-z]*/${PKGNAME}${PKG_SUFX} ${PKGLATESTFILE}
+	@${RM} -f ${PACKAGES}/[a-z]*/${PKGNAME}${PKG_SUFX}
+.if !defined(NO_LATEST_LINK)
+	@${RM} -f ${PKGLATESTFILE}
+.endif
 .endif
 
 .if !target(delete-package)
