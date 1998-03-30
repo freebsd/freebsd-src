5c08bc788d5691cc6168ee07c00f48439fac3701
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-03-30 10:10:29 +0000

    Pickup _time_second instead of _time
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@34963 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/savecore/savecore.c
+++ sbin/savecore/savecore.c
@@ -70,7 +70,7 @@ struct nlist current_nl[] = {	/* Namelist for currently running system. */
 #define X_DUMPLO	1
 	{ "_dumplo" },
 #define X_TIME		2
-	{ "_time" },
+	{ "_time_second" },
 #define	X_DUMPSIZE	3
 	{ "_dumpsize" },
 #define X_VERSION	4
@@ -87,7 +87,7 @@ int dumpsyms[] = { X_TIME, X_DUMPSIZE, X_VERSION, X_PANICSTR, X_DUMPMAG, -1 };
 struct nlist dump_nl[] = {	/* Name list for dumped system. */
 	{ "_dumpdev" },		/* Entries MUST be the same as */
 	{ "_dumplo" },		/*	those in current_nl[].  */
-	{ "_time" },
+	{ "_time_second" },
 	{ "_dumpsize" },
 	{ "_version" },
 	{ "_panicstr" },
