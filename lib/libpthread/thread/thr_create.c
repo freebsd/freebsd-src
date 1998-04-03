a8e4a3f0a9def8d4a7584a265ea4f7e6e619fd7f
jb <jb@FreeBSD.org>
1998-04-03 09:31:15 +0000

    Add a magic field to the pthread structure to help recognize valid threads from invalid ones. The pthread structure is opaque to the user so this change does not cause any incompatibilities.
    Hopefully this change will help code that was written for draft 4
    fail gracefully if the programmer ignores the compiler warning about
    the change in the level of indirection for the argument passed to
    pthread_detach(). I got burnt, so I fixed then (expletive deleted)
    thing.
    
    These functions comply with the revised standard. That should shut
    Terry up!

--- lib/libpthread/thread/thr_create.c
+++ lib/libpthread/thread/thr_create.c
@@ -60,6 +60,12 @@ _thread_create(pthread_t * thread, const pthread_attr_t * attr,
 		/* Insufficient memory to create a thread: */
 		ret = EAGAIN;
 	} else {
+		/*
+		 * Write a magic value to the thread structure to help
+		 * identify valid ones:
+		 */
+		new_thread->magic = PTHREAD_MAGIC;
+
 		/* Check if default thread attributes are required: */
 		if (attr == NULL || *attr == NULL) {
 			/* Use the default thread attributes: */
