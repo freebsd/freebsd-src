1e9e791cf27d2f9861f0bcc8ddf821c33d973c65
phk <phk@FreeBSD.org>
1998-04-12 11:15:54 +0000

    openlog() needs to have LOG_NDELAY added, or else the syslog() calls after the chroot will not get sent to syslogd.
    PR:		4910
    Reviewed by:	phk
    Submitted by:	Jim Mercer <jim@komodo.reptiles.org>

--- libexec/tftpd/tftpd.c
+++ libexec/tftpd/tftpd.c
@@ -42,7 +42,7 @@ static const char copyright[] =
 static char sccsid[] = "@(#)tftpd.c	8.1 (Berkeley) 6/4/93";
 #endif
 static const char rcsid[] =
-	"$Id$";
+	"$Id: tftpd.c,v 1.10 1997/12/03 07:19:58 charnier Exp $";
 #endif /* not lint */
 
 /*
@@ -122,7 +122,7 @@ main(argc, argv)
 	char *chroot_dir = NULL;
 	struct passwd *nobody;
 
-	openlog("tftpd", LOG_PID, LOG_FTP);
+	openlog("tftpd", LOG_PID | LOG_NDELAY, LOG_FTP);
 	while ((ch = getopt(argc, argv, "lns:")) != -1) {
 		switch (ch) {
 		case 'l':
