72618c7990b921fdec9b65204302a5e9346e62ac
dfr <dfr@FreeBSD.org>
1998-04-13 12:11:24 +0000

    Fixes for using amd with non-NFSv3 servers and for choosing the right address for a multihomed server.
    PR:		bin/6231, bin/4678
    Submitted by:	Mika Nystrom <mika@cs.caltech.edu

--- usr.sbin/amd/amd/wire.c
+++ usr.sbin/amd/amd/wire.c
@@ -37,7 +37,7 @@
  *
  *	@(#)wire.c	8.1 (Berkeley) 6/6/93
  *
- * $Id$
+ * $Id: wire.c,v 1.4 1997/02/22 16:01:46 peter Exp $
  *
  */
 
@@ -84,6 +84,8 @@ static addrlist *localnets = 0;
 #define GFBUFLEN 1024
 #define clist (ifc.ifc_ifcu.ifcu_req)
 #define count (ifc.ifc_len/sizeof(struct ifreq))
+
+extern unsigned long mysubnet;
 
 char *getwire P((void));
 char *getwire()
@@ -136,6 +138,8 @@ char *getwire()
 	/*
 	 * Scan the list looking for a suitable interface
 	 */
+	mysubnet = 0;
+
 	for (cp = buf; cp < cplim; cp += size(ifr)) {
 		addrlist *al;
 		ifr = (struct ifreq *) cp;
@@ -227,10 +231,13 @@ char *getwire()
 			/* This is probably very wrong. */
 			np = getnetbyaddr(subnet, AF_INET);
 #endif /* IN_CLASSA */
-			if (np)
+			if (np) {
 				s = np->n_name;
+				mysubnet = np->n_net;
+			}
 			else {
 				subnet = address & netmask;
+				mysubnet = subnet;
 				hp = gethostbyaddr((char *) &subnet, 4, AF_INET);
 				if (hp)
 					s = hp->h_name;
