cbcfa1ba6ab892b56a3341e0fc1478114053ed0e
David E. O'Brien <obrien@FreeBSD.org>
1998-04-24 11:50:30 +0000

    Discussed with:	bde

--- sys/kern/subr_diskslice.c
+++ sys/kern/subr_diskslice.c
@@ -46,7 +46,7 @@
  *	from: wd.c,v 1.55 1994/10/22 01:57:12 phk Exp $
  *	from: @(#)ufs_disksubr.c	7.16 (Berkeley) 5/4/91
  *	from: ufs_disksubr.c,v 1.8 1994/06/07 01:21:39 phk Exp $
- *	$Id: subr_diskslice.c,v 1.43 1998/04/19 23:31:56 julian Exp $
+ *	$Id: subr_diskslice.c,v 1.44 1998/04/24 11:49:57 obrien Exp $
  */
 
 #include "opt_devfs.h"
