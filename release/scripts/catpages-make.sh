a7e595e7b611d9e575fba075931e8e31f868ca40
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-04-26 08:34:26 +0000

    Add the whatis database to catpages too
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@35461 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/scripts/catpages-make.sh
+++ release/scripts/catpages-make.sh
@@ -5,6 +5,6 @@
 if [ -d ${RD}/trees/manpages/usr/share/man ]; then
 	su -m man -c 'catman ${RD}/trees/manpages/usr/share/man' > /dev/null 2>&1;
 	( cd ${RD}/trees/manpages/usr/share/man;
-	find cat* | cpio -dumpl ${RD}/trees/catpages/usr/share/man ) &&
+	find cat* whatis | cpio -dumpl ${RD}/trees/catpages/usr/share/man ) &&
 	rm -rf ${RD}/trees/manpages/usr/share/man/cat*;
 fi
