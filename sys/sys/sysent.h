288078be0fb8fe3e781a3e76038d6af644bdfc2e
Eivind Eklund <eivind@FreeBSD.org>
1998-04-28 18:15:08 +0000

    Translate T_PROTFLT to SIGSEGV instead of SIGBUS when running under Linux emulation.  This make Allegro Common Lisp 4.3 work under FreeBSD!
    Submitted by: Fred Gilham <gilham@csl.sri.com>
    Commented on by: bde, dg, msmith, tg
    Hoping he got everything right:  eivind

--- sys/sys/sysent.h
+++ sys/sys/sysent.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: sysent.h,v 1.15 1997/11/06 19:29:47 phk Exp $
+ *	$Id: sysent.h,v 1.16 1998/02/03 21:51:57 bde Exp $
  */
 
 #ifndef _SYS_SYSENT_H_
@@ -58,6 +58,8 @@ struct sysentvec {
 	int		*sv_sigtbl;	/* signal translation table */
 	int		sv_errsize;	/* size of errno translation table */
 	int 		*sv_errtbl;	/* errno translation table */
+	int		(*sv_transtrap) __P((int, int));
+					/* translate trap-to-signal mapping */
 	int		(*sv_fixup) __P((int **, struct image_params *));
 					/* stack fixup function */
 	void		(*sv_sendsig) __P((void (*)(int), int, int, u_long));
