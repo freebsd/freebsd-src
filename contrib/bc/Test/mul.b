0ec6169bea8adb8ddbf8f9ce363f6e1803f88621
andreas <andreas@FreeBSD.org>
1998-04-29 21:53:01 +0000

    Import GNU bc 1.04 PR:		4183

---
+++ contrib/bc/Test/mul.b
@@ -0,0 +1,7 @@
+scale = 20
+for (i=0; i<1000; i++) {
+  for (j=1; j<100; j++) b=i*j
+}
+b
+quit
+
---
+++ contrib/bc/acconfig.h
@@ -0,0 +1,15 @@
+/* PACKAGE name */
+#undef PACKAGE
+
+/* Package VERSION number */
+#undef VERSION
+
+/* define if the math lib is to be loaded from a file. */
+#undef BC_MATH_FILE
+
+/* Define to use the readline library. */
+#undef READLINE
+
+/* Define to `size_t' if <sys/types.h> and <stddef.h> don't define.  */
+#undef ptrdiff_t
+
