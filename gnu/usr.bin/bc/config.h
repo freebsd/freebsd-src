fc12edd4a3a57854ab862cc45431edded6741a13
andreas <andreas@FreeBSD.org>
1998-04-29 22:05:39 +0000

    remove old bc-1.0.3 stuff modify Makefile, to get bc-1.0.4 from src/contrib/bc adjust config.h PR:		closes 4183

--- gnu/usr.bin/bc/config.h
+++ gnu/usr.bin/bc/config.h
@@ -2,3 +2,4 @@
 #ifndef __STDC__
 #define VARARGS
 #endif
+#define YYTEXT_POINTER 1
--- gnu/usr.bin/bc/version.h
+++
@@ -1,3 +0,0 @@
-#define BC_VERSION  \
-"bc 1.03 (Nov 2, 1994)\nCopyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc."
-
