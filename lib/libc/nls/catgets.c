3cae518a9f6797df0c0dc104d4aefb9c33cc92dd
ache <ache@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-04-30 10:14:55 +0000

    Prototypes/typedefs cleanup Fix error return codes
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@35546 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/nls/catgets.c
+++ lib/libc/nls/catgets.c
@@ -1,4 +1,4 @@
-/*	$Id$ */
+/*	$Id: catgets.c,v 1.3 1997/02/22 15:00:47 peter Exp $ */
 
 /*
  * Written by J.T. Conklin, 10/05/94
@@ -13,14 +13,14 @@ __indr_reference(_catgets,catgets);
 
 #include <nl_types.h>
 
-extern char * _catgets __P((nl_catd, int, int, char *));
+extern char * _catgets __P((nl_catd, int, int, __const char *));
 
 char *
 catgets(catd, set_id, msg_id, s)
 	nl_catd catd;
 	int set_id;
 	int msg_id;
-	char *s;
+	__const char *s;
 {
 	return _catgets(catd, set_id, msg_id, s);
 }
