0ea1352bad3f7ef66613e717e35a38f6cf7ed8ed
bde <bde@FreeBSD.org>
1998-05-01 18:13:37 +0000

    Added -ansi to CWARNFLAGS so that ANSI errors don't come back.

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -1,11 +1,11 @@
-#	$Id: bsd.kern.mk,v 1.7 1997/10/21 10:36:07 bde Exp $
+#	$Id: bsd.kern.mk,v 1.8 1997/10/21 10:39:27 bde Exp $
 
 #
 # Warning flags for compiling the kernel and components of the kernel.
 #
 CWARNFLAGS?=	-Wreturn-type -Wcomment -Wredundant-decls -Wimplicit \
 		-Wnested-externs -Wstrict-prototypes -Wmissing-prototypes \
-		-Wpointer-arith -Winline -Wuninitialized
+		-Wpointer-arith -Winline -Wuninitialized -ansi
 #
 # The following flags are next up for working on:
 #	-W -Wunused -Wcast-qual -Wformat -Wall
