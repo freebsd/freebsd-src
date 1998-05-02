82fff9408a4ac991876b57e27de20d592790b68f
kato <kato@FreeBSD.org>
1998-05-02 02:06:08 +0000

    Support 1.44MB floppy disk.
    Submitted by:	Nobuyuki Koganemaru <kogane@koganemaru.co.jp>,
                 	NOKUBI Hirotaka <hnokubi@yyy.or.jp>

--- sys/pc98/boot/biosboot/table.c
+++ sys/pc98/boot/biosboot/table.c
@@ -24,7 +24,7 @@
  * the rights to redistribute these changes.
  *
  *	from: Mach, Revision 2.2  92/04/04  11:36:43  rpd
- *	$Id$
+ *	$Id: table.c,v 1.5 1997/02/22 09:43:11 peter Exp $
  */
 
 /*
@@ -145,5 +145,5 @@ struct pseudo_desc Idtr_real = { 0x400 - 1, 0x0, 0x0 };
  * All initialized data is defined in one file to reduce space wastage from
  * fragmentation.
  */
-char *devs[] = { "wd", "dk", "fd", "wt", "sd", 0 };
+char *devs[] = { "wd", "dk", "fd", "wt", "sd", "dk", "fd", 0 };
 unsigned long tw_chars = 0x5C2D2F7C;	/* "\-/|" */
