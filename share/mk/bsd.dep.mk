29901fcc760af5da084dd481d6bd48ee2366e9e5
Bruce Evans <bde@FreeBSD.org>
1998-05-08 06:31:05 +0000

    Moved lex and yacc source handling from bsd.prog.mk to bsd.dep.mk so that it applies to libraries (and lkms) as well as programs.

--- share/mk/bsd.dep.mk
+++ share/mk/bsd.dep.mk
@@ -1,4 +1,4 @@
-#	$Id: bsd.dep.mk,v 1.19 1998/03/23 14:58:26 eivind Exp $
+#	$Id: bsd.dep.mk,v 1.20 1998/05/06 18:43:58 bde Exp $
 #
 # The include file <bsd.dep.mk> handles Makefile dependencies.
 #
@@ -72,6 +72,45 @@ beforedepend:
 .if !target(afterdepend)
 afterdepend:
 .endif
+.endif
+
+.if defined(SRCS)
+CLEANFILES?=
+
+.for _LSRC in ${SRCS:M*.l:N*/*}
+.for _LC in ${_LSRC:S/.l/.c/}
+${_LC}: ${_LSRC}
+	${LEX} -t ${LFLAGS} ${.ALLSRC} > ${.TARGET}
+SRCS:=	${SRCS:S/${_LSRC}/${_LC}/}
+CLEANFILES:= ${CLEANFILES} ${_LC}
+.endfor
+.endfor
+
+.for _YSRC in ${SRCS:M*.y:N*/*}
+.for _YC in ${_YSRC:S/.y/.c/}
+SRCS:=	${SRCS:S/${_YSRC}/${_YC}/}
+CLEANFILES:= ${CLEANFILES} ${_YC}
+.if ${YFLAGS:M-d} != "" && ${SRCS:My.tab.h}
+.ORDER: ${_YC} y.tab.h
+${_YC} y.tab.h: ${_YSRC}
+	${YACC} ${YFLAGS} ${.ALLSRC}
+	cp y.tab.c ${_YC}
+SRCS:=	${SRCS} y.tab.h
+CLEANFILES:= ${CLEANFILES} y.tab.c y.tab.h
+.elif ${YFLAGS:M-d} != ""
+.for _YH in ${_YC:S/.c/.h/}
+.ORDER: ${_YC} ${_YH}
+${_YC} ${_YH}: ${_YSRC}
+	${YACC} ${YFLAGS} -o ${_YC} ${.ALLSRC}
+SRCS:=	${SRCS} ${_YH}
+CLEANFILES:= ${CLEANFILES} ${_YH}
+.endfor
+.else
+${_YC}: ${_YSRC}
+	${YACC} ${YFLAGS} -o ${_YC} ${.ALLSRC}
+.endif
+.endfor
+.endfor
 .endif
 
 .if defined(NOTAGS)
