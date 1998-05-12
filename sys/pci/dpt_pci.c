e9393cf3f56e73aed436e95d9a944adea367a70a
gibbs <gibbs@FreeBSD.org>
1998-05-12 19:59:31 +0000

    MFC - DPT driver from current.

--- sys/pci/dpt_pci.c
+++ sys/pci/dpt_pci.c
@@ -25,6 +25,7 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
  */
 
 /*
@@ -33,36 +34,27 @@
  *  caveats:   We may need an eisa and an isa files too
  */
 
-#ident "$Id: dpt_pci.c,v 1.4.2.1 1998/03/06 23:45:05 julian Exp $"
+#ident "$Id: dpt_pci.c,v 1.5 1998/03/11 00:30:16 julian Exp $"
 
+#include "opt_devfs.h"
 #include "opt_dpt.h"
-#include <pci.h>
 
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/malloc.h>
 #include <sys/buf.h>
-#include <sys/proc.h>
 #include <sys/kernel.h>
 
-#include <scsi/scsi_all.h>
-#include <scsi/scsi_message.h>
 #include <scsi/scsiconf.h>
 
 #include <pci/pcireg.h>
-#include <sys/queue.h>
 #include <pci/pcivar.h>
 
-#include <dev/dpt/dpt.h>
+#include <sys/dpt.h>
 #include <pci/dpt_pci.h>
 
-#include <machine/clock.h>
-
 #include <vm/vm.h>
-#include <vm/vm_param.h>
 #include <vm/pmap.h>
-
-#include <dev/dpt/dpt.h>
 
 #define PCI_BASEADR0  PCI_MAP_REG_START  /* I/O Address */
 #define PCI_BASEADR1  PCI_MAP_REG_START + 4  /* Mem I/O Address */
@@ -71,13 +63,13 @@
 
 /* Global variables */
 
-int dpt_controllers_present = 0;
-
 /* Function Prototypes */
 
-char    *dpt_pci_probe(pcici_t tag, pcidi_t type);
-void     dpt_pci_attach(pcici_t config_id, int unit);
-int      dpt_pci_shutdown(int foo, int bar);
+static char    *dpt_pci_probe(pcici_t tag, pcidi_t type);
+static void     dpt_pci_attach(pcici_t config_id, int unit);
+static int      dpt_pci_shutdown(int foo, int bar);
+
+extern struct cdevsw dpt_cdevsw;
 
 static  struct pci_device dpt_pci_driver =
 {
@@ -96,7 +88,7 @@ DATA_SET(pcidevice_set, dpt_pci_driver);
  * because we do not know for sure how the two relate.
  */
 
-char   *
+static char *
 dpt_pci_probe(pcici_t tag, pcidi_t type)
 {
     static char silly_message[64];
@@ -170,7 +162,7 @@ dpt_pci_probe(pcici_t tag, pcidi_t type)
     return (NULL);
 }
 
-void
+static void
 dpt_pci_attach(pcici_t config_id, int unit)
 {
     int          ospl;
@@ -185,17 +177,17 @@ dpt_pci_attach(pcici_t config_id, int unit)
     dpt_conf_t  *config;
     dpt_softc_t *dpt;
 
-    if ( dpt_controllers_present >= DPT_MAX_ADAPTERS ){
-	printf("dpt%d: More than %d Adapters found!  Adapter rejected\n",
-	       unit, DPT_MAX_ADAPTERS);
-	return;
+    if (dpt_controllers_present >= DPT_MAX_ADAPTERS) {
+	  printf("dpt%d: More than %d Adapters found!  Adapter rejected\n",
+			 unit, DPT_MAX_ADAPTERS);
+	  return;
     }
 
     if ((dpt = (dpt_softc_t *) malloc(sizeof(dpt_softc_t), M_DEVBUF, M_NOWAIT))
-	== NULL) {
-	printf("dpt%d: Failed to allocate %d bytes for a DPT softc\n",
-	       unit, sizeof(dpt_softc_t));
-	return;
+		== NULL) {
+	  printf("dpt%d: Failed to allocate %d bytes for a DPT softc\n",
+			 unit, sizeof(dpt_softc_t));
+	  return;
     }
 
     /*
@@ -213,8 +205,9 @@ dpt_pci_attach(pcici_t config_id, int unit)
     TAILQ_INIT(&dpt->completed_ccbs);
 
     if (TAILQ_EMPTY(&dpt_softc_list)) {
-	TAILQ_INIT(&dpt_softc_list);
+	  TAILQ_INIT(&dpt_softc_list);
     }
+
     TAILQ_INSERT_TAIL(&dpt_softc_list, dpt, links);
     dpt->queue_status       = DPT_QUEUES_NONE_ACTIVE;
     dpt->commands_processed = 0;
@@ -222,13 +215,26 @@ dpt_pci_attach(pcici_t config_id, int unit)
 #ifdef DPT_MEASURE_PERFORMANCE
     /* Zero out all command counters */
     bzero((void *)&dpt->performance, sizeof(dpt_perf_t));
+    for ( ndx = 0; ndx < 256; ndx ++ )
+	  dpt->performance.min_command_time[ndx] = BIG_ENOUGH;
+    
+    dpt->performance.min_intr_time     = BIG_ENOUGH;
+    dpt->performance.min_waiting_time  = BIG_ENOUGH;
+    dpt->performance.min_submit_time   = BIG_ENOUGH;
+    dpt->performance.min_complete_time = BIG_ENOUGH;
+    dpt->performance.min_eata_tries    = BIG_ENOUGH;
+    
+    for (ndx = 0; ndx < 10; ndx++ ) {
+	    dpt->performance.read_by_size_min_time[ndx] = BIG_ENOUGH;
+	    dpt->performance.write_by_size_min_time[ndx] = BIG_ENOUGH;
+    }
 #endif        /* DPT_MEASURE_PERFORMANCE */
 
     dpt->unit = unit;
     dpt->handle_interrupts = 0;  /* 
-                               * Do not set to 1 until all
-                               * initialization is done 
-                               */
+								  * Do not set to 1 until all
+								  * initialization is done 
+								  */
     dpt->v_membase = NULL;
     dpt->p_membase = NULL;
     io_base = 0;
@@ -239,102 +245,98 @@ dpt_pci_attach(pcici_t config_id, int unit)
 #ifdef DPT_ALLOW_MEMIO
     if ( (command & PCI_COMMAND_MEM_ENABLE) == 0 ) {
 #ifdef DPT_DEBUG_PCI
-	printf("dpt%d: Cannot be memory mapped\n", unit);
+	  printf("dpt%d: Cannot be memory mapped\n", unit);
 #endif
-      force_io:
-	if ((command & PCI_COMMAND_IO_ENABLE) == 0 ) {
+	force_io:
+	  if ((command & PCI_COMMAND_IO_ENABLE) == 0 ) {
 	    printf("dpt%d: Cannot be I/O mapped either :-(\n", unit);
 	    free(dpt, M_DEVBUF);
 	    return;
-	} else {
+	  } else {
 	    data = pci_conf_read(config_id, PCI_MAP_REG_START);
 	    if ( pci_map_port(config_id, PCI_MAP_REG_START, &io_base) == 0 ) {
 #ifdef DPT_DEBUG_ERROR
-		printf("dpt%d: Failed to map as I/O :-(\n", unit);
+		  printf("dpt%d: Failed to map as I/O :-(\n", unit);
 #endif
-		free(dpt, M_DEVBUF);
-		return;
+		  free(dpt, M_DEVBUF);
+		  return;
 	    } else {
-		dpt->io_base = io_base + 0x10;
+		  dpt->io_base = io_base + 0x10;
 #ifdef DPT_DEBUG_PCI
-		printf("dpt%d: Mapped registers to I/O space, "
-		       "starting at %x\n",
-		       dpt->unit, dpt->io_base);
+		  printf("dpt%d: Mapped registers to I/O space, "
+				 "starting at %x\n",
+				 dpt->unit, dpt->io_base);
 #endif
 	    }
-	}
+	  }
     } else {
-	if ( pci_map_mem(config_id, PCI_MAP_REG_START + 4, &vaddr,
-			 &paddr) == 0 ) {
+	  if ( pci_map_mem(config_id, PCI_MAP_REG_START + 4, &vaddr,
+					   &paddr) == 0 ) {
 #ifdef DPT_DEBUG_ERROR
 	    printf("dpt%d: Failed to map as MEMORY.\n"
-		   "  Attemting to force I/O mapping\n", unit);
+			   "  Attemting to force I/O mapping\n", unit);
 #endif
 	    goto force_io;
-	} else {
+	  } else {
 	    dpt->v_membase = (volatile u_int8_t *)(vaddr + 0x10);
 	    dpt->p_membase = (volatile u_int8_t *)(paddr + 0x10);
 #ifdef DPT_DEBUG_PCI
 	    printf("dpt%d: Mapped registers to MEMORY space, "
-		   "starting at %x/%x\n",
-		   dpt->unit, dpt->v_membase, dpt->p_membase);
+			   "starting at %x/%x\n",
+			   dpt->unit, dpt->v_membase, dpt->p_membase);
 #endif
-	}
+	  }
     }
 
 #else /* !DPT_ALLOW_MEMIO */
     data = pci_conf_read(config_id, PCI_MAP_REG_START);
     if ((command & PCI_COMMAND_IO_ENABLE) == 0 ) {
-	printf("dpt%d: Registers cannot be I/O mapped :-(\n", unit);
-	free(dpt, M_DEVBUF);
-	return;
+	  printf("dpt%d: Registers cannot be I/O mapped :-(\n", unit);
+	  free(dpt, M_DEVBUF);
+	  return;
     } else {
-	if ( pci_map_port(config_id, PCI_MAP_REG_START, &io_base) == 0 ) {
+	  if ( pci_map_port(config_id, PCI_MAP_REG_START, &io_base) == 0 ) {
 #ifdef DPT_DEBUG_ERROR
 	    printf("dpt%d: Failed to map registers as I/O :-(\n", unit);
 #endif
 	    free(dpt, M_DEVBUF);
 	    return;
-	} else {
+	  } else {
 	    dpt->io_base = io_base + 0x10;
 #ifdef DPT_DEBUG_PCI
 	    printf("dpt%d: Mapped registers to I/O space, starting at %x\n",
-		   dpt->unit, dpt->io_base);
+			   dpt->unit, dpt->io_base);
 #endif
-	}
+	  }
     }
 #endif /* !DPT_ALLOW_MEMIO */
   
-#ifdef DPT_USE_DPT_SWI
-    if ( pci_map_int(config_id, dpt_intr, (void *) dpt, &bio_imask) == 0 ) {
-#else
     if (pci_map_int(config_id, dpt_intr, (void *)dpt, &cam_imask) == 0) {
-#endif
 #ifdef DPT_DEBUG_WARN
-	printf("dpt%d: Failed to map interrupt :-(\n", unit);
+	  printf("dpt%d: Failed to map interrupt :-(\n", unit);
 #endif
-	free(dpt, M_DEVBUF);
-	return;
+	  free(dpt, M_DEVBUF);
+	  return;
     }
 
     /* If the DPT is mapped as an IDE controller, let it be IDE controller */
     if (io_base == (ISA_PRIMARY_WD_ADDRESS)) {
 #ifdef DPT_DEBUG_WARN
-	printf("dpt%d: Mapped as an IDE controller.  "
-	       "Disabling SCSI setup\n", unit);
+	  printf("dpt%d: Mapped as an IDE controller.  "
+			 "Disabling SCSI setup\n", unit);
 #endif
-	free(dpt, M_DEVBUF);
-	return;
+	  free(dpt, M_DEVBUF);
+	  return;
     } else {
-	if ((config = dpt_get_conf(dpt, 0xc1, 7,
-				   sizeof(dpt_conf_t), 1)) == NULL) {
+	  if ((config = dpt_get_conf(dpt, 0xc1, 7,
+								 sizeof(dpt_conf_t), 1)) == NULL) {
 #ifdef DPT_DEBUG_ERROR
 	    printf("dpt%d: Failed to get board configuration (%x)\n",
-		   unit, BaseRegister(dpt));
+			   unit, BaseRegister(dpt));
 #endif
 	    free(dpt, M_DEVBUF);
 	    return;
-	}
+	  }
     }
 
     dpt->max_id      = config->MAX_ID;
@@ -345,136 +347,153 @@ dpt_pci_attach(pcici_t config_id, int unit)
 
 #ifdef DPT_DEBUG_SETUP
     printf("dpt%d: max_id = %d, max_chan = %d, max_lun = %d\n",
-	   dpt->unit, dpt->max_id, dpt->channels, dpt->max_lun);
+		   dpt->unit, dpt->max_id, dpt->channels, dpt->max_lun);
 #endif
 
     if (result = dpt_setup(dpt, config)) {
-	free(config, M_TEMP);
-	free(dpt, M_DEVBUF);
-	printf("dpt%d: dpt_setup failed (%d).  Driver Disabled :-(\n",
-	       dpt->unit, result);
+	  free(config, M_TEMP);
+	  free(dpt, M_DEVBUF);
+	  printf("dpt%d: dpt_setup failed (%d).  Driver Disabled :-(\n",
+			 dpt->unit, result);
     } else {
-	/* clean up the informational data, and display */
-	char clean_vendor[9];
-	char clean_model[17];
-	char clean_firmware[5];
-	char clean_protocol[5];
-	char clean_other[7];
+	  /* clean up the informational data, and display */
+	  char clean_vendor[9];
+	  char clean_model[17];
+	  char clean_firmware[5];
+	  char clean_protocol[5];
+	  char clean_other[7];
 
-	int     ndx;
+	  int     ndx;
 
-	strncpy(clean_other, dpt->board_data.otherData, 8);
-	clean_other[6] = '\0';
-	for (ndx = 5; ndx >= 0; ndx--) {
+	  strncpy(clean_other, dpt->board_data.otherData, 8);
+	  clean_other[6] = '\0';
+	  for (ndx = 5; ndx >= 0; ndx--) {
 	    if (clean_other[ndx] == ' ')
-		clean_other[ndx] = '\0';
+		  clean_other[ndx] = '\0';
 	    else
-		break;
-	}
-	strncpy(dpt->board_data.otherData, clean_other, 6);
+		  break;
+	  }
+	  strncpy(dpt->board_data.otherData, clean_other, 6);
 
-	strncpy(clean_vendor, dpt->board_data.vendor, 8);
-	clean_vendor[8] = '\0';
-	for (ndx = 7; ndx >= 0; ndx--) {
+	  strncpy(clean_vendor, dpt->board_data.vendor, 8);
+	  clean_vendor[8] = '\0';
+	  for (ndx = 7; ndx >= 0; ndx--) {
 	    if (clean_vendor[ndx] == ' ')
-		clean_vendor[ndx] = '\0';
+		  clean_vendor[ndx] = '\0';
 	    else
-		break;
-	}
-	strncpy(dpt->board_data.vendor, clean_vendor, 8);
+		  break;
+	  }
+	  strncpy(dpt->board_data.vendor, clean_vendor, 8);
 
-	strncpy(clean_model, dpt->board_data.modelNum, 16);
-	clean_model[16] = '\0';
-	for (ndx = 15; ndx >= 0; ndx--) {
+	  strncpy(clean_model, dpt->board_data.modelNum, 16);
+	  clean_model[16] = '\0';
+	  for (ndx = 15; ndx >= 0; ndx--) {
 	    if (clean_model[ndx] == ' ')
-		clean_model[ndx] = '\0';
+		  clean_model[ndx] = '\0';
 	    else
-		break;
-	}
-	strncpy(dpt->board_data.modelNum, clean_model, 16);
+		  break;
+	  }
+	  strncpy(dpt->board_data.modelNum, clean_model, 16);
 
-	strncpy(clean_firmware, dpt->board_data.firmware, 4);
-	clean_firmware[4] = '\0';
-	for (ndx = 3; ndx >= 0; ndx--) {
+	  strncpy(clean_firmware, dpt->board_data.firmware, 4);
+	  clean_firmware[4] = '\0';
+	  for (ndx = 3; ndx >= 0; ndx--) {
 	    if (clean_firmware[ndx] == ' ')
-		clean_firmware[ndx] = '\0';
+		  clean_firmware[ndx] = '\0';
 	    else
-		break;
-	}
-	strncpy(dpt->board_data.firmware, clean_firmware, 4);
+		  break;
+	  }
+	  strncpy(dpt->board_data.firmware, clean_firmware, 4);
 
-	strncpy(clean_protocol, dpt->board_data.protocol, 4);
-	clean_protocol[4] = '\0';
-	for (ndx = 3; ndx >= 0; ndx--) {
+	  strncpy(clean_protocol, dpt->board_data.protocol, 4);
+	  clean_protocol[4] = '\0';
+	  for (ndx = 3; ndx >= 0; ndx--) {
 	    if (clean_protocol[ndx] == ' ')
-		clean_protocol[ndx] = '\0';
+		  clean_protocol[ndx] = '\0';
 	    else
-		break;
-	}
-	strncpy(dpt->board_data.protocol, clean_protocol, 4);
+		  break;
+	  }
+	  strncpy(dpt->board_data.protocol, clean_protocol, 4);
 
-	dpt_detect_cache(dpt);
+	  dpt_detect_cache(dpt);
 
-	printf("dpt%d: %s type %x, model %s firmware %s, Protocol %s \n"
-	       "      on port %x with %dMB %s cache.  LED = %s\n",
-	       dpt->unit, clean_vendor, dpt->board_data.deviceType,
-	       clean_model, clean_firmware, clean_protocol, dpt->io_base,
-	       dpt->cache_size,
-	       (dpt->cache_type == DPT_NO_CACHE)
-	       ? "Disabled"
-	       : (dpt->cache_type == DPT_CACHE_WRITETHROUGH)
-	       ? "Write-Through"
-	       : "Write-Back",
-	       i2bin(dpt_blinking_led(dpt), 8));
-	printf("dpt%d: Enabled Options:\n", dpt->unit);
+	  printf("dpt%d: %s type %x, model %s firmware %s, Protocol %s \n"
+			 "      on port %x with %s cache.  LED = %s\n",
+			 dpt->unit, clean_vendor, dpt->board_data.deviceType,
+			 clean_model, clean_firmware, clean_protocol, dpt->io_base,
+			 (dpt->cache_type == DPT_NO_CACHE)
+			 ? "Disabled"
+			 : (dpt->cache_type == DPT_CACHE_WRITETHROUGH)
+			 ? "Write-Through"
+			 : "Write-Back",
+			 i2bin(dpt_blinking_led(dpt), 8));
+	  printf("dpt%d: Enabled Options:\n", dpt->unit);
+#ifdef DPT_LOST_IRQ
+	  printf("      Recover Lost Interrupts\n");
+#endif
 #ifdef DPT_VERIFY_HINTR
-	printf("      Verify Lost Transactions\n");
+	  printf("      Verify Lost Transactions\n");
 #endif
 #ifdef DPT_RESTRICTED_FREELIST
-	printf("      Restrict the Freelist Size\n");
-#endif
-#ifdef DPT_TRACK_CCB_STATES
-	printf("      Precisely Track State Transitions\n");
+	  printf("      Restrict the Freelist Size\n");
 #endif
 #ifdef DPT_MEASURE_PERFORMANCE
-	printf("      Collect Metrics\n");
+	  printf("      Collect Metrics\n");
 #endif
 #ifdef DPT_FREELIST_IS_STACK
-	printf("      Optimize CPU Cache\n");
+	  printf("      Optimize CPU Cache\n");
 #endif
 #ifdef DPT_HANDLE_TIMEOUTS
-	printf("      Handle Timeouts\n");
+	  printf("      Handle Timeouts\n");
 #endif
 #ifdef DPT_ALLOW_MEMIO
-	printf("      Allow I/O to be Memeory Mapped\n");
+	  printf("      Allow I/O to be Memeory Mapped\n");
 #endif
 #ifdef DPT_HINTR_CHECK_SOFTC
-	printf("      Validate SoftC at Interrupt\n");
+	  printf("      Validate SoftC at Interrupt\n");
 #endif
 
-    /* register shutdown handlers */
-	result = at_shutdown((bootlist_fn)dpt_shutdown, (void *)dpt,
-			     SHUTDOWN_POST_SYNC);
-	switch ( result ) {
-	case 0:
+	  /* register shutdown handlers */
+	  result = at_shutdown((bootlist_fn)dpt_shutdown, (void *)dpt,
+						   SHUTDOWN_POST_SYNC);
+	  switch ( result ) {
+	  case 0:
 #ifdef DPT_DEBUG_SHUTDOWN
-	    printf("dpt%d: Shutdown handler registered\n", dpt->unit);
+		printf("dpt%d: Shutdown handler registered\n", dpt->unit);
 #endif
-	    break;
-	default:
+		break;
+	  default:
 #ifdef DPT_DEBUG_WARN
-	    printf("dpt%d: Failed to register shutdown handler (%d)\n",
-		   dpt->unit, result);
+		printf("dpt%d: Failed to register shutdown handler (%d)\n",
+			   dpt->unit, result);
 #endif
-	    break;
-	}
-	dpt_attach(dpt);
-    }
+		break;
+	  }
 
-    ++dpt_controllers_present;
+	  /* Attach SCSI devices */
+	  dpt_attach(dpt);
+	  ++dpt_controllers_present;
+
+	  /*
+	   * Now we create the DEVFS entry.
+	   * This would be normally done from dpt_control.c,
+	   * But since it appears to be called before we do here,
+	   * We never get the entries made.
+       */
+#ifdef DEVFS
+	  dpt->devfs_data_token = devfs_add_devswf(&dpt_cdevsw, dpt->unit, DV_CHR,
+											   UID_ROOT, GID_WHEEL, 0600,
+											   "dpt%d", dpt->unit);
+	  dpt->devfs_ctl_token = devfs_add_devswf(&dpt_cdevsw,
+											  dpt->unit | SCSI_CONTROL_MASK,
+											  DV_CHR,
+											  UID_ROOT, GID_WHEEL, 0600,
+											  "dpt%d.ctl", dpt->unit);
+#endif
+    }
 }
 
-int
+static int
 dpt_pci_shutdown(int foo, int bar)
 {
 #ifdef DPT_DEBUG_WARN
@@ -483,4 +502,21 @@ dpt_pci_shutdown(int foo, int bar)
     return (0);
 }
 
-/* End of the DPT PCI part o f the driver */
+/* End of the DPT PCI part of the driver */
+
+/*
+ * Hello emacs, these are the
+ * Local Variables:
+ *  c-indent-level:               8
+ *  c-continued-statement-offset: 8
+ *  c-continued-brace-offset:     0
+ *  c-brace-offset:              -8
+ *  c-brace-imaginary-offset:     0
+ *  c-argdecl-indent:             8
+ *  c-label-offset:              -8
+ *  c++-hanging-braces:           1
+ *  c++-access-specifier-offset: -8
+ *  c++-empty-arglist-indent:     8
+ *  c++-friend-offset:            0
+ * End:
+ */
