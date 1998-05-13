9442bea514547de2fcf547518b9a235edd586680
charnier <charnier@FreeBSD.org>
1998-05-13 07:29:15 +0000

    Restore include of sys/types.h and sys/param.h.

--- bin/csh/parse.c
+++ bin/csh/parse.c
@@ -36,10 +36,11 @@
 static char sccsid[] = "@(#)parse.c	8.1 (Berkeley) 5/31/93";
 #else
 static const char rcsid[] =
-	"$Id: parse.c,v 1.6 1997/08/07 21:42:13 steve Exp $";
+	"$Id: parse.c,v 1.7 1998/05/06 06:50:56 charnier Exp $";
 #endif
 #endif /* not lint */
 
+#include <sys/types.h>
 #include <string.h>
 #if __STDC__
 # include <stdarg.h>
