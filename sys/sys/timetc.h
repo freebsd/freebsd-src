159f68347c74109d3474697054f638f2d0388e65
phk <phk@FreeBSD.org>
1998-05-19 18:55:02 +0000

    Change a data type internal to the timecounters, and remove the "delta" function.
    Reviewed, but not entirely approved by: bde

--- sys/sys/timetc.h
+++ sys/sys/timetc.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)time.h	8.5 (Berkeley) 5/4/95
- * $Id: time.h,v 1.26 1998/04/23 14:12:06 eivind Exp $
+ * $Id: time.h,v 1.27 1998/05/17 11:53:40 phk Exp $
  */
 
 #ifndef _SYS_TIME_H_
@@ -129,14 +129,12 @@ struct timezone {
  */
 
 struct timecounter;
-typedef unsigned timecounter_get_t __P((struct timecounter *));
-typedef	u_int64_t timecounter_delta_t __P((void));
+typedef u_int timecounter_get_t __P((void));
 
 struct timecounter {
 	/* These fields must be initialized by the driver. */
-	timecounter_get_t	*get_timedelta;
-	timecounter_delta_t	*get_timecount;
-	u_int64_t		counter_mask;
+	timecounter_get_t	*get_timecount;
+	u_int			counter_mask;
 	u_int32_t		frequency;
 	char			*name;
 	/* These fields will be managed by the generic code. */
@@ -145,7 +143,7 @@ struct timecounter {
 	u_int32_t		scale_micro;
 	u_int32_t		scale_nano_i;
 	u_int32_t		scale_nano_f;
-	u_int64_t		offset_count;
+	u_int			offset_count;
 	u_int32_t		offset_sec;
 	u_int32_t		offset_micro;
 	u_int64_t		offset_nano;
