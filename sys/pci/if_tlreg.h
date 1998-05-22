ca59a583e6f2cb2065a9a38fdac682ec28394e92
wpaul <wpaul@FreeBSD.org>
1998-05-22 15:32:22 +0000

    Remove 2.2.x compatibility code and #ifdefs. Once the shakedown period in -current is over, I'll put a 2.2.x specific version in the RELENG_2_2 branch. If somebody wants a 2.2 version of this driver now, they can check out the previous version from CVS or ask me via e-mail.
    Gee people, I didn't mean to stir up such a controversy. I just wanted
    to make sure I could get this thing to work with both kernel versions
    and didn't want to have to maintain two separate copies. All ya hadda
    do was ask. :)

--- sys/pci/if_tlreg.h
+++ sys/pci/if_tlreg.h
@@ -29,7 +29,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  *
- *	$Id: if_tlreg.h,v 1.1 1998/05/21 03:19:56 wpaul Exp $
+ *	$Id: if_tlreg.h,v 1.2 1998/05/21 16:24:05 jkh Exp $
  */
 
 
@@ -122,10 +122,7 @@ struct tl_softc {
 	struct tl_list_data	*tl_ldata;	/* TX/RX lists and mbufs */
 	struct tl_chain_data	tl_cdata;
 	int			tl_txeoc;
-#if defined(__FreeBSD__) && __FreeBSD__ >= 3
 	struct callout_handle	tl_stat_ch;
-#endif
-
 };
 
 #define TX_THR		0x00000007
