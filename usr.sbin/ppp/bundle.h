1384bd27d8a0f3ca4fa7b4cbd15103c61c9fccb7
Brian Somers <brian@FreeBSD.org>
1998-05-28 23:17:51 +0000

    o #define the name "tun" in defs.h against the future possibility   of supporting architectures with different device names. o Close /dev/tunX when destroying the bundle. o Don't forget to close the parent end of the pipe in the child   process when exec'ing a program from a chat script. o If we close our controlling terminal, ditch the current session   with it, allowing getty(8) (or whatever) to regain control. o After transferring our controlling terminal descriptor to another   ppp instance, we now fork a new ppp to continue where we left off,   transferring ownership of all uucp locks and the /var/run/tunX.pid   file.  Meanwhile the parent closes all file descriptors, defaults   all signals and does a pause() to wait for a HUP after the   transferred descriptor is finally closed.   We don't run /bin/cat any more (again!).
      Suggested by: bde
    
    TODO: It seems clocal devices need their pause()d session leader
          to be given a manual HUP, as closing the last open descriptor
          doesn't do the job.

--- usr.sbin/ppp/bundle.h
+++ usr.sbin/ppp/bundle.h
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: bundle.h,v 1.2 1998/05/21 21:44:14 brian Exp $
+ *	$Id: bundle.h,v 1.3 1998/05/23 22:24:30 brian Exp $
  */
 
 #define	PHASE_DEAD		0	/* Link is dead */
@@ -164,3 +164,4 @@ extern void bundle_ReceiveDatalink(struct bundle *, int, struct sockaddr_un *);
 extern int bundle_SetMode(struct bundle *, struct datalink *, int);
 extern int bundle_RenameDatalink(struct bundle *, struct datalink *,
                                  const char *);
+extern void bundle_setsid(struct bundle *, int);
