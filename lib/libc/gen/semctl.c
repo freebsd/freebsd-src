3f58cad6388b21fbbcdc7285fc907d620c32cf1a
Steve Price <steve@FreeBSD.org>
1998-05-31 04:09:09 +0000

    Several sources including Unix98 say that semctl's fourth parameter is optional except where: 	cmd == {IPC_SET || IPC_STAT || GETALL || SETVAL || SETALL}
    PR:		2448
    Reviewed by:	bde
    Submitted by:	Tim Singletary <tsingle@sunland.gsfc.nasa.gov>
    Minor tweaks by: steve

--- lib/libc/gen/semctl.c
+++ lib/libc/gen/semctl.c
@@ -1,19 +1,38 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
 #if __STDC__
-int semctl(int semid, int semnum, int cmd, union semun semun)
+int semctl(int semid, int semnum, int cmd, ...)
 #else
-int semctl(semid, int semnum, cmd, semun)
+int semctl(semid, semnum, cmd, va_alist)
 	int semid, semnum;
 	int cmd;
-	union semun semun;
+	va_dcl
 #endif
 {
-#ifdef	__NETBSD_SYSCALLS
-	return (__semctl(semid, semnum, cmd, &semun));
+	va_list ap;
+	union semun semun;
+	union semun *semun_ptr;
+#ifdef __STDC__
+	va_start(ap, cmd);
 #else
-	return (semsys(0, semid, semnum, cmd, &semun));
+	va_start(ap);
+#endif
+	if (cmd == IPC_SET || cmd == IPC_STAT || cmd == GETALL
+	    || cmd == SETVAL || cmd == SETALL) {
+		semun = va_arg(ap, union semun);
+		semun_ptr = &semun;
+	} else {
+		semun_ptr = NULL;
+	}
+	va_end(ap);
+
+#ifdef	__NETBSD_SYSCALLS
+	return (__semctl(semid, semnum, cmd, semun_ptr));
+#else
+	return (semsys(0, semid, semnum, cmd, semun_ptr));
 #endif
 }
