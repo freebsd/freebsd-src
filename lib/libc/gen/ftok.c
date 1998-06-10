363e8996d77c1c696d9c6a636fafcb72c6247d08
John Polstra <jdp@FreeBSD.org>
1998-06-10 04:24:21 +0000

    This is a null commit to log the fact that I have done a repository copy to bring these files into libc from libcompat.  I will enable them and kill off the libcompat versions on the main branch soon.
    PR:		step one toward closing misc/6763

--- lib/libc/gen/ftok.c
+++ lib/libc/gen/ftok.c
@@ -26,7 +26,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$Id$";
+static char *rcsid = "$Id: ftok.c,v 1.3 1997/02/22 15:06:10 peter Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
