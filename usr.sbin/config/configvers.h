916e39c6d2d5ad5a7e12c31c8803f47dbf1dbe7b
bde <bde@FreeBSD.org>
1998-06-17 15:16:53 +0000

    Don't generate declarations for isa device structs in "ioconf.h".
    Don't generate declarations for isa interrupt handlers at all.
    Isa interrupt handlers are now declared in <i386/isa/isa_device.h>
    but should be converted take a `void *' arg and staticized as
    soon as possible.
    
    Updated CONFIGVERS.  New configs are very incompatible with
    previous versions.

--- usr.sbin/config/configvers.h
+++ usr.sbin/config/configvers.h
@@ -6,6 +6,6 @@
  * The numbering scheme is inspired by the sys/conf/newvers.sh RELDATE
  * and <osreldate.h> system.
  *
- * $Id: configvers.h,v 1.1 1997/10/22 00:38:48 peter Exp $
+ * $Id: configvers.h,v 1.2 1997/11/06 03:11:40 msmith Exp $
  */
-#define CONFIGVERS 300003
+#define	CONFIGVERS	300004
