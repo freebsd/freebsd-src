2d7fa8d8770f84d0dc4f7913928f407644323b5b
Bruce Evans <bde@FreeBSD.org>
1998-06-21 16:51:06 +0000

    Removed unused includes. Ifdefed conditionally used includes. Finished rev.1.39.  (I only submitted the request.)

--- sys/i386/isa/if_fe.c
+++ sys/i386/isa/if_fe.c
@@ -21,7 +21,7 @@
  */
 
 /*
- * $Id: if_fe.c,v 1.40 1998/03/28 13:24:09 bde Exp $
+ * $Id: if_fe.c,v 1.41 1998/06/07 17:10:31 dfr Exp $
  *
  * Device driver for Fujitsu MB86960A/MB86965A based Ethernet cards.
  * To be used with FreeBSD 2.x
@@ -76,11 +76,7 @@
 #include "opt_ipx.h"
 
 #include <sys/param.h>
-#include <sys/kernel.h>
 #include <sys/systm.h>
-
-#include <sys/conf.h>
-
 #include <sys/sockio.h>
 #include <sys/mbuf.h>
 #include <sys/socket.h>
@@ -124,6 +120,7 @@
 /* PCCARD suport */
 #include "card.h"
 #if NCARD > 0
+#include <sys/kernel.h>
 #include <sys/select.h>
 #include <pccard/cardinfo.h>
 #include <pccard/slot.h>
@@ -324,9 +321,9 @@ outblk ( struct fe_softc * sc, int offs, u_char const * mem, int len )
 /*
  *      PC-Card (PCMCIA) specific code.
  */
-static int feinit	(struct pccard_devinfo *);
-static void feunload	(struct pccard_devinfo *);
-static int fe_card_intr	(struct pccard_devinfo *);
+static int	feinit		( struct pccard_devinfo * );
+static void	feunload	( struct pccard_devinfo * );
+static int	fe_card_intr	( struct pccard_devinfo * );
 
 static struct pccard_device fe_info = {
 	"fe",
@@ -334,8 +331,7 @@ static struct pccard_device fe_info = {
 	feunload,
 	fe_card_intr,
 	0,			/* Attributes - presently unused */
-	&net_imask		/* Interrupt mask for device */
-				/* XXX - Should this also include net_imask? */
+	&net_imask		/* XXX - Should this also include tty_imask? */
 };
 
 DATA_SET(pccarddrv_set, fe_info);
