7d0cc08e7f7f17d088d78f4b1bf68b00eb5502d2
Guido van Rooij <guido@FreeBSD.org>
1998-06-25 18:50:06 +0000

    Fix btree problems.  This passes regressions tests. PR:	7009 Obtained from: http://www.sleepycat.com/update/patch.185.html, patches 1.2, 		1.3 and 1.4

--- lib/libc/db/btree/bt_split.c
+++ lib/libc/db/btree/bt_split.c
@@ -673,7 +673,8 @@ bt_psplit(t, h, l, r, pskip, ilen)
 		 * where we decide to try and copy too much onto the left page.
 		 * Make sure that doesn't happen.
 		 */
-		if (skip <= off && used + nbytes >= full) {
+		if (skip <= off &&
+		    used + nbytes + sizeof(indx_t) >= full || nxt == top - 1) {
 			--off;
 			break;
 		}
@@ -686,7 +687,7 @@ bt_psplit(t, h, l, r, pskip, ilen)
 			memmove((char *)l + l->upper, src, nbytes);
 		}
 
-		used += nbytes;
+		used += nbytes + sizeof(indx_t);
 		if (used >= half) {
 			if (!isbigkey || bigkeycnt == 3)
 				break;
