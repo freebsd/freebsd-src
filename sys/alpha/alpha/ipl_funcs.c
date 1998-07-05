638cb3b4c05afdee1c2001c18e7454cedabc7ac4
dfr <dfr@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-07-05 12:08:59 +0000

    Define bio_imask to allow the ncr driver to compile.  The idea of passing mask address around when registering interrupts is wrong IMHO.  We should use a priority level like IPL_BIO and the lower levels can then translate it into a mask if they want.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@37398 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/alpha/alpha/ipl_funcs.c
+++ sys/alpha/alpha/ipl_funcs.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: ipl_funcs.c,v 1.1 1998/06/10 10:52:49 dfr Exp $
+ *	$Id: ipl_funcs.c,v 1.2 1998/06/11 11:51:26 dfr Exp $
  */
 
 #include <sys/types.h>
@@ -31,6 +31,8 @@
 #include <machine/ipl.h>
 #include <machine/cpu.h>
 #include <net/netisr.h>
+
+unsigned int bio_imask;		/* XXX */
 
 unsigned int netisr;
 void	(*netisrs[32]) __P((void));
