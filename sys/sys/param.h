36d767067379474b26dd211a71b627c7fea53e20
eivind <eivind@FreeBSD.org>
1998-07-06 16:07:43 +0000

    Shorten line.
    Prodded (long time ago) by:	bde

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -36,7 +36,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)param.h	8.3 (Berkeley) 4/4/95
- * $Id: param.h,v 1.32 1998/06/02 20:58:22 jdp Exp $
+ * $Id: param.h,v 1.33 1998/06/07 17:13:03 dfr Exp $
  */
 
 #ifndef _SYS_PARAM_H_
@@ -46,7 +46,7 @@
 #define BSD4_3	1
 #define BSD4_4	1
 #undef __FreeBSD_version
-#define __FreeBSD_version 300003	/* Master version, propagated to newvers */
+#define __FreeBSD_version 300003	/* Master, propagated to newvers */
 
 #ifndef NULL
 #define	NULL	0
