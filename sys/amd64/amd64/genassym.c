79b54f96f64c57aa61957296f9aeac57874c5807
bde <bde@FreeBSD.org>
1998-07-11 12:17:07 +0000

    Fixed printf format errors.
    Use offsetof() instead of null pointer hacks.

--- sys/amd64/amd64/genassym.c
+++ sys/amd64/amd64/genassym.c
@@ -34,10 +34,12 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)genassym.c	5.11 (Berkeley) 5/10/91
- *	$Id: genassym.c,v 1.57 1998/05/17 23:08:03 tegge Exp $
+ *	$Id: genassym.c,v 1.58 1998/05/28 09:29:55 phk Exp $
  */
 
 #include "opt_vm86.h"
+
+#include <stddef.h>
 
 #include <sys/param.h>
 #include <sys/buf.h>
@@ -72,50 +74,37 @@
 #endif
 #include <machine/globaldata.h>
 
+#define	OS(s, m)	((u_int)offsetof(struct s, m))
+
 int	main __P((void));
 int	printf __P((const char *, ...));
 
 int
 main()
 {
-	struct proc *p = (struct proc *)0;
-	struct vmmeter *vm = (struct vmmeter *)0;
-	struct user *up = (struct user *)0;
-	struct rusage *rup = (struct rusage *)0;
-	struct uprof *uprof = (struct uprof *)0;
-	struct vmspace *vms = (struct vmspace *)0;
-	struct pcb *pcb = (struct pcb *)0;
-	struct i386tss *tss = (struct i386tss *)0;
-	struct trapframe *tf = (struct trapframe *)0;
-	struct sigframe *sigf = (struct sigframe *)0;
-	struct bootinfo *bootinfo = (struct bootinfo *)0;
-	struct globaldata *globaldata = (struct globaldata *)0;
-#ifdef SMP
-	struct privatespace *privatespace = (struct privatespace *)0;
-#endif
 
-	printf("#define\tP_FORW %p\n", &p->p_procq.tqe_next);
-	printf("#define\tP_BACK %p\n", &p->p_procq.tqe_prev);
-	printf("#define\tP_VMSPACE %p\n", &p->p_vmspace);
-	printf("#define\tVM_PMAP %p\n", &vms->vm_pmap);
-	printf("#define\tP_ADDR %p\n", &p->p_addr);
-	printf("#define\tP_PRI %p\n", &p->p_priority);
-	printf("#define\tP_RTPRIO_TYPE %p\n", &p->p_rtprio.type);
-	printf("#define\tP_RTPRIO_PRIO %p\n", &p->p_rtprio.prio);
-	printf("#define\tP_STAT %p\n", &p->p_stat);
-	printf("#define\tP_WCHAN %p\n", &p->p_wchan);
-	printf("#define\tP_FLAG %p\n", &p->p_flag);
-	printf("#define\tP_PID %p\n", &p->p_pid);
-	printf("#define\tP_SWITCHTIME %p\n", &p->p_switchtime);
+	printf("#define\tP_FORW %#x\n", OS(proc, p_procq.tqe_next));
+	printf("#define\tP_BACK %#x\n", OS(proc, p_procq.tqe_prev));
+	printf("#define\tP_VMSPACE %#x\n", OS(proc, p_vmspace));
+	printf("#define\tVM_PMAP %#x\n", OS(vmspace, vm_pmap));
+	printf("#define\tP_ADDR %#x\n", OS(proc, p_addr));
+	printf("#define\tP_PRI %#x\n", OS(proc, p_priority));
+	printf("#define\tP_RTPRIO_TYPE %#x\n", OS(proc, p_rtprio.type));
+	printf("#define\tP_RTPRIO_PRIO %#x\n", OS(proc, p_rtprio.prio));
+	printf("#define\tP_STAT %#x\n", OS(proc, p_stat));
+	printf("#define\tP_WCHAN %#x\n", OS(proc, p_wchan));
+	printf("#define\tP_FLAG %#x\n", OS(proc, p_flag));
+	printf("#define\tP_PID %#x\n", OS(proc, p_pid));
+	printf("#define\tP_SWITCHTIME %#x\n", OS(proc, p_switchtime));
 #ifdef SMP
-	printf("#define\tP_ONCPU %p\n", &p->p_oncpu);
-	printf("#define\tP_LASTCPU %p\n", &p->p_lastcpu);
+	printf("#define\tP_ONCPU %#x\n", OS(proc, p_oncpu));
+	printf("#define\tP_LASTCPU %#x\n", OS(proc, p_lastcpu));
 #endif
 	printf("#define\tSSLEEP %d\n", SSLEEP);
 	printf("#define\tSRUN %d\n", SRUN);
-	printf("#define\tV_TRAP %p\n", &vm->v_trap);
-	printf("#define\tV_SYSCALL %p\n", &vm->v_syscall);
-	printf("#define\tV_INTR %p\n", &vm->v_intr);
+	printf("#define\tV_TRAP %#x\n", OS(vmmeter, v_trap));
+	printf("#define\tV_SYSCALL %#x\n", OS(vmmeter, v_syscall));
+	printf("#define\tV_INTR %#x\n", OS(vmmeter, v_intr));
 	printf("#define\tUPAGES %d\n", UPAGES);
 	printf("#define\tPAGE_SIZE %d\n", PAGE_SIZE);
 	printf("#define\tNPTEPG %d\n", NPTEPG);
@@ -127,114 +116,115 @@ main()
 	printf("#define\tPAGE_SHIFT %d\n", PAGE_SHIFT);
 	printf("#define\tPAGE_MASK %d\n", PAGE_MASK);
 	printf("#define\tPDRSHIFT %d\n", PDRSHIFT);
-	printf("#define\tUSRSTACK 0x%lx\n", USRSTACK);
-	printf("#define\tVM_MAXUSER_ADDRESS 0x%lx\n", VM_MAXUSER_ADDRESS);
-	printf("#define\tKERNBASE 0x%x\n", KERNBASE);
+	printf("#define\tUSRSTACK %#x\n", USRSTACK);
+	printf("#define\tVM_MAXUSER_ADDRESS %#x\n", VM_MAXUSER_ADDRESS);
+	printf("#define\tKERNBASE %#x\n", KERNBASE);
 	printf("#define\tMCLBYTES %d\n", MCLBYTES);
-	printf("#define\tPCB_CR3 %p\n", &pcb->pcb_cr3);
-	printf("#define\tPCB_EDI %p\n", &pcb->pcb_edi);
-	printf("#define\tPCB_ESI %p\n", &pcb->pcb_esi);
-	printf("#define\tPCB_EBP %p\n", &pcb->pcb_ebp);
-	printf("#define\tPCB_ESP %p\n", &pcb->pcb_esp);
-	printf("#define\tPCB_EBX %p\n", &pcb->pcb_ebx);
-	printf("#define\tPCB_EIP %p\n", &pcb->pcb_eip);
-	printf("#define\tTSS_ESP0 %p\n", &tss->tss_esp0);
-	printf("#define\tPCB_USERLDT %p\n", &pcb->pcb_ldt);
-	printf("#define\tPCB_FS %p\n", &pcb->pcb_fs);
-	printf("#define\tPCB_GS %p\n", &pcb->pcb_gs);
+	printf("#define\tPCB_CR3 %#x\n", OS(pcb, pcb_cr3));
+	printf("#define\tPCB_EDI %#x\n", OS(pcb, pcb_edi));
+	printf("#define\tPCB_ESI %#x\n", OS(pcb, pcb_esi));
+	printf("#define\tPCB_EBP %#x\n", OS(pcb, pcb_ebp));
+	printf("#define\tPCB_ESP %#x\n", OS(pcb, pcb_esp));
+	printf("#define\tPCB_EBX %#x\n", OS(pcb, pcb_ebx));
+	printf("#define\tPCB_EIP %#x\n", OS(pcb, pcb_eip));
+	printf("#define\tTSS_ESP0 %#x\n", OS(i386tss, tss_esp0));
+	printf("#define\tPCB_USERLDT %#x\n", OS(pcb, pcb_ldt));
+	printf("#define\tPCB_FS %#x\n", OS(pcb, pcb_fs));
+	printf("#define\tPCB_GS %#x\n", OS(pcb, pcb_gs));
 #ifdef VM86
-	printf("#define\tPCB_EXT %p\n", &pcb->pcb_ext);
+	printf("#define\tPCB_EXT %#x\n", OS(pcb, pcb_ext));
 #endif
 #ifdef SMP
-	printf("#define\tPCB_MPNEST %p\n", &pcb->pcb_mpnest);
+	printf("#define\tPCB_MPNEST %#x\n", OS(pcb, pcb_mpnest));
 #endif
-	printf("#define\tU_PROF %p\n", &up->u_stats.p_prof);
-	printf("#define\tU_PROFSCALE %p\n", &up->u_stats.p_prof.pr_scale);
-	printf("#define\tPR_BASE %p\n", &uprof->pr_base);
-	printf("#define\tPR_SIZE %p\n", &uprof->pr_size);
-	printf("#define\tPR_OFF %p\n", &uprof->pr_off);
-	printf("#define\tPR_SCALE %p\n", &uprof->pr_scale);
-	printf("#define\tRU_MINFLT %p\n", &rup->ru_minflt);
-	printf("#define\tPCB_FLAGS %p\n", &pcb->pcb_flags);
-	printf("#define\tPCB_SAVEFPU %p\n", &pcb->pcb_savefpu);
-	printf("#define\tPCB_SAVEFPU_SIZE %d\n", sizeof pcb->pcb_savefpu);
-	printf("#define\tPCB_ONFAULT %p\n", &pcb->pcb_onfault);
+	printf("#define\tU_PROF %#x\n", OS(user, u_stats.p_prof));
+	printf("#define\tU_PROFSCALE %#x\n",
+	    OS(user, u_stats.p_prof.pr_scale));
+	printf("#define\tPR_BASE %#x\n", OS(uprof, pr_base));
+	printf("#define\tPR_SIZE %#x\n", OS(uprof, pr_size));
+	printf("#define\tPR_OFF %#x\n", OS(uprof, pr_off));
+	printf("#define\tPR_SCALE %#x\n", OS(uprof, pr_scale));
+	printf("#define\tRU_MINFLT %#x\n", OS(rusage, ru_minflt));
+	printf("#define\tPCB_FLAGS %#x\n", OS(pcb, pcb_flags));
+	printf("#define\tPCB_SAVEFPU %#x\n", OS(pcb, pcb_savefpu));
+	printf("#define\tPCB_SAVEFPU_SIZE %u\n", sizeof(struct save87));
+	printf("#define\tPCB_ONFAULT %#x\n", OS(pcb, pcb_onfault));
 #ifdef SMP
-	printf("#define\tPCB_SIZE %d\n", sizeof(struct pcb));
+	printf("#define\tPCB_SIZE %u\n", sizeof(struct pcb));
 #endif
 
-	printf("#define\tTF_ES %p\n", &tf->tf_es);
-	printf("#define\tTF_DS %p\n", &tf->tf_ds);
-	printf("#define\tTF_EDI %p\n", &tf->tf_edi);
-	printf("#define\tTF_ESI %p\n", &tf->tf_esi);
-	printf("#define\tTF_EBP %p\n", &tf->tf_ebp);
-	printf("#define\tTF_ISP %p\n", &tf->tf_isp);
-	printf("#define\tTF_EBX %p\n", &tf->tf_ebx);
-	printf("#define\tTF_EDX %p\n", &tf->tf_edx);
-	printf("#define\tTF_ECX %p\n", &tf->tf_ecx);
-	printf("#define\tTF_EAX %p\n", &tf->tf_eax);
-	printf("#define\tTF_TRAPNO %p\n", &tf->tf_trapno);
-	printf("#define\tTF_ERR %p\n", &tf->tf_err);
-	printf("#define\tTF_EIP %p\n", &tf->tf_eip);
-	printf("#define\tTF_CS %p\n", &tf->tf_cs);
-	printf("#define\tTF_EFLAGS %p\n", &tf->tf_eflags);
-	printf("#define\tTF_ESP %p\n", &tf->tf_esp);
-	printf("#define\tTF_SS %p\n", &tf->tf_ss);
+	printf("#define\tTF_ES %#x\n", OS(trapframe, tf_es));
+	printf("#define\tTF_DS %#x\n", OS(trapframe, tf_ds));
+	printf("#define\tTF_EDI %#x\n", OS(trapframe, tf_edi));
+	printf("#define\tTF_ESI %#x\n", OS(trapframe, tf_esi));
+	printf("#define\tTF_EBP %#x\n", OS(trapframe, tf_ebp));
+	printf("#define\tTF_ISP %#x\n", OS(trapframe, tf_isp));
+	printf("#define\tTF_EBX %#x\n", OS(trapframe, tf_ebx));
+	printf("#define\tTF_EDX %#x\n", OS(trapframe, tf_edx));
+	printf("#define\tTF_ECX %#x\n", OS(trapframe, tf_ecx));
+	printf("#define\tTF_EAX %#x\n", OS(trapframe, tf_eax));
+	printf("#define\tTF_TRAPNO %#x\n", OS(trapframe, tf_trapno));
+	printf("#define\tTF_ERR %#x\n", OS(trapframe, tf_err));
+	printf("#define\tTF_EIP %#x\n", OS(trapframe, tf_eip));
+	printf("#define\tTF_CS %#x\n", OS(trapframe, tf_cs));
+	printf("#define\tTF_EFLAGS %#x\n", OS(trapframe, tf_eflags));
+	printf("#define\tTF_ESP %#x\n", OS(trapframe, tf_esp));
+	printf("#define\tTF_SS %#x\n", OS(trapframe, tf_ss));
 
-	printf("#define\tSIGF_SIGNUM %p\n", &sigf->sf_signum);
-	printf("#define\tSIGF_CODE %p\n", &sigf->sf_code);
-	printf("#define\tSIGF_SCP %p\n", &sigf->sf_scp);
-	printf("#define\tSIGF_HANDLER %p\n", &sigf->sf_handler);
-	printf("#define\tSIGF_SC %p\n", &sigf->sf_sc);
+	printf("#define\tSIGF_SIGNUM %#x\n", OS(sigframe, sf_signum));
+	printf("#define\tSIGF_CODE %#x\n", OS(sigframe, sf_code));
+	printf("#define\tSIGF_SCP %#x\n", OS(sigframe, sf_scp));
+	printf("#define\tSIGF_HANDLER %#x\n", OS(sigframe, sf_handler));
+	printf("#define\tSIGF_SC %#x\n", OS(sigframe, sf_sc));
 
-	printf("#define\tB_READ %d\n", B_READ);
+	printf("#define\tB_READ %#x\n", B_READ);
 	printf("#define\tENOENT %d\n", ENOENT);
 	printf("#define\tEFAULT %d\n", EFAULT);
 	printf("#define\tENAMETOOLONG %d\n", ENAMETOOLONG);
 	printf("#define\tMAXPATHLEN %d\n", MAXPATHLEN);
 
-	printf("#define\tBOOTINFO_SIZE %d\n", sizeof *bootinfo);
-	printf("#define\tBI_VERSION %p\n", &bootinfo->bi_version);
-	printf("#define\tBI_KERNELNAME %p\n", &bootinfo->bi_kernelname);
-	printf("#define\tBI_NFS_DISKLESS %p\n", &bootinfo->bi_nfs_diskless);
-	printf("#define\tBI_ENDCOMMON %p\n", &bootinfo->bi_endcommon);
-	printf("#define\tNFSDISKLESS_SIZE %d\n", sizeof(struct nfs_diskless));
-	printf("#define\tBI_SIZE %p\n", &bootinfo->bi_size);
-	printf("#define\tBI_SYMTAB %p\n", &bootinfo->bi_symtab);
-	printf("#define\tBI_ESYMTAB %p\n", &bootinfo->bi_esymtab);
+	printf("#define\tBOOTINFO_SIZE %u\n", sizeof(struct bootinfo));
+	printf("#define\tBI_VERSION %#x\n", OS(bootinfo, bi_version));
+	printf("#define\tBI_KERNELNAME %#x\n", OS(bootinfo, bi_kernelname));
+	printf("#define\tBI_NFS_DISKLESS %#x\n", OS(bootinfo, bi_nfs_diskless));
+	printf("#define\tBI_ENDCOMMON %#x\n", OS(bootinfo, bi_endcommon));
+	printf("#define\tNFSDISKLESS_SIZE %u\n", sizeof(struct nfs_diskless));
+	printf("#define\tBI_SIZE %#x\n", OS(bootinfo, bi_size));
+	printf("#define\tBI_SYMTAB %#x\n", OS(bootinfo, bi_symtab));
+	printf("#define\tBI_ESYMTAB %#x\n", OS(bootinfo, bi_esymtab));
 
-	printf("#define\tGD_SIZEOF %d\n", sizeof(struct globaldata));
-	printf("#define\tGD_CURPROC %d\n", &globaldata->curproc);
-	printf("#define\tGD_NPXPROC %d\n", &globaldata->npxproc);
-	printf("#define\tGD_CURPCB %d\n", &globaldata->curpcb);
-	printf("#define\tGD_COMMON_TSS %d\n", &globaldata->common_tss);
-	printf("#define\tGD_SWITCHTIME %d\n", &globaldata->switchtime);
+	printf("#define\tGD_SIZEOF %u\n", sizeof(struct globaldata));
+	printf("#define\tGD_CURPROC %#x\n", OS(globaldata, curproc));
+	printf("#define\tGD_NPXPROC %#x\n", OS(globaldata, npxproc));
+	printf("#define\tGD_CURPCB %#x\n", OS(globaldata, curpcb));
+	printf("#define\tGD_COMMON_TSS %#x\n", OS(globaldata, common_tss));
+	printf("#define\tGD_SWITCHTIME %#x\n", OS(globaldata, switchtime));
 #ifdef VM86
-	printf("#define\tGD_COMMON_TSSD %d\n", &globaldata->common_tssd);
-	printf("#define\tGD_PRIVATE_TSS %d\n", &globaldata->private_tss);
-	printf("#define\tGD_MY_TR %d\n", &globaldata->my_tr);
+	printf("#define\tGD_COMMON_TSSD %#x\n", OS(globaldata, common_tssd));
+	printf("#define\tGD_PRIVATE_TSS %#x\n", OS(globaldata, private_tss));
+	printf("#define\tGD_MY_TR %#x\n", OS(globaldata, my_tr));
 #endif
 #ifdef SMP
-	printf("#define\tGD_CPUID %d\n", &globaldata->cpuid);
-	printf("#define\tGD_CPU_LOCKID %d\n", &globaldata->cpu_lockid);
-	printf("#define\tGD_OTHER_CPUS %d\n", &globaldata->other_cpus);
-	printf("#define\tGD_MY_IDLEPTD %d\n", &globaldata->my_idlePTD);
-	printf("#define\tGD_SS_EFLAGS %d\n", &globaldata->ss_eflags);
-	printf("#define\tGD_PRV_CMAP1 %d\n", &globaldata->prv_CMAP1);
-	printf("#define\tGD_PRV_CMAP2 %d\n", &globaldata->prv_CMAP2);
-	printf("#define\tGD_PRV_CMAP3 %d\n", &globaldata->prv_CMAP3);
-	printf("#define\tGD_PRV_PMAP1 %d\n", &globaldata->prv_PMAP1);
-	printf("#define\tGD_INSIDE_INTR %d\n", &globaldata->inside_intr);
-	printf("#define\tPS_GLOBALDATA 0x%x\n", &privatespace->globaldata);
-	printf("#define\tPS_PRVPT 0x%x\n", &privatespace->prvpt);
-	printf("#define\tPS_LAPIC 0x%x\n", &privatespace->lapic);
-	printf("#define\tPS_IDLESTACK 0x%x\n", &privatespace->idlestack);
-	printf("#define\tPS_IDLESTACK_TOP 0x%x\n", &privatespace->CPAGE1);
-	printf("#define\tPS_CPAGE1 0x%x\n", &privatespace->CPAGE1);
-	printf("#define\tPS_CPAGE2 0x%x\n", &privatespace->CPAGE2);
-	printf("#define\tPS_CPAGE3 0x%x\n", &privatespace->CPAGE3);
-	printf("#define\tPS_PPAGE1 0x%x\n", &privatespace->PPAGE1);
-	printf("#define\tPS_IOAPICS 0x%x\n", &privatespace->ioapics);
+	printf("#define\tGD_CPUID %#x\n", OS(globaldata, cpuid));
+	printf("#define\tGD_CPU_LOCKID %#x\n", OS(globaldata, cpu_lockid));
+	printf("#define\tGD_OTHER_CPUS %#x\n", OS(globaldata, other_cpus));
+	printf("#define\tGD_MY_IDLEPTD %#x\n", OS(globaldata, my_idlePTD));
+	printf("#define\tGD_SS_EFLAGS %#x\n", OS(globaldata, ss_eflags));
+	printf("#define\tGD_PRV_CMAP1 %#x\n", OS(globaldata, prv_CMAP1));
+	printf("#define\tGD_PRV_CMAP2 %#x\n", OS(globaldata, prv_CMAP2));
+	printf("#define\tGD_PRV_CMAP3 %#x\n", OS(globaldata, prv_CMAP3));
+	printf("#define\tGD_PRV_PMAP1 %#x\n", OS(globaldata, prv_PMAP1));
+	printf("#define\tGD_INSIDE_INTR %#x\n", OS(globaldata, inside_intr));
+	printf("#define\tPS_GLOBALDATA %#x\n", OS(privatespace, globaldata));
+	printf("#define\tPS_PRVPT %#x\n", OS(privatespace, prvpt));
+	printf("#define\tPS_LAPIC %#x\n", OS(privatespace, lapic));
+	printf("#define\tPS_IDLESTACK %#x\n", OS(privatespace, idlestack));
+	printf("#define\tPS_IDLESTACK_TOP %#x\n", OS(privatespace, CPAGE1));
+	printf("#define\tPS_CPAGE1 %#x\n", OS(privatespace, CPAGE1));
+	printf("#define\tPS_CPAGE2 %#x\n", OS(privatespace, CPAGE2));
+	printf("#define\tPS_CPAGE3 %#x\n", OS(privatespace, CPAGE3));
+	printf("#define\tPS_PPAGE1 %#x\n", OS(privatespace, PPAGE1));
+	printf("#define\tPS_IOAPICS %#x\n", OS(privatespace, ioapics));
 #endif
 
 	return (0);
