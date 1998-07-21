9d851d06f59bdb7fd4744552ea2f637acbfbfd93
jkh <jkh@FreeBSD.org>
1998-07-21 13:49:50 +0000

    Now that the tree is tagged, set to 2.2.7-STABLE

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -32,11 +32,11 @@
 # SUCH DAMAGE.
 #
 #	@(#)newvers.sh	8.1 (Berkeley) 4/20/94
-#	$Id: newvers.sh,v 1.26.2.25 1998/03/24 18:07:21 jkh Exp $
+#	$Id: newvers.sh,v 1.26.2.26 1998/07/21 12:06:10 jkh Exp $
 
 TYPE="FreeBSD"
 REVISION="2.2.7"
-BRANCH="RELEASE"
+BRANCH="STABLE"
 RELEASE="${REVISION}-${BRANCH}"
 SNAPDATE=""
 if [ "X${SNAPDATE}" != "X" ]; then
