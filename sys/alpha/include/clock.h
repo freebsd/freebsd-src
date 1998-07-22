9bc86e47bdf187b647385939aef7896aff44dd2b
dfr <dfr@FreeBSD.org>
1998-07-22 08:26:23 +0000

    Add declaration of {aquire,release}_timer2().

--- sys/alpha/include/clock.h
+++ sys/alpha/include/clock.h
@@ -3,7 +3,7 @@
  * Garrett Wollman, September 1994.
  * This file is in the public domain.
  *
- *	$Id: clock.h,v 1.34 1998/03/05 21:45:42 tegge Exp $
+ *	$Id: clock.h,v 1.1 1998/06/10 10:54:35 dfr Exp $
  */
 
 #ifndef _MACHINE_CLOCK_H_
@@ -16,6 +16,8 @@ extern	int	adjkerntz;
 
 void	DELAY __P((int usec));
 int	sysbeep __P((int pitch, int period));
+int	acquire_timer2 __P((int mode));
+int	release_timer2 __P((void));
 
 #endif /* KERNEL */
 
