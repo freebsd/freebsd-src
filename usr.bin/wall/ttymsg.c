9b9e03a220ec19ceb98ea50d90f27e54c4599db5
phk <phk@FreeBSD.org>
1998-07-22 20:34:24 +0000

    A bit of PR7278 which belongs here. PR:		7278 Reviewed by:	phk Submitted by:	Harlan.Stenn@pfcs.com

--- usr.bin/wall/ttymsg.c
+++ usr.bin/wall/ttymsg.c
@@ -36,7 +36,7 @@
 static char sccsid[] = "@(#)ttymsg.c	8.2 (Berkeley) 11/16/93";
 #endif
 static const char rcsid[] =
-	"$Id$";
+	"$Id: ttymsg.c,v 1.2 1998/03/23 07:47:31 charnier Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -68,7 +68,7 @@ ttymsg(iov, iovcnt, line, tmout)
 	static char device[MAXNAMLEN] = _PATH_DEV;
 	static char errbuf[1024];
 	register int cnt, fd, left, wret;
-	struct iovec localiov[6];
+	struct iovec localiov[7];
 	int forked = 0;
 
 	if (iovcnt > sizeof(localiov) / sizeof(localiov[0]))
