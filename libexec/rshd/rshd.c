d84a831797f684a9ba3a216dc0df8d7f4d1258a2
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-07-24 04:32:21 +0000

    Fix bug I introduced in REMOTEHOST handling (whoops!).
    Submitted by:	Andre Albsmeier <andre.albsmeier@mchp.siemens.de>

--- libexec/rshd/rshd.c
+++ libexec/rshd/rshd.c
@@ -42,7 +42,7 @@ static const char copyright[] =
 static const char sccsid[] = "@(#)rshd.c	8.2 (Berkeley) 4/6/94";
 #endif
 static const char rcsid[] =
-	"$Id: rshd.c,v 1.9.2.7 1998/07/19 06:53:02 jkh Exp $";
+	"$Id: rshd.c,v 1.9.2.8 1998/07/19 11:02:20 jkh Exp $";
 #endif /* not lint */
 
 /*
@@ -304,7 +304,7 @@ doit(fromp)
 	(void) alarm(0);
 	if (port != 0) {
 		int lport = IPPORT_RESERVED - 1;
-		int s = rresvport(&lport);
+		s = rresvport(&lport);
 		if (s < 0) {
 			syslog(LOG_ERR, "can't get stderr port: %m");
 			exit(1);
