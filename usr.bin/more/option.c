42580a0726d7803edcbbd248a755203098b01317
Tim Vanderhoek <hoek@FreeBSD.org>
1998-07-25 05:56:49 +0000

    Nuke obsolote and broken -# option. Order options.
    PR:		bin/5996
    Submitted by:	Max Euston <meuston@jmrodgers.com>

--- usr.bin/more/option.c
+++ usr.bin/more/option.c
@@ -37,7 +37,7 @@
 static char sccsid[] = "@(#)option.c	8.1 (Berkeley) 6/6/93";
 #endif
 static const char rcsid[] =
-	"$Id$";
+	"$Id: option.c,v 1.3 1997/07/29 06:42:33 charnier Exp $";
 #endif /* not lint */
 
 #include <stdio.h>
@@ -75,23 +75,8 @@ option(argc, argv)
 			(*a)[0] = '-';
 
 	optind = 1;		/* called twice, re-init getopt. */
-	while ((ch = getopt(argc, argv, "0123456789/:ceinst:ux:f")) != -1)
+	while ((ch = getopt(argc, argv, "/:ceinst:ux:f")) != -1)
 		switch((char)ch) {
-		case '0': case '1': case '2': case '3': case '4':
-		case '5': case '6': case '7': case '8': case '9':
-			/*
-			 * kludge: more was originally designed to take
-			 * a number after a dash.
-			 */
-			if (!sc_window_set) {
-				p = argv[optind - 1];
-				if (p[0] == '-' && p[1] == ch && !p[2])
-					sc_height = atoi(++p);
-				else
-					sc_height = atoi(argv[optind] + 1);
-				sc_window_set = 1;
-			}
-			break;
 		case '/':
 			firstsearch = optarg;
 			break;
@@ -135,6 +120,6 @@ static void
 usage()
 {
 	fprintf(stderr,
-	"usage: more [-ceinus] [-t tag] [-x tabs] [-/ pattern] [-#] [file ...]\n");
+	"usage: more [-ceinsu] [-t tag] [-x tabs] [-/ pattern] [file ...]\n");
 	exit(1);
 }
