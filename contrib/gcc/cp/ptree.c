559a9a5f7d40627eaecd0fc3cd60b6d49c3853b9
bde <bde@FreeBSD.org>
1998-08-02 14:57:00 +0000

    Fixed printf format errors.  gcc should have a macro HOST_PTR_PRINTF_TYPE to go with its HOST_PTR_PRINTF[_FORMAT], since if %p is wrong for the format then `void *' is probably wrong for the type.

--- contrib/gcc/cp/ptree.c
+++ contrib/gcc/cp/ptree.c
@@ -46,17 +46,17 @@ print_lang_decl (file, node, indent)
   if (DECL_MAIN_VARIANT (node))
     {
       fprintf (file, " decl-main-variant ");
-      fprintf (file, HOST_PTR_PRINTF, DECL_MAIN_VARIANT (node));
+      fprintf (file, HOST_PTR_PRINTF, (void *) DECL_MAIN_VARIANT (node));
     }
   if (DECL_PENDING_INLINE_INFO (node))
     {
       fprintf (file, " pending-inline-info ");
-      fprintf (file, HOST_PTR_PRINTF, DECL_PENDING_INLINE_INFO (node));
+      fprintf (file, HOST_PTR_PRINTF, (void *) DECL_PENDING_INLINE_INFO (node));
     }
   if (DECL_TEMPLATE_INFO (node))
     {
       fprintf (file, " template-info ");
-      fprintf (file, HOST_PTR_PRINTF,  DECL_TEMPLATE_INFO (node));
+      fprintf (file, HOST_PTR_PRINTF, (void *) DECL_TEMPLATE_INFO (node));
     }
 }
 
