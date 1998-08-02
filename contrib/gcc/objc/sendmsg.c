6e3d74bd30e13b6a0ca70f0ccd2f7246183bc70d
bde <bde@FreeBSD.org>
1998-08-02 14:57:00 +0000

    Fixed printf format errors.  gcc should have a macro HOST_PTR_PRINTF_TYPE to go with its HOST_PTR_PRINTF[_FORMAT], since if %p is wrong for the format then `void *' is probably wrong for the type.

--- contrib/gcc/objc/sendmsg.c
+++ contrib/gcc/objc/sendmsg.c
@@ -542,12 +542,15 @@ void __objc_print_dtable_stats()
 #endif
 	 );
 
-  printf("arrays: %d = %ld bytes\n", narrays, (int)narrays*sizeof(struct sarray));
+  printf("arrays: %d = %lu bytes\n",
+	 narrays, (unsigned long)narrays*sizeof(struct sarray));
   total += narrays*sizeof(struct sarray);
-  printf("buckets: %d = %ld bytes\n", nbuckets, (int)nbuckets*sizeof(struct sbucket));
+  printf("buckets: %d = %lu bytes\n",
+	 nbuckets, (unsigned long)nbuckets*sizeof(struct sbucket));
   total += nbuckets*sizeof(struct sbucket);
 
-  printf("idxtables: %d = %ld bytes\n", idxsize, (int)idxsize*sizeof(void*));
+  printf("idxtables: %d = %lu bytes\n",
+	 idxsize, (unsigned long)idxsize*sizeof(void*));
   total += idxsize*sizeof(void*);
   printf("-----------------------------------\n");
   printf("total: %d bytes\n", total);
