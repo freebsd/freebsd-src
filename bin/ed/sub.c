d1ade4ad42c694ccf7a23e837dd3bf92e5232da2
Doug Rabson <dfr@FreeBSD.org>
1998-08-07 09:39:11 +0000

    Silence some warnings when building on the alpha.

--- bin/ed/sub.c
+++ bin/ed/sub.c
@@ -31,7 +31,7 @@
 static char * const rcsid = "@(#)sub.c,v 1.1 1994/02/01 00:34:44 alm Exp";
 #else
 static char * const rcsid =
-	"$Id: sub.c,v 1.9 1997/02/22 14:03:19 peter Exp $";
+	"$Id: sub.c,v 1.10 1997/08/07 21:33:44 steve Exp $";
 #endif
 #endif /* not lint */
 
@@ -46,7 +46,7 @@ int rhbufi;			/* rhs substitution buffer index */
 int
 extract_subst_tail(flagp, np)
 	int *flagp;
-	int *np;
+	long *np;
 {
 	char delimiter;
 
