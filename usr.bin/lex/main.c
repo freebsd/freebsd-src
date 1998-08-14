93d91eb78e032a8c4265d130cc099e96d33eb794
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-08-14 06:35:37 +0000

    /usr/src/usr.bin/lex/Makefile puts FlexLexer.h into /usr/include/g++, but flex still generates "#include <FlexLexer.h".  As a result, C++ sources flex generates failed to be compiled.
    PR:		7575
    Reviewed by:	phk
    Submitted by:	MOROHOSHI Akihiko <moro@remus.dti.ne.jp>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@38314 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/lex/main.c
+++ usr.bin/lex/main.c
@@ -32,7 +32,7 @@ char copyright[] =
  All rights reserved.\n";
 #endif /* not lint */
 
-/* $Header: /home/ncvs/src/usr.bin/lex/main.c,v 1.1.1.2 1996/06/19 20:26:16 nate Exp $ */
+/* $Header: /home/ncvs/src/usr.bin/lex/main.c,v 1.3 1996/06/19 20:47:29 nate Exp $ */
 
 
 #include "flexdef.h"
@@ -1006,7 +1006,7 @@ _( "Variable trailing context rules entail a large performance penalty\n" ) );
 
 	if ( C_plus_plus )
 		{
-		outn( "\n#include <FlexLexer.h>" );
+		outn( "\n#include <g++/FlexLexer.h>" );
 
 		if ( yyclass )
 			{
