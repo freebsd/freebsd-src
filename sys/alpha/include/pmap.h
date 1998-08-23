c0efc73d67bcdbf621c1f90b4917bd5bccd40619
dfr <dfr@FreeBSD.org>
1998-08-23 16:05:55 +0000

    Use address space numbers to reduce TLB flushes.

--- sys/alpha/include/pmap.h
+++ sys/alpha/include/pmap.h
@@ -43,7 +43,7 @@
  *	from: hp300: @(#)pmap.h	7.2 (Berkeley) 12/16/90
  *	from: @(#)pmap.h	7.4 (Berkeley) 5/12/91
  *	from: i386 pmap.h,v 1.54 1997/11/20 19:30:35 bde Exp
- * 	$Id$
+ * 	$Id: pmap.h,v 1.2 1998/06/10 10:55:16 dfr Exp $
  */
 
 #ifndef _MACHINE_PMAP_H_
@@ -166,6 +166,9 @@ struct pmap {
 	TAILQ_HEAD(,pv_entry)	pm_pvlist;	/* list of mappings in pmap */
 	int			pm_count;	/* reference count */
 	int			pm_flags;	/* pmap flags */
+	int			pm_active;	/* active flag */
+	int			pm_asn;		/* address space number */
+	u_int			pm_asngen;	/* generation number of pm_asn */
 	struct pmap_statistics	pm_stats;	/* pmap statistics */
 	struct	vm_page		*pm_ptphint;	/* pmap ptp hint */
 };
