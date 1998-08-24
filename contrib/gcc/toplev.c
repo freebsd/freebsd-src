cf6b3ab11664ebb7cba71890cb273c2fd72670f5
bde <bde@FreeBSD.org>
1998-08-24 06:33:52 +0000

    Support printf format checking of FreeBSD kernel formats %[Dbrz]. This is enabled by the undocumented option -fformat-extensions. This option should be named better and/or give more control over the extensions.
    Fixed a message - don't warn about the field width when it's the
    precision that has the wrong type.  Didn't fix excessive checking
    for the precision relative to the type - ANSI requires both to be
    ints, but gcc permits the field width to be either int or unsigned
    int.

--- contrib/gcc/toplev.c
+++ contrib/gcc/toplev.c
@@ -521,6 +521,10 @@ int flag_gnu_linker = 1;
 /* Tag all structures with __attribute__(packed) */
 int flag_pack_struct = 0;
 
+/* Nonzero means that -Wformat accepts certain system-dependent formats.  */
+
+int flag_format_extensions = 0;
+
 /* Table of language-independent -f options.
    STRING is the option name.  VARIABLE is the address of the variable.
    ON_VALUE is the value to store in VARIABLE
@@ -567,6 +571,7 @@ struct { char *string; int *variable; int on_value;} f_options[] =
   {"verbose-asm", &flag_verbose_asm, 1},
   {"gnu-linker", &flag_gnu_linker, 1},
   {"pack-struct", &flag_pack_struct, 1},
+  {"format-extensions", &flag_format_extensions, 1},
   {"bytecode", &output_bytecode, 1}
 };
 
