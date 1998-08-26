85d9f4f7858b4f3689c7b7ec09aba0617ba4cd30
Satoshi Asami <asami@FreeBSD.org>
1998-08-26 23:53:36 +0000

    Oops, accidentally merged the changes before committing first. ;)
    The previous commit was to merge 1.285 (will be committed now) from
    the main branch.

--- share/mk/bsd.port.mk
+++ share/mk/bsd.port.mk
@@ -1,7 +1,7 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4
 #
-#	$Id: bsd.port.mk,v 1.227.2.46 1998/08/15 17:37:46 markm Exp $
+#	$Id: bsd.port.mk,v 1.227.2.47 1998/08/26 23:46:54 asami Exp $
 #	$NetBSD: $
 #
 #	bsd.port.mk - 940820 Jordan K. Hubbard.
