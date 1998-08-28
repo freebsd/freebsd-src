33a677a8c923c47f656cbbd432a6c47b51245166
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-08-28 01:03:58 +0000

    MFC: chflags -R noschg /usr/lib

--- release/sysinstall/installUpgrade.c
+++ release/sysinstall/installUpgrade.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: installUpgrade.c,v 1.33.2.20 1998/03/23 23:41:13 jkh Exp $
+ * $Id: installUpgrade.c,v 1.33.2.21 1998/05/24 19:50:18 steve Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -276,7 +276,7 @@ installUpgrade(dialogMenuItem *self)
 	}
 
 	msgNotify("chflags'ing old binaries - please wait.");
-	(void)vsystem("chflags -R noschg /bin /sbin /usr/sbin /usr/bin /kernel*");
+	(void)vsystem("chflags -R noschg /bin /sbin /usr/sbin /usr/bin /usr/lib /kernel*");
 
 	if (file_readable("/kernel")) {
 	    msgNotify("Moving old kernel to /kernel.prev");
