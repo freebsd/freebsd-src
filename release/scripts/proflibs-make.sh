ccf6866a9aec50eac195071d19011718737319ad
jb <jb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-08-30 02:52:04 +0000

    BINFORMAT -> OBJFORMAT ready for E-day.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@38635 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/scripts/proflibs-make.sh
+++ release/scripts/proflibs-make.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Move the profiled libraries out to their own dist
-for i in ${RD}/trees/bin/usr/lib/${BINFORMAT}/*_p.a; do
+for i in ${RD}/trees/bin/usr/lib/${OBJFORMAT}/*_p.a; do
 	if [ -f $i ]; then
-		mv $i ${RD}/trees/proflibs/usr/lib/${BINFORMAT};
+		mv $i ${RD}/trees/proflibs/usr/lib/${OBJFORMAT};
 	fi;
 done
