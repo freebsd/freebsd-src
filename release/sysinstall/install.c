5fb5c2a80fb85301e1f1a19a8e183d637649d512
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-08-31 17:48:39 +0000

    MFC: Leave router_enable alone.

--- release/sysinstall/install.c
+++ release/sysinstall/install.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: install.c,v 1.134.2.68 1998/03/24 05:14:07 jkh Exp $
+ * $Id: install.c,v 1.134.2.69 1998/07/18 09:35:23 jkh Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -997,7 +997,6 @@ installVarDefaults(dialogMenuItem *self)
     char *cp;
 
     /* Set default startup options */
-    variable_set2(VAR_ROUTER_ENABLE,		"NO");
     variable_set2(VAR_RELNAME,			RELEASE_NAME);
     variable_set2(VAR_CPIO_VERBOSITY,		"high");
     variable_set2(VAR_TAPE_BLOCKSIZE,		DEFAULT_TAPE_BLOCKSIZE);
