629ae66a3d30be34600a82080f52d45046ce36ce
David E. O'Brien <obrien@FreeBSD.org>
1998-09-01 22:13:41 +0000

    [As I was saying]
    I believe this by bde from bsd.prog.mk still applies:
    
    To get the fixes and simplifications, application Makefiles should put the raw
    lex and yacc source names (foo.l and bar.y) in SRCS and not put the names of
    any generated files in SRCS or CLEANFILES.
    
    If you want to regress to generating foo.c from foo.y via y.tab.c for crufty
    applications that want a y.tab.h file.  This want must be specified by putting
    y.tab.h in SRCS (and defaulting to or putting -d in YFLAGS).  This only works
    if there is only one yacc parser, of course.
    
    Complications: if bar.y is in SRCS and -d is in YFLAGS, then bar.h will be
    generated whether or not it is used, even if this clobbers a real source file.

--- share/mk/bsd.dep.mk
+++ share/mk/bsd.dep.mk
@@ -1,4 +1,4 @@
-#	$Id: bsd.dep.mk,v 1.25 1998/06/05 18:38:54 dt Exp $
+#	$Id: bsd.dep.mk,v 1.8.2.6 1998/09/01 22:09:46 obrien Exp $
 #
 # The include file <bsd.dep.mk> handles Makefile dependencies.
 #
