9a6b0ba36d96579ea1310c28726be4873c5c4d11
kato <kato@FreeBSD.org>
1998-09-01 02:04:17 +0000

    - Fix style bug. - hw.ispc98 -> machdep.ispc98.
    Submitted by:	Garrett Wollman (hw -> machdep)

--- sys/i386/i386/machdep.c
+++ sys/i386/i386/machdep.c
@@ -35,7 +35,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91
- *	$Id: machdep.c,v 1.305 1998/08/31 08:41:36 kato Exp $
+ *	$Id: machdep.c,v 1.306 1998/08/31 16:18:45 luoqi Exp $
  */
 
 #include "apm.h"
@@ -170,11 +170,11 @@ SYSCTL_INT(_debug, OID_AUTO, tlb_flush_count,
 #endif
 
 #ifdef PC98
-int ispc98 = 1;
+int	ispc98 = 1;
 #else
 int	ispc98 = 0;
 #endif
-SYSCTL_INT(_hw, OID_AUTO, ispc98, CTLFLAG_RD, &ispc98, 0, "");
+SYSCTL_INT(_machdep, OID_AUTO, ispc98, CTLFLAG_RD, &ispc98, 0, "");
 
 int physmem = 0;
 int cold = 1;
