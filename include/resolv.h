49e8b6c146df22e6509f9ba2e55cffa554ac8060
brian <brian@FreeBSD.org>
1998-09-07 19:54:08 +0000

    Remove the extern decl of ``h_errno''.  It's already externed in netdb.h - where all of its #define values live.  If anything requires h_errno here (nothing in /usr/src seems to) it's almost definitely broken.

--- include/resolv.h
+++ include/resolv.h
@@ -51,7 +51,7 @@
 /*
  *	@(#)resolv.h	8.1 (Berkeley) 6/2/93
  *	From Id: resolv.h,v 8.12 1998/04/28 19:36:46 halley Exp $
- *	$Id: resolv.h,v 1.15 1998/06/11 08:55:13 peter Exp $
+ *	$Id: resolv.h,v 1.16 1998/06/11 12:52:45 peter Exp $
  */
 
 #ifndef _RESOLV_H_
@@ -71,15 +71,6 @@
  */
 
 #define	__RES	19960801
-
-/*
- * This used to be defined in res_query.c, now it's in herror.c.  It was
- * never extern'd by any *.h file before it was placed here.  herror.c is
- * part of libresolv.a even though it might make more sense in libnetdb.a
- * or even libnet.a.
- */
-
-extern int h_errno;
 
 /*
  * Resolver configuration file.
