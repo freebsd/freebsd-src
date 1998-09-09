38ad69f345a0d683f184a8d1546f72eb101b99d9
bde <bde@FreeBSD.org>
1998-09-09 10:04:58 +0000

    Turned on -Wformat -fformat-extensions.  -fformat-extensions requires a -current version of gcc.  Without it, -Wformat would complain about all the nonstandard %[Dbrz] formats in the kernel.

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -1,14 +1,15 @@
-#	$Id: bsd.kern.mk,v 1.8 1997/10/21 10:39:27 bde Exp $
+#	$Id: bsd.kern.mk,v 1.9 1998/05/01 18:13:37 bde Exp $
 
 #
 # Warning flags for compiling the kernel and components of the kernel.
 #
 CWARNFLAGS?=	-Wreturn-type -Wcomment -Wredundant-decls -Wimplicit \
 		-Wnested-externs -Wstrict-prototypes -Wmissing-prototypes \
-		-Wpointer-arith -Winline -Wuninitialized -ansi
+		-Wpointer-arith -Winline -Wuninitialized -Wformat \
+		-fformat-extensions -ansi
 #
 # The following flags are next up for working on:
-#	-W -Wunused -Wcast-qual -Wformat -Wall
+#	-W -Wunused -Wcast-qual -Wall
 #
 # When working on removing warnings from code, the `-Werror' flag should be
 # of material assistance.
