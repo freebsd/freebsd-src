5279ba7b15b4567ed68a3eca5a0b3197d8a9f116
brian <brian@FreeBSD.org>
1998-09-09 00:03:09 +0000

    Initialise lcp::his_mru to the ``set mtu'' value if it's less than DEF_MRU, allowing our interface mtu to be decreased despite negotiation with the peer.

--- usr.sbin/ppp/lcp.c
+++ usr.sbin/ppp/lcp.c
@@ -17,7 +17,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: lcp.c,v 1.62 1998/08/07 18:42:49 brian Exp $
+ * $Id: lcp.c,v 1.63 1998/09/04 18:25:59 brian Exp $
  *
  * TODO:
  *	o Limit data field length by MRU
@@ -233,7 +233,9 @@ lcp_Setup(struct lcp *lcp, int openmode)
   lcp->fsm.open_mode = openmode;
   lcp->fsm.maxconfig = 10;
 
-  lcp->his_mru = DEF_MRU;
+  lcp->his_mru = lcp->fsm.bundle->cfg.mtu;
+  if (!lcp->his_mru || lcp->his_mru > DEF_MRU)
+    lcp->his_mru = DEF_MRU;
   lcp->his_mrru = 0;
   lcp->his_magic = 0;
   lcp->his_lqrperiod = 0;
