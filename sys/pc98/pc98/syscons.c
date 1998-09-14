db6f50bf508af669a58aab9151cd69fdcfa9ef79
KATO Takenori <kato@FreeBSD.org>
1998-09-14 11:37:58 +0000

    Sync with sys/i386/isa/syscons.c revision 1.277.

--- sys/pc98/pc98/syscons.c
+++ sys/pc98/pc98/syscons.c
@@ -25,7 +25,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *  $Id: syscons.c,v 1.99 1998/08/19 09:35:18 kato Exp $
+ *  $Id: syscons.c,v 1.100 1998/08/28 12:45:43 kato Exp $
  */
 
 #include "sc.h"
@@ -160,7 +160,6 @@ static  scr_stat    	main_console;
 static  scr_stat    	*console[MAXCONS];
 #ifdef DEVFS
 static	void		*sc_devfs_token[MAXCONS];
-static	void		*sc_vga_devfs_token;
 static	void		*sc_mouse_devfs_token;
 static	void		*sc_console_devfs_token;
 #endif
@@ -966,7 +965,6 @@ scattach(struct isa_device *dev)
     for (vc = 0; vc < MAXCONS; vc++)
         sc_devfs_token[vc] = devfs_add_devswf(&sc_cdevsw, vc, DV_CHR,
 				UID_ROOT, GID_WHEEL, 0600, "ttyv%r", vc);
-    sc_vga_devfs_token = devfs_link(sc_devfs_token[0], "vga");
     sc_mouse_devfs_token = devfs_add_devswf(&sc_cdevsw, SC_MOUSE, DV_CHR,
 				UID_ROOT, GID_WHEEL, 0600, "sysmouse");
     sc_console_devfs_token = devfs_add_devswf(&sc_cdevsw, SC_CONSOLE, DV_CHR,
