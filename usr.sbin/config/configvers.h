56a45ac8704b7a7445fe858195f96879c1d1e776
gibbs <gibbs@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-09-15 21:07:54 +0000

    Bump config version.  Also mention that even after rebuilding a new config, you may need to resynchronize your config file with syntax or name changes documented in GENERIC or LINT.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@39307 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/config/configvers.h
+++ usr.sbin/config/configvers.h
@@ -6,6 +6,6 @@
  * The numbering scheme is inspired by the sys/conf/newvers.sh RELDATE
  * and <osreldate.h> system.
  *
- * $Id: configvers.h,v 1.4 1998/07/12 09:52:45 bde Exp $
+ * $Id: configvers.h,v 1.5 1998/09/03 21:03:43 nsouch Exp $
  */
-#define	CONFIGVERS	300006
+#define	CONFIGVERS	300007
