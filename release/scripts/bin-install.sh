0c51219434eee084881510ea4a86191ae47ac3b7
jkh <jkh@FreeBSD.org>
1998-09-22 08:43:10 +0000

    Allow the use of ${DESTDIR} in the enviroment to optionally get these various collections to install someplace else.

--- release/scripts/bin-install.sh
+++ release/scripts/bin-install.sh
@@ -4,9 +4,10 @@ if [ "`id -u`" != "0" ]; then
 	echo "Sorry, this must be done as root."
 	exit 1
 fi
-echo "You are about to extract the base distribution into / - are you SURE"
+
+echo "You are about to extract the base distribution into ${DESTDIR:-/} - are you SURE"
 echo -n "you want to do this over your installed system (y/n)? "
 read ans 
 if [ "$ans" = "y" ]; then
-	cat bin.?? | tar --unlink -xpzf - -C /
+	cat bin.?? | tar --unlink -xpzf - -C ${DESTDIR:-/}
 fi
--- release/scripts/proflibs-install.sh
+++ release/scripts/proflibs-install.sh
@@ -4,5 +4,5 @@ if [ "`id -u`" != "0" ]; then
 	echo "Sorry, this must be done as root."
 	exit 1
 fi
-cat proflibs.?? | tar --unlink -xpzf - -C /
+cat proflibs.?? | tar --unlink -xpzf - -C ${DESTDIR:-/}
 exit 0
