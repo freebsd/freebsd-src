fd87130aa057f555ce6532f96b0ef67ae2a178c3
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-09-22 08:43:10 +0000

    Allow the use of ${DESTDIR} in the enviroment to optionally get these various collections to install someplace else.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@39556 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/scripts/catpages-install.sh
+++ release/scripts/catpages-install.sh
@@ -4,5 +4,5 @@ if [ "`id -u`" != "0" ]; then
 	echo "Sorry, this must be done as root."
 	exit 1
 fi
-cat catpages.?? | tar --unlink -xpzf - -C /
+cat catpages.?? | tar --unlink -xpzf - -C ${DESTDIR:-/}
 exit 0
