28568245483854af53b5e403d5cc9edc8473086c
wpaul <wpaul@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-09-26 01:29:51 +0000

    #ifdef out DES ioctls which don't exist in FreeBSD. These originated with the SunRPC code to allow the use of hardware DES on certain Sun hardware that supported it (if you installed the appropriate kit). We don't have them and they apparently break the ioctl table generation for kdump.
    Pointed out by: bde
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@39661 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- include/rpc/des.h
+++ include/rpc/des.h
@@ -56,6 +56,14 @@ struct desparams {
 #	define des_buf	UDES.UDES_buf	/* otherwise, pointer to data */
 };
 
+#ifdef notdef
+
+/*
+ * These ioctls are only implemented in SunOS. Maybe someday
+ * if somebody writes a driver for DES hardware that works
+ * with FreeBSD, we can being that back.
+ */
+
 /*
  * Encrypt an arbitrary sized buffer
  */
@@ -65,6 +73,8 @@ struct desparams {
  * Encrypt of small amount of data, quickly
  */
 #define DESIOCQUICK	_IOWR(d, 7, struct desparams) 
+
+#endif
 
 /*
  * Software DES.
