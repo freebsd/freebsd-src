e406ae60dce378743f3161a920841d6318e807fe
kato <kato@FreeBSD.org>
1998-09-28 08:28:40 +0000

    Sync with sys/i386/conf/options.i386 and sys/i386/isa/syscons.c revisions 1.26.2.19 and 1.182.2.36, respectively.
    Submitted by:	Takahashi Yoshihiro <nyan@wyvern.cc.kogakuin.ac.jp>

--- sys/pc98/pc98/syscons.c
+++ sys/pc98/pc98/syscons.c
@@ -25,7 +25,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *  $Id: syscons.c,v 1.13.2.32 1998/08/07 18:08:05 kato Exp $
+ *  $Id: syscons.c,v 1.13.2.33 1998/08/10 16:59:16 kato Exp $
  */
 
 #include "sc.h"
@@ -5465,24 +5465,24 @@ set_font_mode(u_char *buf)
     outb(ATC, 0x20);            		/* enable palette */
 
 #if SLOW_VGA
-#ifndef SC_BAD_FLICKER
+#ifdef SC_ALT_SEQACCESS
     outb(TSIDX, 0x00); outb(TSREG, 0x01);
 #endif
     outb(TSIDX, 0x02); outb(TSREG, 0x04);
     outb(TSIDX, 0x04); outb(TSREG, 0x07);
-#ifndef SC_BAD_FLICKER
+#ifdef SC_ALT_SEQACCESS
     outb(TSIDX, 0x00); outb(TSREG, 0x03);
 #endif
     outb(GDCIDX, 0x04); outb(GDCREG, 0x02);
     outb(GDCIDX, 0x05); outb(GDCREG, 0x00);
     outb(GDCIDX, 0x06); outb(GDCREG, 0x04);
 #else
-#ifndef SC_BAD_FLICKER
+#ifdef SC_ALT_SEQACCESS
     outw(TSIDX, 0x0100);
 #endif
     outw(TSIDX, 0x0402);
     outw(TSIDX, 0x0704);
-#ifndef SC_BAD_FLICKER
+#ifdef SC_ALT_SEQACCESS
     outw(TSIDX, 0x0300);
 #endif
     outw(GDCIDX, 0x0204);
@@ -5505,12 +5505,12 @@ set_normal_mode(u_char *buf)
     outb(ATC, 0x20);            		/* enable palette */
 
 #if SLOW_VGA
-#ifndef SC_BAD_FLICKER
+#ifdef SC_ALT_SEQACCESS
     outb(TSIDX, 0x00); outb(TSREG, 0x01);
 #endif
     outb(TSIDX, 0x02); outb(TSREG, buf[0]);
     outb(TSIDX, 0x04); outb(TSREG, buf[1]);
-#ifndef SC_BAD_FLICKER
+#ifdef SC_ALT_SEQACCESS
     outb(TSIDX, 0x00); outb(TSREG, 0x03);
 #endif
     outb(GDCIDX, 0x04); outb(GDCREG, buf[2]);
@@ -5521,12 +5521,12 @@ set_normal_mode(u_char *buf)
 	outb(GDCIDX, 0x06); outb(GDCREG,(buf[4] & 0x03) | 0x0c);
     }
 #else
-#ifndef SC_BAD_FLICKER
+#ifdef SC_ALT_SEQACCESS
     outw(TSIDX, 0x0100);
 #endif
     outw(TSIDX, 0x0002 | (buf[0] << 8));
     outw(TSIDX, 0x0004 | (buf[1] << 8));
-#ifndef SC_BAD_FLICKER
+#ifdef SC_ALT_SEQACCESS
     outw(TSIDX, 0x0300);
 #endif
     outw(GDCIDX, 0x0004 | (buf[2] << 8));
