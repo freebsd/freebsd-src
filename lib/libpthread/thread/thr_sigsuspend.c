7f2185436751a74c97244b3a2d383e3d5d64d37f
jb <jb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-09-30 06:27:31 +0000

    Implementation of an additional state called SIGWAIT (with the previous one renamed to SIGSUSPEND) to fix sigwait().
    Submitted by: Daniel M. Eischen <eischen@vigrid.com>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@39805 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libpthread/thread/thr_sigsuspend.c
+++ lib/libpthread/thread/thr_sigsuspend.c
@@ -44,14 +44,14 @@ sigsuspend(const sigset_t * set)
 
 	/* Check if a new signal set was provided by the caller: */
 	if (set != NULL) {
-		/* Save the current sigmal mask: */
+		/* Save the current signal mask: */
 		oset = _thread_run->sigmask;
 
-		/* Combine the caller's mask with the current one: */
-		_thread_run->sigmask |= *set;
+		/* Change the caller's mask: */
+		_thread_run->sigmask = *set;
 
 		/* Wait for a signal: */
-		_thread_kern_sched_state(PS_SIGWAIT, __FILE__, __LINE__);
+		_thread_kern_sched_state(PS_SIGSUSPEND, __FILE__, __LINE__);
 
 		/* Always return an interrupted error: */
 		errno = EINTR;
