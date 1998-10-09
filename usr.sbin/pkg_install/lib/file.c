1f21e28dc65a305fd2bd4e5192d900026b17bd76
jkh <jkh@FreeBSD.org>
1998-10-09 00:01:16 +0000

    Use lstat() rather than stat in determining whether a file exists, otherwise we'll miss the "symlink exists but points nowhere" case. Submitted by:	asami and/or possibly <ru@ucb.crimea.ua>

--- usr.sbin/pkg_install/lib/file.c
+++ usr.sbin/pkg_install/lib/file.c
@@ -1,6 +1,6 @@
 #ifndef lint
 static const char rcsid[] =
-	"$Id: file.c,v 1.31 1998/09/08 10:42:26 jkh Exp $";
+	"$Id: file.c,v 1.32 1998/09/11 07:26:58 jkh Exp $";
 #endif
 
 /*
@@ -47,7 +47,7 @@ isdir(char *fname)
 {
     struct stat sb;
 
-    if (stat(fname, &sb) != FAIL && S_ISDIR(sb.st_mode))
+    if (lstat(fname, &sb) != FAIL && S_ISDIR(sb.st_mode))
 	return TRUE;
     else
 	return FALSE;
