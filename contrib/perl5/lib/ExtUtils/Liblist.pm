8cb25d1f36a274cd78c98daf1acf82d920a1aa21
markm <markm@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-10-11 17:27:33 +0000

    Fix minor bogon pointed out by perl5's own perldoc.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@40230 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/perl5/lib/ExtUtils/Liblist.pm
+++ contrib/perl5/lib/ExtUtils/Liblist.pm
@@ -2,7 +2,7 @@ package ExtUtils::Liblist;
 use vars qw($VERSION);
 # Broken out of MakeMaker from version 4.11
 
-$VERSION = substr q$Revision: 1.25 $, 10;
+$VERSION = substr q$Revision: 1.1.1.1 $, 10;
 
 use Config;
 use Cwd 'cwd';
@@ -651,7 +651,7 @@ library to search for otherwise.  The C<$Config{lib_ext}> suffix will
 be appended to any entries that are not directories and don't already have
 the suffix.
 
-Note that the C<-L> and <-l> prefixes are B<not required>, but authors
+Note that the C<-L> and C<-l> prefixes are B<not required>, but authors
 who wish their extensions to be portable to Unix or OS/2 should use the
 prefixes, since the Unix-OS/2 version of ext() requires them.
 
