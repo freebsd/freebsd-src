fa0c344f5b9585f93e10eae79e6368ff6ec1b9a8
obrien <obrien@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-10-17 22:20:06 +0000

    To clarify last commit, msmith says: The reason the Alpha has less room than the i386 is because the bootstrap is given a 256K mapping by the firmware; to add more requires extra work.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@40499 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libstand/zalloc_defs.h
+++ lib/libstand/zalloc_defs.h
@@ -26,7 +26,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: zalloc_defs.h,v 1.3 1998/10/01 17:35:08 msmith Exp $
+ *	$Id: zalloc_defs.h,v 1.4 1998/10/16 19:23:37 msmith Exp $
  */
 
 /*
