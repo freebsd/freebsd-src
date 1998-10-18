f503adff97fd986296135bd5e1c880ad2d3df6fb
dfr <dfr@FreeBSD.org>
1998-10-18 19:05:07 +0000

    Rename boot2 to loader.

--- sys/boot/alpha/boot1/boot1.c
+++ sys/boot/alpha/boot1/boot1.c
@@ -1,5 +1,5 @@
 /*
- * $Id: boot1.c,v 1.1.1.1 1998/08/21 03:17:41 msmith Exp $
+ * $Id: boot1.c,v 1.2 1998/09/26 10:51:36 dfr Exp $
  * From	$NetBSD: bootxx.c,v 1.4 1997/09/06 14:08:29 drochner Exp $ 
  */
 
@@ -181,7 +181,7 @@ main()
 
     init_prom_calls();
     
-    loadfile("/boot/boot2", loadaddr);
+    loadfile("/boot/loader", loadaddr);
 
     entry = (void (*)())loadaddr;
     (*entry)();
