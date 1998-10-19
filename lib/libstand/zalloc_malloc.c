17c3d6ec9461b24d5a823534f752107e24759cf3
dfr <dfr@FreeBSD.org>
1998-10-19 09:08:40 +0000

    Implement a hook to allow us to reclaim the memory used by the first stage of the bootstrap (the bit which loads /boot/loader).

--- lib/libstand/zalloc_malloc.c
+++ lib/libstand/zalloc_malloc.c
@@ -26,7 +26,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: zalloc_malloc.c,v 1.2 1998/09/26 10:48:50 dfr Exp $
+ *	$Id: zalloc_malloc.c,v 1.3 1998/10/01 17:35:08 msmith Exp $
  */
 
 /*
@@ -47,6 +47,15 @@ void mallocstats(void);
 #ifdef malloc
 #undef malloc
 #undef free
+#endif
+
+#ifdef __alpha__
+void
+free_region(void *start, void *end)
+{
+    zextendPool(&MallocPool, start, (caddr_t)end - (caddr_t)start);
+    zfree(&MallocPool, start, (caddr_t)end - (caddr_t)start);
+}
 #endif
 
 void *
