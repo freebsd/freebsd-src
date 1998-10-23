c93cd32637aab4af303ca12b02151406a2f9d884
Mike Smith <msmith@FreeBSD.org>
1998-10-23 22:29:19 +0000

    Enable the PCI BIOS PnP enumerator.

--- sys/boot/i386/loader/conf.c
+++ sys/boot/i386/loader/conf.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: conf.c,v 1.8 1998/10/21 20:10:33 msmith Exp $
+ *	$Id: conf.c,v 1.9 1998/10/22 20:23:58 msmith Exp $
  */
 
 #include <stand.h>
@@ -86,11 +86,11 @@ struct console *consoles[] = {
 
 extern struct pnphandler isapnphandler;
 extern struct pnphandler biospnphandler;
-/* extern struct pnphandler pcipnphandler;*/
+extern struct pnphandler biospcihandler;
 
 struct pnphandler *pnphandlers[] = {
     &biospnphandler,		/* should go first, as it may set isapnp_readport */
     &isapnphandler,
-/*    &pcipnphandler, */
+    &biospcihandler,
     NULL
 };
