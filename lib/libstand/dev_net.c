2baf3bb5a02e424dc38b6b9f9ce0969e040e313f
Mike Smith <msmith@FreeBSD.org>
1998-10-31 02:48:29 +0000

    Add a new field to the devsw structure; dv_print, to print all valid units etc. associated with the device entry. Consider EOF an 'error' for fgetstr if we haven't read anything yet.
    You *MUST* recompile and reinstall libstand before rebuilding the bootstrap.

--- lib/libstand/dev_net.c
+++ lib/libstand/dev_net.c
@@ -73,6 +73,8 @@ static int netdev_opens;
 
 static int net_getparams(int sock);
 
+static void net_print(int verbose);
+
 struct devsw netdev = {
     "net", 
     DEVT_NET, 
@@ -81,7 +83,18 @@ struct devsw netdev = {
     net_open, 
     net_close, 
     noioctl
+    net_print,
 };
+
+/*
+ * Print stuff about our net 'device'.
+ */
+static void
+net_print(int verbose)
+{
+    pager_output("    net:  network interface");
+    /* XXX much more verbose stuff here */
+}
 
 /*
  * Called by devopen after it sets f->f_dev to our devsw entry.
