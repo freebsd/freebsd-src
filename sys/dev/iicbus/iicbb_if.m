1f9c4b34fd77c6def5a763655a9217638914c660
nsouch <nsouch@FreeBSD.org>
1998-11-08 18:35:53 +0000

    Add semicolon to INTERFACE declarations

--- sys/dev/iicbus/iicbb_if.m
+++ sys/dev/iicbus/iicbb_if.m
@@ -23,10 +23,10 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-#	$Id$
+#	$Id: iicbb_if.m,v 1.1 1998/10/31 11:31:07 nsouch Exp $
 #
 
-INTERFACE iicbb
+INTERFACE iicbb;
 
 #
 # iicbus callback
