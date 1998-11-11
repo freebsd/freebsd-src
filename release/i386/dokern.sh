0007dac63c0a5bffe202ab954cba57d4fd6f9a5a
jkh <jkh@FreeBSD.org>
1998-11-11 23:08:24 +0000

    Add MATH_EMULATE back to kern.flp.  Now at least the FPU-less have *some* way to install 3.0.x.

--- release/i386/dokern.sh
+++ release/i386/dokern.sh
@@ -40,7 +40,6 @@ elif [ "$MFS" = "NO" ]; then		# can be much bigger
 		-e '/gzip/d' \
 		-e '/PROCFS/d' \
 		-e '/KTRACE/d' \
-		-e '/MATH_EMULATE/d' \
 		-e 's/GENERIC/BOOTMFS/g' \
 fi
 echo "options  NFS_NOSERVER" 
