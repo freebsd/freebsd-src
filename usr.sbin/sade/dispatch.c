ffd97e5342d926be2f35b764575b61c28801658c
jkh <jkh@FreeBSD.org>
1998-11-15 09:06:20 +0000

    o Break fixups into binary fixups and XFree86 based fixups. o Move fixups into extraction routine so all consumers don't have to duplicate   the right behavior. o Make some things more orthogonal (just for asthetics sake) o Add option to go back and do it again if XF86Setup fails (possibly with   a different setup - this one has always annoyed me).

--- usr.sbin/sade/dispatch.c
+++ usr.sbin/sade/dispatch.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: dispatch.c,v 1.24 1997/09/17 16:18:13 pst Exp $
+ * $Id: dispatch.c,v 1.25 1998/07/18 09:41:58 jkh Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -82,7 +82,8 @@ static struct _word {
     { "installExpress",		installExpress		},
     { "installNovice",		installNovice		},
     { "installUpgrade",		installUpgrade		},
-    { "installFixup",		installFixup		},
+    { "installFixupBin",	installFixupBin		},
+    { "installFixupXFree",	installFixupXFree	},
     { "installFixitHoloShell",	installFixitHoloShell	},
     { "installFixitCDROM",	installFixitCDROM	},
     { "installFixitFloppy",	installFixitFloppy	},
