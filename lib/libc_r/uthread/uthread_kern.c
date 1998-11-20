29ea7641e1231f211a3d6643019cf7a0d9b6ca9c
tg <tg@FreeBSD.org>
1998-11-20 07:31:37 +0000

    MFC: Close a window between unlocking a spinlock and changing the thread state. Interrupt threads waiting in select, sleep, and for an fd.

--- lib/libc_r/uthread/uthread_kern.c
+++ lib/libc_r/uthread/uthread_kern.c
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: uthread_kern.c,v 1.3.2.4 1998/04/30 22:26:30 jb Exp $
+ * $Id: uthread_kern.c,v 1.3.2.5 1998/11/04 08:42:11 tg Exp $
  *
  */
 #include <errno.h>
@@ -610,6 +610,22 @@ _thread_kern_sched_state(enum pthread_state state, char *fname, int lineno)
 	_thread_run->state = state;
 	_thread_run->fname = fname;
 	_thread_run->lineno = lineno;
+
+	/* Schedule the next thread that is ready: */
+	_thread_kern_sched(NULL);
+	return;
+}
+
+void
+_thread_kern_sched_state_unlock(enum pthread_state state,
+    spinlock_t *lock, char *fname, int lineno)
+{
+	/* Change the state of the current thread: */
+	_thread_run->state = state;
+	_thread_run->fname = fname;
+	_thread_run->lineno = lineno;
+
+	_SPINUNLOCK(lock);
 
 	/* Schedule the next thread that is ready: */
 	_thread_kern_sched(NULL);
