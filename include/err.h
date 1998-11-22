027e8cf9b19db85639fbc9d3d36d4f459ea49f5c
bde <bde@FreeBSD.org>
1998-11-22 05:12:27 +0000

    Don't forget to use the appropriate __printf*like attribute for verr* and vwarn*.
    Sorted attributes.
    
    UnFrom'ed vendor id.

--- include/err.h
+++ include/err.h
@@ -30,8 +30,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	From: @(#)err.h	8.1 (Berkeley) 6/2/93
- *	$Id: err.h,v 1.5 1998/09/12 21:02:15 wollman Exp $
+ *	@(#)err.h	8.1 (Berkeley) 6/2/93
+ *	$Id: err.h,v 1.6 1998/10/29 22:18:47 jdp Exp $
  */
 
 #ifndef _ERR_H_
@@ -48,18 +48,21 @@
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
-void	err __P((int, const char *, ...)) __printf0like(2, 3) __dead2;
-void	verr __P((int, const char *, _BSD_VA_LIST_)) __dead2;
-void	errc __P((int, int, const char *, ...)) __printf0like(3, 4) __dead2;
-void	verrc __P((int, int, const char *, _BSD_VA_LIST_)) __dead2;
-void	errx __P((int, const char *, ...)) __printf0like(2, 3) __dead2;
-void	verrx __P((int, const char *, _BSD_VA_LIST_)) __dead2;
+void	err __P((int, const char *, ...)) __dead2 __printf0like(2, 3);
+void	verr __P((int, const char *, _BSD_VA_LIST_)) __dead2
+	    __printf0like(2, 0);
+void	errc __P((int, int, const char *, ...)) __dead2 __printf0like(3, 4);
+void	verrc __P((int, int, const char *, _BSD_VA_LIST_)) __dead2
+	    __printf0like(3, 0);
+void	errx __P((int, const char *, ...)) __dead2 __printf0like(2, 3);
+void	verrx __P((int, const char *, _BSD_VA_LIST_)) __dead2
+	    __printf0like(2, 0);
 void	warn __P((const char *, ...)) __printf0like(1, 2);
-void	vwarn __P((const char *, _BSD_VA_LIST_));
+void	vwarn __P((const char *, _BSD_VA_LIST_)) __printf0like(1, 0);
 void	warnc __P((int, const char *, ...)) __printf0like(2, 3);
-void	vwarnc __P((int, const char *, _BSD_VA_LIST_));
+void	vwarnc __P((int, const char *, _BSD_VA_LIST_)) __printf0like(2, 0);
 void	warnx __P((const char *, ...)) __printflike(1, 2);
-void	vwarnx __P((const char *, _BSD_VA_LIST_));
+void	vwarnx __P((const char *, _BSD_VA_LIST_)) __printflike(1, 0);
 void	err_set_file __P((void *));
 void	err_set_exit __P((void (*)(int)));
 __END_DECLS
