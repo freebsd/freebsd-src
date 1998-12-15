8b09cf1cc79a8e113c109388a7af4ee9f7d5340e
Brian Somers <brian@FreeBSD.org>
1998-12-15 19:12:24 +0000

    Fix a rather bad latency problem (uncovered by the recent timer commit). Problem reported by: Christopher Hall <hsw@acm.org>

--- usr.sbin/ppp/datalink.c
+++ usr.sbin/ppp/datalink.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: datalink.c,v 1.20 1998/10/17 12:28:06 brian Exp $
+ *	$Id: datalink.c,v 1.21 1998/12/10 18:36:53 brian Exp $
  */
 
 #include <sys/types.h>
@@ -256,9 +256,9 @@ datalink_UpdateSet(struct descriptor *d, fd_set *r, fd_set *w, fd_set *e,
               log_Printf(LogCHAT, "%s: Dial attempt %u of %d\n",
                         dl->name, dl->cfg.dial.max - dl->dial_tries,
                         dl->cfg.dial.max);
-            return datalink_UpdateSet(d, r, w, e, n);
           } else
             datalink_LoginDone(dl);
+          return datalink_UpdateSet(d, r, w, e, n);
         } else {
           if (!(dl->physical->type & (PHYS_DDIAL|PHYS_DEDICATED)) &&
               dl->cfg.dial.max)
