3d6d60242a7d4ccad87b4cb97889793dc12883af
imp <imp@FreeBSD.org>
1998-12-16 04:51:27 +0000

    Free memory from setmode.
    Obtained from: OpenBSD

--- usr.bin/xinstall/xinstall.c
+++ usr.bin/xinstall/xinstall.c
@@ -42,7 +42,7 @@ static const char copyright[] =
 static char sccsid[] = "From: @(#)xinstall.c	8.1 (Berkeley) 7/21/93";
 #endif
 static const char rcsid[] =
-	"$Id: xinstall.c,v 1.32 1998/06/02 12:00:07 peter Exp $";
+	"$Id: xinstall.c,v 1.33 1998/10/13 14:52:33 des Exp $";
 #endif /* not lint */
 
 /*-
@@ -164,6 +164,7 @@ main(argc, argv)
 				errx(EX_USAGE, "invalid file mode: %s",
 				     optarg);
 			mode = getmode(set, 0);
+			free(set);
 			break;
 		case 'M':
 			nommap = 1;
