1f33c42b9f1f6391bf1eb6fc59a0f26146a1a553
brian <brian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-12-17 00:28:12 +0000

    Don't return stack-based data.  This may have caused server-side CHAP authentication problems in the past :-/
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@41888 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/ppp/auth.c
+++ usr.sbin/ppp/auth.c
@@ -17,7 +17,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: auth.c,v 1.32 1998/08/07 18:42:47 brian Exp $
+ * $Id: auth.c,v 1.33 1998/08/26 17:39:36 brian Exp $
  *
  *	TODO:
  *		o Implement check against with registered IP addresses.
@@ -212,7 +212,7 @@ auth_GetSecret(struct bundle *bundle, const char *system, int len,
   FILE *fp;
   int n;
   char *vector[5];
-  char buff[LINE_LEN];
+  static char buff[LINE_LEN];
 
   fp = OpenSecret(SECRETFILE);
   if (fp == NULL)
