8aa296f883d7c928eafd1494ea68f1cc04b3d8f5
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1998-12-31 02:35:43 +0000

    Eliminate all dependence on boot1 and boot2.  This is passed in by Set_Boot_Blocks() anyway and should thus have never been a part of libdisk, it should have been provided by the client of libdisk since passing the information in is already part of the API.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@42187 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libdisk/libdisk.h
+++ lib/libdisk/libdisk.h
@@ -6,7 +6,7 @@
 * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
 * ----------------------------------------------------------------------------
 *
-* $Id: libdisk.h,v 1.28 1998/01/20 11:03:15 bde Exp $
+* $Id: libdisk.h,v 1.29 1998/10/06 11:57:08 dfr Exp $
 *
 */
 
@@ -86,7 +86,6 @@ struct chunk {
 };
 
 extern const char *chunk_n[];
-extern const u_char boot1[], boot2[];
 
 const char *
 slice_type_name( int type, int subtype );
