01dc6e1a45123f16d4de4db6e97764c45856c744
danny <danny@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-01-05 10:13:54 +0000

    Merge from 2_2 man page change.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@42310 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/touch/touch.c
+++ usr.bin/touch/touch.c
@@ -262,7 +262,7 @@ stime_arg2(arg, year, tvp)
 	t->tm_min = ATOI2(arg);
 	if (year) {
 		t->tm_year = ATOI2(arg);
-		if (t->tm_year < 38)	/* support 2000-2038 not 1902-1969 */
+		if (t->tm_year < 39)	/* support 2000-2038 not 1902-1969 */
 			t->tm_year += 100;
 	}
 
