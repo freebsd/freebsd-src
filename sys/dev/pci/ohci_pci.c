a10090836fbd879ac28aa19511c3eeb7b181c425
n_hibma <n_hibma@FreeBSD.org>
1999-01-06 12:31:28 +0000

    Removed NetBSD specific code

--- sys/dev/pci/ohci_pci.c
+++ sys/dev/pci/ohci_pci.c
@@ -1,5 +1,4 @@
-/*	$NetBSD: ohci_pci.c,v 1.5 1998/11/25 22:32:04 augustss Exp $	*/
-/*	FreeBSD $Id: ohci_pci.c,v 1.5 1998/12/14 09:40:14 n_hibma Exp $ */
+/*	FreeBSD $Id: ohci_pci.c,v 1.6 1998/12/14 21:14:11 julian Exp $ */
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -45,23 +44,15 @@
  * USB spec: http://www.teleport.com/cgi-bin/mailmerge.cgi/~usb/cgiform.tpl
  */
 
-#include <dev/usb/usb_port.h>
-
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kernel.h>
-#if defined(__FreeBSD__)
 #include <sys/module.h>
 #include <sys/bus.h>
-#endif
 #include <sys/device.h>
 #include <sys/proc.h>
 #include <sys/queue.h>
 
-#if defined(__NetBSD__)
-#include <machine/bus.h>
-#include <dev/pci/pcivar.h>
-#elif defined(__FreeBSD__)
 #include <pci/pcivar.h>
 #include <pci/pcireg.h>
 
@@ -72,7 +63,6 @@
 #define PCI_SUBCLASS_SERIALBUS_SSA              0x00020000
 #define PCI_SUBCLASS_SERIALBUS_USB              0x00030000
 #define PCI_SUBCLASS_SERIALBUS_FIBER            0x00040000
-#endif
 
 #include <dev/usb/usb.h>
 #include <dev/usb/usbdi.h>
@@ -81,15 +71,6 @@
 
 #include <dev/usb/ohcireg.h>
 #include <dev/usb/ohcivar.h>
-
-#if defined(__NetBSD__)
-int	ohci_pci_match __P((struct device *, struct cfdata *, void *));
-void	ohci_pci_attach __P((struct device *, struct device *, void *));
-
-struct cfattach ohci_pci_ca = {
-	sizeof(struct ohci_softc), ohci_pci_match, ohci_pci_attach
-};
-#elif defined(__FreeBSD__)
 
 #define PCI_INTERFACE(d)        (((d) >> 8) & 0xff)
 #define PCI_SUBCLASS(d)         ((d) & PCI_SUBCLASS_MASK)
@@ -114,25 +95,7 @@ static struct pci_device ohci_pci_device = {
 };
 
 DATA_SET(pcidevice_set, ohci_pci_device);
-#endif
 
-#if defined(__NetBSD__)
-int
-ohci_pci_match(parent, match, aux)
-	struct device *parent;
-	struct cfdata *match;
-	void *aux;
-{
-	struct pci_attach_args *pa = (struct pci_attach_args *) aux;
-
-	if (PCI_CLASS(pa->pa_class) == PCI_CLASS_SERIALBUS &&
-	    PCI_SUBCLASS(pa->pa_class) == PCI_SUBCLASS_SERIALBUS_USB &&
-	    PCI_INTERFACE(pa->pa_class) == PCI_INTERFACE_OHCI)
-		return 1;
- 
-	return 0;
-}
-#elif defined(__FreeBSD__)
 static const char *
 ohci_pci_probe(pcici_t config_id, pcidi_t device_id)
 {
@@ -142,82 +105,10 @@ ohci_pci_probe(pcici_t config_id, pcidi_t device_id)
 	if (   (PCI_CLASS(class)     == PCI_CLASS_SERIALBUS)
 	    && (PCI_SUBCLASS(class)  == PCI_SUBCLASS_SERIALBUS_USB)
 	    && (PCI_INTERFACE(class) == PCI_INTERFACE_OHCI))
-		return("OHCI Host Controller");
+		return("OHCI USB Host Controller (generic)");
 
 	return NULL;	/* dunno */
 }
-#endif
-
-#if defined(__NetBSD__)
-void
-ohci_pci_attach(parent, self, aux)
-	struct device *parent;
-	struct device *self;
-	void *aux;
-{
-	struct ohci_softc *sc = (struct ohci_softc *)self;
-	struct pci_attach_args *pa = (struct pci_attach_args *)aux;
-	pci_chipset_tag_t pc = pa->pa_pc;
-	char const *intrstr;
-	pci_intr_handle_t ih;
-	pcireg_t csr;
-	char devinfo[256];
-	usbd_status r;
-	char *vendor;
-
-	pci_devinfo(pa->pa_id, pa->pa_class, 0, devinfo);
-	printf(": %s (rev. 0x%02x)\n", devinfo, PCI_REVISION(pa->pa_class));
-
-	/* Map I/O registers */
-	if (pci_mapreg_map(pa, PCI_CBMEM, PCI_MAPREG_TYPE_MEM, 0,
-			   &sc->iot, &sc->ioh, NULL, NULL)) {
-		printf("%s: can't map mem space\n", sc->sc_bus.bdev.dv_xname);
-		return;
-	}
-
-	sc->sc_dmatag = pa->pa_dmat;
-
-	/* Enable the device. */
-	csr = pci_conf_read(pa->pa_pc, pa->pa_tag, PCI_COMMAND_STATUS_REG);
-	pci_conf_write(pa->pa_pc, pa->pa_tag, PCI_COMMAND_STATUS_REG,
-		       csr | PCI_COMMAND_MASTER_ENABLE);
-
-	/* Map and establish the interrupt. */
-	if (pci_intr_map(pc, pa->pa_intrtag, pa->pa_intrpin,
-	    pa->pa_intrline, &ih)) {
-		printf("%s: couldn't map interrupt\n", sc->sc_bus.bdev.dv_xname);
-		return;
-	}
-	intrstr = pci_intr_string(pc, ih);
-	sc->sc_ih = pci_intr_establish(pc, ih, IPL_USB, ohci_intr, sc);
-	if (sc->sc_ih == NULL) {
-		printf("%s: couldn't establish interrupt",
-		    sc->sc_bus.bdev.dv_xname);
-		if (intrstr != NULL)
-			printf(" at %s", intrstr);
-		printf("\n");
-		return;
-	}
-	printf("%s: interrupting at %s\n", sc->sc_bus.bdev.dv_xname, intrstr);
-
-	/* Figure out vendor for root hub descriptor. */
-	vendor = pci_findvendor(pa->pa_id);
-	if (vendor)
-		strncpy(sc->sc_vendor, vendor, sizeof(sc->sc_vendor));
-	else
-		sprintf(sc->sc_vendor, "vendor 0x%04x", PCI_VENDOR(pa->pa_id));
-	
-	r = ohci_init(sc);
-	if (r != USBD_NORMAL_COMPLETION) {
-		printf("%s: init failed, error=%d\n", sc->sc_bus.bdev.dv_xname,
-		       r);
-		return;
-	}
-
-	/* Attach usb device. */
-	config_found((void *)sc, &sc->sc_bus, usbctlprint);
-}
-#elif defined(__FreeBSD__)
 
 static void
 ohci_pci_attach(pcici_t config_id, int unit)
@@ -281,8 +172,9 @@ ohci_pci_attach(pcici_t config_id, int unit)
 	 */
 	sc->sc_bus.bdev = device_add_child(root_bus, "usb", unit, sc);
 	if (!sc->sc_bus.bdev) {
-		DEVICE_ERROR(sc->sc_bus.bdev,
-			("unable to add USB device to root bus\n"));
+		printf("%s%d: unable to add USB device to root bus\n",
+			device_get_name(sc->sc_bus.bdev),
+			device_get_unit(sc->sc_bus.bdev));
 		return;
 	}
 
@@ -293,8 +185,7 @@ ohci_pci_attach(pcici_t config_id, int unit)
 		return;
 	}
 
-	device_set_desc(sc->sc_bus.bdev, "OHCI Host Controller");
+	device_set_desc(sc->sc_bus.bdev, "OHCI USB Host Controller (generic)");
 
 	return;
 }
-#endif
