b6ec0aa465ba23c30a11221b9f22071588bcd0a0
julian <julian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-01-06 23:05:42 +0000

    Add (but don't activate) code for a special VM option to make downward growing stacks more general. Add (but don't activate) code to use the new stack facility when running threads, (specifically the linux threads support). This allows people to use both linux compiled linuxthreads, and also the native FreeBSD linux-threads port.
    The code is conditional on VM_STACK. Not using this will
    produce the old heavily tested system.
    
    Submitted by: Richard Seaman <dick@tar.com>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@42360 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/mman.h
+++ sys/sys/mman.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)mman.h	8.2 (Berkeley) 1/9/95
- * $Id: mman.h,v 1.22 1998/03/08 17:25:33 dufault Exp $
+ * $Id: mman.h,v 1.23 1998/03/28 11:50:38 dufault Exp $
  */
 
 #ifndef _SYS_MMAN_H_
@@ -64,6 +64,9 @@
 #define	MAP_INHERIT	 0x0080	/* region is retained after exec */
 #define	MAP_NOEXTEND	 0x0100	/* for MAP_FILE, don't change file size */
 #define	MAP_HASSEMAPHORE 0x0200	/* region may contain semaphores */
+#ifdef VM_STACK
+#define	MAP_STACK	 0x0400	/* region grows down, like a stack */
+#endif
 
 #ifdef _P1003_1B_VISIBLE
 /*
