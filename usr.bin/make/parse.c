7ba3364238645869d73880a33550134e6ebecb75
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-01-08 18:37:34 +0000

    Don't allow pointer underrun in loop.
    Spotted by:	John W. DeBoskey <jwd@unx.sas.com>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@42409 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/make/parse.c
+++ usr.bin/make/parse.c
@@ -41,7 +41,7 @@
 static char sccsid[] = "@(#)parse.c	8.3 (Berkeley) 3/19/94";
 #endif
 static const char rcsid[] =
-	"$Id: parse.c,v 1.18 1997/07/24 06:58:08 charnier Exp $";
+	"$Id: parse.c,v 1.19 1998/05/25 03:28:37 steve Exp $";
 #endif /* not lint */
 
 /*-
@@ -1412,8 +1412,10 @@ Parse_DoVar (line, ctxt)
 	default:
 #ifdef SUNSHCMD
 	    while (*opc != ':')
-		if (--opc < line)
+		if (opc == line)
 		    break;
+		else
+		    --opc;
 
 	    if (strncmp(opc, ":sh", 3) == 0) {
 		type = VAR_SHELL;
