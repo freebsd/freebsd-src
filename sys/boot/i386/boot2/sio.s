4778b8bc9a48715f8d25872c0439ca25c9fda017
rnordier <rnordier@FreeBSD.org>
1999-01-10 14:48:05 +0000

    Use etc/make.conf settings for serial port and speed.
    Submitted by: rvb
    Reviewed by: bde

--- sys/boot/i386/boot2/sio.s
+++ sys/boot/i386/boot2/sio.s
@@ -13,11 +13,11 @@
 # purpose.
 #
 
-#	$Id: sio.s,v 1.1 1998/10/15 20:04:21 rnordier Exp $
+#	$Id: sio.s,v 1.2 1998/10/20 20:20:48 rnordier Exp $
 
 		.set SIO_PRT,SIOPRT		# Base port
 		.set SIO_FMT,SIOFMT		# 8N1
-		.set SIO_DIV,SIODIV		# 115200 / BPS
+		.set SIO_DIV,(115200/SIOSPD)	# 115200 / SPD
 
 		.globl sio_init
 		.globl sio_flush
