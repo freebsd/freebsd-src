6b75c010a06508626c4e864a56c2cedf47979522
Andrzej Bialecki <abial@FreeBSD.org>
1999-01-14 23:23:19 +0000

    Use the new kget syntax.

--- release/picobsd/router/lang/update.pl
+++ release/picobsd/router/lang/update.pl
@@ -1,11 +1,11 @@
 #!/bin/sh
-# $Id: update.pl,v 1.2 1998/08/10 19:17:55 abial Exp $
+# $Id: update.pl,v 1.1.1.1 1998/08/27 17:38:44 abial Exp $
 echo -n "Uaktualniam katalog /etc na dyskietce...  "
 mount /dev/fd0a /start_floppy
 cd /etc
 cp -Rp . /start_floppy/etc/
 echo " Zrobione."
 echo -n "Uaktualniam parametry jadra..."
-kget -incore /start_floppy/kernel.config /stand/vanilla
+kget /start_floppy/kernel.config
 umount /dev/fd0a
 echo " Zrobione."
