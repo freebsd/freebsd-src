fc05999161f8975f32d2605624c708f8769d1b48
kato <kato@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-01-18 14:55:38 +0000

    Added copyright.
    Pointed out by:	Kazutaka YOKOTA <yokota@zodiac.mech.utsunomiya-u.ac.jp>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@42805 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/pc98/cbus/gdc.c
+++ sys/pc98/cbus/gdc.c
@@ -1,8 +1,31 @@
-#define TEXT_GDC	IO_GDC1	/* 0x60 */
-#define	ROW		25
-#define	COL		80
-/*-
- * $Id: pc98gdc.c,v 1.1 1999/01/18 08:38:07 kato Exp $
+/*
+ * Copyright (c) 1999 FreeBSD(98) port team.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer as
+ *    the first lines of this file unmodified.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *	$Id$
  */
 
 #include "gdc.h"
@@ -31,6 +54,10 @@
 #include <pc98/pc98/pc98_machdep.h>
 
 #include <i386/isa/isa_device.h>
+
+#define TEXT_GDC	IO_GDC1	/* 0x60 */
+#define	ROW		25
+#define	COL		80
 
 #define DRIVER_NAME	"gdc"
 
