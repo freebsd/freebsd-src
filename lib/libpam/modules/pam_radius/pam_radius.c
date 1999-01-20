8e07825413f288cbeaf9592f9e27d279123b4947
jdp <jdp@FreeBSD.org>
1999-01-20 21:55:30 +0000

    Make it possible to use PAM in statically-linked applications.

--- lib/libpam/modules/pam_radius/pam_radius.c
+++ lib/libpam/modules/pam_radius/pam_radius.c
@@ -296,3 +296,5 @@ pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
 	return PAM_SUCCESS;
 }
+
+PAM_MODULE_ENTRY("pam_radius");
--- lib/libpam/modules/pam_tacplus/pam_tacplus.c
+++ lib/libpam/modules/pam_tacplus/pam_tacplus.c
@@ -254,3 +254,5 @@ pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
 	return PAM_SUCCESS;
 }
+
+PAM_MODULE_ENTRY("pam_tacplus");
