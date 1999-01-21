2919fc336a438cff0cf3a8e412a4c4345f5566cb
Jordan K. Hubbard <jkh@FreeBSD.org>
1999-01-21 05:33:18 +0000

    Now we're -STABLE

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -32,11 +32,11 @@
 # SUCH DAMAGE.
 #
 #	@(#)newvers.sh	8.1 (Berkeley) 4/20/94
-#	$Id: newvers.sh,v 1.40 1998/10/16 06:55:36 jkh Exp $
+#	$Id: newvers.sh,v 1.41 1998/11/01 15:36:20 wosch Exp $
 
 TYPE="FreeBSD"
 REVISION="3.0"
-BRANCH="CURRENT"
+BRANCH="STABLE"
 RELEASE="${REVISION}-${BRANCH}"
 SNAPDATE=""
 if [ "X${SNAPDATE}" != "X" ]; then
