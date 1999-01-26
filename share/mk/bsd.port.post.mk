20c60bd80621ff7892b524631fc8edcd4fc16d5b
Satoshi Asami <asami@FreeBSD.org>
1999-01-26 08:14:05 +0000

    MFC: move bsd.port.*mk to ${PORTSDIR}/Mk.

--- share/mk/bsd.port.post.mk
+++ share/mk/bsd.port.post.mk
@@ -1,7 +1,7 @@
-#	$Id$
+#	$Id: bsd.port.post.mk,v 1.1.2.1 1998/11/11 05:23:20 asami Exp $
 
 AFTERPORTMK=	yes
 
-.include <bsd.port.mk>
+.include "bsd.port.mk"
 
 .undef AFTERPORTMK
--- share/mk/bsd.port.pre.mk
+++ share/mk/bsd.port.pre.mk
@@ -1,7 +1,7 @@
-#	$Id$
+#	$Id: bsd.port.pre.mk,v 1.1.2.1 1998/11/11 05:23:21 asami Exp $
 
 BEFOREPORTMK=	yes
 
-.include <bsd.port.mk>
+.include "bsd.port.mk"
 
 .undef BEFOREPORTMK
