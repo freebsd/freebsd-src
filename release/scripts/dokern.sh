ccfcf93e5340cdce5ea948fd820b29318f2e88b6
Jordan K. Hubbard <jkh@FreeBSD.org>
1999-01-26 15:14:11 +0000

    1. Adjust fs sizes to get floppies back under control.
    2. Viciously slash all CD support out of boot.flp.  It's basically just
       a net boot floppy now.

--- release/scripts/dokern.sh
+++ release/scripts/dokern.sh
@@ -13,19 +13,28 @@ fi
 
 if [ "$MFS" = "YES" ]; then
 	sed	-e '/pty/d' \
-		-e '/wfd0/d' \
+		-e '/acd0/d' \
+		-e '/cd0/d' \
 		-e '/mcd0/d' \
 		-e '/matcd0/d' \
 		-e '/scd0/d' \
+		-e '/ft0/d' \
 		-e '/wt0/d' \
+		-e '/wfd0/d' \
 		-e '/pass0/d' \
 		-e '/apm0/d' \
-		-e '/ft0/d' \
 		-e '/ppp/d' \
+		-e '/eisa0/d' \
+		-e '/aha0/d' \
+		-e '/ahb0/d' \
+		-e '/bt0/d' \
+		-e '/fd1/d' \
+		-e '/splash/d' \
 		-e '/gzip/d' \
-		-e '/isp0/d' \
 		-e '/NFS/d' \
 		-e '/PROCFS/d' \
+		-e '/MSDOSFS/d' \
+		-e '/CD9660/d' \
 		-e '/SYSVSHM/d' \
 		-e '/KTRACE/d' \
 		-e '/MATH_EMULATE/d' \
@@ -37,11 +46,11 @@ else
 		-e '/apm0/d' \
 		-e '/ppp/d' \
 		-e '/gzip/d' \
+		-e '/splash/d' \
 		-e '/PROCFS/d' \
 		-e '/KTRACE/d' \
 		-e 's/GENERIC/BOOTMFS/g'
 fi
 echo "options  NFS_NOSERVER" 
-echo 'options  "MAXCONS=4"' 
 echo "options  SCSI_NO_OP_STRINGS" 
 echo "options  SCSI_NO_SENSE_STRINGS"
