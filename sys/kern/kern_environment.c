21be37ad8075aaff45b1285c5f3c7f08667c7dc0
dillon <dillon@FreeBSD.org>
1999-01-27 21:24:50 +0000

    Fix getenv() comparison against '=' ... was *cp = '=' instead of     *cp == '='.

--- sys/kern/kern_environment.c
+++ sys/kern/kern_environment.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: kern_environment.c,v 1.3 1998/10/09 21:21:34 msmith Exp $
+ *	$Id: kern_environment.c,v 1.4 1999/01/15 17:24:56 msmith Exp $
  */
 
 /*
@@ -57,7 +57,7 @@ getenv(char *name)
 	for (ep = cp; (*ep != '=') && (*ep != 0); ep++)
 	    ;
 	len = ep - cp;
-	if (*ep = '=')
+	if (*ep == '=')
 	    ep++;
 	if (!strncmp(name, cp, len))
 	    return(ep);
