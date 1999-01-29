b3558d663de947ddfafd4209347a26a3e1b99ba2
bde <bde@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-01-29 10:57:18 +0000

    Fixed comment and whitespace lossage in recent commits.  The -W flag is not implied by -Wall as claimed by gcc.1.  Adding it causes a measly 7193 new warnings for LINT, mostly for "unused parameter" and "comparison between signed and unsigned".
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@43390 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -1,15 +1,22 @@
-#	$Id: bsd.kern.mk,v 1.13 1999/01/27 22:53:58 dillon Exp $
+#	$Id: bsd.kern.mk,v 1.14 1999/01/29 06:22:19 dillon Exp $
 
 #
 # Warning flags for compiling the kernel and components of the kernel.
 #
-#	Note that the newly added -Wcast-qual is responsible for generating 
-#	most of the remaining warnings.  Warnings introduced with -Wall
-#	will also pop up, but are easier to fix.
-
-CWARNFLAGS?=    -Wall -Wredundant-decls -Wnested-externs -Wstrict-prototypes \
-                -Wmissing-prototypes -Wpointer-arith -Winline -Wcast-qual \
+# Note that the newly added -Wcast-qual is responsible for generating 
+# most of the remaining warnings.  Warnings introduced with -Wall will
+# also pop up, but are easier to fix.
+#
+CWARNFLAGS?=	-Wall -Wredundant-decls -Wnested-externs -Wstrict-prototypes \
+		-Wmissing-prototypes -Wpointer-arith -Winline -Wcast-qual \
 		-fformat-extensions -ansi
+#
+# The following flags are next up for working on:
+#	-W
+#
+# When working on removing warnings from code, the `-Werror' flag should be
+# of material assistance.
+#
 
 #
 # On the alpha, make sure that we don't use floating-point registers and
