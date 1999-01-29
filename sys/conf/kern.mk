a91f66d4f6374976fe02167150ba00bfda5fbd37
Matthew Dillon <dillon@FreeBSD.org>
1999-01-29 06:22:19 +0000

    Cleanup bsd.kern.mk after last commit.

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -1,25 +1,15 @@
-#	$Id: bsd.kern.mk,v 1.12 1998/12/17 22:36:21 dfr Exp $
+#	$Id: bsd.kern.mk,v 1.13 1999/01/27 22:53:58 dillon Exp $
 
 #
 # Warning flags for compiling the kernel and components of the kernel.
 #
-#CWARNFLAGS?=	-Wreturn-type -Wcomment -Wredundant-decls -Wimplicit \
-#		-Wnested-externs -Wstrict-prototypes -Wmissing-prototypes \
-#		-Wpointer-arith -Winline -Wuninitialized -Wformat -Wunused \
-#		-fformat-extensions -ansi
+#	Note that the newly added -Wcast-qual is responsible for generating 
+#	most of the remaining warnings.  Warnings introduced with -Wall
+#	will also pop up, but are easier to fix.
 
 CWARNFLAGS?=    -Wall -Wredundant-decls -Wnested-externs -Wstrict-prototypes \
                 -Wmissing-prototypes -Wpointer-arith -Winline -Wcast-qual \
 		-fformat-extensions -ansi
-
-
-#
-# The following flags are next up for working on:
-#	-W	( note: -W may be included by -Wall )
-#
-# When working on removing warnings from code, the `-Werror' flag should be
-# of material assistance.
-#
 
 #
 # On the alpha, make sure that we don't use floating-point registers and
