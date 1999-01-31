ca08a426a5cdca9e0caa89c6cc124151d47742a1
paul <paul@FreeBSD.org>
1999-01-31 00:57:46 +0000

    Merge multicast support from current.

--- sys/i386/isa/if_lnc.h
+++ sys/i386/isa/if_lnc.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: $
+ * $Id: if_lnc.h,v 1.10 1999/01/31 00:56:32 paul Exp $
  */
 
 #include <i386/isa/ic/Am7990.h>
@@ -55,6 +55,9 @@
 
 #define MEM_SLEW 8
 
+/* LNC Flags */
+#define LNC_INITIALISED 1
+#define LNC_ALLMULTI 2
 
 /* BICC port addresses */
 #define BICC_IOSIZE    16
