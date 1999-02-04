a65d5aa7395b5ee548aaabd35ba417fa9e8d4de3
kato <kato@FreeBSD.org>
1999-02-04 14:01:03 +0000

    Sync with sys/i386/i386/userconfig.c revision 1.126.2.1.
    Submitted by:	Takahashi Yoshihiro <nyan@dd.catv.ne.jp>

--- sys/pc98/i386/userconfig.c
+++ sys/pc98/i386/userconfig.c
@@ -46,7 +46,7 @@
  ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **
- **      $Id: userconfig.c,v 1.64 1999/01/16 11:39:08 kato Exp $
+ **      $Id: userconfig.c,v 1.65 1999/01/18 11:01:57 kato Exp $
  **/
 
 /**
@@ -427,8 +427,13 @@ static DEV_INFO device_info[] = {
 {"ppc",         "Parallel Port chipset",		0,		CLS_COMMS},
 {"gp",          "National Instruments AT-GPIB/TNT driver",	0,	CLS_COMMS},
 
+{"atkbdc",      "Keyboard controller",			FLG_INVISIBLE,	CLS_INPUT},
+{"atkbd",       "Keyboard",				FLG_FIXED,	CLS_INPUT},
+#ifdef PC98
+{"pckbd",       "Keyboard",				FLG_FIXED,	CLS_INPUT},
+#endif
 {"mse",         "Microsoft Bus Mouse",			0,		CLS_INPUT},
-{"psm",         "PS/2 Mouse",				0,		CLS_INPUT},
+{"psm",         "PS/2 Mouse",				FLG_FIXED,	CLS_INPUT},
 {"joy",         "Joystick",				FLG_FIXED,	CLS_INPUT},
 {"vt",          "PCVT console driver",			FLG_IMMUTABLE,	CLS_INPUT},
 {"sc",          "Syscons console driver",		FLG_IMMUTABLE,	CLS_INPUT},
@@ -2531,7 +2536,7 @@ visuserconfig(void)
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *      $Id: userconfig.c,v 1.64 1999/01/16 11:39:08 kato Exp $
+ *      $Id: userconfig.c,v 1.65 1999/01/18 11:01:57 kato Exp $
  */
 
 #include "scbus.h"
