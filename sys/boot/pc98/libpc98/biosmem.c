efb240791d8785c9ac73c2588eb5f9681e4c90d9
cvs2svn <cvs2svn@FreeBSD.org>
1999-02-06 02:54:48 +0000

    This commit was manufactured by cvs2svn to create branch 'RELENG_3'.

---
+++ sys/boot/pc98/libpc98/biosmem.c
@@ -0,0 +1,59 @@
+/*
+ * mjs copyright
+ */
+
+/*
+ * Obtain memory configuration information from the BIOS
+ *
+ * Note that we don't try too hard here; knowing the size of
+ * base memory and extended memory out to 16 or 64M is enough for
+ * the requirements of the bootstrap.
+ *
+ * We also maintain a pointer to the top of physical memory
+ * once called to allow rangechecking of load/copy requests.
+ */
+#include <stand.h>
+#include "btxv86.h"
+
+vm_offset_t	memtop;
+
+/*
+ * Return base memory size in kB.
+ */
+int
+getbasemem(void)
+{
+#ifdef PC98
+    return ((*(u_char *)PTOV(0xA1501)&0x07)+1)*128;
+#else
+    v86.ctl = 0;
+    v86.addr = 0x12;		/* int 0x12 */
+    v86int();
+
+    return(v86.eax & 0xffff);
+#endif
+}
+
+/*
+ * Return extended memory size in kB
+ */
+int
+getextmem(void)
+{
+    int		extkb;
+
+#ifdef PC98
+    extkb = *(u_char *)PTOV(0xA1401)*128 + *(unsigned short *)PTOV(0xA1594)*1024;
+#else    
+    v86.ctl = 0;
+    v86.addr = 0x15;		/* int 0x15 function 0x88*/
+    v86.eax = 0x8800;
+    v86int();
+    extkb = v86.eax & 0xffff;
+#endif
+    /* Set memtop to actual top or 16M, whicheve is less */
+    memtop = min((0x100000 + (extkb * 1024)), (16 * 1024 * 1024));
+    
+    return(extkb);
+}
+
---
+++ sys/boot/pc98/loader/main.c
@@ -0,0 +1,244 @@
+/*-
+ * Copyright (c) 1998 Michael Smith <msmith@freebsd.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	$Id: main.c,v 1.14 1998/11/02 23:28:11 msmith Exp $
+ */
+
+/*
+ * MD bootstrap main() and assorted miscellaneous
+ * commands.
+ */
+
+#include <stand.h>
+#include <string.h>
+#include <machine/bootinfo.h>
+#include <sys/reboot.h>
+
+#include "bootstrap.h"
+#include "../../i386/libi386/libi386.h"
+#include "btxv86.h"
+
+/* Arguments passed in from the boot1/boot2 loader */
+static struct 
+{
+    u_int32_t	howto;
+    u_int32_t	bootdev;
+    u_int32_t	res0;
+    u_int32_t	res1;
+    u_int32_t	res2;
+    u_int32_t	bootinfo;
+} *kargs;
+
+static u_int32_t	initial_howto;
+static u_int32_t	initial_bootdev;
+#ifdef PC98
+struct bootinfo	*initial_bootinfo;
+#else
+static struct bootinfo	*initial_bootinfo;
+#endif
+
+struct arch_switch	archsw;		/* MI/MD interface boundary */
+
+static void		extract_currdev(void);
+static int		isa_inb(int port);
+static void		isa_outb(int port, int value);
+
+/* from vers.c */
+extern	char bootprog_name[], bootprog_rev[], bootprog_date[], bootprog_maker[];
+
+/* XXX debugging */
+extern char end[];
+
+void
+main(void)
+{
+    int			i;
+
+    /* Pick up arguments */
+    kargs = (void *)__args;
+    initial_howto = kargs->howto;
+    initial_bootdev = kargs->bootdev;
+    initial_bootinfo = (struct bootinfo *)PTOV(kargs->bootinfo);
+
+    /* 
+     * Initialise the heap as early as possible.  Once this is done, malloc() is usable.
+     *
+     * XXX better to locate end of memory and use that
+     */
+    setheap((void *)end, (void *)(end + (384 * 1024)));
+    
+    /* 
+     * XXX Chicken-and-egg problem; we want to have console output early, but some
+     * console attributes may depend on reading from eg. the boot device, which we
+     * can't do yet.
+     *
+     * We can use printf() etc. once this is done.
+     * If the previous boot stage has requested a serial console, prefer that.
+     */
+    if (initial_howto & RB_SERIAL)
+	setenv("console", "comconsole", 1);
+    cons_probe();
+
+    /*
+     * Initialise the block cache
+     */
+    bcache_init(32, 512);	/* 16k cache XXX tune this */
+
+    /*
+     * March through the device switch probing for things.
+     */
+    for (i = 0; devsw[i] != NULL; i++)
+	if (devsw[i]->dv_init != NULL)
+	    (devsw[i]->dv_init)();
+
+    printf("\n");
+    printf("%s, Revision %s  %d/%dkB\n", bootprog_name, bootprog_rev, getbasemem(), getextmem());
+    printf("(%s, %s)\n", bootprog_maker, bootprog_date);
+
+    extract_currdev();				/* set $currdev and $loaddev */
+    setenv("LINES", "24", 1);			/* optional */
+    
+    archsw.arch_autoload = i386_autoload;
+    archsw.arch_getdev = i386_getdev;
+    archsw.arch_copyin = i386_copyin;
+    archsw.arch_copyout = i386_copyout;
+    archsw.arch_readin = i386_readin;
+    archsw.arch_isainb = isa_inb;
+    archsw.arch_isaoutb = isa_outb;
+
+    interact();			/* doesn't return */
+}
+
+/*
+ * Set the 'current device' by (if possible) recovering the boot device as 
+ * supplied by the initial bootstrap.
+ *
+ * XXX should be extended for netbooting.
+ */
+static void
+extract_currdev(void)
+{
+    struct i386_devdesc	currdev;
+    int			major, biosdev;
+
+    /* We're booting from a BIOS disk, try to spiff this */
+    currdev.d_dev = devsw[0];				/* XXX presumes that biosdisk is first in devsw */
+    currdev.d_type = currdev.d_dev->dv_type;
+
+    if ((initial_bootdev & B_MAGICMASK) != B_DEVMAGIC) {
+	/* The passed-in boot device is bad */
+	currdev.d_kind.biosdisk.slice = -1;
+	currdev.d_kind.biosdisk.partition = 0;
+	biosdev = -1;
+    } else {
+	currdev.d_kind.biosdisk.slice = (B_ADAPTOR(initial_bootdev) << 4) + B_CONTROLLER(initial_bootdev) - 1;
+	currdev.d_kind.biosdisk.partition = B_PARTITION(initial_bootdev);
+	biosdev = initial_bootinfo->bi_bios_dev;
+	major = B_TYPE(initial_bootdev);
+
+	/*
+	 * If we are booted by an old bootstrap, we have to guess at the BIOS
+	 * unit number.  We will loose if there is more than one disk type
+	 * and we are not booting from the lowest-numbered disk type 
+	 * (ie. SCSI when IDE also exists).
+	 */
+#ifdef PC98
+	if (major == 6)
+	    biosdev = 0x30 + B_UNIT(initial_bootdev);
+	else
+	    biosdev = (major << 3) + 0x80 + B_UNIT(initial_bootdev);
+#else
+	if ((biosdev == 0) && (B_TYPE(initial_bootdev) != 2))	/* biosdev doesn't match major */
+	    biosdev = 0x80 + B_UNIT(initial_bootdev);		/* assume harddisk */
+#endif
+    }
+    
+    if ((currdev.d_kind.biosdisk.unit = bd_bios2unit(biosdev)) == -1) {
+	printf("Can't work out which disk we are booting from.\n"
+	       "Guessed BIOS device 0x%x not found by probes, defaulting to disk0:\n", biosdev);
+	currdev.d_kind.biosdisk.unit = 0;
+    }
+    env_setenv("currdev", EV_VOLATILE, i386_fmtdev(&currdev), i386_setcurrdev, env_nounset);
+    env_setenv("loaddev", EV_VOLATILE, i386_fmtdev(&currdev), env_noset, env_nounset);
+}
+
+COMMAND_SET(reboot, "reboot", "reboot the system", command_reboot);
+
+static int
+command_reboot(int argc, char *argv[])
+{
+
+    printf("Rebooting...\n");
+    delay(1000000);
+    __exit(0);
+}
+
+/* provide this for panic, as it's not in the startup code */
+void
+exit(int code)
+{
+    __exit(code);
+}
+
+COMMAND_SET(heap, "heap", "show heap usage", command_heap);
+
+static int
+command_heap(int argc, char *argv[])
+{
+    mallocstats();
+    printf("heap base at %p, top at %p\n", end, sbrk(0));
+    return(CMD_OK);
+}
+
+/* ISA bus access functions for PnP, derived from <machine/cpufunc.h> */
+static int		
+isa_inb(int port)
+{
+    u_char	data;
+    
+    if (__builtin_constant_p(port) && 
+	(((port) & 0xffff) < 0x100) && 
+	((port) < 0x10000)) {
+	__asm __volatile("inb %1,%0" : "=a" (data) : "id" ((u_short)(port)));
+    } else {
+	__asm __volatile("inb %%dx,%0" : "=a" (data) : "d" (port));
+    }
+    return(data);
+}
+
+static void
+isa_outb(int port, int value)
+{
+    u_char	al = value;
+    
+    if (__builtin_constant_p(port) && 
+	(((port) & 0xffff) < 0x100) && 
+	((port) < 0x10000)) {
+	__asm __volatile("outb %0,%1" : : "a" (al), "id" ((u_short)(port)));
+    } else {
+        __asm __volatile("outb %0,%%dx" : : "a" (al), "d" (port));
+    }
+}
+
