0989f69ef6fed09c47763328cc3fafd4447b0368
Satoshi Asami <asami@FreeBSD.org>
1999-02-08 22:57:48 +0000

    We are now in the beta cycle for 3.1.
    Approved by:	jkh

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -32,11 +32,11 @@
 # SUCH DAMAGE.
 #
 #	@(#)newvers.sh	8.1 (Berkeley) 4/20/94
-#	$Id: newvers.sh,v 1.41 1998/11/01 15:36:20 wosch Exp $
+#	$Id: newvers.sh,v 1.41.2.1 1999/01/21 05:33:18 jkh Exp $
 
 TYPE="FreeBSD"
-REVISION="3.0"
-BRANCH="STABLE"
+REVISION="3.1"
+BRANCH="BETA"
 RELEASE="${REVISION}-${BRANCH}"
 SNAPDATE=""
 if [ "X${SNAPDATE}" != "X" ]; then
