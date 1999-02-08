09f3411c47494c76fe44f28a7a6084de468b9728
dt <dt@FreeBSD.org>
1999-02-08 21:32:38 +0000

    Added functions fseeko() and ftello() (from susv2). Fixed fgetpos() and fsetpos() for offsets > 2GB.
    PR:		8637
    Submitted by:	 Dan Nelson <dnelson@emsphone.com> (adjusted by me a little)

--- lib/libc/stdio/fgetpos.c
+++ lib/libc/stdio/fgetpos.c
@@ -39,7 +39,7 @@
 static char sccsid[] = "@(#)fgetpos.c	8.1 (Berkeley) 6/4/93";
 #endif
 static const char rcsid[] =
-		"$Id: fgetpos.c,v 1.5 1997/02/22 15:01:53 peter Exp $";
+		"$Id: fgetpos.c,v 1.6 1998/04/11 07:40:42 jb Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdio.h>
@@ -52,7 +52,7 @@ fgetpos(fp, pos)
 {
 	int retval;
 	FLOCKFILE(fp);
-	retval = (*pos = ftell(fp)) == (fpos_t)-1;
+	retval = (*pos = ftello(fp)) == (fpos_t)-1;
 	FUNLOCKFILE(fp);
 	return(retval);
 }
