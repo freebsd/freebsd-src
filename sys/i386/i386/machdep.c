a86cd76be1fc1d97602040fac97e14adfd32df73
Mike Smith <msmith@FreeBSD.org>
1999-02-11 19:37:44 +0000

    MFS - fix %edx on entry to static Linux binaries.

--- sys/i386/i386/machdep.c
+++ sys/i386/i386/machdep.c
@@ -35,7 +35,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91
- *	$Id: machdep.c,v 1.322 1999/01/15 17:24:05 msmith Exp $
+ *	$Id: machdep.c,v 1.322.2.1 1999/02/03 14:12:01 dg Exp $
  */
 
 #include "apm.h"
@@ -860,6 +860,13 @@ setregs(p, entry, stack)
 	/* Initialize the npx (if any) for the current process. */
 	npxinit(__INITIAL_NPXCW__);
 #endif
+
+	/*
+	 * XXX - Linux emulator
+	 * Make sure sure edx is 0x0 on entry. Linux binaries depend
+	 * on it.
+	 */
+	p->p_retval[1] = 0;
 }
 
 static int
