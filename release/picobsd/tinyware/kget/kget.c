e31fe953ac6fa49a5cbfbfaa20477525ab3f80fe
abial <abial@FreeBSD.org>
1999-02-12 20:36:13 +0000

    Merge from RELENG_3.

--- release/picobsd/tinyware/kget/kget.c
+++ release/picobsd/tinyware/kget/kget.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: kget.c,v 1.1 1999/01/08 21:09:42 abial Exp $
+ * $Id: kget.c,v 1.2.2.1 1999/02/12 20:14:47 abial Exp $
  */
 
 #include <stdio.h>
@@ -31,8 +31,10 @@
 #include <sys/types.h>
 #include <sys/sysctl.h>
 #include "i386/isa/isa_device.h"
+#include "i386/isa/pnp.h"
 
 struct isa_device *id;
+struct pnp_cinfo *c;
 char *p;
 
 int
@@ -40,7 +42,8 @@ main(int argc, char *argv[])
 {
 	int len,i;
 	char *buf;
-	char *mib="machdep.uc_devlist";
+	char *mib1="machdep.uc_devlist";
+	char *mib2="machdep.uc_pnplist";
 	char name[9];
 	FILE *fout;
 
@@ -60,14 +63,15 @@ main(int argc, char *argv[])
 
 	/* We use sysctlbyname, because the oid is unknown (OID_AUTO) */
 
+	/* First, print the changes made to ISA devices */
 	/* get the buffer size */
-	i=sysctlbyname(mib,NULL,&len,NULL,NULL);
+	i=sysctlbyname(mib1,NULL,&len,NULL,NULL);
 	if(i) {
 		perror("buffer sizing");
 		exit(-1);
 	}
 	buf=(char *)malloc(len*sizeof(char));
-	i=sysctlbyname(mib,buf,&len,NULL,NULL);
+	i=sysctlbyname(mib1,buf,&len,NULL,NULL);
 	if(i) {
 		perror("retrieving data");
 		exit(-1);
@@ -106,6 +110,53 @@ main(int argc, char *argv[])
 		}
 		i+=sizeof(struct isa_device)+8;
 	}
+	free(buf);
+	/* Now, print the changes to PnP override table */
+	/* get the buffer size */
+	i=sysctlbyname(mib2,NULL,&len,NULL,NULL);
+	if(i) {
+		perror("buffer sizing");
+		exit(-1);
+	}
+	buf=(char *)malloc(len*sizeof(char));
+	i=sysctlbyname(mib2,buf,&len,NULL,NULL);
+	if(i) {
+		perror("retrieving data");
+		exit(-1);
+	}
+	i=0;
+	/* Print the PnP override table. Taken from userconfig.c */
+	do {
+		c = (struct pnp_cinfo *)(buf+i);
+		if (c->csn >0 && c->csn != 255) {
+	    		int pmax, mmax;
+	    		char buf1[256];
+
+			if(c->enable==0) {
+				fprintf(fout,"pnp %d %d disable\n",
+					c->csn, c->ldn);
+				continue;
+			}
+			fprintf(fout,"pnp %d %d %s irq0 %d irq1 %d drq0 %d drq1 %d",
+				c->csn, c->ldn,
+				c->override ? "os":"bios",
+				c->irq[0], c->irq[1], c->drq[0], c->drq[1]);
+	    		if (c->flags)
+				fprintf(fout," flags 0x%lx",c->flags);
+			pmax=0;
+			while(c->port[pmax]!=0 && pmax<8) {
+				fprintf(fout," port%d %d",pmax,c->port[pmax]);
+				pmax++;
+			}
+			mmax=0;
+			while(c->mem[mmax].base!=0 && mmax<8) {
+				fprintf(fout," mem%d %d",mmax,c->mem[mmax].base);
+				mmax++;
+			}
+	    		fprintf(fout,"\n");
+    		}
+
+	} while ((i+=sizeof(struct pnp_cinfo))<len);
 	fprintf(fout,"q\n");
 	fclose(fout);
 	free(buf);
