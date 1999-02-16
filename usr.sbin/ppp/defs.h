5e1d9ef90f486c31e1a00e50fc44fe6db91e8c9a
brian <brian@FreeBSD.org>
1999-02-16 00:16:56 +0000

    Wait by default for one second after the login script is complete before checking carrier.  If it's there, the device supports carrier.  If it's not it doesn't.
    Add the ``set cd'' command for deciding how soon to check
    for carrier, and for deciding if carrier is REQUIRED.
    
    The default has changed:  Pre 2.0 versions of ppp waited
    for 1 second.  Version 2 didn't wait, but this causes
    problems with some (few?) modems that don't assert carrier
    immediately on reporting CONNECT.  The one second delay
    is back now and can be removed with ``set cd 0''.
    
    Bump the ppp version number in case this needs to be changed
    again....

--- usr.sbin/ppp/defs.h
+++ usr.sbin/ppp/defs.h
@@ -15,7 +15,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: defs.h,v 1.37 1999/01/28 01:56:31 brian Exp $
+ * $Id: defs.h,v 1.38 1999/02/11 10:14:08 brian Exp $
  *
  *	TODO:
  */
@@ -57,6 +57,7 @@
 #define MIN_FSMRETRY 3		/* Minimum FSM retry frequency */
 #define DEF_FSMRETRY 3		/* FSM retry frequency */
 #define DEF_REQs     5		/* This number of REQs in IRC */
+#define DEF_CDDELAY  1		/* Delay before checking for carrier */
 
 #define	CONFFILE 	"ppp.conf"
 #define	LINKUPFILE 	"ppp.linkup"
