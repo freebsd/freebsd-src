0f920ee28c616577522b4217777fd87541e3b466
mjacob <mjacob@FreeBSD.org>
1999-02-16 20:34:56 +0000

    add now required queue.h include

--- sys/alpha/alpha/clock.c
+++ sys/alpha/alpha/clock.c
@@ -1,4 +1,4 @@
-/* $Id: clock.c,v 1.4 1998/10/06 08:40:18 dfr Exp $ */
+/* $Id: clock.c,v 1.5 1998/11/08 19:26:23 phk Exp $ */
 /* $NetBSD: clock.c,v 1.20 1998/01/31 10:32:47 ross Exp $ */
 
 /*
@@ -47,6 +47,7 @@
 
 #include <sys/param.h>
 #include <sys/kernel.h>
+#include <sys/queue.h>
 #include <sys/sysctl.h>
 #include <sys/systm.h>
 #include <sys/bus.h>
