4f8ac28b96e457253fdce1be66db6b115ccb0fa4
jdp <jdp@FreeBSD.org>
1999-02-20 23:54:00 +0000

    Increment __FreeBSD_version to reflect the fact that the kernel can now handle dynamic linkers which are statically linked executables.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -36,7 +36,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)param.h	8.3 (Berkeley) 4/4/95
- * $Id: param.h,v 1.39 1999/01/21 08:29:08 dillon Exp $
+ * $Id: param.h,v 1.40 1999/01/22 08:39:58 eivind Exp $
  */
 
 #ifndef _SYS_PARAM_H_
@@ -46,7 +46,7 @@
 #define BSD4_3	1
 #define BSD4_4	1
 #undef __FreeBSD_version
-#define __FreeBSD_version 400000	/* Master, propagated to newvers */
+#define __FreeBSD_version 400001	/* Master, propagated to newvers */
 
 #ifndef NULL
 #define	NULL	0
