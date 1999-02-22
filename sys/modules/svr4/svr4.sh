70a1fd22d85ea119bf1dad6319c15bea8af02157
newton <newton@FreeBSD.org>
1999-02-22 11:44:46 +0000

    Converted "streams" pseudo-device into a KLD

--- sys/modules/svr4/svr4.sh
+++ sys/modules/svr4/svr4.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-# $Id$
+# $Id: svr4.sh,v 1.1 1999/01/30 06:29:48 newton Exp $
 
-FOUND=`kldstat -v | egrep 'svr4elf'`
+STREAMS=`kldstat -v | egrep 'streams'`
+SVR4=`kldstat -v | egrep 'svr4elf'`
 
-if [ "x$FOUND" != x ] ; then           
+if [ "x$SVR4" != x ] ; then           
 	echo SysVR4 driver already loaded
 	exit 1
 else    
+	if [ "x$STREAMS" = x ] ; then
+		kldload streams
+		echo "Loaded SysVR4 STREAMS driver"
+	fi
 	kldload svr4
+	echo "Loaded SysVR4 emulator"
 fi
