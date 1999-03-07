0c510fa17bf9d2bd0c232ab38dba9e98094a76c0
brian <brian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-03-07 01:02:39 +0000

    Reopen descriptor 0 as /dev/tty when in interactive mode in OpenBSD as well as FreeBSD (I still don't know why). Add a debug diagnostic when creating a child process as a link.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@44539 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/ppp/main.c
+++ usr.sbin/ppp/main.c
@@ -17,7 +17,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: main.c,v 1.149 1999/02/02 09:35:29 brian Exp $
+ * $Id: main.c,v 1.150 1999/02/06 02:54:46 brian Exp $
  *
  *	TODO:
  */
@@ -276,11 +276,10 @@ main(int argc, char **argv)
 #endif
   label = ProcessArgs(argc, argv, &mode, &alias);
 
-#ifdef __FreeBSD__
   /*
-   * A FreeBSD hack to dodge a bug in the tty driver that drops output
-   * occasionally.... I must find the real reason some time.  To display
-   * the dodgy behaviour, comment out this bit, make yourself a large
+   * A FreeBSD & OpenBSD hack to dodge a bug in the tty driver that drops
+   * output occasionally.... I must find the real reason some time.  To
+   * display the dodgy behaviour, comment out this bit, make yourself a large
    * routing table and then run ppp in interactive mode.  The `show route'
    * command will drop chunks of data !!!
    */
@@ -291,7 +290,6 @@ main(int argc, char **argv)
       return 2;
     }
   }
-#endif
 
   /* Allow output for the moment (except in direct mode) */
   if (mode == PHYS_DIRECT)
