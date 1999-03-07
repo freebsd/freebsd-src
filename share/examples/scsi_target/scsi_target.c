55d66c27964317c7f5bcb2a1299e54c31b4a5dea
Justin T. Gibbs <gibbs@FreeBSD.org>
1999-03-07 00:44:17 +0000

    Correction for last log message. MFC: Dynamically allocate target instances at run time.

--- share/examples/scsi_target/scsi_target.c
+++ share/examples/scsi_target/scsi_target.c
@@ -26,7 +26,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *      $Id: scsi_target.c,v 1.2 1998/12/10 04:00:03 gibbs Exp $
+ *      $Id: scsi_target.c,v 1.2.2.1 1999/03/07 00:38:56 gibbs Exp $
  */
 
 #include <sys/types.h>
