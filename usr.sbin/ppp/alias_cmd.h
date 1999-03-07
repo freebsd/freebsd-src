02c03fa6c3db938ad460eaafcf22f78b4c6d6f21
brian <brian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-03-07 18:13:44 +0000

    Support PPTP via libalias (``alias pptp addr'').
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@44557 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/ppp/alias_cmd.h
+++ usr.sbin/ppp/alias_cmd.h
@@ -2,7 +2,7 @@
  * The code in this file was written by Eivind Eklund <perhaps@yes.no>,
  * who places it in the public domain without restriction.
  *
- *	$Id: alias_cmd.h,v 1.8 1998/05/21 21:43:45 brian Exp $
+ *	$Id: alias_cmd.h,v 1.9 1999/03/07 15:02:37 brian Exp $
  */
 
 struct cmdargs;
@@ -10,3 +10,4 @@ struct cmdargs;
 extern int alias_RedirectPort(struct cmdargs const *);
 extern int alias_RedirectAddr(struct cmdargs const *);
 extern int alias_ProxyRule(struct cmdargs const *);
+extern int alias_Pptp(struct cmdargs const *);
