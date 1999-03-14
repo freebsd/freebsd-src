7f9e2073285c5150415cf1d03067355e2ec69409
markm <markm@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-03-14 20:41:14 +0000

    Bitrot. Remove objlink as it is not pleasant to be downwind.
    PR:		8071
    Reviewed by:	Sheldon Hearn <sheldonh@iafrica.com>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@44755 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/bsd.subdir.mk
+++ share/mk/bsd.subdir.mk
@@ -1,5 +1,5 @@
 #	from: @(#)bsd.subdir.mk	5.9 (Berkeley) 2/1/91
-#	$Id: bsd.subdir.mk,v 1.24 1998/05/06 16:53:53 wosch Exp $
+#	$Id: bsd.subdir.mk,v 1.25 1998/08/08 07:02:08 peter Exp $
 #
 # The include file <bsd.subdir.mk> contains the default targets
 # for building subdirectories. 
@@ -33,7 +33,7 @@
 #
 #	afterdistribute, afterinstall, all, beforeinstall, checkdpadd,
 #	clean, cleandepend, cleandir, depend, install, lint, maninstall,
-#	obj, objlink, realinstall, regress, tags
+#	obj, realinstall, regress, tags
 #
 
 .if !target(__initialized__)
@@ -88,7 +88,7 @@ ${SUBDIR}::
 
 
 .for __target in all checkdpadd clean cleandepend cleandir depend lint \
-		 maninstall obj objlink regress tags
+		 maninstall obj regress tags
 .if !target(${__target})
 ${__target}: _SUBDIRUSE
 .endif
