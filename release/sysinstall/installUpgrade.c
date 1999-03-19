6b9667f4af11337bd39657fab72b3857e97387fd
Jordan K. Hubbard <jkh@FreeBSD.org>
1999-03-19 08:22:49 +0000

    MFC: preserve rc.firewall

--- release/sysinstall/installUpgrade.c
+++ release/sysinstall/installUpgrade.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: installUpgrade.c,v 1.61.2.2 1999/02/13 05:18:24 jkh Exp $
+ * $Id: installUpgrade.c,v 1.61.2.3 1999/02/15 02:52:09 jkh Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -104,6 +104,7 @@ static HitList etc_files [] = {
    { JUST_COPY,		"profile",		TRUE, NULL },
    { JUST_COPY,		"pwd.db",		TRUE, NULL },
    { JUST_COPY,		"rc.local",		TRUE, NULL },
+   { JUST_COPY,		"rc.firewall",		TRUE, NULL },
    { JUST_COPY,		"rc.conf.local",	TRUE, NULL },
    { JUST_COPY,		"remote",		TRUE, NULL },
    { JUST_COPY,		"resolv.conf",		TRUE, NULL },
