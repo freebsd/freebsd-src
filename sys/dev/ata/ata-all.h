55bfaed1c0175f8d18c843b31171ac69354a20ae
Søren Schmidt <sos@FreeBSD.org>
1999-03-28 18:57:20 +0000

    Fourth update to the new ATA/ATAPI driver:
    Well, better late than newer, but things has been hectic
    around here, sorry for the long delay.
    
    DMA support has been added to the ATA disk driver.
    This only works on Intel PIIX3/4, Acer Aladdin and Promise controllers.
    The promise support works without the BIOS on the board,
    and timing modes are set to support up to UDMA speed. This
    solves the problems with having more than one promise controller
    in the same system.
    There is support for "generic" DMA, that might work on other
    controllers, but now you have been warned :)
    More chipset specific code will come soon, I have to find testers
    with the approbiate HW, more on that when I have it ready.
    
    The system now uses its own major numbers, please run MAKEDEV
    with the devices you need (ad?, acd?, afd?, ast?).
    For now the disk driver will also attach to the old wd major
    so one can at least boot without this step, but be warned, this
    will eventually go away. The bootblocks will have to be changed
    before one can boot directly from an "ad" device though.
    
    Fixed problems:
    
        All known hang problems should be solved
    	The probe code has been sligthly changed, this should solve
    	the reports I have lying around (I hope).
    
        Hangs when accessing ata & atapi device on the same channel simultaniously.
    	A real braino in ata_start caused this, fixed.
    
    As usual USE AT YOUR OWN RISK!!, this is still pre alpha level code.
    Especially the DMA support can hose your disk real bad if anything
    goes wrong, agaiin you have been warned :)
    
    But please tell me how it works for you!
    
    Enjoy!
    
    -Søren

--- sys/dev/ata/ata-all.h
+++ sys/dev/ata/ata-all.h
@@ -25,22 +25,28 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *	$Id: ata-all.h,v 1.3 1999/03/05 09:43:30 sos Exp $
+ *	$Id: ata-all.h,v 1.4 1999/03/07 21:49:14 sos Exp $
  */
 
 /* ATA register defines */
 #define ATA_DATA			0x00	/* data register */
 #define	ATA_ERROR			0x01	/* (R) error register */
-#define ATA_PRECOMP			0x01	/* (W) precompensation */
-#define ATA_COUNT			0x02	/* sector count */
+#define ATA_FEATURE			0x01	/* (W) feature register */
+#define		ATA_F_DMA		0x01	/* enable DMA */
+#define		ATA_F_OVL		0x02	/* enable overlap */
+
+#define ATA_COUNT			0x02	/* (W) sector count */
+#define ATA_IREASON			0x02	/* (R) interrupt reason */
 #define		ATA_I_CMD		0x01	/* cmd (1) | data (0) */
 #define		ATA_I_IN		0x02	/* read (1) | write (0) */
 #define		ATA_I_RELEASE		0x04	/* released bus (1) */
+#define		ATA_I_TAGMASK		0xf8	/* tag mask */
 
 #define	ATA_SECTOR			0x03	/* sector # */
 #define	ATA_CYL_LSB			0x04	/* cylinder# LSB */
 #define	ATA_CYL_MSB			0x05	/* cylinder# MSB */
 #define ATA_DRIVE			0x06	/* Sector/Drive/Head register */
+#define		ATA_D_LBA		0x40	/* use LBA adressing */
 #define		ATA_D_IBM		0xa0	/* 512 byte sectors, ECC */
 
 #define ATA_CMD				0x07	/* command register */
@@ -51,15 +57,21 @@
 #define		ATA_C_READ_MULTI	0xc4	/* read multi command */
 #define		ATA_C_WRITE_MULTI	0xc5	/* write multi command */
 #define		ATA_C_SET_MULTI		0xc6	/* set multi size command */
-#define		ATA_C_PACKET_CMD	0xa0	/* set multi size command */
+#define		ATA_C_READ_DMA		0xc8	/* read w/DMA command */
+#define		ATA_C_WRITE_DMA		0xca	/* write w/DMA command */
+#define		ATA_C_PACKET_CMD	0xa0	/* packet command */
+#define		ATA_C_SETFEATURES	0xef	/* features command */
+#define		    ATA_C_FEA_SETXFER	0x03	/* set transfer mode */
 
 #define ATA_STATUS			0x07	/* status register */
 #define		ATA_S_ERROR		0x01	/* error */
 #define		ATA_S_INDEX		0x02	/* index */
 #define		ATA_S_CORR		0x04	/* data corrected */
 #define		ATA_S_DRQ		0x08	/* data request */
-#define		ATA_S_DSC		0x10	/* drive Seek Completed */
+#define		ATA_S_DSC		0x10	/* drive seek completed */
+#define		ATA_S_SERV		0x10	/* drive needs service */
 #define		ATA_S_DWF		0x20	/* drive write fault */
+#define		ATA_S_DMRD		0x20	/* DMA ready */
 #define		ATA_S_DRDY		0x40	/* drive ready */
 #define		ATA_S_BSY		0x80	/* busy */
 
@@ -68,28 +80,57 @@
 #define		ATA_A_RESET		0x04	/* RESET controller */
 #define 	ATA_A_4BIT		0x08	/* 4 head bits */
 
-/* Misc defines */
+/* misc defines */
 #define	ATA_MASTER			0x00
 #define	ATA_SLAVE			0x10
 #define	ATA_IOSIZE			0x08
 #define ATA_OP_FINISHED			0x00
 #define ATA_OP_CONTINUES		0x01
 
-/* Devices types */
+/* devices types */
 #define ATA_ATA_MASTER			0x01
 #define ATA_ATA_SLAVE			0x02
 #define ATA_ATAPI_MASTER		0x04
 #define ATA_ATAPI_SLAVE			0x08
 
-/* Structure describing an ATA device */
-struct ata_softc {
-    u_int32_t			unit;		/* this instance's number */
-    u_int32_t			ioaddr;		/* port addr */
-    u_int32_t			altioaddr;	/* alternate port addr */
-    void    			*dmacookie;	/* handle for DMA services */
-    int32_t			flags;		/* controller flags */
-#define		ATA_F_SLAVE_ONLY	0x0001
+/* busmaster DMA related defines */
+#define ATA_BM_OFFSET1			0x08
+#define ATA_DMA_ENTRIES			256
+#define ATA_DMA_EOT			0x80000000
 
+#define ATA_BMCMD_PORT			0x00
+#define ATA_BMCMD_START_STOP		0x01
+#define ATA_BMCMD_WRITE_READ		0x08
+
+#define ATA_BMSTAT_PORT			0x02
+#define ATA_BMSTAT_MASK			0x07
+#define ATA_BMSTAT_ACTIVE		0x01
+#define ATA_BMSTAT_ERROR		0x02
+#define ATA_BMSTAT_INTERRUPT		0x04
+#define ATA_BMSTAT_DMA_MASTER		0x20
+#define ATA_BMSTAT_DMA_SLAVE		0x40
+
+#define ATA_BMDTP_PORT			0x04
+
+#define ATA_WDMA2			0x22
+#define ATA_UDMA2			0x42
+
+/* structure for holding DMA address data */
+struct ata_dmaentry {
+        u_int32_t base;
+        u_int32_t count;
+};  
+
+/* structure describing an ATA device */
+struct ata_softc {
+    int32_t			unit;		/* unit on this controller */
+    int32_t			lun;		/* logical unit # */
+    pcici_t			tag;		/* PCI tag for this device */
+    int32_t			ioaddr;		/* port addr */
+    int32_t			altioaddr;	/* alternate port addr */
+    int32_t			bmaddr;		/* bus master DMA port */
+    struct ata_dmaentry		*dmatab[2];	/* DMA transfer tables */
+    int32_t			flags;		/* controller flags */
     int32_t			devices;	/* what is present */
     u_int8_t			status;		/* last controller status */
     u_int8_t			error;		/* last controller error */
@@ -111,8 +152,13 @@ extern struct ata_softc *atadevices[];
  
 /* public prototypes */
 void ata_start(struct ata_softc *);
-int32_t ata_wait(struct ata_softc *, u_int8_t);
-int32_t ata_command(struct ata_softc *, int32_t, u_int32_t, u_int32_t, u_int32_t, u_int32_t, u_int32_t, int32_t);
+int32_t ata_wait(struct ata_softc *, int32_t, u_int8_t);
+int32_t ata_command(struct ata_softc *, int32_t, u_int32_t, u_int32_t, u_int32_t, u_int32_t, u_int32_t, u_int32_t, int32_t);
+int32_t ata_dmainit(struct ata_softc *, int32_t, int32_t, int32_t, int32_t);
+int32_t ata_dmasetup(struct ata_softc *, int32_t, int8_t *, int32_t, int32_t);
+void ata_dmastart(struct ata_softc *, int32_t);
+int32_t ata_dmastatus(struct ata_softc *, int32_t);
+int32_t ata_dmadone(struct ata_softc *, int32_t);
 void bswap(int8_t *, int32_t);
 void btrim(int8_t *, int32_t);
 void bpack(int8_t *, int8_t *, int32_t);
