4e7d6076460c4592ae43a2ab6734643b0bae33cc
Poul-Henning Kamp <phk@FreeBSD.org>
1999-03-30 11:49:29 +0000

    MFC: remove unneeded prototypes.

--- sys/sys/timepps.h
+++ sys/sys/timepps.h
@@ -6,7 +6,7 @@
  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
  * ----------------------------------------------------------------------------
  *
- * $Id: timepps.h,v 1.4 1998/06/22 21:09:10 phk Exp $
+ * $Id: timepps.h,v 1.4.2.1 1999/03/22 13:09:36 phk Exp $
  *
  * The is a FreeBSD protype version of the "draft-mogul-pps-api-02.txt" 
  * specification for Pulse Per Second timing interfaces.  
@@ -106,15 +106,6 @@ int pps_ioctl __P((u_long cmd, caddr_t data, struct pps_state *pps));
 void hardpps __P((struct timespec *tsp, long nsec));
 
 #else /* !KERNEL */
-
-int time_pps_create(int filedes, pps_handle_t *handle);
-int time_pps_destroy(pps_handle_t handle);
-int time_pps_setparams(pps_handle_t handle, const pps_params_t *ppsparams);
-int time_pps_getparams(pps_handle_t handle, pps_params_t *ppsparams);
-int time_pps_getcap(pps_handle_t handle, int *mode);
-int time_pps_fetch(pps_handle_t handle, pps_info_t *ppsinfobuf);
-int time_pps_wait(pps_handle_t handle, const struct timespec *timeout, 
-	pps_info_t *ppsinfobuf);
 
 static __inline int
 time_pps_create(int filedes, pps_handle_t *handle)
