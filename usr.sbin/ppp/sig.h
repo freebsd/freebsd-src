486105bcb0aa143d1377f1e1c28abe26a1aede6c
Brian Somers <brian@FreeBSD.org>
1999-03-30 00:44:57 +0000

    Maintain a `necessary' marker to indicate that we *probably* need to process a signal (usually a SIGALRM).  Check to see if we need to process a signal both before *and* after calling select() as older (pre-2.0) versions of ppp used to.
    This handles the possibility that ppp may block at some
    point (maybe due to an open() of a misconfigured device).
    Previously, we'd potentially lock up in select().
    
    The `necessary' marker reduces the increased signal checking
    overhead so that at full speed with no compression transferring
    an 83Mb file via a ``!ppp -direct'' device, we get a 1%
    throughput gain.

--- usr.sbin/ppp/sig.h
+++ usr.sbin/ppp/sig.h
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: sig.h,v 1.11.2.1 1998/05/01 19:25:57 brian Exp $
+ *	$Id: sig.h,v 1.12 1998/05/21 21:48:23 brian Exp $
  */
 
 typedef void (*sig_type)(int);
@@ -32,4 +32,4 @@ typedef void (*sig_type)(int);
 extern sig_type sig_signal(int, sig_type);
 
 /* Call this when you want things to *actually* happen */
-extern void sig_Handle(void);
+extern int sig_Handle(void);
