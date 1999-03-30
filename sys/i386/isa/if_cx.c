61ffcdf16891e85d054bddbfbdce417a005ff0ab
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-03-30 13:28:23 +0000

    Organize the various modes (CISCO/AUTO/DEMAND/LEASED) a little bit better, centralize the code.
    Remember to call TLF/TLS on the hardware in CISCO mode.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@45151 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/isa/if_cx.c
+++ sys/i386/isa/if_cx.c
@@ -54,9 +54,6 @@ extern struct cdevsw cx_cdevsw;
 #include <machine/cronyx.h>
 #include <i386/isa/cxreg.h>
 
-/* XXX exported. */
-void cxswitch (cx_chan_t *c, cx_soft_opt_t new);
-
 static int cxprobe __P((struct isa_device *id));
 static int cxattach __P((struct isa_device *id));
 static void cxput __P((cx_chan_t *c, char b));
@@ -821,20 +818,3 @@ cxinput (cx_chan_t *c, void *buf, unsigned len)
 	sppp_input (c->master, m);
 }
 
-void cxswitch (cx_chan_t *c, cx_soft_opt_t new)
-{
-	new.ext = 0;
-	if (! new.ext) {
-		struct sppp *sp = (struct sppp*) c->ifp;
-
-		if (new.cisco)
-			sp->pp_flags |= PP_CISCO;
-		else
-			sp->pp_flags &= ~PP_CISCO;
-		if (new.keepalive)
-			sp->pp_flags |= PP_KEEPALIVE;
-		else
-			sp->pp_flags &= ~PP_KEEPALIVE;
-	}
-	c->sopt = new;
-}
