69704f1466bdc629204ea6012f4fa03804d8e77b
obrien <obrien@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-04-04 16:36:35 +0000

    Minimum set of changes to switch from Gcc 2.7.2 (in contrib/gcc) to Egcs 1.1.2 (in contrib/egcs)
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@45299 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ gnu/usr.bin/cc/cc_tools/auto-host.h
@@ -0,0 +1,204 @@
+/* auto-host.h.  Generated automatically by configure.  */
+
+/* config.in.  Generated automatically from configure.in by autoheader.  */
+/* Define if printf supports "%p".  */
+#define HAVE_PRINTF_PTR 1
+
+/* Define if you want expensive run-time checks. */
+/* #undef ENABLE_CHECKING */
+
+/* Define if your cpp understands the stringify operator.  */
+#define HAVE_CPP_STRINGIFY 1
+
+/* Define if your compiler understands volatile.  */
+#define HAVE_VOLATILE 1
+
+/* Define if your assembler supports specifying the maximum number
+   of bytes to skip when using the GAS .p2align command. */
+/* #undef HAVE_GAS_MAX_SKIP_P2ALIGN */
+
+/* Define if your assembler supports .balign and .p2align.  */
+/* #undef HAVE_GAS_BALIGN_AND_P2ALIGN */
+
+/* Define if you have a working <inttypes.h> header file.  */
+/* #undef HAVE_INTTYPES_H */
+
+/* Whether malloc must be declared even if <stdlib.h> is included.  */
+/* #undef NEED_DECLARATION_MALLOC */
+
+/* Whether realloc must be declared even if <stdlib.h> is included.  */
+/* #undef NEED_DECLARATION_REALLOC */
+
+/* Whether calloc must be declared even if <stdlib.h> is included.  */
+/* #undef NEED_DECLARATION_CALLOC */
+
+/* Whether free must be declared even if <stdlib.h> is included.  */
+/* #undef NEED_DECLARATION_FREE */
+
+/* Whether bcopy must be declared even if <string.h> is included.  */
+/* #undef NEED_DECLARATION_BCOPY */
+
+/* Whether bcmp must be declared even if <string.h> is included.  */
+/* #undef NEED_DECLARATION_BCMP */
+
+/* Whether bzero must be declared even if <string.h> is included.  */
+/* #undef NEED_DECLARATION_BZERO */
+
+/* Whether index must be declared even if <string.h> is included.  */
+/* #undef NEED_DECLARATION_INDEX */
+
+/* Whether rindex must be declared even if <string.h> is included.  */
+/* #undef NEED_DECLARATION_RINDEX */
+
+/* Whether getenv must be declared even if <stdlib.h> is included.  */
+/* #undef NEED_DECLARATION_GETENV */
+
+/* Whether atol must be declared even if <stdlib.h> is included.  */
+/* #undef NEED_DECLARATION_ATOL */
+
+/* Whether sbrk must be declared even if <stdlib.h> is included.  */
+/* #undef NEED_DECLARATION_SBRK */
+
+/* Whether abort must be declared even if <stdlib.h> is included.  */
+/* #undef NEED_DECLARATION_ABORT */
+
+/* Whether strerror must be declared even if <string.h> is included.  */
+/* #undef NEED_DECLARATION_STRERROR */
+
+/* Whether getcwd must be declared even if <unistd.h> is included.  */
+/* #undef NEED_DECLARATION_GETCWD */
+
+/* Whether getwd must be declared even if <unistd.h> is included.  */
+/* #undef NEED_DECLARATION_GETWD */
+
+/* Whether getrlimit must be declared even if <sys/resource.h> is included.  */
+#define NEED_DECLARATION_GETRLIMIT 1
+
+/* Whether setrlimit must be declared even if <sys/resource.h> is included.  */
+#define NEED_DECLARATION_SETRLIMIT 1
+
+/* Define if you want expensive run-time checks. */
+/* #undef ENABLE_CHECKING */
+
+/* Define if you don't have vprintf but do have _doprnt.  */
+/* #undef HAVE_DOPRNT */
+
+/* Define if you have the vprintf function.  */
+#define HAVE_VPRINTF 1
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if `sys_siglist' is declared by <signal.h>.  */
+#define SYS_SIGLIST_DECLARED 1
+
+/* Define if you can safely include both <sys/time.h> and <time.h>.  */
+#define TIME_WITH_SYS_TIME 1
+
+/* Define if you have the atoll function.  */
+/* #undef HAVE_ATOLL */
+
+/* Define if you have the atoq function.  */
+/* #undef HAVE_ATOQ */
+
+/* Define if you have the bcmp function.  */
+#define HAVE_BCMP 1
+
+/* Define if you have the bcopy function.  */
+#define HAVE_BCOPY 1
+
+/* Define if you have the bsearch function.  */
+#define HAVE_BSEARCH 1
+
+/* Define if you have the bzero function.  */
+#define HAVE_BZERO 1
+
+/* Define if you have the getrlimit function.  */
+#define HAVE_GETRLIMIT 1
+
+/* Define if you have the gettimeofday function.  */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Define if you have the index function.  */
+#define HAVE_INDEX 1
+
+/* Define if you have the isascii function.  */
+#define HAVE_ISASCII 1
+
+/* Define if you have the kill function.  */
+#define HAVE_KILL 1
+
+/* Define if you have the popen function.  */
+#define HAVE_POPEN 1
+
+/* Define if you have the putenv function.  */
+#define HAVE_PUTENV 1
+
+/* Define if you have the rindex function.  */
+#define HAVE_RINDEX 1
+
+/* Define if you have the setrlimit function.  */
+#define HAVE_SETRLIMIT 1
+
+/* Define if you have the strchr function.  */
+#define HAVE_STRCHR 1
+
+/* Define if you have the strerror function.  */
+#define HAVE_STRERROR 1
+
+/* Define if you have the strrchr function.  */
+#define HAVE_STRRCHR 1
+
+/* Define if you have the strtoul function.  */
+#define HAVE_STRTOUL 1
+
+/* Define if you have the sysconf function.  */
+#define HAVE_SYSCONF 1
+
+/* Define if you have the <fcntl.h> header file.  */
+#define HAVE_FCNTL_H 1
+
+/* Define if you have the <limits.h> header file.  */
+#define HAVE_LIMITS_H 1
+
+/* Define if you have the <stab.h> header file.  */
+#define HAVE_STAB_H 1
+
+/* Define if you have the <stddef.h> header file.  */
+#define HAVE_STDDEF_H 1
+
+/* Define if you have the <stdlib.h> header file.  */
+#define HAVE_STDLIB_H 1
+
+/* Define if you have the <string.h> header file.  */
+#define HAVE_STRING_H 1
+
+/* Define if you have the <strings.h> header file.  */
+#define HAVE_STRINGS_H 1
+
+/* Define if you have the <sys/file.h> header file.  */
+#define HAVE_SYS_FILE_H 1
+
+/* Define if you have the <sys/param.h> header file.  */
+#define HAVE_SYS_PARAM_H 1
+
+/* Define if you have the <sys/resource.h> header file.  */
+#define HAVE_SYS_RESOURCE_H 1
+
+/* Define if you have the <sys/time.h> header file.  */
+#define HAVE_SYS_TIME_H 1
+
+/* Define if you have the <sys/times.h> header file.  */
+#define HAVE_SYS_TIMES_H 1
+
+/* Define if you have the <sys/wait.h> header file.  */
+#define HAVE_SYS_WAIT_H 1
+
+/* Define if you have the <time.h> header file.  */
+#define HAVE_TIME_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Define if you have the <wait.h> header file.  */
+/* #undef HAVE_WAIT_H */
