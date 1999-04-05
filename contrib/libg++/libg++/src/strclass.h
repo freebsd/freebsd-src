256e5d49b40982fc0ec2b4db8b691698dd6daaa9
David E. O'Brien <obrien@FreeBSD.org>
1999-04-05 05:37:27 +0000

    libg++ is OBE.

--- contrib/libg++/libg++/src/strclass.h
+++
@@ -1,5 +0,0 @@
-#ifndef _strclass_h
-#define _strclass_h
-#include <String.h>
-typedef class String string;
-#endif
--- contrib/libg++/libstdc++/std/cstdarg.h
+++
@@ -1,7 +0,0 @@
-// The -*- C++ -*- variable argument handling header.
-// This file is part of the GNU ANSI C++ Library.
-
-#ifndef __CSTDARG__
-#define __CSTDARG__
-#include <stdarg.h>
-#endif
