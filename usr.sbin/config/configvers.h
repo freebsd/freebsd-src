2c43119102490d61e66de2b6857b31ce7c3aebf0
Peter Wemm <peter@FreeBSD.org>
1999-04-16 21:28:10 +0000

    Corresponding minimal changes for kernel configuration after new-bus commit.

--- usr.sbin/config/configvers.h
+++ usr.sbin/config/configvers.h
@@ -6,6 +6,6 @@
  * The numbering scheme is inspired by the sys/conf/newvers.sh RELDATE
  * and <osreldate.h> system.
  *
- * $Id: configvers.h,v 1.10 1999/04/13 18:22:57 peter Exp $
+ * $Id: configvers.h,v 1.11 1999/04/15 14:52:22 bde Exp $
  */
-#define	CONFIGVERS	300012
+#define	CONFIGVERS	400013
