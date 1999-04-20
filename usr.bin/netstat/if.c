f79ee4379a6ba159ce8012518b96f4193550455c
Bill Fumerola <billf@FreeBSD.org>
1999-04-20 22:07:05 +0000

    MFC: IPX address formatting nit. (1.24 -> 1.25)

--- usr.bin/netstat/if.c
+++ usr.bin/netstat/if.c
@@ -36,7 +36,7 @@
 static char sccsid[] = "@(#)if.c	8.3 (Berkeley) 4/28/95";
 */
 static const char rcsid[] =
-	"$Id: if.c,v 1.23 1997/02/22 19:56:21 peter Exp $";
+	"$Id: if.c,v 1.25 1999/04/20 22:04:31 billf Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -197,7 +197,7 @@ intpr(interval, ifnetaddr)
 
 				*(union ipx_net *) &net = sipx->sipx_addr.x_net;
 				sprintf(netnum, "%lx", (u_long)ntohl(net));
-				printf("ipx:%-8s ", netnum);
+				printf("ipx:%-8s  ", netnum);
 /*				printf("ipx:%-8s ", netname(net, 0L)); */
 				printf("%-15s ",
 				    ipx_phost((struct sockaddr *)sipx));
