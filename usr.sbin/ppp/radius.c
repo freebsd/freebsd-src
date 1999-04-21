28e610e378d359693787641fc78f2b04d013df1a
Brian Somers <brian@FreeBSD.org>
1999-04-21 08:13:25 +0000

    int -> size_t

--- usr.sbin/ppp/radius.c
+++ usr.sbin/ppp/radius.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: radius.c,v 1.3 1999/02/06 02:54:47 brian Exp $
+ *	$Id: radius.c,v 1.4 1999/03/03 23:00:41 brian Exp $
  *
  */
 
@@ -80,7 +80,8 @@ radius_Process(struct radius *r, int got)
 {
   char *argv[MAXARGS], *nuke;
   struct bundle *bundle;
-  int len, argc, addrs;
+  int argc, addrs;
+  size_t len;
   struct in_range dest;
   struct in_addr gw;
   const void *data;
