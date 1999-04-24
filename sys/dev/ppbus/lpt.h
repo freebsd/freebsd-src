fe8010ef44722d699e097b424c571d3becdd3074
David E. O'Brien <obrien@FreeBSD.org>
1999-04-24 02:20:22 +0000

    Bring lpt.{c,h} from -CURRENT.  These files are esentually the same as the previous nlpt.{c,h}.  MAJOR history was lost when nlpt.{c,h} -> lpt.{c,h} in -CURRENT as these files weren't repository copied.  So for the record:
    Major ppbus commit with:
    
            + ECP parallel port chipset FIFO detection
            + DMA+FIFO parallel I/O handled as chipset specific
            + nlpt updated in order to use the above enhanced parallel I/O.
              Use 'lptcontrol -e' to use enhanced I/O
            + Various options documented in LINT
            + Full IEEE1284 NIBBLE and BYTE modes support. See ppbus(4) for
              an overview of the IEEE1284 standard
            + Detection of PnP parallel devices at boot
            + Read capability added to nlpt driver to get IEEE1284 compliant
              printer status with a simple 'cat /dev/lpt0'
            + IEEE1284 peripheral emulation added to BYTE mode. Two computers
              may dialog according to IEEE1284 signaling method.
              See PERIPH_1284 option and /sys/dev/ppbus/ppi.c
    
    All this code is supposed to provide basic functions for IEEE1284 programming.
    ppi.c and lpt.c may act as examples.

--- sys/dev/ppbus/lpt.h
+++ sys/dev/ppbus/lpt.h
@@ -34,7 +34,7 @@
  *	Id: lptreg.h,v 1.6 1997/02/22 09:36:52 peter Exp 
  *	From Id: nlpt.h,v 1.3 1999/01/10 12:04:54 nsouch Exp
  *
- *	$Id: lpt.h,v 1.1 1999/02/14 11:59:59 nsouch Exp $
+ *	$Id: lpt.h,v 1.2 1999/02/14 16:19:16 nsouch Exp $
  */
 
 /*
