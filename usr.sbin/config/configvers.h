6a4b0fc1b7b97dc216237a0e9a465bb3d32c8b30
peter <peter@FreeBSD.org>
1999-04-24 21:38:50 +0000

    Bump configvers; when the updates to generic/lint get committed, the old config has severe indigestion.

--- usr.sbin/config/configvers.h
+++ usr.sbin/config/configvers.h
@@ -6,6 +6,6 @@
  * The numbering scheme is inspired by the sys/conf/newvers.sh RELDATE
  * and <osreldate.h> system.
  *
- * $Id: configvers.h,v 1.11 1999/04/15 14:52:22 bde Exp $
+ * $Id: configvers.h,v 1.12 1999/04/16 21:28:10 peter Exp $
  */
-#define	CONFIGVERS	400013
+#define	CONFIGVERS	400014
