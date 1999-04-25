8b3de8552d4b5ab8a72c3fe8dcceba0bf6ee51b7
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-04-25 22:33:30 +0000

    More egcs warning fixes: 	o main returns int not void 	o use braces to avoid potentially ambiguous else
    Note: The fix to natd is potentially functional in nature since I used
    the indentation as the right thing rather than the struct semantics.
    Someone more familiar with the code should double check me on this one.
    
    Reviewed by: obrien and chuckr
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@46080 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/fsdb/fsdb.c
+++ sbin/fsdb/fsdb.c
@@ -30,7 +30,7 @@
 
 #ifndef lint
 static const char rcsid[] =
-	"$Id: fsdb.c,v 1.10 1998/06/15 07:12:19 charnier Exp $";
+	"$Id: fsdb.c,v 1.11 1998/11/09 03:43:55 truckman Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -69,7 +69,7 @@ char nflag = 0;
  * fsreadfd is set up to read from the file system, fswritefd to write to
  * the file system.
  */
-void
+int
 main(argc, argv)
 	int argc;
 	char *argv[];
