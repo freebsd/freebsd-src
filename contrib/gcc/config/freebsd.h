522e272a7bbc871510c4447a31ce6ce84e90a698
obrien <obrien@FreeBSD.org>
1999-04-28 18:48:08 +0000

    Rather than hack config/freebsd.h to contain our hacks for a native compiler, just include them in tm.h (as built from cc/cc_tools/Makefile).
    This will reduce the diffs from the vendor sources.
    
    Excellent idea by:	jdp

--- contrib/gcc/config/freebsd.h
+++ contrib/gcc/config/freebsd.h
@@ -1,4 +1,4 @@
-/* $Id: freebsd.h,v 1.9 1999/04/27 15:43:56 obrien Exp $ */
+/* $Id: freebsd.h,v 1.10 1999/04/27 19:29:41 obrien Exp $ */
 /* Base configuration file for all FreeBSD targets.
    Copyright (C) 1999 Free Software Foundation, Inc.
 
@@ -86,47 +86,3 @@ Boston, MA 02111-1307, USA.  */
 
 /* Tell libgcc2.c that FreeBSD targets support atexit(3).  */
 #define HAVE_ATEXIT
-
-
-/* FREEBSD_NATIVE is defined when gcc is integrated into the FreeBSD
-   source tree so it can be configured appropriately without using
-   the GNU configure/build mechanism. */
-
-#ifdef FREEBSD_NATIVE
-
-/* Look for the include files in the system-defined places.  */
-
-#define GPLUSPLUS_INCLUDE_DIR		"/usr/include/g++"
-#define GCC_INCLUDE_DIR			"/usr/include"
-
-/* Now that GCC knows what the include path applies to, put the G++ one first.
-   C++ can now have include files that override the default C ones.  */
-#undef INCLUDE_DEFAULTS
-#define INCLUDE_DEFAULTS			\
-  {						\
-    { GPLUSPLUS_INCLUDE_DIR, "C++", 1, 1 },	\
-    { GCC_INCLUDE_DIR, "GCC", 0, 0 },		\
-    { 0, 0, 0, 0 }				\
-  }
-
-/* Under FreeBSD, the normal location of the compiler back ends is the
-   /usr/libexec directory.  */
-
-#undef STANDARD_EXEC_PREFIX
-#undef TOOLDIR_BASE_PREFIX
-#undef MD_EXEC_PREFIX
-
-#define STANDARD_EXEC_PREFIX		"/usr/libexec/"
-#define TOOLDIR_BASE_PREFIX		"/usr/libexec/"
-#define MD_EXEC_PREFIX			"/usr/libexec/"
-
-/* Under FreeBSD, the normal location of the various *crt*.o files is the
-   /usr/lib directory.  */
-
-#undef STANDARD_STARTFILE_PREFIX
-#define STANDARD_STARTFILE_PREFIX	"/usr/lib/"
-
-/* FreeBSD is 4.4BSD derived */
-#define bsd4_4
-
-#endif /* FREEBSD_NATIVE */
