07901f227b2763d5e616e0944141cd056d0bb5e8
Poul-Henning Kamp <phk@FreeBSD.org>
1999-04-30 06:51:51 +0000

    Add beer-ware license and $Id$
    Noticed by:	dillon

--- sys/kern/kern_jail.c
+++ sys/kern/kern_jail.c
@@ -1,3 +1,14 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+ *
+ * $Id: malloc.c,v 1.44 1999/03/28 14:16:05 phk Exp $
+ *
+ */
 
 #include <sys/param.h>
 #include <sys/types.h>
