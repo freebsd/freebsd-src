339a39351dc9d42a073b2d70f327acc3fbe0c81e
nate <nate@FreeBSD.org>
1999-05-02 05:59:13 +0000

    MFC: Recognize alot more PCI/CardBus bridges so FreeBSD works on more      laptops out of the box.
    From Torsten Blum, PAO via Nick Sayer, and myself.

--- sys/pci/pcic_p.c
+++ sys/pci/pcic_p.c
@@ -26,7 +26,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: pcic_p.c,v 1.6 1998/08/18 00:32:48 bde Exp $
+ * $Id: pcic_p.c,v 1.9 1999/04/01 15:28:09 nsayer Exp $
  */
 
 #include "pci.h"
@@ -67,14 +67,40 @@ static const char *
 pcic_pci_probe(pcici_t tag, pcidi_t type)
 {
 	switch (type) {
+	case PCI_DEVICE_ID_PCIC_CLPD6832:
+		return ("Cirrus Logic PD6832 PCI/CardBus Bridge");
+	case PCI_DEVICE_ID_PCIC_TI1130:
+		return ("TI PCI-1130 PCI-CardBus Bridge");
+	case PCI_DEVICE_ID_PCIC_TI1131:
+		return ("TI PCI-1131 PCI-CardBus Bridge");
+	case PCI_DEVICE_ID_PCIC_TI1220:
+		return ("TI PCI-1220 PCI-CardBus Bridge");
+	case PCI_DEVICE_ID_PCIC_TI1221:
+		return ("TI PCI-1221 PCI-CardBus Bridge");
+	case PCI_DEVICE_ID_PCIC_TI1250:
+		return ("TI PCI-1250 PCI-CardBus Bridge");
+	case PCI_DEVICE_ID_PCIC_TI1251:
+		return ("TI PCI-1251 PCI-CardBus Bridge");
+	case PCI_DEVICE_ID_TOSHIBA_TOPIC95:
+		return ("Toshiba ToPIC95 PCI-CardBus Bridge");
+	case PCI_DEVICE_ID_TOSHIBA_TOPIC97:
+		return ("Toshiba ToPIC97 PCI-CardBus Bridge");
+ 	case PCI_DEVICE_ID_RICOH_RL5C465:
+		return ("Ricoh RL5C465 PCI-CardBus Brige");
+	case PCI_DEVICE_ID_RICOH_RL5C475:
+		return ("Ricoh RL5C475 PCI-CardBus Brige");
+	case PCI_DEVICE_ID_RICOH_RL5C476:
+		return ("Ricoh RL5C476 PCI-CardBus Brige");
+	case PCI_DEVICE_ID_RICOH_RL5C478:
+		return ("Ricoh RL5C478 PCI-CardBus Brige");
+	/* 16bit PC-card bridges */
 	case PCI_DEVICE_ID_PCIC_CLPD6729:
 		return ("Cirrus Logic PD6729/6730 PC-Card Controller");
-	case PCI_DEVICE_ID_PCIC_CLPD6832:
-		return ("Cirrus Logic PD6832 CardBus Adapter");
-	case PCI_DEVICE_ID_PCIC_TI1130:
-		return ("TI 1130 PCMCIA/CardBus Bridge");
-	case PCI_DEVICE_ID_PCIC_TI1131:
-		return ("TI 1131 PCI to PCMCIA/CardBus bridge");
+	case PCI_DEVICE_ID_PCIC_OZ6729:
+		return ("O2micro OZ6729 PC-Card Bridge");
+	case PCI_DEVICE_ID_PCIC_OZ6730:
+		return ("O2micro OZ6730 PC-Card Bridge");
+
 	default:
 		break;
 	}
