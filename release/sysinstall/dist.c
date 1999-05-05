670249cb0810be4afeaab302c596b8a4006b212d
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-05-05 11:34:03 +0000

    Another round of updates for -current and 3.2
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@46503 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/sysinstall/dist.c
+++ release/sysinstall/dist.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: dist.c,v 1.137 1999/03/19 10:54:37 jkh Exp $
+ * $Id: dist.c,v 1.138 1999/04/21 07:22:37 obrien Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -202,8 +202,6 @@ distVerifyFlags(void)
 	XF86Dists |= DIST_XF86_FONTS;
     if (XF86Dists || XF86ServerDists || XF86FontDists)
 	Dists |= DIST_XF86;
-    if (Dists & DIST_XF86)
-	Dists |= DIST_COMPAT22;
     if (isDebug())
 	msgDebug("Dist Masks: Dists: %0x, DES: %0x, Srcs: %0x\nXServer: %0x, XFonts: %0x, XDists: %0x\n",
 		 Dists, DESDists, SrcDists, XF86ServerDists, XF86FontDists, XF86Dists);
