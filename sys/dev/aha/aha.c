2bb789d7a3f2adcda33230cdaeda95918cf54a8d
ken <ken@FreeBSD.org>
1999-05-06 20:16:39 +0000

    Add a number of interrelated CAM feature enhancements and bug fixes.
    NOTE:  These changes will require recompilation of any userland
    applications, like cdrecord, xmcd, etc., that use the CAM passthrough
    interface.  A make world is recommended.
    
    camcontrol.[c8]:
     - We now support two new commands, "tags" and "negotiate".
    
    	- The tags commands allows users to view the number of tagged
    	  openings for a device as well as a number of other related
    	  parameters, and it allows users to set tagged openings for
    	  a device.
    
    	- The negotiate command allows users to enable and disable
    	  disconnection and tagged queueing, set sync rates, offsets
    	  and bus width.  Note that not all of those features are
    	  available for all controllers.  Only the adv, ahc, and ncr
    	  drivers fully support all of the features at this point.
    	  Some cards do not allow the setting of sync rates, offsets and
    	  the like, and some of the drivers don't have any facilities to
    	  do so.  Some drivers, like the adw driver, only support enabling
    	  or disabling sync negotiation, but do not support setting sync
    	  rates.
    
     - new description in the camcontrol man page of how to format a disk
     - cleanup of the camcontrol inquiry command
     - add support in the 'devlist' command for skipping unconfigured devices if
       -v was not specified on the command line.
     - make use of the new base_transfer_speed in the path inquiry CCB.
     - fix CCB bzero cases
    
    cam_xpt.c, cam_sim.[ch], cam_ccb.h:
    
     - new flags on many CCB function codes to designate whether they're
       non-immediate, use a user-supplied CCB, and can only be passed from
       userland programs via the xpt device.  Use these flags in the transport
       layer and pass driver to categorize CCBs.
    
     - new flag in the transport layer device matching code for device nodes
       that indicates whether a device is unconfigured
    
     - bump the CAM version from 0x10 to 0x11
    
     - Change the CAM ioctls to use the version as their group code, so we can
       force users to recompile code even when the CCB size doesn't change.
    
     - add + fill in a new value in the path inquiry CCB, base_transfer_speed.
       Remove a corresponding field from the cam_sim structure, and add code to
       every SIM to set this field to the proper value.
    
     - Fix the set transfer settings code in the transport layer.
    
    scsi_cd.c:
    
     - make some variables volatile instead of just casting them in various
       places
     - fix a race condition in the changer code
     - attach unless we get a "logical unit not supported" error.  This should
       fix all of the cases where people have devices that return weird errors
       when they don't have media in the drive.
    
    scsi_da.c:
    
     - attach unless we get a "logical unit not supported" error
    
    scsi_pass.c:
    
     - for immediate CCBs, just malloc a CCB to send the user request in.  This
       gets rid of the 'held' count problem in camcontrol tags.
    
    scsi_pass.h:
    
     - change the CAM ioctls to use the CAM version as their group code.
    
    adv driver:
    
     - Allow changing the sync rate and offset separately.
    
    adw driver
    
     - Allow changing the sync rate and offset separately.
    
    aha driver:
    
     - Don't return CAM_REQ_CMP for SET_TRAN_SETTINGS CCBs.
    
    ahc driver:
    
     - Allow setting offset and sync rate separately
    
    bt driver:
    
     - Don't return CAM_REQ_CMP for SET_TRAN_SETTINGS CCBs.
    
    NCR driver:
    
     - Fix the ultra/ultra 2 negotiation bug
     - allow setting both the sync rate and offset separately
    
    Other HBA drivers:
     - Put code in to set the base_transfer_speed field for
       XPT_GET_TRAN_SETTINGS CCBs.
    
    Reviewed by:	gibbs, mjacob (isp), imp (aha)

--- sys/dev/aha/aha.c
+++ sys/dev/aha/aha.c
@@ -55,7 +55,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *      $Id: aha.c,v 1.20 1999/03/02 20:56:07 imp Exp $
+ *      $Id: aha.c,v 1.21 1999/04/11 02:55:50 eivind Exp $
  */
 
 #include "pnp.h"
@@ -1029,7 +1029,7 @@ ahaaction(struct cam_sim *sim, union ccb *ccb)
 	case XPT_SET_TRAN_SETTINGS:
 	{
 		/* XXX Implement */
-		ccb->ccb_h.status = CAM_REQ_CMP;
+		ccb->ccb_h.status = CAM_PROVIDE_FAIL;
 		xpt_done(ccb);
 		break;
 	}
@@ -1120,6 +1120,7 @@ ahaaction(struct cam_sim *sim, union ccb *ccb)
 		cpi->max_lun = 7;
 		cpi->initiator_id = aha->scsi_id;
 		cpi->bus_id = cam_sim_bus(sim);
+		cpi->base_transfer_speed = 3300;
 		strncpy(cpi->sim_vid, "FreeBSD", SIM_IDLEN);
 		strncpy(cpi->hba_vid, "Adaptec", HBA_IDLEN);
 		strncpy(cpi->dev_name, cam_sim_name(sim), DEV_IDLEN);
