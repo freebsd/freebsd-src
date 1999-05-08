d122c02a74cad7c46cd723e87f68008d90f0a88f
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-05-08 12:52:26 +0000

    ls_items[] is most definately not a set of pointers to const objects.. sysinits, for example, are sorted by swapping those objects(!).  Perhaps they should be const and the sysinit sorting should be swapping the pointers rather than the targets.  This is on my revisit list, but it has the side effect of removing a lot of warnings.  With -Wcast-qual, it doesn't seem easy to get rid of the constness when you *know* it's not. (With apologies to bde, this essentially reverts rev 1.21 of kernel.h  from back in 1996)
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@46692 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/linker_set.h
+++ sys/sys/linker_set.h
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id$
+ *	$Id: linker_set.h,v 1.1 1999/01/14 05:48:46 jdp Exp $
  */
 
 #ifndef _SYS_LINKER_SET_H_
@@ -81,8 +81,8 @@
 #endif
 
 struct linker_set {
-	int		ls_length;
-	const void	*ls_items[1];		/* really ls_length of them,
+	int	ls_length;
+	void	*ls_items[1];		/* really ls_length of them,
 						 * trailing NULL */
 };
 
