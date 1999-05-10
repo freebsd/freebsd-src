9ce49f6de3783f545eebcd01ed627d57723adce0
Mike Spengler <mks@FreeBSD.org>
1999-05-10 23:02:29 +0000

    Allow configuration of up to 256 network interfaces per physical interface - just like the docs say it should.

--- sys/dev/hfa/fore_globals.c
+++ sys/dev/hfa/fore_globals.c
@@ -23,7 +23,7 @@
  * Copies of this Software may be made, however, the above copyright
  * notice must be reproduced on all copies.
  *
- *	@(#) $Id: fore_globals.c,v 1.1 1998/09/15 08:22:55 phk Exp $
+ *	@(#) $Id: fore_globals.c,v 1.2 1998/10/31 20:06:52 phk Exp $
  *
  */
 
@@ -38,7 +38,7 @@
 #include <dev/hfa/fore_include.h>
 
 #ifndef lint
-__RCSID("@(#) $Id: fore_globals.c,v 1.1 1998/09/15 08:22:55 phk Exp $");
+__RCSID("@(#) $Id: fore_globals.c,v 1.2 1998/10/31 20:06:52 phk Exp $");
 #endif
 
 
@@ -101,7 +101,7 @@ struct sp_info fore_nif_pool = {
 	"fore nif pool",		/* si_name */
 	sizeof(struct atm_nif),		/* si_blksiz */
 	5,				/* si_blkcnt */
-	20				/* si_maxallow */
+	52				/* si_maxallow */
 };
 
 struct sp_info fore_vcc_pool = {
