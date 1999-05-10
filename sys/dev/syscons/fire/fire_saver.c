4a5222d798e245b6ed2b121022d53e34a988a8bc
Dag-Erling Smørgrav <des@FreeBSD.org>
1999-05-10 15:25:50 +0000

    MFS: indent-assisted style cleanup.

--- sys/dev/syscons/fire/fire_saver.c
+++ sys/dev/syscons/fire/fire_saver.c
@@ -23,12 +23,11 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *	$Id: fire_saver.c,v 1.2 1999/05/04 12:58:56 jkh Exp $
+ *	$Id: fire_saver.c,v 1.2.2.1 1999/05/10 15:20:30 des Exp $
  */
 
 /*
- * brad forschinger, 19990504
- * <retch@flag.blackened.net>
+ * brad forschinger, 19990504 <retch@flag.blackened.net>
  * 
  * written with much help from warp_saver.c
  * 
@@ -48,81 +47,79 @@
 #define X_SIZE 320
 #define Y_SIZE 200
 
-static int blanked;
-static u_char fire_pal[768];
-static u_char buf[X_SIZE * (Y_SIZE + 1)];
-static u_char *vid;
+static int      blanked;
+static u_char   fire_pal[768];
+static u_char   buf[X_SIZE * (Y_SIZE + 1)];
+static u_char  *vid;
 
 static int
 fire_saver(video_adapter_t *adp, int blank)
 {
-    int x, y;
+    int             x, y;
 
     if (blank) {
 	if (blanked <= 0) {
-	    int red, green, blue;
-	    int palette_index;
-      
+	    int             red, green, blue;
+	    int             palette_index;
+
 	    set_video_mode(adp, M_VGA_CG320);
-      
+
 	    /* build and load palette */
 	    red = green = blue = 0;
 	    for (palette_index = 0; palette_index < 256; palette_index++) {
 		red++;
 		if (red > 128)
 		    green += 2;
-	
+
 		fire_pal[(palette_index * 3) + 0] = red;
 		fire_pal[(palette_index * 3) + 1] = green;
 		fire_pal[(palette_index * 3) + 2] = blue;
 	    }
 	    load_palette(adp, fire_pal);
-      
+
 	    blanked++;
-	    vid = (u_char *)adp->va_window;
+	    vid = (u_char *) adp->va_window;
 	}
-    
 	/* make a new bottom line */
-	for (x = 0, y = Y_SIZE; x < X_SIZE; x++) 
+	for (x = 0, y = Y_SIZE; x < X_SIZE; x++)
 	    buf[x + (y * X_SIZE)] = random() % 160 + 96;
-  
+
 	/* fade the flames out */
 	for (y = 0; y < Y_SIZE; y++) {
 	    for (x = 0; x < X_SIZE; x++) {
 		buf[x + (y * X_SIZE)] = (buf[(x + 0) + ((y + 0) * X_SIZE)] +
 					 buf[(x - 1) + ((y + 1) * X_SIZE)] +
 					 buf[(x + 0) + ((y + 1) * X_SIZE)] +
-					 buf[(x + 1) + ((y + 1) * X_SIZE)]) / 4;
+				     buf[(x + 1) + ((y + 1) * X_SIZE)]) / 4;
 		if (buf[x + (y * X_SIZE)] > 0)
 		    buf[x + (y * X_SIZE)]--;
 	    }
 	}
-    
+
 	/* blit our buffer into video ram */
 	memcpy(vid, buf, X_SIZE * Y_SIZE);
     } else {
 	blanked = 0;
     }
-  
+
     return 0;
 }
 
 static int
 fire_initialise(video_adapter_t *adp)
-{   
-    video_info_t info;
-  
+{
+    video_info_t    info;
+
     /* check that the console is capable of running in 320x200x256 */
     if (get_mode_info(adp, M_VGA_CG320, &info)) {
 	log(LOG_NOTICE, "fire_saver: the console does not support M_VGA_CG320\n");
-	return(ENODEV);
-    } 
-  
-    blanked = 0;    
-  
+	return (ENODEV);
+    }
+    blanked = 0;
+
     return 0;
 }
-  
+
 static int
 fire_terminate(video_adapter_t *adp)
 {
