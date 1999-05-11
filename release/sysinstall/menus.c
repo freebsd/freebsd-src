095c0c74afbbc607f115b18633423d976bdf1faf
Jordan K. Hubbard <jkh@FreeBSD.org>
1999-05-11 10:25:34 +0000

    MFC: desktop ordering, preserve /root.

--- release/sysinstall/menus.c
+++ release/sysinstall/menus.c
@@ -4,7 +4,7 @@
  * This is probably the last program in the `sysinstall' line - the next
  * generation being essentially a complete rewrite.
  *
- * $Id: menus.c,v 1.180.2.16 1999/05/01 11:28:41 jkh Exp $
+ * $Id: menus.c,v 1.180.2.17 1999/05/05 11:34:44 jkh Exp $
  *
  * Copyright (c) 1995
  *	Jordan Hubbard.  All rights reserved.
@@ -460,10 +460,10 @@ DMenu MenuXDesktops = {
     "you can choose any one of the following desktops as alternatives.",
     NULL,
     NULL,
-    { { "Gnome",	"The GNOME desktop environment.",
-	NULL, dmenuSetVariable, NULL, VAR_DESKSTYLE "=gnome" },
-      { "KDE",		"The K Desktop Environment.",
+    { { "KDE",		"The K Desktop Environment.",
 	NULL, dmenuSetVariable, NULL, VAR_DESKSTYLE "=kde" },
+      { "GNOME",	"The GNOME desktop environment.",
+	NULL, dmenuSetVariable, NULL, VAR_DESKSTYLE "=gnome" },
       { "Afterstep",	"The Afterstep Window manager",
 	NULL, dmenuSetVariable, NULL, VAR_DESKSTYLE "=afterstep" },
       { "Windowmaker",	"The Windowmaker Window manager",
