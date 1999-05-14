600829cdfcd2ebb76d2cf809f6e090c0f3fc949c
Justin T. Gibbs <gibbs@FreeBSD.org>
1999-05-14 17:39:54 +0000

    Nuke ucmpdi2.c from i386/libkern to serve as a reminder that switch statements on 64bit values generate poor code.
    Requested by: bde

--- sys/libkern/ucmpdi2.c
+++ sys/libkern/ucmpdi2.c
@@ -34,10 +34,10 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: ucmpdi2.c,v 1.4 1997/02/22 09:40:05 peter Exp $
+ * $Id: ucmpdi2.c,v 1.5 1999/05/14 05:05:33 gibbs Exp $
  */
 
-#include <libkern/quad.h>
+#include "quad.h"
 
 /*
  * Return 0, 1, or 2 as a <, =, > b respectively.
