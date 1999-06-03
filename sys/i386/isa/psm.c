0caf7fea4606da844f83b4fea2d544553afe21a1
Kazutaka YOKOTA <yokota@FreeBSD.org>
1999-06-03 12:42:48 +0000

    MFC: Fix PS/2 MouseMan+ protocol.

--- sys/i386/isa/psm.c
+++ sys/i386/isa/psm.c
@@ -20,7 +20,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: psm.c,v 1.59 1999/01/11 03:18:23 yokota Exp $
+ * $Id: psm.c,v 1.60 1999/01/12 00:36:33 eivind Exp $
  */
 
 /*
@@ -1794,7 +1794,7 @@ psmintr(int unit)
 	    if ((c & ~MOUSE_PS2_BUTTONS) == 0xc8) {
 		/* the extended data packet encodes button and wheel events */
 		x = y = 0;
-		z = (sc->ipacket[1] & MOUSE_PS2PLUS_ZNEG)
+		z = (sc->ipacket[2] & MOUSE_PS2PLUS_ZNEG)
 		    ? (sc->ipacket[2] & 0x0f) - 16 : (sc->ipacket[2] & 0x0f);
 		ms.button |= (sc->ipacket[2] & MOUSE_PS2PLUS_BUTTON4DOWN)
 		    ? MOUSE_BUTTON4DOWN : 0;
