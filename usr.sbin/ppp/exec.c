17f84fdd518b60418b290f5e5a8ca1ce45a61598
brian <brian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-06-09 08:47:36 +0000

    Use the correct pid when substituting PROCESSID. Problem reported by: Amedeo Beck Peccoz <gea@gressoney.it>
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@47849 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/ppp/exec.c
+++ usr.sbin/ppp/exec.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: exec.c,v 1.4 1999/06/01 19:08:57 brian Exp $
+ *	$Id: exec.c,v 1.5 1999/06/05 21:35:50 brian Exp $
  */
 
 #include <sys/param.h>
@@ -118,7 +118,7 @@ exec_Create(struct physical *p)
 	         strerror(errno));
     else {
       int stat, argc;
-      pid_t pid;
+      pid_t pid, realpid;
       char *argv[MAXARGS];
 
       stat = fcntl(fids[0], F_GETFL, 0);
@@ -126,6 +126,7 @@ exec_Create(struct physical *p)
         stat |= O_NONBLOCK;
         fcntl(fids[0], F_SETFL, stat);
       }
+      realpid = getpid();
       switch ((pid = fork())) {
         case -1:
           log_Printf(LogPHASE, "Unable to create pipe for line exec: %s\n",
@@ -156,7 +157,7 @@ exec_Create(struct physical *p)
           log_Printf(LogDEBUG, "Exec'ing ``%s''\n", p->name.base);
           argc = MakeArgs(p->name.base, argv, VECSIZE(argv));
           command_Expand(argv, argc, (char const *const *)argv,
-                         p->dl->bundle, 0);
+                         p->dl->bundle, 0, realpid);
           execvp(*argv, argv);
           fprintf(stderr, "execvp failed: %s: %s\r\n", *argv, strerror(errno));
           _exit(127);
