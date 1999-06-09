ecf825269af66055f6b494fc5496274d0254783b
brian <brian@FreeBSD.org>
1999-06-09 08:47:36 +0000

    Use the correct pid when substituting PROCESSID. Problem reported by: Amedeo Beck Peccoz <gea@gressoney.it>

--- usr.sbin/ppp/command.h
+++ usr.sbin/ppp/command.h
@@ -15,7 +15,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: command.h,v 1.15 1998/06/15 19:06:06 brian Exp $
+ * $Id: command.h,v 1.16 1999/02/11 10:14:08 brian Exp $
  *
  *	TODO:
  */
@@ -55,7 +55,7 @@ extern const char Version[];
 extern const char VersionDate[];
 
 extern void command_Expand(char **, int, char const *const *, struct bundle *,
-                           int);
+                           int, pid_t);
 extern int command_Interpret(char *, int, char *vector[MAXARGS]);
 extern void command_Run(struct bundle *, int, char const *const *,
                         struct prompt *, const char *, struct datalink *);
