db5877d58586994ea7fdd52034a838e40de43da0
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-06-09 09:08:22 +0000

    Various edits to shrink fixit and enable dhcp-client
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@47851 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/amd64/dokern.sh
+++ release/amd64/dokern.sh
@@ -11,6 +11,9 @@ sed	-e '/pty/d' \
 	-e '/SYSVMSG/d' \
 	-e 's/GENERIC/BOOTMFS/g'
 
+# So dhclient will work (just on boot floppy).
+echo "pseudo-device bpfilter 4"
+
 echo "options  NFS_NOSERVER" 
 echo "options  SCSI_NO_OP_STRINGS" 
 echo "options  SCSI_NO_SENSE_STRINGS"
