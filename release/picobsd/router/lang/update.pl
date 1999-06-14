c3cd20594fe9794249ba71b97a63e1f99008dfb4
roger <roger@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-06-14 15:45:56 +0000

    Update to PicoBSD 0.44 from RELENG_3
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@47906 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/picobsd/router/lang/update.pl
+++ release/picobsd/router/lang/update.pl
@@ -1,11 +1,11 @@
 #!/bin/sh
-# $Id: update.pl,v 1.1.1.1 1998/08/27 17:38:44 abial Exp $
+# $Id: update.pl,v 1.2.2.1 1999/05/07 10:03:38 abial Exp $
 echo -n "Uaktualniam katalog /etc na dyskietce...  "
 mount /dev/fd0a /start_floppy
 cd /etc
 cp -Rp . /start_floppy/etc/
 echo " Zrobione."
 echo -n "Uaktualniam parametry jadra..."
-kget /start_floppy/kernel.config
+kget /start_floppy/boot/kernel.conf
 umount /dev/fd0a
 echo " Zrobione."
