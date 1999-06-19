be1ab5cffb7cb8995fff2ec54e147e9e9d3c1011
n_hibma <n_hibma@FreeBSD.org>
1999-06-19 13:32:27 +0000

    Add again the ':' after the x option in th eargument list to getopt.
    It disappeared in rev. 1.23 newfs.c
    
    PR: 12292
    Submitted by: Cy Schubert <cy@cschuber.net.gov.bc.ca>

--- sbin/newfs/newfs.c
+++ sbin/newfs/newfs.c
@@ -42,7 +42,7 @@ static const char copyright[] =
 static char sccsid[] = "@(#)newfs.c	8.13 (Berkeley) 5/1/95";
 #endif
 static const char rcsid[] =
-	"$Id: newfs.c,v 1.27 1998/10/17 08:03:52 bde Exp $";
+	"$Id: newfs.c,v 1.28 1999/02/09 17:19:19 dillon Exp $";
 #endif /* not lint */
 
 /*
@@ -249,7 +249,7 @@ main(argc, argv)
 
 	opstring = mfs ?
 	    "NF:T:a:b:c:d:e:f:i:m:o:s:" :
-	    "NOS:T:a:b:c:d:e:f:i:k:l:m:n:o:p:r:s:t:u:vx";
+	    "NOS:T:a:b:c:d:e:f:i:k:l:m:n:o:p:r:s:t:u:vx:";
 	while ((ch = getopt(argc, argv, opstring)) != -1)
 		switch (ch) {
 		case 'N':
