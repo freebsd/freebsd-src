c9a9cf971a17f3ffd6df72e29cdf5d5f599c9305
Greg Lehey <grog@FreeBSD.org>
1999-06-22 05:54:39 +0000

    Remove ancient, mouldy comments.

--- sbin/vinum/v.c
+++ sbin/vinum/v.c
@@ -101,22 +101,6 @@ void start_daemon(void);
 
 #define ofs(x) ((void *) (& ((struct confdata *) 0)->x))    /* offset of x in struct confdata */
 
-/*   create description-file
-   Create a volume as described in description-file
-   modify description-file
-   Modify the objects as described in description-file
-   list [-r] [volume | plex | subdisk]
-   List information about specified objects
-   set [-f] state volume | plex | subdisk | disk
-   Set the state of the object to state
-   rm [-f] [-r] volume | plex | subdisk
-   Remove an object
-   start [volume | plex | subdisk]
-   Allow the system to access the objects
-   stop [-f] [volume | plex | subdisk]
-   Terminate access the objects
- */
-
 char *token[MAXARGS];					    /* pointers to individual tokens */
 int tokens;						    /* number of tokens */
 
