d6cced4c5f302cbca22b104eabc06601010a15e2
bde <bde@FreeBSD.org>
1999-07-04 08:12:36 +0000

    MFC (1.14: quick fix for breakage of bounds checking in rev.1.12).

--- sys/net/bpf_filter.c
+++ sys/net/bpf_filter.c
@@ -37,7 +37,7 @@
  *
  *      @(#)bpf_filter.c	8.1 (Berkeley) 6/10/93
  *
- * $Id: bpf_filter.c,v 1.11 1998/12/07 03:26:34 eivind Exp $
+ * $Id: bpf_filter.c,v 1.12 1998/12/07 16:31:15 eivind Exp $
  */
 
 #include <sys/param.h>
@@ -328,7 +328,7 @@ bpf_filter(pc, p, wirelen, buflen)
 
 		case BPF_LD|BPF_B|BPF_IND:
 			k = X + pc->k;
-			if (pc->k >= buflen || X >= buflen - k) {
+			if (pc->k >= buflen || X >= buflen - pc->k) {
 #ifdef KERNEL
 				register struct mbuf *m;
 
