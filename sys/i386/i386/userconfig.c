bcab2f59d73f8ab11875b1e6f1d7a5fd8548ea11
wpaul <wpaul@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-07-09 04:30:09 +0000

    This commit adds driver support for the SysKonnect SK-984x series gigabit ethernet adapters. This includes two single port cards (single mode and multimode fiber) and two dual port cards (also single mode and multimode fiber). SysKonnect is currently the only vendor with a dual port gigabit ethernet NIC.
    The ports on dual port adapters are treated as separate network
    interfaces. Thus, if you have an SK-9844 dual port SX card, you
    should have both sk0 and sk1 interfaces attached. Dual port cards
    are implemented using two XMAC II chips connected to a single
    SysKonnect GEnesis controller. Hence, dual port cards are really
    one PCI device, as opposed to two separate PCI devices connected
    through a PCI to PCI bridge. Note that SysKonnect's drivers use
    the two ports for failover purposes rather that as two separate
    interfaces, plus they don't support jumbo frames. This applies to
    their Linux driver too. :)
    
    Support is provided for hardware multicast filtering, BPF and
    jumbo frames. The SysKonnect cards support TCP checksum offload
    however this feature is not currently enabled (hopefully it will
    be once we get checksum offload support).
    
    There are still a few things that need to be implemeted, like
    the ability to communicate with the on-board LM80 voltage/temperature
    monitor, but I wanted to get the driver under CVS control and into
    -current so people could bang on it.
    
    A big thanks for SysKonnect for making all their programming info
    for these cards (and for their FDDI and token ring cards) available
    without NDA (see www.syskonnect.com).
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@48693 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/i386/userconfig.c
+++ sys/i386/i386/userconfig.c
@@ -46,7 +46,7 @@
  ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **
- **      $Id: userconfig.c,v 1.146 1999/06/28 12:07:36 peter Exp $
+ **      $Id: userconfig.c,v 1.147 1999/06/29 17:37:44 yokota Exp $
  **/
 
 /**
@@ -387,6 +387,7 @@ static DEV_INFO device_info[] = {
 {"ix",          "Intel EtherExpress Ethernet adapter",	0,		CLS_NETWORK},
 {"le",          "DEC Etherworks 2 and 3 Ethernet adapters",	0,	CLS_NETWORK},
 {"lnc",         "Isolan, Novell NE2100/NE32-VL Ethernet adapters",	0,CLS_NETWORK},
+{"sk",          "SysKonnect SK-984x gigabit Ethernet adapters",		0,CLS_NETWORK},
 {"ti",          "Alteon Networks Tigon gigabit Ethernet adapters",	0,CLS_NETWORK},
 {"tl",          "Texas Instruments ThunderLAN Ethernet adapters",	0,CLS_NETWORK},
 {"tx",          "SMC 9432TX Ethernet adapters",			0,	CLS_NETWORK},
@@ -2542,7 +2543,7 @@ visuserconfig(void)
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *      $Id: userconfig.c,v 1.146 1999/06/28 12:07:36 peter Exp $
+ *      $Id: userconfig.c,v 1.147 1999/06/29 17:37:44 yokota Exp $
  */
 
 #include "scbus.h"
