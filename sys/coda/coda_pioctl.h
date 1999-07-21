9261597c760a9c5c1e9bcab7643953d9f250854f
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-07-21 12:51:36 +0000

    Remove the RCS "Log" and all the verbiage it has generated.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@48960 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/coda/coda_pioctl.h
+++ sys/coda/coda_pioctl.h
@@ -27,7 +27,7 @@
  * Mellon the rights to redistribute these changes without encumbrance.
  * 
  * 	@(#) src/sys/coda/coda_pioctl.h,v 1.1.1.1 1998/08/29 21:14:52 rvb Exp $ 
- *  $Id: coda_pioctl.h,v 1.3 1998/09/11 18:50:17 rvb Exp $
+ *  $Id: coda_pioctl.h,v 1.4 1998/09/13 13:57:59 rvb Exp $
  * 
  */
 
@@ -40,69 +40,6 @@
  * the terms and conditions for use and redistribution.
  */
 
-/*
- * HISTORY
- * $Log: coda_pioctl.h,v $
- * Revision 1.3  1998/09/11 18:50:17  rvb
- * All the references to cfs, in symbols, structs, and strings
- * have been changed to coda.  (Same for CFS.)
- *
- * Revision 1.2  1998/09/02 19:09:53  rvb
- * Pass2 complete
- *
- * Revision 1.1.1.1  1998/08/29 21:14:52  rvb
- * Very Preliminary Coda
- *
- * Revision 1.7  1998/08/28 18:12:26  rvb
- * Now it also works on FreeBSD -current.  This code will be
- * committed to the FreeBSD -current and NetBSD -current
- * trees.  It will then be tailored to the particular platform
- * by flushing conditional code.
- *
- * Revision 1.6  1998/08/18 17:05:26  rvb
- * Don't use __RCSID now
- *
- * Revision 1.5  1998/08/18 16:31:51  rvb
- * Sync the code for NetBSD -current; test on 1.3 later
- *
- * Revision 1.4  98/01/23  11:53:54  rvb
- * Bring RVB_CODA1_1 to HEAD
- * 
- * Revision 1.3.2.1  97/12/06  17:41:29  rvb
- * Sync with peters coda.h
- * 
- * Revision 1.3  97/12/05  10:39:31  rvb
- * Read CHANGES
- * 
- * Revision 1.2.34.2  97/11/13  22:03:06  rvb
- * pass2 cfs_NetBSD.h mt
- * 
- * Revision 1.2.34.1  97/11/12  12:38:11  rvb
- * mach_vioctl.h -> pioctl.h
- * 
- * Revision 1.2  96/01/02  16:57:27  bnoble
- * Added support for Coda MiniCache and raw inode calls (final commit)
- * 
- * Revision 1.1.2.1  1995/12/20 01:57:54  bnoble
- * Added CODA-specific files
- *
- * Revision 2.4  90/08/30  11:51:12  bohman
- * 	Ioctl changes for STDC.
- * 	[90/08/28            bohman]
- * 
- * Revision 2.3  89/03/09  22:10:26  rpd
- * 	More cleanup.
- * 
- * Revision 2.2  89/02/25  17:58:32  gm0w
- * 	Changes for cleanup.
- * 
- *  7-Feb-87  Avadis Tevanian (avie) at Carnegie-Mellon University
- *	No need for VICE conditional.
- *
- * 22-Oct-86  Jay Kistler (jjk) at Carnegie-Mellon University
- *	Created from Andrew's vice.h and viceioctl.h.
- *
- */
 /*
  * ITC Remote file system - vice ioctl interface module
  */
