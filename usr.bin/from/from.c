e3b18788049abb82791583364e214e75355b1de0
Brian Feldman <green@FreeBSD.org>
1999-07-27 02:08:55 +0000

    This covers the previous version, since a null commit message was accidentally used:
    Add a -c flag to print a count of messages and exit.
    
    PR:             10921
    Submitted by:   James Howard <howardjp@byzantine.student.umd.edu>
    Requested by:	DES

--- usr.bin/from/from.c
+++ usr.bin/from/from.c
@@ -42,7 +42,7 @@ static const char copyright[] =
 static char sccsid[] = "@(#)from.c	8.1 (Berkeley) 6/6/93";
 #endif
 static const char rcsid[] =
-	"$Id: from.c,v 1.5 1997/07/08 10:59:50 charnier Exp $";
+	"$Id: from.c,v 1.6 1999/07/26 23:12:12 green Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
