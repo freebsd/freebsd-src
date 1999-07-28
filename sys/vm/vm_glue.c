f620cbcd9093f858ace7e5ee892e76999e0fcdf9
Alan Cox <alc@FreeBSD.org>
1999-07-28 07:40:02 +0000

    MFC: Fix a race condition between pmap_growkernel and process creation.  [pmap.h: rev 1.31, vm_glue.c: rev 1.87, vm_map.c: rev 1.173]
    Hide access to vmspace::vm_pmap behind inline function vmspace_pmap.
    (This function is assumed by the above patch.)
     [vm_map.h: rev 1.36]

--- sys/vm/vm_glue.c
+++ sys/vm/vm_glue.c
@@ -59,7 +59,7 @@
  * any improvements or extensions that they make and grant Carnegie the
  * rights to redistribute these changes.
  *
- * $Id: vm_glue.c,v 1.80 1999/01/07 21:23:50 julian Exp $
+ * $Id: vm_glue.c,v 1.80.2.1 1999/01/27 20:51:43 julian Exp $
  */
 
 #include "opt_rlimit.h"
@@ -220,6 +220,8 @@ vm_fork(p1, p2, flags)
 
 	if ((flags & RFMEM) == 0) {
 		p2->p_vmspace = vmspace_fork(p1->p_vmspace);
+
+		pmap_pinit2(vmspace_pmap(p2->p_vmspace));
 
 		if (p1->p_vmspace->vm_shm)
 			shmfork(p1, p2);
