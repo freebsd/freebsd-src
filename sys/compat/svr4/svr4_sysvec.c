39f2cb8a1d7be4f9872baef4a9f6a6ce1aff5628
newton <newton@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-07-30 13:42:56 +0000

    Previous commit also finished cleaning up some dev_t -> udev_t transformations related to the commit for rev 1.3 of svr4_stat.c. svr4_sysvec.c also received a copyright message (which is why it grew by 28 lines).
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@49279 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/compat/svr4/svr4_sysvec.c
+++ sys/compat/svr4/svr4_sysvec.c
@@ -27,7 +27,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * 
- * $Id$
+ * $Id: svr4_sysvec.c,v 1.4 1999/07/30 12:45:19 newton Exp $
  */
 
 /* XXX we use functions that might not exist. */
