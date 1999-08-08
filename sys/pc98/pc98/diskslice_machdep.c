7149486634ffad68b395f148bf1f7b871a4df6e4
kato <kato@FreeBSD.org>
1999-08-08 10:08:12 +0000

    MFC: Fix a panic caused by freeing unallocated structure.

--- sys/pc98/pc98/diskslice_machdep.c
+++ sys/pc98/pc98/diskslice_machdep.c
@@ -35,7 +35,7 @@
  *
  *	from: @(#)ufs_disksubr.c	7.16 (Berkeley) 5/4/91
  *	from: ufs_disksubr.c,v 1.8 1994/06/07 01:21:39 phk Exp $
- *	$Id: diskslice_machdep.c,v 1.16 1998/08/10 16:55:54 kato Exp $
+ *	$Id: diskslice_machdep.c,v 1.17 1998/12/04 22:54:54 archie Exp $
  */
 
 /*
@@ -325,7 +325,6 @@ reread_mbr:
 		/* IBM-PC HDD */
 		bp->b_flags = B_INVAL | B_AGE;
 		brelse(bp);
-		free(ssp, M_DEVBUF);
 		return atcompat_dsinit(dname, dev, strat, lp, sspp);
 	}
 #endif
