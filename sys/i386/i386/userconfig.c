ed3d7889970282af1d075c12fe381150451a595e
mpp <mpp@FreeBSD.org>
1999-08-09 13:52:12 +0000

    Remove a reference to config(8) when IRQ 2 is remapped to IRQ 9. Config(8) contains no documentation about this.
    Fix the help for the PnP irq and drq commands.  This one caused
    me a bit of head scratching the other night while trying to get
    a problematic PnP device configured properly.

--- sys/i386/i386/userconfig.c
+++ sys/i386/i386/userconfig.c
@@ -46,7 +46,7 @@
  ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **
- **      $Id: userconfig.c,v 1.150 1999/07/25 13:16:06 cracauer Exp $
+ **      $Id: userconfig.c,v 1.151 1999/08/09 10:34:44 phk Exp $
  **/
 
 /**
@@ -2544,7 +2544,7 @@ visuserconfig(void)
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *      $Id: userconfig.c,v 1.150 1999/07/25 13:16:06 cracauer Exp $
+ *      $Id: userconfig.c,v 1.151 1999/08/09 10:34:44 phk Exp $
  */
 
 #include "scbus.h"
@@ -2854,7 +2854,7 @@ set_device_irq(CmdParm *parms)
 
     irq = parms[1].parm.iparm;
     if (irq == 2) {
-	printf("Warning: Remapping IRQ 2 to IRQ 9 - see config(8)\n");
+	printf("Warning: Remapping IRQ 2 to IRQ 9\n");
 	irq = 9;
     }
     else if (irq != -1 && irq > 15) {
@@ -3080,8 +3080,8 @@ helpfunc(CmdParm *parms)
     "pnp <csn> <ldn> [os|bios]\tset parameters using FreeBSD or BIOS\n"
     "pnp <csn> <ldn> [portX <addr>]\tset addr for port X (0..7)\n"
     "pnp <csn> <ldn> [memX <maddr>]\tset addr for memory range X (0..3)\n"
-    "pnp <csn> <ldn> [irq <number>]\tset irq X (0..1) to number, 0=unused\n"
-    "pnp <csn> <ldn> [drq <number>]\tset drq X (0..1) to number, 4=unused\n");
+    "pnp <csn> <ldn> [irqX <number>]\tset irq X (0..1) to number, 0=unused\n"
+    "pnp <csn> <ldn> [drqX <number>]\tset drq X (0..1) to number, 4=unused\n");
 #endif
 #if NEISA > 0
     printf("eisa <number>\t\tSet the number of EISA slots to probe\n");
