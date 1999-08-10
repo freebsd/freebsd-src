4368f240893bf7954a5c542c47bef3a0ce3bff1d
rnordier <rnordier@FreeBSD.org>
1999-08-10 21:36:51 +0000

    Treat an attempt to read from a write-only stream more consistently.
    Submitted by: Anton Berezin <tobez@plab.ku.dk>
    PR          : 12852

--- lib/libc/stdio/refill.c
+++ lib/libc/stdio/refill.c
@@ -39,7 +39,7 @@
 static char sccsid[] = "@(#)refill.c	8.1 (Berkeley) 6/4/93";
 #endif
 static const char rcsid[] =
-		"$Id$";
+		"$Id: refill.c,v 1.6 1997/02/22 15:02:22 peter Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <errno.h>
@@ -82,6 +82,7 @@ __srefill(fp)
 	if ((fp->_flags & __SRD) == 0) {
 		if ((fp->_flags & __SRW) == 0) {
 			errno = EBADF;
+			fp->_flags |= __SERR;
 			return (EOF);
 		}
 		/* switch to reading */
