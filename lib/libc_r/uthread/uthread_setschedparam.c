e17cbfeb5911de38fc0d1954dc8c40487b36e9c9
Peter Wemm <peter@FreeBSD.org>
1999-08-14 15:33:46 +0000

    Forced commit.  The previous delta was backdated to try and make dated branch checkouts consistant.

--- lib/libc_r/uthread/uthread_setschedparam.c
+++ lib/libc_r/uthread/uthread_setschedparam.c
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: uthread_setschedparam.c,v 1.2 1999/06/20 08:28:44 jb Exp $
+ * $Id: uthread_setschedparam.c,v 1.1.2.1 1999/05/09 07:48:40 peter Exp $
  */
 #include <errno.h>
 #include <sys/param.h>
