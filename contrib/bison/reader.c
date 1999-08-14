705387917b5c8bddf7f8aeec9d94e134bfeefc99
obrien <obrien@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-08-14 22:15:21 +0000

    Add "--broken-undeftoken-init" option.  This makes Bison 1.28 bug compatable with version 1.25.
    Bison 1.28 fixed a bug in the initialization of the `undefined' symbol
    table entry.  Unfortunately something about the way we are compiling
    egcs-1.1.2's cc1plus breaks with this bugfix.
    
    "--broken-undeftoken-init" is needed when compiling cc1plus.  Otherwise
    /usr/libexec/cc1plus cannot compile libgcc.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@49801 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/bison/reader.c
+++ contrib/bison/reader.c
@@ -44,6 +44,7 @@ The entry point is reader().  */
 int rline_allocated;
 
 extern char *program_name;
+extern int broken_undeftoken_init;
 extern int definesflag;
 extern int nolinesflag;
 extern int noparserflag;
@@ -199,7 +200,10 @@ reader (void)
   /* it is always token number 2.  */
   undeftoken = getsym("$undefined.");
   undeftoken->class = STOKEN;
-  undeftoken->user_token_number = 2;
+  /* XXX ``broken_undeftoken_init'' makes Bison 1.28 bug-compatable
+     with Bison 1.25.  FreeBSD depends on this behavior when compiling
+     EGCS-1.1.2's cc1plus.  */
+  undeftoken->user_token_number = broken_undeftoken_init ? 0 : 2;
   /* Read the declaration section.  Copy %{ ... %} groups to ftable and fdefines file.
      Also notice any %token, %left, etc. found there.  */
   if (noparserflag)
