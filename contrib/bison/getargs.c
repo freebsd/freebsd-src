896ffac087c20395eaf35d2e122b9547680ffa17
obrien <obrien@FreeBSD.org>
1999-08-14 22:15:21 +0000

    Add "--broken-undeftoken-init" option.  This makes Bison 1.28 bug compatable with version 1.25.
    Bison 1.28 fixed a bug in the initialization of the `undefined' symbol
    table entry.  Unfortunately something about the way we are compiling
    egcs-1.1.2's cc1plus breaks with this bugfix.
    
    "--broken-undeftoken-init" is needed when compiling cc1plus.  Otherwise
    /usr/libexec/cc1plus cannot compile libgcc.

--- contrib/bison/getargs.c
+++ contrib/bison/getargs.c
@@ -31,6 +31,7 @@ int nolinesflag;
 int noparserflag = 0;
 int toknumflag = 0;
 int rawtoknumflag = 0;
+int broken_undeftoken_init;
 char *spec_name_prefix; /* for -p.  */
 char *spec_file_prefix; /* for -b. */
 extern int fixed_outfiles;/* for -y */
@@ -44,6 +45,7 @@ extern void warns PARAMS((char *, char *));	/* main.c */
 
 struct option longopts[] =
 {
+  {"broken-undeftoken-init", 0, &broken_undeftoken_init, 1},
   {"debug", 0, &debugflag, 1},
   {"defines", 0, &definesflag, 1},
   {"file-prefix", 1, 0, 'b'},
