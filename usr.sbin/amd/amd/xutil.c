d2302d51c3745f5e6c8a67fa43c0cc6a7f0ffb03
David E. O'Brien <obrien@FreeBSD.org>
1999-08-25 19:55:03 +0000

    Rev 1.1.1.1.8.2 was build tested by mharo.

--- usr.sbin/amd/amd/xutil.c
+++ usr.sbin/amd/amd/xutil.c
@@ -41,7 +41,7 @@
 static char sccsid[] = "@(#)xutil.c 8.1 (Berkeley) 6/6/93";
 #endif
 static const char rcsid[] =
-	"$Id: xutil.c,v 1.1.1.1.8.1 1997/09/03 06:27:04 charnier Exp $";
+	"$Id: xutil.c,v 1.1.1.1.8.2 1999/08/25 19:49:44 obrien Exp $";
 #endif /* not lint */
 
 #include "config.h"
