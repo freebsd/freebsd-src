18ebbcc08a554db7b7b2acfcbbb8a0d3cbeae87f
green <green@FreeBSD.org>
1999-08-27 16:35:37 +0000

    Add FIODTYPE ioctl for getting d_flags (type) info on a device.
    Okayed by:	phk

--- sys/sys/filio.h
+++ sys/sys/filio.h
@@ -36,7 +36,7 @@
  * SUCH DAMAGE.
  *
  *	@(#)filio.h	8.1 (Berkeley) 3/28/94
- * $Id$
+ * $Id: filio.h,v 1.4 1997/02/22 09:45:16 peter Exp $
  */
 
 #ifndef	_SYS_FILIO_H_
@@ -52,5 +52,6 @@
 #define	FIOASYNC	_IOW('f', 125, int)	/* set/clear async i/o */
 #define	FIOSETOWN	_IOW('f', 124, int)	/* set owner */
 #define	FIOGETOWN	_IOR('f', 123, int)	/* get owner */
+#define	FIODTYPE	_IOR('f', 122, int)	/* get d_flags type part */
 
 #endif /* !_SYS_FILIO_H_ */
