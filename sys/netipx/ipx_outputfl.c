06da3c3890a8a7f50396df134ae88512f762bb78
jhay <jhay@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-08-28 18:21:55 +0000

    Get rid of the old XNS checksum code and implement it the IPX way.
    PR:		13374
    Submitted by:	Boris Popov <bp@butya.kz>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@50519 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netipx/ipx_outputfl.c
+++ sys/netipx/ipx_outputfl.c
@@ -247,12 +247,8 @@ ipx_output_type20(m)
 			ipx->ipx_dna.x_net = dst.sipx_addr.x_net;
 			m1 = m_copym(m, 0, M_COPYALL, M_DONTWAIT);
 
-			if(ipx->ipx_sum != 0xffff) {
-				int len = ntohs(ipx->ipx_len);
-				ipx->ipx_sum = 0;
-				len = ((len - 1) | 1) + 1;
-				ipx->ipx_sum = ipx_cksum(m, len);
-			}
+			if(ipx->ipx_sum != 0xffff)
+				ipx->ipx_sum = ipx_cksum(m, ntohs(ipx->ipx_len));
 			if(m1) {
 				error = (*ifp->if_output)(ifp, m1,
 					(struct sockaddr *)&dst, NULL);
