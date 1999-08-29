c780381f51d1bba84c85f1cd36ce1ffc14f0c76c
grog <grog@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-08-29 01:41:53 +0000

    Initialize the si_bsize fields of the new dev structure.  This fixes some swapper problems analogous to those experienced with ccd.
    This fix is a kludge: since we currently don't track the "sector size"
    in a volume label, we guess a worst case (4 kB, as used by vnode
    devices).  If the concept of sector size is here to stay, I'll make
    some changes to track the "sector size" of a volume.  This will
    probably be the maximum of the sector sizes of all component drives,
    but things could get ugly if we start allowing non-standard sector
    sizes such as 524 bytes.
    
    Unkludged-version-submitted-by:	phk
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@50537 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/vinum/vinum.c
+++ sys/dev/vinum/vinum.c
@@ -271,6 +271,9 @@ vinumopen(dev_t dev,
     int devminor;					    /* minor number */
 
     devminor = minor(dev);
+    dev->si_bsize_phys = DEV_BSIZE;
+    dev->si_bsize_best = VINUM_BSIZE_BEST;		    /* kludge until we track drive block sizes */
+    dev->si_bsize_max = MAXBSIZE;
     error = 0;
     /* First, decide what we're looking at */
     switch (DEVTYPE(dev)) {
@@ -469,3 +472,6 @@ vinumdump(dev_t dev)
     /* Not implemented. */
     return ENXIO;
 }
+/* Local Variables: */
+/* fill-column: 50 */
+/* End: */
