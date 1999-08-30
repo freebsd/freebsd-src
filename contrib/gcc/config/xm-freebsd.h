756f260a30d8c16678dde7dbc6bb510b43f1e27b
obrien <obrien@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-08-30 10:12:33 +0000

    This brings rev 1.2 into EGCS 1.1.2.  Also tweaks gleemed from EGCS ChangeLog's.  Note that rev 1.3 changes (HOST_PTR_PRINTF) was not brought into EGCS 1.1.2 as that is the default.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@50647 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/gcc/config/xm-freebsd.h
+++ contrib/gcc/config/xm-freebsd.h
@@ -22,15 +22,16 @@ Boston, MA 02111-1307, USA.  */
    running FreeBSD.  This file should not be specified as $xm_file itself;
    instead $xm_file should be CPU/xm-freebsd.h, which should include both
    CPU/xm-CPU.h and this file xm-freebsd.h.  */
-   
-#define HAVE_PUTENV
 
-#define HAVE_STRERROR
+#ifndef HAVE_ATEXIT
+#define HAVE_ATEXIT
+#endif
+
+/* Tell gcc and collect2 that FreeBSD targets support putenv(3).  */
+#define HAVE_PUTENV
 
 /* We have _sys_siglist, but the declaration in <signal.h> conflicts with
    the declarations in collect2.c and mips-tfile.c, so disable the declarations
    in those files.  */
 
-#define DONT_DECLARE_SYS_SIGLIST
-
-#define HOST_PTR_PRINTF	"%p"
+#define SYS_SIGLIST_DECLARED
