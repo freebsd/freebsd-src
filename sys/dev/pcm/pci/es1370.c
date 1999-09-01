0d7e0303801347f266706b853e5e29afcaca6859
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-09-01 06:58:27 +0000

    $Id$ -> $FreeBSD$
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@50733 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/pcm/pci/es1370.c
+++ sys/dev/pcm/pci/es1370.c
@@ -37,7 +37,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *	$Id: es1370.c,v 1.4 1999/05/09 17:06:45 peter Exp $
+ * $FreeBSD$
  */
 
 #include "pci.h"
