d2e11b0c0cb34420d238ebd9384b014b53aa09f1
luoqi <luoqi@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-09-02 21:03:32 +0000

    Use definitions provided in sysarch.h for args structures.
    Reviewed by:	marcel
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@50817 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/i386/sys/i386_get_ioperm.c
+++ lib/libc/i386/sys/i386_get_ioperm.c
@@ -32,16 +32,10 @@ static const char rcsid[] = "$FreeBSD$";
 
 #include <machine/sysarch.h>
 
-struct parms {
-	unsigned int 	start;
-	unsigned int	length;
-        int	 	enable;
-};
-
 int
 i386_get_ioperm(unsigned int start, unsigned int *length, int *enable)
 {
-	struct parms p;
+	struct i386_ioperm_args p;
 	int error;
 
 	p.start = start;
