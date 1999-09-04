3751a3e374c5f71fc7efd836663f04d9870d675a
jkh <jkh@FreeBSD.org>
1999-09-04 08:46:32 +0000

    MFC: changes for DHCP support, some updates to docs (NOTE:  I've merged the USB and ISDN sections more or less unchanged; if there are still significant differences between the support in -current and the support currently in -stable for either of these families of devices, please let me know to adjust the docs.  I don't follow either the USB or ISDN efforts closely enough to know for sure!).

--- release/scripts/proflibs-install.sh
+++ release/scripts/proflibs-install.sh
@@ -5,4 +5,9 @@ if [ "`id -u`" != "0" ]; then
 	exit 1
 fi
 cat proflibs.?? | tar --unlink -xpzf - -C ${DESTDIR:-/}
+cd ${DESTDIR:-/}usr/lib
+if [ -f libdescrypt_p.a ]
+then
+	ln -f -s libdescrypt_p.a libcrypt_p.a
+fi
 exit 0
