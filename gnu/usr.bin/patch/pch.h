605340bddba36646433fa5720c8f1d6e28640c0e
peter <peter@FreeBSD.org>
1999-09-05 17:31:55 +0000

    Clean up some $Log$ debris, including some real messes.

--- gnu/usr.bin/patch/pch.h
+++ gnu/usr.bin/patch/pch.h
@@ -1,15 +1,6 @@
 /* $FreeBSD$
  *
  * $Log: pch.h,v $
- * Revision 1.4  1998/01/21 14:37:24  ache
- * Resurrect patch 2.1 without FreeBSD Index: hack
- *
- * Revision 1.2  1995/05/30 05:02:36  rgrimes
- * Remove trailing whitespace.
- *
- * Revision 1.1.1.1  1993/06/19  14:21:52  paul
- * b-maked patch-2.10
- *
  * Revision 2.0.1.1  87/01/30  22:47:16  lwall
  * Added do_ed_script().
  *
