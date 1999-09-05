072ca4aea09c7ffcde269d73093924534713e074
Peter Wemm <peter@FreeBSD.org>
1999-09-05 17:31:55 +0000

    Clean up some $Log$ debris, including some real messes.

--- gnu/usr.bin/patch/version.c
+++ gnu/usr.bin/patch/version.c
@@ -1,15 +1,6 @@
 /* $FreeBSD$
  *
  * $Log: version.c,v $
- * Revision 1.4  1998/01/21 14:37:26  ache
- * Resurrect patch 2.1 without FreeBSD Index: hack
- *
- * Revision 1.2  1995/05/30 05:02:39  rgrimes
- * Remove trailing whitespace.
- *
- * Revision 1.1.1.1  1993/06/19  14:21:52  paul
- * b-maked patch-2.10
- *
  * Revision 2.0  86/09/17  15:40:11  lwall
  * Baseline for netwide release.
  *
