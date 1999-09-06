cc7ae9bc92c8b7fdf7ac80442471db2451240a0c
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-09-06 06:34:44 +0000

    Kludge together support for the old pccard system with newbus.  This will allow newbus based drivers to have pccard attachments.  Also start printing out probe messages for pccards stating the resources used and regularize many of the pccard printfs.
    Reviewed by: Peter Wemm.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@50991 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ sys/pccard/pccard_nbk.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 1999, M. Warner Losh.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	$Id:$
+ */
+
+#ifndef PCCARD_PCCARD_NBK_H
+#define PCCARD_PCCARD_NBK_H
+
+struct pccard_device;
+extern devclass_t pccard_devclass;
+extern int pccnbk_alloc_resources(device_t);
+extern void pccnbk_release_resources(device_t);
+extern void pccnbk_wrap_old_driver(struct pccard_device *);
+
+#endif /* ! PCCARD_PCCARD_NBK_H */
