174524fe91fc87442c420bc96195a8e5864c2873
alfred <alfred@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-09-07 22:42:38 +0000

    All unimplemented VFS ops now have entries in kern/vfs_default.c that return reasonable defaults.
    This avoids confusing and ugly casting to eopnotsupp or making dummy functions.
    Bogus casting of filesystem sysctls to eopnotsupp() have been removed.
    
    This should make *_vfsops.c more readable and reduce bloat.
    
    Reviewed by:	msmith, eivind
    Approved by:	phk
    Tested by:	Jeroen Ruigrok/Asmodai <asmodai@wxs.nl>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@51068 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/miscfs/kernfs/kernfs.h
+++ sys/miscfs/kernfs/kernfs.h
@@ -51,17 +51,6 @@ struct kernfs_node {
 #define VFSTOKERNFS(mp)	((struct kernfs_mount *)((mp)->mnt_data))
 #define	VTOKERN(vp) ((struct kernfs_node *)(vp)->v_data)
 
-#define kernfs_fhtovp ((int (*) __P((struct mount *, struct fid *, \
-	    struct sockaddr *, struct vnode **, int *, struct ucred **)))eopnotsupp)
-#define kernfs_quotactl ((int (*) __P((struct mount *, int, uid_t, caddr_t, \
-	    struct proc *)))eopnotsupp)
-#define kernfs_sync ((int (*) __P((struct mount *, int, struct ucred *, \
-	    struct proc *)))nullop)
-#define kernfs_sysctl ((int (*) __P((int *, u_int, void *, size_t *, void *, \
-	    size_t, struct proc *)))eopnotsupp)
-#define kernfs_vget ((int (*) __P((struct mount *, ino_t, struct vnode **))) \
-	    eopnotsupp)
-#define kernfs_vptofh ((int (*) __P((struct vnode *, struct fid *)))eopnotsupp)
 extern vop_t **kernfs_vnodeop_p;
 extern dev_t rrootdev;
 #endif /* KERNEL */
