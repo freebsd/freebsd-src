4ce61a57d82fd35f513722f7d63298ec512d042d
roger <roger@FreeBSD.org>
1999-09-08 08:43:08 +0000

    MFC: Added Australian channel set

--- sys/i386/include/ioctl_bt848.h
+++ sys/i386/include/ioctl_bt848.h
@@ -14,8 +14,9 @@
 #define CHNLSET_JPNBCST         5
 #define CHNLSET_JPNCABLE        6
 #define CHNLSET_XUSSR           7
+#define CHNLSET_AUSTRALIA       8
 #define CHNLSET_MIN	        CHNLSET_NABCST
-#define CHNLSET_MAX	        CHNLSET_XUSSR
+#define CHNLSET_MAX	        CHNLSET_AUSTRALIA
 
 
 /*
