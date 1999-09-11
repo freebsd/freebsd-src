db9dbb0a3c024448b456899d2a4c17709a425477
jkh <jkh@FreeBSD.org>
1999-09-11 06:11:52 +0000

    Preserve permissions through tar pipelines.

--- release/scripts/info-make.sh
+++ release/scripts/info-make.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# $FreeBSD$
+
 # Move the info files out to their own dist
 if [ -d ${RD}/trees/bin/usr/share/info ]; then
 	tar -cf - -C ${RD}/trees/bin/usr/share/info . |
-		tar -xf - -C ${RD}/trees/info/usr/share/info &&
+		tar -xpf - -C ${RD}/trees/info/usr/share/info &&
 	rm -rf ${RD}/trees/bin/usr/share/info;
 fi
