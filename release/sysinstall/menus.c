a64d762ec0c9ee8ab779ed77680d2fd74536738a
obrien <obrien@FreeBSD.org>
1999-09-14 10:38:45 +0000

    Foo.  Maybe I should get Peter to hook ispell up in commit_prep.pl.

--- release/sysinstall/menus.c
+++ release/sysinstall/menus.c
@@ -1264,7 +1264,7 @@ DMenu MenuStartup = {
 	dmenuVarCheck, dmenuISetVariable, NULL, "named_flags" },
       { "nis client",	"This host wishes to be an NIS client.",
 	dmenuVarCheck, dmenuToggleVariable, NULL, "nis_client_enable=YES" },
-      { "nis domainname",	"Set NIS doimainname (if enabled)",
+      { "nis domainname",	"Set NIS domainname (if enabled)",
 	dmenuVarCheck, dmenuISetVariable, NULL, "nisdomainname" },
       { "nis server",	"This host wishes to be an NIS server.",
 	dmenuVarCheck, dmenuToggleVariable, NULL, "nis_server_enable=YES" },
