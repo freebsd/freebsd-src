d6dc8b813ff49be85062013190128aea077a15ee
dillon <dillon@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-09-17 06:10:27 +0000

    Add vfs.enable_userblk_io sysctl to control whether user reads and writes     to buffered block devices are allowed.  The default is to be backwards     compatible, i.e. reads and writes are allowed.
        The idea is for a larger crowd to start running with this disabled and
        see what problems, if any, crop up, and then to change the default to
        off and see if any problems crop up in the next 6 months prior to
        potentially removing support entirely.  There are still a few people,
        Julian and myself included, who believe the buffered block device
        access from usermode to be useful.
    
        Remove use of vnode->v_lastr from buffered block device I/O in
        preparation for removal of vnode->v_lastr field, replacing it with
        the already existing seqcount metric to detect sequential operation.
    
    Reviewed by:	Alan Cox <alc@cs.rice.edu>, David Greenman <dg@root.com>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@51345 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -280,6 +280,7 @@ extern	struct vm_zone *namei_zone;
 extern	int prtactive;			/* nonzero to call vprint() */
 extern	struct vattr va_null;		/* predefined null vattr structure */
 extern	int vfs_ioopt;
+extern  int enable_userblk_io;
 
 /*
  * Macro/function to check for client cache inconsistency w.r.t. leasing.
