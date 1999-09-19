170f347106454c60c4298a0542c314e4bff3af2b
markm <markm@FreeBSD.org>
1999-09-19 22:00:58 +0000

    Fixes for new Kerberos4. Userland to follow.

--- kerberosIV/include/version.h
+++ kerberosIV/include/version.h
@@ -1,2 +1,3 @@
-char *krb4_long_version = "@(#)$Version: krb4-0.9.6 by root on greenpeace.grondar.za (i386-unknown-freebsd3.0) Sun Jun 22 18:14:59 SAT 1997 $";
-char *krb4_version = "krb4-0.9.6";
+/* $FreeBSD$ */
+char *krb4_long_version = "@(#)$Version: krb4-0.10pre3 by root on greenpeace.grondar.za (i386-unknown-freebsd4.0) Wed Aug 18 19:04:21 SAST 1999 $";
+char *krb4_version = "krb4-0.10pre3";
