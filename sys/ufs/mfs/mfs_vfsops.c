f348d4f637fa9b59060e4bc4f0d5f1004d3bc9e4
phk <phk@FreeBSD.org>
1999-09-20 19:57:28 +0000

    Step one of replacing devsw->d_maxio with si_bsize_max.
    Rename dev->si_bsize_max to si_iosize_max and set it in spec_open
    if the device didn't.
    
    Set vp->v_maxio from dev->si_bsize_max in spec_open rather than
    in ufs_bmap.c

--- sys/ufs/mfs/mfs_vfsops.c
+++ sys/ufs/mfs/mfs_vfsops.c
@@ -335,7 +335,7 @@ mfs_mount(mp, path, data, ndp, p)
 	dev = make_dev(&mfs_cdevsw, mfs_minor, 0, 0, 0, "MFS%d", mfs_minor);
 	/* It is not clear that these will get initialized otherwise */
 	dev->si_bsize_phys = DEV_BSIZE;
-	dev->si_bsize_max = MAXBSIZE;
+	dev->si_iosize_max = DFLTPHYS;
 	addaliasu(devvp, makeudev(253, mfs_minor++));
 	devvp->v_data = mfsp;
 	mfsp->mfs_baseoff = args.base;
@@ -497,7 +497,7 @@ mfs_init(vfsp)
 		    0, 0, 0, "MFS%d", mfs_minor);
 		/* It is not clear that these will get initialized otherwise */
 		rootdev->si_bsize_phys = DEV_BSIZE;
-		rootdev->si_bsize_max = MAXBSIZE;
+		rootdev->si_iosize_max = DFLTPHYS;
 		mfs_minor++;
 	} else if (bootverbose)
 		printf("No MFS image available as root f/s.\n");
