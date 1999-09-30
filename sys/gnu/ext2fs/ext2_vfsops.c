4508ca252eaa51ea1fd0fb3838590d952d144427
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-09-30 07:11:30 +0000

    Remove the D_NOCLUSTER[RW] options which were added because vn had problems.  Now that Matt has fixed vn, this can go.  The vn driver should have used d_maxio (now si_iosize_max) anyway.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@51808 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/gnu/ext2fs/ext2_vfsops.c
+++ sys/gnu/ext2fs/ext2_vfsops.c
@@ -135,10 +135,6 @@ ext2_mountroot()
 	bzero((char *)mp, (u_long)sizeof(struct mount));
 	mp->mnt_op = &ext2fs_vfsops;
 	mp->mnt_flag = MNT_RDONLY;
-	if (devsw(rootdev)->d_flags & D_NOCLUSTERR)
-		mp->mnt_flag |= MNT_NOCLUSTERR;
-	if (devsw(rootdev)->d_flags & D_NOCLUSTERW)
-		mp->mnt_flag |= MNT_NOCLUSTERW;
 	if (error = ext2_mountfs(rootvp, mp, p)) {
 		bsd_free(mp, M_MOUNT);
 		return (error);
@@ -193,17 +189,11 @@ ext2_mount(mp, path, data, ndp, p)
 	/*
 	 * If updating, check whether changing from read-only to
 	 * read/write; if there is no device name, that's all we do.
-	 * Disallow clearing MNT_NOCLUSTERR and MNT_NOCLUSTERW flags,
-	 * if block device requests.
 	 */
 	if (mp->mnt_flag & MNT_UPDATE) {
 		ump = VFSTOUFS(mp);
 		fs = ump->um_e2fs;
 		error = 0;
-		if (devsw(ump->um_dev)->d_flags & D_NOCLUSTERR)
-			mp->mnt_flag |= MNT_NOCLUSTERR;
-		if (devsw(ump->um_dev)->d_flags & D_NOCLUSTERW)
-			mp->mnt_flag |= MNT_NOCLUSTERW;
 		if (fs->s_rd_only == 0 && (mp->mnt_flag & MNT_RDONLY)) {
 			flags = WRITECLOSE;
 			if (mp->mnt_flag & MNT_FORCE)
@@ -293,10 +283,6 @@ ext2_mount(mp, path, data, ndp, p)
 	}
 
 	if ((mp->mnt_flag & MNT_UPDATE) == 0) {
-		if (devsw(devvp->v_rdev)->d_flags & D_NOCLUSTERR)
-			mp->mnt_flag |= MNT_NOCLUSTERR;
-		if (devsw(devvp->v_rdev)->d_flags & D_NOCLUSTERW)
-			mp->mnt_flag |= MNT_NOCLUSTERW;
 		error = ext2_mountfs(devvp, mp, p);
 	} else {
 		if (devvp != ump->um_devvp)
