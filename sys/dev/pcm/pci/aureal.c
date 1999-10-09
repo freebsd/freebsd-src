a83205516577a269a3530855cddb79f695b0ee31
Warner Losh <imp@FreeBSD.org>
1999-10-09 03:52:07 +0000

    Make interrupts shareable with RF_SHAREABLE.  This is untested, but it compiles and should just work.

--- sys/dev/pcm/pci/aureal.c
+++ sys/dev/pcm/pci/aureal.c
@@ -623,7 +623,7 @@ au_pci_attach(device_t dev)
 
 	irqid = 0;
 	irq = bus_alloc_resource(dev, SYS_RES_IRQ, &irqid,
-				 0, ~0, 1, RF_ACTIVE);
+				 0, ~0, 1, RF_ACTIVE | RF_SHAREABLE);
 	if (!irq
 	    || bus_setup_intr(dev, irq, INTR_TYPE_TTY, au_intr, au, &ih)) {
 		device_printf(dev, "unable to map interrupt\n");
