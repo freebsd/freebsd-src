4e3e581d9bc9ef535789be7f5d7409fcb1f84d83
jlemon <jlemon@FreeBSD.org>
1999-10-09 03:51:19 +0000

    Hopefully fix a bug that was causing corruption (blocks of nulls) during large data transfers.  The hdr.flags field (actually the size of the request, in words) was a magic constant, which was probably the wrong value.
    Tested by:	 cucu@cuneydi.com

--- sys/i386/isa/ida.c
+++ sys/i386/isa/ida.c
@@ -1206,7 +1206,8 @@ ida_queue_buf(int unit, struct buf *bp)
 
   if (PCI_CONTROLLER(ida)) {
     qcbp->hdr.priority = 0x00;
-    qcbp->hdr.flags = 0x24;
+    qcbp->hdr.flags =
+      (sizeof(struct ida_req) + sizeof(struct ida_sgb) * IDA_MAX_SGLEN) >> 2;
   } else {
     qcbp->hdr.priority = IDA_DEF_PRIORITY;
     qcbp->hdr.flags = 0x10;
