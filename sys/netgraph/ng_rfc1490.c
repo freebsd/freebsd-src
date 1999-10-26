1e37349d12b1ee8417027d50a1090109abcab4e4
julian <julian@FreeBSD.org>
1999-10-26 22:33:54 +0000

    cleanups regarding misused m_pullup() and similar.

--- sys/netgraph/ng_rfc1490.c
+++ sys/netgraph/ng_rfc1490.c
@@ -226,7 +226,8 @@ ng_rfc1490_rcvdata(hook_p hook, struct mbuf *m, meta_p meta)
 	if (hook == priv->downlink) {
 		u_char *start, *ptr;
 
-		if (!m || !(m = m_pullup(m, MAX_ENCAPS_HDR)))
+		if (!m || (m->m_len < MAX_ENCAPS_HDR
+		    && !(m = m_pullup(m, MAX_ENCAPS_HDR))))
 			ERROUT(ENOBUFS);
 		ptr = start = mtod(m, u_char *);
 
