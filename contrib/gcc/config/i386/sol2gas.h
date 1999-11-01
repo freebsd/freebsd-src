d14ec649a54f7e8bcb26942b5b16b8fc2ee25044
David E. O'Brien <obrien@FreeBSD.org>
1999-11-01 08:28:22 +0000

    Virgin import of the GCC 2.95.2 compilers

---
+++ contrib/gcc/config/i386/sol2gas.h
@@ -0,0 +1,11 @@
+/* Definitions of target machine for GNU compiler, for SPARC running
+   Solaris 2 with GNU as up to 2.9.5.0.12.
+   
+   Copyright (C) 1999 Free Software Foundation, Inc.
+*/
+
+#ifndef GAS_DOES_NOT_SUPPORT_MINUS_S
+#define GAS_DOES_NOT_SUPPORT_MINUS_S 1
+#endif
+
+#include "i386/sol2.h"
