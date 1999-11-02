73a856e29c691c9cd5f58d071fdea4db9688e9f8
archie <archie@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-11-02 22:50:34 +0000

    Consolidate some of the various ctype(3) macros in one location.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@52815 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -81,6 +81,19 @@ extern int bootverbose;		/* nonzero to print verbose messages */
 #define	KASSERT(exp,msg)
 #endif
 
+#define isspace(c)	((c) == ' ' || ((c) >= '\t' && (c) <= '\r'))
+#define isascii(c)	(((c) & ~0x7f) == 0)
+#define isupper(c)	((c) >= 'A' && (c) <= 'Z')
+#define islower(c)	((c) >= 'a' && (c) <= 'z')
+#define isalpha(c)	(isupper(c) || islower(c))
+#define isdigit(c)	((c) >= '0' && (c) <= '9')
+#define isxdigit(c)	(isdigit(c) \
+			  || ((c) >= 'A' && (c) <= 'F') \
+			  || ((c) >= 'a' && (c) <= 'f'))
+
+#define toupper(c)	((c) - 0x20 * (((c) >= 'a') && ((c) <= 'z')))
+#define tolower(c)	((c) + 0x20 * (((c) >= 'A') && ((c) <= 'Z')))
+
 /*
  * General function declarations.
  */
