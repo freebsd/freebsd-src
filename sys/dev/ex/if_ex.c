d16bedd42a4958ed282a87fa1df7b1271937a0bf
mdodd <mdodd@FreeBSD.org>
1999-11-03 15:45:14 +0000

    Fix bus_setup_intr(); I changed exintr() to take a pointer to ex_softc but didn't change bus_setup_intr() to pass the softc in.
    Submitted by:	Warner Losh <imp@village.org>

--- sys/dev/ex/if_ex.c
+++ sys/dev/ex/if_ex.c
@@ -363,7 +363,7 @@ int ex_attach(device_t dev)
 	}
 
 	error = bus_setup_intr(dev, sc->irq, INTR_TYPE_NET,
-			       exintr, (void *)unit, &ih);
+			       exintr, (void *)sc, &ih);
 
 	if (error) {
 		device_printf(dev, "bus_setup_intr() failed!\n");
