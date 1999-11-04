006d9606ec80b95faa7b23d40288634d4e9c5919
ache <ache@FreeBSD.org>
1999-11-04 04:16:28 +0000

    Add unsigned char cast to isspace arg

--- lib/libc/gen/getcap.c
+++ lib/libc/gen/getcap.c
@@ -32,6 +32,8 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ * $FreeBSD$
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
@@ -682,7 +684,7 @@ cgetnext(bp, db_array)
 				slash = 0;
 				continue;
 			}
-			if (isspace(*line) ||
+			if (isspace((unsigned char)*line) ||
 			    *line == ':' || *line == '#' || slash) {
 				if (line[len - 2] == '\\')
 					slash = 1;
