32458f8c0144f938624a6f0e4d0e6c6fc501c6e3
David E. O'Brien <obrien@FreeBSD.org>
1999-11-17 07:18:25 +0000

    * Remove a trailing ";" from a macro. * Style cleanup.

--- contrib/gcc/config/alpha/freebsd.h
+++ contrib/gcc/config/alpha/freebsd.h
@@ -36,7 +36,6 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #undef WCHAR_UNSIGNED
 #define WCHAR_UNSIGNED 0
 
-
 /* Provide a CPP_SPEC appropriate for FreeBSD.  Current we just deal with
    the GCC option `-posix'.  */
 
@@ -54,11 +53,11 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 /* Output assembler code to FILE to increment profiler label # LABELNO
    for profiling a function entry.  Under FreeBSD/Alpha, the assembler does
-   nothing special with -pg. */
+   nothing special with -pg.  */
 
 #undef FUNCTION_PROFILER
-#define FUNCTION_PROFILER(FILE, LABELNO)			\
-	fputs ("\tjsr $28,_mcount\n", (FILE)); /* at */
+#define FUNCTION_PROFILER(FILE, LABELNO)				\
+	fputs ("\tjsr $28,_mcount\n", (FILE))  /* at */
 
 /* Show that we need a GP when profiling.  */
 #define TARGET_PROFILING_NEEDS_GP
@@ -69,41 +68,42 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #define PREFERRED_DEBUGGING_TYPE DBX_DEBUG
 
 #undef TARGET_VERSION
-#define TARGET_VERSION fprintf (stderr, " (FreeBSD/alpha ELF)");
+#define TARGET_VERSION fprintf (stderr, " (FreeBSD/Alpha ELF)");
 
 /* Names to predefine in the preprocessor for this target machine.
    XXX FreeBSD, by convention, shouldn't do __alpha, but lots of applications
    expect it because that's what OSF/1 does.  */
 
 #undef CPP_PREDEFINES
-#define CPP_PREDEFINES \
+#define CPP_PREDEFINES							\
   "-D__alpha__ -D__alpha -D__ELF__ -Acpu(alpha) -Amachine(alpha)"	\
   CPP_FBSD_PREDEFINES
 
 #undef LINK_SPEC
 #define LINK_SPEC "-m elf64alpha					\
-  %{p:%e`-p' not supported; use `-pg' and gprof(1)}		\
-  %{Wl,*:%*}							\
-  %{assert*} %{R*} %{rpath*} %{defsym*}				\
-  %{shared:-Bshareable %{h*} %{soname*}}			\
-  %{symbolic:-Bsymbolic}					\
-  %{!shared:							\
-    %{!static:							\
-      %{rdynamic:-export-dynamic}				\
-      %{!dynamic-linker:-dynamic-linker /usr/libexec/ld-elf.so.1}} \
+  %{p:%e`-p' not supported; use `-pg' and gprof(1)}			\
+  %{Wl,*:%*}								\
+  %{assert*} %{R*} %{rpath*} %{defsym*}					\
+  %{shared:-Bshareable %{h*} %{soname*}}				\
+  %{symbolic:-Bsymbolic}						\
+  %{!shared:								\
+    %{!static:								\
+      %{rdynamic:-export-dynamic}					\
+      %{!dynamic-linker:-dynamic-linker /usr/libexec/ld-elf.so.1}}	\
     %{static:-Bstatic}}"
 
 /* Provide a STARTFILE_SPEC for FreeBSD that is compatible with the
-   non-aout version used on i386. */
-   
-#undef	STARTFILE_SPEC
-#define STARTFILE_SPEC \
- "%{!shared: %{pg:gcrt1.o%s} %{!pg:%{p:gcrt1.o%s} %{!p:crt1.o%s}}} \
-    %{!shared:crtbegin.o%s} %{shared:crtbeginS.o%s}"
+   non-aout version used on i386.  */
 
-/* Provide a ENDFILE_SPEC appropriate for FreeBSD.  Here we tack on
-   the file which provides part of the support for getting C++
-   file-scope static object deconstructed after exiting `main' */
+#undef	STARTFILE_SPEC
+#define STARTFILE_SPEC							\
+  "%{!shared: %{pg:gcrt1.o%s} %{!pg:%{p:gcrt1.o%s} %{!p:crt1.o%s}}}	\
+     %{!shared:crtbegin.o%s} %{shared:crtbeginS.o%s}"
+
+/* Provide a ENDFILE_SPEC appropriate for ELF.  Here we tack on the
+   magical crtend.o file which provides part of the support for
+   getting C++ file-scope static object constructed before entering
+   `main', followed by a normal ELF "finalizer" file, `crtn.o'.  */
 
 #undef	ENDFILE_SPEC
 #define ENDFILE_SPEC \
@@ -548,9 +548,9 @@ dtors_section ()							\
 
 #if defined(__i386__)
 #undef HOST_BITS_PER_LONG
-#define	HOST_BITS_PER_LONG 32
+#define HOST_BITS_PER_LONG 32
 #undef HOST_WIDE_INT
-#define	HOST_WIDE_INT long long
+#define HOST_WIDE_INT long long
 #undef HOST_BITS_PER_WIDE_INT
-#define	HOST_BITS_PER_WIDE_INT 64
+#define HOST_BITS_PER_WIDE_INT 64
 #endif
