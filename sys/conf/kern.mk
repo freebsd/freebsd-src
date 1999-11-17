65e383d031b6ae53df5fb1705a211706a2758a56
obrien <obrien@FreeBSD.org>
1999-11-17 07:30:35 +0000

    Make "-mpreferred-stack-boundary=2" the default on the i386. This reduces the size of the kernel and modules when compiled with GCC 2.95.

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -19,6 +19,19 @@ CWARNFLAGS?=	-Wall -Wredundant-decls -Wnested-externs -Wstrict-prototypes \
 #
 
 #
+# On the i386, do not align the stack to 16-byte boundaries.  Otherwise GCC
+# 2.95 adds code to the entry and exit point of every function to align the
+# stack to 16-byte boundaries -- thus wasting approximately 12 bytes of stack
+# per function call.  While the 16-byte alignment may benefit micro benchmarks, 
+# it is probably an overall loose as it makes the code bigger (less efficient
+# use of code cache tag lines) and uses more stack (less efficient use of data
+# cache tag lines)
+#
+.if ${MACHINE_ARCH} == "i386"
+CFLAGS+=	-mpreferred-stack-boundary=2
+.endif
+
+#
 # On the alpha, make sure that we don't use floating-point registers and
 # allow the use of EV56 instructions (only needed for low-level i/o).
 #
