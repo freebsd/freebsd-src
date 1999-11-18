4e94045ef2f9070ac4e72d60f3a42d771a4e1224
alfred <alfred@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-11-18 08:57:03 +0000

    remove PCATCH from a tsleep that isn't checked for (avoid infinite loop).
    this needs to be revisited.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@53362 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/vinum/vinumrequest.c
+++ sys/dev/vinum/vinumrequest.c
@@ -392,7 +392,7 @@ launch_requests(struct request *rq, int reviveok)
 		drive = &DRIVE[rqe->driveno];		    /* look at drive */
 		while ((drive->active >= DRIVE_MAXACTIVE)   /* it has too much to do already, */
 		||(vinum_conf.active >= VINUM_MAXACTIVE))   /* or too many requests globally */
-		    tsleep(&launch_requests, PRIBIO | PCATCH, "vinbuf", 0); /* wait for it to subside */
+		    tsleep(&launch_requests, PRIBIO, "vinbuf", 0); /* wait for it to subside, XXX: should PCATCH */
 		drive->active++;
 		if (drive->active >= drive->maxactive)
 		    drive->maxactive = drive->active;
