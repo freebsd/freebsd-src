95da6829732952ab29ca558ea4a63516e7ca765b
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-11-19 21:34:50 +0000

    Use LIST_FOREACH to traverse the allproc list.
    Submitted by:   Jake Burkholder jake@checker.org
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@53433 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/amd64/amd64/pmap.c
+++ sys/amd64/amd64/pmap.c
@@ -1461,7 +1461,7 @@ pmap_growkernel(vm_offset_t addr)
 		newpdir = (pd_entry_t) (ptppaddr | PG_V | PG_RW | PG_A | PG_M);
 		pdir_pde(PTD, kernel_vm_end) = newpdir;
 
-		for (p = allproc.lh_first; p != 0; p = p->p_list.le_next) {
+		LIST_FOREACH(p, &allproc, p_list) {
 			if (p->p_vmspace) {
 				pmap = vmspace_pmap(p->p_vmspace);
 				*pmap_pde(pmap, kernel_vm_end) = newpdir;
@@ -3410,7 +3410,7 @@ pmap_pid_dump(int pid) {
 	struct proc *p;
 	int npte = 0;
 	int index;
-	for (p = allproc.lh_first; p != NULL; p = p->p_list.le_next) {
+	LIST_FOREACH(p, &allproc, p_list) {
 		if (p->p_pid != pid)
 			continue;
 
