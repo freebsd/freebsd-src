2179472c710a24833d795754a7d279628c76a145
gallatin <gallatin@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-11-22 14:30:41 +0000

    Allow a DMA channel of 0.  This gets the on-board mss audio device working on Digital AlphaStations 200 and 500 machines (and probably others as well).
    Submitted by: dfr
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@53585 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/isa/isahint.c
+++ sys/isa/isahint.c
@@ -70,7 +70,7 @@ isahint_add_device(device_t parent, const char *name, int unit)
 	if (resource_int_value(name, unit, "irq", &start) == 0 && start > 0)
 		bus_set_resource(child, SYS_RES_IRQ, 0, start, 1);
 
-	if (resource_int_value(name, unit, "drq", &start) == 0 && start > 0)
+	if (resource_int_value(name, unit, "drq", &start) == 0 && start >= 0)
 		bus_set_resource(child, SYS_RES_DRQ, 0, start, 1);
 
 	if (resource_int_value(name, unit, "flags", &t) == 0)
