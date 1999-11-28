548bfbf85525286754ef9cebc1c296373cc50807
roberto <roberto@FreeBSD.org>
1999-11-28 00:59:08 +0000

    MFC: Add ftp.nz.freebsd.org to the list of available FTP sites.
    Submitted by:	"Dan Langille" <dan@freebsddiary.org>
    Approved by:	jordan

--- release/sysinstall/menus.c
+++ release/sysinstall/menus.c
@@ -636,6 +636,8 @@ DMenu MenuMediaFTP = {
 	VAR_FTP_PATH _AP("=ftp://ftp4.kr.freebsd.org") },
       { "Korea #5",	"ftp5.kr.freebsd.org", NULL, dmenuSetVariable, NULL,
 	VAR_FTP_PATH _AP("=ftp://ftp5.kr.freebsd.org") },
+      { "New Zealand",	"ftp.nz.freebsd.org", NULL, dmenuSetVariable, NULL,
+	VAR_FTP_PATH _AP("=ftp://ftp.nz.freebsd.org") },
       { "Poland",	"ftp.pl.freebsd.org", NULL, dmenuSetVariable, NULL,
 	VAR_FTP_PATH _AP("=ftp://ftp.pl.freebsd.org") },
       { "Portugal",	"ftp.pt.freebsd.org", NULL, dmenuSetVariable, NULL,
