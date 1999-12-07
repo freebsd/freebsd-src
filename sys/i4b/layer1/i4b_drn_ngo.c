89f3d7021408003aa1e971810322ac2de613e590
peter <peter@FreeBSD.org>
1999-12-07 12:06:31 +0000

    Temporary adjustment to build after pnp changes.  There is a new version in the pipeline apparently.
    OK'ed by: hm

--- sys/i4b/layer1/i4b_drn_ngo.c
+++ sys/i4b/layer1/i4b_drn_ngo.c
@@ -37,14 +37,14 @@
 
 #include "isic.h"
 #include "opt_i4b.h"
-#include "pnp.h"
 
 #else
 
 #define	NISIC	1
-#define	NPNP	1
 
 #endif
+
+#define	NPNP	1
 
 #if (NISIC > 0) && (NPNP > 0) && defined(DRN_NGO)
 
