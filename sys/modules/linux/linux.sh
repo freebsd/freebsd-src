e5fb63d88144c7b708ef6b3a431ed468b240e1a1
cracauer <cracauer@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
1999-12-13 08:38:22 +0000

    Add a run of Linux ldconfig.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@54541 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/modules/linux/linux.sh
+++ sys/modules/linux/linux.sh
@@ -4,9 +4,18 @@
 
 FOUND=`kldstat -v | egrep 'linux(aout|elf)'`
 
+exitcode=0
+
 if [ "x$FOUND" != x ] ; then           
 	echo Linux driver already loaded
-	exit 1
+	exitcode=1
 else    
 	kldload linux                                                
+	exitcode=$?
 fi
+
+if [ -f /compat/linux/sbin/ldconfig ] ; then
+	/compat/linux/sbin/ldconfig
+fi
+
+exit $exitcode
