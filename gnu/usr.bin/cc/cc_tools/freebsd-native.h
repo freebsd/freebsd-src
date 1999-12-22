8ed9d0ffe7cca394e5e3b3ad1cc0c0e12a47f2b6
obrien <obrien@FreeBSD.org>
1999-12-22 05:00:28 +0000

    Reduce the FreeBSD custom hacks to gcc.c by using the GCC provided methods of changing the search dirs.  This also removes an used search dir, removes unneeded redundancy, and a bugus dir we enherited on the i386 by baseing off of svr4.h.
    We went from:
    	install: /usr/libexec/(null)
    	programs: /usr/libexec/<OBJFORMAT>/:/usr/libexec/:/usr/bin/:/usr/libexec/
    	libraries: /usr/libdata/gcc/:/usr/libexec/:/usr/ccs/lib/:/usr/lib/
    
    to:
    	install: /usr/libexec/(null)
    	programs: /usr/libexec/<OBJFORMAT>/:/usr/libexec/
    	libraries: /usr/libexec/:/usr/lib/

--- gnu/usr.bin/cc/cc_tools/freebsd-native.h
+++ gnu/usr.bin/cc/cc_tools/freebsd-native.h
@@ -4,36 +4,41 @@
    source tree so it can be configured appropriately without using
    the GNU configure/build mechanism. */
 
-/* Look for the include files in the system-defined places.  */
+#undef SYSTEM_INCLUDE_DIR		/* We don't need one for now. */
+#undef GCC_INCLUDE_DIR			/* We don't need one for now. */
+#undef TOOL_INCLUDE_DIR			/* We don't need one for now. */
+#undef LOCAL_INCLUDE_DIR		/* We don't wish to support one. */
 
+/* Look for the include files in the system-defined places.  */
 #define GPLUSPLUS_INCLUDE_DIR		"/usr/include/g++"
 #define GCC_INCLUDE_DIR			"/usr/include"
 
-/* Now that GCC knows what the include path applies to, put the G++ one first.
-   C++ can now have include files that override the default C ones.  */
-#undef INCLUDE_DEFAULTS
-#define INCLUDE_DEFAULTS			\
-  {						\
-    { GPLUSPLUS_INCLUDE_DIR, "C++", 1, 1 },	\
-    { GCC_INCLUDE_DIR, "GCC", 0, 0 },		\
-    { 0, 0, 0, 0 }				\
-  }
-
 /* Under FreeBSD, the normal location of the compiler back ends is the
-   /usr/libexec directory.  */
+   /usr/libexec directory.
+
+   ``cc --print-search-dirs'' gives:
+   install: STANDARD_EXEC_PREFIX/(null)
+   programs: /usr/libexec/<OBJFORMAT>/:MD_EXEC_PREFIX
+   libraries: MD_EXEC_PREFIX:MD_STARTFILE_PREFIX:STANDARD_STARTFILE_PREFIX
+
+   We really don't need a "STANDARD_EXEC_PREFIX".  However w/o it,
+   "--print-search-dirs" reports "install: /usr/local/lib/gcc-lib/(null)".
+   It is not harmful, but is just plain wrong.  So we define a
+   "STANDARD_EXEC_PREFIX" to not be misleading.  */
 
 #undef STANDARD_EXEC_PREFIX
-#undef TOOLDIR_BASE_PREFIX
+#undef TOOLDIR_BASE_PREFIX		/* Old??  This is not documented. */
 #undef MD_EXEC_PREFIX
 
 #define STANDARD_EXEC_PREFIX		"/usr/libexec/"
-#define TOOLDIR_BASE_PREFIX		"/usr/libexec/"
 #define MD_EXEC_PREFIX			"/usr/libexec/"
 
 /* Under FreeBSD, the normal location of the various *crt*.o files is the
    /usr/lib directory.  */
 
 #undef STANDARD_STARTFILE_PREFIX
+#undef MD_STARTFILE_PREFIX		/* We don't need one for now. */
+
 #define STANDARD_STARTFILE_PREFIX	"/usr/lib/"
 
 /* FreeBSD is 4.4BSD derived */
