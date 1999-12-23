1ff26c090495a1c08327cfb1f31c95a0e741f0a6
marcel <marcel@FreeBSD.org>
1999-12-23 21:19:32 +0000

    Use genassym(1) and <sys/assym.h> to generate assembler symbols.

--- sys/i386/svr4/svr4_genassym.c
+++ sys/i386/svr4/svr4_genassym.c
@@ -1,6 +1,7 @@
 /* $FreeBSD$ */
 /* Derived from:  Id: linux_genassym.c,v 1.8 1998/07/29 15:50:41 bde Exp */
 
+#include <sys/assym.h>
 #include <sys/param.h>
 
 struct proc;
@@ -14,23 +15,8 @@ struct proc;
 #define SVR4_MACHDEP_JUST_REGS
 #include <i386/svr4/svr4_machdep.h>
 
-#define	offsetof(type, member)	((size_t)(&((type *)0)->member))
-#define	OS(s, m)	((u_int)offsetof(struct s, m))
-
-int	main __P((void));
-int	printf __P((const char *, ...));
-
-int
-main()
-{
-	printf("#define\tSVR4_SIGF_HANDLER %u\n",
-	    OS(svr4_sigframe, sf_handler));
-	printf("#define\tSVR4_SIGF_UC %u\n", OS(svr4_sigframe, sf_uc));
-	printf("#define\tSVR4_UC_FS %u\n",
-			OS(svr4_ucontext, uc_mcontext.greg[SVR4_X86_FS]));
-	printf("#define\tSVR4_UC_GS %u\n",
-			OS(svr4_ucontext, uc_mcontext.greg[SVR4_X86_GS]));
-	printf("#define\tSVR4_UC_EFLAGS %u\n",
-			OS(svr4_ucontext, uc_mcontext.greg[SVR4_X86_EFL]));
-	return (0);
-}
+ASSYM(SVR4_SIGF_HANDLER, offsetof(svr4_sigframe, sf_handler));
+ASSYM(SVR4_SIGF_UC, offsetof(svr4_sigframe, sf_uc));
+ASSYM(SVR4_UC_FS, offsetof(svr4_ucontext, uc_mcontext.greg[SVR4_X86_FS]));
+ASSYM(SVR4_UC_GS, offsetof(svr4_ucontext, uc_mcontext.greg[SVR4_X86_GS]));
+ASSYM(SVR4_UC_EFLAGS, offsetof(svr4_ucontext, uc_mcontext.greg[SVR4_X86_EFL]));
