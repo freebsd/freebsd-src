6628432ceee00c8c0058166efc4903087dcb0ced
jkh <jkh@FreeBSD.org>
1999-12-25 07:22:10 +0000

    Sacrifice, for now, NFS and NETGRAPH on the alpha boot floppy so that we can get it back onto 1.44MB boot media again.  This does not affect the i386 class build.

--- release/i386/dokern.sh
+++ release/i386/dokern.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+ARCH="`uname -m`"
+
+# First check for the standard x86 PC class.
+if [ "${ARCH}" = "i386" ]; then
+
 sed	-e '/pty/d' \
 	-e '/pass0/d' \
 	-e '/apm0/d' \
@@ -12,12 +17,30 @@ sed	-e '/pty/d' \
 	-e '/maxusers/d' \
 	-e 's/GENERIC/BOOTMFS/g'
 
+echo "options  NETGRAPH"
+echo "options  NETGRAPH_PPPOE"
+echo "options  NETGRAPH_SOCKET"
+
+# Otherwise maybe it's an alpha, and it has big binaries.
+elif [ "${ARCH}" = "alpha" ]; then
+
+sed	-e '/pty/d' \
+	-e '/pass0/d' \
+	-e '/apm0/d' \
+	-e '/ppp/d' \
+	-e '/gzip/d' \
+	-e '/splash/d' \
+	-e '/PROCFS/d' \
+	-e '/KTRACE/d' \
+	-e '/SYSV/d' \
+	-e '/NFS/d' \
+	-e '/maxusers/d' \
+	-e 's/GENERIC/BOOTMFS/g'
+fi
+
 # reset maxusers to something lower
 echo "maxusers	5"
 
 echo "options  NFS_NOSERVER" 
 echo "options  SCSI_NO_OP_STRINGS" 
 echo "options  SCSI_NO_SENSE_STRINGS"
-echo "options  NETGRAPH"
-echo "options  NETGRAPH_PPPOE"
-echo "options  NETGRAPH_SOCKET"
