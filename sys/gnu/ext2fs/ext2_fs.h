a5e8250f8cd3d7d14638c62ec2b7337f4a4b34da
bde <bde@FreeBSD.org>
2000-01-01 14:43:20 +0000

    Use an ifdef in ext2_fs.h instead of a bogus separate file (ext2_fs_i.h) to avoid the namespace problems caused by <ufs/ufs/inode.h> #defining i_mode, etc.
    ext2_fs_i.h had nothing to do with the Linux version.  It was a small
    part of the Linux version of ext2_fs.h (the part that declares extra
    in-core fields for an inode).  We don't need it because we use the
    ufs in-core inode for the extra fields.

--- sys/gnu/ext2fs/ext2_fs.h
+++ sys/gnu/ext2fs/ext2_fs.h
@@ -3,6 +3,8 @@
  *
  *  Aug 1995, Godmar Back (gback@cs.utah.edu)
  *  University of Utah, Department of Computer Science
+ *
+ * $FreeBSD$
  */
 /*
  *  linux/include/linux/ext2_fs.h
@@ -225,6 +227,71 @@ struct ext2_group_desc
 #define	EXT2_IOC_SETFLAGS		_IOW('f', 2, long)
 #define	EXT2_IOC_GETVERSION		_IOR('v', 1, long)
 #define	EXT2_IOC_SETVERSION		_IOW('v', 2, long)
+
+/*
+ * Only declare `struct ext2_inode' if <ufs/ufs/inode.h> hasn't made things
+ * difficult by #defining i_mode and other struct members.  The details of
+ * the struct are only needed in ext2_inode_cnv.c where the ext2fs on-disk
+ * inode is converted to a ufs in-core inode.
+ */
+#ifndef i_mode
+
+/*
+ * Structure of an inode on the disk
+ */
+struct ext2_inode {
+        __u16   i_mode;         /* File mode */
+        __u16   i_uid;          /* Owner Uid */
+        __u32   i_size;         /* Size in bytes */
+        __u32   i_atime;        /* Access time */
+        __u32   i_ctime;        /* Creation time */
+        __u32   i_mtime;        /* Modification time */
+        __u32   i_dtime;        /* Deletion Time */
+        __u16   i_gid;          /* Group Id */
+        __u16   i_links_count;  /* Links count */
+        __u32   i_blocks;       /* Blocks count */
+        __u32   i_flags;        /* File flags */
+        union {
+                struct {
+                        __u32  l_i_reserved1;
+                } linux1;
+                struct {
+                        __u32  h_i_translator;
+                } hurd1;
+                struct {
+                        __u32  m_i_reserved1;
+                } masix1;
+        } osd1;                         /* OS dependent 1 */
+        __u32   i_block[EXT2_N_BLOCKS];/* Pointers to blocks */
+        __u32   i_version;      /* File version (for NFS) */
+        __u32   i_file_acl;     /* File ACL */
+        __u32   i_dir_acl;      /* Directory ACL */
+        __u32   i_faddr;        /* Fragment address */
+        union {
+                struct {
+                        __u8    l_i_frag;       /* Fragment number */
+                        __u8    l_i_fsize;      /* Fragment size */
+                        __u16   i_pad1;
+                        __u32   l_i_reserved2[2];
+                } linux2;
+                struct {
+                        __u8    h_i_frag;       /* Fragment number */
+                        __u8    h_i_fsize;      /* Fragment size */
+                        __u16   h_i_mode_high;
+                        __u16   h_i_uid_high;
+                        __u16   h_i_gid_high;
+                        __u32   h_i_author;
+                } hurd2;
+                struct {
+                        __u8    m_i_frag;       /* Fragment number */
+                        __u8    m_i_fsize;      /* Fragment size */
+                        __u16   m_pad1;
+                        __u32   m_i_reserved2[2];
+                } masix2;
+        } osd2;                         /* OS dependent 2 */
+};
+
+#endif /* i_mode */
 
 /*
  * File system states
