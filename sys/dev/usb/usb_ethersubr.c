a0067d7b89525cd8ed446d5e5c157514d043f74a
Bill Paul <wpaul@FreeBSD.org>
2000-01-10 23:12:54 +0000

    Attempt to fix a problem with receiving packets on USB ethernet interfaces. Packets are received inside USB bulk transfer callbacks, which run at splusb() (actually splbio()). The packet input queues are meant to be manipulated at splimp(). However the locking apparently breaks down under certain circumstances and the input queues can get trampled.
    There's a similar problem with if_ppp, which is driven by hardware/tty
    interrupts from the serial driver, but which must also manipulate the
    packet input queues at splimp(). The fix there is to use a netisr, and
    that's the fix I used here. (I can hear you groaning back there. Hush up.)
    
    The usb_ethersubr module maintains a single queue of its own. When a
    packet is received in the USB callback routine, it's placed on this
    queue with usb_ether_input(). This routine also schedules a soft net
    interrupt with schednetisr(). The ISR routine then runs later, at
    splnet, outside of the USB callback/interrupt context, and passes the
    packet to ether_input(), hopefully in a safe manner.
    
    The reason this is implemented as a separate module is that there are
    a limited number of NETISRs that we can use, and snarfing one up for
    each driver that needs it is wasteful (there will be three once I get
    the CATC driver done). It also reduces code duplication to a certain
    small extent. Unfortunately, it also needs to be linked in with the
    usb.ko module in order for the USB ethernet drivers to share it.
    
    Also removed some uneeded includes from if_aue.c and if_kue.c
    
    Fix suggested by: peter
    Not rejected as a hairbrained idea by: n_hibma

---
+++ sys/dev/usb/usb_ethersubr.c
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 1997, 1998, 1999, 2000
+ *	Bill Paul <wpaul@ee.columbia.edu>.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Bill Paul.
+ * 4. Neither the name of the author nor the names of any co-contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+/*
+ * Callbacks in the USB code operate at splusb() (actually splbio()
+ * in FreeBSD). However adding packets to the input queues has to be
+ * done at splimp(). It is conceivable that this arrangement could
+ * trigger a condition where the splimp() is ignored and the input
+ * queues could get trampled in spite of our best effors to prevent
+ * it. To work around this, we implement a special input queue for
+ * USB ethernet adapter drivers. Rather than passing the frames directly
+ * to ether_input(), we pass them here, then schedule a soft interrupt
+ * to hand them to ether_input() later, outside of the USB interrupt
+ * context.
+ *
+ * It's questional as to whether this code should be expanded to
+ * handle other kinds of devices, or handle USB transfer callbacks
+ * in general. Right now, I need USB network interfaces to work
+ * properly.
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/sockio.h>
+#include <sys/mbuf.h>
+#include <sys/kernel.h>
+#include <sys/socket.h>
+
+#include <net/if.h>
+#include <net/if_types.h>
+#include <net/if_arp.h>
+#include <net/ethernet.h>
+#include <net/netisr.h>
+
+#include <dev/usb/usb_ethersubr.h>
+
+#ifndef lint
+static const char rcsid[] =
+  "$FreeBSD$";
+#endif
+
+static struct ifqueue usbq;
+
+static void usbintr		__P((void));
+
+static void usbintr()
+{
+	struct ether_header	*eh;
+	struct mbuf		*m;
+	int			s;
+
+	s = splimp();
+
+	while(1) {
+		IF_DEQUEUE(&usbq, m);
+		if (m == NULL)
+			break;
+		eh = mtod(m, struct ether_header *);
+		m_adj(m, sizeof(struct ether_header));
+		ether_input(m->m_pkthdr.rcvif, eh, m);
+	}
+
+	splx(s);
+
+	return;
+}
+
+void usb_register_netisr()
+{
+	register_netisr(NETISR_USB, usbintr);
+	return;
+}
+
+/*
+ * Must be called at splusp() (actually splbio()). This should be
+ * the case when called from a transfer callback routine.
+ */
+void usb_ether_input(m)
+	struct mbuf		*m;
+{
+	int			s;
+	s = splimp();
+	IF_ENQUEUE(&usbq, m);
+	schednetisr(NETISR_USB);
+	splx(s);
+	return;
+}
