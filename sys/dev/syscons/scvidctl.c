f28b294bf4af31fc177de8f9137b154a4de2a894
yokota <yokota@FreeBSD.org>
2000-01-20 13:21:47 +0000

    Fix wrong usage of FONT_NONE.  It was not meant to be set in scp->font_size in the first place.  It is redundant now and is removed.
    Found by: bde

--- sys/dev/syscons/scvidctl.c
+++ sys/dev/syscons/scvidctl.c
@@ -273,7 +273,7 @@ sc_set_graphics_mode(scr_stat *scp, struct tty *tp, int mode)
     scp->xpixel = info.vi_width;
     scp->ypixel = info.vi_height;
     scp->font = NULL;
-    scp->font_size = FONT_NONE;
+    scp->font_size = 0;
 #ifndef SC_NO_SYSMOUSE
     /* move the mouse cursor at the center of the screen */
     sc_mouse_move(scp, scp->xpixel / 2, scp->ypixel / 2);
@@ -316,7 +316,7 @@ sc_set_pixel_mode(scr_stat *scp, struct tty *tp, int xsize, int ysize,
 	return ENODEV;		/* this shouldn't happen */
 
     /* adjust argument values */
-    if ((fontsize <= 0) || (fontsize == FONT_NONE))
+    if (fontsize <= 0)
 	fontsize = info.vi_cheight;
     if (fontsize < 14) {
 	fontsize = 8;
