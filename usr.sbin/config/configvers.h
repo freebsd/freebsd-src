d67724c7c9ab9bde86a7a962e17cd2d1bf205306
Philippe Charnier <charnier@FreeBSD.org>
2000-01-23 20:16:21 +0000

    Use Ns and Sx. -current is 4 not 3.

--- usr.sbin/config/configvers.h
+++ usr.sbin/config/configvers.h
@@ -1,7 +1,7 @@
 /*
  * 6 digits of version.  The most significant are branch indicators
- * (eg: RELENG_2_2 = 22, -current presently = 30 etc). The least
- * significant digits are incremented for each incompatable change.
+ * (eg: RELENG_2_2 = 22, -current presently = 40 etc). The least
+ * significant digits are incremented for each incompatible change.
  *
  * The numbering scheme is inspired by the sys/conf/newvers.sh RELDATE
  * and <osreldate.h> system.
