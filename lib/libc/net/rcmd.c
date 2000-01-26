ae42b66633602eca51aff154ecfab3cc9ad8ae02
Yoshinobu Inoue <shin@FreeBSD.org>
2000-01-26 14:13:41 +0000

    Removed 3rd arg from bindresvport_sa() call, because the 3rd arg have been already removed.
    Specified by: Philipp Mergenthaler <un1i@rz.uni-karlsruhe.de>

--- lib/libc/net/rcmd.c
+++ lib/libc/net/rcmd.c
@@ -333,8 +333,7 @@ rresvport_af(alport, family)
 	}
 #endif
 	*sport = 0;
-	if (bindresvport_sa(s, (struct sockaddr *)&ss,
-			    ((struct sockaddr *)&ss)->sa_len) == -1) {
+	if (bindresvport_sa(s, (struct sockaddr *)&ss) == -1) {
 		(void)_libc_close(s);
 		return (-1);
 	}
