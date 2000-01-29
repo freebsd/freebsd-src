1c6f64061cc309aaee134a49f229372522ae4fe9
Peter Wemm <peter@FreeBSD.org>
2000-01-29 18:42:45 +0000

    Remove #if NDGB > 0 and #if NDGM > 0

--- sys/gnu/i386/isa/dgm.c
+++ sys/gnu/i386/isa/dgm.c
@@ -35,9 +35,6 @@
 
 #include "dgm.h"
 
-#if NDGM > 0 
-
-
 /* Helg: i.e.25 times per sec board will be polled */
 #define POLLSPERSEC 25
 /* How many charactes can we write to input tty rawq */
@@ -2003,5 +2000,3 @@ disc_optim(tp, t)
 	else
 		tp->t_state &= ~TS_CAN_BYPASS_L_RINT;
 }
-
-#endif /* NDGM > 0 */
