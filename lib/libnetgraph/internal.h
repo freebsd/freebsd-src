2d29c2dcb61b68b32b56de6dbf86e74b629dcaf8
Archie Cobbs <archie@FreeBSD.org>
2000-02-01 01:32:33 +0000

    MFC: load the socket node type KLD if it's not already loaded

--- lib/libnetgraph/internal.h
+++ lib/libnetgraph/internal.h
@@ -41,9 +41,11 @@
  */
 
 #include <sys/types.h>
+#include <sys/param.h>
 #include <sys/time.h>
 #include <sys/socket.h>
 #include <sys/select.h>
+#include <sys/linker.h>
 
 #include <stdlib.h>
 #include <stdio.h>
