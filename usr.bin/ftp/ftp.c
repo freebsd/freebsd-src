b2bfbd50c8dba95453d45f00b452ad9664b60bc5
shin <shin@FreeBSD.org>
2000-02-15 15:11:40 +0000

    Add -4 and -6 options.
     Current getaddrinfo() implemetation has a problem of too much resolving
     waiting time on INET6 enabled systems.
     -4 and -6 options can limit name resolving address family and is a possible
     workaround for the problem.
    
    Approved by: jkh

--- usr.bin/ftp/ftp.c
+++ usr.bin/ftp/ftp.c
@@ -121,7 +121,7 @@ hookup(host0, port)
 	host = hostnamebuf;
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_flags = AI_CANONNAME;
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family = family;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = 0;
 	error = getaddrinfo(host, port, &hints, &res0);
