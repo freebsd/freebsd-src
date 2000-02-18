db9ac4c0e59c3a57697f27ab0379886120c00857
Alfred Perlstein <alfred@FreeBSD.org>
2000-02-18 23:45:04 +0000

    MFC: 1.8 (re-add the month may)

--- bin/date/vary.c
+++ bin/date/vary.c
@@ -41,8 +41,8 @@ struct trans {
 
 static struct trans trans_mon[] = {
   { 1, "january" }, { 2, "february" }, { 3, "march" }, { 4, "april" },
-  { 6, "june" }, { 7, "july" }, { 8, "august" }, { 9, "september" },
-  { 10, "october" }, { 11, "november" }, { 12, "december" },
+  { 5, "may" }, { 6, "june" }, { 7, "july" }, { 8, "august" },
+  { 9, "september" }, { 10, "october" }, { 11, "november" }, { 12, "december" },
   { -1, NULL }
 };
 
