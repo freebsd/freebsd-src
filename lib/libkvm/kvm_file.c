e1e39bf64030ea7fb3f7066ccc30f59457858790
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-02-18 16:39:00 +0000

    Correct an error message presumably as a result of cut/paste. kvm_getfiles() referred to itself as kvm_getprocs().
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@57321 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libkvm/kvm_file.c
+++ lib/libkvm/kvm_file.c
@@ -138,7 +138,7 @@ kvm_getfiles(kd, op, arg, cnt)
 		mib[1] = KERN_FILE;
 		st = sysctl(mib, 2, NULL, &size, NULL, 0);
 		if (st == -1) {
-			_kvm_syserr(kd, kd->program, "kvm_getprocs");
+			_kvm_syserr(kd, kd->program, "kvm_getfiles");
 			return (0);
 		}
 		if (kd->argspc == 0)
