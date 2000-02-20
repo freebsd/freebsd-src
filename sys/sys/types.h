04f42bfc9784329d11c222da303263263249d792
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-02-20 13:36:26 +0000

    Fix select(2) for the Alpha. (!!)  It was never returning true for fd's in the range of 32-63, 96-127 etc.  The first problem was the FD_*() macros were shifting a 32 bit integer "1" left by more than 32 bits.  The same problem happened in selscan().  ffs() also takes an int argument and causes failure.  For cases where int == long (ie: the usual case for x86, but not always as gcc can have long being a 64 bit quantity) ffs() could be used.
    Reported by:	Marian Stagarescu <marian@bile.skycache.com>
    Reviewed by:	dfr, gallatin (sys/types.h only)
    Approved by:	jkh
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@57357 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/types.h
+++ sys/sys/types.h
@@ -178,9 +178,10 @@ typedef	struct fd_set {
 	fd_mask	fds_bits[howmany(FD_SETSIZE, NFDBITS)];
 } fd_set;
 
-#define	FD_SET(n, p)	((p)->fds_bits[(n)/NFDBITS] |= (1 << ((n) % NFDBITS)))
-#define	FD_CLR(n, p)	((p)->fds_bits[(n)/NFDBITS] &= ~(1 << ((n) % NFDBITS)))
-#define	FD_ISSET(n, p)	((p)->fds_bits[(n)/NFDBITS] & (1 << ((n) % NFDBITS)))
+#define	_fdset_mask(n)	((fd_mask)1 << ((n) % NFDBITS))
+#define	FD_SET(n, p)	((p)->fds_bits[(n)/NFDBITS] |= _fdset_mask(n))
+#define	FD_CLR(n, p)	((p)->fds_bits[(n)/NFDBITS] &= ~_fdset_mask(n))
+#define	FD_ISSET(n, p)	((p)->fds_bits[(n)/NFDBITS] & _fdset_mask(n))
 #define	FD_COPY(f, t)	bcopy(f, t, sizeof(*(f)))
 #define	FD_ZERO(p)	bzero(p, sizeof(*(p)))
 
