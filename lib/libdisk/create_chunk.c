af76e7b9bcaf7ae8ca96282b53656faf8bb99dfa
jlemon <jlemon@FreeBSD.org>
2000-03-09 15:05:20 +0000

    Fix pointer addition bug (3 -> 4).
    Submitted by:	 david.w.james@bt.com

--- lib/libdisk/create_chunk.c
+++ lib/libdisk/create_chunk.c
@@ -268,7 +268,7 @@ MakeDev(struct chunk *c1, const char *path)
     else if (!strncmp(p, "fla", 3))
 	cmaj = 102, p += 3;
     else if (!strncmp(p, "idad", 4))
-	cmaj = 109, p += 3;
+	cmaj = 109, p += 4;
     else if (!strncmp(p, "mlxd", 4))
 	cmaj = 131, p += 4;
     else if (!strncmp(p, "amrd", 4))
