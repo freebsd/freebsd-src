bca85718e8c285c179ece526dbff4e6c6bddb8cb
shin <shin@FreeBSD.org>
2000-03-11 20:44:53 +0000

    Forbid include of netinet6/ip6.h from user-land, and if included, print an error message which say, "include netinet/ip6.h". This is postponed to apply to avoid tcpdump compile error. Now apply this because tcpdump has been already fixed.
    Approved by: jkh
    
    Obtained from: KAME project

--- sys/netinet6/ip6.h
+++ sys/netinet6/ip6.h
@@ -68,9 +68,7 @@
 #define _NETINET6_IPV6_H_
 
 #if !defined(_KERNEL) && !defined(__KAME_NETINET_IP6_H_INCLUDED_)
-#if 0 /* XXX: waiting for contrib/addrtoname.c fix */
 #error "do not include netinet6/ip6.h directly, include netinet/ip6.h"
-#endif
 #endif
 
 /*
