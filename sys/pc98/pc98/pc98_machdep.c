55d4f1b7fac57cc1aa24e9df495f51c96b8a4163
nyan <nyan@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-03-16 12:14:00 +0000

    Fixed to probe extended memory for over 256M or under 64M.
    Submitted by:   chi@bd.mbn.or.jp (Chiharu Shibata)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@58145 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/pc98/pc98/pc98_machdep.c
+++ sys/pc98/pc98/pc98_machdep.c
@@ -41,13 +41,6 @@
 
 #include <pc98/pc98/pc98_machdep.h>
 
-extern	int Maxmem;
-extern	int Maxmem_under16M;
-
-#ifdef notyet
-static	void init_cpu_accel_mem __P((void));
-#endif
-
 /*
  * Initialize DMA controller
  */
@@ -74,130 +67,50 @@ static	void init_epson_memwin __P((void));
 static void
 init_epson_memwin(void)
 {
+	/* Disable 15MB-16MB caching. */
+	switch (epson_machine_id) {
+	case 0x34:	/* PC486HX */
+	case 0x35:	/* PC486HG */
+	case 0x3B:	/* PC486HA */
+		/* Cache control start. */
+		outb(0x43f, 0x42);
+		outw(0xc40, 0x0033);
 
-	if (pc98_machine_type & M_EPSON_PC98) {
-		if (Maxmem > 3840) {
-			if (Maxmem == Maxmem_under16M) {
-				Maxmem = 3840;
-				Maxmem_under16M = 3840;
-			} else if (Maxmem_under16M > 3840) {
-				Maxmem_under16M = 3840;
-			}
-		}
+		/* Disable 0xF00000-0xFFFFFF. */
+		outb(0xc48, 0x49);
+		outb(0xc4c, 0x00);
+		outb(0xc48, 0x48);
+		outb(0xc4c, 0xf0);
+		outb(0xc48, 0x4d);
+		outb(0xc4c, 0x00);
+		outb(0xc48, 0x4c);
+		outb(0xc4c, 0xff);
+		outb(0xc48, 0x4f);
+		outb(0xc4c, 0x00);
 
-		/* Disable 15MB-16MB caching. */
-		switch (epson_machine_id) {
-		case 0x34:	/* PC486HX */
-		case 0x35:	/* PC486HG */
-		case 0x3B:	/* PC486HA */
-			/* Cache control start. */
-			outb(0x43f, 0x42);
-			outw(0xc40, 0x0033);
+		/* Cache control end. */
+		outb(0x43f, 0x40);
+		break;
 
-			/* Disable 0xF00000-0xFFFFFF. */
-			outb(0xc48, 0x49);
-			outb(0xc4c, 0x00);
-			outb(0xc48, 0x48);
-			outb(0xc4c, 0xf0);
-			outb(0xc48, 0x4d);
-			outb(0xc4c, 0x00);
-			outb(0xc48, 0x4c);
-			outb(0xc4c, 0xff);
-			outb(0xc48, 0x4f);
-			outb(0xc4c, 0x00);
+	case 0x2B:	/* PC486GR/GF */
+	case 0x30:	/* PC486P */
+	case 0x31:	/* PC486GRSuper */
+	case 0x32:	/* PC486GR+ */
+	case 0x37:	/* PC486SE */
+	case 0x38:	/* PC486SR */
+		/* Disable 0xF00000-0xFFFFFF. */
+		outb(0x43f, 0x42);
+		outb(0x467, 0xe0);
+		outb(0x567, 0xd8);
 
-			/* Cache control end. */
-			outb(0x43f, 0x40);
-			break;
-
-		case 0x2B:	/* PC486GR/GF */
-		case 0x30:	/* PC486P */
-		case 0x31:	/* PC486GRSuper */
-		case 0x32:	/* PC486GR+ */
-		case 0x37:	/* PC486SE */
-		case 0x38:	/* PC486SR */
-			/* Disable 0xF00000-0xFFFFFF. */
-			outb(0x43f, 0x42);
-			outb(0x467, 0xe0);
-			outb(0x567, 0xd8);
-
-			outb(0x43f, 0x40);
-			outb(0x467, 0xe0);
-			outb(0x567, 0xe0);
-			break;
-		}
-
-		/* Disable 15MB-16MB RAM and enable memory window. */
-		outb(0x43b, inb(0x43b) & 0xfd);	/* Clear bit1. */
+		outb(0x43f, 0x40);
+		outb(0x467, 0xe0);
+		outb(0x567, 0xe0);
+		break;
 	}
-}
-#endif
 
-#ifdef notyet
-static	void init_cpu_accel_mem(void);
-
-static void
-init_cpu_accel_mem(void)
-{
-	u_int target_page;
-	/*
-	 * Certain 'CPU accelerator' supports over 16MB memory on
-	 * the machines whose BIOS doesn't store true size.  
-	 * To support this, we don't trust BIOS values if Maxmem < 4096.
-	 */
-	if (Maxmem < 4096) {
-		for (target_page = ptoa(4096);		/* 16MB */
-			 target_page < ptoa(32768);		/* 128MB */
-			 target_page += 256 * PAGE_SIZE	/* 1MB step */) {
-			u_int tmp, page_bad = FALSE, OrigMaxmem = Maxmem;
-
-			*(int *)CMAP1 = PG_V | PG_RW | PG_N | target_page;
-			invltlb();
-
-			tmp = *(u_int *)CADDR1;
-			/*
-			 * Test for alternating 1's and 0's
-			 */
-			*(volatile u_int *)CADDR1 = 0xaaaaaaaa;
-			if (*(volatile u_int *)CADDR1 != 0xaaaaaaaa) {
-				page_bad = TRUE;
-			}
-			/*
-			 * Test for alternating 0's and 1's
-			 */
-			*(volatile u_int *)CADDR1 = 0x55555555;
-			if (*(volatile u_int *)CADDR1 != 0x55555555) {
-				page_bad = TRUE;
-			}
-			/*
-			 * Test for all 1's
-			 */
-			*(volatile u_int *)CADDR1 = 0xffffffff;
-			if (*(volatile u_int *)CADDR1 != 0xffffffff) {
-				page_bad = TRUE;
-			}
-			/*
-			 * Test for all 0's
-			 */
-			*(volatile u_int *)CADDR1 = 0x0;
-			if (*(volatile u_int *)CADDR1 != 0x0) {
-				/*
-				 * test of page failed
-				 */
-				page_bad = TRUE;
-			}
-			/*
-			 * Restore original value.
-			 */
-			*(u_int *)CADDR1 = tmp;
-			if (page_bad == TRUE) {
-				Maxmem = atop(target_page) + 256;
-			} else 
-				break;
-		}
-		*(int *)CMAP1 = 0;
-		invltlb();
-	}
+	/* Disable 15MB-16MB RAM and enable memory window. */
+	outb(0x43b, inb(0x43b) & 0xfd);	/* Clear bit1. */
 }
 #endif
 
@@ -205,22 +118,33 @@ init_cpu_accel_mem(void)
  * Get physical memory size
  */
 void
-pc98_getmemsize(void)
+pc98_getmemsize(unsigned *base, unsigned *ext, unsigned *under16)
 {
-	unsigned char under16, over16;
+	unsigned int over16;
 
-	/* available protected memory size under 16MB / 128KB */
-	under16 = PC98_SYSTEM_PARAMETER(0x401);
-	/* available protected memory size over 16MB / 1MB */
-	over16 = PC98_SYSTEM_PARAMETER(0x594);
-	/* add conventional memory size (1024KB / 128KB = 8) */
-	under16 += 8;
+	/* available conventional memory size */
+	*base = ((PC98_SYSTEM_PARAMETER(0x501) & 7) + 1) * 128;
 
-	Maxmem = Maxmem_under16M = under16 * 128 * 1024 / PAGE_SIZE;
-	Maxmem += (over16 * 1024 * 1024 / PAGE_SIZE);
+	/* available protected memory size under 16MB */
+	*under16 = PC98_SYSTEM_PARAMETER(0x401) * 128 + 1024;
 #ifdef EPSON_MEMWIN
-	init_epson_memwin();
+	if (pc98_machine_type & M_EPSON_PC98) {
+		if (*under16 > (15 * 1024)) {
+			/* chop under16 memory to 15MB */
+			*under16 = 15 * 1024;
+		}
+		init_epson_memwin();
+	}
 #endif
+
+	/* available protected memory size over 16MB / 1MB */
+	over16  = PC98_SYSTEM_PARAMETER(0x594);
+	over16 += PC98_SYSTEM_PARAMETER(0x595) * 256;
+
+	*ext = *under16;
+	if (over16 > 0) {
+		*ext = (16 + over16) * 1024;
+	}
 }
 
 #include "da.h"
