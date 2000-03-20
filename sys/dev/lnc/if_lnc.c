7e7f4d9be935d9208c041fce5228add3f6c71825
paul <paul@FreeBSD.org>
2000-03-20 00:53:46 +0000

    Update the copyright. Remove the advertising clause.

--- sys/dev/lnc/if_lnc.c
+++ sys/dev/lnc/if_lnc.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 1994-1998
+ * Copyright (c) 1994-2000
  *	Paul Richards.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,10 +12,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Paul Richards.
- * 4. The name Paul Richards may not be used to endorse or promote products
+ * 3. The name Paul Richards may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY PAUL RICHARDS ``AS IS'' AND
