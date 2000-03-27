5c16c2a8f10491c18e04caf839491a5a2ec6320c
charnier <charnier@FreeBSD.org>
2000-03-27 20:41:17 +0000

    Revert spelling mistake I made in the previous commit Requested by: Alan and Bruce

--- sys/vm/default_pager.c
+++ sys/vm/default_pager.c
@@ -11,7 +11,7 @@
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgment:
+ *    must display the following acknowledgement:
  *	This product includes software developed by David Greenman.
  * 4. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
