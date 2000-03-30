0a57de884057400b555802b47c795e50ac659013
peter <peter@FreeBSD.org>
2000-03-30 06:44:50 +0000

    Try and make the Alpha kernel compile.  There are still some loose ends (eg: common declarations in includes that I think are causing gensetdefs  warnings) that need to be tied up, but it compiles and runs.

--- sys/alpha/include/cpu.h
+++ sys/alpha/include/cpu.h
@@ -92,7 +92,6 @@ struct clockframe {
 #define	aston()		(astpending = 1)
 
 #ifdef _KERNEL
-u_int32_t astpending;		/* need to trap before returning to user mode */
 u_int32_t intr_nesting_level;	/* bookeeping only; counts software intr */
 u_int32_t want_resched;		/* resched() was called */
 #endif
