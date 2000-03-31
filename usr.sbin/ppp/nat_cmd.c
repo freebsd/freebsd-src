4389d5ff8ecddee1fbdba3159bf77f219c788dfb
brian <brian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-03-31 14:26:23 +0000

    Add the ``nat target'' command.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@58867 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/ppp/nat_cmd.c
+++ usr.sbin/ppp/nat_cmd.c
@@ -339,6 +339,30 @@ nat_Pptp(struct cmdargs const *arg)
   return 0;
 }
 
+int
+nat_SetTarget(struct cmdargs const *arg)
+{
+  struct in_addr addr;
+
+  if (arg->argc == arg->argn) {
+    addr.s_addr = INADDR_ANY;
+    PacketAliasSetTarget(addr);
+    return 0;
+  }
+
+  if (arg->argc != arg->argn + 1)
+    return -1;
+
+  addr = GetIpAddr(arg->argv[arg->argn]);
+  if (addr.s_addr == INADDR_NONE) {
+    log_Printf(LogWARN, "%s: invalid address\n", arg->argv[arg->argn]);
+    return 1;
+  }
+
+  PacketAliasSetTarget(addr);
+  return 0;
+}
+
 static struct mbuf *
 nat_LayerPush(struct bundle *bundle, struct link *l, struct mbuf *bp,
                 int pri, u_short *proto)
