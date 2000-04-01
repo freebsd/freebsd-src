52e09355c7c33342b9a30f19f1b435aa9e6580ea
n_hibma <n_hibma@FreeBSD.org>
2000-04-01 19:13:20 +0000

    Correct my e-mail address. The skylink one is no longer working.

--- sys/dev/usb/usb_if.m
+++ sys/dev/usb/usb_if.m
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 1992-1998 Nick Hibma <hibma@skylink.it>
+# Copyright (c) 1992-1998 Nick Hibma <n_hibma@freebsd.org>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
