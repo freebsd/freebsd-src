14033363bfeba2294019c5e62796a457e92ec502
mdodd <mdodd@FreeBSD.org>
2000-04-07 02:50:24 +0000

    - Convert dpt_pci.c to newbus. - Add support for ISA based DPT adapters (this doesn't quite work yet). - Sync dpt_eisa.c with my local copy.   - Simplify how EISA IDs are matched.   - Prototype.   - Formatting nits.   - Conform to how I do things in dpt_pci.c/dpt_isa.c. - Modify dpt_scsi.c:dpt_alloc() to DTRT with newbus. - Add some comments to dpt_scsi.c:dpt_pio_get_conf(). - Add additional check to dpt_scsi.c:dpt_get_conf(). - Add some useful error messages to dpt_scsi.c:dpt_init().

--- sys/dev/dpt/dpt_isa.c
+++ sys/dev/dpt/dpt_isa.c
@@ -2,9 +2,6 @@
  * Copyright (c) 2000 Matthew N. Dodd <winter@jurai.net>
  * All rights reserved.
  *
- * Copyright (c) 1997 Simon Shapiro
- * All Rights Reserved
- *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -35,72 +32,62 @@
 #include <sys/module.h>
 #include <sys/bus.h>
 
-#include <machine/bus_memio.h>
 #include <machine/bus_pio.h>
 #include <machine/bus.h>
 #include <machine/resource.h>
 #include <sys/rman.h>
 
-#include <pci/pcireg.h>
-#include <pci/pcivar.h>
+#include <isa/isavar.h>
 
 #include <cam/scsi/scsi_all.h>
 
 #include <dev/dpt/dpt.h>
 
-#define	DPT_VENDOR_ID		0x1044
-#define	DPT_DEVICE_ID		0xa400
-
-#define	DPT_PCI_IOADDR		PCIR_MAPS		/* I/O Address */
-#define	DPT_PCI_MEMADDR		(PCIR_MAPS + 4)		/* Mem I/O Address */
-
-#define	ISA_PRIMARY_WD_ADDRESS	0x1f8
-
-static int	dpt_pci_probe	(device_t);
-static int	dpt_pci_attach	(device_t);
+static int	dpt_isa_probe		(device_t);
+static int	dpt_isa_attach		(device_t);
 
 static int
-dpt_pci_probe (device_t dev)
+dpt_isa_probe (device_t dev)
 {
-	if ((pci_get_vendor(dev) == DPT_VENDOR_ID) &&
-	    (pci_get_device(dev) == DPT_DEVICE_ID)) {
-		device_set_desc(dev, "DPT Caching SCSI RAID Controller");
-		return (0);
-	}
-	return (ENXIO);
+	dpt_conf_t *	conf;
+	u_int32_t	io_base;
+
+	/* No pnp support */
+	if (isa_get_vendorid(dev))
+		return (ENXIO);
+
+	if ((io_base = bus_get_resource_start(dev, SYS_RES_IOPORT, 0)) == 0)
+		return (ENXIO);
+
+	conf = dpt_pio_get_conf(io_base);
+        if (!conf) {
+                printf("dpt: dpt_pio_get_conf() failed.\n");
+                return (ENXIO);
+        }
+
+	device_set_desc(dev, "ISA DPT SCSI controller");
+	bus_set_resource(dev, SYS_RES_IRQ, 0, conf->IRQ, 1);
+	bus_set_resource(dev, SYS_RES_DRQ, 0, ((8 - conf->DMA_channel) & 7), 1);
+
+	return 0;
 }
 
 static int
-dpt_pci_attach (device_t dev)
+dpt_isa_attach (device_t dev)
 {
-	dpt_softc_t *	dpt;
+	dpt_softc_t *	dpt = NULL;
 	struct resource *io = 0;
 	struct resource *irq = 0;
+	struct resource *drq = 0;
 	int		s;
 	int		rid;
 	void *		ih;
 	int		error = 0;
 
-	int		iotype = 0;
-	u_int32_t	command;
-
-	command = pci_read_config(dev, PCIR_COMMAND, /*bytes*/1);
-
-#ifdef DPT_ALLOW_MMIO
-	if ((command & PCIM_CMD_MEMEN) != 0) {
-		rid = DPT_PCI_MEMADDR;
-		iotype = SYS_RES_MEMORY;
-		io = bus_alloc_resource(dev, iotype, &rid, 0, ~0, 1, RF_ACTIVE);
-	}
-#endif
-	if (io == NULL && (command & PCI_COMMAND_IO_ENABLE) != 0) {
-		rid = DPT_PCI_IOADDR;
-		iotype = SYS_RES_IOPORT;
-		io = bus_alloc_resource(dev, iotype, &rid, 0, ~0, 1, RF_ACTIVE);
-	}
-
-	if (io == NULL) {
-		device_printf(dev, "can't allocate register resources\n");
+	rid = 0;
+	io = bus_alloc_resource(dev, SYS_RES_IOPORT, &rid, 0, ~0, 1, RF_ACTIVE);
+	if (!io) {
+		device_printf(dev, "No I/O space?!\n");
 		error = ENOMEM;
 		goto bad;
 	}
@@ -108,21 +95,16 @@ dpt_pci_attach (device_t dev)
 	rid = 0;
 	irq = bus_alloc_resource(dev, SYS_RES_IRQ, &rid, 0, ~0, 1, RF_ACTIVE);
 	if (!irq) {
-		device_printf(dev, "No irq?!\n");
+		device_printf(dev, "No IRQ!\n");
 		error = ENOMEM;
 		goto bad;
 	}
 
-	/* Ensure busmastering is enabled */
-	command |= PCIM_CMD_BUSMASTEREN;
-	pci_write_config(dev, PCIR_COMMAND, command, /*bytes*/1);
-
-	if (rman_get_start(io) == (ISA_PRIMARY_WD_ADDRESS - 0x10)) {
-#ifdef DPT_DEBUG_WARN
-		device_printf(dev, "Mapped as an IDE controller.  "
-				   "Disabling SCSI setup\n");
-#endif
-		error = ENXIO;
+	rid = 0;
+	drq = bus_alloc_resource(dev, SYS_RES_DRQ, &rid, 0, ~0, 1, RF_ACTIVE);
+	if (!drq) {
+		device_printf(dev, "No DRQ?!\n");
+		error = ENOMEM;
 		goto bad;
 	}
 
@@ -132,12 +114,13 @@ dpt_pci_attach (device_t dev)
 		goto bad;
 	}
 
+	isa_dmacascade(rman_get_start(drq));
+
 	/* Allocate a dmatag representing the capabilities of this attachment */
-	/* XXX Should be a child of the PCI bus dma tag */
-	if (bus_dma_tag_create(	/* parent    */	NULL,
+	if (bus_dma_tag_create( /* parent    */	NULL,
 				/* alignemnt */	1,
 				/* boundary  */	0,
-				/* lowaddr   */	BUS_SPACE_MAXADDR_32BIT,
+				/* lowaddr   */	BUS_SPACE_MAXADDR_24BIT,
 				/* highaddr  */	BUS_SPACE_MAXADDR,
 				/* filter    */	NULL,
 				/* filterarg */	NULL,
@@ -146,7 +129,6 @@ dpt_pci_attach (device_t dev)
 				/* maxsegsz  */	BUS_SPACE_MAXSIZE_32BIT,
 				/* flags     */	0,
 				&dpt->parent_dmat) != 0) {
-		dpt_free(dpt);
 		error = ENXIO;
 		goto bad;
 	}
@@ -154,7 +136,7 @@ dpt_pci_attach (device_t dev)
 	s = splcam();
 
 	if (dpt_init(dpt) != 0) {
-		dpt_free(dpt);
+		splx(s);
 		error = ENXIO;
 		goto bad;
 	}
@@ -172,29 +154,33 @@ dpt_pci_attach (device_t dev)
 
 	return (error);
 
-bad:
+ bad:
+	if (dpt)
+		dpt_free(dpt);
 	if (io)
-		bus_release_resource(dev, iotype, 0, io);
+		bus_release_resource(dev, SYS_RES_IOPORT, 0, io);
 	if (irq)
 		bus_release_resource(dev, SYS_RES_IRQ, 0, irq);
+	if (drq)
+		bus_release_resource(dev, SYS_RES_DRQ, 0, drq);
 
 	return (error);
 }
 
-static device_method_t dpt_pci_methods[] = {
+static device_method_t dpt_isa_methods[] = {
 	/* Device interface */
-	DEVMETHOD(device_probe,         dpt_pci_probe),
-	DEVMETHOD(device_attach,        dpt_pci_attach),
+	DEVMETHOD(device_probe,		dpt_isa_probe),
+	DEVMETHOD(device_attach,	dpt_isa_attach),
 
 	{ 0, 0 }
 };
 
-static driver_t dpt_pci_driver = {
+static driver_t dpt_isa_driver = {
 	"dpt",
-	dpt_pci_methods,
+	dpt_isa_methods,
 	sizeof(dpt_softc_t),
 };
 
 static devclass_t dpt_devclass;
 
-DRIVER_MODULE(dpt, pci, dpt_pci_driver, dpt_devclass, 0, 0);
+DRIVER_MODULE(dpt, isa, dpt_isa_driver, dpt_devclass, 0, 0);
