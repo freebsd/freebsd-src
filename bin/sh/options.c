f01e3d0cfe6ba7cc1e269472f2a8267359aabeda
Martin Cracauer <cracauer@FreeBSD.org>
2000-04-20 09:49:16 +0000

    Fix warnings, some of them serious because sh violated name spaces reserved by the header files it includes.
    mkinit.c still produces C code with redundant declarations, although
    they are more harmless since they automatically derived from the right
    places.

--- bin/sh/options.c
+++ bin/sh/options.c
@@ -67,7 +67,7 @@ static const char rcsid[] =
 char *arg0;			/* value of $0 */
 struct shparam shellparam;	/* current positional parameters */
 char **argptr;			/* argument list for builtin commands */
-char *optarg;			/* set by nextopt (like getopt) */
+char *shoptarg;			/* set by nextopt (like getopt) */
 char *optptr;			/* used by nextopt */
 
 char *minusc;			/* argument to -c option */
@@ -539,7 +539,7 @@ nextopt(optstring)
 	if (*++q == ':') {
 		if (*p == '\0' && (p = *argptr++) == NULL)
 			error("No arg for -%c option", c);
-		optarg = p;
+		shoptarg = p;
 		p = NULL;
 	}
 	optptr = p;
