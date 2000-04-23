abef450cb59caad34b0ddade32dc7b64b50903d3
wollman <wollman@FreeBSD.org>
2000-04-23 04:00:00 +0000

    A couple months ago, Kirk and I were doing a walkthrough of the radix-tree search routine, and scratching our heads over why it was so obfuscated. This delta fixes a number of confusing style bugs and renames several structure members to have more meaningful names.  There remain a number of odd control-flow structures.  These changes do not affect the generated code.

--- sys/net/route.c
+++ sys/net/route.c
@@ -781,7 +781,7 @@ rt_fixchange(rn, vp)
 	xm1 = (u_char *)rt_mask(rt0);
 	xk2 = (u_char *)rt_key(rt);
 
-	for (i = rnh->rnh_treetop->rn_off; i < len; i++) {
+	for (i = rnh->rnh_treetop->rn_offset; i < len; i++) {
 		if ((xk2[i] & xm1[i]) != xk1[i]) {
 #ifdef DEBUG
 			if(rtfcdebug) printf("no match\n");
