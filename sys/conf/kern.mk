001348752bf5ad6d6fa0bb690aef766934d655ce
dfr <dfr@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-04-28 08:44:42 +0000

    Reserve register t7 on alpha to point at per-cpu global variables.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@59722 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -34,7 +34,8 @@ CFLAGS+=	-mpreferred-stack-boundary=2
 #
 # On the alpha, make sure that we don't use floating-point registers and
 # allow the use of EV56 instructions (only needed for low-level i/o).
+# Also, reserve register t7 to point at per-cpu global variables.
 #
 .if ${MACHINE_ARCH} == "alpha"
-CFLAGS+=	-mno-fp-regs -Wa,-mev56
+CFLAGS+=	-mno-fp-regs -ffixed-8 -Wa,-mev56
 .endif
