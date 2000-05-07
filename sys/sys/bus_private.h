686c68b3fa051e51bed9200eb576736318f59223
n_hibma <n_hibma@FreeBSD.org>
2000-05-07 20:26:51 +0000

    MFC: If selecting a new unit number for a new device, don't pick the first free one at the end of the list, use the first free one in the list. This avoids ever increasing unit numbers.
    Also, call BUS_NOMATCH only once, at boot. Avoids the unneeded claiming
    of devices by chip whenever a PCI module is loaded.

--- sys/sys/bus_private.h
+++ sys/sys/bus_private.h
@@ -53,7 +53,6 @@ struct devclass {
     char		*name;
     device_t		*devices; /* array of devices indexed by unit */
     int			maxunit; /* size of devices array */
-    int			nextunit; /* next unused unit number */
 };
 
 /*
@@ -134,6 +133,7 @@ struct device {
 #define DF_WILDCARD	4	/* unit was originally wildcard */
 #define DF_DESCMALLOCED	8	/* description was malloced */
 #define DF_QUIET	16	/* don't print verbose attach message */
+#define DF_DONENOMATCH	32	/* don't execute DEVICE_NOMATCH again */
     u_char		order;	/* order from device_add_child_ordered() */
     u_char		pad;
 #ifdef DEVICE_SYSCTLS
