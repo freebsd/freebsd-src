4aae1da6dd0b75dd78b839734bb87ebe1f8a96e9
Jayanth Vijayaraghavan <jayanth@FreeBSD.org>
2000-05-11 22:28:28 +0000

    Temporarily turn off the newreno flag until we can track down the known data corruption problem.

--- sys/netinet/tcp_output.c
+++ sys/netinet/tcp_output.c
@@ -98,7 +98,7 @@ int ss_fltsz_local = TCP_MAXWIN;               /* something large */
 SYSCTL_INT(_net_inet_tcp, OID_AUTO, local_slowstart_flightsize, CTLFLAG_RW,
 	&ss_fltsz_local, 1, "Slow start flight size for local networks");
 
-int     tcp_do_newreno = 1;
+int     tcp_do_newreno = 0;
 SYSCTL_INT(_net_inet_tcp, OID_AUTO, newreno, CTLFLAG_RW, &tcp_do_newreno,
         0, "Enable NewReno Algorithms");
 /*
