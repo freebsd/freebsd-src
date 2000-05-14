78d2db5fc1710bbf6587321ee3db71371d6ac99f
n_hibma <n_hibma@FreeBSD.org>
2000-05-14 19:51:38 +0000

    Sync with NetBSD: Add quirks for self-powered hubs that are not.

--- sys/dev/usb/usb_quirks.h
+++ sys/dev/usb/usb_quirks.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: usb_quirks.h,v 1.9 1999/11/15 22:04:15 augustss Exp $	*/
+/*	$NetBSD: usb_quirks.h,v 1.11 2000/04/27 15:26:50 augustss Exp $	*/
 /*	$FreeBSD$	*/
 
 /*
@@ -48,8 +48,6 @@ struct usbd_quirks {
 #define UQ_BUS_POWERED	0x20	/* device is bus powered, despite claim */
 #define UQ_BAD_AUDIO	0x40	/* device claims audio class, but isn't */
 #define UQ_SPUR_BUT_UP	0x80	/* spurious mouse button up events */
-#define UQ_NO_TSLEEP	0x100	/* device needs to work at interrupt
-				 * context, must avoid tsleep! */
 };
 
 extern struct usbd_quirks usbd_no_quirk;
