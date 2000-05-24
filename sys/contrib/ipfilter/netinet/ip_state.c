55921895822475a7d65c6b94a5f0eca232709ca9
darrenr <darrenr@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-05-24 19:38:17 +0000

    fix duplicate rcsid's
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@60883 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/contrib/ipfilter/netinet/ip_state.c
+++ sys/contrib/ipfilter/netinet/ip_state.c
@@ -7,10 +7,7 @@
  */
 #if !defined(lint)
 static const char sccsid[] = "@(#)ip_state.c	1.8 6/5/96 (C) 1993-1995 Darren Reed";
-/*static const char rcsid[] = "@(#)$Id: ip_state.c,v 2.3.2.18 2000/01/27 08:51:30 darrenr Exp $";*/
 static const char rcsid[] = "@(#)$FreeBSD$";
-static const char sccsid[] = "@(#)ip_state.c	1.8 6/5/96 (C) 1993-2000 Darren Reed";
-static const char rcsid[] = "@(#)$Id: ip_state.c,v 2.30.2.9 2000/05/22 10:26:15 darrenr Exp $";
 #endif
 
 #include <sys/errno.h>
