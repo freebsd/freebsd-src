f46d0033f1010a879222571225d8f00939165537
dfr <dfr@FreeBSD.org>
2000-05-29 19:40:42 +0000

    Add SWI_TQ_MASK to imask definition.

--- sys/i386/isa/atpic_vector.s
+++ sys/i386/isa/atpic_vector.s
@@ -217,7 +217,7 @@ imasks:				/* masks for interrupt handlers */
 	.space	NHWI*4		/* padding; HWI masks are elsewhere */
 
 	.long	SWI_TTY_MASK, SWI_NET_MASK, SWI_CAMNET_MASK, SWI_CAMBIO_MASK
-	.long	SWI_VM_MASK, 0, 0, 0
+	.long	SWI_VM_MASK, SWI_TQ_MASK, 0, 0
 	.long	0, 0, 0, 0
 	.long	0, 0, SWI_CLOCK_MASK, 0
 
