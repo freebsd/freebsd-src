2cf177b839dadddbde6fc377b797dfe71f587f53
phk <phk@FreeBSD.org>
2000-06-10 19:21:20 +0000

    fix a typo

--- sys/kern/kern_resource.c
+++ sys/kern/kern_resource.c
@@ -525,7 +525,7 @@ calcru(p, up, sp, ip)
 		 */
 		microuptime(&tv);
 		if (timevalcmp(&tv, &switchtime, <))
-			printf("microuptime() went backwards (%ld.%06ld -> %ld,%06ld)\n",
+			printf("microuptime() went backwards (%ld.%06ld -> %ld.%06ld)\n",
 			    switchtime.tv_sec, switchtime.tv_usec, 
 			    tv.tv_sec, tv.tv_usec);
 		else
