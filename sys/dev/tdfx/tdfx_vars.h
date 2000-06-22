f6ffa982ef76612c0ec170b54f031f6c52ac0006
cokane <cokane@FreeBSD.org>
2000-06-22 05:37:17 +0000

    Stick in header, $FreeBSD$.

--- sys/dev/tdfx/tdfx_vars.h
+++ sys/dev/tdfx/tdfx_vars.h
@@ -1,10 +1,47 @@
+/*
+ * Copyright (c) 2000 by Coleman Kane <cokane@FreeBSD.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Gardner Buchanan.
+ * 4. The name of Gardner Buchanan may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *   $FreeBSD$
+ */
+
 /* tdfx_vars.h -- constants and structs used in the tdfx driver
-	Copyright (C) 2000 by Coleman Kane <cokane@pohl.ececs.uc.edu>
+	Copyright (C) 2000 by Coleman Kane <cokane@FreeBSD.org>
 */
 #ifndef	TDFX_VARS_H
 #define	TDFX_VARS_H
 
 #include <sys/memrange.h>
+#include <sys/param.h>
+#include <sys/bus_private.h>
+#include <sys/bus.h>
+#include <sys/cdefs.h>
 
 #define	CDEV_MAJOR	107
 #define  PCI_DEVICE_ALLIANCE_AT3D	0x643d1142
@@ -38,10 +75,6 @@
 #define	TDFX_IOC_QRY_BOARDS	2
 #define	TDFX_IOC_QRY_FETCH	3
 #define	TDFX_IOC_QRY_UPDATE	4
-#include <sys/param.h>
-#include <sys/bus_private.h>
-#include <sys/bus.h>
-#include <sys/cdefs.h>
 
 struct tdfx_softc {
 	int cardno;
