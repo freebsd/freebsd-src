43ef1488310880a55a0117e513ddc288062aab38
ru <ru@FreeBSD.org>
2000-06-27 16:29:31 +0000

    MFC: - Added support for FTP EPRT (RFC 2428) command. - Added support for passive mode FTP servers behind NAT. - Added security checks for FTP aliasing. - Load Sharing using IP Network Address Translation (RFC 2391). - Real PPTP support.

--- lib/libalias/alias.h
+++ lib/libalias/alias.h
@@ -14,10 +14,6 @@
 #ifndef _ALIAS_H_
 #define _ALIAS_H_
 
-#ifndef NULL
-#define NULL 0
-#endif
-
 /* Alias link representative (incomplete struct) */
 struct alias_link;
 
@@ -56,8 +52,15 @@ struct alias_link;
                             u_char);
 
     extern int
-    PacketAliasPptp(struct in_addr);
+    PacketAliasAddServer(struct alias_link *link,
+                         struct in_addr addr,
+                         u_short port);
 
+    extern struct alias_link *
+    PacketAliasRedirectProto(struct in_addr,
+                             struct in_addr,
+                             struct in_addr,
+                             u_char);
 
     extern struct alias_link *
     PacketAliasRedirectAddr(struct in_addr,
@@ -92,7 +95,7 @@ struct alias_link;
 
 
 /********************** Mode flags ********************/
-/* Set these flags using SetPacketAliasMode() */
+/* Set these flags using PacketAliasSetMode() */
 
 /* If PKT_ALIAS_LOG is set, a message will be printed to
 	/var/log/alias.log every time a link is created or deleted.  This
@@ -105,7 +108,7 @@ struct alias_link;
 #define PKT_ALIAS_DENY_INCOMING 0x02
 
 /* If PKT_ALIAS_SAME_PORTS is set, packets will be attempted sent from
-	the same port as they originated on.  This allows eg rsh to work
+	the same port as they originated on.  This allows e.g. rsh to work
 	*99% of the time*, but _not_ 100%.  (It will be slightly flakey
 	instead of not working at all.)  This mode bit is set by
         PacketAliasInit(), so it is a default mode of operation. */
@@ -117,13 +120,12 @@ struct alias_link;
 	port it chooses.  This will avoid interference with the host
 	machine.  Fully specified links do not require this.  This bit
         is set after a call to PacketAliasInit(), so it is a default
-        mode of operation.*/
+        mode of operation. */
 #define PKT_ALIAS_USE_SOCKETS 0x08
 
-/* If PKT_ALIAS_UNREGISTERED_ONLY is set, then only packets with with
-	unregistered source addresses will be aliased (along with those
-	of the ppp host maching itself.  Private addresses are those
-        in the following ranges:
+/* If PKT_ALIAS_UNREGISTERED_ONLY is set, then only packets with
+	unregistered source addresses will be aliased.  Private
+	addresses are those in the following ranges:
 		10.0.0.0     ->   10.255.255.255
 		172.16.0.0   ->   172.31.255.255
 		192.168.0.0  ->   192.168.255.255  */
@@ -132,7 +134,7 @@ struct alias_link;
 /* If PKT_ALIAS_RESET_ON_ADDR_CHANGE is set, then the table of dynamic
 	aliasing links will be reset whenever PacketAliasSetAddress()
         changes the default aliasing address.  If the default aliasing
-        address is left unchanged by this functions call, then the
+        address is left unchanged by this function call, then the
         table of dynamic aliasing links will be left intact.  This
         bit is set after a call to PacketAliasInit(). */
 #define PKT_ALIAS_RESET_ON_ADDR_CHANGE 0x20
--- lib/libalias/alias_ftp.c
+++ lib/libalias/alias_ftp.c
@@ -1,15 +1,16 @@
 /*
     Alias_ftp.c performs special processing for FTP sessions under
-    TCP.  Specifically, when a PORT command from the client side
-    is sent, it is intercepted and modified.  The address is changed
-    to the gateway machine and an aliasing port is used.
+    TCP.  Specifically, when a PORT/EPRT command from the client
+    side or 227/229 reply from the server is sent, it is intercepted
+    and modified.  The address is changed to the gateway machine
+    and an aliasing port is used.
 
-    For this routine to work, the PORT command must fit entirely
-    into a single TCP packet.  This is typically the case, but exceptions
+    For this routine to work, the message must fit entirely into a
+    single TCP packet.  This is typically the case, but exceptions
     can easily be envisioned under the actual specifications.
 
     Probably the most troubling aspect of the approach taken here is
-    that the new PORT command will typically be a different length, and
+    that the new message will typically be a different length, and
     this causes a certain amount of bookkeeping to keep track of the
     changes of sequence and acknowledgment numbers, since the client
     machine is totally unaware of the modification to the TCP stream.
@@ -18,6 +19,8 @@
     This software is placed into the public domain with no restrictions
     on its distribution.
 
+    References: RFC 959, RFC 2428.
+
     Initial version:  August, 1996  (cjm)
 
     Version 1.6
@@ -25,13 +28,16 @@
          error for modified IP packets.
 
     Version 1.7:  January 9, 1996 (cjm)
-         Differental checksum computation for change
+         Differential checksum computation for change
          in IP packet length.
-   
+
     Version 2.1:  May, 1997 (cjm)
          Very minor changes to conform with
          local/global/function naming conventions
-         withing the packet alising module.
+         within the packet aliasing module.
+
+    Version 3.1:  May, 2000 (eds)
+	 Add support for passive mode, alias the 227 replies.
 
     See HISTORY file for record of revisions.
 
@@ -40,7 +46,7 @@
 
 /* Includes */
 #include <ctype.h>
-#include <stdio.h> 
+#include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
 #include <netinet/in_systm.h>
@@ -50,9 +56,25 @@
 
 #include "alias_local.h"
 
-static void NewFtpPortCommand(struct ip *, struct alias_link *, struct in_addr, u_short, int);
+#define FTP_CONTROL_PORT_NUMBER 21
+#define MAX_MESSAGE_SIZE	128
 
+enum ftp_message_type {
+    FTP_PORT_COMMAND,
+    FTP_EPRT_COMMAND,
+    FTP_227_REPLY,
+    FTP_229_REPLY,
+    FTP_UNKNOWN_MESSAGE
+};
 
+static int ParseFtpPortCommand(char *, int);
+static int ParseFtpEprtCommand(char *, int);
+static int ParseFtp227Reply(char *, int);
+static int ParseFtp229Reply(char *, int);
+static void NewFtpMessage(struct ip *, struct alias_link *, int, int);
+
+static struct in_addr true_addr;	/* in network byte order. */
+static u_short true_port;		/* in host byte order. */
 
 void
 AliasHandleFtpOut(
@@ -61,93 +83,367 @@ struct alias_link *link, /* The link to go through (aliased port) */
 int maxpacketsize  /* The maximum size this packet can grow to (including headers) */)
 {
     int hlen, tlen, dlen;
-    struct in_addr true_addr;
-    u_short true_port;
     char *sptr;
     struct tcphdr *tc;
-        
+    int ftp_message_type;
+
 /* Calculate data length of TCP packet */
     tc = (struct tcphdr *) ((char *) pip + (pip->ip_hl << 2));
     hlen = (pip->ip_hl + tc->th_off) << 2;
     tlen = ntohs(pip->ip_len);
     dlen = tlen - hlen;
 
-/* Return is data length is too long or too short */
-    if (dlen<10 || dlen>80)
-        return;
-
 /* Place string pointer and beginning of data */
-    sptr = (char *) pip;  
+    sptr = (char *) pip;
     sptr += hlen;
 
-/* Parse through string using state diagram method */
-    {
-        char ch, zero;
-        int i, state;
-        u_long a1, a2, a3, a4;
-        u_short p1, p2; 
+/*
+ * Check that data length is not too long and previous message was
+ * properly terminated with CRLF.
+ */
+    if (dlen <= MAX_MESSAGE_SIZE && GetLastLineCrlfTermed(link)) {
+	ftp_message_type = FTP_UNKNOWN_MESSAGE;
 
-        a1=0; a2=0; a3=0; a4=0; p1=0; p2=0;
-        zero = '0';
-        state=-4;
-        for (i=0; i<dlen; i++)
-        {
-            ch = sptr[i];
-            switch (state)
-            {
-                case -4: if (ch == 'P') state=-3; else return; break;
-                case -3: if (ch == 'O') state=-2; else return; break;
-                case -2: if (ch == 'R') state=-1; else return; break;
-                case -1: if (ch == 'T') state= 0; else return; break;
+	if (ntohs(tc->th_dport) == FTP_CONTROL_PORT_NUMBER) {
+/*
+ * When aliasing a client, check for the PORT/EPRT command.
+ */
+	    if (ParseFtpPortCommand(sptr, dlen))
+		ftp_message_type = FTP_PORT_COMMAND;
+	    else if (ParseFtpEprtCommand(sptr, dlen))
+		ftp_message_type = FTP_EPRT_COMMAND;
+	} else {
+/*
+ * When aliasing a server, check for the 227/229 reply.
+ */
+	    if (ParseFtp227Reply(sptr, dlen))
+		ftp_message_type = FTP_227_REPLY;
+	    else if (ParseFtp229Reply(sptr, dlen))
+		ftp_message_type = FTP_229_REPLY;
+	}
 
-                case 0 :
-                    if (isdigit(ch)) {a1=ch-zero; state=1 ;} break;
-                case 1 :
-                    if (isdigit(ch)) a1=10*a1+ch-zero; else state=2 ; break;
-                case 2 :
-                    if (isdigit(ch)) {a2=ch-zero; state=3 ;} break;
-                case 3 :
-                    if (isdigit(ch)) a2=10*a2+ch-zero; else state=4 ; break;
-                case 4 :
-                    if (isdigit(ch)) {a3=ch-zero; state=5 ;} break;
-                case 5 :
-                    if (isdigit(ch)) a3=10*a3+ch-zero; else state=6 ; break;
-                case 6 :
-                    if (isdigit(ch)) {a4=ch-zero; state=7 ;} break;
-                case 7 :
-                    if (isdigit(ch)) a4=10*a4+ch-zero; else state=8 ; break;
-                case 8 :
-                    if (isdigit(ch)) {p1=ch-zero; state=9 ;} break;
-                case 9 :
-                    if (isdigit(ch)) p1=10*p1+ch-zero; else state=10; break;
-                case 10:
-                    if (isdigit(ch)) {p2=ch-zero; state=11;} break;
-                case 11:
-                    if (isdigit(ch)) p2=10*p2+ch-zero; break;
-            }
-        }
+	if (ftp_message_type != FTP_UNKNOWN_MESSAGE)
+	    NewFtpMessage(pip, link, maxpacketsize, ftp_message_type);
+    }
 
-        if (state == 11)
-        {
-            true_port = htons((p1<<8) + p2);
-            true_addr.s_addr = htonl((a1<<24) + (a2<<16) +(a3<<8) + a4);
-            NewFtpPortCommand(pip, link, true_addr, true_port, maxpacketsize);
-        }
+/* Track the msgs which are CRLF term'd for PORT/PASV FW breach */
+
+    if (dlen) {                  /* only if there's data */
+      sptr = (char *) pip; 	 /* start over at beginning */
+      tlen = ntohs(pip->ip_len); /* recalc tlen, pkt may have grown */
+      SetLastLineCrlfTermed(link,
+			    (sptr[tlen-2] == '\r') && (sptr[tlen-1] == '\n'));
     }
 }
 
+static int
+ParseFtpPortCommand(char *sptr, int dlen)
+{
+    char ch;
+    int i, state;
+    u_int32_t addr;
+    u_short port;
+    u_int8_t octet;
+
+    /* Format: "PORT A,D,D,R,PO,RT". */
+
+    /* Return if data length is too short. */
+    if (dlen < 18)
+	return 0;
+
+    addr = port = octet = 0;
+    state = -4;
+    for (i = 0; i < dlen; i++) {
+	ch = sptr[i];
+	switch (state) {
+	case -4: if (ch == 'P') state++; else return 0; break;
+	case -3: if (ch == 'O') state++; else return 0; break;
+	case -2: if (ch == 'R') state++; else return 0; break;
+	case -1: if (ch == 'T') state++; else return 0; break;
+
+	case 0:
+	    if (isspace(ch))
+		break;
+	    else
+		state++;
+	case 1: case 3: case 5: case 7: case 9: case 11:
+	    if (isdigit(ch)) {
+		octet = ch - '0';
+		state++;
+	    } else
+		return 0;
+	    break;
+	case 2: case 4: case 6: case 8:
+	    if (isdigit(ch))
+		octet = 10 * octet + ch - '0';
+            else if (ch == ',') {
+		addr = (addr << 8) + octet;
+		state++;
+	    } else
+		return 0;
+	    break;
+	case 10: case 12:
+	    if (isdigit(ch))
+		octet = 10 * octet + ch - '0';
+	    else if (ch == ',' || state == 12) {
+		port = (port << 8) + octet;
+		state++;
+	    } else
+		return 0;
+	    break;
+	}
+    }
+
+    if (state == 13) {
+	true_addr.s_addr = htonl(addr);
+	true_port = port;
+	return 1;
+    } else
+	return 0;
+}
+
+static int
+ParseFtpEprtCommand(char *sptr, int dlen)
+{
+    char ch, delim;
+    int i, state;
+    u_int32_t addr;
+    u_short port;
+    u_int8_t octet;
+
+    /* Format: "EPRT |1|A.D.D.R|PORT|". */
+
+    /* Return if data length is too short. */
+    if (dlen < 18)
+	return 0;
+
+    addr = port = octet = 0;
+    delim = '|';			/* XXX gcc -Wuninitialized */
+    state = -4;
+    for (i = 0; i < dlen; i++) {
+	ch = sptr[i];
+	switch (state)
+	{
+	case -4: if (ch == 'E') state++; else return 0; break;
+	case -3: if (ch == 'P') state++; else return 0; break;
+	case -2: if (ch == 'R') state++; else return 0; break;
+	case -1: if (ch == 'T') state++; else return 0; break;
+
+	case 0:
+	    if (!isspace(ch)) {
+		delim = ch;
+		state++;
+	    }
+	    break;
+	case 1:
+	    if (ch == '1')	/* IPv4 address */
+		state++;
+	    else
+		return 0;
+	    break;
+	case 2:
+	    if (ch == delim)
+		state++;
+	    else
+		return 0;
+	    break;
+	case 3: case 5: case 7: case 9:
+	    if (isdigit(ch)) {
+		octet = ch - '0';
+		state++;
+	    } else
+		return 0;
+	    break;
+	case 4: case 6: case 8: case 10:
+	    if (isdigit(ch))
+		octet = 10 * octet + ch - '0';
+            else if (ch == '.' || state == 10) {
+		addr = (addr << 8) + octet;
+		state++;
+	    } else
+		return 0;
+	    break;
+	case 11:
+	    if (isdigit(ch)) {
+		port = ch - '0';
+		state++;
+	    } else
+		return 0;
+	    break;
+	case 12:
+	    if (isdigit(ch))
+		port = 10 * port + ch - '0';
+	    else if (ch == delim)
+		state++;
+	    else
+		return 0;
+	    break;
+	}
+    }
+
+    if (state == 13) {
+	true_addr.s_addr = htonl(addr);
+	true_port = port;
+	return 1;
+    } else
+	return 0;
+}
+
+static int
+ParseFtp227Reply(char *sptr, int dlen)
+{
+    char ch;
+    int i, state;
+    u_int32_t addr;
+    u_short port;
+    u_int8_t octet;
+
+    /* Format: "227 Entering Passive Mode (A,D,D,R,PO,RT)" */
+
+    /* Return if data length is too short. */
+    if (dlen < 17)
+	return 0;
+
+    addr = port = octet = 0;
+
+    state = -3;
+    for (i = 0; i < dlen; i++) {
+        ch = sptr[i];
+        switch (state)
+        {
+        case -3: if (ch == '2') state++; else return 0; break;
+        case -2: if (ch == '2') state++; else return 0; break;
+        case -1: if (ch == '7') state++; else return 0; break;
+
+	case 0:
+	    if (ch == '(')
+		state++;
+	    break;
+	case 1: case 3: case 5: case 7: case 9: case 11:
+	    if (isdigit(ch)) {
+		octet = ch - '0';
+		state++;
+	    } else
+		return 0;
+	    break;
+	case 2: case 4: case 6: case 8:
+	    if (isdigit(ch))
+		octet = 10 * octet + ch - '0';
+            else if (ch == ',') {
+		addr = (addr << 8) + octet;
+		state++;
+	    } else
+		return 0;
+	    break;
+	case 10: case 12:
+	    if (isdigit(ch))
+		octet = 10 * octet + ch - '0';
+	    else if (ch == ',' || (state == 12 && ch == ')')) {
+		port = (port << 8) + octet;
+		state++;
+	    } else
+		return 0;
+	    break;
+	}
+    }
+
+    if (state == 13) {
+        true_port = port;
+        true_addr.s_addr = htonl(addr);
+	return 1;
+    } else
+	return 0;
+}
+
+static int
+ParseFtp229Reply(char *sptr, int dlen)
+{
+    char ch, delim;
+    int i, state;
+    u_short port;
+
+    /* Format: "229 Entering Extended Passive Mode (|||PORT|)" */
+
+    /* Return if data length is too short. */
+    if (dlen < 11)
+	return 0;
+
+    port = 0;
+    delim = '|';			/* XXX gcc -Wuninitialized */
+
+    state = -3;
+    for (i = 0; i < dlen; i++) {
+	ch = sptr[i];
+	switch (state)
+	{
+	case -3: if (ch == '2') state++; else return 0; break;
+	case -2: if (ch == '2') state++; else return 0; break;
+	case -1: if (ch == '9') state++; else return 0; break;
+
+	case 0:
+	    if (ch == '(')
+		state++;
+	    break;
+	case 1:
+	    delim = ch;
+	    state++;
+	    break;
+	case 2: case 3:
+	    if (ch == delim)
+		state++;
+	    else
+		return 0;
+	    break;
+	case 4:
+	    if (isdigit(ch)) {
+		port = ch - '0';
+		state++;
+	    } else
+		return 0;
+	    break;
+	case 5:
+	    if (isdigit(ch))
+		port = 10 * port + ch - '0';
+	    else if (ch == delim)
+		state++;
+	    else
+		return 0;
+	    break;
+	case 6:
+	    if (ch == ')')
+		state++;
+	    else
+		return 0;
+	    break;
+	}
+    }
+
+    if (state == 7) {
+	true_port = port;
+	return 1;
+    } else
+	return 0;
+}
+
 static void
-NewFtpPortCommand(struct ip *pip,
-                  struct alias_link *link,
-                  struct in_addr true_addr,
-                  u_short true_port,
-                  int maxpacketsize)
-{ 
+NewFtpMessage(struct ip *pip,
+              struct alias_link *link,
+              int maxpacketsize,
+              int ftp_message_type)
+{
     struct alias_link *ftp_link;
 
-/* Establish link to address and port found in PORT command */
+/* Security checks. */
+    if (ftp_message_type != FTP_229_REPLY &&
+	pip->ip_src.s_addr != true_addr.s_addr)
+	return;
+
+    if (true_port < IPPORT_RESERVED)
+	return;
+
+/* Establish link to address and port found in FTP control message. */
     ftp_link = FindUdpTcpOut(true_addr, GetDestAddress(link),
-                             true_port, 0, IPPROTO_TCP);
+                             htons(true_port), 0, IPPROTO_TCP);
 
     if (ftp_link != NULL)
     {
@@ -155,8 +451,11 @@ NewFtpPortCommand(struct ip *pip,
         struct tcphdr *tc;
 
 #ifndef NO_FW_PUNCH
-/* Punch hole in firewall */
-        PunchFWHole(ftp_link);
+	if (ftp_message_type == FTP_PORT_COMMAND ||
+	    ftp_message_type == FTP_EPRT_COMMAND) {
+	    /* Punch hole in firewall */
+	    PunchFWHole(ftp_link);
+	}
 #endif
 
 /* Calculate data length of TCP packet */
@@ -165,33 +464,57 @@ NewFtpPortCommand(struct ip *pip,
         tlen = ntohs(pip->ip_len);
         dlen = tlen - hlen;
 
-/* Create new PORT command */
+/* Create new FTP message. */
         {
-            char stemp[80];
+            char stemp[MAX_MESSAGE_SIZE + 1];
             char *sptr;
             u_short alias_port;
             u_char *ptr;
-            int a1, a2, a3, a4, p1, p2; 
+            int a1, a2, a3, a4, p1, p2;
             struct in_addr alias_address;
-        
+
 /* Decompose alias address into quad format */
             alias_address = GetAliasAddress(link);
             ptr = (u_char *) &alias_address.s_addr;
             a1 = *ptr++; a2=*ptr++; a3=*ptr++; a4=*ptr;
 
-/* Decompose alias port into pair format */
-            alias_port = GetAliasPort(ftp_link);
-            ptr = (char *) &alias_port;
-            p1 = *ptr++; p2=*ptr;
+	    alias_port = GetAliasPort(ftp_link);
 
-/* Generate command string */
-            sprintf(stemp, "PORT %d,%d,%d,%d,%d,%d\r\n",
-                     a1,a2,a3,a4,p1,p2);
+	    switch (ftp_message_type)
+	    {
+	    case FTP_PORT_COMMAND:
+	    case FTP_227_REPLY:
+		/* Decompose alias port into pair format. */
+		ptr = (char *) &alias_port;
+		p1 = *ptr++; p2=*ptr;
+
+		if (ftp_message_type == FTP_PORT_COMMAND) {
+		    /* Generate PORT command string. */
+		    sprintf(stemp, "PORT %d,%d,%d,%d,%d,%d\r\n",
+			    a1,a2,a3,a4,p1,p2);
+		} else {
+		    /* Generate 227 reply string. */
+		    sprintf(stemp,
+			    "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d)\r\n",
+			    a1,a2,a3,a4,p1,p2);
+		}
+		break;
+	    case FTP_EPRT_COMMAND:
+		/* Generate EPRT command string. */
+		sprintf(stemp, "EPRT |1|%d.%d.%d.%d|%d|\r\n",
+			a1,a2,a3,a4,ntohs(alias_port));
+		break;
+	    case FTP_229_REPLY:
+		/* Generate 229 reply string. */
+		sprintf(stemp, "229 Entering Extended Passive Mode (|||%d|)\r\n",
+			ntohs(alias_port));
+		break;
+	    }
 
 /* Save string length for IP header modification */
             slen = strlen(stemp);
 
-/* Copy into IP packet */
+/* Copy modified buffer into IP packet. */
             sptr = (char *) pip; sptr += hlen;
             strncpy(sptr, stemp, maxpacketsize-hlen);
         }
