1897cf38f9c2d9936b118809e2512760582b8f4d
itojun <itojun@FreeBSD.org>
2000-07-04 18:43:32 +0000

    be sure to wipe out m_pkthdr when you set M_PKTHDR, you may see junk pointer in m_pkthdr portion. actually, we should not change pkthdr mbuf <-> non-pkthdr mbuf.

--- sys/dev/vx/if_vx.c
+++ sys/dev/vx/if_vx.c
@@ -813,7 +813,7 @@ vxget(sc, totlen)
         sc->next_mb = (sc->next_mb + 1) % MAX_MBS;
         m->m_data = m->m_pktdat;
         m->m_flags = M_PKTHDR;
-        m->m_pkthdr.csum_flags = 0;
+	bzero(&m->m_pkthdr, sizeof(m->m_pkthdr));
     }
     m->m_pkthdr.rcvif = ifp;
     m->m_pkthdr.len = totlen;
