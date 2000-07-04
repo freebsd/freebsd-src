92d99305d8ab84a7e8477043a29608cfcb58f276
Matt Jacob <mjacob@FreeBSD.org>
2000-07-04 02:30:09 +0000

    MFC: Add mailbox bitmask macros (numbers of available mailbox registers based upon Qlogic chip type). Define maximum mailboxes. Add INT_PENDING_MASK macro. Change mailbox offset macro name.
    cleanup i_int_X vs. uint_X definitions
    
    The storage for WWN from NVRAM is actually the PORT WWN, not the NODE WWN.

--- sys/dev/isp/ispreg.h
+++ sys/dev/isp/ispreg.h
@@ -225,6 +225,9 @@
 #define	INT_PENDING(isp, isr)	(IS_FC(isp)? \
 	((isr & BIU2100_ISR_RISC_INT) != 0) : ((isr & BIU_ISR_RISC_INT) != 0))
 
+#define	INT_PENDING_MASK(isp)	\
+	(IS_FC(isp)? BIU2100_ISR_RISC_INT: BIU_ISR_RISC_INT)
+
 /* BUS SEMAPHORE REGISTER */
 #define	BIU_SEMA_STATUS		0x0002	/* Semaphore Status Bit */
 #define	BIU_SEMA_LOCK  		0x0001	/* Semaphore Lock Bit */
@@ -337,10 +340,15 @@
 #define	OUTMAILBOX6	(MBOX_BLOCK+0xC)
 #define	OUTMAILBOX7	(MBOX_BLOCK+0xE)
 
-#define	OMBOX_OFFN(n)	(MBOX_BLOCK + (n * 2))
+#define	MBOX_OFF(n)	(MBOX_BLOCK + ((n) << 1))
 #define	NMBOX(isp)	\
 	(((((isp)->isp_type & ISP_HA_SCSI) >= ISP_HA_SCSI_1040A) || \
 	 ((isp)->isp_type & ISP_HA_FC))? 8 : 6)
+#define	NMBOX_BMASK(isp)	\
+	(((((isp)->isp_type & ISP_HA_SCSI) >= ISP_HA_SCSI_1040A) || \
+	 ((isp)->isp_type & ISP_HA_FC))? 0xff : 0x3f)
+
+#define	MAX_MAILBOX	8
 
 /*
  * SXP Block Register Offsets
@@ -699,26 +707,26 @@
 
 /* Offset 5 */
 /*
-	uint8_t bios_configuration_mode     :2;
-	uint8_t bios_disable                :1;
-	uint8_t selectable_scsi_boot_enable :1;
-	uint8_t cd_rom_boot_enable          :1;
-	uint8_t disable_loading_risc_code   :1;
-	uint8_t enable_64bit_addressing     :1;
-	uint8_t unused_7                    :1;
+	u_int8_t bios_configuration_mode     :2;
+	u_int8_t bios_disable                :1;
+	u_int8_t selectable_scsi_boot_enable :1;
+	u_int8_t cd_rom_boot_enable          :1;
+	u_int8_t disable_loading_risc_code   :1;
+	u_int8_t enable_64bit_addressing     :1;
+	u_int8_t unused_7                    :1;
  */
 
 /* Offsets 6, 7 */
 /*
-        uint8_t boot_lun_number    :5;
-        uint8_t scsi_bus_number    :1;
-        uint8_t unused_6           :1;
-        uint8_t unused_7           :1;
-        uint8_t boot_target_number :4;
-        uint8_t unused_12          :1;
-        uint8_t unused_13          :1;
-        uint8_t unused_14          :1;
-        uint8_t unused_15          :1;
+        u_int8_t boot_lun_number    :5;
+        u_int8_t scsi_bus_number    :1;
+        u_int8_t unused_6           :1;
+        u_int8_t unused_7           :1;
+        u_int8_t boot_target_number :4;
+        u_int8_t unused_12          :1;
+        u_int8_t unused_13          :1;
+        u_int8_t unused_14          :1;
+        u_int8_t unused_15          :1;
  */
 
 #define	ISP1080_NVRAM_HBA_ENABLE(c)			ISPBSMX(c, 16, 3, 0x01)
