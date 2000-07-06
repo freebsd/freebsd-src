ee2ffd26e76b7a41c9f17a94d19a54a07d150d89
David E. O'Brien <obrien@FreeBSD.org>
2000-07-06 21:45:03 +0000

    MFC: upgrade to version 4.1.

--- contrib/libreadline/rlconf.h
+++ contrib/libreadline/rlconf.h
@@ -1,3 +1,4 @@
+/* $FreeBSD$ */
 /* rlconf.h -- readline configuration definitions */
 
 /* Copyright (C) 1994 Free Software Foundation, Inc.
@@ -8,7 +9,7 @@
 
    The Library is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 1, or (at your option)
+   the Free Software Foundation; either version 2, or (at your option)
    any later version.
 
    The Library is distributed in the hope that it will be useful, but
@@ -19,7 +20,7 @@
    The GNU General Public License is often shipped with GNU software, and
    is generally kept in a file called COPYING or LICENSE.  If you do not
    have a copy of the license, write to the Free Software Foundation,
-   675 Mass Ave, Cambridge, MA 02139, USA. */
+   59 Temple Place, Suite 330, Boston, MA 02111 USA. */
 
 #if !defined (_RLCONF_H_)
 #define _RLCONF_H_
@@ -29,10 +30,6 @@
 
 /* Define this to get an indication of file type when listing completions. */
 #define VISIBLE_STATS
-
-/* If defined, readline shows opening parens and braces when closing
-   paren or brace entered. */
-/* #define PAREN_MATCHING */
 
 /* This definition is needed by readline.c, rltty.c, and signals.c. */
 /* If on, then readline handles signals in a way that doesn't screw. */
