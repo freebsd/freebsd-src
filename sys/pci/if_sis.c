2a3a1379634e66af05a2e4f94e5ed4b50058c920
wpaul <wpaul@FreeBSD.org>
2000-07-06 19:21:07 +0000

    Fix a minor bug in the MAC address unscrambling code. (Some shift rights should have been shift lefts.)

--- sys/pci/if_sis.c
+++ sys/pci/if_sis.c
@@ -774,11 +774,11 @@ static int sis_attach(dev)
 
 			/* Shift everything over one bit. */
 			tmp[3] = tmp[3] >> 1;
-			tmp[3] |= tmp[2] >> 15;
+			tmp[3] |= tmp[2] << 15;
 			tmp[2] = tmp[2] >> 1;
-			tmp[2] |= tmp[1] >> 15;
+			tmp[2] |= tmp[1] << 15;
 			tmp[1] = tmp[1] >> 1;
-			tmp[1] |= tmp[0] >> 15;
+			tmp[1] |= tmp[0] << 15;
 
 			/* Now reverse all the bits. */
 			tmp[3] = sis_reverse(tmp[3]);
