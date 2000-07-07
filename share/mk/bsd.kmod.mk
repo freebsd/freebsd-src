ab82d24513d1981d110777d1811f507043d524f6
peter <peter@FreeBSD.org>
2000-07-07 09:45:29 +0000

    MFC: 1.85: use an extra .. for one more nesting level.  This should make it comparable with -CURRENT and therefore easier to build a 4.x kernel under -current and vice versa.

--- share/mk/bsd.kmod.mk
+++ share/mk/bsd.kmod.mk
@@ -1,7 +1,7 @@
 # $FreeBSD$
 
 # Search for kernel source tree in standard places.
-.for _dir in ${.CURDIR}/../.. ${.CURDIR}/../../.. /sys /usr/src/sys
+.for _dir in ${.CURDIR}/../.. ${.CURDIR}/../../.. ${.CURDIR}/../../../.. /sys /usr/src/sys
 .if !defined(SYSDIR) && exists(${_dir}/kern/) && exists(${_dir}/conf/)
 SYSDIR=	${_dir}
 .endif
