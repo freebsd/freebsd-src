ca308843fe7902198ef831d8d63fb1a0eb49bae9
jdp <jdp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-07-08 04:10:38 +0000

    Solve the dynamic linker's problems with multithreaded programs once and for all (I hope).  Packages such as wine, JDK, and linuxthreads should no longer have any problems with re-entering the dynamic linker.
    This commit replaces the locking used in the dynamic linker with a
    new spinlock-based reader/writer lock implementation.  Brian
    Fundakowski Feldman <green> argued for this from the very beginning,
    but it took me a long time to come around to his point of view.
    Spinlocks are the only kinds of locks that work with all thread
    packages.  But on uniprocessor systems they can be inefficient,
    because while a contender for the lock is spinning the holder of the
    lock cannot make any progress toward releasing it.  To alleviate
    this disadvantage I have borrowed a trick from Sleepycat's Berkeley
    DB implementation.  When spinning for a lock, the requester does a
    nanosleep() call for 1 usec. each time around the loop.  This will
    generally yield the CPU to other threads, allowing the lock holder
    to finish its business and release the lock.  I chose 1 usec. as the
    minimum sleep which would with reasonable certainty not be rounded
    down to 0.
    
    The formerly machine-independent file "lockdflt.c" has been moved
    into the architecture-specific subdirectories by repository copy.
    It now contains the machine-dependent spinlocking code.  For the
    spinlocks I used the very nifty "simple, non-scalable reader-preference
    lock" which I found at
    
      <http://www.cs.rochester.edu/u/scott/synchronization/pseudocode/rw.html>
    
    on all CPUs except the 80386 (the specific CPU model, not the
    architecture).  The 80386 CPU doesn't support the necessary "cmpxchg"
    instruction, so on that CPU a simple exclusive test-and-set lock
    is used instead.  80386 CPUs are detected at initialization time by
    trying to execute "cmpxchg" and catching the resulting SIGILL
    signal.
    
    To reduce contention for the locks, I have revamped a couple of
    key data structures, permitting all common operations to be done
    under non-exclusive (reader) locking.  The only operations that
    require exclusive locking now are the rare intrusive operations
    such as dlopen() and dlclose().
    
    The dllockinit() interface is now deprecated.  It still exists,
    but only as a do-nothing stub.  I plan to remove it as soon as is
    reasonably possible.  (From the very beginning it was clearly
    labeled as experimental and subject to change.)  As far as I know,
    only the linuxthreads port uses dllockinit().  This interface turned
    out to have several problems.  As one example, when the dynamic
    linker called a client-supplied locking function, that function
    sometimes needed lazy binding, causing re-entry into the dynamic
    linker and a big looping mess.  And in any case, it turned out to be
    too burdensome to require threads packages to register themselves
    with the dynamic linker.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@62801 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- libexec/rtld-elf/alpha/rtld_machdep.h
+++ libexec/rtld-elf/alpha/rtld_machdep.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 1999 John D. Polstra.
+ * Copyright (c) 1999, 2000 John D. Polstra.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -33,5 +33,11 @@
 #define rtld_dynamic(obj)	(&_DYNAMIC)
 
 void reloc_jmpslot(Elf_Addr *, Elf_Addr);
+
+/* Atomic operations. */
+int cmp0_and_store_int(volatile int *, int);
+void atomic_add_int(volatile int *, int);
+void atomic_incr_int(volatile int *);
+void atomic_decr_int(volatile int *);
 
 #endif
