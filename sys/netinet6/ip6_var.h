cef89d53e4b6d2024c7aa391bbd5edd7d1022355
itojun <itojun@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-07-12 16:39:13 +0000

    remove m_pulldown statistics, which is highly experimental and does not belong to *bsd-merged tree
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@63024 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet6/ip6_var.h
+++ sys/netinet6/ip6_var.h
@@ -170,20 +170,6 @@ struct	ip6stat {
 	u_quad_t ip6s_exthdrtoolong;	/* ext hdr are not continuous */
 	u_quad_t ip6s_nogif;		/* no match gif found */
 	u_quad_t ip6s_toomanyhdr;	/* discarded due to too many headers */
-	/* XXX the following two items are not really AF_INET6 thing */
-	u_quad_t ip6s_exthdrget;	/* # of calls to IP6_EXTHDR_GET */
-	u_quad_t ip6s_exthdrget0;	/* # of calls to IP6_EXTHDR_GET0 */
-	u_quad_t ip6s_pulldown;		/* # of calls to m_pulldown */
-	u_quad_t ip6s_pulldown_copy;	/* # of mbuf copies in m_pulldown */
-	u_quad_t ip6s_pulldown_alloc;	/* # of mbuf allocs in m_pulldown */
-	u_quad_t ip6s_pullup;		/* # of calls to m_pullup */
-	u_quad_t ip6s_pullup_copy;	/* # of possible m_pullup copies */
-	u_quad_t ip6s_pullup_alloc;	/* # of possible m_pullup mallocs */
-	u_quad_t ip6s_pullup_fail;	/* # of possible m_pullup failures */
-	u_quad_t ip6s_pullup2;		/* # of calls to m_pullup2 */
-	u_quad_t ip6s_pullup2_copy;	/* # of possible m_pullup2 copies */
-	u_quad_t ip6s_pullup2_alloc;	/* # of possible m_pullup2 mallocs */
-	u_quad_t ip6s_pullup2_fail;	/* # of possible m_pullup2 failures */
 
 	/*
 	 * statistics for improvement of the source address selection
