b0e7a9a2d1632a998888e7cdaf1f21eb719c0e0f
obrien <obrien@FreeBSD.org>
2000-07-16 06:40:42 +0000

    MFC: revs 1.28-29  (squeeze more out of the BOOTMFS install kernel)
    Approved by:	JKH

--- release/scripts/dokern.sh
+++ release/scripts/dokern.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# $FreeBSD$
+
 ARCH="`uname -m`"
 
 # First check for the standard x86 PC class.
@@ -9,15 +11,20 @@ sed	-e '/pty/d' \
 	-e '/pass/d' \
 	-e '/apm0/d' \
 	-e '/ppp/d' \
+	-e '/gif/d' \
+	-e '/faith/d' \
 	-e '/gzip/d' \
 	-e '/splash/d' \
-	-e '/IPv6/d' \
 	-e '/PROCFS/d' \
 	-e '/KTRACE/d' \
 	-e '/SYSVMSG/d' \
 	-e '/SOFTUPDATES/d' \
+	-e '/MFS/d' \
+	-e '/NFS_ROOT/d' \
+	-e '/RANDOMDEV/d' \
+	-e '/atapist/d' \
 	-e '/maxusers/d' \
-	-e 's/GENERIC/BOOTMFS/g'
+	-e 's/ident.*GENERIC/ident		BOOTMFS/g'
 
 echo "options  NETGRAPH"
 echo "options  NETGRAPH_PPPOE"
@@ -30,13 +37,33 @@ sed	-e '/pty/d' \
 	-e '/pass/d' \
 	-e '/apm0/d' \
 	-e '/ppp/d' \
+	-e '/gif/d' \
+	-e '/faith/d' \
 	-e '/gzip/d' \
 	-e '/splash/d' \
-	-e '/IPv6/d' \
 	-e '/PROCFS/d' \
 	-e '/KTRACE/d' \
 	-e '/SYSV/d' \
 	-e '/SOFTUPDATES/d' \
+	-e '/MFS/d' \
+	-e '/NFS_ROOT/d' \
+	-e '/RANDOMDEV/d' \
+	-e '/atapist/d' \
+	-e '/lpt/d' \
+	-e '/ppi/d' \
+	-e '/vpo/d' \
+	-e '/uhci/d' \
+	-e '/ohci/d' \
+	-e '/usb/d' \
+	-e '/ugen/d' \
+	-e '/uhid/d' \
+	-e '/ukbd/d' \
+	-e '/ulpt/d' \
+	-e '/umass/d' \
+	-e '/ums/d' \
+	-e '/aue/d' \
+	-e '/cue/d' \
+	-e '/kue/d' \
 	-e '/maxusers/d' \
 	-e 's/GENERIC/BOOTMFS/g'
 fi
