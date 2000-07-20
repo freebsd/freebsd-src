2109c578d019379385a9e8f5aa61afddc8b05f09
fenner <fenner@FreeBSD.org>
2000-07-20 21:26:45 +0000

    Import rev 1.26 of tcpdump.org's inet.c -- don't free   the buffer before it's used.

--- contrib/libpcap/inet.c
+++ contrib/libpcap/inet.c
@@ -33,7 +33,7 @@
 
 #ifndef lint
 static const char rcsid[] =
-    "@(#) $Header: /tcpdump/master/libpcap/inet.c,v 1.24.2.1 2000/01/14 18:00:50 mcr Exp $ (LBL)";
+    "@(#) $Header: /tcpdump/master/libpcap/inet.c,v 1.26 2000/01/14 23:55:31 mcr Exp $ (LBL)";
 #endif
 
 #include <sys/param.h>
@@ -173,15 +173,16 @@ pcap_lookupdev(errbuf)
 			mp = ifrp;
 		}
 	}
-	free(buf);
 	(void)close(fd);
 	if (mp == NULL) {
 		(void)strcpy(errbuf, "no suitable device found");
+		free(buf);
 		return (NULL);
 	}
 
 	(void)strncpy(device, mp->ifr_name, sizeof(device) - 1);
 	device[sizeof(device) - 1] = '\0';
+	free(buf);
 	return (device);
 }
 
