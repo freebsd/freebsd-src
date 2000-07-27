a794883a9610947866791035df670560758d49d0
jlemon <jlemon@FreeBSD.org>
2000-07-27 23:06:14 +0000

    Have kevent() automatically restart if interrupted by a signal.  If this is not desired, then the user can register an EV_SIGNAL filter to explicitly catch a signal event.
    Change requested by: jayanth, ps, peter
    		     "Why is kevent non-restartable after a signal?"

--- sys/kern/kern_event.c
+++ sys/kern/kern_event.c
@@ -557,12 +557,9 @@ start:
 		kq->kq_state |= KQ_SLEEP;
 		error = tsleep(kq, PSOCK | PCATCH, "kqread", timeout);
 		splx(s);
-		if (error == 0)
+		if (error == 0 || error == ERESTART)
 			goto retry;
-		/* don't restart after signals... */
-		if (error == ERESTART)
-			error = EINTR;
-		else if (error == EWOULDBLOCK)
+		if (error == EWOULDBLOCK)
 			error = 0;
 		goto done;
 	}
