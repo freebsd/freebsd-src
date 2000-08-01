fefa7044142dd7c01c47dedb36e413a1d02e21d8
Nick Hibma <n_hibma@FreeBSD.org>
2000-08-01 23:26:26 +0000

    MFC: make the recognition of Zip drives work.

--- sys/dev/ppbus/ppb_base.c
+++ sys/dev/ppbus/ppb_base.c
@@ -131,10 +131,11 @@ ppb_set_mode(device_t bus, int mode)
 	struct ppb_data *ppb = DEVTOSOFTC(bus);
 	int old_mode = ppb_get_mode(bus);
 
-	if (!PPBUS_SETMODE(device_get_parent(bus), mode)) {
+	if (PPBUS_SETMODE(device_get_parent(bus), mode))
+		return -1;
+
 		/* XXX yet device mode = ppbus mode = chipset mode */
 		ppb->mode = (mode & PPB_MASK);
-	}
 
 	return (old_mode);
 }
