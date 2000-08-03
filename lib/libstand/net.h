04f2dc8e0bc028215d67716bd57720b48e9867ff
jhb <jhb@FreeBSD.org>
2000-08-03 09:08:29 +0000

    Cleanup warnings by adding missint prototypes, removing unneeded duplicate prototypes, and adding in several 'const's.  Also, add some missing $FreeBSD$'s.
    Found by:	BDECFLAGS

--- lib/libstand/net.h
+++ lib/libstand/net.h
@@ -36,6 +36,8 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ * $FreeBSD$
  */
 
 #define NETIF_DEBUG 1
@@ -110,6 +112,9 @@ ssize_t	sendrecv(struct iodesc *,
 			void *, size_t,
 		        ssize_t (*)(struct iodesc *, void *, size_t, time_t),
 			void *, size_t);
+
+/* bootp/DHCP */
+void	bootp(int);
 
 /* Utilities: */
 char	*ether_sprintf(u_char *);
