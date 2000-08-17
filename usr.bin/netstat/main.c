726e250815d49c5bd3c1c3033abf34e3e8ba26c7
Ruslan Ermilov <ru@FreeBSD.org>
2000-08-17 07:25:38 +0000

    MFC: (rev 1.37) do not display icmp sockets as non-existent bridge sockets.

--- usr.bin/netstat/main.c
+++ usr.bin/netstat/main.c
@@ -174,7 +174,7 @@ struct protox {
 	{ -1,		N_IPSECSTAT,	1,	0,
 	  ipsec_stats,	NULL,		"ipsec",	0},
 #endif
-	{ -1,		-1,		1,	protopr,
+	{ -1,		-1,		1,	0,
 	  bdg_stats,	NULL,		"bdg",	1 /* bridging... */ },
 	{ -1,		-1,		0,	0,
 	  0,		NULL,		0 }
@@ -198,7 +198,7 @@ struct protox ip6protox[] = {
 	{ -1,		N_PIM6STAT,	1,	0,
 	  pim6_stats,	NULL,		"pim6",	0 },
 #endif
-	{ -1,		-1,		1,	protopr,
+	{ -1,		-1,		1,	0,
 	  bdg_stats,	NULL,		"bdg",	1 /* bridging... */ },
 	{ -1,		-1,		0,	0,
 	  0,		NULL,		0,	0 }
