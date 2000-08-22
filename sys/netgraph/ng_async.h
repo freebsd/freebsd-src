02f9596bb8b209cdb46f03805fc6ee2b7b597fd8
archie <archie@FreeBSD.org>
2000-08-22 18:52:16 +0000

    MFC: take advantage of the new unsigned and hex integer types

--- sys/netgraph/ng_async.h
+++ sys/netgraph/ng_async.h
@@ -71,14 +71,14 @@ struct ng_async_stat {
 /* Keep this in sync with the above structure definition */
 #define NG_ASYNC_STATS_TYPE_INFO	{			\
 	{							\
-	  { "syncOctets",	&ng_parse_int32_type	},	\
-	  { "syncFrames",	&ng_parse_int32_type	},	\
-	  { "syncOverflows",	&ng_parse_int32_type	},	\
-	  { "asyncOctets",	&ng_parse_int32_type	},	\
-	  { "asyncFrames",	&ng_parse_int32_type	},	\
-	  { "asyncRunts",	&ng_parse_int32_type	},	\
-	  { "asyncOverflows",	&ng_parse_int32_type	},	\
-	  { "asyncBadCheckSums",&ng_parse_int32_type	},	\
+	  { "syncOctets",	&ng_parse_uint32_type	},	\
+	  { "syncFrames",	&ng_parse_uint32_type	},	\
+	  { "syncOverflows",	&ng_parse_uint32_type	},	\
+	  { "asyncOctets",	&ng_parse_uint32_type	},	\
+	  { "asyncFrames",	&ng_parse_uint32_type	},	\
+	  { "asyncRunts",	&ng_parse_uint32_type	},	\
+	  { "asyncOverflows",	&ng_parse_uint32_type	},	\
+	  { "asyncBadCheckSums",&ng_parse_uint32_type	},	\
 	  { NULL },						\
 	}							\
 }
@@ -95,9 +95,9 @@ struct ng_async_cfg {
 #define NG_ASYNC_CONFIG_TYPE_INFO	{			\
 	{							\
 	  { "enabled",		&ng_parse_int8_type	},	\
-	  { "amru",		&ng_parse_int16_type	},	\
-	  { "smru",		&ng_parse_int16_type	},	\
-	  { "accm",		&ng_parse_int32_type	},	\
+	  { "amru",		&ng_parse_uint16_type	},	\
+	  { "smru",		&ng_parse_uint16_type	},	\
+	  { "accm",		&ng_parse_hint32_type	},	\
 	  { NULL },						\
 	}							\
 }
