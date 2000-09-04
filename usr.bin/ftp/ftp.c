b07b8563ff4c0802ccb1cefdc6aca67c8896c6d1
Warner Losh <imp@FreeBSD.org>
2000-09-04 05:59:25 +0000

    optarg and optind are declared in unistd.h. h_error is declared in netdb.h

--- usr.bin/ftp/ftp.c
+++ usr.bin/ftp/ftp.c
@@ -76,8 +76,6 @@ __RCSID_SOURCE("$NetBSD: ftp.c,v 1.29.2.1 1997/11/18 01:01:04 mellon Exp $");
 #define	NI_WITHSCOPEID	0
 #endif
 
-extern int h_errno;
-
 int	data = -1;
 int	abrtflag = 0;
 jmp_buf	ptabort;
