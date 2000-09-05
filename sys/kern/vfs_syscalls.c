d7aa95c1e7a0af6d3d29b063418e9ef540137ab9
Robert Watson <rwatson@FreeBSD.org>
2000-09-05 02:13:48 +0000

    o MFC of 1.165 o Remove commented out code which modified return values from   extattr_{get,set} syscalls in the face of partial reads or writes.
    Obtained from:	TrustedBSD Project

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -3454,9 +3454,6 @@ extattr_set_file(p, uap)
 	cnt = auio.uio_resid;
 	error = VOP_SETEXTATTR(nd.ni_vp, attrname, &auio, p->p_cred->pc_ucred,
 	    p);
-	if (auio.uio_resid != cnt && (error == ERESTART ||
-	    error == EINTR || error == EWOULDBLOCK))
-		error = 0;
 	cnt -= auio.uio_resid;
 	p->p_retval[0] = cnt;
 done:
@@ -3523,9 +3520,6 @@ extattr_get_file(p, uap)
 	cnt = auio.uio_resid;
 	error = VOP_GETEXTATTR(nd.ni_vp, attrname, &auio, p->p_cred->pc_ucred,
 	    p);
-	if (auio.uio_resid != cnt && (error == ERESTART ||
-	    error == EINTR || error == EWOULDBLOCK))
-		error = 0;
 	cnt -= auio.uio_resid;
 	p->p_retval[0] = cnt;
 done:
