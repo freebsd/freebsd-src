5d01b2c4647f1614ab7c160ceb78865a4cb66a25
brian <brian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-09-14 18:01:54 +0000

    Understand IPPROTO_ESP and IPPROTO_AH packets
    Submitted by: Angelos D. Keromytis <angelos@dsl.cis.upenn.edu>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@65846 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/ppp/ip.c
+++ usr.sbin/ppp/ip.c
@@ -269,6 +269,16 @@ FilterCheck(const struct ip *pip, const struct filter *filter, unsigned *psecs)
             sport = ntohs(0);
             break;
 #endif
+          case IPPROTO_ESP:
+            cproto = P_ESP;
+            estab = syn = finrst = -1;
+            sport = ntohs(0);
+            break;
+          case IPPROTO_AH:
+            cproto = P_AH;
+            estab = syn = finrst = -1;
+            sport = ntohs(0);
+            break;
           case IPPROTO_UDP:
           case IPPROTO_IPIP:
             cproto = P_UDP;
@@ -632,6 +642,30 @@ PacketCheck(struct bundle *bundle, unsigned char *cp, int nb,
       loglen += strlen(logbuf + loglen);
       snprintf(logbuf + loglen, sizeof logbuf - loglen,
                "%s:%d", inet_ntoa(pip->ip_dst), ntohs(uh->uh_dport));
+      loglen += strlen(logbuf + loglen);
+    }
+    break;
+
+  case IPPROTO_ESP:
+    if (logit && loglen < sizeof logbuf) {
+      snprintf(logbuf + loglen, sizeof logbuf - loglen,
+               "ESP: %s ---> ", inet_ntoa(pip->ip_src));
+      loglen += strlen(logbuf + loglen);
+      snprintf(logbuf + loglen, sizeof logbuf - loglen,
+               "%s, spi %08x", inet_ntoa(pip->ip_dst),
+               (u_int32_t) ptop);
+      loglen += strlen(logbuf + loglen);
+    }
+    break;
+
+  case IPPROTO_AH:
+    if (logit && loglen < sizeof logbuf) {
+      snprintf(logbuf + loglen, sizeof logbuf - loglen,
+               "AH: %s ---> ", inet_ntoa(pip->ip_src));
+      loglen += strlen(logbuf + loglen);
+      snprintf(logbuf + loglen, sizeof logbuf - loglen,
+               "%s, spi %08x", inet_ntoa(pip->ip_dst),
+               (u_int32_t) (ptop + sizeof(u_int32_t)));
       loglen += strlen(logbuf + loglen);
     }
     break;
