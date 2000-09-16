8f44e5f58ca082e0cfd6ffa47f43df0e9deab011
dcs <dcs@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-09-16 19:59:41 +0000

    The default search path for kernel and modules was bogus. It makes no sense for /boot/kernel to come last.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@65940 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/boot/common/module.c
+++ sys/boot/common/module.c
@@ -49,7 +49,7 @@ struct file_metadata*		metadata_next(struct file_metadata *base_mp, int type);
 /* load address should be tweaked by first module loaded (kernel) */
 static vm_offset_t	loadaddr = 0;
 
-static const char	*default_searchpath ="/boot/modules;/modules;/boot/kernel";
+static const char	*default_searchpath ="/boot/kernel;/boot/modules;/modules";
 
 struct preloaded_file *preloaded_files = NULL;
 
