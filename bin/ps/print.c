6d59ffa2c348c8b149fc3da6aaf34b354a151b33
Jordan K. Hubbard <jkh@FreeBSD.org>
2000-09-20 02:16:11 +0000

    MFC: spelling correction

--- bin/ps/print.c
+++ bin/ps/print.c
@@ -185,7 +185,7 @@ state(k, ve)
 		break;
 
 	case SSLEEP:
-		if (flag & P_SINTR)	/* interuptable (long) */
+		if (flag & P_SINTR)	/* interruptable (long) */
 			*cp = p->p_slptime >= MAXSLP ? 'I' : 'S';
 		else
 			*cp = 'D';
