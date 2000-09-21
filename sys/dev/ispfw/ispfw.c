31e58da1403adcad32c2afe66c841b194f511f49
mjacob <mjacob@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-09-21 20:16:04 +0000

    some copyright cleanups
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@66189 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ispfw/ispfw.c
+++ sys/dev/ispfw/ispfw.c
@@ -2,10 +2,8 @@
 /*
  * ISP Firmware Helper Pseudo Device for FreeBSD
  *
- *---------------------------------------
  * Copyright (c) 2000, by Matthew Jacob
  * All rights reserved.
- *---------------------------------------
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -13,9 +11,8 @@
  * 1. Redistributions of source code must retain the above copyright
  *    notice immediately at the beginning of the file, without modification,
  *    this list of conditions, and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
+ * 2. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
