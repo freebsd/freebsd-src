4976f189725e4a7f825a151306ca5bc704cf1703
gibbs <gibbs@FreeBSD.org>
2000-09-22 22:19:55 +0000

    Add Perforce RCSIDs for easy revision correlation to my local tree.
    Add support for constructing a table of critical section regions in
    the firmware image.  The kernel driver will soon have support for
    single stepping the sequencer outside of a critical region prior
    to starting exception handling.

--- sys/dev/aic7xxx/aicasm/aicasm_symbol.c
+++ sys/dev/aic7xxx/aicasm/aicasm_symbol.c
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id$
+ * $Id: //depot/src/aic7xxx/aicasm/aicasm_symbol.c#3 $
  *
  * $FreeBSD$
  */
