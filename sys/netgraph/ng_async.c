ee79f58e349ea3bbeec927c33e6a2b33c8154ed8
Archie Cobbs <archie@FreeBSD.org>
2000-09-22 16:51:14 +0000

    Remove unnecessary #include's as reported by phk's script.

--- sys/netgraph/ng_async.c
+++ sys/netgraph/ng_async.c
@@ -48,10 +48,8 @@
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kernel.h>
-#include <sys/proc.h>
 #include <sys/mbuf.h>
 #include <sys/malloc.h>
-#include <sys/file.h>
 #include <sys/errno.h>
 
 #include <netgraph/ng_message.h>
