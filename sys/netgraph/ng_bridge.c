7280bb2b3dab32a16f26e9dbb98f48764ca0aa0c
archie <archie@FreeBSD.org>
2000-09-23 23:22:27 +0000

    Use m_dup() instead of m_copypacket() for the time being. Not all of the code in the kernel properly checks for read-onlyness before writing into an mbuf data area. When that code is fixed, the m_dup() can go back to being m_copypacket().
    Requested by:	nsayer

--- sys/netgraph/ng_bridge.c
+++ sys/netgraph/ng_bridge.c
@@ -675,7 +675,7 @@ ng_bridge_rcvdata(hook_p hook, struct mbuf *m, meta_p meta,
 			m2 = m;
 			meta2 = meta;
 		}  else {
-			m2 = m_copypacket(m, M_NOWAIT);	     /* XXX m_dup()? */
+			m2 = m_dup(m, M_NOWAIT);	/* XXX m_copypacket() */
 			if (m2 == NULL) {
 				link->stats.memoryFailures++;
 				NG_FREE_DATA(m, meta);
