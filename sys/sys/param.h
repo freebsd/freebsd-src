66996099f0dee3da7d4bfce931d1e22fdb2a4c53
jkh <jkh@FreeBSD.org>
2000-09-25 06:52:21 +0000

    We're now FreeBSD 4.1.1

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -46,7 +46,7 @@
 #define BSD4_3	1
 #define BSD4_4	1
 #undef __FreeBSD_version
-#define __FreeBSD_version 410002	/* Master, propagated to newvers */
+#define __FreeBSD_version 411000	/* Master, propagated to newvers */
 
 #ifndef NULL
 #define	NULL	0
