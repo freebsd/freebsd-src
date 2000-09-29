1b4f9c99b835c9924e91542d3d3d8957bb836d33
Kris Kennaway <kris@FreeBSD.org>
2000-09-29 08:49:56 +0000

    MFC: Use stronger randomness for TCP_ISSINCR and tcp_iss

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -130,7 +130,7 @@ tcp_init()
 {
 	int hashsize;
 	
-	tcp_iss = random();	/* wrong, but better than a constant */
+	tcp_iss = arc4random();	/* wrong, but better than a constant */
 	tcp_ccgen = 1;
 	tcp_cleartaocache();
 	LIST_INIT(&tcb);
