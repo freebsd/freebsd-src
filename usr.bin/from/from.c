4c4d600f19db12b16577bf408f47b1eb4fda2ead
ru <ru@FreeBSD.org>
2000-10-06 08:19:19 +0000

    MFC: (rev 1.9) optarg and optind are now declared in <unistd.h>.      (rev 1.10) replaced unsafe use of sprintf() with snprintf().

--- usr.bin/from/from.c
+++ usr.bin/from/from.c
@@ -63,8 +63,6 @@ main(argc, argv)
 	int argc;
 	char **argv;
 {
-	extern char *optarg;
-	extern int optind;
 	struct passwd *pwd;
 	int ch, count, newline;
 	char *file, *sender, *p;
@@ -98,14 +96,14 @@ main(argc, argv)
 
 	if (!file) {
 		if (*argv) {
-			(void)sprintf(buf, "%s/%s", _PATH_MAILDIR, *argv);
+			(void)snprintf(buf, sizeof(buf), "%s/%s", _PATH_MAILDIR, *argv);
 			file  = buf;
 		} else {
 			if (!(file = getenv("MAIL"))) {
 				if (!(pwd = getpwuid(getuid())))
 					errx(1, "no password file entry for you");
 				file = pwd->pw_name;
-				(void)sprintf(buf,
+				(void)snprintf(buf, sizeof(buf),
 				    "%s/%s", _PATH_MAILDIR, file);
 				file = buf;
 			}
