30a889f40d0a96ec2c92146dfc0d26e315b203f0
alfred <alfred@FreeBSD.org>
2000-10-07 23:15:17 +0000

    Remove headers not needed.
    Pointed out by: phk

--- sys/netinet/accf_http.c
+++ sys/netinet/accf_http.c
@@ -30,23 +30,11 @@
 #define ACCEPT_FILTER_MOD
 
 #include <sys/param.h>
-#include <sys/systm.h>
-#include <sys/sysproto.h>
 #include <sys/kernel.h>
 #include <sys/proc.h>
-#include <sys/malloc.h> 
-#include <sys/unistd.h>
-#include <sys/file.h>
-#include <sys/fcntl.h>
-#include <sys/protosw.h>
 #include <sys/sysctl.h>
-#include <sys/socket.h>
 #include <sys/socketvar.h>
-#include <sys/stat.h>
 #include <sys/mbuf.h>
-#include <sys/resource.h>
-#include <sys/sysent.h>
-#include <sys/resourcevar.h>
 
 /* check for GET/HEAD */
 static void sohashttpget(struct socket *so, void *arg, int waitflag);
