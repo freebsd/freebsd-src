365f12a3c49e50c80d25e602541319498779d9da
bmilekic <bmilekic@FreeBSD.org>
2000-10-08 03:52:27 +0000

    Cleanup comment in machine/param.h regarding mbuf-related sizes, and get rid of MCLOFSET, which does not appear to be used anywhere anymore, and if it is, it probably shouldn't be.

--- sys/i386/include/param.h
+++ sys/i386/include/param.h
@@ -102,20 +102,16 @@
 
 /*
  * Constants related to network buffer management.
- * MCLBYTES must be no larger than CLBYTES (the software page size), and,
- * on machines that exchange pages of input or output buffers with mbuf
- * clusters (MAPPED_MBUFS), MCLBYTES must also be an integral multiple
- * of the hardware page size.
+ * MCLBYTES must be no larger than PAGE_SIZE.
  */
 #ifndef	MSIZE
 #define MSIZE		256		/* size of an mbuf */
 #endif	/* MSIZE */
 
 #ifndef	MCLSHIFT
-#define MCLSHIFT	11		/* convert bytes to m_buf clusters */
+#define MCLSHIFT	11		/* convert bytes to mbuf clusters */
 #endif	/* MCLSHIFT */
-#define MCLBYTES	(1 << MCLSHIFT)	/* size of an m_buf cluster */
-#define MCLOFSET	(MCLBYTES - 1)	/* offset within an m_buf cluster */
+#define MCLBYTES	(1 << MCLSHIFT)	/* size of an mbuf cluster */
 
 /*
  * Some macros for units conversion
