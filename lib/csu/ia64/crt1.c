17aba88ea3f39ae034b13e304fe2bbcd2d51fa10
dfr <dfr@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-10-14 16:38:08 +0000

    CSU code for ia64.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@67116 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ lib/csu/ia64/crt1.c
@@ -0,0 +1,115 @@
+/*-
+ * Copyright 1996-1998 John D. Polstra.
+ * All rights reserved.
+ * Copyright (c) 1995 Christopher G. Demetriou
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Christopher G. Demetriou
+ *    for the NetBSD Project.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef __GNUC__
+#error "GCC is needed to compile this file"
+#endif
+
+#include <stdlib.h>
+
+struct Struct_Obj_Entry;
+struct ps_strings;
+
+#pragma weak _DYNAMIC
+extern int _DYNAMIC;
+
+extern void _init(void);
+extern void _fini(void);
+extern int main(int, char **, char **);
+
+#ifdef GCRT
+extern void _mcleanup(void);
+extern void monstartup(void *, void *);
+extern int eprol;
+extern int etext;
+#endif
+
+char **environ;
+char *__progname = "";
+
+__asm ("
+	.text
+	.global _start
+	.proc _start
+_start:
+	alloc	r16=ar.pfs,0,0,3,0 ;;
+	mov	out0=sp
+	mov	out1=r14
+	mov	out2=r15
+	movl	gp=_GLOBAL_OFFSET_TABLE_
+	br.call.sptk rp=_start2
+	.endp	_start");
+
+/* The entry function. */
+void
+_start2(char **ap,
+       struct ps_strings *ps_strings,
+       void (*cleanup)(void))
+{
+	int argc;
+	char **argv;
+	char **env;
+
+	argc = * (long *) ap;
+	argv = ap + 1;
+	env  = ap + 2 + argc;
+	environ = env;
+	if(argc > 0 && argv[0] != NULL) {
+		char *s;
+		__progname = argv[0];
+		for (s = __progname; *s != '\0'; s++)
+			if (*s == '/')
+				__progname = s + 1;
+	}
+
+	if (&_DYNAMIC != NULL)
+		atexit(cleanup);
+
+#ifdef GCRT
+	atexit(_mcleanup);
+#endif
+	atexit(_fini);
+#ifdef GCRT
+	monstartup(&eprol, &etext);
+#endif
+	_init();
+	exit( main(argc, argv, env) );
+}
+
+#ifdef GCRT
+__asm__(".text");
+__asm__("eprol:");
+__asm__(".previous");
+#endif
