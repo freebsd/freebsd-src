af84d4dcdc600d52abcbf06b7c8d10a58940f3d4
peter <peter@FreeBSD.org>
2000-10-15 08:50:45 +0000

    Untangle some resource matching loops that were getting on my nerves and seemed to be getting cut/pasted to places they shouldn't be.

--- sys/dev/atkbdc/atkbdc_isa.c
+++ sys/dev/atkbdc/atkbdc_isa.c
@@ -173,6 +173,7 @@ atkbdc_attach(device_t dev)
 	int		error;
 	int		rid;
 	int		i;
+	const char	*name;
 
 	unit = device_get_unit(dev);
 	sc = *(atkbdc_softc_t **)device_get_softc(dev);
@@ -213,22 +214,20 @@ atkbdc_attach(device_t dev)
 	/*
 	 * Add all devices configured to be attached to atkbdc0.
 	 */
-	for (i = resource_query_string(-1, "at", device_get_nameunit(dev));
-	     i != -1;
-	     i = resource_query_string(i, "at", device_get_nameunit(dev))) {
+	name = device_get_nameunit(dev);
+	i = -1;
+	while ((i = resource_query_string(i, "at", name)) != -1)
 		atkbdc_add_device(dev, resource_query_name(i),
 				  resource_query_unit(i));
-	}
 
 	/*
 	 * and atkbdc?
 	 */
-	for (i = resource_query_string(-1, "at", device_get_name(dev));
-	     i != -1;
-	     i = resource_query_string(i, "at", device_get_name(dev))) {
+	name = device_get_name(dev);
+	i = -1;
+	while ((i = resource_query_string(i, "at", name)) != -1)
 		atkbdc_add_device(dev, resource_query_name(i),
 				  resource_query_unit(i));
-	}
 
 	bus_generic_attach(dev);
 
