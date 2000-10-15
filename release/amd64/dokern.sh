d95c3401119efed7568055ce3218380fcc3e6150
obrien <obrien@FreeBSD.org>
2000-10-15 01:23:02 +0000

    I don't think we'll ever take any input (or send output) to a Diamond Rio 500 MP3 player during an installation.

--- release/amd64/dokern.sh
+++ release/amd64/dokern.sh
@@ -53,6 +53,7 @@ sed	-e '/	pci$/d' \
 	-e '/MFS/d' \
 	-e '/NFS_ROOT/d' \
 	-e '/atapist/d' \
+	-e '/urio/d' \
 	-e '/maxusers/d' \
 	-e 's/ident.*GENERIC/ident		BOOTMFS/g'
 
@@ -73,6 +74,7 @@ sed	-e '/pty/d' \
 	-e '/MFS/d' \
 	-e '/NFS_ROOT/d' \
 	-e '/atapist/d' \
+	-e '/urio/d' \
 	-e '/maxusers/d' \
 	-e 's/ident.*GENERIC/ident		BOOTMFS/g'
 
@@ -115,6 +117,7 @@ sed	-e '/pty/d' \
 	-e '/ulpt/d' \
 	-e '/umass/d' \
 	-e '/ums/d' \
+	-e '/urio/d' \
 	-e '/aue/d' \
 	-e '/cue/d' \
 	-e '/kue/d' \
