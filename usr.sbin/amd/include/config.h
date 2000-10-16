d7dcad45d78d5bcc39466a1b3ca8911672027ced
Brian Somers <brian@FreeBSD.org>
2000-10-16 09:14:44 +0000

    Include sys/types.h

--- usr.sbin/amd/include/config.h
+++ usr.sbin/amd/include/config.h
@@ -936,6 +936,7 @@
 /* Define if your processor stores words with the most significant
    byte first (like Motorola and SPARC, unlike Intel and VAX).  */
 /* #undef WORDS_BIGENDIAN */
+#include <sys/types.h>
 #include <machine/endian.h>
 #if BYTE_ORDER == BIG_ENDIAN
 #define WORDS_BIGENDIAN
