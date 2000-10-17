793633949553d1b81882833453269637946522b7
Andrew Gallatin <gallatin@FreeBSD.org>
2000-10-17 00:00:20 +0000

    A start at an implemention of linux_rt_sendsig & linux_rt_sigreturn and associated user-level signal trampoline glue.
    Without this patch, an SA_SIGINFO style handler can be installed by a linux
    app, but if the handler accesses its sip argument, it will get a garbage
    pointer and likely segfault.
    
    We currently supply a valid pointer, but its contents are mainly
    garbage.  Filling this in properly is future work.
    
    This is the second of 3 commits that will get IBM's JDK 1.3 working with
    FreeBSD ...

--- sys/i386/linux/linux_genassym.c
+++ sys/i386/linux/linux_genassym.c
@@ -11,3 +11,7 @@ ASSYM(LINUX_SIGF_HANDLER, offsetof(struct linux_sigframe, sf_handler));
 ASSYM(LINUX_SIGF_SC, offsetof(struct linux_sigframe, sf_sc));
 ASSYM(LINUX_SC_GS, offsetof(struct linux_sigcontext, sc_gs));
 ASSYM(LINUX_SC_EFLAGS, offsetof(struct linux_sigcontext, sc_eflags));
+ASSYM(LINUX_RT_SIGF_HANDLER, offsetof(struct linux_rt_sigframe, sf_handler));
+ASSYM(LINUX_RT_SIGF_UC, offsetof(struct linux_rt_sigframe, sf_sc));
+
+
--- sys/i386/linux/linux_locore.s
+++ sys/i386/linux/linux_locore.s
@@ -14,11 +14,27 @@ NON_GPROF_ENTRY(linux_sigcode)
 	int	$0x80				/* enter kernel with args */
 0:	jmp	0b
 	ALIGN_TEXT
+/* XXXXX */
+	
+_linux_rt_sigcode:
+	call	*LINUX_RT_SIGF_HANDLER(%esp)
+	leal	LINUX_RT_SIGF_UC(%esp),%ebx	/* linux ucp */
+	movl	LINUX_SC_GS(%ebx),%gs
+	push	%eax				/* fake ret addr */
+	movl	$LINUX_SYS_linux_rt_sigreturn,%eax	/* linux_rt_sigreturn() */
+	int	$0x80				/* enter kernel with args */
+0:	jmp	0b
+	ALIGN_TEXT
+/* XXXXX */	
 _linux_esigcode:
 
 	.data
-	.globl	_linux_szsigcode
+	.globl	_linux_szsigcode, _linux_sznonrtsigcode
 _linux_szsigcode:
 	.long	_linux_esigcode-_linux_sigcode
-
+_linux_sznonrtsigcode:
+	.long	_linux_rt_sigcode-_linux_sigcode
 	.text
+
+
+	
