499a484b0938cc2ad86450a18a41c86dd4a5a088
obrien <obrien@FreeBSD.org>
2000-10-23 08:15:58 +0000

    Minimal set of diffs from the i386 bits to the AlphaLinux.

--- sys/alpha/linux/linux_genassym.c
+++ sys/alpha/linux/linux_genassym.c
@@ -5,7 +5,7 @@
 #include <sys/param.h>
 #include <sys/assym.h>
 
-#include <i386/linux/linux.h>
+#include <alpha/linux/linux.h>
 
 ASSYM(LINUX_SIGF_HANDLER, offsetof(struct linux_sigframe, sf_handler));
 ASSYM(LINUX_SIGF_SC, offsetof(struct linux_sigframe, sf_sc));
