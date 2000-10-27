5bfbb02805e4ab5b15f62d46e0118db642611ff3
phk <phk@FreeBSD.org>
2000-10-27 21:32:10 +0000

    Remove 86 unneeded #includes

--- sys/dev/hea/eni_if.c
+++ sys/dev/hea/eni_if.c
@@ -36,14 +36,9 @@
  */
 
 #include <sys/param.h>
-#include <sys/systm.h>
-#include <sys/types.h>
-#include <sys/errno.h>
-#include <sys/time.h>
 #include <sys/socket.h>
 #include <sys/socketvar.h>
 #include <net/if.h>
-#include <netinet/in.h>
 #include <netatm/port.h>
 #include <netatm/queue.h>
 #include <netatm/atm.h>
