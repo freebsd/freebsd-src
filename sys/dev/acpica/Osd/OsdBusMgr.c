64e150eaa441bc8b9ecc4f649c022b7d522e487f
msmith <msmith@FreeBSD.org>
2000-10-28 06:56:15 +0000

    FreeBSD-specific OSD (operating system dependant) modules for the Intel ACPICA code.

---
+++ sys/dev/acpica/Osd/OsdBusMgr.c
@@ -0,0 +1,147 @@
+/*-
+ * Copyright (c) 2000 Michael Smith
+ * Copyright (c) 2000 BSDi
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	$FreeBSD$
+ */
+
+/*
+ * OSD interfaces for the BusMgr module
+ */
+
+#include "acpi.h"
+#include "bmosd.h"
+
+#include <sys/eventhandler.h>
+#include <sys/reboot.h>
+
+struct osd_eventhandle {
+    eventhandler_tag	tag;
+    OSD_IDLE_HANDLER	Function;
+    void		*Context;
+};
+
+#define NUM_IDLEHANDLERS	10
+struct osd_eventhandle idlehandlers[NUM_IDLEHANDLERS] = {{0, 0}};
+#define NUM_SHUTDOWNHANDLERS	20
+struct osd_eventhandle shutdownhandlers[NUM_SHUTDOWNHANDLERS] = {{0, 0}};
+
+static void
+osd_idlehandler(void *arg, int junk)
+{
+    struct osd_eventhandle	*oh = (struct osd_eventhandle *)arg;
+
+    oh->Function(oh->Context);
+}
+
+static void
+osd_shutdownhandler(void *arg, int howto)
+{
+    struct osd_eventhandle	*oh = (struct osd_eventhandle *)arg;
+
+    oh->Function(oh->Context);
+}
+
+ACPI_STATUS
+AcpiOsInstallIdleHandler(OSD_IDLE_HANDLER Function, void *Context)
+{
+    int	i;
+
+    if (Function == NULL)
+	return(AE_BAD_PARAMETER);
+    for (i = 0; i < NUM_IDLEHANDLERS; i++) {
+	if (idlehandlers[i].Function == NULL) {
+	    idlehandlers[i].Function = Function;
+	    idlehandlers[i].Context = Context;
+	    idlehandlers[i].tag = EVENTHANDLER_FAST_REGISTER(idle_event, osd_idlehandler, 
+							     &idlehandlers[i], IDLE_PRI_FIRST);
+	    return(AE_OK);
+	}
+    }
+    return(AE_NO_MEMORY);
+}
+
+ACPI_STATUS
+AcpiOsRemoveIdleHandler(OSD_IDLE_HANDLER Function)
+{
+    int i;
+
+    if (Function == NULL)
+	return(AE_BAD_PARAMETER);
+    for (i = 0; i < NUM_IDLEHANDLERS; i++) {
+	if (idlehandlers[i].Function == Function) {
+	    EVENTHANDLER_FAST_DEREGISTER(idle_event, idlehandlers[i].tag);
+	    idlehandlers[i].Function = NULL;
+	    return(AE_OK);
+	}
+    }
+    return(AE_NOT_EXIST);
+}
+
+/*
+ * It's not clear where exactly in the shutdown order these should be
+ * queued.
+ */
+ACPI_STATUS
+AcpiOsInstallShutdownHandler(OSD_SHUTDOWN_HANDLER Function, void *Context)
+{
+    int	i;
+
+    if (Function == NULL)
+	return(AE_BAD_PARAMETER);
+    for (i = 0; i < NUM_SHUTDOWNHANDLERS; i++) {
+	if (shutdownhandlers[i].Function == NULL) {
+	    shutdownhandlers[i].Function = Function;
+	    shutdownhandlers[i].Context = Context;
+	    shutdownhandlers[i].tag = EVENTHANDLER_REGISTER(shutdown_final, osd_shutdownhandler, 
+							     &shutdownhandlers[i], SHUTDOWN_PRI_LAST);
+	    return(AE_OK);
+	}
+    }
+    return(AE_NO_MEMORY);
+}
+
+ACPI_STATUS
+AcpiOsRemoveShutdownHandler(OSD_SHUTDOWN_HANDLER Function)
+{
+    int i;
+
+    if (Function == NULL)
+	return(AE_BAD_PARAMETER);
+    for (i = 0; i < NUM_SHUTDOWNHANDLERS; i++) {
+	if (shutdownhandlers[i].Function == Function) {
+	    EVENTHANDLER_DEREGISTER(shutdown_final, shutdownhandlers[i].tag);
+	    shutdownhandlers[i].Function = NULL;
+	    return(AE_OK);
+	}
+    }
+    return(AE_NOT_EXIST);
+}
+
+ACPI_STATUS
+AcpiOsShutdown (void)
+{
+    shutdown_nice(0);
+}
---
+++ sys/dev/acpica/Osd/OsdSchedule.c
@@ -0,0 +1,132 @@
+/*-
+ * Copyright (c) 2000 Michael Smith
+ * Copyright (c) 2000 BSDi
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	$FreeBSD$
+ */
+
+/*
+ * 6.3 : Scheduling services
+ */
+
+#include "acpi.h"
+
+#include <sys/kernel.h>
+#include <sys/malloc.h>
+#include <sys/taskqueue.h>
+#include <machine/clock.h>
+
+/*
+ * This is a little complicated due to the fact that we need to build and then
+ * free a 'struct task' for each task we enqueue.
+ *
+ * We use the default taskqueue_swi queue, since it really doesn't matter what
+ * else we're queued along with.
+ */
+
+MALLOC_DEFINE(M_ACPITASK, "acpitask", "ACPI deferred task");
+
+static void	AcpiOsExecuteQueue(void *arg, int pending);
+
+struct acpi_task {
+    struct task			at_task;
+    OSD_EXECUTION_CALLBACK	at_function;
+    void			*at_context;
+};
+
+ACPI_STATUS
+AcpiOsQueueForExecution(UINT32 Priority, OSD_EXECUTION_CALLBACK Function, void *Context)
+{
+    struct acpi_task	*at;
+
+    if (Function == NULL)
+	return(AE_BAD_PARAMETER);
+
+    /* XXX is it OK to block here? */
+    at = malloc(sizeof(*at), M_ACPITASK, M_WAITOK);
+    bzero(at, sizeof(*at));
+
+    at->at_function = Function;
+    at->at_context = Context;
+    at->at_task.ta_func = AcpiOsExecuteQueue;
+    at->at_task.ta_context = at;
+    switch (Priority) {
+    case OSD_PRIORITY_HIGH:
+	at->at_task.ta_priority = 3;
+	break;
+    case OSD_PRIORITY_MED:
+	at->at_task.ta_priority = 2;
+	break;
+    case OSD_PRIORITY_LO:
+	at->at_task.ta_priority = 1;
+	break;
+    default:
+	free(at, M_ACPITASK);
+	return(AE_BAD_PARAMETER);
+    }
+
+    taskqueue_enqueue(taskqueue_swi, (struct task *)at);
+    return(AE_OK);
+}
+
+static void
+AcpiOsExecuteQueue(void *arg, int pending)
+{
+    struct acpi_task		*at = (struct acpi_task *)arg;
+    OSD_EXECUTION_CALLBACK	Function;
+    void			*Context;
+
+    Function = (OSD_EXECUTION_CALLBACK)at->at_function;
+    Context = at->at_context;
+
+    free(at, M_ACPITASK);
+
+    Function(Context);
+}
+
+/*
+ * We don't have any sleep granularity better than hz, so
+ * make do with that.
+ */
+void
+AcpiOsSleep (UINT32 Seconds, UINT32 Milliseconds)
+{
+    int		timo;
+
+    timo = (Seconds * hz) + Milliseconds / (1000 * hz);
+    if (timo == 0)
+	timo = 1;
+    tsleep(NULL, 0, "acpislp", timo);
+}
+
+void
+AcpiOsSleepUsec (UINT32 Microseconds)
+{
+    if (Microseconds > 1000) {	/* long enough to be worth the overhead of sleeping */
+	AcpiOsSleep(0, Microseconds / 1000);
+    } else {
+	DELAY(Microseconds);
+    }
+}
