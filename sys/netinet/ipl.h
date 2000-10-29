f0af50f6d83d477f68fe4441d17c0d48d2142627
darrenr <darrenr@FreeBSD.org>
2000-10-29 07:50:11 +0000

    Import IP filter 3.4.13

--- sys/netinet/ipl.h
+++ sys/netinet/ipl.h
@@ -6,12 +6,12 @@
  * to the original author and the contributors.
  *
  * @(#)ipl.h	1.21 6/5/96
- * $Id: ipl.h,v 2.15.2.13 2000/10/25 11:08:41 darrenr Exp $
+ * $Id: ipl.h,v 2.15.2.14 2000/10/27 22:54:41 darrenr Exp $
  */
 
 #ifndef	__IPL_H__
 #define	__IPL_H__
 
-#define	IPL_VERSION	"IP Filter: v3.4.12"
+#define	IPL_VERSION	"IP Filter: v3.4.13"
 
 #endif
