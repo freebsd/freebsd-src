590563f02c09066f7364e7bbdb64d2ff0d5444f3
obrien <obrien@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-11-01 08:48:53 +0000

    I don't know why this used to compile for me, but now struct linux_sigcontext seems to have changed and some members are gone.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@68158 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/alpha/linux/linux_genassym.c
+++ sys/alpha/linux/linux_genassym.c
@@ -9,5 +9,5 @@
 
 ASSYM(LINUX_SIGF_HANDLER, offsetof(struct linux_sigframe, sf_handler));
 ASSYM(LINUX_SIGF_SC, offsetof(struct linux_sigframe, sf_sc));
-ASSYM(LINUX_SC_GS, offsetof(struct linux_sigcontext, sc_gs));
-ASSYM(LINUX_SC_EFLAGS, offsetof(struct linux_sigcontext, sc_eflags));
+/* ASSYM(LINUX_SC_GS, offsetof(struct linux_sigcontext, sc_gs)); */
+/* ASSYM(LINUX_SC_EFLAGS, offsetof(struct linux_sigcontext, sc_eflags)); */
