f95e798de355aef37ac46a3f764905a0d0b6c0e6
obrien <obrien@FreeBSD.org>
2000-11-05 09:06:06 +0000

    Switch over to using the Christos Zoulas maintained version in contrib/ This also gives use the same exact results as NetBSD, thus sharing more code with our bretheren.

---
+++ usr.bin/file/config.h
@@ -0,0 +1,81 @@
+/* $FreeBSD$ */
+
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define if your struct stat has st_rdev.  */
+#define HAVE_ST_RDEV 1
+
+/* Define if you have <sys/wait.h> that is POSIX.1 compatible.  */
+#define HAVE_SYS_WAIT_H 1
+
+/* Define if major, minor, and makedev are declared in <mkdev.h>.  */
+/* #undef MAJOR_IN_MKDEV */
+
+/* Define if major, minor, and makedev are declared in <sysmacros.h>.  */
+/* #undef MAJOR_IN_SYSMACROS */
+
+/* Define to `long' if <sys/types.h> doesn't define.  */
+/* #undef off_t */
+
+/* Define to `unsigned' if <sys/types.h> doesn't define.  */
+/* #undef size_t */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if builtin ELF support is enabled.  */
+#define BUILTIN_ELF 1
+
+/* Define if ELF core file support is enabled.  */
+#define ELFCORE 1
+
+/* Define if the `long long' type works.  */
+#define HAVE_LONG_LONG 1
+
+/* Define to `unsigned char' if standard headers don't define.  */
+/* #undef uint8_t */
+
+/* Define to `unsigned short' if standard headers don't define.  */
+/* #undef uint16_t */
+
+/* Define to `unsigned int' if standard headers don't define.  */
+/* #undef uint32_t */
+
+/* Define to `unsigned long long', if available, or `unsigned long', if
+   standard headers don't define.  */
+/* #undef uint64_t */
+
+/* The number of bytes in a uint8_t.  */
+#define SIZEOF_UINT8_T 1
+
+/* The number of bytes in a uint16_t.  */
+#define SIZEOF_UINT16_T 2
+
+/* The number of bytes in a uint32_t.  */
+#define SIZEOF_UINT32_T 4
+
+/* The number of bytes in a uint64_t.  */
+#define SIZEOF_UINT64_T 8
+
+/* Define if you have the strerror function.  */
+#define HAVE_STRERROR 1
+
+/* Define if you have the strtoul function.  */
+#define HAVE_STRTOUL 1
+
+/* Define if you have the <locale.h> header file.  */
+#define HAVE_LOCALE_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Name of package */
+#define PACKAGE "file"
+
+/* Version number of package */
+#define VERSION "3.32"
+
