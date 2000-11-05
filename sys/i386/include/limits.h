89df7f46c9ff51a4b3bcecd8b914d5452e7805a5
obrien <obrien@FreeBSD.org>
2000-11-05 09:21:42 +0000

    MFC: rev 1.16 (SHRT_MIN definition was 4 bits too long)
    Approved by:	JKH

--- sys/i386/include/limits.h
+++ sys/i386/include/limits.h
@@ -65,7 +65,7 @@
 
 #define	USHRT_MAX	0xffff		/* max value for an unsigned short */
 #define	SHRT_MAX	0x7fff		/* max value for a short */
-#define	SHRT_MIN	(-0x7ffff - 1)	/* min value for a short */
+#define	SHRT_MIN	(-0x7fff - 1)	/* min value for a short */
 
 #define	UINT_MAX	0xffffffffU	/* max value for an unsigned int */
 #define	INT_MAX		0x7fffffff	/* max value for an int */
