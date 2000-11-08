e251beeafaea4e7f3798b372c1d871fb83ce9cda
sanpei <sanpei@FreeBSD.org>
2000-11-08 14:43:40 +0000

    add a couple ESS Technology products(pci device id only).
    	FreeBSD src/sys/dev/sound/pcm/solo.c
    	NetBSD syssrc/sys/dev/pci/pcidevs
    	OpenBSD src/sys/dev/pci/pcidevs

--- sys/pci/pcisupport.c
+++ sys/pci/pcisupport.c
@@ -1355,10 +1355,24 @@ pci_chip_match(device_t dev)
 		return ("NeoMagic MagicMedia 256ZX Audio controller");
 
 	/* ESS Technology Inc -- vendor 0x125d */
+	case 0x0100125d:
+		return ("ESS Technology Maestro 1 Audio controller");
+	case 0x1818125d:
+		return ("ESS Technology Solo-1 Audio controller");
+	case 0x1968125d:
+		return ("ESS Technology Maestro 2 Audio controller");
 	case 0x1969125d:
 		return ("ESS Technology Solo-1 Audio controller");
 	case 0x1978125d:
 		return ("ESS Technology Maestro 2E Audio controller");
+	case 0x1998125d:
+		return ("ESS Technology Maestro 3 Audio controller");
+	case 0x1999125d:
+		return ("ESS Technology Maestro 3 Modem");
+	case 0x2898125d:
+		return ("ESS Technology ES2898 Modem");
+	case 0x8888125d:
+		return ("ESS Technology Solo-1E Audio controller");
 
 	/* Aureal Inc.-- vendor 0x12eb */
 	case 0x000112eb:
