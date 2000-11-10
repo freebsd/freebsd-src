935db20592bcc7fc323ff1a1c490ef15b7096ac9
gibbs <gibbs@FreeBSD.org>
2000-11-10 19:54:17 +0000

    Sync perforce IDs.

--- sys/dev/aic7xxx/aicasm/aicasm.c
+++ sys/dev/aic7xxx/aicasm/aicasm.c
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: //depot/src/aic7xxx/aicasm/aicasm.c#5 $
+ * $Id: //depot/src/aic7xxx/aicasm/aicasm.c#6 $
  *
  * $FreeBSD$
  */
