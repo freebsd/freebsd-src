b7db1f98188cf0f6f7b04e9b75ae60edb8490042
Bosko Milekic <bmilekic@FreeBSD.org>
2000-11-11 23:08:22 +0000

    Change MEXTADD usage to pass the two new arguments.
    Reviewed by: jlemon

--- sys/pci/if_wb.c
+++ sys/pci/if_wb.c
@@ -1106,7 +1106,8 @@ static int wb_newbuf(sc, c, m)
 		}
 		m_new->m_data = c->wb_buf;
 		m_new->m_pkthdr.len = m_new->m_len = WB_BUFBYTES;
-		MEXTADD(m_new, c->wb_buf, WB_BUFBYTES, wb_bfree, NULL);
+		MEXTADD(m_new, c->wb_buf, WB_BUFBYTES, wb_bfree, NULL, 0,
+		    EXT_NET_DRV);
 	} else {
 		m_new = m;
 		m_new->m_len = m_new->m_pkthdr.len = WB_BUFBYTES;
