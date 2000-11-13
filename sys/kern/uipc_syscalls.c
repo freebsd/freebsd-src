3fd243ed59a5800d389c63221a8d12d24738404f
David Greenman <dg@FreeBSD.org>
2000-11-13 07:15:44 +0000

    Merged rev 1.76: Fix for a page not busy panic when an IO error occurs.

--- sys/kern/uipc_syscalls.c
+++ sys/kern/uipc_syscalls.c
@@ -1607,8 +1607,10 @@ retry_lookup:
 				 */
 				if (pg->wire_count == 0 && pg->valid == 0 &&
 				    pg->busy == 0 && !(pg->flags & PG_BUSY) &&
-				    pg->hold_count == 0)
+				    pg->hold_count == 0) {
+					vm_page_busy(pg);
 					vm_page_free(pg);
+				}
 				sbunlock(&so->so_snd);
 				goto done;
 			}
