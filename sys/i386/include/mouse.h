13772d628f4fa9b653cc48a314d5ac067804e83d
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-11-20 22:00:25 +0000

    Make programs which still #include <machine/{mouse,console}.h> fail at compiletime, with an explanatory error message.  Previously they would only get a warning.
    These files will be finally removed 2001-01-15
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@68974 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/include/mouse.h
+++ sys/i386/include/mouse.h
@@ -1,6 +1,4 @@
 /* $FreeBSD$ */
 #if __GNUC__
-#warning "this file includes <machine/mouse.h> which is deprecated, use <sys/mouse.h> instead"
+#error "this file includes <machine/mouse.h> which is deprecated, use <sys/mouse.h> instead"
 #endif
-
-#include <sys/mouse.h>
--- sys/ia64/include/console.h
+++ sys/ia64/include/console.h
@@ -1,14 +1,4 @@
 /* $FreeBSD$ */
-
-#ifndef _MACHINE_CONSOLE_H_
-#define _MACHINE_CONSOLE_H_
-
 #if __GNUC__
-#warning "this file includes <machine/console.h> which is deprecated, use <sys/{kb,cons,fb}io.h> instead"
+#error "this file includes <machine/console.h> which is deprecated, use <sys/{kb,cons,fb}io.h> instead"
 #endif
-
-#include <sys/fbio.h>
-#include <sys/kbio.h>
-#include <sys/consio.h>
-
-#endif /* !_MACHINE_CONSOLE_H_ */
