92b1b8c9b1291b9d2f27707f0989be9c7e8e476c
imp <imp@FreeBSD.org>
2000-11-21 20:03:38 +0000

    MFC: Many long filename buffer overflows

--- usr.sbin/config/main.c
+++ usr.sbin/config/main.c
@@ -98,7 +98,7 @@ main(argc, argv)
 		switch (ch) {
 		case 'd':
 			if (*destdir == '\0')
-				strcpy(destdir, optarg);
+				strlcpy(destdir, optarg, sizeof(destdir));
 			else
 				errx(2, "directory already set");
 			break;
@@ -135,8 +135,8 @@ main(argc, argv)
 			destdir[--len] = '\0';
 		get_srcdir();
 	} else {
-		strcpy(destdir, CDIR);
-		strcat(destdir, PREFIX);
+		strlcpy(destdir, CDIR, sizeof(destdir));
+		strlcat(destdir, PREFIX, sizeof(destdir));
 	}
 
 	p = path((char *)NULL);
@@ -183,7 +183,7 @@ main(argc, argv)
 	 * and similarly for "machine".
 	 */
 	{
-	char xxx[80];
+	char xxx[MAXPATHLEN];
 	if (*srcdir == '\0')
 		(void)snprintf(xxx, sizeof(xxx), "../../%s/include",
 		    machinename);
