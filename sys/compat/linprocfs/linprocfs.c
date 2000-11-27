e6aa0256c7f725ba0a52ec59d970ce5428b91199
des <des@FreeBSD.org>
2000-11-27 11:10:03 +0000

    Add bogomips to cpuinfo (set it equal to the CPU frequency, which is bogus but not more so than Linux' definition). This should get the IBM JDK 1.3 working again.
    Prompted by:	sobomax

--- sys/compat/linprocfs/linprocfs.c
+++ sys/compat/linprocfs/linprocfs.c
@@ -218,15 +218,15 @@ linprocfs_docpuinfo(curp, p, pfs, uio)
 
 	ps = psbuf;
 	ps += sprintf(ps,
-			"processor       : %d\n"
-			"vendor_id       : %.20s\n"
-			"cpu family      : %d\n"
-			"model           : %d\n"
-			"stepping        : %d\n",
+			"processor\t: %d\n"
+			"vendor_id\t: %.20s\n"
+			"cpu family\t: %d\n"
+			"model\t\t: %d\n"
+			"stepping\t: %d\n",
 			0, cpu_vendor, class, cpu, cpu_id & 0xf);
 
         ps += sprintf(ps,
-                        "flags           :");
+                        "flags\t\t:");
 
         if (!strcmp(cpu_vendor, "AuthenticAMD") && (class < 6)) {
 		flags[16] = "fcmov";
@@ -240,7 +240,10 @@ linprocfs_docpuinfo(curp, p, pfs, uio)
 	ps += sprintf(ps, "\n");
         if (class >= 5) {
 		ps += sprintf(ps,
-			"cpu MHz         : %d.%02d\n",
+			"cpu MHz\t\t: %d.%02d\n"
+			"bogomips\t: %d.%02d\n",
+                        (tsc_freq + 4999) / 1000000,
+                        ((tsc_freq + 4999) / 10000) % 100,
                         (tsc_freq + 4999) / 1000000,
                         ((tsc_freq + 4999) / 10000) % 100);
         }
