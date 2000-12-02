3d21e45d3685657dc33ae981b9708d09ef869241
sanpei <sanpei@FreeBSD.org>
2000-12-02 12:49:56 +0000

    [MFC] Add PC-Card melody beep(PC Card bus, kludge version)
    Original idea from:	PAO3

--- sys/pccard/driver.h
+++ sys/pccard/driver.h
@@ -19,6 +19,6 @@ void	pccard_insert_beep __P((void));
 void	pccard_remove_beep __P((void));
 void	pccard_success_beep __P((void));
 void	pccard_failure_beep __P((void));
-int	pccard_beep_select __P((enum beepstate));
+int	pccard_beep_select __P((int));
 
 #endif /* !_PCCARD_DRIVER_H_ */
