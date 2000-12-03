b34e81139a862200f91025a1a58308ca77831895
abial <abial@FreeBSD.org>
2000-12-03 00:09:08 +0000

    Properly check the return values from malloc(3). Remove some PNP-related dead code that is unlikely to survive the changes in -current PNP anyway.
    Submitted by:	fixes from Chris Faulhaber <jedgar@freebsd.org>

--- sbin/kget/kget.c
+++ sbin/kget/kget.c
@@ -27,16 +27,13 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/sysctl.h>
 #include <machine/uc_device.h>
-#if 0
-#include <isa/pnp.h>
-#endif
 
 struct uc_device *id;
-struct pnp_cinfo *c;
 char *p;
 
 int
@@ -45,7 +42,6 @@ main(int argc, char *argv[])
 	int len,i;
 	char *buf;
 	char *mib1="machdep.uc_devlist";
-	char *mib2="machdep.uc_pnplist";
 	char name[9];
 	FILE *fout;
 
@@ -65,7 +61,7 @@ main(int argc, char *argv[])
 
 	/* We use sysctlbyname, because the oid is unknown (OID_AUTO) */
 
-	/* First, print the changes made to ISA devices */
+	/* Print the changes made to ISA devices */
 	/* get the buffer size */
 	i=sysctlbyname(mib1,NULL,&len,NULL,NULL);
 	if(i) {
@@ -73,6 +69,10 @@ main(int argc, char *argv[])
 		exit(-1);
 	}
 	buf=(char *)malloc(len*sizeof(char));
+	if (buf==NULL) {
+		perror("malloc");
+		exit(-1);
+	}
 	i=sysctlbyname(mib1,buf,&len,NULL,NULL);
 	if(i) {
 		perror("retrieving data");
@@ -113,56 +113,6 @@ main(int argc, char *argv[])
 		i+=sizeof(struct uc_device)+8;
 	}
 	free(buf);
-#if 0
-	/* Now, print the changes to PnP override table */
-	/* get the buffer size */
-	i=sysctlbyname(mib2,NULL,&len,NULL,NULL);
-	if(i) {
-		/* Hmm.. No PnP table? */
-		goto finish;
-	}
-	buf=(char *)malloc(len*sizeof(char));
-	i=sysctlbyname(mib2,buf,&len,NULL,NULL);
-	if(i) {
-		perror("retrieving data");
-		exit(-1);
-	}
-	i=0;
-	/* Print the PnP override table. Taken from userconfig.c */
-	do {
-		c = (struct pnp_cinfo *)(buf+i);
-		if (c->csn >0 && c->csn != 255) {
-	    		int pmax, mmax;
-	    		char buf1[256];
-
-			if(c->enable==0) {
-				fprintf(fout,"pnp %d %d disable\n",
-					c->csn, c->ldn);
-				continue;
-			}
-			fprintf(fout,"pnp %d %d %s irq0 %d irq1 %d drq0 %d drq1 %d",
-				c->csn, c->ldn,
-				c->override ? "os":"bios",
-				c->irq[0], c->irq[1], c->drq[0], c->drq[1]);
-	    		if (c->flags)
-				fprintf(fout," flags 0x%lx",c->flags);
-			pmax=0;
-			while(c->port[pmax]!=0 && pmax<8) {
-				fprintf(fout," port%d %d",pmax,c->port[pmax]);
-				pmax++;
-			}
-			mmax=0;
-			while(c->mem[mmax].base!=0 && mmax<8) {
-				fprintf(fout," mem%d %d",mmax,c->mem[mmax].base);
-				mmax++;
-			}
-	    		fprintf(fout,"\n");
-    		}
-
-	} while ((i+=sizeof(struct pnp_cinfo))<len);
-	free(buf);
-#endif
-finish:
 	fprintf(fout,"quit\n");
 	fclose(fout);
 	exit(0);
