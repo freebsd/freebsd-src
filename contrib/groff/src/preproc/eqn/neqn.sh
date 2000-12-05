fe338b0725cf9a68c1af547768213e8fea1f30d4
ru <ru@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2000-12-05 19:06:41 +0000

    Resolve conflicts after v1.16.1 import.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@69631 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/groff/src/preproc/eqn/neqn.sh
+++ contrib/groff/src/preproc/eqn/neqn.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Provision of this shell script should not be taken to imply that use of
-# GNU eqn with groff -Tascii|-Tlatin1 is supported.
+# GNU eqn with groff -Tascii|-Tlatin1|-Tutf8|-Tcp1047 is supported.
+# $FreeBSD$
 
 # Default device.
 locale=${LC_CTYPE:-$LANG}
