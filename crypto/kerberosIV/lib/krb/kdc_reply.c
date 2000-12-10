32ce969d51756de86d53a1779b7fd3c5e8102afc
assar <assar@FreeBSD.org>
2000-12-10 21:00:35 +0000

    merge fix from vendor for removing buffer overrun

--- crypto/kerberosIV/lib/krb/kdc_reply.c
+++ crypto/kerberosIV/lib/krb/kdc_reply.c
@@ -121,6 +121,9 @@ kdc_reply_cipher(KTEXT reply, KTEXT cip)
     p += krb_get_int(p, &exp_date, 4, little_endian);
     p++; /* master key version number */
     p += krb_get_int(p, &clen, 2, little_endian);
+    if (reply->length - (p - reply->dat) < clen)
+	return INTK_PROT;
+
     cip->length = clen;
     memcpy(cip->dat, p, clen);
     p += clen;
