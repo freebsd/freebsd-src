bbdd3453c7f7b2634b824fde24970497f97ee26c
David E. O'Brien <obrien@FreeBSD.org>
2000-12-25 02:18:41 +0000

    MFC: don't abuse SCCS markers.

--- usr.bin/indent/parse.c
+++ usr.bin/indent/parse.c
@@ -36,7 +36,7 @@
 #ifndef lint
 static char sccsid[] = "@(#)parse.c	8.1 (Berkeley) 6/6/93";
 static const char rcsid[] =
-  "@(#)$FreeBSD$";
+  "$FreeBSD$";
 #endif /* not lint */
 
 #include <stdio.h>
