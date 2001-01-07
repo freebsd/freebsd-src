f6f2ec6ddc13edc54ce1b2d246abae47d364e4b1
gibbs <gibbs@FreeBSD.org>
2001-01-07 22:52:36 +0000

    MFC: Sync perforce ids.

--- sys/dev/aic7xxx/aicasm/aicasm_symbol.h
+++ sys/dev/aic7xxx/aicasm/aicasm_symbol.h
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: //depot/src/aic7xxx/aicasm/aicasm_symbol.h#3 $
+ * $Id: //depot/src/aic7xxx/aicasm/aicasm_symbol.h#4 $
  *
  * $FreeBSD$
  */
