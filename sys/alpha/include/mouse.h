d147a07119d67bf03c280cfd8c6a5334d32764bc
phk <phk@FreeBSD.org>
2001-01-16 10:01:56 +0000

    These files have been on deathrow for a couple of months, no appeal.

--- sys/alpha/include/mouse.h
+++
@@ -1,4 +0,0 @@
-/* $FreeBSD$ */
-#if __GNUC__
-#error "this file includes <machine/mouse.h> which is deprecated, use <sys/mouse.h> instead"
-#endif
--- sys/i386/include/console.h
+++
@@ -1,4 +0,0 @@
-/* $FreeBSD$ */
-#if __GNUC__
-#error "this file includes <machine/console.h> which is deprecated, use <sys/{kb,cons,fb}io.h> instead"
-#endif
