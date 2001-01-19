fc8658293350d04c34c9ef92b9fb0891ac9e9a3b
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-01-19 11:43:13 +0000

    Add in a space that got lost in the previous commit in some debugging code so that '&' becomes a binary operator and not a unary operator.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@71249 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/isa/npx.c
+++ sys/i386/isa/npx.c
@@ -513,7 +513,7 @@ npxexit(p)
 		u_int	masked_exceptions;
 
 		masked_exceptions = PCPU_GET(curpcb)->pcb_savefpu.sv_env.en_cw
-		    &PCPU_GET(curpcb)->pcb_savefpu.sv_env.en_sw & 0x7f;
+		    & PCPU_GET(curpcb)->pcb_savefpu.sv_env.en_sw & 0x7f;
 		/*
 		 * Log exceptions that would have trapped with the old
 		 * control word (overflow, divide by 0, and invalid operand).
