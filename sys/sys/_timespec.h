a25e45d40c2f07cbbd00d73ea156edc7a7e01025
Garrett Wollman <wollman@FreeBSD.org>
2001-01-23 04:40:54 +0000

    Synchronize with some of my local changes.  To quote:
     * This file must be kept synchronized with <sys/timespec.h>.
     * It defines a structure which must be a type pun for
     * `struct timespec'; this structure is used in header files where
     * the ABI uses a `struct timespec' but standards prohibit its
     * definition.  (Currently only <sys/stat.h>.)

---
+++ sys/sys/_timespec.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 1982, 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)time.h	8.5 (Berkeley) 5/4/95
+ * from: FreeBSD: src/sys/sys/time.h,v 1.43 2000/03/20 14:09:05 phk Exp
+ *	$FreeBSD$
+ */
+
+/*
+ * Prerequisites: <sys/cdefs.h>, <machine/ansi.h>
+ *
+ * This file must be kept synchronized with <sys/timespec.h>.
+ * It defines a structure which must be a type pun for
+ * `struct timespec'; this structure is used in header files where
+ * the ABI uses a `struct timespec' but standards prohibit its
+ * definition.  (Currently only <sys/stat.h>.)
+ */
+
+#ifndef _SYS__TIMESPEC_H_
+#define _SYS__TIMESPEC_H_
+
+struct __timespec {
+#ifdef _BSD_TIME_T_
+	_BSD_TIME_T_	__tv_sec; /* seconds, but time_t is not yet defined */
+#else
+	time_t		__tv_sec; /* seconds */
+#endif
+	long		__tv_nsec; /* and nanoseconds */
+};
+
+#endif /* _SYS__TIMESPEC_H_ */
