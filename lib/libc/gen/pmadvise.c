5988d1653846c888b305eed76591c5ac6f96ba44
wollman <wollman@FreeBSD.org>
2001-01-23 04:49:39 +0000

    Add a couple of new library interfaces (will be activated when the relavant header file changes are committed) for POSIX support.

---
+++ lib/libc/gen/pmadvise.c
@@ -0,0 +1,14 @@
+/*
+ * The contents of this file are in the public domain.
+ * Written by Garrett A. Wollman, 2000-10-07.
+ *
+ * $FreeBSD$
+ */
+
+#include <sys/mman.h>
+
+int
+(posix_madvise)(void *address, size_t size, int how)
+{
+	return posix_madvise(address, size, how);
+}
