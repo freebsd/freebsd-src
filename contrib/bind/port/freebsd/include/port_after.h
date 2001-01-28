de347231c76ad97a537093003b3ff7c502f5f12d
asmodai <asmodai@FreeBSD.org>
2001-01-28 23:06:48 +0000

    Resolve conflicts after import.

--- contrib/bind/port/freebsd/include/port_after.h
+++ contrib/bind/port/freebsd/include/port_after.h
@@ -73,4 +73,8 @@ struct sockaddr_in6 {
 	u_int32_t	sin6_scope_id;	/* set of interfaces for a scope */
 };
 #endif	/* HAS_INET6_STRUCTS */
+#ifndef ISC_FACILITY
+#define ISC_FACILITY LOG_DAEMON
+#endif
+
 #endif /* ! PORT_AFTER_H */
