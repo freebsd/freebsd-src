ca28f35d1aa830c0d77b70ba89755307def40447
Kenneth D. Merry <ken@FreeBSD.org>
2001-01-29 01:50:53 +0000

    MFC, writeable CD support.
    This allows writing to DVD-RAM, PD and similar drives that probe as CD
    devices.  Note that these are randomly writeable devices, not
    sequential-only devices like CD-R/CD-RW drives, which are supported
    by cdrecord.
    
    The following files/revisions have been merged:
    
    scsi_cd.c:		the rest of 1.42 and all of 1.43
    subr_diskslice.c:	1.92
    diskslice.h		1.40
    
    Reviewed by:	bde (original changeset)

--- sys/sys/diskslice.h
+++ sys/sys/diskslice.h
@@ -80,6 +80,7 @@ struct diskslices {
 /* Flags for dsopen(). */
 #define	DSO_NOLABELS	1
 #define	DSO_ONESLICE	2
+#define	DSO_COMPATLABEL	4
 
 #define	dsgetlabel(dev, ssp)	(ssp->dss_slices[dkslice(dev)].ds_label)
 
