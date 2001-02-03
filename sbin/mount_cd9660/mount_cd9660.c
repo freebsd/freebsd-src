07e1281b772cd82126aff0bf46826033f7e1300f
bsd <bsd@FreeBSD.org>
2001-02-03 21:22:39 +0000

    MFC: rev 1.16 - print out the device name when the mount fails

--- sbin/mount_cd9660/mount_cd9660.c
+++ sbin/mount_cd9660/mount_cd9660.c
@@ -176,7 +176,7 @@ main(int argc, char **argv)
 		errx(1, "cd9660 filesystem is not available");
 
 	if (mount(vfc.vfc_name, mntpath, mntflags, &args) < 0)
-		err(1, NULL);
+		err(1, "%s", args.fspec);
 	exit(0);
 }
 
