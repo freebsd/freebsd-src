54fa1fab4444b4363042080b30c232fc096b663b
charnier <charnier@FreeBSD.org>
2001-02-06 20:01:40 +0000

    Rework diagnostics text Remove unused #include

--- usr.bin/comm/comm.c
+++ usr.bin/comm/comm.c
@@ -50,11 +50,9 @@ static const char rcsid[] =
 
 #include <ctype.h>
 #include <err.h>
-#include <errno.h>
 #include <limits.h>
 #include <locale.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
