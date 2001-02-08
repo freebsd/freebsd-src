e396ac20a49e0d26712570446f953f9d9bf9d89e
phantom <phantom@FreeBSD.org>
2001-02-08 16:58:53 +0000

    Make FreeBSD locale support complete: add support for rest locale categories LC_MONETARY, LC_NUMERIC and LC_MESSAGES. Remove stub functions since they don't need anymore.
    Reviewed by:	silence on -i18n

---
+++ lib/libc/locale/lmessages.c
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2001 Alexey Zelkin
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#include "lmessages.h"
+#include "ldpart.h"
+
+#define LCMESSAGES_SIZE (sizeof(struct lc_messages_T) / sizeof(char *))
+
+char empty[] = "";
+
+static const struct lc_messages_T _C_messages_locale = {
+	"^[yY]" ,	/* yesexpr */
+	"^[nN]" ,	/* noexpr */
+	"yes" , 	/* yesstr */
+	"no"		/* nostr */
+};
+
+static struct lc_messages_T _messages_locale;
+static int	_messages_using_locale;
+static char *	messages_locale_buf;
+
+int
+__messages_load_locale(const char *name) {
+
+	int ret;
+	ret = __part_load_locale(name, &_messages_using_locale,
+		messages_locale_buf, "LC_MESSAGES", LCMESSAGES_SIZE,
+		(const char **)&_messages_locale);
+	if (ret == -1) {
+		/* Assume that we have incomplete locale file (without
+		 * "yesstr" and "nostr" declared. Try it also.
+		 */
+		ret = __part_load_locale(name, &_messages_using_locale,
+			messages_locale_buf, "LC_MESSAGES", LCMESSAGES_SIZE/2,
+			(const char **)&_messages_locale);
+		if (ret != -1) {
+			_messages_locale.yesstr = empty;
+			_messages_locale.nostr = empty;
+		}
+	}
+	return ret;
+}
+
+struct lc_messages_T *
+__get_current_messages_locale(void) {
+
+	return (_messages_using_locale
+		? &_messages_locale
+		: (struct lc_messages_T *)&_C_messages_locale);
+}
+
+#ifdef LOCALE_DEBUG
+void
+msgdebug() {
+printf(	"yesexpr = %s\n"
+	"noexpr = %s\n"
+	"yesstr = %s\n"
+	"nostr = %s\n",
+	_messages_locale.yesexpr,
+	_messages_locale.noexpr,
+	_messages_locale.yesstr,
+	_messages_locale.nostr
+);
+}
+#endif /* LOCALE_DEBUG */
