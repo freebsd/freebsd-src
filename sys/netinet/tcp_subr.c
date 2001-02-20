468dcf9ef5f676e798c332dadbc86cb3805f46a1
jesper <jesper@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-02-20 23:25:04 +0000

    Only call in_pcbnotify if the src port number != 0, as we treat 0 as a wildcard in src/sys/in_pbc.c:in_pcbnotify()
    It's sufficient to check for src|local port, as we'll have no
    sessions with src|local port == 0
    
    Without this a attacker sending ICMP messages, where the attached
    IP header (+ 8 bytes) has the address and port numbers == 0, would
    have the ICMP message applied to all sessions.
    
    PR:		kern/25195
    Submitted by:	originally by jesper, reimplimented by jlemon's advice
    Reviewed by:	jlemon
    Approved by:	jlemon
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@72778 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -1032,6 +1032,20 @@ tcp_ctlinput(cmd, sa, vip)
 				       + (IP_VHL_HL(ip->ip_vhl) << 2));
 		if (tcp_seq_check == 1)
 			tcp_sequence = ntohl(th->th_seq);
+		/*
+		 * Only call in_pcbnotify if the src port number != 0, as we 
+		 * treat 0 as a wildcard in src/sys/in_pbc.c:in_pcbnotify()
+		 *
+		 * It's sufficient to check for src|local port, as we'll have no
+		 * sessions with src|local port == 0
+		 *
+		 * Without this a attacker sending ICMP messages, where the attached 
+		 * IP header (+ 8 bytes) has the address and port numbers == 0, would
+		 * have the ICMP message applied to all sessions (modulo TCP sequence
+		 * number check).
+		 */
+		if (th->th_sport == 0)
+			return;
 		in_pcbnotify(&tcb, sa, th->th_dport, ip->ip_src, th->th_sport,
 			cmd, notify, tcp_sequence, tcp_seq_check);
 	} else
