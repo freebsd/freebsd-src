fc530eab12e76e59a9993227b3a70921f8c99416
gallatin <gallatin@FreeBSD.org>
2001-02-21 23:01:57 +0000

    MFC: Linux signal handling fixes from current.  This should improve emulation of signal intensive Linux apps (such as Java).  This allows an older version of the IBM JDK to run on -stable and gets rid of the "cannot uninstall sigaltstack" message from the Sun Linux JDK 1.3
    Reviewed by: marcel
    Approved by: marcel

--- sys/i386/linux/linux_genassym.c
+++ sys/i386/linux/linux_genassym.c
@@ -1,7 +1,6 @@
 /* $FreeBSD$ */
 
 #include <stddef.h>
-
 #include <sys/param.h>
 #include <sys/assym.h>
 
@@ -11,3 +10,7 @@ ASSYM(LINUX_SIGF_HANDLER, offsetof(struct linux_sigframe, sf_handler));
 ASSYM(LINUX_SIGF_SC, offsetof(struct linux_sigframe, sf_sc));
 ASSYM(LINUX_SC_GS, offsetof(struct linux_sigcontext, sc_gs));
 ASSYM(LINUX_SC_EFLAGS, offsetof(struct linux_sigcontext, sc_eflags));
+ASSYM(LINUX_RT_SIGF_HANDLER, offsetof(struct linux_rt_sigframe, sf_handler));
+ASSYM(LINUX_RT_SIGF_UC, offsetof(struct linux_rt_sigframe, sf_sc));
+
+
