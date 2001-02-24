6636914a20992f176e7d5e49c5f7a4dc1705cade
jlemon <jlemon@FreeBSD.org>
2001-02-24 01:44:03 +0000

    Add an EV_SET() convenience macro for initializing struct kevent prior to the call to kevent().
    Update the copyright notices as well.

--- sys/kern/kern_event.c
+++ sys/kern/kern_event.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 1999,2000 Jonathan Lemon <jlemon@FreeBSD.org>
+ * Copyright (c) 1999,2000,2001 Jonathan Lemon <jlemon@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
