2ae83a7282ae23b51c6336596ec8fd31cccfe458
ru <ru@FreeBSD.org>
2001-02-26 08:09:51 +0000

    Backout -a restriction hack.
    Requested by:	rwatson

--- sbin/dmesg/dmesg.c
+++ sbin/dmesg/dmesg.c
@@ -45,7 +45,6 @@ static const char rcsid[] =
   "$FreeBSD$";
 #endif /* not lint */
 
-#include <sys/types.h>
 #include <sys/msgbuf.h>
 
 #include <err.h>
@@ -54,10 +53,9 @@ static const char rcsid[] =
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sysexits.h>
-#include <syslog.h>
 #include <unistd.h>
 #include <vis.h>
+#include <sys/syslog.h>
 
 struct nlist nl[] = {
 #define	X_MSGBUF	0
@@ -110,9 +108,6 @@ main(argc, argv)
 	 */
 	if (memf != NULL || nlistf != NULL)
 		setgid(getgid());
-
-	if (all && getuid())
-		errx(EX_NOPERM, "must be root to use -a");
 
 	/* Read in kernel message buffer, do sanity checks. */
 	if ((kd = kvm_open(nlistf, memf, NULL, O_RDONLY, "dmesg")) == NULL)
