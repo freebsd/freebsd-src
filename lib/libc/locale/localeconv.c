2f20595c38edbab7534dc5801d6a81cde7fd3fc0
David E. O'Brien <obrien@FreeBSD.org>
2001-03-05 10:21:18 +0000

    MFC: add $FreeBSD$

--- lib/libc/locale/localeconv.c
+++ lib/libc/locale/localeconv.c
@@ -33,6 +33,8 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 static char sccsid[] = "@(#)localeconv.c	8.1 (Berkeley) 6/4/93";
+static char rcsid[] =
+  "$FreeBSD$";
 #endif /* LIBC_SCCS and not lint */
 
 #include <locale.h>
