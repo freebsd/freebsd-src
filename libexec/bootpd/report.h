966dde0e53c0261974f66eed2ebf9eef97f3285d
Kris Kennaway <kris@FreeBSD.org>
2001-03-05 10:58:59 +0000

    MFC: Constify

--- libexec/bootpd/report.h
+++ libexec/bootpd/report.h
@@ -1,4 +1,5 @@
 /* report.h */
+/* $FreeBSD$ */
 
 #ifdef	__STDC__
 #define P(args) args
@@ -7,7 +8,7 @@
 #endif
 
 extern void report_init P((int nolog));
-extern void report P((int, char *, ...));
-extern char *get_errmsg P((void));
+extern void report P((int, const char *, ...));
+extern const char *get_errmsg P((void));
 
 #undef P
