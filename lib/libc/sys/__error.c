3e1a8b992e6fdf09b3779f188ccf064ad8329f14
obrien <obrien@FreeBSD.org>
2001-03-05 11:45:51 +0000

    MFC: style sync with -current

--- lib/libc/sys/__error.c
+++ lib/libc/sys/__error.c
@@ -29,6 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
+ * $FreeBSD$
  */
 
 #include <sys/cdefs.h>
@@ -41,7 +42,8 @@ extern int errno;
  */
 #pragma weak __error=__error_unthreaded
 
-int * __error_unthreaded()
+int *
+__error_unthreaded()
 {
 	return(&errno);
 }
