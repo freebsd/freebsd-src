92553ca53ba04b54adecde2bc1d5b8cee460becc
jasone <jasone@FreeBSD.org>
2001-03-05 19:59:41 +0000

    Implement shared/exclusive locks.
    Reviewed by:	bmilekic, jake, jhb

---
+++ sys/sys/sx.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2001 Jason Evans <jasone@freebsd.org>.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice(s), this list of conditions and the following disclaimer as
+ *    the first lines of this file unmodified other than the possible 
+ *    addition of one or more copyright notices.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice(s), this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef	_SYS_SX_H_
+#define	_SYS_SX_H_
+
+#ifndef	LOCORE
+#include <sys/mutex.h>
+#include <sys/condvar.h>
+
+struct sx {
+	struct mtx	sx_lock;	/* General protection lock and xlock. */
+	int		sx_cnt;		/* -1: xlock, > 0: slock count. */
+	struct cv	sx_shrd_cv;	/* slock waiters. */
+	int		sx_shrd_wcnt;	/* Number of slock waiters. */
+	struct cv	sx_excl_cv;	/* xlock waiters. */
+	int		sx_excl_wcnt;	/* Number of xlock waiters. */
+};
+
+#ifdef _KERNEL
+void	sx_init(struct sx *sx, const char *description);
+void	sx_destroy(struct sx *sx);
+void	sx_slock(struct sx *sx);
+void	sx_xlock(struct sx *sx);
+void	sx_sunlock(struct sx *sx);
+void	sx_xunlock(struct sx *sx);
+
+#ifdef INVARIANTS
+/*
+ * SX_ASSERT_SLOCKED() can only detect that at least *some* thread owns an
+ * slock, but it cannot guarantee that *this* thread owns an slock.
+ */
+#define	SX_ASSERT_SLOCKED(sx) do {					\
+	mtx_lock(&(sx)->sx_lock);					\
+	KASSERT(((sx)->sx_cnt > 0), ("%s: lacking slock\n",		\
+	    __FUNCTION__));						\
+	mtx_unlock(&(sx)->sx_lock);					\
+} while (0)
+/*
+ * SX_ASSERT_XLOCKED() can only detect that at least *some* thread owns an
+ * xlock, but it cannot guarantee that *this* thread owns an xlock.
+ */
+#define	SX_ASSERT_XLOCKED(sx) do {					\
+	mtx_lock(&(sx)->sx_lock);					\
+	KASSERT(((sx)->sx_cnt == -1), ("%s: lacking xlock\n",		\
+	    __FUNCTION__));						\
+	mtx_unlock(&(sx)->sx_lock);					\
+} while (0)
+#else	/* INVARIANTS */
+#define	SX_ASSERT_SLOCKED(sx)
+#define	SX_ASSERT_XLOCKER(sx)
+#endif	/* INVARIANTS */
+
+#endif	/* _KERNEL */
+#endif	/* !LOCORE */
+#endif	/* _SYS_SX_H_ */
