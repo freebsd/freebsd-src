84709752a1c6fac263e353b6ab0ea5c6c0940e7f
obrien <obrien@FreeBSD.org>
2001-03-08 09:04:40 +0000

    Move _PATH_DEFTAPE to <paths.h> to remove all the duplication of definitons, and remove leading `r'(aw) from it.

--- sbin/restore/dirs.c
+++ sbin/restore/dirs.c
@@ -55,12 +55,12 @@ static const char rcsid[] =
 
 #include <err.h>
 #include <errno.h>
+#include <paths.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#include "pathnames.h"
 #include "restore.h"
 #include "extern.h"
 
