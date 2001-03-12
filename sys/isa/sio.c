53dab80f9fbb4f0699511042e0e129fcd1f35b74
sanpei <sanpei@FreeBSD.org>
2001-03-12 13:47:29 +0000

    MFC:	Add GVC1601(rev.1.328) and IOD0081(rev.1.327)
    Approved by:	jkh

--- sys/isa/sio.c
+++ sys/isa/sio.c
@@ -674,9 +674,11 @@ static struct isa_pnp_id sio_ids[] = {
 	{0x1200c31e, NULL},	/* GVC0012 - VF1128HV-R9 (win modem?) */
 	{0x0303c31e, NULL},	/* GVC0303 - MaxTech 33.6 PnP D/F/V */
 	{0x0505c31e, NULL},	/* GVC0505 - GVC 56k Faxmodem */
+	{0x0116c31e, NULL},	/* GVC1601 - Rockwell V.34 Plug & Play Modem */
 	{0x0050c31e, NULL},	/* GVC5000 - some GVC modem */
 	{0x3800f91e, NULL},	/* GWY0038 - Telepath with v.90 */
 	{0x9062f91e, NULL},	/* GWY6290 - Telepath with x2 Technology */
+	{0x8100e425, NULL},	/* IOD0081 - I-O DATA DEVICE,INC. IFML-560 */
 	{0x21002534, NULL},	/* MAE0021 - Jetstream Int V.90 56k Voice Series 2*/
 	{0x0000f435, NULL},	/* MOT0000 - Motorola ModemSURFR 33.6 Intern */
 	{0x5015f435, NULL},	/* MOT1550 - Motorola ModemSURFR 56K Modem */
