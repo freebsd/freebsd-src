266cea85ee85a1343c8d97993dc1f12fadb5db16
peter <peter@FreeBSD.org>
2001-03-20 02:10:18 +0000

    Use the same API as the example code. Allow the initial hash value to be passed in, as the examples do. Incrementally hash in the dvp->v_id (using the official api) rather than add it.  This seems to help power-of-two predictable filename trees where the filenames repeat on a power-of-two cycle and the directory trees have power-of-two components in it.  The simple add then mask was causing things like 12000+ entry collision chains while most other entries have between 0 and 3 entries each.  This way seems to improve things.

--- sys/sys/fnv_hash.h
+++ sys/sys/fnv_hash.h
@@ -8,19 +8,20 @@
  * $FreeBSD$
  */
 
-#define FNV_32_PRIME ((u_int32_t) 0x01000193UL)
-#define FNV1_32_INIT ((u_int32_t) 33554467UL)
+typedef u_int32_t Fnv32_t;
+typedef u_int64_t Fnv64_t;
 
-#define FNV_64_PRIME ((u_int64_t) 0x100000001b3ULL)
-#define FNV1_64_INIT ((u_int64_t) 0xcbf29ce484222325ULL)
+#define FNV1_32_INIT ((Fnv32_t) 33554467UL)
+#define FNV1_64_INIT ((Fnv64_t) 0xcbf29ce484222325ULL)
 
-static __inline u_int32_t
-fnv32_hashbuf(const void *buf, size_t len)
+#define FNV_32_PRIME ((Fnv32_t) 0x01000193UL)
+#define FNV_64_PRIME ((Fnv64_t) 0x100000001b3ULL)
+
+static __inline Fnv32_t
+fnv_32_buf(const void *buf, size_t len, Fnv32_t hval)
 {
 	const u_int8_t *s = (const u_int8_t *)buf;
-	u_int32_t hval;
 
-	hval = FNV1_32_INIT;
 	while (len-- != 0) {
 		hval *= FNV_32_PRIME;
 		hval ^= *s++;
@@ -28,13 +29,12 @@ fnv32_hashbuf(const void *buf, size_t len)
 	return hval;
 }
 
-static __inline u_int32_t
-fnv32_hashstr(const char *str)
+static __inline Fnv32_t
+fnv_32_str(const char *str, Fnv32_t hval)
 {
 	const u_int8_t *s = (const u_int8_t *)str;
-	u_int32_t hval, c;
+	Fnv32_t c;
 
-	hval = FNV1_32_INIT;
 	while ((c = *s++) != 0) {
 		hval *= FNV_32_PRIME;
 		hval ^= c;
@@ -42,13 +42,11 @@ fnv32_hashstr(const char *str)
 	return hval;
 }
 
-static __inline u_int64_t
-fnv64_hashbuf(const void *buf, size_t len)
+static __inline Fnv64_t
+fnv_64_buf(const void *buf, size_t len, Fnv64_t hval)
 {
 	const u_int8_t *s = (const u_int8_t *)buf;
-	u_int64_t hval;
 
-	hval = FNV1_64_INIT;
 	while (len-- != 0) {
 		hval *= FNV_64_PRIME;
 		hval ^= *s++;
@@ -56,14 +54,12 @@ fnv64_hashbuf(const void *buf, size_t len)
 	return hval;
 }
 
-static __inline u_int64_t
-fnv64_hashstr(const char *str)
+static __inline Fnv64_t
+fnv_64_str(const char *str, Fnv64_t hval)
 {
 	const u_int8_t *s = (const u_int8_t *)str;
-	u_int64_t hval;
 	u_register_t c;		 /* 32 bit on i386, 64 bit on alpha,ia64 */
 
-	hval = FNV1_64_INIT;
 	while ((c = *s++) != 0) {
 		hval *= FNV_64_PRIME;
 		hval ^= c;
