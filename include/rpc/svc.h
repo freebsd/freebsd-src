71c1f154949518f27b1f9713d76a2b93c4f7f648
alfred <alfred@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-03-22 18:44:57 +0000

    forgot prototyle for __rpc_get_local_uid()
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@74658 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- include/rpc/svc.h
+++ include/rpc/svc.h
@@ -410,6 +410,8 @@ extern SVCXPRT *svc_raw_create __P((void));
  */
 int svc_dg_enablecache __P((SVCXPRT *, const u_int));
 
+int __rpc_get_local_uid(SVCXPRT *transp, uid_t *uid);
+
 __END_DECLS
 
 
