d79c544fc39fa9790ece38fef62acfba6553aac4
obrien <obrien@FreeBSD.org>
2001-03-27 17:05:23 +0000

    Mirror the newfs(8) defaults change I made in rev 1.33 of newfs.c where I made `22' the default number of cylinders per group.

--- usr.sbin/sysinstall/install.c
+++ usr.sbin/sysinstall/install.c
@@ -1073,7 +1073,7 @@ installVarDefaults(dialogMenuItem *self)
 	variable_set2(SYSTEM_STATE,		"update", 0);
     else
 	variable_set2(SYSTEM_STATE,		"init", 0);
-    variable_set2(VAR_NEWFS_ARGS,		"-b 8192 -f 1024", 0);
+    variable_set2(VAR_NEWFS_ARGS,		"-b 8192 -f 1024 -c 22", 0);
     return DITEM_SUCCESS;
 }
 
