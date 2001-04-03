b1b355c17729de30a19172f4da691adf266c462f
bmilekic <bmilekic@FreeBSD.org>
2001-04-03 04:50:13 +0000

    - Change the msleep()s to condition variables.   The mbuf and mcluster free lists now each "own" a condition variable,   m_starved.
    - Clean up minor indentention issues in sys/mbuf.h caused by previous
      commit.

--- sys/sys/mbuf.h
+++ sys/sys/mbuf.h
@@ -39,6 +39,7 @@
 
 #include <sys/lock.h>
 #include <sys/mutex.h>
+#include <sys/condvar.h>
 
 /*
  * Mbufs are of a single size, MSIZE (machine/param.h), which
@@ -254,10 +255,12 @@ union mext_refcnt {
  */
 struct mbffree_lst {
 	struct mbuf *m_head;
+	struct cv m_starved;
 };
 
 struct mclfree_lst {
         union mcluster *m_head;
+	struct cv m_starved;
 };
   
 struct mcntfree_lst {
@@ -265,14 +268,15 @@ struct mcntfree_lst {
 };
 
 /*
- * Wake up the next instance (if any) of a sleeping allocation - which is
- * waiting for a {cluster, mbuf} to be freed.
+ * Signal a single instance (if any) blocked on a m_starved cv (i.e. an
+ * instance waiting for an {mbuf, cluster} to be freed to the global
+ * cache lists).
  *
- * Must be called with the appropriate mutex held.
+ * Must be called with mbuf_mtx held.
  */
-#define	MBWAKEUP(m_wid) do {						\
-	if ((m_wid))							\
-		wakeup_one(&(m_wid)); 					\
+#define	MBWAKEUP(m_wid, m_cv) do {					\
+	if ((m_wid) > 0)						\
+		cv_signal((m_cv));					\
 } while (0)
 
 /*
@@ -298,7 +302,7 @@ struct mcntfree_lst {
 #define _MEXT_ALLOC_CNT(m_cnt, how) do {				\
 	union mext_refcnt *__mcnt;					\
 									\
-	mtx_lock(&mbuf_mtx);					\
+	mtx_lock(&mbuf_mtx);						\
 	if (mcntfree.m_head == NULL)					\
 		m_alloc_ref(1, (how));					\
 	__mcnt = mcntfree.m_head;					\
@@ -307,18 +311,18 @@ struct mcntfree_lst {
 		mbstat.m_refree--;					\
 		__mcnt->refcnt = 0;					\
 	}								\
-	mtx_unlock(&mbuf_mtx);					\
+	mtx_unlock(&mbuf_mtx);						\
 	(m_cnt) = __mcnt;						\
 } while (0)
 
 #define _MEXT_DEALLOC_CNT(m_cnt) do {					\
 	union mext_refcnt *__mcnt = (m_cnt);				\
 									\
-	mtx_lock(&mbuf_mtx);					\
+	mtx_lock(&mbuf_mtx);						\
 	__mcnt->next_ref = mcntfree.m_head;				\
 	mcntfree.m_head = __mcnt;					\
 	mbstat.m_refree++;						\
-	mtx_unlock(&mbuf_mtx);					\
+	mtx_unlock(&mbuf_mtx);						\
 } while (0)
 
 #define MEXT_INIT_REF(m, how) do {					\
@@ -369,15 +373,15 @@ struct mcntfree_lst {
 	int _mhow = (how);						\
 	int _mtype = (type);						\
 									\
-	mtx_lock(&mbuf_mtx);					\
+	mtx_lock(&mbuf_mtx);						\
 	_MGET(_mm, _mhow);						\
 	if (_mm != NULL) {						\
 		mbtypes[_mtype]++;					\
-		mtx_unlock(&mbuf_mtx);				\
+		mtx_unlock(&mbuf_mtx);					\
 		_MGET_SETUP(_mm, _mtype);				\
 	} else {							\
-		mbstat.m_drops++;			\
-		mtx_unlock(&mbuf_mtx);				\
+		mbstat.m_drops++;					\
+		mtx_unlock(&mbuf_mtx);					\
 	}								\
 	(m) = _mm;							\
 } while (0)
@@ -398,15 +402,15 @@ struct mcntfree_lst {
 	int _mhow = (how);						\
 	int _mtype = (type);						\
 									\
-	mtx_lock(&mbuf_mtx);					\
+	mtx_lock(&mbuf_mtx);						\
 	_MGET(_mm, _mhow);						\
 	if (_mm != NULL) {						\
 		mbtypes[_mtype]++;					\
-		mtx_unlock(&mbuf_mtx);				\
+		mtx_unlock(&mbuf_mtx);					\
 		_MGETHDR_SETUP(_mm, _mtype);				\
 	} else {							\
-		mbstat.m_drops++;			\
-		mtx_unlock(&mbuf_mtx);				\
+		mbstat.m_drops++;					\
+		mtx_unlock(&mbuf_mtx);					\
 	}								\
 	(m) = _mm;							\
 } while (0)
@@ -439,7 +443,7 @@ struct mcntfree_lst {
 #define	MCLGET(m, how) do {						\
 	struct mbuf *_mm = (m);						\
 									\
-	mtx_lock(&mbuf_mtx);					\
+	mtx_lock(&mbuf_mtx);						\
 	_MCLALLOC(_mm->m_ext.ext_buf, (how));				\
 	if (_mm->m_ext.ext_buf != NULL) {				\
 		mtx_unlock(&mbuf_mtx);					\
@@ -455,10 +459,10 @@ struct mcntfree_lst {
 			_mm->m_ext.ext_size = MCLBYTES;			\
 			_mm->m_ext.ext_type = EXT_CLUSTER;		\
 		}							\
-	} else {								\
-		mbstat.m_drops++;			\
+	} else {							\
+		mbstat.m_drops++;					\
 		mtx_unlock(&mbuf_mtx);					\
-	}											\
+	}								\
 } while (0)
 
 #define MEXTADD(m, buf, size, free, args, flags, type) do {		\
@@ -479,12 +483,12 @@ struct mcntfree_lst {
 #define	_MCLFREE(p) do {						\
 	union mcluster *_mp = (union mcluster *)(p);			\
 									\
-	mtx_lock(&mbuf_mtx);					\
+	mtx_lock(&mbuf_mtx);						\
 	_mp->mcl_next = mclfree.m_head;					\
 	mclfree.m_head = _mp;						\
 	mbstat.m_clfree++;						\
-	MBWAKEUP(m_clalloc_wid);					\
-	mtx_unlock(&mbuf_mtx); 					\
+	MBWAKEUP(m_clalloc_wid, &mclfree.m_starved);			\
+	mtx_unlock(&mbuf_mtx); 						\
 } while (0)
 
 /* MEXTFREE:
@@ -519,15 +523,15 @@ struct mcntfree_lst {
 	KASSERT(_mm->m_type != MT_FREE, ("freeing free mbuf"));		\
 	if (_mm->m_flags & M_EXT)					\
 		MEXTFREE(_mm);						\
-	mtx_lock(&mbuf_mtx);					\
+	mtx_lock(&mbuf_mtx);						\
 	mbtypes[_mm->m_type]--;						\
 	_mm->m_type = MT_FREE;						\
 	mbtypes[MT_FREE]++;						\
 	(n) = _mm->m_next;						\
 	_mm->m_next = mmbfree.m_head;					\
 	mmbfree.m_head = _mm;						\
-	MBWAKEUP(m_mballoc_wid);					\
-	mtx_unlock(&mbuf_mtx); 					\
+	MBWAKEUP(m_mballoc_wid, &mmbfree.m_starved);			\
+	mtx_unlock(&mbuf_mtx); 						\
 } while (0)
 
 /*
@@ -618,10 +622,10 @@ struct mcntfree_lst {
 	struct mbuf *_mm = (m);						\
 	int _mt = (t);							\
 									\
-	mtx_lock(&mbuf_mtx);					\
-	mbtypes[_mm->m_type]--;			\
-	mbtypes[_mt]++;				\
-	mtx_unlock(&mbuf_mtx);					\
+	mtx_lock(&mbuf_mtx);						\
+	mbtypes[_mm->m_type]--;						\
+	mbtypes[_mt]++;							\
+	mtx_unlock(&mbuf_mtx);						\
 	_mm->m_type = (_mt);						\
 } while (0)
 
@@ -653,7 +657,7 @@ extern	struct mbuf	*mbutl;		/* virtual address of mclusters */
 extern	struct mclfree_lst	mclfree;
 extern	struct mbffree_lst	mmbfree;
 extern	struct mcntfree_lst	mcntfree;
-extern	struct mtx	mbuf_mtx;
+extern	struct mtx	 mbuf_mtx;
 extern	int		 nmbclusters;
 extern	int		 nmbufs;
 extern	int		 nsfbufs;
