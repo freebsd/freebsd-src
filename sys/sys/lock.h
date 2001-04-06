3dcb6789d7169a0182dd7915d9eea429086e9fc8
John Baldwin <jhb@FreeBSD.org>
2001-04-06 21:37:52 +0000

    - Split out the functionality of displaying the contents of a single lock   list into a public witness_list_locks() function.  Call this function   twice in witness_list() instead of using an evil goto. - Adjust the 'show locks' command to take an optional parameter which   specifies the pid of a process to list the locks of.  By default the   locks held by the current process are displayed.

--- sys/sys/lock.h
+++ sys/sys/lock.h
@@ -164,6 +164,7 @@ void	witness_lock(struct lock_object *, int, const char *, int);
 void	witness_unlock(struct lock_object *, int, const char *, int);
 void	witness_save(struct lock_object *, const char **, int *);
 void	witness_restore(struct lock_object *, const char *, int);
+int	witness_list_locks(struct lock_list_entry **);
 int	witness_list(struct proc *);
 int	witness_sleep(int, struct lock_object *, const char *, int);
 
