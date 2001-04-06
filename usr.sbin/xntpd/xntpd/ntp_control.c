cdd8f71174024a0f04559a7fee8200b22c8f9b5c
Chris D. Faulhaber <jedgar@FreeBSD.org>
2001-04-06 18:21:00 +0000

    MFC: Fix potential remote exploit

--- usr.sbin/xntpd/xntpd/ntp_control.c
+++ usr.sbin/xntpd/xntpd/ntp_control.c
@@ -1,6 +1,11 @@
 /*
  * ntp_control.c - respond to control messages and send async traps
  */
+
+/*
+ * $FreeBSD$
+ */
+
 #include <stdio.h>
 #include <ctype.h>
 #include <sys/types.h>
@@ -1684,7 +1689,7 @@ ctl_getitem(var_list, data)
 	/*
 	 * Delete leading commas and white space
 	 */
-	while (reqpt < reqend && (*reqpt == ',' || isspace(*reqpt))) {
+	while (reqpt < reqend && (*reqpt == ',' || isspace((unsigned char)*reqpt))) {
 		reqpt++;
 	}
 
@@ -1708,7 +1713,7 @@ ctl_getitem(var_list, data)
 				tp++;
 			}
 			if ((*tp == '\0') || (*tp == '=')) {
-				while (cp < reqend && isspace(*cp))
+				while (cp < reqend && isspace((unsigned char)*cp))
 					cp++;
 				if (cp == reqend || *cp == ',') {
 					buf[0] = '\0';
@@ -1721,14 +1726,18 @@ ctl_getitem(var_list, data)
 				if (*cp == '=') {
 					cp++;
 					tp = buf;
-					while (cp < reqend && isspace(*cp))
+					while (cp < reqend && isspace((unsigned char)*cp))
 						cp++;
-					while (cp < reqend && *cp != ',')
+					while (cp < reqend && *cp != ',') {
 						*tp++ = *cp++;
+						if (tp >= buf + sizeof(buf))
+							return (0);
+					}
 					if (cp < reqend)
 						cp++;
 					*tp = '\0';
-					while (isspace(*(tp-1)))
+					while (tp != buf &&
+					    isspace((unsigned char)*(tp-1)))
 						*(--tp) = '\0';
 					reqpt = cp;
 					*data = buf;
