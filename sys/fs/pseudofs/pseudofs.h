9733a808399b6599b24768c5f8f225be95f1cae1
Dag-Erling Smørgrav <des@FreeBSD.org>
2001-04-07 19:51:12 +0000

    Let pseudofs into the warmth of the FreeBSD CVS repo.
    It's not finished yet (I still have to find a way to implement process-
    dependent nodes without consuming too much memory, and the permission
    system needs tightening up), but it's becoming hard to work on without
    a repo (I've accidentally almost nuked it once already), and it works
    (except for the lack of process-dependent nodes, that is).
    
    I was supposed to commit this a week ago, but timed out waiting for jkh
    to reply to some questions I had. Pass him a spoonful of bad karma :)

---
+++ sys/fs/pseudofs/pseudofs.h
@@ -0,0 +1,181 @@
+/*-
+ * Copyright (c) 2001 Dag-Erling Coïdan Smørgrav
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer
+ *    in this position and unchanged.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *      $FreeBSD$
+ */
+
+#ifndef _PSEUDOFS_H_INCLUDED
+#define _PSEUDOFS_H_INCLUDED
+
+/*
+ * Limits and constants
+ */
+#define PFS_NAMELEN		24
+#define PFS_DELEN		(8 + PFS_NAMELEN)
+
+typedef enum {
+	pfstype_none = 0,
+	pfstype_root,
+	pfstype_dir,
+	pfstype_this,
+	pfstype_parent,
+	pfstype_file,
+	pfstype_symlink,
+	pfstype_procdep
+} pfs_type_t;
+
+/* Flags */
+#define	PFS_DYNAMIC		1
+
+/*
+ * Data structures
+ */
+struct pfs_info;
+struct pfs_node;
+struct pfs_bitmap;
+
+typedef int (*pfs_fill_t)(struct pfs_node *, struct proc *, struct sbuf *);
+
+struct pfs_bitmap;		/* opaque */
+
+/*
+ * pfs_info: describes a pseudofs instance
+ */
+struct pfs_info {
+	char			 pi_name[MFSNAMELEN];
+	struct pfs_node		*pi_root;
+	/* members below this line aren't initialized */
+	/* currently, the mutex is only used to protect the bitmap */
+	struct mtx		 pi_mutex;
+	struct pfs_bitmap	*pi_bitmap;
+};
+
+/*
+ * pfs_node: describes a  node (file or directory) within a pseudofs
+ */
+struct pfs_node {
+	char			 pn_name[PFS_NAMELEN];
+	pfs_type_t		 pn_type;
+	int			 pn_flags;
+	uid_t			 pn_uid;
+	gid_t			 pn_gid;
+	mode_t			 pn_mode;
+	union {
+		void		*_pn_data;
+		pfs_fill_t	 _pn_func;
+		struct pfs_node	*_pn_nodes;
+	} u1;
+#define pn_data		u1._pn_data
+#define pn_func		u1._pn_func
+#define pn_nodes	u1._pn_nodes
+	/* members below this line aren't initialized */
+	struct pfs_node		*pn_parent;
+	union {
+		u_int32_t	 _pn_fileno;
+		struct pfs_node	*_pn_shadow;
+	} u2;
+#define pn_fileno	u2._pn_fileno
+#define pn_shadow	u2._pn_shadow
+};
+
+#define PFS_NODE(name, type, flags, uid, gid, mode, data) \
+        { (name), (type), (flags), (uid), (gid), (mode), { (data) } }
+#define PFS_DIR(name, flags, uid, gid, mode, nodes) \
+        PFS_NODE(name, pfstype_dir, flags, uid, gid, mode, nodes)
+#define PFS_ROOT(nodes) \
+        PFS_NODE("/", pfstype_root, 0, 0, 0, 0555, nodes)
+#define PFS_THIS \
+	PFS_NODE(".", pfstype_this, 0, 0, 0, 0, NULL)
+#define PFS_PARENT \
+	PFS_NODE("..", pfstype_parent, 0, 0, 0, 0, NULL)
+#define PFS_FILE(name, flags, uid, gid, mode, func) \
+	PFS_NODE(name, pfstype_file, flags, uid, gid, mode, func)
+#define PFS_SYMLINK(name, flags, uid, gid, mode, func) \
+	PFS_NODE(name, pfstype_symlink, flags, uid, gid, mode, func)
+#define PFS_LASTNODE \
+	PFS_NODE("", pfstype_none, 0, 0, 0, 0, NULL)
+
+/*
+ * VFS interface
+ */
+int	 pfs_mount		(struct pfs_info *pi,
+				 struct mount *mp, char *path, caddr_t data,
+				 struct nameidata *ndp, struct proc *p);
+int	 pfs_unmount		(struct mount *mp, int mntflags,
+				 struct proc *p);
+int	 pfs_root		(struct mount *mp, struct vnode **vpp);
+int	 pfs_statfs		(struct mount *mp, struct statfs *sbp,
+				 struct proc *p);
+int	 pfs_init		(struct pfs_info *pi, struct vfsconf *vfc);
+int	 pfs_uninit		(struct pfs_info *pi, struct vfsconf *vfc);
+
+/*
+ * Now for some initialization magic...
+ */
+#define PSEUDOFS(name, root)						\
+									\
+static struct pfs_info name##_info = {					\
+	#name,								\
+	&(root)								\
+};									\
+									\
+static int								\
+_##name##_mount(struct mount *mp, char *path, caddr_t data,		\
+	     struct nameidata *ndp, struct proc *p) {			\
+        return pfs_mount(&name##_info, mp, path, data, ndp, p);		\
+}									\
+									\
+static int								\
+_##name##_init(struct vfsconf *vfc) {					\
+        return pfs_init(&name##_info, vfc);				\
+}									\
+									\
+static int								\
+_##name##_uninit(struct vfsconf *vfc) {					\
+        return pfs_uninit(&name##_info, vfc);				\
+}									\
+									\
+static struct vfsops testfs_vfsops = {					\
+	_##name##_mount,						\
+	vfs_stdstart,							\
+	pfs_unmount,							\
+	pfs_root,							\
+	vfs_stdquotactl,						\
+	pfs_statfs,							\
+	vfs_stdsync,							\
+	vfs_stdvget,							\
+	vfs_stdfhtovp,							\
+	vfs_stdcheckexp,						\
+	vfs_stdvptofh,							\
+	_##name##_init,							\
+	_##name##_uninit,						\
+	vfs_stdextattrctl,						\
+};									\
+VFS_SET(name##_vfsops, name, VFCF_SYNTHETIC);				\
+MODULE_DEPEND(name, pseudofs, 1, 1, 1);
+
+#endif
---
+++ sys/fs/pseudofs/pseudofs_vnops.c
@@ -0,0 +1,419 @@
+/*-
+ * Copyright (c) 2001 Dag-Erling Coïdan Smørgrav
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer
+ *    in this position and unchanged.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *	$FreeBSD$
+ */
+
+#include <sys/param.h>
+#include <sys/kernel.h>
+#include <sys/systm.h>
+#include <sys/ctype.h>
+#include <sys/dirent.h>
+#include <sys/mount.h>
+#include <sys/namei.h>
+#include <sys/proc.h>
+#include <sys/sbuf.h>
+#include <sys/sx.h>
+#include <sys/sysctl.h>
+#include <sys/vnode.h>
+
+#include <fs/pseudofs/pseudofs.h>
+#include <fs/pseudofs/pseudofs_internal.h>
+
+/*
+ * Verify permissions
+ */
+static int
+pfs_access(struct vop_access_args *va)
+{
+	struct vnode *vn = va->a_vp;
+	struct vattr vattr;
+	int error;
+	
+	error = VOP_GETATTR(vn, &vattr, va->a_cred, va->a_p);
+	if (error)
+		return (error);
+	error = vaccess(vn->v_type, vattr.va_mode, vattr.va_uid,
+	    vattr.va_gid, va->a_mode, va->a_cred, NULL);
+	return (error);
+}
+
+/*
+ * Close a file or directory
+ */
+static int
+pfs_close(struct vop_close_args *va)
+{
+	return (0);
+}
+
+/*
+ * Get file attributes
+ */
+static int
+pfs_getattr(struct vop_getattr_args *va)
+{
+	struct vnode *vn = va->a_vp;
+	struct pfs_node *pn = (struct pfs_node *)vn->v_data;
+	struct vattr *vap = va->a_vap;
+
+	VATTR_NULL(vap);
+	vap->va_type = vn->v_type;
+	vap->va_mode = pn->pn_mode;
+	vap->va_fileid = pn->pn_fileno;
+	vap->va_flags = 0;
+	vap->va_blocksize = PAGE_SIZE;
+	vap->va_bytes = vap->va_size = 0;
+	vap->va_fsid = vn->v_mount->mnt_stat.f_fsid.val[0];
+	vap->va_nlink = 1;
+	nanotime(&vap->va_ctime);
+	vap->va_atime = vap->va_mtime = vap->va_ctime;
+	vap->va_uid = pn->pn_uid;
+	vap->va_gid = pn->pn_gid;
+
+	return (0);
+}
+
+/*
+ * Look up a file or directory
+ */
+static int
+pfs_lookup(struct vop_lookup_args *va)
+{
+	struct vnode *dvp = va->a_dvp;
+	struct vnode **vpp = va->a_vpp;
+	struct componentname *cnp = va->a_cnp;
+#if 0
+	struct pfs_info *pi = (struct pfs_info *)dvp->v_mount->mnt_data;
+#endif
+	struct pfs_node *pd = (struct pfs_node *)dvp->v_data, *pn;
+	struct proc *p;
+	char *pname;
+	int error, i;
+	pid_t pid;
+
+	if (dvp->v_type != VDIR)
+		return (ENOTDIR);
+	
+	/* don't support CREATE, RENAME or DELETE */
+	if (cnp->cn_nameiop != LOOKUP)
+		return (EROFS);
+
+	/* shortcut */
+	if (cnp->cn_namelen >= PFS_NAMELEN)
+		return (ENOENT);
+	
+	/* self */
+	pname = cnp->cn_nameptr;
+	if (cnp->cn_namelen == 1 && *pname == '.') {
+		pn = pd;
+		*vpp = dvp;
+		VREF(dvp);
+		goto got_vnode;
+	}
+
+	/* parent */
+	if (cnp->cn_flags & ISDOTDOT) {
+		if (pd->pn_type == pfstype_root)
+			return (EIO);
+		KASSERT(pd->pn_parent, ("non-root directory has no parent"));
+		return pfs_vncache_alloc(dvp->v_mount, vpp, pd->pn_parent);
+	}
+
+	/* process dependent */
+	for (i = 0, pid = 0; i < cnp->cn_namelen && isdigit(pname[i]); ++i)
+		pid = pid * 10 + pname[i] - '0';
+	/* XXX assume that 8 digits is the maximum safe length for a pid */
+	if (i == cnp->cn_namelen && i < 8) {
+		/* see if this directory has process-dependent children */
+		for (pn = pd->pn_nodes; pn->pn_type; ++pn)
+			if (pn->pn_type == pfstype_procdep)
+				break;
+		if (pn->pn_type) {
+			/* XXX pfind(0) should DTRT here */
+			p = pid ? pfind(pid) : &proc0;
+			if (p == NULL)
+				return (ENOENT);
+			if (p_can(cnp->cn_proc, p, P_CAN_SEE, NULL))
+				/* pretend it doesn't exist */
+				return (ENOENT);
+#if 0
+			if (!pn->pn_shadow)
+				pfs_create_shadow(pn, p);
+			pn = pn->pn_shadow;
+			goto got_pnode;
+#else
+			/* not yet implemented */
+			return (EIO);
+#endif
+		}
+	}
+	
+	/* something else */
+	for (pn = pd->pn_nodes; pn->pn_type; ++pn) {
+		for (i = 0; i < cnp->cn_namelen && pn->pn_name[i]; ++i)
+			if (pname[i] != pn->pn_name[i])
+				break;
+		if (i == cnp->cn_namelen)
+			goto got_pnode;
+	}
+
+	return (ENOENT);
+ got_pnode:
+	if (!pn->pn_parent)
+		pn->pn_parent = pd;
+	error = pfs_vncache_alloc(dvp->v_mount, vpp, pn);
+	if (error)
+		return error;
+ got_vnode:
+	if (cnp->cn_flags & MAKEENTRY)
+		cache_enter(dvp, *vpp, cnp);
+	return (0);
+}
+
+/*
+ * Open a file or directory.
+ */
+static int
+pfs_open(struct vop_open_args *va)
+{
+	/* XXX */
+	return (0);
+}
+
+/*
+ * Read from a file
+ */
+static int
+pfs_read(struct vop_read_args *va)
+{
+	struct vnode *vn = va->a_vp;
+	struct pfs_node *pn = vn->v_data;
+	struct uio *uio = va->a_uio;
+	struct sbuf sb;
+	char *ps;
+	int error, xlen;
+
+	if (vn->v_type != VREG)
+		return (EINVAL);
+
+	error = sbuf_new(&sb, NULL, uio->uio_offset + uio->uio_resid, 0);
+	if (error)
+		return (EIO);
+
+	error = (pn->pn_func)(pn, curproc, &sb);
+	
+	/* XXX we should possibly detect and handle overflows */
+	sbuf_finish(&sb);
+	ps = sbuf_data(&sb) + uio->uio_offset;
+	xlen = sbuf_len(&sb) - uio->uio_offset;
+	xlen = imin(xlen, uio->uio_resid);
+	error = (xlen <= 0 ? 0 : uiomove(ps, xlen, uio));
+	sbuf_delete(&sb);
+	return (error);
+}
+
+/*
+ * Return directory entries.
+ */
+static int
+pfs_readdir(struct vop_readdir_args *va)
+{
+	struct vnode *vn = va->a_vp;
+	struct pfs_info *pi;
+	struct pfs_node *pd, *pn;
+	struct dirent entry;
+	struct uio *uio;
+#if 0
+	struct proc *p;
+#endif
+	off_t offset;
+	int error, i, resid;
+
+	if (vn->v_type != VDIR)
+		return (ENOTDIR);
+	pi = (struct pfs_info *)vn->v_mount->mnt_data;
+	pd = (struct pfs_node *)vn->v_data;
+	pn = pd->pn_nodes;
+	uio = va->a_uio;
+
+	/* only allow reading entire entries */
+	offset = uio->uio_offset;
+	resid = uio->uio_resid;
+	if (offset < 0 || offset % PFS_DELEN != 0 || resid < PFS_DELEN)
+		return (EINVAL);
+
+	/* skip unwanted entries */
+	for (; pn->pn_type && offset > 0; ++pn, offset -= PFS_DELEN)
+		/* nothing */ ;
+
+	/* fill in entries */
+	entry.d_reclen = PFS_DELEN;
+	for (; pn->pn_type && resid > 0; ++pn) {
+		if (!pn->pn_parent)
+			pn->pn_parent = pd;
+		if (!pn->pn_fileno)
+			pfs_fileno_alloc(pi, pn);
+		entry.d_fileno = pn->pn_fileno;
+		/* PFS_DELEN was picked to fit PFS_NAMLEN */
+		for (i = 0; i < PFS_NAMELEN - 1 && pn->pn_name[i] != '\0'; ++i)
+			entry.d_name[i] = pn->pn_name[i];
+		entry.d_name[i] = 0;
+		entry.d_namlen = i;
+		switch (pn->pn_type) {
+		case pfstype_root:
+		case pfstype_dir:
+		case pfstype_this:
+		case pfstype_parent:
+			entry.d_type = DT_DIR;
+			break;
+		case pfstype_file:
+			entry.d_type = DT_REG;
+			break;
+		case pfstype_symlink:
+			entry.d_type = DT_LNK;
+			break;
+		case pfstype_procdep:
+			/* don't handle process-dependent nodes here */
+			continue;
+		default:
+			panic("%s has unexpected node type: %d", pn->pn_name, pn->pn_type);
+		}
+		if ((error = uiomove((caddr_t)&entry, PFS_DELEN, uio)))
+			return (error);
+		offset += PFS_DELEN;
+		resid -= PFS_DELEN;
+	}
+#if 0
+	for (pn = pd->pn_nodes; pn->pn_type && resid > 0; ++pn) {
+		if (pn->pn_type != pfstype_procdep)
+			continue;
+			
+		sx_slock(&allproc_lock);
+		p = LIST_FIRST(&allproc);
+		
+		sx_sunlock(&allproc_lock);
+		offset += PFS_DELEN;
+		resid -= PFS_DELEN;
+		break;
+	}
+#endif
+	
+	uio->uio_offset += offset;
+	return (0);
+}
+
+/*
+ * Read a symbolic link
+ */
+static int
+pfs_readlink(struct vop_readlink_args *va)
+{
+	struct vnode *vn = va->a_vp;
+	struct pfs_node *pn = vn->v_data;
+	struct uio *uio = va->a_uio;
+	char buf[MAXPATHLEN], *ps;
+	struct sbuf sb;
+	int error, xlen;
+
+	if (vn->v_type != VLNK)
+		return (EINVAL);
+
+	/* sbuf_new() can't fail with a static buffer */
+	sbuf_new(&sb, buf, sizeof buf, 0);
+
+	error = (pn->pn_func)(pn, curproc, &sb);
+	
+	/* XXX we should detect and handle overflows */
+	sbuf_finish(&sb);
+	ps = sbuf_data(&sb) + uio->uio_offset;
+	xlen = sbuf_len(&sb) - uio->uio_offset;
+	xlen = imin(xlen, uio->uio_resid);
+	error = (xlen <= 0 ? 0 : uiomove(ps, xlen, uio));
+	sbuf_delete(&sb);
+	return (error);
+}
+
+/*
+ * Reclaim a vnode
+ */
+static int
+pfs_reclaim(struct vop_reclaim_args *va)
+{
+	return (pfs_vncache_free(va->a_vp));
+}
+
+/*
+ * Set attributes
+ */
+static int
+pfs_setattr(struct vop_setattr_args *va)
+{
+	if (va->a_vap->va_flags != VNOVAL)
+		return (EOPNOTSUPP);
+	return (0);
+}
+
+/*
+ * Dummy operations
+ */
+static int pfs_erofs(void *va)		{ return (EROFS); }
+static int pfs_null(void *va)		{ return (0); }
+
+/*
+ * Vnode operations
+ */
+vop_t **pfs_vnodeop_p;
+static struct vnodeopv_entry_desc pfs_vnodeop_entries[] = {
+	{ &vop_default_desc,		(vop_t *)vop_defaultop	},
+	{ &vop_access_desc,		(vop_t *)pfs_access	},
+	{ &vop_close_desc,		(vop_t *)pfs_close	},
+	{ &vop_create_desc,		(vop_t *)pfs_erofs	},
+	{ &vop_getattr_desc,		(vop_t *)pfs_getattr	},
+	{ &vop_link_desc,		(vop_t *)pfs_erofs	},
+	{ &vop_lookup_desc,		(vop_t *)pfs_lookup	},
+	{ &vop_mkdir_desc,		(vop_t *)pfs_erofs	},
+	{ &vop_open_desc,		(vop_t *)pfs_open	},
+	{ &vop_read_desc,		(vop_t *)pfs_read	},
+	{ &vop_readdir_desc,		(vop_t *)pfs_readdir	},
+	{ &vop_readlink_desc,		(vop_t *)pfs_readlink	},
+	{ &vop_reclaim_desc,		(vop_t *)pfs_reclaim	},
+	{ &vop_remove_desc,		(vop_t *)pfs_erofs	},
+	{ &vop_rename_desc,		(vop_t *)pfs_erofs	},
+	{ &vop_rmdir_desc,		(vop_t *)pfs_erofs	},
+	{ &vop_setattr_desc,		(vop_t *)pfs_setattr	},
+	{ &vop_symlink_desc,		(vop_t *)pfs_erofs	},
+	{ &vop_write_desc,		(vop_t *)pfs_erofs	},
+	/* XXX I've probably forgotten a few that need pfs_erofs */
+	{ NULL,				(vop_t *)NULL		}
+};
+
+static struct vnodeopv_desc pfs_vnodeop_opv_desc =
+	{ &pfs_vnodeop_p, pfs_vnodeop_entries };
+
+VNODEOP_SET(pfs_vnodeop_opv_desc);
+
