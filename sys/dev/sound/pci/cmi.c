9a3c0b7476cc02f4b6a682fd8b2c3e00b72fa904
orion <orion@FreeBSD.org>
2001-04-07 14:12:53 +0000

    Quieten when re-triggering.

--- sys/dev/sound/pci/cmi.c
+++ sys/dev/sound/pci/cmi.c
@@ -268,6 +268,8 @@ cmi_ch0_stop(struct sc_info *sc, struct sc_chinfo *ch)
 
 	cmi_clr4(sc, CMPCI_REG_INTR_CTRL, CMPCI_REG_CH0_INTR_ENABLE);
 	cmi_clr4(sc, CMPCI_REG_FUNC_0, CMPCI_REG_CH0_ENABLE); 
+        cmi_set4(sc, CMPCI_REG_FUNC_0, CMPCI_REG_CH0_RESET); 
+        cmi_clr4(sc, CMPCI_REG_FUNC_0, CMPCI_REG_CH0_RESET);
 	ch->dma_active = 0;
 	return r;
 }
@@ -291,6 +293,8 @@ cmi_ch1_stop(struct sc_info *sc, struct sc_chinfo *ch)
 
 	cmi_clr4(sc, CMPCI_REG_INTR_CTRL, CMPCI_REG_CH1_INTR_ENABLE);
 	cmi_clr4(sc, CMPCI_REG_FUNC_0, CMPCI_REG_CH1_ENABLE); 
+        cmi_set4(sc, CMPCI_REG_FUNC_0, CMPCI_REG_CH1_RESET); 
+        cmi_clr4(sc, CMPCI_REG_FUNC_0, CMPCI_REG_CH1_RESET);
 	ch->dma_active = 0;
 	return r;
 }
