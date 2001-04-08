8bcbed9d961a3a71ec7fa74f53b1e172161c760b
joe <joe@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-04-08 21:35:17 +0000

    Don't complain about deprecated environment variables, like MUSIC_CD, being present in the environment if the user has CDROM defined, or has specified a device on the command line.
    This avoids users of ports like 'workman' that use these variables
    getting gratuitous warnings from cdcontrol.
    
    Suggested by:	John Sellens <jsellens@generalconcepts.com>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@75324 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/cdcontrol/cdcontrol.c
+++ usr.sbin/cdcontrol/cdcontrol.c
@@ -176,14 +176,6 @@ int main (int argc, char **argv)
 	int cmd;
 	char *arg;
 
-	cdname = use_cdrom_instead("MUSIC_CD");
-	if (! cdname)
-		cdname = use_cdrom_instead("CD_DRIVE");
-	if (! cdname)
-		cdname = use_cdrom_instead("DISC");
-	if (! cdname)
-		cdname = use_cdrom_instead("CDPLAY");
-
 	for (;;) {
 		switch (getopt (argc, argv, "svhf:")) {
 		case EOF:
@@ -212,6 +204,15 @@ int main (int argc, char **argv)
 	if (! cdname) {
 		cdname = getenv("CDROM");
 	}
+
+	if (! cdname)
+		cdname = use_cdrom_instead("MUSIC_CD");
+	if (! cdname)
+		cdname = use_cdrom_instead("CD_DRIVE");
+	if (! cdname)
+		cdname = use_cdrom_instead("DISC");
+	if (! cdname)
+		cdname = use_cdrom_instead("CDPLAY");
 
 	if (! cdname) {
 		cdname = DEFAULT_CD_DRIVE;
