555153469623b11a50ac7fddd7a4f5375646363e
Murray Stokely <murray@FreeBSD.org>
2001-04-11 02:12:14 +0000

    Source rc.conf so that named.restart can restart named with the correct flags.
    PR:		misc/25049
    Submitted by:	Richard Roderick <richard@gohome.net>

--- usr.sbin/named.restart/named.restart.sh
+++ usr.sbin/named.restart/named.restart.sh
@@ -4,4 +4,10 @@
 # $FreeBSD$
 #
 
-exec %DESTSBIN%/%INDOT%ndc restart
+if [ -r /etc/defaults/rc.conf ]; then
+        . /etc/defaults/rc.conf
+        source_rc_confs
+elif [ -r /etc/rc.conf ]; then
+        . /etc/rc.conf
+fi
+exec %DESTSBIN%/%INDOT%ndc restart ${named_flags}
