66ec9f838f3ff9cc78bd75c4a506a733bfbc91fd
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-04-11 18:50:50 +0000

    Stick proc0 in the PID hash table.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@75423 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -287,6 +287,7 @@ proc0_init(void *dummy __unused)
 	 * Create process 0 (the swapper).
 	 */
 	LIST_INSERT_HEAD(&allproc, p, p_list);
+	LIST_INSERT_HEAD(PIDHASH(0), p, p_hash);
 	p->p_pgrp = &pgrp0;
 	LIST_INSERT_HEAD(PGRPHASH(0), &pgrp0, pg_hash);
 	LIST_INIT(&pgrp0.pg_members);
