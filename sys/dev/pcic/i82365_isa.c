7ae2f6eb4831c0e552c6edc281cfdc0bc02785e5
imp <imp@FreeBSD.org>
2001-04-11 20:22:16 +0000

    Add IBM3765 to newcard's pcic pnp device list

--- sys/dev/pcic/i82365_isa.c
+++ sys/dev/pcic/i82365_isa.c
@@ -111,12 +111,13 @@ int	pcicisa_debug = PCICISADEBUG;
 #endif
 
 static struct isa_pnp_id pcic_ids[] = {
+	{PCIC_PNP_ACTIONTEC,		NULL},		/* AEI0218 */
+	{PCIC_PNP_IBM3765,		NULL},		/* IBM3765 */
 	{PCIC_PNP_82365,		NULL},		/* PNP0E00 */
 	{PCIC_PNP_CL_PD6720,		NULL},		/* PNP0E01 */
 	{PCIC_PNP_VLSI_82C146,		NULL},		/* PNP0E02 */
 	{PCIC_PNP_82365_CARDBUS,	NULL},		/* PNP0E03 */
-	{PCIC_PNP_ACTIONTEC,		NULL},		/* AEI0218 */ 
-	{PCIC_PNP_SCM_SWAPBOX,		NULL},		/* SCM0469 */ 
+	{PCIC_PNP_SCM_SWAPBOX,		NULL},		/* SCM0469 */
 	{0}
 };
 
