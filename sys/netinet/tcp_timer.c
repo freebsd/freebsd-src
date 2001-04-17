f0a04f3f51485131962ee392decaedefe989018c
Kris Kennaway <kris@FreeBSD.org>
2001-04-17 18:08:01 +0000

    Randomize the TCP initial sequence numbers more thoroughly.
    Obtained from:	OpenBSD
    Reviewed by:	jesper, peter, -developers

--- sys/netinet/tcp_timer.c
+++ sys/netinet/tcp_timer.c
@@ -132,8 +132,8 @@ tcp_slowtimo()
 
 	tcp_maxidle = tcp_keepcnt * tcp_keepintvl;
 
-	tcp_iss += TCP_ISSINCR/PR_SLOWHZ;		/* increment iss */
 #ifdef TCP_COMPAT_42
+	tcp_iss += TCP_ISSINCR/PR_SLOWHZ;		/* increment iss */
 	if ((int)tcp_iss < 0)
 		tcp_iss = TCP_ISSINCR;			/* XXX */
 #endif
