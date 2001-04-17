9f06645d9986a56ea23299485482738e57879315
Ruslan Ermilov <ru@FreeBSD.org>
2001-04-17 12:23:50 +0000

    Removed files not present in v1.17 import.

--- contrib/groff/eqn/neqn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Provision of this shell script should not be taken to imply that use of
-# GNU eqn with groff -Tascii|-Tlatin1|-Tutf8|-Tcp1047 is supported.
-
-exec @g@eqn -Tascii ${1+"$@"}
