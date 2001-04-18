61abb2eff01abad30725f9e2506332c049cc0fa6
ru <ru@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-04-18 12:13:08 +0000

    Add missing -mtty-char for MANBUILDCAT.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@75655 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/bsd.man.mk
+++ share/mk/bsd.man.mk
@@ -53,7 +53,7 @@ MINSTALL=	${INSTALL} ${COPY} -o ${MANOWN} -g ${MANGRP} -m ${MANMODE}
 
 CATDIR=		${MANDIR:H:S/$/\/cat/}
 CATEXT=		.cat
-MROFF_CMD?=	groff -Tascii -man
+MROFF_CMD?=	groff -Tascii -mtty-char -man
 
 MCOMPRESS_CMD?=	${COMPRESS_CMD}
 MCOMPRESS_EXT?=	${COMPRESS_EXT}
