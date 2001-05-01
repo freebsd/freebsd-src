26ebd8ffb4cfc65caeb14a33c88dccf7193ef1a1
joe <joe@FreeBSD.org>
2001-05-01 09:25:24 +0000

    * Add Config.pm to fight situations when perldoc(1) showed a manual page   for a module overridden by BSDPAN instead of the original module.
    * Fix wrong manual section numbers in SEE ALSO.
    
    * Add `Revision 42' to the beer-ware license.  The BSDPAN author did not
      originally get the reference and removed the revision from original
      phk's version.
    
    Submitted by:	Anton Berezin <tobez@tobez.org>

--- gnu/usr.bin/perl/BSDPAN/BSDPAN/Override.pm
+++ gnu/usr.bin/perl/BSDPAN/BSDPAN/Override.pm
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# "THE BEER-WARE LICENSE"
+# "THE BEER-WARE LICENSE" (Revision 42)
 # <tobez@tobez.org> wrote this file.  As long as you retain this notice you
 # can do whatever you want with this stuff. If we meet some day, and you think
 # this stuff is worth it, you can buy me a beer in return.   Anton Berezin
@@ -148,6 +148,6 @@ Anton Berezin, tobez@tobez.org
 
 =head1 SEE ALSO
 
-perl(1), L<BSDPAN(1)>.
+perl(1), L<BSDPAN(3)>.
 
 =cut
