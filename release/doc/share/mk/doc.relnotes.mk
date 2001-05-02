1eb55ff65dd85e929f20bc5c4f11d71d76096b92
Bruce A. Mah <bmah@FreeBSD.org>
2001-05-02 22:02:50 +0000

    RELNOTESng infrastructure fixes:
    1.  There is now only one RELNOTESng stylesheet; the architecture-specific
    stylesheets (to handle different values of the arch= attribute) are gone.
    
    2.  Several Makefile variable definitions were factored into
    doc.relnotes.mk.
    
    Submitted by:	dd

--- release/doc/share/mk/doc.relnotes.mk
+++ release/doc/share/mk/doc.relnotes.mk
@@ -2,5 +2,12 @@
 
 DOC_PREFIX?= ${RELN_ROOT}/../../../doc
 
+# Find the RELNOTESng document catalog
+EXTRA_CATALOGS+= ${RELN_ROOT}/share/sgml/catalog
+
+# Use the appropriate architecture-dependent RELNOTESng stylesheet
+DSLHTML?=	${RELN_ROOT}/share/sgml/release.dsl
+DSLPRINT?=	${RELN_ROOT}/share/sgml/release.dsl
+
 # XXX using /release/doc as anchor!
 DESTDIR?= ${DOCDIR}/${.CURDIR:C/^.*\/release\/doc//}
