461d7e14727691ea7057292a302d20690782e1e6
green <green@FreeBSD.org>
2001-05-03 00:29:28 +0000

    Add a "VersionAddendum" configuration setting for sshd which allows anyone to easily change the part of the OpenSSH version after the main version number.  The FreeBSD-specific version banner could be disabled that way, for example:
    # Call ourselves plain OpenSSH
    VersionAddendum

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -1,4 +1,12 @@
 /* $FreeBSD$ */
 /* $OpenBSD: version.h,v 1.13 2000/10/16 09:38:45 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_2.3.0 green@FreeBSD.org 20010319"
+#ifndef	SSH_VERSION
+
+#define SSH_VERSION		(ssh_version_get())
+#define SSH_VERSION_BASE	"OpenSSH_2.3.0"
+#define	SSH_VERSION_ADDENDUM	"green@FreeBSD.org 20010319"
+
+const char *ssh_version_get(void);
+void ssh_version_set_addendum(const char *add);
+#endif /* SSH_VERSION */
