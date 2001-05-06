d4be05a1d14cfcf9f9d255bed47aa2d6b3943a95
kris <kris@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-05-06 03:03:45 +0000

    * fix ctm(8) to use mkstemp() instead of tempnam() for tempfile   creation.
    * Tag the internal err() function with __printflike to allow checking
      for non-constant format string arguments (none exist)
    
    * Use fmtcheck() to sanitize the tar command obtained via -t to make
      sure it doesn't contain extraneous format operators.
    
    Reviewed by:    mikeh
    MFC after:      1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@76300 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/ctm/ctm_rmail/error.h
+++ usr.sbin/ctm/ctm_rmail/error.h
@@ -1,3 +1,5 @@
+/* $FreeBSD$ */
+
 extern	void	err_set_log(char *log_file);
 extern	void	err_prog_name(char *name);
-extern	void	err(char *fmt, ...);
+extern	void	err(const char *fmt, ...) __printflike(1, 2);
