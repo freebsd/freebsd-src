b909392dac462e6fce443b0caaa4b6b22c7a8929
imp <imp@FreeBSD.org>
2001-05-09 07:29:06 +0000

    MFC: 1.110 (eject hang fix) 1.111 (PC98 MECIA controller fix)

--- sys/pccard/pcic.c
+++ sys/pccard/pcic.c
@@ -502,8 +502,11 @@ pcic_probe(device_t dev)
 		cinfo.maxio	= 2;	/* fake for UE2212 LAN card */
 #endif  
 		validslots++;
-		/* XXX Do I need to allocated the port resources? */
-		device_set_desc(dev, "NEC PC98 Original PCMCIA Controller");
+		slt = pccard_alloc_slot(&cinfo);
+		slt->cdata = sp;
+		sp->slt = slt;
+		/* XXX need to allocated the port resources */
+		device_set_desc(dev, "MECIA PC98 Original PCMCIA Controller");
 	}
 #endif  /* PC98 */
 	return(validslots ? 0 : ENXIO);
@@ -855,9 +858,9 @@ pcicintr(void *arg)
 						pccard_event(sp->slt,
 						    card_inserted);
 					} else {
-						cinfo.disable(sp->slt);
 						pccard_event(sp->slt,
 						    card_removed);
+						cinfo.disable(sp->slt);
 					}
 				}
 			}
