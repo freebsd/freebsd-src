e79f2a8ad8163613578eb9db9689e145e5fba81a
assar <assar@FreeBSD.org>
2001-05-11 16:56:36 +0000

    de-constify to make it compatible with (krb4) and make-print-version
    Submitted by:	Peter Pentchev <roam@orbitel.bg> (just inverted)

--- kerberos5/include/version.h
+++ kerberos5/include/version.h
@@ -1,5 +1,5 @@
 /* $FreeBSD$ */
-const char *krb4_long_version = "@(#)$Version: krb4-1.0.5 (FreeBSD) $";
-const char *krb4_version = "krb4-1.0.5";
+char *krb4_long_version = "@(#)$Version: krb4-1.0.5 (FreeBSD) $";
+char *krb4_version = "krb4-1.0.5";
 const char *heimdal_long_version = "@(#)$Version: heimdal-0.3e (FreeBSD) $";
 const char *heimdal_version = "heimdal-0.3e";
