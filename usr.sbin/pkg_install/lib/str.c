ff7543ca42f8a42b04545cb0c23f0fb3a00eced2
Kris Kennaway <kris@FreeBSD.org>
2001-05-12 09:44:32 +0000

    Clean up some of the evil string handling in this code, replace warnx()+exit() with errx() and replace a big if..then..else construct to determine the package download directory with a lookup table.
    Reviewed by:	jkh
    MFC after:	2 weeks

--- usr.sbin/pkg_install/lib/str.c
+++ usr.sbin/pkg_install/lib/str.c
@@ -61,6 +61,20 @@ get_dash_string(char **str)
     return *str;
 }
 
+/* Do a strlcpy and test for overflow */
+int
+s_strlcpy(char *dst, const char *src, size_t size)
+{
+	return (strlcpy(dst, src, size) >= size);
+}
+
+/* Do a strlcat and test for overflow */
+int
+s_strlcat(char *dst, const char *src, size_t size)
+{
+	return (strlcat(dst, src, size) >= size);
+}
+
 /* Rather Obvious */
 char *
 copy_string(char *str)
