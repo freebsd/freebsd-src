e4d30079b1e939ccc009e8cfbe32015743681c23
green <green@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-05-15 01:50:40 +0000

    If a host would exceed 16 characters in the utmp entry, record only it's IP address/base host instead.
    Submitted by:	brian
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@76607 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- crypto/openssh/sshlogin.c
+++ crypto/openssh/sshlogin.c
@@ -99,7 +99,7 @@ record_login(pid_t pid, const char *ttyname, const char *user, uid_t uid,
 	strncpy(u.ut_line, ttyname + 5, sizeof(u.ut_line));
 	u.ut_time = time(NULL);
 	strncpy(u.ut_name, user, sizeof(u.ut_name));
-	strncpy(u.ut_host, host, sizeof(u.ut_host));
+	realhostname_sa(u.ut_host, sizeof(u.ut_host), addr, addr->sa_len);
 
 	login(&u);
 	lastlog = _PATH_LASTLOG;
