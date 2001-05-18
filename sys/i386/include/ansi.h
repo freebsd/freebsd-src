9b8531a258e38f8b9f4f5c3560bf58ecbd9b7048
David E. O'Brien <obrien@FreeBSD.org>
2001-05-18 05:53:09 +0000

    MFC: Add mbstate_t and wint_t.      Spell _BSD_TIME_T_ consistently as `int'. 	 Update comments. 	 Style tweaks.

--- sys/i386/include/ansi.h
+++ sys/i386/include/ansi.h
@@ -42,20 +42,22 @@
  * in more than one standard header are defined here.  Standard headers
  * then use:
  *	#ifdef	_BSD_SIZE_T_
- *	typedef	_BSD_SIZE_T_ size_t;
+ *	typedef	_BSD_SIZE_T_	size_t;
  *	#undef	_BSD_SIZE_T_
  *	#endif
  */
 #define	_BSD_CLOCK_T_	unsigned long		/* clock()... */
 #define	_BSD_CLOCKID_T_	int			/* clock_gettime()... */
+#define	_BSD_MBSTATE_T_	__mbstate_t		/* mbstate_t */
 #define	_BSD_PTRDIFF_T_	int			/* ptr1 - ptr2 */
 #define	_BSD_RUNE_T_	_BSD_CT_RUNE_T_		/* rune_t (see below) */
 #define	_BSD_SIZE_T_	unsigned int		/* sizeof() */
-#define	_BSD_SOCKLEN_T_	__uint32_t
+#define	_BSD_SOCKLEN_T_	__uint32_t		/* socklen_t (duh) */
 #define	_BSD_SSIZE_T_	int			/* byte count or error */
-#define	_BSD_TIME_T_	long			/* time()... */
+#define	_BSD_TIME_T_	int			/* time()... */
 #define	_BSD_TIMER_T_	int			/* timer_gettime()... */
 #define	_BSD_WCHAR_T_	_BSD_CT_RUNE_T_		/* wchar_t (see below) */
+#define	_BSD_WINT_T_	_BSD_CT_RUNE_T_		/* wint_t (see below) */
 
 /*
  * Types which are fundamental to the implementation and must be used
@@ -69,7 +71,7 @@
 #define	_BSD_VA_LIST_	char *			/* va_list */
 
 /*
- * The rune type is declared to be an ``int'' instead of the more natural
+ * The rune type above is declared to be an ``int'' instead of the more natural
  * ``unsigned long'' or ``long''.  Two things are happening here.  It is not
  * unsigned so that EOF (-1) can be naturally assigned to it and used.  Also,
  * it looks like 10646 will be a 31 bit standard.  This means that if your
@@ -78,6 +80,12 @@
  * ANSI C), but they use _BSD_CT_RUNE_T_ instead of int.  By changing it
  * here, you lose a bit of ANSI conformance, but your programs will still
  * work.
+ *
+ * NOTE: rune_t is not covered by ANSI nor other standards, and should not
+ * be instantiated outside of lib/libc/locale.  Use wchar_t.  wchar_t and
+ * rune_t must be the same type.  Also wint_t must be no narrower than
+ * wchar_t, and should also be able to hold all members of the largest
+ * character set plus one extra value (WEOF). wint_t must be at least 16 bits.
  */
 
 /*
@@ -99,6 +107,7 @@
 #define __offsetof(type, field) ((size_t)(&((type *)0)->field))
 
 /*
+ * XXX this paragraph is very out of date.
  * Typedefs for especially magic types.  #define's wouldn't work in the
  * __GNUC__ case, since __attribute__(()) only works in certain contexts.
  * This is not in <machine/types.h>, since that has too much namespace
@@ -127,5 +136,14 @@ typedef	unsigned int		 __uint32_t;
 
 typedef	int			 __intptr_t;
 typedef	unsigned int		__uintptr_t;
+
+/*
+ * mbstate_t is an opaque object to keep conversion state, during multibyte
+ * stream conversions.  The content must not be referenced by user programs.
+ */
+typedef union {
+	char		__mbstate8[128];
+	__int64_t	_mbstateL;		/* for alignment */
+} __mbstate_t;
 
 #endif /* !_MACHINE_ANSI_H_ */
