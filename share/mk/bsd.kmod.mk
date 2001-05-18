8dcffe87b0219d7b7bd18fb01da872dc2da40b76
Jonathan Lemon <jlemon@FreeBSD.org>
2001-05-18 19:42:32 +0000

    MFC: r1.86; don't check for ${_dir}/kern/.  This allows the tree to be built with the sources located elsewhere than /usr/src.

--- share/mk/bsd.kmod.mk
+++ share/mk/bsd.kmod.mk
@@ -2,11 +2,11 @@
 
 # Search for kernel source tree in standard places.
 .for _dir in ${.CURDIR}/../.. ${.CURDIR}/../../.. ${.CURDIR}/../../../.. /sys /usr/src/sys
-.if !defined(SYSDIR) && exists(${_dir}/kern/) && exists(${_dir}/conf/)
+.if !defined(SYSDIR) && exists(${_dir}/conf/kmod.mk)
 SYSDIR=	${_dir}
 .endif
 .endfor
-.if !defined(SYSDIR) || !exists(${SYSDIR}/kern/) || !exists(${SYSDIR}/conf/)
+.if !defined(SYSDIR) || !exists(${SYSDIR}/kern) || !exists(${SYSDIR}/conf/)
 .error "can't find kernel source tree"
 .endif
 
