99d3deb016e8671d0492641fac111747e4994bb6
obrien <obrien@FreeBSD.org>
2001-05-18 01:40:40 +0000

    Style changes -- revert ordering to mostly two revs ago. Embellish some comments, fix tab'ing.
    Requested by:	bde

--- sys/i386/include/ansi.h
+++ sys/i386/include/ansi.h
@@ -38,6 +38,76 @@
 #define	_MACHINE_ANSI_H_
 
 /*
+ * Types which are fundamental to the implementation and must be declared
+ * in more than one standard header are defined here.  Standard headers
+ * then use:
+ *	#ifdef	_BSD_SIZE_T_
+ *	typedef	_BSD_SIZE_T_	size_t;
+ *	#undef	_BSD_SIZE_T_
+ *	#endif
+ */
+#define	_BSD_CLOCK_T_	unsigned long		/* clock()... */
+#define	_BSD_CLOCKID_T_	int			/* clock_gettime()... */
+#define	_BSD_MBSTATE_T_	__mbstate_t		/* mbstate_t */
+#define	_BSD_PTRDIFF_T_	int			/* ptr1 - ptr2 */
+#define	_BSD_RUNE_T_	_BSD_CT_RUNE_T_		/* rune_t (see below) */
+#define	_BSD_SIZE_T_	unsigned int		/* sizeof() */
+#define	_BSD_SOCKLEN_T_	__uint32_t		/* socklen_t (duh) */
+#define	_BSD_SSIZE_T_	int			/* byte count or error */
+#define	_BSD_TIME_T_	long			/* time()... */
+#define	_BSD_TIMER_T_	int			/* timer_gettime()... */
+#define	_BSD_WCHAR_T_	_BSD_CT_RUNE_T_		/* wchar_t (see below) */
+#define	_BSD_WINT_T_	_BSD_CT_RUNE_T_		/* wint_t (see below) */
+
+/*
+ * Types which are fundamental to the implementation and must be used
+ * in more than one standard header although they are only declared in
+ * one (perhaps nonstandard) header are defined here.  Standard headers
+ * use _BSD_XXX_T_ without undef'ing it.
+ */
+#define	_BSD_CT_RUNE_T_	int			/* arg type for ctype funcs */
+#define	_BSD_OFF_T_	__int64_t		/* file offset */
+#define	_BSD_PID_T_	int			/* process [group] */
+#define	_BSD_VA_LIST_	char *			/* va_list */
+
+/*
+ * The rune type above is declared to be an ``int'' instead of the more natural
+ * ``unsigned long'' or ``long''.  Two things are happening here.  It is not
+ * unsigned so that EOF (-1) can be naturally assigned to it and used.  Also,
+ * it looks like 10646 will be a 31 bit standard.  This means that if your
+ * ints cannot hold 32 bits, you will be in trouble.  The reason an int was
+ * chosen over a long is that the is*() and to*() routines take ints (says
+ * ANSI C), but they use _BSD_CT_RUNE_T_ instead of int.  By changing it
+ * here, you lose a bit of ANSI conformance, but your programs will still
+ * work.
+ *
+ * NOTE: rune_t is not covered by ANSI nor other standards, and should not
+ * be instantiated outside of lib/libc/locale.  Use wchar_t.  wchar_t and
+ * rune_t must be the same type.  Also wint_t must be no narrower than
+ * wchar_t, and should also be able to hold all members of the largest
+ * character set plus one extra value (WEOF). wint_t must be at least 16 bits.
+ */
+
+/*
+ * Frequencies of the clock ticks reported by clock() and times().  They
+ * are the same as stathz for bogus historical reasons.  They should be
+ * 1e6 because clock() and times() are implemented using getrusage() and
+ * there is no good reason why they should be less accurate.  There is
+ * the bad reason that (broken) programs might not like clock_t or
+ * CLOCKS_PER_SEC being ``double'' (``unsigned long'' is not large enough
+ * to hold the required 24 hours worth of ticks if the frequency is
+ * 1000000ul, and ``unsigned long long'' would be nonstandard).
+ */
+#define	_BSD_CLK_TCK_		128
+#define	_BSD_CLOCKS_PER_SEC_	128
+
+/*
+ * We define this here since both <stddef.h> and <sys/types.h> needs it.
+ */
+#define __offsetof(type, field) ((size_t)(&((type *)0)->field))
+
+/*
+ * XXX this paragraph is very out of date.
  * Typedefs for especially magic types.  #define's wouldn't work in the
  * __GNUC__ case, since __attribute__(()) only works in certain contexts.
  * This is not in <machine/types.h>, since that has too much namespace
@@ -68,82 +138,12 @@ typedef	int			 __intptr_t;
 typedef	unsigned int		__uintptr_t;
 
 /*
- * Types which are fundamental to the implementation and must be declared
- * in more than one standard header are defined here.  Standard headers
- * then use:
- *	#ifdef	_BSD_SIZE_T_
- *	typedef	_BSD_SIZE_T_ size_t;
- *	#undef	_BSD_SIZE_T_
- *	#endif
- */
-#define	_BSD_CLOCK_T_	unsigned long		/* clock()... */
-#define	_BSD_CLOCKID_T_	int			/* clock_gettime()... */
-#define	_BSD_PTRDIFF_T_	int			/* ptr1 - ptr2 */
-#define	_BSD_RUNE_T_	_BSD_CT_RUNE_T_		/* rune_t (see below) */
-#define	_BSD_SIZE_T_	unsigned int		/* sizeof() */
-#define	_BSD_SOCKLEN_T_	__uint32_t
-#define	_BSD_SSIZE_T_	int			/* byte count or error */
-#define	_BSD_TIME_T_	long			/* time()... */
-#define	_BSD_TIMER_T_	int			/* timer_gettime()... */
-#define	_BSD_WCHAR_T_	_BSD_CT_RUNE_T_		/* wchar_t (see below) */
-
-/*
- * Types which are fundamental to the implementation and must be used
- * in more than one standard header although they are only declared in
- * one (perhaps nonstandard) header are defined here.  Standard headers
- * use _BSD_XXX_T_ without undef'ing it.
- */
-#define	_BSD_OFF_T_	__int64_t		/* file offset */
-#define	_BSD_PID_T_	int			/* process [group] */
-#define	_BSD_VA_LIST_	char *			/* va_list */
-
-/*
- * NOTE: rune_t is not covered by ANSI nor other standards, and should not
- * be instantiated outside of lib/libc/locale.  Use wchar_t.
- *
- * The rune type is declared to be an ``int'' instead of the more natural
- * ``unsigned long'' or ``long''.  Two things are happening here.  It is not
- * unsigned so that EOF (-1) can be naturally assigned to it and used.  Also,
- * it looks like 10646 will be a 31 bit standard.  This means that if your
- * ints cannot hold 32 bits, you will be in trouble.  The reason an int was
- * chosen over a long is that the is*() and to*() routines take ints (says
- * ANSI C), but they use _BSD_CT_RUNE_T_ instead of int.  By changing it
- * here, you lose a bit of ANSI conformance, but your programs will still
- * work.
- *
- * Note that _WCHAR_T_ and _RUNE_T_ must be of the same type.
- */
-#define	_BSD_CT_RUNE_T_	int			/* arg type for ctype funcs */
-#define	_BSD_RUNE_T_	_BSD_CT_RUNE_T_		/* rune_t */
-#define	_BSD_WCHAR_T_	_BSD_CT_RUNE_T_		/* wchar_t */
-#define	_BSD_WINT_T_	_BSD_CT_RUNE_T_		/* wint_t */
-
-/*
  * mbstate_t is an opaque object to keep conversion state, during multibyte
  * stream conversions.  The content must not be referenced by user programs.
  */
 typedef union {
 	char		__mbstate8[128];
 	__int64_t	_mbstateL;		/* for alignment */
- } __mbstate_t;
-#define	_BSD_MBSTATE_T_	__mbstate_t		/* mbstate_t */
-
-/*
- * Frequencies of the clock ticks reported by clock() and times().  They
- * are the same as stathz for bogus historical reasons.  They should be
- * 1e6 because clock() and times() are implemented using getrusage() and
- * there is no good reason why they should be less accurate.  There is
- * the bad reason that (broken) programs might not like clock_t or
- * CLOCKS_PER_SEC being ``double'' (``unsigned long'' is not large enough
- * to hold the required 24 hours worth of ticks if the frequency is
- * 1000000ul, and ``unsigned long long'' would be nonstandard).
- */
-#define	_BSD_CLK_TCK_		128
-#define	_BSD_CLOCKS_PER_SEC_	128
-
-/*
- * We define this here since both <stddef.h> and <sys/types.h> needs it.
- */
-#define __offsetof(type, field) ((size_t)(&((type *)0)->field))
+} __mbstate_t;
 
 #endif /* !_MACHINE_ANSI_H_ */
