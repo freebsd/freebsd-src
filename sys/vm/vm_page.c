1028f732ee1e58de3475dbacc2b9d3fc7964d96a
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-05-22 07:01:11 +0000

    Sort includes.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@76978 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/vm/vm_page.c
+++ sys/vm/vm_page.c
@@ -71,8 +71,8 @@
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/lock.h>
-#include <sys/mutex.h>
 #include <sys/malloc.h>
+#include <sys/mutex.h>
 #include <sys/proc.h>
 #include <sys/vmmeter.h>
 #include <sys/vnode.h>
