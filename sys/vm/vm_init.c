a4d233e45a91c23426d0b6aec3e92a248e0a6bdf
jhb <jhb@FreeBSD.org>
2001-05-22 05:35:45 +0000

    Sort includes from previous commit.

--- sys/vm/vm_init.c
+++ sys/vm/vm_init.c
@@ -71,9 +71,9 @@
 #include <sys/param.h>
 #include <sys/kernel.h>
 #include <sys/lock.h>
+#include <sys/mutex.h>
 #include <sys/proc.h>
 #include <sys/systm.h>
-#include <sys/mutex.h>
 
 #include <vm/vm.h>
 #include <vm/vm_object.h>
