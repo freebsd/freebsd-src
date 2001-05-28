01707e0035062e46b35977381c90da78a664751b
obrien <obrien@FreeBSD.org>
2001-05-28 09:52:21 +0000

    One needs to introduce things with a `.file' directive before trying to do a .loc on it.  BTW, the .loc needs to be in a .text section. gas 2.11.0 catches these oversights where previous versions did not.

--- sys/alpha/alpha/prom_disp.s
+++ sys/alpha/alpha/prom_disp.s
@@ -39,7 +39,9 @@ __KERNEL_RCSID(2, "$NetBSD: prom_disp.s,v 1.8 1997/11/03 04:22:03 ross Exp $");
 	.text
 	.align	4
 
-inc3:	.stabs	__FILE__,132,0,0,inc3; .loc	1 __LINE__
+inc3:	.stabs	__FILE__,132,0,0,inc3
+	.text 
+	.loc	1 __LINE__
 /*
  * Dispatcher routine.  Implements prom's calling machinery, saves our
  * callee-saved registers as required by C.
