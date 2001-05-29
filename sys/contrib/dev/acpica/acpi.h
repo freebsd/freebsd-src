82163b01bda57c613957181f921be6e8123b2d1d
msmith <msmith@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-05-29 20:03:32 +0000

    - wbinvd isn't an inline on ia64.  - includes are not in subdirectories.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@77429 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/contrib/dev/acpica/acpi.h
+++ sys/contrib/dev/acpica/acpi.h
@@ -123,7 +123,7 @@
  * in the rest of the source code again and again.
  */
 #include "acconfig.h"           /* Configuration constants */
-#include "platform/acenv.h"     /* Target environment specific items */
+#include "acenv.h"              /* Target environment specific items */
 #include "actypes.h"            /* Fundamental common data types */
 #include "acexcep.h"            /* ACPI exception codes */
 #include "acmacros.h"           /* C macros */
