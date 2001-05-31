72e99644587f2cc28f71523f4af47c220fbbee9c
jesper <jesper@FreeBSD.org>
2001-05-31 21:58:10 +0000

    Move the definition of NMBCLUSTERS from src/sys/kern/uipc_mbuf.c to <sys/param.h>, so it's available to src/sys/netinet/ip_input.c
    MFC after:	1 week

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -232,4 +232,9 @@
 #ifdef _KERNEL
 void	panic __P((const char *, ...)) __dead2 __printflike(1, 2);
 #endif
+
+#include "opt_param.h"
+#ifndef NMBCLUSTERS
+#define NMBCLUSTERS     (512 + MAXUSERS * 16)
+#endif
 #endif	/* _SYS_PARAM_H_ */
