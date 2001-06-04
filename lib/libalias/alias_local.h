d140bccb58d382e6099e758922beceb06b8178ab
brian <brian@FreeBSD.org>
2001-06-04 14:59:06 +0000

    Add a BSD style copyright
    Approved by: Charles Mott <cmott@scientech.com>

--- lib/libalias/alias_local.h
+++ lib/libalias/alias_local.h
@@ -1,4 +1,34 @@
-/* -*- mode: c; tab-width: 3; c-basic-offset: 3; -*-
+/* -*- mode: c; tab-width: 3; c-basic-offset: 3; -*- */
+
+/*-
+ * Copyright (c) 2001 Charles Mott <cmott@scientech.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+/*
     Alias_local.h contains the function prototypes for alias.c,
     alias_db.c, alias_util.c and alias_ftp.c, alias_irc.c (as well
     as any future add-ons).  It also includes macros, globals and
@@ -13,9 +43,8 @@
     Initial version:  August, 1996  (cjm)    
     
      <updated several times by original author and Eivind Eklund>
-
-    $FreeBSD$
 */
+
 #ifndef ALIAS_LOCAL_H
 #define ALIAS_LOCAL_H
 
