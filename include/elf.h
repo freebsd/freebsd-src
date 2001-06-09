fdd4e11d31c25fa70378d4d026e7cd468db3e896
David E. O'Brien <obrien@FreeBSD.org>
2001-06-09 05:22:31 +0000

    Update copyright.

--- include/elf.h
+++ include/elf.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 1997 John D. Polstra.
+ * Copyright (c) 2001 David E. O'Brien.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
