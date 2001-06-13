7a816b3371979e0d6409bf467e61800690bc8b7e
dillon <dillon@FreeBSD.org>
2001-06-13 07:26:58 +0000

    MFC the two out-of-swap fixes (kill the correct process and start blasting away at processes a little earlier, before the machine begins to lockup)

--- sys/vm/vm_pageout.c
+++ sys/vm/vm_pageout.c
@@ -1094,10 +1094,14 @@ rescan0:
 	}
 
 	/*
-	 * make sure that we have swap space -- if we are low on memory and
-	 * swap -- then kill the biggest process.
+	 * If we are out of swap and were not able to reach our paging
+	 * target, kill the largest process.
 	 */
+	if ((vm_swap_size < 64 && vm_page_count_min()) ||
+	    (swap_pager_full && vm_paging_target() > 0)) {
+#if 0
 	if ((vm_swap_size < 64 || swap_pager_full) && vm_page_count_min()) {
+#endif
 		bigproc = NULL;
 		bigsize = 0;
 		for (p = allproc.lh_first; p != 0; p = p->p_list.le_next) {
@@ -1119,7 +1123,8 @@ rescan0:
 			/*
 			 * get the process size
 			 */
-			size = vmspace_resident_count(p->p_vmspace);
+			size = vmspace_resident_count(p->p_vmspace) +
+				vmspace_swap_count(p->p_vmspace);
 			/*
 			 * if the this process is bigger than the biggest one
 			 * remember it.
