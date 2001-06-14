f925c7b74bf9e1e7680d6ca25299112f8ed39922
dd <dd@FreeBSD.org>
2001-06-14 23:23:21 +0000

    Don't hardcode in "/release/doc/"; instead, make it the default of a new RELN_ANCHOR variable.  It may be necessary to override this if the path to the checked out release notes tree doesn't end with "release/doc".

--- release/doc/share/mk/doc.relnotes.mk
+++ release/doc/share/mk/doc.relnotes.mk
@@ -9,5 +9,8 @@ EXTRA_CATALOGS+= ${RELN_ROOT}/share/sgml/catalog
 DSLHTML?=	${RELN_ROOT}/en_US.ISO8859-1/share/sgml/release.dsl
 DSLPRINT?=	${RELN_ROOT}/en_US.ISO8859-1/share/sgml/release.dsl
 
-# XXX using /release/doc as anchor!
-DESTDIR?= ${DOCDIR}/${.CURDIR:C/^.*\/release\/doc//}
+# XXX It may be possible to get this right just about all of the time
+# using ${RELN_ROOT}; "/release/doc" is just something that (I think)
+# covers the majority of the cases.
+RELN_ANCHOR?=	/release/doc/
+DESTDIR?= ${DOCDIR}/${.CURDIR:C/^.*${RELN_ANCHOR}/}
