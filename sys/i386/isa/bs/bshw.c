d3f395e0dd1176edcfb9b077f3e9589568480028
nyan <nyan@FreeBSD.org>
2001-06-14 11:09:11 +0000

    Correct typo.

--- sys/i386/isa/bs/bshw.c
+++ sys/i386/isa/bs/bshw.c
@@ -286,17 +286,17 @@ bshw_board_probe(bsc, drq, irq)
 		break;
 
 	case 1:
-		bsc->sc_cspeed &= ~(IDR_FS_12_15 | IDR_FS_15_20);
+		bsc->sc_cspeed &= ~(IDR_FS_12_15 | IDR_FS_16_20);
 		break;
 
 	case 2:
-		bsc->sc_cspeed &= ~(IDR_FS_12_15 | IDR_FS_15_20);
+		bsc->sc_cspeed &= ~(IDR_FS_12_15 | IDR_FS_16_20);
 		bsc->sc_cspeed |= IDR_FS_12_15;
 		break;
 
 	case 3:
-		bsc->sc_cspeed &= ~(IDR_FS_12_15 | IDR_FS_15_20);
-		bsc->sc_cspeed |= IDR_FS_15_20;
+		bsc->sc_cspeed &= ~(IDR_FS_12_15 | IDR_FS_16_20);
+		bsc->sc_cspeed |= IDR_FS_16_20;
 		break;
 	}
 
