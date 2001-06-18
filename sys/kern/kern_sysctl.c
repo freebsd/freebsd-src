a97750f68511ae12060fae214e98a3e37866a9b0
Dima Dorfman <dd@FreeBSD.org>
2001-06-18 23:48:13 +0000

    MFC 1.108: allow zero-length data (useful for strings).

--- sys/kern/kern_sysctl.c
+++ sys/kern/kern_sysctl.c
@@ -863,7 +863,7 @@ kernel_sysctl(struct proc *p, int *name, u_int namelen, void *old, size_t *oldle
 		req.oldptr= old;
 	}
 
-	if (newlen) {
+	if (new != NULL) {
 		req.newlen = newlen;
 		req.newptr = new;
 	}
@@ -1102,7 +1102,7 @@ userland_sysctl(struct proc *p, int *name, u_int namelen, void *old, size_t *old
 		req.oldptr= old;
 	}
 
-	if (newlen) {
+	if (new != NULL) {
 		if (!useracc(new, req.newlen, VM_PROT_READ))
 			return (EFAULT);
 		req.newlen = newlen;
