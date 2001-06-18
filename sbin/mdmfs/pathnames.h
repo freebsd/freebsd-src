2a395fdfb3953a05ad2c981452ebd271fe8e242a
dd <dd@FreeBSD.org>
2001-06-18 23:46:58 +0000

    Introduce mdmfs(8), a wrapper around mdconfig(8), disklabel(8), newfs(8), and mount(8) that mimics the command line option set of the deprecated mount_mfs(8).
    Approved by:	jkh, phk, -hackers

---
+++ sbin/mdmfs/pathnames.h
@@ -0,0 +1,10 @@
+/* $FreeBSD$ */
+#ifndef	MDMFS_PATHNAMES_H
+#define	MDMFS_PATHNAMES_H
+
+#define	PATH_MDCONFIG	"/sbin/mdconfig"
+#define	PATH_DISKLABEL	"/sbin/disklabel"
+#define	PATH_NEWFS	"/sbin/newfs"
+#define	PATH_MOUNT	"/sbin/mount"
+
+#endif	/* !MDMFS_PATHNAMES_H */
