dc801f8230a60549dccdfbbdf2a489fc1bdd24e9
des <des@FreeBSD.org>
2001-06-21 12:10:35 +0000

    More -fno-builtin failures.

--- games/adventure/subr.c
+++ games/adventure/subr.c
@@ -47,6 +47,7 @@ static const char rcsid[] =
 /*      Re-coding of advent in C: subroutines from main                 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "hdr.h"
 
