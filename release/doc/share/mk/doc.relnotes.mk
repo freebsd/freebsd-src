7450e0624939102d98422a12cc6507ac0fb76565
Dima Dorfman <dd@FreeBSD.org>
2001-06-21 20:43:43 +0000

    Change the way the value of DESTDIR is determined for the release notes build. Instead of having doc.relnotes.mk make a guess, hardcode quite a bit (but as little as possible) in Makefile.inc's sprinkled strategicly throughout the tree.  This has the advantage of actually working properly (which is a Good Thing(tm)), and the disadvantages of more files in the repository and more hardcoded paths (which are both Bad Things(tm)).

--- release/doc/share/mk/doc.relnotes.mk
+++ release/doc/share/mk/doc.relnotes.mk
@@ -8,9 +8,3 @@ EXTRA_CATALOGS+= ${RELN_ROOT}/share/sgml/catalog
 # Use the appropriate architecture-dependent RELNOTESng stylesheet
 DSLHTML?=	${RELN_ROOT}/en_US.ISO8859-1/share/sgml/release.dsl
 DSLPRINT?=	${RELN_ROOT}/en_US.ISO8859-1/share/sgml/release.dsl
-
-# XXX It may be possible to get this right just about all of the time
-# using ${RELN_ROOT}; "/release/doc" is just something that (I think)
-# covers the majority of the cases.
-RELN_ANCHOR?=	/release/doc/
-DESTDIR?= ${DOCDIR}/${.CURDIR:C/^.*${RELN_ANCHOR}/}
