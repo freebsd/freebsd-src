bceac71cc33ff35459665d223984eb82cba43cee
Bruce A. Mah <bmah@FreeBSD.org>
2001-06-22 00:35:11 +0000

    MFC:  ISO_ -> ISO rename.

--- release/doc/share/mk/doc.relnotes.mk
+++ release/doc/share/mk/doc.relnotes.mk
@@ -6,8 +6,8 @@ DOC_PREFIX?= ${RELN_ROOT}/../../../doc
 EXTRA_CATALOGS+= ${RELN_ROOT}/share/sgml/catalog
 
 # Use the appropriate architecture-dependent RELNOTESng stylesheet
-DSLHTML?=	${RELN_ROOT}/en_US.ISO_8859-1/share/sgml/release.dsl
-DSLPRINT?=	${RELN_ROOT}/en_US.ISO_8859-1/share/sgml/release.dsl
+DSLHTML?=	${RELN_ROOT}/en_US.ISO8859-1/share/sgml/release.dsl
+DSLPRINT?=	${RELN_ROOT}/en_US.ISO8859-1/share/sgml/release.dsl
 
 # XXX using /release/doc as anchor!
 DESTDIR?= ${DOCDIR}/${.CURDIR:C/^.*\/release\/doc//}
