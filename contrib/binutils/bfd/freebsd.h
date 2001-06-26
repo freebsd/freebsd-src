ca9b927b4383592b995dc5f4aece95d326a7e76e
obrien <obrien@FreeBSD.org>
2001-06-26 17:44:22 +0000

    Merge rev 1.2 (configuration changes to support FreeBSD) into Binutils 2.11.2.

--- contrib/binutils/bfd/freebsd.h
+++ contrib/binutils/bfd/freebsd.h
@@ -1,5 +1,6 @@
 /* BFD back-end definitions used by all FreeBSD targets.
-   Copyright (C) 1990, 1991, 1992, 1996, 2000 Free Software Foundation, Inc.
+   Copyright 1990, 1991, 1992, 1996, 1997, 2000, 2001
+   Free Software Foundation, Inc.
 
 This file is part of BFD, the Binary File Descriptor library.
 
