00c48bc9cf58aa38c4a255e9bdbe431fe7238222
jlemon <jlemon@FreeBSD.org>
2001-06-28 04:39:49 +0000

    Correct comment: so_q -> so_comp, so_q0 -> so_incomp.
    Submitted by: Adagio Vangogh <adagio_v@pacbell.net>

--- sys/sys/socketvar.h
+++ sys/sys/socketvar.h
@@ -62,10 +62,10 @@ struct socket {
  * Variables for connection queuing.
  * Socket where accepts occur is so_head in all subsidiary sockets.
  * If so_head is 0, socket is not related to an accept.
- * For head socket so_q0 queues partially completed connections,
- * while so_q is a queue of connections ready to be accepted.
+ * For head socket so_incomp queues partially completed connections,
+ * while so_comp is a queue of connections ready to be accepted.
  * If a connection is aborted and it has so_head set, then
- * it has to be pulled out of either so_q0 or so_q.
+ * it has to be pulled out of either so_incomp or so_comp.
  * We allow connections to queue up based on current queue lengths
  * and limit on number of queued connections for this socket.
  */
