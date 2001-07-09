9f13c453b84bc93f0b903f592de53f569b31b5f7
bmah <bmah@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-07-09 16:01:38 +0000

    Better support for multi-lingual release documentation.  We now support language-dependant SGML catalogs (in ${LANG_CODE}/share/sgml) and also use a default.dsl stylesheet similar to what the rest of the DocProj documents use.
    Requested by:	hrs, Alex Kapranoff <kapr@acm.org>
    Reviewed by:	hrs, dd
    MFC after:	2 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@79462 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/doc/share/mk/doc.relnotes.mk
+++ release/doc/share/mk/doc.relnotes.mk
@@ -2,9 +2,10 @@
 
 DOC_PREFIX?= ${RELN_ROOT}/../../../doc
 
-# Find the RELNOTESng document catalog
+# Find the RELNOTESng document catalogs
 EXTRA_CATALOGS+= ${RELN_ROOT}/share/sgml/catalog
+EXTRA_CATALOGS+= ${RELN_ROOT}/${LANGCODE}/share/sgml/catalog
 
 # Use the appropriate architecture-dependent RELNOTESng stylesheet
-DSLHTML?=	${RELN_ROOT}/en_US.ISO8859-1/share/sgml/release.dsl
-DSLPRINT?=	${RELN_ROOT}/en_US.ISO8859-1/share/sgml/release.dsl
+DSLHTML?=	${RELN_ROOT}/share/sgml/default.dsl
+DSLPRINT?=	${RELN_ROOT}/share/sgml/default.dsl
