1b8cb1cd38833815186cfb8cb20c2f5628992d82
markm <markm@FreeBSD.org>
2001-07-09 18:14:43 +0000

    Almost completely rewrite the PAM module options processing routines, and provide a more extended API for doing this.
    Provide an API for debug logging.
    
    Audit and clean up the code.

--- lib/libpam/libpam/pam_std_option.c
+++ lib/libpam/libpam/pam_std_option.c
@@ -27,40 +27,148 @@
  */
 
 #include <security/pam_modules.h>
+#include <stdio.h>
 #include <string.h>
+#include <syslog.h>
 #include "pam_mod_misc.h"
 
-/*
- * If the given name is a standard option, set the corresponding flag in
- * the options word and return 0.  Else return -1.
+/* Everyone has to have these options. It is not an error to
+ * specify them and then not use them.
  */
-int
-pam_std_option(int *options, const char *name)
-{
-	struct opttab {
-		const char *name;
-		int value;
-	};
-	static struct opttab std_options[] = {
-		{ "debug",		PAM_OPT_DEBUG },
-		{ "no_warn",		PAM_OPT_NO_WARN },
-		{ "use_first_pass",	PAM_OPT_USE_FIRST_PASS },
-		{ "try_first_pass",	PAM_OPT_TRY_FIRST_PASS },
-		{ "use_mapped_pass",	PAM_OPT_USE_MAPPED_PASS },
-		{ "echo_pass",		PAM_OPT_ECHO_PASS },
-		{ "auth_as_self",	PAM_OPT_AUTH_AS_SELF },
-		{ "nullok",		PAM_OPT_NULLOK },
-		{ "no_anon",		PAM_OPT_NO_ANON },
-		{ "ignore",		PAM_OPT_IGNORE },
-		{ NULL,			0 }
-	};
-	struct opttab *p;
+static struct opttab std_options[PAM_MAX_OPTIONS] = {
+	{ "debug",		PAM_OPT_DEBUG },
+	{ "no_warn",		PAM_OPT_NO_WARN },
+	{ "echo_pass",		PAM_OPT_ECHO_PASS },
+	{ "use_first_pass",	PAM_OPT_USE_FIRST_PASS },
+	{ "try_first_pass",	PAM_OPT_TRY_FIRST_PASS },
+	{ "use_mapped_pass",	PAM_OPT_USE_MAPPED_PASS },
+	{ "expose_account",	PAM_OPT_EXPOSE_ACCOUNT },
+	{ NULL,			0 }
+};
 
-	for (p = std_options;  p->name != NULL;  p++) {
-		if (strcmp(name, p->name) == 0) {
-			*options |= p->value;
-			return 0;
+/* Populate the options structure, syslogging all errors */
+void
+pam_std_option(struct options *options, struct opttab other_options[],
+    int argc, const char **argv)
+{
+	struct opttab *oo;
+	int i, j, arglen, found;
+
+	j = 0;
+	for (i = 0; i < PAM_MAX_OPTIONS; i++) {
+		options->opt[i].bool = 0;
+		options->opt[i].arg = NULL;
+		if (j == 0 && std_options[i].name == NULL)
+			j = i;
+		/* XXX Bad juju happens if loop exits with j == 0 */
+	}
+	if (other_options)
+		for (oo = other_options; oo->name != NULL; oo++) {
+			if (oo == NULL || oo->name == NULL)
+				break;
+			std_options[j].name = oo->name;
+			std_options[j].value = oo->value;
+			j++;
+		}
+	if (j < PAM_MAX_OPTIONS) {
+		std_options[j].name = NULL;
+		std_options[j].value = 0;
+	}
+	for (j = 0; j < argc; j++) {
+#ifdef DEBUG
+		syslog(LOG_DEBUG, "Doing arg %s ", argv[j]);
+#endif
+		found = 0;
+		for (i = 0; i < PAM_MAX_OPTIONS; i++) {
+			if (std_options[i].name == NULL)
+				break;
+			if (strcmp(argv[j], std_options[i].name) == 0) {
+				options->opt[std_options[i].value].bool = 1;
+				found = 1;
+				break;
+			}
+			else {
+				arglen = strlen(std_options[i].name);
+				if (strncmp(argv[j], std_options[i].name,
+					    arglen) == 0
+				    && argv[j][arglen] == '=')  {
+					options->opt[std_options[i].value].bool
+					    = 1;
+					options->opt[std_options[i].value].arg =
+					    strdup(&argv[j][arglen + 1]);
+					found = 1;
+					break;
+				}
+			}
+		}
+		if (!found)
+			syslog(LOG_WARNING, "PAM option: %s invalid", argv[j]);
+	}
+}
+
+/* Test if option is set in options */
+int
+pam_test_option(struct options *options, enum opt option, char **arg)
+{
+	if (arg != NULL)
+		*arg = options->opt[option].arg;
+	return options->opt[option].bool;
+}
+
+/* Set option in options, errors to syslog */
+void
+pam_set_option(struct options *options, enum opt option)
+{
+	if (option < PAM_OPT_STD_MAX)
+		options->opt[option].bool = 1;
+#ifdef DEBUG
+	else
+		syslog(LOG_DEBUG, "PAM options: attempt to set option %d",
+		    option);
+#endif
+}
+
+/* Clear option in options, errors to syslog */
+void
+pam_clear_option(struct options *options, enum opt option)
+{
+	if (option < PAM_OPT_STD_MAX)
+		options->opt[option].bool = 0;
+#ifdef DEBUG
+	else
+		syslog(LOG_DEBUG, "PAM options: attempt to clear option %d",
+		    option);
+#endif
+}
+
+#if DEBUG1
+enum { PAM_OPT_FOO=PAM_OPT_STD_MAX, PAM_OPT_BAR, PAM_OPT_BAZ, PAM_OPT_QUX };
+
+struct opttab other_options[] = {
+	{ "foo", PAM_OPT_FOO },
+	{ "bar", PAM_OPT_BAR },
+	{ "baz", PAM_OPT_BAZ },
+	{ "qux", PAM_OPT_QUX },
+	{ NULL, 0 }
+};
+
+int
+main(int argc, const char *argv[])
+{
+	struct options options;
+	int i, opt;
+	char *arg;
+
+	pam_std_option(&options, other_options, argc, argv);
+	for (i = 0; i < PAM_MAX_OPTIONS; i++) {
+		opt = pam_test_option(&options, i, &arg);
+		if (opt) {
+			if (arg == NULL)
+				printf("%d []\n", i);
+			else
+				printf("%d [%s]\n", i, arg);
 		}
 	}
-	return -1;
+	return 0;
 }
+#endif
