e20f313e2c2a20a95adff77391cfc07afc989980
Dima Dorfman <dd@FreeBSD.org>
2001-07-12 00:21:09 +0000

    MFC: hardcode DESTDIR instead of trying to guess it and doing a bad job.

--- release/doc/share/mk/doc.relnotes.mk
+++ release/doc/share/mk/doc.relnotes.mk
@@ -8,6 +8,3 @@ EXTRA_CATALOGS+= ${RELN_ROOT}/share/sgml/catalog
 # Use the appropriate architecture-dependent RELNOTESng stylesheet
 DSLHTML?=	${RELN_ROOT}/en_US.ISO8859-1/share/sgml/release.dsl
 DSLPRINT?=	${RELN_ROOT}/en_US.ISO8859-1/share/sgml/release.dsl
-
-# XXX using /release/doc as anchor!
-DESTDIR?= ${DOCDIR}/${.CURDIR:C/^.*\/release\/doc//}
