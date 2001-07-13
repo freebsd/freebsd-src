81e561cdf2290b24739d6bf033832f0bd9489480
David E. O'Brien <obrien@FreeBSD.org>
2001-07-13 18:38:04 +0000

    Bump net.inet.tcp.sendspace to 32k and net.inet.tcp.recvspace to 65k. This should help us in nieve benchmark "tests".
    It seems a wide number of people think 32k buffers would not cause major
    issues, and is in fact in use by many other OS's at this time.  The
    receive buffers can be bumped higher as buffers are hardly used and several
    research papers indicate that receive buffers rarely use much space at all.
    
    Submitted by:			Leo Bicknell <bicknell@ufp.org>
    				<20010713101107.B9559@ussenterprise.ufp.org>
    Agreed to in principle by:	dillon (at the 32k level)

--- sys/netinet/tcp_usrreq.c
+++ sys/netinet/tcp_usrreq.c
@@ -995,10 +995,10 @@ tcp_ctloutput(so, sopt)
  * sizes, respectively.  These are obsolescent (this information should
  * be set by the route).
  */
-u_long	tcp_sendspace = 1024*16;
+u_long	tcp_sendspace = 1024*32;
 SYSCTL_INT(_net_inet_tcp, TCPCTL_SENDSPACE, sendspace, CTLFLAG_RW, 
     &tcp_sendspace , 0, "Maximum outgoing TCP datagram size");
-u_long	tcp_recvspace = 1024*16;
+u_long	tcp_recvspace = 1024*64;
 SYSCTL_INT(_net_inet_tcp, TCPCTL_RECVSPACE, recvspace, CTLFLAG_RW, 
     &tcp_recvspace , 0, "Maximum incoming TCP datagram size");
 
