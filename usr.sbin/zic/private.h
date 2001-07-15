12905cca18a426ad61e22cdda820116f6b9ecfcf
dd <dd@FreeBSD.org>
2001-07-15 06:20:45 +0000

    Constify, add some prototypes, and set WARNS=2.
    Submitted by:	Mike Barcroft <mike@q9media.com>

--- usr.sbin/zic/private.h
+++ usr.sbin/zic/private.h
@@ -99,7 +99,7 @@ char *	imalloc P((int n));
 void *	irealloc P((void * pointer, int size));
 void	icfree P((char * pointer));
 void	ifree P((char * pointer));
-char *	scheck P((const char *string, const char *format));
+const char * scheck P((const char *string, const char *format));
 
 /*
 ** Finally, some convenience items.
