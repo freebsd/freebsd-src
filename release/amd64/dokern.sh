8f0380ea5a543dc21427c50701f73504ec8f2a6c
obrien <obrien@FreeBSD.org>
2001-07-21 17:32:40 +0000

    Unconditionalize these now that they live in ARCH-specific directories.

--- release/amd64/dokern.sh
+++ release/amd64/dokern.sh
@@ -9,11 +9,6 @@ else
 	FDSIZE=$1
 fi
 
-ARCH="`uname -m`"
-
-# First check for the standard x86 PC class.
-if [ "${ARCH}" = "i386" ]; then
-
 if [ "${FDSIZE}" = "SMALL" ]; then
 
 sed	-e '/	pci$/d' \
@@ -112,62 +107,6 @@ echo "options  NETGRAPH"
 echo "options  NETGRAPH_ETHER"
 echo "options  NETGRAPH_PPPOE"
 echo "options  NETGRAPH_SOCKET"
-
-# Otherwise maybe it's an alpha, and it has big binaries.
-elif [ "${ARCH}" = "alpha" ]; then
-
-sed	\
-	-e '/DEC_3000_300/d' \
-	-e '/DEC_3000_500/d' \
-	-e '/SOFTUPDATES/d' \
-	-e '/MFS/d' \
-	-e '/NFS_ROOT/d' \
-	-e '/MSDOSFS/d' \
-	-e '/PROCFS/d' \
-	-e '/KTRACE/d' \
-	-e '/SYSV/d' \
-	-e '/P1003_1B/d' \
-	-e '/_KPOSIX_PRIORITY_SCHEDULING/d' \
- 	-e '/DEBUG/d' \
- 	-e '/DDB/d' \
- 	-e '/INVARIANTS/d' \
- 	-e '/INVARIANT_SUPPORT/d' \
- 	-e '/WITNESS/d' \
-	-e '/atapist/d' \
-	-e '/	ncr/d' \
-	-e '/pass/d' \
-	-e '/splash/d' \
-	-e '/	apm/d' \
-	-e '/pmtimer/d' \
-	-e '/pcm/d' \
-	-e '/lpt/d' \
-	-e '/ppi/d' \
-	-e '/vpo/d' \
-	-e '/	le	/d' \
-	-e '/random/d' \
-	-e '/gzip/d' \
-	-e '/	sl	/d' \
-	-e '/ppp/d' \
-	-e '/pty/d' \
-	-e '/gif/d' \
-	-e '/faith/d' \
-	-e '/uhci/d' \
-	-e '/ohci/d' \
-	-e '/usb/d' \
-	-e '/ugen/d' \
-	-e '/uhid/d' \
-	-e '/ukbd/d' \
-	-e '/ulpt/d' \
-	-e '/umass/d' \
-	-e '/ums/d' \
-	-e '/urio/d' \
-	-e '/uscanner/d' \
-	-e '/aue/d' \
-	-e '/cue/d' \
-	-e '/kue/d' \
-	-e '/maxusers/d' \
-	-e 's/ident.*GENERIC/ident		BOOTMFS/g'
-fi
 
 # reset maxusers to something lower
 echo "maxusers	5"
