6deb915d9d9c02fdf394873a2712aa2f7647225b
assar <assar@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-07-23 12:34:31 +0000

    make vm_page_select_cache static
    Requested by:	bde
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@80204 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/vm/vm_page.c
+++ sys/vm/vm_page.c
@@ -776,7 +776,7 @@ vm_page_rename(vm_page_t m, vm_object_t new_object, vm_pindex_t new_pindex)
  *	This routine must be called at splvm().
  *	This routine may not block.
  */
-vm_page_t
+static vm_page_t
 vm_page_select_cache(vm_object_t object, vm_pindex_t pindex)
 {
 	vm_page_t m;
