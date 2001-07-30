58e5e9396f3bce4ca61b91185fca0ecc34db8766
peter <peter@FreeBSD.org>
2001-07-30 22:49:47 +0000

    MFC: 1.110-1.112: fix style/warning in tcp_new_isn()

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -1099,21 +1099,19 @@ tcp6_ctlinput(cmd, sa, d)
 tcp_seq
 tcp_new_isn()
 {
-	if ((tcp_seq_genscheme > 1) || (tcp_seq_genscheme < 0))
+
+	if (tcp_seq_genscheme > 1 || tcp_seq_genscheme < 0)
 		tcp_seq_genscheme = 1;
 
 	switch (tcp_seq_genscheme) {
-		case 0:	/*
-			 * Random positive increments
-			 */
-			tcp_iss += TCP_ISSINCR/2;
-			return tcp_iss;
-		case 1:	/*
-			 * OpenBSD randomized scheme
-			 */
-			return tcp_rndiss_next();
+	case 0:			/* Random positive increments */
+		tcp_iss += TCP_ISSINCR/2;
+		return tcp_iss;
+	case 1:			/* OpenBSD randomized scheme */
+		return tcp_rndiss_next();
+	default:
+		panic("cannot happen");
 	}
-
 }
 
 #define TCP_RNDISS_ROUNDS	16
