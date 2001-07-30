187f447ec49ccef3d9080ce1bc953e36db13aed5
dd <dd@FreeBSD.org>
2001-07-30 09:52:38 +0000

    MFC 1.47: call kvm_close() on exit for consistency.

--- usr.bin/w/w.c
+++ usr.bin/w/w.c
@@ -260,8 +260,10 @@ main(argc, argv)
 
 	if (header || wcmd == 0) {
 		pr_header(&now, nusers);
-		if (wcmd == 0)
+		if (wcmd == 0) {
+			(void)kvm_close(kd);
 			exit(0);
+		}
 
 #define HEADER_USER		"USER"
 #define HEADER_TTY		"TTY"
@@ -415,6 +417,7 @@ main(argc, argv)
 		longidle = pr_idle(ep->idle);
 		(void)printf("%.*s\n", argwidth - longidle, ep->args);
 	}
+	(void)kvm_close(kd);
 	exit(0);
 }
 
