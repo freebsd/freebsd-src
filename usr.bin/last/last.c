545bc96354fdb4bdce93238249681b1330c871b1
dd <dd@FreeBSD.org>
2001-07-30 09:51:07 +0000

    MFC 1.18: don't choke on two successive "login" marks.

--- usr.bin/last/last.c
+++ usr.bin/last/last.c
@@ -346,13 +346,10 @@ wtmp()
 							    86400, ct);
 					}
 				}
-				LIST_REMOVE(tt, list);
-				free(tt);
 				if (maxrec != -1 && !--maxrec)
 					return;
-			} else {
-				tt->logout = bp->ut_time;
 			}
+			tt->logout = bp->ut_time;
 		}
 	}
 	tm = localtime(&buf[0].ut_time);
