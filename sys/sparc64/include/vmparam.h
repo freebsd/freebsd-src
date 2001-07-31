66b3f22c825c41bcc67d75acaf6a8d373c30fa68
jake <jake@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-07-31 05:45:16 +0000

    Add skeleton machine dependent headers and c files for a port of freebsd to a new architecture.  This is the base of the sparc64 port, but contains limited machine dependent code, and can be used a base for ports.  Included are: - standard machine dependent headers, tweaked for a 64 bit, big endian   architecture, including empty versions of all the machine dependent   structures - a machine independent atomic.h, which can be used until a port has   support for interrupts and the operations really need to be atomic - stub versions of all the machine dependent functions, which panic   when called and print out the name of the function that needs to   be implemented.  functions which are normally in assembly files are   not included, but this should reduce the number of different undefined   references on the first few compiles from hundreds to 5 or 6 Given minimal startup code and console support it should be trivial to make this compile and run the first few sysinits on almost any architecture.
    Requested by:   alfred, imp, jhb
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@80708 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ sys/sparc64/include/vmparam.h
@@ -0,0 +1,90 @@
+/*-
+ * Copyright (c) 2001 Jake Burkholder.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef	_MACHINE_VMPARAM_H_
+#define	_MACHINE_VMPARAM_H_
+
+/*
+ * Virtual memory related constants, all in bytes
+ */
+#ifndef MAXTSIZ
+#define	MAXTSIZ		(1*1024*1024*1024)	/* max text size */
+#endif
+#ifndef DFLDSIZ
+#define	DFLDSIZ		(128*1024*1024)		/* initial data size limit */
+#endif
+#ifndef MAXDSIZ
+#define	MAXDSIZ		(1*1024*1024*1024)	/* max data size */
+#endif
+#ifndef	DFLSSIZ
+#define	DFLSSIZ		(128*1024*1024)		/* initial stack size limit */
+#endif
+#ifndef	MAXSSIZ
+#define	MAXSSIZ		(1*1024*1024*1024)	/* max stack size */
+#endif
+#ifndef	SGROWSIZ
+#define	SGROWSIZ	(128*1024)		/* amount to grow stack */
+#endif
+
+/*
+ * The time for a process to be blocked before being very swappable.
+ * This is a number of seconds which the system takes as being a non-trivial
+ * amount of real time.  You probably shouldn't change this;
+ * it is used in subtle ways (fractions and multiples of it are, that is, like
+ * half of a ``long time'', almost a long time, etc.)
+ * It is related to human patience and other factors which don't really
+ * change over time.
+ */
+#define	MAXSLP			20
+
+#define	VM_MAXUSER_ADDRESS	0
+
+#define	USRSTACK		VM_MAXUSER_ADDRESS
+
+#define	VM_MIN_ADDRESS		0
+
+/*
+ * Virtual size (bytes) for various kernel submaps.
+ */
+#ifndef	VM_KMEM_SIZE
+#define	VM_KMEM_SIZE	(12*1024*1024)
+#endif
+
+#define	VM_MIN_KERNEL_ADDRESS	(0)
+#define	VM_MAX_KERNEL_ADDRESS	(0)
+
+#define	KERNBASE		(0)
+
+/*
+ * Initial pagein size of beginning of executable file.
+ */
+#ifndef	VM_INITIAL_PAGEIN
+#define	VM_INITIAL_PAGEIN	16
+#endif
+
+#endif /* !_MACHINE_VMPARAM_H_ */
