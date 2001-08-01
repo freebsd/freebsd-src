846fcfeba99ccb6bf09965f4eb97315ea640d829
David E. O'Brien <obrien@FreeBSD.org>
2001-08-01 17:04:17 +0000

    MFC: sync additions from -current

--- share/mk/bsd.libnames.mk
+++ share/mk/bsd.libnames.mk
@@ -13,11 +13,11 @@ LIBALIAS?=	${DESTDIR}${LIBDIR}/libalias.a
 LIBATM?=	${DESTDIR}${LIBDIR}/libatm.a
 LIBBZ2?=	${DESTDIR}${LIBDIR}/libbz2.a
 LIBC?=		${DESTDIR}${LIBDIR}/libc.a
-LIBC_PIC=	${DESTDIR}${LIBDIR}/libc_pic.a
+LIBC_PIC?=	${DESTDIR}${LIBDIR}/libc_pic.a
 LIBCALENDAR?=	${DESTDIR}${LIBDIR}/libcalendar.a
 LIBCAM?=	${DESTDIR}${LIBDIR}/libcam.a
 LIBCIPHER?=	${DESTDIR}${LIBDIR}/libcipher.a	# XXX in secure dist, not base
-LIBCOM_ERR=	${DESTDIR}${LIBDIR}/libcom_err.a
+LIBCOM_ERR?=	${DESTDIR}${LIBDIR}/libcom_err.a
 LIBCOMPAT?=	${DESTDIR}${LIBDIR}/libcompat.a
 LIBCRYPT?=	${DESTDIR}${LIBDIR}/libcrypt.a
 LIBCRYPTO?=	${DESTDIR}${LIBDIR}/libcrypto.a	# XXX in secure dist, not base
@@ -28,10 +28,13 @@ LIBDIALOG?=	${DESTDIR}${LIBDIR}/libdialog.a
 LIBDISK?=	${DESTDIR}${LIBDIR}/libdisk.a
 LIBEDIT?=	${DESTDIR}${LIBDIR}/libedit.a
 LIBF2C?=	${DESTDIR}${LIBDIR}/libf2c.a
+LIBFETCH?=	${DESTDIR}${LIBDIR}/libfetch.a
 LIBFL?=		"don't use LIBFL, use LIBL"
+LIBFORM?=	${DESTDIR}${LIBDIR}/libform.a
 LIBFORMS?=	${DESTDIR}${LIBDIR}/libforms.a
 LIBFTPIO?=	${DESTDIR}${LIBDIR}/libftpio.a
 LIBGPLUSPLUS?=	${DESTDIR}${LIBDIR}/libg++.a
+LIBG2C?=	${DESTDIR}${LIBDIR}/libg2c.a
 LIBGCC?=	${DESTDIR}${LIBDIR}/libgcc.a
 LIBGCC_PIC?=	${DESTDIR}${LIBDIR}/libgcc_pic.a
 LIBGMP?=	${DESTDIR}${LIBDIR}/libgmp.a
@@ -42,12 +45,14 @@ LIBIPX?=	${DESTDIR}${LIBDIR}/libipx.a
 LIBISC?=	${DESTDIR}${LIBDIR}/libisc.a
 LIBKDB?=	${DESTDIR}${LIBDIR}/libkdb.a	# XXX in secure dist, not base
 LIBKRB?=	${DESTDIR}${LIBDIR}/libkrb.a	# XXX in secure dist, not base
+LIBKRB5?=	${DESTDIR}${LIBDIR}/libkrb5.a	# XXX in secure dist, not base
 LIBKEYCAP?=	${DESTDIR}${LIBDIR}/libkeycap.a
 LIBKVM?=	${DESTDIR}${LIBDIR}/libkvm.a
 LIBL?=		${DESTDIR}${LIBDIR}/libl.a
-LIBLN?=		"don't use, LIBLN, use LIBL"
+LIBLN?=		"don't use LIBLN, use LIBL"
 LIBM?=		${DESTDIR}${LIBDIR}/libm.a
 LIBMD?=		${DESTDIR}${LIBDIR}/libmd.a
+LIBMENU?=	${DESTDIR}${LIBDIR}/libmenu.a
 LIBMP?=		${DESTDIR}${LIBDIR}/libmp.a
 LIBMYTINFO?=	${DESTDIR}${LIBDIR}/libmytinfo.a
 LIBNCP?=	${DESTDIR}${LIBDIR}/libncp.a
@@ -58,7 +63,7 @@ LIBOPIE?=	${DESTDIR}${LIBDIR}/libopie.a
 
 # The static PAM library doesn't know its secondary dependencies,
 # so we have to specify them explictly.
-LIBPAM?=	${DESTDIR}${LIBDIR}/libpam.a	# XXX doesn't exist
+LIBPAM?=	${DESTDIR}${LIBDIR}/libpam.a
 MINUSLPAM?=	-lpam
 .if defined(NOSHARED) && ${NOSHARED} != "no" && ${NOSHARED} != "NO"
 .ifdef MAKE_KERBEROS4
@@ -70,6 +75,7 @@ LIBPAM+=	${LIBRADIUS} ${LIBTACPLUS} ${LIBSKEY} ${LIBCRYPT} ${LIBMD} \
 MINUSLPAM+=	-lradius -ltacplus -lskey -lcrypt -lmd -lutil
 .endif
 
+LIBPANEL?=	${DESTDIR}${LIBDIR}/libpanel.a
 LIBPC?=		${DESTDIR}${LIBDIR}/libpc.a	# XXX doesn't exist
 LIBPCAP?=	${DESTDIR}${LIBDIR}/libpcap.a
 LIBPERL?=	${DESTDIR}${LIBDIR}/libperl.a
