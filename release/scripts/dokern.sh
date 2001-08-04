b1d376e2ff49452942ddec6bf38ebac87d26aa9d
David E. O'Brien <obrien@FreeBSD.org>
2001-08-04 22:20:52 +0000

    Cut more from the Alpha kernel in order to build the install floppies: 	DEC_3000_300, DEC_3000_500, ncr, sa, amr, plip, le, pcn, wx, sl.

--- release/scripts/dokern.sh
+++ release/scripts/dokern.sh
@@ -100,6 +100,8 @@ echo "options  NETGRAPH_SOCKET"
 elif [ "${ARCH}" = "alpha" ]; then
 
 sed	\
+	-e '/DEC_3000_300/d' \
+	-e '/DEC_3000_500/d' \
 	-e '/SOFTUPDATES/d' \
 	-e '/MFS/d' \
 	-e '/NFS_ROOT/d' \
@@ -107,15 +109,27 @@ sed	\
 	-e '/PROCFS/d' \
 	-e '/KTRACE/d' \
 	-e '/SYSV/d' \
+	-e '/P1003_1B/d' \
+	-e '/_KPOSIX_PRIORITY_SCHEDULING/d' \
+	-e '/ICMP_BANDLIM/d' \
 	-e '/atapist/d' \
+	-e '/	ncr/d' \
+	-e '/	sa/d' \
 	-e '/pass/d' \
 	-e '/splash/d' \
+	-e '/	amr/d' \
 	-e '/	apm0/d' \
+	-e '/ppc/d' \
+	-e '/ppbus/d' \
+	-e '/plip/d' \
 	-e '/lpt/d' \
 	-e '/ppi/d' \
 	-e '/vpo/d' \
-	-e '/RANDOMDEV/d' \
+	-e '/	le	/d' \
+	-e '/	pcn	/d' \
+	-e '/	wx	/d' \
 	-e '/gzip/d' \
+	-e '/	sl	/d' \
 	-e '/ppp/d' \
 	-e '/pty/d' \
 	-e '/gif/d' \
@@ -129,6 +143,7 @@ sed	\
 	-e '/ulpt/d' \
 	-e '/umass/d' \
 	-e '/ums/d' \
+	-e '/urio/d' \
 	-e '/uscanner/d' \
 	-e '/aue/d' \
 	-e '/cue/d' \
