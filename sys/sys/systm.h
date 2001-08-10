591f102bf270110396b100bb2dc7ad8bb7869026
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-08-10 06:37:05 +0000

    - Remove asleep(), await(), and M_ASLEEP. - Callers of asleep() and await() have been converted to calling tsleep().   The only caller outside of M_ASLEEP was the ata driver, which called both   asleep() and await() with spl-raised, so there was no need for the   asleep() and await() pair.  M_ASLEEP was unused.
    Reviewed by:	jasone, peter
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@81397 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -262,8 +262,6 @@ extern watchdog_tickle_fn	wdog_tickler;
 int	msleep __P((void *chan, struct mtx *mtx, int pri, const char *wmesg,
 		    int timo));
 #define	tsleep(chan, pri, wmesg, timo)	msleep(chan, NULL, pri, wmesg, timo)
-int	asleep __P((void *chan, int pri, const char *wmesg, int timo));
-int	await  __P((int pri, int timo));
 void	wakeup __P((void *chan));
 void	wakeup_one __P((void *chan));
 
