9e51adb9850a3122ba600941a073155ddb5573b5
Gregory Neil Shapiro <gshapiro@FreeBSD.org>
2001-08-21 01:36:37 +0000

    MFC: Import sendmail 8.11.6
    Approved by:	re (jkh)

--- contrib/sendmail/src/version.c
+++ contrib/sendmail/src/version.c
@@ -12,7 +12,7 @@
  */
 
 #ifndef lint
-static char id[] = "@(#)$Id: version.c,v 8.43.4.37 2001/07/31 22:34:20 gshapiro Exp $";
+static char id[] = "@(#)$Id: version.c,v 8.43.4.39 2001/08/20 14:45:34 gshapiro Exp $";
 #endif /* ! lint */
 
-char	Version[] = "8.11.5";
+char	Version[] = "8.11.6";
