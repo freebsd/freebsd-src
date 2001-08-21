d2cb5f313755cebc367c502f7197af9d7b963446
Doug Rabson <dfr@FreeBSD.org>
2001-08-21 16:53:23 +0000

    Make sure stack is aligned to 16 bytes.

--- lib/csu/ia64/crt1.c
+++ lib/csu/ia64/crt1.c
@@ -65,10 +65,13 @@ __asm ("
 	.global _start
 	.proc _start
 _start:
-	alloc	r16=ar.pfs,0,0,3,0 ;;
+	alloc	r14=ar.pfs,0,0,3,0 ;;
 	mov	out0=sp
 	mov	out1=r14
 	mov	out2=r15
+	mov	r14=15 ;;
+	andcm	sp=sp,r14 ;;
+	add	sp=-16,sp
 	movl	gp=_GLOBAL_OFFSET_TABLE_
 	br.call.sptk rp=_start2
 	.endp	_start");
