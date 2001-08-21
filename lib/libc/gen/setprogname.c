1476d8bd3808d7525183b17b6059274cde8550c1
nectar <nectar@FreeBSD.org>
2001-08-21 17:16:32 +0000

    Pass the pointy hat, please.
    Submitted by:	ru

--- lib/libc/gen/setprogname.c
+++ lib/libc/gen/setprogname.c
@@ -3,6 +3,8 @@ static const char rcsid[] =
   "$FreeBSD$";
 #endif /* LIBC_RCS and not lint */
 
+#include <string.h>
+
 extern const char *__progname;
 
 void
@@ -10,6 +12,6 @@ setprogname(const char *progname)
 {
 	char *p;
 
-	p = strrchr('/', progname);
-	__progname = p ? p+1 : progname;
+	p = strrchr(progname, '/');
+	__progname = p ? p + 1 : progname;
 }
