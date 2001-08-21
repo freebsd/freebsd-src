99a7e304b07023acd1b8a6fc6b975f50364fb613
ru <ru@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-08-21 17:28:39 +0000

    Fixed warnings.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@82081 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/gen/setprogname.c
+++ lib/libc/gen/setprogname.c
@@ -3,6 +3,7 @@ static const char rcsid[] =
   "$FreeBSD$";
 #endif /* LIBC_RCS and not lint */
 
+#include <stdlib.h>
 #include <string.h>
 
 extern const char *__progname;
@@ -10,7 +11,7 @@ extern const char *__progname;
 void
 setprogname(const char *progname)
 {
-	char *p;
+	const char *p;
 
 	p = strrchr(progname, '/');
 	__progname = p ? p + 1 : progname;
