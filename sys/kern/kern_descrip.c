194c6c0cd525c12c082ce9b126719339050eb378
ache <ache@FreeBSD.org>
2001-08-23 07:42:40 +0000

    Detect off_t EOVERFLOW of start/end offsets calculations for adv. lock, as POSIX require.

--- sys/kern/kern_descrip.c
+++ sys/kern/kern_descrip.c
@@ -40,6 +40,7 @@
  */
 
 #include "opt_compat.h"
+#include <machine/limits.h>
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/lock.h>
@@ -303,8 +304,14 @@ fcntl(p, uap)
 			fdrop(fp, p);
 			return (error);
 		}
-		if (fl.l_whence == SEEK_CUR)
+		if (fl.l_whence == SEEK_CUR) {
+			if ((fl.l_start > 0 &&
+			     fp->f_offset > OFF_MAX - fl.l_start) ||
+			    (fl.l_start < 0 &&
+			     fp->f_offset < OFF_MIN - fl.l_start))
+				return (EOVERFLOW);
 			fl.l_start += fp->f_offset;
+		}
 
 		switch (fl.l_type) {
 		case F_RDLCK:
@@ -356,8 +363,14 @@ fcntl(p, uap)
 			fdrop(fp, p);
 			return (EINVAL);
 		}
-		if (fl.l_whence == SEEK_CUR)
+		if (fl.l_whence == SEEK_CUR) {
+			if ((fl.l_start > 0 &&
+			     fp->f_offset > OFF_MAX - fl.l_start) ||
+			    (fl.l_start < 0 &&
+			     fp->f_offset < OFF_MIN - fl.l_start))
+				return (EOVERFLOW);
 			fl.l_start += fp->f_offset;
+		}
 		error = VOP_ADVLOCK(vp, (caddr_t)p->p_leader, F_GETLK,
 			    &fl, F_POSIX);
 		fdrop(fp, p);
