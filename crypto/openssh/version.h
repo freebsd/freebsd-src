d018e85763f8bed4322bd9d216ccf6a4b79569c9
green <green@FreeBSD.org>
2001-08-23 22:42:41 +0000

    Update the version string here, too.

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -1,4 +1,4 @@
 /* $FreeBSD$ */
 /* $OpenBSD: version.h,v 1.13 2000/10/16 09:38:45 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_2.3.0 green@FreeBSD.org 20010321"
+#define SSH_VERSION	"OpenSSH_2.3.0 FreeBSD localisations 20010713"
