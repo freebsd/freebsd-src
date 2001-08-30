a0bf781b06c592c4d0aa605f42d7f6cb79b176e5
Murray Stokely <murray@FreeBSD.org>
2001-08-30 12:29:58 +0000

    Import the latest (1998-era!) unsupported 2.0.0 version of the stallion driver from ftp.stallion.com (stl(4) and stli(4)).  This adds support for the "EasyIO" family of PCI multiport serial cards with the stl driver.
    The default device names were changed to allow stl and stli
    devices to coexist on a system.
     - stl driver dialin devices are ttyE* as before
     - stli driver dialin devices no longer share the ttyE names, but now use
       ttyF* names.
     - dialout devices are now called cuaE* and cuaF* (cf cue*) in line with the
       rest of the system
    
    PR: 6368, 19890, 22967, 28831
    Submitted by: Gregory Bond <gnb@itga.com.au>,
                  Jan L. Peterson <jlp@flipdog.com>
    Approved by: jkh

--- usr.sbin/stallion/stlstats/stlstats.c
+++ usr.sbin/stallion/stlstats/stlstats.c
@@ -3,7 +3,7 @@
 /*
  * stlstats.c  -- stallion intelligent multiport stats display.
  *
- * Copyright (c) 1994-1996 Greg Ungerer (gerg@stallion.oz.au).
+ * Copyright (c) 1994-1998 Greg Ungerer (gerg@stallion.oz.au).
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -57,7 +57,7 @@ static const char rcsid[] =
 
 /*****************************************************************************/
 
-char	*version = "1.0.0";
+char	*version = "2.0.0";
 char	*defdevice = "/dev/staliomem0";
 
 char	*ctrldevice;
