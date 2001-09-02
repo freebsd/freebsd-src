5cd43074df1745b1ca99340aba5af9e2b2f505d7
obrien <obrien@FreeBSD.org>
2001-09-02 19:31:00 +0000

    We are returning to the stock (6.0.7) file now. + The bufov vulnerability was fixed in the vendor sources. + The vendor sources are now POSIX [me harder] compliant.

--- contrib/amd/wire-test/wire-test.c
+++ contrib/amd/wire-test/wire-test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997-1999 Erez Zadok
+ * Copyright (c) 1997-2001 Erez Zadok
  * Copyright (c) 1990 Jan-Simon Pendry
  * Copyright (c) 1990 Imperial College of Science, Technology & Medicine
  * Copyright (c) 1990 The Regents of the University of California.
@@ -38,7 +38,7 @@
  *
  *      %W% (Berkeley) %G%
  *
- * $Id: wire-test.c,v 1.4 1999/02/04 07:24:54 ezk Exp $
+ * $Id: wire-test.c,v 1.5.2.1 2001/01/10 03:23:43 ezk Exp $
  * $FreeBSD$
  *
  */
