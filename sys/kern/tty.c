0d2aa3285e83a59710df6ce8af629aa204d2b772
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-09-10 12:05:47 +0000

    Fix a warning on alpha (real problem) and make pstat -t work as a bonus. 'struct tty' was out of sync in user and kernel due to dev_t/udev_t mixups.  This takes advantage of the fact that dev_t changes type in userland, so it isn't too pretty.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@83300 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -2528,7 +2528,7 @@ sysctl_kern_ttys(SYSCTL_HANDLER_ARGS)
 	SLIST_FOREACH(tp, &tty_list, t_list) {
 		t = *tp;
 		if (t.t_dev)
-			t.t_dev = (dev_t)dev2udev(t.t_dev);
+			t.ttyu.t_udev = dev2udev(t.t_dev);
 		error = SYSCTL_OUT(req, (caddr_t)&t, sizeof(t));
 		if (error)
 			return (error);
