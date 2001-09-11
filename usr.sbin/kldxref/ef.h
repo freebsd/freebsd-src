9c6f92408c29a8980e7aec26a2590087c3b3d8ed
Peter Wemm <peter@FreeBSD.org>
2001-09-11 01:13:15 +0000

    Add kldxref(8), for maintaining the linker.hints file for translating module->pathname.ko.  It supports only ELF for now.
    Submitted by:   bp  (with some minor tweaks)

---
+++ usr.sbin/kldxref/ef.h
@@ -0,0 +1,43 @@
+/* $FreeBSD$ */
+
+#ifndef _EF_H_
+#define _EF_H_
+
+#define	EFT_KLD		1
+#define	EFT_KERNEL	2
+
+typedef struct elf_file {
+	char*		ef_name;
+	Elf_Phdr *	ef_ph;
+	int		ef_fd;
+	int		ef_type;
+	Elf_Ehdr	ef_hdr;
+	void*		ef_fpage;		/* First block of the file */
+	int		ef_fplen;		/* length of first block */
+	Elf_Dyn*	ef_dyn;			/* Symbol table etc. */
+	Elf_Off		ef_nbuckets;
+	Elf_Off		ef_nchains;
+	Elf_Off*	ef_buckets;
+	Elf_Off*	ef_chains;
+	Elf_Off*	ef_hashtab;
+	Elf_Off		ef_stroff;
+	caddr_t		ef_strtab;
+	int		ef_strsz;
+	Elf_Off		ef_symoff;
+	Elf_Sym*	ef_symtab;
+	int		ef_nsegs;
+	Elf_Phdr *	ef_segs[2];
+	int		ef_verbose;
+} *elf_file_t;
+
+__BEGIN_DECLS
+int ef_open(const char *, elf_file_t, int);
+int ef_close(elf_file_t ef);
+int ef_read(elf_file_t ef, Elf_Off offset, size_t len, void* dest);
+int ef_read_entry(elf_file_t ef, Elf_Off offset, size_t len, void **ptr);
+int ef_seg_read(elf_file_t ef, Elf_Off offset, size_t len, void *dest);
+int ef_seg_read_entry(elf_file_t ef, Elf_Off offset, size_t len, void**ptr);
+int ef_lookup_symbol(elf_file_t ef, const char* name, Elf_Sym** sym);
+__END_DECLS
+
+#endif /* _EF_H_*/
