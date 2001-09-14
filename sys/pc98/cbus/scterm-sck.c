114995eb4e57d11c3e8f07d85db3e7272831d73f
imp <imp@FreeBSD.org>
2001-09-14 04:54:38 +0000

    Merge KSE changes from sys/dev/syscons/scterm-sc.c 1.17
    # there are more "trivial" diffs in this file that have accumulated over time
    # and I didn't try to fix those.
    
    Reviewed by: julian, bde, jhb

--- sys/pc98/cbus/scterm-sck.c
+++ sys/pc98/cbus/scterm-sck.c
@@ -933,7 +933,7 @@ kanji_end:
 
 static int
 scterm_ioctl(scr_stat *scp, struct tty *tp, u_long cmd, caddr_t data,
-	     int flag, struct proc *p)
+	     int flag, struct thread *td)
 {
 	term_stat *tcp = scp->ts;
 	vid_info_t *vi;
