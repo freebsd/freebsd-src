7363b328ce7845804dd62d86b311be35a5f9f146
dillon <dillon@FreeBSD.org>
2001-09-15 03:42:10 +0000

    Convert a library to use the new __FBSDID()'s for testing and review purposes.

--- lib/libatm/atm_addr.c
+++ lib/libatm/atm_addr.c
@@ -36,6 +36,7 @@
  */
 
 #include <sys/types.h>
+#include <sys/cdefs.h>
 #include <sys/param.h>
 #include <sys/socket.h>
 #include <net/if.h>
@@ -52,10 +53,7 @@
 
 #include "libatm.h"
 
-#ifndef lint
-__RCSID("@(#) $FreeBSD$");
-#endif
-
+__FBSDID("$FreeBSD$");
 
 extern char	*prog;
 
