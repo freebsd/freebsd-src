378482865e37ad99bf3b1adbb1d217d94d619d6b
Doug Rabson <dfr@FreeBSD.org>
2001-09-24 19:27:38 +0000

    Factor out PTE and related definitions from pmap.h - they are useful in the loader.

--- sys/ia64/include/pmap.h
+++ sys/ia64/include/pmap.h
@@ -49,94 +49,8 @@
 #ifndef _MACHINE_PMAP_H_
 #define	_MACHINE_PMAP_H_
 
-#ifdef LOCORE
-
-#define PTE_P		(1<<0)
-#define PTE_MA_WB	(0<<2)
-#define PTE_MA_UC	(4<<2)
-#define PTE_MA_UCE	(5<<2)
-#define PTE_MA_WC	(6<<2)
-#define PTE_MA_NATPAGE	(7<<2)
-#define PTE_A		(1<<5)
-#define PTE_D		(1<<6)
-#define PTE_PL_KERN	(0<<7)
-#define PTE_PL_USER	(3<<7)
-#define PTE_AR_R	(0<<9)
-#define PTE_AR_RX	(1<<9)
-#define PTE_AR_RW	(2<<9)
-#define PTE_AR_RWX	(3<<9)
-#define PTE_AR_R_RW	(4<<9)
-#define PTE_AR_RX_RWX	(5<<9)
-#define PTE_AR_RWX_RW	(6<<9)
-#define PTE_AR_X_RX	(7<<9)
-
-#else
-
-#define PTE_MA_WB	0
-#define PTE_MA_UC	4
-#define PTE_MA_UCE	5
-#define PTE_MA_WC	6
-#define PTE_MA_NATPAGE	7
-
-#define PTE_PL_KERN	0
-#define PTE_PL_USER	3
-
-#define PTE_AR_R	0
-#define PTE_AR_RX	1
-#define PTE_AR_RW	2
-#define PTE_AR_RWX	3
-#define PTE_AR_R_RW	4
-#define PTE_AR_RX_RWX	5
-#define PTE_AR_RWX_RW	6
-#define PTE_AR_X_RX	7
-
-#define PTE_IG_WIRED	1
-#define PTE_IG_MANAGED	2
-
-/*
- * A short-format VHPT entry. Also matches the TLB insertion format.
- */
-struct ia64_pte {
-	u_int64_t	pte_p	:1;	/* bits 0..0 */
-	u_int64_t	pte_rv1	:1;	/* bits 1..1 */
-	u_int64_t	pte_ma	:3;	/* bits 2..4 */
-	u_int64_t	pte_a	:1;	/* bits 5..5 */
-	u_int64_t	pte_d	:1;	/* bits 6..6 */
-	u_int64_t	pte_pl	:2;	/* bits 7..8 */
-	u_int64_t	pte_ar	:3;	/* bits 9..11 */
-	u_int64_t	pte_ppn	:38;	/* bits 12..49 */
-	u_int64_t	pte_rv2	:2;	/* bits 50..51 */
-	u_int64_t	pte_ed	:1;	/* bits 52..52 */
-	u_int64_t	pte_ig	:11;	/* bits 53..63 */
-};
-
-/*
- * A long-format VHPT entry.
- */
-struct ia64_lpte {
-	u_int64_t	pte_p	:1;	/* bits 0..0 */
-	u_int64_t	pte_rv1	:1;	/* bits 1..1 */
-	u_int64_t	pte_ma	:3;	/* bits 2..4 */
-	u_int64_t	pte_a	:1;	/* bits 5..5 */
-	u_int64_t	pte_d	:1;	/* bits 6..6 */
-	u_int64_t	pte_pl	:2;	/* bits 7..8 */
-	u_int64_t	pte_ar	:3;	/* bits 9..11 */
-	u_int64_t	pte_ppn	:38;	/* bits 12..49 */
-	u_int64_t	pte_rv2	:2;	/* bits 50..51 */
-	u_int64_t	pte_ed	:1;	/* bits 52..52 */
-	u_int64_t	pte_ig	:11;	/* bits 53..63 */
-
-	u_int64_t	pte_rv3	:2;	/* bits 0..1 */
-	u_int64_t	pte_ps	:6;	/* bits 2..7 */
-	u_int64_t	pte_key	:24;	/* bits 8..31 */
-	u_int64_t	pte_rv4	:32;	/* bits 32..63 */
-
-	u_int64_t	pte_tag;	/* includes ti */
-
-	u_int64_t	pte_chain;	/* pa of collision chain */
-};
-
 #include <sys/queue.h>
+#include <machine/pte.h>
 
 #ifdef _KERNEL
 
@@ -224,7 +138,5 @@ void	pmap_set_opt_bsp	__P((void));
 struct pmap *pmap_install __P((struct pmap *pmap));
 
 #endif /* _KERNEL */
-
-#endif /* !LOCORE */
 
 #endif /* !_MACHINE_PMAP_H_ */
