c24fb02d96f28fdf4e98ef82c534f32d2c562c90
Ian Dowse <iedowse@FreeBSD.org>
2001-09-29 12:52:52 +0000

    MFC: Bring sizeof(struct inode) back to 256 bytes by reducing the size of i_spare[] and making ext2fs use a spare dinode field instead.

--- sys/ufs/ufs/inode.h
+++ sys/ufs/ufs/inode.h
@@ -93,7 +93,7 @@ struct inode {
 	doff_t	  i_offset;	/* Offset of free space in directory. */
 	ino_t	  i_ino;	/* Inode number of found directory. */
 	u_int32_t i_reclen;	/* Size of found directory entry. */
-	u_int32_t i_spare[4];	/* XXX actually non-spare (for ext2fs). */
+	u_int32_t i_spare[3];	/* XXX actually non-spare (for ext2fs). */
 
 	struct dirhash *i_dirhash; /* Hashing for large directories */
 	/*
