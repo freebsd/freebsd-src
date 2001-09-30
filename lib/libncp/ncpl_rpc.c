79eb3a0b10e2600ae990d3c5337e50ba1e3e4b06
dillon <dillon@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-09-30 22:01:19 +0000

    Add __FBSDID()s to libncp
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@84213 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libncp/ncpl_rpc.c
+++ lib/libncp/ncpl_rpc.c
@@ -29,9 +29,11 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  * NetWare RPCs
- *
- * $FreeBSD$
  */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
 #include <sys/types.h>
 #include <sys/time.h>
 #include <errno.h>
