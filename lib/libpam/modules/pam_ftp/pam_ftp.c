a3cbbd58819678c961004127e28aaf93fe4879ed
dillon <dillon@FreeBSD.org>
2001-09-30 22:11:06 +0000

    Add __FBSDID()s to libpam

--- lib/libpam/modules/pam_ftp/pam_ftp.c
+++ lib/libpam/modules/pam_ftp/pam_ftp.c
@@ -22,9 +22,10 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $FreeBSD$
  */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #define	PROMPT		"Password required for %s."
 #define	GUEST_PROMPT	"Guest login ok, send your e-mail address as password."
