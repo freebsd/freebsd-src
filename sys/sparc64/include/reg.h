6815a9f8598bb19fa58c494739a7a907f10cb521
jake <jake@FreeBSD.org>
2001-09-30 19:59:07 +0000

    Add contents to struct *reg.

--- sys/sparc64/include/reg.h
+++ sys/sparc64/include/reg.h
@@ -1,6 +1,9 @@
 /*-
- * Copyright (c) 2001 Jake Burkholder.
+ * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * William Jolitz.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -10,11 +13,18 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -23,26 +33,47 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
+ *	from: @(#)reg.h	5.5 (Berkeley) 1/18/91
+ *	from: FreeBSD: src/sys/i386/include/reg.h,v 1.23 2000/09/21
  * $FreeBSD$
  */
 
 #ifndef	_MACHINE_REG_H_
 #define	_MACHINE_REG_H_
 
+/*
+ * Register set accessible via /proc/$pid/regs and PT_{SET,GET}REGS.
+ */
 struct reg {
+	u_long	r_global[8];
+	u_long	r_out[8];
+	u_long	r_tstate;
+	u_long	r_pc;
+	u_long	r_npc;
+	u_int	r_y;
 };
 
+/*
+ * Register set accessible via /proc/$pid/fpregs.
+ */
 struct fpreg {
+	u_int	fr_regs[64];
+	u_long	fr_fsr;
 };
 
+/*
+ * Register set accessible via /proc/$pid/dbregs.
+ */
 struct dbreg {
 };
 
 #ifdef _KERNEL
+/*
+ * XXX these interfaces are MI, so they should be declared in a MI place.
+ */
 int	set_fpregs(struct thread *td, struct fpreg *fpregs);
 int	set_regs(struct thread *td, struct reg *regs);
 void	setregs(struct thread *td, u_long e, u_long s, u_long ps);
-int	set_dbregs(struct thread *td, struct dbreg *dbregs);
 #endif
 
 #endif /* !_MACHINE_REG_H_ */
