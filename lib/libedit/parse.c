9478329f864635ae5e2ac113fbda60f934239340
obrien <obrien@FreeBSD.org>
2001-10-01 23:00:29 +0000

    Fix ID's after a total boch CVS merge due to multiple people making commits to these files. As I sing to CVS: 	Have I told you lately that I hate your guts?  Have I told you 	all SCM's are above you?  You fill my heart with pain, take away 	all my merging joy, grow my troubles that's what you do."

--- lib/libedit/parse.c
+++ lib/libedit/parse.c
@@ -36,8 +36,6 @@
  *	$NetBSD: parse.c,v 1.13 2000/09/04 22:06:31 lukem Exp $
  */
 
-#include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
 #if !defined(lint) && !defined(SCCSID)
 static char sccsid[] = "@(#)parse.c	8.1 (Berkeley) 6/4/93";
 #endif /* not lint && not SCCSID */
