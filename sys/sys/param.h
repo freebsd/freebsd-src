bafc53748d96e0f73363527ad7cfb2d955e93d3f
dwmalone <dwmalone@FreeBSD.org>
2001-10-04 16:14:43 +0000

    Bump __FreeBSD_version after change in ABI for discriptor and creds passing on 64 bit platforms.
    Reviewed by:	imp

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -52,7 +52,7 @@
  * there.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 500024	/* Master, propagated to newvers */
+#define __FreeBSD_version 500025	/* Master, propagated to newvers */
 
 #ifndef NULL
 #define	NULL	0
