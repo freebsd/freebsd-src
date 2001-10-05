3c252600584c9f07e9a9145a104a08e001090d00
jayanth <jayanth@FreeBSD.org>
2001-10-05 21:33:38 +0000

    Add a flag TF_LASTIDLE, that forces a previously idle connection to send all its data, especially when the data is less than one MSS. This fixes an issue where the stack was delaying the sending of data, eventhough there was enough window to send all the data and the sending of data was emptying the socket buffer.
    Problem found by Yoshihiro Tsuchiya (tsuchiya@flab.fujitsu.co.jp)
    
    Submitted by: Jayanth Vijayaraghavan

--- sys/netinet/tcp_var.h
+++ sys/netinet/tcp_var.h
@@ -95,6 +95,7 @@ struct tcpcb {
 #define	TF_SENDCCNEW	0x08000		/* send CCnew instead of CC in SYN */
 #define	TF_MORETOCOME	0x10000		/* More data to be appended to sock */
 #define	TF_LQ_OVERFLOW	0x20000		/* listen queue overflow */
+#define	TF_LASTIDLE	0x40000		/* connection was previously idle */
 	int	t_force;		/* 1 if forcing out a byte */
 
 	tcp_seq	snd_una;		/* send unacknowledged */
