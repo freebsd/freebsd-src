d0ad824d688d8ca92ff5b7a0ee404d3c079ad5e9
Doug Rabson <dfr@FreeBSD.org>
2001-10-06 15:57:22 +0000

    Assume round-to-nearest mode for floating point.

--- sys/ia64/include/float.h
+++ sys/ia64/include/float.h
@@ -39,12 +39,8 @@
 
 #include <sys/cdefs.h>
 
-__BEGIN_DECLS
-int	__flt_rounds __P((void));
-__END_DECLS
-
 #define FLT_RADIX	2		/* b */
-#define FLT_ROUNDS	__flt_rounds()
+#define FLT_ROUNDS	1		/* FP addition rounds to nearest */
 
 #define FLT_MANT_DIG	24		/* p */
 #define FLT_EPSILON	1.19209290E-07F	/* b**(1-p) */
