1f43bba0a85f4a68e9877bc05c4a4f777fd306e1
luigi <luigi@FreeBSD.org>
2001-10-07 00:29:59 +0000

    Fix a typo (missing "; at the end of a string)

--- release/picobsd/tinyware/login/pico-login.c
+++ release/picobsd/tinyware/login/pico-login.c
@@ -42,7 +42,7 @@ static char copyright[] =
 static char sccsid[] = "@(#)login.c	8.4 (Berkeley) 4/2/94";
 #endif
 static const char rcsid[] =
-  "$FreeBSD$
+  "$FreeBSD$";
 #endif /* not lint */
 
 /*
