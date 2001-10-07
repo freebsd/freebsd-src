0c1686270f3438c5ef9b78cdba38a0123ecb875a
benno <benno@FreeBSD.org>
2001-10-07 13:24:56 +0000

    Add new files needed by previous commit.

---
+++ sys/boot/ofw/libofw/elf_freebsd.c
@@ -0,0 +1,117 @@
+/*-
+ * Copyright (c) 2001 Benno Rice <benno@FreeBSD.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#include <sys/param.h>
+#include <sys/linker.h>
+
+#include <machine/bootinfo.h>
+#include <machine/elf.h>
+
+#include <stand.h>
+
+#include "bootstrap.h"
+#include "libofw.h"
+#include "openfirm.h"
+
+extern char		end[];
+extern vm_offset_t	reloc;	/* From <arch>/conf.c */
+
+int
+ofw_elf_loadfile(char *filename, vm_offset_t dest,
+    struct preloaded_file **result)
+{
+	int	r;
+	void	*addr;
+
+	r = elf_loadfile(filename, dest, result);
+	if (r != 0)
+		return (r);
+
+	addr = OF_claim((void *)(*result)->f_addr, (*result)->f_size, 0);
+
+	if (addr == (void *)-1 || addr != (void *)(*result)->f_addr)
+		return (ENOMEM);
+
+	return (0);
+}
+
+int
+ofw_elf_exec(struct preloaded_file *fp)
+{
+	struct file_metadata	*md;
+	Elf_Ehdr		*ehdr;
+	vm_offset_t		entry, bootinfop;
+	int			boothowto, err, bootdev;
+	struct bootinfo		*bi;
+	vm_offset_t		ssym, esym;
+
+	if ((md = file_findmetadata(fp, MODINFOMD_ELFHDR)) == NULL) {
+		return(EFTYPE);			/* XXX actually EFUCKUP */
+	}
+	ehdr = (Elf_Ehdr *)&(md->md_data);
+
+#if 0
+	if ((err = bi_load(fp->f_args, &boothowto, &bootdev, &bootinfop)) != 0)
+		return(err);
+#endif
+	entry = ehdr->e_entry & 0xffffff;
+
+	ssym = esym = 0;
+	if ((md = file_findmetadata(fp, MODINFOMD_SSYM)) != NULL)
+		ssym = *((vm_offset_t *)&(md->md_data));
+	if ((md = file_findmetadata(fp, MODINFOMD_ESYM)) != NULL)
+		esym = *((vm_offset_t *)&(md->md_data));
+	if (ssym == 0 || esym == 0)
+		ssym = esym = 0;		/* sanity */
+#if 0
+	bi = (struct bootinfo *)PTOV(bootinfop);
+	bi->bi_symtab = ssym;
+	bi->bi_esymtab = esym;
+#endif
+
+/*
+#ifdef DEBUG
+*/
+	printf("Start @ 0x%lx ...\n", entry);
+/*
+#endif
+*/
+
+	dev_cleanup();
+	ofw_release_heap();
+/*	OF_chain(0, 0, entry, bootinfop, sizeof(struct bootinfo));*/
+	OF_chain((void *)reloc, end - (char *)reloc, (void *)entry, NULL, 0);
+
+	panic("exec returned");
+}
+
+struct file_format	ofw_elf =
+{
+	ofw_elf_loadfile,
+	ofw_elf_exec
+};
---
+++ sys/boot/ofw/libofw/ofw_memory.c
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2001 Benno Rice
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Benno Rice ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#include <sys/param.h>
+#include <sys/types.h>
+
+#include <stand.h>
+
+#include "libofw.h"
+#include "openfirm.h"
+
+static void		*heap_base = 0;
+static unsigned int	heap_size = 0;
+
+struct ofw_mapping {
+        vm_offset_t     va;
+        int             len;
+        vm_offset_t     pa;
+        int             mode;
+};
+
+void
+ofw_memmap(void)
+{
+        ihandle_t       mmui;
+        phandle_t       mmu;
+        int             nmapping, i;
+        struct          ofw_mapping mappings[256];
+   
+        OF_getprop(chosen, "mmu", &mmui, 4);
+        mmu = OF_instance_to_package(mmui);
+ 
+        bzero(mappings, sizeof(mappings));
+ 
+        nmapping = OF_getprop(mmu, "translations", mappings, sizeof(mappings));
+	if (nmapping == -1) {
+		printf("Could not get memory map (%d)\n",
+		    nmapping);
+		return;
+	}
+        nmapping /= sizeof(struct ofw_mapping);
+
+        printf("%17s %17s %8s %6s\n", "Virtual Range", "Physical Range",
+            "#Pages", "Mode");
+
+        for (i = 0; i < nmapping; i++) {
+                printf("%08x-%08x %08x-%08x %8d %6x\n", mappings[i].pa,
+                    mappings[i].pa + mappings[i].len, mappings[i].va,
+                    mappings[i].va + mappings[i].len, mappings[i].len / 0x1000,
+                    mappings[i].mode);
+	}
+}
+
+void *
+ofw_alloc_heap(unsigned int size)
+{
+	ihandle_t	meminstance;
+	phandle_t	memory;
+	struct		ofw_reg available;
+	void		*base;
+
+	OF_getprop(chosen, "memory", &meminstance, sizeof(meminstance));
+	memory = OF_instance_to_package(meminstance);
+	OF_getprop(memory, "available", &available, sizeof(available));
+
+	heap_base = OF_claim((void *)available.base, size, 4);
+
+	if (heap_base != (void *)0xffffffff) {
+		heap_size = size;
+	}
+
+	return (heap_base);
+}
+
+void
+ofw_release_heap(void)
+{
+	OF_release(heap_base, heap_size);
+}
