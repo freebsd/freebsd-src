b6aaa482efc4c4f2713499389011fc941f5fd2a8
Thomas Moestl <tmm@FreeBSD.org>
2001-10-12 15:58:48 +0000

    Save the floating point context to the right pcb in cpu_fork(), and add an empty stub for is_physical_memory().

--- sys/sparc64/include/md_var.h
+++ sys/sparc64/include/md_var.h
@@ -47,6 +47,7 @@ void	cpu_identify(u_int clock);
 void	cpu_reset(void);
 int	fill_fpregs(struct thread *td, struct fpreg *fpregs);
 int	fill_regs(struct thread *td, struct reg *regs);
+int	is_physical_memory(vm_offset_t addr);
 void	swi_vm(void *v);
 
 #endif /* !_MACHINE_MD_VAR_H_ */
