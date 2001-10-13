cb078bdae4155df937d79a47972ae40186870d9d
bde <bde@FreeBSD.org>
2001-10-13 04:38:46 +0000

    Backed out "Compensate for header dethreading [mistakes]" mistakes in alpha files too.

--- gnu/usr.bin/binutils/gdb/alpha/kvm-fbsd.c
+++ gnu/usr.bin/binutils/gdb/alpha/kvm-fbsd.c
@@ -32,7 +32,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 #include <sys/param.h>
-#include <sys/lock.h>
 #include <sys/time.h>
 #include <sys/proc.h>
 #include <sys/user.h>
