7c3412ba58d21592838b61f53653b507551ea52b
tegge <tegge@FreeBSD.org>
2001-10-14 21:11:33 +0000

    Repair normally unused is_digit() macro.

--- bin/sh/mksyntax.c
+++ bin/sh/mksyntax.c
@@ -351,7 +351,7 @@ print(name)
  */
 
 static char *macro[] = {
-	"#define is_digit(c)\t((c >= 0 && is_type+SYNBASE)[c] & ISDIGIT)",
+	"#define is_digit(c)\t((is_type+SYNBASE)[c] & ISDIGIT)",
 	"#define is_alpha(c)\t((c) != PEOF && ((c) < CTLESC || (c) > CTLQUOTEMARK) && isalpha((unsigned char) (c)))",
 	"#define is_name(c)\t((c) != PEOF && ((c) < CTLESC || (c) > CTLQUOTEMARK) && ((c) == '_' || isalpha((unsigned char) (c))))",
 	"#define is_in_name(c)\t((c) != PEOF && ((c) < CTLESC || (c) > CTLQUOTEMARK) && ((c) == '_' || isalnum((unsigned char) (c))))",
