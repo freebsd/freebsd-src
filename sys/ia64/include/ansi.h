85adfe338c1e2205fcae76de705c00b943b8679f
obrien <obrien@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-10-18 00:27:39 +0000

    Add support for "__gnuc_va_list".  Some overly "smart" libraries assume the existence of the __gnuc_va_list type[*] because our compiler is GCC.
    [*] __gnuc_va_list is defined in the GCC ginclude/stdarg.h replacement
    headerwhich we don't use.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@85085 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/ia64/include/ansi.h
+++ sys/ia64/include/ansi.h
@@ -71,7 +71,13 @@
 #define	_BSD_CT_RUNE_T_	int			/* arg type for ctype funcs */
 #define	_BSD_OFF_T_	long			/* file offset */
 #define	_BSD_PID_T_	int			/* process [group] */
-#define	_BSD_VA_LIST_	__builtin_va_list	/* va_list */
+
+#if defined __GNUC__
+#define	_BSD_VA_LIST_	__builtin_va_list	/* internally known to gcc */
+typedef _BSD_VA_LIST_ __gnuc_va_list;		/* compatibility w/GNU headers*/
+#else
+#error Must add va_list support for this non-GCC compiler.
+#endif /*__GNUC__*/
 
 /*
  * The rune type above is declared to be an ``int'' instead of the more natural
