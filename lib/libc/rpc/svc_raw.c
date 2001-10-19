1b131bca2f74efdc3627f5ad1fce812c67b57975
alfred <alfred@FreeBSD.org>
2001-10-19 07:52:11 +0000

    s/kernal/kernel

--- lib/libc/rpc/svc_raw.c
+++ lib/libc/rpc/svc_raw.c
@@ -45,7 +45,7 @@ static char *rcsid = "$FreeBSD$";
  * svc_raw.c,   This a toy for simple testing and timing.
  * Interface to create an rpc client and server in the same UNIX process.
  * This lets us similate rpc and get rpc (round trip) overhead, without
- * any interference from the kernal.
+ * any interference from the kernel.
  *
  */
 
