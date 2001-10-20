050b3d63282ef4df7ff026b6b396263414d70d80
gallatin <gallatin@FreeBSD.org>
2001-10-20 21:05:14 +0000

    - splhigh()/splx() -> critical_enter()/critical_exit() - fix KV macro in t2_pci.c to include the sable_lynx_base variable so that the T2 CSRs can be found on lynxes.   Current should be bootable on lynxes now.

--- sys/alpha/pci/t2.c
+++ sys/alpha/pci/t2.c
@@ -118,14 +118,14 @@ t2_set_hae_mem(void *arg, u_int32_t pa)
 		msb = pa & 0xf8000000;
 		pa -= msb;
 		msb >>= 27;	/* t2 puts high bits in the bottom of the register */
-		s = splhigh();
+		s = critical_enter();
 		if (msb != t2_hae_mem[hose]) {
 			t2_hae_mem[hose] = msb;
 			t2_csr[hose]->hae0_1 = t2_hae_mem[hose];
 			alpha_mb();
 			t2_hae_mem[hose] = t2_csr[hose]->hae0_1;
 		}
-		splx(s);
+		critical_exit(s);
 	}
 	return pa;
 }
