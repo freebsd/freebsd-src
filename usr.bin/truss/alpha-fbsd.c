9738c0ef711a1880227dd3229d8c9d60b11fe1e0
des <des@FreeBSD.org>
2001-10-22 02:02:00 +0000

    #include <sys/proc.h> where needed (for the stop event definitions) and reorder includes everywhere to conform to style(9).

--- usr.bin/truss/alpha-fbsd.c
+++ usr.bin/truss/alpha-fbsd.c
@@ -44,6 +44,14 @@ static const char rcsid[] =
  * This file is almost nothing more than a slightly-edited i386-fbsd.c.
  */
 
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/pioctl.h>
+#include <sys/syscall.h>
+
+#include <machine/reg.h>
+#include <machine/psl.h>
+
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -52,11 +60,6 @@ static const char rcsid[] =
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
-#include <sys/pioctl.h>
-#include <machine/reg.h>
-#include <machine/psl.h>
-#include <sys/syscall.h>
 
 #include "syscall.h"
 
--- usr.bin/truss/amd64-linux32.c
+++ usr.bin/truss/amd64-linux32.c
@@ -40,6 +40,13 @@ static const char rcsid[] =
  * it in support routines that can be used by any personality support.
  */
 
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/pioctl.h>
+
+#include <machine/reg.h>
+#include <machine/psl.h>
+
 #include <errno.h>
 #include <fcntl.h>
 #include <signal.h>
@@ -47,10 +54,6 @@ static const char rcsid[] =
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
-#include <sys/pioctl.h>
-#include <machine/reg.h>
-#include <machine/psl.h>
 
 #include "syscall.h"
 
