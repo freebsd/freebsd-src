c193b945eb49a7fe30fcf892fdadb715aad045ff
Dag-Erling Smørgrav <des@FreeBSD.org>
2001-10-22 16:13:38 +0000

    No, you may not /* FALLTHROUGH */.  Not only will you return an incorrect result, but you'd corrupt the kernel malloc() arena if it weren't for a small but life-saving optimization in ioctl().
    MFC after:	1 week

--- sys/fs/procfs/procfs_vnops.c
+++ sys/fs/procfs/procfs_vnops.c
@@ -268,7 +268,7 @@ procfs_ioctl(ap)
 	  break;
 	case PIOCGFL:
 	  *(unsigned int*)ap->a_data = (unsigned int)procp->p_pfsflags;
-	  /* FALLTHROUGH */
+	  break;
 	case PIOCSTATUS:
 	  psp = (struct procfs_status *)ap->a_data;
 	  psp->state = (procp->p_step == 0);
