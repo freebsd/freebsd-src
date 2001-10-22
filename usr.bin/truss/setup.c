580e0a2b7ab7273f868e7f4f9cbe96662b67a700
Dag-Erling Smørgrav <des@FreeBSD.org>
2001-10-22 02:02:00 +0000

    #include <sys/proc.h> where needed (for the stop event definitions) and reorder includes everywhere to conform to style(9).

--- usr.bin/truss/setup.c
+++ usr.bin/truss/setup.c
@@ -39,6 +39,12 @@ static const char rcsid[] =
  * I'm afraid.
  */
 
+#include <sys/param.h>
+#include <sys/ioctl.h>
+#include <sys/pioctl.h>
+#include <sys/proc.h>
+#include <sys/wait.h>
+
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -47,10 +53,6 @@ static const char rcsid[] =
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
-#include <sys/pioctl.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
 static int evflags = 0;
 
