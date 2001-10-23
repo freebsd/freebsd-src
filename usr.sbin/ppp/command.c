dad51e5ce8a405b90a5086a3fe73a81d72e8dfe7
Brian Somers <brian@FreeBSD.org>
2001-10-23 13:52:19 +0000

    Don't avoid setting a 0 second timer in datalink_StartDialTimer() by not setting any timer.  Instead, set a 1 millisecond timer.
    This ensures that ppp will come out of it's select() call after
    losing carrier in -ddial mode with a reconnect period of 0 and
    going to ST_OPENING, rather than waiting indefinitely for some
    other event to wake ppp up.
    
    Bump the ppp version number to indicate the event.
    
    MFC after: 3 days

--- usr.sbin/ppp/command.c
+++ usr.sbin/ppp/command.c
@@ -164,7 +164,7 @@
 #define NEG_MPPE	54
 #define NEG_CHAP81	55
 
-const char Version[] = "3.0.0";
+const char Version[] = "3.0.1";
 
 static int ShowCommand(struct cmdargs const *);
 static int TerminalCommand(struct cmdargs const *);
