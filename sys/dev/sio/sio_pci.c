2ce87d3448689ae986a3dc02969f161875fbec4a
Warner Losh <imp@FreeBSD.org>
2001-10-23 15:17:33 +0000

    Break out the bus front ends into their own files.  Rewrite sio_pccard_detach to use new siodetach.  Add an extra arg to sioprobe to tell driver to probe/not probe the device for IRQs.
    This incorporates most of Bruce's review material.  I'm at a good
    checkpoint, but there will be more to come based on bde's further
    reviews.
    
    Reviewed by: bde

---
+++ sys/dev/sio/sio_pci.c
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2001 M. Warner Losh.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/bus.h>
+#include <sys/conf.h>
+#include <sys/kernel.h>
+#include <sys/lock.h>
+#include <sys/malloc.h>
+#include <sys/mutex.h>
+#include <sys/module.h>
+#include <sys/tty.h>
+#include <machine/clock.h>
+#include <machine/bus_pio.h>
+#include <machine/bus.h>
+#include <sys/timepps.h>
+
+#include <dev/sio/siovar.h>
+
+#include <pci/pcireg.h>
+#include <pci/pcivar.h>
+
+static	int	sio_pci_attach __P((device_t dev));
+static	void	sio_pci_kludge_unit __P((device_t dev));
+static	int	sio_pci_probe __P((device_t dev));
+
+static device_method_t sio_pci_methods[] = {
+	/* Device interface */
+	DEVMETHOD(device_probe,		sio_pci_probe),
+	DEVMETHOD(device_attach,	sio_pci_attach),
+
+	{ 0, 0 }
+};
+
+static driver_t sio_pci_driver = {
+	sio_driver_name,
+	sio_pci_methods,
+	sizeof(struct com_s),
+};
+
+struct pci_ids {
+	u_int32_t	type;
+	const char	*desc;
+	int		rid;
+};
+
+static struct pci_ids pci_ids[] = {
+	{ 0x100812b9, "3COM PCI FaxModem", 0x10 },
+	{ 0x2000131f, "CyberSerial (1-port) 16550", 0x10 },
+	{ 0x01101407, "Koutech IOFLEX-2S PCI Dual Port Serial", 0x10 },
+	{ 0x01111407, "Koutech IOFLEX-2S PCI Dual Port Serial", 0x10 },
+	{ 0x048011c1, "Lucent kermit based PCI Modem", 0x14 },
+	{ 0x95211415, "Oxford Semiconductor PCI Dual Port Serial", 0x10 },
+	{ 0x0000151f, "SmartLink 5634PCV SurfRider", 0x10 },
+	/* { 0xXXXXXXXX, "Xircom Cardbus modem", 0x10 }, */
+	{ 0x00000000, NULL, 0 }
+};
+
+static int
+sio_pci_attach(dev)
+	device_t	dev;
+{
+	u_int32_t	type;
+	struct pci_ids	*id;
+
+	type = pci_get_devid(dev);
+	id = pci_ids;
+	while (id->type && id->type != type)
+		id++;
+	if (id->desc == NULL)
+		return (ENXIO);
+	sio_pci_kludge_unit(dev);
+	return (sioattach(dev, id->rid));
+}
+
+/*
+ * Don't cut and paste this to other drivers.  It is a horrible kludge
+ * which will fail to work and also be unnecessary in future versions.
+ */
+static void
+sio_pci_kludge_unit(dev)
+	device_t dev;
+{
+	devclass_t	dc;
+	int		err;
+	int		start;
+	int		unit;
+
+	unit = 0;
+	start = 0;
+	while (resource_int_value("sio", unit, "port", &start) == 0 && 
+	    start > 0)
+		unit++;
+	if (device_get_unit(dev) < unit) {
+		dc = device_get_devclass(dev);
+		while (devclass_get_device(dc, unit))
+			unit++;
+		device_printf(dev, "moving to sio%d\n", unit);
+		err = device_set_unit(dev, unit);	/* EVIL DO NOT COPY */
+		if (err)
+			device_printf(dev, "error moving device %d\n", err);
+	}
+}
+
+static int
+sio_pci_probe(dev)
+	device_t	dev;
+{
+	u_int32_t	type;
+	struct pci_ids	*id;
+
+	type = pci_get_devid(dev);
+	id = pci_ids;
+	while (id->type && id->type != type)
+		id++;
+	if (id->desc == NULL)
+		return (ENXIO);
+	device_set_desc(dev, id->desc);
+	return (sioprobe(dev, id->rid, 0));
+}
+
+DRIVER_MODULE(sio, pci, sio_pci_driver, sio_devclass, 0, 0);
+DRIVER_MODULE(sio, cardbus, sio_pci_driver, sio_devclass, 0, 0);
