21df309496575a43e39336a3c372ee6b89a8405d
brian <brian@FreeBSD.org>
2001-10-26 10:00:39 +0000

    MFC: Don't avoid setting a 0 second timer in datalink_StartDialTimer() by      not setting any timer.  Instead, set a 1 millisecond timer.

--- usr.sbin/ppp/command.c
+++ usr.sbin/ppp/command.c
@@ -159,7 +159,7 @@
 #define NEG_MPPE	54
 #define NEG_CHAP81	55
 
-const char Version[] = "2.3.2";
+const char Version[] = "2.3.3";
 
 static int ShowCommand(struct cmdargs const *);
 static int TerminalCommand(struct cmdargs const *);
