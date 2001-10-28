3e277dbdb4c09f45f2b8c460917a89bca86d20e1
dillon <dillon@FreeBSD.org>
2001-10-28 20:19:14 +0000

    Properly convert long to time_t

--- sbin/ipfw/ipfw.c
+++ sbin/ipfw/ipfw.c
@@ -202,8 +202,9 @@ show_ipfw(struct ip_fw *chain)
 	if (do_time) {
 		if (chain->timestamp) {
 			char timestr[30];
+			time_t t = long_to_time(chain->timestamp);
 
-			strcpy(timestr, ctime((time_t *)&chain->timestamp));
+			strcpy(timestr, ctime(&t));
 			*strchr(timestr, '\n') = '\0';
 			printf("%s ", timestr);
 		} else {
