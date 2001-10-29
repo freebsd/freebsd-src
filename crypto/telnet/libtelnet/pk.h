a814c66f6b7428e9103f7eb57feb044c5b418eac
jhb <jhb@FreeBSD.org>
2001-10-29 19:22:38 +0000

    Fix world by trimming an extra comment terminator.

--- crypto/telnet/libtelnet/pk.h
+++ crypto/telnet/libtelnet/pk.h
@@ -26,7 +26,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  * 
- * $FreeBSD$ */
+ * $FreeBSD$
  */
 
 /* header for the des routines that we will use */
