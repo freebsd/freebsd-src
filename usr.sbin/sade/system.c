7a631f774d4fa89187f841371e6db00372b229a2
Jordan K. Hubbard <jkh@FreeBSD.org>
2001-11-01 23:32:46 +0000

    MFS: Stop reaping children.  It makes evil things happen to the extraction loop.

--- usr.sbin/sade/system.c
+++ usr.sbin/sade/system.c
@@ -188,7 +188,9 @@ systemInitialize(int argc, char **argv)
 	i = 0;
 	sysctlbyname("machdep.unaligned_print", NULL, 0, &i, sizeof(i));
 #endif
+#if 0
 	signal(SIGCHLD, reap_children);
+#endif
     }
     else {
 	char hname[256];
