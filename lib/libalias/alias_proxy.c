8c020e51506b7cdce94b5777a210ff80b27e2bed
Brian Somers <brian@FreeBSD.org>
2001-11-03 11:37:13 +0000

    MFC: cmott@scientech.com -> cm@linktel.net

--- lib/libalias/alias_proxy.c
+++ lib/libalias/alias_proxy.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2001 Charles Mott <cmott@scientech.com>
+ * Copyright (c) 2001 Charles Mott <cm@linktel.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
