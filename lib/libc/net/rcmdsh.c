e117e7a5b0d0e742d9405f4ad7165b227e04704e
Ruslan Ermilov <ru@FreeBSD.org>
2001-11-09 15:19:25 +0000

    - Put missing prototype for rcmd() in <unistd.h>. - Clean up the manpage. - style(9) rcmdsh.c.
    Committed from:	BSDCon/EU 2k+1 terminal room

--- lib/libc/net/rcmdsh.c
+++ lib/libc/net/rcmdsh.c
@@ -1,30 +1,27 @@
-/*	$OpenBSD: rcmdsh.c,v 1.5 1998/04/25 16:23:58 millert Exp $	*/ 
+/*	$OpenBSD: rcmdsh.c,v 1.5 1998/04/25 16:23:58 millert Exp $	*/
 
 /*
- * This is an rcmd() replacement originally by 
+ * This is an rcmd() replacement originally by
  * Chris Siebenmann <cks@utcc.utoronto.ca>.
- *
- * $FreeBSD$
  */
 
-#if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$FreeBSD$"
-#endif /* LIBC_SCCS and not lint */
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
-#include      <sys/types.h>
-#include      <sys/socket.h>
-#include      <sys/wait.h>
-#include      <signal.h>
-#include      <errno.h>
-#include      <netdb.h>
-#include      <stdio.h>
-#include      <string.h>
-#include      <pwd.h>
-#include      <paths.h>
-#include      <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+
+#include <errno.h>
+#include <netdb.h>
+#include <paths.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 #ifndef _PATH_RSH
-#define _PATH_RSH "/usr/bin/rsh"
+#define	_PATH_RSH	"/usr/bin/rsh"
 #endif
 
 /*
@@ -36,9 +33,8 @@ static char *rcsid = "$FreeBSD$"
 int
 rcmdsh(ahost, rport, locuser, remuser, cmd, rshprog)
 	char **ahost;
-	int rport;
-	const char *locuser, *remuser, *cmd;
-	char *rshprog;
+	int rport __unused;
+	const char *locuser, *remuser, *cmd, *rshprog;
 {
 	struct hostent *hp;
 	int cpid, sp[2];
@@ -51,41 +47,41 @@ rcmdsh(ahost, rport, locuser, remuser, cmd, rshprog)
 
 	/* locuser must exist on this host. */
 	if ((pw = getpwnam(locuser)) == NULL) {
-		(void) fprintf(stderr, "rcmdsh: unknown user: %s\n", locuser);
-		return(-1);
+		(void)fprintf(stderr, "rcmdsh: unknown user: %s\n", locuser);
+		return (-1);
 	}
 
 	/* Validate remote hostname. */
 	if (strcmp(*ahost, "localhost") != 0) {
 		if ((hp = gethostbyname(*ahost)) == NULL) {
 			herror(*ahost);
-			return(-1);
+			return (-1);
 		}
 		*ahost = hp->h_name;
 	}
 
 	/* Get a socketpair we'll use for stdin and stdout. */
-	if (socketpair(AF_UNIX, SOCK_STREAM, PF_UNSPEC, sp) < 0) {
+	if (socketpair(AF_UNIX, SOCK_STREAM, PF_UNSPEC, sp) == -1) {
 		perror("rcmdsh: socketpair");
-		return(-1);
+		return (-1);
 	}
 
 	cpid = fork();
-	if (cpid < 0) {
+	if (cpid == -1) {
 		perror("rcmdsh: fork failed");
-		return(-1);
+		return (-1);
 	} else if (cpid == 0) {
 		/*
 		 * Child.  We use sp[1] to be stdin/stdout, and close sp[0].
 		 */
-		(void) close(sp[0]);
-		if (dup2(sp[1], 0) < 0 || dup2(0, 1) < 0) {
+		(void)close(sp[0]);
+		if (dup2(sp[1], 0) == -1 || dup2(0, 1) == -1) {
 			perror("rcmdsh: dup2 failed");
 			_exit(255);
 		}
 		/* Fork again to lose parent. */
 		cpid = fork();
-		if (cpid < 0) {
+		if (cpid == -1) {
 			perror("rcmdsh: fork to lose parent failed");
 			_exit(255);
 		}
@@ -93,38 +89,39 @@ rcmdsh(ahost, rport, locuser, remuser, cmd, rshprog)
 			_exit(0);
 
 		/* In grandchild here.  Become local user for rshprog. */
-		if (setuid(pw->pw_uid)) {
-			(void) fprintf(stderr, "rcmdsh: setuid(%u): %s\n",
-				       pw->pw_uid, strerror(errno));
+		if (setuid(pw->pw_uid) == -1) {
+			(void)fprintf(stderr, "rcmdsh: setuid(%u): %s\n",
+			    pw->pw_uid, strerror(errno));
 			_exit(255);
 		}
 
 		/*
-		 * If remote host is "localhost" and local and remote user
+		 * If remote host is "localhost" and local and remote users
 		 * are the same, avoid running remote shell for efficiency.
 		 */
-		if (!strcmp(*ahost, "localhost") && !strcmp(locuser, remuser)) {
+		if (strcmp(*ahost, "localhost") == 0 &&
+		    strcmp(locuser, remuser) == 0) {
 			if (pw->pw_shell[0] == '\0')
 				rshprog = _PATH_BSHELL;
 			else
 				rshprog = pw->pw_shell;
 			p = strrchr(rshprog, '/');
-			execlp(rshprog, p ? p+1 : rshprog, "-c", cmd,
-			       (char *) NULL);
+			execlp(rshprog, p ? p + 1 : rshprog, "-c", cmd,
+			    (char *)NULL);
 		} else {
 			p = strrchr(rshprog, '/');
-			execlp(rshprog, p ? p+1 : rshprog, *ahost, "-l",
-			       remuser, cmd, (char *) NULL);
+			execlp(rshprog, p ? p + 1 : rshprog, *ahost, "-l",
+			    remuser, cmd, (char *)NULL);
 		}
-		(void) fprintf(stderr, "rcmdsh: execlp %s failed: %s\n",
-			       rshprog, strerror(errno));
+		(void)fprintf(stderr, "rcmdsh: execlp %s failed: %s\n",
+		    rshprog, strerror(errno));
 		_exit(255);
 	} else {
 		/* Parent. close sp[1], return sp[0]. */
-		(void) close(sp[1]);
+		(void)close(sp[1]);
 		/* Reap child. */
-		(void) wait(NULL);
-		return(sp[0]);
+		(void)wait(NULL);
+		return (sp[0]);
 	}
 	/* NOTREACHED */
 }
