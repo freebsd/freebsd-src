dcba1df2157c55e7fe21c0fe64ad9975bdd3eccb
tmm <tmm@FreeBSD.org>
2001-11-09 20:10:55 +0000

    Add some OpenFirmware bus support code and definitions.

---
+++ sys/sparc64/sparc64/ofw_machdep.c
@@ -0,0 +1,50 @@
+/*-
+ * Copyright (c) 2001 by Thomas Moestl <tmm@FreeBSD.org>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+/*
+ * Some OpenFirmware helper functions that are likely machine dependent.
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+
+#include <ofw/openfirm.h>
+
+#include <machine/idprom.h>
+#include <machine/ofw_machdep.h>
+
+void
+OF_getetheraddr(device_t dev, u_char *addr)
+{
+	phandle_t node;
+	struct idprom idp;
+
+	node = OF_peer(0);
+	if (node <= 0 || OF_getprop(node, "idprom", &idp, sizeof(idp)) == -1)
+		panic("Could not determine the machine ethernet address");
+	bcopy(&idp.id_ether, addr, sizeof(addr));
+}
