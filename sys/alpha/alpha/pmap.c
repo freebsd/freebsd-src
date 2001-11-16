4cd0f1ee9934af05c6f17e5f61ba64e5c1aacedb
Peter Wemm <peter@FreeBSD.org>
2001-11-16 02:02:21 +0000

    Merge part of i386/pmap.c rev 1.220 that got missed in alpha/pmap.c rev 1.10

--- sys/alpha/alpha/pmap.c
+++ sys/alpha/alpha/pmap.c
@@ -1276,11 +1276,7 @@ _pmap_unwire_pte_hold(pmap_t pmap, vm_offset_t va, vm_page_t m)
 		--m->wire_count;
 		if (m->wire_count == 0) {
 
-			if (m->flags & PG_WANTED) {
-				vm_page_flag_clear(m, PG_WANTED);
-				wakeup(m);
-			}
-
+			vm_page_flash(m);
 			vm_page_busy(m);
 			vm_page_free_zero(m);
 			--cnt.v_wire_count;
