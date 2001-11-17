6729cb8800c63ef4a9aa0b17704363ce7b61a0f1
Peter Wemm <peter@FreeBSD.org>
2001-11-17 01:38:32 +0000

    Start bringing i386/pmap.c into line with cleanups that were done to alpha pmap.  In particular - - pd_entry_t and pt_entry_t are now u_int32_t instead of a pointer.   This is to enable cleaner PAE and x86-64 support down the track sor   that we can change the pd_entry_t/pt_entry_t types to 64 bit entities. - Terminate "unsigned *ptep, pte" with extreme prejudice and use the   correct pt_entry_t/pd_entry_t types. - Various other cosmetic changes to match cleanups elsewhere. - This eliminates a boatload of casts. - use VM_MAXUSER_ADDRESS in place of UPT_MIN_ADDRESS in a couple of places   where we're testing user address space limits.  Assuming the page tables   start directly after the end of user space is not a safe assumption. There is still more to go.

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -1291,7 +1291,7 @@ getmemsize(int first)
 	struct vm86frame vmf;
 	struct vm86context vmc;
 	vm_offset_t pa, physmap[PHYSMAP_SIZE];
-	pt_entry_t pte;
+	pt_entry_t *pte;
 	const char *cp;
 	struct bios_smap *smap;
 
@@ -1331,7 +1331,7 @@ getmemsize(int first)
 	 */
 	for (pa = trunc_page(basemem * 1024);
 	     pa < ISA_HOLE_START; pa += PAGE_SIZE) {
-		pte = (pt_entry_t)vtopte(pa + KERNBASE);
+		pte = vtopte(pa + KERNBASE);
 		*pte = pa | PG_RW | PG_V;
 	}
 
@@ -1339,7 +1339,7 @@ getmemsize(int first)
 	 * if basemem != 640, map pages r/w into vm86 page table so 
 	 * that the bios can scribble on it.
 	 */
-	pte = (pt_entry_t)vm86paddr;
+	pte = (pt_entry_t *)vm86paddr;
 	for (i = basemem / 4; i < 160; i++)
 		pte[i] = (i << PAGE_SHIFT) | PG_V | PG_RW | PG_U;
 
@@ -1347,7 +1347,7 @@ getmemsize(int first)
 	 * map page 1 R/W into the kernel page table so we can use it
 	 * as a buffer.  The kernel will unmap this page later.
 	 */
-	pte = (pt_entry_t)vtopte(KERNBASE + (1 << PAGE_SHIFT));
+	pte = vtopte(KERNBASE + (1 << PAGE_SHIFT));
 	*pte = (1 << PAGE_SHIFT) | PG_RW | PG_V;
 
 	/*
@@ -1532,9 +1532,9 @@ physmap_done:
 	phys_avail[pa_indx++] = physmap[0];
 	phys_avail[pa_indx] = physmap[0];
 #if 0
-	pte = (pt_entry_t)vtopte(KERNBASE);
+	pte = vtopte(KERNBASE);
 #else
-	pte = (pt_entry_t)CMAP1;
+	pte = CMAP1;
 #endif
 
 	/*
