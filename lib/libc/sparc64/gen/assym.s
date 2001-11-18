a85982e09cffc5f78f8a86cc5ab21ae6eb2fc2e4
jake <jake@FreeBSD.org>
2001-11-18 04:35:09 +0000

    Catch up to jmpbuf changes.  Define offsets used from assmebly language in another file so that it is easy to see what they are and keep them in sync with the headers (grafted from the kernel generated assym.s).

---
+++ lib/libc/sparc64/gen/assym.s
@@ -0,0 +1,16 @@
+/*
+ * Offsets into into structures used from asm.  Must be kept in sync with
+ * appropriate headers.
+ *
+ * $FreeBSD$
+ */
+
+#define	UC_SIGMASK	0x0
+#define	UC_MC		0x10
+#define	MC_G1		0x8
+#define	MC_O0		0x40
+#define	MC_O1		0x48
+#define	MC_O6		0x70
+#define	MC_O7		0x78
+#define	MC_TNPC		0x90
+#define	MC_TPC		0x88
