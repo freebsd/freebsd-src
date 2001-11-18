41a88cc21ed205a347a753833646e12e0e9a03ba
jake <jake@FreeBSD.org>
2001-11-18 17:56:46 +0000

    Add kernel headers needed to build libc.  Some are bogus and/or just enough to compile.
    Mostly obtained from:	netbsd

---
+++ sys/sparc64/include/profile.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 1994, 1995, 1996 Carnegie-Mellon University.
+ * All rights reserved.
+ *
+ * Author: Chris G. Demetriou
+ * 
+ * Permission to use, copy, modify and distribute this software and
+ * its documentation is hereby granted, provided that both the copyright
+ * notice and this permission notice appear in all copies of the
+ * software, derivative works or modified versions, and any portions
+ * thereof, and that both notices appear in supporting documentation.
+ * 
+ * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" 
+ * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND 
+ * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+ * 
+ * Carnegie Mellon requests users of this software to return to
+ *
+ *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
+ *  School of Computer Science
+ *  Carnegie Mellon University
+ *  Pittsburgh PA 15213-3890
+ *
+ * any improvements or extensions that they make and grant Carnegie the
+ * rights to redistribute these changes.
+ *
+ *	from: NetBSD: profile.h,v 1.9 1997/04/06 08:47:37 cgd Exp
+ *	from: FreeBSD: src/sys/alpha/include/profile.h,v 1.4 1999/12/29
+ * $FreeBSD$
+ */
+
+#ifndef _MACHINE_PROFILE_H_
+#define	_MACHINE_PROFILE_H_
+
+#define	_MCOUNT_DECL	void mcount
+
+#define FUNCTION_ALIGNMENT 32
+
+typedef u_long	fptrdiff_t;
+typedef u_long	uintfptr_t;
+
+#define	MCOUNT \
+void \
+_mcount() \
+{ \
+}
+
+#ifdef _KERNEL
+#define MCOUNT_ENTER(s)
+#define MCOUNT_EXIT(s)
+#define	MCOUNT_DECL(s)
+#ifdef GUPROF
+struct gmonparam;
+
+void	nullfunc_loop_profiled __P((void));
+void	nullfunc_profiled __P((void));
+void	startguprof __P((struct gmonparam *p));
+void	stopguprof __P((struct gmonparam *p));
+#else
+#define startguprof(p)
+#define stopguprof(p)
+#endif /* GUPROF */
+#endif
+
+#endif /* !_MACHINE_PROFILE_H_ */
