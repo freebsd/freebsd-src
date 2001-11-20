db07a3da464f34a6d11573582ed94abe53c5731d
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-11-20 07:13:40 +0000

    Unifdef all the SGI code.  It mainly added clutter while providing some specialized logging, SGI-specific priority massaging, and SCI-specific time trimming support.  Also add missing $FreeBSD$'s.
    Inspired by:	NetBSD
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@86645 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/timed/timed/pathnames.h
+++ usr.sbin/timed/timed/pathnames.h
@@ -31,14 +31,11 @@
  * SUCH DAMAGE.
  *
  *	@(#)pathnames.h	8.1 (Berkeley) 6/6/93
+ *
+ * $FreeBSD$
  */
 
 #include <paths.h>
 
-#ifdef sgi
-#define	_PATH_MASTERLOG	"/usr/adm/timed.masterlog"
-#define	_PATH_TIMEDLOG	"/usr/adm/timed.log"
-#else
 #define	_PATH_MASTERLOG	"/var/log/timed.masterlog"
 #define	_PATH_TIMEDLOG	"/var/log/timed.log"
-#endif
