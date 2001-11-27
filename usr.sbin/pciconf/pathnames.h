a73032848c8880a803fcbbd9a6d6bc8248e3e231
sheldonh <sheldonh@FreeBSD.org>
2001-11-27 13:51:39 +0000

    MFC: PCI vendor and device description support (-v option)

--- usr.sbin/pciconf/pathnames.h
+++ usr.sbin/pciconf/pathnames.h
@@ -1,1 +1,3 @@
+/* $FreeBSD$ */
 #define	_PATH_DEVPCI	"/dev/pci"
+#define	_PATH_PCIVDB	"/usr/share/misc/pci_vendors"
--- usr.sbin/pciconf/pciconf.c
+++ usr.sbin/pciconf/pciconf.c
@@ -41,10 +41,34 @@ static const char rcsid[] =
 #include <string.h>
 #include <unistd.h>
 #include <sys/pciio.h>
+#include <sys/queue.h>
+
+#include <pci/pcireg.h>
 
 #include "pathnames.h"
 
-static void list_devs(void);
+struct pci_device_info 
+{
+    TAILQ_ENTRY(pci_device_info)	link;
+    int					id;
+    char				*desc;
+};
+
+struct pci_vendor_info
+{
+    TAILQ_ENTRY(pci_vendor_info)	link;
+    TAILQ_HEAD(,pci_device_info)	devs;
+    int					id;
+    char				*desc;
+};
+
+TAILQ_HEAD(,pci_vendor_info)	pci_vendors;
+
+static void list_devs(int vendors);
+static void list_verbose(struct pci_conf *p);
+static char *guess_class(struct pci_conf *p);
+static char *guess_subclass(struct pci_conf *p);
+static int load_vendors(void);
 static void readit(const char *, const char *, int);
 static void writeit(const char *, const char *, const char *, int);
 static void chkattached(const char *, int);
@@ -55,7 +79,7 @@ static void
 usage()
 {
 	fprintf(stderr, "%s\n%s\n%s\n%s\n",
-		"usage: pciconf -l",
+		"usage: pciconf -l [-v]",
 		"       pciconf -a sel",
 		"       pciconf -r [-b | -h] sel addr[:addr]",
 		"       pciconf -w [-b | -h] sel addr [value]");
@@ -66,12 +90,12 @@ int
 main(int argc, char **argv)
 {
 	int c;
-	int listmode, readmode, writemode, attachedmode;
+	int listmode, readmode, writemode, attachedmode, verbose;
 	int byte, isshort;
 
-	listmode = readmode = writemode = attachedmode = byte = isshort = 0;
+	listmode = readmode = writemode = attachedmode = verbose = byte = isshort = 0;
 
-	while ((c = getopt(argc, argv, "alrwbh")) != -1) {
+	while ((c = getopt(argc, argv, "alrwbhv")) != -1) {
 		switch(c) {
 		case 'a':
 			attachedmode = 1;
@@ -97,6 +121,10 @@ main(int argc, char **argv)
 			isshort = 1;
 			break;
 
+		case 'v':
+			verbose = 1;
+			break;
+
 		default:
 			usage();
 		}
@@ -109,7 +137,7 @@ main(int argc, char **argv)
 		usage();
 
 	if (listmode) {
-		list_devs();
+		list_devs(verbose);
 	} else if(attachedmode) {
 		chkattached(argv[optind], 
 		       byte ? 1 : isshort ? 2 : 4);
@@ -127,12 +155,15 @@ main(int argc, char **argv)
 }
 
 static void
-list_devs(void)
+list_devs(int verbose)
 {
 	int fd;
 	struct pci_conf_io pc;
 	struct pci_conf conf[255], *p;
 	int none_count = 0;
+
+	if (verbose)
+		load_vendors();
 
 	fd = open(_PATH_DEVPCI, O_RDWR, 0);
 	if (fd < 0)
@@ -179,11 +210,217 @@ list_devs(void)
 			       (p->pc_subdevice << 16) | p->pc_subvendor,
 			       (p->pc_device << 16) | p->pc_vendor,
 			       p->pc_revid, p->pc_hdr);
+			if (verbose)
+				list_verbose(p);
 		}
 	} while (pc.status == PCI_GETCONF_MORE_DEVS);
 
 	close(fd);
 }
+
+static void
+list_verbose(struct pci_conf *p)
+{
+	struct pci_vendor_info	*vi;
+	struct pci_device_info	*di;
+	char *dp;
+	
+	TAILQ_FOREACH(vi, &pci_vendors, link) {
+		if (vi->id == p->pc_vendor) {
+			printf("    vendor   = '%s'\n", vi->desc);
+			break;
+		}
+	}
+	if (vi == NULL) {
+		di = NULL;
+	} else {
+		TAILQ_FOREACH(di, &vi->devs, link) {
+			if (di->id == p->pc_device) {
+				printf("    device   = '%s'\n", di->desc);
+				break;
+			}
+		}
+	}
+	if ((dp = guess_class(p)) != NULL)
+		printf("    class    = %s\n", dp);
+	if ((dp = guess_subclass(p)) != NULL)
+		printf("    subclass = %s\n", dp);
+}
+
+/*
+ * This is a direct cut-and-paste from the table in sys/dev/pci/pci.c.
+ */
+static struct
+{
+	int	class;
+	int	subclass;
+	char	*desc;
+} pci_nomatch_tab[] = {
+	{PCIC_OLD,		-1,			"old"},
+	{PCIC_OLD,		PCIS_OLD_NONVGA,	"non-VGA display device"},
+	{PCIC_OLD,		PCIS_OLD_VGA,		"VGA-compatible display device"},
+	{PCIC_STORAGE,		-1,			"mass storage"},
+	{PCIC_STORAGE,		PCIS_STORAGE_SCSI,	"SCSI"},
+	{PCIC_STORAGE,		PCIS_STORAGE_IDE,	"ATA"},
+	{PCIC_STORAGE,		PCIS_STORAGE_FLOPPY,	"floppy disk"},
+	{PCIC_STORAGE,		PCIS_STORAGE_IPI,	"IPI"},
+	{PCIC_STORAGE,		PCIS_STORAGE_RAID,	"RAID"},
+	{PCIC_NETWORK,		-1,			"network"},
+	{PCIC_NETWORK,		PCIS_NETWORK_ETHERNET,	"ethernet"},
+	{PCIC_NETWORK,		PCIS_NETWORK_TOKENRING,	"token ring"},
+	{PCIC_NETWORK,		PCIS_NETWORK_FDDI,	"fddi"},
+	{PCIC_NETWORK,		PCIS_NETWORK_ATM,	"ATM"},
+	{PCIC_DISPLAY,		-1,			"display"},
+	{PCIC_DISPLAY,		PCIS_DISPLAY_VGA,	"VGA"},
+	{PCIC_DISPLAY,		PCIS_DISPLAY_XGA,	"XGA"},
+	{PCIC_MULTIMEDIA,	-1,			"multimedia"},
+	{PCIC_MULTIMEDIA,	PCIS_MULTIMEDIA_VIDEO,	"video"},
+	{PCIC_MULTIMEDIA,	PCIS_MULTIMEDIA_AUDIO,	"audio"},
+	{PCIC_MEMORY,		-1,			"memory"},
+	{PCIC_MEMORY,		PCIS_MEMORY_RAM,	"RAM"},
+	{PCIC_MEMORY,		PCIS_MEMORY_FLASH,	"flash"},
+	{PCIC_BRIDGE,		-1,			"bridge"},
+	{PCIC_BRIDGE,		PCIS_BRIDGE_HOST,	"HOST-PCI"},
+	{PCIC_BRIDGE,		PCIS_BRIDGE_ISA,	"PCI-ISA"},
+	{PCIC_BRIDGE,		PCIS_BRIDGE_EISA,	"PCI-EISA"},
+	{PCIC_BRIDGE,		PCIS_BRIDGE_MCA,	"PCI-MCA"},
+	{PCIC_BRIDGE,		PCIS_BRIDGE_PCI,	"PCI-PCI"},
+	{PCIC_BRIDGE,		PCIS_BRIDGE_PCMCIA,	"PCI-PCMCIA"},
+	{PCIC_BRIDGE,		PCIS_BRIDGE_NUBUS,	"PCI-NuBus"},
+	{PCIC_BRIDGE,		PCIS_BRIDGE_CARDBUS,	"PCI-CardBus"},
+	{PCIC_BRIDGE,		PCIS_BRIDGE_OTHER,	"PCI-unknown"},
+	{PCIC_SIMPLECOMM,	-1,			"simple comms"},
+	{PCIC_SIMPLECOMM,	PCIS_SIMPLECOMM_UART,	"UART"},	/* could detect 16550 */
+	{PCIC_SIMPLECOMM,	PCIS_SIMPLECOMM_PAR,	"parallel port"},
+	{PCIC_BASEPERIPH,	-1,			"base peripheral"},
+	{PCIC_BASEPERIPH,	PCIS_BASEPERIPH_PIC,	"interrupt controller"},
+	{PCIC_BASEPERIPH,	PCIS_BASEPERIPH_DMA,	"DMA controller"},
+	{PCIC_BASEPERIPH,	PCIS_BASEPERIPH_TIMER,	"timer"},
+	{PCIC_BASEPERIPH,	PCIS_BASEPERIPH_RTC,	"realtime clock"},
+	{PCIC_INPUTDEV,		-1,			"input device"},
+	{PCIC_INPUTDEV,		PCIS_INPUTDEV_KEYBOARD,	"keyboard"},
+	{PCIC_INPUTDEV,		PCIS_INPUTDEV_DIGITIZER,"digitizer"},
+	{PCIC_INPUTDEV,		PCIS_INPUTDEV_MOUSE,	"mouse"},
+	{PCIC_DOCKING,		-1,			"docking station"},
+	{PCIC_PROCESSOR,	-1,			"processor"},
+	{PCIC_SERIALBUS,	-1,			"serial bus"},
+	{PCIC_SERIALBUS,	PCIS_SERIALBUS_FW,	"FireWire"},
+	{PCIC_SERIALBUS,	PCIS_SERIALBUS_ACCESS,	"AccessBus"},	 
+	{PCIC_SERIALBUS,	PCIS_SERIALBUS_SSA,	"SSA"},
+	{PCIC_SERIALBUS,	PCIS_SERIALBUS_USB,	"USB"},
+	{PCIC_SERIALBUS,	PCIS_SERIALBUS_FC,	"Fibre Channel"},
+	{PCIC_SERIALBUS,	PCIS_SERIALBUS_SMBUS,	"SMBus"},
+	{0, 0,		NULL}
+};
+
+static char *
+guess_class(struct pci_conf *p)
+{
+	int	i;
+
+	for (i = 0; pci_nomatch_tab[i].desc != NULL; i++) {
+		if (pci_nomatch_tab[i].class == p->pc_class)
+			return(pci_nomatch_tab[i].desc);
+	}
+	return(NULL);
+}
+
+static char *
+guess_subclass(struct pci_conf *p)
+{
+	int	i;
+
+	for (i = 0; pci_nomatch_tab[i].desc != NULL; i++) {
+		if ((pci_nomatch_tab[i].class == p->pc_class) &&
+		    (pci_nomatch_tab[i].subclass == p->pc_subclass))
+			return(pci_nomatch_tab[i].desc);
+	}
+	return(NULL);
+}
+
+static int
+load_vendors(void)
+{
+	char *dbf;
+	FILE *db;
+	struct pci_vendor_info *cv;
+	struct pci_device_info *cd;
+	char buf[100], str[100];
+	int id, error;
+
+	/*
+	 * Locate the database and initialise.
+	 */
+	TAILQ_INIT(&pci_vendors);
+	if ((dbf = getenv("PCICONF_VENDOR_DATABASE")) == NULL)
+		dbf = _PATH_PCIVDB;
+	if ((db = fopen(dbf, "r")) == NULL)
+		return(1);
+	cv = NULL;
+	cd = NULL;
+	error = 0;
+
+	/*
+	 * Scan input lines from the database
+	 */
+	for (;;) {
+		if (fgets(buf, sizeof(buf), db) == NULL)
+			break;
+
+		/* Check for vendor entry */
+		if ((buf[0] != '\t') && (sscanf(buf, "%04x\t%[^\n]", &id, str) == 2)) {
+			if ((id == 0) || (strlen(str) < 1))
+				continue;
+			if ((cv = malloc(sizeof(struct pci_vendor_info))) == NULL) {
+				warn("allocating vendor entry");
+				error = 1;
+				break;
+			}
+			if ((cv->desc = strdup(str)) == NULL) {
+				free(cv);
+				warn("allocating vendor description");
+				error = 1;
+				break;
+			}
+			cv->id = id;
+			TAILQ_INIT(&cv->devs);
+			TAILQ_INSERT_TAIL(&pci_vendors, cv, link);
+			continue;
+		}
+		
+		/* Check for device entry */
+		if ((buf[0] == '\t') && (sscanf(buf + 1, "%04x\t%[^\n]", &id, str) == 2)) {
+			if ((id == 0) || (strlen(str) < 1))
+				continue;
+			if (cv == NULL) {
+				warnx("device entry with no vendor!");
+				continue;
+			}
+			if ((cd = malloc(sizeof(struct pci_device_info))) == NULL) {
+				warn("allocating device entry");
+				error = 1;
+				break;
+			}
+			if ((cd->desc = strdup(str)) == NULL) {
+				free(cd);
+				warn("allocating device description");
+				error = 1;
+				break;
+			}
+			cd->id = id;
+			TAILQ_INSERT_TAIL(&cv->devs, cd, link);
+			continue;
+		}
+
+		/* It's a comment or junk, ignore it */
+	}
+	if (ferror(db))
+		error = 1;
+	fclose(db);
+	
+	return(error);
+}
+
 
 static struct pcisel
 getsel(const char *str)
