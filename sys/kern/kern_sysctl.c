9bde9167adff08ace0dbeef7926c110c632e7151
peter <peter@FreeBSD.org>
2001-11-28 03:11:16 +0000

    Dont print the sysctl node tree unless you're root.
    Found by:	jkb (Yahoo OS troublemaker)

--- sys/kern/kern_sysctl.c
+++ sys/kern/kern_sysctl.c
@@ -452,6 +452,11 @@ sysctl_sysctl_debug_dump_node(struct sysctl_oid_list *l, int i)
 static int
 sysctl_sysctl_debug(SYSCTL_HANDLER_ARGS)
 {
+	int error;
+
+	error = suser(req->td->td_proc);
+	if (error)
+		return error;
 	sysctl_sysctl_debug_dump_node(&sysctl__children, 0);
 	return ENOENT;
 }
