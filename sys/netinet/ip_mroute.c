ccc50e905fffcd3b8041feb5add63de753f31e94
ru <ru@FreeBSD.org>
2001-12-01 13:48:16 +0000

    Allow for ip_output() to be called with a NULL route pointer. This fixes a panic I introduced yesterday in ip_icmp.c,v 1.64.

--- sys/netinet/ip_mroute.c
+++ sys/netinet/ip_mroute.c
@@ -1867,7 +1867,6 @@ tbf_send_packet(vifp, m)
 {
     struct ip_moptions imo;
     int error;
-    static struct route ro;
     int s = splnet();
 
     if (vifp->v_flags & VIFF_TUNNEL) {
@@ -1886,7 +1885,7 @@ tbf_send_packet(vifp, m)
 	 * should get rejected because they appear to come from
 	 * the loopback interface, thus preventing looping.
 	 */
-	error = ip_output(m, (struct mbuf *)0, &ro,
+	error = ip_output(m, (struct mbuf *)0, NULL,
 			  IP_FORWARDING, &imo);
 
 	if (mrtdebug & DEBUG_XMIT)
