f906e265a66ec7b0676e11b20dffdc20838fe1a0
Mark Murray <markm@FreeBSD.org>
2001-12-01 17:34:42 +0000

    WARNS=2 and s/register//.
    Set NO_WERROR=true because there are some flex(1) issues that need
    to be dealt with.

--- usr.bin/yacc/warshall.c
+++ usr.bin/yacc/warshall.c
@@ -49,15 +49,15 @@ transitive_closure(R, n)
 unsigned *R;
 int n;
 {
-    register int rowsize;
-    register unsigned i;
-    register unsigned *rowj;
-    register unsigned *rp;
-    register unsigned *rend;
-    register unsigned *ccol;
-    register unsigned *relend;
-    register unsigned *cword;
-    register unsigned *rowi;
+    int rowsize;
+    unsigned i;
+    unsigned *rowj;
+    unsigned *rp;
+    unsigned *rend;
+    unsigned *ccol;
+    unsigned *relend;
+    unsigned *cword;
+    unsigned *rowi;
 
     rowsize = WORDSIZE(n);
     relend = R + n*rowsize;
@@ -102,10 +102,10 @@ reflexive_transitive_closure(R, n)
 unsigned *R;
 int n;
 {
-    register int rowsize;
-    register unsigned i;
-    register unsigned *rp;
-    register unsigned *relend;
+    int rowsize;
+    unsigned i;
+    unsigned *rp;
+    unsigned *relend;
 
     transitive_closure(R, n);
 
