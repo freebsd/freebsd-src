7708beb45d27b7a0f2cf5156ac47e4d38c37bfb9
markm <markm@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-12-02 22:46:47 +0000

    Use __FBSDID().
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@87236 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/cap_mkdb/cap_mkdb.c
+++ usr.bin/cap_mkdb/cap_mkdb.c
@@ -31,19 +31,19 @@
  * SUCH DAMAGE.
  */
 
+#include <sys/cdefs.h>
+
+__FBSDID("$FreeBSD$");
+
 #ifndef lint
 static const char copyright[] =
 "@(#) Copyright (c) 1992, 1993\n\
 	The Regents of the University of California.  All rights reserved.\n";
-#endif /* not lint */
+#endif
 
 #ifndef lint
-#if 0
-static char sccsid[] = "@(#)cap_mkdb.c	8.1 (Berkeley) 6/6/93";
+static const char sccsid[] = "@(#)cap_mkdb.c	8.1 (Berkeley) 6/6/93";
 #endif
-static const char rcsid[] =
-	"$FreeBSD$";
-#endif /* not lint */
 
 #include <sys/param.h>
 #include <sys/stat.h>
