7d8c30fa44f83749bd1c0a3f0d19bbbea5685294
sos <sos@FreeBSD.org>
2001-12-02 12:19:16 +0000

    ARG! committed the wrong patch!! I need more sleep :(

--- sys/dev/ata/ata-pci.c
+++ sys/dev/ata/ata-pci.c
@@ -75,7 +75,7 @@ ata_find_dev(device_t dev, u_int32_t devid, u_int32_t revid)
 	return 0;
 
     for (i = 0; i < nchildren; i++) {
-	if (pci_get_slot(dev) == pci_get_slot(child) &&
+	if (pci_get_slot(dev) == pci_get_slot(children[i]) &&
 	    pci_get_devid(children[i]) == devid &&
 	    pci_get_revid(children[i]) >= revid) {
 	    free(children, M_TEMP);
