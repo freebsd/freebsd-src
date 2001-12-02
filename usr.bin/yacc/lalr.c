c73844e02d229e510b00a5df2f361eb43b38c8de
markm <markm@FreeBSD.org>
2001-12-02 21:24:03 +0000

    Use __FBSDID().

--- usr.bin/yacc/lalr.c
+++ usr.bin/yacc/lalr.c
@@ -32,13 +32,15 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $FreeBSD$
  */
+
+#include <sys/cdefs.h>
+
+__FBSDID("$FreeBSD$");
 
 #ifndef lint
 static char const sccsid[] = "@(#)lalr.c	5.3 (Berkeley) 6/1/90";
-#endif /* not lint */
+#endif
 
 #include <stdlib.h>
 #include "defs.h"
