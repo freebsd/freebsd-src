9b3e907904bcfe704328b37fdf3bf0980290f934
Mark Murray <markm@FreeBSD.org>
2001-12-02 13:55:09 +0000

    WARNS=2 fixup.

--- usr.bin/false/false.c
+++ usr.bin/false/false.c
@@ -29,19 +29,22 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ * $FreeBSD$
  */
 
 #ifndef lint
-static char copyright[] =
+static const char copyright[] =
 "@(#) Copyright (c) 1988, 1993\n\
 	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)false.c	8.1 (Berkeley) 6/6/93";
+static const char sccsid[] = "@(#)false.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
+int
 main()
 {
-	exit(1);
+	return 1;
 }
