eb5bc300e8404bf52f6c6a17aea717f6c4084757
Jacques Vidrine <nectar@FreeBSD.org>
2001-12-03 22:47:51 +0000

    Update version string since we applied a fix for the UseLogin issue.

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -5,7 +5,7 @@
 
 #define SSH_VERSION		(ssh_version_get())
 #define SSH_VERSION_BASE	"OpenSSH_2.9"
-#define	SSH_VERSION_ADDENDUM	"FreeBSD localisations 20010713"
+#define	SSH_VERSION_ADDENDUM	"FreeBSD localisations 20011202"
 
 const char *ssh_version_get(void);
 void ssh_version_set_addendum(const char *add);
