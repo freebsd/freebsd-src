0fbda1fc584f2acddf3d5b22a775186b5a53334e
Dag-Erling Smørgrav <des@FreeBSD.org>
2001-12-05 17:05:15 +0000

    Fix a bug that made PAM fail if /etc/pam.d/ existed but was empty.
    Sponsored by:	DARPA, NAI Labs

--- contrib/libpam/libpam/pam_handlers.c
+++ contrib/libpam/libpam/pam_handlers.c
@@ -5,6 +5,42 @@
  * Currently maintained by Andrew G. Morgan <morgan@linux.kernel.org>
  *
  * $Id: pam_handlers.c,v 1.3 2001/02/05 06:50:41 agmorgan Exp $
+ *
+ */
+
+/*-
+ * Copyright (c) 2001 Networks Associates Technologies, Inc.
+ * All rights reserved.
+ *
+ * Portions of this software was developed for the FreeBSD Project by
+ * ThinkSec AS and NAI Labs, the Security Research Division of Network
+ * Associates, Inc.  under DARPA/SPAWAR contract N66001-01-C-8035
+ * ("CBOSS"), as part of the DARPA CHATS research program.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
  * $FreeBSD$
  *
  */
@@ -317,7 +353,17 @@ int _pam_init_handlers(pam_handle_t *pamh)
 		if ((f = fopen(PAM_CONFIG,"r")) != NULL) {
 		    retval = _pam_parse_conf_file(pamh, f, NULL, 1);
 		    fclose(f);
+		    if (retval != PAM_SUCCESS) {
+			    _pam_system_log(LOG_ERR, "_pam_init_handlers: "
+					    "error reading %s", PAM_CONFIG);
+			    _pam_system_log(LOG_ERR, "_pam_init_handlers: [%s]",
+					    pam_strerror(pamh, retval));
+		    } else {
+			    read_something = 1;
+		    }
 		} else
+			_pam_system_log(LOG_ERR, "_pam_init_handlers: "
+					"could not open " PAM_CONFIG);
 #endif /* PAM_READ_BOTH_CONFS */
 		retval = PAM_SUCCESS;
 		/*
