22043aad6700965c760a4d89c1da87f18dfa88ba
jdp <jdp@FreeBSD.org>
2001-12-08 19:07:51 +0000

    Increase the maximum message queue length to 256, and increase the number of cached message structs to the same number.  Modern NICs (especially gigabit NICs) often have 64-256 or more receive packet descriptors, so they can dump a lot of packets into the network stack at once.  With the old message queue limit, it was easy to overflow the queue when using the ng_ether node.
    I am committing this directly to RELENG_4.  The queuing mechanism is
    quite different in -current, and these limits don't apply there.

--- sys/netgraph/ng_base.c
+++ sys/netgraph/ng_base.c
@@ -1832,11 +1832,11 @@ struct ng_queue_entry {
 
 static struct ng_queue_entry   *ngqbase;	/* items to be unqueued */
 static struct ng_queue_entry   *ngqlast;	/* last item queued */
-static const int		ngqroom = 64;	/* max items to queue */
+static const int		ngqroom = 256;	/* max items to queue */
 static int			ngqsize;	/* number of items in queue */
 
 static struct ng_queue_entry   *ngqfree;	/* free ones */
-static const int		ngqfreemax = 16;/* cache at most this many */
+static const int		ngqfreemax = 256;/* cache at most this many */
 static int			ngqfreesize;	/* number of cached entries */
 
 /*
