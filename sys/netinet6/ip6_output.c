915c6a9163411e9ef9369d91dfac3efc96aadc1e
arr <arr@FreeBSD.org>
2001-12-09 17:48:08 +0000

    - Replace M_WAIT with M_TRYWAIT since the M_WAIT flag is deprecated.
    Spotted by: bde

--- sys/netinet6/ip6_output.c
+++ sys/netinet6/ip6_output.c
@@ -1540,7 +1540,7 @@ do { \
 				if (in6p->in6p_options) {
 					struct mbuf *m;
 					m = m_copym(in6p->in6p_options,
-					    0, M_COPYALL, M_WAIT);
+					    0, M_COPYALL, M_TRYWAIT);
 					error = soopt_mcopyout(sopt, m);
 					if (error == 0)
 						m_freem(m);
