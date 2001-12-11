ffe6184ecfaad47ebb41e33373284dfe6515740d
markm <markm@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-12-11 23:38:24 +0000

    WARNS=2 is going to be the default, so don't specify it. Use __FBSDID().
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@87706 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/touch/touch.c
+++ usr.bin/touch/touch.c
@@ -31,19 +31,19 @@
  * SUCH DAMAGE.
  */
 
+#include <sys/cdefs.h>
+
+__FBSDID("$FreeBSD$");
+
 #ifndef lint
 static const char copyright[] =
 "@(#) Copyright (c) 1993\n\
 	The Regents of the University of California.  All rights reserved.\n";
-#endif /* not lint */
+#endif
 
 #ifndef lint
-#if 0
 static const char sccsid[] = "@(#)touch.c	8.1 (Berkeley) 6/6/93";
 #endif
-static const char rcsid[] =
-  "$FreeBSD$";
-#endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/stat.h>
