b1d0b77b84d703166bc228e1249b3b0cb2eb118c
charnier <charnier@FreeBSD.org>
2001-12-12 18:22:09 +0000

    Start option definition with: ``The following options are available'' Use .Ev for env variables. Remove unused #includes, and sort others.

--- usr.bin/biff/biff.c
+++ usr.bin/biff/biff.c
@@ -46,14 +46,10 @@ static char sccsid[] = "@(#)biff.c	8.1 (Berkeley) 6/6/93";
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <errno.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <err.h>
+#include <stdio.h>
+#include <unistd.h>
 
 static void usage __P((void));
 
