baaef98bf547afa3b56986d87bbb5bb3f9a596f7
jlemon <jlemon@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-12-14 19:33:29 +0000

    minor style and whitespace fixes.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@87915 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/ip_input.c
+++ sys/netinet/ip_input.c
@@ -450,20 +450,21 @@ iphack:
 		 * See the comment in ip_output for the return values
 		 * produced by the firewall.
 		 */
-		i = ip_fw_chk_ptr(&ip,
-		    hlen, NULL, &divert_cookie, &m, &rule, &ip_fw_fwd_addr);
+		i = ip_fw_chk_ptr(&ip, hlen, NULL,
+		    &divert_cookie, &m, &rule, &ip_fw_fwd_addr);
 		if (i & IP_FW_PORT_DENY_FLAG) { /* XXX new interface-denied */
-		    if (m)
-			m_freem(m);
-		    return ;
+			if (m)
+				m_freem(m);
+			return;
 		}
 		if (m == NULL) {	/* Packet discarded by firewall */
-		    static int __debug=10;
-		    if (__debug >0) {
-			printf("firewall returns NULL, please update!\n");	
-			__debug-- ;
-		    }
-		    return;
+			static int __debug=10;
+			if (__debug > 0) {
+				printf(
+				    "firewall returns NULL, please update!\n");	
+				__debug--;
+			}
+			return;
 		}
 		if (i == 0 && ip_fw_fwd_addr == NULL)	/* common case */
 			goto pass;
