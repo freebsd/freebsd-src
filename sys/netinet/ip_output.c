bbcde00a9dec611dbebb6c0a8ff316cc255b1e02
jlemon <jlemon@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2001-12-14 19:34:11 +0000

    whitespace and style fixes recovered from -stable.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@87916 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/ip_output.c
+++ sys/netinet/ip_output.c
@@ -596,48 +596,50 @@ skip_ipsec:
                 /*
                  * On return we must do the following:
                  * m == NULL         -> drop the pkt (old interface, deprecated)
-                 * (off & 0x40000)   -> drop the pkt (new interface)
-                 * 1<=off<= 0xffff   -> DIVERT
-                 * (off & 0x10000)   -> send to a DUMMYNET pipe
-                 * (off & 0x20000)   -> TEE the packet
-                 * dst != old        -> IPFIREWALL_FORWARD
-                 * off==0, dst==old  -> accept
-                 * If some of the above modules is not compiled in, then
+                 * (off & IP_FW_PORT_DENY_FLAG)	-> drop the pkt (new interface)
+                 * 1<=off<= 0xffff		-> DIVERT
+                 * (off & IP_FW_PORT_DYNT_FLAG)	-> send to a DUMMYNET pipe
+                 * (off & IP_FW_PORT_TEE_FLAG)	-> TEE the packet
+                 * dst != old			-> IPFIREWALL_FORWARD
+                 * off==0, dst==old		-> accept
+                 * If some of the above modules are not compiled in, then
                  * we should't have to check the corresponding condition
                  * (because the ipfw control socket should not accept
                  * unsupported rules), but better play safe and drop
                  * packets in case of doubt.
                  */
 		if (off & IP_FW_PORT_DENY_FLAG) { /* XXX new interface-denied */
-		    if (m)
-			m_freem(m);
-		    error = EACCES ;
-		    goto done;
+			if (m)
+				m_freem(m);
+			error = EACCES;
+			goto done;
 		}
-		if (!m) { /* firewall said to reject */
-		    static int __debug=10;
-		    if (__debug >0) {
-			printf("firewall returns NULL, please update!\n");	
-			__debug-- ;
-		    }
-		    error = EACCES;
-		    goto done;
+		if (!m) {			/* firewall said to reject */
+			static int __debug=10;
+
+			if (__debug > 0) {
+				printf(
+				    "firewall returns NULL, please update!\n");
+				__debug--;
+			}
+			error = EACCES;
+			goto done;
 		}
-		if (off == 0 && dst == old) /* common case */
-			goto pass ;
+		if (off == 0 && dst == old)		/* common case */
+			goto pass;
                 if (DUMMYNET_LOADED && (off & IP_FW_PORT_DYNT_FLAG) != 0) {
-                    /*
-                     * pass the pkt to dummynet. Need to include
-                     * pipe number, m, ifp, ro, dst because these are
-                     * not recomputed in the next pass.
-                     * All other parameters have been already used and
-                     * so they are not needed anymore. 
-                     * XXX note: if the ifp or ro entry are deleted
-                     * while a pkt is in dummynet, we are in trouble!
-                     */ 
-		    error = ip_dn_io_ptr(off & 0xffff, DN_TO_IP_OUT, m,
-				ifp,ro,dst,rule, flags);
-		    goto done;
+			/*
+			 * pass the pkt to dummynet. Need to include
+			 * pipe number, m, ifp, ro, dst because these are
+			 * not recomputed in the next pass.
+			 * All other parameters have been already used and
+			 * so they are not needed anymore. 
+			 * XXX note: if the ifp or ro entry are deleted
+			 * while a pkt is in dummynet, we are in trouble!
+			 */ 
+			error = ip_dn_io_ptr(off & 0xffff, DN_TO_IP_OUT, m,
+			    ifp, ro, dst, rule, flags);
+			goto done;
 		}
 #ifdef IPDIVERT
 		if (off != 0 && (off & IP_FW_PORT_DYNT_FLAG) == 0) {
