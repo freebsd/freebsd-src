b9bc94b33fd10ea1008f0173d35dfe1bad9f7595
Archie Cobbs <archie@FreeBSD.org>
2001-12-15 20:53:15 +0000

    Typo.

--- sys/netgraph/ng_bridge.c
+++ sys/netgraph/ng_bridge.c
@@ -973,7 +973,7 @@ ng_bridge_remove_hosts(priv_p priv, int linkNum)
  * a detected loopback condition, and we remove any hosts from
  * the hashtable whom we haven't heard from in a long while.
  *
- * If the node has the NG_INVALID flag set, out job is to kill it.
+ * If the node has the NG_INVALID flag set, our job is to kill it.
  */
 static void
 ng_bridge_timeout(void *arg)
