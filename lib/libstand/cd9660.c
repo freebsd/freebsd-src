8c99ee67774894d3ad130425c19215c29cf4df96
John Baldwin <jhb@FreeBSD.org>
2001-12-21 22:17:44 +0000

    MFC: Revs 1.9 - 1.11: - Use ANSI function declarations and prototypes. - Add a simple SUSP parser. - Add RockRidge support when looking up names as well as in readdir().
    Approved by:	re

--- lib/libstand/cd9660.c
+++ lib/libstand/cd9660.c
@@ -42,16 +42,42 @@
 #include <string.h>
 #include <sys/dirent.h>
 #include <isofs/cd9660/iso.h>
+#include <isofs/cd9660/cd9660_rrip.h>
 
 #include "stand.h"
 
+#define	SUSP_CONTINUATION	"CE"
+#define	SUSP_PRESENT		"SP"
+#define	SUSP_STOP		"ST"
+#define	SUSP_EXTREF		"ER"
+#define	RRIP_NAME		"NM"
+
+typedef struct {
+	ISO_SUSP_HEADER		h;
+	u_char signature	[ISODCL (  5,    6)];
+	u_char len_skp		[ISODCL (  7,    7)]; /* 711 */
+} ISO_SUSP_PRESENT;
+	
+static int	buf_read_file(struct open_file *f, char **buf_p,
+		    size_t *size_p);
 static int	cd9660_open(const char *path, struct open_file *f);
 static int	cd9660_close(struct open_file *f);
-static int	cd9660_read(struct open_file *f, void *buf, size_t size, size_t *resid);
-static int	cd9660_write(struct open_file *f, void *buf, size_t size, size_t *resid);
+static int	cd9660_read(struct open_file *f, void *buf, size_t size,
+		    size_t *resid);
+static int	cd9660_write(struct open_file *f, void *buf, size_t size,
+		    size_t *resid);
 static off_t	cd9660_seek(struct open_file *f, off_t offset, int where);
 static int	cd9660_stat(struct open_file *f, struct stat *sb);
 static int	cd9660_readdir(struct open_file *f, struct dirent *d);
+static int	dirmatch(struct open_file *f, const char *path,
+		    struct iso_directory_record *dp, int use_rrip, int lenskip);
+static int	rrip_check(struct open_file *f, struct iso_directory_record *dp,
+		    int *lenskip);
+static char	*rrip_lookup_name(struct open_file *f,
+		    struct iso_directory_record *dp, int lenskip, size_t *len);
+static ISO_SUSP_HEADER *susp_lookup_record(struct open_file *f,
+		    const char *identifier, struct iso_directory_record *dp,
+		    int lenskip);
 
 struct fs_ops cd9660_fsops = {
 	"cd9660",
@@ -64,13 +90,18 @@ struct fs_ops cd9660_fsops = {
 	cd9660_readdir
 };
 
+#define	F_ISDIR		0x0001		/* Directory */
+#define	F_ROOTDIR	0x0002		/* Root directory */
+#define	F_RR		0x0004		/* Rock Ridge on this volume */
+
 struct file {
-	int 		f_isdir;	/* nonzero if file is directory */
+	int 		f_flags;	/* file flags */
 	off_t 		f_off;		/* Current offset within file */
 	daddr_t 	f_bno;		/* Starting block number */
 	off_t 		f_size;		/* Size of file */
 	daddr_t		f_buf_blkno;	/* block number of data block */	
 	char		*f_buf;		/* buffer for data block */
+	int		f_susp_skip;	/* len_skip for SUSP records */
 };
 
 struct ptable_ent {
@@ -100,19 +131,132 @@ isonum_732(p)
 	return (*p << 24)|(p[1] << 16)|(p[2] << 8)|p[3];
 }
 
-static int
-dirmatch(path, dp)
-	const char *path;
-	struct iso_directory_record *dp;
+static ISO_SUSP_HEADER *
+susp_lookup_record(struct open_file *f, const char *identifier,
+    struct iso_directory_record *dp, int lenskip)
 {
-	char *cp;
-	int i;
+	static char susp_buffer[ISO_DEFAULT_BLOCK_SIZE];
+	ISO_SUSP_HEADER *sh;
+	ISO_RRIP_CONT *shc;
+	char *p, *end;
+	int error;
+	size_t read;
 
-	cp = dp->name;
-	for (i = isonum_711(dp->name_len); --i >= 0; path++, cp++) {
+	p = dp->name + isonum_711(dp->name_len) + lenskip;
+	/* Names of even length have a padding byte after the name. */
+	if ((isonum_711(dp->name_len) & 1) == 0)
+		p++;
+	end = (char *)dp + isonum_711(dp->length);
+	while (p + 3 < end) {
+		sh = (ISO_SUSP_HEADER *)p;
+		if (bcmp(sh->type, identifier, 2) == 0)
+			return (sh);
+		if (bcmp(sh->type, SUSP_STOP, 2) == 0)
+			return (NULL);
+		if (bcmp(sh->type, SUSP_CONTINUATION, 2) == 0) {
+			shc = (ISO_RRIP_CONT *)sh;
+			error = f->f_dev->dv_strategy(f->f_devdata, F_READ,
+			    cdb2devb(isonum_733(shc->location)),
+			    ISO_DEFAULT_BLOCK_SIZE, susp_buffer, &read);
+
+			/* Bail if it fails. */
+			if (error != 0 || read != ISO_DEFAULT_BLOCK_SIZE)
+				return (NULL);
+			p = susp_buffer + isonum_733(shc->offset);
+			end = p + isonum_733(shc->length);
+		} else
+			/* Ignore this record and skip to the next. */
+			p += isonum_711(sh->length);
+	}
+	return (NULL);
+}
+
+static char *
+rrip_lookup_name(struct open_file *f, struct iso_directory_record *dp,
+    int lenskip, size_t *len)
+{
+	ISO_RRIP_ALTNAME *p;
+
+	if (len == NULL)
+		return (NULL);
+
+	p = (ISO_RRIP_ALTNAME *)susp_lookup_record(f, RRIP_NAME, dp, lenskip);
+	if (p == NULL)
+		return (NULL);
+	switch (*p->flags) {
+	case ISO_SUSP_CFLAG_CURRENT:
+		*len = 1;
+		return (".");
+	case ISO_SUSP_CFLAG_PARENT:
+		*len = 2;
+		return ("..");
+	case 0:
+		*len = isonum_711(p->h.length) - 5;
+		return ((char *)p + 5);
+	default:
+		/*
+		 * We don't handle hostnames or continued names as they are
+		 * too hard, so just bail and use the default name.
+		 */
+		return (NULL);
+	}
+}
+
+static int
+rrip_check(struct open_file *f, struct iso_directory_record *dp, int *lenskip)
+{
+	ISO_SUSP_PRESENT *sp;
+	ISO_RRIP_EXTREF *er;
+	char *p;
+
+	/* First, see if we can find a SP field. */
+	p = dp->name + isonum_711(dp->name_len);
+	if (p > (char *)dp + isonum_711(dp->length))
+		return (0);
+	sp = (ISO_SUSP_PRESENT *)p;
+	if (bcmp(sp->h.type, SUSP_PRESENT, 2) != 0)
+		return (0);
+	if (isonum_711(sp->h.length) != sizeof(ISO_SUSP_PRESENT))
+		return (0);
+	if (sp->signature[0] != 0xbe || sp->signature[1] != 0xef)
+		return (0);
+	*lenskip = isonum_711(sp->len_skp);
+
+	/*
+	 * Now look for an ER field.  If RRIP is present, then there must
+	 * be at least one of these.  It would be more pedantic to walk
+	 * through the list of fields looking for a Rock Ridge ER field.
+	 */
+	er = (ISO_RRIP_EXTREF *)susp_lookup_record(f, SUSP_EXTREF, dp, 0);
+	if (er == NULL)
+		return (0);
+	return (1);
+}
+
+static int
+dirmatch(struct open_file *f, const char *path, struct iso_directory_record *dp,
+    int use_rrip, int lenskip)
+{
+	size_t len;
+	char *cp;
+	int i, icase;
+
+	if (use_rrip)
+		cp = rrip_lookup_name(f, dp, lenskip, &len);
+	else
+		cp = NULL;
+	if (cp == NULL) {
+		len = isonum_711(dp->name_len);
+		cp = dp->name;
+		icase = 1;
+	} else
+		icase = 0;
+	for (i = len; --i >= 0; path++, cp++) {
 		if (!*path || *path == '/')
 			break;
-		if (toupper(*path) == *cp)
+		if (*path == *cp)
+			continue;
+		if (!icase && toupper(*path) == *cp)
 			continue;
 		return 0;
 	}
@@ -135,9 +279,7 @@ dirmatch(path, dp)
 }
 
 static int
-cd9660_open(path, f)
-	const char *path;
-	struct open_file *f;
+cd9660_open(const char *path, struct open_file *f)
 {
 	struct file *fp = 0;
 	void *buf;
@@ -146,7 +288,7 @@ cd9660_open(path, f)
 	daddr_t bno, boff;
 	struct iso_directory_record rec;
 	struct iso_directory_record *dp = 0;
-	int rc;
+	int rc, first, use_rrip, lenskip;
 
 	/* First find the volume descriptor */
 	buf = malloc(buf_size = ISO_DEFAULT_BLOCK_SIZE);
@@ -175,6 +317,8 @@ cd9660_open(path, f)
 	rec = *(struct iso_directory_record *) vd->root_directory_record;
 	if (*path == '/') path++; /* eat leading '/' */
 
+	first = 1;
+	use_rrip = 0;
 	while (*path) {
 		bno = isonum_733(rec.extent) + isonum_711(rec.ext_attr_length);
 		dsize = isonum_733(rec.size);
@@ -204,8 +348,16 @@ cd9660_open(path, f)
 			    continue;
 			}
 
-			if (dirmatch(path, dp))
+			/* See if RRIP is in use. */
+			if (first)
+				use_rrip = rrip_check(f, dp, &lenskip);
+
+			if (dirmatch(f, path, dp, use_rrip,
+				first ? 0 : lenskip)) {
+				first = 0;
 				break;
+			} else
+				first = 0;
 
 			dp = (struct iso_directory_record *)
 				((char *) dp + isonum_711(dp->length));
@@ -227,7 +379,30 @@ cd9660_open(path, f)
 	bzero(fp, sizeof(struct file));
 	f->f_fsdata = (void *)fp;
 
-	fp->f_isdir = (isonum_711(rec.flags) & 2) != 0;
+	if ((isonum_711(rec.flags) & 2) != 0) {
+		fp->f_flags = F_ISDIR;
+	}
+	if (first) {
+		fp->f_flags |= F_ROOTDIR;
+
+		/* Check for Rock Ridge since we didn't in the loop above. */
+		bno = isonum_733(rec.extent) + isonum_711(rec.ext_attr_length);
+		twiddle();
+		rc = f->f_dev->dv_strategy(f->f_devdata, F_READ, cdb2devb(bno),
+		    ISO_DEFAULT_BLOCK_SIZE, buf, &read);
+		if (rc)
+			goto out;
+		if (read != ISO_DEFAULT_BLOCK_SIZE) {
+			rc = EIO;
+			goto out;
+		}
+		dp = (struct iso_directory_record *)buf;
+		use_rrip = rrip_check(f, dp, &lenskip);
+	}
+	if (use_rrip) {
+		fp->f_flags |= F_RR;
+		fp->f_susp_skip = lenskip;
+	}
 	fp->f_off = 0;
 	fp->f_bno = isonum_733(rec.extent) + isonum_711(rec.ext_attr_length);
 	fp->f_size = isonum_733(rec.size);
@@ -244,8 +419,7 @@ out:
 }
 
 static int
-cd9660_close(f)
-	struct open_file *f;
+cd9660_close(struct open_file *f)
 {
 	struct file *fp = (struct file *)f->f_fsdata;
 
@@ -256,10 +430,7 @@ cd9660_close(f)
 }
 
 static int
-buf_read_file(f, buf_p, size_p)
-	struct open_file *f;
-	char **buf_p;
-	size_t *size_p;
+buf_read_file(struct open_file *f, char **buf_p, size_t *size_p)
 {
 	struct file *fp = (struct file *)f->f_fsdata;
 	daddr_t blkno, blkoff;
@@ -293,11 +464,7 @@ buf_read_file(f, buf_p, size_p)
 }
 
 static int
-cd9660_read(f, start, size, resid)
-	struct open_file *f;
-	void *start;
-	size_t size;
-	size_t *resid;
+cd9660_read(struct open_file *f, void *start, size_t size, size_t *resid)
 {
 	struct file *fp = (struct file *)f->f_fsdata;
 	char *buf, *addr;
@@ -332,7 +499,8 @@ cd9660_readdir(struct open_file *f, struct dirent *d)
 	struct iso_directory_record *ep;
 	size_t buf_size, reclen, namelen;
 	int error = 0;
-	char *buf;
+	int lenskip;
+	char *buf, *name;
 
 again:
 	if (fp->f_off >= fp->f_size)
@@ -351,9 +519,26 @@ again:
 		goto again;
 	}
 
-	namelen = isonum_711(ep->name_len);
-	if (namelen == 1 && ep->name[0] == 1)
-		namelen = 2;
+	if (fp->f_flags & F_RR) {
+		if (fp->f_flags & F_ROOTDIR && fp->f_off == 0)
+			lenskip = 0;
+		else
+			lenskip = fp->f_susp_skip;
+		name = rrip_lookup_name(f, ep, lenskip, &namelen);
+	} else
+		name = NULL;
+	if (name == NULL) {
+		namelen = isonum_711(ep->name_len);
+		name = ep->name;
+		if (namelen == 1) {
+			if (ep->name[0] == 0)
+				name = ".";
+			else if (ep->name[0] == 1) {
+				namelen = 2;
+				name = "..";
+			}
+		}
+	}
 	reclen = sizeof(struct dirent) - (MAXNAMLEN+1) + namelen + 1;
 	reclen = (reclen + 3) & ~3;
 
@@ -365,12 +550,7 @@ again:
 		d->d_type = DT_REG;
 	d->d_namlen = namelen;
 
-	if (isonum_711(ep->name_len) == 1 && ep->name[0] == 0)
-		strcpy(d->d_name, ".");
-	else if (isonum_711(ep->name_len) == 1 && ep->name[0] == 1)
-		strcpy(d->d_name, "..");
-	else
-		bcopy(ep->name, d->d_name, d->d_namlen);
+	bcopy(name, d->d_name, d->d_namlen);
 	d->d_name[d->d_namlen] = 0;
 
 	fp->f_off += isonum_711(ep->length);
@@ -378,20 +558,13 @@ again:
 }
 
 static int
-cd9660_write(f, start, size, resid)
-	struct open_file *f;
-	void *start;
-	size_t size;
-	size_t *resid;
+cd9660_write(struct open_file *f, void *start, size_t size, size_t *resid)
 {
 	return EROFS;
 }
 
 static off_t
-cd9660_seek(f, offset, where)
-	struct open_file *f;
-	off_t offset;
-	int where;
+cd9660_seek(struct open_file *f, off_t offset, int where)
 {
 	struct file *fp = (struct file *)f->f_fsdata;
 
@@ -412,15 +585,13 @@ cd9660_seek(f, offset, where)
 }
 
 static int
-cd9660_stat(f, sb)
-	struct open_file *f;
-	struct stat *sb;
+cd9660_stat(struct open_file *f, struct stat *sb)
 {
 	struct file *fp = (struct file *)f->f_fsdata;
 
 	/* only important stuff */
 	sb->st_mode = S_IRUSR | S_IRGRP | S_IROTH;
-	if (fp->f_isdir)
+	if (fp->f_flags & F_ISDIR)
 		sb->st_mode |= S_IFDIR;
 	else
 		sb->st_mode |= S_IFREG;
