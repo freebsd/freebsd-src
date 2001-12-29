c5a29982d39eb7d9e78254506575604db83b8ec8
Jake Burkholder <jake@FreeBSD.org>
2001-12-29 06:38:31 +0000

    __infinity is not const.

--- lib/libc/sparc64/gen/infinity.c
+++ lib/libc/sparc64/gen/infinity.c
@@ -9,4 +9,4 @@ __FBSDID("$FreeBSD$");
 #include <math.h>
 
 /* bytes for +Infinity on a sparc */
-const char __infinity[] = { 0x7f, (char)0xf0, 0, 0, 0, 0, 0, 0 };
+char __infinity[] = { 0x7f, (char)0xf0, 0, 0, 0, 0, 0, 0 };
