a0de19b9a02ba622c5d35dedb501c05a75267b43
Sheldon Hearn <sheldonh@FreeBSD.org>
2001-12-30 13:12:50 +0000

    Add PNP Id for Create Labs Phoneblaster.
    PR:		kern/32891
    Submitted by:	Louis Mamakos <louie@TransSys.COM>

--- sys/dev/sio/sio_isa.c
+++ sys/dev/sio/sio_isa.c
@@ -81,6 +81,7 @@ static struct isa_pnp_id sio_ids[] = {
 	{0x00b4490a, NULL},	/* BRIB400 - Boca 56k PnP */
 	{0x0030320d, NULL},	/* CIR3000 - Cirrus Logic V43 */
 	{0x0100440e, NULL},	/* CRD0001 - Cardinal MVP288IV ? */
+	{0x01308c0e, NULL},	/* CTL3001 - Creative Labs Phoneblaster */
 	{0x36033610, NULL},     /* DAV0336 - DAVICOM 336PNP MODEM */
 	{0x0000aa1a, NULL},	/* FUJ0000 - FUJITSU Modem 33600 PNP/I2 */
 	{0x1200c31e, NULL},	/* GVC0012 - VF1128HV-R9 (win modem?) */
