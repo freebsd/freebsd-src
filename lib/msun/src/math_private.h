1bd8e05903fb7fad520fc212cb666ef0ae844d93
jake <jake@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-01-02 06:54:18 +0000

    Add ifdef sparc64.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@88801 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/msun/src/math_private.h
+++ lib/msun/src/math_private.h
@@ -225,7 +225,7 @@ float	__kernel_cosf __P((float,float));
 float	__kernel_tanf __P((float,float,int));
 int	__kernel_rem_pio2f __P((float*,float*,int,int,int,const int*));
 
-#if defined(__alpha__) || defined(__ia64__)
+#if defined(__alpha__) || defined(__ia64__) || defined(__sparc64__)
 #define __generic___ieee754_acos	__ieee754_acos
 #define __generic___ieee754_asin	__ieee754_asin
 #define __generic___ieee754_atan2	__ieee754_atan2
