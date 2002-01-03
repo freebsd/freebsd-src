3f3ff202f445b9cf12972786a14946639f6b144d
mjacob <mjacob@FreeBSD.org>
2002-01-03 20:43:22 +0000

    Implement REDUCED INTERRUPT OPERATION usage form FC cards- this allows the firmware to delay completion of commands so that it can attempt to batch a bunch of completions at once- either returning 16 bit handles in mailbox registers, or in a resposne queue entry that has a whole wad of 16 bit handles.
    Distinguish between 2300 and 2312 chipsets- if only because the revisions
    on the chips have different meanings.
    
    Add more instrumentation plus ISP_GET_STATS and ISP_CLR_STATS ioctls.
    Run up the maximum number of response queue entities we'll look at
    per interrupt.
    
    If we haven't set HBA role yet, always return success from isp_fc_runstate.
    
    MFC after:	2 weeks

--- sys/dev/isp/isp_inline.h
+++ sys/dev/isp/isp_inline.h
@@ -209,7 +209,7 @@ isp_fc_runstate(struct ispsoftc *isp, int tval)
 	fcparam *fcp;
 	int *tptr;
 
-	if (IS_SCSI(isp))
+	if (IS_SCSI(isp) || isp->isp_role == ISP_ROLE_NONE)
 		return (0);
 
 	tptr = tval? &tval : NULL;
@@ -268,6 +268,8 @@ static INLINE void
 isp_get_response(struct ispsoftc *, ispstatusreq_t *, ispstatusreq_t *);
 static INLINE void
 isp_get_response_x(struct ispsoftc *, ispstatus_cont_t *, ispstatus_cont_t *);
+static INLINE void
+isp_get_rio2(struct ispsoftc *, isp_rio2_t *, isp_rio2_t *);
 static INLINE void
 isp_put_icb(struct ispsoftc *, isp_icb_t *, isp_icb_t *);
 static INLINE void
@@ -552,6 +554,22 @@ isp_get_response_x(struct ispsoftc *isp, ispstatus_cont_t *cpsrc,
 	for (i = 0; i < 60; i++) {
 		ISP_IOXGET_8(isp, &cpsrc->req_sense_data[i],
 		    cpdst->req_sense_data[i]);
+	}
+}
+
+static INLINE void
+isp_get_rio2(struct ispsoftc *isp, isp_rio2_t *r2src, isp_rio2_t *r2dst)
+{
+	int i;
+	isp_copy_in_hdr(isp, &r2src->req_header, &r2dst->req_header);
+	if (r2dst->req_header.rqs_seqno > 30)
+		r2dst->req_header.rqs_seqno = 30;
+	for (i = 0; i < r2dst->req_header.rqs_seqno; i++) {
+		ISP_IOXGET_16(isp, &r2src->req_handles[i],
+		    r2dst->req_handles[i]);
+	}
+	while (i < 30) {
+		r2dst->req_handles[i++] = 0;
 	}
 }
 
