8e4609db35771cf40957f45cebb7962f70af966c
Jake Burkholder <jake@FreeBSD.org>
2002-01-08 05:53:31 +0000

    Catch up to the latest and greatest.

--- sys/sparc64/sparc64/genassym.c
+++ sys/sparc64/sparc64/genassym.c
@@ -56,6 +56,7 @@
 #include <machine/pstate.h>
 #include <machine/setjmp.h>
 #include <machine/sigframe.h>
+#include <machine/smp.h>
 #include <machine/pv.h>
 #include <machine/tte.h>
 #include <machine/tlb.h>
@@ -70,7 +71,6 @@ ASSYM(ENAMETOOLONG, ENAMETOOLONG);
 
 ASSYM(KSTACK_PAGES, KSTACK_PAGES);
 ASSYM(KSTACK_GUARD_PAGES, KSTACK_GUARD_PAGES);
-ASSYM(PANIC_STACK_PAGES, PANIC_STACK_PAGES);
 ASSYM(UAREA_PAGES, UAREA_PAGES);
 ASSYM(PAGE_SIZE, PAGE_SIZE);
 
@@ -84,7 +84,12 @@ ASSYM(LSU_VW, LSU_VW);
 
 ASSYM(TLB_DAR_TSB_USER_PRIMARY, TLB_DAR_SLOT(TLB_SLOT_TSB_USER_PRIMARY));
 ASSYM(TLB_DEMAP_NUCLEUS, TLB_DEMAP_NUCLEUS);
+ASSYM(TLB_DEMAP_SECONDARY, TLB_DEMAP_SECONDARY);
+ASSYM(TLB_DEMAP_CONTEXT, TLB_DEMAP_CONTEXT);
 ASSYM(TLB_DEMAP_PAGE, TLB_DEMAP_PAGE);
+
+ASSYM(TLB_DTLB, TLB_DTLB);
+ASSYM(TLB_ITLB, TLB_ITLB);
 
 ASSYM(TSB_BUCKET_ADDRESS_BITS, TSB_BUCKET_ADDRESS_BITS);
 ASSYM(TSB_BUCKET_SHIFT, TSB_BUCKET_SHIFT);
@@ -93,9 +98,18 @@ ASSYM(TSB_KERNEL_VA_MASK, TSB_KERNEL_VA_MASK);
 ASSYM(PAGE_SHIFT, PAGE_SHIFT);
 ASSYM(PAGE_MASK, PAGE_MASK);
 
+ASSYM(CPU_INITED, CPU_INITED);
+ASSYM(CPU_STARTED, CPU_STARTED);
+
+ASSYM(CSA_MID, offsetof(struct cpu_start_args, csa_mid));
+ASSYM(CSA_STATE, offsetof(struct cpu_start_args, csa_state));
+ASSYM(CSA_DATA, offsetof(struct cpu_start_args, csa_data));
+ASSYM(CSA_VA, offsetof(struct cpu_start_args, csa_va));
+
 ASSYM(KTR_COMPILE, KTR_COMPILE);
 ASSYM(KTR_PROC, KTR_PROC);
 ASSYM(KTR_TRAP, KTR_TRAP);
+ASSYM(KTR_SMP, KTR_SMP);
 ASSYM(KTR_SYSC, KTR_SYSC);
 ASSYM(KTR_INTR, KTR_INTR);
 ASSYM(KTR_CT1, KTR_CT1);
@@ -133,6 +147,9 @@ ASSYM(V_INTR, offsetof(struct vmmeter, v_intr));
 ASSYM(PC_CURTHREAD, offsetof(struct pcpu, pc_curthread));
 ASSYM(PC_CURPCB, offsetof(struct pcpu, pc_curpcb));
 ASSYM(PC_CPUID, offsetof(struct pcpu, pc_cpuid));
+ASSYM(PC_IQ, offsetof(struct pcpu, pc_iq));
+ASSYM(PC_MID, offsetof(struct pcpu, pc_mid));
+ASSYM(PC_SIZEOF, sizeof(struct pcpu));
 
 ASSYM(IH_SHIFT, IH_SHIFT);
 
@@ -146,6 +163,14 @@ ASSYM(IQE_PRI, offsetof(struct iqe, iqe_pri));
 ASSYM(IQE_VEC, offsetof(struct iqe, iqe_vec));
 ASSYM(IQE_FUNC, offsetof(struct iqe, iqe_func));
 ASSYM(IQE_ARG, offsetof(struct iqe, iqe_arg));
+
+ASSYM(ILA_LEVEL, offsetof(struct ipi_level_args, ila_level));
+
+ASSYM(ITA_TLB, offsetof(struct ipi_tlb_args, ita_tlb));
+ASSYM(ITA_CTX, offsetof(struct ipi_tlb_args, ita_ctx));
+ASSYM(ITA_START, offsetof(struct ipi_tlb_args, ita_start));
+ASSYM(ITA_END, offsetof(struct ipi_tlb_args, ita_end));
+ASSYM(ITA_VA, offsetof(struct ipi_tlb_args, ita_va));
 
 ASSYM(IV_SHIFT, IV_SHIFT);
 ASSYM(IV_FUNC, offsetof(struct intr_vector, iv_func));
