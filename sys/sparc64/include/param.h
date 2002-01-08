a116c7d02ee33b4d5310c9171e48f900b22e310d
jake <jake@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-01-08 05:05:42 +0000

    Remove PANIC_STACK_PAGES which is no longer used. Redefine the compile time assertion macro to take one parameter.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@89040 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sparc64/include/param.h
+++ sys/sparc64/include/param.h
@@ -124,8 +124,6 @@
 #define KSTACK_PAGES		4	/* pages of kernel stack (with pcb) */
 #define UAREA_PAGES		1	/* pages of user area */
 
-#define PANIC_STACK_PAGES	1
-
 #define KSTACK_GUARD 		/* compile in kstack guard page */
 #define KSTACK_GUARD_PAGES	1
 
@@ -174,9 +172,9 @@
 
 #define	pgtok(x)		((unsigned long)(x) * (PAGE_SIZE / 1024))
 
-#define	ASSERT_EQUAL(x, y)	_ASSERT_EQUAL(x, y, __LINE__)
-#define	_ASSERT_EQUAL(x, y, z)	__ASSERT_EQUAL(x, y, z)
-#define	__ASSERT_EQUAL(x, y, z)	typedef char __assert ## z[(x) == (y) ? 1 : -1]
+#define	CTASSERT(x)		_CTASSERT(x, __LINE__)
+#define	_CTASSERT(x, y)		__CTASSERT(x, y)
+#define	__CTASSERT(x, y)	typedef char __assert ## y[(x) ? 1 : -1]
 
 #endif /* !_MACHINE_PARAM_H_ */
 #endif /* !_NO_NAMESPACE_POLLUTION */
