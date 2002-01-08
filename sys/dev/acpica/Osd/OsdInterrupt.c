5fbc0fd326e18abac62516e7b10140d589caee88
msmith <msmith@FreeBSD.org>
2002-01-08 19:14:59 +0000

    find_devclass -> devclass_find.

--- sys/dev/acpica/Osd/OsdInterrupt.c
+++ sys/dev/acpica/Osd/OsdInterrupt.c
@@ -108,7 +108,7 @@ AcpiOsRemoveInterruptHandler (UINT32 InterruptNumber, OSD_HANDLER ServiceRoutine
     if (ServiceRoutine == NULL)
 	return_ACPI_STATUS(AE_BAD_PARAMETER);
 
-    if ((sc = devclass_get_softc(find_devclass("acpi"), 0)) == NULL)
+    if ((sc = devclass_get_softc(devclass_find("acpi"), 0)) == NULL)
 	panic("can't find ACPI device to deregister interrupt");
 
     if (sc->acpi_irq == NULL)
