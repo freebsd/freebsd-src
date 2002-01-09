a9e1a3e8692732a2404fc7d864297ebd05a1d5f5
msmith <msmith@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-01-09 04:58:49 +0000

    Add a new sysinit SI_SUB_DEVFS.  Devfs hooks into the kernel at SI_ORDER_FIRST, and devices can be created anytime after that.
    Print a warning if an atttempt is made to create a device too early.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@89118 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/kernel.h
+++ sys/sys/kernel.h
@@ -130,6 +130,7 @@ enum sysinit_sub_id {
 	SI_SUB_MBUF		= 0x2700000,	/* mbuf subsystem */
 	SI_SUB_INTR		= 0x2800000,	/* interrupt threads */
 	SI_SUB_SOFTINTR		= 0x2800001,	/* start soft interrupt thread */
+	SI_SUB_DEVFS		= 0x2F00000,	/* devfs ready for devices */
 	SI_SUB_INIT_IF		= 0x3000000,	/* prep for net interfaces */
 	SI_SUB_DRIVERS		= 0x3100000,	/* Let Drivers initialize */
 	SI_SUB_CONFIGURE	= 0x3800000,	/* Configure devices */
