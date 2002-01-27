e8e3198e6450f7ddc0a4f84deffaa1eb611e2506
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-01-27 22:41:09 +0000

    This commit was generated by cvs2svn to compensate for changes in r89884, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@89885 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/gcc/config/i386/sysv4gdb.h
+++
@@ -1,7 +0,0 @@
-/* Target definitions for GNU compiler for Intel 80386 running System V.4
-   with gas and gdb.  */
-
-/* Use stabs instead of DWARF debug format.  */
-#define PREFERRED_DEBUGGING_TYPE DBX_DEBUG
-
-#include "i386/sysv4.h"
