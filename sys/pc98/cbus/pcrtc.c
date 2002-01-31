da47d8fa71364c6cae119f651dc88cc0c07d777e
nyan <nyan@FreeBSD.org>
2002-01-31 04:28:52 +0000

    MFi386: revision 1.180

--- sys/pc98/cbus/pcrtc.c
+++ sys/pc98/cbus/pcrtc.c
@@ -53,6 +53,7 @@
  */
 
 #include "opt_clock.h"
+#include "opt_isa.h"
 #include "opt_mca.h"
 
 #include <sys/param.h>
@@ -93,7 +94,9 @@
 #include <i386/isa/isa.h>
 #include <isa/rtc.h>
 #endif
+#ifdef DEV_ISA
 #include <isa/isavar.h>
+#endif
 #include <i386/isa/timerreg.h>
 
 #include <i386/isa/intr_machdep.h>
@@ -1611,6 +1614,7 @@ tsc_get_timecount(struct timecounter *tc)
 	return (rdtsc());
 }
 
+#ifdef DEV_ISA
 /*
  * Attach to the ISA PnP descriptors for the timer and realtime clock.
  */
@@ -1659,3 +1663,4 @@ DRIVER_MODULE(attimer, isa, attimer_driver, attimer_devclass, 0, 0);
 #ifndef PC98
 DRIVER_MODULE(attimer, acpi, attimer_driver, attimer_devclass, 0, 0);
 #endif
+#endif /* DEV_ISA */
