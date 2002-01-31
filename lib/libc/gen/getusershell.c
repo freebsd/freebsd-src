ec4c2e360534aa35a086da61ea953bf67225ad22
David E. O'Brien <obrien@FreeBSD.org>
2002-01-31 17:26:37 +0000

    Fix minor style nit.

--- lib/libc/gen/getusershell.c
+++ lib/libc/gen/getusershell.c
@@ -33,10 +33,12 @@
  * SUCH DAMAGE.
  */
 
-#include <sys/cdefs.h>
+#if 0
 #if defined(LIBC_SCCS) && !defined(lint)
 static char sccsid[] = "@(#)getusershell.c	8.1 (Berkeley) 6/4/93";
 #endif /* LIBC_SCCS and not lint */
+#endif
+#include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
