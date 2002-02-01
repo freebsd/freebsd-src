0736ba240846982521a872123a6740d4e2bf2e4a
bde <bde@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-02-01 23:26:32 +0000

    Fixed tab lossage in vendor id and nearby style bugs in previous commit.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@90096 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/gen/err.c
+++ lib/libc/gen/err.c
@@ -31,9 +31,12 @@
  * SUCH DAMAGE.
  */
 
+#if 0
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)err.c^I8.1 (Berkeley) 6/4/93";
+static char sccsid[] = "@(#)err.c	8.1 (Berkeley) 6/4/93";
 #endif /* LIBC_SCCS and not lint */
+#endif
+
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
