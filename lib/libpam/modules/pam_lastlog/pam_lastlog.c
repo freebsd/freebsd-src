ac5699692e72965189208bddac22dafaf5b7c476
Mark Murray <markm@FreeBSD.org>
2002-02-03 15:17:57 +0000

    WARNS=n fixes (and some stylistic issues).

--- lib/libpam/modules/pam_lastlog/pam_lastlog.c
+++ lib/libpam/modules/pam_lastlog/pam_lastlog.c
@@ -219,7 +219,7 @@ pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
 }
 
 PAM_EXTERN int
-pam_sm_close_session(pam_handle_t *pamh, int flags __unused, int argc, const char **argv)
+pam_sm_close_session(pam_handle_t *pamh __unused, int flags __unused, int argc, const char **argv)
 {
 	struct options options;
 
