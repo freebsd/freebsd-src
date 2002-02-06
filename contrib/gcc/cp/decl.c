1b2d18310566cfa42f3acd476706af1981a6686d
obrien <obrien@FreeBSD.org>
2002-02-06 05:04:25 +0000

    A slightly different version of rev 1.2 that is closer to something the GCC maintainers might actually accept.

--- contrib/gcc/cp/decl.c
+++ contrib/gcc/cp/decl.c
@@ -57,6 +57,13 @@ extern const struct attribute_spec *lang_attribute_table;
 #define BOOL_TYPE_SIZE CHAR_TYPE_SIZE
 #endif
 
+#ifndef HOST_PTR_PRINTF_FORMAT
+#define HOST_PTR_PRINTF_FORMAT HOST_PTR_PRINTF
+#endif
+#ifndef HOST_PTR_PRINTF_TYPE
+#define HOST_PTR_PRINTF_TYPE (void *)
+#endif
+
 static tree grokparms				PARAMS ((tree));
 static const char *redeclaration_error_message	PARAMS ((tree, tree));
 
@@ -2004,7 +2011,7 @@ print_binding_level (lvl)
   tree t;
   int i = 0, len;
   fprintf (stderr, " blocks=");
-  fprintf (stderr, HOST_PTR_PRINTF, (void *) lvl->blocks);
+  fprintf (stderr, HOST_PTR_PRINTF_FORMAT, HOST_PTR_PRINTF_TYPE lvl->blocks);
   fprintf (stderr, " n_incomplete=%d parm_flag=%d keep=%d",
 	   list_length (lvl->incomplete), lvl->parm_flag, lvl->keep);
   if (lvl->tag_transparent)
@@ -2108,7 +2115,7 @@ print_other_binding_stack (stack)
   for (level = stack; level != global_binding_level; level = level->level_chain)
     {
       fprintf (stderr, "binding level ");
-      fprintf (stderr, HOST_PTR_PRINTF, (void *) level);
+      fprintf (stderr, HOST_PTR_PRINTF_FORMAT, HOST_PTR_PRINTF_TYPE level);
       fprintf (stderr, "\n");
       print_binding_level (level);
     }
@@ -2119,11 +2126,11 @@ print_binding_stack ()
 {
   struct binding_level *b;
   fprintf (stderr, "current_binding_level=");
-  fprintf (stderr, HOST_PTR_PRINTF, (void *) current_binding_level);
+  fprintf (stderr, HOST_PTR_PRINTF_FORMAT, HOST_PTR_PRINTF_TYPE current_binding_level);
   fprintf (stderr, "\nclass_binding_level=");
-  fprintf (stderr, HOST_PTR_PRINTF, (void *) class_binding_level);
+  fprintf (stderr, HOST_PTR_PRINTF_FORMAT, HOST_PTR_PRINTF_TYPE class_binding_level);
   fprintf (stderr, "\nglobal_binding_level=");
-  fprintf (stderr, HOST_PTR_PRINTF, (void *) global_binding_level);
+  fprintf (stderr, HOST_PTR_PRINTF_FORMAT, HOST_PTR_PRINTF_TYPE global_binding_level);
   fprintf (stderr, "\n");
   if (class_binding_level)
     {
