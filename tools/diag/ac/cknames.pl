64189afa130556528bbddd0bd2cd3764020b44cb
phantom <phantom@FreeBSD.org>
2002-02-07 19:27:29 +0000

    . add a hack dedicated to n_himba (or correctly to say problem with his nick   in docbook environment) . clarify comments . mention that this script is dedicated to freefall's environment . add bsd style copyright

--- tools/diag/ac/cknames.pl
+++ tools/diag/ac/cknames.pl
@@ -1,8 +1,34 @@
 #!/usr/bin/perl -w
-# Copyright (c) 2002 Alexey Zelkin <phantom@FreeBSD.org>
 #
-# cknames.pl -- this scripts checks for existence persons listed
-#               in authors.ent and access files
+# Copyright (c) 2002 Alexey Zelkin <phantom@FreeBSD.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# cknames.pl -- this scripts checks for integrity of person lists
+#               between authors.ent, CVSROOT/access and passwd database
+#               at freefall.freebsd.org
+#
+# NOTE: This script is supposed to run at freefall.freebsd.org *only*
 #
 # $FreeBSD$
 #
@@ -18,6 +44,8 @@ $cvs = "cvs -R -d $cvsroot co -p";
 open(PASSWD, 'ypcat passwd |') || die "open passwd data: $!\n";
 while (<PASSWD>) {
 	($login,undef) = split(/:/);
+	$login =~ s/_//g;	# remove _ from usernames since this
+				# character is not allowed in docbook entities
 	print "passwd user: $login\n" if $debug;
 	$users{$login} = 1;
 }
