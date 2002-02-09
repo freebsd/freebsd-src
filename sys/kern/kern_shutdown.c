9faa8d780298dcbfa630180f77ae934458c2f7d4
julian <julian@FreeBSD.org>
2002-02-09 01:38:16 +0000

    Replace accidentally removed setrunqueue() solves problem with machines failing to sync in booting. Submitted by: Tor.Egge@cvsup.no.freebsd.org

--- sys/kern/kern_shutdown.c
+++ sys/kern/kern_shutdown.c
@@ -272,6 +272,7 @@ boot(int howto)
 				DROP_GIANT();
    				for (subiter = 0; subiter < 50 * iter; subiter++) {
      					mtx_lock_spin(&sched_lock);
+					setrunqueue(curthread);
 					curthread->td_proc->p_stats->p_ru.ru_nvcsw++;
      					mi_switch(); /* Allow interrupt threads to run */
      					mtx_unlock_spin(&sched_lock);
