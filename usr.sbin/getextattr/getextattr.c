4a8fa4b0b958ed1a6e61a1e587e16be897b5faaa
rwatson <rwatson@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-02-10 06:14:03 +0000

    Update license.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@90460 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/getextattr/getextattr.c
+++ usr.sbin/getextattr/getextattr.c
@@ -10,7 +10,6 @@
  * DARPA/SPAWAR contract N66001-01-C-8035 ("CBOSS"), as part of the DARPA
  * CHATS research program.
  *
- *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -19,6 +18,9 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
+ * 3. The names of the authors may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
