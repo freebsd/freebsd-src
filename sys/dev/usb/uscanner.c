2c0634eab6f126d3e81daa7c88b308483c74f0ac
Josef Karthauser <joe@FreeBSD.org>
2002-02-11 02:25:47 +0000

    ANSIfy the function declarations, in line with NetBSD.

--- sys/dev/usb/uscanner.c
+++ sys/dev/usb/uscanner.c
@@ -326,11 +326,7 @@ USB_ATTACH(uscanner)
 }
 
 int
-uscanneropen(dev, flag, mode, p)
-	dev_t dev;
-	int flag;
-	int mode;
-	usb_proc_ptr p;
+uscanneropen(dev_t dev, int flag, int mode, usb_proc_ptr p)
 {
 	struct uscanner_softc *sc;
 	int unit = USCANNERUNIT(dev);
@@ -389,11 +385,7 @@ uscanneropen(dev, flag, mode, p)
 }
 
 int
-uscannerclose(dev, flag, mode, p)
-	dev_t dev;
-	int flag;
-	int mode;
-	usb_proc_ptr p;
+uscannerclose(dev_t dev, int flag, int mode, usb_proc_ptr p)
 {
 	struct uscanner_softc *sc;
 
@@ -450,10 +442,7 @@ uscanner_do_close(struct uscanner_softc *sc)
 }
 
 Static int
-uscanner_do_read(sc, uio, flag)
-	struct uscanner_softc *sc;
-	struct uio *uio;
-	int flag;
+uscanner_do_read(struct uscanner_softc *sc, struct uio *uio, int flag)
 {
 	u_int32_t n, tn;
 	usbd_status err;
@@ -492,10 +481,7 @@ uscanner_do_read(sc, uio, flag)
 }
 
 int
-uscannerread(dev, uio, flag)
-	dev_t dev;
-	struct uio *uio;
-	int flag;
+uscannerread(dev_t dev, struct uio *uio, int flag)
 {
 	struct uscanner_softc *sc;
 	int error;
@@ -511,10 +497,7 @@ uscannerread(dev, uio, flag)
 }
 
 Static int
-uscanner_do_write(sc, uio, flag)
-	struct uscanner_softc *sc;
-	struct uio *uio;
-	int flag;
+uscanner_do_write(struct uscanner_softc *sc, struct uio *uio, int flag)
 {
 	u_int32_t n;
 	int error = 0;
@@ -548,10 +531,7 @@ uscanner_do_write(sc, uio, flag)
 }
 
 int
-uscannerwrite(dev, uio, flag)
-	dev_t dev;
-	struct uio *uio;
-	int flag;
+uscannerwrite(dev_t dev, struct uio *uio, int flag)
 {
 	struct uscanner_softc *sc;
 	int error;
@@ -567,9 +547,7 @@ uscannerwrite(dev, uio, flag)
 
 #if defined(__NetBSD__) || defined(__OpenBSD__)
 int
-uscanner_activate(self, act)
-	device_ptr_t self;
-	enum devact act;
+uscanner_activate(device_ptr_t self, enum devact act)
 {
 	struct uscanner_softc *sc = (struct uscanner_softc *)self;
 
@@ -640,10 +618,7 @@ USB_DETACH(uscanner)
 }
 
 int
-uscannerpoll(dev, events, p)
-	dev_t dev;
-	int events;
-	usb_proc_ptr p;
+uscannerpoll(dev_t dev, int events, usb_proc_ptr p)
 {
 	struct uscanner_softc *sc;
 	int revents = 0;
