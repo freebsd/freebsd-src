202438124c0c6be5c5473493f3a677a85d712f49
Mike Silbersack <silby@FreeBSD.org>
2002-02-15 04:23:59 +0000

    MFC rev 1.101:
    Remove printf's on mbuf/cluster allocation failures. There are now
    equivalent and less dangerous (rate limited) messages in
    the mbuf allocation code.

--- sys/pci/if_xl.c
+++ sys/pci/if_xl.c
@@ -1721,16 +1721,11 @@ static int xl_newbuf(sc, c)
 	struct mbuf		*m_new = NULL;
 
 	MGETHDR(m_new, M_DONTWAIT, MT_DATA);
-	if (m_new == NULL) {
-		printf("xl%d: no memory for rx list -- "
-		    "packet dropped!\n", sc->xl_unit);
+	if (m_new == NULL)
 		return(ENOBUFS);
-	}
 
 	MCLGET(m_new, M_DONTWAIT);
 	if (!(m_new->m_flags & M_EXT)) {
-		printf("xl%d: no memory for rx list -- "
-		    "packet dropped!\n", sc->xl_unit);
 		m_freem(m_new);
 		return(ENOBUFS);
 	}
