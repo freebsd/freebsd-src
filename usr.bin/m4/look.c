09c1a581825a81e0204019e6295aecf7321a6ea1
jmallett <jmallett@FreeBSD.org>
2002-02-16 21:27:48 +0000

    Import OpenBSD m4 as of today.

--- usr.bin/m4/look.c
+++ usr.bin/m4/look.c
@@ -1,3 +1,5 @@
+/*	$OpenBSD: look.c,v 1.9 2002/02/16 21:27:48 millert Exp $	*/
+
 /*
  * Copyright (c) 1989, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -47,19 +49,22 @@ static char sccsid[] = "@(#)look.c	8.1 (Berkeley) 6/6/93";
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include <string.h>
 #include "mdef.h"
 #include "stdd.h"
 #include "extern.h"
 
-int
+static void freent(ndptr);
+
+unsigned
 hash(name)
-register char *name;
+	const char *name;
 {
-	register unsigned long h = 0;
+	unsigned int h = 0;
 	while (*name)
 		h = (h << 5) + h + *name++;
-	return (h % HASHSIZE);
+	return (h);
 }
 
 /*
@@ -67,12 +72,14 @@ register char *name;
  */
 ndptr 
 lookup(name)
-char *name;
+	const char *name;
 {
-	register ndptr p;
+	ndptr p;
+	unsigned int h;
 
-	for (p = hashtab[hash(name)]; p != nil; p = p->nxtptr)
-		if (STREQ(name, p->name))
+	h = hash(name);
+	for (p = hashtab[h % HASHSIZE]; p != nil; p = p->nxtptr)
+		if (h == p->hv && STREQ(name, p->name))
 			break;
 	return (p);
 }
@@ -83,28 +90,27 @@ char *name;
  */
 ndptr 
 addent(name)
-char *name;
+	const char *name;
 {
-	register int h;
+	unsigned int h;
 	ndptr p;
 
 	h = hash(name);
 	p = (ndptr) xalloc(sizeof(struct ndblock));
-	p->nxtptr = hashtab[h];
-	hashtab[h] = p;
+	p->nxtptr = hashtab[h % HASHSIZE];
+	hashtab[h % HASHSIZE] = p;
 	p->name = xstrdup(name);
+	p->hv = h;
 	return p;
 }
 
 static void
 freent(p)
-ndptr p;
+	ndptr p;
 {
-	if (!(p->type & STATIC)) {
-		free((char *) p->name);
-		if (p->defn != null)
-			free((char *) p->defn);
-	}
+	free((char *) p->name);
+	if (p->defn != null)
+		free((char *) p->defn);
 	free((char *) p);
 }
 
@@ -113,21 +119,21 @@ ndptr p;
  */
 void
 remhash(name, all)
-char *name;
-int all;
+	const char *name;
+	int all;
 {
-	register int h;
-	register ndptr xp, tp, mp;
+	unsigned int h;
+	ndptr xp, tp, mp;
 
 	h = hash(name);
-	mp = hashtab[h];
+	mp = hashtab[h % HASHSIZE];
 	tp = nil;
 	while (mp != nil) {
-		if (STREQ(mp->name, name)) {
+		if (mp->hv == h && STREQ(mp->name, name)) {
 			mp = mp->nxtptr;
 			if (tp == nil) {
-				freent(hashtab[h]);
-				hashtab[h] = mp;
+				freent(hashtab[h % HASHSIZE]);
+				hashtab[h % HASHSIZE] = mp;
 			}
 			else {
 				xp = tp->nxtptr;
