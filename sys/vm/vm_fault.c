ef6020d18714af9f7540fd367ac87f7a831ecf99
Mike Silbersack <silby@FreeBSD.org>
2002-02-19 18:34:02 +0000

    Changes to make the OOM killer much more effective:
    - Allow the OOM killer to target processes currently locked in
      memory.  These very often are the ones doing the memory hogging.
    - Drop the wakeup priority of processes currently sleeping while
      waiting for their page fault to complete.  In order for the OOM
      killer to work well, the killed process and other system processes
      waiting on memory must be allowed to wakeup first.
    
    Reviewed by:	dillon
    MFC after:	1 week

--- sys/vm/vm_fault.c
+++ sys/vm/vm_fault.c
@@ -347,7 +347,7 @@ RetryFault:;
 			if ((queue - fs.m->pc) == PQ_CACHE && vm_page_count_severe()) {
 				vm_page_activate(fs.m);
 				unlock_and_deallocate(&fs);
-				VM_WAIT;
+				VM_WAITPFAULT;
 				goto RetryFault;
 			}
 
@@ -388,7 +388,7 @@ RetryFault:;
 			}
 			if (fs.m == NULL) {
 				unlock_and_deallocate(&fs);
-				VM_WAIT;
+				VM_WAITPFAULT;
 				goto RetryFault;
 			}
 		}
