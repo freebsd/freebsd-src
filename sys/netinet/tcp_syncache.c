6b33ceb8e3125880bea84652d769d73d2e92b371
Jonathan Lemon <jlemon@FreeBSD.org>
2002-02-20 16:47:11 +0000

    When expanding a syncache entry into a socket, inherit the socket options from the current listen socket instead of the cached (and possibly stale) TCB pointer.

--- sys/netinet/tcp_syncache.c
+++ sys/netinet/tcp_syncache.c
@@ -666,7 +666,7 @@ syncache_socket(sc, lso)
 	tp->rcv_wnd = sc->sc_wnd;
 	tp->rcv_adv += tp->rcv_wnd;
 
-	tp->t_flags = sc->sc_tp->t_flags & (TF_NOPUSH|TF_NODELAY);
+	tp->t_flags = sototcpcb(lso)->t_flags & (TF_NOPUSH|TF_NODELAY);
 	if (sc->sc_flags & SCF_NOOPT)
 		tp->t_flags |= TF_NOOPT;
 	if (sc->sc_flags & SCF_WINSCALE) {
