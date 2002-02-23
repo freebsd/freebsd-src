5c1eca55510ced8982522fa919a1ad4349eb96f2
Dag-Erling Smørgrav <des@FreeBSD.org>
2002-02-23 01:22:51 +0000

    Vendor import of OpenPAM Calamite

---
+++ contrib/openpam/lib/pam_getenv.c
@@ -0,0 +1,67 @@
+/*-
+ * Copyright (c) 2002 Networks Associates Technologies, Inc.
+ * All rights reserved.
+ *
+ * This software was developed for the FreeBSD Project by ThinkSec AS and
+ * NAI Labs, the Security Research Division of Network Associates, Inc.
+ * under DARPA/SPAWAR contract N66001-01-C-8035 ("CBOSS"), as part of the
+ * DARPA CHATS research program.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id$
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <security/pam_appl.h>
+
+#include "openpam_impl.h"
+
+/*
+ * XSSO 4.2.1
+ * XSSO 6 page 44
+ *
+ * Retrieve the value of a PAM environment variable
+ */
+
+char *
+pam_getenv(pam_handle_t *pamh,
+	const char *name)
+{
+	int i;
+
+	if (pamh == NULL)
+		return (NULL);
+
+	/* sanity checks */
+	if (name == NULL || strchr(name, '=') != NULL)
+		return (NULL);
+
+	if ((i = openpam_findenv(pamh, name, strlen(name))) == -1)
+		return (NULL);
+	return (strdup(pamh->env[i]));
+}
---
+++ contrib/openpam/lib/pam_strerror.c
@@ -0,0 +1,123 @@
+/*-
+ * Copyright (c) 2002 Networks Associates Technologies, Inc.
+ * All rights reserved.
+ *
+ * This software was developed for the FreeBSD Project by ThinkSec AS and
+ * NAI Labs, the Security Research Division of Network Associates, Inc.
+ * under DARPA/SPAWAR contract N66001-01-C-8035 ("CBOSS"), as part of the
+ * DARPA CHATS research program.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id$
+ */
+
+#include <stdio.h>
+
+#include <security/pam_appl.h>
+
+#include "openpam_impl.h"
+
+/*
+ * XSSO 4.2.1
+ * XSSO 6 page 92
+ *
+ * Get PAM standard error message string
+ */
+
+const char *
+pam_strerror(pam_handle_t *pamh,
+	int error_number)
+{
+	static char unknown[16];
+
+	pamh = pamh;
+
+	switch (error_number) {
+	case PAM_SUCCESS:
+		return ("success");
+	case PAM_OPEN_ERR:
+		return ("failed to load module");
+	case PAM_SYMBOL_ERR:
+		return ("symbol not found in module");
+	case PAM_SERVICE_ERR:
+		return ("error in service module");
+	case PAM_SYSTEM_ERR:
+		return ("system error");
+	case PAM_BUF_ERR:
+		return ("memory buffer error");
+	case PAM_CONV_ERR:
+		return ("conversation failure");
+	case PAM_PERM_DENIED:
+		return ("permission denied");
+	case PAM_MAXTRIES:
+		return ("maximum number of tries exceeded");
+	case PAM_AUTH_ERR:
+		return ("authentication error");
+	case PAM_NEW_AUTHTOK_REQD:
+		return ("new authentication token required");
+	case PAM_CRED_INSUFFICIENT:
+		return ("insufficient credentials");
+	case PAM_AUTHINFO_UNAVAIL:
+		return ("authentication information is unavailable");
+	case PAM_USER_UNKNOWN:
+		return ("unknown user");
+	case PAM_CRED_UNAVAIL:
+		return ("failed to retrieve user credentials");
+	case PAM_CRED_EXPIRED:
+		return ("user credentials have expired");
+	case PAM_CRED_ERR:
+		return ("failed to set user credentials");
+	case PAM_ACCT_EXPIRED:
+		return ("user accound has expired");
+	case PAM_AUTHTOK_EXPIRED:
+		return ("password has expired");
+	case PAM_SESSION_ERR:
+		return ("session failure");
+	case PAM_AUTHTOK_ERR:
+		return ("authentication token failure");
+	case PAM_AUTHTOK_RECOVERY_ERR:
+		return ("failed to recover old authentication token");
+	case PAM_AUTHTOK_LOCK_BUSY:
+		return ("authentication token lock busy");
+	case PAM_AUTHTOK_DISABLE_AGING:
+		return ("authentication token ageing disabled");
+	case PAM_NO_MODULE_DATA:
+		return ("module data not found");
+	case PAM_IGNORE:
+		return ("ignore this module");
+	case PAM_ABORT:
+		return ("general failure");
+	case PAM_TRY_AGAIN:
+		return ("try again");
+	case PAM_MODULE_UNKNOWN:
+		return ("unknown module type");
+	case PAM_DOMAIN_UNKNOWN:
+		return ("unknown authentication domain");
+	default:
+		snprintf(unknown, sizeof unknown, "#%d", error_number);
+		return (unknown);
+	}
+}
