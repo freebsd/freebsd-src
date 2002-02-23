bd4ecb856d256aa2069974f1f0c841915d51d645
jake <jake@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-02-23 20:25:20 +0000

    Adapt the tsb_foreach interface to take a source and a destination pmap so that it can be used for pmap_copy.  Other consumers ignore the second pmap. Add statistics gathering for tsb_foreach. Implement pmap_copy.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@91168 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sparc64/include/tsb.h
+++ sys/sparc64/include/tsb.h
@@ -73,14 +73,14 @@ tsb_kvtotte(vm_offset_t va)
 	return (tsb_kvpntotte(va >> PAGE_SHIFT));
 }
 
-typedef int (tsb_callback_t)(struct pmap *, struct tte *, vm_offset_t);
+typedef int (tsb_callback_t)(struct pmap *, struct pmap *, struct tte *, vm_offset_t);
 
 struct	tte *tsb_tte_lookup(pmap_t pm, vm_offset_t va);
 void	tsb_tte_remove(struct tte *stp);
 struct	tte *tsb_tte_enter(pmap_t pm, vm_page_t m, vm_offset_t va,
 			   struct tte tte);
 void	tsb_tte_local_remove(struct tte *tp);
-void	tsb_foreach(pmap_t pm, vm_offset_t start, vm_offset_t end,
+void	tsb_foreach(pmap_t pm1, pmap_t pm2, vm_offset_t start, vm_offset_t end,
 		    tsb_callback_t *callback);
 
 #endif /* !_MACHINE_TSB_H_ */
