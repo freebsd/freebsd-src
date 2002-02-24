5e7ef0a58c9e04427b471d97971092a1f0646b23
Alfred Perlstein <alfred@FreeBSD.org>
2002-02-24 22:45:33 +0000

    bump __FreeBSD_version for usb structure rename.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -52,7 +52,7 @@
  * there.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 450000	/* Master, propagated to newvers */
+#define __FreeBSD_version 450001	/* Master, propagated to newvers */
 
 #ifndef NULL
 #define	NULL	0
