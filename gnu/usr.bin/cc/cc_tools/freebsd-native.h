d066aacb4ab3993b957c582b4ffbebde5bedb888
obrien <obrien@FreeBSD.org>
2002-02-24 23:22:51 +0000

    MFC: rev 1.13 (use libgcc_p.a when -pg is in effect)

--- gnu/usr.bin/cc/cc_tools/freebsd-native.h
+++ gnu/usr.bin/cc/cc_tools/freebsd-native.h
@@ -47,5 +47,9 @@
 
 #define STANDARD_STARTFILE_PREFIX	PREFIX"/lib/"
 
+/* For the native system compiler, we actually build libgcc in a profiled
+   version.  So we should use it with -pg.  */
+#define LIBGCC_SPEC "%{!pg: -lgcc} %{pg: -lgcc_p}"
+
 /* FreeBSD is 4.4BSD derived */
 #define bsd4_4
