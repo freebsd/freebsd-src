922c27c5705508264649a33b1ed6bf7df41a1e96
sos <sos@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-02-26 10:38:33 +0000

    Hide "bla bla exists, skipping it" behind bootverbose.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@91298 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/subr_bus.c
+++ sys/kern/subr_bus.c
@@ -359,10 +359,10 @@ devclass_alloc_unit(devclass_t dc, int *unitp)
 	/* If we were given a wired unit number, check for existing device */
 	/* XXX imp XXX */
 	if (unit != -1) {
-		if (unit >= 0 && unit < dc->maxunit &&
-		    dc->devices[unit] != NULL) {
-			printf("%s: %s%d already exists; skipping it\n",
-			    dc->name, dc->name, *unitp);
+		if (unit >= 0 && unit < dc->maxunit && dc->devices[unit]) {
+			if (bootverbose)
+				printf("%s: %s%d already exists, skipping it\n",
+			    		dc->name, dc->name, *unitp);
 			return (EEXIST);
 		}
 	} else {
