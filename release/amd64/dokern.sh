117d814a624e090a16b076541f77c0a42a760d06
nyan <nyan@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-03-03 10:38:40 +0000

    - Sort some entries. - Don't remove MATH_EMULATE and MSDOS from a 'SMALL' floppy. - Use the pattern '<tab><device name><tab>' instead of '<device name>' to   avoid an unexpected matching.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@91582 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/amd64/dokern.sh
+++ release/amd64/dokern.sh
@@ -11,40 +11,15 @@ fi
 
 if [ "${FDSIZE}" = "SMALL" ]; then
 
-sed	-e '/	pci$/d' \
-	-e '/	adv/d' \
-	-e '/	ahc/d' \
-	-e '/	amd/d' \
-	-e '/	isp/d' \
-	-e '/	ncr/d' \
-	-e '/	sym/d' \
-	-e '/	de/d' \
-	-e '/	fxp/d' \
-	-e '/	tx/d' \
-	-e '/	txp/d' \
-	-e '/	vx/d' \
-	-e '/	wx/d' \
-	-e '/	dc/d' \
-	-e '/	pcn/d' \
-	-e '/	rl/d' \
-	-e '/	sf/d' \
-	-e '/	sis/d' \
-	-e '/	ste/d' \
-	-e '/	tl/d' \
-	-e '/	vr/d' \
-	-e '/	wb/d' \
-	-e '/	xl/d' \
-	-e '/pty/d' \
-	-e '/pass/d' \
-	-e '/	apm/d' \
-	-e '/pmtimer/d' \
-	-e '/ppp/d' \
-	-e '/gif/d' \
-	-e '/faith/d' \
-	-e '/gzip/d' \
-	-e '/random/d' \
-	-e '/splash/d' \
-	-e '/MATH_EMULATE/d' \
+sed	-e '/	pty	/d' \
+	-e '/	pass	/d' \
+	-e '/	apm$/d' \
+	-e '/	pmtimer$/d' \
+	-e '/	ppp	/d' \
+	-e '/	gif	/d' \
+	-e '/	faith	/d' \
+	-e '/	random	/d' \
+	-e '/	splash$/d' \
 	-e '/PROCFS/d' \
 	-e '/KTRACE/d' \
 	-e '/SYSV/d' \
@@ -53,35 +28,56 @@ sed	-e '/	pci$/d' \
 	-e '/MFS/d' \
 	-e '/NFSSERVER/d' \
 	-e '/NFS_ROOT/d' \
-	-e '/P1003_1B/d' \
 	-e '/DEBUG/d' \
 	-e '/DDB/d' \
 	-e '/INVARIANTS/d' \
 	-e '/INVARIANT_SUPPORT/d' \
 	-e '/WITNESS/d' \
-	-e '/MSDOS/d' \
-	-e '/pcm/d' \
-	-e '/atapist/d' \
-	-e '/ugen/d' \
-	-e '/uhid/d' \
-	-e '/ulpt/d' \
-	-e '/urio/d' \
-	-e '/uscanner/d' \
+	-e '/	pci$/d' \
+	-e '/	adv	/d' \
+	-e '/	ahc	/d' \
+	-e '/	amd	/d' \
+	-e '/	isp	/d' \
+	-e '/	sym	/d' \
+	-e '/	ncr	/d' \
+	-e '/	pcm/d' \
+	-e '/	atapist	/d' \
+	-e '/	lpt	/d' \
+	-e '/	ppi	/d' \
+	-e '/	de	/d' \
+	-e '/	txp	/d' \
+	-e '/	vx	/d' \
+	-e '/	dc	/d' \
+	-e '/	fxp	/d' \
+	-e '/	pcn	/d' \
+	-e '/	rl	/d' \
+	-e '/	sf	/d' \
+	-e '/	sis	/d' \
+	-e '/	ste	/d' \
+	-e '/	tl	/d' \
+	-e '/	tx	/d' \
+	-e '/	vr	/d' \
+	-e '/	wb	/d' \
+	-e '/	xl	/d' \
+	-e '/	ugen	/d' \
+	-e '/	uhid	/d' \
+	-e '/	ulpt	/d' \
+	-e '/	urio	/d' \
+	-e '/	uscanner	/d' \
 	-e '/maxusers/d' \
 	-e 's/ident.*GENERIC/ident		BOOTMFS/g'
 
 else
 
-sed	-e '/pty/d' \
-	-e '/pass/d' \
-	-e '/	apm/d' \
-	-e '/pmtimer/d' \
-	-e '/ppp/d' \
-	-e '/gif/d' \
-	-e '/faith/d' \
-	-e '/gzip/d' \
-	-e '/random/d' \
-	-e '/splash/d' \
+sed	-e '/	pty	/d' \
+	-e '/	pass	/d' \
+	-e '/	apm$/d' \
+	-e '/	pmtimer$/d' \
+	-e '/	ppp	/d' \
+	-e '/	gif	/d' \
+	-e '/	faith	/d' \
+	-e '/	random	/d' \
+	-e '/	splash$/d' \
 	-e '/PROCFS/d' \
 	-e '/KTRACE/d' \
 	-e '/SYSV/d' \
@@ -95,20 +91,19 @@ sed	-e '/pty/d' \
  	-e '/INVARIANTS/d' \
  	-e '/INVARIANT_SUPPORT/d' \
  	-e '/WITNESS/d' \
-	-e '/	ncr/d' \
-	-e '/pcm/d' \
-	-e '/atapist/d' \
-	-e '/wds/d' \
-	-e '/lpt/d' \
-	-e '/ppi/d' \
-	-e '/	txp/d' \
-	-e '/	sf/d' \
-	-e '/	ste/d' \
-	-e '/ugen/d' \
-	-e '/uhid/d' \
-	-e '/ulpt/d' \
-	-e '/urio/d' \
-	-e '/uscanner/d' \
+	-e '/	ncr	/d' \
+	-e '/	pcm/d' \
+	-e '/	atapist	/d' \
+	-e '/	lpt	/d' \
+	-e '/	ppi	/d' \
+	-e '/	txp	/d' \
+	-e '/	sf	/d' \
+	-e '/	ste	/d' \
+	-e '/	ugen	/d' \
+	-e '/	uhid	/d' \
+	-e '/	ulpt	/d' \
+	-e '/	urio	/d' \
+	-e '/	uscanner	/d' \
 	-e '/maxusers/d' \
 	-e 's/ident.*GENERIC/ident		BOOTMFS/g'
 
