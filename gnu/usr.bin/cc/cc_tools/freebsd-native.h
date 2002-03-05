0bd13d931327a59ece878c9310fcdc329120f679
obrien <obrien@FreeBSD.org>
2002-03-05 01:34:00 +0000

    Crap.  Pretend this was part of rev 1.5.2.6.

--- gnu/usr.bin/cc/cc_tools/freebsd-native.h
+++ gnu/usr.bin/cc/cc_tools/freebsd-native.h
@@ -29,7 +29,7 @@
 
 #undef  TOOLDIR_BASE_PREFIX		/* Old??  This is not documented. */
 #define STANDARD_EXEC_PREFIX		PREFIX"/libexec/"
-#define MD_EXEC_PREFIX			/* We don't want one. */
+#undef  MD_EXEC_PREFIX			/* We don't want one. */
 
 /* Under FreeBSD, the normal location of the various *crt*.o files is the
    /usr/lib directory.  */
