12683c99a53a78f60ca655a76516fac074785b4d
jake <jake@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-03-10 01:25:47 +0000

    Make this build on sparc64.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@91976 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ usr.bin/gprof/sparc64.c
@@ -0,0 +1,13 @@
+/* $FreeBSD$ */
+
+#include "gprof.h"
+
+/*
+ * gprof -c isn't currently supported...
+ */
+findcall( parentp , p_lowpc , p_highpc )
+    nltype		*parentp;
+    unsigned long	p_lowpc;
+    unsigned long	p_highpc;
+{
+}
