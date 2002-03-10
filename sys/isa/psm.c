6bbe628e1d59bc70ac4f2d52534ae6247b1c8a44
takawata <takawata@FreeBSD.org>
2002-03-10 12:22:17 +0000

    Add SONY VAIO psm controller ISA-PnP ID.

--- sys/isa/psm.c
+++ sys/isa/psm.c
@@ -2881,6 +2881,7 @@ static struct isa_pnp_id psmcpnp_ids[] = {
 	{ 0x1303d041, "PS/2 port" },			/* PNP0313, XXX */
 	{ 0x80374d24, "IBM PS/2 mouse port" },		/* IBM3780, ThinkPad */
 	{ 0x81374d24, "IBM PS/2 mouse port" },		/* IBM3781, ThinkPad */
+	{ 0x0490d94d, "SONY VAIO PS/2 mouse port"},     /* SNY9004, Vaio*/
 	{ 0 }
 };
 
