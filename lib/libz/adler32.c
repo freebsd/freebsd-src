b4e491dc91944c92222734544989af89f92a8f29
green <green@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-03-11 22:36:26 +0000

    Fix conflicts.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@92114 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libz/adler32.c
+++ lib/libz/adler32.c
@@ -1,5 +1,5 @@
 /* adler32.c -- compute the Adler-32 checksum of a data stream
- * Copyright (C) 1995-1998 Mark Adler
+ * Copyright (C) 1995-2002 Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h 
  */
 
