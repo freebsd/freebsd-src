beb648e82be6e8579ce0eb81e53842d0e9ed5542
Alexey Zelkin <phantom@FreeBSD.org>
2002-03-12 17:24:54 +0000

    MFC: sync with -CURRENT's stdtime code
    This MFC brings to -STABLE following changes/features:
    
    . get rid of non-standard %E[Ff] formats
    . add %F per POSIX
    . add %r per POSIX (ampm_fmt)
    . hardcode c_fmt in a different way since used in nl_langinfo now
    . make '%A' and '%a', '%B' and '%b' same per POSIX
    . introduce non-standard md_order (month/day order locale field).
      This field is available to user-land apps via nl_langinfo(3)
      and supposed to replace all usages of %E[Ff] in more correct way.
    . allow POSIX legal data on 3 chars abbrev, name length
      and %c format
    
    WARNING: please beware about this commit. It can trigger some
    hidden or implicit problems with locale/time specific functions or
    related/dependent stuff in userland applications. Please report
    anything unusual!
    
    Following files/revisions were MFCed:
    
    	Makefile.inc	1.11
    	strftime.3	1.25, 1.26
    	strftime.c	1.29, 1.30, 1.32 - 1.34
    	strptime.c	1.20 - 1.25
    	timelocal.c	1.12 - 1.22
    	timelocal.h	1.7 - 1.11

--- lib/libc/stdtime/timelocal.c
+++ lib/libc/stdtime/timelocal.c
@@ -1,4 +1,5 @@
 /*-
+ * Copyright (c) 2001 Alexey Zelkin <phantom@FreeBSD.org>
  * Copyright (c) 1997 FreeBSD Inc.
  * All rights reserved.
  *
@@ -26,30 +27,18 @@
  * $FreeBSD$
  */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/syslimits.h>
-#include <fcntl.h>
-#include <locale.h>
 #include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
-#include "setlocale.h"
+
+#include "ldpart.h"
 #include "timelocal.h"
 
-static int split_lines(char *, const char *);
-static void set_from_buf(const char *, int);
+static struct lc_time_T _time_locale;
+static int _time_using_locale;
+static char *time_locale_buf;
 
-struct lc_time_T _time_localebuf;
-int _time_using_locale;
+#define LCTIME_SIZE (sizeof(struct lc_time_T) / sizeof(char *))
 
-#define	LCTIME_SIZE_FULL (sizeof(struct lc_time_T) / sizeof(char *))
-#define	LCTIME_SIZE_1 \
-	(offsetof(struct lc_time_T, alt_month[0]) / sizeof(char *))
-#define LCTIME_SIZE_2 \
-	(offsetof(struct lc_time_T, Ef_fmt) / sizeof(char *))
-
-const struct lc_time_T	_C_time_locale = {
+static const struct lc_time_T	_C_time_locale = {
 	{
 		"Jan", "Feb", "Mar", "Apr", "May", "Jun",
 		"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
@@ -68,19 +57,18 @@ const struct lc_time_T	_C_time_locale = {
 	"%H:%M:%S",
 
 	/*
-	** x_fmt
-	** Since the C language standard calls for
-	** "date, using locale's date format," anything goes.
-	** Using just numbers (as here) makes Quakers happier;
-	** it's also compatible with SVR4.
-	*/
+	 * x_fmt
+	 * Since the C language standard calls for
+	 * "date, using locale's date format," anything goes.
+	 * Using just numbers (as here) makes Quakers happier;
+	 * it's also compatible with SVR4.
+	 */
 	"%m/%d/%y",
 
 	/*
-	** c_fmt (ctime-compatible)
-	** Not used, just compatibility placeholder.
-	*/
-	NULL,
+	 * c_fmt
+	 */
+	"%a %b %e %H:%M:%S %Y",
 
 	/* am */
 	"AM",
@@ -89,155 +77,43 @@ const struct lc_time_T	_C_time_locale = {
 	"PM",
 
 	/* date_fmt */
-	"%a %Ef %X %Z %Y",
+	"%a %b %e %H:%M:%S %Z %Y",
 	
+	/* alt_month
+	 * Standalone months forms for %OB
+	 */
 	{
 		"January", "February", "March", "April", "May", "June",
 		"July", "August", "September", "October", "November", "December"
 	},
 
-	/* Ef_fmt
-	** To determine short months / day order
-	*/
-	"%b %e",
+	/* md_order
+	 * Month / day order in dates
+	 */
+	"md",
 
-	/* EF_fmt
-	** To determine long months / day order
-	*/
-	"%B %e"
+	/* ampm_fmt
+	 * To determine 12-hour clock format time (empty, if N/A)
+	 */
+	"%I:%M:%S %p"
 };
 
+struct lc_time_T *
+__get_current_time_locale(void) {
+	return (_time_using_locale
+		? &_time_locale
+		: (struct lc_time_T *)&_C_time_locale);
+}
 
 int
-__time_load_locale(const char *name)
-{
-	static char *		locale_buf;
-	static char		locale_buf_C[] = "C";
-	static int		num_lines;
+__time_load_locale(const char *name) {
 
-	int			fd;
-	char *			lbuf;
-	char *			p;
-	const char *		plim;
-	char                    filename[PATH_MAX];
-	struct stat		st;
-	size_t			namesize;
-	size_t			bufsize;
-	int                     save_using_locale;
+	int	ret;
 
-	save_using_locale = _time_using_locale;
-	_time_using_locale = 0;
+	ret = __part_load_locale(name, &_time_using_locale,
+			time_locale_buf, "LC_TIME",
+			LCTIME_SIZE, LCTIME_SIZE,
+			(const char **)&_time_locale);
 
-	if (name == NULL)
-		goto no_locale;
-
-	if (!strcmp(name, "C") || !strcmp(name, "POSIX"))
-		return 0;
-
-	/*
-	** If the locale name is the same as our cache, use the cache.
-	*/
-	lbuf = locale_buf;
-	if (lbuf != NULL && strcmp(name, lbuf) == 0) {
-		set_from_buf(lbuf, num_lines);
-		_time_using_locale = 1;
-		return 0;
-	}
-	/*
-	** Slurp the locale file into the cache.
-	*/
-	namesize = strlen(name) + 1;
-
-	if (!_PathLocale)
-		goto no_locale;
-	/* Range checking not needed, 'name' size is limited */
-	strcpy(filename, _PathLocale);
-	strcat(filename, "/");
-	strcat(filename, name);
-	strcat(filename, "/LC_TIME");
-	fd = _open(filename, O_RDONLY);
-	if (fd < 0)
-		goto no_locale;
-	if (fstat(fd, &st) != 0)
-		goto bad_locale;
-	if (st.st_size <= 0)
-		goto bad_locale;
-	bufsize = namesize + st.st_size;
-	locale_buf = NULL;
-	lbuf = (lbuf == NULL || lbuf == locale_buf_C) ?
-		malloc(bufsize) : reallocf(lbuf, bufsize);
-	if (lbuf == NULL)
-		goto bad_locale;
-	(void) strcpy(lbuf, name);
-	p = lbuf + namesize;
-	plim = p + st.st_size;
-	if (_read(fd, p, (size_t) st.st_size) != st.st_size)
-		goto bad_lbuf;
-	if (_close(fd) != 0)
-		goto bad_lbuf;
-	/*
-	** Parse the locale file into localebuf.
-	*/
-	if (plim[-1] != '\n')
-		goto bad_lbuf;
-	num_lines = split_lines(p, plim);
-	if (num_lines >= LCTIME_SIZE_FULL)
-		num_lines = LCTIME_SIZE_FULL;
-	else if (num_lines >= LCTIME_SIZE_2)
-		num_lines = LCTIME_SIZE_2;
-	else if (num_lines >= LCTIME_SIZE_1)
-		num_lines = LCTIME_SIZE_1;
-	else
-		goto reset_locale;
-	set_from_buf(lbuf, num_lines);
-	/*
-	** Record the successful parse in the cache.
-	*/
-	locale_buf = lbuf;
-
-	_time_using_locale = 1;
-	return 0;
-
-reset_locale:
-	/*
-	 * XXX - This may not be the correct thing to do in this case.
-	 * setlocale() assumes that we left the old locale alone.
-	 */
-	locale_buf = locale_buf_C;
-	_time_localebuf = _C_time_locale;
-	save_using_locale = 0;
-bad_lbuf:
-	free(lbuf);
-bad_locale:
-	(void)_close(fd);
-no_locale:
-	_time_using_locale = save_using_locale;
-	return -1;
-}
-
-static int
-split_lines(char *p, const char *plim)
-{
-	int i;
-
-	for (i = 0; p < plim; i++) {
-		p = strchr(p, '\n');
-		*p++ = '\0';
-	}
-	return i;
-}
-
-static void
-set_from_buf(const char *p, int num_lines)
-{
-	const char **ap;
-	int i;
-
-	for (ap = (const char **) &_time_localebuf, i = 0;
-	    i < num_lines; ++ap, ++i)
-		*ap = p += strlen(p) + 1;
-	if (num_lines >= LCTIME_SIZE_2)
-		return;
-	for (i = 0; i < 12; i++)
-		_time_localebuf.alt_month[i] = _time_localebuf.month[i];
+	return (ret);
 }
