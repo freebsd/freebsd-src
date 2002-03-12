238080adceca8e0a1e8cc35898c64bb4bcf67b09
phantom <phantom@FreeBSD.org>
2002-03-12 19:51:51 +0000

    MFC: use AM/PM time only when available in locale, correctly handle comma as load avg. separator
    	extern.hs  rev 1.4
    	pr_time.c  revs 1.15, 1.16, 1.17
    	w.c        revs 1.42, 1.43, 1.44

--- usr.bin/w/w.c
+++ usr.bin/w/w.c
@@ -71,6 +71,8 @@ static const char rcsid[] =
 #include <fcntl.h>
 #include <kvm.h>
 #include <limits.h>
+#include <langinfo.h>
+#include <locale.h>
 #include <netdb.h>
 #include <nlist.h>
 #include <paths.h>
@@ -80,7 +82,6 @@ static const char rcsid[] =
 #include <unistd.h>
 #include <utmp.h>
 #include <vis.h>
-#include <locale.h>
 
 #include <arpa/nameser.h>
 #include <resolv.h>
@@ -99,6 +100,8 @@ int		header = 1;	/* true if -h flag: don't print heading */
 int		nflag;		/* true if -n flag: don't convert addrs */
 int		dflag;		/* true if -d flag: output debug info */
 int		sortidle;	/* sort by idle time */
+int		use_ampm;	/* use AM/PM time */
+int             use_comma;      /* use comma as floats separator */
 char	      **sel_users;	/* login array of particular users selected */
 char		domain[MAXHOSTNAMELEN];
 
@@ -141,6 +144,8 @@ main(argc, argv)
 	char buf[MAXHOSTNAMELEN], errbuf[_POSIX2_LINE_MAX];
 
 	(void)setlocale(LC_ALL, "");
+	use_ampm = (*nl_langinfo(T_FMT_AMPM) != '\0');
+	use_comma = (*nl_langinfo(RADIXCHAR) != ',');
 
 	/* Are we w(1) or uptime(1)? */
 	if (this_is_uptime(argv[0]) == 0) {
@@ -436,7 +441,8 @@ pr_header(nowp, nusers)
 	/*
 	 * Print time of day.
 	 */
-	(void)strftime(buf, sizeof(buf) - 1, "%l:%M%p", localtime(nowp));
+	(void)strftime(buf, sizeof(buf)	- 1,
+		       use_ampm	? "%l:%M%p" : "%k:%M", localtime(nowp));
 	buf[sizeof(buf) - 1] = '\0';
 	(void)printf("%s ", buf);
 
@@ -481,8 +487,11 @@ pr_header(nowp, nusers)
 		(void)printf(", no load average information available\n");
 	else {
 		(void)printf(", load averages:");
-		for (i = 0; i < (sizeof(avenrun) / sizeof(avenrun[0])); i++)
-			(void)printf("%s %.2f", i > 0 ? "," : "", avenrun[i]);
+		for (i = 0; i < (sizeof(avenrun) / sizeof(avenrun[0])); i++) {
+			if (use_comma && i > 0)
+				(void)printf(",");
+			(void)printf(" %.2f", avenrun[i]);
+		}
 		(void)printf("\n");
 	}
 }
