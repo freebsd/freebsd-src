856bb440c7eb9f6003a4aaff98725b3b43303b42
Dag-Erling Smørgrav <des@FreeBSD.org>
2002-03-14 20:42:07 +0000

    Vendor import of OpenPAM Centaury.

--- contrib/openpam/lib/pam_get_data.c
+++ contrib/openpam/lib/pam_get_data.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2002 Networks Associates Technologies, Inc.
+ * Copyright (c) 2002 Networks Associates Technology, Inc.
  * All rights reserved.
  *
  * This software was developed for the FreeBSD Project by ThinkSec AS and
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $P4: //depot/projects/openpam/lib/pam_get_data.c#6 $
+ * $P4: //depot/projects/openpam/lib/pam_get_data.c#7 $
  */
 
 #include <string.h>
--- contrib/openpam/lib/pam_set_item.c
+++ contrib/openpam/lib/pam_set_item.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2002 Networks Associates Technologies, Inc.
+ * Copyright (c) 2002 Networks Associates Technology, Inc.
  * All rights reserved.
  *
  * This software was developed for the FreeBSD Project by ThinkSec AS and
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $P4: //depot/projects/openpam/lib/pam_set_item.c#11 $
+ * $P4: //depot/projects/openpam/lib/pam_set_item.c#12 $
  */
 
 #include <sys/param.h>
