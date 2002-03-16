a2e0658045625ad772d6de1578fc13c05f5758bb
Dag-Erling Smørgrav <des@FreeBSD.org>
2002-03-16 00:25:53 +0000

    Move the definition of PT_[GS]ET{,DB,FP}REGS from the MD ptrace.h to the MI ptrace.h, since all platforms define them.  Keep the MD ptrace.h around for FIX_SSTEP (which is currently only needed on Alpha).

--- sys/i386/include/ptrace.h
+++ sys/i386/include/ptrace.h
@@ -37,14 +37,4 @@
 #ifndef _MACHINE_PTRACE_H_
 #define _MACHINE_PTRACE_H_
 
-/*
- * Machine dependent trace commands.
- */
-#define PT_GETREGS      (PT_FIRSTMACH + 1)
-#define PT_SETREGS      (PT_FIRSTMACH + 2)
-#define PT_GETFPREGS    (PT_FIRSTMACH + 3)
-#define PT_SETFPREGS    (PT_FIRSTMACH + 4)
-#define PT_GETDBREGS    (PT_FIRSTMACH + 5)
-#define PT_SETDBREGS    (PT_FIRSTMACH + 6)
-
 #endif
