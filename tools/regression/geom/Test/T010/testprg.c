804ad7989bca49b47ffde3269eda03750d32b3d8
phk <phk@FreeBSD.org>
2002-03-17 18:53:58 +0000

    Add the GEOM regression test framework.
    This is a set of userland shims in which GEOM can be run through simple
    tests.
    
    The simulation of kernel synchronization primitives is very primitive
    and consequently some times tests will fail because of races.
    
    Data/ contains a number of files in XML format which describe the
    key sectors for a number of disk images
    
    This is a very handy tool for people developing GEOM methods.  The
    "simdisk" method can be told to read from a "real disk" and afterwards
    dump the accessed sectors in XML format for further use.
    
    I hope future method writes will see the benefit of this test
    collection and add to it when they write methods for GEOM.
    
    You will need ports/textproc/expat for the XML parser.
    
    Sponsored by:   DARPA, NAI Labs.

---
+++ tools/regression/geom/Test/T010/testprg.c
@@ -0,0 +1,98 @@
+/*-
+ * Copyright (c) 2002 Poul-Henning Kamp
+ * Copyright (c) 2002 Networks Associates Technology, Inc.
+ * All rights reserved.
+ *
+ * This software was developed for the FreeBSD Project by Poul-Henning Kamp
+ * and NAI Labs, the Security Research Division of Network Associates, Inc.
+ * under DARPA/SPAWAR contract N66001-01-C-8035 ("CBOSS"), as part of the
+ * DARPA CHATS research program.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The names of the authors may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <err.h>
+#include <sys/errno.h>
+#include <sys/time.h>
+#include <sys/sbuf.h>
+#include <geom/geom.h>
+
+int
+thread_sim(void *ptr __unused)
+{
+	struct g_consumer *cp;
+	struct g_geom *gp;
+
+	rattle();
+	g_simdisk_init();
+	g_bsd_init();
+	g_mbr_init();
+	g_mbrext_init();
+	g_dev_init(NULL);
+	rattle();
+
+	g_simdisk_xml_load("ad0", "../../Data/disk.empty.flp.xml");
+	rattle();
+	cp = g_dev_opendev("ad0", 1, 0, 0);
+	g_topology_lock();
+	gp = g_create_geomf("BSD-method", cp->provider, NULL);
+	g_topology_unlock();
+	printf("gp = %p\n", gp);
+	rattle();
+	conff("1");
+	sdumpf("2");
+#if 0
+	g_simdisk_stop("ad0");
+	bp = g_new_bio();
+	bp->bio_cmd = BIO_READ;
+	bp->bio_offset = 0;
+	bp->bio_length = 512;
+	bp->bio_data = g_malloc(512, M_WAITOK);
+	g_dev_request("ad0s1a", bp);
+	rattle();
+	sdumpf("2a");
+	g_simdisk_destroy("ad0");
+	rattle();
+	conff("1");
+	sdumpf("2b");
+	g_simdisk_restart("ad0");
+	rattle();
+	conff("1");
+	sdumpf("2c");
+
+#endif
+	printf("Done\n");
+	done();
+	return (0);
+}
+
---
+++ tools/regression/geom/geom_simdev.c
@@ -0,0 +1,159 @@
+/*-
+ * Copyright (c) 2002 Poul-Henning Kamp
+ * Copyright (c) 2002 Networks Associates Technology, Inc.
+ * All rights reserved.
+ *
+ * This software was developed for the FreeBSD Project by Poul-Henning Kamp
+ * and NAI Labs, the Security Research Division of Network Associates, Inc.
+ * under DARPA/SPAWAR contract N66001-01-C-8035 ("CBOSS"), as part of the
+ * DARPA CHATS research program.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The names of the authors may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+
+#include <sys/param.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <err.h>
+#include <sys/errno.h>
+#include <sys/time.h>
+#include <geom/geom.h>
+
+
+static struct g_geom *
+dev_taste(struct g_method *mp, struct g_provider *pp, struct thread *tp __unused, int insist __unused)
+{
+	struct g_geom *gp;
+	struct g_consumer *cp;
+
+	g_trace(G_T_TOPOLOGY, "dev_taste(%s,%s)", mp->name, pp->name);
+	g_topology_assert();
+	LIST_FOREACH(cp, &pp->consumers, consumers) {
+		if (cp->geom->method == mp) {
+			g_topology_unlock();
+			return (NULL);
+		}
+	}
+	gp = g_new_geomf(mp, pp->name);
+	cp = g_new_consumer(gp);
+	g_attach(cp, pp);
+	return (gp);
+}
+
+
+static void
+g_dev_orphan(struct g_consumer *cp, struct thread *tp __unused)
+{
+	struct g_geom *gp;
+
+	gp = cp->geom;
+	gp->flags |= G_GEOM_WITHER;
+	g_trace(G_T_TOPOLOGY, "g_dev_orphan(%p(%s))", cp, gp->name);
+	if (cp->biocount > 0)
+		return;
+	if (cp->acr  > 0 || cp->acw  > 0 || cp->ace > 0)
+		g_access_rel(cp, -cp->acr, -cp->acw, -cp->ace);
+	g_dettach(cp);
+	g_destroy_consumer(cp);
+	g_destroy_geom(gp);
+}
+
+
+static struct g_method dev_method	= {
+	"DEV-method",
+	dev_taste,
+	NULL,
+	g_dev_orphan,
+	NULL,
+	G_METHOD_INITSTUFF
+};
+
+static struct g_geom *
+g_dev_findg(char *name)
+{
+	struct g_geom *gp;
+
+	LIST_FOREACH(gp, &dev_method.geom, geom)
+		if (!strcmp(gp->name, name))
+			break;
+	return (gp);
+}
+
+void
+g_dev_init(void *junk __unused)
+{
+
+	g_add_method(&dev_method);
+}
+
+
+struct g_consumer *
+g_dev_opendev(char *name, int r, int w, int e)
+{
+	struct g_geom *gp;
+	struct g_consumer *cp;
+	int error;
+
+	gp = g_dev_findg(name);
+	if (gp == NULL)
+		return (NULL);
+	g_topology_lock();
+	cp = LIST_FIRST(&gp->consumer);
+	error = g_access_rel(cp, r, w, e);
+	g_topology_unlock();
+	if (error)
+		return(NULL);
+	return(cp);
+}
+
+static void
+g_dev_done(struct bio *bp)
+{
+
+	if (bp->bio_from->biocount > 0)
+		return;
+	g_topology_lock();
+	g_dev_orphan(bp->bio_from, NULL);
+	g_topology_unlock();
+}
+
+int
+g_dev_request(char *name, struct bio *bp)
+{
+	struct g_geom *gp;
+
+	gp = g_dev_findg(name);
+	if (gp == NULL)
+		return (-1);
+	bp->bio_done = g_dev_done;
+	g_io_request(bp, LIST_FIRST(&gp->consumer));
+	return (1);
+}
