732ca9dc6a28f477a9936d8ef375a0d3f338070d
alfred <alfred@FreeBSD.org>
2002-03-20 02:15:46 +0000

    Remove __P.

--- sys/libkern/bsearch.c
+++ sys/libkern/bsearch.c
@@ -63,7 +63,7 @@ bsearch(key, base0, nmemb, size, compar)
 	const void *base0;
 	size_t nmemb;
 	register size_t size;
-	register int (*compar) __P((const void *, const void *));
+	register int (*compar)(const void *, const void *);
 {
 	register const char *base = base0;
 	register size_t lim;
