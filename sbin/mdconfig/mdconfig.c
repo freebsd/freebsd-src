40dcc8608de0d9badccfe8f9ce6883aecc616096
Mike Barcroft <mike@FreeBSD.org>
2002-03-21 12:03:09 +0000

    Revert 1.20: Memory disks may be used for other purposes besides newfs(8), so it isn't helpful to require the minimum size meet newfs(8)'s criteria.

--- sbin/mdconfig/mdconfig.c
+++ sbin/mdconfig/mdconfig.c
@@ -144,8 +144,6 @@ main(int argc, char **argv)
 				mdio.md_size *= (1024 * 1024 * 1024 / DEV_BSIZE);
 			else
 				errx(1, "Unknown suffix on -s argument");
-			if (mdio.md_size < 192)
-				errx(1, "Filesystem too small");
 			break;
 		case 'u':
 			if (cmdline != 2 && cmdline != 3)
