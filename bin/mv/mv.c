0302f42b510f3914213d7e72c708879257152069
David E. O'Brien <obrien@FreeBSD.org>
2002-03-22 19:52:59 +0000

    Update SCM ID.

--- bin/mv/mv.c
+++ bin/mv/mv.c
@@ -44,9 +44,9 @@ static char const copyright[] =
 #if 0
 static char sccsid[] = "@(#)mv.c	8.2 (Berkeley) 4/2/94";
 #endif
-static const char rcsid[] =
-  "$FreeBSD$";
 #endif /* not lint */
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
 #include <sys/time.h>
