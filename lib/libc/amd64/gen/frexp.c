4b40711fbd4d3dbca56bef803d7c2ec3b815e767
obrien <obrien@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-03-23 02:10:28 +0000

    Restore CSRG ID's lost in January 1995.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@92999 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/amd64/gen/frexp.c
+++ lib/libc/amd64/gen/frexp.c
@@ -31,6 +31,9 @@
  * SUCH DAMAGE.
  */
 
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)frexp.c	8.1 (Berkeley) 6/4/93";
+#endif /* LIBC_SCCS and not lint */
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
