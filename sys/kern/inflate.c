a83aaaa1fa7fa32356cc0916bcd6a05ce1292f10
phk <phk@FreeBSD.org>
2002-03-25 13:52:45 +0000

    Modernize my email address.

--- sys/kern/inflate.c
+++ sys/kern/inflate.c
@@ -2,7 +2,7 @@
  * Most parts of this file are not covered by:
  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
- * <phk@login.dknet.dk> wrote this file.  As long as you retain this notice you
+ * <phk@FreeBSD.org> wrote this file.  As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
  * ----------------------------------------------------------------------------
