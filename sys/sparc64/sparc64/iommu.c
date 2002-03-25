db60769c8fca4758b2f9fa8136cbb435457651ca
tmm <tmm@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-03-25 04:34:52 +0000

    Remove second copy of iommu_decode_fault() which I accidentially added.
    Pointy hat to:	tmm
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@93118 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sparc64/sparc64/iommu.c
+++ sys/sparc64/sparc64/iommu.c
@@ -433,21 +433,6 @@ iommu_decode_fault(struct iommu_state *is, vm_offset_t phys)
 	printf("IOMMU fault virtual address %#lx\n", (u_long)va);
 }
 
-void
-iommu_decode_fault(struct iommu_state *is, vm_offset_t phys)
-{
-	bus_addr_t va;
-	long idx;
-
-	idx = phys - is->is_ptsb;
-	if (phys < is->is_ptsb ||
-	    idx > (PAGE_SIZE << is->is_tsbsize))
-		return;
-	va = is->is_dvmabase +
-	    (((bus_addr_t)idx >> IOTTE_SHIFT) << IO_PAGE_SHIFT);
-	printf("IOMMU fault virtual address %#lx\n", (u_long)va);
-}
-
 static int
 iommu_strbuf_flush_done(struct iommu_state *is)
 {
