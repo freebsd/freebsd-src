de491c511736bd5fbcde5171baedf861680ca055
obrien <obrien@FreeBSD.org>
2002-03-26 02:59:00 +0000

    Get the guarding right.  The IA-64 has a different organization for this than our other platforms.

--- sys/ia64/include/ansi.h
+++ sys/ia64/include/ansi.h
@@ -73,14 +73,13 @@
 
 #ifdef __GNUC__
 #define	_BSD_VA_LIST_	__builtin_va_list	/* internally known to gcc */
-typedef _BSD_VA_LIST_	__gnuc_va_list;		/* compatibility w/GNU headers*/
-#else
-#error Must add va_list support for this non-GCC compiler.
-#endif /*__GNUC__*/
-#if defined __GNUC__ && !defined(__GNUC_VA_LIST) && !defined(__NO_GNUC_VA_LIST)
+#if !defined(__GNUC_VA_LIST) && !defined(__NO_GNUC_VA_LIST)
 #define __GNUC_VA_LIST
 typedef _BSD_VA_LIST_ __gnuc_va_list;		/* compatibility w/GNU headers*/
 #endif
+#else
+#error Must add va_list support for this non-GCC compiler.
+#endif /*__GNUC__*/
 
 /*
  * The rune type above is declared to be an ``int'' instead of the more natural
