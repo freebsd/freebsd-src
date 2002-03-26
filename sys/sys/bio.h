3d1eaf5230c1501812cb5815688365e0caa84bf6
phk <phk@FreeBSD.org>
2002-03-26 19:16:37 +0000

    Push BIO_FORMAT into a local hack inside the floppy drivers where it belongs.

--- sys/sys/bio.h
+++ sys/sys/bio.h
@@ -95,9 +95,8 @@ struct bio {
 #define BIO_READ	0x00000001
 #define BIO_WRITE	0x00000002
 #define BIO_DELETE	0x00000004
-#define BIO_FORMAT	0x00000008
-#define BIO_GETATTR	0x00000010
-#define BIO_SETATTR	0x00000020
+#define BIO_GETATTR	0x00000008
+#define BIO_SETATTR	0x00000010
 #define BIO_CMD1	0x40000000	/* Available for local hacks */
 #define BIO_CMD2	0x80000000	/* Available for local hacks */
 
