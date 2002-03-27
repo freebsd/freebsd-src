6fc7b42212d2ccbe79f6920e3292c45527c9b978
David E. O'Brien <obrien@FreeBSD.org>
2002-03-27 04:55:43 +0000

    Enhance GDB's abillity WRT stack traces where signal handlers are involved. Correct backtrace was made more complex when the new signal trampoline was introduced to support more than 32 signals, while keeping a modified version of the old signal trampoline.
    The 'where' command will now show:
    
    	#2  <signal handler called>
    
    where appropiate.
    
    Submitted by:	Tor.Egge@fast.no

--- gnu/usr.bin/binutils/gdb/i386/tm.h
+++ gnu/usr.bin/binutils/gdb/i386/tm.h
@@ -43,18 +43,22 @@ extern CORE_ADDR fbsd_kern_frame_saved_pc (struct frame_info *);
 #define FRAME_SAVED_PC(FRAME) \
   (kernel_debugging ? fbsd_kern_frame_saved_pc(FRAME) : \
   (((FRAME)->signal_handler_caller \
-    ? sigtramp_saved_pc (FRAME) \
+    ? fbsd_sigtramp_saved_pc (FRAME) \
     : read_memory_integer ((FRAME)->frame + 4, 4)) \
    ))
 
-/* On FreeBSD, sigtramp has size 0x18 and is immediately below the
+/* On FreeBSD, sigtramp has size 0x48 and is immediately below the
    ps_strings struct which has size 0x10 and is at the top of the
    user stack.  */
 
 #undef SIGTRAMP_START
 #undef SIGTRAMP_END
-#define SIGTRAMP_START(pc)	0xbfbfdfd8
-#define SIGTRAMP_END(pc)	0xbfbfdff0
+#define SIGTRAMP_START(pc)	0xbfbfffa8
+#define SIGTRAMP_END(pc)	0xbfbffff0
+#undef  SIGCONTEXT_PC_OFFSET
+#define OSIGCONTEXT_PC_OFFSET 20
+#define NSIGCONTEXT_PC_OFFSET 76
+#define OSIGCODE_MAGIC_OFFSET 20
 
 struct objfile;
 void freebsd_uthread_new_objfile PARAMS ((struct objfile *objfile));
