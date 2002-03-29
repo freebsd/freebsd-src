c65b06dde4f155866b7aafadc8e3c11bb720f5b2
murray <murray@FreeBSD.org>
2002-03-29 23:03:17 +0000

    Only build and link the pccard module on architectures that support it (and that have room for pccardd on mfsroot.flp).

--- usr.sbin/sade/sade.h
+++ usr.sbin/sade/sade.h
@@ -54,6 +54,9 @@
 
 /*** Defines ***/
 
+#if defined(__i386__) || defined(PC98)
+#define PCCARD_ARCH 1		        /* Support PCCARD installations */
+#endif
 /* device limits */
 #define DEV_NAME_MAX		64	/* The maximum length of a device name	*/
 #define DEV_MAX			100	/* The maximum number of devices we'll deal with */
