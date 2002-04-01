826a9fe52662cd36f3450622b6a22009d38f3ef2
ken <ken@FreeBSD.org>
2002-04-01 03:59:47 +0000

    Fix an indentation problem.

--- sys/cam/scsi/scsi_all.c
+++ sys/cam/scsi/scsi_all.c
@@ -2040,13 +2040,13 @@ scsi_sense_sbuf(struct cam_device *device, struct ccb_scsiio *csio,
 					snprintf(tmpstr2, sizeof(tmpstr2),
 						 "bit %d",
 						sense->sense_key_spec[0] & 0x7);
-					sbuf_printf(sb,
-						   ": %s byte %d %s is invalid",
-						    bad_command ?
-						    "Command" : "Data",
-						    scsi_2btoul(
-						    &sense->sense_key_spec[1]),
-						    tmpstr2);
+				sbuf_printf(sb,
+					   ": %s byte %d %s is invalid",
+					    bad_command ?
+					    "Command" : "Data",
+					    scsi_2btoul(
+					    &sense->sense_key_spec[1]),
+					    tmpstr2);
 				break;
 			}
 			case SSD_KEY_RECOVERED_ERROR:
