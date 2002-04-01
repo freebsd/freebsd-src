106ab02ef1a33194e539d24b4c4b4e0cb6f74425
joe <joe@FreeBSD.org>
2002-04-01 15:37:00 +0000

    Tidy up the formatting so that it's in the style of NetBSD's copy of this file.

--- sys/dev/usb/usbdi_util.h
+++ sys/dev/usb/usbdi_util.h
@@ -39,36 +39,32 @@
  */
 
 usbd_status	usbd_get_desc(usbd_device_handle dev, int type, 
-				   int index, int len, void *desc);
+			      int index, int len, void *desc);
 usbd_status	usbd_get_config_desc(usbd_device_handle, int, 
-					  usb_config_descriptor_t *);
-usbd_status	usbd_get_config_desc_full(usbd_device_handle, int, 
-					       void *, int);
+				     usb_config_descriptor_t *);
+usbd_status	usbd_get_config_desc_full(usbd_device_handle, int, void *, int);
 usbd_status	usbd_get_device_desc(usbd_device_handle dev,
-					  usb_device_descriptor_t *d);
+				     usb_device_descriptor_t *d);
 usbd_status	usbd_set_address(usbd_device_handle dev, int addr);
 usbd_status	usbd_get_port_status(usbd_device_handle, 
-				      int, usb_port_status_t *);
+				     int, usb_port_status_t *);
 usbd_status	usbd_set_hub_feature(usbd_device_handle dev, int);
 usbd_status	usbd_clear_hub_feature(usbd_device_handle, int);
 usbd_status	usbd_set_port_feature(usbd_device_handle dev, int, int);
 usbd_status	usbd_clear_port_feature(usbd_device_handle, int, int);
-usbd_status	usbd_get_device_status(usbd_device_handle,usb_status_t*);
-usbd_status	usbd_get_hub_status(usbd_device_handle dev,
-					 usb_hub_status_t *st);
+usbd_status	usbd_get_device_status(usbd_device_handle, usb_status_t *);
+usbd_status	usbd_get_hub_status(usbd_device_handle, usb_hub_status_t *);
 usbd_status	usbd_set_protocol(usbd_interface_handle dev, int report);
-usbd_status	usbd_get_report_descriptor
-	(usbd_device_handle dev, int ifcno, int repid, int size, void *d);
-struct usb_hid_descriptor *usbd_get_hid_descriptor 
-	(usbd_interface_handle ifc);
-usbd_status	usbd_set_report 
-	(usbd_interface_handle iface,int type,int id,void *data,int len);
-usbd_status	usbd_set_report_async
-	(usbd_interface_handle iface,int type,int id,void *data,int len);
-usbd_status	usbd_get_report 
-	(usbd_interface_handle iface,int type,int id,void *data,int len);
-usbd_status	usbd_set_idle 
-	(usbd_interface_handle iface, int duration, int id);
+usbd_status	usbd_get_report_descriptor(usbd_device_handle dev, int ifcno,
+					   int repid, int size, void *d);
+struct usb_hid_descriptor *usbd_get_hid_descriptor(usbd_interface_handle ifc);
+usbd_status	usbd_set_report(usbd_interface_handle iface, int type, int id,
+				void *data,int len);
+usbd_status	usbd_set_report_async(usbd_interface_handle iface, int type,
+				      int id, void *data, int len);
+usbd_status	usbd_get_report(usbd_interface_handle iface, int type, int id,
+				void *data, int len);
+usbd_status	usbd_set_idle(usbd_interface_handle iface, int duration, int id);
 #if defined(__NetBSD__) || defined(__OpenBSD__)
 usbd_status	usbd_alloc_report_desc
 	(usbd_interface_handle ifc, void **descp, int *sizep, int mem);
@@ -76,22 +72,18 @@ usbd_status	usbd_alloc_report_desc
 usbd_status	usbd_alloc_report_desc
 	(usbd_interface_handle ifc, void **descp, int *sizep, struct malloc_type * mem);
 #endif
-usbd_status	usbd_get_config
-	(usbd_device_handle dev, u_int8_t *conf);
-usbd_status	usbd_get_string_desc
-	(usbd_device_handle dev, int sindex, int langid, 
-	     usb_string_descriptor_t *sdesc);
-void usbd_delay_ms (usbd_device_handle, u_int);
+usbd_status	usbd_get_config(usbd_device_handle dev, u_int8_t *conf);
+usbd_status	usbd_get_string_desc(usbd_device_handle dev, int sindex,
+				     int langid, usb_string_descriptor_t *sdesc);
+void		usbd_delay_ms(usbd_device_handle, u_int);
 
 
-usbd_status usbd_set_config_no
-	(usbd_device_handle dev, int no, int msg);
-usbd_status usbd_set_config_index
-	(usbd_device_handle dev, int index, int msg);
+usbd_status usbd_set_config_no(usbd_device_handle dev, int no, int msg);
+usbd_status usbd_set_config_index(usbd_device_handle dev, int index, int msg);
 
-usbd_status usbd_bulk_transfer
-	(usbd_xfer_handle xfer, usbd_pipe_handle pipe, u_int16_t flags,
-	     u_int32_t timeout, void *buf, u_int32_t *size, char *lbl);
+usbd_status usbd_bulk_transfer(usbd_xfer_handle xfer, usbd_pipe_handle pipe,
+			       u_int16_t flags, u_int32_t timeout, void *buf,
+			       u_int32_t *size, char *lbl);
 
 void usb_detach_wait(device_ptr_t);
 void usb_detach_wakeup(device_ptr_t);
