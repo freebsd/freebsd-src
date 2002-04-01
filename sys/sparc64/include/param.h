0f07be6872142812e02eb33d541ac4333b2401b7
jake <jake@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-04-01 21:55:00 +0000

    Move the CTASSERT macro from MD code to systm.h alongside KASSERT so other code can use it.  This takes a single constant argument and fails to compile if it is 0 (false).  The main application of this is to make assertions about structure sizes at compile time, in order to validate assumptions made in other code.  Examples:
    CTASSERT(sizeof(struct foo) == FOO_SIZEOF);
    CTASSERT(sizeof(struct foo) == (1 << FOO_SHIFT));
    
    Requested by:	jhb, phk
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@93600 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sparc64/include/param.h
+++ sys/sparc64/include/param.h
@@ -176,9 +176,5 @@
 
 #define	pgtok(x)		((unsigned long)(x) * (PAGE_SIZE / 1024))
 
-#define	CTASSERT(x)		_CTASSERT(x, __LINE__)
-#define	_CTASSERT(x, y)		__CTASSERT(x, y)
-#define	__CTASSERT(x, y)	typedef char __assert ## y[(x) ? 1 : -1]
-
 #endif /* !_MACHINE_PARAM_H_ */
 #endif /* !_NO_NAMESPACE_POLLUTION */
