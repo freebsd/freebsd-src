029f13c6714669bf95d48a9152392049a526da21
Matt Jacob <mjacob@FreeBSD.org>
2002-04-04 23:46:01 +0000

    Fix bus dma segment count to be based off of MAXPHYS, not BUS_SPACE_MAXSIZE. Grumble. I've seen better documented architectures out of Redmond.
    Redo fabric evaluation to not use GET ALL NEXT (GA_NXT). Switches seem
    to be trying to wriggle out of supporting this well. Instead, use
    GID_FT to get a list of Port IDs and then use GPN_ID/GNN_ID to find the
    port and node wwn. This should make working on fabrics a bit cleaner and
    more stable.
    
    This also caused some cleanup of SNS subcommand canonicalization so that
    we can actually check for FS_ACC and FS_RJT, and if we get an FS_RJT,
    print out the reason and explanation codes.
    
    We'll keep the old GA_NXT method around if people want to uncomment a
    controlling definition in ispvar.h.
    
    This also had us clean up ISPASYNC_FABRICDEV to use a local lportdb argument
    and to have the caller explicitly say that a device is at the end of the
    fabric list.
    
    MFC after:	1 week

--- sys/dev/isp/isp_freebsd.c
+++ sys/dev/isp/isp_freebsd.c
@@ -2734,43 +2734,13 @@ isp_async(struct ispsoftc *isp, ispasync_t cmd, void *arg)
 		break;
 	case ISPASYNC_FABRIC_DEV:
 	{
-		int target, lrange;
-		struct lportdb *lp = NULL;
-		char *pt;
-		sns_ganrsp_t *resp = (sns_ganrsp_t *) arg;
-		u_int32_t portid;
-		u_int64_t wwpn, wwnn;
+		int target, base, lim;
 		fcparam *fcp = isp->isp_param;
+		struct lportdb *lp = NULL;
+		struct lportdb *clp = (struct lportdb *) arg;
+		char *pt;
 
-		portid =
-		    (((u_int32_t) resp->snscb_port_id[0]) << 16) |
-		    (((u_int32_t) resp->snscb_port_id[1]) << 8) |
-		    (((u_int32_t) resp->snscb_port_id[2]));
-
-		wwpn =
-		    (((u_int64_t)resp->snscb_portname[0]) << 56) |
-		    (((u_int64_t)resp->snscb_portname[1]) << 48) |
-		    (((u_int64_t)resp->snscb_portname[2]) << 40) |
-		    (((u_int64_t)resp->snscb_portname[3]) << 32) |
-		    (((u_int64_t)resp->snscb_portname[4]) << 24) |
-		    (((u_int64_t)resp->snscb_portname[5]) << 16) |
-		    (((u_int64_t)resp->snscb_portname[6]) <<  8) |
-		    (((u_int64_t)resp->snscb_portname[7]));
-
-		wwnn =
-		    (((u_int64_t)resp->snscb_nodename[0]) << 56) |
-		    (((u_int64_t)resp->snscb_nodename[1]) << 48) |
-		    (((u_int64_t)resp->snscb_nodename[2]) << 40) |
-		    (((u_int64_t)resp->snscb_nodename[3]) << 32) |
-		    (((u_int64_t)resp->snscb_nodename[4]) << 24) |
-		    (((u_int64_t)resp->snscb_nodename[5]) << 16) |
-		    (((u_int64_t)resp->snscb_nodename[6]) <<  8) |
-		    (((u_int64_t)resp->snscb_nodename[7]));
-		if (portid == 0 || wwpn == 0) {
-			break;
-		}
-
-		switch (resp->snscb_port_type) {
+		switch (clp->port_type) {
 		case 1:
 			pt = "   N_Port";
 			break;
@@ -2793,40 +2763,66 @@ isp_async(struct ispsoftc *isp, ispasync_t cmd, void *arg)
 			pt = "   E_port";
 			break;
 		default:
-			pt = "?";
+			pt = " ";
 			break;
 		}
+
 		isp_prt(isp, ISP_LOGINFO,
-		    "%s @ 0x%x, Node 0x%08x%08x Port %08x%08x",
-		    pt, portid, ((u_int32_t) (wwnn >> 32)), ((u_int32_t) wwnn),
-		    ((u_int32_t) (wwpn >> 32)), ((u_int32_t) wwpn));
+		    "%s Fabric Device @ PortID 0x%x", pt, clp->portid);
+
 		/*
-		 * We're only interested in SCSI_FCP types (for now)
+		 * If we don't have an initiator role we bail.
+		 *
+		 * We just use ISPASYNC_FABRIC_DEV for announcement purposes.
 		 */
-		if ((resp->snscb_fc4_types[2] & 1) == 0) {
+
+		if ((isp->isp_role & ISP_ROLE_INITIATOR) == 0) {
 			break;
 		}
-		if (fcp->isp_topo != TOPO_F_PORT)
-			lrange = FC_SNS_ID+1;
+
+		/*
+		 * Is this entry for us? If so, we bail.
+		 */
+
+		if (fcp->isp_portid == clp->portid) {
+			break;
+		}
+
+		/*
+		 * Else, the default policy is to find room for it in
+		 * our local port database. Later, when we execute
+		 * the call to isp_pdb_sync either this newly arrived
+		 * or already logged in device will be (re)announced.
+		 */
+
+		if (fcp->isp_topo == TOPO_FL_PORT)
+			base = FC_SNS_ID+1;
 		else
-			lrange = 0;
+			base = 0;
+
+		if (fcp->isp_topo == TOPO_N_PORT)
+			lim = 1;
+		else
+			lim = MAX_FC_TARG;
+
 		/*
 		 * Is it already in our list?
 		 */
-		for (target = lrange; target < MAX_FC_TARG; target++) {
+		for (target = base; target < lim; target++) {
 			if (target >= FL_PORT_ID && target <= FC_SNS_ID) {
 				continue;
 			}
 			lp = &fcp->portdb[target];
-			if (lp->port_wwn == wwpn && lp->node_wwn == wwnn) {
+			if (lp->port_wwn == clp->port_wwn &&
+			    lp->node_wwn == clp->node_wwn) {
 				lp->fabric_dev = 1;
 				break;
 			}
 		}
-		if (target < MAX_FC_TARG) {
+		if (target < lim) {
 			break;
 		}
-		for (target = lrange; target < MAX_FC_TARG; target++) {
+		for (target = base; target < lim; target++) {
 			if (target >= FL_PORT_ID && target <= FC_SNS_ID) {
 				continue;
 			}
@@ -2835,14 +2831,16 @@ isp_async(struct ispsoftc *isp, ispasync_t cmd, void *arg)
 				break;
 			}
 		}
-		if (target == MAX_FC_TARG) {
+		if (target == lim) {
 			isp_prt(isp, ISP_LOGWARN,
-			    "no more space for fabric devices");
+			    "out of space for fabric devices");
 			break;
 		}
-		lp->node_wwn = wwnn;
-		lp->port_wwn = wwpn;
-		lp->portid = portid;
+		lp->port_type = clp->port_type;
+		lp->fc4_type = clp->fc4_type;
+		lp->node_wwn = clp->node_wwn;
+		lp->port_wwn = clp->port_wwn;
+		lp->portid = clp->portid;
 		lp->fabric_dev = 1;
 		break;
 	}
