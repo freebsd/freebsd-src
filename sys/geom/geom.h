4cdf6887f50d5ce03271653ea172be775842679a
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-04-09 15:12:05 +0000

    Constifixation of attribute argument to g_io_[gs]etattr()
    Sponsored by:	DARPA & NAI Labs
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@94283 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/geom/geom.h
+++ sys/geom/geom.h
@@ -210,9 +210,9 @@ struct bio * g_clone_bio(struct bio *);
 void g_destroy_bio(struct bio *);
 void g_io_deliver(struct bio *bp);
 void g_io_fail(struct bio *bp, int error);
-int g_io_getattr(char *attr, struct g_consumer *cp, int *len, void *ptr);
+int g_io_getattr(const char *attr, struct g_consumer *cp, int *len, void *ptr);
 void g_io_request(struct bio *bp, struct g_consumer *cp);
-int g_io_setattr(char *attr, struct g_consumer *cp, int len, void *ptr);
+int g_io_setattr(const char *attr, struct g_consumer *cp, int len, void *ptr);
 struct bio *g_new_bio(void);
 void * g_read_data(struct g_consumer *cp, off_t offset, off_t length, int *error);
 
