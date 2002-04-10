f386c10ddc9b534bf85b7adf67ad7ec0a630b91b
dfr <dfr@FreeBSD.org>
2002-04-10 19:24:08 +0000

    Add ucode values for SIGFPE etc. Copied from i386/include/signal.h.

--- sys/ia64/include/signal.h
+++ sys/ia64/include/signal.h
@@ -35,6 +35,18 @@ typedef long	sig_atomic_t;
 
 #ifndef _ANSI_SOURCE
 
+/* portable macros for SIGFPE/ARITHTRAP */
+#define FPE_INTOVF	1	/* integer overflow */
+#define FPE_INTDIV	2	/* integer divide by zero */
+#define FPE_FLTDIV	3	/* floating point divide by zero */
+#define FPE_FLTOVF	4	/* floating point overflow */
+#define FPE_FLTUND	5	/* floating point underflow */
+#define FPE_FLTRES	6	/* floating point inexact result */
+#define FPE_FLTINV	7	/* invalid floating point operation */
+#define FPE_FLTSUB	8	/* subscript out of range */
+
+#define BUS_SEGM_FAULT	30	/* segment protection base */
+
 /*
  * Minimum signal stack size. The current signal frame
  * for IA-64 is 2656 bytes large.
