578318b6bee516e4795c90286b6ec5f1073e7a5c
gshapiro <gshapiro@FreeBSD.org>
2002-04-10 03:06:22 +0000

    Fix conflicts from sendmail 8.12.3 import

--- contrib/sendmail/src/mci.c
+++ contrib/sendmail/src/mci.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998-2001 Sendmail, Inc. and its suppliers.
+ * Copyright (c) 1998-2002 Sendmail, Inc. and its suppliers.
  *	All rights reserved.
  * Copyright (c) 1995-1997 Eric P. Allman.  All rights reserved.
  * Copyright (c) 1988, 1993
@@ -15,7 +15,7 @@
 
 #include <sendmail.h>
 
-SM_RCSID("@(#)$Id: mci.c,v 8.202 2001/11/05 22:12:17 ca Exp $")
+SM_RCSID("@(#)$Id: mci.c,v 8.204 2002/02/22 18:24:57 ca Exp $")
 
 #if NETINET || NETINET6
 # include <arpa/inet.h>
@@ -159,7 +159,7 @@ mci_scan(savemci)
 			bestmci = &MciCache[i];
 			continue;
 		}
-		if ((mci->mci_lastuse + MciCacheTimeout < now ||
+		if ((mci->mci_lastuse + MciCacheTimeout <= now ||
 		     (mci->mci_mailer != NULL &&
 		      mci->mci_mailer->m_maxdeliveries > 0 &&
 		      mci->mci_deliveries + 1 >= mci->mci_mailer->m_maxdeliveries))&&
@@ -390,7 +390,7 @@ mci_get(host, m)
 		time_t now = curtime();
 
 		/* if this info is stale, ignore it */
-		if (now > mci->mci_lastuse + MciInfoTimeout)
+		if (mci->mci_lastuse + MciInfoTimeout <= now)
 		{
 			mci->mci_lastuse = now;
 			mci->mci_errno = 0;
@@ -512,7 +512,6 @@ static struct mcifbits	MciFlags[] =
 	{ MCIF_SIZE,		"SIZE"		},
 	{ MCIF_8BITMIME,	"8BITMIME"	},
 	{ MCIF_7BIT,		"7BIT"		},
-	{ MCIF_MULTSTAT,	"MULTSTAT"	},
 	{ MCIF_INHEADER,	"INHEADER"	},
 	{ MCIF_CVT8TO7,		"CVT8TO7"	},
 	{ MCIF_DSN,		"DSN"		},
@@ -1313,7 +1312,7 @@ mci_purge_persistent(pathname, hostname)
 				pathname, sm_errstring(errno));
 		return ret;
 	}
-	if (curtime() - statbuf.st_mtime < MciInfoTimeout)
+	if (curtime() - statbuf.st_mtime <= MciInfoTimeout)
 		return 1;
 	if (hostname != NULL)
 	{
