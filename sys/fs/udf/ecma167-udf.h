c2d6947d145d27177ec124f74b1e58fb237c6e0b
Jeroen Ruigrok van der Werven <asmodai@FreeBSD.org>
2002-04-15 19:49:15 +0000

    Sync with UDF p4 tree:	Use POSIX integer types instead of BSD types.

--- sys/fs/udf/ecma167-udf.h
+++ sys/fs/udf/ecma167-udf.h
@@ -47,49 +47,49 @@ enum {
 
 /* Descriptor tag [3/7.2] */
 struct desc_tag {
-	u_int16_t	id;
-	u_int16_t	descriptor_ver;
-	u_int8_t	cksum;
-	u_int8_t	reserved;
-	u_int16_t	serial_num;
-	u_int16_t	desc_crc;
-	u_int16_t	desc_crc_len;
-	u_int32_t	tag_loc;
+	uint16_t	id;
+	uint16_t	descriptor_ver;
+	uint8_t		cksum;
+	uint8_t		reserved;
+	uint16_t	serial_num;
+	uint16_t	desc_crc;
+	uint16_t	desc_crc_len;
+	uint32_t	tag_loc;
 } __attribute__ ((packed));
 
 /* Recorded Address [4/7.1] */
 struct lb_addr {
-	u_int32_t	lb_num;
-	u_int16_t	part_num;
+	uint32_t	lb_num;
+	uint16_t	part_num;
 } __attribute__ ((packed));
 
 /* Extent Descriptor [3/7.1] */
 struct extent_ad {
-	u_int32_t	len;
-	u_int32_t	loc;
+	uint32_t	len;
+	uint32_t	loc;
 } __attribute__ ((packed));
 
 /* Short Allocation Descriptor [4/14.14.1] */
 struct short_ad {
-	u_int32_t	len;
-	u_int32_t	pos;
+	uint32_t	len;
+	uint32_t	pos;
 } __attribute__ ((packed));
 
 /* Long Allocation Descriptor [4/14.14.2] */
 struct long_ad {
-	u_int32_t	len;
+	uint32_t	len;
 	struct lb_addr	loc;
-	u_int16_t	ad_flags;
-	u_int32_t	ad_id;
+	uint16_t	ad_flags;
+	uint32_t	ad_id;
 } __attribute__ ((packed));
 
 /* Extended Allocation Descriptor [4/14.14.3] */
 struct ext_ad {
-	u_int32_t	ex_len;
-	u_int32_t	rec_len;
-	u_int32_t	inf_len;
+	uint32_t	ex_len;
+	uint32_t	rec_len;
+	uint32_t	inf_len;
 	struct lb_addr	ex_loc;
-	u_int8_t	reserved[2];
+	uint8_t		reserved[2];
 } __attribute__ ((packed));
 
 union icb {
@@ -100,42 +100,42 @@ union icb {
 
 /* Character set spec [1/7.2.1] */
 struct charspec {
-	u_int8_t	type;
-	u_int8_t	inf[63];
+	uint8_t		type;
+	uint8_t		inf[63];
 } __attribute__ ((packed));
 
 /* Timestamp [1/7.3] */
 struct timestamp {
-	u_int16_t	type_tz;
-	u_int16_t	year;
-	u_int8_t	month;
-	u_int8_t	day;
-	u_int8_t	hour;
-	u_int8_t	minute;
-	u_int8_t	second;
-	u_int8_t	centisec;
-	u_int8_t	hund_usec;
-	u_int8_t	usec;
+	uint16_t	type_tz;
+	uint16_t	year;
+	uint8_t		month;
+	uint8_t		day;
+	uint8_t		hour;
+	uint8_t		minute;
+	uint8_t		second;
+	uint8_t		centisec;
+	uint8_t		hund_usec;
+	uint8_t		usec;
 } __attribute__ ((packed));
 
 /* Entity Identifier [1/7.4] */
 #define	UDF_REGID_ID_SIZE	23
 struct regid {
-	u_int8_t	flags;
-	u_int8_t	id[UDF_REGID_ID_SIZE];
-	u_int8_t	id_suffix[8];
+	uint8_t		flags;
+	uint8_t		id[UDF_REGID_ID_SIZE];
+	uint8_t		id_suffix[8];
 } __attribute__ ((packed));
 
 /* ICB Tag [4/14.6] */
 struct icb_tag {
-	u_int32_t	prev_num_dirs;
-	u_int16_t	strat_type;
-	u_int8_t	strat_param[2];
-	u_int16_t	max_num_entries;
-	u_int8_t	reserved;
-	u_int8_t	file_type;
+	uint32_t	prev_num_dirs;
+	uint16_t	strat_type;
+	uint8_t		strat_param[2];
+	uint16_t	max_num_entries;
+	uint8_t		reserved;
+	uint8_t		file_type;
 	struct lb_addr	parent_icb;
-	u_int16_t	flags;
+	uint16_t	flags;
 } __attribute__ ((packed));
 #define	UDF_ICB_TAG_FLAGS_SETUID	0x40
 #define	UDF_ICB_TAG_FLAGS_SETGID	0x80
@@ -151,22 +151,22 @@ struct anchor_vdp {
 /* Volume Descriptor Pointer [3/10.3] */
 struct vol_desc_ptr {
 	struct desc_tag		tag;
-	u_int32_t		vds_number;
+	uint32_t		vds_number;
 	struct extent_ad	next_vds_ex;
 } __attribute__ ((packed));
 
 /* Primary Volume Descriptor [3/10.1] */
 struct pri_vol_desc {
 	struct desc_tag		tag;
-	u_int32_t		seq_num;
-	u_int32_t		pdv_num;
+	uint32_t		seq_num;
+	uint32_t		pdv_num;
 	char			vol_id[32];
-	u_int16_t		vds_num;
-	u_int16_t		max_vol_seq;
-	u_int16_t		ichg_lvl;
-	u_int16_t		max_ichg_lvl;
-	u_int32_t		charset_list;
-	u_int32_t		max_charset_list;
+	uint16_t		vds_num;
+	uint16_t		max_vol_seq;
+	uint16_t		ichg_lvl;
+	uint16_t		max_ichg_lvl;
+	uint32_t		charset_list;
+	uint32_t		max_charset_list;
 	char			volset_id[128];
 	struct charspec		desc_charset;
 	struct charspec		explanatory_charset;
@@ -175,77 +175,77 @@ struct pri_vol_desc {
 	struct regid		app_id;
 	struct timestamp	time;
 	struct regid		imp_id;
-	u_int8_t		imp_use[64];
-	u_int32_t		prev_vds_lov;
-	u_int16_t		flags;
-	u_int8_t		reserved[22];
+	uint8_t			imp_use[64];
+	uint32_t		prev_vds_lov;
+	uint16_t		flags;
+	uint8_t			reserved[22];
 } __attribute__ ((packed));
 
 /* Logical Volume Descriptor [3/10.6] */
 struct logvol_desc {
 	struct desc_tag		tag;
-	u_int32_t		seq_num;
+	uint32_t		seq_num;
 	struct charspec		desc_charset;
 	char			logvol_id[128];
-	u_int32_t		lb_size;
+	uint32_t		lb_size;
 	struct regid		domain_id;
 	union {
 		struct long_ad	fsd_loc;
-		u_int8_t	logvol_content_use[16];
+		uint8_t		logvol_content_use[16];
 	} _lvd_use;
-	u_int32_t		mt_l; /* Partition map length */
-	u_int32_t		n_pm; /* Number of partition maps */
+	uint32_t		mt_l; /* Partition map length */
+	uint32_t		n_pm; /* Number of partition maps */
 	struct regid		imp_id;
-	u_int8_t		imp_use[128];
+	uint8_t			imp_use[128];
 	struct extent_ad	integrity_seq_id;
-	u_int8_t		maps[1];
+	uint8_t			maps[1];
 } __attribute__ ((packed));
 
 #define	UDF_PMAP_SIZE	64
 
 /* Type 1 Partition Map [3/10.7.2] */
 struct part_map_1 {
-	u_int8_t		type;
-	u_int8_t		len;
-	u_int16_t		vol_seq_num;
-	u_int16_t		part_num;
+	uint8_t			type;
+	uint8_t			len;
+	uint16_t		vol_seq_num;
+	uint16_t		part_num;
 } __attribute__ ((packed));
 
 /* Type 2 Partition Map [3/10.7.3] */
 struct part_map_2 {
-	u_int8_t		type;
-	u_int8_t		len;
-	u_int8_t		part_id[62];
+	uint8_t			type;
+	uint8_t			len;
+	uint8_t			part_id[62];
 } __attribute__ ((packed));
 
 /* Virtual Partition Map [UDF 2.01/2.2.8] */
 struct part_map_virt {
-	u_int8_t		type;
-	u_int8_t		len;
-	u_int8_t		reserved[2];
+	uint8_t			type;
+	uint8_t			len;
+	uint8_t			reserved[2];
 	struct regid		id;
-	u_int16_t		vol_seq_num;
-	u_int16_t		part_num;
-	u_int8_t		reserved1[24];
+	uint16_t		vol_seq_num;
+	uint16_t		part_num;
+	uint8_t			reserved1[24];
 } __attribute__ ((packed));
 
 /* Sparable Partition Map [UDF 2.01/2.2.9] */
 struct part_map_spare {
-	u_int8_t		type;
-	u_int8_t		len;
-	u_int8_t		reserved[2];
+	uint8_t			type;
+	uint8_t			len;
+	uint8_t			reserved[2];
 	struct regid		id;
-	u_int16_t		vol_seq_num;
-	u_int16_t		part_num;
-	u_int16_t		packet_len;
-	u_int8_t		n_st;	/* Number of Sparing Tables */
-	u_int8_t		reserved1;
-	u_int32_t		st_size;
-	u_int32_t		st_loc[1];
+	uint16_t		vol_seq_num;
+	uint16_t		part_num;
+	uint16_t		packet_len;
+	uint8_t			n_st;	/* Number of Sparing Tables */
+	uint8_t			reserved1;
+	uint32_t		st_size;
+	uint32_t		st_loc[1];
 } __attribute__ ((packed));
 
 union udf_pmap {
-	u_int8_t		data[UDF_PMAP_SIZE];
+	uint8_t			data[UDF_PMAP_SIZE];
 	struct part_map_1	pm1;
 	struct part_map_2	pm2;
 	struct part_map_virt	pmv;
@@ -254,46 +254,46 @@ union udf_pmap {
 
 /* Sparing Map Entry [UDF 2.01/2.2.11] */
 struct spare_map_entry {
-	u_int32_t		org;
-	u_int32_t		map;
+	uint32_t		org;
+	uint32_t		map;
 } __attribute__ ((packed));
 
 /* Sparing Table [UDF 2.01/2.2.11] */
 struct udf_sparing_table {
 	struct desc_tag		tag;
 	struct regid		id;
-	u_int16_t		rt_l;	/* Relocation Table len */
-	u_int8_t		reserved[2];
-	u_int32_t		seq_num;
+	uint16_t		rt_l;	/* Relocation Table len */
+	uint8_t			reserved[2];
+	uint32_t		seq_num;
 	struct spare_map_entry	entries[1];
 } __attribute__ ((packed));
 
 /* Partition Descriptor [3/10.5] */
 struct part_desc {
 	struct desc_tag	tag;
-	u_int32_t	seq_num;
-	u_int16_t	flags;
-	u_int16_t	part_num;
+	uint32_t	seq_num;
+	uint16_t	flags;
+	uint16_t	part_num;
 	struct regid	contents;
-	u_int8_t	contents_use[128];
-	u_int32_t	access_type;
-	u_int32_t	start_loc;
-	u_int32_t	part_len;
+	uint8_t		contents_use[128];
+	uint32_t	access_type;
+	uint32_t	start_loc;
+	uint32_t	part_len;
 	struct regid	imp_id;
-	u_int8_t	imp_use[128];
-	u_int8_t	reserved[156];
+	uint8_t		imp_use[128];
+	uint8_t		reserved[156];
 } __attribute__ ((packed));
 
 /* File Set Descriptor [4/14.1] */
 struct fileset_desc {
 	struct desc_tag		tag;
 	struct timestamp	time;
-	u_int16_t		ichg_lvl;
-	u_int16_t		max_ichg_lvl;
-	u_int32_t		charset_list;
-	u_int32_t		max_charset_list;
-	u_int32_t		fileset_num;
-	u_int32_t		fileset_desc_num;
+	uint16_t		ichg_lvl;
+	uint16_t		max_ichg_lvl;
+	uint32_t		charset_list;
+	uint32_t		max_charset_list;
+	uint32_t		fileset_num;
+	uint32_t		fileset_desc_num;
 	struct charspec		logvol_id_charset;
 	char			logvol_id[128];
 	struct charspec		fileset_charset;
@@ -304,18 +304,18 @@ struct fileset_desc {
 	struct regid		domain_id;
 	struct long_ad		next_ex;
 	struct long_ad		streamdir_icb;
-	u_int8_t		reserved[32];
+	uint8_t			reserved[32];
 } __attribute__ ((packed));
 
 /* File Identifier Descriptor [4/14.4] */
 struct fileid_desc {
 	struct desc_tag	tag;
-	u_int16_t	file_num;
-	u_int8_t	file_char;
-	u_int8_t	l_fi;	/* Length of file identifier area */
+	uint16_t	file_num;
+	uint8_t		file_char;
+	uint8_t		l_fi;	/* Length of file identifier area */
 	struct long_ad	icb;
-	u_int16_t	l_iu;	/* Length of implementaion use area */
-	u_int8_t	data[1];
+	uint16_t	l_iu;	/* Length of implementaion use area */
+	uint8_t		data[1];
 } __attribute__ ((packed));
 #define	UDF_FID_SIZE	38
 
@@ -323,25 +323,25 @@ struct fileid_desc {
 struct file_entry {
 	struct desc_tag		tag;
 	struct icb_tag		icbtag;
-	u_int32_t		uid;
-	u_int32_t		gid;
-	u_int32_t		perm;
-	u_int16_t		link_cnt;
-	u_int8_t		rec_format;
-	u_int8_t		rec_disp_attr;
-	u_int32_t		rec_len;
-	u_int64_t		inf_len;
-	u_int64_t		logblks_rec;
+	uint32_t		uid;
+	uint32_t		gid;
+	uint32_t		perm;
+	uint16_t		link_cnt;
+	uint8_t			rec_format;
+	uint8_t			rec_disp_attr;
+	uint32_t		rec_len;
+	uint64_t		inf_len;
+	uint64_t		logblks_rec;
 	struct timestamp	atime;
 	struct timestamp	mtime;
 	struct timestamp	attrtime;
-	u_int32_t		ckpoint;
+	uint32_t		ckpoint;
 	struct long_ad		ex_attr_icb;
 	struct regid		imp_id;
-	u_int64_t		unique_id;
-	u_int32_t		l_ea;	/* Length of extended attribute area */
-	u_int32_t		l_ad;	/* Length of allocation descriptors */
-	u_int8_t		data[1];
+	uint64_t		unique_id;
+	uint32_t		l_ea;	/* Length of extended attribute area */
+	uint32_t		l_ad;	/* Length of allocation descriptors */
+	uint8_t			data[1];
 } __attribute ((packed));
 #define	UDF_FENTRY_SIZE	176
 #define	UDF_FENTRY_PERM_USER_MASK	0x07
