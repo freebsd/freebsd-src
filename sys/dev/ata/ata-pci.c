e3b54e1219b60117cb56934a5b9e10a96ac60693
sos <sos@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-04-16 08:30:51 +0000

    Add support for the nVIDIA nForce ATA controller.
    Collapse the VIA/AMD/nVIDIA support code into one, they are
    created more or less equal anyway..
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@94826 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ata/ata-pci.c
+++ sys/dev/ata/ata-pci.c
@@ -228,6 +228,9 @@ ata_pci_match(device_t dev)
     case 0x74411022:
 	return "AMD 768 ATA100 controller";
 
+    case 0x01bc10de:
+	return "nVIDIA nForce ATA100 controller";
+
     case 0x02111166:
 	return "ServerWorks ROSB4 ATA33 controller";
 
@@ -465,6 +468,7 @@ ata_pci_attach(device_t dev)
     case 0x74091022: /* AMD 756 default setup */
     case 0x74111022: /* AMD 766 default setup */
     case 0x74411022: /* AMD 768 default setup */
+    case 0x01bc10de: /* nVIDIA nForce default setup */
 	/* set prefetch, postwrite */
 	pci_write_config(dev, 0x41, pci_read_config(dev, 0x41, 1) | 0xf0, 1);
 
