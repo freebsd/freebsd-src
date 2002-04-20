031ac3a6c1a991a253a0dbaa018c1c9a6fd7b56f
gshapiro <gshapiro@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-04-20 20:34:16 +0000

    Resolve conflicts from import of post-8.12.3 bug fixes.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@95157 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/sendmail/makemap/makemap.c
+++ contrib/sendmail/makemap/makemap.c
@@ -20,7 +20,7 @@ SM_IDSTR(copyright,
      Copyright (c) 1992, 1993\n\
 	The Regents of the University of California.  All rights reserved.\n")
 
-SM_IDSTR(id, "@(#)$Id: makemap.c,v 8.175 2001/12/28 22:44:01 ca Exp $")
+SM_IDSTR(id, "@(#)$Id: makemap.c,v 1.1.1.6 2002/02/17 21:56:43 gshapiro Exp $")
 
 /* $FreeBSD$ */
 
