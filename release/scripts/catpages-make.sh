a47c47f73007bfd65d0981c1c019e43676e0e705
ru <ru@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-04-24 12:54:34 +0000

    Build the catpages distribution in a more natural way.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@95363 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/scripts/catpages-make.sh
+++ release/scripts/catpages-make.sh
@@ -3,11 +3,10 @@
 # $FreeBSD$
 #
 
-# Create the catpages dist - must follow manpages dist script, for obvious
-# reasons.
-if [ -d ${RD}/trees/manpages/usr/share/man ]; then
-	su -m man -c 'catman ${RD}/trees/manpages/usr/share/man' > /dev/null 2>&1;
-	( cd ${RD}/trees/manpages/usr/share/man;
-	find cat* whatis | cpio -dumpl ${RD}/trees/catpages/usr/share/man ) &&
-	rm -rf ${RD}/trees/manpages/usr/share/man/cat*;
+# Move all the catpages out to their own dist, using the base dist as a
+# starting point.  This must precede the manpages dist script.
+if [ -d ${RD}/trees/base/usr/share/man ]; then
+	( cd ${RD}/trees/base/usr/share/man;
+	find cat* whatis | cpio -dumpl ${RD}/trees/catpages/usr/share/man > /dev/null 2>&1) &&
+	rm -rf ${RD}/trees/base/usr/share/man/cat*;
 fi
