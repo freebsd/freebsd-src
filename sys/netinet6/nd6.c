83649f07c7d3395b4f14c2b7edbdc6be67d39371
Hajimu UMEMOTO <ume@FreeBSD.org>
2002-04-24 19:09:48 +0000

    Correct timer management (deprecated) in nd6_timer.
    Obtained from:	KAME
    MFC after:	3 days

--- sys/netinet6/nd6.c
+++ sys/netinet6/nd6.c
@@ -578,7 +578,8 @@ nd6_timer(ignored_arg)
 
 			if (regen)
 				goto addrloop; /* XXX: see below */
-		} else if (IFA6_IS_DEPRECATED(ia6)) {
+		}
+		if (IFA6_IS_DEPRECATED(ia6)) {
 			int oldflags = ia6->ia6_flags;
 
 			ia6->ia6_flags |= IN6_IFF_DEPRECATED;
@@ -607,7 +608,7 @@ nd6_timer(ignored_arg)
 					goto addrloop;
 				}
 			}
-		} else if (IFA6_IS_DEPRECATED(ia6)) {
+		} else {
 			/*
 			 * A new RA might have made a deprecated address
 			 * preferred.
