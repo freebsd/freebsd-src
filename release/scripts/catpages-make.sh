8ad1c2807b6a01430c9993819b33676991a90da3
ru <ru@FreeBSD.org>
2002-04-26 17:55:27 +0000

    Milestone #1 in cross-arch make releases.
    Do not install games and profiled libraries to the ${CHROOTDIR}
    with the initial installworld.
    
    Eliminate the need in the second installworld.  For that, make sure
    _everything_ is built in the "world" environment, using the right
    tool chain.
    
    Added SUBDIR_OVERRIDE helper stuff to Makefile.inc1.  Split the
    buildworld process into stages, and skip some stages when
    SUBDIR_OVERRIDE is set (used to build crypto, krb4, and krb5
    dists).
    
    Added NO_MAKEDB_RUN knob to Makefile.inc1 to avoid running
    makewhatis(1) at the end of installworld (used when making crypto,
    krb4, and krb5 dists).
    
    In release/scripts/doFS.sh, ensure that the correct boot blocks are
    used.
    
    Moved the creation of the "crypto" dist from release.5 to
    release.2.
    
    In release.3 and doMFSKERN, build kernels in the "world"
    environment.  KERNELS now means "additional" kernels, GENERIC is
    always built.
    
    Ensure we build crunched binaries in the "world" environment.
    Obfuscate release/Makefile some more (WMAKEENV) to achieve this.
    
    Inline createBOOTMFS target.
    
    Use already built GENERIC kernel modules to augment mfsfd's
    /stand/modules.  GC doMODULES as such.
    
    Assorted fixes:
    
    Get rid of the "afterdistribute" target by moving the single use
    of it from sys/Makefile to etc/Makefile's "distribute".
    
    Makefile.inc1: apparently "etc" no longer needs to be last for
    "distribute" to succeed.
    
    gnu/usr.bin/perl/library/Makefile.inc: do not override the
    "install" and "distribute" targets, do it the "canonical" way.
    
    release/scripts/{man,cat}pages-make.sh: make sure Perl manpages and
    catpages appear in the right dists.  Note that because Perl does
    not respect the MANBUILDCAT (and NOMAN), this results in a loss of
    /usr/share/perl/man/cat* empty directories.  This will be fixed
    soon.
    
    Turn MAKE_KERBEROS4 into a plain boolean variable (if it is set it
    means "make KerberosIV"), as documented in the make.conf(5)
    manpage.  Most of the userland makefiles did not test it for "YES"
    anyway.
    
    XXX Should specialized kerberized libpam versions be included into
    the krb4 and krb5 dists?  (libpam.a would be incorrect anyway if
    both krb4 and krb5 dists were choosen.)
    
    Make sure "games" dist is made before "catpages", otherwise games
    catpages settle in the wrong dist.
    
    Fast build machine provided by: Igor Kucherenko <kivvy@sunbay.com>

--- release/scripts/catpages-make.sh
+++ release/scripts/catpages-make.sh
@@ -10,3 +10,8 @@ if [ -d ${RD}/trees/base/usr/share/man ]; then
 	find cat* whatis | cpio -dumpl ${RD}/trees/catpages/usr/share/man > /dev/null 2>&1) &&
 	rm -rf ${RD}/trees/base/usr/share/man/cat*;
 fi
+if [ -d ${RD}/trees/base/usr/share/perl/man ]; then
+	( cd ${RD}/trees/base/usr/share/perl/man;
+	find cat* whatis | cpio -dumpl ${RD}/trees/catpages/usr/share/perl/man > /dev/null 2>&1) &&
+	rm -rf ${RD}/trees/base/usr/share/perl/man/cat*;
+fi
