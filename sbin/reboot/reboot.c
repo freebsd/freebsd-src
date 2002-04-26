a90c7ec41c8df624dddb39cd7998d00703e16f2c
wes <wes@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-04-26 22:32:15 +0000

    Rename the file used to specify the nextboot to make it clear that this is a loader configuration file and can be used for more than just a kernel name.
    Submitted by:	Gordon Tetlow <gordont@gnf.org>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@95531 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/reboot/reboot.c
+++ sbin/reboot/reboot.c
@@ -123,7 +123,7 @@ main(int argc, char *argv[])
 	}
 
 	if (kflag) {
-		fd = open("/boot/nextkernel", O_WRONLY | O_CREAT, 0444);
+		fd = open("/boot/nextboot.conf", O_WRONLY | O_CREAT, 0444);
 		if (fd > -1) {
 			(void)write(fd, "kernel=\"", 8L);
 			(void)write(fd, kernel, strlen(kernel));
