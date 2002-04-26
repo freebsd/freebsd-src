dbb17987fc44b1f38e4a04d5fd819f36285eaf72
Robert Watson <rwatson@FreeBSD.org>
2002-04-26 03:39:04 +0000

    1.43         (dfr      04-Apr-97): /* 1.43         (dfr      04-Apr-97):  * [dfr] Kludge until I get around to fixing all the vfs locking. 1.43         (dfr      04-Apr-97):  */
    	The new devfs doesn't support VFS locking.  So don't do locking
    	assertions for devfs vnodes.
    
    	With this change, a kernel with options DEBUG_VFS_LOCKS actually
    	gets to single-user mode.
    
    Obtained from:	TrustedBSD Project
    Sponsored by:	DARPA, NAI Labs

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -453,8 +453,7 @@ struct vop_generic_args {
 				 || (vp)->v_tag == VT_NFS		\
 				 || (vp)->v_tag == VT_LFS		\
 				 || (vp)->v_tag == VT_ISOFS		\
-				 || (vp)->v_tag == VT_MSDOSFS		\
-				 || (vp)->v_tag == VT_DEVFS)
+				 || (vp)->v_tag == VT_MSDOSFS)
 
 #define ASSERT_VOP_LOCKED(vp, str)					\
 do {									\
