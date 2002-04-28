07dd938303252be693d1484bf806b0c652a3313b
Poul-Henning Kamp <phk@FreeBSD.org>
2002-04-28 19:25:07 +0000

    Don't pass three args when one will do just fine, and even prevent mistakes like the one brgphy.c (now corrected).

--- sys/dev/mii/tdkphy.c
+++ sys/dev/mii/tdkphy.c
@@ -148,7 +148,7 @@ tdkphy_attach(device_t dev)
 	sc->mii_capabilities =
 	    PHY_READ(sc, MII_BMSR) & ma->mii_capmask;
 	device_printf(dev, " ");
-	mii_add_media(mii, sc->mii_capabilities, sc->mii_inst);
+	mii_add_media(sc);
 	printf("\n");
 #undef ADD
 
