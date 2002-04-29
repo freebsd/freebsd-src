ffe589be53a6d90602d3b16ad2f9fc31102fac0c
mike <mike@FreeBSD.org>
2002-04-29 16:58:54 +0000

    Make this header self-reliant with regard to the types it uses.

--- sys/sys/endian.h
+++ sys/sys/endian.h
@@ -30,8 +30,24 @@
 #define _SYS_ENDIAN_H_
 
 #include <sys/cdefs.h>
+#include <sys/_types.h>
 #include <machine/endian.h>
 
+#ifndef _UINT16_T_DECLARED
+typedef	__uint16_t	uint16_t;
+#define	_UINT16_T_DECLARED
+#endif
+ 
+#ifndef _UINT32_T_DECLARED
+typedef	__uint32_t	uint32_t;
+#define	_UINT32_T_DECLARED
+#endif
+ 
+#ifndef _UINT64_T_DECLARED
+typedef	__uint64_t	uint64_t;
+#define	_UINT64_T_DECLARED
+#endif
+ 
 /*
  * General byte order swapping functions.
  */
@@ -47,27 +63,27 @@
 #define	htobe16(x)	bswap16((x))
 #define	htobe32(x)	bswap32((x))
 #define	htobe64(x)	bswap64((x))
-#define	htole16(x)	((__uint16_t)(x))
-#define	htole32(x)	((__uint32_t)(x))
-#define	htole64(x)	((__uint64_t)(x))
+#define	htole16(x)	((uint16_t)(x))
+#define	htole32(x)	((uint32_t)(x))
+#define	htole64(x)	((uint64_t)(x))
 
 #define	be16toh(x)	bswap16((x))
 #define	be32toh(x)	bswap32((x))
 #define	be64toh(x)	bswap64((x))
-#define	le16toh(x)	((__uint16_t)(x))
-#define	le32toh(x)	((__uint32_t)(x))
-#define	le64toh(x)	((__uint64_t)(x))
+#define	le16toh(x)	((uint16_t)(x))
+#define	le32toh(x)	((uint32_t)(x))
+#define	le64toh(x)	((uint64_t)(x))
 #else /* _BYTE_ORDER != _LITTLE_ENDIAN */
-#define	htobe16(x)	((__uint16_t)(x))
-#define	htobe32(x)	((__uint32_t)(x))
-#define	htobe64(x)	((__uint64_t)(x))
+#define	htobe16(x)	((uint16_t)(x))
+#define	htobe32(x)	((uint32_t)(x))
+#define	htobe64(x)	((uint64_t)(x))
 #define	htole16(x)	bswap16((x))
 #define	htole32(x)	bswap32((x))
 #define	htole64(x)	bswap64((x))
 
-#define	be16toh(x)	((__uint16_t)(x))
-#define	be32toh(x)	((__uint32_t)(x))
-#define	be64toh(x)	((__uint64_t)(x))
+#define	be16toh(x)	((uint16_t)(x))
+#define	be32toh(x)	((uint32_t)(x))
+#define	be64toh(x)	((uint64_t)(x))
 #define	le16toh(x)	bswap16((x))
 #define	le32toh(x)	bswap32((x))
 #define	le64toh(x)	bswap64((x))
