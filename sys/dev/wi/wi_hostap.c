94080c276983174ac22bb12cc101817f6bcbdc53
Warner Losh <imp@FreeBSD.org>
2002-04-29 21:22:59 +0000

    MFC three important fixes (a little quick, but one is a compile issue and the others are in a new feature): 1) don't include pccard stuff in pci attachment (kernel won't compile w/o this    if you don't have pccard in your kernel). 2) Spell NetBSD's splsoftnet() as splimp() rather than splnet(). 3) Properly untimeout stuff before creating a new timeout.

--- sys/dev/wi/wi_hostap.c
+++ sys/dev/wi/wi_hostap.c
@@ -345,7 +345,7 @@ wihap_sta_timeout(void *v)
 	struct wihap_info	*whi = &sc->wi_hostap_info;
 	int	s;
 
-	s = splnet();
+	s = splimp();
 	if (sta->flags & WI_SIFLAGS_ASSOC) {
 		if (sc->arpcom.ac_if.if_flags & IFF_DEBUG)
 			device_printf(sc->dev, "inactivity disassoc: %6D\n",
@@ -431,7 +431,6 @@ wihap_sta_alloc(struct wi_softc *sc, u_int8_t *addr)
 	sta->sc = sc;
 	whi->n_stations++;
 	bcopy(addr, &sta->addr, ETHER_ADDR_LEN);
-	sta->inactivity_timer = whi->inactivity_time;
 
 	return(sta);
 }
@@ -555,7 +554,6 @@ wihap_auth_req(struct wi_softc *sc, struct wi_frame *rxfrm,
 			goto fail;
 		}
 	}
-	sta->inactivity_timer = whi->inactivity_time;
 
 	/* Note: it's okay to leave the station info structure around
 	 * if the authen fails.  It'll be timed out eventually.
@@ -989,7 +987,7 @@ wihap_sta_is_assoc(struct wihap_info *whi, u_int8_t addr[])
 	sta = wihap_sta_find(whi, addr);
 	if (sta != NULL && (sta->flags & WI_SIFLAGS_ASSOC)) {
 		/* Keep it active. */
-		sta->inactivity_timer = whi->inactivity_time;
+		untimeout(wihap_sta_timeout, sta, sta->tmo);
 		sta->tmo = timeout(wihap_sta_timeout, sta,
 		    hz * whi->inactivity_time);
 		return(1);
@@ -1018,6 +1016,7 @@ wihap_check_tx(struct wihap_info *whi, u_int8_t addr[], u_int8_t *txrate)
 	sta = wihap_sta_find(whi, addr);
 	if (sta != NULL && (sta->flags & WI_SIFLAGS_ASSOC)) {
 		/* Keep it active. */
+		untimeout(wihap_sta_timeout, sta, sta->tmo);
 		sta->tmo = timeout(wihap_sta_timeout, sta,
 		    hz * whi->inactivity_time);
 		*txrate = txratetable[ sta->tx_curr_rate ];
@@ -1080,6 +1079,7 @@ wihap_data_input(struct wi_softc *sc, struct wi_frame *rxfrm, struct mbuf *m)
 		return(1);
 	}
 
+	untimeout(wihap_sta_timeout, sta, sta->tmo);
 	sta->tmo = timeout(wihap_sta_timeout, sta,
 	    hz * whi->inactivity_time);
 	sta->sig_info = le16toh(rxfrm->wi_q_info);
