256127b2d1cb33c7d359ea0720e5766704788b90
mdodd <mdodd@FreeBSD.org>
2002-04-30 23:55:16 +0000

    Document the location (in the source tree) of the "Porter's Handbook".

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -50,6 +50,9 @@
  * __FreeBSD_version numbers are documented in the Porter's Handbook.
  * If you bump the version for any reason, you should update the documentation
  * there.
+ * Currently this lives here:
+ *
+ *	doc/en_US.ISO8859-1/books/porters-handbook/book.sgml
  */
 #undef __FreeBSD_version
 #define __FreeBSD_version 500034	/* Master, propagated to newvers */
