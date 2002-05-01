a24531af199d67a5a9ee82109ec0f7d845aa12bc
Mike Silbersack <silby@FreeBSD.org>
2002-05-01 03:27:35 +0000

    MFC:
      Make sure that sockets undergoing accept filtering are aborted in a
      LRU fashion when the listen queue fills up.  Previously, there was
      no mechanism to kick out old sockets, leading to an easy DoS of
      daemons using accept filtering.
    
      Revision  Changes    Path
      1.116     +1 -2      src/sys/kern/uipc_socket.c
      1.87      +7 -1      src/sys/kern/uipc_socket2.c

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -256,7 +256,6 @@ sofree(so)
 		} else {
 			panic("sofree: not queued");
 		}
-		head->so_qlen--;
 		so->so_state &= ~SS_INCOMP;
 		so->so_head = NULL;
 	}
@@ -1641,6 +1640,6 @@ filt_solisten(struct knote *kn, long hint)
 {
 	struct socket *so = (struct socket *)kn->kn_fp->f_data;
 
-	kn->kn_data = so->so_qlen - so->so_incqlen;
+	kn->kn_data = so->so_qlen;
 	return (! TAILQ_EMPTY(&so->so_comp));
 }
