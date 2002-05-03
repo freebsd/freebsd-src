1aacca68bf8649bee674e8cf98cabce3d9eb3c1d
wollman <wollman@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-05-03 17:28:28 +0000

    Fix fd leak.  Threads people: does the call above to `access' need to be underscored as well?
    PR:		37717
    Submitted by:	fred@clift.org (slightly modified by me)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@95989 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/stdtime/localtime.c
+++ lib/libc/stdtime/localtime.c
@@ -316,8 +316,10 @@ struct state * const	sp;
 		     	return -1;
 		if ((fid = _open(name, OPEN_MODE)) == -1)
 			return -1;
-		if ((_fstat(fid, &stab) < 0) || !S_ISREG(stab.st_mode))
+		if ((_fstat(fid, &stab) < 0) || !S_ISREG(stab.st_mode)) {
+			_close(fid);
 			return -1;
+		}
 	}
 	{
 		struct tzhead *	tzhp;
