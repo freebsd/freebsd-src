bb3e2cbf1da439546493182aae771ba4da7312c7
alfred <alfred@FreeBSD.org>
2002-05-11 03:48:49 +0000

    fix build: you may not use string concatination with __FUNCTION__, replace all occurances of: __FUNCTION__ ": error string" with: "%s: error string"

--- usr.sbin/pkg_install/info/show.c
+++ usr.sbin/pkg_install/info/show.c
@@ -179,7 +179,8 @@ show_plist(const char *title, Package *plist, plist_t type, Boolean showall)
 
 	default:
 	    cleanup(0);
-	    errx(2, __FUNCTION__ ": unknown command type %d (%s)", p->type, p->name);
+	    errx(2, "%s: unknown command type %d (%s)",
+		__FUNCTION__, p->type, p->name);
 	    break;
 	}
 	p = p->next;
