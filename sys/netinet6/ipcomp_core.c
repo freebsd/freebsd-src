231bd65333821b5737cb98ba31017e3ad59cfee1
ume <ume@FreeBSD.org>
2002-05-12 14:12:30 +0000

    Recent zlib does not like Z_FLUSH at the end of inflate().
    Reported by:	quak@mydiax.ch
    Obtained from:	KAME
    MFC after:	2 days
    		and approved by re

--- sys/netinet6/ipcomp_core.c
+++ sys/netinet6/ipcomp_core.c
@@ -252,14 +252,17 @@ do { \
 			MOREBLOCK();
 		}
 
-		zerror = mode ? inflate(&zs, Z_FINISH)
+		zerror = mode ? inflate(&zs, Z_SYNC_FLUSH)
 			      : deflate(&zs, Z_FINISH);
 
 		if (zerror == Z_STREAM_END)
 			break;
-		else if (zerror == Z_OK)
-			; /* once more. */
-		else {
+		else if (zerror == Z_OK) {
+			if (mode && zs.avail_out != 0)
+				goto terminate;
+			else
+				; /* once more. */
+		} else {
 			if (zs.msg) {
 				ipseclog((LOG_ERR, "ipcomp_%scompress: "
 				    "%sflate(Z_FINISH): %s\n",
