30eeff14ef9469e83a3ec32bfd77f5e1cb66df8f
Jake Burkholder <jake@FreeBSD.org>
2002-05-13 04:35:08 +0000

    Handle alignment fault fixups in libc rather than in the kernel.

--- lib/libc/sparc64/sys/__sparc_utrap_private.h
+++ lib/libc/sparc64/sys/__sparc_utrap_private.h
@@ -55,6 +55,7 @@ u_long __emul_fetch_reg(struct utrapframe *uf, int reg);
 void __emul_store_reg(struct utrapframe *uf, int reg, u_long val);
 u_long __emul_f3_op2(struct utrapframe *uf, u_int insn);
 u_long __emul_f3_memop_addr(struct utrapframe *uf, u_int insn);
+int __unaligned_fixup(struct utrapframe *uf);
 
 void __sparc_utrap(struct utrapframe *);
 
