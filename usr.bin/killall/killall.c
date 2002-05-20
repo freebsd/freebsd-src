b9075a735b779339021c8b4d68b7cc232667ce6a
dd <dd@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-05-20 07:17:22 +0000

    Mark a warning as such.  Without this, it isn't obvious whether killall stopped after failing to kill one process or whether it kept going.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@96976 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/killall/killall.c
+++ usr.bin/killall/killall.c
@@ -366,8 +366,8 @@ main(int ac, char **av)
 		killed++;
 		if (!dflag && !sflag) {
 			if (kill(thispid, sig) < 0 /* && errno != ESRCH */ ) {
-				warn("kill -%s %d", upper(sys_signame[sig]),
-				    thispid);
+				warn("warning: kill -%s %d",
+				    upper(sys_signame[sig]), thispid);
 				errors = 1;
 			}
 		}
