b27e1e5fdbfb0ac83c5f88bef7d116d5a259252c
jake <jake@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-05-21 00:29:02 +0000

    Redefine the tte accessor macros to take a pointer to a tte, instead of the value of the tag or data field. Add macros for getting the page shift, size and mask for the physical page that a tte maps (which may be one of several sizes). Use the new cache functions for invalidating single pages.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@97027 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sparc64/sparc64/pv.c
+++ sys/sparc64/sparc64/pv.c
@@ -167,7 +167,7 @@ pv_bit_clear(vm_page_t m, u_long bits)
 					vm_page_dirty(m);
 			}
 			atomic_clear_long(&tp->tte_data, bits);
-			tlb_tte_demap(*tp, pv->pv_pmap);
+			tlb_tte_demap(tp, pv->pv_pmap);
 		}
 	}
 }
@@ -269,7 +269,7 @@ pv_remove_all(vm_page_t m)
 				vm_page_dirty(m);
 		}
 		atomic_clear_long(&tp->tte_data, TD_V);
-		tlb_tte_demap(*tp, pv->pv_pmap);
+		tlb_tte_demap(tp, pv->pv_pmap);
 		tp->tte_vpn = 0;
 		tp->tte_data = 0;
 		pv->pv_pmap->pm_stats.resident_count--;
