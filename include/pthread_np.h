aaf974275fc40813f73a13874e2f66b06dd25d81
deischen <deischen@FreeBSD.org>
2002-05-24 04:33:07 +0000

    Add the prototypes for pthread_suspend_all_np and pthread_resume_all_np.

--- include/pthread_np.h
+++ include/pthread_np.h
@@ -45,8 +45,10 @@ typedef void	(*pthread_switch_routine_t)(pthread_t, pthread_t);
 __BEGIN_DECLS
 int pthread_attr_setcreatesuspend_np(pthread_attr_t *);
 int pthread_multi_np(void);
+int pthread_resume_all_np(void);
 int pthread_resume_np(pthread_t);
 int pthread_single_np(void);
+int pthread_suspend_all_np(void);
 int pthread_suspend_np(pthread_t);
 int pthread_mutexattr_getkind_np(pthread_mutexattr_t);
 int pthread_mutexattr_setkind_np(pthread_mutexattr_t *, int);
