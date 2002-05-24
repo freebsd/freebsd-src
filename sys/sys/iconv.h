b5c797e9f921edf24a9bf731e6219ce36f811b02
markm <markm@FreeBSD.org>
2002-05-24 09:40:51 +0000

    The previous ANSIfication did not take into account earlier, non-compliant compilers. Revert to the compatible form to allow upgrade-builds.

--- sys/sys/iconv.h
+++ sys/sys/iconv.h
@@ -146,9 +146,9 @@ int iconv_converter_donestub(struct iconv_converter_class *dp);
 int iconv_converter_handler(module_t mod, int type, void *data);
 
 #ifdef ICONV_DEBUG
-#define ICDEBUG(format, ...) printf("%s: "format, __func__ , __VA_ARGS__)
+#define ICDEBUG(format, args...) printf("%s: "format, __func__ ,## args)
 #else
-#define ICDEBUG(format, ...)
+#define ICDEBUG(format, args...)
 #endif
 
 #endif /* !_KERNEL */
