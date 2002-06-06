282bbce194b9920c9aec04e1d809fe2101de1d79
gibbs <gibbs@FreeBSD.org>
2002-06-06 16:35:58 +0000

    Hook up the ahd driver.

--- sys/dev/aic7xxx/aic79xx_osm.c
+++ sys/dev/aic7xxx/aic79xx_osm.c
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id$
+ * $Id: //depot/aic7xxx/freebsd/dev/aic7xxx/aic79xx_osm.c#15 $
  *
  * $FreeBSD$
  */
