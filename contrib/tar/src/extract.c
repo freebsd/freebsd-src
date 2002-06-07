c44513507c96db5f32b93177b10ad6588c417e36
Maxim Sobolev <sobomax@FreeBSD.org>
2002-06-07 00:03:23 +0000

    IMO it was a quite ugly idea that if we are running as uid 0 then we can safely ignore current umask(2) and assume that permissions should be set right like in the archive. Not only it violates POLA, but introduces huge potential security vulnerability, particularly for ports, where many popular archives come with 777 files and dirs.

--- contrib/tar/src/extract.c
+++ contrib/tar/src/extract.c
@@ -19,6 +19,8 @@
    with this program; if not, write to the Free Software Foundation, Inc.,
    59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
+/* $FreeBSD$ */
+
 #include "system.h"
 #include <quotearg.h>
 
@@ -110,8 +112,10 @@ void
 extr_init (void)
 {
   we_are_root = geteuid () == 0;
+#ifndef __FreeBSD__
   same_permissions_option += we_are_root;
   same_owner_option += we_are_root;
+#endif
   xalloc_fail_func = extract_finish;
 
   /* Option -p clears the kernel umask, so it does not affect proper
