81bcfda2d115601399220d1f95ad50af6967ec78
ache <ache@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-06-08 07:47:23 +0000

    This commit was generated by cvs2svn to compensate for changes in r98038, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@98039 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ contrib/gnu-sort/lib/long-options.h
@@ -0,0 +1,35 @@
+/* long-options.h -- declaration for --help- and --version-handling function.
+   Copyright (C) 1993, 1994, 1998, 1999 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+/* Written by Jim Meyering.  */
+
+#ifndef PARAMS
+# if defined PROTOTYPES || (defined __STDC__ && __STDC__)
+#  define PARAMS(Args) Args
+# else
+#  define PARAMS(Args) ()
+# endif
+#endif
+
+void
+  parse_long_options PARAMS ((int _argc,
+			      char **_argv,
+			      const char *_command_name,
+			      const char *_package,
+			      const char *_version,
+			      const char *_authors,
+			      void (*_usage) (int)));
