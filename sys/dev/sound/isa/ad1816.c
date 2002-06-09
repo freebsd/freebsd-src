30d12b5c8696be0f0f417009bdb15fd111bd087f
Hellmuth Michaelis <hm@FreeBSD.org>
2002-06-09 14:20:17 +0000

    Fix driver to re-enable sound output on AD1816 based cards caused by an obviously bogous return value of ad1816chan_setformat(). PR:             37932 Submitted by:   Martin Kaeske <Martin.Kaeske@Stud.TU-Ilmenau.DE> Reviewed by:    hm MFC after:      10 days

--- sys/dev/sound/isa/ad1816.c
+++ sys/dev/sound/isa/ad1816.c
@@ -368,7 +368,11 @@ ad1816chan_setformat(kobj_t obj, void *data, u_int32_t format)
     	if (format & AFMT_STEREO) fmt |= AD1816_STEREO;
     	io_wr(ad1816, reg, fmt);
 	ad1816_unlock(ad1816);
+#if 0
     	return format;
+#else
+    	return 0;
+#endif    	
 }
 
 static int
