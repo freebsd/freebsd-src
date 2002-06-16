30cbbccadb284f0af71b9e5ff292681ceb574a37
Jacques Vidrine <nectar@FreeBSD.org>
2002-06-16 21:04:15 +0000

    Add Kerberos 5 support to su.
    Reviewed by:	freebsd-audit, security-officer

--- usr.bin/su/su.c
+++ usr.bin/su/su.c
@@ -69,28 +69,43 @@ static const char rcsid[] =
 #include <skey.h>
 #endif
 
-#ifdef KERBEROS
+#ifdef KERBEROS5
+#include <krb5.h>
+
+static long get_su_principal(krb5_context context, const char *target_user,
+    const char *current_user, char **su_principal_name,
+    krb5_principal *su_principal);
+static long kerberos5(krb5_context context, const char *current_user,
+    const char *target_user, krb5_principal su_principal,
+    const char *pass);
+
+int use_kerberos5 = 1;
+#endif
+
+#ifdef KERBEROS4
 #include <openssl/des.h>
 #include <krb.h>
 #include <netdb.h>
 
-#ifdef LOGIN_CAP
-#define	ARGSTR	"-Kflmc:"
-#else
-#define	ARGSTR	"-Kflm"
-#endif
 
-static int kerberos(char *username, char *user, int uid, char *pword);
+static int kerberos4(char *username, char *user, int uid, char *pword);
 static int koktologin(char *name, char *toname);
 
-int use_kerberos = 1;
-#else /* !KERBEROS */
+int use_kerberos4 = 1;
+#endif /* KERBEROS4 */
+
 #ifdef LOGIN_CAP
-#define	ARGSTR	"-flmc:"
+#define LOGIN_CAP_ARG(x) x
 #else
-#define	ARGSTR	"-flm"
+#define LOGIN_CAP_ARG(x)
 #endif
-#endif /* KERBEROS */
+#if defined(KERBEROS4) || defined(KERBEROS5)
+#define KERBEROS_ARG(x) x
+#else
+#define KERBEROS_ARG(x)
+#endif
+#define COMMON_ARG(x) x
+#define ARGSTR	"-" COMMON_ARG("flm") LOGIN_CAP_ARG("c:") KERBEROS_ARG("K")
 
 char   *ontty __P((void));
 int	chshell __P((char *));
@@ -118,8 +133,13 @@ main(argc, argv)
 	char *class=NULL;
 	int setwhat;
 #endif
-#ifdef KERBEROS
+#if defined(KERBEROS4) || defined(KERBEROS5)
 	char *k;
+#endif
+#ifdef KERBEROS5
+	char *su_principal_name, *ccname;
+	krb5_context context;
+	krb5_principal su_principal;
 #endif
 	char shellbuf[MAXPATHLEN];
 
@@ -130,9 +150,14 @@ main(argc, argv)
 	user = "root";
 	while((ch = getopt(argc, argv, ARGSTR)) != -1) 
 		switch((char)ch) {
-#ifdef KERBEROS
+#if defined(KERBEROS4) || defined(KERBEROS5)
 		case 'K':
-			use_kerberos = 0;
+#ifdef KERBEROS4
+			use_kerberos4 = 0;
+#endif
+#ifdef KERBEROS5
+			use_kerberos5 = 0;
+#endif
 			break;
 #endif
 		case 'f':
@@ -179,10 +204,22 @@ main(argc, argv)
 
 	argv += optind;
 
-#ifdef KERBEROS
+#if defined(KERBEROS4) || defined(KERBEROS5)
 	k = auth_getval("auth_list");
-	if (k && !strstr(k, "kerberos"))
-	    use_kerberos = 0;
+	if (k && !strstr(k, "kerberos")) {
+#ifdef KERBEROS4
+	    use_kerberos4 = 0;
+#endif
+#ifdef KERBEROS5
+	    use_kerberos5 = 0;
+#endif
+	}
+#endif
+#ifdef KERBEROS5
+	su_principal_name = NULL;
+	su_principal = NULL;
+	if (krb5_init_context(&context) != 0)
+		use_kerberos5 = 0;
 #endif
 	errno = 0;
 	prio = getpriority(PRIO_PROCESS, 0);
@@ -235,11 +272,21 @@ main(argc, argv)
 #endif /* WHEELSU */
 
 	if (ruid) {
-#ifdef KERBEROS
-		if (use_kerberos && koktologin(username, user)
+#ifdef KERBEROS4
+		if (use_kerberos4 && koktologin(username, user)
 		    && !pwd->pw_uid) {
-			warnx("kerberos: not in %s's ACL.", user);
-			use_kerberos = 0;
+			warnx("kerberos4: not in %s's ACL.", user);
+			use_kerberos4 = 0;
+		}
+#endif
+#ifdef KERBEROS5
+		if (use_kerberos5) {
+			if (get_su_principal(context, user, username,
+			    &su_principal_name, &su_principal) != 0 ||
+			    !krb5_kuserok(context, su_principal, user)) {
+				warnx("kerberos5: not in %s's ACL.", user);
+				use_kerberos5 = 0;
+			}
 		}
 #endif
 		{
@@ -288,15 +335,25 @@ main(argc, argv)
 			p = getpass("Password:");
 			if (strcmp(pwd->pw_passwd, crypt(p, pwd->pw_passwd))) {
 #endif
-#ifdef KERBEROS
-	    			if (!use_kerberos || (use_kerberos && kerberos(username, user, pwd->pw_uid, p)))
+#ifdef KERBEROS4
+				if (use_kerberos4 && kerberos4(username, user,
+				    pwd->pw_uid, p) == 0)
+					goto authok;
 #endif
-					{
-					fprintf(stderr, "Sorry\n");
-					syslog(LOG_AUTH|LOG_WARNING, "BAD SU %s to %s%s", username, user, ontty());
-					exit(1);
-				}
+#ifdef KERBEROS5
+				if (use_kerberos5 && kerberos5(context,
+				    username, user, su_principal, p) == 0)
+					goto authok;
+#endif
+				fprintf(stderr, "Sorry\n");
+				syslog(LOG_AUTH|LOG_WARNING,
+				    "BAD SU %s to %s%s", username, user,
+				    ontty());
+				exit(1);
 			}
+#if defined(KERBEROS4) || defined(KERBEROS5)
+		authok:
+#endif
 #ifdef WHEELSU
 			if (iswheelsu) {
 				pwd = getpwnam(user);
@@ -361,8 +418,11 @@ main(argc, argv)
 	if (!asme) {
 		if (asthem) {
 			p = getenv("TERM");
-#ifdef KERBEROS
+#ifdef KERBEROS4
 			k = getenv("KRBTKFILE");
+#endif
+#ifdef KERBEROS5
+			ccname = getenv("KRB5CCNAME");
 #endif
 			if ((cleanenv = calloc(20, sizeof(char*))) == NULL)
 				errx(1, "calloc");
@@ -376,9 +436,13 @@ main(argc, argv)
 #endif
 			if (p)
 				(void)setenv("TERM", p, 1);
-#ifdef KERBEROS
+#ifdef KERBEROS4
 			if (k)
 				(void)setenv("KRBTKFILE", k, 1);
+#endif
+#ifdef KERBEROS5
+			if (ccname)
+				(void)setenv("KRB5CCNAME", ccname, 1);
 #endif
 			if (chdir(pwd->pw_dir) < 0)
 				errx(1, "no directory");
@@ -413,12 +477,8 @@ main(argc, argv)
 static void
 usage()
 {
-	(void)fprintf(stderr, "usage: su [-] %s%s[login [args]]\n",
-#ifdef KERBEROS
-	    "[-Kflm] ",
-#else
-	    "[-flm] ",
-#endif
+	(void)fprintf(stderr, "usage: su [-] [-%s] %s[login [args]]\n",
+	    KERBEROS_ARG("K") COMMON_ARG("flm"),
 #ifdef LOGIN_CAP
 	    "[-c class] "
 #else
@@ -455,9 +515,159 @@ ontty()
 	return (buf);
 }
 
-#ifdef KERBEROS
+#ifdef KERBEROS5
+const char superuser[] = "root";
+
+/* Authenticate using Kerberos 5.
+ *   context           -- An initialized krb5_context.
+ *   current_user      -- The current username.
+ *   target_user       -- The target account name.
+ *   su_principal      -- The target krb5_principal.
+ *   pass              -- The user's password.
+ * Note that a valid keytab in the default location with a host entry
+ * must be available.
+ * Returns 0 if authentication was successful, or a com_err error code if
+ * it was not.
+ */
+static long
+kerberos5(krb5_context context, const char *current_user,
+    const char *target_user, krb5_principal su_principal,
+    const char *pass)
+{
+	krb5_creds	 creds;
+	krb5_get_init_creds_opt gic_opt;
+	krb5_verify_init_creds_opt vic_opt;
+	long		 rv;
+
+	krb5_get_init_creds_opt_init(&gic_opt);
+	krb5_verify_init_creds_opt_init(&vic_opt);
+	rv = krb5_get_init_creds_password(context, &creds, su_principal,
+	    pass, NULL, NULL, 0, NULL, &gic_opt);
+	if (rv != 0) {
+		syslog(LOG_NOTICE|LOG_AUTH, "BAD Kerberos5 SU: %s to %s%s: %s",
+		    current_user, target_user, ontty(),
+		    krb5_get_err_text(context, rv));
+		return (rv);
+	}
+	krb5_verify_init_creds_opt_set_ap_req_nofail(&vic_opt, 1);
+	rv = krb5_verify_init_creds(context, &creds, NULL, NULL, NULL,
+	    &vic_opt);
+	krb5_free_cred_contents(context, &creds);
+	if (rv != 0) {
+		syslog(LOG_NOTICE|LOG_AUTH, "BAD Kerberos5 SU: %s to %s%s: %s",
+		    current_user, target_user, ontty(),
+		    krb5_get_err_text(context, rv));
+		return (rv);
+	}
+	return (0);
+}
+
+/* Determine the target principal given the current user and the target user.
+ *   context           -- An initialized krb5_context.
+ *   target_user       -- The target username.
+ *   current_user      -- The current username.
+ *   su_principal_name -- (out) The target principal name.
+ *   su_principal      -- (out) The target krb5_principal.
+ *
+ * When target_user is `root', the su_principal will be a `root
+ * instance', e.g. `luser/root@REA.LM'.  Otherwise, the su_principal
+ * will simply be the current user's default principal name.  Note that
+ * in any case, if KRB5CCNAME is set and a credentials cache exists, the
+ * principal name found there will be the `starting point', rather than
+ * the current_user parameter.
+ *
+ * Returns 0 for success, or a com_err error code on failure.
+ */
+static long
+get_su_principal(krb5_context context, const char *target_user,
+    const char *current_user, char **su_principal_name,
+    krb5_principal *su_principal)
+{
+	krb5_principal	 default_principal;
+	krb5_ccache	 ccache;
+	char		*principal_name, *ccname, *p;
+	long		 rv;
+	uid_t		 euid, ruid;
+
+	*su_principal = NULL;
+	default_principal = NULL;
+	/* Lower privs while messing about with the credentials
+	 * cache.
+	 */
+	ruid = getuid();
+	euid = geteuid();
+	rv = seteuid(getuid());
+	if (rv != 0)
+		return (errno);
+	p = getenv("KRB5CCNAME");
+	if (p != NULL)
+		ccname = strdup(p);
+	else
+		(void)asprintf(&ccname, "%s%lu", KRB5_DEFAULT_CCROOT,
+		    (unsigned long)ruid);
+	if (ccname == NULL)
+		return (errno);
+	rv = krb5_cc_resolve(context, ccname, &ccache);
+	free(ccname);
+	if (rv == 0) {
+		rv = krb5_cc_get_principal(context, ccache,
+		    &default_principal);
+		krb5_cc_close(context, ccache);
+		if (rv != 0)
+			default_principal = NULL; /* just to be safe */
+	}
+	rv = seteuid(euid);
+	if (rv != 0)
+		return (errno);
+	if (default_principal == NULL) {
+		rv = krb5_make_principal(context, &default_principal, NULL,
+		    current_user, NULL);
+		if (rv != 0) {
+			warnx("Could not determine default principal name.");
+			return (rv);
+		}
+	}
+	/* Now that we have some principal, if the target account is
+	 * `root', then transform it into a `root' instance, e.g.
+	 * `user@REA.LM' -> `user/root@REA.LM'.
+	 */
+	rv = krb5_unparse_name(context, default_principal, &principal_name);
+	krb5_free_principal(context, default_principal);
+	if (rv != 0) {
+		warnx("krb5_unparse_name: %s", krb5_get_err_text(context, rv));
+		return (rv);
+	}
+	if (strcmp(target_user, superuser) == 0) {
+		p = strrchr(principal_name, '@');
+		if (p == NULL) {
+			warnx("malformed principal name `%s'", principal_name);
+			free(principal_name);
+			return (rv);
+		}
+		*p++ = '\0';
+		(void)asprintf(su_principal_name, "%s/%s@%s", principal_name,
+		    superuser, p);
+		free(principal_name);
+	} else 
+		*su_principal_name = principal_name;
+	if (*su_principal_name == NULL)
+		return errno;
+	rv = krb5_parse_name(context, *su_principal_name, &default_principal);
+	if (rv != 0) {
+		warnx("krb5_parse_name `%s': %s", *su_principal_name,
+		    krb5_get_err_text(context, rv));
+		free(*su_principal_name);
+		return (rv);
+	}
+	*su_principal = default_principal;
+	return 0;
+}
+
+#endif
+
+#ifdef KERBEROS4
 int
-kerberos(username, user, uid, pword)
+kerberos4(username, user, uid, pword)
 	char *username, *user;
 	int uid;
 	char *pword;
@@ -503,14 +713,14 @@ kerberos(username, user, uid, pword)
 
 	if (kerno != KSUCCESS) {
 		if (kerno == KDC_PR_UNKNOWN) {
-			warnx("kerberos: principal unknown: %s.%s@%s",
+			warnx("kerberos4: principal unknown: %s.%s@%s",
 				(uid == 0 ? username : user),
 				(uid == 0 ? "root" : ""), lrealm);
 			return (1);
 		}
-		warnx("kerberos: unable to su: %s", krb_err_txt[kerno]);
+		warnx("kerberos4: unable to su: %s", krb_err_txt[kerno]);
 		syslog(LOG_NOTICE|LOG_AUTH,
-		    "BAD Kerberos SU: %s to %s%s: %s",
+		    "BAD Kerberos4 SU: %s to %s%s: %s",
 		    username, user, ontty(), krb_err_txt[kerno]);
 		return (1);
 	}
@@ -556,7 +766,7 @@ kerberos(username, user, uid, pword)
 
 		if ((kerno = krb_rd_req(&ticket, "rcmd", savehost, faddr,
 		    &authdata, "")) != KSUCCESS) {
-			warnx("kerberos: unable to verify rcmd ticket: %s\n",
+			warnx("kerberos4: unable to verify rcmd ticket: %s\n",
 			    krb_err_txt[kerno]);
 			syslog(LOG_NOTICE|LOG_AUTH,
 			    "failed su: %s to %s%s: %s", username,
