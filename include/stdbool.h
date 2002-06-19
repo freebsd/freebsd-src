08d68d18e0c32c0ec23afac4e139932b3f5e0795
David E. O'Brien <obrien@FreeBSD.org>
2002-06-19 06:04:37 +0000

    Guard definitions for use with C++ code.
    Submitted by:	Ed Hall <edhall@yahoo-inc.com>

--- include/stdbool.h
+++ include/stdbool.h
@@ -31,6 +31,8 @@
 
 #define	__bool_true_false_are_defined	1
 
+#ifndef __cplusplus
+
 #define	false	0
 #define	true	1
 
@@ -38,5 +40,7 @@
 #if __STDC_VERSION__ < 199901L
 typedef	int	_Bool;
 #endif
+
+#endif /* !__cplusplus */
 
 #endif /* !_STDBOOL_H_ */
