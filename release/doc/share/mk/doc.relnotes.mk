6ebf982d711bccf759577165ab547d1940d9b357
bmah <bmah@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-06-20 21:19:49 +0000

    Provide a way to keep out old ("historic") release notes from bloating the size of the rendered release notes.  By default, any element with a role="historic" attribute won't get rendered in the output unless the INCLUDE_HISTORIC Makefile variable is defined.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@98515 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/doc/share/mk/doc.relnotes.mk
+++ release/doc/share/mk/doc.relnotes.mk
@@ -9,3 +9,20 @@ EXTRA_CATALOGS+= ${RELN_ROOT}/share/sgml/catalog
 # Use the appropriate architecture-dependent RELNOTESng stylesheet
 DSLHTML?=	${RELN_ROOT}/share/sgml/default.dsl
 DSLPRINT?=	${RELN_ROOT}/share/sgml/default.dsl
+
+#
+# Tweakable Makefile variables
+#
+# INCLUDE_HISTORIC	Used by relnotes document only.  When set,
+#			causes all release notes entries to be printed,
+#			even those marked as "historic".  If not set
+#			(the default), only print "non-historic"
+#			release note entries.  To designate a release
+#			note entry as "historic", add a role="historic"
+#			attribute to the applicable element(s).
+#
+.if defined(INCLUDE_HISTORIC)
+JADEFLAGS+=	-iinclude.historic
+.else
+JADEFLAGS+=	-ino.include.historic
+.endif
