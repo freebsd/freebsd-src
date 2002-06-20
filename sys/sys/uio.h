72c289faf4819f8733ef7488d70a29df4faef764
peter <peter@FreeBSD.org>
2002-06-20 07:08:43 +0000

    Remove UIO_USERISPACE - we do not support any split instruction/data address space machines (eg: pdp-11) and are not likely to ever do so. Nothing in our kernel sets this.

--- sys/sys/uio.h
+++ sys/sys/uio.h
@@ -52,7 +52,6 @@ enum	uio_rw { UIO_READ, UIO_WRITE };
 enum uio_seg {
 	UIO_USERSPACE,		/* from user data space */
 	UIO_SYSSPACE,		/* from system space */
-	UIO_USERISPACE,		/* from user I space */
 	UIO_NOCOPY		/* don't copy, already in object */
 };
 
