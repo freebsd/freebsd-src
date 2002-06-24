edf54533a880bb4fd61fb2974a0468a11d2d522e
jmallett <jmallett@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-06-24 13:52:28 +0000

    Convert straightforward regression tests to use regress.m4.
    Goodbye, duplicated code, you will certainly not be missed.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@98754 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- tools/regression/usr.bin/uuencode/regress.sh
+++ tools/regression/usr.bin/uuencode/regress.sh
@@ -1,33 +1,11 @@
 # $FreeBSD$
 
-# Go into the regression test directory, handed to us by make(1)
-TESTDIR=$1
-if [ -z "$TESTDIR" ]; then
-  TESTDIR=.
-fi
-cd $TESTDIR
-
-STATUS=0
+REGRESSION_START($1)
 
 # To make sure we end up with matching headers.
 umask 022
 
-for test in traditional base64; do
-  echo "Running test $test"
-  case "$test" in
-  traditional)
-    uuencode regress.in < regress.in | diff -u regress.$test.out -
-    ;;
-  base64)
-    uuencode -m regress.in < regress.in | diff -u regress.$test.out -
-    ;;
-  esac
-  if [ $? -eq 0 ]; then
-    echo "PASS: Test $test detected no regression, output matches."
-  else
-    STATUS=$?
-    echo "FAIL: Test $test failed: regression detected.  See above."
-  fi
-done
+REGRESSION_TEST(`traditional', `uuencode regress.in < regress.in')
+REGRESSION_TEST(`base64', `uuencode -m regress.in < regress.in')
 
-exit $STATUS
+REGRESSION_END()
