a9d8989e3482555261cf1f052a199bce4ec5236c
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-06-24 04:42:46 +0000

    Add kernel printf bits for WI_SUPPRATES and HOSTAP_FLAGS
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@98720 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/wi/wi_hostap.h
+++ sys/dev/wi/wi_hostap.h
@@ -49,6 +49,7 @@ struct hostap_sta {
 #define HOSTAP_FLAGS_AUTHEN	0x0001
 #define HOSTAP_FLAGS_ASSOC	0x0002
 #define HOSTAP_FLAGS_PERM	0x0004
+#define	HOSTAP_FLAGS_BITS	"\20\01ASSOC\02AUTH\03PERM"
 
 #define SIOCHOSTAP_GET 		_IOWR('i', 210, struct ifreq)
 #define SIOCHOSTAP_ADD 		_IOWR('i', 211, struct ifreq)
