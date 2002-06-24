a06be26703d15ed2370d621612247e28cb5ca1af
jake <jake@FreeBSD.org>
2002-06-24 05:23:46 +0000

    Add needed include of mman.h to fix sparc64 buildworld.

--- libexec/rtld-elf/sparc64/reloc.c
+++ libexec/rtld-elf/sparc64/reloc.c
@@ -39,12 +39,14 @@
  * $FreeBSD$
  */
 
+#include <sys/param.h>
+#include <sys/mman.h>
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/stat.h>
 
 #include "debug.h"
 #include "rtld.h"
