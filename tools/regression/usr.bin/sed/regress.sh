0c94b9211fb5b39e0b394aef9db23677a9aee38d
jmallett <jmallett@FreeBSD.org>
2002-06-24 13:52:28 +0000

    Convert straightforward regression tests to use regress.m4.
    Goodbye, duplicated code, you will certainly not be missed.

--- tools/regression/usr.bin/sed/regress.sh
+++ tools/regression/usr.bin/sed/regress.sh
@@ -1,33 +1,9 @@
 # $FreeBSD$
 
-# Go into the regression test directory, handed to us by make(1)
-TESTDIR=$1
-if [ -z "$TESTDIR" ]; then
-  TESTDIR=.
-fi
-cd $TESTDIR
+REGRESSION_START($1)
 
-STATUS=0
+REGRESSION_TEST(`G', `sed G < regress.in')
+REGRESSION_TEST(`P', `sed P < regress.in')
+REGRESSION_TEST(`psl', `sed \$!g\;P\;D < regress.in')
 
-for test in G P psl; do
-  echo "Running test $test"
-  case "$test" in
-  G)
-    sed G < regress.in | diff -u regress.$test.out -
-    ;;
-  P)
-    sed P < regress.in | diff -u regress.$test.out -
-    ;;
-  psl)	
-    sed '$!g; P; D' < regress.in | diff -u regress.$test.out -
-    ;;
-  esac
-  if [ $? -eq 0 ]; then
-    echo "PASS: Test $test detected no regression, output matches."
-  else
-    STATUS=$?
-    echo "FAIL: Test $test failed: regression detected.  See above."
-  fi
-done
-
-exit $STATUS
+REGRESSION_END()
