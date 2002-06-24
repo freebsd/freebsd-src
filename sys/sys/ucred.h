bfedcf9f1f782f1c2f33780907e983deffcaecb0
bde <bde@FreeBSD.org>
2002-06-24 11:37:56 +0000

    Fixed some style bugs (mainly excessive indentation).
    Not completely unapproved by:	julian

--- sys/sys/ucred.h
+++ sys/sys/ucred.h
@@ -44,23 +44,23 @@
  * Only the suser() or suser_cred() function should be used for this.
  */
 struct ucred {
-	u_int		cr_ref;		/* reference count */
+	u_int	cr_ref;			/* reference count */
 #define	cr_startcopy cr_uid
-	uid_t		cr_uid;		/* effective user id */
-	uid_t		cr_ruid;	/* real user id */
-	uid_t		cr_svuid;	/* saved user id */
-	short		cr_ngroups;	/* number of groups */
-	gid_t		cr_groups[NGROUPS]; /* groups */
-	gid_t		cr_rgid;	/* real group id */
-	gid_t		cr_svgid;	/* saved user id */
+	uid_t	cr_uid;			/* effective user id */
+	uid_t	cr_ruid;		/* real user id */
+	uid_t	cr_svuid;		/* saved user id */
+	short	cr_ngroups;		/* number of groups */
+	gid_t	cr_groups[NGROUPS];	/* groups */
+	gid_t	cr_rgid;		/* real group id */
+	gid_t	cr_svgid;		/* saved user id */
 	struct uidinfo	*cr_uidinfo;	/* per euid resource consumption */
 	struct uidinfo	*cr_ruidinfo;	/* per ruid resource consumption */
 	struct prison	*cr_prison;	/* jail(4) */
-#define	cr_endcopy cr_mtxp
+#define	cr_endcopy	cr_mtxp
 	struct mtx	*cr_mtxp;      	/* protect refcount */
 };
-#define NOCRED ((struct ucred *)0)	/* no credential available */
-#define FSCRED ((struct ucred *)-1)	/* filesystem credential */
+#define	NOCRED	((struct ucred *)0)	/* no credential available */
+#define	FSCRED	((struct ucred *)-1)	/* filesystem credential */
 
 /*
  * This is the external representation of struct ucred.
@@ -75,26 +75,26 @@ struct xucred {
 #define	XUCRED_VERSION	0
 
 /* This can be used for both ucred and xucred structures. */
-#define cr_gid cr_groups[0]
+#define	cr_gid cr_groups[0]
 
 #ifdef _KERNEL
 struct thread;
 
-void		cred_update_thread(struct thread *td);
-void		change_egid(struct ucred *newcred, gid_t egid);
-void		change_euid(struct ucred *newcred, struct uidinfo *euip);
-void		change_rgid(struct ucred *newcred, gid_t rgid);
-void		change_ruid(struct ucred *newcred, struct uidinfo *ruip);
-void		change_svgid(struct ucred *newcred, gid_t svgid);
-void		change_svuid(struct ucred *newcred, uid_t svuid);
-void		crcopy(struct ucred *dest, struct ucred *src);
+void	change_egid(struct ucred *newcred, gid_t egid);
+void	change_euid(struct ucred *newcred, struct uidinfo *euip);
+void	change_rgid(struct ucred *newcred, gid_t rgid);
+void	change_ruid(struct ucred *newcred, struct uidinfo *ruip);
+void	change_svgid(struct ucred *newcred, gid_t svgid);
+void	change_svuid(struct ucred *newcred, uid_t svuid);
+void	crcopy(struct ucred *dest, struct ucred *src);
 struct ucred	*crdup(struct ucred *cr);
-void		crfree(struct ucred *cr);
+void	cred_update_thread(struct thread *td);
+void	crfree(struct ucred *cr);
 struct ucred	*crget(void);
 struct ucred	*crhold(struct ucred *cr);
-int		crshared(struct ucred *cr);
-void		cru2x(struct ucred *cr, struct xucred *xcr);
-int		groupmember(gid_t gid, struct ucred *cred);
+int	crshared(struct ucred *cr);
+void	cru2x(struct ucred *cr, struct xucred *xcr);
+int	groupmember(gid_t gid, struct ucred *cred);
 #endif /* _KERNEL */
 
 #endif /* !_SYS_UCRED_H_ */
