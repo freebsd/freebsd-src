023a3f5ec017ace9017a4aef631d2005fbd98f5b
jmallett <jmallett@FreeBSD.org>
2002-06-24 14:22:44 +0000

    Move all remaining tests except for: 	make(1):	Does not work like the other tests.  Its Makefile is 			self-testing. 	m4(1):		It uses complex voodo to test GNU m4(1) features.
    To the new framework.  I had worried about passing the binary data that
    uudecode(1)'s test passes to diff(1) might give a user something nasty,
    but this is unlikely to happen as even with an unmodified old nasty
    diff(1) which doesn't recognise many binary files, these binary files
    are recognised.  Using $DIFF instead of `diff' in the library and making
    it possible to override this with `cmp -s' might be nice some day, but
    as of this second, there's no immediate need.

--- tools/regression/usr.bin/jot/regress.sh
+++ tools/regression/usr.bin/jot/regress.sh
@@ -1,16 +1,7 @@
 # $FreeBSD$
 
-# Go into the regression test directory, handed to us by make(1)
-TESTDIR=$1
-if [ -z "$TESTDIR" ]; then
-  TESTDIR=.
-fi
-cd $TESTDIR
+REGRESSION_START($1)
 
-jot -w '%X' -s ',' 100 1 200 | diff -u regress.out -
-if [ $? -eq 0 ]; then
-  echo "PASS: Test detected no regression, output matches."
-else
-  echo "FAIL: Test failed: regression detected.  See above."
-  exit 1
-fi
+REGRESSION_TEST_ONE(`jot -w "%X" -s , 100 1 200')
+
+REGRESSION_END()
