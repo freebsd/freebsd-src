9b886a82acdc2907873ad3799a9d71a1fb760e39
jmallett <jmallett@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-06-25 12:10:09 +0000

    Add a regression test for transliterate which happens to match characters at an EOL/EOF, and therefore should catch the broken behaviour fixed by Tim J. Robbins in sed(1) recently.
    Suggested by:	obrien
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@98811 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- tools/regression/usr.bin/sed/regress.sh
+++ tools/regression/usr.bin/sed/regress.sh
@@ -5,5 +5,6 @@ REGRESSION_START($1)
 REGRESSION_TEST(`G', `sed G < regress.in')
 REGRESSION_TEST(`P', `sed P < regress.in')
 REGRESSION_TEST(`psl', `sed \$!g\;P\;D < regress.in')
+REGRESSION_TEST(`y', `sed y/\(\)/{}/ < regress.in')
 
 REGRESSION_END()
