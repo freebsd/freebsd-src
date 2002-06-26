1163aebcf2b1878ede1dc30c12660b369c9952a2
jmallett <jmallett@FreeBSD.org>
2002-06-26 16:57:36 +0000

    Erk, I forgot that regress.in has an ending newline, so change the test for transliterate to use echo -n to supress newline and a simple string.

--- tools/regression/usr.bin/sed/regress.sh
+++ tools/regression/usr.bin/sed/regress.sh
@@ -5,6 +5,6 @@ REGRESSION_START($1)
 REGRESSION_TEST(`G', `sed G < regress.in')
 REGRESSION_TEST(`P', `sed P < regress.in')
 REGRESSION_TEST(`psl', `sed \$!g\;P\;D < regress.in')
-REGRESSION_TEST(`y', `sed y/\(\)/{}/ < regress.in')
+REGRESSION_TEST(`y', `echo -n foo | sed y/o/O/')
 
 REGRESSION_END()
