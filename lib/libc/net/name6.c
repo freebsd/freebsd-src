a01d3b2eda1702a5502a41d223672bb2708c59b3
julian <julian@FreeBSD.org>
2002-06-26 21:43:44 +0000

    Add the security fix FreeBSD-SA-02:28.resolv to the 4.4 branch This branch is the one that some really big security consious organisations are running.. It needs to be maintained.
    (already done: newvers.sh)
    
    Obtained from:	Other branches

--- lib/libc/net/name6.c
+++ lib/libc/net/name6.c
@@ -1024,7 +1024,7 @@ getanswer(answer, anslen, qname, qtype, template, errp)
 	register const u_char *cp;
 	register int n;
 	const u_char *eom, *erdata;
-	char *bp, **ap, **hap;
+	char *bp, **ap, **hap, *obp;
 	int type, class, buflen, ancount, qdcount;
 	int haveanswer, had_error;
 	char tbuf[MAXDNAME];
@@ -1238,7 +1238,9 @@ getanswer(answer, anslen, qname, qtype, template, errp)
 				bp += nn;
 				buflen -= nn;
 			}
+			obp = bp; /* ALIGN rounds up */
 			bp = (char *)ALIGN(bp);
+			buflen -= (bp - obp);
 
 			DNS_FATAL(bp + n < &hostbuf[sizeof hostbuf]);
 			DNS_ASSERT(hap < &h_addr_ptrs[MAXADDRS-1]);
