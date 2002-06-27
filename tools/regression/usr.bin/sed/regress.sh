e6acea8d1b433c89587a7deb59f0ea55bee99a50
Brian Feldman <green@FreeBSD.org>
2002-06-27 15:58:59 +0000

    Correct the psl regression test for sed(1)'s now-fixed newline behavior.  Add the bcb regression test which checks for failures due to a backslash ('\') coinciding with the very last character of the command buffer.  The regression test is cf. this PR (which I did not know about) and has a different fix for the bug.
    PR:		bin/22351
    Submitted by:	Stefan Duerholt <stefan.duerholt@t-online.de>

--- tools/regression/usr.bin/sed/regress.sh
+++ tools/regression/usr.bin/sed/regress.sh
@@ -5,6 +5,7 @@ REGRESSION_START($1)
 REGRESSION_TEST(`G', `sed G < regress.in')
 REGRESSION_TEST(`P', `sed P < regress.in')
 REGRESSION_TEST(`psl', `sed \$!g\;P\;D < regress.in')
+REGRESSION_TEST(`bcb', `sed s/X/$(jot -n -bx -s "" 2043)\\\\zz/ < regress.in')
 REGRESSION_TEST(`y', `echo -n foo | sed y/o/O/')
 
 REGRESSION_END()
