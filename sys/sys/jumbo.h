a83560d6774c9a012192abdcde6802d6612fd006
Andrew Gallatin <gallatin@FreeBSD.org>
2002-07-06 02:44:15 +0000

    Remove the advertising clause from the Duke BSD copyright on the zero-copy files
    Requested by: rwatson
    Approved by: Jeff Chase (my old boss at Duke)

--- sys/sys/jumbo.h
+++ sys/sys/jumbo.h
@@ -13,10 +13,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgements:
- *      This product includes software developed by Duke University
- * 4. The name of Duke University may not be used to endorse or promote 
+ * 3. The name of Duke University may not be used to endorse or promote 
  *    products derived from this software without specific prior written 
  *    permission.
  * 
