f1091a5ae31527f9676ef6d2138c2cd2df0990ff
Tim J. Robbins <tjr@FreeBSD.org>
2002-07-09 10:47:59 +0000

    MFC: expand.c revs. 1.12-1.13, expand.1 revs. 1.10-1.11:   Allow <blank>s to be used to separate tab stop positions with the -t   argument, not just ASCII space characters and commas. Don't count   non-printing characters when determining column position. + manual page cleanup, style changes.

--- usr.bin/expand/expand.c
+++ usr.bin/expand/expand.c
@@ -41,12 +41,13 @@ static const char copyright[] =
 #if 0
 static char sccsid[] = "@(#)expand.c	8.1 (Berkeley) 6/9/93";
 #endif
-static const char rcsid[] =
-  "$FreeBSD$";
 #endif /* not lint */
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #include <ctype.h>
 #include <err.h>
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -69,8 +70,11 @@ main(argc, argv)
 	register int n;
 	int rval;
 
+	setlocale(LC_CTYPE, "");
+
 	/* handle obsolete syntax */
-	while (argc > 1 && argv[1][0] == '-' && isdigit(argv[1][1])) {
+	while (argc > 1 && argv[1][0] == '-' &&
+	    isdigit((unsigned char)argv[1][1])) {
 		getstops(&argv[1][1]);
 		argc--; argv++;
 	}
@@ -140,7 +144,8 @@ main(argc, argv)
 
 			default:
 				putchar(c);
-				column++;
+				if (isprint(c))
+					column++;
 				continue;
 
 			case '\n':
@@ -173,7 +178,7 @@ getstops(cp)
 		tabstops[nstops++] = i;
 		if (*cp == 0)
 			break;
-		if (*cp != ',' && *cp != ' ')
+		if (*cp != ',' && !isblank((unsigned char)*cp))
 			errx(1, "bad tab stop spec");
 		cp++;
 	}
