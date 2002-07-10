bad07578ef190024fc0b67bd2d8047e51a642d5e
obrien <obrien@FreeBSD.org>
2002-07-10 06:40:03 +0000

    Target specific FreeBSD kgdb hacks.

---
+++ gnu/usr.bin/binutils/gdb/fbsd-kgdb-i386.h
@@ -0,0 +1,6 @@
+/* $FreeBSD$ */
+
+#ifndef FBSD_KGDB_I386_H
+#define FBSD_KGDB_I386_H
+
+#endif /* FBSD_KGDB_I386_H */
---
+++ gnu/usr.bin/binutils/gdb/fbsd-kgdb-sparc64.h
@@ -0,0 +1,6 @@
+/* $FreeBSD$ */
+
+#ifndef FBSD_KGDB_SPARC64_H
+#define FBSD_KGDB_SPARC64_H
+
+#endif /* FBSD_KGDB_SPARC64_H */
