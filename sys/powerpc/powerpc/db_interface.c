391995a79c103dc3e23dbf9d78283257199bcd8a
benno <benno@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-07-10 12:21:54 +0000

    Add DDB support.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@99730 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ sys/powerpc/powerpc/db_interface.c
@@ -0,0 +1,377 @@
+/*	$FreeBSD$ */
+/*	$NetBSD: db_interface.c,v 1.20 2002/05/13 20:30:09 matt Exp $ */
+/*	$OpenBSD: db_interface.c,v 1.2 1996/12/28 06:21:50 rahnds Exp $	*/
+
+#define USERACC
+
+#include "opt_ddb.h"
+
+#include <sys/param.h>
+#include <sys/proc.h>
+#include <sys/systm.h>
+
+#include <machine/db_machdep.h>
+#include <machine/frame.h>
+#include <machine/trap.h>
+#ifdef PPC_IBM4XX
+#include <machine/tlb.h>
+#include <powerpc/spr.h>
+#include <uvm/uvm_extern.h>
+#endif
+
+#ifdef DDB
+#include <ddb/ddb.h>
+#include <ddb/db_sym.h>
+#include <ddb/db_command.h>
+#include <ddb/db_access.h>
+#include <ddb/db_output.h>
+#endif
+
+#ifdef KGDB
+#include <sys/kgdb.h>
+#endif
+
+#include <dev/ofw/openfirm.h>
+
+int	db_active = 0;
+
+db_regs_t ddb_regs;
+
+void ddb_trap(void);				/* Call into trap_subr.S */
+int ddb_trap_glue(struct trapframe *);		/* Called from trap_subr.S */
+#ifdef PPC_IBM4XX
+static void db_ppc4xx_ctx(db_expr_t, int, db_expr_t, char *);
+static void db_ppc4xx_pv(db_expr_t, int, db_expr_t, char *);
+static void db_ppc4xx_reset(db_expr_t, int, db_expr_t, char *);
+static void db_ppc4xx_tf(db_expr_t, int, db_expr_t, char *);
+static void db_ppc4xx_dumptlb(db_expr_t, int, db_expr_t, char *);
+#ifdef USERACC
+static void db_ppc4xx_useracc(db_expr_t, int, db_expr_t, char *);
+#endif
+#endif /* PPC_IBM4XX */
+
+#ifdef DDB
+void
+Debugger(const char *msg)
+{
+	db_printf("Welcome to Debugger, %s\n", msg);
+	ddb_trap();
+}
+#endif
+
+int
+ddb_trap_glue(frame)
+	struct trapframe *frame;
+{
+	if (!(frame->srr1 & PSL_PR)
+	    && (frame->exc == EXC_TRC || frame->exc == EXC_RUNMODETRC
+		|| (frame->exc == EXC_PGM
+		    && (frame->srr1 & 0x20000))
+		|| frame->exc == EXC_BPT)) {
+		int type = frame->exc;
+		if (type == EXC_PGM && (frame->srr1 & 0x20000)) {
+			type = T_BREAKPOINT;
+		}
+		return kdb_trap(type, frame);
+	}
+	return 0;
+}
+
+int
+kdb_trap(type, v)
+	int type;
+	void *v;
+{
+	struct trapframe *frame = v;
+
+#if 0
+	switch (type) {
+	case T_BREAKPOINT:
+	case -1:
+		break;
+	default:
+		if (!db_onpanic && db_recover == 0)
+			return 0;
+		if (db_recover != 0) {
+			db_error("Faulted in DDB; continuing...\n");
+			/*NOTREACHED*/
+		}
+	}
+#endif
+
+	/* XXX Should switch to kdb's own stack here. */
+
+	memcpy(DDB_REGS->r, frame->fixreg, 32 * sizeof(u_int32_t));
+	DDB_REGS->iar = frame->srr0;
+	DDB_REGS->msr = frame->srr1;
+	DDB_REGS->lr = frame->lr;
+	DDB_REGS->ctr = frame->ctr;
+	DDB_REGS->cr = frame->cr;
+	DDB_REGS->xer = frame->xer;
+#ifdef PPC_IBM4XX
+	DDB_REGS->dear = frame->dear;
+	DDB_REGS->esr = frame->esr;
+	DDB_REGS->pid = frame->pid;
+#endif
+
+#ifdef DDB
+	db_active++;
+	cndbctl(1);
+	db_trap(type, 0);
+	cndbctl(0);
+	db_active--;
+#elif defined(KGDB)
+	if (!kgdb_trap(type, DDB_REGS))
+		return 0;
+#endif
+
+	/* KGDB isn't smart about advancing PC if we
+	 * take a breakpoint trap after kgdb_active is set.
+	 * Therefore, we help out here.
+	 */
+	if (IS_BREAKPOINT_TRAP(type, 0)) {
+		int bkpt;
+		db_read_bytes(PC_REGS(DDB_REGS),BKPT_SIZE,(void *)&bkpt);
+		if (bkpt== BKPT_INST) {
+			PC_REGS(DDB_REGS) += BKPT_SIZE;
+		}
+	}
+
+	memcpy(frame->fixreg, DDB_REGS->r, 32 * sizeof(u_int32_t));
+	frame->srr0 = DDB_REGS->iar;
+	frame->srr1 = DDB_REGS->msr;
+	frame->lr = DDB_REGS->lr;
+	frame->ctr = DDB_REGS->ctr;
+	frame->cr = DDB_REGS->cr;
+	frame->xer = DDB_REGS->xer;
+#ifdef PPC_IBM4XX
+	frame->dear = DDB_REGS->dear;
+	frame->esr = DDB_REGS->esr;
+	frame->pid = DDB_REGS->pid;
+#endif
+
+	return 1;
+}
+
+#ifdef PPC_IBM4XX
+const struct db_command db_machine_command_table[] = {
+	{ "ctx",	db_ppc4xx_ctx,		0,	0 },
+	{ "pv",		db_ppc4xx_pv,		0,	0 },
+	{ "reset",	db_ppc4xx_reset,	0,	0 },
+	{ "tf",		db_ppc4xx_tf,	0,	0 },
+	{ "tlb",	db_ppc4xx_dumptlb,	0,	0 },
+#ifdef USERACC
+	{ "user",	db_ppc4xx_useracc,	0,	0 },
+#endif
+	{ NULL, }
+};
+
+static void
+db_ppc4xx_ctx(db_expr_t addr, int have_addr, db_expr_t count, char *modif)
+{
+	struct proc *p;
+
+	/* XXX LOCKING XXX */
+	for (p = allproc.lh_first; p != 0; p = p->p_list.le_next) {
+		if (p->p_stat) {
+			db_printf("process %p:", p);
+			db_printf("pid:%d pmap:%p ctx:%d %s\n",
+				p->p_pid, p->p_vmspace->vm_map.pmap,
+				p->p_vmspace->vm_map.pmap->pm_ctx,
+				p->p_comm);
+		}
+	}
+	return;
+}
+
+static void
+db_ppc4xx_pv(db_expr_t addr, int have_addr, db_expr_t count, char *modif)
+{
+	struct pv_entry {
+		struct pv_entry *pv_next;	/* Linked list of mappings */
+		vaddr_t pv_va;			/* virtual address of mapping */
+		struct pmap *pv_pm;
+	};
+	struct pv_entry *pa_to_pv(paddr_t);
+	struct pv_entry *pv;
+
+	if (!have_addr) {
+		db_printf("pv: <pa>\n");
+		return;
+	}
+	pv = pa_to_pv(addr);
+	db_printf("pv at %p\n", pv);
+	while (pv && pv->pv_pm) {
+		db_printf("next %p va %p pmap %p\n", pv->pv_next, 
+			(void *)pv->pv_va, pv->pv_pm);
+		pv = pv->pv_next;
+	}
+}
+
+static void
+db_ppc4xx_reset(db_expr_t addr, int have_addr, db_expr_t count, char *modif)
+{
+	printf("Reseting...\n");
+	ppc4xx_reset();
+}
+
+static void
+db_ppc4xx_tf(db_expr_t addr, int have_addr, db_expr_t count, char *modif)
+{
+	struct trapframe *f;
+
+
+	if (have_addr) {
+		f = (struct trapframe *)addr;
+
+		db_printf("r0-r3:  \t%8.8x %8.8x %8.8x %8.8x\n", 
+			f->fixreg[0], f->fixreg[1],
+			f->fixreg[2], f->fixreg[3]);
+		db_printf("r4-r7:  \t%8.8x %8.8x %8.8x %8.8x\n",
+			f->fixreg[4], f->fixreg[5],
+			f->fixreg[6], f->fixreg[7]);
+		db_printf("r8-r11: \t%8.8x %8.8x %8.8x %8.8x\n",
+			f->fixreg[8], f->fixreg[9],
+			f->fixreg[10], f->fixreg[11]);
+		db_printf("r12-r15:\t%8.8x %8.8x %8.8x %8.8x\n",
+			f->fixreg[12], f->fixreg[13],
+			f->fixreg[14], f->fixreg[15]);
+		db_printf("r16-r19:\t%8.8x %8.8x %8.8x %8.8x\n",
+			f->fixreg[16], f->fixreg[17],
+			f->fixreg[18], f->fixreg[19]);
+		db_printf("r20-r23:\t%8.8x %8.8x %8.8x %8.8x\n",
+			f->fixreg[20], f->fixreg[21],
+			f->fixreg[22], f->fixreg[23]);
+		db_printf("r24-r27:\t%8.8x %8.8x %8.8x %8.8x\n",
+			f->fixreg[24], f->fixreg[25],
+			f->fixreg[26], f->fixreg[27]);
+		db_printf("r28-r31:\t%8.8x %8.8x %8.8x %8.8x\n",
+			f->fixreg[28], f->fixreg[29],
+			f->fixreg[30], f->fixreg[31]);
+
+		db_printf("lr: %8.8x cr: %8.8x xer: %8.8x ctr: %8.8x\n",
+			f->lr, f->cr, f->xer, f->ctr);
+		db_printf("srr0(pc): %8.8x srr1(msr): %8.8x "
+			"dear: %8.8x esr: %8.8x\n",
+			f->srr0, f->srr1, f->dear, f->esr);
+		db_printf("exc: %8.8x pid: %8.8x\n",
+			f->exc, f->pid);
+	}
+	return;
+}
+
+static const char *const tlbsizes[] = {
+	  "1kB",
+	  "4kB",
+	 "16kB",
+	 "64kB",
+	"256kB",
+	  "1MB",
+	  "4MB",
+	 "16MB"
+};
+
+static void
+db_ppc4xx_dumptlb(db_expr_t addr, int have_addr, db_expr_t count, char *modif)
+{
+	int i, zone, tlbsize;
+	u_int zpr, pid, opid, msr;
+	u_long tlblo, tlbhi, tlbmask;
+
+	zpr = mfspr(SPR_ZPR);
+	for (i = 0; i < NTLB; i++) {
+		asm volatile("mfmsr %3;"
+			"mfpid %4;"
+			"li %0,0;"
+			"mtmsr %0;"
+			"sync; isync;"
+			"tlbre %0,%5,1;"
+			"tlbre %1,%5,0;"
+			"mfpid %2;"
+			"mtpid %4;"
+			"mtmsr %3;"
+			"sync; isync"
+			: "=&r" (tlblo), "=&r" (tlbhi), "=r" (pid), 
+			"=&r" (msr), "=&r" (opid) : "r" (i));
+
+		if (strchr(modif, 'v') && !(tlbhi & TLB_VALID))
+			continue;
+
+		tlbsize = (tlbhi & TLB_SIZE_MASK) >> TLB_SIZE_SHFT;
+		/* map tlbsize 0 .. 7 to masks for 1kB .. 16MB */
+		tlbmask = ~(1 << (tlbsize * 2 + 10)) + 1;
+
+		if (have_addr && ((tlbhi & tlbmask) != (addr & tlbmask)))
+			continue;
+
+		zone = (tlblo & TLB_ZSEL_MASK) >> TLB_ZSEL_SHFT;
+		db_printf("tlb%c%2d", tlbhi & TLB_VALID ? ' ' : '*', i);
+		db_printf("  PID %3d EPN 0x%08lx %-5s",
+		    pid,
+		    tlbhi & tlbmask,
+		    tlbsizes[tlbsize]);
+		db_printf("  RPN 0x%08lx  ZONE %2d%c  %s %s %c%c%c%c%c %s",
+		    tlblo & tlbmask,
+		    zone,
+		    "NTTA"[(zpr >> ((15 - zone) * 2)) & 3],
+		    tlblo & TLB_EX ? "EX" : "  ",
+		    tlblo & TLB_WR ? "WR" : "  ",
+		    tlblo & TLB_W ? 'W' : ' ',
+		    tlblo & TLB_I ? 'I' : ' ',
+		    tlblo & TLB_M ? 'M' : ' ',
+		    tlblo & TLB_G ? 'G' : ' ',
+		    tlbhi & TLB_ENDIAN ? 'E' : ' ',
+		    tlbhi & TLB_U0 ? "U0" : "  ");
+		db_printf("\n");
+	}
+}
+
+#ifdef USERACC
+static void
+db_ppc4xx_useracc(db_expr_t addr, int have_addr, db_expr_t count, char *modif)
+{
+	static paddr_t oldaddr = -1;
+	int instr = 0;
+	int data;
+	extern vaddr_t opc_disasm(vaddr_t loc, int);
+
+
+	if (!have_addr) {
+		addr = oldaddr;
+	}
+	if (addr == -1) {
+		db_printf("no address\n");
+		return;
+	}
+	addr &= ~0x3; /* align */
+	{
+		register char c, *cp = modif;
+		while ((c = *cp++) != 0)
+			if (c == 'i')
+				instr = 1;
+	}
+	while (count--) {
+		if (db_print_position() == 0) {
+			/* Always print the address. */
+			db_printf("%8.4lx:\t", addr);
+		}
+		oldaddr=addr;
+		copyin((void *)addr, &data, sizeof(data));
+		if (instr) {
+			opc_disasm(addr, data);
+		} else {
+			db_printf("%4.4x\n", data);
+		}
+		addr += 4;
+		db_end_line();
+	}
+
+}
+#endif
+
+#endif /* PPC_IBM4XX */
+
+void
+db_show_mdpcpu(struct pcpu *pc)
+{
+}
---
+++ sys/powerpc/powerpc/db_trace.c
@@ -0,0 +1,293 @@
+/*	$FreeBSD$ */
+/*	$NetBSD: db_trace.c,v 1.20 2002/05/13 20:30:09 matt Exp $	*/
+/*	$OpenBSD: db_trace.c,v 1.3 1997/03/21 02:10:48 niklas Exp $	*/
+
+/* 
+ * Mach Operating System
+ * Copyright (c) 1992 Carnegie Mellon University
+ * All Rights Reserved.
+ * 
+ * Permission to use, copy, modify and distribute this software and its
+ * documentation is hereby granted, provided that both the copyright
+ * notice and this permission notice appear in all copies of the
+ * software, derivative works or modified versions, and any portions
+ * thereof, and that both notices appear in supporting documentation.
+ * 
+ * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
+ * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
+ * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+ * 
+ * Carnegie Mellon requests users of this software to return to
+ * 
+ *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
+ *  School of Computer Science
+ *  Carnegie Mellon University
+ *  Pittsburgh PA 15213-3890
+ * 
+ * any improvements or extensions that they make and grant Carnegie Mellon 
+ * the rights to redistribute these changes.
+ */
+
+#include <sys/param.h>
+#include <sys/proc.h>
+#include <sys/user.h>
+
+#include <vm/vm.h>
+#include <vm/pmap.h>
+#include <vm/vm_extern.h>
+
+#include <machine/db_machdep.h>
+#include <machine/spr.h>
+#include <machine/trap.h>
+
+#include <ddb/ddb.h>
+#include <ddb/db_access.h>
+#include <ddb/db_sym.h>
+#include <ddb/db_variables.h>
+
+struct db_variable db_regs[] = {
+	{ "r0",  (long *)&ddb_regs.r[0],  FCN_NULL },
+	{ "r1",  (long *)&ddb_regs.r[1],  FCN_NULL },
+	{ "r2",  (long *)&ddb_regs.r[2],  FCN_NULL },
+	{ "r3",  (long *)&ddb_regs.r[3],  FCN_NULL },
+	{ "r4",  (long *)&ddb_regs.r[4],  FCN_NULL },
+	{ "r5",  (long *)&ddb_regs.r[5],  FCN_NULL },
+	{ "r6",  (long *)&ddb_regs.r[6],  FCN_NULL },
+	{ "r7",  (long *)&ddb_regs.r[7],  FCN_NULL },
+	{ "r8",  (long *)&ddb_regs.r[8],  FCN_NULL },
+	{ "r9",  (long *)&ddb_regs.r[9],  FCN_NULL },
+	{ "r10", (long *)&ddb_regs.r[10], FCN_NULL },
+	{ "r11", (long *)&ddb_regs.r[11], FCN_NULL },
+	{ "r12", (long *)&ddb_regs.r[12], FCN_NULL },
+	{ "r13", (long *)&ddb_regs.r[13], FCN_NULL },
+	{ "r14", (long *)&ddb_regs.r[14], FCN_NULL },
+	{ "r15", (long *)&ddb_regs.r[15], FCN_NULL },
+	{ "r16", (long *)&ddb_regs.r[16], FCN_NULL },
+	{ "r17", (long *)&ddb_regs.r[17], FCN_NULL },
+	{ "r18", (long *)&ddb_regs.r[18], FCN_NULL },
+	{ "r19", (long *)&ddb_regs.r[19], FCN_NULL },
+	{ "r20", (long *)&ddb_regs.r[20], FCN_NULL },
+	{ "r21", (long *)&ddb_regs.r[21], FCN_NULL },
+	{ "r22", (long *)&ddb_regs.r[22], FCN_NULL },
+	{ "r23", (long *)&ddb_regs.r[23], FCN_NULL },
+	{ "r24", (long *)&ddb_regs.r[24], FCN_NULL },
+	{ "r25", (long *)&ddb_regs.r[25], FCN_NULL },
+	{ "r26", (long *)&ddb_regs.r[26], FCN_NULL },
+	{ "r27", (long *)&ddb_regs.r[27], FCN_NULL },
+	{ "r28", (long *)&ddb_regs.r[28], FCN_NULL },
+	{ "r29", (long *)&ddb_regs.r[29], FCN_NULL },
+	{ "r30", (long *)&ddb_regs.r[30], FCN_NULL },
+	{ "r31", (long *)&ddb_regs.r[31], FCN_NULL },
+	{ "iar", (long *)&ddb_regs.iar,   FCN_NULL },
+	{ "msr", (long *)&ddb_regs.msr,   FCN_NULL },
+	{ "lr",  (long *)&ddb_regs.lr,    FCN_NULL },
+	{ "ctr", (long *)&ddb_regs.ctr,   FCN_NULL },
+	{ "cr",  (long *)&ddb_regs.cr,    FCN_NULL },
+	{ "xer", (long *)&ddb_regs.xer,   FCN_NULL },
+#ifdef PPC_IBM4XX
+	{ "dear", (long *)&ddb_regs.dear, FCN_NULL },
+	{ "esr", (long *)&ddb_regs.esr,   FCN_NULL },
+	{ "pid", (long *)&ddb_regs.pid,   FCN_NULL },
+#endif
+};
+struct db_variable *db_eregs = db_regs + sizeof (db_regs)/sizeof (db_regs[0]);
+
+extern int trapexit[];
+extern int end[];
+
+/*
+ *	Frame tracing.
+ */
+void
+db_stack_trace_print(addr, have_addr, count, modif, pr)
+	db_expr_t addr;
+	int have_addr;
+	db_expr_t count;
+	char *modif;
+	void (*pr) __P((const char *, ...));
+{
+	db_addr_t frame, lr, caller, *args;
+	db_addr_t fakeframe[2];
+	db_expr_t diff;
+	db_sym_t sym;
+	char *symname;
+	boolean_t kernel_only = TRUE;
+	boolean_t trace_thread = FALSE;
+	boolean_t full = FALSE;
+
+	{
+		register char *cp = modif;
+		register char c;
+
+		while ((c = *cp++) != 0) {
+			if (c == 't')
+				trace_thread = TRUE;
+			if (c == 'u')
+				kernel_only = FALSE;
+			if (c == 'f')
+				full = TRUE;
+		}
+	}
+
+	if (have_addr) {
+#if 0
+		if (trace_thread) {
+			struct proc *p;
+			struct user *u;
+
+			(*pr)("trace: pid %d ", (int)addr);
+			p = pfind(addr);
+			if (p == NULL) {
+				(*pr)("not found\n");
+				return;
+			}	
+			if (!(p->p_flag&P_INMEM)) {
+				(*pr)("swapped out\n");
+				return;
+			}
+			u = p->p_addr;
+			frame = (db_addr_t)u->u_pcb.pcb_sp;
+			(*pr)("at %p\n", frame);
+		} else
+#endif
+			frame = (db_addr_t)addr;
+	} else {
+		frame = (db_addr_t)ddb_regs.r[1];
+	}
+	for (;;) {
+		if (frame < PAGE_SIZE)
+			break;
+#ifdef PPC_MPC6XX
+		if (kernel_only &&
+		    ((frame > (db_addr_t) end &&
+		      frame < VM_MIN_KERNEL_ADDRESS) ||
+		     frame >= VM_MAX_KERNEL_ADDRESS))
+			break;
+#endif 
+		frame = *(db_addr_t *)frame;
+	    next_frame:
+		args = (db_addr_t *)(frame + 8);
+		if (frame < PAGE_SIZE)
+			break;
+#ifdef PPC_MPC6XX
+		if (kernel_only &&
+		    ((frame > (db_addr_t) end &&
+		      frame < VM_MIN_KERNEL_ADDRESS) ||
+		     frame >= VM_MAX_KERNEL_ADDRESS))
+			break;
+#endif
+	        if (count-- == 0)
+			break;
+
+		lr = *(db_addr_t *)(frame + 4) - 4;
+		if ((lr & 3) || (lr < 0x100)) {
+			(*pr)("saved LR(0x%x) is invalid.", lr);
+			break;
+		}
+		if ((caller = (db_addr_t)vtophys(lr)) == 0)
+			caller = lr;
+
+		if (frame != (db_addr_t) fakeframe) {
+			(*pr)("0x%08lx: ", frame);
+		} else {
+			(*pr)("     <?>  : ");
+		}
+		if (caller + 4 == (db_addr_t) &trapexit) {
+			const char *trapstr;
+			struct trapframe *tf = (struct trapframe *) (frame+8);
+			(*pr)("%s ", tf->srr1 & PSL_PR ? "user" : "kernel");
+			switch (tf->exc) {
+			case EXC_DSI:
+#ifdef PPC_MPC6XX
+				(*pr)("DSI %s trap @ %#x by ",
+				    tf->dsisr & DSISR_STORE ? "write" : "read",
+				    tf->dar);
+#endif
+#ifdef PPC_IBM4XX
+				(*pr)("DSI %s trap @ %#x by ",
+				    tf->esr & ESR_DST ? "write" : "read",
+				    tf->dear);
+#endif
+				goto print_trap;
+			case EXC_ISI: trapstr = "ISI"; break;
+			case EXC_PGM: trapstr = "PGM"; break;
+			case EXC_SC: trapstr = "SC"; break;
+			case EXC_EXI: trapstr = "EXI"; break;
+			case EXC_MCHK: trapstr = "MCHK"; break;
+			case EXC_VEC: trapstr = "VEC"; break;
+			case EXC_FPU: trapstr = "FPU"; break;
+			case EXC_FPA: trapstr = "FPA"; break;
+			case EXC_DECR: trapstr = "DECR"; break;
+			case EXC_ALI: trapstr = "ALI"; break;
+			case EXC_BPT: trapstr = "BPT"; break;
+			case EXC_TRC: trapstr = "TRC"; break;
+			case EXC_RUNMODETRC: trapstr = "RUNMODETRC"; break;
+			case EXC_PERF: trapstr = "PERF"; break;
+			case EXC_SMI: trapstr = "SMI"; break;
+			case EXC_RST: trapstr = "RST"; break;
+			default: trapstr = NULL; break;
+			}
+			if (trapstr != NULL) {
+				(*pr)("%s trap by ", trapstr);
+			} else {
+				(*pr)("trap %#x by ", tf->exc);
+			}
+		   print_trap:	
+			lr = (db_addr_t) tf->srr0;
+			if ((caller = (db_addr_t)vtophys(lr)) == 0)
+				caller = lr;
+			diff = 0;
+			symname = NULL;
+			sym = db_search_symbol(caller, DB_STGY_ANY, &diff);
+			db_symbol_values(sym, &symname, 0);
+			if (symname == NULL || !strcmp(symname, "end")) {
+				(*pr)("%p: srr1=%#x\n", caller, tf->srr1);
+			} else {
+				(*pr)("%s+%x: srr1=%#x\n", symname, diff,
+				    tf->srr1);
+			}
+			(*pr)("%-10s  r1=%#x cr=%#x xer=%#x ctr=%#x",
+			    "", tf->fixreg[1], tf->cr, tf->xer, tf->ctr);
+#ifdef PPC_MPC6XX
+			if (tf->exc == EXC_DSI)
+				(*pr)(" dsisr=%#x", tf->dsisr);
+#endif
+#ifdef PPC_IBM4XX
+			if (tf->exc == EXC_DSI)
+				(*pr)(" dear=%#x", tf->dear);
+			(*pr)(" esr=%#x pid=%#x", tf->esr, tf->pid);
+#endif
+			(*pr)("\n");
+			fakeframe[0] = (db_addr_t) tf->fixreg[1];
+			fakeframe[1] = (db_addr_t) tf->lr;
+			frame = (db_addr_t) fakeframe;
+			if (kernel_only && (tf->srr1 & PSL_PR))
+				break;
+			goto next_frame;
+		}
+
+		diff = 0;
+		symname = NULL;
+		sym = db_search_symbol(caller, DB_STGY_ANY, &diff);
+		db_symbol_values(sym, &symname, 0);
+		if (symname == NULL || !strcmp(symname, "end"))
+			(*pr)("at %p", caller);
+		else
+			(*pr)("at %s+%#x", symname, diff);
+		if (full)
+			/* Print all the args stored in that stackframe. */
+			(*pr)("(%lx, %lx, %lx, %lx, %lx, %lx, %lx, %lx)",
+				args[0], args[1], args[2], args[3],
+				args[4], args[5], args[6], args[7]);
+		(*pr)("\n");
+	}
+}
+
+void
+db_stack_trace_cmd(db_expr_t addr, boolean_t have_addr, db_expr_t count,
+    char *modif)
+{
+
+	db_stack_trace_print(addr, have_addr, count, modif, db_printf);
+}
+
