54b60d7e1528658e03c9abd5d1984f7ebd1af70b
Alfred Perlstein <alfred@FreeBSD.org>
2002-07-11 22:18:26 +0000

    Fix indentation broken in previous __P removal.
    Grumbled about by: bde

--- usr.sbin/ac/ac.c
+++ usr.sbin/ac/ac.c
@@ -84,20 +84,20 @@ static struct tty_list *Ttys = NULL;
 static int Debug = 0;
 #endif
 
-int main(int, char **);
-int ac(FILE *);
-struct tty_list	*add_tty(char *);
-int do_tty(char *);
-FILE *file(const char *);
-struct utmp_list *log_in(struct utmp_list *, struct utmp *);
-struct utmp_list *log_out(struct utmp_list *, struct utmp *);
-int on_console(struct utmp_list *);
-void show(const char *, time_t);
-void show_today __P((struct user_list *, struct utmp_list *,
-			    time_t));
-void show_users(struct user_list *);
-struct user_list *update_user(struct user_list *, char *, time_t);
-void usage(void);
+int			main(int, char **);
+int			ac(FILE *);
+struct tty_list		*add_tty(char *);
+int			do_tty(char *);
+FILE			*file(const char *);
+struct utmp_list	*log_in(struct utmp_list *, struct utmp *);
+struct utmp_list	*log_out(struct utmp_list *, struct utmp *);
+int			on_console(struct utmp_list *);
+void			show(const char *, time_t);
+void			show_today(struct user_list *, struct utmp_list *,
+			    time_t);
+void			show_users(struct user_list *);
+struct user_list	*update_user(struct user_list *, char *, time_t);
+void			usage(void);
 
 /*
  * open wtmp or die
