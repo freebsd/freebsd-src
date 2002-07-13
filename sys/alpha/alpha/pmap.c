828e129a104d290776ee93555930a9c9f53b7bd6
alc <alc@FreeBSD.org>
2002-07-13 20:55:21 +0000

    o Complete the locking of page queue accesses by vm_page_unwire().  o Assert that the page queues lock is held in vm_page_unwire().  o Make vm_page_lock_queues() and vm_page_unlock_queues() visible    to kernel loadable modules.

--- sys/alpha/alpha/pmap.c
+++ sys/alpha/alpha/pmap.c
@@ -997,8 +997,10 @@ pmap_dispose_thread(td)
 		vm_page_busy(m);
 		ptek[i] = 0;
 		pmap_invalidate_page(kernel_pmap, ks + i * PAGE_SIZE);
+		vm_page_lock_queues();
 		vm_page_unwire(m, 0);
 		vm_page_free(m);
+		vm_page_unlock_queues();
 	}
 
 	/*
@@ -1036,8 +1038,10 @@ pmap_swapout_thread(td)
 		m = vm_page_lookup(ksobj, i);
 		if (m == NULL)
 			panic("pmap_swapout_thread: kstack already missing?");
+		vm_page_lock_queues();
 		vm_page_dirty(m);
 		vm_page_unwire(m, 0);
+		vm_page_unlock_queues();
 		pmap_kremove(ks + i * PAGE_SIZE);
 	}
 }
