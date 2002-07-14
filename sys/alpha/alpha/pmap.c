ec8a106e8a10401d462de8ab256aea0a8f824f53
alc <alc@FreeBSD.org>
2002-07-14 23:51:55 +0000

    o Lock page queue accesses by vm_page_wire() that aren't    within a critical section.  o Assert that the page queues lock is held in vm_page_wire()    unless an Alpha.

--- sys/alpha/alpha/pmap.c
+++ sys/alpha/alpha/pmap.c
@@ -1070,9 +1070,11 @@ pmap_swapin_thread(td)
 			m = vm_page_lookup(ksobj, i);
 			m->valid = VM_PAGE_BITS_ALL;
 		}
+		vm_page_lock_queues();
 		vm_page_wire(m);
 		vm_page_wakeup(m);
 		vm_page_flag_set(m, PG_MAPPED | PG_WRITEABLE);
+		vm_page_unlock_queues();
 	}
 
 	/*
