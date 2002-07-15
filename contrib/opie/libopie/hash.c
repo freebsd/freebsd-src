0777419e7ece83de219bfd386384b8d76ada69f2
des <des@FreeBSD.org>
2002-07-15 14:48:48 +0000

    MFC: OPIE 2.4
    Approved by:	markm

--- contrib/opie/libopie/hash.c
+++ contrib/opie/libopie/hash.c
@@ -1,14 +1,15 @@
 /* hash.c: The opiehash() library function.
 
 %%% copyright-cmetz-96
-This software is Copyright 1996-1998 by Craig Metz, All Rights Reserved.
-The Inner Net License Version 2 applies to this software.
+This software is Copyright 1996-2001 by Craig Metz, All Rights Reserved.
+The Inner Net License Version 3 applies to this software.
 You should have received a copy of the license with this software. If
 you didn't get a copy, you may request one from <license@inner.net>.
 
         History:
 
-	Updated by cmetz for OPIE 2.31. Added SHA support (which may
+	Modified by cmetz for OPIE 2.4. Use struct opie_otpkey for binary arg. 
+	Modified by cmetz for OPIE 2.31. Added SHA support (which may
               not be correct). Backed out previous optimizations as
               they killed thread-safety.
         Created by cmetz for OPIE 2.3 using the old hash.c as a guide.
@@ -17,31 +18,29 @@ $FreeBSD$
 */
 
 #include "opie_cfg.h"
-#if 0
-#include "sha.h"
-#endif /* 0 */
 #include "opie.h"
 
+#include <sha.h>
 #include <md4.h>
 #include <md5.h>
 
-VOIDRET opiehash FUNCTION((x, algorithm), VOIDPTR x AND unsigned algorithm)
+VOIDRET opiehash FUNCTION((x, algorithm), struct opie_otpkey *x AND
+unsigned algorithm)
 {
   UINT4 *results = (UINT4 *)x;
 
   switch(algorithm) {
-#if 0
     case 3:
       {
       SHA_CTX sha;
-      SHAInit(&sha);
-      SHAUpdate(&sha, (unsigned char *)x, 8);
-      SHAFinal(&sha);
-      results[0] = sha.buffer[0] ^ sha.buffer[2] ^ sha.buffer[4];
-      results[1] = sha.buffer[1] ^ sha.buffer[3];
+      UINT4 digest[5];
+      SHA1_Init(&sha);
+      SHA1_Update(&sha, (unsigned char *)x, 8);
+      SHA1_Final((unsigned char *)digest, &sha);
+      results[0] = digest[0] ^ digest[2] ^ digest[4];
+      results[1] = digest[1] ^ digest[3];
       };
       break;
-#endif /* 0 */
     case 4:
       {
       MD4_CTX mdx;
