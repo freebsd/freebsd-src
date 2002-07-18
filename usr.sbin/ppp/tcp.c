f4359ccbbcba2c5a0fbb89a2465e8abaf93a9600
Brian Somers <brian@FreeBSD.org>
2002-07-18 18:50:05 +0000

    Remove unused calls to inet_addr()

--- usr.sbin/ppp/tcp.c
+++ usr.sbin/ppp/tcp.c
@@ -66,7 +66,6 @@ tcp_OpenConnection(const char *name, char *host, char *port)
   struct servent *sp;
 
   dest.sin_family = AF_INET;
-  dest.sin_addr.s_addr = inet_addr(host);
   dest.sin_addr = GetIpAddr(host);
   if (dest.sin_addr.s_addr == INADDR_NONE) {
     log_Printf(LogWARN, "%s: %s: unknown host\n", name, host);
