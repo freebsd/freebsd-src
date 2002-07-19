4084115eff30f586f43afea52de5c961f41a1e31
jhb <jhb@FreeBSD.org>
2002-07-19 22:14:54 +0000

    Add PCI ID for the ICH4 ATA100 controller.
    Sponsored by:	The Weather Channel

--- sys/dev/ata/ata-dma.c
+++ sys/dev/ata/ata-dma.c
@@ -216,6 +216,7 @@ ata_dmainit(struct ata_device *atadev, int apiomode, int wdmamode, int udmamode)
 #endif
     switch (chiptype) {
 
+    case 0x24cb8086:	/* Intel ICH4 */
     case 0x248a8086:	/* Intel ICH3 mobile */ 
     case 0x248b8086:	/* Intel ICH3 */
     case 0x244a8086:	/* Intel ICH2 mobile */ 
