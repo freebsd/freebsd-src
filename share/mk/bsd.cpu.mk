1decb43aa47d1034843d210fbe22d70482c5fc56
Ruslan Ermilov <ru@FreeBSD.org>
2002-07-19 08:09:32 +0000

    MFC: 1.6 (optimize for i486 better).

--- share/mk/bsd.cpu.mk
+++ share/mk/bsd.cpu.mk
@@ -49,7 +49,7 @@ _CPUCFLAGS = -march=pentium
 .  elif ${CPUTYPE} == "i586"
 _CPUCFLAGS = -march=pentium
 .  elif ${CPUTYPE} == "i486"
-_CPUCFLAGS = -m486
+_CPUCFLAGS = -march=i486
 .  endif
 . elif ${MACHINE_ARCH} == "alpha"
 .  if ${CPUTYPE} == "ev6"
