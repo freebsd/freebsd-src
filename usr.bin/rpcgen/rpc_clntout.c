248be67d5431ff0cc7f846cc3a8603f6ab019d34
charnier <charnier@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-07-21 12:55:04 +0000

    Removal of spaces at EOL. Add __FBSDID. New function xmalloc, xrealloc, xstrdup. There is a crash() function that do cleaning before exiting the program. The new functions are wrappers that make use of crash() in case of allocation failure. warn, exit -> err.
    Reviewed by:	alfred
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@100441 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/rpcgen/rpc_clntout.c
+++ usr.bin/rpcgen/rpc_clntout.c
@@ -5,37 +5,38 @@
  * may copy or modify Sun RPC without charge, but are not authorized
  * to license or distribute it to anyone else except as part of a product or
  * program developed by the user.
- * 
+ *
  * SUN RPC IS PROVIDED AS IS WITH NO WARRANTIES OF ANY KIND INCLUDING THE
  * WARRANTIES OF DESIGN, MERCHANTIBILITY AND FITNESS FOR A PARTICULAR
  * PURPOSE, OR ARISING FROM A COURSE OF DEALING, USAGE OR TRADE PRACTICE.
- * 
+ *
  * Sun RPC is provided with no support and without any obligation on the
  * part of Sun Microsystems, Inc. to assist in its use, correction,
  * modification or enhancement.
- * 
+ *
  * SUN MICROSYSTEMS, INC. SHALL HAVE NO LIABILITY WITH RESPECT TO THE
  * INFRINGEMENT OF COPYRIGHTS, TRADE SECRETS OR ANY PATENTS BY SUN RPC
  * OR ANY PART THEREOF.
- * 
+ *
  * In no event will Sun Microsystems, Inc. be liable for any lost revenue
  * or profits or other special, indirect and consequential damages, even if
  * Sun has been advised of the possibility of such damages.
- * 
+ *
  * Sun Microsystems, Inc.
  * 2550 Garcia Avenue
  * Mountain View, California  94043
- *
- * $FreeBSD$
  */
 
 #ident	"@(#)rpc_clntout.c	1.15	94/04/25 SMI"
 
-#ifndef lint
 #if 0
+#ifndef lint
 static char sccsid[] = "@(#)rpc_clntout.c 1.11 89/02/22 (C) 1987 SMI";
 #endif
 #endif
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 /*
  * rpc_clntout.c, Client-stub outputter for the RPC protocol compiler
@@ -177,7 +178,7 @@ void printarglist(proc, result, addargname, addargtype)
 			for (l = proc->args.decls;  l != NULL; l = l->next)
 				f_print(fout, "%s, ", l->decl.name);
 			if (mtflag)
-				f_print(fout, "%s, ", result); 
+				f_print(fout, "%s, ", result);
 
 			f_print(fout, "%s)\n", addargname);
 			for (l = proc->args.decls; l != NULL; l = l->next) {
@@ -262,7 +263,7 @@ printbody(proc)
 			f_print(fout, "\t if ");
 
 		f_print(fout,
-			"(clnt_call(clnt, %s,\n\t\t(xdrproc_t) xdr_void, ", 
+			"(clnt_call(clnt, %s,\n\t\t(xdrproc_t) xdr_void, ",
 			proc->proc_name);
 		f_print(fout,
 			"(caddr_t) NULL,\n\t\t(xdrproc_t) xdr_%s, (caddr_t) %s%s,",
--- usr.bin/rpcgen/rpc_hout.c
+++ usr.bin/rpcgen/rpc_hout.c
@@ -1,4 +1,3 @@
-/* $FreeBSD$ */
 /*
  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for
  * unrestricted use provided that this legend is included on all tape
@@ -6,23 +5,23 @@
  * may copy or modify Sun RPC without charge, but are not authorized
  * to license or distribute it to anyone else except as part of a product or
  * program developed by the user.
- * 
+ *
  * SUN RPC IS PROVIDED AS IS WITH NO WARRANTIES OF ANY KIND INCLUDING THE
  * WARRANTIES OF DESIGN, MERCHANTIBILITY AND FITNESS FOR A PARTICULAR
  * PURPOSE, OR ARISING FROM A COURSE OF DEALING, USAGE OR TRADE PRACTICE.
- * 
+ *
  * Sun RPC is provided with no support and without any obligation on the
  * part of Sun Microsystems, Inc. to assist in its use, correction,
  * modification or enhancement.
- * 
+ *
  * SUN MICROSYSTEMS, INC. SHALL HAVE NO LIABILITY WITH RESPECT TO THE
  * INFRINGEMENT OF COPYRIGHTS, TRADE SECRETS OR ANY PATENTS BY SUN RPC
  * OR ANY PART THEREOF.
- * 
+ *
  * In no event will Sun Microsystems, Inc. be liable for any lost revenue
  * or profits or other special, indirect and consequential damages, even if
  * Sun has been advised of the possibility of such damages.
- * 
+ *
  * Sun Microsystems, Inc.
  * 2550 Garcia Avenue
  * Mountain View, California  94043
@@ -30,11 +29,14 @@
 
 #ident	"@(#)rpc_hout.c	1.16	94/04/25 SMI"
 
-#ifndef lint
 #if 0
+#ifndef lint
 static char sccsid[] = "@(#)rpc_hout.c 1.12 89/02/22 (C) 1987 SMI";
 #endif
 #endif
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 /*
  * rpc_hout.c, Header file outputter for the RPC protocol compiler
@@ -126,7 +128,7 @@ int pointerp;
 {
 	xdrfunc * xdrptr;
 
-	xdrptr = (xdrfunc *) malloc(sizeof (struct xdrfunc));
+	xdrptr = XALLOC(struct xdrfunc);
 
 	xdrptr->name = name;
 	xdrptr->pointerp = pointerp;
@@ -151,9 +153,9 @@ int i;
 {
 	if (i == 2) {
 		f_print(fout, "extern bool_t xdr_%s();\n", name);
-		return; 
+		return;
 	}
-	else  
+	else
 		f_print(fout, "extern  bool_t xdr_%s(XDR *, %s%s);\n", name,
 			name, pointerp ? "*" : "");
 
@@ -337,7 +339,7 @@ pprogramdef(def)
 		puldefine(vers->vers_name, vers->vers_num);
 
 		/*
-		 * Print out 2 definitions, one for ANSI-C, another for 
+		 * Print out 2 definitions, one for ANSI-C, another for
 		 * old K & R C
 		 */
 
