945e03eaf05b7158ce995a60af5e0c4db4929b1a
dd <dd@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-07-28 06:45:30 +0000

    Implement this (quoted from the updated man page): If the first token of a rule specification is a single dash (``-''), rules are read from the standard input and the rest of the specification is ignored.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@100799 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/devfs/devfs.c
+++ sbin/devfs/devfs.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2002 Dima Dorfman.
+ * Copyright (c) 2001, 2002 Dima Dorfman.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -32,7 +32,9 @@
 __FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
+#include <sys/queue.h>
 
+#include <assert.h>
 #include <err.h>
 #include <fcntl.h>
 #include <paths.h>
@@ -130,6 +132,93 @@ eatonum(const char *s)
 		errx(1, "error converting to number: %s", s); /* XXX clarify */
 	return (num);
 }	
+
+/*
+ * Read a line from a /FILE/.  If the return value isn't 0, it is the
+ * length of the line, a pointer to which exists in /line/.  It is the
+ * caller's responsibility to free(3) it.  If the return value is 0,
+ * there was an error or we reached EOF, and /line/ is undefined (so,
+ * obviously, the caller shouldn't try to free(3) it).
+ */
+size_t
+efgetln(FILE *fp, char **line)
+{
+	size_t rv;
+	char *cp;
+
+	cp = fgetln(fp, &rv);
+	if (cp == NULL) {
+		*line = NULL;
+		return (rv);
+	}
+	if (cp[rv - 1] == '\n') {
+		cp[rv - 1] = '\0';
+		*line = strdup(cp);
+		if (*line == NULL)
+			errx(1, "cannot allocate memory");
+		--rv;
+	} else {
+		*line = malloc(rv + 1);
+		if (*line == NULL)
+			errx(1, "cannot allocate memory");
+		memcpy(*line, cp, rv);
+		*line[rv] = '\0';
+	}
+	assert(rv == strlen(*line));
+	return (rv);
+}
+
+struct ptrstq {
+	STAILQ_ENTRY(ptrstq)	 tq;
+	void			*ptr;
+};
+
+/*
+ * Create an argument vector from /line/.  The caller must free(3)
+ * /avp/, and /avp[0]/ when the argument vector is no longer
+ * needed unless /acp/ is 0, in which case /avp/ is undefined.
+ * /avp/ is NULL-terminated, so it is actually one longer than /acp/.
+ */
+void
+tokenize(const char *line, int *acp, char ***avp)
+{
+	static const char *delims = " \t\n";
+	struct ptrstq *pt;
+	STAILQ_HEAD(, ptrstq) plist;
+	char **ap, *cp, *wline, *xcp;
+
+	line += strspn(line, delims);
+	wline = strdup(line);
+	if (wline == NULL)
+		errx(1, "cannot allocate memory");
+
+	STAILQ_INIT(&plist);
+	for (xcp = wline, *acp = 0;
+	     (cp = strsep(&xcp, delims)) != NULL;)
+		if (*cp != '\0') {
+			pt = calloc(1, sizeof(*pt));
+			if (pt == NULL)
+				errx(1, "cannot allocate memory");
+			pt->ptr = cp;
+			STAILQ_INSERT_TAIL(&plist, pt, tq);
+			++*acp;
+		}
+	if (*acp == 0)
+		return;
+	assert(STAILQ_FIRST(&plist)->ptr == wline);
+	*avp = malloc(sizeof(**avp) * (*acp + 1));
+	if (*avp == NULL)
+		errx(1, "cannot allocate memory");
+	for (ap = *avp; !STAILQ_EMPTY(&plist);) {
+		pt = STAILQ_FIRST(&plist);
+		*ap = pt->ptr;
+		++ap;
+		assert(ap <= *avp + (*acp));
+		STAILQ_REMOVE_HEAD(&plist, tq);
+		free(pt);
+	}
+	*ap = NULL;
+}
 
 void
 usage(void)
