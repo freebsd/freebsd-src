efd218930aef7b81355e5b538906f3dd987afc33
Ruslan Ermilov <ru@FreeBSD.org>
2002-07-29 07:30:24 +0000

    MFC: 1.166: Don't shrink socket buffers in tcp_mss(), application might have already configured them with setsockopt(SO_*BUF), for RFC1323's scaled windows.
    PR:		kern/11966

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -2620,7 +2620,8 @@ tcp_mss(tp, offer)
 		bufsize = roundup(bufsize, mss);
 		if (bufsize > sb_max)
 			bufsize = sb_max;
-		(void)sbreserve(&so->so_snd, bufsize, so, NULL);
+		if (bufsize > so->so_snd.sb_hiwat)
+			(void)sbreserve(&so->so_snd, bufsize, so, NULL);
 	}
 	tp->t_maxseg = mss;
 
@@ -2632,7 +2633,8 @@ tcp_mss(tp, offer)
 		bufsize = roundup(bufsize, mss);
 		if (bufsize > sb_max)
 			bufsize = sb_max;
-		(void)sbreserve(&so->so_rcv, bufsize, so, NULL);
+		if (bufsize > so->so_rcv.sb_hiwat)
+			(void)sbreserve(&so->so_rcv, bufsize, so, NULL);
 	}
 
 	/*
