41eccb4d391e7dc48068be70806325e12af377a8
rwatson <rwatson@FreeBSD.org>
2002-07-31 18:07:45 +0000

    Introduce support for Mandatory Access Control and extensible kernel access control.
    Provide implementations of some sample operating system security
    policy extensions.  These are not yet hooked up to the build as
    other infrastructure is still being committed.  Most of these
    work fairly well and are in daily use in our development and (limited)
    production environments.  Some are not yet in their final form,
    and a number of the labeled policies waste a lot of kernel memory
    and will be fixed over the next month or so to be more conservative.
    They do give good examples of the flexibility of the MAC framework
    for implementing a variety of security policies.
    
    mac_biba:	Implementation of fixed-label Biba integrity policy,
    		similar to those found in a number of commercial
    		trusted operating systems.  All subjects and objects
    		are assigned integrity levels, and information flow
    		is controlled based on a read-up, write-down
    		policy.  Currently, purely hierarchal.
    
    mac_bsdextended:	Implementation of a "file system firewall",
    		which allows the administrator to specify a series
    		of rules limiting access by users and groups to
    		objects owned by other users and groups.  This
    		policy is unlabeled, relying on existing system
    		security labeling (file permissions/ownership,
    		process credentials).
    
    mac_ifoff:	Secure interface silencing.  Special-purpose module
    		to limit inappropriate out-going network traffic
    		for silent monitoring scenarios.  Prevents the
    		various network stacks from generating any output
    		despite an interface being live for reception.
    
    mac_mls:	Implementation of fixed-label Multi-Level Security
    		confidentiality policy, similar to those found in
    		a number of commercial trusted operating systems.
    		All subjects and objects are assigned confidentiality
    		levels, and information flow is controlled based on
    		a write-up, read-down policy.  Currently, purely
    		hiearchal, although non-hierarchal support is in the
    		works.
    
    mac_none:	Policy module implementing all MAC policy entry
    		points with empty stubs.  A good place to start if
    		you want all the prototypes types in for you, and
    		don't mind a bit of pruning.  Can be loaded, but
    		has no access control impact.  Useful also for
    		performance measurements.
    
    mac_seeotheruids:	Policy module implementing a security service
    		similar to security.bsd.seeotheruids, only a slightly
    		more detailed policy involving exceptions for members
    		of specific groups, etc.  This policy is unlabeled,
    		relying on existing system security labeling
    		(process credentials).
    
    mac_test:	Policy module implementing basic sanity tests for
    		label handling.  Attempts to ensure that labels are
    		not freed multiple times, etc, etc.
    
    Obtained from:	TrustedBSD Project
    Sponsored by:	DARPA, NAI Labs

---
+++ sys/security/mac_bsdextended/mac_bsdextended.h
@@ -0,0 +1,60 @@
+/*-
+ * Copyright (c) 1999, 2000, 2001, 2002 Robert N. M. Watson
+ * Copyright (c) 2001, 2002 Networks Associates Technology, Inc.
+ * All rights reserved.
+ *
+ * This software was developed by Robert Watson for the TrustedBSD Project.
+ *
+ * This software was developed for the FreeBSD Project in part by NAI Labs,
+ * the Security Research Division of Network Associates, Inc. under
+ * DARPA/SPAWAR contract N66001-01-C-8035 ("CBOSS"), as part of the DARPA
+ * CHATS research program.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The names of the authors may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef _SYS_SECURITY_MAC_BSDEXTENDED_H
+#define	_SYS_SECURITY_MAC_BSDEXTENDED_H
+
+#define	MBI_UID_DEFINED	0x00000001	/* uid field should be used */
+#define	MBI_GID_DEFINED	0x00000002	/* gid field should be used */
+#define	MBI_NEGATED	0x00000004	/* negate uid/gid matches */
+#define	MBI_BITS	(MBI_UID_DEFINED | MBI_GID_DEFINED | MBI_NEGATED)
+
+struct mac_bsdextended_identity {
+	int	mbi_flags;
+	uid_t	mbi_uid;
+	gid_t	mbi_gid;
+};
+
+struct mac_bsdextended_rule {
+	struct mac_bsdextended_identity	mbr_subject;
+	struct mac_bsdextended_identity	mbr_object;
+	mode_t				mbr_mode;	/* maximum access */
+};
+
+#endif /* _SYS_SECURITY_MAC_BSDEXTENDED_H */
