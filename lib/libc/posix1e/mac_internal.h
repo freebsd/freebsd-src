d97fcfce273eb3bc3984441c9e4bcbd5231fb1f5
Robert Watson <rwatson@FreeBSD.org>
2002-08-02 21:14:42 +0000

    Introduce support for Mandatory Access Control and extensible kernel access control.
    Extensions to libc to provide basic MAC label manipulation facilities
    for userland.  These interface will be replaced in the next month
    or two with more flexible interfaces, but provide sufficient support
    to allow use of the Biba and MLS policies for user applications.
    
    libc_r wrappers to follow.
    
    Obtained from:	TrustedBSD Project
    Sponsored by:	DARPA, NAI Labs

---
+++ lib/libc/posix1e/mac_internal.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 1999, 2000, 2001, 2002 Robert N. M. Watson
+ * Copyright (c) 2002 Networks Associates Technology, Inc.
+ * All rights reserved.
+ *
+ * This software was developed by Robert Watson for the TrustedBSD Project.
+ *
+ * This software was developed for the FreeBSD Project in part by NAI Labs,
+ * the Security Research Division of Network Associates, Inc. under
+ * DARPA/SPAWAR contract N66001-01-C-8035 ("CBOSS"), as part of the DARPA
+ * CHATS research program.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The names of the authors may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+/*
+ * Eventually, these per-policy interfaces will be handled via a
+ * standard userland MAC module interface.
+ */
+
+int	 mac_biba_label_from_string __P((char *string, struct mac *label));
+char	*mac_biba_string_from_label __P((struct mac *label));
+int	 mac_mls_label_from_string __P((char *string, struct mac *label));
+char	*mac_mls_string_from_label __P((struct mac *label));
+int	 mac_te_label_from_string __P((char *string, struct mac *label));
+char	*mac_te_string_from_label __P((struct mac *label));
