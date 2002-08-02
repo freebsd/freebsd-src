27073b2e9c4d6db3924769a731ffb24fde13cdac
rwatson <rwatson@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-08-02 07:07:35 +0000

    Introduce support for Mandatory Access Control and extensible kernel access control.
    Provide a library to manage user file system firewall-like rules
    supported by the mac_bsdextended.ko security model.  The kernel
    module exports the current rule set using sysctl, and this
    library provides a front end that includes support for retrieving
    and setting rules, as well as printing and parsing them.
    
    Note: as with other userland components, this is a WIP.  However,
    when used in combination with the soon-to-be-committed ugidfw,
    it can actually be quite useful in multi-user environments to
    allow the administrator to limit inter-user file operations without
    resorting to heavier weight labeled security policies.
    
    Obtained form:	TrustedBSD Project
    Sponsored by:	DARPA, NAI Labs
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@101206 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ lib/libugidfw/ugidfw.h
@@ -0,0 +1,62 @@
+/*-
+ * Copyright (c) 2002 Networks Associates Technology, Inc.
+ * All rights reserved.
+ *
+ * This software was developed for the FreeBSD Project by NAI Labs, the
+ * Security Research Division of Network Associates, Inc. under
+ * DARPA/SPAWAR contract N66001-01-C-8035 ("CBOSS"), as part of the DARPA
+ * CHATS research program.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The names of the authors may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef _UGIDFW_H
+#define	_UGIDFW_H
+
+__BEGIN_DECLS
+int	bsde_rule_to_string(struct mac_bsdextended_rule *rule, char *buf,
+	    size_t buflen);
+int	bsde_parse_identity(int argc, char *argv[],
+	    struct mac_bsdextended_identity *identity, size_t buflen,
+	    char *errstr);
+int	bsde_parse_mode(int argc, char *argv[], mode_t *mode, size_t buflen,
+	    char *errstr);
+int	bsde_parse_rule(int argc, char *argv[],
+	    struct mac_bsdextended_rule *rule, size_t buflen, char *errstr);
+int	bsde_parse_rule_string(const char *string,
+	    struct mac_bsdextended_rule *rule, size_t buflen, char *errstr);
+int	bsde_get_mib(const char *string, int *name, int *namelen);
+int	bsde_get_rule_count(size_t buflen, char *errstr);
+int	bsde_get_rule_slots(size_t buflen, char *errstr);
+int	bsde_get_rule(int rulenum, struct mac_bsdextended_rule *rule,
+	    size_t errlen, char *errstr);
+int	bsde_delete_rule(int rulenum, size_t buflen, char *errstr);
+int	bsde_set_rule(int rulenum, struct mac_bsdextended_rule *rule,
+	    size_t buflen, char *errstr);
+__END_DECLS
+
+#endif
