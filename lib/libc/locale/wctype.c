38509edfd659624ec85f1fe2a67c7faead8e2bf5
tjr <tjr@FreeBSD.org>
2002-08-03 04:18:40 +0000

    Add ISO C90 Amd. 1 wctype(3) and iswctype(3) functions.

--- lib/libc/locale/wctype.c
+++ lib/libc/locale/wctype.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c)1999 Citrus Project,
+ * Copyright (c) 2002 Tim J. Robbins.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -22,56 +22,52 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- *	citrus Id: wctype.h,v 1.4 2000/12/21 01:50:21 itojun Exp
- *	$NetBSD: wctype.h,v 1.3 2000/12/22 14:16:16 itojun Exp $
- * $FreeBSD$
  */
 
-#ifndef _WCTYPE_H_
-#define	_WCTYPE_H_
-
 #include <sys/cdefs.h>
-#include <machine/ansi.h>
+__FBSDID("$FreeBSD$");
 
 #include <ctype.h>
+#include <string.h>
+#include <wctype.h>
 
-#ifdef	_BSD_WINT_T_
-typedef	_BSD_WINT_T_    wint_t;
-#undef	_BSD_WINT_T_
-#endif
+#undef iswctype
+int
+iswctype(wint_t wc, wctype_t charclass)
+{
 
-#ifndef _WCTYPE_T
-typedef long	wctype_t;
-#define _WCTYPE_T
-#endif
+	return (__istype(wc, charclass));
+}
 
-#ifndef WEOF
-#define	WEOF	((wint_t)-1)
-#endif
+wctype_t
+wctype(const char *property)
+{
+	struct {
+		const char	*name;
+		wctype_t	 mask;
+	} props[] = {
+		{ "alnum",	_CTYPE_A|_CTYPE_D },
+		{ "alpha",	_CTYPE_A },
+		{ "blank",	_CTYPE_B },
+		{ "cntrl",	_CTYPE_C },
+		{ "digit",	_CTYPE_D },
+		{ "graph",	_CTYPE_G },
+		{ "lower",	_CTYPE_L },
+		{ "print",	_CTYPE_R },
+		{ "punct",	_CTYPE_P },
+		{ "space",	_CTYPE_S },
+		{ "upper",	_CTYPE_U },
+		{ "xdigit",	_CTYPE_X },
+		{ "ideogram",	_CTYPE_I },	/* BSD extension */
+		{ "special",	_CTYPE_T },	/* BSD extension */
+		{ "phonogram",	_CTYPE_Q },	/* BSD extension */
+		{ NULL,		0L },		/* Default */
+	};
+	int i;
 
-__BEGIN_DECLS
-int	iswctype(wint_t, wctype_t);
-wctype_t wctype(const char *);
-#if 0
-/* XXX: not implemented */
-int	iswalnum(wint_t);
-int	iswalpha(wint_t);
-int	iswblank(wint_t);
-int	iswcntrl(wint_t);
-int	iswdigit(wint_t);
-int	iswgraph(wint_t);
-int	iswlower(wint_t);
-int	iswprint(wint_t);
-int	iswpunct(wint_t);
-int	iswspace(wint_t);
-int	iswupper(wint_t);
-int	iswxdigit(wint_t);
-wint_t	towlower(wint_t);
-wint_t	towupper(wint_t);
-#endif
-__END_DECLS
+	i = 0;
+	while (props[i].name != NULL && strcmp(props[i].name, property) != 0)
+		i++;
 
-#define iswctype(wc, charclass) __istype((wc), (charclass))
-
-#endif		/* _WCTYPE_H_ */
+	return (props[i].mask);
+}
