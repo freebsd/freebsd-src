5020fa3b22a26f1925f59f38b10cdd53d56e8b47
fjoe <fjoe@FreeBSD.org>
2002-08-05 17:32:16 +0000

    back out one change that crept in (will be committed later)

--- sys/dev/sbni/if_sbni_isa.c
+++ sys/dev/sbni/if_sbni_isa.c
@@ -115,7 +115,7 @@ sbni_attach_isa(device_t dev)
 	sc = device_get_softc(dev);
 
 	printf("sbni%d: <Granch SBNI12/ISA adapter> port 0x%x",
-	       next_sbni_unit, rman_get_start(sc->io_res));
+	       next_sbni_unit, sc->base_addr);
 	sc->irq_res = bus_alloc_resource(
 	    dev, SYS_RES_IRQ, &sc->irq_rid, 0ul, ~0ul, 1, RF_ACTIVE);
 
