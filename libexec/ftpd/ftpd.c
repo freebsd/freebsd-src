82e47923d6357389155814e167d38c6a5490b4f6
Yaroslav Tykhiy <ytykhiy@gmail.com>
2002-08-09 15:36:26 +0000

    MFC: Sort options, document undocumented ones.

--- libexec/ftpd/ftpd.c
+++ libexec/ftpd/ftpd.c
@@ -302,14 +302,32 @@ main(argc, argv, envp)
 #endif /* OLD_SETPROCTITLE */
 
 
-	while ((ch = getopt(argc, argv, "AdlDESURrt:T:u:vOoa:p:46")) != -1) {
+	while ((ch = getopt(argc, argv, "46a:AdDEloOp:rRSt:T:u:Uv")) != -1) {
 		switch (ch) {
-		case 'D':
-			daemon_mode++;
+		case '4':
+			enable_v4 = 1;
+			if (family == AF_UNSPEC)
+				family = AF_INET;
+			break;
+
+		case '6':
+			family = AF_INET6;
+			break;
+
+		case 'a':
+			bindname = optarg;
+			break;
+
+		case 'A':
+			anon_only = 1;
 			break;
 
 		case 'd':
 			ftpdebug++;
+			break;
+
+		case 'D':
+			daemon_mode++;
 			break;
 
 		case 'E':
@@ -318,6 +336,18 @@ main(argc, argv, envp)
 
 		case 'l':
 			logging++;	/* > 1 == extra logging */
+			break;
+
+		case 'o':
+			noretr = 1;
+			break;
+
+		case 'O':
+			noguestretr = 1;
+			break;
+
+		case 'p':
+			pid_file = optarg;
 			break;
 
 		case 'r':
@@ -332,28 +362,16 @@ main(argc, argv, envp)
 			stats++;
 			break;
 
-		case 'T':
-			maxtimeout = atoi(optarg);
-			if (timeout > maxtimeout)
-				timeout = maxtimeout;
-			break;
-
 		case 't':
 			timeout = atoi(optarg);
 			if (maxtimeout < timeout)
 				maxtimeout = timeout;
 			break;
 
-		case 'U':
-			restricted_data_ports = 0;
-			break;
-
-		case 'a':
-			bindname = optarg;
-			break;
-
-		case 'p':
-			pid_file = optarg;
+		case 'T':
+			maxtimeout = atoi(optarg);
+			if (timeout > maxtimeout)
+				timeout = maxtimeout;
 			break;
 
 		case 'u':
@@ -367,30 +385,12 @@ main(argc, argv, envp)
 				defumask = val;
 			break;
 		    }
-		case 'A':
-			anon_only = 1;
+		case 'U':
+			restricted_data_ports = 0;
 			break;
 
 		case 'v':
-			ftpdebug = 1;
-			break;
-
-		case '4':
-			enable_v4 = 1;
-			if (family == AF_UNSPEC)
-				family = AF_INET;
-			break;
-
-		case '6':
-			family = AF_INET6;
-			break;
-
-		case 'O':
-			noguestretr = 1;
-			break;
-
-		case 'o':
-			noretr = 1;
+			ftpdebug++;
 			break;
 
 		default:
