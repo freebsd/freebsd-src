3976147b356b54b49797fefc30de5f043863d5e6
Ruslan Ermilov <ru@FreeBSD.org>
2002-08-09 14:49:23 +0000

    MFC: in_rmx.c,v 1.39, ip_input.c,v 1.165, and ip_var.h,v 1.54:
    Invalidate cached forwarding route (ipforward_rt) whenever a new
    route is added to the routing table, otherwise we may end up using
    the wrong route when forwarding.
    
    PR:		kern/10778
    Spotted by:	Sergey Starosek <star@sunbay.com>,
    		Andrew Rukavishnikov <rav@sunbay.com>

--- sys/netinet/ip_var.h
+++ sys/netinet/ip_var.h
@@ -147,6 +147,7 @@ extern u_short	ip_id;				/* ip packet ctr, for ids */
 #endif
 extern int	ip_defttl;			/* default IP ttl */
 extern int	ipforwarding;			/* ip forwarding */
+extern struct route ipforward_rt;		/* ip forwarding cached route */
 extern u_char	ip_protox[];
 extern struct socket *ip_rsvpd;	/* reservation protocol daemon */
 extern struct socket *ip_mrouter; /* multicast routing daemon */
