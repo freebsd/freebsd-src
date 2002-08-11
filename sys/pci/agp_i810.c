89c4844b7889f5cc8ce85136627b27d424784504
alc <alc@FreeBSD.org>
2002-08-11 19:58:12 +0000

    MFC  o If the page returned by vm_page_grab(VM_ALLOC_ZERO) isn't prezeroed,    zero it.

--- sys/pci/agp_i810.c
+++ sys/pci/agp_i810.c
@@ -369,6 +369,8 @@ agp_i810_alloc_memory(device_t dev, int type, vm_size_t size)
 		 */
 		vm_page_t m;
 		m = vm_page_grab(mem->am_obj, 0, VM_ALLOC_ZERO|VM_ALLOC_RETRY);
+		if ((m->flags & PG_ZERO) == 0)
+			vm_page_zero_fill(m);
 		vm_page_wire(m);
 		mem->am_physical = VM_PAGE_TO_PHYS(m);
 		vm_page_wakeup(m);
