3248d0a51743c450cf9c656c793102b98b5aa44e
Robert Drehmel <robert@FreeBSD.org>
2002-08-14 23:45:42 +0000

    - Add the 'restrict' qualifier to the function definitions and    public prototypes of setbuf(3) and setvbuf(3) using the    '__restrict' macro from <sys/cdefs.h> to be compliant with    IEEE Std 1003.1-2001.  - Replace the K&R with ANSI-C function definitions.  - Bring the manual page up-to-date.

--- lib/libc/stdio/setvbuf.c
+++ lib/libc/stdio/setvbuf.c
@@ -52,11 +52,7 @@ __FBSDID("$FreeBSD$");
  * a buffer.
  */
 int
-setvbuf(fp, buf, mode, size)
-	FILE *fp;
-	char *buf;
-	int mode;
-	size_t size;
+setvbuf(FILE *__restrict fp, char *__restrict buf, int mode, size_t size)
 {
 	int ret, flags;
 	size_t iosize;
