c2ce8018e4d117bd60c16e5e53984f38b3a3c36d
Mitsuru IWASAKI <iwasaki@FreeBSD.org>
2002-08-16 04:45:39 +0000

    MFC: Add suspend/resume code. 	if_dc.c		1.72 -> 1.73 	if_dcreg.h	1.24 -> 1.25 I've forgotten this MFC for a long time.

--- sys/pci/if_dcreg.h
+++ sys/pci/if_dcreg.h
@@ -703,6 +703,13 @@ struct dc_softc {
 #ifdef	DEVICE_POLLING
 	int			rxcycles;	/* ... when polling */
 #endif
+	int			suspended;	/* 0 = normal  1 = suspended */
+
+	u_int32_t		saved_maps[5];	/* pci data */
+	u_int32_t		saved_biosaddr;
+	u_int8_t		saved_intline;
+	u_int8_t		saved_cachelnsz;
+	u_int8_t		saved_lattimer;
 };
 
 #define DC_TX_POLL		0x00000001
