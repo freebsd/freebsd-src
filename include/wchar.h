df5a88a26120f08ad7261a487a8f41a5d268a105
ache <ache@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-08-19 21:00:13 +0000

    Sort unsorted prototypes
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@102142 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- include/wchar.h
+++ include/wchar.h
@@ -146,8 +146,8 @@ size_t	wcsrtombs(char * __restrict, const wchar_t ** __restrict, size_t,
 size_t	wcsspn(const wchar_t *, const wchar_t *);
 wchar_t	*wcsstr(const wchar_t *, const wchar_t *);
 int	wcswidth(const wchar_t *, size_t);
-int	wcwidth(wchar_t);
 int	wctob(wint_t);
+int	wcwidth(wchar_t);
 wchar_t	*wmemchr(const wchar_t *, wchar_t, size_t);
 int	wmemcmp(const wchar_t *, const wchar_t *, size_t);
 wchar_t	*wmemcpy(wchar_t * __restrict, const wchar_t * __restrict, size_t);
