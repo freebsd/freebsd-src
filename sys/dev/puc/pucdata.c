76353f68231be8872ef8624ac17e978ca11f517d
John Hay <jhay@FreeBSD.org>
2002-08-22 10:37:55 +0000

    Add an entry for VScom PCI-200L 2 port serial card.
    PR:		41142
    Submitted by:	Shinsuke Matsui <smatsui@internet-inc.co.jp>
    Obtained from:	OpenBSD

--- sys/dev/puc/pucdata.c
+++ sys/dev/puc/pucdata.c
@@ -623,6 +623,16 @@ const struct puc_device_description puc_devices[] = {
 	    },
 	},
 
+	/* VScom PCI-200L: 2S */
+	{   "VScom PCI-200L",
+	    {	0x14d2, 0x8020,	0, 0	},
+	    {	0xffff, 0xffff,	0, 0	},
+	    {
+		{ PUC_PORT_TYPE_COM, 0x14, 0x00, COM_FREQ * 8},
+		{ PUC_PORT_TYPE_COM, 0x18, 0x00, COM_FREQ * 8},
+	    },
+	},
+
 	/* VScom PCI-400: 4S */
 	{   "VScom PCI-400",
 	    {	0x10b5,	0x1077,	0x10b5,	0x1077	},
