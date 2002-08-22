6f456f2f05d82e333bab8412df7ffc936bf87473
jhay <jhay@FreeBSD.org>
2002-08-22 10:40:00 +0000

    Add an entry for Avlab's low profile 4 serial port card.
    PR:		41146
    Submitted by:	Shinsuke Matsui <smatsui@internet-inc.co.jp>
    Obtained from:	NetBSD

--- sys/dev/puc/pucdata.c
+++ sys/dev/puc/pucdata.c
@@ -886,6 +886,18 @@ const struct puc_device_description puc_devices[] = {
 	    },
 	},
 
+	/* Avlab Technology, Inc. Low Profile PCI 4 Serial: 4S */
+	{   "Avlab Low Profile PCI 4 Serial",
+	    {	0x14db,	0x2150,	0,	0	},
+	    {	0xffff,	0xffff,	0,	0	},
+	    {
+		{ PUC_PORT_TYPE_COM, 0x10, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x14, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x18, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x1c, 0x00, COM_FREQ },
+	    },
+	},
+
         /* Decision Computer Inc, serial ports */
         {   "Decision Computer Inc, PCCOM 4-port serial",
             {   0x6666, 0x0001, 0,      0       },
