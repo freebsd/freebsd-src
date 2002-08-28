3e1f49aeccc96a4a6ab98631e998eac1b26dbd76
alfred <alfred@FreeBSD.org>
2002-08-28 20:39:48 +0000

    Allow one to grab the definition of struct ucred by defining _WANT_UCRED instead of forcing _KERNEL.
    Move the include of sys/_label.h in ucred.h under the
    _KERNEL || _WANT_UCRED case.

--- lib/libkvm/kvm_proc.c
+++ lib/libkvm/kvm_proc.c
@@ -52,9 +52,8 @@ static char sccsid[] = "@(#)kvm_proc.c	8.3 (Berkeley) 9/23/93";
  */
 
 #include <sys/param.h>
-#define _KERNEL
+#define _WANT_UCRED	/* make ucred.h give us 'struct ucred' */
 #include <sys/ucred.h>
-#undef _KERNEL
 #include <sys/user.h>
 #include <sys/proc.h>
 #include <sys/exec.h>
