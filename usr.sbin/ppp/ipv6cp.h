a3c48b40e951ee6765c5064c6c44e512cae9ea44
Brian Somers <brian@FreeBSD.org>
2002-08-29 02:44:58 +0000

    - made ppp compliant to RFC 2472 (based on a patch from another   contributor) - support ipv6cpretry and ipv6cpretries, which are IPv6 versions   of ipcpretry and ipcpretries. - improve handling of IPv6 link-local addresses
    Submitted by: JINMEI Tatuya <jinmei@isl.rdc.toshiba.co.jp>

--- usr.sbin/ppp/ipv6cp.h
+++ usr.sbin/ppp/ipv6cp.h
@@ -32,6 +32,8 @@
 #define	TY_TOKEN	1
 #define	TY_COMPPROTO	2
 
+#define	IPV6CP_IFIDLEN	8		/* RFC2472 */
+
 struct ipv6cp {
   struct fsm fsm;			/* The finite state machine */
 
@@ -41,8 +43,8 @@ struct ipv6cp {
 
   unsigned peer_tokenreq : 1;		/* Any TY_TOKEN REQs from the peer ? */
 
-  u_int32_t my_token;			/* Token I'm willing to use */
-  u_int32_t peer_token;			/* Token he's willing to use */
+  u_char my_ifid[IPV6CP_IFIDLEN];	/* Local Interface Identifier */
+  u_char his_ifid[IPV6CP_IFIDLEN];	/* Peer Interface Identifier */
 
   struct ncpaddr myaddr;		/* Local address */
   struct ncpaddr hisaddr;		/* Peer address */
--- usr.sbin/ppp/ncpaddr.c
+++ usr.sbin/ppp/ncpaddr.c
@@ -163,12 +163,19 @@ static void
 adjust_linklocal(struct sockaddr_in6 *sin6)
 {
     /* XXX: ?????!?!?!!!!!  This is horrible ! */
+#if 0
+    /*
+     * The kernel does not understand sin6_scope_id for routing at this moment.
+     * We should rather keep the embedded ID.
+     * jinmei@kame.net, 20011026
+     */
     if (IN6_IS_ADDR_LINKLOCAL(&sin6->sin6_addr) ||
         IN6_IS_ADDR_MC_LINKLOCAL(&sin6->sin6_addr)) {
       sin6->sin6_scope_id =
         ntohs(*(u_short *)&sin6->sin6_addr.s6_addr[2]);
       *(u_short *)&sin6->sin6_addr.s6_addr[2] = 0;
     }
+#endif
 }
 #endif
 
@@ -379,8 +386,13 @@ ncpaddr_ntowa(const struct ncpaddr *addr)
     sin6.sin6_family = AF_INET6;
     sin6.sin6_addr = addr->ncpaddr_ip6addr;
     adjust_linklocal(&sin6);
+#ifdef NI_WITHSCOPEID
     if (getnameinfo((struct sockaddr *)&sin6, sizeof sin6, res, sizeof(res),
                     NULL, 0, NI_WITHSCOPEID | NI_NUMERICHOST) != 0)
+#else
+    if (getnameinfo((struct sockaddr *)&sin6, sizeof sin6, res, sizeof(res),
+                    NULL, 0, NI_NUMERICHOST) != 0)
+#endif
       break;
 
     return res;
