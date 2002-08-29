49abe24622c5a23cba45e6cf876cd09c1d60b286
sobomax <sobomax@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-08-29 12:52:28 +0000

    Correctly handle ifr.ifr_flags/ifr.ifr_flagshigh like ifconfig(8) does.
    MFC after:	1 day
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@102574 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/gifconfig/gifconfig.c
+++ usr.sbin/gifconfig/gifconfig.c
@@ -454,14 +454,15 @@ setifflags(vname, value)
  		exit(1);
  	}
 	strncpy(ifr.ifr_name, name, sizeof (ifr.ifr_name));
- 	flags = ifr.ifr_flags;
+ 	flags = (ifr.ifr_flags & 0xffff) | (ifr.ifr_flagshigh << 16);
 
 	if (value < 0) {
 		value = -value;
 		flags &= ~value;
 	} else
 		flags |= value;
-	ifr.ifr_flags = flags;
+	ifr.ifr_flags = flags & 0xffff;
+	ifr.ifr_flagshigh = flags >> 16;
 	if (ioctl(s, SIOCSIFFLAGS, (caddr_t)&ifr) < 0)
 		Perror(vname);
 }
