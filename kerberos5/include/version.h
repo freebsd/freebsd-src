440ecb83648a860dbbe332e7bde1b8af47f3cea8
nectar <nectar@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-08-30 21:33:20 +0000

    Update build infrastructure after import of Heimdal Kerberos 2002/08/29.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@102649 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- kerberos5/include/version.h
+++ kerberos5/include/version.h
@@ -1,5 +1,5 @@
 /* $FreeBSD$ */
 const char *krb4_long_version = "@(#)$Version: krb4-1.0.5 (FreeBSD) $";
 const char *krb4_version = "krb4-1.0.5";
-const char *heimdal_long_version = "@(#)$Version: heimdal-0.4e (FreeBSD) $";
-const char *heimdal_version = "heimdal-0.4e";
+const char *heimdal_long_version = "@(#)$Version: Heimdal 0.4f (FreeBSD) $";
+const char *heimdal_version = "Heimdal 0.4f";
