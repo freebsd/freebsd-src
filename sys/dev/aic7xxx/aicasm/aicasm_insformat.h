85faa520a4798cfaa883f4ca0f96a26136464807
gibbs <gibbs@FreeBSD.org>
2002-08-31 06:39:41 +0000

    Update assembler syntax and assembler to allow generation of register description tables used for diagnostic "pretty printing".

--- sys/dev/aic7xxx/aicasm/aicasm_insformat.h
+++ sys/dev/aic7xxx/aicasm/aicasm_insformat.h
@@ -37,7 +37,7 @@
  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGES.
  *
- * $Id: //depot/aic7xxx/aic7xxx/aicasm/aicasm_insformat.h#9 $
+ * $Id: //depot/aic7xxx/aic7xxx/aicasm/aicasm_insformat.h#10 $
  *
  * $FreeBSD$
  */
--- sys/dev/aic7xxx/aicasm/aicasm_macro_gram.y
+++ sys/dev/aic7xxx/aicasm/aicasm_macro_gram.y
@@ -38,7 +38,7 @@
  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGES.
  *
- * $Id$
+ * $Id: //depot/aic7xxx/aic7xxx/aicasm/aicasm_macro_gram.y#5 $
  *
  * $FreeBSD$
  */
