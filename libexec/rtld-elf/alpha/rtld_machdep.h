6aa9d91828eb790054a566bcc88709dc9b26bc1b
obrien <obrien@FreeBSD.org>
2002-09-02 02:10:20 +0000

    MFC: ld-elf.so.1 assumed a few too many things about the ordering of sections produced by ld(8) (ie: that _DYNAMIC immediately follows the _GOT). Update ld.so to deal with the new (faster) PLT format that gcc-3.1 and binutils can produce.

--- libexec/rtld-elf/alpha/rtld_machdep.h
+++ libexec/rtld-elf/alpha/rtld_machdep.h
@@ -34,6 +34,9 @@
 
 void reloc_jmpslot(Elf_Addr *, Elf_Addr);
 
+/* Lazy binding entry point, called via PLT. */
+void _rtld_bind_start_old(void);
+
 /* Atomic operations. */
 int cmp0_and_store_int(volatile int *, int);
 void atomic_add_int(volatile int *, int);
