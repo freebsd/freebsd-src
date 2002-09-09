ca459ecd16cd68c42c9d8ac267755a54e80e8192
imp <imp@FreeBSD.org>
2002-09-09 05:45:04 +0000

    A minimalistic version of sys/endian.h from current that supports only little endian machines (which is all the architectures that 4.x supports).
    Submitted by:	mike@
    Approved by:	re@

--- sys/sys/endian.h
+++ sys/sys/endian.h
@@ -30,63 +30,22 @@
 #define _SYS_ENDIAN_H_
 
 #include <sys/cdefs.h>
-#include <sys/_types.h>
 #include <machine/endian.h>
 
-#ifndef _UINT16_T_DECLARED
-typedef	__uint16_t	uint16_t;
-#define	_UINT16_T_DECLARED
-#endif
- 
-#ifndef _UINT32_T_DECLARED
-typedef	__uint32_t	uint32_t;
-#define	_UINT32_T_DECLARED
-#endif
- 
-#ifndef _UINT64_T_DECLARED
-typedef	__uint64_t	uint64_t;
-#define	_UINT64_T_DECLARED
-#endif
- 
-/*
- * General byte order swapping functions.
- */
-#define	bswap16(x)	__bswap16(x)
-#define	bswap32(x)	__bswap32(x)
-#define	bswap64(x)	__bswap64(x)
-
-/*
- * Host to big endian, host to little endian, big endian to host, and little
- * endian to host byte order functions as detailed in byteorder(9).
- */
-#if _BYTE_ORDER == _LITTLE_ENDIAN
-#define	htobe16(x)	bswap16((x))
-#define	htobe32(x)	bswap32((x))
-#define	htobe64(x)	bswap64((x))
+#if BYTE_ORDER == LITTLE_ENDIAN
+#define	htobe16(x)	htons((x))
+#define	htobe32(x)	htonl((x))
 #define	htole16(x)	((uint16_t)(x))
 #define	htole32(x)	((uint32_t)(x))
 #define	htole64(x)	((uint64_t)(x))
 
-#define	be16toh(x)	bswap16((x))
-#define	be32toh(x)	bswap32((x))
-#define	be64toh(x)	bswap64((x))
+#define	be16toh(x)	ntohs((x))
+#define	be32toh(x)	ntohl((x))
 #define	le16toh(x)	((uint16_t)(x))
 #define	le32toh(x)	((uint32_t)(x))
 #define	le64toh(x)	((uint64_t)(x))
-#else /* _BYTE_ORDER != _LITTLE_ENDIAN */
-#define	htobe16(x)	((uint16_t)(x))
-#define	htobe32(x)	((uint32_t)(x))
-#define	htobe64(x)	((uint64_t)(x))
-#define	htole16(x)	bswap16((x))
-#define	htole32(x)	bswap32((x))
-#define	htole64(x)	bswap64((x))
-
-#define	be16toh(x)	((uint16_t)(x))
-#define	be32toh(x)	((uint32_t)(x))
-#define	be64toh(x)	((uint64_t)(x))
-#define	le16toh(x)	bswap16((x))
-#define	le32toh(x)	bswap32((x))
-#define	le64toh(x)	bswap64((x))
-#endif /* _BYTE_ORDER == _LITTLE_ENDIAN */
+#else
+#error "Byte ordering unsupported."
+#endif /* BYTE_ORDER == LITTLE_ENDIAN */
 
 #endif	/* _SYS_ENDIAN_H_ */
