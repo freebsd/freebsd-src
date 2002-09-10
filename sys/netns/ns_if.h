8336bb0d0c7c13e0c611d49d59aff93cc38d989c
sobomax <sobomax@FreeBSD.org>
2002-09-10 08:20:30 +0000

    Remove extraneous extern from function prototype introduced in rev.1.14.

--- sys/netns/ns_if.h
+++ sys/netns/ns_if.h
@@ -81,7 +81,7 @@ struct nsip_req {
 
 #ifdef	_KERNEL
 extern struct ns_ifaddr	*ns_ifaddr;
-extern struct ns_ifaddr	*ns_iaonnetof(void);
+struct ns_ifaddr	*ns_iaonnetof(void);
 void			nsintr(void);
 extern struct ifqueue	nsintrq;	/* XNS input packet queue */
 #endif
