ee0c53086c6a39bd838ce29b0832e48cf9452f4d
Tom Rhodes <trhodes@FreeBSD.org>
2002-09-12 21:33:38 +0000

    MFC: Fix a bug where large msdos partitions were not handled correctly, along with a few fsck_msdosfs related 'issues'.  This also includes the style(9) corrections committed in rev 1.95
    PR:		28536, 30168
    Submitted by:	Jiangyi Lui <jyliu@163.net> && NetBSD
    Approved by:	re (jhb)

--- sys/msdosfs/msdosfs_vfsops.c
+++ sys/msdosfs/msdosfs_vfsops.c
@@ -680,8 +680,15 @@ mountmsdosfs(devvp, mp, p, argp)
 	}
 
 	/*
-	 * Check and validate (or perhaps invalidate?) the fsinfo structure?		XXX
+	 * Check and validate (or perhaps invalidate?) the fsinfo structure?
 	 */
+	if (pmp->pm_fsinfo && pmp->pm_nxtfree > pmp->pm_maxcluster) {
+		printf(
+		"Next free cluster in FSInfo (%lu) exceeds maxcluster (%lu)\n",
+		    pmp->pm_nxtfree, pmp->pm_maxcluster);
+		error = EINVAL;
+		goto error_exit;
+	}
 
 	/*
 	 * Allocate memory for the bitmap of allocated clusters, and then
