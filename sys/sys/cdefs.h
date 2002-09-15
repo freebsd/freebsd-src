69bed3d839fb123735c8243dd1385666e7d15c0b
bde <bde@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-09-15 14:17:29 +0000

    Define __LONG_LONG_SUPPORTED if the compiler claims to support C99. Don't set __LONG_LONG_SUPPORTED for gcc-1.  I didn't check exactly when gcc started to support long long, but it was in the version of gcc in FreeBSD-1.0 (gcc-2.4.5).
    Other C99 features should be ifdefed similarly, but the ifdefs for
    old versions of gcc will be more complicated since the features
    weren't in all versions of gcc-2.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@103345 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/cdefs.h
+++ sys/sys/cdefs.h
@@ -133,8 +133,7 @@
 #define	__func__	NULL
 #endif
 
-/* XXX: should use `#if __STDC_VERSION__ >= 199901'. */
-#if defined(__GNUC__) && !defined(__STRICT_ANSI__)
+#if __GNUC__ >= 2 && !defined(__STRICT_ANSI__) || __STDC_VERSION__ >= 199901
 #define	__LONG_LONG_SUPPORTED
 #endif
 
