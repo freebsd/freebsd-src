52ca4b215c8ec84a9dac3ff7e3f5e704e33529c9
jake <jake@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-09-17 16:21:48 +0000

    Fix standard kse breakge of non-x86 platforms.  sigh.
    Pointy hat to:	kse
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@103494 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sparc64/sparc64/vm_machdep.c
+++ sys/sparc64/sparc64/vm_machdep.c
@@ -119,29 +119,13 @@ cpu_thread_setup(struct thread *td)
 }
 
 void
-cpu_save_upcall(struct thread *td, struct kse *newkse)
-{
-}
-
-void
 cpu_set_upcall(struct thread *td, void *pcb)
 {
 }
 
 void
-cpu_set_args(struct thread *td, struct kse *ke)
+cpu_set_upcall_kse(struct thread *td, struct kse *ke)
 {
-}
-
-void
-cpu_free_kse_mdstorage(struct kse *ke)
-{
-}
-
-int
-cpu_export_context(struct thread *td)
-{
-	return (0);
 }
 
 /*
