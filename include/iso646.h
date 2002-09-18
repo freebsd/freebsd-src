686a79f41c65669a33cfc82909a896aa8d463f37
mike <mike@FreeBSD.org>
2002-09-18 22:23:59 +0000

    style(9): tab after #define.

--- include/iso646.h
+++ include/iso646.h
@@ -27,18 +27,18 @@
  */
 
 #ifndef _ISO646_H_
-#define _ISO646_H_
+#define	_ISO646_H_
 
-#define and	&&
-#define and_eq	&=
-#define bitand	&
-#define bitor	|
-#define compl	~
-#define not	!
-#define not_eq	!=
-#define or	||
-#define or_eq	|=
-#define xor	^
-#define xor_eq	^=
+#define	and	&&
+#define	and_eq	&=
+#define	bitand	&
+#define	bitor	|
+#define	compl	~
+#define	not	!
+#define	not_eq	!=
+#define	or	||
+#define	or_eq	|=
+#define	xor	^
+#define	xor_eq	^=
 
 #endif /* !_ISO646_H_ */
