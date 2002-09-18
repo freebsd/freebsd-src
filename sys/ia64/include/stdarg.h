e35c51c74c9136c275aff6e68216801a7262f546
mike <mike@FreeBSD.org>
2002-09-18 07:33:16 +0000

    Implement C99's va_copy() macro.

--- sys/ia64/include/stdarg.h
+++ sys/ia64/include/stdarg.h
@@ -50,6 +50,9 @@ typedef	__va_list	va_list;
 #define	va_arg(ap, type) \
 	__builtin_va_arg((ap), type)
 
+#define	va_copy(dest, src) \
+	__builtin_va_copy((dest), (src))
+
 #define	va_end(ap) \
 	__builtin_va_end(ap)
 
