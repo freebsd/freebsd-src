d063152de15b045caca51218d4f7046ceaf07fbe
alfred <alfred@FreeBSD.org>
2002-09-19 00:43:32 +0000

    Add the rest of the kernel support for the sem_ API in kern/uipc_sem.c.
    Option 'P1003_1B_SEMAPHORES' to compile them in, or load the "sem" module
    to activate them.
    
    Have kern/makesyscalls.sh emit an include for sys/_semaphore.h into sysproto.h
    to pull in the typedef for semid_t.
    
    Add the syscalls to the syscall table as module stubs.

--- sys/kern/makesyscalls.sh
+++ sys/kern/makesyscalls.sh
@@ -115,8 +115,9 @@ s/\$//g
 		printf " * created from%s\n */\n\n", $0 > sysarg
 		printf "#ifndef %s\n", sysproto_h > sysarg
 		printf "#define\t%s\n\n", sysproto_h > sysarg
-		printf "#include <sys/signal.h>\n\n" > sysarg
-		printf "#include <sys/acl.h>\n\n" > sysarg
+		printf "#include <sys/signal.h>\n" > sysarg
+		printf "#include <sys/acl.h>\n" > sysarg
+		printf "#include <posix4/_semaphore.h>\n\n" > sysarg
 		printf "struct proc;\n\n" > sysarg
 		printf "struct thread;\n\n" > sysarg
 		printf "#define\tPAD_(t)\t(sizeof(register_t) <= sizeof(t) ? \\\n" > sysarg
