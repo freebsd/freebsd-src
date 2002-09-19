f3621a00cfdd63f527a677354ba6fd872ebffb79
jeff <jeff@FreeBSD.org>
2002-09-19 06:05:32 +0000

    - Use my freebsd email alias in the copyright.  - Remove redundant instances of my email alias in the file summary.

--- sys/vm/uma_core.c
+++ sys/vm/uma_core.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, Jeffrey Roberson <jroberson@chesapeake.net>
+ * Copyright (c) 2002, Jeffrey Roberson <jeff@freebsd.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
