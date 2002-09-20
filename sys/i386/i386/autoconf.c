9759856130134543c6a4162ead89e06192891e8a
phk <phk@FreeBSD.org>
2002-09-20 17:59:09 +0000

    We need neither <sys/diskslice.h> nor <sys/disklabel.h> here.
    Sponsored by:	DARPA & NAI Labs.

--- sys/i386/i386/autoconf.c
+++ sys/i386/i386/autoconf.c
@@ -55,8 +55,6 @@
 #include <sys/systm.h>
 #include <sys/bus.h>
 #include <sys/conf.h>
-#include <sys/disklabel.h>
-#include <sys/diskslice.h>
 #include <sys/reboot.h>
 #include <sys/kernel.h>
 #include <sys/malloc.h>
