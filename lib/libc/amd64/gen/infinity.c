207fb06f80b03b76ba9e573a376d32a7b8c6c731
archie <archie@FreeBSD.org>
2002-09-20 15:43:26 +0000

    Revert previous commit to unbreak world until we figure out the right way to do it.

--- lib/libc/amd64/gen/infinity.c
+++ lib/libc/amd64/gen/infinity.c
@@ -8,4 +8,4 @@ __FBSDID("$FreeBSD$");
 #include <math.h>
 
 /* bytes for +Infinity on a 387 */
-const union __infinity_un __infinity = { { 0, 0, 0, 0, 0, 0, 0xf0, 0x7f } };
+char __infinity[] = { 0, 0, 0, 0, 0, 0, 0xf0, 0x7f };
