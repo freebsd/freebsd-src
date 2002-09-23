a15086b7966176514e1a356b573ea9af3ba63308
peter <peter@FreeBSD.org>
2002-09-23 05:04:05 +0000

    Delete a whole bunch of compatability defines that we dont use anymore.

--- sys/amd64/include/tss.h
+++ sys/amd64/include/tss.h
@@ -47,26 +47,20 @@
 struct i386tss {
 	int	tss_link;	/* actually 16 bits: top 16 bits must be zero */
 	int	tss_esp0; 	/* kernel stack pointer privilege level 0 */
-#define	tss_ksp	tss_esp0
 	int	tss_ss0;	/* actually 16 bits: top 16 bits must be zero */
 	int	tss_esp1; 	/* kernel stack pointer privilege level 1 */
 	int	tss_ss1;	/* actually 16 bits: top 16 bits must be zero */
 	int	tss_esp2; 	/* kernel stack pointer privilege level 2 */
 	int	tss_ss2;	/* actually 16 bits: top 16 bits must be zero */
 	int	tss_cr3; 	/* page table directory */
-#define	tss_ptd	tss_cr3
 	int	tss_eip; 	/* program counter */
-#define	tss_pc	tss_eip
 	int	tss_eflags; 	/* program status longword */
-#define	tss_psl	tss_eflags
 	int	tss_eax;
 	int	tss_ecx;
 	int	tss_edx;
 	int	tss_ebx;
 	int	tss_esp; 	/* user stack pointer */
-#define	tss_usp	tss_esp
 	int	tss_ebp; 	/* user frame pointer */
-#define	tss_fp	tss_ebp
 	int	tss_esi;
 	int	tss_edi;
 	int	tss_es;		/* actually 16 bits: top 16 bits must be zero */
