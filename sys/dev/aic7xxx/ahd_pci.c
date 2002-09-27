e554ada9f531cda2559268d9fdcd77a75e642e07
Justin T. Gibbs <gibbs@FreeBSD.org>
2002-09-27 16:29:35 +0000

    MFC: Bring aic79xx driver up to the latest in -current.
    Approved by:	RE (jhb)

--- sys/dev/aic7xxx/ahd_pci.c
+++ sys/dev/aic7xxx/ahd_pci.c
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id$
+ * $Id: //depot/aic7xxx/freebsd/dev/aic7xxx/ahd_pci.c#6 $
  *
  * $FreeBSD$
  */
