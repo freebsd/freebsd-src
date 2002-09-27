b57ab3a8ef678f60d0a465af4e7bc2da851c8c97
gibbs <gibbs@FreeBSD.org>
2002-09-27 16:28:33 +0000

    MFC: Sync Perforce Ids.
    Approved by:	RE (jhb)

--- sys/dev/aic7xxx/ahc_eisa.c
+++ sys/dev/aic7xxx/ahc_eisa.c
@@ -26,7 +26,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: //depot/aic7xxx/freebsd/dev/aic7xxx/ahc_eisa.c#8 $
+ * $Id: //depot/aic7xxx/freebsd/dev/aic7xxx/ahc_eisa.c#9 $
  *
  * $FreeBSD$
  */
