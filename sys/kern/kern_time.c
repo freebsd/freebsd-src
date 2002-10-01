1dc8e58608ee93b99d63563020bbdd66360aaf88
Bruce Evans <bde@FreeBSD.org>
2002-10-01 08:00:41 +0000

    MFC (1.85: round up instead of towards 0 in clock_getres()).
    PR:		41781
    Approved by:	re (jhb)

--- sys/kern/kern_time.c
+++ sys/kern/kern_time.c
@@ -209,7 +209,12 @@ clock_getres(p, uap)
 	error = 0;
 	if (SCARG(uap, tp)) {
 		ts.tv_sec = 0;
-		ts.tv_nsec = 1000000000 / timecounter->tc_frequency;
+		/*
+		 * Round up the result of the division cheaply by adding 1.
+		 * Rounding up is especially important if rounding down
+		 * would give 0.  Perfect rounding is unimportant.
+		 */
+		ts.tv_nsec = 1000000000 / timecounter->tc_frequency + 1;
 		error = copyout(&ts, SCARG(uap, tp), sizeof(ts));
 	}
 	return (error);
