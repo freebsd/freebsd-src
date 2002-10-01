2a0ba62f796de6df6d5f1cc3689a24b827b4b852
jmallett <jmallett@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-10-01 00:07:28 +0000

    (Forced commit, to clarify previous commit of ksiginfo/signal queue code.)
    I've added a structure, kernel-private, to represent a pending or in-delivery
    signal, called `ksiginfo'.  It is roughly analogous to the basic information
    that is exported by the POSIX interface 'siginfo_t', but more basic.  I've
    added functions to allocate these structures, and further to wrap all signal
    operations using them.
    
    Once the operations are wrapped, I've added a TailQ (see queue(3)) of these
    structures to 'struct proc', and all pending signals are in that TailQ.  When
    a signal is being delivered, it is dequeued from the list.  Once I finish
    the spreading of ksiginfo throughout the tree, the dequeued structure will be
    delivered to the process in question, whereas currently and normally, the
    signal number is what is used.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@104245 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/ksiginfo.h
+++ sys/sys/ksiginfo.h
@@ -36,6 +36,7 @@
 #endif
 
 #include <sys/malloc.h>
+#include <sys/signal.h>
 
 /*
  * Structures and prototypes for working with the in-kernel representation
@@ -66,6 +67,7 @@ __BEGIN_DECLS;
 int ksiginfo_alloc(struct ksiginfo **, int);
 int ksiginfo_dequeue(struct ksiginfo **, struct proc *, int);
 int ksiginfo_destroy(struct ksiginfo **);
+int ksiginfo_to_siginfo_t(struct ksiginfo *, siginfo_t *);
 int ksiginfo_to_sigset_t(struct proc *, sigset_t *);
 int signal_add(struct proc *, struct ksiginfo *, int);
 int signal_delete(struct proc *, struct ksiginfo *, int);
