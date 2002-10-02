4c5b608bba4f671c1540b9b9c3097ff4c37bdb8f
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-10-02 07:36:08 +0000

    Add LIBEXPAT definition.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@104353 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/bsd.libnames.mk
+++ share/mk/bsd.libnames.mk
@@ -31,6 +31,7 @@ LIBDEVSTAT?=	${DESTDIR}${LIBDIR}/libdevstat.a
 LIBDIALOG?=	${DESTDIR}${LIBDIR}/libdialog.a
 LIBDISK?=	${DESTDIR}${LIBDIR}/libdisk.a
 LIBEDIT?=	${DESTDIR}${LIBDIR}/libedit.a
+LIBEXPAT?=	${DESTDIR}${LIBDIR}/libexpat.a
 LIBFETCH?=	${DESTDIR}${LIBDIR}/libfetch.a
 LIBFL?=		"don't use LIBFL, use LIBL"
 LIBFORM?=	${DESTDIR}${LIBDIR}/libform.a
