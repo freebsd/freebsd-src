022d92cdb170c94d1769616da5390ef5b4137c93
sos <sos@FreeBSD.org>
2002-10-02 14:13:38 +0000

    MFC: 	Add yet another Promise chip id.
    Approved by: re@

--- sys/dev/ata/ata-pci.c
+++ sys/dev/ata/ata-pci.c
@@ -283,6 +283,7 @@ ata_pci_match(device_t dev)
     case 0x4d69105a:
     case 0x5275105a:
     case 0x6269105a: 
+    case 0x7275105a: 
 	return "Promise TX2 ATA133 controller";
 
     case 0x00041103:
@@ -587,6 +588,7 @@ ata_pci_intr(struct ata_channel *ch)
     case 0x4d69105a:	/* Promise TX2 ATA133 */
     case 0x5275105a:	/* Promise TX2 ATA133 */
     case 0x6269105a:	/* Promise TX2 ATA133 */
+    case 0x7275105a:	/* Promise TX2 ATA133 */
 	ATA_OUTB(ch->r_bmio, ATA_BMDEVSPEC_0, 0x0b);
 	if (!(ATA_INB(ch->r_bmio, ATA_BMDEVSPEC_1) & 0x20))
 	    return 1;
