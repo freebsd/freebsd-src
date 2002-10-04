d8a44cf9b45289a516817b08d6e0e54993314342
mike <mike@FreeBSD.org>
2002-10-04 22:10:06 +0000

    style(9) <machine/setjmp.h> headers so they look mostly the same.

--- sys/powerpc/include/setjmp.h
+++ sys/powerpc/include/setjmp.h
@@ -1,8 +1,10 @@
-/* $FreeBSD$ */
-/* $NetBSD: setjmp.h,v 1.3 1998/09/16 23:51:27 thorpej Exp $ */
+/*-
+ *	$NetBSD: setjmp.h,v 1.3 1998/09/16 23:51:27 thorpej Exp $
+ * $FreeBSD$
+ */
 
-#ifndef	_MACHINE_SETJMP_H_
-#define _MACHINE_SETJMP_H_
+#ifndef _MACHINE_SETJMP_H_
+#define	_MACHINE_SETJMP_H_
 
 #define	_JBLEN	100
 
@@ -12,9 +14,9 @@
  * internally to avoid some run-time errors for mismatches.
  */
 #ifndef _ANSI_SOURCE
-typedef struct _sigjmp_buf { long _sjb[_JBLEN + 1]; } sigjmp_buf[1];
+typedef	struct _sigjmp_buf { long _sjb[_JBLEN + 1]; } sigjmp_buf[1];
 #endif
 
-typedef struct _jmp_buf { long _jb[_JBLEN + 1]; } jmp_buf[1];
+typedef	struct _jmp_buf { long _jb[_JBLEN + 1]; } jmp_buf[1];
 
-#endif	/* _MACHINE_SETJMP_H_ */
+#endif /* !_MACHINE_SETJMP_H_ */
