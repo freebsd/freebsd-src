62d02dc863fb2d9f57dee585b0b394f7e021ba8f
Murray Stokely <murray@FreeBSD.org>
2002-10-08 04:59:24 +0000

    Bump __FreeBSD_version for 4.7-RELEASE.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -54,7 +54,7 @@
  *	doc/en_US.ISO8859-1/books/porters-handbook/book.sgml
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 460102	/* Master, propagated to newvers */
+#define __FreeBSD_version 470000	/* Master, propagated to newvers */
 
 #ifndef NULL
 #define	NULL	0
