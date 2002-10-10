5b687f1f9cac4884e4a7782773a7095a2d460167
kan <kan@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-10-10 04:51:11 +0000

    Merge FreeBSD modifications into gcc 3.2.1-prerelease:   1.2  -fformat-extensions   1.7  FORCE_OPTIMIZATION_DOWNGRADE knob for Alpha   1.14 -O0 -O1 should optimize alignment for time, not size
    Approved by:	obrien
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@104765 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/gcc/toplev.c
+++ contrib/gcc/toplev.c
@@ -884,6 +884,10 @@ int align_labels_max_skip;
 int align_functions;
 int align_functions_log;
 
+/* Like align_functions_log above, but used by front-ends to force the
+   minimum function alignment.  Zero means no alignment is forced.  */
+int force_align_functions_log;
+
 /* Table of supported debugging formats.  */
 static const struct
 {
@@ -3023,12 +3027,12 @@ rest_of_compilation (decl)
 	 block.  The loop infrastructure does the real job for us.  */
       flow_loops_find (&loops, LOOP_TREE);
 
+      if (rtl_dump_file)
+	flow_loops_dump (&loops, rtl_dump_file, NULL, 0);
+
       /* Estimate using heuristics if no profiling info is available.  */
       if (flag_guess_branch_prob)
 	estimate_probability (&loops);
-
-      if (rtl_dump_file)
-	flow_loops_dump (&loops, rtl_dump_file, NULL, 0);
 
       flow_loops_free (&loops);
     }
