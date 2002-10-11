1f2dbc61b90bac50bb82ed36ab450ec4ae18e44c
obrien <obrien@FreeBSD.org>
2002-10-11 06:14:49 +0000

    Bring the binutils_2_13_20021110_snap version of this to the HEAD branch.

--- contrib/binutils/ld/emulparams/elf64alpha.sh
+++ contrib/binutils/ld/emulparams/elf64alpha.sh
@@ -8,13 +8,15 @@ TEMPLATE_NAME=elf32
 OUTPUT_FORMAT="elf64-alpha"
 TEXT_START_ADDR="0x120000000"
 MAXPAGESIZE=0x10000
+COMMONPAGESIZE=0x2000
 NONPAGED_TEXT_START_ADDR="0x120000000"
 ARCH=alpha
 MACHINE=
 GENERATE_SHLIB_SCRIPT=yes
 DATA_PLT=
-# Note that the string of digits is little-endian.
-NOP=0x0000fe2f		# unop
+# Note that the number is always big-endian, thus we have to 
+# reverse the digit string.
+NOP=0x0000fe2f1f04ff47		# unop; nop
 
 OTHER_READONLY_SECTIONS="
   .reginfo      ${RELOCATING-0} : { *(.reginfo) }"
