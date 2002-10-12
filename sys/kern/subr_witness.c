d2575b9651418b24965ca90a03a67927dc0cd940
Peter Wemm <peter@FreeBSD.org>
2002-10-12 00:33:36 +0000

    Register the machine check private state spinlock on ia64.

--- sys/kern/subr_witness.c
+++ sys/kern/subr_witness.c
@@ -234,6 +234,9 @@ static struct witness_order_list_entry order_lists[] = {
 	{ "mutex profiling lock", &lock_class_mtx_spin },
 	{ "zombie_thread_lock", &lock_class_mtx_spin },
 	{ "ALD Queue", &lock_class_mtx_spin },
+#ifdef __ia64__
+	{ "MCA spin lock", &lock_class_mtx_spin },
+#endif
 	{ NULL, NULL },
 	{ NULL, NULL }
 };
