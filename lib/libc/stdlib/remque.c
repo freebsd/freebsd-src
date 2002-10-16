f82557c06579c3534eb41fd463e608787594afc9
robert <robert@FreeBSD.org>
2002-10-16 14:05:29 +0000

    Remove unneeded $FreeBSD$ tags.

--- lib/libc/stdlib/remque.c
+++ lib/libc/stdlib/remque.c
@@ -5,8 +5,6 @@
  *
  * As long as the above copyright statement and this notice remain
  * unchanged, you can do what ever you want with this file. 
- *
- * $FreeBSD$
  */
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
