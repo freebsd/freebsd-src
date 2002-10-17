7c8cdd9da7d9401477a95ad317143bfe049afbd5
Maxim Konovalov <maxim@FreeBSD.org>
2002-10-17 13:19:40 +0000

    Extend rev. 1.105.2.7: make timecounters more robust for negative deltas. This change does not apply to -CURRENT.
    PR:		kern/42875 and "named crash" thread in -bugs
    Spotted by:	bde
    Reviewed by:	phk
    
    Many thanks to Ross Finlayson <finlayson@live.com> and
    Womchick alexeyeff <wom@wom.ru> for testing.

--- sys/kern/kern_clock.c
+++ sys/kern/kern_clock.c
@@ -549,6 +549,11 @@ microtime(struct timeval *tv)
 	tv->tv_usec += ((u_int64_t)tco_delta(tc) * tc->tc_scale_micro) >> 32;
 	tv->tv_usec += boottime.tv_usec;
 	tv->tv_sec += boottime.tv_sec;
+	while (tv->tv_usec < 0) {
+		tv->tv_usec += 1000000;
+		if (tv->tv_sec > 0)
+			tv->tv_sec--;
+	}
 	while (tv->tv_usec >= 1000000) {
 		tv->tv_usec -= 1000000;
 		tv->tv_sec++;
@@ -571,6 +576,11 @@ nanotime(struct timespec *ts)
 	delta += ((u_int64_t)count * tc->tc_scale_nano_i);
 	delta += boottime.tv_usec * 1000;
 	ts->tv_sec += boottime.tv_sec;
+	while (delta < 0) {
+		delta += 1000000000;
+		if (ts->tv_sec > 0)
+			ts->tv_sec--;
+	}
 	while (delta >= 1000000000) {
 		delta -= 1000000000;
 		ts->tv_sec++;
@@ -615,6 +625,11 @@ microuptime(struct timeval *tv)
 	tv->tv_sec = tc->tc_offset_sec;
 	tv->tv_usec = tc->tc_offset_micro;
 	tv->tv_usec += ((u_int64_t)tco_delta(tc) * tc->tc_scale_micro) >> 32;
+	while (tv->tv_usec < 0) {
+		tv->tv_usec += 1000000;
+		if (tv->tv_sec > 0)
+			tv->tv_sec--;
+	}
 	while (tv->tv_usec >= 1000000) {
 		tv->tv_usec -= 1000000;
 		tv->tv_sec++;
@@ -635,6 +650,11 @@ nanouptime(struct timespec *ts)
 	delta += ((u_int64_t)count * tc->tc_scale_nano_f);
 	delta >>= 32;
 	delta += ((u_int64_t)count * tc->tc_scale_nano_i);
+	while (delta < 0) {
+		delta += 1000000000;
+		if (ts->tv_sec > 0)
+			ts->tv_sec--;
+	}
 	while (delta >= 1000000000) {
 		delta -= 1000000000;
 		ts->tv_sec++;
