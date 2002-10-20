ad592c0da6c9355af4171e104509d8f07e34ed22
phk <phk@FreeBSD.org>
2002-10-20 22:35:20 +0000

    We have mem{cpy,cmp,set} functions in the kernel, don't #define them to b{copy,zero,cmp} functions anymore.
    Spotted by:	FlexeLint.

--- sys/net/zlib.c
+++ sys/net/zlib.c
@@ -57,9 +57,6 @@
 #include <sys/time.h>
 #include <sys/systm.h>
 #  define HAVE_MEMCPY
-#  define memcpy(d, s, n)	bcopy((s), (d), (n))
-#  define memset(d, v, n)	bzero((d), (n))
-#  define memcmp		bcmp
 
 #else
 #if defined(__KERNEL__)
