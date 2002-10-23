7b26af3589f249469319ebbc78934b1c75ce6bb8
fjoe <fjoe@FreeBSD.org>
2002-10-23 15:35:07 +0000

    MFC: sysvmsg sysctls and tuneables
    Submitted by:	mr
    Approved by:	alfred

--- sys/kern/sysv_msg.c
+++ sys/kern/sysv_msg.c
@@ -128,6 +128,7 @@ msginit(dummy)
 {
 	register int i;
 
+	msginfo.msgmax = msginfo.msgseg * msginfo.msgssz;
 	msgpool = malloc(msginfo.msgmax, M_MSG, M_WAITOK);
 	if (msgpool == NULL)
 		panic("msgpool is NULL");
@@ -1099,3 +1100,25 @@ msgrcv(p, uap)
 	p->p_retval[0] = msgsz;
 	return(0);
 }
+
+static int
+sysctl_msqids(SYSCTL_HANDLER_ARGS)
+{
+
+	return (SYSCTL_OUT(req, msqids,
+	    sizeof(struct msqid_ds) * msginfo.msgmni));
+}
+
+TUNABLE_INT("kern.ipc.msgseg", &msginfo.msgseg);
+TUNABLE_INT("kern.ipc.msgssz", &msginfo.msgssz);
+TUNABLE_INT("kern.ipc.msgmni", &msginfo.msgmni);
+
+SYSCTL_DECL(_kern_ipc);
+SYSCTL_INT(_kern_ipc, OID_AUTO, msgmax, CTLFLAG_RD, &msginfo.msgmax, 0, "");
+SYSCTL_INT(_kern_ipc, OID_AUTO, msgmni, CTLFLAG_RD, &msginfo.msgmni, 0, "");
+SYSCTL_INT(_kern_ipc, OID_AUTO, msgmnb, CTLFLAG_RD, &msginfo.msgmnb, 0, "");
+SYSCTL_INT(_kern_ipc, OID_AUTO, msgtql, CTLFLAG_RD, &msginfo.msgtql, 0, "");
+SYSCTL_INT(_kern_ipc, OID_AUTO, msgssz, CTLFLAG_RD, &msginfo.msgssz, 0, "");
+SYSCTL_INT(_kern_ipc, OID_AUTO, msgseg, CTLFLAG_RD, &msginfo.msgseg, 0, "");
+SYSCTL_PROC(_kern_ipc, OID_AUTO, msqids, CTLFLAG_RD,
+    NULL, 0, sysctl_msqids, "", "Message queue IDs");
