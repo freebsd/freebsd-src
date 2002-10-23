93c47cddbde9cc7acf2cc1c49619b2f2a118c5c0
assar <assar@FreeBSD.org>
2002-10-23 06:12:21 +0000

    update version numbers to (consistenly):
    krb4 1.0.5 fb1 (including the kadmind fix)
    heimdal 0.5 fb1 (including the kadmind fix)

--- kerberosIV/include/version.h
+++ kerberosIV/include/version.h
@@ -1,5 +1,5 @@
 /* $FreeBSD$ */
-char *krb4_long_version = "@(#)$Version: krb4-1.0 by root on greenpeace.grondar.za (i386-unknown-freebsd4.0) Sat Jan  8 19:34:57 SAST 2000 $";
-char *krb4_version = "krb4-1.0";
-char *heimdal_long_version = "@(#)$Version: heimdal-0.1l by root on greenpeace.grondar.za (i386-unknown-freebsd4.0) Sat Jan  8 20:56:02 SAST 2000 $";
-char *heimdal_version = "heimdal-0.1l";
+char *krb4_long_version = "@(#)$Version: krb4-1.0.5 (FreeBSD) $";
+char *krb4_version = "krb4-1.0.5";
+char *heimdal_long_version = "@(#)$Version: Heimdal 0.5fb1 (FreeBSD) $";
+char *heimdal_version = "Heimdal 0.5fb1";
