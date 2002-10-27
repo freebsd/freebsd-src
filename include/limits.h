188c541ceb9c752d22378cf9508d18c17b045947
Garrett Wollman <wollman@FreeBSD.org>
2002-10-27 18:03:02 +0000

    Update limits and configuration parameters for 1003.1/TC1/D6. Implement new sysconf keys.  Change the implenentation of _SC_ASYNCHRONOUS_IO in preparation for the next set of changes.
    Move some limits which had been in <sys/syslimits.h> to <limits.h> where
    they belong.  They had only ever been in syslimits.h to provide for the
    kernel implementation of the CTL_USER MIB branch, which went away with
    newsysctl years ago.  (There is a #error in <sys/syslimits.h> which I
    will downgrade in the next commit.)

--- include/limits.h
+++ include/limits.h
@@ -41,18 +41,27 @@
 
 #if __POSIX_VISIBLE
 #define	_POSIX_ARG_MAX		4096
-#define	_POSIX_CHILD_MAX	6
+#define	_POSIX_CHILD_MAX	25
 #define	_POSIX_LINK_MAX		8
 #define	_POSIX_MAX_CANON	255
 #define	_POSIX_MAX_INPUT	255
 #define	_POSIX_NAME_MAX		14
-#define	_POSIX_NGROUPS_MAX	0
-#define	_POSIX_OPEN_MAX		16
-#define	_POSIX_PATH_MAX		255
+#define	_POSIX_NGROUPS_MAX	8
+#define	_POSIX_OPEN_MAX		20
+#define	_POSIX_PATH_MAX		256
 #define	_POSIX_PIPE_BUF		512
 #define	_POSIX_SSIZE_MAX	32767
 #define	_POSIX_STREAM_MAX	8
-#define	_POSIX_TZNAME_MAX	3
+#define	_POSIX_TZNAME_MAX	6
+
+#define	BC_BASE_MAX		   99	/* max ibase/obase values in bc(1) */
+#define	BC_DIM_MAX		 2048	/* max array elements in bc(1) */
+#define	BC_SCALE_MAX		   99	/* max scale value in bc(1) */
+#define	BC_STRING_MAX		 1000	/* max const string length in bc(1) */
+#define	COLL_WEIGHTS_MAX	    0	/* max weights for order keyword */
+#define	EXPR_NEST_MAX		   32	/* max expressions nested in expr(1) */
+#define	LINE_MAX		 2048	/* max bytes in an input line */
+#define	RE_DUP_MAX		  255	/* max RE's in interval notation */
 
 #define	_POSIX2_BC_BASE_MAX	99
 #define	_POSIX2_BC_DIM_MAX	2048
@@ -70,19 +79,40 @@
 #define	_POSIX_DELAYTIMER_MAX	32
 #define	_POSIX_MQ_OPEN_MAX	8
 #define	_POSIX_MQ_PRIO_MAX	32
-#define	_POSIX_RTSIG_MAX	0
+#define	_POSIX_RTSIG_MAX	8
 #define	_POSIX_SEM_NSEMS_MAX	256
 #define	_POSIX_SEM_VALUE_MAX	32767
 #define	_POSIX_SIGQUEUE_MAX	32
 #define	_POSIX_TIMER_MAX	32
 #endif
 
+#if __POSIX_VISIBLE >= 199506
+#define	_POSIX_THREAD_DESTRUCTOR_ITERATIONS 4
+#define	_POSIX_THREAD_KEYS_MAX	128
+#define	_POSIX_THREAD_THREADS_MAX 64
+#endif
+
 #if __POSIX_VISIBLE >= 200112
 #define	_POSIX_HOST_NAME_MAX	255
+#define	_POSIX_LOGIN_NAME_MAX	9
+#define	_POSIX_SS_REPL_MAX	4
+#define	_POSIX_SYMLINK_MAX	255
+#define	_POSIX_SYMLOOP_MAX	8
+#define	_POSIX_TRACE_EVENT_NAME_MAX 30
+#define	_POSIX_TRACE_NAME_MAX	8
+#define	_POSIX_TRACE_SYS_MAX	8
+#define	_POSIX_TRACE_USER_EVENT_MAX 32
+#define	_POSIX_TTY_NAME_MAX	9
+#define	_POSIX2_CHARCLASS_NAME_MAX 14
+#define	_POSIX2_COLL_WEIGHTS_MAX 2
+
+#define	_POSIX_RE_DUP_MAX	_POSIX2_RE_DUP_MAX
 #endif
 
 #if __XSI_VISIBLE
 #define	_XOPEN_IOV_MAX		16
+#define	_XOPEN_NAME_MAX		255
+#define	_XOPEN_PATH_MAX		1024
 #define	PASS_MAX		128	/* _PASSWORD_LEN from <pwd.h> */
 
 #define	NL_ARGMAX		99	/* max # of position args for printf */
