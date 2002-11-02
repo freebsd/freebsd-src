5c8552eb5137774ad0c941813c508626fd909dfe
des <des@FreeBSD.org>
2002-11-02 19:55:23 +0000

    Switch to two-clause license, with NAI's permission.

--- crypto/openssh/auth2-pam-freebsd.c
+++ crypto/openssh/auth2-pam-freebsd.c
@@ -15,9 +15,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote
- *    products derived from this software without specific prior written
- *    permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
