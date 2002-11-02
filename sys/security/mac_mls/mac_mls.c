e6faed4db08f2a8629c90733451633e654577348
Maxime Henrion <mux@FreeBSD.org>
2002-11-02 20:16:35 +0000

    Fix some warnings on 64 bit architectures.  The vn_extattr_get() function takes an int * parameter, not a size_t * parameter.
    Arguably, it should rather take a size_t *, but that would
    require changing the uio_resid field of struct uio to be a size_t
    instead of an int, which I don't want to do that close to
    5.0-RELEASE.
    
    Reviewed by:	rwatson

--- sys/security/mac_mls/mac_mls.c
+++ sys/security/mac_mls/mac_mls.c
@@ -894,8 +894,7 @@ mac_mls_associate_vnode_extattr(struct mount *mp, struct label *fslabel,
     struct vnode *vp, struct label *vlabel)
 {
 	struct mac_mls temp, *source, *dest;
-	size_t buflen;
-	int error;
+	int buflen, error;
 
 	source = SLOT(fslabel);
 	dest = SLOT(vlabel);
