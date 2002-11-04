a85d9ae25e8e8696677bc30feb6eaf7fc150e529
roberto <roberto@FreeBSD.org>
2002-11-04 19:36:11 +0000

    Virgin import of ntpd 4.1.1b

--- contrib/ntp/ntpd/refclock_jjy.c
+++ contrib/ntp/ntpd/refclock_jjy.c
@@ -701,7 +701,7 @@ jjy_poll_cdex_jst2000 ( int unit, struct peer *peer )
 	 * Send "<ENQ>1J<ETX>" command
 	 */
 
-	if ( write ( pp->io.fd, "\x051J\x03", 4 ) != 4  ) {
+	if ( write ( pp->io.fd, "\0051J\003", 4 ) != 4  ) {
 		refclock_report ( peer, CEVNT_FAULT ) ;
 	}
 
