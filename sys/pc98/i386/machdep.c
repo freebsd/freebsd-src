c9da5e8fcb9158a9345c838be61c9f4fb35c2307
jmallett <jmallett@FreeBSD.org>
2002-11-06 14:53:35 +0000

    Remove what was a temporary bogus assignment of bits of siginfo_t, as it does not look like the prerequisites to fill it in properly will be in the tree for the upcoming release, but it's mostly done, so there is no need for these to stay around to remind us.

--- sys/pc98/i386/machdep.c
+++ sys/pc98/i386/machdep.c
@@ -492,8 +492,6 @@ freebsd4_sendsig(catcher, sig, mask, code)
 		sf.sf_si.si_signo = sig;
 		sf.sf_si.si_code = code;
 		sf.sf_si.si_addr = (void *)regs->tf_err;
-		sf.sf_si.si_pid = p->p_pid;
-		sf.sf_si.si_uid = p->p_ucred->cr_uid;
 	} else {
 		/* Old FreeBSD-style arguments. */
 		sf.sf_siginfo = code;
@@ -629,8 +627,6 @@ sendsig(catcher, sig, mask, code)
 		sf.sf_si.si_signo = sig;
 		sf.sf_si.si_code = code;
 		sf.sf_si.si_addr = (void *)regs->tf_err;
-		sf.sf_si.si_pid = p->p_pid;
-		sf.sf_si.si_uid = p->p_ucred->cr_uid;
 	} else {
 		/* Old FreeBSD-style arguments. */
 		sf.sf_siginfo = code;
