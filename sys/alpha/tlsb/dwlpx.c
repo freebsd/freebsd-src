fc33c796781938c96c666f019b711ff1a7abba9c
jhb <jhb@FreeBSD.org>
2002-11-08 20:52:21 +0000

    Fix warnings when compiled with SIMOS defined.

--- sys/alpha/tlsb/dwlpx.c
+++ sys/alpha/tlsb/dwlpx.c
@@ -123,7 +123,11 @@ struct dwlpx_softc {
 static driver_intr_t dwlpx_intr;
 
 static u_int32_t imaskcache[DWLPX_NIONODE][DWLPX_NHOSE][NHPC];
+#ifdef SIMOS
+extern void simos_intr(int);
+#else
 static void dwlpx_eintr(unsigned long);
+#endif
 
 /*
  * Direct-mapped window: 2G at 2G
@@ -741,13 +745,19 @@ dwlpx_dma_init(struct dwlpx_softc *sc)
 /*
  */
 
+#ifdef SIMOS
 static void
 dwlpx_intr(void *arg)
 {
-#ifdef SIMOS
-	extern void simos_intr(int);
+
 	simos_intr(0);
-#else
+}
+
+#else /* !SIMOS */
+
+static void
+dwlpx_intr(void *arg)
+{
 	unsigned long vec = (unsigned long) arg;
 	if ((vec & DWLPX_VEC_EMARK) != 0) {
 		dwlpx_eintr(vec);
@@ -758,7 +768,6 @@ dwlpx_intr(void *arg)
 		/* NOTREACHED */
 	}
 	alpha_dispatch_intr(NULL, vec);
-#endif
 }
 
 static void
@@ -833,6 +842,7 @@ dwlpx_eintr(unsigned long vec)
 		REGVAL(PCIA_ERR(i) + sc->sysbase) = hpcs[i].err;
 	}
 }
+#endif /* SIMOS */
 
 static device_method_t dwlpx_methods[] = {
 	/* Device interface */
