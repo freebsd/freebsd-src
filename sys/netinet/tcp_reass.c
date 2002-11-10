79baf19d1e75d85b2cfad66aee0506a0c8cfb8eb
sam <sam@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-11-10 17:17:32 +0000

    a better solution to building FAST_IPSEC w/o INET6
    Submitted by:	Jeffrey Hsu <hsu@FreeBSD.org>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@106736 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/tcp_reass.c
+++ sys/netinet/tcp_reass.c
@@ -86,9 +86,7 @@
 
 #ifdef FAST_IPSEC
 #include <netipsec/ipsec.h>
-#ifdef INET6
 #include <netipsec/ipsec6.h>
-#endif
 #endif /*FAST_IPSEC*/
 
 #ifdef IPSEC
@@ -574,13 +572,11 @@ findpcb:
 	}
 #endif
 #ifdef FAST_IPSEC
-#ifdef INET6
 	if (isipv6) {
 		if (inp != NULL && ipsec6_in_reject(m, inp)) {
 			goto drop;
 		}
 	} else
-#endif
 	if (inp != NULL && ipsec4_in_reject(m, inp)) {
 		goto drop;
 	}
