6bf0763a2798e8397852f1add3403489307e979d
emoore <emoore@FreeBSD.org>
2002-11-11 13:19:10 +0000

    amr.c, amr_cam.c, amrreg.h, amrvar.h: - added support for 12/16 byte cdb's, effecting CAM branch only ( non-disk support )
    amrreg.h:
    - increased number of scatter gather elements from 16 to 26.
    
    amr_pci.c:
    - amr_pci_free(), incorrect bus tag meant for 'amr_mailbox_dmat' was being freed
    
    all:
    - copyright change requested by scottl

--- sys/dev/amr/amr_pci.c
+++ sys/dev/amr/amr_pci.c
@@ -24,12 +24,33 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
+ * Copyright (c) 2002 Eric Moore
+ * Copyright (c) 2002 LSI Logic Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  * 3. The party using or redistributing the source code and binary forms
- *    agrees to the above disclaimer and the terms and conditions set forth
+ *    agrees to the disclaimer below and the terms and conditions set forth
  *    herein.
  *
- * Additional Copyright (c) 2002 by Eric Moore under same license.
- * Additional Copyright (c) 2002 LSI Logic Corporation
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  *
  *	$FreeBSD$
  */
@@ -408,10 +429,10 @@ amr_pci_free(struct amr_softc *sc)
     /* free and destroy DMA memory and tag for mailbox */
     if (sc->amr_mailbox) {
 	p = (u_int8_t *)(uintptr_t)(volatile void *)sc->amr_mailbox;
-	bus_dmamem_free(sc->amr_sg_dmat, p - 16, sc->amr_sg_dmamap);
+	bus_dmamem_free(sc->amr_mailbox_dmat, p - 16, sc->amr_mailbox_dmamap);
     }
-    if (sc->amr_sg_dmat)
-	bus_dma_tag_destroy(sc->amr_sg_dmat);
+    if (sc->amr_mailbox_dmat)
+	bus_dma_tag_destroy(sc->amr_mailbox_dmat);
 
     /* disconnect the interrupt handler */
     if (sc->amr_intr)
