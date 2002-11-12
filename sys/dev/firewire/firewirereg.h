3ed5018d3787f05ac802fe40d5374fadc7548f25
simokawa <simokawa@FreeBSD.org>
2002-11-12 15:22:19 +0000

    Split userland services to fwdev.c.

--- sys/dev/firewire/firewirereg.h
+++ sys/dev/firewire/firewirereg.h
@@ -313,6 +313,8 @@ void fw_xfer_timeout __P((void *));
 void fw_xfer_done __P((struct fw_xfer *));
 void fw_asy_callback __P((struct fw_xfer *));
 struct fw_device *fw_noderesolve __P((struct firewire_comm *, struct fw_eui64));
+struct fw_bind *fw_bindlookup __P((struct firewire_comm *, u_int32_t, u_int32_t));
+
 
 extern int firewire_debug;
 extern devclass_t firewire_devclass;
