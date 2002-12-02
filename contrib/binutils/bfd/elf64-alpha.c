6d3ced451f835fe9c1efa4c5e7aee7040179e82d
obrien <obrien@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-12-02 09:39:02 +0000

    Update HEAD with the stock files.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@107500 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/binutils/bfd/elf64-alpha.c
+++ contrib/binutils/bfd/elf64-alpha.c
@@ -19,8 +19,6 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-/* $FreeBSD$ */
-
 /* We need a published ABI spec for this.  Until one comes out, don't
    assume this'll remain unchanged forever.  */
 
