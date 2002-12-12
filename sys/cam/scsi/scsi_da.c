ff5e0540fc6dd9020dd8d22f458c92be7b211dee
yar <yar@FreeBSD.org>
2002-12-12 18:09:35 +0000

    Clear the "device open" flag in daopen() before returning a error, so the device won't stay marked as open whereas it isn't.
    Approved by:	re, njl
    MFC after:	1 week

--- sys/cam/scsi/scsi_da.c
+++ sys/cam/scsi/scsi_da.c
@@ -612,6 +612,7 @@ daopen(dev_t dev, int flags __unused, int fmt __unused, struct thread *td __unus
 		if ((softc->flags & DA_FLAG_PACK_REMOVABLE) != 0)
 			daprevent(periph, PR_PREVENT);
 	} else {
+		softc->flags &= ~DA_FLAG_OPEN;
 		cam_periph_release(periph);
 	}
 	cam_periph_unlock(periph);
