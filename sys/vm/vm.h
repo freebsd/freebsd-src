fa7dd9c5bc8e36e355cfe61a87e27779f539eef1
Matthew Dillon <dillon@FreeBSD.org>
2002-12-16 19:24:43 +0000

    Change the way ELF coredumps are handled.  Instead of unconditionally skipping read-only pages, which can result in valuable non-text-related data not getting dumped, the ELF loader and the dynamic loader now mark read-only text pages NOCORE and the coredump code only checks (primarily) for complete inaccessibility of the page or NOCORE being set.
    Certain applications which map large amounts of read-only data will
    produce much larger cores.  A new sysctl has been added,
    debug.elf_legacy_coredump, which will revert to the old behavior.
    
    This commit represents collaborative work by all parties involved.
    The PR contains a program demonstrating the problem.
    
    PR:		kern/45994
    Submitted by:	"Peter Edwards" <pmedwards@eircom.net>, Archie Cobbs <archie@dellroad.org>
    Reviewed by:	jdp, dillon
    MFC after:	7 days

--- sys/vm/vm.h
+++ sys/vm/vm.h
@@ -81,6 +81,7 @@ typedef u_char vm_prot_t;	/* protection codes */
 #define	VM_PROT_OVERRIDE_WRITE	((vm_prot_t) 0x08)	/* copy-on-write */
 
 #define	VM_PROT_ALL		(VM_PROT_READ|VM_PROT_WRITE|VM_PROT_EXECUTE)
+#define VM_PROT_RW		(VM_PROT_READ|VM_PROT_WRITE)
 #define	VM_PROT_DEFAULT		VM_PROT_ALL
 
 union vm_map_object;
