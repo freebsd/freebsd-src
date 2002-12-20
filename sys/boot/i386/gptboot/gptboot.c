caafad42bbbf503f1f0db4c093c9659f65738f8c
imp <imp@FreeBSD.org>
2002-12-20 05:49:40 +0000

    Put back the casts to unsigned.  While no strictly necessary for its current uses, the name strcmp has strong connotations that shouldn't lightly be discarded.  This doesn't cost us anything.
    Submitted by: bde

--- sys/boot/i386/gptboot/gptboot.c
+++ sys/boot/i386/gptboot/gptboot.c
@@ -133,7 +133,7 @@ static inline int
 strcmp(const char *s1, const char *s2)
 {
     for (; *s1 == *s2 && *s1; s1++, s2++);
-    return *s1 - *s2;
+    return (unsigned char)*s1 - (unsigned char)*s2;
 }
 
 #include "ufsread.c"
