e2742baf5bff87414504eab6b314e9706abe8676
dillon <dillon@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2002-12-28 21:15:39 +0000

    vm_pager_put_pages() takes VM_PAGER_* flags, not OBJPC_* flags.   It just so happens that OBJPC_SYNC has the same value as VM_PAGER_PUT_SYNC so no harm done.  But fix it :-)
    No operational changes.
    
    MFC after:	1 day
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@108361 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/vm/vm_pageout.c
+++ sys/vm/vm_pageout.c
@@ -392,7 +392,7 @@ vm_pageout_flush(mc, count, flags)
 	vm_object_pip_add(object, count);
 
 	vm_pager_put_pages(object, mc, count,
-	    (flags | ((object == kernel_object) ? OBJPC_SYNC : 0)),
+	    (flags | ((object == kernel_object) ? VM_PAGER_PUT_SYNC : 0)),
 	    pageout_status);
 
 	vm_page_lock_queues();
