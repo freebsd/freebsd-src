bec4c45c2f64a9ad30b0e63c51843a6a44de8027
cvs2svn <cvs2svn@FreeBSD.org>
2003-01-06 04:51:21 +0000

    This commit was manufactured by cvs2svn to create branch 'RELENG_4'.

---
+++ libexec/lukemftpd/nbsd2fbsd.h
@@ -0,0 +1,17 @@
+/* $FreeBSD$ */
+
+/* XXX: Depend on our system headers protecting against multiple includes. */
+#include <paths.h>
+#undef _PATH_FTPUSERS
+
+#include <pwd.h>
+
+#define _DIAGASSERT(x)
+
+#include <sys/_types.h>
+#ifndef _SIZE_T_DECLARED
+typedef	__size_t	size_t;
+#define	_SIZE_T_DECLARED
+#endif
+long long strsuftollx(const char *, const char *,
+    long long, long long, char *, size_t);
