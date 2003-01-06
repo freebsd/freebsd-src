8b8ec8afbd670d70dab8722d3f3fc6624bf11d9c
obrien <obrien@FreeBSD.org>
2003-01-06 04:33:47 +0000

    Add the POSIX sanctioned "LOGIN_NAME_MAX" -- Maximum length of a login name. Minimum Acceptable Value: _POSIX_LOGIN_NAME_MAX. The comments at the bottom of this file claim sysconf(3) provides this value, but it seems sysconf(3) hasn't implemented this yet.

--- sys/sys/syslimits.h
+++ sys/sys/syslimits.h
@@ -52,6 +52,7 @@
 #define	CHILD_MAX		   40	/* max simultaneous processes */
 #endif
 #define	LINK_MAX		32767	/* max file link count */
+#define	LOGIN_NAME_MAX	   17   /* max login name length (incl. NUL) */
 #define	MAX_CANON		  255	/* max bytes in term canon input line */
 #define	MAX_INPUT		  255	/* max bytes in terminal input */
 #define	NAME_MAX		  255	/* max bytes in a file name */
@@ -68,7 +69,6 @@
  * assume conservative values or call sysconf() to get the current value.
  *
  * HOST_NAME_MAX
- * LOGIN_NAME_MAX
  *
  * (We should do this for most of the values currently defined here,
  * but many programs are not prepared to deal with this yet.)
