9dfb70bec37b333f5cdef593b3459bb0d03ab005
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-01-06 01:01:08 +0000

    Explicitly have the timecounter init happen after the cpu_initclocks is called.  Otherwise (depending on a non-deterministic sort), the timecounter code can be initialized before the clock rate has been set (on ia64) and it assumes hz = 100, rather than the real value of 1024.  I'm not sure how much gets upset by this.
    Glanced at by:	phk
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@108755 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/kern_ntptime.c
+++ sys/kern/kern_ntptime.c
@@ -596,7 +596,7 @@ ntp_init()
 #endif /* PPS_SYNC */	   
 }
 
-SYSINIT(ntpclocks, SI_SUB_CLOCKS, SI_ORDER_FIRST, ntp_init, NULL)
+SYSINIT(ntpclocks, SI_SUB_CLOCKS, SI_ORDER_MIDDLE, ntp_init, NULL)
 
 /*
  * hardupdate() - local clock update
