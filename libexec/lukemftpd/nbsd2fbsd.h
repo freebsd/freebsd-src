6e45ea3d6e668cb0f7557adf841d044a83981f0a
obrien <obrien@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-01-06 04:09:20 +0000

    Need to prototype strsuftollx() to quiet a warning.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@108764 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- libexec/lukemftpd/nbsd2fbsd.h
+++ libexec/lukemftpd/nbsd2fbsd.h
@@ -9,3 +9,11 @@
 #define	LOGIN_NAME_MAX MAXLOGNAME	/* <sys/param.h> */
 
 #define _DIAGASSERT(x)
+
+#include <sys/_types.h>
+#ifndef _SIZE_T_DECLARED
+typedef	__size_t	size_t;
+#define	_SIZE_T_DECLARED
+#endif
+long long strsuftollx(const char *, const char *,
+    long long, long long, char *, size_t);
