6dacd14d2af60055bd417b738a24d4f0d4965081
Peter Grehan <grehan@FreeBSD.org>
2003-01-08 12:15:54 +0000

    Remove obsolete NFS_ROOT conditional.
    Approved by:  benno

--- sys/powerpc/powerpc/autoconf.c
+++ sys/powerpc/powerpc/autoconf.c
@@ -29,35 +29,16 @@ static const char rcsid[] =
   "$FreeBSD$";
 #endif
 
-#include "opt_bootp.h"
-#include "opt_nfs.h"
-#include "opt_nfsroot.h"
-
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/bus.h>
 #include <sys/cons.h>
 #include <sys/kernel.h>
 
+static device_t nexusdev;
+
 static void	configure(void *);
 SYSINIT(configure, SI_SUB_CONFIGURE, SI_ORDER_THIRD, configure, NULL)
-#ifdef	NFS_ROOT
-SYSINIT(cpu_rootconf, SI_SUB_ROOT_CONF, SI_ORDER_FIRST, cpu_rootconf, NULL)
-
-#ifndef	BOOTP_NFSROOT
-#error	"NFS_ROOT support not implemented for the non-BOOTP_NFSROOT case"
-#endif
-
-extern void	bootpc_init(void);
-
-void
-cpu_rootconf()
-{
-
-	bootpc_init();
-	rootdevnames[0] = "nfs:";
-}
-#endif
 
 /*
  * Determine i/o configuration for a machine.
@@ -65,8 +46,7 @@ cpu_rootconf()
 static void
 configure(void *dummy)
 {
-	device_add_child(root_bus, "nexus", 0);
-
+	nexusdev = device_add_child(root_bus, "nexus", 0);
 	root_bus_configure();
 
 	/*
