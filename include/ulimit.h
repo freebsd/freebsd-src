478b0bec6fa87815ff91e2e4449226c06907752e
tjr <tjr@FreeBSD.org>
2003-01-08 01:23:00 +0000

    MFC: Rename UL_GETSIZE to UL_GETFSIZE and UL_SETSIZE to UL_SETFSIZE. These are the names required by 1003.1-2001.
    Approved by:	re

--- include/ulimit.h
+++ include/ulimit.h
@@ -29,9 +29,13 @@
 #ifndef _ULIMIT_H_
 #define	_ULIMIT_H_
 
-#define	UL_GETSIZE	1
-#define	UL_SETSIZE	2
+#include <sys/cdefs.h>
 
+#define	UL_GETFSIZE	1
+#define	UL_SETFSIZE	2
+
+__BEGIN_DECLS
 long	ulimit(int, ...);
+__END_DECLS
 
 #endif /* !_ULIMIT_H_ */
