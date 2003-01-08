7da16554d89b9707a86d5dc32961130b83ec7842
Tim J. Robbins <tjr@FreeBSD.org>
2003-01-08 01:23:00 +0000

    MFC: Rename UL_GETSIZE to UL_GETFSIZE and UL_SETSIZE to UL_SETFSIZE. These are the names required by 1003.1-2001.
    Approved by:	re

--- lib/libc/gen/ulimit.c
+++ lib/libc/gen/ulimit.c
@@ -40,16 +40,16 @@ ulimit(int cmd, ...)
 {
 	struct rlimit limit;
 	va_list ap;
-	long arg, size;
+	long arg;
 
-	if (cmd == UL_GETSIZE) {
+	if (cmd == UL_GETFSIZE) {
 		if (getrlimit(RLIMIT_FSIZE, &limit) == -1)
 			return (-1);
 		limit.rlim_cur /= 512;
 		if (limit.rlim_cur > LONG_MAX)
 			return (LONG_MAX);
 		return ((long)limit.rlim_cur);
-	} else if (cmd == UL_SETSIZE) {
+	} else if (cmd == UL_SETFSIZE) {
 		va_start(ap, cmd);
 		arg = va_arg(ap, long);
 		va_end(ap);
