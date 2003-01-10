8745f988d9fd2c04286b96d1abc46152f5144c17
fjoe <fjoe@FreeBSD.org>
2003-01-10 15:41:17 +0000

    MFC: pthread_attr_get_np

--- include/pthread_np.h
+++ include/pthread_np.h
@@ -44,16 +44,17 @@ typedef void	(*pthread_switch_routine_t) __P((pthread_t, pthread_t));
  */
 __BEGIN_DECLS
 int pthread_attr_setcreatesuspend_np __P((pthread_attr_t *));
+int pthread_attr_get_np __P((pthread_t, pthread_attr_t *));
 int pthread_main_np __P((void));
 int pthread_multi_np __P((void));
+int pthread_mutexattr_getkind_np __P((pthread_mutexattr_t));
+int pthread_mutexattr_setkind_np __P((pthread_mutexattr_t *, int));
 void pthread_resume_all_np __P((void));
 int pthread_resume_np __P((pthread_t));
+void pthread_set_name_np __P((pthread_t, const char *));
 int pthread_single_np __P((void));
 void pthread_suspend_all_np __P((void));
 int pthread_suspend_np __P((pthread_t));
-int pthread_mutexattr_getkind_np __P((pthread_mutexattr_t));
-int pthread_mutexattr_setkind_np __P((pthread_mutexattr_t *, int));
-void pthread_set_name_np __P((pthread_t, const char *));
 int pthread_switch_add_np __P((pthread_switch_routine_t));
 int pthread_switch_delete_np __P((pthread_switch_routine_t));
 __END_DECLS
