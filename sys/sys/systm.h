40d242fa1f5f283e9f088deeb4939c7979ae7ca9
phk <phk@FreeBSD.org>
2003-01-15 19:18:49 +0000

    Allow linters to override the CTASSERT macro, since they are unlikely to like the results.

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -81,9 +81,11 @@ extern int maxusers;		/* system tune hint */
 #define	KASSERT(exp,msg)
 #endif
 
+#ifndef CTASSERT		/* Allow lint to override */
 #define	CTASSERT(x)		_CTASSERT(x, __LINE__)
 #define	_CTASSERT(x, y)		__CTASSERT(x, y)
 #define	__CTASSERT(x, y)	typedef char __assert ## y[(x) ? 1 : -1]
+#endif
 
 /*
  * XXX the hints declarations are even more misplaced than most declarations
