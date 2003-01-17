eb46c5a06694c964e8ae418d18d15e3aefe40e81
mike <mike@FreeBSD.org>
2003-01-17 13:39:32 +0000

    MFC:   Sync with head (rev 1.4 and 1.8 through 1.11).

--- lib/libc/string/strerror.c
+++ lib/libc/string/strerror.c
@@ -1,4 +1,4 @@
-/*
+/*-
  * Copyright (c) 1988, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
@@ -31,116 +31,66 @@
  * SUCH DAMAGE.
  */
 
-#if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strerror.c	8.1 (Berkeley) 6/4/93";
-#endif /* LIBC_SCCS and not lint */
-#ifndef lint
-static const char rcsid[] =
-  "$FreeBSD$";
-#endif
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
-#include <errno.h>
+
+#define	UPREFIX		"Unknown error: "
+
+/*
+ * Define a buffer size big enough to describe a 64-bit signed integer
+ * converted to ASCII decimal (19 bytes), with an optional leading sign
+ * (1 byte); finally, we get the prefix and a trailing NUL from UPREFIX.
+ */
+#define	EBUFSIZE	(20 + sizeof(UPREFIX))
+
+/*
+ * Doing this by hand instead of linking with stdio(3) avoids bloat for
+ * statically linked binaries.
+ */
+static void
+errstr(int num, char *buf, size_t len)
+{
+	char *t;
+	unsigned int uerr;
+	char tmp[EBUFSIZE];
+
+	t = tmp + sizeof(tmp);
+	*--t = '\0';
+	uerr = (num >= 0) ? num : -num;
+	do {
+		*--t = "0123456789"[uerr % 10];
+	} while (uerr /= 10);
+	if (num < 0)
+		*--t = '-';
+	strlcpy(buf, UPREFIX, len);
+	strlcat(buf, t, len);
+}
 
 int
 strerror_r(int errnum, char *strerrbuf, size_t buflen)
 {
-	int             len;
 
-	if ((errnum >= 0) && (errnum < sys_nerr)) {
-		len = strlcpy(strerrbuf, (char *)sys_errlist[errnum], buflen);
-		return ((len < buflen) ? 0 : ERANGE);
+	if (errnum < 1 || errnum >= sys_nerr) {
+		errstr(errnum, strerrbuf, buflen);
+		return (EINVAL);
 	}
-	return (EINVAL);
+	if (strlcpy(strerrbuf, sys_errlist[errnum], buflen) >= buflen)
+		return (ERANGE);
+	return (0);
 }
 
 char *
-strerror(num)
-	int             num;
+strerror(int num)
 {
-	char           *p, *t;
-	unsigned int	uerr;
-	static char const unknown_prefix[] = "Unknown error: ";
+	static char ebuf[EBUFSIZE];
 
-	/*
-	 * Define a buffer size big enough to describe a 64-bit
-	 * number in ASCII decimal (19), with optional leading sign
-	 * (+1) and trailing NUL (+1).
-	 */
-#       define		NUMLEN 21
-#	define		EBUFLEN (sizeof unknown_prefix + NUMLEN)
-	char            tmp[NUMLEN];	/* temporary number */
-	static char     ebuf[EBUFLEN];	/* error message */
-
-	if ((num >= 0) && (num < sys_nerr))
+	if (num > 0 && num < sys_nerr)
 		return ((char *)sys_errlist[num]);
-
-	/*
-	 * Set errno to EINVAL per P1003.1-200x Draft June 14, 2001.
-	 */
 	errno = EINVAL;
-
-	/*
-	 * Print unknown errno by hand so we don't link to stdio(3).
-	 * This collects the ASCII digits in reverse order.
-	 */
-	uerr = (num > 0) ? num : -num;
-	t = tmp;
-	do {
-		*t++ = "0123456789"[uerr % 10];
-	} while (uerr /= 10);
-	if (num < 0)
-		*t++ = '-';
-
-	/*
-	 * Copy the "unknown" message and the number into the caller
-	 * supplied buffer, inverting the number string.
-	 */
-	strcpy(ebuf, unknown_prefix);
-	for (p = ebuf + sizeof unknown_prefix - 1; t >= tmp; )
-		*p++ = *--t;
-	*p = '\0';
+	errstr(num, ebuf, sizeof(ebuf));
 	return (ebuf);
 }
-
-#ifdef STANDALONE_TEST
-
-#include <limits.h>
-
-main()
-{
-	char            mybuf[64];
-	int             ret;
-
-	errno = 0;
-
-	printf("strerror(0) yeilds: %s\n", strerror(0));
-	printf("strerror(1) yeilds: %s\n", strerror(1));
-	printf("strerror(47) yeilds: %s\n", strerror(47));
-	printf("strerror(sys_nerr - 1) yeilds: %s\n", strerror(sys_nerr - 1));
-	printf("errno = %d\n", errno); errno = 0;
-
-	printf("strerror(sys_nerr) yeilds: %s\n", strerror(sys_nerr));
-	printf("errno = %d\n", errno);  errno = 0;
-
-	printf("strerror(437) yeilds: %s\n", strerror(437));
-	printf("errno = %d\n", errno);  errno = 0;
-
-	printf("strerror(LONG_MAX) yeilds: %s\n", strerror(LONG_MAX));
-	printf("strerror(LONG_MIN) yeilds: %s\n", strerror(LONG_MIN));
-	printf("strerror(ULONG_MAX) yeilds: %s\n", strerror(ULONG_MAX));
-
-	memset(mybuf, '*', 63); mybuf[63] = '\0';
-	strerror_r(11, mybuf, 64);
-	printf("strerror_r(11) yeilds: %s\n", mybuf);
-
-	memset(mybuf, '*', 63); mybuf[63] = '\0';
-	ret = strerror_r(1234, mybuf, 64);
-	printf("strerror_r(1234) returns %d (%s)\n", ret, mybuf);
-
-	memset(mybuf, '*', 63); mybuf[63] = '\0';
-	ret = strerror_r(1, mybuf, 10);
-	printf("strerror_r on short buffer returns %d (%s)\n", ret, mybuf);
-}
-#endif
