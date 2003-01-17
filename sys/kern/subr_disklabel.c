443424d074394c0db48e238fc847920f71429fda
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-01-17 19:37:55 +0000

    Move alpha_fix_srm_checksum() from subr_diskmbr.c to subr_disklabel.c
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@109442 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/subr_disklabel.c
+++ sys/kern/subr_disklabel.c
@@ -281,3 +281,19 @@ bad:
         return(-1);
 }
 
+#ifdef __alpha__
+void
+alpha_fix_srm_checksum(bp)
+	struct buf *bp;
+{
+	u_int64_t *p;
+	u_int64_t sum;
+	int i;
+
+	p = (u_int64_t *) bp->b_data;
+	sum = 0;
+	for (i = 0; i < 63; i++)
+		sum += p[i];
+	p[63] = sum;
+}
+#endif
