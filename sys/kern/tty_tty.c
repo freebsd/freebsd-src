ec2c4225ce642562d746e69d531d328e1ebabc6f
Poul-Henning Kamp <phk@FreeBSD.org>
2003-01-19 10:23:47 +0000

    When we use DEVFS, we don't need the /dev/tty pseudo-driver to do more than return ENXIO from its open routine, so most of this file is unneeded.
    A straight #ifdef'ing would look quite messy, and make the file
    quite unreadable, so instead I have simply added the DEVFS version
    of the file at the top, protected by #ifndef NODEVFS.
    
    Once we have removed NODEVFS option, we can retain 86 the 86 lines at
    the top and drop the other 287 lines.

--- sys/kern/tty_tty.c
+++ sys/kern/tty_tty.c
@@ -1,3 +1,91 @@
+#ifndef NODEVFS
+/*-
+ * Copyright (c) 2003 Poul-Henning Kamp.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/conf.h>
+#include <sys/kernel.h>
+#include <sys/proc.h>
+#include <sys/vnode.h>
+
+static	d_open_t	cttyopen;
+
+#define	CDEV_MAJOR	1
+
+static struct cdevsw ctty_cdevsw = {
+	/* open */	cttyopen,
+	/* close */	nullclose,
+	/* read */	noread,
+	/* write */	nowrite,
+	/* ioctl */	noioctl,
+	/* poll */	nopoll,
+	/* mmap */	nommap,
+	/* strategy */	nostrategy,
+	/* name */	"ctty",
+	/* maj */	CDEV_MAJOR,
+	/* dump */	nodump,
+	/* psize */	nopsize,
+	/* flags */	D_TTY,
+};
+
+static dev_t ctty;
+
+static	int
+cttyopen(dev_t dev, int flag, int mode, struct thread *td)
+{
+
+	return (ENXIO);
+}
+
+static void
+ctty_clone(void *arg, char *name, int namelen, dev_t *dev)
+{
+
+	if (*dev != NODEV)
+		return;
+	if (strcmp(name, "tty"))
+		return;
+	if (curthread->td_proc->p_flag & P_CONTROLT)
+		*dev = curthread->td_proc->p_session->s_ttyvp->v_rdev;
+	else
+		*dev = ctty;
+}
+
+static void
+ctty_drvinit(void *unused)
+{
+
+	EVENTHANDLER_REGISTER(dev_clone, ctty_clone, 0, 1000);
+	ctty = make_dev(&ctty_cdevsw, 0, 0, 0, 0666, "ctty");
+}
+
+SYSINIT(cttydev,SI_SUB_DRIVERS,SI_ORDER_MIDDLE+CDEV_MAJOR,ctty_drvinit,NULL)
+#else
 /*-
  * Copyright (c) 1982, 1986, 1991, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -282,3 +370,4 @@ ctty_drvinit(unused)
 }
 
 SYSINIT(cttydev,SI_SUB_DRIVERS,SI_ORDER_MIDDLE+CDEV_MAJOR,ctty_drvinit,NULL)
+#endif
