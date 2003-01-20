85b8fbcaccab06eeb204fabc39cb2918ae186138
gibbs <gibbs@FreeBSD.org>
2003-01-20 18:05:46 +0000

    Add definitions for the task management codes sent in SPI4 command information units.

--- sys/cam/scsi/scsi_iu.h
+++ sys/cam/scsi/scsi_iu.h
@@ -29,4 +29,12 @@ struct scsi_status_iu_header
     (12 + (((siu)->flags & SIU_RSPVALID)		\
 	? scsi_4btoul((siu)->pkt_failures_length)	\
 	: 0))
+
+#define	SIU_TASKMGMT_NONE		0x00
+#define	SIU_TASKMGMT_ABORT_TASK		0x01
+#define	SIU_TASKMGMT_ABORT_TASK_SET	0x02
+#define	SIU_TASKMGMT_CLEAR_TASK_SET	0x04
+#define	SIU_TASKMGMT_LUN_RESET		0x08
+#define	SIU_TASKMGMT_TARGET_RESET	0x20
+#define	SIU_TASKMGMT_CLEAR_ACA		0x40
 #endif /*_SCSI_SCSI_IU_H*/
