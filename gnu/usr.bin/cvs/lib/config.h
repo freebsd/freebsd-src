91843f7ea08ebc87c06cc1c46d539410640081f6
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-01-21 22:04:20 +0000

    Update for cvs-1.11.5 import (note to self: gotta fix the version tags)
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@109663 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- gnu/usr.bin/cvs/lib/config.h
+++ gnu/usr.bin/cvs/lib/config.h
@@ -364,13 +364,13 @@
 #define PACKAGE_NAME "Concurrent Versions System (CVS)"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Concurrent Versions System (CVS) 1.11.2.1-20021201-FreeBSD"
+#define PACKAGE_STRING "Concurrent Versions System (CVS) 1.11.5-FreeBSD"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "cvs"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.11.2.1-20021201-FreeBSD"
+#define PACKAGE_VERSION "1.11.5-FreeBSD"
 
 /* Path to the pr utility */
 #define PR_PROGRAM "/usr/bin/pr"
