84860c33f05f7c738f3a1c4ced4a489e065d1f84
Dag-Erling Smørgrav <des@FreeBSD.org>
2003-01-22 14:12:59 +0000

    Force early initialization of the resolver library, since the resolver configuration files will no longer be available once sshd is chrooted.
    PR:		39953, 40894
    Submitted by:	dinoex
    MFC after:	3 days

--- crypto/openssh/sshd.c
+++ crypto/openssh/sshd.c
@@ -54,6 +54,10 @@ RCSID("$FreeBSD$");
 #include <prot.h>
 #endif
 
+#ifdef __FreeBSD__
+#include <resolv.h>
+#endif
+
 #include "ssh.h"
 #include "ssh1.h"
 #include "ssh2.h"
@@ -1417,6 +1421,17 @@ main(int ac, char **av)
 	    setsockopt(sock_in, SOL_SOCKET, SO_KEEPALIVE, &on,
 	    sizeof(on)) < 0)
 		error("setsockopt SO_KEEPALIVE: %.100s", strerror(errno));
+
+#ifdef __FreeBSD__
+	/*
+	 * Initialize the resolver.  This may not happen automatically
+	 * before privsep chroot().                                   
+	 */
+	if ((_res.options & RES_INIT) == 0) {
+		debug("res_init()");         
+		res_init();         
+	}
+#endif
 
 	/*
 	 * Register our connection.  This turns encryption off because we do
