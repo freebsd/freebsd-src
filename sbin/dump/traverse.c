b1b7e72b438fda1020962b0198905969ac0161e8
Matthew Dillon <dillon@FreeBSD.org>
2003-01-25 18:54:59 +0000

    MFC caching code for dump.

--- sbin/dump/traverse.c
+++ sbin/dump/traverse.c
@@ -601,8 +601,8 @@ bread(blkno, buf, size)
 	int cnt, i;
 
 loop:
-	if ((cnt = pread(diskfd, buf, size, ((off_t)blkno << dev_bshift))) ==
-						size)
+	cnt = cread(diskfd, buf, size, ((off_t)blkno << dev_bshift));
+	if (cnt == size)
 		return;
 	if (blkno + (size / dev_bsize) > fsbtodb(sblock, sblock->fs_size)) {
 		/*
@@ -636,7 +636,8 @@ loop:
 			breaderrors = 0;
 	}
 	/*
-	 * Zero buffer, then try to read each sector of buffer separately.
+	 * Zero buffer, then try to read each sector of buffer separately,
+	 * and bypass the cache.
 	 */
 	memset(buf, 0, size);
 	for (i = 0; i < size; i += dev_bsize, buf += dev_bsize, blkno++) {
