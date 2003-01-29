46bec929dffb1073217a95ee89b9f96b91d79ce0
Doug Barton <dougb@FreeBSD.org>
2003-01-29 02:52:22 +0000

    MFC of version 8.3.4 import

--- contrib/bind/bin/named/ns_ncache.c
+++ contrib/bind/bin/named/ns_ncache.c
@@ -1,5 +1,5 @@
 #if !defined(lint) && !defined(SABER)
-static const char rcsid[] = "$Id: ns_ncache.c,v 8.29 2001/06/18 14:43:16 marka Exp $";
+static const char rcsid[] = "$Id: ns_ncache.c,v 8.29.4.1 2002/11/14 13:41:31 marka Exp $";
 #endif /* not lint */
 
 /*
