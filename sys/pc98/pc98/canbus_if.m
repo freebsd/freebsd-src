6a060b5fc48dad5e3095f593a6c4c04f944f86dd
nyan <nyan@FreeBSD.org>
2003-02-04 16:17:13 +0000

    Oops, fix copyright again.
    Noticed by:	hrs

--- sys/pc98/pc98/canbus_if.m
+++ sys/pc98/pc98/canbus_if.m
@@ -1,5 +1,6 @@
 #
 # Copyright (c) 2002 KIYOHARA Takashi <kiyohara@kk.iij4u.or.jp>
+# All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
