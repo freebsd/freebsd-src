8418379ae54515c66d16bc2caecd67f5c8db5d0f
Maxim Sobolev <sobomax@FreeBSD.org>
2003-02-06 13:17:11 +0000

    MFC: add support for IC Book Labs Dreadnought x16 Lite.

--- sys/dev/puc/pucdata.c
+++ sys/dev/puc/pucdata.c
@@ -919,5 +919,28 @@ const struct puc_device_description puc_devices[] = {
 	    },
 	},
 
+	{   "IC Book Labs Dreadnought x16 Lite",
+	    {	0xb00c, 0x091c, 0,	0	},
+	    {   0xffff, 0xffff, 0,      0       },
+	    {
+		{ PUC_PORT_TYPE_COM, 0x10, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x08, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x10, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x18, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x20, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x28, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x30, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x38, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x40, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x48, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x50, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x58, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x60, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x68, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x70, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x78, COM_FREQ },
+	    },
+	},
+
 	{ 0 }
 };
--- sys/dev/puc/pucvar.h
+++ sys/dev/puc/pucvar.h
@@ -64,7 +64,7 @@
  * Author: Christopher G. Demetriou, May 14, 1998.
  */
 
-#define	PUC_MAX_PORTS		12
+#define	PUC_MAX_PORTS		16
 
 struct puc_device_description {
 	const char	*name;
