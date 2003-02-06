6134e2a910778f2381f6b197ca69cb87d4433622
rwatson <rwatson@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-02-06 01:55:40 +0000

    Relocate a call to enable inetd so that it is set regardless of whether the user chooses to edit inetd.conf.
    PR:	39311
    Reported by:	Martin Faxer <gmh003532@brfmasthugget.se>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@110431 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/sysinstall/config.c
+++ usr.sbin/sysinstall/config.c
@@ -941,7 +941,7 @@ configInetd(dialogMenuItem *self)
         variable_set2("inetd_enable", "NO", 1);
     } else {
         /* If inetd is enabled, we'll need an inetd.conf */
-
+        variable_set2("inetd_enable", "YES", 1);
 	if (!msgYesNo("inetd(8) relies on its configuration file, /etc/inetd.conf, to determine\n"
                    "which of its Internet services will be available.  The default FreeBSD\n"
                    "inetd.conf(5) leaves all services disabled by default, so they must be\n"
@@ -953,7 +953,6 @@ configInetd(dialogMenuItem *self)
             sprintf(cmd, "%s /etc/inetd.conf", variable_get(VAR_EDITOR));
             dialog_clear();
             systemExecute(cmd);
-            variable_set2("inetd_enable", "YES", 1);
 	}
     }
     restorescr(w);
