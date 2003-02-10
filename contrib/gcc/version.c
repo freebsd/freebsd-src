6d114ccf45540d1cf2731009cf3ad2d347715fbc
Alexander Kabaev <kan@FreeBSD.org>
2003-02-10 06:03:47 +0000

    Update for Gcc 3.2.2 release.

--- contrib/gcc/version.c
+++ contrib/gcc/version.c
@@ -3,4 +3,4 @@
 #include "ansidecl.h"
 #include "version.h"
 
-const char *const version_string = "3.2.1 [FreeBSD] 20021119 (release)";
+const char *const version_string = "3.2.2 [FreeBSD] 20030205 (release)";
