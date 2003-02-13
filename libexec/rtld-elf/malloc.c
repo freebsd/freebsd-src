92b0ec083202bad8b737ec44069c52da0bfddbaa
Alexander Kabaev <kan@FreeBSD.org>
2003-02-13 17:35:00 +0000

    Add missing include files I forgot about in previous commit.

--- libexec/rtld-elf/malloc.c
+++ libexec/rtld-elf/malloc.c
@@ -50,6 +50,8 @@ static char *rcsid = "$FreeBSD$";
 #include <sys/types.h>
 #include <err.h>
 #include <paths.h>
+#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
