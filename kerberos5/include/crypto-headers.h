897ff42ad87b2b1382bb69539cbb6380e4103131
nectar <nectar@FreeBSD.org>
2003-02-13 20:15:26 +0000

    MFC 1.2: Define OPENSSL_DES_LIBDES_COMPATIBILITY.

--- kerberos5/include/crypto-headers.h
+++ kerberos5/include/crypto-headers.h
@@ -1,6 +1,7 @@
 /* $FreeBSD$ */
 #ifndef __crypto_headers_h__
 #define __crypto_headers_h__
+#define OPENSSL_DES_LIBDES_COMPATIBILITY
 #include <openssl/des.h>
 #include <openssl/rc4.h>
 #include <openssl/md4.h>
