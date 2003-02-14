fc26c5dc65675b63e7edaef1fd4fe3172f742fbe
tjr <tjr@FreeBSD.org>
2003-02-14 09:12:12 +0000

    Lock proc while manipulating p_sigmask p_sigignore and p_siglist.

--- sys/netsmb/smb_subr.c
+++ sys/netsmb/smb_subr.c
@@ -75,11 +75,15 @@ smb_proc_intr(struct proc *p)
 
 	if (p == NULL)
 		return 0;
+	PROC_LOCK(p);
 	tmpset = p->p_siglist;
 	SIGSETNAND(tmpset, p->p_sigmask);
 	SIGSETNAND(tmpset, p->p_sigignore);
-	if (SIGNOTEMPTY(p->p_siglist) && SMB_SIGMASK(tmpset))
+	if (SIGNOTEMPTY(p->p_siglist) && SMB_SIGMASK(tmpset)) {
+		PROC_UNLOCK(p);
                 return EINTR;
+	}
+	PROC_UNLOCK(p);
 	return 0;
 }
 
