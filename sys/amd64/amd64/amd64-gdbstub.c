5d80f8f84b2e36b72f7a133e23179ec3f9bab94d
phk <phk@FreeBSD.org>
2003-02-16 19:22:21 +0000

    Change "dev_t gdbdev" to "void *gdb_arg", some possible paths for GDB will not have a dev_t.

--- sys/amd64/amd64/amd64-gdbstub.c
+++ sys/amd64/amd64/amd64-gdbstub.c
@@ -146,18 +146,18 @@ strcpy (char *dst, const char *src)
 static int
 putDebugChar (int c)		/* write a single character      */
 {
-  if (gdbdev == NODEV)
+  if (gdb_arg == NULL)
 	return 0;
-  (*gdb_putc)(gdbdev, c);
+  (*gdb_putc)(gdb_arg, c);
   return 1;
 }
 
 static int
 getDebugChar (void)		/* read and return a single char */
 {
-  if (gdbdev == NODEV)
+  if (gdb_arg == NULL)
 	return -1;
-  return (*gdb_getc)(gdbdev);
+  return (*gdb_getc)(gdb_arg);
 }
 
 static const char hexchars[]="0123456789abcdef";
@@ -487,7 +487,7 @@ gdb_handle_exception (db_regs_t *raw_regs, int type, int code)
 
   while (1)
     {
-      if (gdbdev == NODEV)
+      if (gdb_arg == NULL)
 	return 1;		/* somebody has removed the gdb device */
       remcomOutBuffer[0] = 0;
 
