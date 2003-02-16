2a7c6634afa5c6ce5f395f3ed26aa3a309a575d2
brueffer <brueffer@FreeBSD.org>
2003-02-16 10:02:42 +0000

    MFC:  Various spelling and grammar fixes
    PR:		44312
    Submitted by:	Chris Pepper <pepper@rockefeller.edu>
    Approved by:	ceri (mentor)

--- share/examples/ipfw/change_rules.sh
+++ share/examples/ipfw/change_rules.sh
@@ -30,7 +30,7 @@
 #
 # Invoke this script to edit ${firewall_script}. It will call ${EDITOR},
 # or vi(1) if the environment variable is not set, for you to edit
-# ${firewall_script}, asks for confirmation and then run
+# ${firewall_script}, ask for confirmation, and then run
 # ${firewall_script}. You can then examine the output of ipfw list and
 # confirm whether you want the new version or not.
 #
@@ -39,14 +39,14 @@
 # flush is present in it).
 #
 # If the new rules are confirmed, they'll replace ${firewall_script} and
-# the previous ones will be copied to ${firewall_script}.{date}. A mail
-# will also be sent to root with the unified diffs of the rule change.
+# the previous ones will be copied to ${firewall_script}.{date}. Mail
+# will also be sent to root with a unified diff of the rule change.
 #
-# Non-approved rules are kept in ${firewall_script}.new, and you are
+# Unapproved rules are kept in ${firewall_script}.new, and you are
 # offered the option of changing them instead of the present rules when
 # you call this script.
 #
-# It is suggested improving this script by using some version control
+# This script could be improved by using version control
 # software.
 
 if [ -r /etc/defaults/rc.conf ]; then
@@ -121,8 +121,8 @@ get_yes_no "Do you want to install the new rules"
 
 cat <<!
 The rules will be changed now. If the message 'Type y to keep the new
-rules' do not appear on the screen or the y key is not pressed in 30
-seconds, the former rules will be restored.
+rules' does not appear on the screen or the y key is not pressed in 30
+seconds, the original rules will be restored.
 The TCP/IP connections might be broken during the change. If so, restore
 the ssh/telnet connection being used.
 !
@@ -144,7 +144,7 @@ DATE=`date "+%Y%m%d%H%M"`
 cp ${edit_file} ${edit_file}.$DATE
 mv ${edit_file}.new ${edit_file} 
 cat <<!
-The new rules are now default. The previous rules have been preserved in
+The new rules are now installed. The previous rules have been preserved in
 the file ${edit_file}.$DATE
 !
 diff -F "^# .*[A-Za-z]" -u ${edit_file}.$DATE ${edit_file} \
