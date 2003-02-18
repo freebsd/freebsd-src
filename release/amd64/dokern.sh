5a09e21c81cfc81710400dd04a13d546565164b2
Makoto Matsushita <matusita@FreeBSD.org>
2003-02-18 12:09:20 +0000

    Yet another kernel diet: remove aacp and _KPOSIX_PRIORITY_SCHEDULING.
    aacp is a passthrough driver for aac, but it seems that aac kernel
    module has a feature provided by aacp; so it can be removed safely.
    
    _KPOSIX_PRIORITY_SCHEDULING provides P1003.1B realtime extension.
    However, in an installation phase, it seems that it helps a little
    for us, so we can remove this option from a kernel for floppy installation.
    
    I know _KPOSIX_PRIORITY_SCHEDULING option is defined in other architecture.
    However, I don't touch them at this time; I can't test it.
    
    Anyway here's result.
    
    	Before diet:
    	-rwxr-xr-x  1 matusita  matusita  4849883 Feb 18 11:22 kernel
    	-rwxr-xr-x  1 matusita  matusita  1727143 Feb 18 11:47 kernel.kgz
    
    	After diet:
    	-rwxr-xr-x  1 matusita  matusita  4840949 Feb 18 09:48 kernel
    	-rwxr-xr-x  1 matusita  matusita  1723911 Feb 18 11:47 kernel.kgz
    
    We've got extra 3232 bytes (using 5-current as of Feb/18/2003).
    
    In cooperation with:	jwd (test ISO installation image)
    Boot tested on:		several PCs around myself
    Installation tested on:	VMware Workstation e.x.p build-4099

--- release/amd64/dokern.sh
+++ release/amd64/dokern.sh
@@ -21,6 +21,8 @@ sed	\
 	-e '/UFS_ACL/d' \
 	-e '/UFS_DIRHASH/d' \
 	-e '/WITNESS/d' \
+	-e '/_KPOSIX_PRIORITY_SCHEDULING/d' \
+	-e '/	aacp	/d' \
 	-e '/	atapist	/d' \
 	-e '/	faith	/d' \
 	-e '/	gif	/d' \
