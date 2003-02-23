07d2563244c3d0476906da8fabed50515aaa1bdd
Tor Egge <tegge@FreeBSD.org>
2003-02-23 23:49:57 +0000

    Allow machines with one CPU and a valid mp table to boot an SMP kernel.

--- sys/amd64/amd64/mptable.c
+++ sys/amd64/amd64/mptable.c
@@ -2727,6 +2727,8 @@ static void
 release_aps(void *dummy __unused)
 {
 
+	if (mp_ncpus == 1) 
+		return;
 	mtx_lock_spin(&sched_lock);
 	atomic_store_rel_int(&aps_ready, 1);
 	while (smp_started == 0)
