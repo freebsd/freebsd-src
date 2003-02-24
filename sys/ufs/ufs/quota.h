9cdb2d4d9d590a2a24bf7a304ca67f65daad98a6
David Schultz <das@FreeBSD.org>
2003-02-24 08:49:59 +0000

    Expand the reference count on struct dquot to 32 bits. This fixes a panic on large systems where a single user may have more than 64K active or inactive vnodes.
    PR:		48234
    Reviewed by:	mike (mentor)

--- sys/ufs/ufs/quota.h
+++ sys/ufs/ufs/quota.h
@@ -122,9 +122,8 @@ struct dquot {
 	LIST_ENTRY(dquot) dq_hash;	/* hash list */
 	TAILQ_ENTRY(dquot) dq_freelist;	/* free list */
 	u_int16_t dq_flags;		/* flags, see below */
-	u_int16_t dq_cnt;		/* count of active references */
-	u_int16_t dq_spare;		/* unused spare padding */
 	u_int16_t dq_type;		/* quota type of this dquot */
+	u_int32_t dq_cnt;		/* count of active references */
 	u_int32_t dq_id;		/* identifier this applies to */
 	struct	ufsmount *dq_ump;	/* filesystem that this is taken from */
 	struct	dqblk dq_dqb;		/* actual usage & quotas */
