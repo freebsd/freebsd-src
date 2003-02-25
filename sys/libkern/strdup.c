354d43abf30325d0059644e2b7bfe2d194041c99
Robert Watson <rwatson@FreeBSD.org>
2003-02-25 22:11:39 +0000

    Pass a malloc type into the libkern strdup() implementation explicitly, so that callers can specify what malloc pool the resulting memory should come from.
    Requested by:	phk

--- sys/libkern/strdup.c
+++ sys/libkern/strdup.c
@@ -42,13 +42,13 @@
 MALLOC_DEFINE(M_STRING, "string", "string buffers");
 
 char *
-strdup(const char *string)
+strdup(const char *string, struct malloc_type *type)
 {
 	size_t len;
 	char *copy;
 
 	len = strlen(string) + 1;
-	copy = malloc(len, M_STRING, M_WAITOK);
+	copy = malloc(len, type, M_WAITOK);
 	bcopy(string, copy, len);
 	return (copy);
 }
