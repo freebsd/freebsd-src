7d819be55789099285aa8b8d940c2203a750872a
mike <mike@FreeBSD.org>
2003-02-26 20:16:58 +0000

    Move the typedef for size_t into _iovec.h, so that size_t is available for struct iovec.

--- sys/sys/_iovec.h
+++ sys/sys/_iovec.h
@@ -37,6 +37,13 @@
 #ifndef _SYS__IOVEC_H_
 #define	_SYS__IOVEC_H_
 
+#include <sys/_types.h>
+
+#ifndef _SIZE_T_DECLARED
+typedef	__size_t	size_t;
+#define	_SIZE_T_DECLARED
+#endif
+
 struct iovec {
 	void	*iov_base;	/* Base address. */
 	size_t	 iov_len;	/* Length. */
