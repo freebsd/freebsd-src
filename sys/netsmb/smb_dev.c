62624ba817e49186d9e1d93f5f7b352610dde30b
tjr <tjr@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-02-27 14:35:21 +0000

    Use noread(), nowrite() and nopoll() instead of our own stub functions.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@111621 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netsmb/smb_dev.c
+++ sys/netsmb/smb_dev.c
@@ -66,10 +66,7 @@
 
 static d_open_t	 nsmb_dev_open;
 static d_close_t nsmb_dev_close;
-static d_read_t	 nsmb_dev_read;
-static d_write_t nsmb_dev_write;
 static d_ioctl_t nsmb_dev_ioctl;
-static d_poll_t	 nsmb_dev_poll;
 
 MODULE_DEPEND(netsmb, libiconv, 1, 1, 1);
 MODULE_VERSION(netsmb, NSMB_VERSION);
@@ -90,10 +87,10 @@ int smb_dev_queue(struct smb_dev *ndp, struct smb_rq *rqp, int prio);
 static struct cdevsw nsmb_cdevsw = {
 	/* open */	nsmb_dev_open,
 	/* close */	nsmb_dev_close,
-	/* read */	nsmb_dev_read,
-	/* write */	nsmb_dev_write,
+	/* read */	noread,
+	/* write */	nowrite,
 	/* ioctl */ 	nsmb_dev_ioctl,
-	/* poll */	nsmb_dev_poll,
+	/* poll */	nopoll,
 	/* mmap */	nommap,
 	/* strategy */	nostrategy,
 	/* name */	NSMB_NAME,
@@ -331,24 +328,6 @@ nsmb_dev_ioctl(dev_t dev, u_long cmd, caddr_t data, int flag, struct thread *td)
 		error = ENODEV;
 	}
 	return error;
-}
-
-static int
-nsmb_dev_read(dev_t dev, struct uio *uio, int flag)
-{
-	return EACCES;
-}
-
-static int
-nsmb_dev_write(dev_t dev, struct uio *uio, int flag)
-{
-	return EACCES;
-}
-
-static int
-nsmb_dev_poll(dev_t dev, int events, struct thread *td)
-{
-	return ENODEV;
 }
 
 static int
