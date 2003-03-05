13faa2f505a98d47bd00da5e52bf40fb79855345
dougb <dougb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-03-05 08:20:55 +0000

    Better handle the recent (desirable) change to create login.conf.db at install time, and make sure to catch any future .db files as well.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@111901 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/mergemaster/mergemaster.sh
+++ usr.sbin/mergemaster/mergemaster.sh
@@ -554,11 +554,11 @@ case "${RERUN}" in
   ;; # End of the "RERUN" test
 esac
 
-# We really don't want to have to deal with these files, since
-# master.passwd is the real file that should be compared, then
-# the user should run pwd_mkdb if necessary.
+# We really don't want to have to deal with files like login.conf.db, pwd.db,
+# or spwd.db.  Instead, we want to compare the text versions, and run *_mkdb.
+# Prompt the user to do so below, as needed.
 #
-rm -f ${TEMPROOT}/etc/spwd.db ${TEMPROOT}/etc/passwd ${TEMPROOT}/etc/pwd.db
+rm -f ${TEMPROOT}/etc/*.db
 
 # We only need to compare things like freebsd.cf once
 find ${TEMPROOT}/usr/obj -type f -delete 2>/dev/null
