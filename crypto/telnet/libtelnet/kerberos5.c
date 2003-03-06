c4f823a8ba441f68ef368cee31f9b5a6b8bba410
nectar <nectar@FreeBSD.org>
2003-03-06 13:41:53 +0000

    Unbreak Kerberos 5 authentication in telnet. (Credential forwarding is still broken.)
    PR:	bin/45397

--- crypto/telnet/libtelnet/kerberos5.c
+++ crypto/telnet/libtelnet/kerberos5.c
@@ -192,6 +192,7 @@ kerberos5_send(const char *name, Authenticator *ap)
 	ap_opts = AP_OPTS_MUTUAL_REQUIRED;
     else
 	ap_opts = 0;
+    ap_opts |= AP_OPTS_USE_SUBKEY;
     
     ret = krb5_auth_con_init (context, &auth_context);
     if (ret) {
@@ -406,6 +407,29 @@ kerberos5_is(Authenticator *ap, unsigned char *data, int cnt)
 		printf("Kerberos V5: "
 		       "krb5_auth_con_getremotesubkey failed (%s)\r\n",
 		       krb5_get_err_text(context, ret));
+	    return;
+	}
+
+	if (key_block == NULL) {
+	    ret = krb5_auth_con_getkey(context,
+				       auth_context,
+				       &key_block);
+	}
+	if (ret) {
+	    Data(ap, KRB_REJECT, "krb5_auth_con_getkey failed", -1);
+	    auth_finished(ap, AUTH_REJECT);
+	    if (auth_debug_mode)
+		printf("Kerberos V5: "
+		       "krb5_auth_con_getkey failed (%s)\r\n",
+		       krb5_get_err_text(context, ret));
+	    return;
+	}
+	if (key_block == NULL) {
+	    Data(ap, KRB_REJECT, "no subkey received", -1);
+	    auth_finished(ap, AUTH_REJECT);
+	    if (auth_debug_mode)
+		printf("Kerberos V5: "
+		       "krb5_auth_con_getremotesubkey returned NULL key\r\n");
 	    return;
 	}
 
