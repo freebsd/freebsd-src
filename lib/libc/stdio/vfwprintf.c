e96823870762a9077147025a0c13bf6ac6acb9f7
tjr <tjr@FreeBSD.org>
2003-03-14 04:46:02 +0000

    Unexpand RCS tag.

--- lib/libc/stdio/vfwprintf.c
+++ lib/libc/stdio/vfwprintf.c
@@ -39,7 +39,7 @@
 #if defined(LIBC_SCCS) && !defined(lint)
 static char sccsid[] = "@(#)vfprintf.c	8.1 (Berkeley) 6/4/93";
 #endif /* LIBC_SCCS and not lint */
-__FBSDID("$FreeBSD$");
+__FBSDID("FreeBSD: src/lib/libc/stdio/vfprintf.c,v 1.51 2003/03/12 20:30:00 das Exp");
 #endif
 __FBSDID("$FreeBSD$");
 
