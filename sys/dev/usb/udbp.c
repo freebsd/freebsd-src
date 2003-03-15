869c12cbb472ef6f384a9ab1b03b51f56b792b10
Takanori Watanabe <takawata@FreeBSD.org>
2003-03-15 13:03:30 +0000

    Add module dependency. This makes udbp.ko to be loaded.

--- sys/dev/usb/udbp.c
+++ sys/dev/usb/udbp.c
@@ -606,6 +606,7 @@ udbp_out_transfer_cb(usbd_xfer_handle xfer, usbd_private_handle priv,
 }
 
 DRIVER_MODULE(udbp, uhub, udbp_driver, udbp_devclass, usbd_driver_load, 0);
+MODULE_DEPEND(udbp, netgraph, NG_ABI_VERSION, NG_ABI_VERSION, NG_ABI_VERSION);
 
 
 /***********************************************************************
