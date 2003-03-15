4a623e073c76d51c33a3f7474b38c023fd3efe0b
phk <phk@FreeBSD.org>
2003-03-15 10:33:32 +0000

    Add a devstat_start_transaction_bio() to match the devstat_end_transaction_bio() we already have.
    For now it just calls devstat_start_transaction(), but that will change
    shortly.

--- sys/sys/devicestat.h
+++ sys/sys/devicestat.h
@@ -225,6 +225,7 @@ struct devstat *devstat_new_entry(const char *dev_name,
 
 void devstat_remove_entry(struct devstat *ds);
 void devstat_start_transaction(struct devstat *ds);
+void devstat_start_transaction_bio(struct devstat *ds, struct bio *bp);
 void devstat_end_transaction(struct devstat *ds, u_int32_t bytes, 
 			     devstat_tag_type tag_type,
 			     devstat_trans_flags flags);
