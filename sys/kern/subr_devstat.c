ce68d8fa2273a645135a8305c067ea2b9522f000
jake <jake@FreeBSD.org>
2003-03-18 23:32:27 +0000

    long != int.  Use SYSCTL_UINT for kern.devstat.generation.  Fixes booting on sparc64.

--- sys/kern/subr_devstat.c
+++ sys/kern/subr_devstat.c
@@ -420,7 +420,7 @@ SYSCTL_PROC(_kern_devstat, OID_AUTO, all, CTLFLAG_RD|CTLTYPE_OPAQUE,
  */
 SYSCTL_INT(_kern_devstat, OID_AUTO, numdevs, CTLFLAG_RD, 
     &devstat_num_devs, 0, "Number of devices in the devstat list");
-SYSCTL_LONG(_kern_devstat, OID_AUTO, generation, CTLFLAG_RD,
+SYSCTL_UINT(_kern_devstat, OID_AUTO, generation, CTLFLAG_RD,
     &devstat_generation, 0, "Devstat list generation");
 SYSCTL_INT(_kern_devstat, OID_AUTO, version, CTLFLAG_RD, 
     &devstat_version, 0, "Devstat list version number");
