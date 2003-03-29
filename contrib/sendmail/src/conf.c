8fc3aa82d855d98a02dfb948d5a296e3f1bac3c7
Gregory Neil Shapiro <gshapiro@FreeBSD.org>
2003-03-29 20:13:35 +0000

    sendmail parsing buffer overflow fix Advisory number to be filled in later
    Approved by:	so (nectar)

--- contrib/sendmail/src/conf.c
+++ contrib/sendmail/src/conf.c
@@ -320,6 +320,8 @@ setdefaults(e)
 	DontLockReadFiles = TRUE;
 	DoubleBounceAddr = "postmaster";
 	MaxHeadersLength = MAXHDRSLEN;
+	MaxMimeHeaderLength = MAXLINE;
+	MaxMimeFieldLength = MaxMimeHeaderLength / 2;
 	MaxForwardEntries = 0;
 #if SASL
 	AuthMechanisms = newstr(AUTH_MECHANISMS);
