6fd9b4689e4be2c63d40e9e64fefaa112ccef20e
murray <murray@FreeBSD.org>
2003-03-30 20:45:02 +0000

    4.8 should look for packages in the packages-4.8-release directory.
    Approved by:	re

--- usr.sbin/pkg_install/add/main.c
+++ usr.sbin/pkg_install/add/main.c
@@ -59,6 +59,7 @@ struct {
 	{ 460000, 460001, "/packages-4.6-release" },
 	{ 460002, 460099, "/packages-4.6.2-release" },
 	{ 470000, 470099, "/packages-4.7-release" },
+	{ 480000, 480099, "/packages-4.8-release" },
 	{ 300000, 399000, "/packages-3-stable" },
 	{ 400000, 499000, "/packages-4-stable" },
 	{ 510000, 599000, "/packages-5-stable" },
