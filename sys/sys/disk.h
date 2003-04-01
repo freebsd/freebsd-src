812de2ed5c8c0680d58707ef6b9081a02e714afb
Poul-Henning Kamp <phk@FreeBSD.org>
2003-04-01 18:55:04 +0000

    I think the divorce successed, so stop #including <geom/geom_disk.h>

--- sys/sys/disk.h
+++ sys/sys/disk.h
@@ -21,8 +21,7 @@
 #include <sys/conf.h>	/* XXX: temporary to avoid breakage */
 #endif
 
-#include <geom/geom_disk.h>
-
+struct disk;
 struct disk *disk_enumerate(struct disk *disk);
 void disk_err(struct bio *bp, const char *what, int blkdone, int nl);
 
