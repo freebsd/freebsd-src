448bd6543c1bf7a2fc1e0b799bf5ea0ea1e5b57e
Ruslan Ermilov <ru@FreeBSD.org>
2003-04-04 11:58:05 +0000

    MFC: 1.44: Don't reserve free space so as to not confuse people about >100%.

--- release/scripts/doFS.sh
+++ release/scripts/doFS.sh
@@ -55,7 +55,7 @@ dofs_vn () {
 
 	vnconfig -s labels -c /dev/r${VNDEVICE} ${FSIMG}
 	disklabel -w -B ${BOOT1} ${BOOT2} ${VNDEVICE} ${FSLABEL}
-	newfs -i ${FSINODE} -o space -m 1 /dev/r${VNDEVICE}c
+	newfs -i ${FSINODE} -o space -m 0 /dev/r${VNDEVICE}c
 
 	mount /dev/${VNDEVICE}c ${MNT}
 
