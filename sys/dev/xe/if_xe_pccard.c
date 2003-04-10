f5746231ff7652f580d3f8f8ecec3f5fe47f1bdb
Warner Losh <imp@FreeBSD.org>
2003-04-10 04:36:02 +0000

    Make sure that pp_name is non-null before setting the device description.  This allows us to rely entirely on the CIS entries if necessary...

--- sys/dev/xe/if_xe_pccard.c
+++ sys/dev/xe/if_xe_pccard.c
@@ -328,7 +328,8 @@ xe_pccard_match(device_t dev)
 
 	if ((pp = pccard_product_lookup(dev, xe_pccard_products,
 	     sizeof(xe_pccard_products[0]), NULL)) != NULL) {
-		device_set_desc(dev, pp->pp_name);
+		if (pp->pp_name != NULL)
+			device_set_desc(dev, pp->pp_name);
 		return (0);
 	}
 	return (EIO);
