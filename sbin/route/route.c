af0cc424b0c504f029c504a0f4e1f12b6d961efb
ru <ru@FreeBSD.org>
2003-04-16 12:06:53 +0000

    There is an undocument feature that ``route -vnd flush'' prints a nice dump of the entire routing table.  Allow non-root users to see it too.

--- sbin/route/route.c
+++ sbin/route/route.c
@@ -213,7 +213,7 @@ flushroutes(argc, argv)
 	char *buf, *next, *lim;
 	struct rt_msghdr *rtm;
 
-	if (uid) {
+	if (uid && !debugonly) {
 		errx(EX_NOPERM, "must be root to alter routing table");
 	}
 	shutdown(s, 0); /* Don't want to read back our messages */
