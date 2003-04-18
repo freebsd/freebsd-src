691d3715d636a88215f08d9d13abaa72e9499e53
bde <bde@FreeBSD.org>
2003-04-18 18:59:34 +0000

    Backed out rev.1.57.  This restores format checking functions like err() again.  All known err() format errors in src that developed while format checking was broken have been fixed.  Tested on i386, alpha, ia64.

--- sys/sys/cdefs.h
+++ sys/sys/cdefs.h
@@ -189,7 +189,7 @@
 #endif
 
 /* Compiler-dependent macros that rely on FreeBSD-specific extensions. */
-#if __FreeBSD_cc_version >= 300001 && __FreeBSD_cc_version < 500003
+#if __FreeBSD_cc_version >= 300001
 #define	__printf0like(fmtarg, firstvararg) \
 	    __attribute__((__format__ (__printf0__, fmtarg, firstvararg)))
 #else
