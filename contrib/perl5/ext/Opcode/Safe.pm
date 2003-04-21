dffb944b7ade28dbfed6e7aa1f26bd812b6e4d7a
Anton Berezin <tobez@FreeBSD.org>
2003-04-21 15:06:03 +0000

    Patch Safe.pm (see http://use.perl.org/articles/02/10/06/1118222.shtml?tid=5).
    Reported by:	kuriyama

--- contrib/perl5/ext/Opcode/Safe.pm
+++ contrib/perl5/ext/Opcode/Safe.pm
@@ -214,7 +214,7 @@ sub reval {
     # Create anon sub ref in root of compartment.
     # Uses a closure (on $expr) to pass in the code to be executed.
     # (eval on one line to keep line numbers as expected by caller)
-	my $evalcode = sprintf('package %s; sub { eval $expr; }', $root);
+	my $evalcode = sprintf('package %s; sub { @_ = (); eval $expr; }', $root);
     my $evalsub;
 
 	if ($strict) { use strict; $evalsub = eval $evalcode; }
@@ -228,7 +228,7 @@ sub rdo {
     my $root = $obj->{Root};
 
     my $evalsub = eval
-	    sprintf('package %s; sub { do $file }', $root);
+	    sprintf('package %s; sub { @_ = (); do $file }', $root);
     return Opcode::_safe_call_sv($root, $obj->{Mask}, $evalsub);
 }
 
