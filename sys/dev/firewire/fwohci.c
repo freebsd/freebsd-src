a2da26fc1ddd285f750b43317d2a61deb284f8a4
Hidetoshi Shimokawa <simokawa@FreeBSD.org>
2003-04-24 15:27:06 +0000

    Fix printf warnings for i386 with PAE.
    Submitted by: harti

--- sys/dev/firewire/fwohci.c
+++ sys/dev/firewire/fwohci.c
@@ -2303,11 +2303,11 @@ print_db(struct fwohcidb_tr *db_tr, volatile struct fwohcidb *db,
 		key = cmd & OHCI_KEY_MASK;
 		stat = res >> OHCI_STATUS_SHIFT;
 #if __FreeBSD_version >= 500000
-		printf("%08tx %s %s %s %s %5d %08x %08x %04x:%04x",
+		printf("%08jx %s %s %s %s %5d %08x %08x %04x:%04x",
 #else
 		printf("%08x %s %s %s %s %5d %08x %08x %04x:%04x",
 #endif
-				db_tr->bus_addr,
+				(uintmax_t)db_tr->bus_addr,
 				dbcode[(cmd >> 28) & 0xf],
 				dbkey[(cmd >> 24) & 0x7],
 				dbcond[(cmd >> 20) & 0x3],
