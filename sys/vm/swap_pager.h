a91c48e613cf209fb1336657a242255480ae5b4e
alc <alc@FreeBSD.org>
2003-04-24 05:29:27 +0000

    - Move swap_pager_isswapped()'s prototype to a more logical place.

--- sys/vm/swap_pager.h
+++ sys/vm/swap_pager.h
@@ -120,6 +120,7 @@ void swap_pager_copy(vm_object_t, vm_object_t, vm_pindex_t, int);
 void swap_pager_freespace(vm_object_t, vm_pindex_t, vm_size_t);
 void swap_pager_dmzspace(vm_object_t, vm_pindex_t, vm_size_t);
 void swap_pager_swap_init(void);
+int swap_pager_isswapped(vm_object_t, int);
 int swap_pager_reserve(vm_object_t, vm_pindex_t, vm_size_t);
 
 /*
