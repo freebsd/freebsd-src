287365fffac637c5d27dd2065c34f9ec8f92713a
Alexander Kabaev <kan@FreeBSD.org>
2003-04-28 22:40:05 +0000

    Add definitions for WCHAR_MIN and WCHAR_MAX.

--- include/wchar.h
+++ include/wchar.h
@@ -69,6 +69,7 @@
 
 #include <sys/cdefs.h>
 #include <sys/_types.h>
+#include <machine/_limits.h>
 
 #ifndef NULL
 #define	NULL	0
@@ -94,6 +95,11 @@ typedef	__wchar_t	wchar_t;
 #ifndef _WINT_T_DECLARED
 typedef	__wint_t	wint_t;
 #define	_WINT_T_DECLARED
+#endif
+
+#ifndef	WCHAR_MIN 
+#define	WCHAR_MIN	__INT_MIN
+#define	WCHAR_MAX	__INT_MAX
 #endif
 
 #ifndef WEOF
