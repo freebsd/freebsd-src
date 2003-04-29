352960dc386c750379c8367544a9c5ca5abdb557
kan <kan@FreeBSD.org>
2003-04-29 18:10:45 +0000

    Enable wchar_t support in libstdc++.

--- gnu/lib/libstdc++/config.h
+++ gnu/lib/libstdc++/config.h
@@ -16,7 +16,7 @@
 #define _GLIBCPP_USE_C99 1
 
 // Define if code specialized for wchar_t should be used.
-/* #undef _GLIBCPP_USE_WCHAR_T */
+#define _GLIBCPP_USE_WCHAR_T 1
 
 // Define if using setrlimit to limit memory usage during 'make check'.
 /* #undef _GLIBCPP_MEM_LIMITS */
