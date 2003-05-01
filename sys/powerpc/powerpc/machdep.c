161af19be7451c59b209446cf4bd7e6e156df051
Peter Wemm <peter@FreeBSD.org>
2003-05-01 03:33:28 +0000

    Back out last commits.  The elf64/elf32 kernel name thing was more pain than it was worth.

--- sys/powerpc/powerpc/machdep.c
+++ sys/powerpc/powerpc/machdep.c
@@ -267,9 +267,7 @@ powerpc_init(u_int startkernel, u_int endkernel, u_int basekernel, void *mdp)
 	 */
 	if (mdp != NULL) {
 		preload_metadata = mdp;
-		kmdp = preload_search_by_type("elf32 kernel");
-		if (kmdp == NULL)
-			kmdp = preload_search_by_type("elf kernel");
+		kmdp = preload_search_by_type("elf kernel");
 		if (kmdp != NULL) {
 			boothowto = MD_FETCH(kmdp, MODINFOMD_HOWTO, int);
 			kern_envp = MD_FETCH(kmdp, MODINFOMD_ENVP, char *);
