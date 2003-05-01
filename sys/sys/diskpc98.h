4b4582a84cbd3fd8ed51b8b84eca661e5af6263b
Yoshihiro Takahashi <nyan@FreeBSD.org>
2003-05-01 14:40:16 +0000

    Remove DIOCGPC98 ioctl.

--- sys/sys/diskpc98.h
+++ sys/sys/diskpc98.h
@@ -69,7 +69,6 @@ CTASSERT(sizeof (struct pc98_partition) == 32);
 void pc98_partition_dec(void const *pp, struct pc98_partition *d);
 void pc98_partition_enc(void *pp, struct pc98_partition *d);
 
-#define DIOCGPC98	_IOR('M', 128, u_char[8192])
 #define DIOCSPC98	_IOW('M', 129, u_char[8192])
 
 #endif /* !_SYS_DISKPC98_H_ */
