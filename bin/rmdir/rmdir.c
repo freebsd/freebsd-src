09a80d4867b85b3df9856d150eb565232dc900d4
David E. O'Brien <obrien@FreeBSD.org>
2003-05-01 16:58:57 +0000

    Quiet warnings about copyright[].

--- bin/rmdir/rmdir.c
+++ bin/rmdir/rmdir.c
@@ -31,6 +31,7 @@
  * SUCH DAMAGE.
  */
 
+#if 0
 #ifndef lint
 static char const copyright[] =
 "@(#) Copyright (c) 1992, 1993, 1994\n\
@@ -38,10 +39,9 @@ static char const copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-#if 0
 static char sccsid[] = "@(#)rmdir.c	8.3 (Berkeley) 4/2/94";
-#endif
 #endif /* not lint */
+#endif
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
