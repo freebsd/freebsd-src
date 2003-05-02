71ffff1b8cd475a1fb23f8333787784f6e88a95c
obrien <obrien@FreeBSD.org>
2003-05-02 06:49:10 +0000

    Use __FBSDID.

--- sbin/camcontrol/modeedit.c
+++ sbin/camcontrol/modeedit.c
@@ -26,10 +26,8 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef lint
-static const char rcsid[] =
-  "$FreeBSD$";
-#endif /* not lint */
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #include <sys/queue.h>
 #include <sys/types.h>
