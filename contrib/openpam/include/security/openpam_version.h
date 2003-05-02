1b2b5956a02e1d56168e372dc96807d70b3ca363
des <des@FreeBSD.org>
2003-05-02 15:08:06 +0000

    Vendor import of OpenPAM Dianthus.

--- contrib/openpam/include/security/openpam_version.h
+++ contrib/openpam/include/security/openpam_version.h
@@ -31,14 +31,14 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $P4: //depot/projects/openpam/include/security/openpam_version.h#6 $
+ * $P4: //depot/projects/openpam/include/security/openpam_version.h#7 $
  */
 
 #ifndef _OPENPAM_VERSION_H_INCLUDED
 #define _OPENPAM_VERSION_H_INCLUDED
 
 #define _OPENPAM
-#define _OPENPAM_VERSION	20020630
-#define _OPENPAM_RELEASE	"Citronella"
+#define _OPENPAM_VERSION	20030502
+#define _OPENPAM_RELEASE	"Dianthus"
 
 #endif
