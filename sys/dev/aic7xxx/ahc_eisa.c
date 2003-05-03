4fe74dd7c3b1bcfd5c4aed34fc82f227d1941173
gibbs <gibbs@FreeBSD.org>
2003-05-03 23:27:57 +0000

    hc_eisa.c: ahc_pci.c: ahd_pci.c:         Don't bogusly use the BUS_DMA_ALLOCNOW flag in our psuedo         parent bus dma tag.

--- sys/dev/aic7xxx/ahc_eisa.c
+++ sys/dev/aic7xxx/ahc_eisa.c
@@ -26,7 +26,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: //depot/aic7xxx/freebsd/dev/aic7xxx/ahc_eisa.c#10 $
+ * $Id: //depot/aic7xxx/freebsd/dev/aic7xxx/ahc_eisa.c#11 $
  *
  * $FreeBSD$
  */
@@ -139,7 +139,7 @@ aic7770_attach(device_t dev)
 				   /*maxsize*/BUS_SPACE_MAXSIZE_32BIT,
 				   /*nsegments*/AHC_NSEG,
 				   /*maxsegsz*/AHC_MAXTRANSFER_SIZE,
-				   /*flags*/BUS_DMA_ALLOCNOW,
+				   /*flags*/0,
 				   &ahc->parent_dmat);
 
 	if (error != 0) {
