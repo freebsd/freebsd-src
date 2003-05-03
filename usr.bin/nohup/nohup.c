2a8f1d64a1061d10f0cb8255e350e74fa4365d3b
obrien <obrien@FreeBSD.org>
2003-05-03 19:44:46 +0000

    Make GCC 3.3 happy with rcsid[], sccs[], and copyright[].

--- usr.bin/nohup/nohup.c
+++ usr.bin/nohup/nohup.c
@@ -31,6 +31,7 @@
  * SUCH DAMAGE.
  */
 
+#if 0
 #ifndef lint
 static const char copyright[] =
 "@(#) Copyright (c) 1989, 1993\n\
@@ -38,12 +39,11 @@ static const char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-#if 0
 static char sccsid[] = "@(#)nohup.c	8.1 (Berkeley) 6/6/93";
-#endif
-static const char rcsid[] =
-  "$FreeBSD$";
 #endif /* not lint */
+#endif
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
 #include <sys/stat.h>
