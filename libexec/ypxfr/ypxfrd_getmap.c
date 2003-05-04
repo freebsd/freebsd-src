e6de895e83d4c3c11d4bc532885a573965e5be0e
obrien <obrien@FreeBSD.org>
2003-05-04 00:59:13 +0000

    Use __FBSDID vs. rcsid[].

--- libexec/ypxfr/ypxfrd_getmap.c
+++ libexec/ypxfr/ypxfrd_getmap.c
@@ -30,10 +30,8 @@
  * SUCH DAMAGE.
  */
 
-#ifndef lint
-static const char rcsid[] =
-  "$FreeBSD$";
-#endif /* not lint */
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #include <errno.h>
 #include <stdlib.h>
