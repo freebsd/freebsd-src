587ffa4508f56f999fe330a12e89b70e35640e38
Robert Watson <rwatson@FreeBSD.org>
2003-05-05 15:12:51 +0000

    Clean up proc locking in procfs: make sure the proc lock is held before entering sys_process.c debugging primitives, or we violate assertions. Also, be more careful about releasing the process lock around calls to uiomove() which may sleep waiting for paging machinations or related notions.  We may want to defer the uiomove() in at least one case, but jhb will look into that at a later date.
    Reported by:	Philippe Charnier <charnier@xp11.frmug.org>
    Reviewed by:	jhb

--- sys/fs/procfs/procfs_regs.c
+++ sys/fs/procfs/procfs_regs.c
@@ -76,15 +76,16 @@ procfs_doprocregs(PFS_FILL_ARGS)
 		kl = uio->uio_resid;
 
 	_PHOLD(p);
-	PROC_UNLOCK(p);
 	if (kl < 0)
 		error = EINVAL;
 	else
 		/* XXXKSE: */
 		error = proc_read_regs(FIRST_THREAD_IN_PROC(p), &r);
-	if (error == 0)
+	if (error == 0) {
+		PROC_UNLOCK(p);
 		error = uiomove(kv, kl, uio);
-	PROC_LOCK(p);
+		PROC_LOCK(p);
+	}
 	if (error == 0 && uio->uio_rw == UIO_WRITE) {
 		if (!P_SHOULDSTOP(p))
 			error = EBUSY;
