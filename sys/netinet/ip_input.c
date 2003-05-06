b072dbe0433a483db2a11a9e10d36d8b1844c346
rwatson <rwatson@FreeBSD.org>
2003-05-06 20:34:04 +0000

    Trim a call to mac_create_mbuf_from_mbuf() since m_tag meta-data copying for mbuf headers now works properly in m_dup_pkthdr(), so we don't need to do an explicit copy.
    Approved by:	re (jhb)
    Obtained from:	TrustedBSD Project
    Sponsored by:	DARPA, Network Associates Laboratories

--- sys/netinet/ip_input.c
+++ sys/netinet/ip_input.c
@@ -1797,13 +1797,10 @@ ip_forward(struct mbuf *m, int srcrt, struct sockaddr_in *next_hop)
 		mcopy->m_len = imin((ip->ip_hl << 2) + 8,
 		    (int)ip->ip_len);
 		m_copydata(m, 0, mcopy->m_len, mtod(mcopy, caddr_t));
-#ifdef MAC
 		/*
-		 * XXXMAC: This will eventually become an explicit
-		 * labeling point.
+		 * XXXMAC: Eventually, we may have an explict labeling
+		 * point here.
 		 */
-		mac_create_mbuf_from_mbuf(m, mcopy);
-#endif
 	}
 
 #ifdef IPSTEALTH
