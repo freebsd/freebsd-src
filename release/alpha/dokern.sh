d60c24aa24653f6465879a4ec817f7bfead98f52
obrien <obrien@FreeBSD.org>
2003-05-07 18:12:38 +0000

    Back out rev 1.63.  Things in this file should be in the same order as in GENERIC.  This greatly simplifies the hunting for the next thing to kill.
    Approved by:	re(blanket as alpha re-builder)

--- release/alpha/dokern.sh
+++ release/alpha/dokern.sh
@@ -3,58 +3,62 @@
 # $FreeBSD$
 #
 
+#	XXX sort by order in GENERIC, not alphabetical
+
 sed	\
-	-e '/DDB/d' \
+	-e 's/ident.*GENERIC/ident		BOOTMFS/g'
 	-e '/DEBUG/d' \
 	-e '/DEC_KN8AE/d' \
-	-e '/INVARIANTS/d' \
-	-e '/INVARIANT_SUPPORT/d' \
-	-e '/KTRACE/d' \
-	-e '/MSDOSFS/d' \
-	-e '/NFSSERVER/d' \
-	-e '/NFS_ROOT/d' \
-	-e '/PROCFS/d' \
-	-e '/PSEUDOFS/d' \
-	-e '/SMP/d' \
 	-e '/SOFTUPDATES/d' \
-	-e '/SYSV/d' \
 	-e '/UFS_ACL/d' \
 	-e '/UFS_DIRHASH/d' \
+	-e '/NFSSERVER/d' \
+	-e '/NFS_ROOT/d' \
+	-e '/MSDOSFS/d' \
+	-e '/PROCFS/d' \
+	-e '/PSEUDOFS/d' \
+	-e '/KTRACE/d' \
+	-e '/SYSV/d' \
 	-e '/_KPOSIX_PRIORITY_SCHEDULING/d' \
+	-e '/DDB/d' \
+	-e '/INVARIANTS/d' \
+	-e '/INVARIANT_SUPPORT/d' \
+	-e '/WITNESS/d' \
+	-e '/SMP/d' \
 	-e '/	atapifd	/d' \
 	-e '/	atapist	/d' \
-	-e '/	aue	/d' \
 	-e '/	ch	/d' \
-	-e '/	cue	/d' \
-	-e '/	faith	/d' \
-	-e '/	gif	/d' \
-	-e '/	kue	/d' \
-	-e '/	lpt	/d' \
-	-e '/	ohci	/d' \
 	-e '/	pass	/d' \
-	-e '/	ppbus	/d' \
-	-e '/	ppc$/d' \
-	-e '/	ppi	/d' \
-	-e '/	ppp	/d' \
-	-e '/	pty	/d' \
-	-e '/	random	/d' \
 	-e '/	sa	/d' \
 	-e '/	ses	/d' \
+	-e '/	splash	/d' \
+	-e '/	ppc$/d' \
+	-e '/	ppbus	/d' \
+	-e '/	lpt	/d' \
+	-e '/	ppi	/d' \
 	-e '/	sf	/d' \
 	-e '/	sis	/d' \
-	-e '/	sl	/d' \
-	-e '/	splash	/d' \
 	-e '/	ste	/d' \
-	-e '/	ugen	/d' \
+	-e '/	wb	/d' \
+	-e '/	random	/d' \
+	-e '/	sl	/d' \
+	-e '/	ppp	/d' \
+	-e '/	pty	/d' \
+	-e '/	gif	/d' \
+	-e '/	faith	/d' \
 	-e '/	uhci	/d' \
+	-e '/	ohci	/d' \
+	-e '/	usb	/d' \
+	-e '/	ugen	/d' \
 	-e '/	uhid	/d' \
 	-e '/	ukbd	/d' \
 	-e '/	ulpt	/d' \
 	-e '/	umass	/d' \
 	-e '/	ums	/d' \
-	-e '/	usb	/d' \
-	-e '/	wb	/d' \
-	-e 's/ident.*GENERIC/ident		BOOTMFS/g'
+	-e '/	aue	/d' \
+	-e '/	axe	/d' \
+	-e '/	cue	/d' \
+	-e '/	kue	/d' \
 
 echo "options  SCSI_NO_OP_STRINGS" 
 echo "options  SCSI_NO_SENSE_STRINGS"
