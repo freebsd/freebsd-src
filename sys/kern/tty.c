00084d3dc9f13a914ef37f147b4084001f389371
ps <ps@FreeBSD.org>
2003-05-14 00:03:55 +0000

    p_sigignore moved into struct sigacts.  move one which was missed.
    Approved by:	re (scottl)

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -1601,7 +1601,7 @@ loop:
 		splx(s);
 		sx_slock(&proctree_lock);
 		PROC_LOCK(p);
-		if (SIGISMEMBER(p->p_sigignore, SIGTTIN) ||
+		if (SIGISMEMBER(p->p_sigacts->ps_sigignore, SIGTTIN) ||
 		    SIGISMEMBER(td->td_sigmask, SIGTTIN) ||
 		    (p->p_flag & P_PPWAIT) || p->p_pgrp->pg_jobc == 0) {
 			PROC_UNLOCK(p);
