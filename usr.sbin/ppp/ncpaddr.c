c3206cf50f7cdf7256347c8482b6e4339445b729
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-05-25 07:39:06 +0000

    Add a pretty cheesy hack to avoid a gcc-3.2.2 ICE (internal compiler error) on amd64 when doing pointer subtraction.  This bug is already fixed in gcc-3.3 (waiting for after the branch), and the hack will be backed out at the first opportunity.  This is in the ipv6 code path.
    Approved by:  re (scottl)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@115303 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/ppp/ncpaddr.c
+++ usr.sbin/ppp/ncpaddr.c
@@ -142,16 +142,16 @@ mask62bits(const struct in6_addr *mask)
 {
   const u_char masks[] = { 0x00, 0x80, 0xc0, 0xe0, 0xf0, 0xf8, 0xfc, 0xfe };
   const u_char *c, *p, *end;
-  int masklen;
+  int masklen, m;
 
   p = (const u_char *)mask;
   for (masklen = 0, end = p + 16; p < end && *p == 0xff; p++)
     masklen += 8;
 
   if (p < end) {
-    for (c = masks; c < masks + sizeof masks; c++)
+    for (c = masks, m = 0; c < masks + sizeof masks; c++, m++)
       if (*c == *p) {
-        masklen += c - masks;
+        masklen += m;
         break;
       }
   }
