04ed7210a37e83beb7d927d1dc33f94366761ebb
gibbs <gibbs@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-05-26 21:24:01 +0000

    Correct/Simplify ignore wide residue message handling
    aic7xxx.c:
    	In ahc_handle_ign_wide_residue():
    	o Use SCB_XFERLEN_ODD SCB field to determine transfer
    	  "oddness" rather than the DATA_COUNT_ODD logic.
    	  SCB_XFERLEN_ODD is toggled on every ignore wide
    	  residue message so that multiple ignore wide residue
    	  messages for the same transaction are properly supported.
    	o If the sg list has been exausted, the sequencer
    	  doesn't bother to update the residual data count
    	  since it is known to be zero.  Perform the zeroing
    	  manually before calculating the remaining data count.
    	o Ensure that SG_LIST_NULL is cleared in the
    	  residual sg pointer for "mid-transfer" ignore
    	  wide residue cases.
    	o Use multibyte in/out macros instead of shifting/masking
    	  by hand.
    
    aic7xxx.h:
    	Modify the SCB_GET_LUN() macro to mask the lun hardware
    	SCB field with LID.  This leaves two bits in the LUN
    	field that can be used for other purposes.
    
    aic7xxx.reg:
    	Change LID to be 0x3F.  This is the maximum supported
    	lun size for non-packetized SCSI.  Map the top bit
    	of the lun to SCB_XFERLEN_ODD.  The host must set
    	this bit whenever a transfer is an odd length.
    
    	Remove the ODD_SEG bit field that was used to carry the odd
    	transfer length information through the SG cache.  This
    	is obviated by SCB_XFERLEN_ODD field.
    
    	Remove the DATA_COUNT_ODD scratch ram byte that was used
    	dynamicaly compute data transfer oddness.  This is obviated
    	by SCB_XFERLEN_ODD field.
    
    aic7xxx.seq:
    	Be more careful in our handling of the SCB_LUN field.  It
    	must be masked with LID if only lun information is desired.
    
    	Remove all updates to the DATA_COUNT_ODD scratch ram field.
    	Remove all uses of ODD_SEG.  These two save quite a few
    	sequencer instructions.
    
    	Use SCB_XFERLEN_ODD to validate the end of transfer
    	ignore wide residue message case.
    
    aic7xxx_inline.h:
    	In ahc_queue_scb(), setup the SCB_XFERLEN_ODD field.
    
    Approved by: RE
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@115333 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/aic7xxx/aic7xxx.h
+++ sys/dev/aic7xxx/aic7xxx.h
@@ -37,7 +37,7 @@
  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGES.
  *
- * $Id: //depot/aic7xxx/aic7xxx/aic7xxx.h#76 $
+ * $Id: //depot/aic7xxx/aic7xxx/aic7xxx.h#77 $
  *
  * $FreeBSD$
  */
@@ -93,7 +93,7 @@ struct seeprom_descriptor;
 #define	SCB_GET_CHANNEL(ahc, scb) \
 	SCSIID_CHANNEL(ahc, (scb)->hscb->scsiid)
 #define	SCB_GET_LUN(scb) \
-	((scb)->hscb->lun)
+	((scb)->hscb->lun & LID)
 #define SCB_GET_TARGET_OFFSET(ahc, scb)	\
 	(SCB_GET_TARGET(ahc, scb) + (SCB_IS_SCSIBUS_B(ahc, scb) ? 8 : 0))
 #define SCB_GET_TARGET_MASK(ahc, scb) \
