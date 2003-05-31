16cef37ea3229048c2a8fc2af65dc706c352200c
Poul-Henning Kamp <phk@FreeBSD.org>
2003-05-31 19:38:43 +0000

    Don't define memcpy() to bcopy() and in particular not memset() to bzero() when we have them all already.
    Found by:       FlexeLint

--- sys/dev/usb/usb_port.h
+++ sys/dev/usb/usb_port.h
@@ -361,9 +361,6 @@ typedef struct thread *usb_proc_ptr;
 
 #define uio_procp uio_td
 
-/* XXX Change this when FreeBSD has memset */
-#define	memcpy(d, s, l)		bcopy((s),(d),(l))
-#define	memset(d, v, l)		bzero((d),(l))
 #define usb_kthread_create1(f, s, p, a0, a1) \
 		kthread_create((f), (s), (p), RFHIGHPID, 0, (a0), (a1))
 #define usb_kthread_create2(f, s, p, a0) \
