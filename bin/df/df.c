c481992ef8165f0d0f0766173e9be210447bf558
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-06-02 22:33:12 +0000

    From the df man page: 	-t Only print out statistics for filesystems of the specified types.
    Make the behavior of df(1) conform to its man page (behavior is otherwise
    undefined).
    
    Submitted by:	Rob Braun <bbraun@apple.com>
    Obtained from:	Apple
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@115744 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- bin/df/df.c
+++ bin/df/df.c
@@ -280,6 +280,15 @@ main(int argc, char *argv[])
 			rv = 1;
 			continue;
 		}
+		/* Check to make sure the arguments we've been
+		 * given are satisfied.  Return an error if we
+		 * have been asked to list a mount point that does
+		 * not match the other args we've been given (-l, -t, etc.)
+		 */
+		if (checkvfsname(statfsbuf.f_fstypename, vfslist)) {
+			rv++;
+			continue;
+		}
 		if (argc == 1) {
 			bzero(&maxwidths, sizeof(maxwidths));
 			update_maxwidths(&maxwidths, &statfsbuf);
