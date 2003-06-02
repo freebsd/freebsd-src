c01ad0a85e6269cfd9a419e7c0eb3f71a96404cf
rwatson <rwatson@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-06-02 17:21:38 +0000

    Use mac_biba_label_copy() and mac_mls_label_copy() to implement the mpo_copy_mbuf_label() entry point for Biba and MLS, respectively. Otherwise, labels in m_tags may not be properly propagated across some classes of mbuf operations.  This problem caused these policies to fail-stop the system with a panic.
    Obtained from:	TrustedBSD Project
    Sponsored by:	DARPA, Network Associates Laboratories
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@115707 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/security/mac_mls/mac_mls.c
+++ sys/security/mac_mls/mac_mls.c
@@ -2393,6 +2393,7 @@ static struct mac_policy_ops mac_mls_ops =
 	.mpo_destroy_socket_label = mac_mls_destroy_label,
 	.mpo_destroy_socket_peer_label = mac_mls_destroy_label,
 	.mpo_destroy_vnode_label = mac_mls_destroy_label,
+	.mpo_copy_mbuf_label = mac_mls_copy_label,
 	.mpo_copy_pipe_label = mac_mls_copy_label,
 	.mpo_copy_vnode_label = mac_mls_copy_label,
 	.mpo_externalize_cred_label = mac_mls_externalize_label,
