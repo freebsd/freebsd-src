ec27a748079eea5188cc0b53a46b236429761fb1
markm <markm@FreeBSD.org>
2003-06-02 19:29:27 +0000

    Add a new hash type. This "NT-hash" is compatible with the password hashing scheme used in Microsoft's NT machines. IT IS NOT SECURE! DON'T USE IT! This is for the use of competent sysadmins only!
    Submitted by:	Michael Bretterklieber

--- lib/libcrypt/crypt.h
+++ lib/libcrypt/crypt.h
@@ -1,3 +1,4 @@
+/* LINTLIBRARY */
 /*
  * Copyright (c) 1999
  *      Mark Murray.  All rights reserved.
@@ -28,11 +29,12 @@
  */
 
 /* magic sizes */
+#define MD4_SIZE 16
 #define MD5_SIZE 16
 
 char *crypt_des(const char *pw, const char *salt);
 char *crypt_md5(const char *pw, const char *salt);
+char *crypt_nthash(const char *pw, const char *salt);
 char *crypt_blowfish(const char *pw, const char *salt);
 
 extern void _crypt_to64(char *s, u_long v, int n);
-
