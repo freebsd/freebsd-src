0cb1948e9a3dcc5dd5b0abeed61c5436a5129bad
nyan <nyan@FreeBSD.org>
2003-06-08 11:18:35 +0000

    Don't route PCI irq on pc98.

--- sys/dev/pci/pci.c
+++ sys/dev/pci/pci.c
@@ -798,7 +798,7 @@ pci_add_resources(device_t pcib, device_t dev)
 	}
 
 	if (cfg->intpin > 0 && PCI_INTERRUPT_VALID(cfg->intline)) {
-#if defined(__ia64__) || (defined(__i386__) && !defined(SMP))
+#if defined(__ia64__) || (defined(__i386__) && !defined(SMP) && !defined(PC98))
 		/*
 		 * Try to re-route interrupts. Sometimes the BIOS or
 		 * firmware may leave bogus values in these registers.
