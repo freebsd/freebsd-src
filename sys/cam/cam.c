37ee58d3050a93505e901a60eb275e5423a3e554
obrien <obrien@FreeBSD.org>
2003-06-10 17:50:20 +0000

    Use __FBSDID().

--- sys/cam/cam.c
+++ sys/cam/cam.c
@@ -24,11 +24,12 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $FreeBSD$
  */
-#include <sys/param.h>
 
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/param.h>
 #ifdef _KERNEL
 #include <sys/param.h>
 #include <sys/systm.h>
