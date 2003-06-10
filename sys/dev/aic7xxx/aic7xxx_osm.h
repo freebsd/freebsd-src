be413c7591255c839e30293fa033c3962b2b1706
gibbs <gibbs@FreeBSD.org>
2003-06-10 03:25:24 +0000

    Sync perforce IDs.

--- sys/dev/aic7xxx/aic7xxx_osm.h
+++ sys/dev/aic7xxx/aic7xxx_osm.h
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: //depot/aic7xxx/freebsd/dev/aic7xxx/aic7xxx_osm.h#14 $
+ * $Id: //depot/aic7xxx/freebsd/dev/aic7xxx/aic7xxx_osm.h#15 $
  *
  * $FreeBSD$
  */
