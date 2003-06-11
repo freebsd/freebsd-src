34c415da2d1aabd4d7e5cc50c999d1c25730cf3c
obrien <obrien@FreeBSD.org>
2003-06-11 07:06:31 +0000

    Use __FBSDID().

--- sys/netatm/sigpvc/sigpvc_subr.c
+++ sys/netatm/sigpvc/sigpvc_subr.c
@@ -1,5 +1,4 @@
 /*
- *
  * ===================================
  * HARP  |  Host ATM Research Platform
  * ===================================
@@ -22,9 +21,6 @@
  *
  * Copies of this Software may be made, however, the above copyright
  * notice must be reproduced on all copies.
- *
- *	@(#) $FreeBSD$
- *
  */
 
 /*
@@ -32,8 +28,10 @@
  * ---------------------------
  *
  * Subroutines
- *
  */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -51,10 +49,6 @@
 #include <netatm/sigpvc/sigpvc_var.h>
 
 #include <vm/uma.h>
-
-#ifndef lint
-__RCSID("@(#) $FreeBSD$");
-#endif
 
 extern uma_zone_t	sigpvc_vc_zone;
 
