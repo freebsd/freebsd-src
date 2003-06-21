d4661f5c9853a65ea4f0b8c7dfc33b5e5cc7b209
Garance A Drosehn <gad@FreeBSD.org>
2003-06-21 06:12:47 +0000

    MFC 1.2:  Include stdlib.h for atoi declaration.  Add $FreeBSD$

--- usr.sbin/lpr/filters.ru/koi2alt/koi2alt.c
+++ usr.sbin/lpr/filters.ru/koi2alt/koi2alt.c
@@ -22,6 +22,8 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ * $FreeBSD$
  */
 
 /*
@@ -31,6 +33,7 @@
 #include <sys/types.h>
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int length = 66;
