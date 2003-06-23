8ea7badd5128957491bcdf9898d3e921301fca67
mdodd <mdodd@FreeBSD.org>
2003-06-23 11:15:22 +0000

    Add a PCI ID for the Apollo Pro 133A.
    PR:		 kern/46983
    Submitted by:	 David Holm <david@realityrift.com>

--- sys/dev/agp/agp_via.c
+++ sys/dev/agp/agp_via.c
@@ -73,6 +73,8 @@ agp_via_match(device_t dev)
 		return ("VIA 82C598 (Apollo MVP3) host to PCI bridge");
 	case 0x06911106:
 		return ("VIA 82C691 (Apollo Pro) host to PCI bridge");
+	case 0x06051106:
+		return ("VIA 82C694X (Apollo Pro 133A) host to PCI bridge");
 	case 0x03051106:
 	    return ("VIA 82C8363 (Apollo KT133A) host to PCI bridge");
 	};
