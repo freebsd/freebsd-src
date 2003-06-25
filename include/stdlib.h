15d972d0803872bc609f4d8196aa2622f1398ec1
obrien <obrien@FreeBSD.org>
2003-06-25 19:06:40 +0000

    Push the alloca #error warning farther down to play nicer with some out of tree local translator.
    Requested by:	 jmallett

--- include/stdlib.h
+++ include/stdlib.h
@@ -236,8 +236,6 @@ extern void (*_malloc_message)(const char *, const char *, const char *,
 #define alloca(sz) __builtin_alloca(sz)
 #elif defined(lint)
 void	*alloca(size_t);
-#else
-#error Please add alloca support on all FreeBSD architectures for this compiler.
 #endif
 
 __uint32_t
