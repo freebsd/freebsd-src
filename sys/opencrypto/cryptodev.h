0feda3c8b4a7066b99c1234e167b6fb182bb0ce1
sam <sam@FreeBSD.org>
2003-06-27 20:07:10 +0000

    Add support to eliminate a context switch per crypto op when using the software crypto device:
    o record crypto device capabilities in each session id
    o add a capability that indicates if the crypto driver operates synchronously
    o tag the software crypto driver as operating synchronously
    
    This commit also introduces crypto session id macros that cleanup their
    construction and querying.

--- sys/opencrypto/cryptodev.h
+++ sys/opencrypto/cryptodev.h
@@ -317,8 +317,9 @@ struct cryptocap {
 	u_int8_t	cc_flags;
 	u_int8_t	cc_qblocked;		/* symmetric q blocked */
 	u_int8_t	cc_kqblocked;		/* asymmetric q blocked */
-#define CRYPTOCAP_F_CLEANUP   0x1
-#define CRYPTOCAP_F_SOFTWARE  0x02
+#define CRYPTOCAP_F_CLEANUP	0x01		/* needs resource cleanup */
+#define CRYPTOCAP_F_SOFTWARE	0x02		/* software implementation */
+#define CRYPTOCAP_F_SYNC	0x04		/* operates synchronously */
 
 	void		*cc_arg;		/* callback argument */
 	int		(*cc_newsession)(void*, u_int32_t*, struct cryptoini*);
@@ -327,6 +328,17 @@ struct cryptocap {
 	void		*cc_karg;		/* callback argument */
 	int		(*cc_kprocess) (void*, struct cryptkop *, int);
 };
+
+/*
+ * Session ids are 64 bits.  The lower 32 bits contain a "local id" which
+ * is a driver-private session identifier.  The upper 32 bits contain a
+ * "hardware id" used by the core crypto code to identify the driver and
+ * a copy of the driver's capabilities that can be used by client code to
+ * optimize operation.
+ */
+#define	CRYPTO_SESID2HID(_sid)	(((_sid) >> 32) & 0xffffff)
+#define	CRYPTO_SESID2CAPS(_sid)	(((_sid) >> 56) & 0xff)
+#define	CRYPTO_SESID2LID(_sid)	(((u_int32_t) (_sid)) & 0xffffffff)
 
 MALLOC_DECLARE(M_CRYPTO_DATA);
 
