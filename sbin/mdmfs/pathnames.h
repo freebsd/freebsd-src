d4ebc5c07423cc0e10b1988604c1f45674fd1dd1
gordon <gordon@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-06-29 18:06:05 +0000

    Move path definitions to include/paths.h. This makes it easier to override these definitions in the /rescue case.
    Submitted by:	Tim Kientzle <kientzle@acm.org>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@117033 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/mdmfs/pathnames.h
+++
@@ -1,9 +0,0 @@
-/* $FreeBSD$ */
-#ifndef	MDMFS_PATHNAMES_H
-#define	MDMFS_PATHNAMES_H
-
-#define	PATH_MDCONFIG	"/sbin/mdconfig"
-#define	PATH_NEWFS	"/sbin/newfs"
-#define	PATH_MOUNT	"/sbin/mount"
-
-#endif	/* !MDMFS_PATHNAMES_H */
