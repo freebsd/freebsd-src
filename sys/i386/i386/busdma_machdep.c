4813f72a9bdc644d61343833a072cbfeff414b73
Maxime Henrion <mux@FreeBSD.org>
2003-07-01 16:54:54 +0000

    Honor the boundary of the busdma tag when allocating bounce pages. This was fixed in revision 1.5 of alpha/alpha/busdma_machdep.c and was never fixed in other busdma backends using bounce pages.

--- sys/i386/i386/busdma_machdep.c
+++ sys/i386/i386/busdma_machdep.c
@@ -818,7 +818,7 @@ alloc_bounce_pages(bus_dma_tag_t dmat, u_int numpages)
 							 M_NOWAIT, 0ul,
 							 dmat->lowaddr,
 							 PAGE_SIZE,
-							 0);
+							 dmat->boundary);
 		mtx_unlock(&Giant);
 		if (bpage->vaddr == 0) {
 			free(bpage, M_DEVBUF);
