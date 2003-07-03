477d2d0ee5ca408a964883aab811e717e109cada
Scott Long <scottl@FreeBSD.org>
2003-07-03 08:35:43 +0000

    Bump __FreeBSD_version to reflect the busdma API change.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -57,7 +57,7 @@
  * scheme is:  <major><two digit minor><0 if release branch, otherwise 1>xx
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 501101	/* Master, propagated to newvers */
+#define __FreeBSD_version 501102	/* Master, propagated to newvers */
 
 #ifndef NULL
 #define	NULL	0
