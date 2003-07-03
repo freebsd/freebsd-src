fbf3adccc890d9c3d523b07131734b291bcff31e
Julian Elischer <julian@FreeBSD.org>
2003-07-03 22:17:03 +0000

    fix a comment that was 180 degrees wrong.. (MFC) Allow the data receiver method to return the error it gets when it hs forwards the data.
    Submitted by: Ian Dowse <iedowse@maths.tcd.ie> (error return)

--- sys/netgraph/ng_tee.c
+++ sys/netgraph/ng_tee.c
@@ -46,7 +46,7 @@
  * entering from the right is passed to the left and duplicated on
  * right2left, and data entering from the left is passed to the right
  * and duplicated on left2right. Data entering from left2right is
- * sent to right, and data from right2left to left.
+ * sent to left, and data from right2left to right.
  */
 
 #include <sys/param.h>
@@ -339,7 +339,7 @@ ngt_rcvdata(hook_p hook, struct mbuf *m, meta_p meta)
 	dest->stats.outOctets += m->m_pkthdr.len;
 	dest->stats.outFrames++;
 	NG_SEND_DATA(error, dest->hook, m, meta);
-	return (0);
+	return (error);
 }
 
 /*
