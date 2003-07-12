8f9b162f7ac04633f3cb24232d93ce7f22ab941c
trhodes <trhodes@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-07-12 15:35:06 +0000

    Postfix, pcre, and Exim should be on the first CD in order for the recent sysinstall(8) commit to function properly.
    Discussed with:	bmah (while the sysinstall(8) patch was sent to re).
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@117476 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/scripts/print-cdrom-packages.sh
+++ release/scripts/print-cdrom-packages.sh
@@ -72,9 +72,12 @@ CDROM_SET_1="${CDROM_SET_1} emulators/osf1_base"
 elif [ "X${PKG_ARCH}" = "Xi386" ]; then
 CDROM_SET_1="${CDROM_SET_1} emulators/linux_base"
 fi
+CDROM_SET_1="${CDROM_SET_1} devel/pcre"
 CDROM_SET_1="${CDROM_SET_1} lang/perl5"
 CDROM_SET_1="${CDROM_SET_1} net/pcnfsd"
 CDROM_SET_1="${CDROM_SET_1} net/rsync"
+CDROM_SET_1="${CDROM_SET_1} mail/exim"
+CDROM_SET_1="${CDROM_SET_1} mail/postfix"
 CDROM_SET_1="${CDROM_SET_1} x11-fonts/XFree86-4-font100dpi"
 CDROM_SET_1="${CDROM_SET_1} x11-fonts/XFree86-4-font75dpi"
 CDROM_SET_1="${CDROM_SET_1} x11-fonts/XFree86-4-fontCyrillic"
