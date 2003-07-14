c44a6dcebd2fe4f5f48d4139d922985349b090e5
Garance A Drosehn <gad@FreeBSD.org>
2003-07-14 22:24:28 +0000

    More changes to use __FBSDID() for setting rcsids, and fix the format of 'sccsid' lines so they consistently match style(9) guidelines.  Note that this means you will have to add '-a' to the 'strings' command when searching for rcs ids, eg:      strings -a /usr/sbin/lpc | grep '$FreeBSD'
    Reviewed by:	discussed on cvs-src & with bde and obrien
    MFC after:	15 days

--- usr.sbin/lpr/lpc/lpc.c
+++ usr.sbin/lpr/lpc/lpc.c
@@ -38,13 +38,14 @@ static const char copyright[] =
 	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
-#ifndef lint
 #if 0
+#ifndef lint
 static char sccsid[] = "@(#)lpc.c	8.3 (Berkeley) 4/28/95";
-#endif
-static const char rcsid[] =
-  "$FreeBSD$";
 #endif /* not lint */
+#endif
+
+#include "lp.cdefs.h"		/* A cross-platform version of <sys/cdefs.h> */
+__FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
 
