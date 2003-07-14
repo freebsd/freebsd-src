d806166ff83e144b130dd3b12edd5a89ae33ea46
gad <gad@FreeBSD.org>
2003-07-14 20:20:58 +0000

    Get the 'sccsid' lines even closer to correct style(9) form.  The '#ifdef lint/#endif' around the lines should not have been removed. Also add blank lines where one (per file) was missing.
    Reviewed by:	First part noticed by bde, blank lines noticed by me
    MFC after:	15 days

--- usr.sbin/lpr/common_source/common.c
+++ usr.sbin/lpr/common_source/common.c
@@ -37,8 +37,11 @@
  */
 
 #if 0
+#ifndef lint
 static char sccsid[] = "@(#)common.c	8.5 (Berkeley) 4/28/95";
+#endif /* not lint */
 #endif
+
 #include "lp.cdefs.h"		/* A cross-platform version of <sys/cdefs.h> */
 __FBSDID("$FreeBSD$");
 
