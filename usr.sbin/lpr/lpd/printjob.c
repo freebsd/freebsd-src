6c501ba529238f3490eeb7d748c91ceea124375f
gad <gad@FreeBSD.org>
2003-07-14 19:59:33 +0000

    Get the 'sccsid' lines even closer to correct style(9) form.  The '#ifdef lint/#endif' around the lines should not have been removed.
    Reviewed by:	noticed by bde
    MFC after:	15 days

--- usr.sbin/lpr/lpd/printjob.c
+++ usr.sbin/lpr/lpd/printjob.c
@@ -39,7 +39,9 @@ static const char copyright[] =
 #endif /* not lint */
 
 #if 0
+#ifndef lint
 static char sccsid[] = "@(#)printjob.c	8.7 (Berkeley) 5/10/95";
+#endif /* not lint */
 #endif
 
 #include "lp.cdefs.h"		/* A cross-platform version of <sys/cdefs.h> */
