f18dd35c15c583d030949ee4c515456fff97f2e7
gad <gad@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-07-15 07:01:01 +0000

    More changes to use __FBSDID() for setting rcsids.  For the filter programs, minor Makefile changes are needed to find lp.cdefs.h. For lpf.c, the 'sccsid' line is formatted to match style(9), and a 'From:' is removed so the sccsid returns to what it was back in the days of '-r CSRG' (1994).
    Reviewed by:	discussed with bde and obrien
    MFC after:	15 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@117609 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/lpr/filters/lpf.c
+++ usr.sbin/lpr/filters/lpf.c
@@ -37,13 +37,14 @@ static const char copyright[] =
 	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
+#if 0
 #ifndef lint
-/*
-static char sccsid[] = "From: @(#)lpf.c	8.1 (Berkeley) 6/6/93";
-*/
-static const char rcsid[] =
-  "$FreeBSD$";
+static char sccsid[] = "@(#)lpf.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
+#endif
+
+#include "lp.cdefs.h"		/* A cross-platform version of <sys/cdefs.h> */
+__FBSDID("$FreeBSD$");
 
 /*
  * 	filter which reads the output of nroff and converts lines
