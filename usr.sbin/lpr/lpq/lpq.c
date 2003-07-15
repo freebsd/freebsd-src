9c742317f091d9a33ceae682a554e2a9c9ecebdb
gad <gad@FreeBSD.org>
2003-07-15 08:48:30 +0000

    (Hopefully) Last set of changes so all of lpr uses __FBSDID() for setting rcsids, and uses a consistent format for 'sccsid' lines.
    Reviewed by:	discussed with bde and obrien
    MFC after:	15 days

--- usr.sbin/lpr/lpq/lpq.c
+++ usr.sbin/lpr/lpq/lpq.c
@@ -38,13 +38,14 @@ static const char copyright[] =
 	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
+#if 0
 #ifndef lint
-/*
 static char sccsid[] = "@(#)lpq.c	8.3 (Berkeley) 5/10/95";
-*/
-static const char rcsid[] =
-  "$FreeBSD$";
 #endif /* not lint */
+#endif
+
+#include "lp.cdefs.h"		/* A cross-platform version of <sys/cdefs.h> */
+__FBSDID("$FreeBSD$");
 
 /*
  * Spool Queue examination program
