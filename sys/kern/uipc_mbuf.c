27cb0555241e9f0d0f4e19429ffb77ab98d849e0
Mike Silbersack <silby@FreeBSD.org>
2003-07-20 02:36:05 +0000

    MFC rev 1.119

--- sys/kern/uipc_mbuf.c
+++ sys/kern/uipc_mbuf.c
@@ -765,7 +765,10 @@ m_prepend(m, len, how)
 {
 	struct mbuf *mn;
 
-	MGET(mn, how, m->m_type);
+	if (m->m_flags & M_PKTHDR)
+		MGETHDR(mn, how, m->m_type);
+	else
+		MGET(mn, how, m->m_type);
 	if (mn == (struct mbuf *)NULL) {
 		m_freem(m);
 		return ((struct mbuf *)NULL);
@@ -819,7 +822,10 @@ m_copym(m, off0, len, wait)
 			    ("m_copym, length > size of mbuf chain"));
 			break;
 		}
-		MGET(n, wait, m->m_type);
+		if (copyhdr)
+			MGETHDR(n, wait, m->m_type);
+		else
+			MGET(n, wait, m->m_type);
 		*np = n;
 		if (n == 0)
 			goto nospace;
@@ -1514,6 +1520,8 @@ m_defrag(struct mbuf *m0, int how)
 
 	if (!(m0->m_flags & M_PKTHDR))
 		return (m0);
+
+	m_fixhdr(m0); /* Needed sanity check */
 
 #ifdef MBUF_STRESS_TEST
 	if (m_defragrandomfailures) {
