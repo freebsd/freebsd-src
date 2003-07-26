d04891f63da1d3c0e403d22de9419be48f2320fc
Bruce Evans <bde@FreeBSD.org>
2003-07-26 12:06:25 +0000

    MFC (1.32: translate to C: don't use hard newlines in a string literal; 1.41: use the correct function name in the string literal).
    This is part of making some RELENG_4 kernels buildable by gcc-3.3.

--- sys/dev/buslogic/bt.c
+++ sys/dev/buslogic/bt.c
@@ -927,10 +927,10 @@ bt_find_probe_range(int ioport, int *port_index, int *max_port_index)
 				break;
 		if ((i >= BT_NUM_ISAPORTS)
 		 || (ioport != bt_isa_ports[i].addr)) {
-			printf("
-bt_isa_probe: Invalid baseport of 0x%x specified.
-bt_isa_probe: Nearest valid baseport is 0x%x.
-bt_isa_probe: Failing probe.\n",
+			printf(
+"bt_find_probe_range: Invalid baseport of 0x%x specified.\n"
+"bt_find_probe_range: Nearest valid baseport is 0x%x.\n"
+"bt_find_probe_range: Failing probe.\n",
 			       ioport,
 			       (i < BT_NUM_ISAPORTS)
 				    ? bt_isa_ports[i].addr
