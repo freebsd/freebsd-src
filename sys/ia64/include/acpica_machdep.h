dc539f3ee0827824a73fd840fac8e55977455984
Marcel Moolenaar <marcel@FreeBSD.org>
2003-07-26 07:23:25 +0000

    Unbreak ia64 builds now -Werror is enabled again. Avoid obsolete memory operand construct.

--- sys/ia64/include/acpica_machdep.h
+++ sys/ia64/include/acpica_machdep.h
@@ -65,7 +65,7 @@
 
 #define ACPI_ACQUIRE_GLOBAL_LOCK(GLptr, Acq) \
     do { \
-    __asm__ volatile ("1:  ld4      r29=%1\n"  \
+    __asm__ volatile ("1:  ld4      r29=[%1]\n"  \
         ";;\n"                  \
         "mov    ar.ccv=r29\n"   \
         "mov    r2=r29\n"       \
@@ -77,7 +77,7 @@
         ";;\n"                  \
         "add    r29=r29,r30\n"  \
         ";;\n"                  \
-        "cmpxchg4.acq   r30=%1,r29,ar.ccv\n" \
+        "cmpxchg4.acq   r30=[%1],r29,ar.ccv\n" \
         ";;\n"                  \
         "cmp.eq p6,p7=r2,r30\n" \
         "(p7) br.dpnt.few 1b\n" \
@@ -85,24 +85,24 @@
         ";;\n"                  \
         "(p8) mov %0=-1\n"      \
         "(p9) mov %0=r0\n"      \
-        :"=r"(Acq):"m"(GLptr):"r2","r29","r30","memory"); \
+        :"=r"(Acq):"r"(GLptr):"r2","r29","r30","memory"); \
     } while (0)
 
 #define ACPI_RELEASE_GLOBAL_LOCK(GLptr, Acq) \
     do { \
-    __asm__ volatile ("1:  ld4      r29=%1\n" \
+    __asm__ volatile ("1:  ld4      r29=[%1]\n" \
         ";;\n"                  \
         "mov    ar.ccv=r29\n"   \
         "mov    r2=r29\n"       \
         "and    r29=-4,r29\n"   \
         ";;\n"                  \
-        "cmpxchg4.acq   r30=%1,r29,ar.ccv\n" \
+        "cmpxchg4.acq   r30=[%1],r29,ar.ccv\n" \
         ";;\n"                  \
         "cmp.eq p6,p7=r2,r30\n" \
         "(p7) br.dpnt.few 1b\n" \
         "and    %0=1,r2\n"      \
         ";;\n"                  \
-        :"=r"(Acq):"m"(GLptr):"r2","r29","r30","memory"); \
+        :"=r"(Acq):"r"(GLptr):"r2","r29","r30","memory"); \
     } while (0)
 /*! [End] no source code translation !*/
 
