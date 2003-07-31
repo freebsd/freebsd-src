9fb1db7bc8bbd3eefb7a9ed685a6139465e70f71
Peter Wemm <peter@FreeBSD.org>
2003-07-31 01:26:40 +0000

    Cosmetic: fix disorder of opt_kstack_pages.h include.

--- sys/i386/i386/machdep.c
+++ sys/i386/i386/machdep.c
@@ -47,11 +47,11 @@ __FBSDID("$FreeBSD$");
 #include "opt_inet.h"
 #include "opt_ipx.h"
 #include "opt_isa.h"
+#include "opt_kstack_pages.h"
 #include "opt_maxmem.h"
 #include "opt_msgbuf.h"
 #include "opt_npx.h"
 #include "opt_perfmon.h"
-#include "opt_kstack_pages.h"
 
 #include <sys/param.h>
 #include <sys/systm.h>
