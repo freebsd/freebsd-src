1beccae67c75ac5f03c4df06fec2b700d7ee4587
John Baldwin <jhb@FreeBSD.org>
2003-07-31 18:50:58 +0000

    Add a new function to look for a spinlock's instance when it is held by another thread.  We use the td_oncpu member of the other field to locate it's associated CPU and then search the that CPU's list of spin locks contained in its per-CPU data.  This is not always safe and may in fact panic or just not work, but it is useful in at least one case.

--- sys/sys/lock.h
+++ sys/sys/lock.h
@@ -206,6 +206,7 @@ void	witness_restore(struct lock_object *, const char *, int);
 int	witness_list_locks(struct lock_list_entry **);
 int	witness_warn(int, struct lock_object *, const char *, ...);
 void	witness_assert(struct lock_object *, int, const char *, int);
+void	witness_display_spinlock(struct lock_object *, struct thread *);
 int	witness_line(struct lock_object *);
 const char *witness_file(struct lock_object *);
 
