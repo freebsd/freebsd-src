e7a849e42d11db4eef219e3c9f0a9250c32a064c
bmilekic <bmilekic@FreeBSD.org>
2003-07-31 03:39:51 +0000

    Make sure that when the PV ENTRY zone is created in pmap, that it's created not only with UMA_ZONE_VM but also with UMA_ZONE_NOFREE.  In the i386 case in particular, the pmap code would hook a special page allocation routine that allocated from kernel_map and not kmem_map, and so when/if the pageout daemon drained the zones, it could actually push out slabs from the PV ENTRY zone but call UMA's default page_free, which resulted in pages allocated from kernel_map being freed to kmem_map; bad.  kmem_free() ignores the return value of the vm_map_delete and just returns.  I'm not sure what the exact repercussions could be, but it doesn't look good.
    In the PAE case on i386, we also set-up a zone in pmap, so be
    conservative for now and make that zone also ZONE_NOFREE and
    ZONE_VM.  Do this for the pmap zones for the other archs too,
    although in some cases it may not be entirely necessarily.  We'd
    rather be safe than sorry at this point.
    
    Perhaps all UMA_ZONE_VM zones should by default be also
    UMA_ZONE_NOFREE?
    
    May fix some of silby's crashes on the PV ENTRY zone.

--- sys/amd64/amd64/pmap.c
+++ sys/amd64/amd64/pmap.c
@@ -610,7 +610,7 @@ pmap_init(phys_start, phys_end)
 	if (initial_pvs < MINPV)
 		initial_pvs = MINPV;
 	pvzone = uma_zcreate("PV ENTRY", sizeof (struct pv_entry), NULL, NULL, 
-	    NULL, NULL, UMA_ALIGN_PTR, UMA_ZONE_VM);
+	    NULL, NULL, UMA_ALIGN_PTR, UMA_ZONE_VM | UMA_ZONE_NOFREE);
 	uma_zone_set_allocf(pvzone, pmap_pv_allocf);
 	uma_prealloc(pvzone, initial_pvs);
 
