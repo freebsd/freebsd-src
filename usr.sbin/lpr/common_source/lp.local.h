dbb8a574d584f0d424f5c96fbc1991bbc354db8e
gad <gad@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-08-01 00:06:41 +0000

    Remove a 'From:' that snuck into an sccsid comment line as part of -v1.3
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@118288 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/lpr/common_source/lp.local.h
+++ usr.sbin/lpr/common_source/lp.local.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	From: @(#)lp.local.h	8.1 (Berkeley) 6/6/93
+ *	@(#)lp.local.h	8.1 (Berkeley) 6/6/93
  * $FreeBSD$
  */
 
