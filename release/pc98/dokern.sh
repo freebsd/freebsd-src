e4bbcfbc420f484d1b46066db22547821c8cd88a
Ruslan Ermilov <ru@FreeBSD.org>
2003-08-02 09:36:49 +0000

    MFi386: sort.
    OK'ed by:	nyan

--- release/pc98/dokern.sh
+++ release/pc98/dokern.sh
@@ -12,62 +12,62 @@ fi
 if [ "${FDSIZE}" = "SMALL" ]; then
 
 sed	-e 's/ident.*GENERIC/ident		BOOTMFS/g' \
+	-e '/COMPAT_FREEBSD4/d' \
+	-e '/DDB/d' \
 	-e '/DEBUG/d' \
-	-e '/SOFTUPDATES/d' \
-	-e '/UFS_DIRHASH/d' \
+	-e '/INVARIANTS/d' \
+	-e '/INVARIANT_SUPPORT/d' \
+	-e '/KTRACE/d' \
 	-e '/NFSSERVER/d' \
 	-e '/NFS_ROOT/d' \
 	-e '/PROCFS/d' \
 	-e '/PSEUDOFS/d' \
-	-e '/COMPAT_FREEBSD4/d' \
-	-e '/KTRACE/d' \
+	-e '/SOFTUPDATES/d' \
 	-e '/SYSV/d' \
-	-e '/DDB/d' \
-	-e '/INVARIANTS/d' \
-	-e '/INVARIANT_SUPPORT/d' \
+	-e '/UFS_DIRHASH/d' \
 	-e '/WITNESS/d' \
-	-e '/	atapifd	/d' \
-	-e '/	atapist	/d' \
-	-e '/	ch	/d' \
-	-e '/	sa	/d' \
-	-e '/	pass	/d' \
-	-e '/	ses	/d' \
-	-e '/	splash	/d' \
-	-e '/	ppc$/d' \
-	-e '/	ppbus	/d' \
-	-e '/	lpt	/d' \
-	-e '/	plip	/d' \
-	-e '/	ppi	/d' \
-	-e '/	an	/d' \
-	-e '/	awi	/d' \
-	-e '/	wi	/d' \
-	-e '/	random	/d' \
-	-e '/	sl	/d' \
-	-e '/	ppp	/d' \
-	-e '/	pty	/d' \
-	-e '/	gif	/d' \
-	-e '/	faith	/d' \
-	-e '/	pci$/d' \
 	-e '/	adv	/d' \
 	-e '/	ahc	/d' \
 	-e '/	amd	/d' \
-	-e '/	isp	/d' \
-	-e '/	sym	/d' \
+	-e '/	an	/d' \
+	-e '/	atapifd	/d' \
+	-e '/	atapist	/d' \
+	-e '/	awi	/d' \
+	-e '/	ch	/d' \
+	-e '/	dc	/d' \
 	-e '/	de	/d' \
 	-e '/	em	/d' \
-	-e '/	txp	/d' \
-	-e '/	vx	/d' \
-	-e '/	dc	/d' \
+	-e '/	faith	/d' \
 	-e '/	fxp	/d' \
+	-e '/	gif	/d' \
+	-e '/	isp	/d' \
+	-e '/	lpt	/d' \
+	-e '/	pass	/d' \
+	-e '/	pci$/d' \
 	-e '/	pcn	/d' \
+	-e '/	plip	/d' \
+	-e '/	ppbus	/d' \
+	-e '/	ppc$/d' \
+	-e '/	ppi	/d' \
+	-e '/	ppp	/d' \
+	-e '/	pty	/d' \
+	-e '/	random	/d' \
 	-e '/	rl	/d' \
+	-e '/	sa	/d' \
+	-e '/	ses	/d' \
 	-e '/	sf	/d' \
 	-e '/	sis	/d' \
+	-e '/	sl	/d' \
+	-e '/	splash	/d' \
 	-e '/	ste	/d' \
+	-e '/	sym	/d' \
 	-e '/	tl	/d' \
 	-e '/	tx	/d' \
+	-e '/	txp	/d' \
 	-e '/	vr	/d' \
+	-e '/	vx	/d' \
 	-e '/	wb	/d' \
+	-e '/	wi	/d' \
 	-e '/	xl	/d'
 
 	echo "options 	ATA_NOPCI" 
@@ -75,38 +75,38 @@ sed	-e 's/ident.*GENERIC/ident		BOOTMFS/g' \
 else
 
 sed	-e 's/ident.*GENERIC/ident		BOOTMFS/g' \
+	-e '/COMPAT_FREEBSD4/d' \
+	-e '/DDB/d' \
 	-e '/DEBUG/d' \
-	-e '/SOFTUPDATES/d' \
-	-e '/UFS_DIRHASH/d' \
+	-e '/INVARIANTS/d' \
+	-e '/INVARIANT_SUPPORT/d' \
+	-e '/KTRACE/d' \
 	-e '/NFSSERVER/d' \
 	-e '/NFS_ROOT/d' \
 	-e '/PROCFS/d' \
 	-e '/PSEUDOFS/d' \
-	-e '/COMPAT_FREEBSD4/d' \
-	-e '/KTRACE/d' \
+	-e '/SOFTUPDATES/d' \
 	-e '/SYSV/d' \
-	-e '/DDB/d' \
-	-e '/INVARIANTS/d' \
-	-e '/INVARIANT_SUPPORT/d' \
+	-e '/UFS_DIRHASH/d' \
 	-e '/WITNESS/d' \
 	-e '/	atapifd	/d' \
 	-e '/	atapist	/d' \
 	-e '/	ch	/d' \
-	-e '/	sa	/d' \
-	-e '/	pass	/d' \
-	-e '/	ses	/d' \
-	-e '/	splash	/d' \
-	-e '/	ppc$/d' \
-	-e '/	ppbus	/d' \
+	-e '/	faith	/d' \
+	-e '/	gif	/d' \
 	-e '/	lpt	/d' \
+	-e '/	pass	/d' \
 	-e '/	plip	/d' \
+	-e '/	ppbus	/d' \
+	-e '/	ppc$/d' \
 	-e '/	ppi	/d' \
-	-e '/	random	/d' \
-	-e '/	sl	/d' \
 	-e '/	ppp	/d' \
 	-e '/	pty	/d' \
-	-e '/	gif	/d' \
-	-e '/	faith	/d'
+	-e '/	random	/d' \
+	-e '/	sa	/d' \
+	-e '/	ses	/d' \
+	-e '/	sl	/d' \
+	-e '/	splash	/d'
 
 fi
 
