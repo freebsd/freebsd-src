98b95fa83b5bef75773be0314641853f19b9cfc6
jhb <jhb@FreeBSD.org>
2003-08-04 20:32:45 +0000

    - Since td_critnest is now initialized in MI code, it doesn't have to be   set in cpu_critical_fork_exit() anymore. - As far as I can tell, cpu_thread_link() has never been used, not even   when it was originally added, so remove it.

--- sys/ia64/ia64/critical.c
+++ sys/ia64/ia64/critical.c
@@ -29,16 +29,5 @@ cpu_critical_fork_exit(void)
 	struct thread *td;
 
 	td = curthread;
-	td->td_critnest = 1;
 	td->td_md.md_savecrit = (ia64_get_psr() | IA64_PSR_I);
-}
-
-/*
- * cpu_thread_link() - thread linkup, initialize machine-dependant fields
- */
-void
-cpu_thread_link(struct thread *td)
-{
-
-	td->td_md.md_savecrit = 0;
 }
