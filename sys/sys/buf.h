bf614e120898c4a3cdc8973322f8d49059428dbf
phk <phk@FreeBSD.org>
2003-08-05 06:43:12 +0000

    Add a B_KEEPGIANT flag so non-SMPng code can get preferential treatment.

--- sys/sys/buf.h
+++ sys/sys/buf.h
@@ -222,7 +222,7 @@ struct buf {
 #define	B_MALLOC	0x00010000	/* malloced b_data */
 #define	B_CLUSTEROK	0x00020000	/* Pagein op, so swap() can count it. */
 #define	B_PHYS		0x00040000	/* I/O to user memory. */
-#define	B_00080000	0x00080000	/* Available flag. */
+#define	B_KEEPGIANT	0x00080000	/* Do not drop Giant in strategy() */
 #define	B_00100000	0x00100000	/* Available flag. */
 #define	B_DIRTY		0x00200000	/* Needs writing later (in EXT2FS). */
 #define	B_RELBUF	0x00400000	/* Release VMIO buffer. */
