d7ba8669eab2ab347b1a4453815a3179fb9f2a55
tjr <tjr@FreeBSD.org>
2003-08-07 08:04:01 +0000

    Implement mbstowcs() as a wrapper around mbsrtowcs(), and wcstombs() as a wrapper around wcsrtombs().

--- lib/libc/locale/wcstombs.c
+++ lib/libc/locale/wcstombs.c
@@ -1,9 +1,6 @@
 /*-
- * Copyright (c) 1993
- *	The Regents of the University of California.  All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * Paul Borman at Krystal Technologies.
+ * Copyright (c) 2002, 2003 Tim J. Robbins.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -13,18 +10,11 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -37,55 +27,18 @@
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include <errno.h>
 #include <stdlib.h>
-#include <limits.h>
-#include <stddef.h>
-#include <rune.h>
+#include <string.h>
+#include <wchar.h>
 
 size_t
 wcstombs(char * __restrict s, const wchar_t * __restrict pwcs, size_t n)
 {
-	char buf[MB_LEN_MAX];
-	char *e;
-	int cnt, nb;
 
-	if (pwcs == NULL || n > INT_MAX) {
-		errno = EINVAL;
-		return (-1);
-	}
-
-	cnt = 0;
-
-	if (s == NULL) {
-		/* Convert and count only, do not store. */
-		while (*pwcs != L'\0') {
-			if (sputrune(*pwcs++, buf, MB_LEN_MAX, &e) == 0) {
-				errno = EILSEQ;
-				return (-1);
-			}
-			cnt += e - buf;
-		}
-		return (cnt);
-	}
-
-	/* Convert, store and count characters. */
-	nb = n;
-	while (nb > 0) {
-		if (*pwcs == L'\0') {
-			*s = '\0';
-			break;
-		}
-		if (sputrune(*pwcs++, s, nb, &e) == 0) {
-			errno = EILSEQ;
-			return (-1);
-		}
-		if (e == NULL)		/* too long */
-			return (cnt);
-		cnt += e - s;
-		nb -= e - s;
-		s = e;
-	}
-
-	return (cnt);
+	/*
+	 * We pass NULL as the state pointer to wcsrtombs() because we don't
+	 * support state-dependent encodings and don't want to waste time
+	 * creating a zeroed mbstate_t that will not be used.
+	 */
+	return (wcsrtombs(s, &pwcs, n, NULL));
 }
