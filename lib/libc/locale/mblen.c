b5845913de7a2074be1bbf2d3781b01c7c59d73e
tjr <tjr@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-08-07 09:34:51 +0000

    Implement mblen(s, n) as mbtowc(NULL, s, n) to avoid calling sgetrune() and to simplify things. This is only valid until we start supporting state-dependent encodings.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@118595 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/locale/mblen.c
+++ lib/libc/locale/mblen.c
@@ -1,9 +1,6 @@
 /*-
- * Copyright (c) 1993
- *	The Regents of the University of California.  All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * Paul Borman at Krystal Technologies.
+ * Copyright (c) 2002, 2003 Tim J. Robbins.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -13,18 +10,11 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -37,22 +27,15 @@
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include <errno.h>
-#include <stddef.h>
 #include <stdlib.h>
-#include <rune.h>
 
 int
 mblen(const char *s, size_t n)
 {
-	const char *e;
 
-	if (s == NULL)
-		/* No support for state dependent encodings. */
-		return (0);	
-	if (sgetrune(s, n, &e) == _INVALID_RUNE) {
-		errno = EILSEQ;
-		return (-1);
-	}
-	return (*s == '\0' ? 0 : e - s);
+	/*
+	 * Calling mbtowc() is only legal because we don't support
+	 * state-dependent encodings.
+	 */
+	return (mbtowc(NULL, s, n));
 }
