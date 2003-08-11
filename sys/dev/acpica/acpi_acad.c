798fb860d5688ccf940d2043943b8ac36739785f
Nate Lawson <njl@FreeBSD.org>
2003-08-11 15:34:43 +0000

    Style cleanups to match the rest of this directory.  For acpi_battery.c, remove unused includes.

--- sys/dev/acpica/acpi_acad.c
+++ sys/dev/acpica/acpi_acad.c
@@ -43,129 +43,29 @@
 #include <dev/acpica/acpivar.h>
 #include <dev/acpica/acpiio.h>
  
-/*
- * Hooks for the ACPI CA debugging infrastructure
- */
+/* Hooks for the ACPI CA debugging infrastructure */
 #define _COMPONENT	ACPI_AC_ADAPTER
 ACPI_MODULE_NAME("AC_ADAPTER")
+
+/* Number of times to retry initialization before giving up. */
+#define ACPI_ACAD_RETRY_MAX		6
 
 #define ACPI_DEVICE_CHECK_PNP		0x00
 #define ACPI_DEVICE_CHECK_EXISTENCE	0x01
 #define ACPI_POWERSOURCE_STAT_CHANGE	0x80
 
-static void acpi_acad_get_status(void * );
-static void acpi_acad_notify_handler(ACPI_HANDLE , UINT32 ,void *);
-static int acpi_acad_probe(device_t);
-static int acpi_acad_attach(device_t);
-static int acpi_acad_ioctl(u_long, caddr_t, void *);
-static int acpi_acad_sysctl(SYSCTL_HANDLER_ARGS);
-static void acpi_acad_init_acline(void *arg);
-
-struct  acpi_acad_softc {
-	int status;
-
-	int initializing;
+struct	acpi_acad_softc {
+    int status;
+    int initializing;
 };
 
-static void
-acpi_acad_get_status(void *context)
-{
-	int newstatus;
-	device_t dev = context;
-	struct acpi_acad_softc *sc = device_get_softc(dev);
-	ACPI_HANDLE h = acpi_get_handle(dev);
-
-	if (ACPI_FAILURE(acpi_EvaluateInteger(h, "_PSR", &newstatus))) {
-		sc->status = -1;
-		return;
-	}
-
-	if (sc->status != newstatus) {
-		sc->status = newstatus;
-		/* set system power profile based on AC adapter status */
-		power_profile_set_state(sc->status ? POWER_PROFILE_PERFORMANCE : POWER_PROFILE_ECONOMY);
-		ACPI_VPRINT(dev, acpi_device_get_parent_softc(dev),
-		    "%s Line\n",(sc->status) ? "On" : "Off");
-	}
-}
-
-static void
-acpi_acad_notify_handler(ACPI_HANDLE h, UINT32 notify, void *context)
-{
-	device_t dev = context;
-
-	ACPI_VPRINT(dev, acpi_device_get_parent_softc(dev),
-	    "Notify %d\n", notify);
-
-	switch (notify) {
-	case ACPI_DEVICE_CHECK_PNP:
-	case ACPI_DEVICE_CHECK_EXISTENCE:
-	case ACPI_POWERSOURCE_STAT_CHANGE:
-		/*Temporally. It is better to notify policy manager*/
-		AcpiOsQueueForExecution(OSD_PRIORITY_LO,
-		    acpi_acad_get_status,context);
-		break;
-	default:
-		break;
-	}
-}
-
-static int
-acpi_acad_probe(device_t dev)
-{
-
-    if ((acpi_get_type(dev) == ACPI_TYPE_DEVICE) &&
-	acpi_MatchHid(dev, "ACPI0003")) {
-      
-      /*
-       * Set device description 
-       */
-	device_set_desc(dev, "AC adapter");
-	return(0);
-    }
-    return(ENXIO);
-}
-
-static int
-acpi_acad_attach(device_t dev)
-{
-	int	 error;
-	struct acpi_acad_softc	*sc;
-	struct acpi_softc	*acpi_sc;
-
-	ACPI_HANDLE handle = acpi_get_handle(dev);
-	AcpiInstallNotifyHandler(handle, 
-				 ACPI_DEVICE_NOTIFY,
-				 acpi_acad_notify_handler, dev);
-	/*Installing system notify is not so good*/
-	AcpiInstallNotifyHandler(handle, 
-				 ACPI_SYSTEM_NOTIFY,
-				 acpi_acad_notify_handler, dev);
-
-	if ((sc = device_get_softc(dev)) == NULL) {
-		return (ENXIO);
-	}
-	if ((error = acpi_register_ioctl(ACPIIO_ACAD_GET_STATUS,
-			acpi_acad_ioctl, dev)) != 0) {
-		return (error);
-	}
-
-	if (device_get_unit(dev) == 0) {
-		acpi_sc = acpi_device_get_parent_softc(dev);
-		SYSCTL_ADD_PROC(&acpi_sc->acpi_sysctl_ctx,
-			SYSCTL_CHILDREN(acpi_sc->acpi_sysctl_tree),
-			OID_AUTO, "acline", CTLTYPE_INT | CTLFLAG_RD,
-			&sc->status, 0, acpi_acad_sysctl, "I", "");
-	}
-
-	/* Get initial status after whole system is up. */
-	sc->status = -1;
-	sc->initializing = 0;
-
-	AcpiOsQueueForExecution(OSD_PRIORITY_LO, acpi_acad_init_acline, dev);
-
-	return(0);
-}
+static void	acpi_acad_get_status(void *);
+static void	acpi_acad_notify_handler(ACPI_HANDLE, UINT32, void *);
+static int	acpi_acad_probe(device_t);
+static int	acpi_acad_attach(device_t);
+static int	acpi_acad_ioctl(u_long, caddr_t, void *);
+static int	acpi_acad_sysctl(SYSCTL_HANDLER_ARGS);
+static void	acpi_acad_init_acline(void *arg);
 
 static device_method_t acpi_acad_methods[] = {
     /* Device interface */
@@ -182,101 +82,191 @@ static driver_t acpi_acad_driver = {
 };
 
 static devclass_t acpi_acad_devclass;
-DRIVER_MODULE(acpi_acad,acpi,acpi_acad_driver,acpi_acad_devclass,0,0);
+DRIVER_MODULE(acpi_acad, acpi, acpi_acad_driver, acpi_acad_devclass, 0, 0);
+
+static void
+acpi_acad_get_status(void *context)
+{
+    struct acpi_acad_softc *sc;
+    device_t	dev;
+    ACPI_HANDLE	h;
+    int		newstatus;
+
+    dev = context;
+    sc = device_get_softc(dev);
+    h = acpi_get_handle(dev);
+    if (ACPI_FAILURE(acpi_EvaluateInteger(h, "_PSR", &newstatus))) {
+	sc->status = -1;
+	return;
+    }
+
+    if (sc->status != newstatus) {
+	sc->status = newstatus;
+
+	/* Set system power profile based on AC adapter status */
+	power_profile_set_state(sc->status ? POWER_PROFILE_PERFORMANCE :
+				POWER_PROFILE_ECONOMY);
+	ACPI_VPRINT(dev, acpi_device_get_parent_softc(dev),
+		    "%s Line\n", sc->status ? "On" : "Off");
+    }
+}
+
+static void
+acpi_acad_notify_handler(ACPI_HANDLE h, UINT32 notify, void *context)
+{
+    device_t dev = context;
+
+    ACPI_VPRINT(dev, acpi_device_get_parent_softc(dev),
+		"Notify %d\n", notify);
+
+    switch (notify) {
+    case ACPI_DEVICE_CHECK_PNP:
+    case ACPI_DEVICE_CHECK_EXISTENCE:
+    case ACPI_POWERSOURCE_STAT_CHANGE:
+	/* Temporarily.  It is better to notify policy manager */
+	AcpiOsQueueForExecution(OSD_PRIORITY_LO, acpi_acad_get_status, context);
+	break;
+    default:
+	break;
+    }
+}
+
+static int
+acpi_acad_probe(device_t dev)
+{
+    if (acpi_get_type(dev) == ACPI_TYPE_DEVICE &&
+	acpi_MatchHid(dev, "ACPI0003")) {
+
+	device_set_desc(dev, "AC adapter");
+	return (0);
+    }
+    return (ENXIO);
+}
+
+static int
+acpi_acad_attach(device_t dev)
+{
+    struct acpi_acad_softc *sc;
+    struct acpi_softc	   *acpi_sc;
+    ACPI_HANDLE	handle;
+    int		error;
+
+    handle = acpi_get_handle(dev);
+    AcpiInstallNotifyHandler(handle, ACPI_DEVICE_NOTIFY,
+			     acpi_acad_notify_handler, dev);
+    /* XXX Installing system notify is not so good */
+    AcpiInstallNotifyHandler(handle, ACPI_SYSTEM_NOTIFY,
+			     acpi_acad_notify_handler, dev);
+
+    if ((sc = device_get_softc(dev)) == NULL)
+	return (ENXIO);
+
+    error = acpi_register_ioctl(ACPIIO_ACAD_GET_STATUS, acpi_acad_ioctl, dev);
+    if (error != 0)
+	return (error);
+
+    if (device_get_unit(dev) == 0) {
+	acpi_sc = acpi_device_get_parent_softc(dev);
+	SYSCTL_ADD_PROC(&acpi_sc->acpi_sysctl_ctx,
+			SYSCTL_CHILDREN(acpi_sc->acpi_sysctl_tree),
+			OID_AUTO, "acline", CTLTYPE_INT | CTLFLAG_RD,
+			&sc->status, 0, acpi_acad_sysctl, "I", "");
+    }
+
+    /* Get initial status after whole system is up. */
+    sc->status = -1;
+    sc->initializing = 0;
+
+    AcpiOsQueueForExecution(OSD_PRIORITY_LO, acpi_acad_init_acline, dev);
+
+    return (0);
+}
 
 static int
 acpi_acad_ioctl(u_long cmd, caddr_t addr, void *arg)
 {
-	device_t		 dev;
-	struct acpi_acad_softc	*sc;
+    struct acpi_acad_softc *sc;
+    device_t dev;
 
-	dev = (device_t)arg;
-	if ((sc = device_get_softc(dev)) == NULL) {
-		return(ENXIO);
-	}
+    dev = (device_t)arg;
+    if ((sc = device_get_softc(dev)) == NULL)
+	return(ENXIO);
 
-        /*
-         * No security check required: information retrieval only.  If
-         * new functions are added here, a check might be required.
-         */
+    /*
+     * No security check required: information retrieval only.  If
+     * new functions are added here, a check might be required.
+     */
+    switch (cmd) {
+    case ACPIIO_ACAD_GET_STATUS:
+	acpi_acad_get_status(dev);
+	*(int *)addr = sc->status;
+	break;
+    default:
+	break;
+    }
 
-	switch (cmd) {
-	case ACPIIO_ACAD_GET_STATUS:
-		acpi_acad_get_status(dev);
-		*(int *)addr = sc->status;
-		break;
-	}
-
-	return(0);
+    return (0);
 }
 
 static int
 acpi_acad_sysctl(SYSCTL_HANDLER_ARGS)
 {
-	int     val;
-	int     error;
+    int val, error;
 
-	if (acpi_acad_get_acline(&val)) {
-		return (ENXIO);
-	}
+    if (acpi_acad_get_acline(&val) != 0)
+	return (ENXIO);
 
-	val = *(u_int *)oidp->oid_arg1;
-	error = sysctl_handle_int(oidp, &val, 0, req);
-	return (error);
+    val = *(u_int *)oidp->oid_arg1;
+    error = sysctl_handle_int(oidp, &val, 0, req);
+    return (error);
 }
 
 static void
 acpi_acad_init_acline(void *arg)
 {
-	int		retry;
-	int		status;
-	device_t	dev = (device_t)arg;
-	struct acpi_acad_softc	*sc = device_get_softc(dev);
-#define ACPI_ACAD_RETRY_MAX	6
+    struct acpi_acad_softc *sc;
+    device_t	dev;
+    int		retry, status;
 
-	if (sc->initializing) {
-		return;
-	}
+    dev = (device_t)arg;
+    sc = device_get_softc(dev);
+    if (sc->initializing)
+	return;
 
-	sc->initializing = 1;
+    sc->initializing = 1;
+    ACPI_VPRINT(dev, acpi_device_get_parent_softc(dev),
+		"acline initialization start\n");
 
-	ACPI_VPRINT(dev, acpi_device_get_parent_softc(dev),
-		    "acline initialization start\n");
+    status = 0;
+    for (retry = 0; retry < ACPI_ACAD_RETRY_MAX; retry++, AcpiOsSleep(10, 0)) {
+	acpi_acad_get_status(dev);
+	if (status != sc->status)
+	    break;
+    }
 
-	status = 0;
-	for (retry = 0; retry < ACPI_ACAD_RETRY_MAX; retry++, AcpiOsSleep(10, 0)) {
-		acpi_acad_get_status(dev);
-		if (status != sc->status)
-			break;
-	}
-
-	ACPI_VPRINT(dev, acpi_device_get_parent_softc(dev),
-		    "acline initialization done, tried %d times\n", retry+1);
-
-	sc->initializing = 0;
+    sc->initializing = 0;
+    ACPI_VPRINT(dev, acpi_device_get_parent_softc(dev),
+		"acline initialization done, tried %d times\n", retry+1);
 }
 
 /*
  * Public interfaces.
  */
-
 int
 acpi_acad_get_acline(int *status)
 {
-	device_t		 dev;
-	struct acpi_acad_softc	*sc;
+    struct acpi_acad_softc *sc;
+    device_t dev;
 
-	if ((dev = devclass_get_device(acpi_acad_devclass, 0)) == NULL) {
-		return (ENXIO);
-	}
+    dev = devclass_get_device(acpi_acad_devclass, 0);
+    if (dev == NULL)
+	return (ENXIO);
+    sc = device_get_softc(dev);
+    if (sc == NULL)
+	return (ENXIO);
 
-	if ((sc = device_get_softc(dev)) == NULL) {
-		return (ENXIO);
-	}
+    acpi_acad_get_status(dev);
+    *status = sc->status;
 
-	acpi_acad_get_status(dev);
-	*status = sc->status;
-
-	return (0);
+    return (0);
 }
-
