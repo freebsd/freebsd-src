c759a3ca06303d9297cfec306429941e95316206
Alan Cox <alc@FreeBSD.org>
2003-08-12 20:10:32 +0000

    Reduce the size of the vm object on 64-bit architectures by moving a field within the structure.

--- sys/vm/vm_object.h
+++ sys/vm/vm_object.h
@@ -96,8 +96,8 @@ struct vm_object {
 	LIST_ENTRY(vm_object) shadow_list; /* chain of shadow objects */
 	TAILQ_HEAD(, vm_page) memq;	/* list of resident pages */
 	vm_page_t root;			/* root of the resident page splay tree */
-	int generation;			/* generation ID */
 	vm_pindex_t size;		/* Object size */
+	int generation;			/* generation ID */
 	int ref_count;			/* How many refs?? */
 	int shadow_count;		/* how many objects that this is a shadow for */
 	objtype_t type;			/* type of pager */
