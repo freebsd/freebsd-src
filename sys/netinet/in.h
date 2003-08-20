1fbe9ae9d63864e6fda19fac4a77ff051f4fd7a7
bms <bms@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-08-20 14:46:40 +0000

    Add the IP_ONESBCAST option, to enable undirected IP broadcasts to be sent on specific interfaces. This is required by aodvd, and may in future help us in getting rid of the requirement for BPF from our import of isc-dhcp.
    Suggested by:   fenestro
    Obtained from:  BSD/OS
    Reviewed by:    mini, sam
    Approved by:    jake (mentor)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@119178 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/in.h
+++ sys/netinet/in.h
@@ -388,6 +388,8 @@ __END_DECLS
 #define	IP_IPSEC_POLICY		21   /* int; set/get security policy */
 #define	IP_FAITH		22   /* bool; accept FAITH'ed connections */
 
+#define	IP_ONESBCAST		23   /* bool: send all-ones broadcast */
+
 #define	IP_FW_ADD     		50   /* add a firewall rule to chain */
 #define	IP_FW_DEL    		51   /* delete a firewall rule from chain */
 #define	IP_FW_FLUSH   		52   /* flush firewall rule chain */
