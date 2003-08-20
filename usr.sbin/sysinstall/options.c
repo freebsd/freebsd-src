4cd54ca65871612a7444561490ef5bc279ebef28
Warner Losh <imp@FreeBSD.org>
2003-08-20 06:24:12 +0000

    Add a new variable 'skipPCCARD'.  This variable will cause sysinstall to ignore all PC Card devices.
    Submitted by: Anders Nordby
    PR: bin/37650
    
    MFC After: 2 weeks

--- usr.sbin/sysinstall/options.c
+++ usr.sbin/sysinstall/options.c
@@ -122,6 +122,8 @@ static Option Options[] = {
       OPT_IS_VAR,	NULL,			VAR_TRY_DHCP,		varCheck	},
 { "IPv6",		"Attempt IPv6 configuration of interfaces",
       OPT_IS_VAR,	NULL,			VAR_TRY_RTSOL,		varCheck	},
+{ "Skip PCCARD",	"Skip PC-card probing, do not use PC-card devices for installation",
+      OPT_IS_VAR,	NULL,			VAR_SKIP_PCCARD,	varCheck	},
 { "FTP username",	"Username and password to use instead of anonymous",
       OPT_IS_FUNC,	mediaSetFTPUserPass,	VAR_FTP_USER,		varCheck	},
 { "Editor",		"Which text editor to use during installation",
