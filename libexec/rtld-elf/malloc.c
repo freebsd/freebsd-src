566ef09073e1467debf7c0b776a2dce66da00324
Warner Losh <imp@FreeBSD.org>
2003-08-22 02:22:59 +0000

    Very minor style nit: sort include files alphabetically.

--- libexec/rtld-elf/malloc.c
+++ libexec/rtld-elf/malloc.c
@@ -51,10 +51,10 @@ static char *rcsid = "$FreeBSD$";
 #include <err.h>
 #include <paths.h>
 #include <stdarg.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
 #include <unistd.h>
 #include <sys/param.h>
 #include <sys/mman.h>
