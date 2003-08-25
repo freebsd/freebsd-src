57fdddeed952f85aa6a02010f18dade9665ceae4
obrien <obrien@FreeBSD.org>
2003-08-25 23:30:41 +0000

    Use __FBSDID(). Also some minor style cleanups.

--- sys/boot/common/interp_forth.c
+++ sys/boot/common/interp_forth.c
@@ -1,4 +1,4 @@
-/*
+/*-
  * Copyright (c) 1998 Michael Smith <msmith@freebsd.org>
  * All rights reserved.
  *
@@ -22,9 +22,10 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $FreeBSD$
  */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #include <sys/param.h>		/* to pick up __FreeBSD_version */
 #include <string.h>
--- sys/boot/common/isapnp.c
+++ sys/boot/common/isapnp.c
@@ -1,4 +1,4 @@
-/*
+/*-
  * Copyright (c) 1998, Michael Smith
  * Copyright (c) 1996, Sujal M. Patel
  * All rights reserved.
@@ -23,9 +23,10 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $FreeBSD$
  */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 /*
  * Machine-independant ISA PnP enumerator implementing a subset of the
