9d9494d58d14ab797db5ae8f3082a45dc5568e3e
imp <imp@FreeBSD.org>
2003-08-27 23:30:01 +0000

    Fix location of $FreeBSD$ from last commit.

--- sys/dev/exca/exca.c
+++ sys/dev/exca/exca.c
@@ -1,7 +1,3 @@
-
-
-#include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
 /*
  * Copyright (c) 2002 M Warner Losh.  All rights reserved.
  *
@@ -55,6 +51,9 @@ __FBSDID("$FreeBSD$");
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
 #include <sys/systm.h>
