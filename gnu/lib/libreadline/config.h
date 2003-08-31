9923fc419f2af63d4061dba25715738b6ecce316
Andrey A. Chernov <ache@FreeBSD.org>
2003-08-31 19:15:35 +0000

    Upgrade to 4.3

--- gnu/lib/libreadline/config.h
+++ gnu/lib/libreadline/config.h
@@ -1,6 +1,6 @@
 /* $FreeBSD$ */
 /* config.h.  Generated automatically by configure.  */
-/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+/* config.h.in.  Maintained by hand. */
 
 /* Define if on MINIX.  */
 /* #undef _MINIX */
@@ -8,13 +8,33 @@
 /* Define as the return type of signal handlers (int or void).  */
 #define RETSIGTYPE void
 
+/* Characteristics of the compiler. */
+/* #undef const */
+
+/* #undef size_t */
+
+/* #undef ssize_t */
+
+#define PROTOTYPES 1
+
+/* #undef __CHAR_UNSIGNED__ */
+
 /* Define if the `S_IS*' macros in <sys/stat.h> do not work properly.  */
 /* #undef STAT_MACROS_BROKEN */
 
 #define VOID_SIGHANDLER 1
 
+/* Define if you have the isascii function. */
+#define HAVE_ISASCII 1
+
+/* Define if you have the isxdigit function. */
+#define HAVE_ISXDIGIT 1
+
 /* Define if you have the lstat function. */
 #define HAVE_LSTAT 1
+
+/* Define if you have the mbsrtowcs function. */
+#define HAVE_MBSRTOWCS 1
 
 /* Define if you have the memmove function. */
 #define HAVE_MEMMOVE 1
@@ -28,31 +48,58 @@
 /* Define if you have the setenv function.  */
 #define HAVE_SETENV 1
 
-/* Define if you have the strcasecmp function.  */
-#define HAVE_STRCASECMP 1
-
 /* Define if you have the setlocale function. */
 #define HAVE_SETLOCALE 1
 
-/* Define if you have the tcgetattr function.  */
-#define HAVE_TCGETATTR 1
+/* Define if you have the strcasecmp function.  */
+#define HAVE_STRCASECMP 1
 
 /* Define if you have the strcoll function.  */
 #define HAVE_STRCOLL 1
 
 /* #undef STRCOLL_BROKEN */
 
+/* Define if you have the strpbrk function.  */
+#define HAVE_STRPBRK 1
+
+/* Define if you have the tcgetattr function.  */
+#define HAVE_TCGETATTR 1
+
+/* Define if you have the vsnprintf function.  */
+#define HAVE_VSNPRINTF 1
+
+/* Define if you have the wcwidth function.  */
+#define HAVE_WCWIDTH 1
+
 /* Define if you have the <dirent.h> header file.  */
 #define HAVE_DIRENT_H 1
 
+/* Define if you have the <langinfo.h> header file.  */
+#define HAVE_LANGINFO_H 1
+
+/* Define if you have the <limits.h> header file.  */
+#define HAVE_LIMITS_H 1
+
+/* Define if you have the <locale.h> header file.  */
+#define HAVE_LOCALE_H 1
+
+/* Define if you have the <memory.h> header file.  */
+#define HAVE_MEMORY_H 1
+
 /* Define if you have the <ndir.h> header file.  */
 /* #undef HAVE_NDIR_H */
+
+/* Define if you have the <stdarg.h> header file.  */
+#define HAVE_STDARG_H 1
 
 /* Define if you have the <stdlib.h> header file.  */
 #define HAVE_STDLIB_H 1
 
 /* Define if you have the <string.h> header file.  */
 #define HAVE_STRING_H 1
+
+/* Define if you have the <strings.h> header file.  */
+#define HAVE_STRINGS_H 1
 
 /* Define if you have the <sys/dir.h> header file.  */
 /* #undef HAVE_SYS_DIR_H */
@@ -90,15 +137,21 @@
 /* Define if you have the <varargs.h> header file.  */
 #define HAVE_VARARGS_H 1
 
-/* Define if you have the <stdarg.h> header file.  */
-#define HAVE_STDARG_H 1
+/* Define if you have the <wchar.h> header file.  */
+#define HAVE_WCHAR_H 1
 
-#define HAVE_LOCALE_H 1
+/* Define if you have the <varargs.h> header file.  */
+#define HAVE_WCTYPE_H 1
+
+#define HAVE_MBSTATE_T 1
+
+/* Define if you have <langinfo.h> and nl_langinfo(CODESET). */
+#define HAVE_LANGINFO_CODESET 1
 
 /* Definitions pulled in from aclocal.m4. */
 #define VOID_SIGHANDLER 1
 
-#define GWINSZ_IN_SYS_IOCTL 1
+/* #undef GWINSZ_IN_SYS_IOCTL */
 
 #define STRUCT_WINSIZE_IN_SYS_IOCTL 1
 
@@ -126,7 +179,6 @@
 
 #define HAVE_POSIX_SIGSETJMP 1
 
-/* config.h.bot */
 /* modify settings or make new ones based on what autoconf tells us. */
 
 /* Ultrix botches type-ahead when switching from canonical to
