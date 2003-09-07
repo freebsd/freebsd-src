1477dfc0549e12d588aeec550c7cb57269599415
ru <ru@FreeBSD.org>
2003-09-07 12:59:22 +0000

    Implement sed(1) commands using the make(1)'s RE variable modifier. (This almost eliminates the need of a sed(1) during installworld.)

--- share/mk/bsd.man.mk
+++ share/mk/bsd.man.mk
@@ -173,7 +173,7 @@ _maninstall: ${MAN}
 .endif
 .endfor
 .else
-	@set `echo ${.ALLSRC} " " | sed 's/\.\([^.]*\) /.\1 \1 /g'`; \
+	@set ${.ALLSRC:C/\.([^.]*)$/.\1 \1/}; \
 	while : ; do \
 		case $$# in \
 			0) break;; \
@@ -204,7 +204,7 @@ _maninstall: ${MAN}
 .endif
 
 .if !defined(NOMLINKS) && defined(MLINKS) && !empty(MLINKS)
-	@set `echo ${MLINKS} " " | sed 's/\.\([^.]*\) /.\1 \1 /g'`; \
+	@set ${MLINKS:C/\.([^.]*)$/.\1 \1/}; \
 	while : ; do \
 		case $$# in \
 			0) break;; \
@@ -219,7 +219,7 @@ _maninstall: ${MAN}
 		ln $${l}${ZEXT} $${t}${ZEXT}; \
 	done
 .if defined(MANBUILDCAT) && !empty(MANBUILDCAT)
-	@set `echo ${MLINKS} " " | sed 's/\.\([^.]*\) /.\1 \1 /g'`; \
+	@set ${MLINKS:C/\.([^.]*)$/.\1 \1/}; \
 	while : ; do \
 		case $$# in \
 			0) break;; \
