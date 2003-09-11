353a628ec945617e27be547f96cf1fcb299982c7
obrien <obrien@FreeBSD.org>
2003-09-11 03:28:21 +0000

    Deal with the LOGIN_NAME_MAX issue in the NetBSD->FreeBSD translation^H^H^Hhack layer.

--- libexec/lukemftpd/nbsd2fbsd.h
+++ libexec/lukemftpd/nbsd2fbsd.h
@@ -15,3 +15,10 @@ typedef	__size_t	size_t;
 #endif
 long long strsuftollx(const char *, const char *,
     long long, long long, char *, size_t);
+
+/*
+ * IEEE Std 1003.1c-95, adopted in X/Open CAE Specification Issue 5 Version 2
+ */
+#if __POSIX_VISIBLE >= 199506 || __XSI_VISIBLE >= 500
+#define	LOGIN_NAME_MAX	MAXLOGNAME	/* max login name length (incl. NUL) */
+#endif
