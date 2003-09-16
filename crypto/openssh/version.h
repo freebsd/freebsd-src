bba06640fb79ad3d1a7dc87f43490c1343a8be7d
Jacques Vidrine <nectar@FreeBSD.org>
2003-09-16 17:51:06 +0000

    MFC buffer.c 1.1.1.7: Do not record expanded size before attempting to reallocate associated memory.

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -5,7 +5,7 @@
 
 #define SSH_VERSION		(ssh_version_get())
 #define SSH_VERSION_BASE	"OpenSSH_2.9"
-#define	SSH_VERSION_ADDENDUM	"FreeBSD localisations 20020307"
+#define	SSH_VERSION_ADDENDUM	"FreeBSD localisations 20030916"
 
 const char *ssh_version_get(void);
 void ssh_version_set_addendum(const char *add);
