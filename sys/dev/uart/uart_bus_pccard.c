de10d50d42dd5a6ffddcbbc32f349f76080e03bc
nyan <nyan@FreeBSD.org>
2003-09-23 09:20:03 +0000

    Remove unneeded includes.

--- sys/dev/uart/uart_bus_pccard.c
+++ sys/dev/uart/uart_bus_pccard.c
@@ -31,12 +31,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/bus.h>
 #include <sys/conf.h>
 #include <sys/kernel.h>
-#include <sys/lock.h>
-#include <sys/malloc.h>
-#include <sys/mutex.h>
 #include <sys/module.h>
-#include <sys/tty.h>
-#include <machine/bus_pio.h>
 #include <machine/bus.h>
 #include <machine/resource.h>
 
