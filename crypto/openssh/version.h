003bece945b5bc7fd9313f3aa3b3d1dfb23c4f81
Dag-Erling Smørgrav <des@FreeBSD.org>
2003-09-24 19:28:35 +0000

    Update version string.

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -5,7 +5,7 @@
 
 #define SSH_VERSION             (ssh_version_get())
 #define SSH_VERSION_BASE        "OpenSSH_3.5p1"
-#define SSH_VERSION_ADDENDUM    "FreeBSD-20030917"
+#define SSH_VERSION_ADDENDUM    "FreeBSD-20030924"
 
 const char *ssh_version_get(void);
 void ssh_version_set_addendum(const char *add);
