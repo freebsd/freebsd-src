351a02de723eff86af83811f8aa6bc673d5d832e
peter <peter@FreeBSD.org>
2003-10-01 01:08:04 +0000

    Use __register_t instead of register_t, otherwise <sys/types.h> is a prerequisite for <ucontext.h> on amd64.  Oops.

--- sys/amd64/include/ucontext.h
+++ sys/amd64/include/ucontext.h
@@ -37,30 +37,30 @@ typedef struct __mcontext {
 	 * sigcontext. So that we can support sigcontext
 	 * and ucontext_t at the same time.
 	 */
-	register_t	mc_onstack;		/* XXX - sigcontext compat. */
-	register_t	mc_rdi;			/* machine state (struct trapframe) */
-	register_t	mc_rsi;
-	register_t	mc_rdx;
-	register_t	mc_rcx;
-	register_t	mc_r8;
-	register_t	mc_r9;
-	register_t	mc_rax;
-	register_t	mc_rbx;
-	register_t	mc_rbp;
-	register_t	mc_r10;
-	register_t	mc_r11;
-	register_t	mc_r12;
-	register_t	mc_r13;
-	register_t	mc_r14;
-	register_t	mc_r15;
-	register_t	mc_trapno;
-	register_t	mc_addr;
-	register_t	mc_err;
-	register_t	mc_rip;
-	register_t	mc_cs;
-	register_t	mc_rflags;
-	register_t	mc_rsp;
-	register_t	mc_ss;
+	__register_t	mc_onstack;		/* XXX - sigcontext compat. */
+	__register_t	mc_rdi;			/* machine state (struct trapframe) */
+	__register_t	mc_rsi;
+	__register_t	mc_rdx;
+	__register_t	mc_rcx;
+	__register_t	mc_r8;
+	__register_t	mc_r9;
+	__register_t	mc_rax;
+	__register_t	mc_rbx;
+	__register_t	mc_rbp;
+	__register_t	mc_r10;
+	__register_t	mc_r11;
+	__register_t	mc_r12;
+	__register_t	mc_r13;
+	__register_t	mc_r14;
+	__register_t	mc_r15;
+	__register_t	mc_trapno;
+	__register_t	mc_addr;
+	__register_t	mc_err;
+	__register_t	mc_rip;
+	__register_t	mc_cs;
+	__register_t	mc_rflags;
+	__register_t	mc_rsp;
+	__register_t	mc_ss;
 
 	long	mc_len;			/* sizeof(mcontext_t) */
 #define	_MC_FPFMT_NODEV		0x10000	/* device not present or configured */
