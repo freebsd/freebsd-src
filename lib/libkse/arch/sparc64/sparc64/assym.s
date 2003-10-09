daa5c8dd0e2bcd0d153320bc53996b11192cc2e8
deischen <deischen@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-10-09 02:32:28 +0000

    Add preliminary sparc64 support to libpthread.  This does not yet work, but hopefully someone familiar with the sparc64 port can pick up the reins.
    Submitted by:	jake
    With mods by:	deischen
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@120924 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ lib/libkse/arch/sparc64/sparc64/assym.s
@@ -0,0 +1,13 @@
+/*
+ * Offsets into structures used from asm.  Must be kept in sync with
+ * appropriate headers.
+ *
+ * $FreeBSD$
+ */
+
+#define	UC_MCONTEXT	0x40
+
+#define	MC_GLOBAL	0x0
+#define	MC_OUT		0x40
+#define	MC_TPC		0xc8
+#define	MC_TNPC		0xc0
