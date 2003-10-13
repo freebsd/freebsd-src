f061acd5371df1360ad5fd845d257ba60d4893a1
bde <bde@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-10-13 04:44:55 +0000

    Fixed some style bugs in the removal of __P(()).  Blind removal of spaces before __P(()) outdented continuation lines to column 0.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@121060 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libkvm/kvm.h
+++ lib/libkvm/kvm.h
@@ -83,12 +83,12 @@ struct kinfo_proc *
 int	  kvm_getswapinfo(kvm_t *, struct kvm_swap *, int, int);
 int	  kvm_nlist(kvm_t *, struct nlist *);
 kvm_t	 *kvm_open
-(const char *, const char *, const char *, int, const char *);
+	    (const char *, const char *, const char *, int, const char *);
 kvm_t	 *kvm_openfiles
-(const char *, const char *, const char *, int, char *);
+	    (const char *, const char *, const char *, int, char *);
 ssize_t	  kvm_read(kvm_t *, unsigned long, void *, size_t);
 ssize_t	  kvm_uread
-(kvm_t *, struct kinfo_proc *, unsigned long, char *, size_t);
+	    (kvm_t *, struct kinfo_proc *, unsigned long, char *, size_t);
 ssize_t	  kvm_write(kvm_t *, unsigned long, const void *, size_t);
 __END_DECLS
 
