3206e56993ec903bd4f48a83384a9456994cdb74
Poul-Henning Kamp <phk@FreeBSD.org>
2003-10-21 08:27:05 +0000

    Remove clause 3 in the copyrights (according to permission in src/COPYRIGHT) Remove trailing whitespace.

--- usr.sbin/mtree/compare.c
+++ usr.sbin/mtree/compare.c
@@ -10,11 +10,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -269,7 +265,7 @@ typeerr:		LABEL;
 			tab = "\t";
 		} else if (strcmp(new_digest, s->sha1digest)) {
 			LABEL;
-			printf("%sSHA-1 expected %s found %s\n", 
+			printf("%sSHA-1 expected %s found %s\n",
 			       tab, s->sha1digest, new_digest);
 			tab = "\t";
 		}
