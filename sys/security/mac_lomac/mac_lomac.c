5589c8e5edc3d1bf32b04fd590d6ae0e9d895aee
rwatson <rwatson@FreeBSD.org>
2003-10-21 15:18:26 +0000

    Line-break sync to the MAC development branch.

--- sys/security/mac_lomac/mac_lomac.c
+++ sys/security/mac_lomac/mac_lomac.c
@@ -2716,4 +2716,5 @@ static struct mac_policy_ops mac_lomac_ops =
 };
 
 MAC_POLICY_SET(&mac_lomac_ops, mac_lomac, "TrustedBSD MAC/LOMAC",
-    MPC_LOADTIME_FLAG_NOTLATE | MPC_LOADTIME_FLAG_LABELMBUFS, &mac_lomac_slot);
+    MPC_LOADTIME_FLAG_NOTLATE | MPC_LOADTIME_FLAG_LABELMBUFS,
+    &mac_lomac_slot);
