7083ddc078ae4c6b681b6db054c6f337c92cf46c
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-10-26 20:09:57 +0000

    Sync to 1.75 of pccarddevs
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@121585 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/pccard/pccarddevs.h
+++ sys/dev/pccard/pccarddevs.h
@@ -4,7 +4,7 @@
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	FreeBSD: src/sys/dev/pccard/pccarddevs,v 1.74 2003/10/26 06:31:46 imp Exp 
+ *	FreeBSD: src/sys/dev/pccard/pccarddevs,v 1.75 2003/10/26 20:09:18 imp Exp 
  */
 /* $NetBSD: pcmciadevs,v 1.186 2003/09/16 08:26:37 onoe Exp $ */
 /* $OpenBSD: pcmciadevs,v 1.93 2002/06/21 08:31:10 henning Exp $ */
@@ -602,8 +602,11 @@
 #define	PCMCIA_PRODUCT_OLICOM_TR	0x2132
 #define	PCMCIA_STR_OLICOM_TR	"GoCard Token Ring 16/4"
 #define	PCMCIA_CIS_OLICOM_OC2220	{ NULL, NULL, NULL, NULL }
-#define	PCMCIA_PRODUCT_OLICOM_OC2220	0x2220
+#define	PCMCIA_PRODUCT_OLICOM_OC2220	0x2022
 #define	PCMCIA_STR_OLICOM_OC2220	"GoCard Ethernet"
+#define	PCMCIA_CIS_OLICOM_OC2232	{ NULL, NULL, NULL, NULL }
+#define	PCMCIA_PRODUCT_OLICOM_OC2232	0x3222
+#define	PCMCIA_STR_OLICOM_OC2232	"GoCard Combo Eth/Modem 336"
 
 /* Ositech Products */
 #define	PCMCIA_CIS_OSITECH_TRUMPCARD_SOD	{ NULL, NULL, NULL, NULL }
