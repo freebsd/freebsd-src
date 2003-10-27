d74e8d0250e299cf2b961d38b18ca927d7b12bd9
jeff <jeff@FreeBSD.org>
2003-10-27 07:15:47 +0000

    - Don't set td_priority directly here, use sched_prio().

--- sys/kern/kern_resource.c
+++ sys/kern/kern_resource.c
@@ -409,7 +409,7 @@ rtp_to_pri(struct rtprio *rtp, struct ksegrp *kg)
 	sched_class(kg, rtp->type);
 	if (curthread->td_ksegrp == kg) {
 		curthread->td_base_pri = kg->kg_user_pri;
-		curthread->td_priority = kg->kg_user_pri; /* XXX dubious */
+		sched_prio(curthread, kg->kg_user_pri); /* XXX dubious */
 	}
 	return (0);
 }
