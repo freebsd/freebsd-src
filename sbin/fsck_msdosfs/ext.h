6cf561e580bd0994aeafa8d384a567c3a9cca850
trhodes <trhodes@FreeBSD.org>
2003-10-30 09:08:09 +0000

    In check.c: Avoid shadowing declarations. Avoid compairing signed and unsigned types.

--- sbin/fsck_msdosfs/ext.h
+++ sbin/fsck_msdosfs/ext.h
@@ -49,8 +49,6 @@ extern int alwaysyes;	/* assume "yes" for all questions */
 extern int preen;	/* we are preening */
 extern int rdonly;	/* device is opened read only (supersedes above) */
 
-extern char *fname;	/* file system currently checked */
-
 extern struct dosDirEntry *rootDir;
 
 /*
