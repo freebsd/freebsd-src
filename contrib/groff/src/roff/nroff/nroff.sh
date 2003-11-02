f5b8c6dcbca4f0b9bba3fa8f1b4fe46a4d6ab6e2
Ruslan Ermilov <ru@FreeBSD.org>
2003-11-02 07:25:24 +0000

    Sort encodings.

--- contrib/groff/src/roff/nroff/nroff.sh
+++ contrib/groff/src/roff/nroff/nroff.sh
@@ -7,30 +7,32 @@ prog="$0"
 # First try the "locale charmap" command, because it's most reliable.
 # On systems where it doesn't exist, look at the environment variables.
 case "`locale charmap 2>/dev/null`" in
-  UTF-8)
-    T=-Tutf8 ;;
   ISO*8859-1 | ISO*8859-15)
     T=-Tlatin1 ;;
-  IBM-1047)
-    T=-Tcp1047 ;;
   KOI8-R)
     T=-Tkoi8-r ;;
+  UTF-8)
+    T=-Tutf8 ;;
+  IBM-1047)
+    T=-Tcp1047 ;;
   *)
     case "${LC_ALL-${LC_CTYPE-${LANG}}}" in
-      *.UTF-8)
-        T=-Tutf8 ;;
       iso_8859_1 | *.ISO*8859-1 | *.ISO*8859-15)
         T=-Tlatin1 ;;
-      *.IBM-1047)
-        T=-Tcp1047 ;;
       *.KOI8-R)
         T=-Tkoi8-r ;;
+      *.UTF-8)
+        T=-Tutf8 ;;
+      *.IBM-1047)
+        T=-Tcp1047 ;;
       *)
         case "$LESSCHARSET" in
-          utf-8)
-            T=-Tutf8 ;;
           latin1)
             T=-Tlatin1 ;;
+          koi8-r)
+            T=-Tkoi8-r ;;
+          utf-8)
+            T=-Tutf8 ;;
           cp1047)
             T=-Tcp1047 ;;
           *)
