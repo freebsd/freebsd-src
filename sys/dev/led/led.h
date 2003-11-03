e7252822dd46b4e49631614093011993ce9934b3
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-11-03 09:47:30 +0000

    Add a generic LED driver for flashing lamps.
    The hardware driver decides the name under /dev/led and provides
    the function to turn the lamp on/off.
    
    All leds are serviced by a single timeout which runs at a basic rate
    of hz/10.
    
    The LED is controlled by ascii strings as follows.
    
    0	Turn off.
    1	Turn on.
    f	Flash: _-
    f2	Flash: __--
    f3	Flash: ___---
    f4...f9 etc.
    d%d	Digits.  "d12": -__________-_-______________________________
    s%s	String, roll your own:
    	'a-j' gives on for (1...10)/10 sec.
    	'A-J' gives on for (1...10)/10 sec.
    	'sAaAbBa': _-_--__-
    m%s	Morse
    	'.' dot
    	'-' dash
    	' ' letter space
    	'\n' word space
    
    My mdoc skills do not reach to express that.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@121941 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ sys/dev/led/led.h
@@ -0,0 +1,21 @@
+/*-
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@FreeBSD.org> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+ *
+ */
+
+#ifndef _DEV_LED_H
+#define _DEV_LED_H
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+typedef	void led_t(void *, int);
+dev_t led_create(led_t *func, void *priv, char const *name);
+void led_destroy(dev_t);
+
+#endif /* _DEV_LED_H */
