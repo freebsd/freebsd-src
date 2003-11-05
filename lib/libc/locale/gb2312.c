966dcbbc7300bd46430035b3b98c6966bef50a16
davidxu <davidxu@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-11-05 22:52:51 +0000

    Add gb2312 encoding.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@122145 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ lib/libc/locale/gb2312.c
@@ -0,0 +1,116 @@
+/*-
+ * Copyright (c) 2003 David Xu <davidxu@freebsd.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/types.h>
+#include <runetype.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <wchar.h>
+
+extern size_t (*__mbrtowc)(wchar_t * __restrict, const char * __restrict,
+    size_t, mbstate_t * __restrict);
+extern size_t (*__wcrtomb)(char * __restrict, wchar_t, mbstate_t * __restrict);
+
+int	_GB2312_init(_RuneLocale *);
+size_t	_GB2312_mbrtowc(wchar_t * __restrict, const char * __restrict, size_t,
+	    mbstate_t * __restrict);
+size_t	_GB2312_wcrtomb(char * __restrict, wchar_t, mbstate_t * __restrict);
+
+int
+_GB2312_init(_RuneLocale *rl)
+{
+
+	_CurrentRuneLocale = rl;
+	__mbrtowc = _GB2312_mbrtowc;
+	__wcrtomb = _GB2312_wcrtomb;
+	__mb_cur_max = 2;
+	return (0);
+}
+
+static inline int
+_GB2312_check(const char *str, size_t n)
+{
+	const u_char *s = (const u_char *)str;
+
+	if (n == 0)
+		/* Incomplete multibyte sequence */
+		return (-2);
+	if (s[0] >= 0xa1 && s[0] <= 0xfe) {
+		if (n < 2)
+			/* Incomplete multibyte sequence */
+			return (-2);
+		if (s[1] < 0xa1 || s[1] > 0xfe)
+			/* Invalid multibyte sequence */
+			return (-1);
+		return (2);
+	} else if (s[0] & 0x80) {
+		/* Invalid multibyte sequence */
+		return (-1);
+	} 
+	return (1);
+}
+
+size_t
+_GB2312_mbrtowc(wchar_t * __restrict pwc, const char * __restrict s, size_t n,
+    mbstate_t * __restrict ps __unused)
+{
+	wchar_t wc;
+	int i, len;
+
+	if (s == NULL)
+		/* Reset to initial shift state (no-op) */
+		return (0);
+	if ((len = _GB2312_check(s, n)) < 0)
+		return ((size_t)len);
+	wc = 0;
+	i = len;
+	while (i-- > 0)
+		wc = (wc << 8) | (unsigned char)*s++;
+	if (pwc != NULL)
+		*pwc = wc;
+	return (wc == L'\0' ? 0 : len);
+}
+
+size_t
+_GB2312_wcrtomb(char * __restrict s, wchar_t wc,
+    mbstate_t * __restrict ps __unused)
+{
+
+	if (s == NULL)
+		/* Reset to initial shift state (no-op) */
+		return (1);
+	if (wc & 0x8000) {
+		*s++ = (wc >> 8) & 0xff;
+		*s = wc & 0xff;
+		return (2);
+	}
+	*s = wc & 0xff;
+	return (1);
+}
