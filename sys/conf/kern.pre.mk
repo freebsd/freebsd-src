d00ce919735feb247d7f4c2edad5e997e41984c0
Bruce Evans <bde@FreeBSD.org>
2003-11-05 12:20:16 +0000

    Moved $FreeBSD$ to the beginning of the file.
    Don't put the name of the file in a comment.  $FreeBSD$ gives more than
    enough about the file's pathname.
    
    Fixed misdescription of the file.  It isn't the whole unified Makefile...
    
    Moved the settings of WERROR and of the standard extra CFLAGS
    -finline-limit and -fno-strict-aliasing to a less wrong place.  They
    were in the section for profiling.

--- sys/conf/kern.pre.mk
+++ sys/conf/kern.pre.mk
@@ -1,10 +1,7 @@
-# kern.pre.mk
-#
-# Unified Makefile for building kernels.  This includes all the definitions
-# that need to be included before %BEFORE_DEPEND
-#
 # $FreeBSD$
-#
+
+# Part of a unified Makefile for building kernels.  This part contains all
+# of the definitions that need to be before %BEFORE_DEPEND.
 
 # Can be overridden by makeoptions or /etc/make.conf
 KERNEL_KO?=	kernel
@@ -39,7 +36,9 @@ INCLUDES+= -I$S/contrib/dev/ath -I$S/contrib/dev/ath/freebsd
 INCLUDES+= -I$S/contrib/ngatm
 
 COPTS=	${INCLUDES} -D_KERNEL -include opt_global.h
-CFLAGS=	${COPTFLAGS} ${CWARNFLAGS} ${DEBUG} ${COPTS} -fno-common
+CFLAGS=	${COPTFLAGS} ${CWARNFLAGS} ${DEBUG} ${COPTS}
+CFLAGS+= -fno-common -finline-limit=${INLINE_LIMIT} -fno-strict-aliasing
+WERROR?= -Werror
 
 # XXX LOCORE means "don't declare C stuff" not "for locore.s".
 ASM_CFLAGS= -x assembler-with-cpp -DLOCORE ${CFLAGS}
@@ -55,9 +54,6 @@ PROF=	-pg
 .endif
 .endif
 DEFINED_PROF=	${PROF}
-WERROR?=	-Werror
-CFLAGS+=	-finline-limit=${INLINE_LIMIT} -fno-strict-aliasing
-
 
 # Put configuration-specific C flags last (except for ${PROF}) so that they
 # can override the others.
