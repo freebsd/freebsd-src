78bcdb61f6cc7e0791547a683ad217e1c7316f7c
Alexander Kabaev <kan@FreeBSD.org>
2003-11-07 03:19:35 +0000

    New GCC version.

--- contrib/gcc/version.c
+++ contrib/gcc/version.c
@@ -8,7 +8,7 @@
    please modify this string to indicate that, e.g. by putting your
    organization's name in parentheses at the end of the string.  */
 
-const char version_string[] = "3.3.1 [FreeBSD]";
+const char version_string[] = "3.3.3 [FreeBSD] 20031106";
 
 /* This is the location of the online document giving instructions for
    reporting bugs.  If you distribute a modified version of GCC,
