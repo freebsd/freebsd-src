a33b4c9d63dad8cad99fc13cbbfeaca3d96d0c2f
harti <harti@FreeBSD.org>
2003-11-10 09:17:34 +0000

    Add build infrastructure for libbsnmp and the modules. Add the netgraph module for the SNMP daemon.

---
+++ lib/libbsnmp/modules/snmp_mibII/mibII_oid.h
@@ -0,0 +1,35 @@
+/* $FreeBSD$ */
+/* generated file, don't edit - use ./genfiles */
+#define OID_ipAddrTable	20
+#define OIDLEN_ipAddrTable	8
+#define OIDX_ipAddrTable	{ 8, { 1, 3, 6, 1, 2, 1, 4, 20, } }
+#define OID_ifTable	2
+#define OIDLEN_ifTable	8
+#define OIDX_ifTable	{ 8, { 1, 3, 6, 1, 2, 1, 2, 2, } }
+#define OID_ifRcvAddressEntry	1
+#define OIDLEN_ifRcvAddressEntry	10
+#define OIDX_ifRcvAddressEntry	{ 10, { 1, 3, 6, 1, 2, 1, 31, 1, 4, 1, } }
+#define OID_ifMIB	31
+#define OIDLEN_ifMIB	7
+#define OIDX_ifMIB	{ 7, { 1, 3, 6, 1, 2, 1, 31, } }
+#define OID_ipMIB	48
+#define OIDLEN_ipMIB	7
+#define OIDX_ipMIB	{ 7, { 1, 3, 6, 1, 2, 1, 48, } }
+#define OID_tcpMIB	49
+#define OIDLEN_tcpMIB	7
+#define OIDX_tcpMIB	{ 7, { 1, 3, 6, 1, 2, 1, 49, } }
+#define OID_udpMIB	50
+#define OIDLEN_udpMIB	7
+#define OIDX_udpMIB	{ 7, { 1, 3, 6, 1, 2, 1, 50, } }
+#define OID_ipForward	24
+#define OIDLEN_ipForward	8
+#define OIDX_ipForward	{ 8, { 1, 3, 6, 1, 2, 1, 4, 24, } }
+#define OID_ifIndex	1
+#define OIDLEN_ifIndex	10
+#define OIDX_ifIndex	{ 10, { 1, 3, 6, 1, 2, 1, 2, 2, 1, 1, } }
+#define OID_linkDown	3
+#define OIDLEN_linkDown	10
+#define OIDX_linkDown	{ 10, { 1, 3, 6, 1, 6, 3, 1, 1, 5, 3, } }
+#define OID_linkUp	4
+#define OIDLEN_linkUp	10
+#define OIDX_linkUp	{ 10, { 1, 3, 6, 1, 6, 3, 1, 1, 5, 4, } }
---
+++ lib/libbsnmp/modules/snmp_netgraph/netgraph_oid.h
@@ -0,0 +1,5 @@
+/* $FreeBSD$ */
+/* generated file, don't edit - use ./genfiles */
+#define OID_begemotNg	2
+#define OIDLEN_begemotNg	9
+#define OIDX_begemotNg	{ 9, { 1, 3, 6, 1, 4, 1, 12325, 1, 2, } }
