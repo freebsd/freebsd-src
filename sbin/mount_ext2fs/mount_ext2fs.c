c226a76059675c865286ccd8e9c6859e6d8e10ac
johan <johan@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-11-13 19:18:43 +0000

    Make this WARNS=2 clean by 	- constifying copyright
    PR:		39867
    Submitted by:	Dan Lukes <dan@obluda.cz>
    Tested by:	make universe
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@122622 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/mount_ext2fs/mount_ext2fs.c
+++ sbin/mount_ext2fs/mount_ext2fs.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char copyright[] =
+static const char copyright[] =
 "@(#) Copyright (c) 1993, 1994\n\
 	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
