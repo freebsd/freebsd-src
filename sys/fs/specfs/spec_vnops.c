6b409c68f0d814b9b1055f4cd432b0bc0aaee43c
phk <phk@FreeBSD.org>
2003-11-13 09:58:09 +0000

    Initialize b_iooffset correctly.

--- sys/fs/specfs/spec_vnops.c
+++ sys/fs/specfs/spec_vnops.c
@@ -555,6 +555,7 @@ spec_freeblks(ap)
 	bp->b_dev = ap->a_vp->v_rdev;
 	bp->b_blkno = ap->a_addr;
 	bp->b_offset = dbtob(ap->a_addr);
+	bp->b_iooffset = bp->b_offset;
 	bp->b_bcount = ap->a_length;
 	BUF_KERNPROC(bp);
 	DEV_STRATEGY(bp);
