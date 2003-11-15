0b74e3df45cbbe2d54ee47f1c7d85e75b44ec7c4
bde <bde@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-11-15 10:04:06 +0000

    Changed the RB_PAUSE flag from 0x40000 to 0x100000 and marked the old value as reserved for internal use in boot blocks, because RB_PAUSE broke binary compatibility by usurping the RB_DUAL flag.  Probably no one except me has boot blocks for which this matters, since most boot blocks based on biosboot including pc98's boot2 can't boot elf kernels, and /boot/loader doesn't properly pass flags set by the previous stage.
    reboot.h:
    Also mark the historical RB_PROBEKBD flag (0x80000) as reserved for
    internal use in boot blocks.
    
    boot2.c:
    Added comments to inhibit usurping of other flags.
    
    Approved by:	guido, imp
    MFC after: 	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@122749 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/boot/i386/boot2/boot2.c
+++ sys/boot/i386/boot2/boot2.c
@@ -41,19 +41,30 @@ __FBSDID("$FreeBSD$");
 
 #define RBX_ASKNAME	0x0	/* -a */
 #define RBX_SINGLE	0x1	/* -s */
+/* 0x2 is reserved for log2(RB_NOSYNC). */
+/* 0x3 is reserved for log2(RB_HALT). */
+/* 0x4 is reserved for log2(RB_INITNAME). */
 #define RBX_DFLTROOT	0x5	/* -r */
 #define RBX_KDB 	0x6	/* -d */
+/* 0x7 is reserved for log2(RB_RDONLY). */
+/* 0x8 is reserved for log2(RB_DUMP). */
+/* 0x9 is reserved for log2(RB_MINIROOT). */
 #define RBX_CONFIG	0xa	/* -c */
 #define RBX_VERBOSE	0xb	/* -v */
 #define RBX_SERIAL	0xc	/* -h */
 #define RBX_CDROM	0xd	/* -C */
+/* 0xe is reserved for log2(RB_POWEROFF). */
 #define RBX_GDB 	0xf	/* -g */
 #define RBX_MUTE	0x10	/* -m */
-#define RBX_PAUSE	0x12	/* -p */
+/* 0x11 is reserved for log2(RB_SELFTEST). */
+/* 0x12 is reserved for boot programs. */
+/* 0x13 is reserved for boot programs. */
+#define RBX_PAUSE	0x14	/* -p */
 #define RBX_NOINTR	0x1c	/* -n */
+/* 0x1d is reserved for log2(RB_MULTIPLE) and is just misnamed here. */
 #define RBX_DUAL	0x1d	/* -D */
 #define RBX_PROBEKBD	0x1e	/* -P */
-/* 0x1f is reserved for the RB_BOOTINFO flag. */
+/* 0x1f is reserved for log2(RB_BOOTINFO). */
 
 /* pass: -a, -s, -r, -d, -c, -v, -h, -C, -g, -m, -p, -D */
 #define RBX_MASK	0x2005ffff
