cde6b0296595621b263e4d1b0c5c35e620505435
Bruce Evans <bde@FreeBSD.org>
2003-11-23 01:47:19 +0000

    MFC (1.23: changed RB_PAUSE to retrospectively avoid breaking binary compatibility, and marked the old value and RB_PROBEKBD as reserved for internal use in boot blocks.  Also unintentionally clobber a style fix in 1.22).
    The corresponding change to boot/boot2.c has not been not merged, since
    RELENG_4's boot2 doesn't actually support RB_PAUSE (or even the 7 year
    old flag RB_MUTE), so any plain merge would just add an incomplete set
    of comments to code that should be rewritten so that it doesn't need
    the comments.  RELENG_4's boot/loader does support these flags, and
    it doesn't need changing since it doesn't hard-code copies of them.

--- sys/sys/reboot.h
+++ sys/sys/reboot.h
@@ -61,7 +61,9 @@
 #define	RB_GDB		0x8000	/* use GDB remote debugger instead of DDB */
 #define	RB_MUTE		0x10000	/* start up with the console muted */
 #define	RB_SELFTEST	0x20000	/* don't complete the boot; do selftest */
-#define	RB_PAUSE	0x40000	/* pause after each output line during probe */
+#define	RB_RESERVED1	0x40000	/* reserved for internal use of boot blocks */
+#define	RB_RESERVED2	0x80000	/* reserved for internal use of boot blocks */
+#define	RB_PAUSE	0x100000 /* pause after each output line during probe */
 
 #define	RB_BOOTINFO	0x80000000	/* have `struct bootinfo *' arg */
 
@@ -104,4 +106,4 @@
 	((controller) << B_CONTROLLERSHIFT) | ((unit) << B_UNITSHIFT) | \
 	((partition) << B_PARTITIONSHIFT) | B_DEVMAGIC)
 
-#endif /* !_SYS_REBOOT_H_ */
+#endif
