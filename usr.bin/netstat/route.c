a36a8ab06d69627279afdd6cb31c3c2e69f77c5a
bms <bms@FreeBSD.org>
2003-11-28 17:34:23 +0000

    Fix some minor nits in netstat whereby large interface names would be truncated. In environments where many tunnel or vlan interfaces are created, interface names have high numbers which overflow the field width.
    PRs:		bin/52349, bin/35838
    Submitted by:	Mike Tancsa, Scot W. Hetzel
    Approved by:	re (rwatson)

--- usr.bin/netstat/route.c
+++ usr.bin/netstat/route.c
@@ -225,13 +225,13 @@ pr_family(int af1)
 #ifndef INET6
 #define	WID_DST_DEFAULT(af) 	18	/* width of destination column */
 #define	WID_GW_DEFAULT(af)	18	/* width of gateway column */
-#define	WID_IF_DEFAULT(af)	6	/* width of netif column */
+#define	WID_IF_DEFAULT(af)	(Wflag ? 8 : 6)	/* width of netif column */
 #else
 #define	WID_DST_DEFAULT(af) \
 	((af) == AF_INET6 ? (numeric_addr ? 33: 18) : 18)
 #define	WID_GW_DEFAULT(af) \
 	((af) == AF_INET6 ? (numeric_addr ? 29 : 18) : 18)
-#define	WID_IF_DEFAULT(af)	((af) == AF_INET6 ? 8 : 6)
+#define	WID_IF_DEFAULT(af)	((af) == AF_INET6 ? 8 : (Wflag ? 8 : 6))
 #endif /*INET6*/
 
 static int wid_dst;
