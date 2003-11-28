378bbc86c773f2a8bbb32b69fc316bd83c7153e7
Hajimu UMEMOTO <ume@FreeBSD.org>
2003-11-28 15:09:03 +0000

    MFC 1.167: AF_LINK sockaddr has to be attached to ifp->if_addrlist until the end, as many of the code assumes that TAILQ_FIRST(ifp->if_addrlist) is non-null.

--- sys/net/if.c
+++ sys/net/if.c
@@ -240,7 +240,7 @@ void
 if_detach(ifp)
 	struct ifnet *ifp;
 {
-	struct ifaddr *ifa;
+	struct ifaddr *ifa, *next;
 	struct radix_node_head	*rnh;
 	int s;
 	int i;
@@ -259,8 +259,11 @@ if_detach(ifp)
 	while (if_index > 0 && ifnet_addrs[if_index - 1] == 0)
 		if_index--;
 
-	for (ifa = TAILQ_FIRST(&ifp->if_addrhead); ifa;
-	     ifa = TAILQ_FIRST(&ifp->if_addrhead)) {
+	for (ifa = TAILQ_FIRST(&ifp->if_addrhead); ifa; ifa = next) {
+		next = TAILQ_NEXT(ifa, ifa_link);
+
+		if (ifa->ifa_addr->sa_family == AF_LINK)
+			continue;
 #ifdef INET
 		/* XXX: Ugly!! ad hoc just for INET */
 		if (ifa->ifa_addr && ifa->ifa_addr->sa_family == AF_INET) {
@@ -295,6 +298,11 @@ if_detach(ifp)
 	 */
 	in6_ifdetach(ifp);
 #endif
+
+	/* We can now free link ifaddr. */
+	ifa = TAILQ_FIRST(&ifp->if_addrhead);
+	TAILQ_REMOVE(&ifp->if_addrhead, ifa, ifa_link);
+	IFAFREE(ifa);
 
 	/*
 	 * Delete all remaining routes using this interface
