49b13d9069931d87fae87c22b8d4706aa6ec1c03
Bruce M Simpson <bms@FreeBSD.org>
2003-11-30 02:58:44 +0000

    MFC: Fix some minor nits in netstat whereby large interface names would be truncated. In environments where many tunnel or vlan interfaces are created, interface names have high numbers which overflow the field width.
    PRs:		bin/52349, bin/35838
    Submitted by:	Mike Tancsa, Scot W. Hetzel

--- usr.bin/netstat/if.c
+++ usr.bin/netstat/if.c
@@ -203,8 +203,12 @@ intpr(int interval, u_long ifnetaddr, void (*pfunc)(char *))
 		return;
 
 	if (!pfunc) {
-		printf("%-5.5s %-5.5s %-13.13s %-15.15s %8.8s %5.5s",
-		       "Name", "Mtu", "Network", "Address", "Ipkts", "Ierrs");
+		if (Wflag)
+			printf("%-7.7s", "Name");
+		else
+			printf("%-5.5s", "Name");
+		printf(" %5.5s %-13.13s %-17.17s %8.8s %5.5s",
+		    "Mtu", "Network", "Address", "Ipkts", "Ierrs");
 		if (bflag)
 			printf(" %10.10s","Ibytes");
 		printf(" %8.8s %5.5s", "Opkts", "Oerrs");
@@ -268,7 +272,11 @@ intpr(int interval, u_long ifnetaddr, void (*pfunc)(char *))
 		drops = ifnet.if_snd.ifq_drops;
 
 		if (ifaddraddr == 0) {
-			printf("%-5.5s %-5lu ", name, ifnet.if_mtu);
+			if (Wflag)
+				printf("%-7.7s", name);
+			else
+				printf("%-5.5s", name);
+			printf(" %5lu ", ifnet.if_mtu);
 			printf("%-13.13s ", "none");
 			printf("%-15.15s ", "none");
 		} else {
@@ -285,7 +293,11 @@ intpr(int interval, u_long ifnetaddr, void (*pfunc)(char *))
 				    (u_long)TAILQ_NEXT(&ifaddr.ifa, ifa_link);
 				continue;
 			}
-			printf("%-5.5s %-5lu ", name, ifnet.if_mtu);
+			if (Wflag)
+				printf("%-7.7s", name);
+			else
+				printf("%-5.5s", name);
+			printf(" %5lu ", ifnet.if_mtu);
 			switch (sa->sa_family) {
 			case AF_UNSPEC:
 				printf("%-13.13s ", "none");
