2d0a65fb5823f4a5e277a0133f8f21bba2a67b48
Bruce Evans <bde@FreeBSD.org>
2003-12-02 06:18:37 +0000

    MFC (1.405: quick fix for most of the pessimization in rev.1.194).

--- sys/i386/isa/sio.c
+++ sys/i386/isa/sio.c
@@ -1520,8 +1520,13 @@ siointr1(com)
 	struct	timecounter *tc;
 	u_int	count;
 
-	int_ctl = inb(com->intr_ctl_port);
-	int_ctl_new = int_ctl;
+	if (COM_IIR_TXRDYBUG(com)) {
+		int_ctl = inb(com->intr_ctl_port);
+		int_ctl_new = int_ctl;
+	} else {
+		int_ctl = 0;
+		int_ctl_new = 0;
+	}
 
 	while (!com->gone) {
 		if (com->pps.ppsparam.mode & PPS_CAPTUREBOTH) {
