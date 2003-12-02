f938d4cf1be6204f31166eeaa2cc853a879592ba
marcus <marcus@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-12-02 20:47:31 +0000

    Depend on kde-lite and gnome2-lite instead of kde3 and gnome2.  This greatly reduces the amount of space occupied by packages on release disc 1.
    Approved by:	re (scottl)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@123111 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/scripts/print-cdrom-packages.sh
+++ release/scripts/print-cdrom-packages.sh
@@ -101,8 +101,8 @@ CDROM_SET_1="${CDROM_SET_1} x11/XFree86-4-clients"
 CDROM_SET_1="${CDROM_SET_1} x11/XFree86-4-documents"
 CDROM_SET_1="${CDROM_SET_1} x11/XFree86-4-libraries"
 CDROM_SET_1="${CDROM_SET_1} x11/XFree86-4-manuals"
-CDROM_SET_1="${CDROM_SET_1} x11/gnome2"
-CDROM_SET_1="${CDROM_SET_1} x11/kde3"
+CDROM_SET_1="${CDROM_SET_1} x11/gnome2-lite"
+CDROM_SET_1="${CDROM_SET_1} x11/kde-lite"
 CDROM_SET_1="${CDROM_SET_1} www/links"
 
 # This is the set of "people really want these" packages.  Please add to
