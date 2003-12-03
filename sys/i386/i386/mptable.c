6afad3b0552116f7075ed9eb78eda5ae5557a871
John Baldwin <jhb@FreeBSD.org>
2003-12-03 20:33:18 +0000

    - Reorder the APIC enumerator SYSINIT's to register enumeators at   SI_SUB_CPU - 1 and probe enumerators, probe CPUs, and setup the local   APIC programming all at SI_SUB_CPU / SI_ORDER_FIRST.  This is needed to   help get the ACPI module working again as it moves the APIC enumeration   code after SI_SUB_KLD. - In the MADT parser, use mp_maxid rather than MAXCPU to terminate a loop   when assigning per-cpu ACPI IDs to avoid a dependency on 'options SMP'. - Allow the apic device to be disabled via 'hint.apic.0.disabled' from the   loader.  Note that since this is done in the local APIC code, it works   for both the ACPI and non-ACPI cases.
    Approved by:	re (scott / blanket)

--- sys/i386/i386/mptable.c
+++ sys/i386/i386/mptable.c
@@ -375,8 +375,8 @@ mptable_register(void *dummy __unused)
 
 	apic_register_enumerator(&mptable_enumerator);
 }
-SYSINIT(mptable_register, SI_SUB_TUNABLES - 1, SI_ORDER_FIRST,
-    mptable_register, NULL)
+SYSINIT(mptable_register, SI_SUB_CPU - 1, SI_ORDER_FIRST, mptable_register,
+    NULL)
 
 /*
  * Call the handler routine for each entry in the MP config table.
