60bb13ef063f683a5919860ffca37fc97b382167
scottl <scottl@FreeBSD.org>
2003-12-07 03:56:54 +0000

    HEAD __FreeBSD_version is at 502100

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -57,7 +57,7 @@
  * scheme is:  <major><two digit minor><0 if release branch, otherwise 1>xx
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 501114	/* Master, propagated to newvers */
+#define __FreeBSD_version 502100	/* Master, propagated to newvers */
 
 #ifndef NULL
 #define	NULL	0
