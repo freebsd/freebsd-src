93a8c3be097e106a4542c501906297ac2901a7cf
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-12-17 02:54:29 +0000

    Conformance: Define FNM_NOSYS (see http://www.opengroup.org/onlinepubs/007904975/basedefs/fnmatch.h.html)
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@123585 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- include/fnmatch.h
+++ include/fnmatch.h
@@ -45,6 +45,8 @@
 #define	FNM_PATHNAME	0x02	/* Slash must be matched by slash. */
 #define	FNM_PERIOD	0x04	/* Period must be matched by period. */
 
+#define FNM_NOSYS       -1      /* Reserved */
+
 #if __BSD_VISIBLE
 #define	FNM_LEADING_DIR	0x08	/* Ignore /<tail> after Imatch. */
 #define	FNM_CASEFOLD	0x10	/* Case insensitive search. */
