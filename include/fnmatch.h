4236015d88c5738e7352ecdf0bd4c5e6da6d29b9
jkh <jkh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2003-12-17 08:03:18 +0000

    Correct inexplicable tab smash. Noticed by:	Nate Lawson <nate@root.org>
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@123589 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- include/fnmatch.h
+++ include/fnmatch.h
@@ -45,7 +45,7 @@
 #define	FNM_PATHNAME	0x02	/* Slash must be matched by slash. */
 #define	FNM_PERIOD	0x04	/* Period must be matched by period. */
 
-#define FNM_NOSYS       -1      /* Reserved */
+#define FNM_NOSYS	-1	/* Reserved */
 
 #if __BSD_VISIBLE
 #define	FNM_LEADING_DIR	0x08	/* Ignore /<tail> after Imatch. */
