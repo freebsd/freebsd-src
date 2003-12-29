4ac80b46115b19f6725fdb4edc6e6d670add5978
grog <grog@FreeBSD.org>
2003-12-29 06:40:00 +0000

    gdbinit target: Remove comment lines from dot.gdbinit.  This allows us to put dire warnings in the original and not find them in the installed version.

--- sys/conf/kern.post.mk
+++ sys/conf/kern.post.mk
@@ -54,9 +54,10 @@ ${KERNEL_KO}: ${FULLKERNEL}
 install.debug reinstall.debug: gdbinit
 	cd ${.CURDIR}; ${MAKE} -DINSTALL_DEBUG ${.TARGET:R}
 
+# Install gdbinit files for kernel debugging
 gdbinit:
-	sed < ${S}/../tools/debugscripts/dot.gdbinit > .gdbinit \
-		"s:MODPATH:${.OBJDIR}/modules:"
+	grep -v '# XXX' ${S}/../tools/debugscripts/dot.gdbinit \
+	  | sed "s:MODPATH:${.OBJDIR}/modules:" > .gdbinit
 	cp ${S}/../tools/debugscripts/gdbinit.kernel \
 		${S}/../tools/debugscripts/gdbinit.vinum ${.CURDIR}
 	cp ${S}/../tools/debugscripts/gdbinit.${MACHINE_ARCH} \
