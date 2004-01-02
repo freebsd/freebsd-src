d6d08deb31a015ad3aa5402216b528690898ba1c
tjr <tjr@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-01-02 22:38:42 +0000

    Add support for SMB request signing, which prevents "man in the middle" attacks and is required to connect to Windows 2003 servers in their default configuration. This adds an extra field to the SMB header containing the truncated 64-bit MD5 digest of a key (a function of the user's password and the server's authentication challenge), an implicit sequence number, and the message data itself. As signing each message imposes a significant performance penalty, we only enable it if the server will not let us connect without it; this should eventually become an option to mount_smbfs.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@124087 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netsmb/smb_conn.h
+++ sys/netsmb/smb_conn.h
@@ -268,6 +268,9 @@ struct smb_vc {
 	int		vc_wxmax;	/* max writex data size */
 	struct smbiod *	vc_iod;
 	struct smb_slock vc_stlock;
+	u_int32_t	vc_seqno;	/* my next sequence number */
+	u_int8_t	*vc_mackey;	/* MAC key */
+	int		vc_mackeylen;	/* length of MAC key */
 };
 
 #define vc_maxmux	vc_sopt.sv_maxmux
