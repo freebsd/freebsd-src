8412b382ed21d3c95d4a7c45451e00e0cc1830cb
Alexander Kabaev <kan@FreeBSD.org>
2004-01-05 20:27:51 +0000

    This commit was generated by cvs2svn to compensate for changes in r124151, which included commits to RCS files with non-trunk default branches.

--- contrib/gcc/config/sparc/sol27-sld.h
+++
@@ -1,8 +0,0 @@
-/* Up through Solaris 2.7, the system linker does not work with DWARF
-   or DWARF2, since it does not have working support for relocations
-   to unaligned data.  */
-
-#undef DWARF_DEBUGGING_INFO
-#undef DWARF2_DEBUGGING_INFO
-#undef PREFERRED_DEBUGGING_TYPE
-#undef ASM_DEBUG_SPEC
