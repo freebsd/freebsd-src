2b77864f1e464f9279ec96b84d7bea90a8089304
Don Lewis <truckman@FreeBSD.org>
2004-01-10 08:28:54 +0000

    Add a somewhat redundant check on the len arguement to getsockaddr() to avoid relying on the minimum memory allocation size to avoid problems. The check is somewhat redundant because the consumers of the returned structure will check that sa_len is a protocol-specific larger size.
    Submitted by:	Matthew Dillon <dillon@apollo.backplane.com>
    Reviewed by:	nectar
    MFC after:	30 days

--- sys/kern/uipc_syscalls.c
+++ sys/kern/uipc_syscalls.c
@@ -1607,6 +1607,8 @@ getsockaddr(namp, uaddr, len)
 
 	if (len > SOCK_MAXADDRLEN)
 		return ENAMETOOLONG;
+	if (len < offsetof(struct sockaddr, sa_data[0]))
+		return EINVAL;
 	MALLOC(sa, struct sockaddr *, len, M_SONAME, M_WAITOK);
 	error = copyin(uaddr, sa, len);
 	if (error) {
