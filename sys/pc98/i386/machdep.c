fb2c79813dede419c68ede373dca64b5eeab319c
nyan <nyan@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-01-11 09:18:33 +0000

    MFi386: revision 1.583.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@124370 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/pc98/i386/machdep.c
+++ sys/pc98/i386/machdep.c
@@ -130,6 +130,9 @@
 #include <sys/ptrace.h>
 #include <machine/sigframe.h>
 
+/* Sanity check for __curthread() */
+CTASSERT(offsetof(struct pcpu, pc_curthread) == 0);
+
 extern void init386(int first);
 extern void dblfault_handler(void);
 
