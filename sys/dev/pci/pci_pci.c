e2c90561bc85c5b5ad7ab399909335d5f2a851e3
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-01-17 21:54:04 +0000

    It appears that the changes in the resources allocated is causing much pain and suffering.  Attempt to back it out by removing the 'if the requested range is larger than the window, clip to the window' code. This is a band-aide until the issues are better understood and the issues with the lazy allocation patches are resolved.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@124641 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/pci/pci_pci.c
+++ sys/dev/pci/pci_pci.c
@@ -321,11 +321,12 @@ pcib_alloc_resource(device_t dev, device_t child, int type, int *rid,
 			}
 		} else {
 			ok = 1;
+#if 0
 			if (start < sc->iobase && end > sc->iolimit) {
 				start = sc->iobase;
 				end = sc->iolimit;
 			}
-			
+#endif			
 		}
 		if (end < start) {
 			device_printf(dev, "ioport: end (%lx) < start (%lx)\n", end, start);
@@ -376,6 +377,7 @@ pcib_alloc_resource(device_t dev, device_t child, int type, int *rid,
 			}
 		} else if (!ok) {
 			ok = 1;	/* subtractive bridge: always ok */
+#if 0
 			if (pcib_is_nonprefetch_open(sc)) {
 				if (start < sc->membase && end > sc->memlimit) {
 					start = sc->membase;
@@ -388,6 +390,7 @@ pcib_alloc_resource(device_t dev, device_t child, int type, int *rid,
 					end = sc->pmemlimit;
 				}
 			}
+#endif
 		}
 		if (end < start) {
 			device_printf(dev, "memory: end (%lx) < start (%lx)\n", end, start);
