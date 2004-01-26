bd064b9335dc38fa5039943c2f43aab94db345cd
David E. O'Brien <obrien@FreeBSD.org>
2004-01-26 09:18:47 +0000

    Fix problem where kgdb misses trap frames.
    Reported by:	iedowse

--- gnu/usr.bin/binutils/gdb/fbsd-kgdb-ia64.h
+++ gnu/usr.bin/binutils/gdb/fbsd-kgdb-ia64.h
@@ -3,4 +3,6 @@
 #ifndef FBSD_KGDB_IA64_H
 #define FBSD_KGDB_IA64_H
 
+#include "fbsd-kgdb.h"
+
 #endif /* FBSD_KGDB_IA64_H */
--- gnu/usr.bin/binutils/gdb/fbsd-kgdb-powerpc.h
+++ gnu/usr.bin/binutils/gdb/fbsd-kgdb-powerpc.h
@@ -3,4 +3,6 @@
 #ifndef FBSD_KGDB_POWERPC_H
 #define FBSD_KGDB_POWERPC_H
 
+#include "fbsd-kgdb.h"
+
 #endif /* FBSD_KGDB_POWERPC_H */
