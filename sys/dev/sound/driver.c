e0591dcd0fdf7f7a1f5837544b3a136fb9ea7ba2
mr <mr@FreeBSD.org>
2004-01-29 14:11:03 +0000

    Add dependency to snd_via8233

--- sys/dev/sound/driver.c
+++ sys/dev/sound/driver.c
@@ -75,5 +75,6 @@ MODULE_DEPEND(snd_driver, snd_sb16, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_sbc, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_solo, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_t4dwave, 1, 1, 1);
+MODULE_DEPEND(snd_driver, snd_via8233, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_via82c686, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_vibes, 1, 1, 1);
