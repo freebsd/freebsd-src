d7d8341eff47f9973351ec8f344821f7aad25425
simokawa <simokawa@FreeBSD.org>
2004-01-30 14:30:19 +0000

    Add NEC uPD72873.
    Submitted by: Christian Laursen <xi@borderworlds.dk>

--- sys/dev/firewire/fwohcireg.h
+++ sys/dev/firewire/fwohcireg.h
@@ -47,6 +47,7 @@
 #define		FW_DEVICE_UPD861	(0x0063 << 16)
 #define		FW_DEVICE_UPD871	(0x00ce << 16)
 #define		FW_DEVICE_UPD72870	(0x00cd << 16)
+#define		FW_DEVICE_UPD72873	(0x00e7 << 16)
 #define		FW_DEVICE_UPD72874	(0x00f2 << 16)
 #define		FW_DEVICE_TITSB22	(0x8009 << 16)
 #define		FW_DEVICE_TITSB23	(0x8019 << 16)
