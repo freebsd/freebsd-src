65c3f7f42a28d286d3f19d1bd6ffc8c45cb402f0
MIHIRA Sanpei Yoshiro <sanpei@FreeBSD.org>
2004-02-01 12:27:16 +0000

    MFC: Add support(rev.1.101) 	- 2nd type of GL641USB USB-IDE bridge 	- Plextor Plexwriter 40/12/40U

--- sys/dev/usb/umass.c
+++ sys/dev/usb/umass.c
@@ -687,12 +687,18 @@ umass_match_proto(struct umass_softc *sc, usbd_interface_handle iface,
 	}
 	if (UGETW(dd->idVendor) == USB_VENDOR_GENESYS &&
 	    (UGETW(dd->idProduct) == USB_PRODUCT_GENESYS_GL641USB2IDE ||
+	     UGETW(dd->idProduct) == USB_PRODUCT_GENESYS_GL641USB2IDE_2 ||
 	     UGETW(dd->idProduct) == USB_PRODUCT_GENESYS_GL641USB)) {
 		sc->quirks |= FORCE_SHORT_INQUIRY | NO_START_STOP | IGNORE_RESIDUE;
 	}
 	if (UGETW(dd->idVendor) == USB_VENDOR_IODATA &&
 	    UGETW(dd->idProduct) == USB_PRODUCT_IODATA_IU_CD2) {
 		sc->proto =  UMASS_PROTO_SCSI | UMASS_PROTO_BBB;
+	}
+	/* Logitec DVD multi plus unit */
+	if (UGETW(dd->idVendor) == USB_VENDOR_LOGITEC &&
+	    UGETW(dd->idProduct) == USB_PRODUCT_LOGITEC_LDR_H443U2) {
+		sc->proto = UMASS_PROTO_SCSI;
 	}
 	if (UGETW(dd->idVendor) == USB_VENDOR_MELCO &&
 	    UGETW(dd->idProduct) == USB_PRODUCT_MELCO_DUBPXXG) {
@@ -702,11 +708,6 @@ umass_match_proto(struct umass_softc *sc, usbd_interface_handle iface,
 	    UGETW(dd->idProduct) == USB_PRODUCT_MSYSTEMS_DISKONKEY2) {
 		sc->proto = UMASS_PROTO_ATAPI | UMASS_PROTO_BBB;
 	}
-	/* Logitec DVD multi plus unit */
-	if (UGETW(dd->idVendor) == USB_VENDOR_LOGITEC &&
-	    UGETW(dd->idProduct) == USB_PRODUCT_LOGITEC_LDR_H443U2) {
-		sc->proto = UMASS_PROTO_SCSI;
-	}
 	if (UGETW(dd->idVendor) == USB_VENDOR_PANASONIC &&
 	    UGETW(dd->idProduct) == USB_PRODUCT_PANASONIC_KXLCB20AN) {
 		sc->proto = UMASS_PROTO_SCSI | UMASS_PROTO_BBB;
@@ -714,6 +715,11 @@ umass_match_proto(struct umass_softc *sc, usbd_interface_handle iface,
 	if (UGETW(dd->idVendor) == USB_VENDOR_PANASONIC &&
 	    UGETW(dd->idProduct) == USB_PRODUCT_PANASONIC_KXLCB35AN) {
 		sc->proto = UMASS_PROTO_SCSI | UMASS_PROTO_BBB;
+	}
+	if (UGETW(dd->idVendor) == USB_VENDOR_PLEXTOR &&
+	    UGETW(dd->idProduct) == USB_PRODUCT_PLEXTOR_40_12_40U) {
+		sc->proto = UMASS_PROTO_SCSI | UMASS_PROTO_BBB;
+		sc->quirks |= NO_TEST_UNIT_READY;
 	}
 	if (UGETW(dd->idVendor) == USB_VENDOR_PNY &&
 	    UGETW(dd->idProduct) == USB_PRODUCT_PNY_ATTACHE) {
