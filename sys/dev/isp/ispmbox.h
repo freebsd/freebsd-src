58df498086762de70c2a0fb6367ec4d0b73b9102
Matt Jacob <mjacob@FreeBSD.org>
2004-02-02 22:38:13 +0000

    MFC: MAKE_WWN_FROM_NODE_NAME macro

--- sys/dev/isp/ispmbox.h
+++ sys/dev/isp/ispmbox.h
@@ -649,12 +649,16 @@ typedef struct isp_icb {
 #define	ICBXOPT_LOOP_2_PTP	(2 << 4)
 #define	ICBXOPT_PTP_2_LOOP	(3 << 4)
 
+/*
+ * The lower 4 bits of the xfwoptions field are the OPERATION MODE bits.
+ * RIO is not defined for the 23XX cards
+ */
 #define	ICBXOPT_RIO_OFF		0
 #define	ICBXOPT_RIO_16BIT	1
 #define	ICBXOPT_RIO_32BIT	2
 #define	ICBXOPT_RIO_16BIT_IOCB	3
 #define	ICBXOPT_RIO_32BIT_IOCB	4
-#define	ICBXOPT_ZIO		5
+#define	ICBXOPT_ZIO		5	
 
 #define	ICBZOPT_ENA_RDXFR_RDY	0x01
 #define	ICBZOPT_ENA_OOF		(1 << 6) /* out of order frame handling */
@@ -697,6 +701,16 @@ typedef struct isp_icb {
 	array[ICB_NNM5] = (u_int8_t) ((wwn >> 40) & 0xff), \
 	array[ICB_NNM6] = (u_int8_t) ((wwn >> 48) & 0xff), \
 	array[ICB_NNM7] = (u_int8_t) ((wwn >> 56) & 0xff)
+
+#define	MAKE_WWN_FROM_NODE_NAME(wwn, array)	\
+	wwn =	((u_int64_t) array[ICB_NNM0]) | \
+		((u_int64_t) array[ICB_NNM1] <<  8) | \
+		((u_int64_t) array[ICB_NNM2] << 16) | \
+		((u_int64_t) array[ICB_NNM3] << 24) | \
+		((u_int64_t) array[ICB_NNM4] << 32) | \
+		((u_int64_t) array[ICB_NNM5] << 40) | \
+		((u_int64_t) array[ICB_NNM6] << 48) | \
+		((u_int64_t) array[ICB_NNM7] << 56)
 
 /*
  * FC-AL Position Map
