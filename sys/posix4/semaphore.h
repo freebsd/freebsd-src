ca81ad4e6b755431ebfad40746b2b53d3bb527d3
deischen <deischen@FreeBSD.org>
2004-02-03 10:15:16 +0000

    Add prototype for sem_timedwait().
    Reviewed by:	standards (Stefan Farfeleder)

--- sys/posix4/semaphore.h
+++ sys/posix4/semaphore.h
@@ -54,6 +54,7 @@ int	 sem_getvalue(sem_t * __restrict, int * __restrict);
 int	 sem_init(sem_t *, int, unsigned int);
 sem_t	*sem_open(const char *, int, ...);
 int	 sem_post(sem_t *);
+int	 sem_timedwait(sem_t * __restrict, const struct timespec * __restrict);
 int	 sem_trywait(sem_t *);
 int	 sem_unlink(const char *);
 int	 sem_wait(sem_t *);
