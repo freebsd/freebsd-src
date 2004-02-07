a83674474f8cf355f2279a2a75695a3a303a257d
scottl <scottl@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-02-07 10:30:59 +0000

    Remove the hack of lowering AAC_MAX_FIB now that the root cause of the problem was found.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@125560 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/aac/aacvar.h
+++ sys/dev/aac/aacvar.h
@@ -58,7 +58,7 @@
  */
 #define AAC_FIB_COUNT		(PAGE_SIZE/sizeof(struct aac_fib))
 #define AAC_PREALLOCATE_FIBS	128
-#define AAC_MAX_FIBS		(512 - AAC_FIB_COUNT)
+#define AAC_MAX_FIBS		512
 
 /*
  * The controller reports status events in AIFs.  We hang on to a number of
