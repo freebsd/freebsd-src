d623b765cfe263c7209ea40569e6dbaa8e9787d9
Colin Percival <cperciva@FreeBSD.org>
2004-02-10 20:42:33 +0000

    style cleanup: Remove duplicate $FreeBSD$ tags.
    These files had tags after the copyright notice,
    inside the comment block (incorrect, removed),
    and outside the comment block (correct).
    
    Approved by:	rwatson (mentor)

--- lib/libc/i386/gen/signalcontext.c
+++ lib/libc/i386/gen/signalcontext.c
@@ -22,12 +22,10 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $FreeBSD$
  */
 
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD");
+__FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
 #include <sys/ucontext.h>
