09934054c27678a04d4c0afef6d4314633480474
grehan <grehan@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-02-11 10:49:30 +0000

    Clean up header files, which fixes compile warning.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@125705 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/powerpc/powerpc/critical.c
+++ sys/powerpc/powerpc/critical.c
@@ -29,17 +29,8 @@
 
 #include <sys/param.h>
 #include <sys/systm.h>
-#include <sys/pcpu.h>
-#include <sys/eventhandler.h>	/* XX */
-#include <sys/ktr.h>		/* XX */
-#include <sys/signalvar.h>
-#include <sys/sysproto.h>	/* XX */
-#include <sys/kernel.h>
 #include <sys/proc.h>
-#include <sys/lock.h>
-#include <sys/mutex.h>
-#include <sys/sysctl.h>
-#include <sys/ucontext.h>
+#include <machine/critical.h>
 
 /*
  * cpu_critical_fork_exit() - cleanup after fork
