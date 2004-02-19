d2df6bc6f718fb3b4771f1057489a9370b4155cb
tjr <tjr@FreeBSD.org>
2004-02-19 09:06:06 +0000

    Enforce the file size limit in VOP_WRITE() as well as VOP_TRUNCATE(); pointed out by bde.

--- sys/gnu/ext2fs/ext2_fs_sb.h
+++ sys/gnu/ext2fs/ext2_fs_sb.h
@@ -3,6 +3,8 @@
  *
  *  Aug 1995, Godmar Back (gback@cs.utah.edu)
  *  University of Utah, Department of Computer Science
+ *
+ * $FreeBSD$
  */
 /*
  *  linux/include/linux/ext2_fs_sb.h
@@ -74,7 +76,7 @@ struct ext2_sb_info {
 	char    s_rd_only;                      /* read-only 		*/
 	char    s_dirt;                         /* fs modified flag */
 	char	s_wasvalid;			/* valid at mount time */
-
+	off_t	fs_maxfilesize;
 	char    fs_fsmnt[MAXMNTLEN];            /* name mounted on */
 };
 
