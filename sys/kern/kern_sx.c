4b4567d8fd69732f13655e8728755c2a49a2768e
pjd <pjd@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-02-19 14:19:31 +0000

    Simplify check. We are only able to check exclusive lock and if 2nd condition is true, first one is true for sure.
    Approved by:	jhb, scottl (mentor)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@126003 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/kern_sx.c
+++ sys/kern/kern_sx.c
@@ -348,8 +348,12 @@ _sx_assert(struct sx *sx, int what, const char *file, int line)
 #ifdef WITNESS
 		witness_assert(&sx->sx_object, what, file, line);
 #else
+		/*
+		 * We are able to check only exclusive lock here,
+		 * we cannot assert that *this* thread owns slock.
+		 */
 		mtx_lock(sx->sx_lock);
-		if (sx->sx_cnt != 0 && sx->sx_xholder == curthread)
+		if (sx->sx_xholder == curthread)
 			printf("Lock %s locked @ %s:%d\n",
 			    sx->sx_object.lo_name, file, line);
 		mtx_unlock(sx->sx_lock);
