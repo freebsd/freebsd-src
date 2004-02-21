756aa54500b12264aec14c4346f0885f06d05e80
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-02-21 23:35:56 +0000

    DOH!!!  Fix signals for freebsd-4.x/i386 binaries.  The ucontext has different alignments due to the sse fxsave dump area.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@126090 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/compat/ia32/ia32_genassym.c
+++ sys/compat/ia32/ia32_genassym.c
@@ -17,7 +17,7 @@ ASSYM(IA32_UC_FS, offsetof(struct ia32_ucontext, uc_mcontext.mc_fs));
 ASSYM(IA32_UC_ES, offsetof(struct ia32_ucontext, uc_mcontext.mc_es));
 ASSYM(IA32_UC_DS, offsetof(struct ia32_ucontext, uc_mcontext.mc_ds));
 #ifdef COMPAT_FREEBSD4
-ASSYM(IA32_SIGF_UC4, offsetof(struct ia32_sigframe, sf_uc));
+ASSYM(IA32_SIGF_UC4, offsetof(struct ia32_sigframe4, sf_uc));
 ASSYM(IA32_UC4_GS, offsetof(struct ia32_ucontext4, uc_mcontext.mc_gs));
 ASSYM(IA32_UC4_FS, offsetof(struct ia32_ucontext4, uc_mcontext.mc_fs));
 ASSYM(IA32_UC4_ES, offsetof(struct ia32_ucontext4, uc_mcontext.mc_es));
