d4ca518cdbba836c4aeb41475677ace187c481ae
cperciva <cperciva@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-02-22 10:03:24 +0000

    Report login attempts to syslog.  Due to the statically-linked nature of nologin(8), this causes a considerable (100K) increase in the binary size, so I've added a NO_LOGIN_LOG option which disables this.
    While I'm here, s/sizeof(MESSAGE)/sizeof(MESSAGE) - 1/, in order to
    avoid writing the string-terminating zero byte.
    
    No complaints from: -current
    Approved by:	rwatson (mentor)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@126117 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/nologin/nologin.c
+++ usr.sbin/nologin/nologin.c
@@ -8,6 +8,7 @@ __FBSDID("$FreeBSD$");
 
 #include <sys/types.h>
 #include <sys/uio.h>
+#include <syslog.h>
 #include <unistd.h>
 
 #define	MESSAGE	"This account is currently not available.\n"
@@ -15,7 +16,19 @@ __FBSDID("$FreeBSD$");
 int
 main(int argc, char *argv[])
 {
+#ifndef NO_NOLOGIN_LOG
+	char *user, *tt;
 
-	write(STDOUT_FILENO, MESSAGE, sizeof(MESSAGE));
+	if ((tt = ttyname(0)) == NULL)
+		tt = "UNKNOWN";
+	if ((user = getlogin()) == NULL)
+		user = "UNKNOWN";
+
+	openlog("nologin", LOG_CONS, LOG_AUTH);
+	syslog(LOG_CRIT, "Attempted login by %s on %s", user, tt);
+	closelog();
+#endif /* NO_NOLOGIN_LOG */
+
+	write(STDOUT_FILENO, MESSAGE, sizeof(MESSAGE) - 1);
 	_exit(1);
 }
