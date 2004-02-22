efbef97de9805dec95db09953426f0b21bb2964f
Peter Wemm <peter@FreeBSD.org>
2004-02-22 02:11:39 +0000

    Change the syscall stub branch orders so that the static branch prediction will assume that syscalls will succeed rather than fail.

--- lib/libc/amd64/SYS.h
+++ lib/libc/amd64/SYS.h
@@ -41,25 +41,22 @@
 #include <machine/asm.h>
 
 #ifdef PIC
-#define	SYSCALL(x)	2: movq PIC_GOT(HIDENAME(cerror)),%rcx;		\
-			jmp *%rcx;					\
-			ENTRY(__CONCAT(__sys_,x));			\
+#define	RSYSCALL(x)	ENTRY(__CONCAT(__sys_,x));			\
 			.weak CNAME(x);					\
 			.set CNAME(x),CNAME(__CONCAT(__sys_,x));	\
 			.weak CNAME(__CONCAT(_,x));			\
 			.set CNAME(__CONCAT(_,x)),CNAME(__CONCAT(__sys_,x)); \
-			mov __CONCAT($SYS_,x),%rax; KERNCALL; jb 2b
+			mov __CONCAT($SYS_,x),%rax; KERNCALL; jb 2f; ret; \
+			2: movq PIC_GOT(HIDENAME(cerror)),%rcx; jmp *%rcx
 #else
-#define	SYSCALL(x)	2: jmp HIDENAME(cerror);			\
-			ENTRY(__CONCAT(__sys_,x));			\
+#define	RSYSCALL(x)	ENTRY(__CONCAT(__sys_,x));			\
 			.weak CNAME(x);					\
 			.set CNAME(x),CNAME(__CONCAT(__sys_,x));	\
 			.weak CNAME(__CONCAT(_,x));			\
 			.set CNAME(__CONCAT(_,x)),CNAME(__CONCAT(__sys_,x)); \
-			mov __CONCAT($SYS_,x),%rax; KERNCALL; jb 2b
+			mov __CONCAT($SYS_,x),%rax; KERNCALL; jb 2f; ret; \
+			2: jmp HIDENAME(cerror)
 #endif
-
-#define	RSYSCALL(x)	SYSCALL(x); ret
 
 #define	PSEUDO(x)	ENTRY(__CONCAT(__sys_,x));			\
 			.weak CNAME(__CONCAT(_,x));			\
