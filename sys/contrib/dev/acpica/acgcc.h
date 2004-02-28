678958907c36ca7208a30d07182074c183be4840
njl <njl@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-02-28 20:35:57 +0000

    Unchanged files from ACPICA 20040220 that are off the vendor branch.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@126375 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/contrib/dev/acpica/acgcc.h
+++ sys/contrib/dev/acpica/acgcc.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acgcc.h - GCC specific defines, etc.
- *       $Revision: 25 $
+ *       $Revision: 26 $
  *
  *****************************************************************************/
 
@@ -9,7 +9,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999 - 2003, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2004, Intel Corp.
  * All rights reserved.
  *
  * 2. License
