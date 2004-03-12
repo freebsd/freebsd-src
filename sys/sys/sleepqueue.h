74dea62446d3ff5e025ee37f58eb44cfcb408383
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-03-12 19:06:18 +0000

    - Remove old sleep queues. - Remove sleepqueue argument from sleepq_set_timeout() since it is not   used.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@126885 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/sleepqueue.h
+++ sys/sys/sleepqueue.h
@@ -97,7 +97,7 @@ struct sleepqueue *sleepq_lookup(void *);
 void	sleepq_release(void *);
 void	sleepq_remove(struct thread *, void *);
 void	sleepq_signal(void *, int, int);
-void	sleepq_set_timeout(struct sleepqueue *sq, void *wchan, int timo);
+void	sleepq_set_timeout(void *wchan, int timo);
 int	sleepq_timedwait(void *wchan, int signal_caught);
 int	sleepq_timedwait_sig(void *wchan, int signal_caught);
 void	sleepq_wait(void *);
