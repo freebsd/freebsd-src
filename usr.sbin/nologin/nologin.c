8d7856111a767eb2753459c0235561c628d05205
cperciva <cperciva@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-03-13 04:41:40 +0000

    Add standard copyright notice; fix style bugs.  (Reported by bde) Remove NO_NOLOGIN_LOG option now that we're off the root partition.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@126904 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/nologin/nologin.c
+++ usr.sbin/nologin/nologin.c
@@ -1,13 +1,33 @@
 /*-
- * This program is in the public domain.  I couldn't bring myself to
- * declare Copyright on a variant of Hello World.
+ * Copyright (c) 2004 The FreeBSD Project.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include <sys/types.h>
-#include <sys/uio.h>
+#include <stdio.h>
 #include <syslog.h>
 #include <unistd.h>
 
@@ -16,19 +36,16 @@ __FBSDID("$FreeBSD$");
 int
 main(int argc, char *argv[])
 {
-#ifndef NO_NOLOGIN_LOG
 	char *user, *tt;
 
 	if ((tt = ttyname(0)) == NULL)
 		tt = "UNKNOWN";
 	if ((user = getlogin()) == NULL)
 		user = "UNKNOWN";
-
 	openlog("nologin", LOG_CONS, LOG_AUTH);
 	syslog(LOG_CRIT, "Attempted login by %s on %s", user, tt);
 	closelog();
-#endif /* NO_NOLOGIN_LOG */
 
-	write(STDOUT_FILENO, MESSAGE, sizeof(MESSAGE) - 1);
-	_exit(1);
+	printf("%s", MESSAGE);
+	return 1;
 }
