4a1be2f9f90e9fa6db88c84967aa260fca2dd481
Robert Watson <rwatson@FreeBSD.org>
2004-03-13 05:51:06 +0000

    Remove stale (unused) unit variables from if_tun and if_tap softc's.

--- sys/net/if_tapvar.h
+++ sys/net/if_tapvar.h
@@ -44,8 +44,6 @@
 struct tap_softc {
 	struct arpcom	arpcom;			/* ethernet common data      */
 #define tap_if		arpcom.ac_if
-	struct resource	*tap_unit;		/* unit                      */
-
 	u_short		tap_flags;		/* misc flags                */
 #define	TAP_OPEN	(1 << 0)
 #define	TAP_INITED	(1 << 1)
