063b9c0e43ef9e0296900baa8e1cbcc59ef2aff2
ru <ru@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-03-14 08:43:55 +0000

    Removed duplicate __FBSDID().  Keep the one that style(9) likes.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@126967 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ep/if_ep.c
+++ sys/dev/ep/if_ep.c
@@ -60,9 +60,6 @@ __FBSDID("$FreeBSD$");
  *                             <mdodd@FreeBSD.org>
  */
 
-#include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
-
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/mbuf.h>
