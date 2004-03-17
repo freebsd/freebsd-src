aa606d7a1638e5032b64759fb701c745fbc9ed82
Jacques Vidrine <nectar@FreeBSD.org>
2004-03-17 12:18:23 +0000

    MFC s3_pkt.c 1.1.1.9: Correct a denial-of-service vulnerability in OpenSSL (CAN-2004-0079).

--- crypto/openssl/crypto/opensslv.h
+++ crypto/openssl/crypto/opensslv.h
@@ -26,7 +26,7 @@
  *  major minor fix final patch/beta)
  */
 #define OPENSSL_VERSION_NUMBER	0x0090701fL
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7a Feb 19 2003"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7a-p1 Feb 19 2003"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 
