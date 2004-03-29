87e47750f2e5250e714d50e272b1fa0fcb013157
Garance A Drosehn <gad@FreeBSD.org>
2004-03-29 01:18:13 +0000

    Since "kp" is a pointer, I should be comparing against NULL not 0.
    Noticed by:	bde

--- bin/ps/ps.c
+++ bin/ps/ps.c
@@ -507,7 +507,7 @@ main(int argc, char *argv[])
 	 */
 	nentries = -1;
 	kp = kvm_getprocs(kd, what, flag, &nentries);
-	if ((kp == 0 && nentries > 0) || (kp != 0 && nentries < 0))
+	if ((kp == NULL && nentries > 0) || (kp != NULL && nentries < 0))
 		errx(1, "%s", kvm_geterr(kd));
 	nkept = 0;
 	if (nentries > 0) {
