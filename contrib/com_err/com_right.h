3e55660227282202072ae50a1b22ecae82118e49
nectar <nectar@FreeBSD.org>
2004-04-03 21:08:58 +0000

    Import libcom_err from Heimdal 0.6.1.

--- contrib/com_err/com_right.h
+++ contrib/com_err/com_right.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 1998 Kungliga Tekniska Högskolan
+ * Copyright (c) 1997 - 2000 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
@@ -14,12 +14,7 @@
  *    notice, this list of conditions and the following disclaimer in the 
  *    documentation and/or other materials provided with the distribution. 
  *
- * 3. All advertising materials mentioning features or use of this software 
- *    must display the following acknowledgement: 
- *      This product includes software developed by Kungliga Tekniska 
- *      Högskolan and its contributors. 
- *
- * 4. Neither the name of the Institute nor the names of its contributors 
+ * 3. Neither the name of the Institute nor the names of its contributors 
  *    may be used to endorse or promote products derived from this software 
  *    without specific prior written permission. 
  *
@@ -36,10 +31,22 @@
  * SUCH DAMAGE. 
  */
 
-/* $Id: com_right.h,v 1.8 1998/02/17 21:19:43 bg Exp $ */
+/* $Id: com_right.h,v 1.11 2000/07/31 01:11:08 assar Exp $ */
 
 #ifndef __COM_RIGHT_H__
 #define __COM_RIGHT_H__
+
+#ifdef __STDC__
+#include <stdarg.h>
+#endif
+
+#ifndef __P
+#ifdef __STDC__
+#define __P(X) X
+#else
+#define __P(X) ()
+#endif
+#endif
 
 struct error_table {
     char const * const * msgs;
@@ -52,8 +59,8 @@ struct et_list {
 };
 extern struct et_list *_et_list;
 
-const char *com_right(struct et_list *list, long code);
-void initialize_error_table_r(struct et_list **, const char **, int, long);
-void free_error_table(struct et_list *);
+const char *com_right __P((struct et_list *list, long code));
+void initialize_error_table_r __P((struct et_list **, const char **, int, long));
+void free_error_table __P((struct et_list *));
 
 #endif /* __COM_RIGHT_H__ */
