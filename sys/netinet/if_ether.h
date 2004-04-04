f7c5baa1c66d89061f673100855c7c32ae63dd28
Luigi Rizzo <luigi@FreeBSD.org>
2004-04-04 06:14:55 +0000

    + arpresolve(): remove an unused argument + struct ifnet: remove unused fields, move ipv6-related field close   to each other, add a pointer to l3<->l2 translation tables (arp,nd6,   etc.) for future use.
    + struct route: remove an unused field, move close to each
      other some fields that might likely go away in the future

--- sys/netinet/if_ether.h
+++ sys/netinet/if_ether.h
@@ -113,8 +113,8 @@ struct sockaddr_inarp {
 extern u_char	ether_ipmulticast_min[ETHER_ADDR_LEN];
 extern u_char	ether_ipmulticast_max[ETHER_ADDR_LEN];
 
-int	arpresolve(struct ifnet *, struct rtentry *, struct mbuf *,
-	    struct sockaddr *, u_char *, struct rtentry *);
+int	arpresolve(struct ifnet *ifp, struct rtentry *rt,
+		struct mbuf *m, struct sockaddr *dst, u_char *desten);
 void	arp_ifinit(struct ifnet *, struct ifaddr *);
 #endif
 
