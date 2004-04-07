3544a7af2b58bcc6e1f6c3c9caf4d1f42508261e
imp <imp@FreeBSD.org>
2004-04-07 20:46:16 +0000

    Remove advertising clause from University of California Regent's license, per letter dated July 22, 1999 and email from Peter Wemm, Alan Cox and Robert Watson.
    Approved by: core, peter, alc, rwatson

--- sys/fs/procfs/procfs_ctl.c
+++ sys/fs/procfs/procfs_ctl.c
@@ -14,10 +14,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
  * 4. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
@@ -37,7 +33,7 @@
  *	@(#)procfs_ctl.c	8.4 (Berkeley) 6/15/94
  *
  * From:
- *	$Id: procfs_ctl.c,v 3.2 1993/12/15 09:40:17 jsp Exp $
+ *	$Id: procfs_ctl.c,v 1.51 2003/12/07 17:40:00 des Exp $
  * $FreeBSD$
  */
 
