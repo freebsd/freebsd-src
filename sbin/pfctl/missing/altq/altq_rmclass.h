4c723140a407d2ba326c134a26cec9569dc52d46
Mark Murray <markm@FreeBSD.org>
2004-04-09 19:58:40 +0000

    Remove advertising clause from University of California Regent's license, per letter dated July 22, 1999.
    Approved by: core, imp

--- sbin/pfctl/missing/altq/altq_rmclass.h
+++ sbin/pfctl/missing/altq/altq_rmclass.h
@@ -13,10 +13,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the Network Research
- *	Group at Lawrence Berkeley Laboratory.
  * 4. Neither the name of the University nor of the Laboratory may be used
  *    to endorse or promote products derived from this software without
  *    specific prior written permission.
