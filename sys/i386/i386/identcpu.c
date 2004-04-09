a26bd9a5dcbe4d2eba871611548f0c6a78658d87
markm <markm@FreeBSD.org>
2004-04-09 17:00:03 +0000

    I hate noticing bugs after committing. :-( ALWAYS set up the CPU base identity string. THEN optionally add features.

--- sys/i386/i386/identcpu.c
+++ sys/i386/i386/identcpu.c
@@ -546,10 +546,10 @@ printcpuinfo(void)
 			strcpy(cpu_model, "VIA C3 Ezra-T");
 			break;
 		case 0x690:
+			strcpy(cpu_model, "VIA C3 Nehemiah");
 			do_cpuid(0xc0000000, regs);
 			if (regs[0] == 0xc0000001) {
 				do_cpuid(0xc0000001, regs);
-				strcpy(cpu_model, "VIA C3 Nehemiah");
 				if ((cpu_id & 0xf) >= 3)
 					if ((regs[3] & 0x0c) == 0x0c)
 						strcat(cpu_model, "+RNG");
