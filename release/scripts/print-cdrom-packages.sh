5ce1201d3c49fdaf5cd77471e6d4d18c09d4231d
murray <murray@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-04-19 08:37:43 +0000

    Add security/portaudit to disc3.
    Requested by:	Xin Li <delphij@frontfree.net>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@128423 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/scripts/print-cdrom-packages.sh
+++ release/scripts/print-cdrom-packages.sh
@@ -174,6 +174,7 @@ CDROM_SET_3="${CDROM_SET_3} editors/xemacs21"
 CDROM_SET_3="${CDROM_SET_3} lang/gnat"
 CDROM_SET_3="${CDROM_SET_3} net/cvsup-without-gui"
 CDROM_SET_3="${CDROM_SET_3} print/teTeX"
+CDROM_SET_3="${CDROM_SET_3} security/portaudit"
 CDROM_SET_3="${CDROM_SET_3} textproc/docproj-jadetex"
 
 ## End of set for CDROM #3
