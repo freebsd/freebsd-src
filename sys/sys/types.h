f4d802a31d45d8a7ead1fa9125e4d5a809ddf06a
obrien <obrien@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-04-19 22:49:00 +0000

    Prevent SysV spellings in int's in the kernel.
    Submitted by:	bde
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@128448 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/types.h
+++ sys/sys/types.h
@@ -49,8 +49,10 @@ typedef	unsigned char	u_char;
 typedef	unsigned short	u_short;
 typedef	unsigned int	u_int;
 typedef	unsigned long	u_long;
+#ifndef _KERNEL
 typedef	unsigned short	ushort;		/* Sys V compatibility */
 typedef	unsigned int	uint;		/* Sys V compatibility */
+#endif
 #endif
 
 /*
