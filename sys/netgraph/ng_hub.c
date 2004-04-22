5b0c4be20c0e72c6fb22266a80f8320c406bf240
Ruslan Ermilov <ru@FreeBSD.org>
2004-04-22 21:43:43 +0000

    MFC: ng_hub(4).

--- sys/netgraph/ng_hub.c
+++ sys/netgraph/ng_hub.c
@@ -45,47 +45,48 @@ static struct ng_type ng_hub_typestruct = {
 	.name =		NG_HUB_NODE_TYPE,
 	.constructor =	ng_hub_constructor,
 	.rcvdata =	ng_hub_rcvdata,
+	.rcvdataq =	ng_hub_rcvdata,
 	.disconnect =	ng_hub_disconnect,
 };
 NETGRAPH_INIT(hub, &ng_hub_typestruct);
 
 
 static int
-ng_hub_constructor(node_p node)
+ng_hub_constructor(node_p *nodep)
 {
 
-	return (0);
+	/* Call the generic node constructor. */
+	return (ng_make_node_common(&ng_hub_typestruct, nodep));
 }
 
 static int
-ng_hub_rcvdata(hook_p hook, item_p item)
+ng_hub_rcvdata(hook_p hook, struct mbuf *m, meta_p meta)
 {
 	const node_p node = NG_HOOK_NODE(hook);
 	int error = 0;
 	hook_p hook2;
-	struct mbuf * const m = NGI_M(item), *m2;
-	meta_p const meta = NGI_META(item);
+	struct mbuf *m2;
 	meta_p meta2;
 	int nhooks;
 
 	if ((nhooks = NG_NODE_NUMHOOKS(node)) == 1) {
-		NG_FREE_ITEM(item);
+		NG_FREE_DATA(m, meta);
 		return (0);
 	}
-	LIST_FOREACH(hook2, &node->nd_hooks, hk_hooks) {
+	LIST_FOREACH(hook2, &node->hooks, hooks) {
 		if (hook2 == hook)
 			continue;
 		if (--nhooks == 1)
-			NG_FWD_ITEM_HOOK(error, item, hook2);
+			NG_SEND_DATA(error, hook2, m, meta);
 		else {
 			if ((m2 = m_dup(m, M_DONTWAIT)) == NULL) {
-				NG_FREE_ITEM(item);
+				NG_FREE_DATA(m, meta);
 				return (ENOBUFS);
 			}
 			if (meta != NULL) {
 				if ((meta2 = ng_copy_meta(meta)) == NULL) {
 					m_freem(m2);
-					NG_FREE_ITEM(item);
+					NG_FREE_DATA(m, meta);
 					return (ENOMEM);
 				}
 			} else
@@ -95,7 +96,6 @@ ng_hub_rcvdata(hook_p hook, item_p item)
 				continue;	/* don't give up */
 		}
 	}
-
 	return (error);
 }
 
@@ -105,6 +105,6 @@ ng_hub_disconnect(hook_p hook)
 
 	if (NG_NODE_NUMHOOKS(NG_HOOK_NODE(hook)) == 0 &&
 	    NG_NODE_IS_VALID(NG_HOOK_NODE(hook)))
-		ng_rmnode_self(NG_HOOK_NODE(hook));
+		ng_rmnode(NG_HOOK_NODE(hook));
 	return (0);
 }
