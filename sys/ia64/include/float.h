bea8ee27aa00e3f1a39bf14fd67ddbe32f50bbdf
das <das@FreeBSD.org>
2004-04-25 02:36:29 +0000

    Hide FLT_EVAL_METHOD and DECIMAL_DIG in pre-C99 compilation environments.
    PR:		63935
    Submitted by:	Stefan Farfeleder <stefan@fafoe.narf.at>

--- sys/ia64/include/float.h
+++ sys/ia64/include/float.h
@@ -37,8 +37,10 @@
 
 #define FLT_RADIX	2		/* b */
 #define FLT_ROUNDS	1		/* FP addition rounds to nearest */
+#if __ISO_C_VISIBLE >= 1999
 #define	FLT_EVAL_METHOD	0		/* no promotions */
 #define	DECIMAL_DIG	35		/* max precision in decimal digits */
+#endif
 
 #define FLT_MANT_DIG	24		/* p */
 #define FLT_EPSILON	1.19209290E-07F	/* b**(1-p) */
