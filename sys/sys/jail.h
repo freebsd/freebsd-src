6bbcc9da29c8a05cfc4e9ea4b0c011a198f922f9
bmilekic <bmilekic@FreeBSD.org>
2004-04-26 19:46:52 +0000

    Give jail(8) the feature to allow raw sockets from within a jail, which is less restrictive but allows for more flexible jail usage (for those who are willing to make the sacrifice). The default is off, but allowing raw sockets within jails can now be accomplished by tuning security.jail.allow_raw_sockets to 1.
    Turning this on will allow you to use things like ping(8)
    or traceroute(8) from within a jail.
    
    The patch being committed is not identical to the patch
    in the PR.  The committed version is more friendly to
    APIs which pjd is working on, so it should integrate
    into his work quite nicely.  This change has also been
    presented and addressed on the freebsd-hackers mailing
    list.
    
    Submitted by: Christian S.J. Peron <maneo@bsdpro.com>
    PR: kern/65800

--- sys/sys/jail.h
+++ sys/sys/jail.h
@@ -82,6 +82,7 @@ extern int	jail_set_hostname_allowed;
 extern int	jail_socket_unixiproute_only;
 extern int	jail_sysvipc_allowed;
 extern int	jail_getfsstat_jailrootonly;
+extern int	jail_allow_raw_sockets;
 
 LIST_HEAD(prisonlist, prison);
 extern struct	prisonlist allprison;
