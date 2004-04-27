b4b831ef39a3e2f33a3acd094eac471c502ec259
Ruslan Ermilov <ru@FreeBSD.org>
2004-04-27 15:00:29 +0000

    - Added rad_demangle() for demangling user-passwords (needed for   MS-CHAPv1 MPPE-keys). - Added rad_demangle_mppe_key() for demangling mppe-keys (needed   for MPPE-keys). - Added some typecasts for avoiding compiler warnings. - Fix: better handle wrong usage of the lib (if the programmer   has not called rad_create_request() but rad_put_*(), then a   weird error message was returned). - Added a new function for putting the Message-Authenticator. - Verify the Message-Authenticator, if it was found inside a   response packet and silently drop the packet, if the validation   failed. - Implicitly put the Message-Authenticator, if the EAP-Message   attribute was added. - Added some missing defines.
    Submitted by:	Michael Bretterklieber
    PR:		46555

--- lib/libradius/radlib_vs.h
+++ lib/libradius/radlib_vs.h
@@ -66,15 +66,19 @@
 	#define	RAD_MICROSOFT_MS_SECONDARY_NBNS_SERVER		31
 	#define	RAD_MICROSOFT_MS_ARAP_CHALLENGE			33
 
+#define SALT_LEN    2
+
 struct rad_handle;
 
 __BEGIN_DECLS
-int	rad_get_vendor_attr(u_int32_t *, const void **, size_t *);
-int	rad_put_vendor_addr(struct rad_handle *, int, int, struct in_addr);
-int	rad_put_vendor_attr(struct rad_handle *, int, int, const void *,
+int	 rad_get_vendor_attr(u_int32_t *, const void **, size_t *);
+int	 rad_put_vendor_addr(struct rad_handle *, int, int, struct in_addr);
+int	 rad_put_vendor_attr(struct rad_handle *, int, int, const void *,
 	    size_t);
-int	rad_put_vendor_int(struct rad_handle *, int, int, u_int32_t);
-int	rad_put_vendor_string(struct rad_handle *, int, int, const char *);
+int	 rad_put_vendor_int(struct rad_handle *, int, int, u_int32_t);
+int	 rad_put_vendor_string(struct rad_handle *, int, int, const char *);
+u_char	*rad_demangle_mppe_key(struct rad_handle *, const void *, size_t,
+	    size_t *);
 __END_DECLS
 
 #endif /* _RADLIB_VS_H_ */
