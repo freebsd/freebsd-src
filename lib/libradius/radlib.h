35b49269668239eb12bbcb3a135d26548cf55f4f
ru <ru@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-04-27 15:00:29 +0000

    - Added rad_demangle() for demangling user-passwords (needed for   MS-CHAPv1 MPPE-keys). - Added rad_demangle_mppe_key() for demangling mppe-keys (needed   for MPPE-keys). - Added some typecasts for avoiding compiler warnings. - Fix: better handle wrong usage of the lib (if the programmer   has not called rad_create_request() but rad_put_*(), then a   weird error message was returned). - Added a new function for putting the Message-Authenticator. - Verify the Message-Authenticator, if it was found inside a   response packet and silently drop the packet, if the validation   failed. - Implicitly put the Message-Authenticator, if the EAP-Message   attribute was added. - Added some missing defines.
    Submitted by:	Michael Bretterklieber
    PR:		46555
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@128684 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libradius/radlib.h
+++ lib/libradius/radlib.h
@@ -32,6 +32,9 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 
+/* Limits */
+#define RAD_MAX_ATTR_LEN		253
+
 /* Message types */
 #define RAD_ACCESS_REQUEST		1
 #define RAD_ACCESS_ACCEPT		2
@@ -98,6 +101,9 @@
 #define RAD_FRAMED_APPLETALK_NETWORK	38	/* Integer */
 #define RAD_FRAMED_APPLETALK_ZONE	39	/* Integer */
      /* reserved for accounting		40-59 */
+#define RAD_ACCT_INPUT_GIGAWORDS	52
+#define RAD_ACCT_OUTPUT_GIGAWORDS	53
+
 #define RAD_CHAP_CHALLENGE		60	/* String */
 #define RAD_NAS_PORT_TYPE		61	/* Integer */
 	#define RAD_ASYNC			0
@@ -123,6 +129,9 @@
 #define RAD_PORT_LIMIT			62	/* Integer */
 #define RAD_LOGIN_LAT_PORT		63	/* Integer */
 #define RAD_CONNECT_INFO		77	/* String */
+#define RAD_EAP_MESSAGE			79	/* Octets */
+#define RAD_MESSAGE_AUTHENTIC		80	/* Octets */
+#define RAD_ACCT_INTERIM_INTERVAL	85	/* Integer */
 #define RAD_NAS_IPV6_ADDRESS		95	/* IPv6 address */
 #define RAD_FRAMED_INTERFACE_ID		96	/* 8 octets */
 #define RAD_FRAMED_IPV6_PREFIX		97	/* Octets */
@@ -134,6 +143,7 @@
 #define RAD_ACCT_STATUS_TYPE		40	/* Integer */
 	#define RAD_START			1
 	#define RAD_STOP			2
+	#define RAD_UPDATE			3
 	#define RAD_ACCOUNTING_ON		7
 	#define RAD_ACCOUNTING_OFF		8
 #define RAD_ACCT_DELAY_TIME		41	/* Integer */
@@ -196,11 +206,15 @@ int			 rad_put_attr(struct rad_handle *, int,
 int			 rad_put_int(struct rad_handle *, int, u_int32_t);
 int			 rad_put_string(struct rad_handle *, int,
 			    const char *);
+int			 rad_put_message_authentic(struct rad_handle *);
 ssize_t			 rad_request_authenticator(struct rad_handle *, char *,
 			    size_t);
 int			 rad_send_request(struct rad_handle *);
 const char		*rad_server_secret(struct rad_handle *);
 const char		*rad_strerror(struct rad_handle *);
+u_char			*rad_demangle(struct rad_handle *, const void *,
+			    size_t);
+
 __END_DECLS
 
 #endif /* _RADLIB_H_ */
