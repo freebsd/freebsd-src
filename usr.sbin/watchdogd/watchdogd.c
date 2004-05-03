4f96babdb8a049b51ed708e44a801a668ef1f16e
smkelly <smkelly@FreeBSD.org>
2004-05-03 21:41:02 +0000

    Bump the copyright year since I forgot last time.

--- usr.sbin/watchdogd/watchdogd.c
+++ usr.sbin/watchdogd/watchdogd.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003  Sean M. Kelly <smkelly@FreeBSD.org>
+ * Copyright (c) 2003-2004  Sean M. Kelly <smkelly@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
