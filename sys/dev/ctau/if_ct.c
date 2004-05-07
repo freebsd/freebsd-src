5e58127328df6b4af3de497cfd745906745e8c2d
rik <rik@FreeBSD.org>
2004-05-07 11:56:07 +0000

    Sync with RELENG_4

--- sys/dev/ctau/if_ct.c
+++ sys/dev/ctau/if_ct.c
@@ -64,7 +64,7 @@ __FBSDID("$FreeBSD$");
 #   include <i386/isa/intr_machdep.h>
 #   endif
 #endif
-#if __FreeBSD_version >= 500000
+#if __FreeBSD_version >= 400000
 #include <dev/cx/machdep.h>
 #include <dev/ctau/ctddk.h>
 #include <dev/cx/cronyxfw.h>
@@ -78,7 +78,7 @@ __FBSDID("$FreeBSD$");
 #   include "opt_netgraph.h"
 #   include <netgraph/ng_message.h>
 #   include <netgraph/netgraph.h>
-#if __FreeBSD_version >= 500000
+#if __FreeBSD_version >= 400000
 #   include <dev/ctau/ng_ct.h>
 #else
 #   include <netgraph/ng_ct.h>
