f309a749f55f70c2cd608bb5406228db7803873f
bde <bde@FreeBSD.org>
2004-05-19 15:41:26 +0000

    Moved most of the "MI" definitions and declarations from <machine/profile.h> to <sys/gmon.h>.  Cleaned them up a little by not attempting to ifdef for incomplete and out of date support for GUPROF in userland, as in the sparc64 version.

--- sys/sparc64/include/profile.h
+++ sys/sparc64/include/profile.h
@@ -46,38 +46,9 @@ typedef u_long	fptrdiff_t;
 #define	MCOUNT_ENTER(s)	s = rdpr(pil); wrpr(pil, 0, 14)
 #define	MCOUNT_EXIT(s)	wrpr(pil, 0, s)
 
-#ifdef GUPROF
-
-#define	CALIB_SCALE	1000
-#define	KCOUNT(p,index) \
-	((p)->kcount[(index) / (HISTFRACTION * sizeof(HISTCOUNTER))])
-#define	PC_TO_I(p, pc)	((uintfptr_t)(pc) - (uintfptr_t)(p)->lowpc)
-
-struct gmonparam;
-
-extern	int cputime_bias;
-
-int	cputime(void);
-void	nullfunc_loop_profiled(void);
-void	nullfunc_profiled(void);
-void	startguprof(struct gmonparam *p);
-void	stopguprof(struct gmonparam *p);
-
-#else /* GUPROF */
-
-#define startguprof(p)
-#define stopguprof(p)
-
-#endif /* GUPROF */
-
-void	empty_loop(void);
-void	kmupetext(uintfptr_t nhighpc);
 void	mcount(uintfptr_t frompc, uintfptr_t selfpc);
-void	mexitcount(uintfptr_t selfpc);
-void	nullfunc(void);
-void	nullfunc_loop(void);
 
-#else /* _KERNEL */
+#else /* !_KERNEL */
 
 typedef u_long	uintfptr_t;
 
