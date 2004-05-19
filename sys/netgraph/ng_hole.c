63a58dc2d2db4c7d8f82237a7e949a7d09c4fc23
ru <ru@FreeBSD.org>
2004-05-19 11:26:33 +0000

    Maintain statistics about the received frames.

--- sys/netgraph/ng_hole.c
+++ sys/netgraph/ng_hole.c
@@ -51,10 +51,53 @@
 #include <sys/mbuf.h>
 #include <netgraph/ng_message.h>
 #include <netgraph/netgraph.h>
+#include <netgraph/ng_parse.h>
 #include <netgraph/ng_hole.h>
+
+/* Per hook private info. */
+struct ng_hole_hookinfo {
+	struct ng_hole_hookstat	stats;
+};
+typedef struct ng_hole_hookinfo *hinfo_p;
+
+/* Parse type for struct ng_hole_hookstat. */
+static const struct ng_parse_struct_field ng_hole_hookstat_type_fields[] =
+	NG_HOLE_HOOKSTAT_TYPE_INFO;
+static const struct ng_parse_type ng_hole_hookstat_type = {
+	&ng_parse_struct_type,
+	&ng_hole_hookstat_type_fields
+};
+
+/* List of commands and how to convert arguments to/from ASCII. */
+static const struct ng_cmdlist ng_hole_cmdlist[] = {
+	{
+	  NGM_HOLE_COOKIE,
+	  NGM_HOLE_GET_STATS,
+	  "getstats",
+	  &ng_parse_hookbuf_type,
+	  &ng_hole_hookstat_type
+	},
+	{
+	  NGM_HOLE_COOKIE,
+	  NGM_HOLE_CLR_STATS,
+	  "clrstats",
+	  &ng_parse_hookbuf_type,
+	  NULL
+	},
+	{
+	  NGM_HOLE_COOKIE,
+	  NGM_HOLE_GETCLR_STATS,
+	  "getclrstats",
+	  &ng_parse_hookbuf_type,
+	  &ng_hole_hookstat_type
+	},
+	{ 0 }
+};
 
 /* Netgraph methods */
 static ng_constructor_t	ngh_cons;
+static ng_rcvmsg_t	ngh_rcvmsg;
+static ng_newhook_t	ngh_newhook;
 static ng_rcvdata_t	ngh_rcvdata;
 static ng_disconnect_t	ngh_disconnect;
 
@@ -63,14 +106,14 @@ static struct ng_type typestruct = {
 	NG_HOLE_NODE_TYPE,
 	NULL,		/* modeventhand_t */
 	ngh_cons,	/* ng_constructor_t */
-	NULL,		/* ng_rcvmsg_t */
+	ngh_rcvmsg,	/* ng_rcvmsg_t */
 	NULL,		/* ng_shutdown_t */
-	NULL,		/* ng_newhook_t */
+	ngh_newhook,	/* ng_newhook_t */
 	NULL,		/* ng_findhook_t */
 	NULL,		/* ng_connect_t */
 	ngh_rcvdata,	/* ng_rcvdata_t */
 	ngh_disconnect,	/* ng_disconnect_t */
-	NULL		/* ng_cmdlist */
+	ng_hole_cmdlist	/* ng_cmdlist */
 };
 NETGRAPH_INIT(hole, &typestruct);
 
@@ -84,11 +127,90 @@ ngh_cons(node_p node)
 }
 
 /*
+ * Add a hook.
+ */
+static int
+ngh_newhook(node_p node, hook_p hook, const char *name)
+{
+	hinfo_p hip;
+
+	/* Create hook private structure. */
+	MALLOC(hip, hinfo_p, sizeof(*hip), M_NETGRAPH, M_NOWAIT | M_ZERO);
+	if (hip == NULL)
+		return (ENOMEM);
+	NG_HOOK_SET_PRIVATE(hook, hip);
+	return (0);
+}
+
+/*
+ * Receive a control message.
+ */
+static int
+ngh_rcvmsg(node_p node, item_p item, hook_p lasthook)
+{
+	struct ng_mesg *msg;
+	struct ng_mesg *resp = NULL;
+	int error = 0;
+	struct ng_hole_hookstat *stats;
+	hook_p hook;
+
+	NGI_GET_MSG(item, msg);
+	switch (msg->header.typecookie) {
+	case NGM_HOLE_COOKIE:
+		switch (msg->header.cmd) {
+		case NGM_HOLE_GET_STATS:
+		case NGM_HOLE_CLR_STATS:
+		case NGM_HOLE_GETCLR_STATS:
+			/* Sanity check. */
+			if (msg->header.arglen != NG_HOOKLEN + 1) {
+				error = EINVAL;
+				break;
+			}
+			/* Find hook. */
+			hook = ng_findhook(node, (char *)msg->data);
+			if (hook == NULL) {
+				error = ENOENT;
+				break;
+			}
+			stats = &((hinfo_p)NG_HOOK_PRIVATE(hook))->stats;
+			/* Build response (if desired). */
+			if (msg->header.cmd != NGM_HOLE_CLR_STATS) {
+				NG_MKRESPONSE(resp, msg, sizeof(*stats),
+				    M_NOWAIT);
+				if (resp == NULL) {
+					error = ENOMEM;
+					break;
+				}
+				bcopy(stats, resp->data, sizeof(*stats));
+			}
+			/* Clear stats (if desired). */
+			if (msg->header.cmd != NGM_HOLE_GET_STATS)
+				bzero(stats, sizeof(*stats));
+			break;
+		default:		/* Unknown command. */
+			error = EINVAL;
+			break;
+		}
+		break;
+	default:			/* Unknown type cookie. */
+		error = EINVAL;
+		break;
+	}
+	NG_RESPOND_MSG(error, node, item, resp);
+	NG_FREE_MSG(msg);
+	return (error);
+}
+
+/*
  * Receive data
  */
 static int
 ngh_rcvdata(hook_p hook, item_p item)
 {
+	const hinfo_p hip = NG_HOOK_PRIVATE(hook);
+
+	hip->stats.frames++;
+	hip->stats.octets += NGI_M(item)->m_pkthdr.len;
 	NG_FREE_ITEM(item);
 	return 0;
 }
@@ -99,6 +221,8 @@ ngh_rcvdata(hook_p hook, item_p item)
 static int
 ngh_disconnect(hook_p hook)
 {
+
+	NG_HOOK_SET_PRIVATE(hook, NULL);
 	if (NG_NODE_NUMHOOKS(NG_HOOK_NODE(hook)) == 0)
 		ng_rmnode_self(NG_HOOK_NODE(hook));
 	return (0);
