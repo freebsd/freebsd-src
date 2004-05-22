cedda3bff25c434b2d5e3edbc56c4e7bff3b034a
kientzle <kientzle@FreeBSD.org>
2004-05-22 17:49:54 +0000

    Add --no-same-owner as a synonym for -o. Note that bsdtar's -o (which follows SUSv2) is not the same as GNU tar's -o. In GNU tar, -o and --no-same-owner are not synonyms.
    Pointed out by: Kris Kennaway (required by xpenguins port)

--- usr.bin/tar/bsdtar.c
+++ usr.bin/tar/bsdtar.c
@@ -101,6 +101,7 @@ const struct option tar_longopts[] = {
 	{ "modification-time",  no_argument,       NULL, 'm' },
 	{ "nodump",             no_argument,       NULL, OPTION_NODUMP },
 	{ "norecurse",          no_argument,       NULL, 'n' },
+	{ "no-same-owner",	no_argument,	   NULL, 'o' },
 	{ "preserve-permissions", no_argument,     NULL, 'p' },
 	{ "read-full-blocks",	no_argument,	   NULL, 'B' },
 	{ "same-permissions",   no_argument,       NULL, 'p' },
