8df3c224c0ca4fa768b6f53c4fffac0752e807dd
Tim Kientzle <kientzle@FreeBSD.org>
2004-05-27 04:30:59 +0000

    Provide framework for exiting with non-zero value on non-critical errors.

--- usr.bin/tar/bsdtar.h
+++ usr.bin/tar/bsdtar.h
@@ -72,6 +72,8 @@ struct bsdtar {
 	size_t		  u_width; /* for 'list_item' in read.c */
 	char		 *user_uname; /* User running this program */
 	uid_t		  user_uid; /* UID running this program */
+	int		  return_value; /* Value returned by main() */
+	char		  warned_lead_slash; /* Already displayed warning */
 
         /*
 	 * Data for various subsystems.  Full definitions are located in
