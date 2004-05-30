5dba30f15ae1cec2dcdc19be3f3efa0331755f42
Poul-Henning Kamp <phk@FreeBSD.org>
2004-05-30 20:27:19 +0000

    add missing #include <sys/module.h>

--- sys/isa/vga_isa.c
+++ sys/isa/vga_isa.c
@@ -34,6 +34,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kernel.h>
+#include <sys/module.h>
 #include <sys/conf.h>
 #include <sys/bus.h>
 #include <sys/fbio.h>
