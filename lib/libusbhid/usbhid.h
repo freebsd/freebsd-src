7b2a405500a65be06e1bbaa38a20548245daebff
des <des@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-06-03 15:04:24 +0000

    Add __BEGIN_DECLS / __END_DECLS so this can be used in C++ code.
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@130030 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libusbhid/usbhid.h
+++ lib/libusbhid/usbhid.h
@@ -81,6 +81,8 @@ typedef struct hid_item {
 #define HID_PAGE(u) (((u) >> 16) & 0xffff)
 #define HID_USAGE(u) ((u) & 0xffff)
 
+__BEGIN_DECLS
+
 /* Obtaining a report descriptor, descr.c: */
 report_desc_t hid_get_report_desc(int file);
 report_desc_t hid_use_report_desc(unsigned char *data, unsigned int size);
@@ -103,3 +105,5 @@ int hid_parse_usage_page(const char *name);
 /* Extracting/insertion of data, data.c: */
 int hid_get_data(const void *p, const hid_item_t *h);
 void hid_set_data(void *p, const hid_item_t *h, int data);
+
+__END_DECLS
