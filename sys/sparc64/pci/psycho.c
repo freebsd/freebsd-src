4a63f9be7f43bfde21e0882ae07cf65d245fc1e2
phk <phk@FreeBSD.org>
2004-06-04 11:52:25 +0000

    Add missing <sys/module.h> #includes

--- sys/sparc64/pci/psycho.c
+++ sys/sparc64/pci/psycho.c
@@ -43,6 +43,7 @@
 #include <sys/systm.h>
 #include <sys/bus.h>
 #include <sys/kernel.h>
+#include <sys/module.h>
 #include <sys/malloc.h>
 #include <sys/pcpu.h>
 
--- sys/sparc64/sparc64/nexus.c
+++ sys/sparc64/sparc64/nexus.c
@@ -39,6 +39,7 @@
 #include <sys/cons.h>
 #include <sys/kernel.h>
 #include <sys/malloc.h>
+#include <sys/module.h>
 
 #include <dev/ofw/openfirm.h>
 
