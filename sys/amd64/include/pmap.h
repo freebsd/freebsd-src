8a41fbc2075568db0ac84b7c65974c27c858362f
peter <peter@FreeBSD.org>
2004-06-08 00:29:42 +0000

    Mask pte's with PG_FRAME before passing it to PHYS_TO_VM_PAGE()..  PG_NX lives in the top 12 'available' bits.  atop() in the PHYS_TO_VM_PAGE() macro only masks off the lower bits (by accident) and the upper bits in the 64 bit ptes turn into "interesting" index values.

--- sys/amd64/include/pmap.h
+++ sys/amd64/include/pmap.h
@@ -67,7 +67,7 @@
 /* Our various interpretations of the above */
 #define PG_W		PG_AVAIL1	/* "Wired" pseudoflag */
 #define	PG_MANAGED	PG_AVAIL2
-#define	PG_FRAME	(~((vm_paddr_t)PAGE_MASK))
+#define	PG_FRAME	(0x000ffffffffff000ul)
 #define	PG_PROT		(PG_RW|PG_U)	/* all protection bits . */
 #define PG_N		(PG_NC_PWT|PG_NC_PCD)	/* Non-cacheable */
 
