d6abe599a81f1955bb129ca1f5c0260f7879efe5
Diomidis Spinellis <dds@FreeBSD.org>
2004-06-15 13:31:20 +0000

    MFC: Bring the description for login_getclassbyname in sync with the function's arguments.  The function has as a second argument a struct passwd * pointer, not a directory name.

--- lib/libutil/login_cap.c
+++ lib/libutil/login_cap.c
@@ -171,9 +171,10 @@ login_close(login_cap_t * lc)
  * login_getclassbyname() get the login class by its name.
  * If the name given is NULL or empty, the default class
  * LOGIN_DEFCLASS (ie. "default") is fetched. If the
- * 'dir' argument contains a non-NULL non-empty string,
- * then the file _FILE_LOGIN_CONF is picked up from that
- * directory instead of the system login database.
+ * 'pwd' argument is non-NULL and contains an non-NULL
+ * dir entry, then the file _FILE_LOGIN_CONF is picked
+ * up from that directory and used before the system
+ * login database.
  * Return a filled-out login_cap_t structure, including
  * class name, and the capability record buffer.
  */
