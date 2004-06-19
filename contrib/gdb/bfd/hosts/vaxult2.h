fdedc45ff6ac8d3d358f1544d3f6f2b1c66c4365
Marcel Moolenaar <marcel@FreeBSD.org>
2004-06-19 15:43:52 +0000

    This file was not part of the GDB 5.2.1 import and should have been deleted from the vendor branch.

--- contrib/gdb/bfd/hosts/vaxult2.h
+++
@@ -1,8 +0,0 @@
-#include <machine/param.h>
-#include <machine/vmparam.h>
-#define	HOST_PAGE_SIZE		(NBPG*CLSIZE)
-#define	HOST_MACHINE_ARCH	bfd_arch_vax
-
-#define	HOST_TEXT_START_ADDR	USRTEXT
-#define	HOST_STACK_END_ADDR	USRSTACK
-#undef	HOST_BIG_ENDIAN_P
