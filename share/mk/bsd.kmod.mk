be8b4bc83157f3acf79926dc2e6e92e3034784a1
Pawel Jakub Dawidek <pjd@FreeBSD.org>
2004-06-21 08:41:50 +0000

    - Missing trailing slash for a kern directory check. - Check in both places if ${_dir}/conf/kmod.mk exists. - Style fixes (lines too long).
    Submitted by:	bde

--- share/mk/bsd.kmod.mk
+++ share/mk/bsd.kmod.mk
@@ -1,12 +1,14 @@
 # $FreeBSD$
 
 # Search for kernel source tree in standard places.
-.for _dir in ${.CURDIR}/../.. ${.CURDIR}/../../.. ${.CURDIR}/../../../.. /sys /usr/src/sys
-.if !defined(SYSDIR) && exists(${_dir}/kern) && exists(${_dir}/conf/kmod.mk)
+.for _dir in ${.CURDIR}/../.. ${.CURDIR}/../../.. ${.CURDIR}/../../../.. \
+    /sys /usr/src/sys                                                                                                                                                       
+.if !defined(SYSDIR) && exists(${_dir}/kern/) && exists(${_dir}/conf/kmod.mk)
 SYSDIR=	${_dir}
 .endif
 .endfor
-.if !defined(SYSDIR) || !exists(${SYSDIR}/kern) || !exists(${SYSDIR}/conf/)
+.if !defined(SYSDIR) || !exists(${SYSDIR}/kern/) || \
+    !exists(${SYSDIR}/conf/kmod.mk)
 .error "can't find kernel source tree"
 .endif
 
