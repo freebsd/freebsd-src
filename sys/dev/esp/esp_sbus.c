a49d49803255dfd6e8fba1105ec3be82f95aeabf
scottl <scottl@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-06-21 07:27:34 +0000

    Include module.h
    Submitted by:	Koop Mast
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@130833 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/esp/esp_sbus.c
+++ sys/dev/esp/esp_sbus.c
@@ -71,6 +71,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/systm.h>
 #include <sys/bus.h>
 #include <sys/kernel.h>
+#include <sys/module.h>
 #include <sys/resource.h>
 #include <sys/lock.h>
 #include <sys/mutex.h>
