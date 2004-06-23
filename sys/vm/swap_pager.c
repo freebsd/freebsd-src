b1c1a50c42dc8d873a2ca96f05718e7ef55d0005
bms <bms@FreeBSD.org>
2004-06-23 15:15:07 +0000

    In swap_pager_getpages(), bp->b_dev can be NULL, particularly for the case of NFS mounted swap, so do not try to dereference it.
    While we're here, brucify the printf() call which happens when we
    time out on acquisition of vm_page_queue_mtx.
    
    PR:		kern/67898
    Submitted by:	bde (style)

--- sys/vm/swap_pager.c
+++ sys/vm/swap_pager.c
@@ -1131,12 +1131,10 @@ swap_pager_getpages(vm_object_t object, vm_page_t *m, int count, int reqpage)
 		vm_page_flag_set(mreq, PG_WANTED | PG_REFERENCED);
 		cnt.v_intrans++;
 		if (msleep(mreq, &vm_page_queue_mtx, PSWP, "swread", hz*20)) {
-			printf(
-			    "swap_pager: indefinite wait buffer: device:"
-				" %s, blkno: %ld, size: %ld\n",
-			    devtoname(bp->b_dev), (long)bp->b_blkno,
-			    bp->b_bcount
-			);
+			printf("swap_pager: indefinite wait buffer: device: "
+			    "%s, blkno: %jd, size: %ld\n",
+			    bp->b_dev == NULL ? "[NULL]" : devtoname(bp->b_dev),
+			    (intmax_t)bp->b_blkno, bp->b_bcount);
 		}
 	}
 	vm_page_unlock_queues();
