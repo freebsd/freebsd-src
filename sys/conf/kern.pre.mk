bac8cc2a430f9c92e42c0e8b79eac42b782d818f
imp <imp@FreeBSD.org>
2004-06-27 23:03:43 +0000

    We don't need GEN_M_CFILES.  Fold it into GEN_CFILES definition and adjust as necessary.
    Suggested by: bde

--- sys/conf/kern.pre.mk
+++ sys/conf/kern.pre.mk
@@ -117,7 +117,7 @@ NORMAL_M= ${AWK} -f $S/tools/makeobjops.awk ${.IMPSRC} -c ; \
 
 NORMAL_LINT=	${LINT} ${LINTFLAGS} ${CFLAGS:M-[DIU]*} ${.IMPSRC}
 
-GEN_CFILES= $S/$M/$M/genassym.c
+GEN_CFILES= $S/$M/$M/genassym.c ${MFILES:T:S/.m$/.c/}
 SYSTEM_CFILES= config.c env.c hints.c majors.c vnode_if.c
 SYSTEM_DEP= Makefile ${SYSTEM_OBJS}
 SYSTEM_OBJS= locore.o ${MDOBJS} ${OBJS}
