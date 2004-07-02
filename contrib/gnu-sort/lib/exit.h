7e89f68317bac7ad9b038e5112ee343787a48357
tjr <tjr@FreeBSD.org>
2004-07-02 09:18:31 +0000

    Import of GNU sort from coreutils 5.2.1 (trimmed)

--- contrib/gnu-sort/lib/exit.h
+++ contrib/gnu-sort/lib/exit.h
@@ -1,6 +1,5 @@
-/* Invoke unistd functions, but avoid some glitches.
-
-   Copyright (C) 2001, 2003 Free Software Foundation, Inc.
+/* exit() function.
+   Copyright (C) 1995, 2001 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -16,6 +15,18 @@
    along with this program; if not, write to the Free Software Foundation,
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-/* Written by Paul Eggert.  */
+#ifndef _EXIT_H
+#define _EXIT_H
 
-int dup_safer (int);
+/* Get exit() declaration.  */
+#include <stdlib.h>
+
+/* Some systems do not define EXIT_*, even with STDC_HEADERS.  */
+#ifndef EXIT_SUCCESS
+# define EXIT_SUCCESS 0
+#endif
+#ifndef EXIT_FAILURE
+# define EXIT_FAILURE 1
+#endif
+
+#endif /* _EXIT_H */
