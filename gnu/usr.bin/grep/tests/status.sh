5c4ea3b2fcac9bff7e94096b3557a074f76089da
tjr <tjr@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-07-04 09:52:08 +0000

    This commit was generated by cvs2svn to compensate for changes in r131554, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@131555 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- gnu/usr.bin/grep/tests/status.sh
+++ gnu/usr.bin/grep/tests/status.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 # Test for status code for GNU grep.
 # status code
 #  0 match found
@@ -24,14 +24,28 @@ if test $? -ne 1 ; then
 fi
 
 # the filename MMMMMMMM.MMM should not exist hopefully
-# should return 2 file not found
-if test -b MMMMMMMM.MMM; then
+if test -r MMMMMMMM.MMM; then
 	echo "Please remove MMMMMMMM.MMM to run check"
 else
-	${GREP} -E -e 'abc' MMMMMMMM.MMM> /dev/null 2>&1
+	# should return 2 file not found
+	${GREP} -E -e 'abc' MMMMMMMM.MMM > /dev/null 2>&1
 	if test $? -ne 2 ; then
         	echo "Status: Wrong status code, test \#3 failed"
         	failures=1
+	fi
+
+	# should return 2 file not found
+	${GREP} -E -s -e 'abc' MMMMMMMM.MMM > /dev/null 2>&1
+	if test $? -ne 2 ; then
+        	echo "Status: Wrong status code, test \#4 failed"
+        	failures=1
+	fi
+
+	# should return 0 found a match
+	echo "abcd" | ${GREP} -E -q -s 'abc' MMMMMMMM.MMM - > /dev/null 2>&1
+	if test $? -ne 0 ; then
+		echo "Status: Wrong status code, test \#5 failed"
+		failures=1
 	fi
 fi
 
--- gnu/usr.bin/grep/tests/warning.sh
+++ gnu/usr.bin/grep/tests/warning.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 #
 # Tell them not to be alarmed.
 
