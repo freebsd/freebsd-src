00f989b708d4d3766b7051f3e62abff73a0a36e1
tjr <tjr@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-07-04 10:02:03 +0000

    Merge local changes.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@131557 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- gnu/usr.bin/grep/getpagesize.h
+++ gnu/usr.bin/grep/getpagesize.h
@@ -4,6 +4,11 @@
 
 #ifndef HAVE_GETPAGESIZE
 
+#if !defined getpagesize && defined __BEOS__
+# include <OS.h>
+# define getpagesize() B_PAGE_SIZE
+#endif
+
 #ifdef HAVE_UNISTD_H
 # include <unistd.h>
 #endif
