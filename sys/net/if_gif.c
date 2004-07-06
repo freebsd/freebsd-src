ca41328fc9e5b6c62aa7755aab141eb861f38a29
bms <bms@FreeBSD.org>
2004-07-06 03:26:26 +0000

    Use M_ZERO instead of bzero().

--- sys/net/if_gif.c
+++ sys/net/if_gif.c
@@ -143,8 +143,7 @@ gif_clone_create(ifc, unit)
 {
 	struct gif_softc *sc;
 
-	sc = malloc (sizeof(struct gif_softc), M_GIF, M_WAITOK);
-	bzero(sc, sizeof(struct gif_softc));
+	sc = malloc(sizeof(struct gif_softc), M_GIF, M_WAITOK | M_ZERO);
 
 	sc->gif_if.if_softc = sc;
 	if_initname(&sc->gif_if, ifc->ifc_name, unit);
