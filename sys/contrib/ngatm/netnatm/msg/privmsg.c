1c410d289ab6d02a22c42034d9d435a726f22a92
harti <harti@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-07-08 16:39:03 +0000

    This commit was generated by cvs2svn to compensate for changes in r131826, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@131827 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/contrib/ngatm/netnatm/msg/privmsg.c
+++ sys/contrib/ngatm/netnatm/msg/privmsg.c
@@ -26,7 +26,7 @@
  *
  * Author: Hartmut Brandt <harti@freebsd.org>
  *
- * $Begemot: libunimsg/atm/msg/privmsg.c,v 1.3 2003/09/19 11:58:15 hbb Exp $
+ * $Begemot: libunimsg/netnatm/msg/privmsg.c,v 1.8 2003/10/10 14:50:05 hbb Exp $
  *
  * Private definitions for the MSG code file.
  *
--- sys/contrib/ngatm/netnatm/saal/sscop.h
+++ sys/contrib/ngatm/netnatm/saal/sscop.h
@@ -26,7 +26,7 @@
  *
  * Author: Hartmut Brandt <harti@freebsd.org>
  *
- * $Begemot: libunimsg/atm/saal/sscop.h,v 1.3 2003/09/19 12:02:03 hbb Exp $
+ * $Begemot: libunimsg/netnatm/saal/sscop.h,v 1.4 2004/07/08 08:22:16 brandt Exp $
  *
  * External interface to sscop.
  */
