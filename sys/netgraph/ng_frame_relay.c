e6f3da3092947cf37e6dda378edb1350254d9828
rwatson <rwatson@FreeBSD.org>
2004-07-12 04:35:42 +0000

    Mark 'makeup' in ng_frame_relay as const, as its values are immutable.

--- sys/netgraph/ng_frame_relay.c
+++ sys/netgraph/ng_frame_relay.c
@@ -99,7 +99,7 @@ typedef struct frmrel_softc *sc_p;
 #define LASTBYTE_D_C	0x02	/* last byte is dl_core or dlci info */
 
 /* Used to do headers */
-static struct segment {
+const static struct segment {
 	u_char  mask;
 	u_char  shift;
 	u_char  width;
