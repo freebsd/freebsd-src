4d3aa77ad3b1fed4cb8736e45006287bd12b1107
cperciva <cperciva@FreeBSD.org>
2004-07-13 13:10:07 +0000

    Replace "uid != 0" with "suser(td->td_ucred) != 0" when checking if we've hit the maximum number of processes.  The last ten processes are reserved for the *non-jailed* superuser.

--- sys/kern/kern_fork.c
+++ sys/kern/kern_fork.c
@@ -300,7 +300,8 @@ fork1(td, flags, pages, procp)
 	 */
 	sx_xlock(&allproc_lock);
 	uid = td->td_ucred->cr_ruid;
-	if ((nprocs >= maxproc - 10 && uid != 0) || nprocs >= maxproc) {
+	if ((nprocs >= maxproc - 10 && suser(td->td_ucred) != 0) ||
+	    nprocs >= maxproc) {
 		error = EAGAIN;
 		goto fail;
 	}
