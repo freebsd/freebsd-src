b37b1b97f1cdef84dea643ac2625071f8cb49773
tjr <tjr@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-07-14 10:06:22 +0000

    Make the 'y' (translate) command aware of multibyte characters.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@132145 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/sed/main.c
+++ usr.bin/sed/main.c
@@ -56,6 +56,7 @@ static const char sccsid[] = "@(#)main.c	8.2 (Berkeley) 1/3/94";
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <locale.h>
 #include <regex.h>
 #include <stddef.h>
