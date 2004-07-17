d75300bfa58d8a70a02b8d00c349d2c643eff45a
Alfred Perlstein <alfred@FreeBSD.org>
2004-07-17 19:19:36 +0000

    When reporting reciept of a signal, print the signal's name.

--- usr.bin/truss/extern.h
+++ usr.bin/truss/extern.h
@@ -35,6 +35,7 @@ extern int setup_and_wait(char **);
 extern int start_tracing(int, int, int);
 extern void restore_proc(int);
 extern const char *ioctlname(register_t val);
+extern char *strsig(int sig);
 #ifdef __alpha__
 extern void alpha_syscall_entry(struct trussinfo *, int);
 extern long alpha_syscall_exit(struct trussinfo *, int);
