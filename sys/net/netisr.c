7540574521e0a4c9637c68aa5af5076bd198d283
rwatson <rwatson@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-07-18 21:50:22 +0000

    Comment clarifying debug_mpsafenet.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@132368 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net/netisr.c
+++ sys/net/netisr.c
@@ -53,10 +53,11 @@
 #include <net/if_var.h>
 #include <net/netisr.h>
 
-/*
- * XXX this is a temporary measure to allow folks to
- * XXX disable Giant locking in the network code without
- * XXX recompiling--in case of problems.
+/* 
+ * debug_mpsafenet controls network subsystem-wide use of the Giant lock,
+ * from system calls down to interrupt handlers.  It can be changed only
+ * via a tunable at boot, not at run-time, due to the complexity of
+ * unwinding.
  */
 int	debug_mpsafenet = 0;
 TUNABLE_INT("debug.mpsafenet", &debug_mpsafenet);
