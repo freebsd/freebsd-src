3e36afbe27fd38e73ac1e02a5d19abd0c2d86c2d
Alan Cox <alc@FreeBSD.org>
2004-07-18 04:38:11 +0000

    Remove the GIANT_REQUIRED preceding pmap_remove() in vm_pageout_map_deactivate_pages().

--- sys/vm/vm_pageout.c
+++ sys/vm/vm_pageout.c
@@ -608,7 +608,6 @@ vm_pageout_map_deactivate_pages(map, desired)
 	 * table pages.
 	 */
 	if (desired == 0 && nothingwired) {
-		GIANT_REQUIRED;
 		pmap_remove(vm_map_pmap(map), vm_map_min(map),
 		    vm_map_max(map));
 	}
