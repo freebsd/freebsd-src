6a51470c5cba7b839b00ce5133ee3473b12be410
marcel <marcel@FreeBSD.org>
2004-07-18 19:29:38 +0000

    Correct the mess I made by committing from the wrong tree. Most notably, this restores some of the contents in thread_db.h as well as David Xu's copyright notice. This also fixes the includes in the MD libpthread files which Scott tried to provide a quick fix for.
    Pointy hat: marcel

--- lib/libthread_db/arch/sparc64/libpthread_md.c
+++ lib/libthread_db/arch/sparc64/libpthread_md.c
@@ -27,12 +27,8 @@
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include <string.h>
-#include <sys/types.h>
-#include <proc_service.h>
-#include <thread_db.h>
-
-#include "libpthread_db.h"
+#include <sys/procfs.h>
+#include <ucontext.h>
 
 void
 pt_reg_to_ucontext(const struct reg *r, ucontext_t *uc)
