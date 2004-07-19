25712d3327b691e058d85b2d56ac6b1bdc7d93f7
harti <harti@FreeBSD.org>
2004-07-19 12:54:00 +0000

    Fix a typo that could provoke a panic or access to random memory.
    PR:		kern/67012
    Submitted by:	Zhenmin <zli4@cs.uiuc.edu>

--- sys/netatm/uni/unisig_subr.c
+++ sys/netatm/uni/unisig_subr.c
@@ -1207,7 +1207,7 @@ unisig_set_attrs(usp, msg, ap)
 		bcopy(&ie_cgad_absent,
 				&msg->msg_ie_cgad->ie_u.ie_cgad,
 				sizeof(ie_cgad_absent));
-		msg->msg_ie_cgsa->ie_ident = UNI_IE_CGSA;
+		msg->msg_ie_cgad->ie_ident = UNI_IE_CGAD;
 		ATM_ADDR_COPY(&ap->calling.addr,
 				&msg->msg_ie_cgad->ie_cgad_addr);
 
