e850e9f20df773d694f8be70fe80f866b272fd49
ume <ume@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-07-21 17:26:40 +0000

    now e.f.f.3.ip6.arpa is delegated, we no longer need to query ip6.int
    Obtained from:	KAME
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@132508 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/net/name6.c
+++ lib/libc/net/name6.c
@@ -2082,7 +2082,7 @@ _dns_ghbyaddr(void *rval, void *cb_data, va_list ap)
 	querybuf *buf;
 	char qbuf[MAXDNAME+1];
 	char *hlist[2];
-	char *tld6[] = { "ip6.arpa", "ip6.int", NULL };
+	char *tld6[] = { "ip6.arpa", NULL };
 	char *tld4[] = { "in-addr.arpa", NULL };
 	char **tld;
 
