7ee771aa57b152c4f2fe6490dbfbbc947cdf4a6c
Tim J. Robbins <tjr@FreeBSD.org>
2004-07-27 01:33:27 +0000

    Use file2c instead of a combination of hexdump, sed and shell script to generate the wakecode[] array from acpi_wakecode.bin. The old method was not safe in multibyte locales.

--- sys/i386/acpica/genwakecode.sh
+++ sys/i386/acpica/genwakecode.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 # $FreeBSD$
 #
-echo 'static char wakecode[] = {';
-hexdump -Cv acpi_wakecode.bin | \
-    sed -e 's/^[0-9a-f][0-9a-f]*//' -e 's/\|.*$//' | \
-    while read line
-    do
-	for code in ${line}
-	do
-	    echo -n "0x${code},";
-	done
-    done
-echo '};'
+file2c 'static char wakecode[] = {' '};' <acpi_wakecode.bin
 
 nm -n acpi_wakecode.o | while read offset dummy what
 do
