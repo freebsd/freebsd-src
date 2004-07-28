52952ef9d17ea86143aa13cd385e665f7bc9aea4
kan <kan@FreeBSD.org>
2004-07-28 05:27:21 +0000

    Bmake glue for GCC 3.4.2-prerelease.

--- gnu/usr.bin/cc/cc_tools/freebsd-native.h
+++ gnu/usr.bin/cc/cc_tools/freebsd-native.h
@@ -14,8 +14,8 @@
 #undef LOCAL_INCLUDE_DIR		/* We don't wish to support one. */
 
 /* Look for the include files in the system-defined places.  */
-#define GPLUSPLUS_INCLUDE_DIR		PREFIX"/include/c++/3.3"
-#define	GPLUSPLUS_BACKWARD_INCLUDE_DIR	PREFIX"/include/c++/3.3/backward"
+#define GPLUSPLUS_INCLUDE_DIR		PREFIX"/include/c++/3.4"
+#define	GPLUSPLUS_BACKWARD_INCLUDE_DIR	PREFIX"/include/c++/3.4/backward"
 #define GCC_INCLUDE_DIR			PREFIX"/include"
 #ifdef CROSS_COMPILE
 #define CROSS_INCLUDE_DIR		PREFIX"/include"
@@ -31,20 +31,19 @@
    programs: /usr/libexec/<OBJFORMAT>/:STANDARD_EXEC_PREFIX:MD_EXEC_PREFIX
    libraries: MD_EXEC_PREFIX:MD_STARTFILE_PREFIX:STANDARD_STARTFILE_PREFIX
 */
-#undef	TOOLDIR_BASE_PREFIX		/* Old??  This is not documented. */
 #undef	STANDARD_BINDIR_PREFIX		/* We don't need one for now. */
 #define	STANDARD_EXEC_PREFIX		PREFIX"/libexec/"
+#define	STANDARD_LIBEXEC_PREFIX		PREFIX"/libexec/"
+#define TOOLDIR_BASE_PREFIX		PREFIX
 #undef	MD_EXEC_PREFIX			/* We don't want one. */
 #define	FBSD_DATA_PREFIX		PREFIX"/libdata/gcc/"
 
 /* Under FreeBSD, the normal location of the various *crt*.o files is the
    /usr/lib directory.  */
 
-#define STANDARD_STARTFILE_PREFIX	PREFIX"/lib/"
-#ifdef CROSS_COMPILE
-#define CROSS_STARTFILE_PREFIX		PREFIX"/lib/"
-#endif
 #undef  MD_STARTFILE_PREFIX		/* We don't need one for now. */
+#define STANDARD_STARTFILE_PREFIX	PREFIX"/lib/"
+#define STARTFILE_PREFIX_SPEC		PREFIX"/lib/"
 
 /* For the native system compiler, we actually build libgcc in a profiled
    version.  So we should use it with -pg.  */
@@ -56,14 +55,6 @@
 
 /* FreeBSD is 4.4BSD derived */
 #define bsd4_4
-
-/* Dike out [stupid, IMHO] libiberty functions.  */
-#define	xmalloc_set_program_name(dummy)
-#define	xmalloc		malloc
-#define	xcalloc		calloc
-#define	xrealloc	realloc
-#define	xstrdup		strdup
-#define	xstrerror	strerror
 
 /* And now they want to replace ctype.h.... grr... [stupid, IMHO] */
 #define xxxISDIGIT	isdigit
