d98bc4ce72bb0f874f5cb999c9f67f01e6cc860c
Hartmut Brandt <harti@FreeBSD.org>
2004-08-03 18:56:31 +0000

    Put variable assignments from the command line into the MAKEFLAGS variable as required by POSIX. This causes such variables to be pushed into all sub-makes called by the make (except when the MAKEFLAGS variable is explicitely changed in the sub-make's environment). This makes them also mostly un-overrideable in sub-makes except on the sub-make's command line. Therefor specifying 'make CC=icc' will cause icc to be used as C compiler in all sub-makes no matter what the Makefiles itself try to do to the CC variable.
    This patch also corrects the handling of the MFLAGS variable. MFLAGS
    contains all the command line flags but not the command line variable
    assignments. The evaluation of the .MFLAGS or .MAKEFLAGS target now
    changes both MFLAGS and MAKEFLAGS (they used to change MAKEFLAGS only).
    Makefiles can use MFLAGS for their own purposes given that they do not
    except MFLAGS to be undefined at the beginning and that they don't evaluate
    .MFLAGS or .MAKEFLAGS. MFLAGS should be removed for POSIX compliance,
    but it is unfortunately heavily used by the X makefiles.
    
    This has been extensively tested by port builds (thanks to portmgr), new
    worlds and kernels.
    
    PR:		standards/57295 (1st part above)
    Submitted by:	James E. Flemer <jflemer@alum.rpi.edu>
    Approved by:	portmgr
    Obtained from:	NetBSD (1st part above)
    MFC after:	4 weeks

--- usr.bin/make/var.c
+++ usr.bin/make/var.c
@@ -832,6 +832,44 @@ VarREError(int err, regex_t *pat, const char *str)
 }
 
 
+#ifdef POSIX
+
+
+/* In POSIX mode, variable assignments passed on the command line are
+ * propagated to sub makes through MAKEFLAGS.
+ */
+void
+Var_AddCmdLine(char *name)
+{
+    const Var *v;
+    LstNode ln;
+    Buffer buf;
+    static const char quotable[] = " \t\n\\'\"";
+    char *s;
+    int first = 1;
+
+    buf = Buf_Init (MAKE_BSIZE);
+
+    for (ln = Lst_First(VAR_CMD->context); ln != NULL;
+	ln = Lst_Succ(ln)) {
+	    if (!first)
+	    	Buf_AddByte(buf, ' ');
+	    first = 0;
+	    /* We assume variable names don't need quoting */
+	    v = (Var *)Lst_Datum(ln);
+	    Buf_AddBytes(buf, strlen(v->name), v->name);
+	    Buf_AddByte(buf, '=');
+	    for (s = Buf_GetAll(v->val, (int *)NULL); *s != '\0'; s++) {
+		if (strchr(quotable, *s))
+		    Buf_AddByte(buf, '\\');
+		Buf_AddByte(buf, *s);
+	    }
+    }
+    Var_Append(name, Buf_GetAll(buf, (int *)NULL), VAR_GLOBAL);
+    Buf_Destroy(buf, 1);
+}
+#endif
+
 /*-
  *-----------------------------------------------------------------------
  * Var_Parse --
