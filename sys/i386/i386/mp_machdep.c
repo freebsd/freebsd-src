af19defc01a280e78dee1d1d699ea4579bead501
rwatson <rwatson@FreeBSD.org>
2004-08-05 00:32:08 +0000

    Move definition of mem_range_softc from mp_machdep.c to machdep.c so that it is defined for non-SMP builds, not just SMP ones.

--- sys/i386/i386/mp_machdep.c
+++ sys/i386/i386/mp_machdep.c
@@ -212,8 +212,6 @@ static int	hlt_cpus_mask;
 static int	hlt_logical_cpus;
 static struct	sysctl_ctx_list logical_cpu_clist;
 
-struct mem_range_softc mem_range_softc;
-
 static void
 mem_range_AP_init(void)
 {
