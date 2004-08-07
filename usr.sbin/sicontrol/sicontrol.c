f483c5f1c4b65c8de0337836a4d494685d521523
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-08-07 04:28:56 +0000

    Per letter dated July 22, 1999 remove 3rd clause of Berkeley derived software (with permission of addtional copyright holders where appropriate)
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@133249 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/sicontrol/sicontrol.c
+++ usr.sbin/sicontrol/sicontrol.c
@@ -16,11 +16,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notices, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Andy Rutter of
- *	Advanced Methods and Tools Ltd. based on original information
- *	from Specialix International.
  * 4. Neither the name of Advanced Methods and Tools, nor Specialix
  *    International may be used to endorse or promote products derived from
  *    this software without specific prior written permission.
