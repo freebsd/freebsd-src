ba0b5c1c3b0eec246be664ba0e782fa25133d6fd
Søren Schmidt <sos@FreeBSD.org>
2004-08-07 12:49:28 +0000

    Correct the last commit so it works in error situations as well.

--- sys/dev/ata/ata-lowlevel.c
+++ sys/dev/ata/ata-lowlevel.c
@@ -295,7 +295,7 @@ ata_generic_transaction(struct ata_request *request)
     }
 
     /* request finish here */
-    if (request->flags & ATA_R_DMA && ch->dma->flags & ATA_DMA_LOADED)
+    if (ch->dma && ch->dma->flags & ATA_DMA_LOADED)
 	ch->dma->unload(ch);
     return ATA_OP_FINISHED;
 }
