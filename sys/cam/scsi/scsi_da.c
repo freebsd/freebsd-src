92c7375fb6f1409a0ca8af211c2f0645ccb1657b
MIHIRA Sanpei Yoshiro <sanpei@FreeBSD.org>
2004-08-08 09:08:37 +0000

    Add support iRiver iFP MP3 player
    PR:		kern/54881, i386/63941, kern/66124
    Submitted by:	Magnus <bsdhead.at.spray.dot.se@FreeBSD.org>,
    		Dmitry Dyomin <old@old.com.ua>,
    		Dmitry Sivachenko <mitya@demos.su>
    MFC after:	1 week

--- sys/cam/scsi/scsi_da.c
+++ sys/cam/scsi/scsi_da.c
@@ -302,6 +302,14 @@ static struct da_quirk_entry da_quirk_table[] =
 		{T_DIRECT, SIP_MEDIA_REMOVABLE, "SEAGRAND", "NP-900*", "*"},
 		/*quirks*/ DA_Q_NO_SYNC_CACHE|DA_Q_NO_PREVENT
 	},
+	{
+		/*
+		 * iRiver iFP MP3 player (with UMS Firmware)
+		 * PR: kern/54881, i386/63941, kern/66124
+		 */
+		{T_DIRECT, SIP_MEDIA_REMOVABLE, "iRiver", "iFP*", "*"},
+		/*quirks*/ DA_Q_NO_SYNC_CACHE
+ 	}
 };
 
 static	disk_strategy_t	dastrategy;
