d29ded002bcddf2cd1977bb8d7ccb65487590ee8
davidxu <davidxu@FreeBSD.org>
2004-08-08 22:37:53 +0000

    1.Use new way to check if a thread is in critical region, defer suspending   if it is true. 2.Add thread_db api td_thr_tls_get_addr to get tls address, the real code   is commented out util tls patch is committed.
    Reviewed by: deischen

--- lib/libthread_db/thread_db.h
+++ lib/libthread_db/thread_db.h
@@ -231,6 +231,7 @@ td_err_e td_thr_set_event(const td_thrhandle_t *, td_thr_events_t *);
 td_err_e td_thr_setfpregs(const td_thrhandle_t *, const prfpregset_t *);
 td_err_e td_thr_setgregs(const td_thrhandle_t *, const prgregset_t);
 td_err_e td_thr_validate(const td_thrhandle_t *);
+td_err_e td_thr_tls_get_addr(const td_thrhandle_t *, void *, size_t, void **);
 
 /* FreeBSD specific extensions. */
 td_err_e td_thr_sstep(const td_thrhandle_t *, int);
