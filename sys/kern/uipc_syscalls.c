939b44f0dedeac6f1c74a648d01b2cbf062df7d1
Hajimu UMEMOTO <ume@FreeBSD.org>
2004-08-13 14:51:45 +0000

    MFC 1.187: allow more than MLEN bytes for ancillary data to meet the requirement of Section 20.1 of RFC3542.

--- sys/kern/uipc_syscalls.c
+++ sys/kern/uipc_syscalls.c
@@ -1350,11 +1350,14 @@ sockargs(mp, buf, buflen, type)
 			buflen = MLEN;		/* unix domain compat. hack */
 		else
 #endif
-		return (EINVAL);
+			if ((u_int)buflen > MCLBYTES)
+				return (EINVAL);
 	}
 	m = m_get(M_WAIT, type);
 	if (m == NULL)
 		return (ENOBUFS);
+	if ((u_int)buflen > MLEN)
+		MCLGET(m, M_WAIT);
 	m->m_len = buflen;
 	error = copyin(buf, mtod(m, caddr_t), (u_int)buflen);
 	if (error)
