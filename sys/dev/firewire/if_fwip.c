a1755216cf83cade19520a121e47d82997ea73b2
Robert Watson <rwatson@FreeBSD.org>
2004-08-13 23:09:41 +0000

    Since if_fwip doesn't contain locking or run with INTR_MPSAFE, mark the interface as IFF_NEEDSGIANT so if_start is run holding Giant.
    Approved by:	dfr

--- sys/dev/firewire/if_fwip.c
+++ sys/dev/firewire/if_fwip.c
@@ -209,7 +209,8 @@ fwip_attach(device_t dev)
 	ifp->if_init = fwip_init;
 	ifp->if_start = fwip_start;
 	ifp->if_ioctl = fwip_ioctl;
-	ifp->if_flags = (IFF_BROADCAST|IFF_SIMPLEX|IFF_MULTICAST);
+	ifp->if_flags = (IFF_BROADCAST|IFF_SIMPLEX|IFF_MULTICAST|
+	    IFF_NEEDSGIANT);
 	ifp->if_snd.ifq_maxlen = TX_MAX_QUEUE;
 
 	s = splimp();
