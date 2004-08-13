47a1257e2073f8024bb032443f3e7336e3dd1187
Roman Kurakin <rik@FreeBSD.org>
2004-08-13 12:35:52 +0000

    Fix resource check while autodetection.

--- sys/dev/ctau/if_ct.c
+++ sys/dev/ctau/if_ct.c
@@ -340,7 +340,7 @@ static void ct_identify (driver_t *driver, device_t dev)
 		 * devices, but we don't have a choise
 		 */
 		for (i = 0; (iobase = porttab [i]) != 0; i++) {
-			if (!ct_is_free_res (dev, 1, SYS_RES_IOPORT,
+			if (!ct_is_free_res (dev, 0, SYS_RES_IOPORT,
 			    iobase, iobase + NPORT, NPORT))
 				continue;
 			if (ct_probe_board (iobase, -1, -1) == 0)
@@ -377,7 +377,7 @@ static void ct_identify (driver_t *driver, device_t dev)
 				if (porttab [i] != iobase)
 					continue;
 					
-				if (!ct_is_free_res (devices[k], 1, SYS_RES_IOPORT,
+				if (!ct_is_free_res (devices[k], 0, SYS_RES_IOPORT,
 				    iobase, iobase + NPORT, NPORT))
 					continue;
 
@@ -404,7 +404,7 @@ static void ct_identify (driver_t *driver, device_t dev)
 			for (i = 0; (iobase = porttab [i]) != 0; i++) {
 				if (porttab [i] == -1)
 					continue;
-				if (!ct_is_free_res (devices[k], 1, SYS_RES_IOPORT,
+				if (!ct_is_free_res (devices[k], 0, SYS_RES_IOPORT,
 				    iobase, iobase + NPORT, NPORT))
 					continue;
 				if (ct_probe_board (iobase, -1, -1) == 0)
@@ -443,7 +443,7 @@ static int ct_probe (device_t dev)
 		return ENXIO;
 	}
 
-	if (!ct_is_free_res (dev, 1, SYS_RES_IOPORT,
+	if (!ct_is_free_res (dev, 0, SYS_RES_IOPORT,
 	    iobase, iobase + NPORT, NPORT)) {
 		printf ("ct%d: Resource IOPORT isn't free\n", unit);
 		return ENXIO;
@@ -566,7 +566,7 @@ static int ct_attach (device_t dev)
 	
 	if (bus_get_resource (dev, SYS_RES_DRQ, 0, &drq, &rescount) != 0) {
 		for (i = 0; (drq = dmatab [i]) != 0; i++) {
-			if (!ct_is_free_res (dev, 1, SYS_RES_DRQ,
+			if (!ct_is_free_res (dev, 0, SYS_RES_DRQ,
 			    drq, drq + 1, 1))
 				continue;
 			bus_set_resource (dev, SYS_RES_DRQ, 0, drq, 1);
@@ -593,7 +593,7 @@ static int ct_attach (device_t dev)
 	
 	if (bus_get_resource (dev, SYS_RES_IRQ, 0, &irq, &rescount) != 0) {
 		for (i = 0; (irq = irqtab [i]) != 0; i++) {
-			if (!ct_is_free_res (dev, 1, SYS_RES_IRQ,
+			if (!ct_is_free_res (dev, 0, SYS_RES_IRQ,
 			    irq, irq + 1, 1))
 				continue;
 			bus_set_resource (dev, SYS_RES_IRQ, 0, irq, 1);
