1871435a01ae850271367e795a906a7160fc8a92
nectar <nectar@FreeBSD.org>
2004-08-26 19:37:06 +0000

    Correct a denial-of-service vulnerability in zlib. For reference, this bug was first reported at http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=252253
    Submitted by:	"Dmitry V. Levin" <ldv@altlinux.org>

--- lib/libz/inflate.c
+++ lib/libz/inflate.c
@@ -864,6 +864,9 @@ int flush;
                 }
             }
 
+            if (state->mode == BAD)
+                break;
+
             /* build code tables */
             state->next = state->codes;
             state->lencode = (code const FAR *)(state->next);
