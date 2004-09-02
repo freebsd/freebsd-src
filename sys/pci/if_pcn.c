f15b0466fa05c51b090c6e8955507aff858d6ceb
Robert Watson <rwatson@FreeBSD.org>
2004-09-02 20:57:40 +0000

    Merge if_pcn.c:1.60, if_sf.c:1.73, if_ti.c:1.97, if_tl.c:1.94, if_wb.c:1.74 to RELENG_5:
      Tag a last set of PCI network interfaces as IFF_NEEDSGIANT until they
      are either locked down or demonstrated MPSAFE.
    
    Approved by:	re (scottl)

--- sys/pci/if_pcn.c
+++ sys/pci/if_pcn.c
@@ -552,7 +552,8 @@ pcn_attach(dev)
 	ifp->if_softc = sc;
 	if_initname(ifp, device_get_name(dev), device_get_unit(dev));
 	ifp->if_mtu = ETHERMTU;
-	ifp->if_flags = IFF_BROADCAST | IFF_SIMPLEX | IFF_MULTICAST;
+	ifp->if_flags = IFF_BROADCAST | IFF_SIMPLEX | IFF_MULTICAST |
+	    IFF_NEEDSGIANT;
 	ifp->if_ioctl = pcn_ioctl;
 	ifp->if_start = pcn_start;
 	ifp->if_watchdog = pcn_watchdog;
