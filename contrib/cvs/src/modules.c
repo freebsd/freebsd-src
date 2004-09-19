c92cfee45d3e94c86d2845e71315ea96512dd1d2
Jacques Vidrine <nectar@FreeBSD.org>
2004-09-19 22:37:11 +0000

    Correct several vulnerabilities in CVS 1.11.5 (CAN-2004-0414, CAN-2004-0416, CAN-2004-0417, CAN-2004-0418, CAN-2004-0778 and others).
    Approved by:	so

--- contrib/cvs/src/modules.c
+++ contrib/cvs/src/modules.c
@@ -178,6 +178,24 @@ do_module (db, mname, m_type, msg, callback_proc, where, shorten,
 		mname);
 
 
+    /* Don't process absolute directories.  Anything else could be a security
+     * problem.  Before this check was put in place:
+     *
+     *   $ cvs -d:fork:/cvsroot co /foo
+     *   cvs server: warning: cannot make directory CVS in /: Permission denied
+     *   cvs [server aborted]: cannot make directory /foo: Permission denied
+     *   $
+     */
+    if (isabsolute (mname))
+	error (1, 0, "Absolute module reference invalid: `%s'", mname);
+
+    /* Similarly for directories that attempt to step above the root of the
+     * repository.
+     */
+    if (pathname_levels (mname) > 0)
+	error (1, 0, "up-level in module reference (`..') invalid: `%s'.",
+		mname);
+
     /* if this is a directory to ignore, add it to that list */
     if (mname[0] == '!' && mname[1] != '\0')
     {
