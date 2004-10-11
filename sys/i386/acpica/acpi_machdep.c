7f35f90eae300092b6cd668fab87f4db1f986a56
Nate Lawson <njl@FreeBSD.org>
2004-10-11 05:42:12 +0000

    Match surrounding style, not style(msmith).

--- sys/i386/acpica/acpi_machdep.c
+++ sys/i386/acpica/acpi_machdep.c
@@ -333,22 +333,22 @@ acpi_SetDefaultIntrModel(int model)
 int
 acpi_machdep_quirks(int *quirks)
 {
-    char *va;
-    int year;
+	char *va;
+	int year;
 
-    /* BIOS address 0xffff5 contains the date in the format mm/dd/yy. */
-    va = pmap_mapdev(0xffff0, 16);
-    sscanf(va + 11, "%2d", &year);
-    pmap_unmapdev((vm_offset_t)va, 16);
+	/* BIOS address 0xffff5 contains the date in the format mm/dd/yy. */
+	va = pmap_mapdev(0xffff0, 16);
+	sscanf(va + 11, "%2d", &year);
+	pmap_unmapdev((vm_offset_t)va, 16);
 
-    /* 
-     * Date must be >= 1/1/1999 or we don't trust ACPI.  Note that this
-     * check must be changed by my 114th birthday.
-     */
-    if (year > 90 && year < 99)
-	*quirks = ACPI_Q_BROKEN;
+	/* 
+	 * Date must be >= 1/1/1999 or we don't trust ACPI.  Note that this
+	 * check must be changed by my 114th birthday.
+	 */
+	if (year > 90 && year < 99)
+		*quirks = ACPI_Q_BROKEN;
 
-    return (0);
+	return (0);
 }
 
 void
