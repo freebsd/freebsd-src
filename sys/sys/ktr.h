6a5cd72bf320b74d96d8403fec2133c1ce467294
rwatson <rwatson@FreeBSD.org>
2004-10-21 18:35:24 +0000

    Add KTR_GEOM, which allows tracing of basic GEOM I/O events occuring in the g_up and g_down threads.  Each time a bio is propelled up and down the stack, an event is generating showing the provider, offset, and length, as well as thread wakeup and work status information.

--- sys/sys/ktr.h
+++ sys/sys/ktr.h
@@ -74,7 +74,8 @@
 #define	KTR_CONTENTION	0x00800000		/* Lock contention */
 #define	KTR_UMA		0x01000000		/* UMA slab allocator */
 #define	KTR_CALLOUT	0x02000000		/* Callouts and timeouts */
-#define	KTR_ALL		0x02ffffff
+#define	KTR_GEOM	0x04000000		/* GEOM I/O events */
+#define	KTR_ALL		0x04ffffff
 
 /*
  * Trace classes which can be assigned to particular use at compile time
