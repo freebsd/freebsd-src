b44588a75924918b6922d72aab537925a0783f6b
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-10-21 12:24:38 +0000

    Add new function ttyinitmode() which sets our systemwide default modes on a tty structure.  Both the ".init" and the current settings are initialized allowing the function to be used both at attach and open time.
    The function takes an argument to decide if echoing should be enabled
    by default.  Echoing should not be enabled for regular physical
    serial ports unless they are consoles, in which case they should
    be configured by ttyconsolemode() instead.
    
    Use the new function throughout.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@136745 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ sys/sys/bufobj.h
@@ -0,0 +1,74 @@
+/*-
+ * Copyright (c) 2004 Poul-Henning Kamp
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+/*
+ * Architectural notes:
+ *
+ * bufobj is a new object which is what buffers hang from in the buffer
+ * cache.
+ *
+ * This used to be vnodes, but we need non-vnode code to be able
+ * to use the buffer cache as well, specifically geom classes like gbde,
+ * raid3 and raid5.
+ *
+ * All vnodes will contain a bufobj initially, but down the road we may
+ * want to only allocate bufobjs when they are needed.  There could be a
+ * large number of vnodes in the system which wouldn't need a bufobj during
+ * their lifetime.
+ *
+ * The exact relationship to the vmobject is not determined at this point,
+ * it may in fact bee that we find them to be two sides of the same object 
+ * once things starts to crystalize.
+ */
+
+#ifndef _SYS_BUFOBJ_H_
+#define _SYS_BUFOBJ_H_
+
+#if defined(_KERNEL) || defined(_KVM_VNODE)
+
+#include <sys/queue.h>
+
+TAILQ_HEAD(buflists, buf);
+
+/* A Buffer splay list */
+struct bufv {
+	struct buflists	bv_hd;		/* Sorted blocklist */
+	struct buf	*bv_root;	/* Buf splay tree */
+	int		bv_cnt;		/* Number of buffers */
+};
+
+struct bufobj {
+	struct mtx	*bo_mtx;	/* Mutex which protects "i" things */
+	struct bufv	bo_clean;	/* i Clean buffers */
+	struct bufv	bo_dirty;	/* i Dirty buffers */
+};
+
+#endif /* defined(_KERNEL) || defined(_KVM_VNODE) */
+#endif /* _SYS_BUFOBJ_H_ */
+
+
