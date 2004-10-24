849321f886fb9dc44706a4db424c634191c633e6
nyan <nyan@FreeBSD.org>
2004-10-24 12:14:05 +0000

    MFi386: Add support for preloading ELF relocatable object format modules.

--- sys/boot/pc98/loader/conf.c
+++ sys/boot/pc98/loader/conf.c
@@ -83,9 +83,11 @@ struct fs_ops *file_system[] = {
  * rather than reading the file go first.
  */
 extern struct file_format	i386_elf;
+extern struct file_format	i386_elf_obj;
 
 struct file_format *file_formats[] = {
     &i386_elf,
+    &i386_elf_obj,
     NULL
 };
 
