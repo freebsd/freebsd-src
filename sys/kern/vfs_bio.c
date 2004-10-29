1f32bcf746ed16541b6a9c4a47fff51c9c28536b
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-10-29 10:52:31 +0000

    Remove the last call in the system to VOP_SPECSTRATEGY():  We can no longer come through the VNODE layer to the disks since all the filesystems now go via geom_vfs to GEOM.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@137042 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -3808,15 +3808,10 @@ bufstrategy(struct bufobj *bo, struct buf *bp)
 	struct vnode *vp;
 
 	vp = bp->b_vp;
-#if 0
-	KASSERT(vp == bo->bo_vnode, ("Inconsistent vnode bufstrategy"));
+	KASSERT(vp == bo->bo_private, ("Inconsistent vnode bufstrategy"));
 	KASSERT(vp->v_type != VCHR && vp->v_type != VBLK,
 	    ("Wrong vnode in bufstrategy(bp=%p, vp=%p)", bp, vp));
-#endif
-	if (vp->v_type == VCHR)
-		i = VOP_SPECSTRATEGY(vp, bp);
-	else
-		i = VOP_STRATEGY(vp, bp);
+	i = VOP_STRATEGY(vp, bp);
 	KASSERT(i == 0, ("VOP_STRATEGY failed bp=%p vp=%p", bp, bp->b_vp));
 }
 
