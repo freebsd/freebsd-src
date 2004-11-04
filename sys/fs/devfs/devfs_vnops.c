b0f45918730327ec6e1b09f1ecf2f0fb34843a4c
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-11-04 09:17:55 +0000

    Add back securelevel check for disks.
    XXX: This should live in geom_dev.c but we don't have access to the
    cred there.
    XXX: XXX:  This may not matter anymore since filesystems use geom_vfs.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@137195 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/fs/devfs/devfs_vnops.c
+++ sys/fs/devfs/devfs_vnops.c
@@ -710,6 +710,18 @@ devfs_open(ap)
 	if (dev->si_iosize_max == 0)
 		dev->si_iosize_max = DFLTPHYS;
 
+	if (vn_isdisk(vp, NULL) &&
+	    ap->a_cred != FSCRED && (ap->a_mode & FWRITE)) {
+		/*
+		* When running in very secure mode, do not allow
+		* opens for writing of any disks.
+		* XXX: should be in geom_dev.c, but we lack the cred there.
+		*/
+		error = securelevel_ge(td->td_ucred, 2);
+		if (error)
+			return (error);
+	}
+
 	dsw = dev_refthread(dev);
 	if (dsw == NULL)
 		return (ENXIO);
