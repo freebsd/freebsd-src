a9af44da51af290f83198e4c6d910ca3bcbc67f6
phk <phk@FreeBSD.org>
2004-11-26 19:28:39 +0000

    Eliminate MNT_NODEV usage, it doesn't have any meaning any more.
    Keep a #define MNT_NODEV 0 around to avoid dealing with contrib
    userland like mount_smbfs.

--- sys/sys/mount.h
+++ sys/sys/mount.h
@@ -184,7 +184,7 @@ struct vnode *__mnt_vnode_next(struct vnode **nvp, struct mount *mp);
 #define	MNT_SYNCHRONOUS	0x00000002	/* filesystem written synchronously */
 #define	MNT_NOEXEC	0x00000004	/* can't exec from filesystem */
 #define	MNT_NOSUID	0x00000008	/* don't honor setuid bits on fs */
-#define	MNT_NODEV	0x00000010	/* don't interpret special files */
+#define	MNT_NODEV	0		/* Deprecated option */
 #define	MNT_UNION	0x00000020	/* union with underlying filesystem */
 #define	MNT_ASYNC	0x00000040	/* filesystem written asynchronously */
 #define	MNT_SUIDDIR	0x00100000	/* special handling of SUID on dirs */
@@ -224,7 +224,7 @@ struct vnode *__mnt_vnode_next(struct vnode **nvp, struct mount *mp);
  * but the 'mount' program may need changing to handle this.
  */
 #define	MNT_VISFLAGMASK	(MNT_RDONLY	| MNT_SYNCHRONOUS | MNT_NOEXEC	| \
-			MNT_NOSUID	| MNT_NODEV	| MNT_UNION	| \
+			MNT_NOSUID	| MNT_UNION	| \
 			MNT_ASYNC	| MNT_EXRDONLY	| MNT_EXPORTED	| \
 			MNT_DEFEXPORTED	| MNT_EXPORTANON| MNT_EXKERB	| \
 			MNT_LOCAL	| MNT_USER	| MNT_QUOTA	| \
@@ -234,7 +234,7 @@ struct vnode *__mnt_vnode_next(struct vnode **nvp, struct mount *mp);
 			MNT_JAILDEVFS	| MNT_MULTILABEL | MNT_ACLS)
 
 /* Mask of flags that can be updated. */
-#define	MNT_UPDATEMASK (MNT_NOSUID	| MNT_NOEXEC	| MNT_NODEV	| \
+#define	MNT_UPDATEMASK (MNT_NOSUID	| MNT_NOEXEC	| \
 			MNT_SYNCHRONOUS	| MNT_UNION	| MNT_ASYNC	| \
 			MNT_NOATIME | \
 			MNT_NOSYMFOLLOW	| MNT_IGNORE	| MNT_JAILDEVFS	| \
