b7d2ffcc40f55d75cd042248aed225bdc617dfa2
marcel <marcel@FreeBSD.org>
2004-11-28 04:34:17 +0000

    Whitespace fixes: o  Remove a bogus comment that relates to alpha. o  s/u_int64_t/uint64_t/g o  Add bi_spare2 to make the internal padding explicit. o  Move BOOTINFO_MAGIC after the field it applies to.

--- sys/ia64/include/bootinfo.h
+++ sys/ia64/include/bootinfo.h
@@ -26,40 +26,25 @@
  * rights to redistribute these changes.
  */
 
-/*
- * The boot program passes a pointer (in the boot environment virtual
- * address address space; "BEVA") to a bootinfo to the kernel using
- * the following convention:
- *
- *	a0 contains first free page frame number
- *	a1 contains page number of current level 1 page table
- *	if a2 contains BOOTINFO_MAGIC and a4 is nonzero:
- *		a3 contains pointer (BEVA) to bootinfo
- *		a4 contains bootinfo version number
- *	if a2 contains BOOTINFO_MAGIC and a4 contains 0 (backward compat):
- *		a3 contains pointer (BEVA) to bootinfo version
- *		    (u_long), then the bootinfo
- */
-
-#define	BOOTINFO_MAGIC			0xdeadbeeffeedface
-
 struct bootinfo {
-	u_int64_t	bi_magic;		/* BOOTINFO_MAGIC */
-	u_int64_t	bi_version;		/* version 1 */
-	u_int64_t	bi_spare[6];		/* was: name of booted kernel */
-	u_int64_t	bi_hcdp;		/* DIG64 HCDP table */
-	u_int64_t	bi_fpswa;		/* FPSWA interface */
-	u_int64_t	bi_boothowto;		/* value for boothowto */
-	u_int64_t	bi_systab;		/* pa of EFI system table */
-	u_int64_t	bi_memmap;		/* pa of EFI memory map */
-	u_int64_t	bi_memmap_size;		/* size of EFI memory map */
-	u_int64_t	bi_memdesc_size;	/* sizeof EFI memory desc */
-	u_int32_t	bi_memdesc_version;	/* EFI memory desc version */
-	u_int64_t	bi_symtab;		/* start of kernel sym table */
-	u_int64_t	bi_esymtab;		/* end of kernel sym table */
-	u_int64_t	bi_kernend;		/* end of kernel space */
-	u_int64_t	bi_envp;		/* environment */
-	u_int64_t	bi_modulep;		/* preloaded modules */
+	uint64_t	bi_magic;		/* BOOTINFO_MAGIC */
+#define	BOOTINFO_MAGIC		0xdeadbeeffeedface
+	uint64_t	bi_version;		/* version 1 */
+	uint64_t	bi_spare[6];		/* was: name of booted kernel */
+	uint64_t	bi_hcdp;		/* DIG64 HCDP table */
+	uint64_t	bi_fpswa;		/* FPSWA interface */
+	uint64_t	bi_boothowto;		/* value for boothowto */
+	uint64_t	bi_systab;		/* pa of EFI system table */
+	uint64_t	bi_memmap;		/* pa of EFI memory map */
+	uint64_t	bi_memmap_size;		/* size of EFI memory map */
+	uint64_t	bi_memdesc_size;	/* sizeof EFI memory desc */
+	uint32_t	bi_memdesc_version;	/* EFI memory desc version */
+	uint32_t	bi_spare2;
+	uint64_t	bi_symtab;		/* start of kernel sym table */
+	uint64_t	bi_esymtab;		/* end of kernel sym table */
+	uint64_t	bi_kernend;		/* end of kernel space */
+	uint64_t	bi_envp;		/* environment */
+	uint64_t	bi_modulep;		/* preloaded modules */
 };
 
 extern struct bootinfo bootinfo;
