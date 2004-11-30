798dec9dfb6cac081955cd4e6ff44c3d4ab19194
Ruslan Ermilov <ru@FreeBSD.org>
2004-11-30 19:36:40 +0000

    Revert last change.  It doesn't break mount(8) reporting but allows a "nodev" in /etc/fstab, etc.

--- sbin/mount/mntopts.h
+++ sbin/mount/mntopts.h
@@ -40,6 +40,7 @@ struct mntopt {
 /* User-visible MNT_ flags. */
 #define MOPT_ASYNC		{ "async",	0, MNT_ASYNC, 0 }
 #define MOPT_NOATIME		{ "atime",	1, MNT_NOATIME, 0 }
+#define MOPT_NODEV		{ "dev",	1, MNT_NODEV, 0 }
 #define MOPT_NOEXEC		{ "exec",	1, MNT_NOEXEC, 0 }
 #define MOPT_NOSUID		{ "suid",	1, MNT_NOSUID, 0 }
 #define MOPT_NOSYMFOLLOW	{ "symfollow",  1, MNT_NOSYMFOLLOW, 0 }
@@ -75,6 +76,7 @@ struct mntopt {
 	MOPT_GROUPQUOTA,						\
 	MOPT_FSTAB_COMPAT,						\
 	MOPT_NOATIME,							\
+	MOPT_NODEV,							\
 	MOPT_NOEXEC,							\
 	MOPT_SUIDDIR,		/* must be before MOPT_NOSUID */	\
 	MOPT_NOSUID,							\
