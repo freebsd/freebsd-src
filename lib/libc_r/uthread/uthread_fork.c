00c948e4cebe608e0a1af60758ffda6ef1047090
Greg Lehey <grog@FreeBSD.org>
2004-12-10 03:36:45 +0000

    Add pthread_atfork().
    PR:  		bin/68841
    Submitted by:	Dan Nelson <dnelson@allantgroup.com>

--- lib/libc_r/uthread/uthread_fork.c
+++ lib/libc_r/uthread/uthread_fork.c
@@ -48,6 +48,7 @@ pid_t
 _fork(void)
 {
 	struct pthread	*curthread = _get_curthread();
+	struct pthread_atfork *af;
 	int             i, flags, use_deadlist = 0;
 	pid_t           ret;
 	pthread_t	pthread;
@@ -59,9 +60,23 @@ _fork(void)
 	 */
 	_thread_kern_sig_defer();
 
+	_pthread_mutex_lock(&_atfork_mutex);
+
+	/* Run down atfork prepare handlers. */
+	TAILQ_FOREACH_REVERSE(af, &_atfork_list, atfork_head, qe) {
+		if (af->prepare != NULL)
+			af->prepare();
+	}
+
 	/* Fork a new process: */
 	if ((ret = __sys_fork()) != 0) {
-		/* Parent process or error. Nothing to do here. */
+		/* Run down atfork parent handlers. */
+		TAILQ_FOREACH(af, &_atfork_list, qe) {
+			if (af->parent != NULL)
+			af->parent();
+		}
+		_pthread_mutex_unlock(&_atfork_mutex);
+
 	} else {
 		/* Close the pthread kernel pipe: */
 		__sys_close(_thread_kern_pipe[0]);
@@ -207,7 +222,14 @@ _fork(void)
 				}
 			}
 		}
+		/* Run down atfork child handlers. */
+		TAILQ_FOREACH(af, &_atfork_list, qe) {
+			if (af->child != NULL)
+				af->child();
+		}
+		_mutex_reinit(&_atfork_mutex);
 	}
+
 
 	/*
 	 * Undefer and handle pending signals, yielding if necessary:
