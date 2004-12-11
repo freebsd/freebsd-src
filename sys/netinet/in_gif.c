09ed67647322feff4729f392a40b72d151aaa06f
Gleb Smirnoff <glebius@FreeBSD.org>
2004-12-11 20:39:12 +0000

    MFC:   Make route caching optional and disabled by default.
    Approved by:	re (kensmith)

--- sys/netinet/in_gif.c
+++ sys/netinet/in_gif.c
@@ -205,7 +205,14 @@ in_gif_output(ifp, family, m)
 	}
 
 	error = ip_output(m, NULL, &sc->gif_ro, 0, NULL, NULL);
-	return(error);
+
+	if (!(sc->gif_if.if_flags & IFF_LINK0) &&
+	    sc->gif_ro.ro_rt != NULL) {
+		RTFREE(sc->gif_ro.ro_rt);
+		sc->gif_ro.ro_rt = NULL;
+	}
+
+	return (error);
 }
 
 void
