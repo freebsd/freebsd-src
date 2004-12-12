e84f6642f1235284c0e9693b5834f9e87c3148d9
njl <njl@FreeBSD.org>
2004-12-12 05:53:57 +0000

    Move the author's copyright notice to match the initial LongRun import now that we have split out this support into longrun.c

--- sys/i386/i386/longrun.c
+++ sys/i386/i386/longrun.c
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2001 Tamotsu Hattori.
+ * Copyright (c) 2001 Mitsuru IWASAKI.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
