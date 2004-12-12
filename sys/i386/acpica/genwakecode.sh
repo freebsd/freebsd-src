75a242a3b7798ab338357f3e1a482337d3daa916
njl <njl@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-12-12 06:59:14 +0000

    Only export defined symbols.  Note that I couldn't find any difference between object code generated without the flag but it makes sense and might make a difference in the future.
    PR:		kern/53008
    Submitted by:	Jens Rehsack rehsack at liwing de
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@138725 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/acpica/genwakecode.sh
+++ sys/i386/acpica/genwakecode.sh
@@ -3,7 +3,7 @@
 #
 file2c 'static char wakecode[] = {' '};' <acpi_wakecode.bin
 
-nm -n acpi_wakecode.o | while read offset dummy what
+nm -n --defined-only acpi_wakecode.o | while read offset dummy what
 do
     echo "#define ${what}	0x${offset}"
 done
