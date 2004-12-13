7de2b6b27391c052e167a0237a822ce612298322
Marius Strobl <marius@FreeBSD.org>
2004-12-13 00:32:48 +0000

    MFC: Remove GNU patch(1) sources unused since FreeBSD moved back to the patch(1) sources in src/gnu/usr.bin/patch nearly seven years ago.

--- contrib/patch/quotearg.h
+++
@@ -1,9 +0,0 @@
-/* quote.h -- declarations for quoting system arguments */
-
-#if defined __STDC__ || __GNUC__
-# define __QUOTEARG_P(args) args
-#else
-# define __QUOTEARG_P(args) ()
-#endif
-
-size_t quote_system_arg __QUOTEARG_P ((char *, char const *));
