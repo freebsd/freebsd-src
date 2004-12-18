50586e8b6be3a9706d420fb99ba3c8337e979781
David Xu <davidxu@FreeBSD.org>
2004-12-18 12:52:44 +0000

    1. make umtx sharable between processes, the way is two or more processes    call mmap() to create a shared space, and then initialize umtx on it,    after that, each thread in different processes can use the umtx same    as threads in same process. 2. introduce a new syscall _umtx_op to support timed lock and condition    variable semantics. also, orignal umtx_lock and umtx_unlock inline    functions now are reimplemented by using _umtx_op, the _umtx_op can    use arbitrary id not just a thread id.

--- sys/sys/umtx.h
+++ sys/sys/umtx.h
@@ -40,13 +40,23 @@ struct umtx {
 	void	*u_owner;	/* Owner of the mutex. */
 };
 
+/* op code for _umtx_op */
+#define UMTX_OP_LOCK		0
+#define UMTX_OP_UNLOCK		1
+#define UMTX_OP_UNLOCK_AND_WAIT	2
+#define UMTX_OP_WAKE		3
+
 #ifndef _KERNEL
 
 /*
  * System calls for acquiring and releasing contested mutexes.
  */
+/* deprecated becaues it can only use thread id */
 int _umtx_lock(struct umtx *mtx);
+/* deprecated becaues it can only use thread id */
 int _umtx_unlock(struct umtx *mtx);
+int _umtx_op(struct umtx *umtx, int op, long id, void *uaddr,
+	struct timespec *abstime);
 
 /*
  * Standard api.  Try uncontested acquire/release and asks the
@@ -57,7 +67,7 @@ umtx_lock(struct umtx *umtx, long id)
 {
 	if (atomic_cmpset_acq_ptr(&umtx->u_owner, (void *)UMTX_UNOWNED,
 	    (void *)id) == 0)
-		if (_umtx_lock(umtx) == -1)
+		if (_umtx_op(umtx, UMTX_OP_LOCK, id, 0, 0) == -1)
 			return (errno);
 	return (0);
 }
@@ -72,14 +82,52 @@ umtx_trylock(struct umtx *umtx, long id)
 }
 
 static __inline int
+umtx_timedlock(struct umtx *umtx, long id, struct timespec *abstime)
+{
+	if (atomic_cmpset_acq_ptr(&umtx->u_owner, (void *)UMTX_UNOWNED,
+	    (void *)id) == 0)
+		if (_umtx_op(umtx, UMTX_OP_LOCK, id, 0, abstime) == -1)
+			return (errno);
+	return (0);
+}
+
+static __inline int
 umtx_unlock(struct umtx *umtx, long id)
 {
 	if (atomic_cmpset_rel_ptr(&umtx->u_owner, (void *)id,
 	    (void *)UMTX_UNOWNED) == 0)
-		if (_umtx_unlock(umtx) == -1)
+		if (_umtx_op(umtx, UMTX_OP_UNLOCK, id, 0, 0) == -1)
 			return (errno);
 	return (0);
 }
-#endif /* !_KERNEL */
 
+/* Unlock umtx and wait on a user address. */
+
+static __inline int
+umtx_wait(struct umtx *umtx, long id, void *uaddr)
+{
+	if (_umtx_op(umtx, UMTX_OP_UNLOCK_AND_WAIT, id, uaddr, 0) == -1)
+		return (errno);
+	return (0);
+}
+
+static __inline int
+umtx_timedwait(struct umtx *umtx, long id, void *uaddr,
+	struct timespec *abstime)
+{
+	if (_umtx_op(umtx, UMTX_OP_UNLOCK_AND_WAIT, id, uaddr, abstime) == -1)
+		return (errno);
+	return (0);
+}
+
+/* Wake threads waiting on a user address. */
+static __inline int
+umtx_wake(void *uaddr, int broadcast)
+{
+	if (_umtx_op(0, UMTX_OP_WAKE, broadcast, uaddr, 0) == -1)
+		return (errno);
+	return (0);
+}
+
+#endif /* !_KERNEL */
 #endif /* !_SYS_UMTX_H_ */
