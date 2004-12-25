57dab8ec33ff8bfe30d6af954c5201a699d05644
Robert Watson <rwatson@FreeBSD.org>
2004-12-25 22:31:11 +0000

    Merge tcp_subr.c:1.209 from HEAD to RELENG_5:
      date: 2004/11/23 16:06:15;  author: rwatson;  state: Exp;  lines: +4 -0
      Assert inpcb lock in tcp_quench(), tcp_drop_syn_sent(), tcp_mtudisc(),
      and tcp_drop(), due to read-modify-write of TCP state variables.
    
      MFC after:      2 weeks

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -654,6 +654,7 @@ tcp_drop(tp, errno)
 {
 	struct socket *so = tp->t_inpcb->inp_socket;
 
+	INP_LOCK_ASSERT(tp->t_inpcb);
 	if (TCPS_HAVERCVDSYN(tp->t_state)) {
 		tp->t_state = TCPS_CLOSED;
 		(void) tcp_output(tp);
@@ -1381,6 +1382,7 @@ tcp_quench(inp, errno)
 {
 	struct tcpcb *tp = intotcpcb(inp);
 
+	INP_LOCK_ASSERT(inp);
 	if (tp != NULL)
 		tp->snd_cwnd = tp->t_maxseg;
 	return (inp);
@@ -1398,6 +1400,7 @@ tcp_drop_syn_sent(inp, errno)
 {
 	struct tcpcb *tp = intotcpcb(inp);
 
+	INP_LOCK_ASSERT(inp);
 	if (tp != NULL && tp->t_state == TCPS_SYN_SENT) {
 		tcp_drop(tp, errno);
 		return (struct inpcb *)0;
@@ -1427,6 +1430,7 @@ tcp_mtudisc(inp, errno)
 #endif /* INET6 */
 	bzero(&tao, sizeof(tao));
 
+	INP_LOCK_ASSERT(inp);
 	if (tp != NULL) {
 #ifdef INET6
 		isipv6 = (tp->t_inpcb->inp_vflag & INP_IPV6) != 0;
