7f36ab3f8e15b477951eb7803236b55da9b41f75
sam <sam@FreeBSD.org>
2004-12-31 22:42:38 +0000

    bump copyright for 2005

--- sys/dev/ath/if_athrate.h
+++ sys/dev/ath/if_athrate.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2004 Sam Leffler, Errno Consulting
+ * Copyright (c) 2004-2005 Sam Leffler, Errno Consulting
  * Copyright (c) 2004 Video54 Technologies, Inc.
  * All rights reserved.
  *
