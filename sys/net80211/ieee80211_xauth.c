5cfcdf5023b5366be02b5e25d6308d0cd47d4c61
sam <sam@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2004-12-31 22:42:38 +0000

    bump copyright for 2005
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@139530 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net80211/ieee80211_xauth.c
+++ sys/net80211/ieee80211_xauth.c
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2004 Video54 Technologies, Inc.
+ * Copyright (c) 2004-2005 Sam Leffler, Errno Consulting
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
