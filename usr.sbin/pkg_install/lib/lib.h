b134a8b6721c1bfd83d15a70b8c51233d696ca74
Paul Richards <paul@FreeBSD.org>
2005-01-04 16:18:55 +0000

    Use INDEX-6 for versions of FreeBSD > 600000

--- usr.sbin/pkg_install/lib/lib.h
+++ usr.sbin/pkg_install/lib/lib.h
@@ -77,7 +77,9 @@
 #define DISPLAY_FNAME		"+DISPLAY"
 #define MTREE_FNAME		"+MTREE_DIRS"
 
-#if defined(__FreeBSD_version) && __FreeBSD_version >= 500036
+#if defined(__FreeBSD_version) && __FreeBSD_version >= 600000
+#define INDEX_FNAME		"INDEX-6"
+#elif defined(__FreeBSD_version) && __FreeBSD_version >= 500036
 #define INDEX_FNAME		"INDEX-5"
 #else
 #define INDEX_FNAME		"INDEX"
