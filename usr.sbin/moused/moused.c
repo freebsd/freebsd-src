45efbba5c78f989a9df7545e9f951d96bf47ea64
Philip Paeps <philip@FreeBSD.org>
2005-01-06 08:38:29 +0000

    Document the -V and -U options in usage().
    Submitted by:	Jordan Sissel <psionic@csh.rit.edu>

--- usr.sbin/moused/moused.c
+++ usr.sbin/moused/moused.c
@@ -1155,8 +1155,8 @@ usage(void)
 {
     fprintf(stderr, "%s\n%s\n%s\n%s\n",
 	"usage: moused [-DRcdfs] [-I file] [-F rate] [-r resolution] [-S baudrate]",
-	"              [-a X [,Y]] [-C threshold] [-m N=M] [-w N] [-z N]",
-	"              [-t <mousetype>] [-l level] [-3 [-E timeout]] -p <port>",
+	"              [-V [-U threshold]] [-a X [,Y]] [-C threshold] [-m N=M] [-w N]",
+	"              [-z N] [-t <mousetype>] [-l level] [-3 [-E timeout]] -p <port>",
 	"       moused [-d] -i <port|if|type|model|all> -p <port>");
     exit(1);
 }
