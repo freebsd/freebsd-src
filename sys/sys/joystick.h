b7717ebc5c7132a9dbfd173a6b6e048c7b3a47a3
keramida <keramida@FreeBSD.org>
2005-01-07 19:31:43 +0000

    Comment typo.
    PR:		kern/75923
    Submitted by:	Matthew D. Fuller <fullermd@over-yonder.net>

--- sys/sys/joystick.h
+++ sys/sys/joystick.h
@@ -43,7 +43,7 @@ struct joystick {
 #define JOY_SETTIMEOUT    _IOW('J', 1, int)    /* set timeout */
 #define JOY_GETTIMEOUT    _IOR('J', 2, int)    /* get timeout */
 #define JOY_SET_X_OFFSET  _IOW('J', 3, int)    /* set offset on X-axis */
-#define JOY_SET_Y_OFFSET  _IOW('J', 4, int)    /* set offset on X-axis */
+#define JOY_SET_Y_OFFSET  _IOW('J', 4, int)    /* set offset on Y-axis */
 #define JOY_GET_X_OFFSET  _IOR('J', 5, int)    /* get offset on X-axis */
 #define JOY_GET_Y_OFFSET  _IOR('J', 6, int)    /* get offset on Y-axis */
 
