66d165347db09db45a3804462081adbb03095fa7
Robert Watson <rwatson@FreeBSD.org>
2005-01-09 07:34:55 +0000

    Mark the IPX netisr as MPSAFE so that inbound IPX traffic is processed without Giant, and can be directly dispatched in the ithread when net.isr.enable is turned on.
    MFC after:	4 weeks

--- sys/netipx/ipx_input.c
+++ sys/netipx/ipx_input.c
@@ -126,7 +126,7 @@ ipx_init()
 
 	ipxintrq.ifq_maxlen = ipxqmaxlen;
 	mtx_init(&ipxintrq.ifq_mtx, "ipx_inq", NULL, MTX_DEF);
-	netisr_register(NETISR_IPX, ipxintr, &ipxintrq, 0);
+	netisr_register(NETISR_IPX, ipxintr, &ipxintrq, NETISR_MPSAFE);
 }
 
 /*
