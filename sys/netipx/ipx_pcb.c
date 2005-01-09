f52379470fbd0a8f3a4f79a41439ef85925cb342
rwatson <rwatson@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-01-09 04:47:42 +0000

    In ipx_setsockaddr(), use M_WAITOK instead of M_NOWAIT so that the call always succeeds, avoiding causing the caller to return success even though the returned *sockaddr is NULL.
    MFC after:	2 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@139924 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netipx/ipx_pcb.c
+++ sys/netipx/ipx_pcb.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2004 Robert N. M. Watson
+ * Copyright (c) 2004-2005 Robert N. M. Watson
  * Copyright (c) 1995, Mike Mitchell
  * Copyright (c) 1984, 1985, 1986, 1987, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -292,7 +292,7 @@ ipx_setsockaddr(ipxp, nam)
 	sipx->sipx_len = sizeof(*sipx);
 	sipx->sipx_family = AF_IPX;
 	sipx->sipx_addr = ipxp->ipxp_laddr;
-	*nam = sodupsockaddr((struct sockaddr *)sipx, M_NOWAIT);
+	*nam = sodupsockaddr((struct sockaddr *)sipx, M_WAITOK);
 }
 
 void
