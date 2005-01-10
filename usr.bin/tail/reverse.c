9d7b2abd69721c40fee19b47220e327b69637c98
dwmalone <dwmalone@FreeBSD.org>
2005-01-10 20:19:46 +0000

    Cast size_t to off_t before adding them to avoid warnings on the alpha. Use %ld and intmax_t for printing an off_t.

--- usr.bin/tail/reverse.c
+++ usr.bin/tail/reverse.c
@@ -50,6 +50,7 @@ __FBSDID("$FreeBSD$");
 #include <err.h>
 #include <errno.h>
 #include <limits.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -128,7 +129,8 @@ r_reg(FILE *fp, enum STYLE style, off_t off, struct stat *sbp)
 	curoff = size - 2;
 	lineend = size;
 	while (curoff >= 0) {
-		if (curoff < map.mapoff || curoff >= map.mapoff + map.maplen) {
+		if (curoff < map.mapoff ||
+		    curoff >= map.mapoff + (off_t)map.maplen) {
 			if (maparound(&map, curoff) != 0) {
 				ierr();
 				return;
@@ -245,7 +247,7 @@ r_buf(FILE *fp)
 	}
 
 	if (enomem) {
-		warnx("warning: %qd bytes discarded", enomem);
+		warnx("warning: %jd bytes discarded", (intmax_t)enomem);
 		rval = 1;
 	}
 
