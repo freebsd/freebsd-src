ea3239f358f08c54ccdd1c92c60376244d3f13ef
phk <phk@FreeBSD.org>
2005-01-11 12:02:26 +0000

    Silently ignore forced argument to unmount.

--- sys/fs/devfs/devfs_vfsops.c
+++ sys/fs/devfs/devfs_vfsops.c
@@ -121,8 +121,6 @@ devfs_unmount(mp, mntflags, td)
 	struct devfs_mount *fmp;
 
 	fmp = VFSTODEVFS(mp);
-	if (mntflags & MNT_FORCE)
-		return (EOPNOTSUPP);
 	/* There is 1 extra root vnode reference from devfs_mount(). */
 	error = vflush(mp, 1, flags, td);
 	if (error)
