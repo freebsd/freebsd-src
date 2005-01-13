b71645face8bd6ef6d856db15ff4591c88f5c2a5
Scott Long <scottl@FreeBSD.org>
2005-01-13 17:55:24 +0000

    Teach libdisk about the ipsd devices.  Note: I don't like to editorilize in commits, but libdisk is just plain lame.  Being required to teach it about each disk device name is part of the magic folklore that is just plain wrong.  This is one of the many many many reasons why libdisk & sysinstall need to be thrown away and rebuilt from scratch.  No amount of tweaking and fixing them will make them better.

--- lib/libdisk/create_chunk.c
+++ lib/libdisk/create_chunk.c
@@ -302,6 +302,8 @@ MakeDev(struct chunk *c1, const char *path)
 	cmaj = 102, p += 3;
     else if (!strncmp(p, "idad", 4))
 	cmaj = 109, p += 4;
+    else if (!strncmp(p, "ipsd", 4))
+	cmaj = 176, p += 4;
     else if (!strncmp(p, "mlxd", 4))
 	cmaj = 131, p += 4;
     else if (!strncmp(p, "amrd", 4))
--- lib/libdisk/disk.c
+++ lib/libdisk/disk.c
@@ -488,7 +488,7 @@ Collapse_Disk(struct disk *d)
 #ifdef PC98
 static char * device_list[] = {"wd", "aacd", "ad", "da", "afd", "fla", "idad", "mlxd", "amrd", "twed", "ar", "fd", 0};
 #else
-static char * device_list[] = {"aacd", "ad", "da", "afd", "fla", "idad", "mlxd", "amrd", "twed", "ar", "fd", 0};
+static char * device_list[] = {"aacd", "ad", "da", "afd", "fla", "idad", "ipsd", "mlxd", "amrd", "twed", "ar", "fd", 0};
 #endif
 
 int qstrcmp(const void* a, const void* b) {
