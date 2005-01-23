6d3b890b3bd175d8f12f90469745d60ecf582a68
das <das@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-01-23 15:46:22 +0000

    If x == y, return y, not x.  C99 (though not IEEE 754) requires that nextafter(+0.0, -0.0) returns -0.0 and nextafter(-0.0, +0.0) returns +0.0.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@140666 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/msun/src/s_nextafter.c
+++ lib/msun/src/s_nextafter.c
@@ -38,7 +38,7 @@ nextafter(double x, double y)
 	if(((ix>=0x7ff00000)&&((ix-0x7ff00000)|lx)!=0) ||   /* x is nan */
 	   ((iy>=0x7ff00000)&&((iy-0x7ff00000)|ly)!=0))     /* y is nan */
 	   return x+y;
-	if(x==y) return x;		/* x=y, return x */
+	if(x==y) return y;		/* x=y, return x */
 	if((ix|lx)==0) {			/* x == 0 */
 	    INSERT_WORDS(x,hy&0x80000000,1);	/* return +-minsubnormal */
 	    y = x*x;
