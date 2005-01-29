38aa5659764e0382faa3e00d4f729e96fcac677a
Robert Watson <rwatson@FreeBSD.org>
2005-01-29 23:19:08 +0000

    Style cleanup for O_DIRECT sysctl comment introduced in nfs_vnops.c:1.242.

--- sys/nfsclient/nfs_vnops.c
+++ sys/nfsclient/nfs_vnops.c
@@ -216,14 +216,15 @@ SYSCTL_INT(_vfs_nfs, OID_AUTO, nfs_directio_enable, CTLFLAG_RW,
 	   &nfs_directio_enable, 0, "Enable NFS directio");
 
 /*
- * This sysctl allows other processes to mmap a file that has been opened O_DIRECT 
- * by a process. In general, having processes mmap the file while Direct IO is in 
- * progress can lead to Data Inconsistencies. But, we allow this by default to 
- * prevent DoS attacks - to prevent a malicious user from opening up files O_DIRECT 
- * preventing other users from mmap'ing these files. "Protected" environments where 
- * stricter consistency guarantees are required can disable this knob. 
- * The process that opened the file O_DIRECT cannot mmap() the file, because 
- * mmap'ed IO on an O_DIRECT open() is not meaningful.
+ * This sysctl allows other processes to mmap a file that has been opened
+ * O_DIRECT by a process.  In general, having processes mmap the file while
+ * Direct IO is in progress can lead to Data Inconsistencies.  But, we allow
+ * this by default to prevent DoS attacks - to prevent a malicious user from
+ * opening up files O_DIRECT preventing other users from mmap'ing these
+ * files.  "Protected" environments where stricter consistency guarantees are
+ * required can disable this knob.  The process that opened the file O_DIRECT
+ * cannot mmap() the file, because mmap'ed IO on an O_DIRECT open() is not
+ * meaningful.
  */
 int nfs_directio_allow_mmap = 1;
 SYSCTL_INT(_vfs_nfs, OID_AUTO, nfs_directio_allow_mmap, CTLFLAG_RW,
