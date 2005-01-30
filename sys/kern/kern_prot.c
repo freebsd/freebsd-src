bc57bbf9bba782d5c6f47bebcec68b74f1c4da46
Robert Watson <rwatson@FreeBSD.org>
2005-01-30 00:35:48 +0000

    Merge kern_prot.c:1.190-1.191 from HEAD to RELENG_5:
      date: 2005/01/23 21:01:00;  author: rwatson;  state: Exp;  lines: +0 -4
      When reading pr_securelevel from a prison, perform a lockless read,
      as it's an integer read operation and the resulting slight race is
      acceptable.
    
      date: 2005/01/23 21:11:39;  author: rwatson;  state: Exp;  lines: +2 -4
      Style cleanup: with removal of mutex operations, we can also remove
      {}'s from securelevel_gt() and securelevel_ge().

--- sys/kern/kern_prot.c
+++ sys/kern/kern_prot.c
@@ -1275,12 +1275,9 @@ securelevel_gt(struct ucred *cr, int level)
 
 	active_securelevel = securelevel;
 	KASSERT(cr != NULL, ("securelevel_gt: null cr"));
-	if (cr->cr_prison != NULL) {
-		mtx_lock(&cr->cr_prison->pr_mtx);
+	if (cr->cr_prison != NULL)
 		active_securelevel = imax(cr->cr_prison->pr_securelevel,
 		    active_securelevel);
-		mtx_unlock(&cr->cr_prison->pr_mtx);
-	}
 	return (active_securelevel > level ? EPERM : 0);
 }
 
@@ -1291,12 +1288,9 @@ securelevel_ge(struct ucred *cr, int level)
 
 	active_securelevel = securelevel;
 	KASSERT(cr != NULL, ("securelevel_ge: null cr"));
-	if (cr->cr_prison != NULL) {
-		mtx_lock(&cr->cr_prison->pr_mtx);
+	if (cr->cr_prison != NULL)
 		active_securelevel = imax(cr->cr_prison->pr_securelevel,
 		    active_securelevel);
-		mtx_unlock(&cr->cr_prison->pr_mtx);
-	}
 	return (active_securelevel >= level ? EPERM : 0);
 }
 
