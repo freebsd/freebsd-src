bf7ad7e2a9771db7cf9dccf6ad436da4a70cf4e1
imp <imp@FreeBSD.org>
2005-02-01 18:28:09 +0000

    The two PLANEX cards listed in pccarddevs, identified only by their CIS, weren't actually used anywhere (other than the generic PC Card code when certain variables are defined).  They aren't used in NetBSD either.  Make things simpler by removing them.  Change PLANEX_2 to PLANEX and tweak wi and owi to use that instead.  The PLANEX id seems to actually be pci ID assigned to planex, not its pcmcia id.  Ooops. I don't know if this is a reporting error from where this entry came from, or if it is a mistake on PLANEX's part.  I suspect the latter, as ACTIONTEC and NEWMEDIA made the same mistake (although new media may be because it uses an advansys chip inside).  Make a note of this in the file.  The 0xc entires may be JEITA assigned, so note that as well.
    # This leaves just 3 entries that are totally unknown: airvast, archos
    # and edimax although the arivast number is the same assigned to
    # avertec in usb...

--- sys/dev/owi/if_owi_pccard.c
+++ sys/dev/owi/if_owi_pccard.c
@@ -135,7 +135,7 @@ static const struct pccard_product wi_pccard_products[] = {
 	/* Must be after other LUCENT ones because it is less specific */
 	PCMCIA_CARD(LUCENT, WAVELAN_IEEE, 0),
 	PCMCIA_CARD(NOKIA, C110_WLAN, 0),
-	PCMCIA_CARD(PLANEX_2, GWNS11H, 0),
+	PCMCIA_CARD(PLANEX, GWNS11H, 0),
 	PCMCIA_CARD(PROXIM, RANGELANDS_8430, 0),
 	PCMCIA_CARD(SAMSUNG, SWL_2000N, 0),
 	PCMCIA_CARD(SIMPLETECH, SPECTRUM24_ALT, 0),
