e5a21b9dea4ddce1cedf55c88ab76429abd31997
Ruslan Ermilov <ru@FreeBSD.org>
2005-02-03 13:46:06 +0000

    MFC: New ASCII message, "getifname".

--- sys/netgraph/ng_eiface.c
+++ sys/netgraph/ng_eiface.c
@@ -56,6 +56,13 @@
 static const struct ng_cmdlist ng_eiface_cmdlist[] = {
 	{
 	  NGM_EIFACE_COOKIE,
+	  NGM_EIFACE_GET_IFNAME,
+	  "getifname",
+	  NULL,
+	  &ng_parse_string_type
+	},
+	{
+	  NGM_EIFACE_COOKIE,
 	  NGM_EIFACE_SET,
 	  "set",
 	  &ng_parse_enaddr_type,
@@ -496,19 +503,14 @@ ng_eiface_rcvmsg(node_p node, struct ng_mesg *msg,
 		    }
 
 		case NGM_EIFACE_GET_IFNAME:
-		    {
-			struct ng_eiface_ifname *arg;
-
-			NG_MKRESPONSE(resp, msg, sizeof(*arg), M_NOWAIT);
+			NG_MKRESPONSE(resp, msg, IFNAMSIZ + 1, M_NOWAIT);
 			if (resp == NULL) {
 				error = ENOMEM;
 				break;
 			}
-			arg = (struct ng_eiface_ifname *)resp->data;
-			sprintf(arg->ngif_name,
+			snprintf(resp->data, IFNAMSIZ + 1,
 			    "%s%d", ifp->if_name, ifp->if_unit);
 			break;
-		    }
 
 		case NGM_EIFACE_GET_IFADDRS:
 		    {
