aad6e16ac751e1a4d5abb0840708296ce93f849e
David E. O'Brien <obrien@FreeBSD.org>
2005-02-06 13:33:33 +0000

    Quite booting by hiding GIANT-LOCKED and INTR_FAST behind verboseboot. (note coding style chosen to reduce diffs to 6-CURRENT)

--- sys/kern/subr_bus.c
+++ sys/kern/subr_bus.c
@@ -3186,11 +3186,11 @@ bus_setup_intr(device_t dev, struct resource *r, int flags,
 		error = BUS_SETUP_INTR(dev->parent, dev, r, flags,
 		    handler, arg, cookiep);
 		if (error == 0) {
-			if (!(flags & (INTR_MPSAFE | INTR_FAST)))
+			if (bootverbose && !(flags & (INTR_MPSAFE | INTR_FAST)))
 				device_printf(dev, "[GIANT-LOCKED]\n");
 			if (bootverbose && (flags & INTR_MPSAFE))
 				device_printf(dev, "[MPSAFE]\n");
-			if (flags & INTR_FAST)
+			if (bootverbose && (flags & INTR_FAST))
 				device_printf(dev, "[FAST]\n");
 		}
 	} else
