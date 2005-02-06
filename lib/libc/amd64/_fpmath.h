2d20c474633089be5c8b65def96d1583468bc652
das <das@FreeBSD.org>
2005-02-06 03:23:31 +0000

    Update my email address.

--- lib/libc/amd64/_fpmath.h
+++ lib/libc/amd64/_fpmath.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2002, 2003 David Schultz <dschultz@uclink.Berkeley.EDU>
+ * Copyright (c) 2002, 2003 David Schultz <das@FreeBSD.ORG>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
