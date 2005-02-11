6891ebdcae899677bc96f6bace37896d96a3e259
sobomax <sobomax@FreeBSD.org>
2005-02-11 14:02:42 +0000

    Add SIGTHR (32) into list of signals permitted to be delivered to the suid application. The problem is that Linux applications using old Linux threads (pre-NPTL) use signal 32 (linux SIGRTMIN) for communication between thread-processes. If such an linux application is installed suid or sgid and security.bsd.conservative_signals=1 (default), then permission will be denied to send such a signal and the application will freeze.
    I believe the same will be true for native applications that use libthr,
    since libthr uses SIGTHR for implementing conditional variables.
    
    PR:		72922
    Submitted by:	Andriy Gapon <avg@icyb.net.ua>
    MFC after:	2 weeks

--- sys/kern/kern_prot.c
+++ sys/kern/kern_prot.c
@@ -1467,6 +1467,7 @@ cr_cansignal(struct ucred *cred, struct proc *proc, int signum)
 		case SIGHUP:
 		case SIGUSR1:
 		case SIGUSR2:
+		case SIGTHR:
 			/*
 			 * Generally, permit job and terminal control
 			 * signals.
