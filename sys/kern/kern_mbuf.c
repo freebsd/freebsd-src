696fea5da0608b22173a9cb9c594feead34fcd53
Bosko Milekic <bmilekic@FreeBSD.org>
2005-02-16 21:53:08 +0000

    MFC: Sync up the Copyright statements for the following files to their latest versions in HEAD: sys/kern/kern_mbuf.c sys/vm/uma.h sys/vm/uma_core.c sys/vm/uma_dbg.c sys/vm/uma_dbg.h sys/vm/uma_int.h (Basically re-add "All rights reserved" in time for 5.4-RELEASE, at least until we clear out the surrounding legal issues once and for all).

--- sys/kern/kern_mbuf.c
+++ sys/kern/kern_mbuf.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 2004, 2005
- * 	Bosko Milekic <bmilekic@FreeBSD.org>.
+ * Copyright (c) 2004, 2005,
+ * 	Bosko Milekic <bmilekic@FreeBSD.org>.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
--- sys/vm/uma_dbg.h
+++ sys/vm/uma_dbg.h
@@ -1,8 +1,8 @@
 /*-
  * Copyright (c) 2004, 2005,
- *     Bosko Milekic <bmilekic@freebsd.org>
+ *     Bosko Milekic <bmilekic@FreeBSD.org>.  All rights reserved.
  * Copyright (c) 2002, 2003, 2004, 2005,
- *     Jeffrey Roberson <jeff@freebsd.org>
+ *     Jeffrey Roberson <jeff@FreeBSD.org>.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
