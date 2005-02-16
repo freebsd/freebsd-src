8076cb5289fd144da08ea6f78296028ed6f708b6
Bosko Milekic <bmilekic@FreeBSD.org>
2005-02-16 21:45:59 +0000

    Well, it seems that I pre-maturely removed the "All rights reserved" statement from some files, so re-add it for the moment, until the related legalese is sorted out.  This change affects:
    sys/kern/kern_mbuf.c
    sys/vm/memguard.c
    sys/vm/memguard.h
    sys/vm/uma.h
    sys/vm/uma_core.c
    sys/vm/uma_dbg.c
    sys/vm/uma_dbg.h
    sys/vm/uma_int.h

--- sys/vm/memguard.c
+++ sys/vm/memguard.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2005,
- *     Bosko Milekic <bmilekic@freebsd.org>
+ *     Bosko Milekic <bmilekic@FreeBSD.org>.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
