9dec2f4d0fcde2b8649dd9e7febf9c351813d622
pjd <pjd@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-02-16 22:19:13 +0000

    Fix year in copyrights.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@141995 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/geom/class/shsec/geom_shsec.c
+++ sbin/geom/class/shsec/geom_shsec.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2004 Pawel Jakub Dawidek <pjd@FreeBSD.org>
+ * Copyright (c) 2005 Pawel Jakub Dawidek <pjd@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
