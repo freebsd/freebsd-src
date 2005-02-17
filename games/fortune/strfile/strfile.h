58140d2518e2554c03570b571c7c5abd81584114
ru <ru@FreeBSD.org>
2005-02-17 18:06:37 +0000

    Make the on-disk format of .dat files produced by strfile(8) architecture independent.  Besides the fixed-width types in the header, the offsets are now stored as 64-bit off_t (also in big endian format).
    Tested on:	i386, amd64, sparc64, ia64

--- games/fortune/strfile/strfile.h
+++ games/fortune/strfile/strfile.h
@@ -37,20 +37,22 @@
  */
 /* $FreeBSD$ */
 
+#include <sys/types.h>
+
 #define	STR_ENDSTRING(line,tbl) \
 	(((unsigned char)(line)[0]) == (tbl).str_delim && (line)[1] == '\n')
 
 typedef struct {				/* information table */
 #define	VERSION		1
-	unsigned long	str_version;		/* version number */
-	unsigned long	str_numstr;		/* # of strings in the file */
-	unsigned long	str_longlen;		/* length of longest string */
-	unsigned long	str_shortlen;		/* length of shortest string */
+	uint32_t	str_version;		/* version number */
+	uint32_t	str_numstr;		/* # of strings in the file */
+	uint32_t	str_longlen;		/* length of longest string */
+	uint32_t	str_shortlen;		/* length of shortest string */
 #define	STR_RANDOM	0x1			/* randomized pointers */
 #define	STR_ORDERED	0x2			/* ordered pointers */
 #define	STR_ROTATED	0x4			/* rot-13'd text */
 #define	STR_COMMENTS	0x8			/* embedded comments */
-	unsigned long	str_flags;		/* bit field for flags */
-	unsigned char	stuff[4];		/* long aligned space */
+	uint32_t	str_flags;		/* bit field for flags */
+	unsigned char	stuff[4];		/* 64-bit aligned space */
 #define	str_delim	stuff[0]		/* delimiting character */
 } STRFILE;
