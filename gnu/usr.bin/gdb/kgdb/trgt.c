325ce5d8fb22cea8ad841246ab4013efdb8413c5
Alexander Kabaev <kan@FreeBSD.org>
2005-02-20 22:55:07 +0000

    Attempt to make kgdb little more useful and easy to use.  Properly initialize it to recognise what ABI  to use on amd64 (and possibly others) platform. Display PID and process name as a part of the 'info threads' output, TIDs alone are too confusing. Introduce new commmands 'tid <tid>' and 'proc <pid>' to accompany gdb's default 'thread <thread num>' to make the task of switching between different contexts easier.

--- gnu/usr.bin/gdb/kgdb/trgt.c
+++ gnu/usr.bin/gdb/kgdb/trgt.c
@@ -27,22 +27,48 @@
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/proc.h>
+#include <sys/sysctl.h>
+#include <sys/user.h>
 #include <kvm.h>
+
+#include <defs.h>
+#include <command.h>
+#include <gdbthread.h>
+#include <inferior.h>
+#include <regcache.h>
+#include <target.h>
 
 #include "kgdb.h"
 
-#include <defs.h>
-#include <gdbthread.h>
-#include <inferior.h>
-#include <target.h>
-
 static struct target_ops kgdb_trgt_ops;
 
-static char *
-kgdb_trgt_extra_thread_info(struct thread_info *ti __unused)
+#define	KERNOFF		(kgdb_kernbase ())
+#define	INKERNEL(x)	((x) >= KERNOFF)
+
+static CORE_ADDR
+kgdb_kernbase (void)
 {
-	return (NULL);
+	static CORE_ADDR kernbase;
+	struct minimal_symbol *sym;
+ 
+	if (kernbase == 0)
+	{
+		sym = lookup_minimal_symbol ("kernbase", NULL, NULL);
+		if (sym == NULL) {
+			kernbase = KERNBASE;
+		} else {
+			kernbase = SYMBOL_VALUE_ADDRESS (sym);
+		}
+	}
+	return kernbase;
+}
+
+static char *
+kgdb_trgt_extra_thread_info(struct thread_info *ti)
+{
+	return (kgdb_thr_extra_thread_info(ptid_get_tid(ti->ptid)));
 }
 
 static void
@@ -53,16 +79,17 @@ kgdb_trgt_find_new_threads(void)
 static char *
 kgdb_trgt_pid_to_str(ptid_t ptid)
 {
-	static char buf[16];
+	static char buf[33];
 
-	snprintf(buf, sizeof(buf), "TID %d", ptid_get_pid(ptid));
+	snprintf(buf, sizeof(buf), "PID %5d TID %5ld", ptid_get_pid(ptid),
+		ptid_get_tid(ptid));
 	return (buf);
 }
 
 static int
 kgdb_trgt_thread_alive(ptid_t ptid)
 {
-	return (kgdb_thr_lookup(ptid_get_pid(ptid)) != NULL);
+	return (kgdb_thr_lookup_tid(ptid_get_tid(ptid)) != NULL);
 }
 
 static int
@@ -76,6 +103,71 @@ kgdb_trgt_xfer_memory(CORE_ADDR memaddr, char *myaddr, int len, int write,
 		return (kvm_read(kvm, memaddr, myaddr, len));
 	else
 		return (kvm_write(kvm, memaddr, myaddr, len));
+}
+
+static void
+kgdb_switch_to_thread(struct kthr *thr)
+{
+	if (thr->tid == ptid_get_tid(inferior_ptid))
+		return;
+
+	inferior_ptid = ptid_build(thr->pid, 0, thr->tid);
+	flush_cached_frames ();
+	registers_changed ();
+	stop_pc = read_pc ();
+	select_frame (get_current_frame ());
+}
+
+static void
+kgdb_set_proc_cmd (char *arg, int from_tty)
+{
+	CORE_ADDR addr;
+	struct kthr *thr;
+
+	if (!arg)
+		error_no_arg ("proc address for the new context");
+
+	if (kvm == NULL)
+		error ("no kernel core file");
+
+	addr = (CORE_ADDR) parse_and_eval_address (arg);
+
+	if (!INKERNEL (addr)) {
+		thr = kgdb_thr_lookup_pid((int)addr);
+		if (thr == NULL)
+			error ("invalid pid");
+	} else {
+		thr = kgdb_thr_lookup_paddr(addr);
+		if (thr == NULL)
+			error("invalid proc address");
+	}
+	kgdb_switch_to_thread(thr);
+}
+
+static void
+kgdb_set_tid_cmd (char *arg, int from_tty)
+{
+	CORE_ADDR addr;
+	struct kthr *thr;
+
+	if (!arg)
+		error_no_arg ("TID or thread address for the new context");
+
+	if (kvm == NULL)
+		error ("no kernel core file");
+
+	addr = (CORE_ADDR) parse_and_eval_address (arg);
+
+	if (!INKERNEL (addr)) {
+		thr = kgdb_thr_lookup_tid((int)addr);
+		if (thr == NULL)
+			error ("invalid TID");
+	} else {
+		thr = kgdb_thr_lookup_taddr(addr);
+		if (thr == NULL)
+			error("invalid thread address");
+	}
+	kgdb_switch_to_thread(thr);
 }
 
 void
@@ -105,8 +197,12 @@ kgdb_target(void)
 
 	kt = kgdb_thr_first();
 	while (kt != NULL) {
-		ti = add_thread(ptid_build(kt->tid, 0, 0));
+		ti = add_thread(ptid_build(kt->pid, 0, kt->tid));
 		kt = kgdb_thr_next(kt);
 	}
-	inferior_ptid = ptid_build(curkthr->tid, 0, 0);
+	inferior_ptid = ptid_build(curkthr->pid, 0, curkthr->tid);
+	add_com ("proc", class_obscure, kgdb_set_proc_cmd,
+	   "Set current process context");
+	add_com ("tid", class_obscure, kgdb_set_tid_cmd,
+	   "Set current process context");
 }
