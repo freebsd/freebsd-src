86862c1cfa103972ad2a6f9efcdcb155d34e6d7c
scottl <scottl@FreeBSD.org>
2005-02-20 23:51:20 +0000

    Check the correct periph pointer after search for it.
    Submitted by: Coverity Prevent analysis tool

--- sys/cam/cam_xpt.c
+++ sys/cam/cam_xpt.c
@@ -2546,7 +2546,7 @@ xptplistperiphfunc(struct cam_periph *periph, void *arg)
 					break;
 			}
 
-			if (pdrv == NULL) {
+			if (*pdrv == NULL) {
 				cdm->status = CAM_DEV_MATCH_ERROR;
 				return(0);
 			}
