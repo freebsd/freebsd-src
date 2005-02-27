e94c6cb4a2a24df0410e5604d4fd079127c09850
Alexey Zelkin <phantom@FreeBSD.org>
2005-02-27 15:11:09 +0000

    . Static'ize functions exported via function reference variables only. . Replace inclusion of sys/param.h to sys/cdefs.h and sys/types.h where   appropriate. . move _*_init() prototypes to mblocal.h, and remove these prototypes   from .c files . use _none_init() in __setrunelocale() instead of duplicating code . move __mb* variables from table.c to none.c allowing us to not to   export _none_*() externs, and appropriately remove them from mblocal.h
    Ok'ed by:	tjr

--- lib/libc/locale/none.c
+++ lib/libc/locale/none.c
@@ -51,16 +51,16 @@ __FBSDID("$FreeBSD$");
 #include <wchar.h>
 #include "mblocal.h"
 
-int	_none_init(_RuneLocale *);
-size_t	_none_mbrtowc(wchar_t * __restrict, const char * __restrict, size_t,
-	    mbstate_t * __restrict);
-int	_none_mbsinit(const mbstate_t *);
-size_t	_none_mbsnrtowcs(wchar_t * __restrict dst,
-	    const char ** __restrict src, size_t nms, size_t len,
-	    mbstate_t * __restrict ps __unused);
-size_t	_none_wcrtomb(char * __restrict, wchar_t, mbstate_t * __restrict);
-size_t	_none_wcsnrtombs(char * __restrict, const wchar_t ** __restrict,
-	    size_t, size_t, mbstate_t * __restrict);
+static size_t	_none_mbrtowc(wchar_t * __restrict, const char * __restrict,
+		    size_t, mbstate_t * __restrict);
+static int	_none_mbsinit(const mbstate_t *);
+static size_t	_none_mbsnrtowcs(wchar_t * __restrict dst,
+		    const char ** __restrict src, size_t nms, size_t len,
+		    mbstate_t * __restrict ps __unused);
+static size_t	_none_wcrtomb(char * __restrict, wchar_t,
+		    mbstate_t * __restrict);
+static size_t	_none_wcsnrtombs(char * __restrict, const wchar_t ** __restrict,
+		    size_t, size_t, mbstate_t * __restrict);
 
 int
 _none_init(_RuneLocale *rl)
@@ -76,7 +76,7 @@ _none_init(_RuneLocale *rl)
 	return(0);
 }
 
-int
+static int
 _none_mbsinit(const mbstate_t *ps __unused)
 {
 
@@ -87,7 +87,7 @@ _none_mbsinit(const mbstate_t *ps __unused)
 	return (1);
 }
 
-size_t
+static size_t
 _none_mbrtowc(wchar_t * __restrict pwc, const char * __restrict s, size_t n,
     mbstate_t * __restrict ps __unused)
 {
@@ -103,7 +103,7 @@ _none_mbrtowc(wchar_t * __restrict pwc, const char * __restrict s, size_t n,
 	return (*s == '\0' ? 0 : 1);
 }
 
-size_t
+static size_t
 _none_wcrtomb(char * __restrict s, wchar_t wc,
     mbstate_t * __restrict ps __unused)
 {
@@ -119,7 +119,7 @@ _none_wcrtomb(char * __restrict s, wchar_t wc,
 	return (1);
 }
 
-size_t
+static size_t
 _none_mbsnrtowcs(wchar_t * __restrict dst, const char ** __restrict src,
     size_t nms, size_t len, mbstate_t * __restrict ps __unused)
 {
@@ -144,7 +144,7 @@ _none_mbsnrtowcs(wchar_t * __restrict dst, const char ** __restrict src,
 	return (nchr);
 }
 
-size_t
+static size_t
 _none_wcsnrtombs(char * __restrict dst, const wchar_t ** __restrict src,
     size_t nwc, size_t len, mbstate_t * __restrict ps __unused)
 {
@@ -177,3 +177,17 @@ _none_wcsnrtombs(char * __restrict dst, const wchar_t ** __restrict src,
 	*src = s;
 	return (nchr);
 }
+
+/* setup defaults */
+
+int __mb_cur_max = 1;
+size_t (*__mbrtowc)(wchar_t * __restrict, const char * __restrict, size_t,
+    mbstate_t * __restrict) = _none_mbrtowc;
+int (*__mbsinit)(const mbstate_t *) = _none_mbsinit;
+size_t (*__mbsnrtowcs)(wchar_t * __restrict, const char ** __restrict,
+    size_t, size_t, mbstate_t * __restrict) = _none_mbsnrtowcs;
+size_t (*__wcrtomb)(char * __restrict, wchar_t, mbstate_t * __restrict) =
+    _none_wcrtomb;
+size_t (*__wcsnrtombs)(char * __restrict, const wchar_t ** __restrict,
+    size_t, size_t, mbstate_t * __restrict) = _none_wcsnrtombs;
+
