474046a7a34b6e8c2a7d304b37a51c27f654351a
David E. O'Brien <obrien@FreeBSD.org>
2005-02-27 21:36:33 +0000

    MFC: rev 1.17: If mixed mode is not enabled by the APIC enumerator, don't assume that pin 0 on the first I/O APIC is an ExtINT pin.  Instead, assume it is ISA IRQ 0.

--- sys/i386/i386/io_apic.c
+++ sys/i386/i386/io_apic.c
@@ -65,7 +65,7 @@ __FBSDID("$FreeBSD$");
 
 #define	TODO		printf("%s: not implemented!\n", __func__)
 
-MALLOC_DEFINE(M_IOAPIC, "I/O APIC", "I/O APIC structures");
+static MALLOC_DEFINE(M_IOAPIC, "I/O APIC", "I/O APIC structures");
 
 /*
  * New interrupt support code..
@@ -423,7 +423,7 @@ ioapic_config_intr(struct intsrc *isrc, enum intr_trigger trig,
 	 * them to be set to active low.
 	 *
 	 * XXX: Should we write to the ELCR if the trigger mode changes for
-	 * an EISA IRQ?
+	 * an EISA IRQ or an ISA IRQ with the ELCR present?
 	 */
 	if (intpin->io_bus == APIC_BUS_EISA)
 		pol = INTR_POLARITY_HIGH;
@@ -547,10 +547,11 @@ ioapic_create(uintptr_t addr, int32_t apic_id, int intbase)
 
 		/*
 		 * Assume that pin 0 on the first I/O APIC is an ExtINT pin
-		 * and that pins 1-15 are ISA interrupts.  Assume that all
+		 * if mixed mode is enabled and an ISA interrupt if not.
+		 * Assume that pins 1-15 are ISA interrupts and that all
 		 * other pins are PCI interrupts.
 		 */
-		if (intpin->io_vector == 0)
+		if (intpin->io_vector == 0 && mixed_mode_enabled)
 			ioapic_set_extint(io, i);
 		else if (intpin->io_vector < IOAPIC_ISA_INTS) {
 			intpin->io_bus = APIC_BUS_ISA;
