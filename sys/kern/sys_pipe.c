4fcf4a6e6a3927ee0068d2af15b52b4258107f2d
Jeff Roberson <jeff@FreeBSD.org>
2005-02-27 02:39:33 +0000

    MFC revision 1.178  - filedesc locking cleanup.

--- sys/kern/sys_pipe.c
+++ sys/kern/sys_pipe.c
@@ -387,15 +387,7 @@ pipe(td, uap)
 	FILE_UNLOCK(rf);
 	error = falloc(td, &wf, &fd);
 	if (error) {
-		FILEDESC_LOCK(fdp);
-		if (fdp->fd_ofiles[td->td_retval[0]] == rf) {
-			fdp->fd_ofiles[td->td_retval[0]] = NULL;
-			fdunused(fdp, td->td_retval[0]);
-			FILEDESC_UNLOCK(fdp);
-			fdrop(rf, td);
-		} else {
-			FILEDESC_UNLOCK(fdp);
-		}
+		fdclose(fdp, rf, td->td_retval[0], td);
 		fdrop(rf, td);
 		/* rpipe has been closed by fdrop(). */
 		pipeclose(wpipe);
