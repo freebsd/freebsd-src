643ff7dec874e285d03b9e5fd4952976ef24db20
njl <njl@FreeBSD.org>
2005-02-28 05:39:34 +0000

    Protect against multiple includes and use _KERNEL to protect the PCI fns.

--- sys/dev/acpica/acpi_pcibvar.h
+++ sys/dev/acpica/acpi_pcibvar.h
@@ -30,6 +30,7 @@
 #ifndef	_ACPI_PCIBVAR_H_
 #define	_ACPI_PCIBVAR_H_
 
+#ifdef _KERNEL
 void	acpi_pci_link_add_reference(device_t dev, int index, device_t pcib,
     int slot, int pin);
 int	acpi_pci_link_route_interrupt(device_t dev, int index);
@@ -37,5 +38,6 @@ int	acpi_pcib_attach(device_t bus, ACPI_BUFFER *prt, int busno);
 int	acpi_pcib_route_interrupt(device_t pcib, device_t dev, int pin,
     ACPI_BUFFER *prtbuf);
 int	acpi_pcib_resume(device_t dev);
+#endif /* _KERNEL */
 
-#endif
+#endif /* !_ACPI_PCIBVAR_H_ */
