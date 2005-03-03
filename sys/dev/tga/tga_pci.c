db8739d64aacadb86ef21fb8f3a52dd0f6464517
David E. O'Brien <obrien@FreeBSD.org>
2005-03-03 04:49:43 +0000

    MFC: rev 1.13: Use BUS_PROBE_DEFAULT in preference to 0.
    Brought to you by the letters:	I-M-P

--- sys/dev/tga/tga_pci.c
+++ sys/dev/tga/tga_pci.c
@@ -105,7 +105,6 @@ static struct cdevsw tga_cdevsw = {
 	.d_ioctl =	pcigfb_ioctl,
 	.d_mmap =	pcigfb_mmap,
 	.d_name =	"tga",
-	.d_maj =	-1,
 };
 
 #endif /* FB_INSTALL_CDEV */
@@ -122,7 +121,7 @@ tga_probe(device_t dev)
 		if((pci_get_vendor(dev) == t->vendor_id) &&
 		   (pci_get_device(dev) == t->device_id)) {
 			device_set_desc(dev, t->name);
-			error = 0;
+			error = BUS_PROBE_DEFAULT;
 			break;
 		}
 		t++;
