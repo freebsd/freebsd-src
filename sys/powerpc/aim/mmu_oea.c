b0c2130963e6840bbda269b731ab078861d2989c
Peter Grehan <grehan@FreeBSD.org>
2005-03-07 07:31:20 +0000

    Replaced previous hw.physmem extraction with des's mods to getenv_ulong() - much simpler.
    Pointed out by:	des

--- sys/powerpc/aim/mmu_oea.c
+++ sys/powerpc/aim/mmu_oea.c
@@ -524,55 +524,6 @@ om_cmp(const void *a, const void *b)
 		return (0);
 }
 
-static vm_size_t
-pmap_tunable_physmem(void)
-{
-	char *cp;
-	vm_size_t retval;
-
-	retval = 0;
-
-        if ((cp = getenv("hw.physmem")) != NULL) {
-                u_int64_t allowmem, sanity;
-                char *ep;
-
-		sanity = allowmem = strtouq(cp, &ep, 0);
-		if ((ep != cp) && (*ep != 0)) {
-			switch(*ep) {
-			case 'g':
-			case 'G':
-				/*
-				 * Can't have more than 4G of RAM
-				 */
-				if (allowmem > 4) {
-					printf("Invalid memory size '%s'\n",
-					       cp);
-					return (0);
-				}
-				allowmem <<= 10;
-			case 'm':
-			case 'M':
-				allowmem <<= 10;
-			case 'k':
-			case 'K':
-				allowmem <<= 10;
-				break;
-			default:
-				allowmem = sanity = 0;
-			}
-			if (allowmem < sanity)
-				allowmem = 0;
-		}
-		if (allowmem == 0)
-			printf("Ignoring invalid memory size of '%s'\n", cp);
-		else
-			retval = allowmem;
-		freeenv(cp);
-	}
-
-	return (retval);
-}
-
 void
 pmap_bootstrap(vm_offset_t kernelstart, vm_offset_t kernelend)
 {
@@ -679,7 +630,8 @@ pmap_bootstrap(vm_offset_t kernelstart, vm_offset_t kernelend)
 	qsort(regions, regions_sz, sizeof(*regions), mr_cmp);
 	phys_avail_count = 0;
 	physsz = 0;
-	hwphyssz = pmap_tunable_physmem();
+	hwphyssz = 0;
+	TUNABLE_ULONG_FETCH("hw.physmem", (u_long *) &hwphyssz);
 	for (i = 0, j = 0; i < regions_sz; i++, j += 2) {
 		CTR3(KTR_PMAP, "region: %#x - %#x (%#x)", regions[i].mr_start,
 		    regions[i].mr_start + regions[i].mr_size,
