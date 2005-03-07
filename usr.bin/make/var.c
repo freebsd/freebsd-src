ebab91848235e5fb932d3b53283d7d2e0cffdfaa
harti <harti@FreeBSD.org>
2005-03-07 16:14:50 +0000

    Style: fix indendation of VarModify().
    Patch:		7.107
    
    Submitted by:	Max Okumoto <okumoto@ucsd.edu>

--- usr.bin/make/var.c
+++ usr.bin/make/var.c
@@ -600,25 +600,25 @@ Var_Value(const char *name, GNode *ctxt, char **frp)
 static char *
 VarModify(char *str, VarModifyProc *modProc, void *datum)
 {
-    Buffer  	  *buf;	    	    /* Buffer for the new string */
-    Boolean 	  addSpace; 	    /* TRUE if need to add a space to the
-				     * buffer before adding the trimmed
-				     * word */
-    char **av;			    /* word list [first word does not count] */
-    int ac, i;
+	Buffer	*buf;		/* Buffer for the new string */
+	Boolean	addSpace;	/* TRUE if need to add a space to the buffer
+				 * before adding the trimmed word */
+	char	**av;		/* word list [first word does not count] */
+	int	ac;
+	int	i;
 
-    buf = Buf_Init(0);
-    addSpace = FALSE;
+	buf = Buf_Init(0);
+	addSpace = FALSE;
 
-    av = brk_string(str, &ac, FALSE);
+	av = brk_string(str, &ac, FALSE);
 
-    for (i = 1; i < ac; i++)
-	addSpace = (*modProc)(av[i], addSpace, buf, datum);
+	for (i = 1; i < ac; i++)
+		addSpace = (*modProc)(av[i], addSpace, buf, datum);
 
-    Buf_AddByte(buf, '\0');
-    str = (char *)Buf_GetAll(buf, (size_t *)NULL);
-    Buf_Destroy(buf, FALSE);
-    return (str);
+	Buf_AddByte(buf, '\0');
+	str = (char *)Buf_GetAll(buf, (size_t *)NULL);
+	Buf_Destroy(buf, FALSE);
+	return (str);
 }
 
 /*-
