4bc4328cd16e278e2bb3dc24d27b82dd5ddf986e
ume <ume@FreeBSD.org>
2005-03-10 11:40:53 +0000

    use cast128 in opencrypto to nuke duplicate code.

--- sys/crypto/cast128/cast128.h
+++ sys/crypto/cast128/cast128.h
@@ -1,26 +1,17 @@
 /*	$FreeBSD$	*/
-/*	$NetBSD: cast128.h,v 1.6 2003/08/26 20:03:57 thorpej Exp $ */
-/*      $OpenBSD: cast.h,v 1.2 2002/03/14 01:26:51 millert Exp $       */
-
-/*
- *	CAST-128 in C
- *	Written by Steve Reid <sreid@sea-to-sky.net>
- *	100% Public Domain - no warranty
- *	Released 1997.10.11
- */
 
 #ifndef _CAST128_H_
 #define _CAST128_H_
 
-typedef struct {
-	u_int32_t	xkey[32];	/* Key, after expansion */
-	int		rounds;		/* Number of rounds to use, 12 or 16 */
-} cast128_key;
+#include <opencrypto/cast.h>
 
-void cast128_setkey(cast128_key *key, const u_int8_t *rawkey, int keybytes);
-void cast128_encrypt(const cast128_key *key, const u_int8_t *inblock,
-		     u_int8_t *outblock);
-void cast128_decrypt(const cast128_key *key, const u_int8_t *inblock,
-		     u_int8_t *outblock);
+#define cast128_key	cast_key
+
+#define cast128_setkey(key, rawkey, keybytes) \
+	cast_setkey((key), (rawkey), (keybytes))
+#define cast128_encrypt(key, inblock, outblock) \
+	cast_encrypt((key), (inblock), (outblock))
+#define cast128_decrypt(key, inblock, outblock) \
+	cast_decrypt((key), (inblock), (outblock))
 
 #endif /* _CAST128_H_ */
