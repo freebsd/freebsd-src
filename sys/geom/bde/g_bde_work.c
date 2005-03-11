68527b3aad57040b661a801cfb4de8b5961ae3c0
Hajimu UMEMOTO <ume@FreeBSD.org>
2005-03-11 15:42:51 +0000

    stop including rijndael-api-fst.h from rijndael.h. this is required to integrate opencrypto into crypto.

--- sys/geom/bde/g_bde_work.c
+++ sys/geom/bde/g_bde_work.c
@@ -70,7 +70,7 @@
 #include <sys/proc.h>
 #include <sys/kthread.h>
 
-#include <crypto/rijndael/rijndael.h>
+#include <crypto/rijndael/rijndael-api-fst.h>
 #include <crypto/sha2/sha2.h>
 #include <geom/geom.h>
 #include <geom/bde/g_bde.h>
