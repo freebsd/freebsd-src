4c79032456805cd72e9a2b8c3b2e0ad993cccf15
ume <ume@FreeBSD.org>
2005-03-11 12:37:07 +0000

    refer opencrypto/cast.h directly.

--- sys/netinet6/esp_core.c
+++ sys/netinet6/esp_core.c
@@ -75,7 +75,15 @@
 
 #include <crypto/des/des.h>
 #include <crypto/blowfish/blowfish.h>
-#include <crypto/cast128/cast128.h>
+
+#include <opencrypto/cast.h>
+#define cast128_key	cast_key
+#define cast128_setkey(key, rawkey, keybytes) \
+	cast_setkey((key), (rawkey), (keybytes))
+#define cast128_encrypt(key, inblock, outblock) \
+	cast_encrypt((key), (inblock), (outblock))
+#define cast128_decrypt(key, inblock, outblock) \
+	cast_decrypt((key), (inblock), (outblock))
 
 #include <net/net_osdep.h>
 
