ea53ec6d3d993faa5f37921d8881e1a2fe454160
phk <phk@FreeBSD.org>
2005-03-15 09:30:17 +0000

    Move devtoname() prototype to systm.h to reduce #include pollution, it is (or should be) used in many printf() calls.

--- sys/sys/conf.h
+++ sys/sys/conf.h
@@ -255,7 +255,6 @@ int	count_dev(struct cdev *_dev);
 void	destroy_dev(struct cdev *_dev);
 struct cdevsw *dev_refthread(struct cdev *_dev);
 void	dev_relthread(struct cdev *_dev);
-const char *devtoname(struct cdev *_dev);
 int	dev_named(struct cdev *_pdev, const char *_name);
 void	dev_depends(struct cdev *_pdev, struct cdev *_cdev);
 void	dev_ref(struct cdev *dev);
