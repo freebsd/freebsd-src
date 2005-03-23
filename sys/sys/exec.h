5372bd44b32215a6c6f543643a497a0838ea895a
das <das@FreeBSD.org>
2005-03-23 08:27:59 +0000

    Make ps_nargvstr and ps_nenvstr unsigned.  This fixes an input validation error in procfs/linprocfs that can be exploited by local users to cause a kernel panic.  All versions of FreeBSD with the patch referenced in SA-04:17.procfs have this bug, but versions without that patch have a more serious bug instead.  This problem only affects systems on which procfs or linprocfs is mounted.
    Found by:	Coverity Prevent analysis tool
    Security:	Local DOS

--- sys/sys/exec.h
+++ sys/sys/exec.h
@@ -48,9 +48,9 @@
  */
 struct ps_strings {
 	char	**ps_argvstr;	/* first of 0 or more argument strings */
-	int	ps_nargvstr;	/* the number of argument strings */
+	unsigned int ps_nargvstr; /* the number of argument strings */
 	char	**ps_envstr;	/* first of 0 or more environment strings */
-	int	ps_nenvstr;	/* the number of environment strings */
+	unsigned int ps_nenvstr; /* the number of environment strings */
 };
 
 /*
