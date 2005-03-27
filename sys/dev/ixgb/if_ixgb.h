18557a86adc6ee96570840458a69f8664bd49bcb
Maxime Henrion <mux@FreeBSD.org>
2005-03-27 16:38:08 +0000

    Fix a bunch of bugs I came accross when looking at the ixgb(4) driver, some of which are rather serious: - Use the device sysctl tree instead of rolling our own. - Don't create a bus_dmamap_t to pass to bus_dmamem_alloc(), it is   bus_dmamem_alloc() that creates it itself.  The DMA map created   by the driver was overwritten and its memory was leaked. - Fix resource handling bugs in the error path of ixgb_dma_alloc(). - Don't use vtophys() to get the base address of the TX and RX rings   when busdma already gave us the correct address to use! - Remove now useless includes and the alpha_XXX_dmamap() hack. - Don't initialize if_output to ether_output(), ether_ifattach() does   it for us already. - Add proper module dependencies on ether and pci.
    Unfortunately, I'm not lucky enough to own an ixgb(4) card, nor a
    machine with a bus where to plug it in and I couldn't find anyone able
    to test these patches, so they are only build-tested and I won't MFC
    them for 5.4-RELEASE.

--- sys/dev/ixgb/if_ixgb.h
+++ sys/dev/ixgb/if_ixgb.h
@@ -66,8 +66,6 @@ POSSIBILITY OF SUCH DAMAGE.
 #include <machine/bus.h>
 #include <sys/rman.h>
 #include <machine/resource.h>
-#include <vm/vm.h>
-#include <vm/pmap.h>
 #include <machine/clock.h>
 #if __FreeBSD_version >= 502000
 #include <dev/pci/pcivar.h>
@@ -227,11 +225,6 @@ POSSIBILITY OF SUCH DAMAGE.
 #define IXGB_RXBUFFER_16384      16384
 
 #define IXGB_MAX_SCATTER           100
-
-#ifdef __alpha__
-#undef vtophys
-#define vtophys(va)     alpha_XXX_dmamap((vm_offset_t)(va))
-#endif				/* __alpha__ */
 
 /*
  * ******************************************************************************
