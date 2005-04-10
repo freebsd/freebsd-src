606a68d9ee80f1f2f4a6c123e793670a449697e0
alc <alc@FreeBSD.org>
2005-04-10 20:49:21 +0000

    Align the entry point to assembly language functions to a 16-byte boundary. (The Opteron's instruction fetcher reads instructions from the L1 cache in 16-byte, aligned packets.)

--- sys/amd64/include/asm.h
+++ sys/amd64/include/asm.h
@@ -56,8 +56,7 @@
 #define CNAME(csym)		csym
 #define HIDENAME(asmsym)	.asmsym
 
-/* XXX should use .p2align 4,0x90 for -m486. */
-#define _START_ENTRY	.text; .p2align 2,0x90
+#define _START_ENTRY	.text; .p2align 4,0x90
 
 #define _ENTRY(x)	_START_ENTRY; \
 			.globl CNAME(x); .type CNAME(x),@function; CNAME(x):
