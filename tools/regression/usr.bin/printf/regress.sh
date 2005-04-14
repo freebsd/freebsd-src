73d2098d76ba5faed40c4fbc11258fb04f02aa56
stefanf <stefanf@FreeBSD.org>
2005-04-14 18:33:14 +0000

    Also test \0 in the format string.

--- tools/regression/usr.bin/printf/regress.sh
+++ tools/regression/usr.bin/printf/regress.sh
@@ -7,7 +7,7 @@ echo '1..8'
 REGRESSION_TEST(`b', `printf "abc%b%b" "def\n" "\cghi"')
 REGRESSION_TEST(`d', `printf "%d,%5d,%.5d,%0*d,%.*d\n" 123 123 123 5 123 5 123')
 REGRESSION_TEST(`f', `printf "%f,%-8.3f,%f,%f\n" +42.25 -42.25 inf nan')
-REGRESSION_TEST(`m1', `printf "%c%%%d\045\n" abc \"abc')
+REGRESSION_TEST(`m1', `printf "%c%%%d\0\045\n" abc \"abc')
 REGRESSION_TEST(`m2', `printf "abc\n\cdef"')
 REGRESSION_TEST(`m3', `printf "%%%s\n" abc def ghi jkl')
 REGRESSION_TEST(`m4', `printf "%d,%f,%c,%s\n"')
