4998705a4152f74a9b21bbe5446192279bc24aa9
ume <ume@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-04-14 11:44:43 +0000

    remove needless res_init() call.
    Inspired by:	NetBSD
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@145066 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/net/getaddrinfo.c
+++ lib/libc/net/getaddrinfo.c
@@ -2310,11 +2310,6 @@ res_queryN(name, target)
 	rcode = NOERROR;
 	ancount = 0;
 
-	if ((_res.options & RES_INIT) == 0 && res_init() == -1) {
-		h_errno = NETDB_INTERNAL;
-		return (-1);
-	}
-
 	buf = malloc(MAXPACKET);
 	if (!buf) {
 		h_errno = NETDB_INTERNAL;
@@ -2561,10 +2556,6 @@ res_querydomainN(name, domain, target)
 	const char *longname = nbuf;
 	size_t n, d;
 
-	if ((_res.options & RES_INIT) == 0 && res_init() == -1) {
-		h_errno = NETDB_INTERNAL;
-		return (-1);
-	}
 #ifdef DEBUG
 	if (_res.options & RES_DEBUG)
 		printf(";; res_querydomain(%s, %s)\n",
