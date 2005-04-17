4b9af58de53b7f647072af467d78b204c49ff94d
silby <silby@FreeBSD.org>
2005-04-17 00:40:27 +0000

    Have the regression test use a path in /tmp for a mount point instead of a directory that only exists on my machine.

--- tools/regression/msdosfs/msdosfstest.sh
+++ tools/regression/msdosfs/msdosfstest.sh
@@ -4,18 +4,20 @@
 # files to it, unmount/remount the filesystem, and make sure all is well.
 # 
 # Not very advanced, but better than nothing. 
+mkdir /tmp/msdosfstest/
 mdconfig -a -t swap -s 128m -u 10
 bsdlabel -w md10 auto
 newfs_msdos -F 16 -b 8192 /dev/md10a
-mount_msdosfs /dev/md10a /mnt/thumb/
-cp -R /usr/src/bin/ /mnt/thumb/
-umount /mnt/thumb
-mount_msdosfs /dev/md10a /mnt/thumb/
-diff -u -r /usr/src/bin /mnt/thumb
+mount_msdosfs /dev/md10a /tmp/msdosfstest/
+cp -R /usr/src/bin/ /tmp/msdosfstest/
+umount /tmp/msdosfstest/
+mount_msdosfs /dev/md10a /tmp/msdosfstest/
+diff -u -r /usr/src/bin /tmp/msdosfstest/
 if [ $? -eq 0 ]; then
 	echo "ok 1";
 else
 	echo "not ok 1";
 fi
-umount /mnt/thumb
+umount /tmp/msdosfstest/
 mdconfig -d -u 10
+rmdir /tmp/msdosfstest/
