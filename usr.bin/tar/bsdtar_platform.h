167525173e688bcdd4645d2ad77d486324127b0c
kientzle <kientzle@FreeBSD.org>
2005-04-17 19:43:37 +0000

    A number of fixes to the autoconf-generated build system.  In particular, acl support is no longer enabled on FreeBSD 4, acl support should be correctly enabled on Linux, dirent.d_namlen should be correctly detected on platforms that support it.
    Thanks to: Greg Lewis, Juergen Lock, and Jaakko Heinonen

--- usr.bin/tar/bsdtar_platform.h
+++ usr.bin/tar/bsdtar_platform.h
@@ -43,6 +43,11 @@
 /* A default configuration for FreeBSD, used if there is no config.h. */
 #define	PACKAGE_NAME "bsdtar"
 
+#if __FreeBSD__ > 4
+#define	HAVE_ACL_GET_PERM 0
+#define	HAVE_ACL_GET_PERM_NP 1
+#define	HAVE_ACL_PERMSET_T 1
+#endif
 #define	HAVE_BZLIB_H 1
 #define	HAVE_CHFLAGS 1
 #define	HAVE_DIRENT_D_NAMLEN 1
@@ -81,9 +86,7 @@
 #define	HAVE_STRRCHR 1
 #define	HAVE_STRUCT_STAT_ST_MTIMESPEC_TV_NSEC 1
 #define	HAVE_STRUCT_STAT_ST_RDEV 1
-#if __FreeBSD__ > 4
 #define	HAVE_SYS_ACL_H 1
-#endif
 #define	HAVE_SYS_IOCTL_H 1
 #define	HAVE_SYS_PARAM_H 1
 #define	HAVE_SYS_STAT_H 1
@@ -95,8 +98,8 @@
 #define	STDC_HEADERS 1
 
 #else /* !__FreeBSD__ */
-/* Warn if the library hasn't been (automatically or manually) configured. */
-#error Oops: No config.h and no built-in configuration in archive_platform.h.
+/* Warn if bsdtar hasn't been (automatically or manually) configured. */
+#error Oops: No config.h and no built-in configuration in bsdtar_platform.h.
 #endif /* !__FreeBSD__ */
 
 #endif /* !HAVE_CONFIG_H */
@@ -112,10 +115,54 @@
 #error Configuration error: did not find libarchive.
 #endif
 
-/* TODO: Test for the functions we use as well... */
-#if HAVE_SYS_ACL_H
-#define	HAVE_POSIX_ACLS	1
+/*
+ * Does this platform have complete-looking POSIX-style ACL support,
+ * including some variant of the acl_get_perm() function (which was
+ * omitted from the POSIX.1e draft)?
+ */
+#if HAVE_SYS_ACL_H && HAVE_ACL_PERMSET_T
+#if HAVE_ACL_GET_PERM || HAVE_ACL_GET_PERM_NP
+#define	HAVE_POSIX_ACL	1
 #endif
+#endif
+
+#if HAVE_ACL_GET_PERM
+#define ACL_GET_PERM acl_get_perm
+#else
+#if HAVE_ACL_GET_PERM_NP
+#define ACL_GET_PERM acl_get_perm_np
+#endif
+#endif
+
+/*
+ * Include "dirent.h" (or it's equivalent on several different platforms).
+ *
+ * This is slightly modified from the GNU autoconf recipe.
+ * In particular, FreeBSD includes d_namlen in it's dirent structure,
+ * so my configure script includes an explicit test for the d_namlen
+ * field.
+ */
+#if HAVE_DIRENT_H
+# include <dirent.h>
+# if HAVE_DIRENT_D_NAMLEN
+#  define DIRENT_NAMLEN(dirent) (dirent)->d_namlen
+# else
+#  define DIRENT_NAMLEN(dirent) strlen((dirent)->d_name)
+# endif
+#else
+# define dirent direct
+# define DIRENT_NAMLEN(dirent) (dirent)->d_namlen
+# if HAVE_SYS_NDIR_H
+#  include <sys/ndir.h>
+# endif
+# if HAVE_SYS_DIR_H
+#  include <sys/dir.h>
+# endif
+# if HAVE_NDIR_H
+#  include <ndir.h>
+# endif
+#endif
+
 
 /*
  * We need to be able to display a filesize using printf().  The type
