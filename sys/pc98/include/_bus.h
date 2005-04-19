8c509864f2dd0cdcc6116de38bf9137583c4ab2f
imp <imp@FreeBSD.org>
2005-04-19 03:19:19 +0000

    Unbreak the pc98 build by including enough information in the _bus.h for _bus.h to compile.
    Pointy hat to: imp
    Breakage noted by: nyan-san

--- sys/pc98/include/_bus.h
+++ sys/pc98/include/_bus.h
@@ -37,6 +37,57 @@
 typedef u_int bus_addr_t;
 typedef u_int bus_size_t;
 
+#define BUS_SPACE_IAT_MAXSIZE	33
+
+/*
+ * bus space tag
+ */
+#define	_PASCAL_CALL	(void)
+
+#define	_BUS_SPACE_CALL_FUNCS_TAB(NAME,TYPE,BWN) \
+	NAME##_space_read_##BWN, 				\
+	NAME##_space_read_multi_##BWN, 				\
+	NAME##_space_read_region_##BWN,				\
+	NAME##_space_write_##BWN, 				\
+	NAME##_space_write_multi_##BWN, 			\
+	NAME##_space_write_region_##BWN,			\
+	NAME##_space_set_multi_##BWN,				\
+	NAME##_space_set_region_##BWN,				\
+	NAME##_space_copy_region_##BWN
+
+#define	_BUS_SPACE_CALL_FUNCS_PROTO(NAME,TYPE,BWN) \
+	TYPE NAME##_space_read_##BWN _PASCAL_CALL;		\
+	void NAME##_space_read_multi_##BWN _PASCAL_CALL;	\
+	void NAME##_space_read_region_##BWN _PASCAL_CALL;	\
+	void NAME##_space_write_##BWN _PASCAL_CALL;		\
+	void NAME##_space_write_multi_##BWN _PASCAL_CALL;	\
+	void NAME##_space_write_region_##BWN _PASCAL_CALL;	\
+	void NAME##_space_set_multi_##BWN _PASCAL_CALL;		\
+	void NAME##_space_set_region_##BWN _PASCAL_CALL;	\
+	void NAME##_space_copy_region_##BWN _PASCAL_CALL;
+
+#define	_BUS_SPACE_CALL_FUNCS(NAME,TYPE,BWN) \
+	TYPE (* NAME##_read_##BWN) _PASCAL_CALL;		\
+	void (* NAME##_read_multi_##BWN) _PASCAL_CALL;		\
+	void (* NAME##_read_region_##BWN) _PASCAL_CALL;		\
+	void (* NAME##_write_##BWN) _PASCAL_CALL;		\
+	void (* NAME##_write_multi_##BWN) _PASCAL_CALL;		\
+	void (* NAME##_write_region_##BWN) _PASCAL_CALL;	\
+	void (* NAME##_set_multi_##BWN) _PASCAL_CALL;		\
+	void (* NAME##_set_region_##BWN) _PASCAL_CALL;		\
+	void (* NAME##_copy_region_##BWN) _PASCAL_CALL;	
+
+struct bus_space_access_methods {
+	/* 8 bits access methods */
+	_BUS_SPACE_CALL_FUNCS(bs,u_int8_t,1)
+
+	/* 16 bits access methods */
+	_BUS_SPACE_CALL_FUNCS(bs,u_int16_t,2)
+
+	/* 32 bits access methods */
+	_BUS_SPACE_CALL_FUNCS(bs,u_int32_t,4)
+};
+
 /*
  * Access methods for bus resources and address space.
  */
