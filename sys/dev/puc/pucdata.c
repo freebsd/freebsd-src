e93f9859cc5bb2d753a53a7f4b1787eda8ae5b50
imp <imp@FreeBSD.org>
2005-04-22 07:49:35 +0000

    Sort Oxford Semi entires.  Add entry for OXCB950, a PCI/CardBus 16C950.  Adding it here doesn't unlock any of the cool 16C950 features (like the 128 byte fifo, the different prescalor, etc), but it does seem to get it working for me in light testing.
    Card Provided by: Ihsan Dogan

--- sys/dev/puc/pucdata.c
+++ sys/dev/puc/pucdata.c
@@ -846,6 +846,15 @@ const struct puc_device_description puc_devices[] = {
 	 * I/O Flex PCI I/O Card Model-223 with 4 serial and 1 parallel ports.
 	 */
 
+	/* Oxford Semiconductor OX12PCI840 PCI Parallel port */
+	{   "Oxford Semiconductor OX12PCI840 Parallel port",
+	    {	0x1415,	0x8403,	0,	0	},
+	    {	0xffff,	0xffff,	0,	0	},
+	    {
+		{ PUC_PORT_TYPE_LPT, 0x10, 0x00, 0x00 },
+	    },
+	},
+
 	/* Oxford Semiconductor OX16PCI954 PCI UARTs */
 	{   "Oxford Semiconductor OX16PCI954 UARTs",
 	    {	0x1415,	0x9501,	0,	0	},
@@ -870,21 +879,12 @@ const struct puc_device_description puc_devices[] = {
 	    },
 	},
 
-	/* Oxford Semiconductor OX16PCI954 PCI Parallel port */
-	{   "Oxford Semiconductor OX16PCI954 Parallel port",
-	    {	0x1415,	0x9513,	0,	0	},
+	/* Oxford Semiconductor OXCB950 PCI/CardBus UARTs */
+	{   "Oxford Semiconductor OXCB950 UART",
+	    {	0x1415,	0x950b,	0,	0	},
 	    {	0xffff,	0xffff,	0,	0	},
 	    {
-		{ PUC_PORT_TYPE_LPT, 0x10, 0x00, 0x00 },
-	    },
-	},
-
-	/* Oxford Semiconductor OX12PCI840 PCI Parallel port */
-	{   "Oxford Semiconductor OX12PCI840 Parallel port",
-	    {	0x1415,	0x8403,	0,	0	},
-	    {	0xffff,	0xffff,	0,	0	},
-	    {
-		{ PUC_PORT_TYPE_LPT, 0x10, 0x00, 0x00 },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x00, COM_FREQ },
 	    },
 	},
 
@@ -900,6 +900,15 @@ const struct puc_device_description puc_devices[] = {
 		{ PUC_PORT_TYPE_COM, 0x10, 0x08, COM_FREQ },
 		{ PUC_PORT_TYPE_COM, 0x10, 0x10, COM_FREQ },
 		{ PUC_PORT_TYPE_COM, 0x10, 0x18, COM_FREQ },
+	    },
+	},
+
+	/* Oxford Semiconductor OX16PCI954 PCI Parallel port */
+	{   "Oxford Semiconductor OX16PCI954 Parallel port",
+	    {	0x1415,	0x9513,	0,	0	},
+	    {	0xffff,	0xffff,	0,	0	},
+	    {
+		{ PUC_PORT_TYPE_LPT, 0x10, 0x00, 0x00 },
 	    },
 	},
 
