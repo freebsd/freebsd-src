cf1d2f3981d6795243f26ba75c9cc16cf07a9dac
marcel <marcel@FreeBSD.org>
2005-04-28 05:50:18 +0000

    Update comment to direct the reader to libkern.h instead of systm.h. The functions were moved.
    Pointed out by: johan@

--- sys/libkern/crc32.c
+++ sys/libkern/crc32.c
@@ -98,7 +98,7 @@ uint32_t crc32_tab[] = {
  * A function that calculates the CRC-32 based on the table above is
  * given below for documentation purposes. An equivalent implementation
  * of this function that's actually used in the kernel can be found
- * in sys/systm.h, where it can be inlined.
+ * in sys/libkern.h, where it can be inlined.
  *
  *	uint32_t
  *	crc32(const void *buf, size_t size)
