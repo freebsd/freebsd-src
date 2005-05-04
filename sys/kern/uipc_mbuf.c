ee7a0c4d124531c87ff1c5a950795a4c19f1aabf
emax <emax@FreeBSD.org>
2005-05-04 18:55:03 +0000

    Change m_uiotombuf so it will accept offset at which data should be copied to the mbuf. Offset cannot exceed MHLEN bytes. This is currently used to fix Ethernet header alignment problem on alpha and sparc64. Also change all users of m_uiotombuf to pass proper offset.
    Reviewed by:	jmg, sam
    Tested by:	Sten Spans "sten AT blinkenlights DOT nl"
    MFC after:	1 week

--- sys/kern/uipc_mbuf.c
+++ sys/kern/uipc_mbuf.c
@@ -1333,7 +1333,7 @@ nospace:
 #endif
 
 struct mbuf *
-m_uiotombuf(struct uio *uio, int how, int len)
+m_uiotombuf(struct uio *uio, int how, int len, int align)
 {
 	struct mbuf *m_new = NULL, *m_final = NULL;
 	int progress = 0, error = 0, length, total;
@@ -1342,12 +1342,15 @@ m_uiotombuf(struct uio *uio, int how, int len)
 		total = min(uio->uio_resid, len);
 	else
 		total = uio->uio_resid;
-	if (total > MHLEN)
+	if (align >= MHLEN)
+		goto nospace;
+	if (total + align > MHLEN)
 		m_final = m_getcl(how, MT_DATA, M_PKTHDR);
 	else
 		m_final = m_gethdr(how, MT_DATA);
 	if (m_final == NULL)
 		goto nospace;
+	m_final->m_data += align;
 	m_new = m_final;
 	while (progress < total) {
 		length = total - progress;
