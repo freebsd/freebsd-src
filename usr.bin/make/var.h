f81f62dc84a2e8acc48662d3f6d5b9896a60838a
harti <harti@FreeBSD.org>
2005-05-10 11:53:20 +0000

    Merge var_modify.c into var.c and move types and function declarations that are now used only in var.c from var.h to var.c
    Patches:	7.193,7.194
    Submitted by:	Max Okumoto <okumoto@ucsd.edu>

--- usr.bin/make/var.h
+++ usr.bin/make/var.h
@@ -42,53 +42,9 @@
 #ifndef var_h_9cccafce
 #define	var_h_9cccafce
 
-#include <regex.h>
-
-#include "config.h"
-
 struct GNode;
 struct Buffer;
 
-typedef struct Var {
-	char		*name;	/* the variable's name */
-	struct Buffer	*val;	/* its value */
-	int		flags;	/* miscellaneous status flags */
-
-#define	VAR_IN_USE	1	/* Variable's value currently being used.
-				 * Used to avoid recursion */
-
-#define	VAR_JUNK	4	/* Variable is a junk variable that
-				 * should be destroyed when done with
-				 * it. Used by Var_Parse for undefined,
-				 * modified variables */
-
-#define	VAR_TO_ENV	8	/* Place variable in environment */
-} Var;
-
-/* Var*Pattern flags */
-#define	VAR_SUB_GLOBAL	0x01	/* Apply substitution globally */
-#define	VAR_SUB_ONE	0x02	/* Apply substitution to one word */
-#define	VAR_SUB_MATCHED	0x04	/* There was a match */
-#define	VAR_MATCH_START	0x08	/* Match at start of word */
-#define	VAR_MATCH_END	0x10	/* Match at end of word */
-
-typedef struct {
-	struct Buffer	*lhs;	/* String to match */
-	struct Buffer	*rhs;	/* Replacement string (w/ &'s removed) */
-
-	regex_t			re;
-	int			nsub;
-	regmatch_t		*matches;
-
-	int	flags;
-} VarPattern;
-
-typedef Boolean VarModifyProc(const char *, Boolean, struct Buffer *, void *);
-
-/*
- * var.c
- */
-void VarREError(int, regex_t *, const char *);
 void Var_Append(const char *, const char *, struct GNode *);
 void Var_Delete(const char *, struct GNode *);
 void Var_Dump(void);
@@ -96,27 +52,10 @@ Boolean Var_Exists(const char *, struct GNode *);
 void Var_Init(char **);
 size_t Var_Match(const char [], struct GNode *);
 char *Var_Parse(const char *, struct GNode *, Boolean, size_t *, Boolean *);
-char *Var_Quote(const char *);
 void Var_Set(const char *, const char *, struct GNode *);
 void Var_SetEnv(const char *, struct GNode *);
 struct Buffer *Var_Subst(const char *, struct GNode *, Boolean);
 struct Buffer *Var_SubstOnly(const char *, const char *, struct GNode *, Boolean);
 char *Var_Value(const char *, struct GNode *, char **);
-
-/*
- * var_modify.c
- */
-VarModifyProc VarHead;
-VarModifyProc VarMatch;
-VarModifyProc VarNoMatch;
-VarModifyProc VarRESubstitute;
-VarModifyProc VarRoot;
-VarModifyProc VarSubstitute;
-VarModifyProc VarSuffix;
-VarModifyProc VarTail;
-
-#ifdef SYSVVARSUB
-VarModifyProc VarSYSVMatch;
-#endif
 
 #endif /* var_h_9cccafce */
