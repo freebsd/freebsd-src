42307a8f43e28c8a286aad36f21d647342ad9933
Tim Kientzle <kientzle@FreeBSD.org>
2005-05-11 03:47:48 +0000

    Since the FreeBSD local modifications are mostly trivial (consisting primarily of pointless $FreeBSD$ tags), sync most files in HEAD with those in the ZLIB branch.  This minimizes the differences between HEAD and ZLIB and should simplify future imports.
    After this, there are only three files with local modifications
    (gzio.c, minigzip.c, and zconf.h) and two non-vendor files
    (Makefile, zopen.c).  The rest exactly match the vendor distribution.
    
    PR:		i386/76294
    MFC after:	2 weeks

--- lib/libz/deflate.c
+++ lib/libz/deflate.c
@@ -1,10 +1,7 @@
 /* deflate.c -- compress data using the deflation algorithm
- * Copyright (C) 1995-2003 Jean-loup Gailly.
+ * Copyright (C) 1995-2004 Jean-loup Gailly.
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
-
-#include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
 
 /*
  *  ALGORITHM
@@ -50,12 +47,12 @@ __FBSDID("$FreeBSD$");
  *
  */
 
-/* @(#) $FreeBSD$ */
+/* @(#) $Id$ */
 
 #include "deflate.h"
 
 const char deflate_copyright[] =
-   " deflate 1.2.1 Copyright 1995-2003 Jean-loup Gailly ";
+   " deflate 1.2.2 Copyright 1995-2004 Jean-loup Gailly ";
 /*
   If you use the zlib library in a product, an acknowledgment is welcome
   in the documentation of your product. If for some reason you cannot
