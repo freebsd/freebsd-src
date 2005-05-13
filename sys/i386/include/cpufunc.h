84848a60632324b3a2cdca114d63cffd3483f9bd
nectar <nectar@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-05-13 00:10:56 +0000

    Add a knob for disabling/enabling HTT, "machdep.hyperthreading_allowed". Default off due to information disclosure on multi-user systems.
    Submitted by:	cperciva
    Reviewed by:	jhb
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@146170 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/include/cpufunc.h
+++ sys/i386/include/cpufunc.h
@@ -93,6 +93,14 @@ do_cpuid(u_int ax, u_int *p)
 }
 
 static __inline void
+cpuid_count(u_int ax, u_int cx, u_int *p)
+{
+	__asm __volatile("cpuid"
+			 : "=a" (p[0]), "=b" (p[1]), "=c" (p[2]), "=d" (p[3])
+			 :  "0" (ax), "c" (cx));
+}
+
+static __inline void
 enable_intr(void)
 {
 	__asm __volatile("sti");
