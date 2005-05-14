c971ca9be571572f89b298ccfbeabe0478a29a44
nyan <nyan@FreeBSD.org>
2005-05-14 10:51:16 +0000

    Fix my copyright.

--- sys/pc98/cbus/sio_cbus.c
+++ sys/pc98/cbus/sio_cbus.c
@@ -1,5 +1,6 @@
 /*-
- * Copyright (c) 2001 Yoshihiro TAKAHASHI.  All rights reserved.
+ * Copyright (c) 2001 TAKAHASHI Yoshihiro
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
