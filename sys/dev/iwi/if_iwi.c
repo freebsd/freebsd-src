6b1fe67d5999ef22ee13dbbb77f8f871669259a1
Warner Losh <imp@FreeBSD.org>
2005-05-15 21:02:51 +0000

    Print a warning once when trying to bring up interface before firmware load.

--- sys/dev/iwi/if_iwi.c
+++ sys/dev/iwi/if_iwi.c
@@ -2096,6 +2096,9 @@ iwi_init(void *priv)
 
 	/* exit immediately if firmware has not been ioctl'd */
 	if (!(sc->flags & IWI_FLAG_FW_CACHED)) {
+		if (!(sc->flags & IWI_FLAG_FW_WARNED))
+			device_printf(sc->sc_dev, "Please load firmware\n");
+		sc->flags |= IWI_FLAG_FW_WARNED;
 		ifp->if_flags &= ~IFF_UP;
 		return;
 	}
