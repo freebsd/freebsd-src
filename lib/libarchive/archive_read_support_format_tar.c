c18fffe04c40074d2f1735608365337744bee452
Tim Kientzle <kientzle@FreeBSD.org>
2005-05-16 02:47:34 +0000

    MFC a number of small changes from about 2 months ago:
     archive_write_open_file.c 1.11: Non-regular files can be added
             to an archive, even if output is going to that file.
     archive_read_support_compression_gzip.c 1.9: better error messages
     archive_read_open_file.c 1.8: better error messages
     archive_read_support_format_cpio.c 1.12: better handling of I/O errors
     archive_read_support_format_tar.c 1.31: better error handling

--- lib/libarchive/archive_read_support_format_tar.c
+++ lib/libarchive/archive_read_support_format_tar.c
@@ -1020,8 +1020,11 @@ pax_header(struct archive *a, struct tar *tar, struct archive_entry *entry,
 				return (-1);
 			line_length *= 10;
 			line_length += *p - '0';
-			if (line_length > 999999)
-				return (-1);
+			if (line_length > 999999) {
+				archive_set_error(a, ARCHIVE_ERRNO_MISC,
+				    "Rejecting pax extended attribute > 1MB");
+				return (ARCHIVE_WARN);
+			}
 			p++;
 			l--;
 		}
