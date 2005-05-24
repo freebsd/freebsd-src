0f18d3256d53f0cfa46cb62e07c766f740b9f13c
Olivier Houchard <cognet@FreeBSD.org>
2005-05-24 21:44:34 +0000

    Use asm versions of in_cksum() and friends.

--- sys/arm/include/in_cksum.h
+++ sys/arm/include/in_cksum.h
@@ -43,9 +43,19 @@
 
 #ifdef _KERNEL
 u_short in_cksum(struct mbuf *m, int len);
-u_int in_cksum_hdr(const struct ip *ip);
 u_short in_addword(u_short sum, u_short b);
 u_short in_pseudo(u_int sum, u_int b, u_int c);
 u_short in_cksum_skip(struct mbuf *m, int len, int skip);
+u_int do_cksum(const void *, int);
+static __inline u_int
+in_cksum_hdr(const struct ip *ip)
+{
+	u_int sum = do_cksum(ip, sizeof(struct ip));
+
+	sum = (sum & 0xffff) + (sum >> 16);
+	if (sum > 0xffff)
+		sum -= 0xffff;
+	return (~sum & 0xffff);
+}
 #endif /* _KERNEL */
 #endif /* _MACHINE_IN_CKSUM_H_ */
