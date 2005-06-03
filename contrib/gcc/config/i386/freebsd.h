441d901f19181afa0a66220d62778abd32c7e980
Alexander Kabaev <kan@FreeBSD.org>
2005-06-03 05:58:56 +0000

    Fix a typo from previos commit.

--- contrib/gcc/config/i386/freebsd.h
+++ contrib/gcc/config/i386/freebsd.h
@@ -22,7 +22,7 @@ along with GCC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
--/* $FreeBSD$ */
+/* $FreeBSD$ */
 
 #undef  CC1_SPEC
 #define CC1_SPEC "%(cc1_cpu) %{profile:-p}"
