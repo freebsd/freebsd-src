9fe02f7e1672d56139e3412536729b0a79fd8636
Jeff Roberson <jeff@FreeBSD.org>
2005-06-04 09:24:15 +0000

    - It's 2005 already, I've been working on this for three years.

--- sys/kern/sched_ule.c
+++ sys/kern/sched_ule.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2002-2003, Jeffrey Roberson <jeff@freebsd.org>
+ * Copyright (c) 2002-2005, Jeffrey Roberson <jeff@freebsd.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
