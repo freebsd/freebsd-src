7ebd858bb1c6df0fc851076d1b05bc0038ccaec0
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-06-05 22:53:09 +0000

    Power state management now done in PCI bus code
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@147030 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/usb/ohci_pci.c
+++ sys/dev/usb/ohci_pci.c
@@ -144,17 +144,6 @@ static int
 ohci_pci_resume(device_t self)
 {
 	ohci_softc_t *sc = device_get_softc(self);
-	u_int32_t reg, int_line;
-
-	if (pci_get_powerstate(self) != PCI_POWERSTATE_D0) {
-                device_printf(self, "chip is in D%d mode "
-                        "-- setting to D0\n", pci_get_powerstate(self));
-                reg = pci_read_config(self, PCI_CBMEM, 4);
-                int_line = pci_read_config(self, PCIR_INTLINE, 4);
-                pci_set_powerstate(self, PCI_POWERSTATE_D0);
-                pci_write_config(self, PCI_CBMEM, reg, 4);
-                pci_write_config(self, PCIR_INTLINE, int_line, 4);
-	}
 
 	ohci_power(PWR_RESUME, sc);
 	bus_generic_resume(self);
