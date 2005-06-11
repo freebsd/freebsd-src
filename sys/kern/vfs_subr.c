6ecbf73dab9bdff0b08946d3befbe399ca373b64
jeff <jeff@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-06-11 08:48:09 +0000

    - Assert that we're not in the name cache anymore in vdestroy().
    Sponsored by:	Isilon Systems, Inc.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@147297 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -723,6 +723,8 @@ vdestroy(struct vnode *vp)
 	VNASSERT(bo->bo_clean.bv_root == NULL, vp, ("cleanblkroot not NULL"));
 	VNASSERT(bo->bo_dirty.bv_cnt == 0, vp, ("dirtybufcnt not 0"));
 	VNASSERT(bo->bo_dirty.bv_root == NULL, vp, ("dirtyblkroot not NULL"));
+	VNASSERT(TAILQ_EMPTY(&vp->v_cache_dst), vp, ("vp has namecache dst"));
+	VNASSERT(LIST_EMPTY(&vp->v_cache_src), vp, ("vp has namecache src"));
 #ifdef MAC
 	mac_destroy_vnode(vp);
 #endif
