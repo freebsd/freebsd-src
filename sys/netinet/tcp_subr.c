55177f9e7f525553ea8cc07f70da71c66260e003
Robert Watson <rwatson@FreeBSD.org>
2005-06-14 11:59:46 +0000

    Merge tcp_subr.c:1.227 from HEAD to RELENG_5:
      Assert that tcbinfo is locked in tcp_input() before calling into
      tcp_drop().

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -652,7 +652,9 @@ tcp_drop(tp, errno)
 {
 	struct socket *so = tp->t_inpcb->inp_socket;
 
+	INP_INFO_WLOCK_ASSERT(&tcbinfo);
 	INP_LOCK_ASSERT(tp->t_inpcb);
+
 	if (TCPS_HAVERCVDSYN(tp->t_state)) {
 		tp->t_state = TCPS_CLOSED;
 		(void) tcp_output(tp);
@@ -773,6 +775,7 @@ tcp_close(tp)
 	struct socket *so = inp->inp_socket;
 #endif
 
+	INP_INFO_WLOCK_ASSERT(&tcbinfo);
 	INP_LOCK_ASSERT(inp);
 
 	tcp_discardcb(tp);
@@ -840,6 +843,7 @@ tcp_notify(inp, error)
 {
 	struct tcpcb *tp = (struct tcpcb *)inp->inp_ppcb;
 
+	INP_INFO_WLOCK_ASSERT(&tcbinfo);
 	INP_LOCK_ASSERT(inp);
 
 	/*
@@ -1440,7 +1444,9 @@ tcp_drop_syn_sent(inp, errno)
 {
 	struct tcpcb *tp = intotcpcb(inp);
 
+	INP_INFO_WLOCK_ASSERT(&tcbinfo);
 	INP_LOCK_ASSERT(inp);
+
 	if (tp != NULL && tp->t_state == TCPS_SYN_SENT) {
 		tcp_drop(tp, errno);
 		return (NULL);
