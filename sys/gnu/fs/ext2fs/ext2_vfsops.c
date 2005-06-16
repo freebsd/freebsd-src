1923d87e483f54a6c09155f3637d54d9869df31e
imp <imp@FreeBSD.org>
2005-06-16 06:51:38 +0000

    Add standard GPL boilerplate to these files.  They are the only ones contaminated with the GPL code.  While this information was present in the COPYRIGHT.INFO file, it is FreeBSD's standard practice to, where possible, include explicit license information in files.
    Approved by: release engineer (scottl)

--- sys/gnu/fs/ext2fs/ext2_vfsops.c
+++ sys/gnu/fs/ext2fs/ext2_vfsops.c
@@ -36,6 +36,24 @@
  * $FreeBSD$
  */
 
+/*-
+ * COPYRIGHT.INFO says this has some GPL'd code from ext2_super.c in it
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/namei.h>
--- sys/gnu/fs/ext2fs/sparc64-bitops.h
+++ sys/gnu/fs/ext2fs/sparc64-bitops.h
@@ -3,6 +3,20 @@
  * bitops.h: Bit string operations on the V9.
  *
  * Copyright 1996, 1997 David S. Miller (davem@caip.rutgers.edu)
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
  */
 
 /* $FreeBSD$ */
