67b9ea3a67a273a9c2a07737a5e8aca3e3059f1f
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-06-16 06:51:38 +0000

    Add standard GPL boilerplate to these files.  They are the only ones contaminated with the GPL code.  While this information was present in the COPYRIGHT.INFO file, it is FreeBSD's standard practice to, where possible, include explicit license information in files.
    Approved by: release engineer (scottl)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@147408 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/gnu/fs/ext2fs/ext2_fs_sb.h
+++ sys/gnu/fs/ext2fs/ext2_fs_sb.h
@@ -19,6 +19,20 @@
  *  linux/include/linux/minix_fs_sb.h
  *
  *  Copyright (C) 1991, 1992  Linus Torvalds
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
  */
 
 #ifndef _LINUX_EXT2_FS_SB
--- sys/gnu/fs/ext2fs/ext2_linux_balloc.c
+++ sys/gnu/fs/ext2fs/ext2_linux_balloc.c
@@ -15,6 +15,20 @@
  * Universite Pierre et Marie Curie (Paris VI)
  *
  *  Enhanced block allocation by Stephen Tweedie (sct@dcs.ed.ac.uk), 1993
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License..
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
  */
 
 /*
