ff3ee9ac58e71b987b55ac5fabed4755816b58b0
Bernd Walter <ticso@FreeBSD.org>
2005-06-17 14:15:27 +0000

    MFC: usbdevs Rev 1.227 device ID for FT2232 chips      uftdi.c Rev 1.22 support for FT2232 chips 	     Rev 1.21 remove useless usbd_errstr
    Reminded by: Mike Tancsa <mike@sentex.net>

--- sys/dev/usb/uftdi.c
+++ sys/dev/usb/uftdi.c
@@ -145,8 +145,12 @@ USB_MATCH(uftdi)
 {
 	USB_MATCH_START(uftdi, uaa);
 
-	if (uaa->iface != NULL)
+	if (uaa->iface != NULL) {
+		if (uaa->vendor == USB_VENDOR_FTDI &&
+		    (uaa->product == USB_PRODUCT_FTDI_SERIAL_2232C))
+			return (UMATCH_VENDOR_IFACESUBCLASS);
 		return (UMATCH_NONE);
+	}
 
 	DPRINTFN(20,("uftdi: vendor=0x%x, product=0x%x\n",
 		     uaa->vendor, uaa->product));
@@ -196,19 +200,23 @@ USB_ATTACH(uftdi)
 
 	devname = USBDEVNAME(ucom->sc_dev);
 
-	/* Move the device into the configured state. */
-	err = usbd_set_config_index(dev, UFTDI_CONFIG_INDEX, 1);
-	if (err) {
-		printf("\n%s: failed to set configuration, err=%s\n",
-		       devname, usbd_errstr(err));
-		goto bad;
-	}
+	if (uaa->iface == NULL) {
+		/* Move the device into the configured state. */
+		err = usbd_set_config_index(dev, UFTDI_CONFIG_INDEX, 1);
+		if (err) {
+			printf("\n%s: failed to set configuration, err=%s\n",
+			       devname, usbd_errstr(err));
+			goto bad;
+		}
 
-	err = usbd_device2interface_handle(dev, UFTDI_IFACE_INDEX, &iface);
-	if (err) {
-		printf("\n%s: failed to get interface, err=%s\n",
-		       devname, usbd_errstr(err));
-		goto bad;
+		err = usbd_device2interface_handle(dev, UFTDI_IFACE_INDEX, &iface);
+		if (err) {
+			printf("\n%s: failed to get interface, err=%s\n",
+			       devname, usbd_errstr(err));
+			goto bad;
+		}
+	} else {
+		iface = uaa->iface;
 	}
 
 	usbd_devinfo(dev, 0, devinfo);
@@ -226,6 +234,7 @@ USB_ATTACH(uftdi)
 			break;
 		case USB_PRODUCT_FTDI_SEMC_DSS20:
 		case USB_PRODUCT_FTDI_SERIAL_8U232AM:
+		case USB_PRODUCT_FTDI_SERIAL_2232C:
 		case USB_PRODUCT_FTDI_CFA_631:
 		case USB_PRODUCT_FTDI_CFA_632:
 		case USB_PRODUCT_FTDI_CFA_633:
@@ -279,8 +288,8 @@ USB_ATTACH(uftdi)
 		int addr, dir, attr;
 		ed = usbd_interface2endpoint_descriptor(iface, i);
 		if (ed == NULL) {
-			printf("%s: could not read endpoint descriptor"
-			       ": %s\n", devname, usbd_errstr(err));
+			printf("%s: could not read endpoint descriptor\n",
+			    devname);
 			goto bad;
 		}
 
@@ -307,7 +316,10 @@ USB_ATTACH(uftdi)
 		goto bad;
 	}
         ucom->sc_parent  = sc;
-	ucom->sc_portno = FTDI_PIT_SIOA;
+	if (uaa->iface == NULL)
+		ucom->sc_portno = FTDI_PIT_SIOA;
+	else
+		ucom->sc_portno = FTDI_PIT_SIOA + id->bInterfaceNumber;
 	/* bulkin, bulkout set above */
 
   	ucom->sc_ibufsize = UFTDIIBUFSIZE;
