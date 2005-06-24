d8c70ae2fc5d41eb0258c2b638b2573fce792e2b
Colin Percival <cperciva@FreeBSD.org>
2005-06-24 13:32:25 +0000

    MFC the addition of sha256 to libmd and md5(1).
    Also, bump __FreeBSD_version so that ports code knows that sha256 is
    now in the base system.
    
    Requested by:	Lots of people

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -55,7 +55,7 @@
  * scheme is:  <major><two digit minor><0 if release branch, otherwise 1>xx
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 504101	/* Master, propagated to newvers */
+#define __FreeBSD_version 504102	/* Master, propagated to newvers */
 
 #ifndef LOCORE
 #include <sys/types.h>
