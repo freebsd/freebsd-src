92023301a5f2cf88f8232733d7c1a824f8598c52
marcel <marcel@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-06-27 23:29:06 +0000

    Cosmetic: s/u_int64_t/uint64_t/g
    Approved by: re (scottl)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@147638 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/ia64/ia64/trap.c
+++ sys/ia64/ia64/trap.c
@@ -163,15 +163,15 @@ static const char *ia64_vector_names[] = {
 };
 
 struct bitname {
-	u_int64_t mask;
+	uint64_t mask;
 	const char* name;
 };
 
 static void
-printbits(u_int64_t mask, struct bitname *bn, int count)
+printbits(uint64_t mask, struct bitname *bn, int count)
 {
 	int i, first = 1;
-	u_int64_t bit;
+	uint64_t bit;
 
 	for (i = 0; i < count; i++) {
 		/*
@@ -230,7 +230,7 @@ struct bitname psr_bits[] = {
 };
 
 static void
-printpsr(u_int64_t psr)
+printpsr(uint64_t psr)
 {
 	printbits(psr, psr_bits, sizeof(psr_bits)/sizeof(psr_bits[0]));
 }
@@ -251,7 +251,7 @@ struct bitname isr_bits[] = {
 	{IA64_ISR_ED,	"ed"},
 };
 
-static void printisr(u_int64_t isr)
+static void printisr(uint64_t isr)
 {
 	printbits(isr, isr_bits, sizeof(isr_bits)/sizeof(isr_bits[0]));
 }
@@ -326,7 +326,7 @@ trap(int vector, struct trapframe *tf)
 {
 	struct proc *p;
 	struct thread *td;
-	u_int64_t ucode;
+	uint64_t ucode;
 	int error, sig, user;
 	u_int sticks;
 
@@ -887,7 +887,7 @@ syscall(struct trapframe *tf)
 	struct sysent *callp;
 	struct proc *p;
 	struct thread *td;
-	u_int64_t *args;
+	uint64_t *args;
 	int code, error;
 	u_int sticks;
 
