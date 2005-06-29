9ce41d76590cdda06c09d0160744666540d1a6e7
Simon L. B. Nielsen <simon@FreeBSD.org>
2005-06-29 21:38:48 +0000

    Correct ipfw packet matching errors with address tables.
    Security:	CAN-2005-2019
    Security:	FreeBSD-SA-05:13.ipfw
    
    Correct bzip2 denial of service and permission race vulnerabilities.
    
    Obtained from:	Redhat, Steve Grubb via RedHat
    Security:	CAN-2005-0953, CAN-2005-1260
    Security:	FreeBSD-SA-05:14.bzip2
    Approved by:	obrien
    
    Correct TCP connection stall denial of service vulnerability.
    
    A TCP packets with the SYN flag set is accepted for established
    connections, allowing an attacker to overwrite certain TCP options.
    
    Security:	CAN-2005-2068
    Security:	FreeBSD-SA-05:15.tcp
    
    Approved by:	cperciva

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -1086,7 +1086,7 @@ after_listen:
 	 * XXX this is traditional behavior, may need to be cleaned up.
 	 */
 	tcp_dooptions(tp, &to, optp, optlen, thflags & TH_SYN, th);
-	if (thflags & TH_SYN) {
+	if (tp->t_state == TCPS_SYN_SENT && (thflags & TH_SYN)) {
 		if (to.to_flags & TOF_SCALE) {
 			tp->t_flags |= TF_RCVD_SCALE;
 			tp->requested_s_scale = to.to_requested_s_scale;
