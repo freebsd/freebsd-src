028dbfaa3e3c553592d8c7e3d58f1c0c23848fda
Scott Long <scottl@FreeBSD.org>
2005-07-11 08:34:49 +0000

    Make the RELENG_6 branch operation official by moving HEAD to 7.0-CURRENT.
    Approved by: re

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -57,7 +57,7 @@
  *		is created, otherwise 1.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 600032	/* Master, propagated to newvers */
+#define __FreeBSD_version 700000	/* Master, propagated to newvers */
 
 #ifndef LOCORE
 #include <sys/types.h>
