6c8ec4b833b3935d80b063c47cf9aed4fe8f6732
marcus <marcus@FreeBSD.org>
2005-07-13 17:10:47 +0000

    Replace fch{mod,own} with straight ch{mod,own} as the former cannot be used on socket file descriptors.
    Reviewed by:	secteam (cperciva)
    Approved by:	imp

--- sbin/devd/devd.cc
+++ sbin/devd/devd.cc
@@ -675,8 +675,8 @@ create_socket(const char *name)
 	if (bind(fd, (struct sockaddr *) & sun, slen) < 0)
 		err(1, "bind");
 	listen(fd, 4);
-	fchown(fd, 0, 0);	/* XXX - root.wheel */
-	fchmod(fd, 0660);
+	chown(name, 0, 0);	/* XXX - root.wheel */
+	chmod(name, 0660);
 	return (fd);
 }
 
