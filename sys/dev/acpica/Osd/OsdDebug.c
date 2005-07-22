47609cfae1f15ac4491383e36c166a27feabcb38
njl <njl@FreeBSD.org>
2005-07-22 23:10:02 +0000

    Standardize __FBSDID identifiers.
    MFC after:	2 days

--- sys/dev/acpica/Osd/OsdDebug.c
+++ sys/dev/acpica/Osd/OsdDebug.c
@@ -23,13 +23,14 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- *	$FreeBSD$
  */
 
 /*
  * 6.8 : Debugging support
  */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #include "opt_ddb.h"
 #include <sys/param.h>
