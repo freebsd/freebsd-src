8ab23e147b5173ac988be1925245f3b0c1f73bac
sam <sam@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-07-22 17:54:40 +0000

    simplify ieee80211_ibss_merge api
    MFC after:	3 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@148306 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net80211/ieee80211_node.h
+++ sys/net80211/ieee80211_node.h
@@ -191,7 +191,7 @@ void	ieee80211_create_ibss(struct ieee80211com*, struct ieee80211_channel *);
 void	ieee80211_reset_bss(struct ieee80211com *);
 void	ieee80211_cancel_scan(struct ieee80211com *);
 void	ieee80211_end_scan(struct ieee80211com *);
-int	ieee80211_ibss_merge(struct ieee80211com *, struct ieee80211_node *);
+int	ieee80211_ibss_merge(struct ieee80211_node *);
 int	ieee80211_sta_join(struct ieee80211com *, struct ieee80211_node *);
 void	ieee80211_sta_leave(struct ieee80211com *, struct ieee80211_node *);
 
