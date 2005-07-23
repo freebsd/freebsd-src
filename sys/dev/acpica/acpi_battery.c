76f6fe4f31fa9c97f4a6f7713149ed9950323286
Nate Lawson <njl@FreeBSD.org>
2005-07-23 19:36:00 +0000

    Rewrite the acpi_battery interface to allow for other battery types (i.e., smart battery) and fix various bugs found during the cleanup.
    API changes:
    * kernel access:
    Access to individual batteries is now via devclass_find("battery").
    Introduce new methods ACPI_BATT_GET_STATUS (for _BST-formatted data) and
    ACPI_BATT_GET_INFO (for _BIF-formatted data).  The helper function
    acpi_battery_get_battinfo() now takes a device_t instead of a unit #
    argument.  If dev is NULL, this signifies all batteries.
    
    * ioctl access:
    The ACPIIO_BATT_GET_TYPE and ACPIIO_BATT_GET_BATTDESC ioctls have been
    removed.  Since there is now no need for a mapping between "virtual" unit
    and physical unit, usermode programs can just specify the unit directly and
    skip the old translation steps.  In fact, acpiconf(8) was actually already
    doing this and virtual unit was the same as physical unit in all cases
    since there was previously only one battery type (acpi_cmbat).  Additionally,
    we now map the ACPIIO_BATT_GET_BIF and ACPIIO_BATT_GET_BST ioctls for all
    batteries, if they provide the associated methods.
    
    * apm compatibility device/ioctls:  no change
    * sysctl:  no change
    
    Since most third-party applications use the apm(4) compat interface, there
    should be very few affected applications (if any).
    
    Reviewed by:	bruno
    MFC after:	5 days

--- sys/dev/acpica/acpi_battery.c
+++ sys/dev/acpica/acpi_battery.c
@@ -1,4 +1,5 @@
 /*-
+ * Copyright (c) 2005 Nate Lawson
  * Copyright (c) 2000 Mitsuru IWASAKI <iwasaki@jp.freebsd.org>
  * All rights reserved.
  *
@@ -39,86 +40,232 @@ __FBSDID("$FreeBSD$");
 #include <dev/acpica/acpivar.h>
 #include <dev/acpica/acpiio.h>
 
-MALLOC_DEFINE(M_ACPIBATT, "acpibatt", "ACPI generic battery data");
+/* Default seconds before re-sampling the battery state. */
+#define	ACPI_BATTERY_INFO_EXPIRE	5
 
-struct acpi_batteries {
-    TAILQ_ENTRY(acpi_batteries)	link;
-    struct acpi_battdesc	battdesc;
-};
+static int	acpi_batteries_initted;
+static int	acpi_battery_info_expire = ACPI_BATTERY_INFO_EXPIRE;
+static struct	acpi_battinfo	acpi_battery_battinfo;
+static struct	sysctl_ctx_list	acpi_battery_sysctl_ctx;
+static struct	sysctl_oid	*acpi_battery_sysctl_tree;
 
-static TAILQ_HEAD(,acpi_batteries) acpi_batteries;
-static int			acpi_batteries_initted;
-static int			acpi_batteries_units;
-static int			acpi_battery_info_expire = 5;
-static struct acpi_battinfo	acpi_battery_battinfo;
 ACPI_SERIAL_DECL(battery, "ACPI generic battery");
 
-int
-acpi_battery_get_info_expire(void)
-{
-    return (acpi_battery_info_expire);
-}
+static void acpi_reset_battinfo(struct acpi_battinfo *info);
+static int acpi_battery_ioctl(u_long cmd, caddr_t addr, void *arg);
+static int acpi_battery_sysctl(SYSCTL_HANDLER_ARGS);
+static int acpi_battery_units_sysctl(SYSCTL_HANDLER_ARGS);
+static int acpi_battery_init(void);
 
 int
-acpi_battery_get_units(void)
+acpi_battery_register(device_t dev)
 {
-    return (acpi_batteries_units);
-}
+    int error;
 
-int
-acpi_battery_get_battdesc(int unit, struct acpi_battdesc *battdesc)
-{
-    struct acpi_batteries *bp;
-    int error, i;
-
-    error = ENXIO;
+    error = 0;
     ACPI_SERIAL_BEGIN(battery);
-    if (unit < 0 || unit >= acpi_batteries_units)
-	goto out;
-
-    i = 0;
-    TAILQ_FOREACH(bp, &acpi_batteries, link) {
-	if (unit == i) {
-	    battdesc->type = bp->battdesc.type;
-	    battdesc->phys_unit = bp->battdesc.phys_unit;
-	    error = 0;
-	    break;
-	}
-	i++;
-    }
-
-out:
+    if (!acpi_batteries_initted)
+	error = acpi_battery_init();
     ACPI_SERIAL_END(battery);
     return (error);
 }
 
 int
-acpi_battery_get_battinfo(int unit, struct acpi_battinfo *battinfo)
+acpi_battery_remove(device_t dev)
 {
-    struct acpi_battdesc battdesc;
-    int error;
 
-    error = 0;
-    if (unit == -1) {
-	error = acpi_cmbat_get_battinfo(-1, battinfo);
-	goto out;
-    } else {
-	error = acpi_battery_get_battdesc(unit, &battdesc);
-	if (error != 0)
+    return (0);
+}
+
+int
+acpi_battery_get_units(void)
+{
+    devclass_t batt_dc;
+
+    batt_dc = devclass_find("battery");
+    if (batt_dc == NULL)
+	return (0);
+    return (devclass_get_count(batt_dc));
+}
+
+int
+acpi_battery_get_info_expire(void)
+{
+
+    return (acpi_battery_info_expire);
+}
+
+/* Check _BST results for validity. */
+int
+acpi_battery_bst_valid(struct acpi_bst *bst)
+{
+    if (bst->state >= ACPI_BATT_STAT_MAX || bst->cap == 0xffffffff ||
+	bst->volt == 0xffffffff)
+	return (FALSE);
+    else
+	return (TRUE);
+}
+
+/* Check _BIF results for validity. */
+int
+acpi_battery_bif_valid(struct acpi_bif *bif)
+{
+    if (bif->lfcap == 0)
+	return (FALSE);
+    else
+	return (TRUE);
+}
+
+/* Get info about one or all batteries. */
+int
+acpi_battery_get_battinfo(device_t dev, struct acpi_battinfo *battinfo)
+{
+    int	batt_stat, devcount, dev_idx, error, i;
+    int total_cap, total_min, valid_rate, valid_units;
+    devclass_t batt_dc;
+    device_t batt_dev;
+    struct acpi_bst *bst;
+    struct acpi_bif *bif;
+    struct acpi_battinfo *bi;
+
+    /*
+     * Get the battery devclass and number of devices.  If there are none
+     * or error, return immediately.
+     */
+    batt_dc = devclass_find("battery");
+    if (batt_dc == NULL)
+	return (ENXIO);
+    devcount = devclass_get_count(batt_dc);
+    if (devcount == 0)
+	return (ENXIO);
+
+    /*
+     * Allocate storage for all _BST data, their derived battinfo data,
+     * and the current battery's _BIF data.
+     */
+    bst = malloc(devcount * sizeof(*bst), M_TEMP, M_WAITOK);
+    bi = malloc(devcount * sizeof(*bi), M_TEMP, M_WAITOK);
+    bif = malloc(sizeof(*bif), M_TEMP, M_WAITOK);
+
+    /*
+     * Pass 1:  for each battery that is present and valid, get its status,
+     * calculate percent capacity remaining, and sum all the current
+     * discharge rates.
+     */
+    dev_idx = -1;
+    batt_stat = valid_rate = valid_units = 0;
+    for (i = 0; i < devcount; i++) {
+	/* Find the device.  If it disappeared, the user can try again. */
+	batt_dev = devclass_get_device(batt_dc, i);
+	if (batt_dev == NULL) {
+	    error = ENOMEM;
 	    goto out;
-
-	switch (battdesc.type) {
-	case ACPI_BATT_TYPE_CMBAT:
-	    error = acpi_cmbat_get_battinfo(battdesc.phys_unit, battinfo);
-	    break;
-	default:
-	    error = ENXIO;
-	    break;
 	}
+
+	/* Default info for every battery is "not present". */
+	acpi_reset_battinfo(&bi[i]);
+
+	/* If examining a specific battery and this is it, record its index. */
+	if (dev != NULL && dev == batt_dev)
+	    dev_idx = i;
+
+	/* Be sure we can get various info from the battery. */
+	if (!acpi_BatteryIsPresent(batt_dev) ||
+	    ACPI_BATT_GET_STATUS(batt_dev, &bst[i]) != 0 ||
+	    ACPI_BATT_GET_INFO(batt_dev, bif) != 0)
+	    continue;
+
+	/* If a battery is not installed, we sometimes get strange values. */
+	if (!acpi_battery_bst_valid(&bst[i]) ||
+	    !acpi_battery_bif_valid(bif))
+	    continue;
+
+	/* Record state and calculate percent capacity remaining. */
+	valid_units++;
+	batt_stat |= bst[i].state;
+	bi[i].state = bst[i].state;
+	bi[i].cap = 100 * bst[i].cap / bif->lfcap;
+
+	/*
+	 * Some laptops report the "design-capacity" instead of the
+	 * "real-capacity" when the battery is fully charged.  That breaks
+	 * the above arithmetic as it needs to be 100% maximum.
+	 */
+	if (bi[i].cap > 100)
+	    bi[i].cap = 100;
+
+	/*
+	 * On systems with more than one battery, they may get used
+	 * sequentially, thus bst.rate may only signify the one currently
+	 * in use.  For the remaining batteries, bst.rate will be zero,
+	 * which makes it impossible to calculate the total remaining time.
+	 * Therefore, we sum the bst.rate for batteries in the discharging
+	 * state and use the sum to calculate the total remaining time.
+	 */
+	if (bst[i].rate > 0 && (bst[i].state & ACPI_BATT_STAT_DISCHARG))
+	    valid_rate += bst[i].rate;
     }
 
+    /* If the caller asked for a device but we didn't find it, error. */
+    if (dev != NULL && dev_idx < 0) {
+	error = ENXIO;
+	goto out;
+    }
+
+    /* Pass 2:  calculate capacity and remaining time for all batteries. */
+    total_cap = total_min = 0;
+    for (i = 0; i < devcount; i++) {
+	/*
+	 * If any batteries are discharging, use the sum of the bst.rate
+	 * values.  Otherwise, we are on AC power, and there is infinite
+	 * time remaining for this battery until we go offline.
+	 */
+	if (valid_rate > 0)
+	    bi[i].min = 60 * bst[i].cap / valid_rate;
+	else
+	    bi[i].min = 0;
+	total_min += bi[i].min;
+	total_cap += bi[i].cap;
+    }
+
+    /*
+     * Return total battery percent and time remaining.  If there are
+     * no valid batteries, report values as unknown.
+     */
+    if (valid_units > 0) {
+	if (dev == NULL) {
+	    battinfo->cap = total_cap / valid_units;
+	    battinfo->min = total_min;
+	    battinfo->state = batt_stat;
+	    battinfo->rate = valid_rate;
+	} else {
+	    battinfo->cap = bi[dev_idx].cap;
+	    battinfo->min = bi[dev_idx].min;
+	    battinfo->state = bi[dev_idx].state;
+	    battinfo->rate = bst[dev_idx].rate;
+	}
+    } else
+	acpi_reset_battinfo(battinfo);
+
+    error = 0;
+
 out:
+    if (bi)
+	free(bi, M_TEMP);
+    if (bif)
+	free(bif, M_TEMP);
+    if (bst)
+	free(bst, M_TEMP);
     return (error);
+}
+
+static void
+acpi_reset_battinfo(struct acpi_battinfo *info)
+{
+    info->cap = -1;
+    info->min = -1;
+    info->state = ACPI_BATT_STAT_NOT_PRESENT;
+    info->rate = -1;
 }
 
 static int
@@ -126,9 +273,15 @@ acpi_battery_ioctl(u_long cmd, caddr_t addr, void *arg)
 {
     union acpi_battery_ioctl_arg *ioctl_arg;
     int error, unit;
+    device_t dev;
 
+    error = ENXIO;
     ioctl_arg = (union acpi_battery_ioctl_arg *)addr;
-    error = 0;
+    unit = ioctl_arg->unit;
+    if (unit != ACPI_BATTERY_ALL_UNITS)
+	dev = devclass_get_device(devclass_find("battery"), unit);
+    else
+	dev = NULL;
 
     /*
      * No security check required: information retrieval only.  If
@@ -138,17 +291,20 @@ acpi_battery_ioctl(u_long cmd, caddr_t addr, void *arg)
     case ACPIIO_BATT_GET_UNITS:
 	*(int *)addr = acpi_battery_get_units();
 	break;
-    case ACPIIO_BATT_GET_BATTDESC:
-	unit = ioctl_arg->unit;
-	error = acpi_battery_get_battdesc(unit, &ioctl_arg->battdesc);
-	break;
     case ACPIIO_BATT_GET_BATTINFO:
-	unit = ioctl_arg->unit;
-	error = acpi_battery_get_battinfo(unit, &ioctl_arg->battinfo);
+	if (dev != NULL || unit == ACPI_BATTERY_ALL_UNITS)
+	    error = acpi_battery_get_battinfo(dev, &ioctl_arg->battinfo);
+	break;
+    case ACPIIO_BATT_GET_BIF:
+	if (dev != NULL)
+	    error = ACPI_BATT_GET_INFO(dev, &ioctl_arg->bif);
+	break;
+    case ACPIIO_BATT_GET_BST:
+	if (dev != NULL)
+	    error = ACPI_BATT_GET_STATUS(dev, &ioctl_arg->bst);
 	break;
     default:
 	error = EINVAL;
-	break;
     }
 
     return (error);
@@ -159,9 +315,19 @@ acpi_battery_sysctl(SYSCTL_HANDLER_ARGS)
 {
     int val, error;
 
-    acpi_battery_get_battinfo(-1, &acpi_battery_battinfo);
+    acpi_battery_get_battinfo(NULL, &acpi_battery_battinfo);
     val = *(u_int *)oidp->oid_arg1;
     error = sysctl_handle_int(oidp, &val, 0, req);
+    return (error);
+}
+
+static int
+acpi_battery_units_sysctl(SYSCTL_HANDLER_ARGS)
+{
+    int count, error;
+
+    count = acpi_battery_get_units();
+    error = sysctl_handle_int(oidp, &count, 0, req);
     return (error);
 }
 
@@ -180,14 +346,7 @@ acpi_battery_init(void)
 	goto out;
     sc = device_get_softc(dev);
 
-    TAILQ_INIT(&acpi_batteries);
-
-    /* XXX We should back out registered ioctls on error. */
     error = acpi_register_ioctl(ACPIIO_BATT_GET_UNITS, acpi_battery_ioctl,
-	NULL);
-    if (error != 0)
-	goto out;
-    error = acpi_register_ioctl(ACPIIO_BATT_GET_BATTDESC, acpi_battery_ioctl,
 	NULL);
     if (error != 0)
 	goto out;
@@ -195,83 +354,46 @@ acpi_battery_init(void)
 	NULL);
     if (error != 0)
 	goto out;
+    error = acpi_register_ioctl(ACPIIO_BATT_GET_BIF, acpi_battery_ioctl, NULL);
+    if (error != 0)
+	goto out;
+    error = acpi_register_ioctl(ACPIIO_BATT_GET_BST, acpi_battery_ioctl, NULL);
+    if (error != 0)
+	goto out;
 
-    sysctl_ctx_init(&sc->acpi_battery_sysctl_ctx);
-    sc->acpi_battery_sysctl_tree = SYSCTL_ADD_NODE(&sc->acpi_battery_sysctl_ctx,
+    sysctl_ctx_init(&acpi_battery_sysctl_ctx);
+    acpi_battery_sysctl_tree = SYSCTL_ADD_NODE(&acpi_battery_sysctl_ctx,
 	SYSCTL_CHILDREN(sc->acpi_sysctl_tree), OID_AUTO, "battery", CTLFLAG_RD,
 	0, "");
-    SYSCTL_ADD_PROC(&sc->acpi_battery_sysctl_ctx,
-	SYSCTL_CHILDREN(sc->acpi_battery_sysctl_tree),
+    SYSCTL_ADD_PROC(&acpi_battery_sysctl_ctx,
+	SYSCTL_CHILDREN(acpi_battery_sysctl_tree),
 	OID_AUTO, "life", CTLTYPE_INT | CTLFLAG_RD,
 	&acpi_battery_battinfo.cap, 0, acpi_battery_sysctl, "I", "");
-    SYSCTL_ADD_PROC(&sc->acpi_battery_sysctl_ctx,
-	SYSCTL_CHILDREN(sc->acpi_battery_sysctl_tree),
+    SYSCTL_ADD_PROC(&acpi_battery_sysctl_ctx,
+	SYSCTL_CHILDREN(acpi_battery_sysctl_tree),
 	OID_AUTO, "time", CTLTYPE_INT | CTLFLAG_RD,
 	&acpi_battery_battinfo.min, 0, acpi_battery_sysctl, "I", "");
-    SYSCTL_ADD_PROC(&sc->acpi_battery_sysctl_ctx,
-	SYSCTL_CHILDREN(sc->acpi_battery_sysctl_tree),
+    SYSCTL_ADD_PROC(&acpi_battery_sysctl_ctx,
+	SYSCTL_CHILDREN(acpi_battery_sysctl_tree),
 	OID_AUTO, "state", CTLTYPE_INT | CTLFLAG_RD,
 	&acpi_battery_battinfo.state, 0, acpi_battery_sysctl, "I", "");
-    SYSCTL_ADD_INT(&sc->acpi_battery_sysctl_ctx,
-	SYSCTL_CHILDREN(sc->acpi_battery_sysctl_tree),
-	OID_AUTO, "units", CTLFLAG_RD, &acpi_batteries_units, 0, "");
-    SYSCTL_ADD_INT(&sc->acpi_battery_sysctl_ctx,
-	SYSCTL_CHILDREN(sc->acpi_battery_sysctl_tree),
+    SYSCTL_ADD_PROC(&acpi_battery_sysctl_ctx,
+	SYSCTL_CHILDREN(acpi_battery_sysctl_tree),
+	OID_AUTO, "units", CTLTYPE_INT | CTLFLAG_RD,
+	NULL, 0, acpi_battery_units_sysctl, "I", "");
+    SYSCTL_ADD_INT(&acpi_battery_sysctl_ctx,
+	SYSCTL_CHILDREN(acpi_battery_sysctl_tree),
 	OID_AUTO, "info_expire", CTLFLAG_RD | CTLFLAG_RW,
 	&acpi_battery_info_expire, 0, "");
 
     acpi_batteries_initted = TRUE;
 
 out:
-    return (error);
-}
-
-int
-acpi_battery_register(int type, int phys_unit)
-{
-    struct acpi_batteries *bp;
-    int error;
-
-    error = 0;
-    bp = malloc(sizeof(*bp), M_ACPIBATT, M_NOWAIT);
-    if (bp == NULL)
-	return (ENOMEM);
-
-    ACPI_SERIAL_BEGIN(battery);
-    if (!acpi_batteries_initted && (error = acpi_battery_init()) != 0) {
-	printf("acpi_battery_register failed for unit %d\n", phys_unit);
-	goto out;
+    if (error != 0) {
+	acpi_deregister_ioctl(ACPIIO_BATT_GET_UNITS, acpi_battery_ioctl);
+	acpi_deregister_ioctl(ACPIIO_BATT_GET_BATTINFO, acpi_battery_ioctl);
+	acpi_deregister_ioctl(ACPIIO_BATT_GET_BIF, acpi_battery_ioctl);
+	acpi_deregister_ioctl(ACPIIO_BATT_GET_BST, acpi_battery_ioctl);
     }
-    bp->battdesc.type = type;
-    bp->battdesc.phys_unit = phys_unit;
-    TAILQ_INSERT_TAIL(&acpi_batteries, bp, link);
-    acpi_batteries_units++;
-
-out:
-    ACPI_SERIAL_END(battery);
-    if (error)
-	free(bp, M_ACPIBATT);
     return (error);
-}
-
-int
-acpi_battery_remove(int type, int phys_unit)
-{
-    struct acpi_batteries *bp, *tmp;
-    int ret;
-
-    ret = ENOENT;
-    ACPI_SERIAL_BEGIN(battery);
-    TAILQ_FOREACH_SAFE(bp, &acpi_batteries, link, tmp) {
-	if (bp->battdesc.type == type && bp->battdesc.phys_unit == phys_unit) {
-	    TAILQ_REMOVE(&acpi_batteries, bp, link);
-	    acpi_batteries_units--;
-	    ret = 0;
-	    break;
-	}
-    }
-    ACPI_SERIAL_END(battery);
-    if (ret == 0)
-	free(bp, M_ACPIBATT);
-    return (ret);
 }
