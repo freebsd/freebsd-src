08429d886646fc02159f2f90529d4073a5e11ee8
ups <ups@FreeBSD.org>
2005-07-31 20:24:14 +0000

    Delay freeing disk space for file system blocks until all dirty buffers are safely released. This fixes softdep problems on truncation (deletion) of files with dirty buffers.
    Reviewed by:	jeff@, mckusick@, ps@, tegge@
    Tested by: 	glebius@, ps@
    MFC after:	3 weeks

--- sys/ufs/ffs/softdep.h
+++ sys/ufs/ffs/softdep.h
@@ -429,6 +429,7 @@ struct freefrag {
  */
 struct freeblks {
 	struct	worklist fb_list;	/* id_inowait or delayed worklist */
+#	define	fb_state fb_list.wk_state /* inode and dirty block state */
 	ino_t	fb_previousinum;	/* inode of previous owner of blocks */
 	uid_t	fb_uid;			/* uid of previous owner of blocks */
 	struct	vnode *fb_devvp;	/* filesystem device vnode */
