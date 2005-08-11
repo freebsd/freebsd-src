a3afd21fc8c00774c0c48131088d2a6b1c88553c
njl <njl@FreeBSD.org>
2005-08-11 17:12:57 +0000

    Modify the fix in rev 1.16 slightly.  Instead of setting the cap to zero, just skip summing it if it's unknown.
    MFC after:	1 day

--- sys/dev/acpica/acpi_battery.c
+++ sys/dev/acpica/acpi_battery.c
@@ -227,9 +227,8 @@ acpi_battery_get_battinfo(device_t dev, struct acpi_battinfo *battinfo)
 	total_min += bi[i].min;
 
 	/* If this battery is not present, don't use its capacity. */
-	if (bi[i].cap == -1)
-	    bi[i].cap = 0;
-	total_cap += bi[i].cap;
+	if (bi[i].cap != -1)
+	    total_cap += bi[i].cap;
     }
 
     /*
