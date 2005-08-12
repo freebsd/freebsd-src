10f961b659e59b13a4c22509c66899ff5c0f018a
njl <njl@FreeBSD.org>
2005-08-12 17:29:58 +0000

    MFC 1.18: Fix for 99% being reported instead of 100% when one battery is missing.
    Approved by:	re (kensmith)

--- sys/dev/acpica/acpi_battery.c
+++ sys/dev/acpica/acpi_battery.c
@@ -225,7 +225,10 @@ acpi_battery_get_battinfo(device_t dev, struct acpi_battinfo *battinfo)
 	else
 	    bi[i].min = 0;
 	total_min += bi[i].min;
-	total_cap += bi[i].cap;
+
+	/* If this battery is not present, don't use its capacity. */
+	if (bi[i].cap != -1)
+	    total_cap += bi[i].cap;
     }
 
     /*
