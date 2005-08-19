03eacec47490d5a892745cc22df6bfb10436d372
pjd <pjd@FreeBSD.org>
2005-08-19 22:10:19 +0000

    Avoid code duplication and implement bitcount32() function in systm.h only.
    Reviewed by:	cperciva
    MFC after:	3 days

--- sys/geom/stripe/g_stripe.h
+++ sys/geom/stripe/g_stripe.h
@@ -120,10 +120,4 @@ stripe_metadata_decode(const u_char *data, struct g_stripe_metadata *md)
 	md->md_provsize = le64dec(data + 64);
 }
 
-#ifndef BITCOUNT
-#define	BITCOUNT(x)	(((BX_(x) + (BX_(x) >> 4)) & 0x0F0F0F0F) % 255)
-#define	BX_(x)		((x) - (((x) >> 1) & 0x77777777) -		\
-			 (((x) >> 2) & 0x33333333) - (((x) >> 3) & 0x11111111))
-#endif
-
 #endif	/* _G_STRIPE_H_ */
