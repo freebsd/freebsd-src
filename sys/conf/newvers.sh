a0c5d836f0b5a4546572ae6fef623a0af5512dd5
cperciva <cperciva@FreeBSD.org>
2005-08-19 04:12:41 +0000

    MFC: Pad the strings sccs[], version[], and osrelease[], and add a BRANCH_OVERRIDE environment variable (which, if set, overrides the BRANCH specified here).  These make my FreeBSD Update build code simpler by making it easier to recognize when two kernels are identical modulo version numbers.
    Approved by:	re (kensmith)

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -33,6 +33,9 @@
 TYPE="FreeBSD"
 REVISION="6.0"
 BRANCH="BETA2"
+if [ "X${BRANCH_OVERRIDE}" != "X" ]; then
+	BRANCH=${BRANCH_OVERRIDE}
+fi
 RELEASE="${REVISION}-${BRANCH}"
 VERSION="${TYPE} ${RELEASE}"
 
@@ -85,10 +88,14 @@ v=`cat version` u=${USER:-root} d=`pwd` h=${HOSTNAME:-`hostname`} t=`date`
 i=`${MAKE:-make} -V KERN_IDENT`
 cat << EOF > vers.c
 $COPYRIGHT
-char sccs[] =  "@(#)${VERSION} #${v}: ${t}";
-char version[] = "${VERSION} #${v}: ${t}\\n    ${u}@${h}:${d}\\n";
+#define SCCSSTR "@(#)${VERSION} #${v}: ${t}"
+#define VERSTR "${VERSION} #${v}: ${t}\\n    ${u}@${h}:${d}\\n"
+#define RELSTR "${RELEASE}"
+
+char sccs[sizeof(SCCSSTR) > 128 ? sizeof(SCCSSTR) : 128] = SCCSSTR;
+char version[sizeof(VERSTR) > 256 ? sizeof(VERSTR) : 256] = VERSTR;
 char ostype[] = "${TYPE}";
-char osrelease[] = "${RELEASE}";
+char osrelease[sizeof(RELSTR) > 32 ? sizeof(RELSTR) : 32] = RELSTR;
 int osreldate = ${RELDATE};
 char kern_ident[] = "${i}";
 EOF
