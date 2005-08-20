406656fdb8461c1f415bd8445528bbe4ce4f0cab
stefanf <stefanf@FreeBSD.org>
2005-08-20 13:11:17 +0000

    Remove a stale occurrence of 'alpha' in a comment.

--- sys/sparc64/include/_limits.h
+++ sys/sparc64/include/_limits.h
@@ -73,7 +73,7 @@
 #define	__OFF_MAX	__LONG_MAX	/* max value for an off_t */
 #define	__OFF_MIN	__LONG_MIN	/* min value for an off_t */
 
-/* Quads and longs are the same on the alpha.  Ensure they stay in sync. */
+/* Quads and longs are the same size.  Ensure they stay in sync. */
 #define	__UQUAD_MAX	(__ULONG_MAX)	/* max value for a uquad_t */
 #define	__QUAD_MAX	(__LONG_MAX)	/* max value for a quad_t */
 #define	__QUAD_MIN	(__LONG_MIN)	/* min value for a quad_t */
