d1f4ea6cf3381ff9c223d748710878cb006c489a
sos <sos@FreeBSD.org>
2005-08-23 08:53:01 +0000

    Apply fix for "pr82261 DMA-support on Sparc64 broken" The Acer chip or wiring that SUN uses has problems that this patch tries to work around. Original patch by Marius Strobl, hacked into shape by me..

--- sys/dev/ata/ata-pci.h
+++ sys/dev/ata/ata-pci.h
@@ -90,6 +90,7 @@ struct ata_connect_task {
 #define ATA_AMD8111             0x74691022
 
 #define ATA_ACER_LABS_ID        0x10b9
+#define ATA_ALI_1533		0x153310b9
 #define ATA_ALI_5229            0x522910b9
 #define ATA_ALI_5281            0x528110b9
 #define ATA_ALI_5287            0x528710b9
