a1f8659aa59bdf14485246bed5f81c142a1ecdfe
pjd <pjd@FreeBSD.org>
2005-08-30 15:09:06 +0000

    MFC:	sbin/class/raid3/geom_raid3.c	1.13 	sbin/misc/subr.c	1.4 	sbin/misc/subr.h	1.4
    Move function for calculating number of bits into more central place.
    
    Approved by:	re (scottl)

--- sbin/geom/misc/subr.c
+++ sbin/geom/misc/subr.c
@@ -95,6 +95,18 @@ g_lcm(unsigned a, unsigned b)
 	return ((a * b) / gcd(a, b));
 }
 
+uint32_t
+bitcount32(uint32_t x)
+{
+
+	x = (x & 0x55555555) + ((x & 0xaaaaaaaa) >> 1);
+	x = (x & 0x33333333) + ((x & 0xcccccccc) >> 2);
+	x = (x & 0x0f0f0f0f) + ((x & 0xf0f0f0f0) >> 4);
+	x = (x & 0x00ff00ff) + ((x & 0xff00ff00) >> 8);
+	x = (x & 0x0000ffff) + ((x & 0xffff0000) >> 16);
+	return (x);
+}
+
 off_t
 g_get_mediasize(const char *name)
 {
