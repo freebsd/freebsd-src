d2bd3ab995038214fa00f73d4a88cd891d0afe34
Scott Long <scottl@FreeBSD.org>
2005-09-07 23:33:26 +0000

    Import new version of the HPTMV driver from Highpoint.  The major change here is the support for amd64, as well as possible support for PAE.  Many thanks to Highpoint for continuing to support FreeBSD.
    Obtained from:  Steve Chang @ Highpoint
    MFC After: 3 days.

--- sys/dev/hptmv/osbsd.h
+++ sys/dev/hptmv/osbsd.h
@@ -1,5 +1,5 @@
-/*-
- * Copyright (c) 2003-2004 HighPoint Technologies, Inc.
+/*
+ * Copyright (c) 2004-2005 HighPoint Technologies, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -31,10 +31,24 @@
 #include <sys/bus.h>
 #include <sys/resource.h>
 #include <sys/eventhandler.h>
+#include <sys/devicestat.h>
 
+#if (__FreeBSD_version < 500043)
+#include <stddef.h>
+#include <sys/buf.h>
+#endif
+
+#include <vm/vm.h>
+#include <vm/pmap.h>
+#include <vm/vm_extern.h>
+
+#if (__FreeBSD_version < 600030)
+#include <machine/bus_memio.h>
+#endif
 #include <machine/bus.h>
 #include <machine/resource.h>
 #include <machine/bus.h>
+
 #include <sys/rman.h>
 
 #include <cam/cam.h>
@@ -43,15 +57,19 @@
 #include <cam/cam_sim.h>
 #include <cam/cam_xpt_sim.h>
 #include <cam/cam_periph.h>
+#include <cam/cam_xpt_periph.h>
+
 #include <cam/scsi/scsi_all.h>
 #include <cam/scsi/scsi_message.h>
+
+
 
 extern intrmask_t lock_driver(void);
 extern void unlock_driver(intrmask_t spl);
 
 typedef struct 
 {
-	UCHAR		status;		/* 0 nonbootable; 80h bootable */
+	UCHAR		status;     		/* 0 nonbootable; 80h bootable */
 	UCHAR      	start_head;
 	USHORT     	start_sector;
 	UCHAR      	type;
@@ -98,8 +116,8 @@ typedef struct _READ_CAPACITY_DATA {
 /****************************************/
 
 /* Bits for HD_ERROR */
-#define NM_ERR		0x02	/* media present */
-#define ABRT_ERR	0x04	/* Command aborted */
+#define NM_ERR			0x02	/* media present */
+#define ABRT_ERR		0x04	/* Command aborted */
 #define MCR_ERR         0x08	/* media change request */
 #define IDNF_ERR        0x10	/* ID field not found */
 #define MC_ERR          0x20	/* media changed */
@@ -107,213 +125,195 @@ typedef struct _READ_CAPACITY_DATA {
 #define WP_ERR          0x40	/* write protect */
 #define ICRC_ERR        0x80	/* new meaning:  CRC error during transfer */
 
-#define REQUESTS_ARRAY_SIZE	(9 * MV_EDMA_REQUEST_QUEUE_SIZE) /* 9 K bytes */
-#define RESPONSES_ARRAY_SIZE	(12*MV_EDMA_RESPONSE_QUEUE_SIZE) /* 3 K bytes */
+#define REQUESTS_ARRAY_SIZE			(9 * MV_EDMA_REQUEST_QUEUE_SIZE) /* 9 K bytes */
+#define RESPONSES_ARRAY_SIZE		(12 * MV_EDMA_RESPONSE_QUEUE_SIZE) /* 3 K bytes */
 
-#define PRD_ENTRIES_PER_CMD	(MAX_SG_DESCRIPTORS+1)
-#define PRD_ENTRIES_SIZE	(MV_EDMA_PRD_ENTRY_SIZE*PRD_ENTRIES_PER_CMD)
-#define PRD_TABLES_FOR_VBUS	(MV_SATA_CHANNELS_NUM*MV_EDMA_QUEUE_LENGTH)
+#define PRD_ENTRIES_PER_CMD         (MAX_SG_DESCRIPTORS+1)
+#define PRD_ENTRIES_SIZE            (MV_EDMA_PRD_ENTRY_SIZE*PRD_ENTRIES_PER_CMD)
+#define PRD_TABLES_FOR_VBUS         (MV_SATA_CHANNELS_NUM*MV_EDMA_QUEUE_LENGTH)
 
-typedef enum _SataEvent {
-	SATA_EVENT_NO_CHANGE = 0,
+typedef enum _SataEvent
+{
+    SATA_EVENT_NO_CHANGE = 0,
 	SATA_EVENT_CHANNEL_CONNECTED,
 	SATA_EVENT_CHANNEL_DISCONNECTED
 } SATA_EVENT;
 
 typedef ULONG_PTR dma_addr_t;
 
-typedef struct _MV_CHANNEL {
+typedef struct _MV_CHANNEL
+{
 	unsigned int		maxUltraDmaModeSupported;
 	unsigned int		maxDmaModeSupported;
 	unsigned int		maxPioModeSupported;
-	MV_BOOLEAN		online;
+	MV_BOOLEAN			online;
 } MV_CHANNEL;
 
-struct _privCommand;
+typedef struct _BUS_DMAMAP
+{	struct _BUS_DMAMAP 	 	*next;
+	struct IALAdapter 			*pAdapter;
+	bus_dmamap_t 			dma_map;
+	SCAT_GATH				psg[MAX_SG_DESCRIPTORS];
+} BUS_DMAMAP, *PBUS_DMAMAP;
 
-typedef struct IALAdapter {
+typedef struct IALAdapter 
+{
 	struct cam_path 	*path;
-	device_t		hpt_dev;		/* bus device */
-	struct resource		*hpt_irq;		/* interrupt */
-	void			*hpt_intr;		/* interrupt handle */
-	struct resource		*mem_res;
-	bus_space_handle_t	mem_bsh;
-	bus_space_tag_t		mem_btag;
-	bus_dma_tag_t		parent_dmat;
-	bus_dma_tag_t		req_dmat;
-	bus_dmamap_t		req_map;
-	bus_dma_tag_t		resp_dmat;
-	bus_dmamap_t		resp_map;
-	bus_dma_tag_t		prd_dmat;
-	bus_dmamap_t		prd_map;
-	bus_dma_tag_t		buf_dmat;
-	
-	struct IALAdapter	*next;
 
-	MV_SATA_ADAPTER		mvSataAdapter;
-	MV_CHANNEL		mvChannel[MV_SATA_CHANNELS_NUM];
-	MV_U8			*requestsArrayBaseAddr;
-	MV_U8			*requestsArrayBaseAlignedAddr;
-	dma_addr_t		requestsArrayBaseDmaAddr;
-	dma_addr_t		requestsArrayBaseDmaAlignedAddr;
-	MV_U8			*responsesArrayBaseAddr;
-	MV_U8			*responsesArrayBaseAlignedAddr;
-	dma_addr_t		responsesArrayBaseDmaAddr;
-	dma_addr_t		responsesArrayBaseDmaAlignedAddr;
-	SATA_EVENT		sataEvents[MV_SATA_CHANNELS_NUM];
+	bus_dma_tag_t	  io_dma_parent; /* I/O buffer DMA tag */
+	PBUS_DMAMAP	  pbus_dmamap_list;
+	PBUS_DMAMAP	  pbus_dmamap;
+
+	device_t			hpt_dev;				/* bus device */
+	struct resource		*hpt_irq;					/* interrupt */
+	struct resource		*mem_res;
+	void				*hpt_intr;				/* interrupt handle */
+	struct IALAdapter   *next;
+
+	MV_SATA_ADAPTER     mvSataAdapter;
+	MV_CHANNEL          mvChannel[MV_SATA_CHANNELS_NUM];
+	MV_U8				*requestsArrayBaseAddr;
+	MV_U8				*requestsArrayBaseAlignedAddr;
+	dma_addr_t			requestsArrayBaseDmaAddr;
+	dma_addr_t			requestsArrayBaseDmaAlignedAddr;
+	MV_U8				*responsesArrayBaseAddr;
+	MV_U8				*responsesArrayBaseAlignedAddr;
+	dma_addr_t			responsesArrayBaseDmaAddr;
+	dma_addr_t			responsesArrayBaseDmaAlignedAddr;
+	SATA_EVENT			sataEvents[MV_SATA_CHANNELS_NUM];
 	
    	struct	callout_handle event_timer_connect;
   	struct	callout_handle event_timer_disconnect;
 
-	struct _VBus		VBus;
-	struct _VDevice		VDevices[MV_SATA_CHANNELS_NUM];
-	PCommand		pCommandBlocks;
-	struct _privCommand	*pPrivateBlocks;
-	TAILQ_HEAD(, _privCommand)	PrivCmdTQH;
-	PUCHAR			prdTableAddr;
-	ULONG			prdTableDmaAddr;
-	void*			pFreePRDLink;
+	struct _VBus        VBus;
+	struct _VDevice     VDevices[MV_SATA_CHANNELS_NUM];
+	PCommand			pCommandBlocks;
+	PUCHAR				prdTableAddr;
+	PUCHAR				prdTableAlignedAddr;
+	void*				pFreePRDLink;
 
-	union ccb		*pending_Q;
+	union ccb           *pending_Q;
 
-	MV_U8   		outstandingCommands;
+	MV_U8   			outstandingCommands;
 
-	UCHAR			status;
-	UCHAR			ver_601;
-	UCHAR			beeping;
+	UCHAR               status;
+	UCHAR               ver_601;
+	UCHAR               beeping;
 
 	eventhandler_tag	eh;
-} IAL_ADAPTER_T;
-
-typedef struct _privCommand {
-	TAILQ_ENTRY(_privCommand)	PrivEntry;
-	IAL_ADAPTER_T			*pAdapter;
-	union ccb			*ccb;
-	bus_dmamap_t			buf_map;
-} *pPrivCommand;
+}
+IAL_ADAPTER_T;
 
 extern IAL_ADAPTER_T *gIal_Adapter;
 
 /*entry.c*/
 typedef void (*HPT_DPC)(IAL_ADAPTER_T *,void*,UCHAR);
+
 int hpt_queue_dpc(HPT_DPC dpc, IAL_ADAPTER_T *pAdapter, void *arg, UCHAR flags);
-void hpt_rebuild_data_block(IAL_ADAPTER_T *pAdapter, PVDevice pArray,
-    UCHAR flags);
+void hpt_rebuild_data_block(IAL_ADAPTER_T *pAdapter, PVDevice pArray, UCHAR flags);
 void Check_Idle_Call(IAL_ADAPTER_T *pAdapter);
-int Kernel_DeviceIoControl(_VBUS_ARG DWORD dwIoControlCode, PVOID lpInBuffer,
-    DWORD nInBufferSize, PVOID lpOutBuffer, DWORD nOutBufferSize,
-    PDWORD lpBytesReturned);
 void fRescanAllDevice(_VBUS_ARG0);
 int hpt_add_disk_to_array(_VBUS_ARG DEVICEID idArray, DEVICEID idDisk);
+
+int Kernel_DeviceIoControl(_VBUS_ARG
+							DWORD dwIoControlCode,       	/* operation control code */
+							PVOID lpInBuffer,            	/* input data buffer */
+							DWORD nInBufferSize,         	/* size of input data buffer */
+							PVOID lpOutBuffer,           	/* output data buffer */
+							DWORD nOutBufferSize,        	/* size of output data buffer */
+							PDWORD lpBytesReturned      	/* byte count */
+						);
 
 
 #define __str_direct(x) #x
 #define __str(x) __str_direct(x)
 #define KMSG_LEADING __str(PROC_DIR_NAME) ": "
-#define hpt_printk(_x_) do {	\
-	printf(KMSG_LEADING);	\
-	printf _x_ ;		\
-} while (0)
+#define hpt_printk(_x_) do { printf(KMSG_LEADING); printf _x_ ; } while (0)
 
 #define DUPLICATE      0
 #define INITIALIZE     1
 #define REBUILD_PARITY 2
 #define VERIFY         3
 
-/**********************************************************/
+/***********************************************************/
+
 static __inline struct cam_periph *
 hpt_get_periph(int path_id,int target_id)
 {
-	struct cam_periph *periph = NULL;
-	struct cam_path	*path;
-	int status;
+	struct cam_periph	*periph = NULL;
+    struct cam_path	*path;
+    int			status;
 
-	status = xpt_create_path(&path, NULL, path_id, target_id, 0);
-	if (status == CAM_REQ_CMP) {
-		periph = cam_periph_find(path, NULL);
+    status = xpt_create_path(&path, NULL, path_id, target_id, 0);
+    if (status == CAM_REQ_CMP) {
+		periph = cam_periph_find(path, "da");
 		xpt_free_path(path);
-		if (periph != NULL) {
-			if (strncmp(periph->periph_name, "da", 2))
-				periph = NULL;
-    		}
-    	}
+			
+    }
 	return periph;	
 }
 
-static __inline void
-FreePrivCommand(IAL_ADAPTER_T *pAdapter, pPrivCommand prvCmd)
+#if (__FreeBSD_version < 500000)
+#define YIELD_THREAD yield(curproc, 0)
+#endif
+
+#ifdef __i386__
+#define BITS_PER_LONG 32
+#define VDEV_TO_ID(pVDev) (DEVICEID)(pVDev)
+#define ID_TO_VDEV(id) (PVDevice)(id)
+#else /*Only support x86_64(AMD64 and EM64T)*/
+#define BITS_PER_LONG 64
+#define VDEV_TO_ID(pVDev) (DEVICEID)(ULONG_PTR)(pVDev)
+#define ID_TO_VDEV(id) (PVDevice)(((ULONG_PTR)gIal_Adapter & 0xffffffff00000000) | (id))
+#endif
+
+#define INVALID_DEVICEID		(-1)
+#define INVALID_STRIPSIZE		(-1)
+
+#define shortswap(w) ((WORD)((w)>>8 | ((w) & 0xFF)<<8))
+
+#ifndef MinBlockSizeShift
+#define MinBlockSizeShift 5
+#define MaxBlockSizeShift 12
+#endif
+
+#pragma pack(1)
+typedef struct _HPT_IOCTL_TRANSFER_PARAM
 {
-	TAILQ_INSERT_TAIL(&pAdapter->PrivCmdTQH, prvCmd, PrivEntry);
-}
+	ULONG nInBufferSize;
+	ULONG nOutBufferSize;
+	UCHAR buffer[0];
+}HPT_IOCTL_TRANSFER_PARAM, *PHPT_IOCTL_TRANSFER_PARAM;
 
-static __inline pPrivCommand
-AllocPrivCommand(IAL_ADAPTER_T *pAdapter)
+typedef struct _HPT_SET_STATE_PARAM
 {
-	pPrivCommand prvCmd;
+	DEVICEID idArray;
+	DWORD state;
+} HPT_SET_STATE_PARAM, *PHPT_SET_STATE_PARAM;
 
-	prvCmd = TAILQ_FIRST(&pAdapter->PrivCmdTQH);
-	if (prvCmd == NULL)
-		return NULL;
-
-	TAILQ_REMOVE(&pAdapter->PrivCmdTQH, prvCmd, PrivEntry);
-	return (prvCmd);
-}
-
-static __inline void
-mv_reg_write_byte(MV_BUS_ADDR_T base, MV_U32 offset, MV_U8 val)
+typedef struct _HPT_SET_ARRAY_INFO
 {
-	IAL_ADAPTER_T *pAdapter;
-	
-	pAdapter = base;
-	bus_space_write_1(pAdapter->mem_btag, pAdapter->mem_bsh, offset, val);
-}
+	DEVICEID idArray;
+	ALTERABLE_ARRAY_INFO Info;
+} HPT_SET_ARRAY_INFO, *PHPT_SET_ARRAY_INFO;
 
-static __inline void
-mv_reg_write_word(MV_BUS_ADDR_T base, MV_U32 offset, MV_U16 val)
+typedef struct _HPT_SET_DEVICE_INFO
 {
-	IAL_ADAPTER_T *pAdapter;
-	
-	pAdapter = base;
-	bus_space_write_2(pAdapter->mem_btag, pAdapter->mem_bsh, offset, val);
-}
+	DEVICEID idDisk;
+	ALTERABLE_DEVICE_INFO Info;
+} HPT_SET_DEVICE_INFO, *PHPT_SET_DEVICE_INFO;
 
-static __inline void
-mv_reg_write_dword(MV_BUS_ADDR_T base, MV_U32 offset, MV_U32 val)
+typedef struct _HPT_ADD_DISK_TO_ARRAY
 {
-	IAL_ADAPTER_T *pAdapter;
+	DEVICEID idArray;
+	DEVICEID idDisk;
+} HPT_ADD_DISK_TO_ARRAY, *PHPT_ADD_DISK_TO_ARRAY;
 
-	pAdapter = base;
-	bus_space_write_4(pAdapter->mem_btag, pAdapter->mem_bsh, offset, val);
-}
+int check_VDevice_valid(PVDevice);
+int hpt_default_ioctl(_VBUS_ARG DWORD, PVOID, DWORD, PVOID, DWORD, PDWORD);
 
-static __inline MV_U8
-mv_reg_read_byte(MV_BUS_ADDR_T base, MV_U32 offset)
-{
-	IAL_ADAPTER_T *pAdapter;
-	
-	pAdapter = base;
-	return (bus_space_read_1(pAdapter->mem_btag, pAdapter->mem_bsh,
-	    offset));
-}
+#define HPT_NULL_ID 0
 
-static __inline MV_U16
-mv_reg_read_word(MV_BUS_ADDR_T base, MV_U32 offset)
-{
-	IAL_ADAPTER_T *pAdapter;
-	
-	pAdapter = base;
-	return (bus_space_read_2(pAdapter->mem_btag, pAdapter->mem_bsh,
-	    offset));
-}
-
-static __inline MV_U32
-mv_reg_read_dword(MV_BUS_ADDR_T base, MV_U32 offset)
-{
-	IAL_ADAPTER_T *pAdapter;
-	
-	pAdapter = base;
-	return (bus_space_read_4(pAdapter->mem_btag, pAdapter->mem_bsh,
-	    offset));
-}
+#pragma pack()
 
 #endif
