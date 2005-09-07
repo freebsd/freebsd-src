6b463eed3ae69b4aa7b87eb3afa1182eca8194c8
Christian S.J. Peron <csjp@FreeBSD.org>
2005-09-07 17:35:16 +0000

    Merge bpfstat's functionality into the netstat(1) utility. This adds a -B option which causes bpf peers to be printed. This option can be used in conjunction with -I if information about specific interfaces is desired. This is similar to what NetBSD added to their version of netstat.
    $ netstat -B
      Pid  Netif  Flags      Recv      Drop     Match Sblen Hblen Command
     1137    lo0 p--s--         0         0         0     0     0 tcpdump
      205   sis0 -ifs-l     37331         0         1     0     0 dhclient
    $
    
    $ netstat -I lo0 -B
      Pid  Netif  Flags      Recv      Drop     Match Sblen Hblen Command
     1174    lo0 p--s--         0         0         0     0     0 tcpdump
    $
    
    -Add bpf.c which stores all the code for retrieving and parsing bpf
     related statistics.
    -Modify main.c to add support for the -B option and hook it into the
     program logic.
    -Add bpf.c to the build.
    -Document this new functionality in the man page and bump the revision
     date.
    -Add prototype for bpf_stats function.

---
+++ usr.bin/netstat/bpf.c
@@ -0,0 +1,119 @@
+/*-
+ * Copyright (c) 2005 Christian S.J. Peron
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+#include <sys/types.h>
+#include <sys/protosw.h>
+#include <sys/socket.h>
+#include <sys/sysctl.h>
+#include <sys/param.h>
+#include <sys/user.h>
+
+#include <net/if.h>
+#include <net/if_var.h>
+#include <net/bpfdesc.h>
+#include <arpa/inet.h>
+
+#include <err.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "netstat.h"
+
+/* print bpf stats */
+
+static char *
+bpf_pidname(pid_t pid)
+{
+	struct kinfo_proc newkp;
+	int error, mib[4];
+	size_t size;
+
+	mib[0] = CTL_KERN;
+	mib[1] = KERN_PROC;
+	mib[2] = KERN_PROC_PID;
+	mib[3] = pid;
+	size = sizeof(newkp);
+	error = sysctl(mib, 4, &newkp, &size, NULL, 0);
+	if (error < 0)
+		return (strdup("??????"));
+	return (strdup(newkp.ki_comm));
+}
+
+static void
+bpf_flags(struct xbpf_d *bd, char *flagbuf)
+{
+
+	*flagbuf++ = bd->bd_promisc ? 'p' : '-';
+	*flagbuf++ = bd->bd_immediate ? 'i' : '-';
+	*flagbuf++ = bd->bd_hdrcmplt ? '-' : 'f';
+	*flagbuf++ = bd->bd_seesent ? 's' : '.';
+	*flagbuf++ = bd->bd_async ? 'a' : '-';
+	*flagbuf++ = bd->bd_locked ? 'l' : '-';
+	*flagbuf++ = '\0';
+}       
+
+void
+bpf_stats(char *interface)
+{
+	struct xbpf_d *d, *bd;
+	char *pname, flagbuf[12];
+	size_t size;
+
+	if (sysctlbyname("net.bpf.stats", NULL, &size,
+	    NULL, 0) < 0) {
+		warn("net.bpf.stats");
+		return;
+	}
+	bd = malloc(size);
+	if (bd == NULL) {
+		warn("malloc failed");
+		return;
+	}
+	if (sysctlbyname("net.bpf.stats", bd, &size,
+	    NULL, 0) < 0) {
+		warn("net.bpf.stats");
+		free(bd);
+		return;
+	}
+	printf("%5s %6s %6s %9s %9s %9s %5s %5s %s\n",
+	    "Pid", "Netif", "Flags", "Recv", "Drop", "Match", "Sblen",
+	    "Hblen", "Command");
+	for (d = &bd[0]; d < &bd[size / sizeof(*d)]; d++) {
+		if (interface && strcmp(interface, d->bd_ifname) != 0)
+			continue;
+		bpf_flags(d, flagbuf);
+		pname = bpf_pidname(d->bd_pid);
+		printf("%5d %6s %6s %9lu %9lu %9lu %5d %5d %s\n",
+		    d->bd_pid, d->bd_ifname, flagbuf,
+		    d->bd_rcount, d->bd_dcount, d->bd_fcount,
+		    d->bd_slen, d->bd_hlen, pname);
+		free(pname);
+	}
+}
