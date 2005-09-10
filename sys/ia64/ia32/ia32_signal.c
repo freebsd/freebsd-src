6798cfc6b59c78fe4bba3810e41d92bda4c52281
marcel <marcel@FreeBSD.org>
2005-09-10 02:12:49 +0000

    Move the ia32_sigcode structure from ia32_sigtramp.c to ia32_signal.c. It's a bit excessive to have it in a file of its own.

--- sys/ia64/ia32/ia32_signal.c
+++ sys/ia64/ia32/ia32_signal.c
@@ -22,9 +22,10 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $FreeBSD$
  */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #include "opt_compat.h"
 
@@ -73,6 +74,22 @@
 #include <i386/include/psl.h>
 #include <i386/include/segments.h>
 #include <i386/include/specialreg.h>
+
+char ia32_sigcode[] = {
+	0xff, 0x54, 0x24, 0x10,		/* call *SIGF_HANDLER(%esp) */
+	0x8d, 0x44, 0x24, 0x14,		/* lea SIGF_UC(%esp),%eax */
+	0x50,				/* pushl %eax */
+	0xf7, 0x40, 0x54, 0x00, 0x00, 0x02, 0x02, /* testl $PSL_VM,UC_EFLAGS(%ea
+x) */
+	0x75, 0x03,			/* jne 9f */
+	0x8e, 0x68, 0x14,		/* movl UC_GS(%eax),%gs */
+	0xb8, 0x57, 0x01, 0x00, 0x00,	/* 9: movl $SYS_sigreturn,%eax */
+	0x50,				/* pushl %eax */
+	0xcd, 0x80,			/* int $0x80 */
+	0xeb, 0xfe,			/* 0: jmp 0b */
+	0
+};
+int sz_ia32_sigcode = sizeof(ia32_sigcode);
 
 /*
  * Signal sending has not been implemented on ia64.  This causes
