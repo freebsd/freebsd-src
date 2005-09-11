6a3d26b2b754f224cf23197d3594c936e1929e1d
Craig Rodrigues <rodrigc@FreeBSD.org>
2005-09-11 16:13:02 +0000

    Forward declare z_errmsg with static linkage since it is defined with static linkage later in the file.  Eliminates GCC 4.0 error.

--- sys/net/zlib.c
+++ sys/net/zlib.c
@@ -94,7 +94,7 @@ typedef unsigned short ush;
 typedef ush FAR ushf;
 typedef unsigned long  ulg;
 
-extern const char *z_errmsg[10]; /* indexed by 2-zlib_error */
+static const char *z_errmsg[10]; /* indexed by 2-zlib_error */
 /* (size given to avoid silly warnings with Visual C++) */
 
 #define ERR_MSG(err) z_errmsg[Z_NEED_DICT-(err)]
