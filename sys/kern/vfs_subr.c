61ac14dab6fa7c0a284debc3e4a6b6c3d9c99d0d
Tor Egge <tegge@FreeBSD.org>
2005-09-16 18:28:12 +0000

    Break out of loop if next buffer pointer has become invalid while flushing current buffer.
    Reviewed by:	kan

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -1081,6 +1081,8 @@ flushbuflist(bufv, flags, bo, slpflag, slptimeo)
 {
 	struct buf *bp, *nbp;
 	int retval, error;
+	daddr_t lblkno;
+	b_xflags_t xflags;
 
 	ASSERT_BO_LOCKED(bo);
 
@@ -1089,6 +1091,13 @@ flushbuflist(bufv, flags, bo, slpflag, slptimeo)
 		if (((flags & V_NORMAL) && (bp->b_xflags & BX_ALTDATA)) ||
 		    ((flags & V_ALT) && (bp->b_xflags & BX_ALTDATA) == 0)) {
 			continue;
+		}
+		lblkno = 0;
+		xflags = 0;
+		if (nbp != NULL) {
+			lblkno = nbp->b_lblkno;
+			xflags = nbp->b_xflags &
+				(BX_BKGRDMARKER | BX_VNDIRTY | BX_VNCLEAN);
 		}
 		retval = EAGAIN;
 		error = BUF_TIMELOCK(bp,
@@ -1125,6 +1134,12 @@ flushbuflist(bufv, flags, bo, slpflag, slptimeo)
 		bp->b_flags &= ~B_ASYNC;
 		brelse(bp);
 		BO_LOCK(bo);
+		if (nbp != NULL &&
+		    (nbp->b_bufobj != bo || 
+		     nbp->b_lblkno != lblkno ||
+		     (nbp->b_xflags &
+		      (BX_BKGRDMARKER | BX_VNDIRTY | BX_VNCLEAN)) != xflags))
+			break;			/* nbp invalid */
 	}
 	return (retval);
 }
