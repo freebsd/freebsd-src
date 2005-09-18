fe0519b1716b0db533acd81e451e3928ae071913
Warner Losh <imp@FreeBSD.org>
2005-09-18 01:32:09 +0000

    MFp4: Expose device_probe_child()

--- sys/sys/bus.h
+++ sys/sys/bus.h
@@ -360,6 +360,7 @@ int	device_is_quiet(device_t dev);
 int	device_print_prettyname(device_t dev);
 int	device_printf(device_t dev, const char *, ...) __printflike(2, 3);
 int	device_probe_and_attach(device_t dev);
+int	device_probe_child(device_t bus, device_t dev);
 int	device_quiesce(device_t dev);
 void	device_quiet(device_t dev);
 void	device_set_desc(device_t dev, const char* desc);
