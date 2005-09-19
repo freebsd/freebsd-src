a01a8c778f7150f4b90007eb4c1436b3019b1e35
andre <andre@FreeBSD.org>
2005-09-19 22:04:41 +0000

    Replace m_extadd() with macro version MEXTADD().

--- sys/dev/hatm/if_hatm_intr.c
+++ sys/dev/hatm/if_hatm_intr.c
@@ -457,7 +457,7 @@ hatm_rx_buffer(struct hatm_softc *sc, u_int group, u_int handle)
 
 		if (m != NULL) {
 			m->m_ext.ref_cnt = &c0->hdr.ref_cnt;
-			m_extadd(m, (void *)c0, MBUF0_SIZE,
+			MEXTADD(m, (void *)c0, MBUF0_SIZE,
 			    hatm_mbuf0_free, sc, M_PKTHDR, EXT_EXTREF);
 			m->m_data += MBUF0_OFFSET;
 		} else
@@ -481,7 +481,7 @@ hatm_rx_buffer(struct hatm_softc *sc, u_int group, u_int handle)
 
 		if (m != NULL) {
 			m->m_ext.ref_cnt = &c1->hdr.ref_cnt;
-			m_extadd(m, (void *)c1, MBUF1_SIZE,
+			MEXTADD(m, (void *)c1, MBUF1_SIZE,
 			    hatm_mbuf1_free, sc, M_PKTHDR, EXT_EXTREF);
 			m->m_data += MBUF1_OFFSET;
 		} else
