a778923149e0b65c1950cc41c7fa34df8e86aabe
Poul-Henning Kamp <phk@FreeBSD.org>
2005-09-24 19:31:10 +0000

    Add two convenience functions for device drivers:  bus_alloc_resources() and bus_free_resources().  These functions take a list of resources and handle them all in one go.  A flag makes it possible to mark a resource as optional.
    A typical device driver can save 10-30 lines of code by using these.
    
    Usage examples will follow RSN.
    
    MFC:	A good idea, eventually.

--- sys/sys/types.h
+++ sys/sys/types.h
@@ -285,6 +285,7 @@ typedef	__uint32_t	intrmask_t;	/* Interrupt mask (spl, xxx_imask...) */
 typedef	__uintfptr_t	uintfptr_t;
 typedef	__uint64_t	uoff_t;
 typedef	struct vm_page	*vm_page_t;
+typedef struct device	*device_t;
 
 #define offsetof(type, field) __offsetof(type, field)
 
