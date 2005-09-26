2f5d3318add374a3dbec59c86b5dca78afcae6eb
John Baldwin <jhb@FreeBSD.org>
2005-09-26 19:35:33 +0000

    MFC: Add a memory barrier for PREWRITE operations to ensure all writes by the CPU have drained before further writes to kick off the DMA operation.
    Approved by:	re (kensmith)

--- sys/alpha/alpha/busdma_machdep.c
+++ sys/alpha/alpha/busdma_machdep.c
@@ -891,6 +891,10 @@ _bus_dmamap_sync(bus_dma_tag_t dmat, bus_dmamap_t map, bus_dmasync_op_t op)
 			}
 		}
 	}
+
+	/* Ensure any pending writes have drained. */
+	if (op & (BUS_DMASYNC_PREWRITE))
+		alpha_mb();
 }
 
 static void
