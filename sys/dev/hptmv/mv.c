c0f80cb28343325398311f6311462dc0c4af92bd
delphij <delphij@FreeBSD.org>
2005-10-06 18:47:58 +0000

    MFC Import of the new version of the HPTMV driver from Highpoint. The major change here is the support for amd64, as well as possible support for PAE.  Many thanks to Highpoint for continuing to support FreeBSD.
    Obtained from:  Steve Chang @ Highpoint
    
    Merged revisions:
    
    | 1.3      dev/hptmv/access601.h
    | 1.1      dev/hptmv/amd64-elf.raid.o.uu
    | 1.3      dev/hptmv/array.h
    | 1.3      dev/hptmv/atapi.h
    | 1.3      dev/hptmv/command.h
    | 1.9+1.10 dev/hptmv/entry.c
    | 1.4      dev/hptmv/global.h
    | 1.5      dev/hptmv/gui_lib.c
    | 1.3      dev/hptmv/hptintf.h
    | 1.6      dev/hptmv/hptproc.c
    | 1.5      dev/hptmv/i386-elf.raid.o.uu
    | 1.5      dev/hptmv/ioctl.c
    | 1.3      dev/hptmv/mv.c
    | 1.4      dev/hptmv/mvOs.h
    | 1.3      dev/hptmv/mvSata.h
    | 1.4      dev/hptmv/mvStorageDev.h
    | 1.5      dev/hptmv/osbsd.h
    | 1.3      dev/hptmv/raid5n.h
    | 1.3      dev/hptmv/readme.txt
    | 1.3      dev/hptmv/vdevice.h
    | 1.4      modules/hptmv/Makefile
    
    Approved by:	re (scottl)

--- sys/dev/hptmv/mv.c
+++ sys/dev/hptmv/mv.c
@@ -1,5 +1,5 @@
-/*-
- * Copyright (c) 2003-2004 HighPoint Technologies, Inc.
+/*
+ * Copyright (c) 2004-2005 HighPoint Technologies, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,51 +27,68 @@
  */
 #include <sys/param.h>
 #include <sys/systm.h>
-#include <sys/kernel.h>
 
-#include <vm/vm.h>
-#include <vm/pmap.h>
-#include <vm/vm_extern.h>
+#include <sys/time.h>
+#include <sys/kernel.h>
+#include <sys/bus.h>
+#include <machine/resource.h>
+
+#if __FreeBSD_version >= 500043
+#include <machine/pci_cfgreg.h>
+#endif
+
+#if (__FreeBSD_version < 500043)
+#include <sys/bus_private.h>
+#endif
+
+#if (__FreeBSD_version < 500000)
+#include <machine/clock.h>
+#endif
+
+#ifndef __KERNEL__
+#define __KERNEL__
+#endif
 
 #include <dev/hptmv/global.h>
 #include <dev/hptmv/hptintf.h>
 #include <dev/hptmv/mvOs.h>
 #include <dev/hptmv/osbsd.h>
 
+
 void HPTLIBAPI
 MV_REG_WRITE_BYTE(MV_BUS_ADDR_T base, MV_U32 offset, MV_U8 val)
-{
-	mv_reg_write_byte(base, offset, val);
+{ 
+	writeb((void *)((ULONG_PTR)base + offset), val); 
 }
 
 void HPTLIBAPI
 MV_REG_WRITE_WORD(MV_BUS_ADDR_T base, MV_U32 offset, MV_U16 val)
-{
-	mv_reg_write_word(base, offset, val);
+{ 
+	writew((void *)((ULONG_PTR)base + offset), val); 
 }
 
 void HPTLIBAPI
 MV_REG_WRITE_DWORD(MV_BUS_ADDR_T base, MV_U32 offset, MV_U32 val)
 {
-	mv_reg_write_dword(base, offset, val);
+	writel((void *)((ULONG_PTR)base + offset), val);
 }
 
-MV_U8
-HPTLIBAPI MV_REG_READ_BYTE(MV_BUS_ADDR_T base, MV_U32 offset)
+MV_U8  HPTLIBAPI
+MV_REG_READ_BYTE(MV_BUS_ADDR_T base, MV_U32 offset)
 {
-	return (mv_reg_read_byte(base, offset));
+	return readb((void *)((ULONG_PTR)base + offset));
 }
 
-MV_U16
-HPTLIBAPI MV_REG_READ_WORD(MV_BUS_ADDR_T base, MV_U32 offset)
+MV_U16 HPTLIBAPI
+MV_REG_READ_WORD(MV_BUS_ADDR_T base, MV_U32 offset)
 {
-	return (mv_reg_read_word(base, offset));
+	return readw((void *)((ULONG_PTR)base + offset));
 }
 
-MV_U32
-HPTLIBAPI MV_REG_READ_DWORD(MV_BUS_ADDR_T base, MV_U32 offset)
+MV_U32 HPTLIBAPI
+MV_REG_READ_DWORD(MV_BUS_ADDR_T base, MV_U32 offset)
 {
-	return (mv_reg_read_dword(base, offset));
+	return readl((void *)((ULONG_PTR)base + offset));
 }
 
 int HPTLIBAPI
@@ -86,29 +103,26 @@ os_memcpy(void *to, const void *from, unsigned len)
 	memcpy(to, from, len);
 }
 
-void
-HPTLIBAPI os_memset(void *s, char c, unsigned len)
+void HPTLIBAPI
+os_memset(void *s, char c, unsigned len)
 {
 	memset(s, c, len);
 }
 
-unsigned
-HPTLIBAPI os_strlen(const char *s)
+unsigned HPTLIBAPI
+os_strlen(const char *s)
 {
 	return strlen(s);
 }
 
-void HPTLIBAPI mvMicroSecondsDelay(MV_U32 msecs)
+void HPTLIBAPI
+mvMicroSecondsDelay(MV_U32 msecs)
 {
 	DELAY(msecs);
 }
 
-/*
- * XXX
- *
- * The binary raid.obj requires this function!
- */
-ULONG_PTR HPTLIBAPI fOsPhysicalAddress(void *addr)
+ULONG_PTR HPTLIBAPI
+fOsPhysicalAddress(void *addr)
 {
 	return (ULONG_PTR)(vtophys(addr));
 }
