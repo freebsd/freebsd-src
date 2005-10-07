284992c3cb414005ad09004a4039c6209d40ad4b
yar <yar@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-10-07 10:18:44 +0000

    Sync usage screen with manpage.
    MFC after:	5 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@151053 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/mail/main.c
+++ usr.bin/mail/main.c
@@ -208,13 +208,12 @@ main(argc, argv)
 			break;
 		case '?':
 			fprintf(stderr, "\
-Usage: %s [-EiInv] [-s subject] [-c cc-addr] [-b bcc-addr] [-F] to-addr ...\n\
-       %*s [- sendmail-options ...]\n\
-       %s [-EHiInNv] [-F] -f [name]\n\
-       %s [-EHiInNv] [-F] [-u user]\n\
-       %s -e [-f name]\n\
-       %s -H\n",__progname, strlen(__progname), "",
-			    __progname, __progname, __progname, __progname);
+Usage: %s [-dEiInv] [-s subject] [-c cc-addr] [-b bcc-addr] [-F] to-addr ...\n\
+       %*s [-sendmail-option ...]\n\
+       %s [-dEHiInNv] [-F] -f [name]\n\
+       %s [-dEHiInNv] [-F] [-u user]\n\
+       %s [-d] -e [-f name]\n", __progname, strlen(__progname), "",
+				__progname, __progname, __progname);
 			exit(1);
 		}
 	}
