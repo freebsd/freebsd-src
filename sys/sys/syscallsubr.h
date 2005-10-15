c63313b35dc037347e1ede7ea87fa59d5274d48b
ps <ps@FreeBSD.org>
2005-10-15 02:54:18 +0000

    Implement 32bit wrappers for clock_gettime, clock_settime, and clock_getres.

--- sys/sys/syscallsubr.h
+++ sys/sys/syscallsubr.h
@@ -60,6 +60,12 @@ int	kern_chmod(struct thread *td, char *path, enum uio_seg pathseg,
 	    int mode);
 int	kern_chown(struct thread *td, char *path, enum uio_seg pathseg, int uid,
 	    int gid);
+int	kern_clock_getres(struct thread *td, clockid_t clock_id,
+	    struct timespec *ts);
+int	kern_clock_gettime(struct thread *td, clockid_t clock_id,
+	    struct timespec *ats);
+int	kern_clock_settime(struct thread *td, clockid_t clock_id,
+	    struct timespec *ats);
 int	kern_connect(struct thread *td, int fd, struct sockaddr *sa);
 int	kern_execve(struct thread *td, struct image_args *args,
 	    struct mac *mac_p);
