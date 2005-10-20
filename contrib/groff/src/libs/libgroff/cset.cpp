ed6ac7236a661fd23333d65fddca384156fa6bb5
Ruslan Ermilov <ru@FreeBSD.org>
2005-10-20 10:53:15 +0000

    Merge Groff 1.19.2 changes.

--- contrib/groff/src/libs/libgroff/cset.cpp
+++ contrib/groff/src/libs/libgroff/cset.cpp
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-/* Copyright (C) 1989, 1990, 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1989, 1990, 1991, 1992, 2004 Free Software Foundation, Inc.
      Written by James Clark (jjc@jclark.com)
 
 This file is part of groff.
@@ -16,7 +16,7 @@ for more details.
 
 You should have received a copy of the GNU General Public License along
 with groff; see the file COPYING.  If not, write to the Free Software
-Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
+Foundation, 51 Franklin St - Fifth Floor, Boston, MA 02110-1301, USA. */
 
 /* $FreeBSD$ */
 
@@ -24,6 +24,8 @@ Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 #ifdef __FreeBSD__
 #include <locale.h>
 #endif
+
+#include "lib.h"
 #include "cset.h"
 
 cset csalpha(CSET_BUILTIN);
