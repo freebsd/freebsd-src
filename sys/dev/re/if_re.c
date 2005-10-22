797ae2cd5f3f09709e18eb44de83b01ea467b0f8
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-10-22 05:01:11 +0000

    The controller -> device switch happened in 4.0, fix comment
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@151544 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/re/if_re.c
+++ sys/dev/re/if_re.c
@@ -147,7 +147,7 @@ MODULE_DEPEND(re, pci, 1, 1, 1);
 MODULE_DEPEND(re, ether, 1, 1, 1);
 MODULE_DEPEND(re, miibus, 1, 1, 1);
 
-/* "controller miibus0" required.  See GENERIC if you get errors here. */
+/* "device miibus" required.  See GENERIC if you get errors here. */
 #include "miibus_if.h"
 
 /*
