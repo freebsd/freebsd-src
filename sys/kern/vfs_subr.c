053c63be2baa6d34170ca7dc54a0ca0448dafdf6
kris <kris@FreeBSD.org>
2005-10-25 20:42:06 +0000

    MFC r1.650:
    Default to mpsafevfs=1 on sparc64
    
    Approved by:	re (scottl)

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -108,7 +108,8 @@ static int	vfs_knllocked(void *arg);
  * Enable Giant pushdown based on whether or not the vm is mpsafe in this
  * build.  Without mpsafevm the buffer cache can not run Giant free.
  */
-#if defined(__alpha__) || defined(__amd64__) || defined(__i386__)
+#if defined(__alpha__) || defined(__amd64__) || defined(__i386__) || \
+	defined(__sparc64__)
 int mpsafe_vfs = 1;
 #else
 int mpsafe_vfs;
