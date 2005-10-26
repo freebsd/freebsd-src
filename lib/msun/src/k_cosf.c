3b46e988e7f563b566f842d7a170d6da4ec3c040
Bruce Evans <bde@FreeBSD.org>
2005-10-26 12:36:18 +0000

    Use a better algorithm for reducing the error in __kernel_cos[f](). This supersedes the fix for the old algorithm in rev.1.8 of k_cosf.c.
    I want this change mainly because it is an optimization.  It helps
    make software cos[f](x) and sin[f](x) faster than the i387 hardware
    versions for small x.  It is also a simplification, and reduces the
    maximum relative error for cosf() and sinf() on machines like amd64
    from about 0.87 ulps to about 0.80 ulps.  It was validated for cosf()
    and sinf() by exhaustive testing.  Exhaustive testing is not possible
    for cos() and sin(), but ucbtest reports a similar reduction for the
    worst case found by non-exhaustive testing.  ucbtest's non-exhaustive
    testing seems to be good enough to find problems in algorithms but not
    maximum relative errors when there are spikes.  E.g., short runs of
    it find only 3 ulp error where the i387 hardware cos() has an error
    of about 2**40 ulps near pi/2.

--- lib/msun/src/k_cosf.c
+++ lib/msun/src/k_cosf.c
@@ -32,33 +32,11 @@ C6  = -1.1359647598e-11; /* 0xad47d74e */
 float
 __kernel_cosf(float x, float y)
 {
-	float a,hz,z,r,qx;
-	int32_t ix;
-	GET_FLOAT_WORD(ix,x);
-	ix &= 0x7fffffff;			/* ix = |x|'s high word*/
+	float hz,z,r,w;
+
 	z  = x*x;
 	r  = z*(C1+z*(C2+z*(C3+z*(C4+z*(C5+z*C6)))));
-	if(ix < 0x3e99999a) 			/* if |x| < 0.3 */
-	    return one - ((float)0.5*z - (z*r - x*y));
-	else {
-	    /*
-	     * qx is an approximation to x*x/2 such that 1-qx and x*x/2-qx
-	     * are both exact.  Its implementation is optimized for
-	     * efficiency in preference to accuracy.  We use x*x/2 ~ x/4 for
-	     * x near 0.5 and mask off just enough low bits (3) for both of
-	     * the above differences to be exact.  We use a constant for
-	     * x > 0.78125 to keep using the same algorithm as k_cos.c,
-	     * although this gives only a small improvement in accuracy, at
-	     * least here.  Using x*x/2 to approximate itself (masking off
-	     * 3 low bits again) would give better accuracy.
-	     */
-	    if(ix > 0x3f480000) {		/* x > 0.78125 */
-		qx = (float)0.28125;
-	    } else {
-	        SET_FLOAT_WORD(qx,(ix-0x01000000)&0xfffffff8);
-	    }
-	    hz = (float)0.5*z-qx;
-	    a  = one-qx;
-	    return a - (hz - (z*r-x*y));
-	}
+	hz = (float)0.5*z;
+	w  = one-hz;
+	return w + (((one-w)-hz) + (z*r-x*y));
 }
