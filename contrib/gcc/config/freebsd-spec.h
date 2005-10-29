c5af7bf469ba0f9624a58cc99eaf7689b9255f0c
obrien <obrien@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-10-29 09:13:31 +0000

    Catch up with FreeBSD 7.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@151857 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/gcc/config/freebsd-spec.h
+++ contrib/gcc/config/freebsd-spec.h
@@ -70,7 +70,7 @@ Boston, MA 02111-1307, USA.  */
 #ifdef FREEBSD_NATIVE
 #define FBSD_NATIVE_TARGET_OS_CPP_BUILTINS()				\
   do {									\
-	builtin_define_std ("__FreeBSD_cc_version=600001");		\
+	builtin_define_std ("__FreeBSD_cc_version=700001");		\
   } while (0)
 #else
 #define FBSD_NATIVE_TARGET_OS_CPP_BUILTINS()				\
