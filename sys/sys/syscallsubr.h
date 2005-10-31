163f39751a6529e9b27fb52a2ad4e36460c59279
ps <ps@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-10-31 21:09:56 +0000

    Reformat socket control messages on input/output for 32bit compatibility on 64bit systems.
    Submitted by:	ps, ups
    Reviewed by:	jhb
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@151909 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/syscallsubr.h
+++ sys/sys/syscallsubr.h
@@ -112,7 +112,7 @@ int	kern_readlink(struct thread *td, char *path, enum uio_seg pathseg,
 	    char *buf, enum uio_seg bufseg, int count);
 int	kern_readv(struct thread *td, int fd, struct uio *auio);
 int	kern_recvit(struct thread *td, int s, struct msghdr *mp, void *namelenp,
-	    enum uio_seg segflg);
+	    enum uio_seg segflg, struct mbuf **controlp);
 int	kern_rename(struct thread *td, char *from, char *to,
 	    enum uio_seg pathseg);
 int	kern_rmdir(struct thread *td, char *path, enum uio_seg pathseg);
