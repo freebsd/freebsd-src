e3ab6622c5b23b577d5f658e095529d74069b75e
alc <alc@FreeBSD.org>
2005-11-01 04:30:21 +0000

    Consider the zero-copy transmission of a page that was wired by mlock(2). If a copy-on-write fault occurs on the page, the new copy should inherit a part of the original page's wire count.
    Submitted by: tegge
    MFC after: 1 week

--- sys/vm/vm_page.c
+++ sys/vm/vm_page.c
@@ -1677,6 +1677,8 @@ vm_page_cowfault(vm_page_t m)
 		mnew->valid = VM_PAGE_BITS_ALL;
 		vm_page_dirty(mnew);
 		vm_page_flag_clear(mnew, PG_BUSY);
+		mnew->wire_count = m->wire_count - m->cow;
+		m->wire_count = m->cow;
 	}
 }
 
