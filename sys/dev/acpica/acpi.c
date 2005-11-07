3a6497c102b4e0a4a7ad596c327dfe08191ca06c
John Baldwin <jhb@FreeBSD.org>
2005-11-07 21:52:06 +0000

    *sigh*  Revert stuff that wasn't supposed to be committed.  The acpi_resource change was a minor nit offered as an early candidate for the recent ACPICA import problem and the acpi.c change is one I need to test still that makes the ordered probing of system devices actually work as advertised (probe devices in order based on the type of device rather than in the order we encounter them in the device tree).

--- sys/dev/acpica/acpi.c
+++ sys/dev/acpica/acpi.c
@@ -1497,13 +1497,13 @@ acpi_probe_order(ACPI_HANDLE handle, int *order)
     ret = 0;
     if (acpi_MatchHid(handle, "PNP0C01") || acpi_MatchHid(handle, "PNP0C02")) {
 	*order = 1;
-	ret = 0;
+	ret = 1;
     } else if (acpi_MatchHid(handle, "PNP0C09")) {
 	*order = 2;
-	ret = 0;
+	ret = 1;
     } else if (acpi_MatchHid(handle, "PNP0C0F")) {
 	*order = 3;
-	ret = 0;
+	ret = 1;
     }
 
     return (ret);
