1098b18801da5a20782d6f8dd6303e51e5c95308
kientzle <kientzle@FreeBSD.org>
2005-11-08 07:44:39 +0000

    Bump the maximum number of archive formats that can be enabled at one time from 4 to 8.

--- lib/libarchive/archive_private.h
+++ lib/libarchive/archive_private.h
@@ -157,7 +157,7 @@ struct archive {
 		int	(*read_data_skip)(struct archive *);
 		int	(*cleanup)(struct archive *);
 		void	 *format_data;	/* Format-specific data for readers. */
-	}	formats[4];
+	}	formats[8];
 	struct archive_format_descriptor	*format; /* Active format. */
 
 	/*
