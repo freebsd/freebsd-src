bbd2f5a902cc6239289763dee3eaddb5db8b1923
rwatson <rwatson@FreeBSD.org>
2005-11-09 10:22:28 +0000

    Merge libmemstat.3:1.8, memstat.h:1.9, memstat_all.c:1.3, memstat_malloc:1.6 from HEAD to RELENG_6:
      Teach libmemstat(3) how to extract malloc(9) statistics using kvm(3),
      so that libmemstat can be used to view full memory statistics from
      kernel core dumps and /dev/mem.  This is provided via a new query
      function, memstat_kvm_malloc(), which is also automatically invoked
      by memstat_kvm_all().  A kvm handle must be passed in.
    
      This will allow malloc(9)-specific code to be removed from vmstat(8).

--- lib/libmemstat/memstat_all.c
+++ lib/libmemstat/memstat_all.c
@@ -50,10 +50,8 @@ int
 memstat_kvm_all(struct memory_type_list *mtlp, void *kvm_handle)
 {
 
-#if NOTYET
 	if (memstat_kvm_malloc(mtlp, kvm_handle) < 0)
 		return (-1);
-#endif
 	if (memstat_kvm_uma(mtlp, kvm_handle) < 0)
 		return (-1);
 	return (0);
