af1fee9b7c9a9384af7a708a354269c820d46261
Hartmut Brandt <harti@FreeBSD.org>
2005-11-10 12:07:12 +0000

    Don't include bsd.init.mk so early - it's just not needed here. Use . instead of ${.OBJDIR}. Move DEFSDIR and BMIBSDIR under the resp. .if clauses so that they get defined only if DEFS and BMIBS are defined.
    Submitted by:	ru

--- share/mk/bsd.snmpmod.mk
+++ share/mk/bsd.snmpmod.mk
@@ -1,13 +1,11 @@
 # $FreeBSD$
 
-.include <bsd.init.mk>
-
 INCSDIR=	${INCLUDEDIR}/bsnmp
 
 SHLIB_NAME=	snmp_${MOD}.so.${SHLIB_MAJOR}
 SRCS+=		${MOD}_oid.h ${MOD}_tree.c ${MOD}_tree.h
 CLEANFILES+=	${MOD}_oid.h ${MOD}_tree.c ${MOD}_tree.h
-CFLAGS+=	-I${.OBJDIR}
+CFLAGS+=	-I.
 
 ${MOD}_oid.h: ${MOD}_tree.def ${EXTRAMIBDEFS}
 	cat ${.ALLSRC} | gensnmptree -e ${XSYM} > ${.TARGET}
@@ -18,12 +16,12 @@ ${MOD}_tree.c ${MOD}_tree.h: ${MOD}_tree.def ${EXTRAMIBDEFS}
 
 .if defined(DEFS)
 FILESGROUPS+=	DEFS
-.endif
 DEFSDIR=	${SHAREDIR}/snmp/defs
+.endif
 
 .if defined(BMIBS)
 FILESGROUPS+=	BMIBS
-.endif
 BMIBSDIR=	${SHAREDIR}/snmp/mibs
+.endif
 
 .include <bsd.lib.mk>
