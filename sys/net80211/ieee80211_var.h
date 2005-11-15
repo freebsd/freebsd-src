6f322b78dedba4abafe70d5cde28e47e0238f770
Sam Leffler <sam@FreeBSD.org>
2005-11-15 05:56:32 +0000

    Update ieee80211_mhz2ieee to understand public safety bands and spectrum that can potentially be mapped to negative ieee #'s.
    NB: before operation on the latter can be supported we need to cleanup
        various code that assumes ieee channel #'s are >= 0

--- sys/net80211/ieee80211_var.h
+++ sys/net80211/ieee80211_var.h
@@ -286,8 +286,8 @@ void	ieee80211_watchdog(struct ieee80211com *);
 int	ieee80211_rate2media(struct ieee80211com *, int,
 		enum ieee80211_phymode);
 int	ieee80211_media2rate(int);
-u_int	ieee80211_mhz2ieee(u_int, u_int);
-u_int	ieee80211_chan2ieee(struct ieee80211com *, struct ieee80211_channel *);
+int	ieee80211_mhz2ieee(u_int, u_int);
+int	ieee80211_chan2ieee(struct ieee80211com *, struct ieee80211_channel *);
 u_int	ieee80211_ieee2mhz(u_int, u_int);
 int	ieee80211_setmode(struct ieee80211com *, enum ieee80211_phymode);
 enum ieee80211_phymode ieee80211_chan2mode(struct ieee80211com *,
