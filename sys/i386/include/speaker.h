8dcc24650f7bf3397727932e73b24d0926256783
ru <ru@FreeBSD.org>
2005-11-16 10:50:12 +0000

    MFC: Bring /dev/speaker support to amd64.

--- sys/i386/include/speaker.h
+++ sys/i386/include/speaker.h
@@ -1,29 +1,16 @@
 /*
- * speaker.h -- interface definitions for speaker ioctl()
- *
- * v1.4 by Eric S. Raymond (esr@snark.thyrsus.com) Aug 1993
- *      modified for FreeBSD by Andrew A. Chernov <ache@astral.msk.su>
- *
  * $FreeBSD$
  */
 
 #ifndef	_MACHINE_SPEAKER_H_
 #define	_MACHINE_SPEAKER_H_
 
-#include <sys/ioccom.h>
+#include <sys/cdefs.h>
 
-#define SPKRTONE        _IOW('S', 1, tone_t)    /* emit tone */
-#define SPKRTUNE        _IO('S', 2)             /* emit tone sequence*/
+#ifdef __CC_SUPPORTS_WARNING
+#warning "machine/speaker.h is deprecated.  Include dev/speaker/speaker.h instead."
+#endif
 
-typedef struct
-{
-    int	frequency;	/* in hertz */
-    int duration;	/* in 1/100ths of a second */
-} tone_t;
-
-/*
- * Strings written to the speaker device are interpreted as tunes and played;
- * see the spkr(4) man page for details.
- */
+#include <dev/speaker/speaker.h>
 
 #endif /* !_MACHINE_SPEAKER_H_ */
