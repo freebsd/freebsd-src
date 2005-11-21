f50b6b81b2b2905d9528be44185b6f9acb397747
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-11-21 21:50:07 +0000

    Various fixes to make de(4) not panic after ru@'s IF_LLADDR() changes: - Don't call tulip_addr_filter() to reset the RX address filter in   tulip_reset() since that gets called before ether_ifattach().  Just   call it in tulip_init_locked(). - Use be16dec() and le16dec() to parse MAC addresses when programming   the RX filter. - Let ether_ioctl() handle SIOCSIFMTU since we were doing the exact same   thing with the added bonus that we leaked the driver lock if the MTU   was > ETHERMTU in the homerolled version.  This part will be MFC'd.
    Clue from:	wpaul (1)
    Stolen from:	marcel (2 via patch for dc(4))
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@152666 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/pci/if_devar.h
+++ sys/pci/if_devar.h
@@ -837,9 +837,9 @@ static const struct {
  * endian case, just shift them left 16.
  */
 #if BYTE_ORDER == BIG_ENDIAN
-#define	TULIP_SP_MAC(x)		((x) << 16)
+#define	TULIP_SP_MAC(ptr)	(be16dec(ptr) << 16)
 #else
-#define	TULIP_SP_MAC(x)		(x)
+#define	TULIP_SP_MAC(ptr)	(le16dec(ptr))
 #endif
 
 /*
