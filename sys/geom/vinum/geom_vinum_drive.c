de257b1b7afafb048a91581cf9ab5afcb9df7886
le <le@FreeBSD.org>
2005-11-24 15:11:41 +0000

    Since we want a vinum geom created anytime the module loads, move the geom creation to a seperate init function and ignore the tasting.
    The config is now parsed only in the vinumdrive geom, which hopefully
    fixes the problem, that the drive class tasted before the vinum class
    had a chance, for good.
    
    Also restore the behaviour that the module can be loaded at boot time
    and on a running system.

--- sys/geom/vinum/geom_vinum_drive.c
+++ sys/geom/vinum/geom_vinum_drive.c
@@ -418,7 +418,7 @@ gv_drive_taste(struct g_class *mp, struct g_provider *pp, int flags __unused)
 	struct gv_freelist *fl;
 	struct gv_hdr *vhdr;
 	int error;
-	char errstr[ERRBUFSIZ];
+	char *buf, errstr[ERRBUFSIZ];
 
 	vhdr = NULL;
 	d = NULL;
@@ -460,11 +460,19 @@ gv_drive_taste(struct g_class *mp, struct g_provider *pp, int flags __unused)
 			break;
 		}
 
+		/* A valid vinum drive, let's parse the on-disk information. */
+		buf = g_read_data(cp, GV_CFG_OFFSET, GV_CFG_LEN, &error);
+		if (buf == NULL || error != 0) {
+			g_free(vhdr);
+			break;
+		}
 		g_topology_lock();
+		gv_parse_config(sc, buf, 1);
+		g_free(buf);
 
 		/*
-		 * We have found a valid vinum drive.  Let's see if it is
-		 * already known in the configuration.
+		 * Let's see if this drive is already known in the
+		 * configuration.
 		 */
 		d = gv_find_drive(sc, vhdr->label.name);
 
