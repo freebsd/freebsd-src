5baa2f368f7949afc71eb7f2728c57ac61c6fb09
glebius <glebius@FreeBSD.org>
2005-11-25 14:34:05 +0000

    MFC 1.89:   - Do not raise IFF_DRV_OACTIVE flag in vlan_start, because this     can lead to stalled interface   - Explain this fact in a comment.
      Reviewed by:    rwatson, thompsa, yar

--- sys/net/if_vlan.c
+++ sys/net/if_vlan.c
@@ -455,6 +455,14 @@ vlan_ifinit(void *foo)
 
 }
 
+/*
+ * The if_start method for vlan(4) interface. It doesn't
+ * raises the IFF_DRV_OACTIVE flag, since it is called
+ * only from IFQ_HANDOFF() macro in ether_output_frame().
+ * If the interface queue is full, and vlan_start() is
+ * not called, the queue would never get emptied and
+ * interface would stall forever.
+ */
 static void
 vlan_start(struct ifnet *ifp)
 {
@@ -467,7 +475,6 @@ vlan_start(struct ifnet *ifp)
 	ifv = ifp->if_softc;
 	p = ifv->ifv_p;
 
-	ifp->if_drv_flags |= IFF_DRV_OACTIVE;
 	for (;;) {
 		IF_DEQUEUE(&ifp->if_snd, m);
 		if (m == 0)
@@ -551,7 +558,6 @@ vlan_start(struct ifnet *ifp)
 		else
 			ifp->if_oerrors++;
 	}
-	ifp->if_drv_flags &= ~IFF_DRV_OACTIVE;
 }
 
 static void
