ee0994f6f1c2600597b5a811fe542019b0ea8bcf
le <le@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-11-25 10:09:30 +0000

    Add sysctl descriptions.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@152784 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/geom/geom_kern.c
+++ sys/geom/geom_kern.c
@@ -229,18 +229,19 @@ SYSCTL_PROC(_kern_geom, OID_AUTO, conftxt, CTLTYPE_STRING|CTLFLAG_RD,
 
 TUNABLE_INT("kern.geom.debugflags", &g_debugflags);
 SYSCTL_INT(_kern_geom, OID_AUTO, debugflags, CTLFLAG_RW,
-	&g_debugflags, 0, "");
+	&g_debugflags, 0, "Set various trace levels for GEOM debugging");
 
 SYSCTL_INT(_kern_geom, OID_AUTO, collectstats, CTLFLAG_RW,
-	&g_collectstats, 0, "");
+	&g_collectstats, 0,
+	"Control statistics collection on GEOM providers and consumers");
 
 SYSCTL_INT(_debug_sizeof, OID_AUTO, g_class, CTLFLAG_RD,
-	0, sizeof(struct g_class), "");
+	0, sizeof(struct g_class), "sizeof(struct g_class)");
 SYSCTL_INT(_debug_sizeof, OID_AUTO, g_geom, CTLFLAG_RD,
-	0, sizeof(struct g_geom), "");
+	0, sizeof(struct g_geom), "sizeof(struct g_geom)");
 SYSCTL_INT(_debug_sizeof, OID_AUTO, g_provider, CTLFLAG_RD,
-	0, sizeof(struct g_provider), "");
+	0, sizeof(struct g_provider), "sizeof(struct g_provider)");
 SYSCTL_INT(_debug_sizeof, OID_AUTO, g_consumer, CTLFLAG_RD,
-	0, sizeof(struct g_consumer), "");
+	0, sizeof(struct g_consumer), "sizeof(struct g_consumer)");
 SYSCTL_INT(_debug_sizeof, OID_AUTO, g_bioq, CTLFLAG_RD,
-	0, sizeof(struct g_bioq), "");
+	0, sizeof(struct g_bioq), "sizeof(struct g_bioq)");
