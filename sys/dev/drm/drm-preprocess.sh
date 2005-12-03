a7a99012027cc10d339c86672a5630be3478f602
anholt <anholt@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-12-03 00:43:57 +0000

    Make cleaner diffs by munging the $FreeBSD$s from the FreeBSD CVS sources back into the new sources.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@153032 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/drm/drm-preprocess.sh
+++ sys/dev/drm/drm-preprocess.sh
@@ -2,14 +2,21 @@
 
 # $FreeBSD$
 
+cvs up -CPd *.[ch]
+
+for i in `ls *.[ch]`; do
+	mv $i $i.cvs
+done
+
 cp /usr/src/drm/bsd-core/*.[ch] .
 rm i810*.[ch]
 rm via*.[ch]
 
 # Replace drm_pciids.h with one with a $FreeBSD$
+line=`grep \\\$FreeBSD drm_pciids.h.cvs`
 rm -f drm_pciids.h
 echo "/*" >> drm_pciids.h
-echo " * \$FreeBSD\$" >> drm_pciids.h
+echo "$line" >> drm_pciids.h
 echo " */" >> drm_pciids.h
 cat /usr/src/drm/bsd-core/drm_pciids.h >> drm_pciids.h
 
@@ -18,3 +25,10 @@ for i in `ls *.[ch]`; do
 	perl drm-subprocess.pl < $i.orig > $i
 done
 
+for orig in `ls *.[ch].cvs`; do
+	real=`echo "$orig" | sed s/.cvs//`
+	line=`grep __FBSDID $orig | sed s/\\\\\$/\\\\\\\\\$/g`
+	perl -pi -e "s|__FBSDID.*|$line|g" $real
+done
+
+rm *.cvs
