df734c828c125f27aaaadd19bd0eeca631f42cd4
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-12-05 22:25:41 +0000

    - Move the code to deal with handling an IPI_STOP IPI out of   ipi_nmi_handler() and into a new cpustop_handler() function.  Change   the Xcpustop IPI_STOP handler to call this function instead of   duplicating all the same logic in assembly. - EOI the local APIC for the lapic timer interrupt in C rather than   assembly. - Bump the lazypmap IPI counter if COUNT_IPIS is defined in C rather than   assembly.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@153141 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/i386/mp_machdep.c
+++ sys/i386/i386/mp_machdep.c
@@ -1276,15 +1276,29 @@ ipi_nmi_selected(u_int32_t cpus)
 }
 
 int
-ipi_nmi_handler()
+ipi_nmi_handler(void)
 {
-	int cpu = PCPU_GET(cpuid);
 	int cpumask = PCPU_GET(cpumask);
 
 	if (!(ipi_nmi_pending & cpumask))
 		return 1;
 
 	atomic_clear_int(&ipi_nmi_pending, cpumask);
+	cpustop_handler();
+	return 0;
+}
+
+#endif /* STOP_NMI */
+
+/*
+ * Handle an IPI_STOP by saving our current context and spinning until we
+ * are resumed.
+ */
+void
+cpustop_handler(void)
+{
+	int cpu = PCPU_GET(cpuid);
+	int cpumask = PCPU_GET(cpumask);
 
 	savectx(&stoppcbs[cpu]);
 
@@ -1302,11 +1316,7 @@ ipi_nmi_handler()
 		cpustop_restartfunc();
 		cpustop_restartfunc = NULL;
 	}
-
-	return 0;
 }
-     
-#endif /* STOP_NMI */
 
 /*
  * This is called once the rest of the system is up and running and we're
