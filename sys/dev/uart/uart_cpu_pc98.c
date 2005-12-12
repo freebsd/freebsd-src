fbd68c4b4ce9f404df4a819e64680f29bf0c7cce
Warner Losh <imp@FreeBSD.org>
2005-12-12 21:00:58 +0000

    Make uart_getenv() not be ns8250 dependent.  This will allow, in the future, compilation of kernels without ns8250 support but using the uart framework. These kernels will be for machines where size matters more, so including code that can never be executed is undesriable...

--- sys/dev/uart/uart_cpu_pc98.c
+++ sys/dev/uart/uart_cpu_pc98.c
@@ -52,6 +52,7 @@ uart_cpu_getdev(int devtype, struct uart_devinfo *di)
 	unsigned int i, ivar, flags;
 
 	/* Check the environment. */
+	di->ops = uart_ns8250_ops;
 	if (uart_getenv(devtype, di) == 0)
 		return (0);
 
