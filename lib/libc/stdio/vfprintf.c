75067f4f702154e49d475982591d9dcffa155afe
Poul-Henning Kamp <phk@FreeBSD.org>
2005-12-16 18:56:39 +0000

    Add an extensible version of our *printf(3) implementation to libc on probationary terms:  it may go away again if it transpires it is a bad idea.
    This extensible printf version will only be used if either
        environment variable USE_XPRINTF is defined
    or
        one of the extension functions are called.
    or
        the global variable __use_xprintf is set greater than zero.
    
    In all other cases our traditional printf implementation will
    be used.
    
    The extensible version is slower than the default printf, mostly
    because less opportunity for combining I/O operation exists when
    faced with extensions.  The default printf on the other hand
    is a bad case of spaghetti code.
    
    The extension API has a GLIBC compatible part and a FreeBSD version
    of same.  The FreeBSD version exists because the GLIBC version may
    run afoul of our FILE * locking in multithreaded programs and it
    even further eliminate the opportunities for combining I/O operations.
    
    Include three demo extensions which can be enabled if desired: time
    (%T), hexdump (%H) and strvis (%V).
    
    %T can format time_t (%T), struct timeval (%lT) and struct timespec (%llT)
       in one of two human readable duration formats:
    	"%.3llT" -> "20349.245"
    	"%#.3llT" -> "5h39m9.245"
    
    %H will hexdump a sequence of bytes and takes a pointer and a length
       argument.  The width specifies number of bytes per line.
    	"%4H" -> "65 72 20 65"
    	"%+4H" -> "0000 65 72 20 65"
    	"%#4H" -> "65 72 20 65  |er e|"
    	"%+#4H" -> "0000 65 72 20 65  |er e|"
    
    %V will dump a string in strvis format.
    	"%V" -> "Hello\tWor\377ld"	(C-style)
    	"%0V" -> "Hello\011Wor\377ld"	(octal)
    	"%+V" -> "Hello%09Wor%FFld"	(http-style)
    
    Tests, comments, bugreports etc are most welcome.

--- lib/libc/stdio/vfprintf.c
+++ lib/libc/stdio/vfprintf.c
@@ -58,6 +58,7 @@ __FBSDID("$FreeBSD$");
 #include <stdlib.h>
 #include <string.h>
 #include <wchar.h>
+#include <printf.h>
 
 #include <stdarg.h>
 #include "un-namespace.h"
@@ -466,6 +467,12 @@ __vfprintf(FILE *fp, const char *fmt0, va_list ap)
 	char sign;		/* sign prefix (' ', '+', '-', or \0) */
 	char thousands_sep;	/* locale specific thousands separator */
 	const char *grouping;	/* locale specific numeric grouping rules */
+
+	if (__use_xprintf == 0 && getenv("USE_XPRINTF"))
+		__use_xprintf = 1;
+	if (__use_xprintf > 0)
+		return (__xvprintf(fp, fmt0, ap));
+
 #ifndef NO_FLOATING_POINT
 	/*
 	 * We can decompose the printed representation of floating
