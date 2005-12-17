07616dc2874921a6f7c902f12f11349b80fae8a2
thompsa <thompsa@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2005-12-17 06:33:51 +0000

    Change from a callback in if_ethersubr to using EVENTHANDLER in order to detach span ports when they disappear. The span port does not have a pointer to the softc so revert r1.31 and bring back the softc linked-list.
    MFC after:	2 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@153494 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net/if_ethersubr.c
+++ sys/net/if_ethersubr.c
@@ -111,7 +111,6 @@ struct mbuf *(*bridge_input_p)(struct ifnet *, struct mbuf *);
 int	(*bridge_output_p)(struct ifnet *, struct mbuf *, 
 		struct sockaddr *, struct rtentry *);
 void	(*bridge_dn_p)(struct mbuf *, struct ifnet *);
-void	(*bridge_detach_p)(struct ifnet *ifp);
 
 static const u_char etherbroadcastaddr[ETHER_ADDR_LEN] =
 			{ 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
@@ -897,12 +896,6 @@ ether_ifdetach(struct ifnet *ifp)
 		KASSERT(ng_ether_detach_p != NULL,
 		    ("ng_ether_detach_p is NULL"));
 		(*ng_ether_detach_p)(ifp);
-	}
-
-	if (ifp->if_bridge) {
-		KASSERT(bridge_detach_p != NULL,
-		    ("bridge_detach_p is NULL"));
-		(*bridge_detach_p)(ifp);
 	}
 
 	bpfdetach(ifp);
