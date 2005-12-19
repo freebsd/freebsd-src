915dad0e92d925650dc579aefdea0351a9d5630a
Maxim Sobolev <sobomax@FreeBSD.org>
2005-12-19 09:00:11 +0000

    Long-long time ago, when the trees were large and memory expensive amount of memory directly available to loader(8) and friends was limited to 640K on i386. Those times have passed long time ago and now loader(8) can directly access up to 4GB of RAM at least theoretically. At the same time, there are several places where it's assumed that malloc() will only allocate memory within first megabyte.
    Remove that assumption by allocating appropriate bounce buffers for BIOS
    calls on stack where necessary.
    
    This allows using memory above first megabyte for heap if necessary.

--- sys/boot/i386/libi386/i386_copy.c
+++ sys/boot/i386/libi386/i386_copy.c
@@ -37,8 +37,6 @@ __FBSDID("$FreeBSD$");
 #include "libi386.h"
 #include "btxv86.h"
 
-#define READIN_BUF	(16 * 1024)
-
 ssize_t
 i386_copyin(const void *src, vm_offset_t dest, const size_t len)
 {
@@ -67,25 +65,11 @@ i386_copyout(const vm_offset_t src, void *dest, const size_t len)
 ssize_t
 i386_readin(const int fd, vm_offset_t dest, const size_t len)
 {
-    void	*buf;
-    size_t	resid, chunk, get;
-    ssize_t	got;
 
-    if (dest + len >= memtop)
-	return(0);
-
-    chunk = min(READIN_BUF, len);
-    buf = malloc(chunk);
-    if (buf == NULL)
-	return(0);
-
-    for (resid = len; resid > 0; resid -= got, dest += got) {
-	get = min(chunk, resid);
-	got = read(fd, buf, get);
-	if (got <= 0)
-	    break;
-	bcopy(buf, PTOV(dest), (size_t)got);
+    if (dest + len >= memtop) {
+	errno = EFBIG;
+	return(-1);
     }
-    free(buf);
-    return(len - resid);
+
+    return (read(fd, PTOV(dest), len));
 }
