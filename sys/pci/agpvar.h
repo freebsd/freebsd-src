9f22e2f35e36371aa72e2704a4470469c0e9cfff
jhb <jhb@FreeBSD.org>
2005-12-20 20:05:21 +0000

    Don't map the AGP aperture into contiguous KVA.  The various graphics drivers already map sections into KVA as needed anyway.  Note that this will probably break the nvidia driver, but I will coordinate to get that fixed.
    MFC after:	2 weeks

--- sys/pci/agpvar.h
+++ sys/pci/agpvar.h
@@ -47,7 +47,6 @@ struct agp_info {
 	u_int32_t	ai_mode;
 	vm_offset_t	ai_aperture_base;
 	vm_size_t	ai_aperture_size;
-	vm_offset_t	ai_aperture_va;
 	vm_size_t	ai_memory_allowed;
 	vm_size_t	ai_memory_used;
 	u_int32_t	ai_devid;
