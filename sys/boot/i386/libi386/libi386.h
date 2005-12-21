aaeace5d5b2863e9c631c7a3766e0f948ce5282e
sobomax <sobomax@FreeBSD.org>
2005-12-21 02:17:58 +0000

    For the cases when loading bzip2-compressed kernels enabled use last 3MB of physical memory for heap instead of range between 1MB and 4MB. This makes this feature working with PAE and amd64 kernels, which are loaded at 2MB. Teach i386_copyin() to avoid using range allocated by heap in such case, so that it won't trash heap in the low memory conditions.
    This should make loading bzip2-compressed kernels/modules/mfs images
    generally useable, so that re@ team is welcome to evaluate merits
    of using this feature in the installation CDs.
    
    Valuable suggestions by:	jhb

--- sys/boot/i386/libi386/libi386.h
+++ sys/boot/i386/libi386/libi386.h
@@ -91,7 +91,10 @@ void	bios_getsmap(void);
 void	bios_getmem(void);
 extern u_int32_t	bios_basemem;				/* base memory in bytes */
 extern u_int32_t	bios_extmem;				/* extended memory in bytes */
-extern vm_offset_t	memtop;
+extern vm_offset_t	memtop;		/* last address of physical memory + 1 */
+extern vm_offset_t	memtop_copyin;	/* memtop less heap size for the cases */
+					/*  when heap is at the top of extended memory */
+					/*  for other cases - just the same as memtop */
 
 int biospci_find_devclass(uint32_t class, int index);
 int biospci_write_config(uint32_t locator, int offset, int width, int val);
