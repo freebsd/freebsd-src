c800c1075af0f891206ec9be4a48e99172c53f19
netchild <netchild@FreeBSD.org>
2005-12-29 17:44:40 +0000

    Merge NetBSD rev. 1.61:  - Support more than 7 buttons for USB mice. Patch from NetBSD kern/30248.
    PR:		83353
    Submitted by:	SeÃ¡n Farley <sean-freebsd@farley.org>

--- sys/dev/usb/ums.c
+++ sys/dev/usb/ums.c
@@ -117,7 +117,7 @@ struct ums_softc {
 #define UMS_SPUR_BUT_UP	0x02	/* spurious button up events */
 #define UMS_T		0x04	/* aa direction available (tilt) */
 	int nbuttons;
-#define MAX_BUTTONS	7	/* chosen because sc_buttons is u_char */
+#define MAX_BUTTONS	31	/* chosen because sc_buttons is int */
 
 	u_char		qbuf[QUEUE_BUFSIZE];	/* must be divisable by 3&4 */
 	u_char		dummy[100];	/* XXX just for safety and for now */
