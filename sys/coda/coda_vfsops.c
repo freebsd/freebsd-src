93d7e294fcdb0d064bd15e9208026d662d5c43ce
maxim <maxim@FreeBSD.org>
2006-01-09 18:07:06 +0000

    o Fix typo in the define: s/MRAK_INT_GEN/MARK_INT_GEN/.  The typo was harmless because the define is not used in coda_vfsops.c.
    Submitted by:	Hugo Meiland

--- sys/coda/coda_vfsops.c
+++ sys/coda/coda_vfsops.c
@@ -75,7 +75,7 @@ struct coda_op_stats coda_vfsopstats[CODA_VFSOPS_SIZE];
 #define MARK_ENTRY(op) (coda_vfsopstats[op].entries++)
 #define MARK_INT_SAT(op) (coda_vfsopstats[op].sat_intrn++)
 #define MARK_INT_FAIL(op) (coda_vfsopstats[op].unsat_intrn++)
-#define MRAK_INT_GEN(op) (coda_vfsopstats[op].gen_intrn++)
+#define MARK_INT_GEN(op) (coda_vfsopstats[op].gen_intrn++)
 
 extern int coda_nc_initialized;     /* Set if cache has been initialized */
 extern int vc_nb_open(struct cdev *, int, int, struct thread *);
