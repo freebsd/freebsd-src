2d9cb187caf40754878c0228b8a6a4123e10b14b
sam <sam@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-01-09 17:13:20 +0000

    Update monitoring support: o record tsf in tx+rx frames o switch from raw rssi to dbm for signal data and record both   signal and noise floor data (hacked for now to assume a fixed   noise floor; is correct with new hal) o add monpass sysctl to control which rx'd frames are passed   up with errors; especially useful to see frames with CRC errors o mark 'd packets w/ a CRC error with radiotap's BADFCS flag
    Also add placeholder code for calibrating the noise floor when
    using newer hals.
    
    Reviewed by:	avatar
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@154140 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ath/if_athioctl.h
+++ sys/dev/ath/if_athioctl.h
@@ -128,24 +128,29 @@ struct ath_diag {
  * Radio capture format.
  */
 #define ATH_RX_RADIOTAP_PRESENT (		\
+	(1 << IEEE80211_RADIOTAP_TSFT)		| \
 	(1 << IEEE80211_RADIOTAP_FLAGS)		| \
 	(1 << IEEE80211_RADIOTAP_RATE)		| \
 	(1 << IEEE80211_RADIOTAP_CHANNEL)	| \
 	(1 << IEEE80211_RADIOTAP_ANTENNA)	| \
-	(1 << IEEE80211_RADIOTAP_DB_ANTSIGNAL)	| \
+	(1 << IEEE80211_RADIOTAP_DBM_ANTSIGNAL)	| \
+	(1 << IEEE80211_RADIOTAP_DBM_ANTNOISE)	| \
 	0)
 
 struct ath_rx_radiotap_header {
 	struct ieee80211_radiotap_header wr_ihdr;
-	u_int8_t	wr_flags;		/* XXX for padding */
+	u_int64_t	wr_tsf;
+	u_int8_t	wr_flags;
 	u_int8_t	wr_rate;
 	u_int16_t	wr_chan_freq;
 	u_int16_t	wr_chan_flags;
-	u_int8_t	wr_antenna;
 	u_int8_t	wr_antsignal;
+	u_int8_t	wr_antnoise;
+	u_int8_t	wr_antenna;
 };
 
 #define ATH_TX_RADIOTAP_PRESENT (		\
+	(1 << IEEE80211_RADIOTAP_TSFT)		| \
 	(1 << IEEE80211_RADIOTAP_FLAGS)		| \
 	(1 << IEEE80211_RADIOTAP_RATE)		| \
 	(1 << IEEE80211_RADIOTAP_CHANNEL)	| \
@@ -155,7 +160,8 @@ struct ath_rx_radiotap_header {
 
 struct ath_tx_radiotap_header {
 	struct ieee80211_radiotap_header wt_ihdr;
-	u_int8_t	wt_flags;		/* XXX for padding */
+	u_int64_t	wt_tsf;
+	u_int8_t	wt_flags;
 	u_int8_t	wt_rate;
 	u_int16_t	wt_chan_freq;
 	u_int16_t	wt_chan_flags;
