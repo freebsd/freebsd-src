b1eb646f8c97c8dc4fda78f74ee04d92bbe6c827
thompsa <thompsa@FreeBSD.org>
2006-01-12 21:05:30 +0000

    Include the bridge interface itself in the special arp handling.
    PR:		90973
    MFC after:	1 week

--- sys/netinet/if_ether.c
+++ sys/netinet/if_ether.c
@@ -607,7 +607,7 @@ in_arpinput(m)
 	int carp_match = 0;
 #endif
 
-	if (ifp->if_bridge)
+	if (ifp->if_bridge || ifp->if_type == IFT_BRIDGE)
 		bridged = 1;
 
 	req_len = arphdr_len2(ifp->if_addrlen, sizeof(struct in_addr));
