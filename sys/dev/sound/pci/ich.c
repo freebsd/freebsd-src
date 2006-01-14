159b68b400be7d49f2a775e488972b39a82a692b
ariff <ariff@FreeBSD.org>
2006-01-14 23:37:08 +0000

    Add another inverted EAPD quirk for Gateway 7326GZ.
    Tested by:	Jeff Cross <jeff.cross at averageadmins.com>
    MFC after:	3 days

--- sys/dev/sound/pci/ich.c
+++ sys/dev/sound/pci/ich.c
@@ -837,8 +837,9 @@ ich_pci_attach(device_t dev)
 	 * 'special' boards.
 	 */
 	switch (subdev) {
+	case 0x202f161f:	/* Gateway 7326GZ */
 	case 0x203a161f:	/* Gateway 4028GZ */
-	case 0x8144104d:	/* Sony - VAIO PCG-TR* */
+	case 0x8144104d:	/* Sony VAIO PCG-TR* */
 	case 0x8197104d:	/* Sony S1XP */
 		ac97_setflags(sc->codec, ac97_getflags(sc->codec) | AC97_F_EAPD_INV);
 		break;
