a6dc98b29897442b0461e988846816d04115ef24
rwatson <rwatson@FreeBSD.org>
2006-01-16 11:54:07 +0000

    Restore use of strncpy(), as there is later unconditional termination of the string, and reliance on the returned pointer.
    Found by:	bde (tm)

--- lib/libutil/login_times.c
+++ lib/libutil/login_times.c
@@ -72,7 +72,7 @@ parse_lt(const char * str)
 	char		buf[64];
 
 	/* Make local copy and force lowercase to simplify parsing */
-	p = strlcpy(buf, str, sizeof buf);
+	p = strncpy(buf, str, sizeof buf);
 	buf[sizeof buf - 1] = '\0';
 	for (i = 0; buf[i]; i++)
 	    buf[i] = (char)tolower(buf[i]);
