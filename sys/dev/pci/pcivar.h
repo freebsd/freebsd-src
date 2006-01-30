89253ecab9ba791b5d108c70a1e59a95eeb16ed0
Warner Losh <imp@FreeBSD.org>
2006-01-30 18:34:41 +0000

    MFC: 	1.69: Remove #ifdef alpha vestige 	1.67: Accessors for CMDREG, CACHELNSZ, MINGNT, MAXLAT and LATTIMER

--- sys/dev/pci/pcivar.h
+++ sys/dev/pci/pcivar.h
@@ -160,11 +160,6 @@ struct pci_devinfo {
 };
 #endif
 
-#ifdef __alpha__
-vm_offset_t pci_cvt_to_dense (vm_offset_t);
-vm_offset_t pci_cvt_to_bwx (vm_offset_t);
-#endif /* __alpha__ */
-
 #ifdef _SYS_BUS_H_
 
 #include "pci_if.h"
@@ -192,6 +187,11 @@ enum pci_device_ivars {
     PCI_IVAR_SLOT,
     PCI_IVAR_FUNCTION,
     PCI_IVAR_ETHADDR,
+    PCI_IVAR_CMDREG,
+    PCI_IVAR_CACHELNSZ,
+    PCI_IVAR_MINGNT,
+    PCI_IVAR_MAXLAT,
+    PCI_IVAR_LATTIMER,
 };
 
 /*
@@ -215,6 +215,11 @@ PCI_ACCESSOR(bus,		BUS,		uint8_t)
 PCI_ACCESSOR(slot,		SLOT,		uint8_t)
 PCI_ACCESSOR(function,		FUNCTION,	uint8_t)
 PCI_ACCESSOR(ether,		ETHADDR,	uint8_t *)
+PCI_ACCESSOR(cmdreg,		CMDREG,		uint8_t)
+PCI_ACCESSOR(cachelnsz,		CACHELNSZ,	uint8_t)
+PCI_ACCESSOR(mingnt,		MINGNT,		uint8_t)
+PCI_ACCESSOR(maxlat,		MAXLAT,		uint8_t)
+PCI_ACCESSOR(lattimer,		LATTIMER,	uint8_t)
 
 #undef PCI_ACCESSOR
 
