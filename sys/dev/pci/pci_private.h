6d1256149d5d6fe7da0097880b78db4c00eb6897
Warner Losh <imp@FreeBSD.org>
2006-01-30 18:38:08 +0000

    MFC: 1.16 expose pci_add_resources to the outside world (well, pci busses)

--- sys/dev/pci/pci_private.h
+++ sys/dev/pci/pci_private.h
@@ -41,6 +41,8 @@ extern devclass_t pci_devclass;
 
 void		pci_add_children(device_t dev, int busno, size_t dinfo_size);
 void		pci_add_child(device_t bus, struct pci_devinfo *dinfo);
+void		pci_add_resources(device_t bus, device_t dev, int force,
+		    uint32_t prefetchmask);
 void		pci_driver_added(device_t dev, driver_t *driver);
 int		pci_print_child(device_t dev, device_t child);
 void		pci_probe_nomatch(device_t dev, device_t child);
