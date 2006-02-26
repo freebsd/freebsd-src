c846eba8f2727cfe078920f5fca36d97dfdcd115
ume <ume@FreeBSD.org>
2006-02-26 06:17:59 +0000

    MFC: Enable NLS catalog of csh(1). The tcsh 6.14 uses libiconv to convert catalogs to appropriate charset dynamically.  However, we don't have libiconv in our tree.  So, I made some hack to load libiconv dynamically.  If libiconv is available, you can use catalogs for more locales than the locales which catalog is actually installed. To use this feature, you need to symlink catalogs to appropriate locales.  You can do this by installing ports/shells/tcsh_nls.
    	bin/csh/Makefile:			1.38
    	bin/csh/config.h:			1.13
    	bin/csh/iconv.h:			1.1
    	bin/csh/iconv_stub.c:			1.1
    	contrib/tcsh/nls/C/charset:		1.1.1.1
    	contrib/tcsh/nls/et/charset		1.1.1.1
    	contrib/tcsh/nls/finnish/charset	1.1.1.1
    	contrib/tcsh/nls/french/charset		1.1.1.1
    	contrib/tcsh/nls/german/charset		1.1.1.1
    	contrib/tcsh/nls/greek/charset		1.1.1.1
    	contrib/tcsh/nls/italian/charset	1.1.1.1
    	contrib/tcsh/nls/ja/charset		1.1.1.1
    	contrib/tcsh/nls/pl/charset		1.1.1.1
    	contrib/tcsh/nls/russian/charset	1.1.1.1
    	contrib/tcsh/nls/spanish/charset	1.1.1.1
    	contrib/tcsh/nls/ukrainian/charset	1.1.1.1
    
    Approved by:	re (hrs)

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -57,7 +57,7 @@
  *		is created, otherwise 1.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 600104	/* Master, propagated to newvers */
+#define __FreeBSD_version 600105	/* Master, propagated to newvers */
 
 #ifndef LOCORE
 #include <sys/types.h>
