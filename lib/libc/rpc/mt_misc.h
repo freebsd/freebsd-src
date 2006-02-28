374f98929acc34cccbf570d8bdaedef6e73ca696
delphij <delphij@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-02-28 07:36:25 +0000

    Don't do a time travel to 12006...
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@156103 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/rpc/mt_misc.h
+++ lib/libc/rpc/mt_misc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 12006 The FreeBSD Project. All rights reserved.
+ * Copyright (C) 2006 The FreeBSD Project. All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
