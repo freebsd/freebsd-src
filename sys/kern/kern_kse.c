d4f501f78deeaa7ff25cfc1500217b006b9ef180
Ed Maste <emaste@FreeBSD.org>
2006-02-28 14:58:36 +0000

    MFC kern_kse.c 1.215 sys_process.c 1.132 to RELENG_5:
      Fix a LOR between sched_lock and sleep queue lock.
    
    kern_kse.c 1.216:
    
      Add missing brackets.
    
    LOR ID:		060
    Approved by:	re (scottl)

--- sys/kern/kern_kse.c
+++ sys/kern/kern_kse.c
@@ -682,8 +682,8 @@ kse_create(struct thread *td, struct kse_create_args *uap)
 	 *  It may or may not use it, but its available.
 	 */
 	mtx_lock_spin(&sched_lock);
-	PROC_UNLOCK(p);
 	upcall_link(newku, newkg);
+	PROC_UNLOCK(p);
 	if (mbx.km_quantum)
 		newkg->kg_upquantum = max(1, mbx.km_quantum/tick);
 
@@ -1428,7 +1428,7 @@ thread_continued(struct proc *p)
 	struct thread *td;
 
 	PROC_LOCK_ASSERT(p, MA_OWNED);
-	mtx_assert(&sched_lock, MA_OWNED);
+	KASSERT(P_SHOULDSTOP(p), ("process not stopped"));
 
 	if (!(p->p_flag & P_SA))
 		return;
@@ -1442,11 +1442,10 @@ thread_continued(struct proc *p)
 			if (!(td->td_pflags & TDP_SA))
 				continue;
 			FOREACH_UPCALL_IN_GROUP(kg, ku) {
+				mtx_lock_spin(&sched_lock);
 				ku->ku_flags |= KUF_DOUPCALL;
+				mtx_unlock_spin(&sched_lock);
 				wakeup(&kg->kg_completed);
-				if (TD_IS_SUSPENDED(ku->ku_owner)) {
-					thread_unsuspend_one(ku->ku_owner);
-				}	
 			}
 		}
 	}
