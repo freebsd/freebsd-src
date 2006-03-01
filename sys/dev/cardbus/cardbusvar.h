1bd3ed3bb85eb9920003872b117c34f6d0a3c06e
imp <imp@FreeBSD.org>
2006-03-01 18:19:33 +0000

    Back out latest cardbus MFC.  This breaks stuff in a lot of weird and unknown ways.  Until that breakage is sorted out, be conservative.  I'm not backing this out in current so I can track down the issues.
    I'm also not backing out the pci parts of this commit because with the
    old cardbus code they work perfectly as far as I've been able to test.
    I'm leaving cardbus_device.c in place as well since I anticipate being
    able to reconnect it when I resolve this issue in head and cvs rm +
    cvs add on branches can be a pita...
    
    Approved by: re@ (scottl)

--- sys/dev/cardbus/cardbusvar.h
+++ sys/dev/cardbus/cardbusvar.h
@@ -29,12 +29,12 @@
 /*
  * Structure definitions for the Cardbus Bus driver
  */
-struct cardbus_devinfo
-{
+struct cardbus_devinfo {
 	struct pci_devinfo pci;
 	uint8_t        mprefetchable; /* bit mask of prefetchable BARs */
 	uint8_t        mbelow1mb; /* bit mask of BARs which require below 1Mb */
 	uint8_t        ibelow1mb; /* bit mask of BARs which require below 1Mb */
+#define        BARBIT(RID) (1<<(((RID)-CARDBUS_BASE0_REG)/4))
 	uint16_t	mfrid;		/* manufacturer id */
 	uint16_t	prodid;		/* product id */
 	u_int		funcid;		/* function id */
@@ -45,35 +45,3 @@ struct cardbus_devinfo
 	} funce;
 	uint32_t	fepresent;	/* bit mask of funce values present */
 };
-
-struct cis_buffer
-{
-	size_t	len;			/* Actual length of the CIS */
-	uint8_t buffer[2040];		/* small enough to be 2k */
-};
-
-struct cardbus_softc 
-{
-	/* XXX need mutex XXX */
-	device_t	sc_dev;
-	struct cdev 	*sc_cisdev;
-	struct cis_buffer *sc_cis;
-	int		sc_cis_open;
-};
-
-struct tuple_callbacks;
-
-typedef int (tuple_cb) (device_t cbdev, device_t child, int id, int len,
-		 uint8_t *tupledata, uint32_t start, uint32_t *off,
-		 struct tuple_callbacks *info, void *);
-
-struct tuple_callbacks {
-	int	id;
-	char	*name;
-	tuple_cb *func;
-};
-
-int	cardbus_device_create(struct cardbus_softc *);
-int	cardbus_device_destroy(struct cardbus_softc *);
-int	cardbus_parse_cis(device_t cbdev, device_t child,
-	    struct tuple_callbacks *callbacks, void *);
