67fbdf66a6c8e2182c48034524f2f6a220b276c9
damien <damien@FreeBSD.org>
2006-03-05 20:36:56 +0000

    Add support for the second (RT2561/RT2561S) and third (RT2661 MIMO XR) generations of 802.11abg chipsets from Ralink Technology. Get rid of the pccard front-end while I'm here since all adapters are cardbus ones.
    Obtained from:	OpenBSD

--- sys/dev/ral/rt2560var.h
+++ sys/dev/ral/rt2560var.h
@@ -17,7 +17,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-struct ral_rx_radiotap_header {
+struct rt2560_rx_radiotap_header {
 	struct ieee80211_radiotap_header wr_ihdr;
 	uint64_t	wr_tsf;
 	uint8_t		wr_flags;
@@ -28,7 +28,7 @@ struct ral_rx_radiotap_header {
 	uint8_t		wr_antsignal;
 };
 
-#define RAL_RX_RADIOTAP_PRESENT						\
+#define RT2560_RX_RADIOTAP_PRESENT					\
 	((1 << IEEE80211_RADIOTAP_TSFT) |				\
 	 (1 << IEEE80211_RADIOTAP_FLAGS) |				\
 	 (1 << IEEE80211_RADIOTAP_RATE) |				\
@@ -36,7 +36,7 @@ struct ral_rx_radiotap_header {
 	 (1 << IEEE80211_RADIOTAP_ANTENNA) |				\
 	 (1 << IEEE80211_RADIOTAP_DB_ANTSIGNAL))
 
-struct ral_tx_radiotap_header {
+struct rt2560_tx_radiotap_header {
 	struct ieee80211_radiotap_header wt_ihdr;
 	uint8_t		wt_flags;
 	uint8_t		wt_rate;
@@ -45,26 +45,26 @@ struct ral_tx_radiotap_header {
 	uint8_t		wt_antenna;
 };
 
-#define RAL_TX_RADIOTAP_PRESENT						\
+#define RT2560_TX_RADIOTAP_PRESENT					\
 	((1 << IEEE80211_RADIOTAP_FLAGS) |				\
 	 (1 << IEEE80211_RADIOTAP_RATE) |				\
 	 (1 << IEEE80211_RADIOTAP_CHANNEL) |				\
 	 (1 << IEEE80211_RADIOTAP_ANTENNA))
 
-struct ral_tx_data {
+struct rt2560_tx_data {
 	bus_dmamap_t			map;
 	struct mbuf			*m;
 	struct ieee80211_node		*ni;
 	struct ral_rssdesc		id;
 };
 
-struct ral_tx_ring {
+struct rt2560_tx_ring {
 	bus_dma_tag_t		desc_dmat;
 	bus_dma_tag_t		data_dmat;
 	bus_dmamap_t		desc_map;
 	bus_addr_t		physaddr;
-	struct ral_tx_desc	*desc;
-	struct ral_tx_data	*data;
+	struct rt2560_tx_desc	*desc;
+	struct rt2560_tx_data	*data;
 	int			count;
 	int			queued;
 	int			cur;
@@ -73,105 +73,97 @@ struct ral_tx_ring {
 	int			next_encrypt;
 };
 
-struct ral_rx_data {
+struct rt2560_rx_data {
 	bus_dmamap_t	map;
 	struct mbuf	*m;
 	int		drop;
 };
 
-struct ral_rx_ring {
+struct rt2560_rx_ring {
 	bus_dma_tag_t		desc_dmat;
 	bus_dma_tag_t		data_dmat;
 	bus_dmamap_t		desc_map;
 	bus_addr_t		physaddr;
-	struct ral_rx_desc	*desc;
-	struct ral_rx_data	*data;
+	struct rt2560_rx_desc	*desc;
+	struct rt2560_rx_data	*data;
 	int			count;
 	int			cur;
 	int			next;
 	int			cur_decrypt;
 };
 
-struct ral_node {
+struct rt2560_node {
 	struct ieee80211_node	ni;
 	struct ral_rssadapt	rssadapt;
 };
 
-struct ral_softc {
-	struct ifnet			*sc_ifp;
-	struct ieee80211com		sc_ic;
-	int				(*sc_newstate)(struct ieee80211com *,
-					    enum ieee80211_state, int);
-	device_t			sc_dev;
+struct rt2560_softc {
+	struct ifnet		*sc_ifp;
+	struct ieee80211com	sc_ic;
+	int			(*sc_newstate)(struct ieee80211com *,
+				    enum ieee80211_state, int);
+	device_t		sc_dev;
+	bus_space_tag_t		sc_st;
+	bus_space_handle_t	sc_sh;
 
-	struct mtx			sc_mtx;
+	struct mtx		sc_mtx;
 
-	struct callout			scan_ch;
-	struct callout			rssadapt_ch;
+	struct callout		scan_ch;
+	struct callout		rssadapt_ch;
 
-	int				irq_rid;
-	int				mem_rid;
-	struct resource			*irq;
-	struct resource			*mem;
-	bus_space_tag_t			sc_st;
-	bus_space_handle_t		sc_sh;
-	void				*sc_ih;
+	int			sc_tx_timer;
 
-	int				sc_tx_timer;
+	uint32_t		asic_rev;
+	uint32_t		eeprom_rev;
+	uint8_t			rf_rev;
 
-	uint32_t			asic_rev;
-	uint32_t			eeprom_rev;
-	uint8_t				rf_rev;
-
-	struct ral_tx_ring		txq;
-	struct ral_tx_ring		prioq;
-	struct ral_tx_ring		atimq;
-	struct ral_tx_ring		bcnq;
-	struct ral_rx_ring		rxq;
+	struct rt2560_tx_ring	txq;
+	struct rt2560_tx_ring	prioq;
+	struct rt2560_tx_ring	atimq;
+	struct rt2560_tx_ring	bcnq;
+	struct rt2560_rx_ring	rxq;
 
 	struct ieee80211_beacon_offsets	sc_bo;
 
-	uint32_t			rf_regs[4];
-	uint8_t				txpow[14];
+	uint32_t		rf_regs[4];
+	uint8_t			txpow[14];
 
 	struct {
-		uint8_t		reg;
-		uint8_t		val;
-	}				bbp_prom[16];
+		uint8_t	reg;
+		uint8_t	val;
+	}			bbp_prom[16];
 
-	int				led_mode;
-	int				hw_radio;
-	int				rx_ant;
-	int				tx_ant;
-	int				nb_ant;
+	int			led_mode;
+	int			hw_radio;
+	int			rx_ant;
+	int			tx_ant;
+	int			nb_ant;
 
-	int				dwelltime;
+	int			dwelltime;
 
-	struct bpf_if			*sc_drvbpf;
+	struct bpf_if		*sc_drvbpf;
 
 	union {
-		struct ral_rx_radiotap_header th;
+		struct rt2560_rx_radiotap_header th;
 		uint8_t	pad[64];
-	}				sc_rxtapu;
+	}			sc_rxtapu;
 #define sc_rxtap	sc_rxtapu.th
-	int				sc_rxtap_len;
+	int			sc_rxtap_len;
 
 	union {
-		struct ral_tx_radiotap_header th;
+		struct rt2560_tx_radiotap_header th;
 		uint8_t	pad[64];
-	}				sc_txtapu;
+	}			sc_txtapu;
 #define sc_txtap	sc_txtapu.th
-	int				sc_txtap_len;
+	int			sc_txtap_len;
 };
 
-extern devclass_t ral_devclass;
-
-int	ral_attach(device_t);
-int	ral_detach(device_t);
-void	ral_shutdown(device_t);
-int	ral_alloc(device_t, int);
-void	ral_free(device_t);
-void	ral_stop(void *);
+int	rt2560_attach(device_t, int);
+int	rt2560_detach(void *);
+void	rt2560_shutdown(void *);
+void	rt2560_suspend(void *);
+void	rt2560_resume(void *);
+void	rt2560_intr(void *);
 
 #define RAL_LOCK(sc)	mtx_lock(&(sc)->sc_mtx)
 #define RAL_UNLOCK(sc)	mtx_unlock(&(sc)->sc_mtx)
