428feeb3b95b5003d11801020d8b4e4b23f82101
Ariff Abdullah <ariff@FreeBSD.org>
2006-03-05 02:26:51 +0000

    Add another inverted amplifier sense quirk for Sony VAIO Type T.
    PR:		kern/94056
    Submitted by:	Yoshihiko Sarumaru <mistral at imasy.or.jp>
    MFC after:	3 days

--- sys/dev/sound/pci/ich.c
+++ sys/dev/sound/pci/ich.c
@@ -839,6 +839,7 @@ ich_pci_attach(device_t dev)
 	case 0x202f161f:	/* Gateway 7326GZ */
 	case 0x203a161f:	/* Gateway 4028GZ */
 	case 0x8144104d:	/* Sony VAIO PCG-TR* */
+	case 0x81c0104d:	/* Sony VAIO type T */
 	case 0x8197104d:	/* Sony S1XP */
 		ac97_setflags(sc->codec, ac97_getflags(sc->codec) | AC97_F_EAPD_INV);
 		break;
