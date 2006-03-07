adcc62544996b810d5600a0e1de6b7b1adb2d4d3
kientzle <kientzle@FreeBSD.org>
2006-03-07 06:02:09 +0000

    Fix a format mismatch (%d integer format, platform-dependent argument type).

--- usr.bin/tar/read.c
+++ usr.bin/tar/read.c
@@ -279,7 +279,7 @@ list_item_verbose(struct bsdtar *bsdtar, FILE *out, struct archive_entry *entry)
 	if (!now)
 		time(&now);
 	bsdtar_strmode(entry, tmp);
-	fprintf(out, "%s %d ", tmp, st->st_nlink);
+	fprintf(out, "%s %d ", tmp, (int)(st->st_nlink));
 
 	/* Use uname if it's present, else uid. */
 	p = archive_entry_uname(entry);
