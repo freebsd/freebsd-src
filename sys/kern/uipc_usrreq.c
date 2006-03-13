7c29a0a04d9cc0c7343a42e27f2a801329deef57
jeff <jeff@FreeBSD.org>
2006-03-13 03:06:03 +0000

    MFC Rev 1.162 VFS SMP fixes, stack api, softupdates fixes.
    Sponsored by:	Isilon Systems, Inc.
    Approved by:	re (scottl)

--- sys/kern/uipc_usrreq.c
+++ sys/kern/uipc_usrreq.c
@@ -47,6 +47,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/lock.h>
 #include <sys/mac.h>
 #include <sys/mbuf.h>
+#include <sys/mount.h>
 #include <sys/mutex.h>
 #include <sys/namei.h>
 #include <sys/proc.h>
@@ -810,9 +811,11 @@ unp_detach(struct unpcb *unp)
 		FREE(unp->unp_addr, M_SONAME);
 	uma_zfree(unp_zone, unp);
 	if (vp) {
-		mtx_lock(&Giant);
+		int vfslocked;
+
+		vfslocked = VFS_LOCK_GIANT(vp->v_mount);
 		vrele(vp);
-		mtx_unlock(&Giant);
+		VFS_UNLOCK_GIANT(vfslocked);
 	}
 	if (local_unp_rights)
 		taskqueue_enqueue(taskqueue_thread, &unp_gc_task);
