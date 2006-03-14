2a3037be480430e55c865a780b459841ca69b328
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-03-14 00:01:22 +0000

    MFi386: The SIGFPE macros were moved to signal.h (FPE_INTOVF etc)
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@156698 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/amd64/include/trap.h
+++ sys/amd64/include/trap.h
@@ -72,16 +72,6 @@
 #define	    ILL_ALIGN_FAULT	T_ALIGNFLT
 #define	    ILL_FPOP_FAULT	T_FPOPFLT	/* coprocessor operand fault */
 
-/* portable macros for SIGFPE/ARITHTRAP */
-#define FPE_INTOVF	1	/* integer overflow */
-#define FPE_INTDIV	2	/* integer divide by zero */
-#define FPE_FLTDIV	3	/* floating point divide by zero */
-#define FPE_FLTOVF	4	/* floating point overflow */
-#define FPE_FLTUND	5	/* floating point underflow */
-#define FPE_FLTRES	6	/* floating point inexact result */
-#define FPE_FLTINV	7	/* invalid floating point operation */
-#define FPE_FLTSUB	8	/* subscript out of range */
-
 /* old FreeBSD macros, deprecated */
 #define	FPE_INTOVF_TRAP	0x1	/* integer overflow */
 #define	FPE_INTDIV_TRAP	0x2	/* integer divide by zero */
