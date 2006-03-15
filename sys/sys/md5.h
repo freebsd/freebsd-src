7084bdc0e12d37cf1723915f2bfa1217df76c074
andre <andre@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-03-15 19:47:12 +0000

    Add definitions for MD5_BLOCK_LENGTH, MD5_DIGEST_LENGTH and MD5_DIGEST_STRING_LENGTH.
    MFC after:	3 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@156752 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/md5.h
+++ sys/sys/md5.h
@@ -27,6 +27,11 @@ documentation and/or software.
 
 #ifndef _SYS_MD5_H_
 #define _SYS_MD5_H_
+
+#define MD5_BLOCK_LENGTH		64
+#define MD5_DIGEST_LENGTH		16
+#define MD5_DIGEST_STRING_LENGTH	(MD5_DIGEST_LENGTH * 2 + 1)
+
 /* MD5 context. */
 typedef struct MD5Context {
   u_int32_t state[4];	/* state (ABCD) */
