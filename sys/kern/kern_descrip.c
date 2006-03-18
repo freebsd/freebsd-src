30bacc08e0ae21c1721282e3541f8f3e22d88db2
Christian S.J. Peron <csjp@FreeBSD.org>
2006-03-18 23:27:21 +0000

    Back out fd optimization introduced in revision 1.280 as it appears to be really breaking things. Simple "close(0); dup(fd)" does not return descriptor "0" in some cases. Further, this change also breaks some MAC interactions with mac_execve_will_transition().  Under certain circumstances, fdcheckstd() can be called in execve(2) causing an assertion that checks to make sure that stdin, stdout and stderr reside at indexes 0, 1 and 2 in the process fd table to fail, resulting in a kernel panic when INVARIANTS is on.
    This should also kill the "dup(2) regression on 6.x" show stopper item on the
    6.1-RELEASE TODO list.
    
    This is a RELENG_6 candidate.
    
    PR:		kern/87208
    Silence from:	des
    MFC after:	1 week

--- sys/kern/kern_descrip.c
+++ sys/kern/kern_descrip.c
@@ -1257,9 +1257,6 @@ fdalloc(struct thread *td, int minfd, int *result)
 
 	FILEDESC_LOCK_ASSERT(fdp, MA_OWNED);
 
-	if (fdp->fd_freefile > minfd)
-		minfd = fdp->fd_freefile;	   
-
 	PROC_LOCK(p);
 	maxfd = min((int)lim_cur(p, RLIMIT_NOFILE), maxfilesperproc);
 	PROC_UNLOCK(p);
@@ -1289,6 +1286,7 @@ fdalloc(struct thread *td, int minfd, int *result)
 	    ("free descriptor isn't"));
 	fdp->fd_ofileflags[fd] = 0; /* XXX needed? */
 	fdused(fdp, fd);
+	fdp->fd_freefile = fd_first_free(fdp, fd, fdp->fd_nfiles);
 	*result = fd;
 	return (0);
 }
