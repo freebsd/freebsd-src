48b072acf250e98467ea975b8605f93b6f974645
jkoshy <jkoshy@FreeBSD.org>
2006-03-22 10:25:37 +0000

    MFC {r1.19,r1.21 sys/dev/hwpmc/hwpmc_mod.c; r1.5 hwpmc_logging.c}:  - When a process is de-configuring a log file, also stop all of its    PMCs that require a log file to operate.  This change should fix    PR 90269. [1]  - When deconfiguring a log, only stop PMCs that are in the RUNNING    state. [2]
    Approved by:	re (scottl) [1], (hrs) [2]
    PR:		kern/90269

--- sys/dev/hwpmc/hwpmc_logging.c
+++ sys/dev/hwpmc/hwpmc_logging.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2005 Joseph Koshy
+ * Copyright (c) 2005-2006 Joseph Koshy
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -619,10 +619,8 @@ pmclog_deconfigure_log(struct pmc_owner *po)
 	if ((po->po_flags & PMC_PO_OWNS_LOGFILE) == 0)
 		return EINVAL;
 
-	/* remove this owner from the global SS pmc owner list */
-	if (po->po_sscount)
-		LIST_REMOVE(po, po_ssnext);
-
+	KASSERT(po->po_sscount == 0,
+	    ("[pmc,%d] po=%p still owning SS PMCs", __LINE__, po));
 	KASSERT(po->po_file != NULL,
 	    ("[pmc,%d] po=%p no log file", __LINE__, po));
 
