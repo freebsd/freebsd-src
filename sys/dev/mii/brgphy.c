e790d1654c0e2174e9fab2ec3aca65296cd523f0
oleg <oleg@FreeBSD.org>
2006-03-23 09:53:39 +0000

    Add basic support for BCM5780 PHY.
    Submitted by:	grehan
    Approved by:	glebius (mentor)
    MFC after:	1 week

--- sys/dev/mii/brgphy.c
+++ sys/dev/mii/brgphy.c
@@ -158,6 +158,12 @@ brgphy_probe(device_t dev)
 		return(0);
 	}
 
+	if (MII_OUI(ma->mii_id1, ma->mii_id2) == MII_OUI_xxBROADCOM &&
+	    MII_MODEL(ma->mii_id2) == MII_MODEL_xxBROADCOM_BCM5780) {
+		device_set_desc(dev, MII_STR_xxBROADCOM_BCM5780);
+		return(0);
+	}
+
 	return(ENXIO);
 }
 
@@ -633,6 +639,7 @@ brgphy_reset(struct mii_softc *sc)
 		break;
 	case MII_MODEL_xxBROADCOM_BCM5750:
 	case MII_MODEL_xxBROADCOM_BCM5714:
+	case MII_MODEL_xxBROADCOM_BCM5780:
 		bcm5750_load_dspcode(sc);
 		break;
 	}
