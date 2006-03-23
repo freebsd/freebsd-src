ed440ef6e41316d133fb0e6c6b2500e7a4f34479
Dag-Erling Smørgrav <des@FreeBSD.org>
2006-03-23 14:09:21 +0000

    Resolve conflicts.

--- lib/libz/minigzip.c
+++ lib/libz/minigzip.c
@@ -1,5 +1,5 @@
 /* minigzip.c -- simulate gzip using the zlib compression library
- * Copyright (C) 1995-2002 Jean-loup Gailly.
+ * Copyright (C) 1995-2005 Jean-loup Gailly.
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
@@ -22,8 +22,6 @@ __FBSDID("$FreeBSD$");
 #ifdef STDC
 #  include <string.h>
 #  include <stdlib.h>
-#else
-   extern void exit  OF((int));
 #endif
 
 #ifdef USE_MMAP
@@ -322,6 +320,8 @@ int main(argc, argv)
         break;
       argc--, argv++;
     }
+    if (outmode[3] == ' ')
+        outmode[3] = 0;
     if (argc == 0) {
         SET_BINARY_MODE(stdin);
         SET_BINARY_MODE(stdout);
