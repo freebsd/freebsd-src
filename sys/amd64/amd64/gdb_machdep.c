4b30b54f9d6c5aa5ce5386aea3e774238d85188b
marcel <marcel@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-04-04 03:00:20 +0000

    Sync with i386: Map exceptions to signals in gdb_cpu_signal() so that kgdb(1) gets a SIGTRAP when it needs to.
    Pointed out by: grehan@
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@157458 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/amd64/amd64/gdb_machdep.c
+++ sys/amd64/amd64/gdb_machdep.c
@@ -91,3 +91,27 @@ gdb_cpu_setreg(int regnum, void *val)
 			kdb_frame->tf_rip = *(register_t *)val;
 	}
 }
+
+int
+gdb_cpu_signal(int type, int code)
+{
+
+	switch (type & ~T_USER) {
+	case 0: return (SIGFPE);	/* Divide by zero. */
+	case 1: return (SIGTRAP);	/* Debug exception. */
+	case 3: return (SIGTRAP);	/* Breakpoint. */
+	case 4: return (SIGSEGV);	/* into instr. (overflow). */
+	case 5: return (SIGURG);	/* bound instruction. */
+	case 6: return (SIGILL);	/* Invalid opcode. */
+	case 7: return (SIGFPE);	/* Coprocessor not present. */
+	case 8: return (SIGEMT);	/* Double fault. */
+	case 9: return (SIGSEGV);	/* Coprocessor segment overrun. */
+	case 10: return (SIGTRAP);	/* Invalid TSS (also single-step). */
+	case 11: return (SIGSEGV);	/* Segment not present. */
+	case 12: return (SIGSEGV);	/* Stack exception. */
+	case 13: return (SIGSEGV);	/* General protection. */
+	case 14: return (SIGSEGV);	/* Page fault. */
+	case 16: return (SIGEMT);	/* Coprocessor error. */
+	}
+	return (SIGEMT);
+}
