0f2be07217364edfb3ae744e5e0b91f531148413
John Baldwin <jhb@FreeBSD.org>
2006-04-06 17:17:45 +0000

    - Don't set CR0_NE and CR0_MP in npx_probe() as they are already set   earlier in cpu_setregs(). - If we know this CPU has a FPU via cpuid, then just assume the INT16   interface and make the npx device quiet to not clutter the dmesg.  This   is true for all Pentium and later CPUs and even some of the later 486dx   CPUs.
    Reviewed by:	bde
    Tested by:	ps
    MFC after:	1 week

--- sys/i386/i386/machdep.c
+++ sys/i386/i386/machdep.c
@@ -1234,9 +1234,22 @@ cpu_setregs(void)
 	unsigned int cr0;
 
 	cr0 = rcr0();
+
 	/*
-	 * CR0_MP, CR0_NE and CR0_TS are also set by npx_probe() for the
-	 * BSP.  See the comments there about why we set them.
+	 * CR0_MP, CR0_NE and CR0_TS are set for NPX (FPU) support:
+	 *
+	 * Prepare to trap all ESC (i.e., NPX) instructions and all WAIT
+	 * instructions.  We must set the CR0_MP bit and use the CR0_TS
+	 * bit to control the trap, because setting the CR0_EM bit does
+	 * not cause WAIT instructions to trap.  It's important to trap
+	 * WAIT instructions - otherwise the "wait" variants of no-wait
+	 * control instructions would degenerate to the "no-wait" variants
+	 * after FP context switches but work correctly otherwise.  It's
+	 * particularly important to trap WAITs when there is no NPX -
+	 * otherwise the "wait" variants would always degenerate.
+	 *
+	 * Try setting CR0_NE to get correct error reporting on 486DX's.
+	 * Setting it should fail or do nothing on lesser processors.
 	 */
 	cr0 |= CR0_MP | CR0_NE | CR0_TS | CR0_WP | CR0_AM;
 	load_cr0(cr0);
