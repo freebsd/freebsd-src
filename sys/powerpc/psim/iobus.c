d3113e56cdec1a8cc09743601365b26931a559a8
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-04-20 04:19:10 +0000

    Set the rid for any resource obtained from rman_resource_reserve.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@157895 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/powerpc/psim/iobus.c
+++ sys/powerpc/psim/iobus.c
@@ -341,6 +341,7 @@ iobus_alloc_resource(device_t bus, device_t child, int type, int *rid,
 		return (NULL);
 	}
 
+	rman_set_rid(rv, *rid);
 	rman_set_bustag(rv, tagval);
 	rman_set_bushandle(rv, rman_get_start(rv));
 
