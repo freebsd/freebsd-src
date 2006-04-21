c535e79f0bfe7c6b5c69d73bbb4898d3dd6893b7
jhb <jhb@FreeBSD.org>
2006-04-21 20:39:51 +0000

    Use LIST_FOREACH().

--- sys/ddb/db_command.c
+++ sys/ddb/db_command.c
@@ -679,7 +679,7 @@ db_stack_trace_all(db_expr_t dummy, boolean_t dummy2, db_expr_t dummy3,
 
 	quit = 0;
 	db_setup_paging(db_simple_pager, &quit, db_lines_per_page);
-	for (p = LIST_FIRST(&allproc); p != NULL; p = LIST_NEXT(p, p_list)) {
+	LIST_FOREACH(p, &allproc, p_list) {
 		FOREACH_THREAD_IN_PROC(p, td) {
 			db_printf("\nTracing command %s pid %d tid %ld td %p\n",
 			    p->p_comm, p->p_pid, (long)td->td_tid, td);
