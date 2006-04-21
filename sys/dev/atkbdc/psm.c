bf03d273725844e40eed81d86aef80d1a1f8e112
takawata <takawata@FreeBSD.org>
2006-04-21 09:40:25 +0000

    MFC:  Rev 1.89 Add psm ID's for glide point devices.
    Approved by: re

--- sys/dev/atkbdc/psm.c
+++ sys/dev/atkbdc/psm.c
@@ -3565,9 +3565,12 @@ static driver_t psmcpnp_driver = {
 
 static struct isa_pnp_id psmcpnp_ids[] = {
 	{ 0x030fd041, "PS/2 mouse port" },		/* PNP0F03 */
+	{ 0x0e0fd041, "PS/2 mouse port" },		/* PNP0F0E */
+	{ 0x120fd041, "PS/2 mouse port" },		/* PNP0F12 */
 	{ 0x130fd041, "PS/2 mouse port" },		/* PNP0F13 */
 	{ 0x1303d041, "PS/2 port" },			/* PNP0313, XXX */
 	{ 0x02002e4f, "Dell PS/2 mouse port" },		/* Lat. X200, Dell */
+	{ 0x0002a906, "ALPS Glide Point" },		/* ALPS Glide Point */
 	{ 0x80374d24, "IBM PS/2 mouse port" },		/* IBM3780, ThinkPad */
 	{ 0x81374d24, "IBM PS/2 mouse port" },		/* IBM3781, ThinkPad */
 	{ 0x0190d94d, "SONY VAIO PS/2 mouse port"},     /* SNY9001, Vaio */
