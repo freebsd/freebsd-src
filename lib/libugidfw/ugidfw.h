2b6ceeb751c7316b5454e1e3eb4ee89b5b288bac
dwmalone <dwmalone@FreeBSD.org>
2006-04-23 17:06:18 +0000

    Add some new options to mac_bsdestended. We can now match on:
    	subject: ranges of uid, ranges of gid, jail id
    	objects: ranges of uid, ranges of gid, filesystem,
    		object is suid, object is sgid, object matches subject uid/gid
    		object type
    
    We can also negate individual conditions. The ruleset language is
    a superset of the previous language, so old rules should continue
    to work.
    
    These changes require a change to the API between libugidfw and the
    mac_bsdextended module. Add a version number, so we can tell if
    we're running mismatched versions.
    
    Update man pages to reflect changes, add extra test cases to
    test_ugidfw.c and add a shell script that checks that the the
    module seems to do what we expect.
    
    Suggestions from: rwatson, trhodes
    Reviewed by: trhodes
    MFC after: 2 months

--- lib/libugidfw/ugidfw.h
+++ lib/libugidfw/ugidfw.h
@@ -37,9 +37,6 @@
 __BEGIN_DECLS
 int	bsde_rule_to_string(struct mac_bsdextended_rule *rule, char *buf,
 	    size_t buflen);
-int	bsde_parse_identity(int argc, char *argv[],
-	    struct mac_bsdextended_identity *identity, size_t buflen,
-	    char *errstr);
 int	bsde_parse_mode(int argc, char *argv[], mode_t *mode, size_t buflen,
 	    char *errstr);
 int	bsde_parse_rule(int argc, char *argv[],
