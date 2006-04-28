b41dfddd47e51ddb316a0d57af69185b565e7de5
jeff <jeff@FreeBSD.org>
2006-04-28 01:05:31 +0000

    - Add a BO_NEEDSGIANT flag to the bufobj.  This flag forces all child    buffers to go on the buf daemon's DIRTYGIANT queue.  - Set BO_NEEDSGIANT on ffs's devvp since the ffs_copyonwrite handler    runs in the context of the buf daemon and may require Giant.

--- sys/sys/bufobj.h
+++ sys/sys/bufobj.h
@@ -106,6 +106,7 @@ struct bufobj {
  */
 #define	BO_ONWORKLST	(1 << 0)	/* On syncer work-list */
 #define	BO_WWAIT	(1 << 1)	/* Wait for output to complete */
+#define	BO_NEEDSGIANT	(1 << 2)	/* Require giant for child buffers. */
 
 #define	BO_LOCK(bo) \
 	do { \
