ea1fe38e6a013d16e2f00a3f51f72be6d096084a
David Malone <dwmalone@FreeBSD.org>
2006-05-02 18:32:43 +0000

    MFC: v1.45, v1.44
    Make port 37 time work on machines with 64 bit longs. Also merge a couple
    of updates to comments.

--- usr.sbin/inetd/builtins.c
+++ usr.sbin/inetd/builtins.c
@@ -64,7 +64,7 @@ static int	getline __P((int, char *, int));
 void		iderror __P((int, int, int, const char *));
 void		ident_stream __P((int, struct servtab *));
 void		initring __P((void));
-unsigned long	machtime __P((void));
+uint32_t	machtime __P((void));
 void		machtime_dg __P((int, struct servtab *));
 void		machtime_stream __P((int, struct servtab *));
 
@@ -117,6 +117,10 @@ initring()
 			*endring++ = i;
 }
 
+/* Character generator
+ * The RFC says that we should send back a random number of
+ * characters chosen from the range 0 to 512. We send LINESIZ+2.
+ */
 /* ARGSUSED */
 void
 chargen_dg(s, sep)		/* Character generator */
@@ -687,7 +691,7 @@ printit:
 }
 
 /*
- * RFC738 Time Server.
+ * RFC738/868 Time Server.
  * Return a machine readable date and time, in the form of the
  * number of seconds since midnight, Jan 1, 1900.  Since gettimeofday
  * returns the number of seconds since midnight, Jan 1, 1970,
@@ -695,7 +699,7 @@ printit:
  * some seventy years Bell Labs was asleep.
  */
 
-unsigned long
+uint32_t
 machtime()
 {
 	struct timeval tv;
@@ -705,8 +709,8 @@ machtime()
 			warnx("unable to get time of day");
 		return (0L);
 	}
-#define	OFFSET ((u_long)25567 * 24*60*60)
-	return (htonl((long)(tv.tv_sec + OFFSET)));
+#define	OFFSET ((uint32_t)25567 * 24*60*60)
+	return (htonl((uint32_t)(tv.tv_sec + OFFSET)));
 #undef OFFSET
 }
 
@@ -716,7 +720,7 @@ machtime_dg(s, sep)
 	int s;
 	struct servtab *sep;
 {
-	unsigned long result;
+	uint32_t result;
 	struct sockaddr_storage ss;
 	socklen_t size;
 
@@ -739,7 +743,7 @@ machtime_stream(s, sep)
 	int s;
 	struct servtab *sep __unused;
 {
-	unsigned long result;
+	uint32_t result;
 
 	result = machtime();
 	(void) send(s, (char *) &result, sizeof(result), MSG_EOF);
