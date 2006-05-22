986f28e5475bb7c38d31de8984e1db6954694275
ache <ache@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-05-22 05:39:57 +0000

    Make gl_offs size_t too, as required by POSIX
    Make non-standard gl_matchc in the similar fashion as gl_pathc size_t too,
    like done in NetBSD & others
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@158810 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- include/glob.h
+++ include/glob.h
@@ -45,8 +45,8 @@
 struct stat;
 typedef struct {
 	size_t gl_pathc;	/* Count of total paths so far. */
-	int gl_matchc;		/* Count of paths matching pattern. */
-	int gl_offs;		/* Reserved at beginning of gl_pathv. */
+	size_t gl_matchc;	/* Count of paths matching pattern. */
+	size_t gl_offs;		/* Reserved at beginning of gl_pathv. */
 	int gl_flags;		/* Copy of flags parameter to glob. */
 	char **gl_pathv;	/* List of paths matching pattern. */
 				/* Copy of errfunc parameter to glob. */
