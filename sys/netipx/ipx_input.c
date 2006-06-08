42b5ee0134f696d6eb62c0e3cc9552170d5ef27f
rwatson <rwatson@FreeBSD.org>
2006-06-08 23:35:42 +0000

    Merge ipx_input.c:1.55 from HEAD to RELENG_6:
      Canonicalize copyright order in one more file that contains my
      copyright.

--- sys/netipx/ipx_input.c
+++ sys/netipx/ipx_input.c
@@ -1,8 +1,9 @@
 /*-
- * Copyright (c) 2004-2005 Robert N. M. Watson
- * Copyright (c) 1995, Mike Mitchell
  * Copyright (c) 1984, 1985, 1986, 1987, 1993
- *	The Regents of the University of California.  All rights reserved.
+ *	The Regents of the University of California.
+ * Copyright (c) 1995, Mike Mitchell
+ * Copyright (c) 2004-2005 Robert N. M. Watson
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
