dc03a81318d4e2925f5b6f723b7331b46dee5b9f
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-06-13 21:36:23 +0000

    Use kern_kldload() and kern_kldunload() to load and unload modules when we intend for the user to be able to unload them later via kldunload(2) instead of calling linker_load_module() and then directly adjusting the ref count on the linker file structure.  This makes the resulting consumer code simpler and cleaner and better hides the linker internals making it possible to sanely lock the linker.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@159590 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net80211/ieee80211_freebsd.c
+++ sys/net80211/ieee80211_freebsd.c
@@ -310,14 +310,9 @@ ieee80211_notify_michael_failure(struct ieee80211com *ic,
 void
 ieee80211_load_module(const char *modname)
 {
-#ifdef notyet
-	struct thread *td = curthread;
 
-	if (suser(td) == 0 && securelevel_gt(td->td_ucred, 0) == 0) {
-		mtx_lock(&Giant);
-		(void) linker_load_module(modname, NULL, NULL, NULL, NULL);
-		mtx_unlock(&Giant);
-	}
+#ifdef notyet
+	(void)kern_kldload(curthread, modname, NULL);
 #else
 	printf("%s: load the %s module by hand for now.\n", __func__, modname);
 #endif
