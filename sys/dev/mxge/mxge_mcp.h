6d87a65da40e668f9eb78a888385f6e7741eca2c
Andrew Gallatin <gallatin@FreeBSD.org>
2006-06-13 13:53:52 +0000

    - Complete the myri10ge -> mxge name change by doing a mechanical   s/myri10ge/mxge/g replacement in the myri10ge files.  A few contuation   lines were joined because of the regained columns. - Hook the mxge driver back to the build.

--- sys/dev/mxge/mxge_mcp.h
+++ sys/dev/mxge/mxge_mcp.h
@@ -32,10 +32,10 @@ POSSIBILITY OF SUCH DAMAGE.
 $FreeBSD$
 ***************************************************************************/
 
-#ifndef _myri10ge_mcp_h
-#define _myri10ge_mcp_h
+#ifndef _mxge_mcp_h
+#define _mxge_mcp_h
 
-#ifdef MYRI10GE_MCP
+#ifdef MXGE_MCP
 typedef signed char          int8_t;
 typedef signed short        int16_t;
 typedef signed int          int32_t;
@@ -98,22 +98,22 @@ typedef struct
    The PADDED flags is set if the packet needs to be padded, and it
    must be set for all segments.
 
-   The  MYRI10GE_MCP_ETHER_FLAGS_ALIGN_ODD must be set if the cumulative
+   The  MXGE_MCP_ETHER_FLAGS_ALIGN_ODD must be set if the cumulative
    length of all previous segments was odd.
 */
 
 
-#define MYRI10GE_MCP_ETHER_FLAGS_VALID      0x1
-#define MYRI10GE_MCP_ETHER_FLAGS_FIRST      0x2
-#define MYRI10GE_MCP_ETHER_FLAGS_ALIGN_ODD  0x4
-#define MYRI10GE_MCP_ETHER_FLAGS_CKSUM      0x8
-#define MYRI10GE_MCP_ETHER_FLAGS_SMALL      0x10
-#define MYRI10GE_MCP_ETHER_FLAGS_NOT_LAST   0x100
-#define MYRI10GE_MCP_ETHER_FLAGS_TSO_HDR    0x200
-#define MYRI10GE_MCP_ETHER_FLAGS_TSO  	    0x400
+#define MXGE_MCP_ETHER_FLAGS_VALID      0x1
+#define MXGE_MCP_ETHER_FLAGS_FIRST      0x2
+#define MXGE_MCP_ETHER_FLAGS_ALIGN_ODD  0x4
+#define MXGE_MCP_ETHER_FLAGS_CKSUM      0x8
+#define MXGE_MCP_ETHER_FLAGS_SMALL      0x10
+#define MXGE_MCP_ETHER_FLAGS_NOT_LAST   0x100
+#define MXGE_MCP_ETHER_FLAGS_TSO_HDR    0x200
+#define MXGE_MCP_ETHER_FLAGS_TSO  	    0x400
 
-#define MYRI10GE_MCP_ETHER_SEND_SMALL_SIZE  1520
-#define MYRI10GE_MCP_ETHER_MAX_MTU          9400
+#define MXGE_MCP_ETHER_SEND_SMALL_SIZE  1520
+#define MXGE_MCP_ETHER_MAX_MTU          9400
 
 typedef union mcp_pso_or_cumlen
 {
@@ -121,8 +121,8 @@ typedef union mcp_pso_or_cumlen
   uint16_t cum_len;
 } mcp_pso_or_cumlen_t;
 
-#define	MYRI10GE_MCP_ETHER_MAX_SEND_DESC 12
-#define MYRI10GE_MCP_ETHER_PAD	    2
+#define	MXGE_MCP_ETHER_MAX_SEND_DESC 12
+#define MXGE_MCP_ETHER_PAD	    2
 
 /* 16 Bytes */
 typedef struct
@@ -146,63 +146,63 @@ typedef struct
 
 /* Commands */
 
-#define MYRI10GE_MCP_CMD_OFFSET 0xf80000
+#define MXGE_MCP_CMD_OFFSET 0xf80000
 
 typedef enum {
-  MYRI10GE_MCP_CMD_NONE = 0,
+  MXGE_MCP_CMD_NONE = 0,
   /* Reset the mcp, it is left in a safe state, waiting
      for the driver to set all its parameters */
-  MYRI10GE_MCP_CMD_RESET,
+  MXGE_MCP_CMD_RESET,
 
   /* get the version number of the current firmware..
      (may be available in the eeprom strings..? */
-  MYRI10GE_MCP_GET_MCP_VERSION,
+  MXGE_MCP_GET_MCP_VERSION,
 
 
   /* Parameters which must be set by the driver before it can
-     issue MYRI10GE_MCP_CMD_ETHERNET_UP. They persist until the next
-     MYRI10GE_MCP_CMD_RESET is issued */
+     issue MXGE_MCP_CMD_ETHERNET_UP. They persist until the next
+     MXGE_MCP_CMD_RESET is issued */
 
-  MYRI10GE_MCP_CMD_SET_INTRQ0_DMA,
-  MYRI10GE_MCP_CMD_SET_INTRQ1_DMA,
-  MYRI10GE_MCP_CMD_SET_BIG_BUFFER_SIZE,	/* in bytes, power of 2 */
-  MYRI10GE_MCP_CMD_SET_SMALL_BUFFER_SIZE,	/* in bytes */
+  MXGE_MCP_CMD_SET_INTRQ0_DMA,
+  MXGE_MCP_CMD_SET_INTRQ1_DMA,
+  MXGE_MCP_CMD_SET_BIG_BUFFER_SIZE,	/* in bytes, power of 2 */
+  MXGE_MCP_CMD_SET_SMALL_BUFFER_SIZE,	/* in bytes */
   
 
   /* Parameters which refer to lanai SRAM addresses where the 
      driver must issue PIO writes for various things */
 
-  MYRI10GE_MCP_CMD_GET_SEND_OFFSET,
-  MYRI10GE_MCP_CMD_GET_SMALL_RX_OFFSET,
-  MYRI10GE_MCP_CMD_GET_BIG_RX_OFFSET,
-  MYRI10GE_MCP_CMD_GET_IRQ_ACK_OFFSET,
-  MYRI10GE_MCP_CMD_GET_IRQ_DEASSERT_OFFSET,
-  MYRI10GE_MCP_CMD_GET_IRQ_ACK_DEASSERT_OFFSET,
+  MXGE_MCP_CMD_GET_SEND_OFFSET,
+  MXGE_MCP_CMD_GET_SMALL_RX_OFFSET,
+  MXGE_MCP_CMD_GET_BIG_RX_OFFSET,
+  MXGE_MCP_CMD_GET_IRQ_ACK_OFFSET,
+  MXGE_MCP_CMD_GET_IRQ_DEASSERT_OFFSET,
+  MXGE_MCP_CMD_GET_IRQ_ACK_DEASSERT_OFFSET,
 
   /* Parameters which refer to rings stored on the MCP,
      and whose size is controlled by the mcp */
 
-  MYRI10GE_MCP_CMD_GET_SEND_RING_SIZE,	/* in bytes */
-  MYRI10GE_MCP_CMD_GET_RX_RING_SIZE,		/* in bytes */
+  MXGE_MCP_CMD_GET_SEND_RING_SIZE,	/* in bytes */
+  MXGE_MCP_CMD_GET_RX_RING_SIZE,		/* in bytes */
 
   /* Parameters which refer to rings stored in the host,
      and whose size is controlled by the host.  Note that
      all must be physically contiguous and must contain 
      a power of 2 number of entries.  */
 
-  MYRI10GE_MCP_CMD_SET_INTRQ_SIZE, 	/* in bytes */
+  MXGE_MCP_CMD_SET_INTRQ_SIZE, 	/* in bytes */
 
   /* command to bring ethernet interface up.  Above parameters
      (plus mtu & mac address) must have been exchanged prior
      to issuing this command  */
-  MYRI10GE_MCP_CMD_ETHERNET_UP,
+  MXGE_MCP_CMD_ETHERNET_UP,
 
   /* command to bring ethernet interface down.  No further sends
-     or receives may be processed until an MYRI10GE_MCP_CMD_ETHERNET_UP
+     or receives may be processed until an MXGE_MCP_CMD_ETHERNET_UP
      is issued, and all interrupt queues must be flushed prior
      to ack'ing this command */
 
-  MYRI10GE_MCP_CMD_ETHERNET_DOWN,
+  MXGE_MCP_CMD_ETHERNET_DOWN,
 
   /* commands the driver may issue live, without resetting
      the nic.  Note that increasing the mtu "live" should
@@ -210,41 +210,41 @@ typedef enum {
      sufficiently large to handle the new mtu.  Decreasing
      the mtu live is safe */
 
-  MYRI10GE_MCP_CMD_SET_MTU,
-  MYRI10GE_MCP_CMD_SET_INTR_COAL_DELAY,  /* in microseconds */
-  MYRI10GE_MCP_CMD_SET_STATS_INTERVAL,   /* in microseconds */
-  MYRI10GE_MCP_CMD_SET_STATS_DMA,
+  MXGE_MCP_CMD_SET_MTU,
+  MXGE_MCP_CMD_SET_INTR_COAL_DELAY,  /* in microseconds */
+  MXGE_MCP_CMD_SET_STATS_INTERVAL,   /* in microseconds */
+  MXGE_MCP_CMD_SET_STATS_DMA,
 
-  MYRI10GE_MCP_ENABLE_PROMISC,
-  MYRI10GE_MCP_DISABLE_PROMISC,
-  MYRI10GE_MCP_SET_MAC_ADDRESS,
+  MXGE_MCP_ENABLE_PROMISC,
+  MXGE_MCP_DISABLE_PROMISC,
+  MXGE_MCP_SET_MAC_ADDRESS,
 
-  MYRI10GE_MCP_ENABLE_FLOW_CONTROL,
-  MYRI10GE_MCP_DISABLE_FLOW_CONTROL
-} myri10ge_mcp_cmd_type_t;
+  MXGE_MCP_ENABLE_FLOW_CONTROL,
+  MXGE_MCP_DISABLE_FLOW_CONTROL
+} mxge_mcp_cmd_type_t;
 
-
-typedef enum {
-  MYRI10GE_MCP_CMD_OK = 0,
-  MYRI10GE_MCP_CMD_UNKNOWN,
-  MYRI10GE_MCP_CMD_ERROR_RANGE,
-  MYRI10GE_MCP_CMD_ERROR_BUSY,
-  MYRI10GE_MCP_CMD_ERROR_EMPTY,
-  MYRI10GE_MCP_CMD_ERROR_CLOSED,
-  MYRI10GE_MCP_CMD_ERROR_HASH_ERROR,
-  MYRI10GE_MCP_CMD_ERROR_BAD_PORT,
-  MYRI10GE_MCP_CMD_ERROR_RESOURCES
-} myri10ge_mcp_cmd_status_t;
 
 typedef enum {
-  MYRI10GE_MCP_INTR_NONE = 0,
-  MYRI10GE_MCP_INTR_ETHER_SEND_DONE,
-  MYRI10GE_MCP_INTR_ETHER_RECV_SMALL,
-  MYRI10GE_MCP_INTR_ETHER_RECV_BIG,
-  MYRI10GE_MCP_INTR_LINK_CHANGE,
-  MYRI10GE_MCP_INTR_STATS_UPDATE,
-  MYRI10GE_MCP_INTR_ETHER_DOWN
-} myri10ge_mcp_intr_type_t;
+  MXGE_MCP_CMD_OK = 0,
+  MXGE_MCP_CMD_UNKNOWN,
+  MXGE_MCP_CMD_ERROR_RANGE,
+  MXGE_MCP_CMD_ERROR_BUSY,
+  MXGE_MCP_CMD_ERROR_EMPTY,
+  MXGE_MCP_CMD_ERROR_CLOSED,
+  MXGE_MCP_CMD_ERROR_HASH_ERROR,
+  MXGE_MCP_CMD_ERROR_BAD_PORT,
+  MXGE_MCP_CMD_ERROR_RESOURCES
+} mxge_mcp_cmd_status_t;
+
+typedef enum {
+  MXGE_MCP_INTR_NONE = 0,
+  MXGE_MCP_INTR_ETHER_SEND_DONE,
+  MXGE_MCP_INTR_ETHER_RECV_SMALL,
+  MXGE_MCP_INTR_ETHER_RECV_BIG,
+  MXGE_MCP_INTR_LINK_CHANGE,
+  MXGE_MCP_INTR_STATS_UPDATE,
+  MXGE_MCP_INTR_ETHER_DOWN
+} mxge_mcp_intr_type_t;
 
 
 /* 32 Bytes */
@@ -262,4 +262,4 @@ typedef struct
 } mcp_stats_t;
 
 
-#endif /* _myri10ge_mcp_h */
+#endif /* _mxge_mcp_h */
