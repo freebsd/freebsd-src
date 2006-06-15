227916affe307aa04ea45666673d3639ac3d90a3
maxim <maxim@FreeBSD.org>
2006-06-15 15:56:55 +0000

    o Make grep ^strlcpy work: put a return value type on separate line.

--- lib/libc/string/strlcpy.c
+++ lib/libc/string/strlcpy.c
@@ -41,7 +41,8 @@ __FBSDID("$FreeBSD$");
  * will be copied.  Always NUL terminates (unless siz == 0).
  * Returns strlen(src); if retval >= siz, truncation occurred.
  */
-size_t strlcpy(dst, src, siz)
+size_t
+strlcpy(dst, src, siz)
 	char *dst;
 	const char *src;
 	size_t siz;
