23748785fb931eb87bd23f77dc3786b70d681595
Christian Brueffer <brueffer@FreeBSD.org>
2006-06-19 19:40:59 +0000

    MFC: rev. 1.38
    Add altq(4) support.
    
    Approved by:	rwatson (mentor)

--- sys/dev/my/if_my.c
+++ sys/dev/my/if_my.c
@@ -886,7 +886,9 @@ my_attach(device_t dev)
 	ifp->if_watchdog = my_watchdog;
 	ifp->if_init = my_init;
 	ifp->if_baudrate = 10000000;
-	ifp->if_snd.ifq_maxlen = IFQ_MAXLEN;
+	IFQ_SET_MAXLEN(&ifp->if_snd, IFQ_MAXLEN);
+	ifp->if_snd.ifq_drv_maxlen = IFQ_MAXLEN;
+	IFQ_SET_READY(&ifp->if_snd);
 
 	if (sc->my_info->my_did == MTD803ID)
 		sc->my_pinfo = my_phys;
@@ -1306,7 +1308,7 @@ my_intr(void *arg)
 
 	/* Re-enable interrupts. */
 	CSR_WRITE_4(sc, MY_IMR, MY_INTRS);
-	if (ifp->if_snd.ifq_head != NULL)
+	if (!IFQ_DRV_IS_EMPTY(&ifp->if_snd))
 		my_start_locked(ifp);
 	MY_UNLOCK(sc);
 	return;
@@ -1406,7 +1408,7 @@ my_start_locked(struct ifnet * ifp)
 	}
 	start_tx = sc->my_cdata.my_tx_free;
 	while (sc->my_cdata.my_tx_free->my_mbuf == NULL) {
-		IF_DEQUEUE(&ifp->if_snd, m_head);
+		IFQ_DRV_DEQUEUE(&ifp->if_snd, m_head);
 		if (m_head == NULL)
 			break;
 
@@ -1693,7 +1695,7 @@ my_watchdog(struct ifnet * ifp)
 	my_stop(sc);
 	my_reset(sc);
 	my_init_locked(sc);
-	if (ifp->if_snd.ifq_head != NULL)
+	if (!IFQ_DRV_IS_EMPTY(&ifp->if_snd))
 		my_start_locked(ifp);
 	MY_LOCK(sc);
 	return;
