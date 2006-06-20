7e90b114e30399ce713a2dcdacb93865417eac34
yar <yar@FreeBSD.org>
2006-06-20 12:44:21 +0000

    We no longer need to disable interrupts in MD trap machinery when we're about to call kdb_trap() because the latter MI function can disable interrupts by itself now.
    Pointed out by:	bde
    X-MFC remark:	depends on kern/subr_kdb.c#1.18
    Sponsored by:	RiNet (Cronyx Plus LLC)

--- sys/i386/i386/trap.c
+++ sys/i386/i386/trap.c
@@ -856,16 +856,12 @@ trap_fatal(frame, eva)
 
 #ifdef KDB
 	if (debugger_on_panic || kdb_active) {
-		register_t eflags = intr_disable();
-
 		frame->tf_err = eva;	/* smuggle fault address to ddb */
 		if (kdb_trap(type, 0, frame)) {
 			frame->tf_err = code;	/* restore error code */
-			intr_restore(eflags);
 			return;
 		}
 		frame->tf_err = code;		/* restore error code */
-		intr_restore(eflags);
 	}
 #endif
 	printf("trap number		= %d\n", type);
