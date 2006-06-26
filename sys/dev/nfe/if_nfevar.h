5094b5a23219e2700799cca9f11ed33575396465
obrien <obrien@FreeBSD.org>
2006-06-26 23:41:07 +0000

    Add a pure open source nForce Ethernet driver, under BSDL. This driver was ported from OpenBSD by Shigeaki Tagashira <shigeaki@se.hiroshima-u.ac.jp> and posted at http://www.se.hiroshima-u.ac.jp/~shigeaki/software/freebsd-nfe.html It was additionally cleaned up by me. It is still a work-in-progress and thus is purposefully not in GENERIC. And it conflicts with nve(4), so only one should be loaded.

--- sys/dev/nfe/if_nfevar.h
+++ sys/dev/nfe/if_nfevar.h
@@ -14,19 +14,22 @@
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * $FreeBSD$
  */
 
-#define NFE_IFQ_MAXLEN	64
+#define	NFE_IFQ_MAXLEN	64
 
 struct nfe_tx_data {
-	bus_dmamap_t	map;
+	bus_dmamap_t	tx_data_map;
 	bus_dmamap_t	active;
+	int		nsegs;
 	struct mbuf	*m;
 };
 
 struct nfe_tx_ring {
-	bus_dmamap_t		map;
-	bus_dma_segment_t	seg;
+	bus_dmamap_t		tx_desc_map;
+	bus_dma_segment_t	tx_desc_segs;
 	bus_addr_t		physaddr;
 	struct nfe_desc32	*desc32;
 	struct nfe_desc64	*desc64;
@@ -34,6 +37,10 @@ struct nfe_tx_ring {
 	int			queued;
 	int			cur;
 	int			next;
+	bus_addr_t		tx_desc_addr;
+	bus_addr_t		tx_data_addr;
+	bus_dma_tag_t		tx_desc_tag;
+	bus_dma_tag_t		tx_data_tag;
 };
 
 struct nfe_jbuf {
@@ -43,49 +50,78 @@ struct nfe_jbuf {
 };
 
 struct nfe_rx_data {
-	bus_dmamap_t	map;
+	bus_dmamap_t	rx_data_map;
+	bus_dma_tag_t	rx_data_tag;
+	bus_addr_t	rx_data_addr;
+	bus_dma_segment_t rx_data_segs;
 	struct mbuf	*m;
 };
 
 struct nfe_rx_ring {
-	bus_dmamap_t		map;
-	bus_dma_segment_t	seg;
-	bus_dmamap_t		jmap;
-	bus_dma_segment_t	jseg;
+	bus_dmamap_t		rx_desc_map;
+	bus_dma_segment_t	rx_desc_segs;
+        bus_dma_tag_t		rx_desc_tag;
+	bus_addr_t		rx_desc_addr;
+#ifndef JMBUF
+	bus_dmamap_t		rx_jumbo_map;
+	bus_dma_segment_t	rx_jumbo_segs;
+        bus_dma_tag_t		rx_jumbo_tag;
+	bus_addr_t		rx_jumbo_addr;
+	caddr_t			jpool;
+	struct nfe_jbuf		jbuf[NFE_JPOOL_COUNT];
+	SLIST_HEAD(, nfe_jbuf)	jfreelist;
+#endif
 	bus_addr_t		physaddr;
 	struct nfe_desc32	*desc32;
 	struct nfe_desc64	*desc64;
-	caddr_t			jpool;
 	struct nfe_rx_data	data[NFE_RX_RING_COUNT];
-	struct nfe_jbuf		jbuf[NFE_JPOOL_COUNT];
-	SLIST_HEAD(, nfe_jbuf)	jfreelist;
 	int			bufsz;
 	int			cur;
 	int			next;
 };
 
 struct nfe_softc {
-	struct device		sc_dev;
-	struct arpcom		sc_arpcom;
-	bus_space_handle_t	sc_memh;
-	bus_space_tag_t		sc_memt;
-	void			*sc_ih;
-	bus_dma_tag_t		sc_dmat;
-	struct mii_data		sc_mii;
-	struct timeout		sc_tick_ch;
-	void			*sc_powerhook;
+	struct ifnet		*nfe_ifp;
+	device_t		nfe_dev;
+	device_t		nfe_miibus;
+	struct mtx		nfe_mtx;
+	bus_space_handle_t	nfe_memh;
+	bus_space_tag_t		nfe_memt;
+	struct resource		*nfe_res;
+	struct resource		*nfe_irq;
+	void			*nfe_intrhand;
+	struct mii_data		nfe_mii;
+	u_int8_t		nfe_unit;
+	struct callout		nfe_stat_ch;
 
-	int			sc_if_flags;
-	u_int			sc_flags;
-#define NFE_JUMBO_SUP	0x01
-#define NFE_40BIT_ADDR	0x02
-#define NFE_HW_CSUM	0x04
-#define NFE_HW_VLAN	0x08
-#define NFE_USE_JUMBO	0x10
+	struct arpcom		nfe_arpcom;
+	bus_dma_tag_t		nfe_parent_tag;
+  /*	struct timeout		nfe_tick_ch; */
+	void			*nfe_powerhook;
 
-	uint32_t		rxtxctl;
-	uint8_t			mii_phyaddr;
+	int			nfe_if_flags;
+	u_int			nfe_flags;
+#define	NFE_JUMBO_SUP	0x01
+#define	NFE_40BIT_ADDR	0x02
+#define	NFE_HW_CSUM	0x04
+#define	NFE_HW_VLAN	0x08
+#define	NFE_USE_JUMBO	0x10
+	u_int32_t		rxtxctl;
+	u_int8_t		mii_phyaddr;
+	u_char			eaddr[ETHER_ADDR_LEN];
+	struct task		nfe_txtask;
+	int			nfe_link;
 
 	struct nfe_tx_ring	txq;
 	struct nfe_rx_ring	rxq;
+
+#ifdef DEVICE_POLLING
+	int			rxcycles;
+#endif
+};
+
+struct nfe_type {
+	u_int16_t	vid_id;
+	u_int16_t	dev_id;
+	char		*name;
 };
