7cae024dfab3f3cfef7f4dea8de130faaffc214e
kib <kib@FreeBSD.org>
2006-07-05 16:33:25 +0000

    Back out my rev. 1.674. The better fix (rev. 1.637) is already in tree.
    Approved by:	kan (mentor)

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -785,6 +785,9 @@ vdestroy(struct vnode *vp)
 	VNASSERT(TAILQ_EMPTY(&vp->v_cache_dst), vp, ("vp has namecache dst"));
 	VNASSERT(LIST_EMPTY(&vp->v_cache_src), vp, ("vp has namecache src"));
 	VI_UNLOCK(vp);
+#ifdef MAC
+	mac_destroy_vnode(vp);
+#endif
 	if (vp->v_pollinfo != NULL) {
 		knlist_destroy(&vp->v_pollinfo->vpi_selinfo.si_note);
 		mtx_destroy(&vp->v_pollinfo->vpi_lock);
@@ -796,9 +799,6 @@ vdestroy(struct vnode *vp)
 #endif
 	lockdestroy(vp->v_vnlock);
 	mtx_destroy(&vp->v_interlock);
-#ifdef MAC
-	mac_destroy_vnode(vp);
-#endif
 	uma_zfree(vnode_zone, vp);
 }
 
