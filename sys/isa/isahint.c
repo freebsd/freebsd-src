6a2cd3a06e9b7e839f844a7f30b2c35c029c47f3
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-07-08 15:51:55 +0000

    Remove old GENERIC kludge.  We no longer need to skip devices named atkbd.  Version 1.162 of GENERIC fixed this problem in April of 1999. Subsequent to that, the hints data was removed from GENERIC and move to hints files.  All the hints file ever created have atkbd at the right location.  This should have been removed just after RELENG_4 was branched (and likely around 4.5 in RELENG_4).
    MFC After: 3 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@160184 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/isa/isahint.c
+++ sys/isa/isahint.c
@@ -95,21 +95,15 @@ isahint_identify(driver_t *driver, device_t parent)
 	 */
 	sprintf(buf, "isa%d", device_get_unit(parent));
 	i = 0;
-	while ((resource_find_match(&i, &dname, &dunit, "at", buf)) == 0) {
-		if (strcmp(dname, "atkbd") == 0)
-			continue;	/* old GENERIC kludge */
+	while ((resource_find_match(&i, &dname, &dunit, "at", buf)) == 0)
 		isahint_add_device(parent, dname, dunit);
-	}
 
 	/*
 	 * and isa?
 	 */
 	i = 0;
-	while ((resource_find_match(&i, &dname, &dunit, "at", "isa")) == 0) {
-		if (strcmp(dname, "atkbd") == 0)
-			continue;	/* old GENERIC kludge */
+	while ((resource_find_match(&i, &dname, &dunit, "at", "isa")) == 0)
 		isahint_add_device(parent, dname, dunit);
-	}
 }
 
 static device_method_t isahint_methods[] = {
