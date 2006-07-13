7f5580116297cafcfea366b3db99c00c11093ebf
davidxu <davidxu@FreeBSD.org>
2006-07-13 06:26:43 +0000

    Add syscalls thr_setscheduler, thr_getscheduler, and thr_setschedparam, these syscalls are designed to set thread's scheduling parameters and policy, because each syscall contains a size parameter, it is possible to support future scheduling option, e.g SCHED_SPORADIC, this option needs other fields in structure sched_param, current they are not avaiblable.

--- sys/sys/thr.h
+++ sys/sys/thr.h
@@ -52,8 +52,9 @@ struct thr_param {
     long	*child_tid;		/* address to store new TID. */
     long	*parent_tid;		/* parent accesses the new TID here. */
     int		flags;			/* thread flags. */
-    struct thr_sched_param *sched;	/* POSIX scheduler parameters .*/
-    void	*spare[3];		/* TODO: cpu affinity mask etc. */
+    struct thr_sched_param *sched_param;	/* POSIX scheduler parameters .*/
+    long	sched_param_size;	/* scheduler parameter size */
+    void	*spare[2];		/* TODO: cpu affinity mask etc. */
 };
 
 /* 
@@ -69,6 +70,12 @@ int thr_kill(long id, int sig);
 int thr_suspend(const struct timespec *timeout);
 int thr_wake(long id);
 int thr_set_name(long id, const char *name);
+int thr_setscheduler(long id, int policy, const struct sched_param *param,
+	int param_size);
+int thr_getscheduler(long id, int *policy, struct sched_param *param,
+	int param_size);
+int thr_setschedparam(long id, const struct sched_param *param,
+	int param_size);
 #endif /* !_KERNEL */
 
 #endif /* ! _SYS_THR_H_ */
