b31ca15b18adc8330e25b7b685ca6223e06db864
netchild <netchild@FreeBSD.org>
2006-07-15 20:22:40 +0000

    - Connect the snd_emu10kx driver to the build. [1] - Bump __FreeBSD_version, no need to build the port now.
    Submitted by:	Yuriy Tsibizov <Yuriy.Tsibizov@gfk.ru> [1]

--- sys/dev/sound/driver.c
+++ sys/dev/sound/driver.c
@@ -62,7 +62,7 @@ MODULE_DEPEND(snd_driver, snd_cs4281, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_csa, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_csapcm, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_ds1, 1, 1, 1);
-MODULE_DEPEND(snd_driver, snd_emu10k1, 1, 1, 1);
+MODULE_DEPEND(snd_driver, snd_emu10kx, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_envy24, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_es137x, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_ess, 1, 1, 1);
