d03a9644b431b4454c0575d72fcb3d32b3dd3c50
cognet <cognet@FreeBSD.org>
2006-07-20 21:28:07 +0000

    Honor ARM_BIG_ENDIAN by adding -mbig-endian to CFLAGS and -EB to LDFLAGS if it is defined.

--- share/mk/bsd.cpu.mk
+++ share/mk/bsd.cpu.mk
@@ -187,6 +187,11 @@ MACHINE_CPU = itanium
 _CPUCFLAGS += -mieee
 .endif
 
+.if ${MACHINE_ARCH} == "arm" && defined(ARM_BIG_ENDIAN)
+CFLAGS += -mbig-endian
+LDFLAGS += -EB
+.endif
+
 # NB: COPTFLAGS is handled in /usr/src/sys/conf/kern.pre.mk
 
 .if !defined(NO_CPU_CFLAGS)
