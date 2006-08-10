8f842c33cf9d17556315095946a6e48e189de8a2
glebius <glebius@FreeBSD.org>
2006-08-10 10:25:48 +0000

    MFC rev. 1.4:   Print pfsync interface status if either syncpeer or syncdev is configured.

--- sbin/ifconfig/ifpfsync.c
+++ sbin/ifconfig/ifpfsync.c
@@ -173,13 +173,18 @@ pfsync_status(int s)
 	if (ioctl(s, SIOCGETPFSYNC, (caddr_t)&ifr) == -1)
 		return;
 
-	if (preq.pfsyncr_syncdev[0] != '\0') {
-		printf("\tpfsync: syncdev: %s ", preq.pfsyncr_syncdev);
-		if (preq.pfsyncr_syncpeer.s_addr != INADDR_PFSYNC_GROUP)
-			printf("syncpeer: %s ",
-			    inet_ntoa(preq.pfsyncr_syncpeer));
+	if (preq.pfsyncr_syncdev[0] != '\0' ||
+	    preq.pfsyncr_syncpeer.s_addr != INADDR_PFSYNC_GROUP)
+			printf("\t");
+
+	if (preq.pfsyncr_syncdev[0] != '\0')
+		printf("pfsync: syncdev: %s ", preq.pfsyncr_syncdev);
+	if (preq.pfsyncr_syncpeer.s_addr != INADDR_PFSYNC_GROUP)
+		printf("syncpeer: %s ", inet_ntoa(preq.pfsyncr_syncpeer));
+
+	if (preq.pfsyncr_syncdev[0] != '\0' ||
+	    preq.pfsyncr_syncpeer.s_addr != INADDR_PFSYNC_GROUP)
 		printf("maxupd: %d\n", preq.pfsyncr_maxupdates);
-	}
 }
 
 static struct cmd pfsync_cmds[] = {
