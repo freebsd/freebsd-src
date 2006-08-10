1e99441ca949b6c9d497cc01108f91b00167a469
Gleb Smirnoff <glebius@FreeBSD.org>
2006-08-10 10:22:07 +0000

    MFC rev. 1.59:   Fix gif_output() so that GIF_UNLOCK() is performed only in case   we have locked the softc.
      PR:             kern/98298
      Submitted by:   Eugene Grosbein

--- sys/net/if_gif.c
+++ sys/net/if_gif.c
@@ -429,6 +429,7 @@ gif_output(ifp, m, dst, rt)
 
 	if (!(ifp->if_flags & IFF_UP) ||
 	    sc->gif_psrc == NULL || sc->gif_pdst == NULL) {
+		GIF_UNLOCK(sc);
 		m_freem(m);
 		error = ENETDOWN;
 		goto end;
@@ -470,13 +471,12 @@ gif_output(ifp, m, dst, rt)
 	default:
 		m_freem(m);		
 		error = ENETDOWN;
-		goto end;
 	}
 
+	GIF_UNLOCK(sc);
   end:
 	if (error)
 		ifp->if_oerrors++;
-	GIF_UNLOCK(sc);
 	return (error);
 }
 
