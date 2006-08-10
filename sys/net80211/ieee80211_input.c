0a253039e7cde89e1f6cd4f59981185d33342b20
Sam Leffler <sam@FreeBSD.org>
2006-08-10 06:21:50 +0000

    MFC 1.93: correct ie length check

--- sys/net80211/ieee80211_input.c
+++ sys/net80211/ieee80211_input.c
@@ -1818,8 +1818,8 @@ ieee80211_recv_mgmt(struct ieee80211com *ic, struct mbuf *m0,
 		scan.bchan = ieee80211_chan2ieee(ic, ic->ic_curchan);
 		scan.chan = scan.bchan;
 
-		while (frm < efrm) {
-			IEEE80211_VERIFY_LENGTH(efrm - frm, frm[1]);
+		while (efrm - frm > 1) {
+			IEEE80211_VERIFY_LENGTH(efrm - frm, frm[1] + 2);
 			switch (*frm) {
 			case IEEE80211_ELEMID_SSID:
 				scan.ssid = frm;
@@ -2065,8 +2065,8 @@ ieee80211_recv_mgmt(struct ieee80211com *ic, struct mbuf *m0,
 		 *	[tlv] extended supported rates
 		 */
 		ssid = rates = xrates = NULL;
-		while (frm < efrm) {
-			IEEE80211_VERIFY_LENGTH(efrm - frm, frm[1]);
+		while (efrm - frm > 1) {
+			IEEE80211_VERIFY_LENGTH(efrm - frm, frm[1] + 2);
 			switch (*frm) {
 			case IEEE80211_ELEMID_SSID:
 				ssid = frm;
@@ -2242,8 +2242,8 @@ ieee80211_recv_mgmt(struct ieee80211com *ic, struct mbuf *m0,
 		if (reassoc)
 			frm += 6;	/* ignore current AP info */
 		ssid = rates = xrates = wpa = wme = NULL;
-		while (frm < efrm) {
-			IEEE80211_VERIFY_LENGTH(efrm - frm, frm[1]);
+		while (efrm - frm > 1) {
+			IEEE80211_VERIFY_LENGTH(efrm - frm, frm[1] + 2);
 			switch (*frm) {
 			case IEEE80211_ELEMID_SSID:
 				ssid = frm;
@@ -2448,8 +2448,8 @@ ieee80211_recv_mgmt(struct ieee80211com *ic, struct mbuf *m0,
 		frm += 2;
 
 		rates = xrates = wpa = wme = NULL;
-		while (frm < efrm) {
-			IEEE80211_VERIFY_LENGTH(efrm - frm, frm[1]);
+		while (efrm - frm > 1) {
+			IEEE80211_VERIFY_LENGTH(efrm - frm, frm[1] + 2);
 			switch (*frm) {
 			case IEEE80211_ELEMID_RATES:
 				rates = frm;
