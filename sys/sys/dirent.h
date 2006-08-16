9815f6ee6f429ea63a67dea4c0ea673afc1cced0
maxim <maxim@FreeBSD.org>
2006-08-16 09:34:56 +0000

    o Re-word a comment.
    PR:		kern/102127
    Submitted by:	Eric Anderson

--- sys/sys/dirent.h
+++ sys/sys/dirent.h
@@ -82,7 +82,7 @@ struct dirent {
 
 /*
  * The _GENERIC_DIRSIZ macro gives the minimum record length which will hold
- * the directory entry.  This requires the amount of space in struct direct
+ * the directory entry.  This returns the amount of space in struct direct
  * without the d_name field, plus enough space for the name with a terminating
  * null byte (dp->d_namlen+1), rounded up to a 4 byte boundary.
  *
