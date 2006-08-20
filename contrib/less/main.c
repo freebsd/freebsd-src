c1917616adbff5a14e5d92da1ef32622d431ebf0
delphij <delphij@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-08-20 15:50:51 +0000

    Resolve conflicts.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@161478 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/less/main.c
+++ contrib/less/main.c
@@ -1,6 +1,6 @@
 /* $FreeBSD$ */
 /*
- * Copyright (C) 1984-2002  Mark Nudelman
+ * Copyright (C) 1984-2004  Mark Nudelman
  *
  * You may distribute under the terms of either the GNU General Public
  * License or the Less License, as specified in the README file.
@@ -120,6 +120,7 @@ main(argc, argv)
 	init_prompt();
 	init_charset();
 	init_line();
+	init_cmdhist();
 	init_option();
 	
 	if (more_mode) {
@@ -350,14 +351,14 @@ sprefix(ps, s, uppercase)
 		c = *ps;
 		if (uppercase)
 		{
-			if (len == 0 && SIMPLE_IS_LOWER(c))
+			if (len == 0 && ASCII_IS_LOWER(c))
 				return (-1);
-			if (SIMPLE_IS_UPPER(c))
-				c = SIMPLE_TO_LOWER(c);
+			if (ASCII_IS_UPPER(c))
+				c = ASCII_TO_LOWER(c);
 		}
 		sc = *s;
-		if (len > 0 && SIMPLE_IS_UPPER(sc))
-			sc = SIMPLE_TO_LOWER(sc);
+		if (len > 0 && ASCII_IS_UPPER(sc))
+			sc = ASCII_TO_LOWER(sc);
 		if (c != sc)
 			break;
 		len++;
@@ -384,6 +385,7 @@ quit(status)
 		save_status = status;
 	quitting = 1;
 	edit((char*)NULL);
+	save_cmdhist();
 	if (any_display && is_tty)
 		clear_bot();
 	deinit();
