775a3a4a3f4a8fdc18b585a42cf70e025fa5f0f2
obrien <obrien@FreeBSD.org>
2006-08-24 18:04:49 +0000

    Don't read in /etc/src.conf when building ports.
    Reported by:	obrien
    Submitted by:	ru

--- share/mk/bsd.port.mk
+++ share/mk/bsd.port.mk
@@ -3,5 +3,8 @@
 PORTSDIR?=	/usr/ports
 BSDPORTMK?=	${PORTSDIR}/Mk/bsd.port.mk
 
+# Needed to keep bsd.own.mk from reading in /etc/src.conf when building ports.
+SRCCONF=	/dev/null
+
 .include <bsd.own.mk>
 .include "${BSDPORTMK}"
