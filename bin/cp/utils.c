85f7c07c3a4c269e4633db35a021881dd5c089d7
ru <ru@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-08-25 09:42:16 +0000

    Remove a stray -a option that probably sneaked in from julian's attempt to enter append mode twice in vi(1).  :-)
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@161607 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- bin/cp/utils.c
+++ bin/cp/utils.c
@@ -426,8 +426,8 @@ usage(void)
 {
 
 	(void)fprintf(stderr, "%s\n%s\n",
-"usage: cp [-R [-H | -L | -P]] [-f | -i | -n] [-aplv] source_file target_file",
-"       cp [-R [-H | -L | -P]] [-f | -i | -n] [-aplv] source_file ... "
+"usage: cp [-R [-H | -L | -P]] [-f | -i | -n] [-plv] source_file target_file",
+"       cp [-R [-H | -L | -P]] [-f | -i | -n] [-plv] source_file ... "
 "target_directory");
 	exit(EX_USAGE);
 }
