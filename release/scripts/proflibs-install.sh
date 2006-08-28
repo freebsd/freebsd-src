22c55c25c47f849476fcb094e94ddb2eedce714e
ru <ru@FreeBSD.org>
2006-08-28 08:08:57 +0000

    Allow the DESTDIR to be specified without a terminating slash (all other install scripts allow this).

--- release/scripts/proflibs-install.sh
+++ release/scripts/proflibs-install.sh
@@ -8,7 +8,7 @@ if [ "`id -u`" != "0" ]; then
 	exit 1
 fi
 cat proflibs.?? | tar --unlink -xpzf - -C ${DESTDIR:-/}
-cd ${DESTDIR:-/}usr/lib
+cd ${DESTDIR}/usr/lib
 if [ -f libdescrypt_p.a ]
 then
 	ln -f -s libdescrypt_p.a libcrypt_p.a
