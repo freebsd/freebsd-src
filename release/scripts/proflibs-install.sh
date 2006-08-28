c2700a0ce24a5ec34999c96001e74d14166c31c3
ru <ru@FreeBSD.org>
2006-08-28 08:16:06 +0000

    MFC: Install scripts laundry.

--- release/scripts/proflibs-install.sh
+++ release/scripts/proflibs-install.sh
@@ -8,9 +8,4 @@ if [ "`id -u`" != "0" ]; then
 	exit 1
 fi
 cat proflibs.?? | tar --unlink -xpzf - -C ${DESTDIR:-/}
-cd ${DESTDIR:-/}usr/lib
-if [ -f libdescrypt_p.a ]
-then
-	ln -f -s libdescrypt_p.a libcrypt_p.a
-fi
 exit 0
