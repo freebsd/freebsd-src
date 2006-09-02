31151d8f380a1eccddb3d41009be67b97de99264
sam <sam@FreeBSD.org>
2006-09-02 15:37:09 +0000

    MFC: sync stats handling

--- sys/dev/ath/if_ath.c
+++ sys/dev/ath/if_ath.c
@@ -5107,6 +5107,9 @@ ath_ioctl(struct ifnet *ifp, u_long cmd, caddr_t data)
 		sc->sc_stats.ast_tx_packets = ifp->if_opackets;
 		sc->sc_stats.ast_rx_packets = ifp->if_ipackets;
 		sc->sc_stats.ast_rx_rssi = ieee80211_getrssi(ic);
+		sc->sc_stats.ast_rx_noise =
+			ath_hal_getchannoise(sc->sc_ah, &sc->sc_curchan);
+		sc->sc_stats.ast_tx_rate = sc->sc_hwmap[sc->sc_txrate].ieeerate;
 		ATH_UNLOCK(sc);
 		/*
 		 * NB: Drop the softc lock in case of a page fault;
