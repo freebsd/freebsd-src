6203afccbdb279e04141102ed4316bf1142b6675
Oleg Bulyzhin <oleg@FreeBSD.org>
2006-09-02 08:07:36 +0000

    MFC rev. 1.141-1.142 - Do not leak memory while flushing rules.   Noticed by:	yar
    - Remove useless NULL pointer check: we are using M_WAITOK flag
      for memory allocation.
      Submitted by:	Andrey Elsukov <bu7cher at yandex dot ru>

--- sys/netinet/ip_fw2.c
+++ sys/netinet/ip_fw2.c
@@ -3325,9 +3325,10 @@ ipfw_ctl(struct sockopt *sopt)
 		IPFW_WLOCK(&layer3_chain);
 		layer3_chain.reap = NULL;
 		free_chain(&layer3_chain, 0 /* keep default rule */);
-		rule = layer3_chain.reap, layer3_chain.reap = NULL;
+		rule = layer3_chain.reap;
+		layer3_chain.reap = NULL;
 		IPFW_WUNLOCK(&layer3_chain);
-		if (layer3_chain.reap != NULL)
+		if (rule != NULL)
 			reap_rules(rule);
 		break;
 
@@ -3447,10 +3448,6 @@ ipfw_ctl(struct sockopt *sopt)
 			}
 			size = sopt->sopt_valsize;
 			tbl = malloc(size, M_TEMP, M_WAITOK);
-			if (tbl == NULL) {
-				error = ENOMEM;
-				break;
-			}
 			error = sooptcopyin(sopt, tbl, size, sizeof(*tbl));
 			if (error) {
 				free(tbl, M_TEMP);
