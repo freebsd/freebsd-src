98409a474145c1689bbf546a50180d8bca1808e5
rwatson <rwatson@FreeBSD.org>
2006-09-03 08:06:59 +0000

    Merge kern_sig.c:1.322, sys_process.c:1.136, vfs_vnops.c:1.238 from HEAD to RELENG_6:
      Audit the arguments to the kill(2) and killpg(2) system calls.
    
      Audit the arguments to the ptrace(2) system call.
    
      Make sure that we are adding a path token to the audit record in open(2).
      Do this by making sure we are using the AUDITVNODE1 mask in the namei
      flags.
    
    Obtained from:	TrustedBSD Project

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -74,6 +74,8 @@ __FBSDID("$FreeBSD$");
 
 #include <machine/cpu.h>
 
+#include <security/audit/audit.h>
+
 #if defined (__alpha__) && !defined(COMPAT_43)
 #error "You *really* need COMPAT_43 on the alpha for longjmp(3)"
 #endif
@@ -1398,6 +1400,7 @@ kill(td, uap)
 	register struct proc *p;
 	int error;
 
+	AUDIT_ARG(signum, uap->signum);
 	if ((u_int)uap->signum > _SIG_MAXSIG)
 		return (EINVAL);
 
@@ -1407,12 +1410,14 @@ kill(td, uap)
 			if ((p = zpfind(uap->pid)) == NULL)
 				return (ESRCH);
 		}
+		AUDIT_ARG(process, p);
 		error = p_cansignal(td, p, uap->signum);
 		if (error == 0 && uap->signum)
 			psignal(p, uap->signum);
 		PROC_UNLOCK(p);
 		return (error);
 	}
+	AUDIT_ARG(pid, uap->pid);
 	switch (uap->pid) {
 	case -1:		/* broadcast signal */
 		return (killpg1(td, uap->signum, 0, 1));
@@ -1441,6 +1446,8 @@ okillpg(td, uap)
 	register struct okillpg_args *uap;
 {
 
+	AUDIT_ARG(signum, uap->signum);
+	AUDIT_ARG(pid, uap->pgid);
 	if ((u_int)uap->signum > _SIG_MAXSIG)
 		return (EINVAL);
 	return (killpg1(td, uap->signum, uap->pgid, 0));
