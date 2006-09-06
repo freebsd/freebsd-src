d398d50285c24e0ce722e2ba5aa60111972118de
Brian Somers <brian@FreeBSD.org>
2006-09-06 06:33:39 +0000

    Remove __DATE__ so that compiling the same source produces the same binary (for non-static binaries at least).

--- usr.sbin/ppp/command.c
+++ usr.sbin/ppp/command.c
@@ -545,7 +545,6 @@ command_Expand(char **nargv, int argc, char const *const *oargv,
   for (; arg < argc; arg++) {
     nargv[arg] = strdup(oargv[arg]);
     nargv[arg] = subst(nargv[arg], "AUTHNAME", bundle->cfg.auth.name);
-    nargv[arg] = subst(nargv[arg], "COMPILATIONDATE", __DATE__);
     nargv[arg] = substip(nargv[arg], "DNS0", bundle->ncp.ipcp.ns.dns[0]);
     nargv[arg] = substip(nargv[arg], "DNS1", bundle->ncp.ipcp.ns.dns[1]);
     nargv[arg] = subst(nargv[arg], "ENDDISC",
@@ -955,7 +954,7 @@ ShowStopped(struct cmdargs const *arg)
 static int
 ShowVersion(struct cmdargs const *arg)
 {
-  prompt_Printf(arg->prompt, "PPP Version %s - %s\n", Version, __DATE__);
+  prompt_Printf(arg->prompt, "PPP Version %s\n", Version);
   return 0;
 }
 
