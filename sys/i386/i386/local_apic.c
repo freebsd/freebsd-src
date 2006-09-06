fe70d0c198a097791c33d74f235093fc2ee6ea94
sobomax <sobomax@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-09-06 22:05:34 +0000

    Unbreak in the case when device apic is compiled into non-SMP kernel.
    Reported by:	jhay
    MFC after:	2 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@162087 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/i386/local_apic.c
+++ sys/i386/i386/local_apic.c
@@ -615,6 +615,7 @@ lapic_handle_timer(struct trapframe frame)
 	/* Send EOI first thing. */
 	lapic_eoi();
 
+#ifdef SMP
 	/*
 	 * Don't do any accounting for the disabled HTT cores, since it
 	 * will provide misleading numbers for the userland.
@@ -624,6 +625,7 @@ lapic_handle_timer(struct trapframe frame)
 	 */
 	if ((hlt_cpus_mask & (1 << PCPU_GET(cpuid))) != 0)
 		return;
+#endif
 
 	/* Look up our local APIC structure for the tick counters. */
 	la = &lapics[PCPU_GET(apic_id)];
