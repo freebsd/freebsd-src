96af6dbd17aa685ed488c592c37a141918c609a6
kensmith <kensmith@FreeBSD.org>
2006-09-15 19:27:40 +0000

    Begin making RELENG_6 think 6.2 exists.  It doesn't officially exist yet but this helps various folks get ready for it.
    Approved by:	re (implicit)

--- usr.sbin/pkg_install/add/main.c
+++ usr.sbin/pkg_install/add/main.c
@@ -76,6 +76,7 @@ struct {
 	{ 504000, 504099, "/packages-5.4-release" },
 	{ 600000, 600099, "/packages-6.0-release" },
 	{ 601000, 601099, "/packages-6.1-release" },
+	{ 602000, 602099, "/packages-6.2-release" },
 	{ 300000, 399000, "/packages-3-stable" },
 	{ 400000, 499000, "/packages-4-stable" },
 	{ 502100, 502128, "/packages-5-current" },
