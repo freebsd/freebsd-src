d038d0bd6679b07ae4f68628febb02f25e5d9a4e
Robert Watson <rwatson@FreeBSD.org>
2006-09-25 12:22:07 +0000

    Merge OpenBSM 1.0 alpha 12 import changes into src/sys/bsm.  New events for the Linuxulator.
    MFC after:	3 days
    Obtained from:	TrustedBSD Project

--- sys/bsm/audit_kevents.h
+++ sys/bsm/audit_kevents.h
@@ -30,7 +30,7 @@
  *
  * @APPLE_BSD_LICENSE_HEADER_END@
  *
- * $P4: //depot/projects/trustedbsd/audit3/sys/bsm/audit_kevents.h#28 $
+ * $P4: //depot/projects/trustedbsd/audit3/sys/bsm/audit_kevents.h#29 $
  * $FreeBSD$
  */
 
@@ -471,6 +471,10 @@
 #define	AUE_KENV		43114	/* FreeBSD. */
 #define	AUE_JAIL_ATTACH		43115	/* FreeBSD. */
 #define	AUE_SYSCTL_WRITE	43116	/* FreeBSD. */
+#define	AUE_IOPERM		43117	/* Linux. */
+#define	AUE_READDIR		43118	/* Linux. */
+#define	AUE_IOPL		43119	/* Linux. */
+#define	AUE_VM86		43120	/* Linux. */
 
 /*
  * Darwin BSM uses a number of AUE_O_* definitions, which are aliased to the
