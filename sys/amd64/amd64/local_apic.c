6e18641bbb034610871846ca9c3007787b1a56bc
Maxim Sobolev <sobomax@FreeBSD.org>
2006-09-26 19:12:11 +0000

    Revert 1.17.2.8, it causes some problems.
    Approved by:	re
    Reported by:	glebius

--- sys/amd64/amd64/local_apic.c
+++ sys/amd64/amd64/local_apic.c
@@ -611,18 +611,6 @@ lapic_handle_timer(struct clockframe frame)
 {
 	struct lapic *la;
 
-#ifdef SMP
-	/*
-	 * Don't do any accounting for the disabled HTT cores, since it
-	 * will provide misleading numbers for the userland.
-	 *
-	 * No locking is necessary here, since even if we loose the race
-	 * when hlt_cpus_mask changes it is not a big deal, really.
-	 */
-	if ((hlt_cpus_mask & (1 << PCPU_GET(cpuid))) != 0)
-		return;
-#endif
-
 	la = &lapics[PCPU_GET(apic_id)];
 	(*la->la_timer_count)++;
 	critical_enter();
