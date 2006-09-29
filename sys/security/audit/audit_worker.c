5f39bbc64231cfe68fd1289fcd0fce14df367092
rwatson <rwatson@FreeBSD.org>
2006-09-29 22:45:49 +0000

    Update kernel audit implementation for OpenBSM 1.0 alpha 12: use new trigger name for kernel-generated audit rotation events.
    Approved by:	re (kensmith)
    Obtained from:	TrustedBSD Project

--- sys/security/audit/audit_worker.c
+++ sys/security/audit/audit_worker.c
@@ -201,7 +201,7 @@ audit_record_write(struct vnode *vp, struct ucred *cred, struct thread *td,
 	if ((audit_fstat.af_filesz != 0) && (audit_file_rotate_wait == 0) &&
 	    (vattr.va_size >= audit_fstat.af_filesz)) {
 		audit_file_rotate_wait = 1;
-		(void)send_trigger(AUDIT_TRIGGER_OPEN_NEW);
+		(void)send_trigger(AUDIT_TRIGGER_ROTATE_KERNEL);
 	}
 
 	/*
