764b1a78fac1a30f6b42ccb1c3f9a007666129f1
maxim <maxim@FreeBSD.org>
2006-09-29 08:00:40 +0000

    o Check for a required "pathname" argument presence.
    PR:		bin/95146
    Submitted by:	candy-sendpr@kgc.co.jp
    MFC after:	3 weeks

--- sbin/ipfw/ipfw2.c
+++ sbin/ipfw/ipfw2.c
@@ -5309,6 +5309,8 @@ ipfw_readfile(int ac, char *av[])
 			 */
 			av += optind - 1;
 			ac -= optind - 1;
+			if (ac < 2)
+				errx(EX_USAGE, "no filename argument");
 			av[ac-1] = NULL;
 			fprintf(stderr, "command is %s\n", av[0]);
 			break;
