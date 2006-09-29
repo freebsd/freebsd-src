03de91198524923dce0f9f58d5af77235f340e5e
Colin Percival <cperciva@FreeBSD.org>
2006-09-29 13:46:41 +0000

    Correct problem in the 2006-09-28 patch concerning the handling of excessively large DH moduli.
    Reported by:	Steve Kiernan (Juniper SIRT)
    Security:	FreeBSD-SA-06:23.openssl
    Approved by:	so (cperciva)

--- crypto/openssl/crypto/dh/dh_key.c
+++ crypto/openssl/crypto/dh/dh_key.c
@@ -165,7 +165,7 @@ static int compute_key(unsigned char *key, const BIGNUM *pub_key, DH *dh)
 	if (BN_num_bits(dh->p) > OPENSSL_DH_MAX_MODULUS_BITS)
 		{
 		DHerr(DH_F_DH_COMPUTE_KEY,DH_R_MODULUS_TOO_LARGE);
-		goto err;
+		return -1;
 		}
 
 	ctx = BN_CTX_new();
