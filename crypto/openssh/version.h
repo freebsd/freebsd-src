9c4442f1ba1dfc05fcb1b64ab45e471656b9ed64
des <des@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-09-30 13:39:07 +0000

    Bump version addendum.
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@162857 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -6,7 +6,7 @@
 #define SSH_VERSION             (ssh_version_get())
 #define SSH_RELEASE             (ssh_version_get())
 #define SSH_VERSION_BASE        "OpenSSH_4.4p1"
-#define SSH_VERSION_ADDENDUM    "FreeBSD-20060322"
+#define SSH_VERSION_ADDENDUM    "FreeBSD-20060930"
 
 const char *ssh_version_get(void);
 void ssh_version_set_addendum(const char *add);
