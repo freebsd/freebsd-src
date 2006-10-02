80b8235497e6ada3a9c999f966f5937538782aba
matteo <matteo@FreeBSD.org>
2006-10-02 18:43:57 +0000

    MFC rev. 1.12 and rev. 1.13
    rev. 1.12 : use socklen_t where appropriate
    rev. 1.13 : Set txrx_error to 1 when we reach abort. This makes the program correctly set the exit code.
    The PR has further details on this. [2]
    
    PR:		bin/103206 [2]
    Approved by:	re@ (bmah@)

--- usr.bin/tftp/tftp.c
+++ usr.bin/tftp/tftp.c
@@ -105,7 +105,7 @@ xmitfile(fd, name, mode)
 	volatile int size, convert;
 	volatile unsigned long amount;
 	struct sockaddr_storage from;
-	int fromlen;
+	socklen_t fromlen;
 	FILE *file;
 	struct sockaddr_storage peer;
 	struct sockaddr_storage serv;	/* valid server port number */
@@ -205,6 +205,7 @@ abort:
 	stopclock();
 	if (amount > 0)
 		printstats("Sent", amount);
+	txrx_error = 1;
 }
 
 /*
@@ -223,7 +224,7 @@ recvfile(fd, name, mode)
 	volatile int size, firsttrip;
 	volatile unsigned long amount;
 	struct sockaddr_storage from;
-	int fromlen;
+	socklen_t fromlen;
 	FILE *file;
 	volatile int convert;		/* true if converting crlf -> lf */
 	struct sockaddr_storage peer;
@@ -330,6 +331,7 @@ abort:						/* ok to ack, since user */
 	stopclock();
 	if (amount > 0)
 		printstats("Received", amount);
+	txrx_error = 1;
 }
 
 static int
