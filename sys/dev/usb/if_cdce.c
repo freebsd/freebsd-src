14cf6c12665cf80b6b26fceaaddf138bdc094c0a
flz <flz@FreeBSD.org>
2006-10-07 17:35:37 +0000

    Add support for Familiar Linux powered iPaq handhelds to cdce(4).
    PR:		usb/103865
    Submitted by:	Alexey Roslyakov <internetworking@mail.ru>
    MFC after:	3 days

--- sys/dev/usb/if_cdce.c
+++ sys/dev/usb/if_cdce.c
@@ -104,6 +104,7 @@ static const struct cdce_type cdce_devs[] = {
   {{ USB_VENDOR_SHARP, USB_PRODUCT_SHARP_SLC750 }, CDCE_ZAURUS | CDCE_NO_UNION },
   {{ USB_VENDOR_GMATE, USB_PRODUCT_GMATE_YP3X00 }, CDCE_NO_UNION },
   {{ USB_VENDOR_NETCHIP, USB_PRODUCT_NETCHIP_ETHERNETGADGET }, CDCE_NO_UNION },
+  {{ USB_VENDOR_COMPAQ, USB_PRODUCT_COMPAQ_IPAQLINUX }, CDCE_NO_UNION },
 };
 #define cdce_lookup(v, p) ((const struct cdce_type *)usb_lookup(cdce_devs, v, p))
 
