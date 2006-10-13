63b6904a7f379974e186186404e57a13c9e5bf11
Doug Barton <dougb@FreeBSD.org>
2006-10-13 17:51:42 +0000

    MFC tzdata2006n

--- share/zoneinfo/yearistype.sh
+++ share/zoneinfo/yearistype.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 
-# $FreeBSD$
+: 'This file is in the public domain, so clarified as of'
+: '2006-07-17 by Arthur David Olson.'
 
-: '@(#)yearistype.sh	7.7'
+: '@(#)yearistype.sh	8.2'
 
 case $#-$1 in
 	2-|2-0*|2-*[!0-9]*)
@@ -11,7 +12,7 @@ case $#-$1 in
 esac
 
 case $#-$2 in
-	2-even)	
+	2-even)
 		case $1 in
 			*[24680])			exit 0 ;;
 			*)				exit 1 ;;
@@ -21,7 +22,7 @@ case $#-$2 in
 			*[02468][048]|*[13579][26])	exit 1 ;;
 			*)				exit 0 ;;
 		esac ;;
-	2-odd)	
+	2-odd)
 		case $1 in
 			*[13579])			exit 0 ;;
 			*)				exit 1 ;;
@@ -31,7 +32,7 @@ case $#-$2 in
 			*[02468][048]|*[13579][26])	exit 0 ;;
 			*)				exit 1 ;;
 		esac ;;
-	2-*)	
+	2-*)
 		echo "$0: wild type - $2" >&2 ;;
 esac
 
