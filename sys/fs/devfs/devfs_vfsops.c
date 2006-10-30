2c801ce044bdcbc3542ecfa1ea75b7ec728fd6d2
Konstantin Belousov <kib@FreeBSD.org>
2006-10-30 15:33:38 +0000

    MFC: rev. 1.30   src/sys/fs/devfs/devfs.h rev. 1.47   src/sys/fs/devfs/devfs_devs.c rev. 1.51   src/sys/fs/devfs/devfs_vfsops.c rev. 1.134  src/sys/fs/devfs/devfs_vnops.c
    Resolve the devfs deadlock caused by LOR between devfs_mount->dm_lock
    and vnode lock in devfs_allocv. Add hold counters for both devfs_mount
    and devfs_dirent, and DE_DOOMED flag for devfs_dirent.
    
    Approved by:	re (hrs)
    PR:             kern/102335

--- sys/fs/devfs/devfs_vfsops.c
+++ sys/fs/devfs/devfs_vfsops.c
@@ -81,6 +81,7 @@ devfs_mount(struct mount *mp, struct thread *td)
 	fmp = malloc(sizeof *fmp, M_DEVFS, M_WAITOK | M_ZERO);
 	fmp->dm_idx = alloc_unr(devfs_unr);
 	sx_init(&fmp->dm_lock, "devfsmount");
+	fmp->dm_holdcnt = 1;
 
 	MNT_ILOCK(mp);
 	mp->mnt_flag |= MNT_LOCAL;
@@ -110,14 +111,25 @@ devfs_mount(struct mount *mp, struct thread *td)
 	return (0);
 }
 
+void
+devfs_unmount_final(struct devfs_mount *fmp)
+{
+	sx_destroy(&fmp->dm_lock);
+	free(fmp, M_DEVFS);
+}
+
 static int
 devfs_unmount(struct mount *mp, int mntflags, struct thread *td)
 {
 	int error;
 	int flags = 0;
 	struct devfs_mount *fmp;
+	int hold;
+	u_int idx;
 
 	fmp = VFSTODEVFS(mp);
+	KASSERT(fmp->dm_mount != NULL,
+		("devfs_unmount unmounted devfs_mount"));
 	/* There is 1 extra root vnode reference from devfs_mount(). */
 	error = vflush(mp, 1, flags, td);
 	if (error)
@@ -125,11 +137,14 @@ devfs_unmount(struct mount *mp, int mntflags, struct thread *td)
 	sx_xlock(&fmp->dm_lock);
 	devfs_cleanup(fmp);
 	devfs_rules_cleanup(fmp);
-	sx_xunlock(&fmp->dm_lock);
+	fmp->dm_mount = NULL;
+	hold = --fmp->dm_holdcnt;
 	mp->mnt_data = NULL;
-	sx_destroy(&fmp->dm_lock);
-	free_unr(devfs_unr, fmp->dm_idx);
-	free(fmp, M_DEVFS);
+	idx = fmp->dm_idx;
+	sx_xunlock(&fmp->dm_lock);
+	free_unr(devfs_unr, idx);
+	if (hold == 0)
+		devfs_unmount_final(fmp);
 	return 0;
 }
 
@@ -143,6 +158,7 @@ devfs_root(struct mount *mp, int flags, struct vnode **vpp, struct thread *td)
 	struct devfs_mount *dmp;
 
 	dmp = VFSTODEVFS(mp);
+	sx_xlock(&dmp->dm_lock);
 	error = devfs_allocv(dmp->dm_rootdir, mp, &vp, td);
 	if (error)
 		return (error);
