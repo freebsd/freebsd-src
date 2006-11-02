657669576694b95bd1ea4df0d3b94b9245f5e1c4
Marcel Moolenaar <marcel@FreeBSD.org>
2006-11-02 02:42:48 +0000

    Sync the EFI headers with version 1.10.14.62 of the Intel sample EFI implementation. This re-introduces C99 style comments that previously were replaced by original C comments.

--- sys/boot/efi/include/efidebug.h
+++ sys/boot/efi/include/efidebug.h
@@ -4,7 +4,14 @@
 
 /*++
 
-Copyright (c) 1998  Intel Corporation
+Copyright (c)  1999 - 2002 Intel Corporation. All rights reserved
+This software and associated documentation (if any) is furnished
+under a license and may only be used or copied in accordance
+with the terms of the license. Except as permitted by such
+license, no part of this software or documentation may be
+reproduced, stored in a retrieval system, or transmitted in any
+form or by any means without the express written consent of
+Intel Corporation.
 
 Module Name:
 
@@ -44,31 +51,37 @@ extern UINTN     EFIDebug;
 
 #endif
 
-#define D_INIT        0x00000001          /* Initialization style messages */
-#define D_WARN        0x00000002          /* Warnings */
-#define D_LOAD        0x00000004          /* Load events */
-#define D_FS          0x00000008          /* EFI File system */
-#define D_POOL        0x00000010          /* Alloc & Free's */
-#define D_PAGE        0x00000020          /* Alloc & Free's */
-#define D_INFO        0x00000040          /* Verbose */
-#define D_VAR         0x00000100          /* Variable */
-#define D_PARSE       0x00000200          /* Command parsing */
-#define D_BM          0x00000400          /* Boot manager */
-#define D_BLKIO       0x00001000          /* BlkIo Driver */
-#define D_BLKIO_ULTRA 0x00002000          /* BlkIo Driver */
-#define D_NET         0x00004000          /* SNI Driver */
-#define D_NET_ULTRA   0x00008000          /* SNI Driver */
-#define D_TXTIN       0x00010000          /* Simple Input Driver */
-#define D_TXTOUT      0x00020000          /* Simple Text Output Driver */
-#define D_ERROR_ATA   0x00040000	  /* ATA error messages */
-#define D_ERROR       0x80000000          /* Error */
+#define D_INIT        0x00000001          // Initialization style messages
+#define D_WARN        0x00000002          // Warnings
+#define D_LOAD        0x00000004          // Load events
+#define D_FS          0x00000008          // EFI File system
+#define D_POOL        0x00000010          // Alloc & Free's
+#define D_PAGE        0x00000020          // Alloc & Free's
+#define D_INFO        0x00000040          // Verbose
+#define D_VARIABLE    0x00000100          // Variable
+#define D_VAR         0x00000100          // Variable
+#define D_BM          0x00000400          // Boot Manager 
+#define D_BLKIO       0x00001000          // BlkIo Driver
+#define D_BLKIO_ULTRA 0x00002000          // BlkIo Driver
+#define D_NET         0x00004000          // SNI Driver
+#define D_NET_ULTRA   0x00008000          // SNI Driver
+#define D_UNDI        0x00010000          // UNDI Driver
+#define D_LOADFILE    0x00020000          // UNDI Driver
+#define D_EVENT       0x00080000          // Event messages
 
-#define D_RESERVED    0x7fffC880          /* Bits not reserved above */
+#define D_ERROR       0x80000000          // Error
 
-/*
- * Current Debug level of the system, value of EFIDebug
- */
+#define D_RESERVED    0x7ff40A80          // Bits not reserved above
+
+//
+// Current Debug level of the system, value of EFIDebug
+//
+//#define EFI_DBUG_MASK   (D_ERROR | D_WARN | D_LOAD | D_BLKIO | D_INIT)
 #define EFI_DBUG_MASK   (D_ERROR)
+
+//
+//
+//
 
 #if EFI_DEBUG
 
@@ -84,9 +97,9 @@ extern UINTN     EFIDebug;
 
 #endif
 
-/*
- * Prototypes
- */
+//
+// Prototypes
+//
 
 INTN
 DbgAssert (
--- sys/boot/efi/include/efistdarg.h
+++ sys/boot/efi/include/efistdarg.h
@@ -4,7 +4,14 @@
 
 /*++
 
-Copyright (c) 1998  Intel Corporation
+Copyright (c)  1999 - 2002 Intel Corporation. All rights reserved
+This software and associated documentation (if any) is furnished
+under a license and may only be used or copied in accordance
+with the terms of the license. Except as permitted by such
+license, no part of this software or documentation may be
+reproduced, stored in a retrieval system, or transmitted in any
+form or by any means without the express written consent of
+Intel Corporation.
 
 Module Name:
 
