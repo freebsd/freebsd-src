d203a1d908310a7d01b7c307026594bc1f46aa05
rrs <rrs@FreeBSD.org>
2006-11-03 17:21:53 +0000

    Opps... in my fix up of all the $FreeBSD:$-> $FreeBSD$ I inserted a few to the new files.. but I falied to add the #include <sys/cdef.h>
    Which causes a compile error.. sorry about that... got it
    now :-)
    
    Approved by:gnn

--- sys/netinet/sctp_os_bsd.h
+++ sys/netinet/sctp_os_bsd.h
@@ -27,6 +27,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
+#include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 #ifndef __sctp_os_bsd_h__
 #define __sctp_os_bsd_h__
