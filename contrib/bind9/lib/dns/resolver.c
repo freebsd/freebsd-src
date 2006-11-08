5008de223bd232f790e1bea584e232ff90f1deae
dougb <dougb@FreeBSD.org>
2006-11-08 01:32:16 +0000

    MFC the upgrade to BIND 9.3.2-P2. This is mostly a noop in the base, since if you're compiling the base with this upgraded, you've already got your openssl upgraded, but it's a good idea to include this for completeness sake.
    Approved by:	re (hrs)

--- contrib/bind9/lib/dns/resolver.c
+++ contrib/bind9/lib/dns/resolver.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: resolver.c,v 1.218.2.18.4.56 2005/10/14 01:38:48 marka Exp $ */
+/* $Id: resolver.c,v 1.218.2.18.4.56.4.2 2006/10/04 07:06:02 marka Exp $ */
 
 #include <config.h>
 
