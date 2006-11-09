e6d544d42d686ac7c0497d92ca618649c27938dd
imp <imp@FreeBSD.org>
2006-11-09 20:32:36 +0000

    MFp4: boot on KB9202 correctly.  Also, reduce the size of the SD/MMC driver somewhat.

--- sys/boot/arm/at91/libat91/memcmp.c
+++ sys/boot/arm/at91/libat91/memcmp.c
@@ -21,11 +21,11 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * This software is derived from software provide by Kwikbyte who specifically
- * disclaimed copyright on the code.
- *
- * $FreeBSD$
  */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
 #include "lib.h"
 
 int
