f486315183c122f23f1b35b42ac8dd3c2b635caa
des <des@FreeBSD.org>
2006-11-10 16:52:41 +0000

    Resolve conflicts.

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -1,12 +1,12 @@
-/* $OpenBSD: version.h,v 1.47 2006/08/30 00:14:37 djm Exp $ */
+/* $OpenBSD: version.h,v 1.48 2006/11/07 10:31:31 markus Exp $ */
 /* $FreeBSD$ */
 
 #ifndef SSH_VERSION
 
 #define SSH_VERSION             (ssh_version_get())
 #define SSH_RELEASE             (ssh_version_get())
-#define SSH_VERSION_BASE        "OpenSSH_4.4p1"
-#define SSH_VERSION_ADDENDUM    "FreeBSD-20060930"
+#define SSH_VERSION_BASE        "OpenSSH_4.5p1"
+#define SSH_VERSION_ADDENDUM    "FreeBSD-20061110"
 
 const char *ssh_version_get(void);
 void ssh_version_set_addendum(const char *add);
