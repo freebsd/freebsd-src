6295d7ccd0b29c4bcc9450a52b9ee5bcb69eafeb
marius <marius@FreeBSD.org>
2006-11-13 17:24:24 +0000

    MFC: sys/dev/dc/if_dc.c 1.185, 1.186 (partial); sys/dev/dc/if_dcreg.h 1.52
    - Consistently use if_printf() only in interface methods: if_start(),
      if_watchdog, etc., or in functions used only in these methods.
      In all other functions in the driver use device_printf().
    - Use __func__ instead of typing function name.
    - Wrap code optimized for architectures without alignment constraints
      in #ifdef __NO_STRICT_ALIGNMENT rather than #ifdef __i386__. This
      means that amd64 now also uses the optimized code.
      While at it, fix a nearby style(9) bug.
    - In dc_setcfg() suppress printing a warning when forcing the receiver
      and transceiver to idle state times out for chips where the status
      bits in question just never change (observed in detail with DM9102A)
      and therefore the warning would be highly likely false positive.
    - In dc_ifmedia_sts() add a missing DC_UNLOCK().
    
    Approved by:	re (kensmith)

--- sys/pci/if_dc.c
+++ sys/pci/if_dc.c
@@ -334,7 +334,7 @@ static driver_t dc_driver = {
 };
 
 static devclass_t dc_devclass;
-#ifdef __i386__
+#ifdef __NO_STRICT_ALIGNMENT
 static int dc_quick = 1;
 SYSCTL_INT(_hw, OID_AUTO, dc_quick, CTLFLAG_RW, &dc_quick, 0,
     "do not m_devget() in dc driver");
@@ -1388,9 +1388,20 @@ dc_setcfg(struct dc_softc *sc, int media)
 			DELAY(10);
 		}
 
-		if (i == DC_TIMEOUT)
-			if_printf(sc->dc_ifp,
-			    "failed to force tx and rx to idle state\n");
+		if (i == DC_TIMEOUT) {
+			if (!(isr & DC_ISR_TX_IDLE) && !DC_IS_ASIX(sc))
+				device_printf(sc->dc_dev,
+				    "%s: failed to force tx to idle state\n",
+				    __func__);
+			if (!((isr & DC_ISR_RX_STATE) == DC_RXSTATE_STOPPED ||
+			    (isr & DC_ISR_RX_STATE) == DC_RXSTATE_WAIT) &&
+			    !(DC_IS_CENTAUR(sc) || DC_IS_CONEXANT(sc) ||
+			    (DC_IS_DAVICOM(sc) && pci_get_revid(sc->dc_dev) >=
+			    DC_REVISION_DM9102A)))
+				device_printf(sc->dc_dev,
+				    "%s: failed to force rx to idle state\n",
+				    __func__);
+		}
 	}
 
 	if (IFM_SUBTYPE(media) == IFM_100_TX) {
@@ -1530,7 +1541,7 @@ dc_reset(struct dc_softc *sc)
 	}
 
 	if (i == DC_TIMEOUT)
-		if_printf(sc->dc_ifp, "reset never completed!\n");
+		device_printf(sc->dc_dev, "reset never completed!\n");
 
 	/* Wait a little while for the chip to get its brains in order. */
 	DELAY(1000);
@@ -1809,6 +1820,7 @@ dc_attach(device_t dev)
 	u_int8_t *mac;
 
 	sc = device_get_softc(dev);
+	sc->dc_dev = dev;
 
 	mtx_init(&sc->dc_mtx, device_get_nameunit(dev), MTX_NETWORK_LOCK,
 	    MTX_DEF);
@@ -2670,7 +2682,7 @@ dc_rx_resync(struct dc_softc *sc)
 static void
 dc_rxeof(struct dc_softc *sc)
 {
-	struct mbuf *m;
+	struct mbuf *m, *m0;
 	struct ifnet *ifp;
 	struct dc_desc *cur_rx;
 	int i, total_len = 0;
@@ -2740,9 +2752,9 @@ dc_rxeof(struct dc_softc *sc)
 
 		/* No errors; receive the packet. */
 		total_len -= ETHER_CRC_LEN;
-#ifdef __i386__
+#ifdef __NO_STRICT_ALIGNMENT
 		/*
-		 * On the x86 we do not have alignment problems, so try to
+		 * On architectures without alignment problems we try to
 		 * allocate a new buffer for the receive ring, and pass up
 		 * the one where the packet is already, saving the expensive
 		 * copy done in m_devget().
@@ -2750,15 +2762,13 @@ dc_rxeof(struct dc_softc *sc)
 		 * if the allocation fails, then use m_devget and leave the
 		 * existing buffer in the receive ring.
 		 */
-		if (dc_quick && dc_newbuf(sc, i, 1) == 0) {
+		if (dc_newbuf(sc, i, 1) == 0) {
 			m->m_pkthdr.rcvif = ifp;
 			m->m_pkthdr.len = m->m_len = total_len;
 			DC_INC(i, DC_RX_LIST_CNT);
 		} else
 #endif
 		{
-			struct mbuf *m0;
-
 			m0 = m_devget(mtod(m, char *), total_len,
 				ETHER_ALIGN, ifp, NULL);
 			dc_newbuf(sc, i, 0);
@@ -2990,13 +3000,14 @@ dc_tx_underrun(struct dc_softc *sc)
 			DELAY(10);
 		}
 		if (i == DC_TIMEOUT) {
-			if_printf(sc->dc_ifp,
-			    "failed to force tx to idle state\n");
+			device_printf(sc->dc_dev,
+			    "%s: failed to force tx to idle state\n",
+			    __func__);
 			dc_init_locked(sc);
 		}
 	}
 
-	if_printf(sc->dc_ifp, "TX underrun -- ");
+	device_printf(sc->dc_dev, "TX underrun -- ");
 	sc->dc_txthresh += DC_TXTHRESH_INC;
 	if (sc->dc_txthresh > DC_TXTHRESH_MAX) {
 		printf("using store and forward mode\n");
@@ -3062,7 +3073,7 @@ dc_poll(struct ifnet *ifp, enum poll_cmd cmd, int count)
 			dc_tx_underrun(sc);
 
 		if (status & DC_ISR_BUS_ERR) {
-			if_printf(ifp, "dc_poll: bus error\n");
+			if_printf(ifp, "%s: bus error\n", __func__);
 			dc_reset(sc);
 			dc_init_locked(sc);
 		}
@@ -3480,7 +3491,7 @@ dc_init_locked(struct dc_softc *sc)
 
 	/* Init circular RX list. */
 	if (dc_list_rx_init(sc) == ENOBUFS) {
-		if_printf(ifp,
+		device_printf(sc->dc_dev,
 		    "initialization failed: no memory for rx buffers\n");
 		dc_stop(sc);
 		return;
@@ -3601,6 +3612,7 @@ dc_ifmedia_sts(struct ifnet *ifp, struct ifmediareq *ifmr)
 		if (IFM_SUBTYPE(ifm->ifm_media) == IFM_HPNA_1) {
 			ifmr->ifm_active = ifm->ifm_media;
 			ifmr->ifm_status = 0;
+			DC_UNLOCK(sc);
 			return;
 		}
 	}
