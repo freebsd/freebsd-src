a33d60eacda11cf9f28b7c9c3920b5478a4831fc
kmacy <kmacy@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-11-14 07:57:19 +0000

    Add Kris Kennaway to reflect his performance improvements to the lock profiling code
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@164270 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/lock_profile.h
+++ sys/sys/lock_profile.h
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2006 Kip Macy kmacy@FreeBSD.org
+ * Copyright (c) 2006 Kris Kennaway kris@FreeBSD.org
  * Copyright (c) 2006 Dag-Erling Smorgrav des@des.no
  *
  * Redistribution and use in source and binary forms, with or without
