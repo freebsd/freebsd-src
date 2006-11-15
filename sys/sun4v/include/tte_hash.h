b49b5ca95ca67e93b1768f73d7f0c7908f92f569
kmacy <kmacy@FreeBSD.org>
2006-11-15 06:29:52 +0000

    Heavily re-factor tte_hash to remove redundant code Add hash resizing support - doesn't quite work yet

--- sys/sun4v/include/tte_hash.h
+++ sys/sun4v/include/tte_hash.h
@@ -71,5 +71,8 @@ uint64_t tte_hash_set_scratchpad_user(tte_hash_t th, uint64_t context);
 
 tte_t tte_hash_update(tte_hash_t hash, vm_offset_t va, tte_t tte_data);
 
+int tte_hash_needs_resize(tte_hash_t th);
+
+tte_hash_t  tte_hash_resize(tte_hash_t th, uint64_t *scratchval);
 
 #endif /* _MACHINE_TTE_HASH_H_ */
