aa5f07668982ccea8739f22df497ca76bc40a56b
ru <ru@FreeBSD.org>
2006-11-19 16:28:52 +0000

    Try harder to not leak src/ build stuff into ports/ environment by not exposing the MK_* variables that were designed for src/.
    Requested by:	many

--- share/mk/bsd.port.mk
+++ share/mk/bsd.port.mk
@@ -3,8 +3,9 @@
 PORTSDIR?=	/usr/ports
 BSDPORTMK?=	${PORTSDIR}/Mk/bsd.port.mk
 
-# Needed to keep bsd.own.mk from reading in /etc/src.conf when building ports.
-SRCCONF=	/dev/null
+# Needed to keep bsd.own.mk from reading in /etc/src.conf
+# and setting MK_* variables when building ports.
+_WITHOUT_SRCCONF=
 
 .include <bsd.own.mk>
 .include "${BSDPORTMK}"
