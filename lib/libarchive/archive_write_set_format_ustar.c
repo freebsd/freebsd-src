43baed1581e985e25d2e3a62c79ac10083a5c3d1
Tim Kientzle <kientzle@FreeBSD.org>
2006-11-26 05:39:28 +0000

    Unbreak libarchive on arm.  Two parts of libarchive relied on a traditional shortcut of defining on-disk layouts using structures of character arrays. Unfortunately, as recently discussed on cvs-all@, this usage is not actually sanctioned by the standards and specifically fails on GCC/arm (unless your data structures happen to be "naturally aligned").
    The new code defines offsets/sizes for data fields and accesses
    them using explicit pointer arithmetic, instead of casting to
    a structure and accessing structure fields.  In particular,
    the new code is now clean with WARNS=6 on arm.
    
    MFC after: 14 days

--- lib/libarchive/archive_write_set_format_ustar.c
+++ lib/libarchive/archive_write_set_format_ustar.c
@@ -61,56 +61,94 @@ struct ustar {
 /*
  * Define structure of POSIX 'ustar' tar header.
  */
-struct archive_entry_header_ustar {
-	char	name[100];
-	char	mode[6];
-	char	mode_padding[2];
-	char	uid[6];
-	char	uid_padding[2];
-	char	gid[6];
-	char	gid_padding[2];
-	char	size[11];
-	char	size_padding[1];
-	char	mtime[11];
-	char	mtime_padding[1];
-	char	checksum[8];
-	char	typeflag[1];
-	char	linkname[100];
-	char	magic[6]; /* For POSIX: "ustar\0" */
-	char	version[2]; /* For POSIX: "00" */
-	char	uname[32];
-	char	gname[32];
-	char	rdevmajor[6];
-	char	rdevmajor_padding[2];
-	char	rdevminor[6];
-	char	rdevminor_padding[2];
-	char	prefix[155];
-	char	padding[12];
-};
+#define	USTAR_name_offset 0
+#define	USTAR_name_size 100
+#define	USTAR_mode_offset 100
+#define	USTAR_mode_size 6
+#define	USTAR_mode_max_size 8
+#define	USTAR_uid_offset 108
+#define	USTAR_uid_size 6
+#define	USTAR_uid_max_size 8
+#define	USTAR_gid_offset 116
+#define	USTAR_gid_size 6
+#define	USTAR_gid_max_size 8
+#define	USTAR_size_offset 124
+#define	USTAR_size_size 11
+#define	USTAR_size_max_size 12
+#define	USTAR_mtime_offset 136
+#define	USTAR_mtime_size 11
+#define	USTAR_mtime_max_size 11
+#define	USTAR_checksum_offset 148
+#define	USTAR_checksum_size 8
+#define	USTAR_typeflag_offset 156
+#define	USTAR_typeflag_size 1
+#define	USTAR_linkname_offset 157
+#define	USTAR_linkname_size 100
+#define	USTAR_magic_offset 257
+#define	USTAR_magic_size 6
+#define	USTAR_version_offset 263
+#define	USTAR_version_size 2
+#define	USTAR_uname_offset 265
+#define	USTAR_uname_size 32
+#define	USTAR_gname_offset 297
+#define	USTAR_gname_size 32
+#define	USTAR_rdevmajor_offset 329
+#define	USTAR_rdevmajor_size 6
+#define	USTAR_rdevmajor_max_size 8
+#define	USTAR_rdevminor_offset 337
+#define	USTAR_rdevminor_size 6
+#define	USTAR_rdevminor_max_size 8
+#define	USTAR_prefix_offset 345
+#define	USTAR_prefix_size 155
+#define	USTAR_padding_offset 500
+#define	USTAR_padding_size 12
 
 /*
  * A filled-in copy of the header for initialization.
  */
-static const struct archive_entry_header_ustar template_header = {
-	{ "" },				/* name */
-	{ '0','0','0','0','0','0' }, { ' ', '\0' }, /* mode, space-null term.*/
-	{ '0','0','0','0','0','0' }, { ' ', '\0' }, /* uid, space-null term. */
-	{ '0','0','0','0','0','0' }, { ' ', '\0' }, /* gid, space-null term. */
-	{ '0','0','0','0','0','0','0','0','0','0','0' }, { ' ' },
-					/* size, space termination. */
-	{ '0','0','0','0','0','0','0','0','0','0','0' }, { ' ' },
-					/* mtime, space termination. */
-	{ ' ',' ',' ',' ',' ',' ',' ',' ' },	/* Initial checksum value. */
-	{ '0' },			/* default: regular file */
-	{ "" },				/* linkname */
-	{ 'u','s','t','a','r' },	/* magic */
-	{ '0', '0' },			/* version */
-	{ "" },				/* uname */
-	{ "" },				/* gname */
-	{ '0','0','0','0','0','0' }, { ' ', '\0' }, /* rdevmajor, space-null */
-	{ '0','0','0','0','0','0' }, { ' ', '\0' }, /* rdevminor, space-null */
-	{ "" },				/* prefix */
-	{ "" }				/* padding */
+static const char template_header[] = {
+	/* name: 100 bytes */
+	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+	0,0,0,0,
+	/* Mode, space-null termination: 8 bytes */
+	'0','0','0','0','0','0', ' ','\0',
+	/* uid, space-null termination: 8 bytes */
+	'0','0','0','0','0','0', ' ','\0',
+	/* gid, space-null termination: 8 bytes */
+	'0','0','0','0','0','0', ' ','\0',
+	/* size, space termation: 12 bytes */
+	'0','0','0','0','0','0','0','0','0','0','0', ' ',
+	/* mtime, space termation: 12 bytes */
+	'0','0','0','0','0','0','0','0','0','0','0', ' ',
+	/* Initial checksum value: 8 spaces */
+	' ',' ',' ',' ',' ',' ',' ',' ',
+	/* Typeflag: 1 byte */
+	'0',			/* '0' = regular file */
+	/* Linkname: 100 bytes */
+	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+	0,0,0,0,
+	/* Magic: 6 bytes, Version: 2 bytes */
+	'u','s','t','a','r','\0', '0','0',
+	/* Uname: 32 bytes */
+	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+	/* Gname: 32 bytes */
+	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+	/* rdevmajor + space/null padding: 8 bytes */
+	'0','0','0','0','0','0', ' ','\0',
+	/* rdevminor + space/null padding: 8 bytes */
+	'0','0','0','0','0','0', ' ','\0',
+	/* Prefix: 155 bytes */
+	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,
+	/* Padding: 12 bytes */
+	0,0,0,0,0,0,0,0, 0,0,0,0
 };
 
 static ssize_t	archive_write_ustar_data(struct archive *a, const void *buff,
@@ -135,6 +173,12 @@ archive_write_set_format_ustar(struct archive *a)
 	/* If someone else was already registered, unregister them. */
 	if (a->format_finish != NULL)
 		(a->format_finish)(a);
+
+	/* Basic internal sanity test. */
+	if (sizeof(template_header) != 512) {
+		archive_set_error(a, ARCHIVE_ERRNO_MISC, "Internal: template_header wrong size: %d should be 512", sizeof(template_header));
+		return (ARCHIVE_FATAL);
+	}
 
 	ustar = (struct ustar *)malloc(sizeof(*ustar));
 	if (ustar == NULL) {
@@ -193,11 +237,10 @@ archive_write_ustar_header(struct archive *a, struct archive_entry *entry)
  * This is exported so that other 'tar' formats can use it.
  */
 int
-__archive_write_format_header_ustar(struct archive *a, char buff[512],
+__archive_write_format_header_ustar(struct archive *a, char h[512],
     struct archive_entry *entry, int tartype, int strict)
 {
 	unsigned int checksum;
-	struct archive_entry_header_ustar *h;
 	int i, ret;
 	size_t copy_length;
 	const char *p, *pp;
@@ -211,9 +254,7 @@ __archive_write_format_header_ustar(struct archive *a, char buff[512],
 	 * signature, various end-of-field markers and other required
 	 * elements.
 	 */
-	memcpy(buff, &template_header, 512);
-
-	h = (struct archive_entry_header_ustar *)buff;
+	memcpy(h, &template_header, 512);
 
 	/*
 	 * Because the block is already null-filled, and strings
@@ -222,11 +263,11 @@ __archive_write_format_header_ustar(struct archive *a, char buff[512],
 	 */
 
 	pp = archive_entry_pathname(entry);
-	if (strlen(pp) <= sizeof(h->name))
-		memcpy(h->name, pp, strlen(pp));
+	if (strlen(pp) <= USTAR_name_size)
+		memcpy(h + USTAR_name_offset, pp, strlen(pp));
 	else {
 		/* Store in two pieces, splitting at a '/'. */
-		p = strchr(pp + strlen(pp) - sizeof(h->name) - 1, '/');
+		p = strchr(pp + strlen(pp) - USTAR_name_size - 1, '/');
 		/*
 		 * If there is no path separator, or the prefix or
 		 * remaining name are too large, return an error.
@@ -235,14 +276,14 @@ __archive_write_format_header_ustar(struct archive *a, char buff[512],
 			archive_set_error(a, ENAMETOOLONG,
 			    "Pathname too long");
 			ret = ARCHIVE_WARN;
-		} else if (p  > pp + sizeof(h->prefix)) {
+		} else if (p  > pp + USTAR_prefix_size) {
 			archive_set_error(a, ENAMETOOLONG,
 			    "Pathname too long");
 			ret = ARCHIVE_WARN;
 		} else {
 			/* Copy prefix and remainder to appropriate places */
-			memcpy(h->prefix, pp, p - pp);
-			memcpy(h->name, p + 1, pp + strlen(pp) - p - 1);
+			memcpy(h + USTAR_prefix_offset, pp, p - pp);
+			memcpy(h + USTAR_name_offset, p + 1, pp + strlen(pp) - p - 1);
 		}
 	}
 
@@ -253,77 +294,77 @@ __archive_write_format_header_ustar(struct archive *a, char buff[512],
 		p = archive_entry_symlink(entry);
 	if (p != NULL && p[0] != '\0') {
 		copy_length = strlen(p);
-		if (copy_length > sizeof(h->linkname)) {
+		if (copy_length > USTAR_linkname_size) {
 			archive_set_error(a, ENAMETOOLONG,
 			    "Link contents too long");
 			ret = ARCHIVE_WARN;
-			copy_length = sizeof(h->linkname);
+			copy_length = USTAR_linkname_size;
 		}
-		memcpy(h->linkname, p, copy_length);
+		memcpy(h + USTAR_linkname_offset, p, copy_length);
 	}
 
 	p = archive_entry_uname(entry);
 	if (p != NULL && p[0] != '\0') {
 		copy_length = strlen(p);
-		if (copy_length > sizeof(h->uname)) {
+		if (copy_length > USTAR_uname_size) {
 			archive_set_error(a, ARCHIVE_ERRNO_MISC,
 			    "Username too long");
 			ret = ARCHIVE_WARN;
-			copy_length = sizeof(h->uname);
+			copy_length = USTAR_uname_size;
 		}
-		memcpy(h->uname, p, copy_length);
+		memcpy(h + USTAR_uname_offset, p, copy_length);
 	}
 
 	p = archive_entry_gname(entry);
 	if (p != NULL && p[0] != '\0') {
 		copy_length = strlen(p);
-		if (strlen(p) > sizeof(h->gname)) {
+		if (strlen(p) > USTAR_gname_size) {
 			archive_set_error(a, ARCHIVE_ERRNO_MISC,
 			    "Group name too long");
 			ret = ARCHIVE_WARN;
-			copy_length = sizeof(h->gname);
+			copy_length = USTAR_gname_size;
 		}
-		memcpy(h->gname, p, copy_length);
+		memcpy(h + USTAR_gname_offset, p, copy_length);
 	}
 
 	st = archive_entry_stat(entry);
 
-	if (format_number(st->st_mode & 07777, h->mode, sizeof(h->mode), 8, strict)) {
+	if (format_number(st->st_mode & 07777, h + USTAR_mode_offset, USTAR_mode_size, USTAR_mode_max_size, strict)) {
 		archive_set_error(a, ERANGE, "Numeric mode too large");
 		ret = ARCHIVE_WARN;
 	}
 
-	if (format_number(st->st_uid, h->uid, sizeof(h->uid), 8, strict)) {
+	if (format_number(st->st_uid, h + USTAR_uid_offset, USTAR_uid_size, USTAR_uid_max_size, strict)) {
 		archive_set_error(a, ERANGE, "Numeric user ID too large");
 		ret = ARCHIVE_WARN;
 	}
 
-	if (format_number(st->st_gid, h->gid, sizeof(h->gid), 8, strict)) {
+	if (format_number(st->st_gid, h + USTAR_gid_offset, USTAR_gid_size, USTAR_gid_max_size, strict)) {
 		archive_set_error(a, ERANGE, "Numeric group ID too large");
 		ret = ARCHIVE_WARN;
 	}
 
-	if (format_number(st->st_size, h->size, sizeof(h->size), 12, strict)) {
+	if (format_number(st->st_size, h + USTAR_size_offset, USTAR_size_size, USTAR_size_max_size, strict)) {
 		archive_set_error(a, ERANGE, "File size out of range");
 		ret = ARCHIVE_WARN;
 	}
 
-	if (format_number(st->st_mtime, h->mtime, sizeof(h->mtime), 12, strict)) {
+	if (format_number(st->st_mtime, h + USTAR_mtime_offset, USTAR_mtime_size, USTAR_mtime_max_size, strict)) {
 		archive_set_error(a, ERANGE,
 		    "File modification time too large");
 		ret = ARCHIVE_WARN;
 	}
 
 	if (S_ISBLK(st->st_mode) || S_ISCHR(st->st_mode)) {
-		if (format_number(major(st->st_rdev), h->rdevmajor,
-			sizeof(h->rdevmajor), 8, strict)) {
+		if (format_number(major(st->st_rdev), h + USTAR_rdevmajor_offset,
+			USTAR_rdevmajor_size, USTAR_rdevmajor_max_size, strict)) {
 			archive_set_error(a, ERANGE,
 			    "Major device number too large");
 			ret = ARCHIVE_WARN;
 		}
 
-		if (format_number(minor(st->st_rdev), h->rdevminor,
-			sizeof(h->rdevminor), 8, strict)) {
+		if (format_number(minor(st->st_rdev), h + USTAR_rdevminor_offset,
+			USTAR_rdevminor_size, USTAR_rdevminor_max_size, strict)) {
 			archive_set_error(a, ERANGE,
 			    "Minor device number too large");
 			ret = ARCHIVE_WARN;
@@ -331,17 +372,17 @@ __archive_write_format_header_ustar(struct archive *a, char buff[512],
 	}
 
 	if (tartype >= 0) {
-		h->typeflag[0] = tartype;
+		h[USTAR_typeflag_offset] = tartype;
 	} else if (mytartype >= 0) {
-		h->typeflag[0] = mytartype;
+		h[USTAR_typeflag_offset] = mytartype;
 	} else {
 		switch (st->st_mode & S_IFMT) {
-		case S_IFREG: h->typeflag[0] = '0' ; break;
-		case S_IFLNK: h->typeflag[0] = '2' ; break;
-		case S_IFCHR: h->typeflag[0] = '3' ; break;
-		case S_IFBLK: h->typeflag[0] = '4' ; break;
-		case S_IFDIR: h->typeflag[0] = '5' ; break;
-		case S_IFIFO: h->typeflag[0] = '6' ; break;
+		case S_IFREG: h[USTAR_typeflag_offset] = '0' ; break;
+		case S_IFLNK: h[USTAR_typeflag_offset] = '2' ; break;
+		case S_IFCHR: h[USTAR_typeflag_offset] = '3' ; break;
+		case S_IFBLK: h[USTAR_typeflag_offset] = '4' ; break;
+		case S_IFDIR: h[USTAR_typeflag_offset] = '5' ; break;
+		case S_IFIFO: h[USTAR_typeflag_offset] = '6' ; break;
 		case S_IFSOCK:
 			archive_set_error(a, ARCHIVE_ERRNO_FILE_FORMAT,
 			    "tar format cannot archive socket");
@@ -357,10 +398,10 @@ __archive_write_format_header_ustar(struct archive *a, char buff[512],
 
 	checksum = 0;
 	for (i = 0; i < 512; i++)
-		checksum += 255 & (unsigned int)buff[i];
-	h->checksum[6] = '\0'; /* Can't be pre-set in the template. */
-	/* h->checksum[7] = ' '; */ /* This is pre-set in the template. */
-	format_octal(checksum, h->checksum, 6);
+		checksum += 255 & (unsigned int)h[i];
+	h[USTAR_checksum_offset + 6] = '\0'; /* Can't be pre-set in the template. */
+	/* h[USTAR_checksum_offset + 7] = ' '; */ /* This is pre-set in the template. */
+	format_octal(checksum, h + USTAR_checksum_offset, 6);
 	return (ret);
 }
 
