d8866048e5e602121237ccb2c14d6437896233b5
yar <yar@FreeBSD.org>
2006-11-27 20:19:05 +0000

    Consistently mark percentage scales as such.
    PR:		bin/101975
    MFC after:	3 days

--- usr.bin/systat/swap.c
+++ usr.bin/systat/swap.c
@@ -128,7 +128,7 @@ labelswap()
 	header = getbsize(&hlen, &blocksize);
 	mvwprintw(wnd, row++, 0, "%-5s%*s%9s %55s",
 	    "Disk", hlen, header, "Used",
-	    "/0%  /10% /20% /30% /40% /50% /60% /70% /80% /90% /100");
+	    "/0%  /10  /20  /30  /40  /50  /60  /70  /80  /90  /100");
 
 	for (i = 0; i < kvnsw; ++i) {
 		mvwprintw(wnd, i + 1, 0, "%-5s", kvmsw[i].ksw_devname);
