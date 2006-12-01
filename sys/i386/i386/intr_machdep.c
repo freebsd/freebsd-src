95714ee293c7653f0a720a8afe69f88fb9585be1
cvs2svn <cvs2svn@FreeBSD.org>
2006-12-01 17:13:57 +0000

    This commit was manufactured by cvs2svn to create branch 'RELENG_6_3'.

---
+++ sys/i386/i386/intr_machdep.c
@@ -0,0 +1,461 @@
+/*-
+ * Copyright (c) 2003 John Baldwin <jhb@FreeBSD.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of any co-contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+/*
+ * Machine dependent interrupt code for i386.  For the i386, we have to
+ * deal with different PICs.  Thus, we use the passed in vector to lookup
+ * an interrupt source associated with that vector.  The interrupt source
+ * describes which PIC the source belongs to and includes methods to handle
+ * that source.
+ */
+
+#include "opt_ddb.h"
+
+#include <sys/param.h>
+#include <sys/bus.h>
+#include <sys/interrupt.h>
+#include <sys/lock.h>
+#include <sys/ktr.h>
+#include <sys/kernel.h>
+#include <sys/mutex.h>
+#include <sys/proc.h>
+#include <sys/syslog.h>
+#include <sys/systm.h>
+#include <machine/clock.h>
+#include <machine/intr_machdep.h>
+#ifdef DDB
+#include <ddb/ddb.h>
+#endif
+
+#define	MAX_STRAY_LOG	5
+
+typedef void (*mask_fn)(void *);
+
+static int intrcnt_index;
+static struct intsrc *interrupt_sources[NUM_IO_INTS];
+static struct mtx intr_table_lock;
+static STAILQ_HEAD(, pic) pics;
+
+#ifdef SMP
+static int assign_cpu;
+
+static void	intr_assign_next_cpu(struct intsrc *isrc);
+#endif
+
+static void	intr_init(void *__dummy);
+static int	intr_pic_registered(struct pic *pic);
+static void	intrcnt_setname(const char *name, int index);
+static void	intrcnt_updatename(struct intsrc *is);
+static void	intrcnt_register(struct intsrc *is);
+
+static int
+intr_pic_registered(struct pic *pic)
+{
+	struct pic *p;
+
+	STAILQ_FOREACH(p, &pics, pics) {
+		if (p == pic)
+			return (1);
+	}
+	return (0);
+}
+
+/*
+ * Register a new interrupt controller (PIC).  This is to support suspend
+ * and resume where we suspend/resume controllers rather than individual
+ * sources.  This also allows controllers with no active sources (such as
+ * 8259As in a system using the APICs) to participate in suspend and resume.
+ */
+int
+intr_register_pic(struct pic *pic)
+{
+	int error;
+
+	mtx_lock_spin(&intr_table_lock);
+	if (intr_pic_registered(pic))
+		error = EBUSY;
+	else {
+		STAILQ_INSERT_TAIL(&pics, pic, pics);
+		error = 0;
+	}
+	mtx_unlock_spin(&intr_table_lock);
+	return (error);
+}
+
+/*
+ * Register a new interrupt source with the global interrupt system.
+ * The global interrupts need to be disabled when this function is
+ * called.
+ */
+int
+intr_register_source(struct intsrc *isrc)
+{
+	int error, vector;
+
+	KASSERT(intr_pic_registered(isrc->is_pic), ("unregistered PIC"));
+	vector = isrc->is_pic->pic_vector(isrc);
+	if (interrupt_sources[vector] != NULL)
+		return (EEXIST);
+	error = intr_event_create(&isrc->is_event, isrc, 0,
+	    (mask_fn)isrc->is_pic->pic_enable_source, "irq%d:", vector);
+	if (error)
+		return (error);
+	mtx_lock_spin(&intr_table_lock);
+	if (interrupt_sources[vector] != NULL) {
+		mtx_unlock_spin(&intr_table_lock);
+		intr_event_destroy(isrc->is_event);
+		return (EEXIST);
+	}
+	intrcnt_register(isrc);
+	interrupt_sources[vector] = isrc;
+	isrc->is_enabled = 0;
+	mtx_unlock_spin(&intr_table_lock);
+	return (0);
+}
+
+struct intsrc *
+intr_lookup_source(int vector)
+{
+
+	return (interrupt_sources[vector]);
+}
+
+int
+intr_add_handler(const char *name, int vector, driver_intr_t handler,
+    void *arg, enum intr_type flags, void **cookiep)
+{
+	struct intsrc *isrc;
+	int error;
+
+	isrc = intr_lookup_source(vector);
+	if (isrc == NULL)
+		return (EINVAL);
+	error = intr_event_add_handler(isrc->is_event, name, handler, arg,
+	    intr_priority(flags), flags, cookiep);
+	if (error == 0) {
+		intrcnt_updatename(isrc);
+		mtx_lock_spin(&intr_table_lock);
+		if (!isrc->is_enabled) {
+			isrc->is_enabled = 1;
+#ifdef SMP
+			if (assign_cpu)
+				intr_assign_next_cpu(isrc);
+#endif
+			mtx_unlock_spin(&intr_table_lock);
+			isrc->is_pic->pic_enable_intr(isrc);
+		} else
+			mtx_unlock_spin(&intr_table_lock);
+		isrc->is_pic->pic_enable_source(isrc);
+	}
+	return (error);
+}
+
+int
+intr_remove_handler(void *cookie)
+{
+	int error;
+
+	error = intr_event_remove_handler(cookie);
+#ifdef XXX
+	if (error == 0)
+		intrcnt_updatename(/* XXX */);
+#endif
+	return (error);
+}
+
+int
+intr_config_intr(int vector, enum intr_trigger trig, enum intr_polarity pol)
+{
+	struct intsrc *isrc;
+
+	isrc = intr_lookup_source(vector);
+	if (isrc == NULL)
+		return (EINVAL);
+	return (isrc->is_pic->pic_config_intr(isrc, trig, pol));
+}
+
+void
+intr_execute_handlers(struct intsrc *isrc, struct intrframe *iframe)
+{
+	struct thread *td;
+	struct intr_event *ie;
+	struct intr_handler *ih;
+	int error, vector, thread;
+
+	td = curthread;
+
+	/*
+	 * We count software interrupts when we process them.  The
+	 * code here follows previous practice, but there's an
+	 * argument for counting hardware interrupts when they're
+	 * processed too.
+	 */
+	(*isrc->is_count)++;
+	PCPU_LAZY_INC(cnt.v_intr);
+
+	ie = isrc->is_event;
+
+	/*
+	 * XXX: We assume that IRQ 0 is only used for the ISA timer
+	 * device (clk).
+	 */
+	vector = isrc->is_pic->pic_vector(isrc);
+	if (vector == 0)
+		clkintr_pending = 1;
+
+	/*
+	 * For stray interrupts, mask and EOI the source, bump the
+	 * stray count, and log the condition.
+	 */
+	if (ie == NULL || TAILQ_EMPTY(&ie->ie_handlers)) {
+		isrc->is_pic->pic_disable_source(isrc, PIC_EOI);
+		(*isrc->is_straycount)++;
+		if (*isrc->is_straycount < MAX_STRAY_LOG)
+			log(LOG_ERR, "stray irq%d\n", vector);
+		else if (*isrc->is_straycount == MAX_STRAY_LOG)
+			log(LOG_CRIT,
+			    "too many stray irq %d's: not logging anymore\n",
+			    vector);
+		return;
+	}
+
+	/*
+	 * Execute fast interrupt handlers directly.
+	 * To support clock handlers, if a handler registers
+	 * with a NULL argument, then we pass it a pointer to
+	 * an intrframe as its argument.
+	 */
+	td->td_intr_nesting_level++;
+	thread = 0;
+	critical_enter();
+	TAILQ_FOREACH(ih, &ie->ie_handlers, ih_next) {
+		if (!(ih->ih_flags & IH_FAST)) {
+			thread = 1;
+			continue;
+		}
+		CTR4(KTR_INTR, "%s: exec %p(%p) for %s", __func__,
+		    ih->ih_handler, ih->ih_argument == NULL ? iframe :
+		    ih->ih_argument, ih->ih_name);
+		if (ih->ih_argument == NULL)
+			ih->ih_handler(iframe);
+		else
+			ih->ih_handler(ih->ih_argument);
+	}
+
+	/*
+	 * If there are any threaded handlers that need to run,
+	 * mask the source as well as sending it an EOI.  Otherwise,
+	 * just send it an EOI but leave it unmasked.
+	 */
+	if (thread)
+		isrc->is_pic->pic_disable_source(isrc, PIC_EOI);
+	else
+		isrc->is_pic->pic_eoi_source(isrc);
+	critical_exit();
+
+	/* Schedule the ithread if needed. */
+	if (thread) {
+		error = intr_event_schedule_thread(ie);
+		KASSERT(error == 0, ("bad stray interrupt"));
+	}
+	td->td_intr_nesting_level--;
+}
+
+void
+intr_resume(void)
+{
+	struct pic *pic;
+
+	mtx_lock_spin(&intr_table_lock);
+	STAILQ_FOREACH(pic, &pics, pics) {
+		if (pic->pic_resume != NULL)
+			pic->pic_resume(pic);
+	}
+	mtx_unlock_spin(&intr_table_lock);
+}
+
+void
+intr_suspend(void)
+{
+	struct pic *pic;
+
+	mtx_lock_spin(&intr_table_lock);
+	STAILQ_FOREACH(pic, &pics, pics) {
+		if (pic->pic_suspend != NULL)
+			pic->pic_suspend(pic);
+	}
+	mtx_unlock_spin(&intr_table_lock);
+}
+
+static void
+intrcnt_setname(const char *name, int index)
+{
+
+	snprintf(intrnames + (MAXCOMLEN + 1) * index, MAXCOMLEN + 1, "%-*s",
+	    MAXCOMLEN, name);
+}
+
+static void
+intrcnt_updatename(struct intsrc *is)
+{
+
+	intrcnt_setname(is->is_event->ie_fullname, is->is_index);
+}
+
+static void
+intrcnt_register(struct intsrc *is)
+{
+	char straystr[MAXCOMLEN + 1];
+
+	/* mtx_assert(&intr_table_lock, MA_OWNED); */
+	KASSERT(is->is_event != NULL, ("%s: isrc with no event", __func__));
+	is->is_index = intrcnt_index;
+	intrcnt_index += 2;
+	snprintf(straystr, MAXCOMLEN + 1, "stray irq%d",
+	    is->is_pic->pic_vector(is));
+	intrcnt_updatename(is);
+	is->is_count = &intrcnt[is->is_index];
+	intrcnt_setname(straystr, is->is_index + 1);
+	is->is_straycount = &intrcnt[is->is_index + 1];
+}
+
+void
+intrcnt_add(const char *name, u_long **countp)
+{
+
+	mtx_lock_spin(&intr_table_lock);
+	*countp = &intrcnt[intrcnt_index];
+	intrcnt_setname(name, intrcnt_index);
+	intrcnt_index++;
+	mtx_unlock_spin(&intr_table_lock);
+}
+
+static void
+intr_init(void *dummy __unused)
+{
+
+	intrcnt_setname("???", 0);
+	intrcnt_index = 1;
+	STAILQ_INIT(&pics);
+	mtx_init(&intr_table_lock, "intr table", NULL, MTX_SPIN);
+}
+SYSINIT(intr_init, SI_SUB_INTR, SI_ORDER_FIRST, intr_init, NULL)
+
+#ifdef DDB
+/*
+ * Dump data about interrupt handlers
+ */
+DB_SHOW_COMMAND(irqs, db_show_irqs)
+{
+	struct intsrc **isrc;
+	int i, quit, verbose;
+
+	quit = 0;
+	if (strcmp(modif, "v") == 0)
+		verbose = 1;
+	else
+		verbose = 0;
+	isrc = interrupt_sources;
+	db_setup_paging(db_simple_pager, &quit, db_lines_per_page);
+	for (i = 0; i < NUM_IO_INTS && !quit; i++, isrc++)
+		if (*isrc != NULL)
+			db_dump_intr_event((*isrc)->is_event, verbose);
+}
+#endif
+
+#ifdef SMP
+/*
+ * Support for balancing interrupt sources across CPUs.  For now we just
+ * allocate CPUs round-robin.
+ */
+
+static u_int cpu_apic_ids[MAXCPU];
+static int current_cpu, num_cpus;
+
+static void
+intr_assign_next_cpu(struct intsrc *isrc)
+{
+	struct pic *pic;
+	u_int apic_id;
+
+	/*
+	 * Assign this source to a local APIC in a round-robin fashion.
+	 */
+	pic = isrc->is_pic;
+	apic_id = cpu_apic_ids[current_cpu];
+	current_cpu++;
+	if (current_cpu >= num_cpus)
+		current_cpu = 0;
+	pic->pic_assign_cpu(isrc, apic_id);
+}
+
+/*
+ * Add a local APIC ID to our list of valid local APIC IDs that can
+ * be destinations of interrupts.
+ */
+void
+intr_add_cpu(u_int apic_id)
+{
+
+	if (bootverbose)
+		printf("INTR: Adding local APIC %d as a target\n", apic_id);
+	if (num_cpus >= MAXCPU)
+		panic("WARNING: Local APIC IDs exhausted!");
+	cpu_apic_ids[num_cpus] = apic_id;
+	num_cpus++;
+}
+
+/*
+ * Distribute all the interrupt sources among the available CPUs once the
+ * AP's have been launched.
+ */
+static void
+intr_shuffle_irqs(void *arg __unused)
+{
+	struct intsrc *isrc;
+	int i;
+
+	/* Don't bother on UP. */
+	if (num_cpus <= 1)
+		return;
+
+	/* Round-robin assign a CPU to each enabled source. */
+	mtx_lock_spin(&intr_table_lock);
+	assign_cpu = 1;
+	for (i = 0; i < NUM_IO_INTS; i++) {
+		isrc = interrupt_sources[i];
+		if (isrc != NULL && isrc->is_enabled)
+			intr_assign_next_cpu(isrc);
+	}
+	mtx_unlock_spin(&intr_table_lock);
+}
+SYSINIT(intr_shuffle_irqs, SI_SUB_SMP, SI_ORDER_SECOND, intr_shuffle_irqs, NULL)
+#endif
---
+++ sys/ia64/ia64/interrupt.c
@@ -0,0 +1,430 @@
+/* $FreeBSD$ */
+/* $NetBSD: interrupt.c,v 1.23 1998/02/24 07:38:01 thorpej Exp $ */
+
+/*-
+ * Copyright (c) 1994, 1995, 1996 Carnegie-Mellon University.
+ * All rights reserved.
+ *
+ * Authors: Keith Bostic, Chris G. Demetriou
+ * 
+ * Permission to use, copy, modify and distribute this software and
+ * its documentation is hereby granted, provided that both the copyright
+ * notice and this permission notice appear in all copies of the
+ * software, derivative works or modified versions, and any portions
+ * thereof, and that both notices appear in supporting documentation.
+ * 
+ * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" 
+ * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND 
+ * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+ * 
+ * Carnegie Mellon requests users of this software to return to
+ *
+ *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
+ *  School of Computer Science
+ *  Carnegie Mellon University
+ *  Pittsburgh PA 15213-3890
+ *
+ * any improvements or extensions that they make and grant Carnegie the
+ * rights to redistribute these changes.
+ */
+/*-
+ * Additional Copyright (c) 1997 by Matthew Jacob for NASA/Ames Research Center.
+ * Redistribute and modify at will, leaving only this additional copyright
+ * notice.
+ */
+
+#include "opt_ddb.h"
+
+#include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/kernel.h>
+#include <sys/proc.h>
+#include <sys/vmmeter.h>
+#include <sys/bus.h>
+#include <sys/malloc.h>
+#include <sys/ktr.h>
+#include <sys/lock.h>
+#include <sys/mutex.h>
+#include <sys/smp.h>
+#include <sys/sysctl.h>
+
+#include <machine/clock.h>
+#include <machine/cpu.h>
+#include <machine/fpu.h>
+#include <machine/frame.h>
+#include <machine/intr.h>
+#include <machine/md_var.h>
+#include <machine/pcb.h>
+#include <machine/reg.h>
+#include <machine/sapicvar.h>
+#include <machine/smp.h>
+
+#ifdef EVCNT_COUNTERS
+struct evcnt clock_intr_evcnt;	/* event counter for clock intrs. */
+#else
+#include <sys/interrupt.h>
+#include <machine/intrcnt.h>
+#endif
+
+#ifdef DDB
+#include <ddb/ddb.h>
+#endif
+
+#ifdef SMP
+extern int mp_ipi_test;
+#endif
+
+volatile int mc_expected, mc_received;
+
+static void 
+dummy_perf(unsigned long vector, struct trapframe *tf)  
+{
+	printf("performance interrupt!\n");
+}
+
+void (*perf_irq)(unsigned long, struct trapframe *) = dummy_perf;
+
+static unsigned int ints[MAXCPU];
+SYSCTL_OPAQUE(_debug, OID_AUTO, ints, CTLFLAG_RW, &ints, sizeof(ints), "IU",
+    "");
+
+static unsigned int clks[MAXCPU];
+#ifdef SMP
+SYSCTL_OPAQUE(_debug, OID_AUTO, clks, CTLFLAG_RW, &clks, sizeof(clks), "IU",
+    "");
+#else
+SYSCTL_INT(_debug, OID_AUTO, clks, CTLFLAG_RW, clks, 0, "");
+#endif
+
+#ifdef SMP
+static unsigned int asts[MAXCPU];
+SYSCTL_OPAQUE(_debug, OID_AUTO, asts, CTLFLAG_RW, &asts, sizeof(asts), "IU",
+    "");
+
+static unsigned int rdvs[MAXCPU];
+SYSCTL_OPAQUE(_debug, OID_AUTO, rdvs, CTLFLAG_RW, &rdvs, sizeof(rdvs), "IU",
+    "");
+#endif
+
+SYSCTL_NODE(_debug, OID_AUTO, clock, CTLFLAG_RW, 0, "clock statistics");
+
+static int adjust_edges = 0;
+SYSCTL_INT(_debug_clock, OID_AUTO, adjust_edges, CTLFLAG_RD,
+    &adjust_edges, 0, "Number of times ITC got more than 12.5% behind");
+
+static int adjust_excess = 0;
+SYSCTL_INT(_debug_clock, OID_AUTO, adjust_excess, CTLFLAG_RD,
+    &adjust_excess, 0, "Total number of ignored ITC interrupts");
+
+static int adjust_lost = 0;
+SYSCTL_INT(_debug_clock, OID_AUTO, adjust_lost, CTLFLAG_RD,
+    &adjust_lost, 0, "Total number of lost ITC interrupts");
+
+static int adjust_ticks = 0;
+SYSCTL_INT(_debug_clock, OID_AUTO, adjust_ticks, CTLFLAG_RD,
+    &adjust_ticks, 0, "Total number of ITC interrupts with adjustment");
+
+int
+interrupt(u_int64_t vector, struct trapframe *tf)
+{
+	struct thread *td;
+	volatile struct ia64_interrupt_block *ib = IA64_INTERRUPT_BLOCK;
+	uint64_t adj, clk, itc;
+	int64_t delta;
+	int count;
+
+	ia64_set_fpsr(IA64_FPSR_DEFAULT);
+
+	td = curthread;
+	atomic_add_int(&td->td_intr_nesting_level, 1);
+
+	/*
+	 * Handle ExtINT interrupts by generating an INTA cycle to
+	 * read the vector.
+	 */
+	if (vector == 0) {
+		vector = ib->ib_inta;
+		printf("ExtINT interrupt: vector=%ld\n", vector);
+		if (vector == 15)
+			goto stray;
+	}
+
+	if (vector == CLOCK_VECTOR) {/* clock interrupt */
+		/* CTR0(KTR_INTR, "clock interrupt"); */
+
+		PCPU_LAZY_INC(cnt.v_intr);
+#ifdef EVCNT_COUNTERS
+		clock_intr_evcnt.ev_count++;
+#else
+		intrcnt[INTRCNT_CLOCK]++;
+#endif
+		clks[PCPU_GET(cpuid)]++;
+
+		critical_enter();
+
+		adj = PCPU_GET(clockadj);
+		itc = ia64_get_itc();
+		ia64_set_itm(itc + ia64_clock_reload - adj);
+		clk = PCPU_GET(clock);
+		delta = itc - clk;
+		count = 0;
+		while (delta >= ia64_clock_reload) {
+			/* Only the BSP runs the real clock */
+			if (PCPU_GET(cpuid) == 0)
+				hardclock((struct clockframe *)tf);
+			else
+				hardclock_process((struct clockframe *)tf);
+			if (profprocs != 0)
+				profclock((struct clockframe *)tf);
+			statclock((struct clockframe *)tf);
+			delta -= ia64_clock_reload;
+			clk += ia64_clock_reload;
+			if (adj != 0)
+				adjust_ticks++;
+			count++;
+		}
+		if (count > 0) {
+			adjust_lost += count - 1;
+			if (delta > (ia64_clock_reload >> 3)) {
+				if (adj == 0)
+					adjust_edges++;
+				adj = ia64_clock_reload >> 4;
+			} else
+				adj = 0;
+		} else {
+			adj = 0;
+			adjust_excess++;
+		}
+		PCPU_SET(clock, clk);
+		PCPU_SET(clockadj, adj);
+
+		critical_exit();
+
+#ifdef SMP
+	} else if (vector == ipi_vector[IPI_AST]) {
+		asts[PCPU_GET(cpuid)]++;
+		CTR1(KTR_SMP, "IPI_AST, cpuid=%d", PCPU_GET(cpuid));
+	} else if (vector == ipi_vector[IPI_HIGH_FP]) {
+		struct thread *thr = PCPU_GET(fpcurthread);
+		if (thr != NULL) {
+			mtx_lock_spin(&thr->td_md.md_highfp_mtx);
+			save_high_fp(&thr->td_pcb->pcb_high_fp);
+			thr->td_pcb->pcb_fpcpu = NULL;
+			PCPU_SET(fpcurthread, NULL);
+			mtx_unlock_spin(&thr->td_md.md_highfp_mtx);
+		}
+	} else if (vector == ipi_vector[IPI_RENDEZVOUS]) {
+		rdvs[PCPU_GET(cpuid)]++;
+		CTR1(KTR_SMP, "IPI_RENDEZVOUS, cpuid=%d", PCPU_GET(cpuid));
+		smp_rendezvous_action();
+	} else if (vector == ipi_vector[IPI_STOP]) {
+		register_t intr;
+		cpumask_t mybit = PCPU_GET(cpumask);
+
+		intr = intr_disable();
+		savectx(PCPU_GET(pcb));
+		atomic_set_int(&stopped_cpus, mybit);
+		while ((started_cpus & mybit) == 0)
+			/* spin */;
+		atomic_clear_int(&started_cpus, mybit);
+		atomic_clear_int(&stopped_cpus, mybit);
+		intr_restore(intr);
+	} else if (vector == ipi_vector[IPI_TEST]) {
+		CTR1(KTR_SMP, "IPI_TEST, cpuid=%d", PCPU_GET(cpuid));
+		mp_ipi_test++;
+#endif
+	} else {
+		ints[PCPU_GET(cpuid)]++;
+		ia64_dispatch_intr(tf, vector);
+	}
+
+stray:
+	atomic_subtract_int(&td->td_intr_nesting_level, 1);
+	return (TRAPF_USERMODE(tf));
+}
+
+/*
+ * Hardware irqs have vectors starting at this offset.
+ */
+#define IA64_HARDWARE_IRQ_BASE	0x20
+
+struct ia64_intr {
+    struct intr_event	*event; /* interrupt event */
+    volatile long	*cntp;  /* interrupt counter */
+};
+
+static struct mtx ia64_intrs_lock;
+static struct ia64_intr *ia64_intrs[256];
+
+extern struct sapic *ia64_sapics[];
+extern int ia64_sapic_count;
+
+static void
+ithds_init(void *dummy)
+{
+
+	mtx_init(&ia64_intrs_lock, "intr table", NULL, MTX_SPIN);
+}
+SYSINIT(ithds_init, SI_SUB_INTR, SI_ORDER_SECOND, ithds_init, NULL);
+
+static void
+ia64_send_eoi(uintptr_t vector)
+{
+	int irq, i;
+
+	irq = vector - IA64_HARDWARE_IRQ_BASE;
+	for (i = 0; i < ia64_sapic_count; i++) {
+		struct sapic *sa = ia64_sapics[i];
+		if (irq >= sa->sa_base && irq <= sa->sa_limit)
+			sapic_eoi(sa, vector);
+	}
+}
+
+int
+ia64_setup_intr(const char *name, int irq, driver_intr_t handler, void *arg,
+		enum intr_type flags, void **cookiep, volatile long *cntp)
+{
+	struct ia64_intr *i;
+	int errcode;
+	intptr_t vector = irq + IA64_HARDWARE_IRQ_BASE;
+	char *intrname;
+
+	/*
+	 * XXX - Can we have more than one device on a vector?  If so, we have
+	 * a race condition here that needs to be worked around similar to
+	 * the fashion done in the i386 inthand_add() function.
+	 */
+	
+	/* First, check for an existing hash table entry for this vector. */
+	mtx_lock_spin(&ia64_intrs_lock);
+	i = ia64_intrs[vector];
+	mtx_unlock_spin(&ia64_intrs_lock);
+
+	if (i == NULL) {
+		/* None was found, so create an entry. */
+		i = malloc(sizeof(struct ia64_intr), M_DEVBUF, M_NOWAIT);
+		if (i == NULL)
+			return ENOMEM;
+		if (cntp == NULL)
+			i->cntp = intrcnt + irq + INTRCNT_ISA_IRQ;
+		else
+			i->cntp = cntp;
+		if (name != NULL && *name != '\0') {
+			/* XXX needs abstraction. Too error phrone. */
+			intrname = intrnames + (irq + INTRCNT_ISA_IRQ) *
+			    INTRNAME_LEN;
+			memset(intrname, ' ', INTRNAME_LEN - 1);
+			bcopy(name, intrname, strlen(name));
+		}
+		errcode = intr_event_create(&i->event, (void *)vector, 0,
+		    (void (*)(void *))ia64_send_eoi, "intr:");
+		if (errcode) {
+			free(i, M_DEVBUF);
+			return errcode;
+		}
+
+		mtx_lock_spin(&ia64_intrs_lock);
+		ia64_intrs[vector] = i;
+		mtx_unlock_spin(&ia64_intrs_lock);
+	}
+
+	/* Second, add this handler. */
+	errcode = intr_event_add_handler(i->event, name, handler, arg,
+	    intr_priority(flags), flags, cookiep);
+	if (errcode)
+		return errcode;
+
+	return (sapic_enable(irq, vector));
+}
+
+int
+ia64_teardown_intr(void *cookie)
+{
+
+	return (intr_event_remove_handler(cookie));
+}
+
+void
+ia64_dispatch_intr(void *frame, unsigned long vector)
+{
+	struct ia64_intr *i;
+	struct intr_event *ie;			/* our interrupt event */
+	struct intr_handler *ih;
+	int error, thread;
+
+	/*
+	 * Find the interrupt thread for this vector.
+	 */
+	i = ia64_intrs[vector];
+	if (i == NULL)
+		return;			/* no event for this vector */
+
+	if (i->cntp)
+		atomic_add_long(i->cntp, 1);
+
+	ie = i->event;
+	KASSERT(ie != NULL, ("interrupt vector without an event"));
+
+	/*
+	 * As an optimization, if an event has no handlers, don't
+	 * schedule it to run.
+	 */
+	if (TAILQ_EMPTY(&ie->ie_handlers))
+		return;
+
+	/*
+	 * Execute all fast interrupt handlers directly without Giant.  Note
+	 * that this means that any fast interrupt handler must be MP safe.
+	 */
+	thread = 0;
+	critical_enter();
+	TAILQ_FOREACH(ih, &ie->ie_handlers, ih_next) {
+		if (!(ih->ih_flags & IH_FAST)) {
+			thread = 1;
+			continue;
+		}
+		CTR4(KTR_INTR, "%s: exec %p(%p) for %s", __func__,
+		    ih->ih_handler, ih->ih_argument, ih->ih_name);
+		ih->ih_handler(ih->ih_argument);
+	}
+	critical_exit();
+
+	if (thread) {
+		error = intr_event_schedule_thread(ie);
+		KASSERT(error == 0, ("got an impossible stray interrupt"));
+	} else
+		ia64_send_eoi(vector);
+}
+
+#ifdef DDB
+
+static void
+db_show_vector(int vector)
+{
+	int irq, i;
+
+	irq = vector - IA64_HARDWARE_IRQ_BASE;
+	for (i = 0; i < ia64_sapic_count; i++) {
+		struct sapic *sa = ia64_sapics[i];
+		if (irq >= sa->sa_base && irq <= sa->sa_limit)
+			sapic_print(sa, irq - sa->sa_base);
+	}
+}
+
+DB_SHOW_COMMAND(irq, db_show_irq)
+{
+	int vector;
+
+	if (have_addr) {
+		vector = ((addr >> 4) % 16) * 10 + (addr % 16);
+		db_show_vector(vector);
+	} else {
+		for (vector = IA64_HARDWARE_IRQ_BASE;
+		     vector < IA64_HARDWARE_IRQ_BASE + 64; vector++)
+			db_show_vector(vector);
+	}
+}
+
+#endif
