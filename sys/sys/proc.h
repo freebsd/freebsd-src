2181efed38e584be909e727d3b2e153c37d723ab
davidxu <davidxu@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2006-12-04 14:15:12 +0000

    if a thread blocked on userland condition variable is pthread_cancel()ed, it is expected that the thread will not consume a pthread_cond_signal(), therefor, we use thr_wake() to mark a flag, the flag tells a thread calling do_cv_wait() in umtx code to not block on a condition variable. Thread library is expected that once a thread detected itself is in pthread_cond_wait, it will call the thr_wake() for itself in its SIGCANCEL handler.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@164876 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -455,6 +455,7 @@ struct thread {
 #define	TDP_GEOM	0x00010000 /* Settle GEOM before finishing syscall */
 #define	TDP_SOFTDEP	0x00020000 /* Stuck processing softdep worklist */
 #define	TDP_NORUNNINGBUF 0x00040000 /* Ignore runningbufspace check */
+#define	TDP_WAKEUP	0x00080000 /* Don't sleep in umtx cond_wait */
 
 /*
  * Reasons that the current thread can not be run yet.
