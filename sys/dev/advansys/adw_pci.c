9714a9dcce394893f73e65eec3b1849df7e28c4d
mjacob <mjacob@FreeBSD.org>
2006-12-11 18:28:31 +0000

    Add MODULE_DEPENDS for cam, pci, mca, eisa and isa where needed.
    PR:		106543
    MFC after:	3 days

--- sys/dev/advansys/adw_pci.c
+++ sys/dev/advansys/adw_pci.c
@@ -146,6 +146,7 @@ static driver_t adw_pci_driver = {
 static devclass_t adw_devclass;
 
 DRIVER_MODULE(adw, pci, adw_pci_driver, adw_devclass, 0, 0);
+MODULE_DEPEND(adw, pci, 1, 1, 1);
 
 static __inline u_int64_t
 adw_compose_id(u_int device, u_int vendor, u_int subdevice, u_int subvendor)
