d52399f3b00af9edcb91834d1b9ac60b603344e1
cognet <cognet@FreeBSD.org>
2006-12-16 14:00:54 +0000

    Do not special-case __ARMEB__, we handle that in the arm code.
    Approved by:	sos

--- sys/dev/ata/ata-all.c
+++ sys/dev/ata/ata-all.c
@@ -601,14 +601,12 @@ ata_getparam(struct ata_device *atadev, int init)
 		   isprint(atadev->param.model[1]))) {
 	struct ata_params *atacap = &atadev->param;
 	char buffer[64];
-#ifndef __ARMEB__
 	int16_t *ptr;
 
 	for (ptr = (int16_t *)atacap;
 	     ptr < (int16_t *)atacap + sizeof(struct ata_params)/2; ptr++) {
 	    *ptr = le16toh(*ptr);
 	}
-#endif
 	if (!(!strncmp(atacap->model, "FX", 2) ||
 	      !strncmp(atacap->model, "NEC", 3) ||
 	      !strncmp(atacap->model, "Pioneer", 7) ||
