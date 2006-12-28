ea40b5655a1ee10b5337f843e0a8c834159cd105
jhb <jhb@FreeBSD.org>
2006-12-28 22:20:41 +0000

    MFC: Make the ACPI and OpenFirmware PCI bus drivers subclasses of the generic PCI bus driver.

--- sys/dev/cardbus/cardbus.c
+++ sys/dev/cardbus/cardbus.c
@@ -664,7 +664,6 @@ static device_method_t cardbus_methods[] = {
 	{0,0}
 };
 
-DECLARE_CLASS(pci_driver);
 DEFINE_CLASS_1(cardbus, cardbus_driver, cardbus_methods, 0, pci_driver);
 
 static devclass_t cardbus_devclass;
