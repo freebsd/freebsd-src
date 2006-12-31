ff240e75e9003e8ded6ef58f4658e0039d41b830
Andrey A. Chernov <ache@FreeBSD.org>
2006-12-31 09:22:31 +0000

    Merge conflicts

--- contrib/libreadline/readline.h
+++ contrib/libreadline/readline.h
@@ -41,9 +41,9 @@ extern "C" {
 #endif
 
 /* Hex-encoded Readline version number. */
-#define RL_READLINE_VERSION	0x0501		/* Readline 5.1 */
+#define RL_READLINE_VERSION	0x0502		/* Readline 5.2 */
 #define RL_VERSION_MAJOR	5
-#define RL_VERSION_MINOR	1
+#define RL_VERSION_MINOR	2
 
 /* Readline data structures. */
 
@@ -757,6 +757,10 @@ extern int rl_ignore_completion_duplicates;
 /* If this is non-zero, completion is (temporarily) inhibited, and the
    completion character will be inserted as any other. */
 extern int rl_inhibit_completion;
+
+/* Input error; can be returned by (*rl_getc_function) if readline is reading
+   a top-level command (RL_ISSTATE (RL_STATE_READCMD)). */
+#define READERR			(-2)
 
 /* Definitions available for use by readline clients. */
 #define RL_PROMPT_START_IGNORE	'\001'
