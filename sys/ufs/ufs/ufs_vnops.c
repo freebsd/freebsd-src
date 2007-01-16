195759e37d06fadf149d159cc58a92b8434ca393
mpp <mpp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-01-16 19:40:25 +0000

    Fix a spelling error.  heirarchy -> hierarchy.
    Obtained from:	OpenBSD
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@166052 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/ufs/ufs/ufs_vnops.c
+++ sys/ufs/ufs/ufs_vnops.c
@@ -1108,7 +1108,7 @@ abortit:
 	/*
 	 * If ".." must be changed (ie the directory gets a new
 	 * parent) then the source directory must not be in the
-	 * directory heirarchy above the target, as this would
+	 * directory hierarchy above the target, as this would
 	 * orphan everything below the source directory. Also
 	 * the user must have write permission in the source so
 	 * as to be able to change "..". We must repeat the call
