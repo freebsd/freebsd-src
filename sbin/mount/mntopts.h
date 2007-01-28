c3870c75a8cec01b80ce8ce290432432435d74c7
rodrigc <rodrigc@FreeBSD.org>
2007-01-28 17:42:06 +0000

    MFC: Add build_iovec_argf() function.

--- sbin/mount/mntopts.h
+++ sbin/mount/mntopts.h
@@ -96,3 +96,4 @@ void rmslashes(char *, char *);
 void checkpath(const char *, char resolved_path[]);
 extern int getmnt_silent;
 void build_iovec(struct iovec **iov, int *iovlen, const char *name, void *val, int len);
+void build_iovec_argf(struct iovec **iov, int *iovlen, const char *name, const char *fmt, ...);
