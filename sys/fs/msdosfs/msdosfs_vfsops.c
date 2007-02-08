620e082cc59f37f076a4c23fd2791cbd20425cd6
rodrigc <rodrigc@FreeBSD.org>
2007-02-08 02:30:55 +0000

    Add noatime to the list of mount options that msdosfs accepts.
    PR:		108896
    Submitted by:	Eugene Grosbein <eugen grosbein pp ru>

--- sys/fs/msdosfs/msdosfs_vfsops.c
+++ sys/fs/msdosfs/msdosfs_vfsops.c
@@ -78,7 +78,7 @@
 /* List of mount options we support */
 static const char *msdosfs_opts[] = {
 	"from",
-	"export", "force", "sync",
+	"atime", "export", "force", "sync",
 	"uid", "gid", "mask", "dirmask",
 	"shortname", "shortnames", "longname", "longnames", "nowin95", "win95",
 	"kiconv", "cs_win", "cs_dos", "cs_local", "large",
