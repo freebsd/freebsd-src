cc8724f825ff3c1b6e5b04ad5937fa82976d4e3c
mlaier <mlaier@FreeBSD.org>
2007-02-10 15:43:58 +0000

    Fix small altq related copy and paste error.

--- sys/arm/at91/if_ate.c
+++ sys/arm/at91/if_ate.c
@@ -213,7 +213,7 @@ ate_attach(device_t dev)
 	ifp->if_init = ateinit;
 	ifp->if_baudrate = 10000000;
 	IFQ_SET_MAXLEN(&ifp->if_snd, IFQ_MAXLEN);
-	ifp->if_snd.ifq_maxlen = IFQ_MAXLEN;
+	ifp->if_snd.ifq_drv_maxlen = IFQ_MAXLEN;
 	IFQ_SET_READY(&ifp->if_snd);
 	ifp->if_timer = 0;
 	ifp->if_linkmib = &sc->mibdata;
