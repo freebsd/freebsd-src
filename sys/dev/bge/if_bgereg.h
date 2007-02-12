ca9c543d97f8c01e02c48b78fc7195da7723cc56
jkim <jkim@FreeBSD.org>
2007-02-12 22:51:25 +0000

    Add PHY DSP code for BCM5755M.
    Obtained from:	OpenBSD

--- sys/dev/bge/if_bgereg.h
+++ sys/dev/bge/if_bgereg.h
@@ -2463,6 +2463,7 @@ struct bge_softc {
 #define BGE_FLAG_5704_A0_BUG	0x00800000
 #define BGE_FLAG_JITTER_BUG	0x01000000
 #define BGE_FLAG_BER_BUG	0x02000000
+#define BGE_FLAG_ADJUST_TRIM	0x04000000
 	uint32_t		bge_chipid;
 	uint8_t			bge_asicrev;
 	uint8_t			bge_chiprev;
