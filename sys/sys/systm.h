b5dbc186f8894fe60bd65a53ab22108c278a2401
cperciva <cperciva@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-02-14 05:21:22 +0000

    Optimize bitcount32 by replacing 6 logical operations with 2.  The key observation here is that it doesn't matter what garbage accumulates in bits which we're going to end up masking away anyway, as long as the garbage doesn't overflow into bits which we care about.
    This improved version may not be the fastest possible on all systems,
    but it's certainly going to be better than what was here before.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@166698 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -391,9 +391,9 @@ bitcount32(uint32_t x)
 
 	x = (x & 0x55555555) + ((x & 0xaaaaaaaa) >> 1);
 	x = (x & 0x33333333) + ((x & 0xcccccccc) >> 2);
-	x = (x & 0x0f0f0f0f) + ((x & 0xf0f0f0f0) >> 4);
-	x = (x & 0x00ff00ff) + ((x & 0xff00ff00) >> 8);
-	x = (x & 0x0000ffff) + ((x & 0xffff0000) >> 16);
+	x = (x + (x >> 4)) & 0x0f0f0f0f;
+	x = (x + (x >> 8));
+	x = (x + (x >> 16)) & 0x000000ff;
 	return (x);
 }
 
