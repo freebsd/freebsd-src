df99d574b5108aba65b940cbb910d8f6dae0b05f
jkim <jkim@FreeBSD.org>
2007-02-15 00:54:40 +0000

    MFP4: 113025, 113146, 113177, 113203, 113500, 113546, 113570
    - PROT_READ, PROT_WRITE, or PROT_EXEC implies PROT_READ and PROT_EXEC.
    Linux/ia64's i386 emulation layer does this and it complies with Linux
    header files.  This fixes mmap05 LTP test case on amd64.
    - Do not adjust stack size when failure has occurred.
    - Synchronize i386 mmap/mprotect with amd64.

--- sys/amd64/linux32/linux.h
+++ sys/amd64/linux32/linux.h
@@ -167,6 +167,15 @@ struct l_rusage {
 #define	LINUX_MAP_ANON		0x0020
 #define	LINUX_MAP_GROWSDOWN	0x0100
 
+struct l_mmap_argv {
+	l_uintptr_t	addr;
+	l_size_t	len;
+	l_int		prot;
+	l_int		flags;
+	l_int		fd;
+	l_off_t		pgoff;
+} __packed;
+
 /*
  * stat family of syscalls
  */
