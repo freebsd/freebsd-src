03dc38a48bf79c08163e83925db2c0e079b1ac57
Robert Watson <rwatson@FreeBSD.org>
2007-02-18 08:57:23 +0000

    #ifdef INET6 printing of inpcb IPv6 addresses in DDB.  Patch committed with minor adjustments.
    Submitted by:	Florian C. Smeets <flo at kasimir dot com>

--- sys/netinet/in_pcb.c
+++ sys/netinet/in_pcb.c
@@ -1261,15 +1261,19 @@ db_print_inconninfo(struct in_conninfo *inc, const char *name, int indent)
 
 	indent += 2;
 
+#ifdef INET6
 	if (inc->inc_flags == 1) {
 		/* IPv6. */
 		ip6_sprintf(laddr_str, &inc->inc6_laddr);
 		ip6_sprintf(faddr_str, &inc->inc6_faddr);
 	} else {
+#endif
 		/* IPv4. */
 		inet_ntoa_r(inc->inc_laddr, laddr_str);
 		inet_ntoa_r(inc->inc_faddr, faddr_str);
+#ifdef INET6
 	}
+#endif
 	db_print_indent(indent);
 	db_printf("inc_laddr %s   inc_lport %u\n", laddr_str,
 	    ntohs(inc->inc_lport));
