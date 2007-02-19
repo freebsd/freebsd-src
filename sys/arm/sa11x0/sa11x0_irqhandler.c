1999479db4269fa650da68faafe34313bb4f9adf
Olivier Houchard <cognet@FreeBSD.org>
2007-02-19 00:23:40 +0000

    Oops, we have no sc here, so use the global sa11x0_softc.
    Spotted out by:	csjp

--- sys/arm/sa11x0/sa11x0_irqhandler.c
+++ sys/arm/sa11x0/sa11x0_irqhandler.c
@@ -111,8 +111,8 @@ arm_get_next_irq()
 {
 	int irq;
 
-	if ((irq = (bus_space_read_4(sc->sc_iot, sc->sc_ioh, SAIPIC_IP) &
-	    sa11x0_irq_mask)) != 0)
+	if ((irq = (bus_space_read_4(sa11x0_softc->sc_iot, sa11x0_softc->sc_ioh,
+	    SAIPIC_IP) & sa11x0_irq_mask)) != 0)
 		return (ffs(irq) - 1);
 	return (-1);
 }
