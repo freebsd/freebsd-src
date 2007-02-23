52789dd7899e7d588940a789eb18040a0f46c638
ariff <ariff@FreeBSD.org>
2007-02-23 19:39:06 +0000

    Use snd_setup_intr() instead of bus_setup_intr() , like other drivers.

--- sys/dev/sound/pci/vibes.c
+++ sys/dev/sound/pci/vibes.c
@@ -762,7 +762,7 @@ sv_attach(device_t dev) {
         sc->irq   = bus_alloc_resource(dev, SYS_RES_IRQ, &sc->irqid,
 				       0, ~0, 1, RF_ACTIVE | RF_SHAREABLE);
         if (!sc->irq ||
-	    bus_setup_intr(dev, sc->irq, INTR_TYPE_AV, NULL, sv_intr, sc, &sc->ih)) {
+	    snd_setup_intr(dev, sc->irq, 0, sv_intr, sc, &sc->ih)) {
                 device_printf(dev, "sv_attach: Unable to map interrupt\n");
                 goto fail;
         }
