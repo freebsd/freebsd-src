37e80fcac22c6734bcdb79e160bc1eac56740c4c
John Baldwin <jhb@FreeBSD.org>
2007-02-23 16:22:09 +0000

    Add a new kernel sleep function pause(9).  pause(9) is for places that want an equivalent of DELAY(9) that sleeps instead of spins.  It accepts a wmesg and a timeout and is not interrupted by signals.  It uses a private wait channel that should never be woken up by wakeup(9) or wakeup_one(9).
    Glanced at by:	phk

--- sys/sys/sleepqueue.h
+++ sys/sys/sleepqueue.h
@@ -84,6 +84,7 @@ struct thread;
 #define	SLEEPQ_TYPE		0x0ff		/* Mask of sleep queue types. */
 #define	SLEEPQ_MSLEEP		0x00		/* Used by msleep/wakeup. */
 #define	SLEEPQ_CONDVAR		0x01		/* Used for a cv. */
+#define	SLEEPQ_PAUSE		0x02		/* Used by pause. */
 #define	SLEEPQ_INTERRUPTIBLE	0x100		/* Sleep is interruptible. */
 
 void	init_sleepqueues(void);
