2dd094b15f75573ac25e5bba66de40112b41bfb1
cognet <cognet@FreeBSD.org>
2007-02-24 16:08:03 +0000

    MFC rev 1.112: Use __NO_STRICT_ALIGNMENT, instead of special casing ia64 and sparc64.

--- sys/net/if_loop.c
+++ sys/net/if_loop.c
@@ -289,7 +289,7 @@ if_simloop(ifp, m, af, hlen)
 	/* Strip away media header */
 	if (hlen > 0) {
 		m_adj(m, hlen);
-#if defined(__alpha__) || defined(__ia64__) || defined(__sparc64__)
+#ifndef __NO_STRICT_ALIGNMENT
 		/* The alpha doesn't like unaligned data.
 		 * We move data down in the first mbuf */
 		if (mtod(m, vm_offset_t) & 3) {
