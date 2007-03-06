32acf7e44635d0e5b2f6ddbde2487b3b32c3332b
scottl <scottl@FreeBSD.org>
2007-03-06 18:28:43 +0000

    Don't increment total_bounced when doing no-op dmamap_sync ops.

--- sys/ia64/ia64/busdma_machdep.c
+++ sys/ia64/ia64/busdma_machdep.c
@@ -820,7 +820,6 @@ _bus_dmamap_sync(bus_dma_tag_t dmat, bus_dmamap_t map, bus_dmasync_op_t op)
 		 * want to add support for invalidating
 		 * the caches on broken hardware
 		 */
-		total_bounced++;
 
 		if (op & BUS_DMASYNC_PREWRITE) {
 			while (bpage != NULL) {
@@ -829,6 +828,7 @@ _bus_dmamap_sync(bus_dma_tag_t dmat, bus_dmamap_t map, bus_dmasync_op_t op)
 				      bpage->datacount);
 				bpage = STAILQ_NEXT(bpage, links);
 			}
+			total_bounced++;
 		}
 
 		if (op & BUS_DMASYNC_POSTREAD) {
@@ -838,6 +838,7 @@ _bus_dmamap_sync(bus_dma_tag_t dmat, bus_dmamap_t map, bus_dmasync_op_t op)
 				      bpage->datacount);
 				bpage = STAILQ_NEXT(bpage, links);
 			}
+			total_bounced++;
 		}
 	}
 }
