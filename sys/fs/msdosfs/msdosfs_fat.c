096ed7804fe6ad8b31d320ed9a4b7ec16b2a1377
trhodes <trhodes@FreeBSD.org>
2007-03-08 03:01:00 +0000

    MFC:
    Add a 3rd entry in the cache, which keeps the end position
    from just before extending a file.  This has the desired effect
    of keeping the write speed constant.  And yes, that helps a lot
    copying large files always at full speed now, and I have seen
    improvements using benchmarks/bonnie.
    
    Stolen from:	NetBSD
    Reviewed by:	bde

--- sys/fs/msdosfs/msdosfs_fat.c
+++ sys/fs/msdosfs/msdosfs_fat.c
@@ -79,6 +79,8 @@ static int fc_bmapcalls;		/* # of times pcbmap was called		 */
 static int fc_lmdistance[LMMAX];/* counters for how far off the last
 				 * cluster mapped entry was. */
 static int fc_largedistance;	/* off by more than LMMAX		 */
+static int fc_wherefrom, fc_whereto, fc_lastclust;
+static int pm_fatblocksize;
 
 static int	chainalloc(struct msdosfsmount *pmp, u_long start,
 		    u_long count, u_long fillwith, u_long *retcluster,
@@ -119,6 +121,7 @@ fatblock(pmp, ofs, bnp, sizep, bop)
 		*sizep = size;
 	if (bop)
 		*bop = ofs % pmp->pm_fatblocksize;
+		pm_fatblocksize = pmp->pm_fatblocksize;
 }
 
 /*
@@ -210,9 +213,12 @@ pcbmap(dep, findcn, bnp, cnp, sp)
 	 */
 	i = 0;
 	fc_lookup(dep, findcn, &i, &cn);
-	if ((bn = findcn - i) >= LMMAX)
+	if ((bn = findcn - i) >= LMMAX) {
 		fc_largedistance++;
-	else
+		fc_wherefrom = i;
+		fc_whereto = findcn;
+		fc_lastclust = dep->de_fc[FC_LASTFC].fc_frcn;
+	} else
 		fc_lmdistance[bn]++;
 
 	/*
@@ -1024,6 +1030,8 @@ extendfile(dep, count, bpp, ncp, flags)
 		if (error != E2BIG)
 			return (error);
 	}
+
+	fc_last_to_nexttolast(dep);
 
 	while (count > 0) {
 		/*
