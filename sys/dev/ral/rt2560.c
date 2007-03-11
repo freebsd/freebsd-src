44a133f2e813fcb90c74f699b232eb1c17e9ccc4
sam <sam@FreeBSD.org>
2007-03-11 22:43:35 +0000

    allow net80211 to fillin rate sets
    MFC after:	2 weeks

--- sys/dev/ral/rt2560.c
+++ sys/dev/ral/rt2560.c
@@ -151,18 +151,6 @@ static void		rt2560_stop(void *);
 static int		rt2560_raw_xmit(struct ieee80211_node *, struct mbuf *,
 				const struct ieee80211_bpf_params *);
 
-/*
- * Supported rates for 802.11a/b/g modes (in 500Kbps unit).
- */
-static const struct ieee80211_rateset rt2560_rateset_11a =
-	{ 8, { 12, 18, 24, 36, 48, 72, 96, 108 } };
-
-static const struct ieee80211_rateset rt2560_rateset_11b =
-	{ 4, { 2, 4, 11, 22 } };
-
-static const struct ieee80211_rateset rt2560_rateset_11g =
-	{ 12, { 2, 4, 11, 22, 12, 18, 24, 36, 48, 72, 96, 108 } };
-
 static const struct {
 	uint32_t	reg;
 	uint32_t	val;
@@ -287,9 +275,6 @@ rt2560_attach(device_t dev, int id)
 	    IEEE80211_C_WPA;		/* 802.11i */
 
 	if (sc->rf_rev == RT2560_RF_5222) {
-		/* set supported .11a rates */
-		ic->ic_sup_rates[IEEE80211_MODE_11A] = rt2560_rateset_11a;
-
 		/* set supported .11a channels */
 		for (i = 36; i <= 64; i += 4) {
 			ic->ic_channels[i].ic_freq =
@@ -307,10 +292,6 @@ rt2560_attach(device_t dev, int id)
 			ic->ic_channels[i].ic_flags = IEEE80211_CHAN_A;
 		}
 	}
-
-	/* set supported .11b and .11g rates */
-	ic->ic_sup_rates[IEEE80211_MODE_11B] = rt2560_rateset_11b;
-	ic->ic_sup_rates[IEEE80211_MODE_11G] = rt2560_rateset_11g;
 
 	/* set supported .11b and .11g channels (1 through 14) */
 	for (i = 1; i <= 14; i++) {
@@ -2090,7 +2071,7 @@ rt2560_start(struct ifnet *ifp)
 static void
 rt2560_watchdog(void *arg)
 {
-	struct rt2560_softc *sc = (struct rt2560_softc *)arg;
+	struct rt2560_softc *sc = arg;
 	struct ieee80211com *ic = &sc->sc_ic;
 
 	if (sc->sc_tx_timer > 0) {
