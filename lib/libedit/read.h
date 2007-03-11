0aafd373d548e089bf97b7c90ae72ad537a4a617
stefanf <stefanf@FreeBSD.org>
2007-03-11 08:41:01 +0000

    Merge changes to the NetBSD copyright (advertising clause removal).

--- lib/libedit/read.h
+++ lib/libedit/read.h
@@ -13,11 +13,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
+ * 3. Neither the name of The NetBSD Foundation nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
  *
@@ -33,7 +29,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- *	$NetBSD: read.h,v 1.4 2004/02/27 14:52:18 christos Exp $
+ *	$NetBSD: read.h,v 1.5 2006/08/21 12:45:30 christos Exp $
  * $FreeBSD$
  */
 
