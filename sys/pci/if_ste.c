3d70d171ff1830d2a93c463b3fdc2f723afd11c8
Pyun YongHyeon <yongari@FreeBSD.org>
2007-03-19 06:58:47 +0000

    MFC if_ste.c rev 1.98, if_stereg.h rev 1.22 to RELENG_6.   Add new ST201 PCI id.

--- sys/pci/if_ste.c
+++ sys/pci/if_ste.c
@@ -85,7 +85,8 @@ MODULE_DEPEND(ste, miibus, 1, 1, 1);
  * Various supported device vendors/types and their names.
  */
 static struct ste_type ste_devs[] = {
-	{ ST_VENDORID, ST_DEVICEID_ST201, "Sundance ST201 10/100BaseTX" },
+	{ ST_VENDORID, ST_DEVICEID_ST201_1, "Sundance ST201 10/100BaseTX" },
+	{ ST_VENDORID, ST_DEVICEID_ST201_2, "Sundance ST201 10/100BaseTX" },
 	{ DL_VENDORID, DL_DEVICEID_DL10050, "D-Link DL10050 10/100BaseTX" },
 	{ 0, 0, NULL }
 };
