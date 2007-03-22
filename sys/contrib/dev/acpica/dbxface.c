3d51f6be85162574efa21c0fe06003e7fab3fa3e
jkim <jkim@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-03-22 18:02:34 +0000

    Resolve conflicts from import of Intel ACPI-CA 20070320.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@167812 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/contrib/dev/acpica/dbxface.c
+++ sys/contrib/dev/acpica/dbxface.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
  *
  * Module Name: dbxface - AML Debugger external interfaces
- *              $Revision: 71 $
+ *              $Revision: 1.78 $
  *
  ******************************************************************************/
 
@@ -9,7 +9,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999 - 2005, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2007, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -532,7 +532,7 @@ AcpiDbInitialize (
 
         /* Create the debug execution thread to execute commands */
 
-        Status = AcpiOsQueueForExecution (0, AcpiDbExecuteThread, NULL);
+        Status = AcpiOsExecute (OSL_DEBUGGER_THREAD, AcpiDbExecuteThread, NULL);
         if (ACPI_FAILURE (Status))
         {
             AcpiOsPrintf ("Could not start debugger thread\n");
