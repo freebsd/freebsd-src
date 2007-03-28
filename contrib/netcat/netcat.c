b2f6436d8f7147ff2b48cd694b1f70130a384c97
Xin LI <delphij@FreeBSD.org>
2007-03-28 01:55:49 +0000

    Import netcat as of today's OPENBSD_4_1 snapshot.

--- contrib/netcat/netcat.c
+++ contrib/netcat/netcat.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: netcat.c,v 1.87 2006/02/01 21:33:14 otto Exp $ */
+/* $OpenBSD: netcat.c,v 1.89 2007/02/20 14:11:17 jmc Exp $ */
 /*
  * Copyright (c) 2001 Eric Jackson <ericj@monkey.org>
  *
@@ -65,7 +65,7 @@
 
 /* Command Line Options */
 int	dflag;					/* detached, no stdin */
-int	iflag;					/* Interval Flag */
+unsigned int iflag;				/* Interval Flag */
 int	jflag;					/* use jumbo frames if we can */
 int	kflag;					/* More than one connect */
 int	lflag;					/* Bind to local port */
@@ -106,13 +106,13 @@ int
 main(int argc, char *argv[])
 {
 	int ch, s, ret, socksv;
-	char *host, *uport, *endp;
+	char *host, *uport;
 	struct addrinfo hints;
 	struct servent *sv;
 	socklen_t len;
 	struct sockaddr_storage cliaddr;
 	char *proxy;
-	const char *proxyhost = "", *proxyport = NULL;
+	const char *errstr, *proxyhost = "", *proxyport = NULL;
 	struct addrinfo proxyhints;
 
 	ret = 1;
@@ -120,7 +120,6 @@ main(int argc, char *argv[])
 	socksv = 5;
 	host = NULL;
 	uport = NULL;
-	endp = NULL;
 	sv = NULL;
 
 	while ((ch = getopt(argc, argv,
@@ -152,9 +151,9 @@ main(int argc, char *argv[])
 			help();
 			break;
 		case 'i':
-			iflag = (int)strtoul(optarg, &endp, 10);
-			if (iflag < 0 || *endp != '\0')
-				errx(1, "interval cannot be negative");
+			iflag = strtonum(optarg, 0, UINT_MAX, &errstr);
+			if (errstr)
+				errx(1, "interval %s: %s", errstr, optarg);
 			break;
 		case 'j':
 			jflag = 1;
@@ -190,11 +189,9 @@ main(int argc, char *argv[])
 			vflag = 1;
 			break;
 		case 'w':
-			timeout = (int)strtoul(optarg, &endp, 10);
-			if (timeout < 0 || *endp != '\0')
-				errx(1, "timeout cannot be negative");
-			if (timeout >= (INT_MAX / 1000))
-				errx(1, "timeout too large");
+			timeout = strtonum(optarg, 0, INT_MAX / 1000, &errstr);
+			if (errstr)
+				errx(1, "timeout %s: %s", errstr, optarg);
 			timeout *= 1000;
 			break;
 		case 'x':
@@ -681,7 +678,8 @@ atelnet(int nfd, unsigned char *buf, unsigned int size)
 void
 build_ports(char *p)
 {
-	char *n, *endp;
+	const char *errstr;
+	char *n;
 	int hi, lo, cp;
 	int x = 0;
 
@@ -693,12 +691,12 @@ build_ports(char *p)
 		n++;
 
 		/* Make sure the ports are in order: lowest->highest. */
-		hi = (int)strtoul(n, &endp, 10);
-		if (hi <= 0 || hi > PORT_MAX || *endp != '\0')
-			errx(1, "port range not valid");
-		lo = (int)strtoul(p, &endp, 10);
-		if (lo <= 0 || lo > PORT_MAX || *endp != '\0')
-			errx(1, "port range not valid");
+		hi = strtonum(n, 1, PORT_MAX, &errstr);
+		if (errstr)
+			errx(1, "port number %s: %s", errstr, n);
+		lo = strtonum(p, 1, PORT_MAX, &errstr);
+		if (errstr)
+			errx(1, "port number %s: %s", errstr, p);
 
 		if (lo > hi) {
 			cp = hi;
@@ -728,9 +726,9 @@ build_ports(char *p)
 			}
 		}
 	} else {
-		hi = (int)strtoul(p, &endp, 10);
-		if (hi <= 0 || hi > PORT_MAX || *endp != '\0')
-			errx(1, "port range not valid");
+		hi = strtonum(p, 1, PORT_MAX, &errstr);
+		if (errstr)
+			errx(1, "port number %s: %s", errstr, p);
 		portlist[0] = calloc(1, PORT_MAX_LEN);
 		if (portlist[0] == NULL)
 			err(1, NULL);
@@ -837,8 +835,8 @@ help(void)
 void
 usage(int ret)
 {
-	fprintf(stderr, "usage: nc [-46DdhklnrStUuvz] [-i interval] [-p source_port]\n");
-	fprintf(stderr, "\t  [-s source_ip_address] [-T ToS] [-w timeout] [-X proxy_version]\n");
+	fprintf(stderr, "usage: nc [-46DdhklnrStUuvz] [-i interval] [-P proxy_username] [-p source_port]\n");
+	fprintf(stderr, "\t  [-s source_ip_address] [-T ToS] [-w timeout] [-X proxy_protocol]\n");
 	fprintf(stderr, "\t  [-x proxy_address[:port]] [hostname] [port[s]]\n");
 	if (ret)
 		exit(1);
