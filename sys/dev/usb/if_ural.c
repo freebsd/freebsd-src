3408c893085bd151919e0a3f5755890391b7d4e0
sam <sam@FreeBSD.org>
2007-03-31 22:00:44 +0000

    MFC 1.49: allow net80211 to fillin rate sets

--- sys/dev/usb/if_ural.c
+++ sys/dev/usb/if_ural.c
@@ -174,18 +174,6 @@ Static void		ural_ratectl(struct ural_amrr *,
 			    struct ieee80211_node *);
 
 /*
- * Supported rates for 802.11a/b/g modes (in 500Kbps unit).
- */
-static const struct ieee80211_rateset ural_rateset_11a =
-	{ 8, { 12, 18, 24, 36, 48, 72, 96, 108 } };
-
-static const struct ieee80211_rateset ural_rateset_11b =
-	{ 4, { 2, 4, 11, 22 } };
-
-static const struct ieee80211_rateset ural_rateset_11g =
-	{ 12, { 2, 4, 11, 22, 12, 18, 24, 36, 48, 72, 96, 108 } };
-
-/*
  * Default values for MAC registers; values taken from the reference driver.
  */
 static const struct {
@@ -467,9 +455,6 @@ USB_ATTACH(ural)
 	    IEEE80211_C_WPA;		/* 802.11i */
 
 	if (sc->rf_rev == RAL_RF_5222) {
-		/* set supported .11a rates */
-		ic->ic_sup_rates[IEEE80211_MODE_11A] = ural_rateset_11a;
-
 		/* set supported .11a channels */
 		for (i = 36; i <= 64; i += 4) {
 			ic->ic_channels[i].ic_freq =
@@ -487,10 +472,6 @@ USB_ATTACH(ural)
 			ic->ic_channels[i].ic_flags = IEEE80211_CHAN_A;
 		}
 	}
-
-	/* set supported .11b and .11g rates */
-	ic->ic_sup_rates[IEEE80211_MODE_11B] = ural_rateset_11b;
-	ic->ic_sup_rates[IEEE80211_MODE_11G] = ural_rateset_11g;
 
 	/* set supported .11b and .11g channels (1 through 14) */
 	for (i = 1; i <= 14; i++) {
