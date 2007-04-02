be2f3e1ce25e30f2f48aba0e246adb5a7e2eb0f1
kientzle <kientzle@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-04-02 00:15:45 +0000

    Style:  bare "unsigned" is deprecated, use "unsigned int" instead.
    Thanks to: Joerg Sonnenberger
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@168231 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libarchive/archive_private.h
+++ lib/libarchive/archive_private.h
@@ -63,8 +63,8 @@ struct archive {
 	 * ridiculous time, or the client passes us an invalid
 	 * pointer, these values allow me to catch that.
 	 */
-	unsigned	  magic;
-	unsigned	  state;
+	unsigned int	magic;
+	unsigned int	state;
 
 	/*
 	 * Some public API functions depend on the "real" type of the
@@ -89,8 +89,8 @@ struct archive {
 };
 
 /* Check magic value and state; exit if it isn't valid. */
-void	__archive_check_magic(struct archive *, unsigned magic,
-	    unsigned state, const char *func);
+void	__archive_check_magic(struct archive *, unsigned int magic,
+	    unsigned int state, const char *func);
 
 void	__archive_errx(int retvalue, const char *msg);
 
