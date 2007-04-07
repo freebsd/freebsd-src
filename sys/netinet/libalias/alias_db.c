b3e0266015310841ae43c0e0bb81c604ea8856ea
piso <piso@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-04-07 09:47:39 +0000

    Silence Coverity about an unused variable.
    Reviewed by: 	glebius
    Approved by: 	glebius (mentor)
    MFC after: 	3 days
    CID: 		538
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@168458 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/libalias/alias_db.c
+++ sys/netinet/libalias/alias_db.c
@@ -2176,7 +2176,7 @@ SetDestCallId(struct alias_link *lnk, u_int16_t cid)
 
 	LIBALIAS_LOCK_ASSERT(la);
 	la->deleteAllLinks = 1;
-	lnk = ReLink(lnk, lnk->src_addr, lnk->dst_addr, lnk->alias_addr,
+	ReLink(lnk, lnk->src_addr, lnk->dst_addr, lnk->alias_addr,
 	    lnk->src_port, cid, lnk->alias_port, lnk->link_type);
 	la->deleteAllLinks = 0;
 }
