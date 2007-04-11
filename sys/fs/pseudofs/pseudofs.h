d7749266efe7334fdfc3722e35a62e43e315d721
des <des@FreeBSD.org>
2007-04-11 22:40:57 +0000

    Add a flag to struct pfs_vdata to mark the vnode as dead (e.g. process- specific nodes when the process exits)
    Move the vnode-cache-walking loop which was duplicated in pfs_exit() and
    pfs_disable() into its own function, pfs_purge(), which looks for vnodes
    marked as dead and / or belonging to the specified pfs_node and reclaims
    them.  Note that this loop is still extremely inefficient.
    
    Add a comment in pfs_vncache_alloc() explaining why we have to purge the
    vnode from the vnode cache before returning, in case anyone should be
    tempted to remove the call to cache_purge().
    
    Move the special handling for pfstype_root nodes into pfs_fileno_alloc()
    and pfs_fileno_free() (the root node's fileno must always be 2).  This
    also fixes a bug where pfs_fileno_free() would reclaim the root node's
    fileno, triggering a panic in the unr code, as that fileno was never
    allocated from unr to begin with.
    
    When destroying a pfs_node, release its fileno and purge it from the
    vnode cache.  I wish we could put off the call to pfs_purge() until
    after the entire tree had been destroyed, but then we'd have vnodes
    referencing freed pfs nodes.  This probably doesn't matter while we're
    still under Giant, but might become an issue later.
    
    When destroying a pseudofs instance, destroy the tree before tearing
    down the fileno allocator.
    
    In pfs_mount(), acquire the mountpoint interlock when required.
    
    MFC after:	3 weeks

--- sys/fs/pseudofs/pseudofs.h
+++ sys/fs/pseudofs/pseudofs.h
@@ -236,6 +236,7 @@ struct pfs_node	*pfs_create_link(struct pfs_node *parent, const char *name,
 				 pfs_vis_t vis, pfs_destroy_t destroy,
 				 int flags);
 struct pfs_node	*pfs_find_node	(struct pfs_node *parent, const char *name);
+void		 pfs_purge	(struct pfs_node *pn);
 int		 pfs_disable	(struct pfs_node *pn);
 int		 pfs_enable	(struct pfs_node *pn);
 int		 pfs_destroy	(struct pfs_node *pn);
