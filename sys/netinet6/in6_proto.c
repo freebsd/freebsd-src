1b349f5ee7bae0073c0189be1b702646af52bcf2
gnn <gnn@FreeBSD.org>
2007-04-24 11:44:22 +0000

    Add a new sysctl net.inet.ip6.rthdr0_allowed, which is default off, to disable the processing of Route Header Type 0 options in IPv6. This is not really an MFC because currently in HEAD the processing of these options is disabled unilateraly pending discussion at the IETF on the future of such headers.
    Suggested By: itojun
    Reviewed By: jinmei

--- sys/netinet6/in6_proto.c
+++ sys/netinet6/in6_proto.c
@@ -376,6 +376,8 @@ time_t	ip6_log_time = (time_t)0L;
 #ifdef IPSTEALTH
 int	ip6stealth = 0;
 #endif
+int     ip6_rthdr0_allowed = 0; /* Disallow use of routing header 0 */
+				/* by default. */
 
 /* icmp6 */
 /*
@@ -519,6 +521,9 @@ SYSCTL_INT(_net_inet6_ip6, IPV6CTL_MCAST_PMTU,
 SYSCTL_INT(_net_inet6_ip6, IPV6CTL_STEALTH, stealth, CTLFLAG_RW,
 	&ip6stealth, 0, "");
 #endif
+SYSCTL_INT(_net_inet6_ip6, IPV6CTL_RTHDR0_ALLOWED, 
+	   rthdr0_allowed, CTLFLAG_RW, &ip6_rthdr0_allowed, 0, "");
+
 
 /* net.inet6.icmp6 */
 SYSCTL_INT(_net_inet6_icmp6, ICMPV6CTL_REDIRACCEPT,
