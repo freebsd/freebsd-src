75d9fd42d2233cd8ac921673dbff5e8016d5ce64
suz <suz@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-05-04 02:34:17 +0000

    fixed a memory leak in unresolved ND queue processing
    Obtained from: KAME
    MFC after: 1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@169241 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet6/nd6.c
+++ sys/netinet6/nd6.c
@@ -1794,24 +1794,17 @@ fail:
 
 			if (ln->ln_hold) {
 				struct mbuf *m_hold, *m_hold_next;
-				for (m_hold = ln->ln_hold; m_hold;
-				     m_hold = m_hold_next) {
-					struct mbuf *mpkt = NULL;
-
+				for (m_hold = ln->ln_hold, ln->ln_hold = NULL;
+				    m_hold; m_hold = m_hold_next) {
 					m_hold_next = m_hold->m_nextpkt;
-					mpkt = m_copym(m_hold, 0, M_COPYALL, M_DONTWAIT);
-					if (mpkt == NULL) {
-						m_freem(m_hold);
-						break;
-					}
-					mpkt->m_nextpkt = NULL;
+					m_hold->m_nextpkt = NULL;
 
 					/*
 					 * we assume ifp is not a p2p here, so
 					 * just set the 2nd argument as the
 					 * 1st one.
 					 */
-					nd6_output(ifp, ifp, mpkt,
+					nd6_output(ifp, ifp, m_hold,
 					     (struct sockaddr_in6 *)rt_key(rt),
 					     rt);
 				}
