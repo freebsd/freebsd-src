c4b6ba9d5ea634d0f711f93cf16a083df73d231c
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-05-04 14:23:29 +0000

    Mark the filedescriptor table entries with VOP_OPEN being performed for them as UF_OPENING. Disable closing of that entries. This should fix the crashes caused by devfs_open() (and fifo_open()) dereferencing struct file * by index, while the filedescriptor is closed by parallel thread.
    Idea by:	tegge
    Reviewed by:	tegge (previous version of patch)
    Tested by:	Peter Holm
    Approved by:	re (kensmith)
    MFC after:	3 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@169250 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -984,7 +984,19 @@ kern_open(struct thread *td, char *path, enum uio_seg pathseg, int flags,
 	cmode = ((mode &~ fdp->fd_cmask) & ALLPERMS) &~ S_ISTXT;
 	NDINIT(&nd, LOOKUP, FOLLOW | AUDITVNODE1 | MPSAFE, pathseg, path, td);
 	td->td_dupfd = -1;		/* XXX check for fdopen */
+	FILEDESC_XLOCK(fdp);
+	if (fp != fdp->fd_ofiles[indx]) {
+		FILEDESC_XUNLOCK(fdp);
+		fdrop(fp, td);
+		td->td_retval[0] = indx;
+		return (0);
+	}
+	fdp->fd_ofileflags[indx] |= UF_OPENING;
+	FILEDESC_XUNLOCK(fdp);
 	error = vn_open(&nd, &flags, cmode, indx);
+	FILEDESC_XLOCK(fdp);
+	fdp->fd_ofileflags[indx] &= ~UF_OPENING;
+	FILEDESC_XUNLOCK(fdp);
 	if (error) {
 		/*
 		 * If the vn_open replaced the method vector, something
