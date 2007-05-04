6ab1aa7971bd4c712b1c6f75118c2dd8d2745ed6
davidch <davidch@FreeBSD.org>
2007-05-04 23:14:19 +0000

    MFC after:	2 weeks
    Updated copyright date to 2007.
    
    Tested with BCM5706 A3.
    
    Added ID for BCM5708 B2.
    
    Removed unused driver version string.
    
    Modified BCE_PRINTF macro to automatically fill-in the sc pointer.
    
    Fixed a kernel panic when the driver was loaded as a module from the
    command-line because the MII bus pointer was null (i.e. the MII bus
    hadn't been enumerated yet).
    
    Added fix proposed by Vladimir Ivanov <wawa@yandex-team.ru> to prevent
    driver state corruption when releasing the lock during the ISR in
    bce_rx_intr() to send packets up the stack.
    
    Added new TX chain and register read sysctl interfaces for debugging.
    
    Cleaned up formatting for various other debug routines.
    
    Added a new statistic maintained by firmware which tracks the number
    of received packets dropped because no receive buffers are available.

--- sys/dev/bce/if_bcefw.h
+++ sys/dev/bce/if_bcefw.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2006 Broadcom Corporation
+ * Copyright (c) 2006-2007 Broadcom Corporation
  *	David Christensen <davidch@broadcom.com>.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
