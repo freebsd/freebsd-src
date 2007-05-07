12b420a7675641fca1f22edcaf015efa7f446a84
ariff <ariff@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-05-07 02:46:48 +0000

    buf_addr should be bus_addr_t rather than u_int32_t.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@169332 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/sound/pcm/buffer.h
+++ sys/dev/sound/pcm/buffer.h
@@ -54,8 +54,8 @@ struct snd_dbuf {
 	u_int32_t flags;
 	bus_dmamap_t dmamap;
 	bus_dma_tag_t dmatag;
+	bus_addr_t buf_addr;
 	int dmaflags;
-	u_int32_t buf_addr;
 	struct selinfo sel;
 	struct pcm_channel *channel;
 	char name[SNDBUF_NAMELEN];
