9b6245d3e27e3beaf20fb549b21878ed67ff5692
rrs <rrs@FreeBSD.org>
2007-05-08 17:01:12 +0000

    -  Copyright change, cisco's silly tool wants it to say:        "Copyright (c) 2001-2007, by Cisco Systems,"    instead of        *Copyright (c) 2001-2007, Cisco Systems,"
    -  Also fix a few straglers that were still in 2006.

--- sys/netinet/sctp_auth.c
+++ sys/netinet/sctp_auth.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2001-2007, Cisco Systems, Inc. All rights reserved.
+ * Copyright (c) 2001-2007, by Cisco Systems, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
