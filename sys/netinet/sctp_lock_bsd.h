ffa53534cf6e2aeba7a7d7282d5f70d1cbff4b58
rrs <rrs@FreeBSD.org>
2007-05-08 17:01:12 +0000

    -  Copyright change, cisco's silly tool wants it to say:        "Copyright (c) 2001-2007, by Cisco Systems,"    instead of        *Copyright (c) 2001-2007, Cisco Systems,"
    -  Also fix a few straglers that were still in 2006.

--- sys/netinet/sctp_lock_bsd.h
+++ sys/netinet/sctp_lock_bsd.h
@@ -1,7 +1,7 @@
 #ifndef __sctp_lock_bsd_h__
 #define __sctp_lock_bsd_h__
 /*-
- * Copyright (c) 2001-2006, Cisco Systems, Inc. All rights reserved.
+ * Copyright (c) 2001-2007, by Cisco Systems, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
