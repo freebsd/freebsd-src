ddfae47b9d0305762c63519054e1e7025279cbda
Scott Long <scottl@FreeBSD.org>
2007-05-10 15:33:41 +0000

    Collapse the mfi_ld object.  Add an ioctl to help management apps map array Id's to FreeBSD device names.

--- sys/dev/mfi/mfi_ioctl.h
+++ sys/dev/mfi/mfi_ioctl.h
@@ -107,6 +107,16 @@ struct mfi_linux_ioc_aen {
 	uint32_t	laen_class_locale;
 } __packed;
 
+struct mfi_query_disk {
+	uint8_t	array_id;
+	uint8_t	present;
+	uint8_t	open;
+	uint8_t reserved;	/* reserved for future use */
+	char	devname[SPECNAMELEN + 1];
+} __packed;
+
+#define MFIIO_QUERY_DISK	_IOWR('Q', 102, struct mfi_query_disk)
+
 /*
  * Create a second set so the FreeBSD native ioctl doesn't
  * conflict in FreeBSD ioctl handler.  Translate in mfi_linux.c.
