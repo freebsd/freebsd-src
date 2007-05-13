511a8faf24f5609cff23167e93a15399f0457151
rwatson <rwatson@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-05-13 15:52:46 +0000

    Add prototypes for ether_aton_r() and ether_ntoa_r() missed in previous commit.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@169529 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net/ethernet.h
+++ sys/net/ethernet.h
@@ -396,9 +396,11 @@ void	ether_vlan_mtap(struct bpf_if *, struct mbuf *,
  */
 __BEGIN_DECLS
 struct	ether_addr *ether_aton(const char *);
+struct	ether_addr *ether_aton_r(const char *, struct ether_addr *);
 int	ether_hostton(const char *, struct ether_addr *);
 int	ether_line(const char *, struct ether_addr *, char *);
 char 	*ether_ntoa(const struct ether_addr *);
+char 	*ether_ntoa_r(const struct ether_addr *, char *);
 int	ether_ntohost(char *, const struct ether_addr *);
 __END_DECLS
 
