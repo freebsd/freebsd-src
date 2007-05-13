44df680f0455c9474bdea72734d18b4c4c2b5e88
rwatson <rwatson@FreeBSD.org>
2007-05-13 12:04:06 +0000

    Move to more conformant style(9) before making functional changes.

--- lib/libc/net/ether_addr.c
+++ lib/libc/net/ether_addr.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1995
- *	Bill Paul <wpaul@ctr.columbia.edu>.  All rights reserved.
+ * Copyright (c) 1995 Bill Paul <wpaul@ctr.columbia.edu>.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,101 +39,88 @@
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include <stdio.h>
-#include <paths.h>
 #include <sys/types.h>
-#include <string.h>
-#include <stdlib.h>
 #include <sys/param.h>
 #include <sys/socket.h>
+
 #include <net/ethernet.h>
+
 #ifdef YP
 #include <rpc/rpc.h>
 #include <rpcsvc/yp_prot.h>
 #include <rpcsvc/ypclnt.h>
 #endif
 
+#include <paths.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #ifndef _PATH_ETHERS
-#define _PATH_ETHERS "/etc/ethers"
+#define	_PATH_ETHERS	"/etc/ethers"
 #endif
 
 /*
- * Parse a string of text containing an ethernet address and hostname
- * and separate it into its component parts.
+ * Parse a string of text containing an ethernet address and hostname and
+ * separate it into its component parts.
  */
 int
-ether_line(l, e, hostname)
-	const char *l;
-	struct ether_addr *e;
-	char *hostname;
+ether_line(const char *l, struct ether_addr *e, char *hostname)
 {
         int i, o[6];
 
-        i = sscanf(l, "%x:%x:%x:%x:%x:%x %s", &o[0], &o[1], &o[2],
-                                              &o[3], &o[4], &o[5],
-					      hostname);
+        i = sscanf(l, "%x:%x:%x:%x:%x:%x %s", &o[0], &o[1], &o[2], &o[3],
+	    &o[4], &o[5], hostname);
 	if (i != 7)
                 return (i);
-
         for (i=0; i<6; i++)
                 e->octet[i] = o[i];
         return (0);
 }
 
 /*
- * Convert an ASCII representation of an ethernet address to
- * binary form.
+ * Convert an ASCII representation of an ethernet address to binary form.
  */
-struct
-ether_addr *ether_aton(a)
-	const char *a;
+struct ether_addr *
+ether_aton(const char *a)
 {
         int i;
 	static struct ether_addr o;
 	unsigned int o0, o1, o2, o3, o4, o5;
 
         i = sscanf(a, "%x:%x:%x:%x:%x:%x", &o0, &o1, &o2, &o3, &o4, &o5);
-
         if (i != 6)
                 return (NULL);
-
         o.octet[0]=o0;
 	o.octet[1]=o1;
 	o.octet[2]=o2;
 	o.octet[3]=o3;
 	o.octet[4]=o4;
 	o.octet[5]=o5;
-
         return ((struct ether_addr *)&o);
 }
 
 /*
- * Convert a binary representation of an ethernet address to
- * an ASCII string.
+ * Convert a binary representation of an ethernet address to an ASCII string.
  */
-char
-*ether_ntoa(n)
-	const struct ether_addr *n;
+char *
+ether_ntoa(const struct ether_addr *n)
 {
         int i;
 	static char a[18];
 
-        i = sprintf(a, "%02x:%02x:%02x:%02x:%02x:%02x",
-	    n->octet[0], n->octet[1], n->octet[2],
-	    n->octet[3], n->octet[4], n->octet[5]);
+        i = sprintf(a, "%02x:%02x:%02x:%02x:%02x:%02x", n->octet[0],
+	    n->octet[1], n->octet[2], n->octet[3], n->octet[4], n->octet[5]);
         if (i < 17)
                 return (NULL);
         return ((char *)&a);
 }
 
 /*
- * Map an ethernet address to a hostname. Use either /etc/ethers or
- * NIS/YP.
+ * Map an ethernet address to a hostname. Use either /etc/ethers or NIS/YP.
  */
 int
-ether_ntohost(hostname, e)
-	char *hostname;
-	const struct ether_addr *e;
+ether_ntohost(char *hostname, const struct ether_addr *e)
 {
 	FILE *fp;
 	char buf[BUFSIZ + 2];
@@ -145,9 +132,9 @@ ether_ntohost(hostname, e)
 	char *ether_a;
 	char *yp_domain;
 #endif
+
 	if ((fp = fopen(_PATH_ETHERS, "r")) == NULL)
 		return (1);
-
 	while (fgets(buf,BUFSIZ,fp)) {
 		if (buf[0] == '#')
 			continue;
@@ -157,7 +144,7 @@ ether_ntohost(hostname, e)
 				continue;
 			ether_a = ether_ntoa(e);
 			if (yp_match(yp_domain, "ethers.byaddr", ether_a,
-				strlen(ether_a), &result, &resultlen)) {
+			    strlen(ether_a), &result, &resultlen)) {
 				continue;
 			}
 			strncpy(buf, result, resultlen);
@@ -167,8 +154,8 @@ ether_ntohost(hostname, e)
 #endif
 		if (!ether_line(buf, &local_ether, local_host)) {
 			if (!bcmp((char *)&local_ether.octet[0],
-				(char *)&e->octet[0], 6)) {
-			/* We have a match */
+			    (char *)&e->octet[0], 6)) {
+				/* We have a match. */
 				strcpy(hostname, local_host);
 				fclose(fp);
 				return(0);
@@ -180,13 +167,10 @@ ether_ntohost(hostname, e)
 }
 
 /*
- * Map a hostname to an ethernet address using /etc/ethers or
- * NIS/YP.
+ * Map a hostname to an ethernet address using /etc/ethers or NIS/YP.
  */
 int
-ether_hostton(hostname, e)
-	const char *hostname;
-	struct ether_addr *e;
+ether_hostton(const char *hostname, struct ether_addr *e)
 {
 	FILE *fp;
 	char buf[BUFSIZ + 2];
@@ -197,9 +181,9 @@ ether_hostton(hostname, e)
 	int resultlen;
 	char *yp_domain;
 #endif
+
 	if ((fp = fopen(_PATH_ETHERS, "r")) == NULL)
 		return (1);
-
 	while (fgets(buf,BUFSIZ,fp)) {
 		if (buf[0] == '#')
 			continue;
@@ -208,7 +192,7 @@ ether_hostton(hostname, e)
 			if (yp_get_default_domain(&yp_domain))
 				continue;
 			if (yp_match(yp_domain, "ethers.byname", hostname,
-				strlen(hostname), &result, &resultlen)) {
+			    strlen(hostname), &result, &resultlen)) {
 				continue;
 			}
 			strncpy(buf, result, resultlen);
@@ -218,9 +202,9 @@ ether_hostton(hostname, e)
 #endif
 		if (!ether_line(buf, &local_ether, local_host)) {
 			if (!strcmp(hostname, local_host)) {
-				/* We have a match */
+				/* We have a match. */
 				bcopy((char *)&local_ether.octet[0],
-					(char *)&e->octet[0], 6);
+				    (char *)&e->octet[0], 6);
 				fclose(fp);
 				return(0);
 			}
