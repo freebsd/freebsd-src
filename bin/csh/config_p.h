59dfb2db03584859a57ab6d9519e2de147bf3c4d
Mark Peek <mp@FreeBSD.org>
2007-05-16 21:22:38 +0000

    Work around a vendor issue that was causing the builtin malloc to be used instead of the system malloc.
    Submitted by:	ume

--- bin/csh/config_p.h
+++ bin/csh/config_p.h
@@ -82,8 +82,6 @@
 #if defined(__FreeBSD__)
 #define NLS_BUGS
 #define BSD_STYLE_COLORLS
-/* we want to use the system malloc when we install as /bin/csh */
-#define SYSMALLOC
 /* Use LC_MESSAGES locale category to open the message catalog */
 #define MCLoadBySet NL_CAT_LOCALE
 #define BUFSIZE 8192
