95bf1ef490be6f898f480348aadc72480bb7a0cd
csjp <csjp@FreeBSD.org>
2007-05-17 16:03:14 +0000

    Correct typo in comment

--- sys/sys/lockf.h
+++ sys/sys/lockf.h
@@ -54,7 +54,7 @@ struct lockf {
 	off_t	lf_start;	    /* Byte # of the start of the lock */
 	off_t	lf_end;		    /* Byte # of the end of the lock (-1=EOF) */
 	caddr_t	lf_id;		    /* Id of the resource holding the lock */
-	struct	lockf **lf_head;    /* Back pointer to the head of the locf list */
+	struct	lockf **lf_head;    /* Back pointer to the head of the lockf list */
 	struct	inode *lf_inode;    /* Back pointer to the inode */
 	struct	lockf *lf_next;	    /* Pointer to the next lock on this inode */
 	struct	locklist lf_blkhd;  /* List of requests blocked on this lock */
