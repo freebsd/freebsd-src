c7182dea609ad229cda1716a8ca2c7bae7d18225
le <le@FreeBSD.org>
2007-05-24 18:28:08 +0000

    Fix handling of filenames containing whitespace.
    PR:      bin/112860
    MFC in:  3 days

--- usr.bin/less/lesspipe.sh
+++ usr.bin/less/lesspipe.sh
@@ -5,12 +5,12 @@
 
 case "$1" in
 	*.Z)
-		exec uncompress -c $1	2>/dev/null
+		exec uncompress -c "$1"	2>/dev/null
 		;;
 	*.gz)
-		exec gzip -d -c $1	2>/dev/null
+		exec gzip -d -c "$1"	2>/dev/null
 		;;
 	*.bz2)
-		exec bzip2 -d -c $1	2>/dev/null
+		exec bzip2 -d -c "$1"	2>/dev/null
 		;;
 esac
