9d9e9abebfd2c3d39723e676e8fc6d9b8d62fd55
kmacy <kmacy@FreeBSD.org>
2007-05-25 16:42:25 +0000

    update license headers

--- sys/dev/cxgb/sys/mvec.h
+++ sys/dev/cxgb/sys/mvec.h
@@ -9,9 +9,9 @@
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
+ * 2. The name of Kip Macy nor the names of other
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
