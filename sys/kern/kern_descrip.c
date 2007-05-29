3ec54e38e27908fef439071cc0873b4a45b64f41
kib <kib@FreeBSD.org>
2007-05-29 10:09:43 +0000

    MFC rev. 1.309 of sys/kern/kern_descrip.c,     rev. 1.438 of sys/kern/vfs_syscalls.c,     rev. 1.77 of sys/sys/filedesc.h: Mark the filedescriptor table entries with VOP_OPEN being performed for them as UF_OPENING. Disable closing of that entries. This should fix the crashes caused by devfs_open() (and fifo_open()) dereferencing struct file * by index, while the filedescriptor is closed by parallel thread.
    RELENG_6 testing by:	Mark Kane <mark at mkproductions org>

--- sys/kern/kern_descrip.c
+++ sys/kern/kern_descrip.c
@@ -666,7 +666,9 @@ do_dup(struct thread *td, enum dup_type type, int old, int new, register_t *retv
 	 * bad file descriptor.  Userland should do its own locking to
 	 * avoid this case.
 	 */
-	if (fdp->fd_ofiles[old] != fp) {
+	if (fdp->fd_ofiles[old] != fp ||
+	    (fdp->fd_ofileflags[old] & UF_OPENING) != 0 ||
+	    (fdp->fd_ofileflags[new] & UF_OPENING) != 0) {
 		/* we've allocated a descriptor which we won't use */
 		if (fdp->fd_ofiles[new] == NULL)
 			fdunused(fdp, new);
@@ -983,7 +985,8 @@ close(td, uap)
 
 	FILEDESC_LOCK(fdp);
 	if ((unsigned)fd >= fdp->fd_nfiles ||
-	    (fp = fdp->fd_ofiles[fd]) == NULL) {
+	    (fp = fdp->fd_ofiles[fd]) == NULL ||
+	    (fdp->fd_ofileflags[fd] & UF_OPENING) != 0) {
 		FILEDESC_UNLOCK(fdp);
 		return (EBADF);
 	}
@@ -1509,7 +1512,8 @@ fdcopy(struct filedesc *fdp)
 	newfdp->fd_freefile = -1;
 	for (i = 0; i <= fdp->fd_lastfile; ++i) {
 		if (fdisused(fdp, i) &&
-		    fdp->fd_ofiles[i]->f_type != DTYPE_KQUEUE) {
+		    fdp->fd_ofiles[i]->f_type != DTYPE_KQUEUE &&
+		    (fdp->fd_ofileflags[i] & UF_OPENING) == 0) {
 			newfdp->fd_ofiles[i] = fdp->fd_ofiles[i];
 			newfdp->fd_ofileflags[i] = fdp->fd_ofileflags[i];
 			fhold(newfdp->fd_ofiles[i]);
