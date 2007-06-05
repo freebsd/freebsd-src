84293163b1545222b42c9897264c6047f5939c07
dougb <dougb@FreeBSD.org>
2007-06-05 22:17:16 +0000

    Remove the special atomic.h case for arm, and allow it to use the platform specific file that imp provided.

--- lib/bind/config.mk
+++ lib/bind/config.mk
@@ -65,8 +65,6 @@ CFLAGS+=	-I${LIB_BIND_DIR}
 # Use the right version of the atomic.h file from lib/isc
 .if ${MACHINE_ARCH} == "amd64" || ${MACHINE_ARCH} == "i386"
 ISC_ATOMIC_ARCH=	x86_32
-.elif ${MACHINE_ARCH} == "arm"
-ISC_ATOMIC_ARCH=	noatomic
 .else
 ISC_ATOMIC_ARCH=	${MACHINE_ARCH}
 .endif
