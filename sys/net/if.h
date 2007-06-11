1294243b0904bb1392f338c03b0cc4fd9a775519
andre <andre@FreeBSD.org>
2007-06-11 20:08:11 +0000

    Add IFCAP_LRO flag for drivers to announce their TCP Large Receive Offload capabilities.

--- sys/net/if.h
+++ sys/net/if.h
@@ -208,6 +208,7 @@ struct if_data {
 #define	IFCAP_VLAN_HWCSUM	0x0080	/* can do IFCAP_HWCSUM on VLANs */
 #define	IFCAP_TSO4		0x0100	/* can do TCP Segmentation Offload */
 #define	IFCAP_TSO6		0x0200	/* can do TCP6 Segmentation Offload */
+#define	IFCAP_LRO		0x0400	/* can do Large Receive Offload */
 
 #define IFCAP_HWCSUM		(IFCAP_RXCSUM | IFCAP_TXCSUM)
 #define	IFCAP_TSO		(IFCAP_TSO4 | IFCAP_TSO6)
