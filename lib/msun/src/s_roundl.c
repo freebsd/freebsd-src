e5ce6c8ae28453d7263a89a8d42bd5ff45974fe2
bde <bde@FreeBSD.org>
2007-06-14 05:20:38 +0000

    MFC:
    s_round.c 1.4
    s_roundf.c 1.4
    s_roundl.c 1.2
    
    Make roundf() actually work, and attempt to fix round() and roundl()
    by cloning the fix for roundf().

--- lib/msun/src/s_roundl.c
+++ lib/msun/src/s_roundl.c
@@ -38,14 +38,14 @@ roundl(long double x)
 		return (x);
 
 	if (x >= 0.0) {
-		t = ceill(x);
-		if (t - x > 0.5)
-			t -= 1.0;
+		t = floorl(x);
+		if (t - x <= -0.5)
+			t += 1.0;
 		return (t);
 	} else {
-		t = ceill(-x);
-		if (t + x > 0.5)
-			t -= 1.0;
+		t = floorl(-x);
+		if (t + x <= -0.5)
+			t += 1.0;
 		return (-t);
 	}
 }
