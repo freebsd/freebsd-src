687b4ebc1a6ad4c42fd9c704edd2eeb36d700ba4
delphij <delphij@FreeBSD.org>
2007-06-14 07:13:28 +0000

    Require users to provide a length information for inet_ntoa_r, this is common on other platforms.
    Reported by:	pointyhat via kris

--- include/arpa/inet.h
+++ include/arpa/inet.h
@@ -148,7 +148,7 @@ uint16_t	 ntohs(uint16_t);
 
 in_addr_t	 inet_addr(const char *);
 /*const*/ char	*inet_ntoa(struct in_addr);
-char			*inet_ntoa_r(struct in_addr, char *buf);
+char		*inet_ntoa_r(struct in_addr, char *buf, socklen_t size);
 const char	*inet_ntop(int, const void * __restrict, char * __restrict,
 		    socklen_t);
 int		 inet_pton(int, const char * __restrict, void * __restrict);
