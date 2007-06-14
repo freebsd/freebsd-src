e90eaa0c29b822e1b9af8efd94b81097f552fd4d
harti <harti@FreeBSD.org>
2007-06-14 20:11:48 +0000

    Make the order of tests the same for user and kernel. Use the natural layer order from bottom to top.

--- tools/regression/atm/RunTest.sh
+++ tools/regression/atm/RunTest.sh
@@ -39,7 +39,7 @@ done
 (cd proto_uni ; sh ./RunTest.sh -u $options)
 (cd proto_cc ; sh ./RunTest.sh -u $options)
 
-(cd proto_uni ; sh ./RunTest.sh $options)
 (cd proto_sscop ; sh ./RunTest.sh $options)
 (cd proto_sscfu ; sh ./RunTest.sh $options)
+(cd proto_uni ; sh ./RunTest.sh $options)
 (cd proto_cc ; sh ./RunTest.sh $options)
