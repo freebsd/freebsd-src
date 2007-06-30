02580fced94f3315aa8a6c757f3b55f37a320cea
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-06-30 14:44:09 +0000

    Quirk for ChipsBnk usb stick
    Submitted by: Mark Andrews
    PR: 103702
    Approved by: re@ (blanket)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@171116 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/cam/scsi/scsi_da.c
+++ sys/cam/scsi/scsi_da.c
@@ -527,6 +527,14 @@ static struct da_quirk_entry da_quirk_table[] =
 		{T_DIRECT, SIP_MEDIA_REMOVABLE, "USB*", "Flash Disk*",
 		 "*"}, /*quirks*/ DA_Q_NO_SYNC_CACHE
 	},
+	{
+		/*
+		 * ChipsBnk usb stick
+		 * PR: 103702
+		 */
+		{T_DIRECT, SIP_MEDIA_REMOVABLE, "ChipsBnk", "USB*",
+		 "*"}, /*quirks*/ DA_Q_NO_SYNC_CACHE
+	},
 };
 
 static	disk_strategy_t	dastrategy;
