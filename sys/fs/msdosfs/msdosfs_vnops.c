0a740782a717055e03d9979cea329b19f668fa12
rodrigc <rodrigc@FreeBSD.org>
2007-07-08 15:30:28 +0000

    MFC: Convert MSDOSFS_LARGE compile time option to a "-o large" mount option.
    PR:	105964

--- sys/fs/msdosfs/msdosfs_vnops.c
+++ sys/fs/msdosfs/msdosfs_vnops.c
@@ -78,8 +78,6 @@
 #include <fs/msdosfs/denode.h>
 #include <fs/msdosfs/fat.h>
 
-#include "opt_msdosfs.h"
-
 #define	DOS_FILESIZE_MAX	0xffffffff
 
 /*
@@ -324,11 +322,12 @@ msdosfs_getattr(ap)
 			fileid = (uint64_t)roottobn(pmp, 0) * dirsperblk;
 		fileid += (uint64_t)dep->de_diroffset / sizeof(struct direntry);
 	}
-#ifdef MSDOSFS_LARGE
-	vap->va_fileid = msdosfs_fileno_map(pmp->pm_mountp, fileid);
-#else
-	vap->va_fileid = (long)fileid;
-#endif
+
+	if (pmp->pm_flags & MSDOSFS_LARGEFS)
+		vap->va_fileid = msdosfs_fileno_map(pmp->pm_mountp, fileid);
+	else
+		vap->va_fileid = (long)fileid;
+
 	if ((dep->de_Attributes & ATTR_READONLY) == 0)
 		mode = S_IRWXU|S_IRWXG|S_IRWXO;
 	else
@@ -1556,12 +1555,14 @@ msdosfs_readdir(ap)
 							  * dirsperblk;
 				else
 					fileno = 1;
-#ifdef MSDOSFS_LARGE
-				dirbuf.d_fileno = msdosfs_fileno_map(
-				    pmp->pm_mountp, fileno);
-#else
-				dirbuf.d_fileno = (uint32_t)fileno;
-#endif
+				if (pmp->pm_flags & MSDOSFS_LARGEFS) {
+					dirbuf.d_fileno =
+					    msdosfs_fileno_map(pmp->pm_mountp,
+					    fileno);
+				} else {
+
+					dirbuf.d_fileno = (uint32_t)fileno;
+				}
 				dirbuf.d_type = DT_DIR;
 				switch (n) {
 				case 0:
@@ -1685,12 +1686,12 @@ msdosfs_readdir(ap)
 				fileno = (uint64_t)offset / sizeof(struct direntry);
 				dirbuf.d_type = DT_REG;
 			}
-#ifdef MSDOSFS_LARGE
-			dirbuf.d_fileno = msdosfs_fileno_map(pmp->pm_mountp,
-			    fileno);
-#else
-			dirbuf.d_fileno = (uint32_t)fileno;
-#endif
+			if (pmp->pm_flags & MSDOSFS_LARGEFS) {
+				dirbuf.d_fileno =
+				    msdosfs_fileno_map(pmp->pm_mountp, fileno);
+			} else
+				dirbuf.d_fileno = (uint32_t)fileno;
+
 			if (chksum != winChksum(dentp->deName)) {
 				dirbuf.d_namlen = dos2unixfn(dentp->deName,
 				    (u_char *)dirbuf.d_name,
