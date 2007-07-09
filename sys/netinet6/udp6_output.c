d2da721391380976ea82817cd24ff73c0843b716
rwatson <rwatson@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-07-09 17:47:04 +0000

    General style, white space, and comment cleanup; move to ANSI C prototypes, don't use register, etc.  Synchronize structure and layout to the IPv4 versions of these functions to a greater extent, making visual comparison easier.
    Remove now stale or incorrect comments.
    
    Enable full lock assertions, and correct one exception handling
    case where the wrong label was jumped to.
    
    Tested by:	bz
    Approved by:	re (bmah)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@171328 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet6/udp6_output.c
+++ sys/netinet6/udp6_output.c
@@ -32,7 +32,8 @@
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
- *	The Regents of the University of California.  All rights reserved.
+ *	The Regents of the University of California.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
