17f7e17e2d95fd7b5b234667c180d7128f889400
kientzle <kientzle@FreeBSD.org>
2007-08-07 05:10:18 +0000

    MFC include guards in bsdtar.c.  Also, add HAVE_TIME_H to bsdtar_platform.h so they actually work correctly.

--- usr.bin/tar/bsdtar.c
+++ usr.bin/tar/bsdtar.c
@@ -26,10 +26,18 @@
 #include "bsdtar_platform.h"
 __FBSDID("$FreeBSD$");
 
+#ifdef HAVE_SYS_PARAM_H
 #include <sys/param.h>
+#endif
+#ifdef HAVE_SYS_STAT_H
 #include <sys/stat.h>
+#endif
+#ifdef HAVE_ERRNO_H
 #include <errno.h>
+#endif
+#ifdef HAVE_FCNTL_H
 #include <fcntl.h>
+#endif
 #ifdef HAVE_GETOPT_LONG
 #include <getopt.h>
 #else
@@ -45,15 +53,25 @@ struct option {
 #ifdef HAVE_LANGINFO_H
 #include <langinfo.h>
 #endif
+#ifdef HAVE_LOCALE_H
 #include <locale.h>
+#endif
 #ifdef HAVE_PATHS_H
 #include <paths.h>
 #endif
 #include <stdio.h>
+#ifdef HAVE_STDLIB_H
 #include <stdlib.h>
+#endif
+#ifdef HAVE_STRING_H
 #include <string.h>
+#endif
+#ifdef HAVE_TIME_H
 #include <time.h>
+#endif
+#ifdef HAVE_UNISTD_H
 #include <unistd.h>
+#endif
 #if HAVE_ZLIB_H
 #include <zlib.h>
 #endif
--- usr.bin/tar/bsdtar_platform.h
+++ usr.bin/tar/bsdtar_platform.h
@@ -93,6 +93,7 @@
 #define	HAVE_SYS_PARAM_H 1
 #define	HAVE_SYS_STAT_H 1
 #define	HAVE_SYS_TYPES_H 1
+#define	HAVE_TIME_H 1
 #define	HAVE_UINTMAX_T 1
 #define	HAVE_UNISTD_H 1
 #define	HAVE_VPRINTF 1
