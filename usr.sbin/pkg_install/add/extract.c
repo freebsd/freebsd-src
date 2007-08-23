f63f445829bd00698bb0108f33d0003ca56edd06
krion <krion@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-08-23 13:05:10 +0000

    Fix pkg_add behaviour to preserve pathnames.
    PR:		bin/93915 bin/109134
    Submitted by:	Jason Heiss <heissj@yahoo-inc.com>, Constantin Stefanov<cstef@mail.ru>
    Approved by:	re@ (kensmith)
    MFC after:	7 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@171934 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/pkg_install/add/extract.c
+++ usr.sbin/pkg_install/add/extract.c
@@ -34,7 +34,7 @@ __FBSDID("$FreeBSD$");
 
 #define PUSHOUT(todir) /* push out string */ \
     if (where_count > (int)sizeof(STARTSTRING)-1) { \
-	strcat(where_args, "|/usr/bin/tar --unlink -xpf - -C "); \
+	strcat(where_args, "|/usr/bin/tar --unlink -xpPf - -C "); \
 	strcat(where_args, todir); \
 	if (system(where_args)) { \
 	    cleanup(0); \
