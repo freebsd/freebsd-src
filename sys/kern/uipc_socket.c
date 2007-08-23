8220153c6f461e2084d44e7904da956d1e5a0f17
JINMEI Tatuya <jinmei@FreeBSD.org>
2007-08-23 18:17:08 +0000

    MFC:   Fix a kernel panic based on receiving an ICMPv6 Packet too Big message.   (MFC was planned but has been missed)
    PR:		99779
    Submitted by:	Jinmei Tatuya
    Reviewed by:	clement, rwatson
    Approved by:	gnn (mentor)
    
    src/sys/kern/uipc_socket.c:	1.280

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -1169,10 +1169,10 @@ dontblock:
 			}
 			cm = cmn;
 		}
-		if (so->so_rcv.sb_mb)
+		if (m != NULL)
 			nextrecord = so->so_rcv.sb_mb->m_nextpkt;
 		else
-			nextrecord = NULL;
+			nextrecord = so->so_rcv.sb_mb;
 		orig_resid = 0;
 	}
 	if (m != NULL) {
