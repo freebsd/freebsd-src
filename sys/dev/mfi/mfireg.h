dc46a6c1d180dbb355de7d4b74e8df373ad305d8
scottl <scottl@FreeBSD.org>
2007-08-26 02:36:31 +0000

    MFC LSI 1078/PERC6 update.
    Submitted by: LSI

--- sys/dev/mfi/mfireg.h
+++ sys/dev/mfi/mfireg.h
@@ -23,6 +23,32 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+/*-
+ * Copyright (c) 2007 LSI Corp.
+ * Copyright (c) 2007 Rajesh Prabhakaran.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 
 #ifndef _MFIREG_H
 #define _MFIREG_H
@@ -55,6 +81,17 @@ __FBSDID("$FreeBSD$");
 #define MFI_OMSK	0x34	/* Outbound interrupt mask */
 #define MFI_IQP		0x40	/* Inbound queue port */
 #define MFI_OQP		0x44	/* Outbound queue port */
+
+/*
+ * 1078 specific related register
+ */
+#define MFI_ODR0	0x9c 		/* outbound doorbell register0 */
+#define MFI_ODCR0	0xa0 		/* outbound doorbell clear register0  */
+#define MFI_OSP0	0xb0 		/* outbound scratch pad0  */
+#define MFI_1078_EIM	0x80000004 	/* 1078 enable intrrupt mask  */
+#define MFI_RMI		0x2 		/* reply message interrupt  */       
+#define MFI_1078_RM	0x80000000 	/* reply 1078 message interrupt  */
+#define MFI_ODC		0x4 		/* outbound doorbell change interrupt */
 
 /* Bits for MFI_OSTS */
 #define MFI_OSTS_INTR_VALID	0x00000002
