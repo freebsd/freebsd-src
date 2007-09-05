527eef962e3440e0484314868471050b85af5543
sam <sam@FreeBSD.org>
2007-09-05 23:00:27 +0000

    Add missing bits that made bg scanning lame: o update ic_lastdata to reflect time of last outbound frame o outbound traffic must preempt/cancel bg scanning to avoid delays
    This stuff was somehow missed in the initial import.
    
    Reviewed by:	thompsa, avatar, sephe (earlier version)
    Approved by:	re (blanket wireless)

--- sys/dev/usb/if_ural.c
+++ sys/dev/usb/if_ural.c
@@ -1472,6 +1472,11 @@ ural_start(struct ifnet *ifp)
 				ifp->if_drv_flags |= IFF_DRV_OACTIVE;
 				break;
 			}
+			/*
+			 * Cancel any background scan.
+			 */
+			if (ic->ic_flags & IEEE80211_F_SCAN)
+				ieee80211_cancel_scan(ic);
 
 			if (m0->m_len < sizeof (struct ether_header) &&
 			    !(m0 = m_pullup(m0, sizeof (struct ether_header))))
@@ -1502,6 +1507,7 @@ ural_start(struct ifnet *ifp)
 		}
 
 		sc->sc_tx_timer = 5;
+		ic->ic_lastdata = ticks;
 		callout_reset(&sc->watchdog_ch, hz, ural_watchdog, sc);
 	}
 }
