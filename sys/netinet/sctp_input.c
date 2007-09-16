44d85d753b560261dfa90581a0e631b16bbc0021
rrs <rrs@FreeBSD.org>
2007-09-16 23:03:38 +0000

    - For the 1-to-1 model, fix an off by one error that   allowed an extra connection over the backlog (by one) Approved by:	re@freebsd.org (B. Mah)

--- sys/netinet/sctp_input.c
+++ sys/netinet/sctp_input.c
@@ -4381,7 +4381,7 @@ process_control_chunks:
 				return (NULL);
 			} else if (inp->sctp_socket->so_qlimit) {
 				/* we are accepting so check limits like TCP */
-				if (inp->sctp_socket->so_qlen >
+				if (inp->sctp_socket->so_qlen >=
 				    inp->sctp_socket->so_qlimit) {
 					/* no space */
 					struct mbuf *oper;
