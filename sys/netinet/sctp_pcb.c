a57e84c93157fb9c8d64c3736bb8fc72a1dcfc6f
rrs <rrs@FreeBSD.org>
2007-10-06 13:23:42 +0000

    - Fix the one-2-one model to properly do a socantrecv() Approved by:	re@freeBSD.org (Ken Smith)

--- sys/netinet/sctp_pcb.c
+++ sys/netinet/sctp_pcb.c
@@ -4235,6 +4235,7 @@ sctp_free_assoc(struct sctp_inpcb *inp, struct sctp_tcb *stcb, int from_inpcbfre
 					    SS_ISCONNECTED);
 				}
 				SOCK_UNLOCK(so);
+				socantrcvmore(so);
 				sctp_sowwakeup(inp, so);
 				sctp_sorwakeup(inp, so);
 				SCTP_SOWAKEUP(so);
