b9e179eb3ffba877e12b00db004e994b873595fc
dougb <dougb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-10-09 07:30:14 +0000

    Deprecate use of the early.sh script as advertised when the support for local rc.d scripts in the overall boot order was added.
    Proper rc.d scripts are run by rc.subr in a subshell, whereas scripts that
    end in .sh are sourced into rc's shell. The latter has potential to create
    serious boot problems, and there is no reason that the same functionality
    cannot be added by the user in the form of a proper rc.d script (as
    opposed to being added by the user in the form of /etc/rc.early).
    
    This script will be removed prior to the 8.0 branch.
    
    Approved by:	re (kensmith)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@172488 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- etc/rc.d/early.sh
+++ etc/rc.d/early.sh
@@ -11,5 +11,8 @@
 # Support for legacy /etc/rc.early script
 #
 if [ -r /etc/rc.early ]; then
+	warn 'Use of the early.sh script is deprecated'
+	warn 'Please use a new-style rc.d script instead'
+	warn 'See rc(8) for more information'
 	. /etc/rc.early
 fi
