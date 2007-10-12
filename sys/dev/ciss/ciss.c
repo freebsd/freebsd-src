805629b231d7136ec7caaa7fe059535c9748f140
scottl <scottl@FreeBSD.org>
2007-10-12 17:03:41 +0000

    Fix a mistake made in the MPSAFE commit that caused CAM to serialize requests to the controller.

--- sys/dev/ciss/ciss.c
+++ sys/dev/ciss/ciss.c
@@ -2491,7 +2491,8 @@ ciss_cam_init(struct ciss_softc *sc)
 	if ((sc->ciss_cam_sim[i] = cam_sim_alloc(ciss_cam_action, ciss_cam_poll,
 						 "ciss", sc,
 						 device_get_unit(sc->ciss_dev),
-						 &sc->ciss_mtx, 1,
+						 &sc->ciss_mtx,
+						 sc->ciss_max_requests - 2,
 						 sc->ciss_max_requests - 2,
 						 sc->ciss_cam_devq)) == NULL) {
 	    ciss_printf(sc, "can't allocate CAM SIM for controller %d\n", i);
