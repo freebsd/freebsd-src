fea6759b60a7ea272749ff53b154fa4d14c8123c
ache <ache@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-10-15 09:51:30 +0000

    Add comment explaining __mb_sb_limit trick here.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@172661 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/locale/utf8.c
+++ lib/libc/locale/utf8.c
@@ -65,6 +65,11 @@ _UTF8_init(_RuneLocale *rl)
 	__wcsnrtombs = _UTF8_wcsnrtombs;
 	_CurrentRuneLocale = rl;
 	__mb_cur_max = 6;
+	/*
+	 * UCS-4 encoding used as the internal representation, so
+	 * slots 0x0080-0x00FF are occuped and must be excluded
+	 * from the single byte ctype by setting the limit.
+	 */
 	__mb_sb_limit = 128;
 
 	return (0);
