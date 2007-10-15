e42b1cae2da41e190d53d244ceeadb19458925f7
se <se@FreeBSD.org>
2007-10-15 22:09:19 +0000

    Make the updatedb script installed as /usr/libexec/locate.updatedb inspect all local file systems, not only ufs and ext2fs. A number of local file systems has been added over time, and at least zfs has the potential to become a popular choice. Without this change a ZFS root file system causes the script to ignore all file-systems and leads to an empty locate db. (An alternative is to add all the relevant file systems individually, which means that at least zfs, xfs, ntfs, ntfs-3g, msdosfs should be added, probably more).

--- usr.bin/locate/locate/updatedb.sh
+++ usr.bin/locate/locate/updatedb.sh
@@ -52,7 +52,7 @@ PATH=$LIBEXECDIR:/bin:/usr/bin:$PATH; export PATH
 : ${FCODES:=/var/db/locate.database}	 # the database
 : ${SEARCHPATHS:="/"}		# directories to be put in the database
 : ${PRUNEPATHS:="/tmp /usr/tmp /var/tmp /var/db/portsnap"} # unwanted directories
-: ${FILESYSTEMS:="ufs ext2fs"}			 # allowed filesystems 
+: ${FILESYSTEMS:="local"}		 # allowed filesystems 
 : ${find:=find}
 
 case X"$SEARCHPATHS" in 
