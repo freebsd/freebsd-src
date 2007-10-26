1dae59cff6b908f0465d9b70c38b8af202797748
imp <imp@FreeBSD.org>
2007-10-26 15:00:34 +0000

    The arm boot code uses this function as well.  Redefining cgbase() saves about 500 bytes in the boot code.  While the AT91RM9200 has 12k of space for the boot loader, which is more than i386's 8k, the code generated by gcc is a bit bigger.
    I've had this in p4 for about two years now.

--- sys/boot/common/ufsread.c
+++ sys/boot/common/ufsread.c
@@ -48,7 +48,7 @@ __FBSDID("$FreeBSD$");
 
 #include <ufs/ufs/dinode.h>
 #include <ufs/ffs/fs.h>
-#if defined(__i386__) && !defined(GPTBOOT)
+#if (defined(__i386__) && !defined(GPTBOOT)) || defined(__arm__)
 /* XXX: Revert to old (broken for over 1.5Tb filesystems) version of cgbase
    (see sys/ufs/ffs/fs.h rev 1.39) so that i386 boot loader (boot2) can
    support both UFS1 and UFS2 again. */
