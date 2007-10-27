d2169cb67d078e8cefea38c7787f9a2b351176f1
Craig Rodrigues <rodrigc@FreeBSD.org>
2007-10-27 16:14:33 +0000

    Remove duplicate "union" from ext2_opts.
    Noticed by:	bde

--- sys/gnu/fs/ext2fs/ext2_vfsops.c
+++ sys/gnu/fs/ext2fs/ext2_vfsops.c
@@ -117,7 +117,7 @@ static int	ext2_check_sb_compat(struct ext2_super_block *es, struct cdev *dev,
 static int	compute_sb_data(struct vnode * devvp,
 		    struct ext2_super_block * es, struct ext2_sb_info * fs);
 
-static const char *ext2_opts[] = { "from", "export", "union", "acls", "exec",
+static const char *ext2_opts[] = { "from", "export", "acls", "exec",
     "noatime", "union", "suiddir", "multilabel", "nosymfollow",
     "noclusterr", "noclusterw", "force", NULL };
  
