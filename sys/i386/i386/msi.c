9a55b414b2e52b627b4d67306fe89e8b206ba3f6
jhb <jhb@FreeBSD.org>
2007-10-30 18:00:56 +0000

    MFC: Update copyright attribution.
    Approved by:	re (kensmith)

--- sys/i386/i386/msi.c
+++ sys/i386/i386/msi.c
@@ -1,6 +1,7 @@
 /*-
- * Copyright (c) 2006 John Baldwin <jhb@FreeBSD.org>
+ * Copyright (c) 2006 Yahoo!, Inc.
  * All rights reserved.
+ * Written by: John Baldwin <jhb@FreeBSD.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
