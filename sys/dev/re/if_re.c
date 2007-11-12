3b9982e59c6d1d74ccfb447b846f01a44deb8ae6
Remko Lodder <remko@FreeBSD.org>
2007-11-12 15:44:00 +0000

    Add support for D-Link DGE-528(T) Rev.B1
    PR:		112774
    Submitted by:	Denis Fortin <fortin at acm dot org>
    Approved by:	imp (mentor), yongari
    MFC After:	3 days

--- sys/dev/re/if_re.c
+++ sys/dev/re/if_re.c
@@ -172,6 +172,8 @@ TUNABLE_INT("hw.re.msi_disable", &msi_disable);
 static struct rl_type re_devs[] = {
 	{ DLINK_VENDORID, DLINK_DEVICEID_528T, RL_HWREV_8169S,
 		"D-Link DGE-528(T) Gigabit Ethernet Adapter" },
+	{ DLINK_VENDORID, DLINK_DEVICEID_528T, RL_HWREV_8169_8110SB,
+		"D-Link DGE-528(T) Rev.B1 Gigabit Ethernet Adapter" },
 	{ RT_VENDORID, RT_DEVICEID_8139, RL_HWREV_8139CPLUS,
 		"RealTek 8139C+ 10/100BaseTX" },
 	{ RT_VENDORID, RT_DEVICEID_8101E, RL_HWREV_8101E,
