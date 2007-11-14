81d686fc69f1221afbf570ab4fb26fc88ddfa3e5
ru <ru@FreeBSD.org>
2007-11-14 19:18:40 +0000

    MFC: 1.32: Fix numerous bugs when parsing options and arguments.
    Approved by:	re (kensmith)

--- usr.bin/killall/killall.c
+++ usr.bin/killall/killall.c
@@ -154,11 +154,12 @@ main(int ac, char **av)
 			switch (**av) {
 			case 'j':
 				++*av;
-				if (**av == '\0')
+				if (**av == '\0') {
 					++av;
-				--ac;
+					--ac;
+				}
 				jflag++;
-				if (!*av)
+				if (*av == NULL)
 				    	errx(1, "must specify jid");
 				jid = strtol(*av, &ep, 10);
 				if (!*av || *ep)
@@ -168,23 +169,32 @@ main(int ac, char **av)
 				break;
 			case 'u':
 				++*av;
-				if (**av == '\0')
+				if (**av == '\0') {
 					++av;
-				--ac;
+					--ac;
+				}
+				if (*av == NULL)
+				    	errx(1, "must specify user");
 				user = *av;
 				break;
 			case 't':
 				++*av;
-				if (**av == '\0')
+				if (**av == '\0') {
 					++av;
-				--ac;
+					--ac;
+				}
+				if (*av == NULL)
+				    	errx(1, "must specify tty");
 				tty = *av;
 				break;
 			case 'c':
 				++*av;
-				if (**av == '\0')
+				if (**av == '\0') {
 					++av;
-				--ac;
+					--ac;
+				}
+				if (*av == NULL)
+				    	errx(1, "must specify procname");
 				cmd = *av;
 				break;
 			case 'v':
