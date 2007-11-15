f6ef33acb6555b41b9981e120bb5fa1a7f437c65
remko <remko@FreeBSD.org>
2007-11-15 07:10:04 +0000

    MFC rev 1.96 if_re.c
      Add support for D-Link DGE-528(T) Rev.B1
    
      PR:		112774
      Submitted by:	Denis Fortin <fortin at acm dot org>
      Approved by:	imp (mentor), yongari
      MFC After:	3 days
    
    Approved by:	imp (mentor, implicit)

--- sys/dev/re/if_re.c
+++ sys/dev/re/if_re.c
@@ -168,6 +168,8 @@ MODULE_DEPEND(re, miibus, 1, 1, 1);
 static struct rl_type re_devs[] = {
 	{ DLINK_VENDORID, DLINK_DEVICEID_528T, RL_HWREV_8169S,
 		"D-Link DGE-528(T) Gigabit Ethernet Adapter" },
+	{ DLINK_VENDORID, DLINK_DEVICEID_528T, RL_HWREV_8169_8110SB,
+		"D-Link DGE-528(T) Rev.B1 Gigabit Ethernet Adapter" },
 	{ RT_VENDORID, RT_DEVICEID_8139, RL_HWREV_8139CPLUS,
 		"RealTek 8139C+ 10/100BaseTX" },
 	{ RT_VENDORID, RT_DEVICEID_8101E, RL_HWREV_8101E,
