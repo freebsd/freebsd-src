242ef17d6d636c228a0fce16e6df2a2a967ac64d
John Birrell <jb@FreeBSD.org>
2007-11-17 23:09:39 +0000

    Use printf formats which match the variable types without casts so we can go back to using -Werror in higher WARNS levels.

--- usr.sbin/IPXrouted/trace.c
+++ usr.sbin/IPXrouted/trace.c
@@ -363,8 +363,8 @@ dumppacket(fd, dir, source, cp, size)
 		fprintf(fd, "Bad cmd 0x%x %s %s#%x\n", ntohs(msg->rip_cmd),
 		    dir, ipxdp_ntoa(&who->sipx_addr), 
 		    ntohs(who->sipx_addr.x_port));
-		fprintf(fd, "size=%d cp=%lx packet=%x\n", size, 
-			(u_long)cp, (u_int)packet);
+		fprintf(fd, "size=%d cp=%p packet=%p\n", size, 
+			cp, packet);
 		return;
 	}
 	switch (ntohs(msg->rip_cmd)) {
@@ -407,8 +407,8 @@ dumpsappacket(fd, dir, source, cp, size)
 		fprintf(fd, "Bad cmd 0x%x %s %s#%x\n", ntohs(msg->sap_cmd),
 		    dir, ipxdp_ntoa(&who->sipx_addr), 
 		    ntohs(who->sipx_addr.x_port));
-		fprintf(fd, "size=%d cp=%lx packet=%x\n", size, 
-			(u_long)cp, (u_int)packet);
+		fprintf(fd, "size=%d cp=%p packet=%p\n", size, 
+			cp, packet);
 		return;
 	}
 	switch (ntohs(msg->sap_cmd)) {
