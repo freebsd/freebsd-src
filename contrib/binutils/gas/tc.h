d136af115468ff2e37d3e42fa775829540926201
Max Khon <fjoe@FreeBSD.org>
2007-11-24 06:53:09 +0000

    MFC:
    Import two binutils header files from FSF 2.15 branch.
    These fix binutils compilation on i386/amd64 with GCC 4.1 and
    have no other effect.
    
    This fixes RELENG_5 build on RELENG_7/HEAD.

--- contrib/binutils/gas/tc.h
+++ contrib/binutils/gas/tc.h
@@ -24,25 +24,6 @@
 
 extern const pseudo_typeS md_pseudo_table[];
 
-/* JF moved this here from as.h under the theory that nobody except MACHINE.c
-   and write.c care about it anyway.  */
-
-struct relax_type
-{
-  /* Forward reach. Signed number. > 0.  */
-  long rlx_forward;
-  /* Backward reach. Signed number. < 0.  */
-  long rlx_backward;
-
-  /* Bytes length of this address.  */
-  unsigned char rlx_length;
-
-  /* Next longer relax-state.  0 means there is no 'next' relax-state.  */
-  relax_substateT rlx_more;
-};
-
-typedef struct relax_type relax_typeS;
-
 extern const int md_reloc_size;	/* Size of a relocation record */
 
 char *md_atof (int what_statement_type, char *literalP, int *sizeP);
