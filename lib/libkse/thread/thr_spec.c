30ca9d872782250e1ef47d962b4523f576b17cf4
David Xu <davidxu@FreeBSD.org>
2007-11-26 02:44:05 +0000

    MFC revision 1.27: 	In _pthread_key_create() ensure that libkse is initialized.
    Approved by: re (kensmith)

--- lib/libkse/thread/thr_spec.c
+++ lib/libkse/thread/thr_spec.c
@@ -63,8 +63,12 @@ __weak_reference(_pthread_setspecific, pthread_setspecific);
 int
 _pthread_key_create(pthread_key_t *key, void (*destructor) (void *))
 {
-	struct pthread *curthread = _get_curthread();
+	struct pthread *curthread;
 	int i;
+
+	if (_thr_initial == NULL)
+		_libpthread_init(NULL);
+	curthread = _get_curthread();
 
 	/* Lock the key table: */
 	THR_LOCK_ACQUIRE(curthread, &_keytable_lock);
