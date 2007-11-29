7d396c5f9cab1f0aa7d16ea25fb06cd59b3df775
John Baldwin <jhb@FreeBSD.org>
2007-11-29 18:36:09 +0000

    MFC: Correct a gtar directory traversal vulnerability.

--- contrib/tar/src/misc.c
+++ contrib/tar/src/misc.c
@@ -210,12 +210,11 @@ contains_dot_dot (char const *name)
       if (p[0] == '.' && p[1] == '.' && (ISSLASH (p[2]) || !p[2]))
 	return 1;
 
-      do
+      while (! ISSLASH (*p))
 	{
 	  if (! *p++)
 	    return 0;
 	}
-      while (! ISSLASH (*p));
 
       do
 	{
