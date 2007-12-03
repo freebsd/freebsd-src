ea8e43af16d58fc503074731df866e8b9c6c48b4
dougb <dougb@FreeBSD.org>
2007-12-03 08:26:34 +0000

    Update this file so that BIND on ARM can actually work. I quote:
    	The problem was, isc_atomic_cmpxchg() is almost like our
    	atomic_cmpset_32(), except it expects the old value to be
    	returned, whereas our atomic_cmpset_32 returns 1 on success,
    	or 0 on failure. So I re-implemented something suitable.
    
    Submitted by:	cognet
    Reviewed by:	bsdimp

--- contrib/bind9/lib/isc/arm/include/isc/atomic.h
+++ contrib/bind9/lib/isc/arm/include/isc/atomic.h
@@ -49,7 +49,29 @@ isc_atomic_store(isc_int32_t *p, isc_int32_t val)
 static inline isc_int32_t
 isc_atomic_cmpxchg(isc_int32_t *p, isc_int32_t cmpval, isc_int32_t val)
 {
-	return atomic_cmpset_int(p, cmpval, val);
+	register int done, ras_start;
+
+	__asm __volatile("1:\n"
+	    "adr	%1, 1b\n"
+	    "mov	%0, #0xe0000004\n"
+	    "str	%1, [%0]\n"
+	    "mov	%0, #0xe0000008\n"
+	    "adr	%1, 2f\n"
+	    "str	%1, [%0]\n"
+	    "ldr	%1, [%2]\n"
+	    "cmp	%1, %3\n"
+	    "streq	%4, [%2]\n"
+	    "2:\n"
+	    "mov	%3, #0\n"
+	    "mov	%0, #0xe0000004\n"
+	    "str	%3, [%0]\n"
+	    "mov	%3, #0xffffffff\n"
+	    "mov	%0, #0xe0000008\n"
+	    "str	%3, [%0]\n"
+	    : "=r" (ras_start), "=r" (done)
+	    ,"+r" (p), "+r" (cmpval), "+r" (val) : : "memory");
+	return (done);
+
 }
 #else /* !FreeBSD */
 
