d5b4af487e923e2a13932535dea021c1bff21683
gnn <gnn@FreeBSD.org>
2007-12-07 08:45:29 +0000

    MFC support for the Camellia block cipher
    HEAD
    Revision  Path
      1.16    src/lib/libipsec/pfkey_dump.c
      1.38    src/sbin/setkey/setkey.8
      1.12    src/sbin/setkey/token.l
      1.81    src/secure/lib/libcrypto/Makefile
      1.47    src/secure/lib/libcrypto/Makefile.inc
      1.8     src/secure/lib/libcrypto/opensslconf-amd64.h
      1.8     src/secure/lib/libcrypto/opensslconf-i386.h
      1.1205  src/sys/conf/files
      1.1     src/sys/crypto/camellia/camellia-api.c (new)
      1.1     src/sys/crypto/camellia/camellia.c (new)
      1.1     src/sys/crypto/camellia/camellia.h (new)
      1.6     src/sys/modules/crypto/Makefile
      1.15    src/sys/net/pfkeyv2.h
      1.1     src/sys/netinet6/esp_camellia.c (new)
      1.1     src/sys/netinet6/esp_camellia.h (new)
      1.25    src/sys/netinet6/esp_core.c
      1.19    src/sys/netipsec/xform_esp.c
      1.34    src/sys/opencrypto/cryptodev.c
      1.25    src/sys/opencrypto/cryptodev.h
      1.19    src/sys/opencrypto/cryptosoft.c
      1.9     src/sys/opencrypto/xform.c
      1.4     src/sys/opencrypto/xform.h
    
    Approved by: re (kensmith)
    Submitted by: Tomoyuki Okazaki <okazaki at kick dot gr dot jp>

--- sys/netipsec/xform_esp.c
+++ sys/netipsec/xform_esp.c
@@ -113,6 +113,8 @@ esp_algorithm_lookup(int alg)
 		return &enc_xform_skipjack;
 	case SADB_EALG_NULL:
 		return &enc_xform_null;
+	case SADB_X_EALG_CAMELLIACBC:
+		return &enc_xform_camellia;
 	}
 	return NULL;
 }
@@ -981,6 +983,7 @@ esp_attach(void)
 	MAXIV(enc_xform_cast5);		/* SADB_X_EALG_CAST128CBC */
 	MAXIV(enc_xform_skipjack);	/* SADB_X_EALG_SKIPJACK */
 	MAXIV(enc_xform_null);		/* SADB_EALG_NULL */
+	MAXIV(enc_xform_camellia);	/* SADB_EALG_CAMELLIACBC */
 
 	xform_register(&esp_xformsw);
 #undef MAXIV
