90d9d97245a6d34caccf262cb0bcce8fd740bdf9
peter <peter@FreeBSD.org>
2007-12-18 03:39:17 +0000

    MFC: scale TCPTV_MIN with hz, not a fixed number of ticks.  Otherwise machines with hz=1000 drop connections in a matter of a few seconds on a fast link (eg: ethernet).  With this change, it drops them in the same time as a machine with hz=100 would.

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -321,6 +321,8 @@ tcp_init()
 	tcp_maxpersistidle = TCPTV_KEEP_IDLE;
 	tcp_msl = TCPTV_MSL;
 	tcp_rexmit_min = TCPTV_MIN;
+	if (tcp_rexmit_min < 1)
+		tcp_rexmit_min = 1;
 	tcp_rexmit_slop = TCPTV_CPU_VAR;
 	tcp_inflight_rttthresh = TCPTV_INFLIGHT_RTTTHRESH;
 
