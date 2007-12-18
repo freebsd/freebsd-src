78cc241915df390b20b66a88b3a89a8dd20b0904
das <das@FreeBSD.org>
2007-12-18 23:46:32 +0000

    Since nan() is supposed to work the same as strtod("nan(...)", NULL), my original implementation made both use the same code. Unfortunately, this meant libm depended on a vendor header at compile time and previously- unexposed vendor bits in libc at runtime.
    Hence, I just wrote my own version of the relevant vendor routine. As it
    turns out, mine has a factor of 8 fewer of lines of code, and is a bit more
    readable anyway. The strtod() and *scanf() routines still use vendor code.
    
    Reviewed by:	bde

--- lib/msun/src/s_nan.c
+++ lib/msun/src/s_nan.c
@@ -26,39 +26,83 @@
  * $FreeBSD$
  */
 
+#include <sys/endian.h>
+#include <ctype.h>
 #include <float.h>
 #include <math.h>
+#include <stdint.h>
+#include <strings.h>
 
-#include "../../../contrib/gdtoa/gdtoaimp.h"
 #include "math_private.h"
+
+/*
+ * Scan a string of hexadecimal digits (the format nan(3) expects) and
+ * make a bit array (using the local endianness). We stop when we
+ * encounter an invalid character, NUL, etc.  If we overflow, we do
+ * the same as gcc's __builtin_nan(), namely, discard the high order bits.
+ *
+ * The format this routine accepts needs to be compatible with what is used
+ * in contrib/gdtoa/hexnan.c (for strtod/scanf) and what is used in
+ * __builtin_nan(). In fact, we're only 100% compatible for strings we
+ * consider valid, so we might be violating the C standard. But it's
+ * impossible to use nan(3) portably anyway, so this seems good enough.
+ */
+void
+_scan_nan(uint32_t *words, int num_words, const char *s)
+{
+	int si;		/* index into s */
+	int bitpos;	/* index into words (in bits) */
+
+	bzero(words, num_words * sizeof(uint32_t));
+
+	/* Allow a leading '0x'. (It's expected, but redundant.) */
+	if (s[0] == '0' && (s[1] == 'x' || s[1] == 'X'))
+		s += 2;
+
+	/* Scan forwards in the string, looking for the end of the sequence. */
+	for (si = 0; isxdigit(s[si]); si++)
+		;
+
+	/* Scan backwards, filling in the bits in words[] as we go. */
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+	for (bitpos = 0; bitpos < 32 * num_words; bitpos += 4) {
+#else
+	for (bitpos = 32 * num_words - 4; bitpos >= 0; bitpos -= 4) {
+#endif
+		if (--si < 0)
+			break;
+		words[bitpos / 32] |= digittoint(s[si]) << (bitpos % 32);
+	}
+}
 
 double
 nan(const char *s)
 {
-	static FPI fpi = { 52, -1074, 971, 1, SI };
+	union {
+		double d;
+		uint32_t bits[2];
+	} u;
 
-	double result;
-	ULong bits[2];
-
-	s--;
-	hexnan(&s, &fpi, bits);
-	SET_HIGH_WORD(result, 0x7ff80000 | bits[1]);
-	SET_LOW_WORD(result, bits[0]);
-	return (result);
+	_scan_nan(u.bits, 2, s);
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+	u.bits[1] |= 0x7ff80000;
+#else
+	u.bits[0] |= 0x7ff80000;
+#endif
+	return (u.d);
 }
 
 float
 nanf(const char *s)
 {
-	static FPI fpi = { 24, -149, 104, 1, SI };
+	union {
+		float f;
+		uint32_t bits[1];
+	} u;
 
-	float result;
-	ULong bits[1];
-
-	s--;
-	hexnan(&s, &fpi, bits);
-	SET_FLOAT_WORD(result, 0x7fc00000 | bits[0]);
-	return (result);
+	_scan_nan(u.bits, 1, s);
+	u.bits[0] |= 0x7fc00000;
+	return (u.f);
 }
 
 #if (LDBL_MANT_DIG == 53)
