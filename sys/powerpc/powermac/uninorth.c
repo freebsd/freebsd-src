2a7967f729d137debbd206360ff5bcfa0971e011
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2007-12-20 21:42:43 +0000

    MFamd64/ia64/i386: Only set the rman bus tags and handles in bus_activate_resource() methods instead of splitting it up between bus_alloc_resource() and bus_activate_resource().
    Glanced at by:	marcel
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@174820 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/powerpc/powermac/uninorth.c
+++ sys/powerpc/powermac/uninorth.c
@@ -382,8 +382,6 @@ uninorth_alloc_resource(device_t bus, device_t child, int type, int *rid,
 	}
 
 	rman_set_rid(rv, *rid);
-	rman_set_bustag(rv, &bs_le_tag);
-	rman_set_bushandle(rv, rman_get_start(rv));
 
 	if (needactivate) {
 		if (bus_activate_resource(child, type, *rid, rv) != 0) {
@@ -429,6 +427,7 @@ uninorth_activate_resource(device_t bus, device_t child, int type, int rid,
 		if (p == NULL)
 			return (ENOMEM);
 		rman_set_virtual(res, p);
+		rman_set_bustag(rv, &bs_le_tag);
 		rman_set_bushandle(res, (u_long)p);
 	}
 
