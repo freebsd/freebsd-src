55e2cb41f216b713fb865aaf392ddb17ebbd57d1
Edwin Groothuis <edwin@FreeBSD.org>
2007-12-25 00:52:24 +0000

    Add the ability to clean up all shared memory segments which are unused in one go.
    From the original PR:
    
    	I've observed that linux apps running under the linuxulator
    	have a habit of leaving behind shared memory segments which
    	are unused, but which eventually cause the system to run
    	out of free segments and these apps will stop working.
    	ipcrm(1) currently only allows removal of unused message
    	queues, shared memory segments and semaphores on an individual
    	basis, or those having a matching (non-zero) key. However
    	it would often be convenient to just do a complete cleanup
    	of everything, usually as root.
    
    PR:		bin/118292
    Submitted by:	Callum Gibson <callumgibson@optusnet.com.au>
    Not reviewed by:	grog@
    Approved by:	grog@

--- usr.bin/ipcrm/ipcrm.c
+++ usr.bin/ipcrm/ipcrm.c
@@ -27,42 +27,47 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
  */
 
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
+#include <sys/param.h>
+#define _KERNEL
 #include <sys/sem.h>
 #include <sys/shm.h>
+#include <sys/msg.h>
+#undef _KERNEL
 
 #include <ctype.h>
 #include <err.h>
-#include <signal.h>
+#include <grp.h>
+#include <kvm.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-#define IPC_TO_STR(x) (x == 'Q' ? "msq" : (x == 'M' ? "shm" : "sem"))
-#define IPC_TO_STRING(x) (x == 'Q' ? "message queue" : \
-	(x == 'M' ? "shared memory segment" : "semaphore"))
+#include "ipc.h"
 
-int signaled;
+int	signaled;
+int	errflg;
+int	rmverbose = 0;
 
-void usage(void);
-int msgrm(key_t, int);
-int shmrm(key_t, int);
-int semrm(key_t, int);
-void not_configured(int);
+void	usage(void);
+
+int	msgrm(key_t, int);
+int	shmrm(key_t, int);
+int	semrm(key_t, int);
+void	not_configured(int);
 
 void
 usage(void)
 {
 
 	fprintf(stderr,
-	    "usage: ipcrm [-q msqid] [-m shmid] [-s semid]\n"
+	    "usage: ipcrm [-W] [-v[v]]\n"
+	    "             [-q msqid] [-m shmid] [-s semid]\n"
 	    "             [-Q msgkey] [-M shmkey] [-S semkey] ...\n");
 	exit(1);
 }
@@ -71,11 +76,40 @@ int
 msgrm(key_t key, int id)
 {
 
+	if (key == -1 || id == -1) {
+		struct msqid_kernel *kxmsqids;
+		size_t kxmsqids_len;
+		int num;
+
+		kget(X_MSGINFO, &msginfo, sizeof(msginfo));
+		kxmsqids_len = sizeof(struct msqid_kernel) * msginfo.msgmni;
+		kxmsqids = malloc(kxmsqids_len);
+		kget(X_MSQIDS, kxmsqids, kxmsqids_len);
+		num = msginfo.msgmni;
+		while (num-- && !signaled)
+			if (kxmsqids[num].u.msg_qbytes != 0) {
+				id = IXSEQ_TO_IPCID(num,
+					kxmsqids[num].u.msg_perm);
+				if (msgctl(id, IPC_RMID, NULL) < 0) {
+					if (rmverbose > 1)
+						warn("msqid(%d): ", id);
+					errflg++;
+				} else
+					if (rmverbose)
+						printf(
+						    "Removed %s %d\n",
+						    IPC_TO_STRING('Q'),
+						    id);
+			}
+		return signaled ? -1 : 0;       /* errors maybe handled above */
+	}
+
 	if (key) {
 		id = msgget(key, 0);
 		if (id == -1)
 			return -1;
 	}
+
 	return msgctl(id, IPC_RMID, NULL);
 }
 
@@ -83,11 +117,40 @@ int
 shmrm(key_t key, int id)
 {
 
+	if (key == -1 || id == -1) {
+		struct shmid_kernel *kxshmids;
+		size_t kxshmids_len;
+		int num;
+
+		kget(X_SHMINFO, &shminfo, sizeof(shminfo));
+		kxshmids_len = sizeof(struct shmid_kernel) * shminfo.shmmni;
+		kxshmids = malloc(kxshmids_len);
+		kget(X_SHMSEGS, kxshmids, kxshmids_len);
+		num = shminfo.shmmni;
+		while (num-- && !signaled)
+			if (kxshmids[num].u.shm_perm.mode & 0x0800) {
+				id = IXSEQ_TO_IPCID(num,
+					kxshmids[num].u.shm_perm);
+				if (shmctl(id, IPC_RMID, NULL) < 0) {
+					if (rmverbose > 1)
+						warn("shmid(%d): ", id);
+					errflg++;
+				} else
+					if (rmverbose)
+						printf(
+						    "Removed %s %d\n",
+						    IPC_TO_STRING('M'),
+						    id);
+			}
+		return signaled ? -1 : 0;       /* errors maybe handled above */
+	}
+
 	if (key) {
 		id = shmget(key, 0, 0);
 		if (id == -1)
 			return -1;
 	}
+
 	return shmctl(id, IPC_RMID, NULL);
 }
 
@@ -96,11 +159,40 @@ semrm(key_t key, int id)
 {
 	union semun arg;
 
+	if (key == -1 || id == -1) {
+		struct semid_kernel *kxsema;
+		size_t kxsema_len;
+		int num;
+
+		kget(X_SEMINFO, &seminfo, sizeof(seminfo));
+		kxsema_len = sizeof(struct semid_kernel) * seminfo.semmni;
+		kxsema = malloc(kxsema_len);
+		kget(X_SEMA, kxsema, kxsema_len);
+		num = seminfo.semmni;
+		while (num-- && !signaled)
+			if ((kxsema[num].u.sem_perm.mode & SEM_ALLOC) != 0) {
+				id = IXSEQ_TO_IPCID(num,
+					kxsema[num].u.sem_perm);
+				if (semctl(id, IPC_RMID, NULL) < 0) {
+					if (rmverbose > 1)
+						warn("semid(%d): ", id);
+					errflg++;
+				} else
+					if (rmverbose)
+						printf(
+						    "Removed %s %d\n",
+						    IPC_TO_STRING('S'),
+						    id);
+			}
+		return signaled ? -1 : 0;       /* errors maybe handled above */
+	}
+
 	if (key) {
 		id = semget(key, 0, 0);
 		if (id == -1)
 			return -1;
 	}
+
 	return semctl(id, 0, IPC_RMID, arg);
 }
 
@@ -114,12 +206,26 @@ not_configured(int signo __unused)
 int
 main(int argc, char *argv[])
 {
-	int c, result, errflg, target_id;
+	int c, result, target_id;
 	key_t target_key;
 
+	while ((c = getopt(argc, argv, "q:m:s:Q:M:S:vWy")) != -1) {
+
+		signaled = 0;
+		switch (c) {
+		case 'v':
+			rmverbose++;
+			break;
+		case 'y':
+			use_sysctl = 0;
+			break;
+		}
+	}
+
+	optind = 1;
 	errflg = 0;
 	signal(SIGSYS, not_configured);
-	while ((c = getopt(argc, argv, ":q:m:s:Q:M:S:")) != -1) {
+	while ((c = getopt(argc, argv, "q:m:s:Q:M:S:vWy")) != -1) {
 
 		signaled = 0;
 		switch (c) {
@@ -170,6 +276,15 @@ main(int argc, char *argv[])
 					    "in the running kernel",
 					    IPC_TO_STRING(c));
 			}
+			break;
+		case 'v':
+		case 'y':
+			/* Handled in other getopt() loop */
+			break;
+		case 'W':
+			msgrm(-1, 0);
+			shmrm(-1, 0);
+			semrm(-1, 0);
 			break;
 		case ':':
 			fprintf(stderr,
