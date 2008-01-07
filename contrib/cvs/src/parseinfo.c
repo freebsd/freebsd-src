3f6ab172071b532112b8410daf3525f55f10bcf7
Peter Wemm <peter@FreeBSD.org>
2008-01-07 09:11:23 +0000

    MFC: rev 1.4: forward compatability with cvs-1.12 repository

--- contrib/cvs/src/parseinfo.c
+++ contrib/cvs/src/parseinfo.c
@@ -367,6 +367,16 @@ warning: this CVS does not support PreservePermissions");
 	       opendir it or something, but I don't see any particular
 	       reason to do that now rather than waiting until lock.c.  */
 	}
+	else if (strcmp(line, "LocalKeyword") == 0)
+	{
+	    /* Recognize cvs-1.12-style keyword control rather than erroring out. */
+	    RCS_setlocalid(p);
+	}
+	else if (strcmp(line, "KeywordExpand") == 0)
+	{
+	    /* Recognize cvs-1.12-style keyword control rather than erroring out. */
+	    RCS_setincexc(p);
+	}
 	else
 	{
 	    /* We may be dealing with a keyword which was added in a
