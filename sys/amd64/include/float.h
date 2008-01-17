3988f1f8cf9e89f548ce94071a8c520c763fafd8
bde <bde@FreeBSD.org>
2008-01-17 13:12:46 +0000

    Translate from the i386.  All FP constants and operations are evaluated in the range and precision of their type(s) on amd64, but FLT_EVAL_METHOD said that they were evalated in the "interesting" (buggy) i387 methods. float_t was broken compatibly with FLT_EVAL_METHOD.
    These definitions seem to be broken on powerpc and possibly on arm.
    float_t is float on powerpc with gcc [-notraditional] according to
    glibc, and FLT_EVAL_METHOD is marked with XXX on arm.

--- sys/amd64/include/float.h
+++ sys/amd64/include/float.h
@@ -42,7 +42,7 @@ __END_DECLS
 #define FLT_RADIX	2		/* b */
 #define FLT_ROUNDS	__flt_rounds()
 #if __ISO_C_VISIBLE >= 1999
-#define	FLT_EVAL_METHOD	(-1)		/* i387 semantics are...interesting */
+#define	FLT_EVAL_METHOD	0		/* no promotions */
 #define	DECIMAL_DIG	21		/* max precision in decimal digits */
 #endif
 
