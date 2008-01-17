4070132854aae59ebc71de6e3a354ebcc4687b20
bde <bde@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-01-17 13:12:46 +0000

    Translate from the i386.  All FP constants and operations are evaluated in the range and precision of their type(s) on amd64, but FLT_EVAL_METHOD said that they were evalated in the "interesting" (buggy) i387 methods. float_t was broken compatibly with FLT_EVAL_METHOD.
    These definitions seem to be broken on powerpc and possibly on arm.
    float_t is float on powerpc with gcc [-notraditional] according to
    glibc, and FLT_EVAL_METHOD is marked with XXX on arm.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@175398 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/amd64/include/_types.h
+++ sys/amd64/include/_types.h
@@ -64,7 +64,7 @@ typedef	__int32_t	__clock_t;		/* clock()... */
 typedef	unsigned int	__cpumask_t;
 typedef	__int64_t	__critical_t;
 typedef	double		__double_t;
-typedef	double		__float_t;
+typedef	float		__float_t;
 typedef	__int64_t	__intfptr_t;
 typedef	__int64_t	__intmax_t;
 typedef	__int64_t	__intptr_t;
