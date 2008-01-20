3646748d3a5e87a887add01aa8e8876397b298c1
maxim <maxim@FreeBSD.org>
2008-01-20 08:31:35 +0000

    o Fix ipfw(8) command line parser bug: "ipfw nat 1 config if"                   requires an argument.
    PR:		bin/119815
    Submitted by:	Dierk Sacher
    MFC after:	1 week

--- sbin/ipfw/ipfw2.c
+++ sbin/ipfw/ipfw2.c
@@ -3994,6 +3994,8 @@ config_nat(int ac, char **av)
 			ac--; av++;
 			break;	    
 		case TOK_IF:
+			if (ac == 0) 
+				errx(EX_DATAERR, "missing option");
 			set_addr_dynamic(av[0], n);
 			ac--; av++;
 			break;
