6c4a8e59f443766e91dd6bcf174cb3f2d96c2ac3
jhb <jhb@FreeBSD.org>
2008-01-29 23:44:34 +0000

    - Rework the kld support to hook into GDB's shared library support.   kgdb(8) now treats kld's as shared libraries relative to the kernel   "binary".  Thus, you can use 'info sharedlibrary' to list the kld's   along with 'sharedlibrary' and 'nosharedlibrary' to manage symbol   loading and unloading.  Note that there isn't an easy way to force GDB   to use a specific path for a shared library.  However, you can use   'nosharedlibrary' to unload all the klds and then use 'sharedlibrary'   to load specific klds where it gets the kld correct and use   'add-kld' for the kld's where the default open behavior doesn't work.   klds opened via 'sharedlibrary' (and during startup) do have their   sections listed in 'info files'. - Change the 'add-kld' command to use filename completion to complete its   argument.

--- gnu/usr.bin/gdb/kgdb/kgdb.h
+++ gnu/usr.bin/gdb/kgdb/kgdb.h
@@ -50,6 +50,7 @@ extern struct kthr *curkthr;
 
 void kgdb_add_kld_cmd(char *, int);
 void kgdb_auto_load_klds(void);
+void kgdb_kld_init(void);
 void kgdb_target(void);
 void kgdb_trgt_fetch_registers(int);
 void kgdb_trgt_store_registers(int);
