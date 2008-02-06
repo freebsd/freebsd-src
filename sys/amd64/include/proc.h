cd28681cbbf6b9766f080af8b5b5d6a60146e679
mav <mav@FreeBSD.org>
2008-02-06 20:50:50 +0000

    MFC ng_base.c rev. 1.141-1.146: Implement stack protection based on GET_STACK_USAGE() macro. This fixes system panics possible with complicated netgraph setups and allows to avoid unneded extra queueing for stack unwrapping.

--- sys/amd64/include/proc.h
+++ sys/amd64/include/proc.h
@@ -44,4 +44,17 @@ struct mdthread {
 struct mdproc {
 };
 
+#ifdef	_KERNEL
+
+/* Get the current kernel thread stack usage. */
+#define GET_STACK_USAGE(total, used) do {				\
+	struct thread	*td = curthread;				\
+	(total) = td->td_kstack_pages * PAGE_SIZE;			\
+	(used) = (char *)td->td_kstack +				\
+	    td->td_kstack_pages * PAGE_SIZE -				\
+	    (char *)&td;						\
+} while (0)
+
+#endif  /* _KERNEL */
+
 #endif /* !_MACHINE_PROC_H_ */
