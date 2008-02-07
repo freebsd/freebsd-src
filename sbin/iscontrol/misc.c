b1e439e170277257d32f092897848c187a95143a
Pierre Beyssac <pb@FreeBSD.org>
2008-02-07 00:08:57 +0000

    MFC rev 1.4: fix a bug in hexadecimal formatting of the CHAP digest: bytes >= 0x80 were formatted as 0xff, causing the authentication to fail.
    Note: fast MFC to RELENG_7 and RELENG_7_0 to get the changes in
    time for 7.0-RC2.
    
    Approved by:	re (kensmith)

--- sbin/iscontrol/misc.c
+++ sbin/iscontrol/misc.c
@@ -158,7 +158,8 @@ char *
 bin2str(char *encoding, unsigned char *md, int blen)
 {
      int	len;
-     char	*dst, *ds, *cp;
+     char	*dst, *ds;
+     unsigned char *cp;
 
      if(strncasecmp(encoding, "0x", 2) == 0) {
 	  char	ofmt[5];
@@ -167,7 +168,7 @@ bin2str(char *encoding, unsigned char *md, int blen)
 	  dst = malloc(len + 3);
 	  strcpy(dst, encoding);
 	  ds = dst + 2;
-	  cp = (char *)md;
+	  cp = md;
 	  sprintf(ofmt, "%%02%c", encoding[1]);
 	  while(blen-- > 0) {
 	       sprintf(ds, ofmt, *cp++);
@@ -183,8 +184,8 @@ bin2str(char *encoding, unsigned char *md, int blen)
 	  dst = malloc(len + 3);
 	  strcpy(dst, encoding);
 	  ds = dst + 2;
-	  cp = (char *)md;
-	  b6 = 0; // to keep copiler happy.
+	  cp = md;
+	  b6 = 0; // to keep compiler happy.
 	  for(i = 0; i < blen; i++) {
 	       switch(i % 3) {
 	       case 0:
