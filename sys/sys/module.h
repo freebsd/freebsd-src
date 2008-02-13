d55b5748d784d47317888c0e6bea5b2db329dc24
jhb <jhb@FreeBSD.org>
2008-02-13 21:34:06 +0000

    Add an automatic kernel module version dependency to prevent loading modules using invalid ABI versions (e.g. a 7.x module with an 8.x kernel) for a given kernel: - Add a 'kernel' module version whose value is __FreeBSD_version. - Add a version dependency on 'kernel' in every module that has an   acceptable version range of __FreeBSD_version up to the end of the   branch __FreeBSD_version is part of.  E.g. a module compiled on 701000   would work on kernels with versions between 701000 and 799999 inclusive.
    Discussed on:	arch@
    MFC after:	1 week

--- sys/sys/module.h
+++ sys/sys/module.h
@@ -114,7 +114,20 @@ struct mod_metadata {
 	MODULE_METADATA(_md_##module##_on_##mdepend, MDT_DEPEND,	\
 	    &_##module##_depend_on_##mdepend, #mdepend)
 
+/*
+ * Every kernel has a 'kernel' module with the version set to
+ * __FreeBSD_version.  We embed a MODULE_DEPEND() inside every module
+ * that depends on the 'kernel' module.  It uses the current value of
+ * __FreeBSD_version as the minimum and preferred versions.  For the
+ * maximum version it rounds the version up to the end of its branch
+ * (i.e. M99999 for M.x).  This allows a module built on M.x to work
+ * on M.y systems where y >= x, but fail on M.z systems where z < x.
+ */
+#define	MODULE_KERNEL_MAXVER	(roundup(__FreeBSD_version, 100000) - 1)
+
 #define	DECLARE_MODULE(name, data, sub, order)				\
+	MODULE_DEPEND(name, kernel, __FreeBSD_version,			\
+	    __FreeBSD_version, MODULE_KERNEL_MAXVER);			\
 	MODULE_METADATA(_md_##name, MDT_MODULE, &data, #name);		\
 	SYSINIT(name##module, sub, order, module_register_init, &data)	\
 	struct __hack
