a7d75a1a2ac7d7261da0c882d56c0854fe9cd364
jfv <jfv@FreeBSD.org>
2008-02-29 22:38:12 +0000

    Need to add define of FAST interrupts as default

--- sys/dev/em/if_em.h
+++ sys/dev/em/if_em.h
@@ -37,6 +37,9 @@ POSSIBILITY OF SUCH DAMAGE.
 
 /* Tunables */
 
+/* Set FAST Interrupt handling as default */
+#define EM_FAST_IRQ
+
 /*
  * EM_TXD: Maximum number of Transmit Descriptors
  * Valid Range: 80-256 for 82542 and 82543-based adapters
