38d4ccbf4b03fee84c290f5676114aa583b5a9e2
rwatson <rwatson@FreeBSD.org>
2008-03-07 15:27:08 +0000

    Add __FBSDID() tag.
    MFC after:	3 days
    Pointed out by:	antoine

--- sys/kern/kern_priv.c
+++ sys/kern/kern_priv.c
@@ -25,11 +25,12 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * $FreeBSD$
  */
 
 #include "opt_mac.h"
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
 #include <sys/jail.h>
