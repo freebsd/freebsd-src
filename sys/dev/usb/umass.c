4e8015d35200591f7cc7dc79a3341d6b446a4ecc
Remko Lodder <remko@FreeBSD.org>
2008-03-07 16:00:28 +0000

    MFC rev 1.162 umass.c MFC rev 1.338 usbdevs
    Add support for the VIA USB2IDE bridge [1].
    
      PR: usb/95173
      Submitted by: Goegele Hannes <diazepam@gmx.net>
      Ben Kelly <bkelly at vadev dot org>
      Tested by: blackend
      Approved by: imp (mentor, blanket for simple patches)
      X-MFC-After: 1 week
    
    Approved by:	imp (mentor, implicit)

--- sys/dev/usb/umass.c
+++ sys/dev/usb/umass.c
@@ -317,6 +317,12 @@ struct umass_devdescr_t {
 #	define NO_INQUIRY_EVPD		0x0800
 	/* Pad all RBC requests to 12 bytes. */
 #	define RBC_PAD_TO_12		0x1000
+	/* Device cannot handle a SCSI synchronize cache command.  Normally
+	 * this quirk would be handled in the cam layer, but for IDE bridges
+	 * we need to associate the quirk with the bridge and not the
+	 * underlying disk device.  This is handled by faking a success result.
+	 */
+#	define NO_SYNCHRONIZE_CACHE	0x4000
 };
 
 Static struct umass_devdescr_t umass_devdescrs[] = {
@@ -505,6 +511,10 @@ Static struct umass_devdescr_t umass_devdescrs[] = {
 	{ USB_VENDOR_TWINMOS, USB_PRODUCT_TWINMOS_MDIV, RID_WILDCARD,
 	  UMASS_PROTO_SCSI | UMASS_PROTO_BBB,
 	  NO_QUIRKS
+	},
+	{ USB_VENDOR_VIA, USB_PRODUCT_VIA_USB2IDEBRIDGE, RID_WILDCARD,
+	  UMASS_PROTO_SCSI | UMASS_PROTO_BBB,
+	  NO_SYNCHRONIZE_CACHE
 	},
 	{ USB_VENDOR_WESTERN,  USB_PRODUCT_WESTERN_EXTHDD, RID_WILDCARD,
 	  UMASS_PROTO_SCSI | UMASS_PROTO_BBB,
@@ -2528,6 +2538,15 @@ umass_cam_action(struct cam_sim *sim, union ccb *ccb)
 
 				memcpy(csio->data_ptr, &fake_inq_data,
 				    sizeof(fake_inq_data));
+				csio->scsi_status = SCSI_STATUS_OK;
+				ccb->ccb_h.status = CAM_REQ_CMP;
+				xpt_done(ccb);
+				return;
+			}
+			if ((sc->quirks & NO_SYNCHRONIZE_CACHE) &&
+			    rcmd[0] == SYNCHRONIZE_CACHE) {
+				struct ccb_scsiio *csio = &ccb->csio;
+
 				csio->scsi_status = SCSI_STATUS_OK;
 				ccb->ccb_h.status = CAM_REQ_CMP;
 				xpt_done(ccb);
