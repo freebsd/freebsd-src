cddc9495236067c8dc3236f843b2b3676a388245
scottl <scottl@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-03-08 18:06:48 +0000

    Fix a mistake made during the import of the driver.  Previous versions of HPT drivers would sometimes test the value of a preprocessor definition but not always make sure that the definition existed in the first place, leading to warnings on newer compilers.  I blindly assumed the same with this driver, and it turned out to be wrong and to enable some code that doesn't work.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@176939 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/hptrr/osm.h
+++ sys/dev/hptrr/osm.h
@@ -187,7 +187,7 @@ HPT_U8 os_get_vbus_seq(void *osext);
 /* debug support */
 int  os_printk(char *fmt, ...);
 
-#ifdef DBG
+#if DBG
 extern int hptrr_dbg_level;
 #define KdPrint(x)  do { if (hptrr_dbg_level) os_printk x; } while (0)
 void __os_dbgbreak(const char *file, int line);
