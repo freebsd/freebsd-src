1b085fde873bf5b584a8ce6ed3fc582d762aa03e
John Baldwin <jhb@FreeBSD.org>
2008-03-11 21:41:36 +0000

    Style(9) these files.  No changes in the compiled code. (Verified by diff'ing objdump -d output).

--- sys/i386/i386/k6_mem.c
+++ sys/i386/i386/k6_mem.c
@@ -47,40 +47,43 @@ __FBSDID("$FreeBSD$");
  * There are two of these in the 64-bit UWCCR.
  */
 
-#define UWCCR 0xc0000085
+#define	UWCCR 0xc0000085
 
-#define K6_REG_GET(reg, addr, mask, wc, uc)	do {			\
+#define	K6_REG_GET(reg, addr, mask, wc, uc) do {			\
 		addr = (reg) & 0xfffe0000;				\
 		mask = ((reg) & 0x1fffc) >> 2;				\
 		wc = ((reg) & 0x2) >> 1;				\
 		uc = (reg) & 0x1;					\
 	} while (0)
 
-#define K6_REG_MAKE(addr, mask, wc, uc) 				\
-		((addr) | ((mask) << 2) | ((wc) << 1) | uc)
+#define	K6_REG_MAKE(addr, mask, wc, uc) 				\
+	((addr) | ((mask) << 2) | ((wc) << 1) | uc)
 
-static void k6_mrinit(struct mem_range_softc *sc);
-static int k6_mrset(struct mem_range_softc *, struct mem_range_desc *, int *);
+static void	k6_mrinit(struct mem_range_softc *sc);
+static int	k6_mrset(struct mem_range_softc *, struct mem_range_desc *,
+		    int *);
 static __inline int k6_mrmake(struct mem_range_desc *, u_int32_t *);
-static void k6_mem_drvinit(void *);
+static void	k6_mem_drvinit(void *);
 
-static struct mem_range_ops k6_mrops = {
+static struct mem_range_ops k6_mrops =
+{
 	k6_mrinit,
 	k6_mrset,
 	NULL
 };
 
 static __inline int
-k6_mrmake(struct mem_range_desc *desc, u_int32_t *mtrr) {
+k6_mrmake(struct mem_range_desc *desc, u_int32_t *mtrr)
+{
 	u_int32_t len = 0, wc, uc;
 	register int bit;
 
 	if (desc->mr_base &~ 0xfffe0000)
-		return EINVAL;
+		return (EINVAL);
 	if (desc->mr_len < 131072 || !powerof2(desc->mr_len))
-		return EINVAL;
+		return (EINVAL);
 	if (desc->mr_flags &~ (MDF_WRITECOMBINE|MDF_UNCACHEABLE|MDF_FORCE))
-		return EOPNOTSUPP;
+		return (EOPNOTSUPP);
 
 	for (bit = ffs(desc->mr_len >> 17) - 1; bit < 15; bit++)
 		len |= 1 << bit; 
@@ -88,11 +91,12 @@ k6_mrmake(struct mem_range_desc *desc, u_int32_t *mtrr) {
 	uc = (desc->mr_flags & MDF_UNCACHEABLE) ? 1 : 0;
 
 	*mtrr = K6_REG_MAKE(desc->mr_base, len, wc, uc);
-	return 0;
+	return (0);
 }
 
 static void
-k6_mrinit(struct mem_range_softc *sc) {
+k6_mrinit(struct mem_range_softc *sc)
+{
 	u_int64_t reg;
 	u_int32_t addr, mask, wc, uc;
 	int d;
@@ -100,7 +104,7 @@ k6_mrinit(struct mem_range_softc *sc) {
 	sc->mr_cap = 0;
 	sc->mr_ndesc = 2; /* XXX (BFF) For now, we only have one msr for this */
 	sc->mr_desc = malloc(sc->mr_ndesc * sizeof(struct mem_range_desc),
-			     M_MEMDESC, M_NOWAIT | M_ZERO);
+	    M_MEMDESC, M_NOWAIT | M_ZERO);
 	if (sc->mr_desc == NULL)
 		panic("k6_mrinit: malloc returns NULL");
 
@@ -116,12 +120,13 @@ k6_mrinit(struct mem_range_softc *sc) {
 		if (uc)
 			sc->mr_desc[d].mr_flags |= MDF_UNCACHEABLE;
 	}
-	
+
 	printf("K6-family MTRR support enabled (%d registers)\n", sc->mr_ndesc);
 }
 
 static int
-k6_mrset(struct mem_range_softc *sc, struct mem_range_desc *desc, int *arg) {
+k6_mrset(struct mem_range_softc *sc, struct mem_range_desc *desc, int *arg)
+{
 	u_int64_t reg;
 	u_int32_t mtrr;
 	int error, d;
@@ -130,7 +135,7 @@ k6_mrset(struct mem_range_softc *sc, struct mem_range_desc *desc, int *arg) {
 	case MEMRANGE_SET_UPDATE:
 		error = k6_mrmake(desc, &mtrr);
 		if (error)
-			return error;
+			return (error);
 		for (d = 0; d < sc->mr_ndesc; d++) {
 			if (!sc->mr_desc[d].mr_len) {
 				sc->mr_desc[d] = *desc;
@@ -138,10 +143,9 @@ k6_mrset(struct mem_range_softc *sc, struct mem_range_desc *desc, int *arg) {
 			}
 			if (sc->mr_desc[d].mr_base == desc->mr_base &&
 			    sc->mr_desc[d].mr_len == desc->mr_len)
-				return EEXIST;
+				return (EEXIST);
 		}
-
-		return ENOSPC;
+		return (ENOSPC);
 	case MEMRANGE_SET_REMOVE:
 		mtrr = 0;
 		for (d = 0; d < sc->mr_ndesc; d++)
@@ -150,14 +154,11 @@ k6_mrset(struct mem_range_softc *sc, struct mem_range_desc *desc, int *arg) {
 				bzero(&sc->mr_desc[d], sizeof(sc->mr_desc[d]));
 				goto out;
 			}
-
-		return ENOENT;
+		return (ENOENT);
 	default:
-		return EOPNOTSUPP;
+		return (EOPNOTSUPP);
 	}
-
-out:
-	
+out:	
 	disable_intr();
 	wbinvd();
 	reg = rdmsr(UWCCR);
@@ -167,18 +168,18 @@ out:
 	wbinvd();
 	enable_intr();
 
-	return 0;
+	return (0);
 }
 
 static void
-k6_mem_drvinit(void *unused) {
+k6_mem_drvinit(void *unused)
+{
+
 	if (!strcmp(cpu_vendor, "AuthenticAMD") &&
 	    (cpu_id & 0xf00) == 0x500 &&
-	    	((cpu_id & 0xf0) > 0x80 ||
-		    ((cpu_id & 0xf0) == 0x80 &&
-		     (cpu_id & 0xf) > 0x7))
-	    )
+	    ((cpu_id & 0xf0) > 0x80 ||
+	    ((cpu_id & 0xf0) == 0x80 &&
+	    (cpu_id & 0xf) > 0x7)))
 		mem_range_softc.mr_op = &k6_mrops;
 }
-
-SYSINIT(k6memdev, SI_SUB_DRIVERS, SI_ORDER_FIRST, k6_mem_drvinit, NULL)
+SYSINIT(k6memdev, SI_SUB_DRIVERS, SI_ORDER_FIRST, k6_mem_drvinit, NULL);
