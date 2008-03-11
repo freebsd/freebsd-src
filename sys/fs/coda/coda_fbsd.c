7af7e32d47297ceafa2716a90ece06ddedbb61d2
Robert Watson <rwatson@FreeBSD.org>
2008-03-11 10:14:47 +0000

    Merge cnode.h:1.25, coda_fbsd.c:1.48, coda_io.h:1.10, coda_kernel.h:1.7, coda_namecache.c:1.25, coda_namecache.h:1.14, coda_opstats.h:1.8, coda_pioctl.h:1.10, coda_psdev.c:1.43, coda_psdev.h:1.8, coda_subr.c:1.35, coda_subr.h:1.11, coda_venus.c:1.30, coda_venus.h:1.11, coda_vfsops.c:1.79, coda_vfsops.h:1.14, coda_vnops.c:1.92, coda_vnops.h:1.23 from HEAD to RELENG_7:
      Since we're now actively maintaining the Coda module in the FreeBSD source
      tree, restyle everything but coda.h (which is more explicitly shared
      across systems) into a closer approximation to style(9).
    
      Remove a few more unused function prototypes.
    
      Add or clarify some comments.
    
    Minor differences required in the MFC due to diverging VFS locking in
    HEAD vs RELENG_7.

--- sys/fs/coda/coda_fbsd.c
+++ sys/fs/coda/coda_fbsd.c
@@ -1,10 +1,10 @@
 /*-
  *             Coda: an Experimental Distributed File System
  *                              Release 3.1
- * 
+ *
  *           Copyright (c) 1987-1998 Carnegie Mellon University
  *                          All Rights Reserved
- * 
+ *
  * Permission  to  use, copy, modify and distribute this software and its
  * documentation is hereby granted,  provided  that  both  the  copyright
  * notice  and  this  permission  notice  appear  in  all  copies  of the
@@ -13,18 +13,18 @@
  * that credit is given to Carnegie Mellon University  in  all  documents
  * and publicity pertaining to direct or indirect use of this code or its
  * derivatives.
- * 
+ *
  * CODA IS AN EXPERIMENTAL SOFTWARE SYSTEM AND IS  KNOWN  TO  HAVE  BUGS,
  * SOME  OF  WHICH MAY HAVE SERIOUS CONSEQUENCES.  CARNEGIE MELLON ALLOWS
  * FREE USE OF THIS SOFTWARE IN ITS "AS IS" CONDITION.   CARNEGIE  MELLON
  * DISCLAIMS  ANY  LIABILITY  OF  ANY  KIND  FOR  ANY  DAMAGES WHATSOEVER
  * RESULTING DIRECTLY OR INDIRECTLY FROM THE USE OF THIS SOFTWARE  OR  OF
  * ANY DERIVATIVE WORK.
- * 
+ *
  * Carnegie  Mellon  encourages  users  of  this  software  to return any
  * improvements or extensions that  they  make,  and  to  grant  Carnegie
  * Mellon the rights to redistribute these changes without encumbrance.
- * 
+ *
  * 	@(#) src/sys/coda/coda_fbsd.cr,v 1.1.1.1 1998/08/29 21:14:52 rvb Exp $
  */
 
@@ -66,17 +66,20 @@ static eventhandler_tag clonetag;
 
 static LIST_HEAD(, coda_mntinfo) coda_mnttbl;
 
-int     vcdebug = 1;
-#define VCDEBUG if (vcdebug) printf
-
-/* for DEVFS, using bpf & tun drivers as examples*/
+/*
+ * For DEVFS, using bpf & tun drivers as examples.
+ *
+ * XXX: Why use a cloned interface, aren't we really just interested in
+ * having a single /dev/cfs0?  It's not clear the coda module knows what to
+ * do with more than one.
+ */
 static void coda_fbsd_clone(void *arg, struct ucred *cred, char *name,
     int namelen, struct cdev **dev);
 
 static int
 codadev_modevent(module_t mod, int type, void *data)
 {
-	struct coda_mntinfo	*mnt;
+	struct coda_mntinfo *mnt;
 
 	switch (type) {
 	case MOD_LOAD:
@@ -84,7 +87,14 @@ codadev_modevent(module_t mod, int type, void *data)
 		clonetag = EVENTHANDLER_REGISTER(dev_clone, coda_fbsd_clone,
 		    0, 1000);
 		break;
+
 	case MOD_UNLOAD:
+		/*
+		 * XXXRW: At the very least, a busy check should occur here
+		 * to prevent untimely unload.  Much more serious collection
+		 * of allocated memory needs to take place; right now we leak
+		 * like a sieve.
+		 */
 		EVENTHANDLER_DEREGISTER(dev_clone, clonetag);
 		while ((mnt = LIST_FIRST(&coda_mnttbl)) != NULL) {
 			LIST_REMOVE(mnt, mi_list);
@@ -96,8 +106,9 @@ codadev_modevent(module_t mod, int type, void *data)
 	default:
 		return (EOPNOTSUPP);
 	}
-	return 0;
+	return (0);
 }
+
 static moduledata_t codadev_mod = {
 	"codadev",
 	codadev_modevent,
@@ -105,37 +116,33 @@ static moduledata_t codadev_mod = {
 };
 DECLARE_MODULE(codadev, codadev_mod, SI_SUB_DRIVERS, SI_ORDER_MIDDLE);
 
-static void coda_fbsd_clone(arg, cred, name, namelen, dev)
-    void *arg;
-    struct ucred *cred;
-    char *name;
-    int namelen;
-    struct cdev **dev;
+static void
+coda_fbsd_clone(void *arg, struct ucred *cred, char *name, int namelen,
+    struct cdev **dev)
 {
-    int u;
-    struct coda_mntinfo *mnt;
+	struct coda_mntinfo *mnt;
+	int u;
 
-    if (*dev != NULL)
-	return;
-    if (dev_stdclone(name,NULL,"cfs",&u) != 1)
-	return;
-
-    *dev = make_dev(&codadevsw,unit2minor(u),UID_ROOT,GID_WHEEL,0600,"cfs%d",u);
-    dev_ref(*dev);
-    mnt = malloc(sizeof(struct coda_mntinfo), M_CODA, M_WAITOK|M_ZERO);
-    LIST_INSERT_HEAD(&coda_mnttbl, mnt, mi_list);
-    mnt->dev = *dev;
+	if (*dev != NULL)
+		return;
+	if (dev_stdclone(name, NULL, "cfs", &u) != 1)
+		return;
+	*dev = make_dev(&codadevsw, unit2minor(u), UID_ROOT, GID_WHEEL, 0600,
+	    "cfs%d", u);
+	dev_ref(*dev);
+	mnt = malloc(sizeof(struct coda_mntinfo), M_CODA, M_WAITOK|M_ZERO);
+	LIST_INSERT_HEAD(&coda_mnttbl, mnt, mi_list);
+	mnt->dev = *dev;
 }
 
 struct coda_mntinfo *
 dev2coda_mntinfo(struct cdev *dev)
 {
-	struct coda_mntinfo	*mnt;
+	struct coda_mntinfo *mnt;
 
 	LIST_FOREACH(mnt, &coda_mnttbl, mi_list) {
 		if (mnt->dev == dev)
-			return mnt;
+			return (mnt);
 	}
-
-	return NULL;
+	return (NULL);
 }
--- sys/fs/coda/coda_psdev.h
+++ sys/fs/coda/coda_psdev.h
@@ -1,11 +1,11 @@
 /*-
- * 
+ *
  *             Coda: an Experimental Distributed File System
  *                              Release 3.1
- * 
+ *
  *           Copyright (c) 1998 Carnegie Mellon University
  *                          All Rights Reserved
- * 
+ *
  * Permission  to  use, copy, modify and distribute this software and its
  * documentation is hereby granted,  provided  that  both  the  copyright
  * notice  and  this  permission  notice  appear  in  all  copies  of the
@@ -14,26 +14,33 @@
  * that credit is given to Carnegie Mellon University  in  all  documents
  * and publicity pertaining to direct or indirect use of this code or its
  * derivatives.
- * 
+ *
  * CODA IS AN EXPERIMENTAL SOFTWARE SYSTEM AND IS  KNOWN  TO  HAVE  BUGS,
  * SOME  OF  WHICH MAY HAVE SERIOUS CONSEQUENCES.  CARNEGIE MELLON ALLOWS
  * FREE USE OF THIS SOFTWARE IN ITS "AS IS" CONDITION.   CARNEGIE  MELLON
  * DISCLAIMS  ANY  LIABILITY  OF  ANY  KIND  FOR  ANY  DAMAGES WHATSOEVER
  * RESULTING DIRECTLY OR INDIRECTLY FROM THE USE OF THIS SOFTWARE  OR  OF
  * ANY DERIVATIVE WORK.
- * 
+ *
  * Carnegie  Mellon  encourages  users  of  this  software  to return any
  * improvements or extensions that  they  make,  and  to  grant  Carnegie
  * Mellon the rights to redistribute these changes without encumbrance.
- * 
+ *
  * 	@(#) src/sys/coda/coda_psdev.c,v 1.1.1.1 1998/08/29 21:14:52 rvb Exp $
  * $FreeBSD$
- * 
  */
 
+#ifndef _CODA_PSDEV_H_
+#define	_CODA_PSDEV_H_
+
+/*
+ * Prototypes for cfs device operations.
+ */
 d_open_t	vc_open;
 d_close_t	vc_close;
 d_read_t	vc_read;
 d_write_t	vc_write;
 d_ioctl_t	vc_ioctl;
 d_poll_t	vc_poll;
+
+#endif /* !_CODA_PSDEV_H_ */
