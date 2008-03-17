eaab59be17045c9744fdf3c60d0aa7ebfc6248f3
marcel <marcel@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-03-17 00:46:52 +0000

    Make remote GDB work for AIM processors. For BookE, the kernel will have a special section, named .PPC.EMB.apuinfo, which will tell GDB that a BookE processor is targeted and which will result in GDB using a different register definition. In order to support remote GDB for BookE, we need the GDB stub in the kernel look for that section and use the BookE definitions.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@177288 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/powerpc/include/gdb_machdep.h
+++ sys/powerpc/include/gdb_machdep.h
@@ -29,20 +29,32 @@
 #ifndef _MACHINE_GDB_MACHDEP_H_
 #define	_MACHINE_GDB_MACHDEP_H_
 
-#define GDB_NREGS	153
+#define	PPC_GDB_NREGS4	(32 + 7 + 2)
+#define	PPC_GDB_NREGS8	32
+#define	PPC_GDB_NREGS16	32
+
+#define GDB_NREGS	(PPC_GDB_NREGS4 + PPC_GDB_NREGS8 + PPC_GDB_NREGS16)
 #define	GDB_REG_PC	64
 
-#define	GDB_BUFSZ	(GDB_NREGS*4)
+#define	GDB_BUFSZ	(PPC_GDB_NREGS4 * 8 +	\
+			 PPC_GDB_NREGS8 * 16 +	\
+			 PPC_GDB_NREGS16 * 32)
 
 static __inline size_t
 gdb_cpu_regsz(int regnum)
 {
-	return (sizeof(int));
+
+	if (regnum >= 32 && regnum <= 63)
+		return (8);
+	if (regnum >= 71 && regnum <= 102)
+		return (16);
+	return (4);
 }
 
 static __inline int
 gdb_cpu_query(void)
 {
+
 	return (0);
 }
 
