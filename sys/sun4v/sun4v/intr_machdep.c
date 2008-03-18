07f7fccaafa75c65e5a12dffbce4bc1a79ee821d
John Baldwin <jhb@FreeBSD.org>
2008-03-18 13:31:45 +0000

    Catch up to intr_event_create() prototype change.
    Pointy hat:	jhb

--- sys/sun4v/sun4v/intr_machdep.c
+++ sys/sun4v/sun4v/intr_machdep.c
@@ -359,7 +359,7 @@ inthand_add(const char *name, int vec, driver_filter_t *filt,
 	mtx_unlock_spin(&intr_table_lock);
 	if (ie == NULL) {
 		errcode = intr_event_create(&ie, (void *)(intptr_t)vec, 0, NULL,
-		    NULL, "vec%d:", vec);
+		    NULL, NULL, NULL, "vec%d:", vec);
 		if (errcode)
 			return (errcode);
 		mtx_lock_spin(&intr_table_lock);
