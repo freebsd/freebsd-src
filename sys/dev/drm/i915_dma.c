bd2ca675fb756fa686ada2feb732418448d1a1cd
remko <remko@FreeBSD.org>
2008-03-24 10:43:41 +0000

    MFC rev 1.9 drm_pciids.h MFC rev 1.7 i915_dma.c
      Add the i915 GME device to DRM.
    
      PR:             kern/121808
      Submitted by:   Volker Werth <volker at vwsoft dot com>
      Approved by:    imp (mentor, implicit for trivial changes)
      MFC after:      3 days
    
    Approved by:	imp (mentor, implicit)

--- sys/dev/drm/i915_dma.c
+++ sys/dev/drm/i915_dma.c
@@ -37,7 +37,8 @@ __FBSDID("$FreeBSD$");
 #define IS_I965G(dev)  (dev->pci_device == 0x2972 || \
 			dev->pci_device == 0x2982 || \
 			dev->pci_device == 0x2992 || \
-			dev->pci_device == 0x29A2)
+			dev->pci_device == 0x29A2 || \
+			dev->pci_device == 0x2A12 )
 
 /* Really want an OS-independent resettable timer.  Would like to have
  * this loop run for (eg) 3 sec, but have the timer reset every time
