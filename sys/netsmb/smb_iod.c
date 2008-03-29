5ff0f4d0623fed901c043f96538771e0ab46c6e3
attilio <attilio@FreeBSD.org>
2008-03-29 23:00:29 +0000

    MFC rev. 1.21: Drop Giant lock before to destroy the kthread.

--- sys/netsmb/smb_iod.c
+++ sys/netsmb/smb_iod.c
@@ -662,12 +662,11 @@ smb_iod_thread(void *arg)
 	while ((iod->iod_flags & SMBIOD_SHUTDOWN) == 0) {
 		smb_iod_main(iod);
 		SMBIODEBUG("going to sleep for %d ticks\n", iod->iod_sleeptimo);
-/*		mtx_unlock(&Giant, MTX_DEF);*/
 		if (iod->iod_flags & SMBIOD_SHUTDOWN)
 			break;
 		tsleep(&iod->iod_flags, PWAIT, "90idle", iod->iod_sleeptimo);
 	}
-/*	mtx_lock(&Giant, MTX_DEF);*/
+	mtx_unlock(&Giant);
 	kthread_exit(0);
 }
 
