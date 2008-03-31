343a5324bfe8dd818293a0ad1210e06d83142ea4
kib <kib@FreeBSD.org>
2008-03-31 12:05:52 +0000

    Implement the fexecve(2) syscall.
    Based on the submission by rdivacky,
    	sponsored by Google Summer of Code 2007
    Reviewed by:	rwatson, rdivacky
    Tested by:	pho

--- sys/sys/imgact.h
+++ sys/sys/imgact.h
@@ -45,6 +45,7 @@ struct image_args {
 	int stringspace;	/* space left in arg & env buffer */
 	int argc;		/* count of argument strings */
 	int envc;		/* count of environment strings */
+	int fd;			/* file descriptor of the executable */
 };
 
 struct image_params {
