a1cd85f02996fab14a32392bd407005edc7712a6
grog <grog@FreeBSD.org>
2008-04-04 03:57:46 +0000

    Add -D option to specify exact format of date and time output with ls -l.

--- bin/ls/ls.h
+++ bin/ls/ls.h
@@ -50,8 +50,9 @@ extern int f_nonprint;		/* show unprintables as ? */
 extern int f_sectime;		/* print the real time for all files */
 extern int f_size;		/* list size in short listing */
 extern int f_slash;		/* append a '/' if the file is a directory */
-extern int f_sortacross;	/* sort across rows, not down columns */ 
+extern int f_sortacross;	/* sort across rows, not down columns */
 extern int f_statustime;	/* use time of last mode change */
+extern char *f_timeformat;      /* user-specified time format */
 extern int f_notabs;		/* don't use tab-separated multi-col output */
 extern int f_type;		/* add type character for non-regular files */
 #ifdef COLORLS
