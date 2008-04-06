0e7cce1381c2ea7d75bd02308aa9921f4b30e62e
Doug Rabson <dfr@FreeBSD.org>
2008-04-06 13:52:17 +0000

    Call listen(2) on bound tcp sockets before passing them to svc_tli_create.

--- usr.sbin/rpc.lockd/lockd.c
+++ usr.sbin/rpc.lockd/lockd.c
@@ -519,6 +519,9 @@ create_service(struct netconfig *nconf)
 			exit(1);
 		}
 
+		if (nconf->nc_semantics != NC_TPI_CLTS)
+		    listen(fd, SOMAXCONN);
+
 		transp = svc_tli_create(fd, nconf, NULL,
 		    RPC_MAXDATASIZE, RPC_MAXDATASIZE);
 
