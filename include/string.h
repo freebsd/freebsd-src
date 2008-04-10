31d589f6373e4e534fb4c6b2ad1a0460b100ab0a
delphij <delphij@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-04-10 00:12:44 +0000

    Add memrchr(3).
    Obtained from:	OpenBSD
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@178051 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- include/string.h
+++ include/string.h
@@ -59,6 +59,7 @@ __BEGIN_DECLS
 void	*memccpy(void * __restrict, const void * __restrict, int, size_t);
 #endif
 void	*memchr(const void *, int, size_t) __pure;
+void	*memrchr(const void *, int, size_t) __pure;
 int	 memcmp(const void *, const void *, size_t) __pure;
 void	*memcpy(void * __restrict, const void * __restrict, size_t);
 #if __BSD_VISIBLE
