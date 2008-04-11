03fb0eb216d08971d67b02038d1a7b5b2700880a
David Xu <davidxu@FreeBSD.org>
2008-04-11 14:39:11 +0000

    MFC: 	Add umtx operation UMTX_OP_WAIT_UINT and userland rwlock.

--- sys/sys/umtx.h
+++ sys/sys/umtx.h
@@ -66,6 +66,23 @@ struct ucond {
 	uint32_t		c_spare[2];	/* Spare space */
 };
 
+struct urwlock {
+	volatile int32_t	rw_state;
+	uint32_t		rw_flags;
+	uint32_t		rw_blocked_readers;
+	uint32_t		rw_blocked_writers;
+	uint32_t		rw_spare[4];
+};
+
+/* urwlock flags */
+#define URWLOCK_PREFER_READER	0x0002
+
+#define URWLOCK_WRITE_OWNER	0x80000000U
+#define URWLOCK_WRITE_WAITERS	0x40000000U
+#define URWLOCK_READ_WAITERS	0x20000000U
+#define URWLOCK_MAX_READERS	0x1fffffffU
+#define URWLOCK_READER_COUNT(c)	((c) & URWLOCK_MAX_READERS)
+
 /* op code for _umtx_op */
 #define	UMTX_OP_LOCK		0
 #define	UMTX_OP_UNLOCK		1
@@ -78,7 +95,11 @@ struct ucond {
 #define	UMTX_OP_CV_WAIT		8
 #define	UMTX_OP_CV_SIGNAL	9
 #define	UMTX_OP_CV_BROADCAST	10
-#define	UMTX_OP_MAX		11
+#define	UMTX_OP_WAIT_UINT	11
+#define	UMTX_OP_RW_RDLOCK	12
+#define	UMTX_OP_RW_WRLOCK	13
+#define	UMTX_OP_RW_UNLOCK	14
+#define	UMTX_OP_MAX		15
 
 /* flags for UMTX_OP_CV_WAIT */
 #define UMTX_CHECK_UNPARKING	0x01
