c9ff53460ebecec1027ba62b9aad4d7a1fcd3192
David Xu <davidxu@FreeBSD.org>
2008-04-11 14:41:18 +0000

    Bump version for added umtx functions.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -57,7 +57,7 @@
  *		is created, otherwise 1.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 700104	/* Master, propagated to newvers */
+#define __FreeBSD_version 700105	/* Master, propagated to newvers */
 
 #ifndef LOCORE
 #include <sys/types.h>
