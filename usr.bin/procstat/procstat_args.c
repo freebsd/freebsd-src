c1d0314893134be0fe6a6bdba6161b3066247aff
Robert Watson <rwatson@FreeBSD.org>
2008-04-11 08:07:10 +0000

    Merge procstat_args.c:1.2, procstat_bin.c:1.2, procstat_cred.c:1.2, and procstat_files.c:1.2 from HEAD to RELENG_7:
      Add 'COMM' column to a few more output modes of procstat(1).  The only
      one it's missing from is the VM display, where there's really not room,
      and the file output display is looking quite cramped.

--- usr.bin/procstat/procstat_args.c
+++ usr.bin/procstat/procstat_args.c
@@ -28,6 +28,7 @@
 
 #include <sys/types.h>
 #include <sys/sysctl.h>
+#include <sys/user.h>
 
 #include <err.h>
 #include <errno.h>
@@ -48,7 +49,7 @@ procstat_args(pid_t pid, struct kinfo_proc *kipp)
 	char *cp;
 
 	if (!hflag)
-		printf("%5s %-70s\n", "PID", "ARGS");
+		printf("%5s %-16s %-53s\n", "PID", "COMM", "ARGS");
 
 	name[0] = CTL_KERN;
 	name[1] = KERN_PROC;
@@ -68,6 +69,7 @@ procstat_args(pid_t pid, struct kinfo_proc *kipp)
 	}
 
 	printf("%5d ", pid);
+	printf("%-16s ", kipp->ki_comm);
 	for (cp = args; cp < args + len; cp += strlen(cp) + 1)
 		printf("%s%s", cp != args ? " " : "", cp);
 	printf("\n");
