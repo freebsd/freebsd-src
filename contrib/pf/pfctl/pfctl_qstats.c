752a7b2cb0922c89cc3ea02751a3d3d6e60ba0dd
mlaier <mlaier@FreeBSD.org>
2008-04-12 18:26:48 +0000

    MFC:   Make ALTQ cope with disappearing interfaces (particularly common with mpd   and netgraph in gernal).  This also allows to add queues for an interface   that is not yet existing (you have to provide the bandwidth for the   interface, however).
      PR:		kern/106400, kern/117827
    
    Tested by:	Florian Smeets, Boris S.

--- contrib/pf/pfctl/pfctl_qstats.c
+++ contrib/pf/pfctl/pfctl_qstats.c
@@ -118,6 +118,10 @@ pfctl_show_altq(int dev, const char *iface, int opts, int verbose2)
 		for (node = root; node != NULL; node = node->next) {
 			if (iface != NULL && strcmp(node->altq.ifname, iface))
 				continue;
+#ifdef __FreeBSD__
+			if (node->altq.local_flags & PFALTQ_FLAG_IF_REMOVED)
+				continue;
+#endif
 			pfctl_print_altq_node(dev, node, 0, opts);
 		}
 	}
@@ -157,7 +161,12 @@ pfctl_update_qstats(int dev, struct pf_altq_node **root)
 			warn("DIOCGETALTQ");
 			return (-1);
 		}
+#ifdef __FreeBSD__
+		if (pa.altq.qid > 0 &&
+		    !(pa.altq.local_flags & PFALTQ_FLAG_IF_REMOVED)) {
+#else
 		if (pa.altq.qid > 0) {
+#endif
 			pq.nr = nr;
 			pq.ticket = pa.ticket;
 			pq.buf = &qstats.data;
@@ -175,6 +184,19 @@ pfctl_update_qstats(int dev, struct pf_altq_node **root)
 				pfctl_insert_altq_node(root, pa.altq, qstats);
 			}
 		}
+#ifdef __FreeBSD__
+		  else if (pa.altq.local_flags & PFALTQ_FLAG_IF_REMOVED) {
+		  	memset(&qstats.data, 0, sizeof(qstats.data));
+			if ((node = pfctl_find_altq_node(*root, pa.altq.qname,
+			    pa.altq.ifname)) != NULL) {
+				memcpy(&node->qstats.data, &qstats.data,
+				    sizeof(qstats.data));
+				update_avg(node);
+			} else {
+				pfctl_insert_altq_node(root, pa.altq, qstats);
+			}		
+		}
+#endif
 	}
 	return (mnr);
 }
@@ -280,6 +302,10 @@ pfctl_print_altq_nodestat(int dev, const struct pf_altq_node *a)
 {
 	if (a->altq.qid == 0)
 		return;
+#ifdef __FreeBSD__
+	if (a->altq.local_flags & PFALTQ_FLAG_IF_REMOVED)
+		return;
+#endif
 
 	switch (a->altq.scheduler) {
 	case ALTQT_CBQ:
