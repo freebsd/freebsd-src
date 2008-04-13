cb6449b749a5591a40dabe2ade3a66a63a0cec31
rwatson <rwatson@FreeBSD.org>
2008-04-13 21:45:52 +0000

    Make naming of include guards for MAC Framework include files more consistent with other kernel include guards (don't start with _SYS).
    MFC after:	3 days

--- sys/security/mac/mac_framework.h
+++ sys/security/mac/mac_framework.h
@@ -43,8 +43,8 @@
  * interact with the TrustedBSD MAC Framework.
  */
 
-#ifndef _SYS_SECURITY_MAC_MAC_FRAMEWORK_H_
-#define	_SYS_SECURITY_MAC_MAC_FRAMEWORK_H_
+#ifndef _SECURITY_MAC_MAC_FRAMEWORK_H_
+#define	_SECURITY_MAC_MAC_FRAMEWORK_H_
 
 #ifndef _KERNEL
 #error "no user-serviceable parts inside"
@@ -431,4 +431,4 @@ void	mac_cred_mmapped_drop_perms(struct thread *td, struct ucred *cred);
  */
 int	vop_stdsetlabel_ea(struct vop_setlabel_args *ap);
 
-#endif /* !_SYS_SECURITY_MAC_MAC_FRAMEWORK_H_ */
+#endif /* !_SECURITY_MAC_MAC_FRAMEWORK_H_ */
