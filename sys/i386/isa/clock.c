bd75233fb45773a012c3bb48a4f42730748dd54e
phk <phk@FreeBSD.org>
2008-04-14 08:00:00 +0000

    Convert amd64 and i386 to share the atrtc device driver.

--- sys/i386/isa/clock.c
+++ sys/i386/isa/clock.c
@@ -39,13 +39,6 @@ __FBSDID("$FreeBSD$");
  * Routines to handle clock hardware.
  */
 
-/*
- * inittodr, settodr and support routines written
- * by Christoph Robitschko <chmr@edvz.tu-graz.ac.at>
- *
- * reintroduced and updated by Chris Stenton <chris@gnome.co.uk> 8/10/94
- */
-
 #include "opt_apic.h"
 #include "opt_clock.h"
 #include "opt_isa.h"
@@ -100,19 +93,14 @@ u_int	i8254_freq = TIMER_FREQ;
 TUNABLE_INT("hw.i8254.freq", &i8254_freq);
 int	i8254_max_count;
 static int i8254_real_max_count;
-#define	RTC_LOCK	mtx_lock_spin(&clock_lock)
-#define	RTC_UNLOCK	mtx_unlock_spin(&clock_lock)
 
-static	struct mtx clock_lock;
+struct mtx clock_lock;
 static	struct intsrc *i8254_intsrc;
 static	u_int32_t i8254_lastcount;
 static	u_int32_t i8254_offset;
 static	int	(*i8254_pending)(struct intsrc *);
 static	int	i8254_ticked;
 static	int	using_lapic_timer;
-static	int	rtc_reg = -1;
-static	u_char	rtc_statusa = RTCSA_DIVIDER | RTCSA_NOPROF;
-static	u_char	rtc_statusb = RTCSB_24HR;
 
 /* Values for timerX_state: */
 #define	RELEASED	0
@@ -230,8 +218,10 @@ timer_spkr_setfreq(int freq)
 static int
 rtcintr(struct trapframe *frame)
 {
+	int flag = 0;
 
 	while (rtcin(RTC_INTR) & RTCIR_PERIOD) {
+		flag = 1;
 		if (profprocs != 0) {
 			if (--pscnt == 0)
 				pscnt = psdiv;
@@ -240,21 +230,8 @@ rtcintr(struct trapframe *frame)
 		if (pscnt == psdiv)
 			statclock(TRAPF_USERMODE(frame));
 	}
-	return (FILTER_HANDLED);
+	return(flag ? FILTER_HANDLED : FILTER_STRAY);
 }
-
-#include "opt_ddb.h"
-#ifdef DDB
-#include <ddb/ddb.h>
-
-DB_SHOW_COMMAND(rtc, rtc)
-{
-	printf("%02x/%02x/%02x %02x:%02x:%02x, A = %02x, B = %02x, C = %02x\n",
-	       rtcin(RTC_YEAR), rtcin(RTC_MONTH), rtcin(RTC_DAY),
-	       rtcin(RTC_HRS), rtcin(RTC_MIN), rtcin(RTC_SEC),
-	       rtcin(RTC_STATUSA), rtcin(RTC_STATUSB), rtcin(RTC_INTR));
-}
-#endif /* DDB */
 
 static int
 getit(void)
@@ -390,49 +367,6 @@ DELAY(int n)
 #endif
 }
 
-/*
- * RTC support routines
- */
-
-int
-rtcin(int reg)
-{
-	u_char val;
-
-	RTC_LOCK;
-	if (rtc_reg != reg) {
-		inb(0x84);
-		outb(IO_RTC, reg);
-		rtc_reg = reg;
-		inb(0x84);
-	}
-	val = inb(IO_RTC + 1);
-	RTC_UNLOCK;
-	return (val);
-}
-
-void
-writertc(int reg, u_char val)
-{
-
-	RTC_LOCK;
-	if (rtc_reg != reg) {
-		inb(0x84);
-		outb(IO_RTC, reg);
-		rtc_reg = reg;
-		inb(0x84);
-	}
-	outb(IO_RTC + 1, val);
-	inb(0x84);
-	RTC_UNLOCK;
-}
-
-static __inline int
-readrtc(int port)
-{
-	return(bcd2bin(rtcin(port)));
-}
-
 static void
 set_i8254_freq(u_int freq, int intr_freq)
 {
@@ -469,19 +403,6 @@ i8254_restore(void)
 	mtx_unlock_spin(&clock_lock);
 }
 
-static void
-rtc_restore(void)
-{
-
-	/* Restore all of the RTC's "status" (actually, control) registers. */
-	/* XXX locking is needed for RTC access. */
-	rtc_reg = -1;
-	writertc(RTC_STATUSB, RTCSB_24HR);
-	writertc(RTC_STATUSA, rtc_statusa);
-	writertc(RTC_STATUSB, rtc_statusb);
-	rtcin(RTC_INTR);
-}
-
 /*
  * Restore all the timers non-atomically (XXX: should be atomically).
  *
@@ -494,7 +415,7 @@ timer_restore(void)
 {
 
 	i8254_restore();		/* restore i8254_freq and hz */
-	rtc_restore();			/* reenable RTC interrupts */
+	atrtc_restore();		/* reenable RTC interrupts */
 }
 
 /* This is separate from startrtclock() so that it can be called early. */
@@ -510,8 +431,7 @@ void
 startrtclock()
 {
 
-	writertc(RTC_STATUSA, rtc_statusa);
-	writertc(RTC_STATUSB, RTCSB_24HR);
+	atrtc_start();
 
 	set_i8254_freq(i8254_freq, hz);
 	tc_init(&i8254_timecounter);
@@ -551,8 +471,7 @@ cpu_initclocks()
 	}
 
 	/* Initialize RTC. */
-	writertc(RTC_STATUSA, rtc_statusa);
-	writertc(RTC_STATUSB, RTCSB_24HR);
+	atrtc_start();
 
 	/*
 	 * If the separate statistics clock hasn't been explicility disabled
@@ -571,13 +490,10 @@ cpu_initclocks()
 		profhz = RTC_PROFRATE;
 
 		/* Enable periodic interrupts from the RTC. */
-		rtc_statusb |= RTCSB_PINTR;
 		intr_add_handler("rtc", 8,
 		    (driver_filter_t *)rtcintr, NULL, NULL,
 		    INTR_TYPE_CLK, NULL);
-
-		writertc(RTC_STATUSB, rtc_statusb);
-		rtcin(RTC_INTR);
+		atrtc_enable_intr();
 	}
 
 	init_TSC_tc();
@@ -589,8 +505,7 @@ cpu_startprofclock(void)
 
 	if (using_lapic_timer)
 		return;
-	rtc_statusa = RTCSA_DIVIDER | RTCSA_PROF;
-	writertc(RTC_STATUSA, rtc_statusa);
+	atrtc_rate(RTCSA_PROF);
 	psdiv = pscnt = psratio;
 }
 
@@ -600,8 +515,7 @@ cpu_stopprofclock(void)
 
 	if (using_lapic_timer)
 		return;
-	rtc_statusa = RTCSA_DIVIDER | RTCSA_NOPROF;
-	writertc(RTC_STATUSA, rtc_statusa);
+	atrtc_rate(RTCSA_NOPROF);
 	psdiv = pscnt = 1;
 }
 
@@ -709,161 +623,5 @@ static devclass_t attimer_devclass;
 
 DRIVER_MODULE(attimer, isa, attimer_driver, attimer_devclass, 0, 0);
 DRIVER_MODULE(attimer, acpi, attimer_driver, attimer_devclass, 0, 0);
-
-#endif /* DEV_ISA */
-
-#ifdef DEV_ISA
-
-/**********************************************************************
- * RTC driver for subr_rtc
- */
-
-#include "clock_if.h"
-
-#include <sys/rman.h>
-
-struct atrtc_softc {
-	int port_rid, intr_rid;
-	struct resource *port_res;
-	struct resource *intr_res;
-};
-
-/*
- * Attach to the ISA PnP descriptors for the timer and realtime clock.
- */
-static struct isa_pnp_id atrtc_ids[] = {
-	{ 0x000bd041 /* PNP0B00 */, "AT realtime clock" },
-	{ 0 }
-};
-
-static int
-atrtc_probe(device_t dev)
-{
-	int result;
-	
-	device_set_desc(dev, "AT Real Time Clock");
-	result = ISA_PNP_PROBE(device_get_parent(dev), dev, atrtc_ids);
-	/* ENXIO if wrong PnP-ID, ENOENT ifno PnP-ID, zero if good PnP-iD */
-	if (result != ENOENT)
-		return(result);
-	/* All PC's have an RTC, and we're hosed without it, so... */
-	return (BUS_PROBE_LOW_PRIORITY);
-}
-
-static int
-atrtc_attach(device_t dev)
-{
-	struct atrtc_softc *sc;
-
-	/*
-	 * Not that we need them or anything, but grab our resources
-	 * so they show up, correctly attributed, in the big picture.
-	 */
-	
-	sc = device_get_softc(dev);
-	if (!(sc->port_res = bus_alloc_resource(dev, SYS_RES_IOPORT,
-	    &sc->port_rid, IO_RTC, IO_RTC + 1, 2, RF_ACTIVE)))
-		device_printf(dev,"Warning: Couldn't map I/O.\n");
-	if (!(sc->intr_res = bus_alloc_resource(dev, SYS_RES_IRQ,
-	    &sc->intr_rid, 8, 8, 1, RF_ACTIVE)))
-		device_printf(dev,"Warning: Couldn't map Interrupt.\n");
-	clock_register(dev, 1000000);
-	return(0);
-}
-
-
-static int
-atrtc_settime(device_t dev __unused, struct timespec *ts)
-{
-	struct clocktime ct;
-
-	clock_ts_to_ct(ts, &ct);
-
-	/* Disable RTC updates and interrupts. */
-	writertc(RTC_STATUSB, RTCSB_HALT | RTCSB_24HR);
-
-	writertc(RTC_SEC, bin2bcd(ct.sec)); 		/* Write back Seconds */
-	writertc(RTC_MIN, bin2bcd(ct.min)); 		/* Write back Minutes */
-	writertc(RTC_HRS, bin2bcd(ct.hour));		/* Write back Hours   */
-
-	writertc(RTC_WDAY, ct.dow + 1);			/* Write back Weekday */
-	writertc(RTC_DAY, bin2bcd(ct.day));		/* Write back Day */
-	writertc(RTC_MONTH, bin2bcd(ct.mon));           /* Write back Month   */
-	writertc(RTC_YEAR, bin2bcd(ct.year % 100));	/* Write back Year    */
-#ifdef USE_RTC_CENTURY
-	writertc(RTC_CENTURY, bin2bcd(ct.year / 100));	/* ... and Century    */
-#endif
-
-	/* Reenable RTC updates and interrupts. */
-	writertc(RTC_STATUSB, rtc_statusb);
-	rtcin(RTC_INTR);
-	return (0);
-}
-
-static int
-atrtc_gettime(device_t dev, struct timespec *ts)
-{
-	struct clocktime ct;
-	int s;
-
-	/* Look if we have a RTC present and the time is valid */
-	if (!(rtcin(RTC_STATUSD) & RTCSD_PWR)) {
-		device_printf(dev, "WARNING: Battery failure indication\n");
-		return (EINVAL);
-	}
-
-	/* wait for time update to complete */
-	/* If RTCSA_TUP is zero, we have at least 244us before next update */
-	s = splhigh();
-	while (rtcin(RTC_STATUSA) & RTCSA_TUP) {
-		splx(s);
-		s = splhigh();
-	}
-	ct.nsec = 0;
-	ct.sec = readrtc(RTC_SEC);
-	ct.min = readrtc(RTC_MIN);
-	ct.hour = readrtc(RTC_HRS);
-	ct.day = readrtc(RTC_DAY);
-	ct.dow = readrtc(RTC_WDAY) - 1;
-	ct.mon = readrtc(RTC_MONTH);
-	ct.year = readrtc(RTC_YEAR);
-#ifdef USE_RTC_CENTURY
-	ct.year += readrtc(RTC_CENTURY) * 100;
-#else
-	ct.year += 2000;
-#endif
-	/* Set dow = -1 because some clocks don't set it correctly. */
-	ct.dow = -1;
-	return (clock_ct_to_ts(&ct, ts));
-}
-
-static device_method_t atrtc_methods[] = {
-	/* Device interface */
-	DEVMETHOD(device_probe,		atrtc_probe),
-	DEVMETHOD(device_attach,	atrtc_attach),
-	DEVMETHOD(device_detach,	bus_generic_detach),
-	DEVMETHOD(device_shutdown,	bus_generic_shutdown),
-	DEVMETHOD(device_suspend,	bus_generic_suspend),
-		/* XXX stop statclock? */
-	DEVMETHOD(device_resume,	bus_generic_resume),
-		/* XXX restart statclock? */
-
-	/* clock interface */
-	DEVMETHOD(clock_gettime,	atrtc_gettime),
-	DEVMETHOD(clock_settime,	atrtc_settime),
-
-	{ 0, 0 }
-};
-
-static driver_t atrtc_driver = {
-	"atrtc",
-	atrtc_methods,
-	sizeof(struct atrtc_softc),
-};
-
-static devclass_t atrtc_devclass;
-
-DRIVER_MODULE(atrtc, isa, atrtc_driver, atrtc_devclass, 0, 0);
-DRIVER_MODULE(atrtc, acpi, atrtc_driver, atrtc_devclass, 0, 0);
 
 #endif /* DEV_ISA */
