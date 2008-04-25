c8b0048ac3ed9aff3bbafe792b35cbb126341cd0
rwatson <rwatson@FreeBSD.org>
2008-04-25 17:34:09 +0000

    Add "ddb capture print" and "ddb capture status" commands do ddb(8), alowing the DDB output capture buffer to be easily extracted from user space.  Both of these commands include -M/-N arguments, allowing them to be used with kernel crash dumps (or /dev/mem).
    This makes it easier to use DDB scripting and output capture with
    minidumps or full dumps rather than with text dumps, allowing DDB
    output (scripted or otherwise) to be easily extracted from a crash
    dump.
    
    MFC after:	1 week
    Discussed with:	brooks, jhb

--- sbin/ddb/ddb.h
+++ sbin/ddb/ddb.h
@@ -29,6 +29,7 @@
 #ifndef DDB_H
 #define	DDB_H
 
+void	ddb_capture(int argc, char *argv[]);
 void	ddb_script(int argc, char *argv[]);
 void	ddb_scripts(int argc, char *argv[]);
 void	ddb_unscript(int argc, char *argv[]);
