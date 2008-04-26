68aac0cfdb8f09d2968846dc0f71c93c126df7d4
jb <jb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-04-26 04:11:16 +0000

    Simplify for FreeBSD for now, bypassing the multibyte char variables that Solaris has. We may need to revisit this issue later.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@178549 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- cddl/contrib/opensolaris/lib/libgen/common/gmatch.c
+++ cddl/contrib/opensolaris/lib/libgen/common/gmatch.c
@@ -32,15 +32,28 @@
 
 /*LINTLIBRARY*/
 
+#if defined(sun)
 #pragma weak gmatch = _gmatch
+#endif
 
+#if defined(sun)
 #include "gen_synonyms.h"
+#endif
 #include <sys/types.h>
 #include <libgen.h>
 #include <stdlib.h>
 #include <limits.h>
+#if defined(sun)
 #include <widec.h>
 #include "_range.h"
+#else
+/* DOODAD */ static int multibyte = 0;
+#define WCHAR_CSMASK    0x30000000
+#define valid_range(c1, c2) \
+    (((c1) & WCHAR_CSMASK) == ((c2) & WCHAR_CSMASK) && \
+    ((c1) > 0xff || !iscntrl((int)c1)) && ((c2) > 0xff || \
+    !iscntrl((int)c2)))
+#endif
 
 #define	Popwchar(p, c) \
 	n = mbtowc(&cl, p, MB_LEN_MAX); \
