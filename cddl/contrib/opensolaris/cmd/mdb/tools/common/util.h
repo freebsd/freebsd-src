4f3d158a74433c3c024dafc69833c832df533791
jb <jb@FreeBSD.org>
2008-04-26 03:47:34 +0000

    Apply FreeBSD changes.

--- cddl/contrib/opensolaris/cmd/mdb/tools/common/util.h
+++ cddl/contrib/opensolaris/cmd/mdb/tools/common/util.h
@@ -40,7 +40,9 @@ extern int findelfsecidx(Elf *, char *);
 extern void die(char *, ...);
 extern void elfdie(char *, ...);
 
+#if defined(sun)
 extern const char *progname;
+#endif
 
 #ifdef __cplusplus
 }
