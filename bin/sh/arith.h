39376f45e298ec9d397f9a7825d9cfb926eb3cae
Stefan Farfeleder <stefanf@FreeBSD.org>
2008-04-27 20:46:45 +0000

    - Fix bugs where the value of arithmetic expansion$((...)) was trucated   to type int. - Change the type used for arithmetic expansion to intmax_t (ie. 64 bit on all   currently supported FreeBSD architectures).  SUSv3 requires at least type   long but allows for larger types.  Other shells (eg. bash, zsh, NetBSD's sh)   do that too.
    PR:		122659
    Submitted by:	Jaakko Heinonen (minor modifications by me)

--- bin/sh/arith.h
+++ bin/sh/arith.h
@@ -30,8 +30,12 @@
  * $FreeBSD$
  */
 
+#include "shell.h"
+
+#define DIGITS(var) (3 + (2 + CHAR_BIT * sizeof((var))) / 3)
+
 extern char *arith_buf, *arith_startbuf;
 
-int arith(char *);
+arith_t arith(char *);
 void arith_lex_reset(void);
 int expcmd(int, char **);
