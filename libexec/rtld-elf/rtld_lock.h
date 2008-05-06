483cd501ba87e1837bb0c83a5ce558f0d0141bf5
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-05-06 09:27:41 +0000

    Fix the problem with the C++ exception handling for the multithreaded programs.
    From the PR description:
    The gcc runtime's _Unwind_Find_FDE function, invoked during exception
    handling's stack unwinding, is not safe to execute from within multiple
    threads. FreeBSD' s dl_iterate_phdr() however permits multiple threads
    to pass through it though. The result is surprisingly reliable infinite
    looping of one or more threads if they just happen to be unwinding at
    the same time.
    
    Introduce the new lock that is write locked around the dl_iterate_pdr,
    thus providing required exclusion for the stack unwinders.
    
    PR:	threads/123062
    Submitted by:	Andy Newman <an at atrn org>
    Reviewed by:	kan
    MFC after:	2 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@178807 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- libexec/rtld-elf/rtld_lock.h
+++ libexec/rtld-elf/rtld_lock.h
@@ -52,6 +52,7 @@ typedef struct rtld_lock *rtld_lock_t;
 
 extern rtld_lock_t	rtld_bind_lock;
 extern rtld_lock_t	rtld_libc_lock;
+extern rtld_lock_t	rtld_phdr_lock;
 
 int	rlock_acquire(rtld_lock_t);
 int 	wlock_acquire(rtld_lock_t);
