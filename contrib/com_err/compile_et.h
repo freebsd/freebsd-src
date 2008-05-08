fcd2e558589bbcf7b8ece36338ff0b5d791b9145
Doug Rabson <dfr@FreeBSD.org>
2008-05-08 10:58:50 +0000

    Import com_err from heimdal-1.1

--- contrib/com_err/compile_et.h
+++ contrib/com_err/compile_et.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE. 
  */
 
-/* $Id: compile_et.h,v 1.6 2000/07/01 20:21:48 assar Exp $ */
+/* $Id: compile_et.h 15426 2005-06-16 19:21:42Z lha $ */
 
 #ifndef __COMPILE_ET_H__
 #define __COMPILE_ET_H__
@@ -40,6 +40,7 @@
 #include <config.h>
 #endif
 
+#include <err.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -47,7 +48,7 @@
 #include <ctype.h>
 #include <roken.h>
 
-extern long base;
+extern long base_id;
 extern int number;
 extern char *prefix;
 extern char name[128];
