a384947e923445d99aafa1388b79a761afc6bb75
Peter Grehan <grehan@FreeBSD.org>
2008-05-08 17:55:44 +0000

    Fix panic and breakage for non-DMA ATA devices e.g. powermac macio cells. Handle cases where dma function pointers may be NULL, and where the max_iosize can't be derived from a DMA data structure. For the latter, revert to the prior behaviour of using DFLTPHYS for the max i/o size when there is no other data.
    Reviewed by:		marcel
    No objection by:	sos

--- sys/dev/ata/atapi-cd.c
+++ sys/dev/ata/atapi-cd.c
@@ -906,8 +906,11 @@ acd_set_ioparm(device_t dev)
 {
     struct ata_channel *ch = device_get_softc(device_get_parent(dev));
     struct acd_softc *cdp = device_get_ivars(dev);
+    uint32_t max_iosize;
 
-    cdp->iomax = min(ch->dma.max_iosize, 65534);
+    max_iosize = ch->dma.max_iosize ? ch->dma.max_iosize : DFLTPHYS;
+
+    cdp->iomax = min(max_iosize, 65534);
 }
 
 static void 
