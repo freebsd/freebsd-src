0b165a02404045fabb5ae6dd3ab8de8e9a4a24b6
alc <alc@FreeBSD.org>
2008-05-09 23:31:42 +0000

    Add a stub for pmap_align_superpage() on machines that don't (yet) implement pmap-level support for superpages.

--- sys/mips/mips/pmap.c
+++ sys/mips/mips/pmap.c
@@ -2851,6 +2851,16 @@ pmap_addr_hint(vm_object_t obj, vm_offset_t addr, vm_size_t size)
 	return addr;
 }
 
+/*
+ *	Increase the starting virtual address of the given mapping if a
+ *	different alignment might result in more superpage mappings.
+ */
+void
+pmap_align_superpage(vm_object_t object, vm_ooffset_t offset,
+    vm_offset_t *addr, vm_size_t size)
+{
+}
+
 int pmap_pid_dump(int pid);
 
 int
