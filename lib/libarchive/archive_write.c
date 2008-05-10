b3a8f0107f978b12cfcff4710d3a6ea9dd121804
kientzle <kientzle@FreeBSD.org>
2008-05-10 06:49:13 +0000

    MFC: Stop using ARCHIVE_DEFAULT_BYTES_PER_BLOCK to set the default block size.  The default setting here is arbitrary and shouldn't be published.  Someday, we should be smarter about the default here.

--- lib/libarchive/archive_write.c
+++ lib/libarchive/archive_write.c
@@ -97,7 +97,12 @@ archive_write_new(void)
 	a->archive.magic = ARCHIVE_WRITE_MAGIC;
 	a->archive.state = ARCHIVE_STATE_NEW;
 	a->archive.vtable = archive_write_vtable();
-	a->bytes_per_block = ARCHIVE_DEFAULT_BYTES_PER_BLOCK;
+	/*
+	 * The value 10240 here matches the traditional tar default,
+	 * but is otherwise arbitrary.
+	 * TODO: Set the default block size from the format selected.
+	 */
+	a->bytes_per_block = 10240;
 	a->bytes_in_last_block = -1;	/* Default */
 
 	/* Initialize a block of nulls for padding purposes. */
