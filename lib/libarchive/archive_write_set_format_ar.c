975f28bca1bb506fd04a4a3e23f9300e4108b6e2
Tim Kientzle <kientzle@FreeBSD.org>
2008-05-10 06:57:04 +0000

    MFC: Set the archive format code and name in the generic portion of the archive structure, not the write-specific portion. (This is a bungle from back when the generic and write-specific portions were first separated and the write-specific part accidentally ended up with copies of some of the generic fields.)  In particular, archive_format() and archive_format_name() now work correctly on write handles.

--- lib/libarchive/archive_write_set_format_ar.c
+++ lib/libarchive/archive_write_set_format_ar.c
@@ -87,8 +87,8 @@ archive_write_set_format_ar_bsd(struct archive *_a)
 	struct archive_write *a = (struct archive_write *)_a;
 	int r = archive_write_set_format_ar(a);
 	if (r == ARCHIVE_OK) {
-		a->archive_format = ARCHIVE_FORMAT_AR_BSD;
-		a->archive_format_name = "ar (BSD)";
+		a->archive.archive_format = ARCHIVE_FORMAT_AR_BSD;
+		a->archive.archive_format_name = "ar (BSD)";
 	}
 	return (r);
 }
@@ -99,8 +99,8 @@ archive_write_set_format_ar_svr4(struct archive *_a)
 	struct archive_write *a = (struct archive_write *)_a;
 	int r = archive_write_set_format_ar(a);
 	if (r == ARCHIVE_OK) {
-		a->archive_format = ARCHIVE_FORMAT_AR_GNU;
-		a->archive_format_name = "ar (GNU/SVR4)";
+		a->archive.archive_format = ARCHIVE_FORMAT_AR_GNU;
+		a->archive.archive_format_name = "ar (GNU/SVR4)";
 	}
 	return (r);
 }
@@ -204,7 +204,7 @@ archive_write_ar_header(struct archive_write *a, struct archive_entry *entry)
 		return (ARCHIVE_WARN);
 	}
 
-	if (a->archive_format == ARCHIVE_FORMAT_AR_GNU) {
+	if (a->archive.archive_format == ARCHIVE_FORMAT_AR_GNU) {
 		/*
 		 * SVR4/GNU variant use a "/" to mark then end of the filename,
 		 * make it possible to have embedded spaces in the filename.
@@ -261,7 +261,7 @@ archive_write_ar_header(struct archive_write *a, struct archive_entry *entry)
 				return (ARCHIVE_WARN);
 			}
 		}
-	} else if (a->archive_format == ARCHIVE_FORMAT_AR_BSD) {
+	} else if (a->archive.archive_format == ARCHIVE_FORMAT_AR_BSD) {
 		/*
 		 * BSD variant: for any file name which is more than
 		 * 16 chars or contains one or more embedded space(s), the
