0482108fde76a2ed53280113dfc3b509471a53b5
alc <alc@FreeBSD.org>
2008-05-10 21:46:20 +0000

    Introduce a new parameter "superpage_align" to kmem_suballoc() that is used to request superpage alignment for the submap.
    Request superpage alignment for the kmem_map.
    
    Pass VMFS_ANY_SPACE instead of TRUE to vm_map_find().  (They are currently
    equivalent but VMFS_ANY_SPACE is the new preferred spelling.)
    
    Remove a stale comment from kmem_malloc().

--- sys/vm/vm_extern.h
+++ sys/vm/vm_extern.h
@@ -63,7 +63,8 @@ void kmem_free(vm_map_t, vm_offset_t, vm_size_t);
 void kmem_free_wakeup(vm_map_t, vm_offset_t, vm_size_t);
 void kmem_init(vm_offset_t, vm_offset_t);
 vm_offset_t kmem_malloc(vm_map_t, vm_size_t, boolean_t);
-vm_map_t kmem_suballoc(vm_map_t, vm_offset_t *, vm_offset_t *, vm_size_t);
+vm_map_t kmem_suballoc(vm_map_t, vm_offset_t *, vm_offset_t *, vm_size_t,
+    boolean_t);
 void swapout_procs(int);
 int useracc(void *, int, int);
 int vm_fault(vm_map_t, vm_offset_t, vm_prot_t, int);
