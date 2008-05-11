0ecc8d7f379e40679064d7a8858910f20a7da485
alc <alc@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-05-11 20:33:47 +0000

    Correct an error in pmap_align_superpage().  Specifically, correctly handle the case where the mapping is greater than a superpage in size but the alignment of the physical pages spans a superpage boundary.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@178947 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/i386/pmap.c
+++ sys/i386/i386/pmap.c
@@ -4608,11 +4608,12 @@ pmap_align_superpage(vm_object_t object, vm_ooffset_t offset,
 {
 	vm_offset_t superpage_offset;
 
+	if (size < NBPDR)
+		return;
 	if (object != NULL && (object->flags & OBJ_COLORED) != 0)
 		offset += ptoa(object->pg_color);
 	superpage_offset = offset & PDRMASK;
-	if (size < superpage_offset ||
-	    size - superpage_offset < NBPDR ||
+	if (size - ((NBPDR - superpage_offset) & PDRMASK) < NBPDR ||
 	    (*addr & PDRMASK) == superpage_offset)
 		return;
 	if ((*addr & PDRMASK) < superpage_offset)
