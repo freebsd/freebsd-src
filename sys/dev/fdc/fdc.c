b3d6d995b7742d6386fde72787097364e5a472a1
kib <kib@FreeBSD.org>
2008-05-16 10:32:52 +0000

    MFC rev. 1.320:
    The wakeup() line from the rev. 1.319 is wrong and reintroduces
    a panic race on module unload. The wakeup() is internal to
    kproc_exit/kthread_exit. The correct fix is to fix the msleep() in
    detach to sleep on fdc->fdc_thread instead of &fdc->fdc_thread.
    
    MFC rev. 1.319:
    Wakeup the thread doing the fdc_detach() when the fdc worker thread exits [1].
    Write access to the write-protected floppy shall call device_unbusy() to
    pair the device_busy() in the fd_access() [2].
    
    PR:	116537 [1], 116539 [2]

--- sys/dev/fdc/fdc.c
+++ sys/dev/fdc/fdc.c
@@ -1373,6 +1373,7 @@ fd_access(struct g_provider *pp, int r, int w, int e)
 	struct fd_data *fd;
 	struct fdc_data *fdc;
 	int ar, aw, ae;
+	int busy;
 
 	fd = pp->geom->softc;
 	fdc = fd->fdc;
@@ -1393,6 +1394,7 @@ fd_access(struct g_provider *pp, int r, int w, int e)
 		return (0);
 	}
 
+	busy = 0;
 	if (pp->acr == 0 && pp->acw == 0 && pp->ace == 0) {
 		if (fdmisccmd(fd, BIO_PROBE, NULL))
 			return (ENXIO);
@@ -1405,10 +1407,14 @@ fd_access(struct g_provider *pp, int r, int w, int e)
 			mtx_unlock(&fdc->fdc_mtx);
 		}
 		device_busy(fd->dev);
+		busy = 1;
 	}
 
-	if (w > 0 && (fd->flags & FD_WP))
+	if (w > 0 && (fd->flags & FD_WP)) {
+		if (busy)
+			device_unbusy(fd->dev);
 		return (EROFS);
+	}
 
 	pp->sectorsize = fd->sectorsize;
 	pp->stripesize = fd->ft->heads * fd->ft->sectrac * fd->sectorsize;
@@ -1703,7 +1709,7 @@ fdc_detach(device_t dev)
 	fdc->flags |= FDC_KTHREAD_EXIT;
 	wakeup(&fdc->head);
 	while ((fdc->flags & FDC_KTHREAD_ALIVE) != 0)
-		msleep(&fdc->fdc_thread, &fdc->fdc_mtx, PRIBIO, "fdcdet", 0);
+		msleep(fdc->fdc_thread, &fdc->fdc_mtx, PRIBIO, "fdcdet", 0);
 	mtx_unlock(&fdc->fdc_mtx);
 
 	/* reset controller, turn motor off */
