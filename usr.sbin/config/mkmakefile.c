2fda3a0dfb44373142070ff3caa1f5f979b406a5
jb <jb@FreeBSD.org>
2008-05-23 03:55:26 +0000

    Add the CTF conversion to the generated makefile. In the case where NO_CTF or !WITH_CTF, the macro is empty.

--- usr.sbin/config/mkmakefile.c
+++ usr.sbin/config/mkmakefile.c
@@ -735,7 +735,7 @@ do_rules(FILE *f)
 				printf("config: don't know rules for %s\n", np);
 				break;
 			}
-			snprintf(cmd, sizeof(cmd), "${%s_%c%s}", ftype,
+			snprintf(cmd, sizeof(cmd), "${%s_%c%s}; ${NORMAL_CTFCONVERT}", ftype,
 			    toupper(och),
 			    ftp->f_flags & NOWERROR ? "_NOWERROR" : "");
 			compilewith = cmd;
