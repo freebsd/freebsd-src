3f3146038ef79b71b0281bff643c34717142b78b
mckusick <mckusick@FreeBSD.org>
2008-05-23 23:13:14 +0000

    When using dump to generate level 0 dumps which are then rsync'ed to a remote machine, the fact that the dump date is stored with each header (inode) record makes rsync significantly less efficient than necessary. This also applies to inode access times when they are not important data to retain. When implementing an offsite backup solution of this type, these dates in particular are not important, especially if it prevents effective offsite backups.
    PR:           bin/91049
    Submitted by: Forrest W Christian <fwc@mt.net>

--- sbin/dump/traverse.c
+++ sbin/dump/traverse.c
@@ -842,6 +842,11 @@ writeheader(ino_t ino)
 {
 	int32_t sum, cnt, *lp;
 
+	if (rsync_friendly >= 2) {
+		/* don't track changes to access time */
+		spcl.c_atime = spcl.c_mtime;
+		spcl.c_atimensec = spcl.c_mtimensec;
+	}
 	spcl.c_inumber = ino;
 	spcl.c_magic = FS_UFS2_MAGIC;
 	spcl.c_checksum = 0;
