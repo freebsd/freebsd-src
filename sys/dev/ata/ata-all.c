c306626136026c939f3c29555b5789be3dc1c3c0
bz <bz@FreeBSD.org>
2008-05-24 10:15:20 +0000

    MFC: rev. 1.287 ata-all.c
      devclass_get_maxunit() returns n+1 with n starting at 0.
      So if we have channel 0..3  devclass_get_maxunit is 4.
    
      It's never been a problem as devclass_get_device() has
      caught any possibly bad input.

--- sys/dev/ata/ata-all.c
+++ sys/dev/ata/ata-all.c
@@ -363,12 +363,13 @@ ata_ioctl(struct cdev *dev, u_long cmd, caddr_t data,
 
     switch (cmd) {
     case IOCATAGMAXCHANNEL:
+	/* In case we have channel 0..n this will return n+1. */
 	*value = devclass_get_maxunit(ata_devclass);
 	error = 0;
 	break;
 
     case IOCATAREINIT:
-	if (*value > devclass_get_maxunit(ata_devclass) ||
+	if (*value >= devclass_get_maxunit(ata_devclass) ||
 	    !(device = devclass_get_device(ata_devclass, *value)))
 	    return ENXIO;
 	error = ata_reinit(device);
@@ -376,7 +377,7 @@ ata_ioctl(struct cdev *dev, u_long cmd, caddr_t data,
 	break;
 
     case IOCATAATTACH:
-	if (*value > devclass_get_maxunit(ata_devclass) ||
+	if (*value >= devclass_get_maxunit(ata_devclass) ||
 	    !(device = devclass_get_device(ata_devclass, *value)))
 	    return ENXIO;
 	/* XXX SOS should enable channel HW on controller */
@@ -384,7 +385,7 @@ ata_ioctl(struct cdev *dev, u_long cmd, caddr_t data,
 	break;
 
     case IOCATADETACH:
-	if (*value > devclass_get_maxunit(ata_devclass) ||
+	if (*value >= devclass_get_maxunit(ata_devclass) ||
 	    !(device = devclass_get_device(ata_devclass, *value)))
 	    return ENXIO;
 	error = ata_detach(device);
@@ -392,7 +393,7 @@ ata_ioctl(struct cdev *dev, u_long cmd, caddr_t data,
 	break;
 
     case IOCATADEVICES:
-	if (devices->channel > devclass_get_maxunit(ata_devclass) ||
+	if (devices->channel >= devclass_get_maxunit(ata_devclass) ||
 	    !(device = devclass_get_device(ata_devclass, devices->channel)))
 	    return ENXIO;
 	bzero(devices->name[0], 32);
