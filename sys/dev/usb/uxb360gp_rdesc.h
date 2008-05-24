aa3f38292e9d8cd408717b9ecdaf81c7513f15b1
ed <ed@FreeBSD.org>
2008-05-24 18:35:55 +0000

    Change my email address to the one from the FreeBSD project.
    Approved by:	philip (mentor)

--- sys/dev/usb/uxb360gp_rdesc.h
+++ sys/dev/usb/uxb360gp_rdesc.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2005 Ed Schouten <ed@fxq.nl>
+ * Copyright (c) 2005 Ed Schouten <ed@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
