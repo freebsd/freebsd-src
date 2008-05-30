cd7d66a21f1a9cc8192126b05cecdd8cf819964a
Doug Rabson <dfr@FreeBSD.org>
2008-05-30 14:47:42 +0000

    Call the fcntl compatiblity wrapper from the thread library fcntl wrappers so that they get the benefit of the (limited) forward ABI compatibility.
    MFC after: 1 week

--- lib/libthr/thread/thr_syscalls.c
+++ lib/libthr/thread/thr_syscalls.c
@@ -132,6 +132,7 @@ int	__aio_suspend(const struct aiocb * const iocbs[], int,
 int	__close(int);
 int	__connect(int, const struct sockaddr *, socklen_t);
 int	__fcntl(int, int,...);
+extern int __fcntl_compat(int, int,...);
 int	__fsync(int);
 int	__msync(void *, size_t, int);
 int	__nanosleep(const struct timespec *, struct timespec *);
@@ -252,7 +253,7 @@ __fcntl(int fd, int cmd,...)
 		ret = __sys_fcntl(fd, cmd);
 		break;
 	default:
-		ret = __sys_fcntl(fd, cmd, va_arg(ap, void *));
+		ret = __fcntl_compat(fd, cmd, va_arg(ap, void *));
 	}
 	va_end(ap);
 
