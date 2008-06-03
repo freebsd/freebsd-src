5ed35ea881beb1b056e4a2a3742b290d26cbc8ad
rdivacky <rdivacky@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-06-03 17:50:13 +0000

    Switch to emulating Linux 2.6 on default.
    Approved by:	kib (mentor)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@179523 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/compat/linux/linux_mib.c
+++ sys/compat/linux/linux_mib.c
@@ -82,8 +82,8 @@ SYSCTL_PROC(_compat_linux, OID_AUTO, osname,
 	    0, 0, linux_sysctl_osname, "A",
 	    "Linux kernel OS name");
 
-static char	linux_osrelease[LINUX_MAX_UTSNAME] = "2.4.2";
-static int	linux_use_linux26 = 0;
+static char	linux_osrelease[LINUX_MAX_UTSNAME] = "2.6.16";
+static int	linux_use_linux26 = 1;
 
 static int
 linux_sysctl_osrelease(SYSCTL_HANDLER_ARGS)
