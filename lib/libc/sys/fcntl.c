6cb47bc7b47c571a809f935e7deaab3bb2c31f86
dfr <dfr@FreeBSD.org>
2008-06-06 09:13:43 +0000

    MFC: r179358,179434 - make libc's fcntl symbol weak and extend the limited forward ABI compatibility for fcntl to the thread libraries.

--- lib/libc/sys/fcntl.c
+++ lib/libc/sys/fcntl.c
@@ -34,8 +34,10 @@ __FBSDID("$FreeBSD$");
 #include <sys/syscall.h>
 #include "libc_private.h"
 
+__weak_reference(__fcntl_compat, fcntl);
+
 int
-fcntl(int fd, int cmd, ...)
+__fcntl_compat(int fd, int cmd, ...)
 {
 	va_list args;
 	long arg;
