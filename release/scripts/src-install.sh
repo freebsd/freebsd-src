896eafd957e06e9f5c0c3aecbc7ac41fbecc606a
Ruslan Ermilov <ru@FreeBSD.org>
2008-06-25 20:29:22 +0000

    src/compat/ is gone back in March.
    Reported by:	Mars G Miro

--- release/scripts/src-install.sh
+++ release/scripts/src-install.sh
@@ -11,7 +11,7 @@ if [ $# -lt 1 ]; then
 	echo "You must specify which components of src to extract"
 	echo "possible subcomponents are:"
 	echo
-	echo "base bin cddl compat contrib crypto etc games gnu include krb5"
+	echo "base bin cddl contrib crypto etc games gnu include krb5"
 	echo "lib libexec release rescue sbin secure share sys tools ubin"
 	echo "usbin"
 	echo
@@ -20,7 +20,7 @@ if [ $# -lt 1 ]; then
 fi
 
 if [ "$1" = "all" ]; then
-	dists="base bin cddl compat contrib crypto etc games gnu include krb5 lib libexec release rescue sbin secure share sys tools ubin usbin"
+	dists="base bin cddl contrib crypto etc games gnu include krb5 lib libexec release rescue sbin secure share sys tools ubin usbin"
 else
 	dists="$*"
 fi
