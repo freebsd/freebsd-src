467b62b6bff4cc75f18513bcb674cbabbb14fc3b
John Baldwin <jhb@FreeBSD.org>
2008-06-27 04:46:06 +0000

    MFC: Add the mpt_user personality to mpt(4).

--- sys/dev/mpt/mpt.h
+++ sys/dev/mpt/mpt.h
@@ -766,6 +766,9 @@ struct mpt_softc {
 	/* Shutdown Event Handler. */
 	eventhandler_tag         eh;
 
+	/* Userland management interface. */
+	struct cdev		*cdev;
+
 	TAILQ_ENTRY(mpt_softc)	links;
 };
 
