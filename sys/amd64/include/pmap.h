db28369d246ff600cbd9c7e9553e0b1d5bc0c607
alc <alc@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-06-29 19:13:27 +0000

    Strictly speaking, the definition of VM_MAX_KERNEL_ADDRESS is wrong.  However, in practice, the error (currently) makes no difference because the computation performed by KVADDR() hides the error.  This revision fixes the error.
    Also, eliminate a (now) unused definition.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@180101 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/amd64/include/pmap.h
+++ sys/amd64/include/pmap.h
@@ -116,7 +116,6 @@
 
 #define NKPML4E		1		/* number of kernel PML4 slots */
 #define NKPDPE		5		/* number of kernel PDP slots */
-#define	NKPDE		(NKPDPE*NPDEPG)	/* number of kernel PD slots */
 
 #define	NUPML4E		(NPML4EPG/2)	/* number of userland PML4 pages */
 #define	NUPDPE		(NUPML4E*NPDPEPG)/* number of userland PDP pages */
