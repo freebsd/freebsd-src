4ab24f900d73c161df7456444a10d86b44244520
alc <alc@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-06-30 03:14:39 +0000

    Document the layout of the address space, borrowing heavily from http://lists.freebsd.org/pipermail/freebsd-amd64/2005-July/005578.html
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@180109 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/amd64/include/vmparam.h
+++ sys/amd64/include/vmparam.h
@@ -148,8 +148,18 @@
 /*
  * Virtual addresses of things.  Derived from the page directory and
  * page table indexes from pmap.h for precision.
- * Because of the page that is both a PD and PT, it looks a little
- * messy at times, but hey, we'll do anything to save a page :-)
+ *
+ * 0x0000000000000000 - 0x00007fffffffffff   user map
+ * 0x0000800000000000 - 0xffff7fffffffffff   does not exist (hole)
+ * 0xffff800000000000 - 0xffff804020100fff   recursive page table (512GB slot)
+ * 0xffff804020101000 - 0xfffffeffffffffff   unused
+ * 0xffffff0000000000 - 0xffffff7fffffffff   512GB direct map mappings
+ * 0xffffff8000000000 - 0xfffffffe7fffffff   unused (506GB)
+ * 0xfffffffe80000000 - 0xffffffffffffffff   6GB kernel map
+ *
+ * Within the kernel map:
+ *
+ * 0xffffffff80000000                        KERNBASE
  */
 
 #define	VM_MAX_KERNEL_ADDRESS	KVADDR(KPML4I, NPDPEPG-1, NPDEPG-1, NPTEPG-1)
