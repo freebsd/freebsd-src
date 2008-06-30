bc461663db7d668044bb6e087de4bd10ac003cf6
rwatson <rwatson@FreeBSD.org>
2008-06-30 22:19:18 +0000

    Merge extattr.h:1.22, ufs_extattr.c:1.91 (r176797) from head to stable/7:
      Continue on-going campaign to replace lockmgr locks with sx locks where
      the specific semantics of lockmgr aren't required: update UFS1 extended
      attributes to protect its data structures using an sx lock.
    
      While here, update comments on lock granularity.

--- sys/ufs/ufs/extattr.h
+++ sys/ufs/ufs/extattr.h
@@ -113,6 +113,8 @@ struct extattr {
 
 #ifdef _KERNEL
 
+#include <sys/_sx.h>
+
 #ifdef MALLOC_DECLARE
 MALLOC_DECLARE(M_EXTATTR);
 #endif
@@ -127,10 +129,9 @@ struct ufs_extattr_list_entry {
 	struct vnode	*uele_backing_vnode;
 };
 
-struct lock;
 struct ucred;
 struct ufs_extattr_per_mount {
-	struct lock	uepm_lock;
+	struct sx	uepm_lock;
 	struct ufs_extattr_list_head	uepm_list;
 	struct ucred	*uepm_ucred;
 	int	uepm_flags;
