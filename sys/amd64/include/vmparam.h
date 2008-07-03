62c08b5fc9a26662a51ba0172b4d2c276ce39a61
alc <alc@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-07-03 04:53:14 +0000

    Increase the ceiling on the kmem map's size to 3.6GB.  Also, define the ceiling as a fraction of the kernel map's size rather than an absolute quantity.  Thus, scaling of the kmem map's size will be automatic with changes to the kernel map's size.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@180210 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/amd64/include/vmparam.h
+++ sys/amd64/include/vmparam.h
@@ -202,7 +202,8 @@
  * Ceiling on amount of kmem_map kva space.
  */
 #ifndef VM_KMEM_SIZE_MAX
-#define	VM_KMEM_SIZE_MAX	(400 * 1024 * 1024)
+#define	VM_KMEM_SIZE_MAX	((VM_MAX_KERNEL_ADDRESS - \
+    VM_MIN_KERNEL_ADDRESS + 1) * 3 / 5)
 #endif
 
 /* initial pagein size of beginning of executable file */
