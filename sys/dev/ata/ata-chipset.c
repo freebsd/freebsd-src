1fe3b8c18cc5acec37c792de1624b2c1a7c1266d
remko <remko@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-07-09 15:06:41 +0000

    Add support for the ICH9 in non AHCI mode (RAID mode).
    Submitted by:	Andrey V. Elsukov <bu7cher at yandex dot ru>
    Tested by:	Vitalij L. Fadeev <fvl at mail dot ru>
    Approved by:	imp (mentor, implicit)
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@180382 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ata/ata-chipset.c
+++ sys/dev/ata/ata-chipset.c
@@ -2136,6 +2136,7 @@ ata_intel_ident(device_t dev)
      { ATA_I82801IB_AH2, 0, AHCI, 0x00, ATA_SA300, "ICH9" },
      { ATA_I82801IB_AH4, 0, AHCI, 0x00, ATA_SA300, "ICH9" },
      { ATA_I82801IB_AH6, 0, AHCI, 0x00, ATA_SA300, "ICH9" },
+     { ATA_I82801IB_R1,  0, AHCI, 0x00, ATA_SA300, "ICH9" },
      { ATA_I31244,       0,    0, 0x00, ATA_SA150, "31244" },
      { 0, 0, 0, 0, 0, 0}};
 
