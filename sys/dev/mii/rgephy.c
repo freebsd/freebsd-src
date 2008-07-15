891ecb92676d8be84f5c73659585d6c7f0a1d93a
yongari <yongari@FreeBSD.org>
2008-07-15 02:04:31 +0000

    MFC r180178.   Add RTL8211C(L) support.   Disable advanced link-down power saving in phy reset.

--- sys/dev/mii/rgephy.c
+++ sys/dev/mii/rgephy.c
@@ -34,7 +34,7 @@
 __FBSDID("$FreeBSD$");
 
 /*
- * Driver for the RealTek 8169S/8110S/8211B internal 10/100/1000 PHY.
+ * Driver for the RealTek 8169S/8110S/8211B/8211C internal 10/100/1000 PHY.
  */
 
 #include <sys/param.h>
@@ -532,6 +532,18 @@ rgephy_load_dspcode(struct mii_softc *sc)
 static void
 rgephy_reset(struct mii_softc *sc)
 {
+	struct rgephy_softc *rsc;
+	uint16_t ssr;
+
+	rsc = (struct rgephy_softc *)sc;
+	if (rsc->mii_revision == 3) {
+		/* RTL8211C(L) */
+		ssr = PHY_READ(sc, RGEPHY_MII_SSR);
+		if ((ssr & RGEPHY_SSR_ALDPS) != 0) {
+			ssr &= ~RGEPHY_SSR_ALDPS;
+			PHY_WRITE(sc, RGEPHY_MII_SSR, ssr);
+		}
+	}
 
 	mii_phy_reset(sc);
 	DELAY(1000);
