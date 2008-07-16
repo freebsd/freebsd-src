edbfc4f6f49821ffa7ce898f0b141b6ed2ed9813
ed <ed@FreeBSD.org>
2008-07-16 11:23:15 +0000

    Sort the ioctl's in <sys/ttycom.h> by number.
    I think one of the reasons why we have so many conflicts in the TTY
    ioctl category, is because the ioctl's aren't ordered logically. This
    commit only sorts them by number. The comments may still be inaccurate.
    
    Approved by:	philip (mentor)

--- sys/sys/ttycom.h
+++ sys/sys/ttycom.h
@@ -74,31 +74,29 @@ struct winsize {
 #define	TIOCGETD	_IOR('t', 26, int)	/* get line discipline */
 #define	TIOCSETD	_IOW('t', 27, int)	/* set line discipline */
 						/* 28-69 free */
-						/* 127-124 compat */
-#define	TIOCSBRK	 _IO('t', 123)		/* set break bit */
-#define	TIOCCBRK	 _IO('t', 122)		/* clear break bit */
-#define	TIOCSDTR	 _IO('t', 121)		/* set data terminal ready */
-#define	TIOCCDTR	 _IO('t', 120)		/* clear data terminal ready */
-#define	TIOCGPGRP	_IOR('t', 119, int)	/* get pgrp of tty */
-#define	TIOCSPGRP	_IOW('t', 118, int)	/* set pgrp of tty */
-						/* 117-116 compat */
-#define	TIOCOUTQ	_IOR('t', 115, int)	/* output queue size */
-#define	TIOCSTI		_IOW('t', 114, char)	/* simulate terminal input */
-#define	TIOCNOTTY	 _IO('t', 113)		/* void tty association */
-#define	TIOCPKT		_IOW('t', 112, int)	/* pty: set/clear packet mode */
-#define		TIOCPKT_DATA		0x00	/* data packet */
-#define		TIOCPKT_FLUSHREAD	0x01	/* flush packet */
-#define		TIOCPKT_FLUSHWRITE	0x02	/* flush packet */
-#define		TIOCPKT_STOP		0x04	/* stop output */
-#define		TIOCPKT_START		0x08	/* start output */
-#define		TIOCPKT_NOSTOP		0x10	/* no more ^S, ^Q */
-#define		TIOCPKT_DOSTOP		0x20	/* now do ^S ^Q */
-#define		TIOCPKT_IOCTL		0x40	/* state change of pty driver */
-#define	TIOCSTOP	 _IO('t', 111)		/* stop output, like ^S */
-#define	TIOCSTART	 _IO('t', 110)		/* start output, like ^Q */
-#define	TIOCMSET	_IOW('t', 109, int)	/* set all modem bits */
-#define	TIOCMBIS	_IOW('t', 108, int)	/* bis modem bits */
-#define	TIOCMBIC	_IOW('t', 107, int)	/* bic modem bits */
+						/* 80-84 slip */
+#define	TIOCGDRAINWAIT	_IOR('t', 86, int)	/* get ttywait timeout */
+#define	TIOCSDRAINWAIT	_IOW('t', 87, int)	/* set ttywait timeout */
+						/* 88 slip, ppp; conflicts */
+#define	TIOCTIMESTAMP	_IOR('t', 89, struct timeval)	/* enable/get timestamp
+						 * of last input event */
+						/* 70-90 ppp; many conflicts */
+#define	TIOCMGDTRWAIT	_IOR('t', 90, int)	/* modem: get wait on close */
+#define	TIOCMSDTRWAIT	_IOW('t', 91, int)	/* modem: set wait on close */
+						/* 90-92 tap; some conflicts */
+#define	TIOCDRAIN	 _IO('t', 94)		/* wait till output drained */
+#define	TIOCSIG		_IOWINT('t', 95)	/* pty: generate signal */
+#define	TIOCEXT		_IOW('t', 96, int)	/* pty: external processing */
+						/* 90-97 tun; some conflicts */
+#define	TIOCSCTTY	 _IO('t', 97)		/* become controlling tty */
+#define	TIOCCONS	_IOW('t', 98, int)	/* become virtual console */
+#define	TIOCGSID	_IOR('t', 99, int)	/* get session id */
+						/* 100 see consio.h */
+#define	TIOCSTAT	 _IO('t', 101)		/* simulate ^T status message */
+#define	TIOCUCNTL	_IOW('t', 102, int)	/* pty: set/clr usr cntl mode */
+#define		UIOCCMD(n)	_IO('u', n)	/* usr cntl op "n" */
+#define	TIOCSWINSZ	_IOW('t', 103, struct winsize)	/* set window size */
+#define	TIOCGWINSZ	_IOR('t', 104, struct winsize)	/* get window size */
 #define	TIOCMGET	_IOR('t', 106, int)	/* get all modem bits */
 #define		TIOCM_LE	0001		/* line enable */
 #define		TIOCM_DTR	0002		/* data terminal ready */
@@ -112,29 +110,31 @@ struct winsize {
 #define		TIOCM_CD	TIOCM_DCD
 #define		TIOCM_CAR	TIOCM_DCD
 #define		TIOCM_RNG	TIOCM_RI
-#define	TIOCGWINSZ	_IOR('t', 104, struct winsize)	/* get window size */
-#define	TIOCSWINSZ	_IOW('t', 103, struct winsize)	/* set window size */
-#define	TIOCUCNTL	_IOW('t', 102, int)	/* pty: set/clr usr cntl mode */
-#define	TIOCSTAT	 _IO('t', 101)		/* simulate ^T status message */
-#define		UIOCCMD(n)	_IO('u', n)	/* usr cntl op "n" */
-						/* 100 see consio.h */
-#define	TIOCGSID	_IOR('t', 99, int)	/* get session id */
-#define	TIOCCONS	_IOW('t', 98, int)	/* become virtual console */
-#define	TIOCSCTTY	 _IO('t', 97)		/* become controlling tty */
-						/* 97-90 tun; some conflicts */
-#define	TIOCEXT		_IOW('t', 96, int)	/* pty: external processing */
-#define	TIOCSIG		_IOWINT('t', 95)	/* pty: generate signal */
-#define	TIOCDRAIN	 _IO('t', 94)		/* wait till output drained */
-						/* 92-90 tap; some conflicts */
-#define	TIOCMSDTRWAIT	_IOW('t', 91, int)	/* modem: set wait on close */
-#define	TIOCMGDTRWAIT	_IOR('t', 90, int)	/* modem: get wait on close */
-						/* 90-70 ppp; many conflicts */
-#define	TIOCTIMESTAMP	_IOR('t', 89, struct timeval)	/* enable/get timestamp
-						 * of last input event */
-						/* 88 slip, ppp; conflicts */
-#define	TIOCSDRAINWAIT	_IOW('t', 87, int)	/* set ttywait timeout */
-#define	TIOCGDRAINWAIT	_IOR('t', 86, int)	/* get ttywait timeout */
-						/* 84-80 slip */
+#define	TIOCMBIC	_IOW('t', 107, int)	/* bic modem bits */
+#define	TIOCMBIS	_IOW('t', 108, int)	/* bis modem bits */
+#define	TIOCMSET	_IOW('t', 109, int)	/* set all modem bits */
+#define	TIOCSTART	 _IO('t', 110)		/* start output, like ^Q */
+#define	TIOCSTOP	 _IO('t', 111)		/* stop output, like ^S */
+#define	TIOCPKT		_IOW('t', 112, int)	/* pty: set/clear packet mode */
+#define		TIOCPKT_DATA		0x00	/* data packet */
+#define		TIOCPKT_FLUSHREAD	0x01	/* flush packet */
+#define		TIOCPKT_FLUSHWRITE	0x02	/* flush packet */
+#define		TIOCPKT_STOP		0x04	/* stop output */
+#define		TIOCPKT_START		0x08	/* start output */
+#define		TIOCPKT_NOSTOP		0x10	/* no more ^S, ^Q */
+#define		TIOCPKT_DOSTOP		0x20	/* now do ^S ^Q */
+#define		TIOCPKT_IOCTL		0x40	/* state change of pty driver */
+#define	TIOCNOTTY	 _IO('t', 113)		/* void tty association */
+#define	TIOCSTI		_IOW('t', 114, char)	/* simulate terminal input */
+#define	TIOCOUTQ	_IOR('t', 115, int)	/* output queue size */
+						/* 116-117 compat */
+#define	TIOCSPGRP	_IOW('t', 118, int)	/* set pgrp of tty */
+#define	TIOCGPGRP	_IOR('t', 119, int)	/* get pgrp of tty */
+#define	TIOCCDTR	 _IO('t', 120)		/* clear data terminal ready */
+#define	TIOCSDTR	 _IO('t', 121)		/* set data terminal ready */
+#define	TIOCCBRK	 _IO('t', 122)		/* clear break bit */
+#define	TIOCSBRK	 _IO('t', 123)		/* set break bit */
+						/* 124-127 compat */
 
 #define	TTYDISC		0		/* termios tty line discipline */
 #define	SLIPDISC	4		/* serial IP discipline */
