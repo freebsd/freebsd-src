25e0dffb548f8a77ff9ffef8a83565cded3e1d98
ache <ache@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-07-22 11:33:49 +0000

    Add arc4random_uniform() function (to avoid "modulo bias")
    Obtained from:  OpenBSD
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@180688 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/gen/arc4random.c
+++ lib/libc/gen/arc4random.c
@@ -240,6 +240,51 @@ arc4random_buf(void *_buf, size_t n)
 	THREAD_UNLOCK();
 }
 
+/*
+ * Calculate a uniformly distributed random number less than upper_bound
+ * avoiding "modulo bias".
+ *
+ * Uniformity is achieved by generating new random numbers until the one
+ * returned is outside the range [0, 2**32 % upper_bound).  This
+ * guarantees the selected random number will be inside
+ * [2**32 % upper_bound, 2**32) which maps back to [0, upper_bound)
+ * after reduction modulo upper_bound.
+ */
+u_int32_t
+arc4random_uniform(u_int32_t upper_bound)
+{
+	u_int32_t r, min;
+
+	if (upper_bound < 2)
+		return 0;
+
+#if (ULONG_MAX > 0xffffffffUL)
+	min = 0x100000000UL % upper_bound;
+#else
+	/* Calculate (2**32 % upper_bound) avoiding 64-bit math */
+	if (upper_bound > 0x80000000)
+		min = 1 + ~upper_bound;		/* 2**32 - upper_bound */
+	else {
+		/* (2**32 - (x * 2)) % x == 2**32 % x when x <= 2**31 */
+		min = ((0xffffffff - (upper_bound * 2)) + 1) % upper_bound;
+	}
+#endif
+
+	/*
+	 * This could theoretically loop forever but each retry has
+	 * p > 0.5 (worst case, usually far better) of selecting a
+	 * number inside the range we need, so it should rarely need
+	 * to re-roll.
+	 */
+	for (;;) {
+		r = arc4random();
+		if (r >= min)
+			break;
+	}
+
+	return (r % upper_bound);
+}
+
 #if 0
 /*-------- Test code for i386 --------*/
 #include <stdio.h>
