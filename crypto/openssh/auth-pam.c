30b35098a697492acbef1602dd253515a6039861
des <des@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-08-01 01:30:26 +0000

    These two files have no local patches except to prevent expansion of the original $FreeBSD$ keywords.  Revert those changes, and simply disable keyword expansion.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@181103 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- crypto/openssh/auth-pam.c
+++ crypto/openssh/auth-pam.c
@@ -45,9 +45,8 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* Based on $xFreeBSD: src/crypto/openssh/auth2-pam-freebsd.c,v 1.11 2003/03/31 13:48:18 des Exp $ */
+/* Based on $FreeBSD: src/crypto/openssh/auth2-pam-freebsd.c,v 1.11 2003/03/31 13:48:18 des Exp $ */
 #include "includes.h"
-__RCSID("$FreeBSD$");
 
 #include <sys/types.h>
 #include <sys/stat.h>
