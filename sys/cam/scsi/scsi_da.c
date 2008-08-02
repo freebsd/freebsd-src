78b638a54e1a2d8a5422754c3dde56fa8a097dfe
remko <remko@FreeBSD.org>
2008-08-02 12:46:22 +0000

    MFC r180389 (which includes another scsi id as well).
      Add quirk for the Samsung YP-U3
    
      PR:             125398
      Submitted by:   Tino Engel <goaengel at gmx dot net>
      Approved by:    imp (mentor, implicit)
      MFC after:      1 week
    
    Approved by:	imp (mentor)

--- sys/cam/scsi/scsi_da.c
+++ sys/cam/scsi/scsi_da.c
@@ -535,6 +535,18 @@ static struct da_quirk_entry da_quirk_table[] =
 		{T_DIRECT, SIP_MEDIA_REMOVABLE, "ChipsBnk", "USB*",
 		 "*"}, /*quirks*/ DA_Q_NO_SYNC_CACHE
 	},
+	{
+		/*
+		 * Samsung YP-U3 mp3-player
+		 * PR: 125398
+		 */
+		{T_DIRECT, SIP_MEDIA_REMOVABLE, "Samsung", "YP-U3",
+		 "*"}, /*quirks*/ DA_Q_NO_SYNC_CACHE
+	},
+	{
+		{T_DIRECT, SIP_MEDIA_REMOVABLE, "Netac", "OnlyDisk*",
+		 "2000"}, /*quirks*/ DA_Q_NO_SYNC_CACHE
+	}
 };
 
 static	disk_strategy_t	dastrategy;
