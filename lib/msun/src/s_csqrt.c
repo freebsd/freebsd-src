65faab72b347278966db612efd77f89b8a25e668
David Schultz <das@FreeBSD.org>
2008-08-08 00:15:16 +0000

    In the line    #pragma STDC CX_LIMITED_RANGE   ON the "ON" needs to be in caps. gcc doesn't understand this pragma anyway and assumes it is always on in any case, but icc supports it and cares about the case.

--- lib/msun/src/s_csqrt.c
+++ lib/msun/src/s_csqrt.c
@@ -40,7 +40,7 @@ __FBSDID("$FreeBSD$");
  * gcc generates is acceptable, since the special cases have already been
  * handled.
  */
-#pragma	STDC CX_LIMITED_RANGE	on
+#pragma	STDC CX_LIMITED_RANGE	ON
 
 /* We risk spurious overflow for components >= DBL_MAX / (1 + sqrt(2)). */
 #define	THRESH	0x1.a827999fcef32p+1022
