c1dc1dd77ed8c8c2606c409ae947552e4e9d1f36
kientzle <kientzle@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-08-10 02:06:28 +0000

    MfP4: Joerg Sonnenberger's fixes to make 'ar' and 'pax' writers robust against multiple calls to their destroy() functions.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@181485 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libarchive/archive_write_set_format_ar.c
+++ lib/libarchive/archive_write_set_format_ar.c
@@ -389,6 +389,9 @@ archive_write_ar_destroy(struct archive_write *a)
 
 	ar = (struct ar_w *)a->format_data;
 
+	if (ar == NULL)
+		return (ARCHIVE_OK);
+
 	if (ar->has_strtab > 0) {
 		free(ar->strtab);
 		ar->strtab = NULL;
