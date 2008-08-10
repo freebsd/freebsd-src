d299214850579afc3ed3771e952fe953ee5c8c82
kientzle <kientzle@FreeBSD.org>
2008-08-10 21:32:26 +0000

    MFC r175343:  Recognize uppercase hex digits in cpio archives.

--- lib/libarchive/archive_read_support_format_cpio.c
+++ lib/libarchive/archive_read_support_format_cpio.c
@@ -321,10 +321,12 @@ static int
 is_hex(const char *p, size_t len)
 {
 	while (len-- > 0) {
-		if (*p < '0' || (*p > '9' && *p < 'a') || *p > 'f') {
+		if ((*p >= '0' && *p <= '9')
+		    || (*p >= 'a' && *p <= 'f')
+		    || (*p >= 'A' && *p <= 'F'))
+			++p;
+		else
 			return (0);
-		}
-	        ++p;
 	}
 	return (1);
 }
