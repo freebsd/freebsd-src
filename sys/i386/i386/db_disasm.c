fc7abe024aa5a5b7a23c0ba2f074831c7169f465
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-08-11 20:19:42 +0000

    Decode some more "exotic" instructions including: fxsave, fxrstor, ldmxcsr, stmxcsr, clflush, lfence, mfence, sfence, syscall, sysret, sysenter, sysexit, pause, monitor, mwait, and swapgs (amd64 only).
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@181606 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/i386/db_disasm.c
+++ sys/i386/i386/db_disasm.c
@@ -153,15 +153,37 @@ static const char * const db_Grp9[] = {
 	""
 };
 
+static const char * const db_Grp15[] = {
+	"fxsave",
+	"fxrstor",
+	"ldmxcsr",
+	"stmxcsr",
+	"",
+	"",
+	"",
+	"clflush"
+};
+
+static const char * const db_Grp15b[] = {
+	"",
+	"",
+	"",
+	"",
+	"",
+	"lfence",
+	"mfence",
+	"sfence"
+};
+
 static const struct inst db_inst_0f0x[] = {
 /*00*/	{ "",	   TRUE,  NONE,  op1(Ew),     db_Grp6 },
 /*01*/	{ "",	   TRUE,  NONE,  op1(Ew),     db_Grp7 },
 /*02*/	{ "lar",   TRUE,  LONG,  op2(E,R),    0 },
 /*03*/	{ "lsl",   TRUE,  LONG,  op2(E,R),    0 },
 /*04*/	{ "",      FALSE, NONE,  0,	      0 },
-/*05*/	{ "",      FALSE, NONE,  0,	      0 },
+/*05*/	{ "syscall",FALSE,NONE,  0,	      0 },
 /*06*/	{ "clts",  FALSE, NONE,  0,	      0 },
-/*07*/	{ "",      FALSE, NONE,  0,	      0 },
+/*07*/	{ "sysret",FALSE, NONE,  0,	      0 },
 
 /*08*/	{ "invd",  FALSE, NONE,  0,	      0 },
 /*09*/	{ "wbinvd",FALSE, NONE,  0,	      0 },
@@ -198,10 +220,10 @@ static const struct inst db_inst_0f3x[] = {
 /*31*/	{ "rdtsc", FALSE, NONE,  0,	      0 },
 /*32*/	{ "rdmsr", FALSE, NONE,  0,	      0 },
 /*33*/	{ "rdpmc", FALSE, NONE,  0,	      0 },
-/*34*/	{ "",	   FALSE, NONE,  0,	      0 },
-/*35*/	{ "",	   FALSE, NONE,  0,	      0 },
+/*34*/	{ "sysenter",FALSE,NONE,  0,	      0 },
+/*35*/	{ "sysexit",FALSE,NONE,  0,	      0 },
 /*36*/	{ "",	   FALSE, NONE,  0,	      0 },
-/*37*/	{ "",	   FALSE, NONE,  0,	      0 },
+/*37*/	{ "getsec",FALSE, NONE,  0,	      0 },
 
 /*38*/	{ "",	   FALSE, NONE,  0,	      0 },
 /*39*/	{ "",	   FALSE, NONE,  0,	      0 },
@@ -289,8 +311,8 @@ static const struct inst db_inst_0fax[] = {
 /*ab*/	{ "bts",   TRUE,  LONG,  op2(R,E),    0 },
 /*ac*/	{ "shrd",  TRUE,  LONG,  op3(Ib,R,E), 0 },
 /*ad*/	{ "shrd",  TRUE,  LONG,  op3(CL,R,E), 0 },
-/*a6*/	{ "",      FALSE, NONE,  0,	      0 },
-/*a7*/	{ "imul",  TRUE,  LONG,  op2(E,R),    0 },
+/*ae*/	{ "",      TRUE,  LONG,  op1(E),      db_Grp15 },
+/*af*/	{ "imul",  TRUE,  LONG,  op2(E,R),    0 },
 };
 
 static const struct inst db_inst_0fbx[] = {
@@ -1122,6 +1144,7 @@ db_disasm(loc, altfmt)
 	boolean_t	first;
 	int	displ;
 	int	prefix;
+	int	rep;
 	int	imm;
 	int	imm2;
 	int	len;
@@ -1135,6 +1158,7 @@ db_disasm(loc, altfmt)
 	/*
 	 * Get prefixes
 	 */
+	rep = FALSE;
 	prefix = TRUE;
 	do {
 	    switch (inst) {
@@ -1169,7 +1193,7 @@ db_disasm(loc, altfmt)
 		    db_printf("repne ");
 		    break;
 		case 0xf3:
-		    db_printf("repe ");	/* XXX repe VS rep */
+		    rep = TRUE;
 		    break;
 		default:
 		    prefix = FALSE;
@@ -1177,6 +1201,14 @@ db_disasm(loc, altfmt)
 	    }
 	    if (prefix) {
 		get_value_inc(inst, loc, 1, FALSE);
+	    }
+	    if (rep == TRUE) {
+		if (inst == 0x90) {
+		    db_printf("pause\n");
+		    return (loc);
+		}
+		db_printf("repe ");	/* XXX repe VS rep */
+		rep = FALSE;
 	    }
 	} while (prefix);
 
@@ -1210,7 +1242,8 @@ db_disasm(loc, altfmt)
 
 	if (ip->i_extra == db_Grp1 || ip->i_extra == db_Grp2 ||
 	    ip->i_extra == db_Grp6 || ip->i_extra == db_Grp7 ||
-	    ip->i_extra == db_Grp8 || ip->i_extra == db_Grp9) {
+	    ip->i_extra == db_Grp8 || ip->i_extra == db_Grp9 ||
+	    ip->i_extra == db_Grp15) {
 	    i_name = ((const char * const *)ip->i_extra)[f_reg(regmodrm)];
 	}
 	else if (ip->i_extra == db_Grp3) {
@@ -1225,6 +1258,27 @@ db_disasm(loc, altfmt)
 	    i_name = ip->i_name;
 	    i_mode = ip->i_mode;
 	    i_size = ip->i_size;
+	}
+
+	/* Special cases that don't fit well in the tables. */
+	if (ip->i_extra == db_Grp7 && f_mod(regmodrm) == 3) {
+		switch (regmodrm) {
+		case 0xc8:
+			i_name = "monitor";
+			i_size = NONE;
+			i_mode = 0;			
+			break;
+		case 0xc9:
+			i_name = "mwait";
+			i_size = NONE;
+			i_mode = 0;
+			break;
+		}
+	}
+	if (ip->i_extra == db_Grp15 && f_mod(regmodrm) == 3) {
+		i_name = db_Grp15b[f_reg(regmodrm)];
+		i_size = NONE;
+		i_mode = 0;
 	}
 
 	if (i_size == SDEP) {
