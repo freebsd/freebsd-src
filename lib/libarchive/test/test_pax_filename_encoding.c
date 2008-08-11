86ecf3bdb90c48e928501c930a91a057b9f15fc5
kientzle <kientzle@FreeBSD.org>
2008-08-11 01:19:36 +0000

    Fix up test suite so it works again with the libarchive 1.x API. In particular, FreeBSD 6 still uses the libarchive 1.x API and this correction will permit MFCing new libarchive features back to FreeBSD 6.

--- lib/libarchive/test/test_pax_filename_encoding.c
+++ lib/libarchive/test/test_pax_filename_encoding.c
@@ -149,7 +149,11 @@ DEFINE_TEST(test_pax_filename_encoding_2)
 	archive_entry_free(entry);
 
 	assertEqualInt(0, archive_write_close(a));
+#if ARCHIVE_VERSION_NUMBER < 2000000
+	archive_write_finish(a);
+#else
 	assertEqualInt(0, archive_write_finish(a));
+#endif
 
 	/*
 	 * Now read the entries back.
@@ -175,7 +179,11 @@ DEFINE_TEST(test_pax_filename_encoding_2)
 	assertEqualString(longname, archive_entry_pathname(entry));
 
 	assertEqualInt(0, archive_read_close(a));
+#if ARCHIVE_VERSION_NUMBER < 2000000
+	archive_read_finish(a);
+#else
 	assertEqualInt(0, archive_read_finish(a));
+#endif
 }
 
 /*
@@ -249,7 +257,11 @@ DEFINE_TEST(test_pax_filename_encoding_3)
 	archive_entry_free(entry);
 
 	assertEqualInt(0, archive_write_close(a));
+#if ARCHIVE_VERSION_NUMBER < 2000000
+	archive_write_finish(a);
+#else
 	assertEqualInt(0, archive_write_finish(a));
+#endif
 
 	/*
 	 * Now read the entries back.
@@ -293,5 +305,9 @@ DEFINE_TEST(test_pax_filename_encoding_3)
 	assertEqualInt(ARCHIVE_EOF, archive_read_next_header(a, &entry));
 
 	assertEqualInt(0, archive_read_close(a));
+#if ARCHIVE_VERSION_NUMBER < 2000000
+	archive_read_finish(a);
+#else
 	assertEqualInt(0, archive_read_finish(a));
+#endif
 }
--- lib/libarchive/test/test_ustar_filenames.c
+++ lib/libarchive/test/test_ustar_filenames.c
@@ -111,7 +111,11 @@ test_filename(const char *prefix, int dlen, int flen)
 
 	/* Close out the archive. */
 	assertA(0 == archive_write_close(a));
-	assertA(0 == archive_write_finish(a));
+#if ARCHIVE_VERSION_NUMBER < 2000000
+	archive_write_finish(a);
+#else
+	assertEqualInt(0, archive_write_finish(a));
+#endif
 
 	/*
 	 * Now, read the data back.
@@ -153,7 +157,11 @@ test_filename(const char *prefix, int dlen, int flen)
 	failure("This fails if entries were written that should not have been written.  dlen=%d, flen=%d", dlen, flen);
 	assertEqualInt(1, archive_read_next_header(a, &ae));
 	assert(0 == archive_read_close(a));
-	assert(0 == archive_read_finish(a));
+#if ARCHIVE_VERSION_NUMBER < 2000000
+	archive_read_finish(a);
+#else
+	assertEqualInt(0, archive_read_finish(a));
+#endif
 }
 
 DEFINE_TEST(test_ustar_filenames)
