2e51cd9be9bf3bb0d8870d6f2117847e6da04a9e
jkim <jkim@FreeBSD.org>
2008-08-18 19:01:58 +0000

    Add simple bpf(9) regression tests and test cases.

---
+++ tools/regression/bpf/bpf_filter/tests/test0006.h
@@ -0,0 +1,31 @@
+/*-
+ * Test 0006:	BPF_LD|BPF_B|BPF_ABS
+ *
+ * $FreeBSD$
+ */
+
+/* BPF program */
+struct bpf_insn pc[] = {
+	BPF_STMT(BPF_LD|BPF_B|BPF_ABS, 1),
+	BPF_STMT(BPF_RET+BPF_A, 0),
+};
+
+/* Packet */
+u_char	pkt[] = {
+	0x01, 0x23,
+};
+
+/* Packet length seen on wire */
+u_int	wirelen =	sizeof(pkt);
+
+/* Packet length passed on buffer */
+u_int	buflen =	sizeof(pkt);
+
+/* Invalid instruction */
+int	invalid =	0;
+
+/* Expected return value */
+u_int	expect =	0x23;
+
+/* Expeced signal */
+int	expect_signal =	0;
