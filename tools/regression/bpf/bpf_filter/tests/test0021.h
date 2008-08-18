b0729b181a6c7d621beb06fcd929047eb8e83019
Jung-uk Kim <jkim@FreeBSD.org>
2008-08-18 19:01:58 +0000

    Add simple bpf(9) regression tests and test cases.

---
+++ tools/regression/bpf/bpf_filter/tests/test0021.h
@@ -0,0 +1,40 @@
+/*-
+ * Test 0021:	BPF_JMP|BPF_JGE|BPF_X
+ *
+ * $FreeBSD$
+ */
+
+/* BPF program */
+struct bpf_insn pc[] = {
+	BPF_STMT(BPF_LD|BPF_IMM, 0x01234567),
+	BPF_STMT(BPF_LDX|BPF_IMM, 0x01234568),
+	BPF_JUMP(BPF_JMP|BPF_JGE|BPF_X, 0, 3, 0),
+	BPF_STMT(BPF_LDX|BPF_IMM, 0x01234567),
+	BPF_JUMP(BPF_JMP|BPF_JGE|BPF_X, 0, 2, 1),
+	BPF_STMT(BPF_LD|BPF_IMM, 0xdeadc0de),
+	BPF_STMT(BPF_RET+BPF_A, 0),
+	BPF_STMT(BPF_LDX|BPF_IMM, 0x01234566),
+	BPF_JUMP(BPF_JMP|BPF_JGE|BPF_X, 0, 0, 1),
+	BPF_STMT(BPF_LD|BPF_IMM, 0xc0decafe),
+	BPF_STMT(BPF_RET+BPF_A, 0),
+};
+
+/* Packet */
+u_char	pkt[] = {
+	0x00,
+};
+
+/* Packet length seen on wire */
+u_int	wirelen =	sizeof(pkt);
+
+/* Packet length passed on buffer */
+u_int	buflen =	sizeof(pkt);
+
+/* Invalid instruction */
+int	invalid =	0;
+
+/* Expected return value */
+u_int	expect =	0xc0decafe;
+
+/* Expeced signal */
+int	expect_signal =	0;
---
+++ tools/regression/bpf/bpf_filter/tests/test0044.h
@@ -0,0 +1,32 @@
+/*-
+ * Test 0044:	Check boundary conditions (BPF_LD|BPF_B|BPF_ABS)
+ *
+ * $FreeBSD$
+ */
+
+/* BPF program */
+struct bpf_insn pc[] = {
+	BPF_STMT(BPF_LD|BPF_IMM, 0xdeadc0de),
+	BPF_STMT(BPF_LD|BPF_B|BPF_ABS, 2),
+	BPF_STMT(BPF_RET+BPF_A, 0),
+};
+
+/* Packet */
+u_char	pkt[] = {
+	0x01, 0x23,
+};
+
+/* Packet length seen on wire */
+u_int	wirelen =	sizeof(pkt);
+
+/* Packet length passed on buffer */
+u_int	buflen =	sizeof(pkt);
+
+/* Invalid instruction */
+int	invalid =	0;
+
+/* Expected return value */
+u_int	expect =	0;
+
+/* Expeced signal */
+int	expect_signal =	0;
