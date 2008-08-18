701af275e017695db4570f66ddcc1e7291eded11
jkim <jkim@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-08-18 19:01:58 +0000

    Add simple bpf(9) regression tests and test cases.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@181847 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ tools/regression/bpf/bpf_filter/tests/test0067.h
@@ -0,0 +1,33 @@
+/*-
+ * Test 0067:	Check boundary conditions (BPF_LD|BPF_B|BPF_IND)
+ *
+ * $FreeBSD$
+ */
+
+/* BPF program */
+struct bpf_insn pc[] = {
+	BPF_STMT(BPF_LD|BPF_IMM, 0xdeadc0de),
+	BPF_STMT(BPF_LDX|BPF_IMM, 0),
+	BPF_STMT(BPF_LD|BPF_B|BPF_IND, 1),
+	BPF_STMT(BPF_RET+BPF_A, 0),
+};
+
+/* Packet */
+u_char	pkt[] = {
+	0x01, 0x23, 0x45,
+};
+
+/* Packet length seen on wire */
+u_int	wirelen =	sizeof(pkt);
+
+/* Packet length passed on buffer */
+u_int	buflen =	0;
+
+/* Invalid instruction */
+int	invalid =	0;
+
+/* Expected return value */
+u_int	expect =	0;
+
+/* Expeced signal */
+int	expect_signal =	0;
