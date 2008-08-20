e167f27f69f5ecab7498b519697b9067e3ad4532
jhb <jhb@FreeBSD.org>
2008-08-20 19:07:21 +0000

    MFC: Change sleepq_add() to accept a lock_object rather than a mutex so that other lock types can be used as interlocks for sleeps.

--- sys/sys/sleepqueue.h
+++ sys/sys/sleepqueue.h
@@ -76,7 +76,7 @@
  * using this interface as well (death to TDI_IWAIT!)
  */
 
-struct mtx;
+struct lock_object;
 struct sleepqueue;
 struct thread;
 
@@ -90,7 +90,7 @@ struct thread;
 
 void	init_sleepqueues(void);
 void	sleepq_abort(struct thread *td, int intrval);
-void	sleepq_add(void *, struct mtx *, const char *, int, int);
+void	sleepq_add(void *, struct lock_object *, const char *, int, int);
 struct sleepqueue *sleepq_alloc(void);
 void	sleepq_broadcast(void *, int, int, int);
 void	sleepq_free(struct sleepqueue *);
