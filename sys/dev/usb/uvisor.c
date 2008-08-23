ad5ef6ac6643621d997358952ac610d791b96369
imp <imp@FreeBSD.org>
2008-08-23 23:28:33 +0000

    Style nit: s/^ }/}/

--- sys/dev/usb/uvisor.c
+++ sys/dev/usb/uvisor.c
@@ -197,7 +197,7 @@ static device_method_t uvisor_methods[] = {
        DEVMETHOD(device_attach, uvisor_attach),
        DEVMETHOD(device_detach, uvisor_detach),
        { 0, 0 }
- };
+};
 
 
 static driver_t uvisor_driver = {
