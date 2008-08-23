4e44ebfd93eaa0026cce623f4752ca50f8ef739c
imp <imp@FreeBSD.org>
2008-08-23 15:29:28 +0000

    It turns out that my analysis of the error handling here was wrong. When there's an error, we don't want to free the children, since it will be stack garbage.  While we did fail to dereference it by setting devs to 0, we didn't fail to call free.  We never failed to fail, it was the easiest thing to do.

--- sys/dev/mii/mlphy.c
+++ sys/dev/mii/mlphy.c
@@ -194,15 +194,15 @@ mlphy_service(xsc, mii, cmd)
 	 * See if there's another PHY on this bus with us.
 	 * If so, we may need it for 10Mbps modes.
 	 */
-	devs = 0;
-	device_get_children(msc->ml_mii.mii_dev, &devlist, &devs);
-	for (i = 0; i < devs; i++) {
-		if (strcmp(device_get_name(devlist[i]), "mlphy")) {
-			other = device_get_softc(devlist[i]);
-			break;
+	if (device_get_children(msc->ml_mii.mii_dev, &devlist, &devs) == 0) {
+		for (i = 0; i < devs; i++) {
+			if (strcmp(device_get_name(devlist[i]), "mlphy")) {
+				other = device_get_softc(devlist[i]);
+				break;
+			}
 		}
+		free(devlist, M_TEMP);
 	}
-	free(devlist, M_TEMP);
 
 	switch (cmd) {
 	case MII_POLLSTAT:
