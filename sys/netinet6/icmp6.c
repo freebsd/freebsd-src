5d611b96f2e463adce235ec0895b9f6c5eac3e44
rwatson <rwatson@FreeBSD.org>
2008-08-31 19:48:10 +0000

    Merge r180965 from head to stable/7:
      Since we fail IPv6 raw socket allocation if inp->in6p_icmp6filt can't
      be allocated, there's no need to conditionize use and freeing of it
      later.

--- sys/netinet6/icmp6.c
+++ sys/netinet6/icmp6.c
@@ -1900,9 +1900,8 @@ icmp6_rip6_input(struct mbuf **mp, int off)
 		   !IN6_ARE_ADDR_EQUAL(&in6p->in6p_faddr, &ip6->ip6_src))
 			continue;
 		INP_RLOCK(in6p);
-		if (in6p->in6p_icmp6filt
-		    && ICMP6_FILTER_WILLBLOCK(icmp6->icmp6_type,
-				 in6p->in6p_icmp6filt)) {
+		if (ICMP6_FILTER_WILLBLOCK(icmp6->icmp6_type,
+		    in6p->in6p_icmp6filt)) {
 			INP_RUNLOCK(in6p);
 			continue;
 		}
@@ -2714,10 +2713,6 @@ icmp6_ctloutput(struct socket *so, struct sockopt *sopt)
 				error = EMSGSIZE;
 				break;
 			}
-			if (inp->in6p_icmp6filt == NULL) {
-				error = EINVAL;
-				break;
-			}
 			error = sooptcopyin(sopt, inp->in6p_icmp6filt, optlen,
 				optlen);
 			break;
@@ -2733,10 +2728,6 @@ icmp6_ctloutput(struct socket *so, struct sockopt *sopt)
 		switch (optname) {
 		case ICMP6_FILTER:
 		    {
-			if (inp->in6p_icmp6filt == NULL) {
-				error = EINVAL;
-				break;
-			}
 			error = sooptcopyout(sopt, inp->in6p_icmp6filt,
 				sizeof(struct icmp6_filter));
 			break;
