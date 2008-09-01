3a87d457ca70a7aed6b5ba34725207b8bd395b85
obrien <obrien@FreeBSD.org>
2008-09-01 22:22:44 +0000

    Add __unix__.  Tweak __KPRINTF_ATTRIBUTE__ while I'm here.

--- contrib/gcc/config/freebsd-spec.h
+++ contrib/gcc/config/freebsd-spec.h
@@ -55,7 +55,8 @@ Boston, MA 02110-1301, USA.  */
     {									\
 	builtin_define_with_int_value ("__FreeBSD__", FBSD_MAJOR);	\
 	builtin_define_std ("unix");					\
-	builtin_define ("__KPRINTF_ATTRIBUTE__");		       	\
+	builtin_define_std ("__unix__");				\
+	builtin_define ("__KPRINTF_ATTRIBUTE__");			\
 	builtin_assert ("system=unix");					\
 	builtin_assert ("system=bsd");					\
 	builtin_assert ("system=FreeBSD");				\
