b089c966994672f46707170363b59a80b1a44a98
kientzle <kientzle@FreeBSD.org>
2008-09-12 03:53:54 +0000

    Windows compatibility:  The stub replacement for compression_program (which always returns an error when invoked) needs its prototype.

--- lib/libarchive/archive_read_support_compression_program.c
+++ lib/libarchive/archive_read_support_compression_program.c
@@ -31,6 +31,8 @@ __FBSDID("$FreeBSD$");
 #if !defined(HAVE_PIPE) || !defined(HAVE_FCNTL) || \
     !(defined(HAVE_FORK) || defined(HAVE_VFORK))
 
+#include "archive.h"
+
 /*
  * On non-Posix systems, allow the program to build, but choke if
  * this function is actually invoked.
