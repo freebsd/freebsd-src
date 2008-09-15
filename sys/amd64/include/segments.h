10085d6e9d86b1f114ebb3bb60dc16b54e4086ab
kib <kib@FreeBSD.org>
2008-09-15 09:35:29 +0000

    MFC r182865: Fix inconsistencies in the comments.
    Approved by:	re (blackend)

--- sys/amd64/include/segments.h
+++ sys/amd64/include/segments.h
@@ -201,7 +201,7 @@ struct region_descriptor {
 #define	GUDATA_SEL	4	/* User 32/64 bit Data Descriptor */
 #define	GUCODE_SEL	5	/* User 64 bit Code Descriptor */
 #define	GPROC0_SEL	6	/* TSS for entering kernel etc */
-/* slot 6 is second half of GPROC0_SEL */
+/* slot 7 is second half of GPROC0_SEL */
 #define	GUGS32_SEL	8	/* User 32 bit GS Descriptor */
 #define	NGDT 		9
 
