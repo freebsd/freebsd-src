8c83611684e2a727922daa0300b0f1223b835a91
Craig Rodrigues <rodrigc@FreeBSD.org>
2008-09-16 17:39:01 +0000

    Bump __FreeBSD_version to reflect that kernel NFS mount code can accept mount options in individual members of nmount() iovec array instead of one big struct nfs_args.
    Requested by:	dfr

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -57,7 +57,7 @@
  *		is created, otherwise 1.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 800047	/* Master, propagated to newvers */
+#define __FreeBSD_version 800048	/* Master, propagated to newvers */
 
 #ifndef LOCORE
 #include <sys/types.h>
