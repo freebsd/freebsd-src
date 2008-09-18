af18f0ea9c5b74988c657197ded0794ac6a8db4a
imp <imp@FreeBSD.org>
2008-09-18 19:54:02 +0000

    Mips doesn't have a long double that's different in size from double.

--- cddl/contrib/opensolaris/lib/libdtrace/common/dt_printf.c
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_printf.c
@@ -306,7 +306,7 @@ pfprint_fp(dtrace_hdl_t *dtp, FILE *fp, const char *format,
 	case sizeof (double):
 		return (dt_printf(dtp, fp, format,
 		    *((double *)addr) / n));
-#if !defined(__arm__) && !defined(__powerpc__)
+#if !defined(__arm__) && !defined(__powerpc__) && !defined(__mips__)
 	case sizeof (long double):
 		return (dt_printf(dtp, fp, format,
 		    *((long double *)addr) / ldn));
