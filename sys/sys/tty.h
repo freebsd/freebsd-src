c93400be8cc010e37eabc0f2938892328ec1051e
Ed Schouten <ed@FreeBSD.org>
2008-09-24 19:43:09 +0000

    Add another TTY locking migration macro: tty_getlock().
    Some subsystems (HPS-USB) like to lock down the TTY through a more
    generic approach, namely the regular mtx(9) macro's. Allow the TTY lock
    to be obtained through the new macro.
    
    Discussed with:	hps, thompsa

--- sys/sys/tty.h
+++ sys/sys/tty.h
@@ -155,6 +155,7 @@ void	tty_rel_gone(struct tty *tp);
 #define	tty_lock(tp)		mtx_lock((tp)->t_mtx)
 #define	tty_unlock(tp)		mtx_unlock((tp)->t_mtx)
 #define	tty_lock_assert(tp,ma)	mtx_assert((tp)->t_mtx, (ma))
+#define	tty_getlock(tp)		((tp)->t_mtx)
 
 /* Device node creation. */
 void	tty_makedev(struct tty *tp, struct ucred *cred, const char *fmt, ...)
