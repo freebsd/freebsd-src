93c2217c005d89f924a795f17f4bee301a2a40da
rnoland <rnoland@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-10-04 14:51:14 +0000

    Don't explicitly bzer driver softc
    new-bus already handles this for us.
    
    Suggested by jhb@
    
    Approved by:	jhb (mentor)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@183605 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/drm/mga_drv.c
+++ sys/dev/drm/mga_drv.c
@@ -130,8 +130,6 @@ mga_attach(device_t nbdev)
 {
 	struct drm_device *dev = device_get_softc(nbdev);
 
-	bzero(dev, sizeof(struct drm_device));
-
 	dev->driver = malloc(sizeof(struct drm_driver_info), M_DRM,
 	    M_WAITOK | M_ZERO);
 
