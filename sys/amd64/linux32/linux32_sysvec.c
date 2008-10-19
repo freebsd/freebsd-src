aa8b201112c871e10cb119d371688098b5395916
Konstantin Belousov <kib@FreeBSD.org>
2008-10-19 10:02:26 +0000

    Correctly fill siginfo for the signals delivered by linux tkill/tgkill. It is required for async cancellation to work.
    Fix PROC_LOCK leak in linux_tgkill when signal delivery attempt is made
    to not linux process.
    
    Do not call em_find(p, ...) with p unlocked.
    
    Move common code for linux_tkill() and linux_tgkill() into
    linux_do_tkill().
    
    Change linux siginfo_t definition to match actual linux one. Extend
    uid fields to 4 bytes from 2. The extension does not change structure
    layout and is binary compatible with previous definition, because i386
    is little endian, and each uid field has 2 byte padding after it.
    
    Reported by:	Nicolas Joly <njoly pasteur fr>
    Submitted by:	dchangin
    MFC after:	1 month

--- sys/amd64/linux32/linux32_sysvec.c
+++ sys/amd64/linux32/linux32_sysvec.c
@@ -334,9 +334,7 @@ linux_rt_sendsig(sig_t catcher, ksiginfo_t *ksi, sigset_t *mask)
 	frame.sf_ucontext = PTROUT(&fp->sf_sc);
 
 	/* Fill in POSIX parts */
-	frame.sf_si.lsi_signo = sig;
-	frame.sf_si.lsi_code = code;
-	frame.sf_si.lsi_addr = PTROUT(ksi->ksi_addr);
+	ksiginfo_to_lsiginfo(ksi, &frame.sf_si, sig);
 
 	/*
 	 * Build the signal context to be used by sigreturn.
