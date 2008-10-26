f709520e4396b85b9812810a7d1b16414e1fa4d3
rwatson <rwatson@FreeBSD.org>
2008-10-26 22:03:52 +0000

    In both dropwithreset paths in tcp_input.c, drop the tcbinfo lock sooner to decomplicate locking and eliminate the need for a rather chatty comment about why we have to handle the global lock in a special way for the benefit of ipfw and pf cred rules.
    MFC after:	3 days

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -896,19 +896,12 @@ findpcb:
 
 dropwithreset:
 	INP_INFO_WLOCK_ASSERT(&V_tcbinfo);
+	INP_INFO_WUNLOCK(&V_tcbinfo);
 
-	/*
-	 * If inp is non-NULL, we call tcp_dropwithreset() holding both inpcb
-	 * and global locks.  However, if NULL, we must hold neither as
-	 * firewalls may acquire the global lock in order to look for a
-	 * matching inpcb.
-	 */
 	if (inp != NULL) {
 		tcp_dropwithreset(m, th, tp, tlen, rstreason);
 		INP_WUNLOCK(inp);
-	}
-	INP_INFO_WUNLOCK(&V_tcbinfo);
-	if (inp == NULL)
+	} else
 		tcp_dropwithreset(m, th, NULL, tlen, rstreason);
 	m = NULL;	/* mbuf chain got consumed. */
 	goto drop;
@@ -2498,19 +2491,12 @@ dropafterack:
 
 dropwithreset:
 	KASSERT(headlocked, ("%s: dropwithreset: head not locked", __func__));
+	INP_INFO_WUNLOCK(&V_tcbinfo);
 
-	/*
-	 * If tp is non-NULL, we call tcp_dropwithreset() holding both inpcb
-	 * and global locks.  However, if NULL, we must hold neither as
-	 * firewalls may acquire the global lock in order to look for a
-	 * matching inpcb.
-	 */
 	if (tp != NULL) {
 		tcp_dropwithreset(m, th, tp, tlen, rstreason);
 		INP_WUNLOCK(tp->t_inpcb);
-	}
-	INP_INFO_WUNLOCK(&V_tcbinfo);
-	if (tp == NULL)
+	} else
 		tcp_dropwithreset(m, th, NULL, tlen, rstreason);
 	return;
 
