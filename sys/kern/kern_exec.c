69b29606c70af2a9bef5b0088f43553775de5e3e
csjp <csjp@FreeBSD.org>
2008-10-28 17:01:16 +0000

    MFC SVN rev 181647
    - Reduce the scope of the vnode lock such that it does not
      cover the various copyouts associated with initializing
      the process's argv/env data in userspace.  It is possible
      that these copyout operations can fault under memory
      pressure, possibly resulting in dead locks.
    
    Approved by:	re@ (kib)

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -476,6 +476,11 @@ interpret:
 	}
 
 	/*
+	 * NB: We unlock the vnode here because it is believed that none
+	 * of the sv_copyout_strings/sv_fixup operations require the vnode.
+	 */
+	VOP_UNLOCK(imgp->vp, 0, td);
+	/*
 	 * Copy out strings (args and env) and initialize stack base
 	 */
 	if (p->p_sysent->sv_copyout_strings)
@@ -512,7 +517,6 @@ interpret:
 	}
 
 	/* close files on exec */
-	VOP_UNLOCK(imgp->vp, 0, td);
 	fdcloseexec(td);
 	vn_lock(imgp->vp, LK_EXCLUSIVE | LK_RETRY, td);
 
