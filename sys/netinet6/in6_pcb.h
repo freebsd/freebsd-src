e8f0700bf7873cc39c4f0b799d7ab0bfd50644d4
bz <bz@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-11-26 20:52:26 +0000

    Remove in6_pcbdetach() as it is exactly the same function as in_pcbdetach() and we don't need the code twice.
    Reviewed by:	rwatson
    MFC after:	6 weeks (*)
    (*) possibly need to leave a stub wrapper in 7 to keep the symbol.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@185344 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet6/in6_pcb.h
+++ sys/netinet6/in6_pcb.h
@@ -73,7 +73,6 @@ void	in6_pcbpurgeif0 __P((struct inpcbinfo *, struct ifnet *));
 void	in6_losing __P((struct inpcb *));
 int	in6_pcbbind __P((struct inpcb *, struct sockaddr *, struct ucred *));
 int	in6_pcbconnect __P((struct inpcb *, struct sockaddr *, struct ucred *));
-void	in6_pcbdetach __P((struct inpcb *));
 void	in6_pcbdisconnect __P((struct inpcb *));
 void	in6_pcbfree __P((struct inpcb *));
 int	in6_pcbladdr __P((struct inpcb *, struct sockaddr *,
