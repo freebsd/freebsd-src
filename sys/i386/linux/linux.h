74f5d680117105500c6bb15a43b6573ec0c9f085
Konstantin Belousov <kib@FreeBSD.org>
2008-11-29 17:14:06 +0000

    Make linux_sendmsg() and linux_recvmsg() work on linux32/amd64. Change types used in the linux' struct msghdr and struct cmsghdr definitions to the properly-sized architecture-specific types. Move ancillary data handler from linux_sendit() to linux_sendmsg().
    Submitted by:	dchagin

--- sys/i386/linux/linux.h
+++ sys/i386/linux/linux.h
@@ -693,6 +693,22 @@ struct l_sockaddr {
 	char		sa_data[14];
 };
 
+struct l_msghdr {
+	l_uintptr_t	msg_name;
+	l_int		msg_namelen;
+	l_uintptr_t	msg_iov;
+	l_size_t	msg_iovlen;
+	l_uintptr_t	msg_control;
+	l_size_t	msg_controllen;
+	l_uint		msg_flags;
+};
+
+struct l_cmsghdr {
+	l_size_t	cmsg_len;
+	l_int		cmsg_level;
+	l_int		cmsg_type;
+};
+
 struct l_ifmap {
 	l_ulong		mem_start;
 	l_ulong		mem_end;
