149fc4da71fad58c768de6eac52009b90bf904a9
kientzle <kientzle@FreeBSD.org>
2008-12-06 07:15:42 +0000

    Make the GCC-specific __dead2 markers conditional on whether we're compiling under GCC.

--- usr.bin/cpio/cpio_platform.h
+++ usr.bin/cpio/cpio_platform.h
@@ -81,4 +81,12 @@
 #endif
 #endif
 
+/* How to mark functions that don't return. */
+#if defined(__GNUC__) && (__GNUC__ > 2 || \
+                          (__GNUC__ == 2 && __GNUC_MINOR__ >= 5))
+#define __LA_DEAD       __attribute__((__noreturn__))
+#else
+#define __LA_DEAD
+#endif
+
 #endif /* !CPIO_PLATFORM_H_INCLUDED */
