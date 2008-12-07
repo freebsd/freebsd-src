598b522b42d772cdb7895eda77507500d0507864
kmacy <kmacy@FreeBSD.org>
2008-12-07 21:15:43 +0000

    - convert radix node head lock from mutex to rwlock  - make radix node head lock not recursive  - fix LOR in rtexpunge  - fix LOR in rtredirect
    Reviewed by:	sam

--- sys/netinet6/in6_rmx.c
+++ sys/netinet6/in6_rmx.c
@@ -83,6 +83,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/socket.h>
 #include <sys/socketvar.h>
 #include <sys/mbuf.h>
+#include <sys/rwlock.h>
 #include <sys/syslog.h>
 #include <sys/callout.h>
 #include <sys/vimage.h>
