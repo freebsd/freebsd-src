2f5aa21081538347f3430f21dd60653e0c2f217e
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-12-08 11:04:17 +0000

    The dqrele() function syncs the dq, then acquires the dqh lock, and then does final drop of the the dq reference to put it onto the free list. There is a possibility that the dq would be found by another thread after sync and before the dqh lock is acquired. If that other thread drops the dq before we have taken the dqh lock, the dirty dq is put on the free list.
    Recheck the DQ_MOD after the dqh lock is relocked. Repeat dqsync() if
    the dq is dirty. This ensures that up to date dq is written in the quota
    file and fixes assertion in dqget().
    
    Reported and tested by:	Frode Nordahl <frode nordahl net>
    MFC after:	3 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@185761 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/ufs/ufs/ufs_quota.c
+++ sys/ufs/ufs/ufs_quota.c
@@ -1262,7 +1262,7 @@ dqrele(struct vnode *vp, struct dquot *dq)
 		return;
 	}
 	DQH_UNLOCK();
-
+sync:
 	(void) dqsync(vp, dq);
 
 	DQH_LOCK();
@@ -1270,6 +1270,18 @@ dqrele(struct vnode *vp, struct dquot *dq)
 	{
 		DQH_UNLOCK();
 		return;
+	}
+
+	/*
+	 * The dq may become dirty after it is synced but before it is
+	 * put to the free list. Checking the DQ_MOD there without
+	 * locking dq should be safe since no other references to the
+	 * dq exist.
+	 */
+	if ((dq->dq_flags & DQ_MOD) != 0) {
+		dq->dq_cnt++;
+		DQH_UNLOCK();
+		goto sync;
 	}
 	TAILQ_INSERT_TAIL(&dqfreelist, dq, dq_freelist);
 	DQH_UNLOCK();
