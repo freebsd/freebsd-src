9a73283b1fba883191e5c9945ec62c4cf25d9cd0
bz <bz@FreeBSD.org>
2008-12-11 16:09:31 +0000

    Order #includes - also to reduce diffs with vimage branches in p4.
    Sponsored by:	The FreeBSD Foundation

--- sys/kern/kern_poll.c
+++ sys/kern/kern_poll.c
@@ -33,19 +33,18 @@ __FBSDID("$FreeBSD$");
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kernel.h>
+#include <sys/kthread.h>
+#include <sys/proc.h>
+#include <sys/resourcevar.h>
 #include <sys/socket.h>			/* needed by net/if.h		*/
 #include <sys/sockio.h>
 #include <sys/sysctl.h>
 #include <sys/syslog.h>
+#include <sys/vimage.h>
 
 #include <net/if.h>			/* for IFF_* flags		*/
 #include <net/netisr.h>			/* for NETISR_POLL		*/
 #include <net/vnet.h>
-
-#include <sys/proc.h>
-#include <sys/resourcevar.h>
-#include <sys/kthread.h>
-#include <sys/vimage.h>
 
 static void netisr_poll(void);		/* the two netisr handlers      */
 static void netisr_pollmore(void);
