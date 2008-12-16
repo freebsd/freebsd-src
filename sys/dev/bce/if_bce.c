094d34e343a4e447c27a9b0bbac9e0f1a4c1304d
delphij <delphij@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-12-16 04:59:04 +0000

    Test whether sc->tx_mbuf_map[i], not whether sc->tx_mbuf_map is NULL before doing bus_dmamap_sync() since it operates on the former, not the latter.
    Reviewed by:	davidch
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@186168 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/bce/if_bce.c
+++ sys/dev/bce/if_bce.c
@@ -5114,7 +5114,7 @@ bce_free_tx_chain(struct bce_softc *sc)
 	/* Unmap, unload, and free any mbufs still in the TX mbuf chain. */
 	for (i = 0; i < TOTAL_TX_BD; i++) {
 		if (sc->tx_mbuf_ptr[i] != NULL) {
-			if (sc->tx_mbuf_map != NULL)
+			if (sc->tx_mbuf_map[i] != NULL)
 				bus_dmamap_sync(sc->tx_mbuf_tag, sc->tx_mbuf_map[i],
 					BUS_DMASYNC_POSTWRITE);
 			m_freem(sc->tx_mbuf_ptr[i]);
