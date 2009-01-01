64f60f25d72b80646751cc304e695f0503f586f3
ed <ed@FreeBSD.org>
2009-01-01 13:26:53 +0000

    Replace syscons terminal renderer by a new renderer that uses libteken.
    Some time ago I started working on a library called libteken, which is
    terminal emulator. It does not buffer any screen contents, but only
    keeps terminal state, such as cursor position, attributes, etc. It
    should implement all escape sequences that are implemented by the
    cons25 terminal emulator, but also a fair amount of sequences that are
    present in VT100 and xterm.
    
    A lot of random notes, which could be of interest to users/developers:
    
    - Even though I'm leaving the terminal type set to `cons25', users can
      do experiments with placing `xterm-color' in /etc/ttys. Because we
      only implement a subset of features of xterm, this may cause
      artifacts. We should consider extending libteken, because in my
      opinion xterm is the way to go. Some missing features:
    
      - Keypad application mode (DECKPAM)
      - Character sets (SCS)
    
    - libteken is filled with a fair amount of assertions, but unfortunately
      we cannot go into the debugger anymore if we fail them. I've done
      development of this library almost entirely in userspace. In
      sys/dev/syscons/teken there are two applications that can be helpful
      when debugging the code:
    
      - teken_demo: a terminal emulator that can be started from a regular
        xterm that emulates a terminal using libteken. This application can
        be very useful to debug any rendering issues.
    
      - teken_stress: a stress testing application that emulates random
        terminal output. libteken has literally survived multiple terabytes
        of random input.
    
    - libteken also includes support for UTF-8, but unfortunately our input
      layer and font renderer don't support this. If users want to
      experiment with UTF-8 support, they can enable `TEKEN_UTF8' in
      teken.h. If you recompile your kernel or the teken_demo application,
      you can hold some nice experiments.
    
    - I've left PC98 the way it is right now. The PC98 platform has a custom
      syscons renderer, which supports some form of localised input. Maybe
      we should port PC98 to libteken by the time syscons supports UTF-8?
    
    - I've removed the `dumb' terminal emulator. It has been broken for
      years. It hasn't survived the `struct proc' -> `struct thread'
      conversion.
    
    - To prevent confusion among people that want to hack on libteken:
      unlike syscons, the state machines that parse the escape sequences are
      machine generated. This means that if you want to add new escape
      sequences, you have to add an entry to the `sequences' file. This will
      cause new entries to be added to `teken_state.h'.
    
    - Any rendering artifacts that didn't occur prior to this commit are by
      accident. They should be reported to me, so I can fix them.
    
    Discussed on:	current@, hackers@
    Discussed with:	philip (at 25C3)

---
+++ sys/dev/syscons/teken/teken.h
@@ -0,0 +1,168 @@
+/*-
+ * Copyright (c) 2008-2009 Ed Schouten <ed@FreeBSD.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef _TEKEN_H_
+#define	_TEKEN_H_
+
+/*
+ * libteken: terminal emulation library.
+ *
+ * This library converts an UTF-8 stream of bytes to terminal drawing
+ * commands. It implements commands similar to xterm-color.
+ */
+
+#if 0
+/*
+ * XXX: Disable UTF-8 support for now. It requires UTF-8 keyboard input
+ * and rendering, which we do not yet support.
+ */
+#define	TEKEN_UTF8
+#endif
+
+#ifdef TEKEN_UTF8
+typedef uint32_t teken_char_t;
+#else /* !TEKEN_UTF8 */
+typedef unsigned char teken_char_t;
+#endif /* TEKEN_UTF8 */
+typedef unsigned short teken_unit_t;
+typedef unsigned char teken_format_t;
+#define	TF_BOLD		0x01
+#define	TF_UNDERLINE	0x02
+#define	TF_BLINK	0x04
+typedef unsigned char teken_color_t;
+#define	TC_BLACK	0
+#define	TC_RED		1
+#define	TC_GREEN	2
+#define	TC_BROWN	3
+#define	TC_BLUE		4
+#define	TC_MAGENTA	5
+#define	TC_CYAN		6
+#define	TC_WHITE	7
+#define	TC_NCOLORS	8
+
+typedef struct {
+	teken_unit_t	tp_row;
+	teken_unit_t	tp_col;
+} teken_pos_t;
+typedef struct {
+	teken_pos_t	tr_begin;
+	teken_pos_t	tr_end;
+} teken_rect_t;
+typedef struct {
+	teken_format_t	ta_format;
+	teken_color_t	ta_fgcolor;
+	teken_color_t	ta_bgcolor;
+} teken_attr_t;
+typedef struct {
+	teken_unit_t	ts_begin;
+	teken_unit_t	ts_end;
+} teken_span_t;
+
+typedef struct __teken teken_t;
+
+typedef void teken_state_t(teken_t *, teken_char_t);
+
+/*
+ * Drawing routines supplied by the user.
+ */
+
+typedef void tf_bell_t(void *);
+typedef void tf_cursor_t(void *, const teken_pos_t *);
+typedef void tf_putchar_t(void *, const teken_pos_t *, teken_char_t,
+    const teken_attr_t *);
+typedef void tf_fill_t(void *, const teken_rect_t *, teken_char_t,
+    const teken_attr_t *);
+typedef void tf_copy_t(void *, const teken_rect_t *, const teken_pos_t *);
+typedef void tf_param_t(void *, int, int);
+#define	TP_SHOWCURSOR	0
+#define	TP_CURSORKEYS	1
+#define	TP_KEYPADAPP	2
+#define	TP_AUTOREPEAT	3
+#define	TP_SWITCHVT	4
+#define	TP_132COLS	5
+typedef void tf_respond_t(void *, const void *, size_t);
+
+typedef struct {
+	tf_bell_t	*tf_bell;
+	tf_cursor_t	*tf_cursor;
+	tf_putchar_t	*tf_putchar;
+	tf_fill_t	*tf_fill;
+	tf_copy_t	*tf_copy;
+	tf_param_t	*tf_param;
+	tf_respond_t	*tf_respond;
+} teken_funcs_t;
+
+/*
+ * Terminal state.
+ */
+
+struct __teken {
+	const teken_funcs_t *t_funcs;
+	void		*t_softc;
+
+	teken_state_t	*t_nextstate;
+	unsigned int	 t_stateflags;
+
+#define T_NUMSIZE	8
+	unsigned int	 t_nums[T_NUMSIZE];
+	unsigned int	 t_curnum;
+
+	teken_pos_t	 t_cursor;
+	teken_attr_t	 t_curattr;
+	teken_pos_t	 t_saved_cursor;
+	teken_attr_t	 t_saved_curattr;
+
+	teken_attr_t	 t_defattr;
+	teken_pos_t	 t_winsize;
+
+	/* For DECSTBM. */
+	teken_span_t	 t_scrollreg;
+	/* For DECOM. */
+	teken_span_t	 t_originreg;
+
+#define	T_NUMCOL	160
+	unsigned int	t_tabstops[T_NUMCOL / (sizeof(unsigned int) * 8)];
+
+#ifdef TEKEN_UTF8
+	unsigned int	t_utf8_left;
+	teken_char_t	t_utf8_partial;
+#endif /* TEKEN_UTF8 */
+};
+
+/* Initialize teken structure. */
+void	teken_init(teken_t *, const teken_funcs_t *, void *);
+
+/* Deliver character input. */
+void	teken_input(teken_t *, const void *, size_t);
+
+/* Set teken attributes. */
+void	teken_set_cursor(teken_t *, const teken_pos_t *);
+void	teken_set_defattr(teken_t *, const teken_attr_t *);
+void	teken_set_winsize(teken_t *, const teken_pos_t *);
+
+#endif /* !_TEKEN_H_ */
