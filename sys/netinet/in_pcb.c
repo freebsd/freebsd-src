670dc83f72dafb92457a00a1958d3fe01aa3bdad
adrian <adrian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2009-01-09 16:02:19 +0000

    Implement a new IP option (not compiled/enabled by default) to allow applications to specify a non-local IP address when bind()'ing a socket to a local endpoint.
    This allows applications to spoof the client IP address of connections
    if (obviously!) they somehow are able to receive the traffic normally
    destined to said clients.
    
    This patch doesn't include any changes to ipfw or the bridging code to
    redirect the client traffic through the PCB checks so TCP gets a shot
    at it. The normal behaviour is that packets with a non-local destination
    IP address are not handled locally. This can be dealth with some IPFW hackery;
    modifications to IPFW to make this less hacky will occur in subsequent
    commmits.
    
    Thanks to Julian Elischer and others at Ironport. This work was approved
    and donated before Cisco acquired them.
    
    Obtained from:	Julian Elischer and others
    MFC after:	2 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@186955 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/in_pcb.c
+++ sys/netinet/in_pcb.c
@@ -35,6 +35,7 @@
 __FBSDID("$FreeBSD$");
 
 #include "opt_ddb.h"
+#include "opt_inet.h"
 #include "opt_ipsec.h"
 #include "opt_inet6.h"
 #include "opt_mac.h"
@@ -346,7 +347,11 @@ in_pcbbind_setup(struct inpcb *inp, struct sockaddr *nam, in_addr_t *laddrp,
 		} else if (sin->sin_addr.s_addr != INADDR_ANY) {
 			sin->sin_port = 0;		/* yech... */
 			bzero(&sin->sin_zero, sizeof(sin->sin_zero));
-			if (ifa_ifwithaddr((struct sockaddr *)sin) == 0)
+			if (
+#if defined(IP_NONLOCALBIND)
+			    ((inp->inp_flags & INP_NONLOCALOK) == 0) &&
+#endif
+			    (ifa_ifwithaddr((struct sockaddr *)sin) == 0))
 				return (EADDRNOTAVAIL);
 		}
 		laddr = sin->sin_addr;
