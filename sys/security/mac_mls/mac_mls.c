bc720c2279c7a8f3788aafa9ffc5d8027cf80817
rwatson <rwatson@FreeBSD.org>
2009-01-10 09:17:16 +0000

    Use MPC_OBJECT_IP6Q to indicate labeling of struct ip6q rather than MPC_OBJECT_IPQ; it was already defined, just not used.
    Obtained from:	TrustedBSD Project
    Sponsored by:	Apple, Inc.

--- sys/security/mac_mls/mac_mls.c
+++ sys/security/mac_mls/mac_mls.c
@@ -3170,6 +3170,7 @@ static struct mac_policy_ops mls_ops =
 			 MPC_OBJECT_DEVFS |				\
 			 MPC_OBJECT_MBUF |				\
 			 MPC_OBJECT_IPQ |				\
+			 MPC_OBJECT_IP6Q |				\
 			 MPC_OBJECT_IFNET |				\
 			 MPC_OBJECT_BPFDESC |				\
 			 MPC_OBJECT_PIPE |				\
