3a17b8ac8d948b924187c27babcb80db9f21b71b
lstewart <lstewart@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2009-01-15 06:44:22 +0000

    Add TCP Appropriate Byte Counting (RFC 3465) support to kernel.
    The new behaviour is on by default, and can be disabled by setting the
    net.inet.tcp.rfc3465 sysctl to 0 to obtain previous behaviour.
    
    The patch changes struct tcpcb in sys/netinet/tcp_var.h which breaks
    the ABI. Bump __FreeBSD_version to 800061 accordingly. User space tools
    that rely on the size of struct tcpcb (e.g. sockstat) need to be recompiled.
    
    Reviewed by:	rpaulo, gnn
    Approved by:	gnn, kmacy (mentors)
    Sponsored by:	FreeBSD Foundation
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@187289 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/tcp_var.h
+++ sys/netinet/tcp_var.h
@@ -189,6 +189,7 @@ struct tcpcb {
 	void	*t_pspare[3];		/* toe usrreqs / toepcb * / congestion algo / vimage / 1 general use */
 	struct toe_usrreqs *t_tu;       /* offload operations vector */
 	void	*t_toe;			/* TOE pcb pointer */
+	int	t_bytes_acked;		/* # bytes acked during current RTT */
 };
 
 /*
