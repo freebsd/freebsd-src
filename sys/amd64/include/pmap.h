78c7568e6017cea52dd56187309e9271d62cae99
alc <alc@FreeBSD.org>
2009-01-19 07:44:18 +0000

    MFC rev 180108   Compute NKPDPE from NKPT.  This reduces the number of knobs that must be   turned in order to change the size of the kernel virtual address space.

--- sys/amd64/include/pmap.h
+++ sys/amd64/include/pmap.h
@@ -109,7 +109,7 @@
 #endif
 
 #define NKPML4E		1		/* number of kernel PML4 slots */
-#define NKPDPE		1		/* number of kernel PDP slots */
+#define NKPDPE		howmany(NKPT, NPDEPG)/* number of kernel PDP slots */
 
 #define	NUPML4E		(NPML4EPG/2)	/* number of userland PML4 pages */
 #define	NUPDPE		(NUPML4E*NPDPEPG)/* number of userland PDP pages */
