c7ed8c0a858e6bd6f871aa053772e0bc704d3e0b
Robert Watson <rwatson@FreeBSD.org>
2009-01-24 13:15:45 +0000

    Use __FBSDID() for $FreeBSD$ version strings in .c files.
    Obtained from:	TrustedBSD Project
    MFC after:	3 days

--- sys/security/mac/mac_priv.c
+++ sys/security/mac/mac_priv.c
@@ -25,13 +25,14 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * $FreeBSD$
  */
 
 /*
  * MAC checks for system privileges.
  */
+
+#include "sys/cdefs.h"
+__FBSDID("$FreeBSD$");
 
 #include "opt_mac.h"
 
