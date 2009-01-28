41a0d6ae54eff85ed9f7a051ddccf9009f36709c
rodrigc <rodrigc@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2009-01-28 07:46:35 +0000

    Fix parsing of acregmin, acregmax, acdirmin and acdirmax NFS mount options when passed as strings via nmount().
    Submitted by: Jaakko Heinonen <jh saunalahti fi>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@187812 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/nfsclient/nfs_vfsops.c
+++ sys/nfsclient/nfs_vfsops.c
@@ -976,39 +976,43 @@ nfs_mount(struct mount *mp, struct thread *td)
 	}
 	if (vfs_getopt(mp->mnt_optnew, "acregmin", (void **)&opt, NULL) == 0) {
 		ret = sscanf(opt, "%d", &args.acregmin);
-		if (ret != 1 || args.acregmin <= 0) {
+		if (ret != 1 || args.acregmin < 0) {
 			vfs_mount_error(mp, "illegal acregmin: %s",
 			    opt);
 			error = EINVAL;
 			goto out;
 		}
+		args.flags |= NFSMNT_ACREGMIN;
 	}
 	if (vfs_getopt(mp->mnt_optnew, "acregmax", (void **)&opt, NULL) == 0) {
 		ret = sscanf(opt, "%d", &args.acregmax);
-		if (ret != 1 || args.acregmax <= 0) {
+		if (ret != 1 || args.acregmax < 0) {
 			vfs_mount_error(mp, "illegal acregmax: %s",
 			    opt);
 			error = EINVAL;
 			goto out;
 		}
+		args.flags |= NFSMNT_ACREGMAX;
 	}
 	if (vfs_getopt(mp->mnt_optnew, "acdirmin", (void **)&opt, NULL) == 0) {
 		ret = sscanf(opt, "%d", &args.acdirmin);
-		if (ret != 1 || args.acdirmin <= 0) {
+		if (ret != 1 || args.acdirmin < 0) {
 			vfs_mount_error(mp, "illegal acdirmin: %s",
 			    opt);
 			error = EINVAL;
 			goto out;
 		}
+		args.flags |= NFSMNT_ACDIRMIN;
 	}
 	if (vfs_getopt(mp->mnt_optnew, "acdirmax", (void **)&opt, NULL) == 0) {
 		ret = sscanf(opt, "%d", &args.acdirmax);
-		if (ret != 1 || args.acdirmax <= 0) {
+		if (ret != 1 || args.acdirmax < 0) {
 			vfs_mount_error(mp, "illegal acdirmax: %s",
 			    opt);
 			error = EINVAL;
 			goto out;
 		}
+		args.flags |= NFSMNT_ACDIRMAX;
 	}
 	if (vfs_getopt(mp->mnt_optnew, "deadthresh", (void **)&opt, NULL) == 0) {
 		ret = sscanf(opt, "%d", &args.deadthresh);
