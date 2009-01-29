bfa45b3bb76cd5efc2aa5585b51801d544ffffa1
jeff <jeff@FreeBSD.org>
2009-01-29 09:22:56 +0000

    - Allocate apic vectors on a per-cpu basis.  This allows us to allocate    more irqs as we have more cpus.  This is principally useful on systems    with msi devices which may want many irqs per-cpu.
    Discussed with:	jhb
    Sponsored by:	Nokia

--- sys/amd64/amd64/msi.c
+++ sys/amd64/amd64/msi.c
@@ -161,7 +161,9 @@ msi_enable_intr(struct intsrc *isrc)
 {
 	struct msi_intsrc *msi = (struct msi_intsrc *)isrc;
 
-	apic_enable_vector(msi->msi_vector);
+	if (msi->msi_vector == 0)
+		msi_assign_cpu(isrc, 0);
+	apic_enable_vector(msi->msi_cpu, msi->msi_vector);
 }
 
 static void
@@ -169,7 +171,7 @@ msi_disable_intr(struct intsrc *isrc)
 {
 	struct msi_intsrc *msi = (struct msi_intsrc *)isrc;
 
-	apic_disable_vector(msi->msi_vector);
+	apic_disable_vector(msi->msi_cpu, msi->msi_vector);
 }
 
 static int
@@ -199,14 +201,34 @@ static void
 msi_assign_cpu(struct intsrc *isrc, u_int apic_id)
 {
 	struct msi_intsrc *msi = (struct msi_intsrc *)isrc;
+	int old_vector;
+	u_int old_id;
+	int vector;
 
+	/* Store information to free existing irq. */
+	old_vector = msi->msi_vector;
+	old_id = msi->msi_cpu;
+	if (old_vector && old_id == apic_id)
+		return;
+	/* Allocate IDT vector on this cpu. */
+	vector = apic_alloc_vector(apic_id, msi->msi_irq);
+	if (vector == 0)
+		return; /* XXX alloc_vector panics on failure. */
 	msi->msi_cpu = apic_id;
+	msi->msi_vector = vector;
 	if (bootverbose)
-		printf("msi: Assigning %s IRQ %d to local APIC %u\n",
+		printf("msi: Assigning %s IRQ %d to local APIC %u vector %u\n",
 		    msi->msi_msix ? "MSI-X" : "MSI", msi->msi_irq,
-		    msi->msi_cpu);	
+		    msi->msi_cpu, msi->msi_vector);
 	pci_remap_msi_irq(msi->msi_dev, msi->msi_irq);
+	/*
+	 * Free the old vector after the new one is established.  This is done
+	 * to prevent races where we could miss an interrupt.
+	 */
+	if (old_vector)
+		apic_free_vector(old_id, old_vector, msi->msi_irq);
 }
+
 
 void
 msi_init(void)
@@ -263,7 +285,7 @@ int
 msi_alloc(device_t dev, int count, int maxcount, int *irqs)
 {
 	struct msi_intsrc *msi, *fsrc;
-	int cnt, i, vector;
+	int cnt, i;
 
 	if (!msi_enabled)
 		return (ENXIO);
@@ -309,22 +331,12 @@ again:
 	/* Ok, we now have the IRQs allocated. */
 	KASSERT(cnt == count, ("count mismatch"));
 
-	/* Allocate 'count' IDT vectors. */
-	vector = apic_alloc_vectors(irqs, count, maxcount);
-	if (vector == 0) {
-		mtx_unlock(&msi_lock);
-		return (ENOSPC);
-	}
-
 	/* Assign IDT vectors and make these messages owned by 'dev'. */
 	fsrc = (struct msi_intsrc *)intr_lookup_source(irqs[0]);
 	for (i = 0; i < count; i++) {
 		msi = (struct msi_intsrc *)intr_lookup_source(irqs[i]);
 		msi->msi_dev = dev;
-		msi->msi_vector = vector + i;
-		if (bootverbose)
-			printf("msi: routing MSI IRQ %d to vector %u\n",
-			    msi->msi_irq, msi->msi_vector);
+		msi->msi_vector = 0;
 		msi->msi_first = fsrc;
 		KASSERT(msi->msi_intsrc.is_handlers == 0,
 		    ("dead MSI has handlers"));
@@ -377,14 +389,18 @@ msi_release(int *irqs, int count)
 		KASSERT(msi->msi_dev == first->msi_dev, ("owner mismatch"));
 		msi->msi_first = NULL;
 		msi->msi_dev = NULL;
-		apic_free_vector(msi->msi_vector, msi->msi_irq);
+		if (msi->msi_vector)
+			apic_free_vector(msi->msi_cpu, msi->msi_vector,
+			    msi->msi_irq);
 		msi->msi_vector = 0;
 	}
 
 	/* Clear out the first message. */
 	first->msi_first = NULL;
 	first->msi_dev = NULL;
-	apic_free_vector(first->msi_vector, first->msi_irq);
+	if (first->msi_vector)
+		apic_free_vector(first->msi_cpu, first->msi_vector,
+		    first->msi_irq);
 	first->msi_vector = 0;
 	first->msi_count = 0;
 
@@ -433,7 +449,7 @@ int
 msix_alloc(device_t dev, int *irq)
 {
 	struct msi_intsrc *msi;
-	int i, vector;
+	int i;
 
 	if (!msi_enabled)
 		return (ENXIO);
@@ -468,15 +484,9 @@ again:
 		goto again;
 	}
 
-	/* Allocate an IDT vector. */
-	vector = apic_alloc_vector(i);
-	if (bootverbose)
-		printf("msi: routing MSI-X IRQ %d to vector %u\n", msi->msi_irq,
-		    vector);
-
 	/* Setup source. */
 	msi->msi_dev = dev;
-	msi->msi_vector = vector;
+	msi->msi_vector = 0;
 	msi->msi_msix = 1;
 
 	KASSERT(msi->msi_intsrc.is_handlers == 0, ("dead MSI-X has handlers"));
@@ -508,7 +518,8 @@ msix_release(int irq)
 
 	/* Clear out the message. */
 	msi->msi_dev = NULL;
-	apic_free_vector(msi->msi_vector, msi->msi_irq);
+	if (msi->msi_vector)
+		apic_free_vector(msi->msi_cpu, msi->msi_vector, msi->msi_irq);
 	msi->msi_vector = 0;
 	msi->msi_msix = 0;
 
