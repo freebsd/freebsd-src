d4b7970dc8d1bc753e4df991d2f37cfaf102a292
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2009-02-04 20:26:27 +0000

    Fix shutdown routine to return 0 and change signature from void return to int.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@188126 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ata/atapi-fd.c
+++ sys/dev/ata/atapi-fd.c
@@ -132,13 +132,14 @@ afd_detach(device_t dev)
     return 0;
 }
 
-static void
+static int
 afd_shutdown(device_t dev)
 {
     struct ata_device *atadev = device_get_softc(dev);
 
     if (atadev->param.support.command2 & ATA_SUPPORT_FLUSHCACHE)
 	ata_controlcmd(dev, ATA_FLUSHCACHE, 0, 0, 0);
+    return 0;
 }
 
 static int
