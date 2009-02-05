0129b996aa3f9aab03e153f78d457ba7ee0d1f3b
imp <imp@FreeBSD.org>
2009-02-05 20:44:06 +0000

    Don't check for mapping above 4GB on 32-bit platforms.

--- sys/dev/exca/exca.c
+++ sys/dev/exca/exca.c
@@ -257,11 +257,13 @@ exca_mem_map(struct exca_softc *sc, int kind, struct resource *res)
 	if (win >= EXCA_MEM_WINS)
 		return (ENOSPC);
 	if (sc->flags & EXCA_HAS_MEMREG_WIN) {
+#ifdef _LP64
 		if (rman_get_start(res) >> (EXCA_MEMREG_WIN_SHIFT + 8) != 0) {
 			device_printf(sc->dev,
 			    "Does not support mapping above 4GB.");
 			return (EINVAL);
 		}
+#endif
 	} else {
 		if (rman_get_start(res) >> EXCA_MEMREG_WIN_SHIFT != 0) {
 			device_printf(sc->dev,
