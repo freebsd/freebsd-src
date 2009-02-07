e5440f43e6376c554b491896cdccd5af06093e26
Bjoern A. Zeeb <bz@FreeBSD.org>
2009-02-07 13:55:12 +0000

    MFC: r187684
      For consistency with prison_{local,remote,check}_ipN rename
      prison_getipN to prison_get_ipN.

--- sys/net/rtsock.c
+++ sys/net/rtsock.c
@@ -368,7 +368,7 @@ rtm_get_jailed(struct rt_addrinfo *info, struct ifnet *ifp,
 				 * 3. As a last resort return the 'default'
 				 * jail address.
 				 */
-				if (prison_getip4(cred, &ia) != 0)
+				if (prison_get_ip4(cred, &ia) != 0)
 					return (ESRCH);
 			}
 			bzero(&saun->sin, sizeof(struct sockaddr_in));
@@ -420,7 +420,7 @@ rtm_get_jailed(struct rt_addrinfo *info, struct ifnet *ifp,
 				 * 3. As a last resort return the 'default'
 				 * jail address.
 				 */
-				if (prison_getip6(cred, &ia6) != 0)
+				if (prison_get_ip6(cred, &ia6) != 0)
 					return (ESRCH);
 			}
 			bzero(&saun->sin6, sizeof(struct sockaddr_in6));
