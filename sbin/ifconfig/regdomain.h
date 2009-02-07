a36d1721a9e0e3451194ee15e830ebec432849ee
sam <sam@FreeBSD.org>
2009-02-07 01:51:22 +0000

    merge r188258: regulatory fixups

--- sbin/ifconfig/regdomain.h
+++ sbin/ifconfig/regdomain.h
@@ -45,6 +45,13 @@ struct freqband {
 	LIST_ENTRY(freqband) next;
 };
 
+/* private flags, don't pass to os */
+#define	REQ_ECM		0x1		/* enable if ECM set */
+#define	REQ_INDOOR	0x2		/* enable only for indoor operation */
+#define	REQ_OUTDOOR	0x4		/* enable only for outdoor operation */
+
+#define	REQ_FLAGS	(REQ_ECM|REQ_INDOOR|REQ_OUTDOOR)
+
 struct netband {
 	const struct freqband *band;	/* channel list description */
 	uint8_t		maxPower;	/* regulatory cap on tx power (dBm) */
