58ae70168f2db0e4761c33cfa08aac170459fac5
Sam Leffler <sam@FreeBSD.org>
2009-02-07 01:53:44 +0000

    add macro for future regulatory mods

--- sys/dev/ath/ath_hal/ah_regdomain.c
+++ sys/dev/ath/ath_hal/ah_regdomain.c
@@ -74,6 +74,11 @@ typedef uint64_t chanbmask_t[BMLEN];
 	  W0(_fg) | W0(_fh) ,	\
 	  W1(_fa) | W1(_fb) | W1(_fc) | W1(_fd) | W1(_fe) | W1(_ff) |	\
 	  W1(_fg) | W1(_fh) }
+#define BM9(_fa, _fb, _fc, _fd, _fe, _ff, _fg, _fh, _fi)	\
+	{ W0(_fa) | W0(_fb) | W0(_fc) | W0(_fd) | W0(_fe) | W0(_ff) |	\
+	  W0(_fg) | W0(_fh) | W0(_fi) ,	\
+	  W1(_fa) | W1(_fb) | W1(_fc) | W1(_fd) | W1(_fe) | W1(_ff) |	\
+	  W1(_fg) | W1(_fh) | W1(_fi) }
 
 /*
  * Mask to check whether a domain is a multidomain or a single domain
