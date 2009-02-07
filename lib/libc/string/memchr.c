0ff7d9b4e9566cf24bac5fc2eecadc2bd158a184
Warner Losh <imp@FreeBSD.org>
2009-02-07 19:34:44 +0000

    Make sure the comparison is done with an unsigned char.

--- lib/libc/string/memchr.c
+++ lib/libc/string/memchr.c
@@ -45,7 +45,7 @@ memchr(const void *s, int c, size_t n)
 		const unsigned char *p = s;
 
 		do {
-			if (*p++ == c)
+			if (*p++ == (unsigned char)c)
 				return ((void *)(p - 1));
 		} while (--n != 0);
 	}
