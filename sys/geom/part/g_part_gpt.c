e4e15d06467ab2073cbf6b047bd970d29f1cd368
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2009-02-10 02:43:07 +0000

    Fix g_part_dumpconf and g_part_name prototpyes.
    Submitted by:	marcel@
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@188429 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/geom/part/g_part_gpt.c
+++ sys/geom/part/g_part_gpt.c
@@ -91,12 +91,12 @@ static int g_part_gpt_add(struct g_part_table *, struct g_part_entry *,
 static int g_part_gpt_bootcode(struct g_part_table *, struct g_part_parms *);
 static int g_part_gpt_create(struct g_part_table *, struct g_part_parms *);
 static int g_part_gpt_destroy(struct g_part_table *, struct g_part_parms *);
-static int g_part_gpt_dumpconf(struct g_part_table *, struct g_part_entry *,
+static void g_part_gpt_dumpconf(struct g_part_table *, struct g_part_entry *,
     struct sbuf *, const char *);
 static int g_part_gpt_dumpto(struct g_part_table *, struct g_part_entry *);
 static int g_part_gpt_modify(struct g_part_table *, struct g_part_entry *,  
     struct g_part_parms *);
-static char *g_part_gpt_name(struct g_part_table *, struct g_part_entry *,
+static const char *g_part_gpt_name(struct g_part_table *, struct g_part_entry *,
     char *, size_t);
 static int g_part_gpt_probe(struct g_part_table *, struct g_consumer *);
 static int g_part_gpt_read(struct g_part_table *, struct g_consumer *);
@@ -447,7 +447,7 @@ g_part_gpt_destroy(struct g_part_table *basetable, struct g_part_parms *gpp)
 	return (0);
 }
 
-static int
+static void
 g_part_gpt_dumpconf(struct g_part_table *table, struct g_part_entry *baseentry, 
     struct sbuf *sb, const char *indent)
 {
@@ -470,7 +470,6 @@ g_part_gpt_dumpconf(struct g_part_table *table, struct g_part_entry *baseentry,
 	} else {
 		/* confxml: scheme information */
 	}
-	return (0);
 }
 
 static int
@@ -502,7 +501,7 @@ g_part_gpt_modify(struct g_part_table *basetable,
 	return (0);
 }
 
-static char *
+static const char *
 g_part_gpt_name(struct g_part_table *table, struct g_part_entry *baseentry,
     char *buf, size_t bufsz)
 {
--- sys/geom/part/g_part_pc98.c
+++ sys/geom/part/g_part_pc98.c
@@ -71,12 +71,12 @@ static int g_part_pc98_add(struct g_part_table *, struct g_part_entry *,
 static int g_part_pc98_bootcode(struct g_part_table *, struct g_part_parms *);
 static int g_part_pc98_create(struct g_part_table *, struct g_part_parms *);
 static int g_part_pc98_destroy(struct g_part_table *, struct g_part_parms *);
-static int g_part_pc98_dumpconf(struct g_part_table *, struct g_part_entry *,
+static void g_part_pc98_dumpconf(struct g_part_table *, struct g_part_entry *,
     struct sbuf *, const char *);
 static int g_part_pc98_dumpto(struct g_part_table *, struct g_part_entry *);
 static int g_part_pc98_modify(struct g_part_table *, struct g_part_entry *,  
     struct g_part_parms *);
-static char *g_part_pc98_name(struct g_part_table *, struct g_part_entry *,
+static const char *g_part_pc98_name(struct g_part_table *, struct g_part_entry *,
     char *, size_t);
 static int g_part_pc98_probe(struct g_part_table *, struct g_consumer *);
 static int g_part_pc98_read(struct g_part_table *, struct g_consumer *);
@@ -257,7 +257,7 @@ g_part_pc98_destroy(struct g_part_table *basetable, struct g_part_parms *gpp)
 	return (0);
 }
 
-static int
+static void
 g_part_pc98_dumpconf(struct g_part_table *table,
     struct g_part_entry *baseentry, struct sbuf *sb, const char *indent)
 {
@@ -268,7 +268,6 @@ g_part_pc98_dumpconf(struct g_part_table *table,
 	entry = (struct g_part_pc98_entry *)baseentry;
 	if (entry == NULL) {
 		/* confxml: scheme information */
-		return (0);
 	}
 
 	type = entry->ent.dp_mid + (entry->ent.dp_sid << 8);
@@ -288,7 +287,6 @@ g_part_pc98_dumpconf(struct g_part_table *table,
 		sbuf_printf(sb, "%s<rawtype>%u</rawtype>\n", indent,
 		    type & 0x7f7f);
 	}
-	return (0);
 }
 
 static int
@@ -318,7 +316,7 @@ g_part_pc98_modify(struct g_part_table *basetable,
 	return (0);
 }
 
-static char *
+static const char *
 g_part_pc98_name(struct g_part_table *table, struct g_part_entry *baseentry,
     char *buf, size_t bufsz)
 {
