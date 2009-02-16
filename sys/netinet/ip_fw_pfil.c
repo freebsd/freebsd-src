d8d42f3f4ea517542f276f595304cd0ec4162e55
Luigi Rizzo <luigi@FreeBSD.org>
2009-02-16 15:10:51 +0000

    correct some #include

--- sys/netinet/ip_fw_pfil.c
+++ sys/netinet/ip_fw_pfil.c
@@ -51,13 +51,13 @@ __FBSDID("$FreeBSD$");
 #include <sys/ucred.h>
 #include <sys/vimage.h>
 
+#define _NET_IF_VAR_H_	/* we don't want if_var.h, only if.h */
 #include <net/if.h>
 #include <net/route.h>
 #include <net/pfil.h>
 
 #include <netinet/in.h>
 #include <netinet/in_systm.h>
-#include <netinet/in_var.h>
 #include <netinet/ip.h>
 #include <netinet/ip_var.h>
 #include <netinet/ip_fw.h>
