0e38e31f496589a02cb6c13a6ee7733ff568c61a
David Christensen <davidch@FreeBSD.org>
2009-02-27 19:25:06 +0000

    - Update copyright to 2009. - Only enable split header operation when ZERO_COPY_SOCKETS is enabled in   the kernel.
    Submitted by:	davidch
    MFC after:	1 week

--- sys/dev/bce/if_bcefw.h
+++ sys/dev/bce/if_bcefw.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2006-2008 Broadcom Corporation
+ * Copyright (c) 2006-2009 Broadcom Corporation
  *	David Christensen <davidch@broadcom.com>.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
