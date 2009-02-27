561cf8ff9ab71f5b74ab7388f6b6d3adf9d1c3e3
jhb <jhb@FreeBSD.org>
2009-02-27 15:25:47 +0000

    Catch up to the 6GB KVA changes.  The original superpages patches on 7 were tested prior to the MFC of the 6GB KVA changes, and I failed to do an extra sanity check yesterday.  This should fix the pmap_init() panics.
    Pointy hat to:	jhb

--- sys/amd64/amd64/pmap.c
+++ sys/amd64/amd64/pmap.c
@@ -631,15 +631,17 @@ pmap_init(void)
 	 * Initialize the vm page array entries for the kernel pmap's
 	 * page table pages.
 	 */ 
-	pd = pmap_pde(kernel_pmap, VM_MIN_KERNEL_ADDRESS);
+	pd = pmap_pde(kernel_pmap, KERNBASE);
 	for (i = 0; i < NKPT; i++) {
 		if ((pd[i] & (PG_PS | PG_V)) == (PG_PS | PG_V))
 			continue;
+		KASSERT((pd[i] & PG_V) != 0,
+		    ("pmap_init: page table page is missing"));
 		mpte = PHYS_TO_VM_PAGE(pd[i] & PG_FRAME);
 		KASSERT(mpte >= vm_page_array &&
 		    mpte < &vm_page_array[vm_page_array_size],
 		    ("pmap_init: page table page is out of range"));
-		mpte->pindex = pmap_pde_pindex(VM_MIN_KERNEL_ADDRESS) + i;
+		mpte->pindex = pmap_pde_pindex(KERNBASE) + i;
 		mpte->phys_addr = pd[i] & PG_FRAME;
 	}
 
