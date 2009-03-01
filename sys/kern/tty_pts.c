712b938977d981b6dec1853839f0558a30b459e1
ed <ed@FreeBSD.org>
2009-03-01 09:50:13 +0000

    Improve my previous changes to the TTY code: also remove memcpy().
    It's better to just use internal language constructs, because it is
    likely the compiler has a better opinion on whether to perform inlining,
    which is very likely to happen to struct winsize.
    
    Submitted by:	Christoph Mallon <christoph mallon gmx de>

--- sys/kern/tty_pts.c
+++ sys/kern/tty_pts.c
@@ -310,7 +310,7 @@ ptsdev_ioctl(struct file *fp, u_long cmd, void *data,
 	case TIOCGETA:
 		/* Obtain terminal flags through tcgetattr(). */
 		tty_lock(tp);
-		memcpy(data, &tp->t_termios, sizeof(struct termios));
+		*(struct termios*)data = tp->t_termios;
 		tty_unlock(tp);
 		return (0);
 #endif /* PTS_LINUX */
