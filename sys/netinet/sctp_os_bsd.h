f2254573b9b4dd71026af9f45fe5afc41b9aff24
rrs <rrs@FreeBSD.org>
2009-03-04 20:54:42 +0000

    - PR-SCTP bug, where the CUM-ACK was not being updated   into the advance_peer_ack point so we would incorrectly   send a wrong value in the FWD-TSN - PR-SCTP bug, where an PR packet is used for a window   probe which could incorrectly get the packet moved   back into the send_queue, which will cause major issues and   should not happen. - Fix a trace to use the proper macro.

--- sys/netinet/sctp_os_bsd.h
+++ sys/netinet/sctp_os_bsd.h
@@ -196,7 +196,7 @@ MALLOC_DECLARE(SCTP_M_SOCKOPT);
 #define SCTP_PRINTF(params...)	printf(params)
 
 #ifdef SCTP_LTRACE_CHUNKS
-#define SCTP_LTRACE_CHK(a, b, c, d) if(SCTP_BASE_SYSCTL(sctp_logging_level) & SCTP_LTRACE_CHUNK_ENABLE) CTR6(KTR_SUBSYS, "SCTP:%d[%d]:%x-%x-%x-%x", SCTP_LOG_CHUNK_PROC, 0, a, b, c, d)
+#define SCTP_LTRACE_CHK(a, b, c, d) if(SCTP_BASE_SYSCTL(sctp_logging_level) & SCTP_LTRACE_CHUNK_ENABLE) SCTP_CTR6(KTR_SUBSYS, "SCTP:%d[%d]:%x-%x-%x-%x", SCTP_LOG_CHUNK_PROC, 0, a, b, c, d)
 #else
 #define SCTP_LTRACE_CHK(a, b, c, d)
 #endif
