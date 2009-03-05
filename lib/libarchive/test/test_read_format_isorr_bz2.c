a60167b66c90489060a2fa5d9d6175041aa0a6d8
Tim Kientzle <kientzle@FreeBSD.org>
2009-03-05 00:42:50 +0000

    Merge r357 from libarchive.googlecode.com: bzip2 compression support can always be enabled even if bzlib doesn't exist on this platform; don't give up until we fail to open the file.

--- lib/libarchive/test/test_read_format_isorr_bz2.c
+++ lib/libarchive/test/test_read_format_isorr_bz2.c
@@ -55,15 +55,15 @@ DEFINE_TEST(test_read_format_isorr_bz2)
 
 	extract_reference_file(refname);
 	assert((a = archive_read_new()) != NULL);
-	r = archive_read_support_compression_bzip2(a);
+	assertEqualInt(0, archive_read_support_compression_bzip2(a));
+	assertEqualInt(0, archive_read_support_format_all(a));
+	r = archive_read_open_filename(a, refname, 10240);
 	if (r == ARCHIVE_FATAL) {
 		skipping("Bzip2 decompression unsupported on this platform");
 		archive_read_finish(a);
 		return;
 	}
 	assertEqualInt(0, r);
-	assertEqualInt(0, archive_read_support_format_all(a));
-	assertEqualInt(0, archive_read_open_filename(a, refname, 10240));
 
 	/* First entry is '.' root directory. */
 	assertEqualInt(0, archive_read_next_header(a, &ae));
