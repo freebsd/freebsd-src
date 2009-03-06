25a2b7a69010f1be23c7c77ed1722108cc380b08
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2009-03-06 15:35:37 +0000

    Extract the no_poll() and vop_nopoll() code into the common routine poll_no_poll(). Return a poll_no_poll() result from devfs_poll_f() when filedescriptor does not reference the live cdev, instead of ENXIO.
    Noted and tested by:	hps
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@189450 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/sys_generic.c
+++ sys/kern/sys_generic.c
@@ -731,6 +731,22 @@ out:
 	return (error);
 }
 
+int
+poll_no_poll(int events)
+{
+	/*
+	 * Return true for read/write.  If the user asked for something
+	 * special, return POLLNVAL, so that clients have a way of
+	 * determining reliably whether or not the extended
+	 * functionality is present without hard-coding knowledge
+	 * of specific filesystem implementations.
+	 */
+	if (events & ~POLLSTANDARD)
+		return (POLLNVAL);
+
+	return (events & (POLLIN | POLLOUT | POLLRDNORM | POLLWRNORM));
+}
+
 #ifndef _SYS_SYSPROTO_H_
 struct select_args {
 	int	nd;
