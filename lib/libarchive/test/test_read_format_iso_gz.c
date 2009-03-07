cdad0e17a1d3b0ff7e1e38220d4cea0b76c8b85a
Tim Kientzle <kientzle@FreeBSD.org>
2009-03-07 02:24:32 +0000

    Merge r558,567,569,571,581,582,583,598 from libarchive.googlecode.com: Support Joliet extensions.  This currently ignores Rockridge extensions if both exist on the same disk unless the '!joliet' option is provided. e.g.: tar -xvf example.iso --options '!joliet' Thanks to: Andreas Henriksson

--- lib/libarchive/test/test_read_format_iso_gz.c
+++ lib/libarchive/test/test_read_format_iso_gz.c
@@ -57,17 +57,20 @@ DEFINE_TEST(test_read_format_iso_gz)
 	struct archive_entry *ae;
 	struct archive *a;
 	assert((a = archive_read_new()) != NULL);
-	assert(0 == archive_read_support_compression_all(a));
-	assert(0 == archive_read_support_format_all(a));
-	assert(0 == archive_read_open_memory(a, archive, sizeof(archive)));
-	assert(0 == archive_read_next_header(a, &ae));
-	assert(archive_compression(a) == ARCHIVE_COMPRESSION_GZIP);
-	assert(archive_format(a) == ARCHIVE_FORMAT_ISO9660);
-	assert(0 == archive_read_close(a));
+	assertEqualIntA(a, ARCHIVE_OK,
+	    archive_read_support_compression_all(a));
+	assertEqualIntA(a, ARCHIVE_OK,
+	    archive_read_support_format_all(a));
+	assertEqualIntA(a, ARCHIVE_OK,
+	    archive_read_open_memory(a, archive, sizeof(archive)));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
+	assertEqualInt(archive_compression(a), ARCHIVE_COMPRESSION_GZIP);
+	assertEqualInt(archive_format(a), ARCHIVE_FORMAT_ISO9660);
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_close(a));
 #if ARCHIVE_VERSION_NUMBER < 2000000
 	archive_read_finish(a);
 #else
-	assert(0 == archive_read_finish(a));
+	assertEqualInt(ARCHIVE_OK, archive_read_finish(a));
 #endif
 #else
 	skipping("Need zlib");
