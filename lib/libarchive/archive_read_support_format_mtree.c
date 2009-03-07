eee9002fb2ebd161dca8891c0e4de94c9ad077e0
Tim Kientzle <kientzle@FreeBSD.org>
2009-03-07 00:52:02 +0000

    Merge r552,r559 from libarchive.googlecode.com: Support high-resolution timestamps on Tru64, AIX, and GNU Hurd.  Thanks to Björn Jacke.

--- lib/libarchive/archive_read_support_format_mtree.c
+++ lib/libarchive/archive_read_support_format_mtree.c
@@ -672,6 +672,15 @@ parse_file(struct archive_read *a, struct archive_entry *entry,
 #elif HAVE_STRUCT_STAT_ST_MTIM_TV_NSEC
 			archive_entry_set_mtime(entry, st->st_mtime,
 			    st->st_mtim.tv_nsec);
+#elif HAVE_STRUCT_STAT_ST_MTIME_N
+			archive_entry_set_mtime(entry, st->st_mtime,
+			    st->st_mtime_n);
+#elif HAVE_STRUCT_STAT_ST_UMTIME
+			archive_entry_set_mtime(entry, st->st_mtime,
+			    st->st_umtime*1000);
+#elif HAVE_STRUCT_STAT_ST_MTIME_USEC
+			archive_entry_set_mtime(entry, st->st_mtime,
+			    st->st_mtime_usec*1000);
 #else
 			archive_entry_set_mtime(entry, st->st_mtime, 0);
 #endif
