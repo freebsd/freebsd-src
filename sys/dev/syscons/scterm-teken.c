b2737cb9ed157467f6f1fb032e215d056013388e
ed <ed@FreeBSD.org>
2009-03-10 11:28:54 +0000

    Make a 1:1 mapping between syscons stats and terminal emulators.
    After I imported libteken into the source tree, I noticed syscons didn't
    store the cursor position inside the terminal emulator, but inside the
    virtual terminal stat. This is not very useful, because when you
    implement more complex forms of line wrapping, you need to keep track of
    more state than just the cursor position.
    
    Because the kernel messages didn't share the same terminal emulator as
    ttyv0, this caused a lot of strange things, like kernel messages being
    misplaced and a missing notification to resize the terminal emulator for
    kernel messages never to be resized when using vidcontrol.
    
    This patch just removes kernel_console_ts and adds a special parameter
    to te_puts to determine whether messages should be printed using regular
    colors or the ones for kernel messages.
    
    Reported by:	ache
    Tested by:	nyan, garga (older version)

--- sys/dev/syscons/scterm-teken.c
+++ sys/dev/syscons/scterm-teken.c
@@ -153,12 +153,23 @@ scteken_term(scr_stat *scp, void **softc)
 }
 
 static void
-scteken_puts(scr_stat *scp, u_char *buf, int len)
+scteken_puts(scr_stat *scp, u_char *buf, int len, int kernel)
 {
 	teken_stat *ts = scp->ts;
+	teken_attr_t backup, kattr;
 
 	scp->sc->write_in_progress++;
-	teken_input(&ts->ts_teken, buf, len);
+	if (kernel) {
+		/* Use special colors for kernel messages. */
+		backup = *teken_get_curattr(&ts->ts_teken);
+		scteken_revattr(SC_KERNEL_CONS_ATTR, &kattr);
+		teken_set_curattr(&ts->ts_teken, &kattr);
+		teken_input(&ts->ts_teken, buf, len);
+		teken_set_curattr(&ts->ts_teken, &backup);
+	} else {
+		/* Print user messages with regular colors. */
+		teken_input(&ts->ts_teken, buf, len);
+	}
 	scp->sc->write_in_progress--;
 }
 
