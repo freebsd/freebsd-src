6dbdc4d5cd66f7177ae3d77dc2950617814c7e97
lulf <lulf@FreeBSD.org>
2009-03-11 22:33:18 +0000

    - Remove the system directory to to a better re-import where we don't have to   deal with conflicts when MFHing.

--- sys/arm/include/ptrace.h
+++
@@ -1,8 +0,0 @@
-/*	$NetBSD: ptrace.h,v 1.2 2001/02/23 21:23:52 reinoud Exp $	*/
-/* $FreeBSD$ */
-
-#ifndef _MACHINE_PTRACE_H_
-#define _MACHINE_PTRACE_H_
-
-#endif /* !_MACHINE_PTRACE_H */
-
--- sys/arm/include/smp.h
+++
@@ -1,6 +0,0 @@
-/* $FreeBSD$ */
-
-#ifndef _MACHINE_SMP_H_
-#define _MACHINE_SMP_H_
-
-#endif /* !_MACHINE_SMP_H_ */
