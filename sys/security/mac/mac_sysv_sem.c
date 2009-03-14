3f00942ac8e08ce2e89e849f9f4b51982b16e817
rwatson <rwatson@FreeBSD.org>
2009-03-14 16:06:06 +0000

    Rework MAC Framework synchronization in a number of ways in order to improve performance:
    - Eliminate custom reference count and condition variable to monitor
      threads entering the framework, as this had both significant overhead
      and behaved badly in the face of contention.
    
    - Replace reference count with two locks: an rwlock and an sx lock,
      which will be read-acquired by threads entering the framework
      depending on whether a give policy entry point is permitted to sleep
      or not.
    
    - Replace previous mutex locking of the reference count for exclusive
      access with write acquiring of both the policy list sx and rw locks,
      which occurs only when policies are attached or detached.
    
    - Do a lockless read of the dynamic policy list head before acquiring
      any locks in order to reduce overhead when no dynamic policies are
      loaded; this a race we can afford to lose.
    
    - For every policy entry point invocation, decide whether sleeping is
      permitted, and if not, use a _NOSLEEP() variant of the composition
      macros, which will use the rwlock instead of the sxlock.  In some
      cases, we decide which to use based on allocation flags passed to the
      MAC Framework entry point.
    
    As with the move to rwlocks/rmlocks in pfil, this may trigger witness
    warnings, but these should (generally) be false positives as all
    acquisition of the locks is for read with two very narrow exceptions
    for policy load/unload, and those code blocks should never acquire
    other locks.
    
    Sponsored by:	Google, Inc.
    Obtained from:	TrustedBSD Project
    Discussed with:	csjp (idea, not specific patch)

--- sys/security/mac/mac_sysv_sem.c
+++ sys/security/mac/mac_sysv_sem.c
@@ -86,7 +86,7 @@ static void
 mac_sysv_sem_label_free(struct label *label)
 {
 
-	MAC_PERFORM(sysvsem_destroy_label, label);
+	MAC_PERFORM_NOSLEEP(sysvsem_destroy_label, label);
 	mac_labelzone_free(label);
 }
 
@@ -104,14 +104,14 @@ void
 mac_sysvsem_create(struct ucred *cred, struct semid_kernel *semakptr)
 {
 
-	MAC_PERFORM(sysvsem_create, cred, semakptr, semakptr->label);
+	MAC_PERFORM_NOSLEEP(sysvsem_create, cred, semakptr, semakptr->label);
 }
 
 void
 mac_sysvsem_cleanup(struct semid_kernel *semakptr)
 {
 
-	MAC_PERFORM(sysvsem_cleanup, semakptr->label);
+	MAC_PERFORM_NOSLEEP(sysvsem_cleanup, semakptr->label);
 }
 
 MAC_CHECK_PROBE_DEFINE3(sysvsem_check_semctl, "struct ucred *",
@@ -123,8 +123,8 @@ mac_sysvsem_check_semctl(struct ucred *cred, struct semid_kernel *semakptr,
 {
 	int error;
 
-	MAC_CHECK(sysvsem_check_semctl, cred, semakptr, semakptr->label,
-	    cmd);
+	MAC_CHECK_NOSLEEP(sysvsem_check_semctl, cred, semakptr,
+	    semakptr->label, cmd);
 	MAC_CHECK_PROBE3(sysvsem_check_semctl, error, cred, semakptr, cmd);
 
 	return (error);
@@ -138,7 +138,8 @@ mac_sysvsem_check_semget(struct ucred *cred, struct semid_kernel *semakptr)
 {
 	int error;
 
-	MAC_CHECK(sysvsem_check_semget, cred, semakptr, semakptr->label);
+	MAC_CHECK_NOSLEEP(sysvsem_check_semget, cred, semakptr,
+	    semakptr->label);
 
 	return (error);
 }
@@ -152,8 +153,8 @@ mac_sysvsem_check_semop(struct ucred *cred, struct semid_kernel *semakptr,
 {
 	int error;
 
-	MAC_CHECK(sysvsem_check_semop, cred, semakptr, semakptr->label,
-	    accesstype);
+	MAC_CHECK_NOSLEEP(sysvsem_check_semop, cred, semakptr,
+	    semakptr->label, accesstype);
 	MAC_CHECK_PROBE3(sysvsem_check_semop, error, cred, semakptr,
 	    accesstype);
 
