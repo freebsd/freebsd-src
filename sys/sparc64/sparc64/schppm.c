0b8d626f08691662ddd1e723f2527da7cb67a534
marius <marius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2009-03-19 20:38:45 +0000

    Take advantage of KOBJMETHOD_END.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@190103 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sparc64/sparc64/schppm.c
+++ sys/sparc64/sparc64/schppm.c
@@ -73,7 +73,7 @@ static device_method_t schppm_methods[] = {
 	DEVMETHOD(device_probe,		schppm_probe),
 	DEVMETHOD(device_attach,	schppm_attach),
 
-	{ NULL, NULL }
+	KOBJMETHOD_END
 };
 
 static devclass_t schppm_devclass;
