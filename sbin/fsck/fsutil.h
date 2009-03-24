ebeb22fe92c6a96b4427e8dde11e4002edc9f8bb
David E. O'Brien <obrien@FreeBSD.org>
2009-03-24 01:51:42 +0000

    MFC: r186581: Sound less scary about errorousous disk geometry.

--- sbin/fsck/fsutil.h
+++ sbin/fsck/fsutil.h
@@ -48,7 +48,6 @@ char *estrdup(const char *);
 #define	CHECK_DEBUG	0x0004
 #define	CHECK_BACKGRD	0x0008
 #define	DO_BACKGRD	0x0010
-#define	CHECK_CLEAN	0x0020
 
 struct fstab;
 int checkfstab(int, int (*)(struct fstab *), 
