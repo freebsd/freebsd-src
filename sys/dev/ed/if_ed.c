82e7564bb1767dc0f07f4091fee31fb7973c546c
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2009-03-30 16:15:06 +0000

    This is a major reworking of the AX88x90 support.
    o Introduce new chip_type AX88790.  There's a few places we need to know the
      exact chip for workaronds.
    o Explain the AX88190 workaround for the ISR bits being stuck, and don't
      apply them to the AX88790.  The datasheet says the bits are fixed, and
      experience confirms.
    o Fix mii bit-bang read code to read and discard the 'floating' bit.
    o Remove empty ed_pccard_ax88x90_mii_reset routine
    o Report error from mii_phy_probe
    o Don't use ed_probe_Novel_generic for ax88x90 chips.  It puts them into
      an odd state sometimes.  Instead, use a more stream-lined version that
      avoids the trouble spots.  This was copied and tweaked from the original.
    o Move chip reset into its own routine.
    o Minor code optimiation on getting MAC address
    o Add code for coping with AX88790 cards that are in power down state and
      need to be kicked before the PHY registers for the internal phy read right.
    o Remove ugly cap of PHYs at 17.
    o For AX88790, we need to set a special bit for accessig phy 16 (the internal
      phy) and clear it for all others according to a chip erratum.
    o streamline the bit-bang code for AX88x90: the delays aren't needed according
      to the datasheet timing diagrams and also the Linux driver
    o Fix minor bit definition for direction bit.
    o Generally: Some comments reformatted
    o Only try the toshiba probe on cards labelled as toshiba
    
    # From another Akihabara card (this one from a few years ago from a
    # friend in Japan).  Fix the Corega FEther II PCC-TXD.  This one is
    # still on sale new, as of a few weeks ago.  should fix all other AX88x90
    # based cards, but I have some testing left to finish on my collection...
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@190559 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ed/if_ed.c
+++ sys/dev/ed/if_ed.c
@@ -427,10 +427,17 @@ ed_stop_hw(struct ed_softc *sc)
 	 * Wait for interface to enter stopped state, but limit # of checks to
 	 * 'n' (about 5ms). It shouldn't even take 5us on modern DS8390's, but
 	 * just in case it's an old one.
+	 *
+	 * The AX88190 and AX88190A chips have a problem with this, it seems,
+	 * but there's no evidence that I've found for excluding the check.
+	 * This may be due to the cryptic references to the ISR register being
+	 * fixed in the AX88790.
 	 */
 	if (sc->chip_type != ED_CHIP_TYPE_AX88190)
 		while (((ed_nic_inb(sc, ED_P0_ISR) & ED_ISR_RST) == 0) && --n)
 			continue;
+	if (n <= 0)
+		device_printf(sc->dev, "ed_stop_hw RST never set\n");
 }
 
 /*
@@ -916,10 +923,10 @@ edintr(void *arg)
 	ed_nic_outb(sc, ED_P0_CR, sc->cr_proto | ED_CR_STA);
 
 	/*
-	 * loop until there are no more new interrupts.  When the card
-	 * goes away, the hardware will read back 0xff.  Looking at
-	 * the interrupts, it would appear that 0xff is impossible,
-	 * or at least extremely unlikely.
+	 * loop until there are no more new interrupts.  When the card goes
+	 * away, the hardware will read back 0xff.  Looking at the interrupts,
+	 * it would appear that 0xff is impossible, or at least extremely
+	 * unlikely.
 	 */
 	while ((isr = ed_nic_inb(sc, ED_P0_ISR)) != 0 && isr != 0xff) {
 
@@ -930,12 +937,14 @@ edintr(void *arg)
 		 */
 		ed_nic_outb(sc, ED_P0_ISR, isr);
 
-		/* 
-		 * XXX workaround for AX88190
+		/*
+		 * The AX88190 and AX88190A has problems acking an interrupt
+		 * and having them clear.  This interferes with top-level loop
+		 * here.  Wait for all the bits to clear.
+		 *
 		 * We limit this to 5000 iterations.  At 1us per inb/outb,
-		 * this translates to about 15ms, which should be plenty
-		 * of time, and also gives protection in the card eject
-		 * case.
+		 * this translates to about 15ms, which should be plenty of
+		 * time, and also gives protection in the card eject case.
 		 */
 		if (sc->chip_type == ED_CHIP_TYPE_AX88190) {
 			count = 5000;		/* 15ms */
@@ -1530,7 +1539,8 @@ ed_setrcr(struct ed_softc *sc)
 	ED_ASSERT_LOCKED(sc);
 
 	/* Bit 6 in AX88190 RCR register must be set. */
-	if (sc->chip_type == ED_CHIP_TYPE_AX88190)
+	if (sc->chip_type == ED_CHIP_TYPE_AX88190 ||
+	    sc->chip_type == ED_CHIP_TYPE_AX88790)
 		reg1 = ED_RCR_INTT;
 	else
 		reg1 = 0x00;
