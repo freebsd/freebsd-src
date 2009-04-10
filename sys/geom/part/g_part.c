853a10a5819946fb176b743e8b1aef0e057e29c0
Andrew Thompson <thompsa@FreeBSD.org>
2009-04-10 04:08:34 +0000

    Revert r190676,190677
    The geom and CAM changes for root_hold are the wrong solution for USB design
    quirks.
    
    Requested by:	scottl

--- sys/geom/part/g_part.c
+++ sys/geom/part/g_part.c
@@ -1474,7 +1474,7 @@ g_part_taste(struct g_class *mp, struct g_provider *pp, int flags __unused)
 		return (NULL);
 	}
 
-	rht = root_mount_hold(mp->name, M_WAITOK);
+	rht = root_mount_hold(mp->name);
 	g_topology_unlock();
 
 	/*
