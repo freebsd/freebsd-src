b4e51c618e05c4345201aafdcfa9eefd7aa5b36f
Xin LI <delphij@FreeBSD.org>
2009-04-11 15:21:11 +0000

    MFC r190482: zero out memory before use and free.
    Security:       Potential Information Leak

--- lib/libc/db/btree/bt_split.c
+++ lib/libc/db/btree/bt_split.c
@@ -385,13 +385,10 @@ bt_page(t, h, lp, rp, skip, ilen)
 	}
 
 	/* Put the new left page for the split into place. */
-	if ((l = (PAGE *)malloc(t->bt_psize)) == NULL) {
+	if ((l = (PAGE *)calloc(1, t->bt_psize)) == NULL) {
 		mpool_put(t->bt_mp, r, 0);
 		return (NULL);
 	}
-#ifdef PURIFY
-	memset(l, 0xff, t->bt_psize);
-#endif
 	l->pgno = h->pgno;
 	l->nextpg = r->pgno;
 	l->prevpg = h->prevpg;
