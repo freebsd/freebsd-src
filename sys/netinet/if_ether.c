582b6122ab661d26235a7d9a385d904018a2cf11
Kip Macy <kmacy@FreeBSD.org>
2009-04-15 20:49:59 +0000

    make LLTABLE visible to netinet

--- sys/netinet/if_ether.c
+++ sys/netinet/if_ether.c
@@ -81,8 +81,6 @@ __FBSDID("$FreeBSD$");
 
 #define SIN(s) ((struct sockaddr_in *)s)
 #define SDL(s) ((struct sockaddr_dl *)s)
-#define LLTABLE(ifp)	\
-	((struct in_ifinfo *)(ifp)->if_afdata[AF_INET])->ii_llt
 
 SYSCTL_DECL(_net_link_ether);
 SYSCTL_NODE(_net_link_ether, PF_INET, inet, CTLFLAG_RW, 0, "");
