ef1a18e0032830d53ca66fad71a54e74621d8299
rwatson <rwatson@FreeBSD.org>
2009-04-20 12:59:23 +0000

    Don't conditionally define CACHE_LINE_SHIFT, as we anticipate sizing a fair number of static data structures, making this an unlikely option to try to change without also changing source code. [1]
    Change default cache line size on ia64, sparc64, and sun4v to 128
    bytes, as this was what rtld-elf was already using on those
    platforms. [2]
    
    Suggested by:	bde [1], jhb [2]
    MFC after:	2 weeks

--- sys/sparc64/include/param.h
+++ sys/sparc64/include/param.h
@@ -75,9 +75,7 @@
  * CACHE_LINE_SIZE is the compile-time maximum cache line size for an
  * architecture.  It should be used with appropriate caution.
  */
-#ifndef CACHE_LINE_SHIFT
-#define	CACHE_LINE_SHIFT	6
-#endif
+#define	CACHE_LINE_SHIFT	7
 #define	CACHE_LINE_SIZE		(1 << CACHE_LINE_SHIFT)
 
 #define	PAGE_SHIFT_8K	13
