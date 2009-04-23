242a8e72ebef3b1925f92504c0b9bdda013f0fe9
Robert Watson <rwatson@FreeBSD.org>
2009-04-23 09:32:30 +0000

    Add a new interface flag, IFF_DYING, which is set when a device driver calls if_free(), and remains set if the refcount is elevated.  IF_DYING skips the bit in the if_flags bitmask previously used by IFF_NEEDSGIANT, so that an MFC can be done without changing which bit is used, as IFF_NEEDSGIANT is still present in 7.x.
    ifnet_byindex_ref() checks for IFF_DYING and returns NULL if it is set,
    preventing new references from by acquired by index, preventing
    monitoring sysctls from seeing it.  Other lookup mechanisms currently
    do not check IFF_DYING, but may need to in the future.
    
    MFC after:	3 weeks

--- sys/net/if.h
+++ sys/net/if.h
@@ -149,6 +149,7 @@ struct if_data {
 #define	IFF_PPROMISC	0x20000		/* (n) user-requested promisc mode */
 #define	IFF_MONITOR	0x40000		/* (n) user-requested monitor mode */
 #define	IFF_STATICARP	0x80000		/* (n) static ARP */
+#define	IFF_DYING	0x200000	/* (n) interface is winding down */
 
 /*
  * Old names for driver flags so that user space tools can continue to use
@@ -162,7 +163,8 @@ struct if_data {
 /* flags set internally only: */
 #define	IFF_CANTCHANGE \
 	(IFF_BROADCAST|IFF_POINTOPOINT|IFF_DRV_RUNNING|IFF_DRV_OACTIVE|\
-	    IFF_SIMPLEX|IFF_MULTICAST|IFF_ALLMULTI|IFF_SMART|IFF_PROMISC)
+	    IFF_SIMPLEX|IFF_MULTICAST|IFF_ALLMULTI|IFF_SMART|IFF_PROMISC|\
+	    IFF_DYING)
 
 /*
  * Values for if_link_state.
