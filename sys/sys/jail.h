01623a5de1a74050397555588ba1ae26279b7846
jamie <jamie@FreeBSD.org>
2009-04-29 21:14:15 +0000

    Introduce the extensible jail framework, using the same "name=value" interface as nmount(2).  Three new system calls are added: * jail_set, to create jails and change the parameters of existing jails.   This replaces jail(2). * jail_get, to read the parameters of existing jails.  This replaces the   security.jail.list sysctl. * jail_remove to kill off a jail's processes and remove the jail. Most jail parameters may now be changed after creation, and jails may be set to exist without any attached processes.  The current jail(2) system call still exists, though it is now a stub to jail_set(2).
    Approved by:	bz (mentor)

--- sys/sys/jail.h
+++ sys/sys/jail.h
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 1999 Poul-Henning Kamp.
+ * Copyright (c) 2009 James Gritton.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -97,15 +98,30 @@ static const struct prison_state {
 	{ PRISON_STATE_DYING,		"DYING" },
 };
 
+/*
+ * Flags for jail_set and jail_get.
+ */
+#define	JAIL_CREATE	0x01	/* Create jail if it doesn't exist */
+#define	JAIL_UPDATE	0x02	/* Update parameters of existing jail */
+#define	JAIL_ATTACH	0x04	/* Attach to jail upon creation */
+#define	JAIL_DYING	0x08	/* Allow getting a dying jail */
+#define	JAIL_SET_MASK	0x0f
+#define	JAIL_GET_MASK	0x08
 
 #ifndef _KERNEL
 
+struct iovec;
+
 int jail(struct jail *);
+int jail_set(struct iovec *, unsigned int, int);
+int jail_get(struct iovec *, unsigned int, int);
 int jail_attach(int);
+int jail_remove(int);
 
 #else /* _KERNEL */
 
 #include <sys/queue.h>
+#include <sys/sysctl.h>
 #include <sys/_lock.h>
 #include <sys/_mutex.h>
 #include <sys/_task.h>
@@ -136,29 +152,44 @@ struct cpuset;
  *   (d) set only during destruction of jail, no mutex needed
  */
 struct prison {
-	LIST_ENTRY(prison) pr_list;			/* (a) all prisons */
+	TAILQ_ENTRY(prison) pr_list;			/* (a) all prisons */
 	int		 pr_id;				/* (c) prison id */
 	int		 pr_ref;			/* (p) refcount */
-	int		 pr_state;			/* (p) prison state */
-	int		 pr_nprocs;			/* (p) process count */
+	int		 pr_uref;			/* (p) user (alive) refcount */
+	unsigned	 pr_flags;			/* (p) PR_* flags */
 	char		 pr_path[MAXPATHLEN];		/* (c) chroot path */
 	struct cpuset	*pr_cpuset;			/* (p) cpuset */
 	struct vnode	*pr_root;			/* (c) vnode to rdir */
 	char		 pr_host[MAXHOSTNAMELEN];	/* (p) jail hostname */
-	char		 pr_name[MAXHOSTNAMELEN];	/* (c) admin jail name */
+	char		 pr_name[MAXHOSTNAMELEN];	/* (p) admin jail name */
 	void		*pr_linux;			/* (p) linux abi */
 	int		 pr_securelevel;		/* (p) securelevel */
 	struct task	 pr_task;			/* (d) destroy task */
 	struct mtx	 pr_mtx;
 	struct osd	 pr_osd;			/* (p) additional data */
-	int		 pr_ip4s;			/* (c) number of v4 IPs */
-	struct in_addr	*pr_ip4;			/* (c) v4 IPs of jail */
-	int		 pr_ip6s;			/* (c) number of v6 IPs */
-	struct in6_addr	*pr_ip6;			/* (c) v6 IPs of jail */
+	int		 pr_ip4s;			/* (p) number of v4 IPs */
+	struct in_addr	*pr_ip4;			/* (p) v4 IPs of jail */
+	int		 pr_ip6s;			/* (p) number of v6 IPs */
+	struct in6_addr	*pr_ip6;			/* (p) v6 IPs of jail */
 };
 #endif /* _KERNEL || _WANT_PRISON */
 
 #ifdef _KERNEL
+/*
+ * Flag bits set via options or internally
+ */
+#define	PR_PERSIST	0x00000001	/* Can exist without processes */
+#define	PR_REMOVE	0x01000000	/* In process of being removed */
+
+/*
+ * OSD methods
+ */
+#define	PR_METHOD_CREATE	0
+#define	PR_METHOD_GET		1
+#define	PR_METHOD_SET		2
+#define	PR_METHOD_CHECK		3
+#define	PR_METHOD_ATTACH	4
+
 /*
  * Sysctl-set variables that determine global jail policy
  *
@@ -171,9 +202,28 @@ extern int	jail_getfsstat_jailrootonly;
 extern int	jail_allow_raw_sockets;
 extern int	jail_chflags_allowed;
 
-LIST_HEAD(prisonlist, prison);
+TAILQ_HEAD(prisonlist, prison);
 extern struct	prisonlist allprison;
 extern struct	sx allprison_lock;
+
+/*
+ * Sysctls to describe jail parameters.
+ */
+SYSCTL_DECL(_security_jail_param);
+
+#define	SYSCTL_JAIL_PARAM(module, param, type, fmt, descr)		\
+    SYSCTL_PROC(_security_jail_param ## module, OID_AUTO, param,	\
+	(type) | CTLFLAG_MPSAFE, NULL, 0, sysctl_jail_param, fmt, descr)
+#define	SYSCTL_JAIL_PARAM_STRING(module, param, access, len, descr)	\
+    SYSCTL_PROC(_security_jail_param ## module, OID_AUTO, param,	\
+	CTLTYPE_STRING | CTLFLAG_MPSAFE | (access), NULL, len,		\
+	sysctl_jail_param, "A", descr)
+#define	SYSCTL_JAIL_PARAM_STRUCT(module, param, access, len, fmt, descr)\
+    SYSCTL_PROC(_security_jail_param ## module, OID_AUTO, param,	\
+	CTLTYPE_STRUCT | CTLFLAG_MPSAFE | (access), NULL, len,		\
+	sysctl_jail_param, fmt, descr)
+#define	SYSCTL_JAIL_PARAM_NODE(module, descr)				\
+    SYSCTL_NODE(_security_jail_param, OID_AUTO, module, CTLFLAG_RW, 0, descr)
 
 /*
  * Kernel support functions for jail().
@@ -182,8 +232,6 @@ struct ucred;
 struct mount;
 struct sockaddr;
 struct statfs;
-struct thread;
-int kern_jail(struct thread *, struct jail *);
 int jailed(struct ucred *cred);
 void getcredhostname(struct ucred *cred, char *, size_t);
 int prison_check(struct ucred *cred1, struct ucred *cred2);
@@ -191,6 +239,7 @@ int prison_canseemount(struct ucred *cred, struct mount *mp);
 void prison_enforce_statfs(struct ucred *cred, struct mount *mp,
     struct statfs *sp);
 struct prison *prison_find(int prid);
+struct prison *prison_find_name(const char *name);
 void prison_free(struct prison *pr);
 void prison_free_locked(struct prison *pr);
 void prison_hold(struct prison *pr);
@@ -210,6 +259,7 @@ int prison_check_ip6(struct ucred *, struct in6_addr *);
 int prison_check_af(struct ucred *cred, int af);
 int prison_if(struct ucred *cred, struct sockaddr *sa);
 int prison_priv_check(struct ucred *cred, int priv);
+int sysctl_jail_param(struct sysctl_oid *, void *, int , struct sysctl_req *);
 
 #endif /* _KERNEL */
 #endif /* !_SYS_JAIL_H_ */
