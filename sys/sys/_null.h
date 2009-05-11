92919e4a54c1c2df2cca7eab38c96c6916d584e1
John Baldwin <jhb@FreeBSD.org>
2009-05-11 21:13:00 +0000

    *sigh*, while the kernel built, userland C did not.  Revert the previous commit and fix it correctly by removing the _KERNEL check entirely.  Now the kernel always sees the same value of NULL as userland meaning that it sees __null, 0, or 0L when compiled as C++, and '(void *)0' when compiled as C.

--- sys/sys/_null.h
+++ sys/sys/_null.h
@@ -28,18 +28,18 @@
 
 #ifndef NULL
 
+#if !defined(__cplusplus)
+#define	NULL	((void *)0)
+#else
 #if defined(__GNUG__) && defined(__GNUC__) && __GNUC__ >= 4
 #define	NULL	__null
-#else
-#if defined(_KERNEL) && !defined(__cplusplus)
-#define	NULL	((void *)0)
 #else
 #if defined(__LP64__)
 #define	NULL	(0L)
 #else
 #define	NULL	0
 #endif	/* __LP64__ */
-#endif	/* _KERNEL && !__cplusplus */
 #endif	/* __GNUG__ */
+#endif	/* !__cplusplus */
 
 #endif
