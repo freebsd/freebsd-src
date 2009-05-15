b86a31ac055a3e2484121999d8e5fc37d69e9893
John Baldwin <jhb@FreeBSD.org>
2009-05-15 19:54:19 +0000

    MFC: Adjust some variables (mostly related to the buffer cache) that hold address space sizes to be longs instead of ints.  This includes an ABI compat shim for the kern.bufspace sysctl for old binaries.

--- sys/ufs/ffs/ffs_snapshot.c
+++ sys/ufs/ffs/ffs_snapshot.c
@@ -2272,7 +2272,7 @@ ffs_copyonwrite(devvp, bp)
 			VI_UNLOCK(devvp);
 			if (saved_runningbufspace != 0) {
 				bp->b_runningbufspace = saved_runningbufspace;
-				atomic_add_int(&runningbufspace,
+				atomic_add_long(&runningbufspace,
 					       bp->b_runningbufspace);
 			}
 			return (0);		/* Snapshot gone */
@@ -2397,7 +2397,7 @@ ffs_copyonwrite(devvp, bp)
 	 */
 	if (saved_runningbufspace != 0) {
 		bp->b_runningbufspace = saved_runningbufspace;
-		atomic_add_int(&runningbufspace, bp->b_runningbufspace);
+		atomic_add_long(&runningbufspace, bp->b_runningbufspace);
 	}
 	return (error);
 }
--- usr.bin/top/machine.c
+++ usr.bin/top/machine.c
@@ -433,7 +433,7 @@ get_system_info(struct system_info *si)
 		static unsigned int swap_delay = 0;
 		static int swapavail = 0;
 		static int swapfree = 0;
-		static int bufspace = 0;
+		static long bufspace = 0;
 		static int nspgsin, nspgsout;
 
 		GETSYSCTL("vfs.bufspace", bufspace);
