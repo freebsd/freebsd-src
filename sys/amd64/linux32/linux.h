3933bde22e2b21e4be54f033529b164b72aca3ea
Dmitry Chagin <dchagin@FreeBSD.org>
2009-05-16 18:48:41 +0000

    Somewhere between 2.6.23 and 2.6.27, Linux added SOCK_CLOEXEC and SOCK_NONBLOCK flags, that allow to save fcntl() calls.
    Implement a variation of the socket() syscall which takes a flags
    in addition to the type argument.
    
    Approved by:	kib (mentor)
    MFC after:	1 month

--- sys/amd64/linux32/linux.h
+++ sys/amd64/linux32/linux.h
@@ -571,6 +571,7 @@ int	linux_ioctl_unregister_handler(struct linux_ioctl_handler *h);
 #define	LINUX_O_DIRECTORY	00200000	/* Must be a directory */
 #define	LINUX_O_NOFOLLOW	00400000	/* Do not follow links */
 #define	LINUX_O_NOATIME		01000000
+#define	LINUX_O_CLOEXEC		02000000
 
 #define	LINUX_F_DUPFD		0
 #define	LINUX_F_GETFD		1
