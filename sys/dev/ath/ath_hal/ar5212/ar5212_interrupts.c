0f82c8665f38e1e1901f0319557270b9a408f38b
sam <sam@FreeBSD.org>
2009-05-19 17:43:31 +0000

    minor cleanup

--- sys/dev/ath/ath_hal/ar5212/ar5212_interrupts.c
+++ sys/dev/ath/ath_hal/ar5212/ar5212_interrupts.c
@@ -55,20 +55,20 @@ HAL_BOOL
 ar5212GetPendingInterrupts(struct ath_hal *ah, HAL_INT *masked)
 {
 	uint32_t isr, isr0, isr1;
-	uint32_t mask2=0;
+	uint32_t mask2;
 	struct ath_hal_5212 *ahp = AH5212(ah);
 
 	isr = OS_REG_READ(ah, AR_ISR);
+	mask2 = 0;
 	if (isr & AR_ISR_BCNMISC) {
-		uint32_t isr2;
-		isr2 = OS_REG_READ(ah, AR_ISR_S2);
+		uint32_t isr2 = OS_REG_READ(ah, AR_ISR_S2);
 		if (isr2 & AR_ISR_S2_TIM)
 			mask2 |= HAL_INT_TIM;
 		if (isr2 & AR_ISR_S2_DTIM)
 			mask2 |= HAL_INT_DTIM;
 		if (isr2 & AR_ISR_S2_DTIMSYNC)
 			mask2 |= HAL_INT_DTIMSYNC;
-		if (isr2 & (AR_ISR_S2_CABEND ))
+		if (isr2 & AR_ISR_S2_CABEND)
 			mask2 |= HAL_INT_CABEND;
 	}
 	isr = OS_REG_READ(ah, AR_ISR_RAC);
@@ -137,7 +137,7 @@ ar5212SetInterrupts(struct ath_hal *ah, HAL_INT ints)
 {
 	struct ath_hal_5212 *ahp = AH5212(ah);
 	uint32_t omask = ahp->ah_maskReg;
-	uint32_t mask,mask2;
+	uint32_t mask, mask2;
 
 	HALDEBUG(ah, HAL_DEBUG_INTERRUPT, "%s: 0x%x => 0x%x\n",
 	    __func__, omask, ints);
@@ -171,7 +171,7 @@ ar5212SetInterrupts(struct ath_hal *ah, HAL_INT ints)
 		if (ints & HAL_INT_DTIMSYNC)
 			mask2 |= AR_IMR_S2_DTIMSYNC;
 		if (ints & HAL_INT_CABEND)
-			mask2 |= (AR_IMR_S2_CABEND );
+			mask2 |= AR_IMR_S2_CABEND;
 	}
 	if (ints & HAL_INT_FATAL) {
 		/*
@@ -184,15 +184,8 @@ ar5212SetInterrupts(struct ath_hal *ah, HAL_INT ints)
 	/* Write the new IMR and store off our SW copy. */
 	HALDEBUG(ah, HAL_DEBUG_INTERRUPT, "%s: new IMR 0x%x\n", __func__, mask);
 	OS_REG_WRITE(ah, AR_IMR, mask);
-	OS_REG_WRITE(ah, AR_IMR_S2, 
-				 (OS_REG_READ(ah, AR_IMR_S2) & 
-				  ~(AR_IMR_S2_TIM |
-					AR_IMR_S2_DTIM |
-					AR_IMR_S2_DTIMSYNC |
-					AR_IMR_S2_CABEND |
-					AR_IMR_S2_CABTO  |
-					AR_IMR_S2_TSFOOR ) ) 
-				 | mask2);
+	OS_REG_WRITE(ah, AR_IMR_S2,
+	    (OS_REG_READ(ah, AR_IMR_S2) &~ AR_IMR_SR2_BCNMISC) | mask2);
 	ahp->ah_maskReg = ints;
 
 	/* Re-enable interrupts if they were enabled before. */
@@ -200,7 +193,5 @@ ar5212SetInterrupts(struct ath_hal *ah, HAL_INT ints)
 		HALDEBUG(ah, HAL_DEBUG_INTERRUPT, "%s: enable IER\n", __func__);
 		OS_REG_WRITE(ah, AR_IER, AR_IER_ENABLE);
 	}
-
-
 	return omask;
 }
