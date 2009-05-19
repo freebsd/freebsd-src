da6e67f40e0c17bbd0d9720ca5b7451e7ecd5e21
gnn <gnn@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2009-05-19 17:40:22 +0000

    Add a new program, perror, which takes an errno as a command line argument and outputs the associated textual message in the same way that perror(3) would if called within a program.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@192398 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ usr.bin/perror/perror.c
@@ -0,0 +1,76 @@
+/*-
+ * Copyright (c) 2009 Advanced Computing Technologies LLC
+ * Written by: George V. Neville-Neil <gnn@FreeBSD.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/errno.h>
+
+#define MAX_ERR 256
+
+void usage()
+{
+
+	printf("usage: perror number\n");
+	printf("number must be between 1 and %d\n", ELAST);
+	exit(1);
+}
+
+int main(int argc, char **argv)
+{
+
+	char errstr[MAX_ERR];
+	char *cp;
+	int errnum;
+
+	if (argc != 2)
+		usage();
+
+	errnum = strtol(argv[1], &cp, 0);
+
+	if (((errnum == 0) && (errno == EINVAL)) || (*cp != '\0')) {
+		printf("Argument %s not a number.\n", argv[1]);
+		usage();
+	}
+
+	if ((errnum <=0) || (errnum > ELAST)) {
+		printf("Number %d out of range.\n", errnum);
+		usage();
+	}
+		
+	if (strerror_r(errnum, errstr, sizeof(errstr)) < 0) {
+		printf("Could not find error number %d.\n", errnum);
+		usage();
+	}
+
+	printf("Error %d is \"%s\"\n", errnum, errstr);
+
+	exit(0);
+}
