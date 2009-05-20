4ab9c8af927629d232120e67a0e73e5f70cbf1b9
John Baldwin <jhb@FreeBSD.org>
2009-05-20 17:19:30 +0000

    Fix a typo.

--- sys/kern/subr_bus.c
+++ sys/kern/subr_bus.c
@@ -712,7 +712,7 @@ fail:
 
 /*
  * Called when there's no match for this device.  This is only called
- * the first time that no match happens, so we don't keep getitng this
+ * the first time that no match happens, so we don't keep getting this
  * message.  Should that prove to be undesirable, we can change it.
  * This is called when all drivers that can attach to a given bus
  * decline to accept this device.  Other errrors may not be detected.
