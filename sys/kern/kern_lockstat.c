a5aedd68b4eb5bb0cafdc51eb8f325c32543ad9a
Stacey Son <sson@FreeBSD.org>
2009-05-26 20:28:22 +0000

    Add the OpenSolaris dtrace lockstat provider.  The lockstat provider adds probes for mutexes, reader/writer and shared/exclusive locks to gather contention statistics and other locking information for dtrace scripts, the lockstat(1M) command and other potential consumers.
    Reviewed by:	attilio jhb jb
    Approved by:	gnn (mentor)

--- sys/kern/kern_lockstat.c
+++ sys/kern/kern_lockstat.c
@@ -1,6 +1,6 @@
-/*
- * Copyright (C) 2008 John Birrell <jb@freebsd.org>
- * 
+/*-
+ * Copyright 2008-2009 Stacey Son <sson@FreeBSD.org>
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -9,7 +9,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -25,51 +25,40 @@
  * $FreeBSD$
  */
 
-#include <sys/cdefs.h>
-#include <sys/param.h>
-#include <sys/systm.h>
-#include <sys/conf.h>
-#include <sys/kernel.h>
-#include <sys/module.h>
-#include <sys/errno.h>
+/*
+ * Backend for the lock tracing (lockstat) kernel support. This is required 
+ * to allow a module to load even though DTrace kernel support may not be 
+ * present. 
+ *
+ */
 
-static int
-dtraceall_modevent(module_t mod __unused, int type, void *data __unused)
+#include "opt_kdtrace.h"
+
+#ifdef KDTRACE_HOOKS
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/lockstat.h>
+
+/*
+ * The following must match the type definition of dtrace_probe.  It is  
+ * defined this way to avoid having to rely on CDDL code.
+ */
+uint32_t lockstat_probemap[LS_NPROBES];
+void (*lockstat_probe_func)(uint32_t, uintptr_t, uintptr_t,
+    uintptr_t, uintptr_t, uintptr_t);
+
+
+uint64_t 
+lockstat_nsecs(void)
 {
-	int error = 0;
+	struct bintime bt;
+	uint64_t ns;
 
-	switch (type) {
-	case MOD_LOAD:
-		break;
-
-	case MOD_UNLOAD:
-		break;
-
-	case MOD_SHUTDOWN:
-		break;
-
-	default:
-		error = EOPNOTSUPP;
-		break;
-
-	}
-
-	return (error);
+	binuptime(&bt);
+	ns = bt.sec * (uint64_t)1000000000;
+	ns += ((uint64_t)1000000000 * (uint32_t)(bt.frac >> 32)) >> 32;
+	return (ns);
 }
 
-DEV_MODULE(dtraceall, dtraceall_modevent, NULL);
-MODULE_VERSION(dtraceall, 1);
-
-/* All the DTrace modules should be dependencies here: */
-MODULE_DEPEND(dtraceall, cyclic, 1, 1, 1);
-MODULE_DEPEND(dtraceall, opensolaris, 1, 1, 1);
-MODULE_DEPEND(dtraceall, dtrace, 1, 1, 1);
-MODULE_DEPEND(dtraceall, dtmalloc, 1, 1, 1);
-MODULE_DEPEND(dtraceall, dtnfsclient, 1, 1, 1);
-#if defined(__amd64__) || defined(__i386__)
-MODULE_DEPEND(dtraceall, fbt, 1, 1, 1);
-#endif
-MODULE_DEPEND(dtraceall, lockstat, 1, 1, 1);
-MODULE_DEPEND(dtraceall, sdt, 1, 1, 1);
-MODULE_DEPEND(dtraceall, systrace, 1, 1, 1);
-MODULE_DEPEND(dtraceall, profile, 1, 1, 1);
+#endif /* KDTRACE_HOOKS */
