477121d4e99bb8ae52cb41b7c34018b023f4c777
brooks <brooks@FreeBSD.org>
2009-06-01 22:34:09 +0000

    Increase NGROUPS_MAX to 32767.

--- sys/sys/syslimits.h
+++ sys/sys/syslimits.h
@@ -54,7 +54,7 @@
 #define	MAX_CANON		  255	/* max bytes in term canon input line */
 #define	MAX_INPUT		  255	/* max bytes in terminal input */
 #define	NAME_MAX		  255	/* max bytes in a file name */
-#define	NGROUPS_MAX		   16	/* max supplemental group id's */
+#define	NGROUPS_MAX	 	32767	/* max supplemental group id's */
 #ifndef OPEN_MAX
 #define	OPEN_MAX		   64	/* max open files per process */
 #endif
