d825c7936c97558399c080bedc884c4712272dcf
Marko Zec <zec@FreeBSD.org>
2009-06-01 21:29:54 +0000

    V_loif is not an array but a pure pointer, so treat it as such.
    Reviewed by:	bz
    Approved by:	julian (mentor)

--- sys/contrib/pf/net/pf_ioctl.c
+++ sys/contrib/pf/net/pf_ioctl.c
@@ -3717,7 +3717,7 @@ pf_check6_in(void *arg, struct mbuf **m, struct ifnet *ifp, int dir,
 	 * order to support scoped addresses. In order to support stateful
 	 * filtering we have change this to lo0 as it is the case in IPv4.
 	 */
-	chk = pf_test6(PF_IN, (*m)->m_flags & M_LOOP ? &V_loif[0] : ifp, m,
+	chk = pf_test6(PF_IN, (*m)->m_flags & M_LOOP ? V_loif : ifp, m,
 	    NULL, inp);
 	if (chk && *m) {
 		m_freem(*m);
