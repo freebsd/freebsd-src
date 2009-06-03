922a2097733e49b869978a264837ad60eca82891
Ed Schouten <ed@FreeBSD.org>
2009-06-03 13:29:08 +0000

    Import Clang, at r72770.

--- test/SemaObjC/objc2-merge-gc-attribue-decl.m
+++ test/SemaObjC/objc2-merge-gc-attribue-decl.m
@@ -10,3 +10,21 @@ extern id p1;
 extern id CFRunLoopGetMain();
 extern __strong id CFRunLoopGetMain();
 
+extern __weak id WLoopGetMain(); // expected-note {{previous declaration is here}}
+extern id WLoopGetMain();	// expected-error {{conflicting types for 'WLoopGetMain'}}
+
+extern id p3;	// expected-note {{previous definition is here}}
+extern __weak id p3;	// expected-error {{redefinition of 'p3' with a different type}}
+
+extern void *p4; // expected-note {{previous definition is here}}
+extern void * __strong p4; // expected-error {{redefinition of 'p4' with a different type}}
+
+extern id p5;
+extern __strong id p5;
+
+extern char* __strong p6; // expected-note {{previous definition is here}}
+extern char* p6; // expected-error {{redefinition of 'p6' with a different type}}
+
+// FIXME. We do not issue error here because we don't put the attribute on the pointer type.
+extern __strong char* p7; 
+extern char* p7; 
