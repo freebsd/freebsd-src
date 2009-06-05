a5176100fc7734ad550247eaa0ccca398167b69c
brooks <brooks@FreeBSD.org>
2009-06-05 16:47:35 +0000

    Remove a redundent assignment to cr_groups[0] immediatly after an assignment to cr_gid (which is currently defined as cr_groups[0]).

--- sys/fs/nfsserver/nfs_nfsdstate.c
+++ sys/fs/nfsserver/nfs_nfsdstate.c
@@ -3577,7 +3577,6 @@ nfsrv_docallback(struct nfsclient *clp, int procnum,
 	nd->nd_repstat = 0;
 	cred->cr_uid = clp->lc_uid;
 	cred->cr_gid = clp->lc_gid;
-	cred->cr_groups[0] = clp->lc_gid;
 	callback = clp->lc_callback;
 	NFSUNLOCKSTATE();
 	cred->cr_ngroups = 1;
