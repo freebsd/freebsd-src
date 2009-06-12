f27ad68328ccf8ec36c6eba415b3e4868d7dcb99
des <des@FreeBSD.org>
2009-06-12 23:39:05 +0000

    Provide correct prototypes for functions with no arguments.
    MFC after:	1 week

--- usr.sbin/nscd/agents/group.h
+++ usr.sbin/nscd/agents/group.h
@@ -28,5 +28,5 @@
 
 #include "../agent.h"
 
-extern struct agent *init_group_agent();
-extern struct agent *init_group_mp_agent();
+extern struct agent *init_group_agent(void);
+extern struct agent *init_group_mp_agent(void);
