2ba2aba56fdc966e58c106d68b73062eeabee761
simon <simon@FreeBSD.org>
2009-06-13 16:34:39 +0000

    jpake is marked experimental so disable it by default.

--- secure/lib/libcrypto/opensslconf-powerpc.h
+++ secure/lib/libcrypto/opensslconf-powerpc.h
@@ -9,6 +9,10 @@
 #ifndef OPENSSL_NO_CAMELLIA
 # define OPENSSL_NO_CAMELLIA
 #endif
+/* jpake is marked experimental in OpenSSL 0.9.8. */
+#ifndef OPENSSL_NO_JPAKE
+# define OPENSSL_NO_JPAKE
+#endif
 /* libgmp is not in the FreeBSD base system. */
 #ifndef OPENSSL_NO_GMP
 # define OPENSSL_NO_GMP
