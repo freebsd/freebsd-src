6eefed928355b0ec006d953f28186807c3cfd6ad
Simon L. B. Nielsen <simon@FreeBSD.org>
2009-06-14 19:46:18 +0000

    Update build infrastructure for OpenSSL 0.9.8k.

--- secure/lib/libcrypto/opensslconf-powerpc.h
+++ secure/lib/libcrypto/opensslconf-powerpc.h
@@ -9,6 +9,18 @@
 #ifndef OPENSSL_NO_CAMELLIA
 # define OPENSSL_NO_CAMELLIA
 #endif
+/* Disabled by default in OpenSSL 0.9.8. */
+#ifndef OPENSSL_NO_CMS
+# define OPENSSL_NO_CMS
+#endif
+/* Disabled by default in OpenSSL 0.9.8. */
+#ifndef OPENSSL_NO_SEED
+# define OPENSSL_NO_SEED
+#endif
+/* jpake is marked experimental in OpenSSL 0.9.8. */
+#ifndef OPENSSL_NO_JPAKE
+# define OPENSSL_NO_JPAKE
+#endif
 /* libgmp is not in the FreeBSD base system. */
 #ifndef OPENSSL_NO_GMP
 # define OPENSSL_NO_GMP
