64ea8acc7c8330c0d2ee0b8b8bf79f58473a155e
Dmitry Chagin <dchagin@FreeBSD.org>
2009-06-17 21:12:32 +0000

    MFC r185438:
    Fix iovec32 for linux32/amd64.
    
    Add a custom version of copyiniov() to deal with the 32-bit iovec
    pointers from userland (to be used later).
    
    Adjust prototypes for linux_readv() and linux_writev() to use new
    l_iovec32 definition and to match actual linux code. In particular,
    use ulong for fd (why ?).
    
    Approved by:	kib (mentor)

--- sys/amd64/linux32/linux.h
+++ sys/amd64/linux32/linux.h
@@ -879,6 +879,16 @@ typedef int l_mqd_t;
 	(LINUX_CLONE_VM | LINUX_CLONE_FS | LINUX_CLONE_FILES |	\
 	LINUX_CLONE_SIGHAND | LINUX_CLONE_THREAD)
 
+struct iovec;
+
+struct l_iovec32 {
+	uint32_t	iov_base;
+	l_size_t	iov_len;
+};
+
+int linux32_copyiniov(struct l_iovec32 *iovp32, l_ulong iovcnt,
+			    struct iovec **iovp, int error);
+
 /* robust futexes */
 struct linux_robust_list {
 	l_uintptr_t			next;
