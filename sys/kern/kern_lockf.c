21376236dde77e2c63790a0607687faa898d87d6
kib <kib@FreeBSD.org>
2009-06-17 12:43:04 +0000

    Decrement state->ls_threads when vnode appeared to be doomed.
    Reported and tested by:	pho

--- sys/kern/kern_lockf.c
+++ sys/kern/kern_lockf.c
@@ -646,6 +646,8 @@ lf_advlockasync(struct vop_advlockasync_args *ap, struct lockf **statep,
 	 */
 	VI_LOCK(vp);
 	if (vp->v_iflag & VI_DOOMED) {
+		state->ls_threads--;
+		wakeup(state);
 		VI_UNLOCK(vp);
 		sx_xunlock(&state->ls_lock);
 		lf_free_lock(lock);
