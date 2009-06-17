0abfb2bd6ab046dff711ca0c7cb16c7a15cd2163
jhb <jhb@FreeBSD.org>
2009-06-17 19:53:47 +0000

    Regen.

--- sys/kern/systrace_args.c
+++ sys/kern/systrace_args.c
@@ -2031,14 +2031,6 @@ systrace_args(int sysnum, void *params, u_int64_t *uarg, int *n_args)
 		*n_args = 1;
 		break;
 	}
-	/* nfsclnt */
-	case 375: {
-		struct nfsclnt_args *p = params;
-		iarg[0] = p->flag; /* int */
-		uarg[1] = (intptr_t) p->argp; /* caddr_t */
-		*n_args = 2;
-		break;
-	}
 	/* eaccess */
 	case 376: {
 		struct eaccess_args *p = params;
@@ -6346,19 +6338,6 @@ systrace_setargdesc(int sysnum, int ndx, char *desc, size_t descsz)
 		switch(ndx) {
 		case 0:
 			p = "int";
-			break;
-		default:
-			break;
-		};
-		break;
-	/* nfsclnt */
-	case 375:
-		switch(ndx) {
-		case 0:
-			p = "int";
-			break;
-		case 1:
-			p = "caddr_t";
 			break;
 		default:
 			break;
