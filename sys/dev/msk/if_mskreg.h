4804d234f6e7818527d6753bd547c838b2a4b946
Pyun YongHyeon <yongari@FreeBSD.org>
2009-06-18 05:06:25 +0000

    MFC r192716,192718-192720
    r192716:
      Remove link handling taskqueue and use mii callback directly. While
      I'm here also checks driver running state.
    
    r192718:
      Use bit definition to represent link state, device suspend instead
      of using separate variables in softc.
    
    r192719:
      Use bit definition to represent MSI and detach state instead of
      using separate variables in softc.
    
    r192720:
      Correctly return the result of mii_mediachg(). Previously it always
      used to return success.

--- sys/dev/msk/if_mskreg.h
+++ sys/dev/msk/if_mskreg.h
@@ -2342,9 +2342,7 @@ struct msk_softc {
 	uint32_t		msk_intrmask;
 	uint32_t		msk_intrhwemask;
 	uint32_t		msk_pflags;
-	int			msk_suspended;
 	int			msk_clock;
-	int			msk_msi;
 	struct msk_if_softc	*msk_if[2];
 	device_t		msk_devs[2];
 	int			msk_txqsize;
@@ -2382,10 +2380,13 @@ struct msk_if_softc {
 	int			msk_framesize;
 	int			msk_phytype;
 	int			msk_phyaddr;
-	int			msk_link;
 	uint32_t		msk_flags;
+#define	MSK_FLAG_MSI		0x0001
 #define	MSK_FLAG_RAMBUF		0x0010
 #define	MSK_FLAG_NOJUMBO	0x0020
+#define	MSK_FLAG_SUSPEND	0x2000
+#define	MSK_FLAG_DETACH		0x4000
+#define	MSK_FLAG_LINK		0x8000
 	struct callout		msk_tick_ch;
 	int			msk_watchdog_timer;
 	uint32_t		msk_txq;	/* Tx. Async Queue offset */
@@ -2395,10 +2396,8 @@ struct msk_if_softc {
 	struct msk_ring_data	msk_rdata;
 	struct msk_softc	*msk_softc;	/* parent controller */
 	struct msk_hw_stats	msk_stats;
-	struct task		msk_link_task;
 	struct task		msk_tx_task;
 	int			msk_if_flags;
-	int			msk_detach;
 	uint16_t		msk_vtag;	/* VLAN tag id. */
 };
 
