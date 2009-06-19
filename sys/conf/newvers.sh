4e16bd5fc2d86faba39e8dc896b592855e3736af
Bjoern A. Zeeb <bz@FreeBSD.org>
2009-06-19 15:31:40 +0000

    MFC: r187413,187486,190514
      Reduce the impact of svnversion.  Just scan the src/sys tree,
      not all of src.
    
      Initially done by peter in 183528, backed out in 183566 due to
      problems with newvers.sh also called from other places during
      world build.  Those two commits were not MFCed.
    
      A working solution was put back in in the MFCed r190514.
    
      Also strip the machine arch from SRCDIR in case it is a cross
      build so svnversion works, done by thompsa r187413,187486.

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -91,13 +91,22 @@ for dir in /bin /usr/bin /usr/local/bin; do
 	if [ -x "${dir}/svnversion" ]; then
 		svnversion=${dir}/svnversion
 		SRCDIR=${d##*obj}
+		if [ -n "$MACHINE" ]; then
+			SRCDIR=${SRCDIR##/$MACHINE}
+		fi
 		SRCDIR=${SRCDIR%%/sys/*}
 		break
 	fi
 done
 
 if [ -n "$svnversion" -a -d "${SRCDIR}/.svn" ] ; then
-	svn=" r`cd $SRCDIR && $svnversion`"
+	# If we are called from the kernel build, limit
+	# the scope of svnversion to sys/ .
+	if [ -e "${SRCDIR}/sys/conf/newvers.sh" ] ; then
+		svn=" r`cd $SRCDIR/sys && $svnversion`"
+	else
+		svn=" r`cd $SRCDIR && $svnversion`"
+	fi
 else
 	svn=""
 fi
