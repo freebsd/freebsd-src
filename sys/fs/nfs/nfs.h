ccb664878ab1ea77e9fcd54af4534c49a4974f25
rmacklem <rmacklem@FreeBSD.org>
2009-06-20 00:54:57 +0000

    Change the size of the nfsc_groups[] array in the experimental nfs client to RPCAUTH_UNIXGIDS + 1 (17), since that is what can go on the wire for AUTH_SYS authentication.
    Reviewed by:	brooks
    Approved by:	kib (mentor)

--- sys/fs/nfs/nfs.h
+++ sys/fs/nfs/nfs.h
@@ -404,13 +404,13 @@ typedef struct {
 		(b)->bits[1] = NFSATTRBIT_REFERRAL1; } while (0)
 
 /*
- * Store uid, gid creds that handle maps to.
- * Since some BSDen define cr_gid as cr_groups[0], I'll just keep them
- * all in nfsc_groups[NGROUPS + 1].
+ * Store uid, gid creds that were used when the stateid was acquired.
+ * The RPC layer allows RPCAUTH_UNIXGIDS + 1 groups to go out on the wire,
+ * so that's how many gets stored here.
  */
 struct nfscred {
 	uid_t 		nfsc_uid;
-	gid_t		nfsc_groups[NGROUPS + 1];
+	gid_t		nfsc_groups[RPCAUTH_UNIXGIDS + 1];
 	int		nfsc_ngroups;
 };
 
