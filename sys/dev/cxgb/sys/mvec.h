a14b0d6132dfbfac032af81d55557533f73971a8
np <np@FreeBSD.org>
2009-06-25 21:50:15 +0000

    mvec routines should have no knowledge of the SG engine.
    Reviewed by:	kmacy
    Approved by:	gnn (mentor)

--- sys/dev/cxgb/sys/mvec.h
+++ sys/dev/cxgb/sys/mvec.h
@@ -43,7 +43,7 @@
 #define m_ulp_mode	m_pkthdr.tso_segsz	/* upper level protocol	*/
 
 static __inline void
-busdma_map_mbuf_fast(struct sge_txq *txq, bus_dmamap_t map,
+busdma_map_mbuf_fast(bus_dma_tag_t tag, bus_dmamap_t map,
     struct mbuf *m, bus_dma_segment_t *seg)
 {
 #if defined(__i386__) || defined(__amd64__)
@@ -52,14 +52,13 @@ busdma_map_mbuf_fast(struct sge_txq *txq, bus_dmamap_t map,
 #else
 	int nsegstmp;
 
-	bus_dmamap_load_mbuf_sg(txq->entry_tag, map, m, seg,
-		    &nsegstmp, 0);
+	bus_dmamap_load_mbuf_sg(tag, map, m, seg, &nsegstmp, 0);
 #endif
 }
 
-int busdma_map_sg_collapse(struct sge_txq *txq, bus_dmamap_t map,
+int busdma_map_sg_collapse(bus_dma_tag_t tag, bus_dmamap_t map,
     struct mbuf **m, bus_dma_segment_t *segs, int *nsegs);
-void busdma_map_sg_vec(struct sge_txq *txq, bus_dmamap_t map,
+void busdma_map_sg_vec(bus_dma_tag_t tag, bus_dmamap_t map,
     struct mbuf *m, bus_dma_segment_t *segs, int *nsegs);
 static __inline int
 busdma_map_sgl(bus_dma_segment_t *vsegs, bus_dma_segment_t *segs, int count) 
