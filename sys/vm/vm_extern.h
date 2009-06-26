c60748693a219928c0fa0f947779a19c62579e22
alc <alc@FreeBSD.org>
2009-06-26 04:47:43 +0000

    This change is the next step in implementing the cache control functionality required by video card drivers.  Specifically, this change introduces vm_cache_mode_t with an appropriate VM_CACHE_DEFAULT definition on all architectures.  In addition, this changes adds a vm_cache_mode_t parameter to kmem_alloc_contig() and vm_phys_alloc_contig().  These will be the interfaces for allocating mapped kernel memory and physical memory, respectively, with non-default cache modes.
    In collaboration with:	jhb

--- sys/vm/vm_extern.h
+++ sys/vm/vm_extern.h
@@ -43,7 +43,7 @@ int kernacc(void *, int, int);
 vm_offset_t kmem_alloc(vm_map_t, vm_size_t);
 vm_offset_t kmem_alloc_contig(vm_map_t map, vm_size_t size, int flags,
     vm_paddr_t low, vm_paddr_t high, unsigned long alignment,
-    unsigned long boundary);
+    unsigned long boundary, vm_cache_mode_t mode);
 vm_offset_t kmem_alloc_nofault(vm_map_t, vm_size_t);
 vm_offset_t kmem_alloc_wait(vm_map_t, vm_size_t);
 void kmem_free(vm_map_t, vm_offset_t, vm_size_t);
