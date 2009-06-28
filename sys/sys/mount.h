fe1d3f15f6b68135ca25a5c67caf135cd137e435
Stanislav Sedov <stas@FreeBSD.org>
2009-06-28 21:49:43 +0000

    - Turn the third (islocked) argument of the knote call into flags parameter.   Introduce the new flag KNF_NOKQLOCK to allow event callers to be called   without KQ_LOCK mtx held. - Modify VFS knote calls to always use KNF_NOKQLOCK flag.  This is required   for ZFS as its getattr implementation may sleep.
    Approved by:	re (rwatson)
    Reviewed by:	kib
    MFC after:	2 weeks

--- sys/sys/mount.h
+++ sys/sys/mount.h
@@ -657,7 +657,7 @@ vfs_statfs_t	__vfs_statfs;
 #define VFS_KNOTE_LOCKED(vp, hint) do					\
 {									\
 	if (((vp)->v_vflag & VV_NOKNOTE) == 0)				\
-		VN_KNOTE((vp), (hint), 1);				\
+		VN_KNOTE((vp), (hint), KNF_LISTLOCKED);			\
 } while (0)
 
 #define VFS_KNOTE_UNLOCKED(vp, hint) do					\
