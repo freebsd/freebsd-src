55f7250f38a12767c8dcf9856ad0c64e3a1ad32c
Alexander Kabaev <kan@FreeBSD.org>
2009-06-30 03:05:50 +0000

    MFC r194298:
    FreeBSD returns main object handle from dlopen(NULL, ...) calls.
    dlsym seaches using this handle are expected to look for symbol
    definitions in all objects loaded at the program start time along
    with all objects currently in RTLD_GLOBAL scope.

--- libexec/rtld-elf/rtld.c
+++ libexec/rtld-elf/rtld.c
@@ -1966,6 +1966,16 @@ do_dlsym(void *handle, const char *name, void *retaddr, const Ver_Entry *ve,
 	    /* Search main program and all libraries loaded by it. */
 	    def = symlook_list(name, hash, &list_main, &defobj, ve, flags,
 			       &donelist);
+
+	    /*
+	     * We do not distinguish between 'main' object and global scope.
+	     * If symbol is not defined by objects loaded at startup, continue
+	     * search among dynamically loaded objects with RTLD_GLOBAL
+	     * scope.
+	     */
+	    if (def == NULL)
+		def = symlook_list(name, hash, &list_global, &defobj, ve,
+		    		    flags, &donelist);
 	} else {
 	    Needed_Entry fake;
 
