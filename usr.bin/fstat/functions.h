b2066769ddbb9370e2aabe0e0251cf5ab691480f
stas <stas@FreeBSD.org>
2009-07-09 09:44:55 +0000

    - Fix copyright line.
    Suggested by:	rwatson

--- usr.bin/fstat/functions.h
+++ usr.bin/fstat/functions.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2009 The FreeBSD Project.
+ * Copyright (c) 2009 Stanislav Sedov <stas@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
