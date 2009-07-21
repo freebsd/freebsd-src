4f9074da93347bbe3167e16ebd27d5bd0e592729
Edwin Groothuis <edwin@FreeBSD.org>
2009-07-21 02:13:57 +0000

    Vendor import of tzcode2009k
    zic.c:
        Do not end a binary file with a POSIX-style time zone string
        for locations that end up in permanent DST (thanks to Andreas
        Schwab).
    
    tz-art.htm
        Add notes on "A Matter of Life and Death"
        (thanks to Dave Cantor).
    
    tz-link.htm
        Remove seemingly obsolete public.planetmirror.com/pub/timezone
        link (thanks to Nathan Stratton Treadway).
    
    Obtained from:	ftp://elsie.nci.nih.gov/pub/

--- zic/zic.c
+++ zic/zic.c
@@ -3,7 +3,7 @@
 ** 2006-07-17 by Arthur David Olson.
 */
 
-static char	elsieid[] = "@(#)zic.c	8.19";
+static char	elsieid[] = "@(#)zic.c	8.20";
 
 #include "private.h"
 #include "locale.h"
@@ -1921,7 +1921,7 @@ const int			zonecount;
 		if (stdrp != NULL && stdrp->r_hiyear == 2037)
 			return;
 	}
-	if (stdrp == NULL && zp->z_nrules != 0)
+	if (stdrp == NULL && (zp->z_nrules != 0 || zp->z_stdoff != 0))
 		return;
 	abbrvar = (stdrp == NULL) ? "" : stdrp->r_abbrvar;
 	doabbr(result, zp->z_format, abbrvar, FALSE, TRUE);
