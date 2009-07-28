805064553e089e09e5512d94e00acc641f1a28da
julian <julian@FreeBSD.org>
2009-07-28 19:43:27 +0000

    Somewhere along the line accept sockets stopped honoring the FIB selected for them. Fix this.
    Reviewed by:	ambrisko
    Approved by:	re (kib)
    MFC after:	3 days

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -758,6 +758,7 @@ findpcb:
 		}
 		inc.inc_fport = th->th_sport;
 		inc.inc_lport = th->th_dport;
+		inc.inc_fibnum = so->so_fibnum;
 
 		/*
 		 * Check for an existing connection attempt in syncache if
