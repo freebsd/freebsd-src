ceda2d70e45650ce9a84e4e16081e9693a2c143d
Michael Tuexen <tuexen@FreeBSD.org>
2009-09-12 18:08:44 +0000

    MFC 196610: Fix a bug where vlan interfaces are not supported by SCTP.
    Approved by: re, rrs (mentor)

--- sys/netinet/sctp_bsd_addr.c
+++ sys/netinet/sctp_bsd_addr.c
@@ -175,6 +175,7 @@ sctp_is_desired_interface_type(struct ifaddr *ifa)
 	case IFT_LOOP:
 	case IFT_SLIP:
 	case IFT_GIF:
+	case IFT_L2VLAN:
 	case IFT_IP:
 	case IFT_IPOVERCDLC:
 	case IFT_IPOVERCLAW:
