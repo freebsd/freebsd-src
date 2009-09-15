695af4571df74cd571c3907e2b50d390af117cd4
attilio <attilio@FreeBSD.org>
2009-09-15 16:59:52 +0000

    Use explicit int values for the device states in order to allow, if necessary, in the future, adds of new states without breaking ABI between revisions.
    Proposed by:	kib
    Approved by:	imp

--- sys/sys/bus.h
+++ sys/sys/bus.h
@@ -50,10 +50,10 @@ struct u_businfo {
  * @brief State of the device.
  */
 typedef enum device_state {
-	DS_NOTPRESENT,			/**< @brief not probed or probe failed */
-	DS_ALIVE,			/**< @brief probe succeeded */
-	DS_ATTACHED,			/**< @brief attach method called */
-	DS_BUSY				/**< @brief device is open */
+	DS_NOTPRESENT = 10,		/**< @brief not probed or probe failed */
+	DS_ALIVE = 20,			/**< @brief probe succeeded */
+	DS_ATTACHED = 30,		/**< @brief attach method called */
+	DS_BUSY = 40			/**< @brief device is open */
 } device_state_t;
 
 /**
