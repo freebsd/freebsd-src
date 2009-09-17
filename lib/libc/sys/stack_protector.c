092671e7274ff11b01fde01cf1b4f299566ff54b
kan <kan@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2009-09-17 13:21:53 +0000

    Make libc.a provide __stack_chk_fail_local weak alias. This is needed to satisfy static libraries that are compiled with -fpic and linked into static binary afterwards. Several libraries in gcc are examples of such static libs.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@197277 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/sys/stack_protector.c
+++ lib/libc/sys/stack_protector.c
@@ -108,4 +108,8 @@ __chk_fail(void)
 	__fail("buffer overflow detected; terminated");
 }
 
+#ifdef PIC
 __sym_compat(__stack_chk_fail_local, __stack_chk_fail, FBSD_1.0);
+#else
+__weak_reference(__stack_chk_fail, __stack_chk_fail_local);
+#endif
