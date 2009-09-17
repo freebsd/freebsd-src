5469543c92c699ae5114a576ce9aad7be5ff9006
Pawel Jakub Dawidek <pjd@FreeBSD.org>
2009-09-17 15:14:28 +0000

    Purge file system namecache when receiving incremental stream and rolling back to it.
    MFC after:	3 days

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_ioctl.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_ioctl.c
@@ -2543,6 +2543,9 @@ zfs_ioc_recv(zfs_cmd_t *zc)
 		if (error == 0) {
 			int resume_err;
 
+#ifdef FREEBSD_NAMECACHE
+			cache_purgevfs(zfsvfs->z_vfs);
+#endif
 			error = dmu_recv_end(&drc);
 			resume_err = zfs_resume_fs(zfsvfs, osname, mode);
 			error = error ? error : resume_err;
