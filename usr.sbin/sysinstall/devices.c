85b198e8f3d79e2f83fec278d8c1270f82c228fd
marius <marius@FreeBSD.org>
2009-09-20 12:24:55 +0000

    MFC: r194249
    Add cas(4).

--- usr.sbin/sysinstall/devices.c
+++ usr.sbin/sysinstall/devices.c
@@ -104,6 +104,7 @@ static struct _devname {
     NETWORK("bce",	"Broadcom NetXtreme II Gigabit Ethernet card"),
     NETWORK("bfe",	"Broadcom BCM440x PCI Ethernet card"),
     NETWORK("bge",	"Broadcom BCM570x PCI Gigabit Ethernet card"),
+    NETWORK("cas",	"Sun Cassini/Cassini+ or NS DP83065 Saturn Ethernet"),
     NETWORK("cue",	"CATC USB Ethernet adapter"),
     NETWORK("cxgb",	"Chelsio T3 10Gb Ethernet card"),
     NETWORK("fpa",	"DEC DEFPA PCI FDDI card"),
