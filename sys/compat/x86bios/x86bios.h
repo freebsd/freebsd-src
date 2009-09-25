6aabed1c776ce3e396d5907366bf194ad603f970
Jung-uk Kim <jkim@FreeBSD.org>
2009-09-25 17:56:32 +0000

    - Reduce BIOS memory mapping.  We want 1MB of physical memory, not 12MB[1]. - Remove CS and IP registers from x86bios.h.  They have no use for us. - Adjust register dump to make it little bit more useful for debugging.
    Submitted by:	paradox (ddkprog yahoo com)[1] (initial version)

--- sys/compat/x86bios/x86bios.h
+++ sys/compat/x86bios/x86bios.h
@@ -54,7 +54,7 @@ union x86_register {
 };
 
 struct x86regs {
-	uint16_t		register_cs;
+	uint16_t		padding;	/* CS is unused. */
 	uint16_t		register_ds;
 	uint16_t		register_es;
 	uint16_t		register_fs;
@@ -70,7 +70,6 @@ struct x86regs {
 	union x86_register	register_bp;
 	union x86_register	register_si;
 	union x86_register	register_di;
-	union x86_register	register_ip;
 };
 
 typedef struct x86regs	x86regs_t;
@@ -102,7 +101,6 @@ typedef struct x86regs	x86regs_t;
 #define R_BP	register_bp.I16_reg.x_reg
 #define R_SI	register_si.I16_reg.x_reg
 #define R_DI	register_di.I16_reg.x_reg
-#define R_IP	register_ip.I16_reg.x_reg
 #define R_FLG	register_flags
 
 /* special registers */
@@ -110,11 +108,9 @@ typedef struct x86regs	x86regs_t;
 #define R_EBP	register_bp.I32_reg.e_reg
 #define R_ESI	register_si.I32_reg.e_reg
 #define R_EDI	register_di.I32_reg.e_reg
-#define R_EIP	register_ip.I32_reg.e_reg
 #define R_EFLG	register_flags
 
 /* segment registers */
-#define R_CS	register_cs
 #define R_DS	register_ds
 #define R_SS	register_ss
 #define R_ES	register_es
@@ -125,8 +121,8 @@ typedef struct x86regs	x86regs_t;
 #define SEG_OFF(x)	((x) & 0x0FFFF)
 #define FARP(x)		((le32toh(x) & 0xffff) + ((le32toh(x) >> 12) & 0xffff00))
 
-#define MAPPED_MEMORY_SIZE	0xc00000
-#define PAGE_RESERV		(4096*5)
+#define MAPPED_MEMORY_SIZE	(1024 * 1024)
+#define PAGE_RESERV		(4096 * 5)
 
 __BEGIN_DECLS
 void *x86bios_alloc(int count, int *segs);
