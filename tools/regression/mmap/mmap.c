a813d67de29fcf149125cdd2082b5be1e54da95b
bz <bz@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2009-09-27 21:03:33 +0000

    Add a simple C program to check mmap calls to various different addresses. The most important test is the mapping fixed at address 0 depending on the new sysctl.
    Things will be updated and possibly converted to m4/.t style once the
    details about the kernel patch will be shaken out.
    
    Submitted by:	simon (initial version)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@197543 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ tools/regression/mmap/mmap.c
@@ -0,0 +1,96 @@
+/*-
+ * Copyright (c) 2009	Simon L. Nielsen <simon@FreeBSD.org>,
+ * 			Bjoern A. Zeeb <bz@FreeBSD.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#include <sys/param.h>
+#include <sys/mman.h>
+#include <sys/sysctl.h>
+#include <sys/types.h>
+
+#include <stdio.h>
+#include <err.h>
+
+const struct tests {
+	void	*addr;
+	int	ok[2];	/* Depending on security.bsd.mmap_zero {0, !=0}. */
+} tests[] = {
+	{ (void *)0,			{ 0, 1 } }, /* Test sysctl. */
+	{ (void *)1,			{ 0, 0 } },
+	{ (void *)(PAGE_SIZE - 1),	{ 0, 0 } },
+	{ (void *)PAGE_SIZE,		{ 1, 1 } },
+	{ (void *)-1,			{ 0, 0 } },
+	{ (void *)(-PAGE_SIZE),		{ 0, 0 } },
+	{ (void *)(-1 - PAGE_SIZE),	{ 0, 0 } },
+	{ (void *)(-1 - PAGE_SIZE - 1),	{ 0, 0 } },
+	{ (void *)(0x1000 * PAGE_SIZE),	{ 1, 1 } },
+};
+
+int
+main(void)
+{
+	void *p;
+	size_t len;
+	int i, error, mib[3], mmap_zero;
+
+	error = 0;
+
+	/* Get the current sysctl value of security.bsd.mmap_zero. */
+	len = sizeof(mib) / sizeof(*mib);
+	if (sysctlnametomib("security.bsd.mmap_zero", mib, &len) == -1)
+		err(1, "sysctlnametomib(security.bsd.mmap_zero)");
+
+	len = sizeof(mmap_zero);
+	if (sysctl(mib, 3, &mmap_zero, &len, NULL, 0) == -1)
+		err(1, "sysctl(security.bsd.mmap_zero)");
+
+	/* Normalize to 0 or 1 for array access. */
+	mmap_zero = !!mmap_zero;
+
+	for (i=0; i < (sizeof(tests) / sizeof(*tests)); i++) {
+		p = mmap((void *)tests[i].addr, PAGE_SIZE,
+		    PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANON | MAP_FIXED,
+		    -1, 0);
+		if (p == MAP_FAILED) {
+			if (tests[i].ok[mmap_zero] != 0)
+				error++;
+			warnx("%s: mmap(%p, ...) failed.",
+			    (tests[i].ok[mmap_zero] == 0) ? "OK " : "ERR",
+			     tests[i].addr);
+		} else {
+			if (tests[i].ok[mmap_zero] != 1)
+				error++;
+			warnx("%s: mmap(%p, ...) succeeded: p=%p",
+			    (tests[i].ok[mmap_zero] == 1) ? "OK " : "ERR",
+			    tests[i].addr, p);
+		}
+	}
+
+	if (error)
+		err(1, "---\nERROR: %d unexpected results.", error);
+
+	return (error != 0);
+}
