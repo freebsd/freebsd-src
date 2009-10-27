b41cfe02325533aae2fa8e8259036118d41b738d
emaste <emaste@FreeBSD.org>
2009-10-27 21:09:52 +0000

    MFC r188743,r188940
      Use outbound message register 0 instead of mailbox 7 in
      aac_{rx,rkt}_get_fwstatus, as done in Adaptec's vendor driver as well as
      the Linux drivers.
    
      Submitted by:   jkim, from Adaptec's driver
    
    ==
    
      SVN rev 188743 modified aac_rx_get_fwstatus to use the AAC_RX_OMR0
      register instead of AAC_RX_FWSTATUS, as that is the way it's done in
      Adaptec's vendor driver and in the Linux drivers.  (The same applies
      to aac_rkt_get_fwstatus as well.)
    
      However, a concern has been raised about the compatibility of this
      change and old hardware / firmware versions.  In the absense of
      specific information, revert to the original behaviour if the firmware
      does not support the "New comm." interface.  Users of old cards or
      firmware haven't reported the problems that are potentially solved by
      switching to OMR0.

--- sys/dev/aac/aac.c
+++ sys/dev/aac/aac.c
@@ -2375,7 +2375,8 @@ aac_rx_get_fwstatus(struct aac_softc *sc)
 {
 	fwprintf(sc, HBA_FLAGS_DBG_FUNCTION_ENTRY_B, "");
 
-	return(AAC_GETREG4(sc, AAC_RX_FWSTATUS));
+	return(AAC_GETREG4(sc, sc->flags & AAC_FLAGS_NEW_COMM ?
+	    AAC_RX_OMR0 : AAC_RX_FWSTATUS));
 }
 
 static int
@@ -2394,7 +2395,8 @@ aac_rkt_get_fwstatus(struct aac_softc *sc)
 {
 	fwprintf(sc, HBA_FLAGS_DBG_FUNCTION_ENTRY_B, "");
 
-	return(AAC_GETREG4(sc, AAC_RKT_FWSTATUS));
+	return(AAC_GETREG4(sc, sc->flags & AAC_FLAGS_NEW_COMM ?
+	    AAC_RKT_OMR0 : AAC_RKT_FWSTATUS));
 }
 
 /*
