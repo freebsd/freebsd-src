41c0d990e74f7032fac4fcd56ee7ffbd20575527
jhb <jhb@FreeBSD.org>
2009-10-30 19:25:13 +0000

    MFC 192106: Port mca(4) to pc98.
    Pointy hat to:	jhb

--- sys/pc98/pc98/machdep.c
+++ sys/pc98/pc98/machdep.c
@@ -112,6 +112,7 @@ __FBSDID("$FreeBSD$");
 #include <machine/cpu.h>
 #include <machine/cputypes.h>
 #include <machine/intr_machdep.h>
+#include <machine/mca.h>
 #include <machine/md_var.h>
 #include <machine/pc/bios.h>
 #include <machine/pcb.h>
@@ -257,6 +258,8 @@ cpu_startup(dummy)
 	vm_pager_bufferinit();
 
 	cpu_setregs();
+
+	mca_init();
 }
 
 /*
