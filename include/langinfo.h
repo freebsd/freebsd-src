250abcae59abf84b718b892517af472e077c3a03
edwin <edwin@FreeBSD.org>
2009-10-31 06:35:40 +0000

    MFCs of r197764, r197765, r197766, r197847:
    Modified locale(1) to be able to show the altmon_X fields and the
    [cxX]_fmt's.  Also modify the "-k list" option to display only
    fields with a certain prefix.
    
    Add the comment "(FreeBSD only)" to the altmonth_x keywords

--- include/langinfo.h
+++ include/langinfo.h
@@ -114,6 +114,20 @@ typedef	__nl_item	nl_item;
 #define	D_MD_ORDER	57	/* month/day order (local extension) */
 #endif
 
+/* standalone months forms for %OB */
+#define	ALTMON_1	58
+#define	ALTMON_2	59
+#define	ALTMON_3	60
+#define	ALTMON_4	61
+#define	ALTMON_5	62
+#define	ALTMON_6	63
+#define	ALTMON_7	64
+#define	ALTMON_8	65
+#define	ALTMON_9	66
+#define	ALTMON_10	67
+#define	ALTMON_11	68
+#define	ALTMON_12	69
+
 __BEGIN_DECLS
 char	*nl_langinfo(nl_item);
 __END_DECLS
