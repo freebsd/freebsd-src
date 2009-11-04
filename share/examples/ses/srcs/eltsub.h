16733ea919f3e2b73c32b38e6a0081afeac627c4
delphij <delphij@FreeBSD.org>
2009-11-04 23:36:23 +0000

    Cleanup code to make it WARNS=6 clean:
     - ANSIfy prototypes;
     - Add __unused for parameters that is not being currently used;
     - Add a header for subrountines being called from other modules.
    
    Reviewed by:	mjacob

--- share/examples/ses/srcs/eltsub.h
+++ share/examples/ses/srcs/eltsub.h
@@ -32,50 +32,5 @@
  * mjacob@feral.com
  */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include SESINC
-
-int
-main(int a, char **v)
-{
-	int fd;
-	int i;
-	ses_objstat obj;
-	long cvt;
-	char *x;
-
-	if (a != 7) {
-usage:
-		fprintf(stderr,
-		    "usage: %s device objectid stat0 stat1 stat2 stat3\n", *v);
-		return (1);
-	}
-	fd = open(v[1], O_RDWR);
-	if (fd < 0) {
-		perror(v[1]);
-		return (1);
-	}
-	x = v[2];
-	cvt = strtol(v[2], &x, 0);
-	if (x == v[2]) {
-		goto usage;
-	}
-	obj.obj_id = cvt;
-	for (i = 0; i < 4; i++) {
-		x = v[3 + i];
-		cvt = strtol(v[3 + i],  &x, 0);
-		if (x == v[3 + i]) {
-			goto usage;
-		}
-		obj.cstat[i] = cvt;
-	}
-	if (ioctl(fd, SESIOC_SETOBJSTAT, (caddr_t) &obj) < 0) {
-		perror("SESIOC_SETOBJSTAT");
-	}
-	(void) close(fd);
-	return (0);
-}
+char * geteltnm(int);
+char * stat2ascii(int, u_char *);
