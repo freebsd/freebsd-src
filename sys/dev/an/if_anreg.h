df547a14dfc4569efc4ed60aaa9094f3313d8235
jhb <jhb@FreeBSD.org>
2009-11-06 18:28:13 +0000

    - Use device_printf() instead of printf() with an explicit unit number   in the PCI attach routine. - Simplify PCI probe. - Remove no-longer-used 'unit' from an_attach() parameters.
    PR:		kern/126924
    Submitted by:	gavin

--- sys/dev/an/if_anreg.h
+++ sys/dev/an/if_anreg.h
@@ -511,7 +511,7 @@ int	an_pci_probe	(device_t);
 int	an_probe	(device_t);
 int	an_shutdown	(device_t);
 void	an_resume	(device_t);
-int	an_attach		(struct an_softc *, int, int);
+int	an_attach		(struct an_softc *, int);
 int	an_detach	(device_t);
 void    an_stop		(struct an_softc *);
 
