138b1174a623634b265299a95c263adcbc9c76de
thompsa <thompsa@FreeBSD.org>
2009-11-08 20:51:15 +0000

    Integrate lost interrupts patch from the old USB stack.
    Some EHCI chips from VIA / ATI seem to trigger interrupts before writing back
    the qTD status, or miss signalling occasionally under heavy load.  If the host
    machine is too fast, we can miss transaction completion - when we scan the
    active list the transaction still seems to be active. This generally exhibits
    itself as a umass stall that never recovers.
    
    We work around this behaviour by setting up this callback after any softintr
    that completes with transactions still pending, giving us another chance to
    check for completion after the writeback has taken place
    
    Submitted by:	Alexander Nedotsuko
    MFC after:	3 days

--- sys/dev/usb/controller/ehci_pci.c
+++ sys/dev/usb/controller/ehci_pci.c
@@ -439,6 +439,19 @@ ehci_pci_attach(device_t self)
 		break;
 	}
 
+	/* Dropped interrupts workaround */
+	switch (pci_get_vendor(self)) {
+	case PCI_EHCI_VENDORID_ATI:
+	case PCI_EHCI_VENDORID_VIA:
+		sc->sc_flags |= EHCI_SCFLG_LOSTINTRBUG;
+		if (bootverbose)
+			device_printf(self,
+			    "Dropped interrupts workaround enabled\n");
+		break;
+	default:
+		break;
+	}
+
 	err = ehci_init(sc);
 	if (!err) {
 		err = device_probe_and_attach(sc->sc_bus.bdev);
