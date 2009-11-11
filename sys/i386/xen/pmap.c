655bef28813f1802bfe254a4d006d927fc4cf9ac
avg <avg@FreeBSD.org>
2009-11-11 14:21:31 +0000

    reflect that pg_ps_enabled is a tunable, not just a read-only sysctl
    Nod from:	jhb

--- sys/i386/xen/pmap.c
+++ sys/i386/xen/pmap.c
@@ -279,7 +279,7 @@ static struct mtx PMAP2mutex;
 
 SYSCTL_NODE(_vm, OID_AUTO, pmap, CTLFLAG_RD, 0, "VM/pmap parameters");
 static int pg_ps_enabled;
-SYSCTL_INT(_vm_pmap, OID_AUTO, pg_ps_enabled, CTLFLAG_RD, &pg_ps_enabled, 0,
+SYSCTL_INT(_vm_pmap, OID_AUTO, pg_ps_enabled, CTLFLAG_RDTUN, &pg_ps_enabled, 0,
     "Are large page mappings enabled?");
 
 SYSCTL_INT(_vm_pmap, OID_AUTO, pv_entry_max, CTLFLAG_RD, &pv_entry_max, 0,
