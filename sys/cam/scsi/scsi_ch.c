2f5418e2b9f72a27fd6515faa265998fda9f1d0b
mav <mav@FreeBSD.org>
2009-11-14 20:13:38 +0000

    MFp4: Fix several device freeze counting bugs.

--- sys/cam/scsi/scsi_ch.c
+++ sys/cam/scsi/scsi_ch.c
@@ -606,7 +606,8 @@ chdone(struct cam_periph *periph, union ccb *done_ccb)
 					retry_scheduled = 0;
 
 				/* Don't wedge this device's queue */
-				cam_release_devq(done_ccb->ccb_h.path,
+				if ((done_ccb->ccb_h.status & CAM_DEV_QFRZN) != 0)
+					cam_release_devq(done_ccb->ccb_h.path,
 						 /*relsim_flags*/0,
 						 /*reduction*/0,
 						 /*timeout*/0,
