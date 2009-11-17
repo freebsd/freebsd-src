3f93fb7729e8f4a099fcde3b2d8d168f9a898662
avg <avg@FreeBSD.org>
2009-11-17 09:37:43 +0000

    MFC r199015: ichwd: don't attach to isa pnp device(s) by accident

--- sys/dev/ichwd/ichwd.c
+++ sys/dev/ichwd/ichwd.c
@@ -68,6 +68,7 @@ __FBSDID("$FreeBSD$");
 #include <machine/resource.h>
 #include <sys/watchdog.h>
 
+#include <isa/isavar.h>
 #include <dev/pci/pcivar.h>
 
 #include <dev/ichwd/ichwd.h>
@@ -393,7 +394,9 @@ static int
 ichwd_probe(device_t dev)
 {
 
-	(void)dev;
+	/* Do not claim some ISA PnP device by accident. */
+	if (isa_get_logicalid(dev) != 0)
+		return (ENXIO);
 	return (0);
 }
 
