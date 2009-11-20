be8a54e0c3db12d3fecac288bf6ddfd350731d11
jkim <jkim@FreeBSD.org>
2009-11-20 19:01:09 +0000

    Correct copyright date in the BPF regression test.

--- tools/regression/bpf/bpf_filter/bpf_test.c
+++ tools/regression/bpf/bpf_filter/bpf_test.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (C) 2009 Jung-uk Kim <jkim@FreeBSD.org>. All rights reserved.
+ * Copyright (C) 2008-2009 Jung-uk Kim <jkim@FreeBSD.org>. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
