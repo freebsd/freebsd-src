a5e831ded986435cfad6a485ebdeaa49280d5d1e
Attilio Rao <attilio@FreeBSD.org>
2009-11-22 16:04:49 +0000

    MFC r199208, r199223: Move inet_aton() (specular to inet_ntoa(), already present in libkern) into libkern in order to made it usable by other modules than alias_proxy.
    Sponsored by:	Sandvine Incorporated

--- sys/netinet/in.h
+++ sys/netinet/in.h
@@ -733,6 +733,7 @@ int	 in_broadcast(struct in_addr, struct ifnet *);
 int	 in_canforward(struct in_addr);
 int	 in_localaddr(struct in_addr);
 int	 in_localip(struct in_addr);
+int	 inet_aton(const char *, struct in_addr *); /* in libkern */
 char	*inet_ntoa(struct in_addr); /* in libkern */
 char	*inet_ntoa_r(struct in_addr ina, char *buf); /* in libkern */
 void	 in_ifdetach(struct ifnet *);
