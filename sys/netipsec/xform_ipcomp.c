2681485085434eacc7451e50ca492e0e209e1ede
bz <bz@FreeBSD.org>
2009-11-29 20:37:30 +0000

    Add more statistics variables for IPcomp.
    Try to version the struct in a backward compatible way.
    People asked for the versioning of the stats structs in general before.
    
    MFC after:	5 days

--- sys/netipsec/xform_ipcomp.c
+++ sys/netipsec/xform_ipcomp.c
@@ -345,7 +345,7 @@ ipcomp_output(
 	 * See RFC 3173, 2.2. Non-Expansion Policy.
 	 */
 	if (m->m_pkthdr.len <= ipcompx->minlen) {
-		/* XXX-BZ V_ipcompstat.threshold++; */
+		V_ipcompstat.ipcomps_threshold++;
 		return ipsec_process_done(m, isr);
 	}
 
@@ -569,8 +569,10 @@ ipcomp_output_cb(struct cryptop *crp)
 			goto bad;
 		}
 	} else {
-		/* compression was useless, we have lost time */
-		/* XXX add statistic */
+		/* Compression was useless, we have lost time. */
+		V_ipcompstat.ipcomps_uncompr++;
+		DPRINTF(("%s: compressions was useless %d - %d <= %d\n",
+		    __func__, crp->crp_ilen, skip, crp->crp_olen));
 		/* XXX remember state to not compress the next couple
 		 *     of packets, RFC 3173, 2.2. Non-Expansion Policy */
 	}
@@ -609,3 +611,13 @@ ipcomp_attach(void)
 }
 
 SYSINIT(ipcomp_xform_init, SI_SUB_PROTO_DOMAIN, SI_ORDER_MIDDLE, ipcomp_attach, NULL);
+
+static void
+vnet_ipcomp_attach(const void *unused __unused)
+{
+
+	V_ipcompstat.version = IPCOMPSTAT_VERSION;
+}
+
+VNET_SYSINIT(vnet_ipcomp_xform_init, SI_SUB_PROTO_DOMAIN, SI_ORDER_MIDDLE,
+    vnet_ipcomp_attach, NULL);
