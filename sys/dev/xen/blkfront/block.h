b9270d3580810fd26492ceb14a52ade38c62ff96
kmacy <kmacy@FreeBSD.org>
2009-11-30 04:20:43 +0000

    Update license to reflect terms in xen 2.0 as of the time when the driver was ported to FreeBSD

--- sys/dev/xen/blkfront/block.h
+++ sys/dev/xen/blkfront/block.h
@@ -1,5 +1,23 @@
 /*
  * 
+ * XenBSD block device driver
+ *
+ * Copyright (c) 2009 Frank Suchomel, Citrix
+ * Copyright (c) 2009 Doug F. Rabson, Citrix
+ * Copyright (c) 2005 Kip Macy
+ * Copyright (c) 2003-2004, Keir Fraser & Steve Hand
+ * Modifications by Mark A. Williamson are (c) Intel Research Cambridge
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
