aaae58c4917f1cb847cbade369ca1d6803a7251e
Bjoern A. Zeeb <bz@FreeBSD.org>
2009-12-01 14:56:00 +0000

    Unbreak user space after if_timer/if_watchdog removal in r199975.
    Tested by:	glebius

--- usr.bin/netstat/netstat.h
+++ usr.bin/netstat/netstat.h
@@ -49,7 +49,6 @@ extern int	numeric_addr;	/* show addresses numerically */
 extern int	numeric_port;	/* show ports numerically */
 extern int	rflag;	/* show routing tables (or routing stats) */
 extern int	sflag;	/* show protocol statistics */
-extern int	tflag;	/* show i/f watchdog timers */
 extern int	Wflag;	/* wide display */
 extern int	xflag;	/* extended display, includes all socket buffer info */
 extern int	zflag;	/* zero stats */
