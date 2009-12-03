879c7d27302e0fb0654529de4b88f210ee11b022
ed <ed@FreeBSD.org>
2009-12-03 15:48:24 +0000

    Add a new library: libulog.
    One of the things I really want to do, is to get rid of the limitations
    of our current utmp(5) mechanism:
    
    - It only allows 8 byte TTY device names.
    - The hostname only allows 16 bytes of storage.
    
    I'm not a big fan of <utmpx.h>, but I think we should at least try to
    add parts of it. Unfortunately we cannot implement <utmpx.h>, because we
    miss various fields, such as ut_id, ut_pid, etc. The API provided by
    libulog shares some similarities with <utmpx.h>, so it shouldn't be too
    hard to port these applications eventually. In most simple cases, it
    should just be a matter of removing the ulog_ prefix everywhere.
    
    As a bonus, it also implements a function called ulog_login_pseudo(),
    which allows unprivileged applications to write log entries, provided
    they have a valid file descriptor to a pseudo-terminal master device.
    
    libulog will allow a smoother transition to a new file format by adding
    a library interface to deal with utmp/wtmp/lastlog files. I initially
    thought about adding the functionality to libutil, but because I'm not
    planning on keeping this library around forever, we'd better keep it
    separated.
    
    Next items on the todo list:
    
    1. Port applications in the base system (and ports) to libulog, instead
       of letting them use <utmp.h>.
    2. Remove <utmp.h>, implement <utmpx.h> and reimplement this library on
       top.
    3. Port as many applications as possible back to <utmpx.h>.

---
+++ lib/libulog/ulog_getutxent.c
@@ -0,0 +1,84 @@
+/*-
+ * Copyright (c) 2009 Ed Schouten <ed@FreeBSD.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <timeconv.h>
+
+#include "ulog_internal.h"
+
+static FILE *ufile;
+
+void
+ulog_endutxent(void)
+{
+	if (ufile != NULL)
+		fclose(ufile);
+	ufile = NULL;
+}
+
+struct ulog_utmpx *
+ulog_getutxent(void)
+{
+	struct futmp ut;
+	static struct ulog_utmpx utx;
+
+	/* Open the utmp file if not already done so. */
+	if (ufile == NULL)
+		ulog_setutxent();
+	if (ufile == NULL)
+		return (NULL);
+
+	if (fread(&ut, sizeof ut, 1, ufile) != 1)
+		return (NULL);
+#define	COPY_STRING(field) do {					\
+	free(utx.ut_ ## field); 				\
+	utx.ut_ ## field = strndup(ut.ut_ ## field,		\
+	    sizeof ut.ut_ ## field);				\
+	if (utx.ut_ ## field == NULL)				\
+		utx.ut_ ## field = __DECONST(char *, "");	\
+} while (0)
+	COPY_STRING(user);
+	COPY_STRING(line);
+	COPY_STRING(host);
+	utx.ut_tv.tv_sec = _time32_to_time(ut.ut_time);
+	utx.ut_tv.tv_usec = 0;
+	
+	return (&utx);
+}
+
+void
+ulog_setutxent(void)
+{
+
+	if (ufile != NULL)
+		fclose(ufile);
+	ufile = fopen(_PATH_UTMP, "r");
+}
