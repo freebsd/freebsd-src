9f94f2e9dc186c6f235e6a985d3e807168f27770
thompsa <thompsa@FreeBSD.org>
2009-12-03 23:24:12 +0000

    Add uhci/ehci controller ids.
    Submitted by:	mitya_cabletv.dp.ua

--- sys/dev/usb/controller/uhci_pci.c
+++ sys/dev/usb/controller/uhci_pci.c
@@ -230,6 +230,18 @@ uhci_pci_match(device_t self)
 		return ("Intel 82801I (ICH9) USB controller");
 	case 0x29398086:
 		return ("Intel 82801I (ICH9) USB controller");
+	case 0x3a348086:
+		return ("Intel 82801IJ (ICH10) USB controller USB-A");
+	case 0x3a358086:
+		return ("Intel 82801IJ (ICH10) USB controller USB-B");
+	case 0x3a368086:
+		return ("Intel 82801IJ (ICH10) USB controller USB-C");
+	case 0x3a378086:
+		return ("Intel 82801IJ (ICH10) USB controller USB-D");
+	case 0x3a388086:
+		return ("Intel 82801IJ (ICH10) USB controller USB-E");
+	case 0x3a398086:
+		return ("Intel 82801IJ (ICH10) USB controller USB-F");
 
 	case 0x719a8086:
 		return ("Intel 82443MX USB controller");
