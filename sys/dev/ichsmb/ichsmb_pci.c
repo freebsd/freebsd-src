d93b49b38c6af2b3e80ee4d8fecf4e76b02e4df3
Andriy Gapon <avg@FreeBSD.org>
2009-12-03 08:01:29 +0000

    ichsmb: add pci ids for some newer supported hardware
    Submitted by:	Dmitry S. Luhtionov <mitya@cabletv.dp.ua>
    MFC after:	5 days

--- sys/dev/ichsmb/ichsmb_pci.c
+++ sys/dev/ichsmb/ichsmb_pci.c
@@ -75,6 +75,9 @@ __FBSDID("$FreeBSD$");
 #define ID_82801EB			0x24D38086
 #define ID_82801FB			0x266A8086
 #define ID_82801GB			0x27da8086
+#define ID_82801H			0x283e8086
+#define ID_82801I			0x29308086
+#define ID_82801JI			0x3a308086
 #define ID_6300ESB			0x25a48086
 #define	ID_631xESB			0x269b8086
 
@@ -151,6 +154,15 @@ ichsmb_pci_probe(device_t dev)
 		break;
 	case ID_82801GB:
 		device_set_desc(dev, "Intel 82801GB (ICH7) SMBus controller");
+		break;
+	case ID_82801H:
+		device_set_desc(dev, "Intel 82801H (ICH8) SMBus controller");
+		break;
+	case ID_82801I:
+		device_set_desc(dev, "Intel 82801I (ICH9) SMBus controller");
+		break;
+	case ID_82801JI:
+		device_set_desc(dev, "Intel 82801JI (ICH10) SMBus controller");
 		break;
 	case ID_6300ESB:
 		device_set_desc(dev, "Intel 6300ESB (ICH) SMBus controller");
