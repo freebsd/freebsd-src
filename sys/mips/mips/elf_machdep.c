c4de0a57f3db2d160c9f5fdfa6bdcb1c166748a5
Bjoern A. Zeeb <bz@FreeBSD.org>
2009-12-05 20:43:15 +0000

    MFC r197729:
      Make sure that the primary native brandinfo always gets added
      first and the native ia32 compat as middle (before other things).
      o(ld)brandinfo as well as third party like linux, kfreebsd, etc.
      stays on SI_ORDER_ANY coming last.
    
      The reason for this is only to make sure that even in case we would
      overflow the MAX_BRANDS sized array, the native FreeBSD brandinfo
      would still be there and the system would be operational.
    
    Reviewed by:	kib

--- sys/mips/mips/elf_machdep.c
+++ sys/mips/mips/elf_machdep.c
@@ -90,7 +90,7 @@ static Elf32_Brandinfo freebsd_brand_info = {
 	.flags		= BI_BRAND_NOTE
 };
 
-SYSINIT(elf32, SI_SUB_EXEC, SI_ORDER_ANY,
+SYSINIT(elf32, SI_SUB_EXEC, SI_ORDER_FIRST,
     (sysinit_cfunc_t) elf32_insert_brand_entry,
     &freebsd_brand_info);
 
