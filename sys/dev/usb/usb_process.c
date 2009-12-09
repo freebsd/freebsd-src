c7838bf992f923309131a3939ee2e4c3d3448903
Andrew Thompson <thompsa@FreeBSD.org>
2009-12-09 22:39:09 +0000

    MFC r199675
     Provide tunables for some of the usb sysctls that affect boot behaviour.
    
    Submitted by:	Andriy Gapon

--- sys/dev/usb/usb_process.c
+++ sys/dev/usb/usb_process.c
@@ -83,6 +83,8 @@ static int usb_proc_debug;
 SYSCTL_NODE(_hw_usb, OID_AUTO, proc, CTLFLAG_RW, 0, "USB process");
 SYSCTL_INT(_hw_usb_proc, OID_AUTO, debug, CTLFLAG_RW, &usb_proc_debug, 0,
     "Debug level");
+
+TUNABLE_INT("hw.usb.proc.debug", &usb_proc_debug);
 #endif
 
 /*------------------------------------------------------------------------*
