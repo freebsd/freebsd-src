8df723e58f3fdc4e824aadfee6f44c1c4133380b
delphij <delphij@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2009-12-11 23:23:57 +0000

    Move unistd.h includes to individual .c files and remove unnecessary includes.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@200418 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/talk/ctl_transact.c
+++ usr.bin/talk/ctl_transact.c
@@ -42,7 +42,6 @@ static const char sccsid[] = "@(#)ctl_transact.c	8.1 (Berkeley) 6/6/93";
 #include <arpa/inet.h>
 
 #include <errno.h>
-#include <string.h>
 
 #include "talk.h"
 #include "talk_ctl.h"
