a43f4edeec1e84a0bf4a27d200ea2b5644b90db3
mav <mav@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2009-12-18 14:41:30 +0000

    Remove duplicate devstat_start_transaction_bio() call. It is already called from geom_disk. Dulicate call causes wrong queue depth and busy accounting.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@200668 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/cam/scsi/scsi_cd.c
+++ sys/cam/scsi/scsi_cd.c
@@ -1464,8 +1464,6 @@ cdstart(struct cam_periph *periph, union ccb *start_ccb)
 		} else {
 			bioq_remove(&softc->bio_queue, bp);
 
-			devstat_start_transaction_bio(softc->disk->d_devstat, bp);
-
 			scsi_read_write(&start_ccb->csio,
 					/*retries*/cd_retry_count,
 					/* cbfcnp */ cddone,
