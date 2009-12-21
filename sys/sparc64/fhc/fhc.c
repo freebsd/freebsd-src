0b3287150f3d093c26f12dddf257b1fbdd93ca8e
marius <marius@FreeBSD.org>
2009-12-21 21:29:16 +0000

    Provide and consume missing module dependency information.

--- sys/sparc64/fhc/fhc.c
+++ sys/sparc64/fhc/fhc.c
@@ -120,8 +120,9 @@ static driver_t fhc_driver = {
 static devclass_t fhc_devclass;
 
 DRIVER_MODULE(fhc, central, fhc_driver, fhc_devclass, 0, 0);
-DRIVER_MODULE(fhc, nexus, fhc_driver, fhc_devclass, 0, 0);
 MODULE_DEPEND(fhc, central, 1, 1, 1);
+DRIVER_MODULE(fhc, nexus, fhc_driver, fhc_devclass, 0, 0);
+MODULE_DEPEND(fhc, nexus, 1, 1, 1);
 MODULE_VERSION(fhc, 1);
 
 static const struct intr_controller fhc_ic = {
