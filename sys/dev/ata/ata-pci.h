4822b84df379c1e8a850868c8bfdb42e70249f76
mav <mav@FreeBSD.org>
2009-12-22 19:48:06 +0000

    Add support for Intel SCH PATA controller.
    PR:		kern/140251

--- sys/dev/ata/ata-pci.h
+++ sys/dev/ata/ata-pci.h
@@ -204,6 +204,7 @@ struct ata_pci_controller {
 #define ATA_I82801JI_R1         0x3a258086
 #define ATA_I82801JI_S2         0x3a268086
 #define ATA_I31244              0x32008086
+#define ATA_ISCH                0x811a8086
 
 #define ATA_ITE_ID              0x1283
 #define ATA_IT8211F             0x82111283
