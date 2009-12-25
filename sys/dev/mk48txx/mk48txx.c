1b0230578503731f5887ed7a3a3d89c229a42d59
Marius Strobl <marius@FreeBSD.org>
2009-12-25 21:41:05 +0000

    Remove clause 3 and 4 from TNF licenses.
    Obtained from:	NetBSD

--- sys/dev/mk48txx/mk48txx.c
+++ sys/dev/mk48txx/mk48txx.c
@@ -13,13 +13,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
@@ -33,7 +26,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- *	from: NetBSD: mk48txx.c,v 1.15 2004/07/05 09:24:31 pk Exp
+ *	$NetBSD: mk48txx.c,v 1.25 2008/04/28 20:23:50 martin Exp $
  */
 
 #include <sys/cdefs.h>
