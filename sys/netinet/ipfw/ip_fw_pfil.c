bc282bc9c2a89e0057fbcafd37e9813dd0005263
luigi <luigi@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2009-12-28 12:29:13 +0000

    bring the NGM_IPFW_COOKIE back into ng_ipfw.h, libnetgraph expects to find it there. Unfortunately this reintroduces the dependency on ip_fw_pfil.c
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@201124 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/ipfw/ip_fw_pfil.c
+++ sys/netinet/ipfw/ip_fw_pfil.c
@@ -61,6 +61,7 @@ __FBSDID("$FreeBSD$");
 #include <netinet/ipfw/ip_fw_private.h>
 #include <netinet/ip_divert.h>
 #include <netinet/ip_dummynet.h>
+#include <netgraph/ng_ipfw.h>
 
 #include <machine/in_cksum.h>
 
