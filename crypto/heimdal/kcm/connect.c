e78fca8580e3c261e52a463af8029e61adc467fc
brooks <brooks@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-01-03 20:45:49 +0000

    The size of credential messages is limited by CMGROUP_MAX rather than NGROUPS.
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@201444 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- crypto/heimdal/kcm/connect.c
+++ crypto/heimdal/kcm/connect.c
@@ -149,7 +149,7 @@ update_client_creds(int s, kcm_client *peer)
 	struct sockcred *sc;
 	
 	memset(&msg, 0, sizeof(msg));
-	crmsgsize = CMSG_SPACE(SOCKCREDSIZE(NGROUPS));
+	crmsgsize = CMSG_SPACE(SOCKCREDSIZE(CMGROUP_MAX));
 	if (crmsgsize == 0)
 	    return 1 ;
 
