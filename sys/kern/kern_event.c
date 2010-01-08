752f1215caaa8328c98075ea0be548fb58d07a7c
Brooks Davis <brooks@FreeBSD.org>
2010-01-08 22:13:15 +0000

    MFC r201352   If a filter has already been added, actually return EEXIST when trying   at add it again.

--- sys/kern/kern_event.c
+++ sys/kern/kern_event.c
@@ -757,6 +757,7 @@ kqueue_add_filteropts(int filt, struct filterops *filtops)
 {
 	int error;
 
+	error = 0;
 	if (filt > 0 || filt + EVFILT_SYSCOUNT < 0) {
 		printf(
 "trying to add a filterop that is out of range: %d is beyond %d\n",
@@ -773,7 +774,7 @@ kqueue_add_filteropts(int filt, struct filterops *filtops)
 	}
 	mtx_unlock(&filterops_lock);
 
-	return (0);
+	return (error);
 }
 
 int
