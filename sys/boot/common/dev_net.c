89a10341f3efdc1278c9cece8ceb2f33e3fa12e6
marius <marius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-01-09 17:09:57 +0000

    Remove clause 3 and 4 from TNF licenses (this was the only 4-clause TNF license FreeBSD had in sys/boot).
    Obtained from:	NetBSD
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@201901 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/boot/common/dev_net.c
+++ sys/boot/common/dev_net.c
@@ -1,6 +1,4 @@
-/*
- * $NetBSD: dev_net.c,v 1.12 1997/12/10 20:38:37 gwr Exp $
- */
+/*	$NetBSD: dev_net.c,v 1.23 2008/04/28 20:24:06 martin Exp $	*/
 
 /*-
  * Copyright (c) 1997 The NetBSD Foundation, Inc.
@@ -17,13 +15,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
