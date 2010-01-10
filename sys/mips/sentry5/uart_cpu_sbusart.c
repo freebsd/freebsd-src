398288a5c7f36f951a89150a0c8b7c40ae7d82df
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-01-10 20:09:30 +0000

    Merge from projects/mips to head by hand:
    r201881 | imp | 2010-01-08 20:08:22 -0700 (Fri, 08 Jan 2010) | 3 lines
    Rename mips_pcpu_init to mips_pcpu0_init since it applies only to the
    BSP.  Provide a missing prototype.
    
    r201845 | imp | 2010-01-08 15:48:21 -0700 (Fri, 08 Jan 2010) | 2 lines
    Centralize initialization of pcpu, and set curthread early...
    
    r201631 | neel | 2010-01-05 23:42:08 -0700 (Tue, 05 Jan 2010) | 5 lines
    Remove all CFE-specific code from locore.S. The CFE entrypoint initialization
    is now done in platform-specific code.
    Approved by: imp (mentor)
    
    r198669 | rrs | 2009-10-30 02:53:11 -0600 (Fri, 30 Oct 2009) | 5 lines
    With this commit our friend RMI will now compile. I have
    not tested it and the chances of it running yet are about
    ZERO.. but it will now compile. The hard part now begins,
     making it run ;-)
    
    r198154 | rrs | 2009-10-15 15:03:32 -0600 (Thu, 15 Oct 2009) | 10 lines
    
    Does 4 things:
    1) Adds future RMI directories
    2) Places intr_machdep.c in specfic files.arch pointing to the generic
       intr_machdep.c.  This allows us to have an architecture dependant
       intr_machdep.c (which we will need for RMI) in the machine specific
       directory
    3) removes intr_machdep.c from files.mips
    4) Adds some TARGET_XLR_XLS ifdef's for the machine specific intra_machdep.h. We
       may need to look at finding a better place to put this. But first I want to
       get this thing compiling.
    
    r196236 | imp | 2009-08-14 19:03:13 -0600 (Fri, 14 Aug 2009) | 3 lines
    Fix style error replicated multiple times.  Move to
    mips_bus_space_generic for octeon obio impl.
    
    r191282 | gonzo | 2009-04-19 16:02:14 -0600 (Sun, 19 Apr 2009) | 3 lines
    - Make mips_bus_space_generic be of type bus_space_tag_t instead of
        struct bus_space and update all relevant places.
    
    r187415 | gonzo | 2009-01-18 16:49:02 -0700 (Sun, 18 Jan 2009) | 3 lines
    - Move Silicon Backplanes code out to system-wide level (dev/siba) as
        it's going to be used not only for siba5 devices.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@202036 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/mips/sentry5/uart_cpu_sbusart.c
+++ sys/mips/sentry5/uart_cpu_sbusart.c
@@ -76,7 +76,7 @@ uart_cpu_getdev(int devtype, struct uart_devinfo *di)
 	di->parity = UART_PARITY_NONE;
 
 	uart_bus_space_io = MIPS_PHYS_TO_KSEG1(SENTRY5_UART1ADR);
-	uart_bus_space_mem = MIPS_PHYS_TO_KSEG1(SENTRY5_UART1ADR);
+	uart_bus_space_mem = mips_bus_space_generic;
 	di->bas.bsh = MIPS_PHYS_TO_KSEG1(SENTRY5_UART1ADR);
 	return (0);
 }
