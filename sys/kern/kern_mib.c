a093b41dafbd7defb7612a20d5672d938b8a54ed
brooks <brooks@FreeBSD.org>
2010-01-12 07:49:34 +0000

    Replace the static NGROUPS=NGROUPS_MAX+1=1024 with a dynamic kern.ngroups+1.  kern.ngroups can range from NGROUPS_MAX=1023 to INT_MAX-1.  Given that the Windows group limit is 1024, this range should be sufficient for most applications.
    MFC after:	1 month

--- sys/kern/kern_mib.c
+++ sys/kern/kern_mib.c
@@ -125,7 +125,7 @@ SYSCTL_INT(_kern, KERN_POSIX1, posix1version, CTLFLAG_RD,
     0, _POSIX_VERSION, "Version of POSIX attempting to comply to");
 
 SYSCTL_INT(_kern, KERN_NGROUPS, ngroups, CTLFLAG_RD,
-    0, NGROUPS_MAX,
+    &ngroups_max, 0,
     "Maximum number of supplemental groups a user can belong to");
 
 SYSCTL_INT(_kern, KERN_JOB_CONTROL, job_control, CTLFLAG_RD,
