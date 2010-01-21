49cc13441b0f01b031ce131443f8416eb979e766
John Baldwin <jhb@FreeBSD.org>
2010-01-21 19:11:18 +0000

    MFC 198411: - Fix several off-by-one errors when using MAXCOMLEN.  The p_comm[] and   td_name[] arrays are actually MAXCOMLEN + 1 in size and a few places that   created shadow copies of these arrays were just using MAXCOMLEN. - Prefer using sizeof() of an array type to explicit constants for the   array length in a few places. - Ensure that all of p_comm[] and td_name[] is always zero'd during   execve() to guard against any possible information leaks.  Previously   trailing garbage in p_comm[] could be leaked to userland in ktrace   record headers via td_name[].

--- sys/kern/subr_taskqueue.c
+++ sys/kern/subr_taskqueue.c
@@ -301,7 +301,7 @@ taskqueue_start_threads(struct taskqueue **tqp, int count, int pri,
 	struct thread *td;
 	struct taskqueue *tq;
 	int i, error;
-	char ktname[MAXCOMLEN];
+	char ktname[MAXCOMLEN + 1];
 
 	if (count <= 0)
 		return (EINVAL);
@@ -309,7 +309,7 @@ taskqueue_start_threads(struct taskqueue **tqp, int count, int pri,
 	tq = *tqp;
 
 	va_start(ap, name);
-	vsnprintf(ktname, MAXCOMLEN, name, ap);
+	vsnprintf(ktname, sizeof(ktname), name, ap);
 	va_end(ap);
 
 	tq->tq_threads = malloc(sizeof(struct thread *) * count, M_TASKQUEUE,
