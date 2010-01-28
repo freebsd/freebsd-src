ffeae29e0e90a9b4c079a6515193a09b18f408e3
imp <imp@FreeBSD.org>
2010-01-28 20:46:40 +0000

    Add Cavium's standard copyright to those files that are currently lacking a copyright/license statement.  All these files were in the Cavium FreeBSD source drop and appear to be written by Cavium (some are nearly verbatim copies of files from the cnusers' 1.9.0 SDK, which also uses this copyright).

--- sys/mips/cavium/dev/rgmii/octeon_rgmx.c
+++ sys/mips/cavium/dev/rgmii/octeon_rgmx.c
@@ -1,3 +1,41 @@
+/***********************license start***************
+ *  Copyright (c) 2003-2008 Cavium Networks (support@cavium.com). All rights
+ *  reserved.
+ *
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials provided
+ *        with the distribution.
+ *
+ *      * Neither the name of Cavium Networks nor the names of
+ *        its contributors may be used to endorse or promote products
+ *        derived from this software without specific prior written
+ *        permission.
+ *
+ *  TO THE MAXIMUM EXTENT PERMITTED BY LAW, THE SOFTWARE IS PROVIDED "AS IS"
+ *  AND WITH ALL FAULTS AND CAVIUM NETWORKS MAKES NO PROMISES, REPRESENTATIONS
+ *  OR WARRANTIES, EITHER EXPRESS, IMPLIED, STATUTORY, OR OTHERWISE, WITH
+ *  RESPECT TO THE SOFTWARE, INCLUDING ITS CONDITION, ITS CONFORMITY TO ANY
+ *  REPRESENTATION OR DESCRIPTION, OR THE EXISTENCE OF ANY LATENT OR PATENT
+ *  DEFECTS, AND CAVIUM SPECIFICALLY DISCLAIMS ALL IMPLIED (IF ANY) WARRANTIES
+ *  OF TITLE, MERCHANTABILITY, NONINFRINGEMENT, FITNESS FOR A PARTICULAR
+ *  PURPOSE, LACK OF VIRUSES, ACCURACY OR COMPLETENESS, QUIET ENJOYMENT, QUIET
+ *  POSSESSION OR CORRESPONDENCE TO DESCRIPTION.  THE ENTIRE RISK ARISING OUT
+ *  OF USE OR PERFORMANCE OF THE SOFTWARE LIES WITH YOU.
+ *
+ *
+ *  For any questions regarding licensing please contact marketing@caviumnetworks.com
+ *
+ ***********************license end**************************************/
+
 /*
  * octeon_rgmx.c     RGMII  Ethernet Interfaces on Octeon
  *
@@ -176,9 +214,6 @@ static void  octeon_rgmx_init	      (void *);
 static int   octeon_rgmx_ioctl        (struct ifnet *, u_long, caddr_t);
 static void  octeon_rgmx_output_start (struct ifnet *);
 static void  octeon_rgmx_output_start_locked (struct ifnet *);
-#if 0
-static void  octeon_rgmx_watchdog     (struct ifnet *);
-#endif
 static int   octeon_rgmx_medchange    (struct ifnet *);
 static void  octeon_rgmx_medstat      (struct ifnet *, struct ifmediareq *);
 
@@ -295,9 +330,6 @@ static int octeon_rgmx_init_ifnet (struct rgmx_softc_dev *sc)
         if_initname(sc->ifp, device_get_name(sc->sc_dev), device_get_unit(sc->sc_dev));
         ifp->if_start    = octeon_rgmx_output_start;
         ifp->if_ioctl    = octeon_rgmx_ioctl;
-        /* Watchdog interface is now deprecated.
-        ifp->if_watchdog = octeon_rgmx_watchdog;
-        */
 	ifp->if_hwassist = CSUM_TCP | CSUM_UDP;
 	ifp->if_capabilities = IFCAP_HWCSUM;
 	ifp->if_capenable = ifp->if_capabilities;
