40a0ca351d2bcdb938cf03d9571810eadbfe0601
rwatson <rwatson@FreeBSD.org>
2010-01-30 18:43:54 +0000

    Merge c170405 from the p4 TrustedBSD Capabilities branch to capabilities8:
      Removed the multiple definitions which snuck in here
    
    Submitted by:	Jonathan Anderson <jonathan.anderson at cl.cam.ac.uk>

--- lib/csu/amd64/crt1.c
+++ lib/csu/amd64/crt1.c
@@ -134,42 +134,4 @@ _capstart(char **ap, void (*cleanup)(void))
 	exit( cap_main(argc, argv, env) );
 }
 
-
-/* The Capsicum entry function. */
-void
-_capstart(char **ap, void (*cleanup)(void))
-{
-	int argc;
-	char **argv;
-	char **env;
-	const char *s;
-
-	argc = *(long *)(void *)ap;
-	argv = ap + 1;
-	env = ap + 2 + argc;
-	environ = env;
-	if (argc > 0 && argv[0] != NULL) {
-		__progname = argv[0];
-		for (s = __progname; *s != '\0'; s++)
-			if (*s == '/')
-				__progname = s + 1;
-	}
-
-	if (&_DYNAMIC != NULL)
-		atexit(cleanup);
-	else
-		_init_tls();
-
-#ifdef GCRT
-	atexit(_mcleanup);
-#endif
-	atexit(_fini);
-#ifdef GCRT
-/*	monstartup(&eprol, &etext);
-__asm__("eprol:");*/        /* XXX: does this interfere with profiling? */
-#endif
-	_init();
-	exit( cap_main(argc, argv, env) );
-}
-
 __asm__(".ident\t\"$FreeBSD$\"");
--- lib/csu/common/crtbrand.c
+++ lib/csu/common/crtbrand.c
@@ -62,17 +62,3 @@ int cap_main(int argc, char **argv, char **env)
 	write(2, warning, sizeof(warning));
 }
 
-
-int cap_main(int argc, char **argv, char **env)
-{
-	const char warning[] =
-		"ERROR: attempting to run a regular binary in capability mode."
-		"\n\n"
-		"If you wish to run a binary in a sandbox, you must provide a "
-		"cap_main() function which takes the same arguments as main()."
-		"\n";
-
-	write(2, warning, sizeof(warning));
-	return 1;
-}
-
