9e88b759a7283d3023fa15ba693426c3e1adfbbd
rwatson <rwatson@FreeBSD.org>
2010-01-30 18:33:10 +0000

    Merge c169761 from the p4 TrustedBSD Capabilities branch to capabilities8:
      Since we're using write() rather than err(), we need to actually return
      something from cap_main()
    
    Submitted by:	Jonathan Anderson <jonathan.anderson at cl.cam.ac.uk>

--- lib/csu/common/crtbrand.c
+++ lib/csu/common/crtbrand.c
@@ -58,5 +58,6 @@ int cap_main(int argc, char **argv, char **env)
 		"ERROR: attempting to run a regular binary in capability mode.\n\nIf you wish to run a binary in a sandbox, you must provide a cap_main() function which takes the same arguments as main().\n";
 
 	write(2, warning, sizeof(warning));
+	return 1;
 }
 
