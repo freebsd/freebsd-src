46891c91c885e7a5df1365dc1f51b03b226d307b
delphij <delphij@FreeBSD.org>
2010-02-01 11:05:19 +0000

    MFC r201406:
    Reduce diff against OpenSolaris - move Giant acquire/release to
    zfs_znode.c.  As a side effect this also eliminates two potential
    Giant leaks.
    
    Approved by:  re (kib)

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_znode.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_znode.c
@@ -1023,6 +1023,7 @@ zfs_zinactive(znode_t *zp)
 	vnode_t	*vp = ZTOV(zp);
 	zfsvfs_t *zfsvfs = zp->z_zfsvfs;
 	uint64_t z_id = zp->z_id;
+	int vfslocked;
 
 	ASSERT(zp->z_dbuf && zp->z_phys);
 
@@ -1055,7 +1056,9 @@ zfs_zinactive(znode_t *zp)
 		ZFS_OBJ_HOLD_EXIT(zfsvfs, z_id);
 		ASSERT(vp->v_count == 0);
 		vrecycle(vp, curthread);
+		vfslocked = VFS_LOCK_GIANT(zfsvfs->z_vfs);
 		zfs_rmnode(zp);
+		VFS_UNLOCK_GIANT(vfslocked);
 		return;
 	}
 	mutex_exit(&zp->z_lock);
