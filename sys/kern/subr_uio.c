3811e248e926272b422fccb874c62fd2eed9b532
ed <ed@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-02-21 19:53:33 +0000

    Decompose the most lousy named file in sys/kern; kern_subr.c.
    Although this file has historically been used as a dumping ground for
    random functions, nowadays it only contains functions related to copying
    bits {from,to} userspace and hash table utility functions.
    
    Behold, subr_uio.c and subr_hash.c.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@204176 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/subr_uio.c
+++ sys/kern/subr_uio.c
@@ -42,13 +42,10 @@ __FBSDID("$FreeBSD$");
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kernel.h>
-#include <sys/ktr.h>
 #include <sys/limits.h>
 #include <sys/lock.h>
 #include <sys/mutex.h>
 #include <sys/proc.h>
-#include <sys/malloc.h>
-#include <sys/resourcevar.h>
 #include <sys/sched.h>
 #include <sys/sysctl.h>
 #include <sys/vnode.h>
@@ -358,96 +355,6 @@ again:
 	uio->uio_resid--;
 	uio->uio_offset++;
 	return (0);
-}
-
-/*
- * General routine to allocate a hash table with control of memory flags.
- */
-void *
-hashinit_flags(int elements, struct malloc_type *type, u_long *hashmask,
-    int flags)
-{
-	long hashsize;
-	LIST_HEAD(generic, generic) *hashtbl;
-	int i;
-
-	if (elements <= 0)
-		panic("hashinit: bad elements");
-
-	/* Exactly one of HASH_WAITOK and HASH_NOWAIT must be set. */
-	KASSERT((flags & HASH_WAITOK) ^ (flags & HASH_NOWAIT),
-	    ("Bad flags (0x%x) passed to hashinit_flags", flags));
-
-	for (hashsize = 1; hashsize <= elements; hashsize <<= 1)
-		continue;
-	hashsize >>= 1;
-
-	if (flags & HASH_NOWAIT)
-		hashtbl = malloc((u_long)hashsize * sizeof(*hashtbl),
-		    type, M_NOWAIT);
-	else
-		hashtbl = malloc((u_long)hashsize * sizeof(*hashtbl),
-		    type, M_WAITOK);
-
-	if (hashtbl != NULL) {
-		for (i = 0; i < hashsize; i++)
-			LIST_INIT(&hashtbl[i]);
-		*hashmask = hashsize - 1;
-	}
-	return (hashtbl);
-}
-
-/*
- * Allocate and initialize a hash table with default flag: may sleep.
- */
-void *
-hashinit(int elements, struct malloc_type *type, u_long *hashmask)
-{
-
-	return (hashinit_flags(elements, type, hashmask, HASH_WAITOK));
-}
-
-void
-hashdestroy(void *vhashtbl, struct malloc_type *type, u_long hashmask)
-{
-	LIST_HEAD(generic, generic) *hashtbl, *hp;
-
-	hashtbl = vhashtbl;
-	for (hp = hashtbl; hp <= &hashtbl[hashmask]; hp++)
-		if (!LIST_EMPTY(hp))
-			panic("hashdestroy: hash not empty");
-	free(hashtbl, type);
-}
-
-static const int primes[] = { 1, 13, 31, 61, 127, 251, 509, 761, 1021, 1531,
-			2039, 2557, 3067, 3583, 4093, 4603, 5119, 5623, 6143,
-			6653, 7159, 7673, 8191, 12281, 16381, 24571, 32749 };
-#define NPRIMES (sizeof(primes) / sizeof(primes[0]))
-
-/*
- * General routine to allocate a prime number sized hash table.
- */
-void *
-phashinit(int elements, struct malloc_type *type, u_long *nentries)
-{
-	long hashsize;
-	LIST_HEAD(generic, generic) *hashtbl;
-	int i;
-
-	if (elements <= 0)
-		panic("phashinit: bad elements");
-	for (i = 1, hashsize = primes[1]; hashsize <= elements;) {
-		i++;
-		if (i == NPRIMES)
-			break;
-		hashsize = primes[i];
-	}
-	hashsize = primes[i - 1];
-	hashtbl = malloc((u_long)hashsize * sizeof(*hashtbl), type, M_WAITOK);
-	for (i = 0; i < hashsize; i++)
-		LIST_INIT(&hashtbl[i]);
-	*nentries = hashsize;
-	return (hashtbl);
 }
 
 void
