1009f2c9dd1788a53e3fa0b5ba8618c8e4615995
Luigi Rizzo <luigi@FreeBSD.org>
2010-03-04 16:08:51 +0000

    remove stale comment

--- sbin/ipfw/dummynet.c
+++ sbin/ipfw/dummynet.c
@@ -10,8 +10,6 @@
  *
  * This software is provided ``AS IS'' without any warranties of any kind.
  *
- * NEW command line interface for IP firewall facility
- *
  * $FreeBSD$
  *
  * dummynet support
