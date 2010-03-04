fdd47c311c0e0a44938bb446f655c310eafb52f9
luigi <luigi@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-03-04 21:01:59 +0000

    don't use keywords as variable names.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@204735 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/ipfw/test/mylist.h
+++ sys/netinet/ipfw/test/mylist.h
@@ -13,19 +13,19 @@ struct list_head {
 #define INIT_LIST_HEAD(l) do {  (l)->prev = (l)->next = (l); } while (0)
 #define list_empty(l)   ( (l)->next == l )
 static inline void
-__list_add(struct list_head *new, struct list_head *prev,
+__list_add(struct list_head *o, struct list_head *prev,
         struct list_head *next)
 {
-        next->prev = new;
-        new->next = next;
-        new->prev = prev;
-        prev->next = new;
+        next->prev = o;
+        o->next = next;
+        o->prev = prev;
+        prev->next = o;
 }
  
 static inline void
-list_add_tail(struct list_head *new, struct list_head *head)
+list_add_tail(struct list_head *o, struct list_head *head)
 {
-        __list_add(new, head->prev, head);
+        __list_add(o, head->prev, head);
 }
 
 #define list_first_entry(pL, ty, member)        \
