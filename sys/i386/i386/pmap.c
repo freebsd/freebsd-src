8f05cbda740d32e7a155e0a5c18198668a897695
jhb <jhb@FreeBSD.org>
2010-03-12 03:08:47 +0000

    Fix the previous attempt to fix kernel builds of HEAD on 7.x.  Use the __gnu_inline__ attribute for PMAP_INLINE when using the 7.x compiler to match what 7.x uses for PMAP_INLINE.

--- sys/i386/i386/pmap.c
+++ sys/i386/i386/pmap.c
@@ -163,7 +163,7 @@ __FBSDID("$FreeBSD$");
 
 #if !defined(DIAGNOSTIC)
 #ifdef __GNUC_GNU_INLINE__
-#define PMAP_INLINE	inline
+#define PMAP_INLINE	__attribute__((__gnu_inline__)) inline
 #else
 #define PMAP_INLINE	extern inline
 #endif
