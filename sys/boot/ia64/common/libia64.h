9d393b2e282ed42898a039931593d757087d1997
Marcel Moolenaar <marcel@FreeBSD.org>
2010-03-14 23:52:30 +0000

    Add ia64_elf_obj to file_formats[] and unify the SKI and EFI support by moving the definition from the separate conf.c to the common exec.c.

--- sys/boot/ia64/common/libia64.h
+++ sys/boot/ia64/common/libia64.h
@@ -32,27 +32,29 @@
 #include <bootstrap.h>
 #include <ia64/include/bootinfo.h>
 
+int bi_load(struct preloaded_file *, uint64_t *);
+
 /*
  * Portability functions provided by the loader
  * implementation specific to the platform.
  */
-extern uint64_t ldr_alloc(vm_offset_t);
-extern int ldr_bootinfo(struct bootinfo *, uint64_t *);
-extern int ldr_enter(const char *);
+uint64_t ldr_alloc(vm_offset_t);
+int ldr_bootinfo(struct bootinfo *, uint64_t *);
+int ldr_enter(const char *);
 
 /*
  * Functions and variables provided by the ia64 common code
  * and shared by all loader implementations.
  */
 
-extern int ia64_autoload(void);
+int ia64_autoload(void);
 
-extern ssize_t ia64_copyin(const void *, vm_offset_t, size_t);
-extern ssize_t ia64_copyout(vm_offset_t, void *, size_t);
-extern ssize_t ia64_readin(int, vm_offset_t, size_t);
+ssize_t ia64_copyin(const void *, vm_offset_t, size_t);
+ssize_t ia64_copyout(vm_offset_t, void *, size_t);
+ssize_t ia64_readin(int, vm_offset_t, size_t);
 
-extern char *ia64_fmtdev(struct devdesc *);
-extern int ia64_getdev(void **, const char *, const char **);
-extern int ia64_setcurrdev(struct env_var *, int, const void *);
+char *ia64_fmtdev(struct devdesc *);
+int ia64_getdev(void **, const char *, const char **);
+int ia64_setcurrdev(struct env_var *, int, const void *);
 
 #endif /* !_LIBIA64_H_ */
