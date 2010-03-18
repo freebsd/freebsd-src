1afdf5764e23446aae43fcbc2a8d90d1142caf2c
rpaulo <rpaulo@FreeBSD.org>
2010-03-18 11:06:38 +0000

    Fix a couple of bugs with 802.11n: o Process the BAR frame on the adhoc, mesh and sta modes o Fix the format of the ADDBA reply frame o Fix references to the spec section numbers
    Also, print the all the MCS rates in bootverbose.
    
    Sponsored by:	iXsystems, Inc.
    Obtained from:	//depot/user/rpaulo/80211n/...

--- sys/net80211/ieee80211_sta.c
+++ sys/net80211/ieee80211_sta.c
@@ -1739,6 +1739,11 @@ sta_recv_mgmt(struct ieee80211_node *ni, struct mbuf *m0,
 }
 
 static void
-sta_recv_ctl(struct ieee80211_node *ni, struct mbuf *m0, int subtype)
+sta_recv_ctl(struct ieee80211_node *ni, struct mbuf *m, int subtype)
 {
+	switch (subtype) {
+	case IEEE80211_FC0_SUBTYPE_BAR:
+		ieee80211_recv_bar(ni, m);
+		break;
+	}
 }
