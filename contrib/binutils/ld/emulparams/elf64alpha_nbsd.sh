25820e5d54346847e0c254ed461a0567e2550868
delphij <delphij@FreeBSD.org>
2010-03-19 00:19:55 +0000

    Create a custom branch where I will be able to do the merge.

--- contrib/binutils/ld/emulparams/elf64alpha_nbsd.sh
+++
@@ -1,2 +0,0 @@
-. ${srcdir}/emulparams/elf64alpha.sh
-ENTRY=__start
--- gnu/usr.bin/binutils/ld/elf32ltsmipn32_fbsd.sh
+++
@@ -1,4 +0,0 @@
-# $FreeBSD$
-. ${srcdir}/emulparams/elf32ltsmipn32.sh
-. ${srcdir}/emulparams/elf_fbsd.sh
-GENERATE_PIE_SCRIPT=yes
