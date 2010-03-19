9d7b18b4c63a1cd67e58b701ff32867807216a40
avg <avg@FreeBSD.org>
2010-03-19 14:48:32 +0000

    pmap amd64/i386: fix a typo in a comment
    MFC after:	3 days

--- sys/i386/i386/pmap.c
+++ sys/i386/i386/pmap.c
@@ -1014,7 +1014,7 @@ pmap_invalidate_cache_range(vm_offset_t sva, vm_offset_t eva)
 
 		/*
 		 * No targeted cache flush methods are supported by CPU,
-		 * or the supplied range is bigger then 2MB.
+		 * or the supplied range is bigger than 2MB.
 		 * Globally invalidate cache.
 		 */
 		pmap_invalidate_cache();
