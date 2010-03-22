ffb9c4e603ced5c273d2921293f9c6622ee6eb9e
Pyun YongHyeon <yongari@FreeBSD.org>
2010-03-22 19:36:47 +0000

    MFC r182414:   Add support in ifconfig to control the vlan hardware filter feature.

--- sbin/ifconfig/ifvlan.c
+++ sbin/ifconfig/ifvlan.c
@@ -180,6 +180,8 @@ static struct cmd vlan_cmds[] = {
 	DEF_CMD("-vlanmtu",	-IFCAP_VLAN_MTU,	setifcap),
 	DEF_CMD("vlanhwtag",	IFCAP_VLAN_HWTAGGING,	setifcap),
 	DEF_CMD("-vlanhwtag",	-IFCAP_VLAN_HWTAGGING,	setifcap),
+	DEF_CMD("vlanhwfilter",	IFCAP_VLAN_HWFILTER,	setifcap),
+	DEF_CMD("-vlanhwfilter", -IFCAP_VLAN_HWFILTER,	setifcap),
 };
 static struct afswtch af_vlan = {
 	.af_name	= "af_vlan",
