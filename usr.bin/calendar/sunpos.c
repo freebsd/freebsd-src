d111bc69c33db2a9bbd55d8eb539fce801244b49
edwin <edwin@FreeBSD.org>
2010-03-30 06:42:01 +0000

    On request of joel@:
    - Remove the 3rd clause of the UC Berkeley copyrighted files.
    - For the files added copyrighted by me, move the "All rights
      reserved" to the next line.

--- usr.bin/calendar/sunpos.c
+++ usr.bin/calendar/sunpos.c
@@ -1,5 +1,6 @@
 /*-
- * Copyright (c) 2009-2010 Edwin Groothuis. All rights reserved.
+ * Copyright (c) 2009-2010 Edwin Groothuis <edwin@FreeBSD.org>.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
