b64177fa09a09121e9b36fd985e2355f3603258c
jfv <jfv@FreeBSD.org>
2010-03-30 19:54:29 +0000

    fix my clobber of the copyright date :)

--- sys/dev/ixgbe/ixgbe.c
+++ sys/dev/ixgbe/ixgbe.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 
-  Copyright (c) 2001-2009, Intel Corporation 
+  Copyright (c) 2001-2010, Intel Corporation 
   All rights reserved.
   
   Redistribution and use in source and binary forms, with or without 
