92f61ecb4bc02c0a89f73d8c1fb18acd6bce31ad
Navdeep Parhar <np@FreeBSD.org>
2010-03-31 00:26:02 +0000

    Fix tx drop statistics.
    MFC after:	3 days

--- sys/dev/cxgb/cxgb_adapter.h
+++ sys/dev/cxgb/cxgb_adapter.h
@@ -248,7 +248,6 @@ struct sge_txq {
 	struct callout	txq_timer;
 	struct callout	txq_watchdog;
 	uint64_t        txq_coalesced;
-	uint32_t        txq_drops;
 	uint32_t        txq_skipped;
 	uint32_t        txq_enqueued;
 	uint32_t	txq_dump_start;
