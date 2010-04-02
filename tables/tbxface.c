9a8b5e6355e39ba0602a7f9484dca40f429fe6b0
Jung-uk Kim <jkim@FreeBSD.org>
2010-04-02 21:05:28 +0000

    Additional fix for root table size regression.
    Set the size of the new root table correctly. Not the current size, but
    the number of current tables + the default increment.
    
    http://git.moblin.org/cgit.cgi/acpica/commit/?id=752d13e80656436cd58e9710225a80c9deb3b053
    
    Obtained from:	Intel

--- tables/tbxface.c
+++ tables/tbxface.c
@@ -300,10 +300,14 @@ AcpiReallocateRootTable (
 
     ACPI_MEMCPY (Tables, AcpiGbl_RootTableList.Tables, CurrentSize);
 
-    /* Update the root table descriptor */
-
+    /*
+     * Update the root table descriptor. The new size will be the current
+     * number of tables plus the increment, independent of the reserved
+     * size of the original table list.
+     */
     AcpiGbl_RootTableList.Tables = Tables;
-    AcpiGbl_RootTableList.Size += ACPI_ROOT_TABLE_SIZE_INCREMENT;
+    AcpiGbl_RootTableList.Size =
+        AcpiGbl_RootTableList.Count + ACPI_ROOT_TABLE_SIZE_INCREMENT;
     AcpiGbl_RootTableList.Flags =
         ACPI_ROOT_ORIGIN_ALLOCATED | ACPI_ROOT_ALLOW_RESIZE;
 
