467d5479951a967aa4ed31d07540c64b7f2500d8
avg <avg@FreeBSD.org>
2010-04-12 21:12:03 +0000

    MFC r206178: ibc/db/hash: cap auto-tuned block size

--- lib/libc/db/hash/hash.c
+++ lib/libc/db/hash/hash.c
@@ -293,6 +293,8 @@ init_hash(HTAB *hashp, const char *file, const HASHINFO *info)
 		if (stat(file, &statbuf))
 			return (NULL);
 		hashp->BSIZE = statbuf.st_blksize;
+		if (hashp->BSIZE > MAX_BSIZE)
+			hashp->BSIZE = MAX_BSIZE;
 		hashp->BSHIFT = __log2(hashp->BSIZE);
 	}
 
