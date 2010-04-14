af4b8626fc0b98c9147e04fa3f430dfb647aa49b
John Baldwin <jhb@FreeBSD.org>
2010-04-14 15:23:16 +0000

    MFC 205536: Reject attempts to create a MAP_ANON mapping with a non-zero offset.

--- sys/vm/vm_mmap.c
+++ sys/vm/vm_mmap.c
@@ -232,7 +232,7 @@ mmap(td, uap)
 	fp = NULL;
 	/* make sure mapping fits into numeric range etc */
 	if ((ssize_t) uap->len < 0 ||
-	    ((flags & MAP_ANON) && uap->fd != -1))
+	    ((flags & MAP_ANON) && (uap->fd != -1 || pos != 0)))
 		return (EINVAL);
 
 	if (flags & MAP_STACK) {
@@ -298,7 +298,6 @@ mmap(td, uap)
 		handle = NULL;
 		handle_type = OBJT_DEFAULT;
 		maxprot = VM_PROT_ALL;
-		pos = 0;
 	} else {
 		/*
 		 * Mapping file, get fp for validation. Obtain vnode and make
