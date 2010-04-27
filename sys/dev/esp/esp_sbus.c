953bb121682fc842f9f54348746d7c98e5f9993c
marius <marius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-04-27 15:50:25 +0000

    Take advantage of OF_getscsinitid().
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@207281 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/esp/esp_sbus.c
+++ sys/dev/esp/esp_sbus.c
@@ -466,9 +466,7 @@ espattach(struct esp_softc *esc, const struct ncr53c9x_glue *gluep)
 		goto fail_lock;
 	}
 
-	if (OF_getprop(ofw_bus_get_node(esc->sc_dev), "scsi-initiator-id",
-	    &sc->sc_id, sizeof(sc->sc_id)) == -1)
-		sc->sc_id = 7;
+	sc->sc_id = OF_getscsinitid(esc->sc_dev);
 
 #ifdef ESP_SBUS_DEBUG
 	device_printf(esc->sc_dev, "%s: sc_id %d, freq %d\n",
