e8006d16492a5a32d329e4021f8fa54db77e0686
alc <alc@FreeBSD.org>
2010-05-06 04:23:52 +0000

    Eliminate dead code.

--- sys/mips/include/pmap.h
+++ sys/mips/include/pmap.h
@@ -175,7 +175,6 @@ void pmap_unmapdev(vm_offset_t, vm_size_t);
 vm_offset_t pmap_steal_memory(vm_size_t size);
 void pmap_set_modified(vm_offset_t pa);
 int page_is_managed(vm_offset_t pa);
-void pmap_page_is_free(vm_page_t m);
 void pmap_kenter(vm_offset_t va, vm_paddr_t pa);
 void pmap_kremove(vm_offset_t va);
 void *pmap_kenter_temporary(vm_paddr_t pa, int i);
