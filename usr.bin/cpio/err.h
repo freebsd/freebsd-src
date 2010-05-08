b6cb6076444c1b3006d0064be4b6098b8fa92b6c
Tim Kientzle <kientzle@FreeBSD.org>
2010-05-08 16:47:33 +0000

    bsdcpio 2.8.3

--- usr.bin/cpio/err.h
+++ usr.bin/cpio/err.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2003-2007 Tim Kientzle
+ * Copyright (c) 2009 Joerg Sonnenberger
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,44 +25,9 @@
  * $FreeBSD$
  */
 
-/*
- * This header is the first thing included in any of the cpio
- * source files.  As far as possible, platform-specific issues should
- * be dealt with here and not within individual source files.
- */
+#ifndef LAFE_ERR_H
+#define LAFE_ERR_H
 
-#ifndef CPIO_PLATFORM_H_INCLUDED
-#define	CPIO_PLATFORM_H_INCLUDED
-
-#if defined(PLATFORM_CONFIG_H)
-/* Use hand-built config.h in environments that need it. */
-#include PLATFORM_CONFIG_H
-#else
-/* Read config.h or die trying. */
-#include "config.h"
-#endif
-
-/* Get a real definition for __FBSDID if we can */
-#if HAVE_SYS_CDEFS_H
-#include <sys/cdefs.h>
-#endif
-
-/* If not, define it so as to avoid dangling semicolons. */
-#ifndef __FBSDID
-#define	__FBSDID(a)     struct _undefined_hack
-#endif
-
-#ifdef HAVE_LIBARCHIVE
-/* If we're using the platform libarchive, include system headers. */
-#include <archive.h>
-#include <archive_entry.h>
-#else
-/* Otherwise, include user headers. */
-#include "archive.h"
-#include "archive_entry.h"
-#endif
-
-/* How to mark functions that don't return. */
 #if defined(__GNUC__) && (__GNUC__ > 2 || \
                           (__GNUC__ == 2 && __GNUC_MINOR__ >= 5))
 #define __LA_DEAD       __attribute__((__noreturn__))
@@ -70,8 +35,9 @@
 #define __LA_DEAD
 #endif
 
-#if defined(_WIN32) && !defined(__CYGWIN__)
-#include "cpio_windows.h"
-#endif
+extern const char *progname;
 
-#endif /* !CPIO_PLATFORM_H_INCLUDED */
+void	warnc(int code, const char *fmt, ...);
+void	errc(int eval, int code, const char *fmt, ...) __LA_DEAD;
+
+#endif
