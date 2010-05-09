c7bd88a0119cb46d0b316c64a2f6f47be00d5ffb
alc <alc@FreeBSD.org>
2010-05-09 00:32:52 +0000

    Remove the page queues lock around a call to vm_page_activate().  Make the page dirty before adding it to the active queue.

--- sys/vm/swap_pager.c
+++ sys/vm/swap_pager.c
@@ -1707,11 +1707,9 @@ swp_pager_force_pagein(vm_object_t object, vm_pindex_t pindex)
 	m = vm_page_grab(object, pindex, VM_ALLOC_NORMAL|VM_ALLOC_RETRY);
 	if (m->valid == VM_PAGE_BITS_ALL) {
 		vm_object_pip_subtract(object, 1);
-		vm_page_lock(m);
-		vm_page_lock_queues();
-		vm_page_activate(m);
 		vm_page_dirty(m);
-		vm_page_unlock_queues();
+		vm_page_lock(m);
+		vm_page_activate(m);
 		vm_page_unlock(m);
 		vm_page_wakeup(m);
 		vm_pager_page_unswapped(m);
