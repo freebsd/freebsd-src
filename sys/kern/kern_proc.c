f0ef3c482fc651fac60f71bc0e85442b0d9569a8
kib <kib@FreeBSD.org>
2010-05-09 12:32:10 +0000

    MFC r207363: Remove caddr_t casts.

--- sys/kern/kern_proc.c
+++ sys/kern/kern_proc.c
@@ -1083,11 +1083,9 @@ sysctl_out_proc_copyout(struct kinfo_proc *ki, struct sysctl_req *req)
 
 	if (req->flags & SCTL_MASK32) {
 		freebsd32_kinfo_proc_out(ki, &ki32);
-		error = SYSCTL_OUT(req, (caddr_t)&ki32,
-		    sizeof(struct kinfo_proc32));
+		error = SYSCTL_OUT(req, &ki32, sizeof(struct kinfo_proc32));
 	} else
-		error = SYSCTL_OUT(req, (caddr_t)ki,
-		    sizeof(struct kinfo_proc));
+		error = SYSCTL_OUT(req, ki, sizeof(struct kinfo_proc));
 	return (error);
 }
 #else
@@ -1095,7 +1093,7 @@ static int
 sysctl_out_proc_copyout(struct kinfo_proc *ki, struct sysctl_req *req)
 {
 
-	return (SYSCTL_OUT(req, (caddr_t)ki, sizeof(struct kinfo_proc)));
+	return (SYSCTL_OUT(req, ki, sizeof(struct kinfo_proc)));
 }
 #endif
 
