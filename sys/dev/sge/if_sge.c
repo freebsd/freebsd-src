143f0c7b6df425604556e3a4b13fce9d6e0fab9b
Pyun YongHyeon <yongari@FreeBSD.org>
2010-05-10 18:37:46 +0000

    MFC r207545,207625: r207545:   Fix wrong dma tag usage. Previously it used TX descriptor ring dma   tag which should be TX mbuf dma tag.
    r207625:
      Remove clearing RxHashTable2 register. The register is reprogrammed
      in sge_rxfilter().

--- sys/dev/sge/if_sge.c
+++ sys/dev/sge/if_sge.c
@@ -1421,7 +1421,7 @@ sge_encap(struct sge_softc *sc, struct mbuf **m_head)
 		}
 		*m_head = m;
 	}
-	error = bus_dmamap_load_mbuf_sg(sc->sge_cdata.sge_tx_tag, map,
+	error = bus_dmamap_load_mbuf_sg(sc->sge_cdata.sge_txmbuf_tag, map,
 	    *m_head, txsegs, &nsegs, 0);
 	if (error != 0) {
 		m_freem(*m_head);
@@ -1430,10 +1430,11 @@ sge_encap(struct sge_softc *sc, struct mbuf **m_head)
 	}
 	/* Check descriptor overrun. */
 	if (sc->sge_cdata.sge_tx_cnt + nsegs >= SGE_TX_RING_CNT) {
-		bus_dmamap_unload(sc->sge_cdata.sge_tx_tag, map);
+		bus_dmamap_unload(sc->sge_cdata.sge_txmbuf_tag, map);
 		return (ENOBUFS);
 	}
-	bus_dmamap_sync(sc->sge_cdata.sge_tx_tag, map, BUS_DMASYNC_PREWRITE);
+	bus_dmamap_sync(sc->sge_cdata.sge_txmbuf_tag, map,
+	    BUS_DMASYNC_PREWRITE);
 
 	cflags = 0;
 	if ((*m_head)->m_pkthdr.csum_flags & CSUM_IP)
@@ -1576,7 +1577,6 @@ sge_init_locked(struct sge_softc *sc)
 	CSR_WRITE_4(sc, RX_DESC, SGE_ADDR_LO(sc->sge_ldata.sge_rx_paddr));
 
 	CSR_WRITE_4(sc, TxMacControl, 0x60);
-	CSR_WRITE_4(sc, 0x6c, 0);
 	CSR_WRITE_4(sc, RxWakeOnLan, 0);
 	CSR_WRITE_4(sc, RxWakeOnLanData, 0);
 	/* Allow receiving VLAN frames. */
