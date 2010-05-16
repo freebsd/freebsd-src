d575fde0f3fec3cc8abba895058a0b8e5a6352b5
Jack F Vogel <jfv@FreeBSD.org>
2010-05-16 04:31:16 +0000

    Change poll routine to conform to proper interface.

--- sys/dev/e1000/if_em.c
+++ sys/dev/e1000/if_em.c
@@ -1345,7 +1345,7 @@ em_init(void *arg)
  *  Legacy polling routine: note this only works with single queue
  *
  *********************************************************************/
-static int
+static void
 em_poll(struct ifnet *ifp, enum poll_cmd cmd, int count)
 {
 	struct adapter *adapter = ifp->if_softc;
@@ -1356,7 +1356,7 @@ em_poll(struct ifnet *ifp, enum poll_cmd cmd, int count)
 	EM_CORE_LOCK(adapter);
 	if ((ifp->if_drv_flags & IFF_DRV_RUNNING) == 0) {
 		EM_CORE_UNLOCK(adapter);
-		return (rx_done);
+		return;
 	}
 
 	if (cmd == POLL_AND_CHECK_STATUS) {
@@ -1386,7 +1386,7 @@ em_poll(struct ifnet *ifp, enum poll_cmd cmd, int count)
 #endif
 	EM_TX_UNLOCK(txr);
 
-	return (rx_done);
+	return;
 }
 #endif /* DEVICE_POLLING */
 
