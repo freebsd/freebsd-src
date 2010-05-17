07430cf7b32c59e3c056aac44aaf7a06ef0b6206
yongari <yongari@FreeBSD.org>
2010-05-17 17:02:42 +0000

    MFC r207851:   Implement TSO and TSO over VLAN. Increase number of allowed   fragmentation of mbuf chain to 32 from 16 because TSO can send 64KB   sized packet which in turn requires long list of mbuf chain. Due to   lack of documentation, I'm not sure whether driver have to pull up   ethernet/IP/TCP header with options to make controller work but   driver have to parse TCP header to update pseudo TCP checksum   anyway. The controller expects pseudo TCP checksum computed by   upper stack and the checksum should follow the MS NDIS   specification to make TSO work.
      Tested by:	xclin <xclin <> cs dot nctu dot edu dot tw >

--- sys/dev/sge/if_sgereg.h
+++ sys/dev/sge/if_sgereg.h
@@ -283,7 +283,9 @@ struct sge_desc {
 #define	SGE_RX_RING_CNT		256 /* [8, 1024] */
 #define	SGE_TX_RING_CNT		256 /* [8, 8192] */
 #define	SGE_DESC_ALIGN		16
-#define	SGE_MAXTXSEGS		16
+#define	SGE_MAXTXSEGS		32
+#define	SGE_TSO_MAXSIZE		(65535 + sizeof(struct ether_vlan_header))
+#define	SGE_TSO_MAXSEGSIZE	4096
 #define	SGE_RX_BUF_ALIGN	sizeof(uint64_t)
 
 #define	SGE_RX_RING_SZ		(SGE_RX_RING_CNT * sizeof(struct sge_desc))
