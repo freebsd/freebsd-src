5349f70253f218c14e9ae0cee2349b9bc429c44f
Gabor Kovesdan <gabor@FreeBSD.org>
2010-06-10 10:17:00 +0000

    - Fix style(9)
    Submitted by:	ed
    Approved by:	delphij (mentor)

--- usr.bin/dc/dc.c
+++ usr.bin/dc/dc.c
@@ -87,7 +87,7 @@ main(int argc, char *argv[])
 	while ((ch = getopt_long(argc, argv, "e:f:Vx", long_options, NULL)) != -1) {
 		switch (ch) {
 		case 'e':
-			if(!preproc_done)
+			if (!preproc_done)
 				init_bmachine(extended_regs);
 			src_setstring(&src, optarg);
 			reset_bmachine(&src);
@@ -95,7 +95,7 @@ main(int argc, char *argv[])
 			preproc_done = true;
 			break;
 		case 'f':
-			if(!preproc_done)
+			if (!preproc_done)
 				init_bmachine(extended_regs);
 			procfile(optarg);
 			preproc_done = true;
