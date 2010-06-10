b07a6c8dccf9684b1406af200ab6c0f245355874
Marcel Moolenaar <marcel@FreeBSD.org>
2010-06-10 21:56:14 +0000

    Print a warning about NULL maps only when booting verbose.

--- sys/kern/subr_busdma.c
+++ sys/kern/subr_busdma.c
@@ -923,7 +923,8 @@ bus_dmamap_unload(bus_dma_tag_t dmat, bus_dmamap_t map)
 	struct bounce_page *bpage;
 
 	if (map == NULL) {
-		warn_null_map(__func__, __builtin_return_address(0));
+		if (bootverbose)
+			warn_null_map(__func__, __builtin_return_address(0));
 		return;
 	}
 
@@ -939,7 +940,8 @@ bus_dmamap_sync(bus_dma_tag_t dmat, bus_dmamap_t map, bus_dmasync_op_t op)
 	struct bounce_page *bpage;
 
 	if (map == NULL) {
-		warn_null_map(__func__, __builtin_return_address(0));
+		if (bootverbose)
+			warn_null_map(__func__, __builtin_return_address(0));
 		return;
 	}
 
