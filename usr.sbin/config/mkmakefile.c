1b799749e4740d47d60773725ea89158b7f12c4f
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-06-13 16:54:11 +0000

    style(9) fixes:
    o make cmd scoped to the whole do_rules function, since it really is
      scoped to the whole fucnion.  Making it static was the wrong way to
      fix referencing it outside of the block in which it was declared
      (and conforms to the style of the rest of the file).
    o remove a couple of meaningless blank lines
    o properly wrap one line.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@209135 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/config/mkmakefile.c
+++ usr.sbin/config/mkmakefile.c
@@ -683,6 +683,7 @@ do_rules(FILE *f)
 	char *cp, *np, och;
 	struct file_list *ftp;
 	char *compilewith;
+	char cmd[128];
 
 	STAILQ_FOREACH(ftp, &ftab, f_next) {
 		if (ftp->f_warn)
@@ -720,25 +721,22 @@ do_rules(FILE *f)
 		compilewith = ftp->f_compilewith;
 		if (compilewith == 0) {
 			const char *ftype = NULL;
-			static char cmd[128];
 
 			switch (ftp->f_type) {
-
 			case NORMAL:
 				ftype = "NORMAL";
 				break;
-
 			case PROFILING:
 				if (!profiling)
 					continue;
 				ftype = "PROFILE";
 				break;
-
 			default:
 				printf("config: don't know rules for %s\n", np);
 				break;
 			}
-			snprintf(cmd, sizeof(cmd), "${%s_%c%s}\n\t@${NORMAL_CTFCONVERT}", ftype,
+			snprintf(cmd, sizeof(cmd),
+			    "${%s_%c%s}\n\t@${NORMAL_CTFCONVERT}", ftype,
 			    toupper(och),
 			    ftp->f_flags & NOWERROR ? "_NOWERROR" : "");
 			compilewith = cmd;
