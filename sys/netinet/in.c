dd62f5c0e633f2e49c3bec5fbf3b26332a455c2c
Qing Li <qingli@FreeBSD.org>
2010-06-25 21:26:34 +0000

    MFC	r208553
    This patch fixes the problem where proxy ARP entries cannot be added
    over the if_ng interface.
    
    Approved by:	re (bz)

--- sys/netinet/in.c
+++ sys/netinet/in.c
@@ -1379,8 +1379,9 @@ in_lltable_rtcheck(struct ifnet *ifp, u_int flags, const struct sockaddr *l3addr
 
 	/* XXX rtalloc1 should take a const param */
 	rt = rtalloc1(__DECONST(struct sockaddr *, l3addr), 0, 0);
-	if (rt == NULL || (rt->rt_flags & RTF_GATEWAY) || 
-	    ((rt->rt_ifp != ifp) && !(flags & LLE_PUB))) {
+	if (rt == NULL || (!(flags & LLE_PUB) &&
+			   ((rt->rt_flags & RTF_GATEWAY) || 
+			    (rt->rt_ifp != ifp)))) {
 #ifdef DIAGNOSTIC
 		log(LOG_INFO, "IPv4 address: \"%s\" is not on the network\n",
 		    inet_ntoa(((const struct sockaddr_in *)l3addr)->sin_addr));
