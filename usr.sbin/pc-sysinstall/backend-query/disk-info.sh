848c62b81aa879e0af4ec0a28d0fa3541f298193
imp <imp@FreeBSD.org>
2010-06-27 16:46:11 +0000

    Spell iXsystems, Inc properly
    Submitted by:	delphij@

--- usr.sbin/pc-sysinstall/backend-query/disk-info.sh
+++ usr.sbin/pc-sysinstall/backend-query/disk-info.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #-
-# Copyright (c) 2010 iX Systems, Inc.  All rights reserved.
+# Copyright (c) 2010 iXsystems, Inc.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
