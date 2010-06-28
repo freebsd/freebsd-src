48fa7c4b2e36580bb22dded372be56922a692e7f
gavin <gavin@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-06-28 08:30:10 +0000

    Make WARNS=6 safe.
    Tested by:	make universe
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@209567 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/lock/lock.c
+++ usr.bin/lock/lock.c
@@ -65,6 +65,7 @@ __FBSDID("$FreeBSD$");
 #include <ctype.h>
 #include <errno.h>
 #include <pwd.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -257,9 +258,9 @@ hi(int signo __unused)
 		if (no_timeout) {
 			(void)putchar('\n');
 		} else {
-			(void)printf("timeout in %ld:%ld minutes\n",
-			    (nexttime - timval.tv_sec) / 60,
-			    (nexttime - timval.tv_sec) % 60);
+			(void)printf("timeout in %jd:%jd minutes\n",
+			    (intmax_t)(nexttime - timval.tv_sec) / 60,
+			    (intmax_t)(nexttime - timval.tv_sec) % 60);
 		}
 	}
 }
