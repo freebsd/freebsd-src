bfa67d84b6912fed5a838e3b390c54cf126ebfb4
emaste <emaste@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-07-06 19:43:40 +0000

    Use canonical spelling of FreeBSD.org
    Submitted by:	Garrett Cooper
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@209745 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- tools/tools/ncpus/biosmptable.c
+++ tools/tools/ncpus/biosmptable.c
@@ -22,7 +22,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * Author: Ed Maste <emaste@freebsd.org>
+ * Author: Ed Maste <emaste@FreeBSD.org>
  */
 
 /*
