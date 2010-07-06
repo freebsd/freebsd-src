202d71e3bf35c5c58f3c7ce8fc4313bd825e5320
Warner Losh <imp@FreeBSD.org>
2010-07-06 23:29:55 +0000

    A few patches from Ed Maste by way of Kris Moore
    1. Change detect-vmware to detect-emulation
    2. improve laptop detection
    3. better, network descriptions

--- usr.sbin/pc-sysinstall/backend-query/detect-nics.sh
+++ usr.sbin/pc-sysinstall/backend-query/detect-nics.sh
@@ -25,17 +25,12 @@
 #
 # $FreeBSD$
 
-rm /tmp/netCards 2>/dev/null
-touch /tmp/netCards
-
-config="`ifconfig -l`"
-
-for i in $config
-do
- echo "${i}" | grep -e "lo0" -e "^fwe" -e "^fwip" -e "lo1" -e "^plip" -e "^pfsync" -e "^pflog" -e "^tun" >/dev/null 2>/dev/null
- if [ "$?" != "0" ]
- then
-   IDENT="<`dmesg | grep ^${i} | grep -v "miibus" | grep '<' | cut -d '<' -f 2 | cut -d '>' -f 1 | head -1`>"
-   echo "${i}: $IDENT"
- fi
+for i in $(ifconfig -l); do
+  case "${i%%[0-9]*}" in
+  lo|fwe|fwip|plip|pfsync|pflog|tun)
+    continue
+    ;;
+  esac
+  IDENT=$(dmesg | sed -n "s/^$i: <\(.*\)>.*$/\1/p" | head -1)
+  echo "${i}: <$IDENT>"
 done
