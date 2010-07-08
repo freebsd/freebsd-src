b825c1937019b18c8cb15cf6cb14dc3ca15de8f6
kib <kib@FreeBSD.org>
2010-07-08 08:39:02 +0000

    Do not mention VM_ALLOC_RETRY in comment, and normalize the terminology (blocking -> sleeping).
    Reviewed by:	alc
    MFC after:	3 days

--- sys/dev/agp/agp.c
+++ sys/dev/agp/agp.c
@@ -537,8 +537,8 @@ agp_generic_bind_memory(device_t dev, struct agp_memory *mem,
 
 	/*
 	 * Allocate the pages early, before acquiring the lock,
-	 * because vm_page_grab() used with VM_ALLOC_RETRY may
-	 * block and we can't hold a mutex while blocking.
+	 * because vm_page_grab() may sleep and we can't hold a mutex
+	 * while sleeping.
 	 */
 	VM_OBJECT_LOCK(mem->am_obj);
 	for (i = 0; i < mem->am_size; i += PAGE_SIZE) {
