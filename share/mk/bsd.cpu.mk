a7d11955f4f70ff36e79c45fcab7cf071a85679c
nwhitehorn <nwhitehorn@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-07-10 02:32:50 +0000

    Minor modifications to know what to do with powerpc64.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@209868 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/bsd.cpu.mk
+++ share/mk/bsd.cpu.mk
@@ -12,7 +12,7 @@ MACHINE_CPU = i486
 MACHINE_CPU = amd64 sse2 sse mmx
 . elif ${MACHINE_ARCH} == "ia64"
 MACHINE_CPU = itanium
-. elif ${MACHINE_ARCH} == "powerpc"
+. elif ${MACHINE_ARCH} == "powerpc" || ${MACHINE_ARCH} == "powerpc64"
 MACHINE_CPU = aim
 . elif ${MACHINE_ARCH} == "sparc64"
 . elif ${MACHINE_ARCH} == "arm"
@@ -121,6 +121,8 @@ _CPUCFLAGS = -Wa,-me500 -msoft-float
 .  else
 _CPUCFLAGS = -mcpu=${CPUTYPE} -mno-powerpc64
 .  endif
+. elif ${MACHINE_ARCH} == "powerpc64"
+_CPUCFLAGS = -mcpu=${CPUTYPE}
 . elif ${MACHINE_ARCH} == "mips"
 .  if ${CPUTYPE} == "mips32"
 _CPUCFLAGS = -march=mips32
