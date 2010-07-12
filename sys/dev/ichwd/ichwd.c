5db6d33dd829249d219bff025fcdf06e789b3dac
Remko Lodder <remko@FreeBSD.org>
2010-07-12 17:46:19 +0000

    MFC r202917
     Add support for the NM10 chipset.
    
     PR:		kern/143118
     Submitted by:	Koshin sumitani <ksumitani at mui dot biglobe dot ne dot jp>
     Approved by:	imp (mentor, implicit)
     MFC after:	3 days

--- sys/dev/ichwd/ichwd.c
+++ sys/dev/ichwd/ichwd.c
@@ -93,6 +93,7 @@ static struct ichwd_device ichwd_devices[] = {
 	{ DEVICEID_ICH7DH,   "Intel ICH7DH watchdog timer",	7 },
 	{ DEVICEID_ICH7M,    "Intel ICH7M watchdog timer",	7 },
 	{ DEVICEID_ICH7MDH,  "Intel ICH7MDH watchdog timer",	7 },
+	{ DEVICEID_NM10,     "Intel NM10 watchdog timer",	7 },
 	{ DEVICEID_ICH8,     "Intel ICH8 watchdog timer",	8 },
 	{ DEVICEID_ICH8DH,   "Intel ICH8DH watchdog timer",	8 },
 	{ DEVICEID_ICH8DO,   "Intel ICH8DO watchdog timer",	8 },
