f6132fb186ea4a6f776beef68b184b50e7833891
luigi <luigi@FreeBSD.org>
2010-07-15 14:37:59 +0000

    whitespace fixes

--- sys/netinet/ipfw/ip_fw2.c
+++ sys/netinet/ipfw/ip_fw2.c
@@ -351,7 +351,7 @@ iface_match(struct ifnet *ifp, ipfw_insn_if *cmd)
 				return(1);
 		}
 	} else {
-#ifdef	__FreeBSD__	/* and OSX too ? */
+#ifdef __FreeBSD__	/* and OSX too ? */
 		struct ifaddr *ia;
 
 		if_addr_rlock(ifp);
@@ -1329,7 +1329,7 @@ do {								\
 				/* For diverted packets, args->rule.info
 				 * contains the divert port (in host format)
 				 * reason and direction.
-	 			 */
+				 */
 				uint32_t i = args->rule.info;
 				match = (i&IPFW_IS_MASK) == IPFW_IS_DIVERT &&
 				    cmd->arg1 & ((i & IPFW_INFO_IN) ? 1 : 2);
