743f4787969c8798fa01015159600e1ddb3c49fe
jeff <jeff@FreeBSD.org>
2010-07-15 01:13:49 +0000

    - Add a linux compatible radix tree implementation.  This only implements    a subset of the linux features required by ofed drivers.
    Sponsored by:	Isilon Systems, iX Systems, and Panasas.

--- sys/ofed/include/linux/radix-tree.h
+++ sys/ofed/include/linux/radix-tree.h
@@ -29,10 +29,29 @@
 #ifndef	_LINUX_RADIX_TREE_H_
 #define	_LINUX_RADIX_TREE_H_
 
-struct radix_tree_root {
+#define	RADIX_TREE_MAP_SHIFT	6
+#define	RADIX_TREE_MAP_SIZE	(1 << RADIX_TREE_MAP_SHIFT)
+#define	RADIX_TREE_MAP_MASK	(RADIX_TREE_MAP_SIZE - 1)
+#define	RADIX_TREE_MAX_HEIGHT						\
+	    DIV_ROUND_UP((sizeof(long) * NBBY), RADIX_TREE_MAP_SHIFT)
+
+struct radix_tree_node {
+	void		*slots[RADIX_TREE_MAP_SIZE];
+	int		count;
 };
 
-#define	RADIX_TREE_INIT(root)
+struct radix_tree_root {
+	struct radix_tree_node	*rnode;
+	gfp_t			gfp_mask;
+	int			height;
+};
+
+#define	RADIX_TREE_INIT(mask)						\
+	    { .rnode = NULL, .gfp_mask = mask, .height = 0 };
+#define	INIT_RADIX_TREE(root, mask)					\
+	    { (root)->rnode = NULL; (root)->gfp_mask = mask; (root)->height = 0; }
+#define	RADIX_TREE(name, mask)						\
+	    struct radix_tree_root name = RADIX_TREE_INIT(mask)
 
 void	*radix_tree_lookup(struct radix_tree_root *, unsigned long);
 void	*radix_tree_delete(struct radix_tree_root *, unsigned long);
