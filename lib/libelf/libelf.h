9d04749f75f55e421ce49d6c318a407a8b44692d
kaiw <kaiw@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-07-21 12:14:50 +0000

    * Deprecate `elf_getshnum()`, `elf_getphnum()` and `elf_getshstrndx()` in   favour of `elf_getshdrnum()`, `elf_getphdrnum()` and `elf_getshdrstrndx()`   respectively. * Add new manual pages for `elf_getshdrstrndx()`, `elf_getphdrnum()` and   `elf_getshdrnum()`. * Add a deprecation warning for `elf_getshstrndx()`, `elf_getphnum()` and   `elf_getshnum()`.
    Obtained from:	elftoolchain
    MFC after:		1 month
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@210345 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libelf/libelf.h
+++ lib/libelf/libelf.h
@@ -193,10 +193,13 @@ Elf_Arsym	*elf_getarsym(Elf *_elf, size_t *_ptr);
 off_t		elf_getbase(Elf *_elf);
 Elf_Data	*elf_getdata(Elf_Scn *, Elf_Data *);
 char		*elf_getident(Elf *_elf, size_t *_ptr);
-int		elf_getphnum(Elf *_elf, size_t *_dst);
+int		elf_getphdrnum(Elf *_elf, size_t *_dst);
+int		elf_getphnum(Elf *_elf, size_t *_dst);	/* Deprecated */
 Elf_Scn		*elf_getscn(Elf *_elf, size_t _index);
-int		elf_getshnum(Elf *_elf, size_t *_dst);
-int		elf_getshstrndx(Elf *_elf, size_t *_dst);
+int		elf_getshdrnum(Elf *_elf, size_t *_dst);
+int		elf_getshnum(Elf *_elf, size_t *_dst);	/* Deprecated */
+int		elf_getshdrstrndx(Elf *_elf, size_t *_dst);
+int		elf_getshstrndx(Elf *_elf, size_t *_dst); /* Deprecated */
 unsigned long	elf_hash(const char *_name);
 Elf_Kind	elf_kind(Elf *_elf);
 Elf		*elf_memory(char *_image, size_t _size);
