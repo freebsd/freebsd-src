f9081276e93a78bf777a0945439da1739963c795
avg <avg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-07-23 17:07:51 +0000

    completely ignore zero-sized elf sections in modules of elf object type (amd64)
    Current code doesn't check size of elf sections and may perform needless
    actions of zero-sized memory allocation and similar.
    The bigger issue is that alignment requirement of a zero-sized section
    gets effectively applied to the next section if it has smaller alignment
    requirement.  But other tools, like gdb and consequently kgdb,
    completely ignore zero-sized sections and thus may map symbols to
    addresses differently.
    
    Zero-sized sections are not typical in general.
    Their typical (only, even) cause in FreeBSD modules is inline assembly that
    creates custom sections which is found in pcpu.h and vnet.h.  Mere inclusion
    of one of those header files produces a custom section in elf output.
    If there is no actual use for the section in a given module, then the
    section remains empty.
    
    Better solution is to avoid creating zero-sized sections altogether,
    which is in plans.
    
    Preloaded modules are handled in boot code (load_elf_obj.c), while
    dynamically loaded modules are handled by kernel (link_elf_obj.c).
    
    Based on code by:	np
    MFC after:		3 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@210423 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/boot/common/load_elf_obj.c
+++ sys/boot/common/load_elf_obj.c
@@ -221,6 +221,8 @@ __elfN(obj_loadimage)(struct preloaded_file *fp, elf_file_t ef, u_int64_t off)
 	for (i = 0; i < hdr->e_shnum; i++)
 		shdr[i].sh_addr = 0;
 	for (i = 0; i < hdr->e_shnum; i++) {
+		if (shdr[i].sh_size == 0)
+			continue;
 		switch (shdr[i].sh_type) {
 		case SHT_PROGBITS:
 		case SHT_NOBITS:
