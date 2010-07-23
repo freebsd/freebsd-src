2e34a46e3b6315cfb35c4b01a03769c9777f0f1c
jhb <jhb@FreeBSD.org>
2010-07-23 15:32:44 +0000

    MFC 209956: Remove a dead test.  We already exclude NMI traps from this code in an earlier condition.

--- sys/amd64/amd64/trap.c
+++ sys/amd64/amd64/trap.c
@@ -275,9 +275,9 @@ trap(struct trapframe *frame)
 			    type);
 			/*
 			 * We shouldn't enable interrupts while holding a
-			 * spin lock or servicing an NMI.
+			 * spin lock.
 			 */
-			if (type != T_NMI && td->td_md.md_spinlock_count == 0)
+			if (td->td_md.md_spinlock_count == 0)
 				enable_intr();
 		}
 	}
