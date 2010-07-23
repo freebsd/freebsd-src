2b17bd2926d01379bc4dbe7530d4567b77a9e0a3
avg <avg@FreeBSD.org>
2010-07-23 17:44:01 +0000

    zfs arc_memory_throttle: available memory is free + cache
    OpenSolaris freemem has the same meaning as our v_free_count +
    v_cache_count.
    
    Obtained from:	Artem Belevich <fbsdlist@src.cx>,
    		Peter Jeremy <peterjeremy@acm.org>
    Discussed with:	pjd
    MFC after:	2 weeks

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/arc.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/arc.c
@@ -3653,7 +3653,8 @@ static int
 arc_memory_throttle(uint64_t reserve, uint64_t inflight_data, uint64_t txg)
 {
 #ifdef _KERNEL
-	uint64_t available_memory = ptoa((uintmax_t)cnt.v_free_count);
+	uint64_t available_memory = ptoa((uintmax_t)cnt.v_free_count
+	    + cnt.v_cache_count);
 	static uint64_t page_load = 0;
 	static uint64_t last_txg = 0;
 
