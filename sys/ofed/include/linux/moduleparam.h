5023e668d6d05921a78aae9a983e518145518090
jeff <jeff@FreeBSD.org>
2010-07-29 02:16:16 +0000

    - Implement a little more of moduleparam just to avoid compiler warnings.
    Sponsored by:	Isilon Systems, iX Systems, and Panasas.

--- sys/ofed/include/linux/moduleparam.h
+++ sys/ofed/include/linux/moduleparam.h
@@ -70,9 +70,16 @@ struct kparam_array
 	void 		*elem;
 };
 
+static inline void
+param_sysinit(struct kernel_param *param)
+{
+}
+
 #define	module_param_call(name, set, get, arg, perm)			\
-	struct kernel_param __param_##name =				\
-	    { #name, perm, 0, set, get, { arg } }
+	static struct kernel_param __param_##name =			\
+	    { #name, perm, 0, set, get, { arg } };			\
+	SYSINIT(name##_param_sysinit, SI_SUB_DRIVERS, SI_ORDER_FIRST,	\
+	    param_sysinit, &__param_##name);
 
 #define	module_param_named(name, var, type, mode)			\
 	module_param_call(name, param_set_##type, param_get_##type, &var, mode)
