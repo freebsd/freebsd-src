57e198319ab8665a9ada4864b0348dda93aef5cc
adrian <adrian@FreeBSD.org>
2010-07-30 13:58:43 +0000

    Oops, restore the previous delay. This restores functionality - it takes longer that 1ms to do the initial NF calibration.

--- sys/dev/ath/ath_hal/ar5416/ar5416_cal.c
+++ sys/dev/ath/ath_hal/ar5416/ar5416_cal.c
@@ -227,7 +227,7 @@ ar5416InitCal(struct ath_hal *ah, const struct ieee80211_channel *chan)
 	 * Try to make sure the above NF cal completes, just so
 	 * it doesn't clash with subsequent percals -adrian
 	 */
-	if (! ar5416WaitNfComplete(ah, 1000)) {
+	if (! ar5416WaitNfComplete(ah, 10000)) {
 		HALDEBUG(ah, HAL_DEBUG_ANY, "%s: initial NF calibration did "
 		"not complete in time; noisy environment?\n", __func__);
 		return AH_FALSE;
