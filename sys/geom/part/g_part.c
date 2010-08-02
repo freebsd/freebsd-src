1f8688de7b5ab792bf7be9f94eba4acef5cf95d5
ae <ae@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-08-02 10:26:15 +0000

    Release access for consumers that are opened, but will be destroyed indirectly by orphan method.
    PR:		148688
    Silence from:	marcel
    Approved by:	mav (mentor)
    MFC after: 	2 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@210746 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/geom/part/g_part.c
+++ sys/geom/part/g_part.c
@@ -1739,6 +1739,7 @@ static void
 g_part_orphan(struct g_consumer *cp)
 {
 	struct g_provider *pp;
+	struct g_part_table *table;
 
 	pp = cp->provider;
 	KASSERT(pp != NULL, (__func__));
@@ -1746,6 +1747,9 @@ g_part_orphan(struct g_consumer *cp)
 	g_topology_assert();
 
 	KASSERT(pp->error != 0, (__func__));
+	table = cp->geom->softc;
+	if (table->gpt_opened)
+		g_access(cp, -1, -1, -1);
 	g_part_wither(cp->geom, pp->error);
 }
 
