ee04083c8a0239d0a2a6a3d668e132b0516ef19b
David Malone <dwmalone@FreeBSD.org>
2010-08-08 20:34:53 +0000

    Don't pass sizeof(u_int) to an argument of SYSCLT_PROC that ends up not being used.

--- sys/amd64/amd64/tsc.c
+++ sys/amd64/amd64/tsc.c
@@ -222,7 +222,7 @@ sysctl_machdep_tsc_freq(SYSCTL_HANDLER_ARGS)
 }
 
 SYSCTL_PROC(_machdep, OID_AUTO, tsc_freq, CTLTYPE_QUAD | CTLFLAG_RW,
-    0, sizeof(u_int), sysctl_machdep_tsc_freq, "QU", "");
+    0, 0, sysctl_machdep_tsc_freq, "QU", "");
 
 static unsigned
 tsc_get_timecount(struct timecounter *tc)
