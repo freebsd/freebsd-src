e21af379452d5190870e0d7c6244518a8d5202b5
Andriy Gapon <avg@FreeBSD.org>
2010-08-21 18:26:55 +0000

    MFC r210427: zfs arc_memory_throttle: available memory is free + cache

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/arc.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/arc.c
@@ -3297,6 +3297,8 @@ arc_memory_throttle(uint64_t reserve, uint64_t txg)
 #ifdef _KERNEL
 	uint64_t inflight_data = arc_anon->arcs_size;
 	uint64_t available_memory = ptoa((uintmax_t)cnt.v_free_count);
+	uint64_t available_memory = ptoa((uintmax_t)cnt.v_free_count
+	    + cnt.v_cache_count);
 	static uint64_t page_load = 0;
 	static uint64_t last_txg = 0;
 
