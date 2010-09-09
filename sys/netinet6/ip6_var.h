e0b209f8e8424e303b600ba33fb4494fb77e0e95
bz <bz@FreeBSD.org>
2010-09-09 06:44:22 +0000

    MFC r212155:   MFp4 CH=183052 183053 183258:
        In protosw we define pr_protocol as short, while on the wire
        it is an uint8_t.  That way we can have "internal" protocols
        like DIVERT, SEND or gaps for modules (PROTO_SPACER).
        Switch ipproto_{un,}register to accept a short protocol number(*)
        and do an upfront check for valid boundries. With this we
        also consistently report EPROTONOSUPPORT for out of bounds
        protocols, as we did for proto == 0.  This allows a caller
        to not error for this case, which is especially important
        if we want to automatically call these from domain handling.
    
        (*) the functions have been without any in-tree consumer
        since the initial introducation, so this is considered save.
    
        Implement ip6proto_{un,}register() similarly to their legacy IP
        counter parts to allow modules to hook up dynamically.
    
      Reviewed by:  philip, will

--- sys/netinet6/ip6_var.h
+++ sys/netinet6/ip6_var.h
@@ -329,6 +329,9 @@ int	icmp6_ctloutput __P((struct socket *, struct sockopt *sopt));
 
 struct in6_ifaddr;
 void	ip6_init __P((void));
+int	ip6proto_register(short);
+int	ip6proto_unregister(short);
+
 void	ip6_input __P((struct mbuf *));
 struct in6_ifaddr *ip6_getdstifaddr __P((struct mbuf *));
 void	ip6_freepcbopts __P((struct ip6_pktopts *));
