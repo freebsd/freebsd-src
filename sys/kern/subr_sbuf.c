e50b33c8b284d9ead1b532a450873e478b24dde4
mdf <mdf@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-09-13 18:48:23 +0000

    Revert r212370, as it causes a LOR on powerpc.  powerpc does a few unexpected things in copyout(9) and so wiring the user buffer is not sufficient to perform a copyout(9) while holding a random mutex.
    Requested by: nwhitehorn
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@212572 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/subr_sbuf.c
+++ sys/kern/subr_sbuf.c
@@ -303,8 +303,8 @@ sbuf_drain(struct sbuf *s)
 		s->s_error = -len;
 		return (s->s_error);
 	}
-	KASSERT(len > 0 && len <= s->s_len,
-	    ("Bad drain amount %d for sbuf %p", len, s));
+
+	KASSERT(len > 0, ("Drain must either error or work!"));
 	s->s_len -= len;
 	/*
 	 * Fast path for the expected case where all the data was
