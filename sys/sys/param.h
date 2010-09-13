fcc1bc561ff6b4c5bd6e02b2a1d8c7f00cbfd380
rpaulo <rpaulo@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-09-13 17:53:43 +0000

    Bump __FreeBSD_version to reflect the userland DTrace changes.
    Sponsored by:	The FreeBSD Foundation
    > Description of fields to fill in above:                     76 columns --|
    > PR:            If a GNATS PR is affected by the change.
    > Submitted by:  If someone else sent in the change.
    > Reviewed by:   If someone else reviewed your modification.
    > Approved by:   If you needed approval for this commit.
    > Obtained from: If the change is from a third party.
    > MFC after:     N [day[s]|week[s]|month[s]].  Request a reminder email.
    > Security:      Vulnerability reference (one per line) or description.
    > Empty fields above will be automatically removed.
    
    M    param.h
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@212568 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -58,7 +58,7 @@
  *		in the range 5 to 9.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 900020	/* Master, propagated to newvers */
+#define __FreeBSD_version 900021	/* Master, propagated to newvers */
 
 #ifndef LOCORE
 #include <sys/types.h>
