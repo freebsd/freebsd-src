a63ae1d8bde2b7c44ef0ff197b6f45eacc6f06ea
Martin Matuska <mm@FreeBSD.org>
2010-09-15 16:20:24 +0000

    MFC r211932, r211947, r211948:
    MFC r211932:
    
    Import changes from OpenSolaris that provide
    - better ACL caching and speedup of ACL permission checks
    - faster handling of stat()
    - lowered mutex contention in the read/writer lock (rrwlock)
    - several related bugfixes
    
    Detailed information (OpenSolaris onnv changesets and Bug IDs):
    
    9749:105f407a2680
    6802734	Support for Access Based Enumeration (not used on FreeBSD)
    6844861	inconsistent xattr readdir behavior with too-small buffer
    
    9866:ddc5f1d8eb4e
    6848431	zfs with rstchown=0 or file_chown_self privilege allows user to "take" ownership
    
    9981:b4907297e740
    6775100	stat() performance on files on zfs should be improved
    6827779	rrwlock is overly protective of its counters
    
    10143:d2d432dfe597
    6857433	memory leaks found at: zfs_acl_alloc/zfs_acl_node_alloc
    6860318	truncate() on zfsroot succeeds when file has a component of its path set without access permission
    
    10232:f37b85f7e03e
    6865875	zfs sometimes incorrectly giving search access to a dir
    
    10250:b179ceb34b62
    6867395	zpool_upgrade_007_pos testcase panic'd with BAD TRAP: type=e (#pf Page fault)
    
    10269:2788675568fd
    6868276	zfs_rezget() can be hazardous when znode has a cached ACL
    
    10295:f7a18a1e9610
    6870564	panic in zfs_getsecattr
    
    MFC r211947:
    
    Move ZUT_OBJS in the same place that is used in vendor code.
    
    MFC r211948:
    
    Return NULL pointer instead of B_FALSE as it is done in the vendor code.
    
    Approved by:	delphij (mentor)
    Obtained from:	OpenSolaris (multiple Bug IDs)

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_vfsops.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_vfsops.c
@@ -983,6 +983,7 @@ zfs_set_fuid_feature(zfsvfs_t *zfsvfs)
 		vfs_set_feature(zfsvfs->z_vfs, VFSFT_SYSATTR_VIEWS);
 		vfs_set_feature(zfsvfs->z_vfs, VFSFT_ACEMASKONACCESS);
 		vfs_set_feature(zfsvfs->z_vfs, VFSFT_ACLONCREATE);
+		vfs_set_feature(zfsvfs->z_vfs, VFSFT_ACCESS_FILTER);
 	}
 }
 
