8e5e1aa4ad2759536d341a614067b5a73da431d0
marius <marius@FreeBSD.org>
2010-09-15 18:09:15 +0000

    MFC: r210176
    Allocate proper amount of memory for interrupt names on sparc64 and
    sun4v, same as done on other architectures. This removes garbage from
    `vmstat -ia` output.

--- sys/sparc64/sparc64/genassym.c
+++ sys/sparc64/sparc64/genassym.c
@@ -160,6 +160,7 @@ ASSYM(HASH_ENTRY_SHIFT, HASH_ENTRY_SHIFT);
 
 ASSYM(V_INTR, offsetof(struct vmmeter, v_intr));
 
+ASSYM(MAXCOMLEN, MAXCOMLEN);
 ASSYM(PC_CURTHREAD, offsetof(struct pcpu, pc_curthread));
 ASSYM(PC_CURPCB, offsetof(struct pcpu, pc_curpcb));
 ASSYM(PC_CPUID, offsetof(struct pcpu, pc_cpuid));
