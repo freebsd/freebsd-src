0b9b7d544171659b4bc34c4d5dee18888462dd98
andre <andre@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-09-16 21:06:45 +0000

    Remove the TCP inflight bandwidth limiter as announced in r211315 to give way for the pluggable congestion control framework.  It is the task of the congestion control algorithm to set the congestion window and amount of inflight data without external interference.
    In 'struct tcpcb' the variables previously used by the inflight
    limiter are renamed to spares to keep the ABI intact and to have
    some more space for future extensions.
    
    In 'struct tcp_info' the variable 'tcpi_snd_bwnd' is not removed to
    preserve the ABI.  It is always set to 0.
    
    In siftr.c in 'struct pkt_node' the variable 'snd_bwnd' is not removed
    to preserve the ABI.  It is always set to 0.
    
    These unused variable in the various structures may be reused in the
    future or garbage collected before the next release or at some other
    point when an ABI change happens anyway for other reasons.
    
    No MFC is planned.  The inflight bandwidth limiter stays disabled by
    default in the other branches but remains available.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@212765 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/tcp_var.h
+++ sys/netinet/tcp_var.h
@@ -135,12 +135,12 @@ struct tcpcb {
 
 	u_long	snd_wnd;		/* send window */
 	u_long	snd_cwnd;		/* congestion-controlled window */
-	u_long	snd_bwnd;		/* bandwidth-controlled window */
+	u_long	snd_spare1;		/* unused */
 	u_long	snd_ssthresh;		/* snd_cwnd size threshold for
 					 * for slow start exponential to
 					 * linear switch
 					 */
-	u_long	snd_bandwidth;		/* calculated bandwidth or 0 */
+	u_long	snd_spare2;		/* unused */
 	tcp_seq	snd_recover;		/* for use in NewReno Fast Recovery */
 
 	u_int	t_maxopd;		/* mss plus options */
@@ -150,8 +150,8 @@ struct tcpcb {
 	u_int	t_rtttime;		/* RTT measurement start time */
 	tcp_seq	t_rtseq;		/* sequence number being timed */
 
-	u_int	t_bw_rtttime;		/* used for bandwidth calculation */
-	tcp_seq	t_bw_rtseq;		/* used for bandwidth calculation */
+	u_int	t_bw_spare1;		/* unused */
+	tcp_seq	t_bw_spare2;		/* unused */
 
 	int	t_rxtcur;		/* current retransmit value (ticks) */
 	u_int	t_maxseg;		/* maximum segment size */
@@ -654,7 +654,6 @@ void	 tcpip_fillheaders(struct inpcb *, void *, void *);
 void	 tcp_timer_activate(struct tcpcb *, int, u_int);
 int	 tcp_timer_active(struct tcpcb *, int);
 void	 tcp_trace(short, short, struct tcpcb *, void *, struct tcphdr *, int);
-void	 tcp_xmit_bandwidth_limit(struct tcpcb *tp, tcp_seq ack_seq);
 /*
  * All tcp_hc_* functions are IPv4 and IPv6 (via in_conninfo)
  */
