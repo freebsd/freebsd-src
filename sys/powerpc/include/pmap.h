1aae5f0786da58a4fab36da88305e1ceb9c3a70a
nwhitehorn <nwhitehorn@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-09-16 00:22:25 +0000

    Replace the SLB backing store splay tree used on 64-bit PowerPC AIM hardware with a lockless sparse tree design. This marginally improves the performance of PMAP and allows copyin()/copyout() to run without acquiring locks when used on wired mappings.
    Submitted by:	mdf
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@212715 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/powerpc/include/pmap.h
+++ sys/powerpc/include/pmap.h
@@ -86,15 +86,13 @@ struct pmap_md {
 #define	NPMAPS		32768
 #endif /* !defined(NPMAPS) */
 
-struct	slbcontainer;
-
-SPLAY_HEAD(slb_tree, slbcontainer);
+struct	slbtnode;
 
 struct	pmap {
 	struct	mtx	pm_mtx;
 	
     #ifdef __powerpc64__
-	struct slb_tree	pm_slbtree;
+	struct slbtnode	*pm_slb_tree_root;
 	struct slb	*pm_slb;
     #else
 	register_t	pm_sr[16];
@@ -139,12 +137,17 @@ struct	md_page {
  * NB: The PMAP MUST be locked already.
  */
 uint64_t va_to_vsid(pmap_t pm, vm_offset_t va);
-int      va_to_slb_entry(pmap_t pm, vm_offset_t va, struct slb *);
+
+/* Lock-free, non-allocating lookup routines */
+uint64_t kernel_va_to_slbv(vm_offset_t va);
+struct slb *user_va_to_slb_entry(pmap_t pm, vm_offset_t va);
 
 uint64_t allocate_vsid(pmap_t pm, uint64_t esid, int large);
-void     slb_insert(pmap_t pm, struct slb *dst, struct slb *);
-int      vsid_to_esid(pmap_t pm, uint64_t vsid, uint64_t *esid);
-void     free_vsids(pmap_t pm);
+void	free_vsid(pmap_t pm, uint64_t esid, int large);
+void	slb_insert(pmap_t pm, struct slb *dst, struct slb *);
+
+struct slbtnode *slb_alloc_tree(void);
+void     slb_free_tree(pmap_t pm);
 struct slb *slb_alloc_user_cache(void);
 void	slb_free_user_cache(struct slb *);
 
