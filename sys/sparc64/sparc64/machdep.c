e44cee3615f3e33b3e662df4a9055b4286d9aeb5
marius <marius@FreeBSD.org>
2010-09-22 20:03:59 +0000

    MFC: rr212709, r212730
    Add a VIS-based block copy function for SPARC64 V and later, which
    additionally takes advantage of the prefetch cache of these CPUs.
    Unlike the uncommitted US-III version, which provide no measurable
    speedup or even resulted in a slight slowdown on certain CPUs models
    compared to using the US-I version with these, the SPARC64 version
    actually results in a slight improvement.

--- sys/sparc64/sparc64/machdep.c
+++ sys/sparc64/sparc64/machdep.c
@@ -494,7 +494,6 @@ sparc64_init(caddr_t mdp, u_long o1, u_long o2, u_long o3, ofw_vec_t *vec)
 	if (cpu_use_vis) {
 		switch (cpu_impl) {
 		case CPU_IMPL_SPARC64:
-		case CPU_IMPL_SPARC64V:
 		case CPU_IMPL_ULTRASPARCI:
 		case CPU_IMPL_ULTRASPARCII:
 		case CPU_IMPL_ULTRASPARCIIi:
@@ -507,6 +506,10 @@ sparc64_init(caddr_t mdp, u_long o1, u_long o2, u_long o3, ofw_vec_t *vec)
 		case CPU_IMPL_ULTRASPARCIIIip:
 			cpu_block_copy = spitfire_block_copy;
 			cpu_block_zero = spitfire_block_zero;
+			break;
+		case CPU_IMPL_SPARC64V:
+			cpu_block_copy = zeus_block_copy;
+			cpu_block_zero = zeus_block_zero;
 			break;
 		}
 	}
