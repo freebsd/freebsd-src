8633d4febda4e188aa7a1e7c906e84a04ed279f2
pjd <pjd@FreeBSD.org>
2010-09-23 12:02:08 +0000

    Update copyright years.
    MFC after:	1 week

--- sys/geom/eli/g_eli.c
+++ sys/geom/eli/g_eli.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2005-2006 Pawel Jakub Dawidek <pjd@FreeBSD.org>
+ * Copyright (c) 2005-2010 Pawel Jakub Dawidek <pjd@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
