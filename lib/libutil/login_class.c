2b5e877ae0c1f1bab7e9762b048f7e1079182dbb
delphij <delphij@FreeBSD.org>
2010-09-24 23:47:42 +0000

    MFC r211393 (des):
    In setusercontext(), do not apply user settings unless running as the
    user in question (usually but not necessarily because we were called
    with LOGIN_SETUSER).  This plugs a hole where users could raise their
    resource limits and expand their CPU mask.
    
    Approved by:	des

--- lib/libutil/login_class.c
+++ lib/libutil/login_class.c
@@ -525,7 +525,7 @@ setusercontext(login_cap_t *lc, const struct passwd *pwd, uid_t uid, unsigned in
     /*
      * Now, we repeat some of the above for the user's private entries
      */
-    if ((lc = login_getuserclass(pwd)) != NULL) {
+    if (getuid() == uid && (lc = login_getuserclass(pwd)) != NULL) {
 	mymask = setlogincontext(lc, pwd, mymask, flags);
 	login_close(lc);
     }
