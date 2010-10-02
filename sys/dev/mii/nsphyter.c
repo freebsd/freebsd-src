1d5a7127a4cfbe96a7287750155e97726d83ae05
marius <marius@FreeBSD.org>
2010-10-02 18:53:12 +0000

    - In the spirit of previous simplifications factor out the checks for a   different PHY instance being selected and isolation out into the wrappers   around the service methods rather than duplicating them over and over   again (besides, a PHY driver shouldn't need to care about which instance   it actually is). - Centralize the check for the need to isolate a non-zero PHY instance not   supporting isolation in mii_mediachg() and just ignore it rather than   panicing, which should sufficient given that a) things are likely to   just work anyway if one doesn't plug in more than one port at a time and   b) refusing to attach in this case just leaves us in a unknown but most   likely also not exactly correct configuration (besides several drivers   setting MIIF_NOISOLATE didn't care about these anyway, probably due to   setting this flag for no real reason). - Minor fixes like removing unnecessary setting of sc->mii_anegticks,   using sc->mii_anegticks instead of hardcoded values etc.

--- sys/dev/mii/nsphyter.c
+++ sys/dev/mii/nsphyter.c
@@ -138,12 +138,10 @@ nsphyter_attach(device_t dev)
 	mii = ma->mii_data;
 	LIST_INSERT_HEAD(&mii->mii_phys, sc, mii_list);
 
-	sc->mii_inst = mii->mii_instance;
+	sc->mii_inst = mii->mii_instance++;
 	sc->mii_phy = ma->mii_phyno;
 	sc->mii_service = nsphyter_service;
 	sc->mii_pdata = mii;
-
-	mii->mii_instance++;
 
 #define	ADD(m, c)	ifmedia_add(&mii->mii_media, (m), (c), NULL)
 
@@ -178,29 +176,12 @@ nsphyter_attach(device_t dev)
 static int
 nsphyter_service(struct mii_softc *sc, struct mii_data *mii, int cmd)
 {
-	struct ifmedia_entry *ife = mii->mii_media.ifm_cur;
-	int reg;
 
 	switch (cmd) {
 	case MII_POLLSTAT:
-		/*
-		 * If we're not polling our PHY instance, just return.
-		 */
-		if (IFM_INST(ife->ifm_media) != sc->mii_inst)
-			return (0);
 		break;
 
 	case MII_MEDIACHG:
-		/*
-		 * If the media indicates a different PHY instance,
-		 * isolate ourselves.
-		 */
-		if (IFM_INST(ife->ifm_media) != sc->mii_inst) {
-			reg = PHY_READ(sc, MII_BMCR);
-			PHY_WRITE(sc, MII_BMCR, reg | BMCR_ISO);
-			return (0);
-		}
-
 		/*
 		 * If the interface is not up, don't do anything.
 		 */
@@ -211,11 +192,6 @@ nsphyter_service(struct mii_softc *sc, struct mii_data *mii, int cmd)
 		break;
 
 	case MII_TICK:
-		/*
-		 * If we're not currently selected, just return.
-		 */
-		if (IFM_INST(ife->ifm_media) != sc->mii_inst)
-			return (0);
 		if (mii_phy_tick(sc) == EJUSTRETURN)
 			return (0);
 		break;
