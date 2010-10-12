ce4b4be6e1c4b1d03ddf9641e439d41fd83f83dd
yongari <yongari@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-10-12 19:31:25 +0000

    Protect bge(4) from accessing invalid NIC internal memory regions on BCM5906.
    Tested by:	Buganini < buganini <> gmail dot com >
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@213747 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/bge/if_bge.c
+++ sys/dev/bge/if_bge.c
@@ -550,6 +550,10 @@ bge_readmem_ind(struct bge_softc *sc, int off)
 	device_t dev;
 	uint32_t val;
 
+	if (sc->bge_asicrev == BGE_ASICREV_BCM5906 &&
+	    off >= BGE_STATS_BLOCK && off < BGE_SEND_RING_1_TO_4)
+		return (0);
+
 	dev = sc->bge_dev;
 
 	pci_write_config(dev, BGE_PCI_MEMWIN_BASEADDR, off, 4);
@@ -562,6 +566,10 @@ static void
 bge_writemem_ind(struct bge_softc *sc, int off, int val)
 {
 	device_t dev;
+
+	if (sc->bge_asicrev == BGE_ASICREV_BCM5906 &&
+	    off >= BGE_STATS_BLOCK && off < BGE_SEND_RING_1_TO_4)
+		return;
 
 	dev = sc->bge_dev;
 
