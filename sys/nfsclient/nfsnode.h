4fef31b66a66337cf8ba2e9ab8e5615c60271450
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-10-18 19:06:46 +0000

    Do not synchronously start the nfsiod threads at all. The r212506 fixed the issues with file descriptor locks, but the same problems are present for vnode lock/user map lock.
    If the nfs_asyncio() cannot find the free nfsiod, schedule task to
    create new nfsiod and return error. This causes fall back to the
    synchronous i/o for nfs_strategy(), or does not start read at all in
    the case of readahead. The caller that holds vnode and potentially
    user map lock does not wait for kproc_create() to finish, preventing
    the LORs.
    
    The change effectively reverts r203072, because we never hand off the
    request to newly created nfsiod thread anymore.
    
    Reviewed by:	jhb
    Tested by:	jhb, pluknet
    MFC after:	3 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@214026 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/nfsclient/nfsnode.h
+++ sys/nfsclient/nfsnode.h
@@ -165,16 +165,13 @@ struct nfsnode {
 #define NFS_TIMESPEC_COMPARE(T1, T2)	(((T1)->tv_sec != (T2)->tv_sec) || ((T1)->tv_nsec != (T2)->tv_nsec))
 
 /*
- * NFS iod threads can be in one of these three states once spawned.
+ * NFS iod threads can be in one of these two states once spawned.
  * NFSIOD_NOT_AVAILABLE - Cannot be assigned an I/O operation at this time.
  * NFSIOD_AVAILABLE - Available to be assigned an I/O operation.
- * NFSIOD_CREATED_FOR_NFS_ASYNCIO - Newly created for nfs_asyncio() and
- *	will be used by the thread that called nfs_asyncio().
  */
 enum nfsiod_state {
 	NFSIOD_NOT_AVAILABLE = 0,
 	NFSIOD_AVAILABLE = 1,
-	NFSIOD_CREATED_FOR_NFS_ASYNCIO = 2,
 };
 
 /*
