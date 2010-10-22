b927df46b1ceac0f03f3a6afea964fa004d799f4
Dimitry Andric <dim@FreeBSD.org>
2010-10-22 20:43:37 +0000

    In case of powerpc64, enable HAVE_LD_NO_DOT_SYMS in gcc's auto-host.h.
    Requested by:	nwhitehorn

--- gnu/usr.bin/cc/cc_tools/auto-host.h
+++ gnu/usr.bin/cc/cc_tools/auto-host.h
@@ -891,7 +891,9 @@
 
 /* Define if your PowerPC64 linker only needs function descriptor syms. */
 #ifndef USED_FOR_TARGET
-/* #undef HAVE_LD_NO_DOT_SYMS */
+# ifdef __powerpc64_
+#  define HAVE_LD_NO_DOT_SYMS 1
+# endif
 #endif
 
 
