eb787525b9958809cc6de862e49d154e1976ffa7
mdf <mdf@FreeBSD.org>
2010-10-22 16:00:45 +0000

    MFC r214062:
    uma_zfree(zone, NULL) should do nothing, to match free(9).
    
    Noticed by:	Ron Steinke <rsteinke at isilon dot com>

--- sys/vm/uma_core.c
+++ sys/vm/uma_core.c
@@ -2250,6 +2250,10 @@ uma_zfree_arg(uma_zone_t zone, void *item, void *udata)
 	CTR2(KTR_UMA, "uma_zfree_arg thread %x zone %s", curthread,
 	    zone->uz_name);
 
+        /* uma_zfree(..., NULL) does nothing, to match free(9). */
+        if (item == NULL)
+                return;
+
 	if (zone->uz_dtor)
 		zone->uz_dtor(item, keg->uk_size, udata);
 #ifdef INVARIANTS
