7dd89dc3ab3c2433a20d5eb4dd832336001e9122
yongari <yongari@FreeBSD.org>
2010-10-23 21:25:50 +0000

    Apply the same workaround for SDI flow control used on BCM5906 A1 to BCM6906 A0/A2. This should fix a long standing BCM5906 A2 lockup issues. Data sheet explicitly mentions BCM5906 A0, A1 and A2 use de-pipelined mode on these revisions. Special thanks to Buganini who tried all combinations of experimental patches for more than 10 days.
    Tested by:	Buganini <buganini <> gmail dot com >

--- sys/dev/bge/if_bge.c
+++ sys/dev/bge/if_bge.c
@@ -1693,11 +1693,14 @@ bge_blockinit(struct bge_softc *sc)
 		bge_writembx(sc, BGE_MBX_RX_MINI_PROD_LO, 0);
 	}
 
-	/* Choose de-pipeline mode for BCM5906 A1. */
-	if (sc->bge_asicrev == BGE_ASICREV_BCM5906 &&
-	    sc->bge_chiprev == BGE_CHIPID_BCM5906_A1)
-		CSR_WRITE_4(sc, BGE_ISO_PKT_TX,
-		    (CSR_READ_4(sc, BGE_ISO_PKT_TX) & ~3) | 2);
+	/* Choose de-pipeline mode for BCM5906 A0, A1 and A2. */
+	if (sc->bge_asicrev == BGE_ASICREV_BCM5906) {
+		if (sc->bge_chiprev == BGE_CHIPID_BCM5906_A0 ||
+		    sc->bge_chiprev == BGE_CHIPID_BCM5906_A1 ||
+		    sc->bge_chiprev == BGE_CHIPID_BCM5906_A2)
+			CSR_WRITE_4(sc, BGE_ISO_PKT_TX,
+			    (CSR_READ_4(sc, BGE_ISO_PKT_TX) & ~3) | 2);
+	}
 	/*
 	 * The BD ring replenish thresholds control how often the
 	 * hardware fetches new BD's from the producer rings in host
