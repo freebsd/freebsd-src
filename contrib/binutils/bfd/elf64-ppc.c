b3a60f22931b262458ab7c5fc74346816309d999
nwhitehorn <nwhitehorn@FreeBSD.org>
2010-10-25 21:49:59 +0000

    Fix a manifestation of GCC bug 16625 that caused ld >= 2.17 to fail on 64-bit PowerPC when linking multiple C++ files referencing the same method, defined in a common header, when that method had a switch statement with more than 4 cases. This change fixes compilation of LLVM tblgen on 64-bit PPC with binutils 2.17.
    Lots of help from:	dim
    Upstream after:		more testing

--- contrib/binutils/bfd/elf64-ppc.c
+++ contrib/binutils/bfd/elf64-ppc.c
@@ -9640,6 +9640,9 @@ ppc64_elf_action_discarded (asection *sec)
   if (strcmp (".toc1", sec->name) == 0)
     return 0;
 
+  if (strcmp (".got", sec->name) == 0)
+    return 0;
+
   return _bfd_elf_default_action_discarded (sec);
 }
 
