c7cb644d8d59e9304f86cb964bdc6d235b4ce26e
kib <kib@FreeBSD.org>
2010-11-03 21:51:05 +0000

    Bump __FreeBSD_version for struct sysentvec changes in r214755.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -58,7 +58,7 @@
  *		in the range 5 to 9.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 801500	/* Master, propagated to newvers */
+#define __FreeBSD_version 801501	/* Master, propagated to newvers */
 
 #ifndef LOCORE
 #include <sys/types.h>
