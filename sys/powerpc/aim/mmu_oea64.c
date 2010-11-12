16bfd6f3478c91e3feb9ae952e7e8dc5d5316539
Nathan Whitehorn <nwhitehorn@FreeBSD.org>
2010-11-12 04:22:00 +0000

    Remove or conditionalize some hypervisor-unfriendly instruction sequences.

--- sys/powerpc/aim/mmu_oea64.c
+++ sys/powerpc/aim/mmu_oea64.c
@@ -1136,10 +1136,6 @@ moea64_bootstrap(mmu_t mmup, vm_offset_t kernelstart, vm_offset_t kernelend)
 		moea64_add_ofw_mappings(mmup, mmu, sz);
 	}
 
-#ifdef SMP
-	TLBSYNC();
-#endif
-
 	/*
 	 * Calculate the last available physical address.
 	 */
