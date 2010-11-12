1205be293db1007b2a1e05e88ac183908ea8b355
tuexen <tuexen@FreeBSD.org>
2010-11-12 20:46:33 +0000

    Don't print an empty line when printing mapping arrays.
    MFC after: 3 days.

--- sys/netinet/sctputil.c
+++ sys/netinet/sctputil.c
@@ -1212,8 +1212,6 @@ sctp_print_mapping_array(struct sctp_association *asoc)
 	printf("Renegable mapping array (last %d entries are zero):\n", asoc->mapping_array_size - limit);
 	for (i = 0; i < limit; i++) {
 		printf("%2.2x%c", asoc->mapping_array[i], ((i + 1) % 16) ? ' ' : '\n');
-		if (((i + 1) % 16) == 0)
-			printf("\n");
 	}
 	if (limit % 16)
 		printf("\n");
