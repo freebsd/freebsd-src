31c6a0037e1a075e397e396a53a3a0822c234122
Dimitry Andric <dim@FreeBSD.org>
2010-11-14 20:38:11 +0000

    Apply the STATIC_VNET_DEFINE and STATIC_DPCPU_DEFINE macros throughout the tree.

--- sys/net/if_ethersubr.c
+++ sys/net/if_ethersubr.c
@@ -144,7 +144,7 @@ MALLOC_DEFINE(M_ARPCOM, "arpcom", "802.* interface internals");
 #if defined(INET) || defined(INET6)
 int
 ether_ipfw_chk(struct mbuf **m0, struct ifnet *dst, int shared);
-static VNET_DEFINE(int, ether_ipfw);
+STATIC_VNET_DEFINE(int, ether_ipfw);
 #define	V_ether_ipfw	VNET(ether_ipfw)
 #endif
 
