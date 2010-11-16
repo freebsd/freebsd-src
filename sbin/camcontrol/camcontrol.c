8d49d2884da4cb69a5f482859812ee93a37b4311
Rebecca Cran <brucec@FreeBSD.org>
2010-11-16 07:51:35 +0000

    MFC r214073:
    Stop disallowing device nodes to be passed to camcontrol(8) since libcam
    already allows both device names and nodes to be specified.

--- sbin/camcontrol/camcontrol.c
+++ sbin/camcontrol/camcontrol.c
@@ -4607,15 +4607,7 @@ main(int argc, char **argv)
 		char name[30];
 		int rv;
 
-		/*
-		 * First catch people who try to do things like:
-		 * camcontrol tur /dev/da0 
-		 * camcontrol doesn't take device nodes as arguments.
-		 */
-		if (argv[2][0] == '/') {
-			warnx("%s is not a valid device identifier", argv[2]);
-			errx(1, "please read the camcontrol(8) man page");
-		} else if (isdigit(argv[2][0])) {
+		if (isdigit(argv[2][0])) {
 			/* device specified as bus:target[:lun] */
 			rv = parse_btl(argv[2], &bus, &target, &lun, &arglist);
 			if (rv < 2)
