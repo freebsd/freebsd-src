f00754129a2467bab22c07a9cd9bd3dbad895834
avg <avg@FreeBSD.org>
2010-11-19 15:07:36 +0000

    specialreg.h: add definitions for MPERF/APERF pair of MSRs
    These MSRs can be used to determine actual (average) performance as
    compared to a maximum defined performance.
    Availability of these MSRs is indicated by bit0 in CPUID.6.ECX on both
    Intel and AMD processors.
    
    MFC after:	5 days

--- sys/i386/include/specialreg.h
+++ sys/i386/include/specialreg.h
@@ -246,6 +246,8 @@
 #define	MSR_BIOS_SIGN		0x08b
 #define	MSR_PERFCTR0		0x0c1
 #define	MSR_PERFCTR1		0x0c2
+#define	MSR_MPERF		0x0e7
+#define	MSR_APERF		0x0e8
 #define	MSR_IA32_EXT_CONFIG	0x0ee	/* Undocumented. Core Solo/Duo only */
 #define	MSR_MTRRcap		0x0fe
 #define	MSR_BBL_CR_ADDR		0x116
