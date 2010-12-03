58271980a41741628d4ea1c4925c6df13b9c01ea
jkim <jkim@FreeBSD.org>
2010-12-03 22:27:51 +0000

    Revert r216161.  It is not necessary because we zero-fill BSS anyway.
    Requested by:	jhb

--- sys/amd64/amd64/tsc.c
+++ sys/amd64/amd64/tsc.c
@@ -46,7 +46,7 @@ __FBSDID("$FreeBSD$");
 
 #include "cpufreq_if.h"
 
-uint64_t	tsc_freq = 0;
+uint64_t	tsc_freq;
 int		tsc_is_broken;
 int		tsc_is_invariant;
 static eventhandler_tag tsc_levels_tag, tsc_pre_tag, tsc_post_tag;
