cbc03d7836d367a26880c77736410bd4af76ebe4
jchandra <jchandra@FreeBSD.org>
2010-12-03 19:22:18 +0000

    1. Fix off by one errors in calls to MIPS_DIRECT_MAPPABLE, reported by alc@ 2. Remove unnecessary #defines from vmparam.h
    Submitted by:	alc (2)
    Reviewed by:	alc (1)

--- sys/mips/include/vmparam.h
+++ sys/mips/include/vmparam.h
@@ -46,11 +46,6 @@
 /*
  * Machine dependent constants mips processors.
  */
-/*
- * USRTEXT is the start of the user text/data space, while USRSTACK
- * is the top (end) of the user stack.
- */
-#define	USRTEXT		(1*PAGE_SIZE)
 
 /*
  * Virtual memory related constants, all in bytes
@@ -94,7 +89,6 @@
 #define	VM_MAX_ADDRESS		((vm_offset_t)(intptr_t)(int32_t)0xffffffff)
 
 #define	VM_MINUSER_ADDRESS	((vm_offset_t)0x00000000)
-#define	VM_MAX_MMAP_ADDR	VM_MAXUSER_ADDRESS
 
 #ifdef __mips_n64
 #define	VM_MAXUSER_ADDRESS	(VM_MINUSER_ADDRESS + (NPDEPG * NBSEG))
