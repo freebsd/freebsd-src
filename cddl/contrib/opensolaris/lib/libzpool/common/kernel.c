bd2682b87020f7db448b2c2497777b5e8c74c347
Martin Matuska <mm@FreeBSD.org>
2010-12-14 15:33:59 +0000

    MFC r210044, r214622:
    MFC r210044 (pjd):
    MFp4 180933:
    
    Initialize rw_count properly so that zdb(8) doesn't trigger assertion in
    rw_enter():
    
    	ASSERT(rwlp->rw_count == 0);
    
    While here, assert that rw_count is 0 when destroying the lock.
    
    MFC r214622 (pjd):
    1. Remove invalid assertion.
    2. Properly recalculate delta in case pthread_cond_timedwait() is interrupted.
    3. Style fix.
    
    Reported by:	[1] App Deb <appdebgr@gmail.com>
    
    Approved by:	re (kib), pjd, delphij (mentor)

--- cddl/contrib/opensolaris/lib/libzpool/common/kernel.c
+++ cddl/contrib/opensolaris/lib/libzpool/common/kernel.c
@@ -174,11 +174,13 @@ rw_init(krwlock_t *rwlp, char *name, int type, void *arg)
 	rwlock_init(&rwlp->rw_lock, USYNC_THREAD, NULL);
 	rwlp->rw_owner = NULL;
 	rwlp->initialized = B_TRUE;
+	rwlp->rw_count = 0;
 }
 
 void
 rw_destroy(krwlock_t *rwlp)
 {
+	ASSERT(rwlp->rw_count == 0);
 	rwlock_destroy(&rwlp->rw_lock);
 	rwlp->rw_owner = (void *)-1UL;
 	rwlp->initialized = B_FALSE;
@@ -306,9 +308,9 @@ cv_timedwait(kcondvar_t *cv, kmutex_t *mp, clock_t abstime)
 	struct timeval tv;
 	clock_t delta;
 
-	ASSERT(abstime > 0);
+	abstime += lbolt;
 top:
-	delta = abstime;
+	delta = abstime - lbolt;
 	if (delta <= 0)
 		return (-1);
 
@@ -319,7 +321,7 @@ top:
 	ts.tv_nsec = tv.tv_usec * 1000 + (delta % hz) * (NANOSEC / hz);
 	ASSERT(ts.tv_nsec >= 0);
 
-	if(ts.tv_nsec >= NANOSEC) {
+	if (ts.tv_nsec >= NANOSEC) {
 		ts.tv_sec++;
 		ts.tv_nsec -= NANOSEC;
 	}
