437ad27f9c81f522301de0affe1fc5fef37d8828
davidxu <davidxu@FreeBSD.org>
2010-12-22 05:01:52 +0000

    MFp4:
    - Add flags CVWAIT_ABSTIME and CVWAIT_CLOCKID for umtx kernel based
      condition variable, this should eliminate an extra system call to get
      current time.
    
    - Add sub-function UMTX_OP_NWAKE_PRIVATE to wake up N channels in single
      system call. Create userland sleep queue for condition variable, in most
      cases, thread will wait in the queue, the pthread_cond_signal will defer
      thread wakeup until the mutex is unlocked, it tries to avoid an extra
      system call and a extra context switch in time window of pthread_cond_signal
      and pthread_mutex_unlock.
    
    The changes are part of process-shared mutex project.

--- lib/libthr/thread/thr_init.c
+++ lib/libthr/thread/thr_init.c
@@ -444,6 +444,8 @@ init_private(void)
 	_thr_once_init();
 	_thr_spinlock_init();
 	_thr_list_init();
+	_thr_wake_addr_init();
+	_sleepq_init();
 
 	/*
 	 * Avoid reinitializing some things if they don't need to be,
