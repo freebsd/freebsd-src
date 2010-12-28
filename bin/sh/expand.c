acd7984f961a69d9e6d6d908f85737d2a9a08cb5
Jilles Tjoelker <jilles@FreeBSD.org>
2010-12-28 21:27:08 +0000

    sh: Don't do optimized command substitution if expansions have side effects.
    Before considering to execute a command substitution in the same process,
    check if any of the expansions may have a side effect; if so, execute it in
    a new process just like happens if it is not a single simple command.
    
    Although the check happens at run time, it is a static check that does not
    depend on current state. It is triggered by:
    - expanding $! (which may cause the job to be remembered)
    - ${var=value} default value assignment
    - assignment operators in arithmetic
    - parameter substitutions in arithmetic except ${#param}, $$, $# and $?
    - command substitutions in arithmetic
    
    This means that $((v+1)) does not prevent optimized command substitution,
    whereas $(($v+1)) does, because $v might expand to something containing
    assignment operators.
    
    Scripts should not depend on these exact details for correctness. It is also
    imaginable to have the shell fork if and when a side effect is encountered
    or to create a new temporary namespace for variables.
    
    Due to the $! change, the construct $(jobs $!) no longer works. The value of
    $! should be stored in a variable outside command substitution first.

--- bin/sh/expand.c
+++ bin/sh/expand.c
@@ -1570,6 +1570,78 @@ cvtnum(int num, char *buf)
 }
 
 /*
+ * Check statically if expanding a string may have side effects.
+ */
+int
+expandhassideeffects(const char *p)
+{
+	int c;
+	int arinest;
+
+	arinest = 0;
+	while ((c = *p++) != '\0') {
+		switch (c) {
+		case CTLESC:
+			p++;
+			break;
+		case CTLVAR:
+			c = *p++;
+			/* Expanding $! sets the job to remembered. */
+			if (*p == '!')
+				return 1;
+			if ((c & VSTYPE) == VSASSIGN)
+				return 1;
+			/*
+			 * If we are in arithmetic, the parameter may contain
+			 * '=' which may cause side effects. Exceptions are
+			 * the length of a parameter and $$, $# and $? which
+			 * are always numeric.
+			 */
+			if ((c & VSTYPE) == VSLENGTH) {
+				while (*p != '=')
+					p++;
+				p++;
+				break;
+			}
+			if ((*p == '$' || *p == '#' || *p == '?') &&
+			    p[1] == '=') {
+				p += 2;
+				break;
+			}
+			if (arinest > 0)
+				return 1;
+			break;
+		case CTLBACKQ:
+		case CTLBACKQ | CTLQUOTE:
+			if (arinest > 0)
+				return 1;
+			break;
+		case CTLARI:
+			arinest++;
+			break;
+		case CTLENDARI:
+			arinest--;
+			break;
+		case '=':
+			if (*p == '=') {
+				/* Allow '==' operator. */
+				p++;
+				continue;
+			}
+			if (arinest > 0)
+				return 1;
+			break;
+		case '!': case '<': case '>':
+			/* Allow '!=', '<=', '>=' operators. */
+			if (*p == '=')
+				p++;
+			break;
+		}
+	}
+	return 0;
+}
+
+/*
  * Do most of the work for wordexp(3).
  */
 
