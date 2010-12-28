8219215297e47749b79ed67b014c7f50fb7e898f
John Baldwin <jhb@FreeBSD.org>
2010-12-28 19:26:57 +0000

    MFC 199805,203532,204329,210063,210065: - Change gcore in order to get rid of the procfs accesses and use FreeBSD's   specific sysctls and ptrace interfaces.   This change switches a bit gcore POLA that is summarized here:   - now gcore can recognize threads within the process and handle dumps     on thread-scope   - the process to be analyzed will be stopped during its gcore run   - gcore may not work with processes which are actively being analyzed     by gdb or truss   - the ptrace interface may cause syscalls to return EINTR, thus     interferring with signals handling within the process - Fix gcore so that it can have the '-s' flag without hanging. - Fix the way the segments are included in the gcore outputs (with the   default invokation):   - Right now if segments are not writable are not included. Remove this.   - Right now if a segment is mapped with NOCORE the check is not honoured.     Change this by checking the newly added flag, from libutil,     KVME_FLAG_NOCOREDUMP.
      Besides that, add a new flag (-f) that forces a 'full' dump of all the
      segments excluding just the malformed ones. This might be used very
      carefully as, among the reported segments, there could be memory
      mapped areas that could be vital to program execution.

--- usr.bin/gcore/gcore.c
+++ usr.bin/gcore/gcore.c
@@ -61,19 +61,19 @@ __FBSDID("$FreeBSD$");
 #include <sys/time.h>
 #include <sys/stat.h>
 #include <sys/linker_set.h>
+#include <sys/sysctl.h>
 
 #include <err.h>
 #include <fcntl.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #include "extern.h"
+int pflags;
 
 static void	killed(int);
-static void	restart_target(void);
 static void	usage(void) __dead2;
 
 static pid_t pid;
@@ -83,20 +83,24 @@ SET_DECLARE(dumpset, struct dumpers);
 int
 main(int argc, char *argv[])
 {
-	int ch, efd, fd, sflag;
+	int ch, efd, fd, name[4];
 	char *binfile, *corefile;
-	char fname[MAXPATHLEN];
+	char passpath[MAXPATHLEN], fname[MAXPATHLEN];
 	struct dumpers **d, *dumper;
+	size_t len;
 
-	sflag = 0;
+	pflags = 0;
 	corefile = NULL;
-        while ((ch = getopt(argc, argv, "c:s")) != -1) {
+        while ((ch = getopt(argc, argv, "c:fs")) != -1) {
                 switch (ch) {
                 case 'c':
 			corefile = optarg;
                         break;
+		case 'f':
+			pflags |= PFLAGS_FULL;
+			break;
 		case 's':
-			sflag = 1;
+			pflags |= PFLAGS_RESUME;
 			break;
 		default:
 			usage();
@@ -109,9 +113,14 @@ main(int argc, char *argv[])
 	switch (argc) {
 	case 1:
 		pid = atoi(argv[0]);
-		asprintf(&binfile, "/proc/%d/file", pid);
-		if (binfile == NULL)
-			errx(1, "allocation failure");
+		name[0] = CTL_KERN;
+		name[1] = KERN_PROC;
+		name[2] = KERN_PROC_PATHNAME;
+		name[3] = pid;
+		len = sizeof(passpath);
+		if (sysctl(name, 4, passpath, &len, NULL, 0) == -1)
+			errx(1, "kern.proc.pathname failure");
+		binfile = passpath;
 		break;
 	case 2:
 		pid = atoi(argv[1]);
@@ -141,34 +150,26 @@ main(int argc, char *argv[])
 	fd = open(corefile, O_RDWR|O_CREAT|O_TRUNC, DEFFILEMODE);
 	if (fd < 0)
 		err(1, "%s", corefile);
-	if (sflag) {
-		signal(SIGHUP, killed);
-		signal(SIGINT, killed);
-		signal(SIGTERM, killed);
-		if (kill(pid, SIGSTOP) == -1)
-			err(1, "%d: stop signal", pid);
-		atexit(restart_target);
-	}
+	/*
+	 * The semantics of the 's' flag is to stop the target process.
+	 * Previous versions of gcore would manage this by trapping SIGHUP,
+	 * SIGINT and SIGTERM (to be passed to the target pid), and then
+	 * signal the child to stop.
+	 *
+	 * However, this messes up if the selected dumper uses ptrace calls
+	 * that leave the child already stopped. The waitpid call in elfcore
+	 * never returns.
+	 *
+	 * The best thing to do here is to externalize the 's' flag and let
+	 * each dumper dispose of what that means, if anything. For the elfcore
+	 * dumper, the 's' flag is a no-op since the ptrace attach stops the
+	 * process in question already.
+	 */
+
 	dumper->dump(efd, fd, pid);
 	(void)close(fd);
 	(void)close(efd);
 	exit(0);
-}
-
-static void
-killed(int sig)
-{
-
-	restart_target();
-	signal(sig, SIG_DFL);
-	kill(getpid(), sig);
-}
-
-static void
-restart_target(void)
-{
-
-	kill(pid, SIGCONT);
 }
 
 void
