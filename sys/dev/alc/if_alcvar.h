f77b5244ce75ab47db65c4df17ce179d9253896d
jhb <jhb@FreeBSD.org>
2011-01-13 21:49:14 +0000

    - Move ether_ifdetach() earlier and remove now-unneeded IN_DETACH flag. - Expand locking in interrupt handler.
    Reviewed by:	yongari

--- sys/dev/alc/if_alcvar.h
+++ sys/dev/alc/if_alcvar.h
@@ -230,7 +230,6 @@ struct alc_softc {
 #define	ALC_FLAG_L0S		0x0400
 #define	ALC_FLAG_L1S		0x0800
 #define	ALC_FLAG_APS		0x1000
-#define	ALC_FLAG_DETACH		0x4000
 #define	ALC_FLAG_LINK		0x8000
 
 	struct callout		alc_tick_ch;
