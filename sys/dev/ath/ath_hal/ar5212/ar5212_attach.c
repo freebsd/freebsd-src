88117a532d5d9edb88c2c107dcc9183806dcf39f
Adrian Chadd <adrian@FreeBSD.org>
2011-01-20 07:56:09 +0000

    Include the initial support for external EEPROMs.
    The AR9100 at least doesn't have an external serial EEPROM
    attached to the MAC; it instead stores the calibration data
    in the normal system flash.
    
    I believe earlier parts can do something similar but I haven't
    experienced it first-hand.
    
    This commit introduces an eepromdata pointer into the API but
    doesn't at all commit to using it. A future commit will
    include the glue needed to allow the AR9100 support code
    to use this data pointer as the EEPROM.

--- sys/dev/ath/ath_hal/ar5212/ar5212_attach.c
+++ sys/dev/ath/ath_hal/ar5212/ar5212_attach.c
@@ -295,7 +295,8 @@ ar5212IsMacSupported(uint8_t macVersion, uint8_t macRev)
  */
 static struct ath_hal *
 ar5212Attach(uint16_t devid, HAL_SOFTC sc,
-	HAL_BUS_TAG st, HAL_BUS_HANDLE sh, HAL_STATUS *status)
+	HAL_BUS_TAG st, HAL_BUS_HANDLE sh, uint16_t *eepromdata,
+	HAL_STATUS *status)
 {
 #define	AH_EEPROM_PROTECT(ah) \
 	(AH_PRIVATE(ah)->ah_ispcie)? AR_EEPROM_PROTECT_PCIE : AR_EEPROM_PROTECT)
