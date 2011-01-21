8f17586af50a21b24e79d602840ac8fb3c3e7bce
jkim <jkim@FreeBSD.org>
2011-01-21 18:31:57 +0000

    MFC:	r217424
    Remove redundant, bogus, and even harmful uses of setting TS bit in CR0.
    It is done from fpstate_drop() when it is really necessary.

--- sys/amd64/ia32/ia32_signal.c
+++ sys/amd64/ia32/ia32_signal.c
@@ -741,7 +741,6 @@ ia32_setregs(td, entry, stack, ps_strings)
 	regs->tf_gs = _ugssel;
 	regs->tf_flags = TF_HASSEGS;
 
-	load_cr0(rcr0() | CR0_MP | CR0_TS);
 	fpstate_drop(td);
 
 	/* Return via doreti so that we can change to a different %cs */
