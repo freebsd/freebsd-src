e4de498f21d5243080eba96cb11e0105bfddae20
John Baldwin <jhb@FreeBSD.org>
2011-01-27 21:21:09 +0000

    MFC 217078,217079: Various priority fixes for creating new threads: - Move sched_fork() later in fork() after the various sections of the new   thread and proc have been copied and zeroed from the old thread and   proc.  Otherwise attempts to modify thread or process data in sched_fork()   could be undone. - Don't copy td_{base,}_user_pri from the old thread to the new thread in   sched_fork_thread() in ULE.  This is already done courtesy the bcopy()   of the thread copy region. - Always initialize the real priority (td_priority) of new threads to the   new thread's base priority (td_base_pri) to avoid bogusly inheriting a   borrowed priority from the parent thread. - Properly initialize the base priority (td_base_pri) of thread0 to PVM   to match the desired priority in td_priority.  Otherwise the first time   thread0 used a borrowed priority it would drop down to PUSER instead of   PVM. - Explicitly initialize the starting priority of new kprocs to PVM to   avoid inheriting some random priority from thread0.

--- sys/kern/kern_fork.c
+++ sys/kern/kern_fork.c
@@ -419,12 +419,6 @@ again:
 	p2 = newproc;
 	p2->p_state = PRS_NEW;		/* protect against others */
 	p2->p_pid = trypid;
-	/*
-	 * Allow the scheduler to initialize the child.
-	 */
-	thread_lock(td);
-	sched_fork(td, td2);
-	thread_unlock(td);
 	AUDIT_ARG(pid, p2->p_pid);
 	LIST_INSERT_HEAD(&allproc, p2, p_list);
 	LIST_INSERT_HEAD(PIDHASH(p2->p_pid), p2, p_hash);
@@ -512,6 +506,13 @@ again:
 	td2->td_sigstk = td->td_sigstk;
 	td2->td_sigmask = td->td_sigmask;
 	td2->td_flags = TDF_INMEM;
+
+	/*
+	 * Allow the scheduler to initialize the child.
+	 */
+	thread_lock(td);
+	sched_fork(td, td2);
+	thread_unlock(td);
 
 	/*
 	 * Duplicate sub-structures as needed.
