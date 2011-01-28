1c569578be289f33c7a03a91a1967b84f73e73c6
pjd <pjd@FreeBSD.org>
2011-01-28 21:51:40 +0000

    Add comments to places where we treat errors as ciritical, but it is possible to handle them more gracefully.
    MFC after:	1 week

--- sbin/hastd/primary.c
+++ sbin/hastd/primary.c
@@ -796,6 +796,7 @@ hastd_primary(struct hast_resource *res)
 	 * Create communication channel between parent and child.
 	 */
 	if (proto_client("socketpair://", &res->hr_ctrl) < 0) {
+		/* TODO: There's no need for this to be fatal error. */
 		KEEP_ERRNO((void)pidfile_remove(pfh));
 		pjdlog_exit(EX_OSERR,
 		    "Unable to create control sockets between parent and child");
@@ -804,6 +805,7 @@ hastd_primary(struct hast_resource *res)
 	 * Create communication channel between child and parent.
 	 */
 	if (proto_client("socketpair://", &res->hr_event) < 0) {
+		/* TODO: There's no need for this to be fatal error. */
 		KEEP_ERRNO((void)pidfile_remove(pfh));
 		pjdlog_exit(EX_OSERR,
 		    "Unable to create event sockets between child and parent");
@@ -811,6 +813,7 @@ hastd_primary(struct hast_resource *res)
 
 	pid = fork();
 	if (pid < 0) {
+		/* TODO: There's no need for this to be fatal error. */
 		KEEP_ERRNO((void)pidfile_remove(pfh));
 		pjdlog_exit(EX_TEMPFAIL, "Unable to fork");
 	}
