579fd4b2ffde98bf5db8eea4296c677fb61cb586
Pawel Jakub Dawidek <pjd@FreeBSD.org>
2011-01-28 21:56:47 +0000

    Add function to assert that the only descriptors we have open are the ones we expect to be open. Also assert that they point at expected type.
    Because openlog(3) API is unable to tell us descriptor number it is using, we
    have to close syslog socket, remember assert message in local buffer and if we
    fail on assertion, reopen syslog socket and log the message.
    
    MFC after:	1 week

--- sbin/hastd/hastd.h
+++ sbin/hastd/hastd.h
@@ -44,6 +44,7 @@ extern bool sigexit_received;
 extern struct pidfh *pfh;
 
 void descriptors_cleanup(struct hast_resource *res);
+void descriptors_assert(const struct hast_resource *res, int pjdlogmode);
 
 void hastd_primary(struct hast_resource *res);
 void hastd_secondary(struct hast_resource *res, struct nv *nvin);
