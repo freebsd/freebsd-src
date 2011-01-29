16700c0fc292f9cfa422807008c4b8a259567dc3
adrian <adrian@FreeBSD.org>
2011-01-29 14:27:20 +0000

    Bring over some NF calibration changes from ath9k.
    Each different radio chipset has a different "good" range of CCA
    (clear channel access) parameters where, if you write something
    out of range, it's possible the radio will go deaf.
    
    Also, since apparently occasionally reading the NF calibration
    returns "wrong" values, so enforce those limits on what is being
    written into the CCA register.
    
    Write a default value if there's no history available.
    
    This isn't the case right now but it may be later on when "off-channel"
    scanning occurs without init'ing or changing the NF history buffer.
    (As each channel may have a different noise floor; so scanning or
    other off-channel activity shouldn't affect the NF history of
    the current channel.)

--- sys/dev/ath/ath_hal/ar9002/ar9285_attach.c
+++ sys/dev/ath/ath_hal/ar9002/ar9285_attach.c
@@ -281,6 +281,13 @@ ar9285Attach(uint16_t devid, HAL_SOFTC sc,
 		OS_REG_WRITE(ah, AR_MISC_MODE, ahp->ah_miscMode);
 
 	ar9285AniSetup(ah);			/* Anti Noise Immunity */
+
+	/* Setup noise floor min/max/nominal values */
+	AH5416(ah)->nf_2g.max = AR_PHY_CCA_MAX_GOOD_VAL_9285_2GHZ;
+	AH5416(ah)->nf_2g.min = AR_PHY_CCA_MIN_GOOD_VAL_9285_2GHZ;
+	AH5416(ah)->nf_2g.nominal = AR_PHY_CCA_NOM_VAL_9285_2GHZ;
+	/* XXX no 5ghz values? */
+
 	ar5416InitNfHistBuff(AH5416(ah)->ah_cal.nfCalHist);
 
 	HALDEBUG(ah, HAL_DEBUG_ATTACH, "%s: return\n", __func__);
