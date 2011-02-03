32ecf620285e971bdc39a43766a8487e8189afcc
Pawel Jakub Dawidek <pjd@FreeBSD.org>
2011-02-03 11:39:49 +0000

    Setup another socketpair between parent and child, so that primary sandboxed worker can ask the main privileged process to connect in worker's behalf and then we can migrate descriptor using this socketpair to worker. This is not really needed now, but will be needed once we start to use capsicum for sandboxing.
    MFC after:	1 week

--- sbin/hastd/control.c
+++ sbin/hastd/control.c
@@ -62,6 +62,10 @@ child_cleanup(struct hast_resource *res)
 		proto_close(res->hr_event);
 		res->hr_event = NULL;
 	}
+	if (res->hr_conn != NULL) {
+		proto_close(res->hr_conn);
+		res->hr_conn = NULL;
+	}
 	res->hr_workerpid = 0;
 }
 
