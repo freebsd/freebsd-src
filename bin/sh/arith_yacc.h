6fe1347c41ce535bdbc97e6680289b33ee8a0f3c
jilles <jilles@FreeBSD.org>
2011-02-08 23:18:06 +0000

    sh: Import arithmetic expression code from dash.
    New features:
    * proper lazy evaluation of || and &&
    * ?: ternary operator
    * executable is considerably smaller (8K on i386) because lex and yacc are
      no longer used
    
    Differences from dash:
    * arith_t instead of intmax_t
    * imaxdiv() not used
    * unset or null variables default to 0
    * let/exp builtin (undocumented, will probably be removed later)
    
    Obtained from:	dash

--- bin/sh/arith_yacc.h
+++ bin/sh/arith_yacc.h
@@ -1,6 +1,11 @@
 /*-
- * Copyright (c) 1995
- *      The Regents of the University of California.  All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
+ * Copyright (c) 2007
+ *	Herbert Xu <herbert@gondor.apana.org.au>.  All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Kenneth Almquist.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -10,7 +15,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -26,14 +31,61 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)arith.h	1.1 (Berkeley) 5/4/95
  * $FreeBSD$
  */
 
-#include "shell.h"
+#define ARITH_ASS 1
 
-#define DIGITS(var) (3 + (2 + CHAR_BIT * sizeof((var))) / 3)
+#define ARITH_OR 2
+#define ARITH_AND 3
+#define ARITH_BAD 4
+#define ARITH_NUM 5
+#define ARITH_VAR 6
+#define ARITH_NOT 7
 
-arith_t arith(const char *);
-void arith_lex_reset(void);
-int expcmd(int, char **);
+#define ARITH_BINOP_MIN 8
+#define ARITH_LE 8
+#define ARITH_GE 9
+#define ARITH_LT 10
+#define ARITH_GT 11
+#define ARITH_EQ 12
+#define ARITH_REM 13
+#define ARITH_BAND 14
+#define ARITH_LSHIFT 15
+#define ARITH_RSHIFT 16
+#define ARITH_MUL 17
+#define ARITH_ADD 18
+#define ARITH_BOR 19
+#define ARITH_SUB 20
+#define ARITH_BXOR 21
+#define ARITH_DIV 22
+#define ARITH_NE 23
+#define ARITH_BINOP_MAX 24
+
+#define ARITH_ASS_MIN 24
+#define ARITH_REMASS 24
+#define ARITH_BANDASS 25
+#define ARITH_LSHIFTASS 26
+#define ARITH_RSHIFTASS 27
+#define ARITH_MULASS 28
+#define ARITH_ADDASS 29
+#define ARITH_BORASS 30
+#define ARITH_SUBASS 31
+#define ARITH_BXORASS 32
+#define ARITH_DIVASS 33
+#define ARITH_ASS_MAX 34
+
+#define ARITH_LPAREN 34
+#define ARITH_RPAREN 35
+#define ARITH_BNOT 36
+#define ARITH_QMARK 37
+#define ARITH_COLON 38
+
+union yystype {
+	arith_t val;
+	char *name;
+};
+
+extern union yystype yylval;
+
+int yylex(void);
