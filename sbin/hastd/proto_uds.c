259da67291e54dd2b4bd4bb1444c4968d0c2bf6c
pjd <pjd@FreeBSD.org>
2011-02-09 08:01:10 +0000

    When we decide to unlink socket file, sun_path must be set. If it is set, but there is problem unlinking the file, log a warning.
    MFC after:	1 week

--- sbin/hastd/proto_uds.c
+++ sbin/hastd/proto_uds.c
@@ -319,7 +319,12 @@ uds_close(void *ctx)
 	 */
 	if (uctx->uc_side == UDS_SIDE_SERVER_LISTEN &&
 	    uctx->uc_owner == getpid()) {
-		(void)unlink(uctx->uc_sun.sun_path);
+		PJDLOG_ASSERT(uctx->uc_sun.sun_path[0] != '\0');
+		if (unlink(uctx->uc_sun.sun_path) == -1) {
+			pjdlog_errno(LOG_WARNING,
+			    "Unable to unlink socket file %s",
+			    uctx->uc_sun.sun_path);
+		}
 	}
 	uctx->uc_owner = 0;
 	uctx->uc_magic = 0;
