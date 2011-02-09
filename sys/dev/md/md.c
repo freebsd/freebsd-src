0d68ab7a476d9fb8ad63c3f73ce41023ff9dedb4
marcel <marcel@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-02-09 19:31:10 +0000

    Use the preload_fetch_addr() and preload_fetch_size() convenience functions and only create the MD device when we have a non-zero pointer and size.
    Sponsored by: Juniper Networks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@218495 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/md/md.c
+++ sys/dev/md/md.c
@@ -1235,7 +1235,6 @@ static void
 g_md_init(struct g_class *mp __unused)
 {
 	caddr_t mod;
-	caddr_t c;
 	u_char *ptr, *name, *type;
 	unsigned len;
 	int i;
@@ -1263,15 +1262,15 @@ g_md_init(struct g_class *mp __unused)
 			continue;
 		if (strcmp(type, "md_image") && strcmp(type, "mfs_root"))
 			continue;
-		c = preload_search_info(mod, MODINFO_ADDR);
-		ptr = *(u_char **)c;
-		c = preload_search_info(mod, MODINFO_SIZE);
-		len = *(size_t *)c;
-		printf("%s%d: Preloaded image <%s> %d bytes at %p\n",
-		    MD_NAME, mdunits, name, len, ptr);
-		sx_xlock(&md_sx);
-		md_preloaded(ptr, len);
-		sx_xunlock(&md_sx);
+		ptr = preload_fetch_addr(mod);
+		len = preload_fetch_size(mod);
+		if (ptr != NULL && len != 0) {
+			printf("%s%d: Preloaded image <%s> %d bytes at %p\n",
+			    MD_NAME, mdunits, name, len, ptr);
+			sx_xlock(&md_sx);
+			md_preloaded(ptr, len);
+			sx_xunlock(&md_sx);
+		}
 	}
 	status_dev = make_dev(&mdctl_cdevsw, INT_MAX, UID_ROOT, GID_WHEEL,
 	    0600, MDCTL_NAME);
