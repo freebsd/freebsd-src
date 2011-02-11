3eec566f2905a4a1f22bd52f0f5ca21801087b16
jfv <jfv@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-02-11 17:18:42 +0000

    Inconsistencies in the updated igb shared code and the older em/lem, breaking the build, correcting that.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@218581 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/e1000/e1000_82542.c
+++ sys/dev/e1000/e1000_82542.c
@@ -51,7 +51,6 @@ static void e1000_rar_set_82542(struct e1000_hw *hw, u8 *addr, u32 index);
 static void e1000_clear_hw_cntrs_82542(struct e1000_hw *hw);
 static s32  e1000_read_mac_addr_82542(struct e1000_hw *hw);
 
-
 /**
  *  e1000_init_phy_params_82542 - Init PHY func ptrs.
  *  @hw: pointer to the HW structure
@@ -192,7 +191,7 @@ static s32 e1000_reset_hw_82542(struct e1000_hw *hw)
 {
 	struct e1000_bus_info *bus = &hw->bus;
 	s32 ret_val = E1000_SUCCESS;
-	u32 ctrl, icr;
+	u32 ctrl;
 
 	DEBUGFUNC("e1000_reset_hw_82542");
 
@@ -223,7 +222,7 @@ static s32 e1000_reset_hw_82542(struct e1000_hw *hw)
 	msec_delay(2);
 
 	E1000_WRITE_REG(hw, E1000_IMC, 0xffffffff);
-	icr = E1000_READ_REG(hw, E1000_ICR);
+	E1000_READ_REG(hw, E1000_ICR);
 
 	if (hw->revision_id == E1000_REVISION_2) {
 		if (bus->pci_cmd_word & CMD_MEM_WRT_INVALIDATE)
