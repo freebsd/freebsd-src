be1d53ac569e84ec62c05ab1a1a17681fb20938b
Warner Losh <imp@FreeBSD.org>
2011-02-11 06:35:53 +0000

    Generate MACHINE= and MACHINE_ARCH= lines based on the machine directive.  Once this is MFC'd, we can move these out of the template files where they are (incosnsitently) defined.
    MFC after:	1 week

--- usr.sbin/config/mkmakefile.c
+++ usr.sbin/config/mkmakefile.c
@@ -140,6 +140,8 @@ makefile(void)
 	if (ofp == 0)
 		err(1, "%s", path("Makefile.new"));
 	fprintf(ofp, "KERN_IDENT=%s\n", ident);
+	fprintf(ofp, "MACHINE=%s\n", machinename);
+	fprintf(ofp, "MACHINE_ARCH=%s\n", machinearch);
 	SLIST_FOREACH_SAFE(op, &mkopt, op_next, t) {
 		fprintf(ofp, "%s=%s", op->op_name, op->op_value);
 		while ((op = SLIST_NEXT(op, op_append)) != NULL)
