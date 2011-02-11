e9d9a399522d23ffebf217c98c97665f230bb79e
mav <mav@FreeBSD.org>
2011-02-11 07:26:40 +0000

    Add some safety belts against g_raid_fail_disk() misuse.

--- sys/geom/raid/g_raid.c
+++ sys/geom/raid/g_raid.c
@@ -1961,6 +1961,17 @@ void g_raid_fail_disk(struct g_raid_softc *sc,
     struct g_raid_subdisk *sd, struct g_raid_disk *disk)
 {
 
+	if (disk == NULL)
+		disk = sd->sd_disk;
+	if (disk == NULL) {
+		G_RAID_DEBUG1(0, sc, "Warning! Fail request to an absent disk!");
+		return;
+	}
+	if (disk->d_state != G_RAID_DISK_S_ACTIVE) {
+		G_RAID_DEBUG1(0, sc, "Warning! Fail request to a disk in a "
+		    "wrong state (%s)!", g_raid_disk_state2str(disk->d_state));
+		return;
+	}
 	if (sc->sc_md)
 		G_RAID_MD_FAIL_DISK(sc->sc_md, sd, disk);
 }
