2fb060368627dc29279d5ac969e6c3eb6b0e7ea3
jilles <jilles@FreeBSD.org>
2011-02-12 23:44:05 +0000

    sh: Detect dividing the smallest integer by -1.
    This overflows and on some architectures such as amd64 it generates SIGFPE.
    Generate an error on all architectures.

--- bin/sh/shell.h
+++ bin/sh/shell.h
@@ -59,6 +59,8 @@ typedef intmax_t arith_t;
 #define	ARITH_FORMAT_STR  "%" PRIdMAX
 #define	atoarith_t(arg)  strtoimax(arg, NULL, 0)
 #define	strtoarith_t(nptr, endptr, base)  strtoimax(nptr, endptr, base)
+#define	ARITH_MIN INTMAX_MIN
+#define	ARITH_MAX INTMAX_MAX
 
 typedef void *pointer;
 #define MKINIT  /* empty */
