38c2adb01c36fc665521b031710493286adf8900
vanhu <vanhu@FreeBSD.org>
2011-02-18 09:40:13 +0000

    Fixed IPsec's HMAC_SHA256-512 support to be RFC4868 compliant. This will break interoperability with all older versions of FreeBSD for those algorithms.
    Reviewed by:	bz, gnn
    Obtained from:	NETASQ
    MFC after:	1w

--- sys/netipsec/xform.h
+++ sys/netipsec/xform.h
@@ -46,6 +46,7 @@
 #include <opencrypto/xform.h>
 
 #define	AH_HMAC_HASHLEN		12	/* 96 bits of authenticator */
+#define	AH_HMAC_MAXHASHLEN	(SHA2_512_HASH_LEN/2)	/* Keep this updated */
 #define	AH_HMAC_INITIAL_RPL	1	/* replay counter initial value */
 
 /*
