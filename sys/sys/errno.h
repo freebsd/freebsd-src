0fbd4a42792bb396f7a17bd8ed9eea61339f2d5b
rwatson <rwatson@FreeBSD.org>
2011-03-01 13:14:28 +0000

    Add ECAPMODE, "Not permitted in capability mode", a new kernel errno constant to indicate that a system call (or perhaps an operation requested via a system call) is not permitted for a capability mode process.
    Submitted by:	anderson
    Sponsored by:	Google, Inc.
    Obtained from:	Capsicum Project
    MFC after:	1 week

--- sys/sys/errno.h
+++ sys/sys/errno.h
@@ -175,10 +175,11 @@ __END_DECLS
 
 #ifndef _POSIX_SOURCE
 #define	ENOTCAPABLE	93		/* Capabilities insufficient */
+#define	ECAPMODE		94		/* Not permitted in capability mode */
 #endif /* _POSIX_SOURCE */
 
 #ifndef _POSIX_SOURCE
-#define	ELAST		93		/* Must be equal largest errno */
+#define	ELAST		94		/* Must be equal largest errno */
 #endif /* _POSIX_SOURCE */
 
 #ifdef _KERNEL
