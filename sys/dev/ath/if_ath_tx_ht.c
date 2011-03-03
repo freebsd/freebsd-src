4a90ebd39f2e64ff7da28e18f5b0240a10bf53f6
adrian <adrian@FreeBSD.org>
2011-03-03 03:02:06 +0000

    Disable trying to do HT/40 and short-GI TX.
    These flags are just plain wrong - they're the node flags from negotiation,
    not the configured flags. I'll jump in later on and figure out exactly
    what should be done to properly set these two flags when in both STA mode
    (ie, what the AP says is possible and what's configured) and AP mode
    (ie, where the AP has a configuration, but then negotiates what's possible
    with each node, so per-node configuration can and will differ.)
    
    This allows the 11n 2.4ghz/ht20 mode to associate (but perform poorly still)
    and exchange MCS rates with atheros reference APs and a Cisco/Linksys
    E3000 AP.

--- sys/dev/ath/if_ath_tx_ht.c
+++ sys/dev/ath/if_ath_tx_ht.c
@@ -128,6 +128,7 @@ ath_rateseries_setup(struct ath_softc *sc, struct ieee80211_node *ni,
 		if (flags & (HAL_TXDESC_RTSENA | HAL_TXDESC_CTSENA))
 			series[i].RateFlags |= HAL_RATESERIES_RTS_CTS;
 
+#if 0
 		if (ni->ni_htcap & IEEE80211_HTCAP_CHWIDTH40)
 			series[i].RateFlags |= HAL_RATESERIES_2040;
 
@@ -138,6 +139,7 @@ ath_rateseries_setup(struct ath_softc *sc, struct ieee80211_node *ni,
 		 */
 		if (ni->ni_htcap & IEEE80211_HTCAP_SHORTGI40)
 			series[i].RateFlags |= HAL_RATESERIES_HALFGI;
+#endif
 
 		series[i].Rate = rt->info[rix[i]].rateCode;
 		/* the short preamble field is only applicable for non-MCS rates */
@@ -150,7 +152,7 @@ ath_rateseries_setup(struct ath_softc *sc, struct ieee80211_node *ni,
 			    ath_computedur_ht(pktlen
 				, series[i].Rate
 				, ic->ic_txstream
-				, (ni->ni_htcap & IEEE80211_HTCAP_CHWIDTH40)
+				, 0 /* disable 20/40 for now */
 				, series[i].RateFlags & HAL_RATESERIES_HALFGI);
 		} else {
 			series[i].PktDuration = ath_hal_computetxtime(ah,
