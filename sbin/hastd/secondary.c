a3ae0953aa712f5fbaa82f65d8c54e17fd07cea1
trociny <trociny@FreeBSD.org>
2011-03-11 12:12:35 +0000

    Make workers inherit debug level from the main process.
    Approved by:	pjd (mentor)
    MFC after:	1 week

--- sbin/hastd/secondary.c
+++ sbin/hastd/secondary.c
@@ -346,7 +346,7 @@ hastd_secondary(struct hast_resource *res, struct nv *nvin)
 	sigset_t mask;
 	pthread_t td;
 	pid_t pid;
-	int error, mode;
+	int error, mode, debuglevel;
 
 	/*
 	 * Create communication channel between parent and child.
@@ -398,6 +398,7 @@ hastd_secondary(struct hast_resource *res, struct nv *nvin)
 
 	gres = res;
 	mode = pjdlog_mode_get();
+	debuglevel = pjdlog_debug_get();
 
 	/* Declare that we are sender. */
 	proto_send(res->hr_event, NULL, 0);
@@ -409,6 +410,7 @@ hastd_secondary(struct hast_resource *res, struct nv *nvin)
 	descriptors_assert(res, mode);
 
 	pjdlog_init(mode);
+	pjdlog_debug_set(debuglevel);
 	pjdlog_prefix_set("[%s] (%s) ", res->hr_name, role2str(res->hr_role));
 	setproctitle("%s (secondary)", res->hr_name);
 
