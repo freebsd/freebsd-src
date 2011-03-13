3563a29f7a59d2164bb76ed5a0a4dbfa62b7f970
nwhitehorn <nwhitehorn@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-03-13 15:08:06 +0000

    Connect the full doc build (Handbook, etc.) to the build. This is not built if no doc checkout is found.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@219610 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/generate-release.sh
+++ release/generate-release.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # generate-release.sh: check out source trees, and build release components with
-#  totally clean, fresh trees
+#  totally clean, fresh trees.
 #
 #  Usage: generate-release.sh svn-branch scratch-dir
 #
 # Environment variables:
-#  CVSUP_HOST: Host of a cvsup server to obtain the ports tree. Must be set
-#   to include ports.
-#  CVSUP_TAG:  CVS tag for ports (HEAD by default)
+#  CVSUP_HOST: Host of a cvsup server to obtain the ports and documentation
+#   trees. Must be set to include ports and documentation.
+#  CVSUP_TAG:  CVS tag for ports and documentation (HEAD by default)
 #  MAKE_FLAGS: optional flags to pass to make (e.g. -j)
 # 
 #  Note: Since this requires a chroot, release cross-builds will not work!
@@ -19,7 +19,7 @@
 mkdir -p $2/usr/src
 svn co svn://svn.freebsd.org/base/$1 $2/usr/src || exit 1
 if [ ! -z $CVSUP_HOST ]; then
-	cat > $2/ports-supfile << EOF
+	cat > $2/docports-supfile << EOF
 	*default host=$CVSUP_HOST
 	*default base=/var/db
 	*default prefix=/usr
@@ -27,9 +27,10 @@ if [ ! -z $CVSUP_HOST ]; then
 	*default delete use-rel-suffix
 	*default compress
 	ports-all
+	doc-all
 EOF
 else
-	RELEASE_FLAGS=-DNOPORTS
+	RELEASE_FLAGS="-DNOPORTS -DNODOC"
 fi
 
 cd $2/usr/src
@@ -39,8 +40,15 @@ mount -t devfs devfs $2/dev
 
 if [ ! -z $CVSUP_HOST ]; then 
 	cp /etc/resolv.conf $2/etc/resolv.conf
-	chroot $2 /usr/bin/csup /ports-supfile || exit 1
+
+	# Checkout ports and doc trees
+	#chroot $2 /usr/bin/csup /docports-supfile || exit 1
+
+	# Build ports to build the docs, then build the docs
+	chroot $2 /bin/sh -c 'pkg_add -r docproj || (cd /usr/ports/textproc/docproj && make install clean BATCH=yes WITHOUT_X11=yes JADETEX=no WITHOUT_PYTHON=yes)' || exit 1
+	chroot $2 /bin/sh -c "cd /usr/doc && make $MAKE_FLAGS 'FORMATS=html html-split txt' URLS_ABSOLUTE=YES" || exit 1
 fi
+
 chroot $2 /bin/sh -c "cd /usr/src && make $MAKE_FLAGS buildworld buildkernel" || exit 1
 mkdir $2/R
 chroot $2 /bin/sh -c "cd /usr/src/release && MAKEOBJDIR=/R make -f Makefile.bsdinstall release $RELEASE_FLAGS" || exit 1
