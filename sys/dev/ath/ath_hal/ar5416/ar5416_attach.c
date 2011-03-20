3923f7a93d657daafff44dd9dc142330f0098a08
adrian <adrian@FreeBSD.org>
2011-03-20 08:42:56 +0000

    Bring over a copy of the AR5212 TX queue reset and setup routines, in preparation for fixing them based on the ath9k related TXQ fixes.
    I've done this so people can go over the history of the diffs to the original
    AR5212 routines (which AR5416 and later chips use) to see what's changed.

--- sys/dev/ath/ath_hal/ar5416/ar5416_attach.c
+++ sys/dev/ath/ath_hal/ar5416/ar5416_attach.c
@@ -112,6 +112,8 @@ ar5416InitState(struct ath_hal_5416 *ahp5416, uint16_t devid, HAL_SOFTC sc,
 	ah->ah_fillTxDesc		= ar5416FillTxDesc;
 	ah->ah_procTxDesc		= ar5416ProcTxDesc;
 	ah->ah_getTxCompletionRates	= ar5416GetTxCompletionRates;
+	ah->ah_setupTxQueue		= ar5416SetupTxQueue;
+	ah->ah_resetTxQueue		= ar5416ResetTxQueue;
 
 	/* Receive Functions */
 	ah->ah_startPcuReceive		= ar5416StartPcuReceive;
