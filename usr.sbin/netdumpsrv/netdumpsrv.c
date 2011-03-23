40c968b267aeac1944b05e7756c9101ef10bed73
attilio <attilio@FreeBSD.org>
2011-03-23 15:53:21 +0000

    Update copyrights.

--- usr.sbin/netdumpsrv/netdumpsrv.c
+++ usr.sbin/netdumpsrv/netdumpsrv.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2005-2010 Sandvine Incorporated. All rights reserved.
+ * Copyright (c) 2005-2011 Sandvine Incorporated. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
