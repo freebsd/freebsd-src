a3e74b07b5c4afb23a781b3276a259ffe856fc00
alc <alc@FreeBSD.org>
2011-03-23 16:38:29 +0000

    Modestly increase the maximum allowed size of the kmem map on i386. Also, express this new maximum as a fraction of the kernel's address space size rather than a constant so that increasing KVA_PAGES will automatically increase this maximum.  As a side-effect of this change, kern.maxvnodes will automatically increase by a proportional amount.
    While I'm here ensure that this change doesn't result in an unintended
    increase in maxpipekva on i386.  Calculate maxpipekva based upon the
    size of the kernel address space and the amount of physical memory
    instead of the size of the kmem map.  The memory backing pipes is not
    allocated from the kmem map.  It is allocated from its own submap of
    the kernel map.  In short, it has no real connection to the kmem map.
    (In fact, the commit messages for the maxpipekva auto-sizing talk
    about using the kernel map size, cf. r117325 and r117391, even though
    the implementation actually used the kmem map size.)  Although the
    calculation is now done differently, the resulting value for
    maxpipekva should remain almost the same on i386.  However, on amd64,
    the value will be reduced by 2/3.  This is intentional.  The recent
    change to VM_KMEM_SIZE_SCALE on amd64 for the benefit of ZFS also had
    the unnecessary side-effect of increasing maxpipekva.  This change is
    effectively restoring maxpipekva on amd64 to its prior value.
    
    Eliminate init_param3() since it is no longer used.

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -171,7 +171,6 @@ void	critical_enter(void);
 void	critical_exit(void);
 void	init_param1(void);
 void	init_param2(long physpages);
-void	init_param3(long kmempages);
 void	init_static_kenv(char *, size_t);
 void	tablefull(const char *);
 int	kvprintf(char const *, void (*)(int, void*), void *, int,
