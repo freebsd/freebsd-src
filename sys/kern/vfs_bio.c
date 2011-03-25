8e48af121bf7f37f38cfbc365af8de1003a88695
mdf <mdf@FreeBSD.org>
2011-03-25 20:51:20 +0000

    MFC r192908 (introduced before the stable/8 split from CURRENT):
    fail(9) support:
    
    Add support for kernel fault injection using KFAIL_POINT_* macros and
    fail_point_* infrastructure. Add example fail point in vfs_bio.c to
    simulate VM buf pressure.
    
    MFC r192978:
    
    Fix style/grammar issues in fail(9) man page.
    
    MFC r216616:
    
    Move the fail_point_entry definition from fail.h to kern_fail.c, which
    allows putting the enumeration constants of fail point types with the
    text string that matches them.
    
    MFC r216620:
    
    Initialize fp_location for explicitly managed fail points, and push
    the parentheses around the location for simple fail points into the
    location string.  This makes the print on fail point set more
    consistent between the two versions.
    
    Also fix up fail.h a little for style(9): only use one of sys/param.h
    and sys/types.h, and use the existing __XSTRING() macro instead of
    rolling our own.  Also fix up a few tabs on changed and nearby lines.
    
    Lastly, since KFAIL_POINT_{BEGIN,END} are not meant for use outside
    this file, just eliminate the macros entirely.

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -48,6 +48,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/buf.h>
 #include <sys/devicestat.h>
 #include <sys/eventhandler.h>
+#include <sys/fail.h>
 #include <sys/limits.h>
 #include <sys/lock.h>
 #include <sys/malloc.h>
@@ -1180,6 +1181,15 @@ buf_dirty_count_severe(void)
 	return(numdirtybuffers >= hidirtybuffers);
 }
 
+static __noinline int
+buf_vm_page_count_severe(void)
+{
+
+	KFAIL_POINT_CODE(DEBUG_FP, buf_pressure, return 1);
+
+	return vm_page_count_severe();
+}
+
 /*
  *	brelse:
  *
@@ -1252,7 +1262,7 @@ brelse(struct buf *bp)
 	 */
 	if (bp->b_flags & B_DELWRI)
 		bp->b_flags &= ~B_RELBUF;
-	else if (vm_page_count_severe()) {
+	else if (buf_vm_page_count_severe()) {
 		/*
 		 * XXX This lock may not be necessary since BKGRDINPROG
 		 * cannot be set while we hold the buf lock, it can only be
@@ -1522,7 +1532,7 @@ bqrelse(struct buf *bp)
 		 * cleared if it is already pending.
 		 */
 		BO_LOCK(bp->b_bufobj);
-		if (!vm_page_count_severe() || bp->b_vflags & BV_BKGRDINPROG) {
+		if (!buf_vm_page_count_severe() || bp->b_vflags & BV_BKGRDINPROG) {
 			BO_UNLOCK(bp->b_bufobj);
 			bp->b_qindex = QUEUE_CLEAN;
 			TAILQ_INSERT_TAIL(&bufqueues[QUEUE_CLEAN], bp,
@@ -1593,7 +1603,7 @@ vfs_vmio_release(struct buf *bp)
 				vm_page_free(m);
 			} else if (bp->b_flags & B_DIRECT) {
 				vm_page_try_to_free(m);
-			} else if (vm_page_count_severe()) {
+			} else if (buf_vm_page_count_severe()) {
 				vm_page_try_to_cache(m);
 			}
 		}
