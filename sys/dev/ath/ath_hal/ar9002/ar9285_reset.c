06db6eddac25c633f1c794ce00ab0806f4263c3b
adrian <adrian@FreeBSD.org>
2011-04-11 11:15:34 +0000

    De-dup the ar5416 rates array definition.

--- sys/dev/ath/ath_hal/ar9002/ar9285_reset.c
+++ sys/dev/ath/ath_hal/ar9002/ar9285_reset.c
@@ -67,20 +67,6 @@ static void ar9285GetGainBoundariesAndPdadcs(struct ath_hal *ah,
 	uint16_t numXpdGains);
 static uint16_t ar9285GetMaxEdgePower(uint16_t, CAL_CTL_EDGES *);
 
-/* XXX gag, this is sick */
-typedef enum Ar5416_Rates {
-	rate6mb,  rate9mb,  rate12mb, rate18mb,
-	rate24mb, rate36mb, rate48mb, rate54mb,
-	rate1l,   rate2l,   rate2s,   rate5_5l,
-	rate5_5s, rate11l,  rate11s,  rateXr,
-	rateHt20_0, rateHt20_1, rateHt20_2, rateHt20_3,
-	rateHt20_4, rateHt20_5, rateHt20_6, rateHt20_7,
-	rateHt40_0, rateHt40_1, rateHt40_2, rateHt40_3,
-	rateHt40_4, rateHt40_5, rateHt40_6, rateHt40_7,
-	rateDupCck, rateDupOfdm, rateExtCck, rateExtOfdm,
-	Ar5416RateSize
-} AR5416_RATES;
-
 HAL_BOOL
 ar9285SetTransmitPower(struct ath_hal *ah,
 	const struct ieee80211_channel *chan, uint16_t *rfXpdGain)
