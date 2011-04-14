08cb9defa325ff643c5dd54b258070fe253b89d1
jkim <jkim@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-04-14 00:07:08 +0000

    Add some tunable descriptions about x86 timers.
    Requested by:	arundel
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@220613 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/x86/x86/tsc.c
+++ sys/x86/x86/tsc.c
@@ -361,7 +361,7 @@ sysctl_machdep_tsc_freq(SYSCTL_HANDLER_ARGS)
 }
 
 SYSCTL_PROC(_machdep, OID_AUTO, tsc_freq, CTLTYPE_U64 | CTLFLAG_RW,
-    0, 0, sysctl_machdep_tsc_freq, "QU", "");
+    0, 0, sysctl_machdep_tsc_freq, "QU", "Time Stamp Counter frequency");
 
 static unsigned
 tsc_get_timecount(struct timecounter *tc)
