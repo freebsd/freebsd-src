58ff0f42ba02cb62fc85f0d93d3bdb2540b0ec8f
Dimitry Andric <dim@FreeBSD.org>
2011-04-19 18:09:21 +0000

    Remove support for the Intel C Compiler from the build infrastructure. This support has not worked for several years, and is not likely to work again, unless Intel decides to release a native FreeBSD version of their compiler. ;)

--- share/mk/bsd.cpu.mk
+++ share/mk/bsd.cpu.mk
@@ -91,31 +91,6 @@ _CPUCFLAGS = -march=prescott
 .  else
 _CPUCFLAGS = -march=${CPUTYPE}
 .  endif # GCC on 'i386'
-.  if ${CPUTYPE} == "crusoe"
-_ICC_CPUCFLAGS = -tpp6 -xiM
-.  elif ${CPUTYPE} == "athlon-mp" || ${CPUTYPE} == "athlon-xp" || \
-    ${CPUTYPE} == "athlon-4"
-_ICC_CPUCFLAGS = -tpp6 -xiMK
-.  elif ${CPUTYPE} == "athlon-tbird" || ${CPUTYPE} == "athlon"
-_ICC_CPUCFLAGS = -tpp6 -xiM
-.  elif ${CPUTYPE} == "k6-3" || ${CPUTYPE} == "k6-2" || ${CPUTYPE} == "k6"
-_ICC_CPUCFLAGS = -tpp6 -xi
-.  elif ${CPUTYPE} == "k5"
-_ICC_CPUCFLAGS = -tpp5
-.  elif ${CPUTYPE} == "pentium4" || ${CPUTYPE} == "pentium4m"
-_ICC_CPUCFLAGS = -tpp7 -xiMKW
-.  elif ${CPUTYPE} == "pentium3" || ${CPUTYPE} == "pentium3m" || \
-     ${CPUTYPE} == "pentium-m"
-_ICC_CPUCFLAGS = -tpp6 -xiMK
-.  elif ${CPUTYPE} == "pentium2" || ${CPUTYPE} == "pentiumpro"
-_ICC_CPUCFLAGS = -tpp6 -xiM
-.  elif ${CPUTYPE} == "pentium-mmx"
-_ICC_CPUCFLAGS = -tpp5 -xM
-.  elif ${CPUTYPE} == "pentium"
-_ICC_CPUCFLAGS = -tpp5
-.  else
-_ICC_CPUCFLAGS =
-.  endif # ICC on 'i386'
 . elif ${MACHINE_CPUARCH} == "amd64"
 _CPUCFLAGS = -march=${CPUTYPE}
 . elif ${MACHINE_CPUARCH} == "arm"
@@ -242,9 +217,5 @@ CFLAGS += -G0
 # NB: COPTFLAGS is handled in /usr/src/sys/conf/kern.pre.mk
 
 .if !defined(NO_CPU_CFLAGS)
-. if ${CC} == "icc"
-CFLAGS += ${_ICC_CPUCFLAGS}
-. else
 CFLAGS += ${_CPUCFLAGS}
-. endif
 .endif
--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -6,14 +6,9 @@
 # Note that the newly added -Wcast-qual is responsible for generating 
 # most of the remaining warnings.  Warnings introduced with -Wall will
 # also pop up, but are easier to fix.
-.if ${CC:T:Micc} == "icc"
-#CWARNFLAGS=	-w2	# use this if you are terribly bored
-CWARNFLAGS=
-.else
 CWARNFLAGS?=	-Wall -Wredundant-decls -Wnested-externs -Wstrict-prototypes \
 		-Wmissing-prototypes -Wpointer-arith -Winline -Wcast-qual \
 		-Wundef -Wno-pointer-sign -fformat-extensions
-.endif
 #
 # The following flags are next up for working on:
 #	-W
@@ -29,7 +24,7 @@ CWARNFLAGS?=	-Wall -Wredundant-decls -Wnested-externs -Wstrict-prototypes \
 # operations inside the kernel itself.  These operations are exclusively
 # reserved for user applications.
 #
-.if ${MACHINE_CPUARCH} == "i386" && ${CC:T:Micc} != "icc"
+.if ${MACHINE_CPUARCH} == "i386"
 .if ${CC:T:Mclang} != "clang"
 CFLAGS+=	-mno-align-long-strings -mpreferred-stack-boundary=2
 .endif
@@ -100,22 +95,13 @@ INLINE_LIMIT?=	8000
 # GCC 3.0 and above like to do certain optimizations based on the
 # assumption that the program is linked against libc.  Stop this.
 #
-.if ${CC:T:Micc} == "icc"
-CFLAGS+=	-nolib_inline
-.else
 CFLAGS+=	-ffreestanding
-.endif
-
-.if ${CC:T:Micc} == "icc"
-CFLAGS+=	-restrict
-.endif
 
 #
 # GCC SSP support.
 #
-.if ${MK_SSP} != "no" && ${CC:T:Micc} != "icc" && \
-    ${MACHINE_CPUARCH} != "ia64" && ${MACHINE_CPUARCH} != "arm" && \
-    ${MACHINE_CPUARCH} != "mips"
+.if ${MK_SSP} != "no" && ${MACHINE_CPUARCH} != "ia64" && \
+    ${MACHINE_CPUARCH} != "arm" && ${MACHINE_CPUARCH} != "mips"
 CFLAGS+=	-fstack-protector
 .endif
 
