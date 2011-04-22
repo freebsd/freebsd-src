d6656eb1dc46b7c50befb9f030678f8b6208b6fd
marius <marius@FreeBSD.org>
2011-04-22 09:22:27 +0000

    Correct spelling in comments.
    Submitted by:	brucec

--- sys/dev/mii/brgphyreg.h
+++ sys/dev/mii/brgphyreg.h
@@ -59,7 +59,7 @@
 #define	BRGPHY_BMSR_EXTSTS	0x0100	/* Extended status present */
 #define	BRGPHY_BMSR_PRESUB	0x0040	/* Preamble surpression */
 #define	BRGPHY_BMSR_ACOMP	0x0020	/* Autoneg complete */
-#define	BRGPHY_BMSR_RFAULT	0x0010	/* Remote fault condition occured */
+#define	BRGPHY_BMSR_RFAULT	0x0010	/* Remote fault condition occurred */
 #define	BRGPHY_BMSR_ANEG	0x0008	/* Autoneg capable */
 #define	BRGPHY_BMSR_LINK	0x0004	/* Link status */
 #define	BRGPHY_BMSR_JABBER	0x0002	/* Jabber detected */
