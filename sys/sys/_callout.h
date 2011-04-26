b2ad91f26baf6b73bb0072ab184ed34f33f04fcb
Konstantin Belousov <kib@FreeBSD.org>
2011-04-26 11:39:56 +0000

    Implement the delayed task execution extension to the taskqueue mechanism. The caller may specify a timeout in ticks after which the task will be scheduled.
    Sponsored by:	The FreeBSD Foundation
    Reviewed by:	jeff, jhb
    MFC after:	1 month

--- sys/sys/_callout.h
+++ sys/sys/_callout.h
@@ -35,52 +35,27 @@
  * $FreeBSD$
  */
 
-#ifndef _SYS_CALLOUT_H_
-#define _SYS_CALLOUT_H_
+#ifndef _SYS__CALLOUT_H
+#define	_SYS__CALLOUT_H
 
-#include <sys/_callout.h>
+#include <sys/queue.h>
 
-#define	CALLOUT_LOCAL_ALLOC	0x0001 /* was allocated from callfree */
-#define	CALLOUT_ACTIVE		0x0002 /* callout is currently active */
-#define	CALLOUT_PENDING		0x0004 /* callout is waiting for timeout */
-#define	CALLOUT_MPSAFE		0x0008 /* callout handler is mp safe */
-#define	CALLOUT_RETURNUNLOCKED	0x0010 /* handler returns with mtx unlocked */
-#define	CALLOUT_SHAREDLOCK	0x0020 /* callout lock held in shared mode */
+struct lock_object;
 
-struct callout_handle {
-	struct callout *callout;
+SLIST_HEAD(callout_list, callout);
+TAILQ_HEAD(callout_tailq, callout);
+
+struct callout {
+	union {
+		SLIST_ENTRY(callout) sle;
+		TAILQ_ENTRY(callout) tqe;
+	} c_links;
+	int	c_time;				/* ticks to the event */
+	void	*c_arg;				/* function argument */
+	void	(*c_func)(void *);		/* function to call */
+	struct lock_object *c_lock;		/* lock to handle */
+	int	c_flags;			/* state of this entry */
+	volatile int c_cpu;			/* CPU we're scheduled on */
 };
 
-#ifdef _KERNEL
-extern int ncallout;
-
-#define	callout_active(c)	((c)->c_flags & CALLOUT_ACTIVE)
-#define	callout_deactivate(c)	((c)->c_flags &= ~CALLOUT_ACTIVE)
-#define	callout_drain(c)	_callout_stop_safe(c, 1)
-void	callout_init(struct callout *, int);
-void	_callout_init_lock(struct callout *, struct lock_object *, int);
-#define	callout_init_mtx(c, mtx, flags)					\
-	_callout_init_lock((c), ((mtx) != NULL) ? &(mtx)->lock_object :	\
-	    NULL, (flags))
-#define	callout_init_rw(c, rw, flags)					\
-	_callout_init_lock((c), ((rw) != NULL) ? &(rw)->lock_object :	\
-	   NULL, (flags))
-#define	callout_pending(c)	((c)->c_flags & CALLOUT_PENDING)
-int	callout_reset_on(struct callout *, int, void (*)(void *), void *, int);
-#define	callout_reset(c, on_tick, fn, arg)				\
-    callout_reset_on((c), (on_tick), (fn), (arg), (c)->c_cpu)
-#define	callout_reset_curcpu(c, on_tick, fn, arg)			\
-    callout_reset_on((c), (on_tick), (fn), (arg), PCPU_GET(cpuid))
-int	callout_schedule(struct callout *, int);
-int	callout_schedule_on(struct callout *, int, int);
-#define	callout_schedule_curcpu(c, on_tick)				\
-    callout_schedule_on((c), (on_tick), PCPU_GET(cpuid))
-#define	callout_stop(c)		_callout_stop_safe(c, 0)
-int	_callout_stop_safe(struct callout *, int);
-void	callout_tick(void);
-int	callout_tickstofirst(int limit);
-extern void (*callout_new_inserted)(int cpu, int ticks);
-
 #endif
-
-#endif /* _SYS_CALLOUT_H_ */
