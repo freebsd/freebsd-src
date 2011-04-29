f16bc90238593ef4692ced877531d76a1c5510a6
delphij <delphij@FreeBSD.org>
2011-04-29 00:40:00 +0000

    MFC r220582:
    Add support for IEE/IEC (and now also SI) power of two notions of
    prefixes (Ki, Mi, Gi...) for humanize_number(3).
    
    Note that applications has to pass HN_IEC_PREFIXES to use this
    feature for backward compatibility reasons.
    
    Reviewed by:	arundel

--- lib/libutil/libutil.h
+++ lib/libutil/libutil.h
@@ -207,7 +207,9 @@ __END_DECLS
 #define HN_NOSPACE		0x02
 #define HN_B			0x04
 #define HN_DIVISOR_1000		0x08
+#define HN_IEC_PREFIXES		0x10
 
+/* maxscale = 0x07 */
 #define HN_GETSCALE		0x10
 #define HN_AUTOSCALE		0x20
 
