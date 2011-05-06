6d973b2bab3b0988a389d1ba9b6a8fc3e3f47301
fabient <fabient@FreeBSD.org>
2011-05-06 14:11:20 +0000

    MFC r220198: Clearing the flag when preempting will let the preempted thread run too much time. This can finish in a scheduler deadlock with ping-pong between two threads.
    One sample of this is:
    - device lapic (to have a preemption point on critical_exit())
    - options DEVICE_POLLING with HZ>1499 (to have lapic freq = hardclock freq)
    - running a cpu intensive task (that does not enter the kernel)
    - only one CPU on SMP or no SMP.
    
    As requested by jhb@ 4BSD have received the same type of fix instead of
    propagating the flag to the new thread.

--- sys/kern/sched_4bsd.c
+++ sys/kern/sched_4bsd.c
@@ -848,13 +848,9 @@ sched_switch(struct thread *td, struct thread *newtd, int flags)
 	if ((p->p_flag & P_NOLOAD) == 0)
 		sched_load_rem();
 
-	if (newtd) {
-		MPASS(newtd->td_lock == &sched_lock);
-		newtd->td_flags |= (td->td_flags & TDF_NEEDRESCHED);
-	}
-
 	td->td_lastcpu = td->td_oncpu;
-	td->td_flags &= ~TDF_NEEDRESCHED;
+	if (!(flags & SW_PREEMPT))
+		td->td_flags &= ~TDF_NEEDRESCHED;
 	td->td_owepreempt = 0;
 	td->td_oncpu = NOCPU;
 
