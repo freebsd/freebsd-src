e749cb6de76cfa2dbaf1bfa2c0bbd5a1face1c50
Marius Strobl <marius@FreeBSD.org>
2011-05-06 20:04:47 +0000

    MFC: r216962
    Inherit the APB and the generic OFW PCI-PCI bridge driver from the generic
    PCI-PCI bridge driver in order to safe some code.

--- sys/sparc64/pci/ofw_pcib.c
+++ sys/sparc64/pci/ofw_pcib.c
@@ -65,31 +65,11 @@ static device_method_t ofw_pcib_methods[] = {
 	/* Device interface */
 	DEVMETHOD(device_probe,		ofw_pcib_probe),
 	DEVMETHOD(device_attach,	ofw_pcib_attach),
-	DEVMETHOD(device_shutdown,	bus_generic_shutdown),
-	DEVMETHOD(device_suspend,	bus_generic_suspend),
-	DEVMETHOD(device_resume,	bus_generic_resume),
 
 	/* Bus interface */
-	DEVMETHOD(bus_print_child,	bus_generic_print_child),
-	DEVMETHOD(bus_read_ivar,	pcib_read_ivar),
-	DEVMETHOD(bus_write_ivar,	pcib_write_ivar),
-	DEVMETHOD(bus_alloc_resource,	pcib_alloc_resource),
-	DEVMETHOD(bus_activate_resource, bus_generic_activate_resource),
-	DEVMETHOD(bus_deactivate_resource, bus_generic_deactivate_resource),
-	DEVMETHOD(bus_release_resource,	bus_generic_release_resource),
-	DEVMETHOD(bus_setup_intr,	bus_generic_setup_intr),
-	DEVMETHOD(bus_teardown_intr,	bus_generic_teardown_intr),
 
 	/* pcib interface */
-	DEVMETHOD(pcib_maxslots,	pcib_maxslots),
-	DEVMETHOD(pcib_read_config,	pcib_read_config),
-	DEVMETHOD(pcib_write_config,	pcib_write_config),
-	DEVMETHOD(pcib_route_interrupt,	ofw_pcib_gen_route_interrupt),
-	DEVMETHOD(pcib_alloc_msi,	pcib_alloc_msi),
-	DEVMETHOD(pcib_release_msi,	pcib_release_msi),
-	DEVMETHOD(pcib_alloc_msix,	pcib_alloc_msix),
-	DEVMETHOD(pcib_release_msix,	pcib_release_msix),
-	DEVMETHOD(pcib_map_msi,		pcib_map_msi),
+	DEVMETHOD(pcib_route_interrupt, ofw_pcib_gen_route_interrupt),
 
 	/* ofw_bus interface */
 	DEVMETHOD(ofw_bus_get_node,	ofw_pcib_gen_get_node),
@@ -99,8 +79,8 @@ static device_method_t ofw_pcib_methods[] = {
 
 static devclass_t pcib_devclass;
 
-DEFINE_CLASS_0(pcib, ofw_pcib_driver, ofw_pcib_methods,
-    sizeof(struct ofw_pcib_gen_softc));
+DEFINE_CLASS_1(pcib, ofw_pcib_driver, ofw_pcib_methods,
+    sizeof(struct ofw_pcib_gen_softc), pcib_driver);
 DRIVER_MODULE(ofw_pcib, pci, ofw_pcib_driver, pcib_devclass, 0, 0);
 MODULE_DEPEND(ofw_pcib, pci, 1, 1, 1);
 
