fd331bf9bbd641c444c632b9904d73cdb1bdf0b5
Adrian Chadd <adrian@FreeBSD.org>
2011-05-11 13:25:43 +0000

    Always log if the NF CCA load fails; so users with debugging enabled can see they're likely in a very noisy environment.

--- sys/dev/ath/ath_hal/ar5416/ar5416_cal.c
+++ sys/dev/ath/ath_hal/ar5416/ar5416_cal.c
@@ -638,8 +638,8 @@ ar5416LoadNF(struct ath_hal *ah, const struct ieee80211_channel *chan)
 		 * here, the baseband nf cal will just be capped by our present
 		 * noisefloor until the next calibration timer.
 		 */
-		HALDEBUG(ah, HAL_DEBUG_ANY, "Timeout while waiting for nf "
-		    "to load: AR_PHY_AGC_CONTROL=0x%x\n",
+		HALDEBUG(ah, HAL_DEBUG_UNMASKABLE, "Timeout while waiting for "
+		    "nf to load: AR_PHY_AGC_CONTROL=0x%x\n",
 		    OS_REG_READ(ah, AR_PHY_AGC_CONTROL));
 		return;
 	}
