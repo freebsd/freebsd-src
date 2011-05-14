ba85839e6776fb71a6249efde23e33dcc69596be
mav <mav@FreeBSD.org>
2011-05-14 00:35:56 +0000

    MFC r220829: According to specification. device should respond to COMRESET with COMINIT in no more then 10ms. If we detected no device presence within that time, there is no reason to wait longer.

--- sys/dev/siis/siis.c
+++ sys/dev/siis/siis.c
@@ -1540,10 +1540,10 @@ siis_devreset(device_t dev)
 	ATA_OUTL(ch->r_mem, SIIS_P_CTLSET, SIIS_P_CTL_DEV_RESET);
 	while (((val = ATA_INL(ch->r_mem, SIIS_P_STS)) &
 	    SIIS_P_CTL_DEV_RESET) != 0) {
-		DELAY(1000);
-		if (timeout++ > 100) {
-			device_printf(dev, "device reset stuck (timeout %dms) "
-			    "status = %08x\n", timeout, val);
+		DELAY(100);
+		if (timeout++ > 1000) {
+			device_printf(dev, "device reset stuck "
+			    "(timeout 100ms) status = %08x\n", val);
 			return (EBUSY);
 		}
 	}
@@ -1731,27 +1731,39 @@ static int
 siis_sata_connect(struct siis_channel *ch)
 {
 	u_int32_t status;
-	int timeout;
+	int timeout, found = 0;
 
 	/* Wait up to 100ms for "connect well" */
-	for (timeout = 0; timeout < 100 ; timeout++) {
+	for (timeout = 0; timeout < 1000 ; timeout++) {
 		status = ATA_INL(ch->r_mem, SIIS_P_SSTS);
+		if ((status & ATA_SS_DET_MASK) != ATA_SS_DET_NO_DEVICE)
+			found = 1;
 		if (((status & ATA_SS_DET_MASK) == ATA_SS_DET_PHY_ONLINE) &&
 		    ((status & ATA_SS_SPD_MASK) != ATA_SS_SPD_NO_SPEED) &&
 		    ((status & ATA_SS_IPM_MASK) == ATA_SS_IPM_ACTIVE))
 			break;
-		DELAY(1000);
+		if ((status & ATA_SS_DET_MASK) == ATA_SS_DET_PHY_OFFLINE) {
+			if (bootverbose) {
+				device_printf(ch->dev, "SATA offline status=%08x\n",
+				    status);
+			}
+			return (0);
+		}
+		if (found == 0 && timeout >= 100)
+			break;
+		DELAY(100);
 	}
-	if (timeout >= 100) {
+	if (timeout >= 1000 || !found) {
 		if (bootverbose) {
-			device_printf(ch->dev, "SATA connect timeout status=%08x\n",
-			    status);
+			device_printf(ch->dev,
+			    "SATA connect timeout time=%dus status=%08x\n",
+			    timeout * 100, status);
 		}
 		return (0);
 	}
 	if (bootverbose) {
-		device_printf(ch->dev, "SATA connect time=%dms status=%08x\n",
-		    timeout, status);
+		device_printf(ch->dev, "SATA connect time=%dus status=%08x\n",
+		    timeout * 100, status);
 	}
 	/* Clear SATA error register */
 	ATA_OUTL(ch->r_mem, SIIS_P_SERR, 0xffffffff);
