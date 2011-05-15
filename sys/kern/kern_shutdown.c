ad369e751e110ead6a23bbce9cb6a9b545f78889
attilio <attilio@FreeBSD.org>
2011-05-15 01:08:51 +0000

    MFC r221121,221173: - Add the possibility to reuse the already last used timeout when patting   the watchdog, via the watchdog(9) interface. - Add the possibility to pat the watchdogs installed via the watchdog(9)   interface from the kernel. - Avoid to pass WD_ACTIVE down in the watchdog handlers. All the control   bit processing should over to the upper layer functions and not passed   down to the handlers at all. - Add the watchdogs patting during the (shutdown time) disk syncing and   disk dumping.
    Sponsored by:	Sandvine Incorporated

--- sys/kern/kern_shutdown.c
+++ sys/kern/kern_shutdown.c
@@ -42,6 +42,7 @@ __FBSDID("$FreeBSD$");
 #include "opt_panic.h"
 #include "opt_show_busybufs.h"
 #include "opt_sched.h"
+#include "opt_watchdog.h"
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -65,6 +66,9 @@ __FBSDID("$FreeBSD$");
 #include <sys/smp.h>
 #include <sys/sysctl.h>
 #include <sys/sysproto.h>
+#ifdef SW_WATCHDOG
+#include <sys/watchdog.h>
+#endif
 
 #include <ddb/ddb.h>
 
@@ -311,6 +315,9 @@ boot(int howto)
 
 		waittime = 0;
 
+#ifdef SW_WATCHDOG
+		wdog_kern_pat(WD_LASTVAL);
+#endif
 		sync(curthread, NULL);
 
 		/*
@@ -336,6 +343,9 @@ boot(int howto)
 			if (nbusy < pbusy)
 				iter = 0;
 			pbusy = nbusy;
+#ifdef SW_WATCHDOG
+			wdog_kern_pat(WD_LASTVAL);
+#endif
 			sync(curthread, NULL);
 
 #ifdef PREEMPTION
