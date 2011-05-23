69f20808c98fd6408fd5077bc1b75068963b8712
pjd <pjd@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-05-23 21:00:37 +0000

    Recognize BIO_FLUSH requests and pass them to userland.
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@222225 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/geom/gate/g_gate.c
+++ sys/geom/gate/g_gate.c
@@ -180,6 +180,7 @@ g_gate_start(struct bio *bp)
 		break;
 	case BIO_DELETE:
 	case BIO_WRITE:
+	case BIO_FLUSH:
 		/* XXX: Hack to allow read-only mounts. */
 		if ((sc->sc_flags & G_GATE_FLAG_READONLY) != 0) {
 			g_io_deliver(bp, EPERM);
@@ -580,6 +581,7 @@ g_gate_ioctl(struct cdev *dev, u_long cmd, caddr_t addr, int flags, struct threa
 		switch (bp->bio_cmd) {
 		case BIO_READ:
 		case BIO_DELETE:
+		case BIO_FLUSH:
 			break;
 		case BIO_WRITE:
 			error = copyout(bp->bio_data, ggio->gctl_data,
@@ -643,6 +645,7 @@ start_end:
 					break;
 				case BIO_DELETE:
 				case BIO_WRITE:
+				case BIO_FLUSH:
 					break;
 				}
 			}
