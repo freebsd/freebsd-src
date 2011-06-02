freebsd/freebsd-src

commit fa767596753a63333c29f8d9ab44f85c42e3879b

Let users' PATH decide which groff suite to pick up.
Let groff pass the -c flag to grotty, which will turn off ANSI
sequences. While these are not a problem for our more/less, they get
mangled by col(1) and this will result in garbage output.

This makes man(1) work together with textproc/groff, in case the
user decided to delete the old groff from base (-DWITHOUT_GROFF).


git-svn-id: svn+ssh://svn.freebsd.org/base/head@221303 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

usr.bin/man/man.sh
@@ -889,16 +889,18 @@ do_whatis() {
 	search_whatis whatis "$@"
 }
 
-EQN=/usr/bin/eqn
+# User's PATH setting decides on the groff-suite to pick up.
+EQN=eqn
+NROFF='groff -S -P-c -Wall -mtty-char -man'
+PIC=pic
+REFER=refer
+TBL=tbl
+TROFF='groff -S -P-c -man'
+VGRIND=vgrind
+
 COL=/usr/bin/col
 LOCALE=/usr/bin/locale
-NROFF='/usr/bin/groff -S -Wall -mtty-char -man'
-PIC=/usr/bin/pic
 SYSCTL=/sbin/sysctl
-TBL=/usr/bin/tbl
-TROFF='/usr/bin/groff -S -man'
-REFER=/usr/bin/refer
-VGRIND=/usr/bin/vgrind
 
 debug=0
 man_default_sections='1:1aout:8:2:3:n:4:5:6:7:9:l'

commit 0174664cc8ce97ef3f63ee541f1f1e2488aadf67

Fix man -t by not passing grotty flags to groff when grotty is not involved.
This fixes a regression introduced with r221303.

Noticed by:	jilles

usr.bin/man/man.sh
@@ -895,7 +895,7 @@ NROFF='groff -S -P-c -Wall -mtty-char -man'
 PIC=pic
 REFER=refer
 TBL=tbl
-TROFF='groff -S -P-c -man'
+TROFF='groff -S -man'
 VGRIND=vgrind
 
 COL=/usr/bin/col