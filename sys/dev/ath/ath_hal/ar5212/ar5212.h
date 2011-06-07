6048512cb91a335cce353f441c477b24d242bd03
adrian <adrian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-06-07 09:03:28 +0000

    Flesh out a new HAL method to fetch the radar PHY error frame information.
    For the AR5211/AR5212, this is apparently a one byte pulse duration
    counter value. It is only coded up here for the AR5212 as I don't have
    any AR5211-series hardware to test it on.
    
    This information was extracted from the Madwifi DFS branch along with
    some local additions.
    
    Please note - all this does is extract out the radar event duration,
    it in no way reflects the presence of a radar. Further code is needed
    to take a set of radar events and filter them to extract out correct
    radar pulse trains (and ignore other events.)
    
    For further information, please see:
    
    http://wiki.freebsd.org/dev/ath_hal%284%29/RadarDetection
    
    This includes references to the relevant patents which describe what
    is going on.
    
    Obtained from:	Madwifi
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@222815 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ath/ath_hal/ar5212/ar5212.h
+++ sys/dev/ath/ath_hal/ar5212/ar5212.h
@@ -622,5 +622,8 @@ extern	HAL_BOOL ar5212IsNFCalInProgress(struct ath_hal *ah);
 extern	HAL_BOOL ar5212WaitNFCalComplete(struct ath_hal *ah, int i);
 extern	void ar5212EnableDfs(struct ath_hal *ah, HAL_PHYERR_PARAM *pe);
 extern	void ar5212GetDfsThresh(struct ath_hal *ah, HAL_PHYERR_PARAM *pe);
+extern	HAL_BOOL ar5212ProcessRadarEvent(struct ath_hal *ah,
+	    struct ath_rx_status *rxs, uint64_t fulltsf, const char *buf,
+	    HAL_DFS_EVENT *event);
 
 #endif	/* _ATH_AR5212_H_ */
