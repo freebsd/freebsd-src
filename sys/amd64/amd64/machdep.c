0a13ac72691420662b02590ab99038a580f0dd66
Dmitry Chagin <dchagin@FreeBSD.org>
2011-06-08 21:39:00 +0000

    MFC r221784:
    Remove wrong comment.

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -1278,9 +1278,6 @@ add_smap_entry(struct bios_smap *smap, vm_paddr_t *physmap, int *physmap_idxp)
  * available physical memory in the system, then test this memory and
  * build the phys_avail array describing the actually-available memory.
  *
- * If we cannot accurately determine the physical memory map, then use
- * value from the 0xE801 call, and failing that, the RTC.
- *
  * Total memory size may be set by the kernel environment variable
  * hw.physmem or the compile-time define MAXMEM.
  *
