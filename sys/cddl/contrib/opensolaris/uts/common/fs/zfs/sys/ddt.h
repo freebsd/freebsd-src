58f2f35e729e4a3caee336321e87373d6fad4cd5
gibbs <gibbs@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-06-10 20:10:30 +0000

    Remove C constructs that are incompatible with C++ from various OpenSolaris and ZFS header files.  These changes are sufficient to allow a C++ program to use the libzfs library.
    Note: The majority of these files already included 'extern "C"'
          declarations, so the intention of providing C++ compatibility
          already existed even if it wasn't provided.
    
    cddl/compat/opensolaris/include/assert.h:
    	Wrap our compatibility assert implementation in
    	'extern "C"'.  Since this is a compatibility header
    	I matched the Solaris style of doing this explicitly
    	rather than rely on FreeBSD's __BEGIN/END_DECLS macro.
    
    sys/cddl/compat/opensolaris/sys/kstat.h:
    sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/arc.h:
    sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/dsl_pool.h:
    sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/ddt.h:
    sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/spa.h:
    sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/zio.h:
    	Rename parameters in function declarations that conflict
    	with C++ keywords.  This was the solution preferred by
    	members of the Illumos community.
    
    sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/zfs_ioctl.h:
    	In C, nested structures are visible in the global namespace,
    	but in C++, they take on the namespace of the structure in
    	which they are contained.  Flatten nested structure
    	definitions within struct zfs_cmd so these structures are
    	visible in the global namespace when compiled in both
    	languages.
    
    Sponsored by:	 Spectra Logic Corporation
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@222950 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/ddt.h
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/ddt.h
@@ -169,15 +169,15 @@ typedef struct ddt_ops {
 #define	DDT_NAMELEN	80
 
 extern void ddt_object_name(ddt_t *ddt, enum ddt_type type,
-    enum ddt_class class, char *name);
+    enum ddt_class cls, char *name);
 extern int ddt_object_walk(ddt_t *ddt, enum ddt_type type,
-    enum ddt_class class, uint64_t *walk, ddt_entry_t *dde);
+    enum ddt_class cls, uint64_t *walk, ddt_entry_t *dde);
 extern uint64_t ddt_object_count(ddt_t *ddt, enum ddt_type type,
-    enum ddt_class class);
+    enum ddt_class cls);
 extern int ddt_object_info(ddt_t *ddt, enum ddt_type type,
-    enum ddt_class class, dmu_object_info_t *);
+    enum ddt_class cls, dmu_object_info_t *);
 extern boolean_t ddt_object_exists(ddt_t *ddt, enum ddt_type type,
-    enum ddt_class class);
+    enum ddt_class cls);
 
 extern void ddt_bp_fill(const ddt_phys_t *ddp, blkptr_t *bp,
     uint64_t txg);
@@ -235,7 +235,7 @@ extern void ddt_unload(spa_t *spa);
 extern void ddt_sync(spa_t *spa, uint64_t txg);
 extern int ddt_walk(spa_t *spa, ddt_bookmark_t *ddb, ddt_entry_t *dde);
 extern int ddt_object_update(ddt_t *ddt, enum ddt_type type,
-    enum ddt_class class, ddt_entry_t *dde, dmu_tx_t *tx);
+    enum ddt_class cls, ddt_entry_t *dde, dmu_tx_t *tx);
 
 extern const ddt_ops_t ddt_zap_ops;
 
