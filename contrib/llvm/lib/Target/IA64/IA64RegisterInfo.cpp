cec6f3af5aae3baea7513c2d1b766dea0126188d
marcel <marcel@FreeBSD.org>
2011-06-11 17:23:51 +0000

    Implement IA64RegisterInfo::getReservedRegs()

--- contrib/llvm/lib/Target/IA64/IA64RegisterInfo.cpp
+++ contrib/llvm/lib/Target/IA64/IA64RegisterInfo.cpp
@@ -32,7 +32,19 @@ IA64RegisterInfo::getCalleeSavedRegs(const MachineFunction *MF) const
 BitVector
 IA64RegisterInfo::getReservedRegs(const MachineFunction &MF) const
 {
-  llvm_unreachable(__func__);
+  BitVector rsvd(getNumRegs());
+
+  rsvd.set(IA64::R0); // constant, hardwired 0
+  rsvd.set(IA64::R1); // global data pointer (aka gp)
+  rsvd.set(IA64::R12); // (memory) stack pointer (aka sp)
+  rsvd.set(IA64::R13); // thread pointer (aka tp)
+
+  rsvd.set(IA64::F0); // constant, hardwired 0.0
+  rsvd.set(IA64::F1); // constant, hardwired 1.0
+
+  rsvd.set(IA64::P0); // constant, hardwired 1 (true)
+
+  return rsvd;
 }
 
 void
