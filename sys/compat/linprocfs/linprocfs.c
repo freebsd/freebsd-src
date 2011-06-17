68ffe4e142184151b0da9f0109b151c49509dfd1
pluknet <pluknet@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-06-17 07:30:56 +0000

    Return empty cmdline/environ string for processes with kernel address space. This is consistent with the behavior in linux.
    PR:		kern/157871
    Reported by:	Petr Salinger <Petr Salinger att seznam cz>
    Verified on:	GNU/kFreeBSD debian 8.2-1-amd64 (by reporter)
    Reviewed by:	kib (some time ago)
    MFC after:	2 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@223182 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/compat/linprocfs/linprocfs.c
+++ sys/compat/linprocfs/linprocfs.c
@@ -1049,6 +1049,15 @@ linprocfs_doproccmdline(PFS_FILL_ARGS)
 		PROC_UNLOCK(p);
 		return (ret);
 	}
+
+	/*
+	 * Mimic linux behavior and pass only processes with usermode
+	 * address space as valid.  Return zero silently otherwize.
+	 */
+	if (p->p_vmspace == &vmspace0) {
+		PROC_UNLOCK(p);
+		return (0);
+	}
 	if (p->p_args != NULL) {
 		sbuf_bcpy(sb, p->p_args->ar_args, p->p_args->ar_length);
 		PROC_UNLOCK(p);
@@ -1072,6 +1081,15 @@ linprocfs_doprocenviron(PFS_FILL_ARGS)
 	if ((ret = p_cansee(td, p)) != 0) {
 		PROC_UNLOCK(p);
 		return (ret);
+	}
+
+	/*
+	 * Mimic linux behavior and pass only processes with usermode
+	 * address space as valid.  Return zero silently otherwize.
+	 */
+	if (p->p_vmspace == &vmspace0) {
+		PROC_UNLOCK(p);
+		return (0);
 	}
 	PROC_UNLOCK(p);
 
