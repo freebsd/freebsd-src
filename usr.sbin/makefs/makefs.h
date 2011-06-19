484b5c257d32ad2e6b00aa536e8806bae740f1d4
Marcel Moolenaar <marcel@FreeBSD.org>
2011-06-19 18:34:49 +0000

    Add support for using mtree(5) manifest files to define the image to be created. The support is based on mtree version 2.0, as used in libarchive, but adds new features on top of it.
    The current implementation is fully functional, but is envisioned
    to grow at least the following additional features over time:
    o   Add support for the /include special command so that manifest
        files can be constructed using includable fragments.
    o   Add support specifying a search path to locate content files.
    o   Content file filters: commands that provide file contents on
        stdout.
    
    The manifest file eliminates the need to first construct a tree
    as root in order to create an image and allows images (releases)
    to be created directly from object trees and/or source trees.
    
    Reviewed by:	deo
    Sponsored by:	Juniper Networks, Inc

--- usr.sbin/makefs/makefs.h
+++ usr.sbin/makefs/makefs.h
@@ -93,11 +93,13 @@ typedef struct _fsnode {
 	uint32_t	 type;		/* type of entry */
 	fsinode		*inode;		/* actual inode data */
 	char		*symlink;	/* symlink target */
+	char		*contents;	/* file to provide contents */
 	char		*name;		/* file name */
 	int		flags;		/* misc flags */
 } fsnode;
 
 #define	FSNODE_F_HASSPEC	0x01	/* fsnode has a spec entry */
+#define	FSNODE_F_OPTIONAL	0x02	/* fsnode is optional */
 
 /*
  * fsinfo_t - contains various settings and parameters pertaining to
@@ -147,6 +149,7 @@ typedef struct {
 void		apply_specfile(const char *, const char *, fsnode *, int);
 void		dump_fsnodes(const char *, fsnode *);
 const char *	inode_type(mode_t);
+fsnode *	read_mtree(const char *, fsnode *);
 int		set_option(option_t *, const char *, const char *);
 fsnode *	walk_dir(const char *, fsnode *);
 void		free_fsnodes(fsnode *);
