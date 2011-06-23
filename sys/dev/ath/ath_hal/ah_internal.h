de5d2ef6140fa8edbd997bcacb0f51ec0a995e71
adrian <adrian@FreeBSD.org>
2011-06-23 12:11:43 +0000

    add missing #define for the non-debug case.

--- sys/dev/ath/ath_hal/ah_internal.h
+++ sys/dev/ath/ath_hal/ah_internal.h
@@ -529,6 +529,7 @@ extern	void DO_HALDEBUG(struct ath_hal *ah, u_int mask, const char* fmt, ...)
 	__printflike(3,4);
 #else
 #define HALDEBUG(_ah, __m, _fmt, ...)
+#define HALDEBUG_G(_ah, __m, _fmt, ...)
 #endif /* AH_DEBUG */
 
 /*
