37931a35441cec30c949e1bb878f04a1b4de7302
Adrian Chadd <adrian@FreeBSD.org>
2011-06-23 02:38:36 +0000

    Break out most of the HAL related tweaks into a per-HAL instance, rather than global variables.
    This specifically allows for debugging to be enabled per-NIC, rather
    than globally.
    
    Since the ath driver doesn't know about AH_DEBUG, and to keep the ABI
    consistent regardless of whether AH_DEBUG is enabled or not, enable the
    debug parameter always but only conditionally compile in the debug
    methods if needed.
    
    The ALQ support is currently still global pending some brainstorming.
    
    Submitted by:	ssgriffonuser@gmail.com
    Reviewed by:	adrian, bschmidt

--- sys/dev/ath/ath_hal/ar5211/ar5211_beacon.c
+++ sys/dev/ath/ath_hal/ar5211/ar5211_beacon.c
@@ -71,9 +71,9 @@ ar5211BeaconInit(struct ath_hal *ah,
 	case HAL_M_IBSS:
 	case HAL_M_HOSTAP:
 		bt.bt_nextdba = (next_beacon -
-			ath_hal_dma_beacon_response_time) << 3;	/* 1/8 TU */
+			ah->ah_config.ah_dma_beacon_response_time) << 3;	/* 1/8 TU */
 		bt.bt_nextswba = (next_beacon -
-			ath_hal_sw_beacon_response_time) << 3;	/* 1/8 TU */
+            ah->ah_config.ah_sw_beacon_response_time) << 3;	/* 1/8 TU */
 		break;
 	}
 	/*
