be82109a76fe7050443dbcbb99516bbf8d7b32f9
marcel <marcel@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-06-25 02:15:14 +0000

    Switch to the event timers infrastructure. This includes: o   Setting td_intr_frame to the XIVs trap frame because it's referenced     by the ET event handler. o   Signal EOI to the CPU before calling the registered XIV handlers.     This prevents lost ITC interrupts, which cause starvation in one-shot     mode. o   Adding support for IPI_HARDCLOCK with corresponding per-CPU counters. o   Have the APs call cpu_initclocks() so as to limited the scattering of     clock related initialization. cpu_initclocks() calls the <self>_bsp()     or <self>_ap() version accordingly. o   Uncomment the ET clock handling in cpu_idle(). o   Update the DDB 'show pcpu' output for the new MD fields. o   Entirely rewritten ia64_ih_clock(). Note that we don't create as many     clock XIVs as we have CPUs, as is done on PowerPC. It doesn't scale.     We can only have 240 XIVs and we can have more CPUs than that. There's     a single intrcnt index for the cumulative clock ticks and we keep per     CPU counts in the PCPU stats structure. o   Register the ITC by hooking SI_SUB_CONFIGURE (2nd order).
    Open issues:
    o   Clock interrupts can still be lost. Some tweaking is still necessary.
    
    Thanks to: mav@ for his support, feedback and explanations.
    
    ET stats while committing:
    eris% sysctl machdep.cpu | grep nclks
    
    machdep.cpu.0.nclks: 24007
    machdep.cpu.1.nclks: 22895
    machdep.cpu.2.nclks: 13523
    machdep.cpu.3.nclks: 9342
    machdep.cpu.4.nclks: 9103
    machdep.cpu.5.nclks: 9298
    machdep.cpu.6.nclks: 10039
    machdep.cpu.7.nclks: 9479
    eris% vmstat -i | grep clock
    clock                      108599         50
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@223526 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/ia64/ia64/machdep.c
+++ sys/ia64/ia64/machdep.c
@@ -347,6 +347,11 @@ cpu_startup(void *dummy)
 
 		SYSCTL_ADD_ULONG(&pc->pc_md.sysctl_ctx,
 		    SYSCTL_CHILDREN(pc->pc_md.sysctl_tree), OID_AUTO,
+		    "nhardclocks", CTLFLAG_RD, &pcs->pcs_nhardclocks,
+		    "Number of IPI_HARDCLOCK interrupts");
+
+		SYSCTL_ADD_ULONG(&pc->pc_md.sysctl_ctx,
+		    SYSCTL_CHILDREN(pc->pc_md.sysctl_tree), OID_AUTO,
 		    "nhighfps", CTLFLAG_RD, &pcs->pcs_nhighfps,
 		    "Number of IPI_HIGH_FP interrupts");
 
@@ -416,12 +421,10 @@ cpu_idle(int busy)
 {
 	register_t ie;
 
-#if 0
 	if (!busy) {
 		critical_enter();
 		cpu_idleclock();
 	}
-#endif
 
 	ie = intr_disable();
 	KASSERT(ie != 0, ("%s called with interrupts disabled\n", __func__));
@@ -436,12 +439,10 @@ cpu_idle(int busy)
 		ia64_enable_intr();
 	}
 
-#if 0
 	if (!busy) {
 		cpu_activeclock();
 		critical_exit();
 	}
-#endif
 }
 
 int
