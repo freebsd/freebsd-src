cae42783ef45821e07f7c6b05d1b5bd49aef8d0c
alc <alc@FreeBSD.org>
2011-07-02 23:42:04 +0000

    When iterating over a paging queue, explicitly check for PG_MARKER, instead of relying on zeroed memory being interpreted as an empty PV list.
    Reviewed by:	kib

--- sys/amd64/amd64/pmap.c
+++ sys/amd64/amd64/pmap.c
@@ -2104,7 +2104,7 @@ pmap_collect(pmap_t locked_pmap, struct vpgqueues *vpq)
 	vm_page_t m, free;
 
 	TAILQ_FOREACH(m, &vpq->pl, pageq) {
-		if (m->hold_count || m->busy)
+		if ((m->flags & PG_MARKER) != 0 || m->hold_count || m->busy)
 			continue;
 		TAILQ_FOREACH_SAFE(pv, &m->md.pv_list, pv_list, next_pv) {
 			va = pv->pv_va;
