b0d3e1fedc7560459d5ddcff80e571c6c7621ca1
dougb <dougb@FreeBSD.org>
2011-07-06 00:50:27 +0000

    Update relevant to the 9.6-ESV-R4-P3 release

--- lib/bind/config.h
+++ lib/bind/config.h
@@ -19,7 +19,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: acconfig.h,v 1.51.334.2 2009/02/16 23:47:15 tbox Exp $ */
+/* $Id: acconfig.h,v 1.51.334.2 2009-02-16 23:47:15 tbox Exp $ */
 
 /*! \file */
 
@@ -146,9 +146,6 @@ int sigwait(const unsigned int *set, int *sig);
 
 /* Define if threads need PTHREAD_SCOPE_SYSTEM */
 /* #undef NEED_PTHREAD_SCOPE_SYSTEM */
-
-/* Define if building universal (internal helper macro) */
-/* #undef AC_APPLE_UNIVERSAL_BUILD */
 
 /* Define if recvmsg() does not meet all of the BSD socket API specifications.
    */
@@ -330,9 +327,6 @@ int sigwait(const unsigned int *set, int *sig);
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME ""
 
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
 /* Define to the version of this package. */
 #define PACKAGE_VERSION ""
 
@@ -354,15 +348,11 @@ int sigwait(const unsigned int *set, int *sig);
 /* #undef WITH_IDN */
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
-   significant byte first (like Motorola and SPARC, unlike Intel). */
-#if defined AC_APPLE_UNIVERSAL_BUILD
-# if defined __BIG_ENDIAN__
-#  define WORDS_BIGENDIAN 1
-# endif
-#else
-# ifndef WORDS_BIGENDIAN
-/* #  undef WORDS_BIGENDIAN */
-# endif
+   significant byte first (like Motorola and SPARC, unlike Intel and VAX). */
+#if defined __BIG_ENDIAN__
+# define WORDS_BIGENDIAN 1
+#elif ! defined __LITTLE_ENDIAN__
+/* # undef WORDS_BIGENDIAN */
 #endif
 
 /* Define to empty if `const' does not conform to ANSI C. */
