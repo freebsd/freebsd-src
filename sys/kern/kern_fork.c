f3df3b84b226d41cd408d911e28f7051c81070f1
kib <kib@FreeBSD.org>
2011-07-12 20:37:18 +0000

    Implement an RFTSIGZMB flag to rfork(2) to specify a signal that is delivered to parent when the child exists.
    Submitted by:	Petr Salinger <Petr.Salinger seznam cz> (Debian/kFreeBSD)
    MFC after:	1 week
    X-MFC-note:	bump __FreeBSD_version

--- sys/kern/kern_fork.c
+++ sys/kern/kern_fork.c
@@ -476,7 +476,10 @@ do_fork(struct thread *td, int flags, struct proc *p2, struct thread *td2,
 		sigacts_copy(newsigacts, p1->p_sigacts);
 		p2->p_sigacts = newsigacts;
 	}
-	if (flags & RFLINUXTHPN) 
+
+	if (flags & RFTSIGZMB)
+	        p2->p_sigparent = RFTSIGNUM(flags);
+	else if (flags & RFLINUXTHPN)
 	        p2->p_sigparent = SIGUSR1;
 	else
 	        p2->p_sigparent = SIGCHLD;
@@ -719,8 +722,20 @@ fork1(struct thread *td, int flags, int pages, struct proc **procp)
 	static int curfail;
 	static struct timeval lastfail;
 
+	/* Check for the undefined or unimplemented flags. */
+	if ((flags & ~(RFFLAGS | RFTSIGFLAGS(RFTSIGMASK))) != 0)
+		return (EINVAL);
+
+	/* Signal value requires RFTSIGZMB. */
+	if ((flags & RFTSIGFLAGS(RFTSIGMASK)) != 0 && (flags & RFTSIGZMB) == 0)
+		return (EINVAL);
+
 	/* Can't copy and clear. */
 	if ((flags & (RFFDG|RFCFDG)) == (RFFDG|RFCFDG))
+		return (EINVAL);
+
+	/* Check the validity of the signal number. */
+	if ((flags & RFTSIGZMB) != 0 && (u_int)RFTSIGNUM(flags) > _SIG_MAXSIG)
 		return (EINVAL);
 
 	p1 = td->td_proc;
