8220ce4e0bf3a78b665d2dfb77bd69945d064e5b
jhb <jhb@FreeBSD.org>
2011-07-14 19:25:42 +0000

    MFC 223870,223937: - Note that -a, -C, -H, -j, and -z are also toggles. - Add a leading space to the status messages output after toggling the   'C' and 'H' flags at runtime.  This matches messages output for other   toggles which leave the first column in the message blank to hold the   cursor.

--- contrib/top/top.c
+++ contrib/top/top.c
@@ -1013,7 +1013,7 @@ restart:
 			    case CMD_thrtog:
 				ps.thread = !ps.thread;
 				new_message(MT_standout | MT_delayed,
-				    "Displaying threads %s",
+				    " Displaying threads %s",
 				    ps.thread ? "separately" : "as a count");
 				header_text = format_header(uname_field);
 				reset_display();
@@ -1022,7 +1022,7 @@ restart:
 			    case CMD_wcputog:
 				ps.wcpu = !ps.wcpu;
 				new_message(MT_standout | MT_delayed,
-				    "Displaying %sCPU",
+				    " Displaying %sCPU",
 				    ps.wcpu ? "W" : "");
 				header_text = format_header(uname_field);
 				reset_display();
