1a8cc2bad92d7f1305d57aac3ee82e82117aab70
bz <bz@FreeBSD.org>
2011-07-18 20:06:15 +0000

    Rename ki_ocomm to ki_tdname and OCOMMLEN to TDNAMLEN. Provide backward compatibility defines under BURN_BRIDGES.
    Suggested by:	jhb
    Reviewed by:	emaste
    Sponsored by:	Sandvine Incorporated
    Approved by:	re (kib)

--- bin/ps/print.c
+++ bin/ps/print.c
@@ -136,7 +136,7 @@ command(KINFO *k, VARENT *ve)
 				(void)printf("%s", k->ki_d.prefix);
 			(void)printf("%s", k->ki_p->ki_comm);
 			if (showthreads && k->ki_p->ki_numthreads > 1)
-				(void)printf("/%s", k->ki_p->ki_ocomm);
+				(void)printf("/%s", k->ki_p->ki_tdname);
 		} else
 			(void)printf("%-*s", v->width, k->ki_p->ki_comm);
 		return;
@@ -190,7 +190,7 @@ command(KINFO *k, VARENT *ve)
 void
 ucomm(KINFO *k, VARENT *ve)
 {
-	char tmpbuff[COMMLEN + OCOMMLEN + 2];
+	char tmpbuff[COMMLEN + TDNAMLEN + 2];
 	VAR *v;
 
 	v = ve->var;
@@ -199,12 +199,12 @@ ucomm(KINFO *k, VARENT *ve)
 			(void)printf("%s", k->ki_d.prefix);
 		(void)printf("%s", k->ki_p->ki_comm);
 		if (showthreads && k->ki_p->ki_numthreads > 1)
-			printf("/%s", k->ki_p->ki_ocomm);
+			printf("/%s", k->ki_p->ki_tdname);
 	} else {
 		bzero(tmpbuff, sizeof(tmpbuff));
 		if (showthreads && k->ki_p->ki_numthreads > 1)
 			sprintf(tmpbuff, "%s/%s", k->ki_p->ki_comm,
-			    k->ki_p->ki_ocomm);
+			    k->ki_p->ki_tdname);
 		else
 			sprintf(tmpbuff, "%s", k->ki_p->ki_comm);
 		(void)printf("%-*s", v->width, tmpbuff);
@@ -218,7 +218,7 @@ tdnam(KINFO *k, VARENT *ve)
 
 	v = ve->var;
 	if (showthreads && k->ki_p->ki_numthreads > 1)
-		(void)printf("%-*s", v->width, k->ki_p->ki_ocomm);
+		(void)printf("%-*s", v->width, k->ki_p->ki_tdname);
 	else
 		(void)printf("%-*s", v->width, "      ");
 }
@@ -943,12 +943,12 @@ loginclass(KINFO *k, VARENT *ve)
 int
 s_comm(KINFO *k)
 {
-	char tmpbuff[COMMLEN + OCOMMLEN + 2];
+	char tmpbuff[COMMLEN + TDNAMLEN + 2];
 
 	bzero(tmpbuff, sizeof(tmpbuff));
 	if (showthreads && k->ki_p->ki_numthreads > 1)
 		sprintf(tmpbuff, "%s/%s", k->ki_p->ki_comm,
-		    k->ki_p->ki_ocomm);
+		    k->ki_p->ki_tdname);
 	else
 		sprintf(tmpbuff, "%s", k->ki_p->ki_comm);
 	return (strlen(tmpbuff));
