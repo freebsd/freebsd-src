c4871972d9910ce9c24dad16b45807032290e840
marius <marius@FreeBSD.org>
2011-07-25 17:54:04 +0000

    MFC: r222828 (partial)
    - Test KTR_CPUMASK against pc_cpuid rather than pc_mid to match what the C
      version does.
    - Initialize global registers early in the MP startup code so PCPU can be
      used.

--- sys/sparc64/sparc64/mp_machdep.c
+++ sys/sparc64/sparc64/mp_machdep.c
@@ -451,9 +451,6 @@ cpu_mp_bootstrap(struct pcpu *pc)
 	 */
 	tlb_flush_nonlocked();
 
-	/* Initialize global registers. */
-	cpu_setregs(pc);
-
 	/*
 	 * Enable interrupts.
 	 * Note that the PIL we be lowered indirectly via sched_throw(NULL)
