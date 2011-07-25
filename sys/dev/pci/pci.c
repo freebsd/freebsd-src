8a25ced86762ca10e4d6590f813a2b26a9e2002d
marius <marius@FreeBSD.org>
2011-07-25 19:11:29 +0000

    MFC: r223984
    PCIB_ALLOC_MSIX() may already fail on the first pass, f.e. when the PCI-PCI
    bridge is blacklisted. In that case just return from pci_alloc_msix_method(),
    otherwise we continue without a single MSI-X resource, causing subsequent
    attempts to use the seemingly available resource to fail or when booting
    verbose to NULL-dereference rle->start when trying to print the IRQ in
    pci_alloc_msix_method().
    
    Reviewed by:	jhb

--- sys/dev/pci/pci.c
+++ sys/dev/pci/pci.c
@@ -1294,8 +1294,11 @@ pci_alloc_msix_method(device_t dev, device_t child, int *count)
 	for (i = 0; i < max; i++) {
 		/* Allocate a message. */
 		error = PCIB_ALLOC_MSIX(device_get_parent(dev), child, &irq);
-		if (error)
+		if (error) {
+			if (i == 0)
+				return (error);
 			break;
+		}
 		resource_list_add(&dinfo->resources, SYS_RES_IRQ, i + 1, irq,
 		    irq, 1);
 	}
