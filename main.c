64b692f13378f3768622419bdbe9afa5525507e7
ru <ru@FreeBSD.org>
2011-08-11 04:58:20 +0000

    Vendor import of bwk's 10-Aug-2011 release.

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20110807";
+const char	*version = "version 20110810";
 
 #define DEBUG
 #include <stdio.h>
