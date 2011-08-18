f2171c2f0748ae8111ac9085af0ce5f36523b583
adrian <adrian@FreeBSD.org>
2011-08-18 03:44:32 +0000

    Fix a locking bug I introduced in the past.

--- sys/dev/ath/if_ath.c
+++ sys/dev/ath/if_ath.c
@@ -4434,8 +4434,8 @@ ath_tx_draintxq(struct ath_softc *sc, struct ath_txq *txq)
 		ATH_TXQ_LOCK(txq);
 		bf = STAILQ_FIRST(&txq->axq_q);
 		if (bf == NULL) {
-			ATH_TXQ_UNLOCK(txq);
 			txq->axq_link = NULL;
+			ATH_TXQ_UNLOCK(txq);
 			break;
 		}
 		ATH_TXQ_REMOVE_HEAD(txq, bf_list);
