cffc73a7ae98b8c481615de8f9a1be7dfb967243
bz <bz@FreeBSD.org>
2011-08-20 16:43:47 +0000

    Add an in6_localip() helper function as in6_localaddr() is not doing what people think: returning true for an address in any connected subnet, not necessarily on the local machine.
    Sponsored by:	Sandvine Incorporated
    MFC after:	2 weeks
    Approved by:	re (kib)

--- sys/netinet6/in6.h
+++ sys/netinet6/in6.h
@@ -631,6 +631,7 @@ struct cmsghdr;
 
 int	in6_cksum __P((struct mbuf *, u_int8_t, u_int32_t, u_int32_t));
 int	in6_localaddr __P((struct in6_addr *));
+int	in6_localip(struct in6_addr *);
 int	in6_addrscope __P((struct in6_addr *));
 struct	in6_ifaddr *in6_ifawithifp __P((struct ifnet *, struct in6_addr *));
 extern void in6_if_up __P((struct ifnet *));
