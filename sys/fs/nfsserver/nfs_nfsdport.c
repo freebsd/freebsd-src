17fb04566b4bea0b2804da2b7359dee8c69f722e
rmacklem <rmacklem@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-09-03 00:28:53 +0000

    Fix the NFS servers so that they can do a Lookup of "..", which requires that ni_strictrelative be set to 0, post-r224810.
    Tested by:	swills (earlier version), geo dot liaskos at gmail.com
    Approved by:	re (kib)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@225356 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/fs/nfsserver/nfs_nfsdport.c
+++ sys/fs/nfsserver/nfs_nfsdport.c
@@ -282,6 +282,7 @@ nfsvno_namei(struct nfsrv_descript *nd, struct nameidata *ndp,
 
 	*retdirp = NULL;
 	cnp->cn_nameptr = cnp->cn_pnbuf;
+	ndp->ni_strictrelative = 0;
 	/*
 	 * Extract and set starting directory.
 	 */
