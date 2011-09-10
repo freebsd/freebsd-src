72cd9688ae31dc5e4f9c2ca8294c9be6dbd5c570
hselasky <hselasky@FreeBSD.org>
2011-09-10 16:52:15 +0000

    MFC r225469:
    Refactor auto-quirk solution so that we break as few external
    drivers as possible.
    
    PR:		usb/160299
    Suggested by:	rwatson

--- sys/dev/usb/usb_device.h
+++ sys/dev/usb/usb_device.h
@@ -149,7 +149,6 @@ struct usb_device {
 
 	uint16_t power;			/* mA the device uses */
 	uint16_t langid;		/* language for strings */
-	uint16_t autoQuirk[USB_MAX_AUTO_QUIRK];		/* dynamic quirks */
 
 	uint8_t	address;		/* device addess */
 	uint8_t	device_index;		/* device index in "bus->devices" */
@@ -191,6 +190,8 @@ struct usb_device {
 #endif
 
 	uint32_t clear_stall_errors;	/* number of clear-stall failures */
+
+	uint16_t autoQuirk[USB_MAX_AUTO_QUIRK];		/* dynamic quirks */
 };
 
 /* globals */
