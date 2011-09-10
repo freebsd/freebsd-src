689f1774c2fab446a00ef1c9fc51b78c7b8d7a13
hselasky <hselasky@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-09-10 15:55:36 +0000

    Refactor auto-quirk solution so that we break as few external drivers as possible.
    PR:		usb/160299
    Approved by:	re (kib)
    Suggested by:	rwatson
    MFC after:	0 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@225469 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/usb/usb_dynamic.h
+++ sys/dev/usb/usb_dynamic.h
@@ -57,6 +57,5 @@ extern devclass_t usb_devclass_ptr;
 void	usb_temp_unload(void *);
 void	usb_quirk_unload(void *);
 void	usb_bus_unload(void *);
-usb_test_quirk_t usb_test_quirk_w;
 
 #endif					/* _USB_DYNAMIC_H_ */
