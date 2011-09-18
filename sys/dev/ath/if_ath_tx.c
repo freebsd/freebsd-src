e8dd42daab99083621baabfe96b8977f56c4f884
adrian <adrian@FreeBSD.org>
2011-09-18 15:11:54 +0000

    Make sure a paused queue gets some frames re-scheduled to the hardware once it's unpaused or it'll hang until the next TX'ed frame occurs.
    Pointy hat to:	adrian, for removing this call a while back and never
     remembering to restore it.

--- sys/dev/ath/if_ath_tx.c
+++ sys/dev/ath/if_ath_tx.c
@@ -2195,6 +2195,8 @@ ath_tx_tid_resume(struct ath_softc *sc, struct ath_tid *tid)
 	}
 
 	ath_tx_tid_sched(sc, tid);
+	/* Punt some frames to the hardware if needed */
+	ath_txq_sched(sc, sc->sc_ac2q[tid->ac]);
 }
 
 static void
