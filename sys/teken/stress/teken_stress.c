12a2a29a2d5c9a86d2c393338df1eceed066db5e
ed <ed@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-10-07 12:42:03 +0000

    Simply let teken_stress use arc4random.
    This makes it run quite a bit faster, since it makes system calls less
    often.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@226100 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/teken/stress/teken_stress.c
+++ sys/teken/stress/teken_stress.c
@@ -99,24 +99,14 @@ int
 main(int argc __unused, char *argv[] __unused)
 {
 	teken_t t;
-	int rnd;
 	unsigned int i, iteration = 0;
 	unsigned char buf[2048];
 
-	rnd = open("/dev/urandom", O_RDONLY);
-	if (rnd < 0) {
-		perror("/dev/urandom");
-		exit(1);
-	}
 
 	teken_init(&t, &tf, NULL);
 
 	for (;;) {
-		if (read(rnd, buf, sizeof buf) != sizeof buf) {
-			perror("read");
-			exit(1);
-		}
-
+		arc4random_buf(buf, sizeof buf);
 		for (i = 0; i < sizeof buf; i++) {
 			if (buf[i] >= 0x80)
 				buf[i] =
