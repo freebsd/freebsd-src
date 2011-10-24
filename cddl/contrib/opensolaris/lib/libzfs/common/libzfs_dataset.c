8411337f94345b4d2b7976d5c7e8dc12dadea674
Pawel Jakub Dawidek <pjd@FreeBSD.org>
2011-10-24 21:22:55 +0000

    Update copyright to include myself.
    MFC after:	2 weeks

--- cddl/contrib/opensolaris/lib/libzfs/common/libzfs_dataset.c
+++ cddl/contrib/opensolaris/lib/libzfs/common/libzfs_dataset.c
@@ -23,6 +23,8 @@
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright 2010 Nexenta Systems, Inc. All rights reserved.
  * Copyright (c) 2011 by Delphix. All rights reserved.
+ * Copyright (c) 2011 Pawel Jakub Dawidek <pawel@dawidek.net>.
+ * All rights reserved.
  */
 
 #include <ctype.h>
