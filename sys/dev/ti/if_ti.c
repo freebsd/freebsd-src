freebsd/freebsd-src

commit 2ef01b0e295afc19abb54d40a1cf92d64de9e280

Give MEXTADD() another argument to make both void pointers to the free function controlable, instead of passing the KVA of the buffer storage as the first argument.
Fix all conventional users of the API to pass the KVA of the buffer
as the first argument, to make this a no-op commit.

Likely break the only non-convetional user of the API, after informing
the relevant committer.

Update the mbuf(9) manual page, which was already out of sync on
this point.

Bump __FreeBSD_version to 800016 as there is no way to tell how
many arguments a CPP macro needs any other way.

This paves the way for giving sendfile(9) a way to wait for the
passed storage to have been accessed before returning.

This does not affect the memory layout or size of mbufs.

Parental oversight by:	sam and rwatson.

No MFC is anticipated.


git-svn-id: svn+ssh://svn.freebsd.org/base/head@175872 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/ti/if_ti.c
@@ -1503,7 +1503,8 @@ ti_newbuf_jumbo(sc, idx, m_old)
 			m[i]->m_data = (void *)sf_buf_kva(sf[i]);
 			m[i]->m_len = PAGE_SIZE;
 			MEXTADD(m[i], sf_buf_kva(sf[i]), PAGE_SIZE,
-			    sf_buf_mext, sf[i], 0, EXT_DISPOSABLE);
+			    sf_buf_mext, (void*)sf_buf_kva(sf[i]), sf[i],
+			    0, EXT_DISPOSABLE);
 			m[i]->m_next = m[i+1];
 		}
 		/* link the buffers to the header */

share/man/man9/mbuf.9
@@ -45,8 +45,9 @@
 .Fa "struct mbuf *mbuf"
 .Fa "caddr_t buf"
 .Fa "u_int size"
-.Fa "void (*free)(void *opt_args)"
-.Fa "void *opt_args"
+.Fa "void (*free)(void *opt_arg1, void *opt_arg2)"
+.Fa "void *opt_arg1"
+.Fa "void *opt_arg2"
 .Fa "short flags"
 .Fa "int type"
 .Fc

sys/compat/ndis/kern_ndis.c
@@ -734,7 +734,7 @@ ndis_ptom(m0, p)
 		m->m_len = MmGetMdlByteCount(buf);
 		m->m_data = MmGetMdlVirtualAddress(buf);
 		MEXTADD(m, m->m_data, m->m_len, ndis_return_packet,
-		    p, 0, EXT_NDIS);
+		    m->m_data, p, 0, EXT_NDIS);
 		p->np_refcnt++;
 
 		totlen += m->m_len;

sys/dev/cxgb/cxgb_sge.c
@@ -2452,7 +2452,8 @@ init_cluster_mbuf(caddr_t cl, int flags, int type, uma_zone_t zone)
 	m->m_ext.ref_cnt = (uint32_t *)(cl + header_size - sizeof(uint32_t));
 	m->m_ext.ext_size = m_getsizefromtype(type);
 	m->m_ext.ext_free = ext_free_handler;
-	m->m_ext.ext_args = (void *)(uintptr_t)type;
+	m->m_ext.ext_arg1 = cl;
+	m->m_ext.ext_arg2 = (void *)(uintptr_t)type;
 	m->m_ext.ext_type = EXT_EXTREF;
 	*(m->m_ext.ref_cnt) = 1;
 	DPRINTF("data=%p ref_cnt=%p\n", m->m_data, m->m_ext.ref_cnt); 

sys/dev/cxgb/ulp/tom/cxgb_cpl_socket.c
@@ -224,7 +224,8 @@ cxgb_vm_page_to_miov(struct toepcb *toep, struct uio *uio, struct mbuf **m)
 	m0->m_flags = (M_EXT|M_NOFREE);
 	m0->m_ext.ext_type = EXT_EXTREF;
 	m0->m_ext.ext_free = cxgb_zero_copy_free;
-	m0->m_ext.ext_args = NULL;
+	m0->m_ext.ext_arg1 = NULL;	/* XXX: probably wrong /phk */
+	m0->m_ext.ext_arg2 = NULL;
     
 	mv = mtomv(m0);
 	mv->mv_count = seg_count;

sys/dev/hatm/if_hatm_intr.c
@@ -458,7 +458,7 @@ hatm_rx_buffer(struct hatm_softc *sc, u_int group, u_int handle)
 		if (m != NULL) {
 			m->m_ext.ref_cnt = &c0->hdr.ref_cnt;
 			MEXTADD(m, (void *)c0, MBUF0_SIZE,
-			    hatm_mbuf0_free, sc, M_PKTHDR, EXT_EXTREF);
+			    hatm_mbuf0_free, c0, sc, M_PKTHDR, EXT_EXTREF);
 			m->m_data += MBUF0_OFFSET;
 		} else
 			hatm_mbuf0_free(c0, sc);
@@ -482,7 +482,7 @@ hatm_rx_buffer(struct hatm_softc *sc, u_int group, u_int handle)
 		if (m != NULL) {
 			m->m_ext.ref_cnt = &c1->hdr.ref_cnt;
 			MEXTADD(m, (void *)c1, MBUF1_SIZE,
-			    hatm_mbuf1_free, sc, M_PKTHDR, EXT_EXTREF);
+			    hatm_mbuf1_free, c1, sc, M_PKTHDR, EXT_EXTREF);
 			m->m_data += MBUF1_OFFSET;
 		} else
 			hatm_mbuf1_free(c1, sc);

sys/dev/iscsi/initiator/isc_soc.c
@@ -164,7 +164,7 @@ isc_sendPDU(isc_session_t *sp, pduq_t *pq)
 		md->m_ext.ref_cnt = &nil_refcnt;
                 l = min(MCLBYTES, len);
                 MEXTADD(md, pp->ds + off, l, nil_fn,
-                        NULL, 0, EXT_EXTREF);
+                        pp->ds + off, NULL, 0, EXT_EXTREF);
                 md->m_len = l;
                 md->m_next = NULL;
                 mh->m_pkthdr.len += l;

sys/dev/lge/if_lge.c
@@ -719,7 +719,7 @@ lge_newbuf(sc, c, m)
 		m_new->m_data = (void *)buf;
 		m_new->m_len = m_new->m_pkthdr.len = LGE_JUMBO_FRAMELEN;
 		MEXTADD(m_new, buf, LGE_JUMBO_FRAMELEN, lge_jfree,
-		    (struct lge_softc *)sc, 0, EXT_NET_DRV);
+		    buf, (struct lge_softc *)sc, 0, EXT_NET_DRV);
 	} else {
 		m_new = m;
 		m_new->m_len = m_new->m_pkthdr.len = LGE_JUMBO_FRAMELEN;

commit 0262ae8fdf62d3033e54395c1142febf479ce443

Make ti(4) build with 'options TI_PRIVATE_JUMBOS'. This was broken in r175872.
We have a UMA backed jumbo allocator and that is much better
implementation than having a local jumbo buffer allocator in
driver. This local allocator would be removed in near future but
fixing build before removal wouldn't be a bad idea.


git-svn-id: svn+ssh://svn.freebsd.org/base/head@227088 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/ti/if_ti.c
@@ -1328,7 +1328,7 @@ ti_newbuf_jumbo(struct ti_softc *sc, int i, struct mbuf *m)
 		/* Attach the buffer to the mbuf. */
 		m_new->m_data = (void *) buf;
 		m_new->m_len = m_new->m_pkthdr.len = TI_JUMBO_FRAMELEN;
-		MEXTADD(m_new, buf, TI_JUMBO_FRAMELEN, ti_jfree,
+		MEXTADD(m_new, buf, TI_JUMBO_FRAMELEN, ti_jfree, buf,
 		    (struct ti_softc *)sc, 0, EXT_NET_DRV);
 	} else {
 		m_new = m;