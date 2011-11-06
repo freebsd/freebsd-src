a2c6b647f175a4883ba5ab0f8ad139455fda88a0
ed <ed@FreeBSD.org>
2011-11-06 17:32:29 +0000

    Add missing static keywords to at(1).
    While there, tidy up the privs.h part, where at.c has to #define to
    declare some globals. Also group static and non-static global variables
    in at.c.

--- usr.bin/at/at.c
+++ usr.bin/at/at.c
@@ -63,8 +63,6 @@ __FBSDID("$FreeBSD$");
 #include "panic.h"
 #include "parsetime.h"
 #include "perm.h"
-
-#define MAIN
 #include "privs.h"
 
 /* Macros */
@@ -90,22 +88,22 @@ enum { ATQ, ATRM, AT, BATCH, CAT };	/* what program we want to run */
 
 /* File scope variables */
 
-const char *no_export[] =
-{
+static const char *no_export[] = {
     "TERM", "TERMCAP", "DISPLAY", "_"
-} ;
+};
 static int send_mail = 0;
+static char *atinput = NULL;	/* where to get input from */
+static char atqueue = 0;	/* which queue to examine for jobs (atq) */
 
 /* External variables */
 
 extern char **environ;
 int fcreated;
 char atfile[] = ATJOB_DIR "12345678901234";
-
-char *atinput = (char*)0;	/* where to get input from */
-char atqueue = 0;		/* which queue to examine for jobs (atq) */
 char atverify = 0;		/* verify time instead of queuing job */
 char *namep;
+uid_t real_uid, effective_uid;
+gid_t real_gid, effective_gid;
 
 /* Function declarations */
 
