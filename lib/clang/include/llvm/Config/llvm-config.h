7231b00508ac8ab0a8726bfd384f75499dffa263
dim <dim@FreeBSD.org>
2011-11-06 14:07:23 +0000

    When one attempts to compile the tree with -march=i386, which also used to be gcc's default before r198344, calls to atomic builtins will not be expanded inline.  Instead, they will be generated as calls to external functions (e.g. __sync_fetch_and_add_N), leading to linking errors later on.
    Put in a seatbelt that disables use of atomic builtins in libstdc++ and
    llvm, when tuning specifically for the real i386 CPU.  This does not
    protect against all possible issues, but it is better than nothing.

--- lib/clang/include/llvm/Config/llvm-config.h
+++ lib/clang/include/llvm/Config/llvm-config.h
@@ -34,7 +34,9 @@
 /* #undef LLVM_ETCDIR */
 
 /* Has gcc/MSVC atomic intrinsics */
+#ifndef __tune_i386__
 #define LLVM_HAS_ATOMICS 1
+#endif
 
 /* Host triple we were built on */
 /* #undef LLVM_HOSTTRIPLE */
