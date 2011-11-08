a73ffc7e7244451b2af3f98fe82deef896488ce6
adrian <adrian@FreeBSD.org>
2011-11-08 14:34:01 +0000

    .. add missing include from an incorrect merge.
    Sponsored by:	Hobnob, Inc.

--- sys/net80211/ieee80211_sta.c
+++ sys/net80211/ieee80211_sta.c
@@ -50,6 +50,8 @@ __FBSDID("$FreeBSD$");
 #include <net/if.h>
 #include <net/if_media.h>
 #include <net/if_llc.h>
+#include <net/if_dl.h>
+#include <net/if_var.h>
 #include <net/ethernet.h>
 
 #include <net/bpf.h>
