ecc426c810405eb429b3d9eb684e7edaccb56eb2
John Baldwin <jhb@FreeBSD.org>
2011-11-09 18:26:19 +0000

    MFC 226748: - Add a new header for the x86 boot code that defines various structures   and constants related to the BIOS Enhanced Disk Drive Specification. - Use this header instead of magic numbers and various duplicate structure   definitions for doing I/O. - Use an actual structure for the request to fetch drive parameters in   drvsize() rather than a gross hack of a char array with some magic   size.  While here, change drvsize() to only pass the 1.1 version of   the structure and not request device path information.  If we want   device path information you have to set the length of the device   path information as an input (along with probably checking the actual   EDD version to see which size one should use as the device path   information is variable-length).  This fixes data smashing problems   from passing an EDD 3 structure to BIOSes supporting EDD 4.
    Approved by:	re (kib)

--- sys/boot/i386/libi386/bioscd.c
+++ sys/boot/i386/libi386/bioscd.c
@@ -48,6 +48,7 @@ __FBSDID("$FreeBSD$");
 
 #include <bootstrap.h>
 #include <btxv86.h>
+#include <edd.h>
 #include "libi386.h"
 
 #define BIOSCD_SECSIZE		2048
@@ -325,9 +326,9 @@ bc_read(int unit, daddr_t dblk, int blks, caddr_t dest)
 				v86int();
 			}
 
-			packet.len = 0x10;
+			packet.len = sizeof(struct edd_packet);
 			packet.count = x;
-			packet.offset = VTOPOFF(xp);
+			packet.off = VTOPOFF(xp);
 			packet.seg = VTOPSEG(xp);
 			packet.lba = dblk;
 			v86.ctl = V86_FLAGS;
