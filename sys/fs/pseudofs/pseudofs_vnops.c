03b8e21e334468eda11a7221a6e2963187faf5af
kib <kib@FreeBSD.org>
2011-11-16 18:41:59 +0000

    Fix build, use %d for int value formatting.

--- sys/fs/pseudofs/pseudofs_vnops.c
+++ sys/fs/pseudofs/pseudofs_vnops.c
@@ -834,7 +834,7 @@ pfs_readdir(struct vop_readdir_args *va)
 		free(pfsent, M_IOV);
 		i++;
 	}
-	PFS_TRACE(("%zd bytes", i * PFS_DELEN));
+	PFS_TRACE(("%d bytes", i * PFS_DELEN));
 	PFS_RETURN (error);
 }
 
