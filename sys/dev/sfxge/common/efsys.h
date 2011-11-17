a968a99d3d4d3688b25d3dec4bde1b92ac87afd9
marius <marius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-11-17 22:59:16 +0000

    Implement prefetch_read_{many,once}() for sparc64 and fix compilation on other !x86 architectures.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@227641 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/sfxge/common/efsys.h
+++ sys/dev/sfxge/common/efsys.h
@@ -97,10 +97,11 @@ extern "C" {
 MALLOC_DECLARE(M_SFXGE);
 
 /* Machine dependend prefetch wrappers */
-#if defined(__i386) || defined(__amd64)
+#if defined(__i386__) || defined(__amd64__)
 static __inline void
 prefetch_read_many(void *addr)
 {
+
 	__asm__(
 	    "prefetcht0 (%0)"
 	    :
@@ -110,10 +111,43 @@ prefetch_read_many(void *addr)
 static __inline void
 prefetch_read_once(void *addr)
 {
+
 	__asm__(
 	    "prefetchnta (%0)"
 	    :
 	    : "r" (addr));
+}
+#elif defined(__sparc64__)
+static __inline void
+prefetch_read_many(void *addr)
+{
+
+	__asm__(
+	    "prefetch [%0], 0"
+	    :
+	    : "r" (addr));
+}
+
+static __inline void
+prefetch_read_once(void *addr)
+{
+
+	__asm__(
+	    "prefetch [%0], 1"
+	    :
+	    : "r" (addr));
+}
+#else
+static __inline void
+prefetch_read_many(void *addr)
+{
+
+}
+
+static __inline void
+prefetch_read_once(void *addr)
+{
+
 }
 #endif
 
