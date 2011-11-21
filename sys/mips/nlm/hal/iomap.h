aa17a3bc326241d6ceb0475f64742f3ad3c0553a
jchandra <jchandra@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-11-21 08:12:36 +0000

    Merge XLP 3XX updates and related rework.
    * Update message station (CMS) code, read queue ids from PCI header.
    * Use interrupts to wakeup message handling threads on 3XX
    * Update PIC code, read interrupt information from PCI header instead
      of using fixed values.
    * Update PCI interrupt handling for the PIC change.
    * Update code for getting chip frequency, new code support XLP 3XX
    * Misc style(9) fixes
    
    In collaboration with: prabhath at netlogicmicro com (CMS/PIC)
                           venkatesh at netlogicmicro.com (PCI)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@227783 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/mips/nlm/hal/iomap.h
+++ sys/mips/nlm/hal/iomap.h
@@ -30,9 +30,9 @@
  */
 
 #ifndef __NLM_HAL_IOMAP_H__
-#define __NLM_HAL_IOMAP_H__
+#define	__NLM_HAL_IOMAP_H__
 
-#define	XLP_DEFAULT_IO_BASE		0x18000000
+#define	XLP_DEFAULT_IO_BASE             0x18000000
 #define	NMI_BASE			0xbfc00000
 #define	XLP_IO_CLK			133333333
 
@@ -68,17 +68,21 @@
 
 #define	XLP_IO_NAE_OFFSET(node)		XLP_HDR_OFFSET(node, 0, 3, 0)
 #define	XLP_IO_POE_OFFSET(node)		XLP_HDR_OFFSET(node, 0, 3, 1)
+#define	XLP_IO_SATA_OFFSET(node)	XLP_HDR_OFFSET(node, 0, 3, 2)
 
 #define	XLP_IO_CMS_OFFSET(node)		XLP_HDR_OFFSET(node, 0, 4, 0)
 
-#define	XLP_IO_DMA_OFFSET(node)		XLP_HDR_OFFSET(node, 0, 5, 1)
-#define	XLP_IO_SEC_OFFSET(node)		XLP_HDR_OFFSET(node, 0, 5, 2)
+#define	XLP_IO_DMA_OFFSET(node)		XLP_HDR_OFFSET(node, 0, 5, 0)
+#define	XLP_IO_SEC_OFFSET(node)		XLP_HDR_OFFSET(node, 0, 5, 1)
+#define	XLP_IO_RSA_OFFSET(node)		XLP_HDR_OFFSET(node, 0, 5, 2)
 #define	XLP_IO_CMP_OFFSET(node)		XLP_HDR_OFFSET(node, 0, 5, 3)
+#define	XLP_IO_SRIO_OFFSET(node)	XLP_HDR_OFFSET(node, 0, 5, 4)
+#define	XLP_IO_REGEX_OFFSET(node)	XLP_HDR_OFFSET(node, 0, 5, 5)
 
 #define	XLP_IO_UART_OFFSET(node, i)	XLP_HDR_OFFSET(node, 0, 6, i)
 #define	XLP_IO_UART0_OFFSET(node)	XLP_HDR_OFFSET(node, 0, 6, 0)
 #define	XLP_IO_UART1_OFFSET(node)	XLP_HDR_OFFSET(node, 0, 6, 1)
-#define	XLP_IO_I2C_OFFSET(node, i)	XLP_HDR_OFFSET(node, 0, 6, 2 + (i))
+#define	XLP_IO_I2C_OFFSET(node, i)	XLP_HDR_OFFSET(node, 0, 6, 2 + i)
 #define	XLP_IO_I2C0_OFFSET(node)	XLP_HDR_OFFSET(node, 0, 6, 2)
 #define	XLP_IO_I2C1_OFFSET(node)	XLP_HDR_OFFSET(node, 0, 6, 3)
 #define	XLP_IO_GPIO_OFFSET(node)	XLP_HDR_OFFSET(node, 0, 6, 4)
@@ -90,9 +94,9 @@
 #define	XLP_IO_NAND_OFFSET(node)	XLP_HDR_OFFSET(node, 0, 7, 1)
 #define	XLP_IO_SPI_OFFSET(node)		XLP_HDR_OFFSET(node, 0, 7, 2)
 /* SD flash */
-#define	XLP_IO_SD_OFFSET(node)		XLP_HDR_OFFSET(node, 0, 7, 3)
+#define	XLP_IO_SD_OFFSET(node)          XLP_HDR_OFFSET(node, 0, 7, 3)
 #define	XLP_IO_MMC_OFFSET(node, slot)   \
-	((XLP_IO_SD_OFFSET(node)) + (slot * 0x100) + XLP_IO_PCI_HDRSZ)
+		((XLP_IO_SD_OFFSET(node))+(slot*0x100)+XLP_IO_PCI_HDRSZ)
 
 /* PCI config header register id's */
 #define	XLP_PCI_CFGREG0			0x00
@@ -147,6 +151,45 @@
 
 extern uint64_t xlp_sys_base;
 extern uint64_t xlp_pic_base;
+
+static __inline__ int
+nlm_dev_exists(uint32_t devoffset)
+{
+	uint64_t pcibase = nlm_pcicfg_base(devoffset);
+
+	return (nlm_read_reg(pcibase, XLP_PCI_CFGREG0) != 0xffffffff);
+}
+
+static __inline__ int
+nlm_qidstart(uint64_t pcibase)
+{
+	return (nlm_read_reg(pcibase, XLP_PCI_MSGSTN_REG) & 0xffff);
+}
+
+static __inline__ int
+nlm_qnum(uint64_t pcibase)
+{
+	return (nlm_read_reg(pcibase, XLP_PCI_MSGSTN_REG) >> 16);
+}
+
+static __inline__ int
+nlm_irtstart(uint64_t pcibase)
+{
+	return (nlm_read_reg(pcibase, XLP_PCI_IRTINFO_REG) & 0xffff);
+}
+
+static __inline__ int
+nlm_irtnum(uint64_t pcibase)
+{
+	return (nlm_read_reg(pcibase, XLP_PCI_IRTINFO_REG) >> 16);
+}
+
+static __inline__ int
+nlm_uenginenum(uint64_t pcibase)
+{
+	return nlm_read_reg(pcibase, XLP_PCI_UCODEINFO_REG);
+}
+
 #endif /* !LOCORE or !__ASSEMBLY */
 
 #endif /* __NLM_HAL_IOMAP_H__ */
