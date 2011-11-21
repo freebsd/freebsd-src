d42e80775b94e995fa8ce9d075772f5e6fd91ebd
jchandra <jchandra@FreeBSD.org>
2011-11-21 08:12:36 +0000

    Merge XLP 3XX updates and related rework.
    * Update message station (CMS) code, read queue ids from PCI header.
    * Use interrupts to wakeup message handling threads on 3XX
    * Update PIC code, read interrupt information from PCI header instead
      of using fixed values.
    * Update PCI interrupt handling for the PIC change.
    * Update code for getting chip frequency, new code support XLP 3XX
    * Misc style(9) fixes
    
    In collaboration with: prabhath at netlogicmicro com (CMS/PIC)
                           venkatesh at netlogicmicro.com (PCI)

--- sys/mips/nlm/hal/pcibus.h
+++ sys/mips/nlm/hal/pcibus.h
@@ -57,28 +57,16 @@
 
 #define	MSI_MIPS_DATA_INTVEC		0x000000ff
 
-/*
- * Build Intel MSI message and data values from a source.  AMD64 systems
- * seem to be compatible, so we use the same function for both.
- */
-#define	MIPS_MSI_ADDR(cpu)					\
-	(MSI_MIPS_ADDR_BASE | (cpu) << 12 |			\
-	MSI_MIPS_ADDR_RH_OFF | MSI_MIPS_ADDR_DM_PHYSICAL)
-
-#define MIPS_MSI_DATA(irq)					\
-	(MSI_MIPS_DATA_TRGRLVL | MSI_MIPS_DATA_DELFIXED |	\
-	MSI_MIPS_DATA_ASSERT | (irq))
-
-#define	PCIE_BRIDGE_CMD			0x1
-#define	PCIE_BRIDGE_MSI_CAP		0x14
-#define	PCIE_BRIDGE_MSI_ADDRL		0x15
-#define	PCIE_BRIDGE_MSI_ADDRH		0x16
-#define	PCIE_BRIDGE_MSI_DATA		0x17
+#define	PCIE_BRIDGE_CMD		0x1
+#define	PCIE_BRIDGE_MSI_CAP	0x14
+#define	PCIE_BRIDGE_MSI_ADDRL	0x15
+#define	PCIE_BRIDGE_MSI_ADDRH	0x16
+#define	PCIE_BRIDGE_MSI_DATA	0x17
 
 /* XLP Global PCIE configuration space registers */
-#define	PCIE_MSI_STATUS			0x25A
-#define	PCIE_MSI_EN			0x25B
-#define	PCIE_INT_EN0			0x261
+#define	PCIE_MSI_STATUS		0x25A
+#define	PCIE_MSI_EN		0x25B
+#define	PCIE_INT_EN0		0x261
 
 /* PCIE_MSI_EN */
 #define	PCIE_MSI_VECTOR_INT_EN		0xFFFFFFFF
@@ -86,4 +74,30 @@
 /* PCIE_INT_EN0 */
 #define	PCIE_MSI_INT_EN			(1 << 9)
 
+#if !defined(LOCORE) && !defined(__ASSEMBLY__)
+
+#define	nlm_read_pcie_reg(b, r)		nlm_read_reg(b, r)
+#define	nlm_write_pcie_reg(b, r, v)	nlm_write_reg(b, r, v)
+#define	nlm_get_pcie_base(node, inst)	\
+				nlm_pcicfg_base(XLP_IO_PCIE_OFFSET(node, inst))
+#define	nlm_get_pcie_regbase(node, inst)	\
+				(nlm_get_pcie_base(node, inst) + XLP_IO_PCI_HDRSZ)
+
+/*
+ * Build Intel MSI message and data values from a source.  AMD64 systems
+ * seem to be compatible, so we use the same function for both.
+ */
+#define	MIPS_MSI_ADDR(cpu)					       \
+        (MSI_MIPS_ADDR_BASE | (cpu) << 12 |			       \
+	 MSI_MIPS_ADDR_RH_OFF | MSI_MIPS_ADDR_DM_PHYSICAL)
+
+#define	MIPS_MSI_DATA(irq)					       \
+        (MSI_MIPS_DATA_TRGRLVL | MSI_MIPS_DATA_DELFIXED |	       \
+	 MSI_MIPS_DATA_ASSERT | (irq))
+
+#endif
+
+#ifndef LOCORE
+int xlp_pcie_link_irt(int link);
+#endif
 #endif /* __XLP_PCIBUS_H__ */
