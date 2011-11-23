0f30a4cf8906e3cbfa5895b21f8f9232f17e39bb
marius <marius@FreeBSD.org>
2011-11-23 20:27:26 +0000

    Use DEVMETHOD_END.

--- sys/dev/mii/gentbi.c
+++ sys/dev/mii/gentbi.c
@@ -95,7 +95,7 @@ static device_method_t gentbi_methods[] = {
 	DEVMETHOD(device_attach,	gentbi_attach),
 	DEVMETHOD(device_detach,	mii_phy_detach),
 	DEVMETHOD(device_shutdown,	bus_generic_shutdown),
-	{0, 0}
+	DEVMETHOD_END
 };
 
 static devclass_t gentbi_devclass;
