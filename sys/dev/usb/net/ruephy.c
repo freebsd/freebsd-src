355ff946c28e5d27831ccb67c70c3d121574cb58
marius <marius@FreeBSD.org>
2011-11-23 20:27:26 +0000

    Use DEVMETHOD_END.

--- sys/dev/usb/net/ruephy.c
+++ sys/dev/usb/net/ruephy.c
@@ -61,7 +61,7 @@ static device_method_t ruephy_methods[] = {
 	DEVMETHOD(device_attach,	ruephy_attach),
 	DEVMETHOD(device_detach,	mii_phy_detach),
 	DEVMETHOD(device_shutdown,	bus_generic_shutdown),
-	{ 0, 0 }
+	DEVMETHOD_END
 };
 
 static devclass_t ruephy_devclass;
