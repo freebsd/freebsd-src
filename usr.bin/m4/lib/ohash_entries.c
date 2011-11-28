a841e1ebe6ffc5da2bfe1237405d64baeed86ead
Baptiste Daroussin <bapt@FreeBSD.org>
2011-11-28 13:32:39 +0000

    Synchronize with laster version of m4 from OpenBSD and NetBSD This bring better compatibility with gnum4
    Reviewed by:	cognet
    Approved by:	cognet
    Obtained from:	OpenBSD, NetBSD

--- usr.bin/m4/lib/ohash_entries.c
+++ usr.bin/m4/lib/ohash_entries.c
@@ -1,6 +1,8 @@
-/* $OpenBSD: expr.c,v 1.18 2010/09/07 19:58:09 marco Exp $ */
-/*
- * Copyright (c) 2004 Marc Espie <espie@cvs.openbsd.org>
+/* $OpenBSD: ohash_entries.c,v 1.2 2004/06/22 20:00:16 espie Exp $ */
+/* ex:ts=8 sw=4: 
+ */
+
+/* Copyright (c) 1999, 2004 Marc Espie <espie@openbsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -17,31 +19,10 @@
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include <stdint.h>
-#include <stdio.h>
-#include <stddef.h>
-#include "mdef.h"
-#include "extern.h"
+#include "ohash_int.h"
 
-int32_t end_result;
-const char *copy_toeval;
-int yyerror(const char *msg);
-
-extern void yy_scan_string(const char *);
-extern int yyparse(void);
-
-int
-yyerror(const char *msg)
+unsigned int
+ohash_entries(struct ohash *h)
 {
-	fprintf(stderr, "m4: %s in expr %s\n", msg, copy_toeval);
-	return(0);
-}
-
-int
-expr(const char *toeval)
-{
-	copy_toeval = toeval;
-	yy_scan_string(toeval);
-	yyparse();
-	return end_result;
+	return h->total - h->deleted;
 }
