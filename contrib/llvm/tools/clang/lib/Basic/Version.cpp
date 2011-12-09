54672e6b5534ceab338fc3b60cee110635d2c8bb
dim <dim@FreeBSD.org>
2011-12-09 22:23:45 +0000

    Upgrade our copy of llvm/clang to 3.0 release.  Release notes can be found at: http://llvm.org/releases/3.0/docs/ReleaseNotes.html
    MFC after:	1 week

--- contrib/llvm/tools/clang/lib/Basic/Version.cpp
+++ contrib/llvm/tools/clang/lib/Basic/Version.cpp
@@ -32,7 +32,7 @@ std::string getClangRepositoryPath() {
 
   // If the SVN_REPOSITORY is empty, try to use the SVN keyword. This helps us
   // pick up a tag in an SVN export, for example.
-  static StringRef SVNRepository("$URL: http://llvm.org/svn/llvm-project/cfe/branches/release_30/lib/Basic/Version.cpp $");
+  static StringRef SVNRepository("$URL: http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_30/final/lib/Basic/Version.cpp $");
   if (URL.empty()) {
     URL = SVNRepository.slice(SVNRepository.find(':'),
                               SVNRepository.find("/lib/Basic"));
