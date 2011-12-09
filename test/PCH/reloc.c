9da628931ebf2609493570f87824ca22402cc65f
Dimitry Andric <dim@FreeBSD.org>
2011-12-09 18:30:42 +0000

    Vendor import of clang 3.0 final release: http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_30/final@145349

--- test/PCH/reloc.c
+++ test/PCH/reloc.c
@@ -1,13 +1,13 @@
-// RUN: %clang_cc1 -emit-pch -o %t -relocatable-pch -isysroot %S/libroot %S/libroot/usr/include/reloc.h
-// RUN: %clang_cc1 -include-pch %t -isysroot %S/libroot %s -verify
-// RUN: not %clang_cc1 -include-pch %t %s
+// RUN: %clang -ccc-host-triple x86_64-apple-darwin10 --relocatable-pch -o %t \
+// RUN:   -isysroot %S/libroot %S/libroot/usr/include/reloc.h
+// RUN: %clang -ccc-host-triple x86_64-apple-darwin10 -fsyntax-only \
+// RUN:   -include-pch %t -isysroot %S/libroot %s -Xclang -verify
+// RUN: not %clang -ccc-host-triple x86_64-apple-darwin10 -include-pch %t %s
 
 #include <reloc.h>
 
 int x = 2; // expected-error{{redefinition}}
 int y = 5; // expected-error{{redefinition}}
-
-
 
 
 // expected-note{{previous definition}}
