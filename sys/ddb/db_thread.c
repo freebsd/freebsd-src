30b0342f622275bfb4948b796470712830e2626a
kib <kib@FreeBSD.org>
2011-12-16 11:42:50 +0000

    Show the thread kernel stack base address for 'show threads'.
    Discussed with:	pho
    MFC after:	1 week

--- sys/ddb/db_thread.c
+++ sys/ddb/db_thread.c
@@ -94,7 +94,8 @@ db_show_threads(db_expr_t addr, boolean_t hasaddr, db_expr_t cnt, char *mod)
 
 	thr = kdb_thr_first();
 	while (!db_pager_quit && thr != NULL) {
-		db_printf("  %6ld (%p)  ", (long)thr->td_tid, thr);
+		db_printf("  %6ld (%p) (stack %p)  ", (long)thr->td_tid, thr,
+		    (void *)thr->td_kstack);
 		prev_jb = kdb_jmpbuf(jb);
 		if (setjmp(jb) == 0) {
 			if (db_trace_thread(thr, 1) != 0)
