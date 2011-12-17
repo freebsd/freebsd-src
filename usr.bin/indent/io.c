f7c0d4c7ead495febae69672a256ed37bfd3ac8a
Dimitry Andric <dim@FreeBSD.org>
2011-12-17 15:41:58 +0000

    In usr.bin/indent/io.c, fix a few warnings about format strings not being literals.
    MFC after:	1 week

--- usr.bin/indent/io.c
+++ usr.bin/indent/io.c
@@ -581,12 +581,12 @@ diag2(int level, const char *msg)
 	found_err = 1;
     if (output == stdout) {
 	fprintf(stdout, "/**INDENT** %s@%d: ", level == 0 ? "Warning" : "Error", line_no);
-	fprintf(stdout, msg);
+	fprintf(stdout, "%s", msg);
 	fprintf(stdout, " */\n");
     }
     else {
 	fprintf(stderr, "%s@%d: ", level == 0 ? "Warning" : "Error", line_no);
-	fprintf(stderr, msg);
+	fprintf(stderr, "%s", msg);
 	fprintf(stderr, "\n");
     }
 }
