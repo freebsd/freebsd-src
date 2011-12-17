6edbec86daa31bfff1c85abbf62e2c8759affe9b
avg <avg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-12-17 15:16:54 +0000

    introduce cngets, a method for kernel to read a string from console
    This is intended as a replacement for libkern's gets and mostly borrows
    its implementation.  It uses cngrab/cnungrab to delimit kernel's access
    to console input.
    
    Note: libkern's gets obviously doesn't share any bits of implementation
    iwth libc's gets.  They also have different APIs and the former doesn't
    have the overflow problems of the latter.
    
    Inspired by:	bde
    MFC after:	2 months
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@228633 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/kern_cons.c
+++ sys/kern/kern_cons.c
@@ -408,6 +408,55 @@ cncheckc(void)
 }
 
 void
+cngets(char *cp, size_t size, int visible)
+{
+	char *lp, *end;
+	int c;
+
+	cngrab();
+
+	lp = cp;
+	end = cp + size - 1;
+	for (;;) {
+		c = cngetc() & 0177;
+		switch (c) {
+		case '\n':
+		case '\r':
+			cnputc(c);
+			*lp = '\0';
+			cnungrab();
+			return;
+		case '\b':
+		case '\177':
+			if (lp > cp) {
+				if (visible) {
+					cnputc(c);
+					cnputs(" \b");
+				}
+				lp--;
+			}
+			continue;
+		case '\0':
+			continue;
+		default:
+			if (lp < end) {
+				switch (visible) {
+				case GETS_NOECHO:
+					break;
+				case GETS_ECHOPASS:
+					cnputc('*');
+					break;
+				default:
+					cnputc(c);
+					break;
+				}
+				*lp++ = c;
+			}
+		}
+	}
+}
+
+void
 cnputc(int c)
 {
 	struct cn_device *cnd;
