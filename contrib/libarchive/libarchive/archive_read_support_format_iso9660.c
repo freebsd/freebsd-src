9836aa783e70e5b9dde285c14fc1f9f2de2a2e44
mm <mm@FreeBSD.org>
2011-12-21 15:06:01 +0000

    Add missing integer casts to comparsions in libarchive read.
    MFC after:	2 weeks

--- contrib/libarchive/libarchive/archive_read_support_format_iso9660.c
+++ contrib/libarchive/libarchive/archive_read_support_format_iso9660.c
@@ -1628,7 +1628,8 @@ parse_file_info(struct archive_read *a, struct file_info *parent,
 	 */
 	if (location > 0 &&
 	    (location + ((fsize + iso9660->logical_block_size -1)
-	       / iso9660->logical_block_size)) > iso9660->volume_block) {
+	       / iso9660->logical_block_size))
+		> (uint32_t)iso9660->volume_block) {
 		archive_set_error(&a->archive, ARCHIVE_ERRNO_MISC,
 		    "Invalid location of extent of file");
 		return (NULL);
--- contrib/libarchive/libarchive/archive_read_support_format_xar.c
+++ contrib/libarchive/libarchive/archive_read_support_format_xar.c
@@ -2530,8 +2530,8 @@ xml_data(void *userData, const char *s, int len)
 #if DEBUG
 	{
 		char buff[1024];
-		if (len > sizeof(buff)-1)
-			len = sizeof(buff)-1;
+		if (len > (int)sizeof(buff)-1)
+			len = (int)sizeof(buff)-1;
 		memcpy(buff, s, len);
 		buff[len] = 0;
 		fprintf(stderr, "\tlen=%d:\"%s\"\n", len, buff);
