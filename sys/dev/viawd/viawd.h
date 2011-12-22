a7c3afc33f08d5f29bacd8650b9bc385a3ddbe70
Fabien Thomas <fabient@FreeBSD.org>
2011-12-22 15:13:32 +0000

    Correctly spell my email address.

--- sys/dev/viawd/viawd.h
+++ sys/dev/viawd/viawd.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2011 Fabien Thomas <fthomas@FreeBSD.org>
+ * Copyright (c) 2011 Fabien Thomas <fabient@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
