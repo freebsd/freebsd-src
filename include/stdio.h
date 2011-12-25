31d1910e85e83de7c8f8c1473440cfa1968dec9d
theraven <theraven@FreeBSD.org>
2011-12-25 16:03:54 +0000

    Restore __is_threaded in C++ mode.  Some Google stuff needs it apparently.
    Reported by:	swills
    Approved by:	dim (mentor)

--- include/stdio.h
+++ include/stdio.h
@@ -470,6 +470,9 @@ static __inline int __sputc(int _c, FILE *_p) {
 			__swbuf((int)(c), p) : \
 		(*(p)->_p = (c), (int)*(p)->_p++))
 #endif
+
+extern int __isthreaded;
+
 #ifndef __cplusplus
 
 #define	__sfeof(p)	(((p)->_flags & __SEOF) != 0)
@@ -477,7 +480,6 @@ static __inline int __sputc(int _c, FILE *_p) {
 #define	__sclearerr(p)	((void)((p)->_flags &= ~(__SERR|__SEOF)))
 #define	__sfileno(p)	((p)->_file)
 
-extern int __isthreaded;
 
 #define	feof(p)		(!__isthreaded ? __sfeof(p) : (feof)(p))
 #define	ferror(p)	(!__isthreaded ? __sferror(p) : (ferror)(p))
