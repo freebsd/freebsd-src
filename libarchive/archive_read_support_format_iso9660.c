1cd00a236b6b591357d4aa3d7d1d89bfbd92c2c9
Martin Matuska <mm@FreeBSD.org>
2011-12-27 10:21:29 +0000

    Update to vendor revision 4016
    Obtained from:	http://libarchive.googlecode.com/svn/release/2.8

--- libarchive/archive_read_support_format_iso9660.c
+++ libarchive/archive_read_support_format_iso9660.c
@@ -1830,7 +1830,7 @@ parse_file_info(struct archive_read *a, struct file_info *parent,
 		    (parent->re || parent->re_descendant))
 			file->re_descendant = 1;
 		if (file->cl_offset) {
-			struct file_info *p;
+			struct file_info *r;
 
 			if (parent == NULL || parent->parent == NULL) {
 				archive_set_error(&a->archive,
@@ -1858,8 +1858,8 @@ parse_file_info(struct archive_read *a, struct file_info *parent,
 			 * Sanity check: cl_offset does not point at its
 			 * the parents or itself.
 			 */
-			for (p = parent; p; p = p->parent) {
-				if (p->offset == file->cl_offset) {
+			for (r = parent; r; r = r->parent) {
+				if (r->offset == file->cl_offset) {
 					archive_set_error(&a->archive,
 					    ARCHIVE_ERRNO_MISC,
 					    "Invalid Rockridge CL");
