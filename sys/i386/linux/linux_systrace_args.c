4eda7b08af24c6ad4014d07eb33877375c12beea
John Baldwin <jhb@FreeBSD.org>
2011-12-29 15:35:47 +0000

    Regen.

--- sys/i386/linux/linux_systrace_args.c
+++ sys/i386/linux/linux_systrace_args.c
@@ -1724,7 +1724,12 @@ systrace_args(int sysnum, void *params, uint64_t *uarg, int *n_args)
 	}
 	/* linux_fadvise64 */
 	case 250: {
-		*n_args = 0;
+		struct linux_fadvise64_args *p = params;
+		iarg[0] = p->fd; /* int */
+		iarg[1] = p->offset; /* l_loff_t */
+		iarg[2] = p->len; /* l_size_t */
+		iarg[3] = p->advice; /* int */
+		*n_args = 4;
 		break;
 	}
 	/* linux_exit_group */
@@ -1873,7 +1878,12 @@ systrace_args(int sysnum, void *params, uint64_t *uarg, int *n_args)
 	}
 	/* linux_fadvise64_64 */
 	case 272: {
-		*n_args = 0;
+		struct linux_fadvise64_64_args *p = params;
+		iarg[0] = p->fd; /* int */
+		iarg[1] = p->offset; /* l_loff_t */
+		iarg[2] = p->len; /* l_loff_t */
+		iarg[3] = p->advice; /* int */
+		*n_args = 4;
 		break;
 	}
 	/* linux_mbind */
@@ -4776,6 +4786,22 @@ systrace_entry_setargdesc(int sysnum, int ndx, char *desc, size_t descsz)
 		break;
 	/* linux_fadvise64 */
 	case 250:
+		switch(ndx) {
+		case 0:
+			p = "int";
+			break;
+		case 1:
+			p = "l_loff_t";
+			break;
+		case 2:
+			p = "l_size_t";
+			break;
+		case 3:
+			p = "int";
+			break;
+		default:
+			break;
+		};
 		break;
 	/* linux_exit_group */
 	case 252:
@@ -4985,6 +5011,22 @@ systrace_entry_setargdesc(int sysnum, int ndx, char *desc, size_t descsz)
 		break;
 	/* linux_fadvise64_64 */
 	case 272:
+		switch(ndx) {
+		case 0:
+			p = "int";
+			break;
+		case 1:
+			p = "l_loff_t";
+			break;
+		case 2:
+			p = "l_loff_t";
+			break;
+		case 3:
+			p = "int";
+			break;
+		default:
+			break;
+		};
 		break;
 	/* linux_mbind */
 	case 274:
@@ -6414,6 +6456,9 @@ systrace_return_setargdesc(int sysnum, int ndx, char *desc, size_t descsz)
 		break;
 	/* linux_fadvise64 */
 	case 250:
+		if (ndx == 0 || ndx == 1)
+			p = "int";
+		break;
 	/* linux_exit_group */
 	case 252:
 		if (ndx == 0 || ndx == 1)
@@ -6498,6 +6543,9 @@ systrace_return_setargdesc(int sysnum, int ndx, char *desc, size_t descsz)
 		break;
 	/* linux_fadvise64_64 */
 	case 272:
+		if (ndx == 0 || ndx == 1)
+			p = "int";
+		break;
 	/* linux_mbind */
 	case 274:
 	/* linux_get_mempolicy */
