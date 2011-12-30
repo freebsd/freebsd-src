f76b3199890af4834e2b3a6e475c6021f2d5036c
Ulrich Spörlein <uqs@FreeBSD.org>
2011-12-30 00:59:08 +0000

    Reencode files to UTF-8. Drop CP1252 em-dash.

--- usr.sbin/pkg_install/updating/pathnames.h
+++ usr.sbin/pkg_install/updating/pathnames.h
@@ -3,7 +3,7 @@
  * "THE BEER-WARE LICENSE" (Revision 42):
  * <beat@chruetertee.ch> wrote this file. As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return.          Beat Gätzi
+ * this stuff is worth it, you can buy me a beer in return.          Beat GÃ¤tzi
  * ----------------------------------------------------------------------------
  *
  * $FreeBSD$
