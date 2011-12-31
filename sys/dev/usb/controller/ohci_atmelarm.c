8abb800da278e06fe97034da441c94e951f01561
hselasky <hselasky@FreeBSD.org>
2011-12-31 15:31:34 +0000

    MFC r227701, r227847 and r227849: Move the device_delete_all_children() function from usb_util.c to kern/subr_bus.c. Simplify this function so that it no longer depends on malloc() to execute. Rename device_delete_all_children() into device_delete_children(). Identify a few other places where it makes sense to use device_delete_children().

--- sys/dev/usb/controller/ohci_atmelarm.c
+++ sys/dev/usb/controller/ohci_atmelarm.c
@@ -174,7 +174,7 @@ ohci_atmelarm_detach(device_t dev)
 		device_delete_child(dev, bdev);
 	}
 	/* during module unload there are lots of children leftover */
-	device_delete_all_children(dev);
+	device_delete_children(dev);
 
 	/*
 	 * Put the controller into reset, then disable clocks and do
