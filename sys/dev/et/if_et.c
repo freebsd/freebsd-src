e3aad66bb57e94dbfef83ac3b2a1899cc90fcd3a
yongari <yongari@FreeBSD.org>
2011-12-31 01:33:25 +0000

    MFC r226480-226481: r226480:   Add missing driver lock in media status handler.
    r226481:
      Make sure to report media change status to caller. Previously it
      always reported success.

--- sys/dev/et/if_et.c
+++ sys/dev/et/if_et.c
@@ -519,9 +519,7 @@ et_ifmedia_upd_locked(struct ifnet *ifp)
 
 	LIST_FOREACH(miisc, &mii->mii_phys, mii_list)
 		mii_phy_reset(miisc);
-	mii_mediachg(mii);
-
-	return (0);
+	return (mii_mediachg(mii));
 }
 
 static int
@@ -543,9 +541,11 @@ et_ifmedia_sts(struct ifnet *ifp, struct ifmediareq *ifmr)
 	struct et_softc *sc = ifp->if_softc;
 	struct mii_data *mii = device_get_softc(sc->sc_miibus);
 
+	ET_LOCK(sc);
 	mii_pollstat(mii);
 	ifmr->ifm_active = mii->mii_media_active;
 	ifmr->ifm_status = mii->mii_media_status;
+	ET_UNLOCK(sc);
 }
 
 static void
