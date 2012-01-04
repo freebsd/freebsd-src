310795b3de70b3f568912e634e8581317c671f36
pluknet <pluknet@FreeBSD.org>
2012-01-04 12:39:52 +0000

    MFC r226833,r227056:
    Remove the long reprecated ``/stand/sysinstall'' from the init_path.

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -641,7 +641,7 @@ static char init_path[MAXPATHLEN] =
 #ifdef	INIT_PATH
     __XSTRING(INIT_PATH);
 #else
-    "/sbin/init:/sbin/oinit:/sbin/init.bak:/rescue/init:/stand/sysinstall";
+    "/sbin/init:/sbin/oinit:/sbin/init.bak:/rescue/init";
 #endif
 SYSCTL_STRING(_kern, OID_AUTO, init_path, CTLFLAG_RD, init_path, 0,
 	"Path used to search the init process");
