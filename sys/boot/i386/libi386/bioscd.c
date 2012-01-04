041ff9c0a2fde09e3284e0e6bb2a7d4765ab6254
jhb <jhb@FreeBSD.org>
2012-01-04 16:39:39 +0000

    MFC 226746: Consolidate duplicate definitions of V86_CY() and V86_ZR() which check for the carry and zero flags being set, respectively, in <btxv86.h> and use them throughout the x86 boot code.

--- sys/boot/i386/libi386/bioscd.c
+++ sys/boot/i386/libi386/bioscd.c
@@ -42,7 +42,6 @@ __FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
 #include <machine/bootinfo.h>
-#include <machine/psl.h>
 
 #include <stdarg.h>
 
@@ -334,7 +333,7 @@ bc_read(int unit, daddr_t dblk, int blks, caddr_t dest)
 			v86.ds = VTOPSEG(&packet);
 			v86.esi = VTOPOFF(&packet);
 			v86int();
-			result = (v86.efl & PSL_C);
+			result = V86_CY(v86.efl);
 			if (result == 0)
 				break;
 		}
