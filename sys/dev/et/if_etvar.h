7184ae7632d52e78690424ee7a1a7e6502a6993b
yongari <yongari@FreeBSD.org>
2012-01-06 19:24:33 +0000

    MFC r228326-228327,228331-228332: r228326:   Controller does not require TX start command for every frame.  So   send a single TX command after setting up all TX frames.  This   removes unnecessary register accesses and bus_dmamap_sync(9) calls.   et(4) uses TX interrupt moderation so it's possible to have TX   buffers that were already transmitted but waiting for TX completion   interrupt.  If the number of available TX descriptor is less then   1/3 of total TX descriptor, try reclaiming first to get enough free   TX descriptors before setting up TX descriptors.   After r228325, et_txeof() no longer tries to send frames after   reclaiming TX buffers.  That change was made to give more chance   to transmit frames in main interrupt handler since we can still   send frames in interrupt handler with RX interrupt.  So right   before exiting interrupt hander, after enabling interrupt, try to   send more frames.  This gives slightly better performance numbers.
      While I'm here reduce number of spare TX descriptors from 8 to 4.
      Controller does not require reserved TX descriptors, it was just to
      reduce TX overhead.  After r228325, driver has much lower TX
      overhead so it does not make sense to reserve 8 TX descriptors.
    
    r228327:
      Remove et_enable_intrs(), et_disable_intrs() functions and
      manipulation of interrupt register access is done through
      CSR_WRITE_4 macro.  Also add disabling interrupt into et_reset()
      because we want interrupt disabled state after controller reset.
      While I'm here slightly change interrupt handler to be more
      readable one.
    
    r228331:
      Rework link state tracking and TX/RX MAC configuration.
       o Do not report link status if driver is not running.
       o TX/RX MAC configuration should be done with resolved speed,
         duplex and flow control after establishing a link so it can't
         be done in driver initialization routine.
         Move the configuration to miibus_statchg callback which will be
         called whenever any link state change is detected.
         At this moment, flow-control is not enabled yet mainly because
         I was not able to set correct flow control parameters to
         generate TX pause frames.
       o Now TX/RX MAC is enabled only when a valid link is detected.
         Rearragnge hardware initialization routine a bit to leave
         enabling MAC to miibus_statchg callback.  In order to that,
         TX/RX DMA engine is enabled in et_init_locked().
       o Introduce ET_FLAG_LINK flag to track current link state.
       o Introduce ET_FLAG_FASTETHER flag to mark whether controller is
         fast ethernet.  This flag is checked in miibus_statchg callback
         to know whether PHY established a valid link.
       o In et_stop(), TX/RX MAC is explicitly disabled instead of
         relying on et_reset().  And move et_reset() from et_stop() to
         controller initialization.  Controler reset is not required here
         and it would also clear critial registers(i.e station address,
         RX filter configuration, WOL etc) that are required to make WOL
         work.
       o Switching to current media is done in et_init_locked() after
         setting IFF_DRV_RUNNING flag.  This should ensure reliable
         auto-negotiation/manual link establishment.
       o In et_start_locked(), check whether driver got a valid link
         before trying to send frames.
       o Remove checking a link in et_tick() as this is done by
         miibus_statchg callback.
    
    r228332:
      Implement hardware MAC statistics counter.  Counters could be
      queried with dev.et.%d.stats sysctl node where %d is an instance of
      device.

--- sys/dev/et/if_etvar.h
+++ sys/dev/et/if_etvar.h
@@ -41,7 +41,7 @@
 #define ET_RING_ALIGN		4096
 #define ET_STATUS_ALIGN		8
 #define ET_NSEG_MAX		32	/* XXX no limit actually */
-#define ET_NSEG_SPARE		8
+#define ET_NSEG_SPARE		4
 
 #define ET_TX_NDESC		512
 #define ET_RX_NDESC		512
@@ -231,6 +231,56 @@ struct et_rxbuf_data {
 	void			(*rbd_discard)(struct et_rxbuf_data *, int);
 };
 
+struct et_hw_stats {
+	/* RX/TX stats. */
+	uint64_t		pkts_64;
+	uint64_t		pkts_65;
+	uint64_t		pkts_128;
+	uint64_t		pkts_256;
+	uint64_t		pkts_512;
+	uint64_t		pkts_1024;
+	uint64_t		pkts_1519;
+	/* RX stats. */
+	uint64_t		rx_bytes;
+	uint64_t		rx_frames;
+	uint32_t		rx_crcerrs;
+	uint64_t		rx_mcast;
+	uint64_t		rx_bcast;
+	uint32_t		rx_control;
+	uint32_t		rx_pause;
+	uint32_t		rx_unknown_control;
+	uint32_t		rx_alignerrs;
+	uint32_t		rx_lenerrs;
+	uint32_t		rx_codeerrs;
+	uint32_t		rx_cserrs;
+	uint32_t		rx_runts;
+	uint64_t		rx_oversize;
+	uint32_t		rx_fragments;
+	uint32_t		rx_jabbers;
+	uint32_t		rx_drop;
+	/* TX stats. */
+	uint64_t		tx_bytes;
+	uint64_t		tx_frames;
+	uint64_t		tx_mcast;
+	uint64_t		tx_bcast;
+	uint32_t		tx_pause;
+	uint32_t		tx_deferred;
+	uint32_t		tx_excess_deferred;
+	uint32_t		tx_single_colls;
+	uint32_t		tx_multi_colls;
+	uint32_t		tx_late_colls;
+	uint32_t		tx_excess_colls;
+	uint32_t		tx_total_colls;
+	uint32_t		tx_pause_honored;
+	uint32_t		tx_drop;
+	uint32_t		tx_jabbers;
+	uint32_t		tx_crcerrs;
+	uint32_t		tx_control;
+	uint64_t		tx_oversize;
+	uint32_t		tx_undersize;
+	uint32_t		tx_fragments;
+};
+
 struct et_softc {
 	struct ifnet		*ifp;
 	device_t		dev;
@@ -271,6 +321,7 @@ struct et_softc {
 	struct et_rxbuf_data	sc_rx_data[ET_RX_NRING];
 	struct et_txbuf_data	sc_tx_data;
 
+	struct et_hw_stats	sc_stats;
 	uint32_t		sc_tx;
 	uint32_t		sc_tx_intr;
 
@@ -289,7 +340,9 @@ struct et_softc {
 
 #define ET_FLAG_PCIE		0x0001
 #define ET_FLAG_MSI		0x0002
+#define ET_FLAG_FASTETHER	0x0004
 #define ET_FLAG_TXRX_ENABLED	0x0100
 #define ET_FLAG_JUMBO		0x0200
+#define ET_FLAG_LINK		0x8000
 
 #endif	/* !_IF_ETVAR_H */
