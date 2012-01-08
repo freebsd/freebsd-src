d79462d1605511d451159b8d49383c7a34ac31fe
glebius <glebius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-01-08 17:11:53 +0000

    Copy ifa->if_data to ifam->ifam_data. This was forgotten in r228571.
    Submitted by:	bz
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@229814 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net/rtsock.c
+++ sys/net/rtsock.c
@@ -1606,6 +1606,7 @@ sysctl_iflist(int af, struct walkarg *w)
 				ifam->ifam_flags = ifa->ifa_flags;
 				ifam->ifam_metric = ifa->ifa_metric;
 				ifam->ifam_addrs = info.rti_addrs;
+				ifam->ifam_data = ifa->if_data;
 				if (carp_get_vhid_p != NULL)
 					ifam->ifam_data.ifi_vhid =
 					    (*carp_get_vhid_p)(ifa);
