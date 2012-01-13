ec7a54041052bad62be7621e6dc01f4607c7c0f6
mckusick <mckusick@FreeBSD.org>
2012-01-13 07:10:52 +0000

    MFC: 226520
    The current /etc/dumpdates file restricts device names to 32 characters.
    With the addition of various GEOM layers some device names now exceed
    this length, for example /dev/mirror/encrypted.elig.journal. This
    change expands the field to 53 bytes which brings the /etc/dumpdates
    lines to 80 characters. Exceeding 80 characters makes the /etc/dumpdates
    file much less human readable. A test is added to dump so that it
    verifies that the device name will fit in the 53 character field
    failing the dump if it is too long.
    
    This change has been checked to verify that its /etc/dumpdates file
    is compatible with older versions of dump.
    
    Reported by: Martin Sugioarto <martin@sugioarto.com>
    PR:          kern/160678

--- sbin/dump/dump.h
+++ sbin/dump/dump.h
@@ -171,9 +171,10 @@ void	putdumptime(void);
     	if (ddatev != NULL) \
 		for (ddp = ddatev[i = 0]; i < nddates; ddp = ddatev[++i])
 
-#define	DUMPOUTFMT	"%-32s %d %s"		/* for printf */
+#define	DUMPFMTLEN	53			/* max device pathname length */
+#define	DUMPOUTFMT	"%-*s %d %s"		/* for printf */
 						/* name, level, ctime(date) */
-#define	DUMPINFMT	"%32s %d %[^\n]\n"	/* inverse for scanf */
+#define	DUMPINFMT	"%s %d %[^\n]\n"	/* inverse for scanf */
 
 void	sig(int signo);
 
