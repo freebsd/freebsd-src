b1c09bd5fffe5b82541c794f393274f163acc9cc
Kevin Lo <kevlo@FreeBSD.org>
2012-01-13 09:08:41 +0000

    Fix copyright year
    Spotted by:	pluknet

--- usr.sbin/config/mkmakefile.c
+++ usr.sbin/config/mkmakefile.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1980, 1993
+ * Copyright (c) 1980, 1990, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
