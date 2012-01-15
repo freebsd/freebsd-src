904932e5f9f346689139d0491520f49e3125846f
avg <avg@FreeBSD.org>
2012-01-15 22:04:28 +0000

    MFC r228426: syscons: make sc_puts static as it is used only privately

--- sys/dev/syscons/syscons.h
+++ sys/dev/syscons/syscons.h
@@ -562,7 +562,6 @@ void		sc_save_font(scr_stat *scp, int page, int size, int width,
 void		sc_show_font(scr_stat *scp, int page);
 
 void		sc_touch_scrn_saver(void);
-void		sc_puts(scr_stat *scp, u_char *buf, int len, int kernel);
 void		sc_draw_cursor_image(scr_stat *scp);
 void		sc_remove_cursor_image(scr_stat *scp);
 void		sc_set_cursor_image(scr_stat *scp);
