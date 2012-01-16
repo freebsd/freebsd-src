01a43bcf7bce6a2d8394745fa8ff2c03ab7cb014
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2012-01-16 11:07:46 +0000

    sh: Fix execution of multiple statements in a trap when evalskip is set
    Before this fix, only the first statement of the trap was executed if
    evalskip was set. This is for example the case when:
        o  "-e" is set for this shell
        o  a trap is set on EXIT
        o  a function returns 1 and causes the script to abort
    
    Reviewed by:	jilles
    MFC after:	2 weeks

--- bin/sh/trap.c
+++ bin/sh/trap.c
@@ -412,7 +412,7 @@ void
 dotrap(void)
 {
 	int i;
-	int savestatus;
+	int savestatus, prev_evalskip, prev_skipcount;
 
 	in_dotrap++;
 	for (;;) {
@@ -427,10 +427,36 @@ dotrap(void)
 					 */
 					if (i == SIGCHLD)
 						ignore_sigchld++;
+
+					/*
+					 * Backup current evalskip
+					 * state and reset it before
+					 * executing a trap, so that the
+					 * trap is not disturbed by an
+					 * ongoing break/continue/return
+					 * statement.
+					 */
+					prev_evalskip  = evalskip;
+					prev_skipcount = skipcount;
+					evalskip = 0;
+
 					last_trapsig = i;
 					savestatus = exitstatus;
 					evalstring(trap[i], 0);
 					exitstatus = savestatus;
+
+					/*
+					 * If such a command was not
+					 * already in progress, allow a
+					 * break/continue/return in the
+					 * trap action to have an effect
+					 * outside of it.
+					 */
+					if (prev_evalskip != 0) {
+						evalskip  = prev_evalskip;
+						skipcount = prev_skipcount;
+					}
+
 					if (i == SIGCHLD)
 						ignore_sigchld--;
 				}
@@ -501,6 +527,11 @@ exitshell_savedstatus(void)
 	}
 	handler = &loc1;
 	if ((p = trap[0]) != NULL && *p != '\0') {
+		/*
+		 * Reset evalskip, or the trap on EXIT could be
+		 * interrupted if the last command was a "return".
+		 */
+		evalskip = 0;
 		trap[0] = NULL;
 		evalstring(p, 0);
 	}
