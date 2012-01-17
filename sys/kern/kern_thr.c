fab22f36dde6d4fe4270be21dcc06145ab8baf23
pho <pho@FreeBSD.org>
2012-01-17 11:04:58 +0000

    MFC: r228218, r228219, 228220, 228221
    Rename copyin_timeout32 to umtx_copyin_timeout32 and move parameter
    check here. Include check for negative seconds value.
    Add umtx_copyin_timeout() and move parameter checks here.
    Add declaration of umtx_copyin_timeout()
    Use umtx_copyin_timeout() to copy and check timeout parameter.

--- sys/kern/kern_thr.c
+++ sys/kern/kern_thr.c
@@ -449,8 +449,7 @@ sys_thr_suspend(struct thread *td, struct thr_suspend_args *uap)
 
 	tsp = NULL;
 	if (uap->timeout != NULL) {
-		error = copyin((const void *)uap->timeout, (void *)&ts,
-		    sizeof(struct timespec));
+		error = umtx_copyin_timeout(uap->timeout, &ts);
 		if (error != 0)
 			return (error);
 		tsp = &ts;
@@ -473,9 +472,6 @@ kern_thr_suspend(struct thread *td, struct timespec *tsp)
 	}
 
 	if (tsp != NULL) {
-		if (tsp->tv_sec < 0 || tsp->tv_nsec < 0 ||
-		    tsp->tv_nsec > 1000000000)
-			return (EINVAL);
 		if (tsp->tv_sec == 0 && tsp->tv_nsec == 0)
 			error = EWOULDBLOCK;
 		else {
