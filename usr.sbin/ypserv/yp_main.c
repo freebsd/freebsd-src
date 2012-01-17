85e6b6d19f1a4c02c0e8eb624ff0b37cad1b17b6
hrs <hrs@FreeBSD.org>
2012-01-17 20:56:12 +0000

    MFC: Revert changes in r229899.  The ypserv daemon could not work with multiple socktypes due to it.

--- usr.sbin/ypserv/yp_main.c
+++ usr.sbin/ypserv/yp_main.c
@@ -255,7 +255,6 @@ create_service(const int sock, const struct netconfig *nconf,
 	const struct __rpc_sockinfo *si)
 {
 	int error;
-	char *sname;
 
 	SVCXPRT *transp;
 	struct addrinfo hints, *res, *res0;
@@ -263,7 +262,6 @@ create_service(const int sock, const struct netconfig *nconf,
 	struct bindaddrlistent *blep;
 	struct netbuf svcaddr;
 
-	sname = NULL;
 	SLIST_INIT(&sle_head);
 	memset(&hints, 0, sizeof(hints));
 	memset(&svcaddr, 0, sizeof(svcaddr));
@@ -343,6 +341,7 @@ create_service(const int sock, const struct netconfig *nconf,
 				if (strncmp("0", servname, 1) == 0) {
 					struct sockaddr *sap;
 					socklen_t slen;
+					char *sname;
 
 					sname = malloc(NI_MAXSERV);
 					if (sname == NULL) {
@@ -443,7 +442,6 @@ create_service(const int sock, const struct netconfig *nconf,
 	}
 	/* XXX: ignore error intentionally */
 	rpcb_set(YPPROG, YPVERS, nconf, &svcaddr);
-	free(sname);
 	freeaddrinfo(res0);
 	return 0;
 }
