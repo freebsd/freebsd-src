b02539907530b46d5ccc18ee5dd00ce6346eb4f9
mav <mav@FreeBSD.org>
2012-01-22 15:44:20 +0000

    Increase snd_hda(4) default maximal buffer size from 16K to 64K and maximal from 64K to 256K.
    We usually don't need 750 sound interrupts per second (1.3ms latency)
    when playing 192K/24/8 stream. 187 should be better. On usual 48K/16/2
    it is just enough for hw.snd.latency=9 at hw.snd.latency_profile=1 with
    23 and 6 interrupts per second.
    
    MFC after:	2 weeks
    Sponsored by:	iXsystems, Inc.

--- sys/dev/sound/pci/hda/hdaa.c
+++ sys/dev/sound/pci/hda/hdaa.c
@@ -74,17 +74,6 @@ static const struct {
 #define HDAA_QUIRKS_TAB_LEN	\
 		(sizeof(hdaa_quirks_tab) / sizeof(hdaa_quirks_tab[0]))
 
-#define HDA_BDL_MIN	2
-#define HDA_BDL_MAX	256
-#define HDA_BDL_DEFAULT	HDA_BDL_MIN
-
-#define HDA_BLK_MIN	HDA_DMA_ALIGNMENT
-#define HDA_BLK_ALIGN	(~(HDA_BLK_MIN - 1))
-
-#define HDA_BUFSZ_MIN		4096
-#define HDA_BUFSZ_MAX		65536
-#define HDA_BUFSZ_DEFAULT	16384
-
 #define HDA_PARSE_MAXDEPTH	10
 
 MALLOC_DEFINE(M_HDAA, "hdaa", "HDA Audio");
