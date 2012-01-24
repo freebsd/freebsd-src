2d065f4d059a4c196d968d34eeea26731cf4ffa0
bz <bz@FreeBSD.org>
2012-01-24 13:57:30 +0000

    Plug a possible ifa_ref leak in case of premature return from in6_purgeaddr().
    Reviewed by:	rwatson
    MFC after:	3 days

--- sys/netinet6/in6.c
+++ sys/netinet6/in6.c
@@ -1477,6 +1477,8 @@ in6_purgeaddr(struct ifaddr *ifa)
 	}
 
 cleanup:
+	if (ifa0 != NULL)
+		ifa_free(ifa0);
 
 	plen = in6_mask2len(&ia->ia_prefixmask.sin6_addr, NULL); /* XXX */
 	if ((ia->ia_flags & IFA_ROUTE) && plen == 128) {
@@ -1501,8 +1503,6 @@ cleanup:
 			return;
 		ia->ia_flags &= ~IFA_ROUTE;
 	}
-	if (ifa0 != NULL)
-		ifa_free(ifa0);
 
 	in6_unlink_ifa(ia, ifp);
 }
