4ae142942e857767124218ba0e04af4d16bc8780
pluknet <pluknet@FreeBSD.org>
2012-01-25 08:53:42 +0000

    Remove unused variable.
    The actual ia6->ia6_lifetime access is hidden in
    IFA6_IS_INVALID/IFA6_IS_DEPRECATED macros since a long time ago
    (see netinet6/nd6.c, r1.104 of KAME for the reference).
    
    MFC after:	3 days

--- sys/netinet6/nd6.c
+++ sys/netinet6/nd6.c
@@ -575,7 +575,6 @@ nd6_timer(void *arg)
 	struct nd_defrouter *dr, *ndr;
 	struct nd_prefix *pr, *npr;
 	struct in6_ifaddr *ia6, *nia6;
-	struct in6_addrlifetime *lt6;
 
 	callout_reset(&V_nd6_timer_ch, V_nd6_prune * hz,
 	    nd6_timer, curvnet);
@@ -598,7 +597,6 @@ nd6_timer(void *arg)
   addrloop:
 	TAILQ_FOREACH_SAFE(ia6, &V_in6_ifaddrhead, ia_link, nia6) {
 		/* check address lifetime */
-		lt6 = &ia6->ia6_lifetime;
 		if (IFA6_IS_INVALID(ia6)) {
 			int regen = 0;
 
