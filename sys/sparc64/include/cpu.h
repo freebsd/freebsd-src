17587aec9f0de7b8de609653edcb45edb6a16b3f
Marius Strobl <marius@FreeBSD.org>
2012-02-01 21:15:27 +0000

    MFC: r230628
    Mark cpu_{halt,reset}() as __dead2 as appropriate.

--- sys/sparc64/include/cpu.h
+++ sys/sparc64/include/cpu.h
@@ -53,8 +53,8 @@ extern	char btext[];
 extern	char etext[];
 
 void	cheetah_init(u_int cpu_impl);
-void	cpu_halt(void);
-void	cpu_reset(void);
+void	cpu_halt(void) __dead2;
+void	cpu_reset(void) __dead2;
 void	fork_trampoline(void);
 void	swi_vm(void *v);
 void	zeus_init(u_int cpu_impl);
