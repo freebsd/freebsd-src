e9edcfe7c1220c2d1db1dbd43c113116d45be7fe
davidxu <davidxu@FreeBSD.org>
2012-02-01 02:53:06 +0000

    If multiple threads call kevent() to get AIO events on same kqueue fd, it is possible that a single AIO event will be reported to multiple threads, it is not threading friendly, and the existing API can not control this behavior. Allocate a kevent flags field sigev_notify_kevent_flags for AIO event notification in sigevent, and allow user to pass EV_CLEAR, EV_DISPATCH or EV_ONESHOT to AIO kernel code, user can control whether the event should be cleared once it is retrieved by a thread. This change should be comptaible with existing application, because the field should have already been zero-filled, and no additional action will be taken by kernel.
    PR:	kern/156567

--- sys/sys/signal.h
+++ sys/sys/signal.h
@@ -169,12 +169,14 @@ struct sigevent {
 			void (*_function)(union sigval);
 			void *_attribute; /* pthread_attr_t * */
 		} _sigev_thread;
+		unsigned short _kevent_flags;
 		long __spare__[8];
 	} _sigev_un;
 };
 
 #if __BSD_VISIBLE
 #define	sigev_notify_kqueue		sigev_signo
+#define	sigev_notify_kevent_flags	_sigev_un._kevent_flags
 #define	sigev_notify_thread_id		_sigev_un._threadid
 #endif
 #define	sigev_notify_function		_sigev_un._sigev_thread._function
