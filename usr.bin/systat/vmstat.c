0c6c5de0e541b67293212484073d21c7a65e7e3f
ed <ed@FreeBSD.org>
2012-02-09 15:21:54 +0000

    MFC r226396:
      Fix whitespace inconsistencies in systat(1).
    
      According to md5(1), the resulting binary is the same.

--- usr.bin/systat/vmstat.c
+++ usr.bin/systat/vmstat.c
@@ -224,7 +224,7 @@ initkre(void)
 		intrloc = calloc(nintr, sizeof (long));
 		intrname = calloc(nintr, sizeof (char *));
 		intrnamebuf = sysctl_dynread("hw.intrnames", NULL);
-		if (intrnamebuf == NULL || intrname == NULL || 
+		if (intrnamebuf == NULL || intrname == NULL ||
 		    intrloc == NULL) {
 			error("Out of memory");
 			if (intrnamebuf)
@@ -854,7 +854,7 @@ dinfo(int dn, int lc, struct statinfo *now, struct statinfo *then)
 		elapsed_time = now->snap_time - then->snap_time;
 	} else {
 		/* Calculate relative to device creation */
-	        elapsed_time = now->snap_time - devstat_compute_etime(
+		elapsed_time = now->snap_time - devstat_compute_etime(
 		    &now->dinfo->devices[di].creation_time, NULL);
 	}
 
