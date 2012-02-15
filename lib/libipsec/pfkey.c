4d6764604917ab27d0263ec8131860893127212d
Bjoern A. Zeeb <bz@FreeBSD.org>
2012-02-15 09:32:40 +0000

    MFC r231515:
     Use the correct constant (with same value) for comparying the SA type.
    
    PR:		kern/142741
    Submitted by:	Matthijs Kooiman (matthijs stdin.nl)

--- lib/libipsec/pfkey.c
+++ lib/libipsec/pfkey.c
@@ -662,7 +662,7 @@ pfkey_send_register(so, satype)
 {
 	int len, algno;
 
-	if (satype == PF_UNSPEC) {
+	if (satype == SADB_SATYPE_UNSPEC) {
 		for (algno = 0;
 		     algno < sizeof(supported_map)/sizeof(supported_map[0]);
 		     algno++) {
