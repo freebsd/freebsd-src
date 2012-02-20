4b2f072458169809b2be156e51b8a0ff3a0ac1bf
Martin Matuska <mm@FreeBSD.org>
2012-02-20 21:10:14 +0000

    MFC r230397, r230438:
    MFC r230397 (pjd):
    By default turn off prefetch when listing snapshots.
    In my tests it makes listing snapshots 19% faster with cold cache and
    47% faster with warm cache.
    
    MFC r230438 (pjd):
    Dramatically optimize listing snapshots when user requests only snapshot
    names and wants to sort them by name, ie. when executes:
    
    	# zfs list -t snapshot -o name -s name
    
    Because only name is needed we don't have to read all snapshot properties.
    
    Below you can find how long does it take to list 34509 snapshots from a single
    disk pool before and after this change with cold and warm cache:
    
    before:
    
    	# time zfs list -t snapshot -o name -s name > /dev/null
    	cold cache: 525s
    	warm cache: 218s
    
    after:
    
    	# time zfs list -t snapshot -o name -s name > /dev/null
    	cold cache: 1.7s
    	warm cache: 1.1s

--- cddl/contrib/opensolaris/lib/libzfs/common/libzfs_impl.h
+++ cddl/contrib/opensolaris/lib/libzfs/common/libzfs_impl.h
@@ -150,7 +150,7 @@ int zpool_standard_error_fmt(libzfs_handle_t *, int, const char *, ...);
 int get_dependents(libzfs_handle_t *, boolean_t, const char *, char ***,
     size_t *);
 zfs_handle_t *make_dataset_handle_zc(libzfs_handle_t *, zfs_cmd_t *);
-
+zfs_handle_t *make_dataset_simple_handle_zc(zfs_handle_t *, zfs_cmd_t *);
 
 int zprop_parse_value(libzfs_handle_t *, nvpair_t *, int, zfs_type_t,
     nvlist_t *, char **, uint64_t *, const char *);
