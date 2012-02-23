freebsd/freebsd-src

commit 9c0f7fcc27b9bb1fe94281d43593384ff0ee41e0

Set the initial mode for the adapter after executing VESA BIOS POST. There is no need to set initial mode for BIOS.
git-svn-id: svn+ssh://svn.freebsd.org/base/head@231843 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/fb/vesa.c
@@ -1469,6 +1469,7 @@ vesa_save_state(video_adapter_t *adp, void *p, size_t size)
 static int
 vesa_load_state(video_adapter_t *adp, void *p)
 {
+	int mode;
 
 	if ((adp != vesa_adp) || (((adp_state_t *)p)->sig != V_STATE_SIG))
 		return ((*prevvidsw->load_state)(adp, p));
@@ -1478,8 +1479,10 @@ vesa_load_state(video_adapter_t *adp, void *p)
 
 	/* Try BIOS POST to restore a sane state. */
 	(void)vesa_bios_post();
-	(void)int10_set_mode(adp->va_initial_bios_mode);
-	(void)vesa_set_mode(adp, adp->va_mode);
+	mode = adp->va_mode;
+	(void)vesa_set_mode(adp, adp->va_initial_mode);
+	if (mode != adp->va_initial_mode);
+		(void)vesa_set_mode(adp, mode);
 
 	return (vesa_bios_save_restore(STATE_LOAD, ((adp_state_t *)p)->regs));
 }

commit 0e63e401761017d916a14d2c69cc25afcfa33249

Fix a typo introduced in r231843.

sys/dev/fb/vesa.c
@@ -1481,7 +1481,7 @@ vesa_load_state(video_adapter_t *adp, void *p)
 	(void)vesa_bios_post();
 	mode = adp->va_mode;
 	(void)vesa_set_mode(adp, adp->va_initial_mode);
-	if (mode != adp->va_initial_mode);
+	if (mode != adp->va_initial_mode)
 		(void)vesa_set_mode(adp, mode);
 
 	return (vesa_bios_save_restore(STATE_LOAD, ((adp_state_t *)p)->regs));