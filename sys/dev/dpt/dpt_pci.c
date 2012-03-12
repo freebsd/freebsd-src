d9c620a2645a599d4e355c4fe37b7568e2f545a2
jmallett <jmallett@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-03-12 19:29:32 +0000

    Remove comments about creating DMA tags as children of the DMA tags of their parent bus where the code has now been modified to do so.
    Reviewed by:	scottl
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@232882 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/dpt/dpt_pci.c
+++ sys/dev/dpt/dpt_pci.c
@@ -132,7 +132,6 @@ dpt_pci_attach (device_t dev)
 	dpt_alloc(dev);
 
 	/* Allocate a dmatag representing the capabilities of this attachment */
-	/* XXX Should be a child of the PCI bus dma tag */
 	if (bus_dma_tag_create(	/* PCI parent */ bus_get_dma_tag(dev),
 				/* alignemnt */	1,
 				/* boundary  */	0,
