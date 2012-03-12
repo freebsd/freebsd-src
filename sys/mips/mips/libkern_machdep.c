195d12217257f3fd4b328858cbb31ccf5a101652
jmallett <jmallett@FreeBSD.org>
2012-03-12 21:25:32 +0000

    o) Use ABI, not ISA_* options, to determine whether to compile bits if libkern    required for the ABI the kernel is being built for.    XXX This is implemented in a kind-of nasty way that involves including source        files, but it's still an improvement. o) Retire ISA_* options since they're unused and were always wrong.

--- sys/mips/mips/libkern_machdep.c
+++ sys/mips/mips/libkern_machdep.c
@@ -1,23 +1,21 @@
 /*-
- * Copyright (c) 2005 M. Warner Losh.
+ * Copyright (c) 2012 Juli Mallett <jmallett@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions, and the following disclaimer,
- *    without modification, immediately at the beginning of the file.
+ *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
@@ -25,26 +23,17 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * from: src/sys/i386/include/_bus.h,v 1.1 2005/04/18 21:45:33 imp
  * $FreeBSD$
  */
 
-#ifndef MIPS_INCLUDE__BUS_H
-#define	MIPS_INCLUDE__BUS_H
-
 /*
- * Bus address and size types
+ * Include libkern support routines for 64-bit operations when building o32
+ * kernels.
  */
-#if defined(CPU_CNMIPS) && !defined(__mips_n64)
-typedef uint64_t bus_addr_t;
-#else
-typedef uintptr_t bus_addr_t;
+#if defined(__mips_o32)
+#include <libkern/divdi3.c>
+#include <libkern/moddi3.c>
+#include <libkern/qdivrem.c>
+#include <libkern/udivdi3.c>
+#include <libkern/umoddi3.c>
 #endif
-typedef uintptr_t bus_size_t;
-
-/*
- * Access methods for bus resources and address space.
- */
-typedef struct bus_space *bus_space_tag_t;
-typedef bus_addr_t bus_space_handle_t;
-#endif /* MIPS_INCLUDE__BUS_H */
