59d82ce6a09c970a27efb6680dc2f55ecf2ad374
jkim <jkim@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-03-16 19:22:29 +0000

    Do not unnecessarily clear display memory when switching modes.
    MFC after:	3 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@233042 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/fb/vesa.c
+++ sys/dev/fb/vesa.c
@@ -1313,7 +1313,7 @@ vesa_set_mode(video_adapter_t *adp, int mode)
 
 	if ((info.vi_flags & V_INFO_LINEAR) != 0)
 		mode |= 0x4000;
-	if (vesa_bios_set_mode(mode))
+	if (vesa_bios_set_mode(mode | 0x8000))
 		return (1);
 
 	/* Palette format is reset by the above VBE function call. */
