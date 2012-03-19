ffa12d7ce5b8c00c960dd757fbe4313da48fc220
tijl <tijl@FreeBSD.org>
2012-03-19 21:29:57 +0000

    Copy i386 psl.h to x86 and replace amd64/i386/pc98 psl.h with stubs.

--- sys/ia64/ia32/ia32_signal.c
+++ sys/ia64/ia32/ia32_signal.c
@@ -72,7 +72,7 @@ __FBSDID("$FreeBSD$");
 #include <compat/freebsd32/freebsd32_util.h>
 #include <compat/freebsd32/freebsd32_proto.h>
 #include <compat/ia32/ia32_signal.h>
-#include <i386/include/psl.h>
+#include <x86/include/psl.h>
 #include <x86/include/segments.h>
 #include <i386/include/specialreg.h>
 
