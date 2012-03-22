e02fd6b8423e63f1fdbfc1f984d7c7291a1bacd1
alc <alc@FreeBSD.org>
2012-03-22 04:52:51 +0000

    Handle spurious page faults that may occur in no-fault sections of the kernel.
    When access restrictions are added to a page table entry, we flush the
    corresponding virtual address mapping from the TLB.  In contrast, when
    access restrictions are removed from a page table entry, we do not
    flush the virtual address mapping from the TLB.  This is exactly as
    recommended in AMD's documentation.  In effect, when access
    restrictions are removed from a page table entry, AMD's MMUs will
    transparently refresh a stale TLB entry.  In short, this saves us from
    having to perform potentially costly TLB flushes.  In contrast,
    Intel's MMUs are allowed to generate a spurious page fault based upon
    the stale TLB entry.  Usually, such spurious page faults are handled
    by vm_fault() without incident.  However, when we are executing
    no-fault sections of the kernel, we are not allowed to execute
    vm_fault().  This change introduces special-case handling for spurious
    page faults that occur in no-fault sections of the kernel.
    
    In collaboration with:	kib
    Tested by:		gibbs (an earlier version)
    
    I would also like to acknowledge Hiroki Sato's assistance in
    diagnosing this problem.
    
    MFC after:	1 week

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -417,6 +417,7 @@ do {									\
 #define	TDP_IGNSUSP	0x00800000 /* Permission to ignore the MNTK_SUSPEND* */
 #define	TDP_AUDITREC	0x01000000 /* Audit record pending on thread */
 #define	TDP_RFPPWAIT	0x02000000 /* Handle RFPPWAIT on syscall exit */
+#define	TDP_RESETSPUR	0x04000000 /* Reset spurious page fault history. */
 
 /*
  * Reasons that the current thread can not be run yet.
