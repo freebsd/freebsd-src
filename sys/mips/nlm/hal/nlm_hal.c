f651ac9a06b49c0bd8329b66308fd9663af3d8d5
jchandra <jchandra@FreeBSD.org>
2012-03-27 07:39:05 +0000

    Support for XLP4xx and XLP 8xx B0 revision
    - Add 4xx processor IDs, add workaround in CPU detection code.
    - Update frequency detection code for XLP 8xx.
    - Add setting device frequency code.
    - Update processor ID checking code.

--- sys/mips/nlm/hal/nlm_hal.c
+++ sys/mips/nlm/hal/nlm_hal.c
@@ -55,33 +55,81 @@ int pic_irt_pcie_lnk2;
 int pic_irt_pcie_lnk3;
 
 uint32_t
-xlp_get_cpu_frequency(int core)
+xlp_get_cpu_frequency(int node, int core)
 {
-	uint64_t sysbase = nlm_get_sys_regbase(nlm_nodeid());
-	uint64_t num;
+	uint64_t sysbase = nlm_get_sys_regbase(node);
 	unsigned int pll_divf, pll_divr, dfs_div, ext_div;
-	unsigned int rstval, dfsval, denom;
+	unsigned int rstval, dfsval;
 
 	rstval = nlm_read_sys_reg(sysbase, SYS_POWER_ON_RESET_CFG);
 	dfsval = nlm_read_sys_reg(sysbase, SYS_CORE_DFS_DIV_VALUE);
 	pll_divf = ((rstval >> 10) & 0x7f) + 1;
 	pll_divr = ((rstval >> 8)  & 0x3) + 1;
-	ext_div  = ((rstval >> 30) & 0x3) + 1;
-	dfs_div  = ((dfsval >> (core * 4)) & 0xf) + 1;
+	if (!nlm_is_xlp8xx_ax())
+		ext_div  = ((rstval >> 30) & 0x3) + 1;
+	else
+		ext_div  = 1;
+	dfs_div  = ((dfsval >> (core << 2)) & 0xf) + 1;
 
-	num = 800000000ULL * pll_divf;
-	denom = 3 * pll_divr * ext_div * dfs_div;
-	num = num/denom;
-	return (num);
+	return ((800000000ULL * pll_divf)/(3 * pll_divr * ext_div * dfs_div));
+}
+
+static u_int
+nlm_get_device_frequency(uint64_t sysbase, int devtype)
+{
+	uint32_t pllctrl, dfsdiv, spf, spr, div_val;
+	int extra_div;
+
+	pllctrl = nlm_read_sys_reg(sysbase, SYS_PLL_CTRL);
+	if (devtype <= 7)
+		div_val = nlm_read_sys_reg(sysbase, SYS_DFS_DIV_VALUE0);
+	else {
+		devtype -= 8;
+		div_val = nlm_read_sys_reg(sysbase, SYS_DFS_DIV_VALUE1);
+	}
+	dfsdiv = ((div_val >> (devtype << 2)) & 0xf) + 1;
+	spf = (pllctrl >> 3 & 0x7f) + 1;
+	spr = (pllctrl >> 1 & 0x03) + 1;
+	extra_div = nlm_is_xlp8xx_ax() ? 1 : 2;
+
+	return ((400 * spf) / (3 * extra_div * spr * dfsdiv));
+}
+
+int
+nlm_set_device_frequency(int node, int devtype, int frequency)
+{
+	uint64_t sysbase;
+	u_int cur_freq;
+	int dec_div;
+
+	sysbase = nlm_get_sys_regbase(node);
+	cur_freq = nlm_get_device_frequency(sysbase, devtype);
+	if (cur_freq < (frequency - 5))
+		dec_div = 1;
+	else
+		dec_div = 0;
+
+	for(;;) {
+		if ((cur_freq >= (frequency - 5)) && (cur_freq <= frequency))
+			break;
+		if (dec_div)
+			nlm_write_sys_reg(sysbase, SYS_DFS_DIV_DEC_CTRL,
+			    (1 << devtype));
+		else
+			nlm_write_sys_reg(sysbase, SYS_DFS_DIV_INC_CTRL,
+			    (1 << devtype));
+		cur_freq = nlm_get_device_frequency(sysbase, devtype);
+	}
+	return (nlm_get_device_frequency(sysbase, devtype));
 }
 
 void
-nlm_pic_irt_init(void)
+nlm_pic_irt_init(int node)
 {
-	pic_irt_ehci0 = nlm_irtstart(nlm_get_usb_pcibase(nlm_nodeid(), 0));
-	pic_irt_ehci1 = nlm_irtstart(nlm_get_usb_pcibase(nlm_nodeid(), 3));
-	pic_irt_uart0 = nlm_irtstart(nlm_get_uart_pcibase(nlm_nodeid(), 0));
-	pic_irt_uart1 = nlm_irtstart(nlm_get_uart_pcibase(nlm_nodeid(), 1));
+	pic_irt_ehci0 = nlm_irtstart(nlm_get_usb_pcibase(node, 0));
+	pic_irt_ehci1 = nlm_irtstart(nlm_get_usb_pcibase(node, 3));
+	pic_irt_uart0 = nlm_irtstart(nlm_get_uart_pcibase(node, 0));
+	pic_irt_uart1 = nlm_irtstart(nlm_get_uart_pcibase(node, 1));
 
 	/* Hardcoding the PCIE IRT information as PIC doesn't 
 	   understand any value other than 78,79,80,81 for PCIE0/1/2/3 */
@@ -107,24 +155,25 @@ int
 xlp_irt_to_irq(int irt)
 {
 	if (irt == pic_irt_ehci0)
-	    return PIC_EHCI_0_IRQ;
+		return PIC_EHCI_0_IRQ;
 	else if (irt == pic_irt_ehci1)
-	    return PIC_EHCI_1_IRQ;
+		return PIC_EHCI_1_IRQ;
 	else if (irt == pic_irt_uart0)
-	    return PIC_UART_0_IRQ;
+		return PIC_UART_0_IRQ;
 	else if (irt == pic_irt_uart1)
-	    return PIC_UART_1_IRQ;
+		return PIC_UART_1_IRQ;
 	else if (irt == pic_irt_pcie_lnk0)
-	    return PIC_PCIE_0_IRQ;
+		return PIC_PCIE_0_IRQ;
 	else if (irt == pic_irt_pcie_lnk1)
-	    return PIC_PCIE_1_IRQ;
+		return PIC_PCIE_1_IRQ;
 	else if (irt == pic_irt_pcie_lnk2)
-	    return PIC_PCIE_2_IRQ;
+		return PIC_PCIE_2_IRQ;
 	else if (irt == pic_irt_pcie_lnk3)
-	    return PIC_PCIE_3_IRQ;
+		return PIC_PCIE_3_IRQ;
 	else {
-	    printf("Cannot find irq for IRT %d\n", irt);
-	    return 0;
+		if (bootverbose)
+			printf("Cannot find irq for IRT %d\n", irt);
+		return 0;
 	 }
 }
 
@@ -156,15 +205,15 @@ int
 xlp_irq_is_picintr(int irq)
 {
 	switch (irq) {
-		case PIC_MMC_IRQ : return 1;
- 		case PIC_EHCI_0_IRQ : return 1;
- 		case PIC_EHCI_1_IRQ : return 1;
-		case PIC_UART_0_IRQ : return 1;
-		case PIC_UART_1_IRQ : return 1;
-		case PIC_PCIE_0_IRQ : return 1;
-		case PIC_PCIE_1_IRQ : return 1;
-		case PIC_PCIE_2_IRQ : return 1;
-		case PIC_PCIE_3_IRQ : return 1;
-		default: return 0;
+ 		case PIC_EHCI_0_IRQ :
+ 		case PIC_EHCI_1_IRQ :
+		case PIC_UART_0_IRQ :
+		case PIC_UART_1_IRQ :
+		case PIC_PCIE_0_IRQ :
+		case PIC_PCIE_1_IRQ :
+		case PIC_PCIE_2_IRQ :
+		case PIC_PCIE_3_IRQ :
+			return (1);
+		default: return (0);
 	}
 }
