5ac9a86d62830b558075d5d1ec0e67514451a0a1
kib <kib@FreeBSD.org>
2012-04-05 10:38:07 +0000

    MFC r233307: Use xmalloc() instead of malloc() in the places where malloc() calls are assumed to not fail.
    Make the xcalloc() calling conventions follow the calloc(3) calling
    conventions and replace unchecked calls to calloc() with calls to
    xcalloc().
    
    Remove redundand declarations from xmalloc.c, which are already
    present in rtld.h.

--- libexec/rtld-elf/xmalloc.c
+++ libexec/rtld-elf/xmalloc.c
@@ -32,14 +32,17 @@
 #include "rtld.h"
 #include "rtld_printf.h"
 
-void *xcalloc(size_t);
-void *xmalloc(size_t);
-char *xstrdup(const char *);
-
 void *
-xcalloc(size_t size)
+xcalloc(size_t number, size_t size)
 {
-    return memset(xmalloc(size), 0, size);
+	void *p;
+
+	p = calloc(number, size);
+	if (p == NULL) {
+		rtld_fdputstr(STDERR_FILENO, "Out of memory\n");
+		_exit(1);
+	}
+	return (p);
 }
 
 void *
