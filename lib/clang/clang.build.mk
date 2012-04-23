db3212b37546201ce99d16cce3867ca5f6a895f9
Dimitry Andric <dim@FreeBSD.org>
2012-04-23 06:33:27 +0000

    Revert r234356 for now, as it leads to run-time problems on 32-bit PowerPC.  Note this will break world.
    Reported by:	andreast
    Pointy hat to:	dim

--- lib/clang/clang.build.mk
+++ lib/clang/clang.build.mk
@@ -11,12 +11,6 @@ CFLAGS+=-I${LLVM_SRCS}/include -I${CLANG_SRCS}/include \
 # LLVM is not strict aliasing safe as of 12/31/2011
 CFLAGS+= -fno-strict-aliasing
 
-# Work around an issue on 32-bit PowerPC, where the clang executable can get
-# too big, causing 'relocation truncated to fit' errors at link time.
-.if ${MACHINE_ARCH} == "powerpc"
-CFLAGS+=-mlongcall
-.endif
-
 TARGET_ARCH?=	${MACHINE_ARCH}
 CFLAGS+=-DLLVM_DEFAULT_TARGET_TRIPLE=\"${TARGET_ARCH:C/amd64/x86_64/}-unknown-freebsd10.0\"
 
