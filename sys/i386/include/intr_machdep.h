0b98e6d835caa6e794deb417e434762d6a00031b
attilio <attilio@FreeBSD.org>
2012-04-26 20:24:25 +0000

    Clean up the intr* MD KPI from the SMP dependency, removing a cause of discrepancy between modules and kernel, but deal with SMP differences within the functions themselves.
    As an added bonus this also helps in terms of code readability.
    
    Requested by:	gibbs
    Reviewed by:	jhb, marius
    MFC after:	1 week

--- sys/i386/include/intr_machdep.h
+++ sys/i386/include/intr_machdep.h
@@ -131,14 +131,10 @@ int	elcr_probe(void);
 enum intr_trigger elcr_read_trigger(u_int irq);
 void	elcr_resume(void);
 void	elcr_write_trigger(u_int irq, enum intr_trigger trigger);
-#ifdef SMP
 void	intr_add_cpu(u_int cpu);
-#endif
 int	intr_add_handler(const char *name, int vector, driver_filter_t filter,
     driver_intr_t handler, void *arg, enum intr_type flags, void **cookiep);
-#ifdef SMP
 int	intr_bind(u_int vector, u_char cpu);
-#endif
 int	intr_config_intr(int vector, enum intr_trigger trig,
     enum intr_polarity pol);
 int	intr_describe(u_int vector, void *ih, const char *descr);
