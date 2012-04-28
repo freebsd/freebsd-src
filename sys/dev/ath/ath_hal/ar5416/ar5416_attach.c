3fff32208405b61903324cdc5af345fa762205a0
adrian <adrian@FreeBSD.org>
2012-04-28 03:07:36 +0000

    Add an AR5416 PCU DMA stop method, as a check for the AR9130 is needed.
    The reference driver has a 3ms delay for the AR9130 but I'm not as yet
    sure why.  From what I can gather, it's likely waiting for some FIFO
    flush to occur.
    
    At some point in the future it may be worthwhile adding a WMAC
    FIFO flush here, but that'd require some side-call through to the SoC
    DDR flush routines.
    
    Obtained from:	Atheros

--- sys/dev/ath/ath_hal/ar5416/ar5416_attach.c
+++ sys/dev/ath/ath_hal/ar5416/ar5416_attach.c
@@ -119,6 +119,7 @@ ar5416InitState(struct ath_hal_5416 *ahp5416, uint16_t devid, HAL_SOFTC sc,
 	/* Receive Functions */
 	ah->ah_getRxFilter		= ar5416GetRxFilter;
 	ah->ah_setRxFilter		= ar5416SetRxFilter;
+	ah->ah_stopDmaReceive		= ar5416StopDmaReceive;
 	ah->ah_startPcuReceive		= ar5416StartPcuReceive;
 	ah->ah_stopPcuReceive		= ar5416StopPcuReceive;
 	ah->ah_setupRxDesc		= ar5416SetupRxDesc;
