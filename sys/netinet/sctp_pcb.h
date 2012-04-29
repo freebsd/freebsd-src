0a558aa5836210953e07f22de8927cf5949850fa
tuexen <tuexen@FreeBSD.org>
2012-04-29 16:10:24 +0000

    MFC r233660:
    Make stream our stream reset implementation
    compliant to RFC6525.
    From rrs@.

--- sys/netinet/sctp_pcb.h
+++ sys/netinet/sctp_pcb.h
@@ -399,6 +399,7 @@ struct sctp_inpcb {
 	uint32_t sctp_frag_point;
 	uint32_t partial_delivery_point;
 	uint32_t sctp_context;
+	uint8_t local_strreset_support;
 	uint32_t sctp_cmt_on_off;
 	uint32_t sctp_ecn_enable;
 	struct sctp_nonpad_sndrcvinfo def_send;
