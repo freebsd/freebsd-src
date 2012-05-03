71aa9fe79467671a4b3b43a2ba1e299565a0cc06
jamie <jamie@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-05-03 21:39:23 +0000

    Add a meta-parameter IP__NULL to enum intparam, instead of mixing enum values and zeroes.  This keeps clang happy (and is just good form).
    Submitted by:	dim
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@234988 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/jail/config.c
+++ usr.sbin/jail/config.c
@@ -328,7 +328,7 @@ add_param(struct cfjail *j, const struct cfparam *p, enum intparam ipnum,
 		}
 	} else {
 		flags = PF_APPEND;
-		if (ipnum != 0) {
+		if (ipnum != IP__NULL) {
 			name = intparams[ipnum].name;
 			flags |= intparams[ipnum].flags;
 		} else if ((cs = strchr(value, '='))) {
@@ -350,7 +350,7 @@ add_param(struct cfjail *j, const struct cfparam *p, enum intparam ipnum,
 	}
 
 	/* See if this parameter has already been added. */
-	if (ipnum != 0)
+	if (ipnum != IP__NULL)
 		dp = j->intparams[ipnum];
 	else
 		TAILQ_FOREACH(dp, &j->params, tq)
@@ -375,10 +375,10 @@ add_param(struct cfjail *j, const struct cfparam *p, enum intparam ipnum,
 		np->flags = flags;
 		np->gen = 0;
 		TAILQ_INSERT_TAIL(&j->params, np, tq);
-		if (ipnum != 0)
+		if (ipnum != IP__NULL)
 			j->intparams[ipnum] = np;
 		else
-			for (ipnum = 1; ipnum < IP_NPARAM; ipnum++)
+			for (ipnum = IP__NULL + 1; ipnum < IP_NPARAM; ipnum++)
 				if (!(intparams[ipnum].flags & PF_CONV) &&
 				    equalopts(name, intparams[ipnum].name)) {
 					j->intparams[ipnum] = np;
