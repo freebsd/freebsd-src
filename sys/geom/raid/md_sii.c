01dfacecfbabda2916c7912c07c6af7bad444006
Alexander Motin <mav@FreeBSD.org>
2012-05-06 15:56:07 +0000

    MFC r234601: Add sos@ copyrights to RAID metadata modules, respecting his efforts in decoding metadata formats in ataraid(4) code.

--- sys/geom/raid/md_sii.c
+++ sys/geom/raid/md_sii.c
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2011 Alexander Motin <mav@FreeBSD.org>
+ * Copyright (c) 2000 - 2008 Søren Schmidt <sos@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
