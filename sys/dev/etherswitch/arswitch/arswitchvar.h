323587f5a4add945247bc62cf7aae50c37ef63c3
adrian <adrian@FreeBSD.org>
2012-05-11 20:53:20 +0000

    Commit the first pass of the etherswitch support.
    This is designed to support the very basic ethernet switch chip behaviour,
    specifically:
    
    * accessing switch register space;
    * accessing per-PHY registers (for switches that actually expose PHYs);
    * basic vlan group support, which applies for the rtl8366 driver but not
      for the atheros switches.
    
    This also includes initial support for:
    
    * rtl8366rb support - which is a 10/100/1000 switch which supports
      vlan groups;
    * Initial Atheros AR8316 switch support - which is a 10/100/1000 switch
      which supports an alternate vlan configuration (so the vlan group
      methods are stubbed.)
    
    The general idea here is that the switch driver may speak to a variety of
    backend busses (mdio, i2c, spi, whatever) and expose:
    
    * If applicable, one or more MDIO busses which ethernet interfaces can
      then attach PHYs to via miiproxy/mdioproxy;
    
    * exposes miibusses, one for each port at the moment, so ..
    
    * .. a PHY can be exposed on each miibus, for each switch port, with all
      of the existing MII/ifnet framework.
    
    However:
    
    * The ifnet is manually created for now, and it isn't linked into the
      interface list, nor can you (currently) send/receive frames on this ifnet.
      At some point in the future there may be _some_ support for this, for
      switches with a multi-port, isolated mode.
    
    * I'm still in the process of sorting out correct(er) locking.
    
    TODO:
    
    * ray's switch code in zrouter (zrouter.org) includes a much more developed
      newbus API that covers the various switch methods, as well as a
      capability API so drivers, the switch layer and the userland utility
      can properly control the subset of supported features.
    
      The plan is to sort that out later, once the rest of ray's switch drivers
      are brought over and extended to export MII busses and PHYs.
    
    Submitted by:	Stefan Bethke <stb@lassitu.de>
    Reviewed by:	ray

---
+++ sys/dev/etherswitch/arswitch/arswitchvar.h
@@ -0,0 +1,91 @@
+/*-
+ * Copyright (c) 2011-2012 Stefan Bethke.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+#ifndef	__ARSWITCHVAR_H__
+#define	__ARSWITCHVAR_H__
+
+typedef enum {
+	AR8X16_SWITCH_NONE,
+	AR8X16_SWITCH_AR8216,
+	AR8X16_SWITCH_AR8226,
+	AR8X16_SWITCH_AR8316,
+} ar8x16_switch_type;
+
+/*
+ * XXX TODO: start using this where required
+ */
+#define	AR8X16_IS_SWITCH(_sc, _type) \
+	    (!!((_sc)->sc_switchtype == AR8X16_SWITCH_ ## _type))
+
+struct arswitch_softc;
+
+struct arswitch_softc {
+	struct mtx	sc_mtx;		/* serialize access to softc */
+	device_t	sc_dev;
+	int		phy4cpu;	/* PHY4 is connected to the CPU */
+	int		numphys;	/* PHYs we manage */
+	int		is_rgmii;	/* PHY mode is RGMII (XXX which PHY?) */
+	int		is_gmii;	/* PHY mode is GMII (XXX which PHY?) */
+	int		page;
+	ar8x16_switch_type	sc_switchtype;
+	char		*ifname[AR8X16_NUM_PHYS];
+	device_t	miibus[AR8X16_NUM_PHYS];
+	struct ifnet	*ifp[AR8X16_NUM_PHYS];
+	struct callout	callout_tick;
+	etherswitch_info_t info;
+
+	struct {
+		int (* arswitch_hw_setup) (struct arswitch_softc *);
+		int (* arswitch_hw_global_setup) (struct arswitch_softc *);
+	} hal;
+};
+
+#define	ARSWITCH_LOCK(_sc)			\
+	    mtx_lock(&(_sc)->sc_mtx)
+#define	ARSWITCH_UNLOCK(_sc)			\
+	    mtx_unlock(&(_sc)->sc_mtx)
+#define	ARSWITCH_LOCK_ASSERT(_sc, _what)	\
+	    mtx_assert(&(_s)c->sc_mtx, (_what))
+#define	ARSWITCH_TRYLOCK(_sc)			\
+	    mtx_trylock(&(_sc)->sc_mtx)
+
+#if defined(DEBUG)
+#define DPRINTF(dev, args...) device_printf(dev, args)
+#define DEVERR(dev, err, fmt, args...) do { \
+		if (err != 0) device_printf(dev, fmt, err, args); \
+	} while (0)
+#define DEBUG_INCRVAR(var)	do { \
+		var++; \
+	} while (0)
+#else
+#define DPRINTF(dev, args...)
+#define DEVERR(dev, err, fmt, args...)
+#define DEBUG_INCRVAR(var)
+#endif
+
+#endif	/* __ARSWITCHVAR_H__ */
+
