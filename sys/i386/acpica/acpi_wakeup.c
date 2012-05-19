5374cd448cdcfb007709e244eeb204d8d1644807
iwasaki <iwasaki@FreeBSD.org>
2012-05-19 02:31:38 +0000

    Revert the previous commit on wakecode address verbose printing. This broke PAE kernel building.

--- sys/i386/acpica/acpi_wakeup.c
+++ sys/i386/acpica/acpi_wakeup.c
@@ -375,6 +375,6 @@ acpi_install_wakeup_handler(struct acpi_softc *sc)
 	WAKECODE_FIXUP(wakeup_ret, void *, resumectx);
 
 	if (bootverbose)
-		device_printf(sc->acpi_dev, "wakeup code va %p pa %p\n",
-		    (void *)sc->acpi_wakeaddr, (void *)sc->acpi_wakephys);
+		device_printf(sc->acpi_dev, "wakeup code va %#x pa %#jx\n",
+		    sc->acpi_wakeaddr, (uintmax_t)sc->acpi_wakephys);
 }
