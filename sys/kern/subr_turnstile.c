a3162fe3a45223b9e0d5518f1bd405b814add5b2
jhb <jhb@FreeBSD.org>
2012-05-21 21:14:09 +0000

    MFC 234190,234196,234280: - Extend the KDB interface to add a per-debugger callback to print a   backtrace for an arbitrary thread (rather than the calling thread).   A kdb_backtrace_thread() wrapper function uses the configured debugger   if possible, otherwise it falls back to using stack(9) if that is   available. - Replace a direct call to db_trace_thread() in propagate_priority()   with a call to kdb_backtrace_thread() instead.

--- sys/kern/subr_turnstile.c
+++ sys/kern/subr_turnstile.c
@@ -65,6 +65,7 @@ __FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
 #include <sys/systm.h>
+#include <sys/kdb.h>
 #include <sys/kernel.h>
 #include <sys/ktr.h>
 #include <sys/lock.h>
@@ -78,7 +79,6 @@ __FBSDID("$FreeBSD$");
 #include <vm/uma.h>
 
 #ifdef DDB
-#include <sys/kdb.h>
 #include <ddb/ddb.h>
 #include <sys/lockmgr.h>
 #include <sys/sx.h>
@@ -216,9 +216,7 @@ propagate_priority(struct thread *td)
 			printf(
 		"Sleeping thread (tid %d, pid %d) owns a non-sleepable lock\n",
 			    td->td_tid, td->td_proc->p_pid);
-#ifdef DDB
-			db_trace_thread(td, -1);
-#endif
+			kdb_backtrace_thread(td);
 			panic("sleeping thread");
 		}
 
