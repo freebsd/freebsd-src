freebsd/freebsd-src

commit e9ae393d7ee6c3bcc0085056aa2962e9e5eead5a

Expose all of the C99 limits.h stuff when we're in C++11 mode (or some approximation thereof).  C++11 finally adds long long to C++.  Now even C++ programmers are allowed to use 64-bit integers!
Approved by:	dim (mentor)


git-svn-id: svn+ssh://svn.freebsd.org/base/head@227475 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/sys/cdefs.h
@@ -253,6 +253,17 @@
 #define	__LONG_LONG_SUPPORTED
 #endif
 
+/* C++11 exposes a load of C99 stuff */
+#if __cplusplus >= 201103L
+#	define	__LONG_LONG_SUPPORTED
+#	ifndef __STDC_LIMIT_MACROS
+#		define __STDC_LIMIT_MACROS
+#	endif
+#	ifndef __STDC_CONSTANT_MACROS
+#		define __STDC_CONSTANT_MACROS
+#	endif
+#endif
+
 /*
  * GCC 2.95 provides `__restrict' as an extension to C90 to support the
  * C99-specific `restrict' type qualifier.  We happen to use `__restrict' as

commit 1a73c9596540216419dfd57d5c0a4276e6a0a0e3

MFC r227478:
Fix kernel build breakage after r227475.  I had forgotten kernels are
built with -Wundef, as opposed to world.

Additionally, cdefs.h tends to not use indentation for preprocessor
directives, so remove that too.

Pointy hat to:	me

sys/sys/cdefs.h
@@ -309,14 +309,14 @@
 #endif
 
 /* C++11 exposes a load of C99 stuff */
-#if __cplusplus >= 201103L
-#	define	__LONG_LONG_SUPPORTED
-#	ifndef __STDC_LIMIT_MACROS
-#		define __STDC_LIMIT_MACROS
-#	endif
-#	ifndef __STDC_CONSTANT_MACROS
-#		define __STDC_CONSTANT_MACROS
-#	endif
+#if defined(__cplusplus) && __cplusplus >= 201103L
+#define	__LONG_LONG_SUPPORTED
+#ifndef	__STDC_LIMIT_MACROS
+#define	__STDC_LIMIT_MACROS
+#endif
+#ifndef	__STDC_CONSTANT_MACROS
+#define	__STDC_CONSTANT_MACROS
+#endif
 #endif
 
 /*