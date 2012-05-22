a195c3c306fae79544be9693cf1fafa508d18380
harti <harti@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-05-22 07:23:41 +0000

    Make dumptid non-static. It is used by libkvm to detect whether this is a VNET-kernel or not. gcc used to put the static symbol into the symbol table, clang does not. This fixes the 'netstat: no namelist' error seen on clang+VNET systems.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@235777 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/kern_shutdown.c
+++ sys/kern/kern_shutdown.c
@@ -151,7 +151,7 @@ static struct dumperinfo dumper;	/* our selected dumper */
 
 /* Context information for dump-debuggers. */
 static struct pcb dumppcb;		/* Registers. */
-static lwpid_t dumptid;			/* Thread ID. */
+lwpid_t dumptid;			/* Thread ID. */
 
 static void poweroff_wait(void *, int);
 static void shutdown_halt(void *junk, int howto);
