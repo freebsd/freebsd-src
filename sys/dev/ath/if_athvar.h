a217e03ba9045e1052cf9bcd4272070d9d1991a9
adrian <adrian@FreeBSD.org>
2012-05-22 06:31:03 +0000

    Fix up some corner cases with aggregation handling.
    I've come across a weird scenario in net80211 where two TX streams will
    happily attempt to setup an aggregation session together.
    If we're very lucky, it happens concurrently on separate CPUs and the
    total lack of locking in the net80211 aggregation code causes this stuff
    to race. Badly.
    
    So >1 call would occur to the ath(4) addba start, but only one call would
    complete to addba complete or timeout.  The TID would thus stay paused.
    
    The real fix is to implement some proper per-node (or maybe per-TID)
    locking in net80211, which then could be leveraged by the ath(4) TX
    aggregation code.
    
    Whilst I'm at it, shuffle around the debugging messages a bit.
    I like to keep people on their toes.

--- sys/dev/ath/if_athvar.h
+++ sys/dev/ath/if_athvar.h
@@ -106,6 +106,7 @@ struct ath_tid {
 	TAILQ_ENTRY(ath_tid)	axq_qelem;
 	int			sched;
 	int			paused;	/* >0 if the TID has been paused */
+	int			addba_tx_pending;	/* TX ADDBA pending */
 	int			bar_wait;	/* waiting for BAR */
 	int			bar_tx;		/* BAR TXed */
 
