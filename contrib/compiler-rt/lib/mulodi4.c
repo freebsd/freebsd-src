b8437abc6a180b344ae66b81a59a9df413882c13
Marius Strobl <marius@FreeBSD.org>
2012-05-25 17:14:47 +0000

    MFC: r222656
    Upgrade libcompiler_rt from revision 117047 to 132478.
    
    It seems there have only been a small amount to the compiler-rt source
    code in the mean time. I'd rather have the code in sync as much as
    possible by the time we release 9.0. Changes:
    
    - The libcompiler_rt library is now dual licensed under both the
      University of Illinois "BSD-Like" license and the MIT license.
    
    - Our local modifications for using .hidden instead of .private_extern
      have been upstreamed, meaning our changes to lib/assembly.h can now be
      reverted.
    
    - A possible endless recursion in __modsi3() has been fixed.
    
    - Support for ARM EABI has been added, but it has no effect on FreeBSD
      (yet).
    
    - The functions __udivmodsi4 and __divmodsi4 have been added.
    
    Requested by:	many, including bf@ and Pedro Giffuni

--- contrib/compiler-rt/lib/mulodi4.c
+++ contrib/compiler-rt/lib/mulodi4.c
@@ -1,4 +1,4 @@
-/*===-- mulvdi3.c - Implement __mulvdi3 -----------------------------------===
+/*===-- mulodi4.c - Implement __mulodi4 -----------------------------------===
  *
  *                     The LLVM Compiler Infrastructure
  *
@@ -7,7 +7,7 @@
  *
  * ===----------------------------------------------------------------------===
  *
- * This file implements __mulvdi3 for the compiler_rt library.
+ * This file implements __mulodi4 for the compiler_rt library.
  *
  * ===----------------------------------------------------------------------===
  */
@@ -16,41 +16,43 @@
 
 /* Returns: a * b */
 
-/* Effects: aborts if a * b overflows */
+/* Effects: sets *overflow to 1  if a * b overflows */
 
 di_int
-__mulvdi3(di_int a, di_int b)
+__mulodi4(di_int a, di_int b, int* overflow)
 {
     const int N = (int)(sizeof(di_int) * CHAR_BIT);
     const di_int MIN = (di_int)1 << (N-1);
     const di_int MAX = ~MIN;
+    *overflow = 0; 
+    di_int result = a * b;
     if (a == MIN)
     {
-        if (b == 0 || b == 1)
-            return a * b;
-        compilerrt_abort();
+        if (b != 0 && b != 1)
+	    *overflow = 1;
+	return result;
     }
     if (b == MIN)
     {
-        if (a == 0 || a == 1)
-            return a * b;
-        compilerrt_abort();
+        if (a != 0 && a != 1)
+	    *overflow = 1;
+        return result;
     }
     di_int sa = a >> (N - 1);
     di_int abs_a = (a ^ sa) - sa;
     di_int sb = b >> (N - 1);
     di_int abs_b = (b ^ sb) - sb;
     if (abs_a < 2 || abs_b < 2)
-        return a * b;
+        return result;
     if (sa == sb)
     {
         if (abs_a > MAX / abs_b)
-            compilerrt_abort();
+            *overflow = 1;
     }
     else
     {
         if (abs_a > MIN / -abs_b)
-            compilerrt_abort();
+            *overflow = 1;
     }
-    return a * b;
+    return result;
 }
