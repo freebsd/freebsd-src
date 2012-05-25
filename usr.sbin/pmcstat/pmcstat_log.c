78cc499478f5e40d3881a4fff15f7010b3ba8e5f
Fabien Thomas <fabient@FreeBSD.org>
2012-05-25 07:23:24 +0000

    MFC r233611: - Support inlined location in calltree output.   In case of multiple level of inlining all the locations are flattened.   Require recent binutils/addr2line (head works or binutils from ports   with the right $PATH order). - Multiple fixes in the calltree output (recursion case, ...) - Fix the calltree top view that previously hide some shared nodes.
    Tested with Kcachegrind(kdesdk4)/qcachegrind(head).
    
    Sponsored by: NETASQ

--- usr.sbin/pmcstat/pmcstat_log.c
+++ usr.sbin/pmcstat/pmcstat_log.c
@@ -428,7 +428,9 @@ pmcstat_image_get_aout_params(struct pmcstat_image *image)
 
 	if ((fd = open(buffer, O_RDONLY, 0)) < 0 ||
 	    (nbytes = read(fd, &ex, sizeof(ex))) < 0) {
-		warn("WARNING: Cannot determine type of \"%s\"", path);
+		if (args.pa_verbosity >= 2)
+			warn("WARNING: Cannot determine type of \"%s\"",
+			    path);
 		image->pi_type = PMCSTAT_IMAGE_INDETERMINABLE;
 		if (fd != -1)
 			(void) close(fd);
@@ -638,8 +640,9 @@ pmcstat_image_get_elf_params(struct pmcstat_image *image)
 	if ((fd = open(buffer, O_RDONLY, 0)) < 0 ||
 	    (e = elf_begin(fd, ELF_C_READ, NULL)) == NULL ||
 	    (elf_kind(e) != ELF_K_ELF)) {
-		warnx("WARNING: Cannot determine the type of \"%s\".",
-		    buffer);
+		if (args.pa_verbosity >= 2)
+			warnx("WARNING: Cannot determine the type of \"%s\".",
+			    buffer);
 		goto done;
 	}
 
@@ -942,6 +945,7 @@ pmcstat_image_addr2line(struct pmcstat_image *image, uintfptr_t addr,
     char *funcname, size_t funcname_len)
 {
 	static int addr2line_warn = 0;
+	unsigned l;
 
 	char *sep, cmdline[PATH_MAX], imagepath[PATH_MAX];
 	int fd;
@@ -957,6 +961,11 @@ pmcstat_image_addr2line(struct pmcstat_image *image, uintfptr_t addr,
 			    pmcstat_string_unintern(image->pi_fullpath));
 		} else
 			close(fd);
+		/*
+		 * New addr2line support recursive inline function with -i
+		 * but the format does not add a marker when no more entries
+		 * are available.
+		 */
 		snprintf(cmdline, sizeof(cmdline), "addr2line -Cfe \"%s\"",
 		    imagepath);
 		image->pi_addr2line = popen(cmdline, "r+");
@@ -997,10 +1006,10 @@ pmcstat_image_addr2line(struct pmcstat_image *image, uintfptr_t addr,
 		return (0);
 	}
 	*sep = '\0';
-	*sourceline = atoi(sep+1);
-	if (*sourceline == 0)
+	l = atoi(sep+1);
+	if (l == 0)
 		return (0);
-
+	*sourceline = l;
 	return (1);
 }
 
