28809d30804dc464468071d2f85781153da91d13
Eygene Ryabinkin <rea@FreeBSD.org>
2012-06-03 18:05:19 +0000

    OpenSSH: allow VersionAddendum to be used again
    Prior to this, setting VersionAddendum will be a no-op: one will
    always have BASE_VERSION + " " + VERSION_HPN for VersionAddendum
    set in the config and a bare BASE_VERSION + VERSION_HPN when there
    is no VersionAddendum is set.
    
    HPN patch requires both parties to have the "hpn" inside their
    advertized versions, so we add VERSION_HPN to the VERSION_BASE
    if HPN is enabled and omitting it if HPN is disabled.
    
    VersionAddendum now uses the following logics:
     * unset (default value): append " " and VERSION_ADDENDUM;
     * VersionAddendum is set and isn't empty: append " "
       and VersionAddendum;
     * VersionAddendum is set and empty: don't append anything.
    
    Approved by: des
    Reviewed by: bz

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -1,14 +1,13 @@
 /* $OpenBSD: version.h,v 1.57 2010/03/07 22:01:32 djm Exp $ */
 /* $FreeBSD$ */
 
-#ifndef SSH_VERSION
+#ifndef _VERSION_H_
+#define _VERSION_H_
 
 #define SSH_VERSION_BASE        "OpenSSH_5.4p1"
 #define SSH_VERSION_ADDENDUM    "FreeBSD-20100308"
 #define SSH_VERSION_HPN		"_hpn13v11"
-#define SSH_VERSION		SSH_VERSION_BASE SSH_VERSION_HPN " " SSH_VERSION_ADDENDUM
-#define SSH_RELEASE             (ssh_version_get())
 
-const char *ssh_version_get(void);
+const char *ssh_version_get(int hpn_disabled);
 void ssh_version_set_addendum(const char *);
-#endif /* SSH_VERSION */
+#endif /* _VERSION_H_ */
