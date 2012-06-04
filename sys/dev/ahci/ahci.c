041e8cc5d05b87955a52af63b3c5d41ba66dbd13
Alexander Motin <mav@FreeBSD.org>
2012-06-04 07:03:56 +0000

    MFC r235333: Add two functions xpt_batch_start() and xpt_batch_done() to the CAM SIM KPI to allow drivers to handle request completion directly without passing them to the CAM SWI thread removing extra context switch. Modify all ATA/SATA drivers to use them.

--- sys/dev/ahci/ahci.c
+++ sys/dev/ahci/ahci.c
@@ -1458,7 +1458,9 @@ ahci_ch_intr_locked(void *data)
 	struct ahci_channel *ch = device_get_softc(dev);
 
 	mtx_lock(&ch->mtx);
+	xpt_batch_start(ch->sim);
 	ahci_ch_intr(data);
+	xpt_batch_done(ch->sim);
 	mtx_unlock(&ch->mtx);
 }
 
