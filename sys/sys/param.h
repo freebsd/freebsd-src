3471557f132a2e730d150c6f8e4bfee13dc4b102
Marius Strobl <marius@FreeBSD.org>
2012-06-05 19:59:09 +0000

    MFC: r236486
    Add nitems(), a macro for determining the number of elements in a
    statically-allocated array.
    
    Obtained from:	OpenBSD (in principle)

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -249,6 +249,7 @@
 #ifndef howmany
 #define	howmany(x, y)	(((x)+((y)-1))/(y))
 #endif
+#define	nitems(x)	(sizeof((x)) / sizeof((x)[0]))
 #define	rounddown(x, y)	(((x)/(y))*(y))
 #define	roundup(x, y)	((((x)+((y)-1))/(y))*(y))  /* to any y */
 #define	roundup2(x, y)	(((x)+((y)-1))&(~((y)-1))) /* if y is powers of two */
