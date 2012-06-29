3f636f0025bc34fea1fec2dd3eef4fee715bfd26
avg <avg@FreeBSD.org>
2012-06-29 09:56:22 +0000

    MFC r235153: sys/boot: add common CTASSERT definition

--- sys/boot/sparc64/loader/main.c
+++ sys/boot/sparc64/loader/main.c
@@ -74,12 +74,6 @@ __FBSDID("$FreeBSD$");
 #include "libofw.h"
 #include "dev_net.h"
 
-#ifndef CTASSERT
-#define	CTASSERT(x)		_CTASSERT(x, __LINE__)
-#define	_CTASSERT(x, y)		__CTASSERT(x, y)
-#define	__CTASSERT(x, y)	typedef char __assert ## y[(x) ? 1 : -1]
-#endif
-
 extern char bootprog_name[], bootprog_rev[], bootprog_date[], bootprog_maker[];
 
 enum {
