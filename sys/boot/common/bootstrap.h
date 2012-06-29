2ccfaaf3fde3eb4c69dded27a721d089aa79f1e8
avg <avg@FreeBSD.org>
2012-06-29 09:17:49 +0000

    MFC r235153: sys/boot: add common CTASSERT definition

--- sys/boot/common/bootstrap.h
+++ sys/boot/common/bootstrap.h
@@ -327,4 +327,10 @@ void	dev_cleanup(void);
 
 time_t	time(time_t *tloc);
 
+#ifndef CTASSERT                /* Allow lint to override */
+#define CTASSERT(x)             _CTASSERT(x, __LINE__)
+#define _CTASSERT(x, y)         __CTASSERT(x, y)
+#define __CTASSERT(x, y)        typedef char __assert ## y[(x) ? 1 : -1]
+#endif
+
 #endif /* !_BOOTSTRAP_H_ */
