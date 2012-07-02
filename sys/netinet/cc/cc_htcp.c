9e7959c371354a4226823599f850cc7cc009369e
Matthew D Fleming <mdf@FreeBSD.org>
2012-07-02 20:36:54 +0000

    MFC r217322.  Original commit log:
    sysctl(9) cleanup checkpoint: amd64 GENERIC builds cleanly.
    Commit the net* piece.
    
    The commit to HEAD was a superset of the proposed patch for PR
    kern/167357.

--- sys/netinet/cc/cc_htcp.c
+++ sys/netinet/cc/cc_htcp.c
@@ -168,8 +168,8 @@ static int htcp_rtt_ref;
 static int htcp_max_diff = INT_MAX / ((1 << HTCP_ALPHA_INC_SHIFT) * 10);
 
 /* Per-netstack vars. */
-static VNET_DEFINE(uint8_t, htcp_adaptive_backoff) = 0;
-static VNET_DEFINE(uint8_t, htcp_rtt_scaling) = 0;
+static VNET_DEFINE(u_int, htcp_adaptive_backoff) = 0;
+static VNET_DEFINE(u_int, htcp_rtt_scaling) = 0;
 #define	V_htcp_adaptive_backoff    VNET(htcp_adaptive_backoff)
 #define	V_htcp_rtt_scaling    VNET(htcp_rtt_scaling)
 
