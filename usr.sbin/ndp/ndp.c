8f654825b47109f5e752b4d9a2d08819b91df6c7
Christian Brueffer <brueffer@FreeBSD.org>
2012-07-02 17:48:19 +0000

    MFC: r217140
    Use ether_ntoa instead of home-grown version.
    
    PR:		169075
    Submitted by:	Alexandr <alter@alter.org.ua>

--- usr.sbin/ndp/ndp.c
+++ usr.sbin/ndp/ndp.c
@@ -148,7 +148,6 @@ static void getdefif __P((void));
 static void setdefif __P((char *));
 #endif
 static char *sec2str __P((time_t));
-static char *ether_str __P((struct sockaddr_dl *));
 static void ts_print __P((const struct timeval *));
 
 #ifdef ICMPV6CTL_ND6_DRLIST
@@ -806,17 +805,14 @@ getnbrinfo(addr, ifindex, warning)
 }
 
 static char *
-ether_str(sdl)
-	struct sockaddr_dl *sdl;
+ether_str(struct sockaddr_dl *sdl)
 {
 	static char hbuf[NI_MAXHOST];
-	u_char *cp;
 
-	if (sdl->sdl_alen) {
-		cp = (u_char *)LLADDR(sdl);
-		snprintf(hbuf, sizeof(hbuf), "%x:%x:%x:%x:%x:%x",
-		    cp[0], cp[1], cp[2], cp[3], cp[4], cp[5]);
-	} else
+	if (sdl->sdl_alen > 0)
+		strlcpy(hbuf, ether_ntoa((struct ether_addr *)LLADDR(sdl)),
+		    sizeof(hbuf));
+	else
 		snprintf(hbuf, sizeof(hbuf), "(incomplete)");
 
 	return(hbuf);
