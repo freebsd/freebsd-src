4a003da23ca8c1a9f309f5da04187b39ebbb0f12
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-07-06 14:28:18 +0000

    Add support for the 'invept' and 'invvpid' instructions.  Beyond simply adding appropriate table entries, the assembler had to be adjusted as these are the first non-SSE instructions to use a 3-byte opcode (and a mandatory prefix to boot).
    MFC after:	1 month
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@238167 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/binutils/gas/config/tc-i386.c
+++ contrib/binutils/gas/config/tc-i386.c
@@ -3990,6 +3990,16 @@ output_insn (void)
 	      goto check_prefix;
 	    }
 	}
+      else if (i.tm.base_opcode == 0x660f3880 || i.tm.base_opcode == 0x660f3881)
+	{
+	  /* invept and invvpid are 3 byte instructions with a
+	     mandatory prefix. */
+	  if (i.tm.base_opcode & 0xff000000)
+	    {
+	      prefix = (i.tm.base_opcode >> 24) & 0xff;
+	      add_prefix (prefix);
+	    }
+	}
       else if ((i.tm.base_opcode & 0xff0000) != 0)
 	{
 	  prefix = (i.tm.base_opcode >> 16) & 0xff;
@@ -4025,6 +4035,12 @@ output_insn (void)
 	{
 	  if ((i.tm.cpu_flags & (CpuSSSE3 | CpuSSE4)) != 0
 	      && (i.tm.cpu_flags & CpuABM) == 0)
+	    {
+	      p = frag_more (3);
+	      *p++ = (i.tm.base_opcode >> 16) & 0xff;
+	    }
+	  else if (i.tm.base_opcode == 0x660f3880 ||
+		   i.tm.base_opcode == 0x660f3881)
 	    {
 	      p = frag_more (3);
 	      *p++ = (i.tm.base_opcode >> 16) & 0xff;
