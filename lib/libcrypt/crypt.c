59e106bdae68138afba50dc1d1d19c389eeb50ff
des <des@FreeBSD.org>
2012-07-15 11:39:35 +0000

    MFH r236751: document sha256 / sha512 support MFH r236892: remove mention of auth.conf from programs that don't use it MFH r236963: remove dead code relating to auth.conf MFH r236965 r236966 r236967 r237005 r237006 r237011: retire auth.conf
    Approved by:    re

--- lib/libcrypt/crypt.c
+++ lib/libcrypt/crypt.c
@@ -79,23 +79,23 @@ static const struct {
 	}
 };
 
+#ifdef HAS_DES
+#define CRYPT_DEFAULT	"des"
+#else
+#define CRYPT_DEFAULT	"md5"
+#endif
+
 static int crypt_type = -1;
 
 static void
 crypt_setdefault(void)
 {
-	char *def;
 	size_t i;
 
 	if (crypt_type != -1)
 		return;
-	def = auth_getval("crypt_default");
-	if (def == NULL) {
-		crypt_type = 0;
-		return;
-	}
 	for (i = 0; i < sizeof(crypt_types) / sizeof(crypt_types[0]) - 1; i++) {
-		if (strcmp(def, crypt_types[i].name) == 0) {
+		if (strcmp(CRYPT_DEFAULT, crypt_types[i].name) == 0) {
 			crypt_type = (int)i;
 			return;
 		}
