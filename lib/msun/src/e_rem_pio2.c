9bb4a2ab0af32baa44354f6cb0c6115ad0a872e4
dim <dim@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-08-11 11:13:48 +0000

    Change a few extern inline functions in libm to static inline, since they need to refer to static constants, which C99 does not allow for extern inline functions.
    While here, change a comment in e_rem_pio2f.c to mention the correct
    number of bits.
    
    Reviewed by:	bde
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@239192 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/msun/src/e_rem_pio2.c
+++ lib/msun/src/e_rem_pio2.c
@@ -48,10 +48,10 @@ pio2_2t =  2.02226624879595063154e-21, /* 0x3BA3198A, 0x2E037073 */
 pio2_3  =  2.02226624871116645580e-21, /* 0x3BA3198A, 0x2E000000 */
 pio2_3t =  8.47842766036889956997e-32; /* 0x397B839A, 0x252049C1 */
 
-#ifndef INLINE_REM_PIO2
-extern
+#ifdef INLINE_REM_PIO2
+static __inline
 #endif
-__inline int
+int
 __ieee754_rem_pio2(double x, double *y)
 {
 	double z,w,t,r,fn;
