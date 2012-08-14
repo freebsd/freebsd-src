20495f54b9bd7aec0b5aa81c5dd4c2624616f4a9
adrian <adrian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-08-14 22:30:17 +0000

    Add an assertion to check that the given TXQ is _not_ locked.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@239261 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ath/if_athvar.h
+++ sys/dev/ath/if_athvar.h
@@ -327,7 +327,10 @@ struct ath_txq {
 #define	ATH_TXQ_LOCK_DESTROY(_tq)	mtx_destroy(&(_tq)->axq_lock)
 #define	ATH_TXQ_LOCK(_tq)		mtx_lock(&(_tq)->axq_lock)
 #define	ATH_TXQ_UNLOCK(_tq)		mtx_unlock(&(_tq)->axq_lock)
-#define	ATH_TXQ_LOCK_ASSERT(_tq)	mtx_assert(&(_tq)->axq_lock, MA_OWNED)
+#define	ATH_TXQ_LOCK_ASSERT(_tq)	\
+	    mtx_assert(&(_tq)->axq_lock, MA_OWNED)
+#define	ATH_TXQ_UNLOCK_ASSERT(_tq)	\
+	    mtx_assert(&(_tq)->axq_lock, MA_NOTOWNED)
 #define	ATH_TXQ_IS_LOCKED(_tq)		mtx_owned(&(_tq)->axq_lock)
 
 #define	ATH_TID_LOCK_ASSERT(_sc, _tid)	\
