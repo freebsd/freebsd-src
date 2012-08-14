6e8fcacda452f3f892a86d81bc52bd4673e6eb9e
Andreas Tobler <andreast@FreeBSD.org>
2012-08-14 05:16:35 +0000

    Fix typo in comment.

--- sys/dev/sound/macio/snapper.c
+++ sys/dev/sound/macio/snapper.c
@@ -174,7 +174,7 @@ MIXER_DECLARE(snapper_mixer);
 #define  SNAPPER_MCR1_W_16	0x00	/*  16 bit */
 #define  SNAPPER_MCR1_W_18	0x01	/*  18 bit */
 #define  SNAPPER_MCR1_W_20	0x02	/*  20 bit */
-#define  SNAPPER_MCR1_W_24	0x03	/*  20 bit */
+#define  SNAPPER_MCR1_W_24	0x03	/*  24 bit */
 #define SNAPPER_MCR2_DL	0x80	/* Download */
 #define SNAPPER_MCR2_AP	0x02	/* All pass mode */
 #define SNAPPER_ACR_ADM	0x80	/* ADC output mode */
