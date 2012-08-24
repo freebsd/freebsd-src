5fc110e79b5361769b97ff3d3508468dd7846e72
Gleb Smirnoff <glebius@FreeBSD.org>
2012-08-24 14:25:57 +0000

    Backout r239186 and provide correct fix.

--- sys/contrib/pf/net/pf.c
+++ sys/contrib/pf/net/pf.c
@@ -1074,7 +1074,10 @@ pf_state_key_clone(struct pf_state_key *orig)
 	if (sk == NULL)
 		return (NULL);
 
-	bcopy(orig, sk, sizeof(struct pf_state_key));
+	bcopy(orig, sk, sizeof(struct pf_state_key_cmp));
+
+	TAILQ_INIT(&sk->states[PF_SK_WIRE]);
+	TAILQ_INIT(&sk->states[PF_SK_STACK]);
 
 	return (sk);
 }
