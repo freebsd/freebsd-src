e9c8267d420eb26d0487290346ec156aef7632f5
jhb <jhb@FreeBSD.org>
2012-08-29 19:12:33 +0000

    MFC 238000,239584: Honor db_pager_quit in 'show malloc', 'show uma', and 'show witness'.

--- sys/kern/subr_witness.c
+++ sys/kern/subr_witness.c
@@ -943,6 +943,8 @@ witness_ddb_display_descendants(int(*prnt)(const char *fmt, ...),
 	indent++;
 	WITNESS_INDEX_ASSERT(w->w_index);
 	for (i = 1; i <= w_max_used_index; i++) {
+		if (db_pager_quit)
+			return;
 		if (w_rmatrix[w->w_index][i] & WITNESS_PARENT)
 			witness_ddb_display_descendants(prnt, &w_data[i],
 			    indent);
@@ -961,6 +963,8 @@ witness_ddb_display_list(int(*prnt)(const char *fmt, ...),
 
 		/* This lock has no anscestors - display its descendants. */
 		witness_ddb_display_descendants(prnt, w, 0);
+		if (db_pager_quit)
+			return;
 	}
 }
 	
@@ -982,12 +986,16 @@ witness_ddb_display(int(*prnt)(const char *fmt, ...))
 	 */
 	prnt("Sleep locks:\n");
 	witness_ddb_display_list(prnt, &w_sleep);
+	if (db_pager_quit)
+		return;
 	
 	/*
 	 * Now do spin locks which have been acquired at least once.
 	 */
 	prnt("\nSpin locks:\n");
 	witness_ddb_display_list(prnt, &w_spin);
+	if (db_pager_quit)
+		return;
 	
 	/*
 	 * Finally, any locks which have not been acquired yet.
@@ -998,6 +1006,8 @@ witness_ddb_display(int(*prnt)(const char *fmt, ...))
 			continue;
 		prnt("%s (type: %s, depth: %d)\n", w->w_name,
 		    w->w_class->lc_name, w->w_ddb_level);
+		if (db_pager_quit)
+			return;
 	}
 }
 #endif /* DDB */
@@ -2393,6 +2403,8 @@ DB_SHOW_ALL_COMMAND(locks, db_witness_list_all)
 			db_printf("Process %d (%s) thread %p (%d)\n", p->p_pid,
 			    p->p_comm, td, td->td_tid);
 			witness_ddb_list(td);
+			if (db_pager_quit)
+				return;
 		}
 	}
 }
