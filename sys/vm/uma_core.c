d8e74cf744e81e5a315118a3a732c46a88550384
John Baldwin <jhb@FreeBSD.org>
2012-08-29 19:12:33 +0000

    MFC 238000,239584: Honor db_pager_quit in 'show malloc', 'show uma', and 'show witness'.

--- sys/vm/uma_core.c
+++ sys/vm/uma_core.c
@@ -3345,6 +3345,8 @@ DB_SHOW_COMMAND(uma, db_show_uma)
 			    (uintmax_t)kz->uk_size,
 			    (intmax_t)(allocs - frees), cachefree,
 			    (uintmax_t)allocs, sleeps);
+			if (db_pager_quit)
+				return;
 		}
 	}
 }
