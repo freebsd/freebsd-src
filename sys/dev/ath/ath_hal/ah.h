dae1b5d00ab90e8437185f690c6cc053d18452a5
Adrian Chadd <adrian@FreeBSD.org>
2012-08-30 06:55:47 +0000

    Migrate the AR9285 diversity configuration LNA configuration to use some HAL definitions rather than local definitions.
    The original source (ath9k) pulled this stuff from the QCA driver and
    removed the HAL_* prefix.  I'm just restoring the correct order of things.
    
    Obtained from:	Qualcomm Atheros

--- sys/dev/ath/ath_hal/ah.h
+++ sys/dev/ath/ath_hal/ah.h
@@ -855,6 +855,29 @@ typedef enum {
 	HAL_MFP_HW_CRYPTO	/* hardware decryption enabled. Merlin can do it. */
 } HAL_MFP_OPT_T;
 
+/* LNA config supported */
+typedef enum {
+	HAL_ANT_DIV_COMB_LNA1_MINUS_LNA2	= 0,
+	HAL_ANT_DIV_COMB_LNA2			= 1,
+	HAL_ANT_DIV_COMB_LNA1			= 2,
+	HAL_ANT_DIV_COMB_LNA1_PLUS_LNA2		= 3,
+} HAL_ANT_DIV_COMB_LNA_CONF;
+
+typedef struct {
+	u_int8_t	main_lna_conf;
+	u_int8_t	alt_lna_conf;
+	u_int8_t	fast_div_bias;
+	u_int8_t	main_gaintb;
+	u_int8_t	alt_gaintb;
+	u_int8_t	antdiv_configgroup;
+	int8_t		lna1_lna2_delta;
+} HAL_ANT_COMB_CONFIG;
+
+#define	DEFAULT_ANTDIV_CONFIG_GROUP	0x00
+#define	HAL_ANTDIV_CONFIG_GROUP_1	0x01
+#define	HAL_ANTDIV_CONFIG_GROUP_2	0x02
+#define	HAL_ANTDIV_CONFIG_GROUP_3	0x03
+
 /*
  * Flag for setting QUIET period
  */
