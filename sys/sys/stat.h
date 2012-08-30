f2d5fa68c5f299141e59e746e63d72b95ce7cd7b
Konstantin Belousov <kib@FreeBSD.org>
2012-08-30 10:23:27 +0000

    MFC r238703: Put struct ostat and struct nstat under #ifdef _KERNEL. The compatibility definitions are only needed for implementation of the syscalls, they cause namespace pollution and are not useful for applications.

--- sys/sys/stat.h
+++ sys/sys/stat.h
@@ -99,7 +99,7 @@ typedef	__uid_t		uid_t;
 #include <sys/time.h>
 #endif
 
-#if __BSD_VISIBLE
+#ifdef _KERNEL
 struct ostat {
 	__uint16_t st_dev;		/* inode's device */
 	ino_t	  st_ino;		/* inode's number */
@@ -117,7 +117,7 @@ struct ostat {
 	fflags_t  st_flags;		/* user defined flags for file */
 	__uint32_t st_gen;		/* file generation number */
 };
-#endif /* __BSD_VISIBLE */
+#endif
 
 struct stat {
 	__dev_t   st_dev;		/* inode's device */
@@ -149,7 +149,7 @@ struct stat {
 	unsigned int :(8 / 2) * (16 - (int)sizeof(struct timespec));
 };
 
-#if __BSD_VISIBLE
+#ifdef _KERNEL
 struct nstat {
 	__dev_t   st_dev;		/* inode's device */
 	ino_t	  st_ino;		/* inode's number */
