8f67c5bc870e1e201a34c10d1ff7d471aa711845
Ed Schouten <ed@FreeBSD.org>
2012-08-31 18:26:39 +0000

    Relicense ac(8).
    Though the license of the original ac(8) source code provides many
    liberties, we are already somewhat violating it. The license requires us
    to clearly comment any modifications to the source code, as the original
    authors of course do not want to get bug reports for modified versions
    of ac(8). This is something we have not done consistently.
    
    The need for such a license clause has become less over time. It is very
    unlikely that end users will contact the original authors, as the
    copyright is from 1994. I contacted both the copyright holders. They
    responded in a timely fashion and were more than willing to relicense it
    to a 2-clause BSD license. To address any concerns about bug reports
    going to the original authors instead of me, add my own name and email
    address to the copyright statements as well.
    
    MFC after:		1 month
    Special thanks to:	Christopher Demetriou and Simon Gerraty

--- usr.sbin/ac/ac.c
+++ usr.sbin/ac/ac.c
@@ -1,16 +1,29 @@
-/*
- *      Copyright (c) 1994 Christopher G. Demetriou.
- *      @(#)Copyright (c) 1994, Simon J. Gerraty.
+/*-
+ * Copyright (c) 1994 Christopher G. Demetriou
+ * Copyright (c) 1994 Simon J. Gerraty
+ * Copyright (c) 2012 Ed Schouten <ed@FreeBSD.org>
+ * All rights reserved.
  *
- *      This is free software.  It comes with NO WARRANTY.
- *      Permission to use, modify and distribute this source code
- *      is granted subject to the following conditions.
- *      1/ that the above copyright notice and this notice
- *      are preserved in all copies and that due credit be given
- *      to the author.
- *      2/ that any changes to this code are clearly commented
- *      as such so that the author does not get blamed for bugs
- *      other than his own.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 #include <sys/cdefs.h>
