57f09aa635306e6a641c9990469ee4a86b708855
alc <alc@FreeBSD.org>
2012-09-01 03:46:28 +0000

    Introduce a new software PTE flag that indicates whether the mapping is for a managed page.
    Tested by:	jchandra

--- sys/mips/include/pmap.h
+++ sys/mips/include/pmap.h
@@ -171,7 +171,6 @@ void pmap_bootstrap(void);
 void *pmap_mapdev(vm_paddr_t, vm_size_t);
 void pmap_unmapdev(vm_offset_t, vm_size_t);
 vm_offset_t pmap_steal_memory(vm_size_t size);
-int page_is_managed(vm_paddr_t pa);
 void pmap_kenter(vm_offset_t va, vm_paddr_t pa);
 void pmap_kenter_attr(vm_offset_t va, vm_paddr_t pa, int attr);
 void pmap_kremove(vm_offset_t va);
