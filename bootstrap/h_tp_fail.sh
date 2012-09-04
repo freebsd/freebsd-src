45147a9eddaeb6ce9ca99161e2cd60a912c72e86
marcel <marcel@FreeBSD.org>
2012-09-04 23:07:32 +0000

    Import ATF version 0,16
    Discussed with: gibbs, gnn, keramida, mdf, mlaier,
    Discussed with: Garrett Cooper

---
+++ bootstrap/h_tp_fail.sh
@@ -0,0 +1,47 @@
+#
+# Automated Testing Framework (atf)
+#
+# Copyright (c) 2009 The NetBSD Foundation, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND
+# CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+atf_test_case @TC_NAME@
+@TC_NAME@_head()
+{
+    atf_set "descr" "An empty test case that always fails"
+}
+@TC_NAME@_body()
+{
+    echo "ignore-me"
+    echo "ignore-me" 1>&2
+    atf_fail "No reason"
+}
+
+atf_init_test_cases()
+{
+    atf_add_test_case @TC_NAME@
+}
+
+# vim: syntax=sh:expandtab:shiftwidth=4:softtabstop=4
