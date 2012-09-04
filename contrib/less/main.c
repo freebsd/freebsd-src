45be5ce5fb50d9f4f3d8b7e0aa7e88fe6ce6451e
delphij <delphij@FreeBSD.org>
2012-09-04 23:24:00 +0000

    MFC: less v451.

--- contrib/less/main.c
+++ contrib/less/main.c
@@ -1,12 +1,11 @@
 /* $FreeBSD$ */
 /*
- * Copyright (C) 1984-2011  Mark Nudelman
+ * Copyright (C) 1984-2012  Mark Nudelman
  *
  * You may distribute under the terms of either the GNU General Public
  * License or the Less License, as specified in the README file.
  *
- * For more information about less, or for information on how to 
- * contact the author, see the README file.
+ * For more information, see the README file.
  */
 
 
@@ -220,6 +219,7 @@ main(argc, argv)
 		argv++;
 		(void) get_ifile(filename, ifile);
 		ifile = prev_ifile(NULL_IFILE);
+		free(filename);
 #endif
 	}
 	/*
