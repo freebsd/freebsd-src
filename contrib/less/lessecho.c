1ba9638658455588b8f0d17c7e9d82ec9d44dddd
Xin LI <delphij@FreeBSD.org>
2012-09-05 06:15:15 +0000

    MFC: less v451.

--- contrib/less/lessecho.c
+++ contrib/less/lessecho.c
@@ -1,11 +1,10 @@
 /*
- * Copyright (C) 1984-2011  Mark Nudelman
+ * Copyright (C) 1984-2012  Mark Nudelman
  *
  * You may distribute under the terms of either the GNU General Public
  * License or the Less License, as specified in the README file.
  *
- * For more information about less, or for information on how to 
- * contact the author, see the README file.
+ * For more information, see the README file.
  */
 
 
@@ -28,7 +27,7 @@
 
 #include "less.h"
 
-static char *version = "$Revision: 1.14 $";
+static char *version = "$Revision: 1.15 $";
 
 static int quote_all = 0;
 static char openquote = '"';
