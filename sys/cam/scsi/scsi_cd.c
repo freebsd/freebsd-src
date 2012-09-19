df9839afd0d777a28aa6e0dbb1c0e190c9522e61
tijl <tijl@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-09-19 18:42:31 +0000

    Fix a panic when trying to play invalid audio tracks.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@240701 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/cam/scsi/scsi_cd.c
+++ sys/cam/scsi/scsi_cd.c
@@ -2080,6 +2080,7 @@ cdioctl(struct disk *dp, u_long cmd, void *addr, int flag, struct thread *td)
 			 	 || (st > (softc->toc.header.ending_track -
 				     softc->toc.header.starting_track))) {
 					error = EINVAL;
+					cam_periph_unlock(periph);
 					break;
 				}
 				sentry = &softc->toc.entries[st].addr;
