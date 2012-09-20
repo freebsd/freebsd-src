90628516533ba3a215431824df6d379724744711
glebius <glebius@FreeBSD.org>
2012-09-20 06:52:05 +0000

    Utilize Jenkins hash with random seed for source nodes storage.

--- sys/netpfil/pf/pf.c
+++ sys/netpfil/pf/pf.c
@@ -387,6 +387,27 @@ pf_hashkey(struct pf_state_key *sk)
 	return (h & V_pf_hashmask);
 }
 
+static __inline uint32_t
+pf_hashsrc(struct pf_addr *addr, sa_family_t af)
+{
+	uint32_t h;
+
+	switch (af) {
+	case AF_INET:
+		h = jenkins_hash32((uint32_t *)&addr->v4,
+		    sizeof(addr->v4)/sizeof(uint32_t), V_pf_hashseed);
+		break;
+	case AF_INET6:
+		h = jenkins_hash32((uint32_t *)&addr->v6,
+		    sizeof(addr->v6)/sizeof(uint32_t), V_pf_hashseed);
+		break;
+	default:
+		panic("%s: unknown address family %u", __func__, af);
+	}
+
+	return (h & V_pf_srchashmask);
+}
+
 #ifdef INET6
 void
 pf_addrcpy(struct pf_addr *dst, struct pf_addr *src, sa_family_t af)
