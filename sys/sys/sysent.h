474dc78a765376e017666faaf9943b42e85357fd
Konstantin Belousov <kib@FreeBSD.org>
2012-09-22 12:27:50 +0000

    MFC r237431: Enchance the shared page chunk allocator.

--- sys/sys/sysent.h
+++ sys/sys/sysent.h
@@ -256,8 +256,13 @@ int	lkmressys(struct thread *, struct nosys_args *);
 int	syscall_thread_enter(struct thread *td, struct sysent *se);
 void	syscall_thread_exit(struct thread *td, struct sysent *se);
 
-int shared_page_fill(int size, int align, const char *data);
+struct sf_buf;
+int shared_page_alloc(int size, int align);
+int shared_page_fill(int size, int align, const void *data);
+void shared_page_write(int base, int size, const void *data);
 void exec_sysvec_init(void *param);
+struct sf_buf *shared_page_write_start(int base);
+void shared_page_write_end(struct sf_buf *sf);
 
 #define INIT_SYSENTVEC(name, sv)					\
     SYSINIT(name, SI_SUB_EXEC, SI_ORDER_ANY,				\
