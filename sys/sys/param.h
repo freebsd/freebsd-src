4d2f652446b359ce2e2d3a850e323b184b863850
pjd <pjd@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-09-22 17:49:25 +0000

    Add rounddown2() macro similar to the roundup2() macro.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@240834 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -275,6 +275,7 @@
 #endif
 #define	nitems(x)	(sizeof((x)) / sizeof((x)[0]))
 #define	rounddown(x, y)	(((x)/(y))*(y))
+#define	rounddown2(x, y) ((x)&(~((y)-1)))          /* if y is power of two */
 #define	roundup(x, y)	((((x)+((y)-1))/(y))*(y))  /* to any y */
 #define	roundup2(x, y)	(((x)+((y)-1))&(~((y)-1))) /* if y is powers of two */
 #define powerof2(x)	((((x)-1)&(x))==0)
