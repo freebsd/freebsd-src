57bd5e6ff57305e3f8404247b4e0449a1aabfe6f
brooks <brooks@FreeBSD.org>
2012-09-25 21:25:47 +0000

    Shim more function names for install's benefit.

--- usr.sbin/nmtree/rmd160.h
+++ usr.sbin/nmtree/rmd160.h
@@ -2,4 +2,8 @@
 
 #include <ripemd.h>
 
+#define RMD160_CTX	RIPEMD160_CTX
+#define	RMD160End	RIPEMD160_End
 #define	RMD160File	RIPEMD160_File
+#define	RMD160Init	RIPEMD160_Init
+#define	RMD160Update	RIPEMD160_Update
--- usr.sbin/nmtree/sha1.h
+++ usr.sbin/nmtree/sha1.h
@@ -1,4 +1,8 @@
 /*	$FreeBSD$	*/
 
 #include <sha.h>
+
+#define	SHA1End		SHA1_End
 #define	SHA1File	SHA1_File
+#define	SHA1Init	SHA1_Init
+#define	SHA1Update	SHA1_Update
