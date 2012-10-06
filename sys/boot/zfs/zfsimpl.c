aae0c9de033bc4c0ecd9ac84b59b86ce7c6ed888
Andriy Gapon <avg@FreeBSD.org>
2012-10-06 19:47:24 +0000

    zfs boot: export boot/primary pool and vdev guid all the way to kenv
    This is work in progress to for znextboot and it also provides
    some convenient infrastructure.
    
    MFC after:	20 days

--- sys/boot/zfs/zfsimpl.c
+++ sys/boot/zfs/zfsimpl.c
@@ -706,6 +706,34 @@ spa_find_by_name(const char *name)
 	return (0);
 }
 
+#ifdef BOOT2
+static spa_t *
+spa_get_primary(void)
+{
+
+	return (STAILQ_FIRST(&zfs_pools));
+}
+
+static vdev_t *
+spa_get_primary_vdev(const spa_t *spa)
+{
+	vdev_t *vdev;
+	vdev_t *kid;
+
+	if (spa == NULL)
+		spa = spa_get_primary();
+	if (spa == NULL)
+		return (NULL);
+	vdev = STAILQ_FIRST(&spa->spa_vdevs);
+	if (vdev == NULL)
+		return (NULL);
+	for (kid = STAILQ_FIRST(&vdev->v_children); kid != NULL;
+	     kid = STAILQ_FIRST(&vdev->v_children))
+		vdev = kid;
+	return (vdev);
+}
+#endif
+
 static spa_t *
 spa_create(uint64_t guid)
 {
