c569f543f0034fecccbbfe8b77033e7dda6a0279
melifaro <melifaro@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-10-10 19:06:11 +0000

    Do not check if found IPv4 rte is dynamic if net.inet.icmp.drop_redirect is enabled. This eliminates one mtx_lock() per each routing lookup thus improving performance in several cases (routing to directly connected interface or routing to default gateway).
    Icmp redirects should not be used to provide routing direction nowadays, even
    for end hosts. Routers should not use them too (and this is explicitly restricted
    in IPv6, see RFC 4861, clause 8.2).
    
    Current commit changes rnh_machaddr function to 'stock' rn_match (and back) for every
    AF_INET routing table in given VNET instance on drop_redirect sysctl change.
    
    This change is part of bigger patch eliminating rte locking.
    
    Sponsored by:	Yandex LLC
    MFC after:	2 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@241406 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/in_var.h
+++ sys/netinet/in_var.h
@@ -424,6 +424,7 @@ inm_acquire_locked(struct in_multi *inm)
 struct	rtentry;
 struct	route;
 struct	ip_moptions;
+struct radix_node_head;
 
 int	imo_multi_filter(const struct ip_moptions *, const struct ifnet *,
 	    const struct sockaddr *, const struct sockaddr *);
@@ -464,6 +465,7 @@ void	 in_rtredirect(struct sockaddr *, struct sockaddr *,
 	    struct sockaddr *, int, struct sockaddr *, u_int);
 int	 in_rtrequest(int, struct sockaddr *,
 	    struct sockaddr *, struct sockaddr *, int, struct rtentry **, u_int);
+void	in_setmatchfunc(struct radix_node_head *, int);
 
 #if 0
 int	 in_rt_getifa(struct rt_addrinfo *, u_int fibnum);
