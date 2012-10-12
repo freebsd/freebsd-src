1a95a637c187ea749129b8355eb0a064eeb314b4
mav <mav@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-10-12 17:18:24 +0000

    Protect xpt_getattr() calls with the SIM lock and assert that.
    Submitted by:	ken@ (earlier version)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@241485 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/cam/scsi/scsi_da.c
+++ sys/cam/scsi/scsi_da.c
@@ -1211,17 +1211,17 @@ dadump(void *arg, void *virtual, vm_offset_t physical, off_t offset, size_t leng
 static int
 dagetattr(struct bio *bp)
 {
-	int ret = -1;
+	int ret;
 	struct cam_periph *periph;
 
-	if (bp->bio_disk == NULL || bp->bio_disk->d_drv1 == NULL)
-		return ENXIO;
 	periph = (struct cam_periph *)bp->bio_disk->d_drv1;
-	if (periph->path == NULL)
-		return ENXIO;
+	if (periph == NULL)
+		return (ENXIO);
 
+	cam_periph_lock(periph);
 	ret = xpt_getattr(bp->bio_data, bp->bio_length, bp->bio_attribute,
 	    periph->path);
+	cam_periph_unlock(periph);
 	if (ret == 0)
 		bp->bio_completed = bp->bio_length;
 	return ret;
