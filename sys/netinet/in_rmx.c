3bff27cd67d8bef2b2737cef089dd0862456e7fa
Alexander V. Chernikov <melifaro@FreeBSD.org>
2012-10-13 09:31:01 +0000

    Cleanup documentation: cloning route support has been removed in r186119.
    MFC after:	2 weeks

--- sys/netinet/in_rmx.c
+++ sys/netinet/in_rmx.c
@@ -27,19 +27,6 @@
  * SUCH DAMAGE.
  */
 
-/*
- * This code does two things necessary for the enhanced TCP metrics to
- * function in a useful manner:
- *  1) It marks all non-host routes as `cloning', thus ensuring that
- *     every actual reference to such a route actually gets turned
- *     into a reference to a host route to the specific destination
- *     requested.
- *  2) When such routes lose all their references, it arranges for them
- *     to be deleted in some random collection of circumstances, so that
- *     a large quantity of stale routing data is not kept in kernel memory
- *     indefinitely.  See in_rtqtimo() below for the exact mechanism.
- */
-
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
