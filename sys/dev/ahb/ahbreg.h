9ffe91c4a4d92a29e7f417b0cf3c9fb449aa5e0f
jhb <jhb@FreeBSD.org>
2012-10-15 16:05:02 +0000

    Add locking to the ahb(4) driver and mark it MPSAFE. - Use device_printf() and device_get_unit() instead of storing the unit   number in the softc. - Remove use of explicit bus space handles and tags. - Compare pointers against NULL. - Let new-bus allocate a softc rather than doing it by hand.
    Tested by:	no one

--- sys/dev/ahb/ahbreg.h
+++ sys/dev/ahb/ahbreg.h
@@ -255,12 +255,13 @@ struct ecb {
 	ecb_state		 state;
 	union ccb		*ccb;
 	bus_dmamap_t		 dmamap;
+	struct callout		 timer;
 };
 
 struct ahb_softc {
 	device_t		 dev;
-	bus_space_tag_t		 tag;
-	bus_space_handle_t	 bsh;
+	struct	resource	*res;
+	struct	mtx		 lock;
 	struct	cam_sim		*sim;
 	struct	cam_path	*path;
 	SLIST_HEAD(,ecb)	 free_ecbs;
@@ -274,7 +275,6 @@ struct ahb_softc {
 	struct	ecb		*immed_ecb;
 	struct	ha_inquiry_data	*ha_inq_data;
 	u_int32_t		 ha_inq_physbase;
-	u_long			 unit;
 	u_int			 init_level;
 	u_int			 scsi_id;
 	u_int			 num_ecbs;
