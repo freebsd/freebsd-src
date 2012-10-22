2ece2b4cebb0399da61b4d5bfd8c98417452b3d3
eadler <eadler@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-10-22 04:18:17 +0000

    Now that device disabling is generic, remove the non-standard implementation from mpt
    Don't MFC this to retain backwards compatibility.
    
    Reviewed by:	des, mjacob
    Approved by:	cperciva
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@241863 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/mpt/mpt_pci.c
+++ sys/dev/mpt/mpt_pci.c
@@ -268,11 +268,6 @@ mpt_set_options(struct mpt_softc *mpt)
 
 	tval = 0;
 	if (resource_int_value(device_get_name(mpt->dev),
-	    device_get_unit(mpt->dev), "disable", &tval) == 0 && tval != 0) {
-		mpt->disabled = 1;
-	}
-	tval = 0;
-	if (resource_int_value(device_get_name(mpt->dev),
 	    device_get_unit(mpt->dev), "debug", &tval) == 0 && tval != 0) {
 		mpt->verbose = tval;
 	}
