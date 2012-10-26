cdbf140d9201a4adc8cbbca3025d62118b5a7475
adrian <adrian@FreeBSD.org>
2012-10-26 19:46:55 +0000

    Oops, missed in my last commit.

--- sys/net80211/ieee80211_freebsd.c
+++ sys/net80211/ieee80211_freebsd.c
@@ -159,7 +159,9 @@ wlan_clone_destroy(struct ifnet *ifp)
 	ic->ic_vap_delete(vap);
 }
 
+#if __FreeBSD_version < 1000020
 IFC_SIMPLE_DECLARE(wlan, 0);
+#endif
 
 void
 ieee80211_vap_destroy(struct ieee80211vap *vap)
