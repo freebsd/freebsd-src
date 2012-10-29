609a74746a13bf5e3c81e96a225ae23786f7b45e
Alexander Motin <mav@FreeBSD.org>
2012-10-29 18:04:38 +0000

    Add basic BIO_DELETE support to GEOM RAID class for all RAID levels.
    If at least one subdisk in the volume supports it, BIO_DELETE requests
    will be propagated down.  Unfortunatelly, for RAID levels with redundancy
    unmapped blocks will be mapped back during first rebuild/resync process.
    
    Sponsored by:	iXsystems, Inc.
    MFC after:	1 month

--- sys/geom/raid/tr_raid1e.c
+++ sys/geom/raid/tr_raid1e.c
@@ -802,7 +802,8 @@ nextdisk:
 			}
 		}
 		remain -= length;
-		addr += length;
+		if (bp->bio_cmd != BIO_DELETE)
+			addr += length;
 		start = 0;
 	}
 	for (cbp = bioq_first(&queue); cbp != NULL;
@@ -858,10 +859,8 @@ g_raid_tr_iostart_raid1e(struct g_raid_tr_object *tr, struct bio *bp)
 		g_raid_tr_iostart_raid1e_read(tr, bp);
 		break;
 	case BIO_WRITE:
-		g_raid_tr_iostart_raid1e_write(tr, bp);
-		break;
 	case BIO_DELETE:
-		g_raid_iodone(bp, EIO);
+		g_raid_tr_iostart_raid1e_write(tr, bp);
 		break;
 	case BIO_FLUSH:
 		g_raid_tr_flush_common(tr, bp);
@@ -1119,7 +1118,7 @@ rebuild_round_done:
 		G_RAID_LOGREQ(2, bp, "REMAP done %d.", bp->bio_error);
 		g_raid_unlock_range(sd->sd_volume, virtual, bp->bio_length);
 	}
-	if (pbp->bio_cmd != BIO_READ) {
+	if (pbp->bio_cmd == BIO_WRITE) {
 		if (pbp->bio_inbed == 1 || pbp->bio_error != 0)
 			pbp->bio_error = bp->bio_error;
 		if (bp->bio_error != 0) {
