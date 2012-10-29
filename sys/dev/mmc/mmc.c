edaea153afc1ac7a1b0f029bd2879dd228b81459
gonzo <gonzo@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-10-29 17:23:45 +0000

    Add BCM2835 SDHCI driver and enable it in Raspberry Pi config
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@242321 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/mmc/mmc.c
+++ sys/dev/mmc/mmc.c
@@ -1731,3 +1731,4 @@ static devclass_t mmc_devclass;
 DRIVER_MODULE(mmc, ti_mmchs, mmc_driver, mmc_devclass, NULL, NULL);
 DRIVER_MODULE(mmc, at91_mci, mmc_driver, mmc_devclass, NULL, NULL);
 DRIVER_MODULE(mmc, sdhci_pci, mmc_driver, mmc_devclass, NULL, NULL);
+DRIVER_MODULE(mmc, sdhci_bcm, mmc_driver, mmc_devclass, NULL, NULL);
