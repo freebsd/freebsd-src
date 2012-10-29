dabdba130671b50d0b296dc9d7856b4c474a97a2
bapt <bapt@FreeBSD.org>
2012-10-29 18:06:09 +0000

    backout r242319, racy and not done in the right place
    Reported by:	Garrett Cooper  <yanegomi@gmail.com>

--- lib/libutil/pw_util.c
+++ lib/libutil/pw_util.c
@@ -96,7 +96,6 @@ pw_init(const char *dir, const char *master)
 #if 0
 	struct rlimit rlim;
 #endif
-	struct stat st;
 
 	if (dir == NULL) {
 		strcpy(passwd_dir, _PATH_ETC);
@@ -122,14 +121,6 @@ pw_init(const char *dir, const char *master)
 			return (-1);
 		}
 		strcpy(masterpasswd, master);
-	}
-
-	if (stat(masterpasswd, &st) == -1)
-		return (-1);
-
-	if (S_ISDIR(st.st_mode)) {
-		errno = EISDIR;
-		return (-1);
 	}
 
 	/*
