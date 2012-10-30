8b95820a21db4bfeba89d4aa9297106d47c08f53
jmallett <jmallett@FreeBSD.org>
2012-10-30 06:36:14 +0000

    Actually check board type rather than using a specialized octeon_is_simulation function.

--- sys/mips/cavium/octeon_ebt3000_cf.c
+++ sys/mips/cavium/octeon_ebt3000_cf.c
@@ -590,7 +590,7 @@ static void cf_swap_ascii (unsigned char str1[], char str2[])
 
 static int cf_probe (device_t dev)
 {
-    	if (octeon_is_simulation())
+    	if (cvmx_sysinfo_get()->board_type == CVMX_BOARD_TYPE_SIM)
 		return (ENXIO);
 
 	if (device_get_unit(dev) != 0) {
@@ -618,7 +618,7 @@ static void cf_identify (driver_t *drv, device_t parent)
 	cvmx_mio_boot_reg_cfgx_t cfg;
 	uint64_t phys_base;
 	
-    	if (octeon_is_simulation())
+    	if (cvmx_sysinfo_get()->board_type == CVMX_BOARD_TYPE_SIM)
 		return;
 
 	phys_base = cvmx_sysinfo_get()->compact_flash_common_base_addr;
@@ -694,7 +694,7 @@ static int cf_attach (device_t dev)
 	struct cf_priv *cf_priv;
 	int error;
 
-    	if (octeon_is_simulation())
+    	if (cvmx_sysinfo_get()->board_type == CVMX_BOARD_TYPE_SIM)
 		return (ENXIO);
 
 	cf_priv = device_get_softc(dev);
