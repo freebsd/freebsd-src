b3a0c2972955e0c643ec29ae3806fbb63f8ca555
delphij <delphij@FreeBSD.org>
2012-11-04 20:47:39 +0000

    Vendor import of less v453 (beta).

--- screen.c
+++ screen.c
@@ -801,7 +801,7 @@ scrsize()
 	else if ((n = ltgetnum("li")) > 0)
  		sc_height = n;
 #endif
-	else
+	if (sc_height <= 0)
 		sc_height = DEF_SC_HEIGHT;
 
 	if (sys_width > 0)
@@ -812,7 +812,7 @@ scrsize()
 	else if ((n = ltgetnum("co")) > 0)
  		sc_width = n;
 #endif
-	else
+	if (sc_width <= 0)
 		sc_width = DEF_SC_WIDTH;
 }
 
