9aa68a3ffae06d619ec56178d1397b59fd7751bf
Greg Lehey <grog@FreeBSD.org>
2012-11-08 00:24:26 +0000

    Add y flag and environment variable LS_SAMESORT to specify the same sorting order for time and name with the -t option.  IEEE Std 1003.2 (POSIX.2) mandates that the -t option sort in descending order, and that if two files have the same timestamp, they should be sorted in ascending order of their names.  The -r flag reverses both of these sort orders, so they're never the same.  This creates significant problems for sequentially named files stored on FAT file systems, where it can be impossible to list them in the order in which they were created.
    Add , (comma) option to print file sizes grouped and separated by
    thousands using the non-monetary separator returned by localeconv(3),
    typically a comma or period.
    
    MFC after:  14 days

--- bin/ls/ls.c
+++ bin/ls/ls.c
@@ -109,10 +109,11 @@ int termwidth = 80;		/* default terminal width */
        int f_humanval;		/* show human-readable file sizes */
        int f_inode;		/* print inode */
 static int f_kblocks;		/* print size in kilobytes */
+       int f_label;		/* show MAC label */
 static int f_listdir;		/* list actual directory, not contents */
 static int f_listdot;		/* list files beginning with . */
-static int f_noautodot;		/* do not automatically enable -A for root */
        int f_longform;		/* long listing format */
+static int f_noautodot;		/* do not automatically enable -A for root */
 static int f_nofollow;		/* don't follow symbolic link arguments */
        int f_nonprint;		/* show unprintables as ? */
 static int f_nosort;		/* don't sort output */
@@ -122,19 +123,21 @@ static int f_numericonly;	/* don't convert uid/gid to name */
        int f_octal_escape;	/* like f_octal but use C escapes if possible */
 static int f_recursive;		/* ls subdirectories also */
 static int f_reversesort;	/* reverse whatever sort is used */
-       int f_sectime;		/* print the real time for all files */
+       int f_samesort;		/* sort time and name in same direction */
+       int f_sectime;		/* print full time information */
 static int f_singlecol;		/* use single column output */
        int f_size;		/* list size in short listing */
+static int f_sizesort;
        int f_slash;		/* similar to f_type, but only for dirs */
        int f_sortacross;	/* sort across rows, not down columns */
        int f_statustime;	/* use time of last mode change */
 static int f_stream;		/* stream the output, separate with commas */
-static int f_timesort;		/* sort by time vice name */
+       int f_thousands;		/* show file sizes with thousands separators */
        char *f_timeformat;      /* user-specified time format */
-static int f_sizesort;
+static int f_timesort;		/* sort by time vice name */
        int f_type;		/* add type character for non-regular files */
 static int f_whiteout;		/* show whiteout entries */
-       int f_label;		/* show MAC label */
+
 #ifdef COLORLS
        int f_color;		/* add type in color for non-regular files */
 
@@ -180,8 +183,10 @@ main(int argc, char *argv[])
 	}
 
 	fts_options = FTS_PHYSICAL;
+	if (getenv("LS_SAMESORT"))
+		f_samesort = 1;
  	while ((ch = getopt(argc, argv,
-	    "1ABCD:FGHILPRSTUWZabcdfghiklmnopqrstuwx")) != -1) {
+	    "1ABCD:FGHILPRSTUWXZabcdfghiklmnopqrstuwxy,")) != -1) {
 		switch (ch) {
 		/*
 		 * The -1, -C, -x and -l options all override each other so
@@ -237,6 +242,9 @@ main(int argc, char *argv[])
 			f_sizesort = 0;
 			break;
                 /* Other flags.  Please keep alphabetic. */
+		case ',':
+			f_thousands = 1;
+			break;
 		case 'B':
 			f_nonprint = 0;
 			f_octal = 1;
@@ -337,6 +345,9 @@ main(int argc, char *argv[])
 			f_nonprint = 0;
 			f_octal = 0;
 			f_octal_escape = 0;
+			break;
+		case 'y':
+			f_samesort = 1;
 			break;
 		default:
 		case '?':
@@ -850,6 +861,8 @@ label_out:
 			d.s_size = sizelen;
 		d.s_user = maxuser;
 	}
+        if (f_thousands)                        /* make space for commas */
+		d.s_size += (d.s_size - 1) / 3;
 	printfcn(&d);
 	output = 1;
 
