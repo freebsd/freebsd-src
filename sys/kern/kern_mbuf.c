79f62ed69004b699d56e16b29fc2ac3a6d3a7d34
Alfred Perlstein <alfred@FreeBSD.org>
2012-11-10 02:08:40 +0000

    Allow maxusers to scale on machines with large address space.
    Some hooks are added to clamp down maxusers and nmbclusters for
    small address space systems.
    
    VM_MAX_AUTOTUNE_MAXUSERS - the max maxusers that will be autotuned based on
    physical memory.
    VM_MAX_AUTOTUNE_NMBCLUSTERS - max nmbclusters based on physical memory.
    
    These are set to the old values on i386 to preserve the clamping that was
    being done to all arches.
    
    Another macro VM_AUTOTUNE_NMBCLUSTERS is provided to allow an override
    for the calculation on a MD basis.  Currently no arch defines this.
    
    Reviewed by: peter
    MFC after: 2 weeks

--- sys/kern/kern_mbuf.c
+++ sys/kern/kern_mbuf.c
@@ -113,8 +113,17 @@ tunable_mbinit(void *dummy)
 
 	/* This has to be done before VM init. */
 	TUNABLE_INT_FETCH("kern.ipc.nmbclusters", &nmbclusters);
-	if (nmbclusters == 0)
+	if (nmbclusters == 0) {
+#ifdef VM_AUTOTUNE_NMBCLUSTERS
+		nmbclusters = VM_AUTOTUNE_NMBCLUSTERS;
+#else
 		nmbclusters = 1024 + maxusers * 64;
+#endif
+#ifdef VM_MAX_AUTOTUNE_NMBCLUSTERS
+		if (nmbclusters > VM_MAX_AUTOTUNE_NMBCLUSTERS)
+			nmbclusters = VM_MAX_AUTOTUNE_NMBCLUSTERS;
+#endif
+	}
 
 	TUNABLE_INT_FETCH("kern.ipc.nmbjumbop", &nmbjumbop);
 	if (nmbjumbop == 0)
