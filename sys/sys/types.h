6efe04e4bb01c0da1355b9920a120f4ccc62a96d
kib <kib@FreeBSD.org>
2012-11-16 06:29:52 +0000

    Move the definition of the idtype_t from sys/types.h to sys/wait.h. Fix the bug, use #if __BSD_VISIBLE instead of #if defined(__BSD_VISIBLE), since __BSD_VISIBLE is always defined. Reformat the comments from the Solaris style to KNF.
    Reported and reviewed by:	bde
    MFC after:	28 days

--- sys/sys/types.h
+++ sys/sys/types.h
@@ -141,46 +141,6 @@ typedef	__id_t		id_t;		/* can hold a uid_t or pid_t */
 #define	_ID_T_DECLARED
 #endif
 
-#ifndef _IDTYPE_T_DECLARED
-
-typedef enum
-#if defined(__BSD_VISIBLE)
-	idtype		/* pollutes XPG4.2 namespace */
-#endif
-		{
-	/*
-	 * These names were mostly lifted from Solaris source code and
-	 * still use Solaris style naming to avoid breaking any
-	 * OpenSolaris code which has been ported to FreeBSD.  There
-	 * is no clear FreeBSD counterpart for all of the names, but
-	 * some have a clear correspondence to FreeBSD entities.
-	 */
-	P_PID,		/* A process identifier.		*/
-	P_PPID,		/* A parent process identifier.		*/
-	P_PGID,		/* A process group identifier.		*/
-	P_SID,		/* A session identifier.		*/
-	P_CID,		/* A scheduling class identifier.	*/
-	P_UID,		/* A user identifier.			*/
-	P_GID,		/* A group identifier.			*/
-	P_ALL,		/* All processes.			*/
-	P_LWPID,	/* An LWP identifier.			*/
-	P_TASKID,	/* A task identifier.			*/
-	P_PROJID,	/* A project identifier.		*/
-	P_POOLID,	/* A pool identifier.			*/
-	P_JAILID,	/* A zone identifier.			*/
-	P_CTID,		/* A (process) contract identifier.	*/
-	P_CPUID,	/* CPU identifier.			*/
-	P_PSETID	/* Processor set identifier		*/
-} idtype_t;		/* The type of id_t we are using.	*/
-
-#if defined(__BSD_VISIBLE)
-#define	P_ZONEID	P_JAILID
-#endif
-
-#define	_IDTYPE_T_DECLARED
-#endif
-
-
 #ifndef _INO_T_DECLARED
 typedef	__ino_t		ino_t;		/* inode number */
 #define	_INO_T_DECLARED
