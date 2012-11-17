2afd280161e26070121aa33652eeac3a66697688
Adrian Chadd <adrian@FreeBSD.org>
2012-11-17 02:37:25 +0000

    Implement AR5210 descriptor decoding.

--- tools/tools/ath/athalq/main.c
+++ tools/tools/ath/athalq/main.c
@@ -33,6 +33,7 @@ __FBSDID("$FreeBSD$");
 #if 0
 #include "ar9300_ds.h"
 #endif
+#include "ar5210_ds.h"
 #include "ar5211_ds.h"
 #include "ar5212_ds.h"
 #include "ar5416_ds.h"
@@ -112,7 +113,9 @@ main(int argc, const char *argv[])
 				ath_alq_print_hdr(&hdr);
 				break;
 			default:
-				if (be32toh(hdr.sc_hal_magic) == AR5211_MAGIC)
+				if (be32toh(hdr.sc_hal_magic) == AR5210_MAGIC)
+					ar5210_alq_payload(a);
+				else if (be32toh(hdr.sc_hal_magic) == AR5211_MAGIC)
 					ar5211_alq_payload(a);
 				else if (be32toh(hdr.sc_hal_magic) == AR5212_MAGIC)
 					ar5212_alq_payload(a);
