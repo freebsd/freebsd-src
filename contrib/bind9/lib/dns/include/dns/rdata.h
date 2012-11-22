0bc54cefa036bdc3edf559305c53a568f6811fa9
Simon L. B. Nielsen <simon@FreeBSD.org>
2012-11-22 23:15:38 +0000

    Fix multiple Denial of Service vulnerabilities with named(8).
    Fix insufficient message length validation for EAP-TLS messages.
    
    Fix Linux compatibility layer input validation error.
    
    This commit includes stable/7 which was missed in r243417.
    
    Security:	FreeBSD-SA-12:06.bind
    Security:	FreeBSD-SA-12:07.hostapd
    Security:	FreeBSD-SA-12:08.linux
    Security:	CVE-2012-4244, CVE-2012-5166, CVE-2012-4445, CVE-2012-4576
    Approved by:	re
    Approved by:	security-officer

--- contrib/bind9/lib/dns/include/dns/rdata.h
+++ contrib/bind9/lib/dns/include/dns/rdata.h
@@ -127,6 +127,17 @@ struct dns_rdata {
 #define DNS_RDATA_UPDATE	0x0001		/*%< update pseudo record */
 
 /*
+ * The maximum length of a RDATA that can be sent on the wire.
+ * Max packet size (65535) less header (12), less name (1), type (2),
+ * class (2), ttl(4), length (2).
+ *
+ * None of the defined types that support name compression can exceed
+ * this and all new types are to be sent uncompressed.
+ */
+
+#define DNS_RDATA_MAXLENGTH	65512U
+
+/*
  * Flags affecting rdata formatting style.  Flags 0xFFFF0000
  * are used by masterfile-level formatting and defined elsewhere.
  * See additional comments at dns_rdata_tofmttext().
