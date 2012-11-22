3362e2e8aac2210ddc407c81fc4857f377543e81
ed <ed@FreeBSD.org>
2012-11-22 15:19:53 +0000

    MFC r229848:
      Add aligned_alloc(3).
    
      The C11 folks reinvented the wheel by introducing an aligned version of
      malloc(3) called aligned_alloc(3), instead of posix_memalign(3). Instead
      of returning the allocation by reference, it returns the address, just
      like malloc(3).
    
    I'm MFCing this now, as it seems aligned_alloc(3) is needed to make the
    new version of libc++ work, which was merged back to FreeBSD 9 in r243376.
    
    Requested by:	dim

--- include/stdlib.h
+++ include/stdlib.h
@@ -155,6 +155,7 @@ _Noreturn void	 _Exit(int);
  * If we're in a mode greater than C99, expose C1x functions.
  */
 #if __ISO_C_VISIBLE >= 2011 || __cplusplus >= 201103L
+void *	aligned_alloc(size_t, size_t);
 _Noreturn void
 	quick_exit(int);
 int	at_quick_exit(void (*)(void));
