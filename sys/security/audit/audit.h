632d7191a2034f3e655f731e7828c7ddd32bc0e5
pjd <pjd@FreeBSD.org>
2012-11-30 23:18:49 +0000

    IFp4 @208451:
    Fix path handling for *at() syscalls.
    
    Before the change directory descriptor was totally ignored,
    so the relative path argument was appended to current working
    directory path and not to the path provided by descriptor, thus
    wrong paths were stored in audit logs.
    
    Now that we use directory descriptor in vfs_lookup, move
    AUDIT_ARG_UPATH1() and AUDIT_ARG_UPATH2() calls to the place where
    we hold file descriptors table lock, so we are sure paths will
    be resolved according to the same directory in audit record and
    in actual operation.
    
    Sponsored by:	FreeBSD Foundation (auditdistd)
    Reviewed by:	rwatson
    MFC after:	2 weeks

--- sys/security/audit/audit.h
+++ sys/security/audit/audit.h
@@ -99,8 +99,8 @@ void	 audit_arg_sockaddr(struct thread *td, struct sockaddr *sa);
 void	 audit_arg_auid(uid_t auid);
 void	 audit_arg_auditinfo(struct auditinfo *au_info);
 void	 audit_arg_auditinfo_addr(struct auditinfo_addr *au_info);
-void	 audit_arg_upath1(struct thread *td, char *upath);
-void	 audit_arg_upath2(struct thread *td, char *upath);
+void	 audit_arg_upath1(struct thread *td, int dirfd, char *upath);
+void	 audit_arg_upath2(struct thread *td, int dirfd, char *upath);
 void	 audit_arg_vnode1(struct vnode *vp);
 void	 audit_arg_vnode2(struct vnode *vp);
 void	 audit_arg_text(char *text);
@@ -276,14 +276,14 @@ void	 audit_thread_free(struct thread *td);
 		audit_arg_uid((uid));					\
 } while (0)
 
-#define	AUDIT_ARG_UPATH1(td, upath) do {				\
+#define	AUDIT_ARG_UPATH1(td, dirfd, upath) do {				\
 	if (AUDITING_TD(curthread))					\
-		audit_arg_upath1((td), (upath));			\
+		audit_arg_upath1((td), (dirfd), (upath));		\
 } while (0)
 
-#define	AUDIT_ARG_UPATH2(td, upath) do {				\
+#define	AUDIT_ARG_UPATH2(td, dirfd, upath) do {				\
 	if (AUDITING_TD(curthread))					\
-		audit_arg_upath2((td), (upath));			\
+		audit_arg_upath2((td), (dirfd), (upath));		\
 } while (0)
 
 #define	AUDIT_ARG_VALUE(value) do {					\
@@ -356,8 +356,8 @@ void	 audit_thread_free(struct thread *td);
 #define	AUDIT_ARG_SUID(suid)
 #define	AUDIT_ARG_TEXT(text)
 #define	AUDIT_ARG_UID(uid)
-#define	AUDIT_ARG_UPATH1(td, upath)
-#define	AUDIT_ARG_UPATH2(td, upath)
+#define	AUDIT_ARG_UPATH1(td, dirfd, upath)
+#define	AUDIT_ARG_UPATH2(td, dirfd, upath)
 #define	AUDIT_ARG_VALUE(value)
 #define	AUDIT_ARG_VNODE1(vp)
 #define	AUDIT_ARG_VNODE2(vp)
