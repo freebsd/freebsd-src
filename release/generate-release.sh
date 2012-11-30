1d1620e2c6b9da61ea6191b2cb73d28d224ab939
Hiroki Sato <hrs@FreeBSD.org>
2012-11-30 16:15:35 +0000

    - Bump versions and revert XML migration of the release documents in   releng/9.1 branch.  The doc tree release/9.1.0 for this release still uses   SGML toolchain[1].
    - Add SVNROOT{BASE,SRC,DOC,PORTS} for subversion repository URLs and
      BRANCH{SRC,DOC,PORTS} for the branches to generate-release.sh, and
      remove -p, -r, -d options.  The revision to be built should be specified
      in the URL.
    
    - Add {WORLD,KERNEL}_FLAGS to generate-release.sh.  These were supported in
      the old release build framework.
    
    - Disable to use binary package for docproj port during a release build.
      This package should be built successfully.
    
    Pointy hat to:	hrs [1]
    Approved by:	re (implicitly)

--- release/generate-release.sh
+++ release/generate-release.sh
@@ -3,13 +3,18 @@
 # generate-release.sh: check out source trees, and build release components with
 #  totally clean, fresh trees.
 #
-#  Usage: generate-release.sh [-r revision] [-d docrevision] \
-#	[-p portsrevision] svn-branch scratch-dir
+#  Usage: generate-release.sh svn-branch[@revision] scratch-dir
 #
-# Environment variables:
-#  SVNROOT:    SVN URL to FreeBSD source repository (by default, 
-#   svn://svn.freebsd.org/base)
-#  MAKE_FLAGS: optional flags to pass to make (e.g. -j)
+# Environment variables (default):
+#  SVNROOTBASE:	SVN base URL to FreeBSD repository (svn://svn.freebsd.org)
+#  SVNROOTSRC:	URL to FreeBSD src tree (${SVNROOTBASE}/base)
+#  SVNROOTDOC:	URL to FreeBSD doc tree (${SVNROOTBASE}/doc)
+#  SVNROOTPORTS:URL to FreeBSD ports tree (${SVNROOTBASE}/ports)
+#  BRANCHSRC:	branch name of src (svn-branch[@revision])
+#  BRANCHDOC:	branch name of doc (release/9.1.0)
+#  BRANCHPORTS:	branch name of ports (tags/RELEASE_9_1_0)
+#  WORLD_FLAGS: optional flags to pass to buildworld (e.g. -j)
+#  KERNEL_FLAGS: optional flags to pass to buildkernel (e.g. -j)
 # 
 #  Note: Since this requires a chroot, release cross-builds will not work!
 #
@@ -18,43 +23,33 @@
 
 usage()
 {
-	echo "Usage: $0 [-r revision] [-d docrevision] [-p portsrevision] svn-branch scratch-dir"
+	echo "Usage: $0 svn-branch[@revision] scratch-dir" 2>&1
 	exit 1
 }
-
-REVISION=
-DOCREVISION=
-PORTSREVISION=
-while getopts d:r:p: opt; do
-	case $opt in
-	d)
-		DOCREVISION="-r $OPTARG"
-		;;
-	r)
-		REVISION="-r $OPTARG"
-		;;
-	p)
-		PORTSREVISION="-r $OPTARG"
-		;;
-	\?)
-		usage
-		;;
-	esac
-done
-shift $(($OPTIND - 1))
 
 if [ $# -lt 2 ]; then
 	usage
 fi
 
+: ${SVNROOTBASE:=svn://svn.freebsd.org}
+: ${SVNROOTSRC:=${SVNROOTBASE}/base}
+: ${SVNROOTDOC:=${SVNROOTBASE}/doc}
+: ${SVNROOTPORTS:=${SVNROOTBASE}/ports}
+: ${SVNROOT:=${SVNROOTSRC}} # for backward compatibility
+BRANCHSRC=$1
+: ${BRANCHDOC:=release/9.1.0}
+: ${BRANCHPORTS:=branches/RELENG_9_1_0}
+: ${WORLD_FLAGS:=${MAKE_FLAGS}}
+: ${KERNEL_FLAGS:=${MAKE_FLAGS}}
+
 set -e # Everything must succeed
 
-svn co ${SVNROOT:-svn://svn.freebsd.org/base}/$1 $2/usr/src
-svn co ${SVNROOT:-svn://svn.freebsd.org/doc}/head $2/usr/doc $DOCREVISION
-svn co ${SVNROOT:-svn://svn.freebsd.org/ports}/head $2/usr/ports $PORTSREVISION
+svn co ${SVNROOT}/${BRANCHSRC} $2/usr/src
+svn co ${SVNROOTDOC}/${BRANCHDOC} $2/usr/doc
+svn co ${SVNROOTPORTS}/${BRANCHPORTS} $2/usr/ports
 
 cd $2/usr/src
-make $MAKE_FLAGS buildworld
+make $WORLD_FLAGS buildworld
 make installworld distribution DESTDIR=$2
 mount -t devfs devfs $2/dev
 trap "umount $2/dev" EXIT # Clean up devfs mount on exit
@@ -63,10 +58,11 @@ if [ -d $2/usr/doc ]; then
 	cp /etc/resolv.conf $2/etc/resolv.conf
 
 	# Install docproj to build release documentation
-	chroot $2 /bin/sh -c '(export ASSUME_ALWAYS_YES=1 && /usr/sbin/pkg install -y docproj) || (cd /usr/ports/textproc/docproj && make install clean BATCH=yes WITHOUT_X11=yes JADETEX=no WITHOUT_PYTHON=yes)'
+	chroot $2 make -C /usr/ports/textproc/docproj install \
+		BATCH=yes WITHOUT_X11=yes JADETEX=no WITHOUT_PYTHON=yes
 fi
 
-chroot $2 make -C /usr/src $MAKE_FLAGS buildworld buildkernel
+chroot $2 make -C /usr/src $WORLD_FLAGS buildworld
+chroot $2 make -C /usr/src $KERNEL_FLAGS buildkernel
 chroot $2 make -C /usr/src/release release
 chroot $2 make -C /usr/src/release install DESTDIR=/R
-
