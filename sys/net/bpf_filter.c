freebsd/freebsd-src

commit 81b40acef9561e5505868d3a4e0a40da733bbf30

Clear the filter memory area before using it. Leaving it uninitialized may leak previous kernel stack contents through a malicioius BPF filter.
PR:		kern/158880
Submitted by:	Guy Harris
Obtained from:	OpenBSD
MFC after:	1 week


git-svn-id: svn+ssh://svn.freebsd.org/base/head@224044 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/net/bpf_filter.c
@@ -177,6 +177,8 @@ bpf_filter(const struct bpf_insn *pc, u_char *p, u_int wirelen, u_int buflen)
 	bpf_u_int32 k;
 	u_int32_t mem[BPF_MEMWORDS];
 
+	bzero(mem, sizeof(mem));
+
 	if (pc == NULL)
 		/*
 		 * No filter means accept all.

commit 776084ac581c49f4e2eb92577e60fb0db7803a1b

Add m_xbyte() and make m_xhalf(), m_xword(), and m_xbyte() global.  These functions are used by bpfjit.c later.  While I am here, make it compilable again in userland (broken in r224044).

sys/net/bpf_filter.c
@@ -66,6 +66,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/mbuf.h>
 #else
 #include <stdlib.h>
+#include <strings.h>
 #endif
 #include <net/bpf.h>
 #ifdef _KERNEL
@@ -82,11 +83,12 @@ __FBSDID("$FreeBSD$");
 	} \
 }
 
-static u_int16_t	m_xhalf(struct mbuf *m, bpf_u_int32 k, int *err);
-static u_int32_t	m_xword(struct mbuf *m, bpf_u_int32 k, int *err);
+uint32_t	m_xhalf(const struct mbuf *, uint32_t, int *);
+uint32_t	m_xword(const struct mbuf *, uint32_t, int *);
+uint32_t	m_xbyte(const struct mbuf *, uint32_t, int *);
 
-static u_int32_t
-m_xword(struct mbuf *m, bpf_u_int32 k, int *err)
+uint32_t
+m_xword(const struct mbuf *m, uint32_t k, int *err)
 {
 	size_t len;
 	u_char *cp, *np;
@@ -134,8 +136,8 @@ m_xword(struct mbuf *m, bpf_u_int32 k, int *err)
 	return (0);
 }
 
-static u_int16_t
-m_xhalf(struct mbuf *m, bpf_u_int32 k, int *err)
+uint32_t
+m_xhalf(const struct mbuf *m, uint32_t k, int *err)
 {
 	size_t len;
 	u_char *cp;
@@ -162,6 +164,15 @@ m_xhalf(struct mbuf *m, bpf_u_int32 k, int *err)
  bad:
 	*err = 1;
 	return (0);
+}
+
+uint32_t
+m_xbyte(const struct mbuf *m, uint32_t k, int *err)
+{
+
+	*err = 0;
+	MINDEX(m, k);
+	return (mtod(m, u_char *)[k]);
 }
 #endif
 