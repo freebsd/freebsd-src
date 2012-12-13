f2fcc434ee4ef829e819d5b1048158f764763794
Jim Harris <jimharris@FreeBSD.org>
2012-12-13 21:27:20 +0000

    Revert r243960 based on feedback regarding keeping x86 headers unified (mdf@, tijl@) and use of KASSERT/systm.h in bus.h (zeising@, bde@).
    Alternate implementation will be made in a separate commit.

--- sys/x86/include/bus.h
+++ sys/x86/include/bus.h
@@ -251,6 +251,10 @@ bus_space_read_4(bus_space_tag_t tag, bus_space_handle_t handle,
 	return (*(volatile u_int32_t *)(handle + offset));
 }
 
+#if 0	/* Cause a link error for bus_space_read_8 */
+#define	bus_space_read_8(t, h, o)	!!! bus_space_read_8 unimplemented !!!
+#endif
+
 /*
  * Read `count' 1, 2, 4, or 8 byte quantities from bus space
  * described by tag/handle/offset and copy into buffer provided.
@@ -332,6 +336,10 @@ bus_space_read_multi_4(bus_space_tag_t tag, bus_space_handle_t bsh,
 #endif
 	}
 }
+
+#if 0	/* Cause a link error for bus_space_read_multi_8 */
+#define	bus_space_read_multi_8	!!! bus_space_read_multi_8 unimplemented !!!
+#endif
 
 /*
  * Read `count' 1, 2, 4, or 8 byte quantities from bus space
@@ -450,6 +458,10 @@ bus_space_read_region_4(bus_space_tag_t tag, bus_space_handle_t bsh,
 	}
 }
 
+#if 0	/* Cause a link error for bus_space_read_region_8 */
+#define	bus_space_read_region_8	!!! bus_space_read_region_8 unimplemented !!!
+#endif
+
 /*
  * Write the 1, 2, 4, or 8 byte value `value' to bus space
  * described by tag/handle/offset.
@@ -499,6 +511,10 @@ bus_space_write_4(bus_space_tag_t tag, bus_space_handle_t bsh,
 	else
 		*(volatile u_int32_t *)(bsh + offset) = value;
 }
+
+#if 0	/* Cause a link error for bus_space_write_8 */
+#define	bus_space_write_8	!!! bus_space_write_8 not implemented !!!
+#endif
 
 /*
  * Write `count' 1, 2, 4, or 8 byte quantities from the buffer
@@ -584,6 +600,11 @@ bus_space_write_multi_4(bus_space_tag_t tag, bus_space_handle_t bsh,
 #endif
 	}
 }
+
+#if 0	/* Cause a link error for bus_space_write_multi_8 */
+#define	bus_space_write_multi_8(t, h, o, a, c)				\
+			!!! bus_space_write_multi_8 unimplemented !!!
+#endif
 
 /*
  * Write `count' 1, 2, 4, or 8 byte quantities from the buffer provided
@@ -702,6 +723,11 @@ bus_space_write_region_4(bus_space_tag_t tag, bus_space_handle_t bsh,
 	}
 }
 
+#if 0	/* Cause a link error for bus_space_write_region_8 */
+#define	bus_space_write_region_8					\
+			!!! bus_space_write_region_8 unimplemented !!!
+#endif
+
 /*
  * Write the 1, 2, 4, or 8 byte value `val' to bus space described
  * by tag/handle/offset `count' times.
@@ -762,6 +788,10 @@ bus_space_set_multi_4(bus_space_tag_t tag, bus_space_handle_t bsh,
 			*(volatile u_int32_t *)(addr) = value;
 }
 
+#if 0	/* Cause a link error for bus_space_set_multi_8 */
+#define	bus_space_set_multi_8 !!! bus_space_set_multi_8 unimplemented !!!
+#endif
+
 /*
  * Write `count' 1, 2, 4, or 8 byte value `val' to bus space described
  * by tag/handle starting at `offset'.
@@ -821,6 +851,10 @@ bus_space_set_region_4(bus_space_tag_t tag, bus_space_handle_t bsh,
 		for (; count != 0; count--, addr += 4)
 			*(volatile u_int32_t *)(addr) = value;
 }
+
+#if 0	/* Cause a link error for bus_space_set_region_8 */
+#define	bus_space_set_region_8	!!! bus_space_set_region_8 unimplemented !!!
+#endif
 
 /*
  * Copy `count' 1, 2, 4, or 8 byte values from bus space starting
@@ -949,6 +983,10 @@ bus_space_copy_region_4(bus_space_tag_t tag, bus_space_handle_t bsh1,
 		}
 	}
 }
+
+#if 0	/* Cause a link error for bus_space_copy_8 */
+#define	bus_space_copy_region_8	!!! bus_space_copy_region_8 unimplemented !!!
+#endif
 
 /*
  * Bus read/write barrier methods.
