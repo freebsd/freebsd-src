c0cb589bf0b1e83aa0da4499ada20be30b07377d
brooks <brooks@FreeBSD.org>
2012-12-20 17:53:45 +0000

    Declare strsuftoll() and strsuftollx().

---
+++ lib/libnetbsd/stdlib.h
@@ -0,0 +1,7 @@
+/* $FreeBSD$ */
+
+#include_next <stdlib.h>
+
+long long strsuftoll(const char *, const char *, long long, long long);
+long long strsuftollx(const char *, const char *,
+    long long, long long, char *, size_t);
