1faf4066d754bea6ae6cbb42a966d4feb041489d
ae <ae@FreeBSD.org>
2012-12-20 11:10:23 +0000

    MFC r244360:   Use M_PROTO7 flag for M_IP6_NEXTHOP, because M_PROTO2 was used for   M_AUTHIPHDR.
    MFC r244365:
      Since we use different flags to detect tcp forwarding, and we share the
      same code for IPv4 and IPv6 in tcp_input, we should check both
      M_IP_NEXTHOP and M_IP6_NEXTHOP flags.
    
    MFC r244386 (by glebius):
      Clear correct flag in INET6 case.
    
    MFC r244387 (by glebius):
      Fix !INET6 build after r244365.

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -792,7 +792,17 @@ findpcb:
 	/*
 	 * Grab info from PACKET_TAG_IPFORWARD tag prepended to the chain.
 	 */
-	if (m->m_flags & M_IP_NEXTHOP)
+        if (
+#ifdef INET6
+	    (isipv6 && (m->m_flags & M_IP6_NEXTHOP))
+#ifdef INET
+	    || (!isipv6 && (m->m_flags & M_IP_NEXTHOP))
+#endif
+#endif
+#if defined(INET) && !defined(INET6)
+	    (m->m_flags & M_IP_NEXTHOP)
+#endif
+	    )
 		fwd_tag = m_tag_find(m, PACKET_TAG_IPFORWARD, NULL);
 
 #ifdef INET6
@@ -821,7 +831,7 @@ findpcb:
 		}
 		/* Remove the tag from the packet.  We don't need it anymore. */
 		m_tag_delete(m, fwd_tag);
-		m->m_flags &= ~M_IP_NEXTHOP;
+		m->m_flags &= ~M_IP6_NEXTHOP;
 		fwd_tag = NULL;
 	} else if (isipv6) {
 		inp = in6_pcblookup_mbuf(&V_tcbinfo, &ip6->ip6_src,
