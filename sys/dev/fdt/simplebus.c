f0cc93d0eb704478931378e591d878b68cb38740
ray <ray@FreeBSD.org>
2012-12-27 21:30:10 +0000

    Cover with bootverbose only message, but not return on no resource.
    Pointed by:	gavin

--- sys/dev/fdt/simplebus.c
+++ sys/dev/fdt/simplebus.c
@@ -256,9 +256,10 @@ simplebus_alloc_resource(device_t bus, device_t child, int type, int *rid,
 			type = SYS_RES_MEMORY;
 
 		rle = resource_list_find(&di->di_res, type, *rid);
-		if (rle == NULL && bootverbose) {
-			device_printf(bus, "no default resources for "
-			    "rid = %d, type = %d\n", *rid, type);
+		if (rle == NULL) {
+			if (bootverbose)
+				device_printf(bus, "no default resources for "
+				    "rid = %d, type = %d\n", *rid, type);
 			return (NULL);
 		}
 		start = rle->start;
