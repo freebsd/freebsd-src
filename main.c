c323c12cd8cea3384b83ddef0592138f8eccbe70
Xin LI <delphij@FreeBSD.org>
2013-01-02 21:53:45 +0000

    Vendor import of bwk's Dec 20, 2012 version.

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20110810";
+const char	*version = "version 20121220";
 
 #define DEBUG
 #include <stdio.h>
