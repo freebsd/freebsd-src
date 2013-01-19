4ec3ea772186699333f1ee5e250c5e54a24dcf9c
Konstantin Belousov <kib@FreeBSD.org>
2013-01-19 06:27:39 +0000

    MFC r245406: Add vfs_hash_index().

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -810,6 +810,7 @@ extern struct vop_vector default_vnodeops;
 typedef int vfs_hash_cmp_t(struct vnode *vp, void *arg);
 
 int vfs_hash_get(const struct mount *mp, u_int hash, int flags, struct thread *td, struct vnode **vpp, vfs_hash_cmp_t *fn, void *arg);
+u_int vfs_hash_index(struct vnode *vp);
 int vfs_hash_insert(struct vnode *vp, u_int hash, int flags, struct thread *td, struct vnode **vpp, vfs_hash_cmp_t *fn, void *arg);
 void vfs_hash_rehash(struct vnode *vp, u_int hash);
 void vfs_hash_remove(struct vnode *vp);
