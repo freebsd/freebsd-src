f28a552089d1cfeebee29bdd8c9638f942ec60bd
Adrian Chadd <adrian@FreeBSD.org>
2013-01-26 00:14:34 +0000

    Migrate the TX sending code out from under the ath0 taskq and into the separate ath0 TX taskq.
    Whilst here, make sure that the TX software scheduler is also
    running out of the TX task, rather than the ath0 taskqueue.
    
    Make sure that the tx taskqueue is blocked/unblocked as necessary.
    
    This allows for a little more parallelism on multi-core machines,
    as well as (eventually) supporting a higher task priority for TX
    tasks, allowing said TX task to preempt an already running RX or
    TX completion task.
    
    Tested:
    
    * AR5416, AR9280 hostap and STA modes

--- sys/dev/ath/if_ath_tx.c
+++ sys/dev/ath/if_ath_tx.c
@@ -3001,7 +3001,7 @@ ath_tx_tid_resume(struct ath_softc *sc, struct ath_tid *tid)
 	ath_tx_tid_sched(sc, tid);
 	/* Punt some frames to the hardware if needed */
 	//ath_txq_sched(sc, sc->sc_ac2q[tid->ac]);
-	taskqueue_enqueue(sc->sc_tq, &sc->sc_txqtask);
+	taskqueue_enqueue(sc->sc_tx_tq, &sc->sc_txqtask);
 }
 
 /*
