4717d76365e50c843dc3874b944d198d0c67b0e5
gabor <gabor@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-01-29 17:03:18 +0000

    - Add a BSD-licensed patch, ported by Pedro F. Giffuni (pfg) from   DragonflyBSD and install it as bsdpatch.  WITH_BSD_PATCH makes it   default and installs GNU patch as gnupatch.
    Submitted by:	pfg
    Obtained from:	The DragonflyBSD Project
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@246074 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ usr.bin/patch/pathnames.h
@@ -0,0 +1,13 @@
+/* $FreeBSD$ */
+/*-
+ * Placed in the public domain by Todd C. Miller <Todd.Miller@courtesan.com>
+ * on July 29, 2003.
+ *
+ * $OpenBSD: pathnames.h,v 1.1 2003/07/29 20:10:17 millert Exp $
+ * $DragonFly: src/usr.bin/patch/pathnames.h,v 1.2 2008/08/11 00:04:12 joerg Exp $
+ */
+
+
+#include <paths.h>
+
+#define	_PATH_ED		"/bin/ed"
