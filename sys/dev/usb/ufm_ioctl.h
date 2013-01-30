7e7b1248d17763443f373cb779bffda883b36d51
Hans Petter Selasky <hselasky@FreeBSD.org>
2013-01-30 10:59:42 +0000

    Add missing header file inclusion guard.

--- sys/dev/usb/ufm_ioctl.h
+++ sys/dev/usb/ufm_ioctl.h
@@ -1,3 +1,4 @@
+/* $FreeBSD$ */
 /*-
  * Copyright (c) 2001 M. Warner Losh
  * All rights reserved.
@@ -28,7 +29,8 @@
  * its contributors.
  */
 
-/*  $FreeBSD$ */
+#ifndef _UFM_IOCTL_H_
+#define	_UFM_IOCTL_H_
 
 #include <sys/ioccom.h>
 
@@ -37,3 +39,5 @@
 #define	FM_START	_IOWR('U', 202, int)
 #define	FM_STOP		_IOWR('U', 203, int)
 #define	FM_GET_STAT	_IOWR('U', 204, int)
+
+#endif			/* _UFM_IOCTL_H_ */
