69db492ce55c9787def3ba14dca33e52909dd5ca
sjg <sjg@FreeBSD.org>
2013-01-31 16:47:34 +0000

    Tag bmake-20130123
    Approved by:	marcel (mentor)

---
+++ 20130123/mk/init.mk
@@ -0,0 +1,44 @@
+# $Id: init.mk,v 1.8 2012/11/11 22:37:02 sjg Exp $
+#
+#	@(#) Copyright (c) 2002, Simon J. Gerraty
+#
+#	This file is provided in the hope that it will
+#	be of use.  There is absolutely NO WARRANTY.
+#	Permission to copy, redistribute or otherwise
+#	use this file is hereby granted provided that 
+#	the above copyright notice and this notice are
+#	left intact. 
+#      
+#	Please send copies of changes and bug-fixes to:
+#	sjg@crufty.net
+#
+
+.if !target(__${.PARSEFILE}__)
+__${.PARSEFILE}__:
+
+.if ${MAKE_VERSION:U0} > 20100408
+_this_mk_dir := ${.PARSEDIR:tA}
+.else
+_this_mk_dir := ${.PARSEDIR}
+.endif
+
+.-include <local.init.mk>
+.-include "${.CURDIR:H}/Makefile.inc"
+.include <own.mk>
+
+.MAIN:		all
+
+.if !empty(WARNINGS_SET) || !empty(WARNINGS_SET_${MACHINE_ARCH})
+.include <warnings.mk>
+.endif
+
+COPTS += ${COPTS.${.IMPSRC:T}}
+CPPFLAGS += ${CPPFLAGS.${.IMPSRC:T}}
+CPUFLAGS += ${CPUFLAGS.${.IMPSRC:T}}
+
+.if ${.MAKE.LEVEL:U1} == 0 && ${BUILD_AT_LEVEL0:Uyes:tl} == "no"
+# this tells lib.mk and prog.mk to not actually build anything
+_SKIP_BUILD = not building at level 0
+.endif
+
+.endif
