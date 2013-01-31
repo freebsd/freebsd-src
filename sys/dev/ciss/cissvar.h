da535d6d26dc98e68bce78c5895c63cf573dff6f
Sean Bruno <sbruno@FreeBSD.org>
2013-01-31 20:32:11 +0000

    MFC r245459
      Satisfy the intent of kern/151564: [ciss] ciss(4) should increase
      CISS_MAX_LOGICAL to 107
    
      Submitter wanted to increase the number of logical disks supported by ciss(4)
      by simply raising the CISS_MAX_LOGICAL value even higher.  Instead, consult
      the documentation for the raid controller (OPENCISS) and poke the controller
      bits to ask it for how many logical/physical disks it can handle.
    
      Revert svn R242089 that raised CISS_MAX_LOGICAL to 64 for all controllers.
    
      For older controllers that don't support this mechanism, fallback to the old
      value of 16 logical disks.  Tested on P420, P410, P400 and 6i model ciss(4)
      controllers.
    
      This should will be MFC'd back to stable/9 stable/8 and stable/7 after the MFC
      period.

--- sys/dev/ciss/cissvar.h
+++ sys/dev/ciss/cissvar.h
@@ -45,6 +45,9 @@ typedef STAILQ_HEAD(, ciss_request)	cr_qhead_t;
 
 /*
  * Maximum number of logical drives we support.
+ * If the controller does not indicate a maximum
+ * value.  This is a compatibiliy value to support
+ * older ciss controllers (e.g. model 6i)
  */
 #define CISS_MAX_LOGICAL	15
 
