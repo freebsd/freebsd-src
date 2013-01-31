a236fc7fcc3822483877a07e7a3e395013f66e2c
marius <marius@FreeBSD.org>
2013-01-31 00:02:36 +0000

    Mark tftp_log() as __printflike() (which would have caught the bug fixed in r246106) and deal with the fallout.
    MFC after:	2 weeks

--- libexec/tftpd/tftp-io.c
+++ libexec/tftpd/tftp-io.c
@@ -106,13 +106,13 @@ send_packet(int peer, uint16_t block, char *pkt, int size)
 	for (i = 0; i < 12 ; i++) {
 		DROPPACKETn("send_packet", 0);
 
-		if (sendto(peer, pkt, size, 0,
-			(struct sockaddr *)&peer_sock, peer_sock.ss_len)
-			== size) {
+		if (sendto(peer, pkt, size, 0, (struct sockaddr *)&peer_sock,
+		    peer_sock.ss_len) == size) {
 			if (i)
 				tftp_log(LOG_ERR,
 				    "%s block %d, attempt %d successful",
-				    block, i);
+		    		    packettype(ntohs(((struct tftphdr *)
+				    (pkt))->th_opcode)), block, i);
 			return (0);
 		}
 		tftp_log(LOG_ERR,
