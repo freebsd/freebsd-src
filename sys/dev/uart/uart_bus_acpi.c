0288c0b6806a0ef9b45389b826b05dad77c89141
avg <avg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-02-02 11:38:26 +0000

    uart: add resume method and enable it for attachments on the most common x86 buses
    Otherwise the uart hardware could be in such a state after the resume
    where IER is cleared and thus no interrupts are generated.
    
    This behavior is observed and tested with QEMU, so I am comitting this
    change to help with my debugging.
    There has been no feedback from users of serial ports on real hardware.
    
    MFC after:	20 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@246243 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/uart/uart_bus_acpi.c
+++ sys/dev/uart/uart_bus_acpi.c
@@ -47,6 +47,7 @@ static device_method_t uart_acpi_methods[] = {
 	DEVMETHOD(device_probe,		uart_acpi_probe),
 	DEVMETHOD(device_attach,	uart_bus_attach),
 	DEVMETHOD(device_detach,	uart_bus_detach),
+	DEVMETHOD(device_resume,	uart_bus_resume),
 	{ 0, 0 }
 };
 
