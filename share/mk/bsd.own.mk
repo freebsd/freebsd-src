d965d42a8802974a525442afc874cea07965857d
Dimitry Andric <dim@FreeBSD.org>
2013-02-02 12:08:28 +0000

    MFC r246131:
    Fix a problem introduced in r231057: in bsd.own.mk, move the test for
    whether clang is enabled to just after the last place where it could
    have been forced to "no".

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -531,10 +531,6 @@ MK_ZFS:=	no
 MK_CTF:=	no
 .endif
 
-.if ${MK_CLANG} == "no"
-MK_CLANG_EXTRAS:= no
-.endif
-
 .if ${MK_CRYPT} == "no"
 MK_OPENSSL:=	no
 MK_OPENSSH:=	no
@@ -581,6 +577,7 @@ MK_GDB:=	no
 .endif
 
 .if ${MK_CLANG} == "no"
+MK_CLANG_EXTRAS:= no
 MK_CLANG_IS_CC:= no
 .endif
 
