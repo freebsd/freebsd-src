b7bcc3ad300811c097cef807161cbc7ac8149bcf
monthadar <monthadar@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-02-07 21:30:29 +0000

    Mesh bug: debug infomartion showing swapped SA and DA address.
    * Fix bug for "forward frame from SA(%6D), DA(%6D)" where addresses where
      swapped between SA and DA;
    
    Approved by:	adrian (mentor)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@246517 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net80211/ieee80211_output.c
+++ sys/net80211/ieee80211_output.c
@@ -264,8 +264,8 @@ ieee80211_start(struct ifnet *ifp)
 				}
 				IEEE80211_DPRINTF(vap, IEEE80211_MSG_OUTPUT,
 				    "forward frame from DS SA(%6D), DA(%6D)\n",
-				    eh->ether_dhost, ":",
-				    eh->ether_shost, ":");
+				    eh->ether_shost, ":",
+				    eh->ether_dhost, ":");
 				ieee80211_mesh_proxy_check(vap, eh->ether_shost);
 			}
 			ni = ieee80211_mesh_discover(vap, eh->ether_dhost, m);
