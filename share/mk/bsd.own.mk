77a6c0dbe12dc86729361b33e135b07af9414897
dim <dim@FreeBSD.org>
2013-02-10 21:24:47 +0000

    MFC r246259:
    Pull in r170135 from upstream clang trunk:
    
      Dont use/link ARCMT, StaticAnalyzer and Rewriter to clang when the user
      specifies not to. Dont build ASTMatchers with Rewriter disabled and
      StaticAnalyzer when it's disabled.
    
      Without all those three, the clang binary shrinks (x86_64) from ~36MB
      to ~32MB (unstripped).
    
    To disable these clang components, and get a smaller clang binary built
    and installed, set WITHOUT_CLANG_FULL in src.conf(5).  During the
    initial stages of buildworld, those extra components are already
    disabled automatically, to save some build time.

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -448,9 +448,9 @@ __T=${MACHINE_ARCH}
 .endif
 # Clang is only for x86 and powerpc right now, by default.
 .if ${__T} == "amd64" || ${__T} == "i386" || ${__T:Mpowerpc*}
-__DEFAULT_YES_OPTIONS+=CLANG
+__DEFAULT_YES_OPTIONS+=CLANG CLANG_FULL
 .else
-__DEFAULT_NO_OPTIONS+=CLANG
+__DEFAULT_NO_OPTIONS+=CLANG CLANG_FULL
 .endif
 # FDT is needed only for arm and powerpc
 .if ${__T:Marm*} || ${__T:Mpowerpc*}
@@ -579,6 +579,7 @@ MK_GDB:=	no
 
 .if ${MK_CLANG} == "no"
 MK_CLANG_EXTRAS:= no
+MK_CLANG_FULL:= no
 MK_CLANG_IS_CC:= no
 .endif
 
