807266d890c61a71f8961d184f6ca5645c8ed32e
neel <neel@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-02-11 20:36:07 +0000

    Implement guest vcpu pinning using 'pthread_setaffinity_np(3)'.
    Prior to this change pinning was implemented via an ioctl (VM_SET_PINNING)
    that called 'sched_bind()' on behalf of the user thread.
    
    The ULE implementation of 'sched_bind()' bumps up 'td_pinned' which in turn
    runs afoul of the assertion '(td_pinned == 0)' in userret().
    
    Using the cpuset affinity to implement pinning of the vcpu threads works with
    both 4BSD and ULE schedulers and has the happy side-effect of getting rid
    of a bunch of code in vmm.ko.
    
    Discussed with:	grehan
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@246686 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/amd64/vmm/io/ppt.c
+++ sys/amd64/vmm/io/ppt.c
@@ -402,31 +402,6 @@ pptintr(void *arg)
 		return (FILTER_HANDLED);
 }
 
-/*
- * XXX
- * When we try to free the MSI resource the kernel will bind the thread to
- * the host cpu was originally handling the MSI. The function freeing the
- * MSI vector (apic_free_vector()) will panic the kernel if the thread
- * is already bound to a cpu.
- * 
- * So, we temporarily unbind the vcpu thread before freeing the MSI resource.
- */
-static void
-PPT_TEARDOWN_MSI(struct vm *vm, int vcpu, struct pptdev *ppt)
-{
-	int pincpu = -1;
-
-	vm_get_pinning(vm, vcpu, &pincpu);
-
-	if (pincpu >= 0)
-		vm_set_pinning(vm, vcpu, -1);
-
-	ppt_teardown_msi(ppt);
-
-	if (pincpu >= 0)
-		vm_set_pinning(vm, vcpu, pincpu);
-}
-
 int
 ppt_setup_msi(struct vm *vm, int vcpu, int bus, int slot, int func,
 	      int destcpu, int vector, int numvec)
@@ -447,7 +422,7 @@ ppt_setup_msi(struct vm *vm, int vcpu, int bus, int slot, int func,
 		return (EBUSY);
 
 	/* Free any allocated resources */
-	PPT_TEARDOWN_MSI(vm, vcpu, ppt);
+	ppt_teardown_msi(ppt);
 
 	if (numvec == 0)		/* nothing more to do */
 		return (0);
@@ -513,7 +488,7 @@ ppt_setup_msi(struct vm *vm, int vcpu, int bus, int slot, int func,
 	}
 	
 	if (i < numvec) {
-		PPT_TEARDOWN_MSI(vm, vcpu, ppt);
+		ppt_teardown_msi(ppt);
 		return (ENXIO);
 	}
 
