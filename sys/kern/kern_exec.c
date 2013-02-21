843e88dd773c4976414954ce8d2dc65c6f261bb1
Konstantin Belousov <kib@FreeBSD.org>
2013-02-21 05:47:52 +0000

    MFC r246484: Allow ptrace(2) operation on the child created by vfork(2), if the debugger is not the parent.

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -646,7 +646,7 @@ interpret:
 	 */
 	p->p_flag |= P_EXEC;
 	if (p->p_pptr && (p->p_flag & P_PPWAIT)) {
-		p->p_flag &= ~P_PPWAIT;
+		p->p_flag &= ~(P_PPWAIT | P_PPTRACE);
 		cv_broadcast(&p->p_pwait);
 	}
 
--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -636,6 +636,7 @@ struct proc {
 #define	P_INMEM		0x10000000 /* Loaded into memory. */
 #define	P_SWAPPINGOUT	0x20000000 /* Process is being swapped out. */
 #define	P_SWAPPINGIN	0x40000000 /* Process is being swapped in. */
+#define	P_PPTRACE	0x80000000 /* PT_TRACEME by vforked child. */
 
 #define	P_STOPPED	(P_STOPPED_SIG|P_STOPPED_SINGLE|P_STOPPED_TRACE)
 #define	P_SHOULDSTOP(p)	((p)->p_flag & P_STOPPED)
