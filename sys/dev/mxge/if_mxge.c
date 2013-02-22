bea0ee26db180e03b524785dea704a63aa879202
gallatin <gallatin@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-02-22 19:23:33 +0000

    Bump mxge copyright.
    Sponsored by: Myricom
    
    MFC After: 7 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@247160 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/mxge/if_mxge.c
+++ sys/dev/mxge/if_mxge.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 
-Copyright (c) 2006-2009, Myricom Inc.
+Copyright (c) 2006-2013, Myricom Inc.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
