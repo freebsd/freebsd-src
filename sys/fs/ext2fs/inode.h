cf9f38a0012d6b63abaa850b36eff86430f37e67
Pedro F. Giffuni <pfg@FreeBSD.org>
2013-03-01 22:03:57 +0000

    MFC	r246562:
    Remove unused MAXSYMLINKLEN macro.
    
    Reviewed by:	mckusick
    PR:		kern/175794

--- sys/fs/ext2fs/inode.h
+++ sys/fs/ext2fs/inode.h
@@ -108,7 +108,6 @@ struct inode {
  */
 #define	i_shortlink	i_db
 #define	i_rdev		i_db[0]
-#define	MAXSYMLINKLEN	((NDADDR + NIADDR) * sizeof(int32_t))
 
 /* File permissions. */
 #define	IEXEC		0000100		/* Executable. */
