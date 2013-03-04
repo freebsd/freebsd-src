715efda91d685a32e6705b9d8db6128e6d2bfdbc
eadler <eadler@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-03-04 02:21:17 +0000

    devd: Use the standard constructor of std::string instead of string("").
    Submitted by:	Christoph Mallon <christoph.mallon@gmx.de>
    Approved by:	cperciva (mentor)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@247757 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/devd/devd.hh
+++ sbin/devd/devd.hh
@@ -144,7 +144,7 @@ private:
 class config
 {
 public:
-	config() : _pidfile("") { push_var_table(); }
+	config() { push_var_table(); }
 	virtual ~config() { reset(); }
 	void add_attach(int, event_proc *);
 	void add_detach(int, event_proc *);
