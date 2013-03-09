c934116100c5a3574cb1bde0dc0b80b6b41d7e54
Attilio Rao <attilio@FreeBSD.org>
2013-03-09 02:05:29 +0000

    Merge from vmc-playground: Introduce a new KPI that verifies if the page cache is empty for a specified vm_object.  This KPI does not make assumptions about the locking in order to be used also for building assertions at init and destroy time. It is mostly used to hide implementation details of the page cache.
    Sponsored by:	EMC / Isilon storage division
    Reviewed by:	jeff
    Reviewed by:	alc (vm_radix based version)
    Tested by:	flo, pho, jhb, davide

--- sys/cddl/compat/opensolaris/sys/vnode.h
+++ sys/cddl/compat/opensolaris/sys/vnode.h
@@ -76,7 +76,7 @@ vn_is_readonly(vnode_t *vp)
 #define	vn_has_cached_data(vp)	\
 	((vp)->v_object != NULL && \
 	 ((vp)->v_object->resident_page_count > 0 || \
-	  (vp)->v_object->cache != NULL))
+	  !vm_object_cache_is_empty((vp)->v_object)))
 #define	vn_exists(vp)		do { } while (0)
 #define	vn_invalid(vp)		do { } while (0)
 #define	vn_renamepath(tdvp, svp, tnm, lentnm)	do { } while (0)
