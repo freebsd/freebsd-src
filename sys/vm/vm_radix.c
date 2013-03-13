07b5846fc9cc30d0615008183c36634d0693dc30
attilio <attilio@FreeBSD.org>
2013-03-13 01:38:32 +0000

    Fix compilation.
    Sponsored by:	EMC / Isilon storage division

--- sys/vm/vm_radix.c
+++ sys/vm/vm_radix.c
@@ -375,7 +375,8 @@ vm_radix_insert(struct vm_radix *rtree, vm_pindex_t index, vm_page_t page)
 	int slot;
 	uint16_t clev;
 
-	KASSERT(index == page->index, ("%s: index != page->index", __func__));
+	KASSERT(index == page->pindex, ("%s: index != page->pindex",
+	    __func__));
 
 	/*
 	 * The owner of record for root is not really important because it
