3f7d9a0dbe6a4a3ab79e5289198dd355d76eaafa
Martin Matuska <mm@FreeBSD.org>
2013-03-16 08:16:11 +0000

    MFC r247187,247265,247348,247398,247540,247585,247852,248265,248267 Merge various ZFS improvements and bugfixes
    MFC r247187:
      Import vendor change to avoid "unitialized variable" warnings.
    
      Illumos ZFS issues:
      3522 zfs module should not allow uninitialized variables
    
    MFC r247265:
      Merge the ZFS I/O deadman thread from vendor (illumos).
      This feature panics the system on hanging ZFS I/O, helps debugging
      and resumes failed service.
    
      The panic behavior can be controlled with the loader-only tunables:
      vfs.zfs.deadman_enabled (enable or disable panic on stalled ZFS I/O)
      vfs.zfs.deadman_synctime (expiration time for stalled ZFS I/O)
    
      By default, ZFS I/O deadman is enabled by default on amd64 and i386
      excluding virtual guest machines.
    
    MFC r247348:
      Be more verbose on ZFS deadman I/O panic
      Patch suggested upstream.
    
    MFC r247398:
      Import metaslab_sync() speedup from vendor (illumos).
    
      Illumos ZFS issues:
      3552 condensing one space map burns 3 seconds of CPU in spa_sync() thread
      3564 spa_sync() spends 5-10% of its time in metaslab_sync() (when not
    	   condensing)
      3578 transferring the freed map to the defer map should be constant time
      3579 ztest trips assertion in metaslab_weight()
    
    MFC r247540:
      Fix the zfs_ioctl compat layer to support zfs_cmd size change introduced
      in r247265 (ZFS deadman thread). Both new utilities now support the old
      kernel and new kernel properly detects old utilities.
    
      For future backwards compatibility, the vfs.zfs.version.ioctl read-only
      sysctl has been introduced. With this sysctl zfs utilities will be able
      to detect the ioctl interface version of the currently loaded zfs module.
    
    MFC r247585:
      Merge new read-only zfs properties from vendor (illumos)
    
      Illumos ZFS issues:
      3588 provide zfs properties for logical (uncompressed) space used and
           referenced
    
    MFC r247852:
      Import ZFS bpobj bugfix from vendor.
    
      Illumos ZFS issues:
      3603 panic from bpobj_enqueue_subobj()
      3604 zdb should print bpobjs more verbosely
    
    MFC r248265:
      Update zfs.8 manpage date (missing in r247585)
    
    MFC r248267:
      Import minor ZFS changes from vendor
    
      Illumos ZFS issues:
      3604 zdb should print bpobjs more verbosely (fix zdb hang)
      3606 zpool status -x shouldn't warn about old on-disk format

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/spa.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/spa.c
@@ -22,6 +22,7 @@
 /*
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2012 by Delphix. All rights reserved.
+ * Copyright (c) 2013 Martin Matuska <mm@FreeBSD.org>. All rights reserved.
  */
 
 /*
@@ -139,6 +140,10 @@ uint_t		zio_taskq_basedc = 80;		/* base duty cycle */
 
 boolean_t	spa_create_process = B_TRUE;	/* no process ==> no sysdc */
 extern int	zfs_sync_pass_deferred_free;
+
+#ifndef illumos
+extern void spa_deadman(void *arg);
+#endif
 
 /*
  * This (illegal) pool name is used when temporarily importing a spa_t in order
@@ -382,7 +387,7 @@ spa_prop_validate(spa_t *spa, nvlist_t *props)
 {
 	nvpair_t *elem;
 	int error = 0, reset_bootfs = 0;
-	uint64_t objnum;
+	uint64_t objnum = 0;
 	boolean_t has_feature = B_FALSE;
 
 	elem = NULL;
@@ -1372,6 +1377,7 @@ spa_load_l2cache(spa_t *spa)
 		newvdevs = kmem_alloc(nl2cache * sizeof (void *), KM_SLEEP);
 	} else {
 		nl2cache = 0;
+		newvdevs = NULL;
 	}
 
 	oldvdevs = sav->sav_vdevs;
@@ -4685,7 +4691,7 @@ spa_vdev_detach(spa_t *spa, uint64_t guid, uint64_t pguid, int replace_done)
 	vdev_t *rvd = spa->spa_root_vdev;
 	vdev_t *vd, *pvd, *cvd, *tvd;
 	boolean_t unspare = B_FALSE;
-	uint64_t unspare_guid;
+	uint64_t unspare_guid = 0;
 	char *vdpath;
 
 	ASSERT(spa_writeable(spa));
@@ -6240,6 +6246,17 @@ spa_sync(spa_t *spa, uint64_t txg)
 
 	tx = dmu_tx_create_assigned(dp, txg);
 
+	spa->spa_sync_starttime = gethrtime();
+#ifdef illumos
+	VERIFY(cyclic_reprogram(spa->spa_deadman_cycid,
+	    spa->spa_sync_starttime + spa->spa_deadman_synctime));
+#else	/* FreeBSD */
+#ifdef _KERNEL
+	callout_reset(&spa->spa_deadman_cycid,
+	    hz * spa->spa_deadman_synctime / NANOSEC, spa_deadman, spa);
+#endif
+#endif
+
 	/*
 	 * If we are upgrading to SPA_VERSION_RAIDZ_DEFLATE this txg,
 	 * set spa_deflate if we have no raid-z vdevs.
@@ -6367,6 +6384,14 @@ spa_sync(spa_t *spa, uint64_t txg)
 		zio_resume_wait(spa);
 	}
 	dmu_tx_commit(tx);
+
+#ifdef illumos
+	VERIFY(cyclic_reprogram(spa->spa_deadman_cycid, CY_INFINITY));
+#else	/* FreeBSD */
+#ifdef _KERNEL
+	callout_drain(&spa->spa_deadman_cycid);
+#endif
+#endif
 
 	/*
 	 * Clear the dirty config list.
