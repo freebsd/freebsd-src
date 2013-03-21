0048458a8f8939aa311ce60b507da295ad4abe2f
smh <smh@FreeBSD.org>
2013-03-21 09:34:41 +0000

    Add TRIM support for L2ARC.
    This adds TRIM support to cache vdevs. When ARC buffers are removed
    from the L2ARC in arc_hdr_destroy(), arc_release() or l2arc_evict(),
    the size previously occupied by the buffer gets scheduled for TRIMming.
    As always, actual TRIMs are only issued to the L2ARC after
    txg_trim_limit.
    
    Reviewed by:	pjd (mentor)
    Approved by:	pjd (mentor)
    Obtained from:	https://github.com/dechamps/zfs/commit/31aae373994fd112256607edba7de2359da3e9dc
    MFC after:	2 weeks

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zio.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zio.c
@@ -2475,7 +2475,7 @@ zio_vdev_io_start(zio_t *zio)
 	}
 
 	if (vd->vdev_ops->vdev_op_leaf && zio->io_type == ZIO_TYPE_FREE) {
-		trim_map_free(zio);
+		trim_map_free(vd, zio->io_offset, zio->io_size);
 		return (ZIO_PIPELINE_CONTINUE);
 	}
 
