ca02c8e40ffd8df627af959bb4bdf3ec761b4dd6
des <des@FreeBSD.org>
2013-04-01 13:48:30 +0000

    Silence warnings about redefined macros.

--- crypto/openssh/umac128.c
+++ crypto/openssh/umac128.c
@@ -1,7 +1,11 @@
 /* $FreeBSD$ */
 #define UMAC_OUTPUT_LEN		16
+#undef umac_new
 #define umac_new		ssh_umac128_new
+#undef umac_update
 #define umac_update		ssh_umac128_update
+#undef umac_final
 #define umac_final		ssh_umac128_final
+#undef umac_delete
 #define umac_delete		ssh_umac128_delete
 #include "umac.c"
