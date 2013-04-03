afebaed58a9ee6f63f17437de80e1262f161771d
Konstantin Belousov <kib@FreeBSD.org>
2013-04-03 15:38:44 +0000

    MFC r248563: Increment the write ref counter for the buffer object before calling bundirty().

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -936,7 +936,13 @@ bufwrite(struct buf *bp)
 	else
 		vp_md = 0;
 
-	/* Mark the buffer clean */
+	/*
+	 * Mark the buffer clean.  Increment the bufobj write count
+	 * before bundirty() call, to prevent other thread from seeing
+	 * empty dirty list and zero counter for writes in progress,
+	 * falsely indicating that the bufobj is clean.
+	 */
+	bufobj_wref(bp->b_bufobj);
 	bundirty(bp);
 
 	bp->b_flags &= ~B_DONE;
@@ -944,7 +950,6 @@ bufwrite(struct buf *bp)
 	bp->b_flags |= B_CACHE;
 	bp->b_iocmd = BIO_WRITE;
 
-	bufobj_wref(bp->b_bufobj);
 	vfs_busy_pages(bp, 1);
 
 	/*
