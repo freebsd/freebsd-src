4373522abfbaa6864430fdff4c51f0dab2b0f1ae
Alexander Motin <mav@FreeBSD.org>
2013-04-04 17:08:49 +0000

    Remove xpt_sim_opened(), the only consumer of which was atapicam, which is now gone.

--- sys/cam/cam_xpt_sim.h
+++ sys/cam/cam_xpt_sim.h
@@ -49,7 +49,6 @@ void		xpt_release_devq(struct cam_path *path,
 		    u_int count, int run_queue);
 void		xpt_release_devq_rl(struct cam_path *path, cam_rl rl,
 		    u_int count, int run_queue);
-int		xpt_sim_opened(struct cam_sim *sim);
 void		xpt_done(union ccb *done_ccb);
 void		xpt_batch_start(struct cam_sim *sim);
 void		xpt_batch_done(struct cam_sim *sim);
