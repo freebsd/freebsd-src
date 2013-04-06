d2ce15bd43b3a1dcce08eecbff8d5d359946d972
Marius Strobl <marius@FreeBSD.org>
2013-04-06 19:12:49 +0000

    - With the demise of !ATA_CAM, ATA_STATIC_ID is the only ata(4) related   option left but actually consumed by ada(4), so move it to opt_ada.h   and get rid of opt_ata.h. - Fix stand-alone build of atacore(4) by adding opt_cam.h. - Use __FBSDID. - Use DEVMETHOD_END. - Use NULL instead of 0 for pointers.

--- sys/dev/ata/ata-all.c
+++ sys/dev/ata/ata-all.c
@@ -27,7 +27,6 @@
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include "opt_ata.h"
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/ata.h>
--- sys/dev/ata/ata-isa.c
+++ sys/dev/ata/ata-isa.c
@@ -27,7 +27,6 @@
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include "opt_ata.h"
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/ata.h>
@@ -195,7 +194,7 @@ static device_method_t ata_isa_methods[] = {
     DEVMETHOD(device_suspend,   ata_isa_suspend),
     DEVMETHOD(device_resume,    ata_isa_resume),
 
-    { 0, 0 }
+    DEVMETHOD_END
 };
 
 static driver_t ata_isa_driver = {
@@ -204,5 +203,5 @@ static driver_t ata_isa_driver = {
     sizeof(struct ata_channel),
 };
 
-DRIVER_MODULE(ata, isa, ata_isa_driver, ata_devclass, 0, 0);
+DRIVER_MODULE(ata, isa, ata_isa_driver, ata_devclass, NULL, NULL);
 MODULE_DEPEND(ata, ata, 1, 1, 1);
