774599212c87dec6d2236c2270e7d379107938e2
emaste <emaste@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-04-11 20:15:37 +0000

    Spelling correction
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@249381 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/stdio/fgetln.c
+++ lib/libc/stdio/fgetln.c
@@ -115,7 +115,7 @@ fgetln(FILE *fp, size_t *lenp)
 	 * As a bonus, though, we can leave off the __SMOD.
 	 *
 	 * OPTIMISTIC is length that we (optimistically) expect will
-	 * accomodate the `rest' of the string, on each trip through the
+	 * accommodate the `rest' of the string, on each trip through the
 	 * loop below.
 	 */
 #define OPTIMISTIC 80
