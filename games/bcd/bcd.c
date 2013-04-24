755b7edd4d7ccc1243f8dbe743ff7f2a192b39ba
eadler <eadler@FreeBSD.org>
2013-04-24 12:47:30 +0000

    MFC r227101: 	Add static keywords to variables and functions where possible in games/.
    	This allows us to detect unused functions and variables and allows the
    	compiler to do a better job at optimisation.

--- games/bcd/bcd.c
+++ games/bcd/bcd.c
@@ -82,7 +82,7 @@ static const char rcsid[] =
 #include <string.h>
 #include <unistd.h>
 
-u_short holes[256] = {
+static u_short holes[256] = {
     0x0,	 0x0,	  0x0,	   0x0,	    0x0,     0x0,     0x0,     0x0,
     0x0,	 0x0,	  0x0,	   0x0,	    0x0,     0x0,     0x0,     0x0,
     0x0,	 0x0,	  0x0,	   0x0,	    0x0,     0x0,     0x0,     0x0,
@@ -117,7 +117,7 @@ u_short holes[256] = {
     0x202,	 0x201,	  0x082,   0x806,   0x822,   0x600,   0x282,   0x0
 };
 
-void printcard(char *);
+static void printcard(char *);
 
 /*
  * i'th bit of w.
@@ -145,7 +145,7 @@ main(int argc, char **argv)
 
 #define	COLUMNS	48
 
-void
+static void
 printcard(char *str)
 {
 	static char rowchars[] = "   123456789";
