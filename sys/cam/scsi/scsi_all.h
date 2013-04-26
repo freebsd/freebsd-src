be35276db0a5010d9b162ce3f2f73a033cdba202
smh <smh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-04-26 16:31:03 +0000

    Added automatic detection of non-rotating media which disables the use of BIO queue sorting, hence optimising performance for devices such as SSD's
    Reviewed by:	scottl
    Approved by:	pjd (mentor)
    MFC after:	2 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@249941 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/cam/scsi/scsi_all.h
+++ sys/cam/scsi/scsi_all.h
@@ -1430,6 +1430,30 @@ struct scsi_diag_page {
 };
 
 /*
+ * Block Device Characteristics VPD Page based on
+ * T10/1799-D Revision 31
+ */
+struct scsi_vpd_block_characteristics
+{
+	u_int8_t device;
+	u_int8_t page_code;
+#define SVPD_BDC			0xB1
+	u_int8_t page_length[2];
+	u_int8_t medium_rotation_rate[2];
+#define SVPD_BDC_RATE_NOT_REPORTED	0x00
+#define SVPD_BDC_RATE_NONE_ROTATING	0x01
+	u_int8_t reserved1;
+	u_int8_t nominal_form_factor;
+#define SVPD_BDC_FORM_NOT_REPORTED	0x00
+#define SVPD_BDC_FORM_5_25INCH		0x01
+#define SVPD_BDC_FORM_3_5INCH		0x02
+#define SVPD_BDC_FORM_2_5INCH		0x03
+#define SVPD_BDC_FORM_1_5INCH		0x04
+#define SVPD_BDC_FORM_LESSTHAN_1_5INCH	0x05
+	u_int8_t reserved2[56];
+};
+
+/*
  * Logical Block Provisioning VPD Page based on
  * T10/1799-D Revision 31
  */
