4cd5e735ccb4a6fa45705d6b10f725759d7559fb
adrian <adrian@FreeBSD.org>
2013-04-26 00:53:34 +0000

    Since we're clearing the BAR and filtered frames tracking in the TID, ensure we also take into account the pause counter.

--- sys/dev/ath/if_ath_tx.c
+++ sys/dev/ath/if_ath_tx.c
@@ -3862,6 +3862,36 @@ ath_tx_tid_reset(struct ath_softc *sc, struct ath_tid *tid)
 #endif
 
 	/*
+	 * If we have a bar_wait set, we need to unpause the TID
+	 * here.  Otherwise once cleanup has finished, the TID won't
+	 * have the right paused counter.
+	 *
+	 * XXX I'm not going through resume here - I don't want the
+	 * node to be rescheuled just yet.  This however should be
+	 * methodized!
+	 */
+	if (tid->bar_wait) {
+		if (tid->paused > 0) {
+			tid->paused --;
+		}
+	}
+
+	/*
+	 * XXX same with a currently filtered TID.
+	 *
+	 * Since this is being called during a flush, we assume that
+	 * the filtered frame list is actually empty.
+	 *
+	 * XXX TODO: add in a check to ensure that the filtered queue
+	 * depth is actually 0!
+	 */
+	if (tid->isfiltered) {
+		if (tid->paused > 0) {
+			tid->paused --;
+		}
+	}
+
+	/*
 	 * Clear BAR, filtered frames, scheduled and ADDBA pending.
 	 * The TID may be going through cleanup from the last association
 	 * where things in the BAW are still in the hardware queue.
