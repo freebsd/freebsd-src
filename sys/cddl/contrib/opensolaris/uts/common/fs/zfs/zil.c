562a9d583b45e3c60958f5a2005afc15b8b7cc71
Steven Hartland <smh@FreeBSD.org>
2013-04-26 11:24:20 +0000

    Changed ZFS TRIM sysctl from vfs.zfs.trim_disable -> vfs.zfs.trim.enabled Enabled ZFS TRIM by default
    Reviewed by:	pjd (mentor)
    Approved by:	pjd (mentor)
    MFC after:	2 weeks

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zil.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zil.c
@@ -83,10 +83,11 @@ boolean_t zfs_nocacheflush = B_FALSE;
 TUNABLE_INT("vfs.zfs.cache_flush_disable", &zfs_nocacheflush);
 SYSCTL_INT(_vfs_zfs, OID_AUTO, cache_flush_disable, CTLFLAG_RDTUN,
     &zfs_nocacheflush, 0, "Disable cache flush");
-boolean_t zfs_notrim = B_TRUE;
-TUNABLE_INT("vfs.zfs.trim_disable", &zfs_notrim);
-SYSCTL_INT(_vfs_zfs, OID_AUTO, trim_disable, CTLFLAG_RDTUN, &zfs_notrim, 0,
-    "Disable trim");
+boolean_t zfs_trim_enabled = B_TRUE;
+SYSCTL_DECL(_vfs_zfs_trim);
+TUNABLE_INT("vfs.zfs.trim.enabled", &zfs_trim_enabled);
+SYSCTL_INT(_vfs_zfs_trim, OID_AUTO, enabled, CTLFLAG_RDTUN, &zfs_trim_enabled, 0,
+    "Enable ZFS TRIM");
 
 static kmem_cache_t *zil_lwb_cache;
 
