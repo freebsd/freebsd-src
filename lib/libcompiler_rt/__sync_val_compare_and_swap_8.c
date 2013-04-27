f201fcc561ac7eab4ed7997403288e40b18a75a4
ed <ed@FreeBSD.org>
2013-04-27 04:56:02 +0000

    Unbreak <stdatomic.h> on ARM + Clang.
    Clang only supports atomic operations for ARMv6. For non-ARMv6, we still
    need to emit these functions.
    
    Clang's prototype for these functions slightly differs, as it is truly
    based on GCC's documentation. It requires the use of signed types, but
    also requires varargs. Still, we are not allowed to simply implement
    this function directly. Cleverly work around this by implementing it
    under a different name and using __strong_reference().

--- lib/libcompiler_rt/__sync_val_compare_and_swap_8.c
+++ lib/libcompiler_rt/__sync_val_compare_and_swap_8.c
@@ -1,6 +1,6 @@
 /* $FreeBSD$ */
 #define	NAME		__sync_val_compare_and_swap_8
-#define	TYPE		uint64_t
+#define	TYPE		int64_t
 #define	CMPSET		atomic_cmpset_64
 
 #include "__sync_val_compare_and_swap_n.h"
