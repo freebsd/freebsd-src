7600f9311cb7bf379929bd55ca1b8ab94cc22796
davidxu <davidxu@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-04-28 03:13:45 +0000

    Remove extra code for SA_RESETHAND, it is not needed because kernel has already done this.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@250013 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libthr/thread/thr_sig.c
+++ lib/libthr/thread/thr_sig.c
@@ -336,13 +336,6 @@ check_deferred_signal(struct pthread *curthread)
 		memcpy(&info, &curthread->deferred_siginfo, sizeof(siginfo_t));
 		/* remove signal */
 		curthread->deferred_siginfo.si_signo = 0;
-		if (act.sa_flags & SA_RESETHAND) {
-			struct sigaction tact;
-
-			tact = act;
-			tact.sa_handler = SIG_DFL;
-			_sigaction(info.si_signo, &tact, NULL);
-		}
 		handle_signal(&act, info.si_signo, &info, uc);
 	}
 }
