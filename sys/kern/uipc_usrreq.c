6d0a588192af34d24fb5a5c4a166be7651e19e2b
Sergey Kandaurov <pluknet@FreeBSD.org>
2013-04-29 21:12:18 +0000

    MFC r246826:  Add support of passing SCM_BINTIME ancillary data object for PF_LOCAL  sockets.
    PR:		kern/175883
    Submitted by:	Andrey Simonenko <simon@comsys.ntu-kpi.kiev.ua>

--- sys/kern/uipc_usrreq.c
+++ sys/kern/uipc_usrreq.c
@@ -1706,6 +1706,7 @@ unp_internalize(struct mbuf **controlp, struct thread *td)
 	struct mbuf *control = *controlp;
 	struct proc *p = td->td_proc;
 	struct filedesc *fdescp = p->p_fd;
+	struct bintime *bt;
 	struct cmsghdr *cm = mtod(control, struct cmsghdr *);
 	struct cmsgcred *cmcred;
 	struct file **rp;
@@ -1812,6 +1813,18 @@ unp_internalize(struct mbuf **controlp, struct thread *td)
 			tv = (struct timeval *)
 			    CMSG_DATA(mtod(*controlp, struct cmsghdr *));
 			microtime(tv);
+			break;
+
+		case SCM_BINTIME:
+			*controlp = sbcreatecontrol(NULL, sizeof(*bt),
+			    SCM_BINTIME, SOL_SOCKET);
+			if (*controlp == NULL) {
+				error = ENOBUFS;
+				goto out;
+			}
+			bt = (struct bintime *)
+			    CMSG_DATA(mtod(*controlp, struct cmsghdr *));
+			bintime(bt);
 			break;
 
 		default:
