e3e2301d5525fea1cb5b17a7a0f690c12be31b7f
adrian <adrian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-05-08 20:46:54 +0000

    Add the ability to change the vlan operation mode.
    This adds a vlan capability field to etherswitch_info structure and some
    definitions of ports flags.
    
    It adds the support to global config parameters which right now is used
    only to switch between the vlan modes, but it is intended to be extended
    to support the setup of others parameters (STP, mirror, etc.).
    
    Submitted by:	Luiz Otavio O Souza <loos.br@gmail.com>
    Reviewed by:	ray
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@250381 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/etherswitch/etherswitch.h
+++ sys/dev/etherswitch/etherswitch.h
@@ -25,18 +25,47 @@ struct etherswitch_phyreg {
 };
 typedef struct etherswitch_phyreg etherswitch_phyreg_t;
 
-#define ETHERSWITCH_NAMEMAX	64
+#define	ETHERSWITCH_NAMEMAX		64
+#define	ETHERSWITCH_VLAN_ISL		(1 << 0)	/* ISL */
+#define	ETHERSWITCH_VLAN_PORT		(1 << 1)	/* Port based vlan */
+#define	ETHERSWITCH_VLAN_DOT1Q		(1 << 2)	/* 802.1q */
+#define	ETHERSWITCH_VLAN_DOT1Q_4K	(1 << 3)	/* 4k support on 802.1q */
+#define	ETHERSWITCH_VLAN_DOUBLE_TAG	(1 << 4)	/* Q-in-Q */
+#define	ETHERSWITCH_VLAN_CAPS_BITS	\
+"\020\1ISL\2PORT\3DOT1Q\4DOT1Q4K\5QinQ"
 
 struct etherswitch_info {
 	int		es_nports;
 	int		es_nvlangroups;
 	char		es_name[ETHERSWITCH_NAMEMAX];
+	uint32_t	es_vlan_caps;
 };
 typedef struct etherswitch_info etherswitch_info_t;
+
+#define	ETHERSWITCH_CONF_FLAGS		(1 << 0)
+#define	ETHERSWITCH_CONF_MIRROR		(1 << 1)
+#define	ETHERSWITCH_CONF_VLAN_MODE	(1 << 2)
+
+struct etherswitch_conf {
+	uint32_t	cmd;		/* What to configure */
+	uint32_t	vlan_mode;	/* Switch VLAN mode */
+};
+typedef struct etherswitch_conf etherswitch_conf_t;
+
+#define	ETHERSWITCH_PORT_CPU		(1 << 0)
+#define	ETHERSWITCH_PORT_STRIPTAG	(1 << 1)
+#define	ETHERSWITCH_PORT_ADDTAG		(1 << 2)
+#define	ETHERSWITCH_PORT_FIRSTLOCK	(1 << 3)
+#define	ETHERSWITCH_PORT_DROPUNTAGGED	(1 << 4)
+#define	ETHERSWITCH_PORT_DOUBLE_TAG	(1 << 5)
+#define	ETHERSWITCH_PORT_INGRESS	(1 << 6)
+#define	ETHERSWITCH_PORT_FLAGS_BITS	\
+"\020\1CPUPORT\2STRIPTAG\3ADDTAG\4FIRSTLOCK\5DROPUNTAGGED\6QinQ\7INGRESS"
 
 struct etherswitch_port {
 	int		es_port;
 	int		es_pvid;
+	uint32_t	es_flags;
 	union {
 		struct ifreq		es_uifr;
 		struct ifmediareq	es_uifmr;
@@ -66,5 +95,7 @@ typedef struct etherswitch_vlangroup etherswitch_vlangroup_t;
 #define IOETHERSWITCHSETVLANGROUP	_IOW('i', 7, etherswitch_vlangroup_t)
 #define IOETHERSWITCHGETPHYREG		_IOWR('i', 8, etherswitch_phyreg_t)
 #define IOETHERSWITCHSETPHYREG		_IOW('i', 9, etherswitch_phyreg_t)
+#define IOETHERSWITCHGETCONF		_IOR('i', 10, etherswitch_conf_t)
+#define IOETHERSWITCHSETCONF		_IOW('i', 11, etherswitch_conf_t)
 
 #endif
