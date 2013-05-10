ad9097e948a5b21dcce73de7bfca01917c3a984a
imp <imp@FreeBSD.org>
2013-05-10 04:49:40 +0000

    As requested, move this back to opt-in and list my boards.

--- sys/contrib/octeon-sdk/cvmx-rtc.h
+++ sys/contrib/octeon-sdk/cvmx-rtc.h
@@ -87,8 +87,17 @@ static inline cvmx_rtc_options_t cvmx_rtc_supported(void)
 	    supported = CVMX_RTC_READ | CVMX_RTC_WRITE | CVMX_RTC_TIME_EPOCH;
 	    break;
 
-	default:
+	case CVMX_BOARD_TYPE_EBH3000:
+	case CVMX_BOARD_TYPE_CN3010_EVB_HS5:
+	case CVMX_BOARD_TYPE_EBH5200:
+#if defined(OCTEON_VENDOR_GEFES)
+	case CVMX_BOARD_TYPE_CUST_W63XX:
+#endif
 	    supported = CVMX_RTC_READ | CVMX_RTC_WRITE | CVMX_RTC_TIME_CAL;
+	    break;
+
+	default:
+	    supported = 0;
 	    break;
 	}
 
@@ -117,9 +126,18 @@ static inline uint32_t cvmx_rtc_read(void)
         return cvmx_rtc_ds1374_read();
         break;
 
-    default:
+    case CVMX_BOARD_TYPE_EBH3000:
+    case CVMX_BOARD_TYPE_CN3010_EVB_HS5:
+    case CVMX_BOARD_TYPE_EBH5200:
+#if defined(OCTEON_VENDOR_GEFES)
+    case CVMX_BOARD_TYPE_CUST_W63XX:
+#endif
 	return cvmx_rtc_ds1337_read();
 	break;
+
+    default:
+        return 0;
+        break;
     }
 }
 
@@ -138,9 +156,18 @@ static inline uint32_t cvmx_rtc_write(uint32_t time)
         return cvmx_rtc_ds1374_write(time);
         break;
 
-    default:
+    case CVMX_BOARD_TYPE_EBH3000:
+    case CVMX_BOARD_TYPE_CN3010_EVB_HS5:
+    case CVMX_BOARD_TYPE_EBH5200:
+#if defined(OCTEON_VENDOR_GEFES)
+    case CVMX_BOARD_TYPE_CUST_W63XX:
+#endif
 	return cvmx_rtc_ds1337_write(time);
 	break;
+
+    default:
+        return 0;
+        break;
     }
 }
 
