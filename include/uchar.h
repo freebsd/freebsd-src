80a2ef88b88406dfdb2ec89f7d66f7bbf1d019bb
ed <ed@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-05-25 16:58:12 +0000

    Fix <uchar.h> in for C++11.
    It turns out that in C++11, char16_t and char32_t are built-in types;
    language keywords. Just fix this by putting traditional _*_T_DECLARED
    blocks around the definitions. We'll just predefine these in
    <sys/_types.h>.
    
    This also opens up the possibility to define char16_t in other header
    files, if ever needed (e.g. if we would gain a <ctype.h> for
    char16_t/char32_t).
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@250985 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- include/uchar.h
+++ include/uchar.h
@@ -32,6 +32,16 @@
 #include <sys/cdefs.h>
 #include <sys/_types.h>
 
+#ifndef _CHAR16_T_DECLARED
+typedef	__char16_t	char16_t;
+#define	_CHAR16_T_DECLARED
+#endif
+
+#ifndef _CHAR32_T_DECLARED
+typedef	__char32_t	char32_t;
+#define	_CHAR32_T_DECLARED
+#endif
+
 #ifndef _MBSTATE_T_DECLARED
 typedef	__mbstate_t	mbstate_t;
 #define	_MBSTATE_T_DECLARED
@@ -41,9 +51,6 @@ typedef	__mbstate_t	mbstate_t;
 typedef	__size_t	size_t;
 #define	_SIZE_T_DECLARED
 #endif
-
-typedef	__char16_t	char16_t;
-typedef	__char32_t	char32_t;
 
 __BEGIN_DECLS
 size_t	c16rtomb(char * __restrict, char16_t, mbstate_t * __restrict);
