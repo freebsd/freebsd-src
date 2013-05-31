2e361cd9271c76e12bc17e5235b06505f483d444
adrian <adrian@FreeBSD.org>
2013-05-31 22:08:25 +0000

    Remove the direct fondling of the net80211 rate table; replace it with a method call.
    Since this is all legacy pre-11n rate fiddling, make sure we clear the
    basic rate bit before we do the lookup.
    
    Tested:
    
    * iwn(4)
    
    Not testeD:
    
    * RAL - I don't currently have any hardware for this. :(

--- sys/dev/iwn/if_iwn.c
+++ sys/dev/iwn/if_iwn.c
@@ -2136,7 +2136,8 @@ iwn_newassoc(struct ieee80211_node *ni, int isnew)
 		for (i = 0; i < ni->ni_rates.rs_nrates; i++) {
 			rate = RV(ni->ni_rates.rs_rates[i]);
 			plcp = rate2plcp(rate);
-			ridx = ic->ic_rt->rateCodeToIndex[rate];
+			ridx = ieee80211_legacy_rate_lookup(ic->ic_rt,
+			    rate & IEEE80211_RATE_VAL);
 			if (ridx < IWN_RIDX_OFDM6 &&
 			    IEEE80211_IS_CHAN_2GHZ(ni->ni_chan))
 				plcp |= IWN_RFLAG_CCK;
@@ -3400,7 +3401,8 @@ iwn_tx_data(struct iwn_softc *sc, struct mbuf *m, struct ieee80211_node *ni)
 		(void) ieee80211_ratectl_rate(ni, NULL, 0);
 		rate = ni->ni_txrate;
 	}
-	ridx = ic->ic_rt->rateCodeToIndex[rate];
+	ridx = ieee80211_legacy_rate_lookup(ic->ic_rt,
+	    rate & IEEE80211_RATE_VAL);
 
 	/* Encrypt the frame if need be. */
 	if (wh->i_fc[1] & IEEE80211_FC1_WEP) {
@@ -3637,7 +3639,8 @@ iwn_tx_data_raw(struct iwn_softc *sc, struct mbuf *m,
 
 	/* Choose a TX rate index. */
 	rate = params->ibp_rate0;
-	ridx = ic->ic_rt->rateCodeToIndex[rate];
+	ridx = ieee80211_legacy_rate_lookup(ic->ic_rt,
+	    rate & IEEE80211_RATE_VAL);
 	if (ridx == (uint8_t)-1) {
 		/* XXX fall back to mcast/mgmt rate? */
 		m_freem(m);
