163379d62d47e4f123dc7ec4a8272fedd248d52d
glebius <glebius@FreeBSD.org>
2013-06-08 13:13:40 +0000

    Make sys_mlock() function just a wrapper around vm_mlock() function that does all the job.
    Reviewed by:	kib, jilles
    Sponsored by:	Nginx, Inc.

--- sys/vm/vm_extern.h
+++ sys/vm/vm_extern.h
@@ -90,5 +90,6 @@ struct sf_buf *vm_imgact_map_page(vm_object_t object, vm_ooffset_t offset);
 void vm_imgact_unmap_page(struct sf_buf *sf);
 void vm_thread_dispose(struct thread *td);
 int vm_thread_new(struct thread *td, int pages);
+int vm_mlock(struct proc *, struct ucred *, const void *, size_t);
 #endif				/* _KERNEL */
 #endif				/* !_VM_EXTERN_H_ */
