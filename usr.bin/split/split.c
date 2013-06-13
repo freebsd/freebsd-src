859b20d925ed3a55ad967bb01e2081a46ce14e7b
Eitan Adler <eadler@FreeBSD.org>
2013-06-13 21:06:17 +0000

    MFC r227183,r229905,r250432,r250451,r250882:
    	- Add missing static keywords to split(1)
    
    	- Fix warning when compiling with gcc46:
    	 error: variable 'defname' set but not use
    
    	- Implement 'split -d' which allows a numeric suffix instead of an
    	alphabetic one.
    
    	- Bump .Dd for recent content change.
    	Avoid signed overflow in error handling code.
    
    PR:		bin/116209

--- usr.bin/split/split.c
+++ usr.bin/split/split.c
@@ -51,6 +51,7 @@ static const char sccsid[] = "@(#)split.c	8.2 (Berkeley) 4/16/94";
 #include <inttypes.h>
 #include <limits.h>
 #include <locale.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -61,16 +62,17 @@ static const char sccsid[] = "@(#)split.c	8.2 (Berkeley) 4/16/94";
 
 #define DEFLINE	1000			/* Default num lines per file. */
 
-off_t	 bytecnt;			/* Byte count to split on. */
-off_t	 chunks = 0;			/* Chunks count to split into. */
-long	 numlines;			/* Line count to split on. */
-int	 file_open;			/* If a file open. */
-int	 ifd = -1, ofd = -1;		/* Input/output file descriptors. */
-char	 bfr[MAXBSIZE];			/* I/O buffer. */
-char	 fname[MAXPATHLEN];		/* File name prefix. */
-regex_t	 rgx;
-int	 pflag;
-long	 sufflen = 2;			/* File name suffix length. */
+static off_t	 bytecnt;		/* Byte count to split on. */
+static off_t	 chunks = 0;		/* Chunks count to split into. */
+static long	 numlines;		/* Line count to split on. */
+static int	 file_open;		/* If a file open. */
+static int	 ifd = -1, ofd = -1;	/* Input/output file descriptors. */
+static char	 bfr[MAXBSIZE];		/* I/O buffer. */
+static char	 fname[MAXPATHLEN];	/* File name prefix. */
+static regex_t	 rgx;
+static int	 pflag;
+static bool	 dflag;
+static long	 sufflen = 2;		/* File name suffix length. */
 
 static void newfile(void);
 static void split1(void);
@@ -88,7 +90,8 @@ main(int argc, char **argv)
 
 	setlocale(LC_ALL, "");
 
-	while ((ch = getopt(argc, argv, "0123456789a:b:l:n:p:")) != -1)
+	dflag = false;
+	while ((ch = getopt(argc, argv, "0123456789a:b:dl:n:p:")) != -1)
 		switch (ch) {
 		case '0': case '1': case '2': case '3': case '4':
 		case '5': case '6': case '7': case '8': case '9':
@@ -130,6 +133,9 @@ main(int argc, char **argv)
 			if (bytecnti > OFF_MAX / scale)
 				errx(EX_USAGE, "%s: offset too large", optarg);
 			bytecnt = (off_t)(bytecnti * scale);
+			break;
+		case 'd':		/* Decimal suffix */
+			dflag = true;
 			break;
 		case 'l':		/* Line count. */
 			if (numlines != 0)
@@ -348,6 +354,8 @@ newfile(void)
 	long i, maxfiles, tfnum;
 	static long fnum;
 	static char *fpnt;
+	char beg, end;
+	int pattlen;
 
 	if (ofd == -1) {
 		if (fname[0] == '\0') {
@@ -359,10 +367,22 @@ newfile(void)
 		ofd = fileno(stdout);
 	}
 
-	/* maxfiles = 26^sufflen, but don't use libm. */
+	if (dflag) {
+		beg = '0';
+		end = '9';
+	}
+	else {
+		beg = 'a';
+		end = 'z';
+	}
+	pattlen = end - beg + 1;
+
+	/* maxfiles = pattlen^sufflen, but don't use libm. */
 	for (maxfiles = 1, i = 0; i < sufflen; i++)
-		if ((maxfiles *= 26) <= 0)
+		if (LONG_MAX / pattlen < maxfiles)
 			errx(EX_USAGE, "suffix is too long (max %ld)", i);
+		else
+			maxfiles *= pattlen;
 
 	if (fnum == maxfiles)
 		errx(EX_DATAERR, "too many files");
@@ -371,8 +391,8 @@ newfile(void)
 	tfnum = fnum;
 	i = sufflen - 1;
 	do {
-		fpnt[i] = tfnum % 26 + 'a';
-		tfnum /= 26;
+		fpnt[i] = tfnum % pattlen + beg;
+		tfnum /= pattlen;
 	} while (i-- > 0);
 	fpnt[sufflen] = '\0';
 
