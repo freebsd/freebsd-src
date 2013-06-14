b0151405942c8e86c2b08eb58626830bc5b0f0ce
adrian <adrian@FreeBSD.org>
2013-06-14 06:15:23 +0000

    Add bluetooth setup method functions to the HAL.

--- sys/contrib/dev/ath/ath_hal/ar9300/ar9300_freebsd.c
+++ sys/contrib/dev/ath/ath_hal/ar9300/ar9300_freebsd.c
@@ -237,6 +237,14 @@ ar9300_attach_freebsd_ops(struct ath_hal *ah)
 	/* ah_set11nRxClear */
 
 	/* bluetooth coexistence functions */
+	ah->ah_btCoexSetInfo		= ar9300_set_bt_coex_info;
+	ah->ah_btCoexSetConfig		= ar9300_bt_coex_config;
+	ah->ah_btCoexSetQcuThresh	= ar9300_bt_coex_set_qcu_thresh;
+	ah->ah_btCoexSetWeights		= ar9300_bt_coex_set_weights;
+	ah->ah_btCoexSetBmissThresh	= ar9300_bt_coex_setup_bmiss_thresh;
+	ah->ah_btCoexSetParameter	= ar9300_bt_coex_set_parameter;
+	ah->ah_btCoexDisable		= ar9300_bt_coex_disable;
+	ah->ah_btCoexEnable		= ar9300_bt_coex_enable;
 
 	/* LNA diversity functions */
 	ah->ah_divLnaConfGet = ar9300_ant_div_comb_get_config;
