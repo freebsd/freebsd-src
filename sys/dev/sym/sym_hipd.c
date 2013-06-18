cb0c43759bec82242355c9f1e56d1b6de22f0d69
Marius Strobl <marius@FreeBSD.org>
2013-06-18 15:14:20 +0000

    MFC: r251403 (partial)
    Flag sym(4) as supporting unmapped I/O; all necessary conversion actually
    already has been done as part of r246713 (MFC'ed to stable/9 in r251874).

--- sys/dev/sym/sym_hipd.c
+++ sys/dev/sym/sym_hipd.c
@@ -7869,7 +7869,6 @@ sym_setup_data_and_start(hcb_p np, struct ccb_scsiio *csio, ccb_p cp)
 		xpt_freeze_simq(np->sim, 1);
 		csio->ccb_h.status |= CAM_RELEASE_SIMQ;
 	}
-	return;
 }
 
 /*
@@ -8055,7 +8054,7 @@ static void sym_action2(struct cam_sim *sim, union ccb *ccb)
 		if ((np->features & FE_WIDE) != 0)
 			cpi->hba_inquiry |= PI_WIDE_16;
 		cpi->target_sprt = 0;
-		cpi->hba_misc = 0;
+		cpi->hba_misc = PIM_UNMAPPED;
 		if (np->usrflags & SYM_SCAN_TARGETS_HILO)
 			cpi->hba_misc |= PIM_SCANHILO;
 		if (np->usrflags & SYM_AVOID_BUS_RESET)
