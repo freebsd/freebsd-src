8b711895c2d1b33d8d9e86daffa6638baf321cf9
ae <ae@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-06-20 09:55:53 +0000

    Use IPSECSTAT_INC() and IPSEC6STAT_INC() macros for ipsec statistics accounting.
    MFC after:	2 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@252026 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet6/raw_ip6.c
+++ sys/netinet6/raw_ip6.c
@@ -263,7 +263,7 @@ rip6_input(struct mbuf **mp, int *offp, int proto)
 			 */
 			if (n && ipsec6_in_reject(n, last)) {
 				m_freem(n);
-				V_ipsec6stat.in_polvio++;
+				IPSEC6STAT_INC(in_polvio);
 				/* Do not inject data into pcb. */
 			} else
 #endif /* IPSEC */
@@ -295,7 +295,7 @@ rip6_input(struct mbuf **mp, int *offp, int proto)
 	 */
 	if ((last != NULL) && ipsec6_in_reject(m, last)) {
 		m_freem(m);
-		V_ipsec6stat.in_polvio++;
+		IPSEC6STAT_INC(in_polvio);
 		IP6STAT_DEC(ip6s_delivered);
 		/* Do not inject data into pcb. */
 		INP_RUNLOCK(last);
