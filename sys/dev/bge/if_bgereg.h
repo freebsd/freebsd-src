e5c3cbcb70be9989ddee8a196f23a38b1176577c
Pyun YongHyeon <yongari@FreeBSD.org>
2013-07-01 07:02:44 +0000

    MFC r251482,251733: r251482:   Correct setting TX random backoff register. This register is   implemented as a 10 bits linear feedback shift register so only   lower 10 bits are valid.   Because this register is used to initialize random backoff interval   register only when resolved duplex is half-duplex, it wouldn't have   caused issues in these days.
    r251733:
      Fix a typo introduced in r213280. IFM_OPTIONS macro should see
      current media word.

--- sys/dev/bge/if_bgereg.h
+++ sys/dev/bge/if_bgereg.h
@@ -797,7 +797,7 @@
 #define	BGE_LEDCTL_BLINKPERIOD_OVERRIDE	0x80000000
 
 /* TX backoff seed register */
-#define	BGE_TX_BACKOFF_SEED_MASK	0x3F
+#define	BGE_TX_BACKOFF_SEED_MASK	0x3FF
 
 /* Autopoll status register */
 #define	BGE_AUTOPOLLSTS_ERROR		0x00000001
