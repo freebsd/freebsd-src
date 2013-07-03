c60f2a4061a5629b080ff2981086c722f316b597
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-07-03 18:27:45 +0000

    As a followup to r252547, propate const down the call stack.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@252583 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libiconv_modules/DECHanyu/citrus_dechanyu.c
+++ lib/libiconv_modules/DECHanyu/citrus_dechanyu.c
@@ -163,10 +163,10 @@ is_94charset(int c)
 static int
 /*ARGSUSED*/
 _citrus_DECHanyu_mbrtowc_priv(_DECHanyuEncodingInfo * __restrict ei,
-    wchar_t * __restrict pwc, char ** __restrict s, size_t n,
+    wchar_t * __restrict pwc, const char ** __restrict s, size_t n,
     _DECHanyuState * __restrict psenc, size_t * __restrict nresult)
 {
-	char *s0;
+	const char *s0;
 	wchar_t wc;
 	int ch;
 
