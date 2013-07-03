a02844e93b39b1329fb99d013834aa360f9f22ca
peter <peter@FreeBSD.org>
2013-07-03 07:03:19 +0000

    Replace the #define for "iconv" so it is for the function name instead of a macro with parameters.  Remove a __DECONST hack and add consts instead for gnu libiconv API compatability.  This makes it work with things like devel/boost-libs that expects to use "iconv" as though it were a pointer.

--- lib/libc/iconv/citrus_iconv_local.h
+++ lib/libc/iconv/citrus_iconv_local.h
@@ -45,7 +45,7 @@ static void	 _citrus_##_m_##_iconv_uninit_shared			\
 		    (struct _citrus_iconv_shared *);			\
 static int	 _citrus_##_m_##_iconv_convert				\
 		    (struct _citrus_iconv * __restrict,			\
-		    char * __restrict * __restrict,			\
+		    const char * __restrict * __restrict,		\
 		    size_t * __restrict,				\
 		    char * __restrict * __restrict,			\
 		    size_t * __restrict outbytes,			\
@@ -74,7 +74,7 @@ typedef void (*_citrus_iconv_uninit_shared_t)
     (struct _citrus_iconv_shared *);
 typedef int (*_citrus_iconv_convert_t)
     (struct _citrus_iconv * __restrict,
-    char *__restrict* __restrict, size_t * __restrict,
+    const char *__restrict* __restrict, size_t * __restrict,
     char * __restrict * __restrict, size_t * __restrict, uint32_t,
     size_t * __restrict);
 typedef int (*_citrus_iconv_init_context_t)(struct _citrus_iconv *);
