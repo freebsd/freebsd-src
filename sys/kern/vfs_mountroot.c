240a76b56684f3a87e1e3a3c2af9f947a0202cd2
avg <avg@FreeBSD.org>
2013-07-06 08:51:56 +0000

    MFC r248645: post mountroot event after a real/final root is mounted

--- sys/kern/vfs_mountroot.c
+++ sys/kern/vfs_mountroot.c
@@ -199,8 +199,6 @@ set_rootvnode(void)
 	VREF(rootvnode);
 
 	FILEDESC_XUNLOCK(p->p_fd);
-
-	EVENTHANDLER_INVOKE(mountroot);
 }
 
 static int
@@ -994,6 +992,8 @@ vfs_mountroot(void)
 	atomic_store_rel_int(&root_mount_complete, 1);
 	wakeup(&root_mount_complete);
 	mtx_unlock(&mountlist_mtx);
+
+	EVENTHANDLER_INVOKE(mountroot);
 }
 
 static struct mntarg *
