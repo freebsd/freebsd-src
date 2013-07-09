e20c3d7ebbf02da4feacb8eabcf0ea2dc2388844
tuexen <tuexen@FreeBSD.org>
2013-07-09 19:04:19 +0000

    Fix a bug where SCTP_ENABLE_STREAM_RESET is not handled by sctp_opt_info().
    MFC after: 3 days

--- lib/libc/net/sctp_sys_calls.c
+++ lib/libc/net/sctp_sys_calls.c
@@ -338,6 +338,9 @@ sctp_opt_info(int sd, sctp_assoc_t id, int opt, void *arg, socklen_t * size)
 	case SCTP_MAX_BURST:
 		((struct sctp_assoc_value *)arg)->assoc_id = id;
 		break;
+	case SCTP_ENABLE_STREAM_RESET:
+		((struct sctp_assoc_value *)arg)->assoc_id = id;
+		break;
 	default:
 		break;
 	}
