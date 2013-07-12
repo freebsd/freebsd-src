a3e24ccf54c1d090cbf4a96962bda6e482647761
Jim Harris <jimharris@FreeBSD.org>
2013-07-12 22:07:33 +0000

    MFC r253112:
      Update copyright dates.
    
    Approved by:	re (kib)

--- sys/dev/nvme/nvme_private.h
+++ sys/dev/nvme/nvme_private.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (C) 2012 Intel Corporation
+ * Copyright (C) 2012-2013 Intel Corporation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
