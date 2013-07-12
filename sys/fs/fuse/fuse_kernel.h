991954f1c3b8fff0471d939cc1fa163209961986
pfg <pfg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-07-12 17:22:59 +0000

    Add creation timestamp (birthtime) support for fuse.
    This is based on similar support in MacFUSE.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@253276 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/fs/fuse/fuse_kernel.h
+++ sys/fs/fuse/fuse_kernel.h
@@ -69,9 +69,15 @@ struct fuse_attr {
 	__u64	atime;
 	__u64	mtime;
 	__u64	ctime;
+#ifdef __FreeBSD__
+	__u64	crtime;
+#endif
 	__u32	atimensec;
 	__u32	mtimensec;
 	__u32	ctimensec;
+#ifdef __FreeBSD__
+	__u32	crtimensec;
+#endif
 	__u32	mode;
 	__u32	nlink;
 	__u32	uid;
