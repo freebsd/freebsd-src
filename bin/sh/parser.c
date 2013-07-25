0bdd387178582c53f6fd5e053636427d08bbea68
Jilles Tjoelker <jilles@FreeBSD.org>
2013-07-25 19:48:15 +0000

    sh: Remove #define MKINIT.
    MKINIT only served for the removed mkinit. Many variables can be static now.

--- bin/sh/parser.c
+++ bin/sh/parser.c
@@ -96,9 +96,9 @@ static struct heredoc *heredoclist;	/* list of here documents to read */
 static int doprompt;		/* if set, prompt the user */
 static int needprompt;		/* true if interactive and at start of line */
 static int lasttoken;		/* last token read */
-MKINIT int tokpushback;		/* last token pushed back */
+int tokpushback;		/* last token pushed back */
 static char *wordtext;		/* text of last word returned by readtoken */
-MKINIT int checkkwd;            /* 1 == check for kwds, 2 == also eat newlines */
+static int checkkwd;            /* 1 == check for kwds, 2 == also eat newlines */
 static struct nodelist *backquotelist;
 static union node *redirnode;
 static struct heredoc *heredoc;
