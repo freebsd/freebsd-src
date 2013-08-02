65beec0fd9198d96ea0bc93fd76715ebda84c0a3
sjg <sjg@FreeBSD.org>
2013-08-02 05:25:11 +0000

    Tag bmake-20130730

---
+++ 20130730/mk/rst2htm.mk
@@ -0,0 +1,43 @@
+# $Id: rst2htm.mk,v 1.8 2011/04/03 21:39:25 sjg Exp $
+#
+#	@(#) Copyright (c) 2009, Simon J. Gerraty
+#
+#	This file is provided in the hope that it will
+#	be of use.  There is absolutely NO WARRANTY.
+#	Permission to copy, redistribute or otherwise
+#	use this file is hereby granted provided that 
+#	the above copyright notice and this notice are
+#	left intact. 
+#      
+#	Please send copies of changes and bug-fixes to:
+#	sjg@crufty.net
+#
+
+# convert reStructuredText to HTML, using rst2html.py from
+# docutils - http://docutils.sourceforge.net/
+
+.if empty(TXTSRCS)
+TXTSRCS != 'ls' -1t ${.CURDIR}/*.txt ${.CURDIR}/*.rst 2>/dev/null; echo
+.endif
+RSTSRCS ?= ${TXTSRCS}
+HTMFILES ?= ${RSTSRCS:R:T:O:u:%=%.htm}
+RST2HTML ?= rst2html.py
+RST2S5 ?= rst2s5.py
+# the following will run RST2S5 if the target name contains the word 'slides'
+# otherwise it uses RST2HTML
+RST2HTM = ${"${.TARGET:T:M*slides*}":?${RST2S5} ${RST2S5_FLAGS}:${RST2HTML} ${RST2HTML_FLAGS}}
+
+RST_SUFFIXES ?= .rst .txt
+
+CLEANFILES += ${HTMFILES}
+
+html:	${HTMFILES}
+
+.SUFFIXES: ${RST_SUFFIXES} .htm
+
+${RST_SUFFIXES:@s@$s.htm@}:
+	${RST2HTM} ${.IMPSRC} ${.TARGET}
+
+.for s in ${RSTSRCS:O:u}
+${s:R:T}.htm: $s
+.endfor
