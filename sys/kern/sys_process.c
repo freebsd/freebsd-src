026067949f83b27e2308953e9227be5adbeff8b0
attilio <attilio@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-08-05 08:55:35 +0000

    Revert r253939: We cannot busy a page before doing pagefaults. Infact, it can deadlock against vnode lock, as it tries to vget(). Other functions, right now, have an opposite lock ordering, like vm_object_sync(), which acquires the vnode lock first and then sleeps on the busy mechanism.
    Before this patch is reinserted we need to break this ordering.
    
    Sponsored by:	EMC / Isilon storage division
    Reported by:	kib
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@253953 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/sys_process.c
+++ sys/kern/sys_process.c
@@ -263,7 +263,6 @@ proc_rwmem(struct proc *p, struct uio *uio)
 	writing = uio->uio_rw == UIO_WRITE;
 	reqprot = writing ? VM_PROT_COPY | VM_PROT_READ : VM_PROT_READ;
 	fault_flags = writing ? VM_FAULT_DIRTY : VM_FAULT_NORMAL;
-	fault_flags |= VM_FAULT_IOBUSY;
 
 	/*
 	 * Only map in one page at a time.  We don't have to, but it
@@ -288,9 +287,9 @@ proc_rwmem(struct proc *p, struct uio *uio)
 		len = min(PAGE_SIZE - page_offset, uio->uio_resid);
 
 		/*
-		 * Fault and busy the page on behalf of the process.
+		 * Fault and hold the page on behalf of the process.
 		 */
-		error = vm_fault_handle(map, pageno, reqprot, fault_flags, &m);
+		error = vm_fault_hold(map, pageno, reqprot, fault_flags, &m);
 		if (error != KERN_SUCCESS) {
 			if (error == KERN_RESOURCE_SHORTAGE)
 				error = ENOMEM;
@@ -316,9 +315,9 @@ proc_rwmem(struct proc *p, struct uio *uio)
 		/*
 		 * Release the page.
 		 */
-		VM_OBJECT_WLOCK(m->object);
-		vm_page_io_finish(m);
-		VM_OBJECT_WUNLOCK(m->object);
+		vm_page_lock(m);
+		vm_page_unhold(m);
+		vm_page_unlock(m);
 
 	} while (error == 0 && uio->uio_resid > 0);
 
