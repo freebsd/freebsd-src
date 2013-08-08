a2d054318c6fd38a40ca8642db4cc8226050336e
hrs <hrs@FreeBSD.org>
2013-08-08 20:30:22 +0000

    MFC 253887,253977:
    - Use pget(PGET_CANDEBUG | PGET_NOTWEXIT) to determine if the specified
      PID is valid for monitoring in FILEMON_SET_PID ioctl.
    
    - Set the monitored PID to -1 when the process exits.
    
    Suggested by:	jilles
    Tested by:	sjg
    Approved by:	re (kib)

--- sys/dev/filemon/filemon.c
+++ sys/dev/filemon/filemon.c
@@ -180,6 +180,7 @@ filemon_ioctl(struct cdev *dev, u_long cmd, caddr_t data, int flag __unused,
 {
 	int error = 0;
 	struct filemon *filemon;
+	struct proc *p;
 
 #if __FreeBSD_version < 701000
 	filemon = dev->si_drv1;
@@ -202,7 +203,12 @@ filemon_ioctl(struct cdev *dev, u_long cmd, caddr_t data, int flag __unused,
 
 	/* Set the monitored process ID. */
 	case FILEMON_SET_PID:
-		filemon->pid = *((pid_t *) data);
+		error = pget(*((pid_t *)data), PGET_CANDEBUG | PGET_NOTWEXIT,
+		    &p);
+		if (error == 0) {
+			filemon->pid = p->p_pid;
+			PROC_UNLOCK(p);
+		}
 		break;
 
 	default:
