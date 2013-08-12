ddacef92d2dd66e77fe3f35ae549b839cea4ccb3
adrian <adrian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-08-12 02:21:44 +0000

    When flushing packets from the powersave queue, make sure that m_nextpkt is NULL before passing it up to the parent transmit method.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@254236 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net80211/ieee80211_power.c
+++ sys/net80211/ieee80211_power.c
@@ -456,6 +456,7 @@ pwrsave_flushq(struct ieee80211_node *ni)
 		while (parent_q != NULL) {
 			m = parent_q;
 			parent_q = m->m_nextpkt;
+			m->m_nextpkt = NULL;
 			/* must be encapsulated */
 			KASSERT((m->m_flags & M_ENCAP),
 			    ("%s: parentq with non-M_ENCAP frame!\n",
