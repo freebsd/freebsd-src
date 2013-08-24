e205f860d9efef4201acaf2781d80b638c3cce34
andre <andre@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-08-24 19:58:36 +0000

    Add mtodo(m, o) macro taking an additional offset into the mbuf data section.
    Sponsored by:	The FreeBSD Foundation
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@254805 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/mbuf.h
+++ sys/sys/mbuf.h
@@ -67,8 +67,10 @@
  * type:
  *
  * mtod(m, t)	-- Convert mbuf pointer to data pointer of correct type.
+ * mtodo(m, o) -- Same as above but with offset 'o' into data.
  */
 #define	mtod(m, t)	((t)((m)->m_data))
+#define	mtodo(m, o)	((void *)(((m)->m_data) + (o)))
 
 /*
  * Argument structure passed to UMA routines during mbuf and packet
