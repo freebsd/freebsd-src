a437be72574480c1aedb04a776cc369e323fba4a
jhb <jhb@FreeBSD.org>
2013-08-26 18:16:05 +0000

    Remove most of the remaining sysctl name list macros.  They were only ever intended for use in sysctl(8) and it has not used them for many years.
    Reviewed by:	bde
    Tested by:	exp-run by bdrewery

--- sys/netinet/tcp_var.h
+++ sys/netinet/tcp_var.h
@@ -593,24 +593,6 @@ struct	xtcpcb {
 #define	TCPCTL_MAXID		16
 #define TCPCTL_FINWAIT2_TIMEOUT        17
 
-#define TCPCTL_NAMES { \
-	{ 0, 0 }, \
-	{ "rfc1323", CTLTYPE_INT }, \
-	{ "mssdflt", CTLTYPE_INT }, \
-	{ "stats", CTLTYPE_STRUCT }, \
-	{ "rttdflt", CTLTYPE_INT }, \
-	{ "keepidle", CTLTYPE_INT }, \
-	{ "keepintvl", CTLTYPE_INT }, \
-	{ "sendspace", CTLTYPE_INT }, \
-	{ "recvspace", CTLTYPE_INT }, \
-	{ "keepinit", CTLTYPE_INT }, \
-	{ "pcblist", CTLTYPE_STRUCT }, \
-	{ "delacktime", CTLTYPE_INT }, \
-	{ "v6mssdflt", CTLTYPE_INT }, \
-	{ "maxid", CTLTYPE_INT }, \
-}
-
-
 #ifdef _KERNEL
 #ifdef SYSCTL_DECL
 SYSCTL_DECL(_net_inet_tcp);
