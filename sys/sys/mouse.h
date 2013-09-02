b4cbe1bfc4320320e6983f434cac3fc05a15a4a2
dumbbell <dumbbell@FreeBSD.org>
2013-09-02 19:49:18 +0000

    sys/mouse.h: Move members introduced in r255153 to end of struct synapticshw
    I didn't know this structure was public and didn't pay enough attention...

--- sys/sys/mouse.h
+++ sys/sys/mouse.h
@@ -101,15 +101,15 @@ typedef struct synapticshw {
 	int capPen;
 	int infoSimplC;
 	int infoGeometry;
-	int nExtendedButtons;
 	int capExtended;
-	int nExtendedQueries;
-	int capMiddle;
-	int capPassthrough;
 	int capSleep;
 	int capFourButtons;
 	int capMultiFinger;
 	int capPalmDetect;
+	int capPassthrough;
+	int capMiddle;
+	int nExtendedButtons;
+	int nExtendedQueries;
 } synapticshw_t;
 
 /* iftype */
