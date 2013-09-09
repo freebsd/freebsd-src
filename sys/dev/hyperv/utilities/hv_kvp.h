2ee2dc6fd672c2d2de53a080a8ddf87fe89a651c
Peter Grehan <grehan@FreeBSD.org>
2013-09-09 19:27:44 +0000

    Revert the kvp code - there's still some work that needs to be done for that.
    Discussed with:	Microsoft hyper-v devs

--- sys/dev/hyperv/utilities/hv_kvp.h
+++ sys/dev/hyperv/utilities/hv_kvp.h
@@ -250,17 +250,17 @@ struct hv_kvp_ip_msg {
 
 #define BSD_SOC_PATH "/etc/hyperv/socket" 
 
-#define HV_SHUT_DOWN		 0
-#define HV_TIME_SYNCH	 1
-#define HV_HEART_BEAT	 2
-#define HV_KVP		 3
-#define HV_MAX_UTIL_SERVICES 4
+#define HV_SHUT_DOWN		0
+#define HV_TIME_SYNCH	 	1
+#define HV_HEART_BEAT	 	2
+#define HV_KVP		 	3
+#define HV_MAX_UTIL_SERVICES 	4
 
 #define HV_WLTIMEDELTA			116444736000000000L /* in 100ns unit */
 #define HV_ICTIMESYNCFLAG_PROBE		0
 #define HV_ICTIMESYNCFLAG_SYNC		1
 #define HV_ICTIMESYNCFLAG_SAMPLE	2
-#define HV_NANO_SEC_PER_SEC 1000000000
+#define HV_NANO_SEC_PER_SEC 		1000000000
 
 typedef struct hv_vmbus_service {
 	hv_guid		guid;		/* Hyper-V GUID */
@@ -282,6 +282,4 @@ typedef struct hv_vmbus_service {
 extern uint8_t* receive_buffer[];
 extern hv_vmbus_service service_table[];
 
-void hv_kvp_callback(void *context);
-int hv_kvp_init(hv_vmbus_service *serv);
 #endif /* _KVP_H */
