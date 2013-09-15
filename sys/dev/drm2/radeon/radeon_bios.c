c8b8d6b96e237ea985da8ce34f2dac00243a9413
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2013-09-15 07:48:42 +0000

    drm/radeon: Add missing "return false" after unmapping invalid BIOS
    Without that, we would try to copy the unmapped BIOS.
    
    Submitted by:	Christoph Mallon <christoph.mallon@gmx.de>
    Approved by:	re (blanket)

--- sys/dev/drm2/radeon/radeon_bios.c
+++ sys/dev/drm2/radeon/radeon_bios.c
@@ -123,6 +123,7 @@ static bool radeon_read_bios(struct radeon_device *rdev)
 			    __func__, bios[0], bios[1]);
 		}
 		vga_pci_unmap_bios(vga_dev, bios);
+		return false;
 	}
 	rdev->bios = malloc(size, DRM_MEM_DRIVER, M_WAITOK);
 	memcpy(rdev->bios, bios, size);
