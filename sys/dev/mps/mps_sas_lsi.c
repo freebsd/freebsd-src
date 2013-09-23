165992728d93125bb5319a18ce0cd2975c1d4c15
Kenneth D. Merry <ken@FreeBSD.org>
2013-09-23 21:52:07 +0000

    MFC r255501
    This is slightly modified from the FreeBSD/head version, to include
    version checks for the scanning changes for not only FreeBSD/head
    (1000039 and higher) but also stable/9 (902502 and higher).
    
      ------------------------------------------------------------------------
      r255501 | ken | 2013-09-12 16:06:12 -0600 (Thu, 12 Sep 2013) | 18 lines
    
      Fix an issue that caused Integrated RAID volumes on LSI mps(4) controllers
      to not get scanned on boot.
    
      The problem originated in change 253549.  With the change to the mps(4)
      driver to scan only targets that it knows it has (as opposed to scanning
      the entire bus), scanning RAID volumes on boot was omitted.
    
      So, for versions of FreeBSD that have the scanning changes
      (__FreeBSD_version 1000039 and higher), scan RAID volumes that are added
      whether or not we're booting.
    
      PR:             kern/181784
      Reported by:    Xiguang Wang <kurapica@gmail.com>
      Tested by:      Dennis Glatting <dg@pki2.com>
      Sponsored by:   Spectra Logic
    
    PR:		kern/181784

--- sys/dev/mps/mps_sas_lsi.c
+++ sys/dev/mps/mps_sas_lsi.c
@@ -892,7 +892,10 @@ mpssas_volume_add(struct mps_softc *sc, u16 handle)
 		free(lun, M_MPT2);
 	}
 	SLIST_INIT(&targ->luns);
+#if ((__FreeBSD_version >= 1000000) && (__FreeBSD_version < 1000039)) || \
+    (__FreeBSD_version < 902502)
 	if ((sassc->flags & MPSSAS_IN_STARTUP) == 0)
+#endif
 		mpssas_rescan_target(sc, targ);
 	mps_dprint(sc, MPS_MAPPING, "RAID target id %d added (WWID = 0x%jx)\n",
 	    targ->tid, wwid);
