56f0ad0dcc28368b3feaf18e949f61bb94dbb6b0
Dag-Erling Smørgrav <des@FreeBSD.org>
2013-10-07 11:23:01 +0000

    When displaying a struct stat, if the -r option was not specified, display the numeric rather than symbolic representation of st_mode.
    Approved by:	re (glebius)
    MFC after:	1 week

--- usr.bin/kdump/kdump.c
+++ usr.bin/kdump/kdump.c
@@ -1655,10 +1655,15 @@ ktrstat(struct stat *statp)
 	 * buffer exactly sizeof(struct stat) bytes long.
 	 */
 	printf("struct stat {");
-	strmode(statp->st_mode, mode);
-	printf("dev=%ju, ino=%ju, mode=%s, nlink=%ju, ",
-		(uintmax_t)statp->st_dev, (uintmax_t)statp->st_ino, mode,
-		(uintmax_t)statp->st_nlink);
+	printf("dev=%ju, ino=%ju, ",
+		(uintmax_t)statp->st_dev, (uintmax_t)statp->st_ino);
+	if (resolv == 0)
+		printf("mode=0%jo, ", (uintmax_t)statp->st_mode);
+	else {
+		strmode(statp->st_mode, mode);
+		printf("mode=%s, ", mode);
+	}
+	printf("nlink=%ju, ", (uintmax_t)statp->st_nlink);
 	if (resolv == 0 || (pwd = getpwuid(statp->st_uid)) == NULL)
 		printf("uid=%ju, ", (uintmax_t)statp->st_uid);
 	else
