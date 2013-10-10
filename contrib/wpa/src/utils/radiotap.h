21b770fcad0308c582adb591099da1f522b91985
Dag-Erling Smørgrav <des@FreeBSD.org>
2013-10-10 15:39:02 +0000

    Revert spurious RCSID change, reducing diff against head.
    Approved by:	grehan

--- contrib/wpa/src/utils/radiotap.h
+++ contrib/wpa/src/utils/radiotap.h
@@ -1,4 +1,4 @@
-/* $FreeBSD: projects/bhyve_svm/contrib/wpa/src/utils/radiotap.h 214734 2010-11-03 10:43:38Z rpaulo $ */
+/* $FreeBSD: head/contrib/wpa/src/utils/radiotap.h 214734 2010-11-03 10:43:38Z rpaulo $ */
 /* $NetBSD: ieee80211_radiotap.h,v 1.11 2005/06/22 06:16:02 dyoung Exp $ */
 
 /*-
