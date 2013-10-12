96ce9379f8496012b132ba4dc277b5ade195bf20
sjg <sjg@FreeBSD.org>
2013-10-12 23:48:21 +0000

    Add more vars we subst into DIRDEPS eg. BOOT_MACHINE_DIR and KERNEL_NAME. Make it easier to add more.

--- share/mk/local.gendirdeps.mk
+++ share/mk/local.gendirdeps.mk
@@ -1,10 +1,26 @@
-# supress optional dependecies
+# supress optional/auto dependecies
 # local.dirdeps.mk will put them in if necessary
-GENDIRDEPS_FILTER+= Ngnu/lib/libssp/libssp_nonshared
+GENDIRDEPS_FILTER+= \
+	Ngnu/lib/libssp/libssp_nonshared \
+	Ncddl/usr.bin/ctf* \
+	Npkgs/pseudo/stage*
 
 # gendirdeps.mk will turn _{VAR} into ${VAR} which keeps this simple
-GENDIRDEPS_FILTER+= ${CSU_DIR:L:@v@S,/${$v},/_{${v}},@}
+# order of this list matters!
+GENDIRDEPS_FILTER_DIR_VARS+= \
+       CSU_DIR \
+       BOOT_MACHINE_DIR
 
-# this could easily get confused
-GENDIRDEPS_FILTER+= ${MACHINE_CPUARCH MACHINE_CPU MACHINE_ARCH MACHINE:L:@v@S,/${$v}/,/_{${v}}/,@:NS,//,*:u}
+# order of this list matters!
+GENDIRDEPS_FILTER_VARS+= \
+       KERNEL_NAME \
+       MACHINE_CPUARCH \
+       MACHINE_ARCH \
+       MACHINE
+
+GENDIRDEPS_FILTER+= ${GENDIRDEPS_FILTER_DIR_VARS:@v@S,${$v},_{${v}},@}
+GENDIRDEPS_FILTER+= ${GENDIRDEPS_FILTER_VARS:@v@S,/${$v}/,/_{${v}}/,@:NS,//,*:u}
+
+# handle the non-standard way that gnu/usr.bin/groff/tmac is staged
+GENDIRDEPS_FILTER+= C,.*usr/share/tmac.*stage,gnu/usr.bin/groff/tmac,
 
