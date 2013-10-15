7e6ee2dce1cd0189f134cf7a9e48bb6a725322b5
ae <ae@FreeBSD.org>
2013-10-15 11:25:12 +0000

    Add in6ifa_ifwithaddr() function. It is similar to ifa_ifwithaddr, but uses in6_addr hash for fast lookup.

--- sys/netinet6/in6_var.h
+++ sys/netinet6/in6_var.h
@@ -814,6 +814,7 @@ void	in6_setmaxmtu(void);
 int	in6_if2idlen(struct ifnet *);
 struct in6_ifaddr *in6ifa_ifpforlinklocal(struct ifnet *, int);
 struct in6_ifaddr *in6ifa_ifpwithaddr(struct ifnet *, struct in6_addr *);
+struct in6_ifaddr *in6ifa_ifwithaddr(const struct in6_addr *, uint32_t);
 struct in6_ifaddr *in6ifa_llaonifp(struct ifnet *);
 char	*ip6_sprintf(char *, const struct in6_addr *);
 int	in6_addr2zoneid(struct ifnet *, struct in6_addr *, u_int32_t *);
