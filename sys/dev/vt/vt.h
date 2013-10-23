f561b1bcf802e3b6aa963552bd5ba5f9d04e389b
ray <ray@FreeBSD.org>
2013-10-23 13:35:46 +0000

    Add new vt_buf flag VBF_HISTORY_FULL - whole history filled.
    Sponsored by:	The FreeBSD Foundation

--- sys/dev/vt/vt.h
+++ sys/dev/vt/vt.h
@@ -120,6 +120,7 @@ struct vt_buf {
 #define	VBF_STATIC	0x2	/* Buffer is statically allocated. */
 #define	VBF_MTX_INIT	0x4	/* Mutex initialized. */
 #define	VBF_SCROLL	0x8	/* scroll locked mode. */
+#define	VBF_HISTORY_FULL 0x10	/* All rows filled. */
 	int			 vb_history_size;
 #define	VBF_DEFAULT_HISTORY_SIZE	200
 	int			 vb_roffset;	/* (b) History rows offset. */
