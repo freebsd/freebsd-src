7ced9c2f66f705eaf6bb3e7d69db4517e329185f
Gleb Smirnoff <glebius@FreeBSD.org>
2013-10-28 07:45:03 +0000

    Instead of putting ifnet declaration into eventhandler.h, move bpf(4) and vlan(4) related event declarations to bpf.h and if_vlan_var.h. To avoid dependency on eventhandler.h, protect these declarations with ifdef SYS_EVENTHANDLER_H.
    Sponsored by:	Netflix
    Sponsored by:	Nginx, Inc.

--- sys/sys/eventhandler.h
+++ sys/sys/eventhandler.h
@@ -203,19 +203,6 @@ typedef void (*vfs_unmounted_notify_fn)(void *, struct mount *,
 EVENTHANDLER_DECLARE(vfs_mounted, vfs_mounted_notify_fn);
 EVENTHANDLER_DECLARE(vfs_unmounted, vfs_unmounted_notify_fn);
 
-/* VLAN state change events */
-struct ifnet;
-typedef void (*vlan_config_fn)(void *, struct ifnet *, uint16_t);
-typedef void (*vlan_unconfig_fn)(void *, struct ifnet *, uint16_t);
-EVENTHANDLER_DECLARE(vlan_config, vlan_config_fn);
-EVENTHANDLER_DECLARE(vlan_unconfig, vlan_unconfig_fn);
-
-/* BPF attach/detach events */
-struct ifnet;
-typedef void (*bpf_track_fn)(void *, struct ifnet *, int /* dlt */,
-    int /* 1 =>'s attach */);
-EVENTHANDLER_DECLARE(bpf_track, bpf_track_fn);
-
 /*
  * Process events
  * process_fork and exit handlers are called without Giant.
