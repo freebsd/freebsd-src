7fb88e21cd7eb227397cb0f1375dac3b94acbbf0
glebius <glebius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-10-28 21:08:04 +0000

    Include if_var.h.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@257275 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/xen/netfront/netfront.c
+++ sys/dev/xen/netfront/netfront.c
@@ -43,6 +43,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/sx.h>
 
 #include <net/if.h>
+#include <net/if_var.h>
 #include <net/if_arp.h>
 #include <net/ethernet.h>
 #include <net/if_dl.h>
@@ -51,7 +52,6 @@ __FBSDID("$FreeBSD$");
 #include <net/bpf.h>
 
 #include <net/if_types.h>
-#include <net/if.h>
 
 #include <netinet/in_systm.h>
 #include <netinet/in.h>
