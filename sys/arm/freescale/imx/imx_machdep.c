13a98c8536d1ad84230f0eefb98375efa19182d9
Ian Lepore <ian@FreeBSD.org>
2013-11-04 19:44:37 +0000

    Begin reducing code duplication in arm pmap.c and pmap-v6.c by factoring out common code related to mapping device memory into a new devmap.c file.
    Remove the growing duplication of code that used pmap_devmap_find_pa() and
    then did some math with the returned results to generate a virtual address,
    and likewise in reverse to get a physical address.  Now there are a pair
    of functions, arm_devmap_vtop() and arm_devmap_ptov(), to do that.  The
    bus_space_map() implementations are rewritten in terms of these.

--- sys/arm/freescale/imx/imx_machdep.c
+++ sys/arm/freescale/imx/imx_machdep.c
@@ -39,6 +39,7 @@ __FBSDID("$FreeBSD$");
 
 #include <machine/armreg.h>
 #include <machine/bus.h>
+#include <machine/devmap.h>
 #include <machine/machdep.h>
 
 #include <arm/freescale/imx/imx_machdep.h>
@@ -168,7 +169,6 @@ bus_dma_get_range_nb(void)
 void
 imx_wdog_cpu_reset(vm_offset_t wdcr_physaddr)
 {
-	const struct pmap_devmap *pd;
 	volatile uint16_t * pcr;
 
 	/*
@@ -178,10 +178,9 @@ imx_wdog_cpu_reset(vm_offset_t wdcr_physaddr)
 	 * reset) bit being set in the watchdog status register after the reset.
 	 * This is how software can distinguish a reset from a wdog timeout.
 	 */
-	if ((pd = pmap_devmap_find_pa(wdcr_physaddr, 2)) == NULL) {
+	if ((pcr = arm_devmap_ptov(wdcr_physaddr, sizeof(*pcr))) == NULL) {
 		printf("cpu_reset() can't find its control register... locking up now.");
 	} else {
-		pcr = (uint16_t *)(pd->pd_va + (wdcr_physaddr - pd->pd_pa));
 		*pcr = WDOG_CR_WDE;
 	}
 	for (;;)
