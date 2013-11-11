7ee9c7bb018a215c159762ac76b1ceb208a08387
pfg <pfg@FreeBSD.org>
2013-11-11 21:18:02 +0000

    cxxfilt: small changes from Apple's developer tools
    From Apple's Developer Tools 4.0 [1]:
    
    demangle.patch
    2007-05-05  Geoffrey Keating
    	(d_name): Detect local-source-name.
    	(d_prefix): Likewise.
    	(d_unqualified_name): Implement local-source-name.
    
    libiberty-printf.patch
    
    MFC after:	1 month
    
    [1] http://opensource.apple.com/source/cxxfilt/cxxfilt-9/patches/

--- contrib/binutils/binutils/cxxfilt.c
+++ contrib/binutils/binutils/cxxfilt.c
@@ -63,12 +63,12 @@ demangle_it (char *mangled_name)
   result = cplus_demangle (mangled_name + skip_first, flags);
 
   if (result == NULL)
-    printf (mangled_name);
+    printf ("%s",mangled_name);
   else
     {
       if (mangled_name[0] == '.')
 	putchar ('.');
-      printf (result);
+      printf ("%s",result);
       free (result);
     }
 }
