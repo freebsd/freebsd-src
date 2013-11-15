bd6deadf7b4703dd4d5e0eeccf23134a769b3f97
gibbs <gibbs@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2013-11-15 16:05:55 +0000

    Fix accounting for hw.realmem on the i386 and amd64 platforms.
    sys/i386/i386/machdep.c:
    sys/amd64/amd64/machdep.c:
    	The value reported by FreeBSD as "real memory" when booting
    	doesn't match what is later reported by sysctl as hw.realmem.
    	This is due to the fact that the value printed during the
    	boot process is fetched from smbios data (when possible),
    	and accounts for holes in physical memory. On the other
    	hand, the value of hw.realmem is unconditionally set to be
    	one larger than the highest page of the physical address
    	space.
    
    	Fix this by setting hw.realmem to the same value printed
    	during boot, this makes hw.realmem honour it's name and
    	account properly for physical memory present in the system.
    
    Submitted by:	Roger Pau Monné
    Reviewed by:	gibbs
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@258176 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/i386/machdep.c
+++ sys/i386/i386/machdep.c
@@ -294,7 +294,6 @@ cpu_startup(dummy)
 #ifdef PERFMON
 	perfmon_init();
 #endif
-	realmem = Maxmem;
 
 	/*
 	 * Display physical memory if SMBIOS reports reasonable amount.
@@ -308,6 +307,7 @@ cpu_startup(dummy)
 	if (memsize < ptoa((uintmax_t)cnt.v_free_count))
 		memsize = ptoa((uintmax_t)Maxmem);
 	printf("real memory  = %ju (%ju MB)\n", memsize, memsize >> 20);
+	realmem = atop(memsize);
 
 	/*
 	 * Display any holes after the first chunk of extended memory.
