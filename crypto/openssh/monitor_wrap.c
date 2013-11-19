7dd0454fc45a1a13bc8ca3890a570f913eeda6d2
Dag-Erling Smørgrav <des@FreeBSD.org>
2013-11-19 11:47:30 +0000

    MFH (r257954): upgrade to OpenSSH 6.4p1
    Approved by:	re (kib)

--- crypto/openssh/monitor_wrap.c
+++ crypto/openssh/monitor_wrap.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: monitor_wrap.c,v 1.76 2013/05/17 00:13:13 djm Exp $ */
+/* $OpenBSD: monitor_wrap.c,v 1.77 2013/11/06 16:52:11 markus Exp $ */
 /*
  * Copyright 2002 Niels Provos <provos@citi.umich.edu>
  * Copyright 2002 Markus Friedl <markus@openbsd.org>
