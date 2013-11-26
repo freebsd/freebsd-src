361f264ba008c226a3d505c871841b09bc8b9adb
Hans Petter Selasky <hselasky@FreeBSD.org>
2013-11-26 11:11:24 +0000

    Style changes.

--- sys/dev/usb/wlan/if_run.c
+++ sys/dev/usb/wlan/if_run.c
@@ -2520,7 +2520,7 @@ run_rx_frame(struct run_softc *sc, struct mbuf *m, uint32_t dmalen)
 	struct rt2870_rxd *rxd;
 	struct rt2860_rxwi *rxwi;
 	uint32_t flags;
-	uint16_t len, phy;
+	uint16_t len;
 	uint8_t ant, rssi;
 	int8_t nf;
 
@@ -2587,6 +2587,7 @@ run_rx_frame(struct run_softc *sc, struct mbuf *m, uint32_t dmalen)
 
 	if (__predict_false(ieee80211_radiotap_active(ic))) {
 		struct run_rx_radiotap_header *tap = &sc->sc_rxtap;
+		uint16_t phy;
 
 		tap->wr_flags = 0;
 		tap->wr_chan_freq = htole16(ic->ic_curchan->ic_freq);
@@ -5286,7 +5287,7 @@ run_stop(void *arg)
 	tmp &= ~(RT2860_RX_DMA_EN | RT2860_TX_DMA_EN);
 	run_write(sc, RT2860_WPDMA_GLO_CFG, tmp);
 
-        for (ntries = 0; ntries < 100; ntries++) {
+	for (ntries = 0; ntries < 100; ntries++) {
 		if (run_read(sc, RT2860_WPDMA_GLO_CFG, &tmp) != 0)
 			return;
 		if ((tmp & (RT2860_TX_DMA_BUSY | RT2860_RX_DMA_BUSY)) == 0)
