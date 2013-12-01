ec0aa90267cba814e1358c0d15990685877a0fd2
nwhitehorn <nwhitehorn@FreeBSD.org>
2013-12-01 19:43:15 +0000

    Rearchitect platform memory map parsing to make it less Open Firmware-centric: - Keep the static list of regions in platform.c instead of ofw_machdep.c - Move various merging and sorting operations to platform.c as well - Move apple_hacks code out of ofw_machdep.c and into platform_powermac.c,   where it belongs - Move CHRP-specific dynamic-reconfiguration memory parsing into   platform_chrp.c instead of pretending it is shared code

--- sys/powerpc/powermac/platform_powermac.c
+++ sys/powerpc/powermac/platform_powermac.c
@@ -57,8 +57,8 @@ extern void *ap_pcpu;
 
 static int powermac_probe(platform_t);
 static int powermac_attach(platform_t);
-void powermac_mem_regions(platform_t, struct mem_region **phys, int *physsz,
-    struct mem_region **avail, int *availsz);
+void powermac_mem_regions(platform_t, struct mem_region *phys, int *physsz,
+    struct mem_region *avail, int *availsz);
 static u_long powermac_timebase_freq(platform_t, struct cpuref *cpuref);
 static int powermac_smp_first_cpu(platform_t, struct cpuref *cpuref);
 static int powermac_smp_next_cpu(platform_t, struct cpuref *cpuref);
@@ -114,10 +114,60 @@ powermac_probe(platform_t plat)
 }
 
 void
-powermac_mem_regions(platform_t plat, struct mem_region **phys, int *physsz,
-    struct mem_region **avail, int *availsz)
+powermac_mem_regions(platform_t plat, struct mem_region *phys, int *physsz,
+    struct mem_region *avail, int *availsz)
 {
-	ofw_mem_regions(phys,physsz,avail,availsz);
+	phandle_t memory;
+	cell_t memoryprop[PHYS_AVAIL_SZ * 2];
+	ssize_t propsize, i, j;
+	int physacells = 1;
+
+	memory = OF_finddevice("/memory");
+
+	/* "reg" has variable #address-cells, but #size-cells is always 1 */
+	OF_getprop(OF_parent(memory), "#address-cells", &physacells,
+	    sizeof(physacells));
+
+	propsize = OF_getprop(memory, "reg", memoryprop, sizeof(memoryprop));
+	propsize /= sizeof(cell_t);
+	for (i = 0, j = 0; i < propsize; i += physacells+1, j++) {
+		phys[j].mr_start = memoryprop[i];
+		if (physacells == 2) {
+#ifndef __powerpc64__
+			/* On 32-bit PPC, ignore regions starting above 4 GB */
+			if (memoryprop[i] != 0) {
+				j--;
+				continue;
+			}
+#else
+			phys[j].mr_start <<= 32;
+#endif
+			phys[j].mr_start |= memoryprop[i+1];
+		}
+		phys[j].mr_size = memoryprop[i + physacells];
+	}
+	*physsz = j;
+
+	/* "available" always has #address-cells = 1 */
+	propsize = OF_getprop(memory, "available", memoryprop,
+	    sizeof(memoryprop));
+	propsize /= sizeof(cell_t);
+	for (i = 0, j = 0; i < propsize; i += 2, j++) {
+		avail[j].mr_start = memoryprop[i];
+		avail[j].mr_size = memoryprop[i + 1];
+	}
+
+#ifdef __powerpc64__
+	/* Add in regions above 4 GB to the available list */
+	for (i = 0; i < *physsz; i++) {
+		if (phys[i].mr_start > BUS_SPACE_MAXADDR_32BIT) {
+			avail[j].mr_start = phys[i].mr_start;
+			avail[j].mr_size = phys[i].mr_size;
+			j++;
+		}
+	}
+#endif
+	*availsz = j;
 }
 
 static int
