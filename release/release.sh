bd3b507d4de59833fd13fa2de14d4309fa54976f
gjb <gjb@FreeBSD.org>
2013-12-10 17:26:52 +0000

    MFstable10 r259151:   Add WITH_DVD to RELEASE_RMAKEFLAGS, otherwise it is not actually   passed to 'make release'.
    Approved by:	re (glebius)
    Sponsored by:	The FreeBSD Foundation

--- release/release.sh
+++ release/release.sh
@@ -72,6 +72,9 @@ KERNEL="GENERIC"
 NODOC=
 NOPORTS=
 
+# Set to non-empty value to build dvd1.iso as part of the release.
+WITH_DVD=
+
 usage() {
 	echo "Usage: $0 [-c release.conf]"
 	exit 1
@@ -129,7 +132,7 @@ CHROOT_DMAKEFLAGS="${CONF_FILES}"
 RELEASE_WMAKEFLAGS="${MAKE_FLAGS} ${WORLD_FLAGS} ${ARCH_FLAGS} ${CONF_FILES}"
 RELEASE_KMAKEFLAGS="${MAKE_FLAGS} ${KERNEL_FLAGS} KERNCONF=\"${KERNEL}\" ${ARCH_FLAGS} ${CONF_FILES}"
 RELEASE_RMAKEFLAGS="${ARCH_FLAGS} KERNCONF=\"${KERNEL}\" ${CONF_FILES} \
-	${DOCPORTS}"
+	${DOCPORTS} WITH_DVD=${WITH_DVD}"
 
 # Force src checkout if configured
 FORCE_SRC_KEY=
