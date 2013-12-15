421adcfd21d625ab9cd9582978d0e85896bf6d51
Pedro F. Giffuni <pfg@FreeBSD.org>
2013-12-15 03:48:54 +0000

    MFC	rr258501, r258507; gcc: Bring updates from Google's enhanced gcc-4.2.1.
    Google released and enhanced version of gcc-4.2.1 plus their local
    patches for Android[1].
    
    The patches are owned by Google and the license hasn't been changed
    from  the original GPLv2. We are only bringing a subset of the
    available patches that may be helpful in FreeBSD, in other words,
    changes specific to android are not included.
    
    From the README.google file[1].
    
    Patches applied to google_vendor_src_branch/gcc/gcc-4.2.1:
    
    gcc/Makefile.in
    gcc/c-common.c
    gcc/c-common.h
    gcc/c-opts.c
    gcc/c-typeck.c
    gcc/cp/typeck.c
    gcc/doc/invoke.texi
    gcc/flags.h
    gcc/opts.c
    gcc/tree-flow.h
    gcc/tree-ssa-alias-warnings.c
    gcc/tree-ssa-alias.c
    
     Backport of -Wstrict-aliasing from mainline.
      Silvius Rus <rus@google.com>
    
    gcc/coverage.c:
      Patch coverage_checksum_string for PR 25351.
      Seongbae Park <spark@google.com>
      Not yet submitted to FSF.
    
    gcc/c-opts.c
    gcc/c-ppoutput.c
    gcc/c.opt
    gcc/doc/cppopts.texi
    libcpp/Makefile.in
    libcpp/directives-only.c
    libcpp/directives.c
    libcpp/files.c
    libcpp/include/cpplib.h
    libcpp/init.c
    libcpp/internal.h
    libcpp/macro.c
      Support for -fdirectives-only.
      Ollie Wild <aaw@google.com>.
      Submitted to FSF but not yet approved.
    
    libstdc++-v3/include/ext/hashtable.h
      http://b/742065
      http://b/629994
      Reduce min size of hashtable for hash_map, hash_set from 53 to 5
    
    libstdc++-v3/include/ext/hashtable.h
      http://b/629994
      Do not iterate over buckets if hashtable is empty.
    
    gcc/common.opt
    gcc/doc/invoke.texi
    gcc/flags.h
    gcc/gimplify.c
    gcc/opts.c
      Add Saito's patch for -finstrument-functions-exclude-* options.
    
    gcc/common.opt
    gcc/doc/invoke.texi
    gcc/final.c
    gcc/flags.h
    gcc/opts.c
    gcc/testsuite/gcc.dg/Wframe-larger-than.c
      Add a new flag -Wframe-larger-than- which enables a new warning
      when a frame size of a function is larger than specified.
      This patch hasn't been integrated into gcc mainline yet.
    
    gcc/tree-vrp.c
      Add a hack to avoid using ivopts information for pointers starting
      at constant values.
    
    Reference:
    
    [1]
    https://android.googlesource.com/toolchain/gcc/+/master/gcc-4.2.1/
    
    Obtained from:	Google Inc.

--- contrib/gcc/c-common.h
+++ contrib/gcc/c-common.h
@@ -654,7 +654,7 @@ extern void binary_op_error (enum tree_code);
 extern tree fix_string_type (tree);
 struct varray_head_tag;
 extern void constant_expression_warning (tree);
-extern void strict_aliasing_warning(tree, tree, tree);
+extern bool strict_aliasing_warning (tree, tree, tree);
 extern void empty_body_warning (tree, tree);
 extern tree convert_and_check (tree, tree);
 extern void overflow_warning (tree);
