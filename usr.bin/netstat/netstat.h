2f0743dad34f9fe5f923cf3f0490c19d31fcb3cf
melifaro <melifaro@FreeBSD.org>
2013-12-18 20:04:04 +0000

    Restore corefiles handling via kvm(3).
    Found by:	John-Mark Gurney <jmg at funkthat.com>
    MFC after:	4 weeks

--- usr.bin/netstat/netstat.h
+++ usr.bin/netstat/netstat.h
@@ -40,6 +40,7 @@ extern int	gflag;	/* show group (multicast) routing or stats */
 extern int	hflag;	/* show counters in human readable format */
 extern int	iflag;	/* show interfaces */
 extern int	Lflag;	/* show size of listen queues */
+extern int	Mflag;	/* read statistics from core */
 extern int	mflag;	/* show memory stats */
 extern int	noutputs;	/* how much outputs before we exit */
 extern int	numeric_addr;	/* show addresses numerically */
