62f78f3261bee6037c07d5d69e9bc3b8ba64259e
Pedro F. Giffuni <pfg@FreeBSD.org>
2013-12-20 14:56:52 +0000

    gcc: more diff reductions against Apple GCC.
    Mostly cosmetical changes to aid further merges.
    
    Obtained from:	gcc 4.3 (rev. 120611, 124839; GPLv2)
    MFC after:	1 week

--- contrib/gcc/target-def.h
+++ contrib/gcc/target-def.h
@@ -587,6 +587,10 @@ Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #define TARGET_CXX_CLASS_DATA_ALWAYS_COMDAT hook_bool_void_true
 #endif
 
+#ifndef TARGET_CXX_LIBRARY_RTTI_COMDAT
+#define TARGET_CXX_LIBRARY_RTTI_COMDAT hook_bool_void_true
+#endif
+
 #ifndef TARGET_CXX_USE_AEABI_ATEXIT
 #define TARGET_CXX_USE_AEABI_ATEXIT hook_bool_void_false
 #endif
@@ -606,6 +610,7 @@ Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
     TARGET_CXX_KEY_METHOD_MAY_BE_INLINE,	\
     TARGET_CXX_DETERMINE_CLASS_DATA_VISIBILITY,	\
     TARGET_CXX_CLASS_DATA_ALWAYS_COMDAT,        \
+    TARGET_CXX_LIBRARY_RTTI_COMDAT,	        \
     TARGET_CXX_USE_AEABI_ATEXIT,		\
     TARGET_CXX_ADJUST_CLASS_AT_DEFINITION	\
   }
