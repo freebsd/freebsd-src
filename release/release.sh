b8dac04a8fa459169772f5a275961c42b264ca01
jmmv <jmmv@FreeBSD.org>
2013-12-25 13:57:23 +0000

    Delay copying of resolv.conf into the chroot until /etc exists.
    With an unpopulated chroot dir, release building was failing because
    the script was attempting to copy resolv.conf into a non-existent /etc
    directory of the chroot.  Fix this by copying the file only after the
    installworld has happened, which will create this directory.
    
    Reviewed by:	gjb
    Approved by:	rpaulo (mentor)

--- release/release.sh
+++ release/release.sh
@@ -162,12 +162,12 @@ if [ "x${NOPORTS}" = "x" ]; then
 	svn co ${SVNROOT}/${PORTBRANCH} ${CHROOTDIR}/usr/ports
 fi
 
-cp /etc/resolv.conf ${CHROOTDIR}/etc/resolv.conf
 cd ${CHROOTDIR}/usr/src
 make ${CHROOT_WMAKEFLAGS} buildworld
 make ${CHROOT_IMAKEFLAGS} installworld DESTDIR=${CHROOTDIR}
 make ${CHROOT_DMAKEFLAGS} distribution DESTDIR=${CHROOTDIR}
 mount -t devfs devfs ${CHROOTDIR}/dev
+cp /etc/resolv.conf ${CHROOTDIR}/etc/resolv.conf
 trap "umount ${CHROOTDIR}/dev" EXIT # Clean up devfs mount on exit
 
 build_doc_ports() {
