6bb5d62ec0b778a72031ad159c6c95dcd380fc81
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2014-01-07 23:50:06 +0000

    Implement CHERI system class stubs / setup routine in libc_cheri so that they are available to all sandboxed applications.  A sandbox must be delegated access to the system class before system-class methods can be used.

---
+++ lib/libc_cheri/cheri_system_stub.c
@@ -0,0 +1,78 @@
+/*-
+ * Copyright (c) 2014 Robert N. M. Watson
+ * All rights reserved.
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+ * ("CTSRD"), as part of the DARPA CRASH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/types.h>
+
+#include <machine/cheri.h>
+#include <machine/cheric.h>
+
+#include <cheri/cheri_invoke.h>
+#include <cheri/cheri_system.h>
+
+#include <stdlib.h>
+
+/*
+ * This C file contains stubs to invoke the CHERI system class, one per
+ * method.
+ */
+static struct cheri_object cheri_system_object;
+
+void
+cheri_system_setup(__capability void *system_codecap,
+    __capability void *system_datacap)
+{
+
+	cheri_system_object.co_codecap = system_codecap;
+	cheri_system_object.co_datacap = system_datacap;
+}
+
+int
+cheri_system_helloworld(void)
+{
+
+	return (cheri_invoke(cheri_system_object.co_codecap,
+	    cheri_system_object.co_datacap, CHERI_SYSTEM_METHOD_HELLOWORLD, 0,
+	    0, 0, 0, 0, 0, 0, cheri_zerocap(), cheri_zerocap(),
+	    cheri_zerocap(), cheri_zerocap(), cheri_zerocap(),
+	    cheri_zerocap(), cheri_zerocap(), cheri_zerocap()));
+}
+
+int
+cheri_system_puts(__capability const char *str)
+{
+	__capability char *str_noconst;
+
+	str_noconst = (__capability char *)str;
+	return (cheri_invoke(cheri_system_object.co_codecap,
+	    cheri_system_object.co_datacap, CHERI_SYSTEM_METHOD_PUTS, 0, 0, 0,
+	    0, 0, 0, 0, str_noconst, cheri_zerocap(), cheri_zerocap(),
+	    cheri_zerocap(), cheri_zerocap(), cheri_zerocap(),
+	    cheri_zerocap(), cheri_zerocap()));
+}
