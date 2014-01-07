dca3ac026470dd39d3468524235c6f840e8f8778
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2014-01-07 23:51:21 +0000

    Implement cheri-helper methods that will invoke the system class's 'helloworld' and 'puts' methods.
    While here, be slightly more thorough about not delegating 'NULL'
    capabilities to a sandbox -- instead use cheri_zerocap().

--- libexec/cheritest-helper/cheritest-helper.c
+++ libexec/cheritest-helper/cheritest-helper.c
@@ -28,39 +28,27 @@
  * SUCH DAMAGE.
  */
 
-#include <sys/cdefs.h>
-
-#if __has_feature(capabilities)
-#define	USE_C_CAPS
-#endif
-
 #include <sys/types.h>
 
 #include <machine/cheri.h>
 #include <machine/cheric.h>
 
-#include <cheri_invoke.h>
+#include <cheri/cheri_invoke.h>
+#include <cheri/cheri_system.h>
+
 #include <md5.h>
 #include <stdlib.h>
 
 #include "cmemcpy.h"
 #include "cheritest-helper.h"
 
-#ifdef USE_C_CAPS
 int	invoke(register_t op, size_t len, __capability void *system_codecap,
 	    __capability void *system_datacap, __capability char *data_input,
 	    __capability char *data_output);
-#else
-int	invoke(register_t op, size_t len);
-#endif
 
 static int
-#ifdef USE_C_CAPS
 invoke_md5(size_t len, __capability char *data_input,
   __capability char *data_output)
-#else
-invoke_md5(size_t len)
-#endif
 {
 	MD5_CTX md5context;
 	char buf[33], ch;
@@ -68,21 +56,13 @@ invoke_md5(size_t len)
 
 	MD5Init(&md5context);
 	for (count = 0; count < len; count++) {
-#ifdef USE_C_CAPS
 		/* XXXRW: Want a CMD5Update() to avoid copying byte by byte. */
 		ch = data_input[count];
-#else
-		memcpy_fromcap(&ch, 5, count, sizeof(ch));
-#endif
 		MD5Update(&md5context, &ch, sizeof(ch));
 	}
 	MD5End(&md5context, buf);
-#ifdef USE_C_CAPS
 	for (count = 0; count < sizeof(buf); count++)
 		data_output[count] = buf[count];
-#else
-	memcpy_tocap(6, buf, 0, sizeof(buf));
-#endif
 
 	return (123456);
 }
@@ -92,51 +72,26 @@ static int
 invoke_cap_fault(register_t op)
 {
 	char buffer[N], ch;
-#ifdef USE_C_CAPS
 	__capability char *cap;
 
 	cap = cheri_ptrperm(buffer, sizeof(buffer), CHERI_PERM_LOAD);
-#else
-	CHERI_CINCBASE(11, 0, (uintptr_t)buffer);
-	CHERI_CSETLEN(11, 11, sizeof(buffer));
-	CHERI_CANDPERM(11, 11, CHERI_PERM_LOAD);
-#endif
-
 	switch (op) {
 	case CHERITEST_HELPER_OP_CP2_BOUND:
-#ifdef USE_C_CAPS
 		ch = cap[N];
-#else
-		CHERI_CLB(ch, N, 0, 11);
-#endif
 		return (ch);
 
 	case CHERITEST_HELPER_OP_CP2_PERM:
-#ifdef USE_C_CAPS
 		cap[0] = 0;
-#else
-		CHERI_CSB(0, 0, 0, 11);
-#endif
 		break;
 
 	case CHERITEST_HELPER_OP_CP2_TAG:
-#ifdef USE_C_CAPS
 		cap = cheri_zerocap();
 		ch = cap[0];
-#else
-		CHERI_CCLEARTAG(11);
-		CHERI_CLB(ch, 0, 0, 11);
-#endif
 		return (ch);
 
 	case CHERITEST_HELPER_OP_CP2_SEAL:
-#ifdef USE_C_CAPS
 		cap = cheri_sealcode(cap);
 		ch = cap[0];
-#else
-		CHERI_CSEALCODE(11, 11);
-		CHERI_CLB(ch, 0, 0, 11);
-#endif
 		return (ch);
 	}
 	return (0);
@@ -180,7 +135,6 @@ invoke_syscall(void)
 }
 
 static int
-#ifdef USE_C_CAPS
 invoke_syscap(__capability void *system_codecap,
     __capability void *system_datacap)
 {
@@ -188,15 +142,6 @@ invoke_syscap(__capability void *system_codecap,
 	return (cheri_invoke(system_codecap, system_datacap, 0, 0, 0, 0, 0, 0,
 	    0, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL));
 }
-#else
-invoke_syscap(void)
-{
-
-	CHERI_CMOVE(1, 3);
-	CHERI_CMOVE(2, 4);
-	return (cheri_invoke(0, 0, 0, 0, 0, 0, 0, 0));
-}
-#endif
 
 /*
  * Sample sandboxed code.  Calculate an MD5 checksum of the data arriving via
@@ -205,23 +150,17 @@ invoke_syscap(void)
  * least) 33 bytes.
  */
 int
-#ifdef USE_C_CAPS
 invoke(register_t op, size_t len, __capability void *system_codecap,
     __capability void *system_datacap, __capability char *data_input,
     __capability char *data_output)
-#else
-invoke(register_t op, size_t len)
-#endif
 {
 	int ret;
 
+	cheri_system_setup(system_codecap, system_datacap);
+
 	switch (op) {
 	case CHERITEST_HELPER_OP_MD5:
-#ifdef USE_C_CAPS
 		return (invoke_md5(len, data_input, data_output));
-#else
-		return (invoke_md5(len));
-#endif
 
 	case CHERITEST_HELPER_OP_ABORT:
 		abort();
@@ -247,11 +186,14 @@ invoke(register_t op, size_t len)
 		return (1/0);
 
 	case CHERITEST_HELPER_OP_SYSCAP:
-#ifdef USE_C_CAPS
 		return (invoke_syscap(system_codecap, system_datacap));
-#else
-		return (invoke_syscap());
-#endif
+
+	case CHERITEST_HELPER_OP_HELLOWORLD:
+		return (cheri_system_helloworld());
+
+	case CHERITEST_HELPER_OP_PUTS:
+		return (cheri_system_puts(
+		    (__capability char *)"sandbox puts\n"));
 	}
 	return (ret);
 }
