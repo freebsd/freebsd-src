26fb1dbcc981ca60aa30484c847e751506ca9780
Brooks Davis <brooks@one-eyed-alien.net>
2014-01-09 22:58:56 +0000

    Remove a bunch of stubs made redundant by additions to libc_cheri.

--- libexec/tcpdump-helper/stub.c
+++ libexec/tcpdump-helper/stub.c
@@ -48,33 +48,15 @@ int	issetugid(void);
 void	exit(int status __unused);
 char	*getenv(const char *name);
 
-FILE	*fopen(const char * restrict path, const char * restrict mode);
-size_t	fread(void * restrict ptr, size_t size, size_t nmemb,
-	    FILE * restrict stream);
 int	fileno(FILE *stream);
-int	fflush(FILE *stream);
 
 int	close(int fildes);
 int	fstat(int fildes, struct stat *buf);
 int	open(const char *path, int oflag, ...);
-void	rewind(FILE *stream);
 ssize_t	write(int fd, const void *buf, size_t nbytes);
 ssize_t	_write(int fd, const void *buf, size_t nbytes);
 
-int	__swbuf(int c, FILE *fp);
-int	putc(int c, FILE *stream);
-int	puts(const char *str);
-int	putchar(int c);
-int	fputc(int c, FILE *stream);
-int	fputs(const char *str, FILE *stream);
-size_t	fwrite(const void * restrict ptr, size_t size, size_t nmemb,
-	    FILE* restrict stream);
-int	fprintf(FILE * restrict stream, const char * restrict format, ...);
-int	fprintf_l(FILE * restrict stream, locale_t loc, const char * restrict format, ...);
-int	sprintf(char * restrict str, const char * restrict format, ...);
-int	vfprintf(FILE * restrict stream, const char * restrict format,
-	    va_list ap);
-int fclose(FILE *stream);
+//int	__swbuf(int c, FILE *fp);
 
 struct tm * gmtime(const time_t *clock);
 time_t	mktime(struct tm *tm);
@@ -85,11 +67,6 @@ int __isthreaded = 0;
 void * __getCurrentRuneLocale(void);
 
 /* XXX no contents, they aren't real, just pointer cookies */
-static FILE __sF[3];
-
-FILE *__stdinp = &__sF[0];
-FILE *__stdoutp = &__sF[1];
-FILE *__stderrp = &__sF[2];
 
 static int	stub_errno;
 
@@ -172,94 +149,19 @@ _read(int d __unused, void *buf __unused, size_t nbytes __unused)
 	return (-1);
 }
 
-int
-printf(const char * restrict format __unused, ...)
-{
-
-	return (-1);
-}
-
-int
-puts(const char *str)
-{
-	return (EOF);
-}
-
-int
-fputs(const char *str, FILE *stream)
-{
-	return (EOF);
-}
-
-/* XXX: eliminate uses of this */
-size_t
-fwrite(const void * restrict ptr __unused, size_t size __unused,
-    size_t nmemb __unused, FILE* restrict stream __unused)
-{
-
-	return (0);
-}
-
-int
-snprintf(char * __restrict str __unused, size_t n __unused,
-    char const * __restrict fmt __unused, ...)
-{
-	return(0);
-}
-
-int
-putchar(int c)
-{
-	return (c);
-}
-
-int	
-putc(int c, FILE *stream __unused)
-{
-	return (c);
-}
-
-int
-fputc(int c, FILE *stream __unused)
-{
-	return (c);
-}
-
 void
 exit(int status __unused)
 {
 	abort();
 }
 
-int
-fprintf(FILE * restrict stream __unused, const char * restrict format __unused, ...)
-{
-	return(0);
-}
-int
-fprintf_l(FILE * restrict stream __unused, locale_t loc __unused, const char * restrict format __unused, ...)
-{
-	return(0);
-}
-
-int
-sprintf(char * restrict str __unused, const char * restrict format __unused, ...)
-{
-	return(0);
-}
-
-int
-vfprintf(FILE * restrict stream __unused, const char * restrict format __unused,
-    va_list ap __unused)
-{
-	return(0);
-}
-
+#if 0
 int
 __swbuf(int c, FILE *fp __unused)
 {
 	return (c);
 }
+#endif
 
 int
 issetugid(void)
@@ -267,40 +169,10 @@ issetugid(void)
 	return (1);
 }
 
-int
-fclose(FILE *stream)
-{
-	return EOF;
-}
-
 char *
 getenv(const char *name)
 {
 	return NULL;
-}
-
-FILE *
-fopen(const char * restrict path __unused, const char * restrict mode __unused)
-{
-	return NULL;
-}
-
-size_t
-fread(void * restrict ptr __unused, size_t size __unused, size_t nmemb __unused,
-	    FILE * restrict stream __unused)
-{
-	return EOF;
-}
-
-int
-fileno(FILE *stream __unused)
-{
-	return -1;
-}
-
-void
-rewind(FILE *stream __unused)
-{
 }
 
 ssize_t
@@ -312,20 +184,6 @@ ssize_t
 _write(int fd __unused, const void *buf __unused, size_t nbytes __unused)
 {
 	return -1;
-}
-
-int
-fflush(FILE *stream __unused)
-{
-	return EOF;
-}
-
-char *fgets(char * restrict str, int size, FILE * restrict stream);
-char *
-fgets(char * restrict str __unused, int size __unused,
-    FILE * restrict stream __unused)
-{
-	return (NULL);
 }
 
 void * getrpcbynumber(int number)
