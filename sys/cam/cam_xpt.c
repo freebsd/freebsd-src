c33e40291b9d642929f63f6d595b8fdfc3c80252
Alexander Motin <mav@FreeBSD.org>
2014-01-11 13:35:36 +0000

    Take additional reference on SCSI probe periph to cover its freeze count.
    Otherwise periph may be invalidated and freed before single-stepping freeze
    is dropped, causing use after free panic.

--- sys/cam/cam_xpt.c
+++ sys/cam/cam_xpt.c
@@ -3156,9 +3156,7 @@ restart:
 			}
 			if (periph->flags & CAM_PERIPH_RUN_TASK)
 				break;
-			xpt_lock_buses();
-			periph->refcount++;	/* Unconditionally acquire */
-			xpt_unlock_buses();
+			cam_periph_doacquire(periph);
 			periph->flags |= CAM_PERIPH_RUN_TASK;
 			taskqueue_enqueue(xsoftc.xpt_taskq,
 			    &periph->periph_run_task);
