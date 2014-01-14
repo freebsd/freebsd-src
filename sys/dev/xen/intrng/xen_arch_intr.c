69f6114e8c7e7411f93535261c459834bc724010
Julien Grall <julien@xen.org>
2014-01-14 01:40:58 +0000

    xen/intrng: add handling of Xen device-tree
    Add the earliest stage of Xen guest initialization.  This checks for the
    presence of Xen and sets flags to indicate the need to initialize other
    pieces (or not).
    
    This retrieves Xen's recommended address range for grant table and
    interrupt for event channel.  The interrupt is forwarded to the Xen
    interrupt handling code.
    
    Originally the device was "xen0", but "xen-dt0" should hopefully be
    clearer in boot messages.  The address range is for grant table and
    the interrupt is used for the event channel.
    
    The test in xen_dt_probe() was adjusted to match how such probes
    are done.  The location where presence is probed was also adjusted.
    
    This was originally implemented by Julien Grall in 2014.  This is was
    broken off of a much larger commit.  Numerous adjustments have been done
    by Elliott Mitchell in 2021.
    
    Submitted by: Elliott Mitchell <ehem+freebsd@m5p.com>
    Original implementation: Julien Grall <julien@xen.org>, 2014-01-13 17:40:58
    Differential Revision: https://reviews.freebsd.org/D29875

--- sys/dev/xen/intrng/xen_arch_intr.c
+++ sys/dev/xen/intrng/xen_arch_intr.c
@@ -1,6 +1,8 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause
  *
+ * Copyright © 2014 Julien Grall
+ * Copyright © 2021 Elliott Mitchell
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -24,3 +26,147 @@
  * SUCH DAMAGE.
  */
 
+#include <sys/cdefs.h>
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/bus.h>
+#include <sys/kernel.h>
+#include <sys/pcpu.h>
+#include <sys/rman.h>
+#include <sys/module.h>
+
+#include <vm/vm.h>
+#include <vm/pmap.h>
+#include <vm/vm_page.h>
+
+#include <dev/ofw/openfirm.h>
+#include <dev/ofw/ofw_bus.h>
+#include <dev/ofw/ofw_bus_subr.h>
+
+#include <dev/fdt/fdt_common.h>
+
+#include <xen/xen-os.h>
+#include <xen/xen_intr.h>
+#include <xen/hypervisor.h>
+#include <contrib/xen/vcpu.h>
+#include <xen/features.h>
+
+
+DPCPU_DEFINE(struct vcpu_info, vcpu_local_info);
+DPCPU_DEFINE(struct vcpu_info *, vcpu_info);
+
+int
+xen_dt_probe(void)
+{
+	/*
+	 * Short-circuit extra attempts at looking for Xen
+	 */
+	if (xen_domain())
+		return (1);
+
+	/*
+	 * The device tree contains the node /hypervisor with the compatible
+	 * string "xen,xen" when the OS will run on top of Xen.
+	 */
+	if (ofw_bus_node_is_compatible(OF_finddevice("/hypervisor"), "xen,xen") == 0)
+		return (0);
+
+	vm_guest = VM_GUEST_XEN;
+	setup_xen_features();
+
+/* Implementation for 126b73dd8bd, but no longer works
+	if (xen_feature(XENFEAT_dom0))
+		HYPERVISOR_start_info->flags |= SIF_INITDOMAIN|SIF_PRIVILEGED;
+	else
+		HYPERVISOR_start_info->flags &= ~(SIF_INITDOMAIN|SIF_PRIVILEGED)
+;
+*/
+
+	return (1);
+}
+
+/* in case of console being disabled, probe again as a fallback */
+C_SYSINIT(xen_probe_fdt, SI_SUB_HYPERVISOR, SI_ORDER_FIRST, (sysinit_cfunc_t)xen_dt_probe, NULL);
+
+
+struct xen_softc {
+	struct resource		*intr;
+	void			*cookie;
+	int			rid;
+};
+
+struct xen_softc *xen_sc = NULL;
+
+static int
+xen_probe(device_t dev)
+{
+	if (!ofw_bus_status_okay(dev))
+		return (ENXIO);
+
+	if (!ofw_bus_is_compatible(dev, "xen,xen"))
+		return (ENXIO);
+
+	device_set_desc(dev, "Xen ARM device-tree");
+
+	return (BUS_PROBE_DEFAULT);
+}
+
+static int
+xen_attach(device_t dev)
+{
+	struct xen_softc *sc;
+	struct vcpu_register_vcpu_info info;
+	struct vcpu_info *vcpu_info;
+	vm_paddr_t phys;
+	int rc, cpu;
+
+	if (xen_sc != NULL)
+		return (ENXIO);
+
+	sc = device_get_softc(dev);
+
+	/* TODO: Move to a proper function */
+	vcpu_info = DPCPU_PTR(vcpu_local_info);
+	cpu = PCPU_GET(cpuid);
+	phys = vtophys(vcpu_info);
+	info.mfn = phys >> PAGE_SHIFT_4K;
+	info.offset = phys & PAGE_MASK_4K;
+
+	rc = HYPERVISOR_vcpu_op(VCPUOP_register_vcpu_info, cpu, &info);
+	KASSERT(rc == 0, ("Unable to register cpu %u\n", cpu));
+	DPCPU_SET(vcpu_info, vcpu_info);
+
+
+	/* Resources */
+	sc->intr = bus_alloc_resource_any(dev, SYS_RES_IRQ, &sc->rid, RF_ACTIVE);
+	if (sc->intr == NULL) {
+		panic("Unable to retrieve Xen event channel interrupt");
+		return (ENXIO);
+	}
+
+	xen_sc = sc;
+
+	/* Setup and enable the event channel interrupt */
+	if (bus_setup_intr(dev, sc->intr, INTR_TYPE_MISC|INTR_MPSAFE,
+		    xen_intr_handle_upcall, NULL, sc, &sc->cookie)) {
+		panic("Could not setup event channel interrupt");
+		return (ENXIO);
+	}
+
+	return (0);
+}
+
+static device_method_t xen_methods[] = {
+	DEVMETHOD(device_probe,		xen_probe),
+	DEVMETHOD(device_attach,	xen_attach),
+	{ 0, 0 }
+};
+
+static driver_t xen_driver = {
+	"xen",
+	xen_methods,
+	sizeof(struct xen_softc),
+};
+
+DRIVER_MODULE(xen, simplebus, xen_driver, 0, 0);
+DRIVER_MODULE(xen, ofwbus, xen_driver, 0, 0);
