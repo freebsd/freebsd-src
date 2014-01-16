05e1fbf7a5188b7f53b9b1437bc34a2a1ee95f12
avg <avg@FreeBSD.org>
2014-01-16 14:08:11 +0000

    MFC r253821,254753,256259
    MFV r253783: 3834 incremental replication of 'holey' file systems is
    slow
    
    MFV r254747:4047 panic from dbuf_free_range() from dmu_free_object()
    while doing zfs receive
    
    MFV r255257: 4082 zfs receive gets EFBIG from dmu_tx_hold_free()

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/dmu_impl.h
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/dmu_impl.h
@@ -21,8 +21,11 @@
 /*
  * Copyright 2010 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
+ */
+/*
  * Copyright (c) 2012, Joyent, Inc. All rights reserved.
  * Copyright (c) 2012, Martin Matuska <mm@FreeBSD.org>. All rights reserved.
+ * Copyright (c) 2013 by Delphix. All rights reserved.
  */
 
 #ifndef _SYS_DMU_IMPL_H
@@ -293,6 +296,9 @@ typedef struct dmu_sendarg {
 	uint64_t dsa_toguid;
 	int dsa_err;
 	dmu_pendop_t dsa_pending_op;
+	boolean_t dsa_incremental;
+	uint64_t dsa_last_data_object;
+	uint64_t dsa_last_data_offset;
 } dmu_sendarg_t;
 
 
