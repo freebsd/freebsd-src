c770f3b5359b9fa548d8ea3926e9bc828ca4e2df
kaiw <kaiw@FreeBSD.org>
2014-01-18 10:13:04 +0000

    Apply r241720 by ed:
    Fix warnings found by -Wmising-variable-declarations.

--- contrib/elftoolchain/libelf/elf_errmsg.c
+++ contrib/elftoolchain/libelf/elf_errmsg.c
@@ -38,7 +38,7 @@ ELFTC_VCSID("$Id: elf_errmsg.c 2225 2011-11-26 18:55:54Z jkoshy $");
  * Retrieve a human readable translation for an error message.
  */
 
-const char *_libelf_errors[] = {
+static const char *_libelf_errors[] = {
 #define	DEFINE_ERROR(N,S)	[ELF_E_##N] = S
 	DEFINE_ERROR(NONE,	"No Error"),
 	DEFINE_ERROR(ARCHIVE,	"Malformed ar(1) archive"),
