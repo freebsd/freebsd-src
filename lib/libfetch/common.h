4524013cd3ca6337f1c7ed57594ebb70fa4ab399
Dag-Erling Smørgrav <des@FreeBSD.org>
2014-01-30 08:37:23 +0000

    Bump copyright dates

--- lib/libfetch/common.h
+++ lib/libfetch/common.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 1998-2011 Dag-Erling Smørgrav
+ * Copyright (c) 1998-2014 Dag-Erling Smørgrav
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
