f7d1827f21057286d8e9f7da871f985255e9c0b1
imp <imp@FreeBSD.org>
2014-01-31 23:28:18 +0000

    Minor cleanup of comments.

--- sys/arm/at91/at91_machdep.c
+++ sys/arm/at91/at91_machdep.c
@@ -131,15 +131,12 @@ struct pv_addr kernelstack;
 /* Static device mappings. */
 const struct arm_devmap_entry at91_devmap[] = {
 	/*
-	 * Map the on-board devices VA == PA so that we can access them
-	 * with the MMU on or off.
+	 * Map the critical on-board devices. The interrupt vector at
+	 * 0xffff0000 makes it impossible to map them PA == VA, so we map all
+	 * 0xfffxxxxx addresses to 0xdffxxxxx. This covers all critical devices
+	 * on all members of the AT91SAM9 and AT91RM9200 families.
 	 */
 	{
-		/*
-		 * This at least maps the interrupt controller, the UART
-		 * and the timer. Other devices should use newbus to
-		 * map their memory anyway.
-		 */
 		0xdff00000,
 		0xfff00000,
 		0x00100000,
