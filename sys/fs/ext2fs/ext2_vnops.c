b28152a02f0cd8d08a6c746876d87b5c756eedda
pfg <pfg@FreeBSD.org>
2014-01-31 03:59:28 +0000

    MFC	r260988, r261034, r261120,  r261235:
    ext2fs: Properly the EXT4_EXTENTS and EXT4_INDEX to the inode flags.
    
    In order to support Ext4 extents we need to pass the Ext4 inode flags
    without interfering with the chflags. This is better done by using the
    i_flag field in the inode and doing proper translation to the linux
    ext4 equivalents.
    
    Solve a potential corruption issue in the dirindex code. The dirindex
    code can now be renabled as the problems related to it have been
    solved.
    
    Suggested by:	bde
    Tested by:	kevlo

--- sys/fs/ext2fs/ext2_vnops.c
+++ sys/fs/ext2fs/ext2_vnops.c
@@ -1621,7 +1621,7 @@ ext2_read(struct vop_read_args *ap)
 	ip = VTOI(vp);
 
 	/*EXT4_EXT_LOCK(ip);*/
-	if (ip->i_flags & EXT4_EXTENTS)
+	if (ip->i_flag & IN_E4EXTENTS)
 		error = ext4_ext_read(ap);
 	else
 		error = ext2_ind_read(ap);
