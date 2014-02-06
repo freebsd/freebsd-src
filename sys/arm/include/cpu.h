a1e035312d6e92b6c3efd7cefa9ed8cedfedb555
andrew <andrew@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-02-06 20:17:58 +0000

    Pass the kernel physical address to initarm through the boot param struct.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@261562 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/arm/include/cpu.h
+++ sys/arm/include/cpu.h
@@ -41,6 +41,7 @@ struct arm_boot_params {
 	register_t	abp_r1;		/* r1 from the boot loader */
 	register_t	abp_r2;		/* r2 from the boot loader */
 	register_t	abp_r3;		/* r3 from the boot loader */
+	vm_offset_t	abp_physaddr;	/* The kernel physical address */
 };
 
 void	arm_vector_init(vm_offset_t, int);
