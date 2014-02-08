007aeeced6c05bc3cefb4ca437b1ec3b85a46e5b
Ian Lepore <ian@FreeBSD.org>
2014-02-08 22:21:38 +0000

    Remove the ARM_USE_SMALL_ALLOC option and code related to it.
    This was an optimization used only by a few xscale platforms.  Part of
    the optimization was to create a direct map for all physical pages, and
    that resulted in making multiple mappings of pages in a way that bypassed
    the logic in pmap.c to handle VIVT cache aliasing.  It also just generally
    made the code more complex and hard to maintain for all SoCs.
    
    Reviewed by:	cognet

--- sys/arm/include/vmparam.h
+++ sys/arm/include/vmparam.h
@@ -82,21 +82,15 @@
 #define	VM_PHYSSEG_DENSE
 
 /*
- * Create two or three free page pools depending on the existence of a direct
- * map: VM_FREEPOOL_DEFAULT is the default pool from which physical pages are
- * allocated, and VM_FREEPOOL_DIRECT is the pool from which physical pages for
- * small UMA objects are allocated.
+ * Create two free page pools.  Since the ARM kernel virtual address
+ * space does not include a mapping onto the machine's entire physical
+ * memory, VM_FREEPOOL_DIRECT is defined as an alias for the default
+ * pool, VM_FREEPOOL_DEFAULT.
  */
-#ifdef ARM_USE_SMALL_ALLOC
-#define	VM_NFREEPOOL		3
-#define	VM_FREEPOOL_CACHE	2
-#define	VM_FREEPOOL_DIRECT	1
-#else
 #define	VM_NFREEPOOL		2
 #define	VM_FREEPOOL_CACHE	1
-#define	VM_FREEPOOL_DIRECT	0
-#endif
 #define	VM_FREEPOOL_DEFAULT	0
+#define	VM_FREEPOOL_DIRECT	0
 
 /*
  * we support 2 free lists:
@@ -132,23 +126,9 @@
 #define UPT_MIN_ADDRESS		VADDR(UPTPTDI, 0)
 
 #define VM_MIN_ADDRESS          (0x00001000)
-#ifdef ARM_USE_SMALL_ALLOC
-/*
- * ARM_KERN_DIRECTMAP is used to make sure there's enough space between
- * VM_MAXUSER_ADDRESS and KERNBASE to map the whole memory.
- * It has to be a compile-time constant, even if arm_init_smallalloc(),
- * which will do the mapping, gets the real amount of memory at runtime,
- * because VM_MAXUSER_ADDRESS is a constant.
- */
-#ifndef ARM_KERN_DIRECTMAP
-#define ARM_KERN_DIRECTMAP 512 * 1024 * 1024 /* 512 MB */
-#endif
-#define VM_MAXUSER_ADDRESS	KERNBASE - ARM_KERN_DIRECTMAP
-#else /* ARM_USE_SMALL_ALLOC */
 #ifndef VM_MAXUSER_ADDRESS
 #define VM_MAXUSER_ADDRESS      KERNBASE
 #endif /* VM_MAXUSER_ADDRESS */
-#endif /* ARM_USE_SMALL_ALLOC */
 #define VM_MAX_ADDRESS          VM_MAXUSER_ADDRESS
 
 #define USRSTACK        VM_MAXUSER_ADDRESS
@@ -186,10 +166,6 @@
 #define	VM_KMEM_SIZE_MAX	((vm_max_kernel_address - \
     VM_MIN_KERNEL_ADDRESS + 1) * 2 / 5)
 #endif
-
-#ifdef ARM_USE_SMALL_ALLOC
-#define UMA_MD_SMALL_ALLOC
-#endif /* ARM_USE_SMALL_ALLOC */
 
 extern vm_offset_t vm_max_kernel_address;
 
