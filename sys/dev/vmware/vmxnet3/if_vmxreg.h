8e9058d2a791166c3db53630b263940ae3d54c07
bryanv <bryanv@FreeBSD.org>
2014-02-09 20:28:58 +0000

    Manually fix mismerges from r261399

--- sys/dev/vmware/vmxnet3/if_vmxreg.h
+++ sys/dev/vmware/vmxnet3/if_vmxreg.h
@@ -1,6 +1,5 @@
 /*-
  * Copyright (c) 2013 Tsubai Masanari
- * Copyright (c) 2013 Bryan Venteicher <bryanv@FreeBSD.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
