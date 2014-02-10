a7450373cb00a9e831738a4b4a4561a1571f63d2
glebius <glebius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-02-10 20:09:10 +0000

    Expose real size of UMA allocations via libmemstat(3).
    Sponsored by:	Nginx, Inc.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@261726 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libmemstat/memstat_uma.c
+++ lib/libmemstat/memstat_uma.c
@@ -212,6 +212,7 @@ retry:
 		}
 
 		mtp->mt_size = uthp->uth_size;
+		mtp->mt_rsize = uthp->uth_rsize;
 		mtp->mt_memalloced = mtp->mt_numallocs * uthp->uth_size;
 		mtp->mt_memfreed = mtp->mt_numfrees * uthp->uth_size;
 		mtp->mt_bytes = mtp->mt_memalloced - mtp->mt_memfreed;
@@ -435,6 +436,7 @@ memstat_kvm_uma(struct memory_type_list *list, void *kvm_handle)
 			}
 skip_percpu:
 			mtp->mt_size = kz.uk_size;
+			mtp->mt_rsize = kz.uk_rsize;
 			mtp->mt_memalloced = mtp->mt_numallocs * mtp->mt_size;
 			mtp->mt_memfreed = mtp->mt_numfrees * mtp->mt_size;
 			mtp->mt_bytes = mtp->mt_memalloced - mtp->mt_memfreed;
