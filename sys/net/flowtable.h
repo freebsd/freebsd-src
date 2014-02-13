7d1964f9ec227aa9c60a2b9ca1ba73219c0ed4a8
glebius <glebius@FreeBSD.org>
2014-02-13 04:59:18 +0000

    o Axe non-pcpu flowtable implementation. It wasn't enabled or used,   and probably is a leftover from first prototyping by Kip. The   non-pcpu implementation used mutexes, so it doubtfully worked   better than simple routing lookup. o Use UMA_ZONE_PCPU zone for pointers instead of [MAXCPU] arrays,   use zpcpu_get() to access data in there. o Substitute own single list implementation with SLIST(). This   has two functional side effects:   - new flows go into head of a list, before they went to tail.   - a bug when incorrect flow was deleted in flow cleaner is     fixed. o Due to cache line alignment, there is no reason to keep   different zones for IPv4 and IPv6 flows. Both consume one   cache line, real size of allocation is equal. o Rely on that f_hash, f_rt, f_lle are stable during fle   lifetime, remove useless volatile quilifiers. o More INET/INET6 splitting.
    Reviewed by:	adrian
    Sponsored by:	Netflix
    Sponsored by:	Nginx, Inc.

--- sys/net/flowtable.h
+++ sys/net/flowtable.h
@@ -44,7 +44,6 @@ struct flowtable_stat {
 #ifdef	_KERNEL
 
 #define	FL_HASH_ALL	(1<<0)	/* hash 4-tuple + protocol */
-#define	FL_PCPU		(1<<1)	/* pcpu cache */
 #define	FL_NOAUTO	(1<<2)	/* don't automatically add flentry on miss */
 #define FL_IPV6  	(1<<9)
 
