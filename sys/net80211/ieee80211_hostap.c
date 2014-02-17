434976f6cd42b5bf4e4b90394940bf104607befe
Kevin Lo <kevlo@FreeBSD.org>
2014-02-17 01:36:53 +0000

    MFC r260444:
    Rename definition of IEEE80211_FC1_WEP to IEEE80211_FC1_PROTECTED.
    
    The origin of WEP comes from IEEE Std 802.11-1997 where it defines
    whether the frame body of MAC frame has been encrypted using WEP
    algorithm or not.
    IEEE Std. 802.11-2007 changes WEP to Protected Frame, indicates
    whether the frame is protected by a cryptographic encapsulation
    algorithm.
    
    Reviewed by:	adrian, rpaulo

--- sys/net80211/ieee80211_hostap.c
+++ sys/net80211/ieee80211_hostap.c
@@ -693,7 +693,7 @@ hostap_input(struct ieee80211_node *ni, struct mbuf *m, int rssi, int nf)
 		 * crypto cipher modules used to do delayed update
 		 * of replay sequence numbers.
 		 */
-		if (wh->i_fc[1] & IEEE80211_FC1_WEP) {
+		if (wh->i_fc[1] & IEEE80211_FC1_PROTECTED) {
 			if ((vap->iv_flags & IEEE80211_F_PRIVACY) == 0) {
 				/*
 				 * Discard encrypted frames when privacy is off.
@@ -711,7 +711,7 @@ hostap_input(struct ieee80211_node *ni, struct mbuf *m, int rssi, int nf)
 				goto out;
 			}
 			wh = mtod(m, struct ieee80211_frame *);
-			wh->i_fc[1] &= ~IEEE80211_FC1_WEP;
+			wh->i_fc[1] &= ~IEEE80211_FC1_PROTECTED;
 		} else {
 			/* XXX M_WEP and IEEE80211_F_PRIVACY */
 			key = NULL;
@@ -855,7 +855,7 @@ hostap_input(struct ieee80211_node *ni, struct mbuf *m, int rssi, int nf)
 			    ether_sprintf(wh->i_addr2), rssi);
 		}
 #endif
-		if (wh->i_fc[1] & IEEE80211_FC1_WEP) {
+		if (wh->i_fc[1] & IEEE80211_FC1_PROTECTED) {
 			if (subtype != IEEE80211_FC0_SUBTYPE_AUTH) {
 				/*
 				 * Only shared key auth frames with a challenge
@@ -883,7 +883,7 @@ hostap_input(struct ieee80211_node *ni, struct mbuf *m, int rssi, int nf)
 				goto out;
 			}
 			wh = mtod(m, struct ieee80211_frame *);
-			wh->i_fc[1] &= ~IEEE80211_FC1_WEP;
+			wh->i_fc[1] &= ~IEEE80211_FC1_PROTECTED;
 		}
 		/*
 		 * Pass the packet to radiotap before calling iv_recv_mgmt().
