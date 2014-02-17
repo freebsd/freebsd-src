c1c5568de30d7fb751c0f824255c33afc09262cf
Kevin Lo <kevlo@FreeBSD.org>
2014-02-17 01:39:03 +0000

    MFC r260463:
    Replace deprecated M_DONTWAIT with M_NOWAIT.

--- sys/dev/usb/wlan/if_urtwn.c
+++ sys/dev/usb/wlan/if_urtwn.c
@@ -625,7 +625,7 @@ urtwn_rx_frame(struct urtwn_softc *sc, uint8_t *buf, int pktlen, int *rssi_p)
 		rssi = URTWN_RSSI(rssi);
 	}
 
-	m = m_getcl(M_DONTWAIT, MT_DATA, M_PKTHDR);
+	m = m_getcl(M_NOWAIT, MT_DATA, M_PKTHDR);
 	if (m == NULL) {
 		device_printf(sc->sc_dev, "could not create RX mbuf\n");
 		return (NULL);
