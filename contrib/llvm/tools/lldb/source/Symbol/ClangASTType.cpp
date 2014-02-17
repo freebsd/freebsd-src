e616d3185cc461109037ea629ad37dc9617caf5a
emaste <emaste@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-02-17 18:50:03 +0000

    Update lldb for clang/llvm 3.4 import
    This commit largely restores the lldb source to the upstream r196259
    snapshot with the addition of threaded inferior support and a few bug
    fixes.
    
    Specific upstream lldb revisions restored include:
       SVN      git
      181387  779e6ac
      181703  7bef4e2
      182099  b31044e
      182650  f2dcf35
      182683  0d91b80
      183862  15c1774
      183929  99447a6
      184177  0b2934b
      184948  4dc3761
      184954  007e7bc
      186990  eebd175
    
    Sponsored by:	DARPA, AFRL
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@262121 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/llvm/tools/lldb/source/Symbol/ClangASTType.cpp
+++ contrib/llvm/tools/lldb/source/Symbol/ClangASTType.cpp
@@ -1476,6 +1476,9 @@ ClangASTType::GetTypeClass () const
         case clang::Type::Decltype:                 break;
         case clang::Type::TemplateSpecialization:   break;
         case clang::Type::Atomic:                   break;
+
+        // pointer type decayed from an array or function type.
+        case clang::Type::Decayed:                  break;
     }
     // We don't know hot to display this type...
     return lldb::eTypeClassOther;
@@ -1912,6 +1915,9 @@ ClangASTType::GetEncoding (uint64_t &count) const
         case clang::Type::Decltype:
         case clang::Type::TemplateSpecialization:
         case clang::Type::Atomic:
+
+        // pointer type decayed from an array or function type.
+        case clang::Type::Decayed:
             break;
     }
     count = 0;
@@ -2040,6 +2046,10 @@ ClangASTType::GetFormat () const
         case clang::Type::Decltype:
         case clang::Type::TemplateSpecialization:
         case clang::Type::Atomic:
+            break;
+
+        // pointer type decayed from an array or function type.
+        case clang::Type::Decayed:
             break;
     }
     // We don't know hot to display this type...
@@ -5227,6 +5237,9 @@ ClangASTType::GetDeclContextForType () const
         case clang::Type::InjectedClassName:        break;
         case clang::Type::DependentName:            break;
         case clang::Type::Atomic:                   break;
+
+        // pointer type decayed from an array or function type.
+        case clang::Type::Decayed:                  break;
     }
     // No DeclContext in this type...
     return NULL;
