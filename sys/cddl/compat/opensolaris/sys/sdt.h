cc43c81b039ea4e8f8aec917d7a7cb781e320310
avg <avg@FreeBSD.org>
2014-02-17 14:47:52 +0000

    MFC r258625: sdt: add support for solaris/illumos style DTRACE_PROBE macros
    Note: in head KDTRACE_HOOKS is defined in opt_global.h and thus there is
    no need to explicitly include any header to check that option.
    In this branch we have to include opt_kdtrace.h and, so, this commit
    carries additional changes to accomodate for that difference.
    
    Sponsored by:	HybridCluster

--- sys/cddl/compat/opensolaris/sys/sdt.h
+++ sys/cddl/compat/opensolaris/sys/sdt.h
@@ -29,19 +29,10 @@
 #ifndef _OPENSOLARIS_SYS_SDT_H_
 #define	_OPENSOLARIS_SYS_SDT_H_
 
+#ifdef _KERNEL
+#include <opt_kdtrace.h>
+#endif
 #include_next <sys/sdt.h>
-
-#undef	DTRACE_PROBE
-#undef	DTRACE_PROBE1
-#undef	DTRACE_PROBE2
-#undef	DTRACE_PROBE3
-#undef	DTRACE_PROBE4
-
-#define	DTRACE_PROBE(name)
-#define	DTRACE_PROBE1(name, type1, arg1)
-#define	DTRACE_PROBE2(name, type1, arg1, type2, arg2)
-#define	DTRACE_PROBE3(name, type1, arg1, type2, arg2, type3, arg3)
-#define	DTRACE_PROBE4(name, type1, arg1, type2, arg2, type3, arg3, type4, arg4)
 
 #define	SET_ERROR(err)	(err)
 
