cecbd459ab081810353e8a6043d3ba0612d28891
Andriy Gapon <avg@FreeBSD.org>
2014-02-17 17:43:13 +0000

    MFC r254591,255753: Enhance the ZFS vdev layer to maintain both a logical and a physical minimum allocation size for devices
    Missed userland bits.

--- cddl/contrib/opensolaris/lib/libzpool/common/kernel.c
+++ cddl/contrib/opensolaris/lib/libzpool/common/kernel.c
@@ -626,6 +626,12 @@ dprintf_setup(int *argc, char **argv)
 		dprintf_print_all = 1;
 }
 
+int
+sysctl_handle_64(SYSCTL_HANDLER_ARGS)
+{
+	return (0);
+}
+
 /*
  * =========================================================================
  * debug printfs
