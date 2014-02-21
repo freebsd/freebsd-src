72f83d178b9c99ef32feb205f3fdd91ac7e6c391
bapt <bapt@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-02-21 16:14:40 +0000

    Fix build on i386
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@262298 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/dma/spool.c
+++ contrib/dma/spool.c
@@ -415,7 +415,7 @@ flushqueue_since(unsigned int period)
 		return (0);
 
 	/* Did the flush file get touched within the last period seconds? */
-	if (st.st_mtim.tv_sec + period >= now.tv_sec)
+	if (st.st_mtim.tv_sec + (int)period >= now.tv_sec)
 		return (1);
 	else
 		return (0);
