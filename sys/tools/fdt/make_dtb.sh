f635034e31026610f780a9a425f49244854d8265
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-02-28 22:37:35 +0000

    Use proper include path for dtc as well as cpp.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@262626 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/tools/fdt/make_dtb.sh
+++ sys/tools/fdt/make_dtb.sh
@@ -8,4 +8,4 @@ dts=$2
 dtb=$3
 
 cpp -x assembler-with-cpp -I $S/gnu/dts/include -I $S/boot/fdt/dts/${MACHINE} -I $S/gnu/dts/${MACHINE} -include $dts /dev/null | 
-	dtc -O dtb -o $dtb -b 0 -p 1024 -i $S/boot/fdt/dts -i $S/gnu/dts/${MACHINE}
+	dtc -O dtb -o $dtb -b 0 -p 1024 -i $S/boot/fdt/dts/${MACHINE} -i $S/gnu/dts/${MACHINE}
