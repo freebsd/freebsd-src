45b1c8bbda1a9a980553831679145e8b2f50c6ae
Ian Lepore <ian@FreeBSD.org>
2014-03-02 21:25:32 +0000

    Add __attribute__((used)) so that the delay implementation doesn't get optimized away as unreferenced, causing linker errors when trying to resolve the weak reference to the missing function.

--- sys/arm/arm/mpcore_timer.c
+++ sys/arm/arm/mpcore_timer.c
@@ -370,6 +370,7 @@ DRIVER_MODULE(mp_tmr, simplebus, arm_tmr_driver, arm_tmr_devclass, 0, 0);
  *	nothing
  */
 static void
+__attribute__((used)) /* Must emit function code for the weak ref below. */
 arm_tmr_DELAY(int usec)
 {
 	int32_t counts_per_usec;
