c914f59ac83054f99ae3306072894a81634d34c0
glebius <glebius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-03-17 11:58:48 +0000

    Make talk(1) capable of displaying UTF-8 characters.
    Sponsored by:	Nginx, Inc.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@263264 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/talk/display.c
+++ usr.bin/talk/display.c
@@ -41,8 +41,13 @@ static const char sccsid[] = "@(#)display.c	8.1 (Berkeley) 6/6/93";
  */
 #include <ctype.h>
 #include <unistd.h>
+#include <wctype.h>
+#define _XOPEN_SOURCE_EXTENDED
+#include <curses.h>
 
 #include "talk.h"
+
+void	display(xwin_t *, wchar_t *);
 
 xwin_t	my_win;
 xwin_t	his_win;
@@ -61,111 +66,130 @@ max(int a, int b)
 	return (a > b ? a : b);
 }
 
+static cchar_t *
+makecchar(wchar_t in)
+{
+	static cchar_t cc;
+	wchar_t wc[2];
+
+	wc[0] = in;
+	wc[1] = L'\0';
+
+	if (setcchar(&cc, wc, A_NORMAL, 0, NULL) != OK)
+		p_error("settchar(3) failure");
+
+	return (&cc);
+}
+
 /*
- * Display some text on somebody's window, processing some control
+ * Display a symbol on somebody's window, processing some control
  * characters while we are at it.
  */
 void
-display(xwin_t *win, char *text, int size)
+display(xwin_t *win, wchar_t *wc)
 {
-	int i;
-	char cch;
 
-	for (i = 0; i < size; i++) {
-		if (*text == '\n' || *text == '\r') {
-			waddch(win->x_win, '\n');
-			getyx(win->x_win, win->x_line, win->x_col);
-			text++;
-			continue;
-		}
-		if (*text == 004 && win == &my_win) {
-			/* control-D clears the screen */
+	/*
+	 * Alas, can't use variables in C switch statement.
+	 * Workaround these 3 cases with goto.
+	 */
+	if (*wc == win->kill)
+		goto kill;
+	else if (*wc == win->cerase)
+		goto cerase;
+	else if (*wc == win->werase)
+		goto werase;
+
+	switch (*wc) {
+	case L'\n':
+	case L'\r':
+		wadd_wch(win->x_win, makecchar(L'\n'));
+		getyx(win->x_win, win->x_line, win->x_col);
+		wrefresh(win->x_win);
+		return;
+
+	case 004:
+		if (win == &my_win) {
+			/* Ctrl-D clears the screen. */
 			werase(my_win.x_win);
 			getyx(my_win.x_win, my_win.x_line, my_win.x_col);
 			wrefresh(my_win.x_win);
 			werase(his_win.x_win);
 			getyx(his_win.x_win, his_win.x_line, his_win.x_col);
 			wrefresh(his_win.x_win);
-			text++;
-			continue;
 		}
+		return;
 
-		/* erase character */
-		if (   *text == win->cerase
-		    || *text == 010     /* BS */
-		    || *text == 0177    /* DEL */
-		   ) {
-			wmove(win->x_win, win->x_line, max(--win->x_col, 0));
-			getyx(win->x_win, win->x_line, win->x_col);
-			waddch(win->x_win, ' ');
-			wmove(win->x_win, win->x_line, win->x_col);
-			getyx(win->x_win, win->x_line, win->x_col);
-			text++;
-			continue;
-		}
+	/* Erase character. */
+	case 010:	/* BS */
+	case 0177:	/* DEL */
+cerase:
+		wmove(win->x_win, win->x_line, max(--win->x_col, 0));
+		getyx(win->x_win, win->x_line, win->x_col);
+		waddch(win->x_win, ' ');
+		wmove(win->x_win, win->x_line, win->x_col);
+		getyx(win->x_win, win->x_line, win->x_col);
+		wrefresh(win->x_win);
+		return;
+
+	case 027:	/* ^W */
+werase:
+	    {
 		/*
 		 * On word erase search backwards until we find
 		 * the beginning of a word or the beginning of
 		 * the line.
 		 */
-		if (   *text == win->werase
-		    || *text == 027     /* ^W */
-		   ) {
-			int endcol, xcol, ii, c;
+		int endcol, xcol, c;
 
-			endcol = win->x_col;
-			xcol = endcol - 1;
-			while (xcol >= 0) {
-				c = readwin(win->x_win, win->x_line, xcol);
-				if (c != ' ')
-					break;
-				xcol--;
-			}
-			while (xcol >= 0) {
-				c = readwin(win->x_win, win->x_line, xcol);
-				if (c == ' ')
-					break;
-				xcol--;
-			}
-			wmove(win->x_win, win->x_line, xcol + 1);
-			for (ii = xcol + 1; ii < endcol; ii++)
-				waddch(win->x_win, ' ');
-			wmove(win->x_win, win->x_line, xcol + 1);
-			getyx(win->x_win, win->x_line, win->x_col);
-			text++;
-			continue;
+		endcol = win->x_col;
+		xcol = endcol - 1;
+		while (xcol >= 0) {
+			c = readwin(win->x_win, win->x_line, xcol);
+			if (c != ' ')
+				break;
+			xcol--;
 		}
-		/* line kill */
-		if (   *text == win->kill
-		    || *text == 025     /* ^U */
-		   ) {
-			wmove(win->x_win, win->x_line, 0);
-			wclrtoeol(win->x_win);
-			getyx(win->x_win, win->x_line, win->x_col);
-			text++;
-			continue;
+		while (xcol >= 0) {
+			c = readwin(win->x_win, win->x_line, xcol);
+			if (c == ' ')
+				break;
+			xcol--;
 		}
-		if (*text == '\f') {
-			if (win == &my_win)
-				wrefresh(curscr);
-			text++;
-			continue;
-		}
-		if (*text == '\7') {
-			write(STDOUT_FILENO, text, 1);
-			text++;
-			continue;
-		}
-		if (!isprint((unsigned char)*text) && *text != '\t') {
-			waddch(win->x_win, '^');
-			getyx(win->x_win, win->x_line, win->x_col);
-			cch = (*text & 63) + 64;
-			waddch(win->x_win, cch);
-		} else
-			waddch(win->x_win, (unsigned char)*text);
+		wmove(win->x_win, win->x_line, xcol + 1);
+		for (int i = xcol + 1; i < endcol; i++)
+			waddch(win->x_win, ' ');
+		wmove(win->x_win, win->x_line, xcol + 1);
 		getyx(win->x_win, win->x_line, win->x_col);
-		text++;
+		wrefresh(win->x_win);
+		return;
+	    }
+
+	case 025:	/* ^U */
+kill:
+		wmove(win->x_win, win->x_line, 0);
+		wclrtoeol(win->x_win);
+		getyx(win->x_win, win->x_line, win->x_col);
+		wrefresh(win->x_win);
+		return;
+
+	case L'\f':
+		if (win == &my_win)
+			wrefresh(curscr);
+		return;
+
+	case L'\7':
+		write(STDOUT_FILENO, wc, sizeof(*wc));
+		return;
 	}
+
+
+	if (iswprint(*wc) || *wc == L'\t')
+		wadd_wch(win->x_win, makecchar(*wc));
+	else
+		beep();
+
+	getyx(win->x_win, win->x_line, win->x_col);
 	wrefresh(win->x_win);
 }
 
