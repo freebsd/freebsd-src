00379e5252fd90c3228d22d82779b74fd04dbb07
Konstantin Belousov <kib@FreeBSD.org>
2014-03-19 13:09:17 +0000

    MFC r263095: Initialize paddr to handle the case of zero size.

--- sys/vm/device_pager.c
+++ sys/vm/device_pager.c
@@ -412,6 +412,7 @@ old_dev_pager_ctor(void *handle, vm_ooffset_t size, vm_prot_t prot,
 	 * XXX assumes VM_PROT_* == PROT_*
 	 */
 	npages = OFF_TO_IDX(size);
+	paddr = 0; /* Make paddr initialized for the case of size == 0. */
 	for (off = foff; npages--; off += PAGE_SIZE) {
 		if (csw->d_mmap(dev, off, &paddr, (int)prot, &dummy) != 0) {
 			dev_relthread(dev, ref);
