a0f99e6fc99e8095a750256681e75e72d53e00be
Dag-Erling Smørgrav <des@FreeBSD.org>
2014-03-20 10:56:05 +0000

    MFH (r254407, r254960, r255371): misc cleanup MFH (r255386): make libssh private MFH (r255369, r255376, r255393, r262530): import OpenPAM Nummularia

--- contrib/openpam/bin/openpam_dump_policy/openpam_dump_policy.c
+++ contrib/openpam/bin/openpam_dump_policy/openpam_dump_policy.c
@@ -6,8 +6,7 @@
  * modification, are permitted provided that the following conditions
  * are met:
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer
- *    in this position and unchanged.
+ *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
@@ -27,7 +26,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: openpam_dump_policy.c 582 2012-04-06 23:23:35Z des $
+ * $Id: openpam_dump_policy.c 648 2013-03-05 17:54:27Z des $
  */
 
 #ifdef HAVE_CONFIG_H
@@ -43,6 +42,7 @@
 #include <security/pam_appl.h>
 
 #include "openpam_impl.h"
+#include "openpam_asprintf.h"
 
 static char *
 openpam_chain_name(const char *service, pam_facility_t fclt)
