360594422619af8da8740c39f5df01d8a1c3df8b
Hans Petter Selasky <hselasky@FreeBSD.org>
2014-03-27 07:06:29 +0000

    MFC r263423: Try to resolve a possible deadlock when detaching USB devices which create character devices. The deadlock can happen if an application is issuing IOCTLs which require USB refcounting, at the same time the USB device is detaching.
    There is already a counter in place in the USB device structure to
    detect this situation, but it was not always checked ahead of invoking
    functions that might destroy character devices, like detach, set
    configuration, set alternate interface or detach active kernel driver.

--- sys/dev/usb/usb_process.h
+++ sys/dev/usb/usb_process.h
@@ -79,6 +79,7 @@ void	usb_proc_mwait(struct usb_process *up, void *pm0, void *pm1);
 void	usb_proc_free(struct usb_process *up);
 void   *usb_proc_msignal(struct usb_process *up, void *pm0, void *pm1);
 void	usb_proc_rewakeup(struct usb_process *up);
+int	usb_proc_is_called_from(struct usb_process *up);
 
 void	usb_proc_explore_mwait(struct usb_device *, void *, void *);
 void   *usb_proc_explore_msignal(struct usb_device *, void *, void *);
