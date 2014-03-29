3dd17d8d4af870f56d7d367ed54eb8a75325681b
dim <dim@FreeBSD.org>
2014-03-29 00:25:27 +0000

    Make clang default to DWARF2 debug info format for FreeBSD 10.x and earlier.  For head, this commit does not change anything, but it is purely meant to be MFC'd.
    MFC after:	3 days

--- contrib/llvm/tools/clang/lib/Driver/Tools.cpp
+++ contrib/llvm/tools/clang/lib/Driver/Tools.cpp
@@ -2635,8 +2635,10 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
       CmdArgs.push_back("-gdwarf-4");
     else if (!A->getOption().matches(options::OPT_g0) &&
              !A->getOption().matches(options::OPT_ggdb0)) {
-      // Default is dwarf-2 for darwin.
-      if (getToolChain().getTriple().isOSDarwin())
+      // Default is dwarf-2 for darwin and FreeBSD <= 10.
+      const llvm::Triple &Triple = getToolChain().getTriple();
+      if (Triple.isOSDarwin() || (Triple.getOS() == llvm::Triple::FreeBSD &&
+          Triple.getOSMajorVersion() <= 10))
         CmdArgs.push_back("-gdwarf-2");
       else
         CmdArgs.push_back("-g");
