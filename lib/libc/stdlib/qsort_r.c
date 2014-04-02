bb1dc35f8afdcec56318d9716494c99ee77c001f
theraven <theraven@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-04-02 16:07:48 +0000

    Add support for some block functions that come from OS X.  These are intended to build with any C compiler.
    Reviewed by:	pfg
    MFC after:	3 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@264042 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/stdlib/qsort_r.c
+++ lib/libc/stdlib/qsort_r.c
@@ -4,5 +4,15 @@
  *
  * $FreeBSD$
  */
+#include "block_abi.h"
 #define I_AM_QSORT_R
 #include "qsort.c"
+
+void
+qsort_b(void *base, size_t nel, size_t width,
+	DECLARE_BLOCK(int, compar, const void *, const void *))
+{
+	qsort_r(base, nel, width, compar,
+		(int (*)(void *, const void *, const void *))
+		GET_BLOCK_FUNCTION(compar));
+}
