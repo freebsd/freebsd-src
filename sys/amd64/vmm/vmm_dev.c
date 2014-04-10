2ac5c08506950e13b16ebb12e7a13d4bdf51ee38
grehan <grehan@FreeBSD.org>
2014-04-10 19:15:58 +0000

    Rework r264179.
    - remove redundant code
    - remove erroneous setting of the error return
      in vmmdev_ioctl()
    - use style(9) initialization
    - in vmx_inject_pir(), document the race condition
      that the final conditional statement was detecting,
    
    Tested with both gcc and clang builds.
    
    Reviewed by:	neel

--- sys/amd64/vmm/vmm_dev.c
+++ sys/amd64/vmm/vmm_dev.c
@@ -171,6 +171,7 @@ vmmdev_ioctl(struct cdev *cdev, u_long cmd, caddr_t data, int fflag,
 	if (sc == NULL)
 		return (ENXIO);
 
+	error = 0;
 	vcpu = -1;
 	state_changed = 0;
 
@@ -231,7 +232,6 @@ vmmdev_ioctl(struct cdev *cdev, u_long cmd, caddr_t data, int fflag,
 		break;
 
 	default:
-		error = EINVAL;
 		break;
 	}
 
