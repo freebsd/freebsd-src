b43b23c140b2fb57bc4e452635456b59f31b0191
nwhitehorn <nwhitehorn@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-04-13 06:30:02 +0000

    Add my copyright here. Most of this is unmodified from the original sparc64 version, but at least some indication of changes that postdate the actual invention of EFI is probably a good idea.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@264404 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/boot/amd64/boot1.efi/boot1.c
+++ sys/boot/amd64/boot1.efi/boot1.c
@@ -3,6 +3,8 @@
  * All rights reserved.
  * Copyright (c) 2001 Robert Drehmel
  * All rights reserved.
+ * Copyright (c) 2014 Nathan Whitehorn
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms are freely
  * permitted provided that the above copyright notice and this
