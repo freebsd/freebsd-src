52bb90a3f450f0ee1e045c53c61e1e4407dcfc91
gjb <gjb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-04-20 20:46:06 +0000

    Apply a slightly different color hammer to arm/release.sh:
     - gcc(1) fails to build usr.bin/dtc
     - lack of WITH_GNUCXX=1 causes cc1plus(1) calls to fail
     - u-boot fails to build with clang (hard-coded gcc(1) calls)
    
    Implement the proper incantation of WITH_/WITHOUT_ knobs
    to get arm snapshot builds working again.
    
    Since the cc(1) binary is no longer expected to be clang(1),
    remove the chroot(8) post-install cc(1) overwrite.
    
    MFC After:	3 days
    X-MFC-With:	r264518,r264697,r264698
    Tested on:	stable/10@r264677 RPI-B
    Sponsored by:	The FreeBSD Foundation
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@264703 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/arm/release.sh
+++ release/arm/release.sh
@@ -92,14 +92,9 @@ install_uboot() {
 }
 
 main() {
-	# Build gcc for use in the chroot for arm builds.
-	# This is not '-j'-safe, so force '-j1' to allow using
-	# additional, non-'-j' options specified in WORLD_FLAGS.
-	eval chroot ${CHROOTDIR} make -C /usr/src/gnu/usr.bin/cc \
-		WITH_GCC=1 ${WORLD_FLAGS} -j1 obj depend all install
 	# Build the 'xdev' target for crochet.
 	eval chroot ${CHROOTDIR} make -C /usr/src \
-		WITHOUT_CLANG_IS_CC=1 WITHOUT_CLANG=1 WITH_GCC=1 \
+		WITH_GCC=1 WITH_GNUCXX=1 WITHOUT_CLANG_IS_CC=1 \
 		XDEV=${XDEV} XDEV_ARCH=${XDEV_ARCH} \
 		${WORLD_FLAGS} xdev
 
