11b0c75cd6ec4be6cac4a1bd2b74ddf111ea5341
gjb <gjb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-04-25 01:55:14 +0000

    Style cleanup  - Indent 1 full tab where needed  - Use $() for shell exec  - Insert a space between '$(( ))' parens
    MFC After:	1 week
    X-MFC-With:	r264907
    Sponsored by:	The FreeBSD Foundation
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@264908 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- release/i386/make-memstick.sh
+++ release/i386/make-memstick.sh
@@ -16,28 +16,28 @@ export PATH
 BLOCKSIZE=10240
 
 if [ $# -ne 2 ]; then
-  echo "make-memstick.sh /path/to/directory /path/to/image/file"
-  exit 1
+	echo "make-memstick.sh /path/to/directory /path/to/image/file"
+	exit 1
 fi
 
 tempfile="${2}.$$"
 
 if [ ! -d ${1} ]; then
-  echo "${1} must be a directory"
-  exit 1
+	echo "${1} must be a directory"
+	exit 1
 fi
 
 if [ -e ${2} ]; then
-  echo "won't overwrite ${2}"
-  exit 1
+	echo "won't overwrite ${2}"
+	exit 1
 fi
 
 rm -f ${tempfile}
 echo "/dev/gpt/rootfs / ufs ro,noatime 1 1" > ${1}/etc/fstab
 makefs ${tempfile} ${1}
 if [ $? -ne 0 ]; then
-  echo "makefs failed"
-  exit 1
+	echo "makefs failed"
+	exit 1
 fi
 rm ${1}/etc/fstab
 
@@ -47,45 +47,45 @@ rm ${1}/etc/fstab
 # division and to provide ample padding.
 #
 
-filesize=`stat -f "%z" ${tempfile}`
-blocks=$(($filesize / ${BLOCKSIZE} + 520 ))
+filesize=$(stat -f "%z" ${tempfile})
+blocks=$(( ${filesize} / ${BLOCKSIZE} + 520 ))
 dd if=/dev/zero of=${2} bs=${BLOCKSIZE} count=${blocks}
 if [ $? -ne 0 ]; then
-  echo "creation of image file failed"
-  exit 1
+	echo "creation of image file failed"
+	exit 1
 fi
 
 unit=`mdconfig -a -t vnode -f ${2}`
 if [ $? -ne 0 ]; then
-  echo "mdconfig failed"
-  exit 1
+	echo "mdconfig failed"
+	exit 1
 fi
 
 gpart create -s gpt /dev/${unit}
 if [ $? -ne 0 ]; then
-  echo "GPT creation failed"
-  exit 1
+	echo "GPT creation failed"
+	exit 1
 fi
 gpart add -t freebsd-boot -s 512k /dev/${unit}
 if [ $? -ne 0 ]; then
-  echo "Creating boot partition failed"
-  exit 1
+	echo "Creating boot partition failed"
+	exit 1
 fi
 gpart bootcode -b ${1}/boot/pmbr -p ${1}/boot/gptboot -i 1 /dev/${unit}
 if [ $? -ne 0 ]; then
-  echo "Writing bootcode failed"
-  exit 1
+	echo "Writing bootcode failed"
+	exit 1
 fi
 gpart add -t freebsd-ufs -l rootfs /dev/${unit}
 if [ $? -ne 0 ]; then
-  echo "Creating UFS partition failed"
-  exit 1
+	echo "Creating UFS partition failed"
+	exit 1
 fi
 
 dd if=${tempfile} of=/dev/${unit}p2 bs=$BLOCKSIZE conv=sync
 if [ $? -ne 0 ]; then
-  echo "copying filesystem into image file failed"
-  exit 1
+	echo "copying filesystem into image file failed"
+	exit 1
 fi
 
 mdconfig -d -u ${unit}
