a7fa939bb3564122505b0b6b66fdf00e13bffe01
Ian Lepore <ian@FreeBSD.org>
2014-04-26 16:48:09 +0000

    Stop calling imx51_ccm_foo() clock functions from imx6 code.  Instead define a few imx_ccm_foo() functions that are implemented by the imx51 or imx6 ccm code.  Of course, the imx6 ccm code is still more a wish than reality, so for now its implementations just return hard-coded numbers.

--- sys/arm/freescale/imx/imx_ccmvar.h
+++ sys/arm/freescale/imx/imx_ccmvar.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2013 Ian Lepore <ian@freebsd.org>
+ * Copyright (c) 2014 Ian Lepore <ian@freebsd.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,35 +26,29 @@
  * $FreeBSD$
  */
 
-#ifndef	IMX_MACHDEP_H
-#define	IMX_MACHDEP_H
-
-#include <sys/types.h>
-
-/* Common functions, implemented in imx_machdep.c. */
-
-void imx_wdog_cpu_reset(vm_offset_t _wdcr_phys)  __attribute__((__noreturn__));
-
-/* From here down, routines are implemented in imxNN_machdep.c. */
+#ifndef	IMX_CCMVAR_H
+#define	IMX_CCMVAR_H
 
 /*
- * SoC identity.
- * According to the documentation, there is such a thing as an i.MX6 Dual
- * (non-lite flavor).  However, Freescale doesn't seem to have assigned it a
- * number in their code for determining the SoC type in u-boot.
+ * We need a clock management system that works across unrelated SoCs and
+ * devices.  For now, to keep imx development moving, define some barebones
+ * functionality that can be shared within the imx family by having each SoC
+ * implement functions with a common name.
  *
- * To-do: put silicon revision numbers into the low-order bits somewhere.
+ * The usb enable functions are best-effort.  They turn on the usb otg, host,
+ * and phy clocks in a SoC-specific manner, but it may take a lot more than that
+ * to make usb work on a given board.  In particular, it can require specific
+ * pinmux setup of gpio pins connected to external phy parts, voltage regulators
+ * and overcurrent detectors, and so on.  On such boards, u-boot or other early
+ * board setup code has to handle those things.
  */
-#define	IMXSOC_51	0x51000000
-#define	IMXSOC_53	0x53000000
-#define	IMXSOC_6SL	0x60000000
-#define	IMXSOC_6DL	0x61000000
-#define	IMXSOC_6S	0x62000000
-#define	IMXSOC_6Q	0x63000000
-#define	IMXSOC_FAMSHIFT	28
 
-u_int imx_soc_type(void);
-u_int imx_soc_family(void);
+uint32_t imx_ccm_ipg_hz(void);
+uint32_t imx_ccm_perclk_hz(void);
+uint32_t imx_ccm_sdhci_hz(void);
+uint32_t imx_ccm_uart_hz(void);
+
+void imx_ccm_usb_enable(device_t _usbdev);
+void imx_ccm_usbphy_enable(device_t _phydev);
 
 #endif
-
