2608a678b9064f07bd27c052a8154ab1c67f5098
adrian <adrian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-04-30 02:19:41 +0000

    Bring over some initial power save management support, reset path fixes and beacon programming / debugging into the ath(4) driver.
    The basic power save tracking:
    
    * Add some new code to track the current desired powersave state; and
    * Add some reference count tracking so we know when the NIC is awake; then
    * Add code in all the points where we're about to touch the hardware and
      push it to force-wake.
    
    Then, how things are moved into power save:
    
    * Only move into network-sleep during a RUN->SLEEP transition;
    * Force wake the hardware up everywhere that we're about to touch
      the hardware.
    
    The net80211 stack takes care of doing RUN<->SLEEP<->(other) state
    transitions so we don't have to do it in the driver.
    
    Next, when to wake things up:
    
    * In short - everywhere we touch the hardware.
    * The hardware will take care of staying awake if things are queued
      in the transmit queue(s); it'll then transit down to sleep if
      there's nothing left.  This way we don't have to track the
      software / hardware transmit queue(s) and keep the hardware
      awake for those.
    
    Then, some transmit path fixes that aren't related but useful:
    
    * Force EAPOL frames to go out at the lowest rate.  This improves
      reliability during the encryption handshake after 802.11
      negotiation.
    
    Next, some reset path fixes!
    
    * Fix the overlap between reset and transmit pause so we don't
      transmit frames during a reset.
    * Some noisy environments will end up taking a lot longer to reset
      than normal, so extend the reset period and drop the raise the
      reset interval to be more realistic and give the hardware some
      time to finish calibration.
    * Skip calibration during the reset path.  Tsk!
    
    Then, beacon fixes in station mode!
    
    * Add a _lot_ more debugging in the station beacon reset path.
      This is all quite fluid right now.
    * Modify the STA beacon programming code to try and take
      the TU gap between desired TSF and the target TU into
      account.  (Lifted from QCA.)
    
    Tested:
    
    * AR5210
    * AR5211
    * AR5212
    * AR5413
    * AR5416
    * AR9280
    * AR9285
    
    TODO:
    
    * More AP, IBSS, mesh, TDMA testing
    * Thorough AR9380 and later testing!
    * AR9160 and AR9287 testing
    
    Obtained from:	QCA
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@265115 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ath/if_ath_rx.c
+++ sys/dev/ath/if_ath_rx.c
@@ -166,10 +166,22 @@ ath_calcrxfilter(struct ath_softc *sc)
 	/* XXX ic->ic_monvaps != 0? */
 	if (ic->ic_opmode == IEEE80211_M_MONITOR || (ifp->if_flags & IFF_PROMISC))
 		rfilt |= HAL_RX_FILTER_PROM;
+
+	/*
+	 * Only listen to all beacons if we're scanning.
+	 *
+	 * Otherwise we only really need to hear beacons from
+	 * our own BSSID.
+	 */
 	if (ic->ic_opmode == IEEE80211_M_STA ||
-	    ic->ic_opmode == IEEE80211_M_IBSS ||
-	    sc->sc_swbmiss || sc->sc_scanning)
-		rfilt |= HAL_RX_FILTER_BEACON;
+	    ic->ic_opmode == IEEE80211_M_IBSS || sc->sc_swbmiss) {
+		if (sc->sc_do_mybeacon && ! sc->sc_scanning) {
+			rfilt |= HAL_RX_FILTER_MYBEACON;
+		} else { /* scanning, non-mybeacon chips */
+			rfilt |= HAL_RX_FILTER_BEACON;
+		}
+	}
+
 	/*
 	 * NB: We don't recalculate the rx filter when
 	 * ic_protmode changes; otherwise we could do
@@ -317,6 +329,15 @@ ath_recv_mgmt(struct ieee80211_node *ni, struct mbuf *m,
 {
 	struct ieee80211vap *vap = ni->ni_vap;
 	struct ath_softc *sc = vap->iv_ic->ic_ifp->if_softc;
+	uint64_t tsf_beacon_old, tsf_beacon;
+	uint64_t nexttbtt;
+	int64_t tsf_delta;
+	int32_t tsf_delta_bmiss;
+	int32_t tsf_remainder;
+	uint64_t tsf_beacon_target;
+
+	tsf_beacon_old = ((uint64_t) LE_READ_4(ni->ni_tstamp.data + 4)) << 32;
+	tsf_beacon_old |= LE_READ_4(ni->ni_tstamp.data);
 
 	/*
 	 * Call up first so subsequent work can use information
@@ -328,14 +349,83 @@ ath_recv_mgmt(struct ieee80211_node *ni, struct mbuf *m,
 		/* update rssi statistics for use by the hal */
 		/* XXX unlocked check against vap->iv_bss? */
 		ATH_RSSI_LPF(sc->sc_halstats.ns_avgbrssi, rssi);
+
+		tsf_beacon = ((uint64_t) LE_READ_4(ni->ni_tstamp.data + 4)) << 32;
+		tsf_beacon |= LE_READ_4(ni->ni_tstamp.data);
+
+		nexttbtt = ath_hal_getnexttbtt(sc->sc_ah);
+
+		/*
+		 * Let's calculate the delta and remainder, so we can see
+		 * if the beacon timer from the AP is varying by more than
+		 * a few TU.  (Which would be a huge, huge problem.)
+		 */
+		tsf_delta = (long long) tsf_beacon - (long long) tsf_beacon_old;
+
+		/*
+		 * For now let's just assume the intval is 100TU, which is
+		 * 102400uS.  So, we can just calculate the remainder from
+		 * that.
+		 */
+		tsf_delta_bmiss = tsf_delta / 102400;
+
+		/*
+		 * If our delta is greater than half the beacon interval,
+		 * let's round the bmiss value up to the next beacon
+		 * interval.  Ie, we're running really, really early
+		 * on the next beacon.
+		 */
+		if (tsf_delta % 102400 > 51200)
+			tsf_delta_bmiss ++;
+
+		tsf_beacon_target = tsf_beacon_old +
+		    (((unsigned long long) tsf_delta_bmiss) * 102400ULL);
+
+		/*
+		 * The remainder using '%' is between 0 .. 102400-1.
+		 * If we're actually running too fast, then the remainder
+		 * will be some large number just under 102400-1.
+		 * So we need to look at whether we're running
+		 * before or after the target beacon interval
+		 * and if we are, modify how we do the remainder
+		 * calculation.
+		 */
+		if (tsf_beacon < tsf_beacon_target) {
+			tsf_remainder = -(102400 - ((tsf_beacon - tsf_beacon_old) % 102400));
+		} else {
+			tsf_remainder = (tsf_beacon - tsf_beacon_old) % 102400;
+		}
+
+		DPRINTF(sc, ATH_DEBUG_BEACON, "%s: old_tsf=%llu, new_tsf=%llu, target_tsf=%llu, delta=%lld, bmiss=%d, remainder=%d\n",
+		    __func__,
+		    (unsigned long long) tsf_beacon_old,
+		    (unsigned long long) tsf_beacon,
+		    (unsigned long long) tsf_beacon_target,
+		    (long long) tsf_delta,
+		    tsf_delta_bmiss,
+		    tsf_remainder);
+
+		DPRINTF(sc, ATH_DEBUG_BEACON, "%s: tsf=%llu, nexttbtt=%llu, delta=%d\n",
+		    __func__,
+		    (unsigned long long) tsf_beacon,
+		    (unsigned long long) nexttbtt,
+		    (int32_t) tsf_beacon - (int32_t) nexttbtt + 102400);
+
 		if (sc->sc_syncbeacon &&
-		    ni == vap->iv_bss && vap->iv_state == IEEE80211_S_RUN) {
+		    ni == vap->iv_bss &&
+		    (vap->iv_state == IEEE80211_S_RUN || vap->iv_state == IEEE80211_S_SLEEP)) {
+			DPRINTF(sc, ATH_DEBUG_BEACON,
+			    "%s: syncbeacon=1; syncing\n",
+			    __func__);
 			/*
 			 * Resync beacon timers using the tsf of the beacon
 			 * frame we just received.
 			 */
 			ath_beacon_config(sc, vap);
+			sc->sc_syncbeacon = 0;
 		}
+
+
 		/* fall thru... */
 	case IEEE80211_FC0_SUBTYPE_PROBE_RESP:
 		if (vap->iv_opmode == IEEE80211_M_IBSS &&
@@ -911,6 +1001,10 @@ ath_rx_proc(struct ath_softc *sc, int resched)
 	kickpcu = sc->sc_kickpcu;
 	ATH_PCU_UNLOCK(sc);
 
+	ATH_LOCK(sc);
+	ath_power_set_power_state(sc, HAL_PM_AWAKE);
+	ATH_UNLOCK(sc);
+
 	DPRINTF(sc, ATH_DEBUG_RX_PROC, "%s: called\n", __func__);
 	ngood = 0;
 	nf = ath_hal_getchannoise(ah, sc->sc_curchan);
@@ -1064,6 +1158,13 @@ rx_proc_next:
 			ath_tx_kick(sc);
 	}
 #undef PA2DESC
+
+	/*
+	 * Put the hardware to sleep again if we're done with it.
+	 */
+	ATH_LOCK(sc);
+	ath_power_restore_power_state(sc);
+	ATH_UNLOCK(sc);
 
 	/*
 	 * If we hit the maximum number of frames in this round,
