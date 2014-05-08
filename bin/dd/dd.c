5bd695fd9651dd452406074cf2dbaa39e49ccba6
asomers <asomers@FreeBSD.org>
2014-05-08 19:10:04 +0000

    Incorporate feedback from bde and jilles regarding r265472 to dd(1).
    * Don't use sysexits.h.  Just exit 1 on error and 0 otherwise.
    * Don't sacrifice precision by converting the output of clock_gettime() to a
      double and then comparing the results.  Instead, subtract the values of
      the two clock_gettime() calls, then convert to double.
    * Don't use CLOCK_MONOTONIC_PRECISE.  It's an unportable synonym for
      CLOCK_MONOTONIC.
    * Use more appropriate names for some local variables.
    * In the summary message, round elapsed time to the nearest microsecond.
    
    Reported by:	bde, jilles
    MFC after:	3 days
    X-MFC-With:	265472

--- bin/dd/dd.c
+++ bin/dd/dd.c
@@ -61,7 +61,6 @@ __FBSDID("$FreeBSD$");
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sysexits.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -126,7 +125,6 @@ static void
 setup(void)
 {
 	u_int cnt;
-	struct timespec tv;
 
 	if (in.name == NULL) {
 		in.name = "stdin";
@@ -245,9 +243,8 @@ setup(void)
 		ctab = casetab;
 	}
 
-	if (clock_gettime(CLOCK_MONOTONIC_PRECISE, &tv))
-		err(EX_OSERR, "clock_gettime");
-	st.start = tv.tv_sec + tv.tv_nsec * 1.0e-9;
+	if (clock_gettime(CLOCK_MONOTONIC, &st.start))
+		err(1, "clock_gettime");
 }
 
 static void
