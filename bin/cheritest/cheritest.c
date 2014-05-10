e45efd9e089c4b16c07f3d18ce0e101725c31677
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2014-05-10 18:45:06 +0000

    Substantial further reworking of CheriBSD's /bin/cheritest to improve it as a testing, rather than just exercise, tool:
    - Flag each test with whether it is expected to terminate with a signal, and
      if so, what signal number, and optionally, what MIPS exception code and
      CHERI exception code to expect.  If these are specified, they are tested
      for to decide if the test passed.
    
    - Specify expected signal/exception information for all tests.
    
    - Fork a new process for each test run, and set up a shared memory page,
      cheritest_child_state, used to propagate signal/exception information back
      to the parent process.
    
    - Install signal handlers for various signals in the child process and use
      that to fill out cheritest_child_state.
    
    - Provide each test with a plaintext description to be printed as tests run
      to make the results more human-friendly.
    
    - Clearly print TEST, and PASS/FAIL information when each test runs.
    
    - A special 'all' command-line argument is supported, which will run all
      available tests in order.
    
    - Resort tests in cheri_tests[] array by general function.
    
    Most tests pass, with the exception of 'invoke_spin', which times out after
    10 seconds with a timer signal, which can't be properly delivered to the
    in-sandbox stack.  This requires some further diagnosis and fixing.
    
    /bin/cheritest is now increasingly suitable to be run as part of a larger
    test environment to ensure that a range of CHERI and CheriBSD functions are
    working properly.  It could use a lot more tests, though.

--- bin/cheritest/cheritest.c
+++ bin/cheritest/cheritest.c
@@ -36,12 +36,17 @@
 #endif
 
 #include <sys/types.h>
+#include <sys/mman.h>
 #include <sys/sysctl.h>
 #include <sys/time.h>
+#include <sys/ucontext.h>
+#include <sys/wait.h>
 
 #include <machine/cheri.h>
 #include <machine/cheric.h>
 #include <machine/cpuregs.h>
+#include <machine/frame.h>
+#include <machine/trap.h>
 
 #include <cheri/cheri_fd.h>
 #include <cheri/sandbox.h>
@@ -50,6 +55,7 @@
 #include <err.h>
 #include <fcntl.h>
 #include <inttypes.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -58,116 +64,267 @@
 
 #include "cheritest.h"
 
+#define	CT_FLAG_SIGNAL		0x00000001  /* Should fault; checks signum. */
+#define	CT_FLAG_MIPS_EXCCODE	0x00000002  /* Check MIPS exception code. */
+#define	CT_FLAG_CP2_EXCCODE	0x00000004  /* Check CP2 exception code. */
+
 static const struct cheri_test {
 	const char	*ct_name;
+	const char	*ct_desc;
 	int		 ct_arg;	/* 0: ct_func; otherwise ct_func_arg. */
 	void		(*ct_func)(void);
 	void		(*ct_func_arg)(int);
+	u_int		 ct_flags;
+	int		 ct_signum;
+	register_t	 ct_mips_exccode;
+	register_t	 ct_cp2_exccode;
 } cheri_tests[] = {
-	{ .ct_name = "test_fault_creturn",
-	  .ct_func = test_fault_creturn },
-	{ .ct_name = "test_nofault_ccall_creturn",
-	  .ct_func = test_nofault_ccall_creturn },
-	{ .ct_name = "test_nofault_ccall_nop_creturn",
-	  .ct_func = test_nofault_ccall_nop_creturn },
+	/*
+	 * Exercise CHERI functions without an expectation of a signal.
+	 */
 	{ .ct_name = "copyregs",
+	  .ct_desc = "Exercise CP2 register assignments",
 	  .ct_func = cheritest_copyregs },
+
 	{ .ct_name = "listregs",
+	  .ct_desc = "Print out a list of CP2 registers and values",
 	  .ct_func = cheritest_listregs },
+
+	/*
+	 * Capability manipulation and use tests that sometimes generate
+	 * signals.
+	 */
+	/* XXXRW: Check this CP2 exception code number. */
+	{ .ct_name = "test_fault_cgetcause",
+	  .ct_desc = "Ensure CGetCause is unavailable in userspace",
+	  .ct_func = test_fault_cgetcause,
+	  .ct_flags = CT_FLAG_SIGNAL | CT_FLAG_MIPS_EXCCODE |
+		    CT_FLAG_CP2_EXCCODE,
+	  .ct_signum = SIGPROT,
+	  .ct_mips_exccode = T_C2E,
+	  .ct_cp2_exccode = CHERI_EXCCODE_ACCESS_EPCC },
+
+	{ .ct_name = "test_fault_overrun",
+	  .ct_desc = "Exercise capability bounds check failure",
+	  .ct_func = test_fault_overrun,
+	  .ct_flags = CT_FLAG_SIGNAL | CT_FLAG_MIPS_EXCCODE |
+		    CT_FLAG_CP2_EXCCODE,
+	  .ct_signum = SIGPROT,
+	  .ct_mips_exccode = T_C2E,
+	  .ct_cp2_exccode = CHERI_EXCCODE_LENGTH },
+
+	{ .ct_name = "test_fault_ccheck_user_fail",
+	  .ct_desc = "Exercise CCheckPerm failure",
+	  .ct_func = test_fault_ccheck_user_fail,
+	  .ct_flags = CT_FLAG_SIGNAL | CT_FLAG_MIPS_EXCCODE |
+		    CT_FLAG_CP2_EXCCODE,
+	  .ct_signum = SIGPROT,
+	  .ct_mips_exccode = T_C2E,
+	  .ct_cp2_exccode = CHERI_EXCCODE_USER_PERM },
+
+	{ .ct_name = "test_nofault_ccheck_user_pass",
+	  .ct_desc = "Exercise CCheckPerm success",
+	  .ct_func = test_nofault_ccheck_user_pass },
+
+	{ .ct_name = "test_fault_read_kr1c",
+	  .ct_desc = "Ensure KR1C is unavailable in userspace",
+	  .ct_func = test_fault_read_kr1c,
+	  .ct_flags = CT_FLAG_SIGNAL | CT_FLAG_MIPS_EXCCODE |
+		    CT_FLAG_CP2_EXCCODE,
+	  .ct_signum = SIGPROT,
+	  .ct_mips_exccode = T_C2E,
+	  .ct_cp2_exccode = CHERI_EXCCODE_ACCESS_KR1C },
+
+	{ .ct_name = "test_fault_read_kr2c",
+	  .ct_desc = "Ensure KR2C is unavailable in userspace",
+	  .ct_func = test_fault_read_kr2c,
+	  .ct_flags = CT_FLAG_SIGNAL | CT_FLAG_MIPS_EXCCODE |
+		    CT_FLAG_CP2_EXCCODE,
+	  .ct_signum = SIGPROT,
+	  .ct_mips_exccode = T_C2E,
+	  .ct_cp2_exccode = CHERI_EXCCODE_ACCESS_KR2C },
+
+	{ .ct_name = "test_fault_read_kcc",
+	  .ct_desc = "Ensure KCC is unavailable in userspace",
+	  .ct_func = test_fault_read_kcc,
+	  .ct_flags = CT_FLAG_SIGNAL | CT_FLAG_MIPS_EXCCODE |
+		    CT_FLAG_CP2_EXCCODE,
+	  .ct_signum = SIGPROT,
+	  .ct_mips_exccode = T_C2E,
+	  .ct_cp2_exccode = CHERI_EXCCODE_ACCESS_KCC },
+
+	{ .ct_name = "test_fault_read_kdc",
+	  .ct_desc = "Ensure KDC is unavailable in userspace",
+	  .ct_func = test_fault_read_kdc,
+	  .ct_flags = CT_FLAG_SIGNAL | CT_FLAG_MIPS_EXCCODE |
+		    CT_FLAG_CP2_EXCCODE,
+	  .ct_signum = SIGPROT,
+	  .ct_mips_exccode = T_C2E,
+	  .ct_cp2_exccode = CHERI_EXCCODE_ACCESS_KDC },
+
+	{ .ct_name = "test_fault_read_epcc",
+	  .ct_desc = "Ensure EPCC is unavailable in userspace",
+	  .ct_func = test_fault_read_epcc,
+	  .ct_flags = CT_FLAG_SIGNAL | CT_FLAG_MIPS_EXCCODE |
+		    CT_FLAG_CP2_EXCCODE,
+	  .ct_signum = SIGPROT,
+	  .ct_mips_exccode = T_C2E,
+	  .ct_cp2_exccode = CHERI_EXCCODE_ACCESS_EPCC },
+
+	/*
+	 * Simple CCall/CReturn tests that sometimes generate signals.
+	 */
+	{ .ct_name = "test_fault_creturn",
+	  .ct_desc = "Exercise trusted stack underflow",
+	  .ct_func = test_fault_creturn,
+	  .ct_flags = CT_FLAG_SIGNAL | CT_FLAG_MIPS_EXCCODE |
+		    CT_FLAG_CP2_EXCCODE,
+	  .ct_signum = SIGPROT,
+	  .ct_mips_exccode = T_C2E,
+	  .ct_cp2_exccode = CHERI_EXCCODE_RETURN },
+
+	{ .ct_name = "test_nofault_ccall_creturn",
+	  .ct_desc = "Exercise CCall/CReturn",
+	  .ct_func = test_nofault_ccall_creturn },
+
+	{ .ct_name = "test_nofault_ccall_nop_creturn",
+	  .ct_desc = "Exercise CCall/NOP/NOP/NOP/CReturn",
+	  .ct_func = test_nofault_ccall_nop_creturn },
+
+	/*
+	 * Test libcheri sandboxing -- and kernel sandbox unwind.
+	 */
 	{ .ct_name = "invoke_abort",
+	  .ct_desc = "Exercise system call in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_ABORT },
+
 	{ .ct_name = "invoke_clock_gettime",
+	  .ct_desc = "Exercise clock_gettime() in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_CS_CLOCK_GETTIME },
+
 	{ .ct_name = "invoke_cp2_bound",
+	  .ct_desc = "Exercise capability bounds check failure in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_CP2_BOUND },
+
 	{ .ct_name = "invoke_cp2_perm",
+	  .ct_desc = "Exercise capability permissions failure in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_CP2_PERM },
+
 	{ .ct_name = "invoke_cp2_tag",
+	  .ct_desc = "Exercise capability tag failure in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_CP2_TAG },
+
 	{ .ct_name = "invoke_cp2_seal",
+	  .ct_desc = "Exercise capability seal failure in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_CP2_SEAL },
+
 	{ .ct_name = "invoke_divzero",
+	  .ct_desc = "Exercise divide-by-zero in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_DIVZERO },
-	{ .ct_name = "invoke_fd_fstat_c",
-	  .ct_func_arg = cheritest_invoke_fd_op,
-	  .ct_arg = CHERITEST_HELPER_OP_FD_FSTAT_C },
-	{ .ct_name = "invoke_fd_lseek_c",
-	  .ct_func_arg = cheritest_invoke_fd_op,
-	  .ct_arg = CHERITEST_HELPER_OP_FD_LSEEK_C },
-	{ .ct_name = "invoke_fd_read_c",
-	  .ct_func_arg = cheritest_invoke_fd_op,
-	  .ct_arg = CHERITEST_HELPER_OP_FD_READ_C },
-	{ .ct_name = "invoke_fd_write_c",
-	  .ct_func_arg = cheritest_invoke_fd_op,
-	  .ct_arg = CHERITEST_HELPER_OP_FD_WRITE_C },
+
 	{ .ct_name = "invoke_helloworld",
+	  .ct_desc = "Print 'hello world' in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_CS_HELLOWORLD },
+
 	{ .ct_name = "invoke_md5",
+	  .ct_desc = "Generate an MD5 checksum in a libcheri sandbox",
 	  .ct_func = cheritest_invoke_md5 },
+
 	{ .ct_name = "invoke_malloc",
+	  .ct_desc = "Malloc memory in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_MALLOC },
+
 	{ .ct_name = "invoke_printf",
+	  .ct_desc = "printf() in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_PRINTF },
+
 	{ .ct_name = "invoke_cs_putchar",
+	  .ct_desc = "putchar() in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_CS_PUTCHAR },
+
 	{ .ct_name = "invoke_cs_puts",
+	  .ct_desc = "puts() in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_CS_PUTS },
+
 	{ .ct_name = "invoke_spin",
+	  .ct_desc = "spin in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
-	  .ct_arg = CHERITEST_HELPER_OP_SPIN },
+	  .ct_arg = CHERITEST_HELPER_OP_SPIN,
+	  .ct_flags = CT_FLAG_SIGNAL,
+	  .ct_signum = SIGALRM },
+
 	{ .ct_name = "invoke_syscall",
+	  .ct_desc = "Invoke a system call in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_SYSCALL },
+
 	{ .ct_name = "invoke_syscap",
+	  .ct_desc = "Invoke the system capability in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_SYSCAP },
+
 	{ .ct_name = "invoke_vm_rfault",
+	  .ct_desc = "Load from an unreadable page in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_VM_RFAULT },
+
 	{ .ct_name = "invoke_vm_wfault",
+	  .ct_desc = "Store to an unwritable page in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_VM_WFAULT },
+
 	{ .ct_name = "invoke_vm_xfault",
+	  .ct_desc = "Execute from an unreadable page in a libcheri sandbox",
 	  .ct_func_arg = cheritest_invoke_simple_op,
 	  .ct_arg = CHERITEST_HELPER_OP_VM_XFAULT },
+
+	/*
+	 * libcheri + cheri_fd tests.
+	 */
+	{ .ct_name = "invoke_fd_fstat_c",
+	  .ct_desc = "Exercise fstat_c() on a cheri_fd in a libcheri sandbox",
+	  .ct_func_arg = cheritest_invoke_fd_op,
+	  .ct_arg = CHERITEST_HELPER_OP_FD_FSTAT_C },
+
+	{ .ct_name = "invoke_fd_lseek_c",
+	  .ct_desc = "Exercise lseek_c() on a cheri_fd in a libcheri sandbox",
+	  .ct_func_arg = cheritest_invoke_fd_op,
+	  .ct_arg = CHERITEST_HELPER_OP_FD_LSEEK_C },
+
+	{ .ct_name = "invoke_fd_read_c",
+	  .ct_desc = "Exercise read_c() on a cheri_fd in a libcheri sandbox",
+	  .ct_func_arg = cheritest_invoke_fd_op,
+	  .ct_arg = CHERITEST_HELPER_OP_FD_READ_C },
+
+	{ .ct_name = "invoke_fd_write_c",
+	  .ct_desc = "Exercise write_c() on a cheri_fd in a libcheri sandbox",
+	  .ct_func_arg = cheritest_invoke_fd_op,
+	  .ct_arg = CHERITEST_HELPER_OP_FD_WRITE_C },
+
 	{ .ct_name = "revoke_fd",
+	  .ct_desc = "Exercise revoke_c() on a cheri_fd",
 	  .ct_func = cheritest_revoke_fd },
-	{ .ct_name = "sleep",
-	  .ct_arg = 10,
-	  .ct_func = sleep },
-	{ .ct_name = "test_fault_cgetcause",
-	  .ct_func = test_fault_cgetcause },
-	{ .ct_name = "test_fault_overrun",
-	  .ct_func = test_fault_overrun },
-	{ .ct_name = "test_fault_ccheck_user_fail",
-	  .ct_func = test_fault_ccheck_user_fail },
-	{ .ct_name = "test_nofault_ccheck_user_pass",
-	  .ct_func = test_nofault_ccheck_user_pass },
-	{ .ct_name = "test_fault_read_kr1c",
-	  .ct_func = test_fault_read_kr1c },
-	{ .ct_name = "test_fault_read_kr2c",
-	  .ct_func = test_fault_read_kr2c },
-	{ .ct_name = "test_fault_read_kcc",
-	  .ct_func = test_fault_read_kcc },
-	{ .ct_name = "test_fault_read_kdc",
-	  .ct_func = test_fault_read_kdc },
-	{ .ct_name = "test_fault_read_epcc",
-	  .ct_func = test_fault_read_epcc },
 };
 static const u_int cheri_tests_len = sizeof(cheri_tests) /
 	    sizeof(cheri_tests[0]);
+
+static struct cheritest_child_state {
+	int		ccs_signum;
+	register_t	ccs_mips_cause;
+	register_t	ccs_cp2_cause;
+} *ccsp;
 
 static void
 usage(void)
@@ -177,6 +334,123 @@ usage(void)
 	for (i = 0; i < cheri_tests_len; i++)
 		fprintf(stderr, "cheritest %s\n", cheri_tests[i].ct_name);
 	exit(EX_USAGE);
+}
+
+static void
+signal_handler(int signum, siginfo_t *info __unused, ucontext_t *uap)
+{
+	struct cheri_frame *cfp;
+
+	cfp = (struct cheri_frame *)uap->uc_mcontext.mc_cp2state;
+	if (cfp == NULL || uap->uc_mcontext.mc_cp2state_len != sizeof(*cfp)) {
+		ccsp->ccs_signum = -1;
+		_exit(EX_SOFTWARE);
+	}
+	ccsp->ccs_signum = signum;
+	ccsp->ccs_mips_cause = uap->uc_mcontext.cause;
+	ccsp->ccs_cp2_cause = cfp->cf_capcause;
+	_exit(EX_SOFTWARE);
+}
+
+static void
+cheritest_run_test(const struct cheri_test *ctp)
+{
+	struct sigaction sa;
+	pid_t childpid;
+	int status;
+	char reason[80];
+	register_t cp2_exccode, mips_exccode;
+
+	bzero(ccsp, sizeof(*ccsp));
+	printf("TEST: %s: %s\n", ctp->ct_name, ctp->ct_desc);
+
+	childpid = fork();
+	if (childpid < 0)
+		err(EX_OSERR, "fork");
+	if (childpid == 0) {
+		/* Install signal handlers. */
+		sa.sa_sigaction = signal_handler;
+		sa.sa_flags = SA_SIGINFO;
+		sigemptyset(&sa.sa_mask);
+		if (sigaction(SIGALRM, &sa, NULL) < 0)
+			err(EX_OSERR, "sigaction(SIGALRM)");
+		if (sigaction(SIGPROT, &sa, NULL) < 0)
+			err(EX_OSERR, "sigaction(SIGPROT)");
+		if (sigaction(SIGSEGV, &sa, NULL) < 0)
+			err(EX_OSERR, "sigaction(SIGSEGV)");
+		if (sigaction(SIGBUS, &sa, NULL) < 0)
+			err(EX_OSERR, "sigaction(SIGBUS");
+
+		/* Run the actual test. */
+		if (ctp->ct_arg != 0)
+			ctp->ct_func_arg(ctp->ct_arg);
+		else
+			ctp->ct_func();
+		exit(0);
+	}
+	(void)waitpid(childpid, &status, 0);
+
+	/*
+	 * Analyse child's signal state returned via shared memory.
+	 */
+	if (!WIFEXITED(status)) {
+		snprintf(reason, sizeof(reason), "Child exited abnormally");
+		goto fail;
+	}
+	if (WEXITSTATUS(status) != 0 && WEXITSTATUS(status) != EX_SOFTWARE) {
+		snprintf(reason, sizeof(reason), "Child status %d", status);
+		goto fail;
+	}
+	if (ccsp->ccs_signum < 0) {
+		snprintf(reason, sizeof(reason),
+		    "Child returned negative signal %d", ccsp->ccs_signum);
+		goto fail;
+	}
+	if (ctp->ct_flags & CT_FLAG_SIGNAL &&
+	    ccsp->ccs_signum != ctp->ct_signum) {
+		snprintf(reason, sizeof(reason), "Expected signal %d, got %d",
+		    ctp->ct_signum, ccsp->ccs_signum);
+	}
+	if (ctp->ct_flags & CT_FLAG_MIPS_EXCCODE) {
+		mips_exccode = (ccsp->ccs_mips_cause & MIPS_CR_EXC_CODE) >>
+		    MIPS_CR_EXC_CODE_SHIFT;
+		if (mips_exccode != ctp->ct_mips_exccode) {
+			snprintf(reason, sizeof(reason),
+			    "Expected MIPS exccode %ju, got %ju",
+			    ctp->ct_mips_exccode, mips_exccode);
+			goto fail;
+		}
+	}
+	if (ctp->ct_flags & CT_FLAG_CP2_EXCCODE) {
+		cp2_exccode = (ccsp->ccs_cp2_cause &
+		    CHERI_CAPCAUSE_EXCCODE_MASK) >>
+		    CHERI_CAPCAUSE_EXCCODE_SHIFT;
+		if (cp2_exccode != ctp->ct_cp2_exccode) {
+			snprintf(reason, sizeof(reason),
+			    "Expected CP2 exccode %ju, got %ju",
+			    ctp->ct_cp2_exccode, cp2_exccode);
+			goto fail;
+		}
+	}
+	fprintf(stderr, "PASS: %s\n", ctp->ct_name);
+	return;
+
+fail:
+	fprintf(stderr, "FAIL: %s: %s\n", ctp->ct_name, reason);
+}
+
+static void
+cheritest_run_test_name(const char *name)
+{
+	u_int i;
+
+	for (i = 0; i < cheri_tests_len; i++) {
+		if (strcmp(name, cheri_tests[i].ct_name) == 0)
+			break;
+	}
+	if (i == cheri_tests_len)
+		usage();
+	cheritest_run_test(&cheri_tests[i]);
 }
 
 int
@@ -196,18 +470,26 @@ main(__unused int argc, __unused char *argv[])
 	if (argc == 0)
 		usage();
 
+	/*
+	 * Allocate a page shared with children processes to return success/
+	 * failure status.
+	 */
+	assert(sizeof(*ccsp) <= (size_t)getpagesize());
+	ccsp = mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_ANON, -1,
+	    0);
+	if (ccsp == MAP_FAILED)
+		err(EX_OSERR, "mmap");
+	if (minherit(ccsp, getpagesize(), INHERIT_SHARE) < 0)
+		err(EX_OSERR, "minherit");
+
+	/* Run the actual tests. */
 	cheritest_libcheri_setup();
-	for (i = 0; i < argc; i++) {
-		for (t = 0; t < cheri_tests_len; t++) {
-			if (strcmp(argv[i], cheri_tests[t].ct_name) == 0)
-				break;
-		}
-		if (t == cheri_tests_len)
-			usage();
-		if (cheri_tests[t].ct_arg != 0)
-			cheri_tests[t].ct_func_arg(cheri_tests[t].ct_arg);
-		else
-			cheri_tests[t].ct_func();
+	if (argc == 1 && strcmp(argv[0], "all") == 0) {
+		for (t = 0; t < cheri_tests_len; t++)
+			cheritest_run_test(&cheri_tests[t]);
+	} else {
+		for (i = 0; i < argc; i++)
+			cheritest_run_test_name(argv[i]);
 	}
 	cheritest_libcheri_destroy();
 	exit(EX_OK);
