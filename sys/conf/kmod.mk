58c2dbbc30fbd7673cbba8d2098bc86f4fa626ef
Warner Losh <imp@FreeBSD.org>
2014-05-10 16:38:18 +0000

    Remove the compatibility hack for FreeBSD 7 systems for MACHINE_CPUARCH. Fewer places to have to hack each time a new one is added.

--- sys/conf/kmod.mk
+++ sys/conf/kmod.mk
@@ -60,9 +60,6 @@
 #		Unload a module.
 #
 
-# backwards compat option for older systems.
-MACHINE_CPUARCH?=${MACHINE_ARCH:C/mips(n32|64)?(el)?/mips/:C/arm(v6)?(eb)?/arm/:C/powerpc64/powerpc/}
-
 AWK?=		awk
 KMODLOAD?=	/sbin/kldload
 KMODUNLOAD?=	/sbin/kldunload
