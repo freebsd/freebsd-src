fffa71cbfe3ffe1ce24098a042e7ee127bac9e51
Hans Petter Selasky <hselasky@FreeBSD.org>
2014-05-16 10:35:21 +0000

    - Add flattended device tree probe-, attach- and detach code for the SAF1761 driver, compatible to existing Linux based FDT tables for the same hardware.
    Sponsored by:	DARPA, AFRL

--- sys/dev/usb/controller/saf1761_dci_fdt.c
+++ sys/dev/usb/controller/saf1761_dci_fdt.c
@@ -50,6 +50,13 @@
 #include <sys/callout.h>
 #include <sys/malloc.h>
 #include <sys/priv.h>
+#include <sys/rman.h>
+
+#include <dev/fdt/fdt_common.h>
+
+#include <dev/ofw/openfirm.h>
+#include <dev/ofw/ofw_bus.h>
+#include <dev/ofw/ofw_bus_subr.h>
 
 #include <dev/usb/usb.h>
 #include <dev/usb/usbdi.h>
@@ -68,3 +75,179 @@
 
 #include <dev/usb/controller/saf1761_dci.h>
 #include <dev/usb/controller/saf1761_dci_reg.h>
+
+static device_probe_t saf1761_dci_fdt_probe;
+static device_attach_t saf1761_dci_fdt_attach;
+static device_detach_t saf1761_dci_fdt_detach;
+
+static device_method_t saf1761_dci_methods[] = {
+	/* Device interface */
+	DEVMETHOD(device_probe, saf1761_dci_fdt_probe),
+	DEVMETHOD(device_attach, saf1761_dci_fdt_attach),
+	DEVMETHOD(device_detach, saf1761_dci_fdt_detach),
+	DEVMETHOD(device_suspend, bus_generic_suspend),
+	DEVMETHOD(device_resume, bus_generic_resume),
+	DEVMETHOD(device_shutdown, bus_generic_shutdown),
+
+	DEVMETHOD_END
+};
+
+static driver_t saf1761_dci_driver = {
+	.name = "saf1761",
+	.methods = saf1761_dci_methods,
+	.size = sizeof(struct saf1761_dci_softc),
+};
+
+static devclass_t saf1761_dci_devclass;
+
+DRIVER_MODULE(saf1761, simplebus, saf1761_dci_driver, saf1761_dci_devclass, 0, 0);
+MODULE_DEPEND(saf1761, usb, 1, 1, 1);
+
+static int
+saf1761_dci_fdt_probe(device_t dev)
+{
+	if (!ofw_bus_status_okay(dev))
+		return (ENXIO);
+
+	if (!ofw_bus_is_compatible(dev, "nxp,usb-isp1761"))
+		return (ENXIO);
+
+	device_set_desc(dev, "ISP1761/SAF1761 DCI USB 2.0 Device Controller");
+
+	return (0);
+}
+
+static int
+saf1761_dci_fdt_attach(device_t dev)
+{
+	struct saf1761_dci_softc *sc = device_get_softc(dev);
+	char param[24];
+	int err;
+	int rid;
+
+	/* get configuration from FDT */
+
+	/* get bus-width, if any */
+	if (OF_getprop(ofw_bus_get_node(dev), "bus-width",
+	    &param, sizeof(param)) > 0) {
+		param[sizeof(param) - 1] = 0;
+		if (strcmp(param, "32") == 0)
+			sc->sc_hw_mode |= SOTG_HW_MODE_CTRL_DATA_BUS_WIDTH;
+	} else {
+		/* assume 32-bit data bus */
+		sc->sc_hw_mode |= SOTG_HW_MODE_CTRL_DATA_BUS_WIDTH;
+	}
+
+	/* get analog over-current setting */
+	if (OF_getprop(ofw_bus_get_node(dev), "analog-oc",
+	    &param, sizeof(param)) > 0) {
+		sc->sc_hw_mode |= SOTG_HW_MODE_CTRL_ANA_DIGI_OC;
+	}
+
+	/* get DACK polarity */
+	if (OF_getprop(ofw_bus_get_node(dev), "dack-polarity",
+	    &param, sizeof(param)) > 0) {
+		sc->sc_hw_mode |= SOTG_HW_MODE_CTRL_DACK_POL;
+	}
+
+	/* get DREQ polarity */
+	if (OF_getprop(ofw_bus_get_node(dev), "dreq-polarity",
+	    &param, sizeof(param)) > 0) {
+		sc->sc_hw_mode |= SOTG_HW_MODE_CTRL_DREQ_POL;
+	}
+
+	/* initialise some bus fields */
+	sc->sc_bus.parent = dev;
+	sc->sc_bus.devices = sc->sc_devices;
+	sc->sc_bus.devices_max = SOTG_MAX_DEVICES;
+
+	/* get all DMA memory */
+	if (usb_bus_mem_alloc_all(&sc->sc_bus,
+	    USB_GET_DMA_TAG(dev), NULL)) {
+		return (ENOMEM);
+	}
+	rid = 0;
+	sc->sc_io_res =
+	    bus_alloc_resource_any(dev, SYS_RES_IOPORT, &rid, RF_ACTIVE);
+
+	if (!sc->sc_io_res) {
+		goto error;
+	}
+	sc->sc_io_tag = rman_get_bustag(sc->sc_io_res);
+	sc->sc_io_hdl = rman_get_bushandle(sc->sc_io_res);
+	sc->sc_io_size = rman_get_size(sc->sc_io_res);
+
+	rid = 0;
+	sc->sc_irq_res = bus_alloc_resource_any(dev, SYS_RES_IRQ, &rid,
+	    RF_SHAREABLE | RF_ACTIVE);
+	if (sc->sc_irq_res == NULL) {
+		goto error;
+	}
+	sc->sc_bus.bdev = device_add_child(dev, "usbus", -1);
+	if (!(sc->sc_bus.bdev)) {
+		goto error;
+	}
+	device_set_ivars(sc->sc_bus.bdev, &sc->sc_bus);
+
+	err = bus_setup_intr(dev, sc->sc_irq_res, INTR_TYPE_BIO | INTR_MPSAFE,
+	    NULL, (driver_intr_t *)saf1761_dci_interrupt, sc, &sc->sc_intr_hdl);
+	if (err) {
+		sc->sc_intr_hdl = NULL;
+		goto error;
+	}
+	err = saf1761_dci_init(sc);
+	if (err) {
+		device_printf(dev, "Init failed\n");
+		goto error;
+	}
+	err = device_probe_and_attach(sc->sc_bus.bdev);
+	if (err) {
+		device_printf(dev, "USB probe and attach failed\n");
+		goto error;
+	}
+	return (0);
+
+error:
+	saf1761_dci_fdt_detach(dev);
+	return (ENXIO);
+}
+
+static int
+saf1761_dci_fdt_detach(device_t dev)
+{
+	struct saf1761_dci_softc *sc = device_get_softc(dev);
+	device_t bdev;
+	int err;
+
+	if (sc->sc_bus.bdev) {
+		bdev = sc->sc_bus.bdev;
+		device_detach(bdev);
+		device_delete_child(dev, bdev);
+	}
+	/* during module unload there are lots of children leftover */
+	device_delete_children(dev);
+
+	if (sc->sc_irq_res && sc->sc_intr_hdl) {
+		/*
+		 * Only call uninit() after init()
+		 */
+		saf1761_dci_uninit(sc);
+
+		err = bus_teardown_intr(dev, sc->sc_irq_res,
+		    sc->sc_intr_hdl);
+		sc->sc_intr_hdl = NULL;
+	}
+	if (sc->sc_irq_res) {
+		bus_release_resource(dev, SYS_RES_IRQ, 0,
+		    sc->sc_irq_res);
+		sc->sc_irq_res = NULL;
+	}
+	if (sc->sc_io_res) {
+		bus_release_resource(dev, SYS_RES_IOPORT, 0,
+		    sc->sc_io_res);
+		sc->sc_io_res = NULL;
+	}
+	usb_bus_mem_free_all(&sc->sc_bus, NULL);
+
+	return (0);
+}
