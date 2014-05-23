db2ff6972d2757c22fbe505613d6b187d0f4822a
hselasky <hselasky@FreeBSD.org>
2014-05-23 06:20:25 +0000

    MFC r265358, r265427, r265777, r265783,  r265806, r265872, r266012 and r266394:
    - Multiple DWC OTG host mode related fixes, improvements and optimisations.
    - Add full support for ISOCHRONOUS transfers to the DWC OTG driver.
    - Use the interrupt filter to handle basic USB FIFO interrupts.
    - Fixed unbalanced unlock in case of "dwc_otg_init_fifo()" failure.
    - Add common spinlock to the USB bus structure.

--- sys/dev/usb/controller/usb_controller.c
+++ sys/dev/usb/controller/usb_controller.c
@@ -900,6 +900,9 @@ usb_bus_mem_alloc_all(struct usb_bus *bus, bus_dma_tag_t dmat,
 	mtx_init(&bus->bus_mtx, device_get_nameunit(bus->parent),
 	    NULL, MTX_DEF | MTX_RECURSE);
 
+	mtx_init(&bus->bus_spin_lock, device_get_nameunit(bus->parent),
+	    NULL, MTX_SPIN | MTX_RECURSE);
+
 	usb_callout_init_mtx(&bus->power_wdog,
 	    &bus->bus_mtx, 0);
 
@@ -953,6 +956,7 @@ usb_bus_mem_free_all(struct usb_bus *bus, usb_bus_mem_cb_t *cb)
 #endif
 
 	mtx_destroy(&bus->bus_mtx);
+	mtx_destroy(&bus->bus_spin_lock);
 }
 
 /* convenience wrappers */
