92f450919ef6f5e75ebc355d32efde1bc9b1a17b
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-05-27 04:59:53 +0000

    Merge svn-1.8.8 -> 1.8.9
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@266731 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/subversion/subversion/libsvn_fs_fs/fs.c
+++ contrib/subversion/subversion/libsvn_fs_fs/fs.c
@@ -98,8 +98,11 @@ fs_serialized_init(svn_fs_t *fs, apr_pool_t *common_pool, apr_pool_t *pool)
       SVN_ERR(svn_mutex__init(&ffsd->txn_current_lock,
                               SVN_FS_FS__USE_LOCK_MUTEX, common_pool));
 
+      /* We also need a mutex for synchronizing access to the active
+         transaction list and free transaction pointer.  This one is
+         enabled unconditionally. */
       SVN_ERR(svn_mutex__init(&ffsd->txn_list_lock,
-                              SVN_FS_FS__USE_LOCK_MUTEX, common_pool));
+                              TRUE, common_pool));
 
       key = apr_pstrdup(common_pool, key);
       status = apr_pool_userdata_set(ffsd, key, NULL, common_pool);
