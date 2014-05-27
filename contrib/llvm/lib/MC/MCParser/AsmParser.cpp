421237f94e446f451d76a958e5d34732a95f7d66
dim <dim@FreeBSD.org>
2014-05-27 18:27:51 +0000

    MFC r265925:
    Upgrade our copy of llvm/clang to 3.4.1 release.  This release contains
    mostly fixes, for the following upstream bugs:
    
    http://llvm.org/PR16365 http://llvm.org/PR17473 http://llvm.org/PR18000
    http://llvm.org/PR18068 http://llvm.org/PR18102 http://llvm.org/PR18165
    http://llvm.org/PR18260 http://llvm.org/PR18290 http://llvm.org/PR18316
    http://llvm.org/PR18460 http://llvm.org/PR18473 http://llvm.org/PR18515
    http://llvm.org/PR18526 http://llvm.org/PR18600 http://llvm.org/PR18762
    http://llvm.org/PR18773 http://llvm.org/PR18860 http://llvm.org/PR18994
    http://llvm.org/PR19007 http://llvm.org/PR19010 http://llvm.org/PR19033
    http://llvm.org/PR19059 http://llvm.org/PR19144 http://llvm.org/PR19326
    
    Approved by:	re (kib)

--- contrib/llvm/lib/MC/MCParser/AsmParser.cpp
+++ contrib/llvm/lib/MC/MCParser/AsmParser.cpp
@@ -4297,6 +4297,10 @@ bool AsmParser::parseMSInlineAsm(
       break;
     }
     case AOK_DotOperator:
+      // Insert the dot if the user omitted it.
+      OS.flush();
+      if (AsmStringIR.at(AsmStringIR.size() - 1) != '.')
+        OS << '.';
       OS << (*I).Val;
       break;
     }
