39a8a8db4ec02fe7b19b8bd979b4a532bc67e2cc
delphij <delphij@FreeBSD.org>
2014-05-27 19:05:26 +0000

    3835 zfs need not store 2 copies of all metadata
    illumos/illumos-gate@edf345e6b8342e8627ec20ce821a977a62cee19d

--- uts/common/sys/fs/zfs.h
+++ uts/common/sys/fs/zfs.h
@@ -21,7 +21,7 @@
 
 /*
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2013 by Delphix. All rights reserved.
+ * Copyright (c) 2011, 2014 by Delphix. All rights reserved.
  * Copyright 2011 Nexenta Systems, Inc.  All rights reserved.
  * Copyright (c) 2013, Joyent, Inc. All rights reserved.
  */
@@ -146,6 +146,7 @@ typedef enum {
 	ZFS_PROP_SNAPSHOT_LIMIT,
 	ZFS_PROP_FILESYSTEM_COUNT,
 	ZFS_PROP_SNAPSHOT_COUNT,
+	ZFS_PROP_REDUNDANT_METADATA,
 	ZFS_NUM_PROPS
 } zfs_prop_t;
 
@@ -339,6 +340,10 @@ typedef enum {
 	ZFS_SYNC_DISABLED = 2
 } zfs_sync_type_t;
 
+typedef enum {
+	ZFS_REDUNDANT_METADATA_ALL,
+	ZFS_REDUNDANT_METADATA_MOST
+} zfs_redundant_metadata_type_t;
 
 /*
  * On-disk version number.
