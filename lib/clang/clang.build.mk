6fd4061119cfa5d6a678e17fa79075c81ddb78c6
Dimitry Andric <dim@FreeBSD.org>
2014-06-10 17:54:24 +0000

    In some scenarios, when generating llvm/clang .inc.h files, multiple source files could be passed to tblgen or clang-tblgen, leading to a "Too many positional arguments specified" error message.  Fix this by replacing the too-generic ${.ALLSRC} sources with explicit paths.
    Reported by:	rysto32@gmail.com, rodrigc
    MFC after:	3 days

--- lib/clang/clang.build.mk
+++ lib/clang/clang.build.mk
@@ -74,149 +74,160 @@ ${arch:T}Gen${hdr:H:C/$/.inc.h/}: ${LLVM_SRCS}/lib/Target/${arch:H}/${arch:T}.td
 Attrs.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-classes \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 AttrDump.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-dump \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 AttrIdentifierArg.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-identifier-arg-list \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 AttrImpl.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-impl \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 AttrLateParsed.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-late-parsed-list \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 AttrList.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-list \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 AttrParsedAttrImpl.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-parsed-attr-impl \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 AttrParsedAttrKinds.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-parsed-attr-kinds \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 AttrParsedAttrList.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-parsed-attr-list \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 AttrPCHRead.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-pch-read \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 AttrPCHWrite.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-pch-write \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 AttrSpellings.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-spelling-list \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 AttrSpellingListIndex.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-spelling-index \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 AttrTemplateInstantiate.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-template-instantiate \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 AttrTypeArg.inc.h: ${CLANG_SRCS}/include/clang/Basic/Attr.td
 	${CLANG_TBLGEN} -gen-clang-attr-type-arg-list \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
-	    ${.ALLSRC}
+	    ${CLANG_SRCS}/include/clang/Basic/Attr.td
 
 CommentCommandInfo.inc.h: ${CLANG_SRCS}/include/clang/AST/CommentCommands.td
 	${CLANG_TBLGEN} -gen-clang-comment-command-info \
-	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} ${.ALLSRC}
+	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
+	    ${CLANG_SRCS}/include/clang/AST/CommentCommands.td
 
 CommentCommandList.inc.h: ${CLANG_SRCS}/include/clang/AST/CommentCommands.td
 	${CLANG_TBLGEN} -gen-clang-comment-command-list \
-	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} ${.ALLSRC}
+	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
+	    ${CLANG_SRCS}/include/clang/AST/CommentCommands.td
 
 CommentHTMLNamedCharacterReferences.inc.h: \
 	${CLANG_SRCS}/include/clang/AST/CommentHTMLNamedCharacterReferences.td
 	${CLANG_TBLGEN} -gen-clang-comment-html-named-character-references \
-	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} ${.ALLSRC}
+	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
+	    ${CLANG_SRCS}/include/clang/AST/CommentHTMLNamedCharacterReferences.td
 
 CommentHTMLTags.inc.h: ${CLANG_SRCS}/include/clang/AST/CommentHTMLTags.td
 	${CLANG_TBLGEN} -gen-clang-comment-html-tags \
-	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} ${.ALLSRC}
+	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
+	    ${CLANG_SRCS}/include/clang/AST/CommentHTMLTags.td
 
 CommentHTMLTagsProperties.inc.h: \
 	${CLANG_SRCS}/include/clang/AST/CommentHTMLTags.td
 	${CLANG_TBLGEN} -gen-clang-comment-html-tags-properties \
-	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} ${.ALLSRC}
+	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
+	    ${CLANG_SRCS}/include/clang/AST/CommentHTMLTags.td
 
 CommentNodes.inc.h: ${CLANG_SRCS}/include/clang/Basic/CommentNodes.td
 	${CLANG_TBLGEN} -gen-clang-comment-nodes \
-	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} ${.ALLSRC}
+	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
+	    ${CLANG_SRCS}/include/clang/Basic/CommentNodes.td
 
 DeclNodes.inc.h: ${CLANG_SRCS}/include/clang/Basic/DeclNodes.td
 	${CLANG_TBLGEN} -gen-clang-decl-nodes \
-	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} ${.ALLSRC}
+	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
+	    ${CLANG_SRCS}/include/clang/Basic/DeclNodes.td
 
 StmtNodes.inc.h: ${CLANG_SRCS}/include/clang/Basic/StmtNodes.td
 	${CLANG_TBLGEN} -gen-clang-stmt-nodes \
-	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} ${.ALLSRC}
+	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
+	    ${CLANG_SRCS}/include/clang/Basic/StmtNodes.td
 
 arm_neon.h: ${CLANG_SRCS}/include/clang/Basic/arm_neon.td
 	${CLANG_TBLGEN} -gen-arm-neon \
-	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} ${.ALLSRC}
+	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
+	    ${CLANG_SRCS}/include/clang/Basic/arm_neon.td
 
 arm_neon.inc.h: ${CLANG_SRCS}/include/clang/Basic/arm_neon.td
 	${CLANG_TBLGEN} -gen-arm-neon-sema \
-	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} ${.ALLSRC}
+	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
+	    ${CLANG_SRCS}/include/clang/Basic/arm_neon.td
 
 DiagnosticGroups.inc.h: ${CLANG_SRCS}/include/clang/Basic/Diagnostic.td
 	${CLANG_TBLGEN} -gen-clang-diag-groups \
 	    -I ${CLANG_SRCS}/include/clang/Basic -d ${.TARGET:C/\.h$/.d/} \
-	    -o ${.TARGET} ${.ALLSRC}
+	    -o ${.TARGET} ${CLANG_SRCS}/include/clang/Basic/Diagnostic.td
 
 DiagnosticIndexName.inc.h: ${CLANG_SRCS}/include/clang/Basic/Diagnostic.td
 	${CLANG_TBLGEN} -gen-clang-diags-index-name \
 	    -I ${CLANG_SRCS}/include/clang/Basic -d ${.TARGET:C/\.h$/.d/} \
-	    -o ${.TARGET} ${.ALLSRC}
+	    -o ${.TARGET} ${CLANG_SRCS}/include/clang/Basic/Diagnostic.td
 
 .for hdr in AST Analysis Comment Common Driver Frontend Lex Parse Sema Serialization
 Diagnostic${hdr}Kinds.inc.h: ${CLANG_SRCS}/include/clang/Basic/Diagnostic.td
 	${CLANG_TBLGEN} -gen-clang-diags-defs -clang-component=${hdr} \
 	    -I ${CLANG_SRCS}/include/clang/Basic -d ${.TARGET:C/\.h$/.d/} \
-	    -o ${.TARGET} ${.ALLSRC}
+	    -o ${.TARGET} ${CLANG_SRCS}/include/clang/Basic/Diagnostic.td
 .endfor
 
 Options.inc.h: ${CLANG_SRCS}/include/clang/Driver/Options.td
 	${TBLGEN} -gen-opt-parser-defs \
 	    -I ${LLVM_SRCS}/include -I ${CLANG_SRCS}/include/clang/Driver \
-	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} ${.ALLSRC}
+	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
+	    ${CLANG_SRCS}/include/clang/Driver/Options.td
 
 CC1AsOptions.inc.h: ${CLANG_SRCS}/include/clang/Driver/CC1AsOptions.td
 	${TBLGEN} -gen-opt-parser-defs \
 	    -I ${LLVM_SRCS}/include -I ${CLANG_SRCS}/include/clang/Driver \
-	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} ${.ALLSRC}
+	    -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
+	    ${CLANG_SRCS}/include/clang/Driver/CC1AsOptions.td
 
-Checkers.inc.h: ${CLANG_SRCS}/lib/StaticAnalyzer/Checkers/Checkers.td \
-	    ${CLANG_SRCS}/include/clang/StaticAnalyzer/Checkers/CheckerBase.td
+Checkers.inc.h: ${CLANG_SRCS}/lib/StaticAnalyzer/Checkers/Checkers.td
 	${CLANG_TBLGEN} -gen-clang-sa-checkers \
 	    -I ${CLANG_SRCS}/include -d ${.TARGET:C/\.h$/.d/} -o ${.TARGET} \
 	    ${CLANG_SRCS}/lib/StaticAnalyzer/Checkers/Checkers.td
