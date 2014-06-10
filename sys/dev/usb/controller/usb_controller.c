000781008f64eea9fc827bb5bad94cced540601c
marius <marius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-06-10 08:20:00 +0000

    Avoid the USB device disconnected and controller shutdown clutter on system shutdown by putting the former under !rebooting and turning the latter into debug messages.
    Reviewed by:	hps
    MFC after:	1 week
    Sponsored by:	Bally Wulff Games & Entertainment GmbH
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@267321 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/usb/controller/usb_controller.c
+++ sys/dev/usb/controller/usb_controller.c
@@ -333,7 +333,7 @@ usb_shutdown(device_t dev)
 		return (0);
 	}
 
-	device_printf(bus->bdev, "Controller shutdown\n");
+	DPRINTF("%s: Controller shutdown\n", device_get_nameunit(bus->bdev));
 
 	USB_BUS_LOCK(bus);
 	usb_proc_msignal(USB_BUS_EXPLORE_PROC(bus),
@@ -345,7 +345,8 @@ usb_shutdown(device_t dev)
 	}
 	USB_BUS_UNLOCK(bus);
 
-	device_printf(bus->bdev, "Controller shutdown complete\n");
+	DPRINTF("%s: Controller shutdown complete\n",
+	    device_get_nameunit(bus->bdev));
 
 	return (0);
 }
