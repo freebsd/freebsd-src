afa0538d5c9eaaafebaacb8af5457ed7741944ca
Pedro F. Giffuni <pfg@FreeBSD.org>
2014-06-22 20:24:17 +0000

    MFC	r267490: patch: unsign the line length to avoid overflows.
    Patch(1) uses a short int for the line length, which is usually
    sufficient for regular diffs, but makes no effort to signal
    when there is an overflow.
    
    Change the line length to an unsigned short int to better use
    the fact that a length is never negative.  The change is loosely
    inspired on a related change in DragonFly, but we avoid spending
    more memory than necessary.
    
    While here adjust the messages to be clearer on what is happening.

--- usr.bin/patch/patch.c
+++ usr.bin/patch/patch.c
@@ -742,14 +742,18 @@ abort_context_hunk(void)
 static void
 rej_line(int ch, LINENUM i)
 {
-	size_t len;
+	unsigned short len;
 	const char *line = pfetch(i);
 
-	len = strlen(line);
+	len = strnlen(line, USHRT_MAX);
 
 	fprintf(rejfp, "%c%s", ch, line);
-	if (len == 0 || line[len-1] != '\n')
-		fprintf(rejfp, "\n\\ No newline at end of file\n");
+	if (len == 0 || line[len-1] != '\n') {
+		if (len >= USHRT_MAX)
+			fprintf(rejfp, "\n\\ Line too long\n");
+		else
+			fprintf(rejfp, "\n\\ No newline at end of line\n");
+	}
 }
 
 static void
@@ -1016,7 +1020,7 @@ patch_match(LINENUM base, LINENUM offset, LINENUM fuzz)
 	LINENUM		pat_lines = pch_ptrn_lines() - fuzz;
 	const char	*ilineptr;
 	const char	*plineptr;
-	short		plinelen;
+	unsigned short	plinelen;
 
 	for (iline = base + offset + fuzz; pline <= pat_lines; pline++, iline++) {
 		ilineptr = ifetch(iline, offset >= 0);
