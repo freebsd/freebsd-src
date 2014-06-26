f0238f19a984b5ee5816f46dea860203562265dc
Konstantin Belousov <kib@FreeBSD.org>
2014-06-26 08:30:08 +0000

    MFC r267630: Add MAP_EXCL flag for mmap(2).

--- sys/vm/vm_map.h
+++ sys/vm/vm_map.h
@@ -315,6 +315,7 @@ long vmspace_resident_count(struct vmspace *vmspace);
 #define MAP_PREFAULT		0x0008
 #define MAP_PREFAULT_PARTIAL	0x0010
 #define MAP_DISABLE_SYNCER	0x0020
+#define	MAP_CHECK_EXCL		0x0040
 #define MAP_DISABLE_COREDUMP	0x0100
 #define MAP_PREFAULT_MADVISE	0x0200	/* from (user) madvise request */
 #define	MAP_VN_WRITECOUNT	0x0400
