7a1d0369e49a8608ac435a5f4013284eb09533fe
Brooks Davis <brooks@one-eyed-alien.net>
2014-06-27 19:27:32 +0000

    Allow the dtb to be passed in via struct bootinfo.

--- sys/mips/beri/beri_machdep.c
+++ sys/mips/beri/beri_machdep.c
@@ -230,11 +230,16 @@ platform_start(__register_t a0, __register_t a1,  __register_t a2,
 #ifdef FDT
 	/*
 	 * Find the dtb passed in by the boot loader (currently fictional).
+	 *
+	 * Prefer a dtb provided as a module to one from bootinfo as we may
+	 * have loaded an alternative one or created a modified version.
 	 */
 	if (kmdp != NULL)
 		dtbp = MD_FETCH(kmdp, MODINFOMD_DTBP, vm_offset_t);
 	else
 		dtbp = (vm_offset_t)NULL;
+	if (dtbp == (vm_offset_t)NULL && bootinfop->bi_dtb != (bi_ptr_t)NULL)
+		dtbp = bootinfop->bi_dtb;
 
 #if defined(FDT_DTB_STATIC)
 	/*
