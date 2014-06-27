c8f23b47b8dcddd17596d284c16fc75da2b280b8
John Baldwin <jhb@FreeBSD.org>
2014-06-27 17:22:18 +0000

    MFC 261781: Don't waste a page of KVA for the boot-time memory test on x86.  For amd64, reuse the first page of the crashdumpmap as CMAP1/CADDR1.  For i386, remove CMAP1/CADDR1 entirely and reuse CMAP3/CADDR3 for the memory test.

--- sys/pc98/pc98/machdep.c
+++ sys/pc98/pc98/machdep.c
@@ -1953,7 +1953,7 @@ getmemsize(int first)
 	phys_avail[pa_indx++] = physmap[0];
 	phys_avail[pa_indx] = physmap[0];
 	dump_avail[da_indx] = physmap[0];
-	pte = CMAP1;
+	pte = CMAP3;
 
 	/*
 	 * Get dcons buffer address
@@ -1974,7 +1974,7 @@ getmemsize(int first)
 			end = trunc_page(physmap[i + 1]);
 		for (pa = round_page(physmap[i]); pa < end; pa += PAGE_SIZE) {
 			int tmp, page_bad, full;
-			int *ptr = (int *)CADDR1;
+			int *ptr = (int *)CADDR3;
 
 			full = FALSE;
 			/*
