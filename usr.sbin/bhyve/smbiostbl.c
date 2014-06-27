8d8b7d89d9c731ed8f7b0a223e45292d137d5379
grehan <grehan@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-06-27 05:27:37 +0000

    Set the version and date to fixed fields rather than using preprocessor macros that don't allow reproducible builds. As a side-effect, the date string is now spec-compliant.
    root@bhyve:~ # dmidecode
    # dmidecode 2.12
    SMBIOS 2.4 present.
    12 structures occupying 514 bytes.
    Table at 0x000F101F.
    
    Handle 0x0001, DMI type 0, 24 bytes
    BIOS Information
            Vendor: BHYVE
            Version: 1.0
            Release Date: 03/14/2014
    
    Submitted by:	des (original version)
    Reviewed by:	tychon
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@267949 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/bhyve/smbiostbl.c
+++ usr.sbin/bhyve/smbiostbl.c
@@ -321,8 +321,8 @@ struct smbios_table_type0 smbios_type0_template = {
 
 const char *smbios_type0_strings[] = {
 	"BHYVE",	/* vendor string */
-	__TIME__,	/* bios version string */
-	__DATE__,	/* bios release date string */
+	"1.00",		/* bios version string */
+	"03/14/2014",	/* bios release date string */
 	NULL
 };
 
