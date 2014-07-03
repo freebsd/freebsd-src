57eb02157dea23f665437dd4afa0db77b76e893b
Hans Petter Selasky <hselasky@FreeBSD.org>
2014-07-03 17:42:26 +0000

    MFC r268221 and r268222: - Remove some unused variables. - Add proper rangechecks in "axge_rx_frame()" function and fix receive loop header parsing. - Add new USB IDs.
    Approved by: 	re, gjb @
    PR:		191432

--- sys/dev/tx/if_tx.c
+++ sys/dev/tx/if_tx.c
@@ -1150,12 +1150,10 @@ epic_ifmedia_sts(struct ifnet *ifp, struct ifmediareq *ifmr)
 {
 	epic_softc_t *sc;
 	struct mii_data *mii;
-	struct ifmedia *ifm;
 
 	sc = ifp->if_softc;
 	mii = device_get_softc(sc->miibus);
 	EPIC_LOCK(sc);
-	ifm = &mii->mii_media;
 
 	/* Nothing should be selected if interface is down. */
 	if ((ifp->if_flags & IFF_UP) == 0) {
