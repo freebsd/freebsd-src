09132ba6ac12c39efcdda0c33c59c9a7cb5a820b
Alan Cox <alc@FreeBSD.org>
2014-07-06 17:42:38 +0000

    Introduce pmap_unwire().  It will replace pmap_change_wiring().  There are several reasons for this change:
    pmap_change_wiring() has never (in my memory) been used to set the wired
    attribute on a virtual page.  We have always used pmap_enter() to do that.
    Moreover, it is not really safe to use pmap_change_wiring() to set the wired
    attribute on a virtual page.  The description of pmap_change_wiring() says
    that it assumes the existence of a mapping in the pmap.  However, non-wired
    mappings may be reclaimed by the pmap at any time.  (See pmap_collect().)
    Many implementations of pmap_change_wiring() will crash if the mapping does
    not exist.
    
    pmap_unwire() accepts a range of virtual addresses, whereas
    pmap_change_wiring() acts upon a single virtual page.  Since we are
    typically unwiring a range of virtual addresses, pmap_unwire() will be more
    efficient.  Moreover, pmap_unwire() allows us to unwire superpage mappings.
    Previously, we were forced to demote the superpage mapping, because
    pmap_change_wiring() only allowed us to express the unwiring of a single
    base page mapping at a time.  This added to the overhead of unwiring for
    large ranges of addresses, including the implicit unwiring that occurs at
    process termination.
    
    Implementations for arm and powerpc will follow.
    
    Discussed with:	jeff, marcel
    Reviewed by:	kib
    Sponsored by:	EMC / Isilon Storage Division

--- sys/amd64/amd64/pmap.c
+++ sys/amd64/amd64/pmap.c
@@ -4736,6 +4736,102 @@ out:
 }
 
 /*
+ *	Clear the wired attribute from the mappings for the specified range of
+ *	addresses in the given pmap.  Every valid mapping within that range
+ *	must have the wired attribute set.  In contrast, invalid mappings
+ *	cannot have the wired attribute set, so they are ignored.
+ *
+ *	The wired attribute of the page table entry is not a hardware feature,
+ *	so there is no need to invalidate any TLB entries.
+ */
+void
+pmap_unwire(pmap_t pmap, vm_offset_t sva, vm_offset_t eva)
+{
+	vm_offset_t va_next;
+	pml4_entry_t *pml4e;
+	pdp_entry_t *pdpe;
+	pd_entry_t *pde;
+	pt_entry_t *pte, PG_V;
+	boolean_t pv_lists_locked;
+
+	PG_V = pmap_valid_bit(pmap);
+	pv_lists_locked = FALSE;
+resume:
+	PMAP_LOCK(pmap);
+	for (; sva < eva; sva = va_next) {
+		pml4e = pmap_pml4e(pmap, sva);
+		if ((*pml4e & PG_V) == 0) {
+			va_next = (sva + NBPML4) & ~PML4MASK;
+			if (va_next < sva)
+				va_next = eva;
+			continue;
+		}
+		pdpe = pmap_pml4e_to_pdpe(pml4e, sva);
+		if ((*pdpe & PG_V) == 0) {
+			va_next = (sva + NBPDP) & ~PDPMASK;
+			if (va_next < sva)
+				va_next = eva;
+			continue;
+		}
+		va_next = (sva + NBPDR) & ~PDRMASK;
+		if (va_next < sva)
+			va_next = eva;
+		pde = pmap_pdpe_to_pde(pdpe, sva);
+		if ((*pde & PG_V) == 0)
+			continue;
+		if ((*pde & PG_PS) != 0) {
+			if ((*pde & PG_W) == 0)
+				panic("pmap_unwire: pde %#jx is missing PG_W",
+				    (uintmax_t)*pde);
+
+			/*
+			 * Are we unwiring the entire large page?  If not,
+			 * demote the mapping and fall through.
+			 */
+			if (sva + NBPDR == va_next && eva >= va_next) {
+				atomic_clear_long(pde, PG_W);
+				pmap->pm_stats.wired_count -= NBPDR /
+				    PAGE_SIZE;
+				continue;
+			} else {
+				if (!pv_lists_locked) {
+					pv_lists_locked = TRUE;
+					if (!rw_try_rlock(&pvh_global_lock)) {
+						PMAP_UNLOCK(pmap);
+						rw_rlock(&pvh_global_lock);
+						/* Repeat sva. */
+						goto resume;
+					}
+				}
+				if (!pmap_demote_pde(pmap, pde, sva))
+					panic("pmap_unwire: demotion failed");
+			}
+		}
+		if (va_next > eva)
+			va_next = eva;
+		for (pte = pmap_pde_to_pte(pde, sva); sva != va_next; pte++,
+		    sva += PAGE_SIZE) {
+			if ((*pte & PG_V) == 0)
+				continue;
+			if ((*pte & PG_W) == 0)
+				panic("pmap_unwire: pte %#jx is missing PG_W",
+				    (uintmax_t)*pte);
+
+			/*
+			 * PG_W must be cleared atomically.  Although the pmap
+			 * lock synchronizes access to PG_W, another processor
+			 * could be setting PG_M and/or PG_A concurrently.
+			 */
+			atomic_clear_long(pte, PG_W);
+			pmap->pm_stats.wired_count--;
+		}
+	}
+	if (pv_lists_locked)
+		rw_runlock(&pvh_global_lock);
+	PMAP_UNLOCK(pmap);
+}
+
+/*
  *	Copy the range specified by src_addr/len
  *	from the source map to the range dst_addr/len
  *	in the destination map.
