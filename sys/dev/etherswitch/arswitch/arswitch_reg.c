94e9ac21253398e72e800f2f5a256496643f663d
rpaulo <rpaulo@FreeBSD.org>
2014-07-12 01:04:10 +0000

    Move iic.h to sys/ so that it's automatically installed in /usr/include/sys.
    This lets us call iic(4) ioctls without needing the kernel source code
    and follows the same model of GPIO.
    
    MFC after:	3 weeks

--- sys/dev/etherswitch/arswitch/arswitch_reg.c
+++ sys/dev/etherswitch/arswitch/arswitch_reg.c
@@ -44,7 +44,7 @@
 #include <net/if_types.h>
 
 #include <machine/bus.h>
-#include <dev/iicbus/iic.h>
+#include <sys/iic.h>
 #include <dev/iicbus/iiconf.h>
 #include <dev/iicbus/iicbus.h>
 #include <dev/mii/mii.h>
