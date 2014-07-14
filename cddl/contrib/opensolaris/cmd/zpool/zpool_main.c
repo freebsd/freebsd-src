4a4868928eeb36b4589e82efc2080147ba5fe351
smh <smh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-07-14 14:33:03 +0000

    Don't report non-native block-size pools under zpool status -x
    zpool status -x is used to identify pools that are exhibiting
    errors or are otherwise unavailable, therefore non-native
    block-size pools shouldn't be reported.
    
    Also update man page to clarify other additional conditions
    which won't cause a pool to be displayed under zpool status -x.
    
    Sponsored by:	Multiplay
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@268621 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- cddl/contrib/opensolaris/cmd/zpool/zpool_main.c
+++ cddl/contrib/opensolaris/cmd/zpool/zpool_main.c
@@ -4101,6 +4101,7 @@ status_callback(zpool_handle_t *zhp, void *data)
 	if (cbp->cb_explain &&
 	    (reason == ZPOOL_STATUS_OK ||
 	    reason == ZPOOL_STATUS_VERSION_OLDER ||
+	    reason == ZPOOL_STATUS_NON_NATIVE_ASHIFT ||
 	    reason == ZPOOL_STATUS_FEAT_DISABLED)) {
 		if (!cbp->cb_allpools) {
 			(void) printf(gettext("pool '%s' is healthy\n"),
