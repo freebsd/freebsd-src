a6f6f7a6329ccdc871149734d5978284a0134689
Alexander Motin <mav@FreeBSD.org>
2014-07-17 11:38:37 +0000

    Fix ctld crash on startup if target alias is not set.
    MFC after:	3 days

--- usr.sbin/ctld/kernel.c
+++ usr.sbin/ctld/kernel.c
@@ -851,9 +851,12 @@ kernel_port_add(struct target *targ)
 	req.args[0].value = &port_id;
 	req.args[0].flags = CTL_BEARG_WR;
 	str_arg(&req.args[1], "cfiscsi_target", targ->t_name);
-	str_arg(&req.args[2], "cfiscsi_target_alias", targ->t_alias);
 	snprintf(tagstr, sizeof(tagstr), "%d", targ->t_portal_group->pg_tag);
-	str_arg(&req.args[3], "cfiscsi_portal_group_tag", tagstr);
+	str_arg(&req.args[2], "cfiscsi_portal_group_tag", tagstr);
+	if (targ->t_alias)
+		str_arg(&req.args[3], "cfiscsi_target_alias", targ->t_alias);
+	else
+		req.num_args--;
 
 	error = ioctl(ctl_fd, CTL_PORT_REQ, &req);
 	free(req.args);
