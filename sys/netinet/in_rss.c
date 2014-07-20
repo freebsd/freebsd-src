8a3ce3eb520bffe18f85cab4cce5f288c30f2b8c
adrian <adrian@FreeBSD.org>
2014-07-20 07:36:59 +0000

    Implement rss_gethashconfig() - return the currently supported hash methods by the stack.
    Right now the stack isn't really setup for RSS with 4-tuple UDP hashing
    for either IPv4 and IPv6.
    
    The specifics:
    
    * The UDP init path udp_init() and udplite_init() specify the hash as
      2-tuple, so the PCBGROUPS code only tries a 2-tuple check;
    * The PCBGROUPS and RSS code doesn't know about the UDP hash types
      just yet, so they're never treated as valid hashes.
    * For correctness, 4-tuple can't be enabled in the general case because
      UDP datagrams can be more fragmented than IP datagrams may be.
    
    Strictly speaking, TCP datagrams may also be fragmented and this could
    cause issues with PCBGROUPS/RSS until the IP defragment path grows some
    code to re-calculate the RSS hash.
    
    I'll follow this commit up with awareness of the UDP 4-tuple for those
    who wish to configure it, but for now it'll stay disabled.
    
    No drivers (yet) know to use this function when RSS is enabled.

--- sys/netinet/in_rss.c
+++ sys/netinet/in_rss.c
@@ -535,6 +535,40 @@ rss_getnumcpus(void)
 }
 
 /*
+ * Return the supported RSS hash configuration.
+ *
+ * NICs should query this to determine what to configure in their redirection
+ * matching table.
+ */
+u_int
+rss_gethashconfig(void)
+{
+	/* Return 4-tuple for TCP; 2-tuple for others */
+	/*
+	 * UDP may fragment more often than TCP and thus we'll end up with
+	 * NICs returning 2-tuple fragments.
+	 * udp_init() and udplite_init() both currently initialise things
+	 * as 2-tuple.
+	 * So for now disable UDP 4-tuple hashing until all of the other
+	 * pieces are in place.
+	 */
+	return (
+	    RSS_HASHTYPE_RSS_IPV4
+	|    RSS_HASHTYPE_RSS_TCP_IPV4
+	|    RSS_HASHTYPE_RSS_IPV6
+	|    RSS_HASHTYPE_RSS_TCP_IPV6
+	|    RSS_HASHTYPE_RSS_IPV6_EX
+	|    RSS_HASHTYPE_RSS_TCP_IPV6_EX
+#if 0
+	|    RSS_HASHTYPE_RSS_UDP_IPV4
+	|    RSS_HASHTYPE_RSS_UDP_IPV4_EX
+	|    RSS_HASHTYPE_RSS_UDP_IPV6
+	|    RSS_HASHTYPE_RSS_UDP_IPV6_EX
+#endif
+	);
+}
+
+/*
  * XXXRW: Confirm that sysctl -a won't dump this keying material, don't want
  * it appearing in debugging output unnecessarily.
  */
