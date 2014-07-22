f2c8c4c7d8375602f7f002c9b28991907a6a1021
ache <ache@FreeBSD.org>
2014-07-22 20:13:46 +0000

    Back the whole change out until I figure out how to obtain O_APPEND, it can't be used in this field at all.

--- lib/libc/stdio/ftell.c
+++ lib/libc/stdio/ftell.c
@@ -39,7 +39,6 @@ __FBSDID("$FreeBSD$");
 #include "namespace.h"
 #include <sys/types.h>
 #include <errno.h>
-#include <fcntl.h>
 #include <limits.h>
 #include <stdio.h>
 #include "un-namespace.h"
@@ -119,11 +118,6 @@ _ftello(FILE *fp, fpos_t *offset)
 		if (HASUB(fp))
 			pos -= fp->_r;  /* Can be negative at this point. */
 	} else if ((fp->_flags & __SWR) && fp->_p != NULL) {
-		if (fp->_flags & (__SAPP|O_APPEND)) {
-			pos = _sseek(fp, (fpos_t)0, SEEK_END);
-			if (pos == -1)
-				return (1);
-		}
 		/*
 		 * Writing.  Any buffered characters cause the
 		 * position to be greater than that in the
