50b74c6ef15ce805efd193683c682b25107d78dc
Xin LI <delphij@FreeBSD.org>
2014-07-26 19:07:08 +0000

    Add two sysctls for newly added tunables.
    MFC after:	2 weeks

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/metaslab.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/metaslab.c
@@ -114,6 +114,11 @@ SYSCTL_INT(_vfs_zfs, OID_AUTO, mg_noalloc_threshold, CTLFLAG_RWTUN,
  * class have also crossed this threshold.
  */
 int zfs_mg_fragmentation_threshold = 85;
+SYSCTL_INT(_vfs_zfs, OID_AUTO, mg_fragmentation_threshold, CTLFLAG_RWTUN,
+    &zfs_mg_fragmentation_threshold, 0,
+    "Percentage of metaslab group size that should be considered "
+    "eligible for allocations unless all metaslab groups within the metaslab class "
+    "have also crossed this threshold");
 
 /*
  * Allow metaslabs to keep their active state as long as their fragmentation
@@ -122,6 +127,9 @@ int zfs_mg_fragmentation_threshold = 85;
  * status allowing better metaslabs to be selected.
  */
 int zfs_metaslab_fragmentation_threshold = 70;
+SYSCTL_INT(_vfs_zfs_metaslab, OID_AUTO, fragmentation_threshold, CTLFLAG_RWTUN,
+    &zfs_metaslab_fragmentation_threshold, 0,
+    "Maximum percentage of metaslab fragmentation level to keep their active state");
 
 /*
  * When set will load all metaslabs when pool is first opened.
