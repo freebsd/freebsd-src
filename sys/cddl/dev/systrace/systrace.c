d77f75a9080273e2cf2ea9dff10fb0376b562f86
Mark Johnston <markj@FreeBSD.org>
2014-07-29 22:26:42 +0000

    MFC r264435: Ensure that all eight syscall arguments are available to dtrace_probe(), rather than just the first five.

--- sys/cddl/dev/systrace/systrace.c
+++ sys/cddl/dev/systrace/systrace.c
@@ -168,6 +168,9 @@ static dtrace_pops_t systrace_pops = {
 static struct cdev		*systrace_cdev;
 static dtrace_provider_id_t	systrace_id;
 
+typedef void (*systrace_dtrace_probe_t)(dtrace_id_t, uintptr_t, uintptr_t,
+    uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t);
+
 #if !defined(LINUX_SYSTRACE)
 /*
  * Probe callback function.
@@ -180,6 +183,7 @@ static void
 systrace_probe(u_int32_t id, int sysnum, struct sysent *sysent, void *params,
     int ret)
 {
+	systrace_dtrace_probe_t probe;
 	int		n_args	= 0;
 	u_int64_t	uargs[8];
 
@@ -211,7 +215,9 @@ systrace_probe(u_int32_t id, int sysnum, struct sysent *sysent, void *params,
 	}
 
 	/* Process the probe using the converted argments. */
-	dtrace_probe(id, uargs[0], uargs[1], uargs[2], uargs[3], uargs[4]);
+	probe = (systrace_dtrace_probe_t)dtrace_probe;
+	probe(id, uargs[0], uargs[1], uargs[2], uargs[3], uargs[4], uargs[5],
+	    uargs[6], uargs[7]);
 }
 #endif
 
