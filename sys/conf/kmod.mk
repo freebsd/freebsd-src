661b96f06e13e80cbb17562fe9f9672c7759429d
imp <imp@FreeBSD.org>
2014-08-04 22:37:02 +0000

    Move most of the 15 variations on generating opt_inet.h and opt_inet6.h into kmod.mk by forcing almost everybody to eat the same dogfood. While at it, consolidate the opt_bpf.h and opt_mroute.h targets here too.

--- sys/conf/kmod.mk
+++ sys/conf/kmod.mk
@@ -315,6 +315,34 @@ unload:
 	${KMODUNLOAD} -v ${PROG}
 .endif
 
+# Generate options files that otherwise would be built
+# in substantially similar ways through the tree. Move
+# the code here when they all produce identical results
+# (or should)
+.if !defined(KERNBUILDDIR)
+opt_bpf.h:
+	echo "#define DEV_BPF 1" > ${.TARGET}
+.if ${MK_INET_SUPPORT} != "no"
+opt_inet.h:
+	@echo "#define INET 1" > ${.TARGET}
+	@echo "#define TCP_OFFLOAD 1" >> ${.TARGET}
+.endif
+.if ${MK_INET6_SUPPORT} != "no"
+opt_inet6.h:
+	@echo "#define INET6 1" > ${.TARGET}
+.endif
+opt_mrouting.h:
+	echo "#define MROUTING 1" > ${.TARGET}
+opt_natm.h:
+	echo "#define NATM 1" > ${.TARGET}
+opt_scsi.h:
+	echo "#define SCSI_DELAY 15000" > ${.TARGET}
+opt_wlan.h:
+	echo "#define IEEE80211_DEBUG 1" > ${.TARGET}
+	echo "#define IEEE80211_AMPDU_AGE 1" >> ${.TARGET}
+	echo "#define IEEE80211_SUPPORT_MESH 1" >> ${.TARGET}
+.endif
+
 .if defined(KERNBUILDDIR)
 .PATH: ${KERNBUILDDIR}
 CFLAGS+=	-I${KERNBUILDDIR}
