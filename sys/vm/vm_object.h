faaf5447600bcb498c0df6363248fd5b8c9ee93e
Konstantin Belousov <kib@FreeBSD.org>
2014-08-06 19:30:35 +0000

    Add wrappers to assert that vm object is unlocked and for try upgrade.
    Reviewed by:	alc
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week

--- sys/vm/vm_object.h
+++ sys/vm/vm_object.h
@@ -225,6 +225,8 @@ extern struct vm_object kmem_object_store;
 	rw_assert(&(object)->lock, RA_RLOCKED)
 #define	VM_OBJECT_ASSERT_WLOCKED(object)				\
 	rw_assert(&(object)->lock, RA_WLOCKED)
+#define	VM_OBJECT_ASSERT_UNLOCKED(object)				\
+	rw_assert(&(object)->lock, RA_UNLOCKED)
 #define	VM_OBJECT_LOCK_DOWNGRADE(object)				\
 	rw_downgrade(&(object)->lock)
 #define	VM_OBJECT_RLOCK(object)						\
@@ -237,6 +239,8 @@ extern struct vm_object kmem_object_store;
 	rw_try_rlock(&(object)->lock)
 #define	VM_OBJECT_TRYWLOCK(object)					\
 	rw_try_wlock(&(object)->lock)
+#define	VM_OBJECT_TRYUPGRADE(object)					\
+	rw_try_upgrade(&(object)->lock)
 #define	VM_OBJECT_WLOCK(object)						\
 	rw_wlock(&(object)->lock)
 #define	VM_OBJECT_WUNLOCK(object)					\
