dabcba750228d2768fd72b2591a259a27781a84f
adrian <adrian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-08-09 09:13:10 +0000

    Add two new debug mark entries for chip power configuration.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@269749 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ath/ath_hal/ah_decode.h
+++ sys/dev/ath/ath_hal/ah_decode.h
@@ -53,6 +53,8 @@ enum {
 	AH_MARK_ANI_POLL,		/* ar*AniReset, listen time */
 	AH_MARK_ANI_CONTROL,		/* ar*AniReset, cmd */
 	AH_MARK_RX_CTL,			/* RX DMA control */
+	AH_MARK_CHIP_POWER,		/* chip power control, mode */
+	AH_MARK_CHIP_POWER_DONE,	/* chip power control done, status */
 };
 
 enum {
@@ -61,6 +63,7 @@ enum {
 	AH_MARK_RX_CTL_DMA_START,
 	AH_MARK_RX_CTL_DMA_STOP,
 	AH_MARK_RX_CTL_DMA_STOP_ERR,
+	AH_MARK_RX_CTL_DMA_STOP_OK,
 };
 
 #endif /* _ATH_AH_DECODE_H_ */
