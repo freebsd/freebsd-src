9abce0e567c9a5a0520cdd94d5c633c7baf9a184
araujo <araujo@FreeBSD.org>
2014-08-11 03:04:16 +0000

    - Remove unneeded include.
    Phabric:	D563
    Reviewed by:	kevlo
    Approved by:	kevlo

--- sys/net/if_lagg.c
+++ sys/net/if_lagg.c
@@ -46,7 +46,6 @@ __FBSDID("$FreeBSD$");
 #include <net/if_clone.h>
 #include <net/if_arp.h>
 #include <net/if_dl.h>
-#include <net/if_llc.h>
 #include <net/if_media.h>
 #include <net/if_types.h>
 #include <net/if_var.h>
