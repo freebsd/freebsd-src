b33b50b292537807bb2f0c16bea67e9a3a631594
Ed Maste <emaste@freebsd.org>
2014-08-11 11:26:57 -0400

    Correct usage typo: mtlclt -> mtlctl

--- ctsrd/usr.sbin/mtlctl/mtlctl.c
+++ ctsrd/usr.sbin/mtlctl/mtlctl.c
@@ -82,13 +82,13 @@ usage(void)
 {
 	
 	printf("usage:	\n");
-	printf("	mtlclt fbdump <file>\n");
-	printf("	mtlclt fbdumppng <file>\n");
-	printf("	mtlclt fbfill <RRGGBB>\n");
-	printf("	mtlclt fbloaddump <file>\n");
-	printf("	mtlclt fbloadpng <file>\n");
-	printf("	mtlclt gesture\n");
-	printf("	mtlclt gestures\n");
+	printf("	mtlctl fbdump <file>\n");
+	printf("	mtlctl fbdumppng <file>\n");
+	printf("	mtlctl fbfill <RRGGBB>\n");
+	printf("	mtlctl fbloaddump <file>\n");
+	printf("	mtlctl fbloadpng <file>\n");
+	printf("	mtlctl gesture\n");
+	printf("	mtlctl gestures\n");
 	exit(1);
 }
 
