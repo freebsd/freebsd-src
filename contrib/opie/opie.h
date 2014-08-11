780fb5edaea6813e41d0805acad57ec67b0c3b81
ache <ache@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-08-11 12:26:48 +0000

    Fix too long (seed length >12 chars) challenge handling. 1) " ext" length should be included into OPIE_CHALLENGE_MAX (as all places of opie code expects that). 2) Overflow check in challenge.c is off by 1 even with corrected OPIE_CHALLENGE_MAX 3) When fallback to randomchallenge() happens and rval is 0 (i.e. challenge is too long), its value should be set to error state too.
    To demonstrate the bug, run opiepasswd with valid seed:
    opiepasswd -s 1234567890123456
    and notice that it falls back to randomchallenge() (i.e. no
    1234567890123456 in the prompt).
    
    PR:             191511
    Submitted by:   mitsururike@gmail.com (partially)
    MFC after:      1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@269806 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/opie/opie.h
+++ contrib/opie/opie.h
@@ -72,8 +72,8 @@ struct opie {
 /* Max length of hash algorithm name (md4/md5) */
 #define OPIE_HASHNAME_MAX 3
 
-/* Maximum length of a challenge (otp-md? 9999 seed) */
-#define OPIE_CHALLENGE_MAX (4+OPIE_HASHNAME_MAX+1+4+1+OPIE_SEED_MAX)
+/* Maximum length of a challenge (otp-md? 9999 seed ext) */
+#define OPIE_CHALLENGE_MAX (4+OPIE_HASHNAME_MAX+1+4+1+OPIE_SEED_MAX+1+3)
 
 /* Maximum length of a response that we allow */
 #define OPIE_RESPONSE_MAX (9+1+19+1+9+OPIE_SEED_MAX+1+19+1+19+1+19)
