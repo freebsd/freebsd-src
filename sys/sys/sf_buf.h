8d4691470d9e085cebf829c80aef28443cb9ef14
glebius <glebius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-08-11 12:59:55 +0000

    Provide sf_buf_ref() to optimize refcounting of already allocated sendfile(2) buffers.
    Sponsored by:	Netflix
    Sponsored by:	Nginx, Inc.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@269807 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/sf_buf.h
+++ sys/sys/sf_buf.h
@@ -106,6 +106,7 @@ struct sf_buf;
 #ifdef SFBUF
 struct sf_buf *sf_buf_alloc(struct vm_page *, int);
 void sf_buf_free(struct sf_buf *);
+void sf_buf_ref(struct sf_buf *);
 
 static inline vm_offset_t
 sf_buf_kva(struct sf_buf *sf)
@@ -166,6 +167,11 @@ sf_buf_alloc(struct vm_page *m, int pri)
 
 static inline void
 sf_buf_free(struct sf_buf *sf)
+{
+}
+
+static inline void
+sf_buf_ref(struct sf_buf *sf)
 {
 }
 #endif /* SFBUF */
