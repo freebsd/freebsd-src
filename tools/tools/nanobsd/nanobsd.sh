738e8e62a5f18cde4905b0d94715064718fdea6f
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-08-14 16:17:23 +0000

    Only install the boot loader if it actually exists. This is a stop-gap change, since larger changes to use geom more exclusively to create partitions is in th works.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@269983 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- tools/tools/nanobsd/nanobsd.sh
+++ tools/tools/nanobsd/nanobsd.sh
@@ -581,8 +581,14 @@ create_i386_diskimage ( ) (
 	fdisk ${MD}
 	# XXX: params
 	# XXX: pick up cached boot* files, they may not be in image anymore.
-	boot0cfg -B -b ${NANO_WORLDDIR}/${NANO_BOOTLOADER} ${NANO_BOOT0CFG} ${MD}
-	bsdlabel -w -B -b ${NANO_WORLDDIR}/boot/boot ${MD}s1
+	if [ -f ${NANO_WORLDDIR}/${NANO_BOOTLOADER} ]; then
+		boot0cfg -B -b ${NANO_WORLDDIR}/${NANO_BOOTLOADER} ${NANO_BOOT0CFG} ${MD}
+	fi
+	if [ -f ${NANO_WORLDDIR}/boot/boot ]; then
+		bsdlabel -w -B -b ${NANO_WORLDDIR}/boot/boot ${MD}s1
+	else
+		bsdlabel -w ${MD}s1
+	fi
 	bsdlabel ${MD}s1
 
 	# Create first image
