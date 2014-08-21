8ee8edd44132fc05c5ea41c05479ddb5b1a7f3fb
Steven Hartland <smh@FreeBSD.org>
2014-08-21 22:47:03 +0000

    MFC r269974 - Added 4K quirks for Corsair Force GT and Samsung 840 SSDs
    Sponsored by:	Multiplay

--- sys/cam/ata/ata_da.c
+++ sys/cam/ata/ata_da.c
@@ -299,10 +299,10 @@ static struct ada_quirk_entry ada_quirk_table[] =
 	},
 	{
 		/*
-		 * Corsair Force GT SSDs
+		 * Corsair Force GT & GS SSDs
 		 * 4k optimised & trim only works in 4k requests + 4k aligned
 		 */
-		{ T_DIRECT, SIP_MEDIA_FIXED, "*", "Corsair Force GT*", "*" },
+		{ T_DIRECT, SIP_MEDIA_FIXED, "*", "Corsair Force G*", "*" },
 		/*quirks*/ADA_Q_4K
 	},
 	{
@@ -439,6 +439,14 @@ static struct ada_quirk_entry ada_quirk_table[] =
 		 * 4k optimised
 		 */
 		{ T_DIRECT, SIP_MEDIA_FIXED, "*", "SAMSUNG SSD 830 Series*", "*" },
+		/*quirks*/ADA_Q_4K
+	},
+	{
+		/*
+		 * Samsung 840 SSDs
+		 * 4k optimised
+		 */
+		{ T_DIRECT, SIP_MEDIA_FIXED, "*", "Samsung SSD 840*", "*" },
 		/*quirks*/ADA_Q_4K
 	},
 	{
