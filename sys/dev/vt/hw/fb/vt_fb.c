ca5a70504df41a7667c56883c3bebc16f013caa3
dumbbell <dumbbell@FreeBSD.org>
2014-08-21 10:25:35 +0000

    vt(4): Colors are indexed against a console palette, not a VGA palette
    Rename vt_generate_vga_palette() to vt_generate_cons_palette() and
    change it to build a palette where the color index is the same than in
    terminal escape codes, not the VGA index. That's what TCHAR_CREATE()
    uses and passes to vt(4).
    
    The main differences between both orders are:
        o  Blue and red are swapped (1 <-> 4)
        o  Yellow and cyan are swapped (3 <-> 6)
    
    The problem remained unnoticed, because the RGB bit indexes passed to
    vt_generate_vga_palette() were reversed. This inversion was cancelled
    by the colors inversions in the generated palette. For instance, red
    (0xff0000) and blue (0x0000ff) have bytes in opposite order, but were
    swapped in the palette. But after changing the value of blue (see last
    paragraph), the modified color was in fact the red one.
    
    While here, tune the palette to better match console colors and improve
    the readability (especially the dark blue).
    
    This is an MFC of r269783 and r269791.

--- sys/dev/vt/hw/fb/vt_fb.c
+++ sys/dev/vt/hw/fb/vt_fb.c
@@ -268,18 +268,18 @@ vt_fb_init_cmap(uint32_t *cmap, int depth)
 
 	switch (depth) {
 	case 8:
-		return (vt_generate_vga_palette(cmap, COLOR_FORMAT_RGB,
+		return (vt_generate_cons_palette(cmap, COLOR_FORMAT_RGB,
 		    0x7, 5, 0x7, 2, 0x3, 0));
 	case 15:
-		return (vt_generate_vga_palette(cmap, COLOR_FORMAT_RGB,
+		return (vt_generate_cons_palette(cmap, COLOR_FORMAT_RGB,
 		    0x1f, 10, 0x1f, 5, 0x1f, 0));
 	case 16:
-		return (vt_generate_vga_palette(cmap, COLOR_FORMAT_RGB,
+		return (vt_generate_cons_palette(cmap, COLOR_FORMAT_RGB,
 		    0x1f, 11, 0x3f, 5, 0x1f, 0));
 	case 24:
 	case 32: /* Ignore alpha. */
-		return (vt_generate_vga_palette(cmap, COLOR_FORMAT_RGB,
-		    0xff, 0, 0xff, 8, 0xff, 16));
+		return (vt_generate_cons_palette(cmap, COLOR_FORMAT_RGB,
+		    0xff, 16, 0xff, 8, 0xff, 0));
 	default:
 		return (1);
 	}
