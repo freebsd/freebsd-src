27d7b4720df996e709a9b06cb942fad59fd327bd
andrew <andrew@FreeBSD.org>
2014-08-29 09:03:17 +0000

    __hldtoa uses compiler-rt symbols we don't provide.
    An update to compiler-rt will be needed for some symbols clang
    expects to be available.

--- lib/libc/gdtoa/_hldtoa.c
+++ lib/libc/gdtoa/_hldtoa.c
@@ -76,6 +76,8 @@ char *
 __hldtoa(long double e, const char *xdigs, int ndigits, int *decpt, int *sign,
     char **rve)
 {
+	abort();
+#if 0
 	union IEEEl2bits u;
 	char *s, *s0;
 	manh_t manh;
@@ -162,6 +164,7 @@ __hldtoa(long double e, const char *xdigs, int ndigits, int *decpt, int *sign,
 	if (rve != NULL)
 		*rve = s;
 	return (s0);
+#endif
 }
 
 #else	/* (LDBL_MANT_DIG == DBL_MANT_DIG) */
