bd6edcc077ffc3e30265b53cb1cff6c4f2e9d80c
trasz <trasz@FreeBSD.org>
2014-08-31 20:21:08 +0000

    MFC r270279:
    Make the iSCSI stack use __FBSDID() properly.
    
    Sponsored by:	The FreeBSD Foundation

--- sys/dev/iscsi/icl.c
+++ sys/dev/iscsi/icl.c
@@ -26,13 +26,15 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $FreeBSD$
  */
 
 /*
  * iSCSI Common Layer.  It's used by both the initiator and target to send
  * and receive iSCSI PDUs.
  */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
 #include <sys/capability.h>
