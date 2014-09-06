156d227468db2c1bac22adf18cbc2a1b29b28718
Alexander Motin <mav@FreeBSD.org>
2014-09-06 15:26:38 +0000

    MFC r270423: Restore pre-r239157 handling of sched_yield(), when thread time slice was aborted, allowing other threads to run.  Without this change thread is just rescheduled again, that was illustrated by provided test tool.
    PR:		192926
    Submitted by:	eric@vangyzen.net
    Approved by:	re (marius)

--- sys/kern/sched_4bsd.c
+++ sys/kern/sched_4bsd.c
@@ -983,7 +983,8 @@ sched_switch(struct thread *td, struct thread *newtd, int flags)
 		sched_load_rem();
 
 	td->td_lastcpu = td->td_oncpu;
-	preempted = !(td->td_flags & TDF_SLICEEND);
+	preempted = !((td->td_flags & TDF_SLICEEND) ||
+	    (flags & SWT_RELINQUISH));
 	td->td_flags &= ~(TDF_NEEDRESCHED | TDF_SLICEEND);
 	td->td_owepreempt = 0;
 	td->td_oncpu = NOCPU;
