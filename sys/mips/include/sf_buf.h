73675a9dc684b10e1953ed2f5631e7d48b2cd380
adrian <adrian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-09-06 22:38:32 +0000

    Implement local sfbuf_map and sfbuf_unmap for MIPS32.
    The pre-rework behaviour was not to keep the cached mappings around after
    the sfbuf was used but instead to recycle said mappings.
    
    PR:		kern/193400
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@271213 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/mips/include/sf_buf.h
+++ sys/mips/include/sf_buf.h
@@ -48,4 +48,23 @@ sf_buf_page(struct sf_buf *sf)
 }
 
 #endif /* __mips_n64 */
+
+#ifndef	__mips_n64	/* in 32 bit mode we manage our own mappings */
+
+static inline void
+sf_buf_map(struct sf_buf *sf, int flags)
+{
+
+	pmap_qenter(sf->kva, &sf->m, 1);
+}
+
+static inline int
+sf_buf_unmap(struct sf_buf *sf)
+{
+	pmap_qremove(sf->kva, 1);
+	return (1);
+}
+
+#endif	/* ! __mips_n64 */
+
 #endif /* !_MACHINE_SF_BUF_H_ */
