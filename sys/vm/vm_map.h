6d3129753a01dc6bf97295290d26bea8e73cc4d7
alc <alc@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-09-08 02:25:01 +0000

    Oops.  vm_map_simplify_entry() is used by mac_proc_vm_revoke_recurse(), so it can't be static.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@271244 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/vm/vm_map.h
+++ sys/vm/vm_map.h
@@ -381,6 +381,7 @@ void vm_map_lookup_done (vm_map_t, vm_map_entry_t);
 boolean_t vm_map_lookup_entry (vm_map_t, vm_offset_t, vm_map_entry_t *);
 int vm_map_protect (vm_map_t, vm_offset_t, vm_offset_t, vm_prot_t, boolean_t);
 int vm_map_remove (vm_map_t, vm_offset_t, vm_offset_t);
+void vm_map_simplify_entry(vm_map_t map, vm_map_entry_t entry);
 void vm_map_startup (void);
 int vm_map_submap (vm_map_t, vm_offset_t, vm_offset_t, vm_map_t);
 int vm_map_sync(vm_map_t, vm_offset_t, vm_offset_t, boolean_t, boolean_t);
