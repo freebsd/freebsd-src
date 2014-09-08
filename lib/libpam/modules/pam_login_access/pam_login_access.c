f8a7a673e61ca0bc8c20c53b3dcf770f9d43c65d
des <des@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-09-08 09:19:01 +0000

    Fail rather than segfault if neither PAM_TTY nor PAM_RHOST is set.
    PR:		83099
    MFC after:	3 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@271256 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libpam/modules/pam_login_access/pam_login_access.c
+++ lib/libpam/modules/pam_login_access/pam_login_access.c
@@ -79,7 +79,14 @@ pam_sm_acct_mgmt(pam_handle_t *pamh, int flags __unused,
 
 	gethostname(hostname, sizeof hostname);
 
-	if (rhost == NULL || *(const char *)rhost == '\0') {
+	if (rhost != NULL && *(const char *)rhost != '\0') {
+		PAM_LOG("Checking login.access for user %s from host %s",
+		    (const char *)user, (const char *)rhost);
+		if (login_access(user, rhost) != 0)
+			return (PAM_SUCCESS);
+		PAM_VERBOSE_ERROR("%s is not allowed to log in from %s",
+		    user, rhost);
+	} else if (tty != NULL || *(const char *)tty != '\0') {
 		PAM_LOG("Checking login.access for user %s on tty %s",
 		    (const char *)user, (const char *)tty);
 		if (login_access(user, tty) != 0)
@@ -87,12 +94,8 @@ pam_sm_acct_mgmt(pam_handle_t *pamh, int flags __unused,
 		PAM_VERBOSE_ERROR("%s is not allowed to log in on %s",
 		    user, tty);
 	} else {
-		PAM_LOG("Checking login.access for user %s from host %s",
-		    (const char *)user, (const char *)rhost);
-		if (login_access(user, rhost) != 0)
-			return (PAM_SUCCESS);
-		PAM_VERBOSE_ERROR("%s is not allowed to log in from %s",
-		    user, rhost);
+		PAM_VERBOSE_ERROR("PAM_RHOST or PAM_TTY required");
+		return (PAM_AUTHINFO_UNAVAIL);
 	}
 
 	return (PAM_AUTH_ERR);
