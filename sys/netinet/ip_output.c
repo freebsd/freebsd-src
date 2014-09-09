5a667fcd3803767c016280e2c9e8068c76a0d28c
adrian <adrian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-09-09 00:19:02 +0000

    Add a flag to ip_output() - IP_NODEFAULTFLOWID - which prevents it from overriding an existing flowid/flowtype field in the outbound mbuf with the inp_flowid/inp_flowtype details.
    The upcoming RSS UDP support calculates a valid RSS value for outbound
    mbufs and since it may change per send, it doesn't cache it in the inpcb.
    So overriding it here would be wrong.
    
    Differential Revision:	https://reviews.freebsd.org/D527
    Reviewed by:	grehan
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@271291 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/ip_output.c
+++ sys/netinet/ip_output.c
@@ -145,7 +145,8 @@ ip_output(struct mbuf *m, struct mbuf *opt, struct route *ro, int flags,
 	if (inp != NULL) {
 		INP_LOCK_ASSERT(inp);
 		M_SETFIB(m, inp->inp_inc.inc_fibnum);
-		if (inp->inp_flags & (INP_HW_FLOWID|INP_SW_FLOWID)) {
+		if (((flags & IP_NODEFAULTFLOWID) == 0) &&
+		    inp->inp_flags & (INP_HW_FLOWID|INP_SW_FLOWID)) {
 			m->m_pkthdr.flowid = inp->inp_flowid;
 			M_HASHTYPE_SET(m, inp->inp_flowtype);
 			m->m_flags |= M_FLOWID;
