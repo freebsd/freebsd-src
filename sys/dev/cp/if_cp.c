505a5eaa3d0bdd0d64057fb44ca77f36a8e0666c
rwatson <rwatson@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-09-10 09:57:32 +0000

    Replace local copy-and-paste implementations of printmbuf() in several device drivers with calls to the centralised m_print() implementation. While the formatting and output details differ a little, the content is essentially the same, and it is unlikely anyone has used this debugging output in some time.
    This change reduces awareness of mbuf cluster allocation (and,
    especially, the M_EXT flag) outside of the mbuf allocator, which will
    make it easier to refine the external storage mechanism without
    disrupting drivers in the future.
    
    Style bugs are preserved.
    
    Reviewed by:	bz, glebius
    MFC after:	3 days
    Sponsored by:	EMC / Isilon Storage Division
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@271373 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/cp/if_cp.c
+++ sys/dev/cp/if_cp.c
@@ -182,22 +182,6 @@ static struct cdevsw cp_cdevsw = {
 };
 
 /*
- * Print the mbuf chain, for debug purposes only.
- */
-static void printmbuf (struct mbuf *m)
-{
-	printf ("mbuf:");
-	for (; m; m=m->m_next) {
-		if (m->m_flags & M_PKTHDR)
-			printf (" HDR %d:", m->m_pkthdr.len);
-		if (m->m_flags & M_EXT)
-			printf (" EXT:");
-		printf (" %d", m->m_len);
-	}
-	printf ("\n");
-}
-
-/*
  * Make an mbuf from data.
  */
 static struct mbuf *makembuf (void *buf, unsigned len)
@@ -909,7 +893,7 @@ static void cp_receive (cp_chan_t *c, unsigned char *data, int len)
 		return;
 	}
 	if (c->debug > 1)
-		printmbuf (m);
+		m_print (m, 0);
 #ifdef NETGRAPH
 	m->m_pkthdr.rcvif = 0;
 	NG_SEND_DATA_ONLY (error, d->hook, m);
