ea8cbe1ddb8328ee712998622880bb6917505bd9
sbruno <sbruno@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-09-11 22:39:27 +0000

    Add device name used in geom_map verbose output.  This helps when using geom_map with multiple flash/spi devices.
    Phabric:  https://reviews.freebsd.org/D766
    Reviewed by:	adrian
    MFC after:	2 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@271442 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/geom/geom_map.c
+++ sys/geom/geom_map.c
@@ -153,8 +153,8 @@ find_marker(struct g_consumer *cp, const char *line, off_t *offset)
 		return (1);
 
 	if (bootverbose) {
-		printf("MAP: search key \"%s\" from 0x%jx, step 0x%jx\n",
-		    search_key, (intmax_t)search_start, (intmax_t)search_step);
+		printf("MAP: search %s for key \"%s\" from 0x%jx, step 0x%jx\n",
+		    cp->geom->name, search_key, (intmax_t)search_start, (intmax_t)search_step);
 	}
 
 	/* error if search_key is empty */
@@ -321,9 +321,9 @@ g_map_parse_part(struct g_class *mp, struct g_provider *pp,
 	}
 
 	if (bootverbose) {
-		printf("MAP: %jxx%jx, data=%jxx%jx "
+		printf("MAP: %s: %jxx%jx, data=%jxx%jx "
 		    "\"/dev/map/%s\"\n",
-		    (intmax_t)start, (intmax_t)size, (intmax_t)offset,
+		    cp->geom->name, (intmax_t)start, (intmax_t)size, (intmax_t)offset,
 		    (intmax_t)dsize, name);
 	}
 
