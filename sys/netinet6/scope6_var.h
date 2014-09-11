41874e85d6edda0cebdc84cb279c8de5cf88ba38
Andrey V. Elsukov <ae@FreeBSD.org>
2014-09-11 12:33:37 +0000

    Introduce new scope related functions.
    * new macro to remove magic number - IPV6_ADDR_SCOPES_COUNT;
    * sa6_checkzone() - this function checks sockaddr_in6 structure
      for correctness of sin6_scope_id. It also can fill correct
      value sometimes.
    * in6_getscopezone() - this function returns scope zone id for
      specified interface and scope.
    * in6_getlinkifnet() - this function returns struct ifnet for
      corresponding zone id of link-local scope.
    
    Obtained from:	Yandex LLC
    Sponsored by:	Yandex LLC

--- sys/netinet6/scope6_var.h
+++ sys/netinet6/scope6_var.h
@@ -36,12 +36,13 @@
 #ifdef _KERNEL
 #include <net/vnet.h>
 
+#define	IPV6_ADDR_SCOPES_COUNT	16
 struct scope6_id {
 	/*
 	 * 16 is correspondent to 4bit multicast scope field.
 	 * i.e. from node-local to global with some reserved/unassigned types.
 	 */
-	u_int32_t s6id_list[16];
+	uint32_t s6id_list[IPV6_ADDR_SCOPES_COUNT];
 };
 
 VNET_DECLARE(int, deembed_scopeid);
@@ -56,9 +57,12 @@ int	scope6_get_default(struct scope6_id *);
 u_int32_t scope6_addr2default(struct in6_addr *);
 int	sa6_embedscope(struct sockaddr_in6 *, int);
 int	sa6_recoverscope(struct sockaddr_in6 *);
+int	sa6_checkzone(struct sockaddr_in6 *);
 int	in6_setscope(struct in6_addr *, struct ifnet *, u_int32_t *);
 int	in6_clearscope(struct in6_addr *);
 uint16_t in6_getscope(struct in6_addr *);
+uint32_t in6_getscopezone(const struct ifnet *, int);
+struct ifnet* in6_getlinkifnet(uint32_t);
 #endif /* _KERNEL */
 
 #endif /* _NETINET6_SCOPE6_VAR_H_ */
