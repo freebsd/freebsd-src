2ee41ff6f35356865523a81ef1fb4fb3cc37254e
Xin LI <delphij@FreeBSD.org>
2014-09-13 15:05:10 +0000

    5136 fix write throttle comment in dsl_pool.c Reviewed by: Adam Leventhal <ahl@delphix.com> Reviewed by: Christopher Siden <christopher.siden@delphix.com> Reviewed by: Max Grossman <max.grossman@delphix.com> Reviewed by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net> Reviewed by: Richard Elling <richard.elling@gmail.com> Approved by: Dan McDonald <danmcd@omniti.com> Author: Matthew Ahrens <mahrens@delphix.com>
    illumos/illumos-gate@d85a1e969ce08b3bc4a7cb3626a6e33a28b5caeb

--- uts/common/fs/zfs/dsl_pool.c
+++ uts/common/fs/zfs/dsl_pool.c
@@ -111,8 +111,8 @@ int zfs_delay_min_dirty_percent = 60;
 
 /*
  * This controls how quickly the delay approaches infinity.
- * Larger values cause it to delay less for a given amount of dirty data.
- * Therefore larger values will cause there to be more dirty data for a
+ * Larger values cause it to delay more for a given amount of dirty data.
+ * Therefore larger values will cause there to be less dirty data for a
  * given throughput.
  *
  * For the smoothest delay, this value should be about 1 billion divided
@@ -123,12 +123,6 @@ int zfs_delay_min_dirty_percent = 60;
  * multiply in dmu_tx_delay().
  */
 uint64_t zfs_delay_scale = 1000 * 1000 * 1000 / 2000;
-
-
-/*
- * XXX someday maybe turn these into #defines, and you have to tune it on a
- * per-pool basis using zfs.conf.
- */
 
 
 hrtime_t zfs_throttle_delay = MSEC2NSEC(10);
