5545fadd845adf3a8884d3d33ab8ce86b101fee4
Jilles Tjoelker <jilles@FreeBSD.org>
2014-09-14 15:59:15 +0000

    sh: Add some const keywords.

--- bin/sh/input.h
+++ bin/sh/input.h
@@ -53,7 +53,7 @@ int pgetc(void);
 int preadbuffer(void);
 int preadateof(void);
 void pungetc(void);
-void pushstring(char *, int, struct alias *);
+void pushstring(const char *, int, struct alias *);
 void setinputfile(const char *, int);
 void setinputfd(int, int);
 void setinputstring(const char *, int);
