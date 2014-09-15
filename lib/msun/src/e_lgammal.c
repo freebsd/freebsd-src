80b8071609d328fad184f145d51bb9d0fd790d9c
kargl <kargl@FreeBSD.org>
2014-09-15 23:21:57 +0000

    * Makefile:   . Hook e_lgammal[_r].c to the build.   . Create man page links for lgammal[-r].3.
    * Symbol.map:
      . Sort lgammal to its rightful place.
      . Add FBSD_1.4 section for the new lgamal_r symbol.
    
    * ld128/e_lgammal_r.c:
      . 128-bit implementataion of lgammal_r().
    
    * ld80/e_lgammal_r.c:
      . Intel 80-bit format implementation of lgammal_r().
    
    * src/e_lgamma.c:
      . Expose lgammal as a weak reference to lgamma for platforms
        where long double is mapped to double.
    
    * src/e_lgamma_r.c:
      . Use integer literal constants instead of real literal constants.
        Let compiler(s) do the job of conversion to the appropriate type.
      . Expose lgammal_r as a weak reference to lgamma_r for platforms
        where long double is mapped to double.
    
    * src/e_lgammaf_r.c:
      . Fixed the Cygnus Support conversion of e_lgamma_r.c to float.
        This includes the generation of new polynomial and rational
        approximations with fewer terms.  For each approximation, include
        a comment on an estimate of the accuracy over the relevant domain.
      . Use integer literal constants instead of real literal constants.
        Let compiler(s) do the job of conversion to the appropriate type.
        This allows the removal of several explicit casts of double values
        to float.
    
    * src/e_lgammal.c:
      . Wrapper for lgammal() about lgammal_r().
    
    * src/imprecise.c:
      . Remove the lgamma.
    
    * src/math.h:
      . Add a prototype for lgammal_r().
    
    * man/lgamma.3:
      . Document the new functions.
    
    Reviewed by:	bde

--- lib/msun/src/e_lgammal.c
+++ lib/msun/src/e_lgammal.c
@@ -1,4 +1,3 @@
-
 /* @(#)e_lgamma.c 1.3 95/01/18 */
 /*
  * ====================================================
@@ -6,34 +5,21 @@
  *
  * Developed at SunSoft, a Sun Microsystems, Inc. business.
  * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice 
+ * software is freely granted, provided that this notice
  * is preserved.
  * ====================================================
- *
  */
 
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
-
-/* __ieee754_lgamma(x)
- * Return the logarithm of the Gamma function of x.
- *
- * Method: call __ieee754_lgamma_r
- */
-
-#include <float.h>
 
 #include "math.h"
 #include "math_private.h"
 
 extern int signgam;
 
-double
-__ieee754_lgamma(double x)
+long double
+lgammal(long double x)
 {
-	return __ieee754_lgamma_r(x,&signgam);
+	return lgammal_r(x,&signgam);
 }
-
-#if (LDBL_MANT_DIG == 53)
-__weak_reference(lgamma, lgammal);
-#endif
