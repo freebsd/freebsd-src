ace3f7ae8ae332ac376e7f39d82e5e7cf743c2e5
Dimitry Andric <dim@FreeBSD.org>
2014-09-18 06:47:48 +0000

    MFC r257658 (modified to apply to stable/7):
    For C++ programs, don't emit any explicit dependency to libstdc++.a or
    libc++.a during the early build stages (bootstrap-tools, build-tools,
    cross-tools), since it is not possible to know in advance which C++
    library is available on the host system.
    
    Instead, just use the bootstrap compiler's built-in default.  This
    should eventually make it possible to build stable/9 on head, or on
    stable/10, which ship without libstdc++ by default.
    
    [Note: this should get the stable/7 tinderbox at least a bit further in
    buildworld.]

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -126,7 +126,7 @@ _EXTRADEPEND:
 .endif
 .else
 	echo ${PROG}: ${LIBC} ${DPADD} >> ${DEPENDFILE}
-.if defined(PROG_CXX)
+.if defined(PROG_CXX) && !defined(BOOTSTRAPPING)
 	echo ${PROG}: ${LIBSTDCPLUSPLUS} >> ${DEPENDFILE}
 .endif
 .endif
