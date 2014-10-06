1be3714fc8a45d63ca39efe4f179499f7d58d66e
mav <mav@FreeBSD.org>
2014-10-06 13:29:15 +0000

    MFC r271954: Deny ANCHOR flag set without UNMAP flag set in WRITE SAME commands.

--- sys/cam/ctl/ctl.c
+++ sys/cam/ctl/ctl.c
@@ -5989,8 +5989,9 @@ ctl_write_same(struct ctl_scsiio *ctsio)
 		break; /* NOTREACHED */
 	}
 
-	/* NDOB flag can be used only together with UNMAP */
-	if ((byte2 & (SWS_NDOB | SWS_UNMAP)) == SWS_NDOB) {
+	/* NDOB and ANCHOR flags can be used only together with UNMAP */
+	if ((byte2 & SWS_UNMAP) == 0 &&
+	    (byte2 & (SWS_NDOB | SWS_ANCHOR)) != 0) {
 		ctl_set_invalid_field(ctsio, /*sks_valid*/ 1,
 		    /*command*/ 1, /*field*/ 1, /*bit_valid*/ 1, /*bit*/ 0);
 		ctl_done((union ctl_io *)ctsio);
