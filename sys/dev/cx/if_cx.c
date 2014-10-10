ebe31204a33c5a256e06dbb57f65586a59b4c914
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-10-10 19:12:04 +0000

    Don't pass RF_ALLOCATED to bus_alloc_resource().
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@272904 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/cx/if_cx.c
+++ sys/dev/cx/if_cx.c
@@ -410,8 +410,7 @@ static int cx_is_free_res (device_t dev, int rid, int type, u_long start,
 {
 	struct resource *res;
 	
-	if (!(res = bus_alloc_resource (dev, type, &rid, start, end, count,
-	    RF_ALLOCATED)))
+	if (!(res = bus_alloc_resource (dev, type, &rid, start, end, count, 0)))
 		return 0;
 		
 	bus_release_resource (dev, type, rid, res);
