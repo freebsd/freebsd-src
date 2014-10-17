0e53ac55b70c7ac8e919f5fedc3b2a244193ddce
Ed Maste <emaste@FreeBSD.org>
2014-10-17 13:55:45 +0000

    Do nothing in vt_upgrade if there is no vt driver
    Previously, if no drivers attached at boot we would panic with
    "vtbuf_fill_locked begin.tp_row 0 must be < screen height 0".
    
    PR:		192248
    Reviewed by:	ray
    MFC after:	3 days
    Sponsored by:	DARPA, AFRL
    Differential Revision: https://reviews.freebsd.org/D954

--- sys/dev/vt/vt_core.c
+++ sys/dev/vt/vt_core.c
@@ -2498,6 +2498,8 @@ vt_upgrade(struct vt_device *vd)
 
 	if (!vty_enabled(VTY_VT))
 		return;
+	if (main_vd->vd_driver == NULL)
+		return;
 
 	for (i = 0; i < VT_MAXWINDOWS; i++) {
 		vw = vd->vd_windows[i];
