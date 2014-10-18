07ca323d0e8328231c2869a45158c2366b682e21
Pedro F. Giffuni <pfg@FreeBSD.org>
2014-10-18 19:22:59 +0000

    MFC	r272958, r273006:
    ddb: space/tab fixes and ANSI-fy function declarations.
    
    No functional change.

--- sys/ddb/db_textdump.c
+++ sys/ddb/db_textdump.c
@@ -348,8 +348,8 @@ textdump_dump_msgbuf(struct dumperinfo *di)
 	 */
 	total_len = 0;
 	offset = 0;
-        msgbuf_peekbytes(msgbufp, NULL, 0, &seq);
-        while ((len = msgbuf_peekbytes(msgbufp, buf, sizeof(buf), &seq)) > 0) {
+	msgbuf_peekbytes(msgbufp, NULL, 0, &seq);
+	while ((len = msgbuf_peekbytes(msgbufp, buf, sizeof(buf), &seq)) > 0) {
 		for (i = 0; i < len; i++) {
 			if (buf[i] == '\0')
 				continue;
@@ -362,7 +362,7 @@ textdump_dump_msgbuf(struct dumperinfo *di)
 			total_len += offset;
 			offset = 0;
 		}
-        }
+	}
 	total_len += offset;	/* Without the zero-padding. */
 	if (offset != 0) {
 		bzero(textdump_block_buffer + offset,
