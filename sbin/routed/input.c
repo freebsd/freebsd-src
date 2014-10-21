588a04005b552b200ad780989a9dc4ae1cdd44ca
delphij <delphij@FreeBSD.org>
2014-10-21 20:21:10 +0000

    Fix rtsold(8) remote buffer overflow vulnerability. [SA-14:20]
    Fix routed(8) remote denial of service vulnerability. [SA-14:21]
    
    Fix memory leak in sandboxed namei lookup. [SA-14:22]
    
    Fix OpenSSL multiple vulnerabilities. [SA-14:23]
    
    Approved by:	so

--- sbin/routed/input.c
+++ sbin/routed/input.c
@@ -288,6 +288,10 @@ input(struct sockaddr_in *from,		/* received from this IP address */
 				/* Answer a query from a utility program
 				 * with all we know.
 				 */
+				if (aifp == NULL) {
+					trace_pkt("ignore remote query");
+					return;
+				}
 				if (from->sin_port != htons(RIP_PORT)) {
 					supply(from, aifp, OUT_QUERY, 0,
 					       rip->rip_vers, ap != 0);
