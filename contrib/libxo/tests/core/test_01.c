8b02936eb5d99746a7e6dded2d33ac70d996c0c4
marcel <marcel@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-10-23 22:30:14 +0000

    Import libxo 0.1.4
    Obtained from:	https://github.com/Juniper/libxo
    Sponsored by:	Juniper Networks, Inc.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@273562 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

---
+++ contrib/libxo/tests/core/test_01.c
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2014, Juniper Networks, Inc.
+ * All rights reserved.
+ * This SOFTWARE is licensed under the LICENSE provided in the
+ * ../Copyright file. By downloading, installing, copying, or otherwise
+ * using the SOFTWARE, you agree to be bound by the terms of that
+ * LICENSE.
+ * Phil Shafer, July 2014
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "xo.h"
+
+int
+main (int argc, char **argv)
+{
+    static char base_grocery[] = "GRO";
+    static char base_hardware[] = "HRD";
+    struct item {
+	const char *i_title;
+	int i_sold;
+	int i_instock;
+	int i_onorder;
+	const char *i_sku_base;
+	int i_sku_num;
+    };
+    struct item list[] = {
+	{ "gum", 1412, 54, 10, base_grocery, 415 },
+	{ "rope", 85, 4, 2, base_hardware, 212 },
+	{ "ladder", 0, 2, 1, base_hardware, 517 },
+	{ "bolt", 4123, 144, 42, base_hardware, 632 },
+	{ "water", 17, 14, 2, base_grocery, 2331 },
+	{ NULL, 0, 0, 0, NULL, 0 }
+    };
+    struct item list2[] = {
+	{ "fish", 1321, 45, 1, base_grocery, 533 },
+	{ NULL, 0, 0, 0, NULL, 0 }
+    };
+    struct item *ip;
+    xo_info_t info[] = {
+	{ "in-stock", "number", "Number of items in stock" },
+	{ "name", "string", "Name of the item" },
+	{ "on-order", "number", "Number of items on order" },
+	{ "sku", "string", "Stock Keeping Unit" },
+	{ "sold", "number", "Number of items sold" },
+	{ NULL, NULL, NULL },
+    };
+    int info_count = (sizeof(info) / sizeof(info[0])) - 1;
+    
+    argc = xo_parse_args(argc, argv);
+    if (argc < 0)
+	return 1;
+
+    for (argc = 1; argv[argc]; argc++) {
+	if (strcmp(argv[argc], "xml") == 0)
+	    xo_set_style(NULL, XO_STYLE_XML);
+	else if (strcmp(argv[argc], "json") == 0)
+	    xo_set_style(NULL, XO_STYLE_JSON);
+	else if (strcmp(argv[argc], "text") == 0)
+	    xo_set_style(NULL, XO_STYLE_TEXT);
+	else if (strcmp(argv[argc], "html") == 0)
+	    xo_set_style(NULL, XO_STYLE_HTML);
+	else if (strcmp(argv[argc], "pretty") == 0)
+	    xo_set_flags(NULL, XOF_PRETTY);
+	else if (strcmp(argv[argc], "xpath") == 0)
+	    xo_set_flags(NULL, XOF_XPATH);
+	else if (strcmp(argv[argc], "info") == 0)
+	    xo_set_flags(NULL, XOF_INFO);
+    }
+
+    xo_set_info(NULL, info, info_count);
+    xo_set_flags(NULL, XOF_KEYS);
+
+    xo_open_container_h(NULL, "top");
+
+    xo_open_container("data");
+    xo_open_list("item");
+
+    xo_emit("{T:Item/%-10s}{T:Total Sold/%12s}{T:In Stock/%12s}"
+	    "{T:On Order/%12s}{T:SKU/%5s}\n");
+
+    for (ip = list; ip->i_title; ip++) {
+	xo_open_instance("item");
+
+	xo_emit("{keq:sku/%s-%u/%s-000-%u}"
+		"{k:name/%-10s/%s}{n:sold/%12u/%u}{:in-stock/%12u/%u}"
+		"{:on-order/%12u/%u}{qkd:sku/%5s-000-%u/%s-000-%u}\n",
+		ip->i_sku_base, ip->i_sku_num,
+		ip->i_title, ip->i_sold, ip->i_instock, ip->i_onorder,
+		ip->i_sku_base, ip->i_sku_num);
+
+	xo_close_instance("item");
+    }
+
+    xo_close_list("item");
+    xo_close_container("data");
+
+    xo_emit("\n\n");
+
+    xo_open_container("data");
+    xo_open_list("item");
+
+    for (ip = list; ip->i_title; ip++) {
+	xo_open_instance("item");
+
+	xo_emit("{keq:sku/%s-%u/%s-000-%u}", ip->i_sku_base, ip->i_sku_num);
+	xo_emit("{L:Item} '{k:name/%s}':\n", ip->i_title);
+	xo_emit("{P:   }{L:Total sold}: {n:sold/%u%s}\n",
+		ip->i_sold, ip->i_sold ? ".0" : "");
+	xo_emit("{P:   }{Lcw:In stock}{:in-stock/%u}\n", ip->i_instock);
+	xo_emit("{P:   }{Lcw:On order}{:on-order/%u}\n", ip->i_onorder);
+	xo_emit("{P:   }{L:SKU}: {qkd:sku/%s-000-%u}\n",
+		ip->i_sku_base, ip->i_sku_num);
+
+	xo_close_instance("item");
+    }
+
+    xo_close_list("item");
+    xo_close_container("data");
+
+    xo_open_container("data");
+    xo_open_list("item");
+
+    for (ip = list2; ip->i_title; ip++) {
+	xo_open_instance("item");
+
+	xo_emit("{keq:sku/%s-%u/%s-000-%u}", ip->i_sku_base, ip->i_sku_num);
+	xo_emit("{L:Item} '{k:name/%s}':\n", ip->i_title);
+	xo_emit("{P:   }{L:Total sold}: {n:sold/%u%s}\n",
+		ip->i_sold, ip->i_sold ? ".0" : "");
+	xo_emit("{P:   }{Lcw:In stock}{:in-stock/%u}\n", ip->i_instock);
+	xo_emit("{P:   }{Lcw:On order}{:on-order/%u}\n", ip->i_onorder);
+	xo_emit("{P:   }{L:SKU}: {qkd:sku/%s-000-%u}\n",
+		ip->i_sku_base, ip->i_sku_num);
+
+	xo_close_instance("item");
+    }
+
+    xo_close_list("item");
+    xo_close_container("data");
+
+    xo_close_container_h(NULL, "top");
+
+    xo_finish();
+
+    return 0;
+}
