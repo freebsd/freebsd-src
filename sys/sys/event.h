42ed59292e6b1396cd7ee9e0c052b7f332c7f5bf
ian <ian@FreeBSD.org>
2014-10-26 18:46:03 +0000

    MFC r272528:  Make kevent(2) periodic timer events more reliably periodic.

--- sys/sys/event.h
+++ sys/sys/event.h
@@ -214,6 +214,7 @@ struct knote {
 		struct		proc *p_proc;	/* proc pointer */
 		struct		aiocblist *p_aio;	/* AIO job pointer */
 		struct		aioliojob *p_lio;	/* LIO job pointer */ 
+		sbintime_t	*p_nexttime;	/* next timer event fires at */
 	} kn_ptr;
 	struct			filterops *kn_fop;
 	void			*kn_hook;
