3b640d6bfc72630e851e489fea2449f0201e8c88
Baptiste Daroussin <bapt@FreeBSD.org>
2014-10-28 15:33:15 +0000

    Fix atf syntax

--- usr.sbin/pw/tests/pw_delete.sh
+++ usr.sbin/pw/tests/pw_delete.sh
@@ -27,7 +27,7 @@ group_do_not_delete_wheel_if_group_unkown_body() {
 	populate_etc_skel
 	atf_check -s exit:0 -o inline:"wheel:*:0:root\n" -x pw -V ${HOME} groupshow wheel
 	atf_check -e inline:"pw: -g expects a number\n" -s exit:64 -x pw -V ${HOME} groupdel -g I_do_not_exist
-	atf_check -s exit:0 -o "wheel:*:0:root\n" -x pw -V ${HOME} groupshow wheel
+	atf_check -s exit:0 -o inline:"wheel:*:0:root\n" -x pw -V ${HOME} groupshow wheel
 }
 
 atf_init_test_cases() {
