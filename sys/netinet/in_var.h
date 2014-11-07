d7ead6f1665190d2cd64931c7a8588eef0a7fe39
melifaro <melifaro@FreeBSD.org>
2014-11-07 00:29:00 +0000

    Remove legacy inet lookup functions.

--- sys/netinet/in_var.h
+++ sys/netinet/in_var.h
@@ -381,7 +381,6 @@ inm_acquire_locked(struct in_multi *inm)
 #define MCAST_MUTED		3	/* [deprecated] */
 
 struct	rtentry;
-struct	route;
 struct	ip_moptions;
 struct radix_node_head;
 
@@ -419,9 +418,6 @@ void	in_domifdetach(struct ifnet *, void *);
 
 
 /* XXX */
-void	 in_rtalloc_ign(struct route *ro, u_long ignflags, u_int fibnum);
-void	 in_rtalloc(struct route *ro, u_int fibnum);
-struct rtentry *in_rtalloc1(struct sockaddr *, int, u_long, u_int);
 void	 in_rtredirect(struct sockaddr *, struct sockaddr *,
 	    struct sockaddr *, int, struct sockaddr *, u_int);
 int	 in_rtrequest(int, struct sockaddr *,
