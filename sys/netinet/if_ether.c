b4b1367ae445f81d591a0fa3fc1c062b90f212f2
Alexander V. Chernikov <melifaro@FreeBSD.org>
2014-11-15 18:54:07 +0000

    * Move lle creation/deletion from lla_lookup to separate functions:   lla_lookup(LLE_CREATE) -> lla_create   lla_lookup(LLE_DELETE) -> lla_delete   Assume lla_create to return LLE_EXCLUSIVE lock for lle. * Rework lla_rt_output to perform all lle changes under afdata WLOCK. * change arp_ifscrub() ackquire afdata WLOCK, the same as arp_ifinit().

--- sys/netinet/if_ether.c
+++ sys/netinet/if_ether.c
@@ -152,10 +152,9 @@ arp_ifscrub(struct ifnet *ifp, uint32_t addr)
 	addr4.sin_len    = sizeof(addr4);
 	addr4.sin_family = AF_INET;
 	addr4.sin_addr.s_addr = addr;
-	IF_AFDATA_RLOCK(ifp);
-	lla_lookup(LLTABLE(ifp), (LLE_DELETE | LLE_IFADDR),
-	    (struct sockaddr *)&addr4);
-	IF_AFDATA_RUNLOCK(ifp);
+	IF_AFDATA_WLOCK(ifp);
+	lla_delete(LLTABLE(ifp), LLE_IFADDR, (struct sockaddr *)&addr4);
+	IF_AFDATA_WUNLOCK(ifp);
 }
 #endif
 
@@ -373,8 +372,9 @@ arpresolve(struct ifnet *ifp, struct rtentry *rt0, struct mbuf *m,
 	u_int flags = 0;
 	struct mbuf *curr = NULL;
 	struct mbuf *next = NULL;
-	int error, renew;
+	int create, error, renew;
 
+	create = 0;
 	*lle = NULL;
 	if (m != NULL) {
 		if (m->m_flags & M_BCAST) {
@@ -395,13 +395,14 @@ retry:
 	IF_AFDATA_RUNLOCK(ifp);
 	if ((la == NULL) && ((flags & LLE_EXCLUSIVE) == 0)
 	    && ((ifp->if_flags & (IFF_NOARP | IFF_STATICARP)) == 0)) {
-		flags |= (LLE_CREATE | LLE_EXCLUSIVE);
+		create = 1;
+		flags |= LLE_EXCLUSIVE;
 		IF_AFDATA_WLOCK(ifp);
-		la = lla_lookup(LLTABLE(ifp), flags, dst);
+		la = lla_create(LLTABLE(ifp), flags, dst);
 		IF_AFDATA_WUNLOCK(ifp);
 	}
 	if (la == NULL) {
-		if (flags & LLE_CREATE)
+		if (create != 0)
 			log(LOG_DEBUG,
 			    "arpresolve: can't allocate llinfo for %s on %s\n",
 			    inet_ntoa(SIN(dst)->sin_addr), ifp->if_xname);
@@ -613,7 +614,7 @@ in_arpinput(struct mbuf *m)
 	int op, flags;
 	int req_len;
 	int bridged = 0, is_bridge = 0;
-	int carped;
+	int carped, create;
 	struct nhop4_extended nh_ext;
 	struct sockaddr_in sin;
 	sin.sin_len = sizeof(struct sockaddr_in);
@@ -765,10 +766,13 @@ match:
 	sin.sin_len = sizeof(struct sockaddr_in);
 	sin.sin_family = AF_INET;
 	sin.sin_addr = isaddr;
-	flags = (itaddr.s_addr == myaddr.s_addr) ? LLE_CREATE : 0;
-	flags |= LLE_EXCLUSIVE;
+	create = (itaddr.s_addr == myaddr.s_addr) ? 1 : 0;
+	flags = LLE_EXCLUSIVE;
 	IF_AFDATA_LOCK(ifp);
-	la = lla_lookup(LLTABLE(ifp), flags, (struct sockaddr *)&sin);
+	if (create != 0)
+		la = lla_create(LLTABLE(ifp), 0, (struct sockaddr *)&sin);
+	else
+		la = lla_lookup(LLTABLE(ifp), flags, (struct sockaddr *)&sin);
 	IF_AFDATA_UNLOCK(ifp);
 	if (la != NULL) {
 		/* the following is not an error when doing bridging */
@@ -975,14 +979,14 @@ arp_ifinit(struct ifnet *ifp, struct ifaddr *ifa)
 		 * that L2 entry as permanent
 		 */
 		IF_AFDATA_LOCK(ifp);
-		lle = lla_lookup(LLTABLE(ifp), (LLE_CREATE | LLE_IFADDR | LLE_STATIC),
+		lle = lla_create(LLTABLE(ifp), LLE_IFADDR | LLE_STATIC,
 				 (struct sockaddr *)IA_SIN(ifa));
 		IF_AFDATA_UNLOCK(ifp);
 		if (lle == NULL)
 			log(LOG_INFO, "arp_ifinit: cannot create arp "
 			    "entry for interface address\n");
 		else
-			LLE_RUNLOCK(lle);
+			LLE_WUNLOCK(lle);
 	}
 	ifa->ifa_rtrequest = NULL;
 }
