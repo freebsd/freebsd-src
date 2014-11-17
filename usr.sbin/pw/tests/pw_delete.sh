f3bcbf239921b6180bf1a4f544b02f0afbc5089a
Brad Davis <brd@FreeBSD.org>
2014-11-17 15:44:00 +0000

    Clean up some language
    Reviewed by:	will

--- usr.sbin/pw/tests/pw_delete.sh
+++ usr.sbin/pw/tests/pw_delete.sh
@@ -15,33 +15,33 @@ rmuser_seperate_group_body() {
 	pw -V ${HOME} useradd test || atf_fail "Creating test user"
 	pw -V ${HOME} groupmod test -M 'test,root' || \
 		atf_fail "Modifying the group"
-	pw -V ${HOME} userdel test || atf_fail "delete the user"
+	pw -V ${HOME} userdel test || atf_fail "Delete the test user"
 }
 
-atf_test_case group_do_not_delete_wheel_if_group_unkown
-group_do_not_delete_wheel_if_group_unkown_head() {
-	atf_set "descr" "Make sure we do not consider as gid 0 an unknown group"
+atf_test_case group_do_not_delete_wheel_if_group_unknown
+group_do_not_delete_wheel_if_group_unknown_head() {
+	atf_set "descr" "Make sure we do not consider gid 0 an unknown group"
 }
 
-group_do_not_delete_wheel_if_group_unkown_body() {
+group_do_not_delete_wheel_if_group_unknown_body() {
 	populate_etc_skel
 	atf_check -s exit:0 -o inline:"wheel:*:0:root\n" -x pw -V ${HOME} groupshow wheel
 	atf_check -e inline:"pw: -g expects a number\n" -s exit:64 -x pw -V ${HOME} groupdel -g I_do_not_exist
 	atf_check -s exit:0 -o inline:"wheel:*:0:root\n" -x pw -V ${HOME} groupshow wheel
 }
 
-atf_test_case user_do_not_try_to_delete_root_if_user_unkown
-user_do_not_try_to_delete_root_if_user_unkown_head() {
-	atf_set "descr" "Make sure not to try to remove root if deleteing an unknown user"
+atf_test_case user_do_not_try_to_delete_root_if_user_unknown
+user_do_not_try_to_delete_root_if_user_unknown_head() {
+	atf_set "descr" "Make sure not to try to remove root if deleting an unknown user"
 }
 
-user_do_not_try_to_delete_root_if_user_unkown_body() {
+user_do_not_try_to_delete_root_if_user_unknown_body() {
 	populate_etc_skel
 	atf_check -e inline:"pw: -u expects a number\n" -s exit:64 -x pw -V ${HOME} userdel -u plop
 }
 
 atf_init_test_cases() {
 	atf_add_test_case rmuser_seperate_group
-	atf_add_test_case group_do_not_delete_wheel_if_group_unkown
-	atf_add_test_case user_do_not_try_to_delete_root_if_user_unkown
+	atf_add_test_case group_do_not_delete_wheel_if_group_unknown
+	atf_add_test_case user_do_not_try_to_delete_root_if_user_unknown
 }
