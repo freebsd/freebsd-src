86852a881cf2cf854502a09ac3dbae2955763926
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2014-11-20 20:50:05 +0000

    Lock the scsi_low code and the drivers which use it along with other related cleanups: - Require each driver to initalize a mutex in the scsi_low_softc that   is shared with the scsi_low code.  This mutex is used for CAM SIMs,   timers, and interrupt handlers. - Replace the osdep function switch with direct calls to the relevant   CAM functions and direct manipulation of timers via callout(9). - Collapse the CAM-specific scsi_low_osdep_interface substructure   directly into scsi_low_softc. - Use bus_*() instead of bus_space_*(). - Return BUS_PROBE_DEFAULT from probe routines instead of 0. - No need to zero softcs. - Pass 0ul and ~0ul instead of 0 and ~0 to bus_alloc_resource(). - Spell "dettach" as "detach". - Remove unused 'dvname' variables. - De-spl().
    Tested by:	no one
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@274760 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/stg/tmc18c30_pci.c
+++ sys/dev/stg/tmc18c30_pci.c
@@ -99,8 +99,8 @@ stg_pci_attach(device_t dev)
 	}
 
 	/* XXXX remove INTR_ENTROPY below for MFC */
-	error = bus_setup_intr(dev, sc->irq_res, INTR_TYPE_CAM | INTR_ENTROPY,
-			       NULL, stg_intr, (void *)sc, &sc->stg_intrhand);
+	error = bus_setup_intr(dev, sc->irq_res, INTR_TYPE_CAM | INTR_ENTROPY |
+	    INTR_MPSAFE, NULL, stg_intr, sc, &sc->stg_intrhand);
 	if (error) {
 		stg_release_resource(dev);
 		return(error);
