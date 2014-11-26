1bac48ea8f21c4121f1c45311ec4f05577c58abc
mav <mav@FreeBSD.org>
2014-11-26 04:23:21 +0000

    Add bunch of PCI IDs of Intel Wildcat Point (9 Series) chipsets.
    MFC after:	1 week

--- sys/dev/ichsmb/ichsmb_pci.c
+++ sys/dev/ichsmb/ichsmb_pci.c
@@ -88,6 +88,7 @@ __FBSDID("$FreeBSD$");
 #define ID_AVOTON			0x1f3c8086
 #define ID_COLETOCRK			0x23B08086 
 #define ID_LPT				0x8c228086
+#define ID_WCPT				0x8ca28086
 
 #define PCIS_SERIALBUS_SMBUS_PROGIF	0x00
 
@@ -196,6 +197,9 @@ ichsmb_pci_probe(device_t dev)
 		break;
 	case ID_LPT:
 		device_set_desc(dev, "Intel Lynx Point SMBus controller");
+		break;
+	case ID_WCPT:
+		device_set_desc(dev, "Intel Wildcat Point SMBus controller");
 		break;
 	case ID_COLETOCRK:
 		device_set_desc(dev, "Intel Coleto Creek SMBus controller");
