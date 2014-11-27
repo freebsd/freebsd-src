37527364ca0c858a27f951e4aa1544244de9250d
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2014-11-27 10:59:25 +0000

    Bump trusted-stack size from 4 frames to 8 frames, which fits now that the kernel uses one or both of multiple fixed TLB entries, or large TLB entries, for the kernel stack.
    (If we start experiencing occasional kernel panics due to stack overflow,
    we may need to back off this a bit -- or move to a more general solution.)
    
    This changes the ABI on the kernel's trusted-stack data structure exposed
    to userspace via sysarch().  Programs using this feature (e.g., cheritest)
    will need to be recompiled.  We have a further TODO to improve the ABI to
    avoid this happening again in the future if we need to grow the trusted
    stack further.

--- sys/mips/include/cheri.h
+++ sys/mips/include/cheri.h
@@ -190,7 +190,7 @@ struct cheri_stack_frame {
 #endif
 };
 
-#define	CHERI_STACK_DEPTH	4	/* XXXRW: 4 is a nice round number. */
+#define	CHERI_STACK_DEPTH	8	/* XXXRW: 8 is a nice round number. */
 struct cheri_stack {
 	register_t	cs_tsp;		/* Byte offset, not frame index. */
 	register_t	cs_tsize;	/* Stack size, in bytes. */
