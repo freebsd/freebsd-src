a97617028aef0adbf0f98680284321d0157f5291
kevlo <kevlo@FreeBSD.org>
2014-12-01 02:59:05 +0000

    MFC r275162: Init array field in the proper place.
    Obtained from:	OpenBSD

--- usr.bin/dc/stack.c
+++ usr.bin/dc/stack.c
@@ -137,14 +137,12 @@ stack_swap(struct stack *stack)
 static void
 stack_grow(struct stack *stack)
 {
-	size_t i, new_size;
+	size_t new_size;
 
 	if (++stack->sp == stack->size) {
 		new_size = stack->size * 2 + 1;
 		stack->stack = brealloc(stack->stack,
 		    new_size * sizeof(*stack->stack));
-		for (i = stack->size; i < new_size; i++)
-			stack->stack[i].array = NULL;
 		stack->size = new_size;
 	}
 }
@@ -156,6 +154,7 @@ stack_pushnumber(struct stack *stack, struct number *b)
 	stack_grow(stack);
 	stack->stack[stack->sp].type = BCODE_NUMBER;
 	stack->stack[stack->sp].u.num = b;
+	stack->stack[stack->sp].array = NULL;
 }
 
 void
@@ -165,6 +164,7 @@ stack_pushstring(struct stack *stack, char *string)
 	stack_grow(stack);
 	stack->stack[stack->sp].type = BCODE_STRING;
 	stack->stack[stack->sp].u.string = string;
+	stack->stack[stack->sp].array = NULL;
 }
 
 void
