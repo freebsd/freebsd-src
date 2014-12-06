c49fbad7c00d685c8abb332f03847a04f303c49e
delphij <delphij@FreeBSD.org>
2014-12-06 00:01:19 +0000

    3363 Mark non-returning functions in ctftools Reviewed by: Richard Lowe <richlowe@richlowe.net> Reviewed by: Josef 'Jeff' Sipek <josef.sipek@nexenta.com> Approved by: Albert Lee <trisk@omniti.com> Author: Erik Cederstrand <erik@cederstrand.dk>
    illumos/illumos-gate@a6bde1a23b60f140c7ed78df979c2e22b1ed9b2c

--- tools/ctf/cvt/ctftools.h
+++ tools/ctf/cvt/ctftools.h
@@ -26,8 +26,6 @@
 #ifndef _CTFTOOLS_H
 #define	_CTFTOOLS_H
 
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
-
 /*
  * Functions and data structures used in the manipulation of stabs and CTF data
  */
@@ -38,6 +36,8 @@
 #include <libelf.h>
 #include <gelf.h>
 #include <pthread.h>
+
+#include <sys/ccompile.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -434,8 +434,8 @@ int streq(const char *, const char *);
 int findelfsecidx(Elf *, const char *, const char *);
 size_t elf_ptrsz(Elf *);
 char *mktmpname(const char *, const char *);
-void terminate(char *, ...);
-void aborterr(char *, ...);
+void terminate(char *, ...) __NORETURN;
+void aborterr(char *, ...) __NORETURN;
 void set_terminate_cleanup(void (*)());
 void elfterminate(const char *, const char *, ...);
 void warning(char *, ...);
