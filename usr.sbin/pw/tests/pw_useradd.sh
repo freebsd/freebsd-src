a9f7c229c6a9dc1d708c3deef661d6fc84ce4e8d
brd <brd@FreeBSD.org>
2014-12-16 14:50:33 +0000

    Add tests for pw -N
    PR:		150449
    Submitted by:	Robert O'Neil <oneil.rs@gmail.com>
    Approved by:	will

--- usr.sbin/pw/tests/pw_useradd.sh
+++ usr.sbin/pw/tests/pw_useradd.sh
@@ -13,7 +13,16 @@ user_add_body() {
 		grep "^test:.*" $HOME/master.passwd
 }
 
+# Test add user with option -N
+atf_test_case user_add_noupdate
+user_add_noupdate_body() {
+	populate_etc_skel
 
+	atf_check -s exit:0 -o match:"^test:.*" ${PW} useradd test -N
+	atf_check -s exit:1 -o empty grep "^test:.*" $HOME/master.passwd
+}
+
+# Test add user with comments
 atf_test_case user_add_comments
 user_add_comments_body() {
 	populate_etc_skel
@@ -23,6 +32,17 @@ user_add_comments_body() {
 		grep "^test:.*:Test User,work,123,456:" $HOME/master.passwd
 }
 
+# Test add user with comments and option -N
+atf_test_case user_add_comments_noupdate
+user_add_comments_noupdate_body() {
+	populate_etc_skel
+
+	atf_check -s exit:0 -o match:"^test:.*:Test User,work,123,456:" \
+		${PW} useradd test -c "Test User,work,123,456" -N
+	atf_check -s exit:1 -o empty grep "^test:.*" $HOME/master.passwd
+}
+
+# Test add user with invalid comments
 atf_test_case user_add_comments_invalid
 user_add_comments_invalid_body() {
 	populate_etc_skel
@@ -33,8 +53,21 @@ user_add_comments_invalid_body() {
 		grep "^test:.*:Test User,work,123:456,456:" $HOME/master.passwd
 }
 
+# Test add user with invalid comments and option -N
+atf_test_case user_add_comments_invalid_noupdate
+user_add_comments_invalid_noupdate_body() {
+	populate_etc_skel
+
+	atf_check -s exit:65 -e match:"invalid character" \
+		${PW} useradd test -c "Test User,work,123:456,456" -N
+	atf_check -s exit:1 -o empty grep "^test:.*" $HOME/master.passwd
+}
+
 atf_init_test_cases() {
 	atf_add_test_case user_add
+	atf_add_test_case user_add_noupdate
 	atf_add_test_case user_add_comments
+	atf_add_test_case user_add_comments_noupdate
 	atf_add_test_case user_add_comments_invalid 
+	atf_add_test_case user_add_comments_invalid_noupdate
 }
