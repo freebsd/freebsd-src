31f3551be7eac2f24974f6fa6d15ee5b1f7fbda2
avg <avg@FreeBSD.org>
2014-12-18 13:46:32 +0000

    MFC r275401: zfs_putpages: actually update mtime and ctime

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_vnops.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_vnops.c
@@ -5914,6 +5914,7 @@ top:
 		    &zp->z_pflags, 8);
 		zfs_tstamp_update_setup(zp, CONTENT_MODIFIED, mtime, ctime,
 		    B_TRUE);
+		(void)sa_bulk_update(zp->z_sa_hdl, bulk, count, tx);
 		zfs_log_write(zfsvfs->z_log, tx, TX_WRITE, zp, off, len, 0);
 
 		VM_OBJECT_LOCK(object);
