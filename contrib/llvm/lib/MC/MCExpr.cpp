c47b215d21e5c8728a6e1192dd9ce115c57a9410
Dimitry Andric <dim@FreeBSD.org>
2014-12-25 23:57:31 +0000

    Pull in r224415 from upstream llvm trunk (by Justin Hibbits):
      Add parsing of 'foo@local".
    
      Summary:
      Currently, it supports generating, but not parsing, this expression.
      Test added as well.
    
      Test Plan: New test added, no regressions due to this.
    
      Reviewers: hfinkel
    
      Reviewed By: hfinkel
    
      Subscribers: llvm-commits
    
      Differential Revision: http://reviews.llvm.org/D6672
    
    Pull in r224494 from upstream llvm trunk (by Justin Hibbits):
    
      Add a corresponding '@LOCAL' parse to match r224415.
    
      Pointed out by Jim Grosbach.

--- contrib/llvm/lib/MC/MCExpr.cpp
+++ contrib/llvm/lib/MC/MCExpr.cpp
@@ -348,6 +348,8 @@ MCSymbolRefExpr::getVariantKindForName(StringRef Name) {
     .Case("got@h", VK_PPC_GOT_HI)
     .Case("GOT@HA", VK_PPC_GOT_HA)
     .Case("got@ha", VK_PPC_GOT_HA)
+    .Case("local", VK_PPC_LOCAL)
+    .Case("LOCAL", VK_PPC_LOCAL)
     .Case("TOCBASE", VK_PPC_TOCBASE)
     .Case("tocbase", VK_PPC_TOCBASE)
     .Case("TOC", VK_PPC_TOC)
