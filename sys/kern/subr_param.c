75237c254e294c3c273a137b044071689e22d46f
bryanv <bryanv@FreeBSD.org>
2014-12-31 07:00:36 +0000

    Add basic bhyve guest detection

--- sys/kern/subr_param.c
+++ sys/kern/subr_param.c
@@ -160,6 +160,7 @@ static const char *const vm_guest_sysctl_names[] = {
 	"hv",
 	"vmware",
 	"kvm",
+	"bhyve",
 	NULL
 };
 CTASSERT(nitems(vm_guest_sysctl_names) - 1 == VM_LAST);
---
+++ sys/x86/x86/bhyve.c
@@ -0,0 +1,64 @@
+/*-
+ * Copyright (c) 2014 Bryan Venteicher <bryanv@FreeBSD.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/param.h>
+#include <sys/systm.h>
+
+#include <x86/hypervisor.h>
+
+static int	bhyve_identify(void);
+
+const struct hypervisor_info bhyve_hypervisor_info = {
+	.hvi_name =		"bhyve",
+	.hvi_signature =	"bhyve bhyve",
+	.hvi_type =		VM_GUEST_BHYVE,
+	.hvi_identify =		bhyve_identify,
+};
+
+static uint32_t bhyve_cpuid_base = -1;
+static uint32_t bhyve_cpuid_high = -1;
+
+static uint32_t
+bhyve_cpuid_identify(void)
+{
+
+	if (bhyve_cpuid_base == -1) {
+		hypervisor_cpuid_base(bhyve_hypervisor_info.hvi_signature,
+		    0, &bhyve_cpuid_base, &bhyve_cpuid_high);
+	}
+
+	return (bhyve_cpuid_base);
+}
+
+static int
+bhyve_identify(void)
+{
+
+	return (bhyve_cpuid_identify() != 0);
+}
