47fc54d8b02723738049bb0bce7351d34ebd6099
Ed Maste <emaste@FreeBSD.org>
2014-12-31 01:48:23 +0000

    libelf: Rearrange size test to prevent integer overflow
    Sponsored by:	The FreeBSD Foundation

--- contrib/elftoolchain/libelf/elf_scn.c
+++ contrib/elftoolchain/libelf/elf_scn.c
@@ -62,7 +62,7 @@ _libelf_load_section_headers(Elf *e, void *ehdr)
 #define	CHECK_EHDR(E,EH)	do {				\
 		if (fsz != (EH)->e_shentsize ||			\
 		    shnum > SIZE_MAX / fsz ||			\
-		    shoff + fsz * shnum > e->e_rawsize) {	\
+		    fsz * shnum > e->e_rawsize - shoff) {	\
 			LIBELF_SET_ERROR(HEADER, 0);		\
 			return (0);				\
 		}						\
