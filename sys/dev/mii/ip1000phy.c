c5fa01f958a347042c880553ba1dcd389910a982
glebius <glebius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-01-12 22:27:38 +0000

    In miibus(4) drivers provide functions that allow to get NIC driver name and NIC driver softc via the device(9) tree, instead of going dirty through the ifnet(9) layer.
    Differential Revision:	D1506
    Reviewed by:		imp, jhb
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@277093 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/mii/ip1000phy.c
+++ sys/dev/mii/ip1000phy.c
@@ -110,7 +110,7 @@ ip1000phy_attach(device_t dev)
 	ma = device_get_ivars(dev);
 	flags = MIIF_NOISOLATE | MIIF_NOMANPAUSE;
 	if (MII_MODEL(ma->mii_id2) == MII_MODEL_xxICPLUS_IP1000A &&
-	     strcmp(if_getdname(ma->mii_data->mii_ifp), "stge") == 0 &&
+	     mii_dev_mac_match(dev, "stge") &&
 	     (miibus_get_flags(dev) & MIIF_MACPRIV0) != 0)
 		flags |= MIIF_PHYPRIV0;
 	mii_phy_dev_attach(dev, flags, &ip1000phy_funcs, 1);
