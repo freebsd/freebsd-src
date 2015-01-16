53d673996b1d91c5367cf8b487f8a7e7fc6ae512
Warner Losh <imp@FreeBSD.org>
2015-01-16 06:19:39 +0000

    Move the suspsned and resume functions to the bus attachment. They were accessing PCI config registers, which won't work for the ISA version.

--- sys/dev/pccbb/pccbb.c
+++ sys/dev/pccbb/pccbb.c
@@ -1563,61 +1563,6 @@ cbb_write_ivar(device_t brdev, device_t child, int which, uintptr_t value)
 }
 
 int
-cbb_suspend(device_t brdev)
-{
-	int			error = 0;
-	struct cbb_softc	*sc = device_get_softc(brdev);
-
-	error = bus_generic_suspend(brdev);
-	if (error != 0)
-		return (error);
-	cbb_set(sc, CBB_SOCKET_MASK, 0);	/* Quiet hardware */
-	sc->cardok = 0;				/* Card is bogus now */
-	return (0);
-}
-
-int
-cbb_resume(device_t brdev)
-{
-	int	error = 0;
-	struct cbb_softc *sc = (struct cbb_softc *)device_get_softc(brdev);
-	uint32_t tmp;
-
-	/*
-	 * In the APM and early ACPI era, BIOSes saved the PCI config
-	 * registers. As chips became more complicated, that functionality moved
-	 * into the ACPI code / tables. We must therefore, restore the settings
-	 * we made here to make sure the device come back. Transitions to Dx
-	 * from D0 and back to D0 cause the bridge to lose its config space, so
-	 * all the bus mappings and such are preserved.
-	 *
-	 * For most drivers, the PCI layer handles this saving. However, since
-	 * there's much black magic and arcane art hidden in these few lines of
-	 * code that would be difficult to transition into the PCI
-	 * layer. chipinit was several years of trial and error to write.
-	 */
-	pci_write_config(brdev, CBBR_SOCKBASE, rman_get_start(sc->base_res), 4);
-	DEVPRINTF((brdev, "PCI Memory allocated: %08lx\n",
-	    rman_get_start(sc->base_res)));
-
-	sc->chipinit(sc);
-
-	/* reset interrupt -- Do we really need to do this? */
-	tmp = cbb_get(sc, CBB_SOCKET_EVENT);
-	cbb_set(sc, CBB_SOCKET_EVENT, tmp);
-
-	/* CSC Interrupt: Card detect interrupt on */
-	cbb_setb(sc, CBB_SOCKET_MASK, CBB_SOCKET_MASK_CD);
-
-	/* Signal the thread to wakeup. */
-	wakeup(&sc->intrhand);
-
-	error = bus_generic_resume(brdev);
-
-	return (error);
-}
-
-int
 cbb_child_present(device_t parent, device_t child)
 {
 	struct cbb_softc *sc = (struct cbb_softc *)device_get_softc(parent);
