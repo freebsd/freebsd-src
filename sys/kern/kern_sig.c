299917ec2501d7b22ce781203d1e254e54c1363d
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-01-18 15:13:11 +0000

    Add procctl(2) PROC_TRACE_CTL command to enable or disable debugger attachment to the process.  Note that the command is not intended to be a security measure, rather it is an obfuscation feature, implemented for parity with other operating systems.
    Discussed with:	jilles, rwatson
    Man page fixes by:	rwatson
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@277322 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -3247,7 +3247,8 @@ coredump(struct thread *td)
 	MPASS((p->p_flag & P_HADTHREADS) == 0 || p->p_singlethread == td);
 	_STOPEVENT(p, S_CORE, 0);
 
-	if (!do_coredump || (!sugid_coredump && (p->p_flag & P_SUGID) != 0)) {
+	if (!do_coredump || (!sugid_coredump && (p->p_flag & P_SUGID) != 0) ||
+	    (p->p_flag2 & P2_NOTRACE) != 0) {
 		PROC_UNLOCK(p);
 		return (EFAULT);
 	}
