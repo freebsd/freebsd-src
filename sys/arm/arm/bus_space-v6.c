942dfd1f3fe3465f7776c4a9d00d13c0e23f14c6
Ian Lepore <ian@FreeBSD.org>
2015-01-21 02:35:04 +0000

    Use the explicit member initializer style to init the bus_space struct. Fill in some formerly NULL members where the implementation function exists.  Add a dummy function that panics and use it as a placeholder for thigns that are still unimplemented.  Remove a few unused includes.

--- sys/arm/arm/bus_space-v6.c
+++ sys/arm/arm/bus_space-v6.c
@@ -34,125 +34,130 @@ __FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
 #include <sys/systm.h>
-#include <sys/bus.h>
-#include <sys/kernel.h>
-#include <sys/malloc.h>
-
 #include <machine/bus.h>
 
 /* Prototypes for all the bus_space structure functions */
 bs_protos(generic);
 bs_protos(generic_armv4);
 
+static void
+bs_unimplemented(void)
+{
+
+	panic("unimplemented bus_space function called");
+}
+
+#define	BS_UNIMPLEMENTED	(void *)bs_unimplemented
+
 /*
  * The bus space tag.  This is constant for all instances, so
  * we never have to explicitly "create" it.
  */
 static struct bus_space _base_tag = {
-	/* cookie */
-	(void *) 0,
+	/* privdata is whatever the implementer wants; unused in base tag */
+	.bs_privdata	= NULL,
 
 	/* mapping/unmapping */
-	generic_bs_map,
-	generic_bs_unmap,
-	generic_bs_subregion,
+	.bs_map		= generic_bs_map,
+	.bs_unmap	= generic_bs_unmap,
+	.bs_subregion	= generic_bs_subregion,
 
 	/* allocation/deallocation */
-	generic_bs_alloc,
-	generic_bs_free,
+	.bs_alloc	= generic_bs_alloc,
+	.bs_free	= generic_bs_free,
 
 	/* barrier */
-	generic_bs_barrier,
+	.bs_barrier	= generic_bs_barrier,
 
 	/* read (single) */
-	NULL,	/* bs_r_1, Use inline code in bus.h */
-	NULL,	/* bs_r_2, Use inline code in bus.h */
-	NULL,	/* bs_r_4, Use inline code in bus.h */
-	NULL,	/* bs_r_8, Use inline code in bus.h */
+	.bs_r_1		= NULL,	/* Use inline code in bus.h */
+	.bs_r_2		= NULL,	/* Use inline code in bus.h */
+	.bs_r_4		= NULL,	/* Use inline code in bus.h */
+	.bs_r_8		= NULL,	/* Use inline code in bus.h */
 
 	/* read multiple */
-	generic_bs_rm_1,
-	generic_armv4_bs_rm_2,
-	generic_bs_rm_4,
-	NULL,
+	.bs_rm_1	= generic_bs_rm_1,
+	.bs_rm_2	= generic_armv4_bs_rm_2,
+	.bs_rm_4	= generic_bs_rm_4,
+	.bs_rm_8	= BS_UNIMPLEMENTED,
 
 	/* read region */
-	generic_bs_rr_1,
-	generic_armv4_bs_rr_2,
-	generic_bs_rr_4,
-	NULL,
+	.bs_rr_1	= generic_bs_rr_1,
+	.bs_rr_2	= generic_armv4_bs_rr_2,
+	.bs_rr_4	= generic_bs_rr_4,
+	.bs_rr_8	= BS_UNIMPLEMENTED,
 
 	/* write (single) */
-	NULL,	/* bs_w_1, Use inline code in bus.h */
-	NULL,	/* bs_w_2, Use inline code in bus.h */
-	NULL,	/* bs_w_4, Use inline code in bus.h */
-	NULL,	/* bs_w_8, Use inline code in bus.h */
+	.bs_w_1		= NULL,	/* Use inline code in bus.h */
+	.bs_w_2		= NULL,	/* Use inline code in bus.h */
+	.bs_w_4		= NULL,	/* Use inline code in bus.h */
+	.bs_w_8		= NULL,	/* Use inline code in bus.h */
 
 	/* write multiple */
-	generic_bs_wm_1,
-	generic_armv4_bs_wm_2,
-	generic_bs_wm_4,
-	NULL,
+	.bs_wm_1	= generic_bs_wm_1,
+	.bs_wm_2	= generic_armv4_bs_wm_2,
+	.bs_wm_4	= generic_bs_wm_4,
+	.bs_wm_8	= BS_UNIMPLEMENTED,
 
 	/* write region */
-	generic_bs_wr_1,
-	generic_armv4_bs_wr_2,
-	generic_bs_wr_4,
-	NULL,
+	.bs_wr_1	= generic_bs_wr_1,
+	.bs_wr_2	= generic_armv4_bs_wr_2,
+	.bs_wr_4	= generic_bs_wr_4,
+	.bs_wr_8	= BS_UNIMPLEMENTED,
 
 	/* set multiple */
-	NULL,
-	NULL,
-	NULL,
-	NULL,
+	.bs_sm_1	= BS_UNIMPLEMENTED,
+	.bs_sm_2	= BS_UNIMPLEMENTED,
+	.bs_sm_4	= BS_UNIMPLEMENTED,
+	.bs_sm_8	= BS_UNIMPLEMENTED,
 
 	/* set region */
-	generic_bs_sr_1,
-	generic_armv4_bs_sr_2,
-	generic_bs_sr_4,
-	NULL,
+	.bs_sr_1	= generic_bs_sr_1,
+	.bs_sr_2	= generic_armv4_bs_sr_2,
+	.bs_sr_4	= generic_bs_sr_4,
+	.bs_sr_8	= BS_UNIMPLEMENTED,
 
 	/* copy */
-	NULL,
-	generic_armv4_bs_c_2,
-	NULL,
-	NULL,
+	.bs_c_1		= BS_UNIMPLEMENTED,
+	.bs_c_2		= generic_armv4_bs_c_2,
+	.bs_c_4		= BS_UNIMPLEMENTED,
+	.bs_c_8		= BS_UNIMPLEMENTED,
 
 	/* read stream (single) */
-	NULL,   /* bs_r_1_s, Use inline code in bus.h */ 
-	NULL,   /* bs_r_2_s, Use inline code in bus.h */ 
-	NULL,   /* bs_r_4_s, Use inline code in bus.h */ 
-	NULL,   /* bs_r_8_s, Use inline code in bus.h */ 
+	.bs_r_1_s	= NULL,   /* Use inline code in bus.h */ 
+	.bs_r_2_s	= NULL,   /* Use inline code in bus.h */ 
+	.bs_r_4_s	= NULL,   /* Use inline code in bus.h */ 
+	.bs_r_8_s	= NULL,   /* Use inline code in bus.h */ 
 
 	/* read multiple stream */
-	NULL,
-	generic_armv4_bs_rm_2,		/* bus_space_read_multi_stream_2 */
-	NULL,
-	NULL,
+	.bs_rm_1_s	= generic_bs_rm_1,
+	.bs_rm_2_s	= generic_armv4_bs_rm_2,
+	.bs_rm_4_s	= generic_bs_rm_4,
+	.bs_rm_8_s	= BS_UNIMPLEMENTED,
 
 	/* read region stream */
-	NULL,
-	NULL,
-	NULL,
-	NULL,
+	.bs_rr_1_s	= generic_bs_rr_1,
+	.bs_rr_2_s	= generic_bs_rr_2,
+	.bs_rr_4_s	= generic_bs_rr_4,
+	.bs_rr_8_s	= BS_UNIMPLEMENTED,
 
 	/* write stream (single) */
-	NULL,   /* bs_w_1_s, Use inline code in bus.h */ 
-	NULL,   /* bs_w_2_s, Use inline code in bus.h */ 
-	NULL,   /* bs_w_4_s, Use inline code in bus.h */ 
-	NULL,   /* bs_w_8_s, Use inline code in bus.h */ 
+	.bs_w_1_s	= NULL,   /* Use inline code in bus.h */ 
+	.bs_w_2_s	= NULL,   /* Use inline code in bus.h */ 
+	.bs_w_4_s	= NULL,   /* Use inline code in bus.h */ 
+	.bs_w_8_s	= NULL,   /* Use inline code in bus.h */ 
 
 	/* write multiple stream */
-	NULL,
-	generic_armv4_bs_wm_2,		/* bus_space_write_multi_stream_2 */
-	NULL,
-	NULL,
+	.bs_wm_1_s	= generic_bs_wm_1,
+	.bs_wm_2_s	= generic_armv4_bs_wm_2,
+	.bs_wm_4_s	= generic_bs_wm_4,
+	.bs_wm_8_s	= BS_UNIMPLEMENTED,
 
 	/* write region stream */
-	NULL,
-	NULL,
-	NULL,
-	NULL
+	.bs_wr_1_s	= generic_bs_wr_1,
+	.bs_wr_2_s	= generic_bs_wr_4,
+	.bs_wr_4_s	= generic_bs_wr_8,
+	.bs_wr_8_s	= BS_UNIMPLEMENTED,
 };
 
 bus_space_tag_t fdtbus_bs_tag = &_base_tag;
