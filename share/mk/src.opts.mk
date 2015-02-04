b1eb43fa35490041ca6ecc184ce41abbe6e27d8d
ngie <ngie@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-02-04 10:19:32 +0000

    Add the following options to enable/disable several features in the base system
    WITHOUT_BOOTPARAMD - bootparamd
    WITHOUT_BOOTPD - bootpd
    WITHOUT_FINGER - finger, fingerd
    WITHOUT_FTP - ftp, ftpd
    WITHOUT_INETD - inetd
    WITHOUT_RBOOTD - rbootd
    WITHOUT_TCP_WRAPPERS - tcpd, et al
    WITHOUT_TFTP - tftp, tftp-server
    WITHOUT_TIMED - timed
    
    MFC after: 2 weeks
    Sponsored by: EMC / Isilon Storage Division
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@278192 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -58,6 +58,8 @@ __DEFAULT_YES_OPTIONS = \
     BINUTILS_BOOTSTRAP \
     BLUETOOTH \
     BOOT \
+    BOOTPARAMD \
+    BOOTPD \
     BSD_CPIO \
     BSDINSTALL \
     BSNMP \
@@ -81,11 +83,13 @@ __DEFAULT_YES_OPTIONS = \
     ELFTOOLCHAIN_TOOLS \
     EXAMPLES \
     FDT \
+    FINGER \
     FLOPPY \
     FMTREE \
     FORTH \
     FP_LIBC \
     FREEBSD_UPDATE \
+    FTP \
     GAMES \
     GCOV \
     GDB \
@@ -100,6 +104,7 @@ __DEFAULT_YES_OPTIONS = \
     ICONV \
     INET \
     INET6 \
+    INETD \
     IPFILTER \
     IPFW \
     ISCSI \
@@ -137,6 +142,7 @@ __DEFAULT_YES_OPTIONS = \
     QUOTAS \
     RADIUS_SUPPORT \
     RCMDS \
+    RBOOTD \
     RCS \
     RESCUE \
     ROUTED \
@@ -151,10 +157,13 @@ __DEFAULT_YES_OPTIONS = \
     SYSCONS \
     SYSINSTALL \
     TALK \
+    TCP_WRAPPERS \
     TCSH \
     TELNET \
     TESTS \
     TEXTPROC \
+    TFTP \
+    TIMED \
     UNBOUND \
     USB \
     UTMPX \
