07d7a6fc4ee30f5e87ed13677d171413c9acd4e7
marius <marius@FreeBSD.org>
2015-02-08 20:44:50 +0000

    MFC: r276299
    - Make PCI_QUIRK_MSI_INTX_BUG work by using the ID of the actual PCI device
      for the lookup.
    - For devices affected by PCI_QUIRK_MSI_INTX_BUG, ensure PCIM_CMD_INTxDIS
      is cleared when using MSI/MSI-X.
    - Employ PCI_QUIRK_MSI_INTX_BUG for BCM5714(S)/BCM5715(S)/BCM5780(S) rather
      than clearing PCIM_CMD_INTxDIS unconditionally for all devices in bge(4).

--- sys/dev/bge/if_bge.c
+++ sys/dev/bge/if_bge.c
@@ -1937,11 +1937,9 @@ bge_chipinit(struct bge_softc *sc)
 
 	/*
 	 * Disable memory write invalidate.  Apparently it is not supported
-	 * properly by these devices.  Also ensure that INTx isn't disabled,
-	 * as these chips need it even when using MSI.
+	 * properly by these devices.
 	 */
-	PCI_CLRBIT(sc->bge_dev, BGE_PCI_CMD,
-	    PCIM_CMD_INTxDIS | PCIM_CMD_MWIEN, 4);
+	PCI_CLRBIT(sc->bge_dev, BGE_PCI_CMD, PCIM_CMD_MWIEN, 4);
 
 	/* Set the timer prescaler (always 66 MHz). */
 	CSR_WRITE_4(sc, BGE_MISC_CFG, BGE_32BITTIME_66MHZ);
