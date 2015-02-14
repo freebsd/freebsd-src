45468c535679073d66091fc7b94dacd46257d337
Konstantin Belousov <kib@FreeBSD.org>
2015-02-14 11:47:40 +0000

    Properly interpose libc spinlocks, was missed in r276630.  In particular, stdio locking was affected.
    Reported and tested by:	"Matthew D. Fuller" <fullermd@over-yonder.net>
    Sponsored by:	The FreeBSD Foundation
    MFC after:	3 days

--- lib/libthr/thread/thr_private.h
+++ lib/libthr/thread/thr_private.h
@@ -928,6 +928,10 @@ int __thr_sigwait(const sigset_t *set, int *sig);
 int __thr_sigwaitinfo(const sigset_t *set, siginfo_t *info);
 int __thr_swapcontext(ucontext_t *oucp, const ucontext_t *ucp);
 
+struct _spinlock;
+void __thr_spinunlock(struct _spinlock *lck);
+void __thr_spinlock(struct _spinlock *lck);
+
 struct tcb *_tcb_ctor(struct pthread *, int);
 void	_tcb_dtor(struct tcb *);
 
