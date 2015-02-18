b9e7ad90e09016ff40b8524d44c3c6b7e5c7348f
Robert Norton <robert.norton@cl.cam.ac.uk>
2015-02-18 16:59:33 +0000

    Add optimised versions of portable find first set functions in libkern. These are called frequently during scheduling so optimisation is not a bad idea. These implementations also have the benefit of being constant time so are more predictable for benchmarking on cheri.

--- sys/libkern/ffs.c
+++ sys/libkern/ffs.c
@@ -27,22 +27,100 @@
  * SUCH DAMAGE.
  */
 
-#include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
 
+#include <sys/cdefs.h>
+#include <sys/types.h>
+
+#ifdef UNIT_TEST
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#else
+__FBSDID("$FreeBSD$");
 #include <sys/libkern.h>
+#endif
 
 /*
- * Find First Set bit
+ * Find First Set bit starting at least significant and 
+ * numbered starting from one. Returns zero if no bits are set.
+ *
+ * Portable implementation with no assumption about sizeof(x). We use
+ * bisect for an implementation which should be efficient on most
+ * architectures with approx. constant runtime proprotional to
+ * log(sizeof(x)).
  */
 int
-ffs(int mask)
+ffs(int x)
 {
-	int bit;
-
-	if (mask == 0)
+	unsigned int  mask  = (unsigned int) -1;
+	unsigned int  shift = sizeof(x) * 8;
+	unsigned int  r     = 1;
+	if (x == 0)
 		return (0);
-	for (bit = 1; !(mask & 1); bit++)
-		mask = (unsigned int)mask >> 1;
-	return (bit);
+	/* 
+	 * Invariants: 
+	 * 0) At least one bit set in x.
+	 * 1) first set bit is in bottom 'shift' bits of x,
+	 *    with corresponding mask in 'mask'. 
+	 * 2) Answer is r + first set bit in x (when numbered from zero).
+	 * Stopping condition: only one bit left.
+	 */
+	while(shift > 1)
+	{
+		/* Set mask to half remaining bits */
+		shift  /= 2;
+		mask  >>= shift;
+		//printf("x=%016lx mask=%016lx shift=%u r=%u\n", x, mask, shift, r);
+		if ((x & mask) == 0)
+		{
+			/* 
+			 * First set bit must be in top half of
+			 * remaining bits, so increment return value
+			 * and shift down to maintain invariants 
+			 */
+			x >>= shift;
+			r +=  shift;
+		}
+	}
+	return (r);
 }
+
+#ifdef UNIT_TEST
+
+static int
+old_ffs(int mask)
+{
+  int bit;
+  if (mask == 0)
+    return (0);
+  for (bit = 1; !(mask & 1); bit++)
+    mask = (unsigned int) mask >> 1;
+  return (bit);
+}
+
+#if 0
+int main(void)
+{
+  for(long x = 0; x < 0xffffffff; x++)
+    {
+      assert(ffs(x)  == old_ffs(x));
+      if ((x & 0xfffffff) == 0)
+	printf(".\n");
+    }
+  printf("SUCCESS!\n");
+}
+#else
+int main(void)
+{
+  printf("%d %d\n", 0, ffs(0));
+  assert(ffs(0) == 0);
+  for(int x = 0; x < 8*(sizeof(int)); x++)
+    {
+      int r = ffs((random() << x) | (1l << x));
+      printf("%d %d\n", x + 1, r);
+      assert(r == x + 1);
+    }
+  printf("SUCCESS!\n");
+}
+#endif
+#endif
