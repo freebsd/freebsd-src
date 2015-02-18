c38fa96db834872e06aadf4f462d2a51d919929e
Xin LI <delphij@FreeBSD.org>
2015-02-18 22:29:52 +0000

    Fix CVE-2015-1349.
    This is a direct commit to stable/8 because BIND is removed from HEAD.

--- contrib/bind9/lib/dns/zone.c
+++ contrib/bind9/lib/dns/zone.c
@@ -7931,6 +7931,12 @@ keyfetch_done(isc_task_t *task, isc_event_t *event) {
 					     namebuf, tag);
 				trustkey = ISC_TRUE;
 			}
+		} else {
+			/*
+			 * No previously known key, and the key is not
+			 * secure, so skip it.
+			 */
+			continue;
 		}
 
 		/* Delete old version */
@@ -7979,7 +7985,7 @@ keyfetch_done(isc_task_t *task, isc_event_t *event) {
 			trust_key(zone, keyname, &dnskey, mctx);
 		}
 
-		if (!deletekey)
+		if (secure && !deletekey)
 			set_refreshkeytimer(zone, &keydata, now);
 	}
 
