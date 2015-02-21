a8c8f476faad468e3b3462061c1e2207fb502e32
ngie <ngie@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-02-21 23:39:43 +0000

    Disguising stderr from jls generally considered harmful
    Undisguising it dumps out the following logspew:
    
    jls: unknown parameter: allow
    
    PR: 191019
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@279121 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- bin/pkill/tests/pkill-j_test.sh
+++ bin/pkill/tests/pkill-j_test.sh
@@ -4,7 +4,7 @@
 jail_name_to_jid()
 {
 	local check_name="$1"
-	jls -j "$check_name" -s 2>/dev/null | tr ' ' '\n' | grep jid= | sed -e 's/.*=//g'
+	jls -j "$check_name" -s | tr ' ' '\n' | grep jid= | sed -e 's/.*=//g'
 }
 
 base=pkill_j_test
@@ -40,6 +40,7 @@ for i in `seq 1 10`; do
 	esac
 	sleep 0.1
 done
+sleep 0.5
 
 if pkill -f -j "$jid" $sleep && sleep 0.5 &&
     ! -f ${PWD}/${base}_1_1.pid &&
