8617260a035acbc099d6fb839f27668911019296
Pedro F. Giffuni <pfg@FreeBSD.org>
2015-02-22 01:15:09 +0000

    msun: drop unnecessary checks.
    Drop an unnecessary check in some calculations. The check
    would have Coverity falsely conclude that variables could
    be left undefined.
    
    Discussed with:	kargl, bde
    Reviewed by:	bde

--- lib/msun/src/e_j0f.c
+++ lib/msun/src/e_j0f.c
@@ -234,7 +234,7 @@ static const float pS2[5] = {
 	if(ix>=0x41000000)     {p = pR8; q= pS8;}
 	else if(ix>=0x40f71c58){p = pR5; q= pS5;}
 	else if(ix>=0x4036db68){p = pR3; q= pS3;}
-	else if(ix>=0x40000000){p = pR2; q= pS2;}
+	else                   {p = pR2; q= pS2;}	/* ix>=0x40000000 */
 	z = one/(x*x);
 	r = p[0]+z*(p[1]+z*(p[2]+z*(p[3]+z*(p[4]+z*p[5]))));
 	s = one+z*(q[0]+z*(q[1]+z*(q[2]+z*(q[3]+z*q[4]))));
