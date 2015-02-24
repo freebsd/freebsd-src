8d11dd01a19b095bf6c81fc93fbe0fd479296553
nwhitehorn <nwhitehorn@FreeBSD.org>
2015-02-24 21:37:20 +0000

    New pmap implementation for 64-bit PowerPC processors. The main focus of this change is to improve concurrency: - Drop global state stored in the shadow overflow page table (and all other   global state) - Remove all global locks - Use per-PTE lock bits to allow parallel page insertion - Reconstruct state when requested for evicted PTEs instead of buffering   it during overflow
    This drops total wall time for make buildworld on a 32-thread POWER8 system
    by a factor of two and system time by a factor of three, providing performance
    20% better than similarly clocked Core i7 Xeons per-core. Performance on
    smaller SMP systems, where PMAP lock contention was not as much of an issue,
    is nearly unchanged.
    
    Tested on:	POWER8, POWER5+, G5 UP, G5 SMP (64-bit and 32-bit kernels)
    Merged from:	user/nwhitehorn/ppc64-pmap-rework
    Looked over by:	jhibbits, andreast
    MFC after:	3 months
    Relnotes:	yes
    Sponsored by:	FreeBSD Foundation

--- sys/powerpc/include/pmap.h
+++ sys/powerpc/include/pmap.h
@@ -86,11 +86,19 @@ typedef	struct pmap *pmap_t;
 
 struct pvo_entry {
 	LIST_ENTRY(pvo_entry) pvo_vlink;	/* Link to common virt page */
+#ifndef __powerpc64__
 	LIST_ENTRY(pvo_entry) pvo_olink;	/* Link to overflow entry */
+#endif
 	RB_ENTRY(pvo_entry) pvo_plink;	/* Link to pmap entries */
-	union {
-		struct	pte pte;		/* 32 bit PTE */
-		struct	lpte lpte;		/* 64 bit PTE */
+	struct {
+#ifndef __powerpc64__
+		/* 32-bit fields */
+		struct	pte pte;
+#endif
+		/* 64-bit fields */
+		uintptr_t   slot;
+		vm_paddr_t  pa;
+		vm_prot_t   prot;
 	} pvo_pte;
 	pmap_t		pvo_pmap;		/* Owning pmap */
 	vm_offset_t	pvo_vaddr;		/* VA of entry */
@@ -101,12 +109,17 @@ RB_HEAD(pvo_tree, pvo_entry);
 int pvo_vaddr_compare(struct pvo_entry *, struct pvo_entry *);
 RB_PROTOTYPE(pvo_tree, pvo_entry, pvo_plink, pvo_vaddr_compare);
 
+/* Used by 32-bit PMAP */
 #define	PVO_PTEGIDX_MASK	0x007UL		/* which PTEG slot */
 #define	PVO_PTEGIDX_VALID	0x008UL		/* slot is valid */
+/* Used by 64-bit PMAP */
+#define	PVO_HID			0x008UL		/* PVO entry in alternate hash*/
+/* Used by both */
 #define	PVO_WIRED		0x010UL		/* PVO entry is wired */
 #define	PVO_MANAGED		0x020UL		/* PVO entry is managed */
 #define	PVO_BOOTSTRAP		0x080UL		/* PVO entry allocated during
 						   bootstrap */
+#define PVO_DEAD		0x100UL		/* waiting to be deleted */
 #define PVO_LARGE		0x200UL		/* large page */
 #define	PVO_VADDR(pvo)		((pvo)->pvo_vaddr & ~ADDR_POFF)
 #define	PVO_PTEGIDX_GET(pvo)	((pvo)->pvo_vaddr & PVO_PTEGIDX_MASK)
@@ -135,7 +148,7 @@ struct	pmap {
 };
 
 struct	md_page {
-	u_int64_t	 mdpg_attrs;
+	volatile int32_t mdpg_attrs;
 	vm_memattr_t	 mdpg_cache_attrs;
 	struct	pvo_head mdpg_pvoh;
 };
