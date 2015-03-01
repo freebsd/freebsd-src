632c178a7bd4bd0dd6e4149dccd4d2bf554d468a
nwhitehorn <nwhitehorn@FreeBSD.org>
2015-03-01 21:23:23 +0000

    Initialize NX stack capabilities and direct map status in pmap like on AIM.

--- sys/powerpc/booke/machdep.c
+++ sys/powerpc/booke/machdep.c
@@ -226,9 +226,6 @@ cpu_booke_startup(void *dummy)
 	/* Set up buffers, so they can be used to read disk labels. */
 	bufinit();
 	vm_pager_bufferinit();
-
-	/* Cpu supports execution permissions on the pages. */
-	elf32_nxstack = 1;
 }
 
 static char *
@@ -454,7 +451,6 @@ booke_init(uint32_t arg1, uint32_t arg2)
 	/* Initialise virtual memory. */
 	pmap_mmu_install(MMU_TYPE_BOOKE, 0);
 	pmap_bootstrap((uintptr_t)kernel_text, end);
-	pmap_bootstrapped = 1;
 	debugf("MSR = 0x%08x\n", mfmsr());
 #if defined(BOOKE_E500)
 	//tlb1_print_entries();
