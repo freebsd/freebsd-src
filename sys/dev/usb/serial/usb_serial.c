bb057644cf126fa94ebf280ffe74f4dde0e502d0
Hans Petter Selasky <hselasky@FreeBSD.org>
2015-03-07 18:25:58 +0000

    Add PPS support to USB serial drivers. Bump kernel version to reflect structure change.
    PR:		196897
    MFC after:	1 week

--- sys/dev/usb/serial/usb_serial.c
+++ sys/dev/usb/serial/usb_serial.c
@@ -96,6 +96,11 @@ __FBSDID("$FreeBSD$");
 
 static SYSCTL_NODE(_hw_usb, OID_AUTO, ucom, CTLFLAG_RW, 0, "USB ucom");
 
+static int ucom_pps_mode;
+
+SYSCTL_INT(_hw_usb_ucom, OID_AUTO, pps_mode, CTLFLAG_RWTUN,
+    &ucom_pps_mode, 0, "pulse capturing mode - 0/1/2 - disabled/CTS/DCD");
+
 #ifdef USB_DEBUG
 static int ucom_debug = 0;
 
@@ -408,6 +413,10 @@ ucom_attach_tty(struct ucom_super_softc *ssc, struct ucom_softc *sc)
 	tty_makedev(tp, NULL, "%s", buf);
 
 	sc->sc_tty = tp;
+
+	sc->sc_pps.ppscap = PPS_CAPTUREBOTH;
+	sc->sc_pps.mtx = sc->sc_mtx;
+	pps_init(&sc->sc_pps);
 
 	DPRINTF("ttycreate: %s\n", buf);
 
@@ -858,6 +867,8 @@ ucom_ioctl(struct tty *tp, u_long cmd, caddr_t data, struct thread *td)
 		} else {
 			error = ENOIOCTL;
 		}
+		if (error == ENOIOCTL)
+			error = pps_ioctl(cmd, data, &sc->sc_pps);
 		break;
 	}
 	return (error);
@@ -1061,7 +1072,7 @@ ucom_cfg_status_change(struct usb_proc_msg *_task)
 	struct tty *tp;
 	uint8_t new_msr;
 	uint8_t new_lsr;
-	uint8_t onoff;
+	uint8_t msr_delta;
 	uint8_t lsr_delta;
 
 	tp = sc->sc_tty;
@@ -1085,15 +1096,37 @@ ucom_cfg_status_change(struct usb_proc_msg *_task)
 		/* TTY device closed */
 		return;
 	}
-	onoff = ((sc->sc_msr ^ new_msr) & SER_DCD);
+	msr_delta = (sc->sc_msr ^ new_msr);
 	lsr_delta = (sc->sc_lsr ^ new_lsr);
 
 	sc->sc_msr = new_msr;
 	sc->sc_lsr = new_lsr;
 
-	if (onoff) {
+	/* time pulse counting support */
+	switch(ucom_pps_mode) {
+	case 1:
+		if ((sc->sc_pps.ppscap & PPS_CAPTUREBOTH) &&
+		    (msr_delta & SER_CTS)) {
+			pps_capture(&sc->sc_pps);
+			pps_event(&sc->sc_pps, (sc->sc_msr & SER_CTS) ?
+			    PPS_CAPTUREASSERT : PPS_CAPTURECLEAR);
+		}
+		break;
+	case 2:
+		if ((sc->sc_pps.ppscap & PPS_CAPTUREBOTH) &&
+		    (msr_delta & SER_DCD)) {
+			pps_capture(&sc->sc_pps);
+			pps_event(&sc->sc_pps, (sc->sc_msr & SER_DCD) ?
+			    PPS_CAPTUREASSERT : PPS_CAPTURECLEAR);
+		}
+		break;
+	default:
+		break;
+	}
 
-		onoff = (sc->sc_msr & SER_DCD) ? 1 : 0;
+	if (msr_delta & SER_DCD) {
+
+		int onoff = (sc->sc_msr & SER_DCD) ? 1 : 0;
 
 		DPRINTF("DCD changed to %d\n", onoff);
 
