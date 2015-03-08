5bc10ce457f5434ecd2b070399e8e4a42e561199
Dimitry Andric <dim@FreeBSD.org>
2015-03-08 22:39:01 +0000

    MFC r265829 (by imp):   Support, to the extent we generate proper command lines, compiling   with clang 3.3. Useful for test building -current on a -stable system   in individual directories. Potentially useful if we ever want to   support, say, gcc 4.8 or 4.9's new warnings when building with an   external toolchain (but such support not yet committed). Document   the bsd.compiler.mk interface.
    MFC r266587 (by imp):
      Allow CC to not actually exist. During the ports INDEX run, all the
      Makefiles are evaluated without building things. In a normal build,
      the prerequisites would be built, and CC would be an actual thing. In
      an INDEX build, though, they don't exists. Redirect stderr to get rid
      of annoying messages, and assume that the compiler version is 0 if the
      actual compiler can't tell us. Do this in preference to guessing based
      on numbers because gcc410 might be 4.10, or 4.1.0 and without
      carefully crafted special knowledge we differentiate between them
      easily (also ming-gcc has no clues at all). Elsewhere, don't trust
      the compiler version if it is 0.
    
    MFC r273405 (by bapt):
      When using an external toolchain note that gcc 4.8+ supports C++11
    
      Submitted by:	imp
    
    MFC r275557 (by ngie):
      Fix typos in comments and wrap to <80 columns
    
    MFC r275588 (by ngie):
      ${CC} --version doesn't need to be queried if both COMPILER_TYPE and
      COMPILER_VERSION are known
    
    MFC r275589 (by ngie):
      The previous commit should have been a logical or not a logical "and"
    
      Pointyhat to: me

--- share/mk/bsd.compiler.mk
+++ share/mk/bsd.compiler.mk
@@ -1,26 +1,52 @@
 # $FreeBSD$
 
+# Setup variables for the compiler
+#
+# COMPILER_TYPE is the major type of compiler. Currently gcc and clang support
+# automatic detection. Other compiler types can be shoe-horned in, but require
+# explicit setting of the compiler type. The compiler type can also be set
+# explicitly if, say, you install gcc as clang...
+#
+# COMPILER_VERSION is a numeric constant equal to:
+#     major * 10000 + minor * 100 + tiny
+# It too can be overriden on the command line. When testing it, be sure to
+# make sure that you are limiting the test to a specific compiler. Testing
+# against 30300 for gcc likely isn't  what you wanted (since versions of gcc
+# prior to 4.2 likely have no prayer of working).
+#
+# COMPILER_FEATURES will contain one or more of the following, based on
+# compiler support for that feature:
+#
+# - c++11 : supports full (or nearly full) C++11 programming environment.
+#
+# This file may be included multiple times, but only has effect the first time.
+#
+
+.if !defined(COMPILER_TYPE) || !defined(COMPILER_VERSION)
+_v!=	${CC} --version 2>/dev/null || echo 0.0.0
 .if !defined(COMPILER_TYPE)
-. if ${CC:T:Mgcc*}
+. if ${CC:T:M*gcc*}
 COMPILER_TYPE:=	gcc  
-. elif ${CC:T:Mclang}
+. elif ${CC:T:M*clang*}
+COMPILER_TYPE:=	clang
+. elif ${_v:Mgcc}
+COMPILER_TYPE:=	gcc
+. elif ${_v:M\(GCC\)}
+COMPILER_TYPE:=	gcc
+. elif ${_v:Mclang}
 COMPILER_TYPE:=	clang
 . else
-_COMPILER_VERSION!=	${CC} --version
-.  if ${_COMPILER_VERSION:Mgcc}
-COMPILER_TYPE:=	gcc
-.  elif ${_COMPILER_VERSION:M\(GCC\)}
-COMPILER_TYPE:=	gcc
-.  elif ${_COMPILER_VERSION:Mclang}
-COMPILER_TYPE:=	clang
-.  else
 .error Unable to determine compiler type for ${CC}.  Consider setting COMPILER_TYPE.
-.  endif
-.  undef _COMPILER_VERSION
 . endif
 .endif
+.if !defined(COMPILER_VERSION)
+COMPILER_VERSION!=echo ${_v:M[1-9].[0-9]*} | awk -F. '{print $$1 * 10000 + $$2 * 100 + $$3;}'
+.endif
+.undef _v
+.endif
 
-.if ${COMPILER_TYPE} == "clang"
+.if ${COMPILER_TYPE} == "clang" || \
+	(${COMPILER_TYPE} == "gcc" && ${COMPILER_VERSION} >= 40800)
 COMPILER_FEATURES=	c++11
 .else
 COMPILER_FEATURES=
