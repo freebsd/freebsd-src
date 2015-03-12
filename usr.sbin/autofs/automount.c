8ee4f5ef45e1478131114d75b0716b5c58f82950
Edward Tomasz Napierala <trasz@FreeBSD.org>
2015-03-12 12:36:08 +0000

    Make "automount -LL -o whatever" present options in the same order as used by automountd(8).
    MFC after:	1 month
    Sponsored by:	The FreeBSD Foundation

--- usr.sbin/autofs/automount.c
+++ usr.sbin/autofs/automount.c
@@ -381,13 +381,12 @@ main_automount(int argc, char **argv)
 	parse_master(root, AUTO_MASTER_PATH);
 
 	if (show_maps) {
-		root->n_options = concat(options, ',', root->n_options);
 		if (show_maps > 1) {
 			node_expand_indirect_maps(root);
 			node_expand_ampersand(root, NULL);
 		}
 		node_expand_defined(root);
-		node_print(root);
+		node_print(root, options);
 		return (0);
 	}
 
