bff38d637c8024f84d6c8053be33b3e09df94096
Jack F Vogel <jfv@FreeBSD.org>
2015-03-18 20:11:59 +0000

    Fix i386 LINT build issues, and remove unused variable.

--- sys/dev/ixgbe/if_ixv.c
+++ sys/dev/ixgbe/if_ixv.c
@@ -1979,13 +1979,13 @@ ixv_add_stats_sysctls(struct adapter *adapter)
 	struct sysctl_oid_list *stat_list, *queue_list;
 
 	/* Driver Statistics */
-	SYSCTL_ADD_UQUAD(ctx, child, OID_AUTO, "dropped",
+	SYSCTL_ADD_ULONG(ctx, child, OID_AUTO, "dropped",
 			CTLFLAG_RD, &adapter->dropped_pkts,
 			"Driver dropped packets");
-	SYSCTL_ADD_UQUAD(ctx, child, OID_AUTO, "mbuf_defrag_failed",
+	SYSCTL_ADD_ULONG(ctx, child, OID_AUTO, "mbuf_defrag_failed",
 			CTLFLAG_RD, &adapter->mbuf_defrag_failed,
 			"m_defrag() failed");
-	SYSCTL_ADD_UQUAD(ctx, child, OID_AUTO, "watchdog_events",
+	SYSCTL_ADD_ULONG(ctx, child, OID_AUTO, "watchdog_events",
 			CTLFLAG_RD, &adapter->watchdog_events,
 			"Watchdog timeouts");
 
