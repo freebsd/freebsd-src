71c9695164560878b6f07ac3e0462de93fddc286
Andrey V. Elsukov <ae@FreeBSD.org>
2015-03-19 13:10:09 +0000

    MFC r279920:   Add if_input_default() method, that will be used for if_input   initialization, when no input method specified before if_attach().
      This prevents panics when if_input() method called directly e.g.
      from bpf(4) code.
    
      PR:		192426

--- sys/net/if.c
+++ sys/net/if.c
@@ -162,6 +162,7 @@ static int	ifconf(u_long, caddr_t);
 static void	if_freemulti(struct ifmultiaddr *);
 static void	if_init(void *);
 static void	if_grow(void);
+static void	if_input_default(struct ifnet *, struct mbuf *);
 static void	if_route(struct ifnet *, int flag, int fam);
 static int	if_setflag(struct ifnet *, int, int, int *, int);
 static int	if_transmit(struct ifnet *ifp, struct mbuf *m);
@@ -665,7 +666,9 @@ if_attach_internal(struct ifnet *ifp, int vmove)
 		ifp->if_transmit = if_transmit;
 		ifp->if_qflush = if_qflush;
 	}
-	
+	if (ifp->if_input == NULL)
+		ifp->if_input = if_input_default;
+
 	if (!vmove) {
 #ifdef MAC
 		mac_ifnet_create(ifp);
@@ -3568,6 +3571,13 @@ if_transmit(struct ifnet *ifp, struct mbuf *m)
 
 	IFQ_HANDOFF(ifp, m, error);
 	return (error);
+}
+
+static void
+if_input_default(struct ifnet *ifp __unused, struct mbuf *m)
+{
+
+	m_freem(m);
 }
 
 int
