c2b18a1a19aade302814e1cb1b2615ef92f643ad
mjg <mjg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-03-21 20:25:34 +0000

    proc: use MTX_NEW flag in proc_init
    This allows us to get rid of bzero which was added specifically to make
    mtx_init on p_mtx reliable.
    
    This also fixes a potential problem where mtx_init on other mutexes
    could trip over on unitialized memory and fire an assertion.
    
    Reviewed by:	kib
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@280332 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/kern_proc.c
+++ sys/kern/kern_proc.c
@@ -225,12 +225,11 @@ proc_init(void *mem, int size, int flags)
 	p = (struct proc *)mem;
 	SDT_PROBE(proc, kernel, init, entry, p, size, flags, 0, 0);
 	p->p_sched = (struct p_sched *)&p[1];
-	bzero(&p->p_mtx, sizeof(struct mtx));
-	mtx_init(&p->p_mtx, "process lock", NULL, MTX_DEF | MTX_DUPOK);
-	mtx_init(&p->p_slock, "process slock", NULL, MTX_SPIN);
-	mtx_init(&p->p_statmtx, "pstatl", NULL, MTX_SPIN);
-	mtx_init(&p->p_itimmtx, "pitiml", NULL, MTX_SPIN);
-	mtx_init(&p->p_profmtx, "pprofl", NULL, MTX_SPIN);
+	mtx_init(&p->p_mtx, "process lock", NULL, MTX_DEF | MTX_DUPOK | MTX_NEW);
+	mtx_init(&p->p_slock, "process slock", NULL, MTX_SPIN | MTX_NEW);
+	mtx_init(&p->p_statmtx, "pstatl", NULL, MTX_SPIN | MTX_NEW);
+	mtx_init(&p->p_itimmtx, "pitiml", NULL, MTX_SPIN | MTX_NEW);
+	mtx_init(&p->p_profmtx, "pprofl", NULL, MTX_SPIN | MTX_NEW);
 	cv_init(&p->p_pwait, "ppwait");
 	cv_init(&p->p_dbgwait, "dbgwait");
 	TAILQ_INIT(&p->p_threads);	     /* all threads in proc */
