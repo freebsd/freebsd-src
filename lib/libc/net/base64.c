be730b4f667ca567fbf504e14305d551a4974174
Ed Maste <emaste@FreeBSD.org>
2015-03-23 20:51:35 +0000

    MFC r275163: ANSIfy b64_pton

--- lib/libc/net/base64.c
+++ lib/libc/net/base64.c
@@ -193,10 +193,7 @@ b64_ntop(u_char const *src, size_t srclength, char *target, size_t targsize) {
  */
 
 int
-b64_pton(src, target, targsize)
-	char const *src;
-	u_char *target;
-	size_t targsize;
+b64_pton(const char *src, u_char *target, size_t targsize)
 {
 	int tarindex, state, ch;
 	u_char nextbyte;
