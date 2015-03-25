f143062f39c943fadc40b4ddb56472ab73db47ff
glebius <glebius@FreeBSD.org>
2015-03-25 14:24:04 +0000

    Remove remnants of vlan_cookie()/vlan_setcookie().

--- sys/net/if.c
+++ sys/net/if.c
@@ -2140,8 +2140,6 @@ void	(*vlan_trunk_cap_p)(struct ifnet *);		/* XXX: private from if_vlan */
 struct ifnet *(*vlan_trunkdev_p)(struct ifnet *);
 struct	ifnet *(*vlan_devat_p)(struct ifnet *, uint16_t);
 int	(*vlan_tag_p)(struct ifnet *, uint16_t *);
-int	(*vlan_setcookie_p)(struct ifnet *, void *);
-void	*(*vlan_cookie_p)(struct ifnet *);
 
 /*
  * Handle a change in the interface link state. To avoid LORs
