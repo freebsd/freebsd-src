1e93b73d6b867b97de973a9d2842ec10212c2a7d
Brooks Davis <brooks@one-eyed-alien.net>
2015-03-30 21:31:41 +0000

    Remove a completely rediculous method of checking for a bit in an integer.
    Clang is miscompiling the loop such that the offset of buf is insane
    in snprintf() when two flags are set unless a printf of some aspect of buf or
    buflen is added.

--- contrib/tcpdump/util-print.c
+++ contrib/tcpdump/util-print.c
@@ -334,26 +334,14 @@ bittok2str_internal(register const struct tok *lp, register const char *fmt,
 {
         static char buf[256]; /* our stringbuffer */
         int buflen=0;
-        register int rotbit; /* this is the bit we rotate through all bitpositions */
-        register int tokval;
         const char * sepstr = "";
 
 	while (lp != NULL && lp->s != NULL) {
-            tokval=lp->v;   /* load our first value */
-            rotbit=1;
-            while (rotbit != 0) {
-                /*
-                 * lets AND the rotating bit with our token value
-                 * and see if we have got a match
-                 */
-		if (tokval == (v&rotbit)) {
+	    if (lp->v & v) {
                     /* ok we have found something */
                     buflen+=snprintf(buf+buflen, sizeof(buf)-buflen, "%s%s",
                                      sepstr, lp->s);
                     sepstr = sep ? ", " : "";
-                    break;
-                }
-                rotbit=rotbit<<1; /* no match - lets shift and try again */
             }
             lp++;
 	}
