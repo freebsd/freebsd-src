f2c2231e0c55b53a044f36aec2941b157cefce4a
Ryan Stone <rstone@FreeBSD.org>
2015-04-01 12:42:26 +0000

    Fix integer truncation bug in malloc(9)
    A couple of internal functions used by malloc(9) and uma truncated
    a size_t down to an int.  This could cause any number of issues
    (e.g. indefinite sleeps, memory corruption) if any kernel
    subsystem tried to allocate 2GB or more through malloc.  zfs would
    attempt such an allocation when run on a system with 2TB or more
    of RAM.
    
    Note to self: When this is MFCed, sparc64 needs the same fix.
    
    Differential revision:	https://reviews.freebsd.org/D2106
    Reviewed by:	kib
    Reported by:	Michael Fuckner <michael@fuckner.net>
    Tested by:	Michael Fuckner <michael@fuckner.net>
    MFC after:	2 weeks

--- sys/powerpc/aim/mmu_oea64.c
+++ sys/powerpc/aim/mmu_oea64.c
@@ -1437,7 +1437,8 @@ retry:
 static mmu_t installed_mmu;
 
 static void *
-moea64_uma_page_alloc(uma_zone_t zone, int bytes, u_int8_t *flags, int wait) 
+moea64_uma_page_alloc(uma_zone_t zone, vm_size_t bytes, uint8_t *flags,
+    int wait)
 {
 	struct pvo_entry *pvo;
         vm_offset_t va;
