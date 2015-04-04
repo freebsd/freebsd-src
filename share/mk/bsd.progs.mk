ae9b09c61b59f35714edff9954741790dc77ef94
Baptiste Daroussin <bapt@FreeBSD.org>
2015-04-04 00:31:40 +0000

    Do not install scripts multiple time when using bsd.progs.mk

--- share/mk/bsd.progs.mk
+++ share/mk/bsd.progs.mk
@@ -96,14 +96,14 @@ x.$p= PROG_CXX=$p
 
 $p ${p}_p: .PHONY .MAKE
 	(cd ${.CURDIR} && ${MAKE} -f ${MAKEFILE} _RECURSING_PROGS= \
-	    SUBDIR= PROG=$p \
+	    SUBDIR= SCRIPTS= PROG=$p \
 	    DEPENDFILE=.depend.$p .MAKE.DEPENDFILE=.depend.$p \
 	    ${x.$p})
 
 .for t in ${PROGS_TARGETS:O:u}
 $p.$t: .PHONY .MAKE
 	(cd ${.CURDIR} && ${MAKE} -f ${MAKEFILE} _RECURSING_PROGS= \
-	    SUBDIR= PROG=$p \
+	    SUBDIR= SCRIPTS= PROG=$p \
 	    DEPENDFILE=.depend.$p .MAKE.DEPENDFILE=.depend.$p \
 	    ${x.$p} ${@:E})
 .endfor
