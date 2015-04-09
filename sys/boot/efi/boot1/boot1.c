c42a17ad9df7bc23bbc3a0b1706636e24fb29997
andrew <andrew@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-04-09 10:15:47 +0000

    Print error values with hex to make it easier to find the EFI error type.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@281307 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/boot/efi/boot1/boot1.c
+++ sys/boot/efi/boot1/boot1.c
@@ -330,18 +330,18 @@ load(const char *fname)
 	status = systab->BootServices->LoadImage(TRUE, image, bootdevpath,
 	    buffer, bufsize, &loaderhandle);
 	if (EFI_ERROR(status))
-		printf("LoadImage failed with error %d\n", status);
+		printf("LoadImage failed with error %lx\n", status);
 
 	status = systab->BootServices->HandleProtocol(loaderhandle,
 	    &LoadedImageGUID, (VOID**)&loaded_image);
 	if (EFI_ERROR(status))
-		printf("HandleProtocol failed with error %d\n", status);
+		printf("HandleProtocol failed with error %lx\n", status);
 
 	loaded_image->DeviceHandle = bootdevhandle;
 
 	status = systab->BootServices->StartImage(loaderhandle, NULL, NULL);
 	if (EFI_ERROR(status))
-		printf("StartImage failed with error %d\n", status);
+		printf("StartImage failed with error %lx\n", status);
 }
 
 static void
