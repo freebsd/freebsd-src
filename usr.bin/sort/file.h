f3aa91c0843bcc324941e3ce2dc188ea47ef3294
pfg <pfg@FreeBSD.org>
2015-04-14 18:57:50 +0000

    MFC	r281181, r281182;
    sort(1): Cleanups and a small memory leak.
    Remove custom getdelim(3) and fix a small memory leak.
    
    Obtained from:  OpenBSD

--- usr.bin/sort/file.h
+++ usr.bin/sort/file.h
@@ -70,16 +70,6 @@ struct file_list
 	bool			 tmp;
 };
 
-/*
- * Structure for zero-separated file reading (for input files list)
- */
-struct file0_reader
-{
-	char			*current_line;
-	FILE			*f;
-	size_t			 current_sz;
-};
-
 /* memory */
 
 /**/
@@ -109,8 +99,6 @@ extern const char* compress_program;
 struct file_reader *file_reader_init(const char *fsrc);
 struct bwstring *file_reader_readline(struct file_reader *fr);
 void file_reader_free(struct file_reader *fr);
-
-char *read_file0_line(struct file0_reader *f0r);
 
 void init_tmp_files(void);
 void clear_tmp_files(void);
