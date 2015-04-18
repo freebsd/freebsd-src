dcf8e9199abf6f05a3b0adffc5ccfb9deaf0506e
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-04-18 21:50:13 +0000

    The lseek(2), mmap(2), truncate(2), ftruncate(2), pread(2), and pwrite(2) syscalls are wrapped to provide compatibility with pre-7.x kernels which required padding before the off_t parameter.  The fcntl(2) contains compatibility code to handle kernels before the struct flock was changed during the 8.x CURRENT development.  The shims were reasonable to allow easier revert to the older kernel at that time.
    Now, two or three major releases later, shims do not serve any
    purpose.  Such old kernels cannot handle current libc, so revert the
    compatibility code.
    
    Make padded syscalls support conditional under the COMPAT6 config
    option.  For COMPAT32, the syscalls were under COMPAT6 already.
    
    Remove WITHOUT_SYSCALL_COMPAT build option, which only purpose was to
    (partially) disable the removed shims.
    
    Reviewed by:	jhb, imp (previous versions)
    Discussed with:	peter
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@281714 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/include/compat.h
+++ lib/libc/include/compat.h
@@ -47,9 +47,7 @@ __sym_compat(shmctl, freebsd7_shmctl, FBSD_1.0);
 #define	__weak_reference(sym,alias)	\
 	.weak	alias;.equ	alias,sym
 
-#ifndef SYSCALL_COMPAT
 __weak_reference(__sys_fcntl,__fcntl_compat)
-#endif
 
 #undef __weak_reference
 
