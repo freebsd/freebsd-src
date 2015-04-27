3d80e82d60a4f145b1d94cc8100c78a2003c68b2
Andrey V. Elsukov <ae@FreeBSD.org>
2015-04-27 00:55:56 +0000

    Fix possible use after free due to security policy deletion.
    When we are passing mbuf to IPSec processing via ipsec[46]_process_packet(),
    we hold one reference to security policy and release it just after return
    from this function. But IPSec processing can be deffered and when we release
    reference to security policy after ipsec[46]_process_packet(), user can
    delete this security policy from SPDB. And when IPSec processing will be
    done, xform's callback function will do access to already freed memory.
    
    To fix this move KEY_FREESP() into callback function. Now IPSec code will
    release reference to SP after processing will be finished.
    
    Differential Revision:	https://reviews.freebsd.org/D2324
    No objections from:	#network
    Sponsored by:	Yandex LLC

--- sys/netinet/ip_ipsec.c
+++ sys/netinet/ip_ipsec.c
@@ -199,6 +199,9 @@ ip_ipsec_output(struct mbuf **m, struct inpcb *inp, int *error)
 
 		/* NB: callee frees mbuf */
 		*error = ipsec4_process_packet(*m, sp->req);
+		/* Release SP if an error occured */
+		if (*error != 0)
+			KEY_FREESP(&sp);
 		if (*error == EJUSTRETURN) {
 			/*
 			 * We had a SP with a level of 'use' and no SA. We
@@ -238,9 +241,7 @@ done:
 		KEY_FREESP(&sp);
 	return 0;
 reinjected:
-	if (sp != NULL)
-		KEY_FREESP(&sp);
-	return -1;
+	return (-1);
 bad:
 	if (sp != NULL)
 		KEY_FREESP(&sp);
