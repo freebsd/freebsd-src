33ef4de95f1785bb813b6992231a05c6d6159800
Pedro F. Giffuni <pfg@FreeBSD.org>
2015-04-28 21:32:27 +0000

    libgomp: bring initial BSD support from upstream.
    Initial support for FreeBSD specific routines related to counting
    online processors and dynamic load balancing.
    
    Fix "detection" of the <sys/sysctl> header which upstream seems to have
    done wrong.
    
    Obtained from:	GCC pre-4.4 branch (rev. 140497; LGPLv2.1+)

--- gnu/lib/libgomp/config.h
+++ gnu/lib/libgomp/config.h
@@ -55,6 +55,9 @@
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
 
+/* Define to 1 if you have the <sys/sysctl.h> header file. */
+#define HAVE_SYS_SYSCTL_H 1
+
 /* Define to 1 if you have the <sys/time.h> header file. */
 #define HAVE_SYS_TIME_H 1
 
