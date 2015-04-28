5a416dd4674acce5d5d199e31c3774cdc4ae5af7
Ed Maste <emaste@FreeBSD.org>
2015-04-28 01:15:17 +0000

    MFC r281005: Make die available as rtld_die for use by MD relocation code
    Sponsored by:	The FreeBSD Foundation

--- libexec/rtld-elf/rtld.h
+++ libexec/rtld-elf/rtld.h
@@ -355,6 +355,7 @@ typedef struct Struct_SymLook {
 } SymLook;
 
 void _rtld_error(const char *, ...) __printflike(1, 2) __exported;
+void rtld_die(void) __dead2;
 const char *rtld_strerror(int);
 Obj_Entry *map_object(int, const char *, const struct stat *);
 void *xcalloc(size_t, size_t);
