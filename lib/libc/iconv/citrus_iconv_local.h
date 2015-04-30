b709ec868adb5170d09bc5a66b18d0e0d5987ab6
tijl <tijl@FreeBSD.org>
2015-04-30 16:08:47 +0000

    MFC r275805:
    Fix incorrect type of "invalids" argument in __iconv() prototype.
    
    MFC r281550,281591:
    
    Remove the const qualifier from iconv(3) to comply with POSIX:
    http://pubs.opengroup.org/onlinepubs/9699919799/functions/iconv.html
    
    Adjust all code that calls iconv.
    
    PR:		199099

--- lib/libc/iconv/citrus_iconv_local.h
+++ lib/libc/iconv/citrus_iconv_local.h
@@ -46,7 +46,7 @@ static void	 _citrus_##_m_##_iconv_uninit_shared			\
 		    (struct _citrus_iconv_shared *);			\
 static int	 _citrus_##_m_##_iconv_convert				\
 		    (struct _citrus_iconv * __restrict,			\
-		    const char * __restrict * __restrict,		\
+		    char * __restrict * __restrict,			\
 		    size_t * __restrict,				\
 		    char * __restrict * __restrict,			\
 		    size_t * __restrict outbytes,			\
@@ -75,7 +75,7 @@ typedef void (*_citrus_iconv_uninit_shared_t)
     (struct _citrus_iconv_shared *);
 typedef int (*_citrus_iconv_convert_t)
     (struct _citrus_iconv * __restrict,
-    const char *__restrict* __restrict, size_t * __restrict,
+    char *__restrict* __restrict, size_t * __restrict,
     char * __restrict * __restrict, size_t * __restrict, uint32_t,
     size_t * __restrict);
 typedef int (*_citrus_iconv_init_context_t)(struct _citrus_iconv *);
