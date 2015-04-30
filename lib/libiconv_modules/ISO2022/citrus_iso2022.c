cb784c9f283e7e1908b76643109bd98a527a13c4
Tijl Coosemans <tijl@FreeBSD.org>
2015-04-30 16:08:47 +0000

    MFC r275805:
    Fix incorrect type of "invalids" argument in __iconv() prototype.
    
    MFC r281550,281591:
    
    Remove the const qualifier from iconv(3) to comply with POSIX:
    http://pubs.opengroup.org/onlinepubs/9699919799/functions/iconv.html
    
    Adjust all code that calls iconv.
    
    PR:		199099

--- lib/libiconv_modules/ISO2022/citrus_iso2022.c
+++ lib/libiconv_modules/ISO2022/citrus_iso2022.c
@@ -574,7 +574,7 @@ terminate:
 
 static wchar_t
 _ISO2022_sgetwchar(_ISO2022EncodingInfo * __restrict ei __unused,
-    const char * __restrict string, size_t n, const char ** __restrict result,
+    char * __restrict string, size_t n, char ** __restrict result,
     _ISO2022State * __restrict psenc)
 {
 	const struct seqtable *sp;
@@ -842,10 +842,10 @@ asis:
 
 static int
 _citrus_ISO2022_mbrtowc_priv(_ISO2022EncodingInfo * __restrict ei,
-    wchar_t * __restrict pwc, const char ** __restrict s,
+    wchar_t * __restrict pwc, char ** __restrict s,
     size_t n, _ISO2022State * __restrict psenc, size_t * __restrict nresult)
 {
-	const char *p, *result, *s0;
+	char *p, *result, *s0;
 	wchar_t wchar;
 	int c, chlenbak;
 
