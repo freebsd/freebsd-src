fe5d5d7d05f8f6620833ed624601f07df398b973
Andrew Turner <andrew@FreeBSD.org>
2015-05-06 14:14:14 +0000

    AcpiGbl_FACS will not be defined when building using the reduced hardware model. This may be the case on ARM.

--- sys/dev/acpica/acpi.c
+++ sys/dev/acpica/acpi.c
@@ -605,9 +605,11 @@ acpi_attach(device_t dev)
     if (AcpiGbl_FADT.Flags & ACPI_FADT_RESET_REGISTER)
 	sc->acpi_handle_reboot = 1;
 
+#if !ACPI_REDUCED_HARDWARE
     /* Only enable S4BIOS by default if the FACS says it is available. */
     if (AcpiGbl_FACS != NULL && AcpiGbl_FACS->Flags & ACPI_FACS_S4_BIOS_PRESENT)
 	sc->acpi_s4bios = 1;
+#endif
 
     /* Probe all supported sleep states. */
     acpi_sleep_states[ACPI_STATE_S0] = TRUE;
