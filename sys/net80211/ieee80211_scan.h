d46813ebcd015680317630193d9b872cb9e38514
adrian <adrian@FreeBSD.org>
2015-05-10 06:59:34 +0000

    Turn ieee80211_start_scan_locked() into a swscan module private method.
    It turns out that ieee80211_start_scan_locked() is only ever called by
    the swscan code and it won't likely be required by firmware scanning
    implementations.
    
    So, don't bother keeping it in ieee80211_scan.c and it likely won't
    become an API call.
    
    Tested:
    
    * Intel 5100, STA mode
    * AR5416, STA mode

--- sys/net80211/ieee80211_scan.h
+++ sys/net80211/ieee80211_scan.h
@@ -307,10 +307,6 @@ void	ieee80211_scan_copy_ssid(struct ieee80211vap *vap,
 void	ieee80211_scan_dump_probe_beacon(uint8_t subtype, int isnew,
 		const uint8_t mac[IEEE80211_ADDR_LEN],
 		const struct ieee80211_scanparams *sp, int rssi);
-int	ieee80211_start_scan_locked(const struct ieee80211_scanner *scan,
-	struct ieee80211vap *vap, int flags, u_int duration,
-	u_int mindwell, u_int maxdwell,
-	u_int nssid, const struct ieee80211_scan_ssid ssids[]);
 void	ieee80211_scan_dump(struct ieee80211_scan_state *ss);
 
 #endif /* _NET80211_IEEE80211_SCAN_H_ */
