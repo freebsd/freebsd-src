8c9f47e5dc70ddef50a93fbfe8d6d4ecb6718f1d
adrian <adrian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-05-12 16:55:50 +0000

    Do not check sequence number for QoS Null frames; set it for generated QoS Null frames to 0
    From IEEE Std. 802.11-2012, 8.3.2.1 "Data frame format", p. 415 (513):
    "The Sequence Control field for QoS (+)Null frames is ignored by the receiver
    upon reception."
    
    At this moment, any <mode>_input() function interprets them as regular QoS data
    frames with TID = 0. As a result, stations, that use another TX sequence for
    QoS Null frames (e.g. wpi(4), where (QoS) Null frames are generated by the
    firmware), may experience significant packet loss with any other NIC in hostap
    mode.
    
    Tested:
    
    * wpi(4) (author)
    * iwn(4) - Intel 5100, STA mode (me)
    
    PR:		kern/200128
    Submitted by:	Andriy Voskoboinyk <s3erios@gmail.com>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@282820 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net80211/ieee80211_wds.c
+++ sys/net80211/ieee80211_wds.c
@@ -406,7 +406,6 @@ wds_newstate(struct ieee80211vap *vap, enum ieee80211_state nstate, int arg)
 static int
 wds_input(struct ieee80211_node *ni, struct mbuf *m, int rssi, int nf)
 {
-#define	HAS_SEQ(type)	((type & 0x4) == 0)
 	struct ieee80211vap *vap = ni->ni_vap;
 	struct ieee80211com *ic = ni->ni_ic;
 	struct ifnet *ifp = vap->iv_ifp;
@@ -488,7 +487,7 @@ wds_input(struct ieee80211_node *ni, struct mbuf *m, int rssi, int nf)
 	}
 	IEEE80211_RSSI_LPF(ni->ni_avgrssi, rssi);
 	ni->ni_noise = nf;
-	if (HAS_SEQ(type)) {
+	if (IEEE80211_HAS_SEQ(type, subtype)) {
 		uint8_t tid = ieee80211_gettid(wh);
 		if (IEEE80211_QOS_HAS_SEQ(wh) &&
 		    TID_TO_WME_AC(tid) >= WME_AC_VI)
