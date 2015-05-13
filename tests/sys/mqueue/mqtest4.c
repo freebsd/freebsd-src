c5c0e8d2f31ee19a358a80b396cd472864ae085a
Enji Cooper <ngie@FreeBSD.org>
2015-05-13 12:09:01 +0000

    MFC r281593,r282071,r282074,r282133,r282134,r282135,r282136,r282137,r282138:
    r282071:
    
    Integrate tools/regression/mqueue into the FreeBSD test suite as
    tests/sys/mqueue
    
    r282074:
    
    Integrate tools/regression/aio/aiotest and tools/regression/aio/kqueue into the
    FreeBSD test suite as tests/sys/aio
    
    r282133:
    
    Fill in the copyright boilerplate for the test program
    
    r282134:
    
    Add initial (unpolished) macros for interfacing with the FreeBSD test suite
    
    This is very rough, but will be replaced/redesigned some time soon after I fix
    the Jenkins breakage I introduced
    
    r282135:
    
    Use ATF_REQUIRE_KERNEL_MODULE instead of aio_available function
    
    r282136:
    
    - Use ATF_REQUIRE_KERNEL_MDOULE to require aio(4)
    - Don't use /tmp as a basis for temporary files as it's outside of the ATF
      sandbox
    - Don't override MAX macro in sys/param.h
    
    r282137:
    
    Use PLAIN_REQUIRE_KERNEL_MODULE to require "mqueuefs"
    
    r282138:
    
    Adjust CFLAGS to find freebsd_test_suite/macros.h

--- tests/sys/mqueue/mqtest4.c
+++ tests/sys/mqueue/mqtest4.c
@@ -1,6 +1,7 @@
 /* $FreeBSD$ */
 
 #include <sys/types.h>
+#include <sys/event.h>
 #include <sys/select.h>
 #include <sys/wait.h>
 #include <err.h>
@@ -11,63 +12,70 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define MQNAME	"/mytstqueue3"
+#include "freebsd_test_suite/macros.h"
+
+#define MQNAME	"/mytstqueue4"
 #define LOOPS	1000
 #define PRIO	10
 
-void sighandler(int sig)
+static void
+sighandler(int sig __unused)
 {
 	write(1, "timeout\n", 8);
 	_exit(1);
 }
 
-int main()
+int
+main(void)
 {
-	mqd_t mq;
-	int status;
+	struct kevent kev;
 	struct mq_attr attr;
-	int pid;
-	fd_set set;
+	mqd_t mq;
+	int kq, status;
+	pid_t pid;
+
+	PLAIN_REQUIRE_KERNEL_MODULE("mqueuefs", 0);
 
 	mq_unlink(MQNAME);
 
 	attr.mq_maxmsg  = 5;
 	attr.mq_msgsize = 128;
 	mq = mq_open(MQNAME, O_CREAT | O_RDWR | O_EXCL, 0666, &attr);
-	if (mq == (mqd_t)-1)
+	if (mq == (mqd_t) -1)
 		err(1, "mq_open()");
 	status = mq_getattr(mq, &attr);
 	if (status)
 		err(1, "mq_getattr()");
-	
 	pid = fork();
 	if (pid == 0) { /* child */
-		int prio, j, i;
 		char *buf;
+		int j, i;
+		unsigned int prio;
 
 		mq_close(mq);
-
-		signal(SIGALRM, sighandler);
-
+		kq = kqueue();
 		mq = mq_open(MQNAME, O_RDWR);
 		if (mq == (mqd_t)-1)
-			err(1, "child process: mq_open");
+			err(1, "child: mq_open");
+		EV_SET(&kev, __mq_oshandle(mq), EVFILT_READ, EV_ADD, 0, 0, 0);
+		status = kevent(kq, &kev, 1, NULL, 0, NULL);
+		if (status == -1)
+			err(1, "child: kevent");
 		buf = malloc(attr.mq_msgsize);
 		for (j = 0; j < LOOPS; ++j) {
-			FD_ZERO(&set);
-			FD_SET(__mq_oshandle(mq), &set);
 			alarm(3);
-			status = select(__mq_oshandle(mq)+1, &set, NULL, NULL, NULL);
+			status = kevent(kq, NULL, 0, &kev, 1, NULL);
 			if (status != 1)
-				err(1, "child process: select()");
+				err(1, "child: kevent 2");
 			status = mq_receive(mq, buf, attr.mq_msgsize, &prio);
 			if (status == -1)
-				err(2, "child process: mq_receive");
+				err(2, "child: mq_receive");
 			for (i = 0; i < attr.mq_msgsize; ++i)
 				if (buf[i] != i)
-					err(3, "message data corrupted");
+					err(3, "child: message data corrupted");
 			if (prio != PRIO)
-				err(4, "priority is incorrect: %d", prio);
+				err(4, "child: priority is incorrect: %d",
+					 prio);
 		}
 		alarm(0);
 		free(buf);
@@ -77,26 +85,29 @@ int main()
 		err(1, "fork()");
 	} else {
 		char *buf;
-		int i, j, prio;
+		int i, j;
 
 		signal(SIGALRM, sighandler);
+		kq = kqueue();
+		EV_SET(&kev, __mq_oshandle(mq), EVFILT_WRITE, EV_ADD, 0, 0, 0);
+		status = kevent(kq, &kev, 1, NULL, 0, NULL);
+		if (status == -1)
+			err(1, "kevent");
 		buf = malloc(attr.mq_msgsize);
 		for (j = 0; j < LOOPS; ++j) {
 			for (i = 0; i < attr.mq_msgsize; ++i) {
 				buf[i] = i;
 			}
 			alarm(3);
-			FD_ZERO(&set);
-			FD_SET(__mq_oshandle(mq), &set);
-			status = select(__mq_oshandle(mq)+1, NULL, &set, NULL, NULL);
+			status = kevent(kq, NULL, 0, &kev, 1, NULL);
 			if (status != 1)
-				err(1, "select()");
+				err(1, "child: kevent 2");
 			status = mq_send(mq, buf, attr.mq_msgsize, PRIO);
 			if (status) {
-				kill(pid, SIGKILL);
 				err(2, "mq_send()");
 			}
 		}
+		free(buf);
 		alarm(3);
 		wait(&status);
 		alarm(0);
