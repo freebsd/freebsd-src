b1fc2fbae970fc4d2aad4e8411330f54d3073b9c
gjb <gjb@FreeBSD.org>
2015-05-20 19:32:57 +0000

    MFC r282500, r282693:  r282500:   Add initial support for building RPI2 images.
      In release.sh, allow overriding buildenv_setup() before
      the handoff to arm/release.sh.
    
      Copy arm/RPI-B.conf -> arm/RPI2.conf, set UBOOT_PORT and
      the correct KERNEL, and add the buildenv_setup() override
      to install the sysutils/u-boot-rpi2 port/package.
    
      Copy tools/arm/crochet-RPI-B.conf -> tools/arm/crochet-RPI2.conf,
      and set the correct entries for the RaspberryPi2 board.
    
     r282693:
      Merge ^/projects/release-arm-redux into ^/head.
    
      Of note:
    
      - This commit adds native FreeBSD/arm release build support without
        requiring out-of-tree utilities.
    
      - Part of this merge removes the WANDBOARD-{SOLO,DUAL,QUAD} kernel
        configuration files, for which the IMX6 kernel configuration file
        should be used instead.
    
      - The resulting images have a 'freebsd' user (password 'freebsd'),
        to allow ssh(1) access when console access is not available (VGA
        or serial).  The default 'root' user password is set to 'root'.
    
      - The /etc/ttys file for arm images now enable both ttyv0 and ttyu0
        by default.
    
    Note:  The RPI2 kernel configuration does not yet exist in stable/10,
    however the merge conflicts needed to be properly resolved.
    Additionally, SRCBRANCH has been set to base/stable/10 in the updated
    arm configuration files as part of this commit.
    
    Sponsored by:	The FreeBSD Foundation

--- release/release.sh
+++ release/release.sh
@@ -36,250 +36,359 @@
 # $FreeBSD$
 #
 
-PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin"
-export PATH
+export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin"
 
-# The directory within which the release will be built.
-CHROOTDIR="/scratch"
-RELENGDIR="$(realpath $(dirname $(basename ${0})))"
+VERSION=2
 
-# The default version control system command to obtain the sources.
-VCSCMD="svn checkout"
-
-# The default svn checkout server, and svn branches for src/, doc/,
-# and ports/.
-SVNROOT="svn://svn.FreeBSD.org/"
-SRCBRANCH="base/head@rHEAD"
-DOCBRANCH="doc/head@rHEAD"
-PORTBRANCH="ports/head@rHEAD"
-
-# Set for embedded device builds.
-EMBEDDEDBUILD=
-
-# Sometimes one needs to checkout src with --force svn option.
-# If custom kernel configs copied to src tree before checkout, e.g.
-SRC_FORCE_CHECKOUT=
-
-# The default make.conf and src.conf to use.  Set to /dev/null
-# by default to avoid polluting the chroot(8) environment with
-# non-default settings.
-MAKE_CONF="/dev/null"
-SRC_CONF="/dev/null"
-
-# The number of make(1) jobs, defaults to the number of CPUs available for
-# buildworld, and half of number of CPUs available for buildkernel.
-WORLD_FLAGS="-j$(sysctl -n hw.ncpu)"
-KERNEL_FLAGS="-j$(( $(( $(sysctl -n hw.ncpu) + 1 )) / 2))"
-
-MAKE_FLAGS="-s"
-
-# The name of the kernel to build, defaults to GENERIC.
-KERNEL="GENERIC"
-
-# Set to non-empty value to disable checkout of doc/ and/or ports/.  Disabling
-# ports/ checkout also forces NODOC to be set.
-NODOC=
-NOPORTS=
-
-# Set to non-empty value to build dvd1.iso as part of the release.
-WITH_DVD=
-WITH_COMPRESSED_IMAGES=
-
-# Set to non-empty value to build virtual machine images as part of
-# the release.
-WITH_VMIMAGES=
-WITH_COMPRESSED_VMIMAGES=
-
-# Set to non-empty value to build virtual machine images for various
-# cloud providers as part of the release.
-WITH_CLOUDWARE=
+# Prototypes that can be redefined per-chroot or per-target.
+load_chroot_env() { }
+load_target_env() { }
+buildenv_setup() { }
 
 usage() {
 	echo "Usage: $0 [-c release.conf]"
 	exit 1
 }
 
-while getopts c: opt; do
-	case ${opt} in
-	c)
-		RELEASECONF="${OPTARG}"
-		if [ ! -e "${RELEASECONF}" ]; then
-			echo "ERROR: Configuration file ${RELEASECONF} does not exist."
-			exit 1
-		fi
-		# Source the specified configuration file for overrides
-		. ${RELEASECONF}
-		;;
-	\?)
-		usage
-		;;
-	esac
-done
-shift $(($OPTIND - 1))
+# env_setup(): Set up the default build environment variables, such as the
+# CHROOTDIR, VCSCMD, SVNROOT, etc.  This is called before the release.conf
+# file is sourced, if '-c <release.conf>' is specified.
+env_setup() {
+	# The directory within which the release will be built.
+	CHROOTDIR="/scratch"
+	RELENGDIR="$(dirname $(realpath ${0}))"
 
-# Fix for backwards-compatibility with release.conf that does not have the
-# trailing '/'.
-case ${SVNROOT} in
-	*svn*)
-		SVNROOT="${SVNROOT}/"
-		;;
-	*)
-		;;
-esac
+	# The default version control system command to obtain the sources.
+	for _dir in /usr/bin /usr/local/bin; do
+		for _svn in svn svnlite; do
+			[ -x "${_dir}/${_svn}" ] && VCSCMD="${_dir}/${_svn}"
+			[ ! -z "${VCSCMD}" ] && break 2
+		done
+	done
+	VCSCMD="${VCSCMD} checkout"
 
-# Prefix the branches with the SVNROOT for the full checkout URL.
-SRCBRANCH="${SVNROOT}${SRCBRANCH}"
-DOCBRANCH="${SVNROOT}${DOCBRANCH}"
-PORTBRANCH="${SVNROOT}${PORTBRANCH}"
+	# The default svn checkout server, and svn branches for src/, doc/,
+	# and ports/.
+	SVNROOT="svn://svn.FreeBSD.org/"
+	SRCBRANCH="base/head@rHEAD"
+	DOCBRANCH="doc/head@rHEAD"
+	PORTBRANCH="ports/head@rHEAD"
 
-if [ -n "${EMBEDDEDBUILD}" ]; then
-	if [ -z "${XDEV}" ] || [ -z "${XDEV_ARCH}" ]; then
-		echo "ERROR: XDEV and XDEV_ARCH must be set in ${RELEASECONF}."
-		exit 1
-	fi
+	# Set for embedded device builds.
+	EMBEDDEDBUILD=
+
+	# Sometimes one needs to checkout src with --force svn option.
+	# If custom kernel configs copied to src tree before checkout, e.g.
+	SRC_FORCE_CHECKOUT=
+
+	# The default make.conf and src.conf to use.  Set to /dev/null
+	# by default to avoid polluting the chroot(8) environment with
+	# non-default settings.
+	MAKE_CONF="/dev/null"
+	SRC_CONF="/dev/null"
+
+	# The number of make(1) jobs, defaults to the number of CPUs available
+	# for buildworld, and half of number of CPUs available for buildkernel.
+	WORLD_FLAGS="-j$(sysctl -n hw.ncpu)"
+	KERNEL_FLAGS="-j$(( $(( $(sysctl -n hw.ncpu) + 1 )) / 2))"
+
+	MAKE_FLAGS="-s"
+
+	# The name of the kernel to build, defaults to GENERIC.
+	KERNEL="GENERIC"
+
+	# Set to non-empty value to disable checkout of doc/ and/or ports/.
+	# Disabling ports/ checkout also forces NODOC to be set.
+	NODOC=
+	NOPORTS=
+
+	# Set to non-empty value to build dvd1.iso as part of the release.
 	WITH_DVD=
 	WITH_COMPRESSED_IMAGES=
-	NODOC=yes
-fi
 
-# If PORTS is set and NODOC is unset, force NODOC=yes because the ports tree
-# is required to build the documentation set.
-if [ -n "${NOPORTS}" ] && [ -z "${NODOC}" ]; then
-	echo "*** NOTICE: Setting NODOC=1 since ports tree is required"
-	echo "            and NOPORTS is set."
-	NODOC=yes
-fi
+	# Set to non-empty value to build virtual machine images as part of
+	# the release.
+	WITH_VMIMAGES=
+	WITH_COMPRESSED_VMIMAGES=
+	XZ_THREADS=0
 
-# If NOPORTS and/or NODOC are unset, they must not pass to make as variables.
-# The release makefile verifies definedness of NOPORTS/NODOC variables
-# instead of their values.
-DOCPORTS=
-if [ -n "${NOPORTS}" ]; then
-	DOCPORTS="NOPORTS=yes "
-fi
-if [ -n "${NODOC}" ]; then
-	DOCPORTS="${DOCPORTS}NODOC=yes"
-fi
+	# Set to non-empty value to build virtual machine images for various
+	# cloud providers as part of the release.
+	WITH_CLOUDWARE=
 
-# The aggregated build-time flags based upon variables defined within
-# this file, unless overridden by release.conf.  In most cases, these
-# will not need to be changed.
-CONF_FILES="__MAKE_CONF=${MAKE_CONF} SRCCONF=${SRC_CONF}"
-if [ -n "${TARGET}" ] && [ -n "${TARGET_ARCH}" ]; then
-	ARCH_FLAGS="TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH}"
-else
-	ARCH_FLAGS=
-fi
-CHROOT_MAKEENV="${CHROOT_MAKEENV} MAKEOBJDIRPREFIX=${CHROOTDIR}/tmp/obj"
-CHROOT_WMAKEFLAGS="${MAKE_FLAGS} ${WORLD_FLAGS} ${CONF_FILES}"
-CHROOT_IMAKEFLAGS="${CONF_FILES}"
-CHROOT_DMAKEFLAGS="${CONF_FILES}"
-RELEASE_WMAKEFLAGS="${MAKE_FLAGS} ${WORLD_FLAGS} ${ARCH_FLAGS} ${CONF_FILES}"
-RELEASE_KMAKEFLAGS="${MAKE_FLAGS} ${KERNEL_FLAGS} KERNCONF=\"${KERNEL}\" ${ARCH_FLAGS} ${CONF_FILES}"
-RELEASE_RMAKEFLAGS="${ARCH_FLAGS} KERNCONF=\"${KERNEL}\" ${CONF_FILES} \
-	${DOCPORTS} WITH_DVD=${WITH_DVD} WITH_VMIMAGES=${WITH_VMIMAGES} \
-	WITH_CLOUDWARE=${WITH_CLOUDWARE}"
+	return 0
+} # env_setup()
 
-# Force src checkout if configured
-FORCE_SRC_KEY=
-if [ -n "${SRC_FORCE_CHECKOUT}" ]; then
-	FORCE_SRC_KEY="--force"
-fi
+# env_check(): Perform sanity tests on the build environment, such as ensuring
+# files/directories exist, as well as adding backwards-compatibility hacks if
+# necessary.  This is called unconditionally, and overrides the defaults set
+# in env_setup() if '-c <release.conf>' is specified.
+env_check() {
+	chroot_build_release_cmd="chroot_build_release"
+	# Fix for backwards-compatibility with release.conf that does not have
+	# the trailing '/'.
+	case ${SVNROOT} in
+		*svn*)
+			SVNROOT="${SVNROOT}/"
+			;;
+		*)
+			;;
+	esac
 
-if [ -z "${CHROOTDIR}" ]; then
-	echo "Please set CHROOTDIR."
-	exit 1
-fi
+	# Prefix the branches with the SVNROOT for the full checkout URL.
+	SRCBRANCH="${SVNROOT}${SRCBRANCH}"
+	DOCBRANCH="${SVNROOT}${DOCBRANCH}"
+	PORTBRANCH="${SVNROOT}${PORTBRANCH}"
 
-if [ $(id -u) -ne 0 ]; then
-	echo "Needs to be run as root."
-	exit 1
-fi
-
-set -e # Everything must succeed
-
-mkdir -p ${CHROOTDIR}/usr
-
-if [ -z "${SRC_UPDATE_SKIP}" ]; then
-	${VCSCMD} ${FORCE_SRC_KEY} ${SRCBRANCH} ${CHROOTDIR}/usr/src
-fi
-if [ -z "${NODOC}" ] && [ -z "${DOC_UPDATE_SKIP}" ]; then
-	${VCSCMD} ${DOCBRANCH} ${CHROOTDIR}/usr/doc
-fi
-if [ -z "${NOPORTS}" ] && [ -z "${PORTS_UPDATE_SKIP}" ]; then
-	${VCSCMD} ${PORTBRANCH} ${CHROOTDIR}/usr/ports
-fi
-
-if [ -z "${CHROOTBUILD_SKIP}" ]; then
-	cd ${CHROOTDIR}/usr/src
-	env ${CHROOT_MAKEENV} make ${CHROOT_WMAKEFLAGS} buildworld
-	env ${CHROOT_MAKEENV} make ${CHROOT_IMAKEFLAGS} installworld \
-		DESTDIR=${CHROOTDIR}
-	env ${CHROOT_MAKEENV} make ${CHROOT_DMAKEFLAGS} distribution \
-		DESTDIR=${CHROOTDIR}
-fi
-mount -t devfs devfs ${CHROOTDIR}/dev
-cp /etc/resolv.conf ${CHROOTDIR}/etc/resolv.conf
-trap "umount ${CHROOTDIR}/dev" EXIT # Clean up devfs mount on exit
-
-# If MAKE_CONF and/or SRC_CONF are set and not character devices (/dev/null),
-# copy them to the chroot.
-if [ -e ${MAKE_CONF} ] && [ ! -c ${MAKE_CONF} ]; then
-	mkdir -p ${CHROOTDIR}/$(dirname ${MAKE_CONF})
-	cp ${MAKE_CONF} ${CHROOTDIR}/${MAKE_CONF}
-fi
-if [ -e ${SRC_CONF} ] && [ ! -c ${SRC_CONF} ]; then
-	mkdir -p ${CHROOTDIR}/$(dirname ${SRC_CONF})
-	cp ${SRC_CONF} ${CHROOTDIR}/${SRC_CONF}
-fi
-
-# Embedded builds do not use the 'make release' target.
-if [ -n "${EMBEDDEDBUILD}" ]; then
-	# If a crochet configuration file exists in *this* checkout of
-	# release/, copy it to the /tmp/external directory within the chroot.
-	# This allows building embedded releases without relying on updated
-	# scripts and/or configurations to exist in the branch being built.
-	if [ -e ${RELENGDIR}/tools/${XDEV}/crochet-${KERNEL}.conf ] && \
-		[ -e ${RELENGDIR}/${XDEV}/release.sh ]; then
-			mkdir -p ${CHROOTDIR}/tmp/external/${XDEV}/
-			cp ${RELENGDIR}/tools/${XDEV}/crochet-${KERNEL}.conf \
-				${CHROOTDIR}/tmp/external/${XDEV}/crochet-${KERNEL}.conf
-			/bin/sh ${RELENGDIR}/${XDEV}/release.sh
+	if [ -n "${EMBEDDEDBUILD}" ]; then
+		WITH_DVD=
+		WITH_COMPRESSED_IMAGES=
+		NODOC=yes
+		case ${EMBEDDED_TARGET}:${EMBEDDED_TARGET_ARCH} in
+			arm:armv6)
+				chroot_build_release_cmd="chroot_arm_armv6_build_release"
+				;;
+			*)
+		esac
 	fi
-	# If the script does not exist for this architecture, exit.
-	# This probably should be checked earlier, but allowing the rest
-	# of the build process to get this far will at least set up the
-	# chroot environment for testing.
-	exit 0
-else
-	# Not embedded.
-	continue
-fi
 
-if [ -d ${CHROOTDIR}/usr/ports ]; then
+	# If PORTS is set and NODOC is unset, force NODOC=yes because the ports
+	# tree is required to build the documentation set.
+	if [ -n "${NOPORTS}" ] && [ -z "${NODOC}" ]; then
+		echo "*** NOTICE: Setting NODOC=1 since ports tree is required"
+		echo "            and NOPORTS is set."
+		NODOC=yes
+	fi
+
+	# If NOPORTS and/or NODOC are unset, they must not pass to make as
+	# variables.  The release makefile verifies definedness of the
+	# NOPORTS/NODOC variables instead of their values.
+	DOCPORTS=
+	if [ -n "${NOPORTS}" ]; then
+		DOCPORTS="NOPORTS=yes "
+	fi
+	if [ -n "${NODOC}" ]; then
+		DOCPORTS="${DOCPORTS}NODOC=yes"
+	fi
+
+	# The aggregated build-time flags based upon variables defined within
+	# this file, unless overridden by release.conf.  In most cases, these
+	# will not need to be changed.
+	CONF_FILES="__MAKE_CONF=${MAKE_CONF} SRCCONF=${SRC_CONF}"
+	if [ -n "${TARGET}" ] && [ -n "${TARGET_ARCH}" ]; then
+		ARCH_FLAGS="TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH}"
+	else
+		ARCH_FLAGS=
+	fi
+	# Force src checkout if configured
+	FORCE_SRC_KEY=
+	if [ -n "${SRC_FORCE_CHECKOUT}" ]; then
+		FORCE_SRC_KEY="--force"
+	fi
+
+	if [ -z "${CHROOTDIR}" ]; then
+		echo "Please set CHROOTDIR."
+		exit 1
+	fi
+
+	if [ $(id -u) -ne 0 ]; then
+		echo "Needs to be run as root."
+		exit 1
+	fi
+
+	CHROOT_MAKEENV="${CHROOT_MAKEENV} \
+		MAKEOBJDIRPREFIX=${CHROOTDIR}/tmp/obj"
+	CHROOT_WMAKEFLAGS="${MAKE_FLAGS} ${WORLD_FLAGS} ${CONF_FILES}"
+	CHROOT_IMAKEFLAGS="${CONF_FILES}"
+	CHROOT_DMAKEFLAGS="${CONF_FILES}"
+	RELEASE_WMAKEFLAGS="${MAKE_FLAGS} ${WORLD_FLAGS} ${ARCH_FLAGS} \
+		${CONF_FILES}"
+	RELEASE_KMAKEFLAGS="${MAKE_FLAGS} ${KERNEL_FLAGS} \
+		KERNCONF=\"${KERNEL}\" ${ARCH_FLAGS} ${CONF_FILES}"
+	RELEASE_RMAKEFLAGS="${ARCH_FLAGS} \
+		KERNCONF=\"${KERNEL}\" ${CONF_FILES} ${DOCPORTS} \
+		WITH_DVD=${WITH_DVD} WITH_VMIMAGES=${WITH_VMIMAGES} \
+		WITH_CLOUDWARE=${WITH_CLOUDWARE} XZ_THREADS=${XZ_THREADS}"
+
+	return 0
+} # env_check()
+
+# chroot_setup(): Prepare the build chroot environment for the release build.
+chroot_setup() {
+	load_chroot_env
+	mkdir -p ${CHROOTDIR}/usr
+
+	if [ -z "${SRC_UPDATE_SKIP}" ]; then
+		${VCSCMD} ${FORCE_SRC_KEY} ${SRCBRANCH} ${CHROOTDIR}/usr/src
+	fi
+	if [ -z "${NODOC}" ] && [ -z "${DOC_UPDATE_SKIP}" ]; then
+		${VCSCMD} ${DOCBRANCH} ${CHROOTDIR}/usr/doc
+	fi
+	if [ -z "${NOPORTS}" ] && [ -z "${PORTS_UPDATE_SKIP}" ]; then
+		${VCSCMD} ${PORTBRANCH} ${CHROOTDIR}/usr/ports
+	fi
+
+	if [ -z "${CHROOTBUILD_SKIP}" ]; then
+		cd ${CHROOTDIR}/usr/src
+		env ${CHROOT_MAKEENV} make ${CHROOT_WMAKEFLAGS} buildworld
+		env ${CHROOT_MAKEENV} make ${CHROOT_IMAKEFLAGS} installworld \
+			DESTDIR=${CHROOTDIR}
+		env ${CHROOT_MAKEENV} make ${CHROOT_DMAKEFLAGS} distribution \
+			DESTDIR=${CHROOTDIR}
+	fi
+
+	return 0
+} # chroot_setup()
+
+# extra_chroot_setup(): Prepare anything additional within the build
+# necessary for the release build.
+extra_chroot_setup() {
+	mkdir -p ${CHROOTDIR}/dev
+	mount -t devfs devfs ${CHROOTDIR}/dev
+	[ -e /etc/resolv.conf ] && cp /etc/resolv.conf \
+		${CHROOTDIR}/etc/resolv.conf
 	# Run ldconfig(8) in the chroot directory so /var/run/ld-elf*.so.hints
 	# is created.  This is needed by ports-mgmt/pkg.
-	chroot ${CHROOTDIR} /etc/rc.d/ldconfig forcerestart
+	eval chroot ${CHROOTDIR} /etc/rc.d/ldconfig forcerestart
 
-	## Trick the ports 'run-autotools-fixup' target to do the right thing.
-	_OSVERSION=$(sysctl -n kern.osreldate)
-	REVISION=$(chroot ${CHROOTDIR} make -C /usr/src/release -V REVISION)
-	BRANCH=$(chroot ${CHROOTDIR} make -C /usr/src/release -V BRANCH)
-	UNAME_r=${REVISION}-${BRANCH}
-	if [ -d ${CHROOTDIR}/usr/doc ] && [ -z "${NODOC}" ]; then
-		PBUILD_FLAGS="OSVERSION=${_OSVERSION} BATCH=yes"
-		PBUILD_FLAGS="${PBUILD_FLAGS} UNAME_r=${UNAME_r}"
-		PBUILD_FLAGS="${PBUILD_FLAGS} OSREL=${REVISION}"
-		chroot ${CHROOTDIR} make -C /usr/ports/textproc/docproj \
-			${PBUILD_FLAGS} OPTIONS_UNSET="FOP IGOR" \
-			install clean distclean
+	# If MAKE_CONF and/or SRC_CONF are set and not character devices
+	# (/dev/null), copy them to the chroot.
+	if [ -e ${MAKE_CONF} ] && [ ! -c ${MAKE_CONF} ]; then
+		mkdir -p ${CHROOTDIR}/$(dirname ${MAKE_CONF})
+		cp ${MAKE_CONF} ${CHROOTDIR}/${MAKE_CONF}
 	fi
-fi
+	if [ -e ${SRC_CONF} ] && [ ! -c ${SRC_CONF} ]; then
+		mkdir -p ${CHROOTDIR}/$(dirname ${SRC_CONF})
+		cp ${SRC_CONF} ${CHROOTDIR}/${SRC_CONF}
+	fi
 
-eval chroot ${CHROOTDIR} make -C /usr/src ${RELEASE_WMAKEFLAGS} buildworld
-eval chroot ${CHROOTDIR} make -C /usr/src ${RELEASE_KMAKEFLAGS} buildkernel
-eval chroot ${CHROOTDIR} make -C /usr/src/release ${RELEASE_RMAKEFLAGS} \
-	release
-eval chroot ${CHROOTDIR} make -C /usr/src/release ${RELEASE_RMAKEFLAGS} \
-	install DESTDIR=/R WITH_COMPRESSED_IMAGES=${WITH_COMPRESSED_IMAGES} \
-	WITH_COMPRESSED_VMIMAGES=${WITH_COMPRESSED_VMIMAGES}
+	if [ -d ${CHROOTDIR}/usr/ports ]; then
+		# Trick the ports 'run-autotools-fixup' target to do the right
+		# thing.
+		_OSVERSION=$(chroot ${CHROOTDIR} /usr/bin/uname -U)
+		REVISION=$(chroot ${CHROOTDIR} make -C /usr/src/release -V REVISION)
+		BRANCH=$(chroot ${CHROOTDIR} make -C /usr/src/release -V BRANCH)
+		UNAME_r=${REVISION}-${BRANCH}
+		if [ -d ${CHROOTDIR}/usr/doc ] && [ -z "${NODOC}" ]; then
+			PBUILD_FLAGS="OSVERSION=${_OSVERSION} BATCH=yes"
+			PBUILD_FLAGS="${PBUILD_FLAGS} UNAME_r=${UNAME_r}"
+			PBUILD_FLAGS="${PBUILD_FLAGS} OSREL=${REVISION}"
+			chroot ${CHROOTDIR} make -C /usr/ports/textproc/docproj \
+				${PBUILD_FLAGS} OPTIONS_UNSET="FOP IGOR" \
+				install clean distclean
+		fi
+	fi
+
+	if [ ! -z "${EMBEDDEDPORTS}" ]; then
+		for _PORT in ${EMBEDDEDPORTS}; do
+			eval chroot ${CHROOTDIR} make -C /usr/ports/${_PORT} \
+				BATCH=1 FORCE_PKG_REGISTER=1 install clean distclean
+		done
+	fi
+
+	buildenv_setup
+
+	return 0
+} # extra_chroot_setup()
+
+# chroot_build_target(): Build the userland and kernel for the build target.
+chroot_build_target() {
+	load_target_env
+	if [ ! -z "${EMBEDDEDBUILD}" ]; then
+		RELEASE_WMAKEFLAGS="${RELEASE_WMAKEFLAGS} \
+			TARGET=${EMBEDDED_TARGET} \
+			TARGET_ARCH=${EMBEDDED_TARGET_ARCH}"
+		RELEASE_KMAKEFLAGS="${RELEASE_KMAKEFLAGS} \
+			TARGET=${EMBEDDED_TARGET} \
+			TARGET_ARCH=${EMBEDDED_TARGET_ARCH}"
+	fi
+	eval chroot ${CHROOTDIR} make -C /usr/src ${RELEASE_WMAKEFLAGS} buildworld
+	eval chroot ${CHROOTDIR} make -C /usr/src ${RELEASE_KMAKEFLAGS} buildkernel
+
+	return 0
+} # chroot_build_target
+
+# chroot_build_release(): Invoke the 'make release' target.
+chroot_build_release() {
+	load_target_env
+	eval chroot ${CHROOTDIR} make -C /usr/src/release \
+		${RELEASE_RMAKEFLAGS} release
+	eval chroot ${CHROOTDIR} make -C /usr/src/release \
+		${RELEASE_RMAKEFLAGS} install DESTDIR=/R \
+		WITH_COMPRESSED_IMAGES=${WITH_COMPRESSED_IMAGES} \
+		WITH_COMPRESSED_VMIMAGES=${WITH_COMPRESSED_VMIMAGES}
+
+	return 0
+} # chroot_build_release()
+
+# chroot_arm_armv6_build_release(): Create arm/armv6 SD card image.
+chroot_arm_armv6_build_release() {
+	load_target_env
+	eval chroot ${CHROOTDIR} make -C /usr/src/release obj
+	if [ -e "${RELENGDIR}/tools/${EMBEDDED_TARGET}.subr" ]; then
+		. "${RELENGDIR}/tools/${EMBEDDED_TARGET}.subr"
+	fi
+	[ ! -z "${RELEASECONF}" ] && . "${RELEASECONF}"
+	WORLDDIR="$(eval chroot ${CHROOTDIR} make -C /usr/src/release -V WORLDDIR)"
+	OBJDIR="$(eval chroot ${CHROOTDIR} make -C /usr/src/release -V .OBJDIR)"
+	DESTDIR="${OBJDIR}/${KERNEL}"
+	IMGBASE="${CHROOTDIR}/${OBJDIR}/${KERNEL}.img"
+	OSRELEASE="$(eval chroot ${CHROOTDIR} make -C /usr/src/release \
+		TARGET=${EMBEDDED_TARGET} TARGET_ARCH=${EMBEDDED_TARGET_ARCH} \
+		-V OSRELEASE)"
+	chroot ${CHROOTDIR} mkdir -p ${DESTDIR}
+	chroot ${CHROOTDIR} truncate -s ${IMAGE_SIZE} ${IMGBASE##${CHROOTDIR}}
+	export mddev=$(chroot ${CHROOTDIR} \
+		mdconfig -f ${IMGBASE##${CHROOTDIR}} ${MD_ARGS})
+	arm_create_disk
+	arm_install_base
+	arm_install_uboot
+	mdconfig -d -u ${mddev}
+	chroot ${CHROOTDIR} rmdir ${DESTDIR}
+	mv ${IMGBASE} ${CHROOTDIR}/${OBJDIR}/${OSRELEASE}-${KERNEL}.img
+	chroot ${CHROOTDIR} mkdir -p /R
+	chroot ${CHROOTDIR} cp -p ${OBJDIR}/${OSRELEASE}-${KERNEL}.img \
+		/R/${OSRELEASE}-${KERNEL}.img
+	chroot ${CHROOTDIR} xz -T ${XZ_THREADS} /R/${OSRELEASE}-${KERNEL}.img
+	cd ${CHROOTDIR}/R && sha256 ${OSRELEASE}* \
+		> CHECKSUM.SHA256
+	cd ${CHROOTDIR}/R && md5 ${OSRELEASE}* \
+		> CHECKSUM.MD5
+
+	return 0
+} # chroot_arm_armv6_build_release()
+
+# main(): Start here.
+main() {
+	set -e # Everything must succeed
+	env_setup
+	while getopts c: opt; do
+		case ${opt} in
+			c)
+				RELEASECONF="${OPTARG}"
+				;;
+			\?)
+				usage
+				;;
+		esac
+	done
+	shift $(($OPTIND - 1))
+	if [ ! -z "${RELEASECONF}" ]; then
+		if [ -e "${RELEASECONF}" ]; then
+			. ${RELEASECONF}
+		else
+			echo "Nonexistent configuration file: ${RELEASECONF}"
+			echo "Using default build environment."
+		fi
+	fi
+	env_check
+	trap "umount ${CHROOTDIR}/dev" EXIT # Clean up devfs mount on exit
+	chroot_setup
+	extra_chroot_setup
+	chroot_build_target
+	${chroot_build_release_cmd}
+
+	return 0
+} # main()
+
+main "${@}"
