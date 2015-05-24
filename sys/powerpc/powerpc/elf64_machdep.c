a067071309f419ff90a12c479cb504495390a5df
dchagin <dchagin@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-05-24 14:51:29 +0000

    In preparation for switching linuxulator to the use the native 1:1 threads add a hook for cleaning thread resources before the thread die.
    Differential Revision:	https://reviews.freebsd.org/D1038
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@283382 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/powerpc/powerpc/elf64_machdep.c
+++ sys/powerpc/powerpc/elf64_machdep.c
@@ -84,6 +84,7 @@ struct sysentvec elf64_freebsd_sysvec = {
 	.sv_shared_page_base = SHAREDPAGE,
 	.sv_shared_page_len = PAGE_SIZE,
 	.sv_schedtail	= NULL,
+	.sv_thread_detach = NULL,
 };
 INIT_SYSENTVEC(elf64_sysvec, &elf64_freebsd_sysvec);
 
