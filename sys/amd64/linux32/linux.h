a7ac45761335b1d514804cd0bfe44bdb2c3a3e0c
Dmitry Chagin <dchagin@FreeBSD.org>
2015-05-24 17:44:08 +0000

    According to Linux man sigaltstack(3) shall return EINVAL if the ss argument is not a null pointer, and the ss_flags member pointed to by ss contains flags other than SS_DISABLE. However, in fact, Linux also allows SS_ONSTACK flag which is simply ignored.
    For buggy apps (at least mono) ignore other than SS_DISABLE
    flags as a Linux do.
    
    While here move MI part of sigaltstack code to the appropriate place.
    
    Reported by:	abi at abinet dot ru

--- sys/amd64/linux32/linux.h
+++ sys/amd64/linux32/linux.h
@@ -328,9 +328,6 @@ struct l_statfs64 {
 #define	LINUX_SS_ONSTACK	1
 #define	LINUX_SS_DISABLE	2
 
-int linux_to_bsd_sigaltstack(int lsa);
-int bsd_to_linux_sigaltstack(int bsa);
-
 typedef l_uintptr_t l_handler_t;
 typedef l_ulong	l_osigset_t;
 
