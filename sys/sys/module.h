a36f0ec32104fa3e26da65210b7e4d6d4eef12d1
Dimitry Andric <dim@FreeBSD.org>
2015-05-28 06:45:13 +0000

    MFC r283254:
    Ensure that the static structs emitted by the MODULE_DEPEND() and
    MODULE_VERSION() macros don't end up as .comm symbols, when all the
    version fields are zero.
    
    Normally, such symbols will end up in .bss, but for kernel module
    version objects, this can lead to "garbage" version numbers.
    
    Fix this by instructing the compiler to always put these structs in the
    .data segment instead.
    
    Reported by:	delphij, ae

--- sys/sys/module.h
+++ sys/sys/module.h
@@ -106,7 +106,8 @@ struct mod_metadata {
 	DATA_SET(modmetadata_set, _mod_metadata##uniquifier)
 
 #define	MODULE_DEPEND(module, mdepend, vmin, vpref, vmax)		\
-	static struct mod_depend _##module##_depend_on_##mdepend = {	\
+	static struct mod_depend _##module##_depend_on_##mdepend	\
+	    __section(".data") = {					\
 		vmin,							\
 		vpref,							\
 		vmax							\
@@ -146,7 +147,8 @@ struct mod_metadata {
 	DECLARE_MODULE_WITH_MAXVER(name, data, sub, order, __FreeBSD_version)
 
 #define	MODULE_VERSION(module, version)					\
-	static struct mod_version _##module##_version = {		\
+	static struct mod_version _##module##_version			\
+	    __section(".data") = {					\
 		version							\
 	};								\
 	MODULE_METADATA(_##module##_version, MDT_VERSION,		\
