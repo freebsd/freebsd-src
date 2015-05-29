980006f4e8faae5fed42779e0df67f8b3dabde07
David C Somayajulu <davidcs@FreeBSD.org>
2015-05-29 21:30:11 +0000

    MFC r283274
    Submitted by:	gary.zambrano@qlogic.com
    Approved by:davidcs@freebsd.org

--- sys/dev/bxe/bxe_stats.c
+++ sys/dev/bxe/bxe_stats.c
@@ -1227,6 +1227,7 @@ bxe_drv_stats_update(struct bxe_softc *sc)
         UPDATE_ESTAT_QSTAT(rx_calls);
         UPDATE_ESTAT_QSTAT(rx_pkts);
         UPDATE_ESTAT_QSTAT(rx_tpa_pkts);
+        UPDATE_ESTAT_QSTAT(rx_jumbo_sge_pkts);
         UPDATE_ESTAT_QSTAT(rx_soft_errors);
         UPDATE_ESTAT_QSTAT(rx_hw_csum_errors);
         UPDATE_ESTAT_QSTAT(rx_ofld_frames_csum_ip);
