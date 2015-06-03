0c85196b0c51b4e5eba8fcace026f947f9112c9e
Jung-uk Kim <jkim@FreeBSD.org>
2015-06-03 19:27:36 +0000

    - Update for new NFIT table GUIDs. - Fix ill-formed GUID strings for NFIT tables. - Fix a possible fault when performing a UUID search.
    https://github.com/acpica/acpica/commit/83727be
    https://github.com/acpica/acpica/commit/6005294
    https://github.com/acpica/acpica/commit/4a1cca9

--- source/tools/acpihelp/ahdecode.c
+++ source/tools/acpihelp/ahdecode.c
@@ -912,12 +912,32 @@ AhDisplayUuids (
     const AH_UUID           *Info;
 
 
-    printf ("ACPI-related UUIDs:\n\n");
+    printf ("ACPI-related UUIDs/GUIDs:\n");
+
+    /* Display entire table of known ACPI-related UUIDs/GUIDs */
 
     for (Info = AcpiUuids; Info->Description; Info++)
     {
-        printf ("%32s : %s\n", Info->Description, Info->String);
+        if (!Info->String) /* Null UUID string means group description */
+        {
+            printf ("\n%36s\n", Info->Description);
+        }
+        else
+        {
+            printf ("%32s : %s\n", Info->Description, Info->String);
+        }
     }
+
+    /* Help info on how UUIDs/GUIDs strings are encoded */
+
+    printf ("\n\nByte encoding of UUID/GUID strings"
+        " into ACPI Buffer objects (use ToUUID from ASL):\n\n");
+
+    printf ("%32s : %s\n", "Input UUID/GUID String format",
+        "aabbccdd-eeff-gghh-iijj-kkllmmnnoopp");
+
+    printf ("%32s : %s\n", "Expected output ACPI buffer",
+        "dd,cc,bb,aa, ff,ee, hh,gg, ii,jj, kk,ll,mm,nn,oo,pp");
 }
 
 
