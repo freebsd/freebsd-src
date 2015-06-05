7dbdb3de2005eae8716098ffc90605a7f88b9470
avg <avg@FreeBSD.org>
2015-06-05 17:26:07 +0000

    1778 Assertion failed: rn->rn_nozpool == B_FALSE, file ../common/libzfs_import.c, line 1077, function zpool_open_func
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: George Wilson <george@delphix.com>
    Reviewed by: Richard Elling <richard.elling@richardelling.com>
    Approved by: Gordon Ross <gordon.ross@nexenta.com>
    Author: Andrew Stormont <andyjstormont@gmail.com>
    
    illumos/illumos-gate@bd0f709169e67f4bd34526e186a7c34f595f0d9b

--- lib/libzfs/common/libzfs_import.c
+++ lib/libzfs/common/libzfs_import.c
@@ -23,6 +23,7 @@
  * Copyright 2015 Nexenta Systems, Inc.  All rights reserved.
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2013 by Delphix. All rights reserved.
+ * Copyright 2015 RackTop Systems.
  */
 
 /*
@@ -1079,11 +1080,7 @@ zpool_open_func(void *arg)
 	}
 	(void) close(fd);
 
-
 	rn->rn_config = config;
-	if (config != NULL) {
-		assert(rn->rn_nozpool == B_FALSE);
-	}
 }
 
 /*
