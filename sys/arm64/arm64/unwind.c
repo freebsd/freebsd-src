b9b324dc0d6afb34b1070f7049bc7913a8a43e13
br <br@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-06-11 12:47:13 +0000

    Split out db_unwind_frame() so it can be used by DTrace.
    Sponsored by:	ARM Ltd.
    Differential Revision:	https://reviews.freebsd.org/D2741
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@284257 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/arm64/arm64/unwind.c
+++ sys/arm64/arm64/unwind.c
@@ -1,6 +1,9 @@
 /*-
- * Copyright (c) 2001 Jake Burkholder.
+ * Copyright (c) 2015 The FreeBSD Foundation
  * All rights reserved.
+ *
+ * This software was developed by Semihalf under
+ * the sponsorship of the FreeBSD Foundation.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -14,7 +17,7 @@
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -22,22 +25,28 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $FreeBSD$
  */
 
-#ifndef _MACHINE_STACK_H_
-#define	_MACHINE_STACK_H_
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+#include <sys/param.h>
 
-#define	INKERNEL(va) \
-	((va) >= VM_MIN_KERNEL_ADDRESS && (va) <= VM_MAX_KERNEL_ADDRESS)
+#include <machine/stack.h>
 
-struct unwind_state {
+int
+unwind_frame(struct unwind_state *frame)
+{
 	uint64_t fp;
-	uint64_t sp;
-	uint64_t pc;
-};
 
-int unwind_frame(struct unwind_state *);
+	fp = frame->fp;
+	if (fp == 0)
+		return (-1);
 
-#endif /* !_MACHINE_STACK_H_ */
+	frame->sp = fp + 0x10;
+	/* FP to previous frame (X29) */
+	frame->fp = *(uint64_t *)(fp);
+	/* LR (X30) */
+	frame->pc = *(uint64_t *)(fp + 8) - 4;
+
+	return (0);
+}
