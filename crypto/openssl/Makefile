0ce0dd18b3a24092af17d63e21de3004fb69ae0f
Jung-uk Kim <jkim@FreeBSD.org>
2015-06-11 19:39:27 +0000

    Merge OpenSSL 0.9.8zg.

--- crypto/openssl/Makefile
+++ crypto/openssl/Makefile
@@ -4,7 +4,7 @@
 ## Makefile for OpenSSL
 ##
 
-VERSION=0.9.8zf
+VERSION=0.9.8zg
 MAJOR=0
 MINOR=9.8
 SHLIB_VERSION_NUMBER=0.9.8
--- crypto/openssl/crypto/opensslv.h
+++ crypto/openssl/crypto/opensslv.h
@@ -26,11 +26,11 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-# define OPENSSL_VERSION_NUMBER  0x009081ffL
+# define OPENSSL_VERSION_NUMBER  0x0090820fL
 # ifdef OPENSSL_FIPS
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 0.9.8zf-fips 19 Mar 2015"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 0.9.8zg-fips 11 Jun 2015"
 # else
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 0.9.8zf-freebsd 19 Mar 2015"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 0.9.8zg-freebsd 11 Jun 2015"
 # endif
 # define OPENSSL_VERSION_PTEXT   " part of " OPENSSL_VERSION_TEXT
 
