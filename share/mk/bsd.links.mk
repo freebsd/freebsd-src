89cabb5370832e2f74f272178c3179314afeb074
ngie <ngie@FreeBSD.org>
2015-06-13 23:12:06 +0000

    Restore the more complicated logic removed in r280122 so fmake can bootstrap bmake for buildworld

--- share/mk/bsd.links.mk
+++ share/mk/bsd.links.mk
@@ -7,6 +7,7 @@
 afterinstall: _installlinks
 .ORDER: realinstall _installlinks
 _installlinks:
+.ifdef .PARSEDIR
 .for s t in ${LINKS}
 	@${ECHO} "$t -> $s" ;\
 	${INSTALL_LINK} ${DESTDIR}$s ${DESTDIR}$t
@@ -15,3 +16,27 @@ _installlinks:
 	@${ECHO} "$t -> $s" ;\
 	${INSTALL_SYMLINK} $s ${DESTDIR}/$t
 .endfor
+.else # The following logic is needed for FMAKE in the bootstrapping process
+.if defined(LINKS) && !empty(LINKS)
+	@set ${LINKS}; \
+	while test $$# -ge 2; do \
+		l=${DESTDIR}$$1; \
+		shift; \
+		t=${DESTDIR}$$1; \
+		shift; \
+		${ECHO} $$t -\> $$l; \
+		${INSTALL_LINK} $$l $$t; \
+	done; true
+.endif
+.if defined(SYMLINKS) && !empty(SYMLINKS)
+	@set ${SYMLINKS}; \
+	while test $$# -ge 2; do \
+		l=$$1; \
+		shift; \
+		t=${DESTDIR}$$1; \
+		shift; \
+		${ECHO} $$t -\> $$l; \
+		${INSTALL_SYMLINK} $$l $$t; \
+	done; true
+.endif
+.endif
