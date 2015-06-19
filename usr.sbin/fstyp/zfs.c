cffc38e9630e8434c12cf3ccd4cf4bfae4578e4a
allanjude <allanjude@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-06-19 05:09:02 +0000

    Add the ability to detect ZFS and GELI encrypted file systems to fstyp(8)
    Functionality is hidden behind the -u flag to avoid confusing automounters
    
    PR:		200823
    Reviewed by:	asomers, eadler, wblock (man page)
    Approved by:	trasz
    MFC after:	10 days
    Relnotes:	yes
    Sponsored by:	ScaleEngine Inc.
    Differential Revision:	https://reviews.freebsd.org/D2045
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@284589 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/fstyp/zfs.c
+++ usr.sbin/fstyp/zfs.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2014 The FreeBSD Foundation
+ * Copyright (c) 2015 Allan Jude <allanjude@FreeBSD.org>
  * All rights reserved.
  *
  * This software was developed by Edward Tomasz Napierala under sponsorship
@@ -14,10 +14,10 @@
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -25,27 +25,49 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $FreeBSD$
  */
 
-#ifndef FSTYP_H
-#define	FSTYP_H
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
-#define	MIN(a,b) (((a)<(b))?(a):(b))
+#include <sys/types.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-void	*read_buf(FILE *fp, off_t off, size_t len);
-char	*checked_strdup(const char *s);
-void	rtrim(char *label, size_t size);
+#include <libnvpair.h>
+#include <sys/vdev_impl.h>
 
-int	fstyp_cd9660(FILE *fp, char *label, size_t size);
-int	fstyp_ext2fs(FILE *fp, char *label, size_t size);
-int	fstyp_geli(FILE *fp, char *label, size_t size);
-int	fstyp_msdosfs(FILE *fp, char *label, size_t size);
-int	fstyp_ntfs(FILE *fp, char *label, size_t size);
-int	fstyp_ufs(FILE *fp, char *label, size_t size);
-#ifdef HAVE_CDDL
-int	fstyp_zfs(FILE *fp, char *label, size_t size);
-#endif
+#include "fstyp.h"
 
-#endif /* !FSTYP_H */
+int
+fstyp_zfs(FILE *fp, char *label, size_t labelsize)
+{
+	vdev_label_t *zpool_ptr = NULL;
+	vdev_label_t zpool_label;
+	char *buf = zpool_label.vl_vdev_phys.vp_nvlist;
+	char *zpool_name = NULL;
+	size_t buflen = sizeof (zpool_label.vl_vdev_phys.vp_nvlist);
+	nvlist_t *config = NULL;
+
+	zpool_ptr = (vdev_label_t *)read_buf(fp, 0, sizeof(zpool_label));
+	if (zpool_ptr == NULL)
+		return (1);
+	zpool_label = *zpool_ptr;
+	if (nvlist_unpack(buf, buflen, &config, 0) != 0)
+		goto zfserr;
+	if (nvlist_lookup_string(config, "name", &zpool_name) != 0)
+		goto zfserr;
+	strlcpy(label, zpool_name, labelsize);
+	nvlist_free(config);
+	free(zpool_ptr);
+	return (0);
+
+zfserr:
+	nvlist_free(config);
+	free(zpool_ptr);
+
+	return (1);
+}
