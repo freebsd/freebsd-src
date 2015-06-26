f8cfbb75a7a0395b305b7f344fb1bf4420bceec9
andrew <andrew@FreeBSD.org>
2015-06-26 09:02:40 +0000

    pc_curpmap is only in the armv6 pcpu data.

--- sys/arm/arm/genassym.c
+++ sys/arm/arm/genassym.c
@@ -136,7 +136,9 @@ ASSYM(ARM_RAS_END, ARM_RAS_END);
 ASSYM(PCB_VFPSTATE, offsetof(struct pcb, pcb_vfpstate));
 #endif
 
+#if __ARM_ARCH >= 6
 ASSYM(PC_CURPMAP, offsetof(struct pcpu, pc_curpmap));
+#endif
 
 ASSYM(PAGE_SIZE, PAGE_SIZE);
 ASSYM(PMAP_DOMAIN_KERNEL, PMAP_DOMAIN_KERNEL);
