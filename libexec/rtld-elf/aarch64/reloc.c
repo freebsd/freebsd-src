025a2146d6bbb52dc0326907f80382b6ae77c290
andrew <andrew@FreeBSD.org>
2015-07-05 11:42:01 +0000

    Add on the addend when in the R_AARCH64_ABS64 and R_AARCH64_GLOB_DAT cases. This fixes at least sshd, and some of the boehm-gc tests.
    Sponsored by:	ABT Systems Ltd

--- libexec/rtld-elf/aarch64/reloc.c
+++ libexec/rtld-elf/aarch64/reloc.c
@@ -341,7 +341,8 @@ reloc_non_plt(Obj_Entry *obj, Obj_Entry *obj_rtld, int flags,
 			if (def == NULL)
 				return (-1);
 
-			*where = (Elf_Addr)defobj->relocbase + def->st_value;
+			*where = (Elf_Addr)defobj->relocbase + def->st_value +
+			    rela->r_addend;
 			break;
 		case R_AARCH64_COPY:
 			/*
