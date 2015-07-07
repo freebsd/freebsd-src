5e1b577ce643ec5ebafbb45278e9730f92659b48
Xin LI <delphij@FreeBSD.org>
2015-07-07 21:43:23 +0000

    Fix BIND resolver remote denial of service when validating.
    Security:	CVE-2015-4620
    Security:	FreeBSD-SA-15:11.bind

--- contrib/bind9/lib/dns/validator.c
+++ contrib/bind9/lib/dns/validator.c
@@ -1406,7 +1406,6 @@ compute_keytag(dns_rdata_t *rdata, dns_rdata_dnskey_t *key) {
  */
 static isc_boolean_t
 isselfsigned(dns_validator_t *val) {
-	dns_fixedname_t fixed;
 	dns_rdataset_t *rdataset, *sigrdataset;
 	dns_rdata_t rdata = DNS_RDATA_INIT;
 	dns_rdata_t sigrdata = DNS_RDATA_INIT;
@@ -1461,7 +1460,7 @@ isselfsigned(dns_validator_t *val) {
 
 			result = dns_dnssec_verify2(name, rdataset, dstkey,
 						    ISC_TRUE, mctx, &sigrdata,
-						    dns_fixedname_name(&fixed));
+						    NULL);
 			dst_key_free(&dstkey);
 			if (result != ISC_R_SUCCESS)
 				continue;
