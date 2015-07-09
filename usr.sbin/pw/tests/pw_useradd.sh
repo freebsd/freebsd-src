freebsd/freebsd-src

commit 09900c1c0553957ec9cf8dd9077adf0dee50b698

Also validate inputs of pw groupmod -h and groupmod -H
git-svn-id: svn+ssh://svn.freebsd.org/base/head@285136 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

usr.sbin/pw/pw_group.c
@@ -47,6 +47,50 @@ static void	delete_members(char ***members, int *grmembers, int *i,
 static int	print_group(struct group * grp);
 static gid_t    gr_gidpolicy(struct userconf * cnf, long id);
 
+static void
+set_passwd(struct group *grp, bool update)
+{
+	int		 b;
+	int		 istty;
+	struct termios	 t, n;
+	char		*p, line[256];
+
+	if (conf.fd == '-') {
+		grp->gr_passwd = "*";	/* No access */
+		return;
+	}
+	
+	if ((istty = isatty(conf.fd))) {
+		n = t;
+		/* Disable echo */
+		n.c_lflag &= ~(ECHO);
+		tcsetattr(conf.fd, TCSANOW, &n);
+		printf("%sassword for group %s:", update ? "New p" : "P",
+		    grp->gr_name);
+		fflush(stdout);
+	}
+	b = read(conf.fd, line, sizeof(line) - 1);
+	if (istty) {	/* Restore state */
+		tcsetattr(conf.fd, TCSANOW, &t);
+		fputc('\n', stdout);
+		fflush(stdout);
+	}
+	if (b < 0)
+		err(EX_OSERR, "-h file descriptor");
+	line[b] = '\0';
+	if ((p = strpbrk(line, " \t\r\n")) != NULL)
+		*p = '\0';
+	if (!*line)
+		errx(EX_DATAERR, "empty password read on file descriptor %d",
+		    conf.fd);
+	if (conf.precrypted) {
+		if (strchr(line, ':') != 0)
+			errx(EX_DATAERR, "wrong encrypted passwrd");
+		grp->gr_passwd = line;
+	} else
+		grp->gr_passwd = pw_pwcrypt(line);
+}
+
 int
 pw_group(int mode, char *name, long id, struct cargs * args)
 {
@@ -156,52 +200,8 @@ pw_group(int mode, char *name, long id, struct cargs * args)
 	 * software.
 	 */
 
-	if ((arg = getarg(args, 'h')) != NULL ||
-	    (arg = getarg(args, 'H')) != NULL) {
-		if (strcmp(arg->val, "-") == 0)
-			grp->gr_passwd = "*";	/* No access */
-		else {
-			int             fd = atoi(arg->val);
-			int		precrypt = (arg->ch == 'H');
-			int             b;
-			int             istty = isatty(fd);
-			struct termios  t;
-			char           *p, line[256];
-
-			if (istty) {
-				if (tcgetattr(fd, &t) == -1)
-					istty = 0;
-				else {
-					struct termios  n = t;
-
-					/* Disable echo */
-					n.c_lflag &= ~(ECHO);
-					tcsetattr(fd, TCSANOW, &n);
-					printf("%sassword for group %s:", (mode == M_UPDATE) ? "New p" : "P", grp->gr_name);
-					fflush(stdout);
-				}
-			}
-			b = read(fd, line, sizeof(line) - 1);
-			if (istty) {	/* Restore state */
-				tcsetattr(fd, TCSANOW, &t);
-				fputc('\n', stdout);
-				fflush(stdout);
-			}
-			if (b < 0)
-				err(EX_OSERR, "-h file descriptor");
-			line[b] = '\0';
-			if ((p = strpbrk(line, " \t\r\n")) != NULL)
-				*p = '\0';
-			if (!*line)
-				errx(EX_DATAERR, "empty password read on file descriptor %d", fd);
-			if (precrypt) {
-				if (strchr(line, ':') != NULL)
-					return EX_DATAERR;
-				grp->gr_passwd = line;
-			} else
-				grp->gr_passwd = pw_pwcrypt(line);
-		}
-	}
+	if (conf.fd != -1)
+		set_passwd(grp, mode == M_UPDATE);
 
 	if (((arg = getarg(args, 'M')) != NULL ||
 	    (arg = getarg(args, 'd')) != NULL ||

commit 0fc58d14467f114a86e2bae4bd411ade583afade

Do not try to set password on group if the group is added as a consequence of of creating a user (regression from r285136)
Reported by:	Fabian Keil <fk@fabiankeil.de>

usr.sbin/pw/pw.c
@@ -215,6 +215,7 @@ main(int argc, char *argv[])
 	if (mode == -1 || which == -1)
 		cmdhelp(mode, which);
 
+	conf.which = which;
 	/*
 	 * We know which mode we're in and what we're about to do, so now
 	 * let's dispatch the remaining command line args in a genric way.

usr.sbin/pw/pw_group.c
@@ -200,7 +200,7 @@ pw_group(int mode, char *name, long id, struct cargs * args)
 	 * software.
 	 */
 
-	if (conf.fd != -1)
+	if (conf.which == W_GROUP && conf.fd != -1)
 		set_passwd(grp, mode == M_UPDATE);
 
 	if (((arg = getarg(args, 'M')) != NULL ||

usr.sbin/pw/pwupd.h
@@ -86,6 +86,7 @@ struct pwconf {
 	char		*newname;
 	char		*config;
 	int		 fd;
+	int		 which;
 	bool		 dryrun;
 	bool		 pretty;
 	bool		 v7;

usr.sbin/pw/tests/pw_useradd.sh
@@ -231,6 +231,15 @@ user_add_invalid_group_entry_body() {
 	atf_check -s exit:1 -e match:"Invalid group entry" ${PW} useradd foo2
 }
 
+atf_test_case user_add_password_from_h
+user_add_password_from_h_body() {
+	populate_etc_skel
+
+	atf_check -s exit:0 ${PW} useradd test -h 0 <<-EOF
+	$(echo test)
+	EOF
+}
+
 atf_init_test_cases() {
 	atf_add_test_case user_add
 	atf_add_test_case user_add_noupdate
@@ -251,4 +260,5 @@ atf_init_test_cases() {
 	atf_add_test_case user_add_expiration
 	atf_add_test_case user_add_invalid_user_entry
 	atf_add_test_case user_add_invalid_group_entry
+	atf_add_test_case user_add_password_from_h
 }