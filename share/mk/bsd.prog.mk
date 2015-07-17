2721a23ca692fbce7d24c4ab53ec951ec5cf33dd
Brooks Davis <brooks@one-eyed-alien.net>
2015-07-17 17:33:17 +0000

    Tag pure cheri programs (and compartment objects) with the correct ELF machine type.

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -96,6 +96,9 @@ ${PROG_FULL}: ${OBJS}
 .if ${MK_CTF} != "no"
 	${CTFMERGE} ${CTFFLAGS} -o ${.TARGET} ${OBJS}
 .endif
+.if defined(NEED_CHERI) && ${NEED_CHERI} == "pure"
+	${CHERIFY} ${.TARGET}
+.endif
 
 .else	# !defined(SRCS)
 
