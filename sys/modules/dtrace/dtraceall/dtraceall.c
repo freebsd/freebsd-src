2e95c6bde7df63e64c4e6fa449cba75f88595f0a
markj <markj@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-07-19 22:14:09 +0000

    Implement the lockstat provider using SDT(9) instead of the custom provider in lockstat.ko. This means that lockstat probes now have typed arguments and will utilize SDT probe hot-patching support when it arrives.
    Reviewed by:	gnn
    Differential Revision:	https://reviews.freebsd.org/D2993
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@285703 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/modules/dtrace/dtraceall/dtraceall.c
+++ sys/modules/dtrace/dtraceall/dtraceall.c
@@ -76,7 +76,6 @@ MODULE_DEPEND(dtraceall, fbt, 1, 1, 1);
 #if defined(__amd64__) || defined(__i386__)
 MODULE_DEPEND(dtraceall, fasttrap, 1, 1, 1);
 #endif
-MODULE_DEPEND(dtraceall, lockstat, 1, 1, 1);
 MODULE_DEPEND(dtraceall, sdt, 1, 1, 1);
 MODULE_DEPEND(dtraceall, systrace, 1, 1, 1);
 #if defined(COMPAT_FREEBSD32)
