aa11395ba1a50b611ccfe3c6ce1a91a7e3502f7c
Xin LI <delphij@FreeBSD.org>
2015-07-21 23:42:56 +0000

    Fix resource exhaustion due to sessions stuck in LAST_ACK state.
    Security:	CVE-2015-5358
    Security:	SA-15:13.tcp
    Submitted by:	Jonathan Looney (Juniper SIRT)
    Approved by:	so

--- sys/netinet/tcp_output.c
+++ sys/netinet/tcp_output.c
@@ -400,7 +400,7 @@ after_sack_rexmit:
 		flags &= ~TH_FIN;
 	}
 
-	if (len < 0) {
+	if (len <= 0) {
 		/*
 		 * If FIN has been sent but not acked,
 		 * but we haven't been called to retransmit,
@@ -410,9 +410,16 @@ after_sack_rexmit:
 		 * to (closed) window, and set the persist timer
 		 * if it isn't already going.  If the window didn't
 		 * close completely, just wait for an ACK.
+		 *
+		 * We also do a general check here to ensure that
+		 * we will set the persist timer when we have data
+		 * to send, but a 0-byte window. This makes sure
+		 * the persist timer is set even if the packet
+		 * hits one of the "goto send" lines below.
 		 */
 		len = 0;
-		if (sendwin == 0) {
+		if ((sendwin == 0) && (TCPS_HAVEESTABLISHED(tp->t_state)) &&
+			(off < (int) so->so_snd.sb_cc)) {
 			tcp_timer_activate(tp, TT_REXMT, 0);
 			tp->t_rxtshift = 0;
 			tp->snd_nxt = tp->snd_una;
