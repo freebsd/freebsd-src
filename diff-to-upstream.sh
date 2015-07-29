5a7fd5682c35e50d8ec3864ad16543389cad2b58
Brooks Davis <brooks@one-eyed-alien.net>
2015-07-29 00:05:18 +0000

    Update diff script to match most recent merge of FreeBSD HEAD.

--- diff-to-upstream.sh
+++ diff-to-upstream.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-git diff --name-only freebsd-head-20150514 | \
+UPSTREAM_TAG=freebsd-head-20150723
+
+git diff --name-only ${UPSTREAM_TAG} | \
     grep -v -E '(cheri|bin/shmem_bench|contrib/(curl|jpeg|libpng|netsurf)|^ctsrd|lib/lib(curl|helloworld|jpeg|png)|libexec/.*-helper|usr.bin/libpng_sb_test|usr.bin/nsfb)' | \
-    xargs git diff freebsd-head-20150514 --
+    xargs git diff ${UPSTREAM_TAG} --
