3291146f907bb939e3e2d3a11f982e0658e8c5d8
dim <dim@FreeBSD.org>
2015-07-29 14:07:29 +0000

    Revert r286007-r286009 for now, until I can figure out how to make the fix compile with older gcc and libstdc++.

--- contrib/llvm/lib/Target/X86/X86ISelDAGToDAG.cpp
+++ contrib/llvm/lib/Target/X86/X86ISelDAGToDAG.cpp
@@ -290,13 +290,6 @@ namespace {
     const X86InstrInfo *getInstrInfo() const {
       return getTargetMachine().getInstrInfo();
     }
-
-    /// \brief Address-mode matching performs shift-of-and to and-of-shift
-    /// reassociation in order to expose more scaled addressing
-    /// opportunities.
-    bool ComplexPatternFuncMutatesDAG() const override {
-      return true;
-    }
   };
 }
 
