685318f335be91988efd5573d69047da1a3398ac
jmg <jmg@FreeBSD.org>
2015-07-31 00:31:52 +0000

    these are comparing authenticators and need to be constant time... This could be a side channel attack...  Now that we have a function for this, use it...
    jmgurney/ipsecgcm:	24d704cc and 7f37a14

--- sys/netipsec/xform_esp.c
+++ sys/netipsec/xform_esp.c
@@ -534,7 +534,7 @@ esp_input_cb(struct cryptop *crp)
 		ptr = (caddr_t) (tc + 1);
 
 		/* Verify authenticator */
-		if (bcmp(ptr, aalg, alen) != 0) {
+		if (timingsafe_bcmp(ptr, aalg, alen) != 0) {
 			DPRINTF(("%s: authentication hash mismatch for "
 			    "packet in SA %s/%08lx\n", __func__,
 			    ipsec_address(&saidx->dst, buf, sizeof(buf)),
