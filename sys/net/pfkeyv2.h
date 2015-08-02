9b301851a34b3888b82e57798d34f68aa91901b8
jmg <jmg@FreeBSD.org>
2015-08-02 00:15:52 +0000

    convert to C11's _Static_assert, and pull in sys/cdefs.h for compatibility w/ older non-C11 compilers...
    passed make tinerdbox..
    
    Suggested by:	imp

--- sys/net/pfkeyv2.h
+++ sys/net/pfkeyv2.h
@@ -39,9 +39,7 @@
 #ifndef _NET_PFKEYV2_H_
 #define _NET_PFKEYV2_H_
 
-#ifndef _KERNEL
-#define CTASSERT(x)	struct __thisisjustnothing;
-#endif
+#include <sys/cdefs.h>
 
 /*
 This file defines structures and symbols for the PF_KEY Version 2
@@ -231,7 +229,7 @@ struct sadb_x_policy {
   u_int32_t sadb_x_policy_id;
   u_int32_t sadb_x_policy_reserved2;
 };
-CTASSERT(sizeof(struct sadb_x_policy) == 16);
+_Static_assert(sizeof(struct sadb_x_policy) == 16, "struct size mismatch");
 
 /*
  * When policy_type == IPSEC, it is followed by some of
@@ -267,7 +265,7 @@ struct sadb_x_nat_t_type {
   u_int8_t sadb_x_nat_t_type_type;
   u_int8_t sadb_x_nat_t_type_reserved[3];
 };
-CTASSERT(sizeof(struct sadb_x_nat_t_type) == 8);
+_Static_assert(sizeof(struct sadb_x_nat_t_type) == 8, "struct size mismatch");
 
 /* NAT-Traversal source or destination port. */
 struct sadb_x_nat_t_port { 
@@ -276,7 +274,7 @@ struct sadb_x_nat_t_port {
   u_int16_t sadb_x_nat_t_port_port;
   u_int16_t sadb_x_nat_t_port_reserved;
 };
-CTASSERT(sizeof(struct sadb_x_nat_t_port) == 8);
+_Static_assert(sizeof(struct sadb_x_nat_t_port) == 8, "struct size mismatch");
 
 /* ESP fragmentation size. */
 struct sadb_x_nat_t_frag {
@@ -285,7 +283,7 @@ struct sadb_x_nat_t_frag {
   u_int16_t sadb_x_nat_t_frag_fraglen;
   u_int16_t sadb_x_nat_t_frag_reserved;
 };
-CTASSERT(sizeof(struct sadb_x_nat_t_frag) == 8);
+_Static_assert(sizeof(struct sadb_x_nat_t_frag) == 8, "struct size mismatch");
 
 
 #define SADB_EXT_RESERVED             0
