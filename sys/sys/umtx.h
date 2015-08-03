1e907b673bd11a639ff8703cf3f5f9719cff26dc
ed <ed@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-08-03 21:11:33 +0000

    Add missing const keyword to function parameter.
    The umtx_key_get() function does not dereference the address off the
    userspace object. The pointer can safely be const.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@286257 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/umtx.h
+++ sys/sys/umtx.h
@@ -153,7 +153,7 @@ umtx_key_match(const struct umtx_key *k1, const struct umtx_key *k2)
 }
 
 int umtx_copyin_timeout(const void *, struct timespec *);
-int umtx_key_get(void *, int, int, struct umtx_key *);
+int umtx_key_get(const void *, int, int, struct umtx_key *);
 void umtx_key_release(struct umtx_key *);
 struct umtx_q *umtxq_alloc(void);
 void umtxq_free(struct umtx_q *);
