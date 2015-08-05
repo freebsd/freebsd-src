7d0d4342e3649b5a1894a77c3b1af102aae854c6
Alexander Motin <mav@FreeBSD.org>
2015-08-05 22:24:49 +0000

    Pass SYNCHRONIZE CACHE command parameters to backends.
    At this point IMMED flag is translated to MNT_NOWAIT flag of VOP_FSYNC(),
    hoping that file system implements that (ZFS seems doesn't).
    
    MFC after:	2 weeks
    Sponsored by:	iXsystems, Inc.

--- sys/cam/ctl/ctl_cmd_table.c
+++ sys/cam/ctl/ctl_cmd_table.c
@@ -788,7 +788,7 @@ const struct ctl_cmd_entry ctl_cmd_table[256] =
 {ctl_sync_cache, CTL_SERIDX_SYNC, CTL_CMD_FLAG_OK_ON_SLUN |
 				  CTL_FLAG_DATA_NONE,
  CTL_LUN_PAT_NONE,
- 10, {0, 0xff, 0xff, 0xff, 0xff, 0, 0xff, 0xff, 0x07}},
+ 10, {0x02, 0xff, 0xff, 0xff, 0xff, 0, 0xff, 0xff, 0x07}},
 
 /* 36 LOCK UNLOCK CACHE(10) */
 {NULL, CTL_SERIDX_INVLD, CTL_CMD_FLAG_NONE, CTL_LUN_PAT_NONE},
@@ -1141,7 +1141,7 @@ const struct ctl_cmd_entry ctl_cmd_table[256] =
 {ctl_sync_cache, CTL_SERIDX_SYNC, CTL_CMD_FLAG_OK_ON_SLUN |
 				  CTL_FLAG_DATA_NONE,
  CTL_LUN_PAT_NONE,
- 16, {0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+ 16, {0x02, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
       0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0, 0x07}},
 
 /* 92 LOCK UNLOCK CACHE(16) */
