freebsd/freebsd-src

commit 28234c0d4e5c8fd0b60e743da79422e190d5bd24

xargs now takes -P0, creating as many concurrent processes as possible
PR:		199976
Submitted by:	Nikolai Lifanov <lifanov@mail.lifanov.com>
Reviewed by:	mjg, bjk
Approved by:	bapt (mentor)
MFC after:	1 month
Relnotes:	yes
Sponsored by:	ScaleEngine Inc.
Differential Revision:	https://reviews.freebsd.org/D2616


git-svn-id: svn+ssh://svn.freebsd.org/base/head@286289 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

usr.bin/xargs/xargs.c
@@ -46,9 +46,11 @@ static char sccsid[] = "@(#)xargs.c	8.1 (Berkeley) 6/6/93";
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include <sys/param.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-
+#include <sys/time.h>
+#include <sys/limits.h>
+#include <sys/resource.h>
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -100,6 +102,7 @@ main(int argc, char *argv[])
 	long arg_max;
 	int ch, Jflag, nargs, nflag, nline;
 	size_t linelen;
+	struct rlimit rl;
 	char *endptr;
 	const char *errstr;
 
@@ -166,6 +169,14 @@ main(int argc, char *argv[])
 			maxprocs = strtonum(optarg, 1, INT_MAX, &errstr);
 			if (errstr)
 				errx(1, "-P %s: %s", optarg, errstr);
+			if (getrlimit(RLIMIT_NPROC, &rl) != 0)
+				errx(1, "getrlimit failed");
+			if (*endptr != '\0')
+				errx(1, "invalid number for -P option");
+			if (maxprocs < 0)
+				errx(1, "value for -P option should be >= 0");
+			if (maxprocs == 0 || maxprocs > rl.rlim_cur)
+				maxprocs = rl.rlim_cur;
 			break;
 		case 'p':
 			pflag = 1;

usr.bin/xargs/xargs.1
@@ -33,7 +33,7 @@
 .\" $FreeBSD$
 .\" $xMach: xargs.1,v 1.2 2002/02/23 05:23:37 tim Exp $
 .\"
-.Dd March 16, 2012
+.Dd August 4, 2015
 .Dt XARGS 1
 .Os
 .Sh NAME
@@ -207,6 +207,11 @@ Parallel mode: run at most
 invocations of
 .Ar utility
 at once.
+If
+.Ar maxprocs
+is set to 0,
+.Nm
+will run as many processes as possible.
 .It Fl p
 Echo each command to be executed and ask the user whether it should be
 executed.

commit 1ff25ef12190c7d73d0bd54d0a32b06d1bd30ed4

fix regression in xargs -Px (introduced in r286289) and add regression tests
PR:		202152
Submitted by:	jbeich (original),  Nikolai Lifanov (final)
Reviewed by:	jbeich
Approved by:	bapt (mentor)
Differential Revision:	https://reviews.freebsd.org/D3330


git-svn-id: svn+ssh://svn.freebsd.org/base/head@286461 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

usr.bin/xargs/xargs.c
@@ -166,15 +166,11 @@ main(int argc, char *argv[])
 			oflag = 1;
 			break;
 		case 'P':
-			maxprocs = strtonum(optarg, 1, INT_MAX, &errstr);
+			maxprocs = strtonum(optarg, 0, INT_MAX, &errstr);
 			if (errstr)
 				errx(1, "-P %s: %s", optarg, errstr);
 			if (getrlimit(RLIMIT_NPROC, &rl) != 0)
 				errx(1, "getrlimit failed");
-			if (*endptr != '\0')
-				errx(1, "invalid number for -P option");
-			if (maxprocs < 0)
-				errx(1, "value for -P option should be >= 0");
 			if (maxprocs == 0 || maxprocs > rl.rlim_cur)
 				maxprocs = rl.rlim_cur;
 			break;

usr.bin/xargs/tests/Makefile
@@ -10,13 +10,16 @@ FILES+=		regress.0.out
 FILES+=		regress.0I.out
 FILES+=		regress.0J.out
 FILES+=		regress.0L.out
+FILES+=		regress.0P1.out
 FILES+=		regress.I.out
 FILES+=		regress.J.out
 FILES+=		regress.L.out
+FILES+=		regress.P1.out
 FILES+=		regress.R.out
 FILES+=		regress.in
 FILES+=		regress.n1.out
 FILES+=		regress.n2.out
+FILES+=		regress.n2P0.out
 FILES+=		regress.n3.out
 FILES+=		regress.normal.out
 FILES+=		regress.quotes.in

usr.bin/xargs/tests/regress.0P1.out
@@ -1,0 +1,4 @@
+quick ' brown fox jumped
+over	"the lazy dog
+quick brown fox jumped over the lazy dog
+

usr.bin/xargs/tests/regress.P1.out
@@ -1,0 +1,1 @@
+quick brown fox jumped over the lazy dog

usr.bin/xargs/tests/regress.n2P0.out
@@ -1,0 +1,4 @@
+quick brown
+fox jumped
+over the
+lazy dog

usr.bin/xargs/tests/regress.sh
@@ -1,6 +1,6 @@
 # $FreeBSD$
 
-echo 1..13
+echo 1..16
 
 REGRESSION_START($1)
 
@@ -8,14 +8,17 @@ REGRESSION_TEST(`normal', `xargs echo The <${SRCDIR}/regress.in')
 REGRESSION_TEST(`I', `xargs -I% echo The % % % %% % % <${SRCDIR}/regress.in')
 REGRESSION_TEST(`J', `xargs -J% echo The % again. <${SRCDIR}/regress.in')
 REGRESSION_TEST(`L', `xargs -L3 echo <${SRCDIR}/regress.in')
+REGRESSION_TEST(`P1', `xargs -P1 echo <${SRCDIR}/regress.in')
 REGRESSION_TEST(`R', `xargs -I% -R1 echo The % % % %% % % <${SRCDIR}/regress.in')
 REGRESSION_TEST(`n1', `xargs -n1 echo <${SRCDIR}/regress.in')
 REGRESSION_TEST(`n2', `xargs -n2 echo <${SRCDIR}/regress.in')
+REGRESSION_TEST(`n2P0',`xargs -n2 -P0 echo <${SRCDIR}/regress.in')
 REGRESSION_TEST(`n3', `xargs -n3 echo <${SRCDIR}/regress.in')
 REGRESSION_TEST(`0', `xargs -0 -n1 echo <${SRCDIR}/regress.0.in')
 REGRESSION_TEST(`0I', `xargs -0 -I% echo The % %% % <${SRCDIR}/regress.0.in')
 REGRESSION_TEST(`0J', `xargs -0 -J% echo The % again. <${SRCDIR}/regress.0.in')
 REGRESSION_TEST(`0L', `xargs -0 -L2 echo <${SRCDIR}/regress.0.in')
+REGRESSION_TEST(`0P1', `xargs -0 -P1 echo <${SRCDIR}/regress.0.in')
 REGRESSION_TEST(`quotes', `xargs -n1 echo <${SRCDIR}/regress.quotes.in')
 
 REGRESSION_END()