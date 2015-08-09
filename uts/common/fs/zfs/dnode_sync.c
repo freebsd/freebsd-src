0dff95d155ce4670f6db75f3eb3356f286c41f79
mav <mav@FreeBSD.org>
2015-08-09 19:35:08 +0000

    5630 stale bonus buffer in recycled dnode_t leads to data corruption
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: George Wilson <george@delphix.com>
    Reviewed by: Will Andrews <will@freebsd.org>
    Approved by: Robert Mustacchi <rm@joyent.com>
    Author: Justin T. Gibbs <justing@spectralogic.com>

--- uts/common/fs/zfs/dnode_sync.c
+++ uts/common/fs/zfs/dnode_sync.c
@@ -430,6 +430,12 @@ dnode_evict_dbufs(dnode_t *dn)
 	}
 	mutex_exit(&dn->dn_dbufs_mtx);
 
+	dnode_evict_bonus(dn);
+}
+
+void
+dnode_evict_bonus(dnode_t *dn)
+{
 	rw_enter(&dn->dn_struct_rwlock, RW_WRITER);
 	if (dn->dn_bonus && refcount_is_zero(&dn->dn_bonus->db_holds)) {
 		mutex_enter(&dn->dn_bonus->db_mtx);
