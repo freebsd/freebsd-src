7dcd82dac44ba750e9f586f41e16d0e4aae47e2a
hselasky <hselasky@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-08-14 12:57:53 +0000

    Improve the realtime properties of USB transfers for embedded systems like RPI-B and RPI-2.
    Description of problem:
    USB transfers can process data in their callbacks sometimes causing
    unacceptable latency for other USB transfers. Separate BULK completion
    callbacks from CONTROL, INTERRUPT and ISOCHRONOUS callbacks, and give
    BULK completion callbacks lesser execution priority than the
    others. This way USB audio won't be interfered by heavy USB ethernet
    usage for example.
    
    Further serve USB transfer completion in a round robin fashion,
    instead of only serving the most CPU hungry. This has been done by
    adding a third flag to USB transfer queue structure which keeps track
    of looping callbacks. The "command" callback function then decides
    what to do when looping.
    
    MFC after:		2 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@286773 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/usb/controller/usb_controller.c
+++ sys/dev/usb/controller/usb_controller.c
@@ -231,7 +231,8 @@ usb_detach(device_t dev)
 	/* Get rid of USB callback processes */
 
 	usb_proc_free(USB_BUS_GIANT_PROC(bus));
-	usb_proc_free(USB_BUS_NON_GIANT_PROC(bus));
+	usb_proc_free(USB_BUS_NON_GIANT_ISOC_PROC(bus));
+	usb_proc_free(USB_BUS_NON_GIANT_BULK_PROC(bus));
 
 	/* Get rid of USB explore process */
 
@@ -395,7 +396,8 @@ usb_bus_explore(struct usb_proc_msg *pm)
 		 */
 		usb_proc_rewakeup(USB_BUS_CONTROL_XFER_PROC(bus));
 		usb_proc_rewakeup(USB_BUS_GIANT_PROC(bus));
-		usb_proc_rewakeup(USB_BUS_NON_GIANT_PROC(bus));
+		usb_proc_rewakeup(USB_BUS_NON_GIANT_ISOC_PROC(bus));
+		usb_proc_rewakeup(USB_BUS_NON_GIANT_BULK_PROC(bus));
 #endif
 
 		USB_BUS_UNLOCK(bus);
@@ -860,9 +862,13 @@ usb_attach_sub(device_t dev, struct usb_bus *bus)
 	    &bus->bus_mtx, device_get_nameunit(dev), USB_PRI_MED)) {
 		device_printf(dev, "WARNING: Creation of USB Giant "
 		    "callback process failed.\n");
-	} else if (usb_proc_create(USB_BUS_NON_GIANT_PROC(bus),
+	} else if (usb_proc_create(USB_BUS_NON_GIANT_ISOC_PROC(bus),
+	    &bus->bus_mtx, device_get_nameunit(dev), USB_PRI_HIGHEST)) {
+		device_printf(dev, "WARNING: Creation of USB non-Giant ISOC "
+		    "callback process failed.\n");
+	} else if (usb_proc_create(USB_BUS_NON_GIANT_BULK_PROC(bus),
 	    &bus->bus_mtx, device_get_nameunit(dev), USB_PRI_HIGH)) {
-		device_printf(dev, "WARNING: Creation of USB non-Giant "
+		device_printf(dev, "WARNING: Creation of USB non-Giant BULK "
 		    "callback process failed.\n");
 	} else if (usb_proc_create(USB_BUS_EXPLORE_PROC(bus),
 	    &bus->bus_mtx, device_get_nameunit(dev), USB_PRI_MED)) {
