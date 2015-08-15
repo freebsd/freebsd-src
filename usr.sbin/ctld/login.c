69ac9bf2f90b9e49030d2ebb15aab28098b15cf3
mav <mav@FreeBSD.org>
2015-08-15 11:08:30 +0000

    MFC r286456: Remove some code duplication.

--- usr.sbin/ctld/login.c
+++ usr.sbin/ctld/login.c
@@ -59,6 +59,7 @@ login_set_nsg(struct pdu *response, int nsg)
 
 	bhslr->bhslr_flags &= 0xFC;
 	bhslr->bhslr_flags |= nsg;
+	bhslr->bhslr_flags |= BHSLR_FLAGS_TRANSIT;
 }
 
 static int
@@ -339,15 +340,12 @@ login_send_chap_success(struct pdu *request,
 {
 	struct pdu *response;
 	struct keys *request_keys, *response_keys;
-	struct iscsi_bhs_login_response *bhslr2;
 	struct rchap *rchap;
 	const char *chap_i, *chap_c;
 	char *chap_r;
 	int error;
 
 	response = login_new_response(request);
-	bhslr2 = (struct iscsi_bhs_login_response *)response->pdu_bhs;
-	bhslr2->bhslr_flags |= BHSLR_FLAGS_TRANSIT;
 	login_set_nsg(response, BHSLR_STAGE_OPERATIONAL_NEGOTIATION);
 
 	/*
@@ -705,7 +703,6 @@ login_negotiate(struct connection *conn, struct pdu *request)
 
 	response = login_new_response(request);
 	bhslr2 = (struct iscsi_bhs_login_response *)response->pdu_bhs;
-	bhslr2->bhslr_flags |= BHSLR_FLAGS_TRANSIT;
 	bhslr2->bhslr_tsih = htons(0xbadd);
 	login_set_csg(response, BHSLR_STAGE_OPERATIONAL_NEGOTIATION);
 	login_set_nsg(response, BHSLR_STAGE_FULL_FEATURE_PHASE);
@@ -745,7 +742,6 @@ login(struct connection *conn)
 {
 	struct pdu *request, *response;
 	struct iscsi_bhs_login_request *bhslr;
-	struct iscsi_bhs_login_response *bhslr2;
 	struct keys *request_keys, *response_keys;
 	struct auth_group *ag;
 	struct portal_group *pg;
@@ -906,8 +902,6 @@ login(struct connection *conn)
 			    "transitioning anyway");
 
 		response = login_new_response(request);
-		bhslr2 = (struct iscsi_bhs_login_response *)response->pdu_bhs;
-		bhslr2->bhslr_flags |= BHSLR_FLAGS_TRANSIT;
 		login_set_nsg(response, BHSLR_STAGE_OPERATIONAL_NEGOTIATION);
 		response_keys = keys_new();
 		/*
