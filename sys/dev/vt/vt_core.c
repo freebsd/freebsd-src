5e9c108f3de1cd164f4779d9e293dd67107553f1
cem <cem@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-08-21 15:21:56 +0000

    vt_cpulogos: Resize all terms/windows when tearing down logos
    PR:		202288 (partial)
    Tested by:	Jakob Alvermark
    Reviewed by:	ed
    Approved by:	markj (mentor)
    Sponsored by:	EMC / Isilon Storage Division
    Differential Revision:	https://reviews.freebsd.org/D3388
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@286997 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/vt/vt_core.c
+++ sys/dev/vt/vt_core.c
@@ -144,7 +144,6 @@ VT_SYSCTL_INT(splash_cpu_style, 2, "Draw logo style "
     "(0 = Alternate beastie, 1 = Beastie, 2 = Orb)");
 VT_SYSCTL_INT(splash_cpu_duration, 10, "Hide logos after (seconds)");
 
-static struct vt_device	vt_consdev;
 static unsigned int vt_unit = 0;
 static MALLOC_DEFINE(M_VT, "vt", "vt device");
 struct vt_device *main_vd = &vt_consdev;
@@ -187,7 +186,7 @@ SET_DECLARE(vt_drv_set, struct vt_driver);
 
 struct terminal	vt_consterm;
 static struct vt_window	vt_conswindow;
-static struct vt_device	vt_consdev = {
+struct vt_device	vt_consdev = {
 	.vd_driver = NULL,
 	.vd_softc = NULL,
 	.vd_prev_driver = NULL,
