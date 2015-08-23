99f1ebbac69c893dfdb68b41f63ffa38db07a651
bapt <bapt@FreeBSD.org>
2015-08-23 21:42:27 +0000

    MFC: r285133,r285136,r285137,r285156,r285157,r285158,r285256,r285318,r285395, r285396,r285398,r285401,r285403,r285405,r285406,r285408,r285409,r285411, r285412,r285413,r285415,r285418,r285430,r285433,r285434,r285442,r285948, r285984,r285985,r285989,r285996,r285997,r286045,r286047,r286066,r286150, r286151,r286152,r286154,r286155,r286156,r286157,r286173,r286196,r286197, r286198,r286199,r286200,r286201,r286202,r286203,r286204,r286210,r286211, r286217,r286218,r286258,r286259,r286341,r286775,r286982,r286986,r286991, r286993
    Validate most pw inputs.
    Rewrite the way parsing sub arguments is made to simplify code and improve
    maintenability
    Add -y (NIS) to userdel/usermod
    pw userdel -r <rootdir> now deletes directories in the rootdir
    Only parse pw.conf when needed
    Reject usermod and userdel if the user concerned is not on the user database
    supposed to be manipulated

--- usr.sbin/pw/pw.c
+++ usr.sbin/pw/pw.c
@@ -32,14 +32,12 @@ static const char rcsid[] =
 #include <err.h>
 #include <fcntl.h>
 #include <locale.h>
-#include <paths.h>
-#include <stdbool.h>
-#include <sys/wait.h>
+#include <string.h>
+#include <sysexits.h>
+#include <unistd.h>
+
 #include "pw.h"
 
-#if !defined(_PATH_YP)
-#define	_PATH_YP	"/var/yp/"
-#endif
 const char     *Modes[] = {
   "add", "del", "mod", "show", "next",
   NULL};
@@ -85,71 +83,53 @@ struct pwf VPWF =
 	vgetgrnam,
 };
 
+static int (*cmdfunc[W_NUM][M_NUM])(int argc, char **argv, char *_name) = {
+	{ /* user */
+		pw_user_add,
+		pw_user_del,
+		pw_user_mod,
+		pw_user_show,
+		pw_user_next,
+		pw_user_lock,
+		pw_user_unlock,
+	},
+	{ /* group */
+		pw_group_add,
+		pw_group_del,
+		pw_group_mod,
+		pw_group_show,
+		pw_group_next,
+	}
+};
+
 struct pwconf conf;
 
-static struct cargs arglist;
-
-static int      getindex(const char *words[], const char *word);
-static void     cmdhelp(int mode, int which);
-
+static int	getindex(const char *words[], const char *word);
+static void	cmdhelp(int mode, int which);
 
 int
 main(int argc, char *argv[])
 {
-	int             ch;
-	int             mode = -1;
-	int             which = -1;
-	long		id = -1;
-	char		*config = NULL;
+	int		mode = -1, which = -1, tmp;
 	struct stat	st;
-	const char	*errstr;
-	char		arg, *name;
+	char		arg, *arg1;
 	bool		relocated, nis;
 
-	static const char *opts[W_NUM][M_NUM] =
-	{
-		{ /* user */
-			"R:V:C:qn:u:c:d:e:p:g:G:mM:k:s:oL:i:w:h:H:Db:NPy:Y",
-			"R:V:C:qn:u:rY",
-			"R:V:C:qn:u:c:d:e:p:g:G:mM:l:k:s:w:L:h:H:FNPY",
-			"R:V:C:qn:u:FPa7",
-			"R:V:C:q",
-			"R:V:C:q",
-			"R:V:C:q"
-		},
-		{ /* grp  */
-			"R:V:C:qn:g:h:H:M:opNPY",
-			"R:V:C:qn:g:Y",
-			"R:V:C:qn:d:g:l:h:H:FM:m:NPY",
-			"R:V:C:qn:g:FPa",
-			"R:V:C:q"
-		 }
-	};
-
-	static int      (*funcs[W_NUM]) (int _mode, char *_name, long _id,
-	    struct cargs * _args) =
-	{			/* Request handlers */
-		pw_user,
-		pw_group
-	};
-
-	name = NULL;
+	arg1 = NULL;
 	relocated = nis = false;
 	memset(&conf, 0, sizeof(conf));
+	strlcpy(conf.rootdir, "/", sizeof(conf.rootdir));
 	strlcpy(conf.etcpath, _PATH_PWD, sizeof(conf.etcpath));
 	conf.checkduplicate = true;
+	conf.fd = -1;
 
-	LIST_INIT(&arglist);
-
-	(void)setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "");
 
 	/*
 	 * Break off the first couple of words to determine what exactly
 	 * we're being asked to do
 	 */
 	while (argc > 1) {
-		int             tmp;
-
 		if (*argv[1] == '-') {
 			/*
 			 * Special case, allow pw -V<dir> <operation> [args] for scripts etc.
@@ -195,15 +175,9 @@ main(int argc, char *argv[])
 			mode = tmp % M_NUM;
 		} else if (strcmp(argv[1], "help") == 0 && argv[2] == NULL)
 			cmdhelp(mode, which);
-		else if (which != -1 && mode != -1) {
-			if (strspn(argv[1], "0123456789") == strlen(argv[1])) {
-				id = strtonum(argv[1], 0, LONG_MAX, &errstr);
-				if (errstr != NULL)
-					errx(EX_USAGE, "Bad id '%s': %s",
-					    argv[1], errstr);
-			} else
-				name = argv[1];
-		} else
+		else if (which != -1 && mode != -1)
+				arg1 = argv[1];
+		else
 			errx(EX_USAGE, "unknown keyword `%s'", argv[1]);
 		++argv;
 		--argc;
@@ -215,155 +189,25 @@ main(int argc, char *argv[])
 	if (mode == -1 || which == -1)
 		cmdhelp(mode, which);
 
-	/*
-	 * We know which mode we're in and what we're about to do, so now
-	 * let's dispatch the remaining command line args in a genric way.
-	 */
-	optarg = NULL;
+	conf.rootfd = open(conf.rootdir, O_DIRECTORY|O_CLOEXEC);
+	if (conf.rootfd == -1)
+		errx(EXIT_FAILURE, "Unable to open '%s'", conf.rootdir);
 
-	while ((ch = getopt(argc, argv, opts[which][mode])) != -1) {
-		switch (ch) {
-		case '?':
-			errx(EX_USAGE, "unknown switch");
-			break;
-		case '7':
-			conf.v7 = true;
-			break;
-		case 'C':
-			conf.config = optarg;
-			config = conf.config;
-			break;
-		case 'N':
-			conf.dryrun = true;
-			break;
-		case 'l':
-			if (strlen(optarg) >= MAXLOGNAME)
-				errx(EX_USAGE, "new name too long: %s", optarg);
-			conf.newname = optarg;
-			break;
-		case 'P':
-			conf.pretty = true;
-			break;
-		case 'Y':
-			nis = true;
-			break;
-		case 'g':
-			if (which == 0) { /* for user* */
-				addarg(&arglist, 'g', optarg);
-				break;
-			}
-			if (strspn(optarg, "0123456789") != strlen(optarg))
-				errx(EX_USAGE, "-g expects a number");
-			id = strtonum(optarg, 0, LONG_MAX, &errstr);
-			if (errstr != NULL)
-				errx(EX_USAGE, "Bad id '%s': %s", optarg,
-				    errstr);
-			break;
-		case 'u':
-			if (strspn(optarg, "0123456789,") != strlen(optarg))
-				errx(EX_USAGE, "-u expects a number");
-			if (strchr(optarg, ',') != NULL) {
-				addarg(&arglist, 'u', optarg);
-				break;
-			}
-			id = strtonum(optarg, 0, LONG_MAX, &errstr);
-			if (errstr != NULL)
-				errx(EX_USAGE, "Bad id '%s': %s", optarg,
-				    errstr);
-			break;
-		case 'n':
-			if (strspn(optarg, "0123456789") != strlen(optarg)) {
-				name = optarg;
-				break;
-			}
-			id = strtonum(optarg, 0, LONG_MAX, &errstr);
-			if (errstr != NULL)
-				errx(EX_USAGE, "Bad id '%s': %s", optarg,
-				    errstr);
-			break;
-		case 'o':
-			conf.checkduplicate = false;
-			break;
-		default:
-			addarg(&arglist, ch, optarg);
-			break;
-		}
-		optarg = NULL;
-	}
-
-	if (name != NULL && strlen(name) >= MAXLOGNAME)
-		errx(EX_USAGE, "name too long: %s", name);
-
-	/*
-	 * Must be root to attempt an update
-	 */
-	if (geteuid() != 0 && mode != M_PRINT && mode != M_NEXT && !conf.dryrun)
-		errx(EX_NOPERM, "you must be root to run this program");
-
-	/*
-	 * We should immediately look for the -q 'quiet' switch so that we
-	 * don't bother with extraneous errors
-	 */
-	if (getarg(&arglist, 'q') != NULL)
-		freopen(_PATH_DEVNULL, "w", stderr);
-
-	/*
-	 * Set our base working path if not overridden
-	 */
-
-	if (config == NULL) {	/* Only override config location if -C not specified */
-		asprintf(&config, "%s/pw.conf", conf.etcpath);
-		if (config == NULL)
-			errx(EX_OSERR, "out of memory");
-	}
-
-	/*
-	 * Now, let's do the common initialisation
-	 */
-	conf.userconf = read_userconfig(config);
-
-	ch = funcs[which] (mode, name, id, &arglist);
-
-	/*
-	 * If everything went ok, and we've been asked to update
-	 * the NIS maps, then do it now
-	 */
-	if (ch == EXIT_SUCCESS && nis) {
-		pid_t	pid;
-
-		fflush(NULL);
-		if (chdir(_PATH_YP) == -1)
-			warn("chdir(" _PATH_YP ")");
-		else if ((pid = fork()) == -1)
-			warn("fork()");
-		else if (pid == 0) {
-			/* Is make anywhere else? */
-			execlp("/usr/bin/make", "make", (char *)NULL);
-			_exit(1);
-		} else {
-			int   i;
-			waitpid(pid, &i, 0);
-			if ((i = WEXITSTATUS(i)) != 0)
-				errx(ch, "make exited with status %d", i);
-			else
-				pw_log(conf.userconf, mode, which, "NIS maps updated");
-		}
-	}
-	return ch;
+	return (cmdfunc[which][mode](argc, argv, arg1));
 }
 
 
 static int
 getindex(const char *words[], const char *word)
 {
-	int             i = 0;
+	int	i = 0;
 
 	while (words[i]) {
 		if (strcmp(words[i], word) == 0)
-			return i;
+			return (i);
 		i++;
 	}
-	return -1;
+	return (-1);
 }
 
 
@@ -413,7 +257,7 @@ cmdhelp(int mode, int which)
 				"  Setting defaults:\n"
 				"\t-V etcdir      alternate /etc location\n"
 				"\t-R rootir      alternate root directory\n"
-			        "\t-D             set user defaults\n"
+				"\t-D             set user defaults\n"
 				"\t-b dir         default home root dir\n"
 				"\t-e period      default expiry period\n"
 				"\t-p period      default password change period\n"
@@ -433,6 +277,7 @@ cmdhelp(int mode, int which)
 				"\t-n name        login name\n"
 				"\t-u uid         user id\n"
 				"\t-Y             update NIS maps\n"
+				"\t-y path        set NIS passwd file path\n"
 				"\t-r             remove home & contents\n",
 				"usage: pw usermod [uid|name] [switches]\n"
 				"\t-V etcdir      alternate /etc location\n"
@@ -457,6 +302,7 @@ cmdhelp(int mode, int which)
 				"\t-h fd          read password on fd\n"
 				"\t-H fd          read encrypted password on fd\n"
 				"\t-Y             update NIS maps\n"
+				"\t-y path        set NIS passwd file path\n"
 				"\t-N             no update\n",
 				"usage: pw usershow [uid|name] [switches]\n"
 				"\t-V etcdir      alternate /etc location\n"
@@ -532,27 +378,4 @@ cmdhelp(int mode, int which)
 		fprintf(stderr, "%s", help[which][mode]);
 	}
 	exit(EXIT_FAILURE);
-}
-
-struct carg    *
-getarg(struct cargs * _args, int ch)
-{
-	struct carg    *c = LIST_FIRST(_args);
-
-	while (c != NULL && c->ch != ch)
-		c = LIST_NEXT(c, list);
-	return c;
-}
-
-struct carg    *
-addarg(struct cargs * _args, int ch, char *argstr)
-{
-	struct carg    *ca = malloc(sizeof(struct carg));
-
-	if (ca == NULL)
-		errx(EX_OSERR, "out of memory");
-	ca->ch = ch;
-	ca->val = argstr;
-	LIST_INSERT_HEAD(_args, ca, list);
-	return ca;
 }
--- usr.sbin/pw/tests/pw_userdel.sh
+++ usr.sbin/pw/tests/pw_userdel.sh
@@ -27,11 +27,41 @@ user_do_not_try_to_delete_root_if_user_unknown_head() {
 }
 user_do_not_try_to_delete_root_if_user_unknown_body() {
 	populate_etc_skel
-	atf_check -e inline:"pw: -u expects a number\n" -s exit:64 -x \
+	atf_check -e inline:"pw: Bad id 'plop': invalid\n" -s exit:64 -x \
 		${PW} userdel -u plop
+}
+
+atf_test_case delete_files
+delete_files_body() {
+	populate_root_etc_skel
+
+	mkdir -p ${HOME}/skel
+	touch ${HOME}/skel/a
+	mkdir -p ${HOME}/home
+	mkdir -p ${HOME}/var/mail
+	echo "foo wedontcare" > ${HOME}/etc/opiekeys
+	atf_check -s exit:0 ${RPW} useradd foo -k /skel -m
+	test -d ${HOME}/home || atf_fail "Fail to create home directory"
+	test -f ${HOME}/var/mail/foo || atf_fail "Mail file not created"
+	atf_check -s exit:0 ${RPW} userdel foo -r
+	atf_check -s exit:0 -o inline:"#oo wedontcare\n" cat ${HOME}/etc/opiekeys
+	if test -f ${HOME}/var/mail/foo; then
+		atf_fail "Mail file not removed"
+	fi
+}
+
+atf_test_case delete_numeric_name
+delete_numeric_name_body() {
+	populate_etc_skel
+
+	atf_check ${PW} useradd -n foo -u 4001
+	atf_check -e inline:"pw: no such user \`4001'\n" -s exit:67 \
+		${PW} userdel -n 4001
 }
 
 atf_init_test_cases() {
 	atf_add_test_case rmuser_seperate_group
 	atf_add_test_case user_do_not_try_to_delete_root_if_user_unknown
+	atf_add_test_case delete_files
+	atf_add_test_case delete_numeric_name
 }
