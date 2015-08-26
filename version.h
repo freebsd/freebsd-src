b1d8fa4383e208e9d18a28ad786f6b08fa152c46
des <des@FreeBSD.org>
2015-08-26 09:27:05 +0000

    Vendor import of OpenSSH 7.1p1.

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
-/* $OpenBSD: version.h,v 1.74 2015/08/02 09:56:42 djm Exp $ */
+/* $OpenBSD: version.h,v 1.75 2015/08/21 03:45:26 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_7.0"
+#define SSH_VERSION	"OpenSSH_7.1"
 
 #define SSH_PORTABLE	"p1"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
