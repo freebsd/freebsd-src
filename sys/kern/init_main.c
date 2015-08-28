48cf3d6ef2d0f964e904b6705129045a84a4df9f
imp <imp@FreeBSD.org>
2015-08-28 19:53:19 +0000

    Per overwhelming sentiment in the code review, use FEATURE instead.
    Differential Revision: https://reviews.freebsd.org/D3488
    MFC After: 2 days

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -119,12 +119,8 @@ SYSCTL_INT(_debug, OID_AUTO, bootverbose, CTLFLAG_RW, &bootverbose, 0,
 
 /* Want to avoid defining INVARIANTS if not already defined */
 #ifdef INVARIANTS
-static int	invariants = 1;
-#else
-static int	invariants = 0;
+FEATURE(invariants, "Kernel compiled with INVARIANTS, may affect performance");
 #endif
-SYSCTL_INT(_debug, OID_AUTO, invariants, CTLFLAG_RD, &invariants, 0,
-	"Kernel compiled with invariants");
 
 /*
  * This ensures that there is at least one entry so that the sysinit_set
