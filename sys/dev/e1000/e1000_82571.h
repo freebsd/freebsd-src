e9e7f9c5f9700dafdbd99a97f5c77f1c60a74ea6
sbruno <sbruno@FreeBSD.org>
2015-09-04 16:30:48 +0000

    e1000: Shared code updates -    Fix compiler warning in 80003es2lan.c -    Add return value handler for e1000_*_kmrn_reg_80003es2lan -    Fix usage of DEBUGOUT -    Remove unnecessary variable initializations. -    Removed unused variables (complaints from gcc). -    Edit defines in 82571.h. -    Add workaround for igb hw errata. -    Shared code changes for Skylake/I219 support. -    Remove unused OBFF and LTR functions.
    Differential Revision:	https://reviews.freebsd.org/D3162
    Submitted by:	erj
    MFC after:	1 month
    Sponsored by:	Intel Corporation

--- sys/dev/e1000/e1000_82571.h
+++ sys/dev/e1000/e1000_82571.h
@@ -50,9 +50,10 @@
 #define E1000_EIAC_82574	0x000DC /* Ext. Interrupt Auto Clear - RW */
 #define E1000_EIAC_MASK_82574	0x01F00000
 
-#define E1000_NVM_INIT_CTRL2_MNGM 0x6000 /* Manageability Operation Mode mask */
+#define E1000_IVAR_INT_ALLOC_VALID	0x8
 
-#define E1000_RXCFGL    0x0B634 /* TimeSync Rx EtherType & Msg Type Reg - RW */
+/* Manageability Operation Mode mask */
+#define E1000_NVM_INIT_CTRL2_MNGM	0x6000
 
 #define E1000_BASE1000T_STATUS		10
 #define E1000_IDLE_ERROR_COUNT_MASK	0xFF
