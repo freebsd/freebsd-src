f98a1bafeaeec75fe110190cf357af4a83a9f397
jhb <jhb@FreeBSD.org>
2015-09-09 23:05:52 +0000

    MFC 269656,270024,270443,270993: Fix problems with orphan handling.  Note that to preserve KBI, p_treeflag has been added to the end of struct proc and is explicitly zero'd during fork.
    269656:
    Correct the problems with the ptrace(2) making the debuggee an orphan.
    One problem is inferior(9) looping due to the process tree becoming a
    graph instead of tree if the parent is traced by child. Another issue
    is due to the use of p_oppid to restore the original parent/child
    relationship, because real parent could already exited and its pid
    reused (noted by mjg).
    
    Add the function proc_realparent(9), which calculates the parent for
    given process. It uses the flag P_TREE_FIRST_ORPHAN to detect the head
    element of the p_orphan list and than stepping back to its container
    to find the parent process. If the parent has already exited, the
    init(8) is returned.
    
    Move the P_ORPHAN and the new helper flag from the p_flag* to new
    p_treeflag field of struct proc, which is protected by proctree lock
    instead of proc lock, since the orphans relationship is managed under
    the proctree_lock already.
    
    The remaining uses of p_oppid in ptrace(PT_DETACH) and process
    reapping are replaced by proc_realparent(9).
    
    270024:
    Correct the order of arguments passed to LIST_INSERT_AFTER().
    
    270443:
    Properly reparent traced processes when the tracer dies.
    
    Previously they were uncoditionally reparented to init. In effect
    it was possible that tracee was never returned to original parent.
    
    270993:
    Fix up proc_realparent to always return correct process.
    
    Prior to the change it would always return initproc for non-traced
    processes.

--- sys/kern/kern_fork.c
+++ sys/kern/kern_fork.c
@@ -408,6 +408,7 @@ do_fork(struct thread *td, int flags, struct proc *p2, struct thread *td2,
 
 	bzero(&p2->p_startzero,
 	    __rangeof(struct proc, p_startzero, p_endzero));
+	p2->p_treeflag = 0;
 
 	p2->p_ucred = crhold(td->td_ucred);
 
--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -593,6 +593,7 @@ struct proc {
 	LIST_ENTRY(proc) p_orphan;	/* (e) List of orphan processes. */
 	LIST_HEAD(, proc) p_orphans;	/* (e) Pointer to list of orphans. */
 	u_char		p_throttled;	/* (c) Flag for racct pcpu throttling */
+	u_int		p_treeflag;	/* (e) P_TREE flags */
 };
 
 #define	p_session	p_pgrp->pg_session
@@ -630,7 +631,7 @@ struct proc {
 #define	P_SINGLE_BOUNDARY 0x400000 /* Threads should suspend at user boundary. */
 #define	P_HWPMC		0x800000 /* Process is using HWPMCs */
 #define	P_JAILED	0x1000000 /* Process is in jail. */
-#define	P_ORPHAN	0x2000000 /* Orphaned. */
+#define	P_UNUSED1	0x2000000
 #define	P_INEXEC	0x4000000 /* Process is in execve(). */
 #define	P_STATCHILD	0x8000000 /* Child process stopped or exited. */
 #define	P_INMEM		0x10000000 /* Loaded into memory. */
@@ -644,6 +645,11 @@ struct proc {
 
 /* These flags are kept in p_flag2. */
 #define	P2_INHERIT_PROTECTED 0x00000001 /* New children get P_PROTECTED. */
+
+/* Flags protected by proctree_lock, kept in p_treeflags. */
+#define	P_TREE_ORPHANED		0x00000001	/* Reparented, on orphan list */
+#define	P_TREE_FIRST_ORPHAN	0x00000002	/* First element of orphan
+						   list */
 
 /*
  * These were process status values (p_stat), now they are only used in
@@ -891,6 +897,7 @@ int	proc_getenvv(struct thread *td, struct proc *p, struct sbuf *sb);
 void	procinit(void);
 void	proc_linkup0(struct proc *p, struct thread *td);
 void	proc_linkup(struct proc *p, struct thread *td);
+struct proc *proc_realparent(struct proc *child);
 void	proc_reap(struct thread *td, struct proc *p, int *status, int options);
 void	proc_reparent(struct proc *child, struct proc *newparent);
 struct	pstats *pstats_alloc(void);
