c4bd8d661db49db946301566d905c1bd5da90297
zbb <zbb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-09-16 23:34:51 +0000

    Add domain support to PCI bus allocation
    When the system has more than a single PCI domain, the bus numbers
    are not unique, thus they cannot be used for "pci" device numbering.
    Change bus numbers to -1 (i.e. to-be-determined automatically)
    wherever the code did not care about domains.
    
    Reviewed by:   jhb
    Obtained from: Semihalf
    Sponsored by:  The FreeBSD Foundation
    Differential Revision: https://reviews.freebsd.org/D3406
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@287882 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/mips/atheros/ar724x_pci.c
+++ sys/mips/atheros/ar724x_pci.c
@@ -429,7 +429,7 @@ ar724x_pci_attach(device_t dev)
 	    | PCIM_CMD_SERRESPEN | PCIM_CMD_BACKTOBACK
 	    | PCIM_CMD_PERRESPEN | PCIM_CMD_MWRICEN, 2);
 
-	device_add_child(dev, "pci", busno);
+	device_add_child(dev, "pci", -1);
 	return (bus_generic_attach(dev));
 }
 
