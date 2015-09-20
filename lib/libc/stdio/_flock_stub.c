141020ed049536fdfe0b91470c9ac9216edff14d
rodrigc <rodrigc@FreeBSD.org>
2015-09-20 20:26:46 +0000

    Remove names from some prototypes

--- lib/libc/stdio/_flock_stub.c
+++ lib/libc/stdio/_flock_stub.c
@@ -55,8 +55,8 @@ __weak_reference(_flockfile_debug_stub, _flockfile_debug);
 __weak_reference(_ftrylockfile, ftrylockfile);
 __weak_reference(_funlockfile, funlockfile);
 
-void _flockfile_debug_stub(FILE *fp, char *fname, int lineno);
-int _ftrylockfile(FILE *fp);
+void _flockfile_debug_stub(FILE *, char *, int);
+int _ftrylockfile(FILE *);
 
 void
 _flockfile(FILE *fp)
