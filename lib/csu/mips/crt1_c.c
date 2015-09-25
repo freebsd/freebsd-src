fa347fa8e4a5a2b9a7e414a57fb370e8200939c3
David Chisnall <theraven@FreeBSD.org>
2015-09-25 09:46:32 +0000

    Finish the cheriabi cap_reloc handler.
    Function pointers can now be statically initialised, with the latest
    CHERI LLVM/Clang.

--- lib/csu/mips/crt1_c.c
+++ lib/csu/mips/crt1_c.c
@@ -72,6 +72,14 @@ struct capreloc
 	uint64_t size;
 	uint64_t permissions;
 };
+static const uint64_t function_reloc_flag = 1ULL<<63;
+static const uint64_t function_pointer_permissions =
+	~0 &
+	~__CHERI_CAP_PERMISSION_PERMIT_STORE_CAPABILITY__ &
+	~__CHERI_CAP_PERMISSION_PERMIT_STORE__;
+static const uint64_t global_pointer_permissions =
+	~0 & ~__CHERI_CAP_PERMISSION_PERMIT_EXECUTE__;
+
 __attribute__((weak))
 extern struct capreloc __start___cap_relocs;
 __attribute__((weak))
@@ -81,20 +89,27 @@ static void
 crt_init_globals()
 {
 	void *gdc = __builtin_memcap_global_data_get();
+	void *pcc = __builtin_memcap_program_counter_get();
 
+	gdc = __builtin_memcap_perms_and(gdc, global_pointer_permissions);
+	pcc = __builtin_memcap_perms_and(pcc, function_pointer_permissions);
 	for (struct capreloc *reloc = &__start___cap_relocs ;
 	     reloc < &__stop___cap_relocs ; reloc++)
 	{
+		_Bool isFunction = (reloc->permissions & function_reloc_flag) ==
+			function_reloc_flag;
 		void **dest = __builtin_memcap_offset_set(gdc, reloc->capability_location);
-		void *src = __builtin_memcap_offset_set(gdc, reloc->object);
-		if (reloc->size != 0)
+		void *base = isFunction ? pcc : gdc;
+		void *src = __builtin_memcap_offset_set(base, reloc->object);
+		if (!isFunction && (reloc->size != 0))
 		{
 			src = __builtin_memcap_bounds_set(src, reloc->size);
 		}
 		src = __builtin_memcap_offset_increment(src, reloc->offset);
 		*dest = src;
 	}
-};
+}
+
 #endif
 
 /* The entry function, C part. This performs the bulk of program initialisation
