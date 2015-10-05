ae01179b7c7bc820bea4ed8d5c0ae4becd582d5a
bdrewery <bdrewery@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-10-05 18:08:35 +0000

    Fix tracking of unknown syscalls for 'truss -c'.
    This is done by changing get_syscall() to either lookup the known syscall
    or add it into the list with the default handlers for printing.
    
    This also simplifies some code to not have to check if the syscall variable
    is set or NULL.
    
    Reviewed by:	jhb
    Relnotes:	yes
    Differential Revision:	https://reviews.freebsd.org/D3792
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@288832 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/truss/main.c
+++ usr.bin/truss/main.c
@@ -94,6 +94,7 @@ main(int ac, char **av)
 	trussinfo->strsize = 32;
 	trussinfo->curthread = NULL;
 	LIST_INIT(&trussinfo->proclist);
+	init_syscalls();
 	while ((c = getopt(ac, av, "p:o:facedDs:S")) != -1) {
 		switch (c) {
 		case 'p':	/* specified pid */
