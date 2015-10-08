9cc92b15a00d757e17254bfa7caa9942ed715a6f
Colin Percival <cperciva@FreeBSD.org>
2015-10-08 15:48:44 +0000

    MFC r288446: Disable suspend during shutdown.

--- sbin/init/init.c
+++ sbin/init/init.c
@@ -1499,6 +1499,15 @@ static state_func_t
 death(void)
 {
 	session_t *sp;
+	int block, blocked;
+	size_t len;
+
+	/* Temporarily block suspend. */
+	len = sizeof(blocked);
+	block = 1;
+	if (sysctlbyname("kern.suspend_blocked", &blocked, &len,
+	    &block, sizeof(block)) == -1)
+		blocked = 0;
 
 	/*
 	 * Also revoke the TTY here.  Because runshutdown() may reopen
@@ -1514,6 +1523,11 @@ death(void)
 
 	/* Try to run the rc.shutdown script within a period of time */
 	runshutdown();
+
+	/* Unblock suspend if we blocked it. */
+	if (!blocked)
+		sysctlbyname("kern.suspend_blocked", NULL, NULL,
+		    &blocked, sizeof(blocked));
 
 	return (state_func_t) death_single;
 }
