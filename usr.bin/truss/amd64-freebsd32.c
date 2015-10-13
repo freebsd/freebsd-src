06fc64f88a160ebb32b72484b72606b9b7102dca
bdrewery <bdrewery@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-10-13 18:23:51 +0000

    Simplify syscall generation and ABI source file handling for the build.
    This is to make the Makefile more easily extendable for new ABIs.
    
    This also makes several other subtle changes:
      -  The build now is given a list of ABIs to use based on the MACHINE_ARCH or
         MACHINE_CPUARCH.  These ABIs have a related path in sys/ that is used
         to generate their syscalls.  For each ABI to build check for a
         ABI.c, MACHINE_ARCH-ABI.c, or a MACHINE_CPUARCH-ABI.c.  This matches
         the old behavior needed for archs such as powerpc* and mips*.
      -  The ABI source file selection allows for simpler assignment of common
         ABIs such as "fbsd32" from sys/compat/freebsd32, or cloudabi64.
      - Expand 'fbsd' to 'freebsd' everywhere for consistency.
      -  Split out the powerpc-fbsd.c file into a powerpc64-freebsd32.c to be more
         like the amd64-freebsd32.c file and to more easily allow the auto-generation
         of ABI handling to work.
      -  Rename 'syscalls.h' to 'fbsd_syscalls.h' to lessen the ambiguity and
         avoid confusion with syscall.h (such as in r288997).
      -  For non-native syscall header files, they are now renamed to be
         ABI_syscalls.h, where ABI is what ABI the Makefile is building.
      -  Remove all of the makesyscalls config files.  The "native" one being
         name i386.conf was a long outstanding bug.  They were all the same
         except for the data they generated, so now it is just auto-generated
         as a build artifact.
      -  The syscalls array is now fixed to be static in the syscalls header to
         remove the compiler warning about non-extern.  This was worked around
         in the aarch64-fbsd.c file but not the others.
      -  All syscall table names are now just 'syscallnames' since they don't
         need to be different as they are all static in their own ABI files.  The
         alternative is to name them ABI_syscallnames which does not seem
         necessary.
    
    Reviewed by:	ed, jhb
    MFC after:	3 weeks
    Differential Revision:	https://reviews.freebsd.org/D3851
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@289239 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/truss/amd64-freebsd32.c
+++ usr.bin/truss/amd64-freebsd32.c
@@ -1,7 +1,5 @@
 /*
- * Copyright 2006 Peter Grehan <grehan@freebsd.org>
- * Copyright 2005 Orlando Bassotto <orlando@break.net>
- * Copyright 1998 Sean Eric Fagan
+ * Copyright 1997 Sean Eric Fagan
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -11,6 +9,12 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Sean Eric Fagan
+ * 4. Neither the name of the author may be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -28,28 +32,31 @@
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-/* FreeBSD/powerpc64-specific system call handling. */
+/* FreeBSD/amd64-freebsd32-specific system call handling. */
 
 #include <sys/ptrace.h>
 #include <sys/syscall.h>
 
 #include <machine/reg.h>
-#include <machine/frame.h>
+#include <machine/psl.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "truss.h"
 
-#include "syscalls.h"
+#include "freebsd32_syscalls.h"
 
 static int
-powerpc64_fetch_args(struct trussinfo *trussinfo, u_int narg)
+amd64_freebsd32_fetch_args(struct trussinfo *trussinfo, u_int narg)
 {
 	struct ptrace_io_desc iorequest;
 	struct reg regs;
 	struct current_syscall *cs;
+	unsigned int args32[narg];
+	unsigned long parm_offset;
 	lwpid_t tid;
-	u_int i, reg;
+	u_int i;
 
 	tid = trussinfo->curthread->tid;
 	cs = &trussinfo->curthread->cs;
@@ -57,6 +64,7 @@ powerpc64_fetch_args(struct trussinfo *trussinfo, u_int narg)
 		fprintf(trussinfo->outfile, "-- CANNOT READ REGISTERS --\n");
 		return (-1);
 	}
+	parm_offset = regs.r_rsp + sizeof(int);
 
 	/*
 	 * FreeBSD has two special kinds of system call redirections --
@@ -66,31 +74,32 @@ powerpc64_fetch_args(struct trussinfo *trussinfo, u_int narg)
 	 * The system call argument count and code from ptrace() already
 	 * account for these, but we need to skip over the first argument.
 	 */
-	reg = 0;
-	switch (regs.fixreg[0]) {
+	switch (regs.r_rax) {
 	case SYS_syscall:
+		parm_offset += sizeof(int);
+		break;
 	case SYS___syscall:
-		reg += 1;
+		parm_offset += sizeof(quad_t);
 		break;
 	}
 
-	for (i = 0; i < narg && reg < NARGREG; i++, reg++)
-		cs->args[i] = regs.fixreg[FIRSTARG + reg];
-	if (narg > i) {
-		iorequest.piod_op = PIOD_READ_D;
-		iorequest.piod_offs = (void *)(regs.fixreg[1] + 48);
-		iorequest.piod_addr = &cs->args[i];
-		iorequest.piod_len = (narg - i) * sizeof(cs->args[0]);
-		ptrace(PT_IO, tid, (caddr_t)&iorequest, 0);
-		if (iorequest.piod_len == 0)
-			return (-1);
+	iorequest.piod_op = PIOD_READ_D;
+	iorequest.piod_offs = (void *)parm_offset;
+	iorequest.piod_addr = args32;
+	iorequest.piod_len = sizeof(args32);
+	ptrace(PT_IO, tid, (caddr_t)&iorequest, 0);
+	if (iorequest.piod_len == 0) {
+		return (-1);
 	}
 
+	for (i = 0; i < narg; i++)
+		 cs->args[i] = args32[i];
 	return (0);
 }
 
 static int
-powerpc64_fetch_retval(struct trussinfo *trussinfo, long *retval, int *errorp)
+amd64_freebsd32_fetch_retval(struct trussinfo *trussinfo, long *retval,
+    int *errorp)
 {
 	struct reg regs;
 	lwpid_t tid;
@@ -101,18 +110,28 @@ powerpc64_fetch_retval(struct trussinfo *trussinfo, long *retval, int *errorp)
 		return (-1);
 	}
 
-	retval[0] = regs.fixreg[3];
-	retval[1] = regs.fixreg[4];
-	*errorp = !!(regs.cr & 0x10000000);
+	retval[0] = regs.r_rax & 0xffffffff;
+	retval[1] = regs.r_rdx & 0xffffffff;
+	*errorp = !!(regs.r_rflags & PSL_C);
 	return (0);
 }
 
-static struct procabi powerpc64_fbsd = {
-	"FreeBSD ELF64",
+static struct procabi amd64_freebsd32 = {
+	"FreeBSD ELF32",
 	syscallnames,
 	nitems(syscallnames),
-	powerpc64_fetch_args,
-	powerpc64_fetch_retval
+	amd64_freebsd32_fetch_args,
+	amd64_freebsd32_fetch_retval
 };
 
-PROCABI(powerpc64_fbsd);
+PROCABI(amd64_freebsd32);
+
+static struct procabi amd64_freebsd32_aout = {
+	"FreeBSD a.out",
+	syscallnames,
+	nitems(syscallnames),
+	amd64_freebsd32_fetch_args,
+	amd64_freebsd32_fetch_retval
+};
+
+PROCABI(amd64_freebsd32_aout);
