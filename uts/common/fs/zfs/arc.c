ba6cbf85dd98d23d8ed65018ccec5e11ba56a22a
Alexander Motin <mav@FreeBSD.org>
2015-10-14 10:21:21 +0000

    6293 ztest failure: error == 28 (0xc == 0x1c) in ztest_tx_assign()
    Reviewed by: George Wilson <george.wilson@delphix.com>
    Reviewed by: Prakash Surya <prakash.surya@delphix.com>
    Reviewed by: Richard Elling <Richard.Elling@RichardElling.com>
    Approved by: Richard Lowe <richlowe@richlowe.net>
    Author: Matthew Ahrens <mahrens@delphix.com>
    
    illumos/illumos-gate@8fe00bfb8790ad51653f67b01d5ac14256cbb404

--- uts/common/fs/zfs/arc.c
+++ uts/common/fs/zfs/arc.c
@@ -5021,6 +5021,16 @@ arc_init(void)
 	arc_c_max = MAX(allmem * 3 / 4, arc_c_max);
 
 	/*
+	 * In userland, there's only the memory pressure that we artificially
+	 * create (see arc_available_memory()).  Don't let arc_c get too
+	 * small, because it can cause transactions to be larger than
+	 * arc_c, causing arc_tempreserve_space() to fail.
+	 */
+#ifndef _KERNEL
+	arc_c_min = arc_c_max / 2;
+#endif
+
+	/*
 	 * Allow the tunables to override our calculations if they are
 	 * reasonable (ie. over 64MB)
 	 */
