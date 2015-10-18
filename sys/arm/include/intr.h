94a296e945fd916fa8044cad73984af6e4a004b4
ian <ian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-10-18 18:26:19 +0000

    Import ARM_INTRNG, the "next generation" interrupt architecture for arm and armv6 architecures.  The primary enhancement over the old design is support for hierarchical interrupt controllers (such as a gpio driver which can receive interrupts from a root PIC and act as a PIC itself for clients interested in handling a change of gpio pin state as an interrupt).  The new code also provides an infrastructure for mapping interrupts described in metadata in the form of a "controller reference plus interrupt number" tuple into the simple "0-n" flat numeric space understood by rman and the bus resource mechanisms.
    Use of the new code is enabled by setting the ARM_INTRNG option, and by
    making a few simple changes to the platform's support code.  In addition
    each existing PIC driver needs changes to be ready for INTRNG; this commit
    contains the changes for the arm/gic driver, which most armv6 SoCs use, but
    it does not enable the new code yet on any platform.
    
    This project has been many years in the making, starting as a GSoC project
    by Jakub Klama (jceel@) in 2012.  That didn't get committed right away and
    the source base evolved out from under it to some degree.  In 2014 I rebased
    the diffs to then -current and did some enhancements in the area of mapping
    interrupt numbers and storing associated fdt data, then the project went
    cold again for a while.  Eventually Svata Kraus took that work in progress
    and did another big round of work on it, removing most of the remaining
    rough edges.  Finally I took that and made one more pass through it, mostly
    disabling the "INTR_SOLO" feature for now, pending further design
    discussions on how to most efficiently dispatch a pending interrupt through
    more than one layer of PIC.  The current code with the INTR_SOLO feature
    disabled uses approximate 100 extra cpu cycles for each cascaded PIC the
    interrupt has to be passed to, so what's left to do is about efficiency, not
    correct operation.
    
    Differential Revision:	https://reviews.freebsd.org/D2047
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@289529 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/arm/include/intr.h
+++ sys/arm/include/intr.h
@@ -43,6 +43,102 @@
 #include <dev/ofw/openfirm.h>
 #endif
 
+#ifdef ARM_INTRNG
+
+#ifndef NIRQ
+#define	NIRQ		1024	/* XXX - It should be an option. */
+#endif
+
+#ifdef notyet
+#define	INTR_SOLO	INTR_MD1
+typedef int arm_irq_filter_t(void *arg, struct trapframe *tf);
+#else
+typedef int arm_irq_filter_t(void *arg);
+#endif
+
+#define ARM_ISRC_NAMELEN	(MAXCOMLEN + 1)
+
+typedef void arm_ipi_filter_t(void *arg);
+
+enum arm_isrc_type {
+	ARM_ISRCT_NAMESPACE,
+	ARM_ISRCT_FDT
+};
+
+#define ARM_ISRCF_REGISTERED	0x01	/* registered in a controller */
+#define ARM_ISRCF_PERCPU	0x02	/* per CPU interrupt */
+#define ARM_ISRCF_BOUND		0x04	/* bound to a CPU */
+
+/* Interrupt source definition. */
+struct arm_irqsrc {
+	device_t		isrc_dev;	/* where isrc is mapped */
+	intptr_t		isrc_xref;	/* device reference key */
+	uintptr_t		isrc_data;	/* device data for isrc */
+	u_int			isrc_irq;	/* unique identificator */
+	enum arm_isrc_type	isrc_type;	/* how is isrc decribed */
+	u_int			isrc_flags;
+	char			isrc_name[ARM_ISRC_NAMELEN];
+	uint16_t		isrc_nspc_type;
+	uint16_t		isrc_nspc_num;
+	enum intr_trigger	isrc_trig;
+	enum intr_polarity	isrc_pol;
+	cpuset_t		isrc_cpu;	/* on which CPUs is enabled */
+	u_int			isrc_index;
+	u_long *		isrc_count;
+	u_int			isrc_handlers;
+	struct intr_event *	isrc_event;
+	arm_irq_filter_t *	isrc_filter;
+	arm_ipi_filter_t *	isrc_ipifilter;
+	void *			isrc_arg;
+#ifdef FDT
+	u_int			isrc_ncells;
+	pcell_t			isrc_cells[];	/* leave it last */
+#endif
+};
+
+void arm_irq_set_name(struct arm_irqsrc *isrc, const char *fmt, ...)
+    __printflike(2, 3);
+
+void arm_irq_dispatch(struct arm_irqsrc *isrc, struct trapframe *tf);
+
+#define ARM_IRQ_NSPC_NONE	0
+#define ARM_IRQ_NSPC_PLAIN	1
+#define ARM_IRQ_NSPC_IRQ	2
+#define ARM_IRQ_NSPC_IPI	3
+
+u_int arm_namespace_map_irq(device_t dev, uint16_t type, uint16_t num);
+#ifdef FDT
+u_int arm_fdt_map_irq(phandle_t, pcell_t *, u_int);
+#endif
+
+int arm_pic_register(device_t dev, intptr_t xref);
+int arm_pic_unregister(device_t dev, intptr_t xref);
+int arm_pic_claim_root(device_t dev, intptr_t xref, arm_irq_filter_t *filter,
+    void *arg, u_int ipicount);
+
+int arm_irq_add_handler(device_t dev, driver_filter_t, driver_intr_t, void *,
+    u_int, int, void **);
+int arm_irq_remove_handler(device_t dev, u_int, void *);
+int arm_irq_config(u_int, enum intr_trigger, enum intr_polarity);
+int arm_irq_describe(u_int, void *, const char *);
+
+u_int arm_irq_next_cpu(u_int current_cpu, cpuset_t *cpumask);
+
+#ifdef SMP
+int arm_irq_bind(u_int, int);
+
+void arm_ipi_dispatch(struct arm_irqsrc *isrc, struct trapframe *tf);
+
+#define AISHF_NOALLOC	0x0001
+
+int arm_ipi_set_handler(u_int ipi, const char *name, arm_ipi_filter_t *filter,
+    void *arg, u_int flags);
+
+void arm_pic_init_secondary(void);
+#endif
+
+#else /* ARM_INTRNG */
+
 /* XXX move to std.* files? */
 #ifdef CPU_XSCALE_81342
 #define NIRQ		128
@@ -71,7 +167,6 @@
 #define NIRQ		32
 #endif
 
-
 int arm_get_next_irq(int);
 void arm_mask_irq(uintptr_t);
 void arm_unmask_irq(uintptr_t);
@@ -83,8 +178,6 @@ extern void (*arm_post_filter)(void *);
 extern int (*arm_config_irq)(int irq, enum intr_trigger trig,
     enum intr_polarity pol);
 
-void arm_irq_memory_barrier(uintptr_t);
-
 void arm_pic_init_secondary(void);
 int  gic_decode_fdt(uint32_t iparentnode, uint32_t *intrcells, int *interrupt,
     int *trig, int *pol);
@@ -92,5 +185,9 @@ int  gic_decode_fdt(uint32_t iparentnode, uint32_t *intrcells, int *interrupt,
 #ifdef FDT
 int arm_fdt_map_irq(phandle_t, pcell_t *, int);
 #endif
+
+#endif /* ARM_INTRNG */
+
+void arm_irq_memory_barrier(uintptr_t);
 
 #endif	/* _MACHINE_INTR_H */
