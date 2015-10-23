a65d4909fd8a17ed6d30af12e695991dfe7bd593
mav <mav@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-10-23 16:54:24 +0000

    Improve INOTs handling for 24xx and above chips.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@289838 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/isp/isp_library.h
+++ sys/dev/isp/isp_library.h
@@ -171,7 +171,7 @@ int isp_find_pdb_by_wwn(ispsoftc_t *, int, uint64_t, fcportdb_t **);
 int isp_find_pdb_by_handle(ispsoftc_t *, int, uint32_t, fcportdb_t **);
 int isp_find_pdb_by_sid(ispsoftc_t *, int, uint32_t, fcportdb_t **);
 void isp_find_chan_by_did(ispsoftc_t *, uint32_t, uint16_t *);
-void isp_add_wwn_entry(ispsoftc_t *, int, uint64_t, uint16_t, uint32_t, uint16_t);
+void isp_add_wwn_entry(ispsoftc_t *, int, uint64_t, uint64_t, uint16_t, uint32_t, uint16_t);
 void isp_del_wwn_entry(ispsoftc_t *, int, uint64_t, uint16_t, uint32_t);
 void isp_del_all_wwn_entries(ispsoftc_t *, int);
 void isp_del_wwn_entries(ispsoftc_t *, isp_notify_t *);
