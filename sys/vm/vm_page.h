711ef25019edd831b7770cfe8e598d3ce9630ebf
jah <jah@FreeBSD.org>
2015-10-23 12:06:06 +0000

    Fix capitalization

--- sys/vm/vm_page.h
+++ sys/vm/vm_page.h
@@ -376,7 +376,7 @@ extern long first_page;			/* first physical page number */
 #define VM_PAGE_TO_PHYS(entry)	((entry)->phys_addr)
 
 /*
- * PHYS_TO_VM_PAGE() Returns the vm_page_t object that represents a memory
+ * PHYS_TO_VM_PAGE() returns the vm_page_t object that represents a memory
  * page to which the given physical address belongs. The correct vm_page_t
  * object is returned for addresses that are not page-aligned.
  */
