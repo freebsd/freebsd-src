8a78d05ef8273ccec4a3baba2db57dca964731bf
zbb <zbb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-11-02 16:56:34 +0000

    Add support for branch instruction on armv7 with ptrace single step
    Previous code supported only "continuous" code without any kind of
    branch instructions. To change that, new function was implemented
    which parses current instruction and returns an addres where
    the jump might happen (alternative addr).
    mdthread structure was extended to support two breakpoints
    (one directly below current instruction and the second placed
    at the alternative location).
    One of them must trigger regardless the instruction has or has not been
    executed due to condition field.
    Upon cleanup, both software breakpoints are removed.
    
    This implementation parses only the most common instructions
    that are present in the code (like 99.99% of all), but there
    is a chance there are some left, not covered by the parsing routine.
    Parsing is done only for 32-bit instruction, no Thumb nor Thumb-2
    support is provided.
    
    Reviewed by:   kib
    Submitted by:  Wojciech Macek <wma@semihalf.com>
    Obtained from: Semihalf
    Sponsored by:  Juniper Networks Inc.
    Differential Revision: https://reviews.freebsd.org/D4021
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@290273 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/arm/arm/db_interface.c
+++ sys/arm/arm/db_interface.c
@@ -53,6 +53,7 @@ __FBSDID("$FreeBSD$");
 #include <vm/vm_extern.h>
 
 #include <machine/db_machdep.h>
+#include <machine/machdep.h>
 #include <machine/vmparam.h>
 #include <machine/cpu.h>
 
@@ -291,94 +292,35 @@ db_fetch_reg(int reg)
 	}
 }
 
+static u_int
+db_branch_taken_read_int(void *cookie __unused, vm_offset_t offset, u_int *val)
+{
+	u_int ret;
+
+	db_read_bytes(offset, 4, (char *)&ret);
+	*val = ret;
+
+	return (0);
+}
+
+static u_int
+db_branch_taken_fetch_reg(void *cookie __unused, int reg)
+{
+
+	return (db_fetch_reg(reg));
+}
+
 u_int
 branch_taken(u_int insn, db_addr_t pc)
 {
-	u_int addr, nregs, offset = 0;
+	register_t new_pc;
+	int ret;
 
-	switch ((insn >> 24) & 0xf) {
-	case 0x2:	/* add pc, reg1, #value */
-	case 0x0:	/* add pc, reg1, reg2, lsl #offset */
-		addr = db_fetch_reg((insn >> 16) & 0xf);
-		if (((insn >> 16) & 0xf) == 15)
-			addr += 8;
-		if (insn & 0x0200000) {
-			offset = (insn >> 7) & 0x1e;
-			offset = (insn & 0xff) << (32 - offset) |
-			    (insn & 0xff) >> offset;
-		} else {
+	ret = arm_predict_branch(NULL, insn, (register_t)pc, &new_pc,
+	    db_branch_taken_fetch_reg, db_branch_taken_read_int);
 
-			offset = db_fetch_reg(insn & 0x0f);
-			if ((insn & 0x0000ff0) != 0x00000000) {
-				if (insn & 0x10)
-					nregs = db_fetch_reg((insn >> 8) & 0xf);
-				else
-					nregs = (insn >> 7) & 0x1f;
-				switch ((insn >> 5) & 3) {
-				case 0:
-					/* lsl */
-					offset = offset << nregs;
-					break;
-				case 1:
-					/* lsr */
-					offset = offset >> nregs;
-					break;
-				default:
-					break; /* XXX */
-				}
-			}
-			return (addr + offset);
-		}
-	case 0xa:	/* b ... */
-	case 0xb:	/* bl ... */
-		addr = ((insn << 2) & 0x03ffffff);
-		if (addr & 0x02000000)
-			addr |= 0xfc000000;
-		return (pc + 8 + addr);
-	case 0x7:	/* ldr pc, [pc, reg, lsl #2] */
-		addr = db_fetch_reg(insn & 0xf);
-		addr = pc + 8 + (addr << 2);
-		db_read_bytes(addr, 4, (char *)&addr);
-		return (addr);
-	case 0x1:	/* mov pc, reg */
-		addr = db_fetch_reg(insn & 0xf);
-		return (addr);
-	case 0x4:
-	case 0x5:	/* ldr pc, [reg] */
-		addr = db_fetch_reg((insn >> 16) & 0xf);
-		/* ldr pc, [reg, #offset] */
-		if (insn & (1 << 24))
-			offset = insn & 0xfff;
-		if (insn & 0x00800000)
-			addr += offset;
-		else
-			addr -= offset;
-		db_read_bytes(addr, 4, (char *)&addr);
-		return (addr);
-	case 0x8:	/* ldmxx reg, {..., pc} */
-	case 0x9:
-		addr = db_fetch_reg((insn >> 16) & 0xf);
-		nregs = (insn  & 0x5555) + ((insn  >> 1) & 0x5555);
-		nregs = (nregs & 0x3333) + ((nregs >> 2) & 0x3333);
-		nregs = (nregs + (nregs >> 4)) & 0x0f0f;
-		nregs = (nregs + (nregs >> 8)) & 0x001f;
-		switch ((insn >> 23) & 0x3) {
-		case 0x0:	/* ldmda */
-			addr = addr - 0;
-			break;
-		case 0x1:	/* ldmia */
-			addr = addr + 0 + ((nregs - 1) << 2);
-			break;
-		case 0x2:	/* ldmdb */
-			addr = addr - 4;
-			break;
-		case 0x3:	/* ldmib */
-			addr = addr + 4 + ((nregs - 1) << 2);
-			break;
-		}
-		db_read_bytes(addr, 4, (char *)&addr);
-		return (addr);
-	default:
-		panic("branch_taken: botch");
-	}
+	if (ret != 0)
+		kdb_reenter();
+
+	return (new_pc);
 }
