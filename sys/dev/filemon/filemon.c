1243e7645afbe3663fc07e91ac81cafbd07117b8
bdrewery <bdrewery@FreeBSD.org>
2015-11-04 22:49:34 +0000

    Remove unneeded mutex.h include, missed in r287155.
    MFC after:	1 week
    Sponsored by:	EMC / Isilon Storage Division

--- sys/dev/filemon/filemon.c
+++ sys/dev/filemon/filemon.c
@@ -43,7 +43,6 @@ __FBSDID("$FreeBSD$");
 #include <sys/lock.h>
 #include <sys/malloc.h>
 #include <sys/module.h>
-#include <sys/mutex.h>
 #include <sys/poll.h>
 #include <sys/proc.h>
 #include <sys/queue.h>
