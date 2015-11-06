b94650a2bb1d46f36cd3d9715343fd24115df67b
Alexander Motin <mav@FreeBSD.org>
2015-11-06 18:50:01 +0000

    Removed unused malloc types.
    Submitted by:	Dmitry Luhtionov <dmitryluhtionov@gmail.com>
    MFC after:	1 week

--- sys/cam/ata/ata_da.c
+++ sys/cam/ata/ata_da.c
@@ -631,8 +631,6 @@ static struct periph_driver adadriver =
 
 PERIPHDRIVER_DECLARE(ada, adadriver);
 
-static MALLOC_DEFINE(M_ATADA, "ata_da", "ata_da buffers");
-
 static int
 adaopen(struct disk *dp)
 {
