e94a000973e7841c34b05df390bdd60360fc1ada
bdrewery <bdrewery@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-11-06 21:49:17 +0000

    The 'buildenv' target can safely be ran with '+'.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@290467 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/src.init.mk
+++ share/mk/src.init.mk
@@ -5,7 +5,7 @@ __<src.init.mk>__:
 
 .if !target(buildenv)
 buildenv: .PHONY
-	@env BUILDENV_DIR=${.CURDIR} ${MAKE} -C ${SRCTOP} buildenv
+	${_+_}@env BUILDENV_DIR=${.CURDIR} ${MAKE} -C ${SRCTOP} buildenv
 .endif
 
 .endif	# !target(__<src.init.mk>__)
