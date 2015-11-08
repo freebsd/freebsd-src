9da7d79f25288855aeb80127b2f947e74a188a42
Enji Cooper <ngie@FreeBSD.org>
2015-11-08 02:06:17 +0000

    Integrate tools/regression/lib/libc/locale into the FreeBSD test suite as lib/libc/tests/locale
    MFC after: 1 week
    Sponsored by: EMC / Isilon Storage Division

--- lib/libc/tests/locale/mbrlen_test.c
+++ lib/libc/tests/locale/mbrlen_test.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2002-2004 Tim J. Robbins
+ * Copyright (c) 2002 Tim J. Robbins
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,8 +25,8 @@
  */
 
 /*
- * Test program for mblen(), as specified by IEEE Std. 1003.1-2001 and
- * ISO/IEC 9899:1990.
+ * Test program for mbrlen(), as specified by IEEE Std. 1003.1-2001 and
+ * ISO/IEC 9899:1999.
  *
  * The function is tested with both the "C" ("POSIX") LC_CTYPE setting and
  * "ja_JP.eucJP". Other encodings are not tested.
@@ -35,80 +35,93 @@
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include <assert.h>
+#include <errno.h>
 #include <limits.h>
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <wchar.h>
 
-int
-main(int argc, char *argv[])
+#include <atf-c.h>
+
+ATF_TC_WITHOUT_HEAD(mbrlen_test);
+ATF_TC_BODY(mbrlen_test, tc)
 {
+	mbstate_t s;
 	size_t len;
 	char buf[MB_LEN_MAX + 1];
 
-	/*
-	 * C/POSIX locale.
-	 */
+	/* C/POSIX locale. */
+	ATF_REQUIRE(MB_CUR_MAX == 1);
 
-	printf("1..1\n");
-
-	assert(MB_CUR_MAX == 1);
-
-	/* No shift states in C locale. */
-	assert(mblen(NULL, 0) == 0);
+	/* Null wide character, internal state. */
+	memset(buf, 0xcc, sizeof(buf));
+	buf[0] = 0;
+	ATF_REQUIRE(mbrlen(buf, 1, NULL) == 0);
 
 	/* Null wide character. */
-	memset(buf, 0xcc, sizeof(buf));
-	buf[0] = '\0';
-	assert(mblen(buf, 1) == 0);
+	memset(&s, 0, sizeof(s));
+	ATF_REQUIRE(mbrlen(buf, 1, &s) == 0);
+
+	/* Latin letter A, internal state. */
+	ATF_REQUIRE(mbrlen(NULL, 0, NULL) == 0);
+	buf[0] = 'A';
+	ATF_REQUIRE(mbrlen(buf, 1, NULL) == 1);
 
 	/* Latin letter A. */
-	buf[0] = 'A';
-	assert(mblen(buf, 1) == 1);
+	memset(&s, 0, sizeof(s));
+	ATF_REQUIRE(mbrlen(buf, 1, &s) == 1);
 
 	/* Incomplete character sequence. */
-	buf[0] = '\0';
-	assert(mblen(buf, 0) == -1);
-	assert(mblen(NULL, 0) == 0);
+	memset(&s, 0, sizeof(s));
+	ATF_REQUIRE(mbrlen(buf, 0, &s) == (size_t)-2);
 
-	/*
-	 * Japanese (EUC) locale.
-	 */
+	/* Japanese (EUC) locale. */
 
-	assert(strcmp(setlocale(LC_CTYPE, "ja_JP.eucJP"), "ja_JP.eucJP") == 0);
-	assert(MB_CUR_MAX > 1);
+	ATF_REQUIRE(strcmp(setlocale(LC_CTYPE, "ja_JP.eucJP"), "ja_JP.eucJP") == 0);
+	ATF_REQUIRE(MB_CUR_MAX > 1);
 
-	/* No shift states in EUC. */
-	assert(mblen(NULL, 0) == 0);
+	/* Null wide character, internal state. */
+	ATF_REQUIRE(mbrlen(NULL, 0, NULL) == 0);
+	memset(buf, 0xcc, sizeof(buf));
+	buf[0] = 0;
+	ATF_REQUIRE(mbrlen(buf, 1, NULL) == 0);
 
 	/* Null wide character. */
-	memset(buf, 0xcc, sizeof(buf));
-	buf[0] = '\0';
-	assert(mblen(buf, 1) == 0);
+	memset(&s, 0, sizeof(s));
+	ATF_REQUIRE(mbrlen(buf, 1, &s) == 0);
+
+	/* Latin letter A, internal state. */
+	ATF_REQUIRE(mbrlen(NULL, 0, NULL) == 0);
+	buf[0] = 'A';
+	ATF_REQUIRE(mbrlen(buf, 1, NULL) == 1);
 
 	/* Latin letter A. */
-	buf[0] = 'A';
-	assert(mblen(buf, 1) == 1);
+	memset(&s, 0, sizeof(s));
+	ATF_REQUIRE(mbrlen(buf, 1, &s) == 1);
 
-	/* Incomplete character sequence. */
-	buf[0] = '\0';
-	assert(mblen(buf, 0) == -1);
-	assert(mblen(NULL, 0) == 0);
+	/* Incomplete character sequence (zero length). */
+	memset(&s, 0, sizeof(s));
+	ATF_REQUIRE(mbrlen(buf, 0, &s) == (size_t)-2);
 
 	/* Incomplete character sequence (truncated double-byte). */
 	memset(buf, 0xcc, sizeof(buf));
 	buf[0] = 0xa3;
 	buf[1] = 0x00;
-	assert(mblen(buf, 1) == -1);
-	assert(mblen(NULL, 0) == 0);
+	memset(&s, 0, sizeof(s));
+	ATF_REQUIRE(mbrlen(buf, 1, &s) == (size_t)-2);
 
 	/* Same as above, but complete. */
 	buf[1] = 0xc1;
-	assert(mblen(buf, 2) == 2);
+	memset(&s, 0, sizeof(s));
+	ATF_REQUIRE(mbrlen(buf, 2, &s) == 2);
+}
 
-	printf("ok 1 - mblen()\n");
+ATF_TP_ADD_TCS(tp)
+{
 
-	return (0);
+	ATF_TP_ADD_TC(tp, mbrlen_test);
+
+	return (atf_no_error());
 }
