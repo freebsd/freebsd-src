c76744f16d8b08b403ef88f811e2c2d95892fd7b
melifaro <melifaro@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-11-09 10:35:33 +0000

    Decompose arp_ifinit() into arp_add_ifa_lle() and arp_announce_ifaddr(). Rename arp_ifinit2() into arp_announce_ifaddr().
    Eliminate zeroing ifa_rtrequest: it was used for calling arp_rtrequest()
    which was responsible for handling route cloning requests. It became
    obsolete since r186119 (L2/L3 split).
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@290604 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/if_ether.h
+++ sys/netinet/if_ether.h
@@ -119,7 +119,7 @@ int	arpresolve(struct ifnet *ifp, int is_gw, struct mbuf *m,
 void	arprequest(struct ifnet *, const struct in_addr *,
 	    const struct in_addr *, u_char *);
 void	arp_ifinit(struct ifnet *, struct ifaddr *);
-void	arp_ifinit2(struct ifnet *, struct ifaddr *, u_char *);
+void	arp_announce_ifaddr(struct ifnet *, struct in_addr addr, u_char *);
 #endif
 
 #endif
