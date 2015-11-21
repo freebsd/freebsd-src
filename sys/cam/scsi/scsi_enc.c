1ad504c2e11d5d784bf6bae3b6bd824393467f19
mav <mav@FreeBSD.org>
2015-11-21 10:22:01 +0000

    Add API to obtain primary enclosure name and ID for /dev/sesX devices.
    sesX device number may change between reboots, so to properly identify
    the instance we need more data.  Name and ID reported here may mach ones
    reported by SCSI device, but that is not really required by specs.
    
    MFC after:	1 week
    Sponsored by:	iXsystems, Inc.

--- sys/cam/scsi/scsi_enc.c
+++ sys/cam/scsi/scsi_enc.c
@@ -407,6 +407,8 @@ enc_ioctl(struct cdev *dev, u_long cmd, caddr_t arg_addr, int flag,
 	case ENCIOC_GETELMSTAT:
 	case ENCIOC_GETELMDESC:
 	case ENCIOC_GETELMDEVNAMES:
+	case ENCIOC_GETENCNAME:
+	case ENCIOC_GETENCID:
 		break;
 	default:
 		if ((flag & FWRITE) == 0) {
@@ -461,6 +463,8 @@ enc_ioctl(struct cdev *dev, u_long cmd, caddr_t arg_addr, int flag,
 
 	case ENCIOC_GETSTRING:
 	case ENCIOC_SETSTRING:
+	case ENCIOC_GETENCNAME:
+	case ENCIOC_GETENCID:
 		if (enc->enc_vec.handle_string == NULL) {
 			error = EINVAL;
 			break;
