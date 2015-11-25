cfb3e257cd18a318cb4b2cc6b07b8c3dac1ee37c
ngie <ngie@FreeBSD.org>
2015-11-25 01:14:00 +0000

    Add conf.sh for creating a mountpoint, unmounting the mountpoint and cleaning up any temporary md devices

---
+++ sbin/geom/class/tests/uzip/conf.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# $FreeBSD$
+
+class="uzip"
+base=`basename $0`
+mntpoint=$(mktemp -d $TMPDIR/tmp.XXXXXX) || exit
+
+uzip_test_cleanup()
+{
+	umount $mntpoint
+	rmdir $mntpoint
+	geom_test_cleanup
+}
+trap uzip_test_cleanup ABRT EXIT INT TERM
+
+. `dirname $0`/../geom_subr.sh
