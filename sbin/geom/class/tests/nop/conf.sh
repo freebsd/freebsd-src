2a21ca898bc762a485efb3b613009eb2a5f158f8
ngie <ngie@FreeBSD.org>
2015-11-25 03:53:14 +0000

    conf.sh: - Implement gnop_test_cleanup - Use a random name for the shsec
    1_test.sh/2_test.sh:
    - Attach md devices with attach_md
    - Remove unnecessary mdconfig -d -u and gstripe destroy calls at the end
      of the scripts

--- sbin/geom/class/tests/nop/conf.sh
+++ sbin/geom/class/tests/nop/conf.sh
@@ -4,4 +4,11 @@
 class="nop"
 base=`basename $0`
 
+gnop_test_cleanup()
+{
+	[ -c /dev/${us}.nop ] && gnop destroy ${us}.nop
+	geom_test_cleanup
+}
+trap gnop_test_cleanup ABRT EXIT INT TERM
+
 . `dirname $0`/../geom_subr.sh
