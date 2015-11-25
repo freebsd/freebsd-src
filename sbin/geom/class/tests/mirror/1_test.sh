55e96df06b559a7623d59acb4f8d3bea4bf2d6ef
ngie <ngie@FreeBSD.org>
2015-11-25 04:07:31 +0000

    Remove braces around us0, us1, and us2 in gmirror label operation

--- sbin/geom/class/tests/mirror/1_test.sh
+++ sbin/geom/class/tests/mirror/1_test.sh
@@ -9,7 +9,7 @@ us0=$(attach_md -t malloc -s 1M) || exit 1
 us1=$(attach_md -t malloc -s 2M) || exit 1
 us2=$(attach_md -t malloc -s 3M) || exit 1
 
-gmirror label $name /dev/${us0} /dev/${us1} /dev/${us2} || exit 1
+gmirror label $name /dev/$us0 /dev/$us1 /dev/$us2 || exit 1
 devwait
 
 # Size of created device should be 1MB - 512b.
