f6e7322fd71fc885009640e62bb71cffc7fbf7ba
adrian <adrian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-11-28 00:14:37 +0000

    wrap in ATH_DEBUG.
    Thanks sparc64 build!
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@291412 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ath/if_ath_tx_ht.c
+++ sys/dev/ath/if_ath_tx_ht.c
@@ -585,7 +585,7 @@ ath_rateseries_setup(struct ath_softc *sc, struct ieee80211_node *ni,
 	}
 }
 
-#if 1
+#ifdef	ATH_DEBUG
 static void
 ath_rateseries_print(struct ath_softc *sc, HAL_11N_RATE_SERIES *series)
 {
@@ -627,7 +627,7 @@ ath_buf_set_rate(struct ath_softc *sc, struct ieee80211_node *ni,
 
 	ath_rateseries_setup(sc, ni, bf, series);
 
-#if 1
+#ifdef	ATH_DEBUG
 	if (sc->sc_debug & ATH_DEBUG_XMIT)
 		ath_rateseries_print(sc, series);
 #endif
