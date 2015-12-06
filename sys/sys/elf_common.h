64760d92ce5faf4c97b721b6219e2e1f4fc5a559
Ed Maste <emaste@FreeBSD.org>
2015-12-06 21:16:01 +0000

    Add definitions for ELF note types used in executables
    Sponsored by:	The FreeBSD Foundation

--- sys/sys/elf_common.h
+++ sys/sys/elf_common.h
@@ -727,6 +727,11 @@ typedef struct {
 #define	LL_DELAY_LOAD		0x10
 #define	LL_DELTA		0x20
 
+/* Values for n_type used in executables. */
+#define	NT_FREEBSD_ABI_TAG	1
+#define	NT_FREEBSD_NOINIT_TAG	2
+#define	NT_FREEBSD_ARCH_TAG	3
+
 /* Values for n_type.  Used in core files. */
 #define	NT_PRSTATUS	1	/* Process status. */
 #define	NT_FPREGSET	2	/* Floating point registers. */
