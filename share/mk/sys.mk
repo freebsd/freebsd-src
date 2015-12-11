14805954a17a389c3a7ebfbc340d6ea2e964721d
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-12-11 05:39:44 +0000

    Move the inclusion of bsd.cpu.mk from sys.mk to bsd.opts.mk. However, for historical behavior that ports depends on, include it if we're inside the ports tree.
    Differential Review: https://reviews.freebsd.org/D4383
    Ports Exp run: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=205021
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@292084 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/sys.mk
+++ share/mk/sys.mk
@@ -406,6 +406,15 @@ __MAKE_SHELL?=/bin/sh
 	path=${__MAKE_SHELL}
 .endif
 
+# Hack for ports compatibility. Historically, ports makefiles have
+# assumed hey can examine MACHINE_CPU without including anything
+# because this was automatically included in sys.mk. For /usr/src,
+# this file has moved to being included from bsd.opts.mk. Until all
+# the ports files are modernized, and a reasonable transitition
+# period has passed, include it while we're in a ports tree here
+# to preserve historic behavior.
+.if exists(${.CURDIR}/../../Mk/bsd.port.mk)
 .include <bsd.cpu.mk>
+.endif
 
 .endif # ! Posix
