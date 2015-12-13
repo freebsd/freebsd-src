6d59fb25339e6bd8fc6e6321b9bbcc2c90d9faf1
trasz <trasz@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-12-13 11:08:29 +0000

    Avoid useless relocking.
    MFC after:	1 month
    Sponsored by:	The FreeBSD Foundation
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@292160 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/kern_racct.c
+++ sys/kern/kern_racct.c
@@ -631,8 +631,8 @@ racct_add_force(struct proc *p, int resource, uint64_t amount)
 
 	mtx_lock(&racct_lock);
 	racct_adjust_resource(p->p_racct, resource, amount);
+	racct_add_cred_locked(p->p_ucred, resource, amount);
 	mtx_unlock(&racct_lock);
-	racct_add_cred(p->p_ucred, resource, amount);
 }
 
 static int
