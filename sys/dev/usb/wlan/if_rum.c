85f57550a1c0c6068b3edb38021f901fec6769b2
avos <avos@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-12-13 22:08:27 +0000

    wpi, rum and urtwn: update copyright headers
    Approved by:	adrian (mentor)
    Differential Revision:	https://reviews.freebsd.org/D4489
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@292176 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/usb/wlan/if_rum.c
+++ sys/dev/usb/wlan/if_rum.c
@@ -4,6 +4,7 @@
  * Copyright (c) 2005-2007 Damien Bergamini <damien.bergamini@free.fr>
  * Copyright (c) 2006 Niall O'Higgins <niallo@openbsd.org>
  * Copyright (c) 2007-2008 Hans Petter Selasky <hselasky@FreeBSD.org>
+ * Copyright (c) 2015 Andriy Voskoboinyk <avos@FreeBSD.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
