bf8e18c8fac3666f0236bc349a0f6e26c73aabbf
bdrewery <bdrewery@FreeBSD.org>
2015-12-15 02:27:31 +0000

    DIRDEPS_BUILD: Avoid duplicates in DIRDEPS for bootstrapping.
    This logic is potentially included multiple times, so overwrite the temporary
    variable rather than append to it.
    
    Sponsored by:	EMC / Isilon Storage Division

--- share/mk/local.dirdeps.mk
+++ share/mk/local.dirdeps.mk
@@ -134,7 +134,7 @@ DIRDEPS+=	usr.bin/yacc.host
 # BUILD_AT_LEVEL0, which we don't use).
 # This only works for DPADD with full OBJ/SRC paths, which is mostly just
 # _INTERNALLIBS.
-_DP_DIRDEPS+= \
+_DP_DIRDEPS= \
 	${DPADD:M${OBJTOP}*:H:tA:C,${OBJTOP}[^/]*/,,:N.:O:u} \
 	${DPADD:M${OBJROOT}*:N${OBJTOP}*:N${STAGE_ROOT}/*:H:S,${OBJROOT},,:C,^([^/]+)/(.*),\2.\1,:S,${HOST_TARGET}$,host,:N.*:O:u}
 # Resolve the paths to RELDIRs
