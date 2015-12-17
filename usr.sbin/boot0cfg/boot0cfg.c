863288da908803e36259fb014031d86f866cfb91
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2015-12-17 20:42:05 +0000

    Exit cleanly if malloc() fails to allocate a buffer for a copy of the current MBR.
    PR:		205322
    Submitted by:	Alexander Kuleshov <kuleshovmail@gmail.com>
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@292410 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/boot0cfg/boot0cfg.c
+++ usr.sbin/boot0cfg/boot0cfg.c
@@ -337,6 +337,8 @@ read_mbr(const char *disk, u_int8_t **mbr, int check_version)
 	return (mbr_size);
     }
     *mbr = malloc(sizeof(buf));
+    if (mbr == NULL)
+	errx(1, "%s: unable to allocate MBR buffer", disk);
     memcpy(*mbr, buf, sizeof(buf));
     close(fd);
 
