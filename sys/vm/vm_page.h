c4a7697b9e04a22ecd7c20d63441ce206b6f844e
alc <alc@FreeBSD.org>
2015-12-18 21:18:07 +0000

    Retire vm_page_cache().

--- sys/vm/vm_page.h
+++ sys/vm/vm_page.h
@@ -453,7 +453,6 @@ vm_page_t vm_page_alloc_contig(vm_object_t object, vm_pindex_t pindex, int req,
     vm_paddr_t boundary, vm_memattr_t memattr);
 vm_page_t vm_page_alloc_freelist(int, int);
 vm_page_t vm_page_grab (vm_object_t, vm_pindex_t, int);
-void vm_page_cache(vm_page_t);
 void vm_page_cache_free(vm_object_t, vm_pindex_t, vm_pindex_t);
 void vm_page_cache_transfer(vm_object_t, vm_pindex_t, vm_object_t);
 int vm_page_try_to_free (vm_page_t);
