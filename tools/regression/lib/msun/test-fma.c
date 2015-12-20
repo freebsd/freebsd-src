54a00b864cbbc09fc48625e32e90741d50ec07b8
Enji Cooper <ngie@FreeBSD.org>
2015-12-20 04:32:30 +0000

    Don't run test-fma on i386
    It completely fails all assertions on i386 on both stable/9 and stable/10
    
    PR: 205448
    MFC after: 1 week
    X-MFC to: stable/10
    Sponsored by: EMC / Isilon Storage Division

--- tools/regression/lib/msun/test-fma.c
+++ tools/regression/lib/msun/test-fma.c
@@ -37,6 +37,7 @@ __FBSDID("$FreeBSD$");
 #include <float.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "test-utils.h"
 
@@ -475,6 +476,11 @@ main(int argc, char *argv[])
 {
 	int rmodes[] = { FE_TONEAREST, FE_UPWARD, FE_DOWNWARD, FE_TOWARDZERO };
 	int i, j;
+
+#if defined(__i386__)
+	printf("1..0 # SKIP all testcases fail on i386\n");
+	exit(0);
+#endif
 
 	printf("1..19\n");
 
