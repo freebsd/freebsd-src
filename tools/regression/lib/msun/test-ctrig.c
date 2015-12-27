dd77e1af1becad7cc2de2f49be7df4b316a22a7a
ngie <ngie@FreeBSD.org>
2015-12-27 21:17:30 +0000

    MFC r292489:
    Disable test-ctrig.t testcases which fail assertions on i386
    
    Tested on: stable/10 (amd64/i386), head (amd64/i386)
    PR: 205446
    X-MFC to: stable/10
    Sponsored by: EMC / Isilon Storage Division

--- tools/regression/lib/msun/test-ctrig.c
+++ tools/regression/lib/msun/test-ctrig.c
@@ -427,6 +427,7 @@ test_large(void)
 	test_odd_tol(ctanh, z,
 		     CMPLXL(1.0, 8.95257245135025991216632140458264468e-309L),
 		     DBL_ULP());
+#if !defined(__i386__)
 	z = CMPLXL(30, 0x1p1023L);
 	test_odd_tol(ctanh, z,
 		     CMPLXL(1.0, -1.62994325413993477997492170229268382e-26L),
@@ -436,6 +437,7 @@ test_large(void)
 		     CMPLXL(0.878606311888306869546254022621986509L,
 			    -0.225462792499754505792678258169527424L),
 		     DBL_ULP());
+#endif
 
 	z = CMPLXL(710.6, 0.78539816339744830961566084581987572L);
 	test_odd_tol(csinh, z,
