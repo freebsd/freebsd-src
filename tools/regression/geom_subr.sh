18e6b1b06722aee06744d866e7a2d659825d5db0
Enji Cooper <ngie@FreeBSD.org>
2016-01-01 02:25:10 +0000

    Add functions for managing md(4) devices and cleaning up said md(4) devices
    These will be used soon in the various test scripts that source geom_subr.sh
    
    MFC after: 2 weeks
    Sponsored by: EMC / Isilon Storage Division

--- tools/regression/geom_subr.sh
+++ tools/regression/geom_subr.sh
@@ -17,3 +17,32 @@ devwait()
 		sleep 0.2
 	done
 }
+
+# Need to keep track of the test md devices to avoid the scenario where a test
+# failing will cause the other tests to bomb out, or a test failing will leave
+# a large number of md(4) devices lingering around
+: ${TMPDIR=/tmp}
+export TMPDIR
+TEST_MDS_FILE=${TMPDIR}/test_mds
+
+attach_md()
+{
+	local test_md
+
+	test_md=$(mdconfig -a "$@") || exit
+	echo $test_md >> $TEST_MDS_FILE || exit
+	echo $test_md
+}
+
+geom_test_cleanup()
+{
+	local test_md
+
+	if [ -f $TEST_MDS_FILE ]; then
+		while read test_md; do
+			# The "#" tells the TAP parser this is a comment
+			echo "# Removing test memory disk: $test_md"
+			mdconfig -d -u $test_md
+		done < $TEST_MDS_FILE
+	fi
+}
