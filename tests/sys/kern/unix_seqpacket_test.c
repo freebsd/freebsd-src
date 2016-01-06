6631311a86b5a2c574f55cc9e5d82c296407ede6
ngie <ngie@FreeBSD.org>
2016-01-06 20:43:41 +0000

    MFC r271397: r271397 (by asomers):
    Abort the create_socket test if socket creation fails.
    
    CID:		1232756

--- tests/sys/kern/unix_seqpacket_test.c
+++ tests/sys/kern/unix_seqpacket_test.c
@@ -386,7 +386,7 @@ ATF_TC_BODY(create_socket, tc)
 	int s;
 
 	s = socket(PF_LOCAL, SOCK_SEQPACKET, 0);
-	ATF_CHECK(s >= 0);
+	ATF_REQUIRE(s >= 0);
 }
 
 /* Create SEQPACKET sockets using socketpair(2) */
