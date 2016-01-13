260b37cd3a2bcbf463f34c8bf1270e16da9bb946
Brooks Davis <brooks@one-eyed-alien.net>
2016-01-13 16:32:53 +0000

    Update to match the latest tag.

--- diff-to-upstream.sh
+++ diff-to-upstream.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-UPSTREAM_TAG=freebsd-head-20150723
+UPSTREAM_TAG=freebsd-head-20150827
 
 git diff --name-only ${UPSTREAM_TAG} | \
     grep -v -E '(cheri|bin/shmem_bench|contrib/(curl|jpeg|libpng|netsurf)|^ctsrd|lib/lib(curl|helloworld|jpeg|png)|libexec/.*-helper|usr.bin/libpng_sb_test|usr.bin/nsfb)' | \
