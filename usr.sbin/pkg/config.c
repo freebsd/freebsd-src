d11935e3039113a1842b7e8bdfc5e51c117e9366
bapt <bapt@FreeBSD.org>
2016-01-13 17:59:12 +0000

    Remove some unneeded headers

--- usr.sbin/pkg/config.c
+++ usr.sbin/pkg/config.c
@@ -31,21 +31,14 @@ __FBSDID("$FreeBSD$");
 #include <sys/param.h>
 #include <sys/queue.h>
 #include <sys/sbuf.h>
-#include <sys/types.h>
 #include <sys/utsname.h>
 #include <sys/sysctl.h>
 
-#include <assert.h>
 #include <dirent.h>
 #include <ucl.h>
-#include <ctype.h>
 #include <err.h>
 #include <errno.h>
-#include <fcntl.h>
-#include <inttypes.h>
-#include <paths.h>
 #include <stdbool.h>
-#include <string.h>
 #include <unistd.h>
 
 #include "config.h"
