828ddfb37d8ce905a3598543904627643d137436
Jonathan T. Looney <jtl@FreeBSD.org>
2016-01-14 22:02:21 +0000

    MFC r290811:   Fix hwpmc "stalled" behavior
      Currently, there is a single pm_stalled flag that tracks whether a
      performance monitor was "stalled" due to insufficent ring buffer
      space for samples. However, because the same performance monitor
      can run on multiple processes or threads at the same time, a single
      pm_stalled flag that impacts them all seems insufficient.
    
      In particular, you can hit corner cases where the code fails to stop
      performance monitors during a context switch out, because it thinks
      the performance monitor is already stopped. However, in reality,
      it may be that only the monitor running on a different CPU was stalled.
    
      This patch attempts to fix that behavior by tracking on a per-CPU basis
      whether a PM desires to run and whether it is "stalled". This lets the
      code make better decisions about when to stop PMs and when to try to
      restart them. Ideally, we should avoid the case where the code fails
      to stop a PM during a context switch out.
    
    MFC r290813:
      Optimizations to the way hwpmc gathers user callchains
    
      Changes to the code to gather user stacks:
      * Delay setting pmc_cpumask until we actually have the stack.
      * When recording user stack traces, only walk the portion of the ring
        that should have samples for us.
    
    MFC r290929:
      Change the driver stats to what they really are: unsigned values.
    
      When pmcstat exits after some samples were dropped, give the user an
      idea of how many were lost. (Granted, these are global numbers, but
      they may still help quantify the scope of the loss.)
    
    MFC r290930:
      Improve accuracy of PMC sampling frequency
    
      The code tracks a counter which is the number of events until the next
      sample. On context switch in, it loads the saved counter. On context
      switch out, it tries to calculate a new saved counter.
    
      Problems:
    
      1. The saved counter was shared by all threads in a process. However, this
      means that all threads would be initially loaded with the same saved
      counter. However, that could result in sampling more often than once every
      X number of events.
    
      2. The calculation to determine a new saved counter was backwards. It
      added when it should have subtracted, and subtracted when it should have
      added. Assume a single-threaded process with a reload count of 1000
      events. Assuming the counter on context switch in was 100 and the counter
      on context switch out was 50 (meaning the thread has "consumed" 50 more
      events), the code would calculate a new saved counter of 150 (instead of
      the proper 50).
    
      Fix:
    
      1. As soon as the saved counter is used to initialize a monitor for a
      thread on context switch in, set the saved counter to the reload count.
      That way, subsequent threads to use the saved counter will get the full
      reload count, assuring we sample at least once every X number of events
      (across all threads).
    
      2. Change the calculation of the saved counter. Due to the change to the
      saved counter in #1, we simply need to add (modulo the reload count) the
      remaining counter time we retrieve from the CPU when a thread is context
      switched out.
    
    MFC r291016:
      Support a wider history counter in pmcstat(8) gmon output
    
      pmcstat(8) contains an option to output sampling data in a gmon format
      compatible with gprof(1). Currently, it uses the default histcounter,
      which is an (unsigned short). With large sets of sampling data, it
      is possible to overflow the maximum value provided by an (unsigned
      short).
    
      This change adds the -e argument to pmcstat. If -e and -g are both
      specified, pmcstat will use a histcounter type of uint64_t.
    
    MFC r291017:
      Fix the date on the pmcstat(8) man page from r291016.

--- sys/sys/pmc.h
+++ sys/sys/pmc.h
@@ -534,14 +534,15 @@ struct pmc_op_configurelog {
  */
 
 struct pmc_op_getdriverstats {
-	int	pm_intr_ignored;	/* #interrupts ignored */
-	int	pm_intr_processed;	/* #interrupts processed */
-	int	pm_intr_bufferfull;	/* #interrupts with ENOSPC */
-	int	pm_syscalls;		/* #syscalls */
-	int	pm_syscall_errors;	/* #syscalls with errors */
-	int	pm_buffer_requests;	/* #buffer requests */
-	int	pm_buffer_requests_failed; /* #failed buffer requests */
-	int	pm_log_sweeps;		/* #sample buffer processing passes */
+	unsigned int	pm_intr_ignored;	/* #interrupts ignored */
+	unsigned int	pm_intr_processed;	/* #interrupts processed */
+	unsigned int	pm_intr_bufferfull;	/* #interrupts with ENOSPC */
+	unsigned int	pm_syscalls;		/* #syscalls */
+	unsigned int	pm_syscall_errors;	/* #syscalls with errors */
+	unsigned int	pm_buffer_requests;	/* #buffer requests */
+	unsigned int	pm_buffer_requests_failed; /* #failed buffer requests */
+	unsigned int	pm_log_sweeps;		/* #sample buffer processing
+						   passes */
 };
 
 /*
@@ -598,6 +599,7 @@ struct pmc_op_getdyneventinfo {
 
 #include <sys/malloc.h>
 #include <sys/sysctl.h>
+#include <sys/_cpuset.h>
 
 #include <machine/frame.h>
 
@@ -713,7 +715,8 @@ struct pmc {
 		pmc_value_t	pm_initial;	/* counting PMC modes */
 	} pm_sc;
 
-	uint32_t	pm_stalled;	/* marks stalled sampling PMCs */
+	volatile cpuset_t pm_stalled;	/* marks stalled sampling PMCs */
+	volatile cpuset_t pm_cpustate;	/* CPUs where PMC should be active */
 	uint32_t	pm_caps;	/* PMC capabilities */
 	enum pmc_event	pm_event;	/* event being measured */
 	uint32_t	pm_flags;	/* additional flags PMC_F_... */
