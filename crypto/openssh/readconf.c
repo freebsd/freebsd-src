f80f6d14f54767ff01edf332cf5c3623bce5ce7e
Gleb Smirnoff <glebius@FreeBSD.org>
2016-01-14 22:47:54 +0000

    Fix OpenSSH client information leak.
    Security:       SA-16:07.openssh
    Security:       CVE-2016-0777
    Approved by:	so

--- crypto/openssh/readconf.c
+++ crypto/openssh/readconf.c
@@ -1643,7 +1643,7 @@ initialize_options(Options * options)
 	options->tun_remote = -1;
 	options->local_command = NULL;
 	options->permit_local_command = -1;
-	options->use_roaming = -1;
+	options->use_roaming = 0;
 	options->visual_host_key = -1;
 	options->ip_qos_interactive = -1;
 	options->ip_qos_bulk = -1;
@@ -1825,8 +1825,7 @@ fill_default_options(Options * options)
 		options->tun_remote = SSH_TUNID_ANY;
 	if (options->permit_local_command == -1)
 		options->permit_local_command = 0;
-	if (options->use_roaming == -1)
-		options->use_roaming = 1;
+	options->use_roaming = 0;
 	if (options->visual_host_key == -1)
 		options->visual_host_key = 0;
 	if (options->ip_qos_interactive == -1)
