4e77ff286d6af64dfa19ce51f2d81d4af260580b
arybchik <arybchik@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-01-18 06:13:09 +0000

    sfxge: convert nvram write method to use partition id
    Submitted by:   Andy Moreton <amoreton at solarflare.com>
    Sponsored by:   Solarflare Communications, Inc.
    MFC after:      2 days
    Differential Revision: https://reviews.freebsd.org/D4961
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@294249 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/sfxge/common/hunt_impl.h
+++ sys/dev/sfxge/common/hunt_impl.h
@@ -369,14 +369,6 @@ ef10_nvram_partn_lock(
 	__in			efx_nic_t *enp,
 	__in			uint32_t partn);
 
-extern	__checkReturn		efx_rc_t
-ef10_nvram_partn_write(
-	__in			efx_nic_t *enp,
-	__in			uint32_t partn,
-	__in			unsigned int offset,
-	__out_bcount(size)	caddr_t data,
-	__in			size_t size);
-
 extern				void
 ef10_nvram_partn_unlock(
 	__in			efx_nic_t *enp,
@@ -400,14 +392,6 @@ ef10_nvram_get_version(
 	__in			efx_nvram_type_t type,
 	__out			uint32_t *subtypep,
 	__out_ecount(4)		uint16_t version[4]);
-
-extern	__checkReturn		efx_rc_t
-ef10_nvram_write_chunk(
-	__in			efx_nic_t *enp,
-	__in			efx_nvram_type_t type,
-	__in			unsigned int offset,
-	__in_bcount(size)	caddr_t data,
-	__in			size_t size);
 
 extern				void
 ef10_nvram_rw_finish(
@@ -457,6 +441,14 @@ ef10_nvram_partn_erase(
 	__in			efx_nic_t *enp,
 	__in			uint32_t partn,
 	__in			unsigned int offset,
+	__in			size_t size);
+
+extern	__checkReturn		efx_rc_t
+ef10_nvram_partn_write(
+	__in			efx_nic_t *enp,
+	__in			uint32_t partn,
+	__in			unsigned int offset,
+	__out_bcount(size)	caddr_t data,
 	__in			size_t size);
 
 #endif	/* EFSYS_OPT_NVRAM */
