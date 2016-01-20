a92d6b2fe02155e3c7b48fb2f819926cd462049e
Andrew Rybchenko <arybchik@FreeBSD.org>
2016-01-20 08:14:42 +0000

    MFC r294092
    sfxge: rename Huntington MAC methods to EF10 and use for Medford
    
    Submitted by:   Mark Spender <mspender at solarflare.com>
    Reviewed by:    gnn
    Sponsored by:   Solarflare Communications, Inc.

--- sys/dev/sfxge/common/efx_mac.c
+++ sys/dev/sfxge/common/efx_mac.c
@@ -113,27 +113,27 @@ static efx_mac_ops_t	__efx_siena_mac_ops = {
 };
 #endif	/* EFSYS_OPT_SIENA */
 
-#if EFSYS_OPT_HUNTINGTON
-static efx_mac_ops_t	__efx_hunt_mac_ops = {
+#if EFSYS_OPT_HUNTINGTON || EFSYS_OPT_MEDFORD
+static efx_mac_ops_t	__efx_ef10_mac_ops = {
 	NULL,					/* emo_reset */
-	hunt_mac_poll,				/* emo_poll */
-	hunt_mac_up,				/* emo_up */
-	hunt_mac_addr_set,			/* emo_addr_set */
-	hunt_mac_reconfigure,			/* emo_reconfigure */
-	hunt_mac_multicast_list_set,		/* emo_multicast_list_set */
-	hunt_mac_filter_default_rxq_set,	/* emo_filter_default_rxq_set */
-	hunt_mac_filter_default_rxq_clear,
+	ef10_mac_poll,				/* emo_poll */
+	ef10_mac_up,				/* emo_up */
+	ef10_mac_addr_set,			/* emo_addr_set */
+	ef10_mac_reconfigure,			/* emo_reconfigure */
+	ef10_mac_multicast_list_set,		/* emo_multicast_list_set */
+	ef10_mac_filter_default_rxq_set,	/* emo_filter_default_rxq_set */
+	ef10_mac_filter_default_rxq_clear,
 					/* emo_filter_default_rxq_clear */
 #if EFSYS_OPT_LOOPBACK
-	hunt_mac_loopback_set,			/* emo_loopback_set */
+	ef10_mac_loopback_set,			/* emo_loopback_set */
 #endif	/* EFSYS_OPT_LOOPBACK */
 #if EFSYS_OPT_MAC_STATS
 	efx_mcdi_mac_stats_upload,		/* emo_stats_upload */
 	efx_mcdi_mac_stats_periodic,		/* emo_stats_periodic */
-	hunt_mac_stats_update			/* emo_stats_update */
+	ef10_mac_stats_update			/* emo_stats_update */
 #endif	/* EFSYS_OPT_MAC_STATS */
 };
-#endif	/* EFSYS_OPT_HUNTINGTON */
+#endif	/* EFSYS_OPT_HUNTINGTON || EFSYS_OPT_MEDFORD */
 
 static efx_mac_ops_t	*__efx_mac_ops[] = {
 	/* [EFX_MAC_INVALID] */
@@ -158,7 +158,13 @@ static efx_mac_ops_t	*__efx_mac_ops[] = {
 #endif
 	/* [EFX_MAC_HUNTINGTON] */
 #if EFSYS_OPT_HUNTINGTON
-	&__efx_hunt_mac_ops,
+	&__efx_ef10_mac_ops,
+#else
+	NULL,
+#endif
+	/* [EFX_MAC_MEDFORD] */
+#if EFSYS_OPT_MEDFORD
+	&__efx_ef10_mac_ops,
 #else
 	NULL,
 #endif
@@ -781,6 +787,13 @@ efx_mac_select(
 	efx_mac_ops_t *emop;
 	int rc = EINVAL;
 
+#if EFSYS_OPT_SIENA
+	if (enp->en_family == EFX_FAMILY_SIENA) {
+		type = EFX_MAC_SIENA;
+		goto chosen;
+	}
+#endif
+
 #if EFSYS_OPT_HUNTINGTON
 	if (enp->en_family == EFX_FAMILY_HUNTINGTON) {
 		type = EFX_MAC_HUNTINGTON;
@@ -788,9 +801,9 @@ efx_mac_select(
 	}
 #endif
 
-#if EFSYS_OPT_SIENA
-	if (enp->en_family == EFX_FAMILY_SIENA) {
-		type = EFX_MAC_SIENA;
+#if EFSYS_OPT_MEDFORD
+	if (enp->en_family == EFX_FAMILY_MEDFORD) {
+		type = EFX_MAC_MEDFORD;
 		goto chosen;
 	}
 #endif
