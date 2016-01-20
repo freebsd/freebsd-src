607cfe837a0008f30b39c69630d638084eff0856
Ed Maste <emaste@FreeBSD.org>
2016-01-20 17:40:11 +0000

    MFC r288083, r288138: elfdump: report MIPS-specific ELF section types

--- usr.bin/elfdump/elfdump.c
+++ usr.bin/elfdump/elfdump.c
@@ -379,7 +379,9 @@ sh_types(uint64_t machine, uint64_t sht) {
 			break;
 		case EM_MIPS:
 			switch (sht) {
+			case SHT_MIPS_REGINFO: return "SHT_MIPS_REGINFO";
 			case SHT_MIPS_OPTIONS: return "SHT_MIPS_OPTIONS";
+			case SHT_MIPS_ABIFLAGS: return "SHT_MIPS_ABIFLAGS";
 			}
 			break;
 		}
