38ffd5a7c159438a3ae734f5662f00d38c913af7
Bjoern A. Zeeb <bz@FreeBSD.org>
2016-01-21 14:11:01 +0000

    MFC 292953:
     This code is not in modules that need KPI stability so no need to use
     the wrapper functions as used in r252511 (head).  We can directly use
     the locking macros.

--- sys/netinet6/in6.c
+++ sys/netinet6/in6.c
@@ -2003,7 +2003,7 @@ in6ifa_llaonifp(struct ifnet *ifp)
 
 	if (ND_IFINFO(ifp)->flags & ND6_IFF_IFDISABLED)
 		return (NULL);
-	if_addr_rlock(ifp);
+	IF_ADDR_RLOCK(ifp);
 	TAILQ_FOREACH(ifa, &ifp->if_addrhead, ifa_link) {
 		if (ifa->ifa_addr->sa_family != AF_INET6)
 			continue;
@@ -2013,7 +2013,7 @@ in6ifa_llaonifp(struct ifnet *ifp)
 		    IN6_IS_ADDR_MC_NODELOCAL(&sin6->sin6_addr))
 			break;
 	}
-	if_addr_runlock(ifp);
+	IF_ADDR_RUNLOCK(ifp);
 
 	return ((struct in6_ifaddr *)ifa);
 }
