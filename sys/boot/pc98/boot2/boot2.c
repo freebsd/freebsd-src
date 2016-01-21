92f20bcad84fee7542f30f239ef80f9ca93567fd
jhb <jhb@FreeBSD.org>
2016-01-21 01:25:28 +0000

    MFC 292682: Fix remaining direct tests of the carry flag in the v86 %eflags via a magic number to use V86_CY() instead.  These should have been fixed as part of the cleanup in r226746 but were missed.
    The md5 sums of the object files were unchanged, so there should be no
    functional change.
    
    PR:		205424
    Submitted by:	Alexander Kuleshov <kuleshovmail@gmail.com>

--- sys/boot/pc98/boot2/boot2.c
+++ sys/boot/pc98/boot2/boot2.c
@@ -326,7 +326,7 @@ bd_getbigeom(int bunit)
     v86.addr = 0x1b;
     v86.eax = 0x8400 | unit;
     v86int();
-    if (v86.efl & 0x1)
+    if (V86_CY(v86.efl))
 	return 0x4F020F;	/* 1200KB FD C:80 H:2 S:15 */
     return ((v86.ecx & 0xffff) << 16) | (v86.edx & 0xffff);
 }
