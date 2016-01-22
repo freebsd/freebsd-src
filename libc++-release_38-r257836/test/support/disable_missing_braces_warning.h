4421370ac85fae53ab337764252b12039f11274c
dim <dim@FreeBSD.org>
2016-01-22 21:14:05 +0000

    Oops, move the vendor tag created in r294165 to the right location.

--- libc++-release_38-r257836/test/support/disable_missing_braces_warning.h
+++
@@ -1,8 +0,0 @@
-#ifndef SUPPORT_DISABLE_MISSING_BRACES_WARNING_H
-#define SUPPORT_DISABLE_MISSING_BRACES_WARNING_H
-
-// std::array is explicitly allowed to be initialized with A a = { init-list };.
-// Disable the missing braces warning for this reason.
-#pragma GCC diagnostic ignored "-Wmissing-braces"
-
-#endif // SUPPORT_DISABLE_MISSING_BRACES_WARNING_H
