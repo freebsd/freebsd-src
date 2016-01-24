80afdcb508cd0f9c3479ad2c01679a943da8a871
Enji Cooper <ngie@FreeBSD.org>
2016-01-24 06:19:49 +0000

    MFC r293073:
    - Use attach_md instead of hardcoding md(4) provider unit numbers
    - Implement a gmirror_test_cleanup function, which in turn calls
      geom_test_cleanup to clean up all md(4) providers allocated in the test
      run.
    - Remove duplicate logic in test scripts for removing md(4) providers.
    - Don't create files in /tmp (outside the kyua sandbox); use the current
      directory instead

--- tools/regression/geom_mirror/conf.sh
+++ tools/regression/geom_mirror/conf.sh
@@ -5,4 +5,11 @@ name="$(mktemp -u mirror.XXXXXX)"
 class="mirror"
 base=`basename $0`
 
+gmirror_test_cleanup()
+{
+	[ -c /dev/$class/$name ] && gmirror destroy $name
+	geom_test_cleanup
+}
+trap gmirror_test_cleanup ABRT EXIT INT TERM
+
 . `dirname $0`/../geom_subr.sh
