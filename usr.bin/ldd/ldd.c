6cdde1eb9e112d83ff4d63a31ab8e0762f564e0d
br <br@FreeBSD.org>
2016-01-24 15:15:57 +0000

    We don't support a.out executables on RISC-V.
    Reviewed by:	emaste
    Sponsored by:	DARPA, AFRL
    Sponsored by:	HEIF5
    Differential Revision:	https://reviews.freebsd.org/D5048

--- usr.bin/ldd/ldd.c
+++ usr.bin/ldd/ldd.c
@@ -48,8 +48,8 @@ __FBSDID("$FreeBSD$");
 
 #include "extern.h"
 
-/* We don't support a.out executables on arm64 */
-#ifndef __aarch64__
+/* We don't support a.out executables on arm64 and riscv */
+#if !defined(__aarch64__) && !defined(__riscv__)
 #include <a.out.h>
 #define	AOUT_SUPPORTED
 #endif
