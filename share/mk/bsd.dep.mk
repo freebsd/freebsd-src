fc2c2cd9e3f32daf40685ade050d5ab4e1e683e7
Bryan Drewery <bdrewery@FreeBSD.org>
2016-01-25 22:29:44 +0000

    Fix PROGS not reading .depend files after r284288 by making DEPENDFILE work.
    We have had this user-modifable DEPENDFILE variable forever that does nothing
    relevant for the user since fmake always used '.depend'.  Bmake
    introduced the .MAKE.DEPENDFILE variable that can be modified to change
    the name of '.depend'.
    
    Prior to r284288, bsd.progs.mk was setting .MAKE.DEPENDFILE to allow
    working incremental builds.  This was modified most likely to not
    conflict with the META MODE handling of .MAKE.DEPENDFILE as it has a lot
    more special logic for that variable.
    
    MFC after:	1 week
    Sponsored by:	EMC / Isilon Storage Division

--- share/mk/bsd.dep.mk
+++ share/mk/bsd.dep.mk
@@ -56,6 +56,7 @@ _MKDEPCC+=	${DEPFLAGS}
 .endif
 MKDEPCMD?=	CC='${_MKDEPCC}' mkdep
 DEPENDFILE?=	.depend
+.MAKE.DEPENDFILE= ${DEPENDFILE}
 DEPENDFILES=	${DEPENDFILE}
 
 # Keep `tags' here, before SRCS are mangled below for `depend'.
