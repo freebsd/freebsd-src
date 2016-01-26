b6b5fab9e2697d34bf00525c03e57e52d9c41d99
smh <smh@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-01-26 12:30:17 +0000

    ixgbe sysctl hardware defaults
    Added hw.ix.flow_control which enables the default flow_control of all ix
    interfaces to be set in loader.conf.
    
    Added hw.ix.advertise_speed which enables the default advertised_speed of
    all ix interfaces to be set in loader.conf.
    
    Made enable_aim device independent based on hw.ix.enable_aim default.
    
    Reviewed by:	erj
    MFC after:	1 week
    Sponsored by:	Multiplay
    Differential Revision:	https://reviews.freebsd.org/D5060
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@294795 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ixgbe/ixgbe.h
+++ sys/dev/ixgbe/ixgbe.h
@@ -497,6 +497,7 @@ struct adapter {
 	u32			optics;
 	u32			fc; /* local flow ctrl setting */
 	int			advertise;  /* link speeds */
+	bool			enable_aim; /* adaptive interrupt moderation */
 	bool			link_active;
 	u16			max_frame_size;
 	u16			num_segs;
