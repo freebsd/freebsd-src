b825ec7f6c74c07cd73603623f597b0d93dbb7c4
Brooks Davis <brooks@one-eyed-alien.net>
2016-01-26 21:43:37 +0000

    Exclude this script from the diff output.

--- diff-to-upstream.sh
+++ diff-to-upstream.sh
@@ -3,5 +3,5 @@
 UPSTREAM_TAG=freebsd-head-20160118
 
 git diff --name-only ${UPSTREAM_TAG} | \
-    grep -v -E '(cheri|bin/shmem_bench|contrib/(curl|jpeg|libpng|netsurf)|^ctsrd|lib/lib(curl|helloworld|jpeg|png)|libexec/.*-helper|usr.bin/libpng_sb_test|usr.bin/nsfb)' | \
+    grep -v -E '(cheri|bin/shmem_bench|contrib/(curl|jpeg|libpng|netsurf)|^ctsrd|diff-to-upstream.sh|lib/lib(curl|helloworld|jpeg|png)|libexec/.*-helper|usr.bin/libpng_sb_test|usr.bin/nsfb)' | \
     xargs git diff ${UPSTREAM_TAG} --
