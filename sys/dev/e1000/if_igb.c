c80429cedb277ccc9626c28a7cf8fafc5a265ff6
Eric Joyner <erj@FreeBSD.org>
2016-02-05 17:14:37 +0000

    Update em(4) to 7.6.1; update igb(4) to 2.5.3.
    Major changes:
    
    - Add i219/i219(2) hardware support. (Found on Skylake generation and newer
      chipsets.)
      - Further to the last Skylake support diff, this one also includes support for
        the Lewisburg chipset (i219(3)).
    
    - Add a workaround to an igb hardware errata.
      All 1G server products need to have IPv6 extension header parsing turned off.
      This should be listed in the specification updates for current 1G server
      products, e.g. for i350 it's errata #37 in this document:
      http://www.intel.com/content/dam/www/public/us/en/documents/specification-updates/ethernet-controller-i350-spec-update.pdf
    
    - Avoton (i354) PHY errata workaround added
    
    And a bunch of minor fixes, as well as #defines for things that the current
    em(4)/igb(4) drivers don't implement.
    
    Differential Revision: https://reviews.freebsd.org/D3162
    Reviewed by: sbruno, marius, gnn
    Approved by: gnn
    MFC after: 2 weeks
    Sponsored by: Intel Corporation

--- sys/dev/e1000/if_igb.c
+++ sys/dev/e1000/if_igb.c
@@ -47,7 +47,7 @@
 /*********************************************************************
  *  Driver version:
  *********************************************************************/
-char igb_driver_version[] = "2.5.2";
+char igb_driver_version[] = "2.5.3-k";
 
 
 /*********************************************************************
@@ -551,9 +551,9 @@ igb_attach(device_t dev)
 		    "Disable Energy Efficient Ethernet");
 		if (adapter->hw.phy.media_type == e1000_media_type_copper) {
 			if (adapter->hw.mac.type == e1000_i354)
-				e1000_set_eee_i354(&adapter->hw);
+				e1000_set_eee_i354(&adapter->hw, TRUE, TRUE);
 			else
-				e1000_set_eee_i350(&adapter->hw);
+				e1000_set_eee_i350(&adapter->hw, TRUE, TRUE);
 		}
 	}
 
@@ -1277,6 +1277,9 @@ igb_init_locked(struct adapter *adapter)
 	if (ifp->if_capenable & IFCAP_TSO)
 		ifp->if_hwassist |= CSUM_TSO;
 
+	/* Clear bad data from Rx FIFOs */
+	e1000_rx_fifo_flush_82575(&adapter->hw);
+
 	/* Configure for OS presence */
 	igb_init_manageability(adapter);
 
@@ -1304,7 +1307,6 @@ igb_init_locked(struct adapter *adapter)
 		return;
 	}
 	igb_initialize_receive_units(adapter);
-	e1000_rx_fifo_flush_82575(&adapter->hw);
 
         /* Enable VLAN support */
 	if (ifp->if_capenable & IFCAP_VLAN_HWTAGGING)
@@ -1341,9 +1343,9 @@ igb_init_locked(struct adapter *adapter)
 	/* Set Energy Efficient Ethernet */
 	if (adapter->hw.phy.media_type == e1000_media_type_copper) {
 		if (adapter->hw.mac.type == e1000_i354)
-			e1000_set_eee_i354(&adapter->hw);
+			e1000_set_eee_i354(&adapter->hw, TRUE, TRUE);
 		else
-			e1000_set_eee_i350(&adapter->hw);
+			e1000_set_eee_i350(&adapter->hw, TRUE, TRUE);
 	}
 }
 
