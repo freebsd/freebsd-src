87ab639764a7d4ff24c9e2ecd1f21536e3b9ceb5
glebius <glebius@FreeBSD.org>
2016-02-08 17:47:58 +0000

    Merge r264915: fix NULL pointer derefernce with special sequence of DIOCADDADDR and DIOCADDRULE.
    PR:		206933
    Approved by:	re (marius)

--- sys/netpfil/pf/pf_ioctl.c
+++ sys/netpfil/pf/pf_ioctl.c
@@ -350,7 +350,9 @@ pf_empty_pool(struct pf_palist *poola)
 			pfi_dynaddr_remove(pa->addr.p.dyn);
 			break;
 		case PF_ADDR_TABLE:
-			pfr_detach_table(pa->addr.p.tbl);
+			/* XXX: this could be unfinished pooladdr on pabuf */
+			if (pa->addr.p.tbl != NULL)
+				pfr_detach_table(pa->addr.p.tbl);
 			break;
 		}
 		if (pa->kif)
