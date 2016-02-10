244f9b32ceead663d5f21f94910212c556ccff73
Brooks Davis <brooks@one-eyed-alien.net>
2016-02-10 21:45:54 +0000

    Update tag, ignore more netsurf bits.

--- diff-to-upstream.sh
+++ diff-to-upstream.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-UPSTREAM_TAG=freebsd-head-20160118
+UPSTREAM_TAG=freebsd-head-20160129
 
 git diff --name-only ${UPSTREAM_TAG} | \
-    grep -v -E '(cheri|bin/shmem_bench|contrib/(curl|jpeg|libpng|netsurf)|^ctsrd|diff-to-upstream.sh|lib/lib(curl|helloworld|jpeg|png)|libexec/.*-helper|usr.bin/libpng_sb_test|usr.bin/nsfb)' | \
+    grep -v -E '(cheri|bin/shmem_bench|contrib/(curl|jpeg|libpng|netsurf)|^ctsrd|diff-to-upstream.sh|lib/(lib(curl|helloworld|jpeg|png)|netsurf)|libexec/.*-helper|usr.bin/libpng_sb_test|share/netsurf|usr.bin/nsfb)' | \
     xargs git diff ${UPSTREAM_TAG} --
