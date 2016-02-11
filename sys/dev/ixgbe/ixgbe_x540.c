fd560f49734672a8d1c7f809460d6a57c5f396c8
smh <smh@FreeBSD.org>
2016-02-11 16:54:23 +0000

    MFC r294734, r295093 & r295094 ixgbe fixes
    MFC r294734
    Fix ixgbe compliation with DBG 1
    
    MFC r295093
    Configure ixgbe phy & gbic power
    
    MFC r295094
    Fix ixgbe flow control autoneg reporting
    
    Approved by:	re (marius)
    Sponsored by:	Multiplay

--- sys/dev/ixgbe/ixgbe_x540.c
+++ sys/dev/ixgbe/ixgbe_x540.c
@@ -82,8 +82,7 @@ s32 ixgbe_init_ops_X540(struct ixgbe_hw *hw)
 	/* PHY */
 	phy->ops.init = ixgbe_init_phy_ops_generic;
 	phy->ops.reset = NULL;
-	if (!ixgbe_mng_present(hw))
-		phy->ops.set_phy_power = ixgbe_set_copper_phy_power;
+	phy->ops.set_phy_power = ixgbe_set_copper_phy_power;
 
 	/* MAC */
 	mac->ops.reset_hw = ixgbe_reset_hw_X540;
