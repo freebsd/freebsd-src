36e9c2cef0d59d4f4e466b558eaaab0b76ad3603
Zbigniew Bodek <zbb@FreeBSD.org>
2016-02-20 12:28:20 +0000

    Revert r295756: Extract common code from PowerPC's ofw_pci
    Import portions of the PowerPC OF PCI implementation into
    new file "ofw_pci.c", common for other platforms. The files ofw_pci.c and
    ofw_pci.h from sys/powerpc/ofw no longer exist. All required declarations
    are moved to sys/dev/ofw/ofw_pci.h.
    
    This creates a new ofw_pci_write_ivar() function and modifies
    ofw_pci_nranges(), ofw_pci_read_ivar(), ofw_pci_route_interrupt()
    methods.
    Most functions contain existing ppc implementations in the majority
    unchanged. Now there is no need to have multiple identical copies
    of methods for various architectures.
    
    Submitted by:  Marcin Mazurek <mma@semihalf.com>
    Obtained from: Semihalf
    Sponsored by:  Annapurna Labs
    Reviewed by:   jhibbits, mmel
    Differential Revision: https://reviews.freebsd.org/D4879
    
    This needs to return to the drawing board as it breaks both
    PowerPC and Sparc64 build.
    
    Pointed out by: jhibbits

--- sys/powerpc/powermac/cpcht.c
+++ sys/powerpc/powermac/cpcht.c
@@ -36,6 +36,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/rman.h>
 
 #include <dev/ofw/openfirm.h>
+#include <dev/ofw/ofw_pci.h>
 
 #include <dev/pci/pcivar.h>
 #include <dev/pci/pcireg.h>
@@ -50,7 +51,7 @@ __FBSDID("$FreeBSD$");
 
 #include <dev/ofw/ofw_bus.h>
 #include <dev/ofw/ofw_bus_subr.h>
-#include <dev/ofw/ofw_pci.h>
+#include <powerpc/ofw/ofw_pci.h>
 
 #include <vm/vm.h>
 #include <vm/pmap.h>
