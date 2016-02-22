dfee19f4e04e29514d4cdbd72f9fe79a097c678f
avos <avos@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-02-22 00:48:53 +0000

    urtwn: add an option to compile the driver without firmware specific code
    - Add URTWN_WITHOUT_UCODE option (will disable any firmware specific code
    when set).
    - Do not exclude the driver from build when MK_SOURCELESS_UCODE is set
    (URTWN_WITHOUT_UCODE will be enforced unconditionally).
    - Do not abort initialization when firmware cannot be loaded;
    behave like the URTWN_WITHOUT_UCODE option was set.
    - Drop some unused variables from urtwn_softc structure.
    
    Tested with RTL8188EU and RTL8188CUS in HOSTAP and STA modes.
    
    Reviewed by:	kevlo
    Approved by:	adrian (mentor)
    Differential Revision:	https://reviews.freebsd.org/D4849
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@295871 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/usb/wlan/if_urtwnvar.h
+++ sys/dev/usb/wlan/if_urtwnvar.h
@@ -155,7 +155,8 @@ struct urtwn_softc {
 	uint8_t				sc_flags;
 #define URTWN_FLAG_CCK_HIPWR	0x01
 #define URTWN_DETACHED		0x02
-#define	URTWN_RUNNING		0x04
+#define URTWN_RUNNING		0x04
+#define URTWN_FW_LOADED		0x08
 #define URTWN_TEMP_MEASURED	0x10
 
 	u_int				chip;
@@ -195,11 +196,6 @@ struct urtwn_softc {
 	urtwn_datahead			sc_tx_active;
 	urtwn_datahead			sc_tx_inactive;
 	urtwn_datahead			sc_tx_pending;
-
-	const char			*fwname;
-	const struct firmware		*fw_fp;
-	struct urtwn_fw_info		fw;
-	void				*fw_virtaddr;
 
 	union urtwn_rom			rom;
 	uint16_t			last_rom_addr;
