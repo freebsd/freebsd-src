a742acf7896380716a7d336cbcd5ce73e0989063
delphij <delphij@FreeBSD.org>
2016-02-23 08:12:39 +0000

    Instant-MFC r295914: MFV r295913:
    Partially apply upstream changeset 6e06b1c8 (kientzle).
    
    Limit filter recursion level to 25 (instead of infinite).  This fixes a
    potential crash issue discovered by Alexander Cherepanov.
    
    PR:		207362
    Reported by:	Robert Clausecker
    Obtained from:	libarchive github project
    Approved by:	so

--- contrib/libarchive/libarchive/archive_read.c
+++ contrib/libarchive/libarchive/archive_read.c
@@ -352,13 +352,13 @@ archive_read_open2(struct archive *_a, void *client_data,
 static int
 build_stream(struct archive_read *a)
 {
-	int number_bidders, i, bid, best_bid;
+	int number_bidders, i, bid, best_bid, n;
 	struct archive_read_filter_bidder *bidder, *best_bidder;
 	struct archive_read_filter *filter;
 	ssize_t avail;
 	int r;
 
-	for (;;) {
+	for (n = 0; n < 25; ++n) {
 		number_bidders = sizeof(a->bidders) / sizeof(a->bidders[0]);
 
 		best_bid = 0;
@@ -402,6 +402,9 @@ build_stream(struct archive_read *a)
 			return (r);
 		}
 	}
+	archive_set_error(&a->archive, ARCHIVE_ERRNO_FILE_FORMAT,
+	    "Input requires too many filters for decoding");
+	return (ARCHIVE_FATAL);
 }
 
 /*
