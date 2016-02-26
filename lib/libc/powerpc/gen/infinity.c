97a5390e6ddbe408a6ec8320d9081277d6ec6960
Nathan Whitehorn <nwhitehorn@FreeBSD.org>
2016-02-26 20:38:23 +0000

    Make unions in PowerPC libc endian-safe.

--- lib/libc/powerpc/gen/infinity.c
+++ lib/libc/powerpc/gen/infinity.c
@@ -11,7 +11,20 @@ __FBSDID("$FreeBSD$");
 #include <math.h>
 
 /* bytes for +Infinity on powerpc */
-const union __infinity_un __infinity = { { 0x7f, 0xf0, 0, 0, 0, 0, 0, 0 } };
+const union __infinity_un __infinity = {
+#if BYTE_ORDER == BIG_ENDIAN
+	{ 0x7f, 0xf0, 0, 0, 0, 0, 0, 0 }
+#else
+	{ 0, 0, 0, 0, 0, 0, 0xf0, 0x7f }
+#endif
+};
 
 /* bytes for NaN */
-const union __nan_un __nan = { { 0xff, 0xc0, 0, 0 } };
+const union __nan_un __nan = {
+#if BYTE_ORDER == BIG_ENDIAN
+	{0xff, 0xc0, 0, 0}
+#else
+	{ 0, 0, 0xc0, 0xff }
+#endif
+};
+
