52ea10af6939231efd8b22d02d95dd960a0f4e0f
Wojciech Macek <wma@FreeBSD.org>
2016-03-02 08:39:59 +0000

    Improve ThunderX PEM driver to work on pass2 revision
        Things changed:
        * do not allocate 4GB of SLI space, because it's the waste of
          system resources. Allocate only small portions when needed.
        * provide own implementation of activate_resource which performs
          address translation between PCI bus and host PA address space.
          This is temporary solution, should be replaced by bus_map_resource
          once implemented.
    
    Obtained from:         Semihalf
    Sponsored by:          Cavium
    Approved by:           cognet (mentor)
    Reviewed by:           jhb
    Differential revision: https://reviews.freebsd.org/D5294

--- sys/arm64/cavium/thunder_pcie_common.c
+++ sys/arm64/cavium/thunder_pcie_common.c
@@ -106,6 +106,28 @@ range_addr_is_phys(struct pcie_range *ranges, uint64_t addr, uint64_t size)
 }
 
 uint64_t
+range_addr_phys_to_pci(struct pcie_range *ranges, uint64_t phys_addr)
+{
+	struct pcie_range *r;
+	uint64_t offset;
+	int tuple;
+
+	/* Find physical address corresponding to given bus address */
+	for (tuple = 0; tuple < MAX_RANGES_TUPLES; tuple++) {
+		r = &ranges[tuple];
+		if (phys_addr >= r->phys_base &&
+		    phys_addr < (r->phys_base + r->size)) {
+			/* Given phys addr is in this range.
+			 * Translate phys addr to bus addr.
+			 */
+			offset = phys_addr - r->phys_base;
+			return (r->pci_base + offset);
+		}
+	}
+	return (0);
+}
+
+uint64_t
 range_addr_pci_to_phys(struct pcie_range *ranges, uint64_t pci_addr)
 {
 	struct pcie_range *r;
