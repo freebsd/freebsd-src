871880cf35b1344de5e5cb2cf361e0791f4c75d7
bz <bz@FreeBSD.org>
2016-03-03 10:27:08 +0000

    No need to keep type stability on raw sockets zone. We've also been running with a KASSERT since r222488 to make sure the ipi_count is 0 on destroy.
    Sponsored by:	The FreeBSD Foundation

--- sys/netinet/raw_ip.c
+++ sys/netinet/raw_ip.c
@@ -209,8 +209,7 @@ rip_init(void)
 {
 
 	in_pcbinfo_init(&V_ripcbinfo, "rip", &V_ripcb, INP_PCBHASH_RAW_SIZE,
-	    1, "ripcb", rip_inpcb_init, NULL, UMA_ZONE_NOFREE,
-	    IPI_HASHFIELDS_NONE);
+	    1, "ripcb", rip_inpcb_init, NULL, 0, IPI_HASHFIELDS_NONE);
 	EVENTHANDLER_REGISTER(maxsockets_change, rip_zone_change, NULL,
 	    EVENTHANDLER_PRI_ANY);
 }
