1d121b9b4976bf84d62f8f74e7bce5c57efa37e7
mav <mav@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-03-08 18:53:00 +0000

    MFV r296540: 4448 zfs diff misprints unicode characters
    Reviewed by: Igor Kozhukhov <ikozhukhov@gmail.com>
    Reviewed by: Toomas Soome <tsoome@me.com>
    Approved by: Matthew Ahrens <mahrens@delphix.com>
    Author: Joshua M. Clulow <jmc@joyent.com>
    
    illumos/illumos-gate@b211eb9181f99c20acbf4c528f94cb44b4ca8c31
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@296541 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- cddl/contrib/opensolaris/lib/libzfs/common/libzfs_diff.c
+++ cddl/contrib/opensolaris/lib/libzfs/common/libzfs_diff.c
@@ -22,6 +22,7 @@
 /*
  * Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright 2015 Nexenta Systems, Inc. All rights reserved.
+ * Copyright 2016 Joyent, Inc.
  */
 
 /*
@@ -136,12 +137,13 @@ get_stats_for_obj(differ_info_t *di, const char *dsname, uint64_t obj,
 static void
 stream_bytes(FILE *fp, const char *string)
 {
-	while (*string) {
-		if (*string > ' ' && *string != '\\' && *string < '\177')
-			(void) fprintf(fp, "%c", *string++);
-		else {
-			(void) fprintf(fp, "\\%03hho",
-			    (unsigned char)*string++);
+	char c;
+
+	while ((c = *string++) != '\0') {
+		if (c > ' ' && c != '\\' && c < '\177') {
+			(void) fprintf(fp, "%c", c);
+		} else {
+			(void) fprintf(fp, "\\%03o", (uint8_t)c);
 		}
 	}
 }
