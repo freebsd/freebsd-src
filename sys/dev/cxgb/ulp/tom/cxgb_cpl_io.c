a079d3866a43cc2f68119b16f5141029ee67bc07
Navdeep Parhar <np@FreeBSD.org>
2016-03-10 14:17:24 +0000

    cxgb(4): Remove redundant part of an assertion.
    PR:		207858
    Submitted by:	David Binderman

--- sys/dev/cxgb/ulp/tom/cxgb_cpl_io.c
+++ sys/dev/cxgb/ulp/tom/cxgb_cpl_io.c
@@ -286,7 +286,7 @@ release_tid(struct toedev *tod, unsigned int tid, int qset)
 	struct tid_info *t = &td->tid_maps;
 #endif
 
-	KASSERT(tid >= 0 && tid < t->ntids,
+	KASSERT(tid < t->ntids,
 	    ("%s: tid=%d, ntids=%d", __func__, tid, t->ntids));
 
 	m = M_GETHDR_OFLD(qset, CPL_PRIORITY_CONTROL, cpl);
