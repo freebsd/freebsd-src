4d7ae5ef71a055fe45993e4d9786163b640da874
Bryan Drewery <bdrewery@FreeBSD.org>
2016-03-11 23:45:05 +0000

    DIRDEPS_BUILD: export DESTDIR for STAGING.
    An example of where this is needed is in share/examples which for
    'etc-examples' runs 'make -C SRCTOP/etc etc-examples' which installs
    to the default DESTDIR otherwise.
    
    Sponsored by:	EMC / Isilon Storage Division

--- share/mk/bsd.sys.mk
+++ share/mk/bsd.sys.mk
@@ -197,6 +197,7 @@ staging stage_libs stage_files stage_as stage_links stage_symlinks:
 .else
 # allow targets like beforeinstall to be leveraged
 DESTDIR= ${STAGE_OBJTOP}
+.export DESTDIR
 
 .if target(beforeinstall)
 .if !empty(_LIBS) || (${MK_STAGING_PROG} != "no" && !defined(INTERNALPROG))
