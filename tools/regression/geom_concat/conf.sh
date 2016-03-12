976f44ffb6fcd46b126d64871bc40d47ed7a3814
ngie <ngie@FreeBSD.org>
2016-03-12 23:11:08 +0000

    MFC r293434:
    - Use attach_md for memory disks so they can be tracked.
    - Add a geom_concat specific cleanup function and trap on that function at
      exit so things are cleaned up properly
    - Don't hardcode /tmp for temporary files, which violates the kyua sandbox

--- tools/regression/geom_concat/conf.sh
+++ tools/regression/geom_concat/conf.sh
@@ -5,4 +5,11 @@ name="$(mktemp -u concat.XXXXXX)"
 class="concat"
 base=`basename $0`
 
+gconcat_test_cleanup()
+{
+	[ -c /dev/$class/$name ] && gconcat destroy $name
+	geom_test_cleanup
+}
+trap gconcat_test_cleanup ABRT EXIT INT TERM
+
 . `dirname $0`/../geom_subr.sh
