6044c03a3795675e364007b579eb0d483ee5ce08
Konstantin Belousov <kib@FreeBSD.org>
2016-03-22 10:51:42 +0000

    Apparently there are some popular programs around which assume that it is safe to call pthread_mutex_init() on the same shared mutex several times.  POSIX claims that the behaviour in this case is undefined.
    Make this working by only allowing one caller to initialize the mutex.
    Other callers either see already completed initialization and do
    nothing, or busy-loop yielding while designated initializer finishes.
    Also make the API requirements loose by initializing mutexes on other
    pthread_mutex*() calls if they see uninitialized shared mutex.
    
    Only mutexes provide the hack for now, but it could be also
    implemented for other process shared primitives from libthr.
    
    Reported and tested by:	"Oleg V. Nauman" <oleg@opentransfer.com>
    Sponsored by:	The FreeBSD Foundation

--- lib/libthr/thread/thr_private.h
+++ lib/libthr/thread/thr_private.h
@@ -146,6 +146,13 @@ TAILQ_HEAD(mutex_queue, pthread_mutex);
 
 #define MAX_DEFER_WAITERS       50
 
+/*
+ * Values for pthread_mutex m_ps indicator.
+ */
+#define	PMUTEX_INITSTAGE_ALLOC	0
+#define	PMUTEX_INITSTAGE_BUSY	1
+#define	PMUTEX_INITSTAGE_DONE	2
+
 struct pthread_mutex {
 	/*
 	 * Lock for accesses to this structure.
@@ -156,6 +163,7 @@ struct pthread_mutex {
 	int				m_count;
 	int				m_spinloops;
 	int				m_yieldloops;
+	int				m_ps;	/* pshared init stage */
 	/*
 	 * Link for all mutexes a thread currently owns, of the same
 	 * prio type.
