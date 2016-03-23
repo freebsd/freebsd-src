060e287744c22dab7c4bcd8737aadccb215e4fc4
Brooks Davis <brooks@one-eyed-alien.net>
2016-03-23 16:32:02 +0000

    Declare alignment macros for compressed CHERI capabilities.
    CHERI_(SEAL_)ALIGN_SHIFT computes the lowest bit which must be zero for
    a given length a la PAGE_SHIFT.
    
    CHERI_(SEAL_)ALIGN_MASK computes the mask of bits which must be zero a la
    PAGE_MASK.
    
    With precise (256-bit) capabilities, CHERI_(SEAL_)ALIGN_SHIFT and
    _MASK are zero. With 128-bit compesses capabilities, *_ALIGN_SHIFT
    is computing by finding the top bit with flsll() and subtracting the
    micro-architecture specific number of bits allowed in base and length.
    
    GCC does not document an __builtin_flsll() so it is unclear it constant
    values will be optimized properly.  If not, we should adjust the
    implementation as appropraite (perhaps using clzll()).

--- sys/mips/include/cherireg.h
+++ sys/mips/include/cherireg.h
@@ -373,4 +373,25 @@
  */
 #define	CHERI_CCALL_EXC_VEC	((intptr_t)(int32_t)0x80000280)
 
+#if CHERICAP_SIZE == 32
+#define	CHERI_ALIGN_SHIFT(l)	0ULL
+#define	CHERI_SEAL_ALIGN_SHIFT(l)	0ULL
+#else /* (!(CHERICAP_SIZE == 32)) */
+
+#define	CHERI_BASELEN_BITS	20
+#define	CHERI_SEAL_BASELEN_BITS	10
+#define	CHERI_SLOP_BITS		2
+#define	CHERI_ADDR_BITS		64
+
+#define	CHERI_ALIGN_SHIFT(l)						\
+    ((flsll(l) <= (CHERI_BASELEN_BITS - CHERI_SLOP_BITS)) ? 0ULL :	\
+    (flsll(l) - (CHERI_BASELEN_BITS - CHERI_SLOP_BITS)))
+#define	CHERI_SEAL_ALIGN_SHIFT(l)					\
+    ((flsll(l) <= (CHERI_SEAL_BASELEN_BITS - CHERI_SLOP_BITS)) ? 0ULL :	\
+    (flsll(l) - (CHERI_SEAL_BASELEN_BITS - CHERI_SLOP_BITS)))
+#endif /* (!(CHERICAP_SIZE == 32)) */
+
+#define	CHERI_ALIGN_MASK(l)		~(~0ULL << CHERI_ALIGN_SHIFT(l))
+#define	CHERI_SEAL_ALIGN_MASK(l)	~(~0ULL << CHERI_SEAL_ALIGN_SHIFT(l))
+
 #endif /* _MIPS_INCLUDE_CHERIREG_H_ */
