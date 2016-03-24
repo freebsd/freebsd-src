eeaebf709773790b28a42a2f0fc59b8b552e178f
hselasky <hselasky@FreeBSD.org>
2016-03-24 09:22:58 +0000

    MFC r292254:
    Properly drain callouts in the IPFW subsystem to avoid use after free
    panics when unloading the dummynet and IPFW modules:
    
    - The callout drain function can sleep and should not be called having
    a non-sleepable lock locked. Remove locks around "ipfw_dyn_uninit(0)".
    
    - Add a new "dn_gone" variable to prevent asynchronous restart of
    dummynet callouts when unloading the dummynet kernel module.
    
    - Call "dn_reschedule()" locked so that "dn_gone" can be set and
    checked atomically with regard to starting a new callout.
    
    PR:			208171
    Requested by:		Franco Fichtner (opnsense.org)
    Differential Revision:	https://reviews.freebsd.org/D3855

--- sys/netpfil/ipfw/ip_fw2.c
+++ sys/netpfil/ipfw/ip_fw2.c
@@ -2704,12 +2704,10 @@ vnet_ipfw_uninit(const void *unused)
 	V_ip_fw_ctl_ptr = NULL;
 	IPFW_UH_WLOCK(chain);
 	IPFW_UH_WUNLOCK(chain);
-	IPFW_UH_WLOCK(chain);
 
-	IPFW_WLOCK(chain);
 	ipfw_dyn_uninit(0);	/* run the callout_drain */
-	IPFW_WUNLOCK(chain);
 
+	IPFW_UH_WLOCK(chain);
 	ipfw_destroy_tables(chain);
 	reap = NULL;
 	IPFW_WLOCK(chain);
