7f4dacfb3ee3176c47abcaa8ebe82d42c6580af5
zbb <zbb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-03-31 11:18:52 +0000

    Disable MSI-x for AHCI on Alpine plattform
    Changes introduced to AHCI code adding support for MSI-x
    caused interrupt storm on Alpine boards.
    This is unintended behaviour so added quirk to omit this functionality.
    
    Reviewed by:   mav
    Submitted by:  Marcin Mazurek <mma@semihalf.com>
    Obtained from: Semihalf
    Sponsored by:  Annapurna Labs
    Differential Revision: https://reviews.freebsd.org/D4301
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@297447 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ahci/ahci_pci.c
+++ sys/dev/ahci/ahci_pci.c
@@ -293,7 +293,7 @@ static const struct {
 	{0x11851039, 0x00, "SiS 968",		0},
 	{0x01861039, 0x00, "SiS 968",		0},
 	{0xa01c177d, 0x00, "ThunderX",		AHCI_Q_ABAR0|AHCI_Q_1MSI},
-	{0x00311c36, 0x00, "Annapurna",		AHCI_Q_FORCE_PI|AHCI_Q_RESTORE_CAP},
+	{0x00311c36, 0x00, "Annapurna",		AHCI_Q_FORCE_PI|AHCI_Q_RESTORE_CAP|AHCI_Q_NOMSIX},
 	{0x00000000, 0x00, NULL,		0}
 };
 
@@ -436,6 +436,9 @@ ahci_pci_attach(device_t dev)
 	if (!(ctlr->r_mem = bus_alloc_resource_any(dev, SYS_RES_MEMORY,
 	    &ctlr->r_rid, RF_ACTIVE)))
 		return ENXIO;
+
+	if (ctlr->quirks & AHCI_Q_NOMSIX)
+		msix_count = 0;
 
 	/* Read MSI-x BAR IDs if supported */
 	if (msix_count > 0) {
