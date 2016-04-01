2ce16cc04d5c109217f9720e42a393de45ccda4f
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2016-04-01 13:09:09 +0000

    As a workaround for linker failures relating to GOT/TLS overflows, use -ftls-model=local-exec when compiling CheriABI binaries.  With this change, we are able to compile a much larger set of CheriABI binaries when using jemalloc.

--- share/mk/bsd.cheri.mk
+++ share/mk/bsd.cheri.mk
@@ -31,7 +31,7 @@ OBJCOPY:=	elfcopy
 _CHERI_CC+=	-mabi=sandbox -mxgot
 LIBDIR:=	/usr/libcheri
 ROOTOBJDIR=	${.OBJDIR:S,${.CURDIR},,}${SRCTOP}/worldcheri${SRCTOP}
-CFLAGS+=	-O2
+CFLAGS+=	-O2 -ftls-model=local-exec
 .if ${MK_CHERI_LINKER} == "yes"
 _CHERI_CC+=	-cheri-linker
 CFLAGS+=	-Wno-error
