b800851e587a5ffe92be9e304f87db2b98079e97
Brooks Davis <brooks@one-eyed-alien.net>
2016-04-07 21:47:17 +0000

    cheriabi_ioctl arguments will need hand parsing.

--- sys/compat/cheriabi/cheriabi_fill_uap_manual.h
+++ sys/compat/cheriabi/cheriabi_fill_uap_manual.h
@@ -29,6 +29,58 @@
  */
 
 static inline int
+CHERIABI_SYS_cheriabi_ioctl_fill_uap(struct thread *td,
+    struct cheriabi_ioctl_args *uap)
+{
+	struct chericap tmpcap;
+	u_int tag;
+	register_t perms, reqperms;
+	register_t sealed;
+	size_t base __unused, length, offset;
+
+	/* [0] int fd */
+	cheriabi_fetch_syscall_arg(td, &tmpcap, CHERIABI_SYS_cheriabi_ioctl, 0);
+	CHERI_CLC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &tmpcap, 0);
+	CHERI_CTOINT(uap->fd, CHERI_CR_CTEMP0);
+
+	/* [1] u_long com */
+	cheriabi_fetch_syscall_arg(td, &tmpcap, CHERIABI_SYS_cheriabi_ioctl, 1);
+	CHERI_CLC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &tmpcap, 0);
+	CHERI_CTOINT(uap->com, CHERI_CR_CTEMP0);
+
+	/* [2] _Inout_opt_ caddr_t data */
+	cheriabi_fetch_syscall_arg(td, &tmpcap, CHERIABI_SYS_cheriabi_ioctl, 2);
+	CHERI_CLC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &tmpcap, 0);
+	CHERI_CGETTAG(tag, CHERI_CR_CTEMP0);
+	if (!tag) {
+		CHERI_CTOINT(uap->data, CHERI_CR_CTEMP0);
+		if (uap->data != NULL)
+			return (EPROT);
+	} else {
+		CHERI_CGETPERM(perms, CHERI_CR_CTEMP0);
+		reqperms = (CHERI_PERM_GLOBAL|CHERI_PERM_LOAD|CHERI_PERM_STORE);
+		if ((perms & reqperms) != reqperms)
+			return (EPROT);
+
+		CHERI_CGETSEALED(sealed, CHERI_CR_CTEMP0);
+		if (sealed)
+			return (EPROT);
+
+		CHERI_CGETLEN(length, CHERI_CR_CTEMP0);
+		CHERI_CGETLEN(offset, CHERI_CR_CTEMP0);
+		if (offset => length)
+			return (EPROT);
+		length -= offset;
+		if (length < sizeof(*uap->data))
+			return (EPROT);
+
+		CHERI_CTOPTR(uap->data, CHERI_CR_CTEMP0, CHERI_CR_KDC);
+	}
+
+	return (0);
+}
+
+static inline int
 CHERIABI_SYS_mincore_fill_uap(struct thread *td,
     struct mincore_args *uap)
 {
