be0f9b70cd70b1e8e9ff645c7033f69fb9257154
Ian Lepore <ian@FreeBSD.org>
2016-04-07 18:19:09 +0000

    Fix a copyright glitch before it gets copy-pasted again.  I think this must have started as collateral damage in a global search-replace, then it got copied around when I cloned a file to begin creating a new file.

--- sys/mips/mips/ofw_machdep.c
+++ sys/mips/mips/ofw_machdep.c
@@ -1,6 +1,6 @@
 /*-
  * Copyright (c) 2015 Ian Lepore <ian@freebsd.org>
- * All rights excluded.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
