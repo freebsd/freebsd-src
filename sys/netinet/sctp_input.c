eac27e6ed9e23ce29080a60d4c858be2a42eca62
tuexen <tuexen@FreeBSD.org>
2016-04-18 06:32:24 +0000

    Remove a left-over debug printf().

--- sys/netinet/sctp_input.c
+++ sys/netinet/sctp_input.c
@@ -906,11 +906,6 @@ sctp_handle_shutdown(struct sctp_shutdown_chunk *cp,
 				    asoc->control_pdapi->on_strm_q);
 			}
 		}
-		printf("%s:%d End added to ctl:%p (%d)\n",
-		    __FUNCTION__,
-		    __LINE__,
-		    asoc->control_pdapi,
-		    asoc->control_pdapi->on_strm_q);
 		asoc->control_pdapi->end_added = 1;
 		asoc->control_pdapi->pdapi_aborted = 1;
 		asoc->control_pdapi = NULL;
@@ -1026,11 +1021,6 @@ sctp_handle_shutdown_ack(struct sctp_shutdown_ack_chunk *cp SCTP_UNUSED,
 		 * With a normal shutdown we assume the end of last record.
 		 */
 		SCTP_INP_READ_LOCK(stcb->sctp_ep);
-		printf("%s:%d End added to ctl:%p (%d)\n",
-		    __FUNCTION__,
-		    __LINE__,
-		    asoc->control_pdapi,
-		    asoc->control_pdapi->on_strm_q);
 		asoc->control_pdapi->end_added = 1;
 		asoc->control_pdapi->pdapi_aborted = 1;
 		asoc->control_pdapi = NULL;
