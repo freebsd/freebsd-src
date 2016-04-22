09f48df71ee206eac7a278253eff0b7cfa0e8151
sephe <sephe@FreeBSD.org>
2016-04-22 05:01:43 +0000

    hyperv: Update copyright to 2016 for the files Microsoft changed in 2016
    Sponsored by:	Microsoft OSTC
    Differential Revision:	https://reviews.freebsd.org/D6039

--- sys/dev/hyperv/vmbus/hv_et.c
+++ sys/dev/hyperv/vmbus/hv_et.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2015 Microsoft Corp.
+ * Copyright (c) 2015,2016 Microsoft Corp.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
