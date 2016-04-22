cc80d70ddcf7d8dfc1ecf0993225222cc0eef9de
ngie <ngie@FreeBSD.org>
2016-04-22 21:26:15 +0000

    MFC r298303:
    Remove trailing whitespace and use `nitems(mib)` instead of `2` when
    calling sysctl(3)

--- lib/libc/gen/getpagesize.c
+++ lib/libc/gen/getpagesize.c
@@ -53,7 +53,7 @@ __FBSDID("$FreeBSD$");
 int
 getpagesize()
 {
-	int mib[2]; 
+	int mib[2];
 	static int value;
 	size_t size;
 	int error;
@@ -68,7 +68,7 @@ getpagesize()
 	mib[0] = CTL_HW;
 	mib[1] = HW_PAGESIZE;
 	size = sizeof value;
-	if (sysctl(mib, 2, &value, &size, NULL, 0) == -1)
+	if (sysctl(mib, nitems(mib), &value, &size, NULL, 0) == -1)
 		return (-1);
 
 	return (value);
