0431769ee9ec66a615119290fe5841320f96682e
pfg <pfg@FreeBSD.org>
2016-04-25 21:14:32 +0000

    dev/random: use our roundup() macro instead of re-implementing it.
    While here also use howmany() macro from sys/param.h
    No functional change.
    
    Reviewed by:	markm (roundup replacement part)
    Approved by:	so

--- sys/dev/random/fortuna.c
+++ sys/dev/random/fortuna.c
@@ -324,7 +324,7 @@ random_fortuna_genrandom(uint8_t *buf, u_int bytecount)
 	 *      - K = GenerateBlocks(2)
 	 */
 	KASSERT((bytecount <= RANDOM_FORTUNA_MAX_READ), ("invalid single read request to Fortuna of %d bytes", bytecount));
-	blockcount = (bytecount + RANDOM_BLOCKSIZE - 1)/RANDOM_BLOCKSIZE;
+	blockcount = howmany(bytecount, RANDOM_BLOCKSIZE);
 	random_fortuna_genblocks(buf, blockcount);
 	random_fortuna_genblocks(temp, RANDOM_KEYS_PER_BLOCK);
 	randomdev_encrypt_init(&fortuna_state.fs_key, temp);
