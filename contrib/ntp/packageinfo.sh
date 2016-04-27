0082edd7441c5f2d674b6e05a5afe718a3dcbc59
delphij <delphij@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-04-27 07:46:38 +0000

    MFV r298691:
    ntp 4.2.8p7.
    
    Security:	CVE-2016-1547, CVE-2016-1548, CVE-2016-1549, CVE-2016-1550
    Security:	CVE-2016-1551, CVE-2016-2516, CVE-2016-2517, CVE-2016-2518
    Security:	CVE-2016-2519
    Security:	FreeBSD-SA-16:16.ntp
    With hat:	so
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@298695 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/ntp/packageinfo.sh
+++ contrib/ntp/packageinfo.sh
@@ -83,7 +83,7 @@ CLTAG=NTP_4_2_0
 # - Numeric values increment
 # - empty 'increments' to 1
 # - NEW 'increments' to empty
-point=6
+point=7
 
 ### betapoint is normally modified by script.
 # ntp-stable Beta number (betapoint)
