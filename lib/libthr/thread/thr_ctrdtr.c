freebsd/freebsd-src

commit ca8d182c82cfc52718f5ceccd5c11c4ffa89dbcf

Make sure the tcb allocation is sufficiently aligned
Otherwise we get an alignment error on the first _tcb_ctor call on
CHERI256 as the tcb pointer is only 16 byte aligned but we actually
need 32 byte alignment:

ALIGNMENT_FIX_ERR: pid 670 tid 100034 (cheri_pthreads), uid 0:
pc 0x120020248 got a write fault (type 0x5) at 0x160418050

lib/libthr/thread/thr_ctrdtr.c
@@ -34,6 +34,15 @@ __FBSDID("$FreeBSD$");
 
 #include "thr_private.h"
 
+/* 16 Byte alignment works for most architectures but on CHERI256 in the pure
+ * capability ABI we need need the TCB to be aligned to 32 bytes */
+#ifndef TCB_ALIGN
+/* TODO: this definition should be enough, no need have another in pthread_md.h */
+/* Or do we want it to be a capability in hybrid code? */
+/* XXX-AR: #include <sys/param.h> -> MAX(sizeof(void*), 16) */
+#define TCB_ALIGN (sizeof(void*) < 16 ? 16 : sizeof(void*))
+#endif
+
 struct tcb *
 _tcb_ctor(struct pthread *thread, int initial)
 {
@@ -42,8 +51,8 @@ _tcb_ctor(struct pthread *thread, int initial)
 	if (initial)
 		tcb = _tcb_get();
 	else
-		tcb = _rtld_allocate_tls(NULL, sizeof(struct tcb), 16);
 	if (tcb)
+		tcb = _rtld_allocate_tls(NULL, sizeof(struct tcb), TCB_ALIGN);
 		tcb->tcb_thread = thread;
 	return (tcb);
 }
@@ -52,5 +61,5 @@ void
 _tcb_dtor(struct tcb *tcb)
 {
 
-	_rtld_free_tls(tcb, sizeof(struct tcb), 16);
+	_rtld_free_tls(tcb, sizeof(struct tcb), TCB_ALIGN);
 }

lib/libthr/arch/mips/include/pthread_md.h
@@ -43,6 +43,12 @@
 
 #define	CPU_SPINWAIT
 #define	DTV_OFFSET		offsetof(struct tcb, tcb_dtv)
+#ifdef __CHERI_PURE_CAPABILITY__
+#define TCB_ALIGN (CHERICAP_SIZE)
+#else
+#define TCB_ALIGN (16)
+#endif
+
 
 /*
  * Variant I tcb. The structure layout is fixed, don't blindly
@@ -51,7 +57,7 @@
 struct tcb {
 	void			*tcb_dtv;
 	struct pthread		*tcb_thread;
-};
+} __packed __aligned(TCB_ALIGN);
 
 /* Called from the thread to set its private data. */
 static __inline void

sys/mips/include/tls.h
@@ -43,7 +43,11 @@
 #define TLS_TP_OFFSET	0x7000
 #define TLS_DTP_OFFSET	0x8000
 
-#ifdef __mips_n64
+
+/* XXX-AR: #define TLS_TCB_SIZE	(2 * sizeof(void*)) for all ABIs? */
+#ifdef __CHERI_PURE_CAPABILITY__
+#define TLS_TCB_SIZE	(2 * CHERICAP_SIZE)
+#elif defined(__mips_n64)
 #define TLS_TCB_SIZE	16
 #ifdef COMPAT_FREEBSD32
 #define TLS_TCB_SIZE32	8

commit 63f76625bef32a07b4d05c371bf8addea347158f

Fix error introduced by ca8d182c8

lib/libthr/thread/thr_ctrdtr.c
@@ -51,8 +51,8 @@ _tcb_ctor(struct pthread *thread, int initial)
 	if (initial)
 		tcb = _tcb_get();
 	else
-	if (tcb)
 		tcb = _rtld_allocate_tls(NULL, sizeof(struct tcb), TCB_ALIGN);
+	if (tcb)
 		tcb->tcb_thread = thread;
 	return (tcb);
 }