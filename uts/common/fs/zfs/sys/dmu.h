69e892980f1388f5b0c04cfb967744c9a80854cf
mav <mav@FreeBSD.org>
2016-05-11 12:50:58 +0000

    6736 ZFS per-vdev ZAPs
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: John Kennedy <john.kennedy@delphix.com>
    Reviewed by: George Wilson <george.wilson@delphix.com>
    Reviewed by: Don Brady <don.brady@intel.com>
    Reviewed by: Dan McDonald <danmcd@omniti.com>
    Approved by: Richard Lowe <richlowe@richlowe.net>
    Author: Joe Stein <joe.stein@delphix.com>
    
    openzfs/openzfs@215198a6ad15cf4832370e2f19247abeb36b951a

--- uts/common/fs/zfs/sys/dmu.h
+++ uts/common/fs/zfs/sys/dmu.h
@@ -321,6 +321,7 @@ typedef struct dmu_buf {
 #define	DMU_POOL_BPTREE_OBJ		"bptree_obj"
 #define	DMU_POOL_EMPTY_BPOBJ		"empty_bpobj"
 #define	DMU_POOL_CHECKSUM_SALT		"org.illumos:checksum_salt"
+#define	DMU_POOL_VDEV_ZAP_MAP		"com.delphix:vdev_zap_map"
 
 /*
  * Allocate an object from this objset.  The range of object numbers
