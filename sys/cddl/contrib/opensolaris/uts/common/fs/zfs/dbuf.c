42b6e04ab522788922608ec061c1bee6abb86ee8
Alexander Motin <mav@FreeBSD.org>
2016-05-11 11:35:37 +0000

    MFC r297832: MFV r297831: 6322 ZFS indirect block predictive prefetch
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: Paul Dagnelie <pcd@delphix.com>
    Author: Alexander Motin <mav@FreeBSD.org>
    
    Improve speculative prefetch of indirect blocks.
    
    Scalability of many operations on wide ZFS pool can be limited by
    requirement to prefetch indirect blocks first.  Recently added
    asynchronous indirect block read partially helped, but did not
    solve the problem completely.  This patch extends existing prefetcher
    functionality to explicitly work with indirect blocks.
    
    Before this change prefetcher issued reads for up to 8MB of data in
    advance.  With this change it also issues indirect block reads
    for up to 64MB of data in advance, so that when it will be time to
    actually read those data, it can be done immediately.  Alike effect
    can be achieved by just increasing maximal data prefetch distance,
    but at higher memory cost.
    
    Also this change introduces indirect block prefetch for rewrite
    operations, that was never done before.  Previously ARC miss for
    Indirect blocks regularly blocked rewrites, converting perfectly
    aligned asynchronous operations into synchronous read-write pairs,
    significantly reducing maximal rewrite speed.
    
    While being there this issue was also fixed:
     - prefetch was done always, even if caching for the dataset was
    completely disabled.
    
    Testing on FreeBSD with zvol on top of 6x striped 2x mirrored pool
    of 12 assorted HDDs shown me such performance numbers:
    ------- BEFORE --------
    Write       491363677 bytes/sec
    Read        312430631 bytes/sec
    Rewrite      97680464 bytes/sec
    -------- AFTER --------
    Write       493524146 bytes/sec
    Read        438598079 bytes/sec
    Rewrite     277506044 bytes/sec
    
    Closes #65
    Closes #80
    
    openzfs/openzfs@792fd28ac04f78cc5e43ead2d72a96f244ea84e8

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/dbuf.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/dbuf.c
@@ -716,7 +716,7 @@ dbuf_read(dmu_buf_impl_t *db, zio_t *zio, uint32_t flags)
 	if (db->db_state == DB_CACHED) {
 		mutex_exit(&db->db_mtx);
 		if (prefetch)
-			dmu_zfetch(&dn->dn_zfetch, db->db_blkid, 1);
+			dmu_zfetch(&dn->dn_zfetch, db->db_blkid, 1, B_TRUE);
 		if ((flags & DB_RF_HAVESTRUCT) == 0)
 			rw_exit(&dn->dn_struct_rwlock);
 		DB_DNODE_EXIT(db);
@@ -730,7 +730,7 @@ dbuf_read(dmu_buf_impl_t *db, zio_t *zio, uint32_t flags)
 		/* dbuf_read_impl has dropped db_mtx for us */
 
 		if (prefetch)
-			dmu_zfetch(&dn->dn_zfetch, db->db_blkid, 1);
+			dmu_zfetch(&dn->dn_zfetch, db->db_blkid, 1, B_TRUE);
 
 		if ((flags & DB_RF_HAVESTRUCT) == 0)
 			rw_exit(&dn->dn_struct_rwlock);
@@ -749,7 +749,7 @@ dbuf_read(dmu_buf_impl_t *db, zio_t *zio, uint32_t flags)
 		 */
 		mutex_exit(&db->db_mtx);
 		if (prefetch)
-			dmu_zfetch(&dn->dn_zfetch, db->db_blkid, 1);
+			dmu_zfetch(&dn->dn_zfetch, db->db_blkid, 1, B_TRUE);
 		if ((flags & DB_RF_HAVESTRUCT) == 0)
 			rw_exit(&dn->dn_struct_rwlock);
 		DB_DNODE_EXIT(db);
