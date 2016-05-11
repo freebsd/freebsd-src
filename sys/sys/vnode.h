54a33d2f9748d9524e643d6814270df34c2f555c
Konstantin Belousov <kib@FreeBSD.org>
2016-05-11 06:32:22 +0000

    Add vfs_hash_ref(9) function, which finds a vnode by the hash value and returns it referenced.
    The function is similar to vfs_hash_get(9), but unlike the later,
    returned vnode is not locked.  This operation cannot be requested with
    the vget(9) flags.
    
    Reviewed and tested by:	rmacklem
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -859,6 +859,8 @@ int vfs_hash_get(const struct mount *mp, u_int hash, int flags,
 u_int vfs_hash_index(struct vnode *vp);
 int vfs_hash_insert(struct vnode *vp, u_int hash, int flags, struct thread *td,
     struct vnode **vpp, vfs_hash_cmp_t *fn, void *arg);
+void vfs_hash_ref(const struct mount *mp, u_int hash, struct thread *td,
+    struct vnode **vpp, vfs_hash_cmp_t *fn, void *arg);
 void vfs_hash_rehash(struct vnode *vp, u_int hash);
 void vfs_hash_remove(struct vnode *vp);
 
