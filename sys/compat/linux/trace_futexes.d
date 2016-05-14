8b556bd097f525e695ac6e68420b737f2ee3f864
pfg <pfg@FreeBSD.org>
2016-05-14 00:35:49 +0000

    MFC r298829 sys/compat/linux*: minor spelling fixes.

--- sys/compat/linux/trace_futexes.d
+++ sys/compat/linux/trace_futexes.d
@@ -120,7 +120,7 @@ linuxulator*:locks:futex_mtx:locked
 linuxulator*:locks:futex_mtx:unlock
 /check[probefunc, arg0] == 0/
 {
-	printf("ERROR: unlock attemt of unlocked %s (%p),", probefunc, arg0);
+	printf("ERROR: unlock attempt of unlocked %s (%p),", probefunc, arg0);
 	printf("       missing SDT probe in kernel, or dtrace program started");
 	printf("       while the %s was already held (race condition).", probefunc);
 	printf("       Stack trace follows:");
