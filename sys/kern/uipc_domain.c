385583f1ea41886754e84dc15f5533024adbb591
bz <bz@FreeBSD.org>
2016-05-16 22:33:48 +0000

    Rather than having a printf under INVARIANTS, make 3rd party code fail compile by removing pr_destroy;  otherwise unhappiness will most likely follow on VNET destroy or module unload.
    Sponsored by:	The FreeBSD Foundation

--- sys/kern/uipc_domain.c
+++ sys/kern/uipc_domain.c
@@ -196,15 +196,7 @@ void
 vnet_domain_uninit(void *arg)
 {
 	struct domain *dp = arg;
-	struct protosw *pr;
 
-	for (pr = dp->dom_protosw; pr < dp->dom_protoswNPROTOSW; pr++)
-		if (pr->pr_destroy) {
-#ifdef INVARIANTS
-			printf("%s: pr %p called pr_destroy\n", __func__, pr);
-#endif
-			(*pr->pr_destroy)();
-		}
 	if (dp->dom_destroy)
 		(*dp->dom_destroy)();
 }
