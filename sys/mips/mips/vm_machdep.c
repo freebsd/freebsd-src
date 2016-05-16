6622a8ec6357732fb9fa26f3bc06644da19a7ac8
Alex Richardson <alexander.richardson@cl.cam.ac.uk>
2016-05-16 12:41:38 +0100

    Initialize MIPS TLS offset in the right function
    Inside cpu_thread_alloc() td->td_proc will always be NULL as that
    function receives a zeroed structure. This means the CHERIABI or
    FREEBSD32 branches were never be taken and td->td_md.md_tls_tcb_offset
    was always set to TLS_TP_OFFSET + TLS_TCB_SIZE. This is not sufficiently
    aligned to load capabilities and resulted in a trap on first TLS access
    outside of the main thread. It would work fine for the main thread as
    there the variable is initialized in cheriabi_exec_setregs().

--- sys/mips/mips/vm_machdep.c
+++ sys/mips/mips/vm_machdep.c
@@ -326,19 +326,6 @@ cpu_thread_alloc(struct thread *td)
 	td->td_pcb = (struct pcb *)(td->td_kstack +
 	    td->td_kstack_pages * PAGE_SIZE) - 1;
 	td->td_frame = &td->td_pcb->pcb_regs;
-
-#if defined(__mips_n64) && defined(COMPAT_FREEBSD32)
-	if (td->td_proc && SV_PROC_FLAG(td->td_proc, SV_ILP32)
-		td->td_md.md_tls_tcb_offset = TLS_TP_OFFSET + TLS_TCB_SIZE32;
-	else
-#endif
-#if defined (COMPAT_CHERIABI)
-	if (td->td_proc && SV_PROC_FLAG(td->td_proc, SV_CHERI))
-		td->td_md.md_tls_tcb_offset = TLS_TP_OFFSET + TLS_TCB_SIZE_C;
-	else
-#endif
-	td->td_md.md_tls_tcb_offset = TLS_TP_OFFSET + TLS_TCB_SIZE;
-
 #ifdef KSTACK_LARGE_PAGE
 	/* Just one entry for one large kernel page. */
 	pte = pmap_pte(kernel_pmap, td->td_kstack);
@@ -602,6 +589,17 @@ int
 cpu_set_user_tls(struct thread *td, void *tls_base)
 {
 
+#if defined(__mips_n64) && defined(COMPAT_FREEBSD32)
+	if (td->td_proc && SV_PROC_FLAG(td->td_proc, SV_ILP32)
+		td->td_md.md_tls_tcb_offset = TLS_TP_OFFSET + TLS_TCB_SIZE32;
+	else
+#endif
+#if defined (COMPAT_CHERIABI)
+	if (td->td_proc && SV_PROC_FLAG(td->td_proc, SV_CHERI))
+		td->td_md.md_tls_tcb_offset = TLS_TP_OFFSET + TLS_TCB_SIZE_C;
+	else
+#endif
+	td->td_md.md_tls_tcb_offset = TLS_TP_OFFSET + TLS_TCB_SIZE;
 	td->td_md.md_tls = (char*)tls_base;
 	if (td == curthread && cpuinfo.userlocal_reg == true) {
 		mips_wr_userlocal((unsigned long)tls_base +
