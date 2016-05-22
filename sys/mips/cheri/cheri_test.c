643f4a9b217f72040057c4c74698d3cb2bc78a22
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2016-05-22 19:07:41 +0000

    Add several sysctls to allow testing how the kernel handles in-kernel capability faults.

---
+++ sys/mips/cheri/cheri_test.c
@@ -0,0 +1,223 @@
+/*-
+ * Copyright (c) 2016 Robert N. M. Watson
+ * All rights reserved.
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+ * ("CTSRD"), as part of the DARPA CRASH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include "opt_ddb.h"
+
+#include <sys/param.h>
+#include <sys/kernel.h>
+#include <sys/cheri_serial.h>
+#include <sys/proc.h>
+#include <sys/sysctl.h>
+#include <sys/sysent.h>
+
+#include <ddb/ddb.h>
+#include <sys/kdb.h>
+
+#include <machine/atomic.h>
+#include <machine/cheri.h>
+#include <machine/cherireg.h>
+#include <machine/pcb.h>
+#include <machine/proc.h>
+#include <machine/sysarch.h>
+
+/*
+ * Various test cases that can be exercised in a CHERI-aware kernel using
+ * sysctl.  They currently assume that the compiler used for the kernel is not
+ * CHERI-aware, and so trigger cases via inline assembly.
+ */
+
+static int	cheri_test_sysctl_nulldereference(SYSCTL_HANDLER_ARGS);
+static int	cheri_test_sysctl_fineload(SYSCTL_HANDLER_ARGS);
+static int	cheri_test_sysctl_finestore(SYSCTL_HANDLER_ARGS);
+static int	cheri_test_sysctl_tagviolation(SYSCTL_HANDLER_ARGS);
+static int	cheri_test_sysctl_boundsviolation(SYSCTL_HANDLER_ARGS);
+static int	cheri_test_sysctl_permsviolation(SYSCTL_HANDLER_ARGS);
+
+SYSCTL_NODE(_security_cheri, OID_AUTO, test, CTLFLAG_RD, 0,
+    "CHERI test cases");
+
+SYSCTL_PROC(_security_cheri_test, OID_AUTO, null_dereference,
+    CTLTYPE_INT | CTLFLAG_RW, NULL, 0, cheri_test_sysctl_nulldereference, "I",
+    "Set to trigger a kernel NULL capability dereference");
+
+SYSCTL_PROC(_security_cheri_test, OID_AUTO, fine_load,
+    CTLTYPE_INT | CTLFLAG_RW, NULL, 0, cheri_test_sysctl_fineload, "I",
+    "Set to trigger a valid load via capability");
+
+SYSCTL_PROC(_security_cheri_test, OID_AUTO, fine_store,
+    CTLTYPE_INT | CTLFLAG_RW, NULL, 0, cheri_test_sysctl_finestore, "I",
+    "Set to trigger a valid store via capability");
+
+SYSCTL_PROC(_security_cheri_test, OID_AUTO, tag_violation,
+    CTLTYPE_INT | CTLFLAG_RW, NULL, 0, cheri_test_sysctl_tagviolation, "I",
+    "Set to trigger a kernel tag violation");
+
+SYSCTL_PROC(_security_cheri_test, OID_AUTO, bounds_violation,
+    CTLTYPE_INT | CTLFLAG_RW, NULL, 0, cheri_test_sysctl_boundsviolation, "I",
+    "Set to trigger a kernel bounds violation");
+
+SYSCTL_PROC(_security_cheri_test, OID_AUTO, perms_violation,
+    CTLTYPE_INT | CTLFLAG_RW, NULL, 0, cheri_test_sysctl_permsviolation, "I",
+    "Set to trigger a kernel permissions violation");
+
+/*
+ * Integer target for various successful (and failing) capability test
+ * operations.
+ */
+static int	cheri_test_int;
+
+/*
+ * Various pre-initialised capabilities to test with.
+ */
+static struct chericap	cheri_test_nullcap;
+static struct chericap	cheri_test_finecap;
+static struct chericap	cheri_test_untaggedcap;
+static struct chericap	cheri_test_nilboundscap;
+static struct chericap	cheri_test_readonlycap;
+
+static void
+cheri_test_init(void)
+{
+
+	/* NULL capability. */
+	cheri_capability_set_null(&cheri_test_nullcap);
+
+	/*
+	 * Valid capability to cheri_test_int -- which should be read-write.
+	 * Most other test caps are derived from this one.
+	 */
+	cheri_capability_set(&cheri_test_finecap,
+	    CHERI_PERM_LOAD | CHERI_PERM_STORE, NULL, &cheri_test_int,
+	    sizeof(cheri_test_int), 0);
+
+	/* Valid capability to cheri_test_int -- but tag stripped. */
+	CHERI_CLC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &cheri_test_finecap, 0);
+	CHERI_CCLEARTAG(CHERI_CR_CTEMP0, CHERI_CR_CTEMP0);
+	CHERI_CSC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &cheri_test_untaggedcap, 0);
+
+	/* Valid capability to cheri_test_int -- but bounds set to 0 length. */
+	CHERI_CLC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &cheri_test_finecap, 0);
+	CHERI_CSETBOUNDS(CHERI_CR_CTEMP0, CHERI_CR_CTEMP0, 0);
+	CHERI_CSC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &cheri_test_nilboundscap, 0);
+
+	/* Valid capability to cheri_test_int -- but read-only. */
+	CHERI_CLC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &cheri_test_finecap, 0);
+	CHERI_CANDPERM(CHERI_CR_CTEMP0, CHERI_CR_CTEMP0, CHERI_PERM_LOAD);
+	CHERI_CSC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &cheri_test_readonlycap, 0);
+}
+SYSINIT(cheri_test_init, SI_SUB_CPU, SI_ORDER_ANY, cheri_test_init, NULL);
+
+/*
+ * Sysctls for various operations -- performmed only if a write is made to the
+ * MIB, in order to avoid accidental execution (e.g., via "sysctl -a").
+ */
+static int
+cheri_test_sysctl_nulldereference(SYSCTL_HANDLER_ARGS)
+{
+	int error, i;
+
+	i = 0;
+	error = sysctl_handle_int(oidp, &i, 0, req);
+	if (error != 0 || req->newptr == NULL)
+		return (error);
+	CHERI_CLC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &cheri_test_nullcap, 0);
+	CHERI_CSW(i, 0, 0, CHERI_CR_CTEMP0);
+	return (0);
+}
+
+static int
+cheri_test_sysctl_fineload(SYSCTL_HANDLER_ARGS)
+{
+	int error, i;
+
+	i = 0;
+	error = sysctl_handle_int(oidp, &i, 0, req);
+	if (error != 0 || req->newptr == NULL)
+		return (error);
+	CHERI_CLC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &cheri_test_finecap, 0);
+	CHERI_CLW(i, 0, 0, CHERI_CR_CTEMP0);
+	return (0);
+}
+
+static int
+cheri_test_sysctl_finestore(SYSCTL_HANDLER_ARGS)
+{
+	int error, i;
+
+	i = 0;
+	error = sysctl_handle_int(oidp, &i, 0, req);
+	if (error != 0 || req->newptr == NULL)
+		return (error);
+	CHERI_CLC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &cheri_test_finecap, 0);
+	CHERI_CSW(i, 0, 0, CHERI_CR_CTEMP0);
+	return (0);
+}
+
+static int
+cheri_test_sysctl_tagviolation(SYSCTL_HANDLER_ARGS)
+{
+	int error, i;
+
+	i = 0;
+	error = sysctl_handle_int(oidp, &i, 0, req);
+	if (error != 0 || req->newptr == NULL)
+		return (error);
+	CHERI_CLC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &cheri_test_untaggedcap, 0);
+	CHERI_CSW(i, 0, 0, CHERI_CR_CTEMP0);
+	return (0);
+}
+
+static int
+cheri_test_sysctl_boundsviolation(SYSCTL_HANDLER_ARGS)
+{
+	int error, i;
+
+	i = 0;
+	error = sysctl_handle_int(oidp, &i, 0, req);
+	if (error != 0 || req->newptr == NULL)
+		return (error);
+	CHERI_CLC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &cheri_test_nilboundscap, 0);
+	CHERI_CSW(i, 0, 0, CHERI_CR_CTEMP0);
+	return (0);
+}
+
+static int
+cheri_test_sysctl_permsviolation(SYSCTL_HANDLER_ARGS)
+{
+	int error, i;
+
+	i = 0;
+	error = sysctl_handle_int(oidp, &i, 0, req);
+	if (error != 0 || req->newptr == NULL)
+		return (error);
+	CHERI_CLC(CHERI_CR_CTEMP0, CHERI_CR_KDC, &cheri_test_readonlycap, 0);
+	CHERI_CSW(i, 0, 0, CHERI_CR_CTEMP0);
+	return (0);
+}
