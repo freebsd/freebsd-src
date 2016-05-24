32a6d64c41a14c4003d3eb79196a8587988a692c
John Baldwin <jhb@FreeBSD.org>
2016-05-24 23:04:16 +0000

    MFC 299310: Don't store generated firmware object files in the source directory.
    Trim the leading directory of a firmware source file from the resulting
    target object file name so the object file is stored in the object
    directory. Previously, using 'FIRMWS= /path/to/fw.bin:fw.bin' would
    store the generated 'fw.bin.fwo' file in the /path/to directory. Now
    it stores it in the object directory of the kernel module being built.

--- sys/conf/kmod.mk
+++ sys/conf/kmod.mk
@@ -149,7 +149,7 @@ SRCS+=	${KMOD:S/$/.c/}
 CLEANFILES+=	${KMOD:S/$/.c/}
 
 .for _firmw in ${FIRMWS}
-${_firmw:C/\:.*$/.fwo/}:	${_firmw:C/\:.*$//}
+${_firmw:C/\:.*$/.fwo/:T}:	${_firmw:C/\:.*$//}
 	@${ECHO} ${_firmw:C/\:.*$//} ${.ALLSRC:M*${_firmw:C/\:.*$//}}
 	@if [ -e ${_firmw:C/\:.*$//} ]; then			\
 		${LD} -b binary --no-warn-mismatch ${LDFLAGS}	\
@@ -161,7 +161,7 @@ ${_firmw:C/\:.*$/.fwo/}:	${_firmw:C/\:.*$//}
 		rm ${_firmw:C/\:.*$//};				\
 	fi
 
-OBJS+=	${_firmw:C/\:.*$/.fwo/}
+OBJS+=	${_firmw:C/\:.*$/.fwo/:T}
 .endfor
 .endif
 
