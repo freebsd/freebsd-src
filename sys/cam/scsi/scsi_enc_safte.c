3e083fd988132cd3cd890bfbbf3dd63379558ca8
Alexander Motin <mav@FreeBSD.org>
2016-05-24 07:21:23 +0000

    MFC r299373: Allow sleepable allocations in enclosure daemon threads.
    There were at least two places where M_NOWAIT was used without NULL check.
    This change should fix NULL-dereference panic there and possibly improve
    operation in other ways under memory pressure.

--- sys/cam/scsi/scsi_enc_safte.c
+++ sys/cam/scsi/scsi_enc_safte.c
@@ -292,11 +292,8 @@ safte_process_config(enc_softc_t *enc, struct enc_fsm_state *state,
 	    cfg->DoorLock + cfg->Ntherm + cfg->Nspkrs + cfg->Ntstats + 1;
 	ENC_FREE_AND_NULL(enc->enc_cache.elm_map);
 	enc->enc_cache.elm_map =
-	    ENC_MALLOCZ(enc->enc_cache.nelms * sizeof(enc_element_t));
-	if (enc->enc_cache.elm_map == NULL) {
-		enc->enc_cache.nelms = 0;
-		return (ENOMEM);
-	}
+	    malloc(enc->enc_cache.nelms * sizeof(enc_element_t),
+	    M_SCSIENC, M_WAITOK|M_ZERO);
 
 	r = 0;
 	/*
