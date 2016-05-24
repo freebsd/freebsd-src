b9d11f2c2458706c05bce8cc7cc74b0b4075e5e4
ngie <ngie@FreeBSD.org>
2016-05-24 04:23:58 +0000

    MFC r299764,r299765,r299767,r299769,r299770,r299774,r299802,r299803,r299805,r299814:
    r299764:
    
    Use the size of the destination buffer, not the source buffer.
    
    Technically this is a no-op, but mute the clang warning in case the malloc call
    above for fstring ever changes in the future
    
    r299765:
    
    Fix theoretical buffer overflow issues in snmp_oid2asn_oid
    
    Increase the size of `string` by 1 to account for the '\0' terminator. In the event
    that `str` doesn't contain any non-alpha chars, i would be set to MAXSTR, and
    the subsequent strlcpy call would overflow by a character.
    
    Remove unnecessary `string[i] = '\0'` -- this is already handled by strlcpy.
    
    r299767:
    
    Mute sign compare warning by casting rc to u_int to match nbindings' type
    
    rc cannot be negative -- that was already tested for earlier on in
    the function
    
    r299769:
    
    Use the size of the destination buffer instead of the malloc size, repeated, in order
    to mute a -Wstrlcpy-strlcat-size warning
    
    r299770:
    
    Fix up r299764
    
    I meant to use nitems, not sizeof(..) with the destination buffer. Using sizeof(..)
    on a pointer will always truncate the output in the destination buffer incorrectly
    
    Pointyhat to: ngie
    
    r299774:
    
    Do minimal work necessary to cure a -Wunused-but-set-variable warning from gcc
    
    How errno is saved before and restored after strtoul calls needs a rethink
    
    r299802:
    
    Fix up both r299764 and r299770
    
    nitems was wrong too, as it was being tested against a pointer instead of a buffer on
    the stack.
    
    Since the old code was just doing malloc, then strlcpy'ing the contents of the source
    buffer into the destination buffer, replace it all with a call to strdup..
    
    Supersized Duncecap to: ngie
    
    r299803:
    
    Replace malloc + memset(.., 0, ..) with calloc calls
    
    r299805:
    
    Fix up r299769
    
    Similar to r299802, it was noted that using nitems on scalar pointers is
    invalid.
    
    Use strdup instead of malloc + strlcpy (which is what the old code was doing
    anyhow).
    
    Pointyhat to: ngie
    
    r299814:
    
    Replace malloc + memset(.., 0, ..) with calloc calls

--- usr.sbin/bsnmpd/tools/libbsnmptools/bsnmptools.c
+++ usr.sbin/bsnmpd/tools/libbsnmptools/bsnmptools.c
@@ -251,7 +251,7 @@ add_filename(struct snmp_toolinfo *snmptoolctx, const char *filename,
 			return (0);
 	}
 
-	if ((fstring = malloc(strlen(filename) + 1)) == NULL) {
+	if ((fstring = strdup(filename)) == NULL) {
 		warnx("malloc() failed - %s", strerror(errno));
 		return (-1);
 	}
@@ -264,7 +264,6 @@ add_filename(struct snmp_toolinfo *snmptoolctx, const char *filename,
 
 	if (cut != NULL)
 		asn_append_oid(&(entry->cut), cut);
-	strlcpy(fstring, filename, strlen(filename) + 1);
 	entry->name = fstring;
 	entry->done = done;
 	SLIST_INSERT_HEAD(&snmptoolctx->filelist, entry, link);
@@ -1060,7 +1059,7 @@ snmp_oid2asn_oid(struct snmp_toolinfo *snmptoolctx, char *str,
     struct asn_oid *oid)
 {
 	int32_t i;
-	char string[MAXSTR], *endptr;
+	char string[MAXSTR + 1], *endptr;
 	struct snmp_object obj;
 
 	for (i = 0; i < MAXSTR; i++)
@@ -1076,7 +1075,6 @@ snmp_oid2asn_oid(struct snmp_toolinfo *snmptoolctx, char *str,
 			return (NULL);
 	} else {
 		strlcpy(string, str, i + 1);
-		string[i] = '\0';
 		if (snmp_lookup_enumoid(snmptoolctx, &obj, string) < 0) {
 			warnx("Unknown string - %s", string);
 			return (NULL);
