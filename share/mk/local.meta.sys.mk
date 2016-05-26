942c9146650bff6a00701e555a6c21c326a95780
bdrewery <bdrewery@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-05-26 23:20:20 +0000

    DIRDEPS_BUILD: Don't show finished stats, which confuses crunchgen(1).
    It would show 'make error:' lines for each auto object directory created.
    
    Sponsored by:	EMC / Isilon Storage Division
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@300801 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/local.meta.sys.mk
+++ share/mk/local.meta.sys.mk
@@ -214,7 +214,7 @@ CSU_DIR := ${CSU_DIR.${MACHINE_ARCH}}
 .if !empty(TIME_STAMP)
 TRACER= ${TIME_STAMP} ${:U}
 .endif
-.if !defined(_RECURSING_PROGS)
+.if !defined(_RECURSING_PROGS) && !defined(_RECURSING_CRUNCH)
 WITH_META_STATS= t
 .endif
 
