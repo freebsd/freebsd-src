b628bdccce4f937fa194d7211cd78e59df6786f7
Andriy Voskoboinyk <avos@FreeBSD.org>
2016-05-26 13:14:08 +0000

    ifconfig: set by default FCC regulatory domain for wireless interfaces.
    Change default regulatory domain from DEBUG (no limitations;
    exposes all device channels) to FCC; as a result, newly created wireless
    interface with default settings will have less chances to violate
    country-specific regulations.
    
    This change will not affect drivers with pre-initialized regdomain
    structure (currentry ath(4) and mwl(4)); in that case, the default
    channel list must correspond to the default regdomain / country setting.
    
    You can switch to another regdomain / country via corresponding
    ifconfig(8) options; the driver must implement ic_getradiocaps()
    method to restore full channel list.
    
    Full country / regdomain list may be obtained via
    'ifconfig <iface> list countries' command.
    
    Example: change country to Germany:
    ifconfig wlan0 down	# all wlans on the device must be down
    ifconfig wlan0 country DE
    ifconfig wlan0 up
    # wpa_supplicant(8), dhclient(8) etc
    
    At the creation time:
    ifconfig wlan0 create wlandev wpi0 country DE
    
    To make changes permanent add the following line to the rc.conf(5):
    create_args_wlan0="country DE"
    
    Tested with
     - Intel 3945BG (wpi(4)).
     - WUSB54GC (rum(4)).
    
    Reviewed by:	adrian
    Relnotes:	yes
    Differential Revision:	https://reviews.freebsd.org/D6228

--- sys/net80211/ieee80211_regdomain.c
+++ sys/net80211/ieee80211_regdomain.c
@@ -69,10 +69,7 @@ ieee80211_regdomain_attach(struct ieee80211com *ic)
 {
 	if (ic->ic_regdomain.regdomain == 0 &&
 	    ic->ic_regdomain.country == CTRY_DEFAULT) {
-		ic->ic_regdomain.country = CTRY_UNITED_STATES;	/* XXX */
 		ic->ic_regdomain.location = ' ';		/* both */
-		ic->ic_regdomain.isocc[0] = 'U';		/* XXX */
-		ic->ic_regdomain.isocc[1] = 'S';		/* XXX */
 		/* NB: driver calls ieee80211_init_channels or similar */
 	}
 	ic->ic_getradiocaps = null_getradiocaps;
