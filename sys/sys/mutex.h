9de2b155660fa4a5ea09cca2d43d0c8d5d6d4b57
mjg <mjg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-06-01 18:32:20 +0000

    Microoptimize locking primitives by avoiding unnecessary atomic ops.
    Inline version of primitives do an atomic op and if it fails they fallback to
    actual primitives, which immediately retry the atomic op.
    
    The obvious optimisation is to check if the lock is free and only then proceed
    to do an atomic op.
    
    Reviewed by:	jhb, vangyzen
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@301157 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/mutex.h
+++ sys/sys/mutex.h
@@ -185,7 +185,7 @@ void	thread_lock_flags_(struct thread *, int, const char *, int);
 #define __mtx_lock(mp, tid, opts, file, line) do {			\
 	uintptr_t _tid = (uintptr_t)(tid);				\
 									\
-	if (!_mtx_obtain_lock((mp), _tid))				\
+	if (((mp)->mtx_lock != MTX_UNOWNED || !_mtx_obtain_lock((mp), _tid)))\
 		_mtx_lock_sleep((mp), _tid, (opts), (file), (line));	\
 	else								\
 		LOCKSTAT_PROFILE_OBTAIN_LOCK_SUCCESS(adaptive__acquire,	\
@@ -203,7 +203,7 @@ void	thread_lock_flags_(struct thread *, int, const char *, int);
 	uintptr_t _tid = (uintptr_t)(tid);				\
 									\
 	spinlock_enter();						\
-	if (!_mtx_obtain_lock((mp), _tid)) {				\
+	if (((mp)->mtx_lock != MTX_UNOWNED || !_mtx_obtain_lock((mp), _tid))) {\
 		if ((mp)->mtx_lock == _tid)				\
 			(mp)->mtx_recurse++;				\
 		else							\
@@ -232,7 +232,7 @@ void	thread_lock_flags_(struct thread *, int, const char *, int);
 									\
 	if ((mp)->mtx_recurse == 0)					\
 		LOCKSTAT_PROFILE_RELEASE_LOCK(adaptive__release, mp);	\
-	if (!_mtx_release_lock((mp), _tid))				\
+	if ((mp)->mtx_lock != _tid || !_mtx_release_lock((mp), _tid))	\
 		_mtx_unlock_sleep((mp), (opts), (file), (line));	\
 } while (0)
 
