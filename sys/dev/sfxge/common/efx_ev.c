5c4c3d9285e98f3b6ee765973aa1617f9029bc72
Andrew Rybchenko <arybchik@FreeBSD.org>
2016-06-01 14:03:07 +0000

    sfxge(4): set moderation in efx_ev_qcreate
    This simplifies setting an initial interrupt moderation value, and
    avoids most calls to evx_ev_qmoderate from contexts where MCDI is
    not allowed (MCDI is need for an EVQ timer workaround in a later patch).
    
    Submitted by:   Andy Moreton <amoreton at solarflare.com>
    Reviewed by:    gnn
    Sponsored by:   Solarflare Communications, Inc.
    MFC after:      1 week
    Differential Revision:  https://reviews.freebsd.org/D6673

--- sys/dev/sfxge/common/efx_ev.c
+++ sys/dev/sfxge/common/efx_ev.c
@@ -70,6 +70,7 @@ siena_ev_qcreate(
 	__in		efsys_mem_t *esmp,
 	__in		size_t n,
 	__in		uint32_t id,
+	__in		uint32_t us,
 	__in		efx_evq_t *eep);
 
 static			void
@@ -226,6 +227,7 @@ efx_ev_qcreate(
 	__in		efsys_mem_t *esmp,
 	__in		size_t n,
 	__in		uint32_t id,
+	__in		uint32_t us,
 	__deref_out	efx_evq_t **eepp)
 {
 	const efx_ev_ops_t *eevop = enp->en_eevop;
@@ -262,7 +264,7 @@ efx_ev_qcreate(
 	enp->en_ev_qcount++;
 	*eepp = eep;
 
-	if ((rc = eevop->eevo_qcreate(enp, index, esmp, n, id, eep)) != 0)
+	if ((rc = eevop->eevo_qcreate(enp, index, esmp, n, id, us, eep)) != 0)
 		goto fail2;
 
 	return (0);
@@ -1257,6 +1259,7 @@ siena_ev_qcreate(
 	__in		efsys_mem_t *esmp,
 	__in		size_t n,
 	__in		uint32_t id,
+	__in		uint32_t us,
 	__in		efx_evq_t *eep)
 {
 	efx_nic_cfg_t *encp = &(enp->en_nic_cfg);
@@ -1311,6 +1314,9 @@ siena_ev_qcreate(
 	    FRF_AZ_EVQ_BUF_BASE_ID, id);
 
 	EFX_BAR_TBL_WRITEO(enp, FR_AZ_EVQ_PTR_TBL, index, &oword, B_TRUE);
+
+	/* Set initial interrupt moderation */
+	siena_ev_qmoderate(eep, us);
 
 	return (0);
 
