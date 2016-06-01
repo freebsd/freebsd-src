fbc35f71fe2703629adefbf3359ab1a6721815a0
Don Lewis <truckman@FreeBSD.org>
2016-06-01 17:37:16 +0000

    MFC r300660
    Fix 1016718 Resource leak.
    
    Don't leak a file descriptor if fchdir() fails.
    
    Reported by:	Coverity
    CID:		1016718

--- lib/libc/gen/fts-compat.c
+++ lib/libc/gen/fts-compat.c
@@ -586,8 +586,10 @@ __fts_children_44bsd(sp, instr)
 	if ((fd = _open(".", O_RDONLY | O_CLOEXEC, 0)) < 0)
 		return (NULL);
 	sp->fts_child = fts_build(sp, instr);
-	if (fchdir(fd))
+	if (fchdir(fd)) {
+		(void)_close(fd);
 		return (NULL);
+	}
 	(void)_close(fd);
 	return (sp->fts_child);
 }
