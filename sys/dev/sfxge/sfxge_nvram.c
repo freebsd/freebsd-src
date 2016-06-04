3453b963f23bdf67f476b8431600cd0e4cfc78c8
Andrew Rybchenko <arybchik@FreeBSD.org>
2016-06-04 17:08:34 +0000

    MFC r300607
    sfxge(4): cleanup: update copyright to 2016
    
    Sponsored by:   Solarflare Communications, Inc.

--- sys/dev/sfxge/sfxge_nvram.c
+++ sys/dev/sfxge/sfxge_nvram.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2010-2015 Solarflare Communications, Inc.
+ * Copyright (c) 2010-2016 Solarflare Communications, Inc.
  * All rights reserved.
  *
  * This software was developed in part by OKTET Labs Ltd. under contract for
