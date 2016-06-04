d2b834cc3411d084ca28c08a897a94ba74a37e49
arybchik <arybchik@FreeBSD.org>
2016-06-04 15:57:01 +0000

    MFC r299722
    sfxge(4): remove obsolete EFSYS_OPT_PHY_PROPS option and APIs
    
    Submitted by:   Andy Moreton <amoreton at solarflare.com>
    Sponsored by:   Solarflare Communications, Inc.

--- sys/dev/sfxge/common/efx_phy.c
+++ sys/dev/sfxge/common/efx_phy.c
@@ -423,43 +423,6 @@ efx_phy_stats_update(
 
 #endif	/* EFSYS_OPT_PHY_STATS */
 
-#if EFSYS_OPT_PHY_PROPS
-
-#if EFSYS_OPT_NAMES
-		const char *
-efx_phy_prop_name(
-	__in	efx_nic_t *enp,
-	__in	unsigned int id)
-{
-	_NOTE(ARGUNUSED(enp, id))
-
-	return (NULL);
-}
-#endif	/* EFSYS_OPT_NAMES */
-
-	__checkReturn	efx_rc_t
-efx_phy_prop_get(
-	__in		efx_nic_t *enp,
-	__in		unsigned int id,
-	__in		uint32_t flags,
-	__out		uint32_t *valp)
-{
-	_NOTE(ARGUNUSED(enp, id, flags, valp))
-
-	return (ENOTSUP);
-}
-
-	__checkReturn	efx_rc_t
-efx_phy_prop_set(
-	__in		efx_nic_t *enp,
-	__in		unsigned int id,
-	__in		uint32_t val)
-{
-	_NOTE(ARGUNUSED(enp, id, val))
-
-	return (ENOTSUP);
-}
-#endif	/* EFSYS_OPT_PHY_STATS */
 
 #if EFSYS_OPT_BIST
 
