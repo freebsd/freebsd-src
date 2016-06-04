7ba7852fd7888bc7ed72915038268ccd0f5810b5
Landon J. Fuller <landonf@FreeBSD.org>
2016-06-04 19:53:47 +0000

    Switch mips/sentry5 to bhnd(4), and unify with mips/broadcom
    Now that bhnd(4) provides feature parity with the previous siba/mips
    implementation, we can switch sentry5 over and begin lifting common
    support code out for use across bhnd(4) embedded targets.
    
    Changes:
    
    - Fixed enumeration of siba(4) per-core address maps, required for
      discovery of memory mapped chipc flash region on siba(4) devices.
    - Simplified bhnd kernel configuration (dropped 'bhndbus' option).
    - Replaced files.broadcom's direct file references with their
      corresponding standard kernel options.
    - Lifted out common bcma/siba nexus support, inheriting from the new
      generic bhnd_nexus driver.
    - Dropped now-unused sentry5 siba code.
    - Re-integrated BCM into the universe build now that it actually compiles.
    
    Approved by:	adrian (mentor)
    Differential Revision:	https://reviews.freebsd.org/D6712

--- sys/dev/bhnd/bcma/bcma_nexus.c
+++ sys/dev/bhnd/bcma/bcma_nexus.c
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2016 Michael Zhilin <mizhka@gmail.com>
+ * Copyright (c) 2015-2016 Landon Fuller <landon@freebsd.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,6 +26,8 @@
  * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGES.
+ * 
+ * $FreeBSD$
  */
 
 #include <sys/cdefs.h>
@@ -34,78 +37,102 @@ __FBSDID("$FreeBSD$");
 #include <sys/kernel.h>
 #include <sys/bus.h>
 #include <sys/module.h>
-#include <sys/errno.h>
 
 #include <machine/bus.h>
+#include <sys/rman.h>
 #include <machine/resource.h>
 
-#include <dev/bhnd/bhnd.h>
+#include <dev/bhnd/bhnd_ids.h>
+#include <dev/bhnd/bhnd_nexusvar.h>
+#include <dev/bhnd/cores/chipc/chipcreg.h>
 
-#include "bhnd_bus_if.h"
 #include "bcmavar.h"
 #include "bcma_eromreg.h"
 
-#define	BCMA_NEXUS_EROM_RID	10
+/*
+ * Supports bcma(4) attachment to a nexus bus.
+ */
+
+static int	bcma_nexus_attach(device_t);
+static int	bcma_nexus_probe(device_t);
+
+struct bcma_nexus_softc {
+	struct bcma_softc		parent_sc;
+	struct bhnd_chipid		bcma_cid;
+};
 
 static int
 bcma_nexus_probe(device_t dev)
 {
-	const struct bhnd_chipid *cid = BHND_BUS_GET_CHIPID(device_get_parent(dev), dev);
+	struct bcma_nexus_softc	*sc;
+	int			 error;
 
-	/* Check bus type */
-	if (cid->chip_type != BHND_CHIPTYPE_BCMA)
+	sc = device_get_softc(dev);
+
+	/* Read the ChipCommon info using the hints the kernel
+	 * was compiled with. */
+	if ((error = bhnd_nexus_read_chipid(dev, &sc->bcma_cid)))
+		return (error);
+
+	if (sc->bcma_cid.chip_type != BHND_CHIPTYPE_BCMA)
 		return (ENXIO);
 
-	/* Delegate to default probe implementation */
-	return (bcma_probe(dev));
+	if ((error = bcma_probe(dev)) > 0) {
+		device_printf(dev, "error %d in probe\n", error);
+		return (error);
+	}
+
+	return (0);
 }
 
 static int
 bcma_nexus_attach(device_t dev)
 {
-	int 		 erom_rid;
-	int 		 error;
-	struct resource	*erom_res;
-	const struct bhnd_chipid *cid = BHND_BUS_GET_CHIPID(device_get_parent(dev), dev);
+	struct bcma_nexus_softc	*sc;
+	struct resource		*erom_res;
+	int			 error;
+	int			 rid;
 
-  	erom_rid = BCMA_NEXUS_EROM_RID;
- 	error = bus_set_resource(dev, SYS_RES_MEMORY, erom_rid, cid->enum_addr, BCMA_EROM_TABLE_SIZE);
- 	if (error != 0) {
- 		BHND_ERROR_DEV(dev, "failed to set EROM resource");
- 		return (error);
- 	}
+	sc = device_get_softc(dev);
 
- 	/* Map the EROM resource and enumerate our children. */
- 	BHND_DEBUG_DEV(dev, "erom enum address: %jx", cid->enum_addr);
- 	erom_res = bus_alloc_resource_any(dev, SYS_RES_MEMORY, &erom_rid, RF_ACTIVE);
- 	if (erom_res == NULL) {
- 		BHND_ERROR_DEV(dev, "failed to allocate EROM resource");
- 		return (ENXIO);
- 	}
+	/* Map the EROM resource and enumerate the bus. */
+	rid = 0;
+	erom_res = bus_alloc_resource(dev, SYS_RES_MEMORY, &rid,
+	    sc->bcma_cid.enum_addr, 
+	    sc->bcma_cid.enum_addr + BCMA_EROM_TABLE_SIZE,
+	    BCMA_EROM_TABLE_SIZE, RF_ACTIVE);
+	if (erom_res == NULL) {
+		device_printf(dev, "failed to allocate EROM resource\n");
+		return (ENXIO);
+	}
 
- 	BHND_DEBUG_DEV(dev, "erom scanning start address: %p", rman_get_virtual(erom_res));
- 	error = bcma_add_children(dev, erom_res, BCMA_EROM_TABLE_START);
+	error = bcma_add_children(dev, erom_res, BCMA_EROM_TABLE_START);
+	bus_release_resource(dev, SYS_RES_MEMORY, rid, erom_res);
 
- 	/* Clean up */
- 	bus_release_resource(dev, SYS_RES_MEMORY, erom_rid, erom_res);
- 	if (error)
- 		return (error);
+	if (error)
+		return (error);
 
- 	/* Call our superclass' implementation */
- 	return (bcma_attach(dev));
+	return (bcma_attach(dev));
+}
+
+static const struct bhnd_chipid *
+bcma_nexus_get_chipid(device_t dev, device_t child) {
+	struct bcma_nexus_softc	*sc = device_get_softc(dev);
+	return (&sc->bcma_cid);
 }
 
 static device_method_t bcma_nexus_methods[] = {
 	/* Device interface */
 	DEVMETHOD(device_probe,			bcma_nexus_probe),
 	DEVMETHOD(device_attach,		bcma_nexus_attach),
+
+	/* bhnd interface */
+	DEVMETHOD(bhnd_bus_get_chipid,		bcma_nexus_get_chipid),
+
 	DEVMETHOD_END
 };
 
-DEFINE_CLASS_1(bhnd, bcma_nexus_driver, bcma_nexus_methods, sizeof(struct bcma_softc), bcma_driver);
-EARLY_DRIVER_MODULE(bcma_nexus, bhnd_soc, bcma_nexus_driver, bhnd_devclass,
-    NULL, NULL, BUS_PASS_BUS + BUS_PASS_ORDER_MIDDLE);
+DEFINE_CLASS_2(bhnd, bcma_nexus_driver, bcma_nexus_methods,
+    sizeof(struct bcma_nexus_softc), bhnd_nexus_driver, bcma_driver);
 
-MODULE_VERSION(bcma_nexus, 1);
-MODULE_DEPEND(bcma_nexus, bcma, 1, 1, 1);
-MODULE_DEPEND(bcma_nexus, bhnd_soc, 1, 1, 1);
+DRIVER_MODULE(bcma_nexus, nexus, bcma_nexus_driver, bhnd_devclass, 0, 0);
