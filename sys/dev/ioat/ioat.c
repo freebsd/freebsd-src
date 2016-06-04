f450ccbc435defb1d7eccce9b4df4fc13ae070bc
cem <cem@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-06-04 04:14:06 +0000

    ioat(4): Always log capabilities on attach
    Different, relatively recent Intel Xeon hardware support radically different
    features.  E.g., BDX support CRC32 while BDX-DE does not.
    
    Reviewed by:	rpokala (spiritually)
    Sponsored by:	EMC / Isilon Storage Division
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@301300 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ioat/ioat.c
+++ sys/dev/ioat/ioat.c
@@ -407,7 +407,7 @@ ioat3_attach(device_t device)
 	ioat = DEVICE2SOFTC(device);
 	ioat->capabilities = ioat_read_dmacapability(ioat);
 
-	ioat_log_message(1, "Capabilities: %b\n", (int)ioat->capabilities,
+	ioat_log_message(0, "Capabilities: %b\n", (int)ioat->capabilities,
 	    IOAT_DMACAP_STR);
 
 	xfercap = ioat_read_xfercap(ioat);
