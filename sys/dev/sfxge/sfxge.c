28894f198e105f007dcd75e9e54b841efdda9b59
arybchik <arybchik@FreeBSD.org>
2016-06-04 17:12:28 +0000

    MFC r300609
    sfxge(4): enable Medford support
    
    Reviewed by:    gnn
    Sponsored by:   Solarflare Communications, Inc.

--- sys/dev/sfxge/sfxge.c
+++ sys/dev/sfxge/sfxge.c
@@ -1174,6 +1174,11 @@ sfxge_probe(device_t dev)
 		return (0);
 	}
 
+	if (family == EFX_FAMILY_MEDFORD) {
+		device_set_desc(dev, "Solarflare SFC9200 family");
+		return (0);
+	}
+
 	DBGPRINT(dev, "impossible controller family %d", family);
 	return (ENXIO);
 }
