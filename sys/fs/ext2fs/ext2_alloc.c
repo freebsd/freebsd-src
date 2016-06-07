43ce40e891e108f7e71b19e47705c8a8982ddee0
Pedro F. Giffuni <pfg@FreeBSD.org>
2016-06-07 14:37:43 +0000

    ext2fs: cleanup generation number management.
    Ext2/3/4 manages generation numbers differently than UFS so adopt
    some rules that should work well. When allocating a new inode,
    make sure we generate a "good" random value specifically avoiding
    zero.
    
    Don't interfere with the numbers that are already generated in
    the filesystem: ext2fs doesn't have the backwards compatibility
    issues  where there were no generation numbers.
    
    Reviewed by:	kevlo
    MFC after:	1 week

--- sys/fs/ext2fs/ext2_alloc.c
+++ sys/fs/ext2fs/ext2_alloc.c
@@ -407,9 +407,11 @@ ext2_valloc(struct vnode *pvp, int mode, struct ucred *cred, struct vnode **vpp)
 
 	/*
 	 * Set up a new generation number for this inode.
+	 * Avoid zero values.
 	 */
-	while (ip->i_gen == 0 || ++ip->i_gen == 0)
+	do {
 		ip->i_gen = arc4random();
+	} while ( ip->i_gen == 0);
 
 	vfs_timestamp(&ts);
 	ip->i_birthtime = ts.tv_sec;
