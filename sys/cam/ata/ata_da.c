c6fd5fa14f248a0eec331edd13b649e25142f924
cem <cem@FreeBSD.org>
2016-06-07 20:33:55 +0000

    Fix memory leaks in (a|)daregister introduced in r298002
    In the case where cam_iosched_init() fails, the ada and da softcs were leaked.
    Instead, free them.
    
    Reported by:	Coverity
    CID:		1356039
    Sponsored by:	EMC / Isilon Storage Division

--- sys/cam/ata/ata_da.c
+++ sys/cam/ata/ata_da.c
@@ -1672,6 +1672,7 @@ adaregister(struct cam_periph *periph, void *arg)
 	if (cam_iosched_init(&softc->cam_iosched, periph) != 0) {
 		printf("adaregister: Unable to probe new device. "
 		       "Unable to allocate iosched memory\n");
+		free(softc, M_DEVBUF);
 		return(CAM_REQ_CMP_ERR);
 	}
 
