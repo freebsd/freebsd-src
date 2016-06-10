7d78de6af2c4648fc56357068357eac772960db7
Enji Cooper <ngie@FreeBSD.org>
2016-06-10 14:40:41 +0000

    MFC r299491: r299491 (by cem):
    route6d(8): Fix potential double-free
    
    In the case that the subsequent sysctl(3) call failed, 'buf' could be free(3)ed
    repeatedly.  It isn't clear to me that that case is possible, but be clear and
    do the right thing in case it is.
    
    CID:		272537

--- usr.sbin/route6d/route6d.c
+++ usr.sbin/route6d/route6d.c
@@ -2602,8 +2602,10 @@ krtread(int again)
 			sleep(1);
 		retry++;
 		errmsg = NULL;
-		if (buf)
+		if (buf) {
 			free(buf);
+			buf = NULL;
+		}
 		if (sysctl(mib, 6, NULL, &msize, NULL, 0) < 0) {
 			errmsg = "sysctl estimate";
 			continue;
