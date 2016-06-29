7f571f869fd3eee62ef59298a19088dde35b70b7
hselasky <hselasky@FreeBSD.org>
2016-06-29 10:29:52 +0000

    MFC r302139: Use correct Q-counter output array.
    Sponsored by:	Mellanox Technologies

--- sys/dev/mlx5/mlx5_core/mlx5_vport.c
+++ sys/dev/mlx5/mlx5_core/mlx5_vport.c
@@ -71,7 +71,7 @@ static int mlx5_query_nic_vport_context(struct mlx5_core_dev *mdev, u32 vport,
 int mlx5_vport_alloc_q_counter(struct mlx5_core_dev *mdev, int *counter_set_id)
 {
 	u32 in[MLX5_ST_SZ_DW(alloc_q_counter_in)];
-	u32 out[MLX5_ST_SZ_DW(alloc_q_counter_in)];
+	u32 out[MLX5_ST_SZ_DW(alloc_q_counter_out)];
 	int err;
 
 	memset(in, 0, sizeof(in));
