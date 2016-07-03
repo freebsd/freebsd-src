3e0cca201457984cf6e5f7aa71eb40fac8496a86
Glen Barber <gjb@FreeBSD.org>
2016-07-03 00:31:33 +0000

    Limit VOLUME_LABEL (first argument to the mkisoimages scripts) to 32 characters.
    This is a direct commit to stable/10, as UEFI support natively
    exists for 11.0-CURRENT and unsupported in 9-STABLE.
    
    Submitted by:	Rick Miller
    PR:		210463
    Sponsored by:	The FreeBSD Foundation

--- release/amd64/mkisoimages-uefi.sh
+++ release/amd64/mkisoimages-uefi.sh
@@ -50,7 +50,7 @@ if [ $# -lt 3 ]; then
 	exit 1
 fi
 
-LABEL=`echo $1 | tr '[:lower:]' '[:upper:]'`; shift
+LABEL=`echo $1 | tr '[:lower:]' '[:upper:]' | cut -c 1-31`; shift
 NAME=$1; shift
 
 publisher="The FreeBSD Project.  http://www.FreeBSD.org/"
