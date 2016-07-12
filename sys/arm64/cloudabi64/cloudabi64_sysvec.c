32f3c06b7023be894c5ff3581f5c40ec8813f953
Ed Schouten <ed@FreeBSD.org>
2016-07-12 06:25:28 +0000

    MFC r302448:
      Don't forget to set sa->narg for CloudABI system calls.
    
      It turns out that this value is not used within the system call code
      under normal conditions, except when using tracing tools like ktrace.
      If we forget to set this value, it is set to random garbage. This may
      cause ktrace to hang indefinitely, making it impossible to kill.
    
    Approved by: re@
    Reported by: Michael Plass
    PR: 210800

--- sys/arm64/cloudabi64/cloudabi64_sysvec.c
+++ sys/arm64/cloudabi64/cloudabi64_sysvec.c
@@ -77,6 +77,7 @@ cloudabi64_fetch_syscall_args(struct thread *td, struct syscall_args *sa)
 	if (sa->code >= CLOUDABI64_SYS_MAXSYSCALL)
 		return (ENOSYS);
 	sa->callp = &cloudabi64_sysent[sa->code];
+	sa->narg = sa->callp->sy_narg;
 
 	/* Fetch system call arguments. */
 	for (i = 0; i < MAXARGS; i++)
