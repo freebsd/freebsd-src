d74e22ed4a65cd38f54b453ad6f70b0cb4c6fe92
Sepherosa Ziehau <sephe@FreeBSD.org>
2016-07-13 04:31:08 +0000

    hyperv/vmbus: Add flags field into hv_vmbus_channel for MNF indication
    This prepares to remove the unnecessary offer message embedding in
    hv_vmbus_channel.
    
    MFC after:	1 week
    Sponsored by:	Microsoft OSTC
    Differential Revision:	https://reviews.freebsd.org/D7019

--- sys/dev/hyperv/include/hyperv.h
+++ sys/dev/hyperv/include/hyperv.h
@@ -538,6 +538,7 @@ typedef struct hv_vmbus_channel {
 	struct vmbus_softc		*vmbus_sc;
 	hv_vmbus_channel_state		state;
 	hv_vmbus_channel_offer_channel	offer_msg;
+	uint32_t			ch_flags;	/* VMBUS_CHAN_FLAG_ */
 	uint32_t			ch_id;		/* channel id */
 	/*
 	 * These are based on the offer_msg.monitor_id.
@@ -635,6 +636,8 @@ typedef struct hv_vmbus_channel {
 } hv_vmbus_channel;
 
 #define HV_VMBUS_CHAN_ISPRIMARY(chan)	((chan)->primary_channel == NULL)
+
+#define VMBUS_CHAN_FLAG_HASMNF		0x0001
 
 static inline void
 hv_set_channel_read_state(hv_vmbus_channel* channel, boolean_t state)
