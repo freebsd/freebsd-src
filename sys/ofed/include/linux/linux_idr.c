4d4db1b5acc7df1cab14dff0c48e0833709dca54
Mark Johnston <markj@FreeBSD.org>
2016-07-16 02:57:37 +0000

    MFC r301877: Add a missing error check for a malloc() call in idr_get().

--- sys/ofed/include/linux/linux_idr.c
+++ sys/ofed/include/linux/linux_idr.c
@@ -267,7 +267,8 @@ idr_get(struct idr *idr)
 		return (il);
 	}
 	il = malloc(sizeof(*il), M_IDR, M_ZERO | M_NOWAIT);
-	bitmap_fill(&il->bitmap, IDR_SIZE);
+	if (il != NULL)
+		bitmap_fill(&il->bitmap, IDR_SIZE);
 	return (il);
 }
 
