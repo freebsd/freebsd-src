fe49fb6bb11fb19d3e09b200e02e1ba6c2a72050
Stephen McConnell <slm@FreeBSD.org>
2016-07-19 16:46:27 +0000

    MFC r302673 Use real values to calculate Max I/O size instead of guessing.
    Reviewed by:	ken, scottl
    Approved by:	re(gjb), ken, scottl, ambrisko (mentors)
    Differential Revision:	https://reviews.freebsd.org/D7043

--- sys/dev/mpr/mpr_sas.c
+++ sys/dev/mpr/mpr_sas.c
@@ -971,6 +971,8 @@ mprsas_action(struct cam_sim *sim, union ccb *ccb)
 	case XPT_PATH_INQ:
 	{
 		struct ccb_pathinq *cpi = &ccb->cpi;
+		struct mpr_softc *sc = sassc->sc;
+		uint8_t sges_per_frame;
 
 		cpi->version_num = 1;
 		cpi->hba_inquiry = PI_SDTR_ABLE|PI_TAG_ABLE|PI_WIDE_16;
@@ -999,13 +1001,23 @@ mprsas_action(struct cam_sim *sim, union ccb *ccb)
 		cpi->transport_version = 0;
 		cpi->protocol = PROTO_SCSI;
 		cpi->protocol_version = SCSI_REV_SPC;
-#if __FreeBSD_version >= 800001
+
 		/*
-		 * XXXSLM-probably need to base this number on max SGL's and
-		 * page size.
+		 * Max IO Size is Page Size * the following:
+		 * ((SGEs per frame - 1 for chain element) *
+		 * Max Chain Depth) + 1 for no chain needed in last frame
+		 *
+		 * If user suggests a Max IO size to use, use the smaller of the
+		 * user's value and the calculated value as long as the user's
+		 * value is larger than 0. The user's value is in pages.
 		 */
-		cpi->maxio = 256 * 1024;
-#endif
+		sges_per_frame = (sc->chain_frame_size /
+		    sizeof(MPI2_IEEE_SGE_SIMPLE64)) - 1;
+		cpi->maxio = (sges_per_frame * sc->facts->MaxChainDepth) + 1;
+		cpi->maxio *= PAGE_SIZE;
+		if ((sc->max_io_pages > 0) && (sc->max_io_pages * PAGE_SIZE <
+		    cpi->maxio))
+			cpi->maxio = sc->max_io_pages * PAGE_SIZE;
 		mprsas_set_ccbstatus(ccb, CAM_REQ_CMP);
 		break;
 	}
--- sys/dev/mpr/mprvar.h
+++ sys/dev/mpr/mprvar.h
@@ -33,7 +33,7 @@
 #ifndef _MPRVAR_H
 #define _MPRVAR_H
 
-#define MPR_DRIVER_VERSION	"13.00.00.00-fbsd"
+#define MPR_DRIVER_VERSION	"13.01.00.00-fbsd"
 
 #define MPR_DB_MAX_WAIT		2500
 
@@ -41,6 +41,7 @@
 #define MPR_EVT_REPLY_FRAMES	32
 #define MPR_REPLY_FRAMES	MPR_REQ_FRAMES
 #define MPR_CHAIN_FRAMES	2048
+#define MPR_MAXIO_PAGES		(-1)
 #define MPR_SENSE_LEN		SSD_FULL_SIZE
 #define MPR_MSI_COUNT		1
 #define MPR_SGE64_SIZE		12
@@ -264,6 +265,7 @@ struct mpr_softc {
 	int				io_cmds_highwater;
 	int				chain_free;
 	int				max_chains;
+	int				max_io_pages;
 	int				chain_free_lowwater;
 	uint32_t			chain_frame_size;
 	uint16_t			chain_seg_size;
