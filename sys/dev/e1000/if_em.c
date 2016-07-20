ca5dbad2e8a75772a3acfc9b4f73019df26ea56e
Sean Bruno <sbruno@FreeBSD.org>
2016-07-20 22:28:26 +0000

    Fix em(4) build after r303107
    Reported by:	Jason Wolf <j@nitrology.com>

--- sys/dev/e1000/if_em.c
+++ sys/dev/e1000/if_em.c
@@ -1220,7 +1220,7 @@ em_ioctl(struct ifnet *ifp, u_long command, caddr_t data)
 		ifp->if_mtu = ifr->ifr_mtu;
 		adapter->hw.mac.max_frame_size =
 		    ifp->if_mtu + ETHER_HDR_LEN + ETHER_CRC_LEN;
-		if (if_getdrvflags(ifp) & IFF_DRV_RUNNING)
+		if (ifp->if_drv_flags & IFF_DRV_RUNNING)
 			em_init_locked(adapter);
 		EM_CORE_UNLOCK(adapter);
 		break;
