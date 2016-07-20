e5e2d96e1386eb0593c458f0b5fda1d2a98cb228
sbruno <sbruno@FreeBSD.org>
2016-07-20 19:21:11 +0000

    Remove uneeded parens.
    MFC after:	1 day

--- sys/dev/e1000/if_igb.c
+++ sys/dev/e1000/if_igb.c
@@ -1106,7 +1106,7 @@ igb_ioctl(struct ifnet *ifp, u_long command, caddr_t data)
 		ifp->if_mtu = ifr->ifr_mtu;
 		adapter->max_frame_size =
 		    ifp->if_mtu + ETHER_HDR_LEN + ETHER_CRC_LEN;
-		if ((ifp->if_drv_flags & IFF_DRV_RUNNING))
+		if (ifp->if_drv_flags & IFF_DRV_RUNNING)
 			igb_init_locked(adapter);
 		IGB_CORE_UNLOCK(adapter);
 		break;
