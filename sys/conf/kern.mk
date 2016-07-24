f080c640fa6898958b6c027c22d24e9343915bc5
br <br@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-07-24 18:04:12 +0000

    Fix style.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@303271 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -51,7 +51,7 @@ CWARNEXTRA?=	-Wno-error=inline -Wno-error=enum-compare -Wno-error=unused-but-set
 		-Wno-error=cast-qual -Wno-error=sequence-point -Wno-error=attributes \
 		-Wno-error=strict-overflow -Wno-error=overflow
 .if ${COMPILER_VERSION} >= 60100
-CWARNEXTRA+=	-Wno-error=nonnull-compare -Wno-error=shift-overflow=
+CWARNEXTRA+=	-Wno-error=nonnull-compare -Wno-error=shift-overflow
 .endif
 .else
 # For gcc 4.2, eliminate the too-often-wrong warnings about uninitialized vars.
