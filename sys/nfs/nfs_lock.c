9dfaeead566bf1efbe78e0f7a9d97ec863cd822e
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-07-27 11:08:59 +0000

    Hide the boottime and bootimebin globals, provide the getboottime(9) and getboottimebin(9) KPI. Change consumers of boottime to use the KPI.  The variables were renamed to avoid shadowing issues with local variables of the same name.
    Issue is that boottime* should be adjusted from tc_windup(), which
    requires them to be members of the timehands structure.  As a
    preparation, this commit only introduces the interface.
    
    Some uses of boottime were found doubtful, e.g. NLM uses boottime to
    identify the system boot instance.  Arguably the identity should not
    change on the leap second adjustment, but the commit is about the
    timekeeping code and the consumers were kept bug-to-bug compatible.
    
    Tested by:	pho (as part of the bigger patch)
    Reviewed by:	jhb (same)
    Discussed with:	bde
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 month
    X-Differential revision:	https://reviews.freebsd.org/D7302
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@303382 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/nfs/nfs_lock.c
+++ sys/nfs/nfs_lock.c
@@ -241,6 +241,7 @@ nfs_dolock(struct vop_advlock_args *ap)
 	struct flock *fl;
 	struct proc *p;
 	struct nfsmount *nmp;
+	struct timeval boottime;
 
 	td = curthread;
 	p = td->td_proc;
@@ -284,6 +285,7 @@ nfs_dolock(struct vop_advlock_args *ap)
 		p->p_nlminfo = malloc(sizeof(struct nlminfo),
 		    M_NLMINFO, M_WAITOK | M_ZERO);
 		p->p_nlminfo->pid_start = p->p_stats->p_start;
+		getboottime(&boottime);
 		timevaladd(&p->p_nlminfo->pid_start, &boottime);
 	}
 	msg.lm_msg_ident.pid_start = p->p_nlminfo->pid_start;
