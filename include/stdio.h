b27c968b4a52a53410d95a11ae44367c885c0ac3
bapt <bapt@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-07-30 01:00:16 +0000

    Remove _WITH_GETLINE and _WITH_DPRINTF guards
    When adding getline(3) and dprintf(3) into libc, those guards were added
    to prevent breaking too many ports.
    
    7 years later the ports tree have been fixed, it is time to remove this
    FreeBSDism
    
    While here remove the extra parenthesis surrounding dprintf(3)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@303524 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- include/stdio.h
+++ include/stdio.h
@@ -357,44 +357,8 @@ ssize_t	 getdelim(char ** __restrict, size_t * __restrict, int,
 FILE	*open_memstream(char **, size_t *);
 int	 renameat(int, const char *, int, const char *);
 int	 vdprintf(int, const char * __restrict, __va_list);
-
-/*
- * Every programmer and his dog wrote functions called getline() and dprintf()
- * before POSIX.1-2008 came along and decided to usurp the names, so we
- * don't prototype them by default unless one of the following is true:
- *   a) the app has requested them specifically by defining _WITH_GETLINE or
- *      _WITH_DPRINTF, respectively
- *   b) the app has requested a POSIX.1-2008 environment via _POSIX_C_SOURCE
- *   c) the app defines a GNUism such as _BSD_SOURCE or _GNU_SOURCE
- */
-#ifndef _WITH_GETLINE
-#if defined(_BSD_SOURCE) || defined(_GNU_SOURCE)
-#define	_WITH_GETLINE
-#elif defined(_POSIX_C_SOURCE)
-#if _POSIX_C_SOURCE >= 200809
-#define	_WITH_GETLINE
-#endif
-#endif
-#endif
-
-#ifdef _WITH_GETLINE
 ssize_t	 getline(char ** __restrict, size_t * __restrict, FILE * __restrict);
-#endif
-
-#ifndef _WITH_DPRINTF
-#if defined(_BSD_SOURCE) || defined(_GNU_SOURCE)
-#define	_WITH_DPRINTF
-#elif defined(_POSIX_C_SOURCE)
-#if _POSIX_C_SOURCE >= 200809
-#define	_WITH_DPRINTF
-#endif
-#endif
-#endif
-
-#ifdef _WITH_DPRINTF
-int	 (dprintf)(int, const char * __restrict, ...);
-#endif
-
+int	 dprintf(int, const char * __restrict, ...);
 #endif /* __POSIX_VISIBLE >= 200809 */
 
 /*
