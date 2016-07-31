f56b19839226fde3ea8c7b82f138392cb511aaa1
pfg <pfg@FreeBSD.org>
2016-07-31 21:36:40 +0000

    indent(1): replace function call to bzero with memset.
    Reference:
    https://github.com/pstef/freebsd_indent/commit/7422f42f80099c69d34833d7106035dc09230235
    
    Differential Revision: https://reviews.freebsd.org/D6966  (Partial)
    Submitted by:	Piotr Stefaniak

--- usr.bin/indent/io.c
+++ usr.bin/indent/io.c
@@ -629,7 +629,7 @@ parsefont(struct fstate *f, const char *s0)
     const char *s = s0;
     int         sizedelta = 0;
 
-    bzero(f, sizeof *f);
+    memset(f, 0, sizeof(struct fstate));
     while (*s) {
 	if (isdigit(*s))
 	    f->size = f->size * 10 + *s - '0';
