7b0bcc9635b00756d3f273a9f9c9f1be1bba0674
pfg <pfg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-08-03 16:33:34 +0000

    indent(1): accept offsetof(3) as a keyword.
    Reference:
    https://github.com/pstef/freebsd_indent/commit/c470e5e2c974aa38450ca4762b93829f7a7bfa4d
    
    Differential Revision: https://reviews.freebsd.org/D6966  (Partial)
    Submitted by:	Piotr Stefaniak
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@303718 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/indent/indent_globs.h
+++ usr.bin/indent/indent_globs.h
@@ -244,10 +244,10 @@ struct parser_state {
 				 * char should be lined up with the / in / followed by * */
     int         comment_delta,
                 n_comment_delta;
-    int         cast_mask;	/* indicates which close parens close off
-				 * casts */
-    int         sizeof_mask;	/* indicates which close parens close off
-				 * sizeof''s */
+    int         cast_mask;	/* indicates which close parens potentially
+				 * close off casts */
+    int         not_cast_mask;	/* indicates which close parens definitely
+				 * close off something else than casts */
     int         block_init;	/* true iff inside a block initialization */
     int         block_init_level;	/* The level of brace nesting in an
 					 * initialization */
@@ -317,8 +317,7 @@ struct parser_state {
 				 * specially */
     int         decl_indent;	/* column to indent declared identifiers to */
     int         local_decl_indent;	/* like decl_indent but for locals */
-    int         its_a_keyword;
-    int         sizeof_keyword;
+    int         keyword;	/* the type of a keyword or 0 */
     int         dumped_decl_indent;
     float       case_indent;	/* The distance to indent case labels from the
 				 * switch statement */
