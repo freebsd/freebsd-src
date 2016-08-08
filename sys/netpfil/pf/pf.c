22bae95c28e300c647c247d83a85604db890b116
Kristof Provost <kp@FreeBSD.org>
2016-08-08 19:44:13 +0000

    MFC r290521:
    pf: Fix broken rule skip calculation
    
    r289932 accidentally broke the rule skip calculation. The address family
    argument to PF_ANEQ() is now important, and because it was set to 0 the macro
    always evaluated to false.
    This resulted in incorrect skip values, which in turn broke the rule
    evaluations.

--- sys/netpfil/pf/pf.c
+++ sys/netpfil/pf/pf.c
@@ -1985,9 +1985,9 @@ pf_addr_wrap_neq(struct pf_addr_wrap *aw1, struct pf_addr_wrap *aw2)
 	switch (aw1->type) {
 	case PF_ADDR_ADDRMASK:
 	case PF_ADDR_RANGE:
-		if (PF_ANEQ(&aw1->v.a.addr, &aw2->v.a.addr, 0))
+		if (PF_ANEQ(&aw1->v.a.addr, &aw2->v.a.addr, AF_INET6))
 			return (1);
-		if (PF_ANEQ(&aw1->v.a.mask, &aw2->v.a.mask, 0))
+		if (PF_ANEQ(&aw1->v.a.mask, &aw2->v.a.mask, AF_INET6))
 			return (1);
 		return (0);
 	case PF_ADDR_DYNIFTL:
