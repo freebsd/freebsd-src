bd937497ea2a1111d9cf6ff1cdbe698b3cdd430a
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2016-08-09 19:32:06 +0000

    Consistently use `device_t`
    Several files use the internal name of `struct device` instead of
    `device_t` which is part of the public API. This patch changes all
    `struct device *` to `device_t`.
    
    The remaining occurrences of `struct device` are those referring to the
    Linux or OpenBSD version of the structure, or the code is not built on
    FreeBSD and it's unclear what to do.
    
    Submitted by:	Matthew Macy <mmacy@nextbsd.org> (previous version)
    Approved by:	emaste, jhibbits, sbruno
    MFC after:	3 days
    Differential Revision:	https://reviews.freebsd.org/D7447

--- sys/dev/e1000/e1000_osdep.h
+++ sys/dev/e1000/e1000_osdep.h
@@ -134,7 +134,7 @@ struct e1000_osdep
 	bus_space_handle_t io_bus_space_handle;
 	bus_space_tag_t    flash_bus_space_tag;
 	bus_space_handle_t flash_bus_space_handle;
-	struct device     *dev;
+	device_t	   dev;
 };
 
 #define E1000_REGISTER(hw, reg) (((hw)->mac.type >= e1000_82543) \
