8caa09bd36376b5a1e3193fbf7ab2d55b2024a6a
Sepherosa Ziehau <sephe@FreeBSD.org>
2016-08-18 05:52:03 +0000

    hyperv/hn: Get rid of the useless netvsc_packet
    MFC after:	1 week
    Sponsored by:	Microsoft
    Differential Revision:	https://reviews.freebsd.org/D7544

--- sys/dev/hyperv/netvsc/hv_rndis_filter.h
+++ sys/dev/hyperv/netvsc/hv_rndis_filter.h
@@ -115,8 +115,8 @@ typedef struct rndis_device_ {
 struct hn_softc;
 struct hn_rx_ring;
 
-int hv_rf_on_receive(netvsc_dev *net_dev,
-    struct hn_rx_ring *rxr, netvsc_packet *pkt);
+int hv_rf_on_receive(netvsc_dev *net_dev, struct hn_rx_ring *rxr,
+    const void *data, int dlen);
 void hv_rf_receive_rollup(netvsc_dev *net_dev);
 void hv_rf_channel_rollup(struct hn_rx_ring *rxr, struct hn_tx_ring *txr);
 int hv_rf_on_device_add(struct hn_softc *sc, void *additl_info, int nchan,
