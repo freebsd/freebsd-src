c9bab4efb0ab00a2abb0807eeb87eadf94c07b3d
Dimitry Andric <dim@FreeBSD.org>
2016-08-27 09:51:57 +0000

    Missed one .mk file where OBJTOP can be used.

--- usr.bin/clang/clang.prog.mk
+++ usr.bin/clang/clang.prog.mk
@@ -3,8 +3,8 @@
 LLVM_SRCS=	${SRCTOP}/contrib/llvm
 CLANG_SRCS=	${LLVM_SRCS}/tools/clang
 
-CFLAGS+=	-I${.OBJDIR}/../../../lib/clang/libclang
-CFLAGS+=	-I${.OBJDIR}/../../../lib/clang/libllvm
+CFLAGS+=	-I${OBJTOP}/lib/clang/libclang
+CFLAGS+=	-I${OBJTOP}/lib/clang/libllvm
 
 .include "${SRCTOP}/lib/clang/clang.build.mk"
 
@@ -12,8 +12,8 @@ LIBDEPS+=	clang
 LIBDEPS+=	llvm
 
 .for lib in ${LIBDEPS}
-DPADD+=		${.OBJDIR}/../../../lib/clang/lib${lib}/lib${lib}.a
-LDADD+=		${.OBJDIR}/../../../lib/clang/lib${lib}/lib${lib}.a
+DPADD+=		${OBJTOP}/lib/clang/lib${lib}/lib${lib}.a
+LDADD+=		${OBJTOP}/lib/clang/lib${lib}/lib${lib}.a
 .endfor
 
 LIBADD+=	ncursesw
