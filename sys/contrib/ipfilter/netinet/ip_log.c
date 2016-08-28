338dc8086dd089e4cca704130d290ae0520327aa
Dimitry Andric <dim@FreeBSD.org>
2016-08-28 19:35:29 +0000

    Follow-up to r304953, in which I broke the build: apparently the SOLARIS macro is defined in lots of different places in ipfilter, so replace all of the nonportable definitions with portable ones.
    Pointy hat to:	dim
    X-MFC-With:	r304959, r304953
    MFC after:	3 days

--- sys/contrib/ipfilter/netinet/ip_log.c
+++ sys/contrib/ipfilter/netinet/ip_log.c
@@ -19,11 +19,11 @@
 # include <osreldate.h>
 #endif
 #ifndef SOLARIS
-#if defined(sun) && (defined(__svr4__) || defined(__SVR4))
-# define	SOLARIS		1
-#else
-# define	SOLARIS		0
-#endif
+# if defined(sun) && (defined(__svr4__) || defined(__SVR4))
+#  define	SOLARIS		1
+# else
+#  define	SOLARIS		0
+# endif
 #endif
 #include <sys/errno.h>
 #include <sys/types.h>
