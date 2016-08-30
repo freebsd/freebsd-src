f8aa7e792e319ca10bb1636d5e011edf0ee07422
kevlo <kevlo@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-08-30 05:15:42 +0000

    Zap M_IOVEC.
    Reviewed by:	Matthew Macy
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@305051 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/mbuf.h
+++ sys/sys/mbuf.h
@@ -391,7 +391,7 @@ struct mbuf {
 #define	EXT_JUMBO9	4	/* jumbo cluster 9216 bytes */
 #define	EXT_JUMBO16	5	/* jumbo cluster 16184 bytes */
 #define	EXT_PACKET	6	/* mbuf+cluster from packet zone */
-#define	EXT_MBUF	7	/* external mbuf reference (M_IOVEC) */
+#define	EXT_MBUF	7	/* external mbuf reference */
 #define	EXT_SFBUF_NOCACHE 8	/* sendfile(2)'s sf_buf not to be cached */
 
 #define	EXT_VENDOR1	224	/* for vendor-internal use */
