702a86ec0f1cfad021599b9a415f01237ba80f8f
Jung-uk Kim <jkim@FreeBSD.org>
2016-08-31 20:21:03 +0000

    MFC:	r304320
    Disable assembly sources when compiler/assembler cannot compile certain
    instructions.  For example, GCC 4.2.1 + binutils 2.17.50 does not support
    AVX instructions.

--- secure/lib/libcrypto/Makefile.asm
+++ secure/lib/libcrypto/Makefile.asm
@@ -6,7 +6,7 @@
 
 .include "Makefile.inc"
 
-.if ${MACHINE_CPUARCH} == "amd64"
+.if defined(ASM_amd64)
 
 .PATH:	${LCRYPTO_SRC}/crypto \
 	${LCRYPTO_SRC}/crypto/aes/asm \
@@ -78,7 +78,7 @@ ${s}.S: ${s}.s
 	cat ${s}.s ) > ${.TARGET}
 .endfor
 
-.elif ${MACHINE_CPUARCH} == "i386"
+.elif defined(ASM_i386)
 
 .PATH:	${LCRYPTO_SRC}/crypto \
 	${LCRYPTO_SRC}/crypto/aes/asm \
