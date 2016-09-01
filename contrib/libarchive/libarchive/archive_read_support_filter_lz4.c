e77f0210a34587f8cd20b59d408d28bc59e3e81d
mm <mm@FreeBSD.org>
2016-09-01 07:53:59 +0000

    MFC r304075,r304989: Sync libarchive with vendor including security fixes
    Vendor issues fixed:
    Issue #731: Reject tar entries >= INT64_MAX
    Issue #744: Very long pathnames evade symlink checks
    Issue #748: libarchive can compress, but cannot decompress zip some files
    PR #750: ustar: fix out of bounds read on empty string ("") filename
    PR #755: fix use of acl_get_flagset_np() on FreeBSD
    Issue #767: Buffer overflow printing a filename
    Issue #770: Zip read: be more careful about extra_length
    
    MFC r304874:
    Temporarily disable two libarchive tests that have not yet been fixed by
    vendor. Tests will be re-enabled after a fix has been merged.

--- contrib/libarchive/libarchive/archive_read_support_filter_lz4.c
+++ contrib/libarchive/libarchive/archive_read_support_filter_lz4.c
@@ -595,7 +595,7 @@ lz4_filter_read_data_block(struct archive_read_filter *self, const void **p)
 #endif
 	}
 
-	/* Check if an error happend in decompression process. */
+	/* Check if an error occurred in the decompression process. */
 	if (uncompressed_size < 0) {
 		archive_set_error(&(self->archive->archive),
 		    ARCHIVE_ERRNO_MISC, "lz4 decompression failed");
