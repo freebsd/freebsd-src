13a03d4bba6a15f5c2b61b19e15d682f4fee98b9
mav <mav@FreeBSD.org>
2016-09-01 13:29:53 +0000

    MFV r302643: 6902 speed up listing of snapshots if requesting name only and sorting by name
    This was our change from the beginning, so just reduce the upstream diff.

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/zfs_ioctl.h
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/zfs_ioctl.h
@@ -20,6 +20,7 @@
  */
 /*
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011-2012 Pawel Jakub Dawidek. All rights reserved.
  * Copyright (c) 2012, 2015 by Delphix. All rights reserved.
  * Copyright 2016 RackTop Systems.
  * Copyright (c) 2014 Integros [integros.com]
