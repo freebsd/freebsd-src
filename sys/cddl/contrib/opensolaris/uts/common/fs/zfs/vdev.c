1f53ccfc66d36f0c0182d8bdf1603c63eaed2b40
mav <mav@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-09-03 08:42:12 +0000

    MFV r303077: 7072 zfs fails to expand if lun added when os is in shutdown state
    illumos/illumos-gate@c39a2aae1e2c439d156021edfc20910dad7f9891
    https://github.com/illumos/illumos-gate/commit/c39a2aae1e2c439d156021edfc20910dad7f9891
    
    https://www.illumos.org/issues/7072
      upstream:
      38733 zfs fails to expand if lun added when os is in shutdown state
      DLPX-36910 spares and caches should not display expandable space
      DLPX-39262 vdev_disk_open spam zfs_dbgmsg buffer
    
    Reviewed by: Igor Kozhukhov <ikozhukhov@gmail.com>
    Reviewed by: Dan Kimmel <dan.kimmel@delphix.com>
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: Prakash Surya <prakash.surya@delphix.com>
    Reviewed by: Alex Reece <alex@delphix.com>
    Approved by: Dan McDonald <danmcd@omniti.com>
    Author: George Wilson <george.wilson@delphix.com>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@305324 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/vdev.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/vdev.c
@@ -2885,6 +2885,7 @@ vdev_get_stats(vdev_t *vd, vdev_stat_t *vs)
 {
 	spa_t *spa = vd->vdev_spa;
 	vdev_t *rvd = spa->spa_root_vdev;
+	vdev_t *tvd = vd->vdev_top;
 
 	ASSERT(spa_config_held(spa, SCL_ALL, RW_READER) != 0);
 
@@ -2895,8 +2896,15 @@ vdev_get_stats(vdev_t *vd, vdev_stat_t *vs)
 	vs->vs_rsize = vdev_get_min_asize(vd);
 	if (vd->vdev_ops->vdev_op_leaf)
 		vs->vs_rsize += VDEV_LABEL_START_SIZE + VDEV_LABEL_END_SIZE;
-	if (vd->vdev_max_asize != 0)
-		vs->vs_esize = vd->vdev_max_asize - vd->vdev_asize;
+	/*
+	 * Report expandable space on top-level, non-auxillary devices only.
+	 * The expandable space is reported in terms of metaslab sized units
+	 * since that determines how much space the pool can expand.
+	 */
+	if (vd->vdev_aux == NULL && tvd != NULL && vd->vdev_max_asize != 0) {
+		vs->vs_esize = P2ALIGN(vd->vdev_max_asize - vd->vdev_asize,
+		    1ULL << tvd->vdev_ms_shift);
+	}
 	vs->vs_configured_ashift = vd->vdev_top != NULL
 	    ? vd->vdev_top->vdev_ashift : vd->vdev_ashift;
 	vs->vs_logical_ashift = vd->vdev_logical_ashift;
