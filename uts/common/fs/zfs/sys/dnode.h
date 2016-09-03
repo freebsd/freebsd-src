52065bfe109e54ecc72ac8dedbac4aede9be33ba
mav <mav@FreeBSD.org>
2016-09-03 10:54:56 +0000

    7004 dmu_tx_hold_zap() does dnode_hold() 7x on same object
    Using a benchmark which has 32 threads creating 2 million files in the
    same directory, on a machine with 16 CPU cores, I observed poor
    performance. I noticed that dmu_tx_hold_zap() was using about 30% of
    all CPU, and doing dnode_hold() 7 times on the same object (the ZAP
    object that is being held).
    
    dmu_tx_hold_zap() keeps a hold on the dnode_t the entire time it is
    running, in dmu_tx_hold_t:txh_dnode, so it would be nice to use the
    dnode_t that we already have in hand, rather than repeatedly calling
    dnode_hold(). To do this, we need to pass the dnode_t down through
    all the intermediate calls that dmu_tx_hold_zap() makes, making these
    routines take the dnode_t* rather than an objset_t* and a uint64_t
    object number. In particular, the following routines will need to have
    analogous *_by_dnode() variants created:
    
    dmu_buf_hold_noread()
    dmu_buf_hold()
    zap_lookup()
    zap_lookup_norm()
    zap_count_write()
    zap_lockdir()
    zap_count_write()
    
    This can improve performance on the benchmark described above by 100%,
    from 30,000 file creations per second to 60,000. (This improvement is on
    top of that provided by working around the object allocation issue. Peak
    performance of ~90,000 creations per second was observed with 8 CPUs;
    adding CPUs past that decreased performance due to lock contention.) The
    CPU used by dmu_tx_hold_zap() was reduced by 88%, from 340 CPU-seconds
    to 40 CPU-seconds.
    
    Sponsored by: Intel Corp.
    
    Closes #109
    
    Reviewed by: Steve Gonczi <steve.gonczi@delphix.com>
    Reviewed by: George Wilson <george.wilson@delphix.com>
    Reviewed by: Pavel Zakharov <pavel.zakharov@delphix.com>
    Reviewed by: Ned Bass <bass6@llnl.gov>
    Reviewed by: Brian Behlendorf <behlendorf1@llnl.gov>
    Author: Matthew Ahrens <mahrens@delphix.com>
    
    openzfs/openzfs@d3e523d489a169ab36f9ec1b2a111a60a5563a9f

--- uts/common/fs/zfs/sys/dnode.h
+++ uts/common/fs/zfs/sys/dnode.h
@@ -20,7 +20,7 @@
  */
 /*
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2012, 2014 by Delphix. All rights reserved.
+ * Copyright (c) 2012, 2016 by Delphix. All rights reserved.
  * Copyright (c) 2014 Spectra Logic Corporation, All rights reserved.
  */
 
@@ -149,7 +149,7 @@ typedef struct dnode_phys {
 	blkptr_t dn_spill;
 } dnode_phys_t;
 
-typedef struct dnode {
+struct dnode {
 	/*
 	 * Protects the structure of the dnode, including the number of levels
 	 * of indirection (dn_nlevels), dn_maxblkid, and dn_next_*
@@ -247,7 +247,7 @@ typedef struct dnode {
 
 	/* holds prefetch structure */
 	struct zfetch	dn_zfetch;
-} dnode_t;
+};
 
 /*
  * Adds a level of indirection between the dbuf and the dnode to avoid
