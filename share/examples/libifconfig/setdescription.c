072565371371d18a83c9ef57aaed59d0c97eec5b
kp <kp@FreeBSD.org>
2016-09-04 20:55:27 +0000

    libifconfig: style(9) fixes
    Also switch from BSD 3-clause to 2-clause license where possible, and
    consolidate duplicate 3-clause license into one.
    
    Submitted by:	Marie Helene Kvello-Aune <marieheleneka@gmail.com>
    Reviewed by:	cem, kp
    Differential Revision:	https://reviews.freebsd.org/D7764

--- share/examples/libifconfig/setdescription.c
+++ share/examples/libifconfig/setdescription.c
@@ -12,10 +12,6 @@
  * this list of conditions and the following disclaimer in the documentation and/or
  * other materials provided with the distribution.
  *
- * 3. Neither the name of the copyright holder nor the names of its contributors
- * may be used to endorse or promote products derived from this software without
- * specific prior written permission.
- *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -38,15 +34,17 @@
 #include <libifconfig.h>
 
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
+	char *ifname, *ifdescr, *curdescr;
+
 	if (argc != 3) {
 		errx(EINVAL, "Invalid number of arguments."
 		    " First argument should be interface name, second argument"
 		    " should be the description to set.");
 	}
 
-	char *ifname, *ifdescr, *curdescr;
 	/* We have a static number of arguments. Therefore we can do it simple. */
 	ifname = strdup(argv[1]);
 	ifdescr = strdup(argv[2]);
