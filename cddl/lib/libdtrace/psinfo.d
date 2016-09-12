5986af9d01080d549cde638c5af2bef5fd977207
gnn <gnn@FreeBSD.org>
2016-09-12 17:05:42 +0000

    MFC: 304825 Unlike Solaris, in FreeBSD p_args can be 0 so check for that instead of walking down to ar_args blindly.
    Reported by:	Amanda Strnad
    Reviewed by:	markj, jhb
    Sponsored by:	DARPA, AFRL

--- cddl/lib/libdtrace/psinfo.d
+++ cddl/lib/libdtrace/psinfo.d
@@ -57,7 +57,7 @@ translator psinfo_t < struct proc *T > {
 	pr_gid = T->p_ucred->cr_rgid;
 	pr_egid = T->p_ucred->cr_groups[0];
 	pr_addr = 0;
-	pr_psargs = (T->p_args->ar_args == 0) ? "" :
+	pr_psargs = (T->p_args == 0) ? "" :
 	    memstr(T->p_args->ar_args, ' ', T->p_args->ar_length);
 	pr_arglen = T->p_args->ar_length;
 	pr_jailid = T->p_ucred->cr_prison->pr_id;
