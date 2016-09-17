d9d5a1040d379d146fc4411e0bacbca09c3d4eec
jceel <jceel@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-09-17 13:48:01 +0000

    Add virtio-console support to bhyve.
    Adds virtio-console device support to bhyve, allowing to create
    bidirectional character streams between host and guest.
    
    Syntax:
    -s <slotnum>,virtio-console,port1=/path/to/port1.sock,anotherport=...
    
    Maximum of 16 ports per device can be created. Every port is named
    and corresponds to an Unix domain socket created by bhyve. bhyve
    accepts at most one connection per port at a time.
    
    Limitations:
    - due to lack of destructors of in bhyve, sockets on the filesystem
      must be cleaned up manually after bhyve exits
    - there's no way to use "console port" feature, nor the console port
      resize as of now
    - emergency write is advertised, but no-op as of now
    
    Approved by:	trasz
    MFC after:	1 month
    Relnotes:	yes
    Sponsored by:	iXsystems, Inc.
    Differential Revision:	D7185
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@305898 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/bhyve/virtio.h
+++ usr.sbin/bhyve/virtio.h
@@ -210,6 +210,7 @@ struct vring_used {
 #define	VIRTIO_DEV_NET		0x1000
 #define	VIRTIO_DEV_BLOCK	0x1001
 #define	VIRTIO_DEV_RANDOM	0x1002
+#define	VIRTIO_DEV_CONSOLE	0x1003
 
 /*
  * PCI config space constants.
