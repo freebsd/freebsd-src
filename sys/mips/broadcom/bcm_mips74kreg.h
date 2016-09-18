f75615f26f90d8eaa648f4e663b8891a2e6b4173
Landon J. Fuller <landonf@FreeBSD.org>
2016-09-18 21:28:09 +0000

    Split bcm_mipscore.c into bcm_bmips (BMIPS32/BMIPS3300) and bcm_mips74k drivers.
    The BMIPS32/BMIPS3300 cores use a register layout distinct from the MIPS74K
    core, and are only found on siba(4) devices.
    
    Reviewed by:	mizhka
    Approved by:	adrian (mentor)
    Differential Revision:	https://reviews.freebsd.org/D7791

--- sys/mips/broadcom/bcm_mips74kreg.h
+++ sys/mips/broadcom/bcm_mips74kreg.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2016 Michael Zhilin <mizhka@gmail.com>
+ * Copyright (c) 2016 Landon Fuller <landonf@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,36 +25,38 @@
  * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGES.
- *
- *  $FreeBSD$
+ * 
+ * $FreeBSD$
  */
 
-#ifndef _BHND_CORES_MIPS_MIPSCOREVAR_H_
-#define _BHND_CORES_MIPS_MIPSCOREVAR_H_
+#ifndef	_MIPS_BROADCOM_MIPS74KREG_H_
+#define	_MIPS_BROADCOM_MIPS74KREG_H_
 
-#define	MIPSCORE_MAX_RSPEC 2
+#define	BCM_MIPS74K_CORECTL		0x00	/**< core control */
+#define	BCM_MIPS74K_EXCBASE		0x04	/**< exception base */
 
-struct mipscore_softc {
-	device_t		dev;	/* CPU device */
-	uint32_t		devid;
-	struct resource_spec	rspec[MIPSCORE_MAX_RSPEC];
-	struct bhnd_resource	*res[MIPSCORE_MAX_RSPEC];
-};
+#define	BCM_MIPS74K_BIST_STATUS		0x0C	/**< built-in self-test status */
+#define	BCM_MIPS74K_INTR_STATUS		0x10	/**< interrupt status */
 
-struct mipscore_regs {
-        uint32_t	corecontrol;
-        uint32_t	exceptionbase;
-        uint32_t	PAD1[1];	/* unmapped address */
-        uint32_t	biststatus;
-        uint32_t	intstatus;
-        uint32_t	intmask[6];
-        uint32_t	nmimask;
-        uint32_t	PAD2[4];	/* unmapped addresses */
-        uint32_t	gpioselect;
-        uint32_t	gpiooutput;
-        uint32_t	gpioenable;
-        uint32_t	PAD3[101];	/* unmapped addresses */
-        uint32_t	clkcontrolstatus;
-};
+/* INTR(0-5)_MASK map bcma(4) OOB interrupt bus lines to MIPS hardware
+ * interrupts. */
+#define	BCM_MIPS74K_INTR0_SEL		0x14	/**< IRQ0 OOBSEL mask */
+#define	BCM_MIPS74K_INTR1_SEL		0x18	/**< IRQ1 OOBSEL mask */
+#define	BCM_MIPS74K_INTR2_SEL		0x1C	/**< IRQ2 OOBSEL mask */
+#define	BCM_MIPS74K_INTR3_SEL		0x20	/**< IRQ3 OOBSEL mask */
+#define	BCM_MIPS74K_INTR4_SEL		0x24	/**< IRQ4 OOBSEL mask */
+#define	BCM_MIPS74K_INTR5_SEL		0x28	/**< IRQ5 OOBSEL mask */
 
-#endif /* _BHND_CORES_MIPS_MIPSCOREVAR_H_ */
+#define	BCM_MIPS74K_INTR_SEL(_intr)	\
+	(BCM_MIPS74K_INTR0_SEL + ((_intr) * 4))
+
+#define	BCM_MIPS74K_NMI_MASK		0x2C	/**< nmi mask */
+
+#define	BCM_MIPS74K_GPIO_SEL		0x40	/**< gpio select */
+#define	BCM_MIPS74K_GPIO_OUT		0x44	/**< gpio output enable */
+#define	BCM_MIPS74K_GPIO_EN		0x48	/**< gpio enable */
+
+
+#define	BCM_MIPS74K_TIMER_IVEC		31	/**< MIPS timer OOBSEL value */
+
+#endif /* _MIPS_BROADCOM_MIPS74KREG_H_ */
