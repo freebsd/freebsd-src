642ffb1e77c9ca7ee701c59ace57176d90cab372
ngie <ngie@FreeBSD.org>
2016-09-18 05:06:15 +0000

    Make sure $TMPDIR is created with 0755 permissions
    This is required to ensure that the temporary script can be executed,
    as the default mode is apparently too restrictive
    
    MFC after:	3 days
    Sponsored by:	Dell EMC Isilon

--- tests/sys/mac/bsdextended/matches_test.sh
+++ tests/sys/mac/bsdextended/matches_test.sh
@@ -36,6 +36,10 @@ if ! sysctl -N security.mac.bsdextended >/dev/null 2>&1; then
 	echo "1..0 # SKIP mac_bsdextended(4) support isn't available"
 	exit 0
 fi
+if ! chmod -Rf 0755 $TMPDIR; then
+	echo "1..0 # SKIP failed to chmod $TMPDIR"
+	exit 0
+fi
 if ! playground=$(mktemp -d $TMPDIR/tmp.XXXXXXX); then
 	echo "1..0 # SKIP failed to create temporary directory"
 	exit 0
