2ae4c4c438ba5cb124bce0ef90b8e971df19b1ee
adrian <adrian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-09-21 19:48:07 +0000

    [net80211] don't add IBSS node table entries for neighbors from other SSIDs.
    The adhoc probe/beacon input path was creating nodes for all SSIDs.
    This wasn't a problem when the NICs were configured to only process
    frames for the current BSSID, but that didn't allow IBSS merges.
    Once avos and I flipped on "beacons from all BSSIDs" to allow for
    correct IBSS merging, we found this interesting behaviour.
    
    This adds a check against the current SSID.
    
    * If there's no VAP SSID, allow anything
    * If there's a VAP SSID, check if the incoming frame has a suitable
      SSID and if so, allow it.
    
    This prevents nodes being created for other SSIDs in probe and beacon
    frames - ie, beacons overlapping IBSSes with different SSIDs, and
    probe requests from arbitrary devices.
    
    Tested:
    
    * AR9380, IBSS mode, both local and other IBSSes.
    
    Reviewed by:	avos
    Differential Revision:	https://reviews.freebsd.org/D7959
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@306139 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net80211/ieee80211_node.h
+++ sys/net80211/ieee80211_node.h
@@ -65,6 +65,7 @@
 struct ieee80211_node_table;
 struct ieee80211com;
 struct ieee80211vap;
+struct ieee80211_scanparams;
 
 /*
  * Information element ``blob''.  We use this structure
@@ -330,6 +331,8 @@ void	ieee80211_setupcurchan(struct ieee80211com *,
 void	ieee80211_setcurchan(struct ieee80211com *, struct ieee80211_channel *);
 void	ieee80211_update_chw(struct ieee80211com *);
 int	ieee80211_ibss_merge_check(struct ieee80211_node *);
+int	ieee80211_ibss_node_check_new(struct ieee80211_node *ni,
+	    const struct ieee80211_scanparams *);
 int	ieee80211_ibss_merge(struct ieee80211_node *);
 struct ieee80211_scan_entry;
 int	ieee80211_sta_join(struct ieee80211vap *, struct ieee80211_channel *,
