c862dd3ee017caabf5a4e17df951172959963974
ed <ed@FreeBSD.org>
2016-09-22 08:14:59 +0000

    Make it possible to safely use TPIDRURW from userspace.
    On amd64, arm64 and i386, we have the possibility to switch between TLS
    areas in userspace. The nice thing about this is that it makes it easier
    to do light-weight threading, if we ever feel like doing that. On armv6,
    let's go into the same direction by making it possible to safely use the
    TPIDRURW register, which is intended for this purpose.
    
    Clean up the ARMv6 code to remove md_tp entirely. Simply add a dedicated
    field to the PCB to hold the value of TPIDRURW across context switches,
    like we do for any other register. As userspace currently uses the
    read-only TPIDRURO register, simply ensure that we keep both values in
    sync where possible. The system calls for modifying the read-only
    register will simply write the intended value into both registers, so
    that it lazily ends up in the PCB during the next context switch.
    
    Reviewed by:	https://reviews.freebsd.org/D7951
    Approved by:	andrew
    Reviewed by:	imp
    Differential Revision:	https://reviews.freebsd.org/D7951

--- sys/arm/arm/vm_machdep.c
+++ sys/arm/arm/vm_machdep.c
@@ -82,8 +82,8 @@ __FBSDID("$FreeBSD$");
  * struct switchframe and trapframe must both be a multiple of 8
  * for correct stack alignment.
  */
-CTASSERT(sizeof(struct switchframe) == 48);
-CTASSERT(sizeof(struct trapframe) == 80);
+_Static_assert((sizeof(struct switchframe) % 8) == 0, "Bad alignment");
+_Static_assert((sizeof(struct trapframe) % 8) == 0, "Bad alignment");
 
 uint32_t initial_fpscr = VFPSCR_DN | VFPSCR_FZ;
 
@@ -134,6 +134,9 @@ cpu_fork(register struct thread *td1, register struct proc *p2,
 	pcb2->pcb_regs.sf_r5 = (register_t)td2;
 	pcb2->pcb_regs.sf_lr = (register_t)fork_trampoline;
 	pcb2->pcb_regs.sf_sp = STACKALIGN(td2->td_frame);
+#if __ARM_ARCH >= 6
+	pcb2->pcb_regs.sf_tpidrurw = (register_t)get_tls();
+#endif
 
 	pcb2->pcb_vfpcpu = -1;
 	pcb2->pcb_vfpstate.fpscr = initial_fpscr;
@@ -147,9 +150,7 @@ cpu_fork(register struct thread *td1, register struct proc *p2,
 	/* Setup to release spin count in fork_exit(). */
 	td2->td_md.md_spinlock_count = 1;
 	td2->td_md.md_saved_cspr = PSR_SVC32_MODE;
-#if __ARM_ARCH >= 6
-	td2->td_md.md_tp = td1->td_md.md_tp;
-#else
+#if __ARM_ARCH < 6
 	td2->td_md.md_tp = *(register_t *)ARM_TP_ADDRESS;
 #endif
 }
@@ -272,16 +273,18 @@ int
 cpu_set_user_tls(struct thread *td, void *tls_base)
 {
 
+#if __ARM_ARCH >= 6
+	td->td_pcb->pcb_regs.sf_tpidrurw = (register_t)tls_base;
+	if (td == curthread)
+		set_tls(tls_base);
+#else
 	td->td_md.md_tp = (register_t)tls_base;
 	if (td == curthread) {
 		critical_enter();
-#if __ARM_ARCH >= 6
-		set_tls(tls_base);
-#else
 		*(register_t *)ARM_TP_ADDRESS = (register_t)tls_base;
-#endif
 		critical_exit();
 	}
+#endif
 	return (0);
 }
 
