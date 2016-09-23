87021e009bc2eacd9274651f1064f4bff6c69ffe
andrew <andrew@FreeBSD.org>
2016-09-23 14:11:23 +0000

    Restrict where we need to define fdt_fixup_table to just PowerPC and Marvell.
    Sponsored by:	ABT Systems Ltd

--- sys/arm/at91/at91_common.c
+++ sys/arm/at91/at91_common.c
@@ -49,10 +49,6 @@ __FBSDID("$FreeBSD$");
 
 extern const struct devmap_entry at91_devmap[];
 
-struct fdt_fixup_entry fdt_fixup_table[] = {
-	{ NULL, NULL }
-};
-
 #ifndef INTRNG
 static int
 fdt_aic_decode_ic(phandle_t node, pcell_t *intr, int *interrupt, int *trig,
