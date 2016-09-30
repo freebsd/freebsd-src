31f575777c1b01493c6f6bbd158c0506de0a880f
Conrad Meyer <cem@FreeBSD.org>
2016-09-30 18:58:50 +0000

    Revert r306516 for now, it is incomplete on i386
    Noted by:	kib

--- sys/amd64/amd64/mp_machdep.c
+++ sys/amd64/amd64/mp_machdep.c
@@ -409,7 +409,6 @@ void
 invltlb_invpcid_handler(void)
 {
 	struct invpcid_descr d;
-	uint64_t generation;
 
 #ifdef COUNT_XINVLTLB_HITS
 	xhits_gbl[PCPU_GET(cpuid)]++;
@@ -418,20 +417,17 @@ invltlb_invpcid_handler(void)
 	(*ipi_invltlb_counts[PCPU_GET(cpuid)])++;
 #endif /* COUNT_IPIS */
 
-	generation = smp_tlb_generation;
 	d.pcid = smp_tlb_pmap->pm_pcids[PCPU_GET(cpuid)].pm_pcid;
 	d.pad = 0;
 	d.addr = 0;
 	invpcid(&d, smp_tlb_pmap == kernel_pmap ? INVPCID_CTXGLOB :
 	    INVPCID_CTX);
-	PCPU_SET(smp_tlb_done, generation);
+	atomic_add_int(&smp_tlb_wait, 1);
 }
 
 void
 invltlb_pcid_handler(void)
 {
-	uint64_t generation;
-  
 #ifdef COUNT_XINVLTLB_HITS
 	xhits_gbl[PCPU_GET(cpuid)]++;
 #endif /* COUNT_XINVLTLB_HITS */
@@ -439,7 +435,6 @@ invltlb_pcid_handler(void)
 	(*ipi_invltlb_counts[PCPU_GET(cpuid)])++;
 #endif /* COUNT_IPIS */
 
-	generation = smp_tlb_generation;
 	if (smp_tlb_pmap == kernel_pmap) {
 		invltlb_glob();
 	} else {
@@ -455,5 +450,5 @@ invltlb_pcid_handler(void)
 			    smp_tlb_pmap->pm_pcids[PCPU_GET(cpuid)].pm_pcid);
 		}
 	}
-	PCPU_SET(smp_tlb_done, generation);
+	atomic_add_int(&smp_tlb_wait, 1);
 }
