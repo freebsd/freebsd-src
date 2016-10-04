4a4d1712207afa156fec1ff8f44ed543519190b6
jhb <jhb@FreeBSD.org>
2016-10-04 22:15:42 +0000

    MFC 305548: Don't break out of the m_advance() loop if len drops to zero.
    If a packet contains the Ethernet header (14 bytes) in the first mbuf
    and the payload (IP + UDP + data) in the second mbuf, then the attempt
    to fetch the l3hdr will return a NULL pointer.  The first loop iteration
    will drop len to zero and exit the loop without setting 'p'.  However,
    the desired data is at the start of the second mbuf, so the correct
    behavior is to loop around and let the conditional set 'p' to m_data of
    the next mbuf (and leave offset as 0).

--- sys/dev/cxgbe/t4_sge.c
+++ sys/dev/cxgbe/t4_sge.c
@@ -2081,7 +2081,7 @@ m_advance(struct mbuf **pm, int *poffset, int len)
 
 	MPASS(len > 0);
 
-	while (len) {
+	for (;;) {
 		if (offset + len < m->m_len) {
 			offset += len;
 			p = mtod(m, uintptr_t) + offset;
