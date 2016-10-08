0e9251c8a5dc849b648649d69b640d45068fc58d
vangyzen <vangyzen@FreeBSD.org>
2016-10-08 14:10:45 +0000

    MFC r306568, r306569
    Add the __printflike attribute to the declarations of
    dprintf(3) and vdprintf(3).
    
    Sponsored by:	Dell EMC

--- include/stdio.h
+++ include/stdio.h
@@ -351,7 +351,7 @@ ssize_t	 getdelim(char ** __restrict, size_t * __restrict, int,
 	    FILE * __restrict);
 FILE	*open_memstream(char **, size_t *);
 int	 renameat(int, const char *, int, const char *);
-int	 vdprintf(int, const char * __restrict, __va_list);
+int	 vdprintf(int, const char * __restrict, __va_list) __printflike(2, 0);
 
 /*
  * Every programmer and his dog wrote functions called getline() and dprintf()
@@ -387,7 +387,7 @@ ssize_t	 getline(char ** __restrict, size_t * __restrict, FILE * __restrict);
 #endif
 
 #ifdef _WITH_DPRINTF
-int	 (dprintf)(int, const char * __restrict, ...);
+int	 (dprintf)(int, const char * __restrict, ...) __printflike(2, 3);
 #endif
 
 #endif /* __BSD_VISIBLE || __POSIX_VISIBLE >= 200809 */
