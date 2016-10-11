26cedadb1310ff81c0128b47e153350b80afc404
Sepherosa Ziehau <sephe@FreeBSD.org>
2016-10-11 07:50:09 +0000

    MFC 302723,302726,302731
    302723
        hyperv: All Hypercall parameters have same alignment requirement.
    
        Sponsored by:   Microsoft OSTC
        Differential Revision:  https://reviews.freebsd.org/D7086
    
    302726
        hyperv: Signal event input parameter is shared w/ MNF
    
        Sponsored by:   Microsoft OSTC
        Differential Revision:  https://reviews.freebsd.org/D7087
    
    302731
        hyperv/vmbus: Reorganize MNF event sending.
    
        Sponsored by:   Microsoft OSTC
        Differential Revision:  https://reviews.freebsd.org/D7088

--- sys/dev/hyperv/vmbus/hyperv_reg.h
+++ sys/dev/hyperv/vmbus/hyperv_reg.h
@@ -133,6 +133,15 @@
 #define CPUID_LEAF_HV_HWFEATURES	0x40000006
 
 /*
+ * Hyper-V Monitor Notification Facility
+ */
+struct hyperv_mon_param {
+	uint32_t	mp_connid;
+	uint16_t	mp_evtflag_ofs;
+	uint16_t	mp_rsvd;
+} __packed;
+
+/*
  * Hyper-V message types
  */
 #define HYPERV_MSGTYPE_NONE		0
@@ -153,13 +162,22 @@
 /*
  * Hypercall input parameters
  */
+#define HYPERCALL_PARAM_ALIGN		8
+#if 0
+/*
+ * XXX
+ * <<Hypervisor Top Level Functional Specification 4.0b>> requires
+ * input parameters size to be multiple of 8, however, many post
+ * message input parameters do _not_ meet this requirement.
+ */
+#define HYPERCALL_PARAM_SIZE_ALIGN	8
+#endif
 
 /*
  * HYPERCALL_POST_MESSAGE
  */
 #define HYPERCALL_POSTMSGIN_DSIZE_MAX	240
 #define HYPERCALL_POSTMSGIN_SIZE	256
-#define HYPERCALL_POSTMSGIN_ALIGN	8
 
 struct hypercall_postmsg_in {
 	uint32_t	hc_connid;
@@ -172,13 +190,8 @@ CTASSERT(sizeof(struct hypercall_postmsg_in) == HYPERCALL_POSTMSGIN_SIZE);
 
 /*
  * HYPERCALL_SIGNAL_EVENT
+ *
+ * struct hyperv_mon_param.
  */
-#define HYPERCALL_SIGEVTIN_ALIGN	8
-
-struct hypercall_sigevt_in {
-	uint32_t	hc_connid;
-	uint16_t	hc_evtflag_ofs;
-	uint16_t	hc_rsvd;
-} __packed;
 
 #endif	/* !_HYPERV_REG_H_ */
