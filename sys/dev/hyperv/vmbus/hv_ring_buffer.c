4bc5505dcda2ba2ca933bb11826eb9c10bd99866
sephe <sephe@FreeBSD.org>
2016-10-12 03:37:43 +0000

    MFC 303283
        hyperv/vmbus: Rename hv_vmbus_priv.h to vmbus_brvar.h
    
        It only contains bufring related bits for a while.
    
        Sponsored by:   Microsoft OSTC
        Differential Revision:  https://reviews.freebsd.org/D7281

--- sys/dev/hyperv/vmbus/hv_ring_buffer.c
+++ sys/dev/hyperv/vmbus/hv_ring_buffer.c
@@ -34,8 +34,8 @@ __FBSDID("$FreeBSD$");
 #include <sys/mutex.h>
 #include <sys/sysctl.h>
 
-#include "hv_vmbus_priv.h"
 #include <dev/hyperv/vmbus/vmbus_reg.h>
+#include <dev/hyperv/vmbus/vmbus_brvar.h>
 
 /* Amount of space to write to */
 #define	HV_BYTES_AVAIL_TO_WRITE(r, w, z)	\
