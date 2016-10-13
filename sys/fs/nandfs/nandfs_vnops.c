786a65db12fa1f3f3ca3cada1ebb28864d98a804
martymac <martymac@FreeBSD.org>
2016-10-13 19:33:07 +0000

    Fix panic() message reporting ufs instead of nandfs
    PR:		213438
    Approved by:	kib

--- sys/fs/nandfs/nandfs_vnops.c
+++ sys/fs/nandfs/nandfs_vnops.c
@@ -2211,7 +2211,7 @@ nandfs_whiteout(struct vop_whiteout_args *ap)
 		/* Create a new directory whiteout */
 #ifdef INVARIANTS
 		if ((cnp->cn_flags & SAVENAME) == 0)
-			panic("ufs_whiteout: missing name");
+			panic("nandfs_whiteout: missing name");
 #endif
 		error = nandfs_add_dirent(dvp, NANDFS_WHT_INO, cnp->cn_nameptr,
 		    cnp->cn_namelen, DT_WHT);
