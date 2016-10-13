1f870997da605a09068541e99521416f96c30f9b
bapt <bapt@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-10-13 22:43:49 +0000

    Stop closing the network device when netbooting for loaders using the common dev_net.c code.
    The NETIF_OPEN_CLOSE_ONCE flag was added in r201932 to prevent that behaviour
    on some architectures (sparc64 and powerpc64) the default was left to always
    open and close the device for each open and close of a file by the loader
    because it was necessary for u-boot on arm.
    
    Since it has been added, the flag was turned on for every arches including the
    u-boot loader for arm.
    
    This also fixes netbooting on RPi3 (tested by gonzo@)
    
    For the loader.efi it greatly speeds up netbooting
    
    Reviewed by:	emaste, gonzo, tsoome
    Approved by:	gonzo
    MFC after:	1 month
    Sponsored by:	Gandi.net
    Differential Revision:	https://reviews.freebsd.org/D8230
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@307238 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/boot/common/dev_net.c
+++ sys/boot/common/dev_net.c
@@ -120,11 +120,9 @@ net_open(struct open_file *f, ...)
 	devname = va_arg(args, char*);
 	va_end(args);
 
-#ifdef	NETIF_OPEN_CLOSE_ONCE
 	/* Before opening another interface, close the previous one first. */
 	if (netdev_sock >= 0 && strcmp(devname, netdev_name) != 0)
 		net_cleanup();
-#endif
 
 	/* On first open, do netif open, mount, etc. */
 	if (netdev_opens == 0) {
@@ -198,21 +196,6 @@ net_close(struct open_file *f)
 
 	f->f_devdata = NULL;
 
-#ifndef	NETIF_OPEN_CLOSE_ONCE
-	/* Extra close call? */
-	if (netdev_opens <= 0)
-		return (0);
-	netdev_opens--;
-	/* Not last close? */
-	if (netdev_opens > 0)
-		return (0);
-	/* On last close, do netif close, etc. */
-#ifdef	NETIF_DEBUG
-	if (debug)
-		printf("net_close: calling net_cleanup()\n");
-#endif
-	net_cleanup();
-#endif
 	return (0);
 }
 
