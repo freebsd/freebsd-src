823a24a08645db229f391151b49ca2f9a97ce1b5
gonzo <gonzo@FreeBSD.org>
2016-10-14 01:23:21 +0000

    Do not set FB_FLAG_MEMATTR if VM_MEMATTR_WRITE_COMBINING is not available
    Pintyhat to: gonzo
    Spotted by: jmallett

--- sys/arm/broadcom/bcm2835/bcm2835_fbd.c
+++ sys/arm/broadcom/bcm2835/bcm2835_fbd.c
@@ -119,8 +119,8 @@ bcm_fb_setup_fbd(struct bcmsc_softc *sc)
 	sc->info.fb_stride = fb.pitch;
 	sc->info.fb_width = fb.xres;
 	sc->info.fb_height = fb.yres;
-	sc->info.fb_flags = FB_FLAG_MEMATTR;
 #ifdef VM_MEMATTR_WRITE_COMBINING
+	sc->info.fb_flags = FB_FLAG_MEMATTR;
 	sc->info.fb_memattr = VM_MEMATTR_WRITE_COMBINING;
 #endif
 
