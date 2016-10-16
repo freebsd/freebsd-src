ab2ef77758e5e8fcf6324db78c0d24e1ce521406
marcel <marcel@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-10-16 02:55:52 +0000

    Switch to using the portable partition scheme headers.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@307387 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/mkimg/mbr.c
+++ usr.bin/mkimg/mbr.c
@@ -33,30 +33,17 @@ __FBSDID("$FreeBSD$");
 #include <string.h>
 #include <unistd.h>
 
-#include <sys/diskmbr.h>
+#include <sys/disk/mbr.h>
 
 #include "endian.h"
 #include "image.h"
 #include "mkimg.h"
 #include "scheme.h"
 
-#ifndef DOSPTYP_FAT16B
-#define	DOSPTYP_FAT16B	0x06
-#endif
-#ifndef DOSPTYP_FAT32
-#define	DOSPTYP_FAT32	0x0b
-#endif
-#ifndef DOSPTYP_PPCBOOT
-#define	DOSPTYP_PPCBOOT	0x41
-#endif
-#ifndef DOSPTYP_EFI
-#define	DOSPTYP_EFI	0xef
-#endif
-
 static struct mkimg_alias mbr_aliases[] = {
     {	ALIAS_EBR, ALIAS_INT2TYPE(DOSPTYP_EXT) },
     {	ALIAS_EFI, ALIAS_INT2TYPE(DOSPTYP_EFI) },
-    {	ALIAS_FAT16B, ALIAS_INT2TYPE(DOSPTYP_FAT16B) },
+    {	ALIAS_FAT16B, ALIAS_INT2TYPE(DOSPTYP_FAT16) },
     {	ALIAS_FAT32, ALIAS_INT2TYPE(DOSPTYP_FAT32) },
     {	ALIAS_FREEBSD, ALIAS_INT2TYPE(DOSPTYP_386BSD) },
     {	ALIAS_NTFS, ALIAS_INT2TYPE(DOSPTYP_NTFS) },
