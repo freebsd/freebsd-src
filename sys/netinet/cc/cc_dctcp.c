4e7f755377b2cc827bd63cfcbb67797c8928b249
Hiren Panchasara <hiren@FreeBSD.org>
2016-10-25 05:45:47 +0000

    FreeBSD tcp stack used to inform respective congestion control module about the loss event but not use or obay the recommendations i.e. values set by it in some cases.
    Here is an attempt to solve that confusion by following relevant RFCs/drafts.
    Stack only sets congestion window/slow start threshold values when there is no
    CC module availalbe to take that action. All CC modules are inspected and
    updated when needed to take appropriate action on loss.
    
    tcp_stacks/fastpath module has been updated to adapt these changes.
    
    Note: Probably, the most significant change would be to not bring congestion
    window down to 1MSS on a loss signaled by 3-duplicate acks and letting
    respective CC decide that value.
    
    In collaboration with:	Matt Macy <mmacy at nextbsd dot org>
    Discussed on:		transport@ mailing list
    Reviewed by:		jtl
    MFC after:		1 month
    Sponsored by:		Limelight Networks
    Differential Revision:	https://reviews.freebsd.org/D8225

--- sys/netinet/cc/cc_dctcp.c
+++ sys/netinet/cc/cc_dctcp.c
@@ -230,10 +230,11 @@ static void
 dctcp_cong_signal(struct cc_var *ccv, uint32_t type)
 {
 	struct dctcp *dctcp_data;
-	u_int win, mss;
+	uint32_t cwin;
+	u_int mss;
 
 	dctcp_data = ccv->cc_data;
-	win = CCV(ccv, snd_cwnd);
+	cwin = CCV(ccv, snd_cwnd);
 	mss = CCV(ccv, t_maxseg);
 
 	switch (type) {
@@ -241,16 +242,16 @@ dctcp_cong_signal(struct cc_var *ccv, uint32_t type)
 		if (!IN_FASTRECOVERY(CCV(ccv, t_flags))) {
 			if (!IN_CONGRECOVERY(CCV(ccv, t_flags))) {
 				CCV(ccv, snd_ssthresh) = mss *
-				    max(win / 2 / mss, 2);
+				    max(cwin / 2 / mss, 2);
 				dctcp_data->num_cong_events++;
 			} else {
 				/* cwnd has already updated as congestion
 				 * recovery. Reverse cwnd value using
 				 * snd_cwnd_prev and recalculate snd_ssthresh
 				 */
-				win = CCV(ccv, snd_cwnd_prev);
+				cwin = CCV(ccv, snd_cwnd_prev);
 				CCV(ccv, snd_ssthresh) =
-				    max(win / 2 / mss, 2) * mss;
+				    max(cwin / 2 / mss, 2) * mss;
 			}
 			ENTER_RECOVERY(CCV(ccv, t_flags));
 		}
@@ -260,18 +261,18 @@ dctcp_cong_signal(struct cc_var *ccv, uint32_t type)
 		 * Save current snd_cwnd when the host encounters both
 		 * congestion recovery and fast recovery.
 		 */
-		CCV(ccv, snd_cwnd_prev) = win;
+		CCV(ccv, snd_cwnd_prev) = cwin;
 		if (!IN_CONGRECOVERY(CCV(ccv, t_flags))) {
 			if (V_dctcp_slowstart &&
 			    dctcp_data->num_cong_events++ == 0) {
 				CCV(ccv, snd_ssthresh) =
-				    mss * max(win / 2 / mss, 2);
+				    mss * max(cwin / 2 / mss, 2);
 				dctcp_data->alpha = MAX_ALPHA_VALUE;
 				dctcp_data->bytes_ecn = 0;
 				dctcp_data->bytes_total = 0;
 				dctcp_data->save_sndnxt = CCV(ccv, snd_nxt);
 			} else
-				CCV(ccv, snd_ssthresh) = max((win - ((win *
+				CCV(ccv, snd_ssthresh) = max((cwin - ((cwin *
 				    dctcp_data->alpha) >> 11)) / mss, 2) * mss;
 			CCV(ccv, snd_cwnd) = CCV(ccv, snd_ssthresh);
 			ENTER_CONGRECOVERY(CCV(ccv, t_flags));
@@ -284,6 +285,8 @@ dctcp_cong_signal(struct cc_var *ccv, uint32_t type)
 			dctcp_update_alpha(ccv);
 			dctcp_data->save_sndnxt += CCV(ccv, t_maxseg);
 			dctcp_data->num_cong_events++;
+			CCV(ccv, snd_ssthresh) = max(2 * mss, cwin / 2);
+			CCV(ccv, snd_cwnd) = mss;
 		}
 		break;
 	}
