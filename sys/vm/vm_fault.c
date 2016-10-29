3e52e88668c7fdc128a65562a6787f72e41c3f56
alc <alc@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-10-29 19:22:38 +0000

    With one exception, "hardfault" is used like a "bool".  Change that exception and make it a "bool".
    Reviewed by:	kib
    MFC after:	7 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@308096 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/vm/vm_fault.c
+++ sys/vm/vm_fault.c
@@ -295,20 +295,20 @@ vm_fault_hold(vm_map_t map, vm_offset_t vaddr, vm_prot_t fault_type,
 	boolean_t dead, growstack, is_first_object_locked, wired;
 	int map_generation;
 	vm_object_t next_object;
-	int hardfault;
 	struct faultstate fs;
 	struct vnode *vp;
 	vm_offset_t e_end, e_start;
 	vm_page_t m;
 	int ahead, behind, cluster_offset, error, locked, rv;
 	u_char behavior;
+	bool hardfault;
 
-	hardfault = 0;
 	growstack = TRUE;
 	PCPU_INC(cnt.v_vm_faults);
 	fs.vp = NULL;
 	faultcount = 0;
 	nera = -1;
+	hardfault = false;
 
 RetryFault:;
 
@@ -708,7 +708,7 @@ vnode_locked:
 			    &behind, &ahead);
 			if (rv == VM_PAGER_OK) {
 				faultcount = behind + 1 + ahead;
-				hardfault++;
+				hardfault = true;
 				break; /* break to PAGE HAS BEEN FOUND */
 			}
 			if (rv == VM_PAGER_ERROR)
