5e4cb227f870199a67f7c3652a982dbae2128acb
Brooks Davis <brooks@one-eyed-alien.net>
2016-11-02 18:55:23 +0000

    Add a proper implementation of atomics for jemalloc.
    Submitted by:	@davidchisnall

--- contrib/jemalloc/include/jemalloc/internal/atomic.h
+++ contrib/jemalloc/include/jemalloc/internal/atomic.h
@@ -169,8 +169,8 @@ atomic_add_uint64(uint64_t *p, uint64_t x)
 #ifndef __CHERI_PURE_CAPABILITY__
 	return (atomic_fetchadd_long(p, (unsigned long)x) + x);
 #else
-	/* XXX: not atomic! */
-	return (*p += x);
+	return (__c11_atomic_fetch_add((_Atomic(uint64_t)*)p, x,
+	    __ATOMIC_SEQ_CST) + x);
 #endif
 }
 
@@ -183,8 +183,8 @@ atomic_sub_uint64(uint64_t *p, uint64_t x)
 #ifndef __CHERI_PURE_CAPABILITY__
 	return (atomic_fetchadd_long(p, (unsigned long)(-(long)x)) - x);
 #else
-	/* XXX: not atomic! */
-	return (*p -= x);
+	return (__c11_atomic_fetch_sub((_Atomic(uint64_t)*)p, x,
+	    __ATOMIC_SEQ_CST) - x);
 #endif
 }
 
@@ -551,11 +551,23 @@ atomic_cas_p(void **p, void *c, void *s)
 	return (atomic_cas_uint32((uint32_t *)p, (uint32_t)c, (uint32_t)s));
 #endif
 #else
-	/* XXX: not atomic! */
-	if (*p != c)
-		return (true);
-	*p = s;
-		return (false);
+	void *old;
+	long result, cmp;
+	__asm__ volatile(
+	    "cllc	%[old], %[addr]\n"
+	    "cexeq	%[cmp], %[old], %[expected]\n"
+	    "bnez	%[cmp], 1f\n"
+	    "nop\n"
+	    "b 2f\n"
+	    "cscc	%[result], %[new], %[addr]\n"
+	    "1:\n"
+	    "move	%[result], $zero\n"
+	    "2:"
+	    : [old] "=C" (old), [result] "=r" (result), [cmp] "=r" (cmp)
+	    : [addr] "C" (p), [expected] "r" (c), [new] "C" (s)
+	    : "memory");
+	return (result);
+
 #endif
 }
 
@@ -563,9 +575,15 @@ atomic_cas_p(void **p, void *c, void *s)
 JEMALLOC_INLINE void *
 atomic_read_p(void **p)
 {
-
-	/* XXX: not atomic! */
-	return (*p);
+	void *result;
+	__asm__ volatile(
+	    "sync\n"
+	    "clc	%[out], $zero, 0(%[addr])\n"
+	    "sync\n"
+	    : [out] "=C" (result)
+	    : [addr] "C" (p)
+	    : "memory");
+	return (result);
 }
 #endif
 
@@ -580,8 +598,13 @@ atomic_write_p(void **p, const void *x)
 	atomic_write_uint32((uint32_t *)p, (uint32_t)x);
 #endif
 #else
-	/* XXX: not atomic! */
-	*p = (void *)(uintptr_t)x;
+	__asm__ volatile(
+	    "sync\n"
+	    "csc	%[in], $zero, 0(%[addr])\n"
+	    "sync\n"
+	    : [in] "=C" (x), [addr] "=C" (p)
+	    :
+	    : "memory");
 #endif
 }
 
