22b759bfd8a1206430d99328f19713db6dd2fdbf
Michal Meloun <mmel@FreeBSD.org>
2016-11-05 10:56:32 +0000

    MFC r306447,r306477:
      r306447:
        TEGRA: Rename (cut & pasted) genahci to tegra_ahci. Make device class
        definition static.
      r306477:
        TEGRA: Prepare Tegra subtree for inclusion into ARM generic kernel.
         - use DEFINE_CLASS_0() for driver classes - unify driver names - cleanup
         driver definitions and bindings

--- sys/arm/nvidia/tegra124/tegra124_pmc.c
+++ sys/arm/nvidia/tegra124/tegra124_pmc.c
@@ -555,12 +555,8 @@ static device_method_t tegra124_pmc_methods[] = {
 	DEVMETHOD_END
 };
 
-static driver_t tegra124_pmc_driver = {
-	"tegra124_pmc",
-	tegra124_pmc_methods,
-	sizeof(struct tegra124_pmc_softc),
-};
-
 static devclass_t tegra124_pmc_devclass;
+static DEFINE_CLASS_0(pmc, tegra124_pmc_driver, tegra124_pmc_methods,
+    sizeof(struct tegra124_pmc_softc));
 EARLY_DRIVER_MODULE(tegra124_pmc, simplebus, tegra124_pmc_driver,
-    tegra124_pmc_devclass, 0, 0, 70);
+    tegra124_pmc_devclass, NULL, NULL, 70);
