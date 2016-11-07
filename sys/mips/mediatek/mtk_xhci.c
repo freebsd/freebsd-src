ea512fc24698cb90f6b4456e0a76fba1ca994e3a
hselasky <hselasky@FreeBSD.org>
2016-11-07 08:36:06 +0000

    MFC r307518: Fix device delete child function.
    When detaching device trees parent devices must be detached prior to
    detaching its children. This is because parent devices can have
    pointers to the child devices in their softcs which are not
    invalidated by device_delete_child(). This can cause use after free
    issues and panic().
    
    Device drivers implementing trees, must ensure its detach function
    detaches or deletes all its children before returning.
    
    While at it remove now redundant device_detach() calls before
    device_delete_child() and device_delete_children(), mostly in
    the USB controller drivers.
    
    Tested by:		Jan Henrik Sylvester <me@janh.de>
    Reviewed by:		jhb
    Differential Revision:	https://reviews.freebsd.org/D8070

--- sys/mips/mediatek/mtk_xhci.c
+++ sys/mips/mediatek/mtk_xhci.c
@@ -161,14 +161,8 @@ static int
 mtk_xhci_fdt_detach(device_t self)
 {
 	struct xhci_softc *sc = device_get_softc(self);
-	device_t bdev;
 	int err;
 
-	if (sc->sc_bus.bdev) {
-		bdev = sc->sc_bus.bdev;
-		device_detach(bdev);
-		device_delete_child(self, bdev);
-	}
 	/* during module unload there are lots of children leftover */
 	device_delete_children(self);
 
