98eee1f35d56897f8e7eaf0d42e900f2af03bd81
andrew <andrew@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2016-11-14 12:03:08 +0000

    Use the correct OF_getencprop over OF_getprop + fdt32_to_cpu to read integer data from the device tree.
    Sponsored by:	ABT Systems Ltd
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@308640 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/arm/ti/am335x/am335x_lcd_syscons.c
+++ sys/arm/ti/am335x/am335x_lcd_syscons.c
@@ -382,13 +382,13 @@ am335x_syscons_configure(int flags)
 	root = OF_finddevice("/");
 	if ((root != 0) && 
 	    (display = am335x_syscons_find_panel_node(root))) {
-		if ((OF_getprop(display, "panel_width", 
-		    &cell, sizeof(cell))) > 0)
-			va_sc->width = (int)fdt32_to_cpu(cell);
+		if ((OF_getencprop(display, "panel_width", &cell,
+		    sizeof(cell))) > 0)
+			va_sc->width = cell;
 
-		if ((OF_getprop(display, "panel_height", 
-		    &cell, sizeof(cell))) > 0)
-			va_sc->height = (int)fdt32_to_cpu(cell);
+		if ((OF_getencprop(display, "panel_height", &cell,
+		    sizeof(cell))) > 0)
+			va_sc->height = cell;
 	}
 
 	if (va_sc->width == 0)
