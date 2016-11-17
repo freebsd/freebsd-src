00b5ffde8e0f31c936d10583daa9a958e1d1ebbb
Gleb Smirnoff <glebius@FreeBSD.org>
2016-11-17 21:36:18 +0000

    Add flag SF_USER_READAHEAD to sendfile(2).  When specified, the syscall won't do any speculations about readahead, and use exactly the amount of readahead specified by user.  E.g. setting SF_FLAGS(0, SF_USER_READAHEAD) will guarantee that no readahead at all will be performed.

--- sys/sys/socket.h
+++ sys/sys/socket.h
@@ -590,6 +590,7 @@ struct sf_hdtr {
 #define	SF_NODISKIO     0x00000001
 #define	SF_MNOWAIT	0x00000002	/* obsolete */
 #define	SF_SYNC		0x00000004
+#define	SF_USER_READAHEAD	0x00000008
 #define	SF_NOCACHE	0x00000010
 #define	SF_FLAGS(rh, flags)	(((rh) << 16) | (flags))
 
