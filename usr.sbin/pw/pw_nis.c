99d5e08d9d0e066585a4c9c8efe62d2138514fd2
asomers <asomers@FreeBSD.org>
2016-11-18 22:28:57 +0000

    MFC r307752
    Close some file descriptor leaks in pw

--- usr.sbin/pw/pw_nis.c
+++ usr.sbin/pw/pw_nis.c
@@ -34,6 +34,7 @@ static const char rcsid[] =
 #include <err.h>
 #include <pwd.h>
 #include <libutil.h>
+#include <unistd.h>
 
 #include "pw.h"
 
@@ -63,8 +64,10 @@ pw_nisupdate(const char * path, struct passwd * pwd, char const * user)
 	}
 	if (pw_copy(pfd, tfd, pw, old_pw) == -1) {
 		pw_fini();
+		close(tfd);
 		err(1, "pw_copy()");
 	}
+	close(tfd);
 	if (chmod(pw_tempname(), 0644) == -1)
 		err(1, "chmod()");
 	if (rename(pw_tempname(), path) == -1)
