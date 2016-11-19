35dfb8cb68786de6fbc7ef126a1e4fa585884b6b
Michael Tuexen <tuexen@FreeBSD.org>
2016-11-19 14:45:08 +0000

    Ensure that TCP state changes to state-closing are reported via dtrace. This does not cover state changes from TIME-WAIT.
    Reviewed by:		gnn
    MFC after:		3 weeks
    Sponsored by:		Netflix
    Differential Revision:	https://reviews.freebsd.org/D8443

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -2206,7 +2206,6 @@ tcp_do_segment(struct mbuf *m, struct tcphdr *th, struct socket *so,
 				case TCPS_LAST_ACK:
 					so->so_error = ECONNRESET;
 				close:
-					tcp_state_change(tp, TCPS_CLOSED);
 					/* FALLTHROUGH */
 				default:
 					tp = tcp_close(tp);
