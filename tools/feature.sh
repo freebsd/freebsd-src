a3b04abd47b2ad0f4de51a7c0d98c05ef4c47af7
cognet <cognet@FreeBSD.org>
2016-11-28 20:22:32 +0000

    Remove files that won't be used during FreeBSD build.

--- tools/feature.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# This will generate the list of feature flags for implemented symbols.
-
-echo '/* DO NOT EDIT. This is auto-generated from feature.sh */'
-nm ../regressions/ck_pr/validate/ck_pr_cas|cut -d ' ' -f 3|sed s/ck_pr/ck_f_pr/|awk '/^ck_f_pr/ {print "#define " toupper($1);}'|sort
