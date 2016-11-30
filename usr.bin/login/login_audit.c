83d1024582063dfb7e53a93b0e371d48ef984b24
rwatson <rwatson@FreeBSD.org>
2016-11-30 14:02:36 +0000

    Clarify warning message when failing to configure audit on user login: when au_user_mask() fails, it's not a failure to set the audit mask, but to calculate the audit mask -- and hence a condfiguration-file issue (of some sort).
    MFC after:	3 days
    Sponsored by:	DARPA, AFRL

--- usr.bin/login/login_audit.c
+++ usr.bin/login/login_audit.c
@@ -80,7 +80,7 @@ au_login_success(void)
 
 	/* Compute and set the user's preselection mask. */
 	if (au_user_mask(pwd->pw_name, &aumask) == -1)
-		errx(1, "could not set audit mask");
+		errx(1, "could not calculate audit mask");
 
 	/* Set the audit info for the user. */
 	auinfo.ai_auid = uid;
