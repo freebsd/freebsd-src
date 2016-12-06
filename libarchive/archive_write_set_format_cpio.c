59f85782a2b386a7803923f3c9c1dc76766fd70f
mm <mm@FreeBSD.org>
2016-12-06 00:35:20 +0000

    Update vendor/libarchive to git ddb3954bfdb9a0a98d50fb1c50cbecb603d9adf0
    Vendor bugfixes:
    
    libarchive #831:
      Spelling fixes
    libarchive #832:
      Relax sanity checks of number fields in tar header even more
    OSS-Fuzz #16:
      Fix possible hang in uudecode_filter_read()
    OSS-Fuzz #220:
      Reject an 'ar' filename table larger than 1GB or a filename larger
      than 1MB.

--- libarchive/archive_write_set_format_cpio.c
+++ libarchive/archive_write_set_format_cpio.c
@@ -289,7 +289,7 @@ write_header(struct archive_write *a, struct archive_entry *entry)
 	sconv = get_sconv(a);
 
 #if defined(_WIN32) && !defined(__CYGWIN__)
-	/* Make sure the path separators in pahtname, hardlink and symlink
+	/* Make sure the path separators in pathname, hardlink and symlink
 	 * are all slash '/', not the Windows path separator '\'. */
 	entry_main = __la_win_entry_in_posix_pathseparator(entry);
 	if (entry_main == NULL) {
