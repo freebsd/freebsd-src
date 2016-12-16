5290398111e3cef5f2c770265af0a28de5e5d78a
manu <manu@FreeBSD.org>
2016-12-16 15:37:18 +0000

    MFC r309063:
    Test that the emac device is enabled in probe function

--- sys/arm/allwinner/if_emac.c
+++ sys/arm/allwinner/if_emac.c
@@ -784,6 +784,9 @@ static int
 emac_probe(device_t dev)
 {
 
+	if (!ofw_bus_status_okay(dev))
+		return (ENXIO);
+
 	if (!ofw_bus_is_compatible(dev, "allwinner,sun4i-a10-emac"))
 		return (ENXIO);
 
