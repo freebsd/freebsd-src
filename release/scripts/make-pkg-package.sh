aeb76c0bb7d53e6ee65bc12c6b75ec8078cbd833
Matthew Seaman <matthew@FreeBSD.org>
2016-12-16 23:05:47 +0000

    Revert r309339, thus re-instating r309314
    The original problem with conflicting definitions of ${PKG_CMD} was
    solved by r427523 in ports (see https://reviews.freebsd.org/D8677), so
    this should be safe now.
    
    Reviewed by:	gjb
    Approved by:	gjb
    Differential Revision:	https://reviews.freebsd.org/D8120

--- release/scripts/make-pkg-package.sh
+++ release/scripts/make-pkg-package.sh
@@ -8,6 +8,7 @@ eval $(make -C ${SRCDIR} TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH} buildenvvar
 export WRKDIRPREFIX=/tmp/ports.${TARGET}
 export WSTAGEDIR=${WSTAGEDIR}
 export REPODIR=${REPODIR}
+export PKG_CMD=${PKG_CMD}
 export PKG_VERSION=${PKG_VERSION}
 export WRKDIR=$(make -C ${PORTSDIR}/ports-mgmt/pkg -V WRKDIR)
 
@@ -15,11 +16,11 @@ make -C ${PORTSDIR}/ports-mgmt/pkg TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH} \
 	CONFIGURE_ARGS="--host=$(uname -m)-portbld-freebsd${REVISION}" \
 	stage create-manifest
 
-pkg -o ABI_FILE=${WSTAGEDIR}/bin/sh \
+${PKG_CMD} -o ABI_FILE=${WSTAGEDIR}/bin/sh \
 	create -v -m ${WRKDIR}/.metadir/ \
 	-r ${WRKDIR}/stage \
 	-p ${WRKDIR}/.PLIST.mktmp \
-	-o ${REPODIR}/$(pkg -o ABI_FILE=${WSTAGEDIR}/bin/sh config ABI)/${PKG_VERSION}
-mkdir -p ${REPODIR}/$(pkg -o ABI_FILE=${WSTAGEDIR}/bin/sh config ABI)/${PKG_VERSION}/Latest/
-cd ${REPODIR}/$(pkg -o ABI_FILE=${WSTAGEDIR}/bin/sh config ABI)/${PKG_VERSION}/Latest/ && \
+	-o ${REPODIR}/$(${PKG_CMD} -o ABI_FILE=${WSTAGEDIR}/bin/sh config ABI)/${PKG_VERSION}
+mkdir -p ${REPODIR}/$(${PKG_CMD} -o ABI_FILE=${WSTAGEDIR}/bin/sh config ABI)/${PKG_VERSION}/Latest/
+cd ${REPODIR}/$(${PKG_CMD} -o ABI_FILE=${WSTAGEDIR}/bin/sh config ABI)/${PKG_VERSION}/Latest/ && \
 	ln -s ../pkg-*.txz
