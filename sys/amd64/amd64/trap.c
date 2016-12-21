0710dc1d383c69020a3486e95cc29539f597f5cb
kib <kib@FreeBSD.org>
2016-12-21 11:34:16 +0000

    MFC r310205: Fix typo.

--- sys/amd64/amd64/trap.c
+++ sys/amd64/amd64/trap.c
@@ -971,7 +971,7 @@ amd64_syscall(struct thread *td, int traced)
 	}
 
 	KASSERT(PCB_USER_FPU(td->td_pcb),
-	    ("System call %s returing with kernel FPU ctx leaked",
+	    ("System call %s returning with kernel FPU ctx leaked",
 	     syscallname(td->td_proc, sa.code)));
 	KASSERT(td->td_pcb->pcb_save == get_pcb_user_save_td(td),
 	    ("System call %s returning with mangled pcb_save",
