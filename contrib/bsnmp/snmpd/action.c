650eded91712b610d20fc7a7981fc8934318dcc1
Enji Cooper <ngie@FreeBSD.org>
2016-12-23 07:55:13 +0000

    Sort #includes per style(9)
    No functional change
    
    MFC after:	3 days

--- contrib/bsnmp/snmpd/action.c
+++ contrib/bsnmp/snmpd/action.c
@@ -38,12 +38,12 @@
 #include <sys/sysctl.h>
 #include <sys/un.h>
 #include <sys/utsname.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <string.h>
 #include <ctype.h>
 #include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <syslog.h>
 
 #include "snmpmod.h"
