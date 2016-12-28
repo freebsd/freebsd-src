56ec54c70fc3da7809ea2707123804b37cce62a5
Andrew Rybchenko <arybchik@FreeBSD.org>
2016-12-28 11:25:17 +0000

    sfxge(4): cleanup: add missing spaces
    Found by DPDK checkpatch.sh
    
    Sponsored by:   Solarflare Communications, Inc.
    MFC after:      2 days

--- sys/dev/sfxge/common/medford_nic.c
+++ sys/dev/sfxge/common/medford_nic.c
@@ -66,7 +66,7 @@ efx_mcdi_get_rxdp_config(
 		/* RX DMA end padding is disabled */
 		end_padding = 0;
 	} else {
-		switch(MCDI_OUT_DWORD_FIELD(req, GET_RXDP_CONFIG_OUT_DATA,
+		switch (MCDI_OUT_DWORD_FIELD(req, GET_RXDP_CONFIG_OUT_DATA,
 					    GET_RXDP_CONFIG_OUT_PAD_HOST_LEN)) {
 		case MC_CMD_SET_RXDP_CONFIG_IN_PAD_HOST_64:
 			end_padding = 64;
