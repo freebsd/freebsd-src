66631bf2868b4aa057ce77206b4cb0e3715c86e2
kan <kan@FreeBSD.org>
2016-12-28 02:55:26 +0000

    Implement pmap_change_attr and related APIs on MIPS
    On platforms that have uncached-accelerate cache attribute, map it
    to VM_MEMATTR_WRITE_COMBINING. Otherwise, leave write comining
    undefined.
    
    Reviewed by:	adrian, jhb (glance)
    Differential Revision:	https://reviews.freebsd.org/D8894

--- sys/mips/include/cpuregs.h
+++ sys/mips/include/cpuregs.h
@@ -171,6 +171,10 @@
 #define	MIPS_CCA_CACHED		MIPS_CCA_CCS
 #endif
 
+#if defined(CPU_XBURST)
+#define	MIPS_CCA_UA		0x01
+#endif
+
 #ifndef	MIPS_CCA_UNCACHED
 #define	MIPS_CCA_UNCACHED	MIPS_CCA_UC
 #endif
@@ -185,6 +189,16 @@
 #define	MIPS_CCA_CACHED	MIPS_CCA_CC
 #else
 #define	MIPS_CCA_CACHED	MIPS_CCA_C
+#endif
+#endif
+
+/*
+ * Use uncached-accelerated mode for write-combining maps, if one is defined,
+ * otherwise fall back to uncached
+ */
+#ifndef MIPS_CCA_WC
+#ifdef MIPS_CCA_UA
+#define	MIPS_CCA_WC MIPS_CCA_UA
 #endif
 #endif
 
