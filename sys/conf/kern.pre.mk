f369aff32d9988b725c400a5173bb799e4f31b5b
Alexander Kabaev <kan@FreeBSD.org>
2016-12-30 00:34:52 +0000

    Support mips[*]hf variants in config files
    Recognize new MACHINE_ARCH names now as we have added hardfloat support.
    Switch JZ4780 to mipselhf and remove all uses of TARGET_ARCH in kernel
    .mk files.
    
    Reviewed by:	adrian
    Differential Revision:	https://reviews.freebsd.org/D8989

--- sys/conf/kern.pre.mk
+++ sys/conf/kern.pre.mk
@@ -74,7 +74,7 @@ CFLAGS+= ${INCLUDES} -D_KERNEL -DHAVE_KERNEL_OPTION_HEADERS -include opt_global.
 CFLAGS_PARAM_INLINE_UNIT_GROWTH?=100
 CFLAGS_PARAM_LARGE_FUNCTION_GROWTH?=1000
 .if ${MACHINE_CPUARCH} == "mips"
-CFLAGS_ARCH_PARAMS?=--param max-inline-insns-single=1000 -DMACHINE_ARCH='"${TARGET_ARCH}"'
+CFLAGS_ARCH_PARAMS?=--param max-inline-insns-single=1000 -DMACHINE_ARCH='"${MACHINE_ARCH}"'
 .endif
 CFLAGS.gcc+= -fno-common -fms-extensions -finline-limit=${INLINE_LIMIT}
 CFLAGS.gcc+= --param inline-unit-growth=${CFLAGS_PARAM_INLINE_UNIT_GROWTH}
