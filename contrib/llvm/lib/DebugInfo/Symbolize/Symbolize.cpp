ded09321a88c5391479c2acbeaffbef2dfc124b7
Dimitry Andric <dim@FreeBSD.org>
2017-01-03 20:19:37 +0000

    Disable PDB support in LLVMSymbolizer for now, to avoid llvm-objdump pulling in all the PDB handling code.

--- contrib/llvm/lib/DebugInfo/Symbolize/Symbolize.cpp
+++ contrib/llvm/lib/DebugInfo/Symbolize/Symbolize.cpp
@@ -395,6 +395,7 @@ LLVMSymbolizer::getOrCreateModuleInfo(const std::string &ModuleName) {
     StringRef PDBFileName;
     auto EC = CoffObject->getDebugPDBInfo(DebugInfo, PDBFileName);
     if (!EC && DebugInfo != nullptr && !PDBFileName.empty()) {
+#if 0
       using namespace pdb;
       std::unique_ptr<IPDBSession> Session;
       if (auto Err = loadDataForEXE(PDB_ReaderType::DIA,
@@ -404,6 +405,11 @@ LLVMSymbolizer::getOrCreateModuleInfo(const std::string &ModuleName) {
         return std::move(Err);
       }
       Context.reset(new PDBContext(*CoffObject, std::move(Session)));
+#else
+      return make_error<StringError>(
+          "PDB support not compiled in",
+          std::make_error_code(std::errc::not_supported));
+#endif
     }
   }
   if (!Context)
