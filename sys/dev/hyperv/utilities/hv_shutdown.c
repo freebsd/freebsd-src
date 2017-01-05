8ad77cb22cb7c30eca5084b9a887ac3daefbb1e6
sephe <sephe@FreeBSD.org>
2017-01-05 08:04:04 +0000

    MFC 310315
        hyperv/ic: Inclusion cleanup
    
        Sponsored by:   Microsoft
        Differential Revision:  https://reviews.freebsd.org/D8847

--- sys/dev/hyperv/utilities/hv_shutdown.c
+++ sys/dev/hyperv/utilities/hv_shutdown.c
@@ -39,8 +39,6 @@ __FBSDID("$FreeBSD$");
 #include <dev/hyperv/utilities/vmbus_icreg.h>
 #include <dev/hyperv/utilities/vmbus_icvar.h>
 
-#include "vmbus_if.h"
-
 #define VMBUS_SHUTDOWN_FWVER_MAJOR	3
 #define VMBUS_SHUTDOWN_FWVER		\
 	VMBUS_IC_VERSION(VMBUS_SHUTDOWN_FWVER_MAJOR, 0)
