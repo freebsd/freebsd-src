9dee77ddf5b8635a7a2d39ebbb1414bfd8068b9a
rpokala <rpokala@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-01-05 03:08:57 +0000

    In the same vein as r311350, fix whitespace in handling of XPT_PATH_INQ in several more drivers.
    Sponsored by:	Panasas
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@311351 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/dpt/dpt_scsi.c
+++ sys/dev/dpt/dpt_scsi.c
@@ -1031,10 +1031,10 @@ dpt_action(struct cam_sim *sim, union ccb *ccb)
 		strlcpy(cpi->hba_vid, "DPT", HBA_IDLEN);
 		strlcpy(cpi->dev_name, cam_sim_name(sim), DEV_IDLEN);
 		cpi->unit_number = cam_sim_unit(sim);
-                cpi->transport = XPORT_SPI;
-                cpi->transport_version = 2;
-                cpi->protocol = PROTO_SCSI;
-                cpi->protocol_version = SCSI_REV_2;
+		cpi->transport = XPORT_SPI;
+		cpi->transport_version = 2;
+		cpi->protocol = PROTO_SCSI;
+		cpi->protocol_version = SCSI_REV_2;
 		cpi->ccb_h.status = CAM_REQ_CMP;
 		xpt_done(ccb);
 		break;
