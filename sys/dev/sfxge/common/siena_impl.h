3628b15bab0b55d53e4afa5ff6fb7977a5950dfd
arybchik <arybchik@FreeBSD.org>
2017-01-06 07:05:02 +0000

    MFC r310765
    sfxge(4): add support for firmware-verified NVRAM updates to the common code
    
    Submitted by:   Andy Moreton <amoreton at solarflare.com>
    Sponsored by:   Solarflare Communications, Inc.

--- sys/dev/sfxge/common/siena_impl.h
+++ sys/dev/sfxge/common/siena_impl.h
@@ -136,7 +136,7 @@ siena_nvram_partn_lock(
 	__in			efx_nic_t *enp,
 	__in			uint32_t partn);
 
-extern				void
+extern	__checkReturn		efx_rc_t
 siena_nvram_partn_unlock(
 	__in			efx_nic_t *enp,
 	__in			uint32_t partn);
@@ -208,7 +208,7 @@ siena_nvram_partn_write(
 	__out_bcount(size)	caddr_t data,
 	__in			size_t size);
 
-extern				void
+extern	__checkReturn		efx_rc_t
 siena_nvram_partn_rw_finish(
 	__in			efx_nic_t *enp,
 	__in			uint32_t partn);
