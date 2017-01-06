b806704804157bfac5c0b6253eff749bbc9e75db
dim <dim@FreeBSD.org>
2017-01-06 20:13:54 +0000

    Vendor import of libc++ trunk r291274: https://llvm.org/svn/llvm-project/libcxx/trunk@291274

--- test/libcxx/utilities/memory/util.dynamic.safety/get_pointer_safety_cxx03.pass.cpp
+++ test/libcxx/utilities/memory/util.dynamic.safety/get_pointer_safety_cxx03.pass.cpp
@@ -11,21 +11,29 @@
 
 // pointer_safety get_pointer_safety();
 
-// UNSUPPORTED: c++98, c++03
-
 #include <memory>
 #include <cassert>
 
+#include "test_macros.h"
 
+// libc++ doesn't offer std::pointer_safety in C++03 under the new ABI
+#if TEST_STD_VER < 11 && defined(_LIBCPP_ABI_POINTER_SAFETY_ENUM_TYPE)
+#define TEST_IS_UNSUPPORTED
+#endif
+
+#ifndef TEST_IS_UNSUPPORTED
 void test_pr26961() {
   std::pointer_safety d;
   d = std::get_pointer_safety();
   assert(d == std::get_pointer_safety());
 }
+#endif
 
 int main()
 {
+#ifndef TEST_IS_UNSUPPORTED
   {
+    // Test that std::pointer_safety is still offered in C++03 under the old ABI.
     std::pointer_safety r = std::get_pointer_safety();
     assert(r == std::pointer_safety::relaxed ||
            r == std::pointer_safety::preferred ||
@@ -34,4 +42,5 @@ int main()
   {
     test_pr26961();
   }
+#endif
 }
