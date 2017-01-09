639f59f02a03127173337ecf5b015d167af11bca
Ian Lepore <ian@FreeBSD.org>
2017-01-09 01:15:18 +0000

    Add support for non-removable media, and a quirk to use polling to detect card insert/remove events on controllers that don't implement the insert and remove interrupts.
    Bridge drivers can set a new slot option, SDHCI_NON_REMOVABLE, to indicate
    non-removable media (such as eMMC).  The sdhci driver will not enable
    insert/remove interrupts, and sdhci_generic_get_card_present() will always
    return true.
    
    Bridge drivers can set a new quirk, SDHCI_QUIRK_POLL_CARD_PRESENT, and the
    sdhci driver will not enable insert/remove interrupts, and instead will use
    a callout to poll the card-present status at 5 Hz.
    
    For bridge drivers that get notified of card insert/remove via gpio
    interrupts, there is a new sdhci_handle_card_present() function they can
    call from the gpio interrupt handler to inform the sdhci code of the event.
    
    In addition to adding these new features, the existing code to debounce card
    insertions was updated to use taskqueue_enqueue_timeout() instead of
    scheduling a callout to do the taskqueue_enqueue().  There is also now a
    comment explaining that insertion-debounce is what's going on -- it took me
    a long time to realize that's what the old sdhci_card_delay() routine was
    really doing.  There is no functional difference between the old and new
    debounce code (I hope!).

--- sys/dev/sdhci/sdhci.h
+++ sys/dev/sdhci/sdhci.h
@@ -65,6 +65,8 @@
 #define	SDHCI_QUIRK_DONT_SET_HISPD_BIT			(1<<15)
 /* Alternate clock source is required when supplying a 400 KHz clock. */
 #define	SDHCI_QUIRK_BCM577XX_400KHZ_CLKSRC		(1<<16)
+/* Card insert/remove interrupts don't work, polling required. */
+#define SDHCI_QUIRK_POLL_CARD_PRESENT				(1<<17)
 
 /*
  * Controller registers
@@ -273,8 +275,9 @@ struct sdhci_slot {
 	device_t	dev;		/* Slot device */
 	u_char		num;		/* Slot number */
 	u_char		opt;		/* Slot options */
-#define SDHCI_HAVE_DMA			1
-#define SDHCI_PLATFORM_TRANSFER		2
+#define	SDHCI_HAVE_DMA			0x01
+#define	SDHCI_PLATFORM_TRANSFER		0x02
+#define	SDHCI_NON_REMOVABLE		0x04
 	u_char		version;
 	int		timeout;	/* Transfer timeout */
 	uint32_t	max_clk;	/* Max possible freq */
@@ -284,7 +287,9 @@ struct sdhci_slot {
 	u_char		*dmamem;
 	bus_addr_t	paddr;		/* DMA buffer address */
 	struct task	card_task;	/* Card presence check task */
-	struct callout	card_callout;	/* Card insert delay callout */
+	struct timeout_task 
+			card_delayed_task;/* Card insert delayed task */
+	struct callout	card_poll_callout;/* Card present polling callout */
 	struct callout	timeout_callout;/* Card command/data response timeout */
 	struct mmc_host host;		/* Host parameters */
 	struct mmc_request *req;	/* Current request */
@@ -323,5 +328,6 @@ int sdhci_generic_release_host(device_t brdev, device_t reqdev);
 void sdhci_generic_intr(struct sdhci_slot *slot);
 uint32_t sdhci_generic_min_freq(device_t brdev, struct sdhci_slot *slot);
 bool sdhci_generic_get_card_present(device_t brdev, struct sdhci_slot *slot);
+void sdhci_handle_card_present(struct sdhci_slot *slot, bool is_present);
 
 #endif	/* __SDHCI_H__ */
