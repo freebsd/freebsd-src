665511c88fc5820248374a5915acdcf99f267021
dim <dim@FreeBSD.org>
2017-01-09 21:23:41 +0000

    Vendor import of libc++ trunk r291476: https://llvm.org/svn/llvm-project/libcxx/trunk@291476

--- test/std/strings/string.view/string_view.literals/literal2.fail.cpp
+++ test/std/strings/string.view/string_view.literals/literal2.fail.cpp
@@ -8,13 +8,14 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifdef __deallocate
-#if !defined(_LIBCPP_DISABLE_MACRO_CONFLICT_WARNINGS)
-#if defined(_MSC_VER) && !defined(__clang__)
-_LIBCPP_WARNING("macro __deallocate is incompatible with C++.  #undefining __deallocate")
-#else
-#warning: macro __deallocate is incompatible with C++.  #undefining __deallocate
-#endif
-#endif
-#undef __deallocate
-#endif
+// UNSUPPORTED: c++98, c++03, c++11
+// UNSUPPORTED: clang-3.3, clang-3.4, clang-3.5, clang-3.6, clang-3.7, clang-3.8, clang-3.9
+// UNSUPPORTED: apple-clang-6, apple-clang-7, apple-clang-8
+
+#include <string_view>
+#include <cassert>
+
+int main()
+{
+    std::string_view foo  =   ""sv;  // should fail w/conversion operator not found
+}
