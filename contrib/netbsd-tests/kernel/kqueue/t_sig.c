a16443ef0ffbf64987c6949440d36e3a41ff9d30
ngie <ngie@FreeBSD.org>
2017-01-14 09:45:46 +0000

    Clarify why sys/types.h #include is needed on FreeBSD

--- contrib/netbsd-tests/kernel/kqueue/t_sig.c
+++ contrib/netbsd-tests/kernel/kqueue/t_sig.c
@@ -35,7 +35,7 @@ __COPYRIGHT("@(#) Copyright (c) 2008\
 __RCSID("$NetBSD: t_sig.c,v 1.3 2017/01/13 21:30:41 christos Exp $");
 
 #ifdef __FreeBSD__
-#include <sys/types.h>
+#include <sys/types.h> /* for kqueue(2) */
 #endif
 #include <sys/event.h>
 #include <sys/ioctl.h>
