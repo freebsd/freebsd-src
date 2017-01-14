d89e7019975d18a50702f1d0bfb5d413cd9ec5ca
ngie <ngie@FreeBSD.org>
2017-01-14 10:23:05 +0000

    MFC r309464:
    Expect 01:main to fail
    
    Changes were made to ZFS in the past year with respect to how ACLs
    are handled, causing failures in this test. Mark it TODO so (hopefully)
    someone more knowledgeable (like mav or trasz) will fix the code or the
    test.
    
    PR:	212323

--- tests/sys/acl/01.sh
+++ tests/sys/acl/01.sh
@@ -81,7 +81,7 @@ perl $TESTDIR/run $TESTDIR/tools-nfs4-psarc.test >&2
 if [ $? -eq 0 ]; then
 	echo "ok 3"
 else
-	echo "not ok 3"
+	echo "not ok 3 # TODO: fails due to ACL changes in ZFS; bug 212323"
 fi
 
 echo "ok 4"
