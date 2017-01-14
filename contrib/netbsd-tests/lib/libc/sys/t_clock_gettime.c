04f40d86dbd10eb02436e8b31c16b66f056b6d8e
Enji Cooper <ngie@FreeBSD.org>
2017-01-14 08:36:43 +0000

    Diff reduce with upstream

--- contrib/netbsd-tests/lib/libc/sys/t_clock_gettime.c
+++ contrib/netbsd-tests/lib/libc/sys/t_clock_gettime.c
@@ -63,6 +63,7 @@ __RCSID("$NetBSD: t_clock_gettime.c,v 1.3 2017/01/13 21:30:41 christos Exp $");
 #include <sys/param.h>
 #include <sys/sysctl.h>
 
+
 #include <atf-c.h>
 #include <errno.h>
 #include <limits.h>
