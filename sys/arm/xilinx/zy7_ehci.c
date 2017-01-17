390511604275922b7c16263b7a98d43e9a81e6a6
gonzo <gonzo@FreeBSD.org>
2017-01-17 00:39:09 +0000

    [zynq] Fix panic on USB PHY initialization failure
    The Zedboard has a hardware bug where initialization of the USB PHY
    occasionally fails on boot-up. Fix regression in -CURRENT when
    kernel panics on such occasion. 11-RELEASE branch works fine
    
    PR:		215862
    Submitted by:	Thomas Skibo <thoma555-bsd@yahoo.com>

--- sys/arm/xilinx/zy7_ehci.c
+++ sys/arm/xilinx/zy7_ehci.c
@@ -325,13 +325,10 @@ zy7_ehci_detach(device_t dev)
 	/* during module unload there are lots of children leftover */
 	device_delete_children(dev);
 	
-	sc->sc_flags &= ~EHCI_SCFLG_DONEINIT;
-
-	if (sc->sc_irq_res && sc->sc_intr_hdl)
-		/* call ehci_detach() after ehci_init() called after
-		 * successful bus_setup_intr().
-		 */
+	if ((sc->sc_flags & EHCI_SCFLG_DONEINIT) != 0) {
 		ehci_detach(sc);
+		sc->sc_flags &= ~EHCI_SCFLG_DONEINIT;
+	}
 
 	if (sc->sc_irq_res) {
 		if (sc->sc_intr_hdl != NULL)
