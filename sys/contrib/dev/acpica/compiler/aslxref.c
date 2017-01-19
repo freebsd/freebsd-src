547c28e7560ec08838ea99975d8bc5f4df829fe3
jkim <jkim@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-01-19 22:07:21 +0000

    Merge ACPICA 20170119.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@312438 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/contrib/dev/acpica/compiler/aslxref.c
+++ sys/contrib/dev/acpica/compiler/aslxref.c
@@ -5,7 +5,7 @@
  *****************************************************************************/
 
 /*
- * Copyright (C) 2000 - 2016, Intel Corp.
+ * Copyright (C) 2000 - 2017, Intel Corp.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -490,20 +490,17 @@ XfNamespaceLocateBegin (
         RegisterNumber = Op->Asl.AmlOpcode - AML_ARG0; /* 0x68 through 0x6F */
         MethodArgs = Node->MethodArgs;
 
+        /* Mark this Arg as referenced */
+
+        MethodArgs[RegisterNumber].Flags |= ASL_ARG_REFERENCED;
+        MethodArgs[RegisterNumber].Op = Op;
+
         if (Op->Asl.CompileFlags & NODE_IS_TARGET)
         {
             /* Arg is being initialized */
 
             MethodArgs[RegisterNumber].Flags |= ASL_ARG_INITIALIZED;
-            MethodArgs[RegisterNumber].Op = Op;
-
-            return_ACPI_STATUS (AE_OK);
         }
-
-        /* Mark this Arg as referenced */
-
-        MethodArgs[RegisterNumber].Flags |= ASL_ARG_REFERENCED;
-        MethodArgs[RegisterNumber].Op = Op;
 
         return_ACPI_STATUS (AE_OK);
     }
