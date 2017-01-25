b4a18937dd5d28736960000c960b2ce9c5f4c1eb
loos <loos@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-01-25 19:04:08 +0000

    After the in_control() changes in r257692, an existing address is (intentionally) deleted first and then completely added again (so all the events, announces and hooks are given a chance to run).
    This cause an issue with CARP where the existing CARP data structure is
    removed together with the last address for a given VHID, which will cause
    a subsequent fail when the address is later re-added.
    
    This change fixes this issue by adding a new flag to keep the CARP data
    structure when an address is not being removed.
    
    There was an additional issue with IPv6 CARP addresses, where the CARP data
    structure would never be removed after a change and lead to VHIDs which
    cannot be destroyed.
    
    Reviewed by:	glebius
    Obtained from:	pfSense
    MFC after:	2 weeks
    Sponsored by:	Rubicon Communications, LLC (Netgate)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@312770 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet6/in6.c
+++ sys/netinet6/in6.c
@@ -554,8 +554,11 @@ in6_control(struct socket *so, u_long cmd, caddr_t data,
 		 */
 		if ((error = in6_update_ifa(ifp, ifra, ia, 0)) != 0)
 			goto out;
-		if (ia != NULL)
+		if (ia != NULL) {
+			if (ia->ia_ifa.ifa_carp)
+				(*carp_detach_p)(&ia->ia_ifa, true);
 			ifa_free(&ia->ia_ifa);
+		}
 		if ((ia = in6ifa_ifpwithaddr(ifp, &ifra->ifra_addr.sin6_addr))
 		    == NULL) {
 			/*
@@ -622,7 +625,7 @@ in6_control(struct socket *so, u_long cmd, caddr_t data,
 			 */
 			if ((error = nd6_prelist_add(&pr0, NULL, &pr)) != 0) {
 				if (carp_attached)
-					(*carp_detach_p)(&ia->ia_ifa);
+					(*carp_detach_p)(&ia->ia_ifa, false);
 				goto out;
 			}
 		}
@@ -1243,7 +1246,7 @@ in6_purgeaddr(struct ifaddr *ifa)
 	int plen, error;
 
 	if (ifa->ifa_carp)
-		(*carp_detach_p)(ifa);
+		(*carp_detach_p)(ifa, false);
 
 	/*
 	 * Remove the loopback route to the interface address.
