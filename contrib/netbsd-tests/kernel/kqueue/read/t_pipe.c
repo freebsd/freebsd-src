9cb0c8886a6a4640d7f27b5d0b5f7ce22cbe33c0
Enji Cooper <ngie@FreeBSD.org>
2017-02-09 21:12:26 +0000

    MFC r305483,r306030,r306031,r306033,r306036:
    r305483:
    
    Fix tests/sys/kqueue NetBSD tests on 32-bit platforms by using proper
    format specifier for pointers when printing them out with printf(3)
    
    Pointyhat to:	ngie
    
    r306030:
    
    Port vnode_leak_test:main to FreeBSD
    
    Use a simpler way of dumping kern.maxvnodes, i.e. `sysctl -n kern.maxvnodes`
    
    The awk filtering method employed in NetBSD doesn't work on FreeBSD
    
    r306031:
    
    Port contrib/netbsd-tests/fs/h_funcs.subr to FreeBSD
    
    Use kldstat -m to determine whether or not a filesystem is loaded. This works
    well with tmpfs, ufs, and zfs
    
    r306033:
    
    Port sizes_test and statvfs_test to FreeBSD
    
    Similar to r306030, use a simpler method for getting the value of
    `hw.pagesize`, i.e. `sysctl -n hw.pagesize`. The awk filtering method doesn't
    work on FreeBSD
    
    r306036:
    
    Port to mknod_test and readdir_test to FreeBSD
    
    The `mknod <file> p` command doesn't exist on FreeBSD, like on NetBSD. Use
    mkfifo instead to create named pipes (FIFOs).

--- contrib/netbsd-tests/kernel/kqueue/read/t_pipe.c
+++ contrib/netbsd-tests/kernel/kqueue/read/t_pipe.c
@@ -67,7 +67,11 @@ ATF_TC_BODY(pipe, tc)
 
 	RL(n = kevent(kq, NULL, 0, event, 1, NULL));
 	(void)printf("kevent num %d flags: %#x, fflags: %#x, data: "
+#ifdef __FreeBSD__
+	    "%" PRIdPTR "\n", n, event[0].flags, event[0].fflags, event[0].data);
+#else
 	    "%" PRId64 "\n", n, event[0].flags, event[0].fflags, event[0].data);
+#endif
 
 	RL(n = read(fds[0], buffer, event[0].data));
 	buffer[n] = '\0';
