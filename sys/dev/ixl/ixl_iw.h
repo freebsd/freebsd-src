cb6b8299fdda0ccd5c9c9b0d29cd9c005f6d780b
Eric Joyner <erj@FreeBSD.org>
2017-02-10 01:04:11 +0000

    ixl(4): Update to 1.7.12-k
    Refresh upstream driver before impending conversion to iflib.
    
    Major new features:
    
    - Support for Fortville-based 25G adapters
    - Support for I2C reads/writes
    
    (To prevent getting or sending corrupt data, you should set
    dev.ixl.0.debug.disable_fw_link_management=1 when using I2C
    [this will disable link!], then set it to 0 when done. The driver implements
    the SIOCGI2C ioctl, so ifconfig -v works for reading I2C data,
    but there are read_i2c and write_i2c sysctls under the .debug sysctl tree
    [the latter being useful for upper page support in QSFP+]).
    
    - Addition of an iWARP client interface (so the future iWARP driver for
      X722 devices can communicate with the base driver).
      - Compiling this option in is enabled by default, with "options IXL_IW" in
        GENERIC.
    
    Differential Revision:	https://reviews.freebsd.org/D9227
    Reviewed by:	sbruno
    MFC after:	2 weeks
    Sponsored by:	Intel Corporation

--- sys/dev/ixl/ixl_iw.h
+++ sys/dev/ixl/ixl_iw.h
@@ -32,40 +32,44 @@
 ******************************************************************************/
 /*$FreeBSD$*/
 
-#ifndef _I40E_DEVIDS_H_
-#define _I40E_DEVIDS_H_
+#ifndef _IXL_IW_H_
+#define _IXL_IW_H_
 
-/* Vendor ID */
-#define I40E_INTEL_VENDOR_ID		0x8086
+#define IXL_IW_MAX_USER_PRIORITY 8
 
-/* Device IDs */
-#define I40E_DEV_ID_SFP_XL710		0x1572
-#define I40E_DEV_ID_QEMU		0x1574
-#define I40E_DEV_ID_KX_B		0x1580
-#define I40E_DEV_ID_KX_C		0x1581
-#define I40E_DEV_ID_QSFP_A		0x1583
-#define I40E_DEV_ID_QSFP_B		0x1584
-#define I40E_DEV_ID_QSFP_C		0x1585
-#define I40E_DEV_ID_10G_BASE_T		0x1586
-#define I40E_DEV_ID_20G_KR2		0x1587
-#define I40E_DEV_ID_20G_KR2_A		0x1588
-#define I40E_DEV_ID_10G_BASE_T4		0x1589
-#define I40E_DEV_ID_25G_B		0x158A
-#define I40E_DEV_ID_25G_SFP28		0x158B
-#define I40E_DEV_ID_VF			0x154C
-#define I40E_DEV_ID_VF_HV		0x1571
-#define I40E_DEV_ID_X722_A0		0x374C
-#define I40E_DEV_ID_X722_A0_VF		0x374D
-#define I40E_DEV_ID_KX_X722		0x37CE
-#define I40E_DEV_ID_QSFP_X722		0x37CF
-#define I40E_DEV_ID_SFP_X722		0x37D0
-#define I40E_DEV_ID_1G_BASE_T_X722	0x37D1
-#define I40E_DEV_ID_10G_BASE_T_X722	0x37D2
-#define I40E_DEV_ID_SFP_I_X722		0x37D3
-#define I40E_DEV_ID_X722_VF		0x37CD
 
-#define i40e_is_40G_device(d)		((d) == I40E_DEV_ID_QSFP_A  || \
-					 (d) == I40E_DEV_ID_QSFP_B  || \
-					 (d) == I40E_DEV_ID_QSFP_C)
+struct ixl_iw_msix_mapping {
+	u8	itr_indx;
+	int	aeq_vector;
+	int	ceq_cnt;
+	int	*ceq_vector;
+};
 
-#endif /* _I40E_DEVIDS_H_ */
+struct ixl_iw_msix {
+	int	base;
+	int	count;
+};
+
+struct ixl_iw_pf {
+	void		*handle;
+	struct ifnet	*ifp;
+	device_t	dev;
+	struct resource	*pci_mem;
+	u8		pf_id;
+	u16		mtu;
+	struct ixl_iw_msix	iw_msix;
+	u16	qs_handle[IXL_IW_MAX_USER_PRIORITY];
+};
+
+struct ixl_iw_ops {
+	int (*init)(struct ixl_iw_pf *pf_info);
+	int (*stop)(struct ixl_iw_pf *pf_info);
+};
+
+int	ixl_iw_pf_reset(void *pf_handle);
+int	ixl_iw_pf_msix_init(void *pf_handle,
+	    struct ixl_iw_msix_mapping *msix_info);
+int	ixl_iw_register(struct ixl_iw_ops *iw_ops);
+int	ixl_iw_unregister(void);
+
+#endif /* _IXL_IW_H_ */
