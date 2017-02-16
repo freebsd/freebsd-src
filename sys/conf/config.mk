3fcffbaf5458e08a9dcb08895d1f7ed4e799e9f2
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-02-16 21:57:35 +0000

    Remove EISA bus support for add-in cards. Remove related kernel and compile options. Remove doxygen pointers to now deleted files. Remove EISA and VME as examples in bus_space.9.
    Retained EISA mode code for IO PIC and MPTABLES because that's not
    EISA bus, per se, and some people have abused EISA to mean "EISA-like
    behavior as opposed to ISA" rather than using it for EISA add-in
    cards.
    
    Relnotes: yes
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@313839 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/conf/config.mk
+++ sys/conf/config.mk
@@ -23,10 +23,6 @@ opt_inet6.h:
 opt_ratelimit.h:
 	@echo "#define RATELIMIT 1" > ${.TARGET}
 .endif
-.if ${MK_EISA} != "no"
-opt_eisa.h:
-	@echo "#define DEV_EISA 1" > ${.TARGET}
-.endif
 opt_mrouting.h:
 	echo "#define MROUTING 1" > ${.TARGET}
 opt_natm.h:
@@ -48,9 +44,6 @@ KERN_OPTS+= INET TCP_OFFLOAD
 .endif
 .if ${MK_INET6_SUPPORT} != "no"
 KERN_OPTS+= INET6
-.endif
-.if ${MK_EISA} != "no"
-KERN_OPTS+= DEV_EISA
 .endif
 .elif !defined(KERN_OPTS)
 KERN_OPTS!=cat ${KERNBUILDDIR}/opt*.h | awk '{print $$2;}' | sort -u
