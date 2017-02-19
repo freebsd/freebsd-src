75b9b9911b0f22617d40fef33621d96c1c6862c4
mav <mav@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-02-19 17:27:58 +0000

    Remove dead mentions of CAM target mode APIs from drivers.
    This makes grepping kernel for target mode implementation much easier.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@313949 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ahci/ahci.c
+++ sys/dev/ahci/ahci.c
@@ -2578,10 +2578,6 @@ ahciaction(struct cam_sim *sim, union ccb *ccb)
 		}
 		ahci_begin_transaction(ch, ccb);
 		return;
-	case XPT_EN_LUN:		/* Enable LUN as a target */
-	case XPT_TARGET_IO:		/* Execute target I/O request */
-	case XPT_ACCEPT_TARGET_IO:	/* Accept Host Target Mode CDB */
-	case XPT_CONT_TARGET_IO:	/* Continue Host Target I/O Connection*/
 	case XPT_ABORT:			/* Abort the specified CCB */
 		/* XXX Implement */
 		ccb->ccb_h.status = CAM_REQ_INVALID;
