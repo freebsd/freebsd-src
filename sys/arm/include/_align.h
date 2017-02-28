a017fce792a6303c1a29a89a935fa353cac91864
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-02-28 23:42:47 +0000

    Renumber copyright clause 4
    Renumber cluase 4 to 3, per what everybody else did when BSD granted
    them permission to remove clause 3. My insistance on keeping the same
    numbering for legal reasons is too pedantic, so give up on that point.
    
    Submitted by:	Jan Schaumann <jschauma@stevens.edu>
    Pull Request:	https://github.com/freebsd/freebsd/pull/96
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@314436 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/arm/include/_align.h
+++ sys/arm/include/_align.h
@@ -1,5 +1,4 @@
 /*-
- * Copyright (c) 2001 David E. O'Brien
  * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
  *
@@ -14,11 +13,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -34,23 +29,35 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	from: @(#)param.h	5.8 (Berkeley) 6/28/91
+ *	from: @(#)isa.h	5.7 (Berkeley) 5/9/91
  * $FreeBSD$
  */
 
-#ifndef _ARM_INCLUDE__ALIGN_H_
-#define	_ARM_INCLUDE__ALIGN_H_
+#ifndef _PC98_PC98_PC98_H_
+#define	_PC98_PC98_PC98_H_
 
 /*
- * Round p (pointer or byte index) up to the hardware-required alignment which
- * is sufficient for any data type, pointer or numeric.  The resulting type
- * is equivelent to arm's uintptr_t (but is purposely spelled "unsigned" here).
+ * PC98 Bus conventions
+ * modified for PC9801 by A.Kojima F.Ukai M.Ishii 
+ *			Kyoto University Microcomputer Club (KMC)
  */
-#if __ARM_ARCH >= 6
-#define	_ALIGNBYTES	(sizeof(int) - 1)
-#else
-#define	_ALIGNBYTES	(sizeof(long long) - 1)
-#endif
-#define	_ALIGN(p)	(((unsigned)(p) + _ALIGNBYTES) & ~_ALIGNBYTES)
 
-#endif /* !_ARM_INCLUDE__ALIGN_H_ */
+/*
+ * Input / Output Port Assignments -- PC98 IO address ... very dirty (^_^;
+ */
+
+#define	IO_ICU1		0x000		/* 8259A Interrupt Controller #1 */
+#define	IO_ICU2		0x008		/* 8259A Interrupt Controller #2 */
+#define	IO_RTC		0x020		/* 4990A RTC */
+#define	IO_SYSPORT	0x031		/* 8255A System Port */
+#define	IO_KBD		0x041		/* 8251A Keyboard */
+#define	IO_COM2		0x0B1		/* 8251A RS232C serial I/O (ext) */
+#define	IO_COM3		0x0B9		/* 8251A RS232C serial I/O (ext) */
+#define	IO_FDPORT	0x0BE		/* FD I/F port (1M<->640K,EMTON) */
+
+/*
+ * Input / Output Port Sizes
+ */
+#define	IO_KBDSIZE	16		/* 8042 Keyboard controllers */
+
+#endif /* !_PC98_PC98_PC98_H_ */
