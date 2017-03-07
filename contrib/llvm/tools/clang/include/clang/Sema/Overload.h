f35d779097559ef263686f3076d9eba3dec5e0e9
Dimitry Andric <dim@FreeBSD.org>
2017-03-07 21:44:05 +0000

    Pull in r291403 from upstream clang trunk (by Richard Smith):
      PR30305: Implement proposed DR resolution to prevent slicing via
      inherited constructor.
    
      The rule we use is that a construction of a class type T from an
      argument of type U cannot use an inherited constructor if U is the
      same as T or is derived from T (or if the initialization would first
      convert it to such a type). This (approximately) matches the rule in
      use by GCC, and matches the current proposed DR resolution.
    
    Pull in r291955 from upstream clang trunk (by Richard Smith):
    
      PR31606: Generalize our tentative DR resolution for inheriting
      copy/move constructors to better match the pre-P0136R1 behavior.
    
    Together, these fix an issue with C++ using declarations sometimes
    enabling illegal implicit casts.
    
    Direct commit to stable/11, since head already has clang 4.0.0, which
    includes this change.
    
    Reported by:	kami
    PR:		215969

--- contrib/llvm/tools/clang/include/clang/Sema/Overload.h
+++ contrib/llvm/tools/clang/include/clang/Sema/Overload.h
@@ -586,7 +586,11 @@ namespace clang {
     ovl_fail_enable_if,
 
     /// This candidate was not viable because its address could not be taken.
-    ovl_fail_addr_not_available
+    ovl_fail_addr_not_available,
+
+    /// This inherited constructor is not viable because it would slice the
+    /// argument.
+    ovl_fail_inhctor_slice,
   };
 
   /// OverloadCandidate - A single candidate in an overload set (C++ 13.3).
