80921123f1e70276dd588ffc30f90c37d505813d
cem <cem@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-03-07 21:40:01 +0000

    ufsread: Use appropriate UFS_MAXNAMLEN constant
    A follow-up to r313475.
    
    Submitted by:	Gaurav Gangalwar <Gaurav.Gangalwar at emc.com>
    Sponsored by:	Dell EMC Isilon
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@314882 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/boot/common/ufsread.c
+++ sys/boot/common/ufsread.c
@@ -121,7 +121,7 @@ fsfind(const char *name, ufs_ino_t * ino)
 static ufs_ino_t
 lookup(const char *path)
 {
-	static char name[MAXNAMLEN + 1];
+	static char name[UFS_MAXNAMLEN + 1];
 	const char *s;
 	ufs_ino_t ino;
 	ssize_t n;
@@ -135,7 +135,7 @@ lookup(const char *path)
 		if (!*path)
 			break;
 		for (s = path; *s && *s != '/'; s++);
-		if ((n = s - path) > MAXNAMLEN)
+		if ((n = s - path) > UFS_MAXNAMLEN)
 			return 0;
 		ls = *path == '?' && n == 1 && !*s;
 		memcpy(name, path, n);
