86a7bcea6018c84f894a399d344ea44668aad549
mav <mav@FreeBSD.org>
2017-03-07 17:41:08 +0000

    Add mechanism to unload CAM periph drivers.
    For now it allows to unload CTL kernel module if there are no target-capable
    SIMs in CAM.  As next step full teardown of CAM targets can be implemented.

--- sys/cam/cam_periph.c
+++ sys/cam/cam_periph.c
@@ -139,6 +139,38 @@ again:
 		(*drv->init)();
 }
 
+int
+periphdriver_unregister(void *data)
+{
+	struct periph_driver *drv = (struct periph_driver *)data;
+	int error, n;
+
+	/* If driver marked as early or it is late now, deinitialize it. */
+	if (((drv->flags & CAM_PERIPH_DRV_EARLY) != 0 && initialized > 0) ||
+	    initialized > 1) {
+		if (drv->deinit == NULL) {
+			printf("CAM periph driver '%s' doesn't have deinit.\n",
+			    drv->driver_name);
+			return (EOPNOTSUPP);
+		}
+		error = drv->deinit();
+		if (error != 0)
+			return (error);
+	}
+
+	xpt_lock_buses();
+	for (n = 0; n < nperiph_drivers && periph_drivers[n] != drv; n++)
+		;
+	KASSERT(n < nperiph_drivers,
+	    ("Periph driver '%s' was not registered", drv->driver_name));
+	for (; n + 1 < nperiph_drivers; n++)
+		periph_drivers[n] = periph_drivers[n + 1];
+	periph_drivers[n + 1] = NULL;
+	nperiph_drivers--;
+	xpt_unlock_buses();
+	return (0);
+}
+
 void
 periphdriver_init(int level)
 {
