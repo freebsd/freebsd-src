adf04fe2456bb5c353fed68b9ff64020128b200a
oshogbo <oshogbo@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-03-09 06:01:24 +0000

    Try to extract the RFC1048 data from PXE. If we get enough info we can skip the bootp(). It removes unnecessary DHCP request from pxeloader.
    Submitted by:	kczekirda
    Sponsored by:	Oktawave
    Initiated by:	Matthew Dillon
    Reviewed by:	smh, gnn, bapt, oshogbo
    MFC after:	3 weeks
    Differential Revision:	https://reviews.freebsd.org/D9847
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@314948 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libstand/bootp.c
+++ lib/libstand/bootp.c
@@ -344,6 +344,17 @@ bad:
 	return (-1);
 }
 
+int
+dhcp_try_rfc1048(u_char *cp, u_int len)
+{
+
+	expected_dhcpmsgtype = DHCPACK;
+	if (bcmp(vm_rfc1048, cp, sizeof(vm_rfc1048)) == 0) {
+		return (vend_rfc1048(cp, len));
+	}
+	return (-1);
+}
+
 static int
 vend_rfc1048(cp, len)
 	u_char *cp;
