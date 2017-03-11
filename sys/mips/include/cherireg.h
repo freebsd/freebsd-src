34d167d33c764f6c0e1cd8a0a7110a09fd3fd54e
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2017-03-11 14:57:11 +0000

    Update copyright header on cherireg.h for 2017 following prior commits.

--- sys/mips/include/cherireg.h
+++ sys/mips/include/cherireg.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2011-2016 Robert N. M. Watson
+ * Copyright (c) 2011-2017 Robert N. M. Watson
  * All rights reserved.
  *
  * This software was developed by SRI International and the University of
