a8de2e6adb849cf30b676f1bc88306c58a4030db
Sepherosa Ziehau <sephe@FreeBSD.org>
2017-03-17 02:48:29 +0000

    MFC: 314382-314485
    314382
        hyperv/hn: Simplify RNDIS packet data offset calculation.
    
        Sponsored by:   Microsoft
        Differential Revision:  https://reviews.freebsd.org/D9699
    
    314483
        hyperv/hn: Simplify RNDIS packet total length calculation.
    
        Sponsored by:   Microsoft
        Differential Revision:  https://reviews.freebsd.org/D9712
    
    314484
        hyperv/hn: Make sure that RNDIS packet message is at least 4B aligned.
    
        Sponsored by:   Microsoft
        Differential Revision:  https://reviews.freebsd.org/D9713
    
    314485
        hyperv/hn: Misaligned chimney sending buffers should not be used
    
        Sponsored by:   Microsoft
        Differential Revision:  https://reviews.freebsd.org/D9714

--- sys/dev/hyperv/netvsc/if_hn.c
+++ sys/dev/hyperv/netvsc/if_hn.c
@@ -1784,12 +1784,6 @@ hn_rndis_pktinfo_append(struct rndis_packet_msg *pkt, size_t pktsize,
 	pi->rm_type = pi_type;
 	pi->rm_pktinfooffset = RNDIS_PKTINFO_OFFSET;
 
-	/* Data immediately follow per-packet-info. */
-	pkt->rm_dataoffset += pi_size;
-
-	/* Update RNDIS packet msg length */
-	pkt->rm_len += pi_size;
-
 	return (pi->rm_data);
 }
 
@@ -1931,8 +1925,8 @@ hn_encap(struct ifnet *ifp, struct hn_tx_ring *txr, struct hn_txdesc *txd,
 	}
 
 	pkt->rm_type = REMOTE_NDIS_PACKET_MSG;
-	pkt->rm_len = sizeof(*pkt) + m_head->m_pkthdr.len;
-	pkt->rm_dataoffset = sizeof(*pkt);
+	pkt->rm_len = m_head->m_pkthdr.len;
+	pkt->rm_dataoffset = 0;
 	pkt->rm_datalen = m_head->m_pkthdr.len;
 	pkt->rm_oobdataoffset = 0;
 	pkt->rm_oobdatalen = 0;
@@ -2002,8 +1996,10 @@ hn_encap(struct ifnet *ifp, struct hn_tx_ring *txr, struct hn_txdesc *txd,
 	}
 
 	pkt_hlen = pkt->rm_pktinfooffset + pkt->rm_pktinfolen;
+	/* Fixup RNDIS packet message total length */
+	pkt->rm_len += pkt_hlen;
 	/* Convert RNDIS packet message offsets */
-	pkt->rm_dataoffset = hn_rndis_pktmsg_offset(pkt->rm_dataoffset);
+	pkt->rm_dataoffset = hn_rndis_pktmsg_offset(pkt_hlen);
 	pkt->rm_pktinfooffset = hn_rndis_pktmsg_offset(pkt->rm_pktinfooffset);
 
 	/*
