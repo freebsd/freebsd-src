72dee17179304641ac67fc56054b671579fc04c9
jhb <jhb@FreeBSD.org>
2017-03-18 18:26:56 +0000

    Decode flock() operation.

--- usr.bin/truss/syscall.h
+++ usr.bin/truss/syscall.h
@@ -45,7 +45,7 @@ enum Argtype { None = 1, Hex, Octal, Int, UInt, LongHex, Name, Ptr, Stat, Ioctl,
 	Pathconf, Rforkflags, ExitStatus, Waitoptions, Idtype, Procctl,
 	LinuxSockArgs, Umtxop, Atfd, Atflags, Timespec2, Accessmode, Long,
 	Sysarch, ExecArgs, ExecEnv, PipeFds, QuadHex, Utrace, IntArray, Pipe2,
-	CapFcntlRights, Fadvice, FileFlags,
+	CapFcntlRights, Fadvice, FileFlags, Flockop,
 
 	CloudABIAdvice, CloudABIClockID, ClouduABIFDSFlags,
 	CloudABIFDStat, CloudABIFileStat, CloudABIFileType,
