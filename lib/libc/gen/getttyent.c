1cffc1793153753ca8e3d819ac0dc94381e783e4
ngie <ngie@FreeBSD.org>
2017-03-22 19:23:13 +0000

    Unbreak world by adding sys/stat.h for stat(2)
    X-MFC with:	r315733
    Reported by:	O. Hartmann <ohartmann@walstatt.org>
    Sponsored by:	Dell EMC Isilon

--- lib/libc/gen/getttyent.c
+++ lib/libc/gen/getttyent.c
@@ -34,6 +34,7 @@ static char sccsid[] = "@(#)getttyent.c	8.1 (Berkeley) 6/4/93";
 __FBSDID("$FreeBSD$");
 
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/sysctl.h>
 
 #include <ctype.h>
