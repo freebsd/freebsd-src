f7dda5b7b47563b74812e94579bfa74506f1c3ec
Andriy Gapon <avg@FreeBSD.org>
2017-03-23 08:10:42 +0000

    MFC r315083: aacraid: fix build with AACRAID_DEBUG=2

--- sys/dev/aacraid/aacraid_cam.c
+++ sys/dev/aacraid/aacraid_cam.c
@@ -244,6 +244,7 @@ aac_cam_probe(device_t dev)
 {
 	struct aac_softc *sc;
 	struct aac_cam *camsc;
+	struct aac_softc *sc;
 
 	camsc = (struct aac_cam *)device_get_softc(dev);
 	if (!camsc->inf)
@@ -1137,6 +1138,7 @@ aac_container_complete(struct aac_command *cm)
 {
 	struct	aac_softc *sc;
 	union	ccb *ccb;
+	struct	aac_softc *sc;
 	u_int32_t status;
 
 	sc = cm->cm_sc;
@@ -1146,7 +1148,7 @@ aac_container_complete(struct aac_command *cm)
 
 	if (cm->cm_flags & AAC_CMD_RESET) {
 		ccb->ccb_h.status = CAM_SCSI_BUS_RESET;
-	} else if (status == ST_OK) {	
+	} else if (status == ST_OK) {
 		ccb->ccb_h.status = CAM_REQ_CMP;
 	} else if (status == ST_NOT_READY) {
 		ccb->ccb_h.status = CAM_BUSY;
