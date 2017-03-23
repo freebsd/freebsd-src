d138596bb734d34234416fd3a04afbf64c6ad065
Andriy Gapon <avg@FreeBSD.org>
2017-03-23 08:00:16 +0000

    MFC r314913: MFV r314911: 7867 ARC space accounting leak

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/arc.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/arc.c
@@ -2612,6 +2612,12 @@ arc_hdr_free_on_write(arc_buf_hdr_t *hdr)
 		    size, hdr);
 	}
 	(void) refcount_remove_many(&state->arcs_size, size, hdr);
+	if (type == ARC_BUFC_METADATA) {
+		arc_space_return(size, ARC_SPACE_META);
+	} else {
+		ASSERT(type == ARC_BUFC_DATA);
+		arc_space_return(size, ARC_SPACE_DATA);
+	}
 
 	l2arc_free_data_on_write(hdr->b_l1hdr.b_pdata, size, type);
 }
