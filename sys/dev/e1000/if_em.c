fdb25f38cb1df032c4f78aec945da27730159e51
Sean Bruno <sbruno@FreeBSD.org>
2017-03-24 14:25:56 +0000

    Add missing 'else' to 3-state conditional during setup of interrupts.
    We don't want to overwrite the 82574 interrupt setup with a different
    configuration.
    
    PR:		218041
    Submitted by:	razmyslov@viva64.com

--- sys/dev/e1000/if_em.c
+++ sys/dev/e1000/if_em.c
@@ -1941,10 +1941,11 @@ em_if_msix_intr_assign(if_ctx_t ctx, int msix)
 			tx_que->eims = 1 << (22 + i);
 			adapter->ims |= tx_que->eims;
 			adapter->ivars |= (8 | tx_que->msix) << (8 + (i * 4));
-		} if (adapter->hw.mac.type == e1000_82575)
+		} else if (adapter->hw.mac.type == e1000_82575) {
 			tx_que->eims = E1000_EICR_TX_QUEUE0 << (i %  adapter->tx_num_queues);
-		else
+		} else {
 			tx_que->eims = 1 << (i %  adapter->tx_num_queues);
+		}
 	}
 
 	/* Link interrupt */
