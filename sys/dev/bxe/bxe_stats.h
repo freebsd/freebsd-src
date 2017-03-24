5a2057c18dbb73d8cf762ccb40341da9adcc52d9
David C Somayajulu <davidcs@FreeBSD.org>
2017-03-24 03:37:58 +0000

    MFC r314365         1. state checks in bxe_tx_mq_start_locked() and bxe_tx_mq_start() to            sync threads during interface down or detach.         2. add sysctl to set pause frame parameters         3. increase max segs for TSO packets to BXE_TSO_MAX_SEGMENTS (32)         4. add debug messages for PHY         5. HW LRO support restricted to FreeBSD versions 8.x and above.
    Submitted by:	Vaishali.Kulkarni@cavium.com

--- sys/dev/bxe/bxe_stats.h
+++ sys/dev/bxe/bxe_stats.h
@@ -266,6 +266,14 @@ struct bxe_eth_stats {
 
     /* num. of times tx queue full occured */
     uint32_t tx_queue_full_return;
+    /* debug stats */
+    uint32_t bxe_tx_mq_sc_state_failures;
+    uint32_t tx_request_link_down_failures;
+    uint32_t bd_avail_too_less_failures;
+    uint32_t tx_mq_not_empty;
+    uint32_t nsegs_path1_errors;
+    uint32_t nsegs_path2_errors;
+
 };
 
 
@@ -372,6 +380,15 @@ struct bxe_eth_q_stats {
 
     /* num. of times tx queue full occured */
     uint32_t tx_queue_full_return;
+
+    /* debug stats */
+    uint32_t bxe_tx_mq_sc_state_failures;
+    uint32_t tx_request_link_down_failures;
+    uint32_t bd_avail_too_less_failures;
+    uint32_t tx_mq_not_empty;
+    uint32_t nsegs_path1_errors;
+    uint32_t nsegs_path2_errors;
+
 };
 
 struct bxe_eth_stats_old {
