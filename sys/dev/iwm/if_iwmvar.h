a7db3a1fcdb9093af7a6eedcdafb69f772b4a50a
adrian <adrian@FreeBSD.org>
2017-03-25 02:49:20 +0000

    [iwm] Enable Energy Based Scan (EBS).
    This can significantly reduce scan duration thus saving time and power.
    EBS failure reported by FW disables EBS for current connection. It is
    re-enabled upon new connection attempt on any WLAN interface.
    
    Obtained from:	dragonflybsd.git 89f579e9823a5c446ca172cf82bbc210d6a054a4

--- sys/dev/iwm/if_iwmvar.h
+++ sys/dev/iwm/if_iwmvar.h
@@ -536,6 +536,8 @@ struct iwm_softc {
 	struct iwm_fw_paging	fw_paging_db[IWM_NUM_OF_FW_PAGING_BLOCKS];
 	uint16_t		num_of_paging_blk;
 	uint16_t		num_of_pages_in_last_blk;
+
+	boolean_t		last_ebs_successful;
 };
 
 #define IWM_LOCK_INIT(_sc) \
