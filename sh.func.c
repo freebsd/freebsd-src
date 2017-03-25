48eb25df687d51c9bdb4677c72e08ed3fde1b186
Dmitry Chagin <dchagin@FreeBSD.org>
2017-03-25 14:09:12 +0000

    Update vendor/tcsh to git b605cb561d
    Vendor changes:
    
    1. PR/471: Daiki Ueno: Delay interpreting arginp until we've processed
    our startup files (which can change the NLS environment).
    
    2. Fix type of read in prompt confirmation (eg. rmstar) (David Kaspar).
    
    3. Fix out of bounds read (Brooks Davis)
    (reproduce by starting tcsh and hitting tab at the prompt).
    
    4. Don't play pointer tricks that are undefined in modern c
    (Brooks Davis).

--- sh.func.c
+++ sh.func.c
@@ -2734,16 +2734,18 @@ nlsclose(void)
 int
 getYN(const char *prompt)
 {
-    int doit, c;
+    int doit;
+    char c;
+
     xprintf("%s", prompt);
     flush();
-    (void) force_read(SHIN, &c, 1);
+    (void) force_read(SHIN, &c, sizeof(c));
     /* 
      * Perhaps we should use the yesexpr from the
      * actual locale
      */
     doit = (strchr(CGETS(22, 14, "Yy"), c) != NULL);
-    while (c != '\n' && force_read(SHIN, &c, 1) == 1)
+    while (c != '\n' && force_read(SHIN, &c, sizeof(c)) == sizeof(c))
 	continue;
     return doit;
 }
