3c6c1b24fea96bf7f174110b335b9688f96fc743
bapt <bapt@FreeBSD.org>
2017-03-26 18:01:53 +0000

    MFC r315053-r315054, r315056
    r315053:
    Add share/man if it exists to the MANPATH
    
    localbase is not consistent with base for manpages:
    /usr/local/man vs /usr/share/man adding share/man allows to fix that
    inconsistency and would permit to remove tons of patches/modifications in the
    ports tree
    
    r315054:
    Extend functionality MANPATH in man(1) to followup with apropos(1) from
    mandoc.
    
    If MANPATH begins with a colon, it is appended to the default list; if it ends
    with a colon, it is prepended to the default list; or if it contains two
    adjacent colons, the standard search path is inserted between the colons.  If
    none of these conditions are met, it overrides the standard search path.
    
    Import the MANPATH description from mandoc into the man(1) man page
    
    Reported by:	kargl
    MFC after:	1 week
    
    r315056:
    Remove the warning when MANPATH is set in the environment
    
    The MANPATH environment variable behaviour is documented properly in the manpage
    and it now has extended to new feature that allows to make MANPATH env variable
    extending the default search path rather than overwriting it making the warning
    painful
    
    Reported by:	kargl
    MFC after:	1 week

--- usr.bin/man/man.sh
+++ usr.bin/man/man.sh
@@ -68,7 +68,23 @@ build_manpath() {
 
 	# If the user has set a manpath, who are we to argue.
 	if [ -n "$MANPATH" ]; then
-		return
+		case "$MANPATH" in
+		*:) PREPEND_MANPATH=${MANPATH} ;;
+		:*) APPEND_MANPATH=${MANPATH} ;;
+		*::*)
+			PREPEND_MANPATH=${MANPATH%%::*}
+			APPEND_MANPATH=${MANPATH#*::}
+			;;
+		*) return ;;
+		esac
+	fi
+
+	if [ -n "$PREPEND_MANPATH" ]; then
+		IFS=:
+		for path in $PREPEND_MANPATH; do
+			add_to_manpath "$path"
+		done
+		unset IFS
 	fi
 
 	search_path
@@ -82,6 +98,13 @@ build_manpath() {
 
 	parse_configs
 
+	if [ -n "$APPEND_MANPATH" ]; then
+		IFS=:
+		for path in $APPEND_MANPATH; do
+			add_to_manpath "$path"
+		done
+		unset IFS
+	fi
 	# Trim leading colon
 	MANPATH=${manpath#:}
 
@@ -238,10 +261,6 @@ manpath_usage() {
 # Usage: manpath_warnings
 # Display some warnings to stderr.
 manpath_warnings() {
-	if [ -z "$Lflag" -a -n "$MANPATH" ]; then
-		echo "(Warning: MANPATH environment variable set)" >&2
-	fi
-
 	if [ -n "$Lflag" -a -n "$MANLOCALES" ]; then
 		echo "(Warning: MANLOCALES environment variable set)" >&2
 	fi
@@ -770,6 +789,8 @@ search_path() {
 			else
 				case "$path" in
 				*/bin)	p="${path%/bin}/man"
+					add_to_manpath "$p"
+					p="${path%/bin}/share/man"
 					add_to_manpath "$p"
 					;;
 				*)	;;
