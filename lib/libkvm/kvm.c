225a567986e17c05623d43941cdf7c963dec4f6a
Enji Cooper <ngie@FreeBSD.org>
2017-03-27 18:22:33 +0000

    MFC r315595,r315601,r315603,r315647:
    r315595:
    
    Remove a commented out line before kvm_getprocs(3)
    
    The commented out return value for kvm_getprocs(3) was misleading -- the
    uncommented line is correct.
    
    No content change
    
    r315601:
    
    kvm_open2(3): remove '*' when describing addr argument for `resolver`
    
    As noted by vangyzen, with a similar issue in D10022, the pointer portion of
    the .Fa macro call is unnecessary, so remove the '*'.
    
    r315603:
    
    kvm_close(3): return `error` instead of blindly returning `0`
    
    `error` is the accumulated error from previous close(2) calls.
    
    This bug has been present since the libcall's import from 4.4BSD
    Lite (r1573).
    
    Noticed by:	vangyzen (D10022)
    Relnotes:	yes
    
    r315647:
    
    Handle kd == NULL gracefully with kvm_close(3)
    
    Don't segfault in kvm_close(3) if provided a NULL pointer. Instead, return
    -1 and set errno to EINVAL.
    
    Document this new behavior explicitly.

--- lib/libkvm/kvm.c
+++ lib/libkvm/kvm.c
@@ -534,6 +534,10 @@ kvm_close(kvm_t *kd)
 {
 	int error = 0;
 
+	if (kd == NULL) {
+		errno = EINVAL;
+		return (-1);
+	}
 	if (kd->vmst != NULL)
 		kd->arch->ka_freevtop(kd);
 	if (kd->pmfd >= 0)
@@ -552,7 +556,7 @@ kvm_close(kvm_t *kd)
 		free((void *)kd->argv);
 	free((void *)kd);
 
-	return (0);
+	return (error);
 }
 
 /*
