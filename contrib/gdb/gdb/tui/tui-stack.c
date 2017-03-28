3532f979260f778fded06463f94a99a405c3139c
Brooks Davis <brooks@one-eyed-alien.net>
2017-03-28 19:38:26 +0000

    Fix an incorrect cast that is harmless in practice on most architectures.

--- contrib/gdb/gdb/tui/tui-stack.c
+++ contrib/gdb/gdb/tui/tui-stack.c
@@ -356,7 +356,7 @@ tui_show_frame_info (struct frame_info *fi)
 	  else
 	    {
 	      if (find_pc_partial_function (get_frame_pc (fi), (char **) NULL,
-					    &low, (CORE_ADDR) NULL) == 0)
+					    &low, (CORE_ADDR*) NULL) == 0)
 		error ("No function contains program counter for selected frame.\n");
 	      else
 		low = tui_get_low_disassembly_address (low, get_frame_pc (fi));
