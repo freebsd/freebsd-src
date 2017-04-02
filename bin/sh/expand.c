a536ad439028ac3a499d79c531068306700595d1
jilles <jilles@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-04-02 14:02:10 +0000

    sh: Remove global state from collate_range_cmp().
    The global state is not used across invocations of collate_range_cmp().
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@316418 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- bin/sh/expand.c
+++ bin/sh/expand.c
@@ -145,10 +145,12 @@ appendarglist(struct arglist *list, char *str)
 static int
 collate_range_cmp(wchar_t c1, wchar_t c2)
 {
-	static wchar_t s1[2], s2[2];
+	wchar_t s1[2], s2[2];
 
 	s1[0] = c1;
+	s1[1] = L'\0';
 	s2[0] = c2;
+	s2[1] = L'\0';
 	return (wcscoll(s1, s2));
 }
 
