6af5cfb886fb3f8cfe135e1d964af7d8ec748356
emaste <emaste@FreeBSD.org>
2017-04-03 20:46:55 +0000

    MFC r306041: Always pass -m to ld for converting binary files to ELF
    This is in preparation for linking with LLVM's lld, which does not have
    a compiled-in default output emulation. lld requires that it is
    specified via the -m option, or obtained from the object file(s) being
    linked.
    
    This will also allow all build targets to share a common linker binary.
    
    Sponsored by:	The FreeBSD Foundation

--- sys/conf/kmod.mk
+++ sys/conf/kmod.mk
@@ -171,11 +171,13 @@ ${_firmw:C/\:.*$/.fwo/:T}:	${_firmw:C/\:.*$//}
 	@${ECHO} ${_firmw:C/\:.*$//} ${.ALLSRC:M*${_firmw:C/\:.*$//}}
 	@if [ -e ${_firmw:C/\:.*$//} ]; then			\
 		${LD} -b binary --no-warn-mismatch ${_LDFLAGS}	\
-		    -r -d -o ${.TARGET}	${_firmw:C/\:.*$//};	\
+		    -m ${LD_EMULATION} -r -d			\
+		    -o ${.TARGET} ${_firmw:C/\:.*$//};		\
 	else							\
 		ln -s ${.ALLSRC:M*${_firmw:C/\:.*$//}} ${_firmw:C/\:.*$//}; \
 		${LD} -b binary --no-warn-mismatch ${_LDFLAGS}	\
-		    -r -d -o ${.TARGET}	${_firmw:C/\:.*$//};	\
+		    -m ${LD_EMULATION} -r -d			\
+		    -o ${.TARGET} ${_firmw:C/\:.*$//};		\
 		rm ${_firmw:C/\:.*$//};				\
 	fi
 
