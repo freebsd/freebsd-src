dc653882d4719cd50c1866da5aff2a862ee36fb9
John Baldwin <jhb@FreeBSD.org>
2017-04-05 03:05:04 +0000

    Always pass the linker emulation via -m when linking modules and kernels.
    Previously the linker emulation was only passed when building binary
    objects for firmware modules.  This change always passes the desired
    output format for kernel modules and kernels rather than requiring the
    toolchain's default output format to match the desired output format.
    This in turn permits use of external toolchains whose default output
    format does not match the desired output format.
    
    Reviewed by:	imp, emaste
    Sponsored by:	DARPA / AFRL
    Differential Revision:	https://reviews.freebsd.org/D10085

--- sys/conf/kern.pre.mk
+++ sys/conf/kern.pre.mk
@@ -184,8 +184,9 @@ MD_ROOT_SIZE_CONFIGURED!=	grep MD_ROOT_SIZE opt_md.h || true ; echo
 SYSTEM_OBJS+= embedfs_${MFS_IMAGE:T:R}.o
 .endif
 .endif
-SYSTEM_LD= @${LD} -Bdynamic -T ${LDSCRIPT} ${_LDFLAGS} --no-warn-mismatch \
-	--warn-common --export-dynamic --dynamic-linker /red/herring \
+SYSTEM_LD= @${LD} -m ${LD_EMULATION} -Bdynamic -T ${LDSCRIPT} ${_LDFLAGS} \
+	--no-warn-mismatch --warn-common --export-dynamic \
+	--dynamic-linker /red/herring \
 	-o ${.TARGET} -X ${SYSTEM_OBJS} vers.o
 SYSTEM_LD_TAIL= @${OBJCOPY} --strip-symbol gcc2_compiled. ${.TARGET} ; \
 	${SIZE} ${.TARGET} ; chmod 755 ${.TARGET}
