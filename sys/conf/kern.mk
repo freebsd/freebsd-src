f14e4278586c5bdb3d648eda4c0fabbcdbc6a69d
jhb <jhb@FreeBSD.org>
2017-04-05 02:52:49 +0000

    Use correct linker emulation name for armeb.
    MFC after:	1 week
    Sponsored by:	DARPA / AFRL
    Differential Revision:	https://reviews.freebsd.org/D10085

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -259,7 +259,7 @@ CFLAGS+=        -std=${CSTD}
 LD_EMULATION_aarch64=aarch64elf
 LD_EMULATION_amd64=elf_x86_64_fbsd
 LD_EMULATION_arm=armelf_fbsd
-LD_EMULATION_armeb=armelf_fbsd
+LD_EMULATION_armeb=armelfb_fbsd
 LD_EMULATION_armv6=armelf_fbsd
 LD_EMULATION_i386=elf_i386_fbsd
 LD_EMULATION_mips= elf32btsmip_fbsd
