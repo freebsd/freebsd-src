3ce31838b9b2094d75b0e2ade05561bcea57afa7
Brooks Davis <brooks@one-eyed-alien.net>
2017-04-08 04:06:07 +0000

    Tidy up kqueue user pointer storage.

--- sys/compat/cheriabi/cheriabi_misc.c
+++ sys/compat/cheriabi/cheriabi_misc.c
@@ -155,6 +155,12 @@ static int cheriabi_kevent_copyout(void *arg, struct kevent *kevp, int count);
 static int cheriabi_kevent_copyin(void *arg, struct kevent *kevp, int count);
 static register_t cheriabi_mmap_prot2perms(int prot);
 
+struct kevent_userptrs {
+	struct chericap	ident;
+	struct chericap	data;
+	struct chericap	udata;
+};
+
 int
 cheriabi_syscall(struct thread *td, struct cheriabi_syscall_args *uap)
 {
@@ -431,6 +437,7 @@ cheriabi_kevent_copyout(void *arg, struct kevent *kevp, int count)
 {
 	struct cheriabi_kevent_args *uap;
 	struct kevent_c	ks_c[KQ_NEVENTS];
+	struct kevent_userptrs *uptrs;
 	int i, error = 0;
 
 	KASSERT(count <= KQ_NEVENTS, ("count (%d) > KQ_NEVENTS", count));
@@ -445,11 +452,10 @@ cheriabi_kevent_copyout(void *arg, struct kevent *kevp, int count)
 		 * Retrieve the ident, data, and udata capabilities stashed by
 		 * cheriabi_kevent_copyin().
 		 */
-		cheri_capability_copy(&ks_c[i].ident, kevp[i].udata);
-		cheri_capability_copy(&ks_c[i].data,
-		    (struct chericap *)kevp[i].udata + 1);
-		cheri_capability_copy(&ks_c[i].udata,
-		    (struct chericap *)kevp[i].udata + 2);
+		uptrs = kevp[i].udata;
+		cheri_capability_copy(&ks_c[i].ident, &uptrs->ident);
+		cheri_capability_copy(&ks_c[i].data, &uptrs->data);
+		cheri_capability_copy(&ks_c[i].udata, &uptrs->udata);
 	}
 	error = copyoutcap(ks_c, uap->eventlist, count * sizeof(*ks_c));
 	if (error == 0)
@@ -465,6 +471,7 @@ cheriabi_kevent_copyin(void *arg, struct kevent *kevp, int count)
 {
 	struct cheriabi_kevent_args *uap;
 	struct kevent_c	ks_c[KQ_NEVENTS];
+	struct kevent_userptrs *uptrs;
 	int error, i, tag;
 	register_t perms;
 
@@ -506,16 +513,14 @@ cheriabi_kevent_copyin(void *arg, struct kevent *kevp, int count)
 				return (EPROT);
 		}
 		/*
-		 * We stash the real data, ident, udata capabilities in
-		 * a malloced array in udata.
+		 * We stash the real ident, data, and udata capabilities in
+		 * a malloced struct in udata.
 		 */
-		kevp[i].udata = malloc(3*sizeof(struct chericap), M_KQUEUE,
-		    M_WAITOK);
-		cheri_capability_copy(kevp[i].udata, &ks_c[i].ident);
-		cheri_capability_copy((struct chericap *)kevp[i].udata + 1,
-		    &ks_c[i].data);
-		cheri_capability_copy((struct chericap *)kevp[i].udata + 2,
-		    &ks_c[i].udata);
+		uptrs = malloc(sizeof(*uptrs), M_KQUEUE, M_WAITOK);
+		kevp[i].udata = uptrs;
+		cheri_capability_copy(&uptrs->ident, &ks_c[i].ident);
+		cheri_capability_copy(&uptrs->data, &ks_c[i].data);
+		cheri_capability_copy(&uptrs->udata, &ks_c[i].udata);
 	}
 done:
 	return (error);
