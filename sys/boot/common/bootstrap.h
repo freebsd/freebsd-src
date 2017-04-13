0692026a1c2515e54cee9074dbbf9c8787e7a357
royger <royger@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-04-13 09:59:12 +0000

    loader/multiboot: fix multiboot loading
    The current multiboot loader code doesn't clean the metadata added to the
    kernel after the bi_load64 dry run, which breaks accounting of the required
    memory for the metadata.
    
    This issue didn't show itself before because all the metadata items where small
    (8bytes), but after r316343 there's a big blob in the metadata, which triggers
    this. Fix it by cleaning the metadata added to the kernel after the bi_load64
    dry run. Also add a comment describing the memory layout when booting using
    multiboot (Xen Dom0).
    
    This unbreaks booting a FreeBSD/Xen Dom0 after r316343.
    
    MFC after:	3 weeks
    Sponsored by:	Citrix Systems R&D
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@316754 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/boot/common/bootstrap.h
+++ sys/boot/common/bootstrap.h
@@ -228,6 +228,7 @@ void file_discard(struct preloaded_file *fp);
 void file_addmetadata(struct preloaded_file *fp, int type, size_t size, void *p);
 int  file_addmodule(struct preloaded_file *fp, char *modname, int version,
 	struct kernel_module **newmp);
+void file_removemetadata(struct preloaded_file *fp);
 
 /* MI module loaders */
 #ifdef __elfN
