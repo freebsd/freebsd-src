9605064550de1801abd64aa56dc4a763a9151a46
Andriy Gapon <avg@FreeBSD.org>
2017-04-14 14:44:00 +0000

    MFC r315851: move thread switch tracing from mi_switch to sched_switch

--- sys/kern/sched_4bsd.c
+++ sys/kern/sched_4bsd.c
@@ -1013,6 +1013,16 @@ sched_switch(struct thread *td, struct thread *newtd, int flags)
 		MPASS(newtd->td_lock == &sched_lock);
 	}
 
+#if (KTR_COMPILE & KTR_SCHED) != 0
+	if (TD_IS_IDLETHREAD(td))
+		KTR_STATE1(KTR_SCHED, "thread", sched_tdname(td), "idle",
+		    "prio:%d", td->td_priority);
+	else
+		KTR_STATE3(KTR_SCHED, "thread", sched_tdname(td), KTDSTATE(td),
+		    "prio:%d", td->td_priority, "wmesg:\"%s\"", td->td_wmesg,
+		    "lockname:\"%s\"", td->td_lockname);
+#endif
+
 	if (td != newtd) {
 #ifdef	HWPMC_HOOKS
 		if (PMC_PROC_IS_USING_PMCS(td->td_proc))
@@ -1060,6 +1070,9 @@ sched_switch(struct thread *td, struct thread *newtd, int flags)
 #endif
 	} else
 		SDT_PROBE0(sched, , , remain__cpu);
+
+	KTR_STATE1(KTR_SCHED, "thread", sched_tdname(td), "running",
+	    "prio:%d", td->td_priority);
 
 #ifdef SMP
 	if (td->td_flags & TDF_IDLETD)
