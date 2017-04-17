59a524a9111affef7c7c2ce1141781301f3ebca8
asomers <asomers@FreeBSD.org>
2017-04-17 20:13:20 +0000

    MFC r313025, r313395, r314113, r314442, r315458, r315656
    r313025:
    Add tests for multi-fib IPv6 routing
    
    PR:		196361
    Submitted by:	jhujhiti@adjectivism.org
    Reported by:	Jason Healy <jhealy@logn.net>
    MFC after:	4 weeks
    Sponsored by:	Spectra Logic Corp
    
    r313395:
    Add fibs_test:udp_dontroute6, another IPv6 multi-FIB test
    
    PR:		196361
    MFC after:	3 weeks
    Sponsored by:	Spectra Logic Corp
    
    r314113:
    Remove tests/sys/netinet/fibs_tests's dependency on net/socat
    
    Instead of bridging two tap interfaces with socat, just use an epair pair.
    
    MFC after:	3 weeks
    Sponsored by:	Spectra Logic Corp
    
    r314442:
    Add an ATF test for IPv6 SLAAC with multiple fibs
    
    Tests that an interface can get a SLAAC address and that it inserts its
    routes into the correct fib. Does not test anything to do with NDP.
    
    PR:		196361
    Reviewed by:	Erick Turnquist <jhujhiti@adjectivism.org>
    MFC after:	3 weeks
    Sponsored by:	Spectra Logic Corp
    Differential Revision:	https://reviews.freebsd.org/D9776
    
    r315458:
    Constrain IPv6 routes to single FIBs when net.add_addr_allfibs=0
    
    sys/netinet6/icmp6.c
    	Use the interface's FIB for source address selection in ICMPv6 error
    	responses.
    
    sys/netinet6/in6.c
    	In in6_newaddrmsg, announce arrival of local addresses on the
    	interface's FIB only.  In in6_lltable_rtcheck, use a per-fib ND6
    	cache instead of a single cache.
    
    sys/netinet6/in6_src.c
    	In in6_selectsrc, use the caller's fib instead of the default fib.
    	In in6_selectsrc_socket, remove a superfluous check.
    
    sys/netinet6/nd6.c
    	In nd6_lle_event, use the interface's fib for routing socket
    	messages.  In nd6_is_new_addr_neighbor, check all FIBs when trying
    	to determine whether an address is a neighbor.  Also, simplify the
    	code for point to point interfaces.
    
    sys/netinet6/nd6.h
    sys/netinet6/nd6.c
    sys/netinet6/nd6_rtr.c
    	Make defrouter_select fib-aware, and make all of its callers pass in
    	the interface fib.
    
    sys/netinet6/nd6_nbr.c
    	When inputting a Neighbor Solicitation packet, consider the
    	interface fib instead of the default fib for DAD.  Output NS and
    	Neighbor Advertisement packets on the correct fib.
    
    sys/netinet6/nd6_rtr.c
    	Allow installing the same host route on different interfaces in
    	different FIBs.  If rt_add_addr_allfibs=0, only install or delete
    	the prefix route on the interface fib.
    
    tests/sys/netinet/fibs_test.sh
    	Clear some expected failures, but add a skip for the newly revealed
    	BUG217871.
    
    PR:		196361
    Submitted by:	Erick Turnquist <jhujhiti@adjectivism.org>
    Reported by:	Jason Healy <jhealy@logn.net>
    Reviewed by:	asomers
    MFC after:	3 weeks
    Sponsored by:	Spectra Logic Corp
    Differential Revision:	https://reviews.freebsd.org/D9451
    
    r315656:
    Fix back-to-back runs of sys/netinet/fibs_test;slaac_on_nondefault_fib6
    
    This test was failing if run twice because rtadvd takes too long to die.
    The rtadvd process from the first run was still running when the
    second run created its interfaces.  The solution is to use SIGKILL during
    the cleanup instead of SIGTERM so rtadvd will die faster.
    
    While I'm here, randomize the addresses used for the test, which makes bugs
    like this easier to spot, and fix the cleanup order to be the opposite of
    the setup order
    
    PR:		217871
    MFC after:	18 days
    X-MFC-With:	315458
    Sponsored by:	Spectra Logic Corp

--- sys/netinet6/nd6_nbr.c
+++ sys/netinet6/nd6_nbr.c
@@ -262,8 +262,7 @@ nd6_ns_input(struct mbuf *m, int off, int icmp6len)
 		bzero(&info, sizeof(info));
 		info.rti_info[RTAX_GATEWAY] = (struct sockaddr *)&rt_gateway;
 
-		/* Always use the default FIB. */
-		if (rib_lookup_info(RT_DEFAULT_FIB, (struct sockaddr *)&dst6,
+		if (rib_lookup_info(ifp->if_fib, (struct sockaddr *)&dst6,
 		    0, 0, &info) == 0) {
 			if ((info.rti_flags & RTF_ANNOUNCE) != 0 &&
 			    rt_gateway.sdl_family == AF_LINK) {
@@ -485,7 +484,7 @@ nd6_ns_output_fib(struct ifnet *ifp, const struct in6_addr *saddr6,
 			uint32_t scopeid;
 
 			in6_splitscope(&ip6->ip6_dst, &dst6, &scopeid);
-			error = in6_selectsrc_addr(RT_DEFAULT_FIB, &dst6,
+			error = in6_selectsrc_addr(fibnum, &dst6,
 			    scopeid, ifp, &src6, NULL);
 			if (error) {
 				char ip6buf[INET6_ADDRSTRLEN];
@@ -982,7 +981,7 @@ nd6_na_output_fib(struct ifnet *ifp, const struct in6_addr *daddr6_0,
 	 * Select a source whose scope is the same as that of the dest.
 	 */
 	in6_splitscope(&daddr6, &dst6, &scopeid);
-	error = in6_selectsrc_addr(RT_DEFAULT_FIB, &dst6,
+	error = in6_selectsrc_addr(fibnum, &dst6,
 	    scopeid, ifp, &src6, NULL);
 	if (error) {
 		char ip6buf[INET6_ADDRSTRLEN];
