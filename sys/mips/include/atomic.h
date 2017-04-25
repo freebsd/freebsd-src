23aedc945bcb7c12da89daf826161d180a46518c
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-04-25 15:49:05 +0000

    Don't define atomic_*_ptr for CheriABI
    These functions currently only work on longs so we shouldn't
    be using them.

--- sys/mips/include/atomic.h
+++ sys/mips/include/atomic.h
@@ -1009,6 +1009,7 @@ atomic_thread_fence_seq_cst(void)
 
 #endif /* __mips_n64 */
 
+#ifndef __CHERI_PURE_CAPABILITY__
 /* Operations on pointers. */
 #define	atomic_set_ptr		atomic_set_long
 #define	atomic_set_acq_ptr	atomic_set_acq_long
@@ -1031,5 +1032,6 @@ atomic_thread_fence_seq_cst(void)
 #define	atomic_load_acq_ptr	atomic_load_acq_long
 #define	atomic_store_rel_ptr	atomic_store_rel_long
 #define	atomic_readandclear_ptr	atomic_readandclear_long
+#endif
 
 #endif /* ! _MACHINE_ATOMIC_H_ */
