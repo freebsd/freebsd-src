abe5d00b9461b98ff4479b738b08db138ac7f700
Brooks Davis <brooks@one-eyed-alien.net>
2017-04-25 21:50:58 +0000

    Merged April 25th.

--- diff-to-upstream.sh
+++ diff-to-upstream.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-UPSTREAM_TAG=freebsd-head-20170418
+UPSTREAM_TAG=freebsd-head-20170425
 
 git diff --name-only ${UPSTREAM_TAG} | \
     grep -v -E '(cheri|bin/(auxargs|helloworld|shmem_bench)|contrib/(curl|gdb|jpeg|libpng|netsurf)|^ctsrd|diff-to-upstream.sh|gnu/usr.bin/gdb|lib/(lib(curl|helloworld|jpeg|png|statcounters)|netsurf)|libexec/.*-helper|usr.bin/libpng_sb_test|share/netsurf|usr.bin/(capsize|nsfb|qtrace))' | \
