230d034a0f0d4b9b13ea136b27b016cfeb525f6b
kib <kib@FreeBSD.org>
2017-04-30 10:47:59 +0000

    Style. - Use ANSI C function definitions. - Remove redundand cast. - Minor style compliance tweaks.
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week

--- lib/libc/gen/sem_new.c
+++ lib/libc/gen/sem_new.c
@@ -77,24 +77,26 @@ struct sem_nameinfo {
 
 static pthread_once_t once = PTHREAD_ONCE_INIT;
 static pthread_mutex_t sem_llock;
-static LIST_HEAD(,sem_nameinfo) sem_list = LIST_HEAD_INITIALIZER(sem_list);
+static LIST_HEAD(, sem_nameinfo) sem_list = LIST_HEAD_INITIALIZER(sem_list);
 
 static void
-sem_prefork()
+sem_prefork(void)
 {
 	
 	_pthread_mutex_lock(&sem_llock);
 }
 
 static void
-sem_postfork()
+sem_postfork(void)
 {
+
 	_pthread_mutex_unlock(&sem_llock);
 }
 
 static void
-sem_child_postfork()
+sem_child_postfork(void)
 {
+
 	_pthread_mutex_unlock(&sem_llock);
 }
 
@@ -116,10 +118,8 @@ sem_check_validity(sem_t *sem)
 
 	if (sem->_magic == SEM_MAGIC)
 		return (0);
-	else {
-		errno = EINVAL;
-		return (-1);
-	}
+	errno = EINVAL;
+	return (-1);
 }
 
 int
@@ -142,13 +142,16 @@ sem_t *
 _sem_open(const char *name, int flags, ...)
 {
 	char path[PATH_MAX];
-
 	struct stat sb;
 	va_list ap;
-	struct sem_nameinfo *ni = NULL;
-	sem_t *sem = NULL;
-	int fd = -1, mode, len, errsave;
-	int value = 0;
+	struct sem_nameinfo *ni;
+	sem_t *sem, tmp;
+	int errsave, fd, len, mode, value;
+
+	ni = NULL;
+	sem = NULL;
+	fd = -1;
+	value = 0;
 
 	if (name[0] != '/') {
 		errno = EINVAL;
@@ -213,8 +216,6 @@ _sem_open(const char *name, int flags, ...)
 			goto error;
 	}
 	if (sb.st_size < sizeof(sem_t)) {
-		sem_t tmp;
-
 		tmp._magic = SEM_MAGIC;
 		tmp._kern._count = value;
 		tmp._kern._flags = USYNC_PROCESS_SHARED | SEM_NAMED;
@@ -222,8 +223,8 @@ _sem_open(const char *name, int flags, ...)
 			goto error;
 	}
 	flock(fd, LOCK_UN);
-	sem = (sem_t *)mmap(NULL, sizeof(sem_t), PROT_READ|PROT_WRITE,
-		MAP_SHARED|MAP_NOSYNC, fd, 0);
+	sem = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE,
+	    MAP_SHARED | MAP_NOSYNC, fd, 0);
 	if (sem == MAP_FAILED) {
 		sem = NULL;
 		if (errno == ENOMEM)
@@ -277,12 +278,11 @@ _sem_close(sem_t *sem)
 				_pthread_mutex_unlock(&sem_llock);
 				return (0);
 			}
-			else
-				break;
+			break;
 		}
 	}
 
-	if (ni) {
+	if (ni != NULL) {
 		LIST_REMOVE(ni, next);
 		_pthread_mutex_unlock(&sem_llock);
 		munmap(sem, sizeof(*sem));
@@ -342,7 +342,8 @@ _sem_getvalue(sem_t * __restrict sem, int * __restrict sval)
 static __inline int
 usem_wake(struct _usem2 *sem)
 {
-	return _umtx_op(sem, UMTX_OP_SEM2_WAKE, 0, NULL, NULL);
+
+	return (_umtx_op(sem, UMTX_OP_SEM2_WAKE, 0, NULL, NULL));
 }
 
 static __inline int
@@ -436,6 +437,7 @@ int
 _sem_timedwait(sem_t * __restrict sem,
 	const struct timespec * __restrict abstime)
 {
+
 	return (_sem_clockwait_np(sem, CLOCK_REALTIME, TIMER_ABSTIME, abstime,
 	    NULL));
 };
@@ -443,7 +445,8 @@ _sem_timedwait(sem_t * __restrict sem,
 int
 _sem_wait(sem_t *sem)
 {
-	return _sem_timedwait(sem, NULL);
+
+	return (_sem_timedwait(sem, NULL));
 }
 
 /*
