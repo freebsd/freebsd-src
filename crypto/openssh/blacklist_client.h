64d41a9ba5585261a0be3cd6c70516a7bb5c3d54
lidl <lidl@FreeBSD.org>
2017-05-12 15:20:12 +0000

    Refine and update blacklist support in sshd
    Adjust notification points slightly to catch all auth failures,
    rather than just the ones caused by bad usernames.
    
    Modify notification point for bad usernames to send new type of
    BLACKLIST_BAD_USER. (Support in libblacklist will be forthcoming soon.)
    Add guards to allow library headers to expose the enum of action values.
    
    Reviewed by:	des
    Approved by:	des
    Sponsored by:	The FreeBSD Foundation

--- crypto/openssh/blacklist_client.h
+++ crypto/openssh/blacklist_client.h
@@ -34,22 +34,26 @@
 #ifndef BLACKLIST_CLIENT_H
 #define BLACKLIST_CLIENT_H
 
+#ifndef BLACKLIST_API_ENUM
 enum {
 	BLACKLIST_AUTH_OK = 0,
-	BLACKLIST_AUTH_FAIL
+	BLACKLIST_AUTH_FAIL,
+	BLACKLIST_ABUSIVE_BEHAVIOR,
+	BLACKLIST_BAD_USER
 };
+#endif
 
 #ifdef USE_BLACKLIST
 void blacklist_init(void);
-void blacklist_notify(int);
+void blacklist_notify(int, const char *);
 
 #define BLACKLIST_INIT() blacklist_init()
-#define BLACKLIST_NOTIFY(x) blacklist_notify(x)
+#define BLACKLIST_NOTIFY(x,msg) blacklist_notify(x,msg)
 
 #else
 
 #define BLACKLIST_INIT()
-#define BLACKLIST_NOTIFY(x)
+#define BLACKLIST_NOTIFY(x,msg)
 
 #endif
 
