afbba9959f9b3cb136a7cbb8e85938e92831f9bc
Brooks Davis <brooks@one-eyed-alien.net>
2017-05-15 18:59:48 +0000

    Make shared page support optional.
    Enable under QEMU.
    
    Per comments on https://reviews.freebsd.org/D10695, this isn't safe on
    many MIPS implementations so needs to be optional.  If it's unsafe on
    BERI then we will likely want to seperatly move the signal trampoline
    to an address not adjacent to the stack.

--- sys/mips/mips/elf_machdep.c
+++ sys/mips/mips/elf_machdep.c
@@ -75,12 +75,19 @@ struct sysentvec elf64_freebsd_sysvec = {
 	.sv_setregs	= exec_setregs,
 	.sv_fixlimit	= NULL,
 	.sv_maxssiz	= NULL,
-	.sv_flags	= SV_ABI_FREEBSD | SV_LP64 | SV_SHP,
+	.sv_flags	= SV_ABI_FREEBSD | SV_LP64 |
+#ifdef MIPS_SHAREDPAGE
+			    SV_SHP,
+#else
+			    0,
+#endif
 	.sv_set_syscall_retval = cpu_set_syscall_retval,
 	.sv_fetch_syscall_args = cpu_fetch_syscall_args,
 	.sv_syscallnames = syscallnames,
+#ifdef MIPS_SHAREDPAGE
 	.sv_shared_page_base = SHAREDPAGE,
 	.sv_shared_page_len = PAGE_SIZE,
+#endif
 	.sv_schedtail	= NULL,
 	.sv_thread_detach = NULL,
 	.sv_trap	= NULL,
@@ -147,12 +154,19 @@ struct sysentvec elf32_freebsd_sysvec = {
 	.sv_setregs	= exec_setregs,
 	.sv_fixlimit	= NULL,
 	.sv_maxssiz	= NULL,
-	.sv_flags	= SV_ABI_FREEBSD | SV_ILP32 | SV_SHP,
+	.sv_flags	= SV_ABI_FREEBSD | SV_ILP32 |
+#ifdef MIPS_SHAREDPAGE
+			    SV_SHP,
+#else
+			    0,
+#endif
 	.sv_set_syscall_retval = cpu_set_syscall_retval,
 	.sv_fetch_syscall_args = cpu_fetch_syscall_args,
 	.sv_syscallnames = syscallnames,
+#ifdef MIPS_SHAREDPAGE
 	.sv_shared_page_base = SHAREDPAGE,
 	.sv_shared_page_len = PAGE_SIZE,
+#endif
 	.sv_schedtail	= NULL,
 	.sv_thread_detach = NULL,
 	.sv_trap	= NULL,
