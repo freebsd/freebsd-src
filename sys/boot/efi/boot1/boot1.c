b74bf67e2891e060aab31587bec7cc02d444d75e
ngie <ngie@FreeBSD.org>
2017-05-22 06:29:20 +0000

    MFC r316103:
    Remove redundant declarations
    
    They're already defined in libstand.h

--- sys/boot/efi/boot1/boot1.c
+++ sys/boot/efi/boot1/boot1.c
@@ -47,7 +47,6 @@ static const boot_module_t *boot_modules[] =
 /* The initial number of handles used to query EFI for partitions. */
 #define NUM_HANDLES_INIT	24
 
-void putchar(int c);
 EFI_STATUS efi_main(EFI_HANDLE Ximage, EFI_SYSTEM_TABLE* Xsystab);
 
 EFI_SYSTEM_TABLE *systab;
