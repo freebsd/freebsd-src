69921123490b99c2588b0c743bc4af32bbe6601c
Konstantin Belousov <kib@FreeBSD.org>
2017-05-23 09:29:05 +0000

    Commit the 64-bit inode project.
    Extend the ino_t, dev_t, nlink_t types to 64-bit ints.  Modify
    struct dirent layout to add d_off, increase the size of d_fileno
    to 64-bits, increase the size of d_namlen to 16-bits, and change
    the required alignment.  Increase struct statfs f_mntfromname[] and
    f_mntonname[] array length MNAMELEN to 1024.
    
    ABI breakage is mitigated by providing compatibility using versioned
    symbols, ingenious use of the existing padding in structures, and
    by employing other tricks.  Unfortunately, not everything can be
    fixed, especially outside the base system.  For instance, third-party
    APIs which pass struct stat around are broken in backward and
    forward incompatible ways.
    
    Kinfo sysctl MIBs ABI is changed in backward-compatible way, but
    there is no general mechanism to handle other sysctl MIBS which
    return structures where the layout has changed. It was considered
    that the breakage is either in the management interfaces, where we
    usually allow ABI slip, or is not important.
    
    Struct xvnode changed layout, no compat shims are provided.
    
    For struct xtty, dev_t tty device member was reduced to uint32_t.
    It was decided that keeping ABI compat in this case is more useful
    than reporting 64-bit dev_t, for the sake of pstat.
    
    Update note: strictly follow the instructions in UPDATING.  Build
    and install the new kernel with COMPAT_FREEBSD11 option enabled,
    then reboot, and only then install new world.
    
    Credits: The 64-bit inode project, also known as ino64, started life
    many years ago as a project by Gleb Kurtsou (gleb).  Kirk McKusick
    (mckusick) then picked up and updated the patch, and acted as a
    flag-waver.  Feedback, suggestions, and discussions were carried
    by Ed Maste (emaste), John Baldwin (jhb), Jilles Tjoelker (jilles),
    and Rick Macklem (rmacklem).  Kris Moore (kris) performed an initial
    ports investigation followed by an exp-run by Antoine Brodin (antoine).
    Essential and all-embracing testing was done by Peter Holm (pho).
    The heavy lifting of coordinating all these efforts and bringing the
    project to completion were done by Konstantin Belousov (kib).
    
    Sponsored by:	The FreeBSD Foundation (emaste, kib)
    Differential revision:	https://reviews.freebsd.org/D10439

--- sys/sys/mount.h
+++ sys/sys/mount.h
@@ -65,8 +65,8 @@ struct fid {
  * filesystem statistics
  */
 #define	MFSNAMELEN	16		/* length of type name including null */
-#define	MNAMELEN	88		/* size of on/from name bufs */
-#define	STATFS_VERSION	0x20030518	/* current version number */
+#define	MNAMELEN	1024		/* size of on/from name bufs */
+#define	STATFS_VERSION	0x20140518	/* current version number */
 struct statfs {
 	uint32_t f_version;		/* structure version number */
 	uint32_t f_type;		/* type of filesystem */
@@ -91,6 +91,34 @@ struct statfs {
 	char	  f_mntfromname[MNAMELEN];  /* mounted filesystem */
 	char	  f_mntonname[MNAMELEN];    /* directory on which mounted */
 };
+
+#if defined(_WANT_FREEBSD11_STATFS) || defined(_KERNEL)
+#define	FREEBSD11_STATFS_VERSION	0x20030518 /* current version number */
+struct freebsd11_statfs {
+	uint32_t f_version;		/* structure version number */
+	uint32_t f_type;		/* type of filesystem */
+	uint64_t f_flags;		/* copy of mount exported flags */
+	uint64_t f_bsize;		/* filesystem fragment size */
+	uint64_t f_iosize;		/* optimal transfer block size */
+	uint64_t f_blocks;		/* total data blocks in filesystem */
+	uint64_t f_bfree;		/* free blocks in filesystem */
+	int64_t	 f_bavail;		/* free blocks avail to non-superuser */
+	uint64_t f_files;		/* total file nodes in filesystem */
+	int64_t	 f_ffree;		/* free nodes avail to non-superuser */
+	uint64_t f_syncwrites;		/* count of sync writes since mount */
+	uint64_t f_asyncwrites;		/* count of async writes since mount */
+	uint64_t f_syncreads;		/* count of sync reads since mount */
+	uint64_t f_asyncreads;		/* count of async reads since mount */
+	uint64_t f_spare[10];		/* unused spare */
+	uint32_t f_namemax;		/* maximum filename length */
+	uid_t	  f_owner;		/* user that mounted the filesystem */
+	fsid_t	  f_fsid;		/* filesystem id */
+	char	  f_charspare[80];	/* spare string space */
+	char	  f_fstypename[16];	/* filesystem type name */
+	char	  f_mntfromname[88];	/* mounted filesystem */
+	char	  f_mntonname[88];	/* directory on which mounted */
+};
+#endif /* _WANT_FREEBSD11_STATFS || _KERNEL */
 
 #ifdef _KERNEL
 #define	OMFSNAMELEN	16	/* length of fs type name, including null */
