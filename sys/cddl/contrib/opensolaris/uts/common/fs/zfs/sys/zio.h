ff130cdd0ae47e089bd26f3c265b297f0e9ffdc4
avg <avg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-05-24 22:21:24 +0000

    MFV r316917: 7968 multi-threaded spa_sync()
    illumos/illumos-gate@94c2d0eb22e9624151ee84a7edbf7178e1bf4087
    https://github.com/illumos/illumos-gate/commit/94c2d0eb22e9624151ee84a7edbf7178e1bf4087
    
    https://www.illumos.org/issues/7968
      spa_sync() iterates over all the dirty dnodes and processes each of them by
      calling dnode_sync(). If there are many dirty dnodes (e.g. because we created
      or removed a lot of files), the single thread of spa_sync() calling
      dnode_sync() can become a bottleneck. Additionally, if many dnodes are dirtied
      concurrently in open context (e.g. due to concurrent file creation), the
      os_lock will experience lock contention via dnode_setdirty().
      The solution is to track dirty dnodes on a multilist_t, and for spa_sync() to
      use separate threads to process each of the sublists in the multilist.
      On the concurrent file creation microbenchmark, the performance improvement
      from dnode_setdirty() is up to 7%. Additionally, the wall clock time spent in
      spa_sync() is reduced to 15%-40% of the single-threaded case. In terms of cost/
      reward, once the other bottlenecks are addressed, fixing this bug will provide
      a medium-large performance gain and require a medium amount of effort to
      implement.
    
    Reviewed by: Pavel Zakharov <pavel.zakharov@delphix.com>
    Reviewed by: Brad Lewis <brad.lewis@delphix.com>
    Reviewed by: Saso Kiselkov <saso.kiselkov@nexenta.com>
    Reviewed by: Brian Behlendorf <behlendorf1@llnl.gov>
    Approved by: Dan McDonald <danmcd@omniti.com>
    Author: Matthew Ahrens <mahrens@delphix.com>
    MFC after:	3 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@318828 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/zio.h
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/zio.h
@@ -22,7 +22,7 @@
 /*
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright 2011 Nexenta Systems, Inc.  All rights reserved.
- * Copyright (c) 2012, 2016 by Delphix. All rights reserved.
+ * Copyright (c) 2012, 2017 by Delphix. All rights reserved.
  * Copyright (c) 2013 by Saso Kiselkov. All rights reserved.
  * Copyright 2016 Toomas Soome <tsoome@me.com>
  */
@@ -485,7 +485,7 @@ struct zio {
 	list_node_t	io_trim_link;
 };
 
-extern int zio_timestamp_compare(const void *, const void *);
+extern int zio_bookmark_compare(const void *, const void *);
 
 extern zio_t *zio_null(zio_t *pio, spa_t *spa, vdev_t *vd,
     zio_done_func_t *done, void *priv, enum zio_flag flags);
