052311172549357d2a7949ea0863cb4dd96ff001
Pedro F. Giffuni <pfg@FreeBSD.org>
2017-05-26 01:14:58 +0000

    fts_open: move bogus initialization further below, before it is used.
    Move an unneeded initialization, introduced in r54770 to quiet down GCC,
    to a place nearer to its first use. This has no practical effect, it just
    keeps the garbage better sorted.
    
    Hinted by:	OpenBSD (CVS rev. 1.56, without obfuscations)

--- lib/libc/gen/fts.c
+++ lib/libc/gen/fts.c
@@ -137,9 +137,6 @@ fts_open(char * const *argv, int options,
 	sp->fts_compar = compar;
 	sp->fts_options = options;
 
-	/* Shush, GCC. */
-	tmp = NULL;
-
 	/* Logical walks turn on NOCHDIR; symbolic links are too hard. */
 	if (ISSET(FTS_LOGICAL))
 		SET(FTS_NOCHDIR);
@@ -155,6 +152,9 @@ fts_open(char * const *argv, int options,
 	if ((parent = fts_alloc(sp, "", 0)) == NULL)
 		goto mem2;
 	parent->fts_level = FTS_ROOTPARENTLEVEL;
+
+	/* Shush, GCC. */
+	tmp = NULL;
 
 	/* Allocate/initialize root(s). */
 	for (root = NULL, nitems = 0; *argv != NULL; ++argv, ++nitems) {
