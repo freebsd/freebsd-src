23bea16699cabe21283f21c13b4fb6d0586a8024
ngie <ngie@FreeBSD.org>
2017-06-01 06:35:40 +0000

    MFC r319157:
    fma_test: mute a warning about unreachable code on amd64 by restructuring
    the #ifdef block to only handle the rest of the logic in the loop in the
    #else case.
    
    CID:		1346844

--- lib/msun/tests/fma_test.c
+++ lib/msun/tests/fma_test.c
@@ -498,11 +498,12 @@ main(int argc, char *argv[])
 		printf("ok %d # SKIP testcase fails assertion on "
 		    "amd64\n", j);
 		continue;
-#endif
+#else
 		printf("rmode = %d\n", rmodes[i]);
 		fesetround(rmodes[i]);
 		test_infinities();
 		printf("ok %d - fma infinities\n", j);
+#endif
 	}
 
 	fesetround(FE_TONEAREST);
