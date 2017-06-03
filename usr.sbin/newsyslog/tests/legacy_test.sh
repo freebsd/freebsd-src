f54f69008fd0a3bf2768a83a1757ca83bfbea2d4
Enji Cooper <ngie@FreeBSD.org>
2017-06-03 18:10:04 +0000

    Don't execute the TODO cases in a subshell
    This messes up the testcase counter, as seen in bug 219756.
    
    PR:		212160, 219756
    Sponsored by:	Dell EMC Isilon

--- usr.sbin/newsyslog/tests/legacy_test.sh
+++ usr.sbin/newsyslog/tests/legacy_test.sh
@@ -380,7 +380,6 @@ tests_time_rotate() {
 
 	sleep 1.1
 
-	(
 	TODO="rotate time 2-4 fail today; bug 212160"
 
 	begin "rotate time 2 ${name_postfix}"
@@ -404,7 +403,7 @@ tests_time_rotate() {
 	ckfe ${LOGFNAME}
 	chkfcnt 3 ${dir}${LOGFNAME}.*${ext}
 	end
-	)
+	unset TODO
 
 	begin "noaction ${name_postfix}"
 	ofiles=`ls -1 ${dir}${LOGFNAME}.*${ext} | tr -d '\n'`
