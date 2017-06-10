dd0c462cf501fd543bcd96ad097fe6f12500ab3b
John Baldwin <jhb@FreeBSD.org>
2017-06-10 00:53:00 +0000

    Decode the arguments to quotactl().

--- usr.bin/truss/syscall.h
+++ usr.bin/truss/syscall.h
@@ -50,7 +50,7 @@ enum Argtype { None = 1, Hex, Octal, Int, UInt, LongHex, Name, Ptr, Stat, Ioctl,
 	Kldunloadflags, Sizet, Madvice, Socklent, Sockprotocol, Sockoptlevel,
 	Sockoptname, Msgflags, CapRights, PUInt, PQuadHex, Acltype,
 	Extattrnamespace, Minherit, Mlockall, Mountflags, Msync, Priowhich,
-	Ptraceop,
+	Ptraceop, Quotactlcmd,
 
 	CloudABIAdvice, CloudABIClockID, ClouduABIFDSFlags,
 	CloudABIFDStat, CloudABIFileStat, CloudABIFileType,
