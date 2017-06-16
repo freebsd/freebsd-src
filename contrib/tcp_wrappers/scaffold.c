eaa789e831bfb8f52fafdb307e4c32046d20b3e3
sbruno <sbruno@FreeBSD.org>
2017-06-16 22:32:23 +0000

    TCP Wrappers: tcpdchk (tcp wrapper configuration checker) and tcpdmatch (tcp wrapper oracle) warning fixes via edits to the C code files
    contrib/tcp_wrappers/fakelog.c
      Warnings for each of functions: openlog( ), vsyslog( ), VARARGS( ),
      closelog( )
        warning: type specifier missing, defaults to 'int' [-Wimplicit-int]
        warning: control reaches end of non-void function [-Wreturn-type]
      Fixes:
          Explicitly added specification of function type to void for each
            function, suppressing both warnings for each function listed
    contrib/tcp_wrappers/inetcf.c
      Warnings:
          warning: incompativle redeclaration of library function 'malloc'
            note: 'malloc' is a builtin with type 'void *(unsigned long)'
          warning: implicit declaration of function 'check_path' is invalid in C99
            [-Wimplicit-function-declaration]
      Fixes:
          Removed redeclaration of malloc on line 21
          Included library <stdlib.h> in the code which contains the malloc( )
            function in it's library
          Included scaffold.h header file in the code that contains check-path( )
            function
    contrib/tcp_wrappers/scaffold.c
      Warnings:
          warning: implicitly declaring library function 'exit' with type
            'void (int) __attribute__((noreturn))' [-Wimplicit-function-declaration]
          note: include the header <stdlib.h> or explicitly provide a declaration
            for 'exit'
      Fixes:
          Included <stdlib.h> in the code which contains the exit( ) function in
          it's library
    contrib/tcp_wrappers/tcpdchk.c
      Warnings:
          warning: implicit declaration of function 'getopt' is invalid
            in C99 [-Wimplicit-function-declaration]
          warning: implicit declaration of function 'atoi' is invalid
            in C99 [-Wimplicit-function-declaration]
      Fixes:
          Included the specific function <getopt.h> library to the code
          Included<stdlib.h> to the code which contains the atoi( ) function in
            the library
    contrib/tcp_wrappers/tcpdmatch.c
      Warnings:
          warning: implicit declaration of function 'getopt' is invalid in C99
            [-Wimplicit-function-declaration]
      Fixes:
          Included<stdlib.h> to the code which contains the getopt( ) function in
            the library
    
    Submitted by:	Aaron Prieger <aprieger@llnw.com>
    Reviewed by:	vangyzen
    Sponsored by:	Limelight Networks
    Differential Revision:	https://reviews.freebsd.org/D10995

--- contrib/tcp_wrappers/scaffold.c
+++ contrib/tcp_wrappers/scaffold.c
@@ -22,6 +22,7 @@ static char sccs_id[] = "@(#) scaffold.c 1.6 97/03/21 19:27:24";
 #include <syslog.h>
 #include <setjmp.h>
 #include <string.h>
+#include <stdlib.h>
 
 #ifndef INADDR_NONE
 #define	INADDR_NONE	(-1)		/* XXX should be 0xffffffff */
