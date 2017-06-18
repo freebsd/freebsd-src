d4e3484bd9166cab99605f864d8c3b102c754953
Alan Cox <alc@FreeBSD.org>
2017-06-18 18:23:39 +0000

    Change blist_alloc()'s allocation policy from first-fit to next-fit so that disk writes are more likely to be sequential.  This change is beneficial on both the solid state and mechanical disks that I've tested.  (A similar change in allocation policy was made by DragonFly BSD in 2013 to speed up Poudriere with "stressful memory parameters".)
    Increase the width of blst_meta_alloc()'s parameter "skip" and the local
    variables whose values are derived from it to 64 bits.  (This matches the
    width of the field "skip" that is stored in the structure "blist" and
    passed to blst_meta_alloc().)
    
    Eliminate a pointless check for a NULL blist_t.
    
    Simplify blst_meta_alloc()'s handling of the ALL-FREE case.
    
    Address nearby style errors.
    
    Reviewed by:	kib, markj
    MFC after:	5 weeks
    Differential Revision:	https://reviews.freebsd.org/D11247

--- sys/sys/blist.h
+++ sys/sys/blist.h
@@ -82,6 +82,7 @@ typedef struct blist {
 	daddr_t		bl_blocks;	/* area of coverage		*/
 	daddr_t		bl_radix;	/* coverage radix		*/
 	daddr_t		bl_skip;	/* starting skip		*/
+	daddr_t		bl_cursor;	/* next-fit search starts at	*/
 	blmeta_t	*bl_root;	/* root of radix tree		*/
 } *blist_t;
 
