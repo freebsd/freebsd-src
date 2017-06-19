7daa48d7765b58d52479bbf683144e4e2dc97cb8
Andriy Gapon <avg@FreeBSD.org>
2017-06-19 15:38:22 +0000

    MFC r311224: Fix PMC architecture check to handle later IPAs including Skylake

--- sys/dev/hwpmc/hwpmc_core.c
+++ sys/dev/hwpmc/hwpmc_core.c
@@ -2857,7 +2857,7 @@ pmc_core_initialize(struct pmc_mdep *md, int maxcpu, int version_override)
 	PMCDBG3(MDP,INI,1,"core-init cputype=%d ncpu=%d ipa-version=%d",
 	    core_cputype, maxcpu, ipa_version);
 
-	if (ipa_version < 1 || ipa_version > 3 ||
+	if (ipa_version < 1 || ipa_version > 4 ||
 	    (core_cputype != PMC_CPU_INTEL_CORE && ipa_version == 1)) {
 		/* Unknown PMC architecture. */
 		printf("hwpc_core: unknown PMC architecture: %d\n",
