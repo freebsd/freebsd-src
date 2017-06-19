ca3410f592e87fd2a82067ca4c875105b73d1cd7
markj <markj@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-06-19 21:09:50 +0000

    Fix the !TD_IS_IDLETHREAD(curthread) locking assertions.
    Most of the lock slowpaths assert that the calling thread isn't an idle
    thread. However, this may not be true if the system has panicked, and in
    some cases the assertion appears before a SCHEDULER_STOPPED() check.
    
    MFC after:	3 days
    Sponsored by:	Dell EMC Isilon
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@320124 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/kern_sx.c
+++ sys/kern/kern_sx.c
@@ -295,7 +295,8 @@ _sx_xlock(struct sx *sx, int opts, const char *file, int line)
 	uintptr_t tid, x;
 	int error = 0;
 
-	KASSERT(kdb_active != 0 || !TD_IS_IDLETHREAD(curthread),
+	KASSERT(kdb_active != 0 || SCHEDULER_STOPPED() ||
+	    !TD_IS_IDLETHREAD(curthread),
 	    ("sx_xlock() by idle thread %p on sx %s @ %s:%d",
 	    curthread, sx->lock_object.lo_name, file, line));
 	KASSERT(sx->sx_lock != SX_LOCK_DESTROYED,
@@ -332,7 +333,7 @@ sx_try_xlock_(struct sx *sx, const char *file, int line)
 	if (SCHEDULER_STOPPED_TD(td))
 		return (1);
 
-	KASSERT(kdb_active != 0 || !TD_IS_IDLETHREAD(curthread),
+	KASSERT(kdb_active != 0 || !TD_IS_IDLETHREAD(td),
 	    ("sx_try_xlock() by idle thread %p on sx %s @ %s:%d",
 	    curthread, sx->lock_object.lo_name, file, line));
 	KASSERT(sx->sx_lock != SX_LOCK_DESTROYED,
@@ -1030,7 +1031,8 @@ _sx_slock(struct sx *sx, int opts, const char *file, int line)
 	uintptr_t x;
 	int error;
 
-	KASSERT(kdb_active != 0 || !TD_IS_IDLETHREAD(curthread),
+	KASSERT(kdb_active != 0 || SCHEDULER_STOPPED() ||
+	    !TD_IS_IDLETHREAD(curthread),
 	    ("sx_slock() by idle thread %p on sx %s @ %s:%d",
 	    curthread, sx->lock_object.lo_name, file, line));
 	KASSERT(sx->sx_lock != SX_LOCK_DESTROYED,
