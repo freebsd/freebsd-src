94243ffdea5d8b7c85c094bb1d8fa50652ddf872
Brooks Davis <brooks@one-eyed-alien.net>
2017-06-24 01:33:10 +0000

    Fix CheriABI build after merge of e2a14c603f20af5eaf11dfafcd181d6d82269dd7

--- sys/mips/cheri/cheriabi_machdep.c
+++ sys/mips/cheri/cheriabi_machdep.c
@@ -111,8 +111,7 @@
 #define	DELAYBRANCH(x)	((int)(x) < 0)
 #define	UCONTEXT_MAGIC	0xACEDBADE
 
-static int	cheriabi_fetch_syscall_args(struct thread *td,
-		    struct syscall_args *sa);
+static int	cheriabi_fetch_syscall_args(struct thread *td);
 static void	cheriabi_set_syscall_retval(struct thread *td, int error);
 static void	cheriabi_sendsig(sig_t, ksiginfo_t *, sigset_t *);
 static void	cheriabi_exec_setregs(struct thread *, struct image_params *,
@@ -294,10 +293,11 @@ cheriabi_fetch_syscall_arg(struct thread *td, struct chericap *arg,
 }
 
 static int
-cheriabi_fetch_syscall_args(struct thread *td, struct syscall_args *sa)
+cheriabi_fetch_syscall_args(struct thread *td)
 {
 	struct trapframe *locr0 = td->td_frame;	 /* aka td->td_pcb->pcv_regs */
 	struct sysentvec *se;
+	struct syscall_args *sa;
 #ifdef OLD_ARG_HANDLING
 	register_t intargs[8];
 	uintptr_t ptrargs[8];
@@ -308,6 +308,7 @@ cheriabi_fetch_syscall_args(struct thread *td, struct syscall_args *sa)
 
 	error = 0;
 
+	sa = &td->td_sa;
 	bzero(sa->args, sizeof(sa->args));
 
 	/* compute next PC after syscall instruction */
