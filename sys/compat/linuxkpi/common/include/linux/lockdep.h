f0b1e63d76fbba3b3e081476ed0f27b4e4080d54
markj <markj@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-06-25 19:23:14 +0000

    Add a couple of macros to lockdep.h in the LinuxKPI.
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@320335 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/compat/linuxkpi/common/include/linux/lockdep.h
+++ sys/compat/linuxkpi/common/include/linux/lockdep.h
@@ -39,7 +39,12 @@ struct lock_class_key {
 
 #define	lockdep_set_class_and_name(lock, key, name)
 
+#define	lockdep_assert_held(m)				\
+	sx_assert(&(m)->sx, SA_XLOCKED)
+
 #define	lockdep_assert_held_once(m)			\
 	sx_assert(&(m)->sx, SA_XLOCKED | SA_NOTRECURSED)
+
+#define	lockdep_is_held(m)	(sx_xholder(&(m)->sx) == curthread)
 
 #endif /* _LINUX_LOCKDEP_H_ */
