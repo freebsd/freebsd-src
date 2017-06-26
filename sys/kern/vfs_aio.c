cdb8bdd13ee93fd0a7b549342468cd55b7c06c8d
Konstantin Belousov <kib@FreeBSD.org>
2017-06-26 12:17:04 +0000

    MFC r320108: Allow negative aio_offset only for the read and write LIO ops on device nodes.
    Approved by:	re (marius)

--- sys/kern/vfs_aio.c
+++ sys/kern/vfs_aio.c
@@ -1550,7 +1550,9 @@ aio_aqueue(struct thread *td, struct aiocb *ujob, struct aioliojob *lj,
 		goto aqueue_fail;
 	}
 
-	if (opcode != LIO_SYNC && job->uaiocb.aio_offset == -1LL) {
+	if ((opcode == LIO_READ || opcode == LIO_WRITE) &&
+	    job->uaiocb.aio_offset < 0 &&
+	    (fp->f_vnode == NULL || fp->f_vnode->v_type != VCHR)) {
 		error = EINVAL;
 		goto aqueue_fail;
 	}
