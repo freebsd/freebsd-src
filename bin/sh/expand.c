7dd60cfdd8a236bc962da535dafb44cd27edafa2
Jilles Tjoelker <jilles@FreeBSD.org>
2017-06-30 21:32:48 +0000

    MFC r315005: sh: Fix executing wrong command with ${x#$(y)}$(z).
    The parsed internal representation of words consists of a byte string with a
    list of nodes (commands in command substitution). Each unescaped CTLBACKQ or
    CTLBACKQ | CTLQUOTE byte corresponds to an entry in the list.
    
    If param in ${param#%##%%word} is not set, the word is not expanded (in a
    deviation of POSIX shared with other ash variants and ksh93). Erroneously,
    the pointer in the list of commands (argbackq) was not advanced. This caused
    the wrong command to be executed later if the outer word contained another
    command substitution.
    
    Example:
      echo "${unsetvar#$(echo a)}$(echo b)"
    wrote "a" but should write "b".

--- bin/sh/expand.c
+++ bin/sh/expand.c
@@ -740,8 +740,10 @@ again: /* jump here after setting a variable with ${var=text} */
 	case VSTRIMLEFTMAX:
 	case VSTRIMRIGHT:
 	case VSTRIMRIGHTMAX:
-		if (!set)
+		if (!set) {
+			set = 1;
 			break;
+		}
 		/*
 		 * Terminate the string and start recording the pattern
 		 * right after it
