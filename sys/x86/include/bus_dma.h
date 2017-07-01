d1caaa930005d8e83736591419549f62a1596d76
jah <jah@FreeBSD.org>
2017-07-01 05:35:29 +0000

    Clean up MD pollution of bus_dma.h:
    --Remove special-case handling of sparc64 bus_dmamap* functions.
      Replace with a more generic mechanism that allows MD busdma
      implementations to generate inline mapping functions by
      defining WANT_INLINE_DMAMAP in <machine/bus_dma.h>.  This
      is currently useful for sparc64, x86, and arm64, which all
      implement non-load dmamap operations as simple wrappers
      around map objects which may be bus- or device-specific.
    
    --Remove NULL-checked bus_dmamap macros.  Implement the
      equivalent NULL checks in the inlined x86 implementation.
      For non-x86 platforms, these checks are a minor pessimization
      as those platforms do not currently allow NULL maps.  NULL
      maps were originally allowed on arm64, which appears to have
      been the motivation behind adding arm[64]-specific barriers
      to bus_dma.h, but that support was removed in r299463.
    
    --Simplify the internal interface used by the bus_dmamap_load*
      variants and move it to bus_dma_internal.h
    
    --Fix some drivers that directly include sys/bus_dma.h
      despite the recommendations of bus_dma(9)
    
    Reviewed by:	kib (previous revision), marius
    Differential Revision:	https://reviews.freebsd.org/D10729

--- sys/x86/include/bus_dma.h
+++ sys/x86/include/bus_dma.h
@@ -1,12 +1,39 @@
-/* $FreeBSD$ */
+/*-
+ * Copyright (c) 2017 Jason A. Harmening.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
 
-#ifndef _MACHINE_BUS_DMA_H_
-#define	_MACHINE_BUS_DMA_H_
+#ifndef _X86_BUS_DMA_H_
+#define _X86_BUS_DMA_H_
 
 #define WANT_INLINE_DMAMAP
 #include <sys/bus_dma.h>
+#include <sys/_null.h>
 
-#include <machine/bus_dma_impl.h>
+#include <x86/busdma_impl.h>
 
 /*
  * Allocate a handle for mapping from kva/uva/physical
@@ -36,7 +63,7 @@ bus_dmamap_destroy(bus_dma_tag_t dmat, bus_dmamap_t map)
 
 /*
  * Allocate a piece of memory that can be efficiently mapped into
- * bus device space based on the constraints listed in the dma tag.
+ * bus device space based on the constraints lited in the dma tag.
  * A dmamap to for use with dmamap_load is also allocated.
  */
 static inline int
@@ -70,8 +97,10 @@ bus_dmamap_unload(bus_dma_tag_t dmat, bus_dmamap_t map)
 {
 	struct bus_dma_tag_common *tc;
 
-	tc = (struct bus_dma_tag_common *)dmat;
-	tc->impl->map_unload(dmat, map);
+	if (map != NULL) {
+		tc = (struct bus_dma_tag_common *)dmat;
+		tc->impl->map_unload(dmat, map);
+	}
 }
 
 static inline void
@@ -79,10 +108,16 @@ bus_dmamap_sync(bus_dma_tag_t dmat, bus_dmamap_t map, bus_dmasync_op_t op)
 {
 	struct bus_dma_tag_common *tc;
 
-	tc = (struct bus_dma_tag_common *)dmat;
-	tc->impl->map_sync(dmat, map, op);
+	if (map != NULL) {
+		tc = (struct bus_dma_tag_common *)dmat;
+		tc->impl->map_sync(dmat, map, op);
+	}
 }
 
+/*
+ * Utility function to load a physical buffer.  segp contains
+ * the starting segment on entrace, and the ending segment on exit.
+ */
 static inline int
 _bus_dmamap_load_phys(bus_dma_tag_t dmat, bus_dmamap_t map, vm_paddr_t buf,
     bus_size_t buflen, int flags, bus_dma_segment_t *segs, int *segp)
@@ -106,6 +141,10 @@ _bus_dmamap_load_ma(bus_dma_tag_t dmat, bus_dmamap_t map, struct vm_page **ma,
 	    segs, segp));
 }
 
+/*
+ * Utility function to load a linear buffer.  segp contains
+ * the starting segment on entrace, and the ending segment on exit.
+ */
 static inline int
 _bus_dmamap_load_buffer(bus_dma_tag_t dmat, bus_dmamap_t map, void *buf,
     bus_size_t buflen, struct pmap *pmap, int flags, bus_dma_segment_t *segs,
@@ -124,8 +163,10 @@ _bus_dmamap_waitok(bus_dma_tag_t dmat, bus_dmamap_t map,
 {
 	struct bus_dma_tag_common *tc;
 
-	tc = (struct bus_dma_tag_common *)dmat;
-	tc->impl->map_waitok(dmat, map, mem, callback, callback_arg);
+	if (map != NULL) {
+		tc = (struct bus_dma_tag_common *)dmat;
+		tc->impl->map_waitok(dmat, map, mem, callback, callback_arg);
+	}
 }
 
 static inline bus_dma_segment_t *
@@ -138,4 +179,5 @@ _bus_dmamap_complete(bus_dma_tag_t dmat, bus_dmamap_t map,
 	return (tc->impl->map_complete(dmat, map, segs, nsegs, error));
 }
 
-#endif /* !_MACHINE_BUS_DMA_H_ */
+#endif /* !_X86_BUS_DMA_H_ */
+
