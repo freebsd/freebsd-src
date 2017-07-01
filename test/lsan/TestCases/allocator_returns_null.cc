c932718cf8e0677adf53f92dcefa582a64b57d8d
dim <dim@FreeBSD.org>
2017-07-01 13:24:15 +0000

    Vendor import of compiler-rt trunk r306956: https://llvm.org/svn/llvm-project/compiler-rt/trunk@306956

--- test/lsan/TestCases/allocator_returns_null.cc
+++ test/lsan/TestCases/allocator_returns_null.cc
@@ -1,41 +1,40 @@
 // Test the behavior of malloc/calloc/realloc/new when the allocation size is
-// more than MSan allocator's max allowed one.
+// more than LSan allocator's max allowed one.
 // By default (allocator_may_return_null=0) the process should crash.
 // With allocator_may_return_null=1 the allocator should return 0, except the
 // operator new(), which should crash anyway (operator new(std::nothrow) should
 // return nullptr, indeed).
 //
-// RUN: %clangxx_msan -O0 %s -o %t
+// RUN: %clangxx_lsan -O0 %s -o %t
 // RUN: not %run %t malloc 2>&1 | FileCheck %s --check-prefix=CHECK-mCRASH
-// RUN: MSAN_OPTIONS=allocator_may_return_null=0 not %run %t malloc 2>&1 \
+// RUN: %env_lsan_opts=allocator_may_return_null=0 not %run %t malloc 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CHECK-mCRASH
-// RUN: MSAN_OPTIONS=allocator_may_return_null=1     %run %t malloc 2>&1 \
+// RUN: %env_lsan_opts=allocator_may_return_null=1     %run %t malloc 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CHECK-mNULL
-// RUN: MSAN_OPTIONS=allocator_may_return_null=0 not %run %t calloc 2>&1 \
+// RUN: %env_lsan_opts=allocator_may_return_null=0 not %run %t calloc 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CHECK-cCRASH
-// RUN: MSAN_OPTIONS=allocator_may_return_null=1     %run %t calloc 2>&1 \
+// RUN: %env_lsan_opts=allocator_may_return_null=1     %run %t calloc 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CHECK-cNULL
-// RUN: MSAN_OPTIONS=allocator_may_return_null=0 not %run %t calloc-overflow 2>&1 \
+// RUN: %env_lsan_opts=allocator_may_return_null=0 not %run %t calloc-overflow 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CHECK-coCRASH
-// RUN: MSAN_OPTIONS=allocator_may_return_null=1     %run %t calloc-overflow 2>&1 \
+// RUN: %env_lsan_opts=allocator_may_return_null=1     %run %t calloc-overflow 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CHECK-coNULL
-// RUN: MSAN_OPTIONS=allocator_may_return_null=0 not %run %t realloc 2>&1 \
+// RUN: %env_lsan_opts=allocator_may_return_null=0 not %run %t realloc 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CHECK-rCRASH
-// RUN: MSAN_OPTIONS=allocator_may_return_null=1     %run %t realloc 2>&1 \
+// RUN: %env_lsan_opts=allocator_may_return_null=1     %run %t realloc 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CHECK-rNULL
-// RUN: MSAN_OPTIONS=allocator_may_return_null=0 not %run %t realloc-after-malloc 2>&1 \
+// RUN: %env_lsan_opts=allocator_may_return_null=0 not %run %t realloc-after-malloc 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CHECK-mrCRASH
-// RUN: MSAN_OPTIONS=allocator_may_return_null=1     %run %t realloc-after-malloc 2>&1 \
+// RUN: %env_lsan_opts=allocator_may_return_null=1     %run %t realloc-after-malloc 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CHECK-mrNULL
-// RUN: MSAN_OPTIONS=allocator_may_return_null=0 not %run %t new 2>&1 \
+// RUN: %env_lsan_opts=allocator_may_return_null=0 not %run %t new 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CHECK-nCRASH
-// RUN: MSAN_OPTIONS=allocator_may_return_null=1 not %run %t new 2>&1 \
+// RUN: %env_lsan_opts=allocator_may_return_null=1 not %run %t new 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CHECK-nCRASH
-// RUN: MSAN_OPTIONS=allocator_may_return_null=0 not %run %t new-nothrow 2>&1 \
+// RUN: %env_lsan_opts=allocator_may_return_null=0 not %run %t new-nothrow 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CHECK-nnCRASH
-// RUN: MSAN_OPTIONS=allocator_may_return_null=1     %run %t new-nothrow 2>&1 \
+// RUN: %env_lsan_opts=allocator_may_return_null=1     %run %t new-nothrow 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CHECK-nnNULL
-
 
 #include <assert.h>
 #include <string.h>
@@ -52,11 +51,13 @@ int main(int argc, char **argv) {
   const char *action = argv[1];
   fprintf(stderr, "%s:\n", action);
 
+  // Use max of ASan and LSan allocator limits to cover both "lsan" and
+  // "lsan + asan" configs.
   static const size_t kMaxAllowedMallocSizePlusOne =
 #if __LP64__ || defined(_WIN64)
-      (8UL << 30) + 1;
+      (1ULL << 40) + 1;
 #else
-      (2UL << 30) + 1;
+      (3UL << 30) + 1;
 #endif
 
   void *x = 0;
@@ -87,26 +88,26 @@ int main(int argc, char **argv) {
 
   // The NULL pointer is printed differently on different systems, while (long)0
   // is always the same.
-  fprintf(stderr, "x: %lx\n", (long)x);
+  fprintf(stderr, "x: %zu\n", (size_t)x);
   free(x);
 
   return x != 0;
 }
 
 // CHECK-mCRASH: malloc:
-// CHECK-mCRASH: MemorySanitizer's allocator is terminating the process
+// CHECK-mCRASH: Sanitizer's allocator is terminating the process
 // CHECK-cCRASH: calloc:
-// CHECK-cCRASH: MemorySanitizer's allocator is terminating the process
+// CHECK-cCRASH: Sanitizer's allocator is terminating the process
 // CHECK-coCRASH: calloc-overflow:
-// CHECK-coCRASH: MemorySanitizer's allocator is terminating the process
+// CHECK-coCRASH: Sanitizer's allocator is terminating the process
 // CHECK-rCRASH: realloc:
-// CHECK-rCRASH: MemorySanitizer's allocator is terminating the process
+// CHECK-rCRASH: Sanitizer's allocator is terminating the process
 // CHECK-mrCRASH: realloc-after-malloc:
-// CHECK-mrCRASH: MemorySanitizer's allocator is terminating the process
+// CHECK-mrCRASH: Sanitizer's allocator is terminating the process
 // CHECK-nCRASH: new:
-// CHECK-nCRASH: MemorySanitizer's allocator is terminating the process
+// CHECK-nCRASH: Sanitizer's allocator is terminating the process
 // CHECK-nnCRASH: new-nothrow:
-// CHECK-nnCRASH: MemorySanitizer's allocator is terminating the process
+// CHECK-nnCRASH: Sanitizer's allocator is terminating the process
 
 // CHECK-mNULL: malloc:
 // CHECK-mNULL: x: 0
