86f1f93e061315c8a8732753ece2eac1d47a9361
Marcelo Araujo <araujo@FreeBSD.org>
2017-07-02 04:19:03 +0000

    MFC r319678:
    Allow sysctl kern.vm_guest to return bhyve when running under bhyve.
    
    Submitted by:	Sean Fagan <sef@ixsystems.com>
    Reviewed by:	grehan
    MFH:		4 weeks.
    Sponsored by:	iXsystems, Inc.
    Differential Revision:	https://reviews.freebsd.org/D11090

--- sys/x86/x86/identcpu.c
+++ sys/x86/x86/identcpu.c
@@ -1282,6 +1282,8 @@ identify_hypervisor(void)
 				vm_guest = VM_GUEST_HV;
 			else if (strcmp(hv_vendor, "KVMKVMKVM") == 0)
 				vm_guest = VM_GUEST_KVM;
+			else if (strcmp(hv_vendor, "bhyve bhyve") == 0)
+				vm_guest = VM_GUEST_BHYVE;
 		}
 		return;
 	}
