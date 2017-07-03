78d1a34e0054b87c259a706a3b4f62f1c7534c87
Mark Johnston <markj@FreeBSD.org>
2017-07-03 18:42:26 +0000

    MFC r303052: Make vm_pageout_wakeup_thresh a u_int rather than an int.

--- sys/vm/vm_pageout.c
+++ sys/vm/vm_pageout.c
@@ -159,7 +159,7 @@ SYSINIT(vmdaemon, SI_SUB_KTHREAD_VM, SI_ORDER_FIRST, kproc_start, &vm_kp);
 #define	VM_INACT_SCAN_RATE	2
 
 int vm_pageout_deficit;		/* Estimated number of pages deficit */
-int vm_pageout_wakeup_thresh;
+u_int vm_pageout_wakeup_thresh;
 static int vm_pageout_oom_seq = 12;
 bool vm_pageout_wanted;		/* Event on which pageout daemon sleeps */
 bool vm_pages_needed;		/* Are threads waiting for free pages? */
