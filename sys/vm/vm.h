2110f0a42cd44df4dc7f29dec76ac97c84fbb482
Konstantin Belousov <kib@FreeBSD.org>
2017-07-05 06:18:00 +0000

    Add MAP_GUARD and use it for stack grow area protection. Bump __FreeBSD_version.
    MFC r320317:
    Implement address space guards.
    
    MFC r320338:
    Remove stale part of the comment.
    
    MFC r320339:
    Correctly handle small MAP_STACK requests.
    
    MFC r320344:
    For now, allow mprotect(2) over the guards to succeed regardless of
    the requested protection.
    
    MFC r320430:
    Treat the addr argument for mmap(2) request without MAP_FIXED flag as
    a hint.
    
    MFC r320560 (by alc):
    Modify vm_map_growstack() to protect itself from the possibility of the
    gap entry in the vm map being smaller than the sysctl-derived stack guard
    size.

--- sys/vm/vm.h
+++ sys/vm/vm.h
@@ -78,6 +78,7 @@ typedef u_char vm_prot_t;	/* protection codes */
 #define	VM_PROT_WRITE		((vm_prot_t) 0x02)
 #define	VM_PROT_EXECUTE		((vm_prot_t) 0x04)
 #define	VM_PROT_COPY		((vm_prot_t) 0x08)	/* copy-on-read */
+#define	VM_PROT_FAULT_LOOKUP	((vm_prot_t) 0x010)
 
 #define	VM_PROT_ALL		(VM_PROT_READ|VM_PROT_WRITE|VM_PROT_EXECUTE)
 #define VM_PROT_RW		(VM_PROT_READ|VM_PROT_WRITE)
