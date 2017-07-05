9f5f79c1f6951302d71c32ceff2ea12a2b86bef2
erj <erj@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-07-05 17:27:03 +0000

    ixgbe(4): Update HEAD (p3) to 3.2.12-k
    Includes:
    
    - Support for X550EM devices.
    - Support for Bypass adapters.
    - Flow Director code moved to separate files
    - SR-IOV code moved to separate files
    - Netmap code moved to separate files
    
    Differential Revision:	https://reviews.freebsd.org/D11232
    Submitted by:	Jeb Cramer <cramerj@intel.com>
    Reviewed by:	erj@
    Tested by:	Jeff Pieper <jeffrey.e.pieper@intel.com>
    Sponsored by:	Intel Corporation
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@320688 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ixgbe/ixgbe_fdir.h
+++ sys/dev/ixgbe/ixgbe_fdir.h
@@ -32,23 +32,27 @@
 ******************************************************************************/
 /*$FreeBSD$*/
 
-#ifndef _IXGBE_82598_H_
-#define _IXGBE_82598_H_
+#ifndef _IXGBE_FDIR_H_
+#define _IXGBE_FDIR_H_
 
-u32 ixgbe_get_pcie_msix_count_82598(struct ixgbe_hw *hw);
-s32 ixgbe_fc_enable_82598(struct ixgbe_hw *hw);
-s32 ixgbe_start_hw_82598(struct ixgbe_hw *hw);
-void ixgbe_enable_relaxed_ordering_82598(struct ixgbe_hw *hw);
-s32 ixgbe_set_vmdq_82598(struct ixgbe_hw *hw, u32 rar, u32 vmdq);
-s32 ixgbe_set_vfta_82598(struct ixgbe_hw *hw, u32 vlan, u32 vind, bool vlan_on,
-			 bool vlvf_bypass);
-s32 ixgbe_read_analog_reg8_82598(struct ixgbe_hw *hw, u32 reg, u8 *val);
-s32 ixgbe_write_analog_reg8_82598(struct ixgbe_hw *hw, u32 reg, u8 val);
-s32 ixgbe_read_i2c_eeprom_82598(struct ixgbe_hw *hw, u8 byte_offset,
-				u8 *eeprom_data);
-u64 ixgbe_get_supported_physical_layer_82598(struct ixgbe_hw *hw);
-s32 ixgbe_init_phy_ops_82598(struct ixgbe_hw *hw);
-void ixgbe_set_lan_id_multi_port_pcie_82598(struct ixgbe_hw *hw);
-void ixgbe_set_pcie_completion_timeout(struct ixgbe_hw *hw);
-s32 ixgbe_enable_rx_dma_82598(struct ixgbe_hw *hw, u32 regval);
-#endif /* _IXGBE_82598_H_ */
+#ifdef IXGBE_FDIR
+
+/*
+ * Flow Director actually 'steals' part of the packet buffer
+ * as its filter pool, this variable controls how much it uses:
+ *  0 = 64K, 1 = 128K, 2 = 256K
+ */
+int fdir_pballoc = 1;
+
+void ixgbe_init_fdir(struct adapter *);
+
+#else
+
+#define ixgbe_init_fdir(_a)
+
+#endif
+
+void ixgbe_reinit_fdir(void *, int);
+void ixgbe_atr(struct tx_ring *, struct mbuf *);
+
+#endif /* _IXGBE_FDIR_H_ */
