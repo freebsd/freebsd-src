f3a15e3998ddb8210f8934d1817889766f6586df
Rick Macklem <rmacklem@FreeBSD.org>
2017-07-15 19:18:37 +0000

    MFC: r320345 Add support to the NFSv4.1/pNFS client for commits through the DS.
    A NFSv4.1/pNFS server using File Layout can specify that Commit operations
    are to be done against the DS instead of MDS. Since no extant pNFS
    server did this, the code was untested and "#ifdef notyet".
    The FreeBSD pNFS server I am developing does specify that Commits be done
    through the DS, so the code has been enabled/tested.
    This patch should only affect the case of a pNFS server that specfies
    Commits through the DS.
    
    Relnotes:	yes

--- sys/fs/nfs/nfs_var.h
+++ sys/fs/nfs/nfs_var.h
@@ -490,7 +490,7 @@ int nfsrpc_layoutreturn(struct nfsmount *, uint8_t *, int, int, int, uint32_t,
     int, uint64_t, uint64_t, nfsv4stateid_t *, int, uint32_t *, struct ucred *,
     NFSPROC_T *, void *);
 int nfsrpc_reclaimcomplete(struct nfsmount *, struct ucred *, NFSPROC_T *);
-int nfscl_doiods(vnode_t, struct uio *, int *, int *, uint32_t,
+int nfscl_doiods(vnode_t, struct uio *, int *, int *, uint32_t, int,
     struct ucred *, NFSPROC_T *);
 int nfscl_findlayoutforio(struct nfscllayout *, uint64_t, uint32_t,
     struct nfsclflayout **);
