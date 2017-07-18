812d35653757126540b1f936db779e77fc7316b9
Enji Cooper <ngie@FreeBSD.org>
2017-07-18 08:15:02 +0000

    MFC r279992,r280149,r280193,r288223,r288484,r321109:
    r279992 (by ian):
    
    Add a new flag, SBUF_INCLUDENUL, and new get/set/clear functions for flags.
    
    The SBUF_INCLUDENUL flag causes the nulterm byte at the end of the string
    to be counted in the length of the data.  If copying the data using the
    sbuf_data() and sbuf_len() functions, or if writing it automatically with
    a drain function, the net effect is that the nulterm byte is copied along
    with the rest of the data.
    
    r280149 (by ian):
    
    Update an sbuf assertion to allow for the new SBUF_INCLUDENUL flag.  If
    INCLUDENUL is set and sbuf_finish() has been called, the length has been
    incremented to count the nulterm byte, and in that case current length is
    allowed to be equal to buffer size, otherwise it must be less than.
    
    Add a predicate macro to test for SBUF_INCLUDENUL, and use it in tests, to
    be consistant with the style in the rest of this file.
    
    r280193 (by ian):
    
    The minimum sbuf buffer size is 2 bytes (a byte plus a nulterm), assert that.
    
    Values smaller than two lead to strange asserts that have nothing to do
    with the actual problem (in the case of size=0), or to writing beyond the
    end of the allocated buffer in sbuf_finish() (in the case of size=1).
    
    r288223 (by cem):
    
    sbuf: Process more than one char at a time
    
    Revamp sbuf_put_byte() to sbuf_put_bytes() in the obvious fashion and
    fixup callers.
    
    Add a thin shim around sbuf_put_bytes() with the old ABI to avoid ugly
    changes to some callers.
    
    Obtained from:	Dan Sledz
    
    r288484 (by phk):
    
    Fail the sbuf if vsnprintf(3) fails.
    
    r321109:
    
    Fix whitespace regression accidentally checked in via ^/head@r280149

--- sys/sys/sbuf.h
+++ sys/sys/sbuf.h
@@ -48,6 +48,7 @@ struct sbuf {
 	ssize_t		 s_len;		/* current length of string */
 #define	SBUF_FIXEDLEN	0x00000000	/* fixed length buffer (default) */
 #define	SBUF_AUTOEXTEND	0x00000001	/* automatically extend buffer */
+#define	SBUF_INCLUDENUL	0x00000002	/* nulterm byte is counted in len */
 #define	SBUF_USRFLAGMSK	0x0000ffff	/* mask of flags the user may specify */
 #define	SBUF_DYNAMIC	0x00010000	/* s_buf must be freed */
 #define	SBUF_FINISHED	0x00020000	/* set by sbuf_finish() */
@@ -72,6 +73,9 @@ __BEGIN_DECLS
 struct sbuf	*sbuf_new(struct sbuf *, char *, int, int);
 #define		 sbuf_new_auto()				\
 	sbuf_new(NULL, NULL, 0, SBUF_AUTOEXTEND)
+int		 sbuf_get_flags(struct sbuf *);
+void		 sbuf_clear_flags(struct sbuf *, int);
+void		 sbuf_set_flags(struct sbuf *, int);
 void		 sbuf_clear(struct sbuf *);
 int		 sbuf_setpos(struct sbuf *, ssize_t);
 int		 sbuf_bcat(struct sbuf *, const void *, size_t);
