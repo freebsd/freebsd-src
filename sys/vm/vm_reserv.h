1179a1717e92634988c87d9823930244dfdb8657
alc <alc@FreeBSD.org>
2017-07-23 16:28:13 +0000

    Utilize pmap_enter(..., psind=1) in vm_fault_soft_fast() on amd64.  (The Differential Revision discusses the benefits of this change.)
    Add a function, vm_reserv_to_superpage(), that returns the superpage
    containing the specified base page.
    
    Reviewed by:	kib, markj
    Tested by:	pho
    MFC after:	10 days
    Differential Revision:	https://reviews.freebsd.org/D11556

--- sys/vm/vm_reserv.h
+++ sys/vm/vm_reserv.h
@@ -64,6 +64,7 @@ void		vm_reserv_rename(vm_page_t m, vm_object_t new_object,
 int		vm_reserv_size(int level);
 vm_paddr_t	vm_reserv_startup(vm_offset_t *vaddr, vm_paddr_t end,
 		    vm_paddr_t high_water);
+vm_page_t	vm_reserv_to_superpage(vm_page_t m);
 
 #endif	/* VM_NRESERVLEVEL > 0 */
 #endif	/* _KERNEL */
