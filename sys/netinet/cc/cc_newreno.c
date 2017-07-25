fb327650f88cd579bbe48354e7b205d656093bd9
sbruno <sbruno@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-07-25 15:03:05 +0000

    Revert r308180 - Set slow start threshold more accurrately on loss ...
    This was discussed between various transport@ members and it was
    requested to be reverted and discussed.
    
    Submitted by:	kevin
    Reported by:	lawerence
    Reviewed by:	hiren
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@321479 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/cc/cc_newreno.c
+++ sys/netinet/cc/cc_newreno.c
@@ -182,26 +182,23 @@ newreno_after_idle(struct cc_var *ccv)
 static void
 newreno_cong_signal(struct cc_var *ccv, uint32_t type)
 {
-	uint32_t cwin, ssthresh_on_loss;
+	uint32_t cwin;
 	u_int mss;
 
 	cwin = CCV(ccv, snd_cwnd);
 	mss = CCV(ccv, t_maxseg);
-	ssthresh_on_loss =
-	    max((CCV(ccv, snd_max) - CCV(ccv, snd_una)) / 2 / mss, 2)
-		* mss;
 
 	/* Catch algos which mistakenly leak private signal types. */
 	KASSERT((type & CC_SIGPRIVMASK) == 0,
 	    ("%s: congestion signal type 0x%08x is private\n", __func__, type));
 
-	cwin = max(cwin / 2 / mss, 2) * mss;
+	cwin = max(2*mss, cwin/2);
 
 	switch (type) {
 	case CC_NDUPACK:
 		if (!IN_FASTRECOVERY(CCV(ccv, t_flags))) {
 			if (!IN_CONGRECOVERY(CCV(ccv, t_flags))) {
-				CCV(ccv, snd_ssthresh) = ssthresh_on_loss;
+				CCV(ccv, snd_ssthresh) = cwin;
 				CCV(ccv, snd_cwnd) = cwin;
 			}
 			ENTER_RECOVERY(CCV(ccv, t_flags));
@@ -209,13 +206,13 @@ newreno_cong_signal(struct cc_var *ccv, uint32_t type)
 		break;
 	case CC_ECN:
 		if (!IN_CONGRECOVERY(CCV(ccv, t_flags))) {
-			CCV(ccv, snd_ssthresh) = ssthresh_on_loss;
+			CCV(ccv, snd_ssthresh) = cwin;
 			CCV(ccv, snd_cwnd) = cwin;
 			ENTER_CONGRECOVERY(CCV(ccv, t_flags));
 		}
 		break;
 	case CC_RTO:
-		CCV(ccv, snd_ssthresh) = ssthresh_on_loss;
+		CCV(ccv, snd_ssthresh) = cwin;
 		CCV(ccv, snd_cwnd) = mss;
 		break;
 	}
