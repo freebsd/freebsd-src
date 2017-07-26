c104732f897447dc4028196bd063d2057baba77a
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-07-26 20:07:05 +0000

    Mark pages after EOF as clean after pageout.
    Suppose that a file on NFS has partially filled last page, and this
    page is dirty.  NFS VOP_PAGEOUT() method only marks the the page clean
    up to the block of the last written byte, leaving other blocks dirty.
    Also any page which erronously exists in the vnode vm_object past EOF
    is also left marked as dirty.
    
    With the introduction of the buf-cache coherent pager, each pass of
    syncer over the object with such page results in creation of B_DELWRI
    buffer due to VOP_WRITE() call.  This buffer is noted on next syncer
    pass, which results e.g. a visible manifestation of shutdown never
    finishing vnode sync.  Note that before buf-cache coherency commit, a
    dirty page might left never synced to server if a partial writes
    occur.
    
    Fix this by clearing dirty bits after EOF.  Only blocks of the partial
    page which are completely after EOF are marked clean, to avoid
    possible user data loss.
    
    Reported by:	mav
    Reviewed by:	alc, markj
    Tested by:	mav, pho
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D11697
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@321581 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/fs/smbfs/smbfs_io.c
+++ sys/fs/smbfs/smbfs_io.c
@@ -621,9 +621,11 @@ smbfs_putpages(ap)
 
 	relpbuf(bp, &smbfs_pbuf_freecnt);
 
-	if (!error)
-		vnode_pager_undirty_pages(pages, rtvals, count - uio.uio_resid);
-	return rtvals[0];
+	if (error == 0) {
+		vnode_pager_undirty_pages(pages, rtvals, count - uio.uio_resid,
+		    npages * PAGE_SIZE, npages * PAGE_SIZE);
+	}
+	return (rtvals[0]);
 #endif /* SMBFS_RWGENERIC */
 }
 
