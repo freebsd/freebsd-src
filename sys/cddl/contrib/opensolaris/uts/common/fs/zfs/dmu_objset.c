94051db8565ce3b209e84d21a82ba4af28b5a071
Alexander Motin <mav@FreeBSD.org>
2017-07-26 16:35:17 +0000

    MFC r318821: MFV r316912: 7793 ztest fails assertion in dmu_tx_willuse_space
    illumos/illumos-gate@61e255ce7267b52208af9daf434b77d37fb75622
    https://github.com/illumos/illumos-gate/commit/61e255ce7267b52208af9daf434b77d37
    fb75622
    
    https://www.illumos.org/issues/7793
      Background information: This assertion about tx_space_* verifies that we
      are not dirtying more stuff than we thought we would. We “need” to know
      how much we will dirty so that we can check if we should fail this
      transaction with ENOSPC/EDQUOT, in dmu_tx_assign(). While the
      transaction is open (i.e. between dmu_tx_assign() and dmu_tx_commit() —
      typically less than a millisecond), we call dbuf_dirty() on the exact
      blocks that will be modified. Once this happens, the temporary
      accounting in tx_space_* is unnecessary, because we know exactly what
      blocks are newly dirtied; we call dnode_willuse_space() to track this
      more exact accounting.
      The fundamental problem causing this bug is that dmu_tx_hold_*() relies
      on the current state in the DMU (e.g. dn_nlevels) to predict how much
      will be dirtied by this transaction, but this state can change before we
      actually perform the transaction (i.e. call dbuf_dirty()).
      This bug will be fixed by removing the assertion that the tx_space_*
      accounting is perfectly accurate (i.e. we never dirty more than was
      predicted by dmu_tx_hold_*()). By removing the requirement that this
      accounting be perfectly accurate, we can also vastly simplify it, e.g.
      removing most of the logic in dmu_tx_count_*().
      The new tx space accounting will be very approximate, and may be more or
      less than what is actually dirtied. It will still be used to determine
      if this transaction will put us over quota. Transactions that are marked
      by dmu_tx_mark_netfree() will be excepted from this check. We won’t make
      an attempt to determine how much space will be freed by the transaction
      — this was rarely accurate enough to determine if a transaction should
      be permitted when we are over quota, which is why dmu_tx_mark_netfree()
      was introduced in 2014.
      We also won’t attempt to give “credit” when overwriting existing blocks,
      if those blocks may be freed. This allows us to remove the
      do_free_accounting logic in dbuf_dirty(), and associated routines. This
    
    Reviewed by: Steve Gonczi <steve.gonczi@delphix.com>
    Reviewed by: George Wilson <george.wilson@delphix.com>
    Reviewed by: Pavel Zakharov <pavel.zakharov@delphix.com>
    Reviewed by: Brian Behlendorf <behlendorf1@llnl.gov>
    Approved by: Robert Mustacchi <rm@joyent.com>
    Author: Matthew Ahrens <mahrens@delphix.com>

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/dmu_objset.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/dmu_objset.c
@@ -2103,3 +2103,20 @@ dmu_fsname(const char *snapname, char *buf)
 	(void) strlcpy(buf, snapname, atp - snapname + 1);
 	return (0);
 }
+
+/*
+ * Call when we think we're going to write/free space in open context to track
+ * the amount of dirty data in the open txg, which is also the amount
+ * of memory that can not be evicted until this txg syncs.
+ */
+void
+dmu_objset_willuse_space(objset_t *os, int64_t space, dmu_tx_t *tx)
+{
+	dsl_dataset_t *ds = os->os_dsl_dataset;
+	int64_t aspace = spa_get_worst_case_asize(os->os_spa, space);
+
+	if (ds != NULL) {
+		dsl_dir_willuse_space(ds->ds_dir, aspace, tx);
+		dsl_pool_dirty_space(dmu_tx_pool(tx), space, tx);
+	}
+}
