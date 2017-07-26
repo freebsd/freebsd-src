ef734bedd3ce123e586cc5aa6cba31021b47836a
hselasky <hselasky@FreeBSD.org>
2017-07-26 16:44:24 +0000

    Set length of socket address in krping(). Else sobind() will fail with EINVAL.
    Submitted by:	Chelsio
    Sponsored by:	Mellanox Technologies

--- sys/contrib/rdma/krping/krping.c
+++ sys/contrib/rdma/krping/krping.c
@@ -1375,11 +1375,13 @@ static void fill_sockaddr(struct sockaddr_storage *sin, struct krping_cb *cb)
 
 	if (cb->addr_type == AF_INET) {
 		struct sockaddr_in *sin4 = (struct sockaddr_in *)sin;
+		sin4->sin_len = sizeof(*sin4);
 		sin4->sin_family = AF_INET;
 		memcpy((void *)&sin4->sin_addr.s_addr, cb->addr, 4);
 		sin4->sin_port = cb->port;
 	} else if (cb->addr_type == AF_INET6) {
 		struct sockaddr_in6 *sin6 = (struct sockaddr_in6 *)sin;
+		sin6->sin6_len = sizeof(*sin6);
 		sin6->sin6_family = AF_INET6;
 		memcpy((void *)&sin6->sin6_addr, cb->addr, 16);
 		sin6->sin6_port = cb->port;
