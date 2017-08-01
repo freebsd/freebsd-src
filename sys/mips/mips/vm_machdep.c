7218f6fe4baea6134485c5b61bb8dba508079c0f
John Baldwin <jhb@FreeBSD.org>
2017-08-01 12:14:08 -0700

    Clear the trusted stack when creating a new thread.
    Previously this code copied the trusted stack for a new thread from the
    creating thread.  However, this is not consistent with how normal thread
    stacks work.

--- sys/mips/mips/vm_machdep.c
+++ sys/mips/mips/vm_machdep.c
@@ -452,7 +452,7 @@ cpu_copy_thread(struct thread *td, struct thread *td0)
 #else
 	cheri_bcopy(td0->td_pcb, pcb2, sizeof(*pcb2));
 	cheri_signal_copy(pcb2, td0->td_pcb);
-	cheri_stack_copy(pcb2, td0->td_pcb);
+	cheri_stack_init(pcb2);
 	cheri_sealcap_copy(pcb2, td0->td_pcb);
 #endif
 
