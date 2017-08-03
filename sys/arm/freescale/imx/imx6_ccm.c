294019bdb5965fd114ab25cba7f7c342b5eabe10
ian <ian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-08-03 14:43:41 +0000

    Add an ahci driver for imx6.
    This was submitted by Rogiel Sulzbach (thank you!) but has a few last-minute
    changes by me, mostly where the code interfaces to my still-utterly-deficient
    imx6_ccm clocks implementation.  So blame me for any mistakes.
    
    Submitted by:	Rogiel Sulzbach <rogiel@rogiel.com>
    Differential Revision:	https://reviews.freebsd.org/D11177
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@322015 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/arm/freescale/imx/imx6_ccm.c
+++ sys/arm/freescale/imx/imx6_ccm.c
@@ -314,6 +314,41 @@ imx_ccm_usbphy_enable(device_t _phydev)
 #endif
 }
 
+int
+imx6_ccm_sata_enable(void)
+{
+	uint32_t v;
+	int timeout;
+
+	/* Un-gate the sata controller. */
+	WR4(ccm_sc, CCM_CCGR5, RD4(ccm_sc, CCM_CCGR5) | CCGR5_SATA);
+
+	/* Power up the PLL that feeds ENET/SATA/PCI phys, wait for lock. */
+	v = RD4(ccm_sc, CCM_ANALOG_PLL_ENET);
+	v &= ~CCM_ANALOG_PLL_ENET_POWERDOWN;
+	WR4(ccm_sc, CCM_ANALOG_PLL_ENET, v);
+
+	for (timeout = 100000; timeout > 0; timeout--) {
+		if (RD4(ccm_sc, CCM_ANALOG_PLL_ENET) &
+		   CCM_ANALOG_PLL_ENET_LOCK) {
+			break;
+		}
+	}
+	if (timeout <= 0) {
+		return ETIMEDOUT;
+	}
+
+	/* Enable the PLL, and enable its 100mhz output. */
+	v |= CCM_ANALOG_PLL_ENET_ENABLE;
+	v &= ~CCM_ANALOG_PLL_ENET_BYPASS;
+	WR4(ccm_sc, CCM_ANALOG_PLL_ENET, v);
+
+	v |= CCM_ANALOG_PLL_ENET_ENABLE_100M;
+	WR4(ccm_sc, CCM_ANALOG_PLL_ENET, v);
+
+	return 0;
+}
+
 uint32_t
 imx_ccm_ipg_hz(void)
 {
