230a19376f9badb3f2ca623d1b648e95c21f042b
Xin LI <delphij@FreeBSD.org>
2017-08-10 06:59:07 +0000

    Fix OpenSSH Denial of Service vulnerability. [SA-17:06]
    Fix VNET kernel panic with asynchronous I/O. [EN-17:07]
    
    Fix pf(4) housekeeping thread causes kernel panic. [EN-17:08]
    
    Approved by:	so

--- sys/kern/sys_socket.c
+++ sys/kern/sys_socket.c
@@ -675,6 +675,7 @@ soaio_process_sb(struct socket *so, struct sockbuf *sb)
 {
 	struct kaiocb *job;
 
+	CURVNET_SET(so->so_vnet);
 	SOCKBUF_LOCK(sb);
 	while (!TAILQ_EMPTY(&sb->sb_aiojobq) && soaio_ready(so, sb)) {
 		job = TAILQ_FIRST(&sb->sb_aiojobq);
@@ -698,6 +699,7 @@ soaio_process_sb(struct socket *so, struct sockbuf *sb)
 	ACCEPT_LOCK();
 	SOCK_LOCK(so);
 	sorele(so);
+	CURVNET_RESTORE();
 }
 
 void
