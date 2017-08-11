ff91b7277c5acc8bf03c0931a978110c7c6ec373
kib <kib@FreeBSD.org>
2017-08-11 11:38:04 +0000

    MFC r322059: Fix off by one in calculation of the number of buckets for the pc addresses.
    Approved by:	re (marius)

--- usr.sbin/pmcstat/pmcpl_gprof.c
+++ usr.sbin/pmcstat/pmcpl_gprof.c
@@ -464,8 +464,8 @@ pmcpl_gmon_process(struct pmcstat_process *pp, struct pmcstat_pmcrecord *pmcr,
 		    image, pmcid);
 		pgf->pgf_pmcid = pmcid;
 		assert(image->pi_end > image->pi_start);
-		pgf->pgf_nbuckets = (image->pi_end - image->pi_start) /
-		    FUNCTION_ALIGNMENT;	/* see <machine/profile.h> */
+		pgf->pgf_nbuckets = howmany(image->pi_end - image->pi_start,
+		    FUNCTION_ALIGNMENT);	/* see <machine/profile.h> */
 		pgf->pgf_ndatabytes = sizeof(struct gmonhdr) +
 		    pgf->pgf_nbuckets * hc_sz;
 		pgf->pgf_nsamples = 0;
