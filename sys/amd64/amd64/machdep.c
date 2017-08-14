173ac9160e83a9396d44938ef9789b6e62e48e70
sephe <sephe@FreeBSD.org>
2017-08-14 06:46:15 +0000

    MFC 322323 by jkim
        Split identify_cpu() into two functions for amd64 as we do for i386.  This
        reduces diff between amd64 and i386.  Also, it fixes a regression introduced
        in r322076, i.e., identify_hypervisor() failed to identify some hypervisors.
        This function assumes cpu_feature2 is already initialized.
    
        Reported by:    dexuan
        Tested by:      dexuan

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -1544,6 +1544,7 @@ hammer_time(u_int64_t modulep, u_int64_t physfree)
 
 	kmdp = init_ops.parse_preload_data(modulep);
 
+	identify_cpu();
 	identify_hypervisor();
 
 	/* Init basic tunables, hz etc */
@@ -1650,7 +1651,7 @@ hammer_time(u_int64_t modulep, u_int64_t physfree)
 	    != NULL)
 		vty_set_preferred(VTY_VT);
 
-	identify_cpu();		/* Final stage of CPU initialization */
+	finishidentcpu();	/* Final stage of CPU initialization */
 	initializecpu();	/* Initialize CPU registers */
 	initializecpucache();
 
