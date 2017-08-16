34a0f7f6f007028cd6291efa9eb13e8eb3053f57
br <br@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-08-16 08:49:11 +0000

    Add OBJ_PG_DTOR flag to VM object.
    Setting this flag allows us to skip pages removal from VM object queue
    during object termination and to leave that for cdev_pg_dtor function.
    
    Move pages removal code to separate function vm_object_terminate_pages()
    as comments does not survive indentation.
    
    This will be required for Intel SGX support where we will have to remove
    pages from VM object manually.
    
    Reviewed by:	kib, alc
    Sponsored by:	DARPA, AFRL
    Differential Revision:	https://reviews.freebsd.org/D11688
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@322571 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/vm/vm_object.h
+++ sys/vm/vm_object.h
@@ -175,6 +175,7 @@ struct vm_object {
 #define	OBJ_NOSPLIT	0x0010		/* dont split this object */
 #define	OBJ_UMTXDEAD	0x0020		/* umtx pshared was terminated */
 #define	OBJ_PIPWNT	0x0040		/* paging in progress wanted */
+#define	OBJ_PG_DTOR	0x0080		/* dont reset object, leave that for dtor */
 #define	OBJ_MIGHTBEDIRTY 0x0100		/* object might be dirty, only for vnode */
 #define	OBJ_TMPFS_NODE	0x0200		/* object belongs to tmpfs VREG node */
 #define	OBJ_TMPFS_DIRTY	0x0400		/* dirty tmpfs obj */
