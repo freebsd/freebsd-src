591cb29011074ad2faa934156e8cd99506271ace
Alexander Motin <mav@FreeBSD.org>
2017-08-16 10:28:34 +0000

    MFC r322308: Add new Intel Lewisburg and Union Point chipset PCI IDs.
    While there, polish some old AHCI ones, since they are still reused.

--- sys/dev/usb/controller/xhci_pci.c
+++ sys/dev/usb/controller/xhci_pci.c
@@ -132,6 +132,10 @@ xhci_pci_match(device_t self)
 		return ("Intel Sunrise Point-LP USB 3.0 controller");
 	case 0xa12f8086:
 		return ("Intel Sunrise Point USB 3.0 controller");
+	case 0xa1af8086:
+		return ("Intel Lewisburg USB 3.0 controller");
+	case 0xa2af8086:
+		return ("Intel Union Point USB 3.0 controller");
 
 	case 0xa01b177d:
 		return ("Cavium ThunderX USB 3.0 controller");
