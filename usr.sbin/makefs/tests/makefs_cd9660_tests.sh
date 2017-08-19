71a9d246c9d123df2f8a0918a1e0326b20f78afe
ngie <ngie@FreeBSD.org>
2017-08-19 01:45:02 +0000

    MFC r321947:
    Require strings(1) with :o_flag_preparer and :o_flag_publisher
    
    strings(1) might not be installed on the system, e.g., if MK_TOOLCHAIN == no

--- usr.sbin/makefs/tests/makefs_cd9660_tests.sh
+++ usr.sbin/makefs/tests/makefs_cd9660_tests.sh
@@ -264,6 +264,10 @@ o_flag_isolevel_3_cleanup()
 }
 
 atf_test_case o_flag_preparer
+o_flag_preparer_head()
+{
+	atf_set "require.progs" "strings"
+}
 o_flag_preparer_body()
 {
 	create_test_dirs
@@ -279,6 +283,10 @@ o_flag_preparer_body()
 }
 
 atf_test_case o_flag_publisher
+o_flag_publisher_head()
+{
+	atf_set "require.progs" "strings"
+}
 o_flag_publisher_body()
 {
 	create_test_dirs
