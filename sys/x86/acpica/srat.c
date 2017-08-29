666efbbb3616033cb79bcc5fd8e52570ba4f8240
mav <mav@FreeBSD.org>
2017-08-29 07:01:15 +0000

    MFC r322802: Fix off-by-one error when parsing SRAT table.

--- sys/x86/acpica/srat.c
+++ sys/x86/acpica/srat.c
@@ -170,7 +170,7 @@ overlaps_phys_avail(vm_paddr_t start, vm_paddr_t end)
 	int i;
 
 	for (i = 0; phys_avail[i] != 0 && phys_avail[i + 1] != 0; i += 2) {
-		if (phys_avail[i + 1] < start)
+		if (phys_avail[i + 1] <= start)
 			continue;
 		if (phys_avail[i] < end)
 			return (1);
