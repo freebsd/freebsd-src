3c7c7a96188170b4c4787515c56d434cf9fa0930
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-08-31 14:21:32 +0000

    The nvme module should explicitly declare dependency on the cam.
    If both nvme and cam are compiled as modules, nvme cannot be kldloaded
    otherwise.
    
    Reviewed by:	imp
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@323054 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/nvme/nvme.c
+++ sys/dev/nvme/nvme.c
@@ -80,6 +80,7 @@ static driver_t nvme_pci_driver = {
 
 DRIVER_MODULE(nvme, pci, nvme_pci_driver, nvme_devclass, nvme_modevent, 0);
 MODULE_VERSION(nvme, 1);
+MODULE_DEPEND(nvme, cam, 1, 1, 1);
 
 static struct _pcsid
 {
