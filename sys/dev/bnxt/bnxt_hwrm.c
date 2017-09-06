96b2e63f10093faddbdd539f159d8ae61b0d681f
Stephen Hurd <shurd@FreeBSD.org>
2017-09-06 20:14:34 +0000

    bnxt: Use correct firmware call for number of queues supported
    1) Based on the suggestion from firmware team, derive
       scctx->isc_ntxqsets_max & scctx->isc_nrxqsets_max based on FUNC_QCFG
       (instead of FUNC_QCAPS).
    2) Bump-up driver version to "1.0.0.2".
    
    Submitted by:	Bhargava Chenna Marreddy <bhargava.marreddy@broadcom.com>
    Reviewed by:	shurd, sbruno
    Approved by:	sbruno (mentor)
    Sponsored by:	Broadcom Limited
    Differential Revision:	https://reviews.freebsd.org/D12128

--- sys/dev/bnxt/bnxt_hwrm.c
+++ sys/dev/bnxt/bnxt_hwrm.c
@@ -438,6 +438,31 @@ fail:
 	return rc;
 }
 
+int 
+bnxt_hwrm_func_qcfg(struct bnxt_softc *softc)
+{
+        struct hwrm_func_qcfg_input req = {0};
+        struct hwrm_func_qcfg_output *resp =
+	    (void *)softc->hwrm_cmd_resp.idi_vaddr;
+	struct bnxt_func_qcfg *fn_qcfg = &softc->fn_qcfg;
+        int rc;
+
+	bnxt_hwrm_cmd_hdr_init(softc, &req, HWRM_FUNC_QCFG);
+        req.fid = htole16(0xffff);
+	BNXT_HWRM_LOCK(softc);
+	rc = _hwrm_send_message(softc, &req, sizeof(req));
+        if (rc)
+		goto fail;
+
+	fn_qcfg->alloc_completion_rings = le16toh(resp->alloc_cmpl_rings);
+	fn_qcfg->alloc_tx_rings = le16toh(resp->alloc_tx_rings);
+	fn_qcfg->alloc_rx_rings = le16toh(resp->alloc_rx_rings);
+	fn_qcfg->alloc_vnics = le16toh(resp->alloc_vnics);
+fail:
+	BNXT_HWRM_UNLOCK(softc);
+        return rc;
+}
+
 int
 bnxt_hwrm_func_reset(struct bnxt_softc *softc)
 {
