19fdf83cea6c7a9b922256f38027b27ebe2c029f
asomers <asomers@FreeBSD.org>
2017-09-07 15:19:10 +0000

    Fix the rootpool tests from the ZFS test suite
    Delete rootpool_003_neg, rootpool_004_pos, and rootpool_006_pos.  These
    tests verify requirements which haven't been true of ZFS for years, on
    either Illumos or FreeBSD.
    
    Reenable rootpool_002_neg.  Apparently the panic it exposed has been fixed.
    
    Sponsored by:	Spectra Logic Corp

--- head/tests/sys/cddl/zfs/tests/rootpool/rootpool_test.sh
+++ head/tests/sys/cddl/zfs/tests/rootpool/rootpool_test.sh
@@ -53,99 +53,11 @@ rootpool_002_neg_head()
 }
 rootpool_002_neg_body()
 {
-	atf_expect_fail "Destroying the root pool will panic FreeBSD BUG25145"
-	atf_fail "Prematurely fail the test so we don't cause a panic"
 	. $(atf_get_srcdir)/../../include/default.cfg
 
 	ksh93 $(atf_get_srcdir)/rootpool_002_neg.ksh || atf_fail "Testcase failed"
 }
 rootpool_002_neg_cleanup()
-{
-	. $(atf_get_srcdir)/../../include/default.cfg
-
-	ksh93 $(atf_get_srcdir)/cleanup.ksh || atf_fail "Cleanup failed"
-}
-
-
-atf_test_case rootpool_003_neg cleanup
-rootpool_003_neg_head()
-{
-	atf_set "descr" "system related filesytems can not be renamed or destroyed"
-	atf_set "require.config"  is_zfs_root
-	atf_set "require.progs"  zfs
-}
-rootpool_003_neg_body()
-{
-	atf_expect_fail "Destroying the root pool will panic FreeBSD BUG25145"
-	atf_fail "Prematurely fail the test so we don't cause a panic"
-	. $(atf_get_srcdir)/../../include/default.cfg
-
-	ksh93 $(atf_get_srcdir)/rootpool_003_neg.ksh || atf_fail "Testcase failed"
-}
-rootpool_003_neg_cleanup()
-{
-	. $(atf_get_srcdir)/../../include/default.cfg
-
-	ksh93 $(atf_get_srcdir)/cleanup.ksh || atf_fail "Cleanup failed"
-}
-
-
-atf_test_case rootpool_004_pos cleanup
-rootpool_004_pos_head()
-{
-	atf_set "descr" "rootfs's canmount property must be noauto"
-	atf_set "require.config"  is_zfs_root
-}
-rootpool_004_pos_body()
-{
-	atf_skip "The expected behavior of this test does not match the behavior of Illumos.  Illumos and FreeBSD behave identically in this regard, so the test is probably wrong"
-	. $(atf_get_srcdir)/../../include/default.cfg
-
-	ksh93 $(atf_get_srcdir)/rootpool_004_pos.ksh || atf_fail "Testcase failed"
-}
-rootpool_004_pos_cleanup()
-{
-	. $(atf_get_srcdir)/../../include/default.cfg
-
-	ksh93 $(atf_get_srcdir)/cleanup.ksh || atf_fail "Cleanup failed"
-}
-
-
-atf_test_case rootpool_005_pos cleanup
-rootpool_005_pos_head()
-{
-	atf_set "descr" "rootpool/ROOT's mountpoint must be legacy"
-	atf_set "require.config"  is_zfs_root
-}
-rootpool_005_pos_body()
-{
-	atf_skip "FreeBSD does not place any special requirements on <rootpool>/ROOT"
-	. $(atf_get_srcdir)/../../include/default.cfg
-
-	ksh93 $(atf_get_srcdir)/rootpool_005_pos.ksh || atf_fail "Testcase failed"
-}
-rootpool_005_pos_cleanup()
-{
-	. $(atf_get_srcdir)/../../include/default.cfg
-
-	ksh93 $(atf_get_srcdir)/cleanup.ksh || atf_fail "Cleanup failed"
-}
-
-
-atf_test_case rootpool_006_pos cleanup
-rootpool_006_pos_head()
-{
-	atf_set "descr" "zfs rootfs's mountpoint must be mounted and must be /"
-	atf_set "require.config"  is_zfs_root
-}
-rootpool_006_pos_body()
-{
-	atf_skip "The expected behavior of this test does not match the behavior of Illumos.  Illumos and FreeBSD behave identically in this regard, so the test is probably wrong"
-	. $(atf_get_srcdir)/../../include/default.cfg
-
-	ksh93 $(atf_get_srcdir)/rootpool_006_pos.ksh || atf_fail "Testcase failed"
-}
-rootpool_006_pos_cleanup()
 {
 	. $(atf_get_srcdir)/../../include/default.cfg
 
@@ -179,9 +91,5 @@ atf_init_test_cases()
 
 	atf_add_test_case rootpool_001_pos
 	atf_add_test_case rootpool_002_neg
-	atf_add_test_case rootpool_003_neg
-	atf_add_test_case rootpool_004_pos
-	atf_add_test_case rootpool_005_pos
-	atf_add_test_case rootpool_006_pos
 	atf_add_test_case rootpool_007_neg
 }
