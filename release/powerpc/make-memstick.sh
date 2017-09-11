e9346a94d12bef37f57e958dfafb7e2a9f1211a8
Ed Maste <emaste@FreeBSD.org>
2017-09-11 14:33:04 +0000

    boot1: remove BOOT1_MAXSIZE default value
    This Makefile relies on Makefile.fat providing the correct value for
    BOOT1_MAXSIZE and BOOT1_OFFSET. Since BOOT1_OFFSET had no default value
    here the build would already fail if Makefile.fat did not provide
    correct values.
    
    Sponsored by:	The FreeBSD Foundation

--- release/powerpc/make-memstick.sh
+++ release/powerpc/make-memstick.sh
@@ -37,7 +37,7 @@ fi
 echo '/dev/da0s3 / ufs ro,noatime 1 1' > ${1}/etc/fstab
 echo 'root_rw_mount="NO"' > ${1}/etc/rc.conf.local
 rm -f ${tempfile}
-makefs -B big ${tempfile} ${1}
+makefs -B big -o version=2 ${tempfile} ${1}
 rm ${1}/etc/fstab
 rm ${1}/etc/rc.conf.local
 
