dc683ea0edfca572c9e52a8962ed2f3af04a1cc0
Ian Lepore <ian@FreeBSD.org>
2017-09-11 02:56:45 +0000

    MFC r320743:
    Fix drivers that assume ticks starts at zero. These drivers all have logic
    similar to "if (ticks > localvar+interval) {localvar=ticks; ...}" where
    localvar is initialized to zero.  Ticks is initialized to a negative value
    since r278230, and that leads to these if statements never being true.

--- sys/dev/bwi/if_bwi.c
+++ sys/dev/bwi/if_bwi.c
@@ -381,6 +381,7 @@ bwi_attach(struct bwi_softc *sc)
 	 */
 	sc->sc_fw_version = BWI_FW_VERSION3;
 	sc->sc_led_idle = (2350 * hz) / 1000;
+	sc->sc_led_ticks = ticks - sc->sc_led_idle;
 	sc->sc_led_blink = 1;
 	sc->sc_txpwr_calib = 1;
 #ifdef BWI_DEBUG
