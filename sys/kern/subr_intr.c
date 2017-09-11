f0fedc0df1a9d2a1f90f1d04606a8869ec733306
Ian Lepore <ian@FreeBSD.org>
2017-09-11 22:32:38 +0000

    MFC r322580:
    Fix compile error with option DEBUG.  This is fallout from some long-ago
    INTRNG refactoring that didn't get caught at the time because code in a
    debugf() statement isn't compiled unless DEBUG is defined.
    
    PR:		221557

--- sys/kern/subr_intr.c
+++ sys/kern/subr_intr.c
@@ -1023,14 +1023,14 @@ intr_setup_irq(device_t dev, struct resource *res, driver_filter_t filt,
 	if (flags & INTR_SOLO) {
 		error = iscr_setup_filter(isrc, name, (intr_irq_filter_t *)filt,
 		    arg, cookiep);
-		debugf("irq %u setup filter error %d on %s\n", irq, error,
+		debugf("irq %u setup filter error %d on %s\n", isrc->isrc_irq, error,
 		    name);
 	} else
 #endif
 		{
 		error = isrc_add_handler(isrc, name, filt, hand, arg, flags,
 		    cookiep);
-		debugf("irq %u add handler error %d on %s\n", irq, error, name);
+		debugf("irq %u add handler error %d on %s\n", isrc->isrc_irq, error, name);
 	}
 	if (error != 0)
 		return (error);
