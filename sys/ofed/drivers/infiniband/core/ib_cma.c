6c898cf89354bce07394c040a9cbd40f790c86ee
hselasky <hselasky@FreeBSD.org>
2017-09-16 16:28:19 +0000

    Set length field of socket address.
    Sponsored by:	Mellanox Technologies

--- sys/ofed/drivers/infiniband/core/ib_cma.c
+++ sys/ofed/drivers/infiniband/core/ib_cma.c
@@ -1118,6 +1118,7 @@ static void cma_save_ip6_info(struct sockaddr_in6 *src_addr,
 {
 	if (src_addr) {
 		*src_addr = (struct sockaddr_in6) {
+			.sin6_len = sizeof(struct sockaddr_in6),
 			.sin6_family = AF_INET6,
 			.sin6_addr = hdr->dst_addr.ip6,
 			.sin6_port = local_port,
