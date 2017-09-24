146c82672ec580007311bfb5d6d91fd50db5c239
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-09-24 05:04:06 +0000

    Don't display empty error context.
    Context extraction didn't handle this case and showed uninitialized memory.
    
    Obtained from: OpenBSD lib.c 1.21
    Sponsored by: Netflix
    Differential Revision: https://reviews.freebsd.org/D12379
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@323965 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/one-true-awk/lib.c
+++ contrib/one-true-awk/lib.c
@@ -617,7 +617,7 @@ void eprint(void)	/* try to print context around error */
 	static int been_here = 0;
 	extern char ebuf[], *ep;
 
-	if (compile_time == 2 || compile_time == 0 || been_here++ > 0)
+	if (compile_time == 2 || compile_time == 0 || been_here++ > 0 || ebuf == ep)
 		return;
 	p = ep - 1;
 	if (p > ebuf && *p == '\n')
