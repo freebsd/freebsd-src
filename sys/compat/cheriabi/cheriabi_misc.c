22d26dada0d3ad26369602904747b1b9e4842797
Brooks Davis <brooks@one-eyed-alien.net>
2017-09-26 17:28:38 +0100

    Fix a style bug

--- sys/compat/cheriabi/cheriabi_misc.c
+++ sys/compat/cheriabi/cheriabi_misc.c
@@ -2190,7 +2190,8 @@ cheriabi_auditon(struct thread *td, struct cheriabi_auditon_args *uap)
 	return (ENOSYS);
 }
 
-int cheriabi_setlogin(struct thread *td, struct cheriabi_setlogin_args *uap)
+int
+cheriabi_setlogin(struct thread *td, struct cheriabi_setlogin_args *uap)
 {
 
 	return (kern_setlogin(td, uap->namebuf));
