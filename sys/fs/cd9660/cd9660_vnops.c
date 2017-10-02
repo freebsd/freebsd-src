eb3d4ccc484c0ea4aa896f35f73f4284ed3cd051
John Baldwin <jhb@FreeBSD.org>
2017-10-02 23:12:02 +0000

    Handle _PC_FILESIZEBITS and _PC_SYMLINK_MAX pathconf() requests in cd9660.
    cd9660 only supports symlinks with Rock Ridge extensions, so
    _PC_SYMLINK_MAX is conditional on Rock Ridge.
    
    Sponsored by:	Chelsio Communications

--- sys/fs/cd9660/cd9660_vnops.c
+++ sys/fs/cd9660/cd9660_vnops.c
@@ -783,6 +783,9 @@ cd9660_pathconf(ap)
 {
 
 	switch (ap->a_name) {
+	case _PC_FILESIZEBITS:
+		*ap->a_retval = 32;
+		return (0);
 	case _PC_LINK_MAX:
 		*ap->a_retval = 1;
 		return (0);
@@ -792,6 +795,12 @@ cd9660_pathconf(ap)
 		else
 			*ap->a_retval = 37;
 		return (0);
+	case _PC_SYMLINK_MAX:
+		if (VTOI(ap->a_vp)->i_mnt->iso_ftype == ISO_FTYPE_RRIP) {
+			*ap->a_retval = MAXPATHLEN;
+			return (0);
+		}
+		return (EINVAL);
 	case _PC_NO_TRUNC:
 		*ap->a_retval = 1;
 		return (0);
