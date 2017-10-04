595f4571aafb365e5212e47b6b61736644af7e6d
oshogbo <oshogbo@FreeBSD.org>
2017-10-04 21:05:44 +0000

    Partially revert r323866.
    Using HAVE_* is a internal tcpdump style standard.
    We want to be consistent with the standard to upstream those changes in
    the future.
    
    Requested by: glebius@

--- usr.sbin/tcpdump/tcpdump/config.h
+++ usr.sbin/tcpdump/tcpdump/config.h
@@ -18,7 +18,7 @@
 
 /* Casper library support available */
 /* See Makefile */
-/* #undef WITH_CASPER */
+/* #undef HAVE_CASPER */
 
 /* Define to 1 if you have the `cap_enter' function. */
 #define HAVE_CAP_ENTER 1
