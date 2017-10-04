141c06dff81317e758f162236202855eb3c5a156
Edward Tomasz Napierala <trasz@FreeBSD.org>
2017-10-04 12:04:35 +0000

    MFC r320803:
    Fix "mount -uw /" when the filesystem type doesn't match.
    
    This basically makes "mount -uw /" work when the filesystem
    mounted on / is NFS, but the one configured in fstab(5) is UFS,
    which can happen when you forget to modify fstab.
    
    Note that the whole special case ("else if (argv[0][0] == '/'")
    is probably not needed anyway.  I'll take a look at removing it
    altogether; for now this is a minimally intrusive fix.

--- sbin/mount/mount.c
+++ sbin/mount/mount.c
@@ -397,7 +397,9 @@ main(int argc, char *argv[])
 					have_fstab = 1;
 					mntfromname = mntbuf->f_mntfromname;
 				} else if (argv[0][0] == '/' &&
-				    argv[0][1] == '\0') {
+				    argv[0][1] == '\0' &&
+				    strcmp(fs->fs_vfstype,
+				    mntbuf->f_fstypename) == 0) {
 					fs = getfsfile("/");
 					have_fstab = 1;
 					mntfromname = fs->fs_spec;
