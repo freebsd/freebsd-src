2e1b4c6feef0ca074bfdb2f007fd08b9037212b1
Warner Losh <imp@FreeBSD.org>
2017-10-06 12:20:24 +0000

    Add efi_devpath_is_prefix
    efi_devpath_is_prefix determines if a path matches a passed-in prefix.
    
    Differential Revision: https://reviews.freebsd.org/D12564
    Submitted by: Eric McCorkle

--- sys/boot/efi/libefi/devpath.c
+++ sys/boot/efi/libefi/devpath.c
@@ -166,3 +166,32 @@ efi_devpath_match(EFI_DEVICE_PATH *devpath1, EFI_DEVICE_PATH *devpath2)
 	}
 	return (true);
 }
+
+int
+efi_devpath_is_prefix(EFI_DEVICE_PATH *prefix, EFI_DEVICE_PATH *path)
+{
+	int len;
+
+	if (prefix == NULL || path == NULL)
+		return (0);
+
+	while (1) {
+		if (IsDevicePathEnd(prefix))
+			break;
+
+		if (DevicePathType(prefix) != DevicePathType(path) ||
+		    DevicePathSubType(prefix) != DevicePathSubType(path))
+			return (0);
+
+		len = DevicePathNodeLength(prefix);
+		if (len != DevicePathNodeLength(path))
+			return (0);
+
+		if (memcmp(prefix, path, (size_t)len) != 0)
+			return (0);
+
+		prefix = NextDevicePathNode(prefix);
+		path = NextDevicePathNode(path);
+	}
+	return (1);
+}
