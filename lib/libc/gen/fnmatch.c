340f3802c2246920e5b5748da098b9883b1b89a0
Pedro F. Giffuni <pfg@FreeBSD.org>
2017-10-10 21:04:40 +0000

    MFC r322368, r322371: fnmatch(3): improve POSIX conformance.
    In a recent interpretation[1], "\\" shall return a non-zero value
    (indicating either no match or an error).
    
    The fix involves a change over r254091 and now the behavior matches the
    Sun/IBM/HP closed source implementations and also likely musl libc.
    
    Submitted by:	Joerg Schilling <joerg at schily.net>
    
    [1] http://austingroupbugs.net/view.php?id=806

--- lib/libc/gen/fnmatch.c
+++ lib/libc/gen/fnmatch.c
@@ -188,7 +188,8 @@ fnmatch1(pattern, string, stringstart, flags, patmbs, strmbs)
 			if (!(flags & FNM_NOESCAPE)) {
 				pclen = mbrtowc(&pc, pattern, MB_LEN_MAX,
 				    &patmbs);
-				if (pclen == (size_t)-1 || pclen == (size_t)-2)
+				if (pclen == 0 || pclen == (size_t)-1 ||
+				    pclen == (size_t)-2)
 					return (FNM_NOMATCH);
 				pattern += pclen;
 			}
