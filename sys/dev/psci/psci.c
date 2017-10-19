1c46f2023fbcee3bc24c2dd2652b0b3d33a95567
emaste <emaste@FreeBSD.org>
2017-10-19 16:40:17 +0000

    psci: change bootverbose string to 'PSCI 0.2 compatible'
    Prior to r324754 we treated PSCI 0.2 and 1.0 as identical, and r324754
    extended that to include all PSCI 1.x revisions. Change the string
    emitted under bootverbose to reference '0.2 compatible' to avoid
    confusion when the system includes a later PSCI rev.
    
    Discussed with:	andrew
    Sponsored by:	The FreeBSD Foundation

--- sys/dev/psci/psci.c
+++ sys/dev/psci/psci.c
@@ -495,7 +495,7 @@ psci_v0_2_init(device_t dev)
 	if ((PSCI_VER_MAJOR(version) == 0 && PSCI_VER_MINOR(version) == 2) ||
 	    PSCI_VER_MAJOR(version) == 1) {
 		if (bootverbose)
-			device_printf(dev, "PSCI version 0.2 available\n");
+			device_printf(dev, "PSCI version 0.2 compatible\n");
 
 		/*
 		 * We only register this for v0.2 since v0.1 doesn't support
