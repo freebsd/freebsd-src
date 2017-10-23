1375d29aadb4d4d52bf91e9302ebd4609d93cc77
Enji Cooper <ngie@FreeBSD.org>
2017-10-23 05:26:21 +0000

    Fix typo in previous commit
    My brain was remembering the spelling of the C++ variable, not the option.

--- share/mk/bsd.lib.mk
+++ share/mk/bsd.lib.mk
@@ -76,7 +76,7 @@ CTFFLAGS+= -g
 _WANTS_DEBUG=
 .endif
 .if ${MK_COVERAGE} != "no" && defined(_WANTS_DEBUG) && defined(SHLIB_NAME)
-_COV_FLAG= --coverage -fprofile_dir=${COVERAGEDIR}
+_COV_FLAG= --coverage -fprofile-dir=${COVERAGEDIR}
 SHARED_CFLAGS+= ${_COV_FLAG}
 SHARED_CXXFLAGS+= ${_COV_FLAG}
 .endif
