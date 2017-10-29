45275e3a26b0ae5f7099ff00a72ec641eb1f9aa6
eadler <eadler@FreeBSD.org>
2017-10-29 08:17:03 +0000

    Update several more URLs
    - Primarily http -> https
    - Primarily FreeBSD project URLs

--- release/powerpc/mkisoimages.sh
+++ release/powerpc/mkisoimages.sh
@@ -61,7 +61,7 @@ fi
 LABEL=`echo "$1" | tr '[:lower:]' '[:upper:]'`; shift
 NAME="$1"; shift
 
-publisher="The FreeBSD Project.  http://www.FreeBSD.org/"
+publisher="The FreeBSD Project.  https://www.FreeBSD.org/"
 echo "/dev/iso9660/$LABEL / cd9660 ro 0 0" > "$1/etc/fstab"
 makefs -t cd9660 $bootable -o rockridge -o label="$LABEL" -o publisher="$publisher" "$NAME" "$@"
 rm -f "$1/etc/fstab"
