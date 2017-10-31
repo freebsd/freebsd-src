d918d935ce32fae6528fd8505407af96e43d89ea
Andriy Gapon <avg@FreeBSD.org>
2017-10-31 09:58:51 +0000

    MFC r324957: iscsi_shutdown_post: do nothing if panic-ing

--- sys/dev/iscsi/iscsi.c
+++ sys/dev/iscsi/iscsi.c
@@ -2384,8 +2384,10 @@ static void
 iscsi_shutdown_post(struct iscsi_softc *sc)
 {
 
-	ISCSI_DEBUG("removing all sessions due to shutdown");
-	iscsi_terminate_sessions(sc);
+	if (panicstr == NULL) {
+		ISCSI_DEBUG("removing all sessions due to shutdown");
+		iscsi_terminate_sessions(sc);
+	}
 }
 
 static int
