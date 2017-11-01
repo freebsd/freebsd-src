842823b9d41634cc22505375db6e06b9f0ba6e79
wulf <wulf@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-11-01 22:37:43 +0000

    evdev: Do not start/stop softrepeat callout if no clients attached
    Reviewed by:		gonzo
    Approved by:		gonzo (mentor)
    MFC after:		2 weeks
    Differential Revision:	https://reviews.freebsd.org/D12676
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@325299 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/evdev/evdev.c
+++ sys/dev/evdev/evdev.c
@@ -579,7 +579,8 @@ evdev_modify_event(struct evdev_dev *evdev, uint16_t type, uint16_t code,
 				*value = KEY_EVENT_REPEAT;
 		} else {
 			/* Start/stop callout for evdev repeats */
-			if (bit_test(evdev->ev_key_states, code) == !*value) {
+			if (bit_test(evdev->ev_key_states, code) == !*value &&
+			    !LIST_EMPTY(&evdev->ev_clients)) {
 				if (*value == KEY_EVENT_DOWN)
 					evdev_start_repeat(evdev, code);
 				else
