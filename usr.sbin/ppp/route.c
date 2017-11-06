a4004cbe6488a4ddd6b1c872e1a5c6b14c021a06
Eugene Grosbein <eugen@FreeBSD.org>
2017-11-06 13:56:03 +0000

    MFC r325157,325158:
      Fix ppp(8) to add RTF_HOST flag to created host route.
    
    Reported by:	Mike Tancsa <mike@sentex.net>
    Approved by:	mav (mentor)

--- usr.sbin/ppp/route.c
+++ usr.sbin/ppp/route.c
@@ -801,7 +801,8 @@ rt_Set(struct bundle *bundle, int cmd, const struct ncprange *dst,
   if (!ncprange_ishost(dst)) {
     cp += memcpy_roundup(cp, &samask, samask.ss_len);
     rtmes.m_rtm.rtm_addrs |= RTA_NETMASK;
-  }
+  } else
+    rtmes.m_rtm.rtm_flags |= RTF_HOST;
 
   nb = cp - (char *)&rtmes;
   rtmes.m_rtm.rtm_msglen = nb;
@@ -905,7 +906,8 @@ rt_Update(struct bundle *bundle, const struct sockaddr *dst,
   if (mask) {
     rtmes.m_rtm.rtm_addrs |= RTA_NETMASK;
     p += memcpy_roundup(p, mask, mask->sa_len);
-  }
+  } else
+    rtmes.m_rtm.rtm_flags |= RTF_HOST;
 
   if (ifa && ifp && ifp->sa_family == AF_LINK) {
     rtmes.m_rtm.rtm_addrs |= RTA_IFP;
