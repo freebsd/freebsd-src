3c355d849c9773c9c1c59217da9511e500bb4e15
jeff <jeff@FreeBSD.org>
2017-11-08 02:39:37 +0000

    Replace manyinstances of VM_WAIT with blocking page allocation flags similar to the kernel memory allocator.
    This simplifies NUMA allocation because the domain will be known at wait
    time and races between failure and sleeping are eliminated.  This also
    reduces boilerplate code and simplifies callers.
    
    A wait primitive is supplied for uma zones for similar reasons.  This
    eliminates some non-specific VM_WAIT calls in favor of more explicit
    sleeps that may be satisfied without new pages.
    
    Reviewed by:	alc, kib, markj
    Tested by:	pho
    Sponsored by:	Netflix, Dell/EMC Isilon

--- sys/sparc64/sparc64/vm_machdep.c
+++ sys/sparc64/sparc64/vm_machdep.c
@@ -394,24 +394,16 @@ uma_small_alloc(uma_zone_t zone, vm_size_t bytes, u_int8_t *flags, int wait)
 {
 	vm_paddr_t pa;
 	vm_page_t m;
-	int pflags;
 	void *va;
 
 	PMAP_STATS_INC(uma_nsmall_alloc);
 
 	*flags = UMA_SLAB_PRIV;
-	pflags = malloc2vm_flags(wait) | VM_ALLOC_WIRED;
 
-	for (;;) {
-		m = vm_page_alloc(NULL, 0, pflags | VM_ALLOC_NOOBJ);
-		if (m == NULL) {
-			if (wait & M_NOWAIT)
-				return (NULL);
-			else
-				VM_WAIT;
-		} else
-			break;
-	}
+	m = vm_page_alloc(NULL, 0, 
+	    malloc2vm_flags(wait) | VM_ALLOC_WIRED | VM_ALLOC_NOOBJ);
+	if (m == NULL)
+		return (NULL);
 
 	pa = VM_PAGE_TO_PHYS(m);
 	if (dcache_color_ignore == 0 && m->md.color != DCACHE_COLOR(pa)) {
