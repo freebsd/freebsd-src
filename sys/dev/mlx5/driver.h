8fb8cd757bb3cf9fb53c66a320e751717f81463c
hselasky <hselasky@FreeBSD.org>
2017-11-10 14:02:12 +0000

    Make physical address of init segment available in the priv of mlx5 core. This change is needed by mlx5ib(4).
    Sponsored by:	Mellanox Technologies
    MFC after:	1 week

--- sys/dev/mlx5/driver.h
+++ sys/dev/mlx5/driver.h
@@ -590,6 +590,7 @@ struct mlx5_core_dev {
 	struct mlx5_port_caps	port_caps[MLX5_MAX_PORTS];
 	u32 hca_caps_cur[MLX5_CAP_NUM][MLX5_UN_SZ_DW(hca_cap_union)];
 	u32 hca_caps_max[MLX5_CAP_NUM][MLX5_UN_SZ_DW(hca_cap_union)];
+	phys_addr_t		iseg_base;
 	struct mlx5_init_seg __iomem *iseg;
 	enum mlx5_device_state	state;
 	void			(*event) (struct mlx5_core_dev *dev,
