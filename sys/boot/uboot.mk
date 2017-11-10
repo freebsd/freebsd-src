407d7d540bb6dd2568614cb81f508d0015e35e8e
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-11-10 23:54:24 +0000

    Remove LOADER_FDT_SUPPORT as a Makefile variable.
    LOADER_FDT_SUPPORT was used inconsistently in the tree. In some
    places, it was used to control whether or not the user wanted FDT
    included, and in other places it was a command to include
    support. Remove it entirely. The former is now enabled -DWITH_FDT,
    while the latter is controlled by Makefiles defining HAVE_FDT.
    
    Supported by: Netflix
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@325689 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/boot/uboot.mk
+++ sys/boot/uboot.mk
@@ -11,10 +11,8 @@ LIBUBOOT=	${BOOTOBJ}/uboot/lib/libuboot.a
 CFLAGS+=	-I${UBOOTSRC}/lib
 CFLAGS+=	-I${BOOTOBJ}/uboot/lib
 
-.if ${LOADER_FDT_SUPPORT} == "yes"
-CFLAGS+=	-I${FDTSRC}
-CFLAGS+=	-I${BOOTOBJ}/fdt
-CFLAGS+=	-DLOADER_FDT_SUPPORT
+.include "${BOOTSRC}/fdt.mk"
+
+.if ${MK_FDT} == "yes"
 LIBUBOOT_FDT=	${BOOTOBJ}/uboot/fdt/libuboot_fdt.a
-LIBFDT=		${BOOTOBJ}/fdt/libfdt.a
 .endif
