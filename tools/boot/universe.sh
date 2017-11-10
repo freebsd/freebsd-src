239164b838b623b2acd3c4cb7c3478ed8f1772be
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-11-10 23:54:41 +0000

    Replace LOADER_FIREWIRE_SUPPORT variable
    Rename LOADER_FIREWIRE_SUPPORT to MK_LOADER_FIREWIRE. Only build
    libfirewire when this is "yes". Add note to updating. Fix build script
    to build this for x86 so the option doesn't decay. sparc64 supports
    ZFS, so also build it MK_ZFS=no.
    
    Sponsored by: Netflix
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@325692 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- tools/boot/universe.sh
+++ tools/boot/universe.sh
@@ -19,9 +19,30 @@
 # Output is put into _.boot.$TARGET_ARCH.log in sys.boot.
 #
 
+dobuild()
+{
+    local ta=$1
+    local lf=$2
+    local opt=$3
+
+    echo -n "Building $ta ${opt} ... "
+    if ! make buildenv TARGET_ARCH=$ta BUILDENV_SHELL="make clean cleandepend cleandir obj depend"  \
+	 > $lf 2>&1; then
+	echo "Fail (cleanup)"
+	continue
+    fi
+    if ! make buildenv TARGET_ARCH=$ta BUILDENV_SHELL="make ${opt} -j 20 all"  \
+	 >> $lf 2>&1; then
+	echo "Fail (build)"
+	continue
+    fi
+    echo "Success"
+}
+
 top=$(make -V SRCTOP)
 cd $top/sys/boot
 
+# Default build for a goodly selection of architectures
 for i in \
 	amd64/amd64 \
 	arm/arm arm/armeb arm/armv7 \
@@ -32,34 +53,24 @@ for i in \
 	sparc64/sparc64 \
 	; do
     ta=${i##*/}
-    echo -n "Building $ta..."
-    if ! make buildenv TARGET_ARCH=$ta BUILDENV_SHELL="make clean cleandepend cleandir obj depend"  \
-	 > _.boot.${ta}.log 2>&1; then
-	echo "Fail (cleanup)"
-	continue
-    fi
-    if ! make buildenv TARGET_ARCH=$ta BUILDENV_SHELL="make -j 20 all"  \
-	 >> _.boot.${ta}.log 2>&1; then
-	echo "Fail (build)"
-	continue
-    fi
-    echo "Success"
+    dobuild $ta _.boot.${ta}.log ""
 done
+
+# Build w/o ZFS
+for i in \
+	amd64/amd64 \
+	i386/i386 \
+	sparc64/sparc64 \
+	; do
+    ta=${i##*/}
+    dobuild $ta _.boot.${ta}.noZFS.log "MK_ZFS=no"
+done
+
+# Build with firewire
 for i in \
 	amd64/amd64 \
 	i386/i386 \
 	; do
     ta=${i##*/}
-    echo -n "Building $ta MK_ZFS=no..."
-    if ! make buildenv TARGET_ARCH=$ta BUILDENV_SHELL="make clean cleandepend cleandir obj depend"  \
-	 > _.boot.${ta}.noZFS.log 2>&1; then
-	echo "Fail (cleanup)"
-	continue
-    fi
-    if ! make buildenv TARGET_ARCH=$ta BUILDENV_SHELL="make MK_ZFS=no -j 20 all"  \
-	 >> _.boot.${ta}.noZFS.log 2>&1; then
-	echo "Fail (build)"
-	continue
-    fi
-    echo "Success"
+    dobuild $ta _.boot.${ta}.firewire.log "MK_LOADER_FIREWIRE=yes"
 done
