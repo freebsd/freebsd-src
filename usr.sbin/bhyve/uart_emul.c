3d7f62e26a5e85528c06834aeb47e1446e44048c
robak <robak@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-11-11 22:50:14 +0000

    bhyve: avoid applying capsicum capabilities to file that was not opened
    When using -l option targeting file that can't be opened (ie. nmdm module
    is not loaded and /dev/nmdm* is specified) bhyve tries to apply capsicum
    capabilities to a file that was not opened.
    
    Enclose that code in an if statement and only run it on correctly opened
    descriptor also providing meaningful message in case of an error.
    
    Submitted by:	Pawel Biernacki <pawel.biernacki@gmail.com>
    Reviewed by:	grehan, emaste
    Sponsoied by:	Mysterious Code Ltd.
    Differential Revision:	D12985
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@325727 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/bhyve/uart_emul.c
+++ usr.sbin/bhyve/uart_emul.c
@@ -678,20 +678,24 @@ uart_set_backend(struct uart_softc *sc, const char *opts)
 	if (retval == 0)
 		retval = fcntl(sc->tty.fd, F_SETFL, O_NONBLOCK);
 
+	if (retval == 0) {
 #ifndef WITHOUT_CAPSICUM
-	cap_rights_init(&rights, CAP_EVENT, CAP_IOCTL, CAP_READ, CAP_WRITE);
-	if (cap_rights_limit(sc->tty.fd, &rights) == -1 && errno != ENOSYS)
-		errx(EX_OSERR, "Unable to apply rights for sandbox");
-	if (cap_ioctls_limit(sc->tty.fd, cmds, nitems(cmds)) == -1 && errno != ENOSYS)
-		errx(EX_OSERR, "Unable to apply rights for sandbox");
-	if (!uart_stdio) {
-		if (caph_limit_stdin() == -1 && errno != ENOSYS)
+		cap_rights_init(&rights, CAP_EVENT, CAP_IOCTL, CAP_READ,
+		    CAP_WRITE);
+		if (cap_rights_limit(sc->tty.fd, &rights) == -1 &&
+		    errno != ENOSYS)
 			errx(EX_OSERR, "Unable to apply rights for sandbox");
-	}
+		if (cap_ioctls_limit(sc->tty.fd, cmds, nitems(cmds)) == -1 &&
+		    errno != ENOSYS)
+			errx(EX_OSERR, "Unable to apply rights for sandbox");
+		if (!uart_stdio) {
+			if (caph_limit_stdin() == -1 && errno != ENOSYS)
+				errx(EX_OSERR,
+				    "Unable to apply rights for sandbox");
+		}
 #endif
-
-	if (retval == 0)
 		uart_opentty(sc);
+	}
 
 	return (retval);
 }
