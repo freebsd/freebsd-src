537d0fb138f94640c40c7d9b12b6e20bf35a3e23
Mateusz Guzik <mjg@FreeBSD.org>
2017-11-11 18:10:09 +0000

    Use pfind_any in linux_rt_sigqueueinfo and kern_sigqueue

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -1870,10 +1870,8 @@ kern_sigqueue(struct thread *td, pid_t pid, int signum, union sigval *value)
 	if (pid <= 0)
 		return (EINVAL);
 
-	if ((p = pfind(pid)) == NULL) {
-		if ((p = zpfind(pid)) == NULL)
-			return (ESRCH);
-	}
+	if ((p = pfind_any(pid)) == NULL)
+		return (ESRCH);
 	error = p_cansignal(td, p, signum);
 	if (error == 0 && signum != 0) {
 		ksiginfo_init(&ksi);
