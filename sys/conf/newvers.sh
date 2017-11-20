981e34b9ca651f4e50ee53ed1be87b2e877dae88
Pedro F. Giffuni <pfg@FreeBSD.org>
2017-11-20 19:56:11 +0000

    Indent protection and some other oops from the prvious commits.

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
-# SPDX-License-Identifier: BSD-3-Clause
 #
+# SPDX-License-Identifier: BSD-3-Clause
 #
 # Copyright (c) 1984, 1986, 1990, 1993
 #	The Regents of the University of California.  All rights reserved.
