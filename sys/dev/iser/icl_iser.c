73806695b1d464285d73d1eac9f294e171358425
hselasky <hselasky@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-11-23 13:57:44 +0000

    Make sure the iSCSI I/O limits are set properly so that the ISCSIDSEND IOCTL can be used prior to the ISCSIDHANDOFF IOCTL which set the negotiated values. Else the login PDU will fail when passing the "-r" option to "iscsictl" which means iSCSI over RDMA instead of TCP/IP.
    Discussed with:	np@ and trasz@
    Sponsored by:	Mellanox Technologies
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@326127 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/iser/icl_iser.c
+++ sys/dev/iser/icl_iser.c
@@ -486,7 +486,11 @@ iser_conn_task_done(struct icl_conn *ic, void *prv)
 static int
 iser_limits(struct icl_drv_limits *idl)
 {
+
 	idl->idl_max_recv_data_segment_length = 128 * 1024;
+	idl->idl_max_send_data_segment_length = 128 * 1024;
+	idl->idl_max_burst_length = 262144;
+	idl->idl_first_burst_length = 65536;
 
 	return (0);
 }
