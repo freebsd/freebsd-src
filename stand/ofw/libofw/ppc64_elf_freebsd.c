ff4917369bd11e7f33fc6c7183997cb4bf836e21
nwhitehorn <nwhitehorn@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-11-24 23:41:04 +0000

    Modify all FreeBSD bootloaders on PowerPC AIM (Book-S) systems to pass a magic number to the kernel in r7 rather than the (currently unused and irrelevant) width of the metadata pointer, which I believe was intended for a never-used approach to the 64-bit port. This enables the kernel, in a future commit, to switch on the cookie to distinguish a real metadata pointer from loader(8) from garbage left in r6 by some other boot loader.
    MFC after:	3 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@326182 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- stand/ofw/libofw/ppc64_elf_freebsd.c
+++ stand/ofw/libofw/ppc64_elf_freebsd.c
@@ -93,11 +93,11 @@ ppc64_ofw_elf_exec(struct preloaded_file *fp)
 
 	if (dtbp != 0) {
 		OF_quiesce();
-		((int (*)(u_long, u_long, u_long, void *, u_long))entry)(dtbp, 0, 0,
-		    mdp, sizeof(mdp));
+		((int (*)(u_long, u_long, u_long, void *, u_long))entry)(dtbp,
+		    0, 0, mdp, 0xfb5d104d);
 	} else {
 		OF_chain((void *)reloc, end - (char *)reloc, (void *)entry,
-		    (void *)mdp, sizeof(mdp));
+		    (void *)mdp, 0xfb5d104d);
 	}
 
 	panic("exec returned");
