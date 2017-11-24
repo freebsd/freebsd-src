32d122363fff7d821f8cbe892d15dfb36e0e95ef
Nathan Whitehorn <nwhitehorn@FreeBSD.org>
2017-11-24 15:48:17 +0000

    Switch the default firmware for npe(4) from the QOS_VLAN one to the plain-vanilla ETH microcode. The QOS_VLAN firmware added support in microcode for handling IEEE 802.1q tags, but the npe(4) driver did not actually support the relevant signalling. As a result, it was impossible to use VLANs with npe(4). Switching to the more basic microcode (same license) removes the on-NIC promisisng and makes vlan(4) work on both NPE interfaces.
    Ref: https://lists.freebsd.org/pipermail/freebsd-arm/2012-August/003826.html

--- sys/arm/xscale/ixp425/ixp425_npevar.h
+++ sys/arm/xscale/ixp425/ixp425_npevar.h
@@ -102,8 +102,8 @@
 #define	NPE_MAX		(NPE_C+1)
 
 #define	IXP425_NPE_A_IMAGEID	0x10820200
-#define	IXP425_NPE_B_IMAGEID	0x01020201
-#define	IXP425_NPE_C_IMAGEID	0x02050201
+#define	IXP425_NPE_B_IMAGEID	0x01000201
+#define	IXP425_NPE_C_IMAGEID	0x02000201
 
 struct ixpnpe_softc;
 struct ixpnpe_softc *ixpnpe_attach(device_t, int npeid);
