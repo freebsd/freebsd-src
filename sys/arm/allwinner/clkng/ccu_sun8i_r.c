dc005b055f3f13a6c55376a6adec6ea6fca3d392
kevans <kevans@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-11-25 15:14:40 +0000

    Add r-ccu support for the Allwinner a83t
    The r-ccu on the a83t differs from the others only by what it names the
    ar100 parents. Export the _CCU macros (now converted to an enu) so that
    ccu_sun8i_r can differentiate between a83t r-ccu and the others, then add
    the compat string for the a83t r-ccu.
    
    Reviewed by:	manu
    Approved by:	emaste (mentor, implicit)
    Differential Revision:	https://reviews.freebsd.org/D13206
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@326190 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/arm/allwinner/clkng/ccu_sun8i_r.c
+++ sys/arm/allwinner/clkng/ccu_sun8i_r.c
@@ -70,8 +70,16 @@ static struct aw_ccung_gate ccu_sun8i_r_gates[] = {
 };
 
 static const char *ar100_parents[] = {"osc32k", "osc24M", "pll_periph0", "iosc"};
+static const char *a83t_ar100_parents[] = {"osc16M-d512", "osc24M", "pll_periph", "osc16M"};
 PREDIV_CLK(ar100_clk, CLK_AR100,				/* id */
     "ar100", ar100_parents,					/* name, parents */
+    0x00,							/* offset */
+    16, 2,							/* mux */
+    4, 2, 0, AW_CLK_FACTOR_POWER_OF_TWO,			/* div */
+    8, 5, 0, AW_CLK_FACTOR_HAS_COND,				/* prediv */
+    16, 2, 2);							/* prediv condition */
+PREDIV_CLK(a83t_ar100_clk, CLK_AR100,				/* id */
+    "ar100", a83t_ar100_parents,				/* name, parents */
     0x00,							/* offset */
     16, 2,							/* mux */
     4, 2, 0, AW_CLK_FACTOR_POWER_OF_TWO,			/* div */
@@ -96,8 +104,12 @@ DIV_CLK(apb0_clk,
     0, 2,			/* shift, width */
     0, NULL);			/* flags, div table */
 
-static struct aw_clk_prediv_mux_def *prediv_mux_clks[] = {
+static struct aw_clk_prediv_mux_def *r_ccu_prediv_mux_clks[] = {
 	&ar100_clk,
+};
+
+static struct aw_clk_prediv_mux_def *a83t_r_ccu_prediv_mux_clks[] = {
+	&a83t_ar100_clk,
 };
 
 static struct clk_div_def *div_clks[] = {
@@ -112,11 +124,18 @@ void
 ccu_sun8i_r_register_clocks(struct aw_ccung_softc *sc)
 {
 	int i;
+	struct aw_clk_prediv_mux_def **prediv_mux_clks;
 
 	sc->resets = ccu_sun8i_r_resets;
 	sc->nresets = nitems(ccu_sun8i_r_resets);
 	sc->gates = ccu_sun8i_r_gates;
 	sc->ngates = nitems(ccu_sun8i_r_gates);
+
+	/* a83t names the parents differently than the others */
+	if (sc->type == A83T_R_CCU)
+		prediv_mux_clks = a83t_r_ccu_prediv_mux_clks;
+	else
+		prediv_mux_clks = r_ccu_prediv_mux_clks;
 
 	for (i = 0; i < nitems(prediv_mux_clks); i++)
 		aw_clk_prediv_mux_register(sc->clkdom, prediv_mux_clks[i]);
