0ea6e1218e185cc93da74626cb2eda252e154d63
markj <markj@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-11-26 19:17:55 +0000

    Move vm_phys_init_page() to vm_page.c.
    Suggested by:	kib
    Reviewed by:	alc, kib
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D13250
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@326235 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/vm/vm_phys.c
+++ sys/vm/vm_phys.c
@@ -171,7 +171,6 @@ static vm_page_t vm_phys_alloc_seg_contig(struct vm_phys_seg *seg,
     vm_paddr_t boundary);
 static void _vm_phys_create_seg(vm_paddr_t start, vm_paddr_t end, int domain);
 static void vm_phys_create_seg(vm_paddr_t start, vm_paddr_t end);
-static int vm_phys_paddr_to_segind(vm_paddr_t pa);
 static void vm_phys_split_pages(vm_page_t m, int oind, struct vm_freelist *fl,
     int order);
 
@@ -729,31 +728,6 @@ vm_phys_split_pages(vm_page_t m, int oind, struct vm_freelist *fl, int order)
 }
 
 /*
- * Initialize a physical page in preparation for adding it to the free
- * lists.
- */
-void
-vm_phys_init_page(vm_paddr_t pa)
-{
-	vm_page_t m;
-
-	m = vm_phys_paddr_to_vm_page(pa);
-	m->object = NULL;
-	m->wire_count = 0;
-	m->busy_lock = VPB_UNBUSIED;
-	m->hold_count = 0;
-	m->flags = 0;
-	m->phys_addr = pa;
-	m->queue = PQ_NONE;
-	m->psind = 0;
-	m->segind = vm_phys_paddr_to_segind(pa);
-	m->order = VM_NFREEORDER;
-	m->pool = VM_FREEPOOL_DEFAULT;
-	m->valid = m->dirty = 0;
-	pmap_page_init(m);
-}
-
-/*
  * Allocate a contiguous, power of two-sized set of physical pages
  * from the free lists.
  *
@@ -1064,7 +1038,7 @@ vm_phys_fictitious_unreg_range(vm_paddr_t start, vm_paddr_t end)
 /*
  * Find the segment containing the given physical address.
  */
-static int
+int
 vm_phys_paddr_to_segind(vm_paddr_t pa)
 {
 	struct vm_phys_seg *seg;
