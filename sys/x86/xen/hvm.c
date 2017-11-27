46a4f0152957574a03077f4ce4797e687f2e91aa
pfg <pfg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-11-27 15:11:47 +0000

    sys/x86: further adoption of SPDX licensing ID tags.
    Mainly focus on files that use BSD 2-Clause license, however the tool I
    was using misidentified many licenses so this was mostly a manual - error
    prone - task.
    
    The Software Package Data Exchange (SPDX) group provides a specification
    to make it easier for automated tools to detect and summarize well known
    opensource licenses. We are gradually adopting the specification, noting
    that the tags are considered only advisory and do not, in any way,
    superceed or replace the license texts.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@326263 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/x86/xen/hvm.c
+++ sys/x86/xen/hvm.c
@@ -1,4 +1,6 @@
-/*
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
  * Copyright (c) 2008, 2013 Citrix Systems, Inc.
  * Copyright (c) 2012 Spectra Logic Corporation
  * All rights reserved.
