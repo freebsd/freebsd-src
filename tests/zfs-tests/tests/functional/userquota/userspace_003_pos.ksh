freebsd/freebsd-src

commit 431bf10cc3af008a0b731d17d96b5b167f251935

Fix coverity defects: CID 49339, 153393
CID 49339: Type:Buffer not null terminated
CID 153393: Type:Buffer not null terminated

Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
Signed-off-by: <cao.xuewen cao.xuewen@zte.com.cn>
Closes #5296

module/zfs/zfs_vfsops.c
@@ -553,7 +553,7 @@ zfs_userspace_one(zfs_sb_t *zsb, zfs_userquota_prop_t type,
 		return (0);
 
 	if (type == ZFS_PROP_USEROBJUSED || type == ZFS_PROP_GROUPOBJUSED) {
-		strncpy(buf, DMU_OBJACCT_PREFIX, DMU_OBJACCT_PREFIX_LEN);
+		strlcpy(buf, DMU_OBJACCT_PREFIX, DMU_OBJACCT_PREFIX_LEN);
 		offset = DMU_OBJACCT_PREFIX_LEN;
 	}
 

tests/zfs-tests/cmd/xattrtest/xattrtest.c
@@ -168,6 +168,7 @@ parse_args(int argc, char **argv)
 			break;
 		case 'p':
 			strncpy(path, optarg, PATH_MAX);
+			path[PATH_MAX - 1] = '\0';
 			break;
 		case 'c':
 			synccaches = 1;
@@ -177,6 +178,7 @@ parse_args(int argc, char **argv)
 			break;
 		case 't':
 			strncpy(script, optarg, PATH_MAX);
+			script[PATH_MAX - 1] = '\0';
 			break;
 		case 'e':
 			seed = strtol(optarg, NULL, 0);

commit 99998d757c256922231ca958efbdda401ce60e3e

Fix 'zfs get {user|group}objused@' functionality
Fix a regression accidentally introduced in 431bf10 that prevents
'zfs get {user|group}objused@' from correctly reporting the requested
value.

Update "userspace_003_pos.ksh" and "groupspace_003_pos.ksh" to verify
this functionality.

Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
Reviewed-by: George Melikov <mail@gmelikov.ru>
Reviewed-by: Giuseppe Di Natale <dinatale2@llnl.gov>
Signed-off-by: loli10K <ezomori.nozomu@gmail.com>
Closes #6908

module/zfs/zfs_vfsops.c
@@ -740,7 +740,7 @@ zfs_userspace_one(zfsvfs_t *zfsvfs, zfs_userquota_prop_t type,
 		return (0);
 
 	if (type == ZFS_PROP_USEROBJUSED || type == ZFS_PROP_GROUPOBJUSED) {
-		strlcpy(buf, DMU_OBJACCT_PREFIX, DMU_OBJACCT_PREFIX_LEN);
+		strlcpy(buf, DMU_OBJACCT_PREFIX, DMU_OBJACCT_PREFIX_LEN + 1);
 		offset = DMU_OBJACCT_PREFIX_LEN;
 	}
 

tests/zfs-tests/tests/functional/userquota/groupspace_003_pos.ksh
@@ -30,6 +30,7 @@
 #
 
 . $STF_SUITE/include/libtest.shlib
+. $STF_SUITE/include/math.shlib
 . $STF_SUITE/tests/functional/userquota/userquota_common.kshlib
 
 #
@@ -56,10 +57,15 @@ function cleanup
 function group_object_count
 {
 	typeset fs=$1
-	typeset user=$2
-	typeset cnt=$(zfs groupspace -oname,objused $fs | grep $user |
-			awk '{print $2}')
-	echo $cnt
+	typeset group=$2
+	typeset -i groupspacecnt=$(zfs groupspace -oname,objused $fs |
+	    awk /$group/'{print $2}')
+	typeset -i zfsgetcnt=$(zfs get -H -ovalue groupobjused@$group $fs)
+
+	# 'zfs groupspace' and 'zfs get groupobjused@' should be equal
+	verify_eq "$groupspacecnt" "$zfsgetcnt" "groupobjused@$group"
+
+	echo $groupspacecnt
 }
 
 log_onexit cleanup

tests/zfs-tests/tests/functional/userquota/userspace_003_pos.ksh
@@ -30,6 +30,7 @@
 #
 
 . $STF_SUITE/include/libtest.shlib
+. $STF_SUITE/include/math.shlib
 . $STF_SUITE/tests/functional/userquota/userquota_common.kshlib
 
 #
@@ -58,9 +59,14 @@ function user_object_count
 {
 	typeset fs=$1
 	typeset user=$2
-	typeset cnt=$(zfs userspace -oname,objused $fs |
+	typeset -i userspacecnt=$(zfs userspace -oname,objused $fs |
 	    awk /$user/'{print $2}')
-	echo $cnt
+	typeset -i zfsgetcnt=$(zfs get -H -ovalue userobjused@$user $fs)
+
+	# 'zfs userspace' and 'zfs get userobjused@' should be equal
+	verify_eq "$userspacecnt" "$zfsgetcnt" "userobjused@$user"
+
+	echo $userspacecnt
 }
 
 log_onexit cleanup