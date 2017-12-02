693ba58ff67a9146bab22a71900b44b3bf79e7bb
Warner Losh <imp@FreeBSD.org>
2017-12-02 07:29:24 +0000

    Add -u (--to-unix) and -e (--to-efi) to convert unix or efi paths to the other.
    Sponsored by: Netflix

--- usr.sbin/efidp/efidp.c
+++ usr.sbin/efidp/efidp.c
@@ -31,6 +31,7 @@ __FBSDID("$FreeBSD$");
 #include <efivar.h>
 #include <efivar-dp.h>
 #include <err.h>
+#include <errno.h>
 #include <getopt.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -42,19 +43,21 @@ __FBSDID("$FreeBSD$");
 
 /* options descriptor */
 static struct option longopts[] = {
+	{ "to-unix",		no_argument,		NULL,	'u' },
+	{ "to-efi",		no_argument,		NULL,	'e' },
 	{ "format",		no_argument,		NULL,	'f' },
 	{ "parse",		no_argument,		NULL,	'p' },
 	{ NULL,			0,			NULL,	0 }
 };
 
 
-static int flag_format, flag_parse;
+static int flag_format, flag_parse, flag_unix, flag_efi;
 
 static void
 usage(void)
 {
 
-	errx(1, "efidp [-fp]");
+	errx(1, "efidp [-efpu]");
 }
 
 static ssize_t
@@ -86,14 +89,20 @@ parse_args(int argc, char **argv)
 {
 	int ch;
 
-	while ((ch = getopt_long(argc, argv, "fp",
+	while ((ch = getopt_long(argc, argv, "efpu",
 		    longopts, NULL)) != -1) {
 		switch (ch) {
+		case 'e':
+			flag_efi++;
+			break;
 		case 'f':
 			flag_format++;
 			break;
 		case 'p':
 			flag_parse++;
+			break;
+		case 'u':
+			flag_unix++;
 			break;
 		default:
 			usage();
@@ -105,7 +114,7 @@ parse_args(int argc, char **argv)
 	if (argc >= 1)
 		usage();
 	
-	if (flag_parse + flag_format != 1) {
+	if (flag_parse + flag_format + flag_efi + flag_unix != 1) {
 		warnx("Can only use one of -p (--parse), "
 		    "and -f (--format)");
 		usage();
@@ -125,49 +134,115 @@ trim(char *s)
 	return s;
 }
 
+static void
+unix_to_efi(void)
+{
+	char buffer[MAXSIZE];
+	char efi[MAXSIZE];
+	efidp dp;
+	char *walker;
+	int rv;
+
+	while (fgets(buffer, sizeof(buffer), stdin)) {
+		walker= trim(buffer);
+		rv = efivar_unix_path_to_device_path(walker, &dp);
+		if (rv != 0) {
+			errno = rv;
+			warn("Can't convert '%s' to efi", walker);
+			continue;
+		}
+		if (efidp_format_device_path(efi, sizeof(efi),
+		    dp, efidp_size(dp)) < 0) {
+			warnx("Can't format dp for '%s'", walker);
+			continue;
+		}
+		printf("%s\n", efi);
+	}
+}
+
+static void
+efi_to_unix(void)
+{
+	char buffer[MAXSIZE];
+	char dpbuf[MAXSIZE];
+	efidp dp;
+	size_t dplen;
+	char *walker, *dev, *relpath, *abspath;
+	int rv;
+
+	dp = (efidp)dpbuf;
+	while (fgets(buffer, sizeof(buffer), stdin)) {
+		walker= trim(buffer);
+		dplen = efidp_parse_device_path(walker, dp, sizeof(dpbuf));
+		rv = efivar_device_path_to_unix_path(dp, &dev, &relpath, &abspath);
+		if (rv == 0)
+			printf("%s:%s %s\n", dev, relpath, abspath);
+		else {
+			errno = rv;
+			warn("Can't convert '%s' to unix", walker);
+		}
+	}
+}
+
+static void
+format(void)
+{
+	char buffer[MAXSIZE];
+	ssize_t fmtlen;
+	ssize_t len;
+	void *data;
+	size_t dplen;
+	const_efidp dp;
+
+	len = read_file(STDIN_FILENO, &data);
+	if (len == -1)
+		err(1, "read");
+	dp = (const_efidp)data;
+	while (len > 0) {
+		dplen = efidp_size(dp);
+		fmtlen = efidp_format_device_path(buffer, sizeof(buffer),
+		    dp, dplen);
+		if (fmtlen > 0)
+			printf("%s\n", buffer);
+		len -= dplen;
+		dp = (const_efidp)((const char *)dp + dplen);
+	}
+	free(data);
+}
+
+static void
+parse(void)
+{
+	char buffer[MAXSIZE];
+	efidp dp;
+	ssize_t dplen;
+	char *walker;
+
+	dplen = MAXSIZE;
+	dp = malloc(dplen);
+	if (dp == NULL)
+		errx(1, "Can't allocate memory.");
+	while (fgets(buffer, sizeof(buffer), stdin)) {
+		walker= trim(buffer);
+		dplen = efidp_parse_device_path(walker, dp, dplen);
+		if (dplen == -1)
+			errx(1, "Can't parse %s", walker);
+		write(STDOUT_FILENO, dp, dplen);
+	}
+	free(dp);
+}
+
 int
 main(int argc, char **argv)
 {
-	char buffer[MAXSIZE];
 
 	parse_args(argc, argv);
-	if (flag_format) {
-		ssize_t fmtlen;
-		ssize_t len;
-		void *data;
-		size_t dplen;
-		const_efidp dp;
-
-		len = read_file(STDIN_FILENO, &data);
-		if (len == -1)
-			err(1, "read");
-		dp = (const_efidp)data;
-		while (len > 0) {
-			dplen = efidp_size(dp);
-			fmtlen = efidp_format_device_path(buffer, sizeof(buffer),
-			    dp, dplen);
-			if (fmtlen > 0)
-				printf("%s\n", buffer);
-			len -= dplen;
-			dp = (const_efidp)((const char *)dp + dplen);
-		}
-		free(data);
-	} else if (flag_parse) {
-		efidp dp;
-		ssize_t dplen;
-		char *walker;
-
-		dplen = MAXSIZE;
-		dp = malloc(dplen);
-		if (dp == NULL)
-			errx(1, "Can't allocate memory.");
-		while (fgets(buffer, sizeof(buffer), stdin)) {
-			walker= trim(buffer);
-			dplen = efidp_parse_device_path(walker, dp, dplen);
-			if (dplen == -1)
-				errx(1, "Can't parse %s", walker);
-			write(STDOUT_FILENO, dp, dplen);
-		}
-		free(dp);
-	}
+	if (flag_unix)
+		efi_to_unix();
+	else if (flag_efi)
+		unix_to_efi();
+	else if (flag_format)
+		format();
+	else if (flag_parse)
+		parse();
 }
