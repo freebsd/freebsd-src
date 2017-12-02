08e0fdd25e179ecb74236fd42d8f1841b39d4c31
pfg <pfg@FreeBSD.org>
2017-12-02 17:22:55 +0000

    SPDX: Complete license IDs for ext2fs.

--- sys/fs/ext2fs/ext2_inode_cnv.c
+++ sys/fs/ext2fs/ext2_inode_cnv.c
@@ -1,4 +1,6 @@
 /*-
+ * SPDX-License-Identifier: MIT-CMU
+ *
  * Copyright (c) 1995 The University of Utah and
  * the Computer Systems Laboratory at the University of Utah (CSL).
  * All rights reserved.
