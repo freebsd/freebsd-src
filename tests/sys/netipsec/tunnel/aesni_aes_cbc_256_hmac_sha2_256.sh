b5095f7a505cc6e897bec45e865426d6576ec48d
kp <kp@FreeBSD.org>
2017-12-03 18:35:07 +0000

    tests: ipsec: Don't load/unload aesni.ko in the test header
    We can't kldunload in the test head as Kyua interprets any output from
    them. This would lead to syntax errors and skipping the entire file.
    
    Move the kld commands into the test case bodies.
    
    Pointed out by: asomers@

--- tests/sys/netipsec/tunnel/aesni_aes_cbc_256_hmac_sha2_256.sh
+++ tests/sys/netipsec/tunnel/aesni_aes_cbc_256_hmac_sha2_256.sh
@@ -7,12 +7,13 @@ v4_head()
 {
 	atf_set descr 'IPSec inet4 tunnel using aes-cbc-256-hmac-sha2-256 and AESNI'
 	atf_set require.user root
-	# Unload AESNI module if loaded
-	kldstat -q -n aesni && kldunload aesni
 }
 
 v4_body()
 {
+	# Unload AESNI module if loaded
+	kldstat -q -n aesni && kldunload aesni
+
 	ist_test 4 rijndael-cbc "12345678901234567890123456789012" hmac-sha2-256 "12345678901234567890123456789012"
 }
 
@@ -26,12 +27,13 @@ v6_head()
 {
 	atf_set descr 'IPSec inet6 tunnel using aes-cbc-256-hmac-sha2-256 and AESNI'
 	atf_set require.user root
-	# Unload AESNI module if loaded
-	kldstat -q -n aesni && kldunload aesni
 }
 
 v6_body()
 {
+	# Unload AESNI module if loaded
+	kldstat -q -n aesni && kldunload aesni
+
 	ist_test 6 rijndael-cbc "12345678901234567890123456789012" hmac-sha2-256 "12345678901234567890123456789012"
 }
 
