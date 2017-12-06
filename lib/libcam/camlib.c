58b5d65a66e609c2a4423bd198cc225e389013a9
asomers <asomers@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-12-06 23:24:11 +0000

    Fix a null-pointer dereference and a tautological check in cam_get_device
    Reported by:	Coverity
    CID:		1017964
    MFC after:	3 weeks
    Sponsored by:	Spectra Logic Corp
    Differential Revision:	https://reviews.freebsd.org/D13184
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@326646 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libcam/camlib.c
+++ lib/libcam/camlib.c
@@ -30,6 +30,7 @@ __FBSDID("$FreeBSD$");
 
 #include <sys/types.h>
 #include <sys/param.h>
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -132,6 +133,9 @@ cam_get_device(const char *path, char *dev_name, int devnamelen, int *unit)
 	 * it so we don't hose the user's string.
 	 */
 	newpath = (char *)strdup(path);
+	if (newpath == NULL)
+		return (-1);
+
 	tmpstr = newpath;
 
 	/*
@@ -140,8 +144,9 @@ cam_get_device(const char *path, char *dev_name, int devnamelen, int *unit)
 	if (*tmpstr == '/') {
 		tmpstr2 = tmpstr;
 		tmpstr = strrchr(tmpstr2, '/');
-		if ((tmpstr != NULL) && (*tmpstr != '\0'))
-			tmpstr++;
+		/* We know that tmpstr2 contains a '/', so strrchr can't fail */
+		assert(tmpstr != NULL && *tmpstr != '\0');
+		tmpstr++;
 	}
 
 	if (*tmpstr == '\0') {
