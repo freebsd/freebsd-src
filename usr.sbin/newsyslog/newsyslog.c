6f80848e097bb58fb490d7cb2930b2e5ac1a653d
Baptiste Daroussin <bapt@FreeBSD.org>
2017-12-06 10:47:50 +0000

    Really fix typo and improve wording of the comment

--- usr.sbin/newsyslog/newsyslog.c
+++ usr.sbin/newsyslog/newsyslog.c
@@ -151,7 +151,7 @@ struct compress_types {
 	const char *flag;	/* Flag in configuration file */
 	const char *suffix;	/* Compression suffix */
 	const char *path;	/* Path to compression program */
-	char **args;		/* Compession arguments */
+	char **args;		/* Compression program arguments */
 };
 
 static char f_arg[] = "-f";
