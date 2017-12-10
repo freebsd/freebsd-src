3ee2fe0199fd4ee7e844b86ccc550ca84e5d36ba
Michal Meloun <mmel@FreeBSD.org>
2017-12-10 08:55:48 +0000

    Relax too restrictive assert.
    The problem has been reported to upstream and similar change will
    be included in next jemalloc release.
    
    Submitted by:	David Goldblatt <davidtgoldblatt@gmail.com>
    MFC after: 	2 weeks

--- contrib/jemalloc/include/jemalloc/internal/jemalloc_internal_inlines_c.h
+++ contrib/jemalloc/include/jemalloc/internal/jemalloc_internal_inlines_c.h
@@ -91,9 +91,11 @@ idalloctm(tsdn_t *tsdn, void *ptr, tcache_t *tcache, alloc_ctx_t *alloc_ctx,
 	if (config_stats && is_internal) {
 		arena_internal_sub(iaalloc(tsdn, ptr), isalloc(tsdn, ptr));
 	}
+#if 0
 	if (!is_internal && tsd_reentrancy_level_get(tsdn_tsd(tsdn)) != 0) {
 		assert(tcache == NULL);
 	}
+#endif
 	arena_dalloc(tsdn, ptr, tcache, alloc_ctx, slow_path);
 }
 
