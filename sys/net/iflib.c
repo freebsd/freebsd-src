96fc97c81fcb613a3098d5dbf944bdb6567b9e35
Stephen Hurd <shurd@FreeBSD.org>
2017-12-19 17:59:00 +0000

    Update Matthew Macy contact info
    Email address has changed, uses consistent name (Matthew, not Matt)
    
    Reported by:	Matthew Macy <mmacy@mattmacy.io>
    Differential Revision:	https://reviews.freebsd.org/D13537

--- sys/net/iflib.c
+++ sys/net/iflib.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2014-2017, Matthew Macy <mmacy@nextbsd.org>
+ * Copyright (c) 2014-2017, Matthew Macy <mmacy@mattmacy.io>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
