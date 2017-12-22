6efa5583c7ecece2f55003a217dbedcb5fb61b97
Warner Losh <imp@FreeBSD.org>
2017-12-22 20:48:49 +0000

    Fix typos from last commit, these should have been #.

--- sys/dev/ntb/ntb_hw/ntb_hw_intel.c
+++ sys/dev/ntb/ntb_hw/ntb_hw_intel.c
@@ -3119,5 +3119,5 @@ static DEFINE_CLASS_0(ntb_hw, ntb_intel_driver, ntb_intel_methods,
 DRIVER_MODULE(ntb_hw_intel, pci, ntb_intel_driver, ntb_hw_devclass, NULL, NULL);
 MODULE_DEPEND(ntb_hw_intel, ntb, 1, 1, 1);
 MODULE_VERSION(ntb_hw_intel, 1);
-MODULE_PNP_INFO("W32:vendor/device;D:@", pci, ntb_hw_intel, pci_ids,
+MODULE_PNP_INFO("W32:vendor/device;D:#", pci, ntb_hw_intel, pci_ids,
     sizeof(pci_ids[0]), nitems(pci_ids));
