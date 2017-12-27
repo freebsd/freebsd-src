9c58cafaa34372f92bc6e2294c38b1d70ff7b3b4
Stephen Hurd <shurd@FreeBSD.org>
2017-12-27 20:42:30 +0000

    Don't pass rids to taskqgroup_attach()
    As everywhere else, we want to pass rman_get_start(irq->ii_res).  This
    caused set affinity errors when not using MSI-X vectors (legacy and MSI
    interrupts).
    
    Reported by:	sbruno
    Sponsored by:	Limelight Networks

--- sys/net/iflib.c
+++ sys/net/iflib.c
@@ -5347,10 +5347,10 @@ iflib_legacy_setup(if_ctx_t ctx, driver_filter_t filter, void *filter_arg, int *
 	if ((err = _iflib_irq_alloc(ctx, irq, tqrid, iflib_fast_intr_ctx, NULL, info, name)) != 0)
 		return (err);
 	GROUPTASK_INIT(gtask, 0, fn, q);
-	taskqgroup_attach(tqg, gtask, q, tqrid, name);
+	taskqgroup_attach(tqg, gtask, q, rman_get_start(irq->ii_res), name);
 
 	GROUPTASK_INIT(&txq->ift_task, 0, _task_fn_tx, txq);
-	taskqgroup_attach(qgroup_if_io_tqg, &txq->ift_task, txq, tqrid, "tx");
+	taskqgroup_attach(qgroup_if_io_tqg, &txq->ift_task, txq, rman_get_start(irq->ii_res), "tx");
 	return (0);
 }
 
