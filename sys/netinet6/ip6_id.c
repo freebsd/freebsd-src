f4b85b0e6674821d24f236e96910cfddacd947fd
pfg <pfg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-12-28 19:42:53 +0000

    netinet6/ip6_id.c: niels kindly dropped clause 3/4 from the license.
    This file is supposed to be based on the OpenBSD CVS v1.6 but checking
    the OpenBSD repository the license had already dropped the 2&3 clasues by
    then. Catch up with the licensing.
    
    Obtained from:	OpenBSD (CVS 1.2)
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@327293 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet6/ip6_id.c
+++ sys/netinet6/ip6_id.c
@@ -1,4 +1,6 @@
 /*-
+ * SPDX-License-Identifier: (BSD-3-Clause AND BSD-2-Clause)
+ *
  * Copyright (C) 2003 WIDE Project.
  * All rights reserved.
  *
@@ -30,8 +32,6 @@
  */
 
 /*-
- * SPDX-License-Identifier: BSD-4-Clause AND BSD-3-Clause
- *
  * Copyright 1998 Niels Provos <provos@citi.umich.edu>
  * All rights reserved.
  *
@@ -48,11 +48,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Niels Provos.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
