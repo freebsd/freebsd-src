b9a4de09d0f0dd0299f682b3c2332af489e6f358
pfg <pfg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-12-28 01:12:28 +0000

    arm/ixp425: Drop 3rd and 4th clauses from Ichiro FUKUHARA's license.
    This syncs us with NetBSD as much of our changes have been upstreamed.
    
    Obtained from:	NetBSD
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@327256 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/arm/xscale/ixp425/ixp425reg.h
+++ sys/arm/xscale/ixp425/ixp425reg.h
@@ -1,6 +1,6 @@
-/*	$NetBSD: ixp425reg.h,v 1.19 2005/12/11 12:16:51 christos Exp $ */
+/*	$NetBSD: ixp425reg.h,v 1.21 2009/10/21 14:15:51 rmind Exp $ */
 /*-
- * SPDX-License-Identifier: BSD-4-Clause
+ * SPDX-License-Identifier: BSD-2-Clause-NetBSD
  *
  * Copyright (c) 2003
  *	Ichiro FUKUHARA <ichiro@ichiro.org>.
@@ -14,12 +14,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Ichiro FUKUHARA.
- * 4. The name of the company nor the name of the author may be used to
- *    endorse or promote products derived from this software without specific
- *    prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY ICHIRO FUKUHARA ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
