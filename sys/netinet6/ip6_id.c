86bab0c0b2df1724b3ef973a8a406b902e37b5a6
pfg <pfg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-12-28 20:10:10 +0000

    Revert r327293 netinet6/ip6_id.c: niels kindly dropped clause 3/4 from the license.
    I was looking at the wrong file. There is an important merge that must be
    done before I can bring this change.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@327294 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet6/ip6_id.c
+++ sys/netinet6/ip6_id.c
@@ -1,5 +1,5 @@
 /*-
- * SPDX-License-Identifier: (BSD-3-Clause AND BSD-2-Clause)
+ * SPDX-License-Identifier: (BSD-3-Clause AND BSD-4-Clause)
  *
  * Copyright (C) 2003 WIDE Project.
  * All rights reserved.
@@ -48,6 +48,11 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Niels Provos.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
