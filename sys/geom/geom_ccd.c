215db6c13d7f04360ff2b58d42f0d51087442338
pfg <pfg@FreeBSD.org>
2017-12-30 01:37:08 +0000

    geom_ccd.c: Update the license with changes from upstream.
    Part of this file originated in NetBSD, with the original file
    carrying two versions of 4-clause BSD licenses. r109471 attempted to
    simplify the situation by putting both licenses together.
    
    Meanwhile, NetBSD dropped Clauses 3 and 4 from their own license, and
    eventually NetBSD got permission from the University of Utah to drop the
    3rd clause.
    
    Keep the license "simple" by dropping the third clause since both TNF,
    Utah/Berkeley and phk agree in principle that it can be dropped.
    
    Obtained from:	NetBSD (ccd.c CVS 1.128, 1.138)

--- sys/geom/geom_ccd.c
+++ sys/geom/geom_ccd.c
@@ -1,5 +1,5 @@
 /*-
- * SPDX-License-Identifier: BSD-4-Clause
+ * SPDX-License-Identifier: BSD-3-Clause
  *
  * Copyright (c) 2003 Poul-Henning Kamp.
  * Copyright (c) 1995 Jason R. Thorpe.
@@ -20,11 +20,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed for the NetBSD Project
- *	by Jason R. Thorpe.
- * 4. The names of the authors may not be used to endorse or promote products
+ * 3. The names of the authors may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
