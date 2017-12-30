6e3fa3f70be3ee37a30517c6ae1c6d26c926a306
cy <cy@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-12-30 19:27:22 +0000

    Fix r327383:
    .../sys/dev/ep/elink.c:31:1: error: '/*' within block comment
    [-Werror,-Wcomment]
    /* $NetBSD: elink.c,v 1.6 1995/01/07 21:37:54 mycroft Exp $
    ^
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@327384 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ep/elink.c
+++ sys/dev/ep/elink.c
@@ -28,7 +28,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
-/* $NetBSD: elink.c,v 1.6 1995/01/07 21:37:54 mycroft Exp $
+ * $NetBSD: elink.c,v 1.6 1995/01/07 21:37:54 mycroft Exp $
  */
 
 #include <sys/cdefs.h>
