9fbe631a1ad7f8a1a2f45645d9c8dd50c2a8bb67
Adrian Chadd <adrian@FreeBSD.org>
2018-01-02 00:07:28 +0000

    [net80211] convert all of the WME use over to a temporary copy of WME info.
    This removes the direct WME info access in the ieee80211com struct and instead
    provides a method of fetching the data.  Right now it's a no-op but eventually
    it'll turn into a per-VAP method for drivers that support it (eg iwn, iwm,
    upcoming ath10k work) as things like p2p support require this kind of behaviour.
    
    Tested:
    
    * ath(4), STA and AP mode
    
    TODO:
    
    * yes, this is slightly stack size-y, but it is an important first step
      to get drivers migrated over to a sensible WME API.  A lot of per-phy things
      need to be converted to per-VAP before P2P, 11ac firmware, etc stuff shows up.

--- sys/dev/mwl/if_mwl.c
+++ sys/dev/mwl/if_mwl.c
@@ -2893,10 +2893,14 @@ mwl_txq_update(struct mwl_softc *sc, int ac)
 {
 #define	MWL_EXPONENT_TO_VALUE(v)	((1<<v)-1)
 	struct ieee80211com *ic = &sc->sc_ic;
+	struct chanAccParams chp;
 	struct mwl_txq *txq = sc->sc_ac2q[ac];
-	struct wmeParams *wmep = &ic->ic_wme.wme_chanParams.cap_wmeParams[ac];
+	struct wmeParams *wmep;
 	struct mwl_hal *mh = sc->sc_mh;
 	int aifs, cwmin, cwmax, txoplim;
+
+	ieee80211_wme_ic_getparams(ic, &chp);
+	wmep = &chp.cap_wmeParams[ac];
 
 	aifs = wmep->wmep_aifsn;
 	/* XXX in sta mode need to pass log values for cwmin/max */
