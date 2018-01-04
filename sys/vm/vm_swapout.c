36ca312db5272cb505e39dcd1c02bb60d1ee4cfb
Alan Cox <alc@FreeBSD.org>
2018-01-04 03:16:32 +0000

    Once we have decided to swap out a process, don't delay the laundering of its per-thread kernel stack pages by making them pass through the inactive queue first.  Instead, immediately place them in the laundry so that they might be cleaned and made available for reclamation sooner.
    Reviewed by:	kib, markj
    MFC after:	1 week

--- sys/vm/vm_swapout.c
+++ sys/vm/vm_swapout.c
@@ -546,7 +546,7 @@ vm_thread_swapout(struct thread *td)
 			panic("vm_thread_swapout: kstack already missing?");
 		vm_page_dirty(m);
 		vm_page_lock(m);
-		vm_page_unwire(m, PQ_INACTIVE);
+		vm_page_unwire(m, PQ_LAUNDRY);
 		vm_page_unlock(m);
 	}
 	VM_OBJECT_WUNLOCK(ksobj);
