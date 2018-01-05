dc8d51112c9d764b95f310040aaf44e9b16a16c7
kib <kib@FreeBSD.org>
2018-01-05 21:06:19 +0000

    Make it possible to re-evaluate cpu_features.
    Add cpuctl(4) ioctl CPUCTL_EVAL_CPU_FEATURES which forces re-read of
    cpu_features, cpu_features2, cpu_stdext_features, and
    std_stdext_features2.
    
    The intent is to allow the kernel to see the changes in the CPU
    features after micocode update.  Of course, the update is not atomic
    across variables and not synchronized with readers.  See the man page
    warning as well.
    
    Reviewed by:	imp (previous version), jilles
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D13770

--- sys/x86/x86/identcpu.c
+++ sys/x86/x86/identcpu.c
@@ -1385,9 +1385,8 @@ fix_cpuid(void)
 	return (false);
 }
 
-#ifdef __amd64__
 void
-identify_cpu(void)
+identify_cpu1(void)
 {
 	u_int regs[4];
 
@@ -1404,7 +1403,29 @@ identify_cpu(void)
 	cpu_feature = regs[3];
 	cpu_feature2 = regs[2];
 }
-#endif
+
+void
+identify_cpu2(void)
+{
+	u_int regs[4], cpu_stdext_disable;
+
+	if (cpu_high >= 7) {
+		cpuid_count(7, 0, regs);
+		cpu_stdext_feature = regs[1];
+
+		/*
+		 * Some hypervisors failed to filter out unsupported
+		 * extended features.  Allow to disable the
+		 * extensions, activation of which requires setting a
+		 * bit in CR4, and which VM monitors do not support.
+		 */
+		cpu_stdext_disable = 0;
+		TUNABLE_INT_FETCH("hw.cpu_stdext_disable", &cpu_stdext_disable);
+		cpu_stdext_feature &= ~cpu_stdext_disable;
+
+		cpu_stdext_feature2 = regs[2];
+	}
+}
 
 /*
  * Final stage of CPU identification.
@@ -1412,7 +1433,7 @@ identify_cpu(void)
 void
 finishidentcpu(void)
 {
-	u_int regs[4], cpu_stdext_disable;
+	u_int regs[4];
 #ifdef __i386__
 	u_char ccr3;
 #endif
@@ -1431,22 +1452,7 @@ finishidentcpu(void)
 		cpu_mon_max_size = regs[1] &  CPUID5_MON_MAX_SIZE;
 	}
 
-	if (cpu_high >= 7) {
-		cpuid_count(7, 0, regs);
-		cpu_stdext_feature = regs[1];
-
-		/*
-		 * Some hypervisors failed to filter out unsupported
-		 * extended features.  Allow to disable the
-		 * extensions, activation of which requires setting a
-		 * bit in CR4, and which VM monitors do not support.
-		 */
-		cpu_stdext_disable = 0;
-		TUNABLE_INT_FETCH("hw.cpu_stdext_disable", &cpu_stdext_disable);
-		cpu_stdext_feature &= ~cpu_stdext_disable;
-
-		cpu_stdext_feature2 = regs[2];
-	}
+	identify_cpu2();
 
 #ifdef __i386__
 	if (cpu_high > 0 &&
