20d1366a4f60a67901c308af4175c5f697bbd658
cem <cem@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-01-10 21:49:45 +0000

    mallocarray(9): panic if the requested allocation would overflow
    Additionally, move the overflow check logic out to WOULD_OVERFLOW() for
    consumers to have a common means of testing for overflowing allocations.
    WOULD_OVERFLOW() should be a secondary check -- on 64-bit platforms, just
    because an allocation won't overflow size_t does not mean it is a sane size
    to request.  Callers should be imposing reasonable allocation limits far,
    far, below overflow.
    
    Discussed with:	emaste, jhb, kp
    Sponsored by:	Dell EMC Isilon
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@327796 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/malloc.h
+++ sys/sys/malloc.h
@@ -41,6 +41,7 @@
 #include <sys/queue.h>
 #include <sys/_lock.h>
 #include <sys/_mutex.h>
+#include <machine/_limits.h>
 
 #define	MINALLOCSIZE	UMA_SMALLEST_UNIT
 
@@ -192,6 +193,20 @@ void	*reallocf(void *addr, unsigned long size, struct malloc_type *type,
 	    int flags) __result_use_check __alloc_size(2);
 
 struct malloc_type *malloc_desc2type(const char *desc);
+
+/*
+ * This is sqrt(SIZE_MAX+1), as s1*s2 <= SIZE_MAX
+ * if both s1 < MUL_NO_OVERFLOW and s2 < MUL_NO_OVERFLOW
+ */
+#define MUL_NO_OVERFLOW		(1UL << (sizeof(size_t) * 8 / 2))
+static inline bool
+WOULD_OVERFLOW(size_t nmemb, size_t size)
+{
+
+	return ((nmemb >= MUL_NO_OVERFLOW || size >= MUL_NO_OVERFLOW) &&
+	    nmemb > 0 && __SIZE_T_MAX / nmemb < size);
+}
+#undef MUL_NO_OVERFLOW
 #endif /* _KERNEL */
 
 #endif /* !_SYS_MALLOC_H_ */
