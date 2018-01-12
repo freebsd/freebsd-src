ab448bbbe19d263befb32d3f43171f29e0a43c5b
pfg <pfg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-01-12 23:12:30 +0000

    libalias: small memory allocation cleanups.
    Make the calloc wrappers behave as expected by using mallocarray.
    It is rather weird that the malloc wrappers also zeroes the memory: update
    a comment to reflect at least two cases where it is expected.
    
    Reviewed by:	tuexen
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@327898 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/libalias/alias_mod.h
+++ sys/netinet/libalias/alias_mod.h
@@ -42,7 +42,7 @@ MALLOC_DECLARE(M_ALIAS);
 /* Use kernel allocator. */
 #if defined(_SYS_MALLOC_H_)
 #define	malloc(x)	malloc(x, M_ALIAS, M_NOWAIT|M_ZERO)
-#define	calloc(x, n)	malloc(x*n)
+#define	calloc(n, x)	mallocarray((n), (x), M_ALIAS, M_NOWAIT|M_ZERO)
 #define	free(x)		free(x, M_ALIAS)
 #endif
 #endif
