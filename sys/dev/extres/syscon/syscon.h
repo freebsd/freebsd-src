b8fd48fae6b55e038f4bc914429d1a931d690e0d
kevans <kevans@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-01-13 19:00:41 +0000

    Add SPDX tags to syscon bits, correct inconsistency in Copyright line.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@327938 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/extres/syscon/syscon.h
+++ sys/dev/extres/syscon/syscon.h
@@ -1,5 +1,7 @@
 /*-
- * Copyright 2017 Kyle Evans <kevans@FreeBSD.org>
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2017 Kyle Evans <kevans@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
