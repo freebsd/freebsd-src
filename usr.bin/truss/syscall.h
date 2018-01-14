00659877e0c9efc03157581d23553767da43d6b7
tuexen <tuexen@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-01-14 14:33:22 +0000

    Improve support for sctp_generic_recvmsg() and sctp_generic_sendmsg() and add support for sctp_generic_sendmsg_iov().
    Handle the struct iovec argument and the struct sctp_sndrcvinfo
    arguments.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@327967 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/truss/syscall.h
+++ usr.bin/truss/syscall.h
@@ -82,7 +82,7 @@ enum Argtype { None = 1, Hex, Octal, Int, UInt, LongHex, Name, Ptr, Stat, Stat11
 	Sockoptname, Msgflags, CapRights, PUInt, PQuadHex, Acltype,
 	Extattrnamespace, Minherit, Mlockall, Mountflags, Msync, Priowhich,
 	Ptraceop, Quotactlcmd, Reboothowto, Rtpriofunc, Schedpolicy, Schedparam,
-	PSig, Siginfo, Kevent11, Iovec,
+	PSig, Siginfo, Kevent11, Iovec, Sctpsndrcvinfo,
 
 	CloudABIAdvice, CloudABIClockID, ClouduABIFDSFlags,
 	CloudABIFDStat, CloudABIFileStat, CloudABIFileType,
