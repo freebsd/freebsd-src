3b0a4e40a08c8c3d0f6862648b3ac0126d2d15e4
Pedro F. Giffuni <pfg@FreeBSD.org>
2018-01-15 21:19:21 +0000

    netgraph: make some use of mallocarray(9).
    Focus on code where we are doing multiplications within malloc(9). None of
    these ire likely to overflow, however the change is still useful as some
    static checkers can benefit from the allocation attributes we use for
    mallocarray.
    
    This initial sweep only covers malloc(9) calls with M_NOWAIT. No good
    reason but I started doing the changes before r327796 and at that time it
    was convenient to make sure the sorrounding code could handle NULL values.
    
    X-Differential revision: https://reviews.freebsd.org/D13837

--- sys/netgraph/ng_deflate.c
+++ sys/netgraph/ng_deflate.c
@@ -427,7 +427,7 @@ static void *
 z_alloc(void *notused, u_int items, u_int size)
 {
 
-	return (malloc(items * size, M_NETGRAPH_DEFLATE, M_NOWAIT));
+	return (mallocarray(items, size, M_NETGRAPH_DEFLATE, M_NOWAIT));
 }
 
 static void
