520a8ac0deaefdbcd3c35f8dcf3b037bc88f513c
pfg <pfg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-01-16 23:19:57 +0000

    SPDX: finish tagging sys/cam.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@328070 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/cam/mmc/mmc.h
+++ sys/cam/mmc/mmc.h
@@ -1,4 +1,6 @@
 /*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
  * Copyright (c) 2014-2016 Ilya Bakulin.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
