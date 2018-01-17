dc16abe4d992ff07242994bb1c0b945aa4d7ffdb
mckusick <mckusick@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-01-17 17:58:24 +0000

    Correct fsck journal-recovery code to update a cylinder-group check-hash after making changes to the cylinder group. The problem was that the journal-recovery code was calling the libufs bwrite() function instead of the cgput() function. The cgput() function updates the cylinder-group check-hash before writing the cylinder group.
    This change required the additions of the cgget() and cgput() functions
    to the libufs API to avoid a gratuitous bcopy of every cylinder group
    to be read or written. These new functions have been added to the
    libufs manual pages. This was the first opportunity that I have had
    to use and document the use of the EDOOFUS error code.
    
    Reviewed by: kib
    Reported by: emaste and others
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@328092 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/fsck_ffs/suj.c
+++ sbin/fsck_ffs/suj.c
@@ -1892,8 +1892,7 @@ cg_write(struct suj_cg *sc)
 	 * before writing the block.
 	 */
 	fs->fs_cs(fs, sc->sc_cgx) = cgp->cg_cs;
-	if (bwrite(disk, fsbtodb(fs, cgtod(fs, sc->sc_cgx)), sc->sc_cgbuf,
-	    fs->fs_bsize) == -1)
+	if (cgput(disk, cgp) == -1)
 		err_suj("Unable to write cylinder group %d\n", sc->sc_cgx);
 }
 
