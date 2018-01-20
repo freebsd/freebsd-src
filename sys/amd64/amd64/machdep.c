50a53194f63cdc9d0c6478a9409f09d0e723361a
Roger Pau Monné <royger@FreeBSD.org>
2018-01-20 14:59:37 +0000

    xen: fix IDT setup after PTI
    On amd64 the IDT handler was not set correctly when using PTI.
    
    While there also fix the selectors to SEL_KPL.
    
    Obtained from:	kib
    MFC with:	r328083

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -1667,7 +1667,8 @@ hammer_time(u_int64_t modulep, u_int64_t physfree)
 	    &IDTVEC(dtrace_ret), SDT_SYSIGT, SEL_UPL, 0);
 #endif
 #ifdef XENHVM
-	setidt(IDT_EVTCHN, &IDTVEC(xen_intr_upcall), SDT_SYSIGT, SEL_UPL, 0);
+	setidt(IDT_EVTCHN, pti ? &IDTVEC(xen_intr_upcall_pti) :
+	    &IDTVEC(xen_intr_upcall), SDT_SYSIGT, SEL_KPL, 0);
 #endif
 	r_idt.rd_limit = sizeof(idt0) - 1;
 	r_idt.rd_base = (long) idt;
