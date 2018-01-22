d0fd40e7c98d56b2327af13ed197bcd51b514c12
Alexander Motin <mav@FreeBSD.org>
2018-01-22 05:59:48 +0000

    8972 zfs holds: In scripted mode, do not pad columns with spaces
    illumos/illumos-gate@e9b7d6e7f7a6477679a35b73eb3934b096b3dd39
    
    https://www.illumos.org/issues/8972:
    'zfs holds -H' does not properly output content in scripted mode. It uses a
    tab instead of two spaces, but it still pads column widths with spaces when
    it should not.
    
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Approved by: Dan McDonald <danmcd@joyent.com>
    Author: Allan Jude <allanjude@freebsd.org>

--- cmd/zfs/zfs_main.c
+++ cmd/zfs/zfs_main.c
@@ -5569,8 +5569,6 @@ print_holds(boolean_t scripted, size_t nwidth, size_t tagwidth, nvlist_t *nvl)
 			uint64_t val = 0;
 			time_t time;
 			struct tm t;
-			char sep = scripted ? '\t' : ' ';
-			size_t sepnum = scripted ? 1 : 2;
 
 			(void) nvpair_value_uint64(nvp2, &val);
 			time = (time_t)val;
@@ -5578,8 +5576,13 @@ print_holds(boolean_t scripted, size_t nwidth, size_t tagwidth, nvlist_t *nvl)
 			(void) strftime(tsbuf, DATETIME_BUF_LEN,
 			    gettext(STRFTIME_FMT_STR), &t);
 
-			(void) printf("%-*s%*c%-*s%*c%s\n", nwidth, zname,
-			    sepnum, sep, tagwidth, tagname, sepnum, sep, tsbuf);
+			if (scripted) {
+				(void) printf("%s\t%s\t%s\n", zname,
+				    tagname, tsbuf);
+			} else {
+				(void) printf("%-*s  %-*s  %s\n", nwidth,
+				    zname, tagwidth, tagname, tsbuf);
+			}
 		}
 	}
 }
