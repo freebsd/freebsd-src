5e8dfe0a527c7bacc0b5750271559e596f08aed0
phk <phk@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-01-22 07:15:24 +0000

    Forgot to edit copy&pasted copyright blurb.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@328258 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/arm/broadcom/bcm2835/bcm2835_clkman.c
+++ sys/arm/broadcom/bcm2835/bcm2835_clkman.c
@@ -1,6 +1,7 @@
 /*-
- * Copyright (C) 2013-2015 Daisuke Aoyama <aoyama@peach.ne.jp>
- * All rights reserved.
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2017 Poul-Henning Kamp <phk@FreeBSD.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
--- sys/arm/broadcom/bcm2835/bcm2835_pwm.c
+++ sys/arm/broadcom/bcm2835/bcm2835_pwm.c
@@ -1,7 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
- * Copyright (c) 2017 Poul-Henning Kamp <phk@freebsd.org>
+ * Copyright (c) 2017 Poul-Henning Kamp <phk@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
