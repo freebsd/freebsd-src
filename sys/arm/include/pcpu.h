a36b6ec0f960b50337107e0d7555dc430e6aa08b
Michal Meloun <mmel@FreeBSD.org>
2018-01-27 11:19:41 +0000

    Implement mitigation for Spectre version 2 attacks on ARMv7.
    Similarly as we already do for arm64, for mitigation is necessary to
    flush branch predictor when we:
    - do task switch
    - receive prefetch abort on non-userspace address
    
    The user can disable this mitigation by setting 'machdep.disable_bp_hardening'
    sysctl variable, or it can check actual system status by reading
    'machdep.spectre_v2_safe'
    
    The situation is complicated by fact that:
    - for Cortex-A8, the BPIALL instruction is effectively NOP until the IBE bit
      in ACTLR is set.
    - for Cortex-A15, the BPIALL is always NOP. The branch predictor can be
      only flushed by doing ICIALLU with special bit (Enable invalidates  of BTB)
      set in ACTLR.
    
    Since access to the ACTLR register is locked to secure monitor/firmware on
    most boards, they will also need update of firmware / U-boot.
    In worst case, when secure monitor is on-chip ROM (e.g. PandaBoard),
    the board is unfixable.
    
    MFC after:	2 weeks
    Reviewed by:	imp, emaste
    Differential Revision:	https://reviews.freebsd.org/D13931

--- sys/arm/include/pcpu.h
+++ sys/arm/include/pcpu.h
@@ -44,6 +44,10 @@ struct vmspace;
 #endif	/* _KERNEL */
 
 #if __ARM_ARCH >= 6
+/* Branch predictor hardening method */
+#define PCPU_BP_HARDEN_KIND_NONE		0
+#define PCPU_BP_HARDEN_KIND_BPIALL		1
+#define PCPU_BP_HARDEN_KIND_ICIALLU		2
 
 #define PCPU_MD_FIELDS							\
 	unsigned int pc_vfpsid;						\
@@ -59,7 +63,9 @@ struct vmspace;
 	void *pc_qmap_pte2p;						\
 	unsigned int pc_dbreg[32];					\
 	int pc_dbreg_cmd;						\
-	char __pad[155]
+	int pc_bp_harden_kind;						\
+	uint32_t pc_original_actlr;					\
+	char __pad[147]
 #else
 #define PCPU_MD_FIELDS							\
 	char __pad[93]
