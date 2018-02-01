ef01ed01d824f2fedfc7517b00ba9bfd5229311d
andrew <andrew@FreeBSD.org>
2018-02-01 14:26:26 +0000

    Only promote userspace mappings to superpages. This was dropped in r328510, however due to the break-before-make requirement on arm64 is is currently unsafe to promote kernel pages.
    Sponsored by:	DARPA, AFRL

--- sys/arm64/arm64/pmap.c
+++ sys/arm64/arm64/pmap.c
@@ -3076,12 +3076,13 @@ validate:
 	}
 
 #if VM_NRESERVLEVEL > 0
-		if ((mpte == NULL || mpte->wire_count == NL3PG) &&
-		    pmap_superpages_enabled() &&
-		    (m->flags & PG_FICTITIOUS) == 0 &&
-		    vm_reserv_level_iffullpop(m) == 0) {
-			pmap_promote_l2(pmap, pde, va, &lock);
-		}
+	if (pmap != pmap_kernel() &&
+	    (mpte == NULL || mpte->wire_count == NL3PG) &&
+	    pmap_superpages_enabled() &&
+	    (m->flags & PG_FICTITIOUS) == 0 &&
+	    vm_reserv_level_iffullpop(m) == 0) {
+		pmap_promote_l2(pmap, pde, va, &lock);
+	}
 #endif
 
 	if (lock != NULL)
