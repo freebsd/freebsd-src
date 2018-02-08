77514a3b115a6e64eca9102a658f0ccf395c08e9
imp <imp@FreeBSD.org>
2018-02-08 17:07:27 +0000

    Move to tabs for indentation and to 8-space notches, per style(9).
    4 space indentation with a mix of tabs and spaces is a hassle. Update
    to project-standard hard-tabs with 8-space indentation in these files.
    This matches the new code coming in better as well.

--- stand/common/interp_parse.c
+++ stand/common/interp_parse.c
@@ -50,143 +50,146 @@ static char		*args[MAXARGS];
  */
 
 #define PARSE_FAIL(expr) \
-if (expr) { \
-    printf("fail at line %d\n", __LINE__); \
-    clean(); \
-    free(copy); \
-    free(buf); \
-    return 1; \
-}
+	if (expr) { \
+		printf("fail at line %d\n", __LINE__); \
+		clean(); \
+		free(copy); \
+		free(buf); \
+		return 1; \
+	}
 
 /* Accept the usual delimiters for a variable, returning counterpart */
 static char
 isdelim(int ch)
 {
-    if (ch == '{')
-	return '}';
-    else if (ch == '(')
-	return ')';
-    return '\0';
+
+	if (ch == '{')
+		return '}';
+	else if (ch == '(')
+		return ')';
+	return '\0';
 }
 
 static int
 isquote(int ch)
 {
-    return (ch == '\'');
+
+	return (ch == '\'');
 }
 
 static int
 isdquote(int ch)
 {
-    return (ch == '"');
+
+	return (ch == '"');
 }
 
 int
 parse(int *argc, char ***argv, const char *str)
 {
-    int ac;
-    char *val, *p, *q, *copy = NULL;
-    size_t i = 0;
-    char token, tmp, quote, dquote, *buf;
-    enum { STR, VAR, WHITE } state;
+	int ac;
+	char *val, *p, *q, *copy = NULL;
+	size_t i = 0;
+	char token, tmp, quote, dquote, *buf;
+	enum { STR, VAR, WHITE } state;
 
-    ac = *argc = 0;
-    dquote = quote = 0;
-    if (!str || (p = copy = backslash(str)) == NULL)
-	return 1;
+	ac = *argc = 0;
+	dquote = quote = 0;
+	if (!str || (p = copy = backslash(str)) == NULL)
+		return 1;
 
-    /* Initialize vector and state */
-    clean();
-    state = STR;
-    buf = (char *)malloc(PARSE_BUFSIZE);
-    token = 0;
+	/* Initialize vector and state */
+	clean();
+	state = STR;
+	buf = (char *)malloc(PARSE_BUFSIZE);
+	token = 0;
 
-    /* And awaaaaaaaaay we go! */
-    while (*p) {
-	switch (state) {
-	case STR:
-	    if ((*p == '\\') && p[1]) {
-		p++;
-		PARSE_FAIL(i == (PARSE_BUFSIZE - 1));
-		buf[i++] = *p++;
-	    } else if (isquote(*p)) {
-		quote = quote ? 0 : *p;
-		if (dquote) { /* keep quote */
-			PARSE_FAIL(i == (PARSE_BUFSIZE - 1));
-			buf[i++] = *p++;
-		} else
-			++p;
-	    } else if (isdquote(*p)) {
-		dquote = dquote ? 0 : *p;
-		if (quote) { /* keep dquote */
-			PARSE_FAIL(i == (PARSE_BUFSIZE - 1));
-			buf[i++] = *p++;
-		} else
-			++p;
-	    } else if (isspace(*p) && !quote && !dquote) {
-		state = WHITE;
-		if (i) {
-		    buf[i] = '\0';
-		    PARSE_FAIL(insert(&ac, buf));
-		    i = 0;
+	/* And awaaaaaaaaay we go! */
+	while (*p) {
+		switch (state) {
+		case STR:
+			if ((*p == '\\') && p[1]) {
+				p++;
+				PARSE_FAIL(i == (PARSE_BUFSIZE - 1));
+				buf[i++] = *p++;
+			} else if (isquote(*p)) {
+				quote = quote ? 0 : *p;
+				if (dquote) { /* keep quote */
+					PARSE_FAIL(i == (PARSE_BUFSIZE - 1));
+					buf[i++] = *p++;
+				} else
+					++p;
+			} else if (isdquote(*p)) {
+				dquote = dquote ? 0 : *p;
+				if (quote) { /* keep dquote */
+					PARSE_FAIL(i == (PARSE_BUFSIZE - 1));
+					buf[i++] = *p++;
+				} else
+					++p;
+			} else if (isspace(*p) && !quote && !dquote) {
+				state = WHITE;
+				if (i) {
+					buf[i] = '\0';
+					PARSE_FAIL(insert(&ac, buf));
+					i = 0;
+				}
+				++p;
+			} else if (*p == '$' && !quote) {
+				token = isdelim(*(p + 1));
+				if (token)
+					p += 2;
+				else
+					++p;
+				state = VAR;
+			} else {
+				PARSE_FAIL(i == (PARSE_BUFSIZE - 1));
+				buf[i++] = *p++;
+			}
+			break;
+
+		case WHITE:
+			if (isspace(*p))
+				++p;
+			else
+				state = STR;
+			break;
+
+		case VAR:
+			if (token) {
+				PARSE_FAIL((q = strchr(p, token)) == NULL);
+			} else {
+				q = p;
+				while (*q && !isspace(*q))
+					++q;
+			}
+			tmp = *q;
+			*q = '\0';
+			if ((val = variable_lookup(p)) != NULL) {
+				size_t len = strlen(val);
+
+				strncpy(buf + i, val, PARSE_BUFSIZE - (i + 1));
+				i += min(len, PARSE_BUFSIZE - 1);
+			}
+			*q = tmp;	/* restore value */
+			p = q + (token ? 1 : 0);
+			state = STR;
+			break;
 		}
-		++p;
-	    } else if (*p == '$' && !quote) {
-		token = isdelim(*(p + 1));
-		if (token)
-		    p += 2;
-		else
-		    ++p;
-		state = VAR;
-	    } else {
-		PARSE_FAIL(i == (PARSE_BUFSIZE - 1));
-		buf[i++] = *p++;
-	    }
-	    break;
-
-	case WHITE:
-	    if (isspace(*p))
-		++p;
-	    else
-		state = STR;
-	    break;
-
-	case VAR:
-	    if (token) {
-		PARSE_FAIL((q = strchr(p, token)) == NULL);
-	    } else {
-		q = p;
-		while (*q && !isspace(*q))
-		    ++q;
-	    }
-	    tmp = *q;
-	    *q = '\0';
-	    if ((val = variable_lookup(p)) != NULL) {
-		size_t len = strlen(val);
-
-		strncpy(buf + i, val, PARSE_BUFSIZE - (i + 1));
-		i += min(len, PARSE_BUFSIZE - 1);
-	    }
-	    *q = tmp;	/* restore value */
-	    p = q + (token ? 1 : 0);
-	    state = STR;
-	    break;
 	}
-    }
-    /* missing terminating ' or " */
-    PARSE_FAIL(quote || dquote);
-    /* If at end of token, add it */
-    if (i && state == STR) {
-	buf[i] = '\0';
-	PARSE_FAIL(insert(&ac, buf));
-    }
-    args[ac] = NULL;
-    *argc = ac;
-    *argv = (char **)malloc((sizeof(char *) * ac + 1));
-    bcopy(args, *argv, sizeof(char *) * ac + 1);
-    free(buf);
-    free(copy);
-    return 0;
+	/* missing terminating ' or " */
+	PARSE_FAIL(quote || dquote);
+	/* If at end of token, add it */
+	if (i && state == STR) {
+		buf[i] = '\0';
+		PARSE_FAIL(insert(&ac, buf));
+	}
+	args[ac] = NULL;
+	*argc = ac;
+	*argv = (char **)malloc((sizeof(char *) * ac + 1));
+	bcopy(args, *argv, sizeof(char *) * ac + 1);
+	free(buf);
+	free(copy);
+	return 0;
 }
 
 #define MAXARGS	20
@@ -195,28 +198,30 @@ parse(int *argc, char ***argv, const char *str)
 static void
 clean(void)
 {
-    int		i;
+	int		i;
 
-    for (i = 0; i < MAXARGS; i++) {
-	if (args[i] != NULL) {
-	    free(args[i]);
-	    args[i] = NULL;
+	for (i = 0; i < MAXARGS; i++) {
+		if (args[i] != NULL) {
+			free(args[i]);
+			args[i] = NULL;
+		}
 	}
-    }
 }
 
 static int
 insert(int *argcp, char *buf)
 {
-    if (*argcp >= MAXARGS)
-	return 1;
-    args[(*argcp)++] = strdup(buf);
-    return 0;
+
+	if (*argcp >= MAXARGS)
+		return 1;
+	args[(*argcp)++] = strdup(buf);
+	return 0;
 }
 
 static char *
 variable_lookup(char *name)
 {
-    /* XXX search "special variable" space first? */
-    return (char *)getenv(name);
+
+	/* XXX search "special variable" space first? */
+	return (char *)getenv(name);
 }
