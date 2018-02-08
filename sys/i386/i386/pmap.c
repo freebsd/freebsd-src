7bc81b6db11ed4d602eb59655876fffe84a98803
markj <markj@FreeBSD.org>
2018-02-08 19:28:51 +0000

    Use vm_page_unwire_noq() instead of directly modifying page wire counts.
    No functional change intended.
    
    Reviewed by:	alc, kib (previous revision)
    MFC after:	2 weeks
    Differential Revision:	https://reviews.freebsd.org/D14266

--- sys/i386/i386/pmap.c
+++ sys/i386/i386/pmap.c
@@ -2057,10 +2057,9 @@ pmap_release(pmap_t pmap)
 		KASSERT(VM_PAGE_TO_PHYS(m) == (pmap->pm_pdpt[i] & PG_FRAME),
 		    ("pmap_release: got wrong ptd page"));
 #endif
-		m->wire_count--;
+		vm_page_unwire_noq(m);
 		vm_page_free_zero(m);
 	}
-	atomic_subtract_int(&vm_cnt.v_wire_count, NPGPTD);
 }
 
 static int
