39090a2546d32c3dfc5d1b4a69dde6caec9a5f01
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-02-11 17:45:38 +0000

    Consistent macro indentation is the hobgoblin of small minds
    Line up the macro definitions and names here like the machine/stdarg.h
    files that this replaced. This is easier to read and also makes it
    easier to match up with other includes. Also two space indent va_end
    to match the rest of the surrounding if block.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@329127 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/_stdarg.h
+++ sys/sys/_stdarg.h
@@ -43,13 +43,13 @@
 #endif
 
 #ifdef __GNUCLIKE_BUILTIN_STDARG
-  #define va_start(ap, last) __builtin_va_start((ap), (last))
-  #define va_arg(ap, type) __builtin_va_arg((ap), type)
-  #define __va_copy(dest, src) __builtin_va_copy((dest), (src))
+  #define	va_start(ap, last)	__builtin_va_start((ap), (last))
+  #define	va_arg(ap, type)	__builtin_va_arg((ap), type)
+  #define	__va_copy(dest, src)	__builtin_va_copy((dest), (src))
   #if __ISO_C_VISIBLE >= 1999
-    #define va_copy(dest, src) __va_copy(dest, src)
+    #define	va_copy(dest, src)	__va_copy(dest, src)
   #endif
-#define va_end(ap) __builtin_va_end(ap)
+  #define	va_end(ap)		__builtin_va_end(ap)
 #endif
 
 #if defined(lint) && !defined(va_start)
