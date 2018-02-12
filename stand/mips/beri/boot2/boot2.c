fa7d3c1c4ff1edf7f6979a384acccc18648254ea
kevans <kevans@FreeBSD.org>
2018-02-12 18:30:20 +0000

    MFC r327705,r328447: Fix BERI bootloader build
    r327705: Use <stand.h> instead of <inttypes.h> and <stdio.h> in boot code.
    r328447: BERI isn't BTX, so we don't have to provide exit().

--- stand/mips/beri/boot2/boot2.c
+++ stand/mips/beri/boot2/boot2.c
@@ -139,7 +139,6 @@ static int comspeed = SIOSPD;
 struct bootinfo bootinfo;
 static uint8_t ioctrl = IO_KEYBOARD;
 
-void exit(int);
 void putchar(int);
 static void boot_fromdram(void);
 static void boot_fromfs(void);
@@ -277,12 +276,6 @@ main(u_int argc, const char *argv[], const char *envv[], uint64_t memsize)
 	else
 	    load();
     }
-}
-
-/* XXX - Needed for btxld to link the boot2 binary; do not remove. */
-void
-exit(int x)
-{
 }
 
 static void
