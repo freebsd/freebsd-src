ad9219f687854fec502d68222080914ccb889e51
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-02-14 00:25:18 +0000

    Do not call pmap_enter() with invalid protection mode.
    If the map entry elookup was performed due to the mapping changes, we
    need to ensure that there is still some access permission bit
    requested which is compatible with the current vm_map_entry mode.  If
    not, restart the handler from scratch instead of trying to save the
    current progress.
    
    Also adjust fault_type to not include cleared permission bits.
    
    Reviewed by:	markj
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D14347
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@329252 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/vm/vm_fault.c
+++ sys/vm/vm_fault.c
@@ -1219,6 +1219,12 @@ readrest:
 			 * write-enabled after all.
 			 */
 			prot &= retry_prot;
+			fault_type &= retry_prot;
+			if (prot == 0) {
+				release_page(&fs);
+				unlock_and_deallocate(&fs);
+				goto RetryFault;
+			}
 		}
 	}
 
