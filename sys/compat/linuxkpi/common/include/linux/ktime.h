f139d86bbb72b9432995e9fc15e87f56d3323850
hselasky <hselasky@FreeBSD.org>
2018-02-14 09:53:23 +0000

    MFC r328971: Fix implementation of ktime_add_ns() and ktime_sub_ns() in the LinuxKPI to actually return the computed result instead of the input value.
    This is a regression issue after r289572.
    
    Found by:	gcc6
    Sponsored by:	Mellanox Technologies

--- sys/compat/linuxkpi/common/include/linux/ktime.h
+++ sys/compat/linuxkpi/common/include/linux/ktime.h
@@ -88,18 +88,14 @@ ktime_to_timeval(ktime_t kt)
 static inline ktime_t
 ktime_add_ns(ktime_t kt, int64_t ns)
 {
-	ktime_t res;
-
-	res.tv64 = kt.tv64 + ns;
+	kt.tv64 += ns;
 	return kt;
 }
 
 static inline ktime_t
 ktime_sub_ns(ktime_t kt, int64_t ns)
 {
-	ktime_t res;
-
-	res.tv64 = kt.tv64 - ns;
+	kt.tv64 -= ns;
 	return kt;
 }
 
