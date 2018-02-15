75cee0089ec693a16fe8c82d87c9040519a87770
Hans Petter Selasky <hselasky@FreeBSD.org>
2018-02-15 09:07:37 +0000

    MFC r325805: Set the default VNET in krping before calling ifunit_ref(). Else using IPv6 link-local addresses when VIMAGE is enabled will cause a so-called NULL pointer dereferencing issue.
    Sponsored by:	Mellanox Technologies

--- sys/contrib/rdma/krping/krping.c
+++ sys/contrib/rdma/krping/krping.c
@@ -3185,7 +3185,9 @@ krping_get_ipv6_scope_id(char *name)
 
 	if (name == NULL)
 		return (0);
+	CURVNET_SET_QUIET(TD_TO_VNET(curthread));
 	ifp = ifunit_ref(name);
+	CURVNET_RESTORE();
 	if (ifp == NULL)
 		return (0);
 	retval = ifp->if_index;
