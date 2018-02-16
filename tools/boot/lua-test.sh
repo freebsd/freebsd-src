52fc075b5e857eb60ee0103732909e2be6bf26ce
kevans <kevans@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-02-16 20:23:48 +0000

    Default to /tmp/loadertest for lua test scripts
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@329406 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- tools/boot/lua-test.sh
+++ tools/boot/lua-test.sh
@@ -12,7 +12,7 @@ obj=$(make -V .OBJDIR)
 t=$obj/userboot/test/test
 u=$obj/userboot/userboot/userboot.so
 
-[ -n "$dir" ] || die "No directory specified"
+[ -n "$dir" ] || dir=/tmp/loadertest
 [ -d "$dir" ] || die "Directory $dir doesn't exist"
 [ -f "$dir/boot/lua/loader.lua" ] || die "No boot/lua/loader.lua found"
 [ -f "$dir/boot/kernel/kernel" ] || die "No kernel to load"
