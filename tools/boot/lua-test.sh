45e0bb8494354840b526a5ce7788a0bf4a49122c
kevans <kevans@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-02-16 20:26:18 +0000

    lua-test: Image the loader test directory if it doesn't exist yet
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@329407 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- tools/boot/lua-test.sh
+++ tools/boot/lua-test.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 # $FreeBSD$
 
+# Will image the test directory (default /tmp/loadertest) if it doesn't exist
+
 die() {
     echo $*
     exit 1
 }
 
 dir=$1
+scriptdir=$(dirname $(realpath $0))
 cd $(make -V SRCTOP)/stand
 obj=$(make -V .OBJDIR)
 t=$obj/userboot/test/test
 u=$obj/userboot/userboot/userboot.so
 
 [ -n "$dir" ] || dir=/tmp/loadertest
-[ -d "$dir" ] || die "Directory $dir doesn't exist"
+[ -d "$dir" ] || ${scriptdir}/lua-img.sh ${dir}
 [ -f "$dir/boot/lua/loader.lua" ] || die "No boot/lua/loader.lua found"
 [ -f "$dir/boot/kernel/kernel" ] || die "No kernel to load"
 [ -x "$t" ] || die "no userboot test jig found ($t)"
