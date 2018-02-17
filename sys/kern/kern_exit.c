fc7474d6fb79f0d29f3302daa11e62272e6e5feb
mjg <mjg@FreeBSD.org>
2018-02-17 08:48:45 +0000

    exit: get rid of PROC_SLOCK when checking a process to report
    All accessed fields are protected with already held process lock.

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -1228,15 +1228,11 @@ loop_locked:
 		nfound++;
 		PROC_LOCK_ASSERT(p, MA_OWNED);
 
-		if ((options & (WTRAPPED | WUNTRACED)) != 0)
-			PROC_SLOCK(p);
-
 		if ((options & WTRAPPED) != 0 &&
 		    (p->p_flag & P_TRACED) != 0 &&
 		    (p->p_flag & (P_STOPPED_TRACE | P_STOPPED_SIG)) != 0 &&
 		    p->p_suspcount == p->p_numthreads &&
 		    (p->p_flag & P_WAITED) == 0) {
-			PROC_SUNLOCK(p);
 			CTR4(KTR_PTRACE,
 			    "wait: returning trapped pid %d status %#x "
 			    "(xstat %d) xthread %d",
@@ -1251,13 +1247,10 @@ loop_locked:
 		    (p->p_flag & P_STOPPED_SIG) != 0 &&
 		    p->p_suspcount == p->p_numthreads &&
 		    (p->p_flag & P_WAITED) == 0) {
-			PROC_SUNLOCK(p);
 			report_alive_proc(td, p, siginfo, status, options,
 			    CLD_STOPPED);
 			return (0);
 		}
-		if ((options & (WTRAPPED | WUNTRACED)) != 0)
-			PROC_SUNLOCK(p);
 		if ((options & WCONTINUED) != 0 &&
 		    (p->p_flag & P_CONTINUED) != 0) {
 			report_alive_proc(td, p, siginfo, status, options,
