5e2d748931989533a93263f0e883ab94ccd14c22
Ian Lepore <ian@FreeBSD.org>
2018-02-18 23:01:33 +0000

    Add the MODULE_DEPEND()s needed so that the kernel linker can resolve all the symbols at load time when iicbus is not compiled into the kernel.

--- sys/arm/freescale/imx/imx_i2c.c
+++ sys/arm/freescale/imx/imx_i2c.c
@@ -192,6 +192,8 @@ static devclass_t  i2c_devclass;
 
 DRIVER_MODULE(imx_i2c, simplebus, i2c_driver, i2c_devclass, 0, 0);
 DRIVER_MODULE(ofw_iicbus, imx_i2c, ofw_iicbus_driver, ofw_iicbus_devclass, 0, 0);
+MODULE_DEPEND(imx_i2c, iicbus, 1, 1, 1);
+MODULE_DEPEND(imx_i2c, ofw_iicbus, 1, 1, 1);
 
 static phandle_t
 i2c_get_node(device_t bus, device_t dev)
