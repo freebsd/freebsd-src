b7c6719f0969fbd3c52ed46511a37e6dd8f9e172
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-02-19 19:08:25 +0000

    Do not free(9) uninitialized pointer.
    Reported and tested by:	allanjude
    Reviewed by:	markj
    Sponsored by:	The FreeBSD Foundation
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@329600 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/ufs/ffs/ffs_subr.c
+++ sys/ufs/ffs/ffs_subr.c
@@ -174,12 +174,17 @@ ffs_sbget(void *devfd, struct fs **fsp, off_t altsuperblock,
 
 	*fsp = NULL;
 	if (altsuperblock != -1) {
-		if ((ret = readsuper(devfd, fsp, altsuperblock, readfunc)) != 0)
+		ret = readsuper(devfd, fsp, altsuperblock, readfunc);
+		if (*fsp != NULL)
+			(*fsp)->fs_csp = NULL;
+		if (ret != 0)
 			return (ret);
 	} else {
 		for (i = 0; sblock_try[i] != -1; i++) {
-			if ((ret = readsuper(devfd, fsp, sblock_try[i],
-			     readfunc)) == 0)
+			ret = readsuper(devfd, fsp, sblock_try[i], readfunc);
+			if (*fsp != NULL)
+				(*fsp)->fs_csp = NULL;
+			if (ret == 0)
 				break;
 			if (ret == ENOENT)
 				continue;
@@ -188,17 +193,17 @@ ffs_sbget(void *devfd, struct fs **fsp, off_t altsuperblock,
 		if (sblock_try[i] == -1)
 			return (ENOENT);
 	}
+
 	/*
-	 * If not filling in summary information, NULL out fs_csp and return.
+	 * Not filling in summary information, return.
 	 */
-	fs = *fsp;
-	if (filltype == NULL) {
-		fs->fs_csp = NULL;
+	if (filltype == NULL)
 		return (0);
-	}
+
 	/*
 	 * Read in the superblock summary information.
 	 */
+	fs = *fsp;
 	size = fs->fs_cssize;
 	blks = howmany(size, fs->fs_fsize);
 	if (fs->fs_contigsumsize > 0)
