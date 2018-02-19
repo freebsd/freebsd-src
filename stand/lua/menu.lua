2e29209102438c2e66baac36cd074cffebc727f3
kevans <kevans@FreeBSD.org>
2018-02-19 18:11:35 +0000

    stand/lua: Add copyright notice in places
    I've made some not-insignificant changes to config, menu, and password bits
    of our lua scripts. Add a copyright notice to them to reflect.

--- stand/lua/menu.lua
+++ stand/lua/menu.lua
@@ -1,5 +1,6 @@
 --
 -- Copyright (c) 2015 Pedro Souza <pedrosouza@freebsd.org>
+-- Copyright (C) 2018 Kyle Evans <kevans@FreeBSD.org>
 -- All rights reserved.
 --
 -- Redistribution and use in source and binary forms, with or without
