c6df5de312396b14903ff73e6dac453b4d20c0d6
kevans <kevans@FreeBSD.org>
2018-02-20 05:11:24 +0000

    stand/lua: Stick a copyright notice on drawer.lua

--- stand/lua/drawer.lua
+++ stand/lua/drawer.lua
@@ -1,5 +1,6 @@
 --
 -- Copyright (c) 2015 Pedro Souza <pedrosouza@freebsd.org>
+-- Copyright (c) 2018 Kyle Evans <kevans@FreeBSD.org>
 -- All rights reserved.
 --
 -- Redistribution and use in source and binary forms, with or without
