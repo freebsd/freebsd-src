31982e5a423cb469bf1994d89d677d53447aa8c4
kevans <kevans@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-02-23 02:51:35 +0000

    Add SPDX tags to lua files
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@329851 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- stand/lua/config.lua
+++ stand/lua/config.lua
@@ -1,4 +1,6 @@
 --
+-- SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+--
 -- Copyright (c) 2015 Pedro Souza <pedrosouza@freebsd.org>
 -- Copyright (C) 2018 Kyle Evans <kevans@FreeBSD.org>
 -- All rights reserved.
