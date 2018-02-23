0d688befe378e232b49870c2b110fba4b6bbc7fb
asomers <asomers@FreeBSD.org>
2018-02-23 03:11:43 +0000

    libifconfig: multiple feature additions
    Added the ability to:
    
    * Create virtual interfaces
    * Create vlan interfaces
    * Get interface fib
    * Get interface groups
    * Get interface status
    * Get nd6 info
    * Get media status
    * Get additional ifaddr info in a convenient struct
    * Get vhids
    * Get carp info
    * Get lagg and laggport status
    * Iterate over all interfaces and ifaddrs
    
    And add more examples, too.
    
    Note that this is a backwards-incompatible change. But that's ok, because it's
    a private library.
    
    MFC after:	3 weeks
    Sponsored by:	Spectra Logic Corp
    Differential Revision:	https://reviews.freebsd.org/D14463

--- share/examples/libifconfig/ifchangevlan.c
+++ share/examples/libifconfig/ifchangevlan.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017, Marie Helene Kvello-Aune
+ * Copyright (c) 2017, Marie Helene Kvello-Aune
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -26,35 +26,42 @@
  * $FreeBSD$
  */
 
-#include <arpa/inet.h>
-#include <net/if.h>
-
 #include <err.h>
 #include <errno.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <libifconfig.h>
 
+#include <net/if_vlan_var.h>
 
 int
 main(int argc, char *argv[])
 {
-	char *ifname, *ifdescr, *curdescr;
+	char *ifname, *parentif;
+	unsigned short vlantag;
+	const char *errstr;
 	ifconfig_handle_t *lifh;
 
-	if (argc != 3) {
+	if (argc != 4) {
 		errx(EINVAL, "Invalid number of arguments."
-		    " First argument should be interface name, second argument"
-		    " should be the description to set.");
+		    " Should provide exactly three arguments: "
+		    "INTERFACE, PARENT_INTERFACE and VLAN_TAG.");
 	}
 
 	/* We have a static number of arguments. Therefore we can do it simple. */
 	ifname = strdup(argv[1]);
-	ifdescr = strdup(argv[2]);
-	curdescr = NULL;
+	parentif = strdup(argv[2]);
+	vlantag = strtonum(argv[3], 0, USHRT_MAX, &errstr);
 
-	printf("Interface name: %s\n", ifname);
+	if (errstr != NULL) {
+		errx(1, "VLAN_TAG must be between 0 and %i.\n", USHRT_MAX);
+	}
+
+	printf("Interface: %s\nNew VLAN tag: %i\n", ifname, vlantag);
 
 	lifh = ifconfig_open();
 	if (lifh == NULL) {
@@ -62,37 +69,41 @@ main(int argc, char *argv[])
 		return (-1);
 	}
 
-	if (ifconfig_get_description(lifh, ifname, &curdescr) == 0) {
-		printf("Old description: %s\n", curdescr);
+	if (ifconfig_set_vlantag(lifh, ifname, parentif, vlantag) == 0) {
+		printf("Successfully changed vlan tag.\n");
+		ifconfig_close(lifh);
+		lifh = NULL;
+		free(ifname);
+		free(parentif);
+		return (0);
 	}
 
-	printf("New description: %s\n\n", ifdescr);
-
-	if (ifconfig_set_description(lifh, ifname, ifdescr) == 0) {
-		printf("New description successfully set.\n");
-	} else {
-		switch (ifconfig_err_errtype(lifh)) {
-		case SOCKET:
-			err(ifconfig_err_errno(lifh), "Socket error");
-			break;
-		case IOCTL:
-			err(ifconfig_err_errno(
-				    lifh), "IOCTL(%lu) error",
-			    ifconfig_err_ioctlreq(lifh));
-			break;
-		default:
-			err(ifconfig_err_errno(lifh), "Other error");
-			break;
+	switch (ifconfig_err_errtype(lifh)) {
+	case SOCKET:
+		warnx("couldn't create socket. This shouldn't happen.\n");
+		break;
+	case IOCTL:
+		if (ifconfig_err_ioctlreq(lifh) == SIOCGETVLAN) {
+			warnx("Target interface isn't a VLAN interface.\n");
 		}
+		if (ifconfig_err_ioctlreq(lifh) == SIOCSETVLAN) {
+			warnx(
+				"Couldn't change VLAN properties of interface.\n");
+		}
+		break;
+	default:
+		warnx(
+			"This is a thorough example accommodating for temporary"
+			" 'not implemented yet' errors. That's likely what happened"
+			" now. If not, your guess is as good as mine. ;)"
+			" Error code: %d\n", ifconfig_err_errno(
+				lifh));
+		break;
 	}
-
-	free(ifname);
-	free(ifdescr);
-	free(curdescr);
-	ifname = NULL;
-	ifdescr = NULL;
-	curdescr = NULL;
 
 	ifconfig_close(lifh);
-	return (0);
+	lifh = NULL;
+	free(ifname);
+	free(parentif);
+	return (-1);
 }
