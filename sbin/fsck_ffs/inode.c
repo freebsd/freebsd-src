2f5d320887e49d704b97b33eae476d34c6ebbe79
Kirk McKusick <mckusick@FreeBSD.org>
2018-02-23 23:07:41 +0000

    MFC of 329749.
    Fix a read past the end of a buffer in fsck.

--- sbin/fsck_ffs/inode.c
+++ sbin/fsck_ffs/inode.c
@@ -451,8 +451,10 @@ cacheino(union dinode *dp, ino_t inumber)
 
 	if (howmany(DIP(dp, di_size), sblock.fs_bsize) > NDADDR)
 		blks = NDADDR + NIADDR;
-	else
+	else if (DIP(dp, di_size) > 0)
 		blks = howmany(DIP(dp, di_size), sblock.fs_bsize);
+	else
+		blks = 1;
 	inp = (struct inoinfo *)
 		Malloc(sizeof(*inp) + (blks - 1) * sizeof(ufs2_daddr_t));
 	if (inp == NULL)
