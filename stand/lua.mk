f735e1eb15d21bda71b5985d15edacddf0fabdae
imp <imp@FreeBSD.org>
2018-02-23 04:04:25 +0000

    Do not include float interfaces when using libsa.
    We don't support float in the boot loaders, so don't include
    interfaces for float or double in systems headers. In addition, take
    the unusual step of spiking double and float to prevent any more
    accidental seepage.

--- stand/lua.mk
+++ stand/lua.mk
@@ -3,5 +3,4 @@
 # Common flags to build lua related files
 
 CFLAGS+=	-I${LUASRC} -I${LDRSRC} -I${LIBLUASRC}
-# CFLAGS+=	-Ddouble=jagged-little-pill -Dfloat=poison-shake -D__OMIT_FLOAT
 CFLAGS+=	-DLUA_FLOAT_TYPE=LUA_FLOAT_INT64
