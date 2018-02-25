e57a5b4d46c32b52725b3772b72e13c6822e1bc2
hselasky <hselasky@FreeBSD.org>
2018-02-25 10:47:33 +0000

    MFC r329472: Allow the put_user() function macro to put constant values by using the existing __put_user() macro.
    Submitted by:	Johannes Lundberg <johalun0@gmail.com>
    Sponsored by:	Mellanox Technologies

--- sys/compat/linuxkpi/common/include/linux/uaccess.h
+++ sys/compat/linuxkpi/common/include/linux/uaccess.h
@@ -58,7 +58,7 @@
 	linux_copyout(&(__x), (_p), sizeof(*(_p)));	\
 })
 #define	get_user(_x, _p)	linux_copyin((_p), &(_x), sizeof(*(_p)))
-#define	put_user(_x, _p)	linux_copyout(&(_x), (_p), sizeof(*(_p)))
+#define	put_user(_x, _p)	__put_user(_x, _p)
 #define	clear_user(...)		linux_clear_user(__VA_ARGS__)
 #define	access_ok(...)		linux_access_ok(__VA_ARGS__)
 
