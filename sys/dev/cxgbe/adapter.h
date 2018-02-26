1389427a51197586b684acd808ee2fa7ff8dc721
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-02-26 22:12:31 +0000

    Move ccr_aes_getdeckey() from ccr(4) to the cxgbe(4) driver.
    This routine will also be used by the TOE module to manage TLS keys.
    
    Sponsored by:	Chelsio Communications
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@330041 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/cxgbe/adapter.h
+++ sys/dev/cxgbe/adapter.h
@@ -1138,6 +1138,7 @@ void t4_os_link_changed(struct port_info *);
 void t4_iterate(void (*)(struct adapter *, void *), void *);
 void t4_init_devnames(struct adapter *);
 void t4_add_adapter(struct adapter *);
+void t4_aes_getdeckey(void *, const void *, unsigned int);
 int t4_detach_common(device_t);
 int t4_filter_rpl(struct sge_iq *, const struct rss_header *, struct mbuf *);
 int t4_map_bars_0_and_4(struct adapter *);
