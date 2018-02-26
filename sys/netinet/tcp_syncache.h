18a753093817abc6b2dc93894b7222e1cb9adbe2
Patrick Kelsey <pkelsey@FreeBSD.org>
2018-02-26 03:03:41 +0000

    Greatly reduce the number of #ifdefs supporting the TCP_RFC7413 kernel option.
    The conditional compilation support is now centralized in
    tcp_fastopen.h and tcp_var.h. This doesn't provide the minimum
    theoretical code/data footprint when TCP_RFC7413 is disabled, but
    nearly all the TFO code should wind up being removed by the optimizer,
    the additional footprint in the syncache entries is a single pointer,
    and the additional overhead in the tcpcb is at the end of the
    structure.
    
    This enables the TCP_RFC7413 kernel option by default in amd64 and
    arm64 GENERIC.
    
    Reviewed by:	hiren
    MFC after:	1 month
    Sponsored by:	Limelight Networks
    Differential Revision:	https://reviews.freebsd.org/D14048

--- sys/netinet/tcp_syncache.h
+++ sys/netinet/tcp_syncache.h
@@ -75,9 +75,7 @@ struct syncache {
 #endif
 	struct label	*sc_label;		/* MAC label reference */
 	struct ucred	*sc_cred;		/* cred cache for jail checks */
-#ifdef TCP_RFC7413
 	void		*sc_tfo_cookie;		/* for TCP Fast Open response */
-#endif
 	void		*sc_pspare;		/* TCP_SIGNATURE */
 	u_int32_t	sc_spare[2];		/* UTO */
 };
