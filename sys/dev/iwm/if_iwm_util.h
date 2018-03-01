a0c76a2cd92257beed4bae3b940005daeebccd0a
eadler <eadler@FreeBSD.org>
2018-03-01 06:34:48 +0000

    MFC r318012:
    [iwm] Allow listening on both chains/atennas to get diversity.
    
    This might improve throughput slightly when far from the accesspoint,
    apparently by allowing the firmware to listen on either of the two
    antennas (if there are two, i.e. on 7260/7265/8260), whichever has
    a better reception.

--- sys/dev/iwm/if_iwm_util.h
+++ sys/dev/iwm/if_iwm_util.h
@@ -120,6 +120,8 @@ extern	int iwm_dma_contig_alloc(bus_dma_tag_t tag, struct iwm_dma_info *dma,
 				 bus_size_t size, bus_size_t alignment);
 extern	void iwm_dma_contig_free(struct iwm_dma_info *);
 
+extern	boolean_t iwm_mvm_rx_diversity_allowed(struct iwm_softc *sc);
+
 extern	uint8_t iwm_ridx2rate(struct ieee80211_rateset *rs, int ridx);
 
 static inline uint8_t
