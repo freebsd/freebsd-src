bd62df10595a8573bb79271b3a3b7dd983545ba2
brooks <brooks@FreeBSD.org>
2018-03-02 22:13:29 +0000

    Don't define struct mymsg.
    PR:		224299, 224443 (exp-run)
    Reviewed by:	kib, jhb
    Exp-run by:	antoine
    Sponsored by:	DARPA, AFRL
    Differential Revision:	https://reviews.freebsd.org/D14491

--- sys/sys/msg.h
+++ sys/sys/msg.h
@@ -101,23 +101,6 @@ struct msqid_ds {
 	time_t	msg_ctime;	/* time of last msgctl() */
 };
 
-#if __BSD_VISIBLE
-/*
- * Structure describing a message.  The SVID doesn't suggest any
- * particular name for this structure.  There is a reference in the
- * msgop man page that reads "The structure mymsg is an example of what
- * this user defined buffer might look like, and includes the following
- * members:".  This sentence is followed by two lines equivalent
- * to the mtype and mtext field declarations below.  It isn't clear
- * if "mymsg" refers to the name of the structure type or the name of an
- * instance of the structure...
- */
-struct mymsg {
-	long	mtype;		/* message type (+ve integer) */
-	char	mtext[1];	/* message body */
-};
-#endif
-
 #ifdef _KERNEL
 struct msg {
 	struct	msg *msg_next;  /* next msg in the chain */
