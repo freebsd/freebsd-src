3133fc1d3216b4977e1ea5754b11db1f8f8aaf05
Konstantin Belousov <kib@FreeBSD.org>
2018-03-03 11:53:44 +0000

    MFC r329905: Hide all vm/vm_pageout.h content under #ifdef _KERNEL.

--- sys/vm/vm_pageout.h
+++ sys/vm/vm_pageout.h
@@ -63,6 +63,8 @@
 #ifndef _VM_VM_PAGEOUT_H_
 #define _VM_VM_PAGEOUT_H_
 
+#ifdef _KERNEL
+
 /*
  *	Header file for pageout daemon.
  */
@@ -101,11 +103,10 @@ void pagedaemon_wakeup(void);
 void vm_wait(void);
 void vm_waitpfault(void);
 
-#ifdef _KERNEL
 int vm_pageout_flush(vm_page_t *, int, int, int, int *, boolean_t *);
 void vm_pageout_oom(int shortage);
 
 void vm_swapout_run(void);
 void vm_swapout_run_idle(void);
-#endif
+#endif /* _KERNEL */
 #endif	/* _VM_VM_PAGEOUT_H_ */
