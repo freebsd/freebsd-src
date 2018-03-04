3c7c22a9aac87f6163e45ac6fbeb42d30417bc03
hselasky <hselasky@FreeBSD.org>
2018-03-04 18:10:18 +0000

    Implement for_each_clear_bit() function macro in the LinuxKPI.
    MFC after:	1 week
    Submitted by:	Johannes Lundberg <johalun0@gmail.com>
    Sponsored by:	Mellanox Technologies
    Sponsored by:	Limelight Networks

--- sys/compat/linuxkpi/common/include/linux/bitops.h
+++ sys/compat/linuxkpi/common/include/linux/bitops.h
@@ -390,6 +390,10 @@ done:
 	     (bit) < (size);					\
 	     (bit) = find_next_bit((addr), (size), (bit) + 1))
 
+#define	for_each_clear_bit(bit, addr, size) \
+	for ((bit) = find_first_zero_bit((addr), (size));		\
+	     (bit) < (size);						\
+	     (bit) = find_next_zero_bit((addr), (size), (bit) + 1))
 
 static inline uint64_t
 sign_extend64(uint64_t value, int index)
