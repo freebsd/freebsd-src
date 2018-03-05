c0cf2b3dbc4c6d66a402594fe8ead5ddbeef876b
Eitan Adler <eadler@FreeBSD.org>
2018-03-05 08:45:39 +0000

    MFC r306449:
    Remove an extra etter.

--- sys/contrib/ipfilter/netinet/fil.c
+++ sys/contrib/ipfilter/netinet/fil.c
@@ -421,7 +421,7 @@ static ipftuneable_t ipf_main_tuneables[] = {
 
 
 /*
- * The next section of code is a a collection of small routines that set
+ * The next section of code is a collection of small routines that set
  * fields in the fr_info_t structure passed based on properties of the
  * current packet.  There are different routines for the same protocol
  * for each of IPv4 and IPv6.  Adding a new protocol, for which there
