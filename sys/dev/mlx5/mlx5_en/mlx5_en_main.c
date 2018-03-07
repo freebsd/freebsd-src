4b6de86da906a5f71944773fd56281be3968c1f6
hselasky <hselasky@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-03-07 14:51:50 +0000

    Add log message for unsupported QSFPs in mlx5core.
    Submitted by:	Matthew Finlay <matt@mellanox.com>
    MFC after:	1 week
    Sponsored by:	Mellanox Technologies
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@330604 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/mlx5/mlx5_en/mlx5_en_main.c
+++ sys/dev/mlx5/mlx5_en/mlx5_en_main.c
@@ -2639,6 +2639,7 @@ mlx5e_ioctl(struct ifnet *ifp, u_long command, caddr_t data)
 	int error = 0;
 	int mask = 0;
 	int size_read = 0;
+	int module_status;
 	int module_num;
 	int max_mtu;
 	uint8_t read_addr;
@@ -2838,8 +2839,9 @@ out:
 			goto err_i2c;
 		}
 		/* Check if module is present before doing an access */
-		if (mlx5_query_module_status(priv->mdev, module_num) !=
-		    MLX5_MODULE_STATUS_PLUGGED) {
+		module_status = mlx5_query_module_status(priv->mdev, module_num);
+		if (module_status != MLX5_MODULE_STATUS_PLUGGED_ENABLED &&
+		    module_status != MLX5_MODULE_STATUS_PLUGGED_DISABLED) {
 			error = EINVAL;
 			goto err_i2c;
 		}
