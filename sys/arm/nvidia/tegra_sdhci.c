89e928429d978f0eca28ecde69cd246641293025
manu <manu@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-03-08 22:49:36 +0000

    Fix build when option MMCCAM is defined.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@330673 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/arm/nvidia/tegra_sdhci.c
+++ sys/arm/nvidia/tegra_sdhci.c
@@ -62,6 +62,8 @@ __FBSDID("$FreeBSD$");
 
 #include "sdhci_if.h"
 
+#include "opt_mmccam.h"
+
 /* Tegra SDHOST controller vendor register definitions */
 #define	SDMMC_VENDOR_CLOCK_CNTRL		0x100
 #define	 VENDOR_CLOCK_CNTRL_CLK_SHIFT			8
@@ -463,5 +465,7 @@ static DEFINE_CLASS_0(sdhci, tegra_sdhci_driver, tegra_sdhci_methods,
     sizeof(struct tegra_sdhci_softc));
 DRIVER_MODULE(sdhci_tegra, simplebus, tegra_sdhci_driver, tegra_sdhci_devclass,
     NULL, NULL);
+#ifndef MMCCAM
 MODULE_DEPEND(sdhci_tegra, sdhci, 1, 1, 1);
 MMC_DECLARE_BRIDGE(sdhci);
+#endif
