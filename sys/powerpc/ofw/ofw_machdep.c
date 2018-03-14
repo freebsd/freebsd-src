049f53933929746636b2ece6a7a985d77147f02b
Kyle Evans <kevans@FreeBSD.org>
2018-03-14 03:45:33 +0000

    MFC r326310: Back out OF module installation in the event of failure.
    PS3 firmware gives some ancient FDT version (2) that fails the init check in
    OFW_FDT. It is still possible to make progress, but not while the OF layer
    is going crazy.

--- sys/powerpc/ofw/ofw_machdep.c
+++ sys/powerpc/ofw/ofw_machdep.c
@@ -389,6 +389,7 @@ boolean_t
 OF_bootstrap()
 {
 	boolean_t status = FALSE;
+	int err = 0;
 
 #ifdef AIM
 	if (openfirmware_entry != NULL) {
@@ -405,7 +406,7 @@ OF_bootstrap()
 		if (status != TRUE)
 			return status;
 
-		OF_init(openfirmware);
+		err = OF_init(openfirmware);
 	} else
 #endif
 	if (fdt != NULL) {
@@ -414,9 +415,15 @@ OF_bootstrap()
 		if (status != TRUE)
 			return status;
 
-		OF_init(fdt);
-		OF_interpret("perform-fixup", 0);
+		err = OF_init(fdt);
+		if (err == 0)
+			OF_interpret("perform-fixup", 0);
 	} 
+
+	if (err != 0) {
+		OF_install(NULL, 0);
+		status = FALSE;
+	}
 
 	return (status);
 }
