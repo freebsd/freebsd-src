e7061c878cdee98670413d8f210462c939b153fc
Eitan Adler <eadler@FreeBSD.org>
2018-03-15 04:59:47 +0000

    MFC r327279,r327571:
    Close fd when we're done dv-sending the file.

--- usr.sbin/fwcontrol/fwdv.c
+++ usr.sbin/fwcontrol/fwdv.c
@@ -409,7 +409,6 @@ again:
 			err(1, "write failed");
 		}
 	}
-	close(fd);
 	fprintf(stderr, "\n");
 send_end:
 	gettimeofday(&end, NULL);
@@ -417,4 +416,5 @@ send_end:
 			+ (end.tv_usec - start.tv_usec) * 1e-6;
 	fprintf(stderr, "%d frames, %.2f secs, %.2f frames/sec\n",
 			frames, rtime, frames/rtime);
+	close(fd);
 }
