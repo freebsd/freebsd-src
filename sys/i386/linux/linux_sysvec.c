9bec2ea66e56d7d887e8166cd88a196ff9d3dd4d
Ed Maste <emaste@FreeBSD.org>
2018-03-19 15:11:10 +0000

    linux*_sysvec.c: rationalize whitespace and comments
    There's a fair amount of duplication between MD linuxulator files.
    Make indentation and comments consistent between the three versions of
    linux_sysvec.c to reduce diffs when comparing them.
    
    Sponsored by:	Turing Robotic Industries Inc.

--- sys/i386/linux/linux_sysvec.c
+++ sys/i386/linux/linux_sysvec.c
@@ -112,6 +112,7 @@ static int	linux_fixup(register_t **stack_base,
 static int	elf_linux_fixup(register_t **stack_base,
 		    struct image_params *iparams);
 static void     linux_sendsig(sig_t catcher, ksiginfo_t *ksi, sigset_t *mask);
+static int	exec_linux_imgact_try(struct image_params *iparams);
 static void	exec_linux_setregs(struct thread *td,
 		    struct image_params *imgp, u_long stack);
 static register_t *linux_copyout_strings(struct image_params *imgp);
@@ -819,35 +820,34 @@ linux_fetch_syscall_args(struct thread *td)
  * be able to modify the interpreter path.  We only do this if a Linux
  * binary is doing the exec, so we do not create an EXEC module for it.
  */
-static int	exec_linux_imgact_try(struct image_params *iparams);
-
 static int
 exec_linux_imgact_try(struct image_params *imgp)
 {
-    const char *head = (const char *)imgp->image_header;
-    char *rpath;
-    int error = -1;
+	const char *head = (const char *)imgp->image_header;
+	char *rpath;
+	int error = -1;
 
-    /*
-     * The interpreter for shell scripts run from a Linux binary needs
-     * to be located in /compat/linux if possible in order to recursively
-     * maintain Linux path emulation.
-     */
-    if (((const short *)head)[0] == SHELLMAGIC) {
-	    /*
-	     * Run our normal shell image activator.  If it succeeds attempt
-	     * to use the alternate path for the interpreter.  If an alternate
-	     * path is found, use our stringspace to store it.
-	     */
-	    if ((error = exec_shell_imgact(imgp)) == 0) {
-		    linux_emul_convpath(FIRST_THREAD_IN_PROC(imgp->proc),
-			imgp->interpreter_name, UIO_SYSSPACE, &rpath, 0, AT_FDCWD);
-		    if (rpath != NULL)
-			    imgp->args->fname_buf =
-				imgp->interpreter_name = rpath;
-	    }
-    }
-    return (error);
+	/*
+	 * The interpreter for shell scripts run from a Linux binary needs
+	 * to be located in /compat/linux if possible in order to recursively
+	 * maintain Linux path emulation.
+	 */
+	if (((const short *)head)[0] == SHELLMAGIC) {
+		/*
+		 * Run our normal shell image activator.  If it succeeds then
+		 * attempt to use the alternate path for the interpreter.  If
+		 * an alternate path is found, use our stringspace to store it.
+		 */
+		if ((error = exec_shell_imgact(imgp)) == 0) {
+			linux_emul_convpath(FIRST_THREAD_IN_PROC(imgp->proc),
+			    imgp->interpreter_name, UIO_SYSSPACE, &rpath, 0,
+			    AT_FDCWD);
+			if (rpath != NULL)
+				imgp->args->fname_buf =
+				    imgp->interpreter_name = rpath;
+		}
+	}
+	return (error);
 }
 
 /*
