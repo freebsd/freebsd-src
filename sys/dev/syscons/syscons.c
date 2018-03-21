17b7d8a1eba4dc43099d01390bcf8922b0bd4549
imp <imp@FreeBSD.org>
2018-03-21 14:47:12 +0000

    Unlock giant when calling shutdown_nice()

--- sys/dev/syscons/syscons.c
+++ sys/dev/syscons/syscons.c
@@ -3858,22 +3858,28 @@ next_code:
 
 	    case RBT:
 #ifndef SC_DISABLE_REBOOT
-		if (enable_reboot && !(flags & SCGETC_CN))
+		if (enable_reboot && !(flags & SCGETC_CN)) {
+			mtx_unlock(&Giant);
 			shutdown_nice(0);
+		}
 #endif
 		break;
 
 	    case HALT:
 #ifndef SC_DISABLE_REBOOT
-		if (enable_reboot && !(flags & SCGETC_CN))
+		if (enable_reboot && !(flags & SCGETC_CN)) {
+			mtx_unlock(&Giant);
 			shutdown_nice(RB_HALT);
+		}
 #endif
 		break;
 
 	    case PDWN:
 #ifndef SC_DISABLE_REBOOT
-		if (enable_reboot && !(flags & SCGETC_CN))
+		if (enable_reboot && !(flags & SCGETC_CN)) {
+			mtx_unlock(&Giant);
 			shutdown_nice(RB_HALT|RB_POWEROFF);
+		}
 #endif
 		break;
 
