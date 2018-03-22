d3043ff9abd0e7f054ba41b3ee28ec855aa2d400
Alexander Motin <mav@FreeBSD.org>
2018-03-22 23:43:48 +0000

    MFC r329628: MFC r316910: 7812 Remove gender specific language
    illumos/illumos-gate@48bbca816818409505a6e214d0911fda44e622e3
    https://github.com/illumos/illumos-gate/commit/48bbca816818409505a6e214d0911fda44e622e3
    
    https://www.illumos.org/issues/7812
      This change removes all gendered language that did not refer specifically
      to an individual person or pet. The convention taken was to use
      variations on "they" when referring to users and/or human beings, while
      using "it" when referring to code, functions, and/or libraries.
      Additionally, we took the liberty to fix up any whitespace issues that
      were found in any files that were already being modified.
    
    Reviewed by: Matt Ahrens <mahrens@delphix.com>
    Reviewed by: Prakash Surya <prakash.surya@delphix.com>
    Reviewed by: Steve Gonczi <steve.gonczi@delphix.com>
    Reviewed by: Chris Williamson <chris.williamson@delphix.com>
    Reviewed by: George Wilson <george.wilson@delphix.com>
    Reviewed by: Igor Kozhukhov <igor@dilos.org>
    Reviewed by: Dan McDonald <danmcd@omniti.com>
    Reviewed by: Robert Mustacchi <rm@joyent.com>
    Approved by: Richard Lowe <richlowe@richlowe.net>
    Author: Daniel Hoffman <dj.hoffman@delphix.com>

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_dir.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_dir.c
@@ -21,7 +21,7 @@
 
 /*
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2013, 2015 by Delphix. All rights reserved.
+ * Copyright (c) 2013, 2016 by Delphix. All rights reserved.
  * Copyright 2017 Nexenta Systems, Inc.
  */
 
