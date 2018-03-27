261c408744d23225d1988d7be4e549a2ffbe7bf6
Jeff Roberson <jeff@FreeBSD.org>
2018-03-27 10:20:50 +0000

    Backout r331606 until I can identify why it does not boot on some machines.

--- sys/x86/x86/intr_machdep.c
+++ sys/x86/x86/intr_machdep.c
@@ -71,8 +71,6 @@
 #include <isa/isareg.h>
 #endif
 
-#include <vm/vm.h>
-
 #define	MAX_STRAY_LOG	5
 
 typedef void (*mask_fn)(void *);
@@ -187,8 +185,7 @@ intr_lookup_source(int vector)
 
 int
 intr_add_handler(const char *name, int vector, driver_filter_t filter,
-    driver_intr_t handler, void *arg, enum intr_type flags, void **cookiep,
-    int domain)
+    driver_intr_t handler, void *arg, enum intr_type flags, void **cookiep)
 {
 	struct intsrc *isrc;
 	int error;
@@ -203,7 +200,6 @@ intr_add_handler(const char *name, int vector, driver_filter_t filter,
 		intrcnt_updatename(isrc);
 		isrc->is_handlers++;
 		if (isrc->is_handlers == 1) {
-			isrc->is_domain = domain;
 			isrc->is_pic->pic_enable_intr(isrc);
 			isrc->is_pic->pic_enable_source(isrc);
 		}
@@ -511,27 +507,14 @@ DB_SHOW_COMMAND(irqs, db_show_irqs)
  */
 
 cpuset_t intr_cpus = CPUSET_T_INITIALIZER(0x1);
-static int current_cpu[MAXMEMDOM];
-
-static void
-intr_init_cpus(void)
-{
-	int i;
-
-	for (i = 0; i < vm_ndomains; i++) {
-		current_cpu[i] = 0;
-		if (!CPU_ISSET(current_cpu[i], &intr_cpus) ||
-		    !CPU_ISSET(current_cpu[i], &cpuset_domain[i]))
-			intr_next_cpu(i);
-	}
-}
+static int current_cpu;
 
 /*
  * Return the CPU that the next interrupt source should use.  For now
  * this just returns the next local APIC according to round-robin.
  */
 u_int
-intr_next_cpu(int domain)
+intr_next_cpu(void)
 {
 	u_int apic_id;
 
@@ -546,13 +529,12 @@ intr_next_cpu(int domain)
 #endif
 
 	mtx_lock_spin(&icu_lock);
-	apic_id = cpu_apic_ids[current_cpu[domain]];
+	apic_id = cpu_apic_ids[current_cpu];
 	do {
-		current_cpu[domain]++;
-		if (current_cpu[domain] > mp_maxid)
-			current_cpu[domain] = 0;
-	} while (!CPU_ISSET(current_cpu[domain], &intr_cpus) ||
-	    !CPU_ISSET(current_cpu[domain], &cpuset_domain[domain]));
+		current_cpu++;
+		if (current_cpu > mp_maxid)
+			current_cpu = 0;
+	} while (!CPU_ISSET(current_cpu, &intr_cpus));
 	mtx_unlock_spin(&icu_lock);
 	return (apic_id);
 }
@@ -586,18 +568,7 @@ intr_add_cpu(u_int cpu)
 	CPU_SET(cpu, &intr_cpus);
 }
 
-#ifdef EARLY_AP_STARTUP
-static void
-intr_smp_startup(void *arg __unused)
-{
-
-	intr_init_cpus();
-	return;
-}
-SYSINIT(intr_smp_startup, SI_SUB_SMP, SI_ORDER_SECOND, intr_smp_startup,
-    NULL);
-
-#else
+#ifndef EARLY_AP_STARTUP
 /*
  * Distribute all the interrupt sources among the available CPUs once the
  * AP's have been launched.
@@ -609,7 +580,6 @@ intr_shuffle_irqs(void *arg __unused)
 	u_int cpu;
 	int i;
 
-	intr_init_cpus();
 	/* Don't bother on UP. */
 	if (mp_ncpus == 1)
 		return;
@@ -629,12 +599,12 @@ intr_shuffle_irqs(void *arg __unused)
 			 */
 			cpu = isrc->is_event->ie_cpu;
 			if (cpu == NOCPU)
-				cpu = current_cpu[isrc->is_domain];
+				cpu = current_cpu;
 			if (isrc->is_pic->pic_assign_cpu(isrc,
 			    cpu_apic_ids[cpu]) == 0) {
 				isrc->is_cpu = cpu;
 				if (isrc->is_event->ie_cpu == NOCPU)
-					intr_next_cpu(isrc->is_domain);
+					intr_next_cpu();
 			}
 		}
 	}
@@ -665,11 +635,10 @@ sysctl_hw_intrs(SYSCTL_HANDLER_ARGS)
 		isrc = interrupt_sources[i];
 		if (isrc == NULL)
 			continue;
-		sbuf_printf(&sbuf, "%s:%d @cpu%d(domain%d): %ld\n",
+		sbuf_printf(&sbuf, "%s:%d @%d: %ld\n",
 		    isrc->is_event->ie_fullname,
 		    isrc->is_index,
 		    isrc->is_cpu,
-		    isrc->is_domain,
 		    *isrc->is_count);
 	}
 
@@ -728,7 +697,7 @@ intr_balance(void *dummy __unused, int pending __unused)
 	 * Restart the scan from the same location to avoid moving in the
 	 * common case.
 	 */
-	intr_init_cpus();
+	current_cpu = 0;
 
 	/*
 	 * Assign round-robin from most loaded to least.
@@ -737,8 +706,8 @@ intr_balance(void *dummy __unused, int pending __unused)
 		isrc = interrupt_sorted[i];
 		if (isrc == NULL  || isrc->is_event->ie_cpu != NOCPU)
 			continue;
-		cpu = current_cpu[isrc->is_domain];
-		intr_next_cpu(isrc->is_domain);
+		cpu = current_cpu;
+		intr_next_cpu();
 		if (isrc->is_cpu != cpu &&
 		    isrc->is_pic->pic_assign_cpu(isrc,
 		    cpu_apic_ids[cpu]) == 0)
@@ -766,7 +735,7 @@ SYSINIT(intr_balance_init, SI_SUB_SMP, SI_ORDER_ANY, intr_balance_init, NULL);
  * Always route interrupts to the current processor in the UP case.
  */
 u_int
-intr_next_cpu(int domain)
+intr_next_cpu(void)
 {
 
 	return (PCPU_GET(apic_id));
