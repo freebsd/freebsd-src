58f19785f7e4a9670234c5471b33399d9275a474
emaste <emaste@FreeBSD.org>
2018-03-30 01:22:47 +0000

    MFC r320695 (bdrewery): Fix out-of-tree kernel builds
    after r331730 (MFC of r320275) when bsd.linker.mk not yet installed.

--- sys/conf/kmod.mk
+++ sys/conf/kmod.mk
@@ -125,7 +125,7 @@ CFLAGS.gcc+= --param large-function-growth=1000
 CFLAGS+=	-fno-common
 LDFLAGS+=	-d -warn-common
 
-.if ${LINKER_FEATURES:Mbuild-id}
+.if defined(LINKER_FEATURES) && ${LINKER_FEATURES:Mbuild-id}
 LDFLAGS+=	-Wl,--build-id=sha1
 .endif
 
