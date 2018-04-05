cecc07e04a7c53548d68cbabbac59abd67fc2c0d
emaste <emaste@FreeBSD.org>
2018-04-05 14:25:39 +0000

    MFC r331442: Fixup return style(9) in amd64 linux*_sysvec.c

--- sys/amd64/linux32/linux32_sysvec.c
+++ sys/amd64/linux32/linux32_sysvec.c
@@ -211,15 +211,15 @@ static int
 translate_traps(int signal, int trap_code)
 {
 	if (signal != SIGBUS)
-		return signal;
+		return (signal);
 	switch (trap_code) {
 	case T_PROTFLT:
 	case T_TSSFLT:
 	case T_DOUBLEFLT:
 	case T_PAGEFLT:
-		return SIGSEGV;
+		return (SIGSEGV);
 	default:
-		return signal;
+		return (signal);
 	}
 }
 
