19cac21b2b8769b36a36e519e91aa35ebc4a70b8
kevans <kevans@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-04-06 19:49:57 +0000

    Re-sort LOADER options
    These have become unsorted from everything else. This is desync'd from
    stable/11 due to some hand-merging that was done there, so the MFC of this
    will look slightly different.
    
    MFC after:	3 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@332146 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -129,14 +129,14 @@ __DEFAULT_YES_OPTIONS = \
     LIBPTHREAD \
     LIBTHR \
     LLVM_COV \
+    LOADER_GELI \
+    LOADER_OFW \
+    LOADER_UBOOT \
     LOCALES \
     LOCATE \
     LPR \
     LS_COLORS \
     LZMA_SUPPORT \
-    LOADER_GELI \
-    LOADER_OFW \
-    LOADER_UBOOT \
     MAIL \
     MAILWRAPPER \
     MAKE \
