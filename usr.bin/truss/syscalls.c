b39a0c5e6fbaa5c7ffd34167e29b635d5f0a8363
tuexen <tuexen@FreeBSD.org>
2018-04-07 21:07:47 +0000

    MFC r328058:
    Using %p already prints "0x", so don't do it explicitly.

--- usr.bin/truss/syscalls.c
+++ usr.bin/truss/syscalls.c
@@ -1159,14 +1159,14 @@ print_sockaddr(FILE *fp, struct trussinfo *trussinfo, void *arg, socklen_t len)
 	}
 	/* If the length is too small, just bail. */
 	if (len < sizeof(*sa)) {
-		fprintf(fp, "0x%p", arg);
+		fprintf(fp, "%p", arg);
 		return;
 	}
 
 	sa = calloc(1, len);
 	if (get_struct(pid, arg, sa, len) == -1) {
 		free(sa);
-		fprintf(fp, "0x%p", arg);
+		fprintf(fp, "%p", arg);
 		return;
 	}
 
@@ -1223,7 +1223,7 @@ print_iovec(FILE *fp, struct trussinfo *trussinfo, void *arg, int iovcnt)
 	bool buf_truncated, iov_truncated;
 
 	if (iovcnt <= 0) {
-		fprintf(fp, "0x%p", arg);
+		fprintf(fp, "%p", arg);
 		return;
 	}
 	if (iovcnt > IOV_LIMIT) {
@@ -1233,7 +1233,7 @@ print_iovec(FILE *fp, struct trussinfo *trussinfo, void *arg, int iovcnt)
 		iov_truncated = false;
 	}
 	if (get_struct(pid, arg, &iov, iovcnt * sizeof(struct iovec)) == -1) {
-		fprintf(fp, "0x%p", arg);
+		fprintf(fp, "%p", arg);
 		return;
 	}
 
@@ -1261,7 +1261,7 @@ print_iovec(FILE *fp, struct trussinfo *trussinfo, void *arg, int iovcnt)
 			    buf_truncated ? "..." : "");
 			free(tmp3);
 		} else {
-			fprintf(fp, "0x%p", iov[i].iov_base);
+			fprintf(fp, "%p", iov[i].iov_base);
 		}
 		fprintf(fp, ",%zu}", iov[i].iov_len);
 	}
@@ -1478,7 +1478,7 @@ print_cmsgs(FILE *fp, pid_t pid, bool recv, struct msghdr *msghdr)
 	len = msghdr->msg_controllen;
 	cmsgbuf = calloc(1, len);
 	if (get_struct(pid, msghdr->msg_control, cmsgbuf, len) == -1) {
-		fprintf(fp, "0x%p", msghdr);
+		fprintf(fp, "%p", msghdr->msg_control);
 		free(cmsgbuf);
 	}
 	msghdr->msg_control = cmsgbuf;
