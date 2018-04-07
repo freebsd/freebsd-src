6ace9798be92be48c573f47cc359b3dd80d4a024
tuexen <tuexen@FreeBSD.org>
2018-04-07 21:05:39 +0000

    MFC r328015:
    Decode msghdr argument of sendmsg() and recvmsg().

--- usr.bin/truss/syscall.h
+++ usr.bin/truss/syscall.h
@@ -81,7 +81,7 @@ enum Argtype { None = 1, Hex, Octal, Int, UInt, LongHex, Name, Ptr, Stat, Ioctl,
 	Sockoptname, Msgflags, CapRights, PUInt, PQuadHex, Acltype,
 	Extattrnamespace, Minherit, Mlockall, Mountflags, Msync, Priowhich,
 	Ptraceop, Quotactlcmd, Reboothowto, Rtpriofunc, Schedpolicy, Schedparam,
-	PSig, Siginfo, Kevent11, Iovec, Sctpsndrcvinfo,
+	PSig, Siginfo, Kevent11, Iovec, Sctpsndrcvinfo, Msghdr,
 
 	CloudABIAdvice, CloudABIClockID, ClouduABIFDSFlags,
 	CloudABIFDStat, CloudABIFileStat, CloudABIFileType,
