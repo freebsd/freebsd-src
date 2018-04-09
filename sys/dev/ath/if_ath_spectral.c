0fea9dad898ad80c02bdd85f86d2a011859f06c7
emaste <emaste@FreeBSD.org>
2018-04-09 12:53:15 +0000

    MFC ath(4) potential memory disclosure fixes
    [1] r327499: ath: fix memory disclosure from ath_btcoex_ioctl
    
    The ath_btcoex_ioctl handler allocated a buffer without M_ZERO and
    returned it to userland without writing to it.
    
    The device has permissions only for root so this is not urgent, and the
    fix can be MFCd and considered for a future EN.
    
    [2] r327500: ath: fix possible memory disclosures in ioctl handlers
    
    Apply the fix from r327499 to additional ioctl handlers.
    
    Note: related fix in r327529 does not apply directly to stable/10 and
    will be addressed in a followup commit.
    
    Submitted by:	Domagoj Stolfa <domagoj.stolfa@gmail.com> [1]
    Reported by:	Ilja van Sprundel <ivansprundel@ioactive.com> [1,2]
    Reviewed by:	adrian [1]
    Sponsored by:	The FreeBSD Foundation

--- sys/dev/ath/if_ath_spectral.c
+++ sys/dev/ath/if_ath_spectral.c
@@ -210,7 +210,7 @@ ath_ioctl_spectral(struct ath_softc *sc, struct ath_diag *ad)
 		 * pointer for us to use below in reclaiming the buffer;
 		 * may want to be more defensive.
 		 */
-		outdata = malloc(outsize, M_TEMP, M_NOWAIT);
+		outdata = malloc(outsize, M_TEMP, M_NOWAIT | M_ZERO);
 		if (outdata == NULL) {
 			error = ENOMEM;
 			goto bad;
@@ -273,6 +273,7 @@ ath_ioctl_spectral(struct ath_softc *sc, struct ath_diag *ad)
 		break;
 		default:
 			error = EINVAL;
+			goto bad;
 	}
 	if (outsize < ad->ad_out_size)
 		ad->ad_out_size = outsize;
