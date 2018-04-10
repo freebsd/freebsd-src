f422673e10d5216be1e9742e2ecdd8a67d0ec9b8
Stephen Hurd <shurd@FreeBSD.org>
2018-04-10 19:42:50 +0000

    Make BPF global lock an SX
    This allows NIC drivers to sleep on polling config operations.
    
    Submitted by:	Matthew Macy <mmacy@mattmacy.io>
    Reviewed by:	shurd
    Sponsored by:	Limelight Networks
    Differential Revision:	https://reviews.freebsd.org/D14982

--- sys/kern/subr_witness.c
+++ sys/kern/subr_witness.c
@@ -569,7 +569,7 @@ static struct witness_order_list_entry order_lists[] = {
 	/*
 	 * BPF
 	 */
-	{ "bpf global lock", &lock_class_mtx_sleep },
+	{ "bpf global lock", &lock_class_sx },
 	{ "bpf interface lock", &lock_class_rw },
 	{ "bpf cdev lock", &lock_class_mtx_sleep },
 	{ NULL, NULL },
