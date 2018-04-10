0a29d6684e4b6a3a9b272a4cda7fdf6ad574d6de
Kyle Evans <kevans@FreeBSD.org>
2018-04-10 13:35:07 +0000

    Revert r331880, MFC of r328331 and bump FreeBSD_version
    There are logistics issues that weren't considered when this was originally
    MFC'd. All rc scripts in ports need audited (this is in progress) for usage
    of ${name}_limits that doesn't line up with the new interpretation, and
    individual rc.conf(5)'s need to be scrubbed of usage that doesn't line up.
    
    It's since been decided that it should be left for a feature in 12.
    
    1101514 introduced interpretation of ${name}_limits for rc scripts; this
    feature no longer exists as of 1101515.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -58,7 +58,7 @@
  *		in the range 5 to 9.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1101514	/* Master, propagated to newvers */
+#define __FreeBSD_version 1101515	/* Master, propagated to newvers */
 
 /*
  * __FreeBSD_kernel__ indicates that this system uses the kernel of FreeBSD,
