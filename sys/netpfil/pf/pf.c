c41420d5dc7cef4f643dec5f676bb990f2c6e589
Kristof Provost <kp@FreeBSD.org>
2018-04-11 11:43:12 +0000

    pf: limit ioctl to a reasonable and tuneable number of elements
    pf ioctls frequently take a variable number of elements as argument. This can
    potentially allow users to request very large allocations.  These will fail,
    but even a failing M_NOWAIT might tie up resources and result in concurrent
    M_WAITOK allocations entering vm_wait and inducing reclamation of caches.
    
    Limit these ioctls to what should be a reasonable value, but allow users to
    tune it should they need to.
    
    Differential Revision:	https://reviews.freebsd.org/D15018

--- sys/netpfil/pf/pf.c
+++ sys/netpfil/pf/pf.c
@@ -369,11 +369,14 @@ u_long	pf_hashmask;
 u_long	pf_srchashmask;
 static u_long	pf_hashsize;
 static u_long	pf_srchashsize;
+u_long	pf_ioctl_maxcount = 65535;
 
 SYSCTL_ULONG(_net_pf, OID_AUTO, states_hashsize, CTLFLAG_RDTUN,
     &pf_hashsize, 0, "Size of pf(4) states hashtable");
 SYSCTL_ULONG(_net_pf, OID_AUTO, source_nodes_hashsize, CTLFLAG_RDTUN,
     &pf_srchashsize, 0, "Size of pf(4) source nodes hashtable");
+SYSCTL_ULONG(_net_pf, OID_AUTO, request_maxcount, CTLFLAG_RDTUN,
+    &pf_ioctl_maxcount, 0, "Maximum number of tables, addresses, ... in a single ioctl() call");
 
 VNET_DEFINE(void *, pf_swi_cookie);
 
