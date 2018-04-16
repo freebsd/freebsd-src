5194e9ef3767a2e18b3ec702b7565525242a7dd1
John Baldwin <jhb@FreeBSD.org>
2018-04-16 20:45:21 +0000

    MFC 331324: Ensure thread library is initialized in pthread_testcancel().
    Call _thr_check_init() before reading curthread in pthread_testcancel().
    
    If a constructor in a library creates a semaphore via sem_init() and
    then waits for it via sem_wait(), the program can core dump in
    _pthread_testcancel() called from sem_wait().  This is because the
    semaphore implementation lives in libc, so the library's constructors
    can be run before libthr's constructors.
    
    Sponsored by:	DARPA / AFRL

--- lib/libthr/thread/thr_cancel.c
+++ lib/libthr/thread/thr_cancel.c
@@ -130,8 +130,10 @@ _pthread_setcanceltype(int type, int *oldtype)
 void
 _pthread_testcancel(void)
 {
-	struct pthread *curthread = _get_curthread();
+	struct pthread *curthread;
 
+	_thr_check_init();
+	curthread = _get_curthread();
 	testcancel(curthread);
 }
 
