12eaa215a17a9786d92c66d896409d1952326648
kevans <kevans@FreeBSD.org>
2018-04-16 00:12:03 +0000

    MFC r322318-r322319
    r322318:
    Mark geom classes as deprecated.
    
    geom_bsd, geom_mbr and geom_sunlabel have been obsolete since Marcel
    Moolenaar's geom_part was in FreeBSD 7. They haven't been in GENERIC
    since FreeBSD 8. Add warning when used.
    
    geom_vol_ffs has been obsolete since ufs support to geom_label was
    committed in FreeBSD 5. It hasn't been in GENERIC since FreeBSD 5.
    Add warning when used.
    
    geom_fox has been obsolete since gmultipath was committed in FreeBSD 7.
    (no warning added, since this is a very obscure class).
    
    These will all be removed in FreeBSD 12.
    
    r322319:
    Also provide a warning for geom_fox.

--- sys/geom/geom_mbr.c
+++ sys/geom/geom_mbr.c
@@ -57,6 +57,8 @@ FEATURE(geom_mbr, "GEOM DOS/MBR partitioning support");
 #define MBR_CLASS_NAME "MBR"
 #define MBREXT_CLASS_NAME "MBREXT"
 
+static int g_mbr_once = 0;
+
 static struct dos_partition historical_bogus_partition_table[NDOSPART] = {
         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
@@ -315,6 +317,12 @@ g_mbr_taste(struct g_class *mp, struct g_provider *pp, int insist)
 	if (LIST_EMPTY(&gp->provider)) {
 		g_slice_spoiled(cp);
 		return (NULL);
+	}
+	if (!g_mbr_once) {
+		g_mbr_once = 1;
+		printf(
+		    "WARNING: geom_mbr (geom %s) is deprecated, "
+		    "use gpart instead.\n", gp->name);
 	}
 	return (gp);
 }
