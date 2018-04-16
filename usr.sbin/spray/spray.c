8fbad34a3b6c60f01d96b11e9584d2f7e793ee0a
asomers <asomers@FreeBSD.org>
2018-04-16 16:28:23 +0000

    MFC r330515:
    spray: fix the spelling in an output string

--- usr.sbin/spray/spray.c
+++ usr.sbin/spray/spray.c
@@ -145,7 +145,7 @@ main(int argc, char *argv[])
 
 
 	/* Spray server with packets */
-	printf ("sending %u packets of lnth %d to %s ...", count, length,
+	printf ("sending %u packets of length %d to %s ...", count, length,
 	    *argv);
 	fflush (stdout);
 
