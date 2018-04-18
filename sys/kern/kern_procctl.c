b94088633898fdee8948787525d080b24aa57154
Konstantin Belousov <kib@FreeBSD.org>
2018-04-18 21:31:13 +0000

    Add PROC_PDEATHSIG_SET to procctl interface.
    Allow processes to request the delivery of a signal upon death of
    their parent process.  Supposed consumer of the feature is PostgreSQL.
    
    Submitted by:	Thomas Munro
    Reviewed by:	jilles, mjg
    MFC after:	2 weeks
    Differential revision:	https://reviews.freebsd.org/D15106

--- sys/kern/kern_procctl.c
+++ sys/kern/kern_procctl.c
@@ -431,7 +431,7 @@ sys_procctl(struct thread *td, struct procctl_args *uap)
 		struct procctl_reaper_pids rp;
 		struct procctl_reaper_kill rk;
 	} x;
-	int error, error1, flags;
+	int error, error1, flags, signum;
 
 	switch (uap->com) {
 	case PROC_SPROTECT:
@@ -467,6 +467,15 @@ sys_procctl(struct thread *td, struct procctl_args *uap)
 	case PROC_TRAPCAP_STATUS:
 		data = &flags;
 		break;
+	case PROC_PDEATHSIG_SET:
+		error = copyin(uap->data, &signum, sizeof(signum));
+		if (error != 0)
+			return (error);
+		data = &signum;
+		break;
+	case PROC_PDEATHSIG_GET:
+		data = &signum;
+		break;
 	default:
 		return (EINVAL);
 	}
@@ -485,6 +494,10 @@ sys_procctl(struct thread *td, struct procctl_args *uap)
 	case PROC_TRAPCAP_STATUS:
 		if (error == 0)
 			error = copyout(&flags, uap->data, sizeof(flags));
+		break;
+	case PROC_PDEATHSIG_GET:
+		if (error == 0)
+			error = copyout(&signum, uap->data, sizeof(signum));
 		break;
 	}
 	return (error);
@@ -527,6 +540,7 @@ kern_procctl(struct thread *td, idtype_t idtype, id_t id, int com, void *data)
 	struct pgrp *pg;
 	struct proc *p;
 	int error, first_error, ok;
+	int signum;
 	bool tree_locked;
 
 	switch (com) {
@@ -537,8 +551,31 @@ kern_procctl(struct thread *td, idtype_t idtype, id_t id, int com, void *data)
 	case PROC_REAP_KILL:
 	case PROC_TRACE_STATUS:
 	case PROC_TRAPCAP_STATUS:
+	case PROC_PDEATHSIG_SET:
+	case PROC_PDEATHSIG_GET:
 		if (idtype != P_PID)
 			return (EINVAL);
+	}
+
+	switch (com) {
+	case PROC_PDEATHSIG_SET:
+		signum = *(int *)data;
+		p = td->td_proc;
+		if ((id != 0 && id != p->p_pid) ||
+		    (signum != 0 && !_SIG_VALID(signum)))
+			return (EINVAL);
+		PROC_LOCK(p);
+		p->p_pdeathsig = signum;
+		PROC_UNLOCK(p);
+		return (0);
+	case PROC_PDEATHSIG_GET:
+		p = td->td_proc;
+		if (id != 0 && id != p->p_pid)
+			return (EINVAL);
+		PROC_LOCK(p);
+		*(int *)data = p->p_pdeathsig;
+		PROC_UNLOCK(p);
+		return (0);
 	}
 
 	switch (com) {
