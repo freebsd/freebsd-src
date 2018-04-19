d0c7b58516d982f52f3ba9c90622bf0ae9d7540c
Pedro F. Giffuni <pfg@FreeBSD.org>
2018-04-19 02:50:15 +0000

    MFC r328957: {ext2|ufs}_readdir: Avoid setting negative ncookies.
    ncookies cannot be negative or the allocator will fail. This should only
    happen if a caller is very broken but we can still try to survive the
    event.
    
    We should probably also verify for uio_resid > MAXPHYS but in that case
    it is not clear that just clipping the ncookies value is an adequate
    response.

--- sys/ufs/ufs/ufs_vnops.c
+++ sys/ufs/ufs/ufs_vnops.c
@@ -2179,7 +2179,10 @@ ufs_readdir(ap)
 	if (ip->i_effnlink == 0)
 		return (0);
 	if (ap->a_ncookies != NULL) {
-		ncookies = uio->uio_resid;
+		if (uio->uio_resid < 0)
+			ncookies = 0;
+		else
+			ncookies = uio->uio_resid;
 		if (uio->uio_offset >= ip->i_size)
 			ncookies = 0;
 		else if (ip->i_size - uio->uio_offset < ncookies)
