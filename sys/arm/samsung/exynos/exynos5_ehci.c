04b42196214e91a8c730dff1cb22e9d6fff537d6
manu <manu@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-04-27 21:05:58 +0000

    arm: Fix duplicate ehci DRIVER_MODULE
    Name each ehci driver uniquely.
    This remove the warning printed at each arm boot :
    module_register: cannot register simplebus/ehci from kernel; already loaded from kernel
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@333074 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/arm/samsung/exynos/exynos5_ehci.c
+++ sys/arm/samsung/exynos/exynos5_ehci.c
@@ -131,8 +131,8 @@ static driver_t ehci_driver = {
 
 static devclass_t ehci_devclass;
 
-DRIVER_MODULE(ehci, simplebus, ehci_driver, ehci_devclass, 0, 0);
-MODULE_DEPEND(ehci, usb, 1, 1, 1);
+DRIVER_MODULE(exynos_ehci, simplebus, ehci_driver, ehci_devclass, 0, 0);
+MODULE_DEPEND(exynos_ehci, usb, 1, 1, 1);
 
 /*
  * Public methods
