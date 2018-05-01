f8715f82c1271bb8d24aeb059f5945a23905755f
jkim <jkim@FreeBSD.org>
2018-05-01 19:17:38 +0000

    MFV:	r333077
    Merge ACPICA 20180427.

--- sys/contrib/dev/acpica/include/acpixf.h
+++ sys/contrib/dev/acpica/include/acpixf.h
@@ -154,7 +154,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20180313
+#define ACPI_CA_VERSION                 0x20180427
 
 #include <contrib/dev/acpica/include/acconfig.h>
 #include <contrib/dev/acpica/include/actypes.h>
