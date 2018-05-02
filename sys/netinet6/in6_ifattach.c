7d4b8facc71c1322df5656f7d007a39939d5d013
shurd <shurd@FreeBSD.org>
2018-05-02 19:36:29 +0000

    Separate list manipulation locking from state change in multicast
    Multicast incorrectly calls in to drivers with a mutex held causing drivers
    to have to go through all manner of contortions to use a non sleepable lock.
    Serialize multicast updates instead.
    
    Submitted by:	mmacy <mmacy@mattmacy.io>
    Reviewed by:	shurd, sbruno
    Sponsored by:	Limelight Networks
    Differential Revision:	https://reviews.freebsd.org/D14969

--- sys/netinet6/in6_ifattach.c
+++ sys/netinet6/in6_ifattach.c
@@ -846,13 +846,13 @@ in6_tmpaddrtimer(void *arg)
 static void
 in6_purgemaddrs(struct ifnet *ifp)
 {
-	LIST_HEAD(,in6_multi)	 purgeinms;
-	struct in6_multi	*inm, *tinm;
+	struct in6_multi_head	 purgeinms;
+	struct in6_multi	*inm;
 	struct ifmultiaddr	*ifma;
 
-	LIST_INIT(&purgeinms);
+	SLIST_INIT(&purgeinms);
 	IN6_MULTI_LOCK();
-
+	IN6_MULTI_LIST_LOCK();
 	/*
 	 * Extract list of in6_multi associated with the detaching ifp
 	 * which the PF_INET6 layer is about to release.
@@ -865,17 +865,13 @@ in6_purgemaddrs(struct ifnet *ifp)
 		    ifma->ifma_protospec == NULL)
 			continue;
 		inm = (struct in6_multi *)ifma->ifma_protospec;
-		LIST_INSERT_HEAD(&purgeinms, inm, in6m_entry);
+		in6m_rele_locked(&purgeinms, inm);
 	}
 	IF_ADDR_RUNLOCK(ifp);
-
-	LIST_FOREACH_SAFE(inm, &purgeinms, in6m_entry, tinm) {
-		LIST_REMOVE(inm, in6m_entry);
-		in6m_release_locked(inm);
-	}
 	mld_ifdetach(ifp);
-
+	IN6_MULTI_LIST_UNLOCK();
 	IN6_MULTI_UNLOCK();
+	in6m_release_list_deferred(&purgeinms);
 }
 
 void
