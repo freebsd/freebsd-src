cdaf2ed02ca528ef928c02f4dae2a64163ff7773
shurd <shurd@FreeBSD.org>
2018-05-08 15:51:40 +0000

    MFC: r308728, r314369, r315243, r316026, r316581, r316616, r318359, r319922, r319990, r321481, r323232-323233, r323321, r323874, r323955, r324323, r324964, r325169, r325488, r325620, r326985, r326999-327001, r327003, r329335
    Update bnxt to latest -CURRENT.
    
    Approved by:	re (gjb@)
    Differential Revision:	https://reviews.freebsd.org/D15143

--- sys/dev/bnxt/bnxt_txrx.c
+++ sys/dev/bnxt/bnxt_txrx.c
@@ -419,6 +419,37 @@ cmpl_invalid:
 	return avail;
 }
 
+static void
+bnxt_set_rsstype(if_rxd_info_t ri, uint8_t rss_hash_type)
+{
+	uint8_t rss_profile_id;
+
+	rss_profile_id = BNXT_GET_RSS_PROFILE_ID(rss_hash_type);
+	switch (rss_profile_id) {
+	case BNXT_RSS_HASH_TYPE_TCPV4:
+		ri->iri_rsstype = M_HASHTYPE_RSS_TCP_IPV4;
+		break;
+	case BNXT_RSS_HASH_TYPE_UDPV4:
+		ri->iri_rsstype = M_HASHTYPE_RSS_UDP_IPV4;
+		break;
+	case BNXT_RSS_HASH_TYPE_IPV4:
+		ri->iri_rsstype = M_HASHTYPE_RSS_IPV4;
+		break;
+	case BNXT_RSS_HASH_TYPE_TCPV6:
+		ri->iri_rsstype = M_HASHTYPE_RSS_TCP_IPV6;
+		break;
+	case BNXT_RSS_HASH_TYPE_UDPV6:
+		ri->iri_rsstype = M_HASHTYPE_RSS_UDP_IPV6;
+		break;
+	case BNXT_RSS_HASH_TYPE_IPV6:
+		ri->iri_rsstype = M_HASHTYPE_RSS_IPV6;
+		break;
+	default:
+		ri->iri_rsstype = M_HASHTYPE_OPAQUE_HASH;
+		break;
+	}
+}
+
 static int
 bnxt_pkt_get_l2(struct bnxt_softc *softc, if_rxd_info_t ri,
     struct bnxt_cp_ring *cpr, uint16_t flags_type)
@@ -436,13 +467,7 @@ bnxt_pkt_get_l2(struct bnxt_softc *softc, if_rxd_info_t ri,
 	/* Extract from the first 16-byte BD */
 	if (flags_type & RX_PKT_CMPL_FLAGS_RSS_VALID) {
 		ri->iri_flowid = le32toh(rcp->rss_hash);
-		/*
-		 * TODO: Extract something useful from rcp->rss_hash_type
-		 * (undocumented)
-		 * May be documented in the "LSI ES"
-		 * also check the firmware code.
-		 */
-		ri->iri_rsstype = M_HASHTYPE_OPAQUE;
+		bnxt_set_rsstype(ri, rcp->rss_hash_type);
 	}
 	else {
 		ri->iri_rsstype = M_HASHTYPE_NONE;
@@ -476,9 +501,11 @@ bnxt_pkt_get_l2(struct bnxt_softc *softc, if_rxd_info_t ri,
 		if (!(errors & RX_PKT_CMPL_ERRORS_IP_CS_ERROR))
 			ri->iri_csum_flags |= CSUM_IP_VALID;
 	}
-	if (flags2 & RX_PKT_CMPL_FLAGS2_L4_CS_CALC) {
+	if (flags2 & (RX_PKT_CMPL_FLAGS2_L4_CS_CALC |
+		      RX_PKT_CMPL_FLAGS2_T_L4_CS_CALC)) {
 		ri->iri_csum_flags |= CSUM_L4_CALC;
-		if (!(errors & RX_PKT_CMPL_ERRORS_L4_CS_ERROR)) {
+		if (!(errors & (RX_PKT_CMPL_ERRORS_L4_CS_ERROR |
+				RX_PKT_CMPL_ERRORS_T_L4_CS_ERROR))) {
 			ri->iri_csum_flags |= CSUM_L4_VALID;
 			ri->iri_csum_data = 0xffff;
 		}
@@ -522,13 +549,7 @@ bnxt_pkt_get_tpa(struct bnxt_softc *softc, if_rxd_info_t ri,
 	/* Extract from the first 16-byte BD */
 	if (le16toh(tpas->low.flags_type) & RX_TPA_START_CMPL_FLAGS_RSS_VALID) {
 		ri->iri_flowid = le32toh(tpas->low.rss_hash);
-		/*
-		 * TODO: Extract something useful from tpas->low.rss_hash_type
-		 * (undocumented)
-		 * May be documented in the "LSI ES"
-		 * also check the firmware code.
-		 */
-		ri->iri_rsstype = M_HASHTYPE_OPAQUE;
+		bnxt_set_rsstype(ri, tpas->low.rss_hash_type);
 	}
 	else {
 		ri->iri_rsstype = M_HASHTYPE_NONE;
