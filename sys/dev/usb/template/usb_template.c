2d1a76975e07946fd82cdd9c94857add59047678
Edward Tomasz Napierala <trasz@FreeBSD.org>
2018-05-17 15:19:29 +0000

    Fix off-by-one in usb_decode_str_desc().  Previously it would decode one character too many.  Note that this function is only used to decode string descriptors generated by the kernel itself.
    Reviewed by:	hselasky@
    MFC after:	2 weeks
    Sponsored by:	The FreeBSD Foundation

--- sys/dev/usb/template/usb_template.c
+++ sys/dev/usb/template/usb_template.c
@@ -127,7 +127,12 @@ usb_decode_str_desc(struct usb_string_descriptor *sd, char *buf, size_t buflen)
 {
 	size_t i;
 
-	for (i = 0; i < buflen - 1 && i < sd->bLength / 2; i++)
+	if (sd->bLength < 2) {
+		buf[0] = '\0';
+		return;
+	}
+
+	for (i = 0; i < buflen - 1 && i < (sd->bLength / 2) - 1; i++)
 		buf[i] = UGETW(sd->bString[i]);
 
 	buf[i] = '\0';
