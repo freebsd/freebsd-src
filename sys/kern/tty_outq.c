3ef78c9c966c15fedf4182d60c949de7c1820f32
Matt Macy <mmacy@FreeBSD.org>
2018-05-19 04:02:29 +0000

    tty: conditionally assign to ret value only used by MPASS statement

--- sys/kern/tty_outq.c
+++ sys/kern/tty_outq.c
@@ -324,13 +324,15 @@ ttyoutq_write(struct ttyoutq *to, const void *buf, size_t nbytes)
 int
 ttyoutq_write_nofrag(struct ttyoutq *to, const void *buf, size_t nbytes)
 {
+#ifdef INVARIANTS
 	size_t ret;
+#endif
 
 	if (ttyoutq_bytesleft(to) < nbytes)
 		return (-1);
 
 	/* We should always be able to write it back. */
-	ret = ttyoutq_write(to, buf, nbytes);
+	DBGSET(ret, ttyoutq_write(to, buf, nbytes));
 	MPASS(ret == nbytes);
 
 	return (0);
