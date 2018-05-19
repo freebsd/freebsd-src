57a47f31dd678069a925ecb257e80237d22140e7
eadler <eadler@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-05-19 22:59:58 +0000

    top(1): assume that we're building on FreeBSD
    This allows us to avoid the ifdefs that we set unconditionally.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@333900 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/top/display.c
+++ usr.bin/top/display.c
@@ -1220,7 +1220,6 @@ register char **names;
     register char *p;
     register int num;
     register char *thisname;
-    register int useM = No;
     char rbuf[6];
 
     /* format each number followed by its string */
