642e6fc8aa3f3b6c773612be28e01691c63b6ac3
eadler <eadler@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-05-20 18:02:40 +0000

    revert r333928
    I had missed a file when testing this, and it does not build. Will try again.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@333930 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/top/screen.c
+++ usr.bin/top/screen.c
@@ -36,7 +36,6 @@
 #  include <termios.h>
 # endif
 #endif
-#if defined(TERMIO) || defined(TERMIOS)
 # ifndef TAB3
 #  ifdef OXTABS
 #   define TAB3 OXTABS
@@ -44,7 +43,6 @@
 #   define TAB3 0
 #  endif
 # endif
-#endif
 #include <curses.h>
 #include <termcap.h>
 #include "screen.h"
@@ -75,18 +73,12 @@ char *end_standout;
 char *terminal_init;
 char *terminal_end;
 
-#ifdef SGTTY
 static struct sgttyb old_settings;
 static struct sgttyb new_settings;
-#endif
-#ifdef TERMIO
 static struct termio old_settings;
 static struct termio new_settings;
-#endif
-#ifdef TERMIOS
 static struct termios old_settings;
 static struct termios new_settings;
-#endif
 static char is_a_terminal = No;
 #ifdef TOStop
 static int old_lword;
@@ -217,24 +209,18 @@ int interactive;
     get_screensize();
 
     /* if stdout is not a terminal, pretend we are a dumb terminal */
-#ifdef SGTTY
     if (ioctl(STDOUT, TIOCGETP, &old_settings) == -1)
     {
 	smart_terminal = No;
     }
-#endif
-#ifdef TERMIO
     if (ioctl(STDOUT, TCGETA, &old_settings) == -1)
     {
 	smart_terminal = No;
     }
-#endif
-#ifdef TERMIOS
     if (tcgetattr(STDOUT, &old_settings) == -1)
     {
 	smart_terminal = No;
     }
-#endif
 }
 
 void
@@ -242,7 +228,6 @@ init_screen()
 
 {
     /* get the old settings for safe keeping */
-#ifdef SGTTY
     if (ioctl(STDOUT, TIOCGETP, &old_settings) != -1)
     {
 	/* copy the settings so we can modify them */
@@ -271,8 +256,6 @@ init_screen()
 	/* send the termcap initialization string */
 	putcap(terminal_init);
     }
-#endif
-#ifdef TERMIO
     if (ioctl(STDOUT, TCGETA, &old_settings) != -1)
     {
 	/* copy the settings so we can modify them */
@@ -295,8 +278,6 @@ init_screen()
 	/* send the termcap initialization string */
 	putcap(terminal_init);
     }
-#endif
-#ifdef TERMIOS
     if (tcgetattr(STDOUT, &old_settings) != -1)
     {
 	/* copy the settings so we can modify them */
@@ -319,7 +300,6 @@ init_screen()
 	/* send the termcap initialization string */
 	putcap(terminal_init);
     }
-#endif
 
     if (!is_a_terminal)
     {
@@ -344,18 +324,12 @@ end_screen()
     /* if we have settings to reset, then do so */
     if (is_a_terminal)
     {
-#ifdef SGTTY
 	(void) ioctl(STDOUT, TIOCSETP, &old_settings);
 #ifdef TOStop
 	(void) ioctl(STDOUT, TIOCLSET, &old_lword);
 #endif
-#endif
-#ifdef TERMIO
 	(void) ioctl(STDOUT, TCSETA, &old_settings);
-#endif
-#ifdef TERMIOS
 	(void) tcsetattr(STDOUT, TCSADRAIN, &old_settings);
-#endif
     }
 }
 
@@ -366,18 +340,12 @@ reinit_screen()
     /* install our settings if it is a terminal */
     if (is_a_terminal)
     {
-#ifdef SGTTY
 	(void) ioctl(STDOUT, TIOCSETP, &new_settings);
 #ifdef TOStop
 	(void) ioctl(STDOUT, TIOCLSET, &new_lword);
 #endif
-#endif
-#ifdef TERMIO
 	(void) ioctl(STDOUT, TCSETA, &new_settings);
-#endif
-#ifdef TERMIOS
 	(void) tcsetattr(STDOUT, TCSADRAIN, &new_settings);
-#endif
     }
 
     /* send init string */
@@ -392,7 +360,6 @@ get_screensize()
 
 {
 
-#ifdef TIOCGWINSZ
 
     struct winsize ws;
 
@@ -408,25 +375,6 @@ get_screensize()
 	}
     }
 
-#else
-#ifdef TIOCGSIZE
-
-    struct ttysize ts;
-
-    if (ioctl (1, TIOCGSIZE, &ts) != -1)
-    {
-	if (ts.ts_lines != 0)
-	{
-	    screen_length = ts.ts_lines;
-	}
-	if (ts.ts_cols != 0)
-	{
-	    screen_width = ts.ts_cols - 1;
-	}
-    }
-
-#endif /* TIOCGSIZE */
-#endif /* TIOCGWINSZ */
 
     (void) strncpy(lower_left, tgoto(cursor_motion, 0, screen_length - 1),
 	sizeof(lower_left) - 1);
