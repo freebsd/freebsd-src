d24e5831339a7082f3b83f282e52b2ce3ba8c24d
fabient <fabient@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-05-22 15:54:25 +0000

    Add a SPD cache to speed up lookups.
    When large SPDs are used, we face two problems:
    
    - too many CPU cycles are spent during the linear searches in the SPD
      for each packet
    - too much contention on multi socket systems, since we use a single
      shared lock.
    
    Main changes:
    
    - added the sysctl tree 'net.key.spdcache' to control the SPD cache
      (disabled by default).
    - cache the sp indexes that are used to perform SP lookups.
    - use a range of dedicated mutexes to protect the cache lines.
    
    Submitted by: Emeric Poupon <emeric.poupon@stormshield.eu>
    Reviewed by: ae
    Sponsored by:	Stormshield
    Differential Revision: https://reviews.freebsd.org/D15050
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@334054 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/uipc_mbuf.c
+++ sys/kern/uipc_mbuf.c
@@ -1633,9 +1633,6 @@ m_unshare(struct mbuf *m0, int how)
 				mprev->m_len += m->m_len;
 				mprev->m_next = m->m_next;	/* unlink from chain */
 				m_free(m);			/* reclaim mbuf */
-#if 0
-				newipsecstat.ips_mbcoalesced++;
-#endif
 			} else {
 				mprev = m;
 			}
@@ -1665,9 +1662,6 @@ m_unshare(struct mbuf *m0, int how)
 			mprev->m_len += m->m_len;
 			mprev->m_next = m->m_next;	/* unlink from chain */
 			m_free(m);			/* reclaim mbuf */
-#if 0
-			newipsecstat.ips_clcoalesced++;
-#endif
 			continue;
 		}
 
