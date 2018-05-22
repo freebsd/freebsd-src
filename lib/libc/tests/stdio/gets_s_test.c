efb5ea6e6c680f9a487e5e1fa87abea8459450f7
cy <cy@FreeBSD.org>
2018-05-22 06:22:58 +0000

    Conform to Berne Convention.
    MFC after:	3 days

--- lib/libc/tests/stdio/gets_s_test.c
+++ lib/libc/tests/stdio/gets_s_test.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2017 Cyril S. E. Schubert.  All rights reserved.
+ * Copyright (c) 2017 Cyril S. E. Schubert
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
