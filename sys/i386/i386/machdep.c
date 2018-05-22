31acfe0f07a27191c91f2a0b0aaaa0c28bc3b059
jhb <jhb@FreeBSD.org>
2018-05-22 00:45:00 +0000

    Cleanups related to debug exceptions on x86.
    - Add constants for fields in DR6 and the reserved fields in DR7.  Use
      these constants instead of magic numbers in most places that use DR6
      and DR7.
    - Refer to T_TRCTRAP as "debug exception" rather than a "trace trap"
      as it is not just for trace exceptions.
    - Always read DR6 for debug exceptions and only clear TF in the flags
      register for user exceptions where DR6.BS is set.
    - Clear DR6 before returning from a debug exception handler as
      recommended by the SDM dating all the way back to the 386.  This
      allows debuggers to determine the cause of each exception.  For
      kernel traps, clear DR6 in the T_TRCTRAP case and pass DR6 by value
      to other parts of the handler (namely, user_dbreg_trap()).  For user
      traps, wait until after trapsignal to clear DR6 so that userland
      debuggers can read DR6 via PT_GETDBREGS while the thread is stopped
      in trapsignal().
    
    Reviewed by:	kib, rgrimes
    MFC after:	1 month
    Differential Revision:	https://reviews.freebsd.org/D15189

--- sys/i386/i386/machdep.c
+++ sys/i386/i386/machdep.c
@@ -3151,14 +3151,23 @@ set_dbregs(struct thread *td, struct dbreg *dbregs)
  * breakpoint was in user space.  Return 0, otherwise.
  */
 int
-user_dbreg_trap(void)
+user_dbreg_trap(register_t dr6)
 {
-        u_int32_t dr7, dr6; /* debug registers dr6 and dr7 */
+        u_int32_t dr7;
         u_int32_t bp;       /* breakpoint bits extracted from dr6 */
         int nbp;            /* number of breakpoints that triggered */
         caddr_t addr[4];    /* breakpoint addresses */
         int i;
-        
+
+        bp = dr6 & DBREG_DR6_BMASK;
+        if (bp == 0) {
+                /*
+                 * None of the breakpoint bits are set meaning this
+                 * trap was not caused by any of the debug registers
+                 */
+                return 0;
+        }
+
         dr7 = rdr7();
         if ((dr7 & 0x000000ff) == 0) {
                 /*
@@ -3170,16 +3179,6 @@ user_dbreg_trap(void)
         }
 
         nbp = 0;
-        dr6 = rdr6();
-        bp = dr6 & 0x0000000f;
-
-        if (!bp) {
-                /*
-                 * None of the breakpoint bits are set meaning this
-                 * trap was not caused by any of the debug registers
-                 */
-                return 0;
-        }
 
         /*
          * at least one of the breakpoints were hit, check to see
