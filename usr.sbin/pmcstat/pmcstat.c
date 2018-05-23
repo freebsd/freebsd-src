834f2e02b273120a723f8a34be2d2362d9f0234a
mmacy <mmacy@FreeBSD.org>
2018-05-23 17:25:00 +0000

    hwpmc: add thread id field to callchain and context switch records to allow filtering on thread in post-processing.
    To generate stacks for just ${THREADID}:
    
    pmcstat -R ${PREFIX}.pmcstat -L ${THREADID} -z100 -G ${PREFIX}.stacks
    
    Sponsored by:	Limelight Networks

--- usr.sbin/pmcstat/pmcstat.c
+++ usr.sbin/pmcstat/pmcstat.c
@@ -500,7 +500,7 @@ main(int argc, char **argv)
 	CPU_COPY(&rootmask, &cpumask);
 
 	while ((option = getopt(argc, argv,
-	    "CD:EF:G:IM:NO:P:R:S:TWa:c:def:gk:l:m:n:o:p:qr:s:t:vw:z:")) != -1)
+	    "CD:EF:G:IL:M:NO:P:R:S:TWa:c:def:gk:l:m:n:o:p:qr:s:t:vw:z:")) != -1)
 		switch (option) {
 		case 'a':	/* Annotate + callgraph */
 			args.pa_flags |= FLAG_DO_ANNOTATE;
@@ -581,6 +581,10 @@ main(int argc, char **argv)
 			args.pa_flags    |= FLAG_HAS_KERNELPATH;
 			break;
 
+		case 'L':
+			args.pa_flags |= FLAG_FILTER_THREAD_ID;
+			args.pa_tid = strtol(optarg, &end, 0);
+			break;
 		case 'l':	/* time duration in seconds */
 			duration = strtod(optarg, &end);
 			if (*end != '\0' || duration <= 0)
