c376ee0076358ce7f14f928a1bc3cd563b3e74f3
andrew <andrew@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-05-24 16:07:47 +0000

    Remove physmap from the arm64 machdep.h. This was missed in r334162.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@334164 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/arm64/include/machdep.h
+++ sys/arm64/include/machdep.h
@@ -45,9 +45,6 @@ enum arm64_bus {
 
 extern enum arm64_bus arm64_bus_method;
 
-extern vm_paddr_t physmap[];
-extern u_int physmap_idx;
-
 void dbg_init(void);
 void initarm(struct arm64_bootparams *);
 extern void (*pagezero)(void *);
