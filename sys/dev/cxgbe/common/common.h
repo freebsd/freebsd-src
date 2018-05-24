164ef69de3ef5f6dea741eab93c861220b527ff2
np <np@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-05-24 08:02:11 +0000

    cxgbe(4): Fix range checks in is_etid.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@334137 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/cxgbe/common/common.h
+++ sys/dev/cxgbe/common/common.h
@@ -358,7 +358,7 @@ struct adapter_params {
 	u_int ftid_min;
 	u_int ftid_max;
 	u_int etid_min;
-	u_int netids;
+	u_int etid_max;
 
 	unsigned int cim_la_size;
 
@@ -448,7 +448,8 @@ static inline int is_ftid(const struct adapter *sc, u_int tid)
 static inline int is_etid(const struct adapter *sc, u_int tid)
 {
 
-	return (tid >= sc->params.etid_min);
+	return (sc->params.etid_min > 0 && tid >= sc->params.etid_min &&
+	    tid <= sc->params.etid_max);
 }
 
 static inline int is_offload(const struct adapter *adap)
