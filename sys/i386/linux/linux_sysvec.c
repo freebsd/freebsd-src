cbf7e0cba7d47c20c73122e8dbefb98bf89fedad
Brooks Davis <brooks@FreeBSD.org>
2018-05-29 20:03:24 +0000

    Correct pointer subtraction in KASSERT().
    The assertion would never fire without truly spectacular future
    programming errors.
    
    Reported by:	Coverity
    CID:		1391370
    Sponsored by:	DARPA, AFRL

--- sys/i386/linux/linux_sysvec.c
+++ sys/i386/linux/linux_sysvec.c
@@ -261,8 +261,7 @@ linux_fixup_elf(register_t **stack_base, struct image_params *imgp)
 
 	free(imgp->auxargs, M_TEMP);
 	imgp->auxargs = NULL;
-	KASSERT((pos - argarray) / sizeof(*pos) <= LINUX_AT_COUNT,
-	    ("Too many auxargs"));
+	KASSERT(pos - argarray <= LINUX_AT_COUNT, ("Too many auxargs"));
 
 	error = copyout(argarray, auxbase, sizeof(*argarray) * LINUX_AT_COUNT);
 	free(argarray, M_TEMP);
