9442a64e531373871f608427031700db475d00f7
Rick Macklem <rmacklem@FreeBSD.org>
2018-06-01 19:47:41 +0000

    Add the BindConnectiontoSession operation to the NFSv4.1 server.
    Under some fairly unusual circumstances, the Linux NFSv4.1 client is
    doing a BindConnectiontoSession operation for TCP connections.
    It is also used by the ESXi6.5 NFSv4.1 client.
    This patch adds this operation to the NFSv4.1 server.
    
    Reported by:	andreas.nagy@frequentis.com
    Tested by:	andreas.nagy@frequentis.com
    MFC after:	2 weeks

--- sys/fs/nfs/nfsproto.h
+++ sys/fs/nfs/nfsproto.h
@@ -659,6 +659,15 @@
 #define	NFSFLEXFLAG_NO_READIO		0x00000004
 #define	NFSFLEXFLAG_WRITE_ONEMIRROR	0x00000008
 
+/* Enum values for Bind Connection to Session. */
+#define	NFSCDFC4_FORE		0x1
+#define	NFSCDFC4_BACK		0x2
+#define	NFSCDFC4_FORE_OR_BOTH	0x3
+#define	NFSCDFC4_BACK_OR_BOTH	0x7
+#define	NFSCDFS4_FORE		0x1
+#define	NFSCDFS4_BACK		0x2
+#define	NFSCDFS4_BOTH		0x3
+
 /* Conversion macros */
 #define	vtonfsv2_mode(t,m) 						\
 		txdr_unsigned(((t) == VFIFO) ? MAKEIMODE(VCHR, (m)) : 	\
