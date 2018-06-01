cfb0dfa3c2902b23f5b21fd7cee95f38e93a4499
Matt Macy <mmacy@FreeBSD.org>
2018-06-01 00:45:48 +0000

    libpmc: Intel doesn't require runtime counter table init

--- lib/libpmc/libpmc.c
+++ lib/libpmc/libpmc.c
@@ -1534,6 +1534,9 @@ pmc_init(void)
 		 * about.  This shouldn't happen since the abi version check
 		 * should have caught this.
 		 */
+#if defined(__amd64__) || defined(__i386__)
+		break;
+#endif
 		errno = ENXIO;
 		return (pmc_syscall = -1);
 	}
