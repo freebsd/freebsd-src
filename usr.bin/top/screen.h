a8efb181f4d494eb27bebac65ddc1f4c956071ce
eadler <eadler@FreeBSD.org>
2018-06-02 07:44:53 +0000

    top(1): remove wrapper around putchar
    This appears to have been written for portability which we no longer
    need.

--- usr.bin/top/screen.h
+++ usr.bin/top/screen.h
@@ -7,7 +7,7 @@
  *  $FreeBSD$
  */
 
-#define TCputs(str)	tputs(str, 1, putstdout)
+#define TCputs(str)	tputs(str, 1, putchar)
 #define putcap(str)	(void)((str) != NULL ? TCputs(str) : 0)
 #define Move_to(x, y)	TCputs(tgoto(cursor_motion, x, y))
 
@@ -26,7 +26,6 @@ extern int  screen_length;
 extern int  screen_width;
 
 /* a function that puts a single character on stdout */
-int		putstdout(int ch);
 int		clear_eol(int len);
 void	top_standout(char *msg);
 void	top_clear(void);
