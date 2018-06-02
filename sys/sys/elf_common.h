cd7fc4fef9b18dc78eabf45ee51cd924ce96d678
jhibbits <jhibbits@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-06-02 20:28:58 +0000

    Included VSX registers in powerpc core dumps
    Summary: Included VSX registers in powerpc core dumps (both kernel and gcore)
    
    Submitted by:	Luis Pires
    Differential Revision: https://reviews.freebsd.org/D15512
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@334538 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/elf_common.h
+++ sys/sys/elf_common.h
@@ -774,6 +774,7 @@ typedef struct {
 #define	NT_PROCSTAT_AUXV	16	/* Procstat auxv data. */
 #define	NT_PTLWPINFO		17	/* Thread ptrace miscellaneous info. */
 #define	NT_PPC_VMX	0x100	/* PowerPC Altivec/VMX registers */
+#define	NT_PPC_VSX	0x102	/* PowerPC VSX registers */
 #define	NT_X86_XSTATE	0x202	/* x86 XSAVE extended state. */
 #define	NT_ARM_VFP	0x400	/* ARM VFP registers */
 
