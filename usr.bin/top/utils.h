0d7f83c56e3995ecb0b15d705cacabdecf849f54
eadler <eadler@FreeBSD.org>
2018-06-03 23:40:54 +0000

    top(1): another pass of cleanup
    - avoid the need to call a function to get size of known array. I'll
    likely re-arrange some of the indirect in a later to avoid the magic
    constants.
    - use correct type
    - add const
    - replace caddr_t with void*. This corrects an alignment warning.
    - remove duplicated include from immediately prior commit
    
    Under base clang we're now down to:
    - 3 warning in top.c, 1 warning in mahcine.c,  4 warning in display.c,
    - 1 warning in utils.c
    
    Tested with base clang, gcc7, gcc9, base gcc (mips)

--- usr.bin/top/utils.h
+++ usr.bin/top/utils.h
@@ -16,11 +16,11 @@ int atoiwi(const char *);
 char *itoa(unsigned int);
 char *itoa7(int);
 int digits(int);
-char **argparse(char *, int *);
+const char * const *argparse(const char *, int *);
 long percentages(int, int *, long *, long *, long *);
 char *format_time(long);
 char *format_k(int);
 char *format_k2(unsigned long long);
-int string_index(const char *string, char *array[]);
+int string_index(const char *string, const char * const *array);
 int find_pid(pid_t pid);
 
