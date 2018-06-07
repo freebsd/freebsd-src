cebd2199bcd54e8dd78d2db27554968097dc093e
Hans Petter Selasky <hselasky@FreeBSD.org>
2018-06-07 07:49:18 +0000

    MFC r334429: Implement support for the PCI_BUS_NUM() function macro in the LinuxKPI.
    Submitted by:	Johannes Lundberg <johalun0@gmail.com>
    Sponsored by:	Mellanox Technologies

--- sys/compat/linuxkpi/common/include/linux/pci.h
+++ sys/compat/linuxkpi/common/include/linux/pci.h
@@ -98,6 +98,7 @@ struct pci_device_id {
 #define PCI_DEVFN(slot, func)   ((((slot) & 0x1f) << 3) | ((func) & 0x07))
 #define PCI_SLOT(devfn)		(((devfn) >> 3) & 0x1f)
 #define PCI_FUNC(devfn)		((devfn) & 0x07)
+#define	PCI_BUS_NUM(devfn)	(((devfn) >> 8) & 0xff)
 
 #define PCI_VDEVICE(_vendor, _device)					\
 	    .vendor = PCI_VENDOR_ID_##_vendor, .device = (_device),	\
