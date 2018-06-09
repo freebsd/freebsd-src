03be3548f2d4ec781af7b2233e7d56d3ee642890
ae <ae@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-06-09 09:57:14 +0000

    Explicitly change the link state when we assingn an address.
    Since we are setting IFF_UP flag on SIOCSIFADDR, it is possible, that
    after this link state information still not initialized properly.
    This leads to problems with routing, since now interface has
    IFCAP_LINKSTATE capability and a route is considered as working only
    when interface's link state is in LINK_STATE_UP (see RT_LINK_IS_UP()
    macro).
    
    Reported by:	Marek Zarychta
    MFC after:	3 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@334875 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net/if_loop.c
+++ sys/net/if_loop.c
@@ -380,6 +380,7 @@ loioctl(struct ifnet *ifp, u_long cmd, caddr_t data)
 	case SIOCSIFADDR:
 		ifp->if_flags |= IFF_UP;
 		ifp->if_drv_flags |= IFF_DRV_RUNNING;
+		if_link_state_change(ifp, LINK_STATE_UP);
 		/*
 		 * Everything else is done at a higher level.
 		 */
