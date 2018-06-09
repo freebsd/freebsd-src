d8b8932e04603e6434202e7d0ac5099fee696b75
rmacklem <rmacklem@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-06-09 23:14:59 +0000

    Delete an unused macro and clean up a comment about it.
    NFSDEV_MIRRORSTR was defined for the pNFS server, but has not been used,
    so this patch deletes it. It also cleans up the comment and hopefully
    makes it more readable.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@334902 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/fs/nfs/nfs.h
+++ sys/fs/nfs/nfs.h
@@ -179,13 +179,10 @@ struct nfsd_nfsd_args {
 };
 
 /*
- * NFSDEV_MIRRORSTR - string of digits that number the DSs 0->999.
- * (To support more than 1000 DSs on an MDS, this needs to be increased.)
- * NFSDEV_MAXMIRRORS - Maximum # of mirrors for a DS.
- * (Most will only have a single mirror, but this setting allows up to 3.)
+ * NFSDEV_MAXMIRRORS - Maximum level of mirroring for a DS.
+ * (Most will only put files on two DSs, but this setting allows up to 4.)
  * NFSDEV_MAXVERS - maximum number of NFS versions supported by Flex File.
  */
-#define	NFSDEV_MIRRORSTR	3
 #define	NFSDEV_MAXMIRRORS	4
 #define	NFSDEV_MAXVERS		4
 
