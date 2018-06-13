7e4718237731a09a9406bd644e31d6d39b63f05b
araujo <araujo@FreeBSD.org>
2018-06-13 07:02:58 +0000

    Add SPDX tags to vmm(4).
    MFC after:	4 weeks.
    Sponsored by:	iXsystems Inc.

--- sys/amd64/vmm/io/vatpit.h
+++ sys/amd64/vmm/io/vatpit.h
@@ -1,4 +1,6 @@
 /*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
  * Copyright (c) 2014 Tycho Nightingale <tycho.nightingale@pluribusnetworks.com>
  * Copyright (c) 2011 NetApp, Inc.
  * All rights reserved.
