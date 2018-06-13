4e050d14e00cd99387acf84fe727c4e1423ca22a
Andrew Turner <andrew@FreeBSD.org>
2018-06-13 15:58:33 +0000

    Add ThunderX2 to the list of CPUs we need to apply the branch predictor hardening to.
    Sponsored by:	DARPA, AFRL

--- sys/arm64/arm64/cpu_errata.c
+++ sys/arm64/arm64/cpu_errata.c
@@ -73,6 +73,12 @@ static struct cpu_quirks cpu_quirks[] = {
 		.midr_value = CPU_ID_RAW(CPU_IMPL_ARM, CPU_PART_CORTEX_A75,0,0),
 		.quirk_install = install_psci_bp_hardening,
 	},
+	{
+		.midr_mask = CPU_IMPL_MASK | CPU_PART_MASK,
+		.midr_value =
+		    CPU_ID_RAW(CPU_IMPL_CAVIUM, CPU_PART_THUNDERX2, 0,0),
+		.quirk_install = install_psci_bp_hardening,
+	},
 };
 
 static void
