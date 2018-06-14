210d8d4b92e0873988679b62217e4a310a6762f8
tsoome <tsoome@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-06-14 16:11:50 +0000

    libsa: open() should use NULL instead of typecasted 0
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@335148 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- stand/libsa/open.c
+++ stand/libsa/open.c
@@ -103,11 +103,11 @@ open(const char *fname, int mode)
 
 	f = &files[fd];
 	f->f_flags = mode + 1;
-	f->f_dev = (struct devsw *)0;
-	f->f_ops = (struct fs_ops *)0;
+	f->f_dev = NULL;
+	f->f_ops = NULL;
 	f->f_offset = 0;
 	f->f_devdata = NULL;
-	file = (char *)0;
+	file = NULL;
 
 	if (exclusive_file_system != NULL) {
 		fs = exclusive_file_system;
@@ -119,11 +119,11 @@ open(const char *fname, int mode)
 
 	error = devopen(f, fname, &file);
 	if (error ||
-	    (((f->f_flags & F_NODEV) == 0) && f->f_dev == (struct devsw *)0))
+	    (((f->f_flags & F_NODEV) == 0) && f->f_dev == NULL))
 		goto err;
 
 	/* see if we opened a raw device; otherwise, 'file' is the file name. */
-	if (file == (char *)0 || *file == '\0') {
+	if (file == NULL || *file == '\0') {
 		f->f_flags |= F_RAW;
 		f->f_rabuf = NULL;
 		return (fd);
