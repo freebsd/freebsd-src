7575d3df5b3dd239d39381efebc9e6e80bac62db
Michael Tuexen <tuexen@FreeBSD.org>
2018-06-18 18:35:29 +0000

    The IP, TCP, and UDP provider report IP addresses as strings. In some cases, the required information is not available and the UDP provider reported an empty string in this case and the IP and TCP provider reported a NULL pointer.
    This patch changes the value provided in this case to the string
    "<unknown>". This make the behaviour consistent and in-line with
    the behaviour of Solaris.
    
    Reviewed by:		markj@, dteske@, gnn@
    Differential Revision:	https://reviews.freebsd.org/D15855

--- cddl/lib/libdtrace/udp.d
+++ cddl/lib/libdtrace/udp.d
@@ -56,11 +56,11 @@ translator udpsinfo_t < struct inpcb *p > {
 	udps_addr =	(uintptr_t)p;
 	udps_lport =	p == NULL ? 0 : ntohs(p->inp_inc.inc_ie.ie_lport);
 	udps_rport =	p == NULL ? 0 : ntohs(p->inp_inc.inc_ie.ie_fport);
-	udps_laddr =	p == NULL ? "" :
+	udps_laddr =	p == NULL ? "<unknown>" :
 	    p->inp_vflag == INP_IPV4 ?
 	    inet_ntoa(&p->inp_inc.inc_ie.ie_dependladdr.id46_addr.ia46_addr4.s_addr) :
 	    inet_ntoa6(&p->inp_inc.inc_ie.ie_dependladdr.id6_addr);
-	udps_raddr =	p == NULL ? "" :
+	udps_raddr =	p == NULL ? "<unknown>" :
 	    p->inp_vflag == INP_IPV4 ?
 	    inet_ntoa(&p->inp_inc.inc_ie.ie_dependfaddr.id46_addr.ia46_addr4.s_addr) :
 	    inet_ntoa6(&p->inp_inc.inc_ie.ie_dependfaddr.id6_addr);
