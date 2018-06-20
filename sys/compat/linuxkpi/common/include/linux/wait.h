031fe0a73c6cc4aa5fb391787dd094d01a6488e0
Hans Petter Selasky <hselasky@FreeBSD.org>
2018-06-20 06:49:26 +0000

    MFC r334715: Implement the might_sleep_if() function macro in the LinuxKPI.
    Submitted by:	Johannes Lundberg <johalun0@gmail.com>
    Sponsored by:	Mellanox Technologies
    Sponsored by:	Limelight Networks

--- sys/compat/linuxkpi/common/include/linux/wait.h
+++ sys/compat/linuxkpi/common/include/linux/wait.h
@@ -47,6 +47,10 @@
 #define	might_sleep()							\
 	WITNESS_WARN(WARN_GIANTOK | WARN_SLEEPOK, NULL, "might_sleep()")
 
+#define	might_sleep_if(cond) do { \
+	if (cond) { might_sleep(); } \
+} while (0)
+
 struct wait_queue;
 struct wait_queue_head;
 
