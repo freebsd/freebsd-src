3d107bf2df34431fec2b144321c606d599fcc0a5
glebius <glebius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-06-23 06:53:53 +0000

    Revert r334843, and partially revert r335180.
    tcp_outflags[] were defined since 4BSD and are defined nowadays in
    all its descendants. Removing them breaks third party application.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@335577 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/tcp_stacks/rack.c
+++ sys/netinet/tcp_stacks/rack.c
@@ -76,6 +76,7 @@ __FBSDID("$FreeBSD$");
 #include <netinet6/in6_pcb.h>
 #include <netinet6/ip6_var.h>
 #include <netinet/tcp.h>
+#define	TCPOUTFLAGS
 #include <netinet/tcp_fsm.h>
 #include <netinet/tcp_log_buf.h>
 #include <netinet/tcp_seq.h>
