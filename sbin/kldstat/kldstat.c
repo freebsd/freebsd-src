4558c9a75a428068810e270ee7c7e4c841af4759
tuexen <tuexen@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-07-02 09:11:30 +0000

    Keep table aligned when address pointer is not of max length.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@335859 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/kldstat/kldstat.c
+++ sbin/kldstat/kldstat.c
@@ -77,11 +77,13 @@ printfile(int fileid, int verbose, int humanized)
 		humanize_number(buf, sizeof(buf), stat.size,
 		    "", HN_AUTOSCALE, HN_DECIMAL | HN_NOSPACE);
 
-		printf("%2d %4d %p %5s %s",
-		    stat.id, stat.refs, stat.address, buf, stat.name);
+		printf("%2d %4d %*p %5s %s",
+		    stat.id, stat.refs, POINTER_WIDTH, stat.address, buf,
+		    stat.name);
 	} else {
-		printf("%2d %4d %p %8zx %s",
-		    stat.id, stat.refs, stat.address, stat.size, stat.name);
+		printf("%2d %4d %*p %8zx %s",
+		    stat.id, stat.refs, POINTER_WIDTH, stat.address, stat.size,
+		    stat.name);
 	}
     }
 
