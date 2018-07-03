d7ac1268f3a949b167cec18d7ec252766c0db228
bdrewery <bdrewery@FreeBSD.org>
2018-07-03 20:03:19 +0000

    Fix r335906+r335907 to not pass multiple source files to genoffset_test.o target.
    This is working around a bug with the FAST_DEPEND logic with custom targets
    that use .ALLSRC.
    
    Reported by:	mmacy

--- sys/conf/kern.post.mk
+++ sys/conf/kern.post.mk
@@ -195,8 +195,8 @@ genoffset.o: $S/kern/genoffset.c
 # is to exercise the CTASSERT that checks that the offsets in the offset.inc
 # _lite struct(s) match those in the original(s). 
 genoffset_test.o: $S/kern/genoffset.c offset.inc
-	${CC} -c ${CFLAGS:N-flto:N-fno-common} -DOFFSET_TEST ${.ALLSRC:M*.c} \
-	    -o ${.TARGET}
+	${CC} -c ${CFLAGS:N-flto:N-fno-common} -DOFFSET_TEST \
+	    $S/kern/genoffset.c -o ${.TARGET}
 
 assym.inc: $S/kern/genassym.sh genassym.o genoffset_test.o
 	NM='${NM}' NMFLAGS='${NMFLAGS}' sh $S/kern/genassym.sh genassym.o > ${.TARGET}
