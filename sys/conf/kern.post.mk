8d54a405c0dbce005facd8308ddd8b40b1e4da5a
bdrewery <bdrewery@FreeBSD.org>
2018-07-03 19:24:44 +0000

    No need to make a copy to build genoffset_test.o

--- sys/conf/kern.post.mk
+++ sys/conf/kern.post.mk
@@ -191,14 +191,12 @@ offset.inc: $S/kern/genoffset.sh genoffset.o
 genoffset.o: $S/kern/genoffset.c
 	${CC} -c ${CFLAGS:N-flto:N-fno-common} $S/kern/genoffset.c
 
-genoffset_test.c: $S/kern/genoffset.c
-	cp $S/kern/genoffset.c genoffset_test.c
-
 # genoffset_test.o is not actually used for anything - the point of compiling it
 # is to exercise the CTASSERT that checks that the offsets in the offset.inc
 # _lite struct(s) match those in the original(s). 
-genoffset_test.o: genoffset_test.c offset.inc
-	${CC} -c ${CFLAGS:N-flto:N-fno-common} -DOFFSET_TEST genoffset_test.c
+genoffset_test.o: $S/kern/genoffset.c offset.inc
+	${CC} -c ${CFLAGS:N-flto:N-fno-common} -DOFFSET_TEST ${.ALLSRC:M*.c} \
+	    -o ${.TARGET}
 
 assym.inc: $S/kern/genassym.sh genassym.o genoffset_test.o
 	NM='${NM}' NMFLAGS='${NMFLAGS}' sh $S/kern/genassym.sh genassym.o > ${.TARGET}
