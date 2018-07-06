d0907e98578c83d305a97b64fffac0a75eed7479
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-07-06 19:50:25 +0000

    Revert to recommit with the proper message.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@336046 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/amd64/include/pcpu.h
+++ sys/amd64/include/pcpu.h
@@ -76,7 +76,8 @@
 	uint32_t pc_pcid_gen;						\
 	uint32_t pc_smp_tlb_done;	/* TLB op acknowledgement */	\
 	uint32_t pc_ibpb_set;						\
-	char	__pad[3288]		/* pad to UMA_PCPU_ALLOC_SIZE */
+	char	__pad[216]		/* be divisor of PAGE_SIZE	\
+					   after cache alignment */
 
 #define	PC_DBREG_CMD_NONE	0
 #define	PC_DBREG_CMD_LOAD	1
