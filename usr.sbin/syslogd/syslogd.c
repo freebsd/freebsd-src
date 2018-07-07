22f435591d098b7633d6940f8bb473f534125b26
ed <ed@FreeBSD.org>
2018-07-07 11:38:44 +0000

    MFC r335861:
      Restore the order in which RFC 3164 messages with fac/pri are formatted.
    
      The refactoring of the syslogd code to format messages using iovecs
      slightly altered the output of syslogd by placing the facility/priority
      after the hostname, as opposed to printing it right before. This change
      reverts the behaviour to be consistent with how it was before.
    
    PR:		229457

--- usr.sbin/syslogd/syslogd.c
+++ usr.sbin/syslogd/syslogd.c
@@ -1871,8 +1871,6 @@ fprintlog_rfc3164(struct filed *f, const char *hostname, const char *app_name,
 		/* Message written to files. */
 		iovlist_append(&il, timebuf);
 		iovlist_append(&il, " ");
-		iovlist_append(&il, hostname);
-		iovlist_append(&il, " ");
 
 		if (LogFacPri) {
 			iovlist_append(&il, "<");
@@ -1916,6 +1914,9 @@ fprintlog_rfc3164(struct filed *f, const char *hostname, const char *app_name,
 
 			iovlist_append(&il, "> ");
 		}
+
+		iovlist_append(&il, hostname);
+		iovlist_append(&il, " ");
 		break;
 	}
 
