6b092c42a4cc62683b3db8913ba811bf0b997bd2
Konstantin Belousov <kib@FreeBSD.org>
2018-07-13 21:30:18 +0000

    MFC r335969,r335996,r335999,r336008,r336010: Improvements to x86 pmap_extract_and_hold().

--- sys/i386/i386/pmap.c
+++ sys/i386/i386/pmap.c
@@ -1536,9 +1536,7 @@ retry:
 				if (vm_page_pa_tryrelock(pmap, (pde &
 				    PG_PS_FRAME) | (va & PDRMASK), &pa))
 					goto retry;
-				m = PHYS_TO_VM_PAGE((pde & PG_PS_FRAME) |
-				    (va & PDRMASK));
-				vm_page_hold(m);
+				m = PHYS_TO_VM_PAGE(pa);
 			}
 		} else {
 			ptep = pmap_pte(pmap, va);
@@ -1549,10 +1547,11 @@ retry:
 				if (vm_page_pa_tryrelock(pmap, pte & PG_FRAME,
 				    &pa))
 					goto retry;
-				m = PHYS_TO_VM_PAGE(pte & PG_FRAME);
-				vm_page_hold(m);
+				m = PHYS_TO_VM_PAGE(pa);
 			}
 		}
+		if (m != NULL)
+			vm_page_hold(m);
 	}
 	PA_UNLOCK_COND(pa);
 	PMAP_UNLOCK(pmap);
