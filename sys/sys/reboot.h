9b7d97ffbeeace51bfe0a7663c5ecfa5de9db082
imp <imp@FreeBSD.org>
2018-07-13 16:43:05 +0000

    Create helper functions for parsing boot args.
    boot_parse_arg		to parse a single arg
    boot_parse_cmdline	to parse a command line string
    boot_parse_args		to parse all the args in a vector
    boot_howto_to_env	Convert howto bits to env vars
    boot_env_to_howto	Return howto mask mased on what's set in the environment.
    
    All these routines return an int that's the bitmask of the args
    translated to RB_* flags. As a special case, the 'S' flag sets the
    comconsole_speed env var. Any arg that looks like a=b will set the env
    key 'a' to value 'b'. If =b is omitted, 'a' is set to '1'.  This
    should help us reduce the number of redundant copies of these routines
    in the tree.  It should also give a more uniform experience between
    platforms.
    
    Also, invent a new flag RB_PROBE that's set when 'P' is parsed.  On
    x86 + BIOS, this means 'probe for the keyboard, and if it's not there
    set both RB_MULTIPLE and RB_SERIAL (which means show the output on
    both video and serial consoles, but make serial primary).  Others it
    may be some similar concept of probing, but it's loader dependent
    what, exactly, it means.
    
    These routines are suitable for /boot/loader and/or the kernel,
    though they may not be suitable for the tightly hand-rolled-for-space
    environments like boot2.
    
    Sponsored by: Netflix
    Differential Revision: https://reviews.freebsd.org/D16205

--- sys/sys/reboot.h
+++ sys/sys/reboot.h
@@ -63,6 +63,7 @@
 #define	RB_PAUSE	0x100000 /* pause after each output line during probe */
 #define	RB_REROOT	0x200000 /* unmount the rootfs and mount it again */
 #define	RB_POWERCYCLE	0x400000 /* Power cycle if possible */
+#define	RB_PROBE	0x10000000	/* Probe multiple consoles */
 #define	RB_MULTIPLE	0x20000000	/* use multiple consoles */
 
 #define	RB_BOOTINFO	0x80000000	/* have `struct bootinfo *' arg */
--- sys/x86/xen/pv.c
+++ sys/x86/xen/pv.c
@@ -305,21 +305,6 @@ xen_pv_set_env(void)
 	init_static_kenv(cmd_line, 0);
 }
 
-static void
-xen_pv_set_boothowto(void)
-{
-	int i;
-	char *env;
-
-	/* get equivalents from the environment */
-	for (i = 0; howto_names[i].ev != NULL; i++) {
-		if ((env = kern_getenv(howto_names[i].ev)) != NULL) {
-			boothowto |= howto_names[i].mask;
-			freeenv(env);
-		}
-	}
-}
-
 #ifdef DDB
 /*
  * The way Xen loads the symtab is different from the native boot loader,
@@ -418,7 +403,7 @@ xen_pv_parse_preload_data(u_int64_t modulep)
 	} else {
 		/* Parse the extra boot information given by Xen */
 		xen_pv_set_env();
-		xen_pv_set_boothowto();
+		boothowto |= boot_env_to_howto();
 		kmdp = NULL;
 	}
 
