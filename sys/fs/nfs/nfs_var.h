aa1d64186eb5d87cb1e0a56e85a048ec9ab43ed1
rmacklem <rmacklem@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-07-14 19:51:44 +0000

    Fix the pNFS client when mirrors aren't on the same machine.
    Without this patch, the client side NFSv4.1 pNFS code erroneously did writes
    and commits to both DS mirrors using the TCP connection of the first one.
    For my test setup this worked, since I have both DSs running on the same
    machine, but it would have failed when the DSs are on separate machines.
    This patch fixes the code to use the correct TCP connection for each DS.
    This patch should only affect the NFSv4.1 client when using "pnfs" mounts
    to mirrored DSs.
    
    MFC after:	2 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@336292 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/fs/nfs/nfs_var.h
+++ sys/fs/nfs/nfs_var.h
@@ -607,7 +607,7 @@ void nfscl_rellayout(struct nfscllayout *, int);
 struct nfscldevinfo *nfscl_getdevinfo(struct nfsclclient *, uint8_t *,
     struct nfscldevinfo *);
 void nfscl_reldevinfo(struct nfscldevinfo *);
-int nfscl_adddevinfo(struct nfsmount *, struct nfscldevinfo *,
+int nfscl_adddevinfo(struct nfsmount *, struct nfscldevinfo *, int,
     struct nfsclflayout *);
 void nfscl_freelayout(struct nfscllayout *);
 void nfscl_freeflayout(struct nfsclflayout *);
