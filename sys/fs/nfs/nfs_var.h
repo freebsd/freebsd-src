5da3882447c722f6be6362d08f62c21de9779f26
Rick Macklem <rmacklem@FreeBSD.org>
2018-07-15 18:54:44 +0000

    Shut down the TCP connection to a DS in the pNFS client when Renew fails.
    When a NFSv4.1 client mount using pNFS detects a failure trying to do a
    Renew (actually just a Sequence operation), the code would simply try
    again and again and again every 30sec.
    This would tie up the "nfscl" thread, which should also be doing other
    things like Renews on other DSs and the MDS.
    This patch adds code which closes down the TCP connection and marks it
    defunct when Renew detects an failure to communicate with the DS, so
    further Renews will not be attempted until a new working TCP connection to
    the DS is established.
    It also makes the call to nfscl_cancelreqs() unconditional, since
    nfscl_cancelreqs() checks the NFSCLDS_SAMECONN flag and does so while holding
    the lock.
    This fix only applies to the NFSv4.1 client whne using pNFS and without it
    the only effect would have been an "nfscl" thread busy doing Renew attempts
    on an unresponsive DS.
    
    MFC after:	2 weeks

--- sys/fs/nfs/nfs_var.h
+++ sys/fs/nfs/nfs_var.h
@@ -603,6 +603,7 @@ struct nfscllayout *nfscl_getlayout(struct nfsclclient *, uint8_t *, int,
     uint64_t, struct nfsclflayout **, int *);
 void nfscl_dserr(uint32_t, uint32_t, struct nfscldevinfo *,
     struct nfscllayout *, struct nfsclds *);
+void nfscl_cancelreqs(struct nfsclds *);
 void nfscl_rellayout(struct nfscllayout *, int);
 struct nfscldevinfo *nfscl_getdevinfo(struct nfsclclient *, uint8_t *,
     struct nfscldevinfo *);
