debf8b839b2f793c78eeb7156ad8e6b1e86c52d2
Warner Losh <imp@FreeBSD.org>
2018-07-17 20:11:52 +0000

    Fix typo in the command summary.
    Of course, I can't get the command to work, but it's a start...

--- stand/efi/libefi/env.c
+++ stand/efi/libefi/env.c
@@ -118,7 +118,7 @@ command_efi_show(int argc, char *argv[])
 	 *	print all the env vars tagged with UUID
 	 * efi-show -v var
 	 *	search all the env vars and print the ones matching var
-	 * eif-show -u UUID -v var
+	 * eif-show -g UUID -v var
 	 * eif-show UUID var
 	 *	print all the env vars that match UUID and var
 	 */
