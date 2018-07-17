fd83d3c1d3e50dfb05afc55feb8f69c6f8578116
markj <markj@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-07-17 14:56:54 +0000

    Add a FALLTHROUGH comment to kvprintf().
    Submitted by:	Sebastian Huber <sebastian.huber@embedded-brains.de>
    MFC after:	3 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@336417 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/subr_prf.c
+++ sys/kern/subr_prf.c
@@ -716,6 +716,7 @@ reswitch:	switch (ch = (u_char)*fmt++) {
 				padc = '0';
 				goto reswitch;
 			}
+			/* FALLTHROUGH */
 		case '1': case '2': case '3': case '4':
 		case '5': case '6': case '7': case '8': case '9':
 				for (n = 0;; ++fmt) {
