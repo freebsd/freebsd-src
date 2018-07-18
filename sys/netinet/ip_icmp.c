4b9f4bff13e99b96a7ecefd3254c6481e2a6e47a
rrs <rrs@FreeBSD.org>
2018-07-18 22:49:53 +0000

    Bump the ICMP echo limits to match the RFC
    Reviewed by:	tuexen
    Sponsored by: Netflix Inc.
    Differential Revision:		https://reviews.freebsd.org/D16333

--- sys/netinet/ip_icmp.c
+++ sys/netinet/ip_icmp.c
@@ -139,8 +139,8 @@ static VNET_DEFINE(int, icmp_rfi) = 0;
 SYSCTL_INT(_net_inet_icmp, OID_AUTO, reply_from_interface, CTLFLAG_VNET | CTLFLAG_RW,
 	&VNET_NAME(icmp_rfi), 0,
 	"ICMP reply from incoming interface for non-local packets");
-
-static VNET_DEFINE(int, icmp_quotelen) = 8;
+/* Router requirements RFC 1812 section 4.3.2.3 requires 576 - 28. */
+static VNET_DEFINE(int, icmp_quotelen) = 548;
 #define	V_icmp_quotelen			VNET(icmp_quotelen)
 SYSCTL_INT(_net_inet_icmp, OID_AUTO, quotelen, CTLFLAG_VNET | CTLFLAG_RW,
 	&VNET_NAME(icmp_quotelen), 0,
