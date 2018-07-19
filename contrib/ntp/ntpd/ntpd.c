3496c981ac86b0541bdbc9a211f7847a97df008d
Ian Lepore <ian@FreeBSD.org>
2018-07-19 23:55:29 +0000

    Make it possible to run ntpd as a non-root user, add ntpd uid and gid.
    Code analysis and runtime analysis using truss(8) indicate that the only
    privileged operations performed by ntpd are adjusting system time, and
    (re-)binding to privileged UDP port 123. These changes add a new mac(4)
    policy module, mac_ntpd(4), which grants just those privileges to any
    process running with uid 123.
    
    This also adds a new user and group, ntpd:ntpd, (uid:gid 123:123), and makes
    them the owner of the /var/db/ntp directory, so that it can be used as a
    location where the non-privileged daemon can write files such as the
    driftfile, and any optional logfile or stats files.
    
    Because there are so many ways to configure ntpd, the question of how to
    configure it to run without root privs can be a bit complex, so that will be
    addressed in a separate commit. These changes are just what's required to
    grant the limited subset of privs to ntpd, and the small change to ntpd to
    prevent it from exiting with an error if running as non-root.
    
    Differential Revision:	https://reviews.freebsd.org/D16281

--- contrib/ntp/ntpd/ntpd.c
+++ contrib/ntp/ntpd/ntpd.c
@@ -123,6 +123,9 @@
 #if defined(HAVE_PRIV_H) && defined(HAVE_SOLARIS_PRIVS)
 # include <priv.h>
 #endif /* HAVE_PRIV_H */
+#if defined(HAVE_TRUSTEDBSD_MAC)
+# include <sys/mac.h>
+#endif /* HAVE_TRUSTEDBSD_MAC */
 #endif /* HAVE_DROPROOT */
 
 #if defined (LIBSECCOMP) && (KERN_SECCOMP)
@@ -634,7 +637,12 @@ ntpdmain(
 	/* MPE lacks the concept of root */
 # if defined(HAVE_GETUID) && !defined(MPE)
 	uid = getuid();
-	if (uid && !HAVE_OPT( SAVECONFIGQUIT )) {
+	if (uid && !HAVE_OPT( SAVECONFIGQUIT )
+#  if defined(HAVE_TRUSTEDBSD_MAC)
+	    /* We can run as non-root if the mac_ntpd policy is enabled. */
+	    && mac_is_present("ntpd") != 1
+#  endif
+	    ) {
 		msyslog_term = TRUE;
 		msyslog(LOG_ERR,
 			"must be run as root, not uid %ld", (long)uid);
@@ -1082,7 +1090,17 @@ getgroup:
 			exit (-1);
 		}
 
-#  if !defined(HAVE_LINUX_CAPABILITIES) && !defined(HAVE_SOLARIS_PRIVS)
+#  if defined(HAVE_TRUSTEDBSD_MAC)
+		/*
+		 * To manipulate system time and (re-)bind to NTP_PORT as needed
+		 * following interface changes, we must either run as uid 0 or
+		 * the mac_ntpd policy module must be enabled.
+		 */
+		if (sw_uid != 0 && mac_is_present("ntpd") != 1) {
+			msyslog(LOG_ERR, "Need MAC 'ntpd' policy enabled to drop root privileges");
+			exit (-1);
+		}
+#  elif !defined(HAVE_LINUX_CAPABILITIES) && !defined(HAVE_SOLARIS_PRIVS)
 		/*
 		 * for now assume that the privilege to bind to privileged ports
 		 * is associated with running with uid 0 - should be refined on
