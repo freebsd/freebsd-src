07c2711fbf96d1b070b319b2d7f331430fc5ec2d
Roger Pau Monné <royger@FreeBSD.org>
2018-07-19 08:13:41 +0000

    xen: allow very early initialization of the hypercall page
    Allow the hypercall page to be initialized very early, even before
    vtophys is functional. Also make the function global so it can be
    called by other files.
    
    This will be needed in order to perform the early bringup on PVHv2
    guests.
    
    Sponsored by: Citrix Systems R&D

--- sys/xen/hvm.h
+++ sys/xen/hvm.h
@@ -91,6 +91,14 @@ enum {
     (((uint64_t)HVM_CB_TYPE_VECTOR << HVM_CB_TYPE_SHIFT) \
    | (((vector) & HVM_CB_GSI_GSI_MASK) << HVM_CB_GSI_GSI_SHIFT))
 
+enum xen_hvm_init_type {
+	XEN_HVM_INIT_EARLY,
+	XEN_HVM_INIT_LATE,
+	XEN_HVM_INIT_CANCELLED_SUSPEND,
+	XEN_HVM_INIT_RESUME,
+};
+
+int xen_hvm_init_hypercall_stubs(enum xen_hvm_init_type);
 void xen_hvm_set_callback(device_t);
 void xen_hvm_suspend(void);
 void xen_hvm_resume(bool suspend_cancelled);
