650f54b65a6d9fe84953dad40d24ede7ebf156d8
mmacy <mmacy@FreeBSD.org>
2018-07-22 23:32:21 +0000

    Add busy detect quirk to list of console options
    This change allows one to set the busy_detect flag
    required by the synopsys UART at the loader prompt.
    This is needed by the EPYC 3000 SoC.
    
    This will give users a working console up to the point where getty is required:
    hw.uart.console="mm:0xfedc9000,rs:2,bd:1"
    
    Reviewed by:	imp
    MFC after:	4 weeks
    Differential Revision:	https://reviews.freebsd.org/D16399

--- sys/dev/uart/uart_dev_ns8250.c
+++ sys/dev/uart/uart_dev_ns8250.c
@@ -469,6 +469,7 @@ ns8250_bus_attach(struct uart_softc *sc)
 
 	bas = &sc->sc_bas;
 
+	ns8250->busy_detect = bas->busy_detect;
 	ns8250->mcr = uart_getreg(bas, REG_MCR);
 	ns8250->fcr = FCR_ENABLE;
 #ifdef CPU_XBURST
