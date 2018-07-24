64365352c68db2de8c164e5a2ca7f64c797a2ea6
emaste <emaste@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-07-24 21:10:17 +0000

    Revert msdosfs MAKEFS #ifdef changes from r319870
    These changes are not needed for current msdosfs makefs WIP.
    
    Submitted by:	Siva Mahadevan
    Sponsored by:	The FreeBSD Foundation
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@336687 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/fs/msdosfs/denode.h
+++ sys/fs/msdosfs/denode.h
@@ -211,7 +211,7 @@ struct denode {
 	     ((dep)->de_Attributes & ATTR_DIRECTORY) ? 0 : (dep)->de_FileSize), \
 	 putushort((dp)->deHighClust, (dep)->de_StartCluster >> 16))
 
-#if defined(_KERNEL) || defined(MAKEFS)
+#ifdef _KERNEL
 
 #define	VTODE(vp)	((struct denode *)(vp)->v_data)
 #define	DETOV(de)	((de)->de_vnode)
@@ -281,6 +281,5 @@ int deupdat(struct denode *dep, int waitfor);
 int removede(struct denode *pdep, struct denode *dep);
 int detrunc(struct denode *dep, u_long length, int flags, struct ucred *cred);
 int doscheckpath( struct denode *source, struct denode *target);
-#endif	/* _KERNEL || MAKEFS */
+#endif	/* _KERNEL */
 #endif	/* !_FS_MSDOSFS_DENODE_H_ */
-
