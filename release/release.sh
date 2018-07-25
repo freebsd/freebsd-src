ef4083a5729bf885b6fbd1332495a301d549b933
manu <manu@FreeBSD.org>
2018-07-25 21:52:49 +0000

    MFC r336598-r336600, r336721
    r336598:
    release: Uniform release arm image size
    
    Create a 50m fat16 for all boards.
    
    Reviewed by:	gjb
    Differential Revision:	https://reviews.freebsd.org/D16238
    
    r336599:
    release: Add arm_install_boot to install the commit boot bits
    
    This reduce the per-board arm_install_uboot to just install u-boot.
    While here remove the installation of rpi.dtb and rpi2.dtb as we load
    them from the UFS partition via ubldr.
    
    Reviewed by:	gjb, imp (older version)
    Differential Revision:	https://reviews.freebsd.org/D16239
    
    r336600:
    release: Add rpi firmware dtb and mmc overlays for RPI-B and RPI2
    
    This is needed with new u-boot that uses the rpi-firmware dtbs.
    
    Reviewed by:	gjb
    Differential Revision:	https://reviews.freebsd.org/D16240
    
    r336721:
    release: Add OL_DIR forgot in r336600
    
    Reviewed by:	gjb
    Differential Revision:	https://reviews.freebsd.org/D16439
    
    Approved by:	gjb (for insta-mfc of r336721)

--- release/release.sh
+++ release/release.sh
@@ -358,6 +358,21 @@ chroot_build_release() {
 	return 0
 } # chroot_build_release()
 
+efi_boot_name()
+{
+	case $1 in
+		arm)
+			echo "bootarm.efi"
+			;;
+		arm64)
+			echo "bootaa64.efi"
+			;;
+		amd64)
+			echo "bootx86.efi"
+			;;
+	esac
+}
+
 # chroot_arm_build_release(): Create arm SD card image.
 chroot_arm_build_release() {
 	load_target_env
@@ -385,6 +400,7 @@ chroot_arm_build_release() {
 		mdconfig -f ${IMGBASE##${CHROOTDIR}} ${MD_ARGS})
 	arm_create_disk
 	arm_install_base
+	arm_install_boot
 	arm_install_uboot
 	mdconfig -d -u ${mddev}
 	chroot ${CHROOTDIR} rmdir ${DESTDIR}
