ccd6ac9f6e5c1f92c0ce92de1f037a1b5d7716cb
Antoine Brodin <antoine@FreeBSD.org>
2018-07-29 12:41:56 +0000

    Add allow.mlock to jail parameters It allows locking or unlocking physical pages in memory within a jail
    This allows running elasticsearch with "bootstrap.memory_lock" inside a jail
    
    Reviewed by:	jamie@
    Differential Revision:	https://reviews.freebsd.org/D16342

--- sys/sys/jail.h
+++ sys/sys/jail.h
@@ -227,9 +227,10 @@ struct prison_racct {
 #define	PR_ALLOW_MOUNT			0x00000010
 #define	PR_ALLOW_QUOTAS			0x00000020
 #define	PR_ALLOW_SOCKET_AF		0x00000040
+#define	PR_ALLOW_MLOCK			0x00000080
 #define	PR_ALLOW_RESERVED_PORTS		0x00008000
 #define	PR_ALLOW_KMEM_ACCESS		0x00010000	/* reserved, not used yet */
-#define	PR_ALLOW_ALL_STATIC		0x0001807f
+#define	PR_ALLOW_ALL_STATIC		0x000180ff
 
 /*
  * OSD methods
