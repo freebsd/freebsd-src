670d920e944ba824126fe244a8899b88ba181eec
andrew <andrew@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-07-30 14:25:17 +0000

    Ensure the DPCPU and VNET module spaces are aligned to hold a pointer. Previously they may have been aligned to a char, leading to misaligned DPCPU and VNET variables.
    Sponsored by:	DARPA, AFRL
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@336906 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/subr_pcpu.c
+++ sys/kern/subr_pcpu.c
@@ -72,7 +72,7 @@ struct dpcpu_free {
 	TAILQ_ENTRY(dpcpu_free) df_link;
 };
 
-DPCPU_DEFINE_STATIC(char, modspace[DPCPU_MODMIN]);
+DPCPU_DEFINE_STATIC(char, modspace[DPCPU_MODMIN] __aligned(__alignof(void *)));
 static TAILQ_HEAD(, dpcpu_free) dpcpu_head = TAILQ_HEAD_INITIALIZER(dpcpu_head);
 static struct sx dpcpu_lock;
 uintptr_t dpcpu_off[MAXCPU];
