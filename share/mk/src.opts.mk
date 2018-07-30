3f6cd86b65da87409c7cdfdc9c57c33241a42d1f
Ed Maste <emaste@FreeBSD.org>
2018-07-30 15:10:06 +0000

    Revert accidental change from r336908
    By default ld.lld should be the bootstrap linker (only) on i386 right
    now.  Once the i386 exp-run with LLD_IS_LD has a good result this will
    also be enabled by default.
    
    Reported by:	andrew
    Pointy hat to:	emaste

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -309,8 +309,11 @@ __DEFAULT_YES_OPTIONS+=LLVM_LIBUNWIND
 .else
 __DEFAULT_NO_OPTIONS+=LLVM_LIBUNWIND
 .endif
-.if ${__T} == "aarch64" || ${__T} == "amd64" || ${__T} == "i386"
+.if ${__T} == "aarch64" || ${__T} == "amd64"
 __DEFAULT_YES_OPTIONS+=LLD_BOOTSTRAP LLD_IS_LD
+.elif ${__T} == "i386"
+__DEFAULT_YES_OPTIONS+=LLD_BOOTSTRAP
+__DEFAULT_NO_OPTIONS+=LLD_IS_LD
 .else
 __DEFAULT_NO_OPTIONS+=LLD_BOOTSTRAP LLD_IS_LD
 .endif
