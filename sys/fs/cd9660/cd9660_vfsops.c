ff7e658694030dc6125ab1f9264b2f025d89a6c2
emaste <emaste@FreeBSD.org>
2018-07-31 12:36:46 +0000

    cd9660: replace bcopy/bzero with C standard equivalents
    To reduce diffs against NetBSD.

--- sys/fs/cd9660/cd9660_vfsops.c
+++ sys/fs/cd9660/cd9660_vfsops.c
@@ -363,7 +363,7 @@ iso_mountfs(devvp, mp)
 	 * filehandle validation.
 	 */
 	isomp->volume_space_size += ssector;
-	bcopy (rootp, isomp->root, sizeof isomp->root);
+	memcpy(isomp->root, rootp, sizeof isomp->root);
 	isomp->root_extent = isonum_733 (rootp->extent);
 	isomp->root_size = isonum_733 (rootp->size);
 
@@ -465,7 +465,7 @@ iso_mountfs(devvp, mp)
 			    joliet_level);
 		rootp = (struct iso_directory_record *)
 			sup->root_directory_record;
-		bcopy (rootp, isomp->root, sizeof isomp->root);
+		memcpy(isomp->root, rootp, sizeof isomp->root);
 		isomp->root_extent = isonum_733 (rootp->extent);
 		isomp->root_size = isonum_733 (rootp->size);
 		isomp->joliet_level = joliet_level;
