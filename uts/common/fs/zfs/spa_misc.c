1f1be2ab1d8ecfc50d1a151089e72186adfa9ac3
mav <mav@FreeBSD.org>
2018-07-31 00:42:31 +0000

    9236 nuke spa_dbgmsg
    We should use zfs_dbgmsg instead of spa_dbgmsg.  Or at least,
    metaslab_condense() should call zfs_dbgmsg because it's important and rare
    enough to always log. It's possible that the message in zio_dva_allocate()
    would be too high-frequency for zfs_dbgmsg.
    
    illumos/illumos-gate@21f7c81cc1156e9202ce3412d3ecaa697c3b2222
    
    Reviewed by: Serapheim Dimitropoulos <serapheim.dimitro@delphix.com>
    Reviewed by: Pavel Zakharov <pavel.zakharov@delphix.com>
    Reviewed by: George Wilson <george.wilson@delphix.com>
    Reviewed by: Richard Elling <Richard.Elling@RichardElling.com>
    Approved by: Richard Lowe <richlowe@richlowe.net>
    Author: Matthew Ahrens <mahrens@delphix.com>

--- uts/common/fs/zfs/spa_misc.c
+++ uts/common/fs/zfs/spa_misc.c
@@ -245,7 +245,7 @@ int spa_mode_global;
  * Everything except dprintf, spa, and indirect_remap is on by default
  * in debug builds.
  */
-int zfs_flags = ~(ZFS_DEBUG_DPRINTF | ZFS_DEBUG_SPA | ZFS_DEBUG_INDIRECT_REMAP);
+int zfs_flags = ~(ZFS_DEBUG_DPRINTF | ZFS_DEBUG_INDIRECT_REMAP);
 #else
 int zfs_flags = 0;
 #endif
@@ -706,8 +706,6 @@ spa_add(const char *name, nvlist_t *config, const char *altroot)
 		spa->spa_iokstat->ks_lock = &spa->spa_iokstat_lock;
 		kstat_install(spa->spa_iokstat);
 	}
-
-	spa->spa_debug = ((zfs_flags & ZFS_DEBUG_SPA) != 0);
 
 	spa->spa_min_ashift = INT_MAX;
 	spa->spa_max_ashift = 0;
@@ -2135,12 +2133,6 @@ spa_scan_get_stats(spa_t *spa, pool_scan_stat_t *ps)
 	ps->pss_pass_scrub_spent_paused = spa->spa_scan_pass_scrub_spent_paused;
 
 	return (0);
-}
-
-boolean_t
-spa_debug_enabled(spa_t *spa)
-{
-	return (spa->spa_debug);
 }
 
 int
