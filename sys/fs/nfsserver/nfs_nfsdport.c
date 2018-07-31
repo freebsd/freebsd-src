f18209d1ca1433d305646dc692837f67c6b0dc66
rmacklem <rmacklem@FreeBSD.org>
2018-07-31 20:57:31 +0000

    MFC: r336215 Ignore the cookie verifier for NFSv4.1 when the cookie is 0.
    RFC5661 states that the cookie verifier should be 0 when the cookie is 0.
    However, the wording is somewhat unclear and a recent discussion on the
    nfsv4@ietf.org mailing list indicated that the NFSv4 server should ignore
    the cookie verifier's value when the dirctory offset cookie is 0.
    This patch deletes the check for this that would return NFSERR_BAD_COOKIE
    when the verifier was not 0.
    This was found during testing of the ESXi client against the NFSv4.1 server.

--- sys/fs/nfsserver/nfs_nfsdport.c
+++ sys/fs/nfsserver/nfs_nfsdport.c
@@ -1876,6 +1876,7 @@ nfsrvd_readdirplus(struct nfsrv_descript *nd, int isdgram,
 	}
 	fullsiz = siz;
 	nd->nd_repstat = getret = nfsvno_getattr(vp, &at, nd->nd_cred, p, 1);
+#if 0
 	if (!nd->nd_repstat) {
 	    if (off && verf != at.na_filerev) {
 		/*
@@ -1884,17 +1885,14 @@ nfsrvd_readdirplus(struct nfsrv_descript *nd, int isdgram,
 		 * removed/added unless that offset cookies returned to
 		 * the client are no longer valid.
 		 */
-#if 0
 		if (nd->nd_flag & ND_NFSV4) {
 			nd->nd_repstat = NFSERR_NOTSAME;
 		} else {
 			nd->nd_repstat = NFSERR_BAD_COOKIE;
 		}
-#endif
-	    } else if ((nd->nd_flag & ND_NFSV4) && off == 0 && verf != 0) {
-		nd->nd_repstat = NFSERR_BAD_COOKIE;
 	    }
 	}
+#endif
 	if (!nd->nd_repstat && vp->v_type != VDIR)
 		nd->nd_repstat = NFSERR_NOTDIR;
 	if (!nd->nd_repstat && cnt == 0)
