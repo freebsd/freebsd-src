e1521ed6c8957879cff38148cd8ac46bacf9453f
Hans Petter Selasky <hselasky@FreeBSD.org>
2018-08-02 08:37:44 +0000

    MFC r336393: Use static device naming instead of dynamic one in mlx5ib.
    When resetting mlx5core instances it can happen that the order of attach and
    detach for mlx5ib instances is changed. Take the unit number for mlx5_%d from
    the parent PCI device, similarly to what is done in mlx5en(4), so that there
    is a direct relationship between mce<N> and mlx5_<N>.
    
    Sponsored by:		Mellanox Technologies

--- sys/dev/mlx5/mlx5_ib/mlx5_ib_main.c
+++ sys/dev/mlx5/mlx5_ib/mlx5_ib_main.c
@@ -2957,7 +2957,6 @@ static void *mlx5_ib_add(struct mlx5_core_dev *mdev)
 	struct mlx5_ib_dev *dev;
 	enum rdma_link_layer ll;
 	int port_type_cap;
-	const char *name;
 	int err;
 	int i;
 
@@ -2990,9 +2989,7 @@ static void *mlx5_ib_add(struct mlx5_core_dev *mdev)
 
 	MLX5_INIT_DOORBELL_LOCK(&dev->uar_lock);
 
-	name = "mlx5_%d";
-
-	strlcpy(dev->ib_dev.name, name, IB_DEVICE_NAME_MAX);
+	snprintf(dev->ib_dev.name, IB_DEVICE_NAME_MAX, "mlx5_%d", device_get_unit(mdev->pdev->dev.bsddev));
 	dev->ib_dev.owner		= THIS_MODULE;
 	dev->ib_dev.node_type		= RDMA_NODE_IB_CA;
 	dev->ib_dev.local_dma_lkey	= 0 /* not supported for now */;
