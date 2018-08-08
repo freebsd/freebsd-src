a916d11c6fbd318a170ad8f7a2c0d935f92f5982
Bryan Drewery <bdrewery@FreeBSD.org>
2018-08-08 19:03:06 +0000

    MFC r320191:
      objwarn should be .PHONY.

--- share/mk/bsd.obj.mk
+++ share/mk/bsd.obj.mk
@@ -102,7 +102,7 @@ OBJTOP?= ${.OBJDIR:S,${.CURDIR},,}${SRCTOP}
 # case 2 (using MAKEOBJDIR), don't issue a warning.  Otherwise,
 # issue a warning differentiating between cases 6 and (3 or 4).
 #
-objwarn:
+objwarn: .PHONY
 .if !defined(NO_OBJ) && ${.OBJDIR} != ${CANONICALOBJDIR} && \
     !(defined(MAKEOBJDIRPREFIX) && exists(${CANONICALOBJDIR}/)) && \
     !(defined(MAKEOBJDIR) && exists(${MAKEOBJDIR}/))
