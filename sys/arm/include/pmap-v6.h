2bf8cb3804d89aa2854eb65cf7631aecafe23ea0
Alan Cox <alc@FreeBSD.org>
2018-08-08 16:55:01 +0000

    Add support for pmap_enter(..., psind=1) to the armv6 pmap.  In other words, add support for explicitly requesting that pmap_enter() create a 1 MB page mapping.  (Essentially, this feature allows the machine-independent layer to create superpage mappings preemptively, and not wait for automatic promotion to occur.)
    Export pmap_ps_enabled() to the machine-independent layer.
    
    Add a flag to pmap_pv_insert_pte1() that specifies whether it should fail
    or reclaim a PV entry when one is not available.
    
    Refactor pmap_enter_pte1() into two functions, one by the same name, that
    is a general-purpose function for creating pte1 mappings, and another,
    pmap_enter_1mpage(), that is used to prefault 1 MB read- and/or execute-
    only mappings for execve(2), mmap(2), and shmat(2).
    
    In addition, as an optimization to pmap_enter(..., psind=0), eliminate the
    use of pte2_is_managed() from pmap_enter().  Unlike the x86 pmap
    implementations, armv6 does not have a managed bit defined within the PTE.
    So, pte2_is_managed() is actually a call to PHYS_TO_VM_PAGE(), which is O(n)
    in the number of vm_phys_segs[].  All but one call to PHYS_TO_VM_PAGE() in
    pmap_enter() can be avoided.
    
    Reviewed by:	kib, markj, mmel
    Tested by:	mmel
    MFC after:	6 weeks
    Differential Revision:	https://reviews.freebsd.org/D16555

--- sys/arm/include/pmap-v6.h
+++ sys/arm/include/pmap-v6.h
@@ -167,6 +167,7 @@ void pmap_bootstrap(vm_offset_t);
 void pmap_kenter(vm_offset_t, vm_paddr_t);
 void pmap_kremove(vm_offset_t);
 boolean_t pmap_page_is_mapped(vm_page_t);
+bool	pmap_ps_enabled(pmap_t pmap);
 
 void pmap_tlb_flush(pmap_t, vm_offset_t);
 void pmap_tlb_flush_range(pmap_t, vm_offset_t, vm_size_t);
