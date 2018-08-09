89edb881e6a45eccbf2940aa403c90a8560c46ac
Dimitry Andric <dim@FreeBSD.org>
2018-08-09 21:28:31 +0000

    Add optional LLVM BPF target support
    BPF (eBPF) is an independent instruction set architecture which is
    introduced in Linux a few years ago. Originally, eBPF execute
    environment was only inside Linux kernel. However, recent years there
    are some user space implementation (https://github.com/iovisor/ubpf,
    https://doc.dpdk.org/guides/prog_guide/bpf_lib.html) and kernel space
    implementation for FreeBSD is going on
    (https://github.com/YutaroHayakawa/generic-ebpf).
    
    The BPF target support can be enabled using WITH_LLVM_TARGET_BPF, as it
    is not built by default.
    
    Submitted by:	Yutaro Hayakawa <yhayakawa3720@gmail.com>
    Reviewed by:	dim, bdrewery
    Differential Revision: https://reviews.freebsd.org/D16033

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -275,6 +275,8 @@ __DEFAULT_DEPENDENT_OPTIONS+=	LLVM_TARGET_${__llt:${__LLVM_TARGET_FILT}:tu}/LLVM
 .endif
 .endfor
 
+__DEFAULT_NO_OPTIONS+=LLVM_TARGET_BPF
+
 .include <bsd.compiler.mk>
 # If the compiler is not C++11 capable, disable Clang and use GCC instead.
 # This means that architectures that have GCC 4.2 as default can not
