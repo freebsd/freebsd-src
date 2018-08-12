74ae12de1bdc2de9e8e158d7ed76e3d7cfc5911b
Konstantin Belousov <kib@FreeBSD.org>
2018-08-12 08:45:23 +0000

    MFC r336569: Move mostly useless examples binaries from OFED, as well as the Subnet Manager, under the new option WITH_OFED_EXTRA, disabled by default.

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -197,6 +197,7 @@ __DEFAULT_NO_OPTIONS = \
     LOADER_FORCE_LE \
     NAND \
     OFED \
+    OFED_EXTRA \
     OPENLDAP \
     REPRODUCIBLE_BUILD \
     RPCBIND_WARMSTART_SUPPORT \
@@ -400,6 +401,10 @@ MK_NLS_CATALOGS:= no
 .if ${MK_OPENSSL} == "no"
 MK_OPENSSH:=	no
 MK_KERBEROS:=	no
+.endif
+
+.if ${MK_OFED} == "no"
+MK_OFED_EXTRA:=	no
 .endif
 
 .if ${MK_PF} == "no"
