18548ca3f9b36cb710b20543c054cc9c6bcd151d
delphij <delphij@FreeBSD.org>
2018-08-18 06:20:45 +0000

    Split arc4random_uniform into it's own file and sync with OpenBSD.
    PR:		182610
    Obtained from:	OpenBSD
    MFC after:	2 weeks

--- lib/libc/gen/arc4random.c
+++ lib/libc/gen/arc4random.c
@@ -244,41 +244,6 @@ arc4random_buf(void *_buf, size_t n)
 	_ARC4_UNLOCK();
 }
 
-/*
- * Calculate a uniformly distributed random number less than upper_bound
- * avoiding "modulo bias".
- *
- * Uniformity is achieved by generating new random numbers until the one
- * returned is outside the range [0, 2**32 % upper_bound).  This
- * guarantees the selected random number will be inside
- * [2**32 % upper_bound, 2**32) which maps back to [0, upper_bound)
- * after reduction modulo upper_bound.
- */
-u_int32_t
-arc4random_uniform(u_int32_t upper_bound)
-{
-	u_int32_t r, min;
-
-	if (upper_bound < 2)
-		return 0;
-
-	/* 2**32 % x == (2**32 - x) % x */
-	min = -upper_bound % upper_bound;
-	/*
-	 * This could theoretically loop forever but each retry has
-	 * p > 0.5 (worst case, usually far better) of selecting a
-	 * number inside the range we need, so it should rarely need
-	 * to re-roll.
-	 */
-	for (;;) {
-		r = arc4random();
-		if (r >= min)
-			break;
-	}
-
-	return r % upper_bound;
-}
-
 #if 0
 /*-------- Test code for i386 --------*/
 #include <stdio.h>
