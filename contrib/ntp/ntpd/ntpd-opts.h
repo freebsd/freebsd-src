a13e82666aa5e027bdeb4adeeb15f1b761884bda
delphij <delphij@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-08-21 02:38:07 +0000

    MFV r338092: ntp 4.2.8p12.
    Relnotes:	yes
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@338126 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/ntp/ntpd/ntpd-opts.h
+++ contrib/ntp/ntpd/ntpd-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpd-opts.h)
  *
- *  It has been AutoGen-ed  February 27, 2018 at 05:13:17 PM by AutoGen 5.18.5
+ *  It has been AutoGen-ed  August 14, 2018 at 08:27:44 AM by AutoGen 5.18.5
  *  From the definitions    ntpd-opts.def
  *  and the template file   options
  *
@@ -106,9 +106,9 @@ typedef enum {
 /** count of all options for ntpd */
 #define OPTION_CT    38
 /** ntpd version */
-#define NTPD_VERSION       "4.2.8p11"
+#define NTPD_VERSION       "4.2.8p12"
 /** Full ntpd version text */
-#define NTPD_FULL_VERSION  "ntpd 4.2.8p11"
+#define NTPD_FULL_VERSION  "ntpd 4.2.8p12"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
