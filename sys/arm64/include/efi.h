20c4c552a70e00a8f321a8c44f6c631edcbb700b
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-09-02 21:37:05 +0000

    Catch exceptions during EFI RT calls on amd64.
    This appeared to be required to have EFI RT support and EFI RTC
    enabled by default, because there are too many reports of faulting
    calls on many different machines.  The knob is added to leave the
    exceptions unhandled to allow to debug the actual bugs.
    
    Reviewed by:	kevans
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Approved by:    re (rgrimes)
    Differential revision:	https://reviews.freebsd.org/D16972
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@338437 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/arm64/include/efi.h
+++ sys/arm64/include/efi.h
@@ -39,6 +39,20 @@
 #define	EFI_TIME_LOCK()
 #define	EFI_TIME_UNLOCK()
 #define	EFI_TIME_OWNED()
+
+#define	EFI_RT_HANDLE_FAULTS_DEFAULT	0
 #endif
+
+struct efirt_callinfo {
+	const char	*ec_name;
+	register_t	ec_efi_status;
+	register_t	ec_fptr;
+	register_t	ec_argcnt;
+	register_t	ec_arg1;
+	register_t	ec_arg2;
+	register_t	ec_arg3;
+	register_t	ec_arg4;
+	register_t	ec_arg5;
+};
 
 #endif /* __ARM64_INCLUDE_EFI_H_ */
