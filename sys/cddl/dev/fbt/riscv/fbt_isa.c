76c28d972bb233a231e975925620a9ade8b18e9d
br <br@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-09-03 14:34:09 +0000

    Add support for 'C'-compressed ISA extension to DTrace FBT provider.
    Approved by:	re (kib)
    Sponsored by:	DARPA, AFRL
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@338444 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/cddl/dev/fbt/riscv/fbt_isa.c
+++ sys/cddl/dev/fbt/riscv/fbt_isa.c
@@ -21,7 +21,7 @@
  * Portions Copyright 2006-2008 John Birrell jb@freebsd.org
  * Portions Copyright 2013 Justin Hibbits jhibbits@freebsd.org
  * Portions Copyright 2013 Howard Su howardsu@freebsd.org
- * Portions Copyright 2016 Ruslan Bukin <br@bsdpad.com>
+ * Portions Copyright 2016-2018 Ruslan Bukin <br@bsdpad.com>
  *
  * $FreeBSD$
  */
@@ -37,10 +37,12 @@
 #include <sys/dtrace.h>
 
 #include <machine/riscvreg.h>
+#include <machine/encoding.h>
 
 #include "fbt.h"
 
-#define	FBT_PATCHVAL		(RISCV_INSN_BREAK)
+#define	FBT_C_PATCHVAL		MATCH_C_EBREAK
+#define	FBT_PATCHVAL		MATCH_EBREAK
 #define	FBT_ENTRY		"entry"
 #define	FBT_RETURN		"return"
 
@@ -73,8 +75,62 @@ void
 fbt_patch_tracepoint(fbt_probe_t *fbt, fbt_patchval_t val)
 {
 
-	*fbt->fbtp_patchpoint = val;
-	cpu_icache_sync_range((vm_offset_t)fbt->fbtp_patchpoint, 4);
+	switch(fbt->fbtp_patchval) {
+	case FBT_C_PATCHVAL:
+		*(uint16_t *)fbt->fbtp_patchpoint = (uint16_t)val;
+		cpu_icache_sync_range((vm_offset_t)fbt->fbtp_patchpoint, 2);
+		break;
+	case FBT_PATCHVAL:
+		*fbt->fbtp_patchpoint = val;
+		cpu_icache_sync_range((vm_offset_t)fbt->fbtp_patchpoint, 4);
+		break;
+	};
+}
+
+static int
+match_opcode(uint32_t insn, int match, int mask)
+{
+
+	if (((insn ^ match) & mask) == 0)
+		return (1);
+
+	return (0);
+}
+
+static int
+check_c_ret(uint32_t **instr)
+{
+	uint16_t *instr1;
+	int i;
+
+	for (i = 0; i < 2; i++) {
+		instr1 = (uint16_t *)(*instr) + i;
+		if (match_opcode(*instr1, (MATCH_C_JR | (X_RA << RD_SHIFT)),
+		    (MASK_C_JR | RD_MASK))) {
+			*instr = (uint32_t *)instr1;
+			return (1);
+		}
+	}
+
+	return (0);
+}
+
+static int
+check_c_sdsp(uint32_t **instr)
+{
+	uint16_t *instr1;
+	int i;
+
+	for (i = 0; i < 2; i++) {
+		instr1 = (uint16_t *)(*instr) + i;
+		if (match_opcode(*instr1, (MATCH_C_SDSP | RS2_C_RA),
+		    (MASK_C_SDSP | RS2_C_MASK))) {
+			*instr = (uint32_t *)instr1;
+			return (1);
+		}
+	}
+
+	return (0);
 }
 
 int
@@ -85,6 +141,8 @@ fbt_provide_module_function(linker_file_t lf, int symindx,
 	uint32_t *instr, *limit;
 	const char *name;
 	char *modname;
+	int patchval;
+	int rval;
 
 	modname = opaque;
 	name = symval->name;
@@ -98,8 +156,20 @@ fbt_provide_module_function(linker_file_t lf, int symindx,
 
 	/* Look for sd operation */
 	for (; instr < limit; instr++) {
-		if ((*instr & SD_RA_SP_MASK) == SD_RA_SP)
+		/* Look for a non-compressed store of ra to sp */
+		if (match_opcode(*instr, (MATCH_SD | RS2_RA | RS1_SP),
+		    (MASK_SD | RS2_MASK | RS1_MASK))) {
+			rval = DTRACE_INVOP_SD;
+			patchval = FBT_PATCHVAL;
 			break;
+		}
+
+		/* Look for a 'C'-compressed store of ra to sp. */
+		if (check_c_sdsp(&instr)) {
+			rval = DTRACE_INVOP_C_SDSP;
+			patchval = FBT_C_PATCHVAL;
+			break;
+		}
 	}
 
 	if (instr >= limit)
@@ -113,8 +183,8 @@ fbt_provide_module_function(linker_file_t lf, int symindx,
 	fbt->fbtp_ctl = lf;
 	fbt->fbtp_loadcnt = lf->loadcnt;
 	fbt->fbtp_savedval = *instr;
-	fbt->fbtp_patchval = FBT_PATCHVAL;
-	fbt->fbtp_rval = DTRACE_INVOP_SD;
+	fbt->fbtp_patchval = patchval;
+	fbt->fbtp_rval = rval;
 	fbt->fbtp_symindx = symindx;
 
 	fbt->fbtp_hashnext = fbt_probetab[FBT_ADDR2NDX(instr)];
@@ -125,8 +195,20 @@ fbt_provide_module_function(linker_file_t lf, int symindx,
 	retfbt = NULL;
 again:
 	for (; instr < limit; instr++) {
-		if (*instr == RISCV_INSN_RET)
+		/* Look for non-compressed return */
+		if (match_opcode(*instr, (MATCH_JALR | (X_RA << RS1_SHIFT)),
+		    (MASK_JALR | RD_MASK | RS1_MASK | IMM_MASK))) {
+			rval = DTRACE_INVOP_RET;
+			patchval = FBT_PATCHVAL;
 			break;
+		}
+
+		/* Look for 'C'-compressed return */
+		if (check_c_ret(&instr)) {
+			rval = DTRACE_INVOP_C_RET;
+			patchval = FBT_C_PATCHVAL;
+			break;
+		}
 	}
 
 	if (instr >= limit)
@@ -150,9 +232,9 @@ again:
 	fbt->fbtp_ctl = lf;
 	fbt->fbtp_loadcnt = lf->loadcnt;
 	fbt->fbtp_symindx = symindx;
-	fbt->fbtp_rval = DTRACE_INVOP_RET;
+	fbt->fbtp_rval = rval;
 	fbt->fbtp_savedval = *instr;
-	fbt->fbtp_patchval = FBT_PATCHVAL;
+	fbt->fbtp_patchval = patchval;
 	fbt->fbtp_hashnext = fbt_probetab[FBT_ADDR2NDX(instr)];
 	fbt_probetab[FBT_ADDR2NDX(instr)] = fbt;
 
