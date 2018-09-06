77f44c41c2dca30c0fdcff167e207c8a43e2e9b2
marius <marius@FreeBSD.org>
2018-09-06 21:09:54 +0000

    - Explicitly compare a pointer to NULL. The __builtin_expect() of clang   3.4.1 otherwise isn't able to cope with the expression. - Fix a nearby whitespace bug.
    Approved by:	re (gjb, kib)

--- sys/dev/sdhci/sdhci.c
+++ sys/dev/sdhci/sdhci.c
@@ -1575,11 +1575,10 @@ sdhci_set_transfer_mode(struct sdhci_slot *slot, struct mmc_data *data)
 #ifdef MMCCAM
 		    slot->ccb->mmcio.stop.opcode == MMC_STOP_TRANSMISSION &&
 #else
-		    slot->req->stop &&
+		    slot->req->stop != NULL &&
 #endif
 		    !(slot->quirks & SDHCI_QUIRK_BROKEN_AUTO_STOP)))
 			mode |= SDHCI_TRNS_ACMD12;
-
 	}
 	if (data->flags & MMC_DATA_READ)
 		mode |= SDHCI_TRNS_READ;
