ae1bb6bcc011df55d12b49b7c3e99e882014d797
John Baldwin <jhb@FreeBSD.org>
2018-09-06 22:30:34 +0000

    MFC 332909: Report proper signal codes for SIGTRAP traps on MIPS.
    - Use TRAP_TRACE for traps after stepping via PT_STEP.
    - Use TRAP_BRKPT for software breakpoint traps and watchpoint traps.
    
    This was tested via the recently added siginfo ptrace() tests.  PT_STEP on
    MIPS has several bugs that prevent it from working yet, but this does fix
    the ptrace__breakpoint_siginfo test on MIPS.

--- sys/mips/mips/trap.c
+++ sys/mips/mips/trap.c
@@ -852,6 +852,7 @@ dofault:
 			if (td->td_md.md_ss_addr != va ||
 			    instr != MIPS_BREAK_SSTEP) {
 				i = SIGTRAP;
+				ucode = TRAP_BRKPT;
 				addr = trapframe->pc;
 				break;
 			}
@@ -863,6 +864,7 @@ dofault:
 			 */
 			addr = trapframe->pc;
 			i = SIGTRAP;
+			ucode = TRAP_TRACE;
 			break;
 		}
 
@@ -877,6 +879,7 @@ dofault:
 				va += sizeof(int);
 			printf("watch exception @ %p\n", (void *)va);
 			i = SIGTRAP;
+			ucode = TRAP_BRKPT;
 			addr = va;
 			break;
 		}
