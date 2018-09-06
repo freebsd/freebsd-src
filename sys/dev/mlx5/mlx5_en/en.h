192152af94f644d631da1416160e9a426acd1d90
hselasky <hselasky@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-09-06 12:28:06 +0000

    Add support for receive side scaling stride, RSSS, in mlx5en(4).
    The receive side scaling stride parameter is a value which define the interval
    between active receive side queues. The traffic for the inactive queues is
    redirected to the nearest active queue by use of modulus. The default value
    of this parameter is one, which means all receive side queues are used.
    
    The point of this feature is to redirect more traffic to fewer receive side
    queues in order to take more advantage of sorted large receive offload,
    sorted LRO. The sorted LRO works better when more packets are accumulated
    per service interval.
    
    MFC after:		3 days
    Approved by:		re (marius)
    Sponsored by:		Mellanox Technologies
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@338492 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/mlx5/mlx5_en/en.h
+++ sys/dev/mlx5/mlx5_en/en.h
@@ -454,6 +454,7 @@ struct mlx5e_params {
 	u32	rx_priority_flow_control __aligned(4);
 	u16	tx_max_inline;
 	u8	tx_min_inline_mode;
+	u8	channels_rsss;
 };
 
 #define	MLX5E_PARAMS(m)							\
@@ -462,6 +463,7 @@ struct mlx5e_params {
   m(+1, u64 tx_queue_size, "tx_queue_size", "Default send queue size")	\
   m(+1, u64 rx_queue_size, "rx_queue_size", "Default receive queue size") \
   m(+1, u64 channels, "channels", "Default number of channels")		\
+  m(+1, u64 channels_rsss, "channels_rsss", "Default channels receive side scaling stride") \
   m(+1, u64 coalesce_usecs_max, "coalesce_usecs_max", "Maximum usecs for joining packets") \
   m(+1, u64 coalesce_pkts_max, "coalesce_pkts_max", "Maximum packets to join") \
   m(+1, u64 rx_coalesce_usecs, "rx_coalesce_usecs", "Limit in usec for joining rx packets") \
