7ab7abddce4a942f9422174bd2ba61c5ada389e3
Martin Matuska <mm@FreeBSD.org>
2018-09-07 00:11:43 +0000

    Update vendor/libarchive/dist to git 5fe69dd018745a88eecf1f7db40daf12d26f7ed0
    libarchive 3.3.3

--- libarchive/archive.h
+++ libarchive/archive.h
@@ -155,7 +155,7 @@ __LA_DECL int		archive_version_number(void);
 /*
  * Textual name/version of the library, useful for version displays.
  */
-#define	ARCHIVE_VERSION_ONLY_STRING "3.3.3dev"
+#define	ARCHIVE_VERSION_ONLY_STRING "3.3.3"
 #define	ARCHIVE_VERSION_STRING "libarchive " ARCHIVE_VERSION_ONLY_STRING
 __LA_DECL const char *	archive_version_string(void);
 
