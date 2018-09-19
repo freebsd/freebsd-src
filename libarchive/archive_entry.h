21d41b7887a19cdbe00ce6559aafa5b111e3efe2
Martin Matuska <mm@FreeBSD.org>
2018-09-19 10:26:45 +0000

    Update vendor/libarchive/dist to git 0cda60af13e709e670af90553b2271bf194e7ccd
    Relevant vendor changes:
      PR #1019: Add allocation check for the zip_entry struct
      Oss-Fuzz #10192: Handle whitespace-only ACL fields correctly

--- libarchive/archive_entry.h
+++ libarchive/archive_entry.h
@@ -30,7 +30,7 @@
 #define	ARCHIVE_ENTRY_H_INCLUDED
 
 /* Note: Compiler will complain if this does not match archive.h! */
-#define	ARCHIVE_VERSION_NUMBER 3003003
+#define	ARCHIVE_VERSION_NUMBER 3003004
 
 /*
  * Note: archive_entry.h is for use outside of libarchive; the
