d0f1d030b3b5c549e2bdfd7765cfa5a6aa9fa96b
Jung-uk Kim <jkim@FreeBSD.org>
2018-09-20 21:34:05 +0000

    Regen assembly files for i386.

--- secure/lib/libcrypto/Makefile.asm
+++ secure/lib/libcrypto/Makefile.asm
@@ -59,6 +59,9 @@ sha256-armv8.S:	sha512-armv8.pl
 	${LCRYPTO_SRC}/crypto/whrlpool/asm \
 	${LCRYPTO_SRC}/engines/asm
 
+# cpuid
+SRCS+=	x86_64cpuid.pl
+
 # aes
 SRCS=	aes-x86_64.pl aesni-mb-x86_64.pl aesni-sha1-x86_64.pl \
 	aesni-sha256-x86_64.pl aesni-x86_64.pl bsaes-x86_64.pl \
@@ -76,9 +79,6 @@ SRCS+=	chacha-x86_64.pl
 
 # ec
 SRCS+=	ecp_nistz256-x86_64.pl x25519-x86_64.pl
-
-# engines
-SRCS+=	e_padlock-x86_64.pl
 
 # md5
 SRCS+=	md5-x86_64.pl
@@ -99,8 +99,8 @@ SRCS+=	keccak1600-x86_64.pl sha1-mb-x86_64.pl sha1-x86_64.pl \
 # whrlpool
 SRCS+=	wp-x86_64.pl
 
-# cpuid
-SRCS+=	x86_64cpuid.pl
+# engines
+SRCS+=	e_padlock-x86_64.pl
 
 SHA_ASM=	sha256-x86_64 sha512-x86_64
 SHA_SRC=	sha512-x86_64.pl
@@ -175,22 +175,30 @@ aes-armv4.S:	aes-armv4.pl
 	${LCRYPTO_SRC}/crypto/bf/asm \
 	${LCRYPTO_SRC}/crypto/bn/asm \
 	${LCRYPTO_SRC}/crypto/camellia/asm \
+	${LCRYPTO_SRC}/crypto/cast/asm \
+	${LCRYPTO_SRC}/crypto/chacha/asm \
 	${LCRYPTO_SRC}/crypto/des/asm \
+	${LCRYPTO_SRC}/crypto/ec/asm \
 	${LCRYPTO_SRC}/crypto/md5/asm \
 	${LCRYPTO_SRC}/crypto/modes/asm \
+	${LCRYPTO_SRC}/crypto/poly1305/asm \
 	${LCRYPTO_SRC}/crypto/rc4/asm \
 	${LCRYPTO_SRC}/crypto/rc5/asm \
 	${LCRYPTO_SRC}/crypto/ripemd/asm \
 	${LCRYPTO_SRC}/crypto/sha/asm \
-	${LCRYPTO_SRC}/crypto/whrlpool/asm
+	${LCRYPTO_SRC}/crypto/whrlpool/asm \
+	${LCRYPTO_SRC}/engines/asm
 
-PERLPATH=	-I${LCRYPTO_SRC}/crypto/des/asm -I${LCRYPTO_SRC}/crypto/perlasm
+#PERLPATH=	-I${LCRYPTO_SRC}/crypto/des/asm -I${LCRYPTO_SRC}/crypto/perlasm
+
+# cpuid
+SRCS=	x86cpuid.pl
 
 # aes
-SRCS=	aes-586.pl aesni-x86.pl vpaes-x86.pl
+SRCS+=	aes-586.pl aesni-x86.pl vpaes-x86.pl
 
 # blowfish
-SRCS+=	bf-586.pl bf-686.pl
+SRCS+=	bf-586.pl
 
 # bn
 SRCS+=	bn-586.pl co-586.pl x86-gf2m.pl x86-mont.pl
@@ -198,14 +206,26 @@ SRCS+=	bn-586.pl co-586.pl x86-gf2m.pl x86-mont.pl
 # camellia
 SRCS+=	cmll-x86.pl
 
+# cast
+SRCS+=	cast-586.pl
+
+# chacha
+SRCS+=	chacha-x86.pl
+
 # des
 SRCS+=	crypt586.pl des-586.pl
+
+# ec
+SRCS+=	ecp_nistz256-x86.pl
 
 # md5
 SRCS+=	md5-586.pl
 
 # modes
 SRCS+=	ghash-x86.pl
+
+# poly1305
+SRCS+=	poly1305-x86.pl
 
 # rc4
 SRCS+=	rc4-586.pl
@@ -222,25 +242,26 @@ SRCS+=	sha1-586.pl sha256-586.pl sha512-586.pl
 # whrlpool
 SRCS+=	wp-mmx.pl
 
-# cpuid
-SRCS+=	x86cpuid.pl
+# engines
+SRCS+=	e_padlock-x86.pl
 
 ASM=	${SRCS:R:S/$/.S/}
 
 all:	${ASM}
 
-CLEANFILES=	${ASM}
+CLEANFILES=	${ASM} ${SRCS:R:S/$/.s/}
 .SUFFIXES:	.pl
 
 .pl.S:
 	( echo '/* $$'FreeBSD'$$ */' ;\
 	echo '/* Do not modify. This file is auto-generated from ${.IMPSRC:T}. */' ;\
 	echo '#ifdef PIC' ;\
-	env CC=cc perl ${PERLPATH} ${.IMPSRC} elf ${CFLAGS} -fpic -DPIC ;\
+	env CC=cc perl ${PERLPATH} ${.IMPSRC} elf ${CFLAGS} -fpic -DPIC ${.IMPSRC:R:S/$/.s/} ;\
+	cat ${.IMPSRC:R:S/$/.s/} ;\
 	echo '#else' ;\
-	env CC=cc perl ${PERLPATH} ${.IMPSRC} elf ${CFLAGS} ;\
-	echo '#endif') |\
-	sed -E 's|(\.file[[:blank:]]+)".*"|\1"${.TARGET}"|' > ${.TARGET}
+	env CC=cc perl ${PERLPATH} ${.IMPSRC} elf ${CFLAGS} ${.IMPSRC:R:S/$/.s/} ;\
+	cat ${.IMPSRC:R:S/$/.s/} ;\
+	echo '#endif' ) > ${.TARGET}
 .endif
 
 .include <bsd.prog.mk>
