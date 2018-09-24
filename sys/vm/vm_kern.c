3d14a7bb43d3bff2dad31f0bc928dda275ad86b1
Mark Johnston <markj@FreeBSD.org>
2018-09-24 15:32:46 +0000

    Ensure that "domain" is initialized when vm_ndomains == 1.
    Reported by:	alc
    Approved by:	re (gjb)

--- sys/vm/vm_kern.c
+++ sys/vm/vm_kern.c
@@ -505,8 +505,10 @@ kmem_back(vm_object_t object, vm_offset_t addr, vm_size_t size, int flags)
 			next = roundup2(addr + 1, KVA_QUANTUM);
 			if (next > end || next < start)
 				next = end;
-		} else
+		} else {
+			domain = 0;
 			next = end;
+		}
 		rv = kmem_back_domain(domain, object, addr, next - addr, flags);
 		if (rv != KERN_SUCCESS) {
 			kmem_unback(object, start, addr - start);
