f297c4c762b534049002779226ab17bf6fe6c0c1
slavash <slavash@FreeBSD.org>
2018-09-26 13:16:55 +0000

    Add PCIV_INVALID definition
    From PCI Spec rev 2.2, 6.2.1. Device Identification:
    Vendor ID This field identifies the manufacturer of the device. Valid
    vendor identifiers are allocated by the PCI SIG to ensure uniqueness.
    0FFFFh is an invalid value for Vendor ID.
    
    MFC after:      3 days
    Approved by:    re (Glen), hselasky (mentor), kib (mentor)
    Sponsored by:   Mellanox Technologies

--- sys/dev/pci/pcireg.h
+++ sys/dev/pci/pcireg.h
@@ -122,6 +122,9 @@
 #define	PCIM_MFDEV		0x80
 #define	PCIR_BIST	0x0f
 
+/* PCI Spec rev 2.2: 0FFFFh is an invalid value for Vendor ID. */
+#define	PCIV_INVALID	0xffff 
+
 /* Capability Register Offsets */
 
 #define	PCICAP_ID	0x0
