44bc2984ae86b8ddec8d27fab0077171de6df953
bz <bz@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-10-12 11:30:46 +0000

    r217592 moved the check for imo in udp_input() into the conditional block but leaving the variable assignment outside the block, where it is no longer used. Move both the variable and the assignment one block further in.
    This should result in no functional changes. It will however make upcoming
    changes slightly easier to apply.
    
    Reviewed by:		markj, jtl, tuexen
    Approved by:		re (kib)
    Differential Revision:	https://reviews.freebsd.org/D17525
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@339327 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/udp_usrreq.c
+++ sys/netinet/udp_usrreq.c
@@ -528,7 +528,6 @@ udp_input(struct mbuf **mp, int *offp, int proto)
 	    in_broadcast(ip->ip_dst, ifp)) {
 		struct inpcb *last;
 		struct inpcbhead *pcblist;
-		struct ip_moptions *imo;
 
 		INP_INFO_RLOCK_ET(pcbinfo, et);
 		pcblist = udp_get_pcblist(proto);
@@ -563,10 +562,12 @@ udp_input(struct mbuf **mp, int *offp, int proto)
 			 * Handle socket delivery policy for any-source
 			 * and source-specific multicast. [RFC3678]
 			 */
-			imo = inp->inp_moptions;
 			if (IN_MULTICAST(ntohl(ip->ip_dst.s_addr))) {
+				struct ip_moptions	*imo;
 				struct sockaddr_in	 group;
 				int			 blocked;
+
+				imo = inp->inp_moptions;
 				if (imo == NULL) {
 					INP_RUNLOCK(inp);
 					continue;
