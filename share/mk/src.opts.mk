983a395d1a43726850c431583d02a76bfeeb363f
Warner Losh <imp@FreeBSD.org>
2018-10-21 06:52:10 +0000

    Use BROKEN_OPTIONS for NVME, HYPERV, CXGBETOOL and MLX5TOOL instead of __DEFAULT_YES/NO. These options simply cannot work where we were using __DEFAULT_NO. The proper thing to do in that case is to use the BROKEN_OPTIONS knob instead.

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -85,6 +85,7 @@ __DEFAULT_YES_OPTIONS = \
     CTM \
     CUSE \
     CXX \
+    CXGBETOOL \
     DIALOG \
     DICT \
     DMAGENT \
@@ -111,6 +112,7 @@ __DEFAULT_YES_OPTIONS = \
     GPIO \
     HAST \
     HTML \
+    HYPERV \
     ICONV \
     INET \
     INET6 \
@@ -140,12 +142,14 @@ __DEFAULT_YES_OPTIONS = \
     MAIL \
     MAILWRAPPER \
     MAKE \
+    MLX5TOOL \
     NDIS \
     NETCAT \
     NETGRAPH \
     NLS_CATALOGS \
     NS_CACHING \
     NTP \
+    NVME \
     OFED \
     OPENSSL \
     PAM \
@@ -365,27 +369,20 @@ BROKEN_OPTIONS+=LOADER_GELI LOADER_LUA
 # profiling won't work on MIPS64 because there is only assembly for o32
 BROKEN_OPTIONS+=PROFILE
 .endif
-.if ${__T} == "aarch64" || ${__T} == "amd64" || ${__T} == "i386" || \
-    ${__T} == "powerpc64" || ${__T} == "sparc64"
-__DEFAULT_YES_OPTIONS+=CXGBETOOL
-__DEFAULT_YES_OPTIONS+=MLX5TOOL
-.else
-__DEFAULT_NO_OPTIONS+=CXGBETOOL
-__DEFAULT_NO_OPTIONS+=MLX5TOOL
+.if ${__T} != "aarch64" && ${__T} != "amd64" && ${__T} != "i386" && \
+    ${__T} != "powerpc64" && ${__T} != "sparc64"
+BROKEN_OPTIONS+=CXGBETOOL
+BROKEN_OPTIONS+=MLX5TOOL
 .endif
 
 # HyperV is currently x86-only
-.if ${__T} == "amd64" || ${__T} == "i386"
-__DEFAULT_YES_OPTIONS+=HYPERV
-.else
-__DEFAULT_NO_OPTIONS+=HYPERV
+.if ${__T} != "amd64" && ${__T} != "i386"
+BROKEN_OPTIONS+=HYPERV
 .endif
 
 # NVME is only x86 and powerpc64
-.if ${__T} == "amd64" || ${__T} == "i386" || ${__T} == "powerpc64"
-__DEFAULT_YES_OPTIONS+=NVME
-.else
-__DEFAULT_NO_OPTIONS+=NVME
+.if ${__T} != "amd64" && ${__T} != "i386" && ${__T} != "powerpc64"
+BROKEN_OPTIONS+=NVME
 .endif
 
 .include <bsd.mkopt.mk>
