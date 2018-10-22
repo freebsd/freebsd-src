920528718c8768daf13df3684c7fade82e84ef54
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-10-22 02:34:10 +0000

    Remove mse(4) from tree
    Remove mse and all support for bus and inport devices from the tree.
    Data from nycbug's dmesg database shows the last sighting of this
    driver was in 4.10 on only one machine.
    
    Relnotes: yes
    Differential Revision: https://reviews.freebsd.org/D17628
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@339564 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/mouse.h
+++ sys/sys/mouse.h
@@ -141,8 +141,8 @@ typedef struct synapticshw {
 /* iftype */
 #define MOUSE_IF_UNKNOWN	(-1)
 #define MOUSE_IF_SERIAL		0
-#define MOUSE_IF_BUS		1
-#define MOUSE_IF_INPORT		2
+/* 1 was bus */
+/* 2 was inport */
 #define MOUSE_IF_PS2		3
 #define MOUSE_IF_SYSMOUSE	4
 #define MOUSE_IF_USB		5
@@ -200,8 +200,8 @@ typedef struct mousemode {
 #define MOUSE_PROTO_LOGI	2	/* Logitech, 3 bytes */
 #define MOUSE_PROTO_MM		3	/* MM series, 3 bytes */
 #define MOUSE_PROTO_LOGIMOUSEMAN 4	/* Logitech MouseMan 3/4 bytes */
-#define MOUSE_PROTO_BUS		5	/* MS/Logitech bus mouse */
-#define MOUSE_PROTO_INPORT	6	/* MS/ATI InPort mouse */
+/* 5 was bus mouse */
+/* 6 was inport mosue */
 #define MOUSE_PROTO_PS2		7	/* PS/2 mouse, 3 bytes */
 #define MOUSE_PROTO_HITTAB	8	/* Hitachi Tablet 3 bytes */
 #define MOUSE_PROTO_GLIDEPOINT	9	/* ALPS GlidePoint, 3/4 bytes */
