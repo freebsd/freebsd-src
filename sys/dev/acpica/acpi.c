2bd5c0e95e787d94f36d0ae2b35744f43c8963ca
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-10-25 17:17:11 +0000

    Update comment for AMI00[12]0 override.
    The AML is even stupider than always returning 0. It will only return
    non-zero for an OS that reports itself as "Windows 2015", at least
    on the Threadripper board's AML that I've examined.
    
    Those AMLs also suggest we may need this quirk for AMI0030 as well.
    There may be other cases where we need to override the _STA in a
    generic way, so we should consider writing code to do that.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@339736 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/acpica/acpi.c
+++ sys/dev/acpica/acpi.c
@@ -2222,10 +2222,10 @@ acpi_DeviceIsPresent(device_t dev)
 	status = acpi_GetInteger(h, "_STA", &s);
 
 	/*
-	 * Onboard serial ports on certain AMD motherboards have an invalid _STA
-	 * method that always returns 0.  Force them to always be treated as present.
-	 *
-	 * This may solely be a quirk of a preproduction BIOS.
+	 * Certain Treadripper boards always returns 0 for FreeBSD because it
+	 * only returns non-zero for the OS string "Windows 2015". Otherwise it
+	 * will return zero. Force them to always be treated as present.
+	 * Beata versions were worse: they always returned 0.
 	 */
 	if (acpi_MatchHid(h, "AMDI0020") || acpi_MatchHid(h, "AMDI0010"))
 		return (TRUE);
