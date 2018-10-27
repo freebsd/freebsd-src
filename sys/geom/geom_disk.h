8ede5e0cab40e5aeefb5efc02b7d4a4bad50f62f
eugen <eugen@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-10-27 16:14:42 +0000

    Extend stripeoffset and stripesize of GEOMs from u_int to off_t
    GEOM's stripeoffset overflows at 4 gigabyte margin (2^32)
    because of its u_int type. This leads to incorrect data in the output
    generated by "sysctl kern.geom.confxml" command, "graid list" etc.
    when GEOM array has volumes larger than 4G, for example.
    
    This change does not affect ABI but changes KBI. No MFC planned.
    
    Differential Revision:	https://reviews.freebsd.org/D13426
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@339815 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/geom/geom_disk.h
+++ sys/geom/geom_disk.h
@@ -109,8 +109,8 @@ struct disk {
 	u_int			d_fwheads;
 	u_int			d_maxsize;
 	off_t			d_delmaxsize;
-	u_int			d_stripeoffset;
-	u_int			d_stripesize;
+	off_t			d_stripeoffset;
+	off_t			d_stripesize;
 	char			d_ident[DISK_IDENT_SIZE];
 	char			d_descr[DISK_IDENT_SIZE];
 	uint16_t		d_hba_vendor;
@@ -151,7 +151,8 @@ void disk_add_alias(struct disk *disk, const char *);
 #define DISK_VERSION_03		0x5856105c
 #define DISK_VERSION_04		0x5856105d
 #define DISK_VERSION_05		0x5856105e
-#define DISK_VERSION		DISK_VERSION_05
+#define DISK_VERSION_06		0x5856105f
+#define DISK_VERSION		DISK_VERSION_06
 
 #endif /* _KERNEL */
 #endif /* _GEOM_GEOM_DISK_H_ */
