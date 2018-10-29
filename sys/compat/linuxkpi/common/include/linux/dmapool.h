9cd3ee7979c2773dcf2c2fac0e43c55c72e44bae
hselasky <hselasky@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-10-29 19:02:36 +0000

    Implement dma_pool_zalloc() in the LinuxKPI.
    Submitted by:		Johannes Lundberg <johalun0@gmail.com>
    MFC after:		3 days
    Sponsored by:		Mellanox Technologies
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@339868 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/compat/linuxkpi/common/include/linux/dmapool.h
+++ sys/compat/linuxkpi/common/include/linux/dmapool.h
@@ -77,6 +77,13 @@ dma_pool_alloc(struct dma_pool *pool, gfp_t mem_flags, dma_addr_t *handle)
 	return (vaddr);
 }
 
+static inline void *
+dma_pool_zalloc(struct dma_pool *pool, gfp_t mem_flags, dma_addr_t *handle)
+{
+
+	return (dma_pool_alloc(pool, mem_flags | __GFP_ZERO, handle));
+}
+
 static inline void
 dma_pool_free(struct dma_pool *pool, void *vaddr, dma_addr_t addr)
 {
