cfde210bf355d0dea78857cff04d9a44900811ce
hselasky <hselasky@FreeBSD.org>
2018-11-02 22:23:25 +0000

    Use correct type for IOCTL request argument. This fixes signed IOCTL value warnings in uhsoctl().
    MFC after:		1 week
    Submitted by:		Marcin Cieslak <saper@saper.info>
    Sponsored by:		Mellanox Technologies

--- usr.sbin/uhsoctl/uhsoctl.c
+++ usr.sbin/uhsoctl/uhsoctl.c
@@ -360,7 +360,7 @@ logger(int pri, const char *fmt, ...)
 
 /* Add/remove IP address from an interface */
 static int
-ifaddr_ad(int d, const char *ifnam, struct sockaddr *sa, struct sockaddr *mask)
+ifaddr_ad(unsigned long d, const char *ifnam, struct sockaddr *sa, struct sockaddr *mask)
 {
 	struct ifaliasreq req;
 	int fd, error;
