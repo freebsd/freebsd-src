861242d5ee886a06bbd33069b8fd5318ccc09176
araujo <araujo@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-11-02 08:01:42 +0000

    Fix resource leak when using strdup(3).
    Reported by:	Coverity
    CID:		1357336
    Sponsored by:	iXsystems Inc.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@340044 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/bhyve/pci_xhci.c
+++ usr.sbin/bhyve/pci_xhci.c
@@ -2700,6 +2700,8 @@ pci_xhci_parse_opts(struct pci_xhci_softc *sc, char *opts)
 
 		sc->ndevices++;
 	}
+	if (uopt != NULL)
+		free(uopt);
 
 portsfinal:
 	sc->portregs = calloc(XHCI_MAX_DEVS, sizeof(struct pci_xhci_portregs));
@@ -2729,6 +2731,7 @@ done:
 			free(devices);
 		}
 	}
+	free(uopt);
 	return (sc->ndevices);
 }
 
