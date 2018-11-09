9a38df59e98c6d3cfd2b34f460cd90c337d1913f
Brooks Davis <brooks@FreeBSD.org>
2018-11-09 21:01:16 +0000

    Fix freebsd32 mknod(at).
    As dev_t is now a 64-bit integer, it requires special handling as a
    system call argument.  64-bit arguments are split between two 64-bit
    integers due to the way arguments are promoted to allow reuse of most
    system call implementations.  They must be reassembled before use.
    Further, 64-bit arguments at an odd offset (counting from zero) are
    padded and slid to the next slot on powerpc and mips.  Fix the
    non-COMPAT11 system call by adding a freebsd32_mknodat() and
    appropriately padded declerations.
    
    The COMPAT11 system calls are fully compatible with the 64-bit
    implementations so remove the freebsd32_ versions.
    
    Use uint32_t consistently as the type of the old dev_t.  This matches
    the old definition.
    
    Reviewed by:	kib
    MFC after:	3 days
    Sponsored by:	DARPA, AFRL
    Differential Revision:	https://reviews.freebsd.org/D17928

--- sys/compat/freebsd32/freebsd32_misc.c
+++ sys/compat/freebsd32/freebsd32_misc.c
@@ -463,25 +463,14 @@ freebsd32_fexecve(struct thread *td, struct freebsd32_fexecve_args *uap)
 	return (error);
 }
 
-#if defined(COMPAT_FREEBSD11)
-int
-freebsd11_freebsd32_mknod(struct thread *td,
-    struct freebsd11_freebsd32_mknod_args *uap)
-{
-
-	return (kern_mknodat(td, AT_FDCWD, uap->path, UIO_USERSPACE, uap->mode,
-	    uap->dev));
-}
 
 int
-freebsd11_freebsd32_mknodat(struct thread *td,
-    struct freebsd11_freebsd32_mknodat_args *uap)
+freebsd32_mknodat(struct thread *td, struct freebsd32_mknodat_args *uap)
 {
 
-	return (kern_mknodat(td, uap->fd, uap->path, UIO_USERSPACE, uap->mode,
-	    uap->dev));
+	return (kern_mknodat(td, uap->fd, uap->path, UIO_USERSPACE,
+	    uap->mode, PAIR32TO64(dev_t, uap->dev)));
 }
-#endif /* COMPAT_FREEBSD11 */
 
 int
 freebsd32_mprotect(struct thread *td, struct freebsd32_mprotect_args *uap)
