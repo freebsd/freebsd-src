82c4e17ac9e01c0e6d74408bb5e9af6f565b3b59
markj <markj@FreeBSD.org>
2018-11-13 18:21:47 +0000

    MFC r339927: Add malloc_domainset(9) and _domainset variants to other allocator KPIs.
    Approved by:	re (gjb)

--- sys/kern/kern_cpuset.c
+++ sys/kern/kern_cpuset.c
@@ -119,6 +119,7 @@ __FBSDID("$FreeBSD$");
  */
 
 LIST_HEAD(domainlist, domainset);
+struct domainset __read_mostly domainset_fixed[MAXMEMDOM];
 struct domainset __read_mostly domainset_prefer[MAXMEMDOM];
 struct domainset __read_mostly domainset_roundrobin;
 
@@ -1402,6 +1403,12 @@ domainset_init(void)
 	_domainset_create(dset, NULL);
 
 	for (i = 0; i < vm_ndomains; i++) {
+		dset = &domainset_fixed[i];
+		DOMAINSET_ZERO(&dset->ds_mask);
+		DOMAINSET_SET(i, &dset->ds_mask);
+		dset->ds_policy = DOMAINSET_POLICY_ROUNDROBIN;
+		_domainset_create(dset, NULL);
+
 		dset = &domainset_prefer[i];
 		DOMAINSET_COPY(&all_domains, &dset->ds_mask);
 		dset->ds_policy = DOMAINSET_POLICY_PREFER;
