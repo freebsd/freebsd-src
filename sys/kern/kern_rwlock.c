d54474e63b501c18d61de3bedd0337cc8a633926
Eric van Gyzen <vangyzen@FreeBSD.org>
2018-11-13 20:48:05 +0000

    Make no assertions about lock state when the scheduler is stopped.
    Change the assert paths in rm, rw, and sx locks to match the lock
    and unlock paths.  I did this for mutexes in r306346.
    
    Reported by:	Travis Lane <tlane@isilon.com>
    MFC after:	2 weeks
    Sponsored by:	Dell EMC Isilon

--- sys/kern/kern_rwlock.c
+++ sys/kern/kern_rwlock.c
@@ -1439,7 +1439,7 @@ __rw_assert(const volatile uintptr_t *c, int what, const char *file, int line)
 {
 	const struct rwlock *rw;
 
-	if (panicstr != NULL)
+	if (SCHEDULER_STOPPED())
 		return;
 
 	rw = rwlock2rw(c);
