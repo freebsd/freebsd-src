ee7eba240b5447bfb38c7221103d23080d657b53
Warner Losh <imp@FreeBSD.org>
2018-11-14 23:15:50 +0000

    Remove trailing white space in advance of other changes.

--- sys/cam/ata/ata_da.c
+++ sys/cam/ata/ata_da.c
@@ -987,7 +987,7 @@ adaclose(struct disk *dp)
 		cam_periph_sleep(periph, &softc->refcount, PRIBIO, "adaclose", 1);
 	cam_periph_unlock(periph);
 	cam_periph_release(periph);
-	return (0);	
+	return (0);
 }
 
 static void
@@ -1011,7 +1011,7 @@ adastrategy(struct bio *bp)
 {
 	struct cam_periph *periph;
 	struct ada_softc *softc;
-	
+
 	periph = (struct cam_periph *)bp->bio_disk->d_drv1;
 	softc = (struct ada_softc *)periph->softc;
 
@@ -1035,7 +1035,7 @@ adastrategy(struct bio *bp)
 	 */
 	if (bp->bio_cmd == BIO_ZONE)
 		bp->bio_flags |= BIO_ORDERED;
-	
+
 	/*
 	 * Place it in the queue of disk activities for this disk
 	 */
@@ -1068,7 +1068,6 @@ adadump(void *arg, void *virtual, vm_offset_t physical, off_t offset, size_t len
 	secsize = softc->params.secsize;
 	lba = offset / secsize;
 	count = length / secsize;
-	
 	if ((periph->flags & CAM_PERIPH_INVALID) != 0)
 		return (ENXIO);
 
@@ -1258,7 +1257,7 @@ adaasync(void *callback_arg, u_int32_t code,
 	{
 		struct ccb_getdev *cgd;
 		cam_status status;
- 
+
 		cgd = (struct ccb_getdev *)arg;
 		if (cgd == NULL)
 			break;
@@ -2161,7 +2160,7 @@ ada_zone_cmd(struct cam_periph *periph, union ccb *ccb, struct bio *bp,
 			error = ENOMEM;
 			goto bailout;
 		}
-		
+
 		ata_zac_mgmt_in(&ccb->ataio,
 				/*retries*/ ada_retry_count,
 				/*cbcfnp*/ adadone,
@@ -3002,7 +3001,7 @@ adadone(struct cam_periph *periph, union ccb *done_ccb)
 			 */
 			if ((softc->valid_logdir_len >=
 			    ((ATA_IDENTIFY_DATA_LOG + 1) * sizeof(uint16_t)))
-			 && (le16dec(softc->ata_logdir.header) == 
+			 && (le16dec(softc->ata_logdir.header) ==
 			     ATA_GP_LOG_DIR_VERSION)
 			 && (le16dec(&softc->ata_logdir.num_pages[
 			     (ATA_IDENTIFY_DATA_LOG *
@@ -3022,7 +3021,7 @@ adadone(struct cam_periph *periph, union ccb *done_ccb)
 				 * then ATA logs are effectively not
 				 * supported even if the bit is set in the
 				 * identify data.
-				 */ 
+				 */
 				softc->flags &= ~(ADA_FLAG_CAN_LOG |
 						  ADA_FLAG_CAN_IDLOG);
 				if ((done_ccb->ccb_h.status &
@@ -3132,7 +3131,7 @@ adadone(struct cam_periph *periph, union ccb *done_ccb)
 			softc->state = ADA_STATE_SUP_CAP;
 			xpt_release_ccb(done_ccb);
 			xpt_schedule(periph, priority);
-		} else 
+		} else
 			adaprobedone(periph, done_ccb);
 		return;
 	}
@@ -3196,11 +3195,10 @@ adadone(struct cam_periph *periph, union ccb *done_ccb)
 					 * to an earlier spec, it won't have
 					 * the field.  So, assume all
 					 * commands are supported.
-					 */ 
+					 */
 					softc->zone_flags |=
 					    ADA_ZONE_FLAG_SUP_MASK;
 				}
-					    
 			}
 		} else {
 			error = adaerror(done_ccb, CAM_RETRY_SELTO,
@@ -3237,7 +3235,7 @@ adadone(struct cam_periph *periph, union ccb *done_ccb)
 			softc->state = ADA_STATE_ZONE;
 			xpt_release_ccb(done_ccb);
 			xpt_schedule(periph, priority);
-		} else  
+		} else
 			adaprobedone(periph, done_ccb);
 		return;
 	}
@@ -3321,7 +3319,6 @@ adadone(struct cam_periph *periph, union ccb *done_ccb)
 							 /*getcount_only*/0);
 				}
 			}
-	
 		}
 		free(ataio->data_ptr, M_ATADA);
 
@@ -3388,7 +3385,7 @@ adagetparams(struct cam_periph *periph, struct ccb_getdev *cgd)
 		dp->secs_per_track = cgd->ident_data.sectors;
 		dp->cylinders = cgd->ident_data.cylinders;
 		dp->sectors = cgd->ident_data.cylinders *
-			      (u_int32_t)(dp->heads * dp->secs_per_track);  
+			      (u_int32_t)(dp->heads * dp->secs_per_track);
 	}
 	lbasize = (u_int32_t)cgd->ident_data.lba_size_1 |
 		  ((u_int32_t)cgd->ident_data.lba_size_2 << 16);
@@ -3602,7 +3599,7 @@ adaresume(void *arg)
 			 /*openings*/0,
 			 /*timeout*/0,
 			 /*getcount_only*/0);
-		
+
 		cam_periph_unlock(periph);
 	}
 }
