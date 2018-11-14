afa742f4502714c167b286f7d197488dee90be5c
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-11-14 14:18:35 +0000

    Add d_off support for multiple filesystems.
    The d_off field has been added to the dirent structure recently.
    Currently filesystems don't support this feature.  Support has been
    added and tested for zfs, ufs, ext2fs, fdescfs, msdosfs and unionfs.
    A stub implementation is available for cd9660, nandfs, udf and
    pseudofs but hasn't been tested.
    
    Motivation for this feature: our usecase is for a userspace nfs server
    (nfs-ganesha) with zfs.  At the moment we cache direntry offsets by
    calling lseek once per entry, with this patch we can get the offset
    directly from getdirentries(2) calls which provides a significant
    speedup.
    
    Submitted by:	Jack Halford <jack@gandi.net>
    Reviewed by:	mckusick, pfg, rmacklem (previous versions)
    Sponsored by:	Gandi.net
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D17917
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@340431 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/fs/devfs/devfs_vnops.c
+++ sys/fs/devfs/devfs_vnops.c
@@ -1381,6 +1381,8 @@ devfs_readdir(struct vop_readdir_args *ap)
 		if (dp->d_reclen > uio->uio_resid)
 			break;
 		dp->d_fileno = de->de_inode;
+		/* NOTE: d_off is the offset for the *next* entry. */
+		dp->d_off = off + dp->d_reclen;
 		if (off >= uio->uio_offset) {
 			error = vfs_read_dirent(ap, dp, off);
 			if (error)
