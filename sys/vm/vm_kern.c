76e2875b92d946a46e1c7d183346afb64c8b62e8
markj <markj@FreeBSD.org>
2018-11-20 00:56:54 +0000

    MFC r340205: Avoid specifying VM_PROT_EXECUTE in mappings from pipe_map and exec_map.

--- sys/vm/vm_kern.c
+++ sys/vm/vm_kern.c
@@ -650,8 +650,8 @@ kmap_alloc_wait(vm_map_t map, vm_size_t size)
 		map->needs_wakeup = TRUE;
 		vm_map_unlock_and_wait(map, 0);
 	}
-	vm_map_insert(map, NULL, 0, addr, addr + size, VM_PROT_ALL,
-	    VM_PROT_ALL, MAP_ACC_CHARGED);
+	vm_map_insert(map, NULL, 0, addr, addr + size, VM_PROT_RW, VM_PROT_RW,
+	    MAP_ACC_CHARGED);
 	vm_map_unlock(map);
 	return (addr);
 }
