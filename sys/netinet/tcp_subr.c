79db6fe7aa5e7bb735eb1566f55edce75615c720
Mark Johnston <markj@FreeBSD.org>
2018-11-22 20:49:41 +0000

    Plug some networking sysctl leaks.
    Various network protocol sysctl handlers were not zero-filling their
    output buffers and thus would export uninitialized stack memory to
    userland.  Fix a number of such handlers.
    
    Reported by:	Thomas Barabosch, Fraunhofer FKIE
    Reviewed by:	tuexen
    MFC after:	3 days
    Security:	kernel memory disclosure
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D18301

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -556,6 +556,7 @@ sysctl_net_inet_list_func_info(SYSCTL_HANDLER_ARGS)
 		cnt++;
 #endif
 		if (req->oldptr != NULL) {
+			bzero(&tfi, sizeof(tfi));
 			tfi.tfi_refcnt = f->tf_fb->tfb_refcnt;
 			tfi.tfi_id = f->tf_fb->tfb_id;
 			(void)strncpy(tfi.tfi_alias, f->tf_name,
@@ -2154,6 +2155,7 @@ tcp_pcblist(SYSCTL_HANDLER_ARGS)
 	if (error != 0)
 		return (error);
 
+	bzero(&xig, sizeof(xig));
 	xig.xig_len = sizeof xig;
 	xig.xig_count = n + m;
 	xig.xig_gen = gencnt;
@@ -3215,8 +3217,8 @@ tcp_inptoxtp(const struct inpcb *inp, struct xtcpcb *xt)
 	struct tcpcb *tp = intotcpcb(inp);
 	sbintime_t now;
 
+	bzero(xt, sizeof(*xt));
 	if (inp->inp_flags & INP_TIMEWAIT) {
-		bzero(xt, sizeof(struct xtcpcb));
 		xt->t_state = TCPS_TIME_WAIT;
 	} else {
 		xt->t_state = tp->t_state;
@@ -3244,7 +3246,6 @@ tcp_inptoxtp(const struct inpcb *inp, struct xtcpcb *xt)
 
 		bcopy(tp->t_fb->tfb_tcp_block_name, xt->xt_stack,
 		    TCP_FUNCTION_NAME_LEN_MAX);
-		bzero(xt->xt_logid, TCP_LOG_ID_LEN);
 #ifdef TCP_BLACKBOX
 		(void)tcp_log_get_id(tp, xt->xt_logid);
 #endif
