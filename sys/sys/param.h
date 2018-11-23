467e55b8abbf80cd144961ee51833b8ff4209b17
andrew <andrew@FreeBSD.org>
2018-11-23 16:45:07 +0000

    Enable the BSD crtbegin/crtend by default.
    It has passed an exp run on amd64 and i386, and has testing on arm64. On
    other architectures it is expected to run, however it can be disabled by
    building world with -DWITHOUT_BSD_CRTBEGIN.
    
    Sponsored by:	DARPA, AFRL

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -60,7 +60,7 @@
  *		in the range 5 to 9.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1300003	/* Master, propagated to newvers */
+#define __FreeBSD_version 1300004	/* Master, propagated to newvers */
 
 /*
  * __FreeBSD_kernel__ indicates that this system uses the kernel of FreeBSD,
