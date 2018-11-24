c68c424c140f23d9942a1fb866d491a7eb342743
Andrew Rybchenko <arybchik@FreeBSD.org>
2018-11-24 15:23:46 +0000

    sfxge(4): fix build issue with PHY LED control enabled
    Fixed build issue with the EFSYS_OPT_PHY_LED_CONTROL for Huntigton and
    Medford.
    
    Submitted by:   Vijay Srivastava <vijays at solarflare.com>
    Sponsored by:   Solarflare Communications, Inc.
    MFC after:      1 week
    Differential Revision:  https://reviews.freebsd.org/D18110

--- sys/dev/sfxge/common/efx_check.h
+++ sys/dev/sfxge/common/efx_check.h
@@ -226,8 +226,8 @@
 
 /* Support for PHY LED control */
 #if EFSYS_OPT_PHY_LED_CONTROL
-# if !EFSYS_OPT_SIENA
-#  error "PHY_LED_CONTROL requires SIENA"
+# if !(EFSYS_OPT_SIENA || EFSYS_OPT_HUNTINGTON || EFSYS_OPT_MEDFORD)
+#  error "PHY_LED_CONTROL requires SIENA or HUNTINGTON or MEDFORD"
 # endif
 #endif /* EFSYS_OPT_PHY_LED_CONTROL */
 
