6e549f669f217133851de49ed90d5fa0395cb9a8
markj <markj@FreeBSD.org>
2018-11-28 16:52:41 +0000

    MFC r340730, r340731: Add taskqueue_quiesce(9) and use it to implement taskq_wait().
    PR:	227784

--- sys/sys/taskqueue.h
+++ sys/sys/taskqueue.h
@@ -93,6 +93,7 @@ void	taskqueue_drain(struct taskqueue *queue, struct task *task);
 void	taskqueue_drain_timeout(struct taskqueue *queue,
 	    struct timeout_task *timeout_task);
 void	taskqueue_drain_all(struct taskqueue *queue);
+void	taskqueue_quiesce(struct taskqueue *queue);
 void	taskqueue_free(struct taskqueue *queue);
 void	taskqueue_run(struct taskqueue *queue);
 void	taskqueue_block(struct taskqueue *queue);
