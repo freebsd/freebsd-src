freebsd/freebsd-src

commit ae408ac7225f355975765baf65a5e8374c15b333

sh: Don't scan word twice in ${param=word}.
git-svn-id: svn+ssh://svn.freebsd.org/base/head@316417 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

bin/sh/expand.c
@@ -98,8 +98,8 @@ static const char *expari(const char *, struct nodelist **restrict, int,
 static void expbackq(union node *, int, int, struct worddest *);
 static const char *subevalvar_trim(const char *, struct nodelist **restrict,
     int, int, int);
-static void subevalvar_misc(const char *, struct nodelist *, const char *, int,
-    int, int);
+static const char *subevalvar_misc(const char *, struct nodelist **restrict,
+    const char *, int, int, int);
 static const char *evalvar(const char *, struct nodelist **restrict, int,
     struct worddest *);
 static int varisset(const char *, int);
@@ -615,15 +615,14 @@ subevalvar_trim(const char *p, struct nodelist **restrict argbackq, int strloc,
 }
 
 
-static void
-subevalvar_misc(const char *p, struct nodelist *argbackq, const char *var, int subtype, int startloc,
-  int varflags)
+static const char *
+subevalvar_misc(const char *p, struct nodelist **restrict argbackq,
+    const char *var, int subtype, int startloc, int varflags)
 {
 	char *startp;
-	struct nodelist *argbackqcopy = argbackq;
 	int amount;
 
-	argstr(p, &argbackqcopy, EXP_TILDE, NULL);
+	p = argstr(p, argbackq, EXP_TILDE, NULL);
 	STACKSTRNUL(expdest);
 	startp = stackblock() + startloc;
 
@@ -632,7 +631,7 @@ subevalvar_misc(const char *p, struct nodelist *argbackq, const char *var, int s
 		setvar(var, startp, 0);
 		amount = startp - expdest;
 		STADJUST(amount, expdest);
-		return;
+		return p;
 
 	case VSQUESTION:
 		if (*p != CTLENDVAR) {
@@ -677,7 +676,6 @@ evalvar(const char *p, struct nodelist **restrict argbackq, int flag,
 	if (! is_name(*p))
 		special = 1;
 	p = strchr(p, '=') + 1;
-again: /* jump here after setting a variable with ${var=text} */
 	if (varflags & VSLINENO) {
 		set = 1;
 		special = 1;
@@ -786,11 +784,12 @@ again: /* jump here after setting a variable with ${var=text} */
 	case VSASSIGN:
 	case VSQUESTION:
 		if (!set) {
-			subevalvar_misc(p, *argbackq, var, subtype,
+			p = subevalvar_misc(p, argbackq, var, subtype,
 			    startloc, varflags);
 			/* assert(subtype == VSASSIGN); */
-			varflags &= ~VSNUL;
-			goto again;
+			val = lookupvar(var);
+			strtodest(val, flag, subtype, varflags & VSQUOTE, dst);
+			return p;
 		}
 		break;
 

commit 0517e01237383dc96b5c6c6400098068961f6afe

sh: Fix ${param?} default error message
If word in ${param?word} is missing, the shell shall write a default error
message. So expanding ${param?} when param is not set should write an error
message like

sh: param: parameter not set

This was broken by r316417.

PR:		233585


git-svn-id: svn+ssh://svn.freebsd.org/base/head@341164 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

bin/sh/expand.c
@@ -623,10 +623,11 @@ static const char *
 subevalvar_misc(const char *p, struct nodelist **restrict argbackq,
     const char *var, int subtype, int startloc, int varflags)
 {
+	const char *end;
 	char *startp;
 	int amount;
 
-	p = argstr(p, argbackq, EXP_TILDE, NULL);
+	end = argstr(p, argbackq, EXP_TILDE, NULL);
 	STACKSTRNUL(expdest);
 	startp = stackblock() + startloc;
 
@@ -635,7 +636,7 @@ subevalvar_misc(const char *p, struct nodelist **restrict argbackq,
 		setvar(var, startp, 0);
 		amount = startp - expdest;
 		STADJUST(amount, expdest);
-		return p;
+		return end;
 
 	case VSQUESTION:
 		if (*p != CTLENDVAR) {

bin/sh/tests/expansion/Makefile
@@ -86,6 +86,7 @@ ${PACKAGE}FILES+=	plus-minus7.0
 ${PACKAGE}FILES+=	plus-minus8.0
 ${PACKAGE}FILES+=	plus-minus9.0
 ${PACKAGE}FILES+=	question1.0
+${PACKAGE}FILES+=	question2.0
 ${PACKAGE}FILES+=	readonly1.0
 ${PACKAGE}FILES+=	redir1.0
 ${PACKAGE}FILES+=	set-u1.0

bin/sh/tests/expansion/question2.0
@@ -1,0 +1,11 @@
+# $FreeBSD$
+
+unset dummyvar
+msg=`(: ${dummyvar?}) 2>&1`
+r=$?
+[ "$r" != 0 ] && case $msg in
+*dummyvar?* | *?dummyvar*) : ;;
+*)
+	printf 'Bad message: [%s]\n' "$msg"
+	exit 1
+esac