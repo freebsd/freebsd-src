3f8f54956a13f49d67be7ad6884e785598521394
Andrew Rybchenko <arybchik@FreeBSD.org>
2018-11-29 06:43:46 +0000

    sfxge(4): report no Tx checksum FW subvariant support
    Sponsored by:   Solarflare Communications, Inc.
    Differential Revision:  https://reviews.freebsd.org/D18226

--- sys/dev/sfxge/common/efx.h
+++ sys/dev/sfxge/common/efx.h
@@ -1287,6 +1287,7 @@ typedef struct efx_nic_cfg_s {
 	boolean_t		enc_init_evq_v2_supported;
 	boolean_t		enc_rx_packed_stream_supported;
 	boolean_t		enc_rx_var_packed_stream_supported;
+	boolean_t		enc_fw_subvariant_no_tx_csum_supported;
 	boolean_t		enc_pm_and_rxdp_counters;
 	boolean_t		enc_mac_stats_40g_tx_size_bins;
 	uint32_t		enc_tunnel_encapsulations_supported;
