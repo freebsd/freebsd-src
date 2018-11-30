f61edc985e3bfadf8172f78416d033a4bb656176
Andrew Rybchenko <arybchik@FreeBSD.org>
2018-11-30 09:56:47 +0000

    MFC r340765
    sfxge(4): cleanup: move into right place
    
    Due to incorrect merge the piece of code was put in incorrect
    place and diverge from libefx in other locations.
    
    Sponsored by:   Solarflare Communications, Inc.

--- sys/dev/sfxge/common/ef10_nic.c
+++ sys/dev/sfxge/common/ef10_nic.c
@@ -1092,6 +1092,18 @@ ef10_get_datapath_caps(
 		CAP_FLAG2(flags2, INIT_EVQ_V2) ? B_TRUE : B_FALSE;
 
 	/*
+	 * Check if firmware-verified NVRAM updates must be used.
+	 *
+	 * The firmware trusted installer requires all NVRAM updates to use
+	 * version 2 of MC_CMD_NVRAM_UPDATE_START (to enable verified update)
+	 * and version 2 of MC_CMD_NVRAM_UPDATE_FINISH (to verify the updated
+	 * partition and report the result).
+	 */
+	encp->enc_fw_verified_nvram_update_required =
+	    CAP_FLAG2(flags2, NVRAM_UPDATE_REPORT_VERIFY_RESULT) ?
+	    B_TRUE : B_FALSE;
+
+	/*
 	 * Check if firmware provides packet memory and Rx datapath
 	 * counters.
 	 */
@@ -1104,18 +1116,6 @@ ef10_get_datapath_caps(
 	 */
 	encp->enc_mac_stats_40g_tx_size_bins =
 	    CAP_FLAG2(flags2, MAC_STATS_40G_TX_SIZE_BINS) ? B_TRUE : B_FALSE;
-
-	/*
-	 * Check if firmware-verified NVRAM updates must be used.
-	 *
-	 * The firmware trusted installer requires all NVRAM updates to use
-	 * version 2 of MC_CMD_NVRAM_UPDATE_START (to enable verified update)
-	 * and version 2 of MC_CMD_NVRAM_UPDATE_FINISH (to verify the updated
-	 * partition and report the result).
-	 */
-	encp->enc_fw_verified_nvram_update_required =
-	    CAP_FLAG2(flags2, NVRAM_UPDATE_REPORT_VERIFY_RESULT) ?
-	    B_TRUE : B_FALSE;
 
 #undef CAP_FLAG
 #undef CAP_FLAG2
