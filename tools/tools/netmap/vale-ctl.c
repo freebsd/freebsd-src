b1eabeef97db5cfa17ccce654e67316de7668fc9
Vincenzo Maffione <vmaffione@FreeBSD.org>
2018-12-03 17:46:53 +0000

    MFC r339685
    netmap: add man page for the vale-ctl program
    
    Added man page for vale-ctl program.
    Small fixes to vale-ctl, including the support for -m option
    (to specify the netmap memory allocator id).
    
    Reviewed by:    0mp
    Approved by:    gnn (mentor)
    Differential Revision:  https://reviews.freebsd.org/D17683

--- tools/tools/netmap/vale-ctl.c
+++ tools/tools/netmap/vale-ctl.c
@@ -25,6 +25,10 @@
 
 /* $FreeBSD$ */
 
+#define NETMAP_WITH_LIBS
+#include <net/netmap_user.h>
+#include <net/netmap.h>
+
 #include <errno.h>
 #include <stdio.h>
 #include <inttypes.h>	/* PRI* macros */
@@ -35,16 +39,8 @@
 #include <sys/param.h>
 #include <sys/socket.h>	/* apple needs sockaddr */
 #include <net/if.h>	/* ifreq */
-#include <net/netmap.h>
-#include <net/netmap_user.h>
 #include <libgen.h>	/* basename */
 #include <stdlib.h>	/* atoi, free */
-
-/* debug support */
-#define ND(format, ...)	do {} while(0)
-#define D(format, ...)					\
-	fprintf(stderr, "%s [%d] " format "\n",		\
-	__FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 /* XXX cut and paste from pkt-gen.c because I'm not sure whether this
  * program may include nm_util.h
@@ -86,7 +82,7 @@ void parse_nmr_config(const char* conf, struct nmreq *nmr)
 }
 
 static int
-bdg_ctl(const char *name, int nr_cmd, int nr_arg, char *nmr_config)
+bdg_ctl(const char *name, int nr_cmd, int nr_arg, char *nmr_config, int nr_arg2)
 {
 	struct nmreq nmr;
 	int error = 0;
@@ -100,9 +96,10 @@ bdg_ctl(const char *name, int nr_cmd, int nr_arg, char *nmr_config)
 	bzero(&nmr, sizeof(nmr));
 	nmr.nr_version = NETMAP_API;
 	if (name != NULL) /* might be NULL */
-		strncpy(nmr.nr_name, name, sizeof(nmr.nr_name));
+		strncpy(nmr.nr_name, name, sizeof(nmr.nr_name)-1);
 	nmr.nr_cmd = nr_cmd;
 	parse_nmr_config(nmr_config, &nmr);
+	nmr.nr_arg2 = nr_arg2;
 
 	switch (nr_cmd) {
 	case NETMAP_BDG_DELIF:
@@ -117,8 +114,11 @@ bdg_ctl(const char *name, int nr_cmd, int nr_arg, char *nmr_config)
 		break;
 	case NETMAP_BDG_ATTACH:
 	case NETMAP_BDG_DETACH:
-		if (nr_arg && nr_arg != NETMAP_BDG_HOST)
+		nmr.nr_flags = NR_REG_ALL_NIC;
+		if (nr_arg && nr_arg != NETMAP_BDG_HOST) {
+			nmr.nr_flags = NR_REG_NIC_SW;
 			nr_arg = 0;
+		}
 		nmr.nr_arg1 = nr_arg;
 		error = ioctl(fd, NIOCREGIF, &nmr);
 		if (error == -1) {
@@ -152,6 +152,36 @@ bdg_ctl(const char *name, int nr_cmd, int nr_arg, char *nmr_config)
 
 		break;
 
+	case NETMAP_BDG_POLLING_ON:
+	case NETMAP_BDG_POLLING_OFF:
+		/* We reuse nmreq fields as follows:
+		 *   nr_tx_slots: 0 and non-zero indicate REG_ALL_NIC
+		 *                REG_ONE_NIC, respectively.
+		 *   nr_rx_slots: CPU core index. This also indicates the
+		 *                first queue in the case of REG_ONE_NIC
+		 *   nr_tx_rings: (REG_ONE_NIC only) indicates the
+		 *                number of CPU cores or the last queue
+		 */
+		nmr.nr_flags |= nmr.nr_tx_slots ?
+			NR_REG_ONE_NIC : NR_REG_ALL_NIC;
+		nmr.nr_ringid = nmr.nr_rx_slots;
+		/* number of cores/rings */
+		if (nmr.nr_flags == NR_REG_ALL_NIC)
+			nmr.nr_arg1 = 1;
+		else
+			nmr.nr_arg1 = nmr.nr_tx_rings;
+
+		error = ioctl(fd, NIOCREGIF, &nmr);
+		if (!error)
+			D("polling on %s %s", nmr.nr_name,
+				nr_cmd == NETMAP_BDG_POLLING_ON ?
+				"started" : "stopped");
+		else
+			D("polling on %s %s (err %d)", nmr.nr_name,
+				nr_cmd == NETMAP_BDG_POLLING_ON ?
+				"couldn't start" : "couldn't stop", error);
+		break;
+
 	default: /* GINFO */
 		nmr.nr_cmd = nmr.nr_arg1 = nmr.nr_arg2 = 0;
 		error = ioctl(fd, NIOCGINFO, &nmr);
@@ -166,36 +196,44 @@ bdg_ctl(const char *name, int nr_cmd, int nr_arg, char *nmr_config)
 	return error;
 }
 
+static void
+usage(int errcode)
+{
+	fprintf(stderr,
+	    "Usage:\n"
+	    "vale-ctl arguments\n"
+	    "\t-g interface	interface name to get info\n"
+	    "\t-d interface	interface name to be detached\n"
+	    "\t-a interface	interface name to be attached\n"
+	    "\t-h interface	interface name to be attached with the host stack\n"
+	    "\t-n interface	interface name to be created\n"
+	    "\t-r interface	interface name to be deleted\n"
+	    "\t-l list all or specified bridge's interfaces (default)\n"
+	    "\t-C string ring/slot setting of an interface creating by -n\n"
+	    "\t-p interface start polling. Additional -C x,y,z configures\n"
+	    "\t\t x: 0 (REG_ALL_NIC) or 1 (REG_ONE_NIC),\n"
+	    "\t\t y: CPU core id for ALL_NIC and core/ring for ONE_NIC\n"
+	    "\t\t z: (ONE_NIC only) num of total cores/rings\n"
+	    "\t-P interface stop polling\n"
+	    "\t-m memid to use when creating a new interface\n");
+	exit(errcode);
+}
+
 int
 main(int argc, char *argv[])
 {
 	int ch, nr_cmd = 0, nr_arg = 0;
-	const char *command = basename(argv[0]);
 	char *name = NULL, *nmr_config = NULL;
+	int nr_arg2 = 0;
 
-	if (argc > 3) {
-usage:
-		fprintf(stderr,
-			"Usage:\n"
-			"%s arguments\n"
-			"\t-g interface	interface name to get info\n"
-			"\t-d interface	interface name to be detached\n"
-			"\t-a interface	interface name to be attached\n"
-			"\t-h interface	interface name to be attached with the host stack\n"
-			"\t-n interface	interface name to be created\n"
-			"\t-r interface	interface name to be deleted\n"
-			"\t-l list all or specified bridge's interfaces (default)\n"
-			"\t-C string ring/slot setting of an interface creating by -n\n"
-			"", command);
-		return 0;
-	}
-
-	while ((ch = getopt(argc, argv, "d:a:h:g:l:n:r:C:")) != -1) {
-		name = optarg; /* default */
+	while ((ch = getopt(argc, argv, "d:a:h:g:l:n:r:C:p:P:m:")) != -1) {
+		if (ch != 'C' && ch != 'm')
+			name = optarg; /* default */
 		switch (ch) {
 		default:
 			fprintf(stderr, "bad option %c %s", ch, optarg);
-			goto usage;
+			usage(-1);
+			break;
 		case 'd':
 			nr_cmd = NETMAP_BDG_DETACH;
 			break;
@@ -217,19 +255,28 @@ usage:
 			break;
 		case 'l':
 			nr_cmd = NETMAP_BDG_LIST;
-			if (optind < argc && argv[optind][0] == '-')
-				name = NULL;
 			break;
 		case 'C':
 			nmr_config = strdup(optarg);
 			break;
-		}
-		if (optind != argc) {
-			// fprintf(stderr, "optind %d argc %d\n", optind, argc);
-			goto usage;
+		case 'p':
+			nr_cmd = NETMAP_BDG_POLLING_ON;
+			break;
+		case 'P':
+			nr_cmd = NETMAP_BDG_POLLING_OFF;
+			break;
+		case 'm':
+			nr_arg2 = atoi(optarg);
+			break;
 		}
 	}
-	if (argc == 1)
+	if (optind != argc) {
+		// fprintf(stderr, "optind %d argc %d\n", optind, argc);
+		usage(-1);
+	}
+	if (argc == 1) {
 		nr_cmd = NETMAP_BDG_LIST;
-	return bdg_ctl(name, nr_cmd, nr_arg, nmr_config) ? 1 : 0;
+		name = NULL;
+	}
+	return bdg_ctl(name, nr_cmd, nr_arg, nmr_config, nr_arg2) ? 1 : 0;
 }
