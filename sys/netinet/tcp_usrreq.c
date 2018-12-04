808a6e26095adfe4064a0562452cc005c1b95513
tuexen <tuexen@FreeBSD.org>
2018-12-04 22:25:24 +0000

    MFC r341335:
    Limit option_len for the TCP_CCALGOOPT.
    
    Limiting the length to 2048 bytes seems to be acceptable, since
    the values used right now are using 8 bytes.
    This issue was found by using syzkaller.
    
    Reviewed by:		glebius, bz, rrs
    Sponsored by:		Netflix, Inc.
    Differential Revision:	https://reviews.freebsd.org/D18366

--- sys/netinet/tcp_usrreq.c
+++ sys/netinet/tcp_usrreq.c
@@ -1549,6 +1549,8 @@ tcp_default_ctloutput(struct socket *so, struct sockopt *sopt, struct inpcb *inp
 	switch (sopt->sopt_name) {
 	case TCP_CCALGOOPT:
 		INP_WUNLOCK(inp);
+		if (sopt->sopt_valsize > CC_ALGOOPT_LIMIT)
+			return (EINVAL);
 		pbuf = malloc(sopt->sopt_valsize, M_TEMP, M_WAITOK | M_ZERO);
 		error = sooptcopyin(sopt, pbuf, sopt->sopt_valsize,
 		    sopt->sopt_valsize);
