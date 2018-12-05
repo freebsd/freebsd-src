97a5c766cbf3893fa5a59abd63492e64b11ab5da
slavash <slavash@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-12-05 14:19:55 +0000

    mlx5fpga: Add set and query connect/disconnect FPGA
    Submitted by:   kib@
    Approved by:    hselasky (mentor)
    MFC after:      1 week
    Sponsored by:   Mellanox Technologies
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@341576 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/mlx5/mlx5_fpga/mlx5fpga_cmd.c
+++ sys/dev/mlx5/mlx5_fpga/mlx5fpga_cmd.c
@@ -37,6 +37,7 @@
 #include <dev/mlx5/device.h>
 #include <dev/mlx5/mlx5_core/mlx5_core.h>
 #include <dev/mlx5/mlx5_fpga/cmd.h>
+#include <dev/mlx5/mlx5_fpga/core.h>
 
 #define MLX5_FPGA_ACCESS_REG_SZ (MLX5_ST_SZ_DW(fpga_access_reg) + \
 				 MLX5_FPGA_ACCESS_REG_SIZE_MAX)
@@ -162,6 +163,33 @@ int mlx5_fpga_query(struct mlx5_core_dev *dev, struct mlx5_fpga_query *query)
 	query->admin_image = MLX5_GET(fpga_ctrl, out, flash_select_admin);
 	query->oper_image = MLX5_GET(fpga_ctrl, out, flash_select_oper);
 	return 0;
+}
+
+int mlx5_fpga_ctrl_connect(struct mlx5_core_dev *dev,
+			   enum mlx5_fpga_connect *connect)
+{
+	u32 in[MLX5_ST_SZ_DW(fpga_ctrl)] = {0};
+	u32 out[MLX5_ST_SZ_DW(fpga_ctrl)];
+	int status;
+	int err;
+
+	if (*connect == MLX5_FPGA_CONNECT_QUERY) {
+		err = mlx5_core_access_reg(dev, in, sizeof(in), out,
+					   sizeof(out), MLX5_REG_FPGA_CTRL,
+					   0, false);
+		if (err)
+			return err;
+		status = MLX5_GET(fpga_ctrl, out, status);
+		*connect = (status == MLX5_FDEV_STATE_DISCONNECTED) ?
+			MLX5_FPGA_CONNECT_DISCONNECT :
+			MLX5_FPGA_CONNECT_CONNECT;
+	} else {
+		MLX5_SET(fpga_ctrl, in, operation, *connect);
+		err = mlx5_core_access_reg(dev, in, sizeof(in), out,
+					   sizeof(out), MLX5_REG_FPGA_CTRL,
+					   0, true);
+	}
+	return err;
 }
 
 int mlx5_fpga_query_mtmp(struct mlx5_core_dev *dev,
