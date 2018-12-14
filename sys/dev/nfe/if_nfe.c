d41acddcc1df4505a79b776849299ea0130925f0
markj <markj@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-12-14 18:16:35 +0000

    Add support for the nForce MCP89 adapter.
    PR:		234015
    Submitted by:	Andrejs Bogdanovs <sinchiroca86@gmail.com>
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@342086 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/nfe/if_nfe.c
+++ sys/dev/nfe/if_nfe.c
@@ -265,6 +265,8 @@ static struct nfe_type nfe_devs[] = {
 	    "NVIDIA nForce MCP79 Networking Adapter"},
 	{PCI_VENDOR_NVIDIA, PCI_PRODUCT_NVIDIA_MCP79_LAN4,
 	    "NVIDIA nForce MCP79 Networking Adapter"},
+	{PCI_VENDOR_NVIDIA, PCI_PRODUCT_NVIDIA_MCP89_LAN,
+	    "NVIDIA nForce MCP89 Networking Adapter"},
 	{0, 0, NULL}
 };
 
@@ -531,6 +533,7 @@ nfe_attach(device_t dev)
 	case PCI_PRODUCT_NVIDIA_MCP79_LAN2:
 	case PCI_PRODUCT_NVIDIA_MCP79_LAN3:
 	case PCI_PRODUCT_NVIDIA_MCP79_LAN4:
+	case PCI_PRODUCT_NVIDIA_MCP89_LAN:
 		/* XXX flow control */
 		sc->nfe_flags |= NFE_JUMBO_SUP | NFE_40BIT_ADDR | NFE_HW_CSUM |
 		    NFE_PWR_MGMT | NFE_CORRECT_MACADDR | NFE_MIB_V3;
