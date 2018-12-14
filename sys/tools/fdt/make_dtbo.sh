0a65536cabcc7c0624dea86b95d6f9455d271bac
Alex Richardson <arichardson@FreeBSD.org>
2018-12-14 23:53:28 +0000

    make_dtb.sh: Use $CPP instead of assuming that cpp is in $PATH
    This fixes building in CheriBSD with a strict tmp path since we don't
    bootstrap a cpp but pass the full path to clang-cpp instead.
    
    While touching this file also fix all shellcheck warnings in make_dtb.sh.
    
    Reviewed By:	manu
    Differential Revision: https://reviews.freebsd.org/D18376

--- sys/tools/fdt/make_dtbo.sh
+++ sys/tools/fdt/make_dtbo.sh
@@ -16,12 +16,13 @@ if [ -z "${MACHINE}" ]; then
     MACHINE=$(uname -m)
 fi
 
-: ${DTC:=dtc}
-: ${ECHO:=echo}
+: "${DTC:=dtc}"
+: "${ECHO:=echo}"
+: "${CPP:=cpp}"
 
 for d in ${dtso}; do
-    dtb=${dtbo_path}/`basename $d .dtso`.dtbo
+    dtb="${dtbo_path}/$(basename "$d" .dtso).dtbo"
     ${ECHO} "converting $d -> $dtb"
-    cpp -P -x assembler-with-cpp -I $S/gnu/dts/include -I $S/dts/${MACHINE} -I $S/gnu/dts/${MACHINE} -include $d /dev/null | 
-	${DTC} -@ -O dtb -o $dtb -i $S/dts/${MACHINE} -i $S/gnu/dts/${MACHINE}
+    ${CPP} -P -x assembler-with-cpp -I "$S/gnu/dts/include" -I "$S/dts/${MACHINE}" -I "$S/gnu/dts/${MACHINE}" -include "$d" /dev/null |
+	${DTC} -@ -O dtb -o "$dtb" -i "$S/dts/${MACHINE}" -i "$S/gnu/dts/${MACHINE}"
 done
