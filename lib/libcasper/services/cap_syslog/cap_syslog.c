2383da0d470fcc680069fb98c4d91b03b9086893
Mark Johnston <markj@FreeBSD.org>
2018-12-14 18:38:10 +0000

    MFC r341692: Let the cap_syslog capability inherit stdio descriptors.

--- lib/libcasper/services/cap_syslog/cap_syslog.c
+++ lib/libcasper/services/cap_syslog/cap_syslog.c
@@ -198,4 +198,4 @@ syslog_command(const char *cmd, const nvlist_t *limits, nvlist_t *nvlin,
 	return (0);
 }
 
-CREATE_SERVICE("system.syslog", NULL, syslog_command, 0);
+CREATE_SERVICE("system.syslog", NULL, syslog_command, CASPER_SERVICE_STDIO);
