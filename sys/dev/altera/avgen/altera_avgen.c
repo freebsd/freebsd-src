d63ba970d3181eb376f85e6369a13991b1b7e986
kib <kib@FreeBSD.org>
2018-12-16 02:46:51 +0000

    MFC r341402: Fix off-by-one (page) errors in checks in d_mmap methods of several drivers.

--- sys/dev/altera/avgen/altera_avgen.c
+++ sys/dev/altera/avgen/altera_avgen.c
@@ -229,6 +229,7 @@ altera_avgen_mmap(struct cdev *dev, vm_ooffset_t offset, vm_paddr_t *paddr,
 			return (EACCES);
 	}
 	if (trunc_page(offset) == offset &&
+	    offset + PAGE_SIZE > offset &&
 	    rman_get_size(sc->avg_res) >= offset + PAGE_SIZE) {
 		*paddr = rman_get_start(sc->avg_res) + offset;
 		*memattr = VM_MEMATTR_UNCACHEABLE;
