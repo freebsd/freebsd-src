b2ec0c4e6236c005bf624dbb53fa11b0156422f3
Ed Maste <emaste@FreeBSD.org>
2018-12-19 18:00:58 +0000

    MFS11 r341401: Update the free page count when blacklisting pages.
    PR:		231296
    Submitted by:	markj
    Approved by:	so
    Security:	FreeBSD-EN-18:17.vm
    Sponsored by:	The FreeBSD Foundation

--- sys/vm/vm_page.c
+++ sys/vm/vm_page.c
@@ -304,8 +304,10 @@ vm_page_blacklist_add(vm_paddr_t pa, bool verbose)
 
 	mtx_lock(&vm_page_queue_free_mtx);
 	ret = vm_phys_unfree_page(m);
+	if (ret != 0)
+		vm_phys_freecnt_adj(m, -1);
 	mtx_unlock(&vm_page_queue_free_mtx);
-	if (ret) {
+	if (ret != 0) {
 		TAILQ_INSERT_TAIL(&blacklist_head, m, listq);
 		if (verbose)
 			printf("Skipping page with pa 0x%jx\n", (uintmax_t)pa);
