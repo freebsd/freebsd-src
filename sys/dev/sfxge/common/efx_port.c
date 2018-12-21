9eb745f460bab1f0544071d78aff756b474eda56
arybchik <arybchik@FreeBSD.org>
2018-12-21 16:13:48 +0000

    MFC r340896
    sfxge(4): remove Falcon-specific concurrency check
    
    Falcon support has been withdrawn from libefx, however, there is still
    an obsolete Falcon-specific assertion that efx_mac_stats_upload()
    and efx_port_poll() aren't concurrent. To be consistent with an overall
    Falcon support revocation it's desirable to remove it.
    
    Fix debug build invalid assertion failure.
    
    Submitted by:   Ivan Malov <ivan.malov at oktetlabs.ru>
    Sponsored by:   Solarflare Communications, Inc.
    Differential Revision:  https://reviews.freebsd.org/D18130

--- sys/dev/sfxge/common/efx_port.c
+++ sys/dev/sfxge/common/efx_port.c
@@ -114,7 +114,6 @@ efx_port_poll(
 	EFSYS_ASSERT3U(enp->en_mod_flags, &, EFX_MOD_PORT);
 
 	EFSYS_ASSERT(emop != NULL);
-	EFSYS_ASSERT(!epp->ep_mac_stats_pending);
 
 	if (link_modep == NULL)
 		link_modep = &ignore_link_mode;
