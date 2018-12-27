8002d620df8cf1cb85af1937fa12f2027534f9ab
Jakub Klama <jakub@conclusive.pl>
2018-12-27 02:26:51 +0100

    Update copyright notice.

--- usr.sbin/bhyve/pci_virtio_9p.c
+++ usr.sbin/bhyve/pci_virtio_9p.c
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2015 iXsystems Inc.
+ * Copyright (c) 2017-2018 Jakub Klama <jceel@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
