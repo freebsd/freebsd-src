726443f14def3067eecd412fac6dfde731f4e47d
tsoome <tsoome@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2018-12-30 15:34:12 +0000

    Add Copyright.
    Reported by:	Rodney W. Grimes
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@342626 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- stand/i386/libi386/bio.c
+++ stand/i386/libi386/bio.c
@@ -1,4 +1,6 @@
 /*-
+ * Copyright 2018 Toomas Soome <tsoome@me.com>
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
