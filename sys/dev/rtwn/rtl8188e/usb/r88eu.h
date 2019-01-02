6dd389770bb7c59863e2580274dfdeb945c649be
avos <avos@FreeBSD.org>
2019-01-02 05:30:41 +0000

    Move USB-specific parts from rtwn(4) to rtwn_usb(4)
    MFC after:	4 days

--- sys/dev/rtwn/rtl8188e/usb/r88eu.h
+++ sys/dev/rtwn/rtl8188e/usb/r88eu.h
@@ -25,9 +25,18 @@
 
 
 /*
+ * Global definitions.
+ */
+#define R88EU_PUBQ_NPAGES	142
+#define R88EU_TX_PAGE_COUNT	169
+
+
+/*
  * Function declarations.
  */
 /* r88eu_init.c */
+void	r88eu_init_bb(struct rtwn_softc *);
+int	r88eu_power_on(struct rtwn_softc *);
 void	r88eu_power_off(struct rtwn_softc *);
 void	r88eu_init_intr(struct rtwn_softc *);
 void	r88eu_init_rx_agg(struct rtwn_softc *);
