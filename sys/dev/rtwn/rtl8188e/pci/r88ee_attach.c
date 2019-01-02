b65ae50c2b44f3e341ab70a5aa9c658630877191
avos <avos@FreeBSD.org>
2019-01-02 06:48:53 +0000

    rtwn_pci(4): add support for RTL8188EE chipset.
    Initially based on https://reviews.freebsd.org/D15692;
    later deduplicated and improved a bit (Tx reports, IQ calibration support).
    
    Submitted by:	Farhan Khan <khanzf@gmail.com>
    MFC after:	4 days
    Relnotes:	yes
    Differential Revision:	https://reviews.freebsd.org/D15692

--- sys/dev/rtwn/rtl8188e/pci/r88ee_attach.c
+++ sys/dev/rtwn/rtl8188e/pci/r88ee_attach.c
@@ -1,9 +1,5 @@
-/*	$OpenBSD: if_urtwn.c,v 1.16 2011/02/10 17:26:40 jakemsr Exp $	*/
-
 /*-
- * Copyright (c) 2010 Damien Bergamini <damien.bergamini@free.fr>
- * Copyright (c) 2014 Kevin Lo <kevlo@FreeBSD.org>
- * Copyright (c) 2015-2016 Andriy Voskoboinyk <avos@FreeBSD.org>
+ * Copyright (c) 2018 Farhan Khan <khanzf@gmail.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -37,6 +33,10 @@ __FBSDID("$FreeBSD$");
 #include <sys/endian.h>
 #include <sys/linker.h>
 
+#include <machine/bus.h>
+#include <machine/resource.h>
+#include <sys/rman.h>
+
 #include <net/if.h>
 #include <net/ethernet.h>
 #include <net/if_media.h>
@@ -48,34 +48,33 @@ __FBSDID("$FreeBSD$");
 #include <dev/rtwn/if_rtwnvar.h>
 #include <dev/rtwn/if_rtwn_nop.h>
 
-#include <dev/rtwn/usb/rtwn_usb_var.h>
+#include <dev/rtwn/pci/rtwn_pci_var.h>
 
-#include <dev/rtwn/rtl8192c/r92c_reg.h>
 #include <dev/rtwn/rtl8192c/r92c_var.h>
-
-#include <dev/rtwn/rtl8192c/usb/r92cu.h>
-#include <dev/rtwn/rtl8192c/usb/r92cu_tx_desc.h>
 
 #include <dev/rtwn/rtl8188e/r88e_priv.h>
 #include <dev/rtwn/rtl8188e/r88e_rom_image.h>	/* for 'macaddr' field */
 
-#include <dev/rtwn/rtl8188e/usb/r88eu.h>
+#include <dev/rtwn/rtl8192c/pci/r92ce.h>
+#include <dev/rtwn/rtl8192c/pci/r92ce_tx_desc.h>
 
+#include <dev/rtwn/rtl8188e/pci/r88ee.h>
+#include <dev/rtwn/rtl8188e/pci/r88ee_reg.h>
 
-static struct rtwn_r88e_txpwr r88e_txpwr;
+static struct rtwn_r88e_txpwr r88ee_txpwr;
 
-void	r88eu_attach(struct rtwn_usb_softc *);
+void	r88ee_attach(struct rtwn_pci_softc *);
 
 static void
-r88eu_set_macaddr(struct rtwn_softc *sc, uint8_t *buf)
+r88ee_set_macaddr(struct rtwn_softc *sc, uint8_t *buf)
 {
 	struct r88e_rom *rom = (struct r88e_rom *)buf;
 
-	IEEE80211_ADDR_COPY(sc->sc_ic.ic_macaddr, rom->diff_d0.usb.macaddr);
+	IEEE80211_ADDR_COPY(sc->sc_ic.ic_macaddr, rom->diff_d0.pci.macaddr);
 }
 
 static void
-r88e_postattach(struct rtwn_softc *sc)
+r88ee_postattach(struct rtwn_softc *sc)
 {
 	struct r92c_softc *rs = sc->sc_priv;
 	struct ieee80211com *ic = &sc->sc_ic;
@@ -87,14 +86,13 @@ r88e_postattach(struct rtwn_softc *sc)
 }
 
 static void
-r88eu_attach_private(struct rtwn_softc *sc)
+r88ee_attach_private(struct rtwn_softc *sc)
 {
 	struct r92c_softc *rs;
 
 	rs = malloc(sizeof(struct r92c_softc), M_RTWN_PRIV, M_WAITOK | M_ZERO);
 
-	rs->rs_txpwr			= &r88e_txpwr;
-	rs->rs_txagc			= NULL;
+	rs->rs_txpwr			= &r88ee_txpwr;
 
 	rs->rs_set_bw20			= r88e_set_bw20;
 	rs->rs_get_txpower		= r88e_get_txpower;
@@ -102,38 +100,46 @@ r88eu_attach_private(struct rtwn_softc *sc)
 	rs->rs_tx_enable_ampdu		= r88e_tx_enable_ampdu;
 	rs->rs_tx_setup_hwseq		= r88e_tx_setup_hwseq;
 	rs->rs_tx_setup_macid		= r88e_tx_setup_macid;
-	rs->rs_set_rom_opts		= r88eu_set_macaddr;
+	rs->rs_set_rom_opts		= r88ee_set_macaddr;
 
-	rs->rf_read_delay[0]		= 10;
-	rs->rf_read_delay[1]		= 100;
-	rs->rf_read_delay[2]		= 10;
+	rs->rf_read_delay[0]		= 1000;
+	rs->rf_read_delay[1]		= 1000;
+	rs->rf_read_delay[2]		= 1000;
 
 	sc->sc_priv = rs;
+
 }
 
 static void
-r88eu_adj_devcaps(struct rtwn_softc *sc)
+r88ee_adj_devcaps(struct rtwn_softc *sc)
 {
-	/* XXX TODO? */
+	/* XXX TODO */
 }
 
 void
-r88eu_attach(struct rtwn_usb_softc *uc)
+r88ee_attach(struct rtwn_pci_softc *pc)
 {
-	struct rtwn_softc *sc		= &uc->uc_sc;
+	struct rtwn_softc *sc		= &pc->pc_sc;
 
-	/* USB part. */
-	uc->uc_align_rx			= r92cu_align_rx;
-	uc->tx_agg_desc_num		= 6;
+	/* PCIe part. */
+	pc->pc_setup_tx_desc		= r92ce_setup_tx_desc;
+	pc->pc_tx_postsetup		= r92ce_tx_postsetup;
+	pc->pc_copy_tx_desc		= r92ce_copy_tx_desc;
+	pc->pc_enable_intr		= r88ee_enable_intr;
+	pc->pc_get_intr_status		= r88ee_get_intr_status;
+
+	pc->pc_qmap			= 0xe771;
+	pc->tcr				= 0x8200;
 
 	/* Common part. */
 	sc->sc_flags			= RTWN_FLAG_EXT_HDR;
 
+	sc->sc_start_xfers		= r88ee_start_xfers;
 	sc->sc_set_chan			= r92c_set_chan;
 	sc->sc_fill_tx_desc		= r92c_fill_tx_desc;
-	sc->sc_fill_tx_desc_raw 	= r92c_fill_tx_desc_raw;
-	sc->sc_fill_tx_desc_null	= r92c_fill_tx_desc_null;
-	sc->sc_dump_tx_desc		= r92cu_dump_tx_desc;
+	sc->sc_fill_tx_desc_raw		= r92c_fill_tx_desc_raw;
+	sc->sc_fill_tx_desc_null	= r92c_fill_tx_desc_null; /* XXX recheck */
+	sc->sc_dump_tx_desc		= r92ce_dump_tx_desc;
 	sc->sc_tx_radiotap_flags	= r92c_tx_radiotap_flags;
 	sc->sc_rx_radiotap_flags	= r92c_rx_radiotap_flags;
 	sc->sc_get_rx_stats		= r88e_get_rx_stats;
@@ -149,8 +155,8 @@ r88eu_attach(struct rtwn_usb_softc *uc)
 	sc->sc_efuse_postread		= rtwn_nop_softc;
 	sc->sc_parse_rom		= r88e_parse_rom;
 	sc->sc_set_led			= r88e_set_led;
-	sc->sc_power_on			= r88eu_power_on;
-	sc->sc_power_off		= r88eu_power_off;
+	sc->sc_power_on			= r88ee_power_on;
+	sc->sc_power_off		= r88ee_power_off;
 #ifndef RTWN_WITHOUT_UCODE
 	sc->sc_fw_reset			= r88e_fw_reset;
 	sc->sc_fw_download_enable	= r88e_fw_download_enable;
@@ -160,15 +166,15 @@ r88eu_attach(struct rtwn_usb_softc *uc)
 	sc->sc_lc_calib			= r92c_lc_calib;
 	sc->sc_iq_calib			= r88e_iq_calib;
 	sc->sc_read_chipid_vendor	= rtwn_nop_softc_uint32;
-	sc->sc_adj_devcaps		= r88eu_adj_devcaps;
+	sc->sc_adj_devcaps		= r88ee_adj_devcaps;
 	sc->sc_vap_preattach		= rtwn_nop_softc_vap;
-	sc->sc_postattach		= r88e_postattach;
+	sc->sc_postattach		= r88ee_postattach;
 	sc->sc_detach_private		= r92c_detach_private;
 	sc->sc_set_media_status		= r88e_set_media_status;
 #ifndef RTWN_WITHOUT_UCODE
 	sc->sc_set_rsvd_page		= r88e_set_rsvd_page;
 	sc->sc_set_pwrmode		= r88e_set_pwrmode;
-	sc->sc_set_rssi			= rtwn_nop_softc;	/* XXX TODO? */
+	sc->sc_set_rssi			= r92c_set_rssi;
 #endif
 	sc->sc_beacon_init		= r92c_beacon_init;
 	sc->sc_beacon_enable		= r88e_beacon_enable;
@@ -176,15 +182,15 @@ r88eu_attach(struct rtwn_usb_softc *uc)
 	sc->sc_beacon_select		= rtwn_nop_softc_int;
 	sc->sc_temp_measure		= r88e_temp_measure;
 	sc->sc_temp_read		= r88e_temp_read;
-	sc->sc_init_tx_agg		= r92cu_init_tx_agg;
-	sc->sc_init_rx_agg		= r88eu_init_rx_agg;
-	sc->sc_init_ampdu		= rtwn_nop_softc;
-	sc->sc_init_intr		= r88eu_init_intr;
-	sc->sc_init_edca		= r92c_init_edca;
-	sc->sc_init_bb			= r88eu_init_bb;
+	sc->sc_init_tx_agg		= rtwn_nop_softc;
+	sc->sc_init_rx_agg		= rtwn_nop_softc;
+	sc->sc_init_ampdu		= r92ce_init_ampdu;
+	sc->sc_init_intr		= r88ee_init_intr;
+	sc->sc_init_edca		= r92ce_init_edca;
+	sc->sc_init_bb			= r88ee_init_bb;
 	sc->sc_init_rf			= r92c_init_rf;
 	sc->sc_init_antsel		= rtwn_nop_softc;
-	sc->sc_post_init		= r88eu_post_init;
+	sc->sc_post_init		= r88ee_post_init;
 	sc->sc_init_bcnq1_boundary	= rtwn_nop_int_softc;
 
 	sc->mac_prog			= &rtl8188e_mac[0];
@@ -195,18 +201,21 @@ r88eu_attach(struct rtwn_usb_softc *uc)
 	sc->agc_size			= nitems(rtl8188e_agc);
 	sc->rf_prog			= &rtl8188e_rf[0];
 
-	sc->name			= "RTL8188EU";
-	sc->fwname			= "rtwn-rtl8188eufw";
+	sc->name			= "RTL8188EE";
+	sc->fwname			= "rtwn-rtl8188eefw";
 	sc->fwsig			= 0x88e;
 
-	sc->page_count			= R88EU_TX_PAGE_COUNT;
+	sc->page_count			= R88EE_TX_PAGE_COUNT;
 	sc->pktbuf_count		= R88E_TXPKTBUF_COUNT;
 
 	sc->ackto			= 0x40;
-	sc->npubqpages			= R88EU_PUBQ_NPAGES;
+	sc->npubqpages			= R88EE_PUBQ_NPAGES;
+	sc->nhqpages			= R88EE_HPQ_NPAGES;
+	sc->nnqpages			= R88EE_NPQ_NPAGES;
+	sc->nlqpages			= R88EE_LPQ_NPAGES;
 	sc->page_size			= R92C_TX_PAGE_SIZE;
 
-	sc->txdesc_len			= sizeof(struct r92cu_tx_desc);
+	sc->txdesc_len			= sizeof(struct r92ce_tx_desc);
 	sc->efuse_maxlen		= R88E_EFUSE_MAX_LEN;
 	sc->efuse_maplen		= R88E_EFUSE_MAP_LEN;
 	sc->rx_dma_size			= R88E_RX_DMA_BUFFER_SIZE;
@@ -217,11 +226,17 @@ r88eu_attach(struct rtwn_usb_softc *uc)
 	sc->temp_delta			= R88E_CALIB_THRESHOLD;
 
 	sc->bcn_status_reg[0]		= R92C_TDECTRL;
+	/*
+	 * TODO: some additional setup is required
+	 * to maintain few beacons at the same time.
+	 *
+	 * XXX BCNQ1 mechanism is not needed here; move it to the USB module.
+	 */
 	sc->bcn_status_reg[1]		= R92C_TDECTRL;
 	sc->rcr				= 0;
 
 	sc->ntxchains			= 1;
 	sc->nrxchains			= 1;
 
-	r88eu_attach_private(sc);
+	r88ee_attach_private(sc);
 }
