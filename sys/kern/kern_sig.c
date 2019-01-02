261ef81beafdd345090d5fd259ab07f4ab26ff2a
jhb <jhb@FreeBSD.org>
2019-01-02 19:11:49 +0000

    MFC 341800: Don't report stale signal information in ptrace_lwpinfo.
    Once a signal's siginfo was copied to 'td_si' as part of the signal
    exchange in issignal(), it was never cleared.  This caused future
    thread events that are reported as SIGTRAP events without signal
    information to report the stale siginfo in 'td_si'.  For example, if a
    debugger created a new process and used SIGSTOP to stop it after
    PT_ATTACH, future system call entry / exit events would set PL_FLAG_SI
    with the SIGSTOP siginfo in pl_siginfo.  This broke 'catch syscall' in
    current versions of gdb as it assumed PL_FLAG_SI with SIGTRAP
    indicates a breakpoint or single step trap.

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -2858,6 +2858,8 @@ issignal(struct thread *td)
 			sig = ptracestop(td, sig, &ksi);
 			mtx_lock(&ps->ps_mtx);
 
+			td->td_si.si_signo = 0;
+
 			/* 
 			 * Keep looking if the debugger discarded or
 			 * replaced the signal.
