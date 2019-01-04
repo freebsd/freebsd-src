85037a09f3a0ceae9bd84ac55f9497735a06ce7f
markj <markj@FreeBSD.org>
2019-01-04 17:31:50 +0000

    Support MSG_DONTWAIT in send*(2).
    As it does for recv*(2), MSG_DONTWAIT indicates that the call should
    not block, returning EAGAIN instead.  Linux and OpenBSD both implement
    this, so the change makes porting easier, especially since we do not
    return EINVAL or so when unrecognized flags are specified.
    
    Submitted by:	Greg V <greg@unrelenting.technology>
    Reviewed by:	tuexen
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D18728

--- sys/netinet/sctp_output.c
+++ sys/netinet/sctp_output.c
@@ -12836,7 +12836,7 @@ sctp_lower_sosend(struct socket *so,
 		}
 	}
 	if (SCTP_SO_IS_NBIO(so)
-	    || (flags & MSG_NBIO)
+	    || (flags & (MSG_NBIO | MSG_DONTWAIT)) != 0
 	    ) {
 		non_blocking = 1;
 	}
