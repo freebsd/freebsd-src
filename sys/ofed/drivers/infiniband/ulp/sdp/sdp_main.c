2f2ddd68a556157fef2a527b95414dd2bad00ded
Mark Johnston <markj@FreeBSD.org>
2019-01-04 17:31:50 +0000

    Support MSG_DONTWAIT in send*(2).
    As it does for recv*(2), MSG_DONTWAIT indicates that the call should
    not block, returning EAGAIN instead.  Linux and OpenBSD both implement
    this, so the change makes porting easier, especially since we do not
    return EINVAL or so when unrecognized flags are specified.
    
    Submitted by:	Greg V <greg@unrelenting.technology>
    Reviewed by:	tuexen
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D18728

--- sys/ofed/drivers/infiniband/ulp/sdp/sdp_main.c
+++ sys/ofed/drivers/infiniband/ulp/sdp/sdp_main.c
@@ -1124,7 +1124,8 @@ restart:
 		}
 		if (space < resid &&
 		    (atomic || space < so->so_snd.sb_lowat)) {
-			if ((so->so_state & SS_NBIO) || (flags & MSG_NBIO)) {
+			if ((so->so_state & SS_NBIO) ||
+			    (flags & (MSG_NBIO | MSG_DONTWAIT)) != 0) {
 				SOCKBUF_UNLOCK(&so->so_snd);
 				error = EWOULDBLOCK;
 				goto release;
