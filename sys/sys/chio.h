c53faa45c5bb656cf2a22264227a79eeeda5f0f2
joerg <joerg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-01-05 21:07:49 +0000

    Fix an old typo in the element status display bits:
    INEAB -> INENAB (import is eNabled)
    
    Note that this kernel definition is exported into chio(1), and used
    for element display there.
    
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@342791 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/chio.h
+++ sys/sys/chio.h
@@ -266,7 +266,7 @@ struct changer_set_voltag_request {
 
 
 #define	CESTATUS_BITS	\
-	"\20\6INEAB\5EXENAB\4ACCESS\3EXCEPT\2IMPEXP\1FULL"
+	"\20\6INENAB\5EXENAB\4ACCESS\3EXCEPT\2IMPEXP\1FULL"
 
 #define	CHIOMOVE	_IOW('c', 0x01, struct changer_move)
 #define	CHIOEXCHANGE	_IOW('c', 0x02, struct changer_exchange)
