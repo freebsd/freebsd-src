fdd7eabaaf305ab6826f4727905c3c4ef65c8062
kevans <kevans@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-01-10 03:27:20 +0000

    libbe(3): Change be_mount to mount/unmount child datasets
    This set of changes is geared towards making bectl respect deep boot
    environments when they exist and are mounted. The deep BE composition
    functionality (`bectl add`) remains disabled for the time being. This set of
    changes has no effect for the average user. but allows deep BE users to
    upgrade properly with their current setup.
    
    libbe(3): Open the target boot environment and get a zfs handle, then pass
    that with the target mountpoint to be_mount_iter; If the BE_MNT_DEEP flag is
    set call zfs_iter_filesystems and mount the child datasets.
    
    Similar logic is employed when unmounting the datasets, save for children
    are unmounted first.
    
    bectl(8): Change bectl_cmd_jail to pass the BE_MNT_DEEP flag when
    calling be_mount as well as call be_unmount when cleaning up after the
    jail has exited instead of umount(2) directly.
    
    PR:		234795
    Submitted by:	Wes Maag <jwmaag_gmail.com> (test additions by kevans)
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D18796
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@342911 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/bectl/tests/bectl_test.sh
+++ sbin/bectl/tests/bectl_test.sh
@@ -42,6 +42,20 @@ bectl_create_setup()
 	atf_check zfs create -o mountpoint=/ -o canmount=noauto \
 	    ${zpool}/ROOT/default
 }
+bectl_create_deep_setup()
+{
+	zpool=$1
+	disk=$2
+	mnt=$3
+
+	bectl_create_setup ${zpool} ${disk} ${mnt}
+	atf_check mkdir -p ${root}
+	atf_check -o ignore bectl -r ${zpool}/ROOT mount default ${root}
+	atf_check mkdir -p ${root}/usr
+	atf_check zfs create -o mountpoint=/usr -o canmount=noauto \
+	    ${zpool}/ROOT/default/usr
+	atf_check -o ignore bectl -r ${zpool}/ROOT umount default
+}
 
 bectl_cleanup()
 {
@@ -183,7 +197,7 @@ bectl_mount_body()
 	mount=${cwd}/mnt
 	root=${mount}/root
 
-	bectl_create_setup ${zpool} ${disk} ${mount}
+	bectl_create_deep_setup ${zpool} ${disk} ${mount}
 	atf_check mkdir -p ${root}
 	# Test unmount first...
 	atf_check -o not-empty bectl -r ${zpool}/ROOT mount default ${root}
@@ -246,7 +260,7 @@ bectl_jail_body()
 	if [ ! -f /rescue/rescue ]; then
 		atf_skip "This test requires a rescue binary"
 	fi
-	bectl_create_setup ${zpool} ${disk} ${mount}
+	bectl_create_deep_setup ${zpool} ${disk} ${mount}
 	# Prepare our minimal BE... plop a rescue binary into it
 	atf_check mkdir -p ${root}
 	atf_check -o ignore bectl -r ${zpool}/ROOT mount default ${root}
@@ -263,9 +277,9 @@ bectl_jail_body()
 	atf_check -o empty -s exit:0 bectl -r ${zpool}/ROOT unjail default
 
 	# Basic command-mode tests, with and without jail cleanup
-	atf_check -o inline:"rescue\n" bectl -r ${zpool}/ROOT \
+	atf_check -o inline:"rescue\nusr\n" bectl -r ${zpool}/ROOT \
 	    jail default /rescue/rescue ls -1
-	atf_check -o inline:"rescue\n" bectl -r ${zpool}/ROOT \
+	atf_check -o inline:"rescue\nusr\n" bectl -r ${zpool}/ROOT \
 	    jail -Uo path=${root} default /rescue/rescue ls -1
 	atf_check [ -f ${root}/rescue/rescue ]
 	atf_check bectl -r ${zpool}/ROOT ujail default
