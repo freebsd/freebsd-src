b3c0d957a2e716b4d8180ada511f54ee673a9e1e
Andrew Turner <andrew@FreeBSD.org>
2019-01-12 11:21:28 +0000

    Add support for the Clang Coverage Sanitizer in the kernel (KCOV).
    When building with KCOV enabled the compiler will insert function calls
    to probes allowing us to trace the execution of the kernel from userspace.
    These probes are on function entry (trace-pc) and on comparison operations
    (trace-cmp).
    
    Userspace can enable the use of these probes on a single kernel thread with
    an ioctl interface. It can allocate space for the probe with KIOSETBUFSIZE,
    then mmap the allocated buffer and enable tracing with KIOENABLE, with the
    trace mode being passed in as the int argument. When complete KIODISABLE
    is used to disable tracing.
    
    The first item in the buffer is the number of trace event that have
    happened. Userspace can write 0 to this to reset the tracing, and is
    expected to do so on first use.
    
    The format of the buffer depends on the trace mode. When in PC tracing just
    the return address of the probe is stored. Under comparison tracing the
    comparison type, the two arguments, and the return address are traced. The
    former method uses on entry per trace event, while the later uses 4. As
    such they are incompatible so only a single mode may be enabled.
    
    KCOV is expected to help fuzzing the kernel, and while in development has
    already found a number of issues. It is required for the syzkaller system
    call fuzzer [1]. Other kernel fuzzers could also make use of it, either
    with the current interface, or by extending it with new modes.
    
    A man page is currently being worked on and is expected to be committed
    soon, however having the code in the kernel now is useful for other
    developers to use.
    
    [1] https://github.com/google/syzkaller
    
    Submitted by:	Mitchell Horne <mhorne063@gmail.com> (Earlier version)
    Reviewed by:	kib
    Testing by:	tuexen
    Sponsored by:	DARPA, AFRL
    Sponsored by:	The FreeBSD Foundation (Mitchell Horne)
    Differential Revision:	https://reviews.freebsd.org/D14599

--- sys/kern/kern_thread.c
+++ sys/kern/kern_thread.c
@@ -82,9 +82,9 @@ _Static_assert(offsetof(struct thread, td_flags) == 0xfc,
     "struct thread KBI td_flags");
 _Static_assert(offsetof(struct thread, td_pflags) == 0x104,
     "struct thread KBI td_pflags");
-_Static_assert(offsetof(struct thread, td_frame) == 0x470,
+_Static_assert(offsetof(struct thread, td_frame) == 0x478,
     "struct thread KBI td_frame");
-_Static_assert(offsetof(struct thread, td_emuldata) == 0x528,
+_Static_assert(offsetof(struct thread, td_emuldata) == 0x530,
     "struct thread KBI td_emuldata");
 _Static_assert(offsetof(struct proc, p_flag) == 0xb0,
     "struct proc KBI p_flag");
