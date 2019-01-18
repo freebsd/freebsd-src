dc3ff827b27d46d08493829043462ee3bc1acf93
dab <dab@FreeBSD.org>
2019-01-18 15:59:49 +0000

    MFC r342770:
    asmc: Patch to add MacBook Pro 9,2 support
    
    PR:   	       211513
    Submitted by:  William Theesfeld Jr <wtheesfeld@mailbox.org>
    Reported by:   William Theesfeld Jr <wtheesfeld@mailbox.org>

--- sys/dev/asmc/asmcvar.h
+++ sys/dev/asmc/asmcvar.h
@@ -255,6 +255,27 @@ struct asmc_softc {
 				  "TMBS", "TP0P", "TPCD", "TW0P", "Th1H", \
 				  "Th2H", "Tm0P", "Ts0P", "Ts0S" }
 
+#define ASMC_MBP9_TEMPS		{ "Ts0P", "Ts0S", "TA0P", "TB1T", "TB2T", \
+				  "TB0T", "TC1C", "TC2C", "TC0E", "TC0F", \
+				  "TC0J", "TC0P", "TCFC", "TCGC", "TCSA", \
+				  "TCTD", "TCXC", "TG1D", "TM0P", "TM0S", \
+				  "TPCD", NULL }
+
+#define ASMC_MBP9_TEMPNAMES	{ "Ts0P", "Ts0S", "TA0P", "TB1T", "TB2T", \
+				  "TB0T", "TC1C", "TC2C", "TC0E", "TC0F", \
+				  "TC0J", "TC0P", "TCFC", "TCGC", "TCSA", \
+				  "TCTD", "TCXC", "TG1D", "TM0P", "TM0S", \
+				  "TPCD" }
+
+#define ASMC_MBP9_TEMPDESCS	{ "Palm Rest", "Memory Proximity", "Airflow 1", \
+				  "Battery 1", "Battery 2", "Battery TS_MAX", \
+				  "CPU Core 1", "CPU Core 2", "CPU1", "CPU1", \
+				  "TC0J", "CPU 1 Proximity", "TCFC", \
+				  "PECI GPU", "PECI SA", "TCTD", "PECI CPU", \
+				  "GPU Die", "Memory Bank A1", "Memory Module A1", \
+				  "PCH Die" }
+
+
 #define ASMC_MBP112_TEMPS	{ "TB0T", "TB1T", "TB2T", "TBXT", "TC0E", \
 				  "TC0F", "TC0P", "TC1C", "TC2C", "TC3C", \
 				  "TC4C", "TCFC", "TCGC", "TCSA", "TCTD", \
