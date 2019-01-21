33ae5285c5f6b1124cc40a18cda5a62a239ef29a
Kyle Evans <kevans@FreeBSD.org>
2019-01-21 02:57:57 +0000

    MFC r342903, r342911: libbe(3)/bectl(8) refactor and fix mount for deep BEs
    r342903: libbe(3): move altroot augmentation bits around a little bit
    
    We could perhaps have a method that does this given a dataset, but it's yet
    clear that we'll always want to bypass the altroot when we grab the
    mountpoint. For now, we'll refactor things a bit so we grab the altroot
    length when libbe is initialized and have a common method that does the
    necessary augmentation (replace with / if it's the root, return a pointer to
    later in the string if not).
    
    This will be used in some upcoming work to make be_mount work properly for
    deep BEs.
    
    r342911: libbe(3): Change be_mount to mount/unmount child datasets
    
    This set of changes is geared towards making bectl respect deep boot
    environments when they exist and are mounted. The deep BE composition
    functionality (`bectl add`) remains disabled for the time being. This set of
    changes has no effect for the average user. but allows deep BE users to
    upgrade properly with their current setup.
    
    libbe(3): Open the target boot environment and get a zfs handle, then pass
    that with the target mountpoint to be_mount_iter; If the BE_MNT_DEEP flag is
    set call zfs_iter_filesystems and mount the child datasets.
    
    Similar logic is employed when unmounting the datasets, save for children
    are unmounted first.
    
    bectl(8): Change bectl_cmd_jail to pass the BE_MNT_DEEP flag when
    calling be_mount as well as call be_unmount when cleaning up after the
    jail has exited instead of umount(2) directly.
    
    PR:		234795

--- lib/libbe/be.c
+++ lib/libbe/be.c
@@ -90,6 +90,7 @@ be_locate_rootfs(libbe_handle_t *lbh)
 libbe_handle_t *
 libbe_init(const char *root)
 {
+	char altroot[MAXPATHLEN];
 	libbe_handle_t *lbh;
 	char *poolname, *pos;
 	int pnamelen;
@@ -139,6 +140,11 @@ libbe_init(const char *root)
 	if (zpool_get_prop(lbh->active_phandle, ZPOOL_PROP_BOOTFS, lbh->bootfs,
 	    sizeof(lbh->bootfs), NULL, true) != 0)
 		goto err;
+
+	if (zpool_get_prop(lbh->active_phandle, ZPOOL_PROP_ALTROOT,
+	    altroot, sizeof(altroot), NULL, true) == 0 &&
+	    strcmp(altroot, "-") != 0)
+		lbh->altroot_len = strlen(altroot);
 
 	return (lbh);
 err:
@@ -314,7 +320,6 @@ be_create(libbe_handle_t *lbh, const char *name)
 	return (set_error(lbh, err));
 }
 
-
 static int
 be_deep_clone_prop(int prop, void *cb)
 {
@@ -345,12 +350,9 @@ be_deep_clone_prop(int prop, void *cb)
 
 	/* Augment mountpoint with altroot, if needed */
 	val = pval;
-	if (prop == ZFS_PROP_MOUNTPOINT && *dccb->altroot != '\0') {
-		if (pval[strlen(dccb->altroot)] == '\0')
-			strlcpy(pval, "/", sizeof(pval));
-		else
-			val = pval + strlen(dccb->altroot);
-	}
+	if (prop == ZFS_PROP_MOUNTPOINT)
+		val = be_mountpoint_augmented(dccb->lbh, val);
+
 	nvlist_add_string(dccb->props, zfs_prop_to_name(prop), val);
 
 	return (ZPROP_CONT);
@@ -392,12 +394,9 @@ be_deep_clone(zfs_handle_t *ds, void *data)
 	nvlist_alloc(&props, NV_UNIQUE_NAME, KM_SLEEP);
 	nvlist_add_string(props, "canmount", "noauto");
 
+	dccb.lbh = isdc->lbh;
 	dccb.zhp = ds;
 	dccb.props = props;
-	if (zpool_get_prop(isdc->lbh->active_phandle, ZPOOL_PROP_ALTROOT,
-	    dccb.altroot, sizeof(dccb.altroot), NULL, true) != 0 ||
-	    strcmp(dccb.altroot, "-") == 0)
-		*dccb.altroot = '\0';
 	if (zprop_iter(be_deep_clone_prop, &dccb, B_FALSE, B_FALSE,
 	    ZFS_TYPE_FILESYSTEM) == ZPROP_INVAL)
 		return (-1);
