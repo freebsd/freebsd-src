a5c195d21f732246fa52d87824fc98f3d920c04a
mav <mav@FreeBSD.org>
2019-01-22 21:35:25 +0000

    MFC r342977 (by cem): amdtemp(4): Add support for Family 15h, Model >=60h
    Family 15h is a bit of an oddball.  Early models used the same temperature
    register and spec (mostly[1]) as earlier CPU families.
    
    Model 60h-6Fh and 70-7Fh use something more like Family 17h's Service
    Management Network, communicating with it in a similar fashion.  To support
    them, add support for their version of SMU indirection to amdsmn(4) and use
    it in amdtemp(4) on these models.
    
    While here, clarify some of the deviceid macros in amdtemp(4) that were
    added with arbitrary, incorrect family numbers, and remove ones that were
    not used.  Additionally, clarify intent and condition of heterogenous
    multi-socket system detection.
    
    [1]: 15h adds the "adjust range by -49°C if a certain condition is met,"
    which previous families did not have.
    
    Reported by:    D. C. <tjoard AT gmail.com>
    PR:             234657
    Tested by:      D. C. <tjoard AT gmail.com>

--- sys/dev/amdsmn/amdsmn.c
+++ sys/dev/amdsmn/amdsmn.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2017 Conrad Meyer <cem@FreeBSD.org>
+ * Copyright (c) 2017-2019 Conrad Meyer <cem@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,7 +25,7 @@
  */
 
 /*
- * Driver for the AMD Family 17h CPU System Management Network.
+ * Driver for the AMD Family 15h and 17h CPU System Management Network.
  */
 
 #include <sys/cdefs.h>
@@ -51,24 +51,45 @@ __FBSDID("$FreeBSD$");
 
 #include <dev/amdsmn/amdsmn.h>
 
-#define	SMN_ADDR_REG	0x60
-#define	SMN_DATA_REG	0x64
+#define	F15H_SMN_ADDR_REG	0xb8
+#define	F15H_SMN_DATA_REG	0xbc
+#define	F17H_SMN_ADDR_REG	0x60
+#define	F17H_SMN_DATA_REG	0x64
 
+#define	PCI_DEVICE_ID_AMD_15H_M60H_ROOT		0x1576
 #define	PCI_DEVICE_ID_AMD_17H_ROOT		0x1450
-#define	PCI_DEVICE_ID_AMD_17H_ROOT_DF_F3	0x1463
 #define	PCI_DEVICE_ID_AMD_17H_M10H_ROOT		0x15d0
-#define	PCI_DEVICE_ID_AMD_17H_M10H_ROOT_DF_F3	0x15eb
 
+struct pciid;
 struct amdsmn_softc {
 	struct mtx smn_lock;
+	const struct pciid *smn_pciid;
 };
 
-static struct pciid {
+static const struct pciid {
 	uint16_t	amdsmn_vendorid;
 	uint16_t	amdsmn_deviceid;
+	uint8_t		amdsmn_addr_reg;
+	uint8_t		amdsmn_data_reg;
 } amdsmn_ids[] = {
-	{ CPU_VENDOR_AMD, PCI_DEVICE_ID_AMD_17H_ROOT },
-	{ CPU_VENDOR_AMD, PCI_DEVICE_ID_AMD_17H_M10H_ROOT },
+	{
+		.amdsmn_vendorid = CPU_VENDOR_AMD,
+		.amdsmn_deviceid = PCI_DEVICE_ID_AMD_15H_M60H_ROOT,
+		.amdsmn_addr_reg = F15H_SMN_ADDR_REG,
+		.amdsmn_data_reg = F15H_SMN_DATA_REG,
+	},
+	{
+		.amdsmn_vendorid = CPU_VENDOR_AMD,
+		.amdsmn_deviceid = PCI_DEVICE_ID_AMD_17H_ROOT,
+		.amdsmn_addr_reg = F17H_SMN_ADDR_REG,
+		.amdsmn_data_reg = F17H_SMN_DATA_REG,
+	},
+	{
+		.amdsmn_vendorid = CPU_VENDOR_AMD,
+		.amdsmn_deviceid = PCI_DEVICE_ID_AMD_17H_M10H_ROOT,
+		.amdsmn_addr_reg = F17H_SMN_ADDR_REG,
+		.amdsmn_data_reg = F17H_SMN_DATA_REG,
+	},
 };
 
 /*
@@ -99,7 +120,7 @@ DRIVER_MODULE(amdsmn, hostb, amdsmn_driver, amdsmn_devclass, NULL, NULL);
 MODULE_VERSION(amdsmn, 1);
 
 static bool
-amdsmn_match(device_t parent)
+amdsmn_match(device_t parent, const struct pciid **pciid_out)
 {
 	uint16_t vendor, device;
 	size_t i;
@@ -107,10 +128,14 @@ amdsmn_match(device_t parent)
 	vendor = pci_get_vendor(parent);
 	device = pci_get_device(parent);
 
-	for (i = 0; i < nitems(amdsmn_ids); i++)
+	for (i = 0; i < nitems(amdsmn_ids); i++) {
 		if (vendor == amdsmn_ids[i].amdsmn_vendorid &&
-		    device == amdsmn_ids[i].amdsmn_deviceid)
+		    device == amdsmn_ids[i].amdsmn_deviceid) {
+			if (pciid_out != NULL)
+				*pciid_out = &amdsmn_ids[i];
 			return (true);
+		}
+	}
 	return (false);
 }
 
@@ -122,7 +147,7 @@ amdsmn_identify(driver_t *driver, device_t parent)
 	/* Make sure we're not being doubly invoked. */
 	if (device_find_child(parent, "amdsmn", -1) != NULL)
 		return;
-	if (!amdsmn_match(parent))
+	if (!amdsmn_match(parent, NULL))
 		return;
 
 	child = device_add_child(parent, "amdsmn", -1);
@@ -134,21 +159,25 @@ static int
 amdsmn_probe(device_t dev)
 {
 	uint32_t family;
+	char buf[64];
 
 	if (resource_disabled("amdsmn", 0))
 		return (ENXIO);
-	if (!amdsmn_match(device_get_parent(dev)))
+	if (!amdsmn_match(device_get_parent(dev), NULL))
 		return (ENXIO);
 
 	family = CPUID_TO_FAMILY(cpu_id);
 
 	switch (family) {
+	case 0x15:
 	case 0x17:
 		break;
 	default:
 		return (ENXIO);
 	}
-	device_set_desc(dev, "AMD Family 17h System Management Network");
+	snprintf(buf, sizeof(buf), "AMD Family %xh System Management Network",
+	    family);
+	device_set_desc_copy(dev, buf);
 
 	return (BUS_PROBE_GENERIC);
 }
@@ -157,6 +186,9 @@ static int
 amdsmn_attach(device_t dev)
 {
 	struct amdsmn_softc *sc = device_get_softc(dev);
+
+	if (!amdsmn_match(device_get_parent(dev), &sc->smn_pciid))
+		return (ENXIO);
 
 	mtx_init(&sc->smn_lock, "SMN mtx", "SMN", MTX_DEF);
 	return (0);
@@ -180,8 +212,8 @@ amdsmn_read(device_t dev, uint32_t addr, uint32_t *value)
 	parent = device_get_parent(dev);
 
 	mtx_lock(&sc->smn_lock);
-	pci_write_config(parent, SMN_ADDR_REG, addr, 4);
-	*value = pci_read_config(parent, SMN_DATA_REG, 4);
+	pci_write_config(parent, sc->smn_pciid->amdsmn_addr_reg, addr, 4);
+	*value = pci_read_config(parent, sc->smn_pciid->amdsmn_data_reg, 4);
 	mtx_unlock(&sc->smn_lock);
 
 	return (0);
@@ -196,8 +228,8 @@ amdsmn_write(device_t dev, uint32_t addr, uint32_t value)
 	parent = device_get_parent(dev);
 
 	mtx_lock(&sc->smn_lock);
-	pci_write_config(parent, SMN_ADDR_REG, addr, 4);
-	pci_write_config(parent, SMN_DATA_REG, value, 4);
+	pci_write_config(parent, sc->smn_pciid->amdsmn_addr_reg, addr, 4);
+	pci_write_config(parent, sc->smn_pciid->amdsmn_data_reg, value, 4);
 	mtx_unlock(&sc->smn_lock);
 
 	return (0);
