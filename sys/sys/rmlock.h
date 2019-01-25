7bd04292ce7cb59ac22953d14d20a62a9fadb6c5
Konstantin Belousov <kib@FreeBSD.org>
2019-01-25 10:58:34 +0000

    MFC r343302: Remove unused *_sysinit_flags() declarations.

--- sys/sys/rmlock.h
+++ sys/sys/rmlock.h
@@ -54,7 +54,6 @@ void	rm_init_flags(struct rmlock *rm, const char *name, int opts);
 void	rm_destroy(struct rmlock *rm);
 int	rm_wowned(const struct rmlock *rm);
 void	rm_sysinit(void *arg);
-void	rm_sysinit_flags(void *arg);
 
 void	_rm_wlock_debug(struct rmlock *rm, const char *file, int line);
 void	_rm_wunlock_debug(struct rmlock *rm, const char *file, int line);
