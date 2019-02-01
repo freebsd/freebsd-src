3c77f34940fb881dbcb7969c5c6b189a743ad0b2
Vincenzo Maffione <vmaffione@FreeBSD.org>
2019-02-01 12:24:56 +0000

    MFC r343552
    vtnet: fix typo in vtnet_free_taskqueues
    
    Because of a typo, the code was mistakenly resetting the
    vtnrx_vq pointer rather than vtntx_tq.
    
    Reviewed by:    bryanv
    Differential Revision:  https://reviews.freebsd.org/D19015

--- sys/dev/virtio/network/if_vtnet.c
+++ sys/dev/virtio/network/if_vtnet.c
@@ -2742,7 +2742,7 @@ vtnet_free_taskqueues(struct vtnet_softc *sc)
 		rxq = &sc->vtnet_rxqs[i];
 		if (rxq->vtnrx_tq != NULL) {
 			taskqueue_free(rxq->vtnrx_tq);
-			rxq->vtnrx_vq = NULL;
+			rxq->vtnrx_tq = NULL;
 		}
 
 		txq = &sc->vtnet_txqs[i];
