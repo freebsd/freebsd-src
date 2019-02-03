freebsd/freebsd-src

commit 0f88bcfa0551ef98e36a2a35b8f2fc1eead2eeb9

Fix crash in systat(4) when certain commands are called without arguments
Add check for missing arguments to dsmatchselect and dsselect

PR:		219689
Submitted by:	Marko Turk <mt@markoturk.info>
MFC after:	1 week


git-svn-id: svn+ssh://svn.freebsd.org/base/head@343222 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

usr.bin/systat/devs.c
@@ -193,6 +193,11 @@ dsmatchselect(const char *args, devstat_select_mode select_mode, int maxshowdevs
 	int i;
 	int retval = 0;
 
+	if (!args) {
+		warnx("dsmatchselect: no arguments");
+		return(0);
+	}
+
 	/*
 	 * Break the (pipe delimited) input string out into separate
 	 * strings.
@@ -250,6 +255,11 @@ dsselect(const char *args, devstat_select_mode select_mode, int maxshowdevs,
 	char *cp, *tmpstr, *tmpstr1, *buffer;
 	int i;
 	int retval = 0;
+
+	if (!args) {
+		warnx("dsselect: no argument");
+		return(1);
+	}
 
 	/*
 	 * If we've gone through this code before, free previously

commit 1ca9ffe31d03fcad20784d667012f926cde976e0

MFC r343222-r343223, r343338
r343222:
Fix crash in systat(4) when certain commands are called without arguments

Add check for missing arguments to dsmatchselect and dsselect

PR:		219689
Submitted by:	Marko Turk <mt@markoturk.info>

r343223:
Fix inconsistency in return values introduced by r343222

Consistently return 1 or the case of missing arguments in both functions

PR:		219689
X-MFC-With:	343222

r343338:
Fix systat's :only command parser for the multiple arguments case

According to systat(1) :only option is supposed to accept multiple drives
but the parser for its arguments stops after first entry. Fix the parser
logic to accept multiple drives.

PR:		59220
Reported by:	Andy Farkas <andyf@speednet.com.au>

usr.bin/systat/devs.c
@@ -193,6 +193,11 @@ dsmatchselect(const char *args, devstat_select_mode select_mode, int maxshowdevs
 	int i;
 	int retval = 0;
 
+	if (!args) {
+		warnx("dsmatchselect: no arguments");
+		return(1);
+	}
+
 	/*
 	 * Break the (pipe delimited) input string out into separate
 	 * strings.
@@ -251,6 +256,11 @@ dsselect(const char *args, devstat_select_mode select_mode, int maxshowdevs,
 	int i;
 	int retval = 0;
 
+	if (!args) {
+		warnx("dsselect: no argument");
+		return(1);
+	}
+
 	/*
 	 * If we've gone through this code before, free previously
 	 * allocated resources.
@@ -278,7 +288,7 @@ dsselect(const char *args, devstat_select_mode select_mode, int maxshowdevs,
 			;
 		if (*cp)
 			*cp++ = '\0';
-		if (cp - args == 0)
+		if (cp - tmpstr1 == 0)
 			break;
 		for (i = 0; i < num_devices; i++) {
 			asprintf(&buffer, "%s%d", dev_select[i].device_name,
@@ -302,7 +312,7 @@ dsselect(const char *args, devstat_select_mode select_mode, int maxshowdevs,
 		}
 		if (i >= num_devices)
 			error("%s: unknown drive", args);
-		args = cp;
+		tmpstr1 = cp;
 	}
 	free(tmpstr);
 