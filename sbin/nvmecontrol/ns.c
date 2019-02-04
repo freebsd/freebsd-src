52467047aa7dd6d21f6303b28e4278cab2d8fe48
Warner Losh <imp@FreeBSD.org>
2019-02-04 21:28:25 +0000

    Regularize the Netflix copyright
    Use recent best practices for Copyright form at the top of
    the license:
    1. Remove all the All Rights Reserved clauses on our stuff. Where we
       piggybacked others, use a separate line to make things clear.
    2. Use "Netflix, Inc." everywhere.
    3. Use a single line for the copyright for grep friendliness.
    4. Use date ranges in all places for our stuff.
    
    Approved by: Netflix Legal (who gave me the form), adrian@ (pmc files)

--- sbin/nvmecontrol/ns.c
+++ sbin/nvmecontrol/ns.c
@@ -1,7 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
- * Copyright (c) 2017 Netflix, Inc
+ * Copyright (c) 2017 Netflix, Inc.
  * Copyright (C) 2018 Alexander Motin <mav@FreeBSD.org>
  *
  * Redistribution and use in source and binary forms, with or without
--- stand/efi/libefi/env.c
+++ stand/efi/libefi/env.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Netflix, Inc. All Rights Reserved.
+ * Copyright (c) 2015 Netflix, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
