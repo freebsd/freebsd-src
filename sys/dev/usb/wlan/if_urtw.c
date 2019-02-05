40be36a4171f6e3b21ca32a3169a5c5d2f178981
Andriy Voskoboinyk <avos@FreeBSD.org>
2019-02-05 03:01:10 +0000

    MFC r343541: Drop some unneeded includes from wireless USB drivers.

--- sys/dev/usb/wlan/if_urtw.c
+++ sys/dev/usb/wlan/if_urtw.c
@@ -34,10 +34,6 @@ __FBSDID("$FreeBSD$");
 #include <sys/endian.h>
 #include <sys/kdb.h>
 
-#include <machine/bus.h>
-#include <machine/resource.h>
-#include <sys/rman.h>
-
 #include <net/if.h>
 #include <net/if_var.h>
 #include <net/if_arp.h>
