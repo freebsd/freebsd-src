f97a5ab9bad669e9a31318664be42db80a2d3cc7
Andriy Voskoboinyk <avos@FreeBSD.org>
2019-02-08 04:32:41 +0000

    MFC r343417: net80211: reuse TICKS_2_MSEC / MSEC_2_TICKS macros from sys/time.h
    Replace in-place implementation with system-wide one; since it
    guarantees non-zero result drop all less-than-one checks from
    drivers and net80211.

--- sys/dev/rtwn/pci/rtwn_pci_reg.c
+++ sys/dev/rtwn/pci/rtwn_pci_reg.c
@@ -118,6 +118,6 @@ rtwn_pci_delay(struct rtwn_softc *sc, int usec)
 		DELAY(usec);
 	else {
 		(void) mtx_sleep(sc, &sc->sc_mtx, 0, "rtwn_pci",
-		    MAX(msecs_to_ticks(usec / 1000), 1));
+		    msecs_to_ticks(usec / 1000));
 	}
 }
