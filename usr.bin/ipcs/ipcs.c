90d92a8d81df815e49013871b71cf89ccb216235
Andriy Voskoboinyk <avos@FreeBSD.org>
2019-02-12 07:03:37 +0000

    MFC r343870: ipcs(1): drop obsolete error checking
    This code is unreachable since r77551.
    
    PR:		201728

--- usr.bin/ipcs/ipcs.c
+++ usr.bin/ipcs/ipcs.c
@@ -198,7 +198,7 @@ main(int argc, char *argv[])
 	}
 
 	kget(X_MSGINFO, &msginfo, sizeof(msginfo));
-	if ((display & (MSGINFO | MSGTOTAL))) {
+	if (display & (MSGINFO | MSGTOTAL)) {
 		if (display & MSGTOTAL)
 			print_kmsqtotal(msginfo);
 
@@ -226,15 +226,10 @@ main(int argc, char *argv[])
 
 			printf("\n");
 		}
-	} else
-		if (display & (MSGINFO | MSGTOTAL)) {
-			fprintf(stderr,
-			    "SVID messages facility "
-			    "not configured in the system\n");
-		}
+	}
 
 	kget(X_SHMINFO, &shminfo, sizeof(shminfo));
-	if ((display & (SHMINFO | SHMTOTAL))) {
+	if (display & (SHMINFO | SHMTOTAL)) {
 
 		if (display & SHMTOTAL)
 			print_kshmtotal(shminfo);
@@ -261,15 +256,10 @@ main(int argc, char *argv[])
 			}
 			printf("\n");
 		}
-	} else
-		if (display & (SHMINFO | SHMTOTAL)) {
-			fprintf(stderr,
-			    "SVID shared memory facility "
-			    "not configured in the system\n");
-		}
+	}
 
 	kget(X_SEMINFO, &seminfo, sizeof(seminfo));
-	if ((display & (SEMINFO | SEMTOTAL))) {
+	if (display & (SEMINFO | SEMTOTAL)) {
 		struct semid_kernel *kxsema;
 		size_t kxsema_len;
 
@@ -298,12 +288,7 @@ main(int argc, char *argv[])
 
 			printf("\n");
 		}
-	} else
-		if (display & (SEMINFO | SEMTOTAL)) {
-			fprintf(stderr,
-			    "SVID semaphores facility "
-			    "not configured in the system\n");
-		}
+	}
 
 	if (!use_sysctl)
 		kvm_close(kd);
