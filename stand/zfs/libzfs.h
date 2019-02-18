8f71efa289b88e3a516b37cb44911d71c09a3fb2
kevans <kevans@FreeBSD.org>
2019-02-18 19:13:28 +0000

    MFC r329727-r329728: lualoader ZFS BE Support
    r329727:
    stand/zfs: Add all bootenvs to environment
    
    For the benefit of lualoader, add all bootenvs to environment when
    init_zfs_bootenv is invoked. All of the boot environment logic can then be
    implemented in pure lua, rather than going back and forth with C to
    implement paging.
    
    This stores all boot environments in bootenvs[idx] and the final count of
    bootenvs in bootenvs_count.
    
    While here, make a copy of currdev for init_zfs_bootenv since it will be
    modifying it and the caller may not necessarily want that. Some of the logic
    was shifted around so that the 'currdev' pointer remains at the beginning of
    the string and 'beroot' is moved around as needed to modify it or ultimately
    store it in zfs_be_root.
    
    The original zfs_bootenv that this was copied from will be able to go away
    only if/when forth eventually goes away.
    
    r329728:
    stand/zfs: Unbreak build, 'truct' ~= 'struct'

--- stand/zfs/libzfs.h
+++ stand/zfs/libzfs.h
@@ -75,7 +75,7 @@ char	*zfs_fmtdev(void *vdev);
 int	zfs_probe_dev(const char *devname, uint64_t *pool_guid);
 int	zfs_list(const char *name);
 uint64_t ldi_get_size(void *);
-void	init_zfs_bootenv(char *currdev);
+void	init_zfs_bootenv(const char *currdev);
 int	zfs_bootenv(const char *name);
 int	zfs_belist_add(const char *name, uint64_t __unused);
 int	zfs_set_env(void);
