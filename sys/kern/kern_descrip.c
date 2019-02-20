bdaa456d73ac6aac95a333678d2c4126cdb94f1f
markj <markj@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-02-20 18:29:52 +0000

    Remove an obsolete comment.
    MFC after:	3 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@344369 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/kern_descrip.c
+++ sys/kern/kern_descrip.c
@@ -2439,9 +2439,6 @@ fdcheckstd(struct thread *td)
  * Internal form of close.  Decrement reference count on file structure.
  * Note: td may be NULL when closing a file that was being passed in a
  * message.
- *
- * XXXRW: Giant is not required for the caller, but often will be held; this
- * makes it moderately likely the Giant will be recursed in the VFS case.
  */
 int
 closef(struct file *fp, struct thread *td)
