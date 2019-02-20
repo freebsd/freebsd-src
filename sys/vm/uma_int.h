214986293479574b25f4f9182d4f5a38f7216ad9
pfg <pfg@FreeBSD.org>
2019-02-20 14:12:25 +0000

    MFC r344042: UMA: unsign some variables related to allocation in hash_alloc().
    As a followup to r343673, unsign some variables related to allocation
    since the hashsize cannot be negative. This gives a bit more space to
    handle bigger allocations and avoid some implicit casting.
    
    While here also unsign uh_hashmask, it makes little sense to keep it
    signed.
    
    Differential Revision:	https://reviews.freebsd.org/D19148

--- sys/vm/uma_int.h
+++ sys/vm/uma_int.h
@@ -144,8 +144,8 @@ SLIST_HEAD(slabhead, uma_slab);
 
 struct uma_hash {
 	struct slabhead	*uh_slab_hash;	/* Hash table for slabs */
-	int		uh_hashsize;	/* Current size of the hash table */
-	int		uh_hashmask;	/* Mask used during hashing */
+	u_int		uh_hashsize;	/* Current size of the hash table */
+	u_int		uh_hashmask;	/* Mask used during hashing */
 };
 
 /*
@@ -386,7 +386,7 @@ static __inline uma_slab_t
 hash_sfind(struct uma_hash *hash, uint8_t *data)
 {
         uma_slab_t slab;
-        int hval;
+        u_int hval;
 
         hval = UMA_HASH(hash, data);
 
