66c63612827d84776b6cb2828a09532d16d7d626
mmacy <mmacy@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-02-22 23:15:32 +0000

    lkpi: allow late binding of linux_alloc_current
    Some consumers may be loosely coupled with the lkpi.
    This allows them to call linux_alloc_current without
    having a static dependency.
    
    Reviewed by:	hps@
    MFC after:	1 week
    Sponsored by:	iX Systems
    Differential Revision:	https://reviews.freebsd.org/D19257
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@344478 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -237,6 +237,13 @@ void	init_param2(long physpages);
 void	init_static_kenv(char *, size_t);
 void	tablefull(const char *);
 
+/*
+ * Allocate per-thread "current" state in the linuxkpi
+ */
+extern int (*lkpi_alloc_current)(struct thread *, int);
+int linux_alloc_current_noop(struct thread *, int);
+
+
 #if defined(KLD_MODULE) || defined(KTR_CRITICAL) || !defined(_KERNEL) || defined(GENOFFSET)
 #define critical_enter() critical_enter_KBI()
 #define critical_exit() critical_exit_KBI()
