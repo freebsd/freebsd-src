e2aca1189b947d09eb9af27cbbdbfbaf402fbcb6
cem <cem@FreeBSD.org>
2019-03-01 23:30:23 +0000

    Embedded chacha: Add 0-bit iv + 128-bit counter mode
    This mode might be suitable for a Fortuna keystream primitive.
    
    Reviewed by:	markm
    Sponsored by:	Dell EMC Isilon
    Differential Revision:	https://reviews.freebsd.org/D19410

--- sys/crypto/chacha20/chacha.h
+++ sys/crypto/chacha20/chacha.h
@@ -26,10 +26,19 @@ Public domain.
 #define LOCAL
 #endif
 
+#ifdef CHACHA_NONCE0_CTR128
+#define CHACHA_UNUSED __unused
+#else
+#define CHACHA_UNUSED
+#endif
+
 LOCAL void chacha_keysetup(struct chacha_ctx *x, const u_char *k, u_int kbits);
-LOCAL void chacha_ivsetup(struct chacha_ctx *x, const u_char *iv, const u_char *ctr);
+LOCAL void chacha_ivsetup(struct chacha_ctx *x, const u_char *iv CHACHA_UNUSED,
+    const u_char *ctr);
 LOCAL void chacha_encrypt_bytes(struct chacha_ctx *x, const u_char *m,
     u_char *c, u_int bytes);
+
+#undef CHACHA_UNUSED
 
 #endif	/* CHACHA_H */
 
