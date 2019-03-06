0e5381fea90778263958a2bb9ec257ff035e98af
markj <markj@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-03-06 16:50:14 +0000

    Reorder copyright lines to preserve the source of "All rights reserved."
    Reported by:	rgrimes
    MFC with:	r344829, r344830
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@344845 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/riscv/riscv/minidump_machdep.c
+++ sys/riscv/riscv/minidump_machdep.c
@@ -1,8 +1,8 @@
 /*-
  * Copyright (c) 2006 Peter Wemm
  * Copyright (c) 2015 The FreeBSD Foundation
- * Copyright (c) 2019 Mitchell Horne
  * All rights reserved.
+ * Copyright (c) 2019 Mitchell Horne
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
