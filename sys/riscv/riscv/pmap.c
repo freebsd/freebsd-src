6a69e0551ffe2f4c1584c099c0dcce60d613eb28
markj <markj@FreeBSD.org>
2019-03-06 00:01:06 +0000

    Implement minidump support for RISC-V.
    Submitted by:	Mitchell Horne <mhorne063@gmail.com>
    Differential Revision:	https://reviews.freebsd.org/D18320

--- sys/riscv/riscv/pmap.c
+++ sys/riscv/riscv/pmap.c
@@ -1645,9 +1645,7 @@ free_pv_chunk(struct pv_chunk *pc)
 	PV_STAT(atomic_add_int(&pc_chunk_frees, 1));
 	/* entire chunk is free, return it */
 	m = PHYS_TO_VM_PAGE(DMAP_TO_PHYS((vm_offset_t)pc));
-#if 0 /* TODO: For minidump */
 	dump_drop_page(m->phys_addr);
-#endif
 	vm_page_unwire(m, PQ_NONE);
 	vm_page_free(m);
 }
@@ -1709,9 +1707,7 @@ retry:
 	}
 	PV_STAT(atomic_add_int(&pc_chunk_count, 1));
 	PV_STAT(atomic_add_int(&pc_chunk_allocs, 1));
-#if 0 /* TODO: This is for minidump */
 	dump_add_page(m->phys_addr);
-#endif
 	pc = (void *)PHYS_TO_DMAP(m->phys_addr);
 	pc->pc_pmap = pmap;
 	pc->pc_map[0] = PC_FREE0 & ~1ul;	/* preallocated bit 0 */
