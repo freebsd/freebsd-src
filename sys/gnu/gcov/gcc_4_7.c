2df4cef12581c736480323014b773c8bec45f907
brooks <brooks@FreeBSD.org>
2019-03-07 22:34:45 +0000

    Correct my previous correction to the license.  It now matches the text in https://spdx.org/licenses/GPL-2.0.html

--- sys/gnu/gcov/gcc_4_7.c
+++ sys/gnu/gcov/gcc_4_7.c
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License.
+// as published by the Free Software Foundation; version 2.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
