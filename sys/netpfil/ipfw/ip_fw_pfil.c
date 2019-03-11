ca80cb589163fffe3e28a6a420195c441560a27d
ae <ae@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-03-11 10:42:09 +0000

    Add IP_FW_NAT64 to codes that ipfw_chk() can return.
    It will be used by upcoming NAT64 changes. We use separate code
    to avoid propogating EACCES error code to user level applications
    when NAT64 consumes a packet.
    
    Obtained from:	Yandex LLC
    MFC after:	1 week
    Sponsored by:	Yandex LLC
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@345004 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netpfil/ipfw/ip_fw_pfil.c
+++ sys/netpfil/ipfw/ip_fw_pfil.c
@@ -313,6 +313,10 @@ again:
 	case IP_FW_REASS:
 		goto again;		/* continue with packet */
 
+	case IP_FW_NAT64:
+		ret = PFIL_CONSUMED;
+		break;
+
 	default:
 		KASSERT(0, ("%s: unknown retval", __func__));
 	}
