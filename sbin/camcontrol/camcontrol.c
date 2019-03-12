6bcafa123f45a1e8450c25ad5797a6cdfaae08ac
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-03-12 04:57:05 +0000

    Add -l to camcontrol readcap.
    The -l flag sends only the READ CAPACITY (16) sevice action. Normally
    we send the READ CAPACITY (10) command, and only send RC16 when the
    capacity is larger than 2TB (since that's the max RC10 can
    report). However, some badly programmed drives report different
    numbers for RC10 and RC16. This can be hard to diagnose, but generally
    there's a "Logical block address out of range" error when RC16 reports
    a larger number than RC10 and the RC10 number is the correct one. By
    comparing the output of readcap with and without the -l argmuent, one
    can determine if there's a mismatch and if the DA_Q_NO_RC16 quirk is
    needed.
    
    Reviewed by: ken@
    Differential Revision: https://reviews.freebsd.org/D19536
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@345051 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/camcontrol/camcontrol.c
+++ sbin/camcontrol/camcontrol.c
@@ -203,7 +203,7 @@ static struct camcontrol_opts option_table[] = {
 	{"load", CAM_CMD_STARTSTOP, CAM_ARG_START_UNIT | CAM_ARG_EJECT, NULL},
 	{"eject", CAM_CMD_STARTSTOP, CAM_ARG_EJECT, NULL},
 	{"reportluns", CAM_CMD_REPORTLUNS, CAM_ARG_NONE, "clr:"},
-	{"readcapacity", CAM_CMD_READCAP, CAM_ARG_NONE, "bhHNqs"},
+	{"readcapacity", CAM_CMD_READCAP, CAM_ARG_NONE, "bhHlNqs"},
 	{"reprobe", CAM_CMD_REPROBE, CAM_ARG_NONE, NULL},
 #endif /* MINIMALISTIC */
 	{"rescan", CAM_CMD_RESCAN, CAM_ARG_NONE, NULL},
@@ -7162,7 +7162,7 @@ scsireadcapacity(struct cam_device *device, int argc, char **argv,
 		 char *combinedopt, int task_attr, int retry_count, int timeout)
 {
 	union ccb *ccb;
-	int blocksizeonly, humanize, numblocks, quiet, sizeonly, baseten;
+	int blocksizeonly, humanize, numblocks, quiet, sizeonly, baseten, longonly;
 	struct scsi_read_capacity_data rcap;
 	struct scsi_read_capacity_data_long rcaplong;
 	uint64_t maxsector;
@@ -7172,6 +7172,7 @@ scsireadcapacity(struct cam_device *device, int argc, char **argv,
 
 	blocksizeonly = 0;
 	humanize = 0;
+	longonly = 0;
 	numblocks = 0;
 	quiet = 0;
 	sizeonly = 0;
@@ -7199,6 +7200,9 @@ scsireadcapacity(struct cam_device *device, int argc, char **argv,
 		case 'H':
 			humanize++;
 			baseten++;
+			break;
+		case 'l':
+			longonly++;
 			break;
 		case 'N':
 			numblocks++;
@@ -7242,6 +7246,9 @@ scsireadcapacity(struct cam_device *device, int argc, char **argv,
 		goto bailout;
 	}
 
+	if (longonly != 0)
+		goto long_only;
+
 	scsi_read_capacity(&ccb->csio,
 			   /*retries*/ retry_count,
 			   /*cbfcnp*/ NULL,
@@ -7284,6 +7291,7 @@ scsireadcapacity(struct cam_device *device, int argc, char **argv,
 	if (maxsector != 0xffffffff)
 		goto do_print;
 
+long_only:
 	scsi_read_capacity_16(&ccb->csio,
 			      /*retries*/ retry_count,
 			      /*cbfcnp*/ NULL,
@@ -9515,7 +9523,7 @@ usage(int printlong)
 "        camcontrol identify   [dev_id][generic args] [-v]\n"
 "        camcontrol reportluns [dev_id][generic args] [-c] [-l] [-r report]\n"
 "        camcontrol readcap    [dev_id][generic args] [-b] [-h] [-H] [-N]\n"
-"                              [-q] [-s]\n"
+"                              [-q] [-s] [-l]\n"
 "        camcontrol start      [dev_id][generic args]\n"
 "        camcontrol stop       [dev_id][generic args]\n"
 "        camcontrol load       [dev_id][generic args]\n"
