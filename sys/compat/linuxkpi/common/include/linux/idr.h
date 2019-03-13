58822319a97beb934c26f7505646dc27c9e2bd8b
hselasky <hselasky@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-03-13 19:53:20 +0000

    Resolve duplicate symbol name conflict after r345095, when building LINT.
    MFC after:		1 week
    Sponsored by:		Mellanox Technologies
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@345110 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/compat/linuxkpi/common/include/linux/idr.h
+++ sys/compat/linuxkpi/common/include/linux/idr.h
@@ -112,13 +112,19 @@ struct ida {
 int	ida_pre_get(struct ida *ida, gfp_t gfp_mask);
 int	ida_get_new_above(struct ida *ida, int starting_id, int *p_id);
 void	ida_remove(struct ida *ida, int id);
-void	ida_free(struct ida *ida, int id);
 void	ida_destroy(struct ida *ida);
 void	ida_init(struct ida *ida);
 
 int	ida_simple_get(struct ida *ida, unsigned int start, unsigned int end,
     gfp_t gfp_mask);
 void	ida_simple_remove(struct ida *ida, unsigned int id);
+
+static inline void
+ida_free(struct ida *ida, int id)
+{
+
+	ida_remove(ida, id);
+}
 
 static inline int
 ida_get_new(struct ida *ida, int *p_id)
