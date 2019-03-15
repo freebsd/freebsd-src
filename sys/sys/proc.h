b1bce1e321a853c9bb30df14bf1c4eb1333758b0
glebius <glebius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-03-15 18:18:05 +0000

    Deanonymize thread and proc state enums, so that a userland app can use them without redefining the value names. New clang no longer allows to redefine a enum value name to the same value.
    Bump __FreeBSD_version, since ports depend on that.
    
    Discussed with:	jhb
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@345196 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -326,7 +326,7 @@ struct thread {
  * or already have been set in the allocator, constructor, etc.
  */
 	struct pcb	*td_pcb;	/* (k) Kernel VA of pcb and kstack. */
-	enum {
+	enum td_states {
 		TDS_INACTIVE = 0x0,
 		TDS_INHIBITED,
 		TDS_CAN_RUN,
@@ -573,7 +573,7 @@ struct proc {
 
 	int		p_flag;		/* (c) P_* flags. */
 	int		p_flag2;	/* (c) P2_* flags. */
-	enum {
+	enum p_states {
 		PRS_NEW = 0,		/* In creation */
 		PRS_NORMAL,		/* threads can be run. */
 		PRS_ZOMBIE
