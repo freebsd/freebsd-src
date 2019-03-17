4e483306b2595a7e1708b6a9cf6d7ab0a26859c1
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-03-17 18:31:48 +0000

    i386: improve detection of the fast page fault assist.
    In particular, check that we are assisting the page fault from kernel mode.
    
    Sponsored by:	The FreeBSD Foundation
    MFC after:	3 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@345246 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/i386/exception.s
+++ sys/i386/i386/exception.s
@@ -131,6 +131,10 @@ IDTVEC(prot)
 	pushl	$T_PROTFLT
 	jmp	irettraps
 IDTVEC(page)
+	testl	$PSL_VM, TF_EFLAGS-TF_ERR(%esp)
+	jnz	1f
+	testb	$SEL_RPL_MASK, TF_CS-TF_ERR(%esp)
+	jnz	1f
 	cmpl	$PMAP_TRM_MIN_ADDRESS, TF_EIP-TF_ERR(%esp)
 	jb	1f
 	movl	%ebx, %cr3
