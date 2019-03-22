a9c742faece08e764e9a60b0a93145aab6eb2006
Konstantin Belousov <kib@FreeBSD.org>
2019-03-22 14:35:15 +0000

    MFC r345190: Provide deterministic (and somewhat useful) value for RDPID result, and for %ecx after RDTSCP.

--- sys/i386/i386/initcpu.c
+++ sys/i386/i386/initcpu.c
@@ -754,6 +754,9 @@ initializecpu(void)
 		elf32_nxstack = 1;
 	}
 #endif
+	if ((amd_feature & AMDID_RDTSCP) != 0 ||
+	    (cpu_stdext_feature2 & CPUID_STDEXT2_RDPID) != 0)
+		wrmsr(MSR_TSC_AUX, PCPU_GET(cpuid));
 }
 
 void
