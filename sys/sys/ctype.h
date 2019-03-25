b37bde59c8c21a1afbac92179c304050d836edcd
hselasky <hselasky@FreeBSD.org>
2019-03-25 13:50:38 +0000

    Change all kernel C-type macros into static inline functions.
    The current kernel C-type macros might obscurely hide the fact that
    the input argument might be used multiple times.
    
    This breaks code like:
    isalpha(*ptr++)
    
    Use static inline functions instead of macros to fix this.
    
    Reviewed by:		kib @
    Differential Revision:	https://reviews.freebsd.org/D19694
    MFC after:		1 week
    Sponsored by:		Mellanox Technologies

--- sys/sys/ctype.h
+++ sys/sys/ctype.h
@@ -41,19 +41,65 @@
 
 #ifdef _KERNEL
 
-#define isspace(c)	((c) == ' ' || ((c) >= '\t' && (c) <= '\r'))
-#define isascii(c)	(((c) & ~0x7f) == 0)
-#define isupper(c)	((c) >= 'A' && (c) <= 'Z')
-#define islower(c)	((c) >= 'a' && (c) <= 'z')
-#define isalpha(c)	(isupper(c) || islower(c))
-#define isdigit(c)	((c) >= '0' && (c) <= '9')
-#define isxdigit(c)	(isdigit(c) \
-			  || ((c) >= 'A' && (c) <= 'F') \
-			  || ((c) >= 'a' && (c) <= 'f'))
-#define isprint(c)	((c) >= ' ' && (c) <= '~')
+static __inline int
+isspace(int c)
+{
+	return (c == ' ' || (c >= '\t' && c <= '\r'));
+}
 
-#define toupper(c)	((c) - 0x20 * (((c) >= 'a') && ((c) <= 'z')))
-#define tolower(c)	((c) + 0x20 * (((c) >= 'A') && ((c) <= 'Z')))
+static __inline int
+isascii(int c)
+{
+	return ((c & ~0x7f) == 0);
+}
+
+static __inline int
+isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
+
+static __inline int
+islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
+static __inline int
+isalpha(int c)
+{
+	return (isupper(c) || islower(c));
+}
+
+static __inline int
+isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+static __inline int
+isxdigit(int c)
+{
+	return (isdigit(c) || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'));
+}
+
+static __inline int
+isprint(int c)
+{
+	return (c >= ' ' && c <= '~');
+}
+
+static __inline int
+toupper(int c)
+{
+	return (c - 0x20 * ((c >= 'a') && (c <= 'z')));
+}
+
+static __inline int
+tolower(int c)
+{
+	return (c + 0x20 * ((c >= 'A') && (c <= 'Z')));
+}
 
 #endif
 #endif /* !_SYS_CTYPE_H_ */
