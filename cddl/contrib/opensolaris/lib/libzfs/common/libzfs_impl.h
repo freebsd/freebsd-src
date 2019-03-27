b0578f749217f485405d4aecaf7587caf9a2e89c
bapt <bapt@FreeBSD.org>
2019-03-27 14:51:03 +0000

    MFC r344569, r344618, r344621
    r344569:
    Implement parallel mounting for ZFS filesystem
    
    It was first implemented on Illumos and then ported to ZoL.
    This patch is a port to FreeBSD of the ZoL version.
    This patch also includes a fix for a race condition that was amended
    
    With such patch Delphix has seen a huge decrease in latency of the mount phase
    (https://github.com/openzfs/openzfs/commit/a3f0e2b569 for details).
    With that current change Gandi has measured improvments that are on par with
    those reported by Delphix.
    
    Zol commits incorporated:
    https://github.com/zfsonlinux/zfs/commit/a10d50f999511d304f910852c7825c70c9c9e303
    https://github.com/zfsonlinux/zfs/commit/e63ac16d25fbe991a356489c86d4077567dfea21
    
    Reviewed by: avg, sef
    Approved by: avg, sef
    Obtained from: ZoL
    Relnotes: yes
    Sponsored by: Gandi.net
    Differential Revision: https://reviews.freebsd.org/D19098
    
    r344618:
    Fix regression introduced in r344569
    
    Reported by: cy
    Tested by: cy
    Submitted by: Fatih Acar <fatih@gandi.net>
    
    r344621:
    Fix a regression introduced in r344569
    
    Import a fix from illumos (thanks Toomas Soomas for pointing at it)
    
    See https://www.illumos.org/issues/10205 for more details
    Illumos commit: https://github.com/illumos/illumos-gate/commit/247b7da039fd88350c50e3d7fef15bdab6bef215
    
    Submitted by: jack@gandi.net
    Reported by: cy
    Reviewed by: tsoome, cy, bapt
    Obtained from: Illumos

--- cddl/contrib/opensolaris/lib/libzfs/common/libzfs_impl.h
+++ cddl/contrib/opensolaris/lib/libzfs/common/libzfs_impl.h
@@ -22,7 +22,7 @@
 /*
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2011 Pawel Jakub Dawidek. All rights reserved.
- * Copyright (c) 2011, 2016 by Delphix. All rights reserved.
+ * Copyright (c) 2011, 2017 by Delphix. All rights reserved.
  * Copyright (c) 2013 Martin Matuska <mm@FreeBSD.org>. All rights reserved.
  */
 
@@ -73,6 +73,13 @@ struct libzfs_handle {
 	int libzfs_storeerr; /* stuff error messages into buffer */
 	void *libzfs_sharehdl; /* libshare handle */
 	boolean_t libzfs_mnttab_enable;
+	/*
+	 * We need a lock to handle the case where parallel mount
+	 * threads are populating the mnttab cache simultaneously. The
+	 * lock only protects the integrity of the avl tree, and does
+	 * not protect the contents of the mnttab entries themselves.
+	 */
+	pthread_mutex_t libzfs_mnttab_cache_lock;
 	avl_tree_t libzfs_mnttab_cache;
 	int libzfs_pool_iter;
 	libzfs_fru_t **libzfs_fru_hash;
