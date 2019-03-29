f5d97e393bb5d010416b14c818141101e748abea
np <np@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-03-29 16:43:24 +0000

    cxgbe/t4_tom: Catch up with r344433, which removed tcb_autorcvbuf_inc.
    The declaration in tcp_var.h is still around so t4_tom continued to
    compile but wouldn't load.  A separate commit will fix tcp_var.h
    
    Reported By: Dustin Marquess (dmarquess at gmail)
    
    Sponsored by:	Chelsio Communications
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@345701 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/cxgbe/tom/t4_cpl_io.c
+++ sys/dev/cxgbe/tom/t4_cpl_io.c
@@ -1631,7 +1631,7 @@ do_rx_data(struct sge_iq *iq, const struct rss_header *rss, struct mbuf *m)
 	    sb->sb_hiwat < V_tcp_autorcvbuf_max &&
 	    len > (sbspace(sb) / 8 * 7)) {
 		unsigned int hiwat = sb->sb_hiwat;
-		unsigned int newsize = min(hiwat + V_tcp_autorcvbuf_inc,
+		unsigned int newsize = min(hiwat + sc->tt.autorcvbuf_inc,
 		    V_tcp_autorcvbuf_max);
 
 		if (!sbreserve_locked(sb, newsize, so, NULL))
