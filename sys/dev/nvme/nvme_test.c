6cc2e1d13c1b9fcda581fd97e10340e14bd9e67d
mav <mav@FreeBSD.org>
2019-04-15 16:27:53 +0000

    MFC r344640 (by imp): Remove #ifdef code to support FreeBSD versions that haven't been supported in years. A number of changes have been made to the driver that likely wouldn't work on those older versions that aren't properly ifdef'd and it's project policy to GC such code once it is stale.

--- sys/dev/nvme/nvme_test.c
+++ sys/dev/nvme/nvme_test.c
@@ -92,9 +92,7 @@ nvme_ns_bio_test(void *arg)
 	struct timeval			t;
 	uint64_t			io_completed = 0, offset;
 	uint32_t			idx;
-#if __FreeBSD_version >= 900017
 	int				ref;
-#endif
 
 	buf = malloc(io_test->size, M_NVME, M_WAITOK);
 	idx = atomic_fetchadd_int(&io_test->td_idx, 1);
@@ -116,11 +114,7 @@ nvme_ns_bio_test(void *arg)
 		bio->bio_bcount = io_test->size;
 
 		if (io_test->flags & NVME_TEST_FLAG_REFTHREAD) {
-#if __FreeBSD_version >= 900017
 			csw = dev_refthread(dev, &ref);
-#else
-			csw = dev_refthread(dev);
-#endif
 		} else
 			csw = dev->si_devsw;
 
@@ -131,11 +125,7 @@ nvme_ns_bio_test(void *arg)
 		mtx_unlock(mtx);
 
 		if (io_test->flags & NVME_TEST_FLAG_REFTHREAD) {
-#if __FreeBSD_version >= 900017
 			dev_relthread(dev, ref);
-#else
-			dev_relthread(dev);
-#endif
 		}
 
 		if ((bio->bio_flags & BIO_ERROR) || (bio->bio_resid > 0))
@@ -164,11 +154,7 @@ nvme_ns_bio_test(void *arg)
 	atomic_subtract_int(&io_test->td_active, 1);
 	mb();
 
-#if __FreeBSD_version >= 800000
 	kthread_exit();
-#else
-	kthread_exit(0);
-#endif
 }
 
 static void
@@ -244,11 +230,7 @@ nvme_ns_io_test(void *arg)
 	atomic_subtract_int(&io_test->td_active, 1);
 	mb();
 
-#if __FreeBSD_version >= 800004
 	kthread_exit();
-#else
-	kthread_exit(0);
-#endif
 }
 
 void
@@ -285,13 +267,8 @@ nvme_ns_test(struct nvme_namespace *ns, u_long cmd, caddr_t arg)
 	getmicrouptime(&io_test_internal->start);
 
 	for (i = 0; i < io_test->num_threads; i++)
-#if __FreeBSD_version >= 800004
 		kthread_add(fn, io_test_internal,
 		    NULL, NULL, 0, 0, "nvme_io_test[%d]", i);
-#else
-		kthread_create(fn, io_test_internal,
-		    NULL, 0, 0, "nvme_io_test[%d]", i);
-#endif
 
 	tsleep(io_test_internal, 0, "nvme_test", io_test->time * 2 * hz);
 
