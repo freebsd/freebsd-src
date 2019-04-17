3f18402bc61b71a85aac995ef1a77454ea453939
dim <dim@FreeBSD.org>
2019-04-17 20:08:01 +0000

    After r346168, also merge build infrastructure for LLVM libomp.
    MFC r345235:
    
    Add lib/libomp, with a Makefile, and generated configuration headers.
    Not connected to the main build yet, as there is still the issue of the
    GNU omp.h header conflicting with the LLVM one.  (That is, if MK_GCC is
    enabled.)
    
    PR:		236062
    
    MFC r345236:
    
    Connect lib/libomp to the build.
    
    * Set MK_OPENMP to yes by default only on amd64, for now.
    * Bump __FreeBSD_version to signal this addition.
    * Ensure gcc's conflicting omp.h is not installed if MK_OPENMP is yes.
    * Update OptionalObsoleteFiles.inc to cope with the conflicting omp.h.
    * Regenerate src.conf(5) with new WITH/WITHOUT fragments.
    
    Relnotes:	yes
    PR:		236062
    
    MFC r345242:
    
    Explicitly link libomp.so against -lpthread, as it depends on pthread
    functionality.  This should make example OpenMP programs work out of the
    box.
    
    Reported by:	jbeich
    PR:		236062, 236581
    
    MFC r345278:
    
    Also explicitly link libomp.so against -lm, as it transitively depends
    on scalbn and a few other math functions, via libcompiler-rt.  This
    should allow OpenMP programs to link with BFD linkers too.
    
    Reported by:	jbeich
    PR:		236062, 236581
    
    MFC r345282:
    
    Remove --as-needed from the linker flags for libomp.so, as these
    actually prevent the transitive dependency on libm.
    
    Reported by:	jbeich
    PR:		236062, 236581
    
    MFC r345291:
    
    Turn on MK_OPENMP for i386 by default, now that it can build.
    
    Noticed by:	jbeich
    PR:		236062, 236582

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -396,6 +396,12 @@ __DEFAULT_YES_OPTIONS+=NVME
 __DEFAULT_NO_OPTIONS+=NVME
 .endif
 
+.if ${COMPILER_FEATURES:Mc++11} && (${__T} == "amd64" || ${__T} == "i386")
+__DEFAULT_YES_OPTIONS+=OPENMP
+.else
+__DEFAULT_NO_OPTIONS+=OPENMP
+.endif
+
 .include <bsd.mkopt.mk>
 
 #
