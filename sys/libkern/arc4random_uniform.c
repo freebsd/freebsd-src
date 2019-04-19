7deb4b1964834c479406cab511de339a6cc0088e
Conrad Meyer <cem@FreeBSD.org>
2019-04-19 20:05:47 +0000

    libkern: Bring in arc4random_uniform(9) from libc
    It is a useful arc4random wrapper in the kernel for much the same reasons as
    in userspace.  Move the source to libkern (because kernel build is
    restricted to sys/, but userspace can include any file it likes) and build
    kernel and libc versions from the same source file.
    
    Copy the documentation from arc4random_uniform(3) to the section 9 page.
    
    While here, add missing arc4random_buf(9) symlink.
    
    Sponsored by:	Dell EMC Isilon

--- sys/libkern/arc4random_uniform.c
+++ sys/libkern/arc4random_uniform.c
@@ -19,7 +19,11 @@
  */
 
 #include <sys/types.h>
+#ifdef _KERNEL
+#include <sys/libkern.h>
+#else
 #include <stdlib.h>
+#endif
 
 /*
  * Calculate a uniformly distributed random number less than upper_bound
