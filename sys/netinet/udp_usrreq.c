e6c9a8836e370ee7458ba516eea9d1e774be1383
bz <bz@FreeBSD.org>
2019-04-22 14:20:49 +0000

    Remove some excessive brackets.
    No functional change.
    
    MFC after:	10 days

--- sys/netinet/udp_usrreq.c
+++ sys/netinet/udp_usrreq.c
@@ -1280,12 +1280,12 @@ udp_output(struct inpcb *inp, struct mbuf *m, struct sockaddr *addr,
 	    (inp->inp_laddr.s_addr == INADDR_ANY && inp->inp_lport == 0)) {
 		INP_HASH_WLOCK(pcbinfo);
 		unlock_udbinfo = UH_WLOCKED;
-	} else if ((sin != NULL && (
-	    (sin->sin_addr.s_addr == INADDR_ANY) ||
-	    (sin->sin_addr.s_addr == INADDR_BROADCAST) ||
-	    (inp->inp_laddr.s_addr == INADDR_ANY) ||
-	    (inp->inp_lport == 0))) ||
-	    (src.sin_family == AF_INET)) {
+	} else if ((sin != NULL &&
+		(sin->sin_addr.s_addr == INADDR_ANY ||
+		sin->sin_addr.s_addr == INADDR_BROADCAST ||
+		inp->inp_laddr.s_addr == INADDR_ANY ||
+		inp->inp_lport == 0)) ||
+	    src.sin_family == AF_INET) {
 		INP_HASH_RLOCK_ET(pcbinfo, et);
 		unlock_udbinfo = UH_RLOCKED;
 	} else
