d38d5862c8a09ab88f2d09571c1f0de4f0e5a912
ngie <ngie@FreeBSD.org>
2019-04-22 11:09:24 +0000

    Fix `get_int_via_sysctlbyname(..)` on Jenkins
    Initialize `oldlen` to the size of the value, instead of leaving the value
    unitialized. Leaving it unitialized seems to work by accident on amd64 when
    running 64-bit programs, but not on i386.
    
    This matches patterns in use in other programs.
    
    PR:		237458
    Approved by:	emaste (mentor; implicit)
    MFC after:	1 week
    Tested on:	^/head (amd64), ^/stable/11 (i386)

--- lib/libc/tests/sys/sendfile_test.c
+++ lib/libc/tests/sys/sendfile_test.c
@@ -62,6 +62,8 @@ get_int_via_sysctlbyname(const char *oidname)
 	size_t oldlen;
 	int int_value;
 
+	oldlen = sizeof(int_value);
+
 	ATF_REQUIRE_EQ_MSG(sysctlbyname(oidname, &int_value, &oldlen, NULL, 0),
 	    0, "sysctlbyname(%s, ...) failed: %s", oidname, strerror(errno));
 	ATF_REQUIRE_EQ_MSG(sizeof(int_value), oldlen, "sanity check failed");
