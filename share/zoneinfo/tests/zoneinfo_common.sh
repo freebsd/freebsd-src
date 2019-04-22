a34c951eb1d42991460cd0de80e7fe0fce8b1746
ngie <ngie@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-04-22 17:52:46 +0000

    Update the spelling of my name
    Previous spellings of my name (NGie, Ngie) weren't my legal spelling. Use Enji
    instead for clarity.
    
    While here, remove "All Rights Reserved" from copyrights I "own".
    
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@346571 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/zoneinfo/tests/zoneinfo_common.sh
+++ share/zoneinfo/tests/zoneinfo_common.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #
-# Copyright (c) 2017 Ngie Cooper <ngie@FreeBSD.org>
-# All rights reserved.
+# Copyright (c) 2017 Enji Cooper <ngie@FreeBSD.org>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
--- usr.bin/getconf/tests/arch_type.c
+++ usr.bin/getconf/tests/arch_type.c
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2017 Ngie Cooper <ngie@FreeBSD.org>
- * All rights reserved.
+ * Copyright (c) 2017 Enji Cooper <ngie@FreeBSD.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
