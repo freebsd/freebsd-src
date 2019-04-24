7f4274d99c4820538ffb13d9d012259845dca7cd
tychon <tychon@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-04-24 20:30:45 +0000

    LinuxKPI should use bus_dma(9) to be compatible with an IOMMU
    Reviewed by:	hselasky, kib
    Tested by:	greg@unrelenting.technology
    Sponsored by:	Dell EMC Isilon
    Differential Revision:	https://reviews.freebsd.org/D19845
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@346645 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/compat/linuxkpi/common/include/linux/dmapool.h
+++ sys/compat/linuxkpi/common/include/linux/dmapool.h
@@ -37,44 +37,44 @@
 #include <linux/device.h>
 #include <linux/slab.h>
 
+struct dma_pool *linux_dma_pool_create(char *name, struct device *dev,
+    size_t size, size_t align, size_t boundary);
+void linux_dma_pool_destroy(struct dma_pool *pool);
+void *linux_dma_pool_alloc(struct dma_pool *pool, gfp_t mem_flags,
+    dma_addr_t *handle);
+void linux_dma_pool_free(struct dma_pool *pool, void *vaddr,
+    dma_addr_t dma_addr);
+
 struct dma_pool {
+	struct pci_dev	*pool_pdev;
 	uma_zone_t	pool_zone;
+	struct mtx	pool_dma_lock;
+	bus_dma_tag_t	pool_dmat;
+	size_t		pool_entry_size;
+	struct mtx	pool_ptree_lock;
+	struct pctrie	pool_ptree;
 };
 
 static inline struct dma_pool *
 dma_pool_create(char *name, struct device *dev, size_t size,
     size_t align, size_t boundary)
 {
-	struct dma_pool *pool;
 
-	pool = kmalloc(sizeof(*pool), GFP_KERNEL);
-	align--;
-	/*
-	 * XXX Eventually this could use a separate allocf to honor boundary
-	 * and physical address requirements of the device.
-	 */
-	pool->pool_zone = uma_zcreate(name, size, NULL, NULL, NULL, NULL,
-	    align, UMA_ZONE_OFFPAGE|UMA_ZONE_HASH);
-
-	return (pool);
+	return (linux_dma_pool_create(name, dev, size, align, boundary));
 }
 
 static inline void
 dma_pool_destroy(struct dma_pool *pool)
 {
-	uma_zdestroy(pool->pool_zone);
-	kfree(pool);
+
+	linux_dma_pool_destroy(pool);
 }
 
 static inline void *
 dma_pool_alloc(struct dma_pool *pool, gfp_t mem_flags, dma_addr_t *handle)
 {
-	void *vaddr;
 
-	vaddr = uma_zalloc(pool->pool_zone, mem_flags);
-	if (vaddr)
-		*handle = vtophys(vaddr);
-	return (vaddr);
+	return (linux_dma_pool_alloc(pool, mem_flags, handle));
 }
 
 static inline void *
@@ -85,9 +85,10 @@ dma_pool_zalloc(struct dma_pool *pool, gfp_t mem_flags, dma_addr_t *handle)
 }
 
 static inline void
-dma_pool_free(struct dma_pool *pool, void *vaddr, dma_addr_t addr)
+dma_pool_free(struct dma_pool *pool, void *vaddr, dma_addr_t dma_addr)
 {
-	uma_zfree(pool->pool_zone, vaddr);
+
+	linux_dma_pool_free(pool, vaddr, dma_addr);
 }
 
 
