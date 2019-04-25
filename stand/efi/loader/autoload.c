c05cec5fde9d6345f300cc7adbb0eaba5748f362
manu <manu@FreeBSD.org>
2019-04-25 20:08:43 +0000

    loader: fdt: Add fdt_is_setup function
    When efi_autoload is called it will call fdt_setup_fdtp which setup the
    dtb and overlays. If a user already loaded at dtb or overlays or just
    printed the efi provided dtb, this will re-setup everything and also
    re-applying the overlays.
    Test that everything is setup before doing it again.
    
    Reviewed by:	kevans
    Differential Revision:	https://reviews.freebsd.org/D20059

--- stand/efi/loader/autoload.c
+++ stand/efi/loader/autoload.c
@@ -50,7 +50,8 @@ efi_autoload(void)
 	 * imply that we're on a platform where FDT is a requirement.  If we
 	 * fix this, then the error handling here should be fixed accordingly.
 	 */
-	fdt_setup_fdtp();
+	if (fdt_is_setup() == 0)
+		fdt_setup_fdtp();
 #endif
 	return (0);
 }
