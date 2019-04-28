b9ff7e6bac2020a47fc0cc79d047b994e62403ea
Dmitry Chagin <dchagin@FreeBSD.org>
2019-04-28 13:40:17 +0000

    MFC r331356 (by emaste@):
    Share Linux errno table with libsysdecode.

--- sys/compat/linux/linux_emul.h
+++ sys/compat/linux/linux_emul.h
@@ -77,6 +77,6 @@ struct linux_pemuldata {
 
 struct linux_pemuldata	*pem_find(struct proc *);
 
-extern const int bsd_to_linux_errno_generic[];
+extern const int linux_errtbl[];
 
 #endif	/* !_LINUX_EMUL_H_ */
