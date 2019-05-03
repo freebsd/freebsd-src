7b42224edefa781e3105b362757d17e3940d6b25
mmacy <mmacy@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-05-03 20:05:31 +0000

    Allow iflib drivers to pass a pointer to their own ifmedia structure.
    Tested by: emaste@
    
    Differential Revision:	https://reviews.freebsd.org/D19946
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@347057 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net/iflib.h
+++ sys/net/iflib.h
@@ -228,7 +228,9 @@ typedef struct if_softc_ctx {
 	pci_vendor_info_t isc_vendor_info;	/* set by iflib prior to attach_pre */
 	int isc_disable_msix;
 	if_txrx_t isc_txrx;
+	struct ifmedia *isc_media;
 } *if_softc_ctx_t;
+
 
 /*
  * Initialization values for device
@@ -361,6 +363,10 @@ typedef enum {
  * Interface needs admin task to ignore interface up/down status
  */
 #define IFLIB_ADMIN_ALWAYS_RUN	0x10000
+/*
+ * Driver will pass the media
+ */
+#define IFLIB_DRIVER_MEDIA	0x20000
 
 
 /*
