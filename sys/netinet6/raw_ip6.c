2b53926a76323efd3112e4f44bde7ffa218fe321
Michael Tuexen <tuexen@FreeBSD.org>
2019-05-04 13:55:51 +0000

    MFC r346400: Improve input validation for the socket option IPV6_CHECKSUM.
    When using the IPPROTO_IPV6 level socket option IPV6_CHECKSUM on a raw
    IPv6 socket, ensure that the value is either -1 or a non-negative even
    number.
    
    MFC r346401:
    Avoid a buffer overwrite in rip6_output() when computing the checksum
    as requested by the user via the IPPROTO_IPV6 level socket option
    IPV6_CHECKSUM. The check if there are enough bytes in the packet to
    store the checksum at the requested offset was wrong by 1.
    
    MFC r346402:
    When a checksum has to be computed for a received IPv6 packet because it
    is requested by the application using the IPPROTO_IPV6 level socket option
    IPV6_CHECKSUM on a raw socket, ensure that the packet contains enough
    bytes to contain the checksum at the specified offset.
    
    MFC r346406:
    When an IPv6 packet is received for a raw socket which has the
    IPPROTO_IPV6 level socket option IPV6_CHECKSUM enabled and the
    checksum check fails, drop the message. Without this fix, an
    ICMP6 message was sent indicating a parameter problem.
    
    Thanks to bz@ for suggesting a way to simplify this fix.
    
    Reviewed by:		bz@
    Sponsored by:		Netflix, Inc.
    Differential Revision:	https://reviews.freebsd.org/D19966
    Differential Revision:  https://reviews.freebsd.org/D19967
    Differential Revision:  https://reviews.freebsd.org/D19968
    Differential Revision:  https://reviews.freebsd.org/D19969

--- sys/netinet6/raw_ip6.c
+++ sys/netinet6/raw_ip6.c
@@ -239,9 +239,16 @@ rip6_input(struct mbuf **mp, int *offp, int proto)
 		}
 		if (in6p->in6p_cksum != -1) {
 			RIP6STAT_INC(rip6s_isum);
-			if (in6_cksum(m, proto, *offp,
+			if (m->m_pkthdr.len - (*offp + in6p->in6p_cksum) < 2 ||
+			    in6_cksum(m, proto, *offp,
 			    m->m_pkthdr.len - *offp)) {
 				RIP6STAT_INC(rip6s_badsum);
+				/*
+				 * Drop the received message, don't send an
+				 * ICMP6 message. Set proto to IPPROTO_NONE
+				 * to achieve that.
+				 */
+				proto = IPPROTO_NONE;
 				goto skip_2;
 			}
 		}
@@ -495,7 +502,7 @@ rip6_output(struct mbuf *m, struct socket *so, ...)
 			off = offsetof(struct icmp6_hdr, icmp6_cksum);
 		else
 			off = in6p->in6p_cksum;
-		if (plen < off + 1) {
+		if (plen < off + 2) {
 			error = EINVAL;
 			goto bad;
 		}
