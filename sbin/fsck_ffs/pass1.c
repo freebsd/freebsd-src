8f4b036ab32eb569774bc35f84f23ac175c11854
mckusick <mckusick@FreeBSD.org>
2019-05-06 19:13:04 +0000

    MFC of 345758
    Properly flush outstanding I/Os when forcibly deleteing a memory disk device.
    
    Sponsored by: Netflix

--- sbin/fsck_ffs/pass1.c
+++ sbin/fsck_ffs/pass1.c
@@ -378,7 +378,6 @@ checkinode(ino_t inumber, struct inodesc *idesc, int rebuildcg)
 		idesc->id_type = SNAP;
 	else
 		idesc->id_type = ADDR;
-	idesc->id_lballoc = -1;
 	(void)ckinode(dp, idesc);
 	if (sblock.fs_magic == FS_UFS2_MAGIC && dp->dp2.di_extsize > 0) {
 		idesc->id_type = ADDR;
@@ -565,7 +564,7 @@ pass1check(struct inodesc *idesc)
 		 */
 		idesc->id_entryno++;
 	}
-	if (idesc->id_lballoc == -1 || idesc->id_lballoc < idesc->id_lbn)
+	if (idesc->id_level == 0 && idesc->id_lballoc < idesc->id_lbn)
 		idesc->id_lballoc = idesc->id_lbn;
 	return (res);
 }
