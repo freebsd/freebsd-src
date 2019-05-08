052073c38209a57b06576c14eb19c008807a3744
Emmanuel Vadot <manu@FreeBSD.org>
2019-05-08 15:42:39 +0000

    Add support for USB 3.0 XHCI via ACPI
    Ampere eMAG systems have XHCI just described in ACPI, not on PCI.
    
    Submitted by:	Greg V <greg@unrelenting.technology>
    Reviewed by:	andrew
    MFC after:	1 month
    Differential Revision:	https://reviews.freebsd.org/D19986

---
+++ sys/dev/usb/controller/generic_xhci.h
@@ -0,0 +1,44 @@
+/* $FreeBSD$ */
+
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2015 Semihalf.
+ * Copyright (c) 2015 Stormshield.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#ifndef _GENERIC_XHCI_H_
+#define _GENERIC_XHCI_H_
+
+#define	XHCI_HC_DEVSTR	"Generic USB 3.0 controller"
+#define	XHCI_HC_VENDOR	"Generic"
+
+extern driver_t generic_xhci_driver;
+
+device_attach_t generic_xhci_attach;
+device_detach_t generic_xhci_detach;
+
+#endif /* !_GENERIC_XHCI_H_ */
