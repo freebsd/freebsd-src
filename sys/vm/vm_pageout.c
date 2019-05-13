54a3a1142189f60edfbbdfb0e1f4085e2d30c115
Mark Johnston <markj@FreeBSD.org>
2019-05-13 16:38:48 +0000

    Provide separate accounting for user-wired pages.
    Historically we have not distinguished between kernel wirings and user
    wirings for accounting purposes.  User wirings (via mlock(2)) were
    subject to a global limit on the number of wired pages, so if large
    swaths of physical memory were wired by the kernel, as happens with
    the ZFS ARC among other things, the limit could be exceeded, causing
    user wirings to fail.
    
    The change adds a new counter, v_user_wire_count, which counts the
    number of virtual pages wired by user processes via mlock(2) and
    mlockall(2).  Only user-wired pages are subject to the system-wide
    limit which helps provide some safety against deadlocks.  In
    particular, while sources of kernel wirings typically support some
    backpressure mechanism, there is no way to reclaim user-wired pages
    shorting of killing the wiring process.  The limit is exported as
    vm.max_user_wired, renamed from vm.max_wired, and changed from u_int
    to u_long.
    
    The choice to count virtual user-wired pages rather than physical
    pages was done for simplicity.  There are mechanisms that can cause
    user-wired mappings to be destroyed while maintaining a wiring of
    the backing physical page; these make it difficult to accurately
    track user wirings at the physical page layer.
    
    The change also closes some holes which allowed user wirings to succeed
    even when they would cause the system limit to be exceeded.  For
    instance, mmap() may now fail with ENOMEM in a process that has called
    mlockall(MCL_FUTURE) if the new mapping would cause the user wiring
    limit to be exceeded.
    
    Note that bhyve -S is subject to the user wiring limit, which defaults
    to 1/3 of physical RAM.  Users that wish to exceed the limit must tune
    vm.max_user_wired.
    
    Reviewed by:	kib, ngie (mlock() test changes)
    Tested by:	pho (earlier version)
    MFC after:	45 days
    Sponsored by:	Netflix
    Differential Revision:	https://reviews.freebsd.org/D19908

--- sys/vm/vm_pageout.c
+++ sys/vm/vm_pageout.c
@@ -194,9 +194,10 @@ SYSCTL_UINT(_vm, OID_AUTO, background_launder_max, CTLFLAG_RWTUN,
 
 int vm_pageout_page_count = 32;
 
-int vm_page_max_wired;		/* XXX max # of wired pages system-wide */
-SYSCTL_INT(_vm, OID_AUTO, max_wired,
-	CTLFLAG_RW, &vm_page_max_wired, 0, "System-wide limit to wired page count");
+u_long vm_page_max_user_wired;
+SYSCTL_ULONG(_vm, OID_AUTO, max_user_wired, CTLFLAG_RW,
+    &vm_page_max_user_wired, 0,
+    "system-wide limit to user-wired page count");
 
 static u_int isqrt(u_int num);
 static int vm_pageout_launder(struct vm_domain *vmd, int launder,
@@ -2041,8 +2042,8 @@ vm_pageout_init(void)
 	if (vm_pageout_update_period == 0)
 		vm_pageout_update_period = 600;
 
-	if (vm_page_max_wired == 0)
-		vm_page_max_wired = freecount / 3;
+	if (vm_page_max_user_wired == 0)
+		vm_page_max_user_wired = freecount / 3;
 }
 
 /*
