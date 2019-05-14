92e7c0f46ef2223c56b362a00cad99f1e390d9ce
kib <kib@FreeBSD.org>
2019-05-14 17:02:20 +0000

    Mitigations for Microarchitectural Data Sampling.
    Microarchitectural buffers on some Intel processors utilizing
    speculative execution may allow a local process to obtain a memory
    disclosure.  An attacker may be able to read secret data from the
    kernel or from a process when executing untrusted code (for example,
    in a web browser).
    
    Reference: https://www.intel.com/content/www/us/en/security-center/advisory/intel-sa-00233.html
    Security:	CVE-2018-12126, CVE-2018-12127, CVE-2018-12130, CVE-2019-11091
    Security:	FreeBSD-SA-19:07.mds
    Reviewed by:	jhb
    Tested by:	emaste, lwhsu
    Approved by:	so (gtetlow)

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -1732,6 +1732,7 @@ hammer_time(u_int64_t modulep, u_int64_t physfree)
 	TUNABLE_INT_FETCH("hw.spec_store_bypass_disable", &hw_ssb_disable);
 	TUNABLE_INT_FETCH("machdep.syscall_ret_l1d_flush",
 	    &syscall_ret_l1d_flush_mode);
+	TUNABLE_INT_FETCH("hw.mds_disable", &hw_mds_disable);
 
 	finishidentcpu();	/* Final stage of CPU initialization */
 	initializecpu();	/* Initialize CPU registers */
