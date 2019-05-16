804d710aff1edb9ed44f51665da1fc997cd8326a
Hans Petter Selasky <hselasky@FreeBSD.org>
2019-05-16 16:00:00 +0000

    MFC r347271: Implement PCAM, MCAM access register commands in mlx5core.
    Introduced registers will expose capabilities of new registers and
    features related to port/management.
    Driver will query MCAM and PCAM in order to avoid failing on old
    firmwares with lack of support.
    
    Linux commit:
    c835ad64683bd3e2d1b31ed2cb1ff4366932edb1
    
    Submitted by:	slavash@
    Sponsored by:	Mellanox Technologies

--- sys/dev/mlx5/mlx5_core/mlx5_core.h
+++ sys/dev/mlx5/mlx5_core/mlx5_core.h
@@ -76,6 +76,10 @@ int mlx5_query_hca_caps(struct mlx5_core_dev *dev);
 int mlx5_query_board_id(struct mlx5_core_dev *dev);
 int mlx5_query_qcam_reg(struct mlx5_core_dev *mdev, u32 *qcam,
 			u8 feature_group, u8 access_reg_group);
+int mlx5_query_pcam_reg(struct mlx5_core_dev *dev, u32 *pcam,
+			u8 feature_group, u8 access_reg_group);
+int mlx5_query_mcam_reg(struct mlx5_core_dev *dev, u32 *mcap,
+			u8 feature_group, u8 access_reg_group);
 int mlx5_cmd_init_hca(struct mlx5_core_dev *dev);
 int mlx5_cmd_teardown_hca(struct mlx5_core_dev *dev);
 int mlx5_cmd_force_teardown_hca(struct mlx5_core_dev *dev);
