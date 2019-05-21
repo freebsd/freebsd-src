d51b14c0f718a3d468d3c7d84ce64046489b05d5
Mark Johnston <markj@FreeBSD.org>
2019-05-21 01:27:08 +0000

    MFC r347570: Specify -z notext when building with -z ifunc-noplt.

--- sys/conf/kern.pre.mk
+++ sys/conf/kern.pre.mk
@@ -131,7 +131,7 @@ LDFLAGS+=	-z max-page-size=2097152
 .if ${LINKER_TYPE} != "lld"
 LDFLAGS+=	-z common-page-size=4096
 .else
-LDFLAGS+=	-z ifunc-noplt
+LDFLAGS+=	-z notext -z ifunc-noplt
 .endif
 .endif
 
