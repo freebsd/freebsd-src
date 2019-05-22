2d173606d8a798e44377a0824d1e96a4c0d94063
johalun <johalun@FreeBSD.org>
2019-05-22 03:58:41 +0000

    MFC r347620: LinuxKPI: Update user_access_begin for Linux v5.0.
    Check the new LINUXKPI_VERSION macro for backwards compatibility.
    This patch is part of D19565
    
    Reviewed by:	hps
    Approved by:	imp (mentor), hps

--- sys/compat/linuxkpi/common/include/asm/uaccess.h
+++ sys/compat/linuxkpi/common/include/asm/uaccess.h
@@ -52,7 +52,11 @@ copy_from_user(void *to, const void *from, unsigned long n)
 #define	__copy_from_user(...)	copy_from_user(__VA_ARGS__)
 #define	__copy_in_user(...)	copy_from_user(__VA_ARGS__)
 
+#if defined(LINUXKPI_VERSION) && LINUXKPI_VERSION >= 50000
+#define	user_access_begin(ptr, len) access_ok(ptr, len)
+#else
 #define	user_access_begin() do { } while (0)
+#endif
 #define	user_access_end() do { } while (0)
 
 #define	unsafe_get_user(x, ptr, err) do { \
