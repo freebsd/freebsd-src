3a67f8b6ac8d34d095cdd9eb941701af4b34b0d9
Johannes Lundberg <johalun@FreeBSD.org>
2019-05-24 17:04:27 +0000

    MFC r347843: LinuxKPI: Add group_leader member to struct task_struct.
    Assign self as group leader at creation to act as the only member of a
    new process group.
    Bump FreeBSD version.
    This patch is part of D19565
    
    Reviewed by:	hps
    Approved by:	imp (mentor), hps

--- sys/compat/linuxkpi/common/include/linux/sched.h
+++ sys/compat/linuxkpi/common/include/linux/sched.h
@@ -80,6 +80,7 @@ struct task_struct {
 	int rcu_recurse;
 	int bsd_interrupt_value;
 	struct work_struct *work;	/* current work struct, if set */
+	struct task_struct *group_leader;
 };
 
 #define	current	({ \
