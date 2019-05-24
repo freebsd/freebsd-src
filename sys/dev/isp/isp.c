freebsd/freebsd-src

commit 72e35e7b76a17616b1a5bc99e230d2303f14864c

Fix CRN resets in the isp(4) driver in certain situations.
The Command Reference Number (CRN) is part of the FC-Tape features
that we enable when talking to tape drives.  It starts at 1, and
goes to 255 and wraps around to 1.  There are a number of reset
type conditions that result in the CRN getting reset to 1.  These
are detailed in section 4.10 (table 8) of the FCP-4r02b specification.

One of the conditions is when a PRLI (Process Login) is sent by
the initiator, and the Establish Image Pair bit is set in Word 0
of the PRLI.

Previously, the isp(4) driver core sent a notification via
isp_async() that the target had changed or stayed in place, but
there was no indication of whether a PRLI was sent and whether the
Establish Image Pair bit was set.

The result of this was that in some situations, notably
switching back and forth between a direct connection and a switch
connection to a tape drive, the isp(4) driver would fail to reset
the CRN in situations that require it according to the spec.  When
the CRN isn't reset in a situation that requires it, the tape drive
then rejects every subsequent command that is sent to the drive.
It is assuming that the commands are being sent out of order.

So, modify the isp(4) driver to include Word 0 of the PRLI command
when it sends isp_async() notifications of target changes.  Look at
the Establish Image Pair bit, and reset the CRN if that bit is set.

With this change, I am able to switch a tape drive back and forth
between a direct connection and a switch connection, and the isp(4)
driver resets the CRN when it should.

sys/dev/isp_stds.h:
	Add bit definitions for PRLI Word 0.

sys/dev/ispmbox.h:
	Add PRLI Word 0 to the port database type, isp_pdb_t.

sys/dev/ispvar.h
	Add PRLI Word 0 to fcportdb_t.

sys/dev/isp.c:
	Populate the new prli_word0 parameter in the port database.

	In isp_pdb_add_update(), add a check to see if the
	Establish Image Pair bit is set in PRLI Word 0.  If it is,

sys/dev/isp/isp.c
@@ -2791,6 +2791,7 @@ isp_getpdb(ispsoftc_t *isp, int chan, uint16_t id, isp_pdb_t *pdb)
 	if (IS_24XX(isp)) {
 		isp_get_pdb_24xx(isp, isp->isp_iocb, &un.bill);
 		pdb->handle = un.bill.pdb_handle;
+		pdb->prli_word0 = un.bill.pdb_prli_svc0;
 		pdb->prli_word3 = un.bill.pdb_prli_svc3;
 		pdb->portid = BITS2WORD_24XX(un.bill.pdb_portid_bits);
 		ISP_MEMCPY(pdb->portname, un.bill.pdb_portname, 8);
@@ -2807,6 +2808,7 @@ isp_getpdb(ispsoftc_t *isp, int chan, uint16_t id, isp_pdb_t *pdb)
 	} else {
 		isp_get_pdb_21xx(isp, isp->isp_iocb, &un.fred);
 		pdb->handle = un.fred.pdb_loopid;
+		pdb->prli_word0 = un.fred.pdb_prli_svc0;
 		pdb->prli_word3 = un.fred.pdb_prli_svc3;
 		pdb->portid = BITS2WORD(un.fred.pdb_portid_bits);
 		ISP_MEMCPY(pdb->portname, un.fred.pdb_portname, 8);
@@ -3196,6 +3198,7 @@ isp_pdb_sync(ispsoftc_t *isp, int chan)
 			lp->state = FC_PORTDB_STATE_VALID;
 			isp_async(isp, ISPASYNC_DEV_CHANGED, chan, lp);
 			lp->portid = lp->new_portid;
+			lp->prli_word0 = lp->new_prli_word0;
 			lp->prli_word3 = lp->new_prli_word3;
 			break;
 		case FC_PORTDB_STATE_VALID:
@@ -3247,7 +3250,8 @@ isp_pdb_add_update(ispsoftc_t *isp, int chan, isp_pdb_t *pdb)
 		/* Old device, nothing new. */
 		if (lp->portid == pdb->portid &&
 		    lp->handle == pdb->handle &&
-		    lp->prli_word3 == pdb->prli_word3) {
+		    lp->prli_word3 == pdb->prli_word3 &&
+		    ((pdb->prli_word0 & PRLI_WD0_EST_IMAGE_PAIR) == 0)) {
 			if (lp->state != FC_PORTDB_STATE_NEW)
 				lp->state = FC_PORTDB_STATE_VALID;
 			isp_prt(isp, ISP_LOG_SANCFG,
@@ -3260,6 +3264,7 @@ isp_pdb_add_update(ispsoftc_t *isp, int chan, isp_pdb_t *pdb)
 		lp->state = FC_PORTDB_STATE_CHANGED;
 		lp->handle = pdb->handle;
 		lp->new_portid = pdb->portid;
+		lp->new_prli_word0 = pdb->prli_word0;
 		lp->new_prli_word3 = pdb->prli_word3;
 		isp_prt(isp, ISP_LOG_SANCFG,
 		    "Chan %d Port 0x%06x@0x%04x is changed",

sys/dev/isp/isp_freebsd.c
@@ -3742,6 +3742,10 @@ isp_async(ispsoftc_t *isp, ispasync_t cmd, ...)
 		break;
 	case ISPASYNC_DEV_CHANGED:
 	case ISPASYNC_DEV_STAYED:		
+	{
+		int crn_reset_done;
+
+		crn_reset_done = 0;
 		va_start(ap, cmd);
 		bus = va_arg(ap, int);
 		lp = va_arg(ap, fcportdb_t *);
@@ -3759,13 +3763,17 @@ isp_async(ispsoftc_t *isp, ispasync_t cmd, ...)
 		     (lp->new_prli_word3 & PRLI_WD3_TARGET_FUNCTION))) {
 			lp->is_target = !lp->is_target;
 			if (lp->is_target) {
-				if (cmd == ISPASYNC_DEV_CHANGED)
+				if (cmd == ISPASYNC_DEV_CHANGED) {
 					isp_fcp_reset_crn(isp, bus, tgt, /*tgt_set*/ 1);
+					crn_reset_done = 1;
+				}
 				isp_make_here(isp, lp, bus, tgt);
 			} else {
 				isp_make_gone(isp, lp, bus, tgt);
-				if (cmd == ISPASYNC_DEV_CHANGED)
+				if (cmd == ISPASYNC_DEV_CHANGED) {
 					isp_fcp_reset_crn(isp, bus, tgt, /*tgt_set*/ 1);
+					crn_reset_done = 1;
+				}
 			}
 		}
 		if (lp->is_initiator !=
@@ -3780,7 +3788,13 @@ isp_async(ispsoftc_t *isp, ispasync_t cmd, ...)
 			adc->arrived = lp->is_initiator;
 			xpt_async(AC_CONTRACT, fc->path, &ac);
 		}
+
+		if ((lp->new_prli_word0 & PRLI_WD0_EST_IMAGE_PAIR) &&
+		    (crn_reset_done == 0))
+			isp_fcp_reset_crn(isp, bus, tgt, /*tgt_set*/ 1);
+
 		break;
+	}
 	case ISPASYNC_DEV_GONE:
 		va_start(ap, cmd);
 		bus = va_arg(ap, int);

sys/dev/isp/isp_stds.h
@@ -284,6 +284,14 @@ typedef struct {
 #define	RNC			0x53
 
 /*
+ * PRLI Word 0 definitions
+ * FPC4-r02b January, 2011
+ */
+#define	PRLI_WD0_TYPE_MASK				0xff000000
+#define	PRLI_WD0_TC_EXT_MASK				0x00ff0000
+#define	PRLI_WD0_EST_IMAGE_PAIR				(1 << 13)
+
+/*
  * PRLI Word 3 definitions
  * FPC4-r02b January, 2011
  */

sys/dev/isp/ispmbox.h
@@ -1384,6 +1384,7 @@ typedef struct {
  */
 typedef struct {
 	uint16_t	handle;
+	uint16_t	prli_word0;
 	uint16_t	prli_word3;
 	uint32_t		: 8,
 			portid	: 24;

sys/dev/isp/ispvar.h
@@ -381,6 +381,9 @@ typedef struct {
 	uint16_t	handle;
 
 	/*
+	 * PRLI word 0 contains the Establish Image Pair bit, which is
+	 * important for knowing when to reset the CRN.
+	 *
 	 * PRLI word 3 parameters contains role as well as other things.
 	 *
 	 * The state is the current state of this entry.
@@ -392,7 +395,9 @@ typedef struct {
 	 * Portid is obvious, as are node && port WWNs. The new_role and
 	 * new_portid is for when we are pending a change.
 	 */
+	uint16_t	prli_word0;		/* PRLI parameters */
 	uint16_t	prli_word3;		/* PRLI parameters */
+	uint16_t	new_prli_word0;		/* Incoming new PRLI parameters */
 	uint16_t	new_prli_word3;		/* Incoming new PRLI parameters */
 	uint16_t			: 12,
 			probational	: 1,

commit 0617d066fadf482f65d30732254ed10b8dd1511c

Fix FC-Tape bugs caused in part by r345008.
The point of r345008 was to reset the Command Reference Number (CRN)
in some situations where a device stayed in the topology, but had
changed somehow.

This can include moving from a switch connection to a direct
connection or vice versa, or a device that temporarily goes away
and comes back.  (e.g. moving to a different switch port)

There were a couple of bugs in that change:
- We were reporting that a device had not changed whenever the
  Establish Image Pair bit was not set.  That is not quite correct.
  Instead, if the Establish Image Pair bit stays the same (set or
  not), the device hasn't changed in that way.

- We weren't setting PRLI Word0 in the port database when a new
  device arrived, so comparisons with the old value for the
  Establish Image Pair bit weren't really possible.  So, make sure
  PRLI Word0 is set in the port database for new devices.

- We were resetting the CRN whenever the Establish Image Pair bit
  was set for a device, even when the device had stayed the same
  and the value of the bit hadn't changed.  Now, only reset the
  CRN for devices that have changed, not devices that sayed the
  same.

The result of all of this was that if we had a single FC device on
an FC port and it went away and came back, we would wind up
correctly resetting the CRN.

But, if we had multiple devices connected via a switch, and there
was any change in one or more of those devices, all of the devices
that stayed the same would also have their CRN values reset.

The result, from a user standpoint, is that the tape drives, etc.
would all start to time out commands and the initiator would send
aborts.

sys/dev/isp/isp.c:
	In isp_pdb_add_update(), look at whether the Establish
	Image Pair bit has changed as part of the check to
	determine whether a device is still the same.   This was
	causing erroneous change notifications.  Also, when
	creating a new port database entry, initialize the
	PRLI Word 0 values.

sys/dev/isp/isp.c
@@ -3251,7 +3251,8 @@ isp_pdb_add_update(ispsoftc_t *isp, int chan, isp_pdb_t *pdb)
 		if (lp->portid == pdb->portid &&
 		    lp->handle == pdb->handle &&
 		    lp->prli_word3 == pdb->prli_word3 &&
-		    ((pdb->prli_word0 & PRLI_WD0_EST_IMAGE_PAIR) == 0)) {
+		    ((pdb->prli_word0 & PRLI_WD0_EST_IMAGE_PAIR) ==
+		     (lp->prli_word0 & PRLI_WD0_EST_IMAGE_PAIR))) {
 			if (lp->state != FC_PORTDB_STATE_NEW)
 				lp->state = FC_PORTDB_STATE_VALID;
 			isp_prt(isp, ISP_LOG_SANCFG,
@@ -3282,6 +3283,7 @@ isp_pdb_add_update(ispsoftc_t *isp, int chan, isp_pdb_t *pdb)
 	lp->probational = 0;
 	lp->state = FC_PORTDB_STATE_NEW;
 	lp->portid = lp->new_portid = pdb->portid;
+	lp->prli_word0 = lp->new_prli_word0 = pdb->prli_word0;
 	lp->prli_word3 = lp->new_prli_word3 = pdb->prli_word3;
 	lp->handle = pdb->handle;
 	lp->port_wwn = wwpn;

sys/dev/isp/isp_freebsd.c
@@ -3789,7 +3789,7 @@ isp_async(ispsoftc_t *isp, ispasync_t cmd, ...)
 			xpt_async(AC_CONTRACT, fc->path, &ac);
 		}
 
-		if ((lp->new_prli_word0 & PRLI_WD0_EST_IMAGE_PAIR) &&
+		if ((cmd == ISPASYNC_DEV_CHANGED) &&
 		    (crn_reset_done == 0))
 			isp_fcp_reset_crn(isp, bus, tgt, /*tgt_set*/ 1);
 