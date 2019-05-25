bffcda439b0ad614de294203bdbee962469c9b75
johalun <johalun@FreeBSD.org>
2019-05-25 18:23:32 +0000

    MFC r344486: Change seq_read to seq_load to avoid namespace conflicts with lkpi
    Sponsored by:	iX Systems

--- sys/kern/kern_descrip.c
+++ sys/kern/kern_descrip.c
@@ -2647,7 +2647,7 @@ fget_unlocked(struct filedesc *fdp, int fd, cap_rights_t *needrightsp,
 	 */
 	for (;;) {
 #ifdef CAPABILITIES
-		seq = seq_read(fd_seq(fdt, fd));
+		seq = seq_load(fd_seq(fdt, fd));
 		fde = &fdt->fdt_ofiles[fd];
 		haverights = *cap_rights_fde_inline(fde);
 		fp = fde->fde_file;
