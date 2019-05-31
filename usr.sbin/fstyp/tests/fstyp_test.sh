3f0dcb331e116f3f172a74554c5f913ea022683d
Alan Somers <asomers@FreeBSD.org>
2019-05-31 16:20:00 +0000

    MFC r345034:
    Drop "All rights reserved" from the files I own
    
    Also, add SPDX tags where needed.

--- usr.sbin/fstyp/tests/fstyp_test.sh
+++ usr.sbin/fstyp/tests/fstyp_test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+#
 # Copyright (c) 2015 Alan Somers
-# All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
