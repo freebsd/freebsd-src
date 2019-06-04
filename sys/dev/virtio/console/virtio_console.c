0f6040f03ebc5d0f99a9ddac8aebe064a495b819
Conrad Meyer <cem@FreeBSD.org>
2019-06-04 02:37:11 +0000

    virtio(4): Add PNP match metadata for virtio devices
    Register MODULE_PNP_INFO for virtio devices using the newbus PNP information
    provided by the previous commit.  Matching can be quite simple; existing
    probe routines only matched on bus (implicit) and device_type.  The same
    matching criteria are retained exactly, but is now also available to
    devmatch(8).
    
    Reviewed by:	bryanv, markj; imp (earlier version)
    Differential Revision:	https://reviews.freebsd.org/D20407

--- sys/dev/virtio/console/virtio_console.c
+++ sys/dev/virtio/console/virtio_console.c
@@ -261,6 +261,10 @@ DRIVER_MODULE(virtio_console, virtio_pci, vtcon_driver, vtcon_devclass,
 MODULE_VERSION(virtio_console, 1);
 MODULE_DEPEND(virtio_console, virtio, 1, 1, 1);
 
+VIRTIO_SIMPLE_PNPTABLE(virtio_console, VIRTIO_ID_CONSOLE,
+    "VirtIO Console Adapter");
+VIRTIO_SIMPLE_PNPINFO(virtio_pci, virtio_console);
+
 static int
 vtcon_modevent(module_t mod, int type, void *unused)
 {
@@ -305,13 +309,7 @@ vtcon_drain_all(void)
 static int
 vtcon_probe(device_t dev)
 {
-
-	if (virtio_get_device_type(dev) != VIRTIO_ID_CONSOLE)
-		return (ENXIO);
-
-	device_set_desc(dev, "VirtIO Console Adapter");
-
-	return (BUS_PROBE_DEFAULT);
+	return (VIRTIO_SIMPLE_PROBE(dev, virtio_console));
 }
 
 static int
--- sys/dev/virtio/virtio.h
+++ sys/dev/virtio/virtio.h
@@ -63,6 +63,21 @@ struct virtio_feature_desc {
 	const char	*vfd_str;
 };
 
+struct virtio_pnp_match {
+	uint32_t	 device_type;
+	const char	*description;
+};
+#define VIRTIO_SIMPLE_PNPTABLE(driver, devtype, desc)			\
+	static const struct virtio_pnp_match driver ## _match = {	\
+		.device_type = devtype,					\
+		.description = desc,					\
+	}
+#define VIRTIO_SIMPLE_PNPINFO(bus, driver)				\
+	MODULE_PNP_INFO("U32:device_type;D:#", bus, driver,		\
+	    &driver ## _match, 1)
+#define VIRTIO_SIMPLE_PROBE(dev, driver)				\
+	(virtio_simple_probe(dev, &driver ## _match))
+
 const char *virtio_device_name(uint16_t devid);
 void	 virtio_describe(device_t dev, const char *msg,
 	     uint64_t features, struct virtio_feature_desc *feature_desc);
@@ -145,5 +160,15 @@ __CONCAT(virtio_set_,name)(device_t dev, void *val)			\
 VIRTIO_WRITE_IVAR(feature_desc,	VIRTIO_IVAR_FEATURE_DESC);
 
 #undef VIRTIO_WRITE_IVAR
+
+static inline int
+virtio_simple_probe(device_t dev, const struct virtio_pnp_match *match)
+{
+
+	if (virtio_get_device_type(dev) != match->device_type)
+		return (ENXIO);
+	device_set_desc(dev, match->description);
+	return (BUS_PROBE_DEFAULT);
+}
 
 #endif /* _VIRTIO_H_ */
