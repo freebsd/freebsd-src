21dcd7c203cb0b4b64cb098c6bcff10124fcc033
cem <cem@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-06-04 02:34:59 +0000

    virtio(4): Expose PNP metadata through newbus
    Expose the same fields and widths from both vtio buses, even though they
    don't quite line up; several virtio drivers can attach to both buses,
    and sharing a PNP info table for both seems more convenient.
    
    In practice, I doubt any virtio driver really needs to match on anything
    other than bus and device_type (eliminating the unused entries for
    vtmmio), and also in practice device_type is << 2^16 (so far, values
    range from 1 to 20).  So it might be fine to only expose a 16-bit
    device_type for PNP purposes.  On the other hand, I don't see much harm
    in overkill here.
    
    Reviewed by:	bryanv, markj (earlier version)
    Differential Revision:	https://reviews.freebsd.org/D20406
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@348598 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/virtio/pci/virtio_pci.c
+++ sys/dev/virtio/pci/virtio_pci.c
@@ -200,6 +200,7 @@ static device_method_t vtpci_methods[] = {
 	/* Bus interface. */
 	DEVMETHOD(bus_driver_added,		  vtpci_driver_added),
 	DEVMETHOD(bus_child_detached,		  vtpci_child_detached),
+	DEVMETHOD(bus_child_pnpinfo_str,	  virtio_child_pnpinfo_str),
 	DEVMETHOD(bus_read_ivar,		  vtpci_read_ivar),
 	DEVMETHOD(bus_write_ivar,		  vtpci_write_ivar),
 
