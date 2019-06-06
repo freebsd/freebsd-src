ddd2f853cddf83f6b83b9c2651470faa22688c0c
avg <avg@FreeBSD.org>
2019-06-06 11:42:26 +0000

    MFC r348153-r348155: gpioled: add a new hint for initial state
    hint.gpioled.%d.state determines the initial state of the LED when the
    driver takes control over it:
      0 - the LED is off
      1 - the LED is on
     -1 - the LED is kept as it was
    
    While here, add a module version declaration.

--- sys/dev/gpio/gpioled.c
+++ sys/dev/gpio/gpioled.c
@@ -115,6 +115,8 @@ gpioled_attach(device_t dev)
 		name = NULL;
 	resource_int_value(device_get_name(dev),
 	    device_get_unit(dev), "invert", &sc->sc_invert);
+	resource_int_value(device_get_name(dev),
+	    device_get_unit(dev), "state", &state);
 
 	sc->sc_leddev = led_create_state(gpioled_control, sc, name ? name :
 	    device_get_nameunit(dev), state);
@@ -155,3 +157,4 @@ static driver_t gpioled_driver = {
 
 DRIVER_MODULE(gpioled, gpiobus, gpioled_driver, gpioled_devclass, 0, 0);
 MODULE_DEPEND(gpioled, gpiobus, 1, 1, 1);
+MODULE_VERSION(gpioled, 1);
