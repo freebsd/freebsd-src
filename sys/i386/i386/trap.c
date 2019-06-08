1729c988e50c6aa0912fa0368d3cd7937f8f908c
kib <kib@FreeBSD.org>
2019-06-08 13:41:39 +0000

    i386 trap.c: Remove unused MAX_TRAP_MSG define.
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week

--- sys/i386/i386/trap.c
+++ sys/i386/i386/trap.c
@@ -125,8 +125,6 @@ void dblfault_handler(void);
 extern inthand_t IDTVEC(bpt), IDTVEC(dbg), IDTVEC(int0x80_syscall);
 extern uint64_t pg_nx;
 
-#define MAX_TRAP_MSG		32
-
 struct trap_data {
 	bool		ei;
 	const char	*msg;
