9e7c77c0b80f27c940e95f04a4b4badb098e76ed
mhorne <mhorne@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-06-09 15:43:38 +0000

    Fix global pointer relaxations in the RISC-V kernel
    The gp register is intended to used by the linker as another means of
    performing relaxations, and should point to the small data section (.sdata).
    
    Currently gp is being used as the pcpu pointer within the kernel, but the more
    appropriate choice for this is the tp register, which is unused.
    
    Swap existing usage of gp with tp within the kernel, and set up gp properly
    at boot with the value of __global_pointer$ for all harts.
    
    Additionally, remove some cases of accessing tp from the PCB, as it is not
    part of the per-thread state. The user's tp and gp should be tracked only
    through the trapframe.
    
    Reviewed by:	markj, jhb
    Approved by:	markj (mentor)
    MFC after:	2 weeks
    Differential Revision:	https://reviews.freebsd.org/D19893
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@348836 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/riscv/include/pcpu.h
+++ sys/riscv/include/pcpu.h
@@ -60,7 +60,7 @@ get_pcpu(void)
 {
 	struct pcpu *pcpu;
 
-	__asm __volatile("mv %0, gp" : "=&r"(pcpu));
+	__asm __volatile("mv %0, tp" : "=&r"(pcpu));
 
 	return (pcpu);
 }
@@ -70,7 +70,7 @@ get_curthread(void)
 {
 	struct thread *td;
 
-	__asm __volatile("ld %0, 0(gp)" : "=&r"(td));
+	__asm __volatile("ld %0, 0(tp)" : "=&r"(td));
 
 	return (td);
 }
