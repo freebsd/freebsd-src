9dfc6fd446f36d5b1836bbfb1c61e07a8d2233d9
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-06-11 20:48:19 +0000

    Minor white space changes.
    Remove trailing white space that's crept into this file.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@348963 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/cam/scsi/scsi_xpt.c
+++ sys/cam/scsi/scsi_xpt.c
@@ -1603,7 +1603,7 @@ probe_device_check:
 				start = strspn(serial_buf->serial_num, " ");
 				slen = serial_buf->length - start;
 				if (slen <= 0) {
-					/* 
+					/*
 					 * SPC5r05 says that an all-space serial
 					 * number means no product serial number
 					 * is available
@@ -2116,7 +2116,7 @@ scsi_scan_bus(struct cam_periph *periph, union ccb *request_ccb)
 			CAM_GET_LUN(target->luns, 0, first);
 			if (first == 0 && scan_info->lunindex[target_id] == 0) {
 				scan_info->lunindex[target_id]++;
-			} 
+			}
 
 			/*
 			 * Skip any LUNs that the HBA can't deal with.
@@ -2601,7 +2601,7 @@ scsi_dev_advinfo(union ccb *start_ccb)
 		 * We fetch extended inquiry data during probe, if
 		 * available.  We don't allow changing it.
 		 */
-		if (cdai->flags & CDAI_FLAG_STORE) 
+		if (cdai->flags & CDAI_FLAG_STORE)
 			return;
 		cdai->provsiz = device->ext_inq_len;
 		if (device->ext_inq_len == 0)
@@ -2993,7 +2993,7 @@ scsi_dev_async(u_int32_t async_code, struct cam_eb *bus, struct cam_et *target,
 		 */
 		if (async_code == AC_SENT_BDR
 		 || async_code == AC_BUS_RESET) {
-			cam_freeze_devq(&newpath); 
+			cam_freeze_devq(&newpath);
 			cam_release_devq(&newpath,
 				RELSIM_RELEASE_AFTER_TIMEOUT,
 				/*reduction*/0,
@@ -3051,7 +3051,7 @@ _scsi_announce_periph(struct cam_periph *periph, u_int *speed, u_int *freq, stru
 	cpi.ccb_h.func_code = XPT_PATH_INQ;
 	xpt_action((union ccb *)&cpi);
 
-	/* Report connection speed */ 
+	/* Report connection speed */
 	*speed = cpi.base_transfer_speed;
 	*freq = 0;
 
