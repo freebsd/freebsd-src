023908374d2b98d659620a3c8c6f01e29ef36f38
trasz <trasz@FreeBSD.org>
2019-06-16 17:32:05 +0000

    In iostat(8) output, skip the decimal point and the fractional part for tps >= 100 and MB/s >= 1000, to prevent them for widening too much.
    MFC after:	2 weeks

--- usr.sbin/iostat/iostat.c
+++ usr.sbin/iostat/iostat.c
@@ -945,9 +945,11 @@ devstats(int perf_select, long double etime, int havelast)
 				       ms_per_transaction);
 		} else {
 			if (Iflag == 0)
-				printf(" %4.1Lf %4.0Lf %5.1Lf ",
+				printf(" %4.*Lf %4.0Lf %5.*Lf ",
+				       kb_per_transfer >= 100 ? 0 : 1,
 				       kb_per_transfer,
 				       transfers_per_second,
+				       mb_per_second >= 1000 ? 0 : 1,
 				       mb_per_second);
 			else {
 				total_mb = total_bytes;
