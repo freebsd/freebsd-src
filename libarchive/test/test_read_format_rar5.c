6de0e4681557b59328c1f8d92f0741387d831fe4
mm <mm@FreeBSD.org>
2019-06-17 11:29:32 +0000

    Update vendor/libarchive/dist to git 809f0dc32fff7434aef45a7c688fa285c7208af7
    Relevant vendor changes:
      PR #1212: RAR5 reader - window_mask was not updated correctly
                (OSS-Fuzz 15278)
      OSS-Fuzz 15120: RAR reader - extend use after free bugfix
      Add HAVE_UNLINKAT to config_freebsd.h

--- libarchive/test/test_read_format_rar5.c
+++ libarchive/test/test_read_format_rar5.c
@@ -1194,3 +1194,24 @@ DEFINE_TEST(test_read_format_rar5_fileattr)
 
 	EPILOGUE();
 }
+
+DEFINE_TEST(test_read_format_rar5_different_window_size)
+{
+	char buf[4096];
+	PROLOGUE("test_read_format_rar5_different_window_size.rar");
+
+	/* Return codes of those calls are ignored, because this sample file
+	 * is invalid. However, the unpacker shouldn't produce any SIGSEGV
+	 * errors during processing. */
+
+	(void) archive_read_next_header(a, &ae);
+	while(0 != archive_read_data(a, buf, sizeof(buf))) {}
+
+	(void) archive_read_next_header(a, &ae);
+	while(0 != archive_read_data(a, buf, sizeof(buf))) {}
+
+	(void) archive_read_next_header(a, &ae);
+	while(0 != archive_read_data(a, buf, sizeof(buf))) {}
+
+	EPILOGUE();
+}
