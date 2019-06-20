bb23d43901b27cf218b8c516322dc2129ed34b6a
Alan Somers <asomers@FreeBSD.org>
2019-06-20 23:12:19 +0000

    fusefs: trivially raise protocol level to 7.14
    The only new feature is splice(2) support on /dev/fuse, which FreeBSD can't
    support.
    
    Sponsored by:	The FreeBSD Foundation

--- sys/fs/fuse/fuse_kernel.h
+++ sys/fs/fuse/fuse_kernel.h
@@ -61,6 +61,9 @@
  * 7.13
  *  - make max number of background requests and congestion threshold
  *    tunables
+ *
+ * 7.14
+ *  - add splice support to fuse device
  */
 
 #ifndef _FUSE_FUSE_KERNEL_H
@@ -81,7 +84,7 @@
 #define FUSE_KERNEL_VERSION 7
 
 /** Minor version number of this interface */
-#define FUSE_KERNEL_MINOR_VERSION 13
+#define FUSE_KERNEL_MINOR_VERSION 14
 
 /** The node ID of the root inode */
 #define FUSE_ROOT_ID 1
