b894196f0bb937f65cb70b4e5c081382ccbbb5ae
Ian Lepore <ian@FreeBSD.org>
2019-06-26 16:38:46 +0000

    MFC r340130, r340134:
    r340130: (by oshogbo)
    
    libcapsicum: Introduce caph_{rights,ioctls,fcntls}_limit
    
    The idea behind those functions is not to force consumers to remember that there
    is a need to check errno on failure. We already have a caph_enter(3) function
    which does the same for cap_enter(2).
    
    r340134: (by oshogbo)
    
    Fix a recusive call introduce in the r340130.

--- lib/libcapsicum/capsicum_helpers.h
+++ lib/libcapsicum/capsicum_helpers.h
@@ -136,6 +136,35 @@ caph_enter(void)
 	return (0);
 }
 
+static __inline int
+caph_rights_limit(int fd, const cap_rights_t *rights)
+{
+
+	if (cap_rights_limit(fd, rights) < 0 && errno != ENOSYS)
+		return (-1);
+
+	return (0);
+}
+
+static __inline int
+caph_ioctls_limit(int fd, const unsigned long *cmds, size_t ncmds)
+{
+
+	if (cap_ioctls_limit(fd, cmds, ncmds) < 0 && errno != ENOSYS)
+		return (-1);
+
+	return (0);
+}
+
+static __inline int
+caph_fcntls_limit(int fd, uint32_t fcntlrights)
+{
+
+	if (cap_fcntls_limit(fd, fcntlrights) < 0 && errno != ENOSYS)
+		return (-1);
+
+	return (0);
+}
 
 static __inline int
 caph_enter_casper(void)
