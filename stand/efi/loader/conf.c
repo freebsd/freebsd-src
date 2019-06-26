172063529ee96a423eb85bc54374b25e643fb81c
bcran <bcran@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-06-26 03:06:57 +0000

    Re-enable loader efi http boot and fix dv_open bug if dv_init failed
    The code in efihttp.c was assuming that dv_open wouldn't be called if
    dv_init failed. But the dv_init return value is currently ignored.
    
    Add a new variable, `efihttp_init_done` and only proceed in dv_open if
    it's true. This fixes the loader on systems without efi http support.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@349404 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- stand/efi/loader/conf.c
+++ stand/efi/loader/conf.c
@@ -39,6 +39,7 @@ struct devsw *devsw[] = {
 	&efipart_fddev,
 	&efipart_cddev,
 	&efipart_hddev,
+	&efihttp_dev, /* ordering with efinet_dev matters */
 	&efinet_dev,
 	&vdisk_dev,
 #ifdef EFI_ZFS_BOOT
@@ -54,6 +55,7 @@ struct fs_ops *file_system[] = {
 	&dosfs_fsops,
 	&ufs_fsops,
 	&cd9660_fsops,
+	&efihttp_fsops,
 	&tftp_fsops,
 	&nfs_fsops,
 	&gzipfs_fsops,
