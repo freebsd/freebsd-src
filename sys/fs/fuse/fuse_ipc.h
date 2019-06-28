8aafc8c3894e5d5d302bd04d88aec04d719b4377
Alan Somers <asomers@FreeBSD.org>
2019-06-28 04:18:10 +0000

    [skip ci] update copyright headers in fusefs files
    Sponsored by:	The FreeBSD Foundation

--- sys/fs/fuse/fuse_ipc.h
+++ sys/fs/fuse/fuse_ipc.h
@@ -32,6 +32,11 @@
  * 
  * Copyright (C) 2005 Csaba Henk.
  * All rights reserved.
+ *
+ * Copyright (c) 2019 The FreeBSD Foundation
+ *
+ * Portions of this software were developed by BFF Storage Systems, LLC under
+ * sponsorship from the FreeBSD Foundation.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
