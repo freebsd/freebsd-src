cb858e4120c6aeb00d6fa2c3508e94db5b977378
luporl <luporl@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-06-28 15:49:14 +0000

    Fix missing powerpc64 in _LLVM_TARGET_FILT
    This change was originally in D20378. Making it in a new diff since it's a
    bugfix.
    
    Submitted by:	alfredo.junior_eldorado.org.br
    Reviewed by:	emaste, luporl
    Differential Revision:	https://reviews.freebsd.org/D20756
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@349508 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -269,7 +269,7 @@ __LLVM_TARGETS= \
 		powerpc \
 		sparc \
 		x86
-__LLVM_TARGET_FILT=	C/(amd64|i386)/x86/:S/sparc64/sparc/:S/arm64/aarch64/
+__LLVM_TARGET_FILT=	C/(amd64|i386)/x86/:S/sparc64/sparc/:S/arm64/aarch64/:S/powerpc64/powerpc/
 .for __llt in ${__LLVM_TARGETS}
 # Default the given TARGET's LLVM_TARGET support to the value of MK_CLANG.
 .if ${__TT:${__LLVM_TARGET_FILT}} == ${__llt}
