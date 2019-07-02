3a76d901d629ebe03757fdac79174fa775654403
Alexander Motin <mav@FreeBSD.org>
2019-07-02 15:01:54 +0000

    Include sys/lock.h, as told by man page.
    MFC after:	1 week

--- sys/dev/ntb/ntb_hw/ntb_hw_amd.c
+++ sys/dev/ntb/ntb_hw/ntb_hw_amd.c
@@ -59,6 +59,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/kernel.h>
 #include <sys/systm.h>
 #include <sys/bus.h>
+#include <sys/lock.h>
 #include <sys/malloc.h>
 #include <sys/module.h>
 #include <sys/mutex.h>
