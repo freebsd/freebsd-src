13738a6816f88c46ee5bd68fad8fa28190bafb63
philip <philip@FreeBSD.org>
2019-07-02 12:22:27 +0000

    MFC r349592: Import tzdata 2019b

--- contrib/tzdata/zoneinfo2tdf.pl
+++ contrib/tzdata/zoneinfo2tdf.pl
@@ -28,7 +28,7 @@ while (<>) {
 
   $contZone = '';
   if ($type eq 'zone') {
-    # Zone  NAME  GMTOFF  RULES/SAVE  FORMAT  [UNTIL]
+    # Zone  NAME  STDOFF  RULES/SAVE  FORMAT  [UNTIL]
     my $nfields = @fields;
     $nfields >= 5 or warn "bad zone line";
     if ($nfields > 6) {
