1ab7a3c679d7799521e36a9c39b4a2b80fd38c6b
delphij <delphij@FreeBSD.org>
2019-07-04 07:29:18 +0000

    MFC r349505: Upgrade to Bzip2 version 1.0.7.

--- contrib/bzip2/spewG.c
+++ contrib/bzip2/spewG.c
@@ -13,8 +13,8 @@
    This file is part of bzip2/libbzip2, a program and library for
    lossless, block-sorting data compression.
 
-   bzip2/libbzip2 version 1.0.6 of 6 September 2010
-   Copyright (C) 1996-2010 Julian Seward <jseward@bzip.org>
+   bzip2/libbzip2 version 1.0.7 of 27 June 2019
+   Copyright (C) 1996-2010 Julian Seward <jseward@acm.org>
 
    Please read the WARNING, DISCLAIMER and PATENTS sections in the 
    README file.
