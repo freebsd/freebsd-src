3da662ea190f1a05a313246ff046a965b8a21ef1
philip <philip@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-07-10 22:36:14 +0000

    telnet: fix minor style violation
    While here also fix a very unlikely NULL pointer dereference.
    
    Submitted by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@349896 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/telnet/telnet/commands.c
+++ contrib/telnet/telnet/commands.c
@@ -45,6 +45,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#include <assert.h>
 #include <ctype.h>
 #include <err.h>
 #include <errno.h>
@@ -1654,11 +1655,13 @@ env_init(void)
 		|| (strncmp((char *)ep->value, "unix:", 5) == 0))) {
 		char hbuf[256+1];
 		char *cp2 = strchr((char *)ep->value, ':');
+                size_t buflen;
 
 		gethostname(hbuf, sizeof(hbuf));
 		hbuf[sizeof(hbuf)-1] = '\0';
-                unsigned int buflen = strlen(hbuf) + strlen(cp2) + 1;
+ 		buflen = strlen(hbuf) + strlen(cp2) + 1;
 		cp = (char *)malloc(sizeof(char)*buflen);
+		assert(cp != NULL);
 		snprintf((char *)cp, buflen, "%s%s", hbuf, cp2);
 		free(ep->value);
 		ep->value = (unsigned char *)cp;
