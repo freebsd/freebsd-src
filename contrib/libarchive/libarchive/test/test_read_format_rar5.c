fa709acf62f2242cfab90006535da65eed0ad4cd
Martin Matuska <mm@FreeBSD.org>
2019-07-11 00:22:10 +0000

    MFC r349527,349538: Sync libarchive with vendor.
    Relevant vendor changes:
      PR #1217: RAR5 reader - fix ARM filter going beyond window buffer boundary
                (OSS-Fuzz 15431)
      PR #1218: Fixes to sparse file handling

--- contrib/libarchive/libarchive/test/test_read_format_rar5.c
+++ contrib/libarchive/libarchive/test/test_read_format_rar5.c
@@ -1215,3 +1215,18 @@ DEFINE_TEST(test_read_format_rar5_different_window_size)
 
 	EPILOGUE();
 }
+
+DEFINE_TEST(test_read_format_rar5_arm_filter_on_window_boundary)
+{
+	char buf[4096];
+	PROLOGUE("test_read_format_rar5_arm_filter_on_window_boundary.rar");
+
+	/* Return codes of those calls are ignored, because this sample file
+	 * is invalid. However, the unpacker shouldn't produce any SIGSEGV
+	 * errors during processing. */
+
+	(void) archive_read_next_header(a, &ae);
+	while(0 != archive_read_data(a, buf, sizeof(buf))) {}
+
+	EPILOGUE();
+}
