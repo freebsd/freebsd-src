2b0cf324da73772e75d312352e0ecf91fb2c8ea7
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-07-15 08:38:01 +0000

    Correctly check for casueword(9) success in do_set_ceiling().
    After r349951, the return code must be checked instead of old == new
    comparision.
    
    Reviewed by:	markj
    Tested by:	pho
    Sponsored by:	The FreeBSD Foundation
    MFC after:	12 days
    Differential revision:	https://reviews.freebsd.org/D20949
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@349994 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/kern_umtx.c
+++ sys/kern/kern_umtx.c
@@ -2456,7 +2456,8 @@ do_set_ceiling(struct thread *td, struct umutex *m, uint32_t ceiling,
 			break;
 		}
 
-		if (owner == UMUTEX_CONTESTED) {
+		if (rv == 0) {
+			MPASS(owner == UMUTEX_CONTESTED);
 			rv = suword32(&m->m_ceilings[0], ceiling);
 			rv1 = suword32(&m->m_owner, UMUTEX_CONTESTED);
 			error = (rv == 0 && rv1 == 0) ? 0: EFAULT;
