b507f690f7dc00df042d0406516649a9f5693c1f
Vincenzo Maffione <vmaffione@FreeBSD.org>
2019-07-15 20:12:54 +0000

    MFC r349966
    netmap: fix bug introduced by r349752
    
    r349752 introduced a NULL pointer reference bug
    in the emulated netmap code.
    
    Reported by:    lwhsu

--- sys/dev/netmap/netmap_generic.c
+++ sys/dev/netmap/netmap_generic.c
@@ -1126,7 +1126,8 @@ generic_netmap_attach(struct ifnet *ifp)
 
 	nm_os_generic_set_features(gna);
 
-	nm_prinf("Emulated adapter for %s created (prev was %s)", na->name, gna->prev->name);
+	nm_prinf("Emulated adapter for %s created (prev was %s)", na->name,
+	    gna->prev ? gna->prev->name : "NULL");
 
 	return retval;
 }
