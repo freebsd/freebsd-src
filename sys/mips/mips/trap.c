c18ca749162a8524ea5b3a6ada0b1e94a46fb81a
John Baldwin <jhb@FreeBSD.org>
2019-07-15 21:25:16 +0000

    Don't pass error from syscallenter() to syscallret().
    syscallret() doesn't use error anymore.  Fix a few other places to permit
    removing the return value from syscallenter() entirely.
    - Remove a duplicated assertion from arm's syscall().
    - Use td_errno for amd64_syscall_ret_flush_l1d.
    
    Reviewed by:	kib
    MFC after:	1 month
    Sponsored by:	DARPA
    Differential Revision:	https://reviews.freebsd.org/D2090

--- sys/mips/mips/trap.c
+++ sys/mips/mips/trap.c
@@ -787,10 +787,8 @@ dofault:
 
 	case T_SYSCALL + T_USER:
 		{
-			int error;
-
 			td->td_sa.trapframe = trapframe;
-			error = syscallenter(td);
+			syscallenter(td);
 
 #if !defined(SMP) && (defined(DDB) || defined(DEBUG))
 			if (trp == trapdebug)
@@ -806,7 +804,7 @@ dofault:
 			 * instead of being done here under a special check
 			 * for SYS_ptrace().
 			 */
-			syscallret(td, error);
+			syscallret(td);
 			return (trapframe->pc);
 		}
 
