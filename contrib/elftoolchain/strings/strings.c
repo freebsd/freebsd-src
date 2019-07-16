8b3b31c02fa1f80a0c09b5d30410313f9e899276
oshogbo <oshogbo@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-07-16 04:17:25 +0000

    strings: extends rights
    The libelf is using mmap if it can, if not it was fall backing to read.
    
    Reported by:	markj
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@350024 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/elftoolchain/strings/strings.c
+++ contrib/elftoolchain/strings/strings.c
@@ -194,7 +194,7 @@ main(int argc, char **argv)
 	argc -= optind;
 	argv += optind;
 
-	cap_rights_init(&rights, CAP_READ, CAP_SEEK, CAP_FSTAT, CAP_FCNTL);
+	cap_rights_init(&rights, CAP_READ, CAP_SEEK, CAP_FSTAT, CAP_FCNTL, CAP_MMAP_R);
 	fa = fileargs_init(argc, argv, O_RDONLY, 0, &rights, FA_OPEN);
 	if (fa == NULL)
 		err(1, "Unable to initialize casper fileargs");
