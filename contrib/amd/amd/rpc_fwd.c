ebf11cb3773487b398efb55831c6c4f404dbbe89
brooks <brooks@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-07-16 16:03:08 +0000

    Fix two mismatches between function declaration and definition.
    In both cases, function pointer arguments were inconsistently declared
    and the result worked because of C's odd rules around function pointer
    (de)references.  With a stricter compiler these fail to compile.
    
    Reviewed by:	cem
    Obtained from:	CheriBSD
    Sponsored by:	DARPA, AFRL
    Differential Revision:	https://reviews.freebsd.org/D20964
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@350049 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/amd/amd/rpc_fwd.c
+++ contrib/amd/amd/rpc_fwd.c
@@ -221,7 +221,7 @@ fwd_locate(u_int id)
  * different address.
  */
 int
-fwd_packet(int type_id, char *pkt, int len, struct sockaddr_in *fwdto, struct sockaddr_in *replyto, opaque_t cb_arg, fwd_fun cb)
+fwd_packet(int type_id, char *pkt, int len, struct sockaddr_in *fwdto, struct sockaddr_in *replyto, opaque_t cb_arg, fwd_fun *cb)
 {
   rpc_forward *p;
   u_int *pkt_int;
