67bce0147ed6b20ffd4d255aa7044c677580a79d
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-07-17 20:43:14 +0000

    Remove now-obsolete comment.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@350094 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/nvme/nvme.c
+++ sys/dev/nvme/nvme.c
@@ -452,8 +452,7 @@ nvme_register_consumer(nvme_cons_ns_fn_t ns_fn, nvme_cons_ctrlr_fn_t ctrlr_fn,
 	int i;
 
 	/*
-	 * TODO: add locking around consumer registration.  Not an issue
-	 *  right now since we only have one nvme consumer - nvd(4).
+	 * TODO: add locking around consumer registration.
 	 */
 	for (i = 0; i < NVME_MAX_CONSUMERS; i++)
 		if (nvme_consumer[i].id == INVALID_CONSUMER_ID) {
