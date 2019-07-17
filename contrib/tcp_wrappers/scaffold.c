7b653f6ca07081deda1f18618408d3dbfe3ecf8d
brooks <brooks@FreeBSD.org>
2019-07-17 23:43:14 +0000

    Remove a duplicate global (rfc931_timeout).
    It is declared here and in rfc931.c and unused here so keep that copy
    and discard this one.
    
    Obtained from:	CheriBSD
    MFC after:	1 week
    Sponsored by:	DARPA, AFRL

--- contrib/tcp_wrappers/scaffold.c
+++ contrib/tcp_wrappers/scaffold.c
@@ -39,7 +39,6 @@ static char sccs_id[] = "@(#) scaffold.c 1.6 97/03/21 19:27:24";
   */
 int     allow_severity = SEVERITY;
 int     deny_severity = LOG_WARNING;
-int     rfc931_timeout = RFC931_TIMEOUT;
 
 #ifndef INET6
 /* dup_hostent - create hostent in one memory block */
