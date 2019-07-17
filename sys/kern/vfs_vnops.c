7cf98b02f3f78dbc425180822e6b2aaa88340df5
asomers <asomers@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-07-17 17:01:07 +0000

    F_READAHEAD: Fix r349248's overflow protection, broken by r349391
    I accidentally broke the main point of r349248 when making stylistic changes
    in r349391.  Restore the original behavior, and also fix an additional
    overflow that was possible when uio->uio_resid was nearly SSIZE_MAX.
    
    Reported by:	cem
    Reviewed by:	bde
    MFC after:	2 weeks
    MFC-With:	349248
    Sponsored by:	The FreeBSD Foundation
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@350088 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/vfs_vnops.c
+++ sys/kern/vfs_vnops.c
@@ -499,8 +499,13 @@ sequential_heuristic(struct uio *uio, struct file *fp)
 		 * closely related to the best I/O size for real disks than
 		 * to any block size used by software.
 		 */
-		fp->f_seqcount += lmin(IO_SEQMAX,
-		    howmany(uio->uio_resid, 16384));
+		if (uio->uio_resid >= IO_SEQMAX * 16384)
+			fp->f_seqcount = IO_SEQMAX;
+		else {
+			fp->f_seqcount += howmany(uio->uio_resid, 16384);
+			if (fp->f_seqcount > IO_SEQMAX)
+				fp->f_seqcount = IO_SEQMAX;
+		}
 		return (fp->f_seqcount << IO_SEQSHIFT);
 	}
 
