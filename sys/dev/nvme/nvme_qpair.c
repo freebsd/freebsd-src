3c612f5151189b5650ed2fc57a2c3e5220ab7f22
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-07-18 21:58:51 +0000

    Provide new tunable hw.nvme.verbose_cmd_dump
    The nvme drive dumps only the most relevant details about a command when it
    fails. However, there are times this is not sufficient (such as debugging weird
    issues for a new drive with a vendor). Setting hw.nvme.verbose_cmd_dump=1
    in loader.conf will enable more complete debugging information about each
    command that fails.
    
    Reviewed by: rpokala
    Sponsored by: Netflix
    Differential Version: https://reviews.freebsd.org/D20988
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@350118 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/nvme/nvme_qpair.c
+++ sys/dev/nvme/nvme_qpair.c
@@ -178,6 +178,16 @@ nvme_qpair_print_command(struct nvme_qpair *qpair, struct nvme_command *cmd)
 		nvme_admin_qpair_print_command(qpair, cmd);
 	else
 		nvme_io_qpair_print_command(qpair, cmd);
+	if (nvme_verbose_cmd_dump) {
+		nvme_printf(qpair->ctrlr,
+		    "nsid:%#x rsvd2:%#x rsvd3:%#x mptr:%#jx prp1:%#jx prp2:%#jx\n",
+		    cmd->nsid, cmd->rsvd2, cmd->rsvd3, (uintmax_t)cmd->mptr,
+		    (uintmax_t)cmd->prp1, (uintmax_t)cmd->prp2);
+		nvme_printf(qpair->ctrlr,
+		    "cdw10: %#x cdw11:%#x cdw12:%#x cdw13:%#x cdw14:%#x cdw15:%#x\n",
+		    cmd->cdw10, cmd->cdw11, cmd->cdw12, cmd->cdw13, cmd->cdw14,
+		    cmd->cdw15);
+	}
 }
 
 struct nvme_status_string {
