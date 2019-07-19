db118f5e8a92ecaf1c93edc5dc3eb7ed9bbc2775
Mark Johnston <markj@FreeBSD.org>
2019-07-19 15:11:14 +0000

    MFC r350037: Fix the arm64 page table entry attribute mask.

--- sys/arm64/include/pte.h
+++ sys/arm64/include/pte.h
@@ -40,7 +40,7 @@ typedef	uint64_t	pt_entry_t;		/* page table entry */
 
 /* Block and Page attributes */
 /* TODO: Add the upper attributes */
-#define	ATTR_MASK_H	UINT64_C(0xfff0000000000000)
+#define	ATTR_MASK_H	UINT64_C(0xfffc000000000000)
 #define	ATTR_MASK_L	UINT64_C(0x0000000000000fff)
 #define	ATTR_MASK	(ATTR_MASK_H | ATTR_MASK_L)
 /* Bits 58:55 are reserved for software */
