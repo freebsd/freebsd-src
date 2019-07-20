274bb93aad142625d2a0471afe20ee7a191fbea3
Li-Wen Hsu <lwhsu@FreeBSD.org>
2019-07-20 07:04:25 +0000

    MFC r349872:
    Correct definitions in sys.opencrypto.runtests.main for 32bit platform
    
    Reviewed by:	cem, jhb
    MFC after:	3 days
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D20894

--- tests/sys/opencrypto/cryptodev.py
+++ tests/sys/opencrypto/cryptodev.py
@@ -35,6 +35,7 @@ import array
 import dpkt
 from fcntl import ioctl
 import os
+import platform
 import signal
 from struct import pack as _pack
 
@@ -106,14 +107,19 @@ class CryptAEAD(dpkt.Packet):
 # h2py.py can't handle multiarg macros
 CRIOGET = 3221513060
 CIOCGSESSION = 3224396645
-CIOCGSESSION2 = 3225445226
 CIOCFSESSION = 2147771238
-CIOCCRYPT = 3224396647
 CIOCKEY = 3230688104
 CIOCASYMFEAT = 1074029417
 CIOCKEY2 = 3230688107
 CIOCFINDDEV = 3223610220
-CIOCCRYPTAEAD = 3225445229
+if platform.architecture()[0] == '64bit':
+    CIOCGSESSION2 = 3225445226
+    CIOCCRYPT = 3224396647
+    CIOCCRYPTAEAD = 3225445229
+else:
+    CIOCGSESSION2 = 3224396650
+    CIOCCRYPT = 3223085927
+    CIOCCRYPTAEAD = 3223872365
 
 def _getdev():
 	fd = os.open('/dev/crypto', os.O_RDWR)
