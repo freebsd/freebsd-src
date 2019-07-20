db99ae1d76424c67f0bc380112755d371f985319
cem <cem@FreeBSD.org>
2019-07-20 20:56:31 +0000

    motd: Generate from template to /var/run
    Update login(1), its manual pages, similar utilities, and motd.5 to refer to
    the new location.
    
    Suggested by:	delphij@ (re: r349256)
    Reviewed by:	bcr (manpages), delphij
    Differential Revision:	https://reviews.freebsd.org/D20721

--- release/picobsd/tinyware/login/pathnames.h
+++ release/picobsd/tinyware/login/pathnames.h
@@ -39,7 +39,7 @@
 #include <paths.h>
 
 #define	_PATH_HUSHLOGIN		".hushlogin"
-#define	_PATH_MOTDFILE		"/etc/motd"
+#define	_PATH_MOTDFILE		"/var/run/motd"
 #define _PATH_LOGACCESS		"/etc/login.access"
 #define _PATH_FBTAB		"/etc/fbtab"
 #define _PATH_LOGINDEVPERM	"/etc/logindevperm"
