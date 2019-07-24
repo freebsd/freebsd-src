782845e111b289f1ef0a512640e962cf06baa278
Gordon Tetlow <gordon@FreeBSD.org>
2019-07-24 12:53:06 +0000

    Fix pts write-after-free.
    Approved by:	so
    Security:	FreeBSD-SA-19:13.pts
    Security:	CVE-2019-5606

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -230,9 +230,6 @@ ttydev_leave(struct tty *tp)
 
 	tp->t_flags |= TF_OPENCLOSE;
 
-	/* Stop asynchronous I/O. */
-	funsetown(&tp->t_sigio);
-
 	/* Remove console TTY. */
 	if (constty == tp)
 		constty_clear();
@@ -1122,6 +1119,9 @@ tty_rel_free(struct tty *tp)
 		tty_unlock(tp);
 		return;
 	}
+
+	/* Stop asynchronous I/O. */
+	funsetown(&tp->t_sigio);
 
 	/* TTY can be deallocated. */
 	dev = tp->t_dev;
