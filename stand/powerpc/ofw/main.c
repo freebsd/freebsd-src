70ed86f4bad71a7258405d6af4329a339f9c40ab
luporl <luporl@FreeBSD.org>
2019-07-31 20:31:36 +0000

    [PPC64] Implement CAS
    Guest PPC OSs running under a hypervisor may communicate the features they
    support, in order for the hypervisor to expose a virtualized machine in the way
    the client (guest OS) expects (see LoPAPR 1.1 - B.6.2.3).
    
    This is done by calling the "/ibm,client-architecture-support" (CAS) method,
    informing supported features in option vectors.  Until now, FreeBSD wasn't
    using CAS, but instead relied on hypervisor/QEMU's defaults.
    
    The problem is that, without CAS, it is very inconvenient to run POWER9 VMs on
    a POWER9 host running with radix enabled.  This happens because, in this case,
    the QEMU default is to present the guest OS a dual MMU (HPT/RPT), instead of
    presenting a regular HPT MMU, as FreeBSD expects, resulting in an early panic.
    The known workarounds required either changing the host to disable radix or
    passing a flag to QEMU to run in a POWER8 compatible mode.
    
    With CAS, FreeBSD is now able to communicate that it wants an HPT MMU,
    independent of the host setup, which now makes FreeBSD work on POWER9/pseries,
    with KVM enabled and without hugepages (support added in a previous commit).
    
    As CAS is invoked through OpenFirmware's call-method interface, it needs to be
    performed early, when OpenFirmware is still operational. Besides, now that FDT
    is the default way to inspect the device tree on PPC, OFW call-method feature
    will be unavailable by default, when control is passed to the kernel. Because
    of this, the call to CAS is being performed at the loader, instead of at the
    kernel.
    
    To avoid regressions with old platforms, this change uses CAS only on
    POWER8/POWER9.
    
    Reviewed by:	jhibbits
    Differential Revision:	https://reviews.freebsd.org/D20827

--- stand/powerpc/ofw/main.c
+++ stand/powerpc/ofw/main.c
@@ -89,6 +89,21 @@ memsize(void)
 	return (memsz);
 }
 
+#ifdef CAS
+extern int ppc64_cas(void);
+
+static int
+ppc64_autoload(void)
+{
+	const char *cas;
+
+	if ((cas = getenv("cas")) && cas[0] == '1')
+		if (ppc64_cas() != 0)
+			return (-1);
+	return (ofw_autoload());
+}
+#endif
+
 int
 main(int (*openfirm)(void *))
 {
@@ -169,7 +184,12 @@ main(int (*openfirm)(void *))
 	archsw.arch_copyin = ofw_copyin;
 	archsw.arch_copyout = ofw_copyout;
 	archsw.arch_readin = ofw_readin;
+#ifdef CAS
+	setenv("cas", "1", 0);
+	archsw.arch_autoload = ppc64_autoload;
+#else
 	archsw.arch_autoload = ofw_autoload;
+#endif
 
 	interact();				/* doesn't return */
 
