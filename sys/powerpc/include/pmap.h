937e9fbaef82e1ff790a0f509fb5d0a2376850a8
jhibbits <jhibbits@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-08-01 03:55:58 +0000

    powerpc64/mmu: Use a SLIST for the PVO delete list, instead of a RB_TREE
    Summary:
    Although it's convenient to reuse the pvo_plist for deletion, RB_TREE
    insertion and removal is not free, and can result in a lot of extra work
    to rebalance the tree.  Instead, use a SLIST as a LIFO delete queue,
    which gives us almost free insertion, deletion, and traversal.
    
    Reviewed by:	luporl
    Differential Revision: https://reviews.freebsd.org/D21061
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@350493 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/powerpc/include/pmap.h
+++ sys/powerpc/include/pmap.h
@@ -90,7 +90,10 @@ struct pvo_entry {
 #ifndef __powerpc64__
 	LIST_ENTRY(pvo_entry) pvo_olink;	/* Link to overflow entry */
 #endif
-	RB_ENTRY(pvo_entry) pvo_plink;	/* Link to pmap entries */
+	union {
+		RB_ENTRY(pvo_entry) pvo_plink;	/* Link to pmap entries */
+		SLIST_ENTRY(pvo_entry) pvo_dlink; /* Link to delete enty */
+	};
 	struct {
 #ifndef __powerpc64__
 		/* 32-bit fields */
@@ -106,6 +109,7 @@ struct pvo_entry {
 	uint64_t	pvo_vpn;		/* Virtual page number */
 };
 LIST_HEAD(pvo_head, pvo_entry);
+SLIST_HEAD(pvo_dlist, pvo_entry);
 RB_HEAD(pvo_tree, pvo_entry);
 int pvo_vaddr_compare(struct pvo_entry *, struct pvo_entry *);
 RB_PROTOTYPE(pvo_tree, pvo_entry, pvo_plink, pvo_vaddr_compare);
