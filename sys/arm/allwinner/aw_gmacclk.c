925f0b8db3e6b5b61fc0ff062c3ce7d1c9542701
manu <manu@FreeBSD.org>
2019-08-05 17:32:16 +0000

    MFC r346293:
    allwinner: clk: Garbage collect old clock implementation
    
    The old clocks are disconneted from the build since r337344.
    Remove all those pseudo drivers. The only one remaining is for gmac
    (the ethernet controller) so move it to sys/arm/allwinner.
    While here remove a83t support from gmacclk as it is unneeded since r326114.

--- sys/arm/allwinner/aw_gmacclk.c
+++ sys/arm/allwinner/aw_gmacclk.c
@@ -27,7 +27,7 @@
  */
 
 /*
- * Allwinner APB clock
+ * Allwinner GMAC clock
  */
 
 #include <sys/cdefs.h>
@@ -45,70 +45,74 @@ __FBSDID("$FreeBSD$");
 #include <dev/ofw/ofw_bus_subr.h>
 #include <dev/ofw/ofw_subr.h>
 
-#include <dev/extres/clk/clk.h>
+#include <dev/extres/clk/clk_mux.h>
+#include <dev/extres/clk/clk_gate.h>
 
 #include "clkdev_if.h"
 
-#define	A10_APB0_CLK_RATIO		(0x3 << 8)
-#define	A10_APB0_CLK_RATIO_SHIFT	8
-#define	A10_APB1_CLK_SRC_SEL		(0x3 << 24)
-#define	A10_APB1_CLK_SRC_SEL_SHIFT	24
-#define	A10_APB1_CLK_SRC_SEL_MAX	0x3
-#define	A10_APB1_CLK_RAT_N		(0x3 << 16)
-#define	A10_APB1_CLK_RAT_N_SHIFT	16
-#define	A10_APB1_CLK_RAT_M		(0x1f << 0)
-#define	A10_APB1_CLK_RAT_M_SHIFT	0
-#define	A23_APB0_CLK_RATIO		(0x3 << 0)
-#define	A23_APB0_CLK_RATIO_SHIFT	0
-#define	A83T_APB1_CLK_RATIO		(0x3 << 8)
-#define	A83T_APB1_CLK_RATIO_SHIFT	8
+#define	GMAC_CLK_PIT		(0x1 << 2)
+#define	GMAC_CLK_PIT_SHIFT	2
+#define	GMAC_CLK_PIT_MII	0
+#define	GMAC_CLK_PIT_RGMII	1
+#define	GMAC_CLK_SRC		(0x3 << 0)
+#define	GMAC_CLK_SRC_SHIFT	0
+#define	GMAC_CLK_SRC_MII	0
+#define	GMAC_CLK_SRC_EXT_RGMII	1
+#define	GMAC_CLK_SRC_RGMII	2
 
-enum aw_apbclk_type {
-	AW_A10_APB0 = 1,
-	AW_A10_APB1,
-	AW_A23_APB0,
-	AW_A83T_APB1,
-};
+#define	EMAC_TXC_DIV_CFG	(1 << 15)
+#define	EMAC_TXC_DIV_CFG_SHIFT	15
+#define	EMAC_TXC_DIV_CFG_125MHZ	0
+#define	EMAC_TXC_DIV_CFG_25MHZ	1
+#define	EMAC_PHY_SELECT		(1 << 16)
+#define	EMAC_PHY_SELECT_SHIFT	16
+#define	EMAC_PHY_SELECT_INT	0
+#define	EMAC_PHY_SELECT_EXT	1
+#define	EMAC_ETXDC		(0x7 << 10)
+#define	EMAC_ETXDC_SHIFT	10
+#define	EMAC_ERXDC		(0x1f << 5)
+#define	EMAC_ERXDC_SHIFT	5
+
+#define	CLK_IDX_MII		0
+#define	CLK_IDX_RGMII		1
+#define	CLK_IDX_COUNT		2
 
 static struct ofw_compat_data compat_data[] = {
-	{ "allwinner,sun4i-a10-apb0-clk",	AW_A10_APB0 },
-	{ "allwinner,sun4i-a10-apb1-clk",	AW_A10_APB1 },
-	{ "allwinner,sun8i-a23-apb0-clk",	AW_A23_APB0 },
-	{ "allwinner,sun8i-a83t-apb1-clk",	AW_A83T_APB1 },
+	{ "allwinner,sun7i-a20-gmac-clk",	1 },
 	{ NULL, 0 }
 };
 
-struct aw_apbclk_sc {
-	device_t		clkdev;
-	bus_addr_t		reg;
-	enum aw_apbclk_type	type;
+struct aw_gmacclk_sc {
+	device_t	clkdev;
+	bus_addr_t	reg;
+
+	int		rx_delay;
+	int		tx_delay;
 };
 
-#define	APBCLK_READ(sc, val)	CLKDEV_READ_4((sc)->clkdev, (sc)->reg, (val))
-#define	APBCLK_WRITE(sc, val)	CLKDEV_WRITE_4((sc)->clkdev, (sc)->reg, (val))
+#define	GMACCLK_READ(sc, val)	CLKDEV_READ_4((sc)->clkdev, (sc)->reg, (val))
+#define	GMACCLK_WRITE(sc, val)	CLKDEV_WRITE_4((sc)->clkdev, (sc)->reg, (val))
 #define	DEVICE_LOCK(sc)		CLKDEV_DEVICE_LOCK((sc)->clkdev)
 #define	DEVICE_UNLOCK(sc)	CLKDEV_DEVICE_UNLOCK((sc)->clkdev)
 
 static int
-aw_apbclk_init(struct clknode *clk, device_t dev)
+aw_gmacclk_init(struct clknode *clk, device_t dev)
 {
-	struct aw_apbclk_sc *sc;
+	struct aw_gmacclk_sc *sc;
 	uint32_t val, index;
 
 	sc = clknode_get_softc(clk);
 
-	switch (sc->type) {
-	case AW_A10_APB0:
-	case AW_A23_APB0:
-	case AW_A83T_APB1:
-		index = 0;
+	DEVICE_LOCK(sc);
+	GMACCLK_READ(sc, &val);
+	DEVICE_UNLOCK(sc);
+
+	switch ((val & GMAC_CLK_SRC) >> GMAC_CLK_SRC_SHIFT) {
+	case GMAC_CLK_SRC_MII:
+		index = CLK_IDX_MII;
 		break;
-	case AW_A10_APB1:
-		DEVICE_LOCK(sc);
-		APBCLK_READ(sc, &val);
-		DEVICE_UNLOCK(sc);
-		index = (val & A10_APB1_CLK_SRC_SEL) >>
-		    A10_APB1_CLK_SRC_SEL_SHIFT;
+	case GMAC_CLK_SRC_RGMII:
+		index = CLK_IDX_RGMII;
 		break;
 	default:
 		return (ENXIO);
@@ -119,85 +123,52 @@ aw_apbclk_init(struct clknode *clk, device_t dev)
 }
 
 static int
-aw_apbclk_recalc_freq(struct clknode *clk, uint64_t *freq)
+aw_gmacclk_set_mux(struct clknode *clk, int index)
 {
-	struct aw_apbclk_sc *sc;
-	uint32_t val, div, m, n;
+	struct aw_gmacclk_sc *sc;
+	uint32_t val, clk_src, pit, txc_div;
+	int error;
 
 	sc = clknode_get_softc(clk);
+	error = 0;
 
-	DEVICE_LOCK(sc);
-	APBCLK_READ(sc, &val);
-	DEVICE_UNLOCK(sc);
-
-	switch (sc->type) {
-	case AW_A10_APB0:
-		div = 1 << ((val & A10_APB0_CLK_RATIO) >>
-		    A10_APB0_CLK_RATIO_SHIFT);
-		if (div == 1)
-			div = 2;
-		*freq = *freq / div;
+	switch (index) {
+	case CLK_IDX_MII:
+		clk_src = GMAC_CLK_SRC_MII;
+		pit = GMAC_CLK_PIT_MII;
+		txc_div = EMAC_TXC_DIV_CFG_25MHZ;
 		break;
-	case AW_A10_APB1:
-		n = 1 << ((val & A10_APB1_CLK_RAT_N) >>
-		    A10_APB1_CLK_RAT_N_SHIFT);
-		m = ((val & A10_APB1_CLK_RAT_N) >>
-		    A10_APB1_CLK_RAT_M_SHIFT) + 1;
-		*freq = *freq / n / m;
-		break;
-	case AW_A23_APB0:
-		div = 1 << ((val & A23_APB0_CLK_RATIO) >>
-		    A23_APB0_CLK_RATIO_SHIFT);
-		*freq = *freq / div;
-		break;
-	case AW_A83T_APB1:
-		div = ((val & A83T_APB1_CLK_RATIO) >>
-		    A83T_APB1_CLK_RATIO_SHIFT) + 1;
-		*freq = *freq / div;
+	case CLK_IDX_RGMII:
+		clk_src = GMAC_CLK_SRC_RGMII;
+		pit = GMAC_CLK_PIT_RGMII;
+		txc_div = EMAC_TXC_DIV_CFG_125MHZ;
 		break;
 	default:
 		return (ENXIO);
 	}
 
-	return (0);
-}
-
-static int
-aw_apbclk_set_mux(struct clknode *clk, int index)
-{
-	struct aw_apbclk_sc *sc;
-	uint32_t val;
-
-	sc = clknode_get_softc(clk);
-
-	if (sc->type != AW_A10_APB1)
-		return (ENXIO);
-
-	if (index < 0 || index > A10_APB1_CLK_SRC_SEL_MAX)
-		return (ERANGE);
-
 	DEVICE_LOCK(sc);
-	APBCLK_READ(sc, &val);
-	val &= ~A10_APB1_CLK_SRC_SEL;
-	val |= (index << A10_APB1_CLK_SRC_SEL_SHIFT);
-	APBCLK_WRITE(sc, val);
+	GMACCLK_READ(sc, &val);
+	val &= ~(GMAC_CLK_SRC | GMAC_CLK_PIT);
+	val |= (clk_src << GMAC_CLK_SRC_SHIFT);
+	val |= (pit << GMAC_CLK_PIT_SHIFT);
+	GMACCLK_WRITE(sc, val);
 	DEVICE_UNLOCK(sc);
 
 	return (0);
 }
 
-static clknode_method_t aw_apbclk_clknode_methods[] = {
+static clknode_method_t aw_gmacclk_clknode_methods[] = {
 	/* Device interface */
-	CLKNODEMETHOD(clknode_init,		aw_apbclk_init),
-	CLKNODEMETHOD(clknode_recalc_freq,	aw_apbclk_recalc_freq),
-	CLKNODEMETHOD(clknode_set_mux,		aw_apbclk_set_mux),
+	CLKNODEMETHOD(clknode_init,		aw_gmacclk_init),
+	CLKNODEMETHOD(clknode_set_mux,		aw_gmacclk_set_mux),
 	CLKNODEMETHOD_END
 };
-DEFINE_CLASS_1(aw_apbclk_clknode, aw_apbclk_clknode_class,
-    aw_apbclk_clknode_methods, sizeof(struct aw_apbclk_sc), clknode_class);
+DEFINE_CLASS_1(aw_gmacclk_clknode, aw_gmacclk_clknode_class,
+    aw_gmacclk_clknode_methods, sizeof(struct aw_gmacclk_sc), clknode_class);
 
 static int
-aw_apbclk_probe(device_t dev)
+aw_gmacclk_probe(device_t dev)
 {
 	if (!ofw_bus_status_okay(dev))
 		return (ENXIO);
@@ -205,15 +176,15 @@ aw_apbclk_probe(device_t dev)
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
-	device_set_desc(dev, "Allwinner APB Clock");
+	device_set_desc(dev, "Allwinner GMAC Clock");
 	return (BUS_PROBE_DEFAULT);
 }
 
 static int
-aw_apbclk_attach(device_t dev)
+aw_gmacclk_attach(device_t dev)
 {
 	struct clknode_init_def def;
-	struct aw_apbclk_sc *sc;
+	struct aw_gmacclk_sc *sc;
 	struct clkdom *clkdom;
 	struct clknode *clk;
 	clk_t clk_parent;
@@ -231,9 +202,9 @@ aw_apbclk_attach(device_t dev)
 
 	error = ofw_bus_parse_xref_list_get_length(node, "clocks",
 	    "#clock-cells", &ncells);
-	if (error != 0) {
-		device_printf(dev, "cannot get clock count\n");
-		return (error);
+	if (error != 0 || ncells != CLK_IDX_COUNT) {
+		device_printf(dev, "couldn't find parent clocks\n");
+		return (ENXIO);
 	}
 
 	clkdom = clkdom_create(dev);
@@ -250,7 +221,7 @@ aw_apbclk_attach(device_t dev)
 	for (i = 0; i < ncells; i++) {
 		error = clk_get_by_ofw_index(dev, 0, i, &clk_parent);
 		if (error != 0) {
-			device_printf(dev, "cannot get clock %d\n", i);
+			device_printf(dev, "cannot get clock %d\n", error);
 			goto fail;
 		}
 		def.parent_names[i] = clk_get_name(clk_parent);
@@ -258,7 +229,7 @@ aw_apbclk_attach(device_t dev)
 	}
 	def.parent_cnt = ncells;
 
-	clk = clknode_create(clkdom, &aw_apbclk_clknode_class, &def);
+	clk = clknode_create(clkdom, &aw_gmacclk_clknode_class, &def);
 	if (clk == NULL) {
 		device_printf(dev, "cannot create clknode\n");
 		error = ENXIO;
@@ -266,9 +237,11 @@ aw_apbclk_attach(device_t dev)
 	}
 
 	sc = clknode_get_softc(clk);
-	sc->type = ofw_bus_search_compatible(dev, compat_data)->ocd_data;
 	sc->reg = paddr;
 	sc->clkdev = device_get_parent(dev);
+	sc->tx_delay = sc->rx_delay = -1;
+	OF_getencprop(node, "tx-delay", &sc->tx_delay, sizeof(sc->tx_delay));
+	OF_getencprop(node, "rx-delay", &sc->rx_delay, sizeof(sc->rx_delay));
 
 	clknode_register(clkdom, clk);
 
@@ -287,21 +260,21 @@ fail:
 	return (error);
 }
 
-static device_method_t aw_apbclk_methods[] = {
+static device_method_t aw_gmacclk_methods[] = {
 	/* Device interface */
-	DEVMETHOD(device_probe,		aw_apbclk_probe),
-	DEVMETHOD(device_attach,	aw_apbclk_attach),
+	DEVMETHOD(device_probe,		aw_gmacclk_probe),
+	DEVMETHOD(device_attach,	aw_gmacclk_attach),
 
 	DEVMETHOD_END
 };
 
-static driver_t aw_apbclk_driver = {
-	"aw_apbclk",
-	aw_apbclk_methods,
+static driver_t aw_gmacclk_driver = {
+	"aw_gmacclk",
+	aw_gmacclk_methods,
 	0
 };
 
-static devclass_t aw_apbclk_devclass;
+static devclass_t aw_gmacclk_devclass;
 
-EARLY_DRIVER_MODULE(aw_apbclk, simplebus, aw_apbclk_driver,
-    aw_apbclk_devclass, 0, 0, BUS_PASS_BUS + BUS_PASS_ORDER_MIDDLE);
+EARLY_DRIVER_MODULE(aw_gmacclk, simplebus, aw_gmacclk_driver,
+    aw_gmacclk_devclass, 0, 0, BUS_PASS_BUS + BUS_PASS_ORDER_MIDDLE);
