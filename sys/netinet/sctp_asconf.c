94962f6ba0e0afacd3b4f503cbfdf516a3c50c0a
Michael Tuexen <tuexen@FreeBSD.org>
2019-08-05 13:22:15 +0000

    Improve consistency. No functional change.
    MFC after:		3 days

--- sys/netinet/sctp_asconf.c
+++ sys/netinet/sctp_asconf.c
@@ -1957,7 +1957,7 @@ sctp_addr_mgmt_assoc(struct sctp_inpcb *inp, struct sctp_tcb *stcb,
 
 			/* invalid if we are a v6 only endpoint */
 			if ((inp->sctp_flags & SCTP_PCB_FLAGS_BOUND_V6) &&
-			    SCTP_IPV6_V6ONLY(&inp->ip_inp.inp))
+			    SCTP_IPV6_V6ONLY(inp))
 				return;
 
 			sin = &ifa->address.sin;
@@ -2030,9 +2030,8 @@ sctp_asconf_iterator_ep(struct sctp_inpcb *inp, void *ptr, uint32_t val SCTP_UNU
 		case AF_INET:
 			{
 				/* invalid if we are a v6 only endpoint */
-
 				if ((inp->sctp_flags & SCTP_PCB_FLAGS_BOUND_V6) &&
-				    SCTP_IPV6_V6ONLY(&inp->ip_inp.inp)) {
+				    SCTP_IPV6_V6ONLY(inp)) {
 					cnt_invalid++;
 					if (asc->cnt == cnt_invalid)
 						return (1);
@@ -2147,7 +2146,7 @@ sctp_asconf_iterator_stcb(struct sctp_inpcb *inp, struct sctp_tcb *stcb,
 
 				/* invalid if we are a v6 only endpoint */
 				if ((inp->sctp_flags & SCTP_PCB_FLAGS_BOUND_V6) &&
-				    SCTP_IPV6_V6ONLY(&inp->ip_inp.inp))
+				    SCTP_IPV6_V6ONLY(inp))
 					continue;
 
 				sin = &ifa->address.sin;
@@ -2164,7 +2163,7 @@ sctp_asconf_iterator_stcb(struct sctp_inpcb *inp, struct sctp_tcb *stcb,
 					continue;
 				}
 				if ((inp->sctp_flags & SCTP_PCB_FLAGS_BOUND_V6) &&
-				    SCTP_IPV6_V6ONLY(&inp->ip_inp.inp)) {
+				    SCTP_IPV6_V6ONLY(inp)) {
 					cnt_invalid++;
 					if (asc->cnt == cnt_invalid)
 						return;
