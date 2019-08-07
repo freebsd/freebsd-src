3194b270146ad7bf128f3d2cb4866954801a81fe
Hans Petter Selasky <hselasky@FreeBSD.org>
2019-08-07 13:35:13 +0000

    Correct PCI device ID for XHCI USB controller.
    Submitted by:	Dmitry Luhtionov <dmitryluhtionov@gmail.com>
    MFC after:	1 week
    Sponsored by:	Mellanox Technologies

--- sys/dev/usb/controller/xhci_pci.c
+++ sys/dev/usb/controller/xhci_pci.c
@@ -149,7 +149,7 @@ xhci_pci_match(device_t self)
 		return ("Intel Lewisburg USB 3.0 controller");
 	case 0xa2af8086:
 		return ("Intel Union Point USB 3.0 controller");
-	case 0x36d88086:
+	case 0xa36d8086:
 		return ("Intel Cannon Lake USB 3.1 controller");
 
 	case 0xa01b177d:
