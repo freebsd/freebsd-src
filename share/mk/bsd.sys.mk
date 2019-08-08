4c8a9e2fb6a968ca2d84c197efbf85b293903a1b
cem <cem@FreeBSD.org>
2019-08-08 04:29:46 +0000

    r350739 try #2
    For some inexplicable reason, C++ compilers reject the -Wno- flag, and also
    (ab)use CWARNFLAGS.
    
    Reported by:	imp

--- share/mk/bsd.sys.mk
+++ share/mk/bsd.sys.mk
@@ -187,7 +187,7 @@ CWARNFLAGS+=	-Wno-unknown-pragmas
 .endif # IGNORE_PRAGMA
 
 # This warning is utter nonsense
-CWARNFLAGS+=	-Wno-format-zero-length
+CFLAGS+=	-Wno-format-zero-length
 
 # We need this conditional because many places that use it
 # only enable it for some files with CLFAGS.$FILE+=${CLANG_NO_IAS}.
