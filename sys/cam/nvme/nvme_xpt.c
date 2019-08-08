1c243686814a84a9dda3a02c0313a5868ecdca43
mav <mav@FreeBSD.org>
2019-08-08 02:01:01 +0000

    MFC r345815: Make cam_error_print() decode NVMe commands.

--- sys/cam/nvme/nvme_xpt.c
+++ sys/cam/nvme/nvme_xpt.c
@@ -769,11 +769,13 @@ nvme_proto_debug_out(union ccb *ccb)
 {
 	char cdb_str[(sizeof(struct nvme_command) * 3) + 1];
 
-	if (ccb->ccb_h.func_code != XPT_NVME_IO)
+	if (ccb->ccb_h.func_code != XPT_NVME_IO ||
+	    ccb->ccb_h.func_code != XPT_NVME_ADMIN)
 		return;
 
 	CAM_DEBUG(ccb->ccb_h.path,
-	    CAM_DEBUG_CDB,("%s. NCB: %s\n", nvme_op_string(&ccb->nvmeio.cmd),
+	    CAM_DEBUG_CDB,("%s. NCB: %s\n", nvme_op_string(&ccb->nvmeio.cmd,
+		ccb->ccb_h.func_code == XPT_NVME_ADMIN),
 		nvme_cmd_string(&ccb->nvmeio.cmd, cdb_str, sizeof(cdb_str))));
 }
 
