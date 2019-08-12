efa2cfbd3c21fd4cde230ecff8a47e585bf3b3a2
mav <mav@FreeBSD.org>
2019-08-12 17:54:28 +0000

    MFC r342358 (by imp): Try the first 256 units with nvmecontrol devlist.
    The nvmecontrol code that did the devlist assumed that we had a
    tightly-packed allocation of units. Since pci writing exists, this
    isn't the case. Loop over the first 256 units, which is a reasonable
    number of possible units.

--- sbin/nvmecontrol/devlist.c
+++ sbin/nvmecontrol/devlist.c
@@ -46,6 +46,8 @@ __FBSDID("$FreeBSD$");
 #define DEVLIST_USAGE							       \
 	"devlist\n"
 
+#define NVME_MAX_UNIT 256
+
 static inline uint32_t
 ns_get_sector_size(struct nvme_namespace_data *nsdata)
 {
@@ -79,19 +81,17 @@ devlist(const struct nvme_function *nf, int argc, char *argv[])
 	ctrlr = -1;
 	found = 0;
 
-	while (1) {
+	while (ctrlr < NVME_MAX_UNIT) {
 		ctrlr++;
 		sprintf(name, "%s%d", NVME_CTRLR_PREFIX, ctrlr);
 
 		ret = open_dev(name, &fd, 0, 0);
 
-		if (ret != 0) {
-			if (ret == EACCES) {
-				warnx("could not open "_PATH_DEV"%s\n", name);
-				continue;
-			} else
-				break;
-		}
+		if (ret == EACCES) {
+			warnx("could not open "_PATH_DEV"%s\n", name);
+			continue;
+		} else if (ret != 0)
+			continue;
 
 		found++;
 		read_controller_data(fd, &cdata);
