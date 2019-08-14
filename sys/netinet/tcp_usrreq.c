6f0b39115bf66fed92538e2a8a5780c7aed3b593
jhb <jhb@FreeBSD.org>
2019-08-14 23:05:57 +0000

    MFC 346360: Push down INP_WLOCK slightly in tcp_ctloutput.
    The inp lock is not needed for testing the V6 flag as that flag is set
    once when the inp is created and never changes.  For non-TCP socket
    options the lock is immediately dropped after checking that flag.
    This just pushes the lock down to only be acquired for TCP socket
    options.
    
    This isn't a hot-path, more a cosmetic cleanup I noticed while reading
    the code.

--- sys/netinet/tcp_usrreq.c
+++ sys/netinet/tcp_usrreq.c
@@ -1578,11 +1578,9 @@ tcp_ctloutput(struct socket *so, struct sockopt *sopt)
 	error = 0;
 	inp = sotoinpcb(so);
 	KASSERT(inp != NULL, ("tcp_ctloutput: inp == NULL"));
-	INP_WLOCK(inp);
 	if (sopt->sopt_level != IPPROTO_TCP) {
 #ifdef INET6
 		if (inp->inp_vflag & INP_IPV6PROTO) {
-			INP_WUNLOCK(inp);
 			error = ip6_ctloutput(so, sopt);
 			/*
 			 * In case of the IPV6_USE_MIN_MTU socket option,
@@ -1627,12 +1625,12 @@ tcp_ctloutput(struct socket *so, struct sockopt *sopt)
 #endif
 #ifdef INET
 		{
-			INP_WUNLOCK(inp);
 			error = ip_ctloutput(so, sopt);
 		}
 #endif
 		return (error);
 	}
+	INP_WLOCK(inp);
 	if (inp->inp_flags & (INP_TIMEWAIT | INP_DROPPED)) {
 		INP_WUNLOCK(inp);
 		return (ECONNRESET);
