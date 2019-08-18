86e43d4a571167f3c9abf3a1268bddd26e2f8125
wulf <wulf@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-08-18 22:11:42 +0000

    ng_ubt(4): do not attach Intel Wireless 8260/8265 in bootloader mode.
    Add helper function for synchronous execution of HCI commands at probe
    stage and use this function to check firmware state of Intel Wireless
    8260/8265 bluetooth devices found in many post 2016 year laptops.
    
    Attempt to initialize FreeBSD bluetooth stack while such a device is in
    bootloader mode locks the adapter hardly so it requires power on/off
    cycle to restore.
    
    This change blocks ng_ubt attachment unless operational firmware is
    loaded thus preventing the lock up.
    
    PR:			237083
    Reviewed by:		hps, emax
    MFC after:		2 weeks
    Differential Revision:	https://reviews.freebsd.org/D21071
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@351196 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netgraph/bluetooth/drivers/ubt/ng_ubt_var.h
+++ sys/netgraph/bluetooth/drivers/ubt/ng_ubt_var.h
@@ -74,6 +74,35 @@ enum {
 	UBT_N_TRANSFER,		/* total number of transfers */
 };
 
+/* USB control request (HCI command) structure */
+struct ubt_hci_cmd {
+	uint16_t	opcode;
+	uint8_t		length;
+	uint8_t		data[];
+} __attribute__ ((packed));
+#define	UBT_HCI_CMD_SIZE(cmd) \
+	((cmd)->length + offsetof(struct ubt_hci_cmd, data))
+
+/* USB interrupt transfer HCI event header structure */
+struct ubt_hci_evhdr {
+	uint8_t		event;
+	uint8_t		length;
+} __attribute__ ((packed));
+/* USB interrupt transfer (generic HCI event) structure */
+struct ubt_hci_event {
+	struct ubt_hci_evhdr	header;
+	uint8_t			data[];
+} __attribute__ ((packed));
+/* USB interrupt transfer (HCI command completion event) structure */
+struct ubt_hci_event_command_compl {
+	struct ubt_hci_evhdr	header;
+	uint8_t			numpkt;
+	uint16_t		opcode;
+	uint8_t			data[];
+} __attribute__ ((packed));
+#define	UBT_HCI_EVENT_SIZE(evt) \
+	((evt)->header.length + offsetof(struct ubt_hci_event, data))
+
 /* USB device softc structure */
 struct ubt_softc {
 	device_t		sc_dev;		/* for debug printf */
@@ -128,6 +157,12 @@ struct ubt_softc {
 };
 typedef struct ubt_softc	ubt_softc_t;
 typedef struct ubt_softc *	ubt_softc_p;
+
+usb_error_t	ubt_do_hci_request(struct usb_device *, struct ubt_hci_cmd *,
+		    void *, usb_timeout_t);
+
+extern	devclass_t	ubt_devclass;
+extern	driver_t	ubt_driver;
 
 #endif /* ndef _NG_UBT_VAR_H_ */
 
