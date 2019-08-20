7d6988fdd2aee0e033034e147f16fe05594a60e4
Dimitry Andric <dim@FreeBSD.org>
2019-08-20 18:01:33 +0000

    Remove upstream files and directories from vendor/lld/dist that we do not use.  This saves on repository space, and reduces the number of tree conflicts when merging.

--- test/ELF/invalid/section-alignment2.s
+++
@@ -1,5 +0,0 @@
-## section-alignment-notpow2.elf has section alignment
-## 0xFFFFFFFF which is not a power of 2.
-# RUN: not ld.lld %p/Inputs/section-alignment-notpow2.elf -o /dev/null 2>&1 | \
-# RUN:   FileCheck %s
-# CHECK: section sh_addralign is not a power of 2
