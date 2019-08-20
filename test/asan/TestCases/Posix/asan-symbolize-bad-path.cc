63714eb5809e39666dec2454c354195e76f916ba
Dimitry Andric <dim@FreeBSD.org>
2019-08-20 18:00:15 +0000

    Remove upstream files and directories from vendor/compiler-rt/dist that we do not use.  This saves on repository space, and reduces the number of tree conflicts when merging.

--- test/asan/TestCases/Posix/asan-symbolize-bad-path.cc
+++
@@ -1,4 +0,0 @@
-// Test that asan_symbolize does not hang when provided with an non-existing
-// path.
-// RUN: echo '#0 0xabcdabcd (%t/bad/path+0x1234)' | %asan_symbolize | FileCheck %s
-// CHECK: #0 0xabcdabcd
