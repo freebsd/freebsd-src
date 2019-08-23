f992cd89daaa796bc2171c0d8a2fab0385cbd778
delphij <delphij@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-08-23 05:23:45 +0000

    Properly update FSInfo block after generation.
    After populating the filesystem, write a FSInfo block with
    proper information.
    
    Reviewed by:	emaste, cem
    MFC after:	2 weeks
    Differential Revision:	https://reviews.freebsd.org/D21363
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@351415 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/makefs/msdos/msdosfs_vfsops.c
+++ usr.sbin/makefs/msdos/msdosfs_vfsops.c
@@ -359,3 +359,33 @@ msdosfs_root(struct msdosfsmount *pmp, struct vnode *vp) {
 	vp->v_data = ndep;
 	return 0;
 }
+
+/*
+ * If we have an FSInfo block, update it.
+ */
+int
+msdosfs_fsiflush(struct msdosfsmount *pmp)
+{
+	struct fsinfo *fp;
+	struct buf *bp;
+	int error;
+
+	if (pmp->pm_fsinfo == 0 || (pmp->pm_flags & MSDOSFS_FSIMOD) == 0) {
+		error = 0;
+		goto out;
+	}
+	error = bread(pmp->pm_devvp, pmp->pm_fsinfo, pmp->pm_BytesPerSec,
+	    NOCRED, &bp);
+	if (error != 0) {
+		brelse(bp);
+		goto out;
+	}
+	fp = (struct fsinfo *)bp->b_data;
+	putulong(fp->fsinfree, pmp->pm_freeclustercount);
+	putulong(fp->fsinxtfree, pmp->pm_nxtfree);
+	pmp->pm_flags &= ~MSDOSFS_FSIMOD;
+	error = bwrite(bp);
+
+out:
+	return (error);
+}
