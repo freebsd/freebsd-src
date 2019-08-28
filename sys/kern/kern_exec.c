b5d239cb970f97e96deed8bcde7d689b61e47bcf
Mark Johnston <markj@FreeBSD.org>
2019-08-28 16:08:06 +0000

    Wire pages in vm_page_grab() when appropriate.
    uiomove_object_page() and exec_map_first_page() would previously wire a
    page after having grabbed it.  Ask vm_page_grab() to perform the wiring
    instead: this removes some redundant code, and is cheaper in the case
    where the requested page is not resident since the page allocator can be
    asked to initialize the page as wired, whereas a separate vm_page_wire()
    call requires the page lock.
    
    In vm_imgact_hold_page(), use vm_page_unwire_noq() instead of
    vm_page_unwire(PQ_NONE).  The latter ensures that the page is dequeued
    before returning, but this is unnecessary since vm_page_free() will
    trigger a batched dequeue of the page.
    
    Reviewed by:	alc, kib
    Tested by:	pho (part of a larger patch)
    MFC after:	1 week
    Sponsored by:	Netflix
    Differential Revision:	https://reviews.freebsd.org/D21440

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -972,11 +972,13 @@ exec_map_first_page(struct image_params *imgp)
 #if VM_NRESERVLEVEL > 0
 	vm_object_color(object, 0);
 #endif
-	ma[0] = vm_page_grab(object, 0, VM_ALLOC_NORMAL | VM_ALLOC_NOBUSY);
+	ma[0] = vm_page_grab(object, 0, VM_ALLOC_NORMAL | VM_ALLOC_NOBUSY |
+	    VM_ALLOC_WIRED);
 	if (ma[0]->valid != VM_PAGE_BITS_ALL) {
 		vm_page_xbusy(ma[0]);
 		if (!vm_pager_has_page(object, 0, NULL, &after)) {
 			vm_page_lock(ma[0]);
+			vm_page_unwire_noq(ma[0]);
 			vm_page_free(ma[0]);
 			vm_page_unlock(ma[0]);
 			VM_OBJECT_WUNLOCK(object);
@@ -1004,6 +1006,8 @@ exec_map_first_page(struct image_params *imgp)
 		if (rv != VM_PAGER_OK) {
 			for (i = 0; i < initial_pagein; i++) {
 				vm_page_lock(ma[i]);
+				if (i == 0)
+					vm_page_unwire_noq(ma[i]);
 				vm_page_free(ma[i]);
 				vm_page_unlock(ma[i]);
 			}
@@ -1014,9 +1018,6 @@ exec_map_first_page(struct image_params *imgp)
 		for (i = 1; i < initial_pagein; i++)
 			vm_page_readahead_finish(ma[i]);
 	}
-	vm_page_lock(ma[0]);
-	vm_page_wire(ma[0]);
-	vm_page_unlock(ma[0]);
 	VM_OBJECT_WUNLOCK(object);
 
 	imgp->firstpage = sf_buf_alloc(ma[0], 0);
