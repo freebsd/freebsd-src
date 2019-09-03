854a74e65c4da703aebb81babb47620d5c20ac9f
imp <imp@FreeBSD.org>
2019-09-03 15:26:11 +0000

    Implement nvme suspend / resume for pci attachment
    When we suspend, we need to properly shutdown the NVME controller. The
    controller may go into D3 state (or may have the power removed), and
    to properly flush the metadata to non-volatile RAM, we must complete a
    normal shutdown. This consists of deleting the I/O queues and setting
    the shutodown bit. We have to do some extra stuff to make sure we
    reset the software state of the queues as well.
    
    On resume, we have to reset the card twice, for reasons described in
    the attach funcion. Once we've done that, we can restart the card. If
    any of this fails, we'll fail the NVMe card, just like we do when a
    reset fails.
    
    Set is_resetting for the duration of the suspend / resume. This keeps
    the reset taskqueue from running a concurrent reset, and also is
    needed to prevent any hw completions from queueing more I/O to the
    card. Pass resetting flag to nvme_ctrlr_start. It doesn't need to get
    that from the global state of the ctrlr. Wait for any pending reset to
    finish. All queued I/O will get sent to the hardware as part of
    nvme_ctrlr_start(), though the upper layers shouldn't send any
    down. Disabling the qpairs is the other failsafe to ensure all I/O is
    queued.
    
    Rename nvme_ctrlr_destory_qpairs to nvme_ctrlr_delete_qpairs to avoid
    confusion with all the other destroy functions.  It just removes the
    queues in hardware, while the other _destroy_ functions tear down
    driver data structures.
    
    Split parts of the hardware reset function up so that I can
    do part of the reset in suspsend. Split out the software disabling
    of the qpairs into nvme_ctrlr_disable_qpairs.
    
    Finally, fix a couple of spelling errors in comments related to
    this.
    
    Relnotes: Yes
    MFC After: 1 week
    Reviewed by: scottl@ (prior version)
    Differential Revision: https://reviews.freebsd.org/D21493

--- sys/dev/nvme/nvme_private.h
+++ sys/dev/nvme/nvme_private.h
@@ -556,4 +556,7 @@ void	nvme_notify_ns(struct nvme_controller *ctrlr, int nsid);
 void	nvme_ctrlr_intx_handler(void *arg);
 void	nvme_ctrlr_poll(struct nvme_controller *ctrlr);
 
+int	nvme_ctrlr_suspend(struct nvme_controller *ctrlr);
+int	nvme_ctrlr_resume(struct nvme_controller *ctrlr);
+
 #endif /* __NVME_PRIVATE_H__ */
