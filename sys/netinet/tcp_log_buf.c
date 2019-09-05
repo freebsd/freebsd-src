c1b1efbe5b2cec80e45c20caf77ed519a87d4b4a
Warner Losh <imp@FreeBSD.org>
2019-09-05 22:38:53 +0000

    MFC r343755:
      Regularize the Netflix copyright

--- sys/netinet/tcp_log_buf.c
+++ sys/netinet/tcp_log_buf.c
@@ -1,8 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
- * Copyright (c) 2016-2018
- *	Netflix Inc.  All rights reserved.
+ * Copyright (c) 2016-2018 Netflix, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
