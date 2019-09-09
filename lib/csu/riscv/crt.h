2fce6d83eab62dbdbe8465f7f471bc0c3c3b2627
kp <kp@FreeBSD.org>
2019-09-09 16:25:09 +0000

    csu: Add the riscv .init call sequence
    Reviewed by:	br
    Sponsored by:	Axiado
    Differential Revision:	https://reviews.freebsd.org/D21537

--- lib/csu/riscv/crt.h
+++ lib/csu/riscv/crt.h
@@ -1,2 +1,9 @@
 /* $FreeBSD$ */
-/* Empty so we can include this unconditionally */
+
+#ifndef _CRT_H_
+#define _CRT_H_
+
+#define	HAVE_CTORS
+#define	INIT_CALL_SEQ(func)	"call " __STRING(func)
+
+#endif
