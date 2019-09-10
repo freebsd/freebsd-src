34401169d2a0567608a69030635844e664ea0b2c
Konstantin Belousov <kib@FreeBSD.org>
2019-09-10 06:45:44 +0000

    MFC r351773: Add procctl(PROC_STACKGAP_CTL).
    PR:	239894

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -756,6 +756,8 @@ interpret:
 	p->p_flag |= P_EXEC;
 	if ((p->p_flag2 & P2_NOTRACE_EXEC) == 0)
 		p->p_flag2 &= ~P2_NOTRACE;
+	if ((p->p_flag2 & P2_STKGAP_DISABLE_EXEC) == 0)
+		p->p_flag2 &= ~P2_STKGAP_DISABLE;
 	if (p->p_flag & P_PPWAIT) {
 		p->p_flag &= ~(P_PPWAIT | P_PPTRACE);
 		cv_broadcast(&p->p_pwait);
