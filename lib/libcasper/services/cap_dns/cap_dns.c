4071628a72af3d26d97f988a073f397c7209144f
Mariusz Zaborski <oshogbo@FreeBSD.org>
2019-09-10 20:49:47 +0000

    MFCr340638:  libcasper: provide compatibility with the old version of service
      Some external tools like tcpdump(1) have upstream the changes with old limits
      name. Because of that provide compatibility with the old names.
    
      Reported by:  emaste

--- lib/libcasper/services/cap_dns/cap_dns.c
+++ lib/libcasper/services/cap_dns/cap_dns.c
@@ -474,7 +474,8 @@ dns_gethostbyname(const nvlist_t *limits, const nvlist_t *nvlin,
 	struct hostent *hp;
 	int family;
 
-	if (!dns_allowed_type(limits, "NAME2ADDR"))
+	if (!dns_allowed_type(limits, "NAME2ADDR") &&
+	    !dns_allowed_type(limits, "NAME"))
 		return (NO_RECOVERY);
 
 	family = (int)nvlist_get_number(nvlin, "family");
@@ -498,7 +499,8 @@ dns_gethostbyaddr(const nvlist_t *limits, const nvlist_t *nvlin,
 	size_t addrsize;
 	int family;
 
-	if (!dns_allowed_type(limits, "ADDR2NAME"))
+	if (!dns_allowed_type(limits, "ADDR2NAME") &&
+	    !dns_allowed_type(limits, "ADDR"))
 		return (NO_RECOVERY);
 
 	family = (int)nvlist_get_number(nvlin, "family");
@@ -524,7 +526,8 @@ dns_getnameinfo(const nvlist_t *limits, const nvlist_t *nvlin, nvlist_t *nvlout)
 	socklen_t salen;
 	int error, flags;
 
-	if (!dns_allowed_type(limits, "ADDR2NAME"))
+	if (!dns_allowed_type(limits, "ADDR2NAME") &&
+	    !dns_allowed_type(limits, "ADDR"))
 		return (NO_RECOVERY);
 
 	error = 0;
@@ -617,7 +620,8 @@ dns_getaddrinfo(const nvlist_t *limits, const nvlist_t *nvlin, nvlist_t *nvlout)
 	unsigned int ii;
 	int error, family, n;
 
-	if (!dns_allowed_type(limits, "NAME2ADDR"))
+	if (!dns_allowed_type(limits, "NAME2ADDR") &&
+	    !dns_allowed_type(limits, "NAME"))
 		return (NO_RECOVERY);
 
 	hostname = dnvlist_get_string(nvlin, "hostname", NULL);
@@ -703,7 +707,9 @@ dns_limit(const nvlist_t *oldlimits, const nvlist_t *newlimits)
 				return (EINVAL);
 			type = nvlist_get_string(newlimits, name);
 			if (strcmp(type, "ADDR2NAME") != 0 &&
-			    strcmp(type, "NAME2ADDR") != 0) {
+			    strcmp(type, "NAME2ADDR") != 0 &&
+			    strcmp(type, "ADDR") != 0 &&
+			    strcmp(type, "NAME") != 0) {
 				return (EINVAL);
 			}
 			if (!dns_allowed_type(oldlimits, type))
