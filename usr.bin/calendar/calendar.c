7ff75f4a5a3d9ada5bd68384fdded357fa4c1105
glebius <glebius@FreeBSD.org>
2019-09-10 04:21:48 +0000

    Fix 'calendar -a' in several ways.
    o Do not run any iconv() processing in -a. The locale of root user is not
      what is desired by most of the users who receive their calendar mail.
      Just assume that users store their calendars in a format that is readable
      to them. This fixes regression from r344340.
    o fork() and setusercontext(LOGIN_SETALL) for every user. This makes LANG
      set inside a calendar file mostly excessive, as we will pick up user's
      login class LANG.
    o This also executes complex function cal() that parses user owned files
      with appropriate user privileges.
      Previously it was run with privileges dropped only temporary for execution
      of cal(), and fully dropped only before invoking sendmail (see r22473).
    
    Reviewed by:	bapt (older version of patch)

--- usr.bin/calendar/calendar.c
+++ usr.bin/calendar/calendar.c
@@ -44,9 +44,11 @@ static char sccsid[] = "@(#)calendar.c	8.3 (Berkeley) 3/25/94";
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
+#include <sys/types.h>
 #include <err.h>
 #include <errno.h>
 #include <locale.h>
+#include <login_cap.h>
 #include <langinfo.h>
 #include <pwd.h>
 #include <stdio.h>
@@ -68,7 +70,7 @@ static time_t	f_time = 0;
 double		UTCOffset = UTCOFFSET_NOTSET;
 int		EastLongitude = LONGITUDE_NOTSET;
 #ifdef WITH_ICONV
-const char	*outputEncoding;
+const char	*outputEncoding = NULL;
 #endif
 
 static void	usage(void) __dead2;
@@ -84,12 +86,6 @@ main(int argc, char *argv[])
 	struct tm tp1, tp2;
 
 	(void)setlocale(LC_ALL, "");
-#ifdef WITH_ICONV
-	/* save the information about the encoding used in the terminal */
-	outputEncoding = strdup(nl_langinfo(CODESET));
-	if (outputEncoding == NULL)
-		errx(1, "cannot allocate memory");
-#endif
 
 	while ((ch = getopt(argc, argv, "-A:aB:D:dF:f:l:t:U:W:?")) != -1)
 		switch (ch) {
@@ -218,15 +214,33 @@ main(int argc, char *argv[])
 
 	if (doall)
 		while ((pw = getpwent()) != NULL) {
-			(void)setegid(pw->pw_gid);
-			(void)initgroups(pw->pw_name, pw->pw_gid);
-			(void)seteuid(pw->pw_uid);
-			if (!chdir(pw->pw_dir))
+			pid_t pid;
+
+			if (chdir(pw->pw_dir) == -1)
+				continue;
+			pid = fork();
+			if (pid < 0)
+				err(1, "fork");
+			if (pid == 0) {
+				login_cap_t *lc;
+
+				lc = login_getpwclass(pw);
+				if (setusercontext(lc, pw, pw->pw_uid,
+				    LOGIN_SETALL) != 0)
+					errx(1, "setusercontext");
 				cal();
-			(void)seteuid(0);
+				exit(0);
+			}
 		}
-	else
+	else {
+#ifdef WITH_ICONV
+		/* Save the information about the encoding used in the terminal. */
+		outputEncoding = strdup(nl_langinfo(CODESET));
+		if (outputEncoding == NULL)
+			errx(1, "cannot allocate memory");
+#endif
 		cal();
+	}
 	exit(0);
 }
 
