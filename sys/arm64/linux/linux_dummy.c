673399d81f41e3fe89b757e46a3bfc5be766d8b9
emaste <emaste@FreeBSD.org>
2019-09-11 17:56:48 +0000

    Update comments and ordering in linux*_dummy.c
    - sort alphabetically
    - getcpu arrived in Linux 2.6.19
    - fanotify_* arrived in 2.6.36

--- sys/arm64/linux/linux_dummy.c
+++ sys/arm64/linux/linux_dummy.c
@@ -101,17 +101,19 @@ DUMMY(migrate_pages);
 DUMMY(unshare);
 /* Linux 2.6.17: */
 DUMMY(splice);
-DUMMY(tee);
 DUMMY(sync_file_range);
+DUMMY(tee);
 DUMMY(vmsplice);
 /* Linux 2.6.18: */
 DUMMY(move_pages);
+/* Linux 2.6.19: */
+DUMMY(getcpu);
 /* Linux 2.6.27: */
 DUMMY(signalfd4);
 DUMMY(inotify_init1);
 /* Linux 2.6.31: */
 DUMMY(perf_event_open);
-/* Linux 2.6.38: */
+/* Linux 2.6.36: */
 DUMMY(fanotify_init);
 DUMMY(fanotify_mark);
 /* Linux 2.6.39: */
@@ -120,7 +122,6 @@ DUMMY(open_by_handle_at);
 DUMMY(clock_adjtime);
 /* Linux 3.0: */
 DUMMY(setns);
-DUMMY(getcpu);
 /* Linux 3.2: */
 DUMMY(process_vm_readv);
 DUMMY(process_vm_writev);
