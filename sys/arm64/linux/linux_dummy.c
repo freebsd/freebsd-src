1515fe49f22918cae20cc5431b7d893b3136f886
Ed Maste <emaste@FreeBSD.org>
2019-09-11 17:04:13 +0000

    linuxulator: seccomp syscall first appeared in Linux 3.17
    Reference: http://man7.org/linux/man-pages/man2/seccomp.2.html

--- sys/arm64/linux/linux_dummy.c
+++ sys/arm64/linux/linux_dummy.c
@@ -131,8 +131,9 @@ DUMMY(finit_module);
 DUMMY(sched_setattr);
 DUMMY(sched_getattr);
 /* Linux 3.15: */
-DUMMY(seccomp);
 DUMMY(memfd_create);
+/* Linux 3.17: */
+DUMMY(seccomp);
 /* Linux 3.18: */
 DUMMY(bpf);
 /* Linux 3.19: */
