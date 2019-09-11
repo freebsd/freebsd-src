379dc28e9fab2fe81ffe09a89b122ff6f041420d
emaste <emaste@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-09-11 17:05:49 +0000

    linuxulator: memfd_create first appeared in Linux 3.17
    Reference: http://man7.org/linux/man-pages/man2/memfd_create.2.html
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@352222 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/amd64/linux32/linux32_dummy.c
+++ sys/amd64/linux32/linux32_dummy.c
@@ -137,9 +137,8 @@ DUMMY(kcmp);
 DUMMY(finit_module);
 DUMMY(sched_setattr);
 DUMMY(sched_getattr);
-/* Linux 3.15: */
-DUMMY(memfd_create);
 /* Linux 3.17: */
+DUMMY(memfd_create);
 DUMMY(seccomp);
 /* Linux 3.18: */
 DUMMY(bpf);
