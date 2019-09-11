130f730aba980554d6b17f88f54da1033de62a01
yuripv <yuripv@FreeBSD.org>
2019-09-11 15:39:28 +0000

    locale: more output fixes
    - make abday, day, abmon, mon, am_pm output quoting match linux
    - workaround localeconv() issue for mon_grouping and grouping (PR172215)
    - for other values not available in default locale, output -1 instead of
      127 (CHAR_MAX) as returned by localeconv()
    
    With these changes, output of `locale` and `locale -k` for all keywords
    specified by POSIX exactly matches the linux one.
    
    PR:		237752
    Reviewed by:	bapt
    Differential Revision:	https://reviews.freebsd.org/D21599

--- usr.bin/locale/tests/locale_test.sh
+++ usr.bin/locale/tests/locale_test.sh
@@ -29,7 +29,8 @@
 atf_test_case k_flag_posix
 k_flag_posix_head()
 {
-	atf_set "descr" "Verify -k handles all POSIX specified keywords"
+	atf_set "descr" \
+	    "Verify output of 'locale -k' for all POSIX specified keywords"
 }
 k_flag_posix_body()
 {
@@ -92,7 +93,75 @@ k_flag_posix_body()
 	    noexpr
 }
 
+atf_test_case no_flags_posix
+no_flags_posix_head()
+{
+	atf_set "descr" \
+	    "Verify output of 'locale' for all POSIX specified keywords"
+}
+no_flags_posix_body()
+{
+	export LC_ALL="C"
+
+	# LC_MONETARY
+	atf_check -o file:"$(atf_get_srcdir)/no_flags_posix_monetary.out" \
+	    locale \
+	    int_curr_symbol \
+	    currency_symbol \
+	    mon_decimal_point \
+	    mon_thousands_sep \
+	    mon_grouping \
+	    positive_sign \
+	    negative_sign \
+	    int_frac_digits \
+	    frac_digits \
+	    p_cs_precedes \
+	    p_sep_by_space \
+	    n_cs_precedes \
+	    n_sep_by_space \
+	    p_sign_posn \
+	    n_sign_posn \
+	    int_p_cs_precedes \
+	    int_n_cs_precedes \
+	    int_p_sep_by_space \
+	    int_n_sep_by_space \
+	    int_p_sign_posn \
+	    int_n_sign_posn
+
+	# LC_NUMERIC
+	atf_check -o file:"$(atf_get_srcdir)/no_flags_posix_numeric.out" \
+	    locale \
+	    decimal_point \
+	    thousands_sep \
+	    grouping
+
+	# LC_TIME
+	atf_check -o file:"$(atf_get_srcdir)/no_flags_posix_time.out" \
+	    locale \
+	    abday \
+	    day \
+	    abmon \
+	    mon \
+	    d_t_fmt \
+	    d_fmt \
+	    t_fmt \
+	    am_pm \
+	    t_fmt_ampm \
+	    era \
+	    era_d_fmt \
+	    era_t_fmt \
+	    era_d_t_fmt \
+	    alt_digits
+
+	# LC_MESSAGES
+	atf_check -o file:"$(atf_get_srcdir)/no_flags_posix_messages.out" \
+	    locale \
+	    yesexpr \
+	    noexpr
+}
+
 atf_init_test_cases()
 {
 	atf_add_test_case k_flag_posix
+	atf_add_test_case no_flags_posix
 }
