b4224aa3d8a366a1c3b6ccd8e0c4ed1549a18aa5
Johannes Lundberg <johalun@FreeBSD.org>
2019-09-12 20:15:04 +0000

    MFC r345100: Implement task_euid() and get_task_state() function macros in the LinuxKPI.
    Submitted by:		Johannes Lundberg <johalun0@gmail.com>
    Sponsored by:		Limelight Networks
    Sponsored by:		Mellanox Technologies

--- sys/compat/linuxkpi/common/include/linux/sched.h
+++ sys/compat/linuxkpi/common/include/linux/sched.h
@@ -96,7 +96,9 @@ struct task_struct {
 #define	get_pid(x)		(x)
 #define	put_pid(x)		do { } while (0)
 #define	current_euid()	(curthread->td_ucred->cr_uid)
+#define	task_euid(task)	((task)->task_thread->td_ucred->cr_uid)
 
+#define	get_task_state(task)		atomic_read(&(task)->state)
 #define	set_task_state(task, x)		atomic_set(&(task)->state, (x))
 #define	__set_task_state(task, x)	((task)->state.counter = (x))
 #define	set_current_state(x)		set_task_state(current, x)
