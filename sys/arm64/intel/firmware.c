7ebcf07f42679d3df511f9d1952e4ba3450c58e6
br <br@FreeBSD.org>
2019-09-13 16:50:57 +0000

    Add support for Intel Stratix 10 platform.
    Intel Stratix 10 SoC includes a quad-core arm64 cluster and FPGA fabric.
    
    This adds support for reconfiguring FPGA.
    
    Accessing FPGA core of this SoC require the level of privilege EL3,
    while kernel runs in EL1 (lower) level of privilege.
    
    This provides an Intel service layer interface that uses SMCCC to pass
    queries to the secure-monitor (EL3).
    
    Sponsored by:	DARPA, AFRL
    Differential Revision:	https://reviews.freebsd.org/D21454

---
+++ sys/arm64/intel/firmware.c
@@ -0,0 +1,122 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2019 Ruslan Bukin <br@bsdpad.com>
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory (Department of Computer Science and
+ * Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"), as part of the
+ * DARPA SSITH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/kernel.h>
+#include <sys/module.h>
+
+#include <dev/fdt/simplebus.h>
+#include <dev/fdt/fdt_common.h>
+#include <dev/ofw/ofw_bus_subr.h>
+
+struct firmware_softc {
+	struct simplebus_softc	simplebus_sc;
+	device_t		dev;
+};
+
+static int
+firmware_probe(device_t dev)
+{
+	phandle_t node;
+
+	node = ofw_bus_get_node(dev);
+
+	/*
+	 * The firmware node has no property compatible.
+	 * Look for a known child.
+	 */
+	if (!fdt_depth_search_compatible(node, "intel,stratix10-svc", 0))
+		return (ENXIO);
+
+	if (!ofw_bus_status_okay(dev))
+		return (ENXIO);
+
+	device_set_desc(dev, "Firmware node");
+
+	return (BUS_PROBE_DEFAULT);
+}
+
+static int
+firmware_attach(device_t dev)
+{
+	struct firmware_softc *sc;
+	phandle_t node;
+
+	sc = device_get_softc(dev);
+	sc->dev = dev;
+
+	node = ofw_bus_get_node(dev);
+	if (node == -1)
+		return (ENXIO);
+
+	simplebus_init(dev, node);
+
+	/*
+	 * Allow devices to identify.
+	 */
+	bus_generic_probe(dev);
+
+	/*
+	 * Now walk the OFW tree and attach top-level devices.
+	 */
+	for (node = OF_child(node); node > 0; node = OF_peer(node))
+		simplebus_add_device(dev, node, 0, NULL, -1, NULL);
+
+	return (bus_generic_attach(dev));
+}
+
+static int
+firmware_detach(device_t dev)
+{
+
+	return (0);
+}
+
+static device_method_t firmware_methods[] = {
+	DEVMETHOD(device_probe,		firmware_probe),
+	DEVMETHOD(device_attach,	firmware_attach),
+	DEVMETHOD(device_detach,	firmware_detach),
+	DEVMETHOD_END
+};
+
+DEFINE_CLASS_1(firmware, firmware_driver, firmware_methods,
+    sizeof(struct firmware_softc), simplebus_driver);
+
+static devclass_t firmware_devclass;
+
+EARLY_DRIVER_MODULE(firmware, simplebus, firmware_driver, firmware_devclass,
+    0, 0, BUS_PASS_INTERRUPT + BUS_PASS_ORDER_MIDDLE);
+MODULE_VERSION(firmware, 1);
