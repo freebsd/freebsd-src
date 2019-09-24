fa1ab856af4190ff4161eaaffca8b3f36b94e372
olivier <olivier@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-09-24 16:49:42 +0000

    Fix a minor typo
    Approved by:	lwhsu
    MFC after:	1 month
    Sponsored by:	Netflix
    Differential Revision:	https://reviews.freebsd.org/D19970
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@352654 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/pccard/pccard.c
+++ sys/dev/pccard/pccard.c
@@ -266,7 +266,7 @@ pccard_probe_and_attach_child(device_t dev, device_t child,
 	 * In NetBSD, the drivers are responsible for activating each
 	 * function of a card and selecting the config to use.  In
 	 * FreeBSD, all that's done automatically in the typical lazy
-	 * way we do device resoruce allocation (except we pick the
+	 * way we do device resource allocation (except we pick the
 	 * cfe up front).  This is the biggest depature from the
 	 * inherited NetBSD model, apart from the FreeBSD resource code.
 	 *
