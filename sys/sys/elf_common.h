ab725e1e1d78c44b1311f5a1636fbe16f8e85dc5
Ed Maste <emaste@FreeBSD.org>
2019-09-26 16:39:33 +0000

    MFC r352551: elf_common: add ELF note names
    r348628 added a definition of NT_GNU_BUILD_ID.  Some software (Valgrind)
    also expects a #define for the note name (ELF_NOTE_GNU) in the case that
    NT_GNU_BUILD_ID is defined.
    
    PR:		239669
    Sponsored by:	The FreeBSD Foundation

--- sys/sys/elf_common.h
+++ sys/sys/elf_common.h
@@ -764,6 +764,12 @@ typedef struct {
 #define	LL_DELAY_LOAD		0x10
 #define	LL_DELTA		0x20
 
+/* Note section names */
+#define	ELF_NOTE_FREEBSD	"FreeBSD"
+#define	ELF_NOTE_NETBSD		"NetBSD"
+#define	ELF_NOTE_SOLARIS	"SUNW Solaris"
+#define	ELF_NOTE_GNU		"GNU"
+
 /* Values for n_type used in executables. */
 #define	NT_FREEBSD_ABI_TAG	1
 #define	NT_FREEBSD_NOINIT_TAG	2
