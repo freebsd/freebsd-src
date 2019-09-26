ad0f86123b0962aa00396c860bc49dbb37ea876f
Michael Gmelin <grembo@FreeBSD.org>
2019-09-26 16:33:20 +0000

    MFC r352513, r352514, r352662:
    As suggested by koobs, MFC recent freebsd-update changes to
    have a consistent user experience on all supported
    versions.
    
    r352513:
    freebsd-update.8: appease igor
    
    igor follows American style guides in the belief that abbreviations i.e.
    and e.g. are always followed by a comma.  Make that change now so that
    future updates to freebsd-update.8 do not complain about this.
    
    r352514:
    freebsd-update: make usage output consistent
    
    Drop trailing . which appeared only on description of IDS.
    
    r352662:
    freebsd-update: Add `updatesready' and `showconfig' commands
    
    `freebsd-update updatesready' can be used to check if there are any pending
    fetched updates that can be installed.
    
    `freebsd-update showconfig' writes freebsd-update's configuration to
    stdout.
    
    This also changes the exit code of `freebsd-update install' to 2 in case
    there are no updates pending to be installed and there wasn't a fetch phase
    in the same invocation. This allows scripts to tell apart these error
    conditions without breaking existing jail managers.
    
    PR:		240757, 240177, 229346
    Reviewed by:	manpages (bcr), secteam (emaste), yuripv
    Differential Revision:	https://reviews.freebsd.org/D21473

--- usr.sbin/freebsd-update/freebsd-update.sh
+++ usr.sbin/freebsd-update/freebsd-update.sh
@@ -62,9 +62,11 @@ Commands:
   cron         -- Sleep rand(3600) seconds, fetch updates, and send an
                   email if updates were found
   upgrade      -- Fetch upgrades to FreeBSD version specified via -r option
+  updatesready -- Check if there are fetched updates ready to install
   install      -- Install downloaded updates or upgrades
   rollback     -- Uninstall most recently installed updates
-  IDS          -- Compare the system against an index of "known good" files.
+  IDS          -- Compare the system against an index of "known good" files
+  showconfig   -- Show configuration
 EOF
 	exit 0
 }
@@ -503,7 +505,8 @@ parse_cmdline () {
 			;;
 
 		# Commands
-		cron | fetch | upgrade | install | rollback | IDS)
+		cron | fetch | upgrade | updatesready | install | rollback |\
+		IDS | showconfig)
 			COMMANDS="${COMMANDS} $1"
 			;;
 
@@ -827,7 +830,7 @@ install_check_params () {
 		echo "No updates are available to install."
 		if [ $ISFETCHED -eq 0 ]; then
 			echo "Run '$0 fetch' first."
-			exit 1
+			exit 2
 		fi
 		exit 0
 	fi
@@ -3333,6 +3336,21 @@ cmd_upgrade () {
 	upgrade_run || exit 1
 }
 
+# Check if there are fetched updates ready to install
+cmd_updatesready () {
+	# Construct a unique name from ${BASEDIR}
+	BDHASH=`echo ${BASEDIR} | sha256 -q`
+
+	# Check that we have updates ready to install
+	if ! [ -L ${BDHASH}-install ]; then
+		echo "No updates are available to install."
+		exit 2
+	fi
+
+	echo "There are updates available to install."
+	echo "Run '$0 install' to proceed."
+}
+
 # Install downloaded updates.
 cmd_install () {
 	install_check_params
@@ -3349,6 +3367,13 @@ cmd_rollback () {
 cmd_IDS () {
 	IDS_check_params
 	IDS_run || exit 1
+}
+
+# Output configuration.
+cmd_showconfig () {
+	for X in ${CONFIGOPTIONS}; do
+		echo $X=$(eval echo \$${X})
+	done
 }
 
 #### Entry point
