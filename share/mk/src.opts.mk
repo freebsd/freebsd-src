ec01b9a21c99c0c9483d9a96314af7c0e1168db2
dim <dim@FreeBSD.org>
2019-09-26 18:24:04 +0000

    Enable OpenMP for powerpc64
    Summary: When powerpc64 switches to LLVM, use this patch to enable
    OpenMP as well. OpenMP on PPC is only for 64-bits, so don't make a
    32-bit libomp. A change to openmp files is necesssary (under review on
    https://reviews.llvm.org/D67190), because it determines ELF format
    version based on endianness, which is incorrect.
    
    Reviewed by:	alfredo.junior_eldorado.org.br, #manpages
    Differential Revision: https://reviews.freebsd.org/D21532

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -397,7 +397,8 @@ BROKEN_OPTIONS+=HYPERV
 .endif
 
 # NVME is only aarch64, x86 and powerpc64
-.if ${__T} != "aarch64" && ${__T} != "amd64" && ${__T} != "i386" && ${__T} != "powerpc64"
+.if ${__T} != "aarch64" && ${__T} != "amd64" && ${__T} != "i386" && \
+    ${__T} != "powerpc64"
 BROKEN_OPTIONS+=NVME
 .endif
 
@@ -406,7 +407,8 @@ BROKEN_OPTIONS+=NVME
 BROKEN_OPTIONS+=BSD_CRTBEGIN
 .endif
 
-.if ${COMPILER_FEATURES:Mc++11} && (${__T} == "amd64" || ${__T} == "i386")
+.if ${COMPILER_FEATURES:Mc++11} && \
+    (${__T} == "amd64" || ${__T} == "i386" || ${__T} == "powerpc64")
 __DEFAULT_YES_OPTIONS+=OPENMP
 .else
 __DEFAULT_NO_OPTIONS+=OPENMP
