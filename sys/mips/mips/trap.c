f2f9a3535023780159333ffe79c0832fdb7dc52d
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-09-27 18:43:36 +0000

    Improve MD page fault handlers.
    Centralize calculation of signal and ucode delivered on unhandled page
    fault in new function vm_fault_trap().  MD trap_pfault() now almost
    always uses the signal numbers and error codes calculated in
    consistent MI way.
    
    This introduces the protection fault compatibility sysctls to all
    non-x86 architectures which did not have that bug, but apparently they
    were already much more wrong in selecting delivered signals on
    protection violations.
    
    Change the delivered signal for accesses to mapped area after the
    backing object was truncated.  According to POSIX description for
    mmap(2):
       The system shall always zero-fill any partial page at the end of an
       object. Further, the system shall never write out any modified
       portions of the last page of an object which are beyond its
       end. References within the address range starting at pa and
       continuing for len bytes to whole pages following the end of an
       object shall result in delivery of a SIGBUS signal.
    
       An implementation may generate SIGBUS signals when a reference
       would cause an error in the mapped object, such as out-of-space
       condition.
    Adjust according to the description, keeping the existing
    compatibility code for SIGSEGV/SIGBUS on protection failures.
    
    For situations where kernel cannot handle page fault due to resource
    limit enforcement, SIGBUS with a new error code BUS_OBJERR is
    delivered.  Also, provide a new error code SEGV_PKUERR for SIGSEGV on
    amd64 due to protection key access violation.
    
    vm_fault_hold() is renamed to vm_fault().  Fixed some nits in
    trap_pfault()s like mis-interpreting Mach errors as errnos.  Removed
    unneeded truncations of the fault addresses reported by hardware.
    
    PR:	211924
    Reviewed by:	alc
    Discussed with:	jilles, markj
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D21566
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@352807 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/mips/mips/trap.c
+++ sys/mips/mips/trap.c
@@ -669,8 +669,9 @@ trap(struct trapframe *trapframe)
 			int rv;
 
 	kernel_fault:
-			va = trunc_page((vm_offset_t)trapframe->badvaddr);
-			rv = vm_fault(kernel_map, va, ftype, VM_FAULT_NORMAL);
+			va = (vm_offset_t)trapframe->badvaddr;
+			rv = vm_fault_trap(kernel_map, va, ftype,
+			    VM_FAULT_NORMAL, NULL, NULL);
 			if (rv == KERN_SUCCESS)
 				return (trapframe->pc);
 			if (td->td_pcb->pcb_onfault != NULL) {
@@ -705,7 +706,7 @@ dofault:
 
 			vm = p->p_vmspace;
 			map = &vm->vm_map;
-			va = trunc_page((vm_offset_t)trapframe->badvaddr);
+			va = (vm_offset_t)trapframe->badvaddr;
 			if (KERNLAND(trapframe->badvaddr)) {
 				/*
 				 * Don't allow user-mode faults in kernel
@@ -714,7 +715,8 @@ dofault:
 				goto nogo;
 			}
 
-			rv = vm_fault(map, va, ftype, VM_FAULT_NORMAL);
+			rv = vm_fault_trap(map, va, ftype, VM_FAULT_NORMAL,
+			    &i, &ucode);
 			/*
 			 * XXXDTRACE: add dtrace_doubletrap_func here?
 			 */
@@ -739,11 +741,6 @@ dofault:
 				}
 				goto err;
 			}
-			i = SIGSEGV;
-			if (rv == KERN_PROTECTION_FAILURE)
-				ucode = SEGV_ACCERR;
-			else
-				ucode = SEGV_MAPERR;
 			addr = trapframe->pc;
 
 			msg = "BAD_PAGE_FAULT";
