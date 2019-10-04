cbd974b4b9f7a260e9241d8a6d08d40ff9ef2a26
Conrad Meyer <cem@FreeBSD.org>
2019-10-04 18:38:47 +0000

    nvdimm(4): Add nvdimm_e820 pseudo-bus
    nvdimm_e820 is a newbus pseudo driver that looks for "legacy" e820 PRAM
    spans and creates ordinary-looking SPA devfs nodes for them
    (/dev/nvdimm_spaN).
    
    As these legacy regions lack real NFIT SPA regions and namespace
    definitions, they must be administratively sliced up externally using
    device.hints.  This is similar in purpose to the Linux memmap= mechanism.
    
    It is assumed that systems with working NFIT tables will not have any use
    for this driver, and that that will be the prevailing style going forward,
    so if there are no explicit hints provided, this driver does not
    automatically create any devices.
    
    Reviewed by:	kib (previous version)
    Sponsored by:	Dell EMC Isilon
    Differential Revision:	https://reviews.freebsd.org/D21885

--- sys/dev/nvdimm/nvdimm_var.h
+++ sys/dev/nvdimm/nvdimm_var.h
@@ -163,6 +163,7 @@ void acpi_nfit_get_control_region(ACPI_TABLE_NFIT *nfitbl,
     uint16_t control_region_index, ACPI_NFIT_CONTROL_REGION **out);
 void acpi_nfit_get_flush_addrs(ACPI_TABLE_NFIT *nfitbl, nfit_handle_t dimm,
     uint64_t ***listp, int *countp);
+enum SPA_mapping_type nvdimm_spa_type_from_name(const char *);
 enum SPA_mapping_type nvdimm_spa_type_from_uuid(struct uuid *);
 struct nvdimm_dev *nvdimm_find_by_handle(nfit_handle_t nv_handle);
 int nvdimm_spa_init(struct SPA_mapping *spa, ACPI_NFIT_SYSTEM_ADDRESS *nfitaddr,
