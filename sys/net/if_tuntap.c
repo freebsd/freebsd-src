d8ae7b30108a19f4d5307ac8a7d471edd88fb1bc
glebius <glebius@FreeBSD.org>
2019-10-10 23:51:14 +0000

    Don't use if_maddr_rlock() in tuntap(4), use epoch(9) directly instead.

--- sys/net/if_tuntap.c
+++ sys/net/if_tuntap.c
@@ -1151,6 +1151,7 @@ tuninit(struct ifnet *ifp)
 {
 	struct tuntap_softc *tp = ifp->if_softc;
 #ifdef INET
+	struct epoch_tracker et;
 	struct ifaddr *ifa;
 #endif
 
@@ -1162,7 +1163,7 @@ tuninit(struct ifnet *ifp)
 		ifp->if_flags |= IFF_UP;
 		getmicrotime(&ifp->if_lastchange);
 #ifdef INET
-		if_addr_rlock(ifp);
+		NET_EPOCH_ENTER(et);
 		CK_STAILQ_FOREACH(ifa, &ifp->if_addrhead, ifa_link) {
 			if (ifa->ifa_addr->sa_family == AF_INET) {
 				struct sockaddr_in *si;
@@ -1176,7 +1177,7 @@ tuninit(struct ifnet *ifp)
 					tp->tun_flags |= TUN_DSTADDR;
 			}
 		}
-		if_addr_runlock(ifp);
+		NET_EPOCH_EXIT(et);
 #endif
 		TUN_UNLOCK(tp);
 	} else {
