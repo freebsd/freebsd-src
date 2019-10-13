98c105f52941e30d83192a71467b76e36bd730b3
mjg <mjg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-10-13 15:42:04 +0000

    nullfs: use MNTK_NOMSYNC
    Reviewed by:	kib
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D22009
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@353472 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/fs/nullfs/null_vfsops.c
+++ sys/fs/nullfs/null_vfsops.c
@@ -205,7 +205,7 @@ nullfs_mount(struct mount *mp)
 		    (MNTK_SHARED_WRITES | MNTK_LOOKUP_SHARED |
 		    MNTK_EXTENDED_SHARED);
 	}
-	mp->mnt_kern_flag |= MNTK_LOOKUP_EXCL_DOTDOT;
+	mp->mnt_kern_flag |= MNTK_LOOKUP_EXCL_DOTDOT | MNTK_NOMSYNC;
 	mp->mnt_kern_flag |= lowerrootvp->v_mount->mnt_kern_flag &
 	    (MNTK_USES_BCACHE | MNTK_NO_IOPF | MNTK_UNMAPPED_BUFS);
 	MNT_IUNLOCK(mp);
