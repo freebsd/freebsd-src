8da1c09853811f6ce71ec3814a6082e43b202c2d
Jeff Roberson <jeff@FreeBSD.org>
2019-10-15 03:38:02 +0000

    (2/6) Don't release xbusy in vm_page_remove(), defer to vm_page_free_prep().
    This persists busy state across operations like rename and replace.
    
    Reviewed by:    kib, markj
    Tested by:      pho
    Sponsored by:   Netflix, Intel
    Differential Revision:  https://reviews.freebsd.org/D21549

--- sys/vm/vm_page.c
+++ sys/vm/vm_page.c
@@ -1227,6 +1227,8 @@ vm_page_putfake(vm_page_t m)
 	KASSERT((m->oflags & VPO_UNMANAGED) != 0, ("managed %p", m));
 	KASSERT((m->flags & PG_FICTITIOUS) != 0,
 	    ("vm_page_putfake: bad page %p", m));
+	if (vm_page_xbusied(m))
+		vm_page_xunbusy(m);
 	uma_zfree(fakepg_zone, m);
 }
 
@@ -1519,8 +1521,7 @@ vm_page_object_remove(vm_page_t m)
 	VM_OBJECT_ASSERT_WLOCKED(object);
 	KASSERT((m->ref_count & VPRC_OBJREF) != 0,
 	    ("page %p is missing its object ref", m));
-	if (vm_page_xbusied(m))
-		vm_page_xunbusy(m);
+
 	mrem = vm_radix_remove(&object->rtree, m->pindex);
 	KASSERT(mrem == m, ("removed page %p, expected page %p", mrem, m));
 
@@ -3590,7 +3591,7 @@ vm_page_free_prep(vm_page_t m)
 	VM_CNT_INC(v_tfree);
 
 	if (vm_page_sbusied(m))
-		panic("vm_page_free_prep: freeing busy page %p", m);
+		panic("vm_page_free_prep: freeing shared busy page %p", m);
 
 	if (m->object != NULL) {
 		vm_page_object_remove(m);
@@ -3606,6 +3607,9 @@ vm_page_free_prep(vm_page_t m)
 		m->object = NULL;
 		m->ref_count -= VPRC_OBJREF;
 	}
+
+	if (vm_page_xbusied(m))
+		vm_page_xunbusy(m);
 
 	/*
 	 * If fictitious remove object association and
