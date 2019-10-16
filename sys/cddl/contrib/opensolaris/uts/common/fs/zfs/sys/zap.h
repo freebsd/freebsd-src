41ecc3a3e048507f8c5204e0aa36b36fdfe851d0
avg <avg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-10-16 07:09:00 +0000

    MFV r353619: 9691 fat zap should prefetch when iterating
    illumos/illumos-gate@52abb70e073c2a88808c0d66fd810ba8c5080572
    https://github.com/illumos/illumos-gate/commit/52abb70e073c2a88808c0d66fd810ba8c5080572
    
    https://www.illumos.org/issues/9691
      When iterating over a ZAP object, we're almost always certain to
      iterate over the entire object. If there are multiple leaf blocks, we
      can realize a performance win by issuing reads for all the leaf blocks
      in parallel when the iteration begins.
      For example, if we have 10,000 snapshots, "zfs destroy -nv
      pool/fs@1%9999" can take 30 minutes when the cache is cold. This
      change provides a >3x performance improvement, by issuing the reads
      for all ~64 blocks of each ZAP object in parallel.
    
    Author: Matthew Ahrens <mahrens@delphix.com>
    Obtained from:	illumos
    MFC after:	2 weeks
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@353621 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/zap.h
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/zap.h
@@ -21,7 +21,7 @@
 
 /*
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2012, 2016 by Delphix. All rights reserved.
+ * Copyright (c) 2012, 2018 by Delphix. All rights reserved.
  * Copyright 2017 Nexenta Systems, Inc.
  */
 
@@ -349,6 +349,7 @@ typedef struct zap_cursor {
 	uint64_t zc_serialized;
 	uint64_t zc_hash;
 	uint32_t zc_cd;
+	boolean_t zc_prefetch;
 } zap_cursor_t;
 
 typedef struct {
@@ -375,6 +376,8 @@ typedef struct {
  * zapobj.  You must _fini the cursor when you are done with it.
  */
 void zap_cursor_init(zap_cursor_t *zc, objset_t *ds, uint64_t zapobj);
+void zap_cursor_init_noprefetch(zap_cursor_t *zc, objset_t *os,
+    uint64_t zapobj);
 void zap_cursor_fini(zap_cursor_t *zc);
 
 /*
