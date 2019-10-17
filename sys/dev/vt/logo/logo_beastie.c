b1460baa73475846db037d0375510599993e1d2b
Ed Maste <emaste@FreeBSD.org>
2019-10-17 16:38:44 +0000

    Update Conrad Meyer's email
    cem is now a committer
    
    Approved by:	cem

--- sys/dev/vt/logo/logo_beastie.c
+++ sys/dev/vt/logo/logo_beastie.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2015 Conrad Meyer <cse.cem@gmail.com>
+ * Copyright (c) 2015 Conrad Meyer <cem@FreeBSD.org>
  * Copyright (c) 2005 The FreeBSD Foundation
  * Copyright (c) 1996 Larry Ewing <lewing@isc.tamu.edu>
  * Copyright (c) 1988 Kirk McKusick <mckusick@FreeBSD.org>
