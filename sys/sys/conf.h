c84ddf2007239e6863938ed9afdb37c603368e69
Kyle Evans <kevans@FreeBSD.org>
2019-10-20 22:01:35 +0000

    MFC r353128-r353129: fully initialize cloned devices w/ make_dev_args
    r353128:
    kern_conf: fully initialize cloned devices with make_dev_args, too
    
    Attempting to initialize si_drv{1,2} with mda_si_drv{1,2} does not work if
    you are operating on cloned devices.
    
    clone_create must be called prior to the make_dev* family to create/return
    the device on the clonelist as needed. This device is later returned early
    in newdev(), prior to si_drv{0,1,2} initialization.
    
    This patch simply breaks out of the loop if we've found a device and
    finishes init.
    
    r353129:
    Remove the remnants of SI_CHEAPCLONE
    
    SI_CHEAPCLONE was introduced in r66067 for use with cloned bpfs. It was
    later also used in tty, tun, tap at points. The rough timeline for being
    removed in each of these is as follows:
    
    - r181690: bpf switched to use cdevpriv API by ed@
    - r181905: ed@ rewrote the TTY later to be mpsafe
    - r204464: kib@ removes it from tun/tap, declaring it unused
    
    I've not yet been able to dig up any other consumers in the intervening 9
    years. It is no longer set on any devices in the tree and leaves an
    interesting situation in make_dev_sv where we're ok with the device already
    being set SI_NAMED.

--- sys/sys/conf.h
+++ sys/sys/conf.h
@@ -59,7 +59,7 @@ struct cdev {
 #define	SI_ETERNAL	0x0001	/* never destroyed */
 #define	SI_ALIAS	0x0002	/* carrier of alias name */
 #define	SI_NAMED	0x0004	/* make_dev{_alias} has been called */
-#define	SI_CHEAPCLONE	0x0008	/* can be removed_dev'ed when vnode reclaims */
+#define	SI_UNUSED1	0x0008	/* unused */
 #define	SI_CHILD	0x0010	/* child of another struct cdev **/
 #define	SI_DUMPDEV	0x0080	/* is kernel dumpdev */
 #define	SI_CLONELIST	0x0200	/* on a clone list */
