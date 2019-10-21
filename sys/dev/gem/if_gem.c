76184490cc0f4654125be2b839222a5d1a1e1f5d
glebius <glebius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-10-21 18:06:57 +0000

    Convert to if_foreach_llmaddr() KPI.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@353815 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/gem/if_gem.c
+++ sys/dev/gem/if_gem.c
@@ -2202,14 +2202,27 @@ gem_ioctl(struct ifnet *ifp, u_long cmd, caddr_t data)
 	return (error);
 }
 
+static u_int
+gem_hash_maddr(void *arg, struct sockaddr_dl *sdl, u_int cnt)
+{
+	uint32_t crc, *hash = arg;
+
+	crc = ether_crc32_le(LLADDR(sdl), ETHER_ADDR_LEN);
+	/* We just want the 8 most significant bits. */
+	crc >>= 24;
+	/* Set the corresponding bit in the filter. */
+	hash[crc >> 4] |= 1 << (15 - (crc & 15));
+
+	return (1);
+}
+
 static void
 gem_setladrf(struct gem_softc *sc)
 {
 	struct ifnet *ifp = sc->sc_ifp;
-	struct ifmultiaddr *inm;
 	int i;
 	uint32_t hash[16];
-	uint32_t crc, v;
+	uint32_t v;
 
 	GEM_LOCK_ASSERT(sc, MA_OWNED);
 
@@ -2245,23 +2258,8 @@ gem_setladrf(struct gem_softc *sc)
 	 * is the MSB).
 	 */
 
-	/* Clear the hash table. */
 	memset(hash, 0, sizeof(hash));
-
-	if_maddr_rlock(ifp);
-	CK_STAILQ_FOREACH(inm, &ifp->if_multiaddrs, ifma_link) {
-		if (inm->ifma_addr->sa_family != AF_LINK)
-			continue;
-		crc = ether_crc32_le(LLADDR((struct sockaddr_dl *)
-		    inm->ifma_addr), ETHER_ADDR_LEN);
-
-		/* We just want the 8 most significant bits. */
-		crc >>= 24;
-
-		/* Set the corresponding bit in the filter. */
-		hash[crc >> 4] |= 1 << (15 - (crc & 15));
-	}
-	if_maddr_runlock(ifp);
+	if_foreach_llmaddr(ifp, gem_hash_maddr, hash);
 
 	v |= GEM_MAC_RX_HASH_FILTER;
 
