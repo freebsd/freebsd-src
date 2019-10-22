866a7f286fb641350b359c5242d97e415dfcf6b2
John Baldwin <jhb@FreeBSD.org>
2019-10-22 20:01:47 +0000

    Always allocate the atid table during attach.
    Previously the table was allocated on first use by TOE and the
    ratelimit code.  The forthcoming NIC KTLS code also uses this table.
    Allocate it unconditionally during attach to simplify consumers.
    
    Reviewed by:	np
    Differential Revision:	https://reviews.freebsd.org/D22028

--- sys/dev/cxgbe/tom/t4_tom.c
+++ sys/dev/cxgbe/tom/t4_tom.c
@@ -1368,7 +1368,6 @@ free_tid_tabs(struct tid_info *t)
 {
 
 	free_tid_tab(t);
-	free_atid_tab(t);
 	free_stid_tab(t);
 }
 
@@ -1378,10 +1377,6 @@ alloc_tid_tabs(struct tid_info *t)
 	int rc;
 
 	rc = alloc_tid_tab(t, M_NOWAIT);
-	if (rc != 0)
-		goto failed;
-
-	rc = alloc_atid_tab(t, M_NOWAIT);
 	if (rc != 0)
 		goto failed;
 
