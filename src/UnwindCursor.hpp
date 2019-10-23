067b0ac8a70ff539e3dbbe2be60c3ac441c8305b
Dimitry Andric <dim@FreeBSD.org>
2019-10-23 17:52:37 +0000

    Vendor import of stripped LLVM libunwind trunk r375505, the last commit before the upstream Subversion repository was made read-only, and the LLVM project migrated to GitHub:
    https://llvm.org/svn/llvm-project/libunwind/trunk@375505

--- src/UnwindCursor.hpp
+++ src/UnwindCursor.hpp
@@ -1222,11 +1222,6 @@ template <typename A, typename R> bool UnwindCursor<A, R>::isSignalFrame() {
 #endif // defined(_LIBUNWIND_SUPPORT_SEH_UNWIND)
 
 #if defined(_LIBUNWIND_ARM_EHABI)
-struct EHABIIndexEntry {
-  uint32_t functionOffset;
-  uint32_t data;
-};
-
 template<typename A>
 struct EHABISectionIterator {
   typedef EHABISectionIterator _Self;
@@ -1991,7 +1986,10 @@ int UnwindCursor<A, R>::step() {
 
 template <typename A, typename R>
 void UnwindCursor<A, R>::getInfo(unw_proc_info_t *info) {
-  *info = _info;
+  if (_unwindInfoMissing)
+    memset(info, 0, sizeof(*info));
+  else
+    *info = _info;
 }
 
 template <typename A, typename R>
