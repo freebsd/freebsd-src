29bffcffa3f75a0b9fa5e25b475051cdcd3c6779
John Baldwin <jhb@FreeBSD.org>
2019-10-24 20:48:30 +0000

    MFC 350549: Set ISOPEN in namei flags when opening executable interpreters.
    These vnodes are explicitly opened via VOP_OPEN via
    exec_check_permissions identical to the main exectuable image.
    Setting ISOPEN allows filesystems to perform suitable checks in
    VOP_LOOKUP (e.g. close-to-open consistency in the NFS client).

--- sys/kern/imgact_elf.c
+++ sys/kern/imgact_elf.c
@@ -687,7 +687,8 @@ __elfN(load_file)(struct proc *p, const char *file, u_long *addr,
 	imgp->object = NULL;
 	imgp->execlabel = NULL;
 
-	NDINIT(nd, LOOKUP, LOCKLEAF | FOLLOW, UIO_SYSSPACE, file, curthread);
+	NDINIT(nd, LOOKUP, ISOPEN | LOCKLEAF | FOLLOW, UIO_SYSSPACE, file,
+	    curthread);
 	if ((error = namei(nd)) != 0) {
 		nd->ni_vp = NULL;
 		goto fail;
