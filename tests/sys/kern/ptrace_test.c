4cbfddfbde896a6e088ec1ab0185d345bbab8af2
Li-Wen Hsu <lwhsu@FreeBSD.org>
2019-10-25 21:52:02 +0000

    MFC r350211, r350220, r350235, r350238-r350239, r350295, r350512, r350700, r352219
    r350211:
    Temporarily skip flakey test case
    sys.kern.ptrace_test.ptrace__follow_fork_child_detached_unrelated_debugger
    
    PR:		239292
    Sponsored by:	The FreeBSD Foundation
    
    r350220:
    Fix URL.
    
    Sponsored by:	The FreeBSD Foundation
    
    r350235:
    Temporarily skip flakey test case
    sys.kern.ptrace_test.ptrace__PT_KILL_competing_stop
    
    PR:		220841
    Sponsored by:	The FreeBSD Foundation
    
    r350238:
    Temporarily skip flakey test case
    sys.kern.ptrace_test.ptrace__follow_fork_both_attached_unrelated_debugger
    
    PR:		239397
    Sponsored by:	The FreeBSD Foundation
    
    r350239:
    Temporarily skip flakey test case
    sys.kern.ptrace_test.ptrace__parent_sees_exit_after_child_debugger
    
    PR:		239399
    Sponsored by:	The FreeBSD Foundation
    
    r350295:
    Temporarily skip flakey test case
    sys.kern.ptrace_test.ptrace__follow_fork_parent_detached_unrelated_debugger
    
    PR:		239425
    Sponsored by:	The FreeBSD Foundation
    
    r350512:
    Only skip test cases sometimes failing in CI when they are running in CI
    
    Suggested by:	jhb
    Sponsored by:	The FreeBSD Foundation
    
    r350700:
    Get configuration variable with default value for not breaking default setting
    
    Reported by:	markj
    Sponsored by:	The FreeBSD Foundation
    
    r352219:
    Temporarily skip flakey test case sys.kern.ptrace_test.ptrace__getppid
    
    PR:		240510
    Sponsored by:	The FreeBSD Foundation

--- tests/sys/kern/ptrace_test.c
+++ tests/sys/kern/ptrace_test.c
@@ -261,6 +261,9 @@ ATF_TC_BODY(ptrace__parent_sees_exit_after_child_debugger, tc)
 	int cpipe[2], dpipe[2], status;
 	char c;
 
+	if (atf_tc_get_config_var_as_bool_wd(tc, "ci", false))
+		atf_tc_skip("https://bugs.freebsd.org/239399");
+
 	ATF_REQUIRE(pipe(cpipe) == 0);
 	ATF_REQUIRE((child = fork()) != -1);
 
@@ -802,6 +805,9 @@ ATF_TC_BODY(ptrace__follow_fork_both_attached_unrelated_debugger, tc)
 	pid_t children[2], fpid, wpid;
 	int cpipe[2], status;
 
+	if (atf_tc_get_config_var_as_bool_wd(tc, "ci", false))
+		atf_tc_skip("https://bugs.freebsd.org/239397");
+
 	ATF_REQUIRE(pipe(cpipe) == 0);
 	ATF_REQUIRE((fpid = fork()) != -1);
 	if (fpid == 0) {
@@ -870,6 +876,9 @@ ATF_TC_BODY(ptrace__follow_fork_child_detached_unrelated_debugger, tc)
 	pid_t children[2], fpid, wpid;
 	int cpipe[2], status;
 
+	if (atf_tc_get_config_var_as_bool_wd(tc, "ci", false))
+		atf_tc_skip("https://bugs.freebsd.org/239292");
+
 	ATF_REQUIRE(pipe(cpipe) == 0);
 	ATF_REQUIRE((fpid = fork()) != -1);
 	if (fpid == 0) {
@@ -933,6 +942,9 @@ ATF_TC_BODY(ptrace__follow_fork_parent_detached_unrelated_debugger, tc)
 	pid_t children[2], fpid, wpid;
 	int cpipe[2], status;
 
+	if (atf_tc_get_config_var_as_bool_wd(tc, "ci", false))
+		atf_tc_skip("https://bugs.freebsd.org/239425");
+
 	ATF_REQUIRE(pipe(cpipe) == 0);
 	ATF_REQUIRE((fpid = fork()) != -1);
 	if (fpid == 0) {
@@ -994,6 +1006,10 @@ ATF_TC_BODY(ptrace__getppid, tc)
 	pid_t child, debugger, ppid, wpid;
 	int cpipe[2], dpipe[2], status;
 	char c;
+
+	if (atf_tc_get_config_var_as_bool_wd(tc, "ci", false))
+		atf_tc_skip("https://bugs.freebsd.org/240510");
+
 
 	ATF_REQUIRE(pipe(cpipe) == 0);
 	ATF_REQUIRE((child = fork()) != -1);
@@ -2078,6 +2094,9 @@ ATF_TC_BODY(ptrace__PT_KILL_competing_stop, tc)
 	lwpid_t main_lwp;
 	struct ptrace_lwpinfo pl;
 	struct sched_param sched_param;
+
+	if (atf_tc_get_config_var_as_bool_wd(tc, "ci", false))
+		atf_tc_skip("https://bugs.freebsd.org/220841");
 
 	ATF_REQUIRE((fpid = fork()) != -1);
 	if (fpid == 0) {
