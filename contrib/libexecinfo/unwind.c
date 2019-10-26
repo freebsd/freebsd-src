6b7774833bf027acc97c37658d20d4b4f4f1e009
cem <cem@FreeBSD.org>
2019-10-26 06:59:59 +0000

    Sync up with NetBSD libexecinfo changes 2014-2019
    Drop portions that are unlit or redundant with llvm-libunwind: builtin.c,
    unwind.h, and unwind_arm_ehabi_stub.c.
    
    This code should now work with -fPIE binaries, should we choose to build any
    that way.
    
    When backtrace() array is full, signal an error so the underlying
    Itanium-style C++ exception handling library (llvm-libunwind) knows to stop
    tracing instead of continuing.  (It should stop on its own when it finishes
    unwinding, so this is mostly an extra seatbelt against an infinite loop bug
    in the unwinder.)

--- contrib/libexecinfo/unwind.c
+++ contrib/libexecinfo/unwind.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: unwind.c,v 1.1 2012/05/26 22:02:29 christos Exp $	*/
+/*	$NetBSD: unwind.c,v 1.3 2019/01/30 22:46:49 mrg Exp $	*/
 
 /*-
  * Copyright (c) 2012 The NetBSD Foundation, Inc.
@@ -51,8 +51,10 @@ tracer(struct _Unwind_Context *ctx, void *arg)
 		return 0;
 	}
 	if (t->n < t->len)
-		t->arr[t->n++] = _Unwind_GetIP(ctx);
-	return 0;
+		t->arr[t->n++] = (void *)_Unwind_GetIP(ctx);
+	else
+		return _URC_END_OF_STACK;
+	return _URC_NO_REASON;
 }
 
 size_t
