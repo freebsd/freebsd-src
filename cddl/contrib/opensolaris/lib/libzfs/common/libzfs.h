22b0eb13b24b859a3d50a37a0bcf90564d31896a
asomers <asomers@FreeBSD.org>
2019-10-26 17:11:02 +0000

    MFZoL:  Avoid retrieving unused snapshot props
    This patch modifies the zfs_ioc_snapshot_list_next() ioctl to enable it
    to take input parameters that alter the way looping through the list of
    snapshots is performed. The idea here is to restrict functions that
    throw away some of the snapshots returned by the ioctl to a range of
    snapshots that these functions actually use. This improves efficiency
    and execution speed for some rollback and send operations.
    
    Reviewed-by: Tom Caputi <tcaputi@datto.com>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed by: Matt Ahrens <mahrens@delphix.com>
    Signed-off-by: Alek Pinchuk <apinchuk@datto.com>
    Closes #8077
    zfsonlinux/zfs@4c0883fb4af0d5565459099b98fcf90ecbfa1ca1
    
    MFC after:	2 weeks

--- cddl/contrib/opensolaris/lib/libzfs/common/libzfs.h
+++ cddl/contrib/opensolaris/lib/libzfs/common/libzfs.h
@@ -28,7 +28,7 @@
  * Copyright (c) 2013 Steven Hartland. All rights reserved.
  * Copyright (c) 2014 Integros [integros.com]
  * Copyright 2016 Nexenta Systems, Inc.
- * Copyright (c) 2017 Datto Inc.
+ * Copyright (c) 2019 Datto Inc.
  */
 
 #ifndef	_LIBZFS_H
@@ -570,8 +570,10 @@ extern int zfs_iter_root(libzfs_handle_t *, zfs_iter_f, void *);
 extern int zfs_iter_children(zfs_handle_t *, zfs_iter_f, void *);
 extern int zfs_iter_dependents(zfs_handle_t *, boolean_t, zfs_iter_f, void *);
 extern int zfs_iter_filesystems(zfs_handle_t *, zfs_iter_f, void *);
-extern int zfs_iter_snapshots(zfs_handle_t *, boolean_t, zfs_iter_f, void *);
-extern int zfs_iter_snapshots_sorted(zfs_handle_t *, zfs_iter_f, void *);
+extern int zfs_iter_snapshots(zfs_handle_t *, boolean_t, zfs_iter_f, void *,
+    uint64_t, uint64_t);
+extern int zfs_iter_snapshots_sorted(zfs_handle_t *, zfs_iter_f, void *,
+    uint64_t, uint64_t);
 extern int zfs_iter_snapspec(zfs_handle_t *, const char *, zfs_iter_f, void *);
 extern int zfs_iter_bookmarks(zfs_handle_t *, zfs_iter_f, void *);
 
