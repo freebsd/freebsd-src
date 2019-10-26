499fe48de8938d4c7b0a91e20eb6c16db9d55633
Conrad Meyer <cem@FreeBSD.org>
2019-10-26 06:59:59 +0000

    Sync up with NetBSD libexecinfo changes 2014-2019
    Drop portions that are unlit or redundant with llvm-libunwind: builtin.c,
    unwind.h, and unwind_arm_ehabi_stub.c.
    
    This code should now work with -fPIE binaries, should we choose to build any
    that way.
    
    When backtrace() array is full, signal an error so the underlying
    Itanium-style C++ exception handling library (llvm-libunwind) knows to stop
    tracing instead of continuing.  (It should stop on its own when it finishes
    unwinding, so this is mostly an extra seatbelt against an infinite loop bug
    in the unwinder.)

--- contrib/libexecinfo/symtab.c
+++ contrib/libexecinfo/symtab.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: symtab.c,v 1.2 2013/08/29 15:01:57 christos Exp $	*/
+/*	$NetBSD: symtab.c,v 1.5 2016/04/20 14:00:16 christos Exp $	*/
 
 /*-
  * Copyright (c) 2012 The NetBSD Foundation, Inc.
@@ -29,12 +29,13 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: symtab.c,v 1.2 2013/08/29 15:01:57 christos Exp $");
+__RCSID("$NetBSD: symtab.c,v 1.5 2016/04/20 14:00:16 christos Exp $");
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <err.h>
 #include <dlfcn.h>
 
@@ -47,7 +48,6 @@ __RCSID("$NetBSD: symtab.c,v 1.2 2013/08/29 15:01:57 christos Exp $");
 #define ELF_ST_TYPE(x)          (((unsigned int)x) & 0xf)
 #endif
 
-
 #include "symtab.h"
 
 struct symbol {
@@ -59,6 +59,7 @@ struct symbol {
 struct symtab {
 	size_t nsymbols;
 	struct symbol *symbols;
+	bool ispie;
 };
 
 static int
@@ -86,6 +87,7 @@ symtab_create(int fd, int bind, int type)
 	Elf *elf;
 	symtab_t *st;
 	Elf_Scn *scn = NULL;
+	GElf_Ehdr ehdr;
 
 	if (elf_version(EV_CURRENT) == EV_NONE) {
 		warnx("Elf Library is out of date.");
@@ -103,6 +105,13 @@ symtab_create(int fd, int bind, int type)
 		elf_end(elf);
 		return NULL;
 	}
+	if (gelf_getehdr(elf, &ehdr) == NULL) {
+		warnx("Error getting ELF Ehdr");
+		elf_end(elf);
+		return NULL;
+	}
+
+	st->ispie = ehdr.e_type == ET_DYN;
 
 	while ((scn = elf_nextscn(elf, scn)) != NULL) {
 		GElf_Shdr shdr;
@@ -126,6 +135,13 @@ symtab_create(int fd, int bind, int type)
 		for (size_t i = 0; i < ns; i++) {
 			GElf_Sym sym;
                         gelf_getsym(edata, (int)i, &sym);
+
+#ifdef SYMTAB_DEBUG
+			fprintf(stderr, "%s: %s@%#jx=%d,%d\n", __func__,
+			    elf_strptr(elf, shdr.sh_link, sym.st_name),
+			    (uintmax_t)sym.st_value, ELF_ST_BIND(sym.st_info),
+			    ELF_ST_TYPE(sym.st_info));
+#endif
 
 			if (bind != -1 &&
 			    (unsigned)bind != ELF_ST_BIND(sym.st_info))
@@ -159,7 +175,7 @@ out:
 	return NULL;
 }
 
-	
+
 int
 symtab_find(const symtab_t *st, const void *p, Dl_info *dli)
 {
@@ -168,8 +184,15 @@ symtab_find(const symtab_t *st, const void *p, Dl_info *dli)
 	size_t hi = ns;
 	size_t lo = 0;
 	size_t mid = ns / 2;
-	uintptr_t dd, sd, me = (uintptr_t)p;
+	uintptr_t fbase = st->ispie ? (uintptr_t)dli->dli_fbase : 0;
+	uintptr_t dd, sd, me = (uintptr_t)p - fbase;
+	uintptr_t ad = (uintptr_t)dli->dli_saddr - fbase;
 
+#ifdef SYMTAB_DEBUG
+	fprintf(stderr, "%s: [fbase=%#jx, saddr=%p, me=%#jx ad=%#jx]\n",
+	    __func__, (uintmax_t)fbase, dli->dli_saddr, (uintmax_t)me,
+	    (uintmax_t)ad);
+#endif
 	for (;;) {
 		if (s[mid].st_value < me)
 			lo = mid;
@@ -183,11 +206,20 @@ symtab_find(const symtab_t *st, const void *p, Dl_info *dli)
 		}
 		mid = (hi + lo) / 2;
 	}
-	dd = me - (uintptr_t)dli->dli_saddr;
+	dd = me - ad;
 	sd = me - s[mid].st_value;
 	if (dd > sd) {
 		dli->dli_saddr = (void *)s[mid].st_value;
 		dli->dli_sname = s[mid].st_name;
+#ifdef SYMTAB_DEBUG
+		fprintf(stderr, "%s: me=%#jx -> [%#jx, %s]\n", __func__,
+		    (uintmax_t)me, (uintmax_t)sd, dli->dli_sname);
+#endif
 	}
+#ifdef SYMTAB_DEBUG
+	else
+		fprintf(stderr, "%s: %#jx -> [%#jx, ***]\n", __func__,
+		    (uintmax_t)me, (uintmax_t)sd);
+#endif
 	return 1;
 }
