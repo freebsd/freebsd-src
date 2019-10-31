024a20f1506c0f6bcd01bc80bc1c837edd611502
vmaffione <vmaffione@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-10-31 22:16:20 +0000

    netmap: fix build issue in netmap_user.h
    The issue was a comparison of integers of different signs
    on 32 bit architectures.
    
    Reported by:	jenkins
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@354231 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net/netmap_user.h
+++ sys/net/netmap_user.h
@@ -1116,7 +1116,7 @@ nm_dispatch(struct nm_desc *d, int cnt, nm_cb_t cb, u_char *arg)
 				slot = &ring->slot[i];
 				d->hdr.len += slot->len;
 				nbuf = (u_char *)NETMAP_BUF(ring, slot->buf_idx);
-				if (oldbuf != NULL && nbuf - oldbuf == ring->nr_buf_size &&
+				if (oldbuf != NULL && nbuf - oldbuf == (int)ring->nr_buf_size &&
 						oldlen == ring->nr_buf_size) {
 					d->hdr.caplen += slot->len;
 					oldbuf = nbuf;
