c5f496c6fd2ee0df5fb4913c6cdd5afda99b59b3
wulf <wulf@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-11-03 20:43:02 +0000

    [ig4] Drop driver's internal RX FIFO
    There is no need to read all controller's RX FIFO data to clear RX_FULL
    bit in interrupt handler as interrupts are masked permanently since
    previous commit.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@354294 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ichiic/ig4_var.h
+++ sys/dev/ichiic/ig4_var.h
@@ -43,9 +43,6 @@
 #include "pci_if.h"
 #include "iicbus_if.h"
 
-#define IG4_RBUFSIZE	128
-#define IG4_RBUFMASK	(IG4_RBUFSIZE - 1)
-
 enum ig4_op { IG4_IDLE, IG4_READ, IG4_WRITE };
 enum ig4_vers { IG4_HASWELL, IG4_ATOM, IG4_SKYLAKE, IG4_APL };
 
@@ -62,9 +59,6 @@ struct ig4iic_softc {
 	enum ig4_vers	version;
 	enum ig4_op	op;
 	int		cmd;
-	int		rnext;
-	int		rpos;
-	char		rbuf[IG4_RBUFSIZE];
 	uint32_t	intr_mask;
 	int		error;
 	uint8_t		last_slave;
@@ -81,19 +75,6 @@ struct ig4iic_softc {
 	 * with the controller acquire an exclusive lock on call_lock
 	 * to prevent interleaving of calls to the interface and a lock on
 	 * io_lock right afterwards, to synchronize controller I/O activity.
-	 *
-	 * The interrupt handler can only read data while no iicbus call
-	 * is in progress or while io_lock is dropped during mtx_sleep in
-	 * wait_status and set_controller. It is safe to drop io_lock in those
-	 * places, because the interrupt handler only accesses those registers:
-	 *
-	 * - IG4_REG_I2C_STA  (I2C Status)
-	 * - IG4_REG_DATA_CMD (Data Buffer and Command)
-	 * - IG4_REG_CLR_INTR (Clear Interrupt)
-	 *
-	 * Locking outside of those places is required to make the content
-	 * of rpos/rnext predictable (e.g. whenever data_read is called and in
-	 * ig4iic_transfer).
 	 */
 	struct sx	call_lock;
 	struct mtx	io_lock;
