41e8562cdc86ecf5f8d662c3db12b84596a07ff6
Andriy Gapon <avg@FreeBSD.org>
2019-11-06 08:58:03 +0000

    10601 10757 Pool allocation classes
    illumos/illumos-gate@663207adb1669640c01c5ec6949ce78fd806efae
    https://github.com/illumos/illumos-gate/commit/663207adb1669640c01c5ec6949ce78fd806efae
    
    10601 Pool allocation classes
    https://www.illumos.org/issues/10601
      illumos port of ZoL Pool allocation classes. Includes at least these two
      commits:
      441709695 Pool allocation classes misplacing small file blocks
      cc99f275a Pool allocation classes
    
    10757 Add -gLp to zpool subcommands for alt vdev names
    https://www.illumos.org/issues/10757
      Port from ZoL of
      d2f3e292d Add -gLp to zpool subcommands for alt vdev names
      Note that a subsequent ZoL commit changed -p to -P
      a77f29f93 Change full path subcommand flag from -p to -P
    
    Portions contributed by: Jerry Jelinek <jerry.jelinek@joyent.com>
    Portions contributed by: Håkan Johansson <f96hajo@chalmers.se>
    Portions contributed by: Richard Yao <ryao@gentoo.org>
    Portions contributed by: Chunwei Chen <david.chen@nutanix.com>
    Portions contributed by: loli10K <ezomori.nozomu@gmail.com>
    Author: Don Brady <don.brady@delphix.com>

--- uts/common/sys/fs/zfs.h
+++ uts/common/sys/fs/zfs.h
@@ -26,6 +26,7 @@
  * Copyright (c) 2014 Integros [integros.com]
  * Copyright 2017 Joyent, Inc.
  * Copyright (c) 2017 Datto Inc.
+ * Copyright (c) 2017, Intel Corporation.
  */
 
 /* Portions Copyright 2010 Robert Milkowski */
@@ -163,6 +164,7 @@ typedef enum {
 	ZFS_PROP_PREV_SNAP,
 	ZFS_PROP_RECEIVE_RESUME_TOKEN,
 	ZFS_PROP_REMAPTXG,		/* not exposed to the user */
+	ZFS_PROP_SPECIAL_SMALL_BLOCKS,
 	ZFS_NUM_PROPS
 } zfs_prop_t;
 
@@ -600,6 +602,8 @@ typedef struct zpool_load_policy {
 #define	ZPOOL_CONFIG_MMP_TXG		"mmp_txg"	/* not stored on disk */
 #define	ZPOOL_CONFIG_MMP_HOSTNAME	"mmp_hostname"	/* not stored on disk */
 #define	ZPOOL_CONFIG_MMP_HOSTID		"mmp_hostid"	/* not stored on disk */
+#define	ZPOOL_CONFIG_ALLOCATION_BIAS	"alloc_bias"	/* not stored on disk */
+
 /*
  * The persistent vdev state is stored as separate values rather than a single
  * 'vdev_state' entry.  This is because a device can be in multiple states, such
@@ -644,6 +648,14 @@ typedef struct zpool_load_policy {
 	"com.delphix:obsolete_counts_are_precise"
 #define	VDEV_TOP_ZAP_POOL_CHECKPOINT_SM \
 	"com.delphix:pool_checkpoint_sm"
+
+#define	VDEV_TOP_ZAP_ALLOCATION_BIAS \
+	"org.zfsonlinux:allocation_bias"
+
+/* vdev metaslab allocation bias */
+#define	VDEV_ALLOC_BIAS_LOG		"log"
+#define	VDEV_ALLOC_BIAS_SPECIAL		"special"
+#define	VDEV_ALLOC_BIAS_DEDUP		"dedup"
 
 #define	VDEV_LEAF_ZAP_INITIALIZE_LAST_OFFSET	\
 	"com.delphix:next_offset_to_initialize"
