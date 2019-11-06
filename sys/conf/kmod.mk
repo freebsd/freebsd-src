0b59366325117cff311b389d33626ef7aff3b502
Warner Losh <imp@FreeBSD.org>
2019-11-06 18:54:04 +0000

    Remove obsolete addition of MACHINE_CPUARCH
    In the past, we would add symbolic links for MACHINE_CPUARCH when it differed
    from MACHINE. This was for pc98 only, however. All other architectures didn't
    need this and it was really due to pc98 pulling from i386 rather than something
    more intrinsic. At the time, we had the split we did to mimic what NetBSD did
    for its 68k ports where many different kernels were possible for the same
    architecture. Since then, both projects have moved away from this convention to
    having a more generic MACHINE for each architecture. FreeBSD's new arm64/aarch64
    breaks this old notion and so was an exception to the rule. So, we no longer
    need to create this link for any old machine or any new machine, delete it
    entirely.
    
    Differential Revision: https://reviews.freebsd.org/D22246

--- sys/conf/kmod.mk
+++ sys/conf/kmod.mk
@@ -283,9 +283,6 @@ _MAP_DEBUG_PREFIX= yes
 .endif
 
 _ILINKS=machine
-.if ${MACHINE} != ${MACHINE_CPUARCH} && ${MACHINE} != "arm64"
-_ILINKS+=${MACHINE_CPUARCH}
-.endif
 .if ${MACHINE_CPUARCH} == "i386" || ${MACHINE_CPUARCH} == "amd64"
 _ILINKS+=x86
 .endif
