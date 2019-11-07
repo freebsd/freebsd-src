d24bd064e1196c0441c02d8c0f43f70abb59a89f
glebius <glebius@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-11-07 20:49:56 +0000

    Since r353292 on input path we are always in network epoch, when we lookup PCBs.  Thus, do not enter epoch recursively in in_pcblookup_hash() and in6_pcblookup_hash().  Same applies to tcp_ctlinput() and tcp6_ctlinput().
    This leaves several sysctl(9) handlers that return PCB credentials
    unprotected.  Add epoch enter/exit to all of them.
    
    Differential Revision:	https://reviews.freebsd.org/D22197
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@354477 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet6/in6_pcb.c
+++ sys/netinet6/in6_pcb.c
@@ -1245,7 +1245,6 @@ in6_pcblookup_hash(struct inpcbinfo *pcbinfo, struct in6_addr *faddr,
 {
 	struct inpcb *inp;
 
-	INP_HASH_RLOCK(pcbinfo);
 	inp = in6_pcblookup_hash_locked(pcbinfo, faddr, fport, laddr, lport,
 	    (lookupflags & ~(INPLOOKUP_RLOCKPCB | INPLOOKUP_WLOCKPCB)), ifp);
 	if (inp != NULL) {
@@ -1272,7 +1271,6 @@ in6_pcblookup_hash(struct inpcbinfo *pcbinfo, struct in6_addr *faddr,
 		}
 #endif
 	}
-	INP_HASH_RUNLOCK(pcbinfo);
 	return (inp);
 }
 
