2838303a877215d8fadef0e3e36d1493fd1f6855
Toomas Soome <tsoome@FreeBSD.org>
2019-11-10 09:32:17 +0000

    MFC r354279: loader: calculate physical vdev psize from asize
    Since physical device asize is calculated from psize and the asize is stored
    in pool label, we can use asize to set the value of psize, which is used to
    calculate the location of the pool labels.

--- sys/cddl/boot/zfs/zfsimpl.h
+++ sys/cddl/boot/zfs/zfsimpl.h
@@ -1660,7 +1660,8 @@ typedef struct vdev {
 	vdev_list_t	v_children;	/* children of this vdev */
 	const char	*v_name;	/* vdev name */
 	uint64_t	v_guid;		/* vdev guid */
-	int		v_id;		/* index in parent */
+	uint64_t	v_id;		/* index in parent */
+	uint64_t	v_psize;	/* physical device capacity */
 	int		v_ashift;	/* offset to block shift */
 	int		v_nparity;	/* # parity for raidz */
 	struct vdev	*v_top;		/* parent vdev */
