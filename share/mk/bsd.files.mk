89c5833b3b9f25e76437e58004e67762311c885a
arichardson <arichardson@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-11-15 18:34:36 +0000

    Fix build race in bsd.files.mk
    We need to ensure that installdirs-FOO runs before installfiles-FOO since
    otherwise the directory may not exist when we attempt to install the target.
    This was randomly causing failures in our Jenkins instance when installing
    drti.o in cddl/lib/drti.
    
    Reviewed By:	brooks
    Differential Revision: https://reviews.freebsd.org/D22382
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@354740 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/bsd.files.mk
+++ share/mk/bsd.files.mk
@@ -112,12 +112,11 @@ STAGE_AS_SETS+=	${${_${group}DIR_${file}}:C,[/*],_,g}
 STAGE_DIR.${${_${group}DIR_${file}}:C,[/*],_,g}= ${STAGE_OBJTOP}${${_${group}DIR_${file}}}
 stage_as.${${_${group}DIR_${file}}:C,[/*],_,g}: ${file}
 
-installfiles-${group}: _${group}INS1_${file}
-_${group}INS1_${file}: installdirs-${_${group}DIR_${file}} _${group}INS_${file}
-_${group}INS_${file}: ${file}
+installfiles-${group}: _${group}INS_${file}
+_${group}INS_${file}: ${file} installdirs-${_${group}DIR_${file}}
 	${INSTALL} ${${group}TAG_ARGS} -o ${${group}OWN_${file}} \
 	    -g ${${group}GRP_${file}} -m ${${group}MODE_${file}} \
-	    ${.ALLSRC} ${${group}PREFIX_${file}}/${${group}NAME_${file}}
+	    ${.ALLSRC:[1]} ${${group}PREFIX_${file}}/${${group}NAME_${file}}
 .endfor # file in ${${group}}
 
 .endif # defined(${group}) && !empty(${group})
