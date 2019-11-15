3eb06a252623bf8c5c430c6f5c01f2fc84b8ede7
scottl <scottl@FreeBSD.org>
2019-11-15 11:53:06 +0000

    Fix a typo in how the AVX512DQ feature bit is checked.
    Reviewed by:	kib
    Sponsored by:	Intel

--- sys/x86/x86/busdma_machdep.c
+++ sys/x86/x86/busdma_machdep.c
@@ -236,6 +236,12 @@ bus_dma_tag_create(bus_dma_tag_t parent, bus_size_t alignment,
 	return (error);
 }
 
+bus_dma_tag_t
+bus_dma_tag_alloc(bus_dma_tag_t parent, bus_dma_props_t props)
+{
+	return (NULL);
+}
+
 int
 bus_dma_tag_destroy(bus_dma_tag_t dmat)
 {
