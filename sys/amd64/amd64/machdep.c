e3721601771396c5e731920d2a716b8a7cbbe176
Scott Long <scottl@FreeBSD.org>
2019-11-16 00:26:42 +0000

    TSX Asynchronous Abort mitigation for Intel CVE-2019-11135. This CVE has already been announced in FreeBSD SA-19:26.mcu.
    Mitigation for TAA involves either turning off TSX or turning on the
    VERW mitigation used for MDS. Some CPUs will also be self-mitigating
    for TAA and require no software workaround.
    
    Control knobs are:
    machdep.mitigations.taa.enable:
            0 - no software mitigation is enabled
            1 - attempt to disable TSX
            2 - use the VERW mitigation
            3 - automatically select the mitigation based on processor
    	    features.
    
    machdep.mitigations.taa.state:
            inactive        - no mitigation is active/enabled
            TSX disable     - TSX is disabled in the bare metal CPU as well as
                            - any virtualized CPUs
            VERW            - VERW instruction clears CPU buffers
    	not vulnerable	- The CPU has identified itself as not being
    			  vulnerable
    
    Nothing in the base FreeBSD system uses TSX.  However, the instructions
    are straight-forward to add to custom applications and require no kernel
    support, so the mitigation is provided for users with untrusted
    applications and tenants.
    
    Reviewed by:	emaste, imp, kib, scottph
    Sponsored by:	Intel
    Differential Revision:	22374

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -1779,6 +1779,7 @@ hammer_time(u_int64_t modulep, u_int64_t physfree)
 	TUNABLE_INT_FETCH("machdep.syscall_ret_l1d_flush",
 	    &syscall_ret_l1d_flush_mode);
 	TUNABLE_INT_FETCH("hw.mds_disable", &hw_mds_disable);
+	TUNABLE_INT_FETCH("machdep.mitigations.taa.enable", &x86_taa_enable);
 
 	finishidentcpu();	/* Final stage of CPU initialization */
 	initializecpu();	/* Initialize CPU registers */
