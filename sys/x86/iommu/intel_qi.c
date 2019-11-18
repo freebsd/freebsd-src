b5677bab6060fb466aeb4746b035a51334c013bd
kib <kib@FreeBSD.org>
2019-11-18 20:56:59 +0000

    bus_dma_dmar_set_buswide(9): KPI to indicate that the whole dmar context should share page tables.
    Practically it means that dma requests from any device on the bus are
    translated according to the entries loaded for the bus:0:0 device.
    KPI requires that the slot and function of the device be 0:0, and that
    no tags for other devices on the bus were used.
    
    The intended use are NTBs which pass TLPs from the downstream to the
    host with slot:func of the downstream originator.
    
    Reviewed and tested by:	mav
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D22434

--- sys/x86/iommu/intel_qi.c
+++ sys/x86/iommu/intel_qi.c
@@ -58,6 +58,7 @@ __FBSDID("$FreeBSD$");
 #include <x86/include/busdma_impl.h>
 #include <x86/iommu/intel_reg.h>
 #include <x86/iommu/busdma_dmar.h>
+#include <dev/pci/pcireg.h>
 #include <x86/iommu/intel_dmar.h>
 
 static bool
