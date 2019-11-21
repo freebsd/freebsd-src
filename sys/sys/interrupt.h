8e11e76e124180dd6b7669a55bd92288ee89e84d
Gleb Smirnoff <glebius@FreeBSD.org>
2019-11-21 23:55:19 +0000

    Delete ih_thread.  Should have gone in r334170.

--- sys/sys/interrupt.h
+++ sys/sys/interrupt.h
@@ -55,7 +55,6 @@ struct intr_handler {
 	int		 ih_need;	/* Needs service. */
 	CK_SLIST_ENTRY(intr_handler) ih_next; /* Next handler for this event. */
 	u_char		 ih_pri;	/* Priority of this handler. */
-	struct intr_thread *ih_thread;	/* Ithread for filtered handler. */
 };
 
 /* Interrupt handle flags kept in ih_flags */
