2f58b9904d13986d211d33224144f3bcd56d24d7
cem <cem@FreeBSD.org>
2019-11-22 20:24:15 +0000

    random(4): De-export random_sources list
    The internal datastructures do not need to be visible outside of
    random_harvestq, and this helps ensure they are not misused.
    
    No functional change.
    
    Approved by:	csprng(delphij, markm)
    Differential Revision:	https://reviews.freebsd.org/D22485

--- sys/dev/random/random_infra.c
+++ sys/dev/random/random_infra.c
@@ -101,8 +101,6 @@ SYSCTL_BOOL(_kern_random_initial_seeding, OID_AUTO,
 
 MALLOC_DEFINE(M_ENTROPY, "entropy", "Entropy harvesting buffers and data structures");
 
-struct sources_head source_list = LIST_HEAD_INITIALIZER(source_list);
-
 #if defined(RANDOM_LOADABLE)
 struct random_algorithm *p_random_alg_context = NULL;
 #else /* !defined(RANDOM_LOADABLE) */
