b5abfdbe10cd7e0c0bdc0d57925b475fcbb919d6
rpokala <rpokala@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-11-22 16:53:30 +0000

    Add and document options to allow rpc.lockd and rpc.statd to run in the foreground.
    This allows a separate process to monitor when and how those programs exit.
    That process can then restart them if needed.
    
    Submitted by:	Alex Burlyga
    Reviewed by:	bcr, imp
    MFC after:	1 week
    Sponsored by:	Panasas
    Differential Revision:	https://reviews.freebsd.org/D22474
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@355006 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.sbin/rpc.lockd/lockd.c
+++ usr.sbin/rpc.lockd/lockd.c
@@ -120,6 +120,7 @@ main(int argc, char **argv)
 	char *endptr, **hosts_bak;
 	struct sigaction sigalarm;
 	int grace_period = 30;
+	int foreground = 0;
 	struct netconfig *nconf;
 	int have_v6 = 1;
 	int maxrec = RPC_MAXDATASIZE;
@@ -127,7 +128,7 @@ main(int argc, char **argv)
 	int attempt_cnt, port_len, port_pos, ret;
 	char **port_list;
 
-	while ((ch = getopt(argc, argv, "d:g:h:p:")) != (-1)) {
+	while ((ch = getopt(argc, argv, "d:Fg:h:p:")) != (-1)) {
 		switch (ch) {
 		case 'd':
 			debug_level = atoi(optarg);
@@ -135,6 +136,9 @@ main(int argc, char **argv)
 				usage();
 				/* NOTREACHED */
 			}
+			break;
+		case 'F':
+			foreground = 1;
 			break;
 		case 'g':
 			grace_period = atoi(optarg);
@@ -422,7 +426,7 @@ main(int argc, char **argv)
 	 * Note that it is NOT sensible to run this program from inetd - the
 	 * protocol assumes that it will run immediately at boot time.
 	 */
-	if (daemon(0, debug_level > 0)) {
+	if ((foreground == 0) && daemon(0, debug_level > 0)) {
 		err(1, "cannot fork");
 		/* NOTREACHED */
 	}
@@ -844,7 +848,7 @@ void
 usage()
 {
 	errx(1, "usage: rpc.lockd [-d <debuglevel>]"
-	    " [-g <grace period>] [-h <bindip>] [-p <port>]");
+	    " [-F] [-g <grace period>] [-h <bindip>] [-p <port>]");
 }
 
 /*
