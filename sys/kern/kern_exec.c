79a9a3df42dd1a28bff7bf14ff8b3d8f62c03e7c
Konstantin Belousov <kib@FreeBSD.org>
2019-11-24 12:35:16 +0000

    MFC r354789: kern_exec: p_osrel and p_fctl0 were obliterated by failed execve(2) attempt.

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -361,7 +361,6 @@ do_execve(struct thread *td, struct image_args *args, struct mac *mac_p)
 	struct ucred *oldcred;
 	struct uidinfo *euip = NULL;
 	register_t *stack_base;
-	int error, i;
 	struct image_params image_params, *imgp;
 	struct vattr attr;
 	int (*img_first)(struct image_params *);
@@ -382,6 +381,7 @@ do_execve(struct thread *td, struct image_args *args, struct mac *mac_p)
 #ifdef HWPMC_HOOKS
 	struct pmckern_procexec pe;
 #endif
+	int error, i, orig_osrel;
 	static const char fexecv_proc_title[] = "(fexecv)";
 
 	imgp = &image_params;
@@ -407,6 +407,7 @@ do_execve(struct thread *td, struct image_args *args, struct mac *mac_p)
 	imgp->attr = &attr;
 	imgp->args = args;
 	oldcred = p->p_ucred;
+	orig_osrel = p->p_osrel;
 
 #ifdef MAC
 	error = mac_execve_enter(imgp, mac_p);
@@ -892,6 +893,10 @@ interpret:
 	SDT_PROBE1(proc, , , exec__success, args->fname);
 
 exec_fail_dealloc:
+	if (error != 0) {
+		p->p_osrel = orig_osrel;
+	}
+
 	if (imgp->firstpage != NULL)
 		exec_unmap_first_page(imgp);
 
