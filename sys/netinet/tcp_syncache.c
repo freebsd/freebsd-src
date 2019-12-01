3cf38784e2948c7dadac9734024299c9bc17b989
Michael Tuexen <tuexen@FreeBSD.org>
2019-12-01 21:01:33 +0000

    Move all ECN related flags from the flags to the flags2 field. This allows adding more ECN related flags in the future. No functional change intended.
    Submitted by:		Richard Scheffenegger
    Reviewed by:		rrs@, tuexen@
    Differential Revision:	https://reviews.freebsd.org/D22497

--- sys/netinet/tcp_syncache.c
+++ sys/netinet/tcp_syncache.c
@@ -1023,7 +1023,7 @@ syncache_socket(struct syncache *sc, struct socket *lso, struct mbuf *m)
 	}
 
 	if (sc->sc_flags & SCF_ECN)
-		tp->t_flags |= TF_ECN_PERMIT;
+		tp->t_flags2 |= TF2_ECN_PERMIT;
 
 	/*
 	 * Set up MSS and get cached values from tcp_hostcache.
