57c37d2759e2ee2e6e4ac89e4d3f87e8a36f42a4
ian <ian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-12-02 16:15:18 +0000

    Remove "all rights reserved" from copyright after getting a response from Luiz that he also was not intentionally asserting that right, it was already there when he added his name.
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@355295 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/gpio/gpioiic.c
+++ sys/dev/gpio/gpioiic.c
@@ -2,7 +2,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  *
  * Copyright (c) 2009 Oleksandr Tymoshenko <gonzo@freebsd.org>
- * Copyright (c) 2010 Luiz Otavio O Souza All rights reserved.
+ * Copyright (c) 2010 Luiz Otavio O Souza
  * Copyright (c) 2019 Ian Lepore <ian@freebsd.org>
  *
  * Redistribution and use in source and binary forms, with or without
