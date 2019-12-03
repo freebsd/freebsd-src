e453f0166885c831686914a59f548ec274612d0c
Xin LI <delphij@FreeBSD.org>
2019-12-03 07:01:28 +0000

    newfs_msdos: -A is incompatible with -r, not -o.
    PR:		242314
    Submitted by:	Guy Yur <guyyur gmail com>
    MFC after:	2 weeks

--- sbin/newfs_msdos/newfs_msdos.c
+++ sbin/newfs_msdos/newfs_msdos.c
@@ -179,7 +179,7 @@ main(int argc, char *argv[])
     if (argc < 1 || argc > 2)
 	usage();
 	if (o.align) {
-		if (o.hidden_sectors_set)
+		if (o.reserved_sectors)
 		    errx(1, "align (-A) is incompatible with -r");
 	}
     fname = *argv++;
