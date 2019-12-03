5422fbcf56d63c01f8fc4257b9b3d7d777e7b6f1
manu <manu@FreeBSD.org>
2019-12-03 19:18:32 +0000

    arm64: rockchip: rl3399: Remove the ability to put the PLL in normal mode at boot
    RK3399 PLLs have three modes :
     - Normal, where they behave normally and their freq is calculated based on
       the registers values.
     - Slow, where the PLL freq is 24Mhz (well, the external oscillator).
     - Deep Slow, used for suspend where the freq is 32Khz.
    
    We used to put every CPU related PLL in normal mode but it can cause problem
    if the firmware didn't setup the clocks register correctly.
    And even if it did but left the pll in slow or deep slow mode that might be
    because the PMIC suppling voltage for the CPU haven't been configured yet
    and we cannot do that at this point.
    So remove the ability to set PLLs to normal mode at boot to avoid any problems.

--- sys/arm64/rockchip/clk/rk_clk_pll.c
+++ sys/arm64/rockchip/clk/rk_clk_pll.c
@@ -54,8 +54,6 @@ struct rk_clk_pll_sc {
 
 	struct rk_clk_pll_rate	*rates;
 	struct rk_clk_pll_rate	*frac_rates;
-
-	bool			normal_mode;
 };
 
 #define	WRITE4(_clk, off, val)						\
@@ -346,18 +344,8 @@ static int
 rk3399_clk_pll_init(struct clknode *clk, device_t dev)
 {
 	struct rk_clk_pll_sc *sc;
-	uint32_t reg;
 
 	sc = clknode_get_softc(clk);
-
-	if (sc->normal_mode) {
-		/* Setting to normal mode */
-		reg = RK3399_CLK_PLL_MODE_NORMAL << RK3399_CLK_PLL_MODE_SHIFT;
-		reg |= RK3399_CLK_PLL_MODE_MASK << RK_CLK_PLL_MASK_SHIFT;
-		WRITE4(clk, sc->base_offset + RK3399_CLK_PLL_MODE_OFFSET,
-		    reg | RK3399_CLK_PLL_WRITE_MASK);
-	}
-
 	clknode_init_parent_idx(clk, 0);
 
 	return (0);
@@ -549,7 +537,6 @@ rk3399_clk_pll_register(struct clkdom *clkdom, struct rk_clk_pll_def *clkdef)
 	sc->flags = clkdef->flags;
 	sc->rates = clkdef->rates;
 	sc->frac_rates = clkdef->frac_rates;
-	sc->normal_mode = clkdef->normal_mode;
 
 	clknode_register(clkdom, clk);
 
