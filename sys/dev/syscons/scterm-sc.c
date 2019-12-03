7d90a522d201fbec19cf0049d6d0e8f6f7ae91e0
Ed Maste <emaste@FreeBSD.org>
2019-12-03 16:45:53 +0000

    scterm-sc.c: convert source file to UTF-8 encoding
    Most source files are already ASCII or UTF-8 but this one was not previously
    converted.

--- sys/dev/syscons/scterm-sc.c
+++ sys/dev/syscons/scterm-sc.c
@@ -1,6 +1,6 @@
 /*-
  * Copyright (c) 1999 Kazutaka YOKOTA <yokota@zodiac.mech.utsunomiya-u.ac.jp>
- * Copyright (c) 1992-1998 Søren Schmidt
+ * Copyright (c) 1992-1998 SÃ¸ren Schmidt
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
