dc5e935947dfc79d4d3e9fdebc89515f17f9a070
tsoome <tsoome@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-12-03 18:36:39 +0000

    loader: ReadKeyStrokeEx may return partial keystrokes
    In some systems we can receive no scancode nor unicodechar values.
    
    PR:		240760
    Reported by:	Ariel Millennium Thornton
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@355347 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- stand/efi/libefi/efi_console.c
+++ stand/efi/libefi/efi_console.c
@@ -1126,10 +1126,11 @@ efi_readkey_ex(void)
 					kp->UnicodeChar++;
 				}
 			}
+			if (kp->ScanCode == 0 && kp->UnicodeChar == 0)
+				return (false);
+			keybuf_inschar(kp);
+			return (true);
 		}
-
-		keybuf_inschar(kp);
-		return (true);
 	}
 	return (false);
 }
