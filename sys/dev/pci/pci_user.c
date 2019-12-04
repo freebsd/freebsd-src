fce5d19d64361e2ccc65b68d0c81bc45b93c7f2f
Stefan Eßer <se@FreeBSD.org>
2019-12-04 08:03:33 +0000

    Remove "All rights reserved" phrase from copyright notes.
    With the ratification of the Berne Convention in 2000, it became obsolete.
    
    I have removed that phrase and the "(c)" only from files without copyright
    claims by other parties. There are 2 files (pci.c, pci_private.h) that are
    also claimed by Michael Smith <msmith@freebsd.org> and by BSDi, which have
    therefore not been included in this commit.
    
    When all member nations of the Buenos Aires Convention adopted the Berne
    Convention, the phrase "All rights reserved" became unnecessary to assert
    copyright. Remove it from files under my copyright.
    
    There are 2 files (pci.c, pci_private.h) that) that do also bear msmith's
    and BSDi's copyright. I have left them unchanged for now, since I do not
    know whether they (or the legal successor in case of BSDi) would agree.

--- sys/dev/pci/pci_user.c
+++ sys/dev/pci/pci_user.c
@@ -1,8 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
- * Copyright (c) 1997, Stefan Esser <se@freebsd.org>
- * All rights reserved.
+ * Copyright 1997, Stefan Esser <se@freebsd.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
