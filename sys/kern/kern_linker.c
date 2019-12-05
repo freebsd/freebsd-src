1850df8b620e82518e2c2cf4d35ef83d5f3aad13
Hans Petter Selasky <hselasky@FreeBSD.org>
2019-12-05 14:50:46 +0000

    MFC r355108 and r355170: Fix panic when loading kernel modules before root file system is mounted. Make sure the rootvnode is always NULL checked.
    Differential Revision:	https://reviews.freebsd.org/D22545
    PR:		241639
    Sponsored by:	Mellanox Technologies

--- sys/kern/kern_linker.c
+++ sys/kern/kern_linker.c
@@ -2061,14 +2061,18 @@ linker_load_module(const char *kldname, const char *modname,
  		 */
 		KASSERT(verinfo == NULL, ("linker_load_module: verinfo"
 		    " is not NULL"));
+		/* check if root file system is not mounted */
+		if (rootvnode == NULL || curproc->p_fd->fd_rdir == NULL)
+			return (ENXIO);
 		pathname = linker_search_kld(kldname);
 	} else {
 		if (modlist_lookup2(modname, verinfo) != NULL)
 			return (EEXIST);
+		/* check if root file system is not mounted */
+		if (rootvnode == NULL || curproc->p_fd->fd_rdir == NULL)
+			return (ENXIO);
 		if (kldname != NULL)
 			pathname = strdup(kldname, M_LINKER);
-		else if (rootvnode == NULL)
-			pathname = NULL;
 		else
 			/*
 			 * Need to find a KLD with required module
