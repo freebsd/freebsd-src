9159d4fe203f848dcd6680cc6c07221d751f361e
hselasky <hselasky@FreeBSD.org>
2019-12-05 15:16:19 +0000

    Add basic support for TCP/IP based hardware TLS offload to mlx5core.
    The hardware offload is primarily targeted for TLS v1.2 and v1.3,
    using AES 128/256 bit pre-shared keys. This patch adds all the needed
    hardware structures, capabilites and firmware commands.
    
    Sponsored by:	Mellanox Technologies

--- sys/dev/mlx5/mlx5_core/mlx5_fw.c
+++ sys/dev/mlx5/mlx5_core/mlx5_fw.c
@@ -227,6 +227,12 @@ int mlx5_query_hca_caps(struct mlx5_core_dev *dev)
 			return err;
 	}
 
+	if (MLX5_CAP_GEN(dev, tls)) {
+		err = mlx5_core_get_caps(dev, MLX5_CAP_TLS);
+		if (err)
+			return err;
+	}
+
 	err = mlx5_core_query_special_contexts(dev);
 	if (err)
 		return err;
