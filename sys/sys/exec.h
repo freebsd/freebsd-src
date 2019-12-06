d3cf9f3c1fa32d662cbfbad822519b22940857f9
John Baldwin <jhb@FreeBSD.org>
2019-12-06 19:20:45 +0000

    Remove SPARE_USRSPACE.
    This constant was used to reserve space at the top of the stack to
    hold translated system call arguments for non-default ABIs (the
    "stackgap").  However, none of the compatibility ABIs have used the
    stackgap in many years and the last use of SPARE_USRSPACE was removed
    in r355373.
    
    Reviewed by:	kib
    Sponsored by:	DARPA
    Differential Revision:	https://reviews.freebsd.org/D22697

--- sys/sys/exec.h
+++ sys/sys/exec.h
@@ -77,7 +77,6 @@ struct execsw {
  * Prefer the kern.ps_strings or kern.proc.ps_strings sysctls to this constant.
  */
 #define	PS_STRINGS	(USRSTACK - sizeof(struct ps_strings))
-#define	SPARE_USRSPACE	4096
 
 int exec_map_first_page(struct image_params *);        
 void exec_unmap_first_page(struct image_params *);       
