6a5e7cc7f80668a764bbe055b30eb21103510407
jeff <jeff@FreeBSD.org>
2019-12-08 01:15:06 +0000

    Use a variant slab structure for offpage zones.  This saves space in embedded slabs but also is an opportunity to tidy up code and add accessor inlines.
    Reviewed by:	markj, rlibby
    Differential Revision:	https://reviews.freebsd.org/D22609

--- sys/vm/uma_int.h
+++ sys/vm/uma_int.h
@@ -139,33 +139,28 @@
 /* Max waste percentage before going to off page slab management */
 #define UMA_MAX_WASTE	10
 
+
 /*
- * I doubt there will be many cases where this is exceeded. This is the initial
- * size of the hash table for uma_slabs that are managed off page. This hash
- * does expand by powers of two.  Currently it doesn't get smaller.
+ * Hash table for freed address -> slab translation.
+ *
+ * Only zones with memory not touchable by the allocator use the
+ * hash table.  Otherwise slabs are found with vtoslab().
  */
 #define UMA_HASH_SIZE_INIT	32		
-
-/* 
- * I should investigate other hashing algorithms.  This should yield a low
- * number of collisions if the pages are relatively contiguous.
- */
 
 #define UMA_HASH(h, s) ((((uintptr_t)s) >> UMA_SLAB_SHIFT) & (h)->uh_hashmask)
 
 #define UMA_HASH_INSERT(h, s, mem)					\
-		SLIST_INSERT_HEAD(&(h)->uh_slab_hash[UMA_HASH((h),	\
-		    (mem))], (s), us_hlink)
-#define UMA_HASH_REMOVE(h, s, mem)					\
-		SLIST_REMOVE(&(h)->uh_slab_hash[UMA_HASH((h),		\
-		    (mem))], (s), uma_slab, us_hlink)
+	LIST_INSERT_HEAD(&(h)->uh_slab_hash[UMA_HASH((h),		\
+	    (mem))], (uma_hash_slab_t)(s), uhs_hlink)
 
-/* Hash table for freed address -> slab translation */
+#define UMA_HASH_REMOVE(h, s)						\
+	LIST_REMOVE((uma_hash_slab_t)(s), uhs_hlink)
 
-SLIST_HEAD(slabhead, uma_slab);
+LIST_HEAD(slabhashhead, uma_hash_slab);
 
 struct uma_hash {
-	struct slabhead	*uh_slab_hash;	/* Hash table for slabs */
+	struct slabhashhead	*uh_slab_hash;	/* Hash table for slabs */
 	u_int		uh_hashsize;	/* Current size of the hash table */
 	u_int		uh_hashmask;	/* Mask used during hashing */
 };
@@ -202,13 +197,15 @@ struct uma_cache {
 
 typedef struct uma_cache * uma_cache_t;
 
+LIST_HEAD(slabhead, uma_slab);
+
 /*
  * Per-domain memory list.  Embedded in the kegs.
  */
 struct uma_domain {
-	LIST_HEAD(,uma_slab)	ud_part_slab;	/* partially allocated slabs */
-	LIST_HEAD(,uma_slab)	ud_free_slab;	/* empty slab list */
-	LIST_HEAD(,uma_slab)	ud_full_slab;	/* full slabs */
+	struct slabhead	ud_part_slab;	/* partially allocated slabs */
+	struct slabhead	ud_free_slab;	/* completely unallocated slabs */
+	struct slabhead ud_full_slab;	/* fully allocated slabs */
 };
 
 typedef struct uma_domain * uma_domain_t;
@@ -271,8 +268,6 @@ BITSET_DEFINE(noslabbits, 0);
  */
 struct uma_slab {
 	LIST_ENTRY(uma_slab)	us_link;	/* slabs in zone */
-	SLIST_ENTRY(uma_slab)	us_hlink;	/* Link for hash table */
-	uint8_t		*us_data;		/* First item */
 	uint16_t	us_freecount;		/* How many are free? */
 	uint8_t		us_flags;		/* Page flags see uma.h */
 	uint8_t		us_domain;		/* Backing NUMA domain. */
@@ -281,7 +276,6 @@ struct uma_slab {
 #endif
 	struct noslabbits us_free;		/* Free bitmask. */
 };
-
 #if MAXMEMDOM >= 255
 #error "Slab domain type insufficient"
 #endif
@@ -292,6 +286,47 @@ typedef struct uma_slab * uma_slab_t;
 size_t slab_sizeof(int nitems);
 size_t slab_space(int nitems);
 int slab_ipers(size_t size, int align);
+
+/*
+ * Slab structure with a full sized bitset and hash link for both
+ * HASH and OFFPAGE zones.
+ */
+struct uma_hash_slab {
+	struct uma_slab		uhs_slab;	/* Must be first. */
+	struct slabbits		uhs_bits;	/* Must be second. */
+	LIST_ENTRY(uma_hash_slab) uhs_hlink;	/* Link for hash table */
+	uint8_t			*uhs_data;	/* First item */
+};
+
+typedef struct uma_hash_slab * uma_hash_slab_t;
+
+static inline void *
+slab_data(uma_slab_t slab, uma_keg_t keg)
+{
+
+	if ((keg->uk_flags & UMA_ZONE_OFFPAGE) == 0)
+		return ((void *)((uintptr_t)slab - keg->uk_pgoff));
+	else
+		return (((uma_hash_slab_t)slab)->uhs_data);
+}
+
+static inline void *
+slab_item(uma_slab_t slab, uma_keg_t keg, int index)
+{
+	uintptr_t data;
+
+	data = (uintptr_t)slab_data(slab, keg);
+	return ((void *)(data + keg->uk_rsize * index));
+}
+
+static inline int
+slab_item_index(uma_slab_t slab, uma_keg_t keg, void *item)
+{
+	uintptr_t data;
+
+	data = (uintptr_t)slab_data(slab, keg);
+	return (((uintptr_t)item - data) / keg->uk_rsize);
+}
 
 TAILQ_HEAD(uma_bucketlist, uma_bucket);
 
@@ -444,14 +479,14 @@ static __inline uma_slab_t hash_sfind(struct uma_hash *hash, uint8_t *data);
 static __inline uma_slab_t
 hash_sfind(struct uma_hash *hash, uint8_t *data)
 {
-        uma_slab_t slab;
+        uma_hash_slab_t slab;
         u_int hval;
 
         hval = UMA_HASH(hash, data);
 
-        SLIST_FOREACH(slab, &hash->uh_slab_hash[hval], us_hlink) {
-                if ((uint8_t *)slab->us_data == data)
-                        return (slab);
+        LIST_FOREACH(slab, &hash->uh_slab_hash[hval], uhs_hlink) {
+                if ((uint8_t *)slab->uhs_data == data)
+                        return (&slab->uhs_slab);
         }
         return (NULL);
 }
