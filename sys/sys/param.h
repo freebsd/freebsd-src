a133c594896c9e64f6fd4a98cf28b174c1763e90
rmacklem <rmacklem@FreeBSD.org>
2019-12-12 23:37:04 +0000

    Bump __FreeBSD_version since r355677 changes the internal interface between the NFS modules such that they all need to be upgraded to post r355677 simultaneously.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -60,7 +60,7 @@
  *		in the range 5 to 9.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1300065	/* Master, propagated to newvers */
+#define __FreeBSD_version 1300066	/* Master, propagated to newvers */
 
 /*
  * __FreeBSD_kernel__ indicates that this system uses the kernel of FreeBSD,
