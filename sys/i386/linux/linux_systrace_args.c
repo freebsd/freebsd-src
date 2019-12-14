e83c6f63e8bb6d75a9dab6dad9702768003c567b
trasz <trasz@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-12-14 13:32:37 +0000

    Regen after r355752.
    MFC after:	2 weeks
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D22371
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@355753 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/linux/linux_systrace_args.c
+++ sys/i386/linux/linux_systrace_args.c
@@ -2194,7 +2194,12 @@ systrace_args(int sysnum, void *params, uint64_t *uarg, int *n_args)
 	}
 	/* linux_sync_file_range */
 	case 314: {
-		*n_args = 0;
+		struct linux_sync_file_range_args *p = params;
+		iarg[0] = p->fd; /* l_int */
+		iarg[1] = p->offset; /* l_loff_t */
+		iarg[2] = p->nbytes; /* l_loff_t */
+		uarg[3] = p->flags; /* unsigned int */
+		*n_args = 4;
 		break;
 	}
 	/* linux_tee */
@@ -6395,6 +6400,22 @@ systrace_entry_setargdesc(int sysnum, int ndx, char *desc, size_t descsz)
 		break;
 	/* linux_sync_file_range */
 	case 314:
+		switch(ndx) {
+		case 0:
+			p = "l_int";
+			break;
+		case 1:
+			p = "l_loff_t";
+			break;
+		case 2:
+			p = "l_loff_t";
+			break;
+		case 3:
+			p = "unsigned int";
+			break;
+		default:
+			break;
+		};
 		break;
 	/* linux_tee */
 	case 315:
@@ -8932,6 +8953,9 @@ systrace_return_setargdesc(int sysnum, int ndx, char *desc, size_t descsz)
 	case 313:
 	/* linux_sync_file_range */
 	case 314:
+		if (ndx == 0 || ndx == 1)
+			p = "int";
+		break;
 	/* linux_tee */
 	case 315:
 	/* linux_vmsplice */
