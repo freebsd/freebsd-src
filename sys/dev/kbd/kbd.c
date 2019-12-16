cb3066a8ff3db8b8d68287ce0abfd7dc7e80fc9d
kevans <kevans@FreeBSD.org>
2019-12-16 03:12:53 +0000

    kbd: remove kbdsw, store pointer to driver in each keyboard_t
    The previous implementation relied on a kbdsw array that mirrored the global
    keyboards array. This is fine, but also requires extra locking consideration
    when accessing to ensure that it's not being resized as new keyboards are
    added.
    
    The extra pointer costs little in a struct that there are relatively few of
    on any given system, and simplifies locking requirements ever-so-slightly as
    we only need to consider the locking requirements of whichever method is
    being invoked.
    
    __FreeBSD_version is bumped as any kbd modules will need rebuilt following
    this change.

--- sys/dev/kbd/kbd.c
+++ sys/dev/kbd/kbd.c
@@ -83,8 +83,6 @@ SET_DECLARE(kbddriver_set, const keyboard_driver_t);
 static int		keyboards = 1;
 static keyboard_t	*kbd_ini;
 static keyboard_t	**keyboard = &kbd_ini;
-static keyboard_switch_t *kbdsw_ini;
-       keyboard_switch_t **kbdsw = &kbdsw_ini;
 
 static int keymap_restrict_change;
 static SYSCTL_NODE(_hw, OID_AUTO, kbd, CTLFLAG_RD, 0, "kbd");
@@ -97,7 +95,6 @@ static int
 kbd_realloc_array(void)
 {
 	keyboard_t **new_kbd;
-	keyboard_switch_t **new_kbdsw;
 	int newsize;
 	int s;
 
@@ -108,21 +105,10 @@ kbd_realloc_array(void)
 		splx(s);
 		return (ENOMEM);
 	}
-	new_kbdsw = malloc(sizeof(*new_kbdsw)*newsize, M_DEVBUF,
-			    M_NOWAIT|M_ZERO);
-	if (new_kbdsw == NULL) {
-		free(new_kbd, M_DEVBUF);
-		splx(s);
-		return (ENOMEM);
-	}
 	bcopy(keyboard, new_kbd, sizeof(*keyboard)*keyboards);
-	bcopy(kbdsw, new_kbdsw, sizeof(*kbdsw)*keyboards);
-	if (keyboards > 1) {
+	if (keyboards > 1)
 		free(keyboard, M_DEVBUF);
-		free(kbdsw, M_DEVBUF);
-	}
 	keyboard = new_kbd;
-	kbdsw = new_kbdsw;
 	keyboards = newsize;
 	splx(s);
 
@@ -226,8 +212,8 @@ kbd_register(keyboard_t *kbd)
 
 	SLIST_FOREACH(p, &keyboard_drivers, link) {
 		if (strcmp(p->name, kbd->kb_name) == 0) {
+			kbd->kb_drv = p;
 			keyboard[index] = kbd;
-			kbdsw[index] = p->kbdsw;
 
 			if (mux != NULL) {
 				bzero(&ki, sizeof(ki));
@@ -243,8 +229,8 @@ kbd_register(keyboard_t *kbd)
 	SET_FOREACH(list, kbddriver_set) {
 		p = *list;
 		if (strcmp(p->name, kbd->kb_name) == 0) {
+			kbd->kb_drv = p;
 			keyboard[index] = kbd;
-			kbdsw[index] = p->kbdsw;
 
 			if (mux != NULL) {
 				bzero(&ki, sizeof(ki));
@@ -287,7 +273,6 @@ kbd_unregister(keyboard_t *kbd)
 	}
 	KBD_INVALID(kbd);
 	keyboard[kbd->kb_index] = NULL;
-	kbdsw[kbd->kb_index] = NULL;
 
 	splx(s);
 	return (0);
