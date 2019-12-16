9334768587d6e18e7f5e4c386ff32a9dac363b54
kevans <kevans@FreeBSD.org>
2019-12-16 16:41:24 +0000

    kbd drivers: don't double register keyboard drivers
    Keyboard drivers are generally registered via linker set. In these cases,
    they're also available as kmods which use KPI for registering/unregistering
    keyboard drivers outside of the linker set.
    
    For built-in modules, we still fire off MOD_LOAD and maybe even MOD_UNLOAD
    if an error occurs, leading to registration via linker set and at MOD_LOAD
    time.
    
    This is a minor optimization at best, but it keeps the internal kbd driver
    tidy as a future change will merge the linker set driver list into its
    internal keyboard_drivers list via SYSINIT and simplify driver lookup by
    removing the need to consult the linker set.

--- sys/dev/gpio/gpiokeys.c
+++ sys/dev/gpio/gpiokeys.c
@@ -983,6 +983,8 @@ KEYBOARD_DRIVER(gpiokeys, gpiokeyssw, gpiokeys_configure);
 static int
 gpiokeys_driver_load(module_t mod, int what, void *arg)
 {
+
+#ifdef KLD_MODULE
 	switch (what) {
 	case MOD_LOAD:
 		kbd_add_driver(&gpiokeys_kbd_driver);
@@ -991,6 +993,7 @@ gpiokeys_driver_load(module_t mod, int what, void *arg)
 		kbd_delete_driver(&gpiokeys_kbd_driver);
 		break;
 	}
+#endif
 	return (0);
 }
 
