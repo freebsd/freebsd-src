a4c5668d12d6f898c2c48fa9c0e8ffa86db8be76
Hans Petter Selasky <hselasky@FreeBSD.org>
2019-12-18 12:06:34 +0000

    Leave multicast group before reaping and committing state for both IPv4 and IPv6.
    This fixes a regression issue after r349369. When trying to exit a
    multicast group before closing the socket, a multicast leave packet
    should be sent.
    
    Differential Revision:	https://reviews.freebsd.org/D22848
    PR: 242677
    Reviewed by:	bz (network)
    Tested by:	Aleksandr Fedorov <aleksandr.fedorov@itglobal.com>
    MFC after:	1 week
    Sponsored by:	Mellanox Technologies

--- sys/netinet/in_mcast.c
+++ sys/netinet/in_mcast.c
@@ -2409,6 +2409,12 @@ inp_leave_group(struct inpcb *inp, struct sockopt *sopt)
 	if (is_final) {
 		ip_mfilter_remove(&imo->imo_head, imf);
 		imf_leave(imf);
+
+		/*
+		 * Give up the multicast address record to which
+		 * the membership points.
+		 */
+		(void) in_leavegroup_locked(imf->imf_inm, imf);
 	} else {
 		if (imf->imf_st[0] == MCAST_EXCLUDE) {
 			error = EADDRNOTAVAIL;
@@ -2463,14 +2469,8 @@ inp_leave_group(struct inpcb *inp, struct sockopt *sopt)
 out_inp_locked:
 	INP_WUNLOCK(inp);
 
-	if (is_final && imf) {
-		/*
-		 * Give up the multicast address record to which
-		 * the membership points.
-		 */
-		(void) in_leavegroup_locked(imf->imf_inm, imf);
+	if (is_final && imf)
 		ip_mfilter_free(imf);
-	}
 
 	IN_MULTI_UNLOCK();
 	return (error);
