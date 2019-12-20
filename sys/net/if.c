e8aee09c1d6d2e030d2aea2acc5999262d51e241
markj <markj@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-12-20 17:21:57 +0000

    Fix a memory leak in if_delgroups() introduced in r334118.
    PR:		242712
    Submitted by:	ghuckriede@blackberry.com
    MFC after:	3 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@355938 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net/if.c
+++ sys/net/if.c
@@ -1597,6 +1597,7 @@ if_delgroups(struct ifnet *ifp)
 								ifgl->ifgl_group);
 			free(ifgl->ifgl_group, M_TEMP);
 		}
+		free(ifgl, M_TEMP);
 		EVENTHANDLER_INVOKE(group_change_event, groupname);
 
 		IFNET_WLOCK();
