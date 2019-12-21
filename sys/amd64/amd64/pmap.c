50079417a5c28caeca3f423b981047db76e8f5cb
Alan Cox <alc@FreeBSD.org>
2019-12-21 22:32:24 +0000

    Micro-optimize the control flow in _pmap_unwire_ptp(), and eliminate unnecessary parentheses.
    Reviewed by:	kib, markj
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D22893

--- sys/amd64/amd64/pmap.c
+++ sys/amd64/amd64/pmap.c
@@ -3542,7 +3542,7 @@ _pmap_unwire_ptp(pmap_t pmap, vm_offset_t va, vm_page_t m, struct spglist *free)
 	/*
 	 * unmap the page table page
 	 */
-	if (m->pindex >= (NUPDE + NUPDPE)) {
+	if (m->pindex >= NUPDE + NUPDPE) {
 		/* PDP page */
 		pml4_entry_t *pml4;
 		pml4 = pmap_pml4e(pmap, va);
@@ -3569,8 +3569,7 @@ _pmap_unwire_ptp(pmap_t pmap, vm_offset_t va, vm_page_t m, struct spglist *free)
 
 		pdpg = PHYS_TO_VM_PAGE(*pmap_pdpe(pmap, va) & PG_FRAME);
 		pmap_unwire_ptp(pmap, va, pdpg, free);
-	}
-	if (m->pindex >= NUPDE && m->pindex < (NUPDE + NUPDPE)) {
+	} else if (m->pindex < NUPDE + NUPDPE) {
 		/* We just released a PD, unhold the matching PDP */
 		vm_page_t pdppg;
 
