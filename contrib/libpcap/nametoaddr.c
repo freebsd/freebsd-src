f4880aada21b66b3f4ccf0f6154fee732ebab6d0
cy <cy@FreeBSD.org>
2019-12-21 21:05:53 +0000

    MFV r355890:
    Fix libpcap issue #893: check for invalid IPv4 addresses.
    
    This fixes errors such as:
    
    tcpdump -i lagg0 net 999.999.999.999
    
    This was originally discovered on a Red Hat 7.7 server and verified
    to also be a bug on FreeBSD.
    
    Obtained from:	https://github.com/the-tcpdump-group/libpcap/commit/ \
    		07070918d5e81a515315b395f334e52589fe0fb
    Fixed by:	https://github.com/guyharris
    MFC after:	2 weeks

--- contrib/libpcap/nametoaddr.c
+++ contrib/libpcap/nametoaddr.c
@@ -653,8 +653,15 @@ __pcap_atoin(const char *s, bpf_u_int32 *addr)
 	len = 0;
 	for (;;) {
 		n = 0;
-		while (*s && *s != '.')
+		while (*s && *s != '.') {
+			if (n > 25) {
+				/* The result will be > 255 */
+				return -1;
+			}
 			n = n * 10 + *s++ - '0';
+		}
+		if (n > 255)
+			return -1;
 		*addr <<= 8;
 		*addr |= n & 0xff;
 		len += 8;
