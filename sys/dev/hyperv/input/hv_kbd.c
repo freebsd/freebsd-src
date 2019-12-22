93d368d89c617f5f2010eedb6da9593930fc3694
Kyle Evans <kevans@FreeBSD.org>
2019-12-22 16:04:12 +0000

    MFC r355794: keyboard switch definitions: standardize on c99 initializers

--- sys/dev/hyperv/input/hv_kbd.c
+++ sys/dev/hyperv/input/hv_kbd.c
@@ -446,25 +446,25 @@ hvkbd_read(keyboard_t *kbd, int wait)
 }
 
 static keyboard_switch_t hvkbdsw = {
-	hvkbd_probe,		/* not used */
-	hvkbd_init,
-	hvkbd_term,		/* not used */
-	hvkbd_intr,		/* not used */
-	hvkbd_test_if,		/* not used */
-	hvkbd_enable,
-	hvkbd_disable,
-	hvkbd_read,
-	hvkbd_check,
-	hvkbd_read_char,
-	hvkbd_check_char,
-	hvkbd_ioctl,
-	hvkbd_lock,		/* not used */
-	hvkbd_clear_state,
-	hvkbd_get_state,	/* not used */
-	hvkbd_set_state,	/* not used */
-	genkbd_get_fkeystr,
-	hvkbd_poll,
-	genkbd_diag,
+	.probe =	hvkbd_probe,		/* not used */
+	.init =		hvkbd_init,
+	.term =		hvkbd_term,		/* not used */
+	.intr =		hvkbd_intr,		/* not used */
+	.test_if =	hvkbd_test_if,		/* not used */
+	.enable =	hvkbd_enable,
+	.disable =	hvkbd_disable,
+	.read =		hvkbd_read,
+	.check =	hvkbd_check,
+	.read_char =	hvkbd_read_char,
+	.check_char =	hvkbd_check_char,
+	.ioctl =	hvkbd_ioctl,
+	.lock =		hvkbd_lock,		/* not used */
+	.clear_state =	hvkbd_clear_state,
+	.get_state =	hvkbd_get_state,	/* not used */
+	.set_state =	hvkbd_set_state,	/* not used */
+	.get_fkeystr =	genkbd_get_fkeystr,
+	.poll =		hvkbd_poll,
+	.diag =		genkbd_diag,
 };
 
 KEYBOARD_DRIVER(hvkbd, hvkbdsw, hvkbd_configure);
