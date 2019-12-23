2c96f04ddd9c80d88774fdf5875ccf16ca637e29
Kyle Evans <kevans@FreeBSD.org>
2019-12-23 21:46:48 +0000

    terasic_mtl: kill off final dummy keyboard driver
    The rest were removed in r355936, which speculated that the cause of this
    phenomenon was due to an inability to have an empty linker set. The comment
    included with this one shows that this was, in fact, not the reason.
    
    Regardless, syscons no longer seems to have an issue with not having any
    keyboard drivers and in-fact ignores the keyboard probe anyways.
    
    X-MFC-With: r355936

--- sys/dev/terasic/mtl/terasic_mtl_syscons.c
+++ sys/dev/terasic/mtl/terasic_mtl_syscons.c
@@ -406,22 +406,6 @@ terasic_mtl_vidsw_putm(video_adapter_t *adp, int x, int y,
 	return (ENODEV);
 }
 
-/*
- * XXXRW: For historical reasons, syscons can't register video consoles
- * without a keyboard implementation.  Provide a dummy.
- */
-static keyboard_switch_t	terasic_mtl_keyboard_switch;
-
-static int
-terasic_mtl_kbd_configure(int flags)
-{
-
-	return (0);
-}
-
-KEYBOARD_DRIVER(mtl_kbd, terasic_mtl_keyboard_switch,
-    terasic_mtl_kbd_configure);
-
 int
 terasic_mtl_syscons_attach(struct terasic_mtl_softc *sc)
 {
