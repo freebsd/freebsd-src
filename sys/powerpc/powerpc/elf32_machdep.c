143bcc8cf4a313de24b580b3da5570a51d2a312f
jhibbits <jhibbits@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2019-12-26 22:47:24 +0000

    powerpc: Move ppc32_runtime_resolve() declaration to the right place
    The function is only used in 32-bit kernels, so move the declaration to
    !powerpc64.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@356102 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/powerpc/powerpc/elf32_machdep.c
+++ sys/powerpc/powerpc/elf32_machdep.c
@@ -63,7 +63,6 @@
 
 extern const char *freebsd32_syscallnames[];
 static void ppc32_fixlimit(struct rlimit *rl, int which);
-static void ppc32_runtime_resolve(void);
 
 static SYSCTL_NODE(_compat, OID_AUTO, ppc32, CTLFLAG_RW, 0, "32-bit mode");
 
@@ -75,6 +74,8 @@ SYSCTL_ULONG(_compat_ppc32, OID_AUTO, maxdsiz, CTLFLAG_RWTUN, &ppc32_maxdsiz,
 u_long ppc32_maxssiz = PPC32_MAXSSIZ;
 SYSCTL_ULONG(_compat_ppc32, OID_AUTO, maxssiz, CTLFLAG_RWTUN, &ppc32_maxssiz,
              0, "");
+#else
+static void ppc32_runtime_resolve(void);
 #endif
 
 struct sysentvec elf32_freebsd_sysvec = {
