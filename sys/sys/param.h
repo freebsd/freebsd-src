adea0d6368dd6eb42b029ed35555ac5d7a664c2e
Justin Hibbits <jhibbits@FreeBSD.org>
2019-12-27 04:07:03 +0000

    Eliminate the last MI difference in AT_* definitions (for powerpc).
    Summary:
    As a transition aide, implement an alternative elfN_freebsd_fixup which
    is called for old powerpc binaries.  Similarly, add a translation to rtld to
    convert old values to new ones (as expected by a new rtld).
    
    Translation of old<->new values  is incomplete, but sufficient to allow an
    installworld of a new userspace from an old one when a new kernel is running.
    
    Test Plan:
    Someone needs to see how a new kernel/rtld/libc works with an old
    binary.  If if works we can probalby ship this.  If not we probalby need
    some more compat bits.
    
    Submitted by:	brooks
    Reviewed by:	jhibbits
    Differential Revision:	https://reviews.freebsd.org/D20799

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -60,7 +60,7 @@
  *		in the range 5 to 9.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1300069	/* Master, propagated to newvers */
+#define __FreeBSD_version 1300070	/* Master, propagated to newvers */
 
 /*
  * __FreeBSD_kernel__ indicates that this system uses the kernel of FreeBSD,
@@ -90,6 +90,7 @@
 #define	P_OSREL_VMTOTAL64		1200054
 #define	P_OSREL_CK_SUPERBLOCK		1300000
 #define	P_OSREL_CK_INODE		1300005
+#define	P_OSREL_POWERPC_NEW_AUX_ARGS	1300070
 
 #define	P_OSREL_MAJOR(x)		((x) / 100000)
 #endif
