351d0fa6dff9f01e9236d922bba6b67c1dcefc53
Alexander Motin <mav@FreeBSD.org>
2019-12-28 17:52:53 +0000

    Fix GEOM_GATE orphanization.
    Previous code closed and destroyed direct read consumer even with I/O still
    in progress.  This patch adds locking and request counting to postpone the
    close till the last of running requests completes.
    
    MFC after:	2 weeks
    Sponsored by:	iXsystems, Inc.

--- sys/geom/gate/g_gate.h
+++ sys/geom/gate/g_gate.h
@@ -91,16 +91,15 @@ struct g_gate_softc {
 	uint32_t		 sc_queue_count;	/* P: sc_queue_mtx */
 	uint32_t		 sc_queue_size;		/* P: (read-only) */
 	u_int			 sc_timeout;		/* P: (read-only) */
-	struct g_consumer	*sc_readcons;		/* P: XXX */
-	off_t			 sc_readoffset;		/* P: XXX */
+	struct g_consumer	*sc_readcons;		/* P: sc_read_mtx */
+	off_t			 sc_readoffset;		/* P: sc_read_mtx */
 	struct callout		 sc_callout;		/* P: (modified only
 							       from callout
 							       thread) */
-	uintptr_t		 sc_seq;		/* P: (modified only
-							       from g_down
-							       thread) */
+	uintptr_t		 sc_seq;		/* P: sc_queue_mtx */
 	LIST_ENTRY(g_gate_softc) sc_next;		/* P: g_gate_list_mtx */
 	char			 sc_info[G_GATE_INFOSIZE]; /* P: (read-only) */
+	struct mtx		 sc_read_mtx;
 };
 
 #define	G_GATE_DEBUG(lvl, ...) \
