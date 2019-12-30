8d00ce82bf9e0f9fb810937664d7c1b6e84fde4f
br <br@FreeBSD.org>
2019-12-30 17:18:50 +0000

    Don't hard-code field offsets of struct riscv_bootparams.
    Submitted by:	James Clarke <jrtc27@jrtc27.com>
    Sponsored by:	DARPA, AFRL
    Differential Revision:	https://reviews.freebsd.org/D22970

--- sys/riscv/riscv/genassym.c
+++ sys/riscv/riscv/genassym.c
@@ -101,3 +101,9 @@ ASSYM(TF_SCAUSE, offsetof(struct trapframe, tf_scause));
 ASSYM(TF_SSTATUS, offsetof(struct trapframe, tf_sstatus));
 
 ASSYM(RISCV_BOOTPARAMS_SIZE, sizeof(struct riscv_bootparams));
+ASSYM(RISCV_BOOTPARAMS_KERN_L1PT, offsetof(struct riscv_bootparams, kern_l1pt));
+ASSYM(RISCV_BOOTPARAMS_KERN_PHYS, offsetof(struct riscv_bootparams, kern_phys));
+ASSYM(RISCV_BOOTPARAMS_KERN_STACK, offsetof(struct riscv_bootparams,
+    kern_stack));
+ASSYM(RISCV_BOOTPARAMS_DTBP_VIRT, offsetof(struct riscv_bootparams, dtbp_virt));
+ASSYM(RISCV_BOOTPARAMS_DTBP_PHYS, offsetof(struct riscv_bootparams, dtbp_phys));
