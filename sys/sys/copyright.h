29e12bc777f58fb2d830447dac370d9e56c32afd
Glen Barber <gjb@FreeBSD.org>
2019-12-31 16:01:36 +0000

    Happy New Year 2020!
    MFC after:	1 minute
    Sponsored by:	Rubicon Communications, LLC (netgate.com)

--- sys/sys/copyright.h
+++ sys/sys/copyright.h
@@ -1,7 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
- * Copyright (C) 1992-2018 The FreeBSD Project. All rights reserved.
+ * Copyright (C) 1992-2020 The FreeBSD Project. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -36,7 +36,7 @@
 
 /* FreeBSD */
 #define COPYRIGHT_FreeBSD \
-	"Copyright (c) 1992-2019 The FreeBSD Project.\n"
+	"Copyright (c) 1992-2020 The FreeBSD Project.\n"
 
 /* Foundation */
 #define	TRADEMARK_Foundation \
