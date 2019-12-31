ca8fe5f7adc2e5c70e956744c040764104651bae
rmacklem <rmacklem@FreeBSD.org>
2019-12-31 05:39:27 +0000

    Switch r356210 to use gone_in() instead of printf().
    Suggested by:	cem

--- sys/nfs/nfs_lock.c
+++ sys/nfs/nfs_lock.c
@@ -89,8 +89,7 @@ nfslock_open(struct cdev *dev, int oflags, int devtype, struct thread *td)
 {
 	int error;
 
-	printf("WARNING: uses Giant and will be removed before FreeBSD 13\n"
-	    "\tuse the kernel NFSLOCKD/nfslockd.ko\n");
+	gone_in(13, "uses Giant; replace with NFSLOCKD/nfslockd.ko");
 	error = priv_check(td, PRIV_NFS_LOCKD);
 	if (error)
 		return (error);
