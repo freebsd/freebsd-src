ec7ee6ebef2756335e9568e9fbd13832be192ae0
emaste <emaste@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-01-08 20:45:18 +0000

    src.opts.mk: enable LLVM_UNWIND by default for all archs
    Only sparc64 did not enable LLVM_LIBUNWIND. After r356513 LLVM_LIBUNWIND
    should at least build on sparc64. The old DWARF unwinder will be removed
    along with GCC 4.2.1 in the near future, so switch sparc64 to use LLVM's
    unwinder in advance of the removal.  Someone with access to the obsolete
    sparc64 hardware supported by FreeBSD will have to test, and investigate
    any failures. I will gladly help, but I don't have any suitable hardware
    myself.
    
    PR:		233405
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@356514 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -129,6 +129,7 @@ __DEFAULT_YES_OPTIONS = \
     LIBPTHREAD \
     LIBTHR \
     LLVM_COV \
+    LLVM_LIBUNWIND \
     LLVM_TARGET_ALL \
     LOADER_GELI \
     LOADER_LUA \
@@ -315,11 +316,6 @@ BROKEN_OPTIONS+=BINUTILS BINUTILS_BOOTSTRAP GCC GCC_BOOTSTRAP GDB
 .endif
 .if ${__T:Mriscv*} != ""
 BROKEN_OPTIONS+=OFED
-.endif
-.if ${__T} != "sparc64"
-__DEFAULT_YES_OPTIONS+=LLVM_LIBUNWIND
-.else
-__DEFAULT_NO_OPTIONS+=LLVM_LIBUNWIND
 .endif
 .if ${__TT} != "mips" && ${__T} != "powerpc" && ${__T} != "powerpcspe" && \
     ${__T} != "sparc64"
