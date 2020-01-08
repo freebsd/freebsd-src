4372a4e795fb82e77fe11b6988e0a46bd4a42490
Jung-uk Kim <jkim@FreeBSD.org>
2020-01-08 17:30:14 +0000

    MFC:	r354056
    Catch up with ACPICA 20191018.

--- usr.sbin/bhyve/pci_emul.c
+++ usr.sbin/bhyve/pci_emul.c
@@ -1268,7 +1268,6 @@ pci_bus_write_dsdt(int bus)
 	dsdt_line("  Device (PC%02X)", bus);
 	dsdt_line("  {");
 	dsdt_line("    Name (_HID, EisaId (\"PNP0A03\"))");
-	dsdt_line("    Name (_ADR, Zero)");
 
 	dsdt_line("    Method (_BBN, 0, NotSerialized)");
 	dsdt_line("    {");
