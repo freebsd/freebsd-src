68cd0e8de13936d52289d63eb86b82e328cfdeca
emaste <emaste@FreeBSD.org>
2020-01-10 22:00:39 +0000

    src.opts.mk: force KERBEROS_SUPPORT off where KERBEROS forced off
    Explicitly setting WITHOUT_KERBEROS implies WITHOUT_KERBEROS_SUPPORT,
    but previously other cases that forced KERBEROS off (such as
    WITHOUT_CRYPT) did not also set KERBEROS_SUPPORT off.  Because the
    _SUPPORT dependent options (KERBEROS/KERBEROS_SUPPORT) are processed
    before other dependencies (CRYPT/KERBEROS) it's not easy to make this
    happen automatically.  Instead just explicitly set KERBEROS_SUPPORT
    off where we set KERBEROS off.
    
    Reported by:	Michael Dexter's Build Option Survey run

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -459,6 +459,7 @@ MK_CTF:=	no
 MK_OPENSSL:=	no
 MK_OPENSSH:=	no
 MK_KERBEROS:=	no
+MK_KERBEROS_SUPPORT:=	no
 .endif
 
 .if ${MK_CXX} == "no"
@@ -498,6 +499,7 @@ MK_NLS_CATALOGS:= no
 .if ${MK_OPENSSL} == "no"
 MK_OPENSSH:=	no
 MK_KERBEROS:=	no
+MK_KERBEROS_SUPPORT:=	no
 MK_LDNS:=	no
 .endif
 
