80126e53e98e05afb0cddab91d58627584214b30
manu <manu@FreeBSD.org>
2020-01-10 18:52:14 +0000

    twsi: Rework how we handle the i2c messages
    We use to handle each message separately in i2c_transfer but that cannot
    work with message with NOSTOP as it confuses the controller that we disable
    the interrupts and start a new message.
    Handle every message in the interrupt handler and fire a new start condition
    if the previous message have NOSTOP, the controller understand this as a
    repeated start.
    This fixes booting on Allwinner A10/A20 platform where before the i2c controller
    used to write 0 to the PMIC register that control the regulators as it though that
    this was the continuation of the write message.
    
    Tested on:   A20 BananaPi, Cubieboard 1 (kevans)
    Reported by:	kevans
    MFC after:	1 month

--- sys/dev/iicbus/twsi/twsi.h
+++ sys/dev/iicbus/twsi/twsi.h
@@ -57,7 +57,9 @@ struct twsi_softc {
 	void *			intrhand;
 	bool			have_intr;
 
-	struct iic_msg		*msg;
+	struct iic_msg		*msgs;
+	uint32_t		nmsgs;
+	uint32_t		msg_idx;
 	uint16_t		sent_bytes;
 	uint16_t		recv_bytes;
 	int			transfer;
