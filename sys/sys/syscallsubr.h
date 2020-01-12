b773be9fe6d42e5f57386dba7aa3c29cf91f2ba5
trasz <trasz@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-01-12 14:25:44 +0000

    dd kern_getpriority(), make Linuxulator use it.
    Reviewed by:	kib, emaste
    MFC after:	2 weeks
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D22842
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@356659 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/syscallsubr.h
+++ sys/sys/syscallsubr.h
@@ -141,6 +141,7 @@ int	kern_getitimer(struct thread *, u_int, struct itimerval *);
 int	kern_getppid(struct thread *);
 int	kern_getpeername(struct thread *td, int fd, struct sockaddr **sa,
 	    socklen_t *alen);
+int	kern_getpriority(struct thread *td, int which, int who);
 int	kern_getrusage(struct thread *td, int who, struct rusage *rup);
 int	kern_getsid(struct thread *td, pid_t pid);
 int	kern_getsockname(struct thread *td, int fd, struct sockaddr **sa,
