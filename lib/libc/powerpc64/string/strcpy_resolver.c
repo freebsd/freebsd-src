7691671fb87eb31749024140388356291f139578
luporl <luporl@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-01-15 19:46:01 +0000

    [PPC64] strcpy optimization
    Assembly optimization of strcpy for PowerPC64, using double words
    instead of bytes to copy strings.
    
    Submitted by:	Leonardo Bianconi <leonardo.bianconi_eldorado.org.br> (original version)
    Reviewed by:	jhibbits
    Differential Revision:	https://reviews.freebsd.org/D15368
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@356765 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libc/powerpc64/string/strcpy_resolver.c
+++ lib/libc/powerpc64/string/strcpy_resolver.c
@@ -1,8 +1,5 @@
 /*-
- * SPDX-License-Identifier: BSD-3-Clause
- *
- * Copyright (c) 1988, 1993
- *	The Regents of the University of California.  All rights reserved.
+ * Copyright (c) 2019 Leandro Lupori
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -12,14 +9,13 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
+ * 3. Neither the name of the author nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -29,28 +25,23 @@
  * SUCH DAMAGE.
  */
 
-#if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strcpy.c	8.1 (Berkeley) 6/4/93";
-#endif /* LIBC_SCCS and not lint */
+
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include <string.h>
-
-#ifdef WEAK_STRCPY
-__weak_reference(__strcpy, strcpy);
-#endif
+#include <machine/cpu.h>
+#include <machine/ifunc.h>
 
 char *
-#ifdef WEAK_STRCPY
-__strcpy
-#else
-strcpy
-#endif
-(char * __restrict to, const char * __restrict from)
-{
-	char *save = to;
+__strcpy_arch_2_05(char * restrict dst, const char * restrict src);
 
-	for (; (*to = *from); ++from, ++to);
-	return(save);
+char *
+__strcpy(char * restrict dst, const char * restrict src);
+
+DEFINE_UIFUNC(, char *, strcpy, (char * restrict, const char * restrict))
+{
+	if (cpu_features & PPC_FEATURE_ARCH_2_05)
+		return (__strcpy_arch_2_05);
+	else
+		return (__strcpy);
 }
