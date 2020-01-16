e33f40c5d4b9ffa2f3f433ea982b119652eaab4c
tuexen <tuexen@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-01-16 17:15:06 +0000

    Remove debug code not needed anymore.
    Submitted by:		Richard Scheffenegger
    Reviewed by:		tuexen@
    MFC after:		1 week
    Differential Revision:	https://reviews.freebsd.org/D23208
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@356796 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/tcp_sack.c
+++ sys/netinet/tcp_sack.c
@@ -165,11 +165,6 @@ tcp_update_dsack_list(struct tcpcb *tp, tcp_seq rcv_start, tcp_seq rcv_end)
 
 	KASSERT(SEQ_LT(rcv_start, rcv_end), ("rcv_start < rcv_end"));
 
-	if (tp->t_inpcb->inp_socket->so_options & SO_DEBUG) {
-		log(LOG_DEBUG, "\nDSACK update: %d..%d, rcv_nxt: %u\n",
-		rcv_start, rcv_end, tp->rcv_nxt);
-	}
-
 	if (SEQ_LT(rcv_end, tp->rcv_nxt) ||
 	    ((rcv_end == tp->rcv_nxt) &&
 	     (tp->rcv_numsacks > 0 ) &&
