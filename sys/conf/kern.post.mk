739a124304d0027eb934240a123e64d37fe93fb3
Yoshihiro Takahashi <nyan@FreeBSD.org>
2020-01-31 12:38:53 +0000

    MFC r357043: Fix kernel-tags target.
    >  - A depend-file is broken up into .depend.*.o files. [1]
    >  - Fix an assembly file support.
    >
    >  PR:           241746
    >  Submitted by: leres [1]

--- sys/conf/kern.post.mk
+++ sys/conf/kern.post.mk
@@ -355,7 +355,8 @@ kernel-cleandepend: .PHONY
 	rm -f .depend .depend.* ${_ILINKS}
 
 kernel-tags:
-	@[ -f .depend ] || { echo "you must make depend first"; exit 1; }
+	@ls .depend.* > /dev/null 2>&1 || \
+	    { echo "you must make depend first"; exit 1; }
 	sh $S/conf/systags.sh
 
 kernel-install: .PHONY
