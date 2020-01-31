0c882841a587aa7d32dffe99029f69a72898c4bf
jeff <jeff@FreeBSD.org>
2020-01-31 02:08:09 +0000

    Don't use "All rights reserved" in new copyrights.
    Requested by:	rgrimes

--- sys/sys/smr.h
+++ sys/sys/smr.h
@@ -1,8 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
- * Copyright (c) 2019,2020 Jeffrey Roberson <jeff@FreeBSD.org>
- * All rights reserved.
+ * Copyright (c) 2019, 2020 Jeffrey Roberson <jeff@FreeBSD.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
