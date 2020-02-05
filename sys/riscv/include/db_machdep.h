e909519fb40629fecd47b6c51fa048f15d98eae9
jhb <jhb@FreeBSD.org>
2020-02-05 20:06:35 +0000

    Read the breakpoint instruction to determine its length in BKPT_SKIP.
    This fixes continuing from debug.kdb.enter=1 after enabling the use of
    compressed instructions since the compiler can emit the two byte
    c.ebreak instead of the 4 byte ebreak.
    
    Reviewed by:	br
    MFC after:	1 week
    Sponsored by:	DARPA
    Differential Revision:	https://reviews.freebsd.org/D23507

--- sys/riscv/include/db_machdep.h
+++ sys/riscv/include/db_machdep.h
@@ -53,8 +53,14 @@ typedef long		db_expr_t;
 #define	BKPT_SIZE	(INSN_SIZE)
 #define	BKPT_SET(inst)	(BKPT_INST)
 
-#define	BKPT_SKIP do {				\
-	kdb_frame->tf_sepc += BKPT_SIZE;	\
+#define	BKPT_SKIP do {							\
+	uint32_t _instr;						\
+									\
+	_instr = db_get_value(PC_REGS(), sizeof(uint32_t), FALSE);	\
+	if ((_instr & 0x3) == 0x3)					\
+		kdb_frame->tf_sepc += 4;	/* ebreak */		\
+	else								\
+		kdb_frame->tf_sepc += 2;	/* c.ebreak */		\
 } while (0)
 
 #define	db_clear_single_step	kdb_cpu_clear_singlestep
