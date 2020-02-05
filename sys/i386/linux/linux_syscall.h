3e153f78c17816881cf5c60ccd272936a4cdf593
emaste <emaste@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-02-05 16:54:16 +0000

    regen linuxulator sysent after r357577
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@357578 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/i386/linux/linux_syscall.h
+++ sys/i386/linux/linux_syscall.h
@@ -222,6 +222,7 @@
 #define	LINUX_SYS_linux_lremovexattr	236
 #define	LINUX_SYS_linux_fremovexattr	237
 #define	LINUX_SYS_linux_tkill	238
+#define	LINUX_SYS_linux_sendfile64	239
 #define	LINUX_SYS_linux_sys_futex	240
 #define	LINUX_SYS_linux_sched_setaffinity	241
 #define	LINUX_SYS_linux_sched_getaffinity	242
