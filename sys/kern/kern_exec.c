146fc63fce9b46ab77ba7959cf69429d9bd88d2a
Konstantin Belousov <kib@FreeBSD.org>
2020-02-09 11:53:12 +0000

    Add a way to manage thread signal mask using shared word, instead of syscall.
    A new syscall sigfastblock(2) is added which registers a uint32_t
    variable as containing the count of blocks for signal delivery.  Its
    content is read by kernel on each syscall entry and on AST processing,
    non-zero count of blocks is interpreted same as the signal mask
    blocking all signals.
    
    The biggest downside of the feature that I see is that memory
    corruption that affects the registered fast sigblock location, would
    cause quite strange application misbehavior. For instance, the process
    would be immune to ^C (but killable by SIGKILL).
    
    With consumers (rtld and libthr added), benchmarks do not show a
    slow-down of the syscalls in micro-measurements, and macro benchmarks
    like buildworld do not demonstrate a difference. Part of the reason is
    that buildworld time is dominated by compiler, and clang already links
    to libthr. On the other hand, small utilities typically used by shell
    scripts have the total number of syscalls cut by half.
    
    The syscall is not exported from the stable libc version namespace on
    purpose.  It is intended to be used only by our C runtime
    implementation internals.
    
    Tested by:	pho
    Disscussed with:	cem, emaste, jilles
    Sponsored by:	The FreeBSD Foundation
    Differential revision:	https://reviews.freebsd.org/D12773

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -1025,6 +1025,7 @@ exec_new_vmspace(struct image_params *imgp, struct sysentvec *sv)
 	int error;
 	struct proc *p = imgp->proc;
 	struct vmspace *vmspace = p->p_vmspace;
+	struct thread *td = curthread;
 	vm_object_t obj;
 	struct rlimit rlim_stack;
 	vm_offset_t sv_minuser, stack_addr;
@@ -1033,6 +1034,10 @@ exec_new_vmspace(struct image_params *imgp, struct sysentvec *sv)
 
 	imgp->vmspace_destroyed = 1;
 	imgp->sysent = sv;
+
+	td->td_pflags &= ~TDP_SIGFASTBLOCK;
+	td->td_sigblock_ptr = NULL;
+	td->td_sigblock_val = 0;
 
 	/* May be called with Giant held */
 	EVENTHANDLER_DIRECT_INVOKE(process_exec, p, imgp);
