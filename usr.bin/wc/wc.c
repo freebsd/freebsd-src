f1c3417c1030dc274bb2f7e8062d63ff90508390
Kyle Evans <kevans@FreeBSD.org>
2020-02-09 19:20:20 +0000

    MFC r357572: wc(1): account for possibility of file == NULL
    file could reasonably be NULL here if we we're using stdin. Albeit less
    likely in normal usage, one could actually hit either of these warnings on
    stdin.

--- usr.bin/wc/wc.c
+++ usr.bin/wc/wc.c
@@ -218,7 +218,7 @@ cnt(const char *file)
 	 */
 	if (doline == 0 && dolongline == 0) {
 		if (fstat(fd, &sb)) {
-			xo_warn("%s: fstat", file);
+			xo_warn("%s: fstat", file != NULL ? file : "stdin");
 			(void)close(fd);
 			return (1);
 		}
@@ -239,7 +239,7 @@ cnt(const char *file)
 	 */
 	while ((len = read(fd, buf, MAXBSIZE))) {
 		if (len == -1) {
-			xo_warn("%s: read", file);
+			xo_warn("%s: read", file != NULL ? file : "stdin");
 			(void)close(fd);
 			return (1);
 		}
