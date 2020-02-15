bce8dee0c44d136815935b9005debd065dc088b9
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-02-15 23:19:23 +0000

    sem_remove(): fix the loop that compacts sem array on semaphores removal.
    As written now, it copies random kernel memory from beyond the bounds
    of the array.
    
    Reported and tested by:	pho
    Reviewed by:	markj
    Sponsored by:	The FreeBSD Foundation (kib)
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D23694
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@357984 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/sysv_sem.c
+++ sys/kern/sysv_sem.c
@@ -584,8 +584,9 @@ sem_remove(int semidx, struct ucred *cred)
 		    sema[i].u.__sem_base > semakptr->u.__sem_base)
 			mtx_lock_flags(&sema_mtx[i], LOP_DUPOK);
 	}
-	for (i = semakptr->u.__sem_base - sem; i < semtot; i++)
-		sem[i] = sem[i + semakptr->u.sem_nsems];
+	for (i = semakptr->u.__sem_base - sem + semakptr->u.sem_nsems;
+	    i < semtot; i++)
+		sem[i - semakptr->u.sem_nsems] = sem[i];
 	for (i = 0; i < seminfo.semmni; i++) {
 		if ((sema[i].u.sem_perm.mode & SEM_ALLOC) &&
 		    sema[i].u.__sem_base > semakptr->u.__sem_base) {
