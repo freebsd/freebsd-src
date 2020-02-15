06257a71aa94cef6d4682a74d616cd427146ee92
Conrad Meyer <cem@FreeBSD.org>
2020-02-15 19:13:37 +0000

    random(6): Re-add undocumented support for floating point denominators
    And document it in the manual page.
    
    PR:		244139
    Submitted by:	Keith White <kwhite AT site.uottawa.ca> (earlier version)

--- usr.bin/random/randomize_fd.h
+++ usr.bin/random/randomize_fd.h
@@ -42,4 +42,40 @@ struct rand_node {
 
 int randomize_fd(int fd, int type, int unique, double denom);
 
+/*
+ * Generates a random number uniformly in the range [0.0, 1.0).
+ */
+static inline double
+random_unit_float(void)
+{
+	static const uint64_t denom = (1ull << 53);
+	static const uint64_t mask = denom - 1;
+
+	uint64_t rand64;
+
+	/*
+	 * arc4random_buf(...) in this use generates integer outputs in [0,
+	 * UINT64_MAX].
+	 *
+	 * The double mantissa only has 53 bits, so we uniformly mask off the
+	 * high 11 bits and then floating-point divide by 2^53 to achieve a
+	 * result in [0, 1).
+	 *
+	 * We are not allowed to emit 1.0, so denom must be one greater than
+	 * the possible range of the preceeding step.
+	 */
+	arc4random_buf(&rand64, sizeof(rand64));
+	rand64 &= mask;
+	return ((double)rand64 / denom);
+}
+
+/*
+ * Returns true with probability 1 / denom (a floating point number >= 1).
+ * Otherwise, returns false.
+ */
+static inline bool
+random_uniform_denom(double denom)
+{
+	return ((uint64_t)(denom * random_unit_float()) == 0);
+}
 #endif
