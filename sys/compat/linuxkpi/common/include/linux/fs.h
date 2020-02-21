1179b649cf47eb40fcb48e2b2417de4f93e82976
Emmanuel Vadot <manu@FreeBSD.org>
2020-02-21 09:28:45 +0000

    linuxkpi: Move shmem related functions in it's own file
    For drmkpi (D23085) we don't want the Linux struct file as we don't emulate
    everything. Also the prototypes should be in shmem_fs.h to have 100%
    compatibility with Linux.
    
    Reviewed by:	hselasky
    MFC after:	Maybe
    Differential Revision:	https://reviews.freebsd.org/D23764

--- sys/compat/linuxkpi/common/include/linux/fs.h
+++ sys/compat/linuxkpi/common/include/linux/fs.h
@@ -302,25 +302,4 @@ call_mmap(struct linux_file *file, struct vm_area_struct *vma)
 	return (file->f_op->mmap(file, vma));
 }
 
-/* Shared memory support */
-unsigned long linux_invalidate_mapping_pages(vm_object_t, pgoff_t, pgoff_t);
-struct page *linux_shmem_read_mapping_page_gfp(vm_object_t, int, gfp_t);
-struct linux_file *linux_shmem_file_setup(const char *, loff_t, unsigned long);
-void linux_shmem_truncate_range(vm_object_t, loff_t, loff_t);
-
-#define	invalidate_mapping_pages(...) \
-  linux_invalidate_mapping_pages(__VA_ARGS__)
-
-#define	shmem_read_mapping_page(...) \
-  linux_shmem_read_mapping_page_gfp(__VA_ARGS__, 0)
-
-#define	shmem_read_mapping_page_gfp(...) \
-  linux_shmem_read_mapping_page_gfp(__VA_ARGS__)
-
-#define	shmem_file_setup(...) \
-  linux_shmem_file_setup(__VA_ARGS__)
-
-#define	shmem_truncate_range(...) \
-  linux_shmem_truncate_range(__VA_ARGS__)
-
 #endif /* _LINUX_FS_H_ */
