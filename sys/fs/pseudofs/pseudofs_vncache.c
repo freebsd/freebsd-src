468ad59c8a01fc85b86c6a1afe2b6d64e47ea57f
kaktus <kaktus@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-02-24 10:37:56 +0000

    Mark more nodes as CTLFLAG_MPSAFE or CTLFLAG_NEEDGIANT (10 of many)
    r357614 added CTLFLAG_NEEDGIANT to make it easier to find nodes that are
    still not MPSAFE (or already are but aren’t properly marked).
    Use it in preparation for a general review of all nodes.
    
    This is non-functional change that adds annotations to SYSCTL_NODE and
    SYSCTL_PROC nodes using one of the soon-to-be-required flags.
    
    Approved by:	kib (mentor, blanket)
    Differential Revision:	https://reviews.freebsd.org/D23629
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@358277 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/fs/pseudofs/pseudofs_vncache.c
+++ sys/fs/pseudofs/pseudofs_vncache.c
@@ -54,7 +54,7 @@ static eventhandler_tag pfs_exit_tag;
 static void pfs_exit(void *arg, struct proc *p);
 static void pfs_purge_all(void);
 
-static SYSCTL_NODE(_vfs_pfs, OID_AUTO, vncache, CTLFLAG_RW, 0,
+static SYSCTL_NODE(_vfs_pfs, OID_AUTO, vncache, CTLFLAG_RW | CTLFLAG_MPSAFE, 0,
     "pseudofs vnode cache");
 
 static int pfs_vncache_entries;
