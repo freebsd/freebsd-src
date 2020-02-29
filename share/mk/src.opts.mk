52fcc2c3146b5875a1ec0fb7460dd382ab0e5b3b
emaste <emaste@FreeBSD.org>
2020-02-29 03:25:51 +0000

    remove GCC 4.2.1 build infrastructure
    As described in Warner's email message[1] to the FreeBSD-arch mailing
    list we have reached GCC 4.2.1's retirement date.  At this time all
    supported architectures either use in-tree Clang, or rely on external
    toolchain (i.e., a contemporary GCC version from ports).
    
    GCC 4.2.1 was released July 18, 2007 and was imported into FreeBSD later
    that year, in r171825.  GCC has served us well, but version 4.2.1 is
    obsolete and not used by default on any architecture in FreeBSD.  It
    does not support modern C and does not support arm64 or RISC-V.
    
    Thanks to everyone responsible for maintaining, updating, and testing
    GCC in the FreeBSD base system over the years.
    
    So long, and thanks for all the fish.
    
    [1] https://lists.freebsd.org/pipermail/freebsd-arch/2020-January/019823.html
    
    PR:		228919
    Reviewed by:	brooks, imp
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D23124

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -199,10 +199,6 @@ __DEFAULT_NO_OPTIONS = \
     CLANG_EXTRAS \
     DTRACE_TESTS \
     EXPERIMENTAL \
-    GCC \
-    GCC_BOOTSTRAP \
-    GCOV \
-    GNUCXX \
     GNU_GREP_COMPAT \
     GPL_DTC \
     HESIOD \
@@ -311,7 +307,7 @@ __DEFAULT_NO_OPTIONS+=CLANG CLANG_BOOTSTRAP CLANG_IS_CC LLD
 .endif
 # In-tree binutils/gcc are older versions without modern architecture support.
 .if ${__T} == "aarch64" || ${__T:Mriscv*} != ""
-BROKEN_OPTIONS+=BINUTILS BINUTILS_BOOTSTRAP GCC GCC_BOOTSTRAP GDB
+BROKEN_OPTIONS+=BINUTILS BINUTILS_BOOTSTRAP GDB
 .endif
 .if ${__T} == "amd64" || ${__T} == "i386" || ${__T:Mpowerpc*}
 __DEFAULT_YES_OPTIONS+=BINUTILS_BOOTSTRAP
@@ -458,7 +454,6 @@ MK_KERBEROS_SUPPORT:=	no
 
 .if ${MK_CXX} == "no"
 MK_CLANG:=	no
-MK_GNUCXX:=	no
 MK_GOOGLETEST:=	no
 MK_TESTS:=	no
 .endif
@@ -529,14 +524,12 @@ MK_ZONEINFO_OLD_TIMEZONES_SUPPORT:= no
 MK_BINUTILS_BOOTSTRAP:= no
 MK_CLANG_BOOTSTRAP:= no
 MK_ELFTOOLCHAIN_BOOTSTRAP:= no
-MK_GCC_BOOTSTRAP:= no
 MK_LLD_BOOTSTRAP:= no
 .endif
 
 .if ${MK_TOOLCHAIN} == "no"
 MK_BINUTILS:=	no
 MK_CLANG:=	no
-MK_GCC:=	no
 MK_GDB:=	no
 MK_INCLUDES:=	no
 MK_LLD:=	no
@@ -574,17 +567,6 @@ MK_${vv:H}:=	${MK_${vv:T}}
 
 .if !${COMPILER_FEATURES:Mc++11}
 MK_LLDB:=	no
-.endif
-
-# gcc 4.8 and newer supports libc++, so suppress gnuc++ in that case.
-# while in theory we could build it with that, we don't want to do
-# that since it creates too much confusion for too little gain.
-# XXX: This is incomplete and needs X_COMPILER_TYPE/VERSION checks too
-#      to prevent Makefile.inc1 from bootstrapping unneeded dependencies
-#      and to support 'make delete-old' when supplying an external toolchain.
-.if ${COMPILER_TYPE} == "gcc" && ${COMPILER_VERSION} >= 40800
-MK_GNUCXX:=no
-MK_GCC:=no
 .endif
 
 .endif #  !target(__<src.opts.mk>__)
