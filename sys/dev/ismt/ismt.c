15bba3dc55b3e0f57ab88eeb87b7fd8f3fe552eb
jhibbits <jhibbits@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-03-03 22:23:56 +0000

    Add Atom C3000 (Denverton) SMT PCI ID
    MFC after:	3 days
    Sponsored by:	Juniper Networks, Inc
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@358595 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ismt/ismt.c
+++ sys/dev/ismt/ismt.c
@@ -717,6 +717,7 @@ fail:
 #define ID_INTEL_S1200_SMT0		0x0c598086
 #define ID_INTEL_S1200_SMT1		0x0c5a8086
 #define ID_INTEL_C2000_SMT		0x1f158086
+#define ID_INTEL_C3000_SMT		0x19ac8086
 
 static int
 ismt_probe(device_t dev)
@@ -732,6 +733,9 @@ ismt_probe(device_t dev)
 		break;
 	case ID_INTEL_C2000_SMT:
 		desc = "Atom Processor C2000 SMBus 2.0";
+		break;
+	case ID_INTEL_C3000_SMT:
+		desc = "Atom Processor C3000 SMBus 2.0";
 		break;
 	default:
 		return (ENXIO);
