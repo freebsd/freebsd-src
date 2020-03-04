855036971475163960287d1a4c9c637c00ca46d0
cy <cy@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-03-04 21:45:12 +0000

    MFV r358616:
    Update ntp-4.2.8p13 --> 4.2.8p14.
    
    The advisory can be found at:
    http://support.ntp.org/bin/view/Main/SecurityNotice#\
    March_2020_ntp_4_2_8p14_NTP_Rele
    
    No CVEs have been documented yet.
    
    MFC after:	now
    Security:	http://support.ntp.org/bin/view/Main/NtpBug3610
    		http://support.ntp.org/bin/view/Main/NtpBug3596
    		http://support.ntp.org/bin/view/Main/NtpBug3592
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@358652 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- contrib/ntp/sntp/tests/run-kodDatabase.c
+++ contrib/ntp/sntp/tests/run-kodDatabase.c
@@ -44,7 +44,9 @@ extern void test_DeleteEntry(void);
 //=======Suite Setup=====
 static void suite_setup(void)
 {
+extern int change_iobufs(int);
 extern int change_logfile(const char*, int);
+change_iobufs(1);
 change_logfile("stderr", 0);
 }
 
