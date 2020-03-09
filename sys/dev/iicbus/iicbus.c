9b7938dcf338e31b4440a207144799db0c309ee6
Vladimir Kondratyev <wulf@FreeBSD.org>
2020-03-09 20:31:38 +0000

    iicbus(4): Add support for ACPI-based children enumeration
    When iicbus is attached as child of Designware I2C controller it scans all
    ACPI nodes for "I2C Serial Bus Connection Resource Descriptor" described
    in section 19.6.57 of ACPI specs.
    If such a descriptor is found, I2C child is added to iicbus, it's I2C
    address, IRQ resource and ACPI handle are added to ivars. Existing
    ACPI bus-hosted child is deleted afterwards.
    
    The driver also installs so called "I2C address space handler" which is
    disabled by default as nontested.
    Set hw.iicbus.enable_acpi_space_handler loader tunable to 1 to enable it.
    
    Reviewed by:		markj
    MFC after:		2 weeks
    Differential Revision:	https://reviews.freebsd.org/D22901

--- sys/dev/iicbus/iicbus.c
+++ sys/dev/iicbus/iicbus.c
@@ -89,8 +89,8 @@ iic_probe_device(device_t dev, u_char addr)
  * We add all the devices which we know about.
  * The generic attach routine will attach them if they are alive.
  */
-static int
-iicbus_attach(device_t dev)
+int
+iicbus_attach_common(device_t dev, u_int bus_freq)
 {
 #if SCAN_IICBUS
 	unsigned char addr;
@@ -100,7 +100,7 @@ iicbus_attach(device_t dev)
 
 	sc->dev = dev;
 	mtx_init(&sc->lock, "iicbus", NULL, MTX_DEF);
-	iicbus_init_frequency(dev, 0);
+	iicbus_init_frequency(dev, bus_freq);
 	iicbus_reset(dev, IIC_FASTEST, 0, NULL);
 	if (resource_int_value(device_get_name(dev),
 		device_get_unit(dev), "strict", &strict) == 0)
@@ -131,6 +131,13 @@ iicbus_attach(device_t dev)
 }
 
 static int
+iicbus_attach(device_t dev)
+{
+
+	return (iicbus_attach_common(dev, 0));
+}
+
+int
 iicbus_detach(device_t dev)
 {
 	struct iicbus_softc *sc = IICBUS_SOFTC(dev);
@@ -158,7 +165,7 @@ iicbus_print_child(device_t dev, device_t child)
 	return (retval);
 }
 
-static void
+void
 iicbus_probe_nomatch(device_t bus, device_t child)
 {
 	struct iicbus_ivar *devi = IICBUS_IVAR(child);
@@ -166,7 +173,7 @@ iicbus_probe_nomatch(device_t bus, device_t child)
 	device_printf(bus, "<unknown card> at addr %#x\n", devi->addr);
 }
 
-static int
+int
 iicbus_child_location_str(device_t bus, device_t child, char *buf,
     size_t buflen)
 {
@@ -176,7 +183,7 @@ iicbus_child_location_str(device_t bus, device_t child, char *buf,
 	return (0);
 }
 
-static int
+int
 iicbus_child_pnpinfo_str(device_t bus, device_t child, char *buf,
     size_t buflen)
 {
@@ -184,7 +191,7 @@ iicbus_child_pnpinfo_str(device_t bus, device_t child, char *buf,
 	return (0);
 }
 
-static int
+int
 iicbus_read_ivar(device_t bus, device_t child, int which, uintptr_t *result)
 {
 	struct iicbus_ivar *devi = IICBUS_IVAR(child);
@@ -199,7 +206,7 @@ iicbus_read_ivar(device_t bus, device_t child, int which, uintptr_t *result)
 	return (0);
 }
 
-static int
+int
 iicbus_write_ivar(device_t bus, device_t child, int which, uintptr_t value)
 {
 	struct iicbus_ivar *devi = IICBUS_IVAR(child);
@@ -215,8 +222,9 @@ iicbus_write_ivar(device_t bus, device_t child, int which, uintptr_t value)
 	return (0);
 }
 
-static device_t
-iicbus_add_child(device_t dev, u_int order, const char *name, int unit)
+device_t
+iicbus_add_child_common(device_t dev, u_int order, const char *name, int unit,
+    size_t ivars_size)
 {
 	device_t child;
 	struct iicbus_ivar *devi;
@@ -224,7 +232,7 @@ iicbus_add_child(device_t dev, u_int order, const char *name, int unit)
 	child = device_add_child_ordered(dev, order, name, unit);
 	if (child == NULL)
 		return (child);
-	devi = malloc(sizeof(struct iicbus_ivar), M_DEVBUF, M_NOWAIT | M_ZERO);
+	devi = malloc(ivars_size, M_DEVBUF, M_NOWAIT | M_ZERO);
 	if (devi == NULL) {
 		device_delete_child(dev, child);
 		return (0);
@@ -232,6 +240,14 @@ iicbus_add_child(device_t dev, u_int order, const char *name, int unit)
 	resource_list_init(&devi->rl);
 	device_set_ivars(child, devi);
 	return (child);
+}
+
+static device_t
+iicbus_add_child(device_t dev, u_int order, const char *name, int unit)
+{
+
+	return (iicbus_add_child_common(
+	    dev, order, name, unit, sizeof(struct iicbus_ivar)));
 }
 
 static void
