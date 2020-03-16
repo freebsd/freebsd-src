5553a14d0a3d7a1c76c982af4be168a472f1a264
hselasky <hselasky@FreeBSD.org>
2020-03-16 12:33:57 +0000

    Fix for double unlock in ipoib.
    The ipoib_unicast_send() function is not supposed to unlock the priv lock.
    
    MFC after:	3 days
    Sponsored by:	Mellanox Technologies

--- sys/ofed/drivers/infiniband/ulp/ipoib/ipoib_main.c
+++ sys/ofed/drivers/infiniband/ulp/ipoib/ipoib_main.c
@@ -731,7 +731,6 @@ ipoib_unicast_send(struct mbuf *mb, struct ipoib_dev_priv *priv, struct ipoib_he
 			}
 
 			if (!path->query && path_rec_start(priv, path)) {
-				spin_unlock_irqrestore(&priv->lock, flags);
 				if (new_path)
 					ipoib_path_free(priv, path);
 				return;
