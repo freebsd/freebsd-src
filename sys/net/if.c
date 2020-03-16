190ee6c37d9572dee1ba89a14903311a98a8b624
Brooks Davis <brooks@FreeBSD.org>
2020-03-16 23:09:25 +0000

    MFC r358592:
    Expose ifr_buffer_get_(buffer|length) outside if.c.
    
    This is a preparatory commit for D23933.
    
    Reviewed by:	jhb
    Obtained from:	CheriBSD
    Sponsored by:	DARPA

--- sys/net/if.c
+++ sys/net/if.c
@@ -2483,7 +2483,7 @@ ifunit(const char *name)
 	return (ifp);
 }
 
-static void *
+void *
 ifr_buffer_get_buffer(void *data)
 {
 	union ifreq_union *ifrup;
@@ -2511,7 +2511,7 @@ ifr_buffer_set_buffer_null(void *data)
 		ifrup->ifr.ifr_ifru.ifru_buffer.buffer = NULL;
 }
 
-static size_t
+size_t
 ifr_buffer_get_length(void *data)
 {
 	union ifreq_union *ifrup;
