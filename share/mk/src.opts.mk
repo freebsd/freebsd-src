82df3b8e17ed2b2cfae662b5ced3330caa6dd464
emaste <emaste@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-03-18 21:28:55 +0000

    src.opts.mk: remove remaining support for pre-C++11 C++ compilers
    Delete the conditions that forcibly disabled GOOGLETEST and LLDB for
    pre-C++11 C++ compilers, since we no longer support such compilers.
    
    Also delete the complicated method of defaulting LIBCPLUSPLUS to YES.
    
    Prodded by:	kevans
    Sponsored by:	The FreeBSD Foundation
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@359091 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -125,6 +125,7 @@ __DEFAULT_YES_OPTIONS = \
     LDNS \
     LDNS_UTILS \
     LEGACY_CONSOLE \
+    LIBCPLUSPLUS \
     LIBPTHREAD \
     LIBTHR \
     LLD \
@@ -386,35 +387,9 @@ __DEFAULT_NO_OPTIONS+=OPENMP
 .include <bsd.mkopt.mk>
 
 #
-# MK_* options that default to "yes" if the compiler is a C++11 compiler.
-#
-.for var in \
-    LIBCPLUSPLUS
-.if !defined(MK_${var})
-.if ${COMPILER_FEATURES:Mc++11}
-.if defined(WITHOUT_${var})
-MK_${var}:=	no
-.else
-MK_${var}:=	yes
-.endif
-.else
-.if defined(WITH_${var})
-MK_${var}:=	yes
-.else
-MK_${var}:=	no
-.endif
-.endif
-.endif
-.endfor
-
-#
 # Force some options off if their dependencies are off.
 # Order is somewhat important.
 #
-.if !${COMPILER_FEATURES:Mc++11}
-MK_GOOGLETEST:=	no
-.endif
-
 .if ${MK_CAPSICUM} == "no"
 MK_CASPER:=	no
 .endif
@@ -553,9 +528,5 @@ MK_${vv:H}:=	${MK_${vv:T}}
 #
 # Set defaults for the MK_*_SUPPORT variables.
 #
-
-.if !${COMPILER_FEATURES:Mc++11}
-MK_LLDB:=	no
-.endif
 
 .endif #  !target(__<src.opts.mk>__)
