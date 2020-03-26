e2df211f525aa162fbba44844a412f1c478b4a37
Hans Petter Selasky <hselasky@FreeBSD.org>
2020-03-26 05:37:08 +0000

    MFC r359120: Correctly implement support for remote wakeup for USB 3.0 device.
    Submitted by:	Horse Ma <Shichun.Ma@dell.com>
    Sponsored by:	Mellanox Technologies

--- sys/dev/usb/usb.h
+++ sys/dev/usb/usb.h
@@ -274,6 +274,11 @@ typedef struct usb_device_request usb_device_request_t;
 #define	UHF_C_BH_PORT_RESET	29
 #define	UHF_FORCE_LINKPM_ACCEPT	30
 
+/* SuperSpeed suspend support */
+#define	USB_INTERFACE_FUNC_SUSPEND 0
+#define	USB_INTERFACE_FUNC_SUSPEND_LP	(1 << 8)
+#define	USB_INTERFACE_FUNC_SUSPEND_RW	(1 << 9)
+
 struct usb_descriptor {
 	uByte	bLength;
 	uByte	bDescriptorType;
