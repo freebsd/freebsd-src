db21f91b547f4fdc821816bde69c73e0c6c042fd
kevans <kevans@FreeBSD.org>
2020-03-28 03:58:57 +0000

    gas: mark dwarf2_loc_mark_labels as extern
    Compiling with -fno-common complains as this header's included in multiple
    compilation units. In fact, the proper definition of dwarf2_loc_mark_labels
    already exists in dwarf2dbg.c, so simply mark this declaration with extern.

--- contrib/binutils/gas/dwarf2dbg.h
+++ contrib/binutils/gas/dwarf2dbg.h
@@ -78,7 +78,7 @@ extern void dwarf2_emit_label (symbolS *);
 /* True when we're supposed to set the basic block mark whenever a label
    is seen.  Unless the target is doing Something Weird, just call 
    dwarf2_emit_label.  */
-bfd_boolean dwarf2_loc_mark_labels;
+extern bfd_boolean dwarf2_loc_mark_labels;
 
 extern void dwarf2_finish (void);
 
