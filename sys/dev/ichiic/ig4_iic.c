38dacaa7e9e1e28e32e950d04191e80f51c2fe3d
Vladimir Kondratyev <wulf@FreeBSD.org>
2020-03-29 23:38:55 +0000

    MFC r358818:
    acpi: Fix stalled value returned by acpi_get_device() after device deletion
    
    Newbus device reference attached to ACPI handle is not cleared when newbus
    device is deleted with devctl(8) delete command. Fix that with calling of
    AcpiDetachData() from "child_deleted" bus method like acpi_pci driver does.
    
    MFC r358819:
    
    acpi: Export functions required by upcoming acpi_iicbus driver.
    
    MFC r358820:
    
    iicbus(4): Add support for ACPI-based children enumeration
    
    When iicbus is attached as child of Designware I2C controller it scans all
    ACPI nodes for "I2C Serial Bus Connection Resource Descriptor" described
    in section 19.6.57 of ACPI specs.
    If such a descriptor is found, I2C child is added to iicbus, it's I2C
    address, IRQ resource and ACPI handle are added to ivars. Existing
    ACPI bus-hosted child is deleted afterwards.
    
    The driver also installs so called "I2C address space handler" which is
    disabled by default as nontested.
    Set hw.iicbus.enable_acpi_space_handler loader tunable to 1 to enable it.
    
    Reviewed by:		markj
    Differential Revision:	https://reviews.freebsd.org/D22901

--- sys/dev/ichiic/ig4_iic.c
+++ sys/dev/ichiic/ig4_iic.c
@@ -1201,5 +1201,9 @@ ig4iic_dump(ig4iic_softc_t *sc)
 devclass_t ig4iic_devclass;
 
 DRIVER_MODULE(iicbus, ig4iic, iicbus_driver, iicbus_devclass, NULL, NULL);
+#ifdef DEV_ACPI
+DRIVER_MODULE(acpi_iicbus, ig4iic, acpi_iicbus_driver, iicbus_devclass, NULL,
+    NULL);
+#endif
 MODULE_DEPEND(ig4iic, iicbus, IICBUS_MINVER, IICBUS_PREFVER, IICBUS_MAXVER);
 MODULE_VERSION(ig4iic, 1);
