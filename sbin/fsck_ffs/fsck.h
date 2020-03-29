462e797af0837be91aaa2b02d4391e4d74ea5f2a
kevans <kevans@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-03-29 20:03:46 +0000

    fsck_ffs/fsdb: fix -fno-common build
    This one is also a small list:
    
    - 3x duplicate definition (ufs2_zino, returntosingle, nflag)
    - 5x 'needs extern', 3/5 of which are referenced in fsdb
    
    -fno-common will become the default in GCC10/LLVM11.
    
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@359427 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/fsck_ffs/fsck.h
+++ sbin/fsck_ffs/fsck.h
@@ -127,7 +127,7 @@ struct inostat {
  * Inode state information is contained on per cylinder group lists
  * which are described by the following structure.
  */
-struct inostatlist {
+extern struct inostatlist {
 	long	il_numalloced;	/* number of inodes allocated in this cg */
 	struct inostat *il_stat;/* inostat info for this cylinder group */
 } *inostathead;
@@ -271,13 +271,13 @@ struct dups {
 	struct dups *next;
 	ufs2_daddr_t dup;
 };
-struct dups *duplist;		/* head of dup list */
-struct dups *muldup;		/* end of unique duplicate dup block numbers */
+extern struct dups *duplist;		/* head of dup list */
+extern struct dups *muldup;		/* end of unique duplicate dup block numbers */
 
 /*
  * Inode cache data structures.
  */
-struct inoinfo {
+extern struct inoinfo {
 	struct	inoinfo *i_nexthash;	/* next entry in hash chain */
 	ino_t	i_number;		/* inode number of this entry */
 	ino_t	i_parent;		/* inode number of parent */
