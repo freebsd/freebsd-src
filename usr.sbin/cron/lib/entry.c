3b54a22d99f5ac3e252715c38b6979ff9beacfab
kevans <kevans@FreeBSD.org>
2020-03-30 03:26:52 +0000

    cron: respect PATH from login.conf
    As a followup to the use of login.conf environment vars (other than PATH) in
    cron, this patch adds PATH (and HOME) to the list of login.conf settings
    respected.
    
    The new logic is as follows:
    
    1. SHELL is always _PATH_BSHELL unless explicitly overridden in the crontab
    file itself; no other settings are respected. This is unchanged.
    
    2. PATH is taken from the first of: crontab file, login.conf, _PATH_DEFPATH
    
    3. HOME is taken from the first of: crontab file, login.conf, passwd entry,
    unset
    
    4. The current directory for invoking the command is taken from the crontab
    file's value of HOME (existing behavior), or the passwd entry, but not
    anywhere else (so it might not equal HOME if that was set in login.conf).
    
    Submitted by:	Andrew Gierth <andrew_tao173.riddles.org.uk>
    Reviewed by:	sigsys_gmail.com
    Differential Revision:	https://reviews.freebsd.org/D23597

--- usr.sbin/cron/lib/entry.c
+++ usr.sbin/cron/lib/entry.c
@@ -369,7 +369,8 @@ load_entry(file, error_func, pw, envp)
 	e->gid = pw->pw_gid;
 
 	/* copy and fix up environment.  some variables are just defaults and
-	 * others are overrides.
+	 * others are overrides; we process only the overrides here, defaults
+	 * are handled in do_command after login.conf is processed.
 	 */
 	e->envp = env_copy(envp);
 	if (e->envp == NULL) {
@@ -388,6 +389,10 @@ load_entry(file, error_func, pw, envp)
 			goto eof;
 		}
 	}
+	/* If LOGIN_CAP, this is deferred to do_command where the login class
+	 * is processed. If !LOGIN_CAP, do it here.
+	 */
+#ifndef LOGIN_CAP
 	if (!env_get("HOME", e->envp)) {
 		prev_env = e->envp;
 		sprintf(envstr, "HOME=%s", pw->pw_dir);
@@ -399,17 +404,7 @@ load_entry(file, error_func, pw, envp)
 			goto eof;
 		}
 	}
-	if (!env_get("PATH", e->envp)) {
-		prev_env = e->envp;
-		sprintf(envstr, "PATH=%s", _PATH_DEFPATH);
-		e->envp = env_set(e->envp, envstr);
-		if (e->envp == NULL) {
-			warn("env_set(%s)", envstr);
-			env_free(prev_env);
-			ecode = e_mem;
-			goto eof;
-		}
-	}
+#endif
 	prev_env = e->envp;
 	sprintf(envstr, "%s=%s", "LOGNAME", pw->pw_name);
 	e->envp = env_set(e->envp, envstr);
