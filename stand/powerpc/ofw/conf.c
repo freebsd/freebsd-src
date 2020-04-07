784d05f1d25bba8b8b8debde166629e42937bf11
luporl <luporl@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-04-07 19:46:00 +0000

    Add support to MSDOS FS in PPC loader
    Although PPC OFW loader already had a LOADER_MSDOS_SUPPORT option, a few lines
    were missing in conf.c, in order to support FAT filesystems.
    
    This is useful when running FreeBSD under QEMU, to be able to easily change the
    kernel and modules when running on hosts without UFS read/write support.
    
    Reviewed by:	jhibbits
    Sponsored by:	Eldorado Research Institute (eldorado.org.br)
    Differential Revision:	https://reviews.freebsd.org/D24328
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@359716 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- stand/powerpc/ofw/conf.c
+++ stand/powerpc/ofw/conf.c
@@ -69,6 +69,9 @@ struct fs_ops *file_system[] = {
 #if defined(LOADER_EXT2FS_SUPPORT)
     &ext2fs_fsops,
 #endif
+#if defined(LOADER_MSDOS_SUPPORT)
+    &dosfs_fsops,
+#endif
 #if defined(LOADER_NFS_SUPPORT)
     &nfs_fsops,
 #endif
