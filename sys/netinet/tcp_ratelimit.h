2b573e03476bb2b3ccb213de95bfcb7b7c3f50fd
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-04-08 17:55:45 +0000

    Fix copyright year and eliminate the obsolete all rights reserved line.
    Reviewed by: rrs@
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@359729 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/tcp_ratelimit.h
+++ sys/netinet/tcp_ratelimit.h
@@ -2,9 +2,8 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
- * Copyright (c) 2018-2019
+ * Copyright (c) 2018-2020
  *	Netflix Inc.
- *      All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
