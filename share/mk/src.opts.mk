3ab5e38d3a760e08234531a4e257641cf8c501ef
delphij <delphij@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-04-09 05:11:18 +0000

    Always install backward compatibility timezones, as they are installed on all major Linux distributions as well as NetBSD and OpenBSD.
    Remove the undocumented ZONEINFO_OLD_TIMEZONES_SUPPORT and the deprecated
    OLDTIMEZONES knobs as they are now the default.
    
    Reviewed by:		ngie, rgrimes
    MFC after:		2 weeks
    Differential Revision:	https://reviews.freebsd.org/D24306
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@359736 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -218,7 +218,6 @@ __DEFAULT_NO_OPTIONS = \
     SORT_THREADS \
     SVN \
     ZONEINFO_LEAPSECONDS_SUPPORT \
-    ZONEINFO_OLD_TIMEZONES_SUPPORT \
 
 # LEFT/RIGHT. Left options which default to "yes" unless their corresponding
 # RIGHT option is disabled.
@@ -466,7 +465,6 @@ MK_GOOGLETEST:=	no
 
 .if ${MK_ZONEINFO} == "no"
 MK_ZONEINFO_LEAPSECONDS_SUPPORT:= no
-MK_ZONEINFO_OLD_TIMEZONES_SUPPORT:= no
 .endif
 
 .if ${MK_CROSS_COMPILER} == "no"
