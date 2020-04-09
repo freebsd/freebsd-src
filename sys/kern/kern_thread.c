c41a559e0c5d4994d12564464c77fb5728d53663
rmacklem <rmacklem@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-04-09 14:44:46 +0000

    Remove the old NFS lock device driver that uses Giant.
    This NFS lock device driver was replaced by the kernel NLM around FreeBSD7 and
    has not normally been used since then.
    To use it, the kernel had to be built without "options NFSLOCKD" and
    the nfslockd.ko had to be deleted as well.
    Since it uses Giant and is no longer used, this patch removes it.
    
    With this device driver removed, there is now a lot of unused code
    in the userland rpc.lockd. That will be removed on a future commit.
    
    Reviewed by:	kib
    Differential Revision:	https://reviews.freebsd.org/D22933
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@359745 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/kern_thread.c
+++ sys/kern/kern_thread.c
@@ -90,11 +90,11 @@ _Static_assert(offsetof(struct proc, p_flag) == 0xb0,
     "struct proc KBI p_flag");
 _Static_assert(offsetof(struct proc, p_pid) == 0xbc,
     "struct proc KBI p_pid");
-_Static_assert(offsetof(struct proc, p_filemon) == 0x3c0,
+_Static_assert(offsetof(struct proc, p_filemon) == 0x3b8,
     "struct proc KBI p_filemon");
-_Static_assert(offsetof(struct proc, p_comm) == 0x3d8,
+_Static_assert(offsetof(struct proc, p_comm) == 0x3d0,
     "struct proc KBI p_comm");
-_Static_assert(offsetof(struct proc, p_emuldata) == 0x4b8,
+_Static_assert(offsetof(struct proc, p_emuldata) == 0x4b0,
     "struct proc KBI p_emuldata");
 #endif
 #ifdef __i386__
@@ -110,11 +110,11 @@ _Static_assert(offsetof(struct proc, p_flag) == 0x68,
     "struct proc KBI p_flag");
 _Static_assert(offsetof(struct proc, p_pid) == 0x74,
     "struct proc KBI p_pid");
-_Static_assert(offsetof(struct proc, p_filemon) == 0x26c,
+_Static_assert(offsetof(struct proc, p_filemon) == 0x268,
     "struct proc KBI p_filemon");
-_Static_assert(offsetof(struct proc, p_comm) == 0x280,
+_Static_assert(offsetof(struct proc, p_comm) == 0x27c,
     "struct proc KBI p_comm");
-_Static_assert(offsetof(struct proc, p_emuldata) == 0x30c,
+_Static_assert(offsetof(struct proc, p_emuldata) == 0x308,
     "struct proc KBI p_emuldata");
 #endif
 
