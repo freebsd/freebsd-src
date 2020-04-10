7b482ceb249ae3ba8c66949e7d00e64a0a982287
markj <markj@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-04-10 20:42:11 +0000

    sbappendcontrol() needs to avoid clearing M_NOTREADY on data mbufs.
    If LOCAL_CREDS is set on a unix socket and sendfile() is called,
    sendfile will call uipc_send(PRUS_NOTREADY), prepending a control
    message to the M_NOTREADY mbufs.  uipc_send() then calls
    sbappendcontrol() instead of sbappend(), and sbappendcontrol() would
    erroneously clear M_NOTREADY.
    
    Pass send flags to sbappendcontrol(), like we do for sbappend(), to
    preserve M_READY when necessary.
    
    Reported by:	syzkaller
    MFC after:	2 weeks
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D24333
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@359779 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/uipc_sockbuf.c
+++ sys/kern/uipc_sockbuf.c
@@ -1101,11 +1101,11 @@ sbappendaddr(struct sockbuf *sb, const struct sockaddr *asa,
 
 void
 sbappendcontrol_locked(struct sockbuf *sb, struct mbuf *m0,
-    struct mbuf *control)
+    struct mbuf *control, int flags)
 {
 	struct mbuf *m, *mlast;
 
-	m_clrprotoflags(m0);
+	sbm_clrprotoflags(m0, flags);
 	m_last(control)->m_next = m0;
 
 	SBLASTRECORDCHK(sb);
@@ -1123,11 +1123,12 @@ sbappendcontrol_locked(struct sockbuf *sb, struct mbuf *m0,
 }
 
 void
-sbappendcontrol(struct sockbuf *sb, struct mbuf *m0, struct mbuf *control)
+sbappendcontrol(struct sockbuf *sb, struct mbuf *m0, struct mbuf *control,
+    int flags)
 {
 
 	SOCKBUF_LOCK(sb);
-	sbappendcontrol_locked(sb, m0, control);
+	sbappendcontrol_locked(sb, m0, control, flags);
 	SOCKBUF_UNLOCK(sb);
 }
 
