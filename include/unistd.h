6371039d47fcde7da8301b6243867c1bbfe0793e
kevans <kevans@FreeBSD.org>
2020-04-12 21:23:19 +0000

    Implement a close_range(2) syscall
    close_range(min, max, flags) allows for a range of descriptors to be
    closed. The Python folk have indicated that they would much prefer this
    interface to closefrom(2), as the case may be that they/someone have special
    fds dup'd to higher in the range and they can't necessarily closefrom(min)
    because they don't want to hit the upper range, but relocating them to lower
    isn't necessarily feasible.
    
    sys_closefrom has been rewritten to use kern_close_range() using ~0U to
    indicate closing to the end of the range. This was chosen rather than
    requiring callers of kern_close_range() to hold FILEDESC_SLOCK across the
    call to kern_close_range for simplicity.
    
    The flags argument of close_range(2) is currently unused, so any flags set
    is currently EINVAL. It was added to the interface in Linux so that future
    flags could be added for, e.g., "halt on first error" and things of this
    nature.
    
    This patch is based on a syscall of the same design that is expected to be
    merged into Linux.
    
    Reviewed by:	kib, markj, vangyzen (all slightly earlier revisions)
    Differential Revision:	https://reviews.freebsd.org/D21627

--- include/unistd.h
+++ include/unistd.h
@@ -494,6 +494,7 @@ struct crypt_data {
 int	 acct(const char *);
 int	 async_daemon(void);
 int	 check_utility_compat(const char *);
+int	 close_range(unsigned int, unsigned int, int);
 ssize_t	 copy_file_range(int, off_t *, int, off_t *, size_t, unsigned int);
 const char *
 	 crypt_get_format(void);
