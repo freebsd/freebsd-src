2edf7995cc3fd9c467cb0b7ec5ce6a7e09427538
kevans <kevans@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-04-13 20:00:44 +0000

    depend-cleanup: fix typo, ^/lib/libc/sys/Makefile.inc generates .S stubs
    Pointy hat:	kevans
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@359896 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- tools/build/depend-cleanup.sh
+++ tools/build/depend-cleanup.sh
@@ -33,6 +33,6 @@ clean_dep()
 
 # Date      Rev      Description
 # 20190916  r352703  shm_open syscall reimplemented in C
-clean_dep lib/libc   shm_open c
+clean_dep lib/libc   shm_open S
 # 20200310  r358851  rename of openmp's ittnotify_static.c to .cpp
 clean_dep lib/libomp ittnotify_static c
