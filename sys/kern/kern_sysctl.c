19112958cfaee4132b1a646dfbf882f3e2b9f31c
Brooks Davis <brooks@FreeBSD.org>
2020-04-16 17:24:13 +0000

    style(9): end continued line with operator.

--- sys/kern/kern_sysctl.c
+++ sys/kern/kern_sysctl.c
@@ -1655,8 +1655,8 @@ sysctl_handle_string(SYSCTL_HANDLER_ARGS)
 	 * string.  In ddb, don't worry about trying to make a malloced
 	 * snapshot.
 	 */
-	if ((oidp->oid_kind & (CTLFLAG_WR | CTLFLAG_TUN)) == 0 || arg2 == 0
-	    || kdb_active) {
+	if ((oidp->oid_kind & (CTLFLAG_WR | CTLFLAG_TUN)) == 0 ||
+	    arg2 == 0 || kdb_active) {
 		arg2 = strlen((char *)arg1) + 1;
 		ro_string = 1;
 	}
