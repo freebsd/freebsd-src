8e2afdfa837d386b2dbc29e0735adbfd774b66ed
cperciva <cperciva@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-04-16 21:56:52 +0000

    Alert devd when acpi_video brightness changes
    On my Dell Latitude 7390 laptop, the brightness hotkeys
    (Fn+<up/down arrow>) send ACPI notifications which acpi_video
    handles by adjusting its brightness setting; but ACPI does not
    actually do anything with the backlight.
    
    Announcing brightness changes via devd makes it possible to close
    the loop by triggering the intel_backlight utility to perform the
    required backlight adjustment.
    
    Reviewed by:	imp
    MFC after:	3 days
    Differential Revision:	https://reviews.freebsd.org/D24424
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@360025 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/acpica/acpi_video.c
+++ sys/dev/acpica/acpi_video.c
@@ -1036,6 +1036,7 @@ vo_get_brightness(ACPI_HANDLE handle)
 static void
 vo_set_brightness(ACPI_HANDLE handle, int level)
 {
+	char notify_buf[16];
 	ACPI_STATUS status;
 
 	ACPI_SERIAL_ASSERT(video_output);
@@ -1043,6 +1044,8 @@ vo_set_brightness(ACPI_HANDLE handle, int level)
 	if (ACPI_FAILURE(status))
 		printf("can't evaluate %s._BCM - %s\n",
 		       acpi_name(handle), AcpiFormatException(status));
+	snprintf(notify_buf, sizeof(notify_buf), "notify=%d", level);
+	devctl_notify("ACPI", "Video", "brightness", notify_buf);
 }
 
 static UINT32
