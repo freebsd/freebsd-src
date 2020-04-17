3f359bfd47430183f69b9c03f34458217e7c7970
Kristof Provost <kp@FreeBSD.org>
2020-04-17 14:57:15 +0000

    bridge tests: Test deleting a bridge with members
    Reviewed by:	philip, emaste
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D24337

--- tests/sys/net/if_bridge_test.sh
+++ tests/sys/net/if_bridge_test.sh
@@ -245,10 +245,37 @@ span_cleanup()
 	vnet_cleanup
 }
 
+atf_test_case "delete_with_members" "cleanup"
+delete_with_members_head()
+{
+	atf_set descr 'Delete a bridge which still has member interfaces'
+	atf_set require.user root
+}
+
+delete_with_members_body()
+{
+	vnet_init
+
+	bridge=$(vnet_mkbridge)
+	epair=$(vnet_mkepair)
+
+	ifconfig ${bridge} 192.0.2.1/24 up
+	ifconfig ${epair}a up
+	ifconfig ${bridge} addm ${epair}a
+
+	ifconfig ${bridge} destroy
+}
+
+delete_with_members_cleanup()
+{
+	vnet_cleanup
+}
+
 atf_init_test_cases()
 {
 	atf_add_test_case "bridge_transmit_ipv4_unicast"
 	atf_add_test_case "stp"
 	atf_add_test_case "static"
 	atf_add_test_case "span"
+	atf_add_test_case "delete_with_members"
 }
