b55128c5ff7c690d9ba7f1701ed92e159fda68a9
Konstantin Belousov <kib@FreeBSD.org>
2020-04-18 03:14:16 +0000

    MFC r359634: Make p_vaddr % p_align == p_offset % p_align for (some) TLS segments.

--- libexec/rtld-elf/xmalloc.c
+++ libexec/rtld-elf/xmalloc.c
@@ -27,6 +27,7 @@
  * $FreeBSD$
  */
 
+#include <sys/param.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
@@ -75,16 +76,21 @@ xstrdup(const char *str)
 }
 
 void *
-malloc_aligned(size_t size, size_t align)
+malloc_aligned(size_t size, size_t align, size_t offset)
 {
-	void *mem, *res;
+	char *mem, *res;
+	uintptr_t x;
 
+	offset &= align - 1;
 	if (align < sizeof(void *))
 		align = sizeof(void *);
 
-	mem = xmalloc(size + sizeof(void *) + align - 1);
-	res = (void *)round((uintptr_t)mem + sizeof(void *), align);
-	*(void **)((uintptr_t)res - sizeof(void *)) = mem;
+	mem = xmalloc(size + 3 * align + offset);
+	x = roundup((uintptr_t)mem + sizeof(void *), align);
+	x += offset;
+	res = (void *)x;
+	x -= sizeof(void *);
+	memcpy((void *)x, &mem, sizeof(mem));
 	return (res);
 }
 
@@ -98,6 +104,6 @@ free_aligned(void *ptr)
 		return;
 	x = (uintptr_t)ptr;
 	x -= sizeof(void *);
-	mem = *(void **)x;
+	memcpy(&mem, (void *)x, sizeof(mem));
 	free(mem);
 }
