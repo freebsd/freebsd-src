b810f1f9801e1dcf8b562e14c9e61af427108640
Dimitry Andric <dim@FreeBSD.org>
2020-04-25 18:23:23 +0000

    MFC r359045 (by brooks):
    Add an internal liblua and use it in flua.
    
    The new liblua will be used in a forthcoming import of kyua.
    
    Reviewed by:	kevans
    Obtained from:	CheriBSD
    Sponsored by:	DARPA
    Differential Revision:	https://reviews.freebsd.org/D24090

--- share/mk/src.libnames.mk
+++ share/mk/src.libnames.mk
@@ -41,6 +41,7 @@ _INTERNALLIBS=	\
 		fifolog \
 		ipf \
 		lpr \
+		lua \
 		netbsd \
 		ntp \
 		ntpevent \
@@ -293,6 +294,7 @@ _DP_memstat=	kvm
 _DP_magic=	z
 _DP_mt=		sbuf bsdxml
 _DP_ldns=	ssl crypto
+_DP_lua=	m
 .if ${MK_OPENSSL} != "no"
 _DP_fetch=	ssl crypto
 .else
@@ -464,6 +466,9 @@ LDADD+=		${LDADD_${_l}}
 # INTERNALLIB definitions.
 LIBELFTCDIR=	${OBJTOP}/lib/libelftc
 LIBELFTC?=	${LIBELFTCDIR}/libelftc${PIE_SUFFIX}.a
+
+LIBLUADIR=	${OBJTOP}/lib/liblua
+LIBLUA?=	${LIBLUADIR}/liblua${PIE_SUFFIX}.a
 
 LIBPEDIR=	${OBJTOP}/lib/libpe
 LIBPE?=		${LIBPEDIR}/libpe${PIE_SUFFIX}.a
