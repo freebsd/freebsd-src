92f073bff589c08880af78e00180ec3ac3542657
Dimitry Andric <dim@FreeBSD.org>
2020-04-26 15:50:32 +0000

    Add casts to work around harmless -Werror warnings from clang 10.0.0, such as:
    usr.sbin/timed/timed/networkdelta.c:160:13: error: implicit conversion from 'long' to 'float' changes value from 9223372036854775807 to 9223372036854775808
          [-Werror,-Wimplicit-int-float-conversion]
            float ap = LONG_MAX;            /* bounds on the median */
                  ~~   ^~~~~~~~
    
    Direct commit to stable/{10,11,12}, since timed has been removed from
    FreeBSD 13.

--- usr.sbin/timed/timed/networkdelta.c
+++ usr.sbin/timed/timed/networkdelta.c
@@ -157,8 +157,8 @@ median(float a, float *eps_ptr, long *x, long *xlim, unsigned int gnuf)
 	/* unsigned int gnuf; */	/* good enough estimate */
 {
 	long *xptr;
-	float ap = LONG_MAX;		/* bounds on the median */
-	float am = -LONG_MAX;
+	float ap = (float)LONG_MAX;		/* bounds on the median */
+	float am = -(float)LONG_MAX;
 	float aa;
 	int npts;			/* # of points above & below guess */
 	float xp;			/* closet point above the guess */
@@ -180,8 +180,8 @@ median(float a, float *eps_ptr, long *x, long *xlim, unsigned int gnuf)
 		sum = 0.0;
 		sumx = 0.0;
 		npts = 0;
-		xp = LONG_MAX;
-		xm = -LONG_MAX;
+		xp = (float)LONG_MAX;
+		xm = -(float)LONG_MAX;
 
 		for (xptr = x; xptr != xlim; xptr++) {
 			float xx = *xptr;
