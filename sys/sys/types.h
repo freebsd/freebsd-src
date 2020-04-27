1041e7c61ba46ffb071a323f1ec4c7888d3bb2db
Kyle Evans <kevans@FreeBSD.org>
2020-04-27 22:27:46 +0000

    MFC r359954: sys/types.h: adjust #endif comment to match reality

--- sys/sys/types.h
+++ sys/sys/types.h
@@ -302,7 +302,7 @@ typedef	_Bool	bool;
 
 #define offsetof(type, field) __offsetof(type, field)
 
-#endif /* !_KERNEL */
+#endif /* _KERNEL */
 
 /*
  * The following are all things that really shouldn't exist in this header,
