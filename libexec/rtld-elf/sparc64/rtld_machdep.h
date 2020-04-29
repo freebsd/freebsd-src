b5764c3fc4d8bff04539bcd4a97dc3cc06f2d277
Konstantin Belousov <kib@FreeBSD.org>
2020-04-29 10:57:21 +0000

    MFC r360091: Align initial-exec TLS segments to the p_vaddr % align.

--- libexec/rtld-elf/sparc64/rtld_machdep.h
+++ libexec/rtld-elf/sparc64/rtld_machdep.h
@@ -58,9 +58,9 @@ Elf_Addr reloc_jmpslot(Elf_Addr *, Elf_Addr,
 
 #define round(size, align)				\
 	(((size) + (align) - 1) & ~((align) - 1))
-#define calculate_first_tls_offset(size, align) \
+#define calculate_first_tls_offset(size, align, offset) \
 	round(size, align)
-#define calculate_tls_offset(prev_offset, prev_size, size, align) \
+#define calculate_tls_offset(prev_offset, prev_size, size, align, offset) \
 	round((prev_offset) + (size), align)
 #define calculate_tls_end(off, size) 	((off) + (size))
 
