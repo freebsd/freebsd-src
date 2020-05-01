5d85138f1379f21bdea7a7de5486d37bb2bbe724
Alexander Motin <mav@FreeBSD.org>
2020-05-01 13:47:13 +0000

    MFC r323320 (by scottl): Fix intrhook release in MFI as well

--- sys/dev/mfi/mfi.c
+++ sys/dev/mfi/mfi.c
@@ -1264,8 +1264,6 @@ mfi_startup(void *arg)
 
 	sc = (struct mfi_softc *)arg;
 
-	config_intrhook_disestablish(&sc->mfi_ich);
-
 	sc->mfi_enable_intr(sc);
 	sx_xlock(&sc->mfi_config_lock);
 	mtx_lock(&sc->mfi_io_lock);
@@ -1274,6 +1272,8 @@ mfi_startup(void *arg)
 	    mfi_syspdprobe(sc);
 	mtx_unlock(&sc->mfi_io_lock);
 	sx_xunlock(&sc->mfi_config_lock);
+
+	config_intrhook_disestablish(&sc->mfi_ich);
 }
 
 static void
