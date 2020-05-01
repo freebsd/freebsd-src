da352a74de717c9dd5df3035fcdcfc15d0ead30c
mhorne <mhorne@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-05-01 21:59:47 +0000

    Use the HSM SBI extension to halt CPUs
    Differential Revision:	https://reviews.freebsd.org/D24498
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@360554 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/riscv/riscv/machdep.c
+++ sys/riscv/riscv/machdep.c
@@ -473,9 +473,16 @@ void
 cpu_halt(void)
 {
 
+	/*
+	 * Try to power down using the HSM SBI extension and fall back to a
+	 * simple wfi loop.
+	 */
 	intr_disable();
+	if (sbi_probe_extension(SBI_EXT_ID_HSM) != 0)
+		sbi_hsm_hart_stop();
 	for (;;)
 		__asm __volatile("wfi");
+	/* NOTREACHED */
 }
 
 /*
