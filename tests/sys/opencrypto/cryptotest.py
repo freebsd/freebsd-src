13bb9d9ecfd2251ce25b9863bd3ca898ab8424f7
jhb <jhb@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-05-02 14:20:32 +0000

    Remove support for the algorithms deprecated in r348876.
    This removes support for the following algorithms:
    - ARC4
    - Blowfish
    - CAST128
    - DES
    - 3DES
    - MD5-HMAC
    - Skipjack
    
    Since /dev/crypto no longer supports 3DES, stop testing the 3DES KAT
    vectors in cryptotest.py.
    
    Reviewed by:	cem (previous version)
    Relnotes:	yes
    Sponsored by:	Chelsio Communications
    Differential Revision:	https://reviews.freebsd.org/D24346
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@360562 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- tests/sys/opencrypto/cryptotest.py
+++ tests/sys/opencrypto/cryptotest.py
@@ -51,7 +51,6 @@ def katg(base, glob):
     return iglob(os.path.join(katdir, base, glob))
 
 aesmodules = [ 'cryptosoft0', 'aesni0', 'armv8crypto0', 'ccr0', 'ccp0' ]
-desmodules = [ 'cryptosoft0', ]
 shamodules = [ 'cryptosoft0', 'aesni0', 'armv8crypto0', 'ccr0', 'ccp0' ]
 
 def GenTestCase(cname):
@@ -331,46 +330,6 @@ def GenTestCase(cname):
                         " Actual: " + repr(binascii.hexlify(r)) + \
                         " Expected: " + repr(data) + \
                         " on " + cname)
-
-        ###############
-        ##### DES #####
-        ###############
-        @unittest.skipIf(cname not in desmodules, 'skipping DES on %s' % (cname))
-        def test_tdes(self):
-            for i in katg('KAT_TDES', 'TCBC[a-z]*.rsp'):
-                self.runTDES(i)
-
-        def runTDES(self, fname):
-            columns = [ 'COUNT', 'KEYs', 'IV', 'PLAINTEXT', 'CIPHERTEXT', ]
-            with cryptodev.KATParser(fname, columns) as parser:
-                self.runTDESWithParser(parser)
-
-        def runTDESWithParser(self, parser):
-            curfun = None
-            for mode, lines in next(parser):
-                if mode == 'ENCRYPT':
-                    swapptct = False
-                    curfun = Crypto.encrypt
-                elif mode == 'DECRYPT':
-                    swapptct = True
-                    curfun = Crypto.decrypt
-                else:
-                    raise RuntimeError('unknown mode: %r' % repr(mode))
-
-                for data in lines:
-                    curcnt = int(data['COUNT'])
-                    key = data['KEYs'] * 3
-                    cipherkey = binascii.unhexlify(key)
-                    iv = binascii.unhexlify(data['IV'])
-                    pt = binascii.unhexlify(data['PLAINTEXT'])
-                    ct = binascii.unhexlify(data['CIPHERTEXT'])
-
-                    if swapptct:
-                        pt, ct = ct, pt
-                    # run the fun
-                    c = Crypto(cryptodev.CRYPTO_3DES_CBC, cipherkey, crid=crid)
-                    r = curfun(c, pt, iv)
-                    self.assertEqual(r, ct)
 
         ###############
         ##### SHA #####
