713c84ae8ec0be445a380635f24afc177870e7f0
bdragon <bdragon@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-05-07 17:58:07 +0000

    [PowerPC] Fix powerpcspe build failure after r360569
    On powerpcspe, vm_paddr_t is 64 bit despite it being a 32 bit platform.
    Adjust compile time assertion to compensate.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@360788 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/uipc_mbuf.c
+++ sys/kern/uipc_mbuf.c
@@ -167,7 +167,12 @@ CTASSERT(sizeof(struct m_ext) == 160);
 #else
 CTASSERT(offsetof(struct mbuf, m_dat) == 24);
 CTASSERT(sizeof(struct pkthdr) == 48);
+#if defined(__powerpc__) && defined(BOOKE)
+/* PowerPC booke has 64-bit physical pointers. */
+CTASSERT(sizeof(struct m_ext) == 184);
+#else
 CTASSERT(sizeof(struct m_ext) == 180);
+#endif
 #endif
 
 /*
