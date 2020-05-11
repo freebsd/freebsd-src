12d0af4853ed784be386de42ad67b4a182b71f3d
jhb <jhb@FreeBSD.org>
2020-05-11 20:54:59 +0000

    Remove support for the skipjack encryption algorithm.
    This was removed from IPsec in r286100 and no longer has any in-tree
    consumers.
    
    Reviewed by:	cem
    Relnotes:	yes
    Sponsored by:	Chelsio Communications
    Differential Revision:	https://reviews.freebsd.org/D24769

--- sys/opencrypto/crypto.c
+++ sys/opencrypto/crypto.c
@@ -604,8 +604,6 @@ crypto_cipher(const struct crypto_session_params *csp)
 		return (&enc_xform_3des);
 	case CRYPTO_BLF_CBC:
 		return (&enc_xform_blf);
-	case CRYPTO_SKIPJACK_CBC:
-		return (&enc_xform_skipjack);
 	case CRYPTO_RIJNDAEL128_CBC:
 		return (&enc_xform_rijndael128);
 	case CRYPTO_AES_XTS:
@@ -689,7 +687,6 @@ static enum alg_type {
 	[CRYPTO_DES_CBC] = ALG_CIPHER,
 	[CRYPTO_3DES_CBC] = ALG_CIPHER,
 	[CRYPTO_BLF_CBC] = ALG_CIPHER,
-	[CRYPTO_SKIPJACK_CBC] = ALG_CIPHER,
 	[CRYPTO_MD5_HMAC] = ALG_KEYED_DIGEST,
 	[CRYPTO_SHA1_HMAC] = ALG_KEYED_DIGEST,
 	[CRYPTO_RIPEMD160_HMAC] = ALG_KEYED_DIGEST,
