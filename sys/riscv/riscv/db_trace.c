dea1ea9ed3db1ded3f5c6214c8bfbb3a49ffb2bf
John Baldwin <jhb@FreeBSD.org>
2020-05-11 20:58:27 +0000

    MFC 357632: Use the context created in makectx() for stack traces.
    Always use the kdb_thr_ctx() for db_trace_thread() as on other
    architectures.  Initialize pcb_ra to be the sepc from the saved
    trapframe rather than the saved ra to avoid skipping a frame.

--- sys/riscv/riscv/db_trace.c
+++ sys/riscv/riscv/db_trace.c
@@ -130,15 +130,12 @@ db_trace_thread(struct thread *thr, int count)
 	struct unwind_state frame;
 	struct pcb *ctx;
 
-	if (thr != curthread) {
-		ctx = kdb_thr_ctx(thr);
+	ctx = kdb_thr_ctx(thr);
 
-		frame.sp = (uint64_t)ctx->pcb_sp;
-		frame.fp = (uint64_t)ctx->pcb_s[0];
-		frame.pc = (uint64_t)ctx->pcb_ra;
-		db_stack_trace_cmd(&frame);
-	} else
-		db_trace_self();
+	frame.sp = (uint64_t)ctx->pcb_sp;
+	frame.fp = (uint64_t)ctx->pcb_s[0];
+	frame.pc = (uint64_t)ctx->pcb_ra;
+	db_stack_trace_cmd(&frame);
 	return (0);
 }
 
