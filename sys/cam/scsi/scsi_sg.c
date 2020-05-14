62863d44bbe90719bf229f63402910def81f49cd
John Baldwin <jhb@FreeBSD.org>
2020-05-14 18:50:43 +0000

    MF11 361038,361040: Don't dereference various user pointers.
    360171:
    Don't access a user buffer directly from the kernel.
    
    The handle_string callback for the ENCIOC_SETSTRING ioctl was passing
    a user pointer to memcpy().  Fix by using copyin() instead.
    
    For ENCIOC_GETSTRING ioctls, the handler was storing the user pointer
    in a CCB's data_ptr field where it was indirected by other code.  Fix
    this by allocating a temporary buffer (which ENCIOC_SETSTRING already
    did) and copying the result out to the user buffer after the CCB has
    been processed.
    
    360179:
    Don't pass a user buffer pointer as the data pointer in a CCB.
    
    Allocate a temporary buffer in the kernel to serve as the CCB data
    pointer for a pass-through transaction and use copyin/copyout to
    shuffle the data to/from the user buffer.
    
    360285:
    Don't indirect user pointers directly in two 802.11s ioctls.
    
    IEEE80211_MESH_RTCMD_ADD was invoking memcmp() to validate the
    supplied address directly on the user pointer rather than first doing
    a copyin() and validating the copied value.
    
    IEEE80211_MESH_RTCMD_DELETE was passing the user pointer directly to
    ieee80211_mesh_rt_del() rather than copying the user buffer into a
    temporary kernel buffer.
    
    360388:
    Don't run strcmp() against strings stored in user memory.
    
    Instead, copy the strings into a temporary buffer on the stack and
    run strcmp on the copies.
    
    360818:
    Fix a memory leak for ENCIOC_GETSTRING I introduced in r360171.
    
    Approved by:	re (gjb)

--- sys/cam/scsi/scsi_sg.c
+++ sys/cam/scsi/scsi_sg.c
@@ -506,6 +506,7 @@ sgioctl(struct cdev *dev, u_long cmd, caddr_t arg, int flag, struct thread *td)
 	struct cam_periph *periph;
 	struct sg_softc *softc;
 	struct sg_io_hdr *req;
+	void *data_ptr;
 	int dir, error;
 
 	periph = (struct cam_periph *)dev->si_drv1;
@@ -550,12 +551,20 @@ sgioctl(struct cdev *dev, u_long cmd, caddr_t arg, int flag, struct thread *td)
 			break;
 		}
 
+		if (req->dxfer_len > MAXPHYS) {
+			error = EINVAL;
+			break;
+		}
+
+		data_ptr = malloc(req->dxfer_len, M_DEVBUF, M_WAITOK);
+
 		ccb = cam_periph_getccb(periph, CAM_PRIORITY_NORMAL);
 		csio = &ccb->csio;
 
 		error = copyin(req->cmdp, &csio->cdb_io.cdb_bytes,
 		    req->cmd_len);
 		if (error) {
+			free(data_ptr, M_DEVBUF);
 			xpt_release_ccb(ccb);
 			break;
 		}
@@ -568,7 +577,7 @@ sgioctl(struct cdev *dev, u_long cmd, caddr_t arg, int flag, struct thread *td)
 			dir = CAM_DIR_IN;
 			break;
 		case SG_DXFER_TO_FROM_DEV:
-			dir = CAM_DIR_IN | CAM_DIR_OUT;
+			dir = CAM_DIR_BOTH;
 			break;
 		case SG_DXFER_NONE:
 		default:
@@ -576,12 +585,21 @@ sgioctl(struct cdev *dev, u_long cmd, caddr_t arg, int flag, struct thread *td)
 			break;
 		}
 
+		if (dir == CAM_DIR_IN || dir == CAM_DIR_BOTH) {
+			error = copyin(req->dxferp, data_ptr, req->dxfer_len);
+			if (error) {
+				free(data_ptr, M_DEVBUF);
+				xpt_release_ccb(ccb);
+				break;
+			}
+		}
+
 		cam_fill_csio(csio,
 			      /*retries*/1,
 			      sgdone,
 			      dir|CAM_DEV_QFRZDIS,
 			      MSG_SIMPLE_Q_TAG,
-			      req->dxferp,
+			      data_ptr,
 			      req->dxfer_len,
 			      req->mx_sb_len,
 			      req->cmd_len,
@@ -591,6 +609,7 @@ sgioctl(struct cdev *dev, u_long cmd, caddr_t arg, int flag, struct thread *td)
 		if (error) {
 			req->host_status = DID_ERROR;
 			req->driver_status = DRIVER_INVALID;
+			free(data_ptr, M_DEVBUF);
 			xpt_release_ccb(ccb);
 			break;
 		}
@@ -609,6 +628,10 @@ sgioctl(struct cdev *dev, u_long cmd, caddr_t arg, int flag, struct thread *td)
 					req->sb_len_wr);
 		}
 
+		if ((dir == CAM_DIR_OUT || dir == CAM_DIR_BOTH) && error == 0)
+			error = copyout(data_ptr, req->dxferp, req->dxfer_len);
+
+		free(data_ptr, M_DEVBUF);
 		xpt_release_ccb(ccb);
 		break;
 		
