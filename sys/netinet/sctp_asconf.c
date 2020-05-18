6395219ae33fb1ec3a74e7380967447dea541022
Michael Tuexen <tuexen@FreeBSD.org>
2020-05-18 18:32:58 +0000

    Avoid an integer underflow.
    MFC after:		3 days

--- sys/netinet/sctp_asconf.c
+++ sys/netinet/sctp_asconf.c
@@ -1797,9 +1797,9 @@ sctp_handle_asconf_ack(struct mbuf *m, int offset,
 		}		/* switch */
 
 		/* update remaining ASCONF-ACK message length to process */
-		ack_length -= SCTP_SIZE32(param_length);
-		if (ack_length <= 0) {
-			/* no more data in the mbuf chain */
+		if (ack_length > SCTP_SIZE32(param_length)) {
+			ack_length -= SCTP_SIZE32(param_length);
+		} else {
 			break;
 		}
 		offset += SCTP_SIZE32(param_length);
