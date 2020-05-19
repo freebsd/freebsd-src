43521b46fc788096648dfa89650dfe394dc0d455
wiklam <wiktorpilar99@gmail.com>
2020-05-19 03:55:08 +0200

    Correcting comment about "sched_interact_score".
    Reviewed by:	jrtc@, imp@
    Pull Request:	https://github.com/freebsd/freebsd-src/pull/431
    
    Sponsored by:		Netflix

--- sys/kern/sched_ule.c
+++ sys/kern/sched_ule.c
@@ -1516,7 +1516,7 @@ sched_initticks(void *dummy)
  * When a thread's sleep time is greater than its run time the
  * calculation is:
  *
- *                           scaling factor 
+ *                           scaling factor
  * interactivity score =  ---------------------
  *                        sleep time / run time
  *
@@ -1524,9 +1524,9 @@ sched_initticks(void *dummy)
  * When a thread's run time is greater than its sleep time the
  * calculation is:
  *
- *                           scaling factor 
- * interactivity score =  ---------------------    + scaling factor
- *                        run time / sleep time
+ *                                                 scaling factor
+ * interactivity score = 2 * scaling factor  -  ---------------------
+ *                                              run time / sleep time
  */
 static int
 sched_interact_score(struct thread *td)
