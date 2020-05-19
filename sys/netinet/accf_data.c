9b995c3d2459b862c0a94df5e59bd3b9a83b7048
markj <markj@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-05-19 18:35:08 +0000

    Define a module version for accept filter modules.
    Otherwise accept filters compiled into the kernel do not preempt
    preloaded accept filter modules.  Then, the preloaded file registers its
    accept filter module before the kernel, and the kernel's attempt fails
    since duplicate accept filter list entries are not permitted.  This
    causes the preloaded file's module to be released, since
    module_register_init() does a lookup by name, so the preloaded file is
    unloaded, and the accept filter's callback points to random memory since
    preload_delete_name() unmaps the file on x86 as of r336505.
    
    Add a new ACCEPT_FILTER_DEFINE macro which wraps the accept filter and
    module definitions, and ensures that a module version is defined.
    
    PR:		245870
    Reported by:	Thomas von Dein <freebsd@daemon.de>
    MFC after:	2 weeks
    Sponsored by:	The FreeBSD Foundation
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@361263 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/netinet/accf_data.c
+++ sys/netinet/accf_data.c
@@ -42,20 +42,7 @@ __FBSDID("$FreeBSD$");
 
 static int	sohasdata(struct socket *so, void *arg, int waitflag);
 
-static struct accept_filter accf_data_filter = {
-	"dataready",
-	sohasdata,
-	NULL,
-	NULL
-};
-
-static moduledata_t accf_data_mod = {
-	"accf_data",
-	accept_filt_generic_mod_event,
-	&accf_data_filter
-};
-
-DECLARE_MODULE(accf_data, accf_data_mod, SI_SUB_DRIVERS, SI_ORDER_MIDDLE);
+ACCEPT_FILTER_DEFINE(accf_data, "dataready", sohasdata, NULL, NULL, 1);
 
 static int
 sohasdata(struct socket *so, void *arg, int waitflag)
