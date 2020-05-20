6faabe5411630cef6155925b1a5619fce7064549
John Baldwin <jhb@FreeBSD.org>
2020-05-20 20:58:17 +0000

    Remove copyinfrom() and copyinstrfrom().
    These functions were added in 2001 and are currently unused.
    copyinfrom() looks to have never been used.  copyinstrfrom() was used
    for two weeks before the code was refactored to remove it's sole use.
    
    Reviewed by:	brooks, kib
    Obtained from:	CheriBSD
    Sponsored by:	DARPA
    Differential Revision:	https://reviews.freebsd.org/D24928

--- sys/sys/uio.h
+++ sys/sys/uio.h
@@ -80,12 +80,8 @@ struct vm_page;
 struct bus_dma_segment;
 
 struct uio *cloneuio(struct uio *uiop);
-int	copyinfrom(const void * __restrict src, void * __restrict dst,
-	    size_t len, int seg);
 int	copyiniov(const struct iovec *iovp, u_int iovcnt, struct iovec **iov,
 	    int error);
-int	copyinstrfrom(const void * __restrict src, void * __restrict dst,
-	    size_t len, size_t * __restrict copied, int seg);
 int	copyinuio(const struct iovec *iovp, u_int iovcnt, struct uio **uiop);
 int	copyout_map(struct thread *td, vm_offset_t *addr, size_t sz);
 int	copyout_unmap(struct thread *td, vm_offset_t addr, size_t sz);
