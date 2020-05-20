c37821f9468b1fa079a641b0e643423c0729a4bc
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-05-20 22:00:31 +0000

    amd64: Add a knob to flush RSB on context switches if machine has SMEP.
    The flush is needed to prevent cross-process ret2spec, which is not handled
    on kernel entry if IBPB is enabled but SMEP is present.
    While there, add i386 RSB flush.
    
    Reported by:	Anthony Steinhauser <asteinhauser@google.com>
    Reviewed by:	markj, Anthony Steinhauser
    Discussed with:	philip
    admbugs:	961
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@361302 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/x86/x86/cpu_machdep.c
+++ sys/x86/x86/cpu_machdep.c
@@ -1397,6 +1397,11 @@ SYSCTL_PROC(_machdep_mitigations_taa, OID_AUTO, state,
     sysctl_taa_state_handler, "A",
     "TAA Mitigation state");
 
+int __read_frequently cpu_flush_rsb_ctxsw;
+SYSCTL_INT(_machdep_mitigations, OID_AUTO, flush_rsb_ctxsw,
+    CTLFLAG_RW | CTLFLAG_NOFETCH, &cpu_flush_rsb_ctxsw, 0,
+    "Flush Return Stack Buffer on context switch");
+
 /*
  * Enable and restore kernel text write permissions.
  * Callers must ensure that disable_wp()/restore_wp() are executed
