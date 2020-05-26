8551662135f50fb362a1a5a1aed88c4f4b3415a4
Marcin Wojtas <mw@FreeBSD.org>
2020-05-26 15:35:22 +0000

    Provide ENA driver version in a sysctl node
    Usage example: $ sysctl hw.ena.driver_version
    
    Submitted by:  Maciej Bielski <mba@semihalf.com>
    Obtained from: Semihalf
    Sponsored by:  Amazon, Inc.

--- sys/dev/ena/ena_sysctl.c
+++ sys/dev/ena/ena_sysctl.c
@@ -48,6 +48,9 @@ int ena_log_level = ENA_ALERT | ENA_WARNING;
 SYSCTL_INT(_hw_ena, OID_AUTO, log_level, CTLFLAG_RWTUN,
     &ena_log_level, 0, "Logging level indicating verbosity of the logs");
 
+SYSCTL_CONST_STRING(_hw_ena, OID_AUTO, driver_version, CTLFLAG_RD,
+    DRV_MODULE_VERSION, "ENA driver version");
+
 /*
  * Use 9k mbufs for the Rx buffers. Default to 0 (use page size mbufs instead).
  * Using 9k mbufs in low memory conditions might cause allocation to take a lot
