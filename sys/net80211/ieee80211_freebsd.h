7e3ac034ee19f78f4c0ace0e6ed3522cbf6b66d6
adrian <adrian@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-05-27 18:32:12 +0000

    [net80211] Fix interrupted scan logic and ticks comparison
    The scan task refactoring stuff circa 2014-2016 broke the blocking task
     into a taskqueue with some async bits, but it apparently broke scans
     being interrupted by traffic.
    
    Notably - the new "field" SCAN_PAUSE sets both SCAN_INTERRUPT and SCAN_CANCEL,
    and a bunch of existing code was checking for SCAN_CANCEL only and breaking
    the scan. Unfortunately it was then (a) cancelling the scan entirely and
    (b) not notifying userland that scan was done.
    
    So:
    * Update the calls to scan_end() to only pass in 1 (saying the scan is complete)
      if SCAN_CANCEL is set WITHOUT SCAN_INTERRUPT. If both are set then yes,
      the scan is interrupted, but it isn't canceled - it's just paused.
    * Update the "did the scan flags change whilst the driver was called" logic
      to check for canceled scans, not interrupted scans.
    * The "scan done" logic now explicitly checks for either interrupted or
      completed scans. This accounts for the situation where a scan is being
      aborted via traffic but it ALSO happens to have finished (ie the last
      channel was checked.)
    
    This doesn't ENTIRELY fix scanning as the resume function is broken
    due to incorrect ticks math. Thus, the second half of this patch
    changes the ieee80211_ticks_*() macros to use int instead of long,
    matching the logic that the TCP code does with ticks and handles
    wrapping / negative ticks values. If cast to long then the wrapping
    math wouldn't work right (ie, if ticks was actually negative,
    ie, after the system has been up for a while.)
    
    This allows contbgscan() to correctly calculate if a scan should
    continue based on ticks and ic->ic_lastdata .
    
    Reviewed by:	bz
    Differential Revision:	https://reviews.freebsd.org/D25031
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@361560 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/net80211/ieee80211_freebsd.h
+++ sys/net80211/ieee80211_freebsd.h
@@ -254,9 +254,9 @@ void	ieee80211_vap_destroy(struct ieee80211vap *);
 #define	ticks_to_msecs(t)	TICKS_2_MSEC(t)
 #define	ticks_to_secs(t)	((t) / hz)
 
-#define ieee80211_time_after(a,b) 	((long)(b) - (long)(a) < 0)
+#define ieee80211_time_after(a,b) 	((int)(b) - (int)(a) < 0)
 #define ieee80211_time_before(a,b)	ieee80211_time_after(b,a)
-#define ieee80211_time_after_eq(a,b)	((long)(a) - (long)(b) >= 0)
+#define ieee80211_time_after_eq(a,b)	((int)(a) - (int)(b) >= 0)
 #define ieee80211_time_before_eq(a,b)	ieee80211_time_after_eq(b,a)
 
 struct mbuf *ieee80211_getmgtframe(uint8_t **frm, int headroom, int pktlen);
