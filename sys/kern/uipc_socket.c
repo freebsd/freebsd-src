f1e7e362cbd861af999db038c6b504f47cfe6563
jhb <jhb@FreeBSD.org>
2020-05-29 00:09:12 +0000

    Permit SO_NO_DDP and SO_NO_OFFLOAD to be read via getsockopt(2).
    MFC after:	2 weeks
    Sponsored by:	Chelsio Communications
    Differential Revision:	https://reviews.freebsd.org/D24627

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -3231,6 +3231,8 @@ sogetopt(struct socket *so, struct sockopt *sopt)
 		case SO_TIMESTAMP:
 		case SO_BINTIME:
 		case SO_NOSIGPIPE:
+		case SO_NO_DDP:
+		case SO_NO_OFFLOAD:
 			optval = so->so_options & sopt->sopt_name;
 integer:
 			error = sooptcopyout(sopt, &optval, sizeof optval);
