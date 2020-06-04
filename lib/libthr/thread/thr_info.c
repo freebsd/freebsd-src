950a6cf391024e53b861c1dfe870f80d8d1f77ce
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-06-04 09:06:03 +0000

    Revert r361770 "Add pthread_getname_np() and pthread_setname_np() aliases" for now.
    It is not compatible enough with Linux.
    
    Requested by:	antoine, jkim
    Sponsored by:	The FreeBSD Foundation
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@361784 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libthr/thread/thr_info.c
+++ lib/libthr/thread/thr_info.c
@@ -46,7 +46,6 @@ __FBSDID("$FreeBSD$");
 #include "thr_private.h"
 
 __weak_reference(_pthread_set_name_np, pthread_set_name_np);
-__weak_reference(_pthread_set_name_np, pthread_setname_np);
 
 static void
 thr_set_name_np(struct pthread *thread, const char *name)
@@ -90,7 +89,6 @@ thr_get_name_np(struct pthread *thread, char *buf, size_t len)
 }
 
 __weak_reference(_pthread_get_name_np, pthread_get_name_np);
-__weak_reference(_pthread_get_name_np, pthread_getname_np);
 
 void
 _pthread_get_name_np(pthread_t thread, char *buf, size_t len)
