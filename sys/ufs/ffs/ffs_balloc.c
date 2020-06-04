2c0b08544135921c57bcc699e3e9dafa60c1f7fd
mckusick <mckusick@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-06-04 18:35:21 +0000

    Two additional places that need to identify IN_IBLKDATA.
    Reviewed by: kib
    MFC with: -r361785
    Differential Revision:  https://reviews.freebsd.org/D25072
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@361801 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/ufs/ffs/ffs_balloc.c
+++ sys/ufs/ffs/ffs_balloc.c
@@ -154,7 +154,8 @@ ffs_balloc_ufs1(struct vnode *vp, off_t startoffset, int size,
 			ip->i_size = smalllblktosize(fs, nb + 1);
 			dp->di_size = ip->i_size;
 			dp->di_db[nb] = dbtofsb(fs, bp->b_blkno);
-			UFS_INODE_SET_FLAG(ip, IN_CHANGE | IN_UPDATE);
+			UFS_INODE_SET_FLAG(ip,
+			    IN_CHANGE | IN_UPDATE | IN_IBLKDATA);
 			if (flags & IO_SYNC)
 				bwrite(bp);
 			else if (DOINGASYNC(vp))
@@ -647,7 +648,7 @@ ffs_balloc_ufs2(struct vnode *vp, off_t startoffset, int size,
 				dp->di_extsize = smalllblktosize(fs, nb + 1);
 				dp->di_extb[nb] = dbtofsb(fs, bp->b_blkno);
 				bp->b_xflags |= BX_ALTDATA;
-				UFS_INODE_SET_FLAG(ip, IN_CHANGE);
+				UFS_INODE_SET_FLAG(ip, IN_CHANGE | IN_BLKDATA);
 				if (flags & IO_SYNC)
 					bwrite(bp);
 				else
