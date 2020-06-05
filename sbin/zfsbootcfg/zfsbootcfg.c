fb37491b81eeea24918d0b146fff0c48d4416b80
tsoome <tsoome@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-06-05 18:18:27 +0000

    zfsbootcfg: use vfs.root.mountfrom and update nextboot on every vdev
    vfs.zfs.boot.primary_pool is only set on BIOS boot, use vfs.root.mountfrom
    instead and update all vdevs on pool.
    
    Reviewed by:	allanjude
    Sponsored by:	Netflix, Klara Inc.
    Differential Revision:	https://reviews.freebsd.org/D25103
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@361842 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sbin/zfsbootcfg/zfsbootcfg.c
+++ sbin/zfsbootcfg/zfsbootcfg.c
@@ -40,13 +40,44 @@ __FBSDID("$FreeBSD$");
 /* Keep in sync with zfsboot.c. */
 #define MAX_COMMAND_LEN	512
 
+int
+install_bootonce(libzfs_handle_t *hdl, uint64_t pool_guid, nvlist_t *nv,
+    const char * const data)
+{
+	nvlist_t **child;
+	uint_t children = 0;
+	uint64_t guid;
+	int rv;
+
+	(void) nvlist_lookup_nvlist_array(nv, ZPOOL_CONFIG_CHILDREN, &child,
+	    &children);
+
+	for (int c = 0; c < children; c++) {
+		rv = install_bootonce(hdl, pool_guid, child[c], data);
+	}
+
+	if (children > 0)
+		return (rv);
+
+	if (nvlist_lookup_uint64(nv, ZPOOL_CONFIG_GUID, &guid) != 0) {
+		perror("can't get vdev guid");
+		return (1);
+	}
+	if (zpool_nextboot(hdl, pool_guid, guid, data) != 0) {
+		perror("ZFS_IOC_NEXTBOOT failed");
+		return (1);
+	}
+	return (0);
+}
+
 int main(int argc, const char * const *argv)
 {
-	char buf[32];
+	char buf[32], *name;
 	libzfs_handle_t *hdl;
+	zpool_handle_t *zphdl;
 	uint64_t pool_guid;
-	uint64_t vdev_guid;
-	int zfs_fd;
+	nvlist_t *nv, *config;
+	int rv;
 	int len;
 
 	if (argc != 2) {
@@ -60,39 +91,56 @@ int main(int argc, const char * const *argv)
 		return (1);
 	}
 
-	if (kenv(KENV_GET, "vfs.zfs.boot.primary_pool", buf, sizeof(buf)) <= 0) {
-		perror("can't get vfs.zfs.boot.primary_pool");
-		return (1);
-	}
-	pool_guid = strtoumax(buf, NULL, 10);
-	if (pool_guid == 0) {
-		perror("can't parse vfs.zfs.boot.primary_pool");
+	if (kenv(KENV_GET, "vfs.root.mountfrom", buf, sizeof(buf)) <= 0) {
+		perror("can't get vfs.root.mountfrom");
 		return (1);
 	}
 
-	if (kenv(KENV_GET, "vfs.zfs.boot.primary_vdev", buf, sizeof(buf)) <= 0) {
-		perror("can't get vfs.zfs.boot.primary_vdev");
+	if (strncmp(buf, "zfs:", 4) == 0) {
+		name = strchr(buf + 4, '/');
+		if (name != NULL)
+			*name = '\0';
+		name = buf + 4;
+	} else {
+		perror("not a zfs root");
 		return (1);
 	}
-	vdev_guid = strtoumax(buf, NULL, 10);
-	if (vdev_guid == 0) {
-		perror("can't parse vfs.zfs.boot.primary_vdev");
-		return (1);
-	}
-
+		
 	if ((hdl = libzfs_init()) == NULL) {
 		(void) fprintf(stderr, "internal error: failed to "
 		    "initialize ZFS library\n");
 		return (1);
 	}
 
-	if (zpool_nextboot(hdl, pool_guid, vdev_guid, argv[1]) != 0) {
-		perror("ZFS_IOC_NEXTBOOT failed");
+	zphdl = zpool_open(hdl, name);
+	if (zphdl == NULL) {
+		perror("can't open pool");
 		libzfs_fini(hdl);
 		return (1);
 	}
 
+	pool_guid = zpool_get_prop_int(zphdl, ZPOOL_PROP_GUID, NULL);
+
+	config = zpool_get_config(zphdl, NULL);
+	if (config == NULL) {
+		perror("can't get pool config");
+		zpool_close(zphdl);
+		libzfs_fini(hdl);
+		return (1);
+	}
+
+	if (nvlist_lookup_nvlist(config, ZPOOL_CONFIG_VDEV_TREE, &nv) != 0) {
+		perror("failed to get vdev tree");
+		zpool_close(zphdl);
+		libzfs_fini(hdl);
+		return (1);
+	}
+
+	rv = install_bootonce(hdl, pool_guid, nv, argv[1]);
+
+	zpool_close(zphdl);
 	libzfs_fini(hdl);
-	printf("zfs next boot options are successfully written\n");
-	return (0);
+	if (rv == 0)
+		printf("zfs next boot options are successfully written\n");
+	return (rv);
 }
