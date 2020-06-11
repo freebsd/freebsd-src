4b7de30a808cb2a270cde57523710135d744742f
0mp <0mp@FreeBSD.org>
2020-06-11 03:03:52 +0000

    Remove duplicate lines from sed tests
    Reported by:	yuripv
    Approved by:	pfg (src)
    MFC after:	2 weeks
    X-MFC-With:	362017

--- usr.bin/sed/tests/sed2_test.sh
+++ usr.bin/sed/tests/sed2_test.sh
@@ -177,7 +177,5 @@ atf_init_test_cases()
 	atf_add_test_case inplace_symlink_src
 	atf_add_test_case escape_subst
 	atf_add_test_case commands_on_stdin
-	atf_add_test_case commands_on_stdin
-	atf_add_test_case commands_on_stdin
 	atf_add_test_case hex_subst
 }
