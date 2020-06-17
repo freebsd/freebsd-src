freebsd/freebsd-src

commit b60ce1001ffe748c7e887f05fb21e1858f53f542

KTLS: Re-work unmapped mbufs to carry ext_pgs in the mbuf itself.
While the original implementation of unmapped mbufs was a large
step forward in terms of reducing cache misses by enabling mbufs
to carry more than a single page for sendfile, they are rather
cache unfriendly when accessing the ext_pgs metadata and
data. This is because the ext_pgs part of the mbuf is allocated
separately, and almost guaranteed to be cold in cache.

This change takes advantage of the fact that unmapped mbufs
are never used at the same time as pkthdr mbufs. Given this
fact, we can overlap the ext_pgs metadata with the mbuf
pkthdr, and carry the ext_pgs meta directly in the mbuf itself.
Similarly, we can carry the ext_pgs data (TLS hdr/trailer/array
of pages) directly after the existing m_ext.

In order to be able to carry 5 pages (which is the minimum
required for a 16K TLS record which is not perfectly aligned) on
LP64, I've had to steal ext_arg2. The only user of this in the
xmit path is sendfile, and I've adjusted it to use arg1 when
using unmapped mbufs.

This change is almost entirely mechanical, except that we
change mb_alloc_ext_pgs() to no longer allow allocating
pkthdrs, the change to avoid ext_arg2 as mentioned above,
and the removal of the ext_pgs zone,

This change saves roughly 2% "raw" CPU (~59% -> 57%), or over
3% "scaled" CPU on a Netflix 100% software kTLS workload at
90+ Gb/s on Broadwell Xeons.

In a follow-on commit, I plan to remove some hacks to avoid
access ext_pgs fields of mbufs, since they will now be in
cache.

Many thanks to glebius for helping to make this better in
the Netflix tree.

Reviewed by:	hselasky, jhb, rrs, glebius (early version)
Sponsored by:	Netflix
Differential Revision:	https://reviews.freebsd.org/D24213


git-svn-id: svn+ssh://svn.freebsd.org/base/head@359919 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/mlx5/mlx5_en/mlx5_en_hw_tls.c
@@ -618,15 +618,14 @@ mlx5e_tls_send_nop(struct mlx5e_sq *sq, struct mlx5e_tls_tag *ptag)
 static struct mbuf *
 sbtls_recover_record(struct mbuf *mb, int wait, uint32_t tcp_old, uint32_t *ptcp_seq)
 {
-	struct mbuf *mr;
+	struct mbuf *mr, *top;
 	uint32_t offset;
 	uint32_t delta;
 
 	/* check format of incoming mbuf */
 	if (mb->m_next == NULL ||
-	    (mb->m_next->m_flags & (M_NOMAP | M_EXT)) != (M_NOMAP | M_EXT) ||
-	    mb->m_next->m_ext.ext_buf == NULL) {
-		mr = NULL;
+	    (mb->m_next->m_flags & (M_NOMAP | M_EXT)) != (M_NOMAP | M_EXT)) {
+		top = NULL;
 		goto done;
 	}
 
@@ -635,14 +634,23 @@ sbtls_recover_record(struct mbuf *mb, int wait, uint32_t tcp_old, uint32_t *ptcp
 
 	/* check if we don't need to re-transmit anything */
 	if (offset == 0) {
-		mr = SBTLS_MBUF_NO_DATA;
+		top = SBTLS_MBUF_NO_DATA;
 		goto done;
 	}
 
-	/* try to get a new mbufs with packet header */
-	mr = m_gethdr(wait, MT_DATA);
-	if (mr == NULL)
+	/* try to get a new  packet header */
+	top = m_gethdr(wait, MT_DATA);
+	if (top == NULL)
 		goto done;
+
+	mr = m_get(wait, MT_DATA);
+	if (mr == NULL) {
+		m_free(top);
+		top = NULL;
+		goto done;
+	}
+
+	top->m_next = mr;
 
 	mb_dupcl(mr, mb->m_next);
 
@@ -650,7 +658,7 @@ sbtls_recover_record(struct mbuf *mb, int wait, uint32_t tcp_old, uint32_t *ptcp
 	mr->m_data = NULL;
 
 	/* setup packet header length */
-	mr->m_pkthdr.len = mr->m_len = offset;
+	top->m_pkthdr.len = mr->m_len = offset;
 
 	/* check for partial re-transmit */
 	delta = *ptcp_seq - tcp_old;
@@ -666,7 +674,7 @@ sbtls_recover_record(struct mbuf *mb, int wait, uint32_t tcp_old, uint32_t *ptcp
 	 */
 	*ptcp_seq -= offset;
 done:
-	return (mr);
+	return (top);
 }
 
 static int
@@ -677,7 +685,7 @@ mlx5e_sq_tls_populate(struct mbuf *mb, uint64_t *pseq)
 	for (; mb != NULL; mb = mb->m_next) {
 		if (!(mb->m_flags & M_NOMAP))
 			continue;
-		ext_pgs = (void *)mb->m_ext.ext_buf;
+		ext_pgs = &mb->m_ext_pgs;
 		*pseq = ext_pgs->seqno;
 		return (1);
 	}

sys/dev/cxgbe/crypto/t4_kern_tls.c
@@ -905,8 +905,8 @@ ktls_tcp_payload_length(struct tlspcb *tlsp, struct mbuf *m_tls)
 	u_int plen, mlen;
 
 	MBUF_EXT_PGS_ASSERT(m_tls);
-	ext_pgs = m_tls->m_ext.ext_pgs;
-	hdr = (void *)ext_pgs->hdr;
+	ext_pgs = &m_tls->m_ext_pgs;
+	hdr = (void *)ext_pgs->m_epg_hdr;
 	plen = ntohs(hdr->tls_length);
 
 	/*
@@ -961,8 +961,8 @@ ktls_payload_offset(struct tlspcb *tlsp, struct mbuf *m_tls)
 #endif
 
 	MBUF_EXT_PGS_ASSERT(m_tls);
-	ext_pgs = m_tls->m_ext.ext_pgs;
-	hdr = (void *)ext_pgs->hdr;
+	ext_pgs = &m_tls->m_ext_pgs;
+	hdr = (void *)ext_pgs->m_epg_hdr;
 	plen = ntohs(hdr->tls_length);
 #ifdef INVARIANTS
 	mlen = mtod(m_tls, vm_offset_t) + m_tls->m_len;
@@ -1008,7 +1008,7 @@ ktls_wr_len(struct tlspcb *tlsp, struct mbuf *m, struct mbuf *m_tls,
 	u_int imm_len, offset, plen, wr_len, tlen;
 
 	MBUF_EXT_PGS_ASSERT(m_tls);
-	ext_pgs = m_tls->m_ext.ext_pgs;
+	ext_pgs = &m_tls->m_ext_pgs;
 
 	/*
 	 * Determine the size of the TLS record payload to send
@@ -1040,7 +1040,7 @@ ktls_wr_len(struct tlspcb *tlsp, struct mbuf *m, struct mbuf *m_tls,
 		return (wr_len);
 	}
 
-	hdr = (void *)ext_pgs->hdr;
+	hdr = (void *)ext_pgs->m_epg_hdr;
 	plen = TLS_HEADER_LENGTH + ntohs(hdr->tls_length) - ext_pgs->trail_len;
 	if (tlen < plen) {
 		plen = tlen;
@@ -1474,7 +1474,7 @@ ktls_write_tunnel_packet(struct sge_txq *txq, void *dst, struct mbuf *m,
 
 	/* Locate the template TLS header. */
 	MBUF_EXT_PGS_ASSERT(m_tls);
-	ext_pgs = m_tls->m_ext.ext_pgs;
+	ext_pgs = &m_tls->m_ext_pgs;
 
 	/* This should always be the last TLS record in a chain. */
 	MPASS(m_tls->m_next == NULL);
@@ -1543,8 +1543,8 @@ ktls_write_tunnel_packet(struct sge_txq *txq, void *dst, struct mbuf *m,
 	    (m->m_pkthdr.l2hlen + m->m_pkthdr.l3hlen + sizeof(*tcp)));
 
 	/* Copy the subset of the TLS header requested. */
-	copy_to_txd(&txq->eq, (char *)ext_pgs->hdr + mtod(m_tls, vm_offset_t),
-	    &out, m_tls->m_len);
+	copy_to_txd(&txq->eq, (char *)ext_pgs->m_epg_hdr +
+	    mtod(m_tls, vm_offset_t), &out, m_tls->m_len);
 	txq->imm_wrs++;
 
 	txq->txpkt_wrs++;
@@ -1603,8 +1603,8 @@ ktls_write_tls_wr(struct tlspcb *tlsp, struct sge_txq *txq,
 
 	/* Locate the TLS header. */
 	MBUF_EXT_PGS_ASSERT(m_tls);
-	ext_pgs = m_tls->m_ext.ext_pgs;
-	hdr = (void *)ext_pgs->hdr;
+	ext_pgs = &m_tls->m_ext_pgs;
+	hdr = (void *)ext_pgs->m_epg_hdr;
 	plen = TLS_HEADER_LENGTH + ntohs(hdr->tls_length) - ext_pgs->trail_len;
 
 	/* Determine how much of the TLS record to send. */
@@ -2031,7 +2031,7 @@ ktls_write_tls_wr(struct tlspcb *tlsp, struct sge_txq *txq,
 	/* Populate the TLS header */
 	out = (void *)(tx_data + 1);
 	if (offset == 0) {
-		memcpy(out, ext_pgs->hdr, ext_pgs->hdr_len);
+		memcpy(out, ext_pgs->m_epg_hdr, ext_pgs->hdr_len);
 		out += ext_pgs->hdr_len;
 	}
 

sys/dev/cxgbe/t4_sge.c
@@ -2419,7 +2419,7 @@ count_mbuf_ext_pgs(struct mbuf *m, int skip, vm_paddr_t *nextaddr)
 	int nsegs = 0;
 
 	MBUF_EXT_PGS_ASSERT(m);
-	ext_pgs = m->m_ext.ext_pgs;
+	ext_pgs = &m->m_ext_pgs;
 	off = mtod(m, vm_offset_t);
 	len = m->m_len;
 	off += skip;
@@ -2435,7 +2435,7 @@ count_mbuf_ext_pgs(struct mbuf *m, int skip, vm_paddr_t *nextaddr)
 			off = 0;
 			len -= seglen;
 			paddr = pmap_kextract(
-			    (vm_offset_t)&ext_pgs->hdr[segoff]);
+			    (vm_offset_t)&ext_pgs->m_epg_hdr[segoff]);
 			if (*nextaddr != paddr)
 				nsegs++;
 			*nextaddr = paddr + seglen;
@@ -2454,7 +2454,7 @@ count_mbuf_ext_pgs(struct mbuf *m, int skip, vm_paddr_t *nextaddr)
 		off = 0;
 		seglen = min(seglen, len);
 		len -= seglen;
-		paddr = ext_pgs->pa[i] + segoff;
+		paddr = ext_pgs->m_epg_pa[i] + segoff;
 		if (*nextaddr != paddr)
 			nsegs++;
 		*nextaddr = paddr + seglen;
@@ -2463,7 +2463,7 @@ count_mbuf_ext_pgs(struct mbuf *m, int skip, vm_paddr_t *nextaddr)
 	if (len != 0) {
 		seglen = min(len, ext_pgs->trail_len - off);
 		len -= seglen;
-		paddr = pmap_kextract((vm_offset_t)&ext_pgs->trail[off]);
+		paddr = pmap_kextract((vm_offset_t)&ext_pgs->m_epg_trail[off]);
 		if (*nextaddr != paddr)
 			nsegs++;
 		*nextaddr = paddr + seglen;

sys/dev/cxgbe/tom/t4_cpl_io.c
@@ -732,7 +732,7 @@ t4_push_frames(struct adapter *sc, struct toepcb *toep, int drop)
 
 			if (m->m_flags & M_NOMAP) {
 #ifdef KERN_TLS
-				if (m->m_ext.ext_pgs->tls != NULL) {
+				if (m->m_ext_pgs.tls != NULL) {
 					toep->flags |= TPF_KTLS;
 					if (plen == 0) {
 						SOCKBUF_UNLOCK(sb);
@@ -1927,7 +1927,7 @@ aiotx_free_pgs(struct mbuf *m)
 	vm_page_t pg;
 
 	MBUF_EXT_PGS_ASSERT(m);
-	ext_pgs = m->m_ext.ext_pgs;
+	ext_pgs = &m->m_ext_pgs;
 	job = m->m_ext.ext_arg1;
 #ifdef VERBOSE_TRACES
 	CTR3(KTR_CXGBE, "%s: completed %d bytes for tid %d", __func__,
@@ -1935,7 +1935,7 @@ aiotx_free_pgs(struct mbuf *m)
 #endif
 
 	for (int i = 0; i < ext_pgs->npgs; i++) {
-		pg = PHYS_TO_VM_PAGE(ext_pgs->pa[i]);
+		pg = PHYS_TO_VM_PAGE(ext_pgs->m_epg_pa[i]);
 		vm_page_unwire(pg, PQ_ACTIVE);
 	}
 
@@ -1984,13 +1984,13 @@ alloc_aiotx_mbuf(struct kaiocb *job, int len)
 		if (npages < 0)
 			break;
 
-		m = mb_alloc_ext_pgs(M_WAITOK, false, aiotx_free_pgs);
+		m = mb_alloc_ext_pgs(M_WAITOK, aiotx_free_pgs);
 		if (m == NULL) {
 			vm_page_unhold_pages(pgs, npages);
 			break;
 		}
 
-		ext_pgs = m->m_ext.ext_pgs;
+		ext_pgs = &m->m_ext_pgs;
 		ext_pgs->first_pg_off = pgoff;
 		ext_pgs->npgs = npages;
 		if (npages == 1) {
@@ -2003,7 +2003,7 @@ alloc_aiotx_mbuf(struct kaiocb *job, int len)
 			    (npages - 2) * PAGE_SIZE;
 		}
 		for (i = 0; i < npages; i++)
-			ext_pgs->pa[i] = VM_PAGE_TO_PHYS(pgs[i]);
+			ext_pgs->m_epg_pa[i] = VM_PAGE_TO_PHYS(pgs[i]);
 
 		m->m_len = mlen;
 		m->m_ext.ext_size = npages * PAGE_SIZE;

sys/dev/cxgbe/tom/t4_tls.c
@@ -1568,24 +1568,26 @@ t4_push_tls_records(struct adapter *sc, struct toepcb *toep, int drop)
 
 #ifdef KERN_TLS
 static int
-count_ext_pgs_segs(struct mbuf_ext_pgs *ext_pgs)
+count_ext_pgs_segs(struct mbuf_ext_pgs *ext_pgs,
+	struct mbuf_ext_pgs_data *ext_pgs_data)
 {
 	vm_paddr_t nextpa;
 	u_int i, nsegs;
 
 	MPASS(ext_pgs->npgs > 0);
 	nsegs = 1;
-	nextpa = ext_pgs->pa[0] + PAGE_SIZE;
+	nextpa = ext_pgs_data->pa[0] + PAGE_SIZE;
 	for (i = 1; i < ext_pgs->npgs; i++) {
-		if (nextpa != ext_pgs->pa[i])
+		if (nextpa != ext_pgs_data->pa[i])
 			nsegs++;
-		nextpa = ext_pgs->pa[i] + PAGE_SIZE;
+		nextpa = ext_pgs_data->pa[i] + PAGE_SIZE;
 	}
 	return (nsegs);
 }
 
 static void
-write_ktlstx_sgl(void *dst, struct mbuf_ext_pgs *ext_pgs, int nsegs)
+write_ktlstx_sgl(void *dst, struct mbuf_ext_pgs *ext_pgs,
+    struct mbuf_ext_pgs_data *ext_pgs_data, int nsegs)
 {
 	struct ulptx_sgl *usgl = dst;
 	vm_paddr_t pa;
@@ -1598,12 +1600,12 @@ write_ktlstx_sgl(void *dst, struct mbuf_ext_pgs *ext_pgs, int nsegs)
 	    V_ULPTX_NSGE(nsegs));
 
 	/* Figure out the first S/G length. */
-	pa = ext_pgs->pa[0] + ext_pgs->first_pg_off;
+	pa = ext_pgs_data->pa[0] + ext_pgs->first_pg_off;
 	usgl->addr0 = htobe64(pa);
 	len = mbuf_ext_pg_len(ext_pgs, 0, ext_pgs->first_pg_off);
 	pa += len;
 	for (i = 1; i < ext_pgs->npgs; i++) {
-		if (ext_pgs->pa[i] != pa)
+		if (ext_pgs_data->pa[i] != pa)
 			break;
 		len += mbuf_ext_pg_len(ext_pgs, i, 0);
 		pa += mbuf_ext_pg_len(ext_pgs, i, 0);
@@ -1615,14 +1617,14 @@ write_ktlstx_sgl(void *dst, struct mbuf_ext_pgs *ext_pgs, int nsegs)
 
 	j = -1;
 	for (; i < ext_pgs->npgs; i++) {
-		if (j == -1 || ext_pgs->pa[i] != pa) {
+		if (j == -1 || ext_pgs_data->pa[i] != pa) {
 			if (j >= 0)
 				usgl->sge[j / 2].len[j & 1] = htobe32(len);
 			j++;
 #ifdef INVARIANTS
 			nsegs--;
 #endif
-			pa = ext_pgs->pa[i];
+			pa = ext_pgs_data->pa[i];
 			usgl->sge[j / 2].addr[j & 1] = htobe64(pa);
 			len = mbuf_ext_pg_len(ext_pgs, i, 0);
 			pa += len;
@@ -1744,7 +1746,7 @@ t4_push_ktls(struct adapter *sc, struct toepcb *toep, int drop)
 
 		KASSERT(m->m_flags & M_NOMAP, ("%s: mbuf %p is not NOMAP",
 		    __func__, m));
-		KASSERT(m->m_ext.ext_pgs->tls != NULL,
+		KASSERT(m->m_ext_pgs.tls != NULL,
 		    ("%s: mbuf %p doesn't have TLS session", __func__, m));
 
 		/* Calculate WR length. */
@@ -1756,7 +1758,8 @@ t4_push_ktls(struct adapter *sc, struct toepcb *toep, int drop)
 		wr_len += AES_BLOCK_LEN;
 
 		/* Account for SGL in work request length. */
-		nsegs = count_ext_pgs_segs(m->m_ext.ext_pgs);
+		nsegs = count_ext_pgs_segs(&m->m_ext_pgs,
+		    &m->m_ext.ext_pgs);
 		wr_len += sizeof(struct ulptx_sgl) +
 		    ((3 * (nsegs - 1)) / 2 + ((nsegs - 1) & 1)) * 8;
 
@@ -1810,22 +1813,22 @@ t4_push_ktls(struct adapter *sc, struct toepcb *toep, int drop)
 			return;
 		}
 
-		thdr = (struct tls_hdr *)m->m_ext.ext_pgs->hdr;
+		thdr = (struct tls_hdr *)&m->m_epg_hdr;
 #ifdef VERBOSE_TRACES
 		CTR5(KTR_CXGBE, "%s: tid %d TLS record %ju type %d len %#x",
-		    __func__, toep->tid, m->m_ext.ext_pgs->seqno, thdr->type,
+		    __func__, toep->tid, m->m_ext_pgs.seqno, thdr->type,
 		    m->m_len);
 #endif
 		txwr = wrtod(wr);
 		cpl = (struct cpl_tx_tls_sfo *)(txwr + 1);
 		memset(txwr, 0, roundup2(wr_len, 16));
 		credits = howmany(wr_len, 16);
-		expn_size = m->m_ext.ext_pgs->hdr_len +
-		    m->m_ext.ext_pgs->trail_len;
+		expn_size = m->m_ext_pgs.hdr_len +
+		    m->m_ext_pgs.trail_len;
 		tls_size = m->m_len - expn_size;
 		write_tlstx_wr(txwr, toep, 0,
 		    tls_size, expn_size, 1, credits, shove, 1);
-		toep->tls.tx_seq_no = m->m_ext.ext_pgs->seqno;
+		toep->tls.tx_seq_no = m->m_ext_pgs.seqno;
 		write_tlstx_cpl(cpl, toep, thdr, tls_size, 1);
 		tls_copy_tx_key(toep, cpl + 1);
 
@@ -1834,7 +1837,8 @@ t4_push_ktls(struct adapter *sc, struct toepcb *toep, int drop)
 		memcpy(buf, thdr + 1, toep->tls.iv_len);
 		buf += AES_BLOCK_LEN;
 
-		write_ktlstx_sgl(buf, m->m_ext.ext_pgs, nsegs);
+		write_ktlstx_sgl(buf, &m->m_ext_pgs, &m->m_ext.ext_pgs,
+		    nsegs);
 
 		KASSERT(toep->tx_credits >= credits,
 			("%s: not enough credits", __func__));

sys/kern/kern_mbuf.c
@@ -295,7 +295,6 @@ uma_zone_t	zone_pack;
 uma_zone_t	zone_jumbop;
 uma_zone_t	zone_jumbo9;
 uma_zone_t	zone_jumbo16;
-uma_zone_t	zone_extpgs;
 
 /*
  * Local prototypes.
@@ -312,9 +311,11 @@ static void	mb_reclaim(uma_zone_t, int);
 /* Ensure that MSIZE is a power of 2. */
 CTASSERT((((MSIZE - 1) ^ MSIZE) + 1) >> 1 == MSIZE);
 
-_Static_assert(sizeof(struct mbuf_ext_pgs) == 256,
-    "mbuf_ext_pgs size mismatch");
-
+_Static_assert(offsetof(struct mbuf, m_ext) ==
+    offsetof(struct mbuf, m_ext_pgs.m_ext),
+    "m_ext offset mismatch between mbuf and ext_pgs");
+_Static_assert(sizeof(struct mbuf) <= MSIZE,
+    "size of mbuf exceeds MSIZE");
 /*
  * Initialize FreeBSD Network buffer allocation.
  */
@@ -368,11 +369,6 @@ mbuf_init(void *dummy)
 		nmbjumbo16 = uma_zone_set_max(zone_jumbo16, nmbjumbo16);
 	uma_zone_set_warning(zone_jumbo16, "kern.ipc.nmbjumbo16 limit reached");
 	uma_zone_set_maxaction(zone_jumbo16, mb_reclaim);
-
-	zone_extpgs = uma_zcreate(MBUF_EXTPGS_MEM_NAME,
-	    sizeof(struct mbuf_ext_pgs),
-	    NULL, NULL, NULL, NULL,
-	    UMA_ALIGN_CACHE, 0);
 
 	/*
 	 * Hook event handler for low-memory situation, used to
@@ -840,8 +836,8 @@ mb_free_notready(struct mbuf *m, int count)
 	for (i = 0; i < count && m != NULL; i++) {
 		if ((m->m_flags & M_EXT) != 0 &&
 		    m->m_ext.ext_type == EXT_PGS) {
-			m->m_ext.ext_pgs->nrdy--;
-			if (m->m_ext.ext_pgs->nrdy != 0)
+			m->m_ext_pgs.nrdy--;
+			if (m->m_ext_pgs.nrdy != 0)
 				continue;
 		}
 		m = m_free(m);
@@ -883,27 +879,22 @@ mb_unmapped_compress(struct mbuf *m)
 	if (*refcnt != 1)
 		return (EBUSY);
 
-	/*
-	 * Copy mbuf header and m_ext portion of 'm' to 'm_temp' to
-	 * create a "fake" EXT_PGS mbuf that can be used with
-	 * m_copydata() as well as the ext_free callback.
-	 */
-	memcpy(&m_temp, m, offsetof(struct mbuf, m_ext) + sizeof (m->m_ext));
-	m_temp.m_next = NULL;
-	m_temp.m_nextpkt = NULL;
+	m_init(&m_temp, M_NOWAIT, MT_DATA, 0);
+
+	/* copy data out of old mbuf */
+	m_copydata(m, 0, m->m_len, mtod(&m_temp, char *));
+	m_temp.m_len = m->m_len;
+
+	/* Free the backing pages. */
+	m->m_ext.ext_free(m);
 
 	/* Turn 'm' into a "normal" mbuf. */
 	m->m_flags &= ~(M_EXT | M_RDONLY | M_NOMAP);
 	m->m_data = m->m_dat;
 
-	/* Copy data from template's ext_pgs. */
-	m_copydata(&m_temp, 0, m_temp.m_len, mtod(m, caddr_t));
+	/* copy data back into m */
+	m_copydata(&m_temp, 0, m_temp.m_len, mtod(m, char *));
 
-	/* Free the backing pages. */
-	m_temp.m_ext.ext_free(&m_temp);
-
-	/* Finally, free the ext_pgs struct. */
-	uma_zfree(zone_extpgs, m_temp.m_ext.ext_pgs);
 	return (0);
 }
 
@@ -959,7 +950,7 @@ _mb_unmapped_to_ext(struct mbuf *m)
 	u_int ref_inc = 0;
 
 	MBUF_EXT_PGS_ASSERT(m);
-	ext_pgs = m->m_ext.ext_pgs;
+	ext_pgs = &m->m_ext_pgs;
 	len = m->m_len;
 	KASSERT(ext_pgs->tls == NULL, ("%s: can't convert TLS mbuf %p",
 	    __func__, m));
@@ -993,7 +984,7 @@ _mb_unmapped_to_ext(struct mbuf *m)
 				goto fail;
 			m_new->m_len = seglen;
 			prev = top = m_new;
-			memcpy(mtod(m_new, void *), &ext_pgs->hdr[segoff],
+			memcpy(mtod(m_new, void *), &ext_pgs->m_epg_hdr[segoff],
 			    seglen);
 		}
 	}
@@ -1011,7 +1002,7 @@ _mb_unmapped_to_ext(struct mbuf *m)
 		seglen = min(seglen, len);
 		len -= seglen;
 
-		pg = PHYS_TO_VM_PAGE(ext_pgs->pa[i]);
+		pg = PHYS_TO_VM_PAGE(ext_pgs->m_epg_pa[i]);
 		m_new = m_get(M_NOWAIT, MT_DATA);
 		if (m_new == NULL)
 			goto fail;
@@ -1045,7 +1036,7 @@ _mb_unmapped_to_ext(struct mbuf *m)
 		else
 			prev->m_next = m_new;
 		m_new->m_len = len;
-		memcpy(mtod(m_new, void *), &ext_pgs->trail[off], len);
+		memcpy(mtod(m_new, void *), &ext_pgs->m_epg_trail[off], len);
 	}
 
 	if (ref_inc != 0) {
@@ -1132,23 +1123,16 @@ mb_unmapped_to_ext(struct mbuf *top)
  * freed.
  */
 struct mbuf *
-mb_alloc_ext_pgs(int how, bool pkthdr, m_ext_free_t ext_free)
+mb_alloc_ext_pgs(int how, m_ext_free_t ext_free)
 {
 	struct mbuf *m;
 	struct mbuf_ext_pgs *ext_pgs;
 
-	if (pkthdr)
-		m = m_gethdr(how, MT_DATA);
-	else
-		m = m_get(how, MT_DATA);
+	m = m_get(how, MT_DATA);
 	if (m == NULL)
 		return (NULL);
 
-	ext_pgs = uma_zalloc(zone_extpgs, how);
-	if (ext_pgs == NULL) {
-		m_free(m);
-		return (NULL);
-	}
+	ext_pgs = &m->m_ext_pgs;
 	ext_pgs->npgs = 0;
 	ext_pgs->nrdy = 0;
 	ext_pgs->first_pg_off = 0;
@@ -1163,7 +1147,6 @@ mb_alloc_ext_pgs(int how, bool pkthdr, m_ext_free_t ext_free)
 	m->m_ext.ext_type = EXT_PGS;
 	m->m_ext.ext_flags = EXT_FLAG_EMBREF;
 	m->m_ext.ext_count = 1;
-	m->m_ext.ext_pgs = ext_pgs;
 	m->m_ext.ext_size = 0;
 	m->m_ext.ext_free = ext_free;
 	return (m);
@@ -1180,7 +1163,7 @@ mb_ext_pgs_check(struct mbuf_ext_pgs *ext_pgs)
 	 */
 	KASSERT(ext_pgs->npgs > 0,
 	    ("ext_pgs with no valid pages: %p", ext_pgs));
-	KASSERT(ext_pgs->npgs <= nitems(ext_pgs->pa),
+	KASSERT(ext_pgs->npgs <= nitems(ext_pgs->m_epg_pa),
 	    ("ext_pgs with too many pages: %p", ext_pgs));
 	KASSERT(ext_pgs->nrdy <= ext_pgs->npgs,
 	    ("ext_pgs with too many ready pages: %p", ext_pgs));
@@ -1195,9 +1178,9 @@ mb_ext_pgs_check(struct mbuf_ext_pgs *ext_pgs)
 		    PAGE_SIZE, ("ext_pgs with single page too large: %p",
 		    ext_pgs));
 	}
-	KASSERT(ext_pgs->hdr_len <= sizeof(ext_pgs->hdr),
+	KASSERT(ext_pgs->hdr_len <= sizeof(ext_pgs->m_epg_hdr),
 	    ("ext_pgs with too large header length: %p", ext_pgs));
-	KASSERT(ext_pgs->trail_len <= sizeof(ext_pgs->trail),
+	KASSERT(ext_pgs->trail_len <= sizeof(ext_pgs->m_epg_trail),
 	    ("ext_pgs with too large header length: %p", ext_pgs));
 }
 #endif
@@ -1277,15 +1260,14 @@ mb_free_ext(struct mbuf *m)
 			    ("%s: ext_free not set", __func__));
 			mref->m_ext.ext_free(mref);
 #ifdef KERN_TLS
-			pgs = mref->m_ext.ext_pgs;
+			pgs = &mref->m_ext_pgs;
 			tls = pgs->tls;
 			if (tls != NULL &&
 			    !refcount_release_if_not_last(&tls->refcount))
 				ktls_enqueue_to_free(pgs);
 			else
 #endif
-				uma_zfree(zone_extpgs, mref->m_ext.ext_pgs);
-			uma_zfree(zone_mbuf, mref);
+				uma_zfree(zone_mbuf, mref);
 			break;
 		}
 		case EXT_SFBUF:

sys/kern/kern_sendfile.c
@@ -174,18 +174,18 @@ sendfile_free_mext_pg(struct mbuf *m)
 	    ("%s: m %p !M_EXT or !EXT_PGS", __func__, m));
 
 	cache_last = m->m_ext.ext_flags & EXT_FLAG_CACHE_LAST;
-	ext_pgs = m->m_ext.ext_pgs;
+	ext_pgs = &m->m_ext_pgs;
 	flags = (m->m_ext.ext_flags & EXT_FLAG_NOCACHE) != 0 ? VPR_TRYFREE : 0;
 
 	for (i = 0; i < ext_pgs->npgs; i++) {
 		if (cache_last && i == ext_pgs->npgs - 1)
 			flags = 0;
-		pg = PHYS_TO_VM_PAGE(ext_pgs->pa[i]);
+		pg = PHYS_TO_VM_PAGE(ext_pgs->m_epg_pa[i]);
 		vm_page_release(pg, flags);
 	}
 
 	if (m->m_ext.ext_flags & EXT_FLAG_SYNC) {
-		struct sendfile_sync *sfs = m->m_ext.ext_arg2;
+		struct sendfile_sync *sfs = m->m_ext.ext_arg1;
 
 		mtx_lock(&sfs->mtx);
 		KASSERT(sfs->count > 0, ("Sendfile sync botchup count == 0"));
@@ -329,7 +329,7 @@ sendfile_iodone(void *arg, vm_page_t *pa, int count, int error)
 #if defined(KERN_TLS) && defined(INVARIANTS)
 	if ((sfio->m->m_flags & M_EXT) != 0 &&
 	    sfio->m->m_ext.ext_type == EXT_PGS)
-		KASSERT(sfio->tls == sfio->m->m_ext.ext_pgs->tls,
+		KASSERT(sfio->tls == sfio->m->m_ext_pgs.tls,
 		    ("TLS session mismatch"));
 	else
 		KASSERT(sfio->tls == NULL,
@@ -958,7 +958,7 @@ retry_space:
 
 				ext_pgs_idx++;
 				if (ext_pgs_idx == max_pgs) {
-					m0 = mb_alloc_ext_pgs(M_WAITOK, false,
+					m0 = mb_alloc_ext_pgs(M_WAITOK,
 					    sendfile_free_mext_pg);
 
 					if (flags & SF_NOCACHE) {
@@ -979,12 +979,18 @@ retry_space:
 					if (sfs != NULL) {
 						m0->m_ext.ext_flags |=
 						    EXT_FLAG_SYNC;
-						m0->m_ext.ext_arg2 = sfs;
+						if (m0->m_ext.ext_type ==
+						    EXT_PGS)
+							m0->m_ext.ext_arg1 =
+								sfs;
+						else
+							m0->m_ext.ext_arg2 =
+								sfs;
 						mtx_lock(&sfs->mtx);
 						sfs->count++;
 						mtx_unlock(&sfs->mtx);
 					}
-					ext_pgs = m0->m_ext.ext_pgs;
+					ext_pgs = &m0->m_ext_pgs;
 					ext_pgs_idx = 0;
 
 					/* Append to mbuf chain. */
@@ -1001,7 +1007,7 @@ retry_space:
 					ext_pgs->nrdy++;
 				}
 
-				ext_pgs->pa[ext_pgs_idx] = VM_PAGE_TO_PHYS(pga);
+				ext_pgs->m_epg_pa[ext_pgs_idx] = VM_PAGE_TO_PHYS(pga);
 				ext_pgs->npgs++;
 				xfs = xfsize(i, npages, off, space);
 				ext_pgs->last_pg_len = xfs;
@@ -1055,6 +1061,10 @@ retry_space:
 				m0->m_ext.ext_flags |= EXT_FLAG_NOCACHE;
 			if (sfs != NULL) {
 				m0->m_ext.ext_flags |= EXT_FLAG_SYNC;
+				if (m0->m_ext.ext_type == EXT_PGS)
+					m0->m_ext.ext_arg1 = sfs;
+				else
+					m0->m_ext.ext_arg2 = sfs;
 				m0->m_ext.ext_arg2 = sfs;
 				mtx_lock(&sfs->mtx);
 				sfs->count++;

sys/kern/subr_bus_dma.c
@@ -123,7 +123,7 @@ _bus_dmamap_load_unmapped_mbuf_sg(bus_dma_tag_t dmat, bus_dmamap_t map,
 	int error, i, off, len, pglen, pgoff, seglen, segoff;
 
 	MBUF_EXT_PGS_ASSERT(m);
-	ext_pgs = m->m_ext.ext_pgs;
+	ext_pgs = &m->m_ext_pgs;
 
 	len = m->m_len;
 	error = 0;
@@ -141,7 +141,7 @@ _bus_dmamap_load_unmapped_mbuf_sg(bus_dma_tag_t dmat, bus_dmamap_t map,
 			off = 0;
 			len -= seglen;
 			error = _bus_dmamap_load_buffer(dmat, map,
-			    &ext_pgs->hdr[segoff], seglen, kernel_pmap,
+			    &ext_pgs->m_epg_hdr[segoff], seglen, kernel_pmap,
 			    flags, segs, nsegs);
 		}
 	}
@@ -159,7 +159,7 @@ _bus_dmamap_load_unmapped_mbuf_sg(bus_dma_tag_t dmat, bus_dmamap_t map,
 		seglen = min(seglen, len);
 		len -= seglen;
 		error = _bus_dmamap_load_phys(dmat, map,
-		    ext_pgs->pa[i] + segoff, seglen, flags, segs, nsegs);
+		    ext_pgs->m_epg_pa[i] + segoff, seglen, flags, segs, nsegs);
 		pgoff = 0;
 	};
 	if (len != 0 && error == 0) {
@@ -167,7 +167,7 @@ _bus_dmamap_load_unmapped_mbuf_sg(bus_dma_tag_t dmat, bus_dmamap_t map,
 		    ("off + len > trail (%d + %d > %d)", off, len,
 		    ext_pgs->trail_len));
 		error = _bus_dmamap_load_buffer(dmat, map,
-		    &ext_pgs->trail[off], len, kernel_pmap, flags, segs,
+		    &ext_pgs->m_epg_trail[off], len, kernel_pmap, flags, segs,
 		    nsegs);
 	}
 	return (error);

commit 0f4cf3c261de95c50ee31cf751fd6dc334c3a91b

Fix HW TLS offload regression issue after r359919, in mlx5en(4).
Changes in the mbuf layout regarding HW TLS, resulted in wrong detection
of starting mbuf. Use a boolean variable to handle this and pass m_adj()
the top mbuf, so that the packet header is adjusted correctly.

MFC after:	1 week
Sponsored by:	Mellanox Technologies


git-svn-id: svn+ssh://svn.freebsd.org/base/head@362272 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/mlx5/mlx5_en/mlx5_en_hw_tls.c
@@ -610,7 +610,7 @@ mlx5e_tls_send_nop(struct mlx5e_sq *sq, struct mlx5e_tls_tag *ptag)
 #define	SBTLS_MBUF_NO_DATA ((struct mbuf *)1)
 
 static struct mbuf *
-sbtls_recover_record(struct mbuf *mb, int wait, uint32_t tcp_old, uint32_t *ptcp_seq)
+sbtls_recover_record(struct mbuf *mb, int wait, uint32_t tcp_old, uint32_t *ptcp_seq, bool *pis_start)
 {
 	struct mbuf *mr, *top;
 	uint32_t offset;
@@ -629,6 +629,7 @@ sbtls_recover_record(struct mbuf *mb, int wait, uint32_t tcp_old, uint32_t *ptcp
 	/* check if we don't need to re-transmit anything */
 	if (offset == 0) {
 		top = SBTLS_MBUF_NO_DATA;
+		*pis_start = true;
 		goto done;
 	}
 
@@ -653,13 +654,19 @@ sbtls_recover_record(struct mbuf *mb, int wait, uint32_t tcp_old, uint32_t *ptcp
 
 	/* setup packet header length */
 	top->m_pkthdr.len = mr->m_len = offset;
+	top->m_len = 0;
 
 	/* check for partial re-transmit */
 	delta = *ptcp_seq - tcp_old;
 
 	if (delta < offset) {
-		m_adj(mr, offset - delta);
+		m_adj(top, offset - delta);
 		offset = delta;
+
+		/* continue where we left off */
+		*pis_start = false;
+	} else {
+		*pis_start = true;
 	}
 
 	/*
@@ -745,10 +752,11 @@ mlx5e_sq_tls_xmit(struct mlx5e_sq *sq, struct mlx5e_xmit_args *parg, struct mbuf
 	}
 
 	if (unlikely(ptls_tag->expected_seq != mb_seq)) {
+		bool is_start;
 		struct mbuf *r_mb;
 		uint32_t tcp_seq = mb_seq;
 
-		r_mb = sbtls_recover_record(mb, M_NOWAIT, ptls_tag->expected_seq, &tcp_seq);
+		r_mb = sbtls_recover_record(mb, M_NOWAIT, ptls_tag->expected_seq, &tcp_seq, &is_start);
 		if (r_mb == NULL) {
 			MLX5E_TLS_STAT_INC(ptls_tag, tx_error, 1);
 			return (MLX5E_TLS_FAILURE);
@@ -757,7 +765,7 @@ mlx5e_sq_tls_xmit(struct mlx5e_sq *sq, struct mlx5e_xmit_args *parg, struct mbuf
 		MLX5E_TLS_STAT_INC(ptls_tag, tx_packets_ooo, 1);
 
 		/* check if this is the first fragment of a TLS record */
-		if (r_mb == SBTLS_MBUF_NO_DATA || r_mb->m_data == NULL) {
+		if (is_start) {
 			/* setup TLS static parameters */
 			MLX5_SET64(sw_tls_cntx, ptls_tag->crypto_params,
 			    param.initial_record_number, rcd_sn);