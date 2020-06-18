e20188e60666725f1f368a9ff7519062d386c3f2
kib <kib@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-06-18 23:07:58 +0000

    rtld: Apply relro to itself.
    Reviewed by:	emaste
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D25319
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@362347 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- libexec/rtld-elf/rtld.c
+++ libexec/rtld-elf/rtld.c
@@ -2280,6 +2280,7 @@ init_rtld(caddr_t mapbase, Elf_Auxinfo **aux_info)
     obj_rtld.path = xstrdup(ld_path_rtld);
 
     parse_rtld_phdr(&obj_rtld);
+    obj_enforce_relro(&obj_rtld);
 
     r_debug.r_brk = r_debug_state;
     r_debug.r_state = RT_CONSISTENT;
