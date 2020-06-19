2f97da750539988b9bb44fcaf6a2af3e6c90c016
Konstantin Belousov <kib@FreeBSD.org>
2020-06-19 11:32:42 +0000

    MFC r362129: x86: add bits definitions for SRBDS mitigation control.

--- sys/x86/x86/identcpu.c
+++ sys/x86/x86/identcpu.c
@@ -1016,6 +1016,7 @@ printcpuinfo(void)
 				       "\003AVX512_4VNNIW"
 				       "\004AVX512_4FMAPS"
 				       "\011AVX512VP2INTERSECT"
+				       "\012MCUOPT"
 				       "\013MD_CLEAR"
 				       "\016TSXFA"
 				       "\023PCONFIG"
