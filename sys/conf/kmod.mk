98c829864f8e4d6160b63031b822fee2b7c08d23
cem <cem@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-06-23 18:25:31 +0000

    kmod.mk: Don't split out debug symbols if requested
    Ports bsd.kmod.mk explicitly sets MK_KERNEL_SYMBOLS=no to prevent auto-
    splitting of debuginfo from kernel modules.  If that knob is set, don't
    split out a .ko.debug and .ko from .ko.full; just generate a .ko with
    debuginfo and leave it be.
    
    Otherwise, with DEBUG_FLAGS set and MK_KERNEL_SYMBOLS=no, we would helpfully
    strip out the debuginfo from the .ko.full and then not install it.  That is
    not the desired result a WITH_DEBUG port kmod build.
    
    Reviewed by:	emaste, jhb
    Differential Revision:	https://reviews.freebsd.org/D24835
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@362549 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/conf/kmod.mk
+++ sys/conf/kmod.mk
@@ -215,7 +215,7 @@ OBJS+=	${SRCS:N*.h:R:S/$/.o/g}
 PROG=	${KMOD}.ko
 .endif
 
-.if !defined(DEBUG_FLAGS)
+.if !defined(DEBUG_FLAGS) || ${MK_KERNEL_SYMBOLS} == "no"
 FULLPROG=	${PROG}
 .else
 FULLPROG=	${PROG}.full
@@ -319,7 +319,7 @@ ${_ILINKS}:
 
 CLEANFILES+= ${PROG} ${KMOD}.kld ${OBJS}
 
-.if defined(DEBUG_FLAGS)
+.if defined(DEBUG_FLAGS) && ${MK_KERNEL_SYMBOLS} != "no"
 CLEANFILES+= ${FULLPROG} ${PROG}.debug
 .endif
 
