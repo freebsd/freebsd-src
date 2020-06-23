92c839a156ba96c821e8b6c16892f583115b6d40
Kirk McKusick <mckusick@FreeBSD.org>
2020-06-23 21:28:26 +0000

    The libufs library needs to track and free the new fs_si structure in addition to the fs_csp structure that it references.
    PR:           247425
    Sponsored by: Netflix

--- sbin/fsck_ffs/setup.c
+++ sbin/fsck_ffs/setup.c
@@ -216,7 +216,7 @@ setup(char *dev)
 	disk.d_ufs = (sblock.fs_magic == FS_UFS1_MAGIC) ? 1 : 2;
 	disk.d_bsize = sblock.fs_fsize / fsbtodb(&sblock, 1);
 	disk.d_sblock = sblock.fs_sblockloc / disk.d_bsize;
-	disk.d_sbcsum = sblock.fs_csp;
+	disk.d_si = sblock.fs_si;
 
 	if (skipclean && ckclean && sblock.fs_clean) {
 		pwarn("FILE SYSTEM CLEAN; SKIPPING CHECKS\n");
