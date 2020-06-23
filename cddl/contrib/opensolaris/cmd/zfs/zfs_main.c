8617bf1892c3eb62b4a91a83e9d08b5b60a9b3a2
tsoome <tsoome@FreeBSD.org>
2020-06-23 06:42:39 +0000

    MFOpenZFS: Add basic zfs ioc input nvpair validation
    We want newer versions of libzfs_core to run against an existing
    zfs kernel module (i.e. a deferred reboot or module reload after
    an update).
    
    Programmatically document, via a zfs_ioc_key_t, the valid arguments
    for the ioc commands that rely on nvpair input arguments (i.e. non
    legacy commands from libzfs_core). Automatically verify the expected
    pairs before dispatching a command.
    
    This initial phase focuses on the non-legacy ioctls. A follow-on
    change can address the legacy ioctl input from the zfs_cmd_t.
    
    The zfs_ioc_key_t for zfs_keys_channel_program looks like:
    
    static const zfs_ioc_key_t zfs_keys_channel_program[] = {
           {"program",     DATA_TYPE_STRING,               0},
           {"arg",         DATA_TYPE_UNKNOWN,              0},
           {"sync",        DATA_TYPE_BOOLEAN_VALUE,        ZK_OPTIONAL},
           {"instrlimit",  DATA_TYPE_UINT64,               ZK_OPTIONAL},
           {"memlimit",    DATA_TYPE_UINT64,               ZK_OPTIONAL},
    };
    
    Introduce four input errors to identify specific input failures
    (in addition to generic argument value errors like EINVAL, ERANGE,
    EBADF, and E2BIG).
    
    ZFS_ERR_IOC_CMD_UNAVAIL the ioctl number is not supported by kernel
    ZFS_ERR_IOC_ARG_UNAVAIL an input argument is not supported by kernel
    ZFS_ERR_IOC_ARG_REQUIRED a required input argument is missing
    ZFS_ERR_IOC_ARG_BADTYPE an input argument has an invalid type
    
    Reviewed by:	allanjude
    Obtained from:	OpenZFS
    Sponsored by:	Netflix, Klara Inc.
    Differential Revision:	https://reviews.freebsd.org/D25393

--- cddl/contrib/opensolaris/cmd/zfs/zfs_main.c
+++ cddl/contrib/opensolaris/cmd/zfs/zfs_main.c
@@ -7235,7 +7235,7 @@ zfs_do_bookmark(int argc, char **argv)
 	fnvlist_free(nvl);
 
 	if (ret != 0) {
-		const char *err_msg;
+		const char *err_msg = NULL;
 		char errbuf[1024];
 
 		(void) snprintf(errbuf, sizeof (errbuf),
@@ -7259,11 +7259,13 @@ zfs_do_bookmark(int argc, char **argv)
 			err_msg = "out of space";
 			break;
 		default:
-			err_msg = "unknown error";
+			(void) zfs_standard_error(g_zfs, ret, errbuf);
 			break;
 		}
-		(void) fprintf(stderr, "%s: %s\n", errbuf,
-		    dgettext(TEXT_DOMAIN, err_msg));
+		if (err_msg != NULL) {
+			(void) fprintf(stderr, "%s: %s\n", errbuf,
+			    dgettext(TEXT_DOMAIN, err_msg));
+		}
 	}
 
 	return (ret != 0);
@@ -7280,7 +7282,7 @@ zfs_do_channel_program(int argc, char **argv)
 	char c;
 	char *progbuf, *filename, *poolname;
 	size_t progsize, progread;
-	nvlist_t *outnvl;
+	nvlist_t *outnvl = NULL;
 	uint64_t instrlimit = ZCP_DEFAULT_INSTRLIMIT;
 	uint64_t memlimit = ZCP_DEFAULT_MEMLIMIT;
 	boolean_t sync_flag = B_TRUE, json_output = B_FALSE;
@@ -7420,7 +7422,8 @@ zfs_do_channel_program(int argc, char **argv)
 		 * falling back on strerror() for an unexpected return code.
 		 */
 		char *errstring = NULL;
-		if (nvlist_exists(outnvl, ZCP_RET_ERROR)) {
+		const char *msg = gettext("Channel program execution failed");
+		if (outnvl != NULL && nvlist_exists(outnvl, ZCP_RET_ERROR)) {
 			(void) nvlist_lookup_string(outnvl,
 			    ZCP_RET_ERROR, &errstring);
 			if (errstring == NULL)
@@ -7449,12 +7452,11 @@ zfs_do_channel_program(int argc, char **argv)
 				    "programs must be run as root.";
 				break;
 			default:
-				errstring = strerror(ret);
+				(void) zfs_standard_error(g_zfs, ret, msg);
 			}
 		}
-		(void) fprintf(stderr,
-		    gettext("Channel program execution failed:\n%s\n"),
-		    errstring);
+		if (errstring != NULL)
+			(void) fprintf(stderr, "%s:\n%s\n", msg, errstring);
 	} else {
 		if (json_output) {
 			(void) nvlist_print_json(stdout, outnvl);
