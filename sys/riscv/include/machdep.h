39ad001797e54b31c8ff06ddd573a36dc6f8db68
mhorne <mhorne@FreeBSD.org>
2020-06-24 15:20:00 +0000

    Handle load from loader(8)
    In locore, we must detect and handle different arguments passed by
    loader(8) compared to what we recieve when booting directly via SBI
    firmware. Currently we receive the hart ID in a0 and a pointer to the
    device tree blob in a1. loader(8) provides only a pointer to its
    metadata in a0.
    
    The solution to this is to add an additional entry point, _alt_start.
    This will be placed first in the .text section, so SBI firmware will
    enter here, and jump to the common pagetable setup shortly after. Since
    loader(8) understands our ELF kernel, it will enter at the ELF's entry
    address, which points to _start. This approach leads to very little
    guesswork as to which way we booted.
    
    Fix-up initriscv() to parse the loader's metadata, continuing to use
    fake_preload_metadata() in the SBI direct boot case.
    
    Reviewed by:	markj, jrtc27 (asm portion)
    Differential Revision:	https://reviews.freebsd.org/D24912

--- sys/riscv/include/machdep.h
+++ sys/riscv/include/machdep.h
@@ -43,12 +43,12 @@ struct riscv_bootparams {
 	vm_offset_t	kern_stack;
 	vm_offset_t	dtbp_virt;	/* Device tree blob virtual addr */
 	vm_offset_t	dtbp_phys;	/* Device tree blob physical addr */
+	vm_offset_t	modulep;	/* loader(8) metadata */
 };
 
 extern vm_paddr_t physmap[PHYS_AVAIL_ENTRIES];
 extern u_int physmap_idx;
 
-vm_offset_t fake_preload_metadata(struct riscv_bootparams *rbp);
 void initriscv(struct riscv_bootparams *);
 
 #endif /* _MACHINE_MACHDEP_H_ */
