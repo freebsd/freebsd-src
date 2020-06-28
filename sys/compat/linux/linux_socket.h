7f9acf82ca01891b6c97fea5facfd9695fde7985
trasz <trasz@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-06-28 18:56:32 +0000

    Make linux(4) support SO_PROTOCOL.  Running Python test suite with python3.8 from Focal triggers those.
    MFC after:	2 weeks
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D25491
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@362735 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/compat/linux/linux_socket.h
+++ sys/compat/linux/linux_socket.h
@@ -197,6 +197,7 @@ int linux_accept(struct thread *td, struct linux_accept_args *args);
 #define	LINUX_SO_ACCEPTCONN	30
 #define	LINUX_SO_SNDBUFFORCE	32
 #define	LINUX_SO_RCVBUFFORCE	33
+#define	LINUX_SO_PROTOCOL	38
 
 /* Socket options */
 #define	LINUX_IP_TOS		1
