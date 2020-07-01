4e6f62a85387ad437f8e428da62f814ce54ded1e
Michael Tuexen <tuexen@FreeBSD.org>
2020-07-01 21:42:52 +0000

    MFC r356227: Add flags for upcoming patches related to improved ECN handling. No functional change.
    Submitted by:		rscheff
    Reviewed by:		rgrimes, tuexen
    Differential Revision:	https://reviews.freebsd.org/D22429

--- sys/netinet/tcp_syncache.h
+++ sys/netinet/tcp_syncache.h
@@ -90,6 +90,10 @@ struct syncache {
 #define SCF_SIGNATURE	0x20			/* send MD5 digests */
 #define SCF_SACK	0x80			/* send SACK option */
 #define SCF_ECN		0x100			/* send ECN setup packet */
+#define SCF_ACE_N	0x200			/* send ACE non-ECT setup */
+#define SCF_ACE_0	0x400			/* send ACE ECT0 setup */
+#define SCF_ACE_1	0x800			/* send ACE ECT1 setup */
+#define SCF_ACE_CE	0x1000			/* send ACE CE setup */
 
 struct syncache_head {
 	struct mtx	sch_mtx;
