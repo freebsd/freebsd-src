4886ed8d88b5001f856de4cf09f2c677f31f808a
mhorne <mhorne@FreeBSD.org>
2020-07-06 18:19:42 +0000

    RISC-V boot1.efi and loader.efi support
    This implementation doesn't have any major deviations from the other EFI
    ports. I've copied the boilerplate from arm and arm64.
    
    I've tested this with the following boot flows:
    OpenSBI (M-mode) -> u-boot (S-mode) -> loader.efi -> FreeBSD
    OpenSBI (M-mode) -> u-boot (S-mode) -> boot1.efi -> loader.efi -> FreeBSD
    
    Due to the way that u-boot handles secondary CPUs, OpenSBI >= v0.7 is required,
    as the HSM extension is needed to bring them up explicitly. Because of this,
    using BBL as the SBI implementation will not be possible. Additionally, there
    are a few recent u-boot changes that are required as well, all of which will be
    present in the upcoming v2020.07 release.
    
    Looks good:	emaste
    Differential Revision:	https://reviews.freebsd.org/D25135

--- stand/defs.mk
+++ stand/defs.mk
@@ -115,13 +115,14 @@ AFLAGS+=	--32
 SSP_CFLAGS=
 
 # Add in the no float / no SIMD stuff and announce we're freestanding
-# aarch64 and riscv don't have -msoft-float, but all others do. riscv
-# currently has no /boot/loader, but may soon.
+# aarch64 and riscv don't have -msoft-float, but all others do.
 CFLAGS+=	-ffreestanding ${CFLAGS_NO_SIMD}
 .if ${MACHINE_CPUARCH} == "aarch64"
 CFLAGS+=	-mgeneral-regs-only -ffixed-x18 -fPIC
 .elif ${MACHINE_CPUARCH} == "riscv"
-CFLAGS+=	-march=rv64imac -mabi=lp64
+CFLAGS+=	-march=rv64imac -mabi=lp64 -fPIC
+CFLAGS.clang+=	-mcmodel=medium
+CFLAGS.gcc+=	-mcmodel=medany
 .else
 CFLAGS+=	-msoft-float
 .endif
@@ -146,6 +147,12 @@ CFLAGS+=	-fPIC -mno-red-zone
 CFLAGS.clang+=	-mno-movt
 CFLAGS.clang+=  -mfpu=none
 CFLAGS+=	-fPIC
+.endif
+
+# Some RISC-V linkers have support for relaxations, while some (lld) do not
+# yet. If this is the case we inhibit the compiler from emitting relaxations.
+.if ${LINKER_FEATURES:Mriscv-relaxations} == ""
+CFLAGS+=	-mno-relax
 .endif
 
 # The boot loader build uses dd status=none, where possible, for reproducible
