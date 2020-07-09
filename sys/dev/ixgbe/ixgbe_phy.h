80a39a2bda8208c716f614f792c8d9a3628d6c26
Guinan Sun <guinanx.sun@intel.com>
2020-07-09 08:00:36 +0000

    ixgbe: create function to restart autoneg
    This patch is for restarting auto negotiation on PHY.
    
    Signed-off-by: Jakub Chylkowski <jakubx.chylkowski@intel.com>
    Signed-off-by: Guinan Sun <guinanx.sun@intel.com>
    Reviewed-by: Wei Zhao <wei.zhao1@intel.com>
    
    Approved by:	imp
    Obtained from:	DPDK (664ea2614eafbec8eda5c86764ff047475a1e5c6)
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D31621

--- sys/dev/ixgbe/ixgbe_phy.h
+++ sys/dev/ixgbe/ixgbe_phy.h
@@ -163,6 +163,7 @@ enum ixgbe_phy_type ixgbe_get_phy_type_from_id(u32 phy_id);
 s32 ixgbe_get_phy_id(struct ixgbe_hw *hw);
 s32 ixgbe_identify_phy_generic(struct ixgbe_hw *hw);
 s32 ixgbe_reset_phy_generic(struct ixgbe_hw *hw);
+void ixgbe_restart_auto_neg(struct ixgbe_hw *hw);
 s32 ixgbe_read_phy_reg_mdi(struct ixgbe_hw *hw, u32 reg_addr, u32 device_type,
 			   u16 *phy_data);
 s32 ixgbe_write_phy_reg_mdi(struct ixgbe_hw *hw, u32 reg_addr, u32 device_type,
