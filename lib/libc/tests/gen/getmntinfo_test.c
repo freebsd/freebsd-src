1f4e879031e788b11000bb65c1460988ac59f94d
Li-Wen Hsu <lwhsu@FreeBSD.org>
2020-07-13 18:19:02 +0000

    Revert r351416 to let lib.libc.gen.getmntinfo_test.getmntinfo_test get more test
    This is supposed to be fixed by r363068
    
    PR:		240049
    Sponsored by:	The FreeBSD Foundation

--- lib/libc/tests/gen/getmntinfo_test.c
+++ lib/libc/tests/gen/getmntinfo_test.c
@@ -57,9 +57,6 @@ ATF_TC_BODY(getmntinfo_test, tc)
 	int nmnts;
 	struct statfs *mntinfo;
 
-	if (atf_tc_get_config_var_as_bool_wd(tc, "ci", false))
-		atf_tc_skip("https://bugs.freebsd.org/240049");
-
 	/* Test bogus mode */
 	nmnts = getmntinfo(&mntinfo, 199);
 	ATF_REQUIRE_MSG(nmnts == 0 && errno == EINVAL,
