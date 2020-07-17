91a852ebeef85c9d86604493f49dbda88330d43e
br <br@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-07-17 14:51:51 +0000

    Add acpi_iort_map_pci_smmuv3().
    This new function allows us to find the SMMU instance assigned
    for a particular PCI RID.
    
    Reviewed by:	andrew
    Sponsored by:	DARPA, AFRL
    Differential Revision:	https://reviews.freebsd.org/D25687
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@363274 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/acpica/acpivar.h
+++ sys/dev/acpica/acpivar.h
@@ -556,6 +556,7 @@ int		acpi_get_domain(device_t dev, device_t child, int *domain);
  * ARM specific ACPI interfaces, relating to IORT table.
  */
 int	acpi_iort_map_pci_msi(u_int seg, u_int rid, u_int *xref, u_int *devid);
+int	acpi_iort_map_pci_smmuv3(u_int seg, u_int rid, u_int *xref, u_int *devid);
 int	acpi_iort_its_lookup(u_int its_id, u_int *xref, int *pxm);
 #endif
 #endif /* _KERNEL */
