3ec7e1695c7db1a892a57b29dff9d2b12c7baa13
Konstantin Belousov <kib@FreeBSD.org>
2020-07-18 18:19:57 +0000

    amd64 pmap: microoptimize local shootdowns for PCID PTI configurations
    When pmap operates in PTI mode, we must reload %cr3 on return to
    userspace.  In non-PCID mode the reload always flushes all non-global
    TLB entries and we take advantage of it by only invalidating the KPT
    TLB entries (there is no cached UPT entries at all).
    
    In PCID mode, we flush both KPT and UPT TLB explicitly, but we can
    take advantage of the fact that PCID mode command to reload %cr3
    includes a flag to flush/not flush target TLB.  In particular, we can
    avoid the flush for UPT, instead record that load of pc_ucr3 into %cr3
    on return to usermode should be flushing.  This is done by providing
    either all-1s or ~CR3_PCID_MASK in pc_ucr3_load_mask.  The mask is
    automatically reset to all-1s on return to usermode.
    
    Similarly, we can avoid flushing UPT TLB on context switch, replacing
    it by setting pc_ucr3_load_mask.  This unifies INVPCID and non-INVPCID
    PTI ifunc, leaving only 4 cases instead of 6.  This trick is also
    applicable both to the TLB shootdown IPI handlers, since handlers
    interrupt the target thread.
    
    But then we need to check pc_curpmap in handlers, and this would
    reopen the same race for INVPCID machines as was fixed in r306350 for
    non-INVPCID.  To not introduce the same bug, unconditionally do
    spinlock_enter() in pmap_activate().
    
    Reviewed by:	alc, markj
    Tested by:	pho
    Sponsored by:	The FreeBSD Foundation
    MFC after:	3 weeks
    Differential revision:	https://reviews.freebsd.org/D25483

--- sys/amd64/amd64/genassym.c
+++ sys/amd64/amd64/genassym.c
@@ -230,6 +230,7 @@ ASSYM(PC_TSS, offsetof(struct pcpu, pc_tss));
 ASSYM(PC_PM_SAVE_CNT, offsetof(struct pcpu, pc_pm_save_cnt));
 ASSYM(PC_KCR3, offsetof(struct pcpu, pc_kcr3));
 ASSYM(PC_UCR3, offsetof(struct pcpu, pc_ucr3));
+ASSYM(PC_UCR3_LOAD_MASK, offsetof(struct pcpu, pc_ucr3_load_mask));
 ASSYM(PC_SAVED_UCR3, offsetof(struct pcpu, pc_saved_ucr3));
 ASSYM(PC_PTI_STACK, offsetof(struct pcpu, pc_pti_stack));
 ASSYM(PC_PTI_STACK_SZ, PC_PTI_STACK_SZ);
