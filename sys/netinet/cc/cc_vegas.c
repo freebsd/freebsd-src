510ae54d7be5dc9f4eccd45dfdc2140b280649e8
rscheff <rscheff@FreeBSD.org>
2020-07-20 23:47:27 +0000

    Add MODULE_VERSION to TCP loadable congestion control modules.
    Without versioning information, using preexisting loader /
    linker code is not easily possible when another module may
    have dependencies on pre-loaded modules, and also doesn't
    allow the automatic loading of dependent modules.
    
    No functional change of the actual modules.
    
    Reviewed by:	tuexen (mentor), rgrimes (mentor)
    Approved by:	tuexen (mentor), rgrimes (mentor)
    MFC after:	2 weeks
    Sponsored by:	NetApp, Inc.
    Differential Revision:	https://reviews.freebsd.org/D25744

--- sys/netinet/cc/cc_vegas.c
+++ sys/netinet/cc/cc_vegas.c
@@ -301,4 +301,5 @@ SYSCTL_PROC(_net_inet_tcp_cc_vegas, OID_AUTO, beta,
     "vegas beta, specified as number of \"buffers\" (0 < alpha < beta)");
 
 DECLARE_CC_MODULE(vegas, &vegas_cc_algo);
+MODULE_VERSION(vegas, 1);
 MODULE_DEPEND(vegas, ertt, 1, 1, 1);
