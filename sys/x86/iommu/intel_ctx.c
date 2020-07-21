b0f96e1ec87eb2151d77bba62ea66b723a71c490
br <br@FreeBSD.org>
2020-07-21 10:38:51 +0000

    Move the Intel DMAR busdma backend to a generic place so it can be used on other IOMMU systems.
    Reviewed by:	kib
    Sponsored by:	DARPA/AFRL
    Differential Revision:	https://reviews.freebsd.org/D25720

--- sys/x86/iommu/intel_ctx.c
+++ sys/x86/iommu/intel_ctx.c
@@ -66,7 +66,7 @@ __FBSDID("$FreeBSD$");
 #include <contrib/dev/acpica/include/accommon.h>
 #include <x86/include/busdma_impl.h>
 #include <x86/iommu/intel_reg.h>
-#include <x86/iommu/busdma_dmar.h>
+#include <dev/iommu/busdma_iommu.h>
 #include <dev/pci/pcireg.h>
 #include <x86/iommu/intel_dmar.h>
 #include <dev/pci/pcivar.h>
