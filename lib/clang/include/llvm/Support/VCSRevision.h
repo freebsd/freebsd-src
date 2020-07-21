27d3dd3a6e35cc2bac77703662af6cc53544a58a
dim <dim@FreeBSD.org>
2020-07-21 17:34:05 +0000

    Merge llvm, clang, compiler-rt, libc++, libunwind, lld, lldb and openmp 10.0.1 final (aka llvmorg-10.0.1-0-gef32c611aa2).
    There were no changes since rc2, except in the upstream regression
    tests, which we do not ship.
    
    Relnotes:	yes
    MFC after:	immediately (no material changes except tag)

--- lib/clang/include/llvm/Support/VCSRevision.h
+++ lib/clang/include/llvm/Support/VCSRevision.h
@@ -1,3 +1,3 @@
 /* $FreeBSD$ */
-#define LLVM_REVISION "llvmorg-10.0.1-rc2-0-g77d76b71d7d"
+#define LLVM_REVISION "llvmorg-10.0.1-0-gef32c611aa2"
 #define LLVM_REPOSITORY "git@github.com:llvm/llvm-project.git"
