cb67b873436a53e46f2b71ec20c398a645fe8c10
markj <markj@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-07-23 14:21:45 +0000

    MFOpenZFS: Fix zpool history unbounded memory usage
    In original implementation, zpool history will read the whole history
    before printing anything, causing memory usage goes unbounded. We fix
    this by breaking it into read-print iterations.
    
    Reviewed-by: Tom Caputi <tcaputi@datto.com>
    Reviewed-by: Matt Ahrens <matt@delphix.com>
    Reviewed-by: Igor Kozhukhov <igor@dilos.org>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Chunwei Chen <david.chen@nutanix.com>
    Closes #9516
    
    Note, this change changes the libzfs.so ABI by modifying the prototype
    of zpool_get_history().  Since libzfs is effectively private to the base
    system it is anticipated that this will not be a problem.
    
    PR:		247557
    Obtained from:	OpenZFS
    Reported and tested by:	Sam Vaughan <samjvaughan@gmail.com>
    Discussed with:	freqlabs
    MFC after:	2 weeks
    Differential Revision:	https://reviews.freebsd.org/D25745
    openzfs/zfs@7125a109dcc55628336ff3f58e59e503f4d7694d
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@363447 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- cddl/contrib/opensolaris/lib/libzfs/common/libzfs.h
+++ cddl/contrib/opensolaris/lib/libzfs/common/libzfs.h
@@ -441,7 +441,8 @@ typedef enum {
 extern char *zpool_vdev_name(libzfs_handle_t *, zpool_handle_t *, nvlist_t *,
     int name_flags);
 extern int zpool_upgrade(zpool_handle_t *, uint64_t);
-extern int zpool_get_history(zpool_handle_t *, nvlist_t **);
+extern int zpool_get_history(zpool_handle_t *, nvlist_t **, uint64_t *,
+    boolean_t *);
 extern int zpool_history_unpack(char *, uint64_t, uint64_t *,
     nvlist_t ***, uint_t *);
 extern void zpool_obj_to_path(zpool_handle_t *, uint64_t, uint64_t, char *,
