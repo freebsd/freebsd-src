9381a25cf2e00ffc93ee8f20d9c22f73333266df
br <br@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-07-28 16:08:14 +0000

    o Move the buswide_ctxs bitmap to iommu_unit and rename related functions. o Rename bus_dma_dmar_load_ident() as well.
    Reviewed by:	kib
    Sponsored by:	DARPA/AFRL
    Differential Revision:	https://reviews.freebsd.org/D25852
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@363650 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/x86/x86/busdma_machdep.c
+++ sys/x86/x86/busdma_machdep.c
@@ -301,13 +301,13 @@ bus_dma_tag_destroy(bus_dma_tag_t dmat)
 
 #ifndef ACPI_DMAR
 bool
-bus_dma_dmar_set_buswide(device_t dev)
+bus_dma_iommu_set_buswide(device_t dev)
 {
 	return (false);
 }
 
 int
-bus_dma_dmar_load_ident(bus_dma_tag_t dmat, bus_dmamap_t map,
+bus_dma_iommu_load_ident(bus_dma_tag_t dmat, bus_dmamap_t map,
     vm_paddr_t start, vm_size_t length, int flags)
 {
 	return (0);
