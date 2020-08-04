8fce2bfdfc6f2d4f105e5e603369bf03b49670e7
John Baldwin <jhb@FreeBSD.org>
2020-08-04 18:19:29 +0000

    Turn off errors for -Wmaybe-uninitialized in GCC 6+.
    Recent changes to <sys/tree.h> trigger this warning and seem like a
    false positive.
    
    Differential Revision:	https://reviews.freebsd.org/D25726
    
    (cherry picked from commit a02fb76280fd663aa46843423002d605a7bd0796)

--- share/mk/bsd.sys.mk
+++ share/mk/bsd.sys.mk
@@ -150,7 +150,8 @@ CWARNFLAGS+=	-Wno-error=address			\
 
 # GCC 6.1.0
 .if ${COMPILER_VERSION} >= 60100
-CWARNFLAGS+=	-Wno-error=nonnull-compare		\
+CWARNFLAGS+=	-Wno-error=maybe-uninitialized		\
+		-Wno-error=nonnull-compare		\
 		-Wno-error=shift-negative-value		\
 		-Wno-error=tautological-compare		\
 		-Wno-error=unused-const-variable
