16696f6057eeef9c756837470f0c0fff3efe7697
Ruslan Bukin <br@FreeBSD.org>
2020-08-06 08:48:23 +0000

    Add iommu_domain constructor and destructor.
    Reviewed by:	kib
    Sponsored by:	DARPA/AFRL
    Differential Revision:	https://reviews.freebsd.org/D25956

--- sys/dev/iommu/busdma_iommu.c
+++ sys/dev/iommu/busdma_iommu.c
@@ -1063,3 +1063,23 @@ bus_dma_iommu_load_ident(bus_dma_tag_t dmat, bus_dmamap_t map1,
 	free(ma, M_TEMP);
 	return (error);
 }
+
+void
+iommu_domain_init(struct iommu_unit *unit, struct iommu_domain *domain,
+    const struct iommu_domain_map_ops *ops)
+{
+
+	domain->ops = ops;
+	domain->iommu = unit;
+
+	RB_INIT(&domain->rb_root);
+	TAILQ_INIT(&domain->unload_entries);
+	mtx_init(&domain->lock, "iodom", NULL, MTX_DEF);
+}
+
+void
+iommu_domain_fini(struct iommu_domain *domain)
+{
+
+	mtx_destroy(&domain->lock);
+}
