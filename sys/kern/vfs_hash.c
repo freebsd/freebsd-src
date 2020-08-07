e7d6284d0df7b5dca901e35e23acacddf3a9d38e
Mateusz Guzik <mjg@FreeBSD.org>
2020-08-07 23:32:42 +0000

    MFC r363931:
        vfs hash: annotate the lock with __exclusive_cache_line

--- sys/kern/vfs_hash.c
+++ sys/kern/vfs_hash.c
@@ -43,7 +43,7 @@ static MALLOC_DEFINE(M_VFS_HASH, "vfs_hash", "VFS hash table");
 static LIST_HEAD(vfs_hash_head, vnode)	*vfs_hash_tbl;
 static LIST_HEAD(,vnode)		vfs_hash_side;
 static u_long				vfs_hash_mask;
-static struct rwlock			vfs_hash_lock;
+static struct rwlock __exclusive_cache_line vfs_hash_lock;
 
 static void
 vfs_hashinit(void *dummy __unused)
