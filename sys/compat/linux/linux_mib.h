9df1da4e120cac9f58848380c45eb8f81c8858cc
Edward Tomasz Napierala <trasz@FreeBSD.org>
2020-08-13 19:05:51 +0000

    MFC r351723:
    Bump Linux version to 3.2.0.  Without it, binaries linked against
    glibc 2.24 and up (eg Ubuntu 19.04) fail with "FATAL: kernel too old".
    
    Sponsored by:	The FreeBSD Foundation

--- sys/compat/linux/linux_mib.h
+++ sys/compat/linux/linux_mib.h
@@ -46,9 +46,9 @@ int	linux_get_oss_version(struct thread *td);
 
 int	linux_kernver(struct thread *td);
 
-#define	LINUX_KVERSION		2
-#define	LINUX_KPATCHLEVEL	6
-#define	LINUX_KSUBLEVEL		36
+#define	LINUX_KVERSION		3
+#define	LINUX_KPATCHLEVEL	2
+#define	LINUX_KSUBLEVEL		0
 
 #define	LINUX_KERNVER(a,b,c)	(((a) << 16) + ((b) << 8) + (c))
 #define	LINUX_VERSION_CODE	LINUX_KERNVER(LINUX_KVERSION,		\
