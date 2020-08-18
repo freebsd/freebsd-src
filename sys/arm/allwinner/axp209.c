73d641c77e14fb1943b366cd85334ef1b7b9e376
imp <imp@FreeBSD.org>
2020-08-18 20:20:45 +0000

    None of system, subsystem, or type may have spaces in them. Convert the spaces to dashes.
    Approved by: manu@

--- sys/arm/allwinner/axp209.c
+++ sys/arm/allwinner/axp209.c
@@ -863,9 +863,9 @@ axp2xx_intr(void *arg)
 		if (reg & AXP2XX_IRQ2_BATT_DISCONN)
 			devctl_notify("PMU", "Battery", "disconnected", NULL);
 		if (reg & AXP2XX_IRQ2_BATT_TEMP_LOW)
-			devctl_notify("PMU", "Battery", "low temp", NULL);
+			devctl_notify("PMU", "Battery", "low-temp", NULL);
 		if (reg & AXP2XX_IRQ2_BATT_TEMP_OVER)
-			devctl_notify("PMU", "Battery", "high temp", NULL);
+			devctl_notify("PMU", "Battery", "high-temp", NULL);
 		axp2xx_write(sc->dev, AXP2XX_IRQ2_STATUS, AXP2XX_IRQ_ACK);
 	}
 
