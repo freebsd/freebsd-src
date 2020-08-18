d5e3895ea4fe4ef9db8823774e07b4368180a23e
Mateusz Guzik <mjg@FreeBSD.org>
2020-08-18 22:03:55 +0000

    linux: consistently use LFREEPATH instead of open-coding it

--- sys/amd64/linux/linux_machdep.c
+++ sys/amd64/linux/linux_machdep.c
@@ -104,7 +104,7 @@ linux_execve(struct thread *td, struct linux_execve_args *args)
 
 	error = exec_copyin_args(&eargs, path, UIO_SYSSPACE, args->argp,
 	    args->envp);
-	free(path, M_TEMP);
+	LFREEPATH(path);
 	if (error == 0)
 		error = linux_common_execve(td, &eargs);
 	return (error);
