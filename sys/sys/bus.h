0a6d62fac1abce3c9f0bdbae5e8e22bff4d9b691
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-08-20 17:14:33 +0000

    Make devctl_queue_data_f and devctl_queue_data private.
    I thought we'd need them, but nobody is using them. Narrow the interface. This
    will facilitate changes in the future.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@364427 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/bus.h
+++ sys/sys/bus.h
@@ -156,16 +156,13 @@ struct devreq {
 
 /**
  * devctl hooks.  Typically one should use the devctl_notify
- * hook to send the message.  However, devctl_queue_data is also
- * included in case devctl_notify isn't sufficiently general.
+ * hook to send the message.
  */
 boolean_t devctl_process_running(void);
 void devctl_notify_f(const char *__system, const char *__subsystem,
     const char *__type, const char *__data, int __flags);
 void devctl_notify(const char *__system, const char *__subsystem,
     const char *__type, const char *__data);
-void devctl_queue_data_f(char *__data, int __flags);
-void devctl_queue_data(char *__data);
 struct sbuf;
 void devctl_safe_quote_sb(struct sbuf *__sb, const char *__src);
 
