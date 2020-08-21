7a372944d9eeb533a5df0f40e3bcbb4c18ab5541
vangyzen <vangyzen@FreeBSD.org>
2020-08-21 19:34:41 +0000

    ixgbe: fix impossible condition
    Coverity flagged this condition: The condition
        offset == 0 && offset == 65535
    can never be true because offset cannot be equal
    to two different values at the same time.
    
    Submitted by:	bret_ketchum@dell.com
    Reported by:	Coverity
    Reviewed by:	tsoome, cem
    MFC after:	2 weeks
    Sponsored by:	Dell EMC Isilon
    Differential Revision:	https://reviews.freebsd.org/D26144

--- sys/dev/ixgbe/ixgbe_common.c
+++ sys/dev/ixgbe/ixgbe_common.c
@@ -5147,8 +5147,8 @@ void ixgbe_get_oem_prod_version(struct ixgbe_hw *hw,
 	nvm_ver->oem_valid = FALSE;
 	hw->eeprom.ops.read(hw, NVM_OEM_PROD_VER_PTR, &offset);
 
-	/* Return is offset to OEM Product Version block is invalid */
-	if (offset == 0x0 && offset == NVM_INVALID_PTR)
+	/* Return if offset to OEM Product Version block is invalid */
+	if (offset == 0x0 || offset == NVM_INVALID_PTR)
 		return;
 
 	/* Read product version block */
