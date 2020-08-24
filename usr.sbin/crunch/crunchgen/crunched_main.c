b33b8a4c24797446cb27698672126f9c66e6ebf6
arichardson <arichardson@FreeBSD.org>
2020-08-24 09:20:23 +0000

    Correctly determine the real executable in crunched binaries
    This should fix cases like su setting argv[0] to _su for /bin/sh.
    Previously cheribsdbox (a crunched tool we use in CheriBSD to reduce the
    size of our minimal disk images to allow loading them onto FPGAs without
    waiting forever for the transfer) would complain about _su not being
    compiled in, but now that we also look at AT_EXECPATH it correctly
    invokes the sh tool.
    
    Note: we use use AT_EXECPATH instead of the KERN_PROC_PATHNAME sysctl to get
    the crunchgen binary name since it seems like KERN_PROC_PATHNAME just
    returns the last cached path for a given hardlink.
    When using `su`, instead of invoking /bin/csh this would invoke the last
    used hardlink to cheribsdbox. This caused weird test failures when running
    tests due to `id` being executed instead of `echo`:
    
    $ id  # id is a hardlink to /bin/cheribsdbox
    $ su postgres -c 'echo 1' # su is also a hardlink
    uid=1001(postgres) gid=1001(postgres) groups=1001(postgres)
    
    Obtained from: CheriBSD
    
    Reviewed By:	emaste, brooks
    Differential Revision: https://reviews.freebsd.org/D25998

--- usr.sbin/crunch/crunchgen/crunched_main.c
+++ usr.sbin/crunch/crunchgen/crunched_main.c
@@ -23,6 +23,38 @@
  *			   Computer Science Department
  *			   University of Maryland at College Park
  */
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright 2020 Alex Richardson <arichardson@FreeBSD.org>
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory (Department of Computer Science and
+ * Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"), as part of the
+ * DARPA SSITH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
 /*
  * crunched_main.c - main program for crunched binaries, it branches to a
  * 	particular subprogram based on the value of argv[0].  Also included
@@ -35,6 +67,11 @@
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
+#include <sys/param.h>
+#include <sys/auxv.h>
+#include <sys/sysctl.h>
+
+#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -44,30 +81,88 @@ struct stub {
 	int (*f)();
 };
 
-extern char *__progname;
+extern const char *__progname;
 extern struct stub entry_points[];
 
 static void crunched_usage(void);
 
-int
-main(int argc, char **argv, char **envp)
+static struct stub *
+find_entry_point(const char *basename)
 {
-	char *slash, *basename;
-	struct stub *ep;
-
-	if (argv[0] == NULL || *argv[0] == '\0')
-		crunched_usage();
-
-	slash = strrchr(argv[0], '/');
-	basename = slash ? slash + 1 : argv[0];
+	struct stub *ep = NULL;
 
 	for (ep = entry_points; ep->name != NULL; ep++)
 		if (!strcmp(basename, ep->name))
 			break;
 
-	if (ep->name)
+	return (ep);
+}
+
+static const char *
+get_basename(const char *exe_path)
+{
+	const char *slash = strrchr(exe_path, '/');
+	return (slash ? slash + 1 : exe_path);
+}
+
+int
+main(int argc, char **argv, char **envp)
+{
+	struct stub *ep = NULL;
+	const char *basename = NULL;
+
+	/*
+	 * Look at __progname first (this will be set if the crunched binary is
+	 * invoked directly).
+	 */
+	if (__progname) {
+		basename = get_basename(__progname);
+		ep = find_entry_point(basename);
+	}
+
+	/*
+	 * Otherwise try to find entry point based on argv[0] (this works for
+	 * both symlinks as well as hardlinks). However, it does not work when
+	 * su invokes a crunched shell because it sets argv[0] to _su when
+	 * invoking the shell. In that case we look at AT_EXECPATH as a
+	 * fallback.
+	 */
+	if (ep == NULL) {
+		basename = get_basename(argv[0]);
+		ep = find_entry_point(basename);
+	}
+
+	/*
+	 * If we didn't find the entry point based on __progname or argv[0],
+	 * try AT_EXECPATH to get the actual binary that was executed.
+	 */
+	if (ep == NULL) {
+		char buf[MAXPATHLEN];
+		int error = elf_aux_info(AT_EXECPATH, &buf, sizeof(buf));
+
+		if (error == 0) {
+			const char *exe_name = get_basename(buf);
+			/*
+			 * Keep using argv[0] if AT_EXECPATH is the crunched
+			 * binary so that symlinks to the crunched binary report
+			 * "not compiled in" instead of invoking
+			 * crunched_main().
+			 */
+			if (strcmp(exe_name, EXECNAME) != 0) {
+				basename = exe_name;
+				ep = find_entry_point(basename);
+			}
+		} else {
+			warnc(error, "elf_aux_info(AT_EXECPATH) failed");
+		}
+	}
+
+	if (basename == NULL || *basename == '\0')
+		crunched_usage();
+
+	if (ep != NULL) {
 		return ep->f(argc, argv, envp);
-	else {
+	} else {
 		fprintf(stderr, "%s: %s not compiled in\n", EXECNAME, basename);
 		crunched_usage();
 	}
@@ -76,16 +171,10 @@ main(int argc, char **argv, char **envp)
 int
 crunched_main(int argc, char **argv, char **envp)
 {
-	char *slash;
-	struct stub *ep;
-	int columns, len;
-
 	if (argc <= 1)
 		crunched_usage();
 
-	slash = strrchr(argv[1], '/');
-	__progname = slash ? slash + 1 : argv[1];
-
+	__progname = get_basename(argv[1]);
 	return main(--argc, ++argv, envp);
 }
 
