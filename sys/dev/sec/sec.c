73361fbc9509e7701dcd0d5eb2bb1e9842411ddd
bdragon <bdragon@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-08-26 19:30:42 +0000

    [PowerPC] Fix build failure in sec.c
    Fix a typo in r364799 that was breaking powerpc and powerpcspe build.
    
    MFC with:	364799
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@364833 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/sec/sec.c
+++ sys/dev/sec/sec.c
@@ -852,7 +852,7 @@ sec_desc_map_dma(struct sec_softc *sc, struct sec_dma_mem *dma_mem,
 		size = m_length(crp->crp_buf.cb_mbuf, NULL);
 		break;
 	case CRYPTO_BUF_VMPAGE:
-		size = PAGE_SIZE - cb->cb_vm_page_offset;
+		size = PAGE_SIZE - crp->crp_buf.cb_vm_page_offset;
 		break;
 	default:
 		return (EINVAL);
