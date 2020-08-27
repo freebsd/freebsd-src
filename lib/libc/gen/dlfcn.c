32dde97c4098bdd0d7b3c77b34aa9b68d0438010
Konstantin Belousov <kib@FreeBSD.org>
2020-08-27 00:33:01 +0000

    MFC r364423: dl_iterate_phdr(3): provide exclusive locking for callback when statically linked.

--- lib/libc/gen/dlfcn.c
+++ lib/libc/gen/dlfcn.c
@@ -40,6 +40,7 @@ __FBSDID("$FreeBSD$");
 #include <pthread.h>
 #include "un-namespace.h"
 #include "libc_private.h"
+#include "reentrant.h"
 
 static char sorry[] = "Service unavailable";
 
@@ -162,6 +163,7 @@ _rtld_thread_init(void *li __unused)
 #ifndef IN_LIBDL
 static pthread_once_t dl_phdr_info_once = PTHREAD_ONCE_INIT;
 static struct dl_phdr_info phdr_info;
+static mutex_t dl_phdr_info_lock = MUTEX_INITIALIZER;
 
 static void
 dl_init_phdr_info(void)
@@ -202,13 +204,17 @@ int
 dl_iterate_phdr(int (*callback)(struct dl_phdr_info *, size_t, void *) __unused,
     void *data __unused)
 {
-
 #ifndef IN_LIBDL
+	int ret;
+
 	__init_elf_aux_vector();
 	if (__elf_aux_vector == NULL)
 		return (1);
 	_once(&dl_phdr_info_once, dl_init_phdr_info);
-	return (callback(&phdr_info, sizeof(phdr_info), data));
+	mutex_lock(&dl_phdr_info_lock);
+	ret = callback(&phdr_info, sizeof(phdr_info), data);
+	mutex_unlock(&dl_phdr_info_lock);
+	return (ret);
 #else
 	return (0);
 #endif
