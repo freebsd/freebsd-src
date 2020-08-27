84b16547713032e21da068847605e4f7bd7e775c
freqlabs <freqlabs@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-08-27 13:05:41 +0000

    libzfs: Add missing crypto dependency
    libzfs_crypto.c uses PKCS5_PBKDF2_HMAC_SHA1 from libcrypto.
    
    Reported by:	John Kennedy
    Sponsored by:	iXsystems, Inc.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@364861 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- share/mk/src.libnames.mk
+++ share/mk/src.libnames.mk
@@ -382,7 +382,7 @@ _DP_fifolog=	z
 _DP_ipf=	kvm
 _DP_tpool=	spl
 _DP_uutil=	avl spl
-_DP_zfs=	md pthread umem util uutil m avl bsdxml geom nvpair \
+_DP_zfs=	md pthread umem util uutil m avl bsdxml crypto geom nvpair \
 	z zfs_core zutil
 _DP_zfs_core=	nvpair
 _DP_zpool=	md pthread z icp spl nvpair avl umem
