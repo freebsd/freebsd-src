c203fd8cf30665dfa555129b945cc1c78701c5a5
jilles <jilles@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-08-28 15:35:45 +0000

    sh: Keep ignored SIGINT/SIGQUIT after set in a background job
    If job control is not enabled, a background job (... &) ignores SIGINT and
    SIGQUIT, but this can be reverted using the trap builtin in the same shell
    environment.
    
    Using the set builtin to change options would also revert SIGINT and SIGQUIT
    to their previous dispositions.
    
    This broke due to r317298. Calling setsignal() reverts the effect of
    ignoresig().
    
    Reported by:	bdrewery
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@364919 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- bin/sh/trap.h
+++ bin/sh/trap.h
@@ -45,6 +45,7 @@ void ignoresig(int);
 int issigchldtrapped(void);
 void onsig(int);
 void dotrap(void);
+void trap_init(void);
 void setinteractive(void);
 void exitshell(int) __dead2;
 void exitshell_savedstatus(void) __dead2;
