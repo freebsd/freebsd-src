b8e804ba5d67fd1628626a3ac4de4cdd2e259f20
Warner Losh <imp@FreeBSD.org>
2020-08-31 23:31:16 +0000

    Warn for the non pccard attachments
    These devices have non-pccard attachments. Warn for those as well. Both an and
    wi don't do the modern cyrpto needed to use these cards on secure wifi networks.
    an needs firmware from Cisco, which I don't think was ever produced. wi could
    in theory do it with raw frames and on-host encryption, but nobody has written
    that in the 15 years since WEP was cracked.
    
    MFC After: 3 days
    Noticed by: rgrimes
    Differential Revision: https://reviews.freebsd.org/D26138

--- sys/dev/wi/if_wi_pccard.c
+++ sys/dev/wi/if_wi_pccard.c
@@ -200,7 +200,7 @@ wi_pccard_attach(device_t dev)
 		error = wi_attach(dev);
 		if (error != 0)
 			wi_free(dev);
-		gone_in_dev(dev, 13, "pccard removed");
+		gone_in_dev(dev, 13, "pccard removed, wi doesn't support modern crypto");
 	}
 	return error;
 }
