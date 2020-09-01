391486af755d87c6e21e35dcd1173680a43511d3
Mateusz Guzik <mjg@FreeBSD.org>
2020-09-01 22:03:55 +0000

    ntb: clean up empty lines in .c and .h files

--- sys/dev/ntb/ntb_transport.c
+++ sys/dev/ntb/ntb_transport.c
@@ -757,8 +757,6 @@ ntb_transport_link_up(struct ntb_transport_qp *qp)
 		callout_reset(&qp->link_work, 0, ntb_qp_link_work, qp);
 }
 
-
-
 /* Transport Tx */
 
 /**
@@ -1204,7 +1202,6 @@ ntb_transport_link_work(void *arg)
 		mw->rx_size = val64;
 		val64 = roundup(val64, mw->xlat_align_size);
 		if (mw->buff_size != val64) {
-
 			rc = ntb_set_mw(nt, i, val64);
 			if (rc != 0) {
 				ntb_printf(0, "link up set mw%d fails, rc %d\n",
@@ -1592,7 +1589,6 @@ ntb_send_link_down(struct ntb_transport_qp *qp)
 
 	ntb_qp_link_down_reset(qp);
 }
-
 
 /* List Management */
 
