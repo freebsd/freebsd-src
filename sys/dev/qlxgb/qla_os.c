4e2f0f58caf7097e9479ce1b9c6bf1cefacec542
markj <markj@FreeBSD.org>
2020-09-01 20:13:50 +0000

    qlxgb: Initialize if_mtu before setting max_frame_size.
    Previously we were relying on ether_ifattach() to set if_mtu, but
    max_frame_size is initialized earlier.  This fixes a regression
    introduced by r250375.
    
    PR:		249050
    Submitted by:	Christian Vallières <novacrash_@hotmail.com>
    MFC after:	3 days

--- sys/dev/qlxgb/qla_os.c
+++ sys/dev/qlxgb/qla_os.c
@@ -671,6 +671,7 @@ qla_init_ifnet(device_t dev, qla_host_t *ha)
 
 	if_initname(ifp, device_get_name(dev), device_get_unit(dev));
 
+	ifp->if_mtu = ETHERMTU;
 	ifp->if_baudrate = IF_Gbps(10);
 	ifp->if_init = qla_init;
 	ifp->if_softc = ha;
