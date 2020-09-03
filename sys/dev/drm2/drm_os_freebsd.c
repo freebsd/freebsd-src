f94babe3b83be3f31c8b97bac190a8474b3bff4f
zeising <zeising@FreeBSD.org>
2020-09-03 05:25:39 +0000

    drm2: Fix build after r365264
    Fix the build after r365264, I forgot to exclude arm in one more place.
    
    Reported by:	rpokala
    Approved by:	manu (implicit, build fix)
    MFC after:	3 days
    X-MFC-With:	365264
    Pointy-hat to:	zeising

--- sys/dev/drm2/drm_os_freebsd.c
+++ sys/dev/drm2/drm_os_freebsd.c
@@ -126,7 +126,9 @@ drm_probe_helper(device_t kdev, const drm_pci_id_list_t *idlist)
 			    device_get_nameunit(kdev), id_entry->name);
 			device_set_desc(kdev, id_entry->name);
 		}
+#if !defined(__arm__)
 		DRM_OBSOLETE(kdev);
+#endif
 		return (-BUS_PROBE_GENERIC);
 	}
 
