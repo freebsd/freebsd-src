25f4a7a62cfe936381262b4eca9a8c9b35526b7b
brooks <brooks@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-09-04 21:55:22 +0000

    Round TF_SIZE up to the stack alignment (16-bytes).
    The kernel adjusts the stack by TF_SIZE and the RISC-V ABI requires
    that it remain 16-byte aligned.
    
    Reported by:	CHERI, jrtc27
    Reviewed by:	mhorne
    Obtained from:	CheriBSD
    Sponsored by:	DARPA
    Differential Revision:	https://reviews.freebsd.org/D26328
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@365344 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/riscv/riscv/genassym.c
+++ sys/riscv/riscv/genassym.c
@@ -87,7 +87,7 @@ ASSYM(TD_FRAME, offsetof(struct thread, td_frame));
 ASSYM(TD_MD, offsetof(struct thread, td_md));
 ASSYM(TD_LOCK, offsetof(struct thread, td_lock));
 
-ASSYM(TF_SIZE, sizeof(struct trapframe));
+ASSYM(TF_SIZE, roundup2(sizeof(struct trapframe), STACKALIGNBYTES + 1));
 ASSYM(TF_RA, offsetof(struct trapframe, tf_ra));
 ASSYM(TF_SP, offsetof(struct trapframe, tf_sp));
 ASSYM(TF_GP, offsetof(struct trapframe, tf_gp));
