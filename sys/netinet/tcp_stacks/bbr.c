67d224ef43cb3cf13bcf5cf512d3b49ea0652368
Bjoern A. Zeeb <bz@FreeBSD.org>
2020-09-05 00:20:32 +0000

    bbr: remove unused static function
    bbr_log_type_hrdwtso() is a file local static unused function.
    Remove it to avoid warnings on kernel compiles.
    
    Reviewed by:	gallatin
    Differential Revision:	https://reviews.freebsd.org/D26331

--- sys/netinet/tcp_stacks/bbr.c
+++ sys/netinet/tcp_stacks/bbr.c
@@ -11716,31 +11716,6 @@ done_with_input:
 }
 
 static void
-bbr_log_type_hrdwtso(struct tcpcb *tp, struct tcp_bbr *bbr, int len, int mod, int what_we_can_send)
-{
-	if (tp->t_logstate != TCP_LOG_STATE_OFF) {
-		union tcp_log_stackspecific log;
-		struct timeval tv;
-		uint32_t cts;
-
-		cts = tcp_get_usecs(&tv);
-		bbr_fill_in_logging_data(bbr, &log.u_bbr, cts);
-		log.u_bbr.flex1 = bbr->r_ctl.rc_pace_min_segs;
-		log.u_bbr.flex2 = what_we_can_send;
-		log.u_bbr.flex3 = bbr->r_ctl.rc_pace_max_segs;
-		log.u_bbr.flex4 = len;
-		log.u_bbr.flex5 = 0;
-		log.u_bbr.flex7 = mod;
-		log.u_bbr.flex8 = 1;
-		TCP_LOG_EVENTP(tp, NULL,
-		    &tp->t_inpcb->inp_socket->so_rcv,
-		    &tp->t_inpcb->inp_socket->so_snd,
-		    TCP_HDWR_TLS, 0,
-		    0, &log, false, &tv);
-	}
-}
-
-static void
 bbr_do_segment(struct mbuf *m, struct tcphdr *th, struct socket *so,
     struct tcpcb *tp, int32_t drop_hdrlen, int32_t tlen, uint8_t iptos)
 {
