freebsd/freebsd-src

commit c84fd589672b24d2dbc2e40a354263ba2b7ac99b

Expose threads-per-core and physical core count information
With new sysctls (to the best of our ability do detect them).  Restructured
smp.4 slightly for clarity (keep relevant stuff closer to the top) while
documenting.

Reviewed by:	markj, jhibbits (ppc parts)
MFC after:	3 days
Sponsored by:	Dell EMC Isilon
Differential Revision:	https://reviews.freebsd.org/D18322


git-svn-id: svn+ssh://svn.freebsd.org/base/head@342771 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/powerpc/powernv/platform_powernv.c
@@ -435,12 +435,16 @@ powernv_smp_topo(platform_t plat)
 		break;
 	}
 
+	smp_threads_per_core = nthreads;
+
 	if (mp_ncpus % nthreads != 0) {
 		printf("WARNING: Irregular SMP topology. Performance may be "
 		     "suboptimal (%d threads, %d on first core)\n",
 		     mp_ncpus, nthreads);
 		return (smp_topo_none());
 	}
+
+	mp_ncores = mp_ncpus / nthreads;
 
 	/* Don't do anything fancier for non-threaded SMP */
 	if (nthreads == 1)

sys/powerpc/powerpc/mp_machdep.c
@@ -186,6 +186,11 @@ cpu_mp_start(void)
 next:
 		error = platform_smp_next_cpu(&cpu);
 	}
+
+#ifdef SMP
+	/* Probe mp_ncores and smp_threads_per_core as a side effect. */
+	(void)cpu_topo();
+#endif
 }
 
 void

sys/powerpc/ps3/platform_ps3.c
@@ -246,6 +246,8 @@ ps3_smp_start_cpu(platform_t plat, struct pcpu *pc)
 static struct cpu_group *
 ps3_smp_topo(platform_t plat)
 {
+	mp_ncores = 1;
+	smp_threads_per_core = 2;
 	return (smp_topo_1level(CG_SHARE_L1, 2, CG_FLAG_SMT));
 }
 #endif

sys/powerpc/pseries/platform_chrp.c
@@ -517,6 +517,8 @@ chrp_smp_topo(platform_t plat)
 		ncpus++;
 	}
 
+	mp_ncores = ncores;
+
 	if (ncpus % ncores != 0) {
 		printf("WARNING: Irregular SMP topology. Performance may be "
 		     "suboptimal (%d CPUS, %d cores)\n", ncpus, ncores);
@@ -527,6 +529,7 @@ chrp_smp_topo(platform_t plat)
 	if (ncpus == ncores)
 		return (smp_topo_none());
 
+	smp_threads_per_core = ncpus / ncores;
 	return (smp_topo_1level(CG_SHARE_L1, ncpus / ncores, CG_FLAG_SMT));
 }
 #endif

share/man/man4/smp.4
@@ -23,7 +23,7 @@
 .\"
 .\" $FreeBSD$
 .\"
-.Dd January 6, 2018
+.Dd January 4, 2019
 .Dt SMP 4
 .Os
 .Sh NAME
@@ -35,27 +35,6 @@
 The
 .Nm
 kernel implements symmetric multi-processor support.
-.Sh COMPATIBILITY
-Support for multi-processor systems is present for all Tier-1
-architectures on
-.Fx .
-Currently, this includes amd64, i386 and sparc64.
-Support is enabled using
-.Cd options SMP .
-It is permissible to use the SMP kernel configuration on non-SMP equipped
-motherboards.
-.Sh I386 NOTES
-For i386 systems, the
-.Nm
-kernel supports motherboards that follow the Intel MP specification,
-version 1.4.
-In addition to
-.Cd options SMP ,
-i386 also requires
-.Cd device apic .
-The
-.Xr mptable 1
-command may be used to view the status of multi-processor support.
 .Pp
 .Nm
 support can be disabled by setting the loader tunable
@@ -66,6 +45,13 @@ The number of CPUs detected by the system is available in
 the read-only sysctl variable
 .Va hw.ncpu .
 .Pp
+The number of online threads per CPU core is available in the read-only sysctl
+variable
+.Va kern.smp.threads_per_core .
+The number of physical CPU cores detected by the system is available in the
+read-only sysctl variable
+.Va kern.smp.cores .
+.Pp
 .Fx
 allows specific CPUs on a multi-processor system to be disabled.
 This can be done using the
@@ -73,6 +59,12 @@ This can be done using the
 tunable, where X is the APIC ID of a CPU.
 Setting this tunable to 1 will result in the corresponding CPU being
 disabled.
+.Pp
+.Fx
+supports simultaneous multithreading on x86 and powerpc platforms.
+On x86, the logical CPUs can be disabled by setting the
+.Va machdep.hyperthreading_allowed
+tunable to zero.
 .Pp
 The
 .Xr sched_ule 4
@@ -122,13 +114,26 @@ two quad-core processors is:
 .Pp
 This information is used internally by the kernel to schedule related
 tasks on CPUs that are closely grouped together.
-.Pp
-.Fx
-supports hyperthreading on Intel CPU's on the i386 and AMD64 platforms.
-Because using logical CPUs can cause performance penalties under certain loads,
-the logical CPUs can be disabled by setting the
-.Va machdep.hyperthreading_allowed
-tunable to zero.
+.Sh COMPATIBILITY
+Support for multi-processor systems is present for all Tier-1 and Tier-2
+architectures on
+.Fx .
+Currently, this includes x86, powerpc, arm, and sparc64.
+Support is enabled using
+.Cd options SMP .
+It is permissible to use the SMP kernel configuration on non-SMP hardware.
+.Sh I386 NOTES
+For i386 systems, the
+.Nm
+kernel supports motherboards that follow the Intel MP specification,
+version 1.4.
+In addition to
+.Cd options SMP ,
+i386 also requires
+.Cd device apic .
+The
+.Xr mptable 1
+command may be used to view the status of multi-processor support.
 .Sh SEE ALSO
 .Xr cpuset 1 ,
 .Xr mptable 1 ,
@@ -166,3 +171,20 @@ in
 also introduced support for SMP on the sparc64 architecture.
 .Sh AUTHORS
 .An Steve Passe Aq Mt fsmp@FreeBSD.org
+.Sh CAVEATS
+The
+.Va kern.smp.threads_per_core
+and
+.Va kern.smp.cores
+sysctl variables are provided as a best-effort guess.
+If an architecture or platform adds SMT and
+.Fx
+has not yet implemented detection, the reported values may be inaccurate.
+In this case,
+.Va kern.smp.threads_per_core
+will report
+.Dv 1
+and
+.Va kern.smp.cores
+will report the same value as
+.Va hw.ncpu .

sys/kern/subr_smp.c
@@ -98,6 +98,14 @@ int smp_cpus = 1;	/* how many cpu's running */
 SYSCTL_INT(_kern_smp, OID_AUTO, cpus, CTLFLAG_RD|CTLFLAG_CAPRD, &smp_cpus, 0,
     "Number of CPUs online");
 
+int smp_threads_per_core = 1;	/* how many SMT threads are running per core */
+SYSCTL_INT(_kern_smp, OID_AUTO, threads_per_core, CTLFLAG_RD|CTLFLAG_CAPRD,
+    &smp_threads_per_core, 0, "Number of SMT threads online per core");
+
+int mp_ncores = -1;	/* how many physical cores running */
+SYSCTL_INT(_kern_smp, OID_AUTO, cores, CTLFLAG_RD|CTLFLAG_CAPRD, &mp_ncores, 0,
+    "Number of CPUs online");
+
 int smp_topology = 0;	/* Which topology we're using. */
 SYSCTL_INT(_kern_smp, OID_AUTO, topology, CTLFLAG_RDTUN, &smp_topology, 0,
     "Topology override setting; 0 is default provided by hardware.");
@@ -154,6 +162,7 @@ mp_start(void *dummy)
 
 	/* Probe for MP hardware. */
 	if (smp_disabled != 0 || cpu_mp_probe() == 0) {
+		mp_ncores = 1;
 		mp_ncpus = 1;
 		CPU_SETOF(PCPU_GET(cpuid), &all_cpus);
 		return;
@@ -162,6 +171,11 @@ mp_start(void *dummy)
 	cpu_mp_start();
 	printf("FreeBSD/SMP: Multiprocessor System Detected: %d CPUs\n",
 	    mp_ncpus);
+
+	/* Provide a default for most architectures that don't have SMT/HTT. */
+	if (mp_ncores < 0)
+		mp_ncores = mp_ncpus;
+
 	cpu_mp_announce();
 }
 SYSINIT(cpu_mp, SI_SUB_CPU, SI_ORDER_THIRD, mp_start, NULL);
@@ -823,6 +837,7 @@ static void
 mp_setvariables_for_up(void *dummy)
 {
 	mp_ncpus = 1;
+	mp_ncores = 1;
 	mp_maxid = PCPU_GET(cpuid);
 	CPU_SETOF(mp_maxid, &all_cpus);
 	KASSERT(PCPU_GET(cpuid) == 0, ("UP must have a CPU ID of zero"));

sys/sys/smp.h
@@ -167,8 +167,10 @@ extern cpuset_t logical_cpus_mask;
 
 extern u_int mp_maxid;
 extern int mp_maxcpus;
+extern int mp_ncores;
 extern int mp_ncpus;
 extern volatile int smp_started;
+extern int smp_threads_per_core;
 
 extern cpuset_t all_cpus;
 extern cpuset_t cpuset_domain[MAXMEMDOM]; 	/* CPUs in each NUMA domain. */

sys/x86/x86/mp_x86.c
@@ -607,6 +607,7 @@ assign_cpu_ids(void)
 {
 	struct topo_node *node;
 	u_int smt_mask;
+	int nhyper;
 
 	smt_mask = (1u << core_id_shift) - 1;
 
@@ -615,6 +616,7 @@ assign_cpu_ids(void)
 	 * beyond MAXCPU.  CPU 0 is always assigned to the BSP.
 	 */
 	mp_ncpus = 0;
+	nhyper = 0;
 	TOPO_FOREACH(node, &topo_root) {
 		if (node->type != TOPO_TYPE_PU)
 			continue;
@@ -642,6 +644,9 @@ assign_cpu_ids(void)
 			continue;
 		}
 
+		if (cpu_info[node->hwid].cpu_hyperthread)
+			nhyper++;
+
 		cpu_apic_ids[mp_ncpus] = node->hwid;
 		apic_cpuids[node->hwid] = mp_ncpus;
 		topo_set_pu_id(node, mp_ncpus);
@@ -651,6 +656,9 @@ assign_cpu_ids(void)
 	KASSERT(mp_maxid >= mp_ncpus - 1,
 	    ("%s: counters out of sync: max %d, count %d", __func__, mp_maxid,
 	    mp_ncpus));
+
+	mp_ncores = mp_ncpus - nhyper;
+	smp_threads_per_core = mp_ncpus / mp_ncores;
 }
 
 /*

commit 40d8b0e9afdc5804a3d70de54976038debbd7034

MFC r342852 (by cem): powerpc: Fix regression introduced in r342771
In r342771, I introduced a regression in Power by abusing the platform
smp_topo() method as a shortcut for providing the MI information needed for
the stated sysctls.  The smp_topo() method was already called later by
sched_ule (under the name cpu_topo()), and initializes a static array of
scheduler topology information.  I had skimmed the smp_topo_foo() functions
and assumed they were idempotent; empirically, they are not (or at least,
detect re-initialization and panic).

Do the cleaner thing I should have done in the first place and add a
platform method specifically for core- and thread-count probing.

PR:		249250

sys/powerpc/include/platform.h
@@ -58,6 +58,7 @@ int	platform_smp_get_bsp(struct cpuref *);
 int	platform_smp_start_cpu(struct pcpu *);
 void	platform_smp_timebase_sync(u_long tb, int ap);
 void	platform_smp_ap_init(void);
+void	platform_smp_probe_threads(void);
   
 const char *installed_platform(void);
 void platform_probe_and_attach(void);

sys/powerpc/powernv/platform_powernv.c
@@ -71,6 +71,7 @@ static int powernv_smp_get_bsp(platform_t, struct cpuref *cpuref);
 static void powernv_smp_ap_init(platform_t);
 #ifdef SMP
 static int powernv_smp_start_cpu(platform_t, struct pcpu *cpu);
+static void powernv_smp_probe_threads(platform_t);
 static struct cpu_group *powernv_smp_topo(platform_t plat);
 #endif
 static void powernv_reset(platform_t);
@@ -89,6 +90,7 @@ static platform_method_t powernv_methods[] = {
 	PLATFORMMETHOD(platform_smp_get_bsp,	powernv_smp_get_bsp),
 #ifdef SMP
 	PLATFORMMETHOD(platform_smp_start_cpu,	powernv_smp_start_cpu),
+	PLATFORMMETHOD(platform_smp_probe_threads,	powernv_smp_probe_threads),
 	PLATFORMMETHOD(platform_smp_topo,	powernv_smp_topo),
 #endif
 
@@ -403,8 +405,8 @@ powernv_smp_start_cpu(platform_t plat, struct pcpu *pc)
 	return (0);
 }
 
-static struct cpu_group *
-powernv_smp_topo(platform_t plat)
+static void
+powernv_smp_probe_threads(platform_t plat)
 {
 	char buf[8];
 	phandle_t cpu, dev, root;
@@ -436,21 +438,26 @@ powernv_smp_topo(platform_t plat)
 	}
 
 	smp_threads_per_core = nthreads;
+	if (mp_ncpus % nthreads == 0)
+		mp_ncores = mp_ncpus / nthreads;
+}
 
-	if (mp_ncpus % nthreads != 0) {
+static struct cpu_group *
+powernv_smp_topo(platform_t plat)
+{
+	if (mp_ncpus % smp_threads_per_core != 0) {
 		printf("WARNING: Irregular SMP topology. Performance may be "
 		     "suboptimal (%d threads, %d on first core)\n",
-		     mp_ncpus, nthreads);
+		     mp_ncpus, smp_threads_per_core);
 		return (smp_topo_none());
 	}
 
-	mp_ncores = mp_ncpus / nthreads;
-
 	/* Don't do anything fancier for non-threaded SMP */
-	if (nthreads == 1)
+	if (smp_threads_per_core == 1)
 		return (smp_topo_none());
 
-	return (smp_topo_1level(CG_SHARE_L1, nthreads, CG_FLAG_SMT));
+	return (smp_topo_1level(CG_SHARE_L1, smp_threads_per_core,
+	    CG_FLAG_SMT));
 }
 
 #endif

sys/powerpc/powerpc/mp_machdep.c
@@ -182,8 +182,7 @@ next:
 	}
 
 #ifdef SMP
-	/* Probe mp_ncores and smp_threads_per_core as a side effect. */
-	(void)cpu_topo();
+	platform_smp_probe_threads();
 #endif
 }
 

sys/powerpc/powerpc/platform.c
@@ -242,6 +242,12 @@ platform_smp_ap_init()
 	PLATFORM_SMP_AP_INIT(plat_obj);
 }
 
+void
+platform_smp_probe_threads(void)
+{
+	PLATFORM_SMP_PROBE_THREADS(plat_obj);
+}
+
 #ifdef SMP
 struct cpu_group *
 cpu_topo(void)

sys/powerpc/powerpc/platform_if.m
@@ -84,6 +84,10 @@ CODE {
 	{
 		return;
 	}
+	static void platform_null_smp_probe_threads(void)
+	{
+		return;
+	}
 };
 
 /**
@@ -195,6 +199,13 @@ METHOD int smp_start_cpu {
 METHOD void smp_ap_init {
 	platform_t	_plat;
 } DEFAULT platform_null_smp_ap_init;
+
+/**
+ * @brief Probe mp_ncores and smp_threads_per_core for early MI code
+ */
+METHOD void smp_probe_threads {
+	platform_t	_plat;
+} DEFAULT platform_null_smp_probe_threads;
 
 /**
  * @brief Return SMP topology

sys/powerpc/ps3/platform_ps3.c
@@ -70,6 +70,7 @@ static int ps3_smp_first_cpu(platform_t, struct cpuref *cpuref);
 static int ps3_smp_next_cpu(platform_t, struct cpuref *cpuref);
 static int ps3_smp_get_bsp(platform_t, struct cpuref *cpuref);
 static int ps3_smp_start_cpu(platform_t, struct pcpu *cpu);
+static void ps3_smp_probe_threads(platform_t);
 static struct cpu_group *ps3_smp_topo(platform_t);
 #endif
 static void ps3_reset(platform_t);
@@ -87,6 +88,7 @@ static platform_method_t ps3_methods[] = {
 	PLATFORMMETHOD(platform_smp_next_cpu,	ps3_smp_next_cpu),
 	PLATFORMMETHOD(platform_smp_get_bsp,	ps3_smp_get_bsp),
 	PLATFORMMETHOD(platform_smp_start_cpu,	ps3_smp_start_cpu),
+	PLATFORMMETHOD(platform_smp_probe_threads,	ps3_smp_probe_threads),
 	PLATFORMMETHOD(platform_smp_topo,	ps3_smp_topo),
 #endif
 
@@ -243,11 +245,16 @@ ps3_smp_start_cpu(platform_t plat, struct pcpu *pc)
 	return ((pc->pc_awake) ? 0 : EBUSY);
 }
 
-static struct cpu_group *
-ps3_smp_topo(platform_t plat)
+static void
+ps3_smp_probe_threads(platform_t plat)
 {
 	mp_ncores = 1;
 	smp_threads_per_core = 2;
+}
+
+static struct cpu_group *
+ps3_smp_topo(platform_t plat)
+{
 	return (smp_topo_1level(CG_SHARE_L1, 2, CG_FLAG_SMT));
 }
 #endif

sys/powerpc/pseries/platform_chrp.c
@@ -78,6 +78,7 @@ static void chrp_smp_ap_init(platform_t);
 static int chrp_cpuref_init(void);
 #ifdef SMP
 static int chrp_smp_start_cpu(platform_t, struct pcpu *cpu);
+static void chrp_smp_probe_threads(platform_t plat);
 static struct cpu_group *chrp_smp_topo(platform_t plat);
 #endif
 static void chrp_reset(platform_t);
@@ -103,6 +104,7 @@ static platform_method_t chrp_methods[] = {
 	PLATFORMMETHOD(platform_smp_get_bsp,	chrp_smp_get_bsp),
 #ifdef SMP
 	PLATFORMMETHOD(platform_smp_start_cpu,	chrp_smp_start_cpu),
+	PLATFORMMETHOD(platform_smp_probe_threads,	chrp_smp_probe_threads),
 	PLATFORMMETHOD(platform_smp_topo,	chrp_smp_topo),
 #endif
 
@@ -499,13 +501,13 @@ chrp_smp_start_cpu(platform_t plat, struct pcpu *pc)
 	return ((pc->pc_awake) ? 0 : EBUSY);
 }
 
-static struct cpu_group *
-chrp_smp_topo(platform_t plat)
+static void
+chrp_smp_probe_threads(platform_t plat)
 {
 	struct pcpu *pc, *last_pc;
-	int i, ncores, ncpus;
+	int i, ncores;
 
-	ncores = ncpus = 0;
+	ncores = 0;
 	last_pc = NULL;
 	for (i = 0; i <= mp_maxid; i++) {
 		pc = pcpu_find(i);
@@ -514,23 +516,29 @@ chrp_smp_topo(platform_t plat)
 		if (last_pc == NULL || pc->pc_hwref != last_pc->pc_hwref)
 			ncores++;
 		last_pc = pc;
-		ncpus++;
 	}
 
 	mp_ncores = ncores;
+	if (mp_ncpus % ncores == 0)
+		smp_threads_per_core = mp_ncpus / ncores;
+}
 
-	if (ncpus % ncores != 0) {
+static struct cpu_group *
+chrp_smp_topo(platform_t plat)
+{
+
+	if (mp_ncpus % mp_ncores != 0) {
 		printf("WARNING: Irregular SMP topology. Performance may be "
-		     "suboptimal (%d CPUS, %d cores)\n", ncpus, ncores);
+		     "suboptimal (%d CPUS, %d cores)\n", mp_ncpus, mp_ncores);
 		return (smp_topo_none());
 	}
 
 	/* Don't do anything fancier for non-threaded SMP */
-	if (ncpus == ncores)
+	if (mp_ncpus == mp_ncores)
 		return (smp_topo_none());
 
-	smp_threads_per_core = ncpus / ncores;
-	return (smp_topo_1level(CG_SHARE_L1, ncpus / ncores, CG_FLAG_SMT));
+	return (smp_topo_1level(CG_SHARE_L1, smp_threads_per_core,
+	    CG_FLAG_SMT));
 }
 #endif
 