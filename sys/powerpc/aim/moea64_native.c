736ec6b542b9a28c5491e9c86d9ef0e23731b239
bdragon <bdragon@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-09-15 20:25:38 +0000

    [PowerPC64LE] Set up the powernv partition table correctly.
    The partition table is always big endian.
    
    Sponsored by:	Tag1 Consulting, Inc.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@365768 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/powerpc/aim/moea64_native.c
+++ sys/powerpc/aim/moea64_native.c
@@ -556,9 +556,9 @@ moea64_bootstrap_native(vm_offset_t kernelstart, vm_offset_t kernelend)
 	    sizeof(struct lpteg));
 	if (cpu_features2 & PPC_FEATURE2_ARCH_3_00) {
 		bzero(__DEVOLATILE(void *, moea64_part_table), PART_SIZE);
-		moea64_part_table[0].pagetab =
+		moea64_part_table[0].pagetab = htobe64(
 			(DMAP_TO_PHYS((vm_offset_t)moea64_pteg_table)) |
-			(uintptr_t)(flsl((moea64_pteg_count - 1) >> 11));
+			(uintptr_t)(flsl((moea64_pteg_count - 1) >> 11)));
 	}
 	ENABLE_TRANS(msr);
 
