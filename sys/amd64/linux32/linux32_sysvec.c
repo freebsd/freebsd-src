5b6a0196959ece5796cea9bb56d66d52a508de31
trasz <trasz@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-09-15 16:41:21 +0000

    Move SV_ABI_ERRNO translation into linux-specific code, to simplify the syscall path and declutter it a bit.  No functional changes intended.
    Reviewed by:	kib (earlier version)
    MFC after:	2 weeks
    Sponsored by:	DARPA
    Differential Revision:	https://reviews.freebsd.org/D26378
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@365755 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/amd64/linux32/linux32_sysvec.c
+++ sys/amd64/linux32/linux32_sysvec.c
@@ -112,6 +112,7 @@ static void	linux32_fixlimit(struct rlimit *rl, int which);
 static bool	linux32_trans_osrel(const Elf_Note *note, int32_t *osrel);
 static void	linux_vdso_install(void *param);
 static void	linux_vdso_deinstall(void *param);
+static void	linux32_set_syscall_retval(struct thread *td, int error);
 
 #define LINUX_T_UNKNOWN  255
 static int _bsd_to_linux_trapcode[] = {
@@ -669,6 +670,19 @@ linux32_fetch_syscall_args(struct thread *td)
 	return (0);
 }
 
+static void
+linux32_set_syscall_retval(struct thread *td, int error)
+{
+	struct trapframe *frame = td->td_frame;
+
+	cpu_set_syscall_retval(td, error);
+
+	if (__predict_false(error != 0)) {
+		if (error != ERESTART && error != EJUSTRETURN)
+			frame->tf_rax = SV_ABI_ERRNO(td->td_proc, error);
+	}
+}
+
 /*
  * Clear registers on exec
  * XXX copied from ia32_signal.c.
@@ -906,7 +920,7 @@ struct sysentvec elf_linux_sysvec = {
 	.sv_fixlimit	= linux32_fixlimit,
 	.sv_maxssiz	= &linux32_maxssiz,
 	.sv_flags	= SV_ABI_LINUX | SV_ILP32 | SV_IA32 | SV_SHP,
-	.sv_set_syscall_retval = cpu_set_syscall_retval,
+	.sv_set_syscall_retval = linux32_set_syscall_retval,
 	.sv_fetch_syscall_args = linux32_fetch_syscall_args,
 	.sv_syscallnames = NULL,
 	.sv_shared_page_base = LINUX32_SHAREDPAGE,
