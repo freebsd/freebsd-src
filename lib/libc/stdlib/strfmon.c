59b28017d24a48dfe852b45a1c60e7792dca2a9a
John Baldwin <jhb@FreeBSD.org>
2020-09-16 22:55:27 +0000

    MFC 365276: Compute the correct size of the string to move forward.
    Previously this was counting the amount of spare room at the start of
    the buffer that the string needed to move forward and passing that as
    the number of bytes to copy to memmove rather than the length of the
    string to be copied.
    
    In the strfmon test in the test suite this caused the memmove to
    overflow the allocated buffer by one byte which CHERI caught.

--- lib/libc/stdlib/strfmon.c
+++ lib/libc/stdlib/strfmon.c
@@ -636,7 +636,7 @@ __format_grouped_double(double value, int *flags,
 		memset(bufend, pad_char, padded);
 	}
 
-	bufsize = bufsize - (bufend - rslt) + 1;
+	bufsize = rslt + bufsize - bufend;
 	memmove(rslt, bufend, bufsize);
 	free(avalue);
 	return (rslt);
