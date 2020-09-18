37068152ae015e666a6172b0c4fc3f076b8303bb
np <np@FreeBSD.org>
2020-09-18 02:10:28 +0000

    Add two new ifnet capabilities for hw checksumming and TSO for VXLAN traffic.
    These are similar to the existing VLAN capabilities.
    
    Reviewed by:	kib@
    Sponsored by:	Chelsio Communications
    Differential Revision:	https://reviews.freebsd.org/D25873

--- sys/net/if.h
+++ sys/net/if.h
@@ -248,6 +248,8 @@ struct if_data {
 #define	IFCAP_NOMAP		0x4000000 /* can TX unmapped mbufs */
 #define	IFCAP_TXTLS4		0x8000000 /* can do TLS encryption and segmentation for TCP */
 #define	IFCAP_TXTLS6		0x10000000 /* can do TLS encryption and segmentation for TCP6 */
+#define	IFCAP_VXLAN_HWCSUM	0x20000000 /* can do IFCAN_HWCSUM on VXLANs */
+#define	IFCAP_VXLAN_HWTSO	0x40000000 /* can do IFCAP_TSO on VXLANs */
 
 #define IFCAP_HWCSUM_IPV6	(IFCAP_RXCSUM_IPV6 | IFCAP_TXCSUM_IPV6)
 
