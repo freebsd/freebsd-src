11e35681c70844133e7f47267fd1c9d637660cb3
bdragon <bdragon@FreeBSD.org>
2020-09-22 23:49:30 +0000

    [PowerPC64LE] Set up powerpc.powerpc64le architecture
    This is the initial set up for PowerPC64LE.
    
    The current plan is for this arch to remain experimental for FreeBSD 13.
    
    This started as a weekend learning project for me and kinda snowballed from
    there.
    
    (More to follow momentarily.)
    
    Reviewed by:	imp (earlier version), emaste
    Sponsored by:	Tag1 Consulting, Inc.
    Differential Revision:	https://reviews.freebsd.org/D26399

--- share/mk/local.meta.sys.mk
+++ share/mk/local.meta.sys.mk
@@ -17,7 +17,7 @@ MK_INSTALL_AS_USER= yes
 TARGET_ARCHES_arm?=     arm armv6 armv7
 TARGET_ARCHES_arm64?=   aarch64
 TARGET_ARCHES_mips?=    mipsel mips mips64el mips64 mipsn32 mipsn32el
-TARGET_ARCHES_powerpc?= powerpc powerpc64 powerpcspe
+TARGET_ARCHES_powerpc?= powerpc powerpc64 powerpc64le powerpcspe
 TARGET_ARCHES_riscv?=   riscv64 riscv64sf
 
 # some corner cases
