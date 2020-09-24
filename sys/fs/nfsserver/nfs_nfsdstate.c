d428f308f6d70060aa8f004781bc07c0b880cb9b
Rick Macklem <rmacklem@FreeBSD.org>
2020-09-24 15:34:47 +0000

    MFC: r365895 Fix a LOR between the NFS server and server side krpc.
    Recent testing of the NFS-over-TLS code found a LOR between the mutex lock
    used for sessions and the sleep lock used for server side krpc socket
    structures in nfsrv_checksequence().  This was fixed by r365789.
    A similar bug exists in nfsrv_bindconnsess(), where SVC_RELEASE() is called
    while mutexes are held.
    This patch applies a fix similar to r365789, moving the SVC_RELEASE() call
    down to after the mutexes are released.
    
    This patch fixes the problem by moving the SVC_RELEASE() call in
    nfsrv_bindconnsess() down a few lines to below where the mutexes are released.

--- sys/fs/nfsserver/nfs_nfsdstate.c
+++ sys/fs/nfsserver/nfs_nfsdstate.c
@@ -6405,6 +6405,7 @@ nfsrv_bindconnsess(struct nfsrv_descript *nd, uint8_t *sessionid, int *foreaftp)
 	int error;
 
 	error = 0;
+	savxprt = NULL;
 	shp = NFSSESSIONHASH(sessionid);
 	NFSLOCKSTATE();
 	NFSLOCKSESSION(shp);
@@ -6432,8 +6433,6 @@ nfsrv_bindconnsess(struct nfsrv_descript *nd, uint8_t *sessionid, int *foreaftp)
 				/* Disable idle timeout. */
 				nd->nd_xprt->xp_idletimeout = 0;
 				sep->sess_cbsess.nfsess_xprt = nd->nd_xprt;
-				if (savxprt != NULL)
-					SVC_RELEASE(savxprt);
 				sep->sess_crflags |= NFSV4CRSESS_CONNBACKCHAN;
 				clp->lc_flags |= LCL_DONEBINDCONN;
 				if (*foreaftp == NFSCDFS4_BACK)
@@ -6460,6 +6459,8 @@ nfsrv_bindconnsess(struct nfsrv_descript *nd, uint8_t *sessionid, int *foreaftp)
 		error = NFSERR_BADSESSION;
 	NFSUNLOCKSESSION(shp);
 	NFSUNLOCKSTATE();
+	if (savxprt != NULL)
+		SVC_RELEASE(savxprt);
 	return (error);
 }
 
