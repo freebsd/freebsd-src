c7e20b9b21cb37b376e49b86367aea6af75432ff
Richard Scheffenegger <rscheff@FreeBSD.org>
2020-09-25 10:57:11 +0000

    MFC r365547: Add -z "TOS" option to ping6, to test DSCP/ECN values
    ping has the option to add the (deprecated) TOS byte
    using the -z option. Adding the same option, with the
    same (deprecated) Traffic Class Byte (nowadays actually
    DSCP and ECN fields) to ping6 to validate proper QoS
    processing in network switches.
    
    Reviewed by:	tuexen
    MFC after:	2 weeks
    Sponsored by:	NetApp, Inc.
    Differential Revision:	https://reviews.freebsd.org/D26384

--- sbin/ping6/ping6.c
+++ sbin/ping6/ping6.c
@@ -229,6 +229,7 @@ static char *hostname;
 static int ident;		/* process id to identify our packets */
 static u_int8_t nonce[8];	/* nonce field for node information */
 static int hoplimit = -1;	/* hoplimit */
+static int tclass = -1;		/* traffic class */
 static u_char *packet = NULL;
 static cap_channel_t *capdns;
 
@@ -351,7 +352,7 @@ main(int argc, char *argv[])
 #endif /*IPSEC_POLICY_IPSEC*/
 #endif
 	while ((ch = getopt(argc, argv,
-	    "a:b:c:DdfHg:h:I:i:l:mnNop:qrRS:s:tvwWx:X:" ADDOPTS)) != -1) {
+	    "a:b:c:DdfHg:h:I:i:l:mnNop:qrRS:s:tvwWx:X:z:" ADDOPTS)) != -1) {
 #undef ADDOPTS
 		switch (ch) {
 		case 'a':
@@ -575,6 +576,14 @@ main(int argc, char *argv[])
 				errx(EX_USAGE, "invalid timeout: `%s' > %d",
 				    optarg, MAXALARM);
 			alarm((int)alarmtimeout);
+			break;
+		case 'z':		/* traffic class */
+			tclass = strtol(optarg, &e, 10);
+			if (*optarg == '\0' || *e != '\0')
+				errx(1, "illegal traffic class %s", optarg);
+			if (255 < tclass || tclass < -1)
+				errx(1,
+				    "illegal traffic class -- %s", optarg);
 			break;
 #ifdef IPSEC
 #ifdef IPSEC_POLICY_IPSEC
@@ -926,6 +935,12 @@ main(int argc, char *argv[])
 		memcpy(CMSG_DATA(scmsgp), &hoplimit, sizeof(hoplimit));
 
 		scmsgp = CMSG_NXTHDR(&smsghdr, scmsgp);
+	}
+
+	if (tclass != -1) {
+		if (setsockopt(ssend, IPPROTO_IPV6, IPV6_TCLASS,
+		    &tclass, sizeof(tclass)) == -1)
+			err(1, "setsockopt(IPV6_TCLASS)");
 	}
 
 	if (argc > 1) {	/* some intermediate addrs are specified */
