d150b458ace2f16d85a39b536abc83bf4e09dd37
imp <imp@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-10-08 20:56:11 +0000

    timer_restore is now unused, remove it
    apm was the only consumer of timer_restore. Now that it's gone, this
    can be removed.
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@366547 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/x86/isa/clock.c
+++ sys/x86/isa/clock.c
@@ -388,26 +388,6 @@ i8254_restore(void)
 		set_i8254_freq(MODE_STOP, 0);
 }
 
-#ifndef __amd64__
-/*
- * Restore all the timers non-atomically (XXX: should be atomically).
- *
- * This function is called from pmtimer_resume() to restore all the timers.
- * This should not be necessary, but there are broken laptops that do not
- * restore all the timers on resume. The APM spec was at best vague on the
- * subject.
- * pmtimer is used only with the old APM power management, and not with
- * acpi, which is required for amd64, so skip it in that case.
- */
-void
-timer_restore(void)
-{
-
-	i8254_restore();		/* restore i8254_freq and hz */
-	atrtc_restore();		/* reenable RTC interrupts */
-}
-#endif
-
 /* This is separate from startrtclock() so that it can be called early. */
 void
 i8254_init(void)
