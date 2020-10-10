889c86e71c99402ce00cd97942dd1b18f6c43f4a
mjg <mjg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-10-10 03:48:17 +0000

    vfs: support lockless dirfd lookups
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@366597 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/sys/file.h
+++ sys/sys/file.h
@@ -52,6 +52,7 @@ struct thread;
 struct uio;
 struct knote;
 struct vnode;
+struct nameidata;
 
 #endif /* _KERNEL */
 
@@ -279,6 +280,7 @@ int fgetvp_read(struct thread *td, int fd, cap_rights_t *rightsp,
     struct vnode **vpp);
 int fgetvp_write(struct thread *td, int fd, cap_rights_t *rightsp,
     struct vnode **vpp);
+int fgetvp_lookup_smr(int fd, struct nameidata *ndp, struct vnode **vpp, bool *fsearch);
 
 static __inline __result_use_check bool
 fhold(struct file *fp)
