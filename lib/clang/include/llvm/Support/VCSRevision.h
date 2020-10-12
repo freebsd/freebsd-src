1396c826a95fd2ad59b74de989cc33a19ad287a1
dim <dim@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-10-12 21:35:29 +0000

    Merge llvm, clang, compiler-rt, libc++, libunwind, lld, lldb and openmp release/11.x llvmorg-11.0.0-0-g176249bd673 (aka 11.0.0 release).
    MFC after:	4 weeks
    X-MFC-With:	r364284
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@366655 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/clang/include/llvm/Support/VCSRevision.h
+++ lib/clang/include/llvm/Support/VCSRevision.h
@@ -1,3 +1,3 @@
 /* $FreeBSD$ */
-#define LLVM_REVISION "llvmorg-11.0.0-rc5-0-g60a25202a7d"
+#define LLVM_REVISION "llvmorg-11.0.0-0-g176249bd673"
 #define LLVM_REPOSITORY "git@github.com:llvm/llvm-project.git"
