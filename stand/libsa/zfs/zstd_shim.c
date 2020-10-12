6fbadee898c077fa82e4488fbcc2b382580731f0
imp <imp@FreeBSD.org>
2020-10-12 22:19:07 +0000

    Add zstd support to the boot loader.
    Add support to the _STANDALONE environment enough bits of the kernel
    that we can compile it. We still have a small zstd_shim.c since there
    were 3 items that were a bit hard to nail down and may be cleaned up
    in the future. These go hand in hand with a number of commits to
    sys/sys in the past weeks, should this need be MFCd.
    
    Discussed with: mmacy (in review and on IRC/Slack)
    Reviewed by: freqlabs (on openzfs repo)
    Differential Revision: https://reviews.freebsd.org/D26218

--- stand/libsa/zfs/zstd_shim.c
+++ stand/libsa/zfs/zstd_shim.c
@@ -1,6 +1,5 @@
-/*
- * Copyright (c) 2020 iXsystems, Inc.
- * All rights reserved.
+/*-
+ * Copyright (c) 2020 M. Warner Losh <imp@FreeBSD.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -11,10 +10,10 @@
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -23,30 +22,19 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $FreeBSD$
+ */
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+/*
+ * Small amount of shim code needed to get zfs_zstd.c to compile. These items
+ * here should all be defined in the SPL or as part of libstand somewhere, but
+ * aren't for reasons that haven't been tracked down yet. Ideally, they would
+ * all go away and we'd compile zfs_zstd.c directly. Based on an original by
+ * Matt Macey, but only the #include remains untouched from that.
  */
 
+#define ZFS_MODULE_PARAM_ARGS void
+typedef int boolean_t;	/* This one may be tough to get rid of */
 
-#ifndef _SPL_KMEM_CACHE_H
-#define	_SPL_KMEM_CACHE_H
-
-#ifdef _KERNEL
-#include <sys/taskq.h>
-
-/* kmem move callback return values */
-typedef enum kmem_cbrc {
-	KMEM_CBRC_YES		= 0,	/* Object moved */
-	KMEM_CBRC_NO		= 1,	/* Object not moved */
-	KMEM_CBRC_LATER		= 2,	/* Object not moved, try again later */
-	KMEM_CBRC_DONT_NEED	= 3,	/* Neither object is needed */
-	KMEM_CBRC_DONT_KNOW	= 4,	/* Object unknown */
-} kmem_cbrc_t;
-
-extern void spl_kmem_cache_set_move(kmem_cache_t *,
-    kmem_cbrc_t (*)(void *, void *, size_t, void *));
-
-#define	kmem_cache_set_move(skc, move)	spl_kmem_cache_set_move(skc, move)
-
-#endif /* _KERNEL */
-
-#endif
+#include <contrib/openzfs/module/zstd/zfs_zstd.c>
