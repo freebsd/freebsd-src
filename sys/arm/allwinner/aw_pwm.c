430be63449e018ec46eeac871217f70cf8b022ec
Andriy Gapon <avg@FreeBSD.org>
2020-10-16 06:40:59 +0000

    MFC r366138: aw_pwm: remove the busy bit check
    The bit seems to always be set on my hardware, H3.
    However, programming the hardware seems to work just fine.

--- sys/arm/allwinner/aw_pwm.c
+++ sys/arm/allwinner/aw_pwm.c
@@ -300,10 +300,6 @@ aw_pwm_channel_config(device_t dev, u_int channel, u_int period, u_int duty)
 	}
 
 	reg = AW_PWM_READ(sc, AW_PWM_CTRL);
-	if (reg & AW_PWM_CTRL_PERIOD_BUSY) {
-		device_printf(sc->dev, "pwm busy\n");
-		return (EBUSY);
-	}
 
 	/* Write the prescalar */
 	reg &= ~AW_PWM_CTRL_PRESCALE_MASK;
