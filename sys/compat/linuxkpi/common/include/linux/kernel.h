144ce47d3d339902cdcf179b79d282ceb8c1c425
hselasky <hselasky@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-10-16 11:01:21 +0000

    Remove ifdefs around IS_ALIGNED() definition in the LinuxKPI.
    Discussed with:		manu@
    MFC after:		1 week
    Sponsored by:		Mellanox Technologies // NVIDIA Networking
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@366751 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/compat/linuxkpi/common/include/linux/kernel.h
+++ sys/compat/linuxkpi/common/include/linux/kernel.h
@@ -135,10 +135,7 @@ extern const volatile int lkpi_build_bug_on_zero;
 #define	ALIGN(x, y)		roundup2((x), (y))
 #undef PTR_ALIGN
 #define	PTR_ALIGN(p, a)		((__typeof(p))ALIGN((uintptr_t)(p), (a)))
-#if defined(LINUXKPI_VERSION) && LINUXKPI_VERSION >= 50000
-/* Moved from linuxkpi_gplv2 */
 #define	IS_ALIGNED(x, a)	(((x) & ((__typeof(x))(a) - 1)) == 0)
-#endif
 #define	DIV_ROUND_UP(x, n)	howmany(x, n)
 #define	__KERNEL_DIV_ROUND_UP(x, n)	howmany(x, n)
 #define	DIV_ROUND_UP_ULL(x, n)	DIV_ROUND_UP((unsigned long long)(x), (n))
