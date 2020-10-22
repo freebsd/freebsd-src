9d40cf60d6875924171eafe6364d4edc69f8d4c2
Hans Petter Selasky <hselasky@FreeBSD.org>
2020-10-22 09:09:53 +0000

    Factor out generic IP over infiniband, IPoIB, definitions and code into net/if_infiniband.c and net/infiniband.h . No functional change intended.
    Differential Revision:	https://reviews.freebsd.org/D26254
    Reviewed by:		melifaro@
    MFC after:		1 week
    Sponsored by:		Mellanox Technologies // NVIDIA Networking

--- sys/ofed/drivers/infiniband/ulp/ipoib/ipoib.h
+++ sys/ofed/drivers/infiniband/ulp/ipoib/ipoib.h
@@ -438,16 +438,7 @@ struct ipoib_path {
 
 extern struct workqueue_struct *ipoib_workqueue;
 
-#define IPOIB_MTAP_PROTO(_ifp, _m, _proto)			\
-do {								\
-	if (bpf_peers_present((_ifp)->if_bpf)) {		\
-		M_ASSERTVALID(_m);				\
-		ipoib_mtap_proto((_ifp), (_m), (_proto));	\
-	}							\
-} while (0)
-
 /* functions */
-void ipoib_mtap_proto(struct ifnet *ifp, struct mbuf *mb, uint16_t proto);
 void ipoib_ib_completion(struct ib_cq *cq, void *dev_ptr);
 void ipoib_send_comp_handler(struct ib_cq *cq, void *dev_ptr);
 
@@ -462,8 +453,6 @@ static inline void ipoib_put_ah(struct ipoib_ah *ah)
 int ipoib_open(struct ipoib_dev_priv *priv);
 int ipoib_add_pkey_attr(struct ipoib_dev_priv *priv);
 int ipoib_add_umcast_attr(struct ipoib_dev_priv *priv);
-
-void ipoib_demux(struct ifnet *ifp, struct mbuf *m, u_short proto);
 
 void ipoib_send(struct ipoib_dev_priv *priv, struct mbuf *mb,
 		struct ipoib_ah *address, u32 qpn);
@@ -540,7 +529,7 @@ int ipoib_poll_tx(struct ipoib_dev_priv *priv, bool do_start);
 
 void ipoib_dma_unmap_rx(struct ipoib_dev_priv *priv, struct ipoib_rx_buf *rx_req);
 void ipoib_dma_mb(struct ipoib_dev_priv *priv, struct mbuf *mb, unsigned int length);
-struct mbuf *ipoib_alloc_map_mb(struct ipoib_dev_priv *priv, struct ipoib_rx_buf *rx_req, int size);
+struct mbuf *ipoib_alloc_map_mb(struct ipoib_dev_priv *priv, struct ipoib_rx_buf *rx_req, int align, int size);
 
 
 void ipoib_set_ethtool_ops(struct ifnet *dev);
--- sys/ofed/drivers/infiniband/ulp/ipoib/ipoib_ib.c
+++ sys/ofed/drivers/infiniband/ulp/ipoib/ipoib_ib.c
@@ -112,17 +112,19 @@ ipoib_dma_mb(struct ipoib_dev_priv *priv, struct mbuf *mb, unsigned int length)
 
 struct mbuf *
 ipoib_alloc_map_mb(struct ipoib_dev_priv *priv, struct ipoib_rx_buf *rx_req,
-    int size)
+    int align, int size)
 {
 	struct mbuf *mb, *m;
 	int i, j;
 
 	rx_req->mb = NULL;
-	mb = m_getm2(NULL, size, M_NOWAIT, MT_DATA, M_PKTHDR);
+	mb = m_getm2(NULL, align + size, M_NOWAIT, MT_DATA, M_PKTHDR);
 	if (mb == NULL)
 		return (NULL);
 	for (i = 0, m = mb; m != NULL; m = m->m_next, i++) {
-		m->m_len = M_SIZE(m);
+		m->m_len = M_SIZE(m) - align;
+		m->m_data += align;
+		align = 0;
 		mb->m_pkthdr.len += m->m_len;
 		rx_req->mapping[i] = ib_dma_map_single(priv->ca,
 		    mtod(m, void *), m->m_len, DMA_FROM_DEVICE);
@@ -174,7 +176,7 @@ ipoib_alloc_rx_mb(struct ipoib_dev_priv *priv, int id)
 {
 
 	return ipoib_alloc_map_mb(priv, &priv->rx_ring[id],
-	    priv->max_ib_mtu + IB_GRH_BYTES);
+	    0, priv->max_ib_mtu + IB_GRH_BYTES);
 }
 
 static int ipoib_ib_post_receives(struct ipoib_dev_priv *priv)
