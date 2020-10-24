87c547f837734a2cc1a3456e417ad119a7fa0764
freqlabs <freqlabs@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-10-24 17:08:59 +0000

    flua: Add a libjail module
    libjail is pretty small, so it makes for a good proof of concept demonstrating
    how a system library can be wrapped to create a loadable Lua module for flua.
    
    * Introduce 3lua section for man pages
    * Add libjail module
    
    Reviewed by:	kevans, manpages
    Relnotes:	yes
    Differential Revision:	https://reviews.freebsd.org/D26080
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@367013 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/man/man.sh
+++ usr.bin/man/man.sh
@@ -1012,7 +1012,7 @@ STTY=/bin/stty
 SYSCTL=/sbin/sysctl
 
 debug=0
-man_default_sections='1:8:2:3:n:4:5:6:7:9:l'
+man_default_sections='1:8:2:3:3lua:n:4:5:6:7:9:l'
 man_default_path='/usr/share/man:/usr/share/openssl/man:/usr/local/share/man:/usr/local/man'
 cattool='/usr/bin/zcat -f'
 
