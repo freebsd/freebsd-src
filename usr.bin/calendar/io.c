5e562dae0b42c9ea7fba14cc953922dcc79aa7ed
se <se@FreeBSD.org>
2020-10-28 14:48:58 +0000

    Add support for nested conditionals
    The previous behavior was to support nested #ifdef and #ifndef, but to
    return to unconditional parsing after the next #endif, independently of
    the number of previously parsed conditions.
    
    E.g. after "#ifdef A / #ifdef B / #endif" the following lines were
    unconditially parsed again, independently of A and/or B being defined.
    
    The new behavior is to count the level of false conditions and to only
    restart parsing of calendar entries when the corresponding number of
    #endif tokens have been seen.
    
    In addition to the above, an #else directive has been added, to toggle
    between parsing and ignoring of the following lines.
    
    No validation of the correct use of the condition directives is made.
    #endif without prior #define or #ifndef is ignored and #else toggles
    between parsing and skipping of entries.
    
    The MFC period has been set to 1 month to allow for a review of the
    changes and for a discussion, whether these modifications should not
    be merged at all.
    
    No correct input file is parsed differently than before, but if calendar
    data files are published that use these new features, those data files
    will not parse correctly on prior versions of this program.
    
    MFC after:	1 month

--- usr.bin/calendar/io.c
+++ usr.bin/calendar/io.c
@@ -140,16 +140,64 @@ cal_fopen(const char *file)
 }
 
 static int
-token(char *line, FILE *out, bool *skip)
+token(char *line, FILE *out, int *skip)
 {
 	char *walk, c, a;
 
 	if (strncmp(line, "endif", 5) == 0) {
-		*skip = false;
+		if (*skip > 0)
+			--*skip;
 		return (T_OK);
 	}
 
-	if (*skip)
+	if (strncmp(line, "ifdef", 5) == 0) {
+		walk = line + 5;
+		trimlr(&walk);
+
+		if (*walk == '\0') {
+			warnx("Expecting arguments after #ifdef");
+			return (T_ERR);
+		}
+
+		if (*skip != 0 || definitions == NULL || sl_find(definitions, walk) == NULL)
+			++*skip;
+
+		return (T_OK);
+	}
+
+	if (strncmp(line, "ifndef", 6) == 0) {
+		walk = line + 6;
+		trimlr(&walk);
+
+		if (*walk == '\0') {
+			warnx("Expecting arguments after #ifndef");
+			return (T_ERR);
+		}
+
+		if (*skip != 0 || (definitions != NULL && sl_find(definitions, walk) != NULL))
+			++*skip;
+
+		return (T_OK);
+	}
+
+	if (strncmp(line, "else", 4) == 0) {
+		walk = line + 4;
+		trimlr(&walk);
+
+		if (*walk != '\0') {
+			warnx("Expecting no arguments after #else");
+			return (T_ERR);
+		}
+
+		if (*skip == 0)
+			*skip = 1;
+		else if (*skip == 1)
+			*skip = 0;
+
+		return (T_OK);
+	}
+
+	if (*skip != 0)
 		return (T_OK);
 
 	if (strncmp(line, "include", 7) == 0) {
@@ -212,36 +260,6 @@ token(char *line, FILE *out, bool *skip)
 		return (T_OK);
 	}
 
-	if (strncmp(line, "ifdef", 5) == 0) {
-		walk = line + 5;
-		trimlr(&walk);
-
-		if (*walk == '\0') {
-			warnx("Expecting arguments after #ifdef");
-			return (T_ERR);
-		}
-
-		if (definitions == NULL || sl_find(definitions, walk) == NULL)
-			*skip = true;
-
-		return (T_OK);
-	}
-
-	if (strncmp(line, "ifndef", 6) == 0) {
-		walk = line + 6;
-		trimlr(&walk);
-
-		if (*walk == '\0') {
-			warnx("Expecting arguments after #ifndef");
-			return (T_ERR);
-		}
-
-		if (definitions != NULL && sl_find(definitions, walk) != NULL)
-			*skip = true;
-
-		return (T_OK);
-	}
-
 	return (T_PROCESS);
 
 }
@@ -269,7 +287,7 @@ cal_parse(FILE *in, FILE *out)
 	int month[MAXCOUNT];
 	int day[MAXCOUNT];
 	int year[MAXCOUNT];
-	bool skip = false;
+	int skip = 0;
 	char dbuf[80];
 	char *pp, p;
 	struct tm tm;
@@ -299,7 +317,7 @@ cal_parse(FILE *in, FILE *out)
 			}
 		}
 
-		if (skip)
+		if (skip != 0)
 			continue;
 
 		buf = line;
