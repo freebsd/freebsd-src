ac04cf18bba3613e3a36722112fb646861c961e6
Stefan Eßer <se@FreeBSD.org>
2020-10-29 08:31:47 +0000

    Simplify test for closing delimiter of #include argument (no functional change).
    While here: Fix invalid parameters of a commented-out debug printf() found
    when testing with this code enabled.
    
    MFC after:	3 days

--- usr.bin/calendar/events.c
+++ usr.bin/calendar/events.c
@@ -207,8 +207,9 @@ event_print_all(FILE *fp)
 
 	while (walkthrough_dates(&e) != 0) {
 #ifdef DEBUG
-		fprintf(stderr, "event_print_allmonth: %d, day: %d\n",
-		    month, day);
+		if (e)
+			fprintf(stderr, "event_print_all month: %d, day: %d\n",
+			    e->month, e->day);
 #endif
 
 		/*
