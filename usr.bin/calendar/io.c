529e4d6011e250c4b395f72f16a32d0faa4f8cc4
se <se@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-10-29 08:31:47 +0000

    Simplify test for closing delimiter of #include argument (no functional change).
    While here: Fix invalid parameters of a commented-out debug printf() found
    when testing with this code enabled.
    
    MFC after:	3 days
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@367127 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- usr.bin/calendar/io.c
+++ usr.bin/calendar/io.c
@@ -215,26 +215,12 @@ token(char *line, FILE *out, int *skip)
 			return (T_ERR);
 		}
 
-		a = *walk;
+		a = *walk == '<' ? '>' : '\"';
 		walk++;
 		c = walk[strlen(walk) - 1];
 
-		switch(c) {
-		case '>':
-			if (a != '<') {
-				warnx("Unterminated include expecting '\"'");
-				return (T_ERR);
-			}
-			break;
-		case '\"':
-			if (a != '\"') {
-				warnx("Unterminated include expecting '>'");
-				return (T_ERR);
-			}
-			break;
-		default:
-			warnx("Unterminated include expecting '%c'",
-			    a == '<' ? '>' : '\"' );
+		if (a != c) {
+			warnx("Unterminated include expecting '%c'", a);
 			return (T_ERR);
 		}
 		walk[strlen(walk) - 1] = '\0';
