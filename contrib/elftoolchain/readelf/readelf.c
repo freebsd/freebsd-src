158e574629d038225a29350b92755af6c613dfa2
bdragon <bdragon@FreeBSD.org>
2020-10-31 22:14:37 +0000

    Fix 32-bit build after r367209
    Fix build on systems with a 32-bit size_t.
    
    Since it's being passed as a pointer, a 64-bit write to it will overflow.
    
    MFC with:	r367209

--- contrib/elftoolchain/readelf/readelf.c
+++ contrib/elftoolchain/readelf/readelf.c
@@ -6909,8 +6909,8 @@ get_symbol_value(struct readelf *re, int symtab, int i)
  * Returns true if sucessful, false otherwise.
  */
 static bool decompress_section(struct section *s,
-    unsigned char *compressed_data_buffer, uint64_t compressed_size,
-    unsigned char **ret_buf, uint64_t *ret_sz)
+    unsigned char *compressed_data_buffer, size_t compressed_size,
+    unsigned char **ret_buf, size_t *ret_sz)
 {
 	GElf_Shdr sh;
 
