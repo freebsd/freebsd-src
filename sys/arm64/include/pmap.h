9815c092decefb1e8dfba8b6ecf932298835a4a8
Andrew Turner <andrew@FreeBSD.org>
2020-11-04 11:48:08 +0000

    Add the pmap.h changes missed in r367320
    Reported by:	bz
    Sponsored by:	Innovate UK

--- sys/arm64/include/pmap.h
+++ sys/arm64/include/pmap.h
@@ -85,6 +85,7 @@ enum pmap_stage {
 struct pmap {
 	struct mtx		pm_mtx;
 	struct pmap_statistics	pm_stats;	/* pmap statistics */
+	uint64_t		pm_ttbr;
 	vm_paddr_t		pm_l0_paddr;
 	pd_entry_t		*pm_l0;
 	TAILQ_HEAD(,pv_chunk)	pm_pvchunk;	/* list of mappings in pmap */
@@ -92,6 +93,7 @@ struct pmap {
 	long			pm_cookie;	/* encodes the pmap's ASID */
 	struct asid_set		*pm_asid_set;	/* The ASID/VMID set to use */
 	enum pmap_stage		pm_stage;
+	int			pm_levels;
 };
 typedef struct pmap *pmap_t;
 
@@ -170,7 +172,7 @@ void	pmap_kremove(vm_offset_t);
 void	pmap_kremove_device(vm_offset_t, vm_size_t);
 void	*pmap_mapdev_attr(vm_offset_t pa, vm_size_t size, vm_memattr_t ma);
 bool	pmap_page_is_mapped(vm_page_t m);
-int	pmap_pinit_stage(pmap_t, enum pmap_stage);
+int	pmap_pinit_stage(pmap_t, enum pmap_stage, int);
 bool	pmap_ps_enabled(pmap_t pmap);
 uint64_t pmap_to_ttbr0(pmap_t pmap);
 
