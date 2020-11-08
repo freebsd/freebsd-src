2d39824195933c173bbfc9b31773070202d2e30e
Alexander V. Chernikov <melifaro@FreeBSD.org>
2020-11-08 18:27:49 +0000

    Switch net.add_addr_allfibs default to 0.
    The goal of the fib support is to provide multiple independent
     routing tables, isolated from each other.
    net.add_addr_allfibs default tries to shift gears in the opposite
     direction, unconditionally inserting all addresses to all of the fibs.
    
    There are use cases when this is necessary, however this is not a
     default expected behaviour, especially compared to other implementations.
    
    Provide WARNING message for the setups with multiple fibs to notify
     potential users of the feature.
    
    Differential Revision:	https://reviews.freebsd.org/D26076

--- sys/net/route/route_tables.c
+++ sys/net/route/route_tables.c
@@ -183,6 +183,11 @@ grow_rtables(uint32_t num_tables)
 	new_rt_tables = mallocarray(num_tables * (AF_MAX + 1), sizeof(void *),
 	    M_RTABLE, M_WAITOK | M_ZERO);
 
+	if ((num_tables > 1) && (V_rt_add_addr_allfibs == 0))
+		printf("WARNING: Adding ifaddrs to all fibs has been turned off "
+			"by default. Consider tuning %s if needed\n",
+			"net.add_addr_allfibs");
+
 	/*
 	 * Current rt_tables layout:
 	 * fib0[af0, af1, af2, .., AF_MAX]fib1[af0, af1, af2, .., Af_MAX]..
