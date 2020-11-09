dc0800a9ad8e63d15d6d6a67d5ae3c923bc0f56f
Navdeep Parhar <np@FreeBSD.org>
2020-11-09 00:01:13 +0000

    cxgbev(4): Use the MAC address set by the the PF if there is one.
    Query the firmware for the MAC address set by the PF for the VF and use
    it instead of the firmware generated MAC if it's available.
    
    MFC after:	2 weeks
    Sponsored by:	Chelsio Communications

--- sys/dev/cxgbe/common/common.h
+++ sys/dev/cxgbe/common/common.h
@@ -912,6 +912,8 @@ int t4vf_get_sge_params(struct adapter *adapter);
 int t4vf_get_rss_glb_config(struct adapter *adapter);
 int t4vf_get_vfres(struct adapter *adapter);
 int t4vf_prep_adapter(struct adapter *adapter);
+int t4vf_get_vf_mac(struct adapter *adapter, unsigned int port,
+		    unsigned int *naddr, u8 *addr);
 int t4_bar2_sge_qregs(struct adapter *adapter, unsigned int qid,
 		enum t4_bar2_qtype qtype, int user, u64 *pbar2_qoffset,
 		unsigned int *pbar2_qid);
