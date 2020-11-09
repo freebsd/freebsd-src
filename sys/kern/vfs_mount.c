b6a5273f8085830ec8b528109054aa733e36dedf
mjg <mjg@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-11-09 23:02:13 +0000

    vfs: group mount per-cpu vars into one struct
    While here move frequently read stuff into the same cacheline.
    
    This shrinks struct mount by 64 bytes.
    
    Tested by:	pho
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@367535 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/kern/vfs_mount.c
+++ sys/kern/vfs_mount.c
@@ -127,14 +127,7 @@ mount_init(void *mem, int size, int flags)
 	mtx_init(&mp->mnt_mtx, "struct mount mtx", NULL, MTX_DEF);
 	mtx_init(&mp->mnt_listmtx, "struct mount vlist mtx", NULL, MTX_DEF);
 	lockinit(&mp->mnt_explock, PVFS, "explock", 0, 0);
-	mp->mnt_thread_in_ops_pcpu = uma_zalloc_pcpu(pcpu_zone_4,
-	    M_WAITOK | M_ZERO);
-	mp->mnt_ref_pcpu = uma_zalloc_pcpu(pcpu_zone_4,
-	    M_WAITOK | M_ZERO);
-	mp->mnt_lockref_pcpu = uma_zalloc_pcpu(pcpu_zone_4,
-	    M_WAITOK | M_ZERO);
-	mp->mnt_writeopcount_pcpu = uma_zalloc_pcpu(pcpu_zone_4,
-	    M_WAITOK | M_ZERO);
+	mp->mnt_pcpu = uma_zalloc_pcpu(pcpu_zone_16, M_WAITOK | M_ZERO);
 	mp->mnt_ref = 0;
 	mp->mnt_vfs_ops = 1;
 	mp->mnt_rootvnode = NULL;
@@ -147,10 +140,7 @@ mount_fini(void *mem, int size)
 	struct mount *mp;
 
 	mp = (struct mount *)mem;
-	uma_zfree_pcpu(pcpu_zone_4, mp->mnt_writeopcount_pcpu);
-	uma_zfree_pcpu(pcpu_zone_4, mp->mnt_lockref_pcpu);
-	uma_zfree_pcpu(pcpu_zone_4, mp->mnt_ref_pcpu);
-	uma_zfree_pcpu(pcpu_zone_4, mp->mnt_thread_in_ops_pcpu);
+	uma_zfree_pcpu(pcpu_zone_16, mp->mnt_pcpu);
 	lockdestroy(&mp->mnt_explock);
 	mtx_destroy(&mp->mnt_listmtx);
 	mtx_destroy(&mp->mnt_mtx);
@@ -462,11 +452,12 @@ sys_nmount(struct thread *td, struct nmount_args *uap)
 void
 vfs_ref(struct mount *mp)
 {
+	struct mount_pcpu *mpcpu;
 
 	CTR2(KTR_VFS, "%s: mp %p", __func__, mp);
-	if (vfs_op_thread_enter(mp)) {
-		vfs_mp_count_add_pcpu(mp, ref, 1);
-		vfs_op_thread_exit(mp);
+	if (vfs_op_thread_enter(mp, mpcpu)) {
+		vfs_mp_count_add_pcpu(mpcpu, ref, 1);
+		vfs_op_thread_exit(mp, mpcpu);
 		return;
 	}
 
@@ -478,11 +469,12 @@ vfs_ref(struct mount *mp)
 void
 vfs_rel(struct mount *mp)
 {
+	struct mount_pcpu *mpcpu;
 
 	CTR2(KTR_VFS, "%s: mp %p", __func__, mp);
-	if (vfs_op_thread_enter(mp)) {
-		vfs_mp_count_sub_pcpu(mp, ref, 1);
-		vfs_op_thread_exit(mp);
+	if (vfs_op_thread_enter(mp, mpcpu)) {
+		vfs_mp_count_sub_pcpu(mpcpu, ref, 1);
+		vfs_op_thread_exit(mp, mpcpu);
 		return;
 	}
 
@@ -503,6 +495,12 @@ vfs_mount_alloc(struct vnode *vp, struct vfsconf *vfsp, const char *fspath,
 	mp = uma_zalloc(mount_zone, M_WAITOK);
 	bzero(&mp->mnt_startzero,
 	    __rangeof(struct mount, mnt_startzero, mnt_endzero));
+	mp->mnt_kern_flag = 0;
+	mp->mnt_flag = 0;
+	mp->mnt_rootvnode = NULL;
+	mp->mnt_vnodecovered = NULL;
+	mp->mnt_op = NULL;
+	mp->mnt_vfc = NULL;
 	TAILQ_INIT(&mp->mnt_nvnodelist);
 	mp->mnt_nvnodelistsize = 0;
 	TAILQ_INIT(&mp->mnt_lazyvnodelist);
@@ -1513,6 +1511,7 @@ dounmount_cleanup(struct mount *mp, struct vnode *coveredvp, int mntkflags)
 void
 vfs_op_enter(struct mount *mp)
 {
+	struct mount_pcpu *mpcpu;
 	int cpu;
 
 	MNT_ILOCK(mp);
@@ -1523,12 +1522,16 @@ vfs_op_enter(struct mount *mp)
 	}
 	vfs_op_barrier_wait(mp);
 	CPU_FOREACH(cpu) {
-		mp->mnt_ref +=
-		    zpcpu_replace_cpu(mp->mnt_ref_pcpu, 0, cpu);
-		mp->mnt_lockref +=
-		    zpcpu_replace_cpu(mp->mnt_lockref_pcpu, 0, cpu);
-		mp->mnt_writeopcount +=
-		    zpcpu_replace_cpu(mp->mnt_writeopcount_pcpu, 0, cpu);
+		mpcpu = vfs_mount_pcpu_remote(mp, cpu);
+
+		mp->mnt_ref += mpcpu->mntp_ref;
+		mpcpu->mntp_ref = 0;
+
+		mp->mnt_lockref += mpcpu->mntp_lockref;
+		mpcpu->mntp_lockref = 0;
+
+		mp->mnt_writeopcount += mpcpu->mntp_writeopcount;
+		mpcpu->mntp_writeopcount = 0;
 	}
 	if (mp->mnt_ref <= 0 || mp->mnt_lockref < 0 || mp->mnt_writeopcount < 0)
 		panic("%s: invalid count(s) on mp %p: ref %d lockref %d writeopcount %d\n",
@@ -1581,13 +1584,13 @@ vfs_op_wait_func(void *arg, int cpu)
 {
 	struct vfs_op_barrier_ipi *vfsopipi;
 	struct mount *mp;
-	int *in_op;
+	struct mount_pcpu *mpcpu;
 
 	vfsopipi = __containerof(arg, struct vfs_op_barrier_ipi, srcra);
 	mp = vfsopipi->mp;
 
-	in_op = zpcpu_get_cpu(mp->mnt_thread_in_ops_pcpu, cpu);
-	while (atomic_load_int(in_op))
+	mpcpu = vfs_mount_pcpu_remote(mp, cpu);
+	while (atomic_load_int(&mpcpu->mntp_thread_in_ops))
 		cpu_spinwait();
 }
 
@@ -1610,15 +1613,17 @@ vfs_op_barrier_wait(struct mount *mp)
 void
 vfs_assert_mount_counters(struct mount *mp)
 {
+	struct mount_pcpu *mpcpu;
 	int cpu;
 
 	if (mp->mnt_vfs_ops == 0)
 		return;
 
 	CPU_FOREACH(cpu) {
-		if (*zpcpu_get_cpu(mp->mnt_ref_pcpu, cpu) != 0 ||
-		    *zpcpu_get_cpu(mp->mnt_lockref_pcpu, cpu) != 0 ||
-		    *zpcpu_get_cpu(mp->mnt_writeopcount_pcpu, cpu) != 0)
+		mpcpu = vfs_mount_pcpu_remote(mp, cpu);
+		if (mpcpu->mntp_ref != 0 ||
+		    mpcpu->mntp_lockref != 0 ||
+		    mpcpu->mntp_writeopcount != 0)
 			vfs_dump_mount_counters(mp);
 	}
 }
@@ -1626,33 +1631,34 @@ vfs_assert_mount_counters(struct mount *mp)
 void
 vfs_dump_mount_counters(struct mount *mp)
 {
-	int cpu, *count;
+	struct mount_pcpu *mpcpu;
 	int ref, lockref, writeopcount;
+	int cpu;
 
 	printf("%s: mp %p vfs_ops %d\n", __func__, mp, mp->mnt_vfs_ops);
 
 	printf("        ref : ");
 	ref = mp->mnt_ref;
 	CPU_FOREACH(cpu) {
-		count = zpcpu_get_cpu(mp->mnt_ref_pcpu, cpu);
-		printf("%d ", *count);
-		ref += *count;
+		mpcpu = vfs_mount_pcpu_remote(mp, cpu);
+		printf("%d ", mpcpu->mntp_ref);
+		ref += mpcpu->mntp_ref;
 	}
 	printf("\n");
 	printf("    lockref : ");
 	lockref = mp->mnt_lockref;
 	CPU_FOREACH(cpu) {
-		count = zpcpu_get_cpu(mp->mnt_lockref_pcpu, cpu);
-		printf("%d ", *count);
-		lockref += *count;
+		mpcpu = vfs_mount_pcpu_remote(mp, cpu);
+		printf("%d ", mpcpu->mntp_lockref);
+		lockref += mpcpu->mntp_lockref;
 	}
 	printf("\n");
 	printf("writeopcount: ");
 	writeopcount = mp->mnt_writeopcount;
 	CPU_FOREACH(cpu) {
-		count = zpcpu_get_cpu(mp->mnt_writeopcount_pcpu, cpu);
-		printf("%d ", *count);
-		writeopcount += *count;
+		mpcpu = vfs_mount_pcpu_remote(mp, cpu);
+		printf("%d ", mpcpu->mntp_writeopcount);
+		writeopcount += mpcpu->mntp_writeopcount;
 	}
 	printf("\n");
 
@@ -1668,27 +1674,34 @@ vfs_dump_mount_counters(struct mount *mp)
 int
 vfs_mount_fetch_counter(struct mount *mp, enum mount_counter which)
 {
-	int *base, *pcpu;
+	struct mount_pcpu *mpcpu;
 	int cpu, sum;
 
 	switch (which) {
 	case MNT_COUNT_REF:
-		base = &mp->mnt_ref;
-		pcpu = mp->mnt_ref_pcpu;
+		sum = mp->mnt_ref;
 		break;
 	case MNT_COUNT_LOCKREF:
-		base = &mp->mnt_lockref;
-		pcpu = mp->mnt_lockref_pcpu;
+		sum = mp->mnt_lockref;
 		break;
 	case MNT_COUNT_WRITEOPCOUNT:
-		base = &mp->mnt_writeopcount;
-		pcpu = mp->mnt_writeopcount_pcpu;
+		sum = mp->mnt_writeopcount;
 		break;
 	}
 
-	sum = *base;
 	CPU_FOREACH(cpu) {
-		sum += *zpcpu_get_cpu(pcpu, cpu);
+		mpcpu = vfs_mount_pcpu_remote(mp, cpu);
+		switch (which) {
+		case MNT_COUNT_REF:
+			sum += mpcpu->mntp_ref;
+			break;
+		case MNT_COUNT_LOCKREF:
+			sum += mpcpu->mntp_lockref;
+			break;
+		case MNT_COUNT_WRITEOPCOUNT:
+			sum += mpcpu->mntp_writeopcount;
+			break;
+		}
 	}
 	return (sum);
 }
