e268fd0a029e620a0cc85c9437ae5dc35a00c699
Brooks Davis <brooks@FreeBSD.org>
2020-11-10 19:15:13 +0000

    Support initializing stack variables on function entry
    There are two options:
     - WITH_INIT_ALL_ZERO: Zero all variables on the stack.
     - WITH_INIT_ALL_PATTERN: Initialize variables with well-defined patterns.
    
    The exact pattern are a compiler implementation detail and vary by type.
    They are somewhat documented in the LLVM commit message:
    https://reviews.llvm.org/rL349442
    I've used WITH_INIT_ALL_* to match Microsoft's InitAll feature rather
    than naming them after the LLVM specific compiler flags.
    
    In a range of consumer products, options like these are used in
    both debug and production builds with debugs builds using patterns
    (intended to provoke crashes on use of uninitialized values) and
    production using zeros (deemed more likely to lead to harmless
    misbehavior or NULL-pointer dereferences).
    
    Reviewed by:	emaste
    Obtained from:	CheriBSD
    Sponsored by:	DARPA
    Differential Revision:	https://reviews.freebsd.org/D27131

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -60,6 +60,25 @@ LDFLAGS+= -Wl,-zretpolineplt
 .endif
 .endif
 
+# Initialize stack variables on function entry
+.if ${MK_INIT_ALL_ZERO} == "yes"
+.if ${COMPILER_FEATURES:Minit-all}
+CFLAGS+= -ftrivial-auto-var-init=zero \
+    -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang
+CXXFLAGS+= -ftrivial-auto-var-init=zero \
+    -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang
+.else
+.warning InitAll (zeros) requested but not support by compiler
+.endif
+.elif ${MK_INIT_ALL_PATTERN} == "yes"
+.if ${COMPILER_FEATURES:Minit-all}
+CFLAGS+= -ftrivial-auto-var-init=pattern
+CXXFLAGS+= -ftrivial-auto-var-init=pattern
+.else
+.warning InitAll (pattern) requested but not support by compiler
+.endif
+.endif
+
 .if ${MACHINE_CPUARCH} == "riscv" && ${LINKER_FEATURES:Mriscv-relaxations} == ""
 CFLAGS += -mno-relax
 .endif
