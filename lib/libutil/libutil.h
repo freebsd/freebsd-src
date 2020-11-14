0cc79f9e514f70afd9e1b89ea0643d0ec1d5f442
scottl <scottl@FreeBSD.org>
2020-11-14 19:04:36 +0000

    Fix a problem with r367686 related to the use of ssize_t.  Not sure how this escaped prior testing, but it should be better now.
    Reported by:	lots

--- lib/libutil/libutil.h
+++ lib/libutil/libutil.h
@@ -65,6 +65,11 @@ typedef	__size_t	size_t;
 #define	_SIZE_T_DECLARED
 #endif
 
+#ifndef _SSIZE_T_DECLARED
+typedef __ssize_t	ssize_t;
+#define _SSIZE_T_DECLARED
+#endif
+
 #ifndef _UID_T_DECLARED
 typedef	__uid_t		uid_t;
 #define	_UID_T_DECLARED
