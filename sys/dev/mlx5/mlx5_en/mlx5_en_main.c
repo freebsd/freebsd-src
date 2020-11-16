27d2100e78d8d22a4f3e63f25834ec0c80c64e03
hselasky <hselasky@FreeBSD.org>
2020-11-16 10:06:10 +0000

    Enhance the mlx5_core_create_cq() function in mlx5core.
    Enhance mlx5_core_create_cq() to get the command out buffer from the
    callers to let them use the output.
    
    Linux commit:
    38164b771947be9baf06e78ffdfb650f8f3e908e
    
    MFC after:	1 week
    Sponsored by:	Mellanox Technologies // NVIDIA Networking

--- sys/dev/mlx5/mlx5_en/mlx5_en_main.c
+++ sys/dev/mlx5/mlx5_en/mlx5_en_main.c
@@ -1989,6 +1989,7 @@ static int
 mlx5e_enable_cq(struct mlx5e_cq *cq, struct mlx5e_cq_param *param, int eq_ix)
 {
 	struct mlx5_core_cq *mcq = &cq->mcq;
+	u32 out[MLX5_ST_SZ_DW(create_cq_out)];
 	void *in;
 	void *cqc;
 	int inlen;
@@ -2017,7 +2018,7 @@ mlx5e_enable_cq(struct mlx5e_cq *cq, struct mlx5e_cq_param *param, int eq_ix)
 	    PAGE_SHIFT);
 	MLX5_SET64(cqc, cqc, dbr_addr, cq->wq_ctrl.db.dma);
 
-	err = mlx5_core_create_cq(cq->priv->mdev, mcq, in, inlen);
+	err = mlx5_core_create_cq(cq->priv->mdev, mcq, in, inlen, out, sizeof(out));
 
 	kvfree(in);
 
