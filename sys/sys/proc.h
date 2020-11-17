7d4dfe7790019ed01ad02d6a3c44b54997bedc05
Kyle Evans <kevans@FreeBSD.org>
2020-11-17 04:06:35 +0000

    sys/proc.h: improve comment for new TDP2 flag
    This was suggested by kib and integrated locally, but somehow did not make
    it into the committed version.

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -519,7 +519,7 @@ do {									\
 #define	TDP_SIGFASTPENDING 0x80000000 /* Pending signal due to sigfastblock */
 
 #define	TDP2_SBPAGES	0x00000001 /* Owns sbusy on some pages */
-#define	TDP2_COMPAT32RB	0x00000002 /* compat32 robust lists */
+#define	TDP2_COMPAT32RB	0x00000002 /* compat32 ABI for robust lists */
 
 /*
  * Reasons that the current thread can not be run yet.
