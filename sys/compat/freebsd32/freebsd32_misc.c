63ecb272a00c6f084a33db1c6ad3e925d4dcf015
Kyle Evans <kevans@FreeBSD.org>
2020-11-17 03:36:58 +0000

    umtx_op: reduce redundancy required for compat32
    All of the compat32 variants are substantially the same, save for
    copyin/copyout (mostly). Apply the same kind of technique used with kevent
    here by having the syscall routines supply a umtx_copyops describing the
    operations needed.
    
    umtx_copyops carries the bare minimum needed- size of timespec and
    _umtx_time are used for determining if copyout is needed in the sem2_wait
    case.
    
    Reviewed by:	kib
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D27222

--- sys/compat/freebsd32/freebsd32_misc.c
+++ sys/compat/freebsd32/freebsd32_misc.c
@@ -84,6 +84,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/thr.h>
 #include <sys/unistd.h>
 #include <sys/ucontext.h>
+#include <sys/umtx.h>
 #include <sys/vnode.h>
 #include <sys/wait.h>
 #include <sys/ipc.h>
@@ -3764,4 +3765,12 @@ freebsd32_sched_rr_get_interval(struct thread *td,
 		error = copyout(&ts32, uap->interval, sizeof(ts32));
 	}
 	return (error);
+}
+
+int
+freebsd32__umtx_op(struct thread *td, struct freebsd32__umtx_op_args *uap)
+{
+
+	return (kern__umtx_op(td, uap->obj, uap->op, uap->val, uap->uaddr,
+	    uap->uaddr2, &umtx_native_ops32));
 }
