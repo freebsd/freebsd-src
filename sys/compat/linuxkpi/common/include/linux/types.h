99f20bdc47f62794eec6f46a7d8c4f434a31870a
Hans Petter Selasky <hselasky@FreeBSD.org>
2020-11-18 13:47:11 +0000

    Allow LinuxKPI types to be used in bootloaders, by checking for the _STANDALONE definition.
    No functional change intended.
    
    MFC after:	1 week
    Sponsored by:	Mellanox Technologies // NVIDIA Networking

--- sys/compat/linuxkpi/common/include/linux/types.h
+++ sys/compat/linuxkpi/common/include/linux/types.h
@@ -57,8 +57,10 @@ typedef uint16_t __aligned_u16 __aligned(sizeof(uint16_t));
 typedef uint32_t __aligned_u32 __aligned(sizeof(uint32_t));
 typedef uint64_t __aligned_u64 __aligned(sizeof(uint64_t));
 
+#ifdef _KERNEL
 typedef unsigned short ushort;
 typedef unsigned int    uint;
+#endif
 typedef unsigned long ulong;
 typedef unsigned gfp_t;
 typedef off_t loff_t;
@@ -67,7 +69,7 @@ typedef uint16_t __bitwise__ __sum16;
 typedef unsigned long pgoff_t;
 typedef unsigned __poll_t;
 
-typedef u64 phys_addr_t;
+typedef uint64_t phys_addr_t;
 
 typedef size_t __kernel_size_t;
 
