cf058833feeb9a16187b39ee652bdae81824899d
Alan Somers <asomers@FreeBSD.org>
2020-11-26 23:34:02 +0000

    MFC r365910:
    fix integer underflow in getgrnam_r and getpwnam_r
    
    Sometimes nscd(8) will return a 1-byte buffer for a nonexistent entry. This
    triggered an integer underflow in grp_unmarshal_func, causing getgrnam_r to
    return ERANGE instead of 0.
    
    Fix the user's buffer size check, and add a correct check for a too-small
    nscd buffer.
    
    PR:		248932
    Event:		September 2020 Bugathon
    Reviewed by:	markj
    Sponsored by:	Axcient
    Differential Revision: https://reviews.freebsd.org/D26204

--- lib/libc/gen/getpwent.c
+++ lib/libc/gen/getpwent.c
@@ -389,10 +389,17 @@ pwd_unmarshal_func(char *buffer, size_t buffer_size, void *retval, va_list ap,
 	orig_buf_size = va_arg(ap, size_t);
 	ret_errno = va_arg(ap, int *);
 
-	if (orig_buf_size <
-	    buffer_size - sizeof(struct passwd) - sizeof(char *)) {
+	if (orig_buf_size + sizeof(struct passwd) + sizeof(char *) <
+	    buffer_size) {
 		*ret_errno = ERANGE;
 		return (NS_RETURN);
+	} else if (buffer_size < sizeof(struct passwd) + sizeof(char *)) {
+		/*
+		 * nscd(8) sometimes returns buffer_size=1 for nonexistent
+		 * entries.
+		 */
+		*ret_errno = 0;
+		return (NS_NOTFOUND);
 	}
 
 	memcpy(pwd, buffer, sizeof(struct passwd));
