fb16e83e5006413eacc3a4336a3b6ef1726f8317
wulf <wulf@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-12-05 10:57:15 +0000

    ig4(4): Add PCI IDs for Intel Tiger Lake
    Submitted by:	Neel Chauhan <neel AT neelc DOT org>
    Differential Revision:	https://reviews.freebsd.org/D27483
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@368366 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- sys/dev/ichiic/ig4_var.h
+++ sys/dev/ichiic/ig4_var.h
@@ -43,10 +43,17 @@
 #include "pci_if.h"
 #include "iicbus_if.h"
 
-enum ig4_vers { IG4_HASWELL, IG4_ATOM, IG4_SKYLAKE, IG4_APL, IG4_CANNONLAKE };
+enum ig4_vers {
+	IG4_HASWELL,
+	IG4_ATOM,
+	IG4_SKYLAKE,
+	IG4_APL,
+	IG4_CANNONLAKE,
+	IG4_TIGERLAKE
+};
+
 /* Controller has additional registers */
-#define	IG4_HAS_ADDREGS(vers)	((vers) == IG4_SKYLAKE || \
-	(vers) == IG4_APL || (vers) == IG4_CANNONLAKE)
+#define	IG4_HAS_ADDREGS(vers)	((vers) >= IG4_SKYLAKE)
 
 struct ig4_hw {
 	uint32_t	ic_clock_rate;	/* MHz */
