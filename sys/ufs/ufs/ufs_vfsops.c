52c914111730d0ba49b45fee1191c78369ebc4cf
kib <kib@FreeBSD.org>
2020-12-06 18:09:14 +0000

    ufs: handle two more cases of possible VNON vnode returned from VFS_VGET().
    Reported by:	kevans
    Reviewed by:	mckusick, mjg
    Tested by:	pho
    Sponsored by:	The FreeBSD Foundation
    Differential revision:	https://reviews.freebsd.org/D27457

--- sys/ufs/ufs/ufs_vfsops.c
+++ sys/ufs/ufs/ufs_vfsops.c
@@ -240,6 +240,8 @@ ufs_fhtovp(mp, ufhp, flags, vpp)
 	ip = VTOI(nvp);
 	if (ip->i_mode == 0 || ip->i_gen != ufhp->ufid_gen ||
 	    ip->i_effnlink <= 0) {
+		if (ip->i_mode == 0)
+			vgone(nvp);
 		vput(nvp);
 		*vpp = NULLVP;
 		return (ESTALE);
