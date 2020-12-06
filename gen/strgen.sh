253e722024bb7ee47360bbb12c6893e7c8d7b780
Stefan Eßer <se@FreeBSD.org>
2020-12-06 11:38:46 +0000

    Import version 3.2.3

--- gen/strgen.sh
+++ gen/strgen.sh
@@ -32,18 +32,17 @@ export LC_CTYPE=C
 
 progname=${0##*/}
 
-if [ $# -lt 4 ]; then
-	echo "usage: $progname input output name header [label [define [remove_tabs]]]"
+if [ $# -lt 3 ]; then
+	echo "usage: $progname input output name [label [define [remove_tabs]]]"
 	exit 1
 fi
 
 input="$1"
 output="$2"
 name="$3"
-header="$4"
-label="$5"
-define="$6"
-remove_tabs="$7"
+label="$4"
+define="$5"
+remove_tabs="$6"
 
 exec < "$input"
 exec > "$output"
