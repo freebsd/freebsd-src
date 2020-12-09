6a5ea8aeb863b3ab2bb0c3cdc75cda825aaaf9bc
kevans <kevans@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-12-09 03:22:44 +0000

    libsysdecode: decode _UMTX_OP flags
    Assume that UMTX_OP with a double underbar following is a flag, while any
    underbar+alphanumeric combination immeiately following is an op.
    
    This was a part of D27325.
    
    Reviewed by:	kib
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@368480 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- lib/libsysdecode/sysdecode.h
+++ lib/libsysdecode/sysdecode.h
@@ -121,6 +121,7 @@ const char *sysdecode_sysarch_number(int _number);
 bool	sysdecode_thr_create_flags(FILE *_fp, int _flags, int *_rem);
 bool	sysdecode_umtx_cvwait_flags(FILE *_fp, u_long _flags, u_long *_rem);
 const char *sysdecode_umtx_op(int _op);
+bool	sysdecode_umtx_op_flags(FILE *_fp, int op, int *_rem);
 bool	sysdecode_umtx_rwlock_flags(FILE *_fp, u_long _flags, u_long *_rem);
 int	sysdecode_utrace(FILE *_fp, void *_buf, size_t _len);
 bool	sysdecode_vmprot(FILE *_fp, int _type, int *_rem);
