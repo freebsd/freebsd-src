df44a43e47c011bceb8939cba3c4b962001a472d
kevans <kevans@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2020-12-12 21:25:38 +0000

    stand: liblua: add a pager module
    This is nearly a 1:1 mapping of the pager API from libsa.  The only real
    difference is that pager.output() will accept any number of arguments and
    coerce all of them to strings for output using luaL_tolstring (i.e. the
    __tostring metamethod will be used).
    
    The only consumer planned at this time is the upcoming "show-module-options"
    implementation.
    
    MFC after:	1 week
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/base/head@368591 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- stand/liblua/lpager.c
+++ stand/liblua/lpager.c
@@ -1,6 +1,5 @@
 /*-
- * Copyright (c) 2014 Pedro Souza <pedrosouza@freebsd.org>
- * All rights reserved.
+ * Copyright (c) 2020 Kyle Evans <kevans@FreeBSD.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -22,12 +21,69 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $FreeBSD$
  */
 
-#include <lua.h>
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
-int	luaopen_loader(lua_State *);
-int	luaopen_io(lua_State *);
-int	luaopen_pager(lua_State *);
+#include <lua.h>
+#include "lauxlib.h"
+
+/* Open the pager.  No arguments, no return value. */
+static int
+lpager_open(lua_State *L)
+{
+
+	pager_open();
+	return (0);
+}
+
+/*
+ * Output to the pager.  All arguments are interpreted as strings and passed to
+ * pager_output().  No return value.
+ */
+static int
+lpager_output(lua_State *L)
+{
+	const char *outstr;
+	int i;
+
+	for (i = 1; i <= lua_gettop(L); i++) {
+		outstr = luaL_tolstring(L,  i, NULL);
+		pager_output(outstr);
+		lua_pop(L, -1);
+	}
+
+	return (0);
+}
+
+/* Output to the pager from a file.  Takes a filename, no return value. */
+static int
+lpager_file(lua_State *L)
+{
+
+	return (pager_file(luaL_checkstring(L, 1)));
+}
+
+static int
+lpager_close(lua_State *L)
+{
+
+	pager_close();
+	return (0);
+}
+
+static const struct luaL_Reg pagerlib[] = {
+	{ "open", lpager_open },
+	{ "output", lpager_output },
+	{ "file", lpager_file },
+	{ "close", lpager_close },
+	{ NULL, NULL },
+};
+
+int
+luaopen_pager(lua_State *L)
+{
+	luaL_newlib(L, pagerlib);
+	return 1;
+}
