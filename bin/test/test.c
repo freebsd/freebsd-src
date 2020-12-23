516b1d9b2b789c29965365473e32ba2c02a7cdb4
Xin LI <delphij@FreeBSD.org>
2020-12-22 21:04:36 -0800

    Remove unused includes.

--- bin/test/test.c
+++ bin/test/test.c
@@ -24,9 +24,7 @@ __FBSDID("$FreeBSD$");
 #include <err.h>
 #include <errno.h>
 #include <inttypes.h>
-#include <limits.h>
 #include <stdarg.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
