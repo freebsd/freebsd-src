5bcd0b860c7d4935db5b290cf932357fc22adf3a
Toomas Soome <tsoome@FreeBSD.org>
2021-01-02 21:03:28 +0200

    contrib: setup terminus

--- contrib/terminus/win32/make-clean.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-if not exist ter-font.rc exit /b 1
-if not exist ter-main.c exit /b 1
-echo rm -f *.bdf *.fnt *.txt ter-*.o terminus.fon fcpw.exe
-for %%i in (*.bdf *.fnt *.txt ter-*.o terminus.fon fcpw.exe) do if exist %%i del /q %%i
--- contrib/terminus/win32/ter-main.c
+++
@@ -1,9 +0,0 @@
-#include <windows.h>
-
-BOOL WINAPI DllMainCRTStartup(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-{
-	(void) hinstDLL;
-	(void) fdwReason;
-	(void) lpvReserved;
-	return TRUE;
-} 
