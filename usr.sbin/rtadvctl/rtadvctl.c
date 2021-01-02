bfe90bb336ccccded5ce51d72ed01ebfbb447337
Xin LI <delphij@FreeBSD.org>
2021-01-01 16:04:55 -0800

    usr.sbin/rtadvctl: Remove support for FreeBSD 9.x.

--- usr.sbin/rtadvctl/rtadvctl.c
+++ usr.sbin/rtadvctl/rtadvctl.c
@@ -514,19 +514,6 @@ action_show(int argc, char **argv)
 		if ((ifi_s->ifi_flags & IFF_UP) &&
 		    ((ifi_s->ifi_state == IFI_STATE_CONFIGURED) ||
 			(ifi_s->ifi_state == IFI_STATE_TRANSITIVE))) {
-#if (__FreeBSD_version < 900000)
-			/*
-			 * RA_RECV: !ip6.forwarding && ip6.accept_rtadv
-			 * RA_SEND: ip6.forwarding
-			 */
-			if (getinet6sysctl(IPV6CTL_FORWARDING) == 0) {
-				if (getinet6sysctl(IPV6CTL_ACCEPT_RTADV))
-					ra_ifstatus = RA_IFSTATUS_RA_RECV;
-				else
-					ra_ifstatus = RA_IFSTATUS_INACTIVE;
-			} else
-				ra_ifstatus = RA_IFSTATUS_RA_SEND;
-#else
 			/*
 			 * RA_RECV: ND6_IFF_ACCEPT_RTADV
 			 * RA_SEND: ip6.forwarding
@@ -537,7 +524,6 @@ action_show(int argc, char **argv)
 				ra_ifstatus = RA_IFSTATUS_RA_SEND;
 			else
 				ra_ifstatus = RA_IFSTATUS_INACTIVE;
-#endif
 		}
 
 		c = 0;
