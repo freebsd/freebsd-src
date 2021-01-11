ba0c4cd107c57cd73c0849e32963147f93ae8ffc
Alexander V. Chernikov <melifaro@FreeBSD.org>
2021-01-11 23:11:55 +0000

    Use rn_match instead of doing indirect calls in fib_algo.
    Relevant inet/inet6 code has the control over deciding what
     the RIB lookup function currently is. With that in mind,
     explicitly set it to the current value (rn_match) in the
     datapath lookups. This avoids cost on indirect call.
    
    Differential Revision: https://reviews.freebsd.org/D28066

--- sys/netinet6/in6_fib_algo.c
+++ sys/netinet6/in6_fib_algo.c
@@ -106,7 +106,7 @@ lradix6_lookup(void *algo_data, const struct flm_lookup_key key, uint32_t scopei
 	};
 	if (IN6_IS_SCOPE_LINKLOCAL(key.addr6))
 		addr6.sin6_addr.s6_addr16[1] = htons(scopeid & 0xffff);
-	ent = (struct radix6_addr_entry *)(rnh->rnh_matchaddr(&addr6, &rnh->rh));
+	ent = (struct radix6_addr_entry *)(rn_match(&addr6, &rnh->rh));
 	if (ent != NULL)
 		return (ent->nhop);
 	return (NULL);
@@ -264,7 +264,7 @@ radix6_lookup(void *algo_data, const struct flm_lookup_key key, uint32_t scopeid
 
 	nh = NULL;
 	RIB_RLOCK(rh);
-	rn = rh->rnh_matchaddr((void *)&sin6, &rh->head);
+	rn = rn_match((void *)&sin6, &rh->head);
 	if (rn != NULL && ((rn->rn_flags & RNF_ROOT) == 0))
 		nh = (RNTORT(rn))->rt_nhop;
 	RIB_RUNLOCK(rh);
