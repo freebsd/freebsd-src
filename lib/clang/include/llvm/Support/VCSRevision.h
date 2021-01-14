88d08ca58fbe412c89b13a3a9c721547c27800b3
Dimitry Andric <dim@FreeBSD.org>
2021-01-14 21:45:55 +0100

    Merge llvm-project llvmorg-11.0.1-0-g43ff75f2c3f (aka 11.0.1 release)
    There were no functional changes after 11.0.1 rc2, so only the version
    strings changed.
    
    MFC after:      4 weeks
    X-MFC-With:     r364284

--- lib/clang/include/llvm/Support/VCSRevision.h
+++ lib/clang/include/llvm/Support/VCSRevision.h
@@ -1,3 +1,3 @@
 /* $FreeBSD$ */
-#define LLVM_REVISION "llvmorg-11.0.1-rc2-0-g43ff75f2c3f"
+#define LLVM_REVISION "llvmorg-11.0.1-0-g43ff75f2c3fe"
 #define LLVM_REPOSITORY "git@github.com:llvm/llvm-project.git"
