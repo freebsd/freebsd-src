b318c6970c83f65cb2727414aaf0b67a9d31eef8
Mark Johnston <markj@FreeBSD.org>
2021-01-14 11:41:28 -0500

    iwm(4): Add support for Intel Killer(R) Wireless-AC 1550i
    PR:		252578
    Submitted by:	shu <ankohuu@outlook.com>
    MFC after:	1 week

--- sys/dev/iwm/if_iwm.c
+++ sys/dev/iwm/if_iwm.c
@@ -5878,6 +5878,7 @@ iwm_intr(void *arg)
 #define	PCI_PRODUCT_INTEL_WL_8265_1	0x24fd
 #define	PCI_PRODUCT_INTEL_WL_9560_1	0x9df0
 #define	PCI_PRODUCT_INTEL_WL_9560_2	0xa370
+#define	PCI_PRODUCT_INTEL_WL_9560_3	0x31dc
 #define	PCI_PRODUCT_INTEL_WL_9260_1	0x2526
 
 static const struct iwm_devices {
@@ -5898,6 +5899,7 @@ static const struct iwm_devices {
 	{ PCI_PRODUCT_INTEL_WL_8265_1, &iwm8265_cfg },
 	{ PCI_PRODUCT_INTEL_WL_9560_1, &iwm9560_cfg },
 	{ PCI_PRODUCT_INTEL_WL_9560_2, &iwm9560_cfg },
+	{ PCI_PRODUCT_INTEL_WL_9560_3, &iwm9560_cfg },
 	{ PCI_PRODUCT_INTEL_WL_9260_1, &iwm9260_cfg },
 };
 
