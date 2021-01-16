2d494afb210f922e78856b83fb9c876d7168ed16
Emmanuel Vadot <manu@FreeBSD.org>
2021-01-16 11:34:10 +0100

    mips: Fix build by using the correct device-tree include path

--- sys/mips/mediatek/mtk_gpio_v2.c
+++ sys/mips/mediatek/mtk_gpio_v2.c
@@ -52,7 +52,7 @@ __FBSDID("$FreeBSD$");
 #include <dev/ofw/ofw_bus.h>
 #include <dev/ofw/ofw_bus_subr.h>
 
-#include <gnu/dts/include/dt-bindings/interrupt-controller/irq.h>
+#include <dt-bindings/interrupt-controller/irq.h>
 
 #include "gpio_if.h"
 #include "pic_if.h"
