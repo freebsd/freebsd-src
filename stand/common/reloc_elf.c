d1f31b9bf70452ea494cbbce584b89ce1f4c4c5b
Emmanuel Vadot <manu@FreeBSD.org>
2021-01-17 17:41:25 +0100

    loader: Partial revert of 987eabdc2ae1
    The arm relocation code doesn't not work and make loader not loading
    dependancies.
    
    Reported by:	Dr. Rolf Jansen <freebsd-rj@obsigna.com>

--- stand/common/reloc_elf.c
+++ stand/common/reloc_elf.c
@@ -175,8 +175,7 @@ __elfN(reloc)(struct elf_file *ef, symaddr_fn *symaddr, const void *reldata,
 	}
 
 	return (0);
-#elif defined(__aarch64__) || defined(__arm__) || defined(__powerpc__) || \
-    defined(__riscv)
+#elif defined(__aarch64__) || defined(__powerpc__) || defined(__riscv)
 	Elf_Size w;
 	const Elf_Rela *rela;
 
@@ -188,8 +187,6 @@ __elfN(reloc)(struct elf_file *ef, symaddr_fn *symaddr, const void *reldata,
 			switch (ELF_R_TYPE(rela->r_info)) {
 #if defined(__aarch64__)
 			case R_AARCH64_RELATIVE:
-#elif defined(__arm__)
-			case R_ARM_RELATIVE:
 #elif defined(__powerpc__)
 			case R_PPC_RELATIVE:
 #elif defined(__riscv)
