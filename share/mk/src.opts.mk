76eb7a96b8869accbc507bd412475e2cd900ba7f
Kyle Evans <kevans@FreeBSD.org>
2021-01-20 08:01:25 -0600

    build: remove LIBPTHREAD/LIBTHR build options
    WITHOUT_LIBTHR has been broken for a little over five years now, since the
    xz 5.2.0 update introduced a hard liblzma dependency on libthr, and building
    a useful system without threading support is becoming increasingly more
    difficult.
    
    Additionally, in the five plus years that it's been broken more reverse
    dependencies have cropped up in libzstd, libsqlite3, and libcrypto (among
    others) that make it more and more difficult to reconcile the effort needed
    to fix these options.
    
    Remove the broken options.
    
    PR:		252760
    Reviewed by:	brooks, emaste, kib
    Differential Revision:	https://reviews.freebsd.org/D28263

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -126,8 +126,6 @@ __DEFAULT_YES_OPTIONS = \
     LDNS_UTILS \
     LEGACY_CONSOLE \
     LIBCPLUSPLUS \
-    LIBPTHREAD \
-    LIBTHR \
     LLD \
     LLD_BOOTSTRAP \
     LLD_IS_LD \
@@ -376,10 +374,6 @@ BROKEN_OPTIONS+=CLANG_BOOTSTRAP LLD_BOOTSTRAP
 #
 .if ${MK_CAPSICUM} == "no"
 MK_CASPER:=	no
-.endif
-
-.if ${MK_LIBPTHREAD} == "no"
-MK_LIBTHR:=	no
 .endif
 
 .if ${MK_SOURCELESS} == "no"
