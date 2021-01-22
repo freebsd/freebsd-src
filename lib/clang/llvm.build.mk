a53ce3fc4938e37d5ec89304846203d2083c61a2
Glen Barber <gjb@FreeBSD.org>
2021-01-21 19:10:07 -0500

    Bump CURRENT to 14.0
    This one goes to 14.
    
    Approved by:	re (implicit)
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

--- lib/clang/llvm.build.mk
+++ lib/clang/llvm.build.mk
@@ -40,7 +40,7 @@ TARGET_ABI=	-gnueabi
 TARGET_ABI=
 .endif
 VENDOR=		unknown
-OS_VERSION=	freebsd13.0
+OS_VERSION=	freebsd14.0
 
 LLVM_TARGET_TRIPLE?=	${TARGET_ARCH:C/amd64/x86_64/:C/[hs]f$//:S/mipsn32/mips64/}-${VENDOR}-${OS_VERSION}${TARGET_ABI}
 LLVM_BUILD_TRIPLE?=	${BUILD_ARCH:C/amd64/x86_64/:C/[hs]f$//:S/mipsn32/mips64/}-${VENDOR}-${OS_VERSION}
