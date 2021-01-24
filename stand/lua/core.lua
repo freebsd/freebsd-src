e25ee296c919d6567aa76058a7049eac974797fb
Kyle Evans <kevans@FreeBSD.org>
2021-01-23 19:32:38 -0600

    stand: lua: enhance lfs.dir() to speed up kernels_autodetect
    This eliminates a lot of stat() calls that happen when lualoader renders the
    menu with the default settings, and greatly speeds up rendering on my
    laptop.
    
    ftype is nil if loader/loader.efi hasn't been updated yet, falling back to
    lfs.attributes() to test.
    
    This is technically incompatible with lfs, but not in a particularly
    terrible way.
    
    Reviewed-by:	cem
    MFC-after:	4 days
    Differential Revision:	https://reviews.freebsd.org/D27542

--- stand/lua/core.lua
+++ stand/lua/core.lua
@@ -240,14 +240,18 @@ function core.kernelList()
 	-- Automatically detect other bootable kernel directories using a
 	-- heuristic.  Any directory in /boot that contains an ordinary file
 	-- named "kernel" is considered eligible.
-	for file in lfs.dir("/boot") do
+	for file, ftype in lfs.dir("/boot") do
 		local fname = "/boot/" .. file
 
 		if file == "." or file == ".." then
 			goto continue
 		end
 
-		if lfs.attributes(fname, "mode") ~= "directory" then
+		if ftype then
+			if ftype ~= lfs.DT_DIR then
+				goto continue
+			end
+		elseif lfs.attributes(fname, "mode") ~= "directory" then
 			goto continue
 		end
 
