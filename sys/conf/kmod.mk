bcb7f57aa22757dd0206e88894bc003a93d0c351
Mateusz Guzik <mjg@FreeBSD.org>
2021-01-27 13:25:21 +0100

    conf/kern.mk: save some work by using realpath instead of cd ; pwd
    I did not check if the entire ordeal can be avoided in the first place.

--- sys/conf/kmod.mk
+++ sys/conf/kmod.mk
@@ -309,7 +309,7 @@ ${_ILINKS}:
 	*) \
 		path=${SYSDIR}/${.TARGET:T}/include ;; \
 	esac ; \
-	path=`(cd $$path && /bin/pwd)` ; \
+	path=`realpath $$path`; \
 	${ECHO} ${.TARGET:T} "->" $$path ; \
 	ln -fns $$path ${.TARGET:T}
 
