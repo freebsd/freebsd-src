7c5a3f32c393d523fd84a54eda31b487c09c0956
Bjoern A. Zeeb <bz@FreeBSD.org>
2021-01-28 16:05:32 +0000

    LinuxKPI: add firmware loading support
    Implement linux firmware KPI compat code.
    This includes: request_firmware() request_firmware_nowait(),
    request_firmware_direct(), firmware_request_nowarn(),
    and release_firmware().
    
    Given we will try to map requested names from natively ported
    or full-linuxkpi-using drivers to a firmware(9) auto-loading
    name format (.ko file name and image name matching),
    we quieten firmware(9) and print success or failure (unless
    the _nowarn() version was called) in the linuxkpi implementation.
    At the moment we try up-to 4 different naming combinations,
    with path stripped, original name, and requested name with '/'
    or '.' replaced.
    
    We do not currently defer loading in the "nowait" case.
    
    Sponsored-by:	The FreeBSD Foundation
    Sponsored-by:	Rubicon Communications, LLC ("Netgate")
    		(firmware(9) nowarn update from D27413)
    MFC after:	3 days
    Reviewed by:	kib, manu (looked at older versions)
    Differential Revision:	https://reviews.freebsd.org/D27414

---
+++ sys/compat/linuxkpi/common/include/linux/firmware.h
@@ -0,0 +1,105 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2020-2021 The FreeBSD Foundation
+ *
+ * This software was developed by Björn Zeeb under sponsorship from
+ * the FreeBSD Foundation.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef	_LINUXKPI_LINUX_FIRMWARE_H
+#define	_LINUXKPI_LINUX_FIRMWARE_H
+
+#include <sys/types.h>
+#include <linux/types.h>
+#include <linux/device.h>
+
+struct firmware;
+
+struct linuxkpi_firmware {
+	size_t			size;
+	const uint8_t		*data;
+	/* XXX Does Linux expose anything else? */
+
+	/* This is LinuxKPI implementation private. */
+	const struct firmware	*fbdfw;
+};
+
+int linuxkpi_request_firmware_nowait(struct module *, bool, const char *,
+    struct device *, gfp_t, void *,
+    void(*cont)(const struct linuxkpi_firmware *, void *));
+int linuxkpi_request_firmware(const struct linuxkpi_firmware **,
+    const char *, struct device *);
+int linuxkpi_firmware_request_nowarn(const struct linuxkpi_firmware **,
+    const char *, struct device *);
+void linuxkpi_release_firmware(const struct linuxkpi_firmware *);
+
+
+static __inline int
+request_firmware_nowait(struct module *mod, bool _t,
+    const char *fw_name, struct device *dev, gfp_t gfp, void *drv,
+    void(*cont)(const struct linuxkpi_firmware *, void *))
+{
+
+
+	return (linuxkpi_request_firmware_nowait(mod, _t, fw_name, dev, gfp,
+	    drv, cont));
+}
+
+static __inline int
+request_firmware(const struct linuxkpi_firmware **fw,
+    const char *fw_name, struct device *dev)
+{
+
+	return (linuxkpi_request_firmware(fw, fw_name, dev));
+}
+
+static __inline int
+request_firmware_direct(const struct linuxkpi_firmware **fw,
+    const char *fw_name, struct device *dev)
+{
+
+	return (linuxkpi_request_firmware(fw, fw_name, dev));
+}
+
+static __inline int
+firmware_request_nowarn(const struct linuxkpi_firmware **fw,
+    const char *fw_name, struct device *dev)
+{
+
+	return (linuxkpi_firmware_request_nowarn(fw, fw_name, dev));
+}
+
+static __inline void
+release_firmware(const struct linuxkpi_firmware *fw)
+{
+
+	linuxkpi_release_firmware(fw);
+}
+
+#define	firmware	linuxkpi_firmware
+
+#endif	/* _LINUXKPI_LINUX_FIRMWARE_H */
