71b460513827a730c4e5dbf0c9a8eb3a18a95112
Mateusz Guzik <mjg@FreeBSD.org>
2021-01-31 13:23:42 +0100

    Drop temporary compat in setproctitle
    (cherry picked from commit 46f168bc665a1aee79d91bd102aef7d4754e7917)

--- lib/libc/gen/setproctitle.c
+++ lib/libc/gen/setproctitle.c
@@ -185,17 +185,7 @@ setproctitle_fast(const char *fmt, ...)
 		oid[1] = KERN_PROC;
 		oid[2] = KERN_PROC_ARGS;
 		oid[3] = -1;
-		if (sysctl(oid, 4, 0, 0, "", 0) != 0) {
-			/*
-			 * Temporary compat for kernels which don't support
-			 * passing -1.
-			 */
-			oid[0] = CTL_KERN;
-			oid[1] = KERN_PROC;
-			oid[2] = KERN_PROC_ARGS;
-			oid[3] = getpid();
-			sysctl(oid, 4, 0, 0, "", 0);
-		}
+		sysctl(oid, 4, 0, 0, "", 0);
 		fast_update = 1;
 	}
 }
@@ -217,17 +207,7 @@ setproctitle(const char *fmt, ...)
 		oid[1] = KERN_PROC;
 		oid[2] = KERN_PROC_ARGS;
 		oid[3] = -1;
-		if (sysctl(oid, 4, 0, 0, buf, strlen(buf) + 1) != 0) {
-			/*
-			 * Temporary compat for kernels which don't support
-			 * passing -1.
-			 */
-			oid[0] = CTL_KERN;
-			oid[1] = KERN_PROC;
-			oid[2] = KERN_PROC_ARGS;
-			oid[3] = getpid();
-			sysctl(oid, 4, 0, 0, buf, strlen(buf) + 1);
-		}
+		sysctl(oid, 4, 0, 0, buf, strlen(buf) + 1);
 		fast_update = 0;
 	}
 }
