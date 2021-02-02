b20f358a2de4fa442e8650f78187ad19764c7db8
Alex Richardson <arichardson@FreeBSD.org>
2021-02-02 09:52:52 +0000

    tests/sys/kern/crc32: Check for SSE4.2 before using it
    This avoids a SIGILL when running these tests on QEMU (which
    defaults to a basic amd64 CPU without SSE4.2).
    
    This commit also tests the table-based implementations in addition to
    testing the hw-accelerated crc32 versions.
    
    Reviewed By:	cem, kib, markj
    Differential Revision: https://reviews.freebsd.org/D28395
    
    (cherry picked from commit 83c20b8a2da04937cf4af127366b3dc92c855784)

--- sys/sys/gsb_crc32.h
+++ sys/sys/gsb_crc32.h
@@ -32,16 +32,21 @@ crc32(const void *buf, size_t size)
 	crc = crc32_raw(buf, size, ~0U);
 	return (crc ^ ~0U);
 }
+#endif
 
 uint32_t calculate_crc32c(uint32_t crc32c, const unsigned char *buffer,
     unsigned int length);
-#endif
 
 #if defined(__amd64__) || defined(__i386__)
 uint32_t sse42_crc32c(uint32_t, const unsigned char *, unsigned);
 #endif
 #if defined(__aarch64__)
 uint32_t armv8_crc32c(uint32_t, const unsigned char *, unsigned int);
+#endif
+
+#ifdef TESTING
+uint32_t singletable_crc32c(uint32_t, const void *, size_t);
+uint32_t multitable_crc32c(uint32_t, const void *, size_t);
 #endif
 
 #endif /* !_SYS_GSB_CRC32_H_ */
