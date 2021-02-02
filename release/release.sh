554e81b0ad1a436c231b1cdd47c4ce60ca3003f4
Glen Barber <gjb@FreeBSD.org>
2021-02-02 12:19:36 -0500

    release: update workaround during transition to git
    PR:		253181
    Submitted by:	Yasuhiro Kimura
    MFC after:	3 days
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

--- release/release.sh
+++ release/release.sh
@@ -246,8 +246,11 @@ chroot_setup() {
 		fi
 	fi
 	if [ -z "${NOPORTS}" ] && [ -z "${PORTS_UPDATE_SKIP}" ]; then
-		if [ -d "${CHROOTDIR}/usr/ports/.git" ]; then
-			git -C ${CHROOTDIR}/usr/ports pull -q
+		# if [ -d "${CHROOTDIR}/usr/ports/.git" ]; then
+			# git -C ${CHROOTDIR}/usr/ports pull -q
+		# XXX: Workaround for the overlap in the Git conversion timeframe.
+		if [ -d "${CHROOTDIR}/usr/ports/.svn" ]; then
+			${SVNCMD} update ${CHROOTDIR}/usr/ports
 		else
 			#${VCSCMD} ${PORT} -b ${PORTBRANCH} ${CHROOTDIR}/usr/ports
 			# XXX: Workaround for the overlap in the Git
