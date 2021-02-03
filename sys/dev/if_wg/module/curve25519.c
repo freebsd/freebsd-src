82874dcb3610b1e57fb6b1b9db96ac4996bfa620
Peter Grehan <grehan@FreeBSD.org>
2021-02-03 19:05:09 +1000

    Always clamp curve25519 keys prior to use.
    Approved by:	re (gjb)
    
    (cherry picked from commit 6136a10e355a7a837edecbccbed04c34b4bc32c9)

--- sys/dev/if_wg/module/curve25519.c
+++ sys/dev/if_wg/module/curve25519.c
@@ -767,6 +767,7 @@ void curve25519_generic(u8 out[CURVE25519_KEY_SIZE],
 	u8 e[32];
 
 	memcpy(e, scalar, 32);
+	curve25519_clamp_secret(e);
 
 	/* The following implementation was transcribed to Coq and proven to
 	 * correspond to unary scalar multiplication in affine coordinates given
