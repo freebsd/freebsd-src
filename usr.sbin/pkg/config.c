6cf4aaf763ca0fb35d77e00d1daede803038bbf0
Kyle Evans <kevans@FreeBSD.org>
2021-02-11 18:58:26 -0600

    pkg(7): add an -r reponame option for bootstrap and add
    This is limited to bootstrap/add because some real pkg(8) commands
    have -r flags with an incompatible meaning/usage, e.g., pkg-audit.
    pkg(7) will still commence the search as it has, but it will ignore any
    repo objects without the given name so that overrides and whatnot still
    work as expected.
    
    The use of it for add is noted in the manpage; notably, that the
    signature config for that repository will be used over global config if
    it's specified. i.e., pkg(7) should assume that the given pkg did come
    from that repository and treat it appropriately.
    
    (cherry picked from commit 18418e1936b59c34a9c4a02a1ba5fe0d00dde1b3)

--- usr.sbin/pkg/config.c
+++ usr.sbin/pkg/config.c
@@ -324,7 +324,7 @@ cleanup:
  * etc...
  */
 static void
-parse_repo_file(ucl_object_t *obj)
+parse_repo_file(ucl_object_t *obj, const char *requested_repo)
 {
 	ucl_object_iter_t it = NULL;
 	const ucl_object_t *cur;
@@ -339,13 +339,17 @@ parse_repo_file(ucl_object_t *obj)
 		if (cur->type != UCL_OBJECT)
 			continue;
 
+		if (requested_repo != NULL && strcmp(requested_repo, key) != 0)
+			continue;
+
 		config_parse(cur, CONFFILE_REPO);
 	}
 }
 
 
 static int
-read_conf_file(const char *confpath, pkg_conf_file_t conftype)
+read_conf_file(const char *confpath, const char *requested_repo,
+    pkg_conf_file_t conftype)
 {
 	struct ucl_parser *p;
 	ucl_object_t *obj = NULL;
@@ -369,7 +373,7 @@ read_conf_file(const char *confpath, pkg_conf_file_t conftype)
 		if (conftype == CONFFILE_PKG)
 			config_parse(obj, conftype);
 		else if (conftype == CONFFILE_REPO)
-			parse_repo_file(obj);
+			parse_repo_file(obj, requested_repo);
 	}
 
 	ucl_object_unref(obj);
@@ -379,7 +383,7 @@ read_conf_file(const char *confpath, pkg_conf_file_t conftype)
 }
 
 static int
-load_repositories(const char *repodir)
+load_repositories(const char *repodir, const char *requested_repo)
 {
 	struct dirent *ent;
 	DIR *d;
@@ -403,8 +407,10 @@ load_repositories(const char *repodir)
 			    repodir,
 			    repodir[strlen(repodir) - 1] == '/' ? "" : "/",
 			    ent->d_name);
-			if (access(path, F_OK) == 0 &&
-			    read_conf_file(path, CONFFILE_REPO)) {
+			if (access(path, F_OK) != 0)
+				continue;
+			if (read_conf_file(path, requested_repo,
+			    CONFFILE_REPO)) {
 				ret = 1;
 				goto cleanup;
 			}
@@ -418,7 +424,7 @@ cleanup:
 }
 
 int
-config_init(void)
+config_init(const char *requested_repo)
 {
 	char *val;
 	int i;
@@ -459,7 +465,7 @@ config_init(void)
 	localbase = getlocalbase();
 	snprintf(confpath, sizeof(confpath), "%s/etc/pkg.conf", localbase);
 
-	if (access(confpath, F_OK) == 0 && read_conf_file(confpath,
+	if (access(confpath, F_OK) == 0 && read_conf_file(confpath, NULL,
 	    CONFFILE_PKG))
 		goto finalize;
 
@@ -477,7 +483,7 @@ config_init(void)
 	}
 
 	STAILQ_FOREACH(cv, c[REPOS_DIR].list, next)
-		if (load_repositories(cv->value))
+		if (load_repositories(cv->value, requested_repo))
 			goto finalize;
 
 finalize:
