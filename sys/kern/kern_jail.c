491cafa9611306ec513c9cbcb766a49ff7167dad
Jamie Gritton <jamie@FreeBSD.org>
2021-02-16 11:19:13 -0800

    MFC jail: Handle a possible race between jail_remove(2) and fork(2)
    jail_remove(2) includes a loop that sends SIGKILL to all processes
    in a jail, but skips processes in PRS_NEW state.  Thus it is possible
    the a process in mid-fork(2) during jail removal can survive the jail
    being removed.
    
    Add a prison flag PR_REMOVE, which is checked before the new process
    returns.  If the jail is being removed, the process will then exit.
    Also check this flag in jail_attach(2) which has a similar issue.
    
    Approved by:	so
    Security:	CVE-2020-25581
    Security:	FreeBSD-SA-21:04.jail_remove
    Reported by:	mjg
    Approved by:	kib
    
    (cherry picked from commit cc7b73065302005ebc4a19503188c8d6d5eb923d)
    (cherry picked from commit c837631bd47af73d03e3d8907f1e58b88403007c)

--- sys/kern/kern_jail.c
+++ sys/kern/kern_jail.c
@@ -1751,6 +1751,7 @@ kern_jail_set(struct thread *td, struct uio *optuio, int flags)
 		}
 	}
 	pr->pr_flags = (pr->pr_flags & ~ch_flags) | pr_flags;
+	pr->pr_flags &= ~PR_REMOVE;
 	mtx_unlock(&pr->pr_mtx);
 
 #ifdef RACCT
@@ -2287,6 +2288,12 @@ prison_remove_one(struct prison *pr)
 	struct proc *p;
 	int deuref;
 
+	/*
+	 * Mark the prison as doomed, so it doesn't accidentally come back
+	 * to life.  It may still be explicitly brought back by jail_set(2).
+	 */
+	pr->pr_flags |= PR_REMOVE;
+
 	/* If the prison was persistent, it is not anymore. */
 	deuref = 0;
 	if (pr->pr_flags & PR_PERSIST) {
@@ -2431,6 +2438,17 @@ do_jail_attach(struct thread *td, struct prison *pr)
 #endif
 	prison_deref(oldcred->cr_prison, PD_DEREF | PD_DEUREF);
 	crfree(oldcred);
+
+	/*
+	 * If the prison was killed while changing credentials, die along
+	 * with it.
+	 */
+	if (pr->pr_flags & PR_REMOVE) {
+		PROC_LOCK(p);
+		kern_psignal(p, SIGKILL);
+		PROC_UNLOCK(p);
+	}
+
 	return (0);
 
  e_unlock:
