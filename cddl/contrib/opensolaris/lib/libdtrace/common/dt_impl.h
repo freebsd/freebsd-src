af80731179386cf019463e392370b6478a714aea
Mark Johnston <markj@FreeBSD.org>
2021-02-17 10:49:38 -0500

    libdtrace: Stop relying on lex compatibility
    It does not appear to be required, and as of commit 6b7e592c215f
    ("lex: Do not let input() return 0 when end-of-file is reached") it
    causes input to return 0 instead of EOF when end-of-input is reached.
    
    PR:		253440
    Sponsored by:	The FreeBSD Foundation
    
    (cherry picked from commit 8ba333e02eaa59337a4e1d5534d4e894344c8226)

--- cddl/contrib/opensolaris/lib/libdtrace/common/dt_impl.h
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_impl.h
@@ -717,7 +717,7 @@ extern dt_pcb_t *yypcb;		/* pointer to current parser control block */
 extern char yyintprefix;	/* int token prefix for macros (+/-) */
 extern char yyintsuffix[4];	/* int token suffix ([uUlL]*) */
 extern int yyintdecimal;	/* int token is decimal (1) or octal/hex (0) */
-extern char yytext[];		/* lex input buffer */
+extern char *yytext;		/* lex input buffer */
 extern int yylineno;		/* lex line number */
 extern int yydebug;		/* lex debugging */
 extern dt_node_t *yypragma;	/* lex token list for control lines */
