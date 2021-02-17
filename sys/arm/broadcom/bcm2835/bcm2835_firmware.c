1cf282363101f5d99b1dadfb0d3250bbe6f482a5
Emmanuel Vadot <manu@FreeBSD.org>
2021-02-17 13:11:36 +0100

    arm64: rpi4: firmware: Attach at BUS_PASS_BUS + BUS_PASS_ORDER_LATE
    The node have now a compatible with simple-mfd so we need to attach
    at the same pass so the specific driver will be used.
    
    MFC after:	3 days
    PR:		252971

--- sys/arm/broadcom/bcm2835/bcm2835_firmware.c
+++ sys/arm/broadcom/bcm2835/bcm2835_firmware.c
@@ -180,5 +180,5 @@ DEFINE_CLASS_1(bcm2835_firmware, bcm2835_firmware_driver,
     simplebus_driver);
 
 EARLY_DRIVER_MODULE(bcm2835_firmware, simplebus, bcm2835_firmware_driver,
-    bcm2835_firmware_devclass, 0, 0, BUS_PASS_INTERRUPT + BUS_PASS_ORDER_LAST);
+    bcm2835_firmware_devclass, 0, 0, BUS_PASS_BUS + BUS_PASS_ORDER_LATE);
 MODULE_DEPEND(bcm2835_firmware, mbox, 1, 1, 1);
