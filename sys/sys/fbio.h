3d88bffe1f6b84c8a6d108280a83d5c30b012236
Alfredo Dal'Ava Junior <alfredo@FreeBSD.org>
2021-02-19 20:50:50 -0300

    fbio: Use appropriate types for the physical and virtual framebuffer address
    Use appropriate types for the physical and virtual framebuffer address.
    Fixes framebuffers mapped above 4G physical on 32-bit systems that
    support physical address extensions like i386 and Book-E powerpc.
    
    Patch developed by bdragon
    
    Reviewed by:	bdragon, luporl
    Relnotes:	yes
    Differential Revision:	https://reviews.freebsd.org/D28604

--- sys/sys/fbio.h
+++ sys/sys/fbio.h
@@ -136,8 +136,8 @@ struct fb_info {
 	fb_leave_t	*leave;
 	fb_setblankmode_t *setblankmode;
 
-	uintptr_t	fb_pbase;	/* For FB mmap. */
-	uintptr_t	fb_vbase;	/* if NULL, use fb_write/fb_read. */
+	vm_paddr_t	fb_pbase;	/* For FB mmap. */
+	vm_offset_t	fb_vbase;	/* if NULL, use fb_write/fb_read. */
 	void		*fb_priv;	/* First argument for read/write. */
 	const char	*fb_name;
 	uint32_t	fb_flags;
