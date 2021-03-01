a2c0e94ccfafc6a873fc6acbaccee8a14f968acf
Elliott Mitchell <ehem+freebsd@m5p.com>
2021-03-01 12:44:54 +0100

    xen: remove x86-ism from Xen common code
    PAT_WRITE_BACK is x86-only, whereas sys/dev/xen could be shared
    between multiple architectures.
    
    Reviewed by: royger
    Differential Revision: https://reviews.freebsd.org/D28831

--- sys/dev/xen/console/xen_console.c
+++ sys/dev/xen/console/xen_console.c
@@ -273,7 +273,7 @@ static void
 xencons_early_init_ring(struct xencons_priv *cons)
 {
 	cons->intf = pmap_mapdev_attr(ptoa(xen_get_console_mfn()), PAGE_SIZE,
-	    PAT_WRITE_BACK);
+	    VM_MEMATTR_WRITE_BACK);
 	cons->evtchn = xen_get_console_evtchn();
 }
 
