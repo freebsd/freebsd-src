720dc6bcb5a8c4283802576e2ef54f42b33fa8d4
Mitchell Horne <mhorne@FreeBSD.org>
2021-03-01 11:07:54 -0400

    Consolidate machine/endian.h definitions
    This change serves two purposes.
    
    First, we take advantage of the compiler provided endian definitions to
    eliminate some long-standing duplication between the different versions
    of this header. __BYTE_ORDER__ has been defined since GCC 4.6, so there
    is no need to rely on platform defaults or e.g. __MIPSEB__ to determine
    endianness. A new common sub-header is added, but there should be no
    changes to the visibility of these definitions.
    
    Second, this eliminates the hand-rolled __bswapNN() routines, again in
    favor of the compiler builtins. This was done already for x86 in
    e6ff6154d203. The benefit here is that we no longer have to maintain our
    own implementations on each arch, and can instead rely on the compiler
    to emit appropriate instructions or libcalls, as available. This should
    result in equivalent or better code generation. Notably 32-bit arm will
    start using the `rev` instruction for these routines, which is available
    on armv6+.
    
    PR:		236920
    Reviewed by:	arichardson, imp
    Tested by:	bdragon (BE powerpc)
    MFC after:	3 weeks
    Differential Revision:	https://reviews.freebsd.org/D29012

--- sys/sys/_endian.h
+++ sys/sys/_endian.h
@@ -27,18 +27,66 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- *	@(#)endian.h	7.8 (Berkeley) 4/3/91
- * $FreeBSD$
  */
 
+#ifndef _SYS__ENDIAN_H_
+#define	_SYS__ENDIAN_H_
+
 #ifndef _MACHINE_ENDIAN_H_
-#define	_MACHINE_ENDIAN_H_
-
-#include <sys/cdefs.h>
-#ifndef	__ASSEMBLER__
-#include <sys/_types.h>
+#error "sys/_endian.h should not be included directly"
 #endif
-#include <sys/_endian.h>
 
-#endif /* !_MACHINE_ENDIAN_H_ */
+/* BSD Compatiblity */
+#define	_BYTE_ORDER	__BYTE_ORDER__
+
+/*
+ * Definitions for byte order, according to byte significance from low
+ * address to high.
+ */
+#define	_LITTLE_ENDIAN	__ORDER_LITTLE_ENDIAN__ /* LSB first: 1234 */
+#define	_BIG_ENDIAN	__ORDER_BIG_ENDIAN__    /* MSB first: 4321 */
+#define	_PDP_ENDIAN	__ORDER_PDP_ENDIAN__    /* LSB first in word,
+						 * MSW first in long: 3412 */
+
+/*
+ * Define the order of 32-bit words in 64-bit words.
+ */
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+#define	_QUAD_HIGHWORD	1
+#define	_QUAD_LOWWORD	0
+#elif _BYTE_ORDER == _BIG_ENDIAN
+#define	_QUAD_HIGHWORD	0
+#define	_QUAD_LOWWORD	1
+#else
+#error "Unsupported endian"
+#endif
+
+/*
+ * Deprecated variants that don't have enough underscores to be useful in more
+ * strict namespaces.
+ */
+#if __BSD_VISIBLE
+#define	LITTLE_ENDIAN   _LITTLE_ENDIAN
+#define	BIG_ENDIAN      _BIG_ENDIAN
+#define	PDP_ENDIAN      _PDP_ENDIAN
+#define	BYTE_ORDER      _BYTE_ORDER
+#endif
+
+/* bswap primitives, based on compiler builtins */
+#define	__bswap16(x)	__builtin_bswap16(x)
+#define	__bswap32(x)	__builtin_bswap32(x)
+#define	__bswap64(x)	__builtin_bswap64(x)
+
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+#define	__ntohl(x)	(__bswap32(x))
+#define	__ntohs(x)	(__bswap16(x))
+#define	__htonl(x)	(__bswap32(x))
+#define	__htons(x)	(__bswap16(x))
+#elif _BYTE_ORDER == _BIG_ENDIAN
+#define	__htonl(x)	((__uint32_t)(x))
+#define	__htons(x)	((__uint16_t)(x))
+#define	__ntohl(x)	((__uint32_t)(x))
+#define	__ntohs(x)	((__uint16_t)(x))
+#endif
+
+#endif /* _SYS__ENDIAN_H_ */
