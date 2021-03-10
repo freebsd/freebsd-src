742d315019789de01d5dbf736cbafd49cc603276
Greg V <greg@unrelenting.technology>
2021-03-10 17:17:09 -0500

    kern.mk: fix -Wno-error style to fix build with Clang 12
    Clang 12 no longer supports -Wno-error-..., only the -Wno-error=...
    style (which is already used everywhere else in the tree).
    
    Differential Revision:	https://reviews.freebsd.org/D29157
    
    (cherry picked from commit 15565e0a2177f53b086609fecd48991c52dad5eb)

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -17,22 +17,22 @@ CWARNFLAGS?=	-Wall -Wredundant-decls -Wnested-externs -Wstrict-prototypes \
 # kernel where fixing them is more trouble than it is worth, or where there is
 # a false positive.
 .if ${COMPILER_TYPE} == "clang"
-NO_WCONSTANT_CONVERSION=	-Wno-error-constant-conversion
+NO_WCONSTANT_CONVERSION=	-Wno-error=constant-conversion
 NO_WSHIFT_COUNT_NEGATIVE=	-Wno-shift-count-negative
 NO_WSHIFT_COUNT_OVERFLOW=	-Wno-shift-count-overflow
 NO_WSELF_ASSIGN=		-Wno-self-assign
-NO_WUNNEEDED_INTERNAL_DECL=	-Wno-error-unneeded-internal-declaration
-NO_WSOMETIMES_UNINITIALIZED=	-Wno-error-sometimes-uninitialized
-NO_WCAST_QUAL=			-Wno-error-cast-qual
+NO_WUNNEEDED_INTERNAL_DECL=	-Wno-error=unneeded-internal-declaration
+NO_WSOMETIMES_UNINITIALIZED=	-Wno-error=sometimes-uninitialized
+NO_WCAST_QUAL=			-Wno-error=cast-qual
 NO_WTAUTOLOGICAL_POINTER_COMPARE= -Wno-tautological-pointer-compare
 # Several other warnings which might be useful in some cases, but not severe
 # enough to error out the whole kernel build.  Display them anyway, so there is
 # some incentive to fix them eventually.
-CWARNEXTRA?=	-Wno-error-tautological-compare -Wno-error-empty-body \
-		-Wno-error-parentheses-equality -Wno-error-unused-function \
-		-Wno-error-pointer-sign
+CWARNEXTRA?=	-Wno-error=tautological-compare -Wno-error=empty-body \
+		-Wno-error=parentheses-equality -Wno-error=unused-function \
+		-Wno-error=pointer-sign
 .if ${COMPILER_VERSION} >= 30700
-CWARNEXTRA+=	-Wno-error-shift-negative-value
+CWARNEXTRA+=	-Wno-error=shift-negative-value
 .endif
 .if ${COMPILER_VERSION} >= 40000
 CWARNEXTRA+=	-Wno-address-of-packed-member
