0fd2e729f8c69e25ec8e7951db3fa671f3ee4809
Kristof Provost <kp@FreeBSD.org>
2021-03-10 11:10:04 +0100

    pf: Factor out pf_krule_free()
    Reviewed by:	melifaro@
    MFC after:	1 week
    Sponsored by:	Rubicon Communications, LLC ("Netgate")
    Differential Revision:	https://reviews.freebsd.org/D29194
    
    (cherry picked from commit 5e9dae8e149ae8848f52148b665f3a0d031ca40f)

--- sys/net/pfvar.h
+++ sys/net/pfvar.h
@@ -1639,6 +1639,8 @@ void			 pf_remove_if_empty_kruleset(struct pf_kruleset *);
 struct pf_kruleset	*pf_find_kruleset(const char *);
 struct pf_kruleset	*pf_find_or_create_kruleset(const char *);
 void			 pf_rs_initialize(void);
+
+void			 pf_krule_free(struct pf_krule *);
 #endif
 
 /* The fingerprint functions can be linked into userland programs (tcpdump) */
