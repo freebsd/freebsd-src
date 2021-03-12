c0f34f06df1bb26b9499f1fe93a22babdb0c46fe
John Baldwin <jhb@FreeBSD.org>
2021-03-12 09:47:31 -0800

    amd64: Cleanups to setting TLS registers for Linux binaries.
    - Use update_pcb_bases() when updating FS or GS base addresses to
      permit use of FSBASE and GSBASE in Linux processes.  This also sets
      PCB_FULL_IRET.  linux32 was setting PCB_32BIT which should be a
      no-op (exec sets it).
    
    - Remove write-only variables to construct unused segment descriptors
      for linux32.
    
    Reviewed by:	kib
    MFC after:	1 week
    Sponsored by:	Netflix
    Differential Revision:	https://reviews.freebsd.org/D29026

--- sys/amd64/linux/linux_machdep.c
+++ sys/amd64/linux/linux_machdep.c
@@ -251,7 +251,7 @@ linux_arch_prctl(struct thread *td, struct linux_arch_prctl_args *args)
 	switch (args->code) {
 	case LINUX_ARCH_SET_GS:
 		if (args->addr < VM_MAXUSER_ADDRESS) {
-			set_pcb_flags(pcb, PCB_FULL_IRET);
+			update_pcb_bases(pcb);
 			pcb->pcb_gsbase = args->addr;
 			td->td_frame->tf_gs = _ugssel;
 			error = 0;
@@ -260,7 +260,7 @@ linux_arch_prctl(struct thread *td, struct linux_arch_prctl_args *args)
 		break;
 	case LINUX_ARCH_SET_FS:
 		if (args->addr < VM_MAXUSER_ADDRESS) {
-			set_pcb_flags(pcb, PCB_FULL_IRET);
+			update_pcb_bases(pcb);
 			pcb->pcb_fsbase = args->addr;
 			td->td_frame->tf_fs = _ufssel;
 			error = 0;
@@ -290,6 +290,7 @@ linux_set_cloned_tls(struct thread *td, void *desc)
 		return (EPERM);
 
 	pcb = td->td_pcb;
+	update_pcb_bases(pcb);
 	pcb->pcb_fsbase = (register_t)desc;
 	td->td_frame->tf_fs = _ufssel;
 
