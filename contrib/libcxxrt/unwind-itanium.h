052e7d54f06073f427d269131a9a6ffd69d35d77
Dimitry Andric <dim@FreeBSD.org>
2021-03-13 14:54:24 +0100

    Partially revert libcxxrt changes to avoid _Unwind_Exception change
    (Note I am also applying this to main and stable/13, to restore the old
    libcxxrt ABI and to avoid having to maintain a compat library.)
    
    After the recent cherry-picking of libcxxrt commits f0f2b13a6612 and
    7564726f5aa0, users reported that editors/libreoffice packages from the
    official package builders did not start anymore. It turns out that the
    combination of these commits subtly changes the ABI, requiring all
    applications that depend on internal details of struct _Unwind_Exception
    (available via unwind-arm.h and unwind-itanium.h) to be recompiled.
    
    However, the FreeBSD package builders always use -RELEASE jails, so
    these still use the old declaration of struct _Unwind_Exception, which
    is not entirely compatible. In particular, LibreOffice uses this struct
    in its internal "uno bridge" component, where it attempts to setup its
    own exception handling mechanism.
    
    To fix this incompatibility, go back to the old declarations of struct
    _Unwind_Exception, and restore the __LP64__ specific workaround we had
    in place before (which was to cope with yet another, older ABI bug).
    
    Effectively, this reverts upstream libcxxrt commits 88bdf6b290da
    ("Specify double-word alignment for ARM unwind") and b96169641f79
    ("Updated Itanium unwind"), and reapplies our commit 3c4fd2463bb2
    ("libcxxrt: add padding in __cxa_allocate_* to fix alignment").
    
    PR:		253840

--- contrib/libcxxrt/unwind-itanium.h
+++ contrib/libcxxrt/unwind-itanium.h
@@ -79,12 +79,9 @@ struct _Unwind_Exception
   {
     uint64_t exception_class;
     _Unwind_Exception_Cleanup_Fn exception_cleanup;
-    uintptr_t private_1;
-    uintptr_t private_2;
-#if __SIZEOF_POINTER__ == 4
-    uint32_t reserved[3];
-#endif
-  } __attribute__((__aligned__));
+    unsigned long private_1;
+    unsigned long private_2;
+  } ;
 
 extern _Unwind_Reason_Code _Unwind_RaiseException (struct _Unwind_Exception *);
 extern _Unwind_Reason_Code _Unwind_ForcedUnwind (struct _Unwind_Exception *,
