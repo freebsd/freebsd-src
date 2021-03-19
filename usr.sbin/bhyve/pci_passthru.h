847adbb116fbc1de0c44ff57ab3b2a843c5f3c94
Corvin Köhne <corvink@FreeBSD.org>
2021-03-19 13:48:34 +0100

    bhyve: define array to protect passthru regs
    GPU passthrough requires a special handling of some PCI config register.
    Therefore, we need a flexible approach for implementing it. Adding an
    array of handler meets this condition.
    
    Start by using the default handler for all accesses to the PCI config
    space. In upcoming commits, we can start to split the default handler
    into several handler for each register that requires emulation.
    
    Reviewed by:		markj
    MFC after:		1 week
    Sponsored by:		Beckhoff Automation GmbH & Co. KG
    Differential Revision:	https://reviews.freebsd.org/D39291
    
    (cherry picked from commit 931bb7bf1c8f90f3303b5291e4868c587f9364e7)

--- usr.sbin/bhyve/pci_passthru.h
+++ usr.sbin/bhyve/pci_passthru.h
@@ -11,5 +11,14 @@
 
 #include "pci_emul.h"
 
+struct passthru_softc;
+
+typedef int (*cfgread_handler)(struct passthru_softc *sc,
+    struct pci_devinst *pi, int coff, int bytes, uint32_t *rv);
+typedef int (*cfgwrite_handler)(struct passthru_softc *sc,
+    struct pci_devinst *pi, int coff, int bytes, uint32_t val);
+
 uint32_t read_config(const struct pcisel *sel, long reg, int width);
 void write_config(const struct pcisel *sel, long reg, int width, uint32_t data);
+int set_pcir_handler(struct passthru_softc *sc, int reg, int len,
+    cfgread_handler rhandler, cfgwrite_handler whandler);
