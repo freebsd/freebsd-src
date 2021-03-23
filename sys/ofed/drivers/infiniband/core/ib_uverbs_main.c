415e9b69a167d242619ae00bc86695e2d339007a
Bjoern A. Zeeb <bz@FreeBSD.org>
2021-03-23 14:24:49 +0000

    LinuxKPI: remove < 5.0 version support
    We are not aware of any out-of-tree consumers anymore
    which would need KPI support for before Linux version 5.
    Update the two in-tree consumers to use the new KPI.
    This allows us to remove the extra version check and
    will also give access to {lower,upper}_32_bits() unconditionally.
    
    Sponsored by:	The FreeBSD Foundation
    Reviewed by:	hselasky, rlibby, rstone
    X-MFC:		to 13 only
    Differential Revision: https://reviews.freebsd.org/D29391
    
    (cherry picked from commit 3b1ecc9fa1b57ca7a1c86661e9a323bc41c97ecc)

--- sys/ofed/drivers/infiniband/core/ib_uverbs_main.c
+++ sys/ofed/drivers/infiniband/core/ib_uverbs_main.c
@@ -856,8 +856,7 @@ static ssize_t ib_uverbs_write(struct file *filp, const char __user *buf,
 				goto out;
 			}
 
-			if (!access_ok(VERIFY_WRITE,
-				       (void __user *) (unsigned long) ex_hdr.response,
+			if (!access_ok((void __user *) (unsigned long) ex_hdr.response,
 				       (hdr.out_words + ex_hdr.provider_out_words) * 8)) {
 				ret = -EFAULT;
 				goto out;
