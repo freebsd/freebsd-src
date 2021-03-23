e7ad19fcb4f3630f90dc7e04c076e97feaa9ce77
Bjoern A. Zeeb <bz@FreeBSD.org>
2021-03-23 16:37:35 +0000

    LinuxKPI: add more linux-specific errno
    Add ERFKILL and EBADE found in iwlwifi and brcmfmac wireless drivers.
    While here add a comment above the block of error numbers above 500 to
    document expectations.
    
    Obtained-from:	bz_iwlwifi
    Sponsored-by:	The FreeBSD Foundation
    MFC-after:	2 weeks
    Reviewed-by:	hselasky, emaste
    Differential Revision:	https://reviews.freebsd.org/D29396

--- sys/compat/linuxkpi/common/include/linux/errno.h
+++ sys/compat/linuxkpi/common/include/linux/errno.h
@@ -45,6 +45,10 @@
 #define	ENOTSUPP	EOPNOTSUPP
 #define	ENONET		EHOSTDOWN
 
+/*
+ * The error numbers below are arbitrary and do not resemble the numbers
+ * used in Linux.  They should not be returned to user space.
+ */
 #define	ERESTARTNOINTR	513
 #define	ERESTARTNOHAND	514
 #define	ERESTART_RESTARTBLOCK 516
@@ -58,5 +62,7 @@
 #define	EBADTYPE	527
 #define	EJUKEBOX	528
 #define	EIOCBQUEUED	529
+#define	ERFKILL		530
+#define	EBADE		531
 
 #endif					/* _LINUX_ERRNO_H_ */
