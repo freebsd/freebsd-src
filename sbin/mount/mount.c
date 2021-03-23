35f05efcc26cadfcd4351e9d3cb0e02ce70221ae
Elyes HAOUAS <ehaouas@noos.fr>
2021-03-23 16:16:42 +0100

    sbin/{ifconfig,mount,nvmecontrol,pfctl}: Fix some typos
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>

--- sbin/mount/mount.c
+++ sbin/mount/mount.c
@@ -224,7 +224,7 @@ restart_mountd(void)
 		return;
 	}
 
-	/* We have mountd(8) PID in mountdpid varible, let's signal it. */
+	/* We have mountd(8) PID in mountdpid variable, let's signal it. */
 	if (kill(mountdpid, SIGHUP) == -1)
 		err(1, "signal mountd");
 }
@@ -481,7 +481,7 @@ ismounted(struct fstab *fs, struct statfs *mntbuf, int mntsize)
 
 	/* 
 	 * Consider the filesystem to be mounted if:
-	 * It has the same mountpoint as a mounted filesytem, and
+	 * It has the same mountpoint as a mounted filesystem, and
 	 * It has the same type as that same mounted filesystem, and
 	 * It has the same device name as that same mounted filesystem, OR
 	 *     It is a nonremountable filesystem
