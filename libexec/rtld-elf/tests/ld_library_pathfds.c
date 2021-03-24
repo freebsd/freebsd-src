7b845dc144e47c629f78dcede957f497bcc71536
Mariusz Zaborski <oshogbo@FreeBSD.org>
2021-03-24 22:10:33 +0100

    rtld: introduce PRELOAD_FDS
    (cherry picked from commit f90218886fc82e7b1fdb9e241adc5d713dadabe3)

--- libexec/rtld-elf/tests/ld_library_pathfds.c
+++ libexec/rtld-elf/tests/ld_library_pathfds.c
@@ -29,6 +29,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+#include "common.h"
 
 struct descriptors {
 	int	binary;
@@ -38,14 +39,8 @@ struct descriptors {
 	int	usr;
 };
 
-static void	setup(struct descriptors *, const atf_tc_t *);
-static void	expect_success(int binary, char *pathfds);
-static void	expect_missing_library(int binary, char *pathfds);
 
-static void	try_to_run(int binary, int expected_exit_status,
-    char * const *env, const char *expected_out, const char *expected_err);
-static int	opendir(const char *name);
-static int	opendirat(int parent, const char *name);
+static void	setup(struct descriptors *, const atf_tc_t *);
 
 
 ATF_TC_WITHOUT_HEAD(missing_library);
@@ -167,55 +162,10 @@ setup(struct descriptors *dp, const atf_tc_t *tc)
 	dp->testdir = opendir(atf_tc_get_config_var(tc, "srcdir"));
 	ATF_REQUIRE(dp->testdir >= 0);
 	ATF_REQUIRE(
-	    (dp->binary = openat(dp->testdir, "target", O_RDONLY)) >= 0);
+	    (dp->binary = openat(dp->testdir, TARGET_ELF_NAME, O_RDONLY)) >= 0);
 
 	ATF_REQUIRE((dp->root = opendir("/")) >= 0);
 	ATF_REQUIRE((dp->etc = opendirat(dp->root, "etc")) >= 0);
 	ATF_REQUIRE((dp->usr = opendirat(dp->root, "usr")) >= 0);
 }
 
-static void
-expect_success(int binary, char *pathfds)
-{
-	char * const env[] = { pathfds, NULL };
-	try_to_run(binary, 0, env, "the hypotenuse of 3 and 4 is 5\n", "");
-}
-
-static void
-expect_missing_library(int binary, char *pathfds)
-{
-	char * const env[] = { pathfds, NULL };
-	try_to_run(binary, 1, env, "",
-	   "ld-elf.so.1: Shared object \"libpythagoras.so.0\" not found,"
-	    " required by \"target\"\n");
-}
-
-
-static void
-try_to_run(int binary, int exit_status, char * const *env,
-        const char *expected_out, const char *expected_err)
-{
-	pid_t child = atf_utils_fork();
-
-	if (child == 0) {
-		char * const args[] = { "target", NULL };
-
-		fexecve(binary, args, env);
-		atf_tc_fail("fexecve() failed");
-	}
-
-	atf_utils_wait(child, exit_status, expected_out, expected_err);
-}
-
-
-static int
-opendir(const char *name)
-{
-	return open(name, O_RDONLY | O_DIRECTORY);
-}
-
-static int
-opendirat(int parent, const char *name)
-{
-	return openat(parent, name, O_RDONLY | O_DIRECTORY);
-}
