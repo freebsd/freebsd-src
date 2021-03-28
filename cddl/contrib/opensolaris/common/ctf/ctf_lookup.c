3c065eeaa7a5ebb56991f5c8123e343a83bf0f0d
Mark Johnston <markj@FreeBSD.org>
2021-03-27 20:36:44 -0400

    libctf: Adjust logic to match upstream after 410556f1f
    No functional change intended.
    
    Suggested by:	jrtc27
    MFC after:	1 week

--- cddl/contrib/opensolaris/common/ctf/ctf_lookup.c
+++ cddl/contrib/opensolaris/common/ctf/ctf_lookup.c
@@ -132,9 +132,9 @@ ctf_lookup_by_name(ctf_file_t *fp, const char *name)
 			continue; /* skip qualifier keyword */
 
 		for (lp = fp->ctf_lookups; lp->ctl_prefix != NULL; lp++) {
-			if ((size_t)(q - p) >= lp->ctl_len &&
-			    (lp->ctl_prefix[0] == '\0' ||
-			    strncmp(p, lp->ctl_prefix, (size_t)(q - p)) == 0)) {
+			if (lp->ctl_prefix[0] == '\0' ||
+			    ((size_t)(q - p) >= lp->ctl_len && strncmp(p,
+			    lp->ctl_prefix, (size_t)(q - p)) == 0)) {
 				for (p += lp->ctl_len; isspace(*p); p++)
 					continue; /* skip prefix and next ws */
 
