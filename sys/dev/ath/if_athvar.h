de8f9c8fe87159d3f96767fadeb23085c334a910
Adrian Chadd <adrian@FreeBSD.org>
2021-03-31 21:48:59 -0700

    [ath] Add ath_hal_getnav and ath_hal_setnav so the driver layer can check the NAV as appropriate.

--- sys/dev/ath/if_athvar.h
+++ sys/dev/ath/if_athvar.h
@@ -1492,6 +1492,10 @@ void	ath_intr(void *);
 	((*(_ah)->ah_setChainMasks)((_ah), (_txchainmask), (_rxchainmask)))
 #define	ath_hal_set_quiet(_ah, _p, _d, _o, _f) \
 	((*(_ah)->ah_setQuiet)((_ah), (_p), (_d), (_o), (_f)))
+#define	ath_hal_getnav(_ah) \
+	((*(_ah)->ah_getNav)((_ah)))
+#define	ath_hal_setnav(_ah, _val) \
+	((*(_ah)->ah_setNav)((_ah), (_val)))
 
 #define	ath_hal_spectral_supported(_ah) \
 	(ath_hal_getcapability(_ah, HAL_CAP_SPECTRAL_SCAN, 0, NULL) == HAL_OK)
