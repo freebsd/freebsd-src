424f6363927a113fe5a1dcb4691039ec25cf3680
Richard Scheffenegger <rscheff@FreeBSD.org>
2021-04-02 20:11:45 +0200

    Make sbuf_drain safe for external use
    While sbuf_drain was an internal function, two
    KASSERTS checked the sanity of it being called.
    However, an external caller may be ignorant if
    there is any data to drain, or if an error has
    already accumulated. Be nice and return immediately
    with the accumulated error.
    
    MFC after: 2 weeks
    Reviewed By: tuexen, #transport
    Sponsored by: NetApp, Inc.
    Differential Revision: https://reviews.freebsd.org/D29544
    
    (cherry picked from commit cad4fd0365a5e3235e715e072e6ee9dffaa7a3ab)

--- sys/kern/subr_sbuf.c
+++ sys/kern/subr_sbuf.c
@@ -380,8 +380,13 @@ sbuf_drain(struct sbuf *s)
 {
 	int len;
 
-	KASSERT(s->s_len > 0, ("Shouldn't drain empty sbuf %p", s));
-	KASSERT(s->s_error == 0, ("Called %s with error on %p", __func__, s));
+	/*
+	 * Immediately return when no work to do,
+	 * or an error has already been accumulated.
+	 */
+	if ((s->s_len == 0) || (s->s_error != 0))
+		return(s->s_error);
+
 	if (SBUF_DODRAINTOEOR(s) && s->s_rec_off == 0)
 		return (s->s_error = EDEADLK);
 	len = s->s_drain_func(s->s_drain_arg, s->s_buf,
