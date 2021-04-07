38b25dade179bec0eeccff95a5ae354e7304e474
Kurosawa Takahiro <takahiro.kurosawa@gmail.com>
2021-04-07 08:17:51 +0200

    pf tests: make synproxy and nat work correctly even if inetd is running
    tests/sys/netfil/pf/synproxy fails if inetd has been running
    outside of the jail because pidfile_open() fails with EEXIST.
    tests/sys/netfil/pf/nat has the same problem but the test succeeds
    because whether inetd is running is not so important.
    
    Fix the problem by changing the pidfile path from the default
    location.
    
    Reviewed by:	kp
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D29622
    
    (cherry picked from commit 5a5623397c73e46b206289d32d1d6b9de420da9c)

--- tests/sys/netpfil/pf/nat.sh
+++ tests/sys/netpfil/pf/nat.sh
@@ -27,7 +27,7 @@ exhaust_body()
 	jexec nat sysctl net.inet.ip.forwarding=1
 
 	jexec echo ifconfig ${epair_echo}b 198.51.100.2/24 up
-	jexec echo /usr/sbin/inetd $(atf_get_srcdir)/echo_inetd.conf
+	jexec echo /usr/sbin/inetd -p inetd-echo.pid $(atf_get_srcdir)/echo_inetd.conf
 
 	# Enable pf!
 	jexec nat pfctl -e
@@ -55,6 +55,7 @@ exhaust_body()
 
 exhaust_cleanup()
 {
+	rm -f inetd-echo.pid
 	pft_cleanup
 }
 
