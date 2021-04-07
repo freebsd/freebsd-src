5984246f9626fbc3d356ee2d3b3cd159f6d0a7c2
Yongbo Yao <yongbo.yao@dell.com>
2021-04-07 13:33:22 -0500

    Loader: support booting OS from memory disk (MD)
    Until now, the boot image can be embedded into the loader with
    /sys/tools/embed_mfs.sh, and memory disk (MD) is already supported
    in loader source. But due to memory disk (MD) driver isn't registered
    to the loader yet, the boot image can't be boot from embedded memory
    disk.
    
    Reviewed by:	dab, tsoome
    MFC after:	1 week
    Sponsored by:	Dell EMC Isilon
    Differential Revision:	https://reviews.freebsd.org/D29512

--- stand/efi/loader/main.c
+++ stand/efi/loader/main.c
@@ -296,6 +296,21 @@ probe_zfs_currdev(uint64_t guid)
 }
 #endif
 
+#ifdef MD_IMAGE_SIZE
+static bool
+probe_md_currdev(void)
+{
+	extern struct devsw md_dev;
+	bool rv;
+
+	set_currdev_devsw(&md_dev, 0);
+	rv = sanity_check_currdev();
+	if (!rv)
+		printf("MD not present\n");
+	return (rv);
+}
+#endif
+
 static bool
 try_as_currdev(pdinfo_t *hd, pdinfo_t *pp)
 {
@@ -568,6 +583,15 @@ find_currdev(bool do_bootmgr, bool is_last,
 			return (0);
 	}
 #endif /* EFI_ZFS_BOOT */
+
+#ifdef MD_IMAGE_SIZE
+	/*
+	 * If there is an embedded MD, try to use that.
+	 */
+	printf("Trying MD\n");
+	if (probe_md_currdev())
+		return (0);
+#endif /* MD_IMAGE_SIZE */
 
 	/*
 	 * Try to find the block device by its handle based on the
