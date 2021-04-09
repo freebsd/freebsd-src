ce8a17c2ba6cb1dba49d25c70b91ff8203ad816c
Konstantin Belousov <kib@FreeBSD.org>
2021-04-10 02:23:54 +0300

    amd64: clear debug registers on execing 32bit native binary
    (cherry picked from commit d50adfec9ee73e88e8d365525f1acef2c1db798a)

--- sys/amd64/ia32/ia32_signal.c
+++ sys/amd64/ia32/ia32_signal.c
@@ -971,6 +971,8 @@ ia32_setregs(struct thread *td, struct image_params *imgp, u_long stack)
 	regs->tf_gs = _ugssel;
 	regs->tf_flags = TF_HASSEGS;
 
+	x86_clear_dbregs(pcb);
+
 	fpstate_drop(td);
 
 	/* Return via doreti so that we can change to a different %cs */
