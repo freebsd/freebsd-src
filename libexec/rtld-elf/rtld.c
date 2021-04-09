freebsd/freebsd-src

commit d36d6816151705907393889d661cbfd25c630ca8

rtld dl_iterate_phdr(): dlpi_tls_data is wrong
dl_iterate_phdr() dlpi_tls_data should provide the TLS module segment
address, and not the TLS init segment address as it does now.

Reported by:	emacsray@gmail.com
PR:	254774
Sponsored by:	The FreeBSD Foundation
MFC after:	1 week

libexec/rtld-elf/rtld.c
@@ -3909,13 +3909,16 @@ dlinfo(void *handle, int request, void *p)
 static void
 rtld_fill_dl_phdr_info(const Obj_Entry *obj, struct dl_phdr_info *phdr_info)
 {
+	tls_index ti;
 
 	phdr_info->dlpi_addr = (Elf_Addr)obj->relocbase;
 	phdr_info->dlpi_name = obj->path;
 	phdr_info->dlpi_phdr = obj->phdr;
 	phdr_info->dlpi_phnum = obj->phsize / sizeof(obj->phdr[0]);
 	phdr_info->dlpi_tls_modid = obj->tlsindex;
-	phdr_info->dlpi_tls_data = obj->tlsinit;
+	ti.ti_module = obj->tlsindex;
+	ti.ti_offset = 0;
+	phdr_info->dlpi_tls_data = __tls_get_addr(&ti);
 	phdr_info->dlpi_adds = obj_loads;
 	phdr_info->dlpi_subs = obj_loads - obj_count;
 }
@@ -5914,3 +5917,6 @@ int _rtld_version__FreeBSD_version = __FreeBSD_version;
 
 extern char _rtld_version_laddr_offset __exported;
 char _rtld_version_laddr_offset;
+
+extern char _rtld_version_dlpi_tls_data __exported;
+char _rtld_version_dlpi_tls_data;

lib/libc/gen/dl_iterate_phdr.3
@@ -15,7 +15,7 @@
 .\"
 .\"   $OpenBSD: dl_iterate_phdr.3,v 1.3 2007/05/31 19:19:48 jmc Exp $
 .\" $FreeBSD$
-.Dd October 9, 2014
+.Dd April 5, 2021
 .Dt DL_ITERATE_PHDR 3
 .Os
 .Sh NAME
@@ -80,7 +80,10 @@ The counter of the object unloads performed by the dynamic linker.
 .It Fa dlpi_tls_modid
 The TLS index of the object.
 .It Fa dlpi_tls_data
-A pointer to the initialization data for the object TLS segment.
+A pointer to the calling thread' TLS data segment for this module,
+if it was allocated,
+.Dv NULL
+otherwise.
 .El
 .Pp
 Future versions of

libexec/rtld-elf/Symbol.map
@@ -34,4 +34,5 @@ FBSDprivate_1.0 {
     _r_debug_postinit;
     _rtld_version__FreeBSD_version;
     _rtld_version_laddr_offset;
+    _rtld_version_dlpi_tls_data;
 };

libexec/rtld-elf/rtld.1
@@ -424,6 +424,13 @@ See
 Also it indicates the presence of
 .Va l_refname
 member of the structure.
+.It Dv _rtld_version_dlpi_tls_data
+The
+.Va dlpi_tls_data
+member of the structure
+.Vt dl_phdr_info
+contains the address of the module TLS segment for the calling thread,
+and not the address of the initialization segment.
 .El
 .Sh FILES
 .Bl -tag -width ".Pa /var/run/ld-elf32.so.hints" -compact

sys/sys/param.h
@@ -60,7 +60,7 @@
  *		in the range 5 to 9.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1400006	/* Master, propagated to newvers */
+#define __FreeBSD_version 1400007	/* Master, propagated to newvers */
 
 /*
  * __FreeBSD_kernel__ indicates that this system uses the kernel of FreeBSD,

commit 7cb32a0d03437f881169b1c55cce89cf70ed43dd

rtld: avoid recursing on rtld_bind_lock for write
This fixes a regression in d36d6816151705907393889, where the call to
__tls_get_address() was performed under rtld_bind_lock write-locked.
Instead use tls_get_addr_slow() directly, with locked = true.

Reported by:	jkim, many others
Tested by:	jkim, bdragon (powerpc), mhorne (riscv)
Sponsored by:	The FreeBSD Foundation
MFC after:	1 week
Differential revision:	https://reviews.freebsd.org/D29623

libexec/rtld-elf/rtld-libc/Makefile.inc
@@ -67,6 +67,8 @@ _libc_other_objects+=syncicache abs
 _libc_other_objects+=syncicache
 .endif
 
+_libc_other_objects+=_get_tp
+
 # Extract all the .o files from libc_nossp_pic.a. This ensures that
 # we don't accidentally pull in the interposing table or similar by linking
 # directly against libc_nossp_pic.a

libexec/rtld-elf/rtld.c
@@ -3910,16 +3910,16 @@ dlinfo(void *handle, int request, void *p)
 static void
 rtld_fill_dl_phdr_info(const Obj_Entry *obj, struct dl_phdr_info *phdr_info)
 {
-	tls_index ti;
+	Elf_Addr **dtvp;
 
 	phdr_info->dlpi_addr = (Elf_Addr)obj->relocbase;
 	phdr_info->dlpi_name = obj->path;
 	phdr_info->dlpi_phdr = obj->phdr;
 	phdr_info->dlpi_phnum = obj->phsize / sizeof(obj->phdr[0]);
 	phdr_info->dlpi_tls_modid = obj->tlsindex;
-	ti.ti_module = obj->tlsindex;
-	ti.ti_offset = 0;
-	phdr_info->dlpi_tls_data = __tls_get_addr(&ti);
+	dtvp = _get_tp();
+	phdr_info->dlpi_tls_data = (char *)tls_get_addr_slow(dtvp,
+	    obj->tlsindex, 0, true) + TLS_DTV_OFFSET;
 	phdr_info->dlpi_adds = obj_loads;
 	phdr_info->dlpi_subs = obj_loads - obj_count;
 }

libexec/rtld-elf/rtld.h
@@ -401,6 +401,7 @@ bool allocate_tls_offset(Obj_Entry *obj);
 void free_tls_offset(Obj_Entry *obj);
 const Ver_Entry *fetch_ventry(const Obj_Entry *obj, unsigned long);
 int convert_prot(int elfflags);
+void *_get_tp(void);	/* libc implementation */
 
 /*
  * MD function declarations.