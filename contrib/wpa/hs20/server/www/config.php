40816acddd74ad2f5a6a2f297a3136ba4cfc4afb
Cy Schubert <cy@FreeBSD.org>
2021-04-17 08:44:52 -0700

    Revert wpa import.
    c42cb556066bcf3a3d8fb2064b4d233f198713cc resulted in a mismerge
    due to having to do a remerge because my tree was over a week out of
    date. Changes that were made to fix the build were lost in the remerge
    resulting in build errors that were fixed a week ago.

--- contrib/wpa/hs20/server/www/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$osu_root = "/home/user/hs20-server";
-$osu_db = "sqlite:$osu_root/AS/DB/eap_user.db";
-$t_c_file = "$osu_root/terms-and-conditions";
-$t_c_timestamp = 123456789;
-$hostapd_ctrl = "udg:///home/user/hs20-server/AS/ctrl/as"
-?>
--- contrib/wpa/src/objs.mk
+++
@@ -1,3 +0,0 @@
-$(_OBJS_VAR) := $(call BUILDOBJ,$($(_OBJS_VAR)))
--include $(filter-out %.a,$($(_OBJS_VAR):%.o=%.d))
-_DIRS += $(dir $($(_OBJS_VAR)))
