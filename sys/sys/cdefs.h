f2ef124469aef4ba17fb201d5412b04cc298bd2a
Greg V <greg@unrelenting.technology>
2021-04-20 01:47:15 +0100

    linker_set: fix globl/weak symbol redefinitions to work on clang 12
    In clang 12.0.0.rc2, going from weak to global is now a hard error:
    
    ```
    /usr/src/stand/libsa/amd64/_setjmp.S:67:25: error: _longjmp changed binding to STB_GLOBAL
    .text; .p2align 4,0x90; .globl _longjmp; .type _longjmp,@function; _longjmp:; .cfi_startproc
    ```
    
    And the other way is a warning, but we have -Werror:
    
    ```
    error: __start_set_Xcommand_set changed binding to STB_WEAK [-Werror,-Winline-asm]
    error: __stop_set_Xcommand_set changed binding to STB_WEAK [-Werror,-Winline-asm]
    ```
    
    ref: https://reviews.llvm.org/D90108
    
    Reviewed By:	arichardson
    MFC after:	1 week
    Differential Revision: https://reviews.freebsd.org/D29159
    
    (cherry picked from commit 32231805fbe2b9438c2de50c229b43c016207a08)

--- sys/sys/cdefs.h
+++ sys/sys/cdefs.h
@@ -569,8 +569,8 @@
 #endif	/* __STDC__ */
 #endif	/* __GNUC__ || __INTEL_COMPILER */
 
-#define	__GLOBL1(sym)	__asm__(".globl " #sym)
-#define	__GLOBL(sym)	__GLOBL1(sym)
+#define	__GLOBL(sym)	__asm__(".globl " __XSTRING(sym))
+#define	__WEAK(sym)	__asm__(".weak " __XSTRING(sym))
 
 #if defined(__GNUC__) || defined(__INTEL_COMPILER)
 #define	__IDSTRING(name,string)	__asm__(".ident\t\"" string "\"")
