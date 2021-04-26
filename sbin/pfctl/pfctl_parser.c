fc58438ca26b46f65439bcfc89052de1008b9774
Kristof Provost <kp@FreeBSD.org>
2021-04-26 10:07:07 +0200

    pfctl: Revert "pfctl: Another set skip <group> fix"
    This reverts commit 0c156a3c32cd0d9168570da5686ddc96abcbbc5a.
    This fix broke using '<ifgroup>:network' in tables.
    
    MFC after:	1 week
    
    (cherry picked from commit d5b08e13dd6beb3436e181ff1f3e034cc8186584)

--- sbin/pfctl/pfctl_parser.c
+++ sbin/pfctl/pfctl_parser.c
@@ -1392,26 +1392,6 @@ ifa_exists(char *ifa_name)
 	return (NULL);
 }
 
-static struct node_host *
-if_lookup(char *if_name)
-{
-	struct node_host *p, *n;
-
-	for (p = iftab; p; p = p->next) {
-		if (! strcmp(if_name, p->ifname)) {
-			n = calloc(1, sizeof(struct node_host));
-			bcopy(p, n, sizeof(struct node_host));
-
-			n->next = NULL;
-			n->tail = n;
-
-			return (n);
-		}
-	}
-
-	return (NULL);
-}
-
 struct node_host *
 ifa_grouplookup(char *ifa_name, int flags)
 {
@@ -1435,7 +1415,7 @@ ifa_grouplookup(char *ifa_name, int flags)
 	for (ifg = ifgr.ifgr_groups; ifg && len >= sizeof(struct ifg_req);
 	    ifg++) {
 		len -= sizeof(struct ifg_req);
-		if ((n = if_lookup(ifg->ifgrq_member)) == NULL)
+		if ((n = ifa_lookup(ifg->ifgrq_member, flags)) == NULL)
 			continue;
 		if (h == NULL)
 			h = n;
