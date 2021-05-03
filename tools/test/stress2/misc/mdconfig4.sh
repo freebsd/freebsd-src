cff133b05ba1481e535e67b37fda6608fb52f2d0
Peter Holm <pho@FreeBSD.org>
2021-05-03 09:08:43 +0200

    stress2: Added a regression test

---
+++ tools/test/stress2/misc/mdconfig4.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+#
+# SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+#
+# Copyright (c) 2021 Peter Holm <pho@FreeBSD.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+# Test scenario from Bug 255119 by nvass@gmx.com
+
+# Fatal trap 12: page fault while in kernel mode
+# Fixed by 54f98c4dbf9b
+
+. ../default.cfg
+
+mount | grep "on $mntpoint " | grep -q /dev/md && umount -f $mntpoint
+mdconfig -l | grep -q md$mdstart &&  mdconfig -d -u $mdstart
+mdconfig -a -t swap -s 512m -u $mdstart || exit 1
+bsdlabel -w md$mdstart auto
+newfs $newfs_flags md$mdstart > /dev/null
+mount /dev/md$mdstart $mntpoint
+
+md2=$((mdstart + 1))
+cp /bin/sleep $mntpoint
+$mntpoint/sleep 60 &
+sleep .2
+mdconfig -f $mntpoint/sleep -u $md2 2>&1 |
+	grep -v 'Text file busy'
+
+kill $!
+mdconfig -l | grep -q md$md2 && mdconfig -d -u $md2
+wait
+
+umount $mntpoint
+mdconfig -d -u $mdstart
+exit 0
