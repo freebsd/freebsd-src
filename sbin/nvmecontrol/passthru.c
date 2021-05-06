76105d233d6e29976fc6b3ad3db2597c7bab7501
Warner Losh <imp@FreeBSD.org>
2021-05-06 16:05:42 -0600

    nvmecontrol: update copyright on passthru command
    I wrote this code, not Intel, so put my copyright on this. I mistakenly
    copied it for the initial commit.
    
    Sponsored by:		Netflix

--- sbin/nvmecontrol/passthru.c
+++ sbin/nvmecontrol/passthru.c
@@ -1,8 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
- * Copyright (C) 2012-2013 Intel Corporation
- * All rights reserved.
+ * Copyright (c) 2019-2021 Netflix, Inc
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
