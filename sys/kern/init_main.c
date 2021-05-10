0aa9ffcd9c3ab174aeece3e8af611e796911a5d1
Mitchell Horne <mhorne@FreeBSD.org>
2021-05-10 11:37:41 -0300

    init_main.c: sort includes
    This is preferred by style(9). Do this ahead of adding another include.
    
    Reviewed by:	imp, kevans, allanjude
    MFC after:	3 days
    Sponsored by:	NetApp, Inc.
    Sponsored by:	Klara, Inc.
    Differential Revision:	https://reviews.freebsd.org/D30186

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -52,7 +52,10 @@ __FBSDID("$FreeBSD$");
 #include "opt_verbose_sysinit.h"
 
 #include <sys/param.h>
-#include <sys/kernel.h>
+#include <sys/systm.h>
+#include <sys/conf.h>
+#include <sys/cpuset.h>
+#include <sys/dtrace_bsd.h>
 #include <sys/epoch.h>
 #include <sys/eventhandler.h>
 #include <sys/exec.h>
@@ -60,30 +63,27 @@ __FBSDID("$FreeBSD$");
 #include <sys/filedesc.h>
 #include <sys/imgact.h>
 #include <sys/jail.h>
+#include <sys/kernel.h>
 #include <sys/ktr.h>
 #include <sys/lock.h>
 #include <sys/loginclass.h>
+#include <sys/malloc.h>
 #include <sys/mount.h>
 #include <sys/mutex.h>
-#include <sys/dtrace_bsd.h>
-#include <sys/syscallsubr.h>
-#include <sys/sysctl.h>
 #include <sys/proc.h>
 #include <sys/racct.h>
-#include <sys/resourcevar.h>
-#include <sys/systm.h>
-#include <sys/signalvar.h>
-#include <sys/vnode.h>
-#include <sys/sysent.h>
 #include <sys/reboot.h>
+#include <sys/resourcevar.h>
 #include <sys/sched.h>
+#include <sys/signalvar.h>
 #include <sys/sx.h>
+#include <sys/syscallsubr.h>
+#include <sys/sysctl.h>
+#include <sys/sysent.h>
 #include <sys/sysproto.h>
-#include <sys/vmmeter.h>
 #include <sys/unistd.h>
-#include <sys/malloc.h>
-#include <sys/conf.h>
-#include <sys/cpuset.h>
+#include <sys/vmmeter.h>
+#include <sys/vnode.h>
 
 #include <machine/cpu.h>
 
