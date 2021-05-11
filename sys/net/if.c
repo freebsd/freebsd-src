5fbf51588d2011dfc650a13f7b18311e4f3d5296
John Baldwin <jhb@FreeBSD.org>
2021-05-11 14:56:23 -0700

    Remove a write-only variable.
    While refactoring an earlier series of changes during review, the
    'saved_data' variable stopped being used at the bottom of if_ioctl().
    
    Suggested by:	brooks
    Reviewed by:	brooks, imp, kib
    Fixes:		d17e0940f79f Rework compat shims in ifioctl().
    Sponsored by:	DARPA
    Differential Revision:	https://reviews.freebsd.org/D30197
    
    (cherry picked from commit ed93deba1106da2cd4755a61612c37bb67edc914)

--- sys/net/if.c
+++ sys/net/if.c
@@ -2922,7 +2922,6 @@ ifioctl(struct socket *so, u_long cmd, caddr_t data, struct thread *td)
 		struct ifgroupreq ifgr;
 		struct ifmediareq ifmr;
 	} thunk;
-	caddr_t saved_data;
 	u_long saved_cmd;
 	struct ifconf32 *ifc32;
 	struct ifdrv32 *ifd32;
@@ -2949,7 +2948,6 @@ ifioctl(struct socket *so, u_long cmd, caddr_t data, struct thread *td)
 
 #ifdef COMPAT_FREEBSD32
 	saved_cmd = cmd;
-	saved_data = data;
 	switch (cmd) {
 	case SIOCGIFCONF32:
 		ifc32 = (struct ifconf32 *)data;
