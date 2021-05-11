freebsd/freebsd-src

commit 1ce9da3d9f2bd99674314d9fd6cdd84f7077949b

bhyvectl: print a better error message when vm_open() fails
Use errno to print a more descriptive error message when vm_open() fails

libvmm: preserve errno when vm_device_open() fails

vm_destroy() squashes errno by making a dive into sysctlbyname() - we
can safely skip vm_destroy() here since it's not doing any critical
clean up at this point. Replace vm_destroy() with a free() call.

PR:             250671
MFC after:      3 days
Submitted by:   marko@apache.org
Reviewed by:	grehan
Differential Revision:	https://reviews.freebsd.org/D29109

lib/libvmmapi/vmmapi.c
@@ -133,7 +133,7 @@ vm_open(const char *name)
 
 	return (vm);
 err:
-	vm_destroy(vm);
+	free(vm);
 	return (NULL);
 }
 

usr.sbin/bhyvectl/bhyvectl.c
@@ -1950,7 +1950,9 @@ main(int argc, char *argv[])
 	if (!error) {
 		ctx = vm_open(vmname);
 		if (ctx == NULL) {
-			printf("VM:%s is not created.\n", vmname);
+			fprintf(stderr,
+			    "vm_open: %s could not be opened: %s\n",
+			    vmname, strerror(errno));
 			exit (1);
 		}
 	}

commit 31a8ed5210f7f364a346fd9748683eb014caaf0d

libvmm: explicitly save and restore errno in vm_open()
In commit 1ce9da3d9f2bd996, vm_destroy() was replaced with free() to
preserve errno. However, it's possible that free() may change the errno
as well. Keep the free() call, but explicitly save and restore errno.

Noted by: jhb
Fixes: 1ce9da3d9f2bd996

lib/libvmmapi/vmmapi.c
@@ -118,6 +118,7 @@ struct vmctx *
 vm_open(const char *name)
 {
 	struct vmctx *vm;
+	int saved_errno;
 
 	vm = malloc(sizeof(struct vmctx) + strlen(name) + 1);
 	assert(vm != NULL);
@@ -133,7 +134,9 @@ vm_open(const char *name)
 
 	return (vm);
 err:
+	saved_errno = errno;
 	free(vm);
+	errno = saved_errno;
 	return (NULL);
 }
 