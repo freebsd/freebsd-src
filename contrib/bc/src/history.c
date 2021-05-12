aefd0abf61bc8735a0a1452a1be646eb9e9918c6
Stefan Eßer <se@FreeBSD.org>
2021-05-12 07:22:37 +0200

    Revert "Vendor import of Gavin D. Howard's bc version 4.0.2"
    The update had been performed on a check-out of the vendor branch,
    but the final push lacked the target designation vendor/bc.

--- contrib/bc/src/history.c
+++ contrib/bc/src/history.c
@@ -1281,11 +1281,8 @@ static BcStatus bc_history_edit(BcHistory *h, const char *prompt) {
 
 			default:
 			{
-				if (c >= BC_ACTION_CTRL_A && c <= BC_ACTION_CTRL_Z) {
+				if (c >= BC_ACTION_CTRL_A && c <= BC_ACTION_CTRL_Z)
 					bc_history_printCtrl(h, c);
-					if (c == BC_ACTION_CTRL_Z) raise(SIGTSTP);
-					if (c == BC_ACTION_CTRL_S) raise(SIGSTOP);
-				}
 				else bc_history_edit_insert(h, cbuf, nread);
 				break;
 			}
