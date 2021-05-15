3c7045ae73d903fdb132a672122a03707ce2ca30
Lv Yunlong <lyl2019@mail.ustc.edu.cn>
2021-05-14 23:06:24 -0700

    ipfilter/netinent: Fix a use after free in ipf_nat_rule_deref
    Bug File: contrib/ipfilter/netinet/ip_nat.c
    
    In function ipf_nat_rule_deref, if (n->in_tqehead[0] != NULL) is true,
    n->in_tqehead[1] will be freed in ipf_freetimeoutqueue() via KFREE().
    But the freed pointer n->in_tqehead[1] is still used in later
    ipf_deletetimeoutqueue(n->in_tqehead[1]), which is a use after free bug.
    
    According the around code pattern, i think this bug is caused by mistyping.
    My patch correct the index number of n->in_tqehead, if (n->in_tqehead[0] != NULL) is true.
    
    Signed-off-by: Lv Yunlong <lyl2019@mail.ustc.edu.cn>

--- sys/contrib/ipfilter/netinet/ip_nat.c
+++ sys/contrib/ipfilter/netinet/ip_nat.c
@@ -6245,7 +6245,7 @@ ipf_nat_rule_deref(softc, inp)
 
 	if (n->in_tqehead[0] != NULL) {
 		if (ipf_deletetimeoutqueue(n->in_tqehead[0]) == 0) {
-			ipf_freetimeoutqueue(softc, n->in_tqehead[1]);
+			ipf_freetimeoutqueue(softc, n->in_tqehead[0]);
 		}
 	}
 
