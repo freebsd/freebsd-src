ae23d302479c5276e4e0a36e6ba1e882dc6e9899
Kristof Provost <kp@FreeBSD.org>
2021-05-18 09:24:50 +0200

    pf: Move nvlist conversion functions to pf_nv
    Separate the conversion functions (between kernel structs and nvlists)
    to pf_nv. This reduces the size of pf_ioctl.c, which is already quite
    large and complex, a good bit. It also keeps all the fairly
    straightforward conversion code together.
    
    MFC after:	1 week
    Sponsored by:	Rubicon Communications, LLC ("Netgate")
    Differential Revision:	https://reviews.freebsd.org/D30359
    
    (cherry picked from commit 3032c353882aa70a461ef251b8aa6c59a0829ec5)

--- sys/netpfil/pf/pf_nv.h
+++ sys/netpfil/pf/pf_nv.h
@@ -28,8 +28,33 @@
 #ifndef _PF_NV_H_
 #define _PF_NV_H_
 
-#include <sys/types.h>
 #include <sys/nv.h>
+#include <sys/sdt.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <net/if.h>
+#include <net/if_var.h>
+#include <net/pfvar.h>
+
+SDT_PROVIDER_DECLARE(pf);
+SDT_PROBE_DECLARE(pf, ioctl, function, error);
+SDT_PROBE_DECLARE(pf, ioctl, nvchk, error);
+
+#define	ERROUT_FUNCTION(target, x)					\
+	do {								\
+		error = (x);						\
+		SDT_PROBE3(pf, ioctl, function, error, __func__, error,	\
+		    __LINE__);						\
+		goto target;						\
+	} while (0)
+
+#define	PFNV_CHK(x)	do {	\
+	error = (x);		\
+	SDT_PROBE2(pf, ioctl, nvchk, error, error, __LINE__);	\
+	if (error != 0)		\
+		goto errout;	\
+	} while (0)
 
 int	pf_nvbinary(const nvlist_t *, const char *, void *, size_t);
 int	pf_nvint(const nvlist_t *, const char *, int *);
@@ -56,11 +81,14 @@ void	pf_uint64_array_nv(nvlist_t *, const char *, const uint64_t *,
 
 int	pf_nvstring(const nvlist_t *, const char *, char *, size_t);
 
-#define	PFNV_CHK(x)	do {	\
-	error = (x);		\
-	SDT_PROBE2(pf, ioctl, nvchk, error, error, __LINE__);	\
-	if (error != 0)		\
-		goto errout;	\
-	} while (0)
+/* Translation functions */
+
+int		 pf_check_rule_addr(const struct pf_rule_addr *);
+
+nvlist_t	*pf_krule_to_nvrule(const struct pf_krule *);
+int		 pf_nvrule_to_krule(const nvlist_t *, struct pf_krule *);
+int		 pf_nvstate_kill_to_kstate_kill(const nvlist_t *,
+		    struct pf_kstate_kill *);
+nvlist_t	*pf_state_to_nvstate(const struct pf_state *);
 
 #endif
