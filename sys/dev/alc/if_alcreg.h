77b637338a3656d4ccedb9798a3f98ac283962f4
Konstantin Belousov <kib@FreeBSD.org>
2021-05-20 01:14:18 +0300

    alc(4): add support for Mikrotik 10/25G NIC
    The new Mikrotik 10/25G NIC is mostly compatible with AR8151 hardware,
    with few exceptions:
    
    * card supports only 32bit DMA operations
    * card does not support write-one-to-clear semantics for interrupt status
      register
    * MDIO operations can take longer to complete
    
    This patch adds support for Mikrotik 10/25G NIC to the alc driver
    while maintaining support for all earlier HW.
    
    The patch was tested with FreeBSD main branch as of commit
    f4b38c360e63a6e66245efedbd6c070f9c0aee55
    
    This was tested on Intel i7-4790K system with Mikrotik 10/25G NIC.
    This was tested on Intel i7-4790K system with RB44Ge (AR8151 based 4-port NIC)
    to verify backwards compatibility.
    
    PR:	256000
    Submitted by:	 Gatis Peisenieks  <gatis@mikrotik.com>
    MFC after:	1 week

--- sys/dev/alc/if_alcreg.h
+++ sys/dev/alc/if_alcreg.h
@@ -1121,6 +1121,14 @@
 #define	MII_EXT_ANEG_NLP78		0x8027
 #define	ANEG_NLP78_120M_DEFAULT		0x8A05
 
+#define ALC_MT_MAGIC			0x1F00
+#define ALC_MT_MODE			0x1F04
+#define ALC_MT_SPEED			0x1F08
+#define ALC_MT_VERSION			0x1F0C
+
+#define MT_MAGIC			0xaabb1234
+#define MT_MODE_4Q			BIT(0)
+
 /* Statistics counters collected by the MAC. */
 struct smb {
 	/* Rx stats. */
