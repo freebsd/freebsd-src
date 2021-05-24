d1ed08d12af319a99f56903619ea690421fe1340
Bjoern A. Zeeb <bz@FreeBSD.org>
2021-05-24 18:26:41 +0000

    LinuxKPI: change BUILD_BUG_ON()
    BUILD_BUG_ON() can be used inside functions where the definition to
    CTASSERT() (_Static_assert()) seems to not work.
    Go back to an old-style CTASSERT() implementation but also add a
    variable dclaration to avoid "unsued typedef" errors and dummy-use
    the variable to avoid "unusued variable" errors.  Given it is all
    self-contained in a block and not used outside this should be
    optimised away.
    
    Sponsored by:	The FreeBSD Foundation
    MFC after:	2 weeks
    Reviewed by:	hselasky
    Differential Revision: https://reviews.freebsd.org/D30431

--- sys/compat/linuxkpi/common/include/linux/kernel.h
+++ sys/compat/linuxkpi/common/include/linux/kernel.h
@@ -89,8 +89,23 @@
 #define	S64_C(x) x ## LL
 #define	U64_C(x) x ## ULL
 
+/*
+ * BUILD_BUG_ON() can happen inside functions where _Static_assert() does not
+ * seem to work.  Use old-schoold-ish CTASSERT from before commit
+ * a3085588a88fa58eb5b1eaae471999e1995a29cf but also make sure we do not
+ * end up with an unused typedef or variable. The compiler should optimise
+ * it away entirely.
+ */
+#define	_O_CTASSERT(x)		_O__CTASSERT(x, __LINE__)
+#define	_O__CTASSERT(x, y)	_O___CTASSERT(x, y)
+#define	_O___CTASSERT(x, y)	while (0) { \
+    typedef char __assert_line_ ## y[(x) ? 1 : -1]; \
+    __assert_line_ ## y _x; \
+    _x[0] = '\0'; \
+}
+
 #define	BUILD_BUG()			do { CTASSERT(0); } while (0)
-#define	BUILD_BUG_ON(x)			CTASSERT(!(x))
+#define	BUILD_BUG_ON(x)			_O_CTASSERT(!(x))
 #define	BUILD_BUG_ON_MSG(x, msg)	BUILD_BUG_ON(x)
 #define	BUILD_BUG_ON_NOT_POWER_OF_2(x)	BUILD_BUG_ON(!powerof2(x))
 #define	BUILD_BUG_ON_INVALID(expr)	while (0) { (void)(expr); }
