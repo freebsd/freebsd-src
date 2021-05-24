c1661d59e68e29a817c870f421a6871563039dbb
Bjoern A. Zeeb <bz@FreeBSD.org>
2021-05-24 18:32:11 +0000

    LinuxKPI: add LINUXKPI_PARAM_charp()
    Add yet another version of the various module_param_named() use cases.
    This one deals with "charp".
    
    Sponsored by:	The FreeBSD Foundation
    MFC after:	2 weeks
    Reviewed by:	hselasky
    Differential Revision: https://reviews.freebsd.org/D30433

--- sys/compat/linuxkpi/common/include/linux/moduleparam.h
+++ sys/compat/linuxkpi/common/include/linux/moduleparam.h
@@ -102,6 +102,12 @@
 	LINUXKPI_PARAM_NAME(name), LINUXKPI_PARAM_PERM(perm), &(var), 0, \
 	LINUXKPI_PARAM_DESC(name)))
 
+#define	LINUXKPI_PARAM_charp(name, var, perm)				\
+	extern const char LINUXKPI_PARAM_DESC(name)[];			\
+	LINUXKPI_PARAM_PASS(SYSCTL_STRING(LINUXKPI_PARAM_PARENT, OID_AUTO, \
+	LINUXKPI_PARAM_NAME(name), LINUXKPI_PARAM_PERM(perm), &(var), 0, \
+	LINUXKPI_PARAM_DESC(name)))
+
 #define	module_param_string(name, str, len, perm)			\
 	extern const char LINUXKPI_PARAM_DESC(name)[];			\
 	LINUXKPI_PARAM_PASS(SYSCTL_STRING(LINUXKPI_PARAM_PARENT, OID_AUTO, \
