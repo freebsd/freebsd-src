9b3c89bdc8b73b3364a9cf556e1fb93f273e02cf
Bjoern A. Zeeb <bz@FreeBSD.org>
2021-05-24 18:47:49 +0000

    LinuxKPI: add kernel_ulong_t typedef in linux/kernel.h.
    Sponsored by:	The FreeBSD Foundation
    Reviewed by:	hselasky
    Differential Revision: https://reviews.freebsd.org/D30438
    
    (cherry picked from commit 5878c7c7b090c8579316ae4479e962684d49a499)

--- sys/compat/linuxkpi/common/include/linux/types.h
+++ sys/compat/linuxkpi/common/include/linux/types.h
@@ -72,6 +72,7 @@ typedef unsigned __poll_t;
 typedef uint64_t phys_addr_t;
 
 typedef size_t __kernel_size_t;
+typedef	unsigned long	kernel_ulong_t;
 
 #define	DECLARE_BITMAP(n, bits)						\
 	unsigned long n[howmany(bits, sizeof(long) * 8)]
