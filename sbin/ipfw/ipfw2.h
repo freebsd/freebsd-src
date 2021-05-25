34055dacee7fb5c3ab38f226b1930ee32e056551
Kristof Provost <kp@FreeBSD.org>
2021-05-25 16:54:32 +0200

    ipfw: Introduce dnctl
    Introduce a link to the ipfw command, dnctl, for dummynet configuration.
    dnctl only handles dummynet configuration, and is part of the effort to
    support dummynet in pf.
    
    /sbin/ipfw continues to accept pipe, queue and sched commands, but these can
    now also be issued via the new dnctl command.
    
    Reviewed by:	donner
    MFC after:	2 weeks
    Sponsored by:	Rubicon Communications, LLC ("Netgate")
    Differential Revision:	https://reviews.freebsd.org/D30465
    
    (cherry picked from commit 0b95680e077b7ef5bc6930c7c0f1a41106251d5d)

--- sbin/ipfw/ipfw2.h
+++ sbin/ipfw/ipfw2.h
@@ -20,6 +20,11 @@
  * $FreeBSD$
  */
 
+enum cmdline_prog {
+	cmdline_prog_ipfw,
+	cmdline_prog_dnctl
+};
+
 /*
  * Options that can be set on the command line.
  * When reading commands from a file, a subset of the options can also
@@ -54,7 +59,10 @@ struct cmdline_opts {
 	uint32_t use_set;	/* work with specified set number */
 		/* 0 means all sets, otherwise apply to set use_set - 1 */
 
+	enum cmdline_prog	prog;	/* Are we ipfw or dnctl? */
 };
+
+int is_ipfw(void);
 
 enum {
 	TIMESTAMP_NONE = 0,
