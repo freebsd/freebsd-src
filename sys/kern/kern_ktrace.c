ff5366235d54f8c6d62908273b7d107b312d6099
Mark Johnston <markj@FreeBSD.org>
2021-05-27 15:49:12 -0400

    ktrace: Handle negative array sizes in ktrstructarray
    ktrstructarray() may be used to create copies of kevent(2) change and
    event arrays.  It is called before parameter validation is done and so
    should check for bogus array lengths before allocating a copy.
    
    Reported by:	syzkaller
    Reviewed by:	kib
    MFC after:	1 week
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D30479

--- sys/kern/kern_ktrace.c
+++ sys/kern/kern_ktrace.c
@@ -878,6 +878,8 @@ ktrstructarray(const char *name, enum uio_seg seg, const void *data,
 
 	if (__predict_false(curthread->td_pflags & TDP_INKTRACE))
 		return;
+	if (num_items < 0)
+		return;
 
 	/* Trim array length to genio size. */
 	max_items = ktr_geniosize / struct_size;
