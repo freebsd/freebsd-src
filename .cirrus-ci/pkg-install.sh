77013d11e6483b970af25e13c9b892075742f7e5
Ed Maste <emaste@FreeBSD.org>
2021-06-02 13:08:29 -0400

    Cirrus-CI: add some timing info on pkg install failure
    Sponsored by:	The FreeBSD Foundation

--- .cirrus-ci/pkg-install.sh
+++ .cirrus-ci/pkg-install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
 
+start_time=$(date +%s)
 pkg install -y "$@" && exit 0
 
 cat <<EOF
-pkg install failed
+pkg install failed after $(($(date +%s) - $start_time))s
 
 dmesg tail:
 $(dmesg | tail)
@@ -12,4 +13,10 @@ $(dmesg | tail)
 trying again
 EOF
 
-pkg install -y "$@"
+start_time=$(date +%s)
+pkg install -y "$@" && exit 0
+
+cat <<EOF
+second pkg install failed after $(($(date +%s) - $start_time))s
+EOF
+exit 1
