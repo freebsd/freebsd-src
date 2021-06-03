ef65f7bdf26e2f69e13d54e1ffd671de7d9032f6
Warner Losh <imp@FreeBSD.org>
2021-06-03 11:26:57 -0600

    mmc:: Undo my conversion of (bool) to !!.
    The need for !! over (bool) pre-dates gcc 4.2, so go with the patch
    as-submitted because the kernel tends to prefer that.
    
    Suggested by:		emaste@
    Sponsored by:		Netflix

--- sys/dev/mmc/mmc_fdt_helpers.c
+++ sys/dev/mmc/mmc_fdt_helpers.c
@@ -407,7 +407,7 @@ mmc_fdt_gpio_get_present(struct mmc_fdt_helper *helper)
 
 	gpio_pin_is_active(helper->cd_pin, &pinstate);
 
-	return (pinstate ^ !!(helper->props & MMC_PROP_CD_INVERTED));
+	return (pinstate ^ (bool)(helper->props & MMC_PROP_CD_INVERTED));
 }
 
 bool
@@ -423,7 +423,7 @@ mmc_fdt_gpio_get_readonly(struct mmc_fdt_helper *helper)
 
 	gpio_pin_is_active(helper->wp_pin, &pinstate);
 
-	return (pinstate ^ !!(helper->props & MMC_PROP_WP_INVERTED));
+	return (pinstate ^ (bool)(helper->props & MMC_PROP_WP_INVERTED));
 }
 
 void
