85f46f9d7f901845be11e2cfeffc6e5d5f55ff3b
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-03 18:48:08 -0400

    Fix compilation issues

--- lib/libc/string/strspn.c
+++ lib/libc/string/strspn.c
@@ -41,7 +41,6 @@ strspn(const char *s, const char *charset)
 {
 	const char *s1;
 	u_long tbl[(UCHAR_MAX + 1) / LONG_BIT];
-	unsigned int idx;
 
 	if(*s == '\0')
 		return (0);
@@ -49,7 +48,7 @@ strspn(const char *s, const char *charset)
 #if LONG_BIT == 64	/* always better to unroll on 64-bit architectures */
 	tbl[3] = tbl[2] = tbl[1] = tbl[0] = 0;
 #else
-	for (idx = 0; idx < sizeof(tbl) / sizeof(tbl[0]); idx++)
+	for (unsigned int idx = 0; idx < sizeof(tbl) / sizeof(tbl[0]); idx++)
 		tbl[idx] = 0;
 #endif
 	for (; *charset != '\0'; charset++) {
