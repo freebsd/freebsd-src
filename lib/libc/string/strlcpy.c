63ec824a5a2828160cbd1c8fe96417e87469d635
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-04 18:12:21 -0400

    Swapped role of dsize and nleft. Renamed nleft.

--- lib/libc/string/strlcpy.c
+++ lib/libc/string/strlcpy.c
@@ -30,20 +30,20 @@ __FBSDID("$FreeBSD$");
 size_t
 strlcpy(char * __restrict dst, const char * __restrict src, size_t dsize)
 {
-	const char *osrc = src;
-	size_t nleft = dsize;
+	const char * const osrc = src;
+	const size_t odsize = dsize;
 
 	/* Copy as many bytes as will fit. */
-	if (nleft != 0) {
-		while (--nleft != 0) {
+	if (dsize != 0) {
+		while (--dsize != 0) {
 			if ((*dst++ = *src++) == '\0')
 				break;
 		}
 	}
 
 	/* Not enough room in dst, add NUL and traverse rest of src. */
-	if (nleft == 0) {
-		if (dsize != 0)
+	if (dsize == 0) {
+		if (odsize != 0)
 			*dst = '\0';		/* NUL-terminate dst */
 		while (*src++)
 			;
