6c4ae7c3fcd05c542e76c315f5c21143e197d1bd
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-04 18:38:58 -0400

    Added a new variable to memchr to make code clearer

--- lib/libc/string/memchr.c
+++ lib/libc/string/memchr.c
@@ -39,21 +39,22 @@ void *
 memchr(const void *src, int c, size_t n)
 {
 	const unsigned char *s = src;
-	c = (unsigned char)c;
+	const unsigned char uc = (unsigned char)c;
+
 #ifdef __GNUC__
-	for (; ((uintptr_t)s & ALIGN) && n && *s != c; s++, n--)
+	for (; ((uintptr_t)s & ALIGN) && n && *s != uc; s++, n--)
 		;
-	if (n && *s != c) {
+	if (n && *s != uc) {
 		typedef size_t __attribute__((__may_alias__)) word;
 		const word *w;
-		size_t k = ONES * c;
+		size_t k = ONES * uc;
 		for (w = (const void *)s; n >= SS && !HASZERO(*w ^ k);
 		     w++, n -= SS)
 			;
 		s = (const void *)w;
 	}
 #endif
-	for (; n && *s != c; s++, n--)
+	for (; n && *s != uc; s++, n--)
 		;
 	return n ? (void *)s : 0;
 }
