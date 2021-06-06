8b2ba742cc2c732bc4bc1d43f8256adce06657d0
Mathy Vanhoef <Mathy.Vanhoef@kuleuven.be>
2021-06-06 22:10:56 +0000

    net80211: prevent plaintext injection by A-MSDU RFC1042/EAPOL frames
    No longer accept plaintext A-MSDU frames that start with an RFC1042
    header with EtherType EAPOL.  This is done by only accepting EAPOL
    packets that are included in non-aggregated 802.11 frames.
    
    Note that before this patch, FreeBSD also only accepted EAPOL frames
    that are sent in a non-aggregated 802.11 frame due to bugs in
    processing EAPOL packets inside A-MSDUs. In other words,
    compatibility with legitimate devices remains the same.
    
    This relates to section 6.5 in the 2021 Usenix "FragAttacks" (Fragment
    and Forge: Breaking Wi-Fi Through Frame Aggregation and Fragmentation)
    paper.
    
    Submitted by:	Mathy Vanhoef (Mathy.Vanhoef kuleuven.be)
    Security:	CVE-2020-26144
    PR:		256120
    
    (cherry picked from commit ffc19cf52da5546973965f78cf32aa0f2c9657f8)

--- sys/net80211/ieee80211_hostap.c
+++ sys/net80211/ieee80211_hostap.c
@@ -757,7 +757,10 @@ hostap_input(struct ieee80211_node *ni, struct mbuf *m,
 			IEEE80211_NODE_STAT(ni, rx_decap);
 			goto err;
 		}
-		eh = mtod(m, struct ether_header *);
+		if (!(qos & IEEE80211_QOS_AMSDU))
+			eh = mtod(m, struct ether_header *);
+		else
+			eh = NULL;
 		if (!ieee80211_node_is_authorized(ni)) {
 			/*
 			 * Deny any non-PAE frames received prior to
@@ -767,11 +770,13 @@ hostap_input(struct ieee80211_node *ni, struct mbuf *m,
 			 * the port is not marked authorized by the
 			 * authenticator until the handshake has completed.
 			 */
-			if (eh->ether_type != htons(ETHERTYPE_PAE)) {
+			if (eh == NULL ||
+			    eh->ether_type != htons(ETHERTYPE_PAE)) {
 				IEEE80211_DISCARD_MAC(vap, IEEE80211_MSG_INPUT,
-				    eh->ether_shost, "data",
-				    "unauthorized port: ether type 0x%x len %u",
-				    eh->ether_type, m->m_pkthdr.len);
+				    ni->ni_macaddr, "data", "unauthorized or "
+				    "unknown port: ether type 0x%x len %u",
+				    eh == NULL ? -1 : eh->ether_type,
+				    m->m_pkthdr.len);
 				vap->iv_stats.is_rx_unauth++;
 				IEEE80211_NODE_STAT(ni, rx_unauth);
 				goto err;
@@ -784,7 +789,8 @@ hostap_input(struct ieee80211_node *ni, struct mbuf *m,
 			if ((vap->iv_flags & IEEE80211_F_DROPUNENC) &&
 			    ((has_decrypted == 0) && (m->m_flags & M_WEP) == 0) &&
 			    (is_hw_decrypted == 0) &&
-			    eh->ether_type != htons(ETHERTYPE_PAE)) {
+			    (eh == NULL ||
+			     eh->ether_type != htons(ETHERTYPE_PAE))) {
 				/*
 				 * Drop unencrypted frames.
 				 */
