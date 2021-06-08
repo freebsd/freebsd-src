56161ea24caa8459012dfb1fbd906a02a8f1b7e0
Dmitry Chagin <dchagin@FreeBSD.org>
2021-06-08 08:18:00 +0300

    Fix copyright, remove "all rights reserved".
    The eventfd code was written by me, rdivacky@ copyrigth applicable only
    to epoll part of the Linuxulator code. Roman is ok to retire his copyright
    from sys/kern/sys_eventfd.c and 'All rights reserved.' lines from
    sys/compat/linux/linux_event.[c|h] and sys/kern/sys_eventfd.c files.
    
    Reviewed by:		kib, emaste
    Approved by:		rdivacky
    Differential Revision:	https://reviews.freebsd.org/D30677
    MFC after:		2 weeks
    
    (cherry picked from commit f570a6723e145ae585bfb402f3f42260687666d3)

--- sys/kern/sys_eventfd.c
+++ sys/kern/sys_eventfd.c
@@ -1,9 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
- * Copyright (c) 2007 Roman Divacky
  * Copyright (c) 2014 Dmitry Chagin
- * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
