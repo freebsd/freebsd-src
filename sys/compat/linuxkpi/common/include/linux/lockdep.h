8a1a42b2a7a428fb97fda9f19fd0d67a4eec7535
Neel Chauhan <nc@FreeBSD.org>
2021-06-09 14:38:52 -0700

    linuxkpi: Add macros for might_lock_nested() and lockdep_(re/un/)pin_lock()
    In Linux, these are macros to locks in the kernel for scheduling purposes.
    But as with other macros in this header, we aren't doing anything with them
    so we are doing `do {} while (0)` for now.
    
    This is needed by the drm-kmod 5.7 update.
    
    Approved by:		hselasky (src)
    MFC after:		3 days
    Differential Revision:	https://reviews.freebsd.org/D30710

--- sys/compat/linuxkpi/common/include/linux/lockdep.h
+++ sys/compat/linuxkpi/common/include/linux/lockdep.h
@@ -79,6 +79,7 @@ lockdep_is_held(void *__m)
 
 #define	might_lock(m)	do { } while (0)
 #define	might_lock_read(m) do { } while (0)
+#define	might_lock_nested(m, n) do { } while (0)
 
 #define	lock_acquire(...) do { } while (0)
 #define	lock_release(...) do { } while (0)
@@ -90,5 +91,9 @@ lockdep_is_held(void *__m)
 #define	lock_map_acquire(_map) do { } while (0)
 #define	lock_map_acquire_read(_map) do { } while (0)
 #define	lock_map_release(_map) do { } while (0)
+
+#define	lockdep_pin_lock(l) do { } while (0)
+#define	lockdep_repin_lock(l,c) do { } while (0)
+#define	lockdep_unpin_lock(l,c) do { } while (0)
 
 #endif /* _LINUX_LOCKDEP_H_ */
