4318e5d919473b0abf62feb0c81ce0a61f0efed9
Greg V <greg@unrelenting.technology>
2021-06-09 19:09:16 -0400

    LinuxKPI: add fault_flag_allow_retry_first
    Used by drm 5.7.
    
    Reviewed by:	bz, hselasky, nc
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D30673

--- sys/compat/linuxkpi/common/include/linux/mm.h
+++ sys/compat/linuxkpi/common/include/linux/mm.h
@@ -92,6 +92,9 @@ CTASSERT((VM_PROT_ALL & -(1 << 8)) == 0);
 #define	FAULT_FLAG_REMOTE	(1 << 7)
 #define	FAULT_FLAG_INSTRUCTION	(1 << 8)
 
+#define fault_flag_allow_retry_first(flags) \
+	(((flags) & (FAULT_FLAG_ALLOW_RETRY | FAULT_FLAG_TRIED)) == FAULT_FLAG_ALLOW_RETRY)
+
 typedef int (*pte_fn_t)(linux_pte_t *, pgtable_t, unsigned long addr, void *data);
 
 struct vm_area_struct {
