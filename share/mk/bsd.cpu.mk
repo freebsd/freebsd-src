445aa3bcc50ca2d924a7d5e9974aa7fc8d3c49cb
Jung-uk Kim <jkim@FreeBSD.org>
2021-06-14 19:06:03 -0400

    bsd.cpu.mk: Correct MACHINE_CPU for Intel Alder Lake
    Apparently Intel Alder Lake lacks AVX-512 instructions.
    
    Fixes:		09e7341c5ea4 "Catch up with Clang 12"
    (cherry picked from commit e80145525b54a90672d36c9a168d83d0c7cb8161)

--- share/mk/bsd.cpu.mk
+++ share/mk/bsd.cpu.mk
@@ -189,14 +189,14 @@ MACHINE_CPU = 3dnow mmx k6 k5 i586
 MACHINE_CPU = mmx k6 k5 i586
 .  elif ${CPUTYPE} == "k5"
 MACHINE_CPU = k5 i586
-.  elif ${CPUTYPE} == "alderlake" || ${CPUTYPE} == "sapphirerapids" || \
-    ${CPUTYPE} == "tigerlake" || ${CPUTYPE} == "cooperlake" || \
-    ${CPUTYPE} == "cascadelake" || ${CPUTYPE} == "icelake-server" || \
-    ${CPUTYPE} == "icelake-client" || ${CPUTYPE} == "cannonlake" || \
-    ${CPUTYPE} == "knm" || ${CPUTYPE} == "skylake-avx512" || ${CPUTYPE} == "knl"
+.  elif ${CPUTYPE} == "sapphirerapids" || ${CPUTYPE} == "tigerlake" || \
+    ${CPUTYPE} == "cooperlake" || ${CPUTYPE} == "cascadelake" || \
+    ${CPUTYPE} == "icelake-server" || ${CPUTYPE} == "icelake-client" || \
+    ${CPUTYPE} == "cannonlake" || ${CPUTYPE} == "knm" || \
+    ${CPUTYPE} == "skylake-avx512" || ${CPUTYPE} == "knl"
 MACHINE_CPU = avx512 avx2 avx sse42 sse41 ssse3 sse3 sse2 sse i686 mmx i586
-.  elif ${CPUTYPE} == "skylake" || ${CPUTYPE} == "broadwell" || \
-    ${CPUTYPE} == "haswell"
+.  elif ${CPUTYPE} == "alderlake" || ${CPUTYPE} == "skylake" || \
+    ${CPUTYPE} == "broadwell" || ${CPUTYPE} == "haswell"
 MACHINE_CPU = avx2 avx sse42 sse41 ssse3 sse3 sse2 sse i686 mmx i586
 .  elif ${CPUTYPE} == "ivybridge" || ${CPUTYPE} == "sandybridge"
 MACHINE_CPU = avx sse42 sse41 ssse3 sse3 sse2 sse i686 mmx i586
@@ -257,14 +257,14 @@ MACHINE_CPU = k8 3dnow sse3
 .  elif ${CPUTYPE} == "opteron" || ${CPUTYPE} == "athlon64" || \
     ${CPUTYPE} == "athlon-fx" || ${CPUTYPE} == "k8"
 MACHINE_CPU = k8 3dnow
-.  elif ${CPUTYPE} == "alderlake" || ${CPUTYPE} == "sapphirerapids" || \
-    ${CPUTYPE} == "tigerlake" || ${CPUTYPE} == "cooperlake" || \
-    ${CPUTYPE} == "cascadelake" || ${CPUTYPE} == "icelake-server" || \
-    ${CPUTYPE} == "icelake-client" || ${CPUTYPE} == "cannonlake" || \
-    ${CPUTYPE} == "knm" || ${CPUTYPE} == "skylake-avx512" || ${CPUTYPE} == "knl"
+.  elif ${CPUTYPE} == "sapphirerapids" || ${CPUTYPE} == "tigerlake" || \
+    ${CPUTYPE} == "cooperlake" || ${CPUTYPE} == "cascadelake" || \
+    ${CPUTYPE} == "icelake-server" || ${CPUTYPE} == "icelake-client" || \
+    ${CPUTYPE} == "cannonlake" || ${CPUTYPE} == "knm" || \
+    ${CPUTYPE} == "skylake-avx512" || ${CPUTYPE} == "knl"
 MACHINE_CPU = avx512 avx2 avx sse42 sse41 ssse3 sse3
-.  elif ${CPUTYPE} == "skylake" || ${CPUTYPE} == "broadwell" || \
-    ${CPUTYPE} == "haswell"
+.  elif ${CPUTYPE} == "alderlake" || ${CPUTYPE} == "skylake" || \
+    ${CPUTYPE} == "broadwell" || ${CPUTYPE} == "haswell"
 MACHINE_CPU = avx2 avx sse42 sse41 ssse3 sse3
 .  elif ${CPUTYPE} == "ivybridge" || ${CPUTYPE} == "sandybridge"
 MACHINE_CPU = avx sse42 sse41 ssse3 sse3
