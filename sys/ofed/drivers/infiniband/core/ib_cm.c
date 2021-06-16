315627b7191dd4fe30a9293609feaf7eeb62e478
Hans Petter Selasky <hselasky@FreeBSD.org>
2021-06-16 15:01:33 +0200

    ibcore: Remove unused and erroneous msg sequence encoding.
    In cm_form_tid(), a two bit message sequence number is OR'ed into bit
    31-30 of the lower TID value.
    
    After Linux commit f06d26537559 ("IB/cm: Randomize starting comm ID"), the
    local_id is XOR'ed with a 32-bit random value. Hence, bit 31-30 in the
    lower TID now has an arbitrarily value and it makes no sense to OR in
    the message sequence number.
    
    Adding to that, the evolution in use of IDR routines in cm_alloc_id()
    has always had the possibility of returning a value with bit 30 set.
    
    In addition, said bits are never checked.
    
    Hence, remove the encoding and the corresponding enum.
    
    Linux commit:
    87a37ce9e400e40daee537ff95343e3c94743c6d
    
    MFC after:	1 week
    Reviewed by:	kib
    Sponsored by:	Mellanox Technologies // NVIDIA Networking

--- sys/ofed/drivers/infiniband/core/ib_cm.c
+++ sys/ofed/drivers/infiniband/core/ib_cm.c
@@ -1130,14 +1130,12 @@ new_id:
 }
 EXPORT_SYMBOL(ib_cm_insert_listen);
 
-static __be64 cm_form_tid(struct cm_id_private *cm_id_priv,
-			  enum cm_msg_sequence msg_seq)
+static __be64 cm_form_tid(struct cm_id_private *cm_id_priv)
 {
 	u64 hi_tid, low_tid;
 
 	hi_tid   = ((u64) cm_id_priv->av.port->mad_agent->hi_tid) << 32;
-	low_tid  = (u64) ((__force u32)cm_id_priv->id.local_id |
-			  (msg_seq << 30));
+	low_tid  = (u64)cm_id_priv->id.local_id;
 	return cpu_to_be64(hi_tid | low_tid);
 }
 
@@ -1160,7 +1158,7 @@ static void cm_format_req(struct cm_req_msg *req_msg,
 	struct ib_sa_path_rec *alt_path = param->alternate_path;
 
 	cm_format_mad_hdr(&req_msg->hdr, CM_REQ_ATTR_ID,
-			  cm_form_tid(cm_id_priv, CM_MSG_SEQUENCE_REQ));
+			  cm_form_tid(cm_id_priv));
 
 	req_msg->local_comm_id = cm_id_priv->id.local_id;
 	req_msg->service_id = param->service_id;
@@ -2171,7 +2169,7 @@ static void cm_format_dreq(struct cm_dreq_msg *dreq_msg,
 			  u8 private_data_len)
 {
 	cm_format_mad_hdr(&dreq_msg->hdr, CM_DREQ_ATTR_ID,
-			  cm_form_tid(cm_id_priv, CM_MSG_SEQUENCE_DREQ));
+			  cm_form_tid(cm_id_priv));
 	dreq_msg->local_comm_id = cm_id_priv->id.local_id;
 	dreq_msg->remote_comm_id = cm_id_priv->id.remote_id;
 	cm_dreq_set_remote_qpn(dreq_msg, cm_id_priv->remote_qpn);
@@ -2775,7 +2773,7 @@ static void cm_format_lap(struct cm_lap_msg *lap_msg,
 			  u8 private_data_len)
 {
 	cm_format_mad_hdr(&lap_msg->hdr, CM_LAP_ATTR_ID,
-			  cm_form_tid(cm_id_priv, CM_MSG_SEQUENCE_LAP));
+			  cm_form_tid(cm_id_priv));
 	lap_msg->local_comm_id = cm_id_priv->id.local_id;
 	lap_msg->remote_comm_id = cm_id_priv->id.remote_id;
 	cm_lap_set_remote_qpn(lap_msg, cm_id_priv->remote_qpn);
@@ -3110,7 +3108,7 @@ static void cm_format_sidr_req(struct cm_sidr_req_msg *sidr_req_msg,
 			       struct ib_cm_sidr_req_param *param)
 {
 	cm_format_mad_hdr(&sidr_req_msg->hdr, CM_SIDR_REQ_ATTR_ID,
-			  cm_form_tid(cm_id_priv, CM_MSG_SEQUENCE_SIDR));
+			  cm_form_tid(cm_id_priv));
 	sidr_req_msg->request_id = cm_id_priv->id.local_id;
 	sidr_req_msg->pkey = param->path->pkey;
 	sidr_req_msg->service_id = param->service_id;
