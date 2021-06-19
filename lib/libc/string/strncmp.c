60b60291ca51f39d55747509d4baaa4a52f616b8
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-19 19:58:16 -0400

    Update strncmp.c

--- lib/libc/string/strncmp.c
+++ lib/libc/string/strncmp.c
@@ -44,11 +44,13 @@ strncmp(const char *s1, const char *s2, size_t n)
 	if (n == 0)
 		return (0);
 	do {
-		if (*s1 != *s2++)
+		if (*s1 != *s2)
 			return (*(const unsigned char *)s1 -
-				*(const unsigned char *)(s2 - 1));
-		if (*s1++ == '\0')
+				*(const unsigned char *)s2;
+		if (*s1 == '\0')
 			break;
+
+		++s1, ++s2;
 	} while (--n != 0);
 	return (0);
 }
