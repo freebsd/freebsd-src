b8393053d5c0fb8847f3a2692c9c5e10a5eb180f
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-19 14:28:19 -0400

    Update memcmp.c

--- lib/libc/string/memcmp.c
+++ lib/libc/string/memcmp.c
@@ -50,8 +50,10 @@ memcmp(const void *s1, const void *s2, size_t n)
 		const unsigned char *p1 = s1, *p2 = s2;
 
 		do {
-			if (*p1++ != *p2++)
-				return (*--p1 - *--p2);
+			if (*p1 != *p2)
+				return (*p1 - *p2);
+
+			p1++, p2++;
 		} while (--n != 0);
 	}
 	return (0);
