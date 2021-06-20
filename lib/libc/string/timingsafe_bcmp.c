3decd496b331dbc83968cc689c3a7ccf37bc73e3
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-20 14:02:06 -0400

    Update timingsafe_bcmp.c

--- lib/libc/string/timingsafe_bcmp.c
+++ lib/libc/string/timingsafe_bcmp.c
@@ -26,9 +26,9 @@ int
 __timingsafe_bcmp(const void *b1, const void *b2, size_t n)
 {
 	const unsigned char *p1 = b1, *p2 = b2;
-	int ret = 0;
+	unsigned int ret = 0;
 
-	for (; n > 0; n--)
+	for (; n; n--)
 		ret |= *p1++ ^ *p2++;
 	return (ret != 0);
 }
