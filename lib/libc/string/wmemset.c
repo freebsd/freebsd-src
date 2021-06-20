d59c1ac08e5a61248a0a1e1bc71928a7e6356332
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-20 14:43:43 -0400

    Update wmemset.c

--- lib/libc/string/wmemset.c
+++ lib/libc/string/wmemset.c
@@ -41,13 +41,10 @@ __FBSDID("$FreeBSD$");
 wchar_t	*
 wmemset(wchar_t *s, wchar_t c, size_t n)
 {
-	size_t i;
-	wchar_t *p;
+	wchar_t * const p = s;
 
-	p = (wchar_t *)s;
-	for (i = 0; i < n; i++) {
-		*p = c;
-		p++;
+	for (; n; --n) {
+		*s++ = c;
 	}
-	return s;
+	return p;
 }
