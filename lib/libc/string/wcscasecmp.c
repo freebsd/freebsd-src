63e5e9576ee3a30e889492736e381d4a9a2fab8c
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-20 14:19:44 -0400

    Update wcscasecmp.c

--- lib/libc/string/wcscasecmp.c
+++ lib/libc/string/wcscasecmp.c
@@ -37,11 +37,13 @@ wcscasecmp(const wchar_t *s1, const wchar_t *s2)
 {
 	wchar_t c1, c2;
 
-	for (; *s1; s1++, s2++) {
+	for (;; s1++, s2++) {
 		c1 = towlower(*s1);
 		c2 = towlower(*s2);
 		if (c1 != c2)
-			return ((int)c1 - c2);
+			return (unsigned int)c1 > (unsigned int)c2 ? 1 : -1;
+		if (c1 == '\0')
+			break;
 	}
-	return (-*s2);
+	return 0;
 }
