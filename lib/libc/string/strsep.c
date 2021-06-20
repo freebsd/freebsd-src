e3ced23c2a6d6cc976defe64f368b3ca808952e9
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-20 13:08:19 -0400

    Update strsep.c

--- lib/libc/string/strsep.c
+++ lib/libc/string/strsep.c
@@ -59,15 +59,15 @@ strsep(char **stringp, const char *delim)
 
 	if ((s = *stringp) == NULL)
 		return (NULL);
-	for (tok = s;;) {
-		c = *s++;
+	for (tok = s;; s++) {
+		c = *s;
 		spanp = delim;
 		do {
 			if ((sc = *spanp) == c) {
 				if (c == '\0')
-					s = NULL;
+					s++, s = NULL;
 				else
-					s[-1] = '\0';
+					*s++ = '\0';
 				*stringp = s;
 				return (tok);
 			}
