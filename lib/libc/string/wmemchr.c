938a00b04e6d612a6a829c5458f04a6fbc2afe34
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-20 14:35:54 -0400

    Update wmemchr.c

--- lib/libc/string/wmemchr.c
+++ lib/libc/string/wmemchr.c
@@ -41,14 +41,12 @@ __FBSDID("$FreeBSD$");
 wchar_t	*
 wmemchr(const wchar_t *s, wchar_t c, size_t n)
 {
-	size_t i;
-
-	for (i = 0; i < n; i++) {
+	for (; n != 0; --n) {
 		if (*s == c) {
 			/* LINTED const castaway */
 			return (wchar_t *)s;
 		}
-		s++;
+		++s;
 	}
 	return NULL;
 }
