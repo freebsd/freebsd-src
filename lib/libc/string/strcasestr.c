6c5a964c4e1e0f2db02a1f548893a3765658b2ac
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-21 12:57:02 -0400

    Update strcasestr.c

--- lib/libc/string/strcasestr.c
+++ lib/libc/string/strcasestr.c
@@ -55,8 +55,8 @@ strcasestr_l(const char *s, const char *find, locale_t locale)
 	FIX_LOCALE(locale);
 
 	if ((c = *find) != 0) {
+		const size_t len = strlen(++find);
 		c = (char)tolower_l((unsigned char)c, locale);
-		len = strlen(++find);
 		do {
 			do {
 				if ((sc = *s) == 0)
