b2deba043c0ce5e89db934ff81dad753535eafa8
Dmitry Chagin <dchagin@FreeBSD.org>
2021-06-22 08:06:05 +0300

    linux(4): Rework Linux ppoll system call.
    For now the Linux emulation layer uses in kernel ppoll(2) without
    conversion of user supplied fd 'events', and does not convert the
    kernel supplied fd 'revents'.
    
    At least POLLRDHUP is handled by FreeBSD differently than by
    Linux. Seems that Linux silencly ignores POLLRDHUP on non socket fd's
    unlike FreeBSD, which does more strictly check and fails.
    
    Rework the Linux ppoll, using kern_poll and converting 'events'
    and 'revents' values.
    While here, move poll events defines to the MI part of code as they
    mostly identical on all arches except arm.
    
    Differential Revision:	https://reviews.freebsd.org/D30716
    MFC after:		2 weeks
    
    (cherry picked from commit 26795a0378b58c3e26b68577a4cc446ab527e8b5)

--- sys/i386/linux/linux.h
+++ sys/i386/linux/linux.h
@@ -478,27 +478,6 @@ struct l_ifreq {
 #define	ifr_hwaddr	ifr_ifru.ifru_hwaddr	/* MAC address */
 #define	ifr_ifindex	ifr_ifru.ifru_ivalue	/* Interface index */
 
-/*
- * poll()
- */
-#define	LINUX_POLLIN		0x0001
-#define	LINUX_POLLPRI		0x0002
-#define	LINUX_POLLOUT		0x0004
-#define	LINUX_POLLERR		0x0008
-#define	LINUX_POLLHUP		0x0010
-#define	LINUX_POLLNVAL		0x0020
-#define	LINUX_POLLRDNORM	0x0040
-#define	LINUX_POLLRDBAND	0x0080
-#define	LINUX_POLLWRNORM	0x0100
-#define	LINUX_POLLWRBAND	0x0200
-#define	LINUX_POLLMSG		0x0400
-
-struct l_pollfd {
-	l_int		fd;
-	l_short		events;
-	l_short		revents;
-};
-
 struct l_user_desc {
 	l_uint		entry_number;
 	l_uint		base_addr;
