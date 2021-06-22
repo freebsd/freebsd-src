e812f2014dadb9614f32a9ee832c20073c78ca61
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-22 17:38:19 -0400

    Update string.h

--- include/string.h
+++ include/string.h
@@ -96,14 +96,7 @@ size_t	 strlcpy(char * __restrict, const char * __restrict, size_t);
 #endif
 size_t	 strlen(const char *) __pure;
 #if __BSD_VISIBLE
-#include <unistd.h>
-
-#ifndef _MODE_T_DECLARED
-typedef __mode_t 	mode_t;
-#define _MODE_T_DECLARED
-#endif
-
-void	 strmode(mode_t, char *);
+void	 strmode(int, char *);
 #endif
 char	*strncat(char * __restrict, const char * __restrict, size_t);
 int	 strncmp(const char *, const char *, size_t) __pure;
