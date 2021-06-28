5227bc68fcfd0cf88fa25dfddb86578a9f46c296
Kristof Provost <kp@FreeBSD.org>
2021-06-28 12:48:20 +0200

    pf tests: Stress state retrieval
    Create and retrieve 20.000 states. There have been issues with nvlists
    causing very slow state retrieval. We don't impose a specific limit on
    the time required to retrieve the states, but do log it. In excessive
    cases the Kyua timeout will fail this test.
    
    Reviewed by:	donner
    MFC after:	1 week
    Sponsored by:	Rubicon Communications, LLC ("Netgate")
    Differential Revision:	https://reviews.freebsd.org/D30943

---
+++ tests/sys/netpfil/pf/get_state.sh
@@ -0,0 +1,81 @@
+# $FreeBSD$
+#
+# SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+#
+# Copyright (c) 2021 Rubicon Communications, LLC (Netgate)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+. $(atf_get_srcdir)/utils.subr
+
+common_dir=$(atf_get_srcdir)/../common
+
+atf_test_case "many" "cleanup"
+many_head()
+{
+	atf_set descr 'Test retrieving many states'
+	atf_set require.user root
+	atf_set require.progs scapy
+}
+
+many_body()
+{
+	pft_init
+
+	epair=$(vnet_mkepair)
+	ifconfig ${epair}a 192.0.2.1/24 up
+
+	vnet_mkjail alcatraz ${epair}b
+	jexec alcatraz ifconfig ${epair}b 192.0.2.2/24 up
+	jexec alcatraz pfctl -e
+
+	pft_set_rules alcatraz "set timeout tcp.closed 60000" \
+		"pass in proto icmp" \
+		"pass in proto tcp"
+
+	# Sanity check
+	atf_check -s exit:0 -o ignore ping -c 1 -W 1 192.0.2.2
+
+	# Now syn flood to create many states
+	${common_dir}/pft_synflood.py \
+		--sendif ${epair}a \
+		--to 192.0.2.2 \
+		--count 20000
+
+	count=$(time jexec alcatraz pfctl -ss | wc -l 2>time.txt)
+	echo "Found $count states in `cat time.txt`"
+	if [ $count -lt 20000 ];
+	then
+		atf_fail "Fail to retrieve states"
+	fi
+}
+
+many_cleanup()
+{
+	rm -f time.txt
+	pft_cleanup
+}
+
+atf_init_test_cases()
+{
+	atf_add_test_case "many"
+}
