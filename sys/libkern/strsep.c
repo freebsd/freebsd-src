291d0d6debcfc072dbc179a2ab19b336e48ce84d
Alfonso Gregory <gfunni234@gmail.com>
2021-06-29 17:52:52 -0400

    Update strsep.c

--- sys/libkern/strsep.c
+++ sys/libkern/strsep.c
@@ -54,23 +54,25 @@ strsep(char **stringp, const char *delim)
 {
 	char *s;
 	const char *spanp;
-	int c, sc;
+	char c, sc;
 	char *tok;
 
 	if ((s = *stringp) == NULL)
 		return (NULL);
-	for (tok = s;;) {
-		c = *s++;
+	for (tok = s;; s++) {
+		c = *s;
 		spanp = delim;
 		do {
-			if ((sc = *spanp++) == c) {
+			if ((sc = *spanp) == c) {
 				if (c == '\0')
 					s = NULL;
 				else
-					s[-1] = '\0';
+					*s++ = '\0';
 				*stringp = s;
 				return (tok);
 			}
+
+			spanp++;
 		} while (sc != '\0');
 	}
 	/* NOTREACHED */
