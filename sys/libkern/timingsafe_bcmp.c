c8ff6c0758b8356ea1a0fc35c6b451672d084a6a
Alfonso Gregory <gfunni234@gmail.com>
2021-06-29 17:55:41 -0400

    Update timingsafe_bcmp.c

--- sys/libkern/timingsafe_bcmp.c
+++ sys/libkern/timingsafe_bcmp.c
@@ -23,10 +23,10 @@
 int
 timingsafe_bcmp(const void *b1, const void *b2, size_t n)
 {
-	const unsigned char *p1 = b1, *p2 = b2;
-	int ret = 0;
+	register const unsigned char *p1 = (const unsigned char *)b1, *p2 = (const unsigned char *)b2;
+	register unsigned int ret = 0;
 
-	for (; n > 0; n--)
+	for (; n; n--)
 		ret |= *p1++ ^ *p2++;
 	return (ret != 0);
 }
