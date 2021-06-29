39e638e82c9af6ee3185db86710a3d1392e64a06
Alfonso Gregory <gfunni234@gmail.com>
2021-06-29 13:54:00 -0400

    Optimize memcmp

--- sys/libkern/memcmp.c
+++ sys/libkern/memcmp.c
@@ -47,8 +47,9 @@ int
 		const unsigned char *p1 = s1, *p2 = s2;
 
 		do {
-			if (*p1++ != *p2++)
-				return (*--p1 - *--p2);
+			if (*p1 != *p2)
+				return (*p1 - *p2);
+			++p1, ++p2;
 		} while (--n != 0);
 	}
 	return (0);
