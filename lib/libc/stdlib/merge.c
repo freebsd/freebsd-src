5ff33928545a531e02884e0fe386d143b62bb049
Alfonso Gregory <gfunni234@gmail.com>
2021-06-30 11:06:26 -0400

    Undo merge change

--- lib/libc/stdlib/merge.c
+++ lib/libc/stdlib/merge.c
@@ -129,7 +129,13 @@ mergesort(void *base, size_t nmemb, size_t size, cmp_t cmp)
 	if (nmemb == 0)
 		return (0);
 
-	iflag = ((size % ISIZE) || ((uintptr_t)base % ISIZE));
+	/*
+	 * XXX
+	 * Stupid subtraction for the Cray.
+	 */
+	iflag = 0;
+	if (!(size % ISIZE) && !(((char *)base - (char *)0) % ISIZE))
+		iflag = 1;
 
 	if ((list2 = malloc(nmemb * size + PSIZE)) == NULL)
 		return (-1);
