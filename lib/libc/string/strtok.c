64a6db1d11704f7987768c7e66a9d2f381537a48
Alfonso Gregory <gfunni234@gmail.com>
2021-07-06 16:52:08 -0400

    Add restrict qualifiers

--- lib/libc/string/strtok.c
+++ lib/libc/string/strtok.c
@@ -51,7 +51,7 @@ char	*__strtok_r(char * __restrict, const char * __restrict, char ** __restrict)
 __weak_reference(__strtok_r, strtok_r);
 
 char *
-__strtok_r(char *s, const char *delim, char **last)
+__strtok_r(char * __restrict s, const char * __restrict delim, char ** __restrict last)
 {
 	char *spanp, *tok;
 	int c, sc;
