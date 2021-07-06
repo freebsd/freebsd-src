348795e0d0a37f5b7e75f1a67d8e0db4ca99bbca
Alfonso Gregory <gfunni234@gmail.com>
2021-07-06 19:42:18 -0400

    Add restrict and pure qualifiers to string functions
    No functional changes intended.
    
    Safe to MFC.

--- lib/libc/string/strtok.c
+++ lib/libc/string/strtok.c
@@ -46,12 +46,12 @@ __FBSDID("$FreeBSD$");
 #endif
 #include <string.h>
 
-char	*__strtok_r(char *, const char *, char **);
+char	*__strtok_r(char * __restrict, const char * __restrict, char ** __restrict);
 
 __weak_reference(__strtok_r, strtok_r);
 
 char *
-__strtok_r(char *s, const char *delim, char **last)
+__strtok_r(char * __restrict s, const char * __restrict delim, char ** __restrict last)
 {
 	char *spanp, *tok;
 	int c, sc;
@@ -97,7 +97,7 @@ cont:
 }
 
 char *
-strtok(char *s, const char *delim)
+strtok(char * __restrict s, const char * __restrict delim)
 {
 	static char *last;
 
