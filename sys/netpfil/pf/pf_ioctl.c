3965592abaeacbe83adfd5ec2568e5743322a456
Kristof Provost <kp@FreeBSD.org>
2021-07-08 10:54:16 +0200

    pf: pf_killstates() never fails, so remove the return value
    Suggested by:	mjg
    MFC after:	1 week
    Sponsored by:	Rubicon Communications, LLC ("Netgate")
    
    (cherry picked from commit 34641052826c718566b994b75cd2bddb53a21583)

--- sys/netpfil/pf/pf_ioctl.c
+++ sys/netpfil/pf/pf_ioctl.c
@@ -202,7 +202,7 @@ struct cdev *pf_dev;
  */
 static void		 pf_clear_all_states(void);
 static unsigned int	 pf_clear_states(const struct pf_kstate_kill *);
-static int		 pf_killstates(struct pf_kstate_kill *,
+static void		 pf_killstates(struct pf_kstate_kill *,
 			    unsigned int *);
 static int		 pf_killstates_row(struct pf_kstate_kill *,
 			    struct pf_idhash *);
@@ -2792,7 +2792,7 @@ DIOCCHANGERULE_error:
 			break;
 
 		psk->psk_killed = 0;
-		error = pf_killstates(&kill, &psk->psk_killed);
+		pf_killstates(&kill, &psk->psk_killed);
 		break;
 	}
 
@@ -4902,7 +4902,7 @@ relock_DIOCCLRSTATES:
 	return (killed);
 }
 
-static int
+static void
 pf_killstates(struct pf_kstate_kill *kill, unsigned int *killed)
 {
 	struct pf_kstate	*s;
@@ -4915,13 +4915,13 @@ pf_killstates(struct pf_kstate_kill *kill, unsigned int *killed)
 			pf_unlink_state(s, PF_ENTER_LOCKED);
 			*killed = 1;
 		}
-		return (0);
+		return;
 	}
 
 	for (unsigned int i = 0; i <= pf_hashmask; i++)
 		*killed += pf_killstates_row(kill, &V_pf_idhash[i]);
 
-	return (0);
+	return;
 }
 
 static int
@@ -4954,9 +4954,7 @@ pf_killstates_nv(struct pfioc_nv *nv)
 	if (error)
 		ERROUT(error);
 
-	error = pf_killstates(&kill, &killed);
-	if (error)
-		ERROUT(error);
+	pf_killstates(&kill, &killed);
 
 	free(nvlpacked, M_NVLIST);
 	nvlpacked = NULL;
