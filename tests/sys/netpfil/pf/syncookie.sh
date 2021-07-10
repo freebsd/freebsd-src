18fe461d67c7626a266369cbdb862de986a85d5b
Kristof Provost <kp@FreeBSD.org>
2021-07-10 13:20:44 +0200

    pf tests: Basic syncookie test
    MFC after:	1 week
    Sponsored by:	Modirum MDPay
    Differential Revision:	https://reviews.freebsd.org/D32138
    
    (cherry picked from commit dc0636636bb1937283d4f218732ac2357f4ec4f1)

--- tests/sys/netpfil/pf/syncookie.sh
+++ tests/sys/netpfil/pf/syncookie.sh
@@ -29,6 +29,48 @@
 
 common_dir=$(atf_get_srcdir)/../common
 
+atf_test_case "basic" "cleanup"
+basic_head()
+{
+	atf_set descr 'Basic syncookie test'
+	atf_set require.user root
+}
+
+basic_body()
+{
+	pft_init
+
+	epair=$(vnet_mkepair)
+
+	vnet_mkjail alcatraz ${epair}b
+	jexec alcatraz ifconfig ${epair}b 192.0.2.1/24 up
+	jexec alcatraz /usr/sbin/inetd -p inetd-alcatraz.pid \
+	    $(atf_get_srcdir)/echo_inetd.conf
+
+	ifconfig ${epair}a 192.0.2.2/24 up
+
+	jexec alcatraz pfctl -e
+	pft_set_rules alcatraz \
+		"set syncookies always" \
+		"pass in" \
+		"pass out"
+
+	# Sanity check
+	atf_check -s exit:0 -o ignore ping -c 1 192.0.2.1
+
+	reply=$(echo foo | nc -N -w 5 192.0.2.1 7)
+	if [ "${reply}" != "foo" ];
+	then
+		atf_fail "Failed to connect to syncookie protected echo daemon"
+	fi
+}
+
+basic_cleanup()
+{
+	rm -f inetd-alcatraz.pid
+	pft_cleanup
+}
+
 atf_test_case "forward" "cleanup"
 forward_head()
 {
@@ -128,6 +170,7 @@ nostate_cleanup()
 
 atf_init_test_cases()
 {
+	atf_add_test_case "basic"
 	atf_add_test_case "forward"
 	atf_add_test_case "nostate"
 }
