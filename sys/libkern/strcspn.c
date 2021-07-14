659383dd8ee2cf4549af9f10a4bb315ac5f5cc06
Alfonso Gregory <gfunni234@gmail.com>
2021-07-14 15:48:35 -0600

    Remove incorrect __restricted labels from strcspn
    strcspn should never have had the __restrict keywords. While both of
    these strings are const, it may have unindended side effects. While this
    is the kernel, the POSIX definition also omits restrict.
    
    Reviewed by:	imp@
    Pull Request:	https://github.com/freebsd/freebsd-src/pull/497

--- sys/libkern/strcspn.c
+++ sys/libkern/strcspn.c
@@ -37,7 +37,7 @@ __FBSDID("$FreeBSD$");
 #define	BIT(c)	((u_long)1 << ((u_char)(c) % LONG_BIT))
 
 size_t 
-strcspn(const char * __restrict s, const char * __restrict charset) 
+strcspn(const char *s, const char *charset)
 {
 	/*
 	 * NB: idx and bit are temporaries whose use causes gcc 3.4.2 to
