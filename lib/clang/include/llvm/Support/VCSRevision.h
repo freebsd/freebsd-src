6eabeb40de9b29cbc7860ec4ebda034e4a7b9aed
Dimitry Andric <dim@FreeBSD.org>
2021-07-16 18:29:42 +0200

    Merge llvm-project 12.0.1 release
    This updates llvm, clang, compiler-rt, libc++, libunwind, lld, lldb and
    openmp to llvmorg-12.0.1-0-gfed41342a82f, a.k.a. 12.0.1 release.
    
    PR:		255570
    MFC after:	6 weeks

--- lib/clang/include/llvm/Support/VCSRevision.h
+++ lib/clang/include/llvm/Support/VCSRevision.h
@@ -1,3 +1,3 @@
 /* $FreeBSD$ */
-#define LLVM_REVISION "llvmorg-12.0.1-rc2-0-ge7dac564cd0e"
+#define LLVM_REVISION "llvmorg-12.0.1-0-gfed41342a82f"
 #define LLVM_REPOSITORY "git@github.com:llvm/llvm-project.git"
