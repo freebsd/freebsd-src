6096c2eb4fa0d5f14d7c60bbe225dddf9c309560
Alfonso Gregory <gfunni234@gmail.com>
2021-07-21 16:26:42 -0400

    Update guid.cc

--- lib/libdevdctl/guid.cc
+++ lib/libdevdctl/guid.cc
@@ -53,7 +53,6 @@ extern "C" {
 
 __FBSDID("$FreeBSD$");
 /*============================ Namespace Control =============================*/
-using namespace std;
 using std::string;
 namespace DevdCtl
 {
