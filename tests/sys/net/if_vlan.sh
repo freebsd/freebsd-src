60c9798db74d0b04dcbb4162d155ecd6b38c0e52
Kristof Provost <kp@FreeBSD.org>
2021-07-22 14:50:27 +0200

    net tests: basic test case for bpf(4)'s ability to set vlan pcp
    Use dhclient with its 'vlan-pcp' option to set a VLAN PCP value and
    verify that it actually gets set.
    
    MFC after:	1 week
    Sponsored by:	Rubicon Communications, LLC ("Netgate")
    Differential Revision:	https://reviews.freebsd.org/D31276
    
    (cherry picked from commit 3e87f800f01b6d2cbe49924a01038379889d8b3b)

--- tests/sys/net/if_vlan.sh
+++ tests/sys/net/if_vlan.sh
@@ -210,6 +210,52 @@ qinq_dot_cleanup()
 	vnet_cleanup
 }
 
+atf_test_case "bpf_pcp" "cleanup"
+bpf_pcp_head()
+{
+	atf_set descr 'Set VLAN PCP through BPF'
+	atf_set require.config 'allow_sysctl_side_effects'
+	atf_set require.user root
+	atf_set require.progs scapy
+}
+
+bpf_pcp_body()
+{
+	vnet_init
+
+	epair=$(vnet_mkepair)
+
+	ifconfig ${epair}a up
+
+	vnet_mkjail alcatraz ${epair}b
+	vlan=$(jexec alcatraz ifconfig vlan create)
+	jexec alcatraz ifconfig ${vlan} vlan 42 vlandev ${epair}b
+	jexec alcatraz ifconfig ${vlan} up
+	jexec alcatraz ifconfig ${epair}b up
+
+	sysctl net.link.vlan.mtag_pcp=1
+
+	jexec alcatraz dhclient ${vlan} &
+	atf_check -s exit:1 -o ignore -e ignore $(atf_get_srcdir)/pcp.py \
+		--expect-pcp 6 \
+		--recvif ${epair}a
+
+	jexec alcatraz killall dhclient
+	sleep 1
+
+	jexec alcatraz dhclient -c $(atf_get_srcdir)/dhclient_pcp.conf ${vlan} &
+	atf_check -s exit:0 -o ignore -e ignore $(atf_get_srcdir)/pcp.py \
+		--expect-pcp 6 \
+		--recvif ${epair}a
+}
+
+bpf_pcp_cleanup()
+{
+	sysctl net.link.vlan.mtag_pcp=0
+	jexec alcatraz killall dhclient
+	vnet_cleanup
+}
+
 atf_init_test_cases()
 {
 	atf_add_test_case "basic"
@@ -217,4 +263,5 @@ atf_init_test_cases()
 	atf_add_test_case "qinq_deep"
 	atf_add_test_case "qinq_legacy"
 	atf_add_test_case "qinq_dot"
+	atf_add_test_case "bpf_pcp"
 }
