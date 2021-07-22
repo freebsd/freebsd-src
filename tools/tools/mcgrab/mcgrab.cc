3630f78b4f2f07bd4409bd568814917d19722413
Alfonso Gregory <gfunni234@gmail.com>
2021-07-21 20:18:24 -0400

    Update mcgrab.cc

--- tools/tools/mcgrab/mcgrab.cc
+++ tools/tools/mcgrab/mcgrab.cc
@@ -42,13 +42,13 @@ __FBSDID("$FreeBSD$");
 
 // Operating System and other C based includes
 #include <unistd.h>
-#include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
+#include <sys/ioctl.h>
 #include <netinet/in.h>
 #include <net/if.h>
+#include <arpa/inet.h>
 
 // Private include files
 #include "mctest.h"
