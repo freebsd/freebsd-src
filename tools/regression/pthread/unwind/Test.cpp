19f39259c7969d2005118531da571e4cff8bced5
Alfonso Gregory <gfunni234@gmail.com>
2021-07-23 14:09:21 -0400

    Refactor test

--- tools/regression/pthread/unwind/Test.cpp
+++ tools/regression/pthread/unwind/Test.cpp
@@ -1,37 +1,37 @@
 /* $FreeBSD$ */
 
-int destructed;
-int destructed2;
+bool destructed;
+bool destructed2;
 
 class Test {
 public:
-	Test() { printf("Test::Test()\n"); }
-	~Test() { printf("Test::~Test()\n"); destructed = 1; }
+	Test() { std::printf("Test::Test()\n"); }
+	~Test() { std::printf("Test::~Test()\n"); destructed = true; }
 };
 
 void
 cleanup_handler(void *arg)
 {
-	destructed2 = 1;
-	printf("%s()\n", __func__);
+	destructed2 = true;
+	std::printf("%s()\n", __func__);
 }
 
 void
 check_destruct(void)
 {
-	if (!destructed)
-		printf("Bug, object destructor is not called\n");
+	if (destructed)
+		std::printf("OK\n");
 	else
-		printf("OK\n");
+		std::printf("Bug, object destructor is not called\n");
 }
 
 void
 check_destruct2(void)
 {
 	if (!destructed)
-		printf("Bug, object destructor is not called\n");
+		std::printf("Bug, object destructor is not called\n");
 	else if (!destructed2)
-		printf("Bug, cleanup handler is not called\n");
+		std::printf("Bug, cleanup handler is not called\n");
 	else
-		printf("OK\n");
+		std::printf("OK\n");
 }
