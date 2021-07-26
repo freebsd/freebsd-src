97382953f2c75b9cbee85daf1b7ad5e6bf0381bf
Edward Tomasz Napierala <trasz@FreeBSD.org>
2021-07-26 23:43:09 +0000

    linux: implement sigaltstack(2) on arm64
    ... by making it machine-independent.
    
    Reviewed By:	dchagin
    Sponsored By:	EPSRC
    Differential Revision:	https://reviews.freebsd.org/D31286
    
    (cherry picked from commit 30c6d982190482857883fc96eefafcc6fd769fa0)

--- sys/amd64/linux/linux_machdep.c
+++ sys/amd64/linux/linux_machdep.c
@@ -189,37 +189,6 @@ linux_pause(struct thread *td, struct linux_pause_args *args)
 }
 
 int
-linux_sigaltstack(struct thread *td, struct linux_sigaltstack_args *uap)
-{
-	stack_t ss, oss;
-	l_stack_t lss;
-	int error;
-
-	memset(&lss, 0, sizeof(lss));
-	LINUX_CTR2(sigaltstack, "%p, %p", uap->uss, uap->uoss);
-
-	if (uap->uss != NULL) {
-		error = copyin(uap->uss, &lss, sizeof(l_stack_t));
-		if (error)
-			return (error);
-
-		ss.ss_sp = PTRIN(lss.ss_sp);
-		ss.ss_size = lss.ss_size;
-		ss.ss_flags = linux_to_bsd_sigaltstack(lss.ss_flags);
-	}
-	error = kern_sigaltstack(td, (uap->uss != NULL) ? &ss : NULL,
-	    (uap->uoss != NULL) ? &oss : NULL);
-	if (!error && uap->uoss != NULL) {
-		lss.ss_sp = PTROUT(oss.ss_sp);
-		lss.ss_size = oss.ss_size;
-		lss.ss_flags = bsd_to_linux_sigaltstack(oss.ss_flags);
-		error = copyout(&lss, uap->uoss, sizeof(l_stack_t));
-	}
-
-	return (error);
-}
-
-int
 linux_arch_prctl(struct thread *td, struct linux_arch_prctl_args *args)
 {
 	unsigned long long cet[3];
