b390f6cdcd51e87b0c61344651938e061ac1d8b3
Kirk McKusick <mckusick@FreeBSD.org>
2021-07-29 16:31:16 -0700

    Clean up orphaned indirdep dependency structures after disk failure.
    During forcible unmount after a disk failure there is a bug that
    causes one or more indirdep dependency structures to fail to be
    deallocated. Until we manage to track down why they fail to get
    cleaned up, this code tracks them down and eliminates them so that
    the unmount can succeed.
    
    Reported by:  Peter Holm
    Help from:    kib
    Reviewed by:  Chuck Silvers
    Tested by:    Peter Holm
    MFC after:    7 days
    Sponsored by: Netflix

--- sys/ufs/ffs/softdep.h
+++ sys/ufs/ffs/softdep.h
@@ -213,10 +213,10 @@ struct worklist {
 	struct mount		*wk_mp;		/* Mount we live in */
 	unsigned int		wk_type:8,	/* type of request */
 				wk_state:24;	/* state flags */
+	LIST_ENTRY(worklist)	wk_all;		/* list of deps of this type */
 #ifdef INVARIANTS
 	const char		*wk_func;	/* func where added / removed */
 	int			wk_line;	/* line where added / removed */
-	LIST_ENTRY(worklist)	wk_all;		/* list of deps of this type */
 #endif
 };
 #define	WK_DATA(wk) ((void *)(wk))
@@ -1075,9 +1075,7 @@ struct mount_softdeps {
 	TAILQ_ENTRY(mount_softdeps) sd_next;	/* List of softdep filesystem */
 	struct	ufsmount *sd_ump;		/* our ufsmount structure */
 	u_long	sd_curdeps[D_LAST + 1];		/* count of current deps */
-#ifdef INVARIANTS
 	struct	workhead sd_alldeps[D_LAST + 1];/* Lists of all deps */
-#endif
 };
 /*
  * Flags for communicating with the syncer thread.
