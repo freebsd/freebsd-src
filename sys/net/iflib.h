6c717a28505d3e77a0c3780ca1f65eb85b538eb9
Eric Joyner <erj@FreeBSD.org>
2021-07-29 16:24:14 -0700

    iflib: Allow drivers to determine which queue to TX on
    Adds a new function pointer to struct if_txrx in order to allow
    drivers to set their own function that will determine which queue
    a packet should be sent on.
    
    Since this includes a kernel ABI change, bump the __FreeBSD_version
    as well.
    
    (This motivation behind this is to allow the driver to examine the
    UP in the VLAN tag and determine which queue to TX on based on
    that, in support of HW TX traffic shaping.)
    
    Signed-off-by: Eric Joyner <erj@FreeBSD.org>
    
    Reviewed by:	kbowling@, stallamr@netapp.com
    Tested by:	jeffrey.e.pieper@intel.com
    Sponsored by:	Intel Corporation
    Differential Revision:	https://reviews.freebsd.org/D31485
    
    (cherry picked from commit 213e91399b7998554d787bb290109ebe602aa279)

--- sys/net/iflib.h
+++ sys/net/iflib.h
@@ -187,6 +187,7 @@ typedef struct if_txrx {
 	void (*ift_rxd_refill) (void * , if_rxd_update_t iru);
 	void (*ift_rxd_flush) (void *, uint16_t qsidx, uint8_t flidx, qidx_t pidx);
 	int (*ift_legacy_intr) (void *);
+	qidx_t (*ift_txq_select) (void *, struct mbuf *);
 } *if_txrx_t;
 
 typedef struct if_softc_ctx {
@@ -391,6 +392,15 @@ typedef enum {
  */
 #define IFLIB_PSEUDO_ETHER	0x80000
 
+/* The following IFLIB_FEATURE_* defines are for driver modules to determine
+ * what features this version of iflib supports. They shall be defined to the
+ * first __FreeBSD_version that introduced the feature.
+ */
+/*
+ * Driver can set its own TX queue selection function
+ * as ift_txq_select in struct if_txrx
+ */
+#define IFLIB_FEATURE_QUEUE_SELECT	1203508
 
 /*
  * These enum values are used in iflib_needs_restart to indicate to iflib
