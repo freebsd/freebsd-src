0c1c8a6f8350a3df2b6f5a98ae23572ed5fb8474
Warner Losh <imp@FreeBSD.org>
2021-08-02 15:49:47 -0600

    clock_id: These symbols weren't in 4.4BSD, adjust copyright
    Peter Wemm added the first CLOCK_* symbols in 0f5ed9f420528 in 1997
    after obtaining them from NetBSD. In NetBSD, jtc@netbsd.org committed
    them in sys/sys/time.h rev 1.19 dated 1996/11/15, along with all the
    system calls associated with 1003.1b. FreeBSD's values are, however,
    different than NetBSD's today. The USL/UCB lawsuit was settled in 1994,
    so these couldn't have been derived from material provided to University
    of California covered in that settlement. This file does not need the
    settlement disclaimer.
    
    Furthermore, I rewrote most of the code (except the symbols and their
    values) when merging it from time.h and sys/time.h. Most of the creative
    content of the file is new, so update copyright to reflect that.
    
    Reviewed by:	kaktus
    Sponsored by:	Netflix
    Differential Revision:	https://reviews.freebsd.org/D31369

--- sys/sys/_clock_id.h
+++ sys/sys/_clock_id.h
@@ -1,13 +1,7 @@
 /*-
- * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-License-Identifier: BSD-2-Clause
  *
- * Copyright (c) 1989, 1993
- *	The Regents of the University of California.  All rights reserved.
- * (c) UNIX System Laboratories, Inc.
- * All or some portions of this file are derived from material licensed
- * to the University of California by American Telephone and Telegraph
- * Co. or Unix System Laboratories, Inc. and are reproduced herein with
- * the permission of UNIX System Laboratories, Inc.
+ * Copyright (c) 2021 Netflix, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -17,9 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -32,8 +23,6 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- *	@(#)time.h	8.3 (Berkeley) 1/21/94
  */
 
 #ifndef _SYS_SYS__CLOCK_ID_H
