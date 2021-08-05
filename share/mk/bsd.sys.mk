89ed2ecb14ceabc27883282cf96559a9e7d52717
Ed Maste <emaste@FreeBSD.org>
2021-08-05 08:34:03 -0400

    Disable compressed debug by default
    In c910570e7573 I enabled compressed debug sections, but it broke mips
    and powerpc.  Disable it for now.
    
    Reported by:	jenkins, manu
    Sponsored by:	The FreeBSD Foundation

--- share/mk/bsd.sys.mk
+++ share/mk/bsd.sys.mk
@@ -247,7 +247,7 @@ CFLAGS+=	${SSP_CFLAGS}
 
 # Additional flags passed in CFLAGS and CXXFLAGS when MK_DEBUG_FILES is
 # enabled.
-DEBUG_FILES_CFLAGS?= -g -gz=zlib
+DEBUG_FILES_CFLAGS?= -g
 
 # Allow user-specified additional warning flags, plus compiler and file
 # specific flag overrides, unless we've overridden this...
