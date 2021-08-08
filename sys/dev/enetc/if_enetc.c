freebsd/freebsd-src

commit f95aeeb2826ceaf7b96b18e887d7464e4a962550

enetc: Support building the driver as a loadable module.
Function level reset has to be done in attach in order to put the
hardware in a known state before configuring it.
The order of DRIVER_MODULEs was changed to ensure that the miibus driver
is loaded when mii_attach is called.

Obtained from: Semihalf
Sponsored by: Alstom Group

sys/dev/enetc/if_enetc.c
@@ -158,8 +158,8 @@ static driver_t enetc_driver = {
 };
 
 static devclass_t enetc_devclass;
-DRIVER_MODULE(enetc, pci, enetc_driver, enetc_devclass, NULL, NULL);
 DRIVER_MODULE(miibus, enetc, miibus_driver, miibus_devclass, NULL, NULL);
+DRIVER_MODULE(enetc, pci, enetc_driver, enetc_devclass, NULL, NULL);
 MODULE_VERSION(enetc, 1);
 
 IFLIB_PNP_INFO(pci, enetc, enetc_vendor_info_array);
@@ -365,6 +365,10 @@ enetc_attach_pre(if_ctx_t ctx)
 	sc->dev = iflib_get_dev(ctx);
 	sc->shared = scctx;
 	ifp = iflib_get_ifp(ctx);
+
+	pci_save_state(sc->dev);
+	pcie_flr(sc->dev, 1000, false);
+	pci_restore_state(sc->dev);
 
 	rid = PCIR_BAR(ENETC_BAR_REGS);
 	sc->regs = bus_alloc_resource_any(sc->dev, SYS_RES_MEMORY, &rid, RF_ACTIVE);

sys/modules/Makefile
@@ -111,6 +111,7 @@ SUBDIR=	\
 	${_efirt} \
 	${_em} \
 	${_ena} \
+	${_enetc} \
 	esp \
 	${_et} \
 	evdev \
@@ -612,6 +613,7 @@ _allwinner=	allwinner
 _armv8crypto=	armv8crypto
 _dwwdt=		dwwdt
 _em=		em
+_enetc=		enetc
 _felix=		felix
 _rockchip=	rockchip
 .endif

sys/modules/enetc/Makefile
@@ -1,0 +1,8 @@
+#$FreeBSD$
+
+.PATH: ${SRCTOP}/sys/dev/enetc
+
+KMOD	= if_enetc
+SRCS	= if_enetc.c enetc_mdio.c
+
+.include <bsd.kmod.mk>

commit 0d9e3906925091e3c4a7d37329150fd1212e5c2f

enetc: Force correct order with DRIVER_MODULE_ORDERED
The toolchain can reorder symbols, meaning that changing
the order of DRIVER_MODULE macros is not enough
to ensure that miibus gets registered first.
Use DRIVER_MODULE_ORDERED instead to fix the problem properly.

Fixes: f95aeeb2826c ("enetc: Support building the driver as a loadable module.")

Reported by: jhb

sys/dev/enetc/if_enetc.c
@@ -159,7 +159,9 @@ static driver_t enetc_driver = {
 
 static devclass_t enetc_devclass;
 DRIVER_MODULE(miibus, enetc, miibus_driver, miibus_devclass, NULL, NULL);
-DRIVER_MODULE(enetc, pci, enetc_driver, enetc_devclass, NULL, NULL);
+/* Make sure miibus gets procesed first. */
+DRIVER_MODULE_ORDERED(enetc, pci, enetc_driver, enetc_devclass, NULL, NULL,
+    SI_ORDER_ANY);
 MODULE_VERSION(enetc, 1);
 
 IFLIB_PNP_INFO(pci, enetc, enetc_vendor_info_array);