5fdfe78b42d0995e3b6849c43cbe94eeb3c308d4
Mark Johnston <markj@FreeBSD.org>
2021-08-10 15:51:03 -0400

    conf: Add a KMSAN kernel option
    Sponsored by:	The FreeBSD Foundation
    
    (cherry picked from commit 30d00832d7733e60f5e030d335c129bfa77dd77a)

--- sys/conf/kern.pre.mk
+++ sys/conf/kern.pre.mk
@@ -122,6 +122,12 @@ KCSAN_ENABLED!=	grep KCSAN opt_global.h || true ; echo
 SAN_CFLAGS+=	-fsanitize=thread
 .endif
 
+KMSAN_ENABLED!= grep KMSAN opt_global.h || true ; echo
+.if !empty(KMSAN_ENABLED)
+SAN_CFLAGS+=	-DSAN_NEEDS_INTERCEPTORS -DSAN_INTERCEPTOR_PREFIX=kmsan \
+		-fsanitize=kernel-memory
+.endif
+
 KUBSAN_ENABLED!=	grep KUBSAN opt_global.h || true ; echo
 .if !empty(KUBSAN_ENABLED)
 SAN_CFLAGS+=	-fsanitize=undefined
