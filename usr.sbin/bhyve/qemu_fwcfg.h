be6b1f7ac37511e0d07586eec58131edbd87d62f
Corvin Köhne <corvink@FreeBSD.org>
2021-08-11 10:00:34 +0200

    bhyve: add emulation for qemu's fwcfg data port
    The data port returns the data of the fwcfg item.
    
    Reviewed by:		markj
    MFC after:		1 week
    Sponsored by:		Beckhoff Automation GmbH & Co. KG
    Differential Revision:	https://reviews.freebsd.org/D38333

--- usr.sbin/bhyve/qemu_fwcfg.h
+++ usr.sbin/bhyve/qemu_fwcfg.h
@@ -9,4 +9,12 @@
 
 #include <vmmapi.h>
 
+#define QEMU_FWCFG_MAX_ARCHS 0x2
+#define QEMU_FWCFG_MAX_ENTRIES 0x4000
+
+struct qemu_fwcfg_item {
+	uint32_t size;
+	uint8_t *data;
+};
+
 int qemu_fwcfg_init(struct vmctx *const ctx);
