05bfa3e0589629bc88b8da31942252af8a559441
Thomas Munro <tmunro@FreeBSD.org>
2021-08-22 21:48:59 +1200

    lio_listio(2):  Allow LIO_READV and LIO_WRITEV.
    Allow multiple vector IOs to be started with one system call.
    aio_readv() and aio_writev() already used these opcodes under the
    covers.  This commit makes them available to user space.
    
    Being non-standard extensions, they're only visible if __BSD_VISIBLE is
    defined, like the functions.
    
    Reviewed by:    asomers, kib
    MFC after:      2 weeks
    Differential Revision:  https://reviews.freebsd.org/D31627
    
    (cherry picked from commit f30a1ae8d5290a52e898279bafc38556bf16bed8)

--- sys/kern/vfs_aio.c
+++ sys/kern/vfs_aio.c
@@ -1419,6 +1419,8 @@ aiocb_copyin(struct aiocb *ujob, struct kaiocb *kjob, int type)
 	error = copyin(ujob, kcb, sizeof(struct aiocb));
 	if (error)
 		return (error);
+	if (type == LIO_NOP)
+		type = kcb->aio_lio_opcode;
 	if (type & LIO_VECTORED) {
 		/* malloc a uio and copy in the iovec */
 		error = copyinuio(__DEVOLATILE(struct iovec*, kcb->aio_iov),
@@ -1557,8 +1559,10 @@ aio_aqueue(struct thread *td, struct aiocb *ujob, struct aioliojob *lj,
 	if (type == LIO_NOP) {
 		switch (job->uaiocb.aio_lio_opcode) {
 		case LIO_WRITE:
+		case LIO_WRITEV:
 		case LIO_NOP:
 		case LIO_READ:
+		case LIO_READV:
 			opcode = job->uaiocb.aio_lio_opcode;
 			break;
 		default:
