9faa0bc4fadeb52b1bab3ed134cca3ccd8524456
Gordon Tetlow <gordon@FreeBSD.org>
2021-08-24 11:08:20 -0700

    Fix multiple OpenSSL vulnerabilities.
    Approved by:    so
    Security:       SA-21:16.openssl
    Security:       CVE-2021-3711
    Security:       CVE-2021-3712

--- crypto/openssl/include/openssl/opensslv.h
+++ crypto/openssl/include/openssl/opensslv.h
@@ -40,7 +40,7 @@ extern "C" {
  *  major minor fix final patch/beta)
  */
 # define OPENSSL_VERSION_NUMBER  0x1010108fL
-# define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.1h-freebsd  22 Sep 2020"
+# define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.1h-freebsd  24 Aug 2021"
 
 /*-
  * The macros below are to be used for shared library (.so, .dll, ...)
