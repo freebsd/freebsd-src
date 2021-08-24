freebsd/freebsd-src

commit e59eff9ad3285838730acf48f6d066cec0e53114

pfctl: fix killing states by ID
Since the conversion to the new DIOCKILLSTATESNV the kernel no longer
exists the id and creatorid to be big-endian.
As a result killing states by id (i.e. `pfctl -k id -k 12345`) no longer
worked.

Reported by:	Özkan KIRIK
MFC after:	1 week
Sponsored by:	Rubicon Communications, LLC ("Netgate")

sbin/pfctl/pfctl.c
@@ -908,7 +908,6 @@ pfctl_id_kill_states(int dev, const char *iface, int opts)
 
 	if ((sscanf(state_kill[1], "%jx/%x",
 	    &kill.cmp.id, &kill.cmp.creatorid)) == 2)
-		HTONL(kill.cmp.creatorid);
 	else if ((sscanf(state_kill[1], "%jx", &kill.cmp.id)) == 1) {
 		kill.cmp.creatorid = 0;
 	} else {
@@ -920,7 +919,6 @@ pfctl_id_kill_states(int dev, const char *iface, int opts)
 		usage();
 	}
 
-	kill.cmp.id = htobe64(kill.cmp.id);
 	if (pfctl_kill_states(dev, &kill, &killed))
 		err(1, "DIOCKILLSTATES");
 

commit 9ce320820e6d760df11a88de11fbae024c18d23c

pfctl: build fix
Fix the build issue introduced in e59eff9ad328 (pfctl: fix killing states by ID)

MFC after:	1 week
Sponsored by:	Rubicon Communications, LLC ("Netgate")

sbin/pfctl/pfctl.c
@@ -907,7 +907,8 @@ pfctl_id_kill_states(int dev, const char *iface, int opts)
 		kill.kill_match = true;
 
 	if ((sscanf(state_kill[1], "%jx/%x",
-	    &kill.cmp.id, &kill.cmp.creatorid)) == 2)
+	    &kill.cmp.id, &kill.cmp.creatorid)) == 2) {
+	}
 	else if ((sscanf(state_kill[1], "%jx", &kill.cmp.id)) == 1) {
 		kill.cmp.creatorid = 0;
 	} else {