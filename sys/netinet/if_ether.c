fb3a780fdd23e6a8366b8e83cdf02e2ac7b01270
orange30 <44566632+orange30@users.noreply.github.com>
2021-09-01 16:50:15 +0800

    free mbuf before return from in_arpinput
    Need free the mbuf before return from in_arpinput.

--- sys/netinet/if_ether.c
+++ sys/netinet/if_ether.c
@@ -1127,6 +1127,7 @@ reply:
 	 * by if_output().
 	 */
 	if (error != 0 && error != EAFNOSUPPORT) {
+		m_freem(m)；
 		ARP_LOG(LOG_ERR, "Failed to calculate ARP header on %s: %d\n",
 		    if_name(ifp), error);
 		return;
