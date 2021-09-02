8a95c6332bfc9b45a30bde15e306ccdccc33a1db
Alfredo Dal'Ava Junior <alfredo@FreeBSD.org>
2021-09-02 14:54:31 -0300

    llvm: Revert "[HardwareLoops] Change order of SCEV expression construction for InitLoopCount."
    Reverts llvm commit 42eaf4fe0adef3344adfd9fbccd49f325cb549ef, pointed
    from bisect as source of regression that causes liblzma to compress/
    uncompress incorrectly. It's know to affect powerpc64 BE only.
    
    The patch unbreaks FreeBSD powerpc64 installation media, since
    bsdinstall can't uncompress the *.txz produced by FreeBSD CI. It's
    probably miscompiling other software bas well.
    
    Upstream PR: https://bugs.llvm.org/show_bug.cgi?id=51714
    
    Reviewed by:	dim
    MFC after:	2 days
    Sponsored by:	Instituto de Pesquisas Eldorado (eldorado.org.br)
    Differential Revision:	https://reviews.freebsd.org/D31804
    
    (cherry picked from commit f21fcae487026987f44154de981c03b69efbc945)

--- contrib/llvm-project/llvm/include/llvm/Analysis/TargetTransformInfo.h
+++ contrib/llvm-project/llvm/include/llvm/Analysis/TargetTransformInfo.h
@@ -94,6 +94,7 @@ struct HardwareLoopInfo {
   Loop *L = nullptr;
   BasicBlock *ExitBlock = nullptr;
   BranchInst *ExitBranch = nullptr;
+  const SCEV *ExitCount = nullptr;
   const SCEV *TripCount = nullptr;
   IntegerType *CountType = nullptr;
   Value *LoopDecrement = nullptr; // Decrement the loop counter by this
