9eeb5028ed67aa0c8140efd19799851f3aaab0c9
Alex Richardson <arichardson@FreeBSD.org>
2021-09-06 09:49:49 +0100

    Add WITH_LLVM_BINUTILS to install LLVM binutils instead of Elftoolchain
    When WITH_LLVM_BINUTILS is set, we will install the LLVM binutils as
    ar/ranlib/nm/objcopy/etc. instead of the elftoolchain ones.
    Having the LLVM binutils instead of the elftoolchain ones allows us to use
    features such as LTO that depend on binutils that understand LLVM IR.
    Another benefit will be an improved user-experience when compiling with
    AddressSanitizer, since ASAN does not symbolize backtraces correctly if
    addr2line is elftoolchain addr2line instead of llvm-symbolizer.
    See https://lists.freebsd.org/archives/freebsd-toolchain/2021-July/000062.html
    for more details.
    
    This is currently off by default but will be turned on by default at some
    point in the near future.
    
    Reviewed By:	emaste
    
    Differential Revision: https://reviews.freebsd.org/D31060
    
    (cherry picked from commit 021385aba56279febcfdcc64d23673a0106ae45d)

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -210,6 +210,7 @@ __DEFAULT_NO_OPTIONS = \
     LOADER_FIREWIRE \
     LOADER_FORCE_LE \
     LOADER_VERIEXEC_PASS_MANIFEST \
+    LLVM_BINUTILS \
     NAND \
     OFED_EXTRA \
     OPENLDAP \
@@ -562,6 +563,7 @@ MK_GDB:=	no
 MK_INCLUDES:=	no
 MK_LLD:=	no
 MK_LLDB:=	no
+MK_LLVM_BINUTILS:=	no
 .endif
 
 .if ${MK_CLANG} == "no"
@@ -569,6 +571,12 @@ MK_CLANG_EXTRAS:= no
 MK_CLANG_FORMAT:= no
 MK_CLANG_FULL:= no
 MK_LLVM_COV:= no
+.endif
+
+.if ${MK_LLVM_BINUTILS} == "yes"
+# MK_LLVM_CXXFILT is a subset of MK_LLVM_BINUTILS and should therefore be
+# enabled if MK_LLVM_BINUTILS is set.
+MK_LLVM_CXXFILT:=	yes
 .endif
 
 .if ${MK_LOADER_VERIEXEC} == "no"
