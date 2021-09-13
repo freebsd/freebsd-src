12ca33f44fc76890336f517ace402b47b9afb134
Konstantin Belousov <kib@FreeBSD.org>
2021-09-13 23:33:37 +0300

    amd64: move signal handling and register structures manipulations into exec_machdep.c
    from machdep.c which is too large pile of unrelated things.
    Some ptrace functions are moved from machdep.c to ptrace_machdep.c.
    
    Now machdep.c contains code mostly related to the low level initialization
    and regular low level operation of the architecture, while signal MD code
    and registers handling is placed in exec_machdep.c.
    
    Reviewed by:	jhb, markj
    Discussed with:	jrtc27
    Tested by:	pho
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D31954

--- sys/amd64/amd64/ptrace_machdep.c
+++ sys/amd64/amd64/ptrace_machdep.c
@@ -32,7 +32,9 @@ __FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
 #include <sys/systm.h>
+#include <sys/lock.h>
 #include <sys/malloc.h>
+#include <sys/mutex.h>
 #include <sys/proc.h>
 #include <sys/ptrace.h>
 #include <sys/sysent.h>
@@ -277,4 +279,35 @@ cpu_ptrace(struct thread *td, int req, void *addr, int data)
 	}
 
 	return (error);
+}
+
+int
+ptrace_set_pc(struct thread *td, unsigned long addr)
+{
+
+	td->td_frame->tf_rip = addr;
+	set_pcb_flags(td->td_pcb, PCB_FULL_IRET);
+	return (0);
+}
+
+int
+ptrace_single_step(struct thread *td)
+{
+
+	PROC_LOCK_ASSERT(td->td_proc, MA_OWNED);
+	if ((td->td_frame->tf_rflags & PSL_T) == 0) {
+		td->td_frame->tf_rflags |= PSL_T;
+		td->td_dbgflags |= TDB_STEP;
+	}
+	return (0);
+}
+
+int
+ptrace_clear_single_step(struct thread *td)
+{
+
+	PROC_LOCK_ASSERT(td->td_proc, MA_OWNED);
+	td->td_frame->tf_rflags &= ~PSL_T;
+	td->td_dbgflags &= ~TDB_STEP;
+	return (0);
 }
