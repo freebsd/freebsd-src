cbbdacf4dc8f2ef2fcdff5ab3ca021468e5dbe61
Alex Richardson <arichardson@FreeBSD.org>
2021-09-13 13:22:07 +0100

    Add missing dep patterns for .pieo
    While adding sanitizer support, I noticed that all other extensions were
    handled but .pieo was missing.
    
    Reviewed By:	emaste, imp
    MFC after:	1 week
    
    Differential Revision: https://reviews.freebsd.org/D31040
    
    (cherry picked from commit 2d78130185b5518e0bdb4a2f3c5d5ede6773312d)

--- share/mk/bsd.dep.mk
+++ share/mk/bsd.dep.mk
@@ -170,8 +170,11 @@ ${_D}.o: ${_DSRC} ${OBJS:S/^${_D}.o$//}
 	@rm -f ${.TARGET}
 	${DTRACE} ${DTRACEFLAGS} -G -o ${.TARGET} -s ${.ALLSRC:N*.h}
 .if defined(LIB)
-CLEANFILES+= ${_D}.pico ${_D}.po ${_D}.nossppico
+CLEANFILES+= ${_D}.pico ${_D}.pieo ${_D}.po ${_D}.nossppico
 ${_D}.pico: ${_DSRC} ${SOBJS:S/^${_D}.pico$//}
+	@rm -f ${.TARGET}
+	${DTRACE} ${DTRACEFLAGS} -G -o ${.TARGET} -s ${.ALLSRC:N*.h}
+${_D}.pieo: ${_DSRC} ${OBJS:S/^${_D}.pieo$//}
 	@rm -f ${.TARGET}
 	${DTRACE} ${DTRACEFLAGS} -G -o ${.TARGET} -s ${.ALLSRC:N*.h}
 ${_D}.po: ${_DSRC} ${POBJS:S/^${_D}.po$//}
