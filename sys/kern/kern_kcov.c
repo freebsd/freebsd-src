cf4670fe0b1049863ed3150a6ffb0b80dad151b2
Mark Johnston <markj@FreeBSD.org>
2021-09-14 14:29:27 -0400

    kcov: Integrate with KMSAN
    - kern_kcov.c needs to be compiled with -fsanitize=kernel-memory when
      KMSAN is configured since it calls into various other subsystems.
    - Disable address and memory sanitizers in kcov(4)'s coverage sanitizer
      callbacks, as they do not provide useful checking.  Moreover, with
      KMSAN we may otherwise get false positives since the caller (coverage
      sanitizer runtime) is not instrumented.
    - Disable KASAN and KMSAN interceptors in subr_coverage.c, as they do
      not provide any benefit but do introduce overhead when fuzzing.
    
    Sponsored by:	The FreeBSD Foundation

--- sys/kern/kern_kcov.c
+++ sys/kern/kern_kcov.c
@@ -35,7 +35,8 @@
  * $FreeBSD$
  */
 
-#ifdef KCSAN
+/* Interceptors are required for KMSAN. */
+#if defined(KASAN) || defined(KCSAN)
 #define	SAN_RUNTIME
 #endif
 
@@ -191,7 +192,7 @@ get_kinfo(struct thread *td)
 	return (info);
 }
 
-static void
+static void __nosanitizeaddress __nosanitizememory
 trace_pc(uintptr_t ret)
 {
 	struct thread *td;
@@ -223,7 +224,7 @@ trace_pc(uintptr_t ret)
 	buf[0] = index + 1;
 }
 
-static bool
+static bool __nosanitizeaddress __nosanitizememory
 trace_cmp(uint64_t type, uint64_t arg1, uint64_t arg2, uint64_t ret)
 {
 	struct thread *td;
