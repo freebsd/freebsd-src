freebsd/freebsd-src

commit 34de7548dcf5d4fb6b57cc400983816e168f2825

cxgbe tom: Permit rcv_nxt mismatches on FIN for iSCSI connections on T6.
The remote peer might send a FIN in the middle of a burst of data
PDUs.  In the case of T6 with data PDU completion moderation, the
driver would not have seen these PDUs since the final PDU in the burst
was never received resulting in a stale rcv_nxt when the FIN is
received.

While here, invert the logic in the condition to be more readable and
always set tp->rcv_nxt from the sequence number in the CPL.  This sets
the proper value of rcv_nxt for FINs on connections with data received
but not reported via a CPL (e.g. a partial iSCSI PDU burst interrupted
by a FIN).

Reported by:	Jithesh Arakkan @ Chelsio
Reviewed by:	np
Sponsored by:	Chelsio Communications
Differential Revision:	https://reviews.freebsd.org/D30871

sys/dev/cxgbe/tom/t4_cpl_io.c
@@ -1288,7 +1288,21 @@ do_peer_close(struct sge_iq *iq, const struct rss_header *rss, struct mbuf *m)
 	if (toep->flags & TPF_ABORT_SHUTDOWN)
 		goto done;
 
-	tp->rcv_nxt++;	/* FIN */
+	if (ulp_mode(toep) == ULP_MODE_RDMA ||
+	    (ulp_mode(toep) == ULP_MODE_ISCSI && chip_id(sc) >= CHELSIO_T6)) {
+		/*
+		 * There might be data received via DDP before the FIN
+		 * not reported to the driver.  Just assume the
+		 * sequence number in the CPL is correct as the
+		 * sequence number of the FIN.
+		 */
+	} else {
+		KASSERT(tp->rcv_nxt + 1 == be32toh(cpl->rcv_nxt),
+		    ("%s: rcv_nxt mismatch: %u %u", __func__, tp->rcv_nxt,
+		    be32toh(cpl->rcv_nxt)));
+	}
+
+	tp->rcv_nxt = be32toh(cpl->rcv_nxt);
 
 	so = inp->inp_socket;
 	socantrcvmore(so);
@@ -1298,12 +1312,6 @@ do_peer_close(struct sge_iq *iq, const struct rss_header *rss, struct mbuf *m)
 		    (DDP_BUF0_ACTIVE | DDP_BUF1_ACTIVE)))
 			handle_ddp_close(toep, tp, cpl->rcv_nxt);
 		DDP_UNLOCK(toep);
-	}
-
-	if (ulp_mode(toep) != ULP_MODE_RDMA) {
-		KASSERT(tp->rcv_nxt == be32toh(cpl->rcv_nxt),
-	    		("%s: rcv_nxt mismatch: %u %u", __func__, tp->rcv_nxt,
-	    		be32toh(cpl->rcv_nxt)));
 	}
 
 	switch (tp->t_state) {

commit 15b8449844244011730c71434543a61cc9620e58

cxgbe tom: Update rcv_nxt for a FIN after handle_ddp_close().
For TCP DDP, handle_ddp_close() needs to see the pre-FIN rcv_nxt to
determine how much data was placed in the local buffer before the FIN
was received.  The changes in 34de7548dcf5 broke this by updating
rcv_nxt before calling handle_ddp_close().

Fixes:		34de7548dcf5 cxgbe tom: Permit rcv_nxt mismatches on FIN for iSCSI connections on T6.
Sponsored by:	Chelsio Communications

sys/dev/cxgbe/tom/t4_cpl_io.c
@@ -1374,6 +1374,16 @@ do_peer_close(struct sge_iq *iq, const struct rss_header *rss, struct mbuf *m)
 	if (toep->flags & TPF_ABORT_SHUTDOWN)
 		goto done;
 
+	so = inp->inp_socket;
+	socantrcvmore(so);
+	if (ulp_mode(toep) == ULP_MODE_TCPDDP) {
+		DDP_LOCK(toep);
+		if (__predict_false(toep->ddp.flags &
+		    (DDP_BUF0_ACTIVE | DDP_BUF1_ACTIVE)))
+			handle_ddp_close(toep, tp, cpl->rcv_nxt);
+		DDP_UNLOCK(toep);
+	}
+
 	if (ulp_mode(toep) == ULP_MODE_RDMA ||
 	    (ulp_mode(toep) == ULP_MODE_ISCSI && chip_id(sc) >= CHELSIO_T6)) {
 		/*
@@ -1389,16 +1399,6 @@ do_peer_close(struct sge_iq *iq, const struct rss_header *rss, struct mbuf *m)
 	}
 
 	tp->rcv_nxt = be32toh(cpl->rcv_nxt);
-
-	so = inp->inp_socket;
-	socantrcvmore(so);
-	if (ulp_mode(toep) == ULP_MODE_TCPDDP) {
-		DDP_LOCK(toep);
-		if (__predict_false(toep->ddp.flags &
-		    (DDP_BUF0_ACTIVE | DDP_BUF1_ACTIVE)))
-			handle_ddp_close(toep, tp, cpl->rcv_nxt);
-		DDP_UNLOCK(toep);
-	}
 
 	switch (tp->t_state) {
 	case TCPS_SYN_RECEIVED:

sys/dev/cxgbe/tom/t4_ddp.c
@@ -700,7 +700,8 @@ handle_ddp_close(struct toepcb *toep, struct tcpcb *tp, __be32 rcv_nxt)
 	INP_WLOCK_ASSERT(toep->inp);
 	DDP_ASSERT_LOCKED(toep);
 
-	len = be32toh(rcv_nxt) - tp->rcv_nxt;
+	/* - 1 is to ignore the byte for FIN */
+	len = be32toh(rcv_nxt) - tp->rcv_nxt - 1;
 	tp->rcv_nxt += len;
 
 	while (toep->ddp.active_count > 0) {