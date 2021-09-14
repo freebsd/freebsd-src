fbc42338395ba185ae859496d2750bf7e9976482
Alexander Motin <mav@FreeBSD.org>
2021-09-14 17:37:02 -0400

    mps/mpr(4): Move xpt_register_async() out of lock.
    It fixes lock ordere reversal between SIM and device locks.  Also
    remove registration for AC_FOUND_DEVICE, unused for a while now.
    
    MFC after:	1 month

--- sys/dev/mps/mps_sas.c
+++ sys/dev/mps/mps_sas.c
@@ -779,6 +779,8 @@ mps_attach_sas(struct mps_softc *sc)
 
 	callout_init(&sassc->discovery_callout, 1 /*mpsafe*/);
 
+	mps_unlock(sc);
+
 	/*
 	 * Register for async events so we can determine the EEDP
 	 * capabilities of devices.
@@ -811,8 +813,6 @@ mps_attach_sas(struct mps_softc *sc)
 		 */
 		mps_printf(sc, "EEDP capabilities disabled.\n");
 	}
-
-	mps_unlock(sc);
 
 	mpssas_register_events(sc);
 out:
@@ -847,18 +847,18 @@ mps_detach_sas(struct mps_softc *sc)
 	if (sassc->ev_tq != NULL)
 		taskqueue_free(sassc->ev_tq);
 
-	/* Make sure CAM doesn't wedge if we had to bail out early. */
-	mps_lock(sc);
-
-	while (sassc->startup_refcount != 0)
-		mpssas_startup_decrement(sassc);
-
 	/* Deregister our async handler */
 	if (sassc->path != NULL) {
 		xpt_register_async(0, mpssas_async, sc, sassc->path);
 		xpt_free_path(sassc->path);
 		sassc->path = NULL;
 	}
+
+	/* Make sure CAM doesn't wedge if we had to bail out early. */
+	mps_lock(sc);
+
+	while (sassc->startup_refcount != 0)
+		mpssas_startup_decrement(sassc);
 
 	if (sassc->flags & MPSSAS_IN_STARTUP)
 		xpt_release_simq(sassc->sim, 1);
@@ -3150,6 +3150,7 @@ mpssas_async(void *callback_arg, uint32_t code, struct cam_path *path,
 
 	sc = (struct mps_softc *)callback_arg;
 
+	mps_lock(sc);
 	switch (code) {
 	case AC_ADVINFO_CHANGED: {
 		struct mpssas_target *target;
@@ -3241,6 +3242,7 @@ mpssas_async(void *callback_arg, uint32_t code, struct cam_path *path,
 	default:
 		break;
 	}
+	mps_unlock(sc);
 }
 
 /*
