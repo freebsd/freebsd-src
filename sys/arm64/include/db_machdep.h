8753039a8f231ab04c7f3db9eaa5d9144d73440d
John Baldwin <jhb@FreeBSD.org>
2021-09-15 09:03:18 -0700

    arm64: Fix a logic bug in is_load_instr().
    Logical and ('&&') was used to join two conditions instead of logical
    or ('||') causing some store instructions to not be recognized.
    
    Reported by:	GCC 9 -Wparentheses
    
    Reviewed by:	andrew
    Differential Revision:	https://reviews.freebsd.org/D31949

--- sys/arm64/include/db_machdep.h
+++ sys/arm64/include/db_machdep.h
@@ -97,7 +97,7 @@ typedef long		db_expr_t;
 				  (((ins) & 0xffe00c00u) != 0x3c800000u)) ||  /* unscaled immediate */ \
 				 ((((ins) & 0x3b000000u) == 0x39000000u) && \
 				  (((ins) & 0x3bc00000u) != 0x39000000u) && \
-				  (((ins) & 0xffc00000u) != 0x3d800000u)) &&  /* unsigned immediate */ \
+				  (((ins) & 0xffc00000u) != 0x3d800000u)) ||  /* unsigned immediate */ \
 				 (((ins) & 0x3bc00000u) == 0x28400000u) || /* pair (offset) */ \
 				 (((ins) & 0x3bc00000u) == 0x28c00000u) || /* pair (post-indexed) */ \
 				 (((ins) & 0x3bc00000u) == 0x29800000u)) /* pair (pre-indexed) */
