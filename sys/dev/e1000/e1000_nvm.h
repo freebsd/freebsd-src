3f45d625bd22964c5e7d6605d08f57d6de5a4942
Kevin Bowling <kbowling@FreeBSD.org>
2021-09-16 19:30:49 -0700

    e1000: Catch up commit with DPDK
    Various syncs with the e1000 shared code from DPDK:
    "cid-gigabit.2020.06.05.tar.gz released by ND"
    
    Approved by:	imp
    Obtained from:	DPDK
    MFC after:	1 week
    
    (cherry picked from commit 984d1616be883bc2c351aff9aa69b1abd7d1214c)

--- sys/dev/e1000/e1000_nvm.h
+++ sys/dev/e1000/e1000_nvm.h
@@ -41,6 +41,22 @@ struct e1000_pba {
 	u16 *pba_block;
 };
 
+struct e1000_fw_version {
+	u32 etrack_id;
+	u16 eep_major;
+	u16 eep_minor;
+	u16 eep_build;
+
+	u8 invm_major;
+	u8 invm_minor;
+	u8 invm_img_type;
+
+	bool or_valid;
+	u16 or_major;
+	u16 or_build;
+	u16 or_patch;
+};
+
 
 void e1000_init_nvm_ops_generic(struct e1000_hw *hw);
 s32  e1000_null_read_nvm(struct e1000_hw *hw, u16 a, u16 b, u16 *c);
@@ -51,6 +67,7 @@ s32  e1000_acquire_nvm_generic(struct e1000_hw *hw);
 
 s32  e1000_poll_eerd_eewr_done(struct e1000_hw *hw, int ee_reg);
 s32  e1000_read_mac_addr_generic(struct e1000_hw *hw);
+s32  e1000_read_pba_num_generic(struct e1000_hw *hw, u32 *pba_num);
 s32  e1000_read_pba_string_generic(struct e1000_hw *hw, u8 *pba_num,
 				   u32 pba_num_size);
 s32  e1000_read_pba_length_generic(struct e1000_hw *hw, u32 *pba_num_size);
@@ -75,6 +92,8 @@ s32  e1000_write_nvm_spi(struct e1000_hw *hw, u16 offset, u16 words,
 s32  e1000_update_nvm_checksum_generic(struct e1000_hw *hw);
 void e1000_stop_nvm(struct e1000_hw *hw);
 void e1000_release_nvm_generic(struct e1000_hw *hw);
+void e1000_get_fw_version(struct e1000_hw *hw,
+			  struct e1000_fw_version *fw_vers);
 
 #define E1000_STM_OPCODE	0xDB00
 
