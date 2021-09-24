9499d3c1e40dfeb1f63f61af7cdf25ee27f9a2ec
Konstantin Belousov <kib@FreeBSD.org>
2021-09-24 03:14:56 +0300

    aio_aqueue(): avoid ucred leak on failure path
    PR:	258698
    
    (cherry picked from commit 45c2c7c484de7747014492b17ff89e323ee66496)

--- sys/kern/vfs_aio.c
+++ sys/kern/vfs_aio.c
@@ -1709,7 +1709,7 @@ no_kqueue:
 	else
 		error = fo_aio_queue(fp, job);
 	if (error)
-		goto err3;
+		goto err4;
 
 	AIO_LOCK(ki);
 	job->jobflags &= ~KAIOCB_QUEUEING;
@@ -1730,6 +1730,8 @@ no_kqueue:
 	AIO_UNLOCK(ki);
 	return (0);
 
+err4:
+	crfree(job->cred);
 err3:
 	if (fp)
 		fdrop(fp, td);
