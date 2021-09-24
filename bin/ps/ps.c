a4bd2216fb5250cee32ed9eb4090fc518018a26c
Math Ieu <sigsys@gmail.com>
2021-09-24 08:58:40 +0200

    ps: fix `ps -aa`
    Passing the -a flag multiple times made ps show no processes.
    
    Differential Revision:	https://reviews.freebsd.org/D27215
    
    (cherry picked from commit bf27a2253f6ba6ee3086046b837b2fe40e22c72c)

--- bin/ps/ps.c
+++ bin/ps/ps.c
@@ -253,11 +253,9 @@ main(int argc, char *argv[])
 			 * added for compatibility with SUSv3, but for
 			 * now it will not be described in the man page.
 			 */
-			nselectors++;
 			all = xkeep = 1;
 			break;
 		case 'a':
-			nselectors++;
 			all = 1;
 			break;
 		case 'C':
@@ -473,7 +471,7 @@ main(int argc, char *argv[])
 	if (!_fmt)
 		parsefmt(dfmt, 0);
 
-	if (nselectors == 0) {
+	if (!all && nselectors == 0) {
 		uidlist.l.ptr = malloc(sizeof(uid_t));
 		if (uidlist.l.ptr == NULL)
 			xo_errx(1, "malloc failed");
@@ -525,9 +523,6 @@ main(int argc, char *argv[])
 		} else if (uidlist.count == 1) {
 			what = KERN_PROC_UID | showthreads;
 			flag = *uidlist.l.uids;
-			nselectors = 0;
-		} else if (all) {
-			/* No need for this routine to select processes. */
 			nselectors = 0;
 		}
 	}
