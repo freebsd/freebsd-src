fd6adf784e862d043d1858aab323f3fb136cf706
Vladimir Kondratyev <wulf@FreeBSD.org>
2021-09-29 23:12:36 +0300

    LinuxKPI: Remove FreeBSD struct resource from all LKPI headers
    except linux/pci.h to avoid conflicts with Linux version.
    This allows to #define resource in drm-kmod globally and strip some #ifdef-s
    
    Reviewed by:	hselasky, manu
    Differential revision:	https://reviews.freebsd.org/D31673
    
    (cherry picked from commit 66ea390652d2ede405b43c168157986bd2b52bb9)

--- sys/compat/linuxkpi/common/include/linux/interrupt.h
+++ sys/compat/linuxkpi/common/include/linux/interrupt.h
@@ -38,111 +38,31 @@
 #include <linux/hardirq.h>
 
 #include <sys/param.h>
-#include <sys/bus.h>
-#include <sys/rman.h>
 #include <sys/interrupt.h>
 
 typedef	irqreturn_t	(*irq_handler_t)(int, void *);
 
 #define	IRQF_SHARED	RF_SHAREABLE
 
-struct irq_ent {
-	struct list_head	links;
-	struct device	*dev;
-	struct resource	*res;
-	void		*arg;
-	irqreturn_t	(*handler)(int, void *);
-	void		*tag;
-	unsigned int	irq;
-
-	/* XXX All new entries must be after this in stable/13 */
-	irqreturn_t	(*thread_handler)(int, void *);
-};
+struct irq_ent;
 
 void linux_irq_handler(void *);
 void lkpi_devm_irq_release(struct device *, void *);
 void lkpi_irq_release(struct device *, struct irq_ent *);
-
-static inline int
-linux_irq_rid(struct device *dev, unsigned int irq)
-{
-	/* check for MSI- or MSIX- interrupt */
-	if (irq >= dev->irq_start && irq < dev->irq_end)
-		return (irq - dev->irq_start + 1);
-	else
-		return (0);
-}
-
-static inline struct irq_ent *
-linux_irq_ent(struct device *dev, unsigned int irq)
-{
-	struct irq_ent *irqe;
-
-	list_for_each_entry(irqe, &dev->irqents, links)
-		if (irqe->irq == irq)
-			return (irqe);
-
-	return (NULL);
-}
-
-static inline int
-_request_irq(struct device *xdev, unsigned int irq,
-    irq_handler_t handler, irq_handler_t thread_handler,
-    unsigned long flags, const char *name, void *arg)
-{
-	struct resource *res;
-	struct irq_ent *irqe;
-	struct device *dev;
-	int error;
-	int rid;
-
-	dev = linux_pci_find_irq_dev(irq);
-	if (dev == NULL)
-		return -ENXIO;
-	if (xdev != NULL && xdev != dev)
-		return -ENXIO;
-	rid = linux_irq_rid(dev, irq);
-	res = bus_alloc_resource_any(dev->bsddev, SYS_RES_IRQ, &rid,
-	    flags | RF_ACTIVE);
-	if (res == NULL)
-		return (-ENXIO);
-	if (xdev != NULL)
-		irqe = lkpi_devres_alloc(lkpi_devm_irq_release, sizeof(*irqe),
-		    GFP_KERNEL | __GFP_ZERO);
-	else
-		irqe = kzalloc(sizeof(*irqe), GFP_KERNEL);
-	irqe->dev = dev;
-	irqe->res = res;
-	irqe->arg = arg;
-	irqe->handler = handler;
-	irqe->thread_handler = thread_handler;
-	irqe->irq = irq;
-
-	error = bus_setup_intr(dev->bsddev, res, INTR_TYPE_NET | INTR_MPSAFE,
-	    NULL, linux_irq_handler, irqe, &irqe->tag);
-	if (error)
-		goto errout;
-	list_add(&irqe->links, &dev->irqents);
-	if (xdev != NULL)
-		devres_add(xdev, irqe);
-
-	return 0;
-
-errout:
-	bus_release_resource(dev->bsddev, SYS_RES_IRQ, rid, irqe->res);
-	if (xdev != NULL)
-		devres_free(irqe);
-	else
-		kfree(irqe);
-	return (-error);
-}
+int  lkpi_request_irq(struct device *, unsigned int, irq_handler_t,
+	irq_handler_t, unsigned long, const char *, void *);
+int  lkpi_enable_irq(unsigned int);
+void lkpi_disable_irq(unsigned int);
+int  lkpi_bind_irq_to_cpu(unsigned int, int);
+void lkpi_free_irq(unsigned int, void *);
+void lkpi_devm_free_irq(struct device *, unsigned int, void *);
 
 static inline int
 request_irq(unsigned int irq, irq_handler_t handler, unsigned long flags,
     const char *name, void *arg)
 {
 
-	return (_request_irq(NULL, irq, handler, NULL, flags, name, arg));
+	return (lkpi_request_irq(NULL, irq, handler, NULL, flags, name, arg));
 }
 
 static inline int
@@ -151,7 +71,7 @@ request_threaded_irq(int irq, irq_handler_t handler,
     const char *name, void *arg)
 {
 
-	return (_request_irq(NULL, irq, handler, thread_handler,
+	return (lkpi_request_irq(NULL, irq, handler, thread_handler,
 	    flags, name, arg));
 }
 
@@ -161,94 +81,38 @@ devm_request_threaded_irq(struct device *dev, int irq,
     unsigned long flags, const char *name, void *arg)
 {
 
-	return (_request_irq(dev, irq, handler, thread_handler,
+	return (lkpi_request_irq(dev, irq, handler, thread_handler,
 	    flags, name, arg));
 }
 
 static inline int
 enable_irq(unsigned int irq)
 {
-	struct irq_ent *irqe;
-	struct device *dev;
-
-	dev = linux_pci_find_irq_dev(irq);
-	if (dev == NULL)
-		return -EINVAL;
-	irqe = linux_irq_ent(dev, irq);
-	if (irqe == NULL || irqe->tag != NULL)
-		return -EINVAL;
-	return -bus_setup_intr(dev->bsddev, irqe->res, INTR_TYPE_NET | INTR_MPSAFE,
-	    NULL, linux_irq_handler, irqe, &irqe->tag);
+	return (lkpi_enable_irq(irq));
 }
 
 static inline void
 disable_irq(unsigned int irq)
 {
-	struct irq_ent *irqe;
-	struct device *dev;
-
-	dev = linux_pci_find_irq_dev(irq);
-	if (dev == NULL)
-		return;
-	irqe = linux_irq_ent(dev, irq);
-	if (irqe == NULL)
-		return;
-	if (irqe->tag != NULL)
-		bus_teardown_intr(dev->bsddev, irqe->res, irqe->tag);
-	irqe->tag = NULL;
+	lkpi_disable_irq(irq);
 }
 
 static inline int
 bind_irq_to_cpu(unsigned int irq, int cpu_id)
 {
-	struct irq_ent *irqe;
-	struct device *dev;
-
-	dev = linux_pci_find_irq_dev(irq);
-	if (dev == NULL)
-		return (-ENOENT);
-
-	irqe = linux_irq_ent(dev, irq);
-	if (irqe == NULL)
-		return (-ENOENT);
-
-	return (-bus_bind_intr(dev->bsddev, irqe->res, cpu_id));
+	return (lkpi_bind_irq_to_cpu(irq, cpu_id));
 }
 
 static inline void
-free_irq(unsigned int irq, void *device __unused)
+free_irq(unsigned int irq, void *device)
 {
-	struct irq_ent *irqe;
-	struct device *dev;
-
-	dev = linux_pci_find_irq_dev(irq);
-	if (dev == NULL)
-		return;
-	irqe = linux_irq_ent(dev, irq);
-	if (irqe == NULL)
-		return;
-	lkpi_irq_release(dev, irqe);
-	kfree(irqe);
+	lkpi_free_irq(irq, device);
 }
 
 static inline void
 devm_free_irq(struct device *xdev, unsigned int irq, void *p)
 {
-	struct device *dev;
-	struct irq_ent *irqe;
-
-	dev = linux_pci_find_irq_dev(irq);
-	if (dev == NULL)
-		return;
-	if (xdev != dev)
-		return;
-	irqe = linux_irq_ent(dev, irq);
-	if (irqe == NULL)
-		return;
-	lkpi_irq_release(dev, irqe);
-	lkpi_devres_unlink(dev, irqe);
-	lkpi_devres_free(irqe);
-	return;
+	lkpi_devm_free_irq(xdev, irq, p);
 }
 
 static inline int
