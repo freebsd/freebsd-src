0e89914326ec11d4c1685e8ecb14f6349ea8f591
Mateusz Guzik <mjg@FreeBSD.org>
2021-10-01 23:13:38 +0200

    vfs: hoist cn_thread assert in namei
    Making it condtional on whether ktrace happens to be enabled makes no
    sense.
    
    (cherry picked from commit c9536389d732feecf61afa4632397d67b04b2ab4)

--- sys/kern/vfs_lookup.c
+++ sys/kern/vfs_lookup.c
@@ -556,6 +556,8 @@ namei(struct nameidata *ndp)
 	cnp = &ndp->ni_cnd;
 	td = cnp->cn_thread;
 #ifdef INVARIANTS
+	KASSERT(cnp->cn_thread == curthread,
+	    ("namei not using curthread"));
 	KASSERT((ndp->ni_debugflags & NAMEI_DBG_CALLED) == 0,
 	    ("%s: repeated call to namei without NDREINIT", __func__));
 	KASSERT(ndp->ni_debugflags == NAMEI_DBG_INITED,
@@ -610,8 +612,6 @@ namei(struct nameidata *ndp)
 
 #ifdef KTRACE
 	if (KTRPOINT(td, KTR_NAMEI)) {
-		KASSERT(cnp->cn_thread == curthread,
-		    ("namei not using curthread"));
 		ktrnamei(cnp->cn_pnbuf);
 	}
 #endif
