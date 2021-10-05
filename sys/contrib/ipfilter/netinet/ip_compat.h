a6443be0b641fcf0903ea149f8fb1ed6ddb6c2d0
Cy Schubert <cy@FreeBSD.org>
2021-10-04 21:16:16 -0700

    ipfilter: Whitespace cleanup
    Clean up whitespace from compaction of DTn macro definitions.
    
    MFC after:	3 days

--- sys/contrib/ipfilter/netinet/ip_compat.h
+++ sys/contrib/ipfilter/netinet/ip_compat.h
@@ -1222,12 +1222,12 @@ typedef	struct	tcpiphdr	tcpiphdr_t;
 #endif
 
 #if defined(DTRACE_PROBE) && defined(_KERNEL)
-#  define	DT(_n)			DTRACE_PROBE(_n)
-#  define	DT1(_n,_a,_b)		DTRACE_PROBE1(_n,_a,_b)
-#  define	DT2(_n,_a,_b,_c,_d)	DTRACE_PROBE2(_n,_a,_b,_c,_d)
-#  define	DT3(_n,_a,_b,_c,_d,_e,_f)	\
+# define	DT(_n)			DTRACE_PROBE(_n)
+# define	DT1(_n,_a,_b)		DTRACE_PROBE1(_n,_a,_b)
+# define	DT2(_n,_a,_b,_c,_d)	DTRACE_PROBE2(_n,_a,_b,_c,_d)
+# define	DT3(_n,_a,_b,_c,_d,_e,_f)	\
 					DTRACE_PROBE3(_n,_a,_b,_c,_d,_e,_f)
-#  define	DT4(_n,_a,_b,_c,_d,_e,_f,_g,_h) \
+# define	DT4(_n,_a,_b,_c,_d,_e,_f,_g,_h) \
 				DTRACE_PROBE4(_n,_a,_b,_c,_d,_e,_f,_g,_h)
 #else
 # define	DT(_n)
