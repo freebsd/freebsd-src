812c2500ebe9607d74261434b671847c30d8d0e1
Corvin Köhne <corvink@FreeBSD.org>
2021-10-07 15:54:46 +0200

    bhyve: add helper struct for acpi device handling
    To simplify the handling of different acpi devices like qemu fwcfg or a
    tpm, add a helper struct. It will handle the reporting of acpi
    resources.
    
    Reviewed by:		markj
    MFC after:		1 week
    Sponsored by:		Beckhoff Automation GmbH & Co. KG
    Differential Revision:	https://reviews.freebsd.org/D38327

---
+++ usr.sbin/bhyve/acpi_device.h
@@ -0,0 +1,31 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2021 Beckhoff Automation GmbH & Co. KG
+ * Author: Corvin Köhne <c.koehne@beckhoff.com>
+ */
+
+#pragma once
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#include <contrib/dev/acpica/include/acpi.h>
+#pragma GCC diagnostic pop
+
+struct vmctx;
+
+struct acpi_device;
+
+/**
+ * Creates an ACPI device.
+ *
+ * @param[out] new_dev Returns the newly create ACPI device.
+ * @param[in]  vm_ctx  VM context the ACPI device is created in.
+ * @param[in]  name    Name of the ACPI device. Should always be a NULL
+ *                     terminated string.
+ * @param[in]  hid     Hardware ID of the ACPI device. Should always be a NULL
+ *                     terminated string.
+ */
+int acpi_device_create(struct acpi_device **const new_dev,
+    struct vmctx *const vm_ctx, const char *const name, const char *const hid);
+void acpi_device_destroy(struct acpi_device *const dev);
