c0dacdb60b07b0d0d38be45ebc17690990629549
Warner Losh <imp@FreeBSD.org>
2021-10-10 10:21:49 -0600

    cross-build: Remove redundant lines.
    There's two identical gid_from_group lines in a row. We only need one.
    Ditto for uid_from_user().
    
    Sponsored by:		Netflix
    Reviewed by:		bapt, jrtc27
    Differential Revision:	https://reviews.freebsd.org/D32442

--- tools/build/cross-build/include/common/pwd.h
+++ tools/build/cross-build/include/common/pwd.h
@@ -46,8 +46,6 @@ int pwcache_userdb(int (*a_setpassent)(int), void (*a_endpwent)(void),
     struct passwd *(*a_getpwuid)(uid_t));
 
 int uid_from_user(const char *name, uid_t *uid);
-
-int uid_from_user(const char *name, uid_t *uid);
 const char *user_from_uid(uid_t uid, int noname);
 
 #ifdef __linux__
