120d808ef90e2c709dad934187f3a9ab925be3dd
Konstantin Belousov <kib@FreeBSD.org>
2021-10-12 01:35:56 +0300

    ldd: do not use dlopen(RTLD_TRACE) for dso when format is specified
    Problem is that rtld cannot reliably access updated environment.
    This was made more obvious by 805073f7848c12cb26.  The application
    environment can be in arbitrary state and place, system components
    can observe it only during execve(2), or in case of rtld, right after
    execve, when environment is still at know location and format.
    
    Instead spawn ld-elf.so.1 in direct exec mode which can correctly read
    all inherited updates to the environment.
    
    PR:	259069
    Reviewed by:	arichardson, jhb
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D32464

--- usr.bin/ldd/ldd.c
+++ usr.bin/ldd/ldd.c
@@ -46,6 +46,7 @@ __FBSDID("$FreeBSD$");
 #include <fcntl.h>
 #include <gelf.h>
 #include <libelf.h>
+#include <rtld_paths.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -134,6 +135,7 @@ int
 main(int argc, char *argv[])
 {
 	char *fmt1, *fmt2;
+	const char *rtld;
 	int aflag, c, fd, rval, status, is_shlib, rv, type;
 
 	aflag = 0;
@@ -234,9 +236,17 @@ main(int argc, char *argv[])
 			if (is_shlib == 0) {
 				execl(*argv, *argv, (char *)NULL);
 				warn("%s", *argv);
-			} else {
+			} else if (fmt1 == NULL && fmt2 == NULL) {
 				dlopen(*argv, RTLD_TRACE);
 				warnx("%s: %s", *argv, dlerror());
+			} else {
+				rtld = _PATH_RTLD;
+#if __ELF_WORD_SIZE > 32 && defined(ELF32_SUPPORTED)
+				if (type == TYPE_ELF32)
+					rtld = _COMPAT32_PATH_RTLD;
+#endif
+				execl(rtld, rtld, "-d", "--",
+				    *argv, (char *)NULL);
 			}
 			_exit(1);
 		}
