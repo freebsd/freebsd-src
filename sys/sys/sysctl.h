532d925b6f02167132edb7b1715836041a6e46fc
Dawid Gorecki <dgr@semihalf.com>
2021-10-13 21:03:37 +0200

    kern_exec: Add kern.stacktop sysctl.
    With stack gap enabled top of the stack is moved down by a random
    amount of bytes. Because of that some multithreaded applications
    which use kern.usrstack sysctl to calculate address of stacks for
    their threads can fail. Add kern.stacktop sysctl, which can be used
    to retrieve address of the stack after stack gap is applied to it.
    Returns value identical to kern.usrstack for processes which have
    no stack gap.
    
    Reviewed by: kib
    Obtained from: Semihalf
    Sponsored by: Stormshield
    MFC after: 1 month
    Differential Revision: https://reviews.freebsd.org/D31897
    
    (cherry picked from commit a97d697122da2bfb0baae5f0939d118d119dae33)

--- sys/sys/sysctl.h
+++ sys/sys/sysctl.h
@@ -982,6 +982,7 @@ TAILQ_HEAD(sysctl_ctx_list, sysctl_ctx_entry);
 #define	KERN_HOSTUUID		36	/* string: host UUID identifier */
 #define	KERN_ARND		37	/* int: from arc4rand() */
 #define	KERN_MAXPHYS		38	/* int: MAXPHYS value */
+#define	KERN_STACKTOP		39	/* int: USRSTACK - stack gap */
 /*
  * KERN_PROC subtypes
  */
