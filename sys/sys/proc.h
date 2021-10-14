86929782cfab2664010a31979e9e3c0626721ebb
Konstantin Belousov <kib@FreeBSD.org>
2021-10-14 23:03:36 +0300

    Fix typo in comment
    Sponsored by:	The FreeBSD Foundation
    MFC after:	3 days

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -307,7 +307,7 @@ struct thread {
 	struct osd	td_osd;		/* (k) Object specific data. */
 	struct vm_map_entry *td_map_def_user; /* (k) Deferred entries. */
 	pid_t		td_dbg_forked;	/* (c) Child pid for debugger. */
-	struct vnode	*td_vp_reserved;/* (k) Prealloated vnode. */
+	struct vnode	*td_vp_reserved;/* (k) Preallocated vnode. */
 	u_int		td_no_sleeping;	/* (k) Sleeping disabled count. */
 	void		*td_su;		/* (k) FFS SU private */
 	sbintime_t	td_sleeptimo;	/* (t) Sleep timeout. */
