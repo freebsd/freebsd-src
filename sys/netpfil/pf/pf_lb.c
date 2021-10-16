915fbd323828b26adea9da8f6ba2fce542e7312a
Kristof Provost <kp@FreeBSD.org>
2021-10-16 09:32:15 +0200

    pf: selecting pf_map_addr is not an error
    When a redirection/nat IP address is selected by pf_map_addr it is
    logged with PF_DEBUG_MISC level. This one according to the manual means
    "Generate debug messages for various errors". Selecting an IP address is
    not an error, it's a normal function of pf for route-to, nat and some
    other operations. Therefore PF_DEBUG_NOISY level should be choosen which
    is means "Generate debug messages for common conditions".
    
    PR:		259184
    Submitted by:	Kajetan Staszkiewicz <vegeta tuxpowered.net>
    Sponsored by:	InnoGames GmbH

--- sys/netpfil/pf/pf_lb.c
+++ sys/netpfil/pf/pf_lb.c
@@ -364,7 +364,7 @@ pf_map_addr(sa_family_t af, struct pf_krule *r, struct pf_addr *saddr,
 			return (1);
 
 		PF_ACPY(naddr, &(*sn)->raddr, af);
-		if (V_pf_status.debug >= PF_DEBUG_MISC) {
+		if (V_pf_status.debug >= PF_DEBUG_NOISY) {
 			printf("pf_map_addr: src tracking maps ");
 			pf_print_host(saddr, 0, af);
 			printf(" to ");
@@ -539,7 +539,7 @@ pf_map_addr(sa_family_t af, struct pf_krule *r, struct pf_addr *saddr,
 	if (*sn != NULL)
 		PF_ACPY(&(*sn)->raddr, naddr, af);
 
-	if (V_pf_status.debug >= PF_DEBUG_MISC &&
+	if (V_pf_status.debug >= PF_DEBUG_NOISY &&
 	    (rpool->opts & PF_POOL_TYPEMASK) != PF_POOL_NONE) {
 		printf("pf_map_addr: selected address ");
 		pf_print_host(naddr, 0, af);
