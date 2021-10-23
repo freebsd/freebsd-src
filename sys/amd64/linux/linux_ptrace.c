f4fd475d457da5e09117c7caba3c15baca2458e4
Edward Tomasz Napierala <trasz@FreeBSD.org>
2021-10-23 19:53:12 +0100

    linux: Improve debug for PTRACE_GETEVENTMSG
    No functional changes.
    
    Sponsored By:	EPSRC
    
    (cherry picked from commit 2ec26ae402fc2589cb97bb5aa713b6d0ba080c17)

--- sys/amd64/linux/linux_ptrace.c
+++ sys/amd64/linux/linux_ptrace.c
@@ -67,6 +67,7 @@ __FBSDID("$FreeBSD$");
 #define	LINUX_PTRACE_DETACH		17
 #define	LINUX_PTRACE_SYSCALL		24
 #define	LINUX_PTRACE_SETOPTIONS		0x4200
+#define	LINUX_PTRACE_GETEVENTMSG	0x4201
 #define	LINUX_PTRACE_GETSIGINFO		0x4202
 #define	LINUX_PTRACE_GETREGSET		0x4204
 #define	LINUX_PTRACE_SEIZE		0x4206
@@ -428,6 +429,14 @@ linux_ptrace_setoptions(struct thread *td, pid_t pid, l_ulong data)
 }
 
 static int
+linux_ptrace_geteventmsg(struct thread *td, pid_t pid, l_ulong data)
+{
+
+	linux_msg(td, "PTRACE_GETEVENTMSG not implemented; returning EINVAL");
+	return (EINVAL);
+}
+
+static int
 linux_ptrace_getsiginfo(struct thread *td, pid_t pid, l_ulong data)
 {
 	struct ptrace_lwpinfo lwpinfo;
@@ -771,6 +780,9 @@ linux_ptrace(struct thread *td, struct linux_ptrace_args *uap)
 		break;
 	case LINUX_PTRACE_SETOPTIONS:
 		error = linux_ptrace_setoptions(td, pid, uap->data);
+		break;
+	case LINUX_PTRACE_GETEVENTMSG:
+		error = linux_ptrace_geteventmsg(td, pid, uap->data);
 		break;
 	case LINUX_PTRACE_GETSIGINFO:
 		error = linux_ptrace_getsiginfo(td, pid, uap->data);
