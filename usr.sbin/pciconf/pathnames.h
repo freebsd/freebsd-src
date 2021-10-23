5dbf622625cbfa8b36ff1d95c59514201aa35690
ladec04 <93056094+ladec04@users.noreply.github.com>
2021-10-23 22:24:00 +0200

    Delete usr.sbin directory

--- usr.sbin/pciconf/pathnames.h
+++
@@ -1,6 +0,0 @@
-/* $FreeBSD$ */
-#include <paths.h>
-
-#define	_PATH_DEVPCI	"/dev/pci"
-#define	_PATH_PCIVDB	"/usr/share/misc/pci_vendors"
-#define	_PATH_LPCIVDB	_PATH_LOCALBASE "/share/pciids/pci.ids"
