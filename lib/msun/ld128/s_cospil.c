a49617abb0c0dc763712fd70fae8bbd66ecca835
Steve Kargl <kargl@FreeBSD.org>
2021-10-25 16:13:52 +0300

    [LIBM] implementations of sinpi[fl], cospi[fl], and tanpi[fl]
    PR:	218514
    
    (cherry picked from commit dce5f3abed7181cc533ca5ed3de44517775e78dd)

---
+++ lib/msun/ld128/s_cospil.c
@@ -0,0 +1,109 @@
+/*-
+ * Copyright (c) 2017 Steven G. Kargl
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * See ../src/s_cospi.c for implementation details.
+ *
+ * FIXME:  This has not been compiled nor has it been tested for accuracy.
+ * FIXME:  This should use bit twiddling.
+ */
+
+#include "math.h"
+#include "math_private.h"
+
+/*
+ * pi_hi contains the leading 56 bits of a 169 bit approximation for pi.
+ */
+static const long double
+pi_hi = 3.14159265358979322702026593105983920e+00L,
+pi_lo = 1.14423774522196636802434264184180742e-17L;
+
+#include "k_cospil.h"
+#include "k_sinpil.h"
+
+volatile static const double vzero = 0;
+
+long double
+cospil(long double x)
+{
+	long double ax, c, xf;
+	uint32_t ix;
+
+	ax = fabsl(x);
+
+	if (ax < 1) {
+		if (ax < 0.25) {
+			if (ax < 0x1p-60) {
+				if ((int)x == 0)
+					return (1);
+			}
+			return (__kernel_cospil(ax));
+		}
+
+		if (ax < 0.5)
+			c = __kernel_sinpil(0.5 - ax);
+		else if (ax < 0.75) {
+			if (ax == 0.5)
+				return (0);
+			c = -__kernel_sinpil(ax - 0.5);
+		} else
+			c = -__kernel_cospil(1 - ax);
+		return (c);
+	}
+
+	if (ax < 0x1p112) {
+		xf = floorl(ax);
+		ax -= xf;
+		if (x < 0.5) {
+			if (x < 0.25)
+				c = ax == 0 ? 1 : __kernel_cospil(ax);
+			else
+				c = __kernel_sinpil(0.5 - ax);
+		} else {
+			if (x < 0.75) {
+				if (ax == 0.5)
+					return (0);
+				c = -__kernel_sinpil(ax - 0.5);
+			} else
+				c = -__kernel_cospil(1 - ax);
+		}
+
+		if (xf > 0x1p50)
+			xf -= 0x1p50;
+		if (xf > 0x1p30)
+			xf -= 0x1p30;
+		ix = (uint32_t)xf;
+		return (ix & 1 ? -c : c);
+	}
+
+	if (isinf(x) || isnan(x))
+		return (vzero / vzero);
+
+	/*
+	 * |x| >= 0x1p112 is always an even integer, so return 1.
+	 */
+	return (1);
+}
---
+++ lib/msun/ld80/s_sinpil.c
@@ -0,0 +1,140 @@
+/*-
+ * Copyright (c) 2017 Steven G. Kargl
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * See ../src/s_sinpi.c for implementation details.
+ */
+
+#ifdef __i386__
+#include <ieeefp.h>
+#endif
+#include <stdint.h>
+
+#include "fpmath.h"
+#include "math.h"
+#include "math_private.h"
+
+static const union IEEEl2bits
+pi_hi_u = LD80C(0xc90fdaa200000000,   1, 3.14159265346825122833e+00L),
+pi_lo_u = LD80C(0x85a308d313198a2e, -33, 1.21542010130123852029e-10L);
+#define	pi_hi	(pi_hi_u.e)
+#define	pi_lo	(pi_lo_u.e)
+
+#include "k_cospil.h"
+#include "k_sinpil.h"
+
+volatile static const double vzero = 0;
+
+long double
+sinpil(long double x)
+{
+	long double ax, hi, lo, s;
+	uint64_t lx, m;
+	uint32_t j0;
+	uint16_t hx, ix;
+
+	EXTRACT_LDBL80_WORDS(hx, lx, x);
+	ix = hx & 0x7fff;
+	INSERT_LDBL80_WORDS(ax, ix, lx);
+
+	ENTERI();
+
+	if (ix < 0x3fff) {			/* |x| < 1 */
+		if (ix < 0x3ffd) {		/* |x| < 0.25 */
+			if (ix < 0x3fdd) {	/* |x| < 0x1p-34 */
+				if (x == 0)
+					RETURNI(x);
+				INSERT_LDBL80_WORDS(hi, hx,
+				    lx & 0xffffffff00000000ull);
+				hi *= 0x1p63L;
+				lo = x * 0x1p63L - hi;
+				s = (pi_lo + pi_hi) * lo + pi_lo * hi +
+				    pi_hi * hi;
+				RETURNI(s * 0x1p-63L);
+			}
+			s = __kernel_sinpil(ax);
+			RETURNI((hx & 0x8000) ? -s : s);
+		}
+
+		if (ix < 0x3ffe)			/* |x| < 0.5 */
+			s = __kernel_cospil(0.5 - ax);
+		else if (lx < 0xc000000000000000ull)	/* |x| < 0.75 */
+			s = __kernel_cospil(ax - 0.5);
+		else
+			s = __kernel_sinpil(1 - ax);
+		RETURNI((hx & 0x8000) ? -s : s);
+	}
+
+	if (ix < 0x403e) {		/* 1 <= |x| < 0x1p63 */
+		/* Determine integer part of ax. */
+		j0 = ix - 0x3fff + 1;
+		if (j0 < 32) {
+			lx = (lx >> 32) << 32;
+			lx &= ~(((lx << 32)-1) >> j0);
+		} else {
+			m = (uint64_t)-1 >> (j0 + 1);
+			if (lx & m) lx &= ~m;
+		}
+		INSERT_LDBL80_WORDS(x, ix, lx);
+
+		ax -= x;
+		EXTRACT_LDBL80_WORDS(ix, lx, ax);
+
+		if (ix == 0) {
+			s = 0;
+		} else {
+			if (ix < 0x3ffe) {		/* |x| < 0.5 */
+				if (ix < 0x3ffd)	/* |x| < 0.25 */
+					s = __kernel_sinpil(ax);
+				else 
+					s = __kernel_cospil(0.5 - ax);
+			} else {
+							/* |x| < 0.75 */
+				if (lx < 0xc000000000000000ull)
+					s = __kernel_cospil(ax - 0.5);
+				else
+					s = __kernel_sinpil(1 - ax);
+			}
+
+			if (j0 > 40)
+				x -= 0x1p40;
+			if (j0 > 30)
+				x -= 0x1p30;
+			j0 = (uint32_t)x;
+			if (j0 & 1) s = -s;
+		}
+		RETURNI((hx & 0x8000) ? -s : s);
+	}
+
+	/* x = +-inf or nan. */
+	if (ix >= 0x7fff)
+		RETURNI(vzero / vzero);
+
+	/*
+	 * |x| >= 0x1p63 is always an integer, so return +-0.
+	 */
+	RETURNI(copysignl(0, x));
+}
