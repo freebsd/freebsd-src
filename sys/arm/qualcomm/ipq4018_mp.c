685b9db334f4a087e685c6ccd2e286a21f78afb0
Adrian Chadd <adrian@FreeBSD.org>
2021-10-29 21:27:02 -0700

    ipq401x: add MP core start-up path for the CPU regulator/clock gate used
    This code implements the "kpssv2" flavour of CPU regulator/clock gating
    in Linux.  It's used by at least the ipq4018/4019 to power on and off
    CPU cores.
    
    This is based on the Linux implementation - the register definitions
    and values are from Linux and I've reverse engineered the sequencing
    requirements.
    
    The MP bring-up is:
    
    * set cold boot address via an SCM call - this is the address used
      by the bootloader/TZ firmware to jump to when the CPUs boot
    
    * power down the LDO feeding the CPU core and wait for it to settle
    
    * program in the right set of LDO and power tree configuration for
      the CPU regulator to power up the core.  Unfortunately these are
      magic numbers that I've not found documented anywhere.
    
    * (I think) power up the shared L2 cache connect if it isn't.
    
    * Clamp the power into the core down; put the core into reset
    
    * Unclamp the power rail; release reset; and then set the core to boot.
    
    The MP core will then boot the bootloader/TZ firmware and then
    will wait until an incoming interrupt kicks it to start @ mpentry.
    
    Tested:
    
    * IPQ4019, 4 CPUs
    
    Release APs
    CPU(3) applied BP hardening: not necessary
    CPU(1) applied BP hardening: not necessary
    CPU(2) applied BP hardening: not necessary
    
    Reviewed by: andrew, manu, imp
    Differential Revision: https://reviews.freebsd.org/D32723

--- sys/arm/qualcomm/ipq4018_mp.c
+++ sys/arm/qualcomm/ipq4018_mp.c
@@ -35,27 +35,82 @@ __FBSDID("$FreeBSD$");
 #include <sys/bus.h>
 #include <sys/reboot.h>
 #include <sys/devmap.h>
+#include <sys/smp.h>
 
 #include <vm/vm.h>
 
+#include <machine/cpu.h>
 #include <machine/bus.h>
 #include <machine/intr.h>
 #include <machine/machdep.h>
 #include <machine/platformvar.h>
+#include <machine/smp.h>
 
 #include <dev/fdt/fdt_common.h>
 #include <dev/ofw/openfirm.h>
+#include <dev/ofw/ofw_cpu.h>
 
 #include <arm/qualcomm/ipq4018_machdep.h>
+#include <arm/qualcomm/qcom_scm_legacy.h>
+#include <arm/qualcomm/qcom_cpu_kpssv2.h>
 
 #include "platform_if.h"
 
 void
 ipq4018_mp_setmaxid(platform_t plat)
 {
+	int ncpu;
+
+	/* If we've already set the global vars don't bother to do it again. */
+	if (mp_ncpus != 0)
+		return;
+
+	/* Read current CP15 Cache Size ID Register */
+	ncpu = cp15_l2ctlr_get();
+	ncpu = CPUV7_L2CTLR_NPROC(ncpu);
+
+	mp_ncpus = ncpu;
+	mp_maxid = ncpu - 1;
+
+	printf("SMP: ncpu=%d\n", ncpu);
+}
+
+static boolean_t
+ipq4018_start_ap(u_int id, phandle_t node, u_int addr_cells, pcell_t *arg)
+{
+
+	/*
+	 * For the IPQ401x we assume the enable method is
+	 * "qcom,kpss-acc-v2".  If this path gets turned into
+	 * something more generic for other 32 bit qualcomm
+	 * SoCs then we'll likely want to turn this into a
+	 * switch based on "enable-method".
+	 */
+	return qcom_cpu_kpssv2_regulator_start(id, node);
 }
 
 void
 ipq4018_mp_start_ap(platform_t plat)
 {
+	int ret;
+
+	/*
+	 * First step - SCM call to set the cold boot address to mpentry, so
+	 * CPUs hopefully start in the MP path.
+	 */
+	ret = qcom_scm_legacy_mp_set_cold_boot_address((vm_offset_t) mpentry);
+	if (ret != 0)
+		panic("%s: Couldn't set cold boot address via SCM "
+		    "(error 0x%08x)", __func__, ret);
+
+	/*
+	 * Next step - loop over the CPU nodes and do the per-CPU setup
+	 * required to power on the CPUs themselves.
+	 */
+	ofw_cpu_early_foreach(ipq4018_start_ap, true);
+
+	/*
+	 * The next set of IPIs to the CPUs will wake them up and enter
+	 * mpentry.
+	 */
 }
---
+++ sys/arm/qualcomm/qcom_cpu_kpssv2_reg.h
@@ -0,0 +1,58 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2021 Adrian Chadd <adrian@FreeBSD.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef	__QCOM_CPU_KPSSV2_REG_H__
+#define	__QCOM_CPU_KPSSV2_REG_H__
+
+
+/*
+ * APCS CPU core regulator registers.
+ */
+#define	QCOM_APCS_CPU_PWR_CTL				0x04
+#define		QCOM_APCS_CPU_PWR_CTL_PLL_CLAMP		(1U << 8)
+#define		QCOM_APCS_CPU_PWR_CTL_CORE_PWRD_UP	(1U << 7)
+#define		QCOM_APCS_CPU_PWR_CTL_COREPOR_RST	(1U << 5)
+#define		QCOM_APCS_CPU_PWR_CTL_CORE_RST		(1U << 4)
+#define		QCOM_APCS_CPU_PWR_CTL_L2DT_SLP		(1U << 3)
+#define		QCOM_APCS_CPU_PWR_CTL_CLAMP		(1U << 0)
+
+#define	QCOM_APC_PWR_GATE_CTL				0x14
+#define		QCOM_APC_PWR_GATE_CTL_BHS_CNT_SHIFT	24
+#define		QCOM_APC_PWR_GATE_CTL_LDO_PWR_DWN_SHIFT	16
+#define		QCOM_APC_PWR_GATE_CTL_LDO_BYP_SHIFT	8
+#define		QCOM_APC_PWR_GATE_CTL_BHS_SEG_SHIFT	1
+#define		QCOM_APC_PWR_GATE_CTL_BHS_EN		(1U << 0)
+
+
+/*
+ * L2 cache regulator registers.
+ */
+#define	QCOM_APCS_SAW2_2_VCTL        0x1c
+
+#endif	/* __QCOM_CPU_KPSSV2_REG_H__ */
