e41e2d5866180787697051a5a16bf48e085d3fa2
Mateusz Piotrowski <0mp@FreeBSD.org>
2021-11-02 08:05:48 +0100

    dpv: Fix synopsis formatting & sort options
    MFC after:	3 days
    
    (cherry picked from commit a85aa7f9d3bfd26f5dc07f70cb7ba281bfc69f20)

--- usr.bin/dpv/dpv.c
+++ usr.bin/dpv/dpv.c
@@ -496,50 +496,50 @@ usage(void)
 	if (debug) /* No need for usage */
 		exit(EXIT_FAILURE);
 
-	fprintf(stderr, "Usage: %s [options] bytes:label\n", pgm);
-	fprintf(stderr, "       %s [options] -m bytes1:label1 path1 "
-	    "[bytes2:label2 path2 ...]\n", pgm);
+	fprintf(stderr, "Usage: %s [options] [bytes:]label\n", pgm);
+	fprintf(stderr, "       %s [options] -m [bytes1:]label1 path1 "
+	    "[[bytes2:]label2 path2 ...]\n", pgm);
 	fprintf(stderr, "OPTIONS:\n");
 #define OPTFMT "\t%-14s %s\n"
 	fprintf(stderr, OPTFMT, "-a text",
 	    "Append text. Displayed below file progress indicators.");
 	fprintf(stderr, OPTFMT, "-b backtitle",
 	    "String to be displayed on the backdrop, at top-left.");
-	fprintf(stderr, OPTFMT, "-d",
-	    "Debug. Write to standard output instead of dialog.");
 	fprintf(stderr, OPTFMT, "-D",
 	    "Use dialog(1) instead of dialog(3) [default].");
+	fprintf(stderr, OPTFMT, "-d",
+	    "Debug. Write to standard output instead of dialog.");
 	fprintf(stderr, OPTFMT, "-h",
 	    "Produce this output on standard error and exit.");
-	fprintf(stderr, OPTFMT, "-i format",
-	    "Customize status line format. See fdpv(1) for details.");
 	fprintf(stderr, OPTFMT, "-I format",
+	    "Customize status line format. See fdpv(1) for details.");
+	fprintf(stderr, OPTFMT, "-i format",
 	    "Customize status line format. See fdpv(1) for details.");
 	fprintf(stderr, OPTFMT, "-L size",
 	    "Label size. Must be a number greater than 0, or -1.");
 	fprintf(stderr, OPTFMT, "-m",
 	    "Enable processing of multiple file argiments.");
-	fprintf(stderr, OPTFMT, "-n num",
-	    "Display at-most num files per screen. Default is -1.");
 	fprintf(stderr, OPTFMT, "-N",
 	    "No overrun. Stop reading input at stated length, if any.");
+	fprintf(stderr, OPTFMT, "-n num",
+	    "Display at-most num files per screen. Default is -1.");
 	fprintf(stderr, OPTFMT, "-o file",
 	    "Output data to file. First %s replaced with label text.");
-	fprintf(stderr, OPTFMT, "-p text",
-	    "Prefix text. Displayed above file progress indicators.");
 	fprintf(stderr, OPTFMT, "-P size",
 	    "Mini-progressbar size. Must be a number greater than 3.");
-	fprintf(stderr, OPTFMT, "-t title",
-	    "Title string to be displayed at top of dialog(1) box.");
+	fprintf(stderr, OPTFMT, "-p text",
+	    "Prefix text. Displayed above file progress indicators.");
 	fprintf(stderr, OPTFMT, "-T",
 	    "Test mode. Don't actually read any data, but fake it.");
+	fprintf(stderr, OPTFMT, "-t title",
+	    "Title string to be displayed at top of dialog(1) box.");
 	fprintf(stderr, OPTFMT, "-U num",
 	    "Update status line num times per-second. Default is 2.");
 	fprintf(stderr, OPTFMT, "-w",
 	    "Wide. Width of `-p' and `-a' text bump dialog(1) width.");
-	fprintf(stderr, OPTFMT, "-x cmd",
-	    "Send data to executed cmd. First %s replaced with label.");
 	fprintf(stderr, OPTFMT, "-X",
 	    "X11. Use Xdialog(1) instead of dialog(1).");
+	fprintf(stderr, OPTFMT, "-x cmd",
+	    "Send data to executed cmd. First %s replaced with label.");
 	exit(EXIT_FAILURE);
 }
