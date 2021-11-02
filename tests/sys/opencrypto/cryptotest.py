ebb536cb634b3b06c009ae46b074ccdb9559c4f3
Kornel Duleba <mindal@semihalf.com>
2021-11-02 12:53:22 +0100

    ossl: Add support for AES-CBC cipher
    AES-CBC OpenSSL assembly is used underneath.
    The glue layer(ossl_aes.c) is based on CHACHA20 implementation.
    Contrary to the SHA and CHACHA20, AES OpenSSL assembly logic
    does not have a fallback implementation in case CPU doesn't
    support required instructions.
    Because of that CPU caps are checked during initialization and AES
    support is advertised only if available.
    The feature is available on all architectures that ossl supports:
    i386, amd64, arm64.
    
    The biggest advantage of this patch over existing solutions
    (aesni(4) and armv8crypto(4)) is that it supports SHA,
    allowing for ETA operations.
    
    Sponsored by:		Stormshield
    Obtained from:		Semihalf
    Reviewed by:		jhb (previous version)
    Differential revision:	https://reviews.freebsd.org/D32099

--- tests/sys/opencrypto/cryptotest.py
+++ tests/sys/opencrypto/cryptotest.py
@@ -50,7 +50,7 @@ def katg(base, glob):
         raise unittest.SkipTest("Missing %s test vectors" % (base))
     return iglob(os.path.join(katdir, base, glob))
 
-aesmodules = [ 'cryptosoft0', 'aesni0', 'armv8crypto0', 'ccr0', 'ccp0', 'safexcel0', 'qat0' ]
+aesmodules = [ 'cryptosoft0', 'aesni0', 'armv8crypto0', 'ccr0', 'ccp0', 'ossl0', 'safexcel0', 'qat0' ]
 shamodules = [ 'cryptosoft0', 'aesni0', 'armv8crypto0', 'ccr0', 'ccp0', 'ossl0', 'safexcel0', 'qat0' ]
 
 def GenTestCase(cname):
