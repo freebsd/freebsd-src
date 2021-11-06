8c0c5bdf9d5ebe08bb239633c16a567eb8a9f898
Emmanuel Vadot <manu@FreeBSD.org>
2021-11-06 10:11:58 +0100

    Introduce MODULES_EXCLUDE
    It's sometimes easier to exclude some modules rather than listing all
    possibly needed ones with MODULES_OVERRIDE.
    So for this add MODULES_EXCLUDE which do exactly as one would guess, excludes
    some modules from the build/install.
    
    For example if one wants to exclude all modules which are only present in the
    GENERIC config on amd64 :
    export MODULES_EXCLUDE=$(grep -E '^device' sys/amd64/conf/GENERIC | awk '{print $2}' | tr '\n' ' ')
    
    MFC after:	2 weeks
    Sponsored by:	Beckhoff Automation GmbH & Co. KG
    Differential Revision:	https://reviews.freebsd.org/D33443

--- share/mk/bsd.kmod.mk
+++ share/mk/bsd.kmod.mk
@@ -1,4 +1,10 @@
 # $FreeBSD$
 
+.if defined(MODULES_EXCLUDE) && defined(KMOD) && ${MODULES_EXCLUDE:M${KMOD}}
+all:
+install:
+cleandir:
+.else
 .include <bsd.sysdir.mk>
 .include "${SYSDIR}/conf/kmod.mk"
+.endif
