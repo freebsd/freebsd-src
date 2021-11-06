df443cb8a930253bddf7aed8d32c7ae0562ffcdb
Rick Macklem <rmacklem@FreeBSD.org>
2021-11-06 13:34:56 -0700

    param.h: Bump __FreeBSD_version for commit b673755fb0f0
    Commit b673755fb0f0 changed the arguments for VOP_ALLOCATE.
    Bump __FreeBSD_version to 1400041.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -76,7 +76,7 @@
  * cannot include sys/param.h and should only be updated here.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1400040
+#define __FreeBSD_version 1400041
 
 /*
  * __FreeBSD_kernel__ indicates that this system uses the kernel of FreeBSD,
