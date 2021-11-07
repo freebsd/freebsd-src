f5be20afc3568876c44269420a542627238e4da0
Brian Behlendorf <behlendorf1@llnl.gov>
2021-11-07 13:27:44 -0800

    Fix lseek(SEEK_DATA/SEEK_HOLE) mmap consistency
    When using lseek(2) to report data/holes memory mapped regions of
    the file were ignored.  This could result in incorrect results.
    To handle this zfs_holey_common() was updated to asynchronously
    writeback any dirty mmap(2) regions prior to reporting holes.
    
    Additionally, while not strictly required, the dn_struct_rwlock is
    now held over the dirty check to prevent the dnode structure from
    changing.  This ensures that a clean dnode can't be dirtied before
    the data/hole is located.  The range lock is now also taken to
    ensure the call cannot race with zfs_write().
    
    Furthermore, the code was refactored to provide a dnode_is_dirty()
    helper function which checks the dnode for any dirty records to
    determine its dirtiness.
    
    Reviewed-by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed-by: Tony Hutter <hutter2@llnl.gov>
    Reviewed-by: Rich Ercolani <rincebrain@gmail.com>
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Issue #11900
    Closes #12724
    (cherry picked from commit 664d487a5dbd758216ac613934a4080fcc1de347)
    
    Approved by:	so
    Security:	FreeBSD-EN-22:11.zfs

--- sys/contrib/openzfs/include/os/freebsd/zfs/sys/zfs_znode_impl.h
+++ sys/contrib/openzfs/include/os/freebsd/zfs/sys/zfs_znode_impl.h
@@ -118,6 +118,7 @@ extern minor_t zfsdev_minor_alloc(void);
 #define	Z_ISDIR(type) ((type) == VDIR)
 
 #define	zn_has_cached_data(zp)	vn_has_cached_data(ZTOV(zp))
+#define	zn_flush_cached_data(zp, sync)	vn_flush_cached_data(ZTOV(zp), sync)
 #define	zn_rlimit_fsize(zp, uio, td)	vn_rlimit_fsize(ZTOV(zp), (uio), (td))
 
 /* Called on entry to each ZFS vnode and vfs operation  */
