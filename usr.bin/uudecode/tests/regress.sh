963fdecd1a26676626a7067965a54ca7d5fa48fb
Piotr Pawel Stefaniak <pstef@FreeBSD.org>
2021-11-08 15:31:03 +0100

    uudecode: correct error message
    PR:		153276
    Reported by:	David Brennan
    
    (cherry picked from commit 9f3203c003144a4ef6309435036f985afe549ff0)
    
    uudecode: add missing test files to Makefile
    
    (cherry picked from commit 1b3af110bcd522a077f01350b6a51b3ffe434393)
    
    uudecode: use SRCDIR in the regression test
    
    (cherry picked from commit f5138631fb6f9dc73d696560caa006f3df714b9a)
    
    uudecode: move the new uudecode test from uuencode/ to uudecode/
    
    I don't know how that happened.
    
    (cherry picked from commit ae6aa2d43daa6276dd18dc974a1c5b7c15264615)

--- usr.bin/uudecode/tests/regress.sh
+++ usr.bin/uudecode/tests/regress.sh
@@ -1,10 +1,13 @@
 # $FreeBSD$
 
-echo 1..2
+echo 1..3
 
 REGRESSION_START($1)
 
 REGRESSION_TEST_ONE(`uudecode -p <${SRCDIR}/regress.traditional.in', `traditional')
 REGRESSION_TEST_ONE(`uudecode -p <${SRCDIR}/regress.base64.in', `base64')
+
+# was uudecode: stdin: /dev/null: character out of range: [33-96]
+REGRESSION_TEST(`153276', `uudecode -o /dev/null <${SRCDIR}/regress.153276.in 2>&1')
 
 REGRESSION_END()
