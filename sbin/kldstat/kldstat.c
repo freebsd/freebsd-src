29de135aa3329e14e88df8a91ca7c730ba51666b
Hans Petter Selasky <hselasky@FreeBSD.org>
2021-11-09 22:09:59 +0100

    kldstat(8): style(9) fixes.
    No functional change intended.
    
    Differential revision:  https://reviews.freebsd.org/D32506
    Submitted by:   christos@
    MFC after:      1 week
    Sponsored by:   NVIDIA Networking

--- sbin/kldstat/kldstat.c
+++ sbin/kldstat/kldstat.c
@@ -29,18 +29,23 @@
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
-#include <err.h>
-#include <libutil.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/param.h>
 #include <sys/module.h>
 #include <sys/linker.h>
-#include <strings.h>
 
-#define	POINTER_WIDTH ((int)(sizeof(void *) * 2 + 2))
+#include <err.h>
+#include <libutil.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+
+#define	PTR_WIDTH ((int)(sizeof(void *) * 2 + 2))
+
+static void printmod(int);
+static void printfile(int, int, int);
+static void usage(void) __dead2;
 
 static int showdata = 0;
 
@@ -51,16 +56,16 @@ printmod(int modid)
 
 	bzero(&stat, sizeof(stat));
 	stat.version = sizeof(struct module_stat);
-	if (modstat(modid, &stat) < 0)
+	if (modstat(modid, &stat) < 0) {
 		warn("can't stat module id %d", modid);
-	else {
-		if (showdata)
-			printf("\t\t%3d %s (%d, %u, 0x%lx)\n", stat.id,
-			    stat.name, stat.data.intval, stat.data.uintval,
-			    stat.data.ulongval);
-		else
-			printf("\t\t%3d %s\n", stat.id, stat.name);
+		return;
 	}
+	if (showdata) {
+		printf("\t\t%3d %s (%d, %u, 0x%lx)\n", stat.id,
+		    stat.name, stat.data.intval, stat.data.uintval,
+		    stat.data.ulongval);
+	} else
+		printf("\t\t%3d %s\n", stat.id, stat.name);
 }
 
 static void
@@ -71,21 +76,19 @@ printfile(int fileid, int verbose, int humanized)
 	char buf[5];
 
 	stat.version = sizeof(struct kld_file_stat);
-	if (kldstat(fileid, &stat) < 0) {
+	if (kldstat(fileid, &stat) < 0)
 		err(1, "can't stat file id %d", fileid);
-	} else {
-		if (humanized) {
-			humanize_number(buf, sizeof(buf), stat.size,
-			    "", HN_AUTOSCALE, HN_DECIMAL | HN_NOSPACE);
+	if (humanized) {
+		humanize_number(buf, sizeof(buf), stat.size,
+		    "", HN_AUTOSCALE, HN_DECIMAL | HN_NOSPACE);
 
-			printf("%2d %4d %*p %5s %s",
-			    stat.id, stat.refs, POINTER_WIDTH, stat.address,
-			    buf, stat.name);
-		} else {
-			printf("%2d %4d %*p %8zx %s",
-			    stat.id, stat.refs, POINTER_WIDTH, stat.address,
-			    stat.size, stat.name);
-		}
+		printf("%2d %4d %*p %5s %s",
+		    stat.id, stat.refs, PTR_WIDTH, stat.address,
+		    buf, stat.name);
+	} else {
+		printf("%2d %4d %*p %8zx %s",
+		    stat.id, stat.refs, PTR_WIDTH, stat.address,
+		    stat.size, stat.name);
 	}
 
 	if (verbose) {
@@ -98,27 +101,28 @@ printfile(int fileid, int verbose, int humanized)
 		printf("\n");
 }
 
-static void
+static void __dead2
 usage(void)
 {
-	fprintf(stderr, "usage: kldstat [-d] [-h] [-q] [-v] [-i id] [-n filename]\n");
-	fprintf(stderr, "       kldstat [-d] [-q] [-m modname]\n");
+	fprintf(stderr, "usage: %1$s [-dhqv] [-i id] [-n filename]\n"
+	    "       %1$s [-dq] [-m modname]\n", getprogname());
 	exit(1);
 }
 
 int
-main(int argc, char** argv)
+main(int argc, char *argv[])
 {
-	int c;
+	struct module_stat stat;
 	int humanized = 0;
 	int verbose = 0;
 	int fileid = 0;
 	int quiet = 0;
-	char* filename = NULL;
-	char* modname = NULL;
-	char* p;
+	int c, modid;
+	char *filename = NULL;
+	char *modname = NULL;
+	char *p;
 
-	while ((c = getopt(argc, argv, "dhi:m:n:qv")) != -1)
+	while ((c = getopt(argc, argv, "dhi:m:n:qv")) != -1) {
 		switch (c) {
 		case 'd':
 			showdata = 1;
@@ -146,6 +150,7 @@ main(int argc, char** argv)
 		default:
 			usage();
 		}
+	}
 	argc -= optind;
 	argv += optind;
 
@@ -153,16 +158,12 @@ main(int argc, char** argv)
 		usage();
 
 	if (modname != NULL) {
-		int modid;
-		struct module_stat stat;
-
 		if ((modid = modfind(modname)) < 0) {
 			if (!quiet)
 				warn("can't find module %s", modname);
-			return 1;
-		} else if (quiet) {
-			return 0;
-		}
+			return (1);
+		} else if (quiet)
+			return (0);
 
 		stat.version = sizeof(struct module_stat);
 		if (modstat(modid, &stat) < 0)
@@ -181,30 +182,30 @@ main(int argc, char** argv)
 			}
 		}
 
-		return 0;
+		return (0);
 	}
 
 	if (filename != NULL) {
 		if ((fileid = kldfind(filename)) < 0) {
 			if (!quiet)
 				warn("can't find file %s", filename);
-			return 1;
-		} else if (quiet) {
-			return 0;
-		}
+			return (1);
+		} else if (quiet)
+			return (0);
 	}
 
-	if (humanized)
-		printf("Id Refs Address%*c %5s Name\n", POINTER_WIDTH - 7,
+	if (humanized) {
+		printf("Id Refs Address%*c %5s Name\n", PTR_WIDTH - 7,
 		    ' ', "Size");
-	else
-		printf("Id Refs Address%*c %8s Name\n", POINTER_WIDTH - 7,
+	} else {
+		printf("Id Refs Address%*c %8s Name\n", PTR_WIDTH - 7,
 		    ' ', "Size");
+	}
 	if (fileid != 0)
 		printfile(fileid, verbose, humanized);
 	else
 		for (fileid = kldnext(0); fileid > 0; fileid = kldnext(fileid))
 			printfile(fileid, verbose, humanized);
 
-	return 0;
+	return (0);
 }
