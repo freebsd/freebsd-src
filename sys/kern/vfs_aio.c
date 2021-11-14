8165540af2cd270e0dd8ffdbb72a6e2e25dbe3d5
Mateusz Guzik <mjg@FreeBSD.org>
2021-11-14 16:59:53 +0000

    aio: whack "set but not used" warnings
    (cherry picked from commit e9c7ec22877f461f530dfe1cb8e17086300c0ee8)

--- sys/kern/vfs_aio.c
+++ sys/kern/vfs_aio.c
@@ -758,7 +758,6 @@ aio_process_rw(struct kaiocb *job)
 {
 	struct ucred *td_savedcred;
 	struct thread *td;
-	struct aiocb *cb;
 	struct file *fp;
 	ssize_t cnt;
 	long msgsnd_st, msgsnd_end;
@@ -778,7 +777,6 @@ aio_process_rw(struct kaiocb *job)
 	td_savedcred = td->td_ucred;
 	td->td_ucred = job->cred;
 	job->uiop->uio_td = td;
-	cb = &job->uaiocb;
 	fp = job->fd_file;
 
 	opcode = job->uaiocb.aio_lio_opcode;
@@ -2472,7 +2470,7 @@ aio_biowakeup(struct bio *bp)
 	size_t nbytes;
 	long bcount = bp->bio_bcount;
 	long resid = bp->bio_resid;
-	int error, opcode, nblks;
+	int opcode, nblks;
 	int bio_error = bp->bio_error;
 	uint16_t flags = bp->bio_flags;
 
@@ -2483,7 +2481,6 @@ aio_biowakeup(struct bio *bp)
 	nbytes =bcount - resid;
 	atomic_add_acq_long(&job->nbytes, nbytes);
 	nblks = btodb(nbytes);
-	error = 0;
 	/*
 	 * If multiple bios experienced an error, the job will reflect the
 	 * error of whichever failed bio completed last.
