fcaf8bcc82032906304a880943b2fe10c849727b
Kirk McKusick <mckusick@FreeBSD.org>
2021-11-15 09:26:21 -0800

    Suppress UFS/FFS superblock check-hash failure messages when identifying disk labels.
    When the geom label subsystem is checking labels to discover if they
    are UFS/FFS filesystems, do not print a kernel error message if a
    superblock is found with a check-hash error. That issue is best
    handled later if an attempt is made to actually use the filesystem.
    
    Sponsored by: Netflix

--- sys/geom/label/g_label_ufs.c
+++ sys/geom/label/g_label_ufs.c
@@ -140,7 +140,7 @@ g_label_ufs_taste_common(struct g_consumer *cp, char *label, size_t size, int wh
 
 	fs = NULL;
 	if (SBLOCKSIZE % pp->sectorsize != 0 || ffs_sbget(cp, &fs,
-	    STDSB_NOHASHFAIL, M_GEOM, g_use_g_read_data) != 0) {
+	    STDSB_NOHASHFAIL_NOMSG, M_GEOM, g_use_g_read_data) != 0) {
 		KASSERT(fs == NULL,
 		    ("g_label_ufs_taste_common: non-NULL fs %p\n", fs));
 		return;
