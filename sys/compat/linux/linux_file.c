61e7d7c9be1bb1f9ad994a4b879fd8a65ce95a5a
John Baldwin <jhb@FreeBSD.org>
2021-11-17 15:51:06 -0800

    linux_linkat: Don't invert AT_* flags.
    Reviewed by:	kib
    Sponsored by:	The University of Cambridge, Google Inc.
    Differential Revision:	https://reviews.freebsd.org/D33048

--- sys/compat/linux/linux_file.c
+++ sys/compat/linux/linux_file.c
@@ -1170,9 +1170,9 @@ linux_linkat(struct thread *td, struct linux_linkat_args *args)
 	if (args->flag & ~(LINUX_AT_SYMLINK_FOLLOW | LINUX_AT_EMPTY_PATH))
 		return (EINVAL);
 
-	flag = (args->flag & LINUX_AT_SYMLINK_FOLLOW) == 0 ? AT_SYMLINK_FOLLOW :
+	flag = (args->flag & LINUX_AT_SYMLINK_FOLLOW) != 0 ? AT_SYMLINK_FOLLOW :
 	    0;
-	flag |= (args->flag & LINUX_AT_EMPTY_PATH) == 0 ? AT_EMPTY_PATH : 0;
+	flag |= (args->flag & LINUX_AT_EMPTY_PATH) != 0 ? AT_EMPTY_PATH : 0;
 
 	olddfd = (args->olddfd == LINUX_AT_FDCWD) ? AT_FDCWD : args->olddfd;
 	newdfd = (args->newdfd == LINUX_AT_FDCWD) ? AT_FDCWD : args->newdfd;
