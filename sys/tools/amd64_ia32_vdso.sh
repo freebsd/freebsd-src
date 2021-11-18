b0e0b4e7b3795e9beb0cddcfd75261cc9327b5b0
Konstantin Belousov <kib@FreeBSD.org>
2021-11-18 03:20:08 +0200

    Enforce that vdso does not consume too much from the shared page
    Reviewed by:	emaste
    Discussed with:	jrtc27
    Tested by:	pho
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 month
    Differential revision:	https://reviews.freebsd.org/D32960

--- sys/tools/amd64_ia32_vdso.sh
+++ sys/tools/amd64_ia32_vdso.sh
@@ -40,6 +40,12 @@ ${LD} --shared -Bsymbolic -soname="elf-vdso32.so.1" \
    --hash-style=sysv --fatal-warnings --strip-all \
    -o elf-vdso32.so.1 ia32_sigtramp.pico
 
+if [ "$(wc -c elf-vdso32.so.1 | ${AWK} '{print $1}')" -gt 2048 ]
+then
+    echo "elf-vdso32.so.1 too large" 1>&2
+    exit 1
+fi
+
 ${CC} -x assembler-with-cpp -DLOCORE -fPIC -nostdinc -c \
    -o elf-vdso32.so.o -I. -I"${S}" -include opt_global.h \
    -DVDSO_NAME=elf_vdso32_so_1 -DVDSO_FILE=elf-vdso32.so.1 \
