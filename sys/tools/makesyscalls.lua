5203efead7092974bbeac20661143702be3921bd
Brooks Davis <brooks@FreeBSD.org>
2021-11-22 22:36:56 +0000

    makesyscalls: Remove _native suffix handling
    This supported a hack we used in CheriBSD which has been removed.
    
    Reviewed by:	kevans

--- sys/tools/makesyscalls.lua
+++ sys/tools/makesyscalls.lua
@@ -581,7 +581,6 @@ local function process_args(args)
 		-- XX TODO: Forward declarations? See: sysstubfwd in CheriBSD
 		if abi_change then
 			local abi_type_suffix = config["abi_type_suffix"]
-			argtype = argtype:gsub("_native ", "")
 			argtype = argtype:gsub("(struct [^ ]*)", "%1" ..
 			    abi_type_suffix)
 			argtype = argtype:gsub("(union [^ ]*)", "%1" ..
