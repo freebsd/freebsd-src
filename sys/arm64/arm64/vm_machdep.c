b5346307ed34f9a7cb8c37b495283df0bdba852a
Andrew Turner <andrew@FreeBSD.org>
2021-11-22 15:20:51 +0000

    Per-thread stack canary on arm64
    With the update to llvm 13 we are able to tell the compiler it can find
    the SSP canary relative to the register that holds the userspace stack
    pointer. As this is unused in most of the kernel it can be used here
    to point to a per-thread SSP canary.
    
    As the kernel could be built with an old toolchain, e.g. when upgrading
    from 13, add a warning that the options was enabled but the compiler
    doesn't support it to both the build and kernel boot.
    
    Discussed with:	emaste
    Sponsored by:	The FreeBSD Foundation
    Differential Revision: https://reviews.freebsd.org/D33079
    
    (cherry picked from commit ae92ace05fd4fcf64e3bb787951578f655b1fa5f)

--- sys/arm64/arm64/vm_machdep.c
+++ sys/arm64/arm64/vm_machdep.c
@@ -115,6 +115,11 @@ cpu_fork(struct thread *td1, struct proc *p2, struct thread *td2, int flags)
 	/* Setup to release spin count in fork_exit(). */
 	td2->td_md.md_spinlock_count = 1;
 	td2->td_md.md_saved_daif = PSR_DAIF_DEFAULT;
+
+#if defined(PERTHREAD_SSP)
+	/* Set the new canary */
+	arc4random_buf(&td2->td_md.md_canary, sizeof(td2->td_md.md_canary));
+#endif
 }
 
 void
@@ -187,6 +192,11 @@ cpu_copy_thread(struct thread *td, struct thread *td0)
 	/* Setup to release spin count in fork_exit(). */
 	td->td_md.md_spinlock_count = 1;
 	td->td_md.md_saved_daif = PSR_DAIF_DEFAULT;
+
+#if defined(PERTHREAD_SSP)
+	/* Set the new canary */
+	arc4random_buf(&td->td_md.md_canary, sizeof(td->td_md.md_canary));
+#endif
 }
 
 /*
