e5f63bbcd9376f43ea91d0357ff91cfc9732b8f0
Warner Losh <imp@FreeBSD.org>
2021-11-23 20:28:45 -0700

    sys/devctl.h: Only declare kernel functions in kernel.
    Also, while I'm here, switch to newer, shorter format and fix my
    copyright to match the others in the tree.
    
    Sponsored by:		Netflix

--- sys/sys/devctl.h
+++ sys/sys/devctl.h
@@ -1,28 +1,7 @@
 /*-
- * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ * Copyright 2020 M. Warner Losh <imp@FreeBSD.org>
  *
- * Copyright 2020 M. Warner Losh.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * SPDX-License-Idnetifier: BSD-2-Clause
  *
  * $FreeBSD$
  *
@@ -31,6 +10,7 @@
 #ifndef _SYS_DEVCTL_H_
 #define _SYS_DEVCTL_H_
 
+#ifdef _KERNEL
 /**
  * devctl hooks.  Typically one should use the devctl_notify
  * hook to send the message.
@@ -40,5 +20,6 @@ void devctl_notify(const char *__system, const char *__subsystem,
     const char *__type, const char *__data);
 struct sbuf;
 void devctl_safe_quote_sb(struct sbuf *__sb, const char *__src);
+#endif
 
 #endif /* _SYS_DEVCTL_H_ */
