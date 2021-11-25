df822a8e059ce9a3363973e930fcfc58c2d01750
Mateusz Guzik <mjg@FreeBSD.org>
2021-11-25 21:42:42 +0000

    vfs: drop the always curthread argument from kern_alternate_path

--- sys/kern/vfs_lookup.c
+++ sys/kern/vfs_lookup.c
@@ -1673,14 +1673,16 @@ out_mismatch:
  * the M_TEMP bucket if one is returned.
  */
 int
-kern_alternate_path(struct thread *td, const char *prefix, const char *path,
-    enum uio_seg pathseg, char **pathbuf, int create, int dirfd)
+kern_alternate_path(const char *prefix, const char *path, enum uio_seg pathseg,
+    char **pathbuf, int create, int dirfd)
 {
+	struct thread *td;
 	struct nameidata nd, ndroot;
 	char *ptr, *buf, *cp;
 	size_t len, sz;
 	int error;
 
+	td = curthread;
 	buf = (char *) malloc(MAXPATHLEN, M_TEMP, M_WAITOK);
 	*pathbuf = buf;
 
