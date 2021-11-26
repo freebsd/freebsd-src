c141cad41d949b344c177fc838f0828442b61a14
Andriy Gapon <avg@FreeBSD.org>
2021-11-26 08:45:12 +0200

    twsi: remove write-only softc field
    (cherry picked from commit f00bc54f62c455c7ed44afee736c3c64873a305a)

--- sys/dev/iicbus/twsi/a10_twsi.c
+++ sys/dev/iicbus/twsi/a10_twsi.c
@@ -121,8 +121,6 @@ a10_twsi_attach(device_t dev)
 	sc->reg_baud_rate = TWI_CCR;
 	sc->reg_soft_reset = TWI_SRST;
 
-	sc->need_ack = true;
-
 	if (ofw_bus_is_compatible(dev, "allwinner,sun6i-a31-i2c") ||
 	    ofw_bus_is_compatible(dev, "allwinner,sun6i-a83t-i2c"))
 		sc->iflag_w1c = true;
