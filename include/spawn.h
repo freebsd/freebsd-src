c7b624979a60816251f848f0c460aa2c11712c23
Konstantin Belousov <kib@FreeBSD.org>
2021-11-28 00:49:03 +0200

    posix_spawn: add chdir-related non-portable actions
    (cherry picked from commit 25cda42a498101612687b32005856cd8b14f2f7a)

--- include/spawn.h
+++ include/spawn.h
@@ -87,6 +87,13 @@ int posix_spawn_file_actions_addopen(posix_spawn_file_actions_t * __restrict,
 int posix_spawn_file_actions_adddup2(posix_spawn_file_actions_t *, int, int);
 int posix_spawn_file_actions_addclose(posix_spawn_file_actions_t *, int);
 
+#if __BSD_VISIBLE
+int posix_spawn_file_actions_addchdir_np(posix_spawn_file_actions_t *
+    __restrict, const char * __restrict);
+int posix_spawn_file_actions_addfchdir_np(posix_spawn_file_actions_t *,
+    int);
+#endif
+
 /*
  * Spawn attributes
  */
