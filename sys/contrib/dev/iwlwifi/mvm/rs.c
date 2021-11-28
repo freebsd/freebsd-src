bfcc09ddd422c95a1a2e4e794b63ee54c4902398
Bjoern A. Zeeb <bz@FreeBSD.org>
2021-11-28 18:35:53 +0000

    iwlwifi: import Intel's iwlwifi/mvm driver.
    Over the past few months we published multiple snapshots for this
    Linux derived driver and it has become fairly stable in terms of
    minimal local changes needed for new updates.
    
    The current version is based on iwlwifi-next update at
    cbaa6aeedee5f92dafa5982eceea2a1f98ce4f7d with the addition of
    a hand full of files replaced for FreeBSD.
    
    Given the lack of full license texts on non-local files this is
    imported under the draft policy for handling SPDX files (D29226). [1]
    
    Do not yet hook this to the build until the remaining compat code
    is all in.  Along with the firmware import this will make publishing
    the last bits and final testing a lot easier.
    
    Sponsored by:   The FreeBSD Foundation
    Approved by:    core (imp) [1]
    MFC after:      10 days

---
+++ sys/contrib/dev/iwlwifi/mvm/rs.c
@@ -0,0 +1,92 @@
+/*-
+ * Copyright (c) 2020-2021 The FreeBSD Foundation
+ *
+ * This software was developed by Björn Zeeb under sponsorship from
+ * the FreeBSD Foundation.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+/*
+ * XXX-BZ:
+ * This file is left as a wrapper to make mvm compile and we will only
+ * deal with it on a need basis.  Most newer chipsets do this in firmware.
+ */
+
+#include <sys/errno.h>
+
+#include "mvm.h"
+
+
+int
+iwl_mvm_rate_control_register(void)
+{
+
+	return (0);
+}
+
+int
+iwl_mvm_tx_protection(struct iwl_mvm *mvm, struct iwl_mvm_sta *sta, bool enable)
+{
+
+	return (0);
+}
+
+void
+iwl_mvm_rate_control_unregister(void)
+{
+}
+
+void
+iwl_mvm_rs_rate_init(struct iwl_mvm *mvm, struct ieee80211_sta *sta,
+    enum nl80211_band band, bool t)
+{
+}
+
+void
+iwl_mvm_rs_tx_status(struct iwl_mvm *mvm, struct ieee80211_sta *sta, int tid,
+    struct ieee80211_tx_info *ba_info, bool t)
+{
+}
+
+void
+rs_update_last_rssi(struct iwl_mvm *mvm, struct iwl_mvm_sta *mvmsta,
+    struct ieee80211_rx_status *rx_status)
+{
+
+	/*
+	 * Assumption based on mvm/sta.h is that this should update
+	 * mvmsta->lq_sta.rs_drv but so far we only saw a iwl_lq_cmd (lq)
+	 * access in that struct so nowhere to put rssi information.
+	 * So the only thing would be if this is required internally
+	 * to functions in this file.
+	 */
+}
+
+int
+rs_pretty_print_rate_v1(char *buf, int bufsz, const u32 rate)
+{
+
+	return (0);
+}
