0574d3640a62873fd4b495493ace2149533eab1c
Gordon Bergling <gbe@FreeBSD.org>
2021-11-30 10:41:46 +0100

    mips: Fix a typo in a source code comment
    - s/segement/segment/
    
    (cherry picked from commit 1b0602f2db94a4789da33302fbadfe5a57454277)

--- sys/mips/mips/elf_trampoline.c
+++ sys/mips/mips/elf_trampoline.c
@@ -98,7 +98,7 @@ bzero(void *addr, size_t count)
 #define	mkptr(x)	((void *)(intptr_t)(int)(x))
 
 /*
- * Relocate PT_LOAD segements of kernel ELF image to their respective
+ * Relocate PT_LOAD segments of kernel ELF image to their respective
  * virtual addresses and return entry point
  */
 void *
