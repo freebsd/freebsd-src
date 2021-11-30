a8adbe528446352a27e774c7c33bea027e7a7c15
Mitchell Horne <mhorne@FreeBSD.org>
2021-11-30 11:15:44 -0400

    arm64, powerpc: fix calculation of 'used' in GET_STACK_USAGE
    We do not consider the space reserved for the pcb to be part of the
    total kstack size, so it should not be included in the calculation of
    the used stack size.
    
    MFC after:	1 week
    Sponsored by:	The FreeBSD Foundation
    
    (cherry picked from commit b02908b051c897117ebbc3dcb55848383b17c435)

--- sys/powerpc/include/proc.h
+++ sys/powerpc/include/proc.h
@@ -74,9 +74,7 @@ struct syscall_args {
 #define	GET_STACK_USAGE(total, used) do {				\
 	struct thread *td = curthread;					\
 	(total) = td->td_kstack_pages * PAGE_SIZE - sizeof(struct pcb);	\
-	(used) = (char *)td->td_kstack +				\
-	    td->td_kstack_pages * PAGE_SIZE -				\
-	    (char *)&td;						\
+	(used) = td->td_kstack + (total) - (vm_offset_t)&td;		\
 } while (0)
 #endif
 
