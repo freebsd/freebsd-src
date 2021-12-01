ef4f3aaf9b95e1befc87fed9d55f61c327190edc
Emmanuel Vadot <manu@FreeBSD.org>
2021-12-01 11:49:57 +0100

    fb: Remove unused FBIOSATTR/FBIOGATTR ioctls
    The FBIOSATTR/FBIOGATTR ioctls were added in 1994 with the 4.4 Lite import
    but were never implemented in either sc(4), fb(4) or vt(4).
    Remove it.
    
    Reviewed by:	emaste, tsoome
    MFC after:      2 weeks
    Sponsored by:   Beckhoff Automation GmbH & Co. KG
    Differential Revision:	https://reviews.freebsd.org/D33215

--- sys/dev/fb/fb.c
+++ sys/dev/fb/fb.c
@@ -738,7 +738,6 @@ fb_commonioctl(video_adapter_t *adp, u_long cmd, caddr_t arg)
 	case FBIO_SETLINEWIDTH:	/* set scan line width in pixel */
 
 	case FBIOGTYPE:
-	case FBIOGATTR:
 	case FBIOSVIDEO:
 	case FBIOGVIDEO:
 	case FBIOVERTICAL:
