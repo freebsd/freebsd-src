fc073e6743a989c9d0b8f619ce0f903271c0db2e
Ed Maste <emaste@FreeBSD.org>
2021-12-02 10:27:12 -0500

    Move _p.a profiling archives under MK_INSTALLLIB
    _p.a are just a special case of .a archives, and should not be installed
    if WITHOUT_INSTALLLIB is set.
    
    MFC after:	1 week
    Sponsored by:	The FreeBSD Foundation

--- share/mk/bsd.lib.mk
+++ share/mk/bsd.lib.mk
@@ -481,10 +481,10 @@ _libinstall:
 .if defined(LIB) && !empty(LIB) && ${MK_INSTALLLIB} != "no"
 	${INSTALL} ${TAG_ARGS:D${TAG_ARGS},dev} -C -o ${LIBOWN} -g ${LIBGRP} -m ${LIBMODE} \
 	    ${_INSTALLFLAGS} lib${LIB_PRIVATE}${LIB}${_STATICLIB_SUFFIX}.a ${DESTDIR}${_LIBDIR}/
-.endif
-.if ${MK_PROFILE} != "no" && defined(LIB) && !empty(LIB)
+.if ${MK_PROFILE} != "no"
 	${INSTALL} ${TAG_ARGS:D${TAG_ARGS},dev} -C -o ${LIBOWN} -g ${LIBGRP} -m ${LIBMODE} \
 	    ${_INSTALLFLAGS} lib${LIB_PRIVATE}${LIB}_p.a ${DESTDIR}${_LIBDIR}/
+.endif
 .endif
 .if defined(SHLIB_NAME)
 	${INSTALL} ${TAG_ARGS} ${STRIP} -o ${LIBOWN} -g ${LIBGRP} -m ${LIBMODE} \
