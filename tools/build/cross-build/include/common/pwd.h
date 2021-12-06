e0cb1fe7dd34e9786320befa623b181d29d6bdf9
Jessica Clarke <jrtc27@FreeBSD.org>
2021-12-06 23:25:53 +0000

    cross-build: Provide _PASSWORD_EFMT1 for libcrypt on Linux
    Linux's pwd.h does not define _PASSWORD_EFMT1 (macOS's does), so we need
    to define it in order to be able to bootstrap libcrypt (crypt-des.c uses
    it) on non-FreeBSD, which will be done in a subsequent commit.
    
    MFC after:	1 week

--- tools/build/cross-build/include/common/pwd.h
+++ tools/build/cross-build/include/common/pwd.h
@@ -41,6 +41,10 @@
 
 #define user_from_uid __nbcompat_user_from_uid
 
+#ifndef _PASSWORD_EFMT1
+#define	_PASSWORD_EFMT1		'_'	/* extended encryption format */
+#endif
+
 int pwcache_userdb(int (*a_setpassent)(int), void (*a_endpwent)(void),
     struct passwd *(*a_getpwnam)(const char *),
     struct passwd *(*a_getpwuid)(uid_t));
