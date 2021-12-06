882f8d059c88d76bcbe902c794c68b6975dcc8ed
Vladimir Kondratyev <wulf@FreeBSD.org>
2021-12-06 10:59:49 +0300

    LinuxKPI: Import linux/jump_label.h
    Required by drm-kmod
    
    Obtained from:	DragonflyBSD
    Differential Revision:	https://reviews.freebsd.org/D33299

---
+++ sys/compat/linuxkpi/common/include/linux/jump_label.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016-2020 François Tigeot <ftigeot@wolfpond.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _LINUX_JUMP_LABEL_H_
+#define _LINUX_JUMP_LABEL_H_
+
+#include <linux/types.h>
+#include <linux/compiler.h>
+#include <linux/bug.h>
+
+#define DEFINE_STATIC_KEY_FALSE(key)	bool key = false
+
+static inline void
+static_branch_enable(bool *flag)
+{
+	*flag = true;
+}
+
+static inline bool
+static_branch_likely(bool *flag)
+{
+	return *flag;
+}
+
+#endif	/* _LINUX_JUMP_LABEL_H_ */
