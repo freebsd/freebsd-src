1b308d0631e5d0d21b631a688cdef1afe378e086
Baptiste Daroussin <bapt@FreeBSD.org>
2021-12-06 11:35:28 +0100

    bsddialog: vendor import 2021-12-05

---
+++ examples_library/sade.c
@@ -0,0 +1,64 @@
+/*-
+ * SPDX-License-Identifier: CC0-1.0
+ *
+ * Written in 2021 by Alfonso Sabato Siciliano.
+ * To the extent possible under law, the author has dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty, see:
+ *   <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <bsddialog.h>
+
+/* Figure 15 - https://docs.freebsd.org/en/books/handbook/bsdinstall/ */
+int main()
+{
+	int i, output;
+	struct bsddialog_conf conf;
+	struct bsddialog_menuitem items[5] = {
+	    {"", false, 0, "ada0",   "16 GB GPT", ""},
+	    {"", false, 1, "ada0p1", "512 KB freebsd-boot", ""},
+	    {"", false, 1, "ada0p2", "15 GB freebsd-ufs", ""},
+	    {"", false, 1, "ada0p3", "819 MB freebsd-swap none", ""},
+	    {"", false, 0, "ada1",   "16 GB", ""}
+	};
+
+	bsddialog_initconf(&conf);
+	conf.title = "Partition Editor";
+	char *text = "Please review the disk setup. When complete, press the "\
+	    "Finish button";
+
+	conf.menu.align_left = true;
+
+	conf.button.ok_label = "Create";
+	
+	conf.button.extra_button = true;
+	conf.button.extra_label = "Delete";
+	
+	conf.button.no_label = "Cancel";
+	
+	conf.button.help_button = true;
+	conf.button.help_label = "Revert";
+	
+	conf.button.generic1_label = "Auto";
+	conf.button.generic2_label = "Finish";
+	
+	conf.button.default_label= "Finish";
+	
+	if (bsddialog_init() < 0)
+		return -1;
+
+	output = bsddialog_menu(&conf, text, 20, 0, 10, 5, items, NULL);
+
+	bsddialog_end();
+
+	printf("Menu:\n");
+	for (i=0; i<5; i++)
+		printf(" [%c] %s\n", items[i].on ? 'X' : ' ', items[i].name);
+		
+	
+	return output;
+}
---
+++ examples_library/theme.c
@@ -0,0 +1,67 @@
+/*-
+ * SPDX-License-Identifier: CC0-1.0
+ *
+ * Written in 2021 by Alfonso Sabato Siciliano.
+ * To the extent possible under law, the author has dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty, see:
+ *   <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <bsddialog.h>
+#include <bsddialog_theme.h>
+
+int main()
+{
+	int output;
+	struct bsddialog_conf conf;
+	enum bsddialog_default_theme theme;
+	struct bsddialog_menuitem items[5] = {
+	    {"", false, 0, "Dialog",    "Current dialog theme",  "BSDDIALOG_THEME_DIALOG" },
+	    {"", false, 0, "BSDDialog", "Future default theme",  "BSDDIALOG_THEME_DEFAULT"},
+	    {"", false, 0, "BlackWhite","Black and White theme", "BSDDIALOG_THEME_BLACKWHITE"},
+	    {"", false, 0, "Magenta",   "Testing",               "BSDDIALOG_THEME_MAGENTA"},
+	    {"", false, 0, "Quit",      "Exit",                  "Quit or Cancel to exit" }
+	};
+
+	bsddialog_initconf(&conf);
+	conf.title = " Theme ";
+	
+	if (bsddialog_init() == BSDDIALOG_ERROR)
+		return BSDDIALOG_ERROR;
+
+	while (true) {
+		bsddialog_backtitle(&conf, "Theme Example");
+
+		output = bsddialog_menu(&conf, "Choose theme", 15, 40, 5, 5, items, NULL);
+
+		if (output != BSDDIALOG_YESOK || items[4].on)
+			break;
+
+		if (items[0].on) {
+			theme = BSDDIALOG_THEME_DIALOG;
+			conf.menu.default_item = items[0].name;
+		}
+		else if (items[1].on) {
+			theme = BSDDIALOG_THEME_BSDDIALOG;
+			conf.menu.default_item = items[1].name;
+		}
+		else if (items[2].on) {
+			theme = BSDDIALOG_THEME_BLACKWHITE;
+			conf.menu.default_item = items[2].name;
+		}
+		else if (items[3].on) {
+			theme = BSDDIALOG_THEME_MAGENTA;
+			conf.menu.default_item = items[3].name;
+		}
+
+		bsddialog_set_default_theme(theme);
+	}
+
+	bsddialog_end();	
+
+	return output;
+}
