1456816425c35ab1ded97f9b53f17c87ae4a701f
Vladimir Kondratyev <wulf@FreeBSD.org>
2021-12-09 00:05:56 +0300

    LinuxKPI: Import linux/ratelimit.h
    Required by drm-kmod.
    
    Obtained from:	OpenBSD
    MFC after:	1 week
    Reviewed by:	manu
    Differential Revision:	https://reviews.freebsd.org/D33560

---
+++ sys/compat/linuxkpi/common/include/linux/ratelimit.h
@@ -0,0 +1,17 @@
+/* Public domain. */
+
+#ifndef _LINUX_RATELIMIT_H
+#define _LINUX_RATELIMIT_H
+
+struct ratelimit_state {
+};
+
+#define DEFINE_RATELIMIT_STATE(name, interval, burst) \
+	int name __used = 1;
+
+#define __ratelimit(x)	(1)
+
+#define ratelimit_state_init(x, y, z)
+#define ratelimit_set_flags(x, y)
+
+#endif
