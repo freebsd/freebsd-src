1a62e9bc0046bfe20f4dd785561e469ff73fd508
John Baldwin <jhb@FreeBSD.org>
2021-12-09 13:17:13 -0800

    Add <machine/tls.h> header to hold MD constants and helpers for TLS.
    The header exports the following:
    
    - Definition of struct tcb.
    - Helpers to get/set the tcb for the current thread.
    - TLS_TCB_SIZE (size of TCB)
    - TLS_TCB_ALIGN (alignment of TCB)
    - TLS_VARIANT_I or TLS_VARIANT_II
    - TLS_DTV_OFFSET (bias of pointers in dtv[])
    - TLS_TP_OFFSET (bias of "thread pointer" relative to TCB)
    
    Note that TLS_TP_OFFSET does not account for if the unbiased thread
    pointer points to the start of the TCB (arm and x86) or the end of the
    TCB (MIPS, PowerPC, and RISC-V).
    
    Note also that for amd64, the struct tcb does not include the unused
    tcb_spare field included in the current structure in libthr.  libthr
    does not use this field, and the existing calls in libc and rtld that
    allocate a TCB for amd64 assume it is the size of 3 Elf_Addr's (and
    thus do not allocate room for tcb_spare).
    
    A <sys/_tls_variant_i.h> header is used by architectures using
    Variant I TLS which uses a common struct tcb.
    
    Reviewed by:	kib (older version of x86/tls.h), jrtc27
    Sponsored by:	The University of Cambridge, Google Inc.
    Differential Revision:	https://reviews.freebsd.org/D33351

--- sys/mips/include/tls.h
+++ sys/mips/include/tls.h
@@ -34,20 +34,52 @@
 #ifndef	__MIPS_TLS_H__
 #define	__MIPS_TLS_H__
 
+#include <sys/_tls_variant_i.h>
+#include <machine/sysarch.h>
+
 /*
  * TLS parameters
  */
 
-#define TLS_TP_OFFSET	0x7000
-#define TLS_DTV_OFFSET	0x8000
+#define	TLS_DTV_OFFSET	0x8000
+#define	TLS_TCB_ALIGN	8
+#define	TLS_TP_OFFSET	0x7000
 
-#ifdef __mips_n64
-#define TLS_TCB_SIZE	16
 #ifdef COMPAT_FREEBSD32
-#define TLS_TCB_SIZE32	8
+#define	TLS_TCB_SIZE32	8
 #endif
+
+#ifndef _KERNEL
+
+static __inline void
+_tcb_set(struct tcb *tcb)
+{
+	sysarch(MIPS_SET_TLS, tcb);
+}
+
+static __inline struct tcb *
+_tcb_get(void)
+{
+	struct tcb *tcb;
+
+#ifdef TLS_USE_SYSARCH
+	sysarch(MIPS_GET_TLS, &tcb);
 #else
-#define TLS_TCB_SIZE	8
+	__asm__ __volatile__ (
+	    ".set\tpush\n\t"
+#ifdef __mips_n64
+	    ".set\tmips64r2\n\t"
+#else
+	    ".set\tmips32r2\n\t"
+#endif
+	    "rdhwr\t%0, $29\n\t"
+	    ".set\tpop"
+	    : "=r" (tcb));
+	tcb = (struct tcb *)((uintptr_t)tcb - TLS_TP_OFFSET - TLS_TCB_SIZE);
+#endif
+	return (tcb);
+}
+
 #endif
 
 #endif	/* __MIPS_TLS_H__ */
