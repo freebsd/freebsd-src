e735fa321224bb81fbd51907638293c22be40b8e
Mateusz Guzik <mjg@FreeBSD.org>
2021-12-09 20:39:40 +0000

    net/if.c: plug set-but-not-unused vars
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

--- sys/net/if.c
+++ sys/net/if.c
@@ -517,7 +517,8 @@ vnet_if_return(const void *unused __unused)
 {
 	struct ifnet *ifp, *nifp;
 	struct ifnet **pending;
-	int found, i;
+	int found __diagused;
+	int i;
 
 	i = 0;
 
@@ -1337,7 +1338,7 @@ if_vmove_loan(struct thread *td, struct ifnet *ifp, char *ifname, int jid)
 	struct prison *pr;
 	struct ifnet *difp;
 	int error;
-	bool found;
+	bool found __diagused;
 	bool shutdown;
 
 	/* Try to find the prison within our visibility. */
@@ -1394,7 +1395,7 @@ if_vmove_reclaim(struct thread *td, char *ifname, int jid)
 	struct prison *pr;
 	struct vnet *vnet_dst;
 	struct ifnet *ifp;
-	int error, found;
+	int error, found __diagused;
  	bool shutdown;
 
 	/* Try to find the prison within our visibility. */
