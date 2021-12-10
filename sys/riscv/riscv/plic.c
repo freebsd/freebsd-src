bdb28e3d8b92a97e0dac0ca36f208b7839e8d001
Jessica Clarke <jrtc27@FreeBSD.org>
2021-12-10 04:51:32 +0000

    riscv: Fix PLIC -Wunused-but-set-variable warnings

--- sys/riscv/riscv/plic.c
+++ sys/riscv/riscv/plic.c
@@ -402,12 +402,6 @@ static int
 plic_setup_intr(device_t dev, struct intr_irqsrc *isrc,
     struct resource *res, struct intr_map_data *data)
 {
-	struct plic_softc *sc;
-	struct plic_irqsrc *src;
-
-	sc = device_get_softc(dev);
-	src = (struct plic_irqsrc *)isrc;
-
 	CPU_ZERO(&isrc->isrc_cpu);
 	plic_bind_intr(dev, isrc);
 
