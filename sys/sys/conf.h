30c3a5f24825a8180ec18adb2921457436b3eb08
Florian Walpen <dev@submerge.ch>
2021-12-10 03:35:38 +0200

    Add idle priority scheduling privilege group to MAC/priority
    (cherry picked from commit a9545eede43b8fac889f3ec3180f5917f0d0b0ea)

--- sys/sys/conf.h
+++ sys/sys/conf.h
@@ -161,6 +161,7 @@ typedef int dumper_hdr_t(struct dumperinfo *di, struct kerneldumpheader *kdh,
 #define		GID_GAMES	13
 #define		GID_VIDEO	44
 #define		GID_RT_PRIO	47
+#define		GID_ID_PRIO	48
 #define		GID_DIALER	68
 #define		GID_NOGROUP	65533
 #define		GID_NOBODY	65534
