a7e96b7047d740f9458fa1b69245eabdac8f4d65
Jessica Clarke <jrtc27@FreeBSD.org>
2021-12-10 05:06:54 +0000

    xdma: Fix another -Wunused-but-set-variable warning previously missed
    MFC after:	1 week

--- sys/dev/xdma/xdma_bio.c
+++ sys/dev/xdma/xdma_bio.c
@@ -77,9 +77,6 @@ xdma_enqueue_bio(xdma_channel_t *xchan, struct bio **bp,
     enum xdma_direction dir)
 {
 	struct xdma_request *xr;
-	xdma_controller_t *xdma;
-
-	xdma = xchan->xdma;
 
 	xr = xchan_bank_get(xchan);
 	if (xr == NULL)
