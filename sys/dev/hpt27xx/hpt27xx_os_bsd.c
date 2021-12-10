63e0f96acecb4ca3aba778858361278177333dc1
Alexander Motin <mav@FreeBSD.org>
2021-12-09 20:24:29 -0500

    hpt27xx: Remove FreeBSD 9.x support including Giant.
    MFC after:	2 weeks

--- sys/dev/hpt27xx/hpt27xx_os_bsd.c
+++ sys/dev/hpt27xx/hpt27xx_os_bsd.c
@@ -250,14 +250,8 @@ void  os_request_timer(void * osext, HPT_U32 interval)
 	PVBUS_EXT vbus_ext = osext;
 
 	HPT_ASSERT(vbus_ext->ext_type==EXT_TYPE_VBUS);
-
-#if (__FreeBSD_version >= 1000510)
 	callout_reset_sbt(&vbus_ext->timer, SBT_1US * interval, 0,
 	    os_timer_for_ldm, vbus_ext, 0);
-#else 
-	untimeout(os_timer_for_ldm, vbus_ext, vbus_ext->timer);
-	vbus_ext->timer = timeout(os_timer_for_ldm, vbus_ext, interval * hz / 1000000);
-#endif
 }
 
 HPT_TIME os_query_time(void)
