6cccfeaafc298e2536bac9ef53a1ea0cbc1956dd
Rick Macklem <rmacklem@FreeBSD.org>
2021-12-11 15:00:30 -0800

    nfscl: Fix must_commit/writeverf handling for Direct I/O
    Without this patch, the KASSERT(must_commit == 0,..) can be
    triggered by the writeverf in the Direct I/O write reply changing.
    This is not a situation that should cause a panic(). Correct
    handling is to ignore the change in "writeverf" for Direct
    I/O, since it is done with NFSWRITE_FILESYNC.
    
    This patch modifies the semantics of the "must_commit"
    argument slightly, allowing an initial value of 2 to indicate
    that a change in "writeverf" should be ignored.
    It also fixes the KASSERT()s.
    
    This bug would affect few, since Direct I/O is not enabled
    by default and "writeverf" rarely changes. Normally "writeverf"
    only changes when a NFS server reboots, however I found the
    bug when testing against a Linux 5.15.1 kernel nfsd, which
    replied to a NFSWRITE_FILESYNC write with a "writeverf" of all
    0x0 bytes.
    
    (cherry picked from commit ead50c94cb604594987e6512289268891a427725)

--- sys/fs/nfsclient/nfs_clbio.c
+++ sys/fs/nfsclient/nfs_clbio.c
@@ -784,12 +784,26 @@ do_sync:
 			uio.uio_rw = UIO_WRITE;
 			uio.uio_td = td;
 			iomode = NFSWRITE_FILESYNC;
+			/*
+			 * When doing direct I/O we do not care if the
+			 * server's write verifier has changed, but we
+			 * do not want to update the verifier if it has
+			 * changed, since that hides the change from
+			 * writes being done through the buffer cache.
+			 * By passing must_commit in set to two, the code
+			 * in nfsrpc_writerpc() will not update the
+			 * verifier on the mount point.
+			 */
+			must_commit = 2;
 			error = ncl_writerpc(vp, &uio, cred, &iomode,
 			    &must_commit, 0);
-			KASSERT((must_commit == 0),
-				("ncl_directio_write: Did not commit write"));
+			KASSERT((must_commit == 2),
+			    ("ncl_directio_write: Updated write verifier"));
 			if (error)
 				return (error);
+			if (iomode != NFSWRITE_FILESYNC)
+				printf("nfs_directio_write: Broken server "
+				    "did not reply FILE_SYNC\n");
 			uiop->uio_offset += size;
 			uiop->uio_resid -= size;
 			if (uiop->uio_iov->iov_len <= size) {
@@ -1592,8 +1606,23 @@ ncl_doio_directwrite(struct buf *bp)
 
 	iomode = NFSWRITE_FILESYNC;
 	uiop->uio_td = NULL; /* NULL since we're in nfsiod */
+	/*
+	 * When doing direct I/O we do not care if the
+	 * server's write verifier has changed, but we
+	 * do not want to update the verifier if it has
+	 * changed, since that hides the change from
+	 * writes being done through the buffer cache.
+	 * By passing must_commit in set to two, the code
+	 * in nfsrpc_writerpc() will not update the
+	 * verifier on the mount point.
+	 */
+	must_commit = 2;
 	ncl_writerpc(bp->b_vp, uiop, bp->b_wcred, &iomode, &must_commit, 0);
-	KASSERT((must_commit == 0), ("ncl_doio_directwrite: Did not commit write"));
+	KASSERT((must_commit == 2), ("ncl_doio_directwrite: Updated write"
+	    " verifier"));
+	if (iomode != NFSWRITE_FILESYNC)
+		printf("ncl_doio_directwrite: Broken server "
+		    "did not reply FILE_SYNC\n");
 	free(iov_base, M_NFSDIRECTIO);
 	free(uiop->uio_iov, M_NFSDIRECTIO);
 	free(uiop, M_NFSDIRECTIO);
@@ -1864,7 +1893,7 @@ ncl_doio(struct vnode *vp, struct buf *bp, struct ucred *cr, struct thread *td,
 	    }
 	}
 	bp->b_resid = uiop->uio_resid;
-	if (must_commit)
+	if (must_commit == 1)
 	    ncl_clearcommit(vp->v_mount);
 	bufdone(bp);
 	return (error);
