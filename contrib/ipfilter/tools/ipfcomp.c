553a6bbba5fccc78e9e54ade09b956bd260be10f
Cy Schubert <cy@FreeBSD.org>
2021-12-10 20:56:10 -0800

    ipfcomp: remove set-but-not-unused vars
    (cherry picked from commit fe5b077c9efcbcc9e92d0d7d877d1e16afc563ce)

--- contrib/ipfilter/tools/ipfcomp.c
+++ contrib/ipfilter/tools/ipfcomp.c
@@ -65,7 +65,6 @@ static	FILE	*cfile = NULL;
 void printc(fr)
 	frentry_t *fr;
 {
-	fripf_t *ipf;
 	u_long *ulp;
 	char *and;
 	FILE *fp;
@@ -78,7 +77,6 @@ void printc(fr)
 	if ((fr->fr_type == FR_T_IPF) &&
 	    ((fr->fr_datype != FRI_NORMAL) || (fr->fr_satype != FRI_NORMAL)))
 		return;
-	ipf = fr->fr_ipf;
 
 	if (cfile == NULL)
 		cfile = fopen("ip_rules.c", "w");
