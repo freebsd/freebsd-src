524020aa3701928e19ae41bf696cb31574b4b8ca
Andriy Gapon <avg@FreeBSD.org>
2021-12-13 09:21:08 +0200

    ds3231: allow configuration via hints on FDT systems
    (cherry picked from commit 1256067c5c575474e626e5f119f50275ea2d975e)
    (cherry picked from commit b277ef3304fa97da041c191b02b2fcfb74378c75)

--- sys/dev/iicbus/ds3231.c
+++ sys/dev/iicbus/ds3231.c
@@ -356,16 +356,20 @@ ds3231_en32khz_sysctl(SYSCTL_HANDLER_ARGS)
 static int
 ds3231_probe(device_t dev)
 {
+	int rc;
 
 #ifdef FDT
 	if (!ofw_bus_status_okay(dev))
 		return (ENXIO);
-	if (!ofw_bus_is_compatible(dev, "maxim,ds3231"))
-		return (ENXIO);
+	if (ofw_bus_is_compatible(dev, "maxim,ds3231"))
+		rc = BUS_PROBE_DEFAULT;
+	else
 #endif
+		rc = BUS_PROBE_NOWILDCARD;
+
 	device_set_desc(dev, "Maxim DS3231 RTC");
 
-	return (BUS_PROBE_DEFAULT);
+	return (rc);
 }
 
 static int
