a21fc0475e2b2c4c5e97f5f905098958769f1a2f
Warner Losh <imp@FreeBSD.org>
2021-12-14 14:38:43 -0700

    loader: narrow the scope of gfx frame buffer wrt tg supported kernels
    Store whether or not we found a vbefb module (eg, a tg supported kernel)
    in the preloaded_file structure. This automatically resets on reload and
    eliminates load_elf knowing about any gfx_* interface. Restrict this to
    i386, which is the only place it's used. Update libi386 to check in the
    preloaded_file struct. Eliminate this from the teken_gfx
    structure. Rewrite the parsing code to be more inline. Check this from
    the same place we check for a relocatable amd64 kernel.
    
    Sponsored by:		Netflix
    Reviewed by:		manu, tsoome
    Differential Revision:	https://reviews.freebsd.org/D33427
    
    (cherry picked from commit aaaa5a2e68e256101c9737c30ad142f1c3f05131)

--- stand/common/gfx_fb.h
+++ stand/common/gfx_fb.h
@@ -220,7 +220,6 @@ typedef struct teken_gfx {
 	size_t		tg_shadow_sz;		/* units of pages */
 	teken_funcs_t	*tg_functions;
 	void		*tg_private;
-	bool		tg_kernel_supported;	/* Loaded kernel is supported */
 } teken_gfx_t;
 
 extern font_list_t fonts;
--- stand/common/module.c
+++ stand/common/module.c
@@ -278,8 +278,6 @@ unload(void)
 	}
 	loadaddr = 0;
 	unsetenv("kernelname");
-	/* Reset tg_kernel_supported to allow next load to check it again. */
-	gfx_state.tg_kernel_supported = false;
 }
 
 COMMAND_SET(unload, "unload", "unload all modules", command_unload);
