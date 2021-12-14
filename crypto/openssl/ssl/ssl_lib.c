27bb8830d555621d4292da8a83f3bc09176fd00d
John Baldwin <jhb@FreeBSD.org>
2021-12-14 10:07:38 -0800

    SSL_sendfile: Replace ERR_raise_data with SYSerr.
    ERR_raise_data is only present in OpenSSL 3.0 and later.
    
    Reviewed by:	jkim
    Obtained from:	CheriBSD
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D33363

--- crypto/openssl/ssl/ssl_lib.c
+++ crypto/openssl/ssl/ssl_lib.c
@@ -2026,7 +2026,8 @@ ossl_ssize_t SSL_sendfile(SSL *s, int fd, off_t offset, size_t size, int flags)
     }
 
 #ifdef OPENSSL_NO_KTLS
-    ERR_raise_data(ERR_LIB_SYS, ERR_R_INTERNAL_ERROR, "calling sendfile()");
+    SYSerr(SSL_F_SSL_SENDFILE, ERR_R_INTERNAL_ERROR);
+    ERR_add_error_data(1, "calling sendfile()");
     return -1;
 #else
     ret = ktls_sendfile(SSL_get_wfd(s), fd, offset, size, flags);
