211db3b927d10bd7290606d07df298eab96a0f8d
Warner Losh <imp@FreeBSD.org>
2021-12-14 21:09:53 -0700

    stand/uboot: reorg
    Build uboot ubldr and friends like we build efi binaries
    o move everything to be under stand/uboot
    o md code goes in arch/$ARCH
    o move everything over from the library
      - Had to rename console.c, disk.c and module.c due to conflicts
    o update version to 1.5 to reflect the new way of building
    
    This results in a more consistent build system and should represent no
    functional change, apart from powerpc version getting new help
    file. Also, moved to exlcuding uboot on powerpc64le by using
    BROKEN_OPTION instead of the incidental exclusion we had before due to
    Makefile reorgs.
    
    Sponsored by:		Netflix
    Feedback by:		stevek, jrtc27
    Differential Revision:	https://reviews.freebsd.org/D33362

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -314,8 +314,8 @@ BROKEN_OPTIONS+=EFI
 .if ${__T:Mpowerpc*} == ""
 BROKEN_OPTIONS+=LOADER_OFW
 .endif
-# UBOOT is only for arm, mips and powerpc, exclude others
-.if ${__T:Marm*} == "" && ${__T:Mmips*} == "" && ${__T:Mpowerpc*} == ""
+# UBOOT is only for arm, and big-endian powerpc
+.if (${__T:Marm*} == "" && ${__T:Mpowerpc*} == "") || ${__T} == "powerpc64le"
 BROKEN_OPTIONS+=LOADER_UBOOT
 .endif
 # GELI and Lua in loader currently cause boot failures on powerpc.
