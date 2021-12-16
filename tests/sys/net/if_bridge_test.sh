d2c6d8cacf27950229c74caa53f51b48ba26d8a3
Warner Losh <imp@FreeBSD.org>
2021-12-15 18:30:41 -0700

    tests: sys.net.if_bridge_test.span is failing in CI
    sys.net.if_bridge_test.span has been failing in CI for a while
    now. Disable it until this can be resolved.
    
    PR:		260461
    Sponsored by:	Netflix

--- tests/sys/net/if_bridge_test.sh
+++ tests/sys/net/if_bridge_test.sh
@@ -275,6 +275,10 @@ span_body()
 	set -x
 	vnet_init
 
+	if [ "$(atf_config_get ci false)" = "true" ]; then
+		atf_skip "https://bugs.freebsd.org/260461"
+	fi
+
 	epair=$(vnet_mkepair)
 	epair_span=$(vnet_mkepair)
 	bridge=$(vnet_mkbridge)
