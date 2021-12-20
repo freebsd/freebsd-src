a086650f6dc864806e65d8020f6eae58a1da0ea5
Ram Kishore Vegesna <ram@FreeBSD.org>
2021-12-20 17:55:13 +0530

    ocs_fc: Populate subvendor and subdevice ids. Enable serialnumber reporting.
    Reviewed by: mav
    MFC after: 3 days
    
    (cherry picked from commit cba757ef2a7ec2bb985fbcd67e8d616328b7209a)
    
    ocs_fs(4): Fix some common typos in source code comments
    
    - s/transfered/transferred/
    - s/associted/associated/
    
    MFC after:	3 days
    
    (cherry picked from commit 16b71d98d96a3a9a2ac666440aa80f12d478959d)

--- sys/dev/ocs_fc/ocs_scsi.c
+++ sys/dev/ocs_fc/ocs_scsi.c
@@ -44,9 +44,7 @@
 #include "ocs.h"
 #include "ocs_els.h"
 #include "ocs_scsi.h"
-#if defined(OCS_ENABLE_VPD_SUPPORT)
 #include "ocs_vpd.h"
-#endif
 #include "ocs_utils.h"
 #include "ocs_device.h"
 
@@ -2826,7 +2824,6 @@ void *ocs_scsi_get_property_ptr(ocs_t *ocs, ocs_scsi_property_e prop)
 	case OCS_SCSI_BIOS_VERSION_STRING:
 		rc = ocs_hw_get_ptr(&ocs->hw, OCS_HW_BIOS_VERSION_STRING);
 		break;
-#if defined(OCS_ENABLE_VPD_SUPPORT)
 	case OCS_SCSI_SERIALNUMBER:
 	{
 		uint8_t *pvpd;
@@ -2877,7 +2874,6 @@ void *ocs_scsi_get_property_ptr(ocs_t *ocs, ocs_scsi_property_e prop)
 		}
 		break;
 	}
-#endif
 	default:
 		break;
 	}
