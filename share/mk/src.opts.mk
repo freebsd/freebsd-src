01336676d58a638975d8cac5de70f245ad26ec54
Piotr Kubaj <pkubaj@FreeBSD.org>
2021-12-21 17:24:20 +0100

    riscv64*: enable OFED
    Summary:
    OFED on riscv64 builds fine, so it's ok to enable it.
    
    Also MFC to stable/13.
    
    Test Plan: make buildworld
    
    Reviewers: mhorne
    
    Subscribers: bdrewery, imp, emaste
    
    Differential Revision: https://reviews.freebsd.org/D33602

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -280,9 +280,6 @@ __DEFAULT_NO_OPTIONS+=LLVM_TARGET_BPF LLVM_TARGET_MIPS
 
 .include <bsd.compiler.mk>
 
-.if ${__T:Mriscv*} != ""
-BROKEN_OPTIONS+=OFED
-.endif
 .if ${__T} == "aarch64" || ${__T} == "amd64" || ${__T} == "i386"
 __DEFAULT_YES_OPTIONS+=LLDB
 .else
