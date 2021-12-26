7518562484e817ebdcba27f440485695707bfa55
Alexander Motin <mav@FreeBSD.org>
2021-12-25 21:40:14 -0500

    hwpmc: Remove CTLFLAG_NEEDGIANT from sysctl.
    MFC after:	2 weeks
    
    (cherry picked from commit 1886cef69bf270bbbe7e9a0fd2a212fc17b76204)

--- sys/dev/hwpmc/hwpmc_mod.c
+++ sys/dev/hwpmc/hwpmc_mod.c
@@ -314,7 +314,7 @@ char	pmc_debugstr[PMC_DEBUG_STRSIZE];
 TUNABLE_STR(PMC_SYSCTL_NAME_PREFIX "debugflags", pmc_debugstr,
     sizeof(pmc_debugstr));
 SYSCTL_PROC(_kern_hwpmc, OID_AUTO, debugflags,
-    CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_NOFETCH | CTLFLAG_NEEDGIANT,
+    CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_NOFETCH | CTLFLAG_MPSAFE,
     0, 0, pmc_debugflags_sysctl_handler, "A",
     "debug flags");
 #endif
