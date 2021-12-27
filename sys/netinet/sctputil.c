34ae6a1a442881681a9c63e93a189b9191db50ed
Michael Tuexen <tuexen@FreeBSD.org>
2021-12-27 18:28:44 +0100

    sctp: cleanup, on functional change intended.
    MFC after:	3 days

--- sys/netinet/sctputil.c
+++ sys/netinet/sctputil.c
@@ -2895,13 +2895,12 @@ sctp_timer_stop(int t_type, struct sctp_inpcb *inp, struct sctp_tcb *stcb,
 uint32_t
 sctp_calculate_len(struct mbuf *m)
 {
-	uint32_t tlen = 0;
 	struct mbuf *at;
+	uint32_t tlen;
 
-	at = m;
-	while (at) {
+	tlen = 0;
+	for (at = m; at != NULL; at = SCTP_BUF_NEXT(at)) {
 		tlen += SCTP_BUF_LEN(at);
-		at = SCTP_BUF_NEXT(at);
 	}
 	return (tlen);
 }
