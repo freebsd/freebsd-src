1b1f80ae1cfd97968ea3832d4e51df3ec7ba275c
Alexander Motin <mav@FreeBSD.org>
2021-12-26 20:37:55 -0500

    sound: Remove CTLFLAG_NEEDGIANT from some sysctls.
    While there, remove some dead code.
    
    MFC after:	2 weeks
    
    (cherry picked from commit 3b4c5433229ac912929b77edee0b4dc001d70fe5)

--- sys/dev/sound/pcm/sndstat.c
+++ sys/dev/sound/pcm/sndstat.c
@@ -101,7 +101,7 @@ sysctl_hw_sndverbose(SYSCTL_HANDLER_ARGS)
 	return (error);
 }
 SYSCTL_PROC(_hw_snd, OID_AUTO, verbose,
-    CTLTYPE_INT | CTLFLAG_RWTUN | CTLFLAG_NEEDGIANT, 0, sizeof(int),
+    CTLTYPE_INT | CTLFLAG_RWTUN | CTLFLAG_MPSAFE, 0, sizeof(int),
     sysctl_hw_sndverbose, "I",
     "verbosity level");
 
