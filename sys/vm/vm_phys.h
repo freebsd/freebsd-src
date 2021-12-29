8119cdd38b4f3ebdba92d8d0658fabbc04a1f238
Doug Moore <dougm@FreeBSD.org>
2021-12-29 11:10:31 -0600

    vm_phys: hide vm_phys_set_pool
    It is only called in the file that defines it, so make it static and
    remove the declaration from the header.
    
    Reviewed by:	kib
    Differential Revision:	https://reviews.freebsd.org/D33688

--- sys/vm/vm_phys.h
+++ sys/vm/vm_phys.h
@@ -79,7 +79,6 @@ void vm_phys_register_domains(int ndomains, struct mem_affinity *affinity,
     int *locality);
 vm_page_t vm_phys_scan_contig(int domain, u_long npages, vm_paddr_t low,
     vm_paddr_t high, u_long alignment, vm_paddr_t boundary, int options);
-void vm_phys_set_pool(int pool, vm_page_t m, int order);
 boolean_t vm_phys_unfree_page(vm_page_t m);
 int vm_phys_mem_affinity(int f, int t);
 void vm_phys_early_add_seg(vm_paddr_t start, vm_paddr_t end);
