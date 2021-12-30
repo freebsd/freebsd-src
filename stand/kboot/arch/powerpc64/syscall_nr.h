0784d61020e428806eee2e335e7013c2ab50c086
Warner Losh <imp@FreeBSD.org>
2021-12-30 16:07:20 -0700

    kboot: move to generic syscall interface
    Just have the MD code provide syscall and have generic code for the
    rest.
    
    Sponsored by:		Netflix
    Differential Revision:	https://reviews.freebsd.org/D33515

---
+++ stand/kboot/arch/powerpc64/syscall_nr.h
@@ -0,0 +1,15 @@
+#define SYS_read		  3
+#define SYS_write		  4
+#define SYS_open		  5
+#define SYS_close		  6
+#define SYS_gettimeofday	 78
+#define SYS_reboot		 88
+#define SYS_mmap		 90
+#define SYS_uname		120
+#define SYS_llseek		140
+#define SYS_getdents		141
+#define SYS_select		142
+#define __NR_kexec_load		268
+
+#define KEXEC_ARCH_PPC64	 21
+#define KEXEC_ARCH		KEXEC_ARCH_PPC64
