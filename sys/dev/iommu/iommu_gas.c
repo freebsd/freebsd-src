cfb2aa3f530f45897c1059a45ab82479d90d63fc
Doug Moore <dougm@FreeBSD.org>
2021-12-31 01:48:11 -0600

    iommu_gas: Rename a function missed earlier
    Reported by:	jenkins
    Fixes:	c606ab59e7f9 vm_extern: use standard address checkers everywhere

--- sys/dev/iommu/iommu_gas.c
+++ sys/dev/iommu/iommu_gas.c
@@ -329,7 +329,7 @@ iommu_gas_match_one(struct iommu_gas_match_args *a, iommu_gaddr_t beg,
 	/* IOMMU_PAGE_SIZE to create gap after new entry. */
 	if (start + a->offset + a->size + IOMMU_PAGE_SIZE <= end &&
 	    start + a->offset + a->size <= maxaddr &&
-	    iommu_test_boundary(start + a->offset, a->size,
+	    vm_addr_bound_ok(start + a->offset, a->size,
 	    a->common->boundary)) {
 		a->entry->start = start;
 		return (true);
