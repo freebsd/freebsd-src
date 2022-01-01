e6c63359e3d1bc1fbb97de9e30a83d4b6ab38f79
Warner Losh <imp@FreeBSD.org>
2022-01-01 09:49:49 -0700

    Welcome 2022, update copyrights.
    Sponsored by:		Netflix

--- sys/sys/copyright.h
+++ sys/sys/copyright.h
@@ -1,7 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
- * Copyright (C) 1992-2021 The FreeBSD Project. All rights reserved.
+ * Copyright (C) 1992-2022 The FreeBSD Project. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,7 +33,7 @@
 
 /* FreeBSD */
 #define COPYRIGHT_FreeBSD \
-	"Copyright (c) 1992-2021 The FreeBSD Project.\n"
+	"Copyright (c) 1992-2022 The FreeBSD Project.\n"
 
 /* Foundation */
 #define	TRADEMARK_Foundation \
