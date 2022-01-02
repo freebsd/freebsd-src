ad0a65469ac8db2840857167e731c4314993f97b
Warner Losh <imp@FreeBSD.org>
2022-01-01 18:11:51 -0700

    bsd.endian.mk: Remove arm big endian
    Remove vestiges of arm big endian support. Also use the more proper
    MACHINE_CPUARCH instead of MACHINE to test for that here.
    
    This leaves powerpc as the only big endian arch.
    
    Sponsored by:		Netflix

--- share/mk/bsd.endian.mk
+++ share/mk/bsd.endian.mk
@@ -3,7 +3,7 @@
 .if ${MACHINE_CPUARCH} == "aarch64" || \
     ${MACHINE_ARCH} == "amd64" || \
     ${MACHINE_ARCH} == "i386" || \
-    (${MACHINE} == "arm" && ${MACHINE_ARCH:Marm*eb*} == "") || \
+    ${MACHINE_CPUARCH} == "arm" || \
     ${MACHINE_ARCH} == "powerpc64le" || \
     ${MACHINE_CPUARCH} == "riscv"
 TARGET_ENDIANNESS= 1234
@@ -11,9 +11,10 @@ CAP_MKDB_ENDIAN= -l
 LOCALEDEF_ENDIAN= -l
 .elif ${MACHINE_ARCH} == "powerpc" || \
     ${MACHINE_ARCH} == "powerpc64" || \
-    ${MACHINE_ARCH} == "powerpcspe" || \
-    (${MACHINE} == "arm" && ${MACHINE_ARCH:Marm*eb*} != "")
+    ${MACHINE_ARCH} == "powerpcspe"
 TARGET_ENDIANNESS= 4321
 CAP_MKDB_ENDIAN= -b
 LOCALEDEF_ENDIAN= -b
+.else
+.error "Don't know the endian of this architecture"
 .endif
