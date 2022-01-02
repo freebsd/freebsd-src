750200fe62b800e53e4123865b589c13d76b9af9
Warner Losh <imp@FreeBSD.org>
2022-01-01 18:10:21 -0700

    bsd.endian.mk: Remove mips
    Remove the enumeration of the big vs little endian platform names.
    
    Sponsored by:		Netflix

--- share/mk/bsd.endian.mk
+++ share/mk/bsd.endian.mk
@@ -5,16 +5,14 @@
     ${MACHINE_ARCH} == "i386" || \
     (${MACHINE} == "arm" && ${MACHINE_ARCH:Marm*eb*} == "") || \
     ${MACHINE_ARCH} == "powerpc64le" || \
-    ${MACHINE_CPUARCH} == "riscv" || \
-    ${MACHINE_ARCH:Mmips*el*} != ""
+    ${MACHINE_CPUARCH} == "riscv"
 TARGET_ENDIANNESS= 1234
 CAP_MKDB_ENDIAN= -l
 LOCALEDEF_ENDIAN= -l
 .elif ${MACHINE_ARCH} == "powerpc" || \
     ${MACHINE_ARCH} == "powerpc64" || \
     ${MACHINE_ARCH} == "powerpcspe" || \
-    (${MACHINE} == "arm" && ${MACHINE_ARCH:Marm*eb*} != "") || \
-    ${MACHINE_ARCH:Mmips*} != ""
+    (${MACHINE} == "arm" && ${MACHINE_ARCH:Marm*eb*} != "")
 TARGET_ENDIANNESS= 4321
 CAP_MKDB_ENDIAN= -b
 LOCALEDEF_ENDIAN= -b
