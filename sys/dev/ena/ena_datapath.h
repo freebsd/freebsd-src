eff45c1e66279c56cbb2e53ad89cffcce2c1ed1d
Dawid Gorecki <dgr@semihalf.com>
2022-01-03 14:49:58 +0100

    ena: properly handle IPv6 L4 checksum offload
    ena_tx_csum function did not check if IPv6 checksum offload was
    requested it only checked checksum offloading flags for IPv4 packets.
    Because of that, when encountering CSUM_IP6_* flags, the function simply
    returned without actually setting checksum offloading in ena_ctx.
    Check CUSM_IP6_* flags to enable IPv6 checksum offload.
    
    Additionally, only IPv4 header was being parsed regardless of EtherType
    field, because of that, value of L4 protocol read when actually trying
    to send IPv6 packets was wrong. Use ip6_lasthdr function to get length
    of all IPv6 headers and payload protocol.
    
    Set the DF flag to 1 in order to allow the device to offload the IPv6
    checksum calculation and achieve optimal performance.
    
    Add CSUM6_OFFLOAD and CSUM_OFFLOAD definitions into ena_datapath.h.
    
    Submitted by: Dawid Gorecki <dgr@semihalf.com>
    Obtained from: Semihalf
    MFC after: 2 weeks
    Sponsored by: Amazon, Inc.
    
    (cherry picked from commit 2bbef9d95dc10a69a3c5813a517f6e8fe583539a)

--- sys/dev/ena/ena_datapath.h
+++ sys/dev/ena/ena_datapath.h
@@ -40,5 +40,6 @@ int	ena_mq_start(if_t ifp, struct mbuf *m);
 void	ena_deferred_mq_start(void *arg, int pending);
 
 #define CSUM_OFFLOAD 	(CSUM_IP|CSUM_TCP|CSUM_UDP)
+#define CSUM6_OFFLOAD	(CSUM_IP6_UDP|CSUM_IP6_TCP)
 
 #endif /* ENA_TXRX_H */
