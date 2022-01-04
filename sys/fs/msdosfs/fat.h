fab22bfae92d6d858fed0b15efd3a6e3d02af954
Konstantin Belousov <kib@FreeBSD.org>
2022-01-04 16:43:02 +0200

    msdosfs: clusterfree() is used only in error handling cases
    Change its return type to void, because its result is ignored in both
    call sites.  Remove oldcnp argument as well, it is NULL always.
    
    Suggested and reviewed by:	markj
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D33721

--- sys/fs/msdosfs/fat.h
+++ sys/fs/msdosfs/fat.h
@@ -97,7 +97,7 @@
 #define	DE_CLEAR	1	/* Zero out the blocks allocated */
 
 int pcbmap(struct denode *dep, u_long findcn, daddr_t *bnp, u_long *cnp, int* sp);
-int clusterfree(struct msdosfsmount *pmp, u_long cn, u_long *oldcnp);
+void clusterfree(struct msdosfsmount *pmp, u_long cn);
 int clusteralloc(struct msdosfsmount *pmp, u_long start, u_long count, u_long fillwith, u_long *retcluster, u_long *got);
 int fatentry(int function, struct msdosfsmount *pmp, u_long cluster, u_long *oldcontents, u_long newcontents);
 int freeclusterchain(struct msdosfsmount *pmp, u_long startchain);
