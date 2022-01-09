freebsd/freebsd-src

commit a21f086a3316346ba04f9381bf10e463a8328394

Fix "set but not used" in the tws driver
Sponsored by: Rubicon Communications, LLC ("Netgate")

sys/dev/tws/tws.c
@@ -349,7 +349,7 @@ tws_detach(device_t dev)
 {
     struct tws_softc *sc = device_get_softc(dev);
     int i;
-    u_int32_t reg;
+    u_int32_t reg __diagused;
 
     TWS_TRACE_DEBUG(sc, "entry", 0, 0);
 
@@ -452,11 +452,11 @@ tws_setup_intr(struct tws_softc *sc, int irqs)
 int
 tws_teardown_intr(struct tws_softc *sc)
 {
-    int i, error;
+    int i;
 
     for(i=0;i<sc->irqs;i++) {
         if (sc->intr_handle[i]) {
-            error = bus_teardown_intr(sc->tws_dev,
+            bus_teardown_intr(sc->tws_dev,
                                       sc->irq_res[i], sc->intr_handle[i]);
             sc->intr_handle[i] = NULL;
         }
@@ -509,7 +509,6 @@ tws_init(struct tws_softc *sc)
 
     u_int32_t max_sg_elements;
     u_int32_t dma_mem_size;
-    int error;
     u_int32_t reg;
 
     sc->seq_id = 0;
@@ -569,7 +568,7 @@ tws_init(struct tws_softc *sc)
 
     /* if bus_dmamem_alloc succeeds then bus_dmamap_load will succeed */
     sc->dma_mem_phys=0;
-    error = bus_dmamap_load(sc->cmd_tag, sc->cmd_map, sc->dma_mem,
+    bus_dmamap_load(sc->cmd_tag, sc->cmd_map, sc->dma_mem,
                     dma_mem_size, tws_dmamap_cmds_load_cbfn,
                     &sc->dma_mem_phys, 0);
 

sys/dev/tws/tws_hdm.c
@@ -82,7 +82,7 @@ void tws_display_ctlr_info(struct tws_softc *sc);
 int 
 tws_init_ctlr(struct tws_softc *sc)
 {
-    u_int64_t reg;
+    u_int64_t reg __diagused;
     u_int32_t regh, regl;
 
     TWS_TRACE_DEBUG(sc, "entry", sc, sc->is64bit);

sys/dev/tws/tws_cam.c
@@ -1067,24 +1067,22 @@ tws_intr(void *arg)
 static void
 tws_intr_attn_aen(struct tws_softc *sc)
 {
-    u_int32_t db=0;
 
     /* maskoff db intrs until all the aens are fetched */
     /* tws_disable_db_intr(sc); */
     tws_fetch_aen((void *)sc);
     tws_write_reg(sc, TWS_I2O0_HOBDBC, TWS_BIT18, 4);
-    db = tws_read_reg(sc, TWS_I2O0_IOBDB, 4);
+    (void)tws_read_reg(sc, TWS_I2O0_IOBDB, 4);
 
 }
 
 static void
 tws_intr_attn_error(struct tws_softc *sc)
 {
-    u_int32_t db=0;
 
     TWS_TRACE(sc, "attn error", 0, 0);
     tws_write_reg(sc, TWS_I2O0_HOBDBC, ~0, 4);
-    db = tws_read_reg(sc, TWS_I2O0_IOBDB, 4);
+    (void)tws_read_reg(sc, TWS_I2O0_IOBDB, 4);
     device_printf(sc->tws_dev, "Micro controller error.\n");
     tws_reset(sc);
 }

commit 174d86a01bf69a042ff34902ecb6c4d490b36996

tws: fix "set but not used" in the tws driver
With __diagused, these warnings were still emitted since INVARIANTS was
defined but TWS_DEBUG was not.

Fixes:  a21f086a3316 ("Fix "set but not used" in the tws driver")
Differential Revision:	https://reviews.freebsd.org/D33784

sys/dev/tws/tws.c
@@ -349,7 +349,7 @@ tws_detach(device_t dev)
 {
     struct tws_softc *sc = device_get_softc(dev);
     int i;
-    u_int32_t reg __diagused;
+    u_int32_t reg __tws_debug;
 
     TWS_TRACE_DEBUG(sc, "entry", 0, 0);
 

sys/dev/tws/tws_hdm.c
@@ -82,7 +82,7 @@ void tws_display_ctlr_info(struct tws_softc *sc);
 int 
 tws_init_ctlr(struct tws_softc *sc)
 {
-    u_int64_t reg __diagused;
+    u_int64_t reg __tws_debug;
     u_int32_t regh, regl;
 
     TWS_TRACE_DEBUG(sc, "entry", sc, sc->is64bit);

sys/dev/tws/tws_services.h
@@ -61,10 +61,12 @@ void tws_init_qs(struct tws_softc *sc);
 #endif
 
 #ifdef TWS_TRACE_DEBUG_ON
+    #define __tws_debug
     #define TWS_TRACE_DEBUG(sc, desc, val1, val2) \
             tws_trace(__FILE__, __func__, __LINE__, sc, desc, \
                                    (u_int64_t)val1, (u_int64_t)val2)
 #else
+    #define __tws_debug __unused
     #define TWS_TRACE_DEBUG(sc, desc, val1, val2)
 #endif
 