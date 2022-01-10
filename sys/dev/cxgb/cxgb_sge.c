ac1b4a8e1ac5b8f5953ad5fdd4d40fadc28e8dd7
Navdeep Parhar <np@FreeBSD.org>
2022-01-10 11:18:38 -0800

    cxgb(4): Fix "set but not used [-Wunused-but-set-variable]" warnings.
    Sponsored by:	Chelsio Communications
    
    (cherry picked from commit 6792568fe2687db960cb02cfc744b2e87947d38b)

--- sys/dev/cxgb/cxgb_sge.c
+++ sys/dev/cxgb/cxgb_sge.c
@@ -2105,27 +2105,8 @@ t3_sge_start(adapter_t *sc)
 void
 t3_sge_stop(adapter_t *sc)
 {
-	int i, nqsets;
-	
-	t3_set_reg_field(sc, A_SG_CONTROL, F_GLOBALENABLE, 0);
 
-	if (sc->tq == NULL)
-		return;
-	
-	for (nqsets = i = 0; i < (sc)->params.nports; i++) 
-		nqsets += sc->port[i].nqsets;
-#ifdef notyet
-	/*
-	 * 
-	 * XXX
-	 */
-	for (i = 0; i < nqsets; ++i) {
-		struct sge_qset *qs = &sc->sge.qs[i];
-		
-		taskqueue_drain(sc->tq, &qs->txq[TXQ_OFLD].qresume_task);
-		taskqueue_drain(sc->tq, &qs->txq[TXQ_CTRL].qresume_task);
-	}
-#endif
+	t3_set_reg_field(sc, A_SG_CONTROL, F_GLOBALENABLE, 0);
 }
 
 /**
@@ -2337,11 +2318,9 @@ restart_offloadq(void *data, int npending)
 	struct sge_qset *qs = data;
 	struct sge_txq *q = &qs->txq[TXQ_OFLD];
 	adapter_t *adap = qs->port->adapter;
-	int cleaned;
-		
-	TXQ_LOCK(qs);
-again:	cleaned = reclaim_completed_tx(qs, 16, TXQ_OFLD);
 
+	TXQ_LOCK(qs);
+again:
 	while ((m = mbufq_first(&q->sendq)) != NULL) {
 		unsigned int gen, pidx;
 		struct ofld_hdr *oh = mtod(m, struct ofld_hdr *);
