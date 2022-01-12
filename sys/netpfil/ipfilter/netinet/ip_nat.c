c5300cae907a24ace5764d447a3ce60190fcb8aa
Cy Schubert <cy@FreeBSD.org>
2022-01-12 13:03:46 -0800

    ipfilter: Fixup comment
    Fix comment documenting checksum block in ip_nat.c. Fastforward doesn't
    perform checksum.
    
    MFC after:	1 week

--- sys/netpfil/ipfilter/netinet/ip_nat.c
+++ sys/netpfil/ipfilter/netinet/ip_nat.c
@@ -5021,10 +5021,8 @@ ipf_nat_out(fr_info_t *fin, nat_t *nat, int natadd, u_32_t nflags)
     defined(BRIDGE_IPF) || defined(__FreeBSD__)
 	else {
 		/*
-		 * Strictly speaking, this isn't necessary on BSD
-		 * kernels because they do checksum calculation after
-		 * this code has run BUT if ipfilter is being used
-		 * to do NAT as a bridge, that code doesn't exist.
+		 * We always do this on FreeBSD because this code doesn't
+		 * exist in fastforward.
 		 */
 		switch (nat->nat_dir)
 		{
