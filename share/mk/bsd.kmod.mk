9b6aa99e1f20577c361cd57e427a5a95013dbac1
Emmanuel Vadot <manu@FreeBSD.org>
2022-01-13 15:59:42 +0100

    Revert "Introduce MODULES_EXCLUDE"
    This is already present as WITHOUT_MODULES
    
    This reverts commit 8c0c5bdf9d5ebe08bb239633c16a567eb8a9f898.
    
    Reported by:	jrtc27

--- share/mk/bsd.kmod.mk
+++ share/mk/bsd.kmod.mk
@@ -1,10 +1,4 @@
 # $FreeBSD$
 
-.if defined(MODULES_EXCLUDE) && defined(KMOD) && ${MODULES_EXCLUDE:M${KMOD}}
-all:
-install:
-cleandir:
-.else
 .include <bsd.sysdir.mk>
 .include "${SYSDIR}/conf/kmod.mk"
-.endif
