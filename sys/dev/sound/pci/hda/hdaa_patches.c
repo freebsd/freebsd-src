emaste/freebsd

sound: add patch for Lenovo Legion 5 AMD
MFC after:	1 week
Differential Revision:	https://reviews.freebsd.org/D30333

sys/dev/sound/pci/hda/hdaa_patches.c
@@ -389,6 +389,16 @@ hdac_pin_patch(struct hdaa_widget *w)
 			patch = "as=1 seq=15";
 			break;
 		}
+	} else if (id == HDA_CODEC_ALC257 &&
+	    (subid == LENOVO_L5AMD_SUBVENDOR)) {
+		switch (nid) {
+		case 20:
+			patch = "as=1 seq=0";
+			break;
+		case 33:
+			patch = "as=1 seq=15";
+			break;
+		}
 	} else if (id == HDA_CODEC_ALC285 &&
 	    (subid == LENOVO_X120KH_SUBVENDOR ||
 	    subid == LENOVO_X120QD_SUBVENDOR)) {

freebsd/freebsd-src

sys/dev/sound/pci/hda/hdaa_patches.c
@@ -312,6 +312,16 @@ hdac_pin_patch(struct hdaa_widget *w)
 			patch_str = "as=1 seq=15";
 			break;
 		}
+	} else if (id == HDA_CODEC_ALC257 &&
+	    (subid == LENOVO_L5AMD_SUBVENDOR)) {
+		switch (nid) {
+		case 20:
+			patch_str = "as=1 seq=0";
+			break;
+		case 33:
+			patch_str = "as=1 seq=15";
+			break;
+		}
 	} else {
 		/*
 		 * loop over hdaa_model_pin_patch