ebaa3b8426a411e51117a2b9a8f74fe80ceda61c
John Baldwin <jhb@FreeBSD.org>
2022-01-24 15:27:39 -0800

    crypto: Add an API supporting curve25519.
    This adds a wrapper around libsodium's curve25519 support matching
    Linux's curve25519 API.  The intended use case for this is WireGuard.
    
    Note that this is not integrated with OCF as it is not related to
    symmetric operations on data.
    
    Reviewed by:	markj
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D33935

--- sys/crypto/libsodium/utils.c
+++ sys/crypto/libsodium/utils.c
@@ -1,4 +1,21 @@
-/* This file is in the public domain. */
+/*
+ * ISC License
+ *
+ * Copyright (c) 2013-2018
+ * Frank Denis <j at pureftpd dot org>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
@@ -11,4 +28,16 @@ void
 sodium_memzero(void *b, size_t n)
 {
 	explicit_bzero(b, n);
+}
+
+int
+sodium_is_zero(const unsigned char *n, const size_t nlen)
+{
+    size_t                 i;
+    volatile unsigned char d = 0U;
+
+    for (i = 0U; i < nlen; i++) {
+        d |= n[i];
+    }
+    return 1 & ((d - 1) >> 8);
 }
