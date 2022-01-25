389844c058ec1ab69bb506601ed673fdd4e4d12b
Baptiste Daroussin <bapt@FreeBSD.org>
2022-01-25 09:17:36 +0100

    locate: change from BSD-4-clause to BSD-3-clause
    We have the authorization from the University of California to remove
    the advertising clause for a while, wosch@ who also hold a copyright
    on this code also approved the relicensing
    
    Approved by:	wosch@
    MFC after:	3 days

--- usr.bin/locate/locate/locate.h
+++ usr.bin/locate/locate/locate.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-License-Identifier: BSD-4-Clause
+ * SPDX-License-Identifier: BSD-3-Clause
  *
  * Copyright (c) 1995 Wolfram Schneider <wosch@FreeBSD.org>. Berlin.
  * Copyright (c) 1989, 1993
@@ -13,11 +13,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
