1a233fd317690e5eaed1476756ac3e991ca03ff2
Mark Johnston <markj@FreeBSD.org>
2022-01-28 12:49:28 -0500

    sort: Fix message catalogue usage
    - Check that catopen() succeeded before calling catclose().  musl will
      crash in the latter if the catalogue descriptor is -1.
    - Keep the message catalogue open for most of sort(1)'s actual
      operation.
    - Don't use catgets(3) to print error messages if catopen(3) had failed.
    
    Reviewed by:	arichardson, emaste
    Sponsored by:	The FreeBSD Foundation
    
    (cherry picked from commit 8d8b9b560afdf88f5537f259a4a14660d2a85b1f)

--- usr.bin/sort/sort.c
+++ usr.bin/sort/sort.c
@@ -57,7 +57,7 @@ __FBSDID("$FreeBSD$");
 
 #ifndef WITHOUT_NLS
 #include <nl_types.h>
-nl_catd catalog;
+nl_catd catalog = (nl_catd)-1;
 #endif
 
 #define	OPTIONS	"bcCdfghik:Mmno:RrsS:t:T:uVz"
@@ -1011,6 +1011,10 @@ main(int argc, char **argv)
 	set_tmpdir();
 	set_sort_opts();
 
+#ifndef WITHOUT_NLS
+	catalog = catopen("sort", NL_CAT_LOCALE);
+#endif
+
 	fix_obsolete_keys(&argc, argv);
 
 	while (((c = getopt_long(argc, argv, OPTIONS, long_options, NULL))
@@ -1194,16 +1198,8 @@ main(int argc, char **argv)
 		argv = argv_from_file0;
 	}
 
-#ifndef WITHOUT_NLS
-	catalog = catopen("sort", NL_CAT_LOCALE);
-#endif
-
 	if (sort_opts_vals.cflag && sort_opts_vals.mflag)
 		errx(1, "%c:%c: %s", 'm', 'c', getstr(1));
-
-#ifndef WITHOUT_NLS
-	catclose(catalog);
-#endif
 
 	if (keys_num == 0) {
 		keys_num = 1;
@@ -1342,6 +1338,11 @@ main(int argc, char **argv)
 	}
 
 	sort_free(outfile);
+
+#ifndef WITHOUT_NLS
+	if (catalog != (nl_catd)-1)
+		catclose(catalog);
+#endif
 
 	return (result);
 }
