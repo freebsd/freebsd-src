e00bf706145da06f71f8e29d54f2695250e4eb20
Elliott Mitchell <ehem+freebsd@m5p.com>
2022-01-29 07:45:51 -0800

    kern/intr: split intr_event_create() apart
    The memory allocation/release are a distinct step from initializing the
    event structure.  The structure allocation could be used in other ways,
    so split them apart.
    
    In some cases it may be better to allocate the intr_event structure
    outside of kern_intr.  Notably if an interrupt controller needed data
    attached to the event instead of the PIC.
    
    More than a decade ago there was consideration of letting interrupt
    events be allocated outside the core.  This was available for less than
    9 months then removed.  Yet now all architectures now need locks during
    setup, resulting in hacks surrounding the use of `intr_event_create()`.
    In particular x86 first allocates the event, then tries to allocate the
    interrupt number.  Other architectures implemented lazy allocation of
    events, then check for a race during the actual allocation.
    
    Differential Revision: https://reviews.freebsd.org/D35417

--- sys/sys/interrupt.h
+++ sys/sys/interrupt.h
@@ -30,10 +30,10 @@
 #define _SYS_INTERRUPT_H_
 
 #include <sys/_lock.h>
-#include <sys/_mutex.h>
 #include <sys/_types.h>
 #include <sys/_types_interrupt.h>
 #include <sys/ck.h>
+#include <sys/mutex.h>
 #include <sys/param.h>
 #include <sys/queue.h>
 
@@ -179,6 +179,16 @@ int	intr_event_bind_ithread(struct intr_event *ie, int cpu);
 struct _cpuset;
 int	intr_event_bind_ithread_cpuset(struct intr_event *ie,
 	    struct _cpuset *mask);
+int	intr_event_initv_(struct intr_event *ie, void (*pre_ithread)(void *),
+	    void (*post_ithread)(void *), void (*post_filter)(void *),
+	    int (*assign_cpu)(void *, int), u_int irq, int flags,
+	    const char *fmt, __va_list ap) __printflike(8, 0)
+	    __result_use_check;
+int	intr_event_init_(struct intr_event *ie, void (*pre_ithread)(void *),
+	    void (*post_ithread)(void *), void (*post_filter)(void *),
+	    int (*assign_cpu)(void *, int), u_int irq, int flags,
+	    const char *fmt, ...) __printflike(8, 9)
+	    __result_use_check;
 int	intr_event_create(struct intr_event **event, void *source,
 	    int flags, unsigned int irq, void (*pre_ithread)(void *),
 	    void (*post_ithread)(void *), void (*post_filter)(void *),
@@ -186,6 +196,7 @@ int	intr_event_create(struct intr_event **event, void *source,
 	    __printflike(9, 10);
 int	intr_event_describe_handler(struct intr_event *ie, void *cookie,
 	    const char *descr);
+int	intr_event_shutdown_(struct intr_event *ie) __result_use_check;
 int	intr_event_destroy_(struct intr_event *ie) __result_use_check;
 int	intr_event_destroy(struct intr_event *ie);
 int	intr_event_handle_(struct intr_event *ie, struct trapframe *frame);
@@ -202,5 +213,11 @@ int	swi_add(struct intr_event **eventp, const char *name,
 	    void **cookiep);
 void	swi_sched(void *cookie, int flags);
 int	swi_remove(void *cookie);
+
+static inline bool
+intr_event_is_valid(struct intr_event *ie)
+{
+	return (mtx_initialized(&ie->ie_lock));
+}
 
 #endif
