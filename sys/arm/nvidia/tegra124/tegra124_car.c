c9dffe5bc22e1927597a28d208b1fa4b696378d4
Gordon Bergling <gbe@FreeBSD.org>
2022-02-06 17:31:05 +0100

    tegra: Fix a common typo in source code comments
    - s/ajusted/adjusted/
    
    (cherry picked from commit b6724f7004cda7a554596a9a899ed5a151a4f151)

--- sys/arm/nvidia/tegra124/tegra124_car.c
+++ sys/arm/nvidia/tegra124/tegra124_car.c
@@ -191,7 +191,7 @@ PLIST(mux_plld_out0_plld2_out0) = {"pllD_out0", "pllD2_out0"};
 PLIST(mux_xusb_hs) = {"xusb_ss_div2", "pllU_60"};
 PLIST(mux_xusb_ss) = {"pc_xusb_ss", "osc_div_clk"};
 
-/* Clocks ajusted online. */
+/* Clocks adjusted online. */
 static struct clk_fixed_def fixed_clk_m =
 	FRATE(TEGRA124_CLK_CLK_M, "clk_m", 12000000);
 static struct clk_fixed_def fixed_osc_div_clk =
