freebsd/freebsd-src

commit 383e75df01b7563e5af2b58505d84a17505084ac

x86/xen: remove PVHv1 code
PVHv1 was officially removed from Xen in 4.9, so just axe the related
code from FreeBSD.

Note FreeBSD supports PVHv2, which is the replacement for PVHv1.

Sponsored by: Citrix Systems R&D
Reviewed by: kib, Elliott Mitchell
Differential Revision: https://reviews.freebsd.org/D30228

sys/x86/xen/xen_intr.c
@@ -114,8 +114,6 @@ DPCPU_DEFINE_STATIC(struct xen_intr_pcpu_data, xen_intr_pcpu) = {
 
 DPCPU_DECLARE(struct vcpu_info *, vcpu_info);
 
-#define	XEN_EEXIST		17 /* Xen "already exists" error */
-#define	XEN_ALLOCATE_VECTOR	0 /* Allocate a vector for this event channel */
 #define	XEN_INVALID_EVTCHN	0 /* Invalid event channel */
 
 #define	is_valid_evtchn(x)	((x) != XEN_INVALID_EVTCHN)
@@ -126,7 +124,6 @@ struct xenisrc {
 	int		xi_cpu;		/* VCPU for delivery. */
 	int		xi_vector;	/* Global isrc vector number. */
 	evtchn_port_t	xi_port;
-	int		xi_pirq;
 	int		xi_virq;
 	void		*xi_cookie;
 	u_int		xi_close:1;	/* close on unbind? */
@@ -149,14 +146,6 @@ static int	xen_intr_config_intr(struct intsrc *isrc,
 		     enum intr_trigger trig, enum intr_polarity pol);
 static int	xen_intr_assign_cpu(struct intsrc *isrc, u_int apic_id);
 
-static void	xen_intr_pirq_enable_source(struct intsrc *isrc);
-static void	xen_intr_pirq_disable_source(struct intsrc *isrc, int eoi);
-static void	xen_intr_pirq_eoi_source(struct intsrc *isrc);
-static void	xen_intr_pirq_enable_intr(struct intsrc *isrc);
-static void	xen_intr_pirq_disable_intr(struct intsrc *isrc);
-static int	xen_intr_pirq_config_intr(struct intsrc *isrc,
-		     enum intr_trigger trig, enum intr_polarity pol);
-
 /**
  * PIC interface for all event channel port types except physical IRQs.
  */
@@ -174,30 +163,9 @@ struct pic xen_intr_pic = {
 	.pic_assign_cpu     = xen_intr_assign_cpu
 };
 
-/**
- * PIC interface for all event channel representing
- * physical interrupt sources.
- */
-struct pic xen_intr_pirq_pic = {
-#ifdef __amd64__
-	.pic_register_sources = xenpv_register_pirqs,
-#endif
-	.pic_enable_source  = xen_intr_pirq_enable_source,
-	.pic_disable_source = xen_intr_pirq_disable_source,
-	.pic_eoi_source     = xen_intr_pirq_eoi_source,
-	.pic_enable_intr    = xen_intr_pirq_enable_intr,
-	.pic_disable_intr   = xen_intr_pirq_disable_intr,
-	.pic_vector         = xen_intr_vector,
-	.pic_source_pending = xen_intr_source_pending,
-	.pic_config_intr    = xen_intr_pirq_config_intr,
-	.pic_assign_cpu     = xen_intr_assign_cpu
-};
-
 static struct mtx	 xen_intr_isrc_lock;
 static u_int		 xen_intr_auto_vector_count;
 static struct xenisrc	*xen_intr_port_to_isrc[NR_EVENT_CHANNELS];
-static u_long		*xen_intr_pirq_eoi_map;
-static boolean_t	 xen_intr_pirq_eoi_map_enabled;
 
 /*------------------------- Private Functions --------------------------------*/
 /**
@@ -305,10 +273,11 @@ xen_intr_find_unused_isrc(enum evtchn_type type)
  *          object or NULL.
  */
 static struct xenisrc *
-xen_intr_alloc_isrc(enum evtchn_type type, int vector)
+xen_intr_alloc_isrc(enum evtchn_type type)
 {
 	static int warned;
 	struct xenisrc *isrc;
+	unsigned int vector;
 
 	KASSERT(mtx_owned(&xen_intr_isrc_lock), ("Evtchn alloc lock not held"));
 
@@ -320,18 +289,15 @@ xen_intr_alloc_isrc(enum evtchn_type type, int vector)
 		return (NULL);
 	}
 
-	if (type != EVTCHN_TYPE_PIRQ) {
-		vector = first_evtchn_irq + xen_intr_auto_vector_count;
-		xen_intr_auto_vector_count++;
-	}
+	vector = first_evtchn_irq + xen_intr_auto_vector_count;
+	xen_intr_auto_vector_count++;
 
 	KASSERT((intr_lookup_source(vector) == NULL),
 	    ("Trying to use an already allocated vector"));
 
 	mtx_unlock(&xen_intr_isrc_lock);
 	isrc = malloc(sizeof(*isrc), M_XENINTR, M_WAITOK | M_ZERO);
-	isrc->xi_intsrc.is_pic =
-	    (type == EVTCHN_TYPE_PIRQ) ? &xen_intr_pirq_pic : &xen_intr_pic;
+	isrc->xi_intsrc.is_pic = &xen_intr_pic;
 	isrc->xi_vector = vector;
 	isrc->xi_type = type;
 	intr_register_source(&isrc->xi_intsrc);
@@ -416,7 +382,7 @@ xen_intr_bind_isrc(struct xenisrc **isrcp, evtchn_port_t local_port,
 	mtx_lock(&xen_intr_isrc_lock);
 	isrc = xen_intr_find_unused_isrc(type);
 	if (isrc == NULL) {
-		isrc = xen_intr_alloc_isrc(type, XEN_ALLOCATE_VECTOR);
+		isrc = xen_intr_alloc_isrc(type);
 		if (isrc == NULL) {
 			mtx_unlock(&xen_intr_isrc_lock);
 			return (ENOSPC);
@@ -632,8 +598,7 @@ xen_intr_init(void *dummy __unused)
 {
 	shared_info_t *s = HYPERVISOR_shared_info;
 	struct xen_intr_pcpu_data *pcpu;
-	struct physdev_pirq_eoi_gmfn eoi_gmfn;
-	int i, rc;
+	int i;
 
 	if (!xen_domain())
 		return (0);
@@ -653,18 +618,7 @@ xen_intr_init(void *dummy __unused)
 	for (i = 0; i < nitems(s->evtchn_mask); i++)
 		atomic_store_rel_long(&s->evtchn_mask[i], ~0);
 
-	/* Try to register PIRQ EOI map */
-	xen_intr_pirq_eoi_map = malloc(PAGE_SIZE, M_XENINTR, M_WAITOK | M_ZERO);
-	eoi_gmfn.gmfn = atop(vtophys(xen_intr_pirq_eoi_map));
-	rc = HYPERVISOR_physdev_op(PHYSDEVOP_pirq_eoi_gmfn_v2, &eoi_gmfn);
-	if (rc != 0 && bootverbose)
-		printf("Xen interrupts: unable to register PIRQ EOI map\n");
-	else
-		xen_intr_pirq_eoi_map_enabled = true;
-
 	intr_register_pic(&xen_intr_pic);
-	if (xen_pv_domain() && xen_initial_domain())
-		intr_register_pic(&xen_intr_pirq_pic);
 
 	if (bootverbose)
 		printf("Xen interrupt system initialized\n");
@@ -1020,184 +974,6 @@ xen_intr_enable_intr(struct intsrc *base_isrc)
 	evtchn_unmask_port(isrc->xi_port);
 }
 
-/*------------------ Physical Interrupt Source PIC Functions -----------------*/
-/*
- * Mask a level triggered interrupt source.
- *
- * \param isrc  The interrupt source to mask (if necessary).
- * \param eoi   If non-zero, perform any necessary end-of-interrupt
- *              acknowledgements.
- */
-static void
-xen_intr_pirq_disable_source(struct intsrc *base_isrc, int eoi)
-{
-	struct xenisrc *isrc;
-
-	isrc = (struct xenisrc *)base_isrc;
-
-	if (isrc->xi_edgetrigger == 0)
-		evtchn_mask_port(isrc->xi_port);
-	if (eoi == PIC_EOI)
-		xen_intr_pirq_eoi_source(base_isrc);
-}
-
-/*
- * Unmask a level triggered interrupt source.
- *
- * \param isrc  The interrupt source to unmask (if necessary).
- */
-static void
-xen_intr_pirq_enable_source(struct intsrc *base_isrc)
-{
-	struct xenisrc *isrc;
-
-	isrc = (struct xenisrc *)base_isrc;
-
-	if (isrc->xi_edgetrigger == 0)
-		evtchn_unmask_port(isrc->xi_port);
-}
-
-/*
- * Perform any necessary end-of-interrupt acknowledgements.
- *
- * \param isrc  The interrupt source to EOI.
- */
-static void
-xen_intr_pirq_eoi_source(struct intsrc *base_isrc)
-{
-	struct xenisrc *isrc;
-	int error;
-
-	isrc = (struct xenisrc *)base_isrc;
-
-	if (xen_test_bit(isrc->xi_pirq, xen_intr_pirq_eoi_map)) {
-		struct physdev_eoi eoi = { .irq = isrc->xi_pirq };
-
-		error = HYPERVISOR_physdev_op(PHYSDEVOP_eoi, &eoi);
-		if (error != 0)
-			panic("Unable to EOI PIRQ#%d: %d\n",
-			    isrc->xi_pirq, error);
-	}
-}
-
-/*
- * Enable and unmask the interrupt source.
- *
- * \param isrc  The interrupt source to enable.
- */
-static void
-xen_intr_pirq_enable_intr(struct intsrc *base_isrc)
-{
-	struct xenisrc *isrc;
-	struct evtchn_bind_pirq bind_pirq;
-	struct physdev_irq_status_query irq_status;
-	int error;
-
-	isrc = (struct xenisrc *)base_isrc;
-
-	if (!xen_intr_pirq_eoi_map_enabled) {
-		irq_status.irq = isrc->xi_pirq;
-		error = HYPERVISOR_physdev_op(PHYSDEVOP_irq_status_query,
-		    &irq_status);
-		if (error)
-			panic("unable to get status of IRQ#%d", isrc->xi_pirq);
-
-		if (irq_status.flags & XENIRQSTAT_needs_eoi) {
-			/*
-			 * Since the dynamic PIRQ EOI map is not available
-			 * mark the PIRQ as needing EOI unconditionally.
-			 */
-			xen_set_bit(isrc->xi_pirq, xen_intr_pirq_eoi_map);
-		}
-	}
-
-	bind_pirq.pirq = isrc->xi_pirq;
-	bind_pirq.flags = isrc->xi_edgetrigger ? 0 : BIND_PIRQ__WILL_SHARE;
-	error = HYPERVISOR_event_channel_op(EVTCHNOP_bind_pirq, &bind_pirq);
-	if (error)
-		panic("unable to bind IRQ#%d", isrc->xi_pirq);
-
-	isrc->xi_port = bind_pirq.port;
-
-	mtx_lock(&xen_intr_isrc_lock);
-	KASSERT((xen_intr_port_to_isrc[bind_pirq.port] == NULL),
-	    ("trying to override an already setup event channel port"));
-	xen_intr_port_to_isrc[bind_pirq.port] = isrc;
-	mtx_unlock(&xen_intr_isrc_lock);
-
-	evtchn_unmask_port(isrc->xi_port);
-}
-
-/*
- * Disable an interrupt source.
- *
- * \param isrc  The interrupt source to disable.
- */
-static void
-xen_intr_pirq_disable_intr(struct intsrc *base_isrc)
-{
-	struct xenisrc *isrc;
-	struct evtchn_close close;
-	int error;
-
-	isrc = (struct xenisrc *)base_isrc;
-
-	evtchn_mask_port(isrc->xi_port);
-
-	close.port = isrc->xi_port;
-	error = HYPERVISOR_event_channel_op(EVTCHNOP_close, &close);
-	if (error)
-		panic("unable to close event channel %d IRQ#%d",
-		    isrc->xi_port, isrc->xi_pirq);
-
-	mtx_lock(&xen_intr_isrc_lock);
-	xen_intr_port_to_isrc[isrc->xi_port] = NULL;
-	mtx_unlock(&xen_intr_isrc_lock);
-
-	isrc->xi_port = 0;
-}
-
-/**
- * Perform configuration of an interrupt source.
- *
- * \param isrc  The interrupt source to configure.
- * \param trig  Edge or level.
- * \param pol   Active high or low.
- *
- * \returns  0 if no events are pending, otherwise non-zero.
- */
-static int
-xen_intr_pirq_config_intr(struct intsrc *base_isrc, enum intr_trigger trig,
-    enum intr_polarity pol)
-{
-	struct xenisrc *isrc = (struct xenisrc *)base_isrc;
-	struct physdev_setup_gsi setup_gsi;
-	int error;
-
-	KASSERT(!(trig == INTR_TRIGGER_CONFORM || pol == INTR_POLARITY_CONFORM),
-	    ("%s: Conforming trigger or polarity\n", __func__));
-
-	setup_gsi.gsi = isrc->xi_pirq;
-	setup_gsi.triggering = trig == INTR_TRIGGER_EDGE ? 0 : 1;
-	setup_gsi.polarity = pol == INTR_POLARITY_HIGH ? 0 : 1;
-
-	error = HYPERVISOR_physdev_op(PHYSDEVOP_setup_gsi, &setup_gsi);
-	if (error == -XEN_EEXIST) {
-		if ((isrc->xi_edgetrigger && (trig != INTR_TRIGGER_EDGE)) ||
-		    (isrc->xi_activehi && (pol != INTR_POLARITY_HIGH)))
-			panic("unable to reconfigure interrupt IRQ#%d",
-			    isrc->xi_pirq);
-		error = 0;
-	}
-	if (error)
-		panic("unable to configure IRQ#%d\n", isrc->xi_pirq);
-
-	isrc->xi_activehi = pol == INTR_POLARITY_HIGH ? 1 : 0;
-	isrc->xi_edgetrigger = trig == INTR_TRIGGER_EDGE ? 1 : 0;
-
-	return (0);
-}
-
 /*--------------------------- Public Functions -------------------------------*/
 /*------- API comments for these methods can be found in xen/xenintr.h -------*/
 int
@@ -1413,101 +1189,6 @@ xen_intr_alloc_and_bind_ipi(u_int cpu, driver_filter_t filter,
 }
 
 int
-xen_register_pirq(int vector, enum intr_trigger trig, enum intr_polarity pol)
-{
-	struct physdev_map_pirq map_pirq;
-	struct xenisrc *isrc;
-	int error;
-
-	if (vector == 0)
-		return (EINVAL);
-
-	if (bootverbose)
-		printf("xen: register IRQ#%d\n", vector);
-
-	map_pirq.domid = DOMID_SELF;
-	map_pirq.type = MAP_PIRQ_TYPE_GSI;
-	map_pirq.index = vector;
-	map_pirq.pirq = vector;
-
-	error = HYPERVISOR_physdev_op(PHYSDEVOP_map_pirq, &map_pirq);
-	if (error) {
-		printf("xen: unable to map IRQ#%d\n", vector);
-		return (error);
-	}
-
-	mtx_lock(&xen_intr_isrc_lock);
-	isrc = xen_intr_alloc_isrc(EVTCHN_TYPE_PIRQ, vector);
-	mtx_unlock(&xen_intr_isrc_lock);
-	KASSERT((isrc != NULL), ("xen: unable to allocate isrc for interrupt"));
-	isrc->xi_pirq = vector;
-	isrc->xi_activehi = pol == INTR_POLARITY_HIGH ? 1 : 0;
-	isrc->xi_edgetrigger = trig == INTR_TRIGGER_EDGE ? 1 : 0;
-
-	return (0);
-}
-
-int
-xen_register_msi(device_t dev, int vector, int count)
-{
-	struct physdev_map_pirq msi_irq;
-	struct xenisrc *isrc;
-	int ret;
-
-	memset(&msi_irq, 0, sizeof(msi_irq));
-	msi_irq.domid = DOMID_SELF;
-	msi_irq.type = count == 1 ?
-	    MAP_PIRQ_TYPE_MSI_SEG : MAP_PIRQ_TYPE_MULTI_MSI;
-	msi_irq.index = -1;
-	msi_irq.pirq = -1;
-	msi_irq.bus = pci_get_bus(dev) | (pci_get_domain(dev) << 16);
-	msi_irq.devfn = (pci_get_slot(dev) << 3) | pci_get_function(dev);
-	msi_irq.entry_nr = count;
-
-	ret = HYPERVISOR_physdev_op(PHYSDEVOP_map_pirq, &msi_irq);
-	if (ret != 0)
-		return (ret);
-	if (count != msi_irq.entry_nr) {
-		panic("unable to setup all requested MSI vectors "
-		    "(expected %d got %d)", count, msi_irq.entry_nr);
-	}
-
-	mtx_lock(&xen_intr_isrc_lock);
-	for (int i = 0; i < count; i++) {
-		isrc = xen_intr_alloc_isrc(EVTCHN_TYPE_PIRQ, vector + i);
-		KASSERT(isrc != NULL,
-		    ("xen: unable to allocate isrc for interrupt"));
-		isrc->xi_pirq = msi_irq.pirq + i;
-		/* MSI interrupts are always edge triggered */
-		isrc->xi_edgetrigger = 1;
-	}
-	mtx_unlock(&xen_intr_isrc_lock);
-
-	return (0);
-}
-
-int
-xen_release_msi(int vector)
-{
-	struct physdev_unmap_pirq unmap;
-	struct xenisrc *isrc;
-	int ret;
-
-	isrc = (struct xenisrc *)intr_lookup_source(vector);
-	if (isrc == NULL)
-		return (ENXIO);
-
-	unmap.pirq = isrc->xi_pirq;
-	ret = HYPERVISOR_physdev_op(PHYSDEVOP_unmap_pirq, &unmap);
-	if (ret != 0)
-		return (ret);
-
-	xen_intr_release_isrc(isrc);
-
-	return (0);
-}
-
-int
 xen_intr_describe(xen_intr_handle_t port_handle, const char *fmt, ...)
 {
 	char descr[MAXCOMLEN + 1];
@@ -1630,7 +1311,6 @@ xen_intr_print_type(enum evtchn_type type)
 {
 	static const char *evtchn_type_to_string[EVTCHN_TYPE_COUNT] = {
 		[EVTCHN_TYPE_UNBOUND]	= "UNBOUND",
-		[EVTCHN_TYPE_PIRQ]	= "PIRQ",
 		[EVTCHN_TYPE_VIRQ]	= "VIRQ",
 		[EVTCHN_TYPE_IPI]	= "IPI",
 		[EVTCHN_TYPE_PORT]	= "PORT",
@@ -1651,12 +1331,6 @@ xen_intr_dump_port(struct xenisrc *isrc)
 
 	db_printf("Port %d Type: %s\n",
 	    isrc->xi_port, xen_intr_print_type(isrc->xi_type));
-	if (isrc->xi_type == EVTCHN_TYPE_PIRQ) {
-		db_printf("\tPirq: %d ActiveHi: %d EdgeTrigger: %d "
-		    "NeedsEOI: %d\n",
-		    isrc->xi_pirq, isrc->xi_activehi, isrc->xi_edgetrigger,
-		    !!xen_test_bit(isrc->xi_pirq, xen_intr_pirq_eoi_map));
-	}
 	if (isrc->xi_type == EVTCHN_TYPE_VIRQ)
 		db_printf("\tVirq: %d\n", isrc->xi_virq);
 

sys/amd64/amd64/machdep.c
@@ -187,13 +187,6 @@ struct init_ops init_ops = {
 	.early_clock_source_init =	i8254_init,
 	.early_delay =			i8254_delay,
 	.parse_memmap =			native_parse_memmap,
-#ifdef SMP
-	.mp_bootaddress =		mp_bootaddress,
-	.start_all_aps =		native_start_all_aps,
-#endif
-#ifdef DEV_PCI
-	.msi_init =			msi_init,
-#endif
 };
 
 /*
@@ -1283,8 +1276,9 @@ getmemsize(caddr_t kmdp, u_int64_t first)
 	 * is configured to support APs and APs for the system start
 	 * in real mode mode (e.g. SMP bare metal).
 	 */
-	if (init_ops.mp_bootaddress)
-		init_ops.mp_bootaddress(physmap, &physmap_idx);
+#ifdef SMP
+	mp_bootaddress(physmap, &physmap_idx);
+#endif
 
 	/* call pmap initialization to make new kernel address space */
 	pmap_bootstrap(&first);

sys/amd64/amd64/mp_machdep.c
@@ -245,7 +245,7 @@ cpu_mp_start(void)
 	mptramp_la57 = la57;
 
 	/* Start each Application Processor */
-	init_ops.start_all_aps();
+	start_all_aps();
 
 	set_interrupt_apic_ids();
 
@@ -397,7 +397,7 @@ mp_realloc_pcpu(int cpuid, int domain)
  * start each AP in our list
  */
 int
-native_start_all_aps(void)
+start_all_aps(void)
 {
 	u_int64_t *pt5, *pt4, *pt3, *pt2;
 	u_int32_t mpbioswarmvec;

sys/amd64/include/smp.h
@@ -38,7 +38,7 @@ inthand_t
 	IDTVEC(rendezvous_pti);
 
 void	invlop_handler(void);
-int	native_start_all_aps(void);
+int	start_all_aps(void);
 void	mp_bootaddress(vm_paddr_t *, unsigned int *);
 
 #endif /* !LOCORE */

sys/conf/files
@@ -3545,7 +3545,6 @@ dev/xen/netback/netback.c	optional xenhvm
 dev/xen/netfront/netfront.c	optional xenhvm
 dev/xen/xenpci/xenpci.c		optional xenpci
 dev/xen/timer/timer.c		optional xenhvm
-dev/xen/pvcpu/pvcpu.c		optional xenhvm
 dev/xen/xenstore/xenstore.c	optional xenhvm
 dev/xen/xenstore/xenstore_dev.c	optional xenhvm
 dev/xen/xenstore/xenstored_dev.c	optional xenhvm

sys/conf/files.amd64
@@ -387,8 +387,6 @@ dev/viawd/viawd.c		optional	viawd
 dev/vmd/vmd.c			optional	vmd
 dev/vmd/vmd_bus.c		optional	vmd_bus
 dev/wbwd/wbwd.c			optional	wbwd
-dev/xen/pci/xen_acpi_pci.c	optional	xenhvm
-dev/xen/pci/xen_pci.c		optional	xenhvm
 isa/syscons_isa.c		optional	sc
 isa/vga_isa.c			optional	vga
 kern/imgact_aout.c		optional compat_aout
@@ -461,8 +459,6 @@ x86/x86/mptable.c		optional	mptable
 x86/x86/mptable_pci.c		optional	mptable pci
 x86/x86/msi.c			optional	pci
 x86/xen/pv.c			optional	xenhvm
-x86/xen/pvcpu_enum.c		optional	xenhvm
-x86/xen/xen_pci_bus.c		optional	xenhvm
 
 compat/linuxkpi/common/src/linux_fpu.c		optional compat_linuxkpi \
 	compile-with "${LINUXKPI_C}"

sys/conf/files.x86
@@ -328,5 +328,3 @@ x86/xen/hvm.c			optional	xenhvm
 x86/xen/xen_intr.c		optional	xenhvm
 x86/xen/xen_apic.c		optional	xenhvm
 x86/xen/xenpv.c			optional	xenhvm
-x86/xen/xen_msi.c		optional	xenhvm
-x86/xen/xen_nexus.c		optional	xenhvm

sys/dev/xen/pci/xen_acpi_pci.c
@@ -1,97 +1,0 @@
-/*
- * Copyright (c) 2014 Roger Pau Monné <roger.pau@citrix.com>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
-#include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
-
-#include <sys/param.h>
-#include <sys/systm.h>
-#include <sys/bus.h>
-#include <sys/kernel.h>
-#include <sys/malloc.h>
-#include <sys/module.h>
-
-#include <contrib/dev/acpica/include/acpi.h>
-#include <contrib/dev/acpica/include/accommon.h>
-
-#include <dev/acpica/acpivar.h>
-#include <dev/acpica/acpi_pcivar.h>
-
-#include <sys/pciio.h>
-#include <dev/pci/pcireg.h>
-#include <dev/pci/pcivar.h>
-#include <dev/pci/pci_private.h>
-
-#include <xen/xen-os.h>
-#include <xen/hypervisor.h>
-#include <xen/xen_pci.h>
-
-#include "pcib_if.h"
-#include "pci_if.h"
-
-static int
-xen_acpi_pci_probe(device_t dev)
-{
-
-	if (!xen_pv_domain())
-		return (ENXIO);
-	if (acpi_get_handle(dev) == NULL)
-		return (ENXIO);
-
-	device_set_desc(dev, "Xen ACPI PCI bus");
-
-	return (BUS_PROBE_SPECIFIC);
-}
-
-static void
-xen_acpi_pci_child_added(device_t dev, device_t child)
-{
-
-	acpi_pci_child_added(dev, child);
-	xen_pci_child_added_method(dev, child);
-}
-
-static device_method_t xen_acpi_pci_methods[] = {
-	/* Device interface */
-	DEVMETHOD(device_probe,		xen_acpi_pci_probe),
-
-	/* PCI interface overwrites */
-	DEVMETHOD(pci_enable_msi,	xen_pci_enable_msi_method),
-	DEVMETHOD(pci_disable_msi,	xen_pci_disable_msi_method),
-	DEVMETHOD(pci_child_added,	xen_acpi_pci_child_added),
-
-	DEVMETHOD_END
-};
-
-static devclass_t pci_devclass;
-
-DECLARE_CLASS(acpi_pci_driver);
-DEFINE_CLASS_1(pci, xen_acpi_pci_driver, xen_acpi_pci_methods,
-    sizeof(struct pci_softc), acpi_pci_driver);
-DRIVER_MODULE(xen_acpi_pci, pcib, xen_acpi_pci_driver, pci_devclass, 0, 0);
-MODULE_DEPEND(xen_acpi_pci, pci, 1, 1, 1);
-MODULE_DEPEND(xen_acpi_pci, acpi, 1, 1, 1);
-MODULE_VERSION(xen_acpi_pci, 1);

commit 7551fd4f47722aa81fa363d6e1ff3ee555b1f566

xen: remove leftover bits missed in commit 383e75df01
These fields are now unused, remove them.

Fixes: 383e75df01b7 ('x86/xen: remove PVHv1 code')
Reviewed by: royger
Differential Revision: https://reviews.freebsd.org/D31206

sys/x86/xen/xen_intr.c
@@ -126,8 +126,6 @@ struct xenisrc {
 	int		xi_virq;
 	void		*xi_cookie;
 	u_int		xi_close:1;	/* close on unbind? */
-	u_int		xi_activehi:1;
-	u_int		xi_edgetrigger:1;
 	u_int		xi_masked:1;
 	volatile u_int	xi_refcount;
 };