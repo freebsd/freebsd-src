e85af89fa7613a4bb506ca6ab8ecafbfbfde782d
Hans Petter Selasky <hselasky@FreeBSD.org>
2022-02-08 00:28:57 +0100

    Add more USB host controller PCI ID's.
    Submitted by:	Gary Jennejohn <gljennjohn@gmail.com>
    MFC after:	1 week
    Sponsored by:	NVIDIA Networking

--- sys/dev/usb/controller/xhci_pci.c
+++ sys/dev/usb/controller/xhci_pci.c
@@ -129,6 +129,8 @@ xhci_pci_match(device_t self)
 		return ("ASMedia ASM1042 USB 3.0 controller");
 	case 0x11421b21:
 		return ("ASMedia ASM1042A USB 3.0 controller");
+	case 0x13431b21:
+		return ("ASMedia ASM1143 USB 3.1 controller");
 	case 0x32421b21:
 		return ("ASMedia ASM3242 USB 3.2 controller");
 
