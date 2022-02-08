47ded797ce9620f055f005e9dccfcd03613b1804
Franco Fichtner <franco@opnsense.org>
2022-02-07 19:22:03 -0700

    netinet: simplify RSS ifdef statements
    Approved by:	transport (rrs)
    MFC after:	3 days
    Differential Revision:	https://reviews.freebsd.org/D31583

--- sys/netinet/tcp_hpts.c
+++ sys/netinet/tcp_hpts.c
@@ -1014,7 +1014,7 @@ static uint16_t
 hpts_cpuid(struct inpcb *inp, int *failed)
 {
 	u_int cpuid;
-#if !defined(RSS) && defined(NUMA)
+#ifdef NUMA
 	struct hpts_domain_info *di;
 #endif
 
@@ -1040,7 +1040,7 @@ hpts_cpuid(struct inpcb *inp, int *failed)
 		return (hpts_random_cpu(inp));
 	else
 		return (cpuid);
-#else
+#endif
 	/*
 	 * We don't have a flowid -> cpuid mapping, so cheat and just map
 	 * unknown cpuids to curcpu.  Not the best, but apparently better
@@ -1063,7 +1063,6 @@ hpts_cpuid(struct inpcb *inp, int *failed)
 		cpuid = inp->inp_flowid % mp_ncpus;
 	counter_u64_add(cpu_uses_flowid, 1);
 	return (cpuid);
-#endif
 }
 
 static void
