cf1507aacb3f52e8c2147cda163b0df8c4df7f80
Piotr Pawel Stefaniak <pstef@FreeBSD.org>
2022-02-13 19:14:55 +0100

    tcp_wrappers: include <unistd.h>
    This is for getdomainname(3).
    
    (cherry picked from commit 7d636a4d3ebd070acefb05c53c896cd75a9ea745)

--- contrib/tcp_wrappers/workarounds.c
+++ contrib/tcp_wrappers/workarounds.c
@@ -23,6 +23,9 @@ char    sccsid[] = "@(#) workarounds.c 1.6 96/03/19 16:22:25";
 #include <stdio.h>
 #include <syslog.h>
 #include <string.h>
+#ifdef USE_GETDOMAIN
+#include <unistd.h>
+#endif
 
 extern int errno;
 
