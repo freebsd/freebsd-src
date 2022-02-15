a528bad95e0c80fe612051076d53b8bc8283e239
Ed Maste <emaste@FreeBSD.org>
2022-02-14 22:04:29 -0500

    elfctl: error if -e is specified multiple times
    Reported by:	jrm
    MFC after:	3 days
    Sponsored by:	The FreeBSD Foundation
    
    (cherry picked from commit f0cf9b602dc0375dfc279500e0314b7c55273bf0)

--- usr.bin/elfctl/elfctl.c
+++ usr.bin/elfctl/elfctl.c
@@ -120,6 +120,8 @@ main(int argc, char **argv)
 			lflag = true;
 			break;
 		case 'e':
+			if (features != NULL)
+				errx(1, "-e may be specified only once");
 			features = optarg;
 			editfeatures = true;
 			break;
