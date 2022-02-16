c6e27c7b4cf253a28602f637c7c84a82f19a779d
Mark Johnston <markj@FreeBSD.org>
2022-02-15 21:50:41 -0500

    armv8crypto: Use cursors to access crypto buffer data
    Currently armv8crypto copies the scheme used in aesni(9), where payload
    data and output buffers are allocated on the fly if the crypto buffer is
    not virtually contiguous.  This scheme is simple but incurs a lot of
    overhead: for an encryption request with a separate output buffer we
    have to
    - allocate a temporary buffer to hold the payload
    - copy input data into the buffer
    - copy the encrypted payload to the output buffer
    - zero the temporary buffer before freeing it
    
    We have a handy crypto buffer cursor abstraction now, so reimplement the
    armv8crypto routines using that instead of temporary buffers.  This
    introduces some extra complexity, but gallatin@ reports a 10% throughput
    improvement with a KTLS workload without additional CPU usage.  The
    driver still allocates an AAD buffer for AES-GCM if necessary.
    
    Reviewed by:	jhb
    Tested by:	gallatin
    Sponsored by:	Ampere Computing LLC
    Submitted by:	Klara Inc.
    MFC after:	1 month
    Differential Revision:	https://reviews.freebsd.org/D28950

--- sys/crypto/armv8/armv8_crypto.h
+++ sys/crypto/armv8/armv8_crypto.h
@@ -63,24 +63,29 @@ void gcm_init_v8(__uint128_val_t Htable[16], const uint64_t Xi[2]);
 void gcm_gmult_v8(uint64_t Xi[2], const __uint128_val_t Htable[16]);
 void gcm_ghash_v8(uint64_t Xi[2], const __uint128_val_t Htable[16], const uint8_t *inp, size_t len);
 
-void armv8_aes_encrypt_cbc(const AES_key_t *, size_t, const uint8_t *,
-    uint8_t *, const uint8_t[static AES_BLOCK_LEN]);
-void armv8_aes_decrypt_cbc(const AES_key_t *, size_t, uint8_t *,
+void armv8_aes_encrypt_cbc(const AES_key_t *key, size_t len,
+    struct crypto_buffer_cursor *fromc, struct crypto_buffer_cursor *toc,
+    const uint8_t iv[static AES_BLOCK_LEN]);
+void armv8_aes_decrypt_cbc(const AES_key_t *, size_t,
+    struct crypto_buffer_cursor *fromc, struct crypto_buffer_cursor *toc,
     const uint8_t[static AES_BLOCK_LEN]);
-void armv8_aes_encrypt_gcm(AES_key_t *, size_t, const uint8_t *,
-    uint8_t *, size_t, const uint8_t*,
+void armv8_aes_encrypt_gcm(AES_key_t *, size_t,
+    struct crypto_buffer_cursor *, struct crypto_buffer_cursor *,
+    size_t, const uint8_t *,
     uint8_t tag[static GMAC_DIGEST_LEN],
     const uint8_t[static AES_BLOCK_LEN],
     const __uint128_val_t *);
-int armv8_aes_decrypt_gcm(AES_key_t *, size_t, const uint8_t *,
-    uint8_t *, size_t, const uint8_t*,
-    const uint8_t tag[static GMAC_DIGEST_LEN],
+int armv8_aes_decrypt_gcm(AES_key_t *, size_t,
+    struct crypto_buffer_cursor *, struct crypto_buffer_cursor *,
+    size_t, const uint8_t *, const uint8_t tag[static GMAC_DIGEST_LEN],
     const uint8_t[static AES_BLOCK_LEN],
     const __uint128_val_t *);
 
 void armv8_aes_encrypt_xts(AES_key_t *, const void *, size_t,
-    const uint8_t *, uint8_t *, const uint8_t[AES_BLOCK_LEN]);
+    struct crypto_buffer_cursor *, struct crypto_buffer_cursor *,
+    const uint8_t[AES_BLOCK_LEN]);
 void armv8_aes_decrypt_xts(AES_key_t *, const void *, size_t,
-    const uint8_t *, uint8_t *, const uint8_t[AES_BLOCK_LEN]);
+    struct crypto_buffer_cursor *, struct crypto_buffer_cursor *,
+    const uint8_t[AES_BLOCK_LEN]);
 
 #endif /* _ARMV8_CRYPTO_H_ */
