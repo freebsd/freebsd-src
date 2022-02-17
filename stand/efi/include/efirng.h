5c73b3e0a3db79a3d8b96b3b4c8bc4fb7fee35f2
Colin Percival <cperciva@FreeBSD.org>
2022-02-17 13:01:11 -0800

    Add support for getting early entropy from UEFI
    UEFI provides a protocol for accessing randomness. This is a good way
    to gather early entropy, especially when there's no driver for the RNG
    on the platform (as is the case on the Marvell Armada8k (MACCHIATObin)
    for now).
    
    If the entropy_efi_seed option is enabled in loader.conf (default: YES)
    obtain 2048 bytes of entropy from UEFI and pass is to the kernel as a
    "module" of name "efi_rng_seed" and type "boot_entropy_platform"; if
    present, ingest it into the kernel RNG.
    
    Submitted by:	Greg V
    Reviewed by:	markm, kevans
    Approved by:	csprng (markm)
    MFC after:	3 weeks
    Differential Revision:	https://reviews.freebsd.org/D20780

---
+++ stand/efi/include/efirng.h
@@ -0,0 +1,50 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright 2006 - 2016 Unified EFI, Inc.<BR>
+ * Copyright (c) 2013 - 2016, Intel Corporation. All rights reserved.<BR>   
+ * This program and the accompanying materials
+ * are licensed and made available under the terms and conditions of the BSD License
+ * which accompanies this distribution.  The full text of the license may be found at 
+ * http://opensource.org/licenses/bsd-license.php
+ *
+ * THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+ *
+ */
+
+#ifndef _EFIRNG_H
+#define _EFIRNG_H
+
+#define EFI_RNG_PROTOCOL_GUID \
+    { 0x3152bca5, 0xeade, 0x433d, {0x86, 0x2e, 0xc0, 0x1c, 0xdc, 0x29, 0x1f, 0x44} }
+
+INTERFACE_DECL(_EFI_RNG_PROTOCOL);
+
+typedef EFI_GUID EFI_RNG_ALGORITHM;
+
+typedef
+EFI_STATUS
+(EFIAPI *EFI_RNG_GET_INFO) (
+    IN struct _EFI_RNG_PROTOCOL		*This,
+    IN  OUT UINTN			*RNGAlgorithmListSize,
+    OUT EFI_RNG_ALGORITHM		*RNGAlgorithmList
+    );
+
+typedef
+EFI_STATUS
+(EFIAPI *EFI_RNG_GET_RNG) (
+    IN struct _EFI_RNG_PROTOCOL		*This,
+    IN EFI_RNG_ALGORITHM		*RNGAlgorithm, OPTIONAL
+    IN UINTN				RNGValueLength,
+    OUT UINT8				*RNGValue
+    );
+
+typedef struct _EFI_RNG_PROTOCOL {
+	EFI_RNG_GET_INFO	GetInfo;
+	EFI_RNG_GET_RNG		GetRNG;
+} EFI_RNG_PROTOCOL;
+
+static EFI_GUID			rng_guid = EFI_RNG_PROTOCOL_GUID;
+
+#endif /* _EFIRNG_H */
