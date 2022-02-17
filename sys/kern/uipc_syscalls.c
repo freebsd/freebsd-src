855ec465df67f36bc4081c8c49778c73cf144e19
Gleb Smirnoff <glebius@FreeBSD.org>
2022-02-17 09:07:31 -0800

    sendto: don't clear transient errors for atomic protocols
    The changeset c16c011c38a uncovered the fact that top layer of sendto(2)
    would clear a transient error code if some data was copied out of uio.
    The clearing of the error makes sense for non-atomic protocols, since
    they have sent some data.  The atomic protocols send all or nothing.
    
    The current implementation of unix/dgram uses sosend_generic(), which
    would always copyout and only then it may fail to deliver a message.
    The sosend_dgram(), currently used by UDP only, also has same behavior.
    
    Reported by:		pho
    Reviewed by:		pho, markj
    Differential revision:	https://reviews.freebsd.org/D34309

--- sys/kern/uipc_syscalls.c
+++ sys/kern/uipc_syscalls.c
@@ -785,8 +785,10 @@ kern_sendit(struct thread *td, int s, struct msghdr *mp, int flags,
 	len = auio.uio_resid;
 	error = sosend(so, mp->msg_name, &auio, 0, control, flags, td);
 	if (error != 0) {
-		if (auio.uio_resid != len && (error == ERESTART ||
-		    error == EINTR || error == EWOULDBLOCK))
+		if (auio.uio_resid != len &&
+		    (so->so_proto->pr_flags & PR_ATOMIC) == 0 &&
+		    (error == ERESTART || error == EINTR ||
+		    error == EWOULDBLOCK))
 			error = 0;
 		/* Generation of SIGPIPE can be controlled per socket */
 		if (error == EPIPE && !(so->so_options & SO_NOSIGPIPE) &&
