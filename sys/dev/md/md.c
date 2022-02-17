3706039bc42385bbe883adb22b1ce3b837e4e9f7
Aleksandr Fedorov <afedorov@FreeBSD.org>
2022-02-17 22:21:56 +0300

    md(4): Add dummy support of the BIO_FLUSH command for malloc and swap backend.
    PR:	260200
    Reported by:	editor@callfortesting.org
    Reviewed by:	vmaffione (mentor), markj
    Approved by:	vmaffione (mentor), markj
    Differential Revision:	https://reviews.freebsd.org/D34260

--- sys/dev/md/md.c
+++ sys/dev/md/md.c
@@ -646,6 +646,8 @@ mdstart_malloc(struct md_s *sc, struct bio *bp)
 	case BIO_WRITE:
 	case BIO_DELETE:
 		break;
+	case BIO_FLUSH:
+		return (0);
 	default:
 		return (EOPNOTSUPP);
 	}
@@ -1026,6 +1028,8 @@ mdstart_swap(struct md_s *sc, struct bio *bp)
 	case BIO_WRITE:
 	case BIO_DELETE:
 		break;
+	case BIO_FLUSH:
+		return (0);
 	default:
 		return (EOPNOTSUPP);
 	}
