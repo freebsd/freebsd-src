c8cc568961dfccf3d91821f06f7072437b04edf5
Kirk McKusick <mckusick@FreeBSD.org>
2022-02-19 21:30:37 -0800

    Provide an interface that allows GEOM modules to return multiple messages.
    The gctl_error() function provides GEOM modules with the ability
    to report only a single message. When running with the verbose
    flag, commands that handle multiple devices may want to report a
    message for each of the devices on which it operates. This commit
    adds the gctl_msg() function that can be called multiple times
    to post messages. When finished issuing messages, the application
    must either call gctl_post_messages() or call gctl_error() to cause
    the messages to be reported to the calling process.
    
    Tested by:    Peter Holm

--- sys/geom/geom.h
+++ sys/geom/geom.h
@@ -435,6 +435,8 @@ char const *gctl_get_asciiparam(struct gctl_req *req, const char *param);
 void *gctl_get_paraml(struct gctl_req *req, const char *param, int len);
 void *gctl_get_paraml_opt(struct gctl_req *req, const char *param, int len);
 int gctl_error(struct gctl_req *req, const char *fmt, ...) __printflike(2, 3);
+void gctl_msg(struct gctl_req *req, const char *fmt, ...) __printflike(2, 3);
+void gctl_post_messages(struct gctl_req *req);
 struct g_class *gctl_get_class(struct gctl_req *req, char const *arg);
 struct g_geom *gctl_get_geom(struct gctl_req *req, struct g_class *mp, char const *arg);
 struct g_provider *gctl_get_provider(struct gctl_req *req, char const *arg);
