5754f5823bbca3e6ebefbe1ff38fe4cad8b641fe
Jose Luis Duran <jlduran@gmail.com>
2022-02-23 22:23:57 -0300

    libefivar: Apply uncrustify changes
    Apply uncrustify changes to .c/.h files.
    
    Reduce the diffs with EDK2 to aid with future merges.  The
    unconventional way we've imported this code makes using a vendor branch
    to manage it at the very least tricky. Update FreeBSD-update to reflect
    the slight shift in advise.
    
    Reviewed by:	imp
    Upstream Bug:	https://bugzilla.tianocore.org/show_bug.cgi?id=3737
    Obtained from:	https://github.com/tianocore/edk2/commit/2f88bd3a1296c522317f1c21377876de63de5be7
    Pull Request:   https://github.com/freebsd/freebsd-src/pull/581

--- lib/libefivar/uefi-dplib.h
+++ lib/libefivar/uefi-dplib.h
@@ -55,28 +55,27 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Library/PcdLib.h>
 #include <IndustryStandard/Bluetooth.h>
 
-#define IS_COMMA(a)                ((a) == ',')
-#define IS_HYPHEN(a)               ((a) == '-')
-#define IS_DOT(a)                  ((a) == '.')
-#define IS_LEFT_PARENTH(a)         ((a) == '(')
-#define IS_RIGHT_PARENTH(a)        ((a) == ')')
-#define IS_SLASH(a)                ((a) == '/')
-#define IS_NULL(a)                 ((a) == '\0')
-
+#define IS_COMMA(a)          ((a) == ',')
+#define IS_HYPHEN(a)         ((a) == '-')
+#define IS_DOT(a)            ((a) == '.')
+#define IS_LEFT_PARENTH(a)   ((a) == '(')
+#define IS_RIGHT_PARENTH(a)  ((a) == ')')
+#define IS_SLASH(a)          ((a) == '/')
+#define IS_NULL(a)           ((a) == '\0')
 
 //
 // Private Data structure
 //
 typedef struct {
-  char  *Str;
-  UINTN   Count;
-  UINTN   Capacity;
+  char      *Str;
+  UINTN     Count;
+  UINTN     Capacity;
 } POOL_PRINT;
 
 typedef
 EFI_DEVICE_PATH_PROTOCOL  *
 (*DEVICE_PATH_FROM_TEXT) (
-  IN char *Str
+  IN  char    *Str
   );
 
 typedef
@@ -89,90 +88,90 @@ VOID
   );
 
 typedef struct {
-  UINT8                Type;
-  UINT8                SubType;
-  DEVICE_PATH_TO_TEXT  Function;
+  UINT8                  Type;
+  UINT8                  SubType;
+  DEVICE_PATH_TO_TEXT    Function;
 } DEVICE_PATH_TO_TEXT_TABLE;
 
 typedef struct {
-  UINT8                Type;
-  const char          *Text;
+  UINT8     Type;
+  const char    *Text;
 } DEVICE_PATH_TO_TEXT_GENERIC_TABLE;
 
 typedef struct {
-  const char                *DevicePathNodeText;
-  DEVICE_PATH_FROM_TEXT     Function;
+  const char               *DevicePathNodeText;
+  DEVICE_PATH_FROM_TEXT    Function;
 } DEVICE_PATH_FROM_TEXT_TABLE;
 
 typedef struct {
-  BOOLEAN ClassExist;
-  UINT8   Class;
-  BOOLEAN SubClassExist;
-  UINT8   SubClass;
+  BOOLEAN    ClassExist;
+  UINT8      Class;
+  BOOLEAN    SubClassExist;
+  UINT8      SubClass;
 } USB_CLASS_TEXT;
 
-#define USB_CLASS_AUDIO            1
-#define USB_CLASS_CDCCONTROL       2
-#define USB_CLASS_HID              3
-#define USB_CLASS_IMAGE            6
-#define USB_CLASS_PRINTER          7
-#define USB_CLASS_MASS_STORAGE     8
-#define USB_CLASS_HUB              9
-#define USB_CLASS_CDCDATA          10
-#define USB_CLASS_SMART_CARD       11
-#define USB_CLASS_VIDEO            14
-#define USB_CLASS_DIAGNOSTIC       220
-#define USB_CLASS_WIRELESS         224
+#define USB_CLASS_AUDIO         1
+#define USB_CLASS_CDCCONTROL    2
+#define USB_CLASS_HID           3
+#define USB_CLASS_IMAGE         6
+#define USB_CLASS_PRINTER       7
+#define USB_CLASS_MASS_STORAGE  8
+#define USB_CLASS_HUB           9
+#define USB_CLASS_CDCDATA       10
+#define USB_CLASS_SMART_CARD    11
+#define USB_CLASS_VIDEO         14
+#define USB_CLASS_DIAGNOSTIC    220
+#define USB_CLASS_WIRELESS      224
 
-#define USB_CLASS_RESERVE          254
-#define USB_SUBCLASS_FW_UPDATE     1
-#define USB_SUBCLASS_IRDA_BRIDGE   2
-#define USB_SUBCLASS_TEST          3
+#define USB_CLASS_RESERVE         254
+#define USB_SUBCLASS_FW_UPDATE    1
+#define USB_SUBCLASS_IRDA_BRIDGE  2
+#define USB_SUBCLASS_TEST         3
 
-#define RFC_1700_UDP_PROTOCOL      17
-#define RFC_1700_TCP_PROTOCOL      6
+#define RFC_1700_UDP_PROTOCOL  17
+#define RFC_1700_TCP_PROTOCOL  6
 
 #pragma pack(1)
 
 typedef struct {
-  EFI_DEVICE_PATH_PROTOCOL  Header;
-  EFI_GUID                  Guid;
-  UINT8                     VendorDefinedData[1];
+  EFI_DEVICE_PATH_PROTOCOL    Header;
+  EFI_GUID                    Guid;
+  UINT8                       VendorDefinedData[1];
 } VENDOR_DEFINED_HARDWARE_DEVICE_PATH;
 
 typedef struct {
-  EFI_DEVICE_PATH_PROTOCOL  Header;
-  EFI_GUID                  Guid;
-  UINT8                     VendorDefinedData[1];
+  EFI_DEVICE_PATH_PROTOCOL    Header;
+  EFI_GUID                    Guid;
+  UINT8                       VendorDefinedData[1];
 } VENDOR_DEFINED_MESSAGING_DEVICE_PATH;
 
 typedef struct {
-  EFI_DEVICE_PATH_PROTOCOL  Header;
-  EFI_GUID                  Guid;
-  UINT8                     VendorDefinedData[1];
+  EFI_DEVICE_PATH_PROTOCOL    Header;
+  EFI_GUID                    Guid;
+  UINT8                       VendorDefinedData[1];
 } VENDOR_DEFINED_MEDIA_DEVICE_PATH;
 
 typedef struct {
-  EFI_DEVICE_PATH_PROTOCOL  Header;
-  UINT32                    Hid;
-  UINT32                    Uid;
-  UINT32                    Cid;
-  CHAR8                     HidUidCidStr[3];
+  EFI_DEVICE_PATH_PROTOCOL    Header;
+  UINT32                      Hid;
+  UINT32                      Uid;
+  UINT32                      Cid;
+  CHAR8                       HidUidCidStr[3];
 } ACPI_EXTENDED_HID_DEVICE_PATH_WITH_STR;
 
 typedef struct {
-  EFI_DEVICE_PATH_PROTOCOL  Header;
-  UINT16                    NetworkProtocol;
-  UINT16                    LoginOption;
-  UINT64                    Lun;
-  UINT16                    TargetPortalGroupTag;
-  CHAR8                     TargetName[1];
+  EFI_DEVICE_PATH_PROTOCOL    Header;
+  UINT16                      NetworkProtocol;
+  UINT16                      LoginOption;
+  UINT64                      Lun;
+  UINT16                      TargetPortalGroupTag;
+  CHAR8                       TargetName[1];
 } ISCSI_DEVICE_PATH_WITH_NAME;
 
 typedef struct {
-  EFI_DEVICE_PATH_PROTOCOL  Header;
-  EFI_GUID                  Guid;
-  UINT8                     VendorDefinedData[1];
+  EFI_DEVICE_PATH_PROTOCOL    Header;
+  EFI_GUID                    Guid;
+  UINT8                       VendorDefinedData[1];
 } VENDOR_DEVICE_PATH_WITH_DATA;
 
 #pragma pack()
@@ -347,8 +346,8 @@ UefiDevicePathLibAppendDevicePathInstance (
 EFI_DEVICE_PATH_PROTOCOL *
 EFIAPI
 UefiDevicePathLibGetNextDevicePathInstance (
-  IN OUT EFI_DEVICE_PATH_PROTOCOL    **DevicePath,
-  OUT UINTN                          *Size
+  IN OUT EFI_DEVICE_PATH_PROTOCOL  **DevicePath,
+  OUT UINTN                        *Size
   );
 
 /**
@@ -373,9 +372,9 @@ UefiDevicePathLibGetNextDevicePathInstance (
 EFI_DEVICE_PATH_PROTOCOL *
 EFIAPI
 UefiDevicePathLibCreateDeviceNode (
-  IN UINT8                           NodeType,
-  IN UINT8                           NodeSubType,
-  IN UINT16                          NodeLength
+  IN UINT8   NodeType,
+  IN UINT8   NodeSubType,
+  IN UINT16  NodeLength
   );
 
 /**
@@ -399,7 +398,6 @@ UefiDevicePathLibIsDevicePathMultiInstance (
   IN CONST EFI_DEVICE_PATH_PROTOCOL  *DevicePath
   );
 
-
 /**
   Converts a device path to its text representation.
 
@@ -418,9 +416,9 @@ UefiDevicePathLibIsDevicePathMultiInstance (
 CHAR16 *
 EFIAPI
 UefiDevicePathLibConvertDevicePathToText (
-  IN CONST EFI_DEVICE_PATH_PROTOCOL   *DevicePath,
-  IN BOOLEAN                          DisplayOnly,
-  IN BOOLEAN                          AllowShortcuts
+  IN CONST EFI_DEVICE_PATH_PROTOCOL  *DevicePath,
+  IN BOOLEAN                         DisplayOnly,
+  IN BOOLEAN                         AllowShortcuts
   );
 
 /**
@@ -460,7 +458,7 @@ UefiDevicePathLibConvertDeviceNodeToText (
 EFI_DEVICE_PATH_PROTOCOL *
 EFIAPI
 UefiDevicePathLibConvertTextToDeviceNode (
-  IN CONST CHAR16 *TextDeviceNode
+  IN CONST CHAR16  *TextDeviceNode
   );
 
 /**
@@ -478,7 +476,7 @@ UefiDevicePathLibConvertTextToDeviceNode (
 EFI_DEVICE_PATH_PROTOCOL *
 EFIAPI
 UefiDevicePathLibConvertTextToDevicePath (
-  IN CONST CHAR16 *TextDevicePath
+  IN CONST CHAR16  *TextDevicePath
   );
 #else
 
