11a9ff5bc150bf555d4af58a658320fcf80abef9
Jose Luis Duran <jlduran@gmail.com>
2022-02-23 21:54:02 -0300

    libefivar: Replace BSD License with BSD+Patent License
    Replace BSD 2-Clause License with BSD+Patent License.  This change is
    based on the following emails:
    
      https://lists.01.org/pipermail/edk2-devel/2019-February/036260.html
      https://lists.01.org/pipermail/edk2-devel/2018-October/030385.html
    
    RFCs with detailed process for the license change:
    
      V3: https://lists.01.org/pipermail/edk2-devel/2019-March/038116.html
      V2: https://lists.01.org/pipermail/edk2-devel/2019-March/037669.html
      V1: https://lists.01.org/pipermail/edk2-devel/2019-March/037500.html
    
    Upstream Bug:	https://bugzilla.tianocore.org/show_bug.cgi?id=1373
    Obtained from:	https://github.com/tianocore/edk2/commit/9344f0921518309295da89c221d10cbead8531aa
    Pull Request:   https://github.com/freebsd/freebsd-src/pull/581

--- lib/libefivar/efivar-dp-parse.c
+++ lib/libefivar/efivar-dp-parse.c
@@ -64,13 +64,7 @@ __FBSDID("$FreeBSD$");
   DevicePathFromText protocol as defined in the UEFI 2.0 specification.
 
 Copyright (c) 2013 - 2018, Intel Corporation. All rights reserved.<BR>
-This program and the accompanying materials
-are licensed and made available under the terms and conditions of the BSD License
-which accompanies this distribution.  The full text of the license may be found at
-http://opensource.org/licenses/bsd-license.php
-
-THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
