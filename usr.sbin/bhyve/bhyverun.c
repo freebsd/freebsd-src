b5b54b376d65d060fdc84faff0c5db3d31c2de4f
Andy Fiddaman <andy@omniosce.org>
2022-02-24 08:36:41 -0900

    bhyve: plug memory leak in topology_parse()
    Reviewed by:    jhb, rew
    Differential Revision:  https://reviews.freebsd.org/D34301

--- usr.sbin/bhyve/bhyverun.c
+++ usr.sbin/bhyve/bhyverun.c
@@ -270,7 +270,7 @@ usage(int code)
 static int
 topology_parse(const char *opt)
 {
-	char *cp, *str;
+	char *cp, *str, *tofree;
 
 	if (*opt == '\0') {
 		set_config_value("sockets", "1");
@@ -280,7 +280,7 @@ topology_parse(const char *opt)
 		return (0);
 	}
 
-	str = strdup(opt);
+	tofree = str = strdup(opt);
 	if (str == NULL)
 		errx(4, "Failed to allocate memory");
 
@@ -302,11 +302,11 @@ topology_parse(const char *opt)
 		else
 			set_config_value("cpus", cp);
 	}
-	free(str);
+	free(tofree);
 	return (0);
 
 out:
-	free(str);
+	free(tofree);
 	return (-1);
 }
 
