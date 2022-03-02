50930b5d9facfc74744b7a808f03b7a20c8e603c
Ed Maste <emaste@FreeBSD.org>
2022-03-02 09:45:23 -0500

    ssh: tidy include handling
    Centralize optional krb5_config.h handling in ssh.mk.  Do not add
    headers (that are committed to the src tree) to SRCS as there is no
    need.
    
    Reviewed by:	imp, jlduran, kevans (all earlier)
    MFC after:	1 month
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D34409

--- secure/ssh.mk
+++ secure/ssh.mk
@@ -5,7 +5,10 @@
 SSHDIR=		${SRCTOP}/crypto/openssh
 
 CFLAGS+= -I${SSHDIR} -include ssh_namespace.h
-SRCS+=	 ssh_namespace.h
+
+.if ${MK_GSSAPI} != "no" && ${MK_KERBEROS_SUPPORT} != "no"
+CFLAGS+= -include krb5_config.h
+.endif
 
 CFLAGS+= -DXAUTH_PATH=\"${LOCALBASE:U/usr/local}/bin/xauth\"
 
