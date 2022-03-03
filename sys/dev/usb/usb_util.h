9157566d7ad58560c6fa0ba6c2b88587688ee02c
Hans Petter Selasky <hselasky@FreeBSD.org>
2022-03-03 10:22:41 +0100

    usb(4): Factor out the usb_check_request() function.
    No functional change.
    
    Sponsored by:	NVIDIA Networking
    
    (cherry picked from commit 8ed5bb59e913c61e61a4232e8221f35256f72ad2)

--- sys/dev/usb/usb_util.h
+++ sys/dev/usb/usb_util.h
@@ -29,8 +29,12 @@
 #ifndef _USB_UTIL_H_
 #define	_USB_UTIL_H_
 
+struct usb_device;
+struct usb_device_request;
+
 uint8_t	usb_make_str_desc(void *ptr, uint16_t max_len, const char *s);
 void	usb_printbcd(char *p, uint16_t p_len, uint16_t bcd);
 void	usb_trim_spaces(char *p);
+int	usb_check_request(struct usb_device *, struct usb_device_request *);
 
 #endif					/* _USB_UTIL_H_ */
