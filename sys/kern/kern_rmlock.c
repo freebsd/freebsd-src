ea683b4bae98f17dd978fbafc734d8eb1dfd66dc
Mark Johnston <markj@FreeBSD.org>
2022-03-07 10:35:59 -0500

    rmlock: Temporarily revert commit c84bb8cd771c
    It appears to have introduced a regression on arm64, possibly due to the
    fact that the pcpu pointer is reloaded outside of the critical section
    in _rm_rlock().  Until this is resolved one way or another, let's
    revert.
    
    Reported by:	Ronald Klop <ronald-lists@klop.ws>
    Sponsored by:	The FreeBSD Foundation
    
    (cherry picked from commit afb44cb01018eb1363cb4ee20a31534b844d00f7)

--- sys/kern/kern_rmlock.c
+++ sys/kern/kern_rmlock.c
@@ -451,7 +451,7 @@ _rm_rlock(struct rmlock *rm, struct rm_priotracker *tracker, int trylock)
 
 	__compiler_membar();
 
-	pc = get_pcpu();
+	pc = cpuid_to_pcpu[td->td_oncpu];
 
 	rm_tracker_add(pc, tracker);
 
@@ -516,7 +516,7 @@ _rm_runlock(struct rmlock *rm, struct rm_priotracker *tracker)
 		return;
 
 	td->td_critnest++;	/* critical_enter(); */
-	pc = get_pcpu();
+	pc = cpuid_to_pcpu[td->td_oncpu];
 	rm_tracker_remove(pc, tracker);
 	td->td_critnest--;
 	sched_unpin();
