a9c6dbe1fd19fb57f40131c3b16cae45ddcd7fe8
Marcin Wojtas <mw@FreeBSD.org>
2022-03-07 17:40:51 +0100

    Revert "sdhci: extend bus_dma_tag boundary to 64-bit space"
    This reverts commit fa15bd9dac9ccbdcbadc1643abce1a6a6ab28ade.
    
    Reason for revert: the patch is incomplete. 64-bit operation
    is supported fully in SDHCI v4.0, v3.0 does it only for ADMA mode.
    This differentiation is missing and should be taken into consideration
    in case the reverted code is re-introduced.
    
    Reported by: mmel

--- sys/dev/sdhci/sdhci.c
+++ sys/dev/sdhci/sdhci.c
@@ -132,7 +132,7 @@ static int sdhci_cam_update_ios(struct sdhci_slot *slot);
 #endif
 
 /* helper routines */
-static int sdhci_dma_alloc(struct sdhci_slot *slot, uint32_t caps);
+static int sdhci_dma_alloc(struct sdhci_slot *slot);
 static void sdhci_dma_free(struct sdhci_slot *slot);
 static void sdhci_dumpcaps(struct sdhci_slot *slot);
 static void sdhci_dumpcaps_buf(struct sdhci_slot *slot, struct sbuf *s);
@@ -788,7 +788,7 @@ sdhci_card_poll(void *arg)
 }
 
 static int
-sdhci_dma_alloc(struct sdhci_slot *slot, uint32_t caps)
+sdhci_dma_alloc(struct sdhci_slot *slot)
 {
 	int err;
 
@@ -821,8 +821,7 @@ sdhci_dma_alloc(struct sdhci_slot *slot, uint32_t caps)
 	 * be aligned to the SDMA boundary.
 	 */
 	err = bus_dma_tag_create(bus_get_dma_tag(slot->bus), slot->sdma_bbufsz,
-	    0, (caps & SDHCI_CAN_DO_64BIT) ? BUS_SPACE_MAXADDR :
-	    BUS_SPACE_MAXADDR_32BIT, BUS_SPACE_MAXADDR, NULL, NULL,
+	    0, BUS_SPACE_MAXADDR_32BIT, BUS_SPACE_MAXADDR, NULL, NULL,
 	    slot->sdma_bbufsz, 1, slot->sdma_bbufsz, BUS_DMA_ALLOCNOW,
 	    NULL, NULL, &slot->dmatag);
 	if (err != 0) {
@@ -1107,7 +1106,7 @@ no_tuning:
 		slot->opt &= ~SDHCI_HAVE_DMA;
 
 	if (slot->opt & SDHCI_HAVE_DMA) {
-		err = sdhci_dma_alloc(slot, caps);
+		err = sdhci_dma_alloc(slot);
 		if (err != 0) {
 			if (slot->opt & SDHCI_TUNING_SUPPORTED) {
 				free(slot->tune_req, M_DEVBUF);
