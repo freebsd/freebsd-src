730510dc1ab2049d707706241d9a6e73a6952a4e
John Baldwin <jhb@FreeBSD.org>
2022-03-09 15:38:49 -0800

    bhyve: Allocate mmio_hint array based on number of guest CPUs.
    This avoids an instance of hardcoding VM_MAXCPU in userspace.
    
    Reviewed by:	grehan
    Differential Revision:	https://reviews.freebsd.org/D34489

--- usr.sbin/bhyve/mem.h
+++ usr.sbin/bhyve/mem.h
@@ -52,7 +52,7 @@ struct mem_range {
 #define	MEM_F_RW		0x3
 #define	MEM_F_IMMUTABLE		0x4	/* mem_range cannot be unregistered */
 
-void	init_mem(void);
+void	init_mem(int ncpu);
 int     emulate_mem(struct vmctx *, int vcpu, uint64_t paddr, struct vie *vie,
 		    struct vm_guest_paging *paging);
 
