3e9cb41400c3ef8691679a2f5ac11106093a0283
Dimitry Andric <dim@FreeBSD.org>
2022-03-17 20:16:00 +0100

    Merge llvm-project release/14.x llvmorg-14.0.0-rc4-2-gadd3ab7f4c8a
    This updates llvm, clang, compiler-rt, libc++, libunwind, lld, lldb and
    openmp to llvmorg-14.0.0-rc4-2-gadd3ab7f4c8a.
    
    PR:		261742
    MFC after:	2 weeks
    
    (cherry picked from commit fb03ea46eb853b2d128828e9d82882125bcc1657)

--- lib/clang/include/llvm/Support/VCSRevision.h
+++ lib/clang/include/llvm/Support/VCSRevision.h
@@ -1,3 +1,3 @@
 /* $FreeBSD$ */
-#define LLVM_REVISION "llvmorg-14.0.0-rc2-12-g09546e1b5103"
+#define LLVM_REVISION "llvmorg-14.0.0-rc4-2-gadd3ab7f4c8a"
 #define LLVM_REPOSITORY "https://github.com/llvm/llvm-project.git"
