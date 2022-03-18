b31b45f26a0ba0df8045d48f78eca497c1c8f402
Ed Maste <emaste@FreeBSD.org>
2022-03-18 16:11:32 -0400

    Remove snd_aureal driver source
    This driver was not finished when it was committed in 1999 and was never
    connected to the build.
    
    A version of the driver used to be available in ports as
    audio/aureal-kmod, but it has been removed.  It did not build on FreeBSD
    10.x or later and the binary objects it required were not available
    after Google Code disappeared.
    
    PR:		124343
    Reported by:	joel
    MFC after:	1 week
    Sponsored by:	The FreeBSD Foundation
    
    (cherry picked from commit ef5dec7cc75b985d9ada767f716e19ec44f5c44a)

--- sys/dev/sound/driver.c
+++ sys/dev/sound/driver.c
@@ -61,7 +61,6 @@ MODULE_VERSION(snd_driver, 1);
 MODULE_DEPEND(snd_driver, snd_ad1816, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_als4000, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_atiixp, 1, 1, 1);
-/* MODULE_DEPEND(snd_driver, snd_aureal, 1, 1, 1); */
 MODULE_DEPEND(snd_driver, snd_cmi, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_cs4281, 1, 1, 1);
 MODULE_DEPEND(snd_driver, snd_csa, 1, 1, 1);
