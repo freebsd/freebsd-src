3f22e8790b5bd65e3735f472052932f24b636c36
Emmanuel Vadot <manu@FreeBSD.org>
2022-03-22 10:28:02 +0100

    linuxkpi: Add down_write_nest_lock
    Simply calls down_write like in Linux (when CONFIG_DEBUG_LOCK_ALLOC isn't
    specified)
    Needed by drm v5.10
    
    MFC after:	1 month
    Reviewed by:	hselasky
    Sponsored by:	Beckhoff Automation GmbH & Co. KG
    Differential Revision:	https://reviews.freebsd.org/D34642

--- sys/compat/linuxkpi/common/include/linux/rwsem.h
+++ sys/compat/linuxkpi/common/include/linux/rwsem.h
@@ -51,6 +51,7 @@ struct rw_semaphore {
 #define	downgrade_write(_rw)		sx_downgrade(&(_rw)->sx)
 #define	down_read_nested(_rw, _sc)	down_read(_rw)
 #define	init_rwsem(_rw)			linux_init_rwsem(_rw, rwsem_name("lnxrwsem"))
+#define	down_write_nest_lock(sem, _rw)	down_write(_rw)
 
 #ifdef WITNESS_ALL
 /* NOTE: the maximum WITNESS name is 64 chars */
