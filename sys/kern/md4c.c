acba84ff6d941154a446da5f6517efd9a70ce453
Kyle Evans <kevans@FreeBSD.org>
2022-03-22 21:39:02 -0500

    Consolidate md4 implementations written in C
    We currently have one in libmd and another in the kernel that's almost
    completely identical.  Standardize on the kernel version.
    
    PR:		280784 (exp-run)

--- sys/kern/md4c.c
+++ sys/kern/md4c.c
@@ -26,7 +26,11 @@
  */
 
 #include <sys/param.h>
+#ifdef _KERNEL
 #include <sys/systm.h>
+#else
+#include <string.h>
+#endif
 #include <sys/md4.h>
 
 typedef unsigned char *POINTER;
@@ -277,3 +281,18 @@ Decode(UINT4 *output, const unsigned char *input, unsigned int len)
     output[i] = ((UINT4)input[j]) | (((UINT4)input[j+1]) << 8) |
       (((UINT4)input[j+2]) << 16) | (((UINT4)input[j+3]) << 24);
 }
+
+#ifdef WEAK_REFS
+/* When building libmd, provide weak references. Note: this is not
+   activated in the context of compiling these sources for internal
+   use in libcrypt.
+ */
+#undef MD4Init
+__weak_reference(_libmd_MD4Init, MD4Init);
+#undef MD4Update
+__weak_reference(_libmd_MD4Update, MD4Update);
+#undef MD4Pad
+__weak_reference(_libmd_MD4Pad, MD4Pad);
+#undef MD4Final
+__weak_reference(_libmd_MD4Final, MD4Final);
+#endif
