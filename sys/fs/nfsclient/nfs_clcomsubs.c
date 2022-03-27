36abd36848a77d58f536d65d1162a6d3940d55fe
Gordon Bergling <gbe@FreeBSD.org>
2022-03-27 19:27:05 +0200

    nfsclient: Fix a typos in source code comments
    - s/ony/only/
    
    Obtained from:	NetBSD
    MFC after:	3 days

--- sys/fs/nfsclient/nfs_clcomsubs.c
+++ sys/fs/nfsclient/nfs_clcomsubs.c
@@ -51,7 +51,7 @@ NFSCLSTATEMUTEX;
 
 /*
  * copies a uio scatter/gather list to an mbuf chain.
- * NOTE: can ony handle iovcnt == 1
+ * NOTE: can only handle iovcnt == 1
  */
 void
 nfsm_uiombuf(struct nfsrv_descript *nd, struct uio *uiop, int siz)
@@ -155,7 +155,7 @@ nfsm_uiombuf(struct nfsrv_descript *nd, struct uio *uiop, int siz)
 /*
  * copies a uio scatter/gather list to an mbuf chain.
  * This version returns the mbuf list and does not use "nd".
- * NOTE: can ony handle iovcnt == 1
+ * NOTE: can only handle iovcnt == 1
  */
 struct mbuf *
 nfsm_uiombuflist(struct uio *uiop, int siz, u_int maxext)
