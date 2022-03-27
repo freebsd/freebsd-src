b5efaf71b125d2217c137a1cfad0e6970ee48a46
Gordon Bergling <gbe@FreeBSD.org>
2022-03-27 18:05:58 +0200

    xen(4): Fix a few typos in source code comments
    - s/querried/queried/
    
    (cherry picked from commit bba12ee45394dc5aea1162c9b354daa7b2d24363)

--- sys/x86/xen/xen_intr.c
+++ sys/x86/xen/xen_intr.c
@@ -491,7 +491,7 @@ xen_intr_isrc(xen_intr_handle_t handle)
  * Determine the event channel ports at the given section of the
  * event port bitmap which have pending events for the given cpu.
  * 
- * \param pcpu  The Xen interrupt pcpu data for the cpu being querried.
+ * \param pcpu  The Xen interrupt pcpu data for the cpu being queried.
  * \param sh    The Xen shared info area.
  * \param idx   The index of the section of the event channel bitmap to
  *              inspect.
