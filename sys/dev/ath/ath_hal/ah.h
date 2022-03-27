8eb774fe54c88a5417a287bd811a917fdfe1c2ba
Gordon Bergling <gbe@FreeBSD.org>
2022-03-27 19:28:49 +0200

    ath: Fix a typo in a source code comment
    - s/ony/only/
    
    MFC after:	3 days

--- sys/dev/ath/ath_hal/ah.h
+++ sys/dev/ath/ath_hal/ah.h
@@ -1030,7 +1030,7 @@ typedef struct {
 	u_int16_t	ss_fft_period;	/* Skip interval for FFT reports */
 	u_int16_t	ss_period;	/* Spectral scan period */
 	u_int16_t	ss_count;	/* # of reports to return from ss_active */
-	u_int16_t	ss_short_report;/* Set to report ony 1 set of FFT results */
+	u_int16_t	ss_short_report;/* Set to report only 1 set of FFT results */
 	u_int8_t	radar_bin_thresh_sel;	/* strong signal radar FFT threshold configuration */
 	u_int16_t	ss_spectral_pri;		/* are we doing a noise power cal ? */
 	int8_t		ss_nf_cal[AH_MAX_CHAINS*2];     /* nf calibrated values for ctl+ext from eeprom */
