f4dc553a72d7b117b79d2e18980c486706beb65f
Gordon Bergling <gbe@FreeBSD.org>
2022-03-27 17:59:43 +0200

    mlx5en(4): Fix a few typos in source code comments
    - s/persistant/persistent/
    
    (cherry picked from commit 4a87beeccb04430a154da19721e1c4715e9ddd31)

--- sys/dev/mlx5/mlx5_en/en.h
+++ sys/dev/mlx5/mlx5_en/en.h
@@ -754,7 +754,7 @@ struct mlx5e_rq_mbuf {
 };
 
 struct mlx5e_rq {
-	/* persistant fields */
+	/* persistent fields */
 	struct mtx mtx;
 	struct mlx5e_rq_stats stats;
 	struct callout watchdog;
@@ -841,7 +841,7 @@ enum {
 };
 
 struct mlx5e_sq {
-	/* persistant fields */
+	/* persistent fields */
 	struct	mtx lock;
 	struct	mtx comp_lock;
 	struct	mlx5e_sq_stats stats;
