01d74e5c7a35195160984d2c015862f08aeda081
Gordon Bergling <gbe@FreeBSD.org>
2022-04-02 15:20:59 +0200

    iscsi(4): Fix a typo in a source code comment
    - s/commmand/command/
    
    MFC after:	3 days

--- sys/dev/isci/scil/sati_request_sense.c
+++ sys/dev/isci/scil/sati_request_sense.c
@@ -86,7 +86,7 @@ __FBSDID("$FreeBSD$");
  * @retval SATI_SUCCESS indicates that the translation was supported and occurred
  *         without error.
  * @retval SATI_FAILURE_CHECK_RESPONSE_DATA This value is returned if
- *         the SATII is processing a format unit commmand.
+ *         the SATII is processing a format unit command.
  * @retval SATI_COMPLETE indicates that the translation was supported, occurred without
  *         error, and no additional translation is necessary.
  */
