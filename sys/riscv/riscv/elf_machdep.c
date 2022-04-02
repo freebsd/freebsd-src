4fab95e6fae2eaa7faf05141a29d9eeb424dfd35
Gordon Bergling <gbe@FreeBSD.org>
2022-04-02 08:58:45 +0200

    risc-v: Fix two typos in a source code comment
    - s/interger/integer/
    - s/manupilate/manipulate/
    
    MFC after:	3 days

--- sys/riscv/riscv/elf_machdep.c
+++ sys/riscv/riscv/elf_machdep.c
@@ -172,7 +172,7 @@ elf64_dump_thread(struct thread *td, void *dst, size_t *off)
 }
 
 /*
- * Following 4 functions are used to manupilate bits on 32bit interger value.
+ * Following 4 functions are used to manipulate bits on 32bit integer value.
  * FIXME: I implemetend for ease-to-understand rather than for well-optimized.
  */
 static uint32_t
