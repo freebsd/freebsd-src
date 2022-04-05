92cba0c90ac5454c6615e844407b297ce6c9702a
Warner Losh <imp@FreeBSD.org>
2022-04-04 20:18:10 -0600

    emu_midi_probe: eliminate write only variable r
    Sponsored by:		Netflix

--- sys/dev/sound/pci/emu10kx-midi.c
+++ sys/dev/sound/pci/emu10kx-midi.c
@@ -142,15 +142,15 @@ static int
 emu_midi_probe(device_t dev)
 {
 	struct emu_midi_softc *scp;
-	uintptr_t func, r, is_emu10k1;
+	uintptr_t func, is_emu10k1;
 
-	r = BUS_READ_IVAR(device_get_parent(dev), dev, 0, &func);
+	BUS_READ_IVAR(device_get_parent(dev), dev, 0, &func);
 	if (func != SCF_MIDI)
 		return (ENXIO);
 
 	scp = device_get_softc(dev);
 	bzero(scp, sizeof(*scp));
-	r = BUS_READ_IVAR(device_get_parent(dev), dev, EMU_VAR_ISEMU10K1, &is_emu10k1);
+	BUS_READ_IVAR(device_get_parent(dev), dev, EMU_VAR_ISEMU10K1, &is_emu10k1);
 	scp->is_emu10k1 = is_emu10k1 ? 1 : 0;
 
 	device_set_desc(dev, "EMU10Kx MIDI Interface");
