7c564a114e1338b2ddc061b24086d575cbe777b6
Warner Losh <imp@FreeBSD.org>
2022-04-04 18:05:03 -0600

    pmc_save_user_callchain: eliminate write only td variable
    Sponsored by:		Netflix

--- sys/dev/hwpmc/hwpmc_arm.c
+++ sys/dev/hwpmc/hwpmc_arm.c
@@ -128,13 +128,11 @@ pmc_save_user_callchain(uintptr_t *cc, int maxsamples,
     struct trapframe *tf)
 {
 	uintptr_t pc, r, oldfp, fp;
-	struct thread *td;
 	int count;
 
 	KASSERT(TRAPF_USERMODE(tf), ("[x86,%d] Not a user trap frame tf=%p",
 	    __LINE__, (void *) tf));
 
-	td = curthread;
 	pc = PMC_TRAPFRAME_TO_PC(tf);
 	*cc++ = pc;
 
