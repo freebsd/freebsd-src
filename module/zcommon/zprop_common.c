freebsd/freebsd-src

commit ddb271d0abca1b8113558744c6d56d2aacaf4542

Flex non-pretty-printed properties and raw-/pretty-print remaining ones
Before:
nabijaczleweli@tarta:~/store/code/zfs$ /sbin/zpool list -Td -o name,size,alloc,free,ckpoint,expandsz,guid,load_guid,frag,cap,dedup,health,altroot,guid,dedupditto,load_guid,maxblocksize,maxdnodesize 2>/dev/null
Sun 20 Feb 03:57:44 CET 2022
NAME         SIZE  ALLOC   FREE  CKPOINT  EXPANDSZ   GUID  LOAD_GUID   FRAG    CAP  DEDUP    HEALTH  ALTROOT   GUID  DEDUPDITTO  LOAD_GUID  MAXBLOCKSIZE  MAXDNODESIZE
filling     25.5T  6.52T  18.9T        -       64M  11512889483096932869  11656109927366648364     1%    25%  1.00x    ONLINE  -        11512889483096932869           0  11656109927366648364       1048576         16384
tarta-boot   240M  50.6M   189M        -         -  2372068846917849656  7752280792179633787    12%    21%  1.00x    ONLINE  -        2372068846917849656           0  7752280792179633787       1048576           512
tarta-zoot  55.5G  6.42G  49.1G        -         -  12971868889665384604  8622632123393589527    17%    11%  1.00x    ONLINE  -        12971868889665384604           0  8622632123393589527       1048576         16384

nabijaczleweli@tarta:~/store/code/zfs$ /sbin/zfs list -o name,guid,keyguid,ivsetguid,createtxg,objsetid,pbkdf2iters,refratio -r tarta-zoot
NAME                                  GUID  KEYGUID  IVSETGUID  CREATETXG  OBJSETID  PBKDF2ITERS  REFRATIO
tarta-zoot                           1110930838977259561     659P          -          1        54            0     1.03x
tarta-zoot/PAGEFILE.SYS              2202570496672997800    3.20E          -       2163      1539            0     1.07x
tarta-zoot/dupa                      16941280502417785695    9.81E          -    2274707      1322  1000000000000     1.00x
tarta-zoot/etc                       17029963068508333530    12.9E          -       3663      1087            0     1.52x
tarta-zoot/home                      3508163802370032575    8.50E          -       3664       294            0     1.00x

module/zcommon/zprop_common.c
@@ -97,7 +97,8 @@ void
 zprop_register_impl(int prop, const char *name, zprop_type_t type,
     uint64_t numdefault, const char *strdefault, zprop_attr_t attr,
     int objset_types, const char *values, const char *colname,
-    boolean_t rightalign, boolean_t visible, const zprop_index_t *idx_tbl,
+    boolean_t rightalign, boolean_t visible, boolean_t flex,
+    const zprop_index_t *idx_tbl,
     const struct zfs_mod_supported_features *sfeatures)
 {
 	zprop_desc_t *prop_tbl = zprop_get_proptable(objset_types);
@@ -122,6 +123,7 @@ zprop_register_impl(int prop, const char *name, zprop_type_t type,
 	pd->pd_visible = visible;
 	pd->pd_zfs_mod_supported =
 	    zfs_mod_supported_prop(name, objset_types, sfeatures);
+	pd->pd_always_flex = flex;
 	pd->pd_table = idx_tbl;
 	pd->pd_table_size = 0;
 	while (idx_tbl && (idx_tbl++)->pi_name != NULL)
@@ -134,17 +136,20 @@ zprop_register_string(int prop, const char *name, const char *def,
     const char *colname, const struct zfs_mod_supported_features *sfeatures)
 {
 	zprop_register_impl(prop, name, PROP_TYPE_STRING, 0, def, attr,
-	    objset_types, values, colname, B_FALSE, B_TRUE, NULL, sfeatures);
+	    objset_types, values, colname, B_FALSE, B_TRUE, B_FALSE, NULL,
+	    sfeatures);
 
 }
 
 void
 zprop_register_number(int prop, const char *name, uint64_t def,
     zprop_attr_t attr, int objset_types, const char *values,
-    const char *colname, const struct zfs_mod_supported_features *sfeatures)
+    const char *colname, boolean_t flex,
+    const struct zfs_mod_supported_features *sfeatures)
 {
 	zprop_register_impl(prop, name, PROP_TYPE_NUMBER, def, NULL, attr,
-	    objset_types, values, colname, B_TRUE, B_TRUE, NULL, sfeatures);
+	    objset_types, values, colname, B_TRUE, B_TRUE, flex, NULL,
+	    sfeatures);
 }
 
 void
@@ -154,17 +159,18 @@ zprop_register_index(int prop, const char *name, uint64_t def,
     const struct zfs_mod_supported_features *sfeatures)
 {
 	zprop_register_impl(prop, name, PROP_TYPE_INDEX, def, NULL, attr,
-	    objset_types, values, colname, B_FALSE, B_TRUE, idx_tbl, sfeatures);
+	    objset_types, values, colname, B_FALSE, B_TRUE, B_FALSE, idx_tbl,
+	    sfeatures);
 }
 
 void
 zprop_register_hidden(int prop, const char *name, zprop_type_t type,
-    zprop_attr_t attr, int objset_types, const char *colname,
+    zprop_attr_t attr, int objset_types, const char *colname, boolean_t flex,
     const struct zfs_mod_supported_features *sfeatures)
 {
 	zprop_register_impl(prop, name, type, 0, NULL, attr,
 	    objset_types, NULL, colname,
-	    type == PROP_TYPE_NUMBER, B_FALSE, NULL, sfeatures);
+	    type == PROP_TYPE_NUMBER, B_FALSE, flex, NULL, sfeatures);
 }
 
 
@@ -440,7 +446,7 @@ zprop_width(int prop, boolean_t *fixed, zfs_type_t type)
 	if (type != ZFS_TYPE_POOL && type != ZFS_TYPE_VDEV)
 		type = ZFS_TYPE_FILESYSTEM;
 
-	*fixed = B_TRUE;
+	*fixed = !pd->pd_always_flex;
 
 	/*
 	 * Start with the width of the column name.
@@ -460,12 +466,6 @@ zprop_width(int prop, boolean_t *fixed, zfs_type_t type)
 		if (ret < 5)
 			ret = 5;
 		/*
-		 * 'creation' is handled specially because it's a number
-		 * internally, but displayed as a date string.
-		 */
-		if (type == ZFS_TYPE_FILESYSTEM && prop == ZFS_PROP_CREATION)
-			*fixed = B_FALSE;
-		/*
 		 * 'health' is handled specially because it's a number
 		 * internally, but displayed as a fixed 8 character string.
 		 */
@@ -482,7 +482,6 @@ zprop_width(int prop, boolean_t *fixed, zfs_type_t type)
 		break;
 
 	case PROP_TYPE_STRING:
-		*fixed = B_FALSE;
 		break;
 	}
 

include/zfs_prop.h
@@ -76,10 +76,11 @@ typedef struct {
 					/* fs | vol | snap; or pool */
 	const char *pd_values;		/* string telling acceptable values */
 	const char *pd_colname;		/* column header for "zfs list" */
-	boolean_t pd_rightalign;	/* column alignment for "zfs list" */
-	boolean_t pd_visible;		/* do we list this property with the */
+	boolean_t pd_rightalign: 1;	/* column alignment for "zfs list" */
+	boolean_t pd_visible: 1;	/* do we list this property with the */
 					/* "zfs get" help message */
-	boolean_t pd_zfs_mod_supported;	/* supported by running zfs module */
+	boolean_t pd_zfs_mod_supported: 1; /* supported by running zfs module */
+	boolean_t pd_always_flex: 1;	/* never fixed-width */
 	const zprop_index_t *pd_table;	/* for index properties, a table */
 					/* defining the possible values */
 	size_t pd_table_size;		/* number of entries in pd_table[] */
@@ -112,19 +113,20 @@ _ZFS_PROP_H zprop_desc_t *vdev_prop_get_table(void);
  */
 _ZFS_PROP_H void zprop_register_impl(int, const char *, zprop_type_t, uint64_t,
     const char *, zprop_attr_t, int, const char *, const char *,
-    boolean_t, boolean_t, const zprop_index_t *,
+    boolean_t, boolean_t, boolean_t, const zprop_index_t *,
     const struct zfs_mod_supported_features *);
 _ZFS_PROP_H void zprop_register_string(int, const char *, const char *,
     zprop_attr_t attr, int, const char *, const char *,
     const struct zfs_mod_supported_features *);
 _ZFS_PROP_H void zprop_register_number(int, const char *, uint64_t,
-    zprop_attr_t, int, const char *, const char *,
+    zprop_attr_t, int, const char *, const char *, boolean_t,
     const struct zfs_mod_supported_features *);
 _ZFS_PROP_H void zprop_register_index(int, const char *, uint64_t, zprop_attr_t,
     int, const char *, const char *, const zprop_index_t *,
     const struct zfs_mod_supported_features *);
 _ZFS_PROP_H void zprop_register_hidden(int, const char *, zprop_type_t,
-    zprop_attr_t, int, const char *, const struct zfs_mod_supported_features *);
+    zprop_attr_t, int, const char *, boolean_t,
+    const struct zfs_mod_supported_features *);
 
 /*
  * Common routines for zfs and zpool property management

lib/libzfs/libzfs.abi
@@ -10,6 +10,7 @@
     <dependency name='libm.so.6'/>
     <dependency name='libcrypto.so.1.1'/>
     <dependency name='libz.so.1'/>
+    <dependency name='libdl.so.2'/>
     <dependency name='libpthread.so.0'/>
     <dependency name='libc.so.6'/>
     <dependency name='ld-linux-x86-64.so.2'/>
@@ -907,7 +908,7 @@
   </abi-instr>
   <abi-instr address-size='64' path='assert.c' language='LANG_C99'>
     <function-decl name='libspl_set_assert_ok' mangled-name='libspl_set_assert_ok' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='libspl_set_assert_ok'>
-      <parameter type-id='f58c8277' name='val'/>
+      <parameter type-id='c19b74c3' name='val'/>
       <return type-id='48b5725f'/>
     </function-decl>
     <function-decl name='libspl_assertf' mangled-name='libspl_assertf' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='libspl_assertf'>
@@ -970,6 +971,11 @@
       <parameter type-id='64698d33' name='target'/>
       <return type-id='48b5725f'/>
     </function-decl>
+    <function-decl name='atomic_add_ptr' mangled-name='atomic_add_ptr' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_add_ptr'>
+      <parameter type-id='fe09dd29' name='target'/>
+      <parameter type-id='79a0948f' name='bits'/>
+      <return type-id='48b5725f'/>
+    </function-decl>
     <function-decl name='atomic_add_8' mangled-name='atomic_add_8' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_add_8'>
       <parameter type-id='aa323ea4' name='target'/>
       <parameter type-id='ee31ee44' name='bits'/>
@@ -990,7 +996,7 @@
       <parameter type-id='bd54fe1a' name='bits'/>
       <return type-id='48b5725f'/>
     </function-decl>
-    <function-decl name='atomic_add_ptr' mangled-name='atomic_add_ptr' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_add_ptr'>
+    <function-decl name='atomic_sub_ptr' mangled-name='atomic_sub_ptr' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_sub_ptr'>
       <parameter type-id='fe09dd29' name='target'/>
       <parameter type-id='79a0948f' name='bits'/>
       <return type-id='48b5725f'/>
@@ -1013,11 +1019,6 @@
     <function-decl name='atomic_sub_long' mangled-name='atomic_sub_long' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_sub_long'>
       <parameter type-id='64698d33' name='target'/>
       <parameter type-id='bd54fe1a' name='bits'/>
-      <return type-id='48b5725f'/>
-    </function-decl>
-    <function-decl name='atomic_sub_ptr' mangled-name='atomic_sub_ptr' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_sub_ptr'>
-      <parameter type-id='fe09dd29' name='target'/>
-      <parameter type-id='79a0948f' name='bits'/>
       <return type-id='48b5725f'/>
     </function-decl>
     <function-decl name='atomic_or_8' mangled-name='atomic_or_8' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_or_8'>
@@ -1092,6 +1093,11 @@
       <parameter type-id='64698d33' name='target'/>
       <return type-id='ee1f298e'/>
     </function-decl>
+    <function-decl name='atomic_add_ptr_nv' mangled-name='atomic_add_ptr_nv' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_add_ptr_nv'>
+      <parameter type-id='fe09dd29' name='target'/>
+      <parameter type-id='79a0948f' name='bits'/>
+      <return type-id='eaa32e2f'/>
+    </function-decl>
     <function-decl name='atomic_add_8_nv' mangled-name='atomic_add_8_nv' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_add_8_nv'>
       <parameter type-id='aa323ea4' name='target'/>
       <parameter type-id='ee31ee44' name='bits'/>
@@ -1112,7 +1118,7 @@
       <parameter type-id='bd54fe1a' name='bits'/>
       <return type-id='ee1f298e'/>
     </function-decl>
-    <function-decl name='atomic_add_ptr_nv' mangled-name='atomic_add_ptr_nv' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_add_ptr_nv'>
+    <function-decl name='atomic_sub_ptr_nv' mangled-name='atomic_sub_ptr_nv' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_sub_ptr_nv'>
       <parameter type-id='fe09dd29' name='target'/>
       <parameter type-id='79a0948f' name='bits'/>
       <return type-id='eaa32e2f'/>
@@ -1136,11 +1142,6 @@
       <parameter type-id='64698d33' name='target'/>
       <parameter type-id='bd54fe1a' name='bits'/>
       <return type-id='ee1f298e'/>
-    </function-decl>
-    <function-decl name='atomic_sub_ptr_nv' mangled-name='atomic_sub_ptr_nv' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_sub_ptr_nv'>
-      <parameter type-id='fe09dd29' name='target'/>
-      <parameter type-id='79a0948f' name='bits'/>
-      <return type-id='eaa32e2f'/>
     </function-decl>
     <function-decl name='atomic_or_8_nv' mangled-name='atomic_or_8_nv' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_or_8_nv'>
       <parameter type-id='aa323ea4' name='target'/>
@@ -1182,6 +1183,12 @@
       <parameter type-id='ee1f298e' name='bits'/>
       <return type-id='ee1f298e'/>
     </function-decl>
+    <function-decl name='atomic_cas_ptr' mangled-name='atomic_cas_ptr' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_cas_ptr'>
+      <parameter type-id='fe09dd29' name='target'/>
+      <parameter type-id='eaa32e2f' name='exp'/>
+      <parameter type-id='eaa32e2f' name='des'/>
+      <return type-id='eaa32e2f'/>
+    </function-decl>
     <function-decl name='atomic_cas_8' mangled-name='atomic_cas_8' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_cas_8'>
       <parameter type-id='aa323ea4' name='target'/>
       <parameter type-id='b96825af' name='exp'/>
@@ -1205,12 +1212,6 @@
       <parameter type-id='ee1f298e' name='exp'/>
       <parameter type-id='ee1f298e' name='des'/>
       <return type-id='ee1f298e'/>
-    </function-decl>
-    <function-decl name='atomic_cas_ptr' mangled-name='atomic_cas_ptr' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_cas_ptr'>
-      <parameter type-id='fe09dd29' name='target'/>
-      <parameter type-id='eaa32e2f' name='exp'/>
-      <parameter type-id='eaa32e2f' name='des'/>
-      <return type-id='eaa32e2f'/>
     </function-decl>
     <function-decl name='atomic_swap_8' mangled-name='atomic_swap_8' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='atomic_swap_8'>
       <parameter type-id='aa323ea4' name='target'/>
@@ -1854,8 +1855,8 @@
     </function-decl>
   </abi-instr>
   <abi-instr address-size='64' path='../../module/zcommon/zfeature_common.c' language='LANG_C99'>
-    <array-type-def dimensions='1' type-id='83f29ca2' size-in-bits='15680' id='d96379d0'>
-      <subrange length='35' type-id='7359adad' id='6a6a7e00'/>
+    <array-type-def dimensions='1' type-id='83f29ca2' size-in-bits='15680' id='9d60dcc5'>
+      <subrange length='35' type-id='7359adad' id='6e6845b5'/>
     </array-type-def>
     <enum-decl name='spa_feature' id='33ecb627'>
       <underlying-type type-id='9cac1fee'/>
@@ -1953,7 +1954,7 @@
     <qualified-type-def type-id='3eee3342' const='yes' id='0c1d5bbb'/>
     <pointer-type-def type-id='0c1d5bbb' size-in-bits='64' id='a3372543'/>
     <pointer-type-def type-id='d6618c78' size-in-bits='64' id='a8425263'/>
-    <var-decl name='spa_feature_table' type-id='d96379d0' mangled-name='spa_feature_table' visibility='default' elf-symbol-id='spa_feature_table'/>
+    <var-decl name='spa_feature_table' type-id='9d60dcc5' mangled-name='spa_feature_table' visibility='default' elf-symbol-id='spa_feature_table'/>
     <var-decl name='zfeature_checks_disable' type-id='c19b74c3' mangled-name='zfeature_checks_disable' visibility='default' elf-symbol-id='zfeature_checks_disable'/>
     <function-decl name='zfeature_is_valid_guid' mangled-name='zfeature_is_valid_guid' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='zfeature_is_valid_guid'>
       <parameter type-id='80f4b756' name='name'/>
@@ -2488,7 +2489,7 @@
       </data-member>
     </class-decl>
     <typedef-decl name='zprop_index_t' type-id='87957af9' id='64636ce3'/>
-    <class-decl name='zprop_desc_t' size-in-bits='704' is-struct='yes' naming-typedef-id='ffa52b96' visibility='default' id='bbff5e4b'>
+    <class-decl name='zprop_desc_t' size-in-bits='640' is-struct='yes' naming-typedef-id='ffa52b96' visibility='default' id='bbff5e4b'>
       <data-member access='public' layout-offset-in-bits='0'>
         <var-decl name='pd_name' type-id='80f4b756' visibility='default'/>
       </data-member>
@@ -2519,16 +2520,19 @@
       <data-member access='public' layout-offset-in-bits='448'>
         <var-decl name='pd_rightalign' type-id='c19b74c3' visibility='default'/>
       </data-member>
-      <data-member access='public' layout-offset-in-bits='480'>
+      <data-member access='public' layout-offset-in-bits='449'>
         <var-decl name='pd_visible' type-id='c19b74c3' visibility='default'/>
       </data-member>
-      <data-member access='public' layout-offset-in-bits='512'>
+      <data-member access='public' layout-offset-in-bits='450'>
         <var-decl name='pd_zfs_mod_supported' type-id='c19b74c3' visibility='default'/>
       </data-member>
-      <data-member access='public' layout-offset-in-bits='576'>
+      <data-member access='public' layout-offset-in-bits='451'>
+        <var-decl name='pd_always_flex' type-id='c19b74c3' visibility='default'/>
+      </data-member>
+      <data-member access='public' layout-offset-in-bits='512'>
         <var-decl name='pd_table' type-id='c8bc397b' visibility='default'/>
       </data-member>
-      <data-member access='public' layout-offset-in-bits='640'>
+      <data-member access='public' layout-offset-in-bits='576'>
         <var-decl name='pd_table_size' type-id='b59d7dce' visibility='default'/>
       </data-member>
     </class-decl>
@@ -2798,6 +2802,7 @@
       <parameter type-id='80f4b756' name='colname'/>
       <parameter type-id='c19b74c3' name='rightalign'/>
       <parameter type-id='c19b74c3' name='visible'/>
+      <parameter type-id='c19b74c3' name='flex'/>
       <parameter type-id='c8bc397b' name='idx_tbl'/>
       <parameter type-id='a3372543' name='sfeatures'/>
       <return type-id='48b5725f'/>
@@ -2821,6 +2826,7 @@
       <parameter type-id='95e97e5e' name='objset_types'/>
       <parameter type-id='80f4b756' name='values'/>
       <parameter type-id='80f4b756' name='colname'/>
+      <parameter type-id='c19b74c3' name='flex'/>
       <parameter type-id='a3372543' name='sfeatures'/>
       <return type-id='48b5725f'/>
     </function-decl>
@@ -2843,6 +2849,7 @@
       <parameter type-id='999701cc' name='attr'/>
       <parameter type-id='95e97e5e' name='objset_types'/>
       <parameter type-id='80f4b756' name='colname'/>
+      <parameter type-id='c19b74c3' name='flex'/>
       <parameter type-id='a3372543' name='sfeatures'/>
       <return type-id='48b5725f'/>
     </function-decl>
@@ -2991,6 +2998,7 @@
     <typedef-decl name='dmu_objset_stats_t' type-id='098f0221' id='b2c14f17'/>
     <enum-decl name='zfs_type_t' naming-typedef-id='2e45de5d' id='5d8f7321'>
       <underlying-type type-id='9cac1fee'/>
+      <enumerator name='ZFS_TYPE_INVALID' value='0'/>
       <enumerator name='ZFS_TYPE_FILESYSTEM' value='1'/>
       <enumerator name='ZFS_TYPE_SNAPSHOT' value='2'/>
       <enumerator name='ZFS_TYPE_VOLUME' value='4'/>
@@ -3360,13 +3368,13 @@
     </function-decl>
     <function-decl name='zfs_crypto_attempt_load_keys' mangled-name='zfs_crypto_attempt_load_keys' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='zfs_crypto_attempt_load_keys'>
       <parameter type-id='b0382bb3' name='hdl'/>
-      <parameter type-id='26a90f95' name='fsname'/>
+      <parameter type-id='80f4b756' name='fsname'/>
       <return type-id='95e97e5e'/>
     </function-decl>
     <function-decl name='zfs_crypto_load_key' mangled-name='zfs_crypto_load_key' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='zfs_crypto_load_key'>
       <parameter type-id='9200a744' name='zhp'/>
       <parameter type-id='c19b74c3' name='noop'/>
-      <parameter type-id='26a90f95' name='alt_keylocation'/>
+      <parameter type-id='80f4b756' name='alt_keylocation'/>
       <return type-id='95e97e5e'/>
     </function-decl>
     <function-decl name='zfs_crypto_unload_key' mangled-name='zfs_crypto_unload_key' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='zfs_crypto_unload_key'>
@@ -4081,9 +4089,6 @@
     </function-decl>
   </abi-instr>
   <abi-instr address-size='64' path='libzfs_mount.c' language='LANG_C99'>
-    <array-type-def dimensions='1' type-id='f1bd64e2' size-in-bits='384' id='b2c36c9f'>
-      <subrange length='2' type-id='7359adad' id='52efc4ef'/>
-    </array-type-def>
     <class-decl name='get_all_cb' size-in-bits='192' is-struct='yes' visibility='default' id='803dac95'>
       <data-member access='public' layout-offset-in-bits='0'>
         <var-decl name='cb_handles' type-id='4507922a' visibility='default'/>
@@ -4096,24 +4101,8 @@
       </data-member>
     </class-decl>
     <typedef-decl name='get_all_cb_t' type-id='803dac95' id='9b293607'/>
-    <class-decl name='proto_table_t' size-in-bits='192' is-struct='yes' naming-typedef-id='f1bd64e2' visibility='default' id='f4c8e1ed'>
-      <data-member access='public' layout-offset-in-bits='0'>
-        <var-decl name='p_prop' type-id='58603c44' visibility='default'/>
-      </data-member>
-      <data-member access='public' layout-offset-in-bits='64'>
-        <var-decl name='p_name' type-id='26a90f95' visibility='default'/>
-      </data-member>
-      <data-member access='public' layout-offset-in-bits='128'>
-        <var-decl name='p_share_err' type-id='95e97e5e' visibility='default'/>
-      </data-member>
-      <data-member access='public' layout-offset-in-bits='160'>
-        <var-decl name='p_unshare_err' type-id='95e97e5e' visibility='default'/>
-      </data-member>
-    </class-decl>
-    <typedef-decl name='proto_table_t' type-id='f4c8e1ed' id='f1bd64e2'/>
     <pointer-type-def type-id='9b293607' size-in-bits='64' id='77bf1784'/>
     <pointer-type-def type-id='9200a744' size-in-bits='64' id='4507922a'/>
-    <var-decl name='proto_table' type-id='b2c36c9f' visibility='default'/>
     <function-decl name='is_mounted' mangled-name='is_mounted' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='is_mounted'>
       <parameter type-id='b0382bb3' name='zfs_hdl'/>
       <parameter type-id='80f4b756' name='special'/>

lib/libzfs/libzfs_dataset.c
@@ -2903,6 +2903,8 @@ zfs_prop_get(zfs_handle_t *zhp, zfs_prop_t prop, char *propbuf, size_t proplen,
 		break;
 
 	case ZFS_PROP_GUID:
+	case ZFS_PROP_KEY_GUID:
+	case ZFS_PROP_IVSET_GUID:
 	case ZFS_PROP_CREATETXG:
 	case ZFS_PROP_OBJSETID:
 	case ZFS_PROP_PBKDF2_ITERS:

lib/libzfs/libzfs_pool.c
@@ -347,6 +347,8 @@ zpool_get_prop(zpool_handle_t *zhp, zpool_prop_t prop, char *buf,
 		case ZPOOL_PROP_FREEING:
 		case ZPOOL_PROP_LEAKED:
 		case ZPOOL_PROP_ASHIFT:
+		case ZPOOL_PROP_MAXBLOCKSIZE:
+		case ZPOOL_PROP_MAXDNODESIZE:
 			if (literal)
 				(void) snprintf(buf, len, "%llu",
 				    (u_longlong_t)intval);
@@ -5160,6 +5162,7 @@ zpool_get_vdev_prop_value(nvlist_t *nvprop, vdev_prop_t prop, char *prop_name,
 		case VDEV_PROP_ASIZE:
 		case VDEV_PROP_PSIZE:
 		case VDEV_PROP_SIZE:
+		case VDEV_PROP_BOOTSIZE:
 		case VDEV_PROP_ALLOCATED:
 		case VDEV_PROP_FREE:
 		case VDEV_PROP_READ_ERRORS:

commit 0ad0f6cbc4320b82d319d6cd69aa6ba6a19f2ea6

Fix string/index variables being unflexed by default
I got the status backward (B_FALSE for fixed, rather than B_TRUE for
flex); before:

  $ zfs get mountpoint tarta-zoot -r
  NAME                                 PROPERTY    VALUE       SOURCE
  tarta-zoot                           mountpoint  /           local
  tarta-zoot/PAGEFILE.SYS              mountpoint  -           -
  tarta-zoot/etc                       mountpoint  /etc        inherited from tarta-zoot
  tarta-zoot/home                      mountpoint  /home       inherited from tarta-zoot
  tarta-zoot/home/xspon                mountpoint  /home/xspon  inherited from tarta-zoot
  tarta-zoot/home/nabijaczleweli       mountpoint  /home/nabijaczleweli  inherited from tarta-zoot
  tarta-zoot/home/nabijaczleweli/tftp  mountpoint  /home/nabijaczleweli/tftp  inherited from tarta-zoot
  tarta-zoot/home/root                 mountpoint  /root       local

after:

  $ zfs get mountpoint tarta-zoot -r
  NAME                                 PROPERTY    VALUE                      SOURCE
  tarta-zoot                           mountpoint  /                          local
  tarta-zoot/PAGEFILE.SYS              mountpoint  -                          -
  tarta-zoot/etc                       mountpoint  /etc                       inherited from tarta-zoot
  tarta-zoot/home                      mountpoint  /home                      inherited from tarta-zoot
  tarta-zoot/home/xspon                mountpoint  /home/xspon                inherited from tarta-zoot
  tarta-zoot/home/nabijaczleweli       mountpoint  /home/nabijaczleweli       inherited from tarta-zoot
  tarta-zoot/home/nabijaczleweli/tftp  mountpoint  /home/nabijaczleweli/tftp  inherited from tarta-zoot
  tarta-zoot/home/root                 mountpoint  /root                      local

Fixes: ddb271d0abca1b8113558744c6d56d2aacaf4542 ("Flex
 non-pretty-printed properties and raw-/pretty-print remaining ones")

module/zcommon/zprop_common.c
@@ -136,7 +136,7 @@ zprop_register_string(int prop, const char *name, const char *def,
     const char *colname, const struct zfs_mod_supported_features *sfeatures)
 {
 	zprop_register_impl(prop, name, PROP_TYPE_STRING, 0, def, attr,
-	    objset_types, values, colname, B_FALSE, B_TRUE, B_FALSE, NULL,
+	    objset_types, values, colname, B_FALSE, B_TRUE, B_TRUE, NULL,
 	    sfeatures);
 
 }
@@ -159,7 +159,7 @@ zprop_register_index(int prop, const char *name, uint64_t def,
     const struct zfs_mod_supported_features *sfeatures)
 {
 	zprop_register_impl(prop, name, PROP_TYPE_INDEX, def, NULL, attr,
-	    objset_types, values, colname, B_FALSE, B_TRUE, B_FALSE, idx_tbl,
+	    objset_types, values, colname, B_FALSE, B_TRUE, B_TRUE, idx_tbl,
 	    sfeatures);
 }
 