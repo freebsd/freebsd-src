4bb43f35aa4fe74d28d139b674c2b3b161ee7f99
Warner Losh <imp@FreeBSD.org>
2022-04-04 20:24:21 -0600

    hdaa_eld_dump: eliminate write only variable len
    Sponsored by:		Netflix

--- sys/dev/sound/pci/hda/hdaa.c
+++ sys/dev/sound/pci/hda/hdaa.c
@@ -605,7 +605,7 @@ hdaa_eld_dump(struct hdaa_widget *w)
 	struct hdaa_devinfo *devinfo = w->devinfo;
 	device_t dev = devinfo->dev;
 	uint8_t *sad;
-	int len, mnl, i, sadc, fmt;
+	int mnl, i, sadc, fmt;
 
 	if (w->eld == NULL || w->eld_len < 4)
 		return;
@@ -614,7 +614,6 @@ hdaa_eld_dump(struct hdaa_widget *w)
 	    w->nid, w->eld[0] >> 3, w->eld[2]);
 	if ((w->eld[0] >> 3) != 0x02)
 		return;
-	len = min(w->eld_len, (u_int)w->eld[2] * 4);
 	mnl = w->eld[4] & 0x1f;
 	device_printf(dev,
 	    "ELD nid=%d: CEA_EDID_Ver=%u MNL=%u\n",
