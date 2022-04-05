d388d98b4c78f5c6bc0b82a7ba8cd1c9d2438ffa
Warner Losh <imp@FreeBSD.org>
2022-04-04 20:15:50 -0600

    sb_probe: eliminate write only variable r
    Sponsored by:		Netflix

--- sys/dev/sound/isa/sb8.c
+++ sys/dev/sound/isa/sb8.c
@@ -691,14 +691,14 @@ static int
 sb_probe(device_t dev)
 {
     	char buf[64];
-	uintptr_t func, ver, r, f;
+	uintptr_t func, ver, f;
 
 	/* The parent device has already been probed. */
-	r = BUS_READ_IVAR(device_get_parent(dev), dev, 0, &func);
+	BUS_READ_IVAR(device_get_parent(dev), dev, 0, &func);
 	if (func != SCF_PCM)
 		return (ENXIO);
 
-	r = BUS_READ_IVAR(device_get_parent(dev), dev, 1, &ver);
+	BUS_READ_IVAR(device_get_parent(dev), dev, 1, &ver);
 	f = (ver & 0xffff0000) >> 16;
 	ver &= 0x0000ffff;
 	if ((f & BD_F_ESS) || (ver >= 0x400))
