bb8e1dfbff30e5df97fa31c88f8218af054fe166
Kristof Provost <kp@FreeBSD.org>
2022-04-06 16:45:18 +0200

    pf: use ERROUT_IOCTL()
    Use ERROUT_IOCTL() rather than hand-rolling the macro. This adds DTrace
    SDTs in the error path, making debugging ioctl errors easier.
    
    MFC after:	1 week
    Sponsored by:	Rubicon Communications, LLC ("Netgate")
    
    (cherry picked from commit bef7104571f35eeffc27b25d8857a7940dd23ab8)

--- sys/netpfil/pf/pf_ioctl.c
+++ sys/netpfil/pf/pf_ioctl.c
@@ -2683,7 +2683,7 @@ DIOCGETRULENV_error:
 			newrule->cpid = td->td_proc ? td->td_proc->p_pid : 0;
 			TAILQ_INIT(&newrule->rpool.list);
 		}
-#define	ERROUT(x)	{ error = (x); goto DIOCCHANGERULE_error; }
+#define	ERROUT(x)	ERROUT_IOCTL(DIOCCHANGERULE_error, x)
 
 		PF_RULES_WLOCK();
 #ifdef PF_WANT_32_TO_64_COUNTER
