ee28ad11b7635715ed7d2f5815ace34c433992d1
John Baldwin <jhb@FreeBSD.org>
2022-04-06 16:45:28 -0700

    iavf: Quite set but not used warnings.
    Move declaration of dev in iavf_if_attach_post under #ifdef IXL_DEBUG.
    
    Remove unused ifp in iavf_stop.

--- sys/dev/iavf/if_iavf_iflib.c
+++ sys/dev/iavf/if_iavf_iflib.c
@@ -463,13 +463,14 @@ iavf_setup_vc_tq(struct iavf_sc *sc)
 static int
 iavf_if_attach_post(if_ctx_t ctx)
 {
-	device_t dev;
+#ifdef IXL_DEBUG
+	device_t dev = iflib_get_dev(ctx);
+#endif
 	struct iavf_sc	*sc;
 	struct iavf_hw	*hw;
 	struct iavf_vsi *vsi;
 	int error = 0;
 
-	dev = iflib_get_dev(ctx);
 	INIT_DBG_DEV(dev, "begin");
 
 	sc = iavf_sc_from_ctx(ctx);
@@ -1767,10 +1768,6 @@ iavf_update_link_status(struct iavf_sc *sc)
 static void
 iavf_stop(struct iavf_sc *sc)
 {
-	struct ifnet *ifp;
-
-	ifp = sc->vsi.ifp;
-
 	iavf_clear_state(&sc->state, IAVF_STATE_RUNNING);
 
 	iavf_disable_intr(&sc->vsi);
