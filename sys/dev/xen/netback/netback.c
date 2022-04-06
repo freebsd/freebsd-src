7650d4b67e53d3916313eb23d618cac50fdec9cc
John Baldwin <jhb@FreeBSD.org>
2022-04-06 16:45:27 -0700

    xen netback: Remove write-only variables.

--- sys/dev/xen/netback/netback.c
+++ sys/dev/xen/netback/netback.c
@@ -2393,7 +2393,7 @@ xnb_send(netif_rx_back_ring_t *ring, domid_t otherend, const struct mbuf *mbufc,
 	 gnttab_copy_table gnttab)
 {
 	struct xnb_pkt pkt;
-	int error, n_entries, n_reqs;
+	int error, n_entries;
 	RING_IDX space;
 
 	space = ring->sring->req_prod - ring->req_cons;
@@ -2408,7 +2408,7 @@ xnb_send(netif_rx_back_ring_t *ring, domid_t otherend, const struct mbuf *mbufc,
 		    hv_ret));
 	}
 
-	n_reqs = xnb_rxpkt2rsp(&pkt, gnttab, n_entries, ring);
+	xnb_rxpkt2rsp(&pkt, gnttab, n_entries, ring);
 
 	return 0;
 }
