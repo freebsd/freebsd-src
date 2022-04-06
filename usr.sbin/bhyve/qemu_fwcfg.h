a02d136df0bc48e7d33e43c4f754e58ec63f5629
Corvin Köhne <corvink@FreeBSD.org>
2022-04-06 11:10:37 +0200

    bhyve: make use of qemus acpi table loader
    Add all acpi tables to qemus acpi table loader. This passes the acpi
    tables by fwcfg to the guest.
    
    Reviewed by:		markj
    MFC after:		1 week
    Sponsored by:		Beckhoff Automation GmbH & Co. KG
    Differential Revision:	https://reviews.freebsd.org/D38439
    
    (cherry picked from commit 7959d80d99ae06ba06cbe7a206ef9bc513e4109b)

--- usr.sbin/bhyve/qemu_fwcfg.h
+++ usr.sbin/bhyve/qemu_fwcfg.h
@@ -13,6 +13,8 @@
 #define QEMU_FWCFG_MAX_ENTRIES 0x4000
 #define QEMU_FWCFG_MAX_NAME 56
 
+#define QEMU_FWCFG_FILE_TABLE_LOADER "etc/table-loader"
+
 struct qemu_fwcfg_item {
 	uint32_t size;
 	uint8_t *data;
