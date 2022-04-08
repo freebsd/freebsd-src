a11819ca1f9b7b0352aef023ca0cfb52cfb8639b
John Baldwin <jhb@FreeBSD.org>
2022-04-07 17:01:27 -0700

    ng_lmi: Move all of highest_dlci under #if 0.

--- sys/netgraph/ng_lmi.c
+++ sys/netgraph/ng_lmi.c
@@ -742,7 +742,9 @@ nglmi_checkdata(hook_p hook, struct mbuf *m)
 	u_char  nextbyte;
 	int     seq_seen = 0;
 	int     resptype_seen = 0;	/* 0 , 1 (partial) or 2 (full) */
+#if 0
 	int     highest_dlci = 0;
+#endif
 
 	packetlen = m->m_len;
 	data = mtod(m, const u_char *);
@@ -959,7 +961,9 @@ nglmi_checkdata(hook_p hook, struct mbuf *m)
 				log(LOG_WARNING, "nglmi: DLCI out of range\n");
 				goto reject;
 			}
+#if 0
 			highest_dlci = dlci;
+#endif
 			break;
 		default:
 			log(LOG_WARNING,
