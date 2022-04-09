61e8d2982c27e6868287b547026adcc177276890
Gordon Bergling <gbe@FreeBSD.org>
2022-04-09 14:19:17 +0200

    jail: Remove a double word in a source code comment
    - s/a a/a/
    
    MFC after:	3 days

--- sys/kern/kern_jail.c
+++ sys/kern/kern_jail.c
@@ -2844,7 +2844,7 @@ prison_free_not_last(struct prison *pr)
 }
 
 /*
- * Hold a a prison for user visibility, by incrementing pr_uref.
+ * Hold a prison for user visibility, by incrementing pr_uref.
  * It is generally an error to hold a prison that isn't already
  * user-visible, except through the the jail system calls.  It is also
  * an error to hold an invalid prison.  A prison record will remain
