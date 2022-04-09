768f9b8b8bf7ca2f0b45bf6d7fde1a38f800c68d
Gordon Bergling <gbe@FreeBSD.org>
2022-04-09 08:53:17 +0200

    kern: Fix a typo in a source code comment
    - s/is is/is/
    
    MFC after:	3 days

--- sys/kern/kern_prot.c
+++ sys/kern/kern_prot.c
@@ -1761,7 +1761,7 @@ p_candebug(struct thread *td, struct proc *p)
 	if ((p->p_flag & P_INEXEC) != 0)
 		return (EBUSY);
 
-	/* Denied explicitely */
+	/* Denied explicitly */
 	if ((p->p_flag2 & P2_NOTRACE) != 0) {
 		error = priv_check(td, PRIV_DEBUG_DENIED);
 		if (error != 0)
