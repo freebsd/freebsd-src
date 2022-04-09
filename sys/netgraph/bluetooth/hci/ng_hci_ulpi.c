77a44875e59b239c696cca17b4cc946105ade612
Gordon Bergling <gbe@FreeBSD.org>
2022-04-09 11:40:14 +0200

    netgraph(4): Fix a typo in a source code comment
    - s/peform/perform/
    
    MFC after:	3 days

--- sys/netgraph/bluetooth/hci/ng_hci_ulpi.c
+++ sys/netgraph/bluetooth/hci/ng_hci_ulpi.c
@@ -1012,9 +1012,9 @@ ng_hci_lp_con_rsp(ng_hci_unit_p unit, item_p item, hook_p hook)
 				sizeof(req->cp.acc.bdaddr));
 
 			/*
-			 * We are accepting connection, so if we support role 
-			 * switch and role switch was enabled then set role to 
-			 * NG_HCI_ROLE_MASTER and let LM peform role switch.
+			 * We are accepting connection, so if we support role
+			 * switch and role switch was enabled then set role to
+			 * NG_HCI_ROLE_MASTER and let LM perform role switch.
 			 * Otherwise we remain slave. In this case LM WILL NOT
 			 * perform role switch.
 			 */
@@ -1025,8 +1025,8 @@ ng_hci_lp_con_rsp(ng_hci_unit_p unit, item_p item, hook_p hook)
 			else
 				req->cp.acc.role = NG_HCI_ROLE_SLAVE;
 
-			/* 
-			 * Adjust connection state 
+			/*
+			 * Adjust connection state
 			 */
 
 			if (hook == unit->acl)
