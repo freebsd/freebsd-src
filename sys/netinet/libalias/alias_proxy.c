3fe7a8a5f566da4b4e580eced222ecef064db3a3
Gordon Bergling <gbe@FreeBSD.org>
2022-04-09 08:54:56 +0200

    libalias(3): Fix two typos in source code comments
    - s/modfied/modified/
    
    MFC after:	3 days

--- sys/netinet/libalias/alias_proxy.c
+++ sys/netinet/libalias/alias_proxy.c
@@ -341,7 +341,7 @@ ProxyEncodeTcpStream(struct alias_link *lnk,
 		memcpy(p, buffer, slen);
 	}
 
-	/* Save information about modfied sequence number */
+	/* Save information about modified sequence number */
 	{
 		int delta;
 
