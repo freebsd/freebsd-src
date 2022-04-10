freebsd/freebsd-src

commit 5241577a223d805282f726cb443a566aca653b9f

vmm: fix set but not used warning

sys/amd64/vmm/vmm_instruction_emul.c
@@ -717,7 +717,7 @@ get_gla(void *vm, int vcpuid, struct vie *vie, struct vm_guest_paging *paging,
 {
 	struct seg_desc desc;
 	uint64_t cr0, val, rflags;
-	int error;
+	int error __diagused;
 
 	error = vie_read_register(vm, vcpuid, VM_REG_GUEST_CR0, &cr0);
 	KASSERT(error == 0, ("%s: error %d getting cr0", __func__, error));

commit d4e8207317ca1827ba2529203d5cc4e67d836fcb

vmm_instruction_emul.c: fix bhyve build
The __diagused macro was used to cure a "set but not used" warning. This
broke the build for bhyve since __diagused is only defined in the
kernel. Define __diagused when not building the kernel.

Fixes:          5241577a223d ("vmm: fix set but not used warning")
Reported by:    Jenkins

sys/amd64/vmm/vmm_instruction_emul.c
@@ -58,6 +58,7 @@ __FBSDID("$FreeBSD$");
 #include <string.h>
 #include <strings.h>
 #include <vmmapi.h>
+#define	__diagused
 #define	KASSERT(exp,msg)	assert((exp))
 #define	panic(...)		errx(4, __VA_ARGS__)
 #endif	/* _KERNEL */