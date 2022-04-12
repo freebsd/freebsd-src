b8717a8709b297a7bdbcfae29e384cda9ce9c89a
John Baldwin <jhb@FreeBSD.org>
2022-04-12 10:06:00 -0700

    Remove checks for __GNUCLIKE macros for varargs.
    All supported compilers (modern versions of GCC and clang) support
    these.
    
    PR:		263102 (exp-run)
    Reviewed by:	brooks, imp
    Differential Revision:	https://reviews.freebsd.org/D34800

--- sys/sys/_stdarg.h
+++ sys/sys/_stdarg.h
@@ -41,7 +41,6 @@
   typedef __va_list       va_list;
 #endif
 
-#ifdef __GNUCLIKE_BUILTIN_STDARG
   #define	va_start(ap, last)	__builtin_va_start((ap), (last))
   #define	va_arg(ap, type)	__builtin_va_arg((ap), type)
   #define	__va_copy(dest, src)	__builtin_va_copy((dest), (src))
@@ -49,6 +48,5 @@
     #define	va_copy(dest, src)	__va_copy(dest, src)
   #endif
   #define	va_end(ap)		__builtin_va_end(ap)
-#endif
 
 #endif /* ! _SYS__STDARG_H_ */
