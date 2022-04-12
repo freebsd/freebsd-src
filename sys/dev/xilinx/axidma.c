3267e2f638344eab4a9f8ade6af4e307e3012e2a
John Baldwin <jhb@FreeBSD.org>
2022-04-12 14:58:57 -0700

    axidma: Remove unused variables.

--- sys/dev/xilinx/axidma.c
+++ sys/dev/xilinx/axidma.c
@@ -318,9 +318,7 @@ static int
 axidma_desc_free(struct axidma_softc *sc, struct axidma_channel *chan)
 {
 	struct xdma_channel *xchan;
-	int nsegments;
 
-	nsegments = chan->descs_num;
 	xchan = chan->xchan;
 
 	free(chan->descs, M_DEVBUF);
@@ -463,7 +461,6 @@ axidma_channel_submit_sg(device_t dev, struct xdma_channel *xchan,
 	uint32_t len;
 	uint32_t tmp;
 	int i;
-	int tail;
 
 	dprintf("%s: sg_n %d\n", __func__, sg_n);
 
@@ -475,8 +472,6 @@ axidma_channel_submit_sg(device_t dev, struct xdma_channel *xchan,
 
 	if (sg_n == 0)
 		return (0);
-
-	tail = chan->idx_head;
 
 	tmp = 0;
 
@@ -577,12 +572,6 @@ axidma_channel_prep_sg(device_t dev, struct xdma_channel *xchan)
 static int
 axidma_channel_control(device_t dev, xdma_channel_t *xchan, int cmd)
 {
-	struct axidma_channel *chan;
-	struct axidma_softc *sc;
-
-	sc = device_get_softc(dev);
-
-	chan = (struct axidma_channel *)xchan->chan;
 
 	switch (cmd) {
 	case XDMA_CMD_BEGIN:
