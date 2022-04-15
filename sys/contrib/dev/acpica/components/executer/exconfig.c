11d64c1d53b0376acf2e746e2744dd5f1a2fa492
Jung-uk Kim <jkim@FreeBSD.org>
2022-04-15 16:29:09 -0400

    acpica: Drop a local patch in favor of an upstream patch
    Use ACPI_ERROR() instead of fprintf().
    
    https://github.com/acpica/acpica/pull/768

--- sys/contrib/dev/acpica/components/executer/exconfig.c
+++ sys/contrib/dev/acpica/components/executer/exconfig.c
@@ -471,9 +471,7 @@ AcpiExLoadOp (
     }
     if (Target->Common.Type != ACPI_TYPE_INTEGER)
     {
-#ifndef _KERNEL
-        fprintf (stderr, "Type not integer: %X\n", Target->Common.Type);
-#endif
+        ACPI_ERROR ((AE_INFO, "Type not integer: %X", Target->Common.Type));
         return_ACPI_STATUS (AE_AML_OPERAND_TYPE);
     }
 
