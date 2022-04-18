f331cf9b043a7e5ad4fd22ed9908c5f59da744d4
Piotr Pawel Stefaniak <pstef@FreeBSD.org>
2022-04-18 13:51:26 +0200

    bintrans: reinstate a test case that I lost
    Reported by:	https://ci.freebsd.org/job/FreeBSD-main-amd64-test/21166

--- usr.bin/bintrans/tests/regress.sh
+++ usr.bin/bintrans/tests/regress.sh
@@ -9,4 +9,7 @@ REGRESSION_TEST(`base64', `uuencode -m regress.in <${SRCDIR}/regress.in')
 REGRESSION_TEST_ONE(`uudecode -p <${SRCDIR}/regress.traditional.in', `traditional')
 REGRESSION_TEST_ONE(`uudecode -p <${SRCDIR}/regress.base64.in', `base64')
 
+# was uudecode: stdin: /dev/null: character out of range: [33-96]
+REGRESSION_TEST(`153276', `uudecode -o /dev/null <${SRCDIR}/regress.153276.in 2>&1')
+
 REGRESSION_END()
