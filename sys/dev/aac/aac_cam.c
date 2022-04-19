5a2ae6d050b6cab6fa9e9e3a40abfeac2a34f26d
John Baldwin <jhb@FreeBSD.org>
2022-04-19 11:24:47 -0700

    aac: Remove unused devclass argument to DRIVER_MODULE.

--- sys/dev/aac/aac_cam.c
+++ sys/dev/aac/aac_cam.c
@@ -88,8 +88,6 @@ static u_int32_t aac_cam_reset_bus(struct cam_sim *, union ccb *);
 static u_int32_t aac_cam_abort_ccb(struct cam_sim *, union ccb *);
 static u_int32_t aac_cam_term_io(struct cam_sim *, union ccb *);
 
-static devclass_t	aac_pass_devclass;
-
 static device_method_t	aac_pass_methods[] = {
 	DEVMETHOD(device_probe,		aac_cam_probe),
 	DEVMETHOD(device_attach,	aac_cam_attach),
@@ -103,7 +101,7 @@ static driver_t	aac_pass_driver = {
 	sizeof(struct aac_cam)
 };
 
-DRIVER_MODULE(aacp, aac, aac_pass_driver, aac_pass_devclass, NULL, NULL);
+DRIVER_MODULE(aacp, aac, aac_pass_driver, NULL, NULL);
 MODULE_DEPEND(aacp, cam, 1, 1, 1);
 
 static MALLOC_DEFINE(M_AACCAM, "aaccam", "AAC CAM info");
