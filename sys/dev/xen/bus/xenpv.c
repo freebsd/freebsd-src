dd02929db16d2ceb8084e644407eb6eab484dbb7
John Baldwin <jhb@FreeBSD.org>
2022-04-25 11:55:30 -0700

    xen: Use devclass_find to lookup devclasses in identify.
    While here, use driver->name instead of hardcoding the xenpv and
    xen_et strings both for devclass_find() and BUS_ADD_CHILD().
    
    Reviewed by:	Elliott Mitchell <ehem+freebsd@m5p.com>, imp, royger
    Differential Revision:	https://reviews.freebsd.org/D35001

--- sys/dev/xen/bus/xenpv.c
+++ sys/dev/xen/bus/xenpv.c
@@ -77,14 +77,14 @@ xenpv_identify(driver_t *driver, device_t parent)
 		return;
 
 	/* Make sure there's only one xenpv device. */
-	if (devclass_get_device(xenpv_devclass, 0))
+	if (devclass_get_device(devclass_find(driver->name), 0))
 		return;
 
 	/*
 	 * The xenpv bus should be the last to attach in order
 	 * to properly detect if an ISA bus has already been added.
 	 */
-	if (BUS_ADD_CHILD(parent, UINT_MAX, "xenpv", 0) == NULL)
+	if (BUS_ADD_CHILD(parent, UINT_MAX, driver->name, 0) == NULL)
 		panic("Unable to attach xenpv bus.");
 }
 
