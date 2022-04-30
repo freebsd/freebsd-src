freebsd/freebsd-src

commit c3e412c083332764a680da896e5d291821cc98bc

sbin/devfs: clarify usage
- Note existence of -m option.
- Note that -s applies to rule keyword, only, by adding usage text
  specifically for the `rule` and `ruleset` keywords.

Don't go into any further detail in usage(..) -- it's best that one
reads the manpage to get a better idea of how things work as there are
a number of different option-specific keywords and arguments, as well
as some rule grammar.

MFC after:	1 week
Sponsored by:	Dell EMC Isilon

sbin/devfs/devfs.c
@@ -225,6 +225,8 @@ void
 usage(void)
 {
 
-	fprintf(stderr, "usage: devfs rule|ruleset arguments\n");
+	fprintf(stderr, "usage: %s\n%s\n",
+	    "\tdevfs [-m mount-point] [-s ruleset] rule ...",
+	    "\tdevfs [-m mount-point] ruleset ...");
 	exit(1);
 }

commit 36e62d9c1bac6ed969418e925e6bcc924698ee12

sbin/devfs: Correct usage
The -s applies to rule keyword only and it follows the rule keyword.

MFC after:	1 week
PR: [[https://bugs.freebsd.org/bugzilla/show_bug.cgi?id= 263289|263289]]
Submitted by: Yuichiro NAITO <naito.yuichiro@gmail.com>
Fixes: c3e412c08333 sbin/devfs: clarify usage
Reviewed by: ngie
Differential Revision: https://reviews.freebsd.org/D34934

sbin/devfs/devfs.c
@@ -228,7 +228,7 @@ usage(void)
 {
 
 	fprintf(stderr, "usage: %s\n%s\n",
-	    "\tdevfs [-m mount-point] [-s ruleset] rule ...",
+	    "\tdevfs [-m mount-point] rule [-s ruleset] ...",
 	    "\tdevfs [-m mount-point] ruleset ...");
 	exit(1);
 }