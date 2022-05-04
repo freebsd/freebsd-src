dd8646dc6333988fb693dd3c5f1e037b824c7fd6
Alan Somers <asomers@FreeBSD.org>
2022-05-04 17:36:17 -0600

    fusefs: handle evil servers that return illegal inode numbers
    * If during FUSE_CREATE, FUSE_MKDIR, etc the server returns the same
      inode number for the new file as for its parent directory, reject it.
      Previously this would triggers a recurse-on-non-recursive lock panic.
    
    * If during FUSE_LINK the server returns a different inode number for
      the new name as for the old one, reject it.  Obviously, that can't be
      a hard link.
    
    * If during FUSE_LOOKUP the server returns the same inode number for the
      new file as for its parent directory, reject it.  Nothing good can
      come of this.
    
    PR:		263662
    Reported by:	Robert Morris <rtm@lcs.mit.edu>
    MFC after:	2 weeks
    Reviewed by:	pfg
    Differential Revision: https://reviews.freebsd.org/D35128

--- sys/fs/fuse/fuse_ipc.h
+++ sys/fs/fuse/fuse_ipc.h
@@ -240,6 +240,7 @@ struct fuse_data {
 #define FSESS_WARN_LSEXTATTR_LONG 0x100000 /* Returned too many extattrs */
 #define FSESS_WARN_CACHE_INCOHERENT 0x200000	/* Read cache incoherent */
 #define FSESS_WARN_WB_CACHE_INCOHERENT 0x400000	/* WB cache incoherent */
+#define	FSESS_WARN_ILLEGAL_INODE  0x800000 /* Illegal inode for new file */
 #define FSESS_MNTOPTS_MASK	( \
 	FSESS_DAEMON_CAN_SPY | FSESS_PUSH_SYMLINKS_IN | \
 	FSESS_DEFAULT_PERMISSIONS | FSESS_INTR)
--- tests/sys/fs/fusefs/symlink.cc
+++ tests/sys/fs/fusefs/symlink.cc
@@ -165,6 +165,28 @@ TEST_F(Symlink, ok)
 	EXPECT_EQ(0, symlink(dst, FULLPATH)) << strerror(errno);
 }
 
+/*
+ * Nothing bad should happen if the server returns the parent's inode number
+ * for the newly created symlink.  Regression test for bug 263662.
+ * https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=263662
+ */
+TEST_F(Symlink, parent_ino)
+{
+	const char FULLPATH[] = "mountpoint/parent/src";
+	const char PPATH[] = "parent";
+	const char RELPATH[] = "src";
+	const char dst[] = "dst";
+	const uint64_t ino = 42;
+
+	expect_lookup(PPATH, ino, S_IFDIR | 0755, 0, 1);
+	EXPECT_LOOKUP(ino, RELPATH)
+	.WillOnce(Invoke(ReturnErrno(ENOENT)));
+	expect_symlink(ino, dst, RELPATH);
+
+	EXPECT_EQ(-1, symlink(dst, FULLPATH));
+	EXPECT_EQ(EIO, errno);
+}
+
 TEST_F(Symlink_7_8, ok)
 {
 	const char FULLPATH[] = "mountpoint/src";
