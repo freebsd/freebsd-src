d2f9be1c891651e77cf99884f942cdd27b43fd53
Alan Somers <asomers@FreeBSD.org>
2022-05-05 15:35:23 -0600

    Correctly measure system load averages > 1024
    The old fixed-point arithmetic used for calculating load averages had an
    overflow at 1024.  So on systems with extremely high load, the observed
    load average would actually fall back to 0 and shoot up again, creating
    a kind of sawtooth graph.
    
    Fix this by using 64-bit math internally, while still reporting the load
    average to userspace as a 32-bit number.
    
    Sponsored by:	Axcient
    Reviewed by:	imp
    Differential Revision: https://reviews.freebsd.org/D35134

--- sys/kern/kern_synch.c
+++ sys/kern/kern_synch.c
@@ -87,7 +87,7 @@ struct loadavg averunnable =
  * Constants for averages over 1, 5, and 15 minutes
  * when sampling at 5 second intervals.
  */
-static fixpt_t cexp[3] = {
+static uint64_t cexp[3] = {
 	0.9200444146293232 * FSCALE,	/* exp(-1/12) */
 	0.9834714538216174 * FSCALE,	/* exp(-1/60) */
 	0.9944598480048967 * FSCALE,	/* exp(-1/180) */
@@ -611,14 +611,15 @@ setrunnable(struct thread *td, int srqflags)
 static void
 loadav(void *arg)
 {
-	int i, nrun;
+	int i;
+	uint64_t nrun;
 	struct loadavg *avg;
 
-	nrun = sched_load();
+	nrun = (uint64_t)sched_load();
 	avg = &averunnable;
 
 	for (i = 0; i < 3; i++)
-		avg->ldavg[i] = (cexp[i] * avg->ldavg[i] +
+		avg->ldavg[i] = (cexp[i] * (uint64_t)avg->ldavg[i] +
 		    nrun * FSCALE * (FSCALE - cexp[i])) >> FSHIFT;
 
 	/*
