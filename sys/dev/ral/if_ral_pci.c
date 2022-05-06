aed8cdc8cd9a810e67d9374d1a18cb27771ef2cc
John Baldwin <jhb@FreeBSD.org>
2022-05-06 15:46:55 -0700

    ral: Remove unused devclass argument to DRIVER_MODULE.

--- sys/dev/ral/if_ral_pci.c
+++ sys/dev/ral/if_ral_pci.c
@@ -176,9 +176,7 @@ static driver_t ral_pci_driver = {
 	sizeof (struct ral_pci_softc)
 };
 
-static devclass_t ral_devclass;
-
-DRIVER_MODULE(ral, pci, ral_pci_driver, ral_devclass, NULL, NULL);
+DRIVER_MODULE(ral, pci, ral_pci_driver, NULL, NULL);
 MODULE_PNP_INFO("U16:vendor;U16:device;D:#", pci, ral, ral_pci_ids,
     nitems(ral_pci_ids) - 1);
 
