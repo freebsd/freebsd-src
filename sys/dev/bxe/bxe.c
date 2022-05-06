694299ac5d1dbb7eeb3274c84bb2b14e1817e8af
John Baldwin <jhb@FreeBSD.org>
2022-05-06 15:46:58 -0700

    bxe: Remove unused devclass argument to DRIVER_MODULE.

--- sys/dev/bxe/bxe.c
+++ sys/dev/bxe/bxe.c
@@ -219,15 +219,9 @@ static driver_t bxe_driver = {
     sizeof(struct bxe_softc) /* extra data */
 };
 
-/*
- * FreeBSD dev class is needed to manage dev instances and
- * to associate with a bus type
- */
-static devclass_t bxe_devclass;
-
 MODULE_DEPEND(bxe, pci, 1, 1, 1);
 MODULE_DEPEND(bxe, ether, 1, 1, 1);
-DRIVER_MODULE(bxe, pci, bxe_driver, bxe_devclass, 0, 0);
+DRIVER_MODULE(bxe, pci, bxe_driver, 0, 0);
 
 DEBUGNET_DEFINE(bxe);
 
