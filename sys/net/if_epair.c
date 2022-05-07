cbbce42345c51536d7dc63643f47a3cbf87d647e
Kristof Provost <kp@FreeBSD.org>
2022-05-07 17:15:34 +0200

    epair: unbind prior to returning to userspace
    If 'options RSS' is set we bind the epair tasks to different CPUs. We
    must take care to not keep the current thread bound to the last CPU when
    we return to userspace.
    
    MFC after:	1 week
    Sponsored by:	Orange Business Services

--- sys/net/if_epair.c
+++ sys/net/if_epair.c
@@ -826,6 +826,9 @@ epair_mod_init(void)
 
 		epair_tasks.tasks++;
 	}
+	thread_lock(curthread);
+	sched_unbind(curthread);
+	thread_unlock(curthread);
 #else
 	snprintf(name, sizeof(name), "epair_task");
 
