607beb88af0c83b2dd559fd3d683fc4a605ea549
Gleb Smirnoff <glebius@FreeBSD.org>
2022-05-09 10:42:48 -0700

    bluetooth: don't assign NULL in C99 sparse initializer

--- sys/netgraph/bluetooth/socket/ng_btsocket.c
+++ sys/netgraph/bluetooth/socket/ng_btsocket.c
@@ -78,7 +78,6 @@ static struct pr_usrreqs	ng_btsocket_hci_raw_usrreqs = {
 	.pru_disconnect =	ng_btsocket_hci_raw_disconnect,
 	.pru_peeraddr =		ng_btsocket_hci_raw_peeraddr,
 	.pru_send =		ng_btsocket_hci_raw_send,
-	.pru_shutdown =		NULL,
 	.pru_sockaddr =		ng_btsocket_hci_raw_sockaddr,
 	.pru_close =		ng_btsocket_hci_raw_close,
 };
@@ -97,7 +96,6 @@ static struct pr_usrreqs	ng_btsocket_l2cap_raw_usrreqs = {
 	.pru_disconnect =	ng_btsocket_l2cap_raw_disconnect,
 	.pru_peeraddr =		ng_btsocket_l2cap_raw_peeraddr,
 	.pru_send =		ng_btsocket_l2cap_raw_send,
-	.pru_shutdown =		NULL,
 	.pru_sockaddr =		ng_btsocket_l2cap_raw_sockaddr,
 	.pru_close =		ng_btsocket_l2cap_raw_close,
 };
@@ -118,7 +116,6 @@ static struct pr_usrreqs	ng_btsocket_l2cap_usrreqs = {
         .pru_listen =		ng_btsocket_l2cap_listen,
 	.pru_peeraddr =		ng_btsocket_l2cap_peeraddr,
 	.pru_send =		ng_btsocket_l2cap_send,
-	.pru_shutdown =		NULL,
 	.pru_sockaddr =		ng_btsocket_l2cap_sockaddr,
 	.pru_close =		ng_btsocket_l2cap_close,
 };
@@ -139,7 +136,6 @@ static struct pr_usrreqs	ng_btsocket_rfcomm_usrreqs = {
         .pru_listen =		ng_btsocket_rfcomm_listen,
 	.pru_peeraddr =		ng_btsocket_rfcomm_peeraddr,
 	.pru_send =		ng_btsocket_rfcomm_send,
-	.pru_shutdown =		NULL,
 	.pru_sockaddr =		ng_btsocket_rfcomm_sockaddr,
 	.pru_close =		ng_btsocket_rfcomm_close,
 };
@@ -160,7 +156,6 @@ static struct pr_usrreqs	ng_btsocket_sco_usrreqs = {
 	.pru_listen =		ng_btsocket_sco_listen,
 	.pru_peeraddr =		ng_btsocket_sco_peeraddr,
 	.pru_send =		ng_btsocket_sco_send,
-	.pru_shutdown =		NULL,
 	.pru_sockaddr =		ng_btsocket_sco_sockaddr,
 	.pru_close =		ng_btsocket_sco_close,
 };
