30f887593040120536fd12e2cedce30926af0cb6
John Baldwin <jhb@FreeBSD.org>
2022-05-10 10:21:39 -0700

    drm2: Remove unused drm_devclass.

--- sys/dev/drm2/drm_os_freebsd.c
+++ sys/dev/drm2/drm_os_freebsd.c
@@ -7,8 +7,6 @@ __FBSDID("$FreeBSD$");
 #include <dev/agp/agpreg.h>
 #include <dev/pci/pcireg.h>
 
-devclass_t drm_devclass;
-
 MALLOC_DEFINE(DRM_MEM_DMA, "drm_dma", "DRM DMA Data Structures");
 MALLOC_DEFINE(DRM_MEM_SAREA, "drm_sarea", "DRM SAREA Data Structures");
 MALLOC_DEFINE(DRM_MEM_DRIVER, "drm_driver", "DRM DRIVER Data Structures");
