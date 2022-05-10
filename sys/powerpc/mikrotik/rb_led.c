54b3d1efbf3c04bf92ec22b744849bec4a820751
John Baldwin <jhb@FreeBSD.org>
2022-05-10 10:21:38 -0700

    rbled: Remove unused devclass argument to DRIVER_MODULE.

--- sys/powerpc/mikrotik/rb_led.c
+++ sys/powerpc/mikrotik/rb_led.c
@@ -67,9 +67,7 @@ static driver_t rbled_driver = {
 	sizeof(struct rbled_softc),
 };
 
-static devclass_t rbled_devclass;
-
-DRIVER_MODULE(rbled, simplebus, rbled_driver, rbled_devclass, 0, 0);
+DRIVER_MODULE(rbled, simplebus, rbled_driver, 0, 0);
 
 static int
 rbled_probe(device_t dev)
