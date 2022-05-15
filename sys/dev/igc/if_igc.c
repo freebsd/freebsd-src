53875d68e336401d8e357b2bf1130e5c685da341
Kevin Bowling <kbowling@FreeBSD.org>
2022-05-15 15:07:59 -0700

    igc: Update PCI IDs
    bf0aa72f:
    I226-K PCI ID got clarified by intel. Add a new I226 ID while here.
    
    Approved by:	grehan
    Differential Revision:	https://reviews.freebsd.org/D35218
    
    48a1a6be:
    igc: Fix typo in PCI ID define usage
    
    Reported by:	jenkins
    Fixes:	bf0aa72f1f59 igc: Update PCI IDs
    
    (cherry picked from commit bf0aa72f1f5911912817a2d3feb4010c8239512c)
    (cherry picked from commit 48a1a6be1968e600235afd0f2b78bf633c065430)

--- sys/dev/igc/if_igc.c
+++ sys/dev/igc/if_igc.c
@@ -59,6 +59,7 @@ static pci_vendor_info_t igc_vendor_info_array[] =
 	PVID(0x8086, IGC_DEV_ID_I225_K2, "Intel(R) Ethernet Controller I225-K(2)"),
 	PVID(0x8086, IGC_DEV_ID_I225_LMVP, "Intel(R) Ethernet Controller I225-LMvP(2)"),
 	PVID(0x8086, IGC_DEV_ID_I226_K, "Intel(R) Ethernet Controller I226-K"),
+	PVID(0x8086, IGC_DEV_ID_I226_LMVP, "Intel(R) Ethernet Controller I226-LMvP"),
 	PVID(0x8086, IGC_DEV_ID_I225_IT, "Intel(R) Ethernet Controller I225-IT(2)"),
 	PVID(0x8086, IGC_DEV_ID_I226_LM, "Intel(R) Ethernet Controller I226-LM"),
 	PVID(0x8086, IGC_DEV_ID_I226_V, "Intel(R) Ethernet Controller I226-V"),
