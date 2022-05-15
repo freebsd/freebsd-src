d405589e6bce754b9bc447e4b2d6dc9dbfef946f
Dmitry Chagin <dchagin@FreeBSD.org>
2022-05-15 21:03:01 +0300

    linux(4): Move sigframe definitions to separate headers
    The signal trampoine-related definitions are used only in the MD part
    of code, wherefore moved from everywhere used linux.h to separate MD
    headers.
    
    MFC after:		2 weeks

--- sys/i386/linux/linux.h
+++ sys/i386/linux/linux.h
@@ -218,8 +218,6 @@ struct l_statfs64 {
 	l_int		f_spare[4];
 };
 
-#define	LINUX_NSIG_WORDS	2
-
 /* sigaction flags */
 #define	LINUX_SA_NOCLDSTOP	0x00000001
 #define	LINUX_SA_NOCLDWAIT	0x00000002
@@ -261,40 +259,6 @@ typedef struct {
 	l_int		ss_flags;
 	l_size_t	ss_size;
 } l_stack_t;
-
-/* The Linux sigcontext, pretty much a standard 386 trapframe. */
-struct l_sigcontext {
-	l_int		sc_gs;
-	l_int		sc_fs;
-	l_int		sc_es;
-	l_int		sc_ds;
-	l_int		sc_edi;
-	l_int		sc_esi;
-	l_int		sc_ebp;
-	l_int		sc_esp;
-	l_int		sc_ebx;
-	l_int		sc_edx;
-	l_int		sc_ecx;
-	l_int		sc_eax;
-	l_int		sc_trapno;
-	l_int		sc_err;
-	l_int		sc_eip;
-	l_int		sc_cs;
-	l_int		sc_eflags;
-	l_int		sc_esp_at_signal;
-	l_int		sc_ss;
-	l_int		sc_387;
-	l_int		sc_mask;
-	l_int		sc_cr2;
-};
-
-struct l_ucontext {
-	l_ulong		uc_flags;
-	void		*uc_link;
-	l_stack_t	uc_stack;
-	struct l_sigcontext	uc_mcontext;
-	l_sigset_t	uc_sigmask;
-};
 
 #define	LINUX_SI_MAX_SIZE	128
 #define	LINUX_SI_PAD_SIZE	((LINUX_SI_MAX_SIZE/sizeof(l_int)) - 3)
@@ -363,62 +327,6 @@ typedef struct l_siginfo {
 #define	lsi_addr	_sifields._sigfault._addr
 #define	lsi_band	_sifields._sigpoll._band
 #define	lsi_fd		_sifields._sigpoll._fd
-
-struct l_fpreg {
-	u_int16_t	significand[4];
-	u_int16_t	exponent;
-};
-
-struct l_fpxreg {
-	u_int16_t	significand[4];
-	u_int16_t	exponent;
-	u_int16_t	padding[3];
-};
-
-struct l_xmmreg {
-	u_int32_t	element[4];
-};
-
-struct l_fpstate {
-	/* Regular FPU environment */
-	u_int32_t		cw;
-	u_int32_t		sw;
-	u_int32_t		tag;
-	u_int32_t		ipoff;
-	u_int32_t		cssel;
-	u_int32_t		dataoff;
-	u_int32_t		datasel;
-	struct l_fpreg		_st[8];
-	u_int16_t		status;
-	u_int16_t		magic;		/* 0xffff = regular FPU data */
-
-	/* FXSR FPU environment */
-	u_int32_t		_fxsr_env[6];	/* env is ignored. */
-	u_int32_t		mxcsr;
-	u_int32_t		reserved;
-	struct l_fpxreg		_fxsr_st[8];	/* reg data is ignored. */
-	struct l_xmmreg		_xmm[8];
-	u_int32_t		padding[56];
-};
-
-/*
- * We make the stack look like Linux expects it when calling a signal
- * handler, but use the BSD way of calling the handler and sigreturn().
- */
-struct l_sigframe {
-	l_int			sf_sig;
-	struct l_sigcontext	sf_sc;
-	struct l_fpstate	sf_fpstate;
-	l_uint			sf_extramask[LINUX_NSIG_WORDS-1];
-};
-
-struct l_rt_sigframe {
-	l_int			sf_sig;
-	l_siginfo_t		*sf_siginfo;
-	struct l_ucontext	*sf_ucontext;
-	l_siginfo_t		sf_si;
-	struct l_ucontext	sf_sc;
-};
 
 extern struct sysentvec linux_sysvec;
 
