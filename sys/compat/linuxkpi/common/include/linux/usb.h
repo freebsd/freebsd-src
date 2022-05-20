a478f4afd83a11f8f11d9b4e594aed21691f63b0
Bjoern A. Zeeb <bz@FreeBSD.org>
2022-05-20 21:50:01 +0000

    LinuxKPI: move pm_message_t from kernel.h to pm.h
    Move pm_message_t from kernel.h to pm.h and remove a private define
    in usb.h as well as adjust the implementation in linux_usb.c.
    This cleans up what I believe to be a historic shortcut and is
    needed for future wireless driver updates.
    
    Leave a note in UPDATING that drm-kmod users need to update to the
    latest version before re-compiling a new kernel to avoid errors
    (see PR).
    
    Sponsored by:	The FreeBSD Foundation
    PR:		264449 (drm-kmod port update, thanks wulf)
    Obtained from:	bz_git_iwlwifi (Dec 2020) (partly)
    Reviewed by:	hselasky, imp
    Differential Revision: https://reviews.freebsd.org/D35276
    
    (cherry picked from commit 0e981d79b19da8fc550ea0dbe46e8658be988db4)

--- sys/compat/linuxkpi/common/include/linux/usb.h
+++ sys/compat/linuxkpi/common/include/linux/usb.h
@@ -37,12 +37,13 @@
 #include <dev/usb/usbdi.h>
 #include <dev/usb/usbdi_util.h>
 
+#include <linux/pm.h>
+
 struct usb_device;
 struct usb_interface;
 struct usb_driver;
 struct urb;
 
-typedef void *pm_message_t;
 typedef void (usb_complete_t)(struct urb *);
 
 #define	USB_MAX_FULL_SPEED_ISOC_FRAMES (60 * 1)
