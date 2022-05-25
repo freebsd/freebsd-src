54210d609268a20bf3a0eab9ec3e6f6d4a408c10
Eric Joyner <erj@FreeBSD.org>
2022-05-24 23:52:57 -0700

    irdma(4): Fix previous commit that broke build on amd64
    Changing the printf specifier to %p fixed the build on powerpc64, but it
    actually broke the amd64 build since the type of the value to be printed
    is uint64_t on amd64.
    
    Instead, keep the new %p specifier but cast the printed argument to a
    uintptr_t and then to a void * in order for it to be valid type for
    %p.
    
    Signed-off-by: Eric Joyner <erj@FreeBSD.org>
    
    Reported by:	jrtc27@
    MFC after:	5 days
    MFC-with:	afa7c84d30d020cc4ff26628fe48eaf354dd35bc
    Sponsored by:	Intel Corporation

--- sys/dev/irdma/icrdma.c
+++ sys/dev/irdma/icrdma.c
@@ -501,7 +501,7 @@ irdma_probe(struct ice_rdma_peer *peer)
 
 	irdma_pr_info("probe: irdma-%s peer=%p, peer->pf_id=%d, peer->ifp=%p, peer->ifp->if_dunit=%d, peer->pci_mem->r_bustag=%p\n",
 		      irdma_driver_version, peer, peer->pf_id, peer->ifp,
-		      pf_if_d(peer), peer->pci_mem->r_bustag);
+		      pf_if_d(peer), (void *)(uintptr_t)peer->pci_mem->r_bustag);
 
 	hdl = irdma_find_handler(peer);
 	if (hdl)
