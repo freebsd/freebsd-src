71bc8bcf660b437b99af1e3d382f7bfdaea5fa9c
Dmitry Chagin <dchagin@FreeBSD.org>
2022-05-28 23:46:05 +0300

    linux(4): Handle SO_TIMESTAMPNS socket option
    The SO_TIMESTAMPNS enables or disables the receiving of the SCM_TIMESTAMPNS
    control message. The cmsg_data field is a struct timespec.
    To distinguish between SO_TIMESTAMP and SO_TIMESTAMPNS in the recvmsg()
    map the last one to the SO_BINTIME and convert bintime to the timespec.
    In the rest, implementation is identical to the SO_TIMESTAMP.
    
    MFC after:		2 weeks

--- sys/compat/linux/linux_emul.h
+++ sys/compat/linux/linux_emul.h
@@ -77,6 +77,7 @@ struct linux_pemuldata {
 	uint32_t	ptrace_flags;	/* used by ptrace(2) */
 	uint32_t	oom_score_adj;	/* /proc/self/oom_score_adj */
 	uint32_t	so_timestamp;	/* requested timeval */
+	uint32_t	so_timestampns;	/* requested timespec */
 };
 
 #define	LINUX_PEM_XLOCK(p)	sx_xlock(&(p)->pem_sx)
