0db22efc94459609b99d5289c2fecf22bda646fc
Mitchell Horne <mhorne@FreeBSD.org>
2022-06-02 10:14:41 -0300

    Use KERNEL_PANICKED() in more places
    This is slightly more optimized than checking panicstr directly. For
    most of these instances performance doesn't matter, but let's make
    KERNEL_PANICKED() the common idiom.
    
    Reviewed by:	mjg
    MFC after:	3 days
    Differential Revision:	https://reviews.freebsd.org/D35373
    
    (cherry picked from commit 35eb9b10c265a27ce1f80a6eb74887240c7f4305)

--- sys/cddl/contrib/opensolaris/uts/common/dtrace/dtrace.c
+++ sys/cddl/contrib/opensolaris/uts/common/dtrace/dtrace.c
@@ -7335,7 +7335,7 @@ dtrace_probe(dtrace_id_t id, uintptr_t arg0, uintptr_t arg1,
 	volatile uint16_t *flags;
 	hrtime_t now;
 
-	if (panicstr != NULL)
+	if (KERNEL_PANICKED())
 		return;
 
 #ifdef illumos
@@ -7366,7 +7366,7 @@ dtrace_probe(dtrace_id_t id, uintptr_t arg0, uintptr_t arg1,
 #ifdef illumos
 	if (panic_quiesce) {
 #else
-	if (panicstr != NULL) {
+	if (KERNEL_PANICKED()) {
 #endif
 		/*
 		 * We don't trace anything if we're panicking.
