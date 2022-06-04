d94958b8bdaff7b74d4c80c239e82918799715ef
Dimitry Andric <dim@FreeBSD.org>
2022-06-04 15:22:35 +0200

    Bump versions llvm-project release/14.x llvmorg-14.0.4-0-g29f1039a7285
    Somehow git rebase made this squashed commit disappear. Restore it.
    
    PR:		261742
    MFC after:	3 days

--- lib/clang/include/llvm/Support/VCSRevision.h
+++ lib/clang/include/llvm/Support/VCSRevision.h
@@ -1,3 +1,3 @@
 /* $FreeBSD$ */
-#define LLVM_REVISION "llvmorg-14.0.3-0-g1f9140064dfb"
+#define LLVM_REVISION "llvmorg-14.0.4-0-g29f1039a7285"
 #define LLVM_REPOSITORY "https://github.com/llvm/llvm-project.git"
