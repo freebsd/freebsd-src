3325c47647b626d7d8b0f80cb22863639cd9dbf6
Elliott Mitchell <ehem+freebsd@m5p.com>
2022-06-04 16:56:13 -0700

    intr/x86: remove ->pic_vector(), switch to ->ie_irq
    Retrieving the interrupt number from isrcs via a ->pic_vector() function
    made sense when the current x86 interrupt framework was implemented.
    Since the kernel event structure has ->ie_irq readily available,
    ->pic_vector() now serves to increase complexity.  All callers know
    the vector at time of interrupt registration.
    
    Remove vector from xen_arch_isrc_t as that is now completely unused.
    
    Differential Revision: https://reviews.freebsd.org/D35406

--- sys/x86/include/interrupt.h
+++ sys/x86/include/interrupt.h
@@ -91,7 +91,6 @@ struct pic {
 	void (*pic_eoi_source)(struct intsrc *);
 	void (*pic_enable_intr)(struct intsrc *);
 	void (*pic_disable_intr)(struct intsrc *);
-	int (*pic_vector)(struct intsrc *);
 	int (*pic_source_pending)(struct intsrc *);
 	void (*pic_suspend)(struct pic *);
 	void (*pic_resume)(struct pic *, bool suspend_cancelled);
@@ -156,7 +155,7 @@ int	intr_describe(struct intsrc *isrc, void *ih, const char *descr);
 void	intr_execute_handlers(struct intsrc *isrc, struct trapframe *frame);
 u_int	intr_next_cpu(int domain);
 int	intr_register_pic(struct pic *pic);
-int	intr_register_source(struct intsrc *isrc);
+int	intr_register_source(u_int vector, struct intsrc *isrc);
 int	intr_remove_handler(void *cookie);
 void	intr_resume(bool suspend_cancelled);
 void	intr_suspend(void);
