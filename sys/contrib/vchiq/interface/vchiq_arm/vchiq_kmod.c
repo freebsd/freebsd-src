370cded5d55fb80a718226cab71b682c8cea9ba2
Elliott Mitchell <ehem+freebsd@m5p.com>
2022-06-08 15:58:22 -0700

    intrcompat: rename "machine/intr*.h" to "machine/interrupt.h"
    Having two distinct names for the interrupt headers serves to interfere
    with most efforts to converge the interrupt frameworks.  Simply rename
    all of them to "machine/interrupt.h".
    
    Define __MACHINE_INTERRUPT_H__ in i386/include/interrupt.h in order to
    be consistent with other platforms.  This is otherwise silly, but
    consistency is important.
    
    Do some sorting of #include order.  Notably a bunch involving
    resource.h.
    
    Merge/remove #ifdefs from subr_bus.c and other file to serve as
    telltales warning of someone breaking this.  These are also likely to
    benefit from having a single shared name.
    
    Differential Revision: https://reviews.freebsd.org/D35559

--- sys/contrib/vchiq/interface/vchiq_arm/vchiq_kmod.c
+++ sys/contrib/vchiq/interface/vchiq_arm/vchiq_kmod.c
@@ -39,7 +39,7 @@ __FBSDID("$FreeBSD$");
 #include <machine/bus.h>
 #include <machine/cpu.h>
 #include <machine/frame.h>
-#include <machine/intr.h>
+#include <machine/interrupt.h>
 
 #include <dev/fdt/fdt_common.h>
 #include <dev/ofw/openfirm.h>
