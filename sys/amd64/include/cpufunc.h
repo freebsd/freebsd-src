24983043bf7948e274b2b00d634ae509a292b976
Brooks Davis <brooks@FreeBSD.org>
2022-06-13 18:35:38 +0100

    x86: cleanup in machine/cpufunc.h
    Reduce diffs between amd64 and i386 and improve whitespace
    
    Reviewed by:	jhb, imp

--- sys/amd64/include/cpufunc.h
+++ sys/amd64/include/cpufunc.h
@@ -105,16 +105,16 @@ static __inline void
 do_cpuid(u_int ax, u_int *p)
 {
 	__asm __volatile("cpuid"
-			 : "=a" (p[0]), "=b" (p[1]), "=c" (p[2]), "=d" (p[3])
-			 :  "0" (ax));
+	    : "=a" (p[0]), "=b" (p[1]), "=c" (p[2]), "=d" (p[3])
+	    :  "0" (ax));
 }
 
 static __inline void
 cpuid_count(u_int ax, u_int cx, u_int *p)
 {
 	__asm __volatile("cpuid"
-			 : "=a" (p[0]), "=b" (p[1]), "=c" (p[2]), "=d" (p[3])
-			 :  "0" (ax), "c" (cx));
+	    : "=a" (p[0]), "=b" (p[1]), "=c" (p[2]), "=d" (p[3])
+	    :  "0" (ax), "c" (cx));
 }
 
 static __inline void
