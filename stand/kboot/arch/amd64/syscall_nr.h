7ae6735aea726289e26be8422469ea9b08b1bd19
Warner Losh <imp@FreeBSD.org>
2022-06-13 11:28:31 -0600

    kboot: Sort system calls alphabetically
    No functional change: sort the system calls alphbetically to make it
    easier to add new ones.
    
    Sponsored by:		Netflix
    
    (cherry picked from commit ce3e4a3a96d951b4fcf231e8680935c01f1cc3e6)

--- stand/kboot/arch/amd64/syscall_nr.h
+++ stand/kboot/arch/amd64/syscall_nr.h
@@ -1,14 +1,14 @@
-#define SYS_read		  0
-#define SYS_write		  1
-#define SYS_open		  2
 #define SYS_close		  3
-#define SYS_gettimeofday	 96
-#define SYS_reboot		169
-#define SYS_mmap		  9
-#define SYS_uname		 63
-#define SYS_lseek		  8
 #define SYS_getdents		 78
+#define SYS_gettimeofday	 96
+#define SYS_lseek		  8
+#define SYS_mmap		  9
+#define SYS_open		  2
+#define SYS_read		  0
+#define SYS_reboot		169
 #define SYS_select		 23
+#define SYS_uname		 63
+#define SYS_write		  1
 #define __NR_kexec_load		246
 
 #define KEXEC_ARCH_X86_64	 62
