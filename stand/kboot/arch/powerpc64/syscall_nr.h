12e5d8d97754b5697e10af772f503c21d5f68bf3
Warner Losh <imp@FreeBSD.org>
2022-06-13 11:33:24 -0600

    kboot: use SYS_kexec_load insteaed of __NR_kexec_load
    Make this regular with the rest of the system calls.
    
    Sponsored by:		Netflix
    
    (cherry picked from commit 43ad112b936c5516e0a9b968930a6d3387e7d86b)

--- stand/kboot/arch/powerpc64/syscall_nr.h
+++ stand/kboot/arch/powerpc64/syscall_nr.h
@@ -1,6 +1,7 @@
 #define SYS_close		  6
 #define SYS_getdents		141
 #define SYS_gettimeofday	 78
+#define SYS_kexec_load		268
 #define SYS_llseek		140
 #define SYS_mmap		 90
 #define SYS_open		  5
@@ -9,7 +10,6 @@
 #define SYS_select		142
 #define SYS_uname		120
 #define SYS_write		  4
-#define __NR_kexec_load		268
 
 #define KEXEC_ARCH_PPC64	 21
 #define KEXEC_ARCH		KEXEC_ARCH_PPC64
