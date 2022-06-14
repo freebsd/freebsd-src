a3a5805405bb5ef13ec25b8254c0dbc11a98be34
Cy Schubert <cy@FreeBSD.org>
2022-06-13 19:31:01 -0700

    kern_thread.c: Fix i386 build
    Chase a2fc0ddb743183d8a810fe54d840458960a4608b by updating static
    assertions of struct proc.

--- sys/kern/kern_thread.c
+++ sys/kern/kern_thread.c
@@ -119,9 +119,9 @@ _Static_assert(offsetof(struct proc, p_pid) == 0x78,
     "struct proc KBI p_pid");
 _Static_assert(offsetof(struct proc, p_filemon) == 0x270,
     "struct proc KBI p_filemon");
-_Static_assert(offsetof(struct proc, p_comm) == 0x284,
+_Static_assert(offsetof(struct proc, p_comm) == 0x288,
     "struct proc KBI p_comm");
-_Static_assert(offsetof(struct proc, p_emuldata) == 0x310,
+_Static_assert(offsetof(struct proc, p_emuldata) == 0x314,
     "struct proc KBI p_emuldata");
 #endif
 
