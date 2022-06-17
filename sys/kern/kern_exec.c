54689a282aee8075063228881ee577de181967b6
Dmitry Chagin <dchagin@FreeBSD.org>
2022-06-17 22:33:05 +0300

    linux(4): Modify sv_onexec hook to return an error.
    Temporary add stubs to the Linux emulation layer which calls the existing hook.
    
    Reviewed by:            kib
    Differential Revision:  https://reviews.freebsd.org/D30911
    MFC after:              2 weeks
    
    (cherry picked from commit 5fd9cd53d256e08c601548c425bfcb3472f2d09b)

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -1196,9 +1196,7 @@ exec_new_vmspace(struct image_params *imgp, struct sysentvec *sv)
 		}
 	}
 
-	if (sv->sv_onexec != NULL)
-		sv->sv_onexec(p, imgp);
-	return (0);
+	return (sv->sv_onexec != NULL ? sv->sv_onexec(p, imgp) : 0);
 }
 
 /*
