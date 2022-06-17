e36349f0e30ed123f375c81e5282ef09e4a28a74
Juraj Lutter <otis@FreeBSD.org>
2022-06-17 15:36:53 +0200

    kldload: Bring functionality in line with manual page
    Honor -q parameter and do not display any warning messages when -q is
    specified.
    
    Approved by:		dfr
    MFC after:		2 weeks
    Sponsored by:		Resulta, s.r.o.
    Differential Revision:	https://reviews.freebsd.org/D35511

--- sbin/kldload/kldload.c
+++ sbin/kldload/kldload.c
@@ -171,21 +171,23 @@ main(int argc, char** argv)
 						printf("%s is already "
 						    "loaded\n", argv[0]);
 				} else {
-					switch (errno) {
-					case EEXIST:
-						warnx("can't load %s: module "
-						    "already loaded or "
-						    "in kernel", argv[0]);
-						break;
-					case ENOEXEC:
-						warnx("an error occurred while "
-						    "loading module %s. "
-						    "Please check dmesg(8) for "
-						    "more details.", argv[0]);
-						break;
-					default:
-						warn("can't load %s", argv[0]);
-						break;
+					if (!quiet) {
+						switch (errno) {
+						case EEXIST:
+							warnx("can't load %s: module "
+							    "already loaded or "
+							    "in kernel", argv[0]);
+							break;
+						case ENOEXEC:
+							warnx("an error occurred while "
+							    "loading module %s. "
+							    "Please check dmesg(8) for "
+							    "more details.", argv[0]);
+							break;
+						default:
+							warn("can't load %s", argv[0]);
+							break;
+						}
 					}
 					errors++;
 				}
