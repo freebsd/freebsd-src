bcb3116e5c2fddc03be4305ef78ec3554eb2da30
Emmanuel Vadot <manu@FreeBSD.org>
2022-06-21 15:52:49 +0200

    fb: Add a default getinfo method
    fb_getinfo is badly designed as it returns either the
    info if the driver have the method or ENXIO via the kobj stuff
    if the driver doesn't have it.
    Add a default method that returns NULL as the code already checks this
    and it avoid changing the interface.
    None of the drm drivers supported have this method and it sometimes
    fails and panic when loading them (for now only usb-c docks seems to be
    affected).
    
    MFC after:	3 days
    Sponsored by:	Beckhoff Automation GmbH & Co. KG

--- sys/dev/fb/fb_if.m
+++ sys/dev/fb/fb_if.m
@@ -3,6 +3,14 @@
 
 INTERFACE fb;
 
+CODE {
+	static struct fb_info *
+	fb_default_getinfo(device_t dev)
+	{
+		return (NULL);
+	}
+};
+
 METHOD struct fb_info * getinfo {
 	device_t dev;
-};
+} DEFAULT fb_default_getinfo;
