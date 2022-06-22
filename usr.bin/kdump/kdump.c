2068608ebdbfaa40ff763af0526e8ee0fb9f3120
Dmitry Chagin <dchagin@FreeBSD.org>
2022-06-22 14:15:20 +0300

    kdump: Decode Linux l_sigset_t.
    Reviewed by:		markj
    Differential revision:	https://reviews.freebsd.org/D35153
    MFC after:		2 weeks
    
    (cherry picked from commit 3606a213bfa42e1970197ba9b3494c88d5827259)

--- usr.bin/kdump/kdump.c
+++ usr.bin/kdump/kdump.c
@@ -2061,7 +2061,10 @@ ktrstruct(char *buf, size_t buflen)
 		ktrbitset(name, set, datalen);
 		free(set);
 	} else {
-		printf("unknown structure\n");
+#ifdef SYSDECODE_HAVE_LINUX
+		if (ktrstruct_linux(name, data, datalen) == false)
+#endif
+			printf("unknown structure\n");
 	}
 	return;
 invalid:
