351a9f9b0d5d74ea69b1de92dfff6ddbc5ecfd47
Elliott Mitchell <ehem+freebsd@m5p.com>
2022-06-22 17:53:47 -0700

    sys: purge EOL release compatibility
    Remove conditionals checking for End-of-Life releases.  Some
    pre-FreeBSD-13.0 conditionals remain, but remove everything before
    FreeBSD 12.3.
    
    Differential Revision: https://reviews.freebsd.org/D35560

--- sys/dev/acpica/acpi_container.c
+++ sys/dev/acpica/acpi_container.c
@@ -68,9 +68,7 @@ static device_method_t acpi_syscont_methods[] = {
     DEVMETHOD(bus_deactivate_resource,	bus_generic_deactivate_resource),
     DEVMETHOD(bus_setup_intr,		bus_generic_setup_intr),
     DEVMETHOD(bus_teardown_intr,	bus_generic_teardown_intr),
-#if __FreeBSD_version >= 1100000
     DEVMETHOD(bus_get_cpus,		bus_generic_get_cpus),
-#endif
 
     /* pcib interface */
     DEVMETHOD(pcib_alloc_msi,		acpi_syscont_alloc_msi),
