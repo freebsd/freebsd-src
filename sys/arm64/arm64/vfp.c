5ab7ba992e697a21729842d3839e46ff595b6aea
Andrew Turner <andrew@FreeBSD.org>
2022-06-24 11:53:15 +0000

    Split out vfp_new_thread
    To keep the vfp thread creation code in one place move into vfp.c. This
    will also help with adding SVE support as it depends on VFP.
    
    Reviewed by:	markj
    Sponsored by:	The FreeBSD Foundation
    Differential Revision: https://reviews.freebsd.org/D35615
    
    (cherry picked from commit baf8f20a4af43283a1fbee49b0c2ad906074f20f)

--- sys/arm64/arm64/vfp.c
+++ sys/arm64/arm64/vfp.c
@@ -200,6 +200,32 @@ vfp_save_state(struct thread *td, struct pcb *pcb)
 }
 
 /*
+ * Update the VFP state for a forked process or new thread. The PCB will
+ * have been copied from the old thread.
+ */
+void
+vfp_new_thread(struct thread *newtd, struct thread *oldtd, bool fork)
+{
+	struct pcb *newpcb;
+
+	newpcb = newtd->td_pcb;
+
+	/* Kernel threads start with clean VFP */
+	if ((oldtd->td_pflags & TDP_KTHREAD) != 0) {
+		newpcb->pcb_fpflags &=
+		    ~(PCB_FP_STARTED | PCB_FP_KERN | PCB_FP_NOSAVE);
+	} else {
+		MPASS((newpcb->pcb_fpflags & (PCB_FP_KERN|PCB_FP_NOSAVE)) == 0);
+		if (!fork) {
+			newpcb->pcb_fpflags &= ~PCB_FP_STARTED;
+		}
+	}
+
+	newpcb->pcb_fpusaved = &newpcb->pcb_fpustate;
+	newpcb->pcb_vfpcpu = UINT_MAX;
+}
+
+/*
  * Reset the FP state to avoid leaking state from the parent process across
  * execve() (and to ensure that we get a consistent floating point environment
  * in every new process).
