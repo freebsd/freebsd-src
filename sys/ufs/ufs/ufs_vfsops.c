466e4f08a19f8317f2adc2f70c678d319a9de9b0
Konstantin Belousov <kib@FreeBSD.org>
2022-06-27 20:16:29 +0300

    UFS quotaoff: start write before unbusying
    Otherwise the mount point could be unmounted meantime.
    
    Reported and tested by:	pho
    Reviewed by:	jah
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D35638

--- sys/ufs/ufs/ufs_vfsops.c
+++ sys/ufs/ufs/ufs_vfsops.c
@@ -129,9 +129,9 @@ ufs_quotactl(mp, cmds, id, arg, mp_busy)
 		vfs_ref(mp);
 		KASSERT(*mp_busy,
 		    ("%s called without busied mount", __func__));
+		vn_start_write(NULL, &mp, V_WAIT | V_MNTREF);
 		vfs_unbusy(mp);
 		*mp_busy = false;
-		vn_start_write(NULL, &mp, V_WAIT | V_MNTREF);
 		error = quotaoff(td, mp, type);
 		vn_finished_write(mp);
 		break;
