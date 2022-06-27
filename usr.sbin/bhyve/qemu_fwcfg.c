b55d08e2338b9e814c6bc820be07caf30ccffe4a
Corvin Köhne <corvink@FreeBSD.org>
2022-06-27 11:05:42 +0200

    bhyve: add QEMU_FWCFG_INDEX_NB_CPUS item
    Requested-by:		OVMF (qemu)
    Reviewed by:		markj
    MFC after:		1 week
    Sponsored by:		Beckhoff Automation GmbH & Co. KG
    Differential Revision:	https://reviews.freebsd.org/D39316

--- usr.sbin/bhyve/qemu_fwcfg.c
+++ usr.sbin/bhyve/qemu_fwcfg.c
@@ -42,6 +42,7 @@
 
 #define QEMU_FWCFG_INDEX_SIGNATURE 0x00
 #define QEMU_FWCFG_INDEX_ID 0x01
+#define QEMU_FWCFG_INDEX_NB_CPUS 0x05
 #define QEMU_FWCFG_INDEX_FILE_DIR 0x19
 
 #define QEMU_FWCFG_FIRST_FILE_INDEX 0x20
@@ -226,6 +227,20 @@ qemu_fwcfg_add_item_id(void)
 }
 
 static int
+qemu_fwcfg_add_item_nb_cpus(void)
+{
+	uint16_t *fwcfg_max_cpus = calloc(1, sizeof(uint16_t));
+	if (fwcfg_max_cpus == NULL) {
+		return (ENOMEM);
+	}
+
+	*fwcfg_max_cpus = htole16(guest_ncpus);
+
+	return (qemu_fwcfg_add_item(QEMU_FWCFG_ARCHITECTURE_GENERIC,
+	    QEMU_FWCFG_INDEX_NB_CPUS, sizeof(uint16_t), fwcfg_max_cpus));
+}
+
+static int
 qemu_fwcfg_add_item_signature(void)
 {
 	struct qemu_fwcfg_signature *const fwcfg_signature = calloc(1,
@@ -406,6 +421,10 @@ qemu_fwcfg_init(struct vmctx *const ctx)
 	}
 	if ((error = qemu_fwcfg_add_item_id()) != 0) {
 		warnx("%s: Unable to add id item", __func__);
+		goto done;
+	}
+	if ((error = qemu_fwcfg_add_item_nb_cpus()) != 0) {
+		warnx("%s: Unable to add nb_cpus item", __func__);
 		goto done;
 	}
 	if ((error = qemu_fwcfg_add_item_file_dir()) != 0) {
