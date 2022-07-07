166b9dc6db2a0b1c4a42aabd4c8750c1f0d4c03f
Gleb Smirnoff <glebius@FreeBSD.org>
2022-07-06 22:19:08 -0700

    sockstat(1): print PID adjusted to the right
    This allows for easy copy-and-paste of a unix(4) peer name for lookup.
    With current implementation it is guaranteed that a peer listed could be
    found in the output.
    
    Differential revision:	https://reviews.freebsd.org/D35727

--- usr.bin/sockstat/sockstat.c
+++ usr.bin/sockstat/sockstat.c
@@ -1340,7 +1340,7 @@ display(void)
 			pos += xprintf("%.10s", getprocname(xf->xf_pid));
 			while (pos < 20)
 				pos += xprintf(" ");
-			pos += xprintf("%lu ", (u_long)xf->xf_pid);
+			pos += xprintf("%5lu ", (u_long)xf->xf_pid);
 			while (pos < 26)
 				pos += xprintf(" ");
 			pos += xprintf("%-3d ", xf->xf_fd);
