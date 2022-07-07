6b62e360edcc1a7e11518f92f42a597306173af9
Warner Losh <imp@FreeBSD.org>
2022-07-07 16:58:27 -0600

    kboot: Reimplement older system calls in terms of newer ones
    aarch64 doesn't have open, just openat, etc. Cope.
    
    Sponsored by:		Netflix

--- stand/kboot/arch/amd64/syscall_nr.h
+++ stand/kboot/arch/amd64/syscall_nr.h
@@ -1,13 +1,13 @@
 #define SYS_close		  3
 #define SYS_getdents		 78
 #define SYS_gettimeofday	 96
-#define SYS_lseek		  8
 #define SYS_kexec_load		246
+#define SYS_lseek		  8
 #define SYS_mmap		  9
-#define SYS_open		  2
+#define SYS_openat		257
+#define SYS_pselect6		270
 #define SYS_read		  0
 #define SYS_reboot		169
-#define SYS_select		 23
 #define SYS_uname		 63
 #define SYS_write		  1
 
--- stand/kboot/arch/powerpc64/syscall_nr.h
+++ stand/kboot/arch/powerpc64/syscall_nr.h
@@ -4,10 +4,10 @@
 #define SYS_kexec_load		268
 #define SYS_llseek		140
 #define SYS_mmap		 90
-#define SYS_open		  5
+#define SYS_openat		286
+#define SYS_pselect6		280
 #define SYS_read		  3
 #define SYS_reboot		 88
-#define SYS_select		142
 #define SYS_uname		120
 #define SYS_write		  4
 
