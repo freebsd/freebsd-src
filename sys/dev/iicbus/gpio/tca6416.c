e490a1c8c5eb5341174ada44e70331e88e878f20
Warner Losh <imp@FreeBSD.org>
2022-07-08 11:53:30 -0600

    tca6416: remove write only sc
    Sponsored by:		Netflix
    
    (cherry picked from commit 22127c7c1b9c0f8feb73f13a829b8b757f2274eb)

--- sys/dev/iicbus/gpio/tca6416.c
+++ sys/dev/iicbus/gpio/tca6416.c
@@ -274,11 +274,8 @@ tca6416_pin_getcaps(device_t dev, uint32_t pin, uint32_t *caps)
 static int
 tca6416_pin_getflags(device_t dev, uint32_t pin, uint32_t *pflags)
 {
-	struct tca6416_softc *sc;
 	int error;
 	uint8_t reg_addr, reg_bit, val;
-
-	sc = device_get_softc(dev);
 
 	if (pin >= NUM_PINS || pflags == NULL)
 		return (EINVAL);
@@ -366,11 +363,8 @@ tca6416_pin_getname(device_t dev, uint32_t pin, char *name)
 static int
 tca6416_pin_get(device_t dev, uint32_t pin, unsigned int *pval)
 {
-	struct tca6416_softc *sc;
 	uint8_t reg_bit, reg_addr, reg_pvalue;
 	int error;
-
-	sc = device_get_softc(dev);
 
 	if (pin >= NUM_PINS || pval == NULL)
 		return (EINVAL);
