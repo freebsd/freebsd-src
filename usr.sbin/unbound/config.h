80a1d93ee98ece55958b6f5b4fafb7c92e738d96
Cy Schubert <cy@FreeBSD.org>
2022-07-17 06:54:32 -0700

    unbound: Update version strings to 1.16.1
    The last number of unbound upgrades failed to manually update the
    version strings in usr.sbin/unbound/config.h. This commit fixes that.
    
    Reported by:	"Herbert J. Skuhra" <herbert@gojira.at>
    Fixes:		c736640727132ab0747207635a01736a254cea28
    		acdcdab5f2cd4198126f479cfb09d1f78e6c1459
    		e6401328735221824b3ab763a9926799f5d84144
    		a4b34ec17a9dd5a89a36192d4af434d9b1c3481e
    		e4d3d11c3320e864110b1a8d451978c638dc5be2
    		cbe68fc7a297e6314f82f3292d579cbb544517aa
    MFC after:	3 days

--- usr.sbin/unbound/config.h
+++ usr.sbin/unbound/config.h
@@ -731,7 +731,7 @@
 #define PACKAGE_NAME "unbound"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "unbound 1.13.1"
+#define PACKAGE_STRING "unbound 1.16.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "unbound"
@@ -740,7 +740,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.13.1"
+#define PACKAGE_VERSION "1.16.1"
 
 /* default pidfile location */
 #define PIDFILE "/var/unbound/unbound.pid"
@@ -762,7 +762,7 @@
 #define ROOT_CERT_FILE "/var/unbound/icannbundle.pem"
 
 /* version number for resource files */
-#define RSRC_PACKAGE_VERSION 1,13,1,0
+#define RSRC_PACKAGE_VERSION 1,16,1,0
 
 /* Directory to chdir to */
 #define RUN_DIR "/var/unbound"
