b7bd54e4ec5b5f59c1651e3600dbc808ddefa316
Alan Cox <alc@FreeBSD.org>
2022-07-17 19:56:39 -0500

    x86/iommu: Shrink the critical section in dmar_qi_task()
    It is safe to test and clear the Invalidation Wait Descriptor
    Complete flag before acquiring the DMAR lock in dmar_qi_task(),
    rather than waiting until the lock is held.
    
    Reviewed by:	kib
    MFC after:	2 weeks
    
    (cherry picked from commit 4eaaacc75535befdb9894cca4e0d8da376328fa4)

--- sys/x86/iommu/intel_qi.c
+++ sys/x86/iommu/intel_qi.c
@@ -354,6 +354,16 @@ dmar_qi_task(void *arg, int pending __unused)
 
 	unit = arg;
 
+	/*
+	 * Request an interrupt on the completion of the next invalidation
+	 * wait descriptor with the IF field set.
+	 */
+	ics = dmar_read4(unit, DMAR_ICS_REG);
+	if ((ics & DMAR_ICS_IWC) != 0) {
+		ics = DMAR_ICS_IWC;
+		dmar_write4(unit, DMAR_ICS_REG, ics);
+	}
+
 	DMAR_LOCK(unit);
 	for (;;) {
 		entry = TAILQ_FIRST(&unit->tlb_flush_entries);
@@ -365,11 +375,6 @@ dmar_qi_task(void *arg, int pending __unused)
 		DMAR_UNLOCK(unit);
 		dmar_domain_free_entry(entry, true);
 		DMAR_LOCK(unit);
-	}
-	ics = dmar_read4(unit, DMAR_ICS_REG);
-	if ((ics & DMAR_ICS_IWC) != 0) {
-		ics = DMAR_ICS_IWC;
-		dmar_write4(unit, DMAR_ICS_REG, ics);
 	}
 	if (unit->inv_seq_waiters > 0)
 		wakeup(&unit->inv_seq_waiters);
