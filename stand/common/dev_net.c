c4f4a6c983fdadfc343344152cfc571a00a1e07a
Warner Losh <imp@FreeBSD.org>
2022-07-24 15:51:32 -0600

    stand: Use c99 structure initialization for network device
    Use c99 structure init for devsw.
    
    Sponsored by:		Netflix

--- stand/common/dev_net.c
+++ stand/common/dev_net.c
@@ -86,15 +86,15 @@ static int	net_print(int);
 static int net_getparams(int sock);
 
 struct devsw netdev = {
-	"net",
-	DEVT_NET,
-	net_init,
-	net_strategy,
-	net_open,
-	net_close,
-	noioctl,
-	net_print,
-	net_cleanup
+	.dv_name = "net",
+	.dv_type = DEVT_NET,
+	.dv_init = net_init,
+	.dv_strategy = net_strategy,
+	.dv_open = net_open,
+	.dv_close = net_close,
+	.dv_ioctl = noioctl,
+	.dv_print = net_print,
+	.dv_cleanup = net_cleanup,
 };
 
 static struct uri_scheme {
