823283ab45b8e705dbe6c44a6570e50d0151ae65
Dimitry Andric <dim@FreeBSD.org>
2022-07-26 21:15:37 +0200

    Adjust function definitions in netipsec's key.c to avoid clang 15 warnings
    With clang 15, the following -Werror warnings are produced:
    
        sys/netipsec/key.c:6432:15: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
        key_getcomb_ah()
                      ^
                       void
        sys/netipsec/key.c:6489:19: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
        key_getcomb_ipcomp()
                          ^
                           void
    
    This is because key_getcomb_ah() and key_getcomb_ipcomp() are declared
    with (void) argument lists, but defined with empty argument lists. Make
    the definitions match the declarations.
    
    MFC after:	3 days
    
    (cherry picked from commit 8bd2887be5105010e73fc35ddd25021e186811b8)

--- sys/netipsec/key.c
+++ sys/netipsec/key.c
@@ -6377,7 +6377,7 @@ key_getsizes_ah(const struct auth_hash *ah, int alg, u_int16_t* min,
  * XXX reorder combinations by preference
  */
 static struct mbuf *
-key_getcomb_ah()
+key_getcomb_ah(void)
 {
 	const struct auth_hash *algo;
 	struct sadb_comb *comb;
@@ -6434,7 +6434,7 @@ key_getcomb_ah()
  * XXX reorder combinations by preference
  */
 static struct mbuf *
-key_getcomb_ipcomp()
+key_getcomb_ipcomp(void)
 {
 	const struct comp_algo *algo;
 	struct sadb_comb *comb;
