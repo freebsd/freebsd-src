be93526576ceb4bbb9511738dd7b6d95d69a71a1
Dimitry Andric <dim@FreeBSD.org>
2022-07-26 21:21:44 +0200

    Fix unused variable warning in netipsec's key_debug.c
    With clang 15, the following -Werror warning is produced:
    
        sys/netipsec/key_debug.c:923:9: error: variable 'j' set but not used [-Werror,-Wunused-but-set-variable]
                int i, j;
                       ^
    
    The 'j' variable was in key_debug.c when it was first added, but it
    appears to have been a debugging aid that has never been used, so remove
    it.
    
    MFC after:	3 days
    
    (cherry picked from commit c01fdd7a9f316c8c04786441ca0113be002b96be)

--- sys/netipsec/key_debug.c
+++ sys/netipsec/key_debug.c
@@ -916,9 +916,9 @@ void
 kdebug_mbuf(const struct mbuf *m0)
 {
 	const struct mbuf *m = m0;
-	int i, j;
+	int i;
 
-	for (j = 0; m; m = m->m_next) {
+	for (; m; m = m->m_next) {
 		kdebug_mbufhdr(m);
 		printf("  m_data:\n");
 		for (i = 0; i < m->m_len; i++) {
@@ -927,7 +927,6 @@ kdebug_mbuf(const struct mbuf *m0)
 			if (i % 4 == 0)
 				printf(" ");
 			printf("%02x", mtod(m, const u_char *)[i]);
-			j++;
 		}
 		printf("\n");
 	}
