54b2a65d47337265db22695c685dbf7d0a4e087a
Dimitry Andric <dim@FreeBSD.org>
2022-07-26 16:11:05 +0200

    Adjust function definitions in kern_dtrace.c to avoid clang 15 warnings
    With clang 15, the following -Werror warnings are produced:
    
        sys/kern/kern_dtrace.c:64:18: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
        kdtrace_proc_size()
                         ^
                          void
        sys/kern/kern_dtrace.c:87:20: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
        kdtrace_thread_size()
                           ^
                            void
    
    This is because kdtrace_proc_size() and kdtrace_thread_size() are
    declared with (void) argument lists, but defined with empty argument
    lists. Make the definitions match the declarations.
    
    MFC after:	3 days
    
    (cherry picked from commit db8ea61ae261cc060f423dea050bf943bcad6793)

--- sys/kern/kern_dtrace.c
+++ sys/kern/kern_dtrace.c
@@ -61,7 +61,7 @@ systrace_probe_func_t 		systrace_probe_func;
 
 /* Return the DTrace process data size compiled in the kernel hooks. */
 size_t
-kdtrace_proc_size()
+kdtrace_proc_size(void)
 {
 
 	return (KDTRACE_PROC_SIZE);
@@ -84,7 +84,7 @@ kdtrace_proc_dtor(struct proc *p)
 
 /* Return the DTrace thread data size compiled in the kernel hooks. */
 size_t
-kdtrace_thread_size()
+kdtrace_thread_size(void)
 {
 
 	return (KDTRACE_THREAD_SIZE);
