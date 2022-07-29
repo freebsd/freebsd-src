c18d676d53cc5f0ab47d3ae8b6031c68c1317697
Neel Chauhan <nc@FreeBSD.org>
2022-07-29 11:06:06 -0700

    vmd: Add 8086:A77F PCI ID for all Intel Raptor Lake SKUs
    Summary:
    This is based on the Linux kernel commit
    922bfd001d1ac02111ebbe0524aaab6ca7925521.
    
    Reviewed by:		mav (src)
    MFC after:		3 days
    Differential Revision:	https://reviews.freebsd.org/D35996
    
    (cherry picked from commit b07a48d4f3a175db53ba21a40f27ef0c1a6d0a11)

--- sys/dev/vmd/vmd.c
+++ sys/dev/vmd/vmd.c
@@ -101,6 +101,7 @@ static struct vmd_type vmd_devs[] = {
         { 0x8086, 0x467f, "Intel Volume Management Device", BUS_RESTRICT | VECTOR_OFFSET },
         { 0x8086, 0x4c3d, "Intel Volume Management Device", BUS_RESTRICT | VECTOR_OFFSET },
         { 0x8086, 0x9a0b, "Intel Volume Management Device", BUS_RESTRICT | VECTOR_OFFSET },
+        { 0x8086, 0xa77f, "Intel Volume Management Device", BUS_RESTRICT | VECTOR_OFFSET },
         { 0, 0, NULL, 0 }
 };
 
