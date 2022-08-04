dc6f578a8f53167b8ed768e53dfe4721a282647c
Bryan Venteicher <bryanv@FreeBSD.org>
2022-08-03 22:54:36 -0500

    virtio_mmio: correct offset of VIRTIO_MMIO_CONFIG_GENERATION

--- sys/dev/virtio/mmio/virtio_mmio.h
+++ sys/dev/virtio/mmio/virtio_mmio.h
@@ -83,7 +83,7 @@ int vtmmio_attach(device_t);
 #define	VIRTIO_MMIO_QUEUE_AVAIL_HIGH	0x094	/* requires version 2 */
 #define	VIRTIO_MMIO_QUEUE_USED_LOW	0x0a0	/* requires version 2 */
 #define	VIRTIO_MMIO_QUEUE_USED_HIGH	0x0a4	/* requires version 2 */
-#define	VIRTIO_MMIO_CONFIG_GENERATION	0x100	/* requires version 2 */
+#define	VIRTIO_MMIO_CONFIG_GENERATION	0x0fc	/* requires version 2 */
 #define	VIRTIO_MMIO_CONFIG		0x100
 #define	VIRTIO_MMIO_MAGIC_VIRT		0x74726976
 #define	VIRTIO_MMIO_INT_VRING		(1 << 0)
