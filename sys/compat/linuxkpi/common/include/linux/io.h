freebsd/freebsd-src

commit 82e5439b739bab78058ab7a17cd4d1b4f8375001

linuxkpi: io.h: Do not include asm/set_memory.h for armv6 and armv7
They do not have the same pmap api and this cannot work for those arch.

Fixes:	4d7a74289c799 ("linuxkpi: Add arch_io_{reserve,free}_memtype_wc")
Sponsored by:	Beckhoff Automation GmbH & Co. KG

sys/compat/linuxkpi/common/include/linux/io.h
@@ -38,7 +38,9 @@
 
 #include <linux/compiler.h>
 #include <linux/types.h>
+#if defined(__amd64__) || defined(__arm64__) || defined(__i386__) || defined(__riscv__)
 #include <asm/set_memory.h>
+#endif
 
 /*
  * XXX This is all x86 specific.  It should be bus space access.

commit 7b01ff41923a384660e7daeeda85da1d0211cd33

linuxkpi: io.h: Only exclude armv6 and armv7 for asm/set_memory.h
Other arches like powerpc* needs it.

Fixes:  82e5439b739ba ("linuxkpi: io.h: Do not include asm/set_memory.h for armv6 and armv7")
Fixes:  4d7a74289c799 ("linuxkpi: Add arch_io_{reserve,free}_memtype_wc")
Sponsored by:	Beckhoff Automation GmbH & Co. KG

sys/compat/linuxkpi/common/include/linux/io.h
@@ -38,7 +38,7 @@
 
 #include <linux/compiler.h>
 #include <linux/types.h>
-#if defined(__amd64__) || defined(__arm64__) || defined(__i386__) || defined(__riscv__)
+#if !defined(__arm__)
 #include <asm/set_memory.h>
 #endif
 