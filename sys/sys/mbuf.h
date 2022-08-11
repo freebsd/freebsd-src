486e4606b982b89a811e509223d9ac18e0a99296
Gleb Smirnoff <glebius@FreeBSD.org>
2022-08-11 09:19:36 -0700

    rtsock: do not allocate mbufs_tags(9) just to store a 8-bit value
    Use local storage of the mbuf packet header instead.
    
    Reviewed by:		melifaro
    Differential revision:	https://reviews.freebsd.org/D36121

--- sys/sys/mbuf.h
+++ sys/sys/mbuf.h
@@ -1385,7 +1385,7 @@ extern bool		mb_use_ext_pgs;	/* Use ext_pgs for sendfile */
 #define	PACKET_TAG_IPFORWARD			18 /* ipforward info */
 #define	PACKET_TAG_MACLABEL	(19 | MTAG_PERSISTENT) /* MAC label */
 #define	PACKET_TAG_PF				21 /* PF/ALTQ information */
-#define	PACKET_TAG_RTSOCKFAM			25 /* rtsock sa family */
+/* was	PACKET_TAG_RTSOCKFAM			25    rtsock sa family */
 #define	PACKET_TAG_IPOPTIONS			27 /* Saved IP options */
 #define	PACKET_TAG_CARP				28 /* CARP info */
 #define	PACKET_TAG_IPSEC_NAT_T_PORTS		29 /* two uint16_t */
