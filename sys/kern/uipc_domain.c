9b967bd65de6647aed68a141dc34f9b223a2593c
Alexander V. Chernikov <melifaro@FreeBSD.org>
2022-08-12 13:36:53 +0000

    domains: allow domains to be unloaded
    Add domain_remove() SYSUNINT callback that removes the domain
     from the domain list if it has DOMF_UNLOADABLE flag set.
    This change is required to support netlink ( D36002 ).
    
    Reviewed by:	glebius
    Differential Revision: https://reviews.freebsd.org/D36173

--- sys/kern/uipc_domain.c
+++ sys/kern/uipc_domain.c
@@ -239,6 +239,29 @@ domain_add(void *data)
 	mtx_unlock(&dom_mtx);
 }
 
+void
+domain_remove(void *data)
+{
+	struct domain *dp = (struct domain *)data;
+
+	if ((dp->dom_flags & DOMF_UNLOADABLE) == 0)
+		return;
+
+	mtx_lock(&dom_mtx);
+	if (domains == dp) {
+		domains = dp->dom_next;
+	} else {
+		struct domain *curr;
+		for (curr = domains; curr != NULL; curr = curr->dom_next) {
+			if (curr->dom_next == dp) {
+				curr->dom_next = dp->dom_next;
+				break;
+			}
+		}
+	}
+	mtx_unlock(&dom_mtx);
+}
+
 /* ARGSUSED*/
 static void
 domaininit(void *dummy)
