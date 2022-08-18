abb6a544f4bce27d8414f02570f17cbe38045f94
Jessica Clarke <jrtc27@FreeBSD.org>
2022-08-18 02:46:27 +0100

    sys: Extract __bitcount* from sys/types.h to new sys/bitcount.h
    This will allow the code to be reused by the cross-build sys/types.h
    wrapper in order to provide the APIs for greater compatibility. This
    also provides a path towards eventually removing the definitions from
    sys/types.h altogether if so desired by gradually migrating users to
    including sys/bitcount.h explicitly, but that is not the primary goal
    here.
    
    Note that the copyright header is a direct copu of sys/types.h's given
    that's where this code comes from. This could be replaced in future with
    a more specific one restricted to just the code in question, depending
    on what the copyright for that code is.
    
    Reviewed by:	emaste
    Differential Revision:	https://reviews.freebsd.org/D36132

--- sys/sys/types.h
+++ sys/sys/types.h
@@ -310,68 +310,7 @@ typedef	_Bool	bool;
  * The following are all things that really shouldn't exist in this header,
  * since its purpose is to provide typedefs, not miscellaneous doodads.
  */
-
-#ifdef __POPCNT__
-#define	__bitcount64(x)	__builtin_popcountll((__uint64_t)(x))
-#define	__bitcount32(x)	__builtin_popcount((__uint32_t)(x))
-#define	__bitcount16(x)	__builtin_popcount((__uint16_t)(x))
-#define	__bitcountl(x)	__builtin_popcountl((unsigned long)(x))
-#define	__bitcount(x)	__builtin_popcount((unsigned int)(x))
-#else
-/*
- * Population count algorithm using SWAR approach
- * - "SIMD Within A Register".
- */
-static __inline __uint16_t
-__bitcount16(__uint16_t _x)
-{
-
-	_x = (_x & 0x5555) + ((_x & 0xaaaa) >> 1);
-	_x = (_x & 0x3333) + ((_x & 0xcccc) >> 2);
-	_x = (_x + (_x >> 4)) & 0x0f0f;
-	_x = (_x + (_x >> 8)) & 0x00ff;
-	return (_x);
-}
-
-static __inline __uint32_t
-__bitcount32(__uint32_t _x)
-{
-
-	_x = (_x & 0x55555555) + ((_x & 0xaaaaaaaa) >> 1);
-	_x = (_x & 0x33333333) + ((_x & 0xcccccccc) >> 2);
-	_x = (_x + (_x >> 4)) & 0x0f0f0f0f;
-	_x = (_x + (_x >> 8));
-	_x = (_x + (_x >> 16)) & 0x000000ff;
-	return (_x);
-}
-
-#ifdef __LP64__
-static __inline __uint64_t
-__bitcount64(__uint64_t _x)
-{
-
-	_x = (_x & 0x5555555555555555) + ((_x & 0xaaaaaaaaaaaaaaaa) >> 1);
-	_x = (_x & 0x3333333333333333) + ((_x & 0xcccccccccccccccc) >> 2);
-	_x = (_x + (_x >> 4)) & 0x0f0f0f0f0f0f0f0f;
-	_x = (_x + (_x >> 8));
-	_x = (_x + (_x >> 16));
-	_x = (_x + (_x >> 32)) & 0x000000ff;
-	return (_x);
-}
-
-#define	__bitcountl(x)	__bitcount64((unsigned long)(x))
-#else
-static __inline __uint64_t
-__bitcount64(__uint64_t _x)
-{
-
-	return (__bitcount32(_x >> 32) + __bitcount32(_x));
-}
-
-#define	__bitcountl(x)	__bitcount32((unsigned long)(x))
-#endif
-#define	__bitcount(x)	__bitcount32((unsigned int)(x))
-#endif
+#include <sys/bitcount.h>
 
 #if __BSD_VISIBLE
 
