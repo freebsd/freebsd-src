ff06a8dbb6777a0475ae961faacd776225cb8791
Justin Hibbits <jhibbits@FreeBSD.org>
2022-08-19 22:02:03 -0400

    Mechanically convert ixgbe(4) to IfAPI
    Reviewed by:	erj
    Sponsored by:	Juniper Networks, Inc.
    Differential Revision: https://reviews.freebsd.org/D37845

--- sys/dev/ixgbe/if_fdir.c
+++ sys/dev/ixgbe/if_fdir.c
@@ -54,7 +54,7 @@ ixgbe_reinit_fdir(void *context)
 {
 	if_ctx_t       ctx = context;
 	struct ixgbe_softc *sc = iflib_get_softc(ctx);
-	struct ifnet   *ifp = iflib_get_ifp(ctx);
+	if_t           ifp = iflib_get_ifp(ctx);
 
 	if (!(sc->feat_en & IXGBE_FEATURE_FDIR))
 		return;
@@ -65,7 +65,7 @@ ixgbe_reinit_fdir(void *context)
 	/* re-enable flow director interrupts */
 	IXGBE_WRITE_REG(&sc->hw, IXGBE_EIMS, IXGBE_EIMS_FLOW_DIR);
 	/* Restart the interface */
-	ifp->if_drv_flags |= IFF_DRV_RUNNING;
+	if_setdrvflagbits(ifp, IFF_DRV_RUNNING, 0);
 } /* ixgbe_reinit_fdir */
 
 /************************************************************************
