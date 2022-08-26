e1dd3e56878907f4321fa5456eb23914673dc930
Rick Macklem <rmacklem@FreeBSD.org>
2022-08-25 20:33:31 -0700

    nfscl: Fix handling of a bad session slot (NFSv4.1/4.2)
    When a session has been marked defunct by the server
    sending a NFSERR_BADSESSION reply to the NFSv4.1/4.2
    client, nfsv4_sequencelookup() returns NFSERR_BADSESSION
    without actually assigning a session slot.
    Without this patch, newnfs_request() would erroneously
    free slot 0.
    
    This could result in the slot being reused prematurely,
    but most likely just generated a "freeing free slot!!"
    console message.
    
    This patch fixes the code to not do the erroneous
    freeing of the slot for this case.
    
    PR:	260011
    (cherry picked from commit 2b612c9d3bb528551de9eaabbdbadae89a36ba8b)

--- sys/fs/nfs/nfs_commonkrpc.c
+++ sys/fs/nfs/nfs_commonkrpc.c
@@ -966,12 +966,12 @@ tryagain:
 							    "slot=%d\n", slot);
 							slot = nd->nd_slotid;
 						}
+						freeslot = slot;
 					} else if (slot != 0) {
 						printf("newnfs_request: Bad "
 						    "session slot=%d\n", slot);
 						slot = 0;
 					}
-					freeslot = slot;
 					if (retseq != sep->nfsess_slotseq[slot])
 						printf("retseq diff 0x%x\n",
 						    retseq);
