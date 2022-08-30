c3af4c1ee8791f4e1bddd4df648c8d5cfdc62b2e
Ashish SHUKLA <ashish@FreeBSD.org>
2022-08-30 08:18:51 +0000

    kvm_close(3): Check kd->sparse_map != NULL before munmap
    PR:		266113
    Reviewed by:	markj
    
    (cherry picked from commit e6901a29bc4345f12e9c85426cc2be81045020a6)

--- lib/libkvm/kvm.c
+++ lib/libkvm/kvm.c
@@ -303,7 +303,7 @@ kvm_close(kvm_t *kd)
 		free(kd->pt_map);
 	if (kd->page_map != NULL)
 		free(kd->page_map);
-	if (kd->sparse_map != MAP_FAILED)
+	if (kd->sparse_map != MAP_FAILED && kd->sparse_map != NULL)
 		munmap(kd->sparse_map, kd->pt_sparse_size);
 	free((void *)kd);
 
