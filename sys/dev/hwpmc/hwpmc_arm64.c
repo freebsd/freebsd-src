freebsd/freebsd-src

commit 2ed63d122dfd74a3f27050539c9bf5398080f6a8

Add support of ARM CMN-600 controller, PMU access functions only. Add support of PMU counters of ARM CMN-600 controller.
Add support of ARM CMN-600 controller, PMU access functions only.
Add support of PMU counters of ARM CMN-600 controller.

Reviewed by: mhorne
Sponsored By: ARM
Differential Revision: https://reviews.freebsd.org/D32321

sys/modules/hwpmc/Makefile
@@ -12,6 +12,7 @@ SRCS+=	vnode_if.h
 
 .if ${MACHINE_CPUARCH} == "aarch64"
 SRCS+=	hwpmc_arm64.c hwpmc_arm64_md.c
+SRCS+=	cmn600.c hwpmc_cmn600.c
 SRCS+=	hwpmc_dmc620.c pmu_dmc620.c
 .endif
 

sys/arm64/include/cmn600_reg.h
@@ -75,6 +75,19 @@ int pmu_cmn600_md8(void *arg, int nodeid, int node_type, off_t reg,
 #define		POR_CFGM_NODE_INFO_NODE_TYPE_MASK	0xffff
 #define		POR_CFGM_NODE_INFO_NODE_TYPE_SHIFT	0
 
+#define		NODE_ID_SUB_MASK		0x3
+#define		NODE_ID_SUB_SHIFT		0
+#define		NODE_ID_PORT_MASK		0x4
+#define		NODE_ID_PORT_SHIFT		2
+#define		NODE_ID_X2B_MASK		(0x3 << 3)
+#define		NODE_ID_X2B_SHIFT		3
+#define		NODE_ID_Y2B_MASK		(0x3 << 5)
+#define		NODE_ID_Y2B_SHIFT		5
+#define		NODE_ID_X3B_MASK		(0x7 << 3)
+#define		NODE_ID_X3B_SHIFT		3
+#define		NODE_ID_Y3B_MASK		(0x7 << 6)
+#define		NODE_ID_Y3B_SHIFT		6
+
 #define	NODE_TYPE_INVALID	0x000
 #define	NODE_TYPE_DVM		0x001
 #define	NODE_TYPE_CFG		0x002
@@ -265,6 +278,8 @@ int pmu_cmn600_md8(void *arg, int nodeid, int node_type, off_t reg,
 #define	POR_DT_PMEVCNTEF			0x2020	/* rw */
 #define	POR_DT_PMEVCNTGH			0x2030	/* rw */
 #define	POR_DT_PMEVCNT(x)			(0x2000 + ((x) * 0x10))
+#define		POR_DT_PMEVCNT_EVENCNT_SHIFT	0
+#define		POR_DT_PMEVCNT_ODDCNT_SHIFT	32
 #define	POR_DT_PMCCNTR				0x2040	/* rw */
 #define	POR_DT_PMEVCNTSRAB			0x2050	/* rw */
 #define	POR_DT_PMEVCNTSRCD			0x2060	/* rw */
@@ -512,6 +527,7 @@ int pmu_cmn600_md8(void *arg, int nodeid, int node_type, off_t reg,
 #define		POR_DTM_PMU_CONFIG_VCNT_INPUT_SEL_SHIFT	32
 #define		POR_DTM_PMU_CONFIG_VCNT_INPUT_SEL_WIDTH	8
 #define	POR_DTM_PMEVCNT				0x2220	/* rw */
+#define		POR_DTM_PMEVCNT_CNTR_WIDTH	16
 #define	POR_DTM_PMEVCNTSR			0x2240	/* rw */
 
 /* RN-D registers */

commit c0cd3d2dcbd1ceac335a9169d027509b061c1b68

arm64: Fix hwpmc module for OPT_ACPI isn't selected
Fixes: 2ed63d122dfd ("Add support of ARM CMN-600 controller ...")
Sponsored by:	Beckhoff Automation GmbH & Co. KG

sys/dev/hwpmc/hwpmc_arm64.c
@@ -38,6 +38,8 @@ __FBSDID("$FreeBSD$");
 #include <machine/pmc_mdep.h>
 #include <machine/cpu.h>
 
+#include "opt_acpi.h"
+
 static int arm64_npmcs;
 
 struct arm64_event_code_map {
@@ -564,11 +566,13 @@ pmc_arm64_initialize(void)
 	/* One AArch64 CPU class */
 	classes = 1;
 
+#ifdef DEV_ACPI
 	/* Query presence of optional classes and set max class. */
 	if (pmc_cmn600_nclasses() > 0)
 		classes = MAX(classes, PMC_MDEP_CLASS_INDEX_CMN600);
 	if (pmc_dmc620_nclasses() > 0)
 		classes = MAX(classes, PMC_MDEP_CLASS_INDEX_DMC620_C);
+#endif
 
 	pmc_mdep = pmc_mdep_alloc(classes);
 
@@ -619,12 +623,14 @@ pmc_arm64_initialize(void)
 
 	pmc_mdep->pmd_npmc   += arm64_npmcs;
 
+#ifdef DEV_ACPI
 	if (pmc_cmn600_nclasses() > 0)
 		pmc_cmn600_initialize(pmc_mdep);
 	if (pmc_dmc620_nclasses() > 0) {
 		pmc_dmc620_initialize_cd2(pmc_mdep);
 		pmc_dmc620_initialize_c(pmc_mdep);
 	}
+#endif
 
 	return (pmc_mdep);
 }

sys/modules/hwpmc/Makefile
@@ -12,8 +12,10 @@ SRCS+=	vnode_if.h
 
 .if ${MACHINE_CPUARCH} == "aarch64"
 SRCS+=	hwpmc_arm64.c hwpmc_arm64_md.c
+.if !empty(OPT_ACPI)
 SRCS+=	cmn600.c hwpmc_cmn600.c
 SRCS+=	hwpmc_dmc620.c pmu_dmc620.c
+.endif
 .endif
 
 .if ${MACHINE_CPUARCH} == "amd64"