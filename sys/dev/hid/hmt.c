f76a02886c3b9a77fb5a5d5bba78a3e8d8a00ee9
Gordon Bergling <gbe@FreeBSD.org>
2022-09-04 13:45:50 +0200

    hid: Remove a double word in a source code comment
    - s/the the/the/
    
    MFC after:	3 days

--- sys/dev/hid/hmt.c
+++ sys/dev/hid/hmt.c
@@ -886,7 +886,7 @@ hmt_set_input_mode(struct hmt_softc *sc, enum hconf_input_mode mode)
 	if (device_get_devclass(hconf) != hconf_devclass)
 		return (ENXIO);
 
-	/* hconf_set_input_mode can drop the the topo lock while sleeping */
+	/* hconf_set_input_mode can drop the topo lock while sleeping */
 	device_busy(hconf);
 	err = hconf_set_input_mode(hconf, mode);
 	device_unbusy(hconf);
