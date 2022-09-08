51f857e2c1f9687b9b2d736dc324c50b602cedd1
Mark Johnston <markj@FreeBSD.org>
2022-09-08 18:48:03 -0400

    bhyve: Address -Wno-unused warnings in atkbd.c
    MFC after:	2 weeks

--- usr.sbin/bhyve/atkbdc.c
+++ usr.sbin/bhyve/atkbdc.c
@@ -307,8 +307,8 @@ atkbdc_dequeue_data(struct atkbdc_softc *sc, uint8_t *buf)
 }
 
 static int
-atkbdc_data_handler(struct vmctx *ctx, int vcpu, int in, int port, int bytes,
-    uint32_t *eax, void *arg)
+atkbdc_data_handler(struct vmctx *ctx __unused, int vcpu __unused, int in,
+    int port __unused, int bytes, uint32_t *eax, void *arg)
 {
 	struct atkbdc_softc *sc;
 	uint8_t buf;
@@ -393,8 +393,8 @@ atkbdc_data_handler(struct vmctx *ctx, int vcpu, int in, int port, int bytes,
 }
 
 static int
-atkbdc_sts_ctl_handler(struct vmctx *ctx, int vcpu, int in, int port,
-    int bytes, uint32_t *eax, void *arg)
+atkbdc_sts_ctl_handler(struct vmctx *ctx, int vcpu __unused, int in,
+    int port __unused, int bytes, uint32_t *eax, void *arg)
 {
 	struct atkbdc_softc *sc;
 	int	error, retval;
