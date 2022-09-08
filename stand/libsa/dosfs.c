freebsd/freebsd-src

commit b4cb3fe0e39a31e60292018dc9fb7510b3de92bf

loader: implement mount/unmount rootfs
We want to keep our root file system open to preserve bcache segment
between file accesses, thus reducing physical disk IO.

Reviewed by:	imp, allanjude, kevans (previous version)
Differential Revision:	https://reviews.freebsd.org/D30848
MFC after:	1 month

stand/libsa/dosfs.c
@@ -38,9 +38,19 @@ __FBSDID("$FreeBSD$");
 #include <stddef.h>
 
 #include "stand.h"
+#include "disk.h"
 
 #include "dosfs.h"
 
+typedef struct dos_mnt {
+	char			*dos_dev;
+	DOS_FS			*dos_fs;
+	int			dos_fd;
+	STAILQ_ENTRY(dos_mnt)	dos_link;
+} dos_mnt_t;
+
+typedef STAILQ_HEAD(dos_mnt_list, dos_mnt) dos_mnt_list_t;
+static dos_mnt_list_t mnt_list = STAILQ_HEAD_INITIALIZER(mnt_list);
 
 static int	dos_open(const char *path, struct open_file *fd);
 static int	dos_close(struct open_file *fd);
@@ -48,16 +58,20 @@ static int	dos_read(struct open_file *fd, void *buf, size_t size, size_t *resid)
 static off_t	dos_seek(struct open_file *fd, off_t offset, int whence);
 static int	dos_stat(struct open_file *fd, struct stat *sb);
 static int	dos_readdir(struct open_file *fd, struct dirent *d);
+static int	dos_mount(const char *dev, const char *path, void **data);
+static int	dos_unmount(const char *dev, void *data);
 
 struct fs_ops dosfs_fsops = {
-	"dosfs",
-	dos_open,
-	dos_close,
-	dos_read,
-	null_write,
-	dos_seek,
-	dos_stat,
-	dos_readdir
+	.fs_name = "dosfs",
+	.fo_open = dos_open,
+	.fo_close = dos_close,
+	.fo_read = dos_read,
+	.fo_write = null_write,
+	.fo_seek = dos_seek,
+	.fo_stat = dos_stat,
+	.fo_readdir = dos_readdir,
+	.fo_mount = dos_mount,
+	.fo_unmount = dos_unmount
 };
 
 #define SECSIZ  512             /* sector size */
@@ -179,12 +193,11 @@ dos_read_fatblk(DOS_FS *fs, struct open_file *fd, u_int blknum)
  * Mount DOS filesystem
  */
 static int
-dos_mount(DOS_FS *fs, struct open_file *fd)
+dos_mount_impl(DOS_FS *fs, struct open_file *fd)
 {
 	int err;
 	u_char *buf;
 
-	bzero(fs, sizeof(DOS_FS));
 	fs->fd = fd;
 
 	if ((buf = malloc(secbyt(1))) == NULL)
@@ -215,11 +228,70 @@ dos_mount(DOS_FS *fs, struct open_file *fd)
 	return (0);
 }
 
+static int
+dos_mount(const char *dev, const char *path, void **data)
+{
+	char *fs;
+	dos_mnt_t *mnt;
+	struct open_file *f;
+	DOS_FILE *df;
+
+	errno = 0;
+	mnt = calloc(1, sizeof(*mnt));
+	if (mnt == NULL)
+		return (errno);
+	mnt->dos_fd = -1;
+	mnt->dos_dev = strdup(dev);
+	if (mnt->dos_dev == NULL)
+		goto done;
+
+	if (asprintf(&fs, "%s%s", dev, path) < 0)
+		goto done;
+
+	mnt->dos_fd = open(fs, O_RDONLY);
+	free(fs);
+	if (mnt->dos_fd == -1)
+		goto done;
+
+	f = fd2open_file(mnt->dos_fd);
+	if (strcmp(f->f_ops->fs_name, "dosfs") == 0) {
+		df = f->f_fsdata;
+		mnt->dos_fs = df->fs;
+		STAILQ_INSERT_TAIL(&mnt_list, mnt, dos_link);
+	} else {
+                errno = ENXIO;
+	}
+
+done:
+	if (errno != 0) {
+		free(mnt->dos_dev);
+		if (mnt->dos_fd >= 0)
+			close(mnt->dos_fd);
+		free(mnt);
+	} else {
+		*data = mnt;
+	}
+
+	return (errno);
+}
+
+static int
+dos_unmount(const char *dev __unused, void *data)
+{
+	dos_mnt_t *mnt = data;
+
+	STAILQ_REMOVE(&mnt_list, mnt, dos_mnt, dos_link);
+	free(mnt->dos_dev);
+	close(mnt->dos_fd);
+	free(mnt);
+	return (0);
+}
+
 /*
  * Unmount mounted filesystem
  */
 static int
-dos_unmount(DOS_FS *fs)
+dos_unmount_impl(DOS_FS *fs)
 {
 	if (fs->links)
 		return (EBUSY);
@@ -237,19 +309,32 @@ dos_open(const char *path, struct open_file *fd)
 	DOS_DE *de;
 	DOS_FILE *f;
 	DOS_FS *fs;
+	dos_mnt_t *mnt;
+	const char *dev;
 	u_int size, clus;
 	int err;
 
-	/* Allocate mount structure, associate with open */
-	if ((fs = malloc(sizeof(DOS_FS))) == NULL)
-		return (errno);
-	if ((err = dos_mount(fs, fd))) {
-		free(fs);
-		return (err);
+	dev = disk_fmtdev(fd->f_devdata);
+	STAILQ_FOREACH(mnt, &mnt_list, dos_link) {
+		if (strcmp(dev, mnt->dos_dev) == 0)
+			break;
+	}
+
+	if (mnt == NULL) {
+		/* Allocate mount structure, associate with open */
+		if ((fs = malloc(sizeof(DOS_FS))) == NULL)
+			return (errno);
+		if ((err = dos_mount_impl(fs, fd))) {
+			free(fs);
+			return (err);
+		}
+	} else {
+		fs = mnt->dos_fs;
 	}
 
 	if ((err = namede(fs, path, &de))) {
-		dos_unmount(fs);
+		if (mnt == NULL)
+			dos_unmount_impl(fs);
 		return (err);
 	}
 
@@ -259,19 +344,20 @@ dos_open(const char *path, struct open_file *fd)
 	if ((!(de->attr & FA_DIR) && (!clus != !size)) ||
 	    ((de->attr & FA_DIR) && size) ||
 	    (clus && !okclus(fs, clus))) {
-		dos_unmount(fs);
+		if (mnt == NULL)
+			dos_unmount_impl(fs);
 		return (EINVAL);
 	}
-	if ((f = malloc(sizeof(DOS_FILE))) == NULL) {
+	if ((f = calloc(1, sizeof(DOS_FILE))) == NULL) {
 		err = errno;
-		dos_unmount(fs);
+		if (mnt == NULL)
+			dos_unmount_impl(fs);
 		return (err);
 	}
-	bzero(f, sizeof(DOS_FILE));
 	f->fs = fs;
 	fs->links++;
 	f->de = *de;
-	fd->f_fsdata = (void *)f;
+	fd->f_fsdata = f;
 	return (0);
 }
 
@@ -381,7 +467,7 @@ dos_close(struct open_file *fd)
 
 	f->fs->links--;
 	free(f);
-	dos_unmount(fs);
+	dos_unmount_impl(fs);
 	return (0);
 }
 

stand/common/bootstrap.h
@@ -29,6 +29,7 @@
 #ifndef _BOOTSTRAP_H_
 #define	_BOOTSTRAP_H_
 
+#include <stand.h>
 #include <sys/types.h>
 #include <sys/queue.h>
 #include <sys/linker_set.h>
@@ -399,6 +400,9 @@ int nvstore_set_var(void *, int, const char *, void *, size_t);
 int nvstore_set_var_from_string(void *, const char *, const char *,
     const char *);
 int nvstore_unset_var(void *, const char *);
+
+/* common code to set currdev variable. */
+extern int mount_currdev(struct env_var *, int, const void *);
 
 #ifndef CTASSERT
 #define	CTASSERT(x)	_Static_assert(x, "compile-time assertion failed")

stand/common/misc.c
@@ -179,3 +179,25 @@ dev_cleanup(void)
 	if (devsw[i]->dv_cleanup != NULL)
 	    (devsw[i]->dv_cleanup)();
 }
+
+/*
+ * mount new rootfs and unmount old, set "currdev" environment variable.
+ */
+int mount_currdev(struct env_var *ev, int flags, const void *value)
+{
+	int rv;
+
+	/* mount new rootfs */
+	rv = mount(value, "/", 0, NULL);
+	if (rv == 0) {
+		/*
+		 * Note we unmount any previously mounted fs only after
+		 * successfully mounting the new because we do not want to
+		 * end up with unmounted rootfs.
+		 */
+		if (ev->ev_value != NULL)
+			unmount(ev->ev_value, 0);
+		env_setenv(ev->ev_name, flags | EV_NOHOOK, value, NULL, NULL);
+	}
+	return (rv);
+}

stand/efi/boot1/zfs_module.c
@@ -160,7 +160,7 @@ load(const char *filepath, dev_info_t *devinfo, void **bufp, size_t *bufsize)
 		return (EFI_NOT_FOUND);
 	}
 
-	if ((err = zfs_mount(spa, 0, &zmount)) != 0) {
+	if ((err = zfs_mount_impl(spa, 0, &zmount)) != 0) {
 		DPRINTF("Failed to mount pool '%s' (%d)\n", spa->spa_name, err);
 		return (EFI_NOT_FOUND);
 	}

stand/efi/libefi/devicename.c
@@ -209,8 +209,7 @@ efi_setcurrdev(struct env_var *ev, int flags, const void *value)
 	rv = efi_parsedev(&ncurr, value, NULL);
 	if (rv != 0)
 		return (rv);
-
 	free(ncurr);
-	env_setenv(ev->ev_name, flags | EV_NOHOOK, value, NULL, NULL);
-	return (0);
+
+	return (mount_currdev(ev, flags, value));
 }

stand/efi/loader/main.c
@@ -187,15 +187,12 @@ static void
 set_currdev(const char *devname)
 {
 
-	/*
-	 * Don't execute hooks here; we may need to try setting these more than
-	 * once here if we're probing for the ZFS pool we're supposed to boot.
-	 * The currdev hook is intended to just validate user input anyways,
-	 * while the loaddev hook makes it immutable once we've determined what
-	 * the proper currdev is.
-	 */
-	env_setenv("currdev", EV_VOLATILE | EV_NOHOOK, devname, efi_setcurrdev,
+	env_setenv("currdev", EV_VOLATILE, devname, efi_setcurrdev,
 	    env_nounset);
+	/*
+	 * Don't execute hook here; the loaddev hook makes it immutable
+	 * once we've determined what the proper currdev is.
+	 */
 	env_setenv("loaddev", EV_VOLATILE | EV_NOHOOK, devname, env_noset,
 	    env_nounset);
 }
@@ -957,6 +954,9 @@ main(int argc, CHAR16 *argv[])
 		setenv("console", "comconsole", 1);
 #endif
 	cons_probe();
+
+	/* Set up currdev variable to have hooks in place. */
+	env_setenv("currdev", EV_VOLATILE, "", efi_setcurrdev, env_nounset);
 
 	/* Init the time source */
 	efi_time_init();

stand/i386/gptzfsboot/Makefile
@@ -66,7 +66,7 @@ gptldr.bin: gptldr.out
 gptldr.out: gptldr.o
 	${LD} ${LD_FLAGS} -e start --defsym ORG=${ORG1} -T ${LDSCRIPT} -o ${.TARGET} gptldr.o
 
-OBJS=	zfsboot.o sio.o cons.o bcache.o devopen.o disk.o part.o zfs_cmd.o
+OBJS=	zfsboot.o sio.o cons.o bcache.o devopen.o disk.o part.o zfs_cmd.o misc.o
 CLEANFILES+=	gptzfsboot.bin gptzfsboot.out ${OBJS} ${OPENCRYPTO_XTS}
 
 # i386 standalone support library

stand/i386/libi386/devicename.c
@@ -204,12 +204,12 @@ i386_fmtdev(void *vdev)
 int
 i386_setcurrdev(struct env_var *ev, int flags, const void *value)
 {
-    struct i386_devdesc	*ncurr;
-    int			rv;
+	struct i386_devdesc	*ncurr;
+	int			rv;
 
-    if ((rv = i386_parsedev(&ncurr, value, NULL)) != 0)
-	return(rv);
-    free(ncurr);
-    env_setenv(ev->ev_name, flags | EV_NOHOOK, value, NULL, NULL);
-    return(0);
+	if ((rv = i386_parsedev(&ncurr, value, NULL)) != 0)
+		return (rv);
+	free(ncurr);
+
+	return (mount_currdev(ev, flags, value));
 }

commit 0eb736c0f673d2804a0c8c14fa1e4eae228ab6d2

stand: Unbreak FAT32 in loader
This corrects an issue introduced in b4cb3fe0e39a3, where a freshly
allocated `DOS_FS` structure would not be initialized properly before
use in `dos_open`.

In case of FAT32 file systems, this would leave `fs->dirents`
uninitialized and - depending on its content and due to checks in
`parsebs` - prevent mounting the file system successfully.

This particularily impacted the EFI loader, as it was sometimes not
able to read files from a FAT32-formatted EFI partition, including
LoaderEnv (`/efi/freebsd/loader.env`).

Accepted by:	imp
MFC after:	1 week
Differential Revision:	https://reviews.freebsd.org/D36482

stand/libsa/dosfs.c
@@ -321,7 +321,7 @@ dos_open(const char *path, struct open_file *fd)
 
 	if (mnt == NULL) {
 		/* Allocate mount structure, associate with open */
-		if ((fs = malloc(sizeof(DOS_FS))) == NULL)
+		if ((fs = calloc(1, sizeof(DOS_FS))) == NULL)
 			return (errno);
 		if ((err = dos_mount_impl(fs, fd))) {
 			free(fs);