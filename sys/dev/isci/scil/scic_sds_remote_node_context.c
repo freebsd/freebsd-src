b4aa7961b1dbcff0dc10f02884394e26607db3c9
Gordon Bergling <gbe@FreeBSD.org>
2022-09-10 13:04:42 +0200

    iscsi(4): Remove a double word in a source code comment
     - s/to to/to/
    
    MFC after:	3 days

--- sys/dev/isci/scil/scic_sds_remote_node_context.c
+++ sys/dev/isci/scil/scic_sds_remote_node_context.c
@@ -963,7 +963,7 @@ SCI_STATUS scic_sds_remote_node_context_await_suspension_state_resume_handler(
 *
 * @param[in] this_rnc The remote node context which is to receive the task
 *       request.
-* @param[in] the_request The task request to be transmitted to to the remote
+* @param[in] the_request The task request to be transmitted to the remote
 *       target device.
 *
 * @return SCI_STATUS
