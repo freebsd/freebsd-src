c71b8367dec5b8eb70cfedf10f276876e3d2005b
Konstantin Belousov <kib@FreeBSD.org>
2022-09-12 22:36:24 +0300

    _elf_aux_info(3): add support for AT_USRSTACK{BASE,LIM}
    (cherry picked from commit 8f2668b0605e26b04a103f63096cfcc856d950c1)

--- lib/libc/gen/auxv.c
+++ lib/libc/gen/auxv.c
@@ -73,6 +73,7 @@ static char *canary, *pagesizes, *execpath;
 static void *ps_strings, *timekeep;
 static u_long hwcap, hwcap2;
 static void *fxrng_seed_version;
+static u_long usrstackbase, usrstacklim;
 
 #ifdef __powerpc__
 static int powerpc_new_auxv_format = 0;
@@ -143,6 +144,14 @@ init_aux(void)
 
 		case AT_FXRNG:
 			fxrng_seed_version = aux->a_un.a_ptr;
+			break;
+
+		case AT_USRSTACKBASE:
+			usrstackbase = aux->a_un.a_val;
+			break;
+
+		case AT_USRSTACKLIM:
+			usrstacklim = aux->a_un.a_val;
 			break;
 #ifdef __powerpc__
 		/*
@@ -367,6 +376,20 @@ _elf_aux_info(int aux, void *buf, int buflen)
 				res = 0;
 			} else
 				res = ENOENT;
+		} else
+			res = EINVAL;
+		break;
+	case AT_USRSTACKBASE:
+		if (buflen == sizeof(u_long)) {
+			*(u_long *)buf = usrstackbase;
+			res = 0;
+		} else
+			res = EINVAL;
+		break;
+	case AT_USRSTACKLIM:
+		if (buflen == sizeof(u_long)) {
+			*(u_long *)buf = usrstacklim;
+			res = 0;
 		} else
 			res = EINVAL;
 		break;
