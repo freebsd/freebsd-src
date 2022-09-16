52804bde1baf251701e345879dff30205abdc6de
Warner Losh <imp@FreeBSD.org>
2022-09-16 09:08:42 -0600

    stand: Create common/modinfo.h
    Move all the MOD_xxx macros to this header. Each user of this interface
    is currently required to define MOD_ALIGNMENT(l). modinfo was selected
    because it sits inbetween modules and metadata and will make it easier
    to migrate to new, shared intefaces.
    
    Sponsored by:		Netflix
    Differential Revision:	https://reviews.freebsd.org/D36571
    
    (cherry picked from commit bca9c87b6104219af35ae5ea4a6af098a1631bca)

--- stand/common/metadata.c
+++ stand/common/metadata.c
@@ -44,6 +44,7 @@ __FBSDID("$FreeBSD$");
 #include <machine/metadata.h>
 
 #include "bootstrap.h"
+#include "modinfo.h"
 
 #ifdef LOADER_GELI_SUPPORT
 #include "geliboot.h"
@@ -92,68 +93,8 @@ md_copyenv(vm_offset_t addr)
     return(addr);
 }
 
-/*
- * Copy module-related data into the load area, where it can be
- * used as a directory for loaded modules.
- *
- * Module data is presented in a self-describing format.  Each datum
- * is preceded by a 32-bit identifier and a 32-bit size field.
- *
- * Currently, the following data are saved:
- *
- * MOD_NAME	(variable)		module name (string)
- * MOD_TYPE	(variable)		module type (string)
- * MOD_ARGS	(variable)		module parameters (string)
- * MOD_ADDR	sizeof(vm_offset_t)	module load address
- * MOD_SIZE	sizeof(size_t)		module size
- * MOD_METADATA	(variable)		type-specific metadata
- */
-
 static int align;
-
 #define MOD_ALIGN(l)	roundup(l, align)
-#define COPY32(v, a, c) {			\
-    uint32_t	x = (v);			\
-    if (c)					\
-        archsw.arch_copyin(&x, a, sizeof(x));	\
-    a += sizeof(x);				\
-}
-
-#define MOD_STR(t, a, s, c) {			\
-    COPY32(t, a, c);				\
-    COPY32(strlen(s) + 1, a, c)			\
-    if (c)					\
-        archsw.arch_copyin(s, a, strlen(s) + 1);\
-    a += MOD_ALIGN(strlen(s) + 1);		\
-}
-
-#define MOD_NAME(a, s, c)	MOD_STR(MODINFO_NAME, a, s, c)
-#define MOD_TYPE(a, s, c)	MOD_STR(MODINFO_TYPE, a, s, c)
-#define MOD_ARGS(a, s, c)	MOD_STR(MODINFO_ARGS, a, s, c)
-
-#define MOD_VAR(t, a, s, c) {			\
-    COPY32(t, a, c);				\
-    COPY32(sizeof(s), a, c);			\
-    if (c)					\
-        archsw.arch_copyin(&s, a, sizeof(s));	\
-    a += MOD_ALIGN(sizeof(s));			\
-}
-
-#define MOD_ADDR(a, s, c)	MOD_VAR(MODINFO_ADDR, a, s, c)
-#define MOD_SIZE(a, s, c)	MOD_VAR(MODINFO_SIZE, a, s, c)
-
-#define MOD_METADATA(a, mm, c) {		\
-    COPY32(MODINFO_METADATA | mm->md_type, a, c);\
-    COPY32(mm->md_size, a, c);			\
-    if (c)					\
-        archsw.arch_copyin(mm->md_data, a, mm->md_size);\
-    a += MOD_ALIGN(mm->md_size);		\
-}
-
-#define MOD_END(a, c) {				\
-    COPY32(MODINFO_END, a, c);			\
-    COPY32(0, a, c);				\
-}
 
 static vm_offset_t
 md_copymodules(vm_offset_t addr, int kern64)
