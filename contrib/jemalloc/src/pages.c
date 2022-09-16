86acbefbf6e560413b2f4f6d49846fbe9d4692e4
Konstantin Belousov <kib@FreeBSD.org>
2022-09-17 00:08:03 +0300

    jemalloc: Define SWAP_RESERVE_* if not provided by the system headers
    instead ifdef-ing out the return statement.
    
    Reported by:	Mark Millard <marklmi@yahoo.com>
    Fixes:	200303384403
    Sponsored by:	The FreeBSD Foundation
    MFC after:	2 weeks

--- contrib/jemalloc/src/pages.c
+++ contrib/jemalloc/src/pages.c
@@ -464,10 +464,12 @@ os_overcommits_sysctl(void) {
 	}
 #endif
 
-#ifdef SWAP_RESERVE_FORCE_ON
+#ifndef SWAP_RESERVE_FORCE_ON
+#define	SWAP_RESERVE_FORCE_ON		(1 << 0)
+#define	SWAP_RESERVE_RLIMIT_ON		(1 << 1)
+#endif
 	return ((vm_overcommit & (SWAP_RESERVE_FORCE_ON |
 	    SWAP_RESERVE_RLIMIT_ON)) == 0);
-#endif
 }
 #endif
 
