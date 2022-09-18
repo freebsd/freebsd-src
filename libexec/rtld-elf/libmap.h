eeabe7d97285ac5269f6aeec685461bb4c050840
Konstantin Belousov <kib@FreeBSD.org>
2022-09-19 01:05:06 +0300

    rtld-elf/libmap.h: add include guard
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week

--- libexec/rtld-elf/libmap.h
+++ libexec/rtld-elf/libmap.h
@@ -2,7 +2,12 @@
  * $FreeBSD$
  */
 
+#ifndef LIBMAP_H
+#define	LIBMAP_H
+
 int	lm_init(const char *);
 void	lm_fini (void);
 char *	lm_find (const char *, const char *);
 char *	lm_findn (const char *, const char *, const size_t);
+
+#endif
