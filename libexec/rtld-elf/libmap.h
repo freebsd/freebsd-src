a45c557366abb187095fa8acaca23b5ad8ad3c06
Konstantin Belousov <kib@FreeBSD.org>
2022-09-19 01:06:33 +0300

    rtld-elf/libmap.h: style declarations
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week

--- libexec/rtld-elf/libmap.h
+++ libexec/rtld-elf/libmap.h
@@ -6,8 +6,8 @@
 #define	LIBMAP_H
 
 int	lm_init(const char *);
-void	lm_fini (void);
-char *	lm_find (const char *, const char *);
-char *	lm_findn (const char *, const char *, const size_t);
+void	lm_fini(void);
+char	*lm_find(const char *, const char *);
+char	*lm_findn(const char *, const char *, const size_t);
 
 #endif
