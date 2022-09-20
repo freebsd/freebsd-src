d5a64a935bc92a89d61aafbb66acbf0e92980cb5
Dmitry Salychev <dsl@FreeBSD.org>
2022-09-20 11:47:41 +0200

    Add initial DPAA2 support
    DPAA2 is a hardware-level networking architecture found in some NXP
    SoCs which contain hardware blocks including Management Complex
    (MC, a command interface to manipulate DPAA2 objects), Wire Rate I/O
    processor (WRIOP, packets distribution, queuing, drop decisions),
    Queues and Buffers Manager (QBMan, Rx/Tx queues control, Rx buffer
    pools) and the others.
    
    The Management Complex runs NXP-supplied firmware which provides DPAA2
    objects as an abstraction layer over those blocks to simplify an
    access to the underlying hardware. Each DPAA2 object has its own
    driver (to perform an initialization at least) and will be visible
    as a separate device in the device tree.
    
    Two new drivers (dpaa2_mc and dpaa2_rc) act like firmware buses in
    order to form a hierarchy of the DPAA2 devices:
    
    	acpiX (or simplebusX)
    	  dpaa2_mcX
    	    dpaa2_rcX
    	      dpaa2_mcp0
    	      ...
    	      dpaa2_mcpN
    	      dpaa2_bpX
    	      dpaa2_macX
    	      dpaa2_io0
    	      ...
    	      dpaa2_ioM
    	      dpaa2_niX
    
    dpaa2_mc is suppossed to be a root of the hierarchy, comes in ACPI
    and FDT flavours and implements helper interfaces to allocate and
    assign bus resources, MSI and "managed" DPAA2 devices (NXP treats some
    of the objects as resources for the other DPAA2 objects to let them
    function properly). Almost all of the DPAA2 objects are assigned to
    the resource containers (dpaa2_rc) to implement isolation.
    
    The initial implementation focuses on the DPAA2 network interface
    to be operational. It is the most complex object in terms of
    dependencies which uses I/O objects to transmit/receive packets.
    
    Approved by:		bz (mentor)
    Tested by:		manu, bz
    Differential Revision:	https://reviews.freebsd.org/D36638
    
    (cherry picked from commit ba7319e9091b4f6ef15a9c4be3d3d076f3047f72)
    (cherry picked from commit 7fb975c8fb970b35fc34561ed30a0fe220346cb6)
    (cherry picked from commit 2782ed8f6cd3d7f59219a783bc7fa7bbfb1fe26f)
    (cherry picked from commit d6eabdac2ef444b62aba186c793fbd5d4226b157)
    (cherry picked from commit 16295b0a5a577aa70f47d3b3314277e631caee63)
    (cherry picked from commit ff270fce1631cdfbf8d3d76b9a9f5342b78972c9)

---
+++ sys/dev/dpaa2/dpaa2_mc.h
@@ -0,0 +1,218 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright © 2021-2022 Dmitry Salychev
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef	_DPAA2_MC_H
+#define	_DPAA2_MC_H
+
+#include <sys/rman.h>
+#include <sys/bus.h>
+#include <sys/queue.h>
+
+#include <net/ethernet.h>
+
+#include <dev/ofw/openfirm.h>
+
+#include "pci_if.h"
+
+#include "dpaa2_types.h"
+#include "dpaa2_mcp.h"
+#include "dpaa2_swp.h"
+#include "dpaa2_ni.h"
+#include "dpaa2_io.h"
+#include "dpaa2_mac.h"
+#include "dpaa2_con.h"
+#include "dpaa2_bp.h"
+
+/*
+ * Maximum number of MSIs supported by the MC for its children without IOMMU.
+ *
+ * TODO: Should be much more with IOMMU translation.
+ */
+#define DPAA2_MC_MSI_COUNT	 32
+
+/* Flags for DPAA2 devices as resources. */
+#define DPAA2_MC_DEV_ALLOCATABLE 0x01u /* to be managed by DPAA2-specific rman */
+#define DPAA2_MC_DEV_ASSOCIATED	 0x02u /* to obtain info about DPAA2 device  */
+#define DPAA2_MC_DEV_SHAREABLE	 0x04u /* to be shared among DPAA2 devices */
+
+struct dpaa2_mc_devinfo; /* about managed DPAA2 devices */
+
+/**
+ * @brief Software context for the DPAA2 Management Complex (MC) driver.
+ *
+ * dev:		Device associated with this software context.
+ * rcdev:	Child device associated with the root resource container.
+ * acpi_based:	Attached using ACPI (true) or FDT (false).
+ * ofw_node:	FDT node of the Management Complex (acpi_based == false).
+ *
+ * res:		Unmapped MC command portal and control registers resources.
+ * map:		Mapped MC command portal and control registers resources.
+ *
+ * dpio_rman:	I/O objects resource manager.
+ * dpbp_rman:	Buffer Pools resource manager.
+ * dpcon_rman:	Concentrators resource manager.
+ * dpmcp_rman:	MC portals resource manager.
+ */
+struct dpaa2_mc_softc {
+	device_t		 dev;
+	device_t		 rcdev;
+	bool			 acpi_based;
+	phandle_t		 ofw_node;
+
+	struct resource 	*res[2];
+	struct resource_map	 map[2];
+
+	/* For allocatable managed DPAA2 objects. */
+	struct rman		 dpio_rman;
+	struct rman		 dpbp_rman;
+	struct rman		 dpcon_rman;
+	struct rman		 dpmcp_rman;
+
+	/* For managed DPAA2 objects. */
+	struct mtx		 mdev_lock;
+	STAILQ_HEAD(, dpaa2_mc_devinfo) mdev_list;
+
+	/* NOTE: Workaround in case of no IOMMU available. */
+#ifndef IOMMU
+	device_t		 msi_owner;
+	bool			 msi_allocated;
+	struct mtx		 msi_lock;
+	struct {
+		device_t	 child;
+		int		 irq;
+	} msi[DPAA2_MC_MSI_COUNT];
+#endif
+};
+
+/**
+ * @brief Software context for the DPAA2 Resource Container (RC) driver.
+ *
+ * dev:		Device associated with this software context.
+ * portal:	Helper object to send commands to the MC portal.
+ * unit:	Helps to distinguish between root (0) and child DRPCs.
+ * cont_id:	Container ID.
+ */
+struct dpaa2_rc_softc {
+	device_t		 dev;
+	int			 unit;
+	uint32_t		 cont_id;
+};
+
+/**
+ * @brief Information about MSI messages supported by the DPAA2 object.
+ *
+ * msi_msgnum:	 Number of MSI messages supported by the DPAA2 object.
+ * msi_alloc:	 Number of MSI messages allocated for the DPAA2 object.
+ * msi_handlers: Number of MSI message handlers configured.
+ */
+struct dpaa2_msinfo {
+	uint8_t			 msi_msgnum;
+	uint8_t			 msi_alloc;
+	uint32_t		 msi_handlers;
+};
+
+/**
+ * @brief Information about DPAA2 device.
+ *
+ * pdev:	Parent device.
+ * dev:		Device this devinfo is associated with.
+ *
+ * id:		ID of a logical DPAA2 object resource.
+ * portal_id:	ID of the MC portal which belongs to the object's container.
+ * icid:	Isolation context ID of the DPAA2 object. It is shared
+ *		between a resource container and all of its children.
+ *
+ * dtype:	Type of the DPAA2 object.
+ * resources:	Resources available for this DPAA2 device.
+ * msi:		Information about MSI messages supported by the DPAA2 object.
+ */
+struct dpaa2_devinfo {
+	device_t		 pdev;
+	device_t		 dev;
+
+	uint32_t		 id;
+	uint32_t		 portal_id;
+	uint32_t		 icid;
+
+	enum dpaa2_dev_type	 dtype;
+	struct resource_list	 resources;
+	struct dpaa2_msinfo	 msi;
+
+	/*
+	 * DPAA2 object might or might not have its own portal allocated to
+	 * execute MC commands. If the portal has been allocated, it takes
+	 * precedence over the portal owned by the resource container.
+	 */
+	struct dpaa2_mcp	*portal;
+};
+
+DECLARE_CLASS(dpaa2_mc_driver);
+
+/* For device interface. */
+
+int dpaa2_mc_attach(device_t dev);
+int dpaa2_mc_detach(device_t dev);
+
+/* For bus interface. */
+
+struct resource * dpaa2_mc_alloc_resource(device_t mcdev, device_t child,
+    int type, int *rid, rman_res_t start, rman_res_t end, rman_res_t count,
+    u_int flags);
+int dpaa2_mc_adjust_resource(device_t mcdev, device_t child, int type,
+    struct resource *r, rman_res_t start, rman_res_t end);
+int dpaa2_mc_release_resource(device_t mcdev, device_t child, int type,
+    int rid, struct resource *r);
+int dpaa2_mc_activate_resource(device_t mcdev, device_t child, int type,
+    int rid, struct resource *r);
+int dpaa2_mc_deactivate_resource(device_t mcdev, device_t child, int type,
+    int rid, struct resource *r);
+
+/* For pseudo-pcib interface. */
+
+int dpaa2_mc_alloc_msi(device_t mcdev, device_t child, int count, int maxcount,
+    int *irqs);
+int dpaa2_mc_release_msi(device_t mcdev, device_t child, int count, int *irqs);
+int dpaa2_mc_map_msi(device_t mcdev, device_t child, int irq, uint64_t *addr,
+    uint32_t *data);
+int dpaa2_mc_get_id(device_t mcdev, device_t child, enum pci_id_type type,
+    uintptr_t *id);
+
+/* For DPAA2 MC bus interface. */
+
+int dpaa2_mc_manage_dev(device_t mcdev, device_t dpaa2_dev, uint32_t flags);
+int dpaa2_mc_get_free_dev(device_t mcdev, device_t *dpaa2_dev,
+    enum dpaa2_dev_type devtype);
+int dpaa2_mc_get_dev(device_t mcdev, device_t *dpaa2_dev,
+    enum dpaa2_dev_type devtype, uint32_t obj_id);
+int dpaa2_mc_get_shared_dev(device_t mcdev, device_t *dpaa2_dev,
+    enum dpaa2_dev_type devtype);
+int dpaa2_mc_reserve_dev(device_t mcdev, device_t dpaa2_dev,
+    enum dpaa2_dev_type devtype);
+int dpaa2_mc_release_dev(device_t mcdev, device_t dpaa2_dev,
+    enum dpaa2_dev_type devtype);
+
+#endif /* _DPAA2_MC_H */
---
+++ sys/dev/dpaa2/memac_mdio_common.c
@@ -0,0 +1,306 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright © 2021-2022 Bjoern A. Zeeb
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/param.h>
+#include <sys/kernel.h>
+#include <sys/bus.h>
+#include <sys/rman.h>
+#include <sys/endian.h>
+#include <sys/socket.h>
+
+#include <machine/bus.h>
+#include <machine/resource.h>
+
+#include <net/if.h>
+#include <net/if_var.h>
+#include <net/if_media.h>
+
+#include <dev/mii/mii.h>
+#include <dev/mii/miivar.h>
+
+#include "memac_mdio.h"
+#include "miibus_if.h"
+
+/* #define	MEMAC_MDIO_DEBUG */
+
+/* -------------------------------------------------------------------------- */
+
+int
+memacphy_miibus_readreg(device_t dev, int phy, int reg)
+{
+
+	return (MIIBUS_READREG(device_get_parent(dev), phy, reg));
+}
+
+int
+memacphy_miibus_writereg(device_t dev, int phy, int reg, int data)
+{
+
+	return (MIIBUS_WRITEREG(device_get_parent(dev), phy, reg, data));
+}
+
+void
+memacphy_miibus_statchg(struct memacphy_softc_common *sc)
+{
+
+	if (sc->dpnidev != NULL)
+		MIIBUS_STATCHG(sc->dpnidev);
+}
+
+int
+memacphy_set_ni_dev(struct memacphy_softc_common *sc, device_t nidev)
+{
+
+	if (nidev == NULL)
+		return (EINVAL);
+
+#if defined(MEMAC_MDIO_DEBUG)
+	if (bootverbose)
+		device_printf(sc->dev, "setting nidev %p (%s)\n",
+		    nidev, device_get_nameunit(nidev));
+#endif
+
+	if (sc->dpnidev != NULL)
+		return (EBUSY);
+
+	sc->dpnidev = nidev;
+	return (0);
+}
+
+int
+memacphy_get_phy_loc(struct memacphy_softc_common *sc, int *phy_loc)
+{
+	int error;
+
+	if (phy_loc == NULL)
+		return (EINVAL);
+
+	if (sc->phy == -1) {
+		*phy_loc = MII_PHY_ANY;
+		error = ENODEV;
+	} else {
+		*phy_loc = sc->phy;
+		error = 0;
+	}
+
+#if defined(MEMAC_MDIO_DEBUG)
+	if (bootverbose)
+		device_printf(sc->dev, "returning phy_loc %d, error %d\n",
+		    *phy_loc, error);
+#endif
+
+	return (error);
+}
+
+/* -------------------------------------------------------------------------- */
+
+/*
+ * MDIO Ethernet Management Interface Registers (internal PCS MDIO PHY)
+ * 0x0030	MDIO Configuration Register (MDIO_CFG)
+ * 0x0034	MDIO Control Register (MDIO_CTL)
+ * 0x0038	MDIO Data Register (MDIO_DATA)
+ * 0x003c	MDIO Register Address Register (MDIO_ADDR)
+ *
+ * External MDIO interfaces
+ * 0x0030	External MDIO Configuration Register (EMDIO_CFG)
+ * 0x0034	External MDIO Control Register (EMDIO_CTL)
+ * 0x0038	External MDIO Data Register (EMDIO_DATA)
+ * 0x003c	External MDIO Register Address Register (EMDIO_ADDR)
+ */
+#define	MDIO_CFG			0x00030
+#define	MDIO_CFG_MDIO_RD_ER		(1 << 1)
+#define	MDIO_CFG_ENC45			(1 << 6)
+#define	MDIO_CFG_BUSY			(1 << 31)
+#define	MDIO_CTL			0x00034
+#define	MDIO_CTL_READ			(1 << 15)
+#define	MDIO_CTL_PORT_ADDR(_x)		(((_x) & 0x1f) << 5)
+#define	MDIO_CTL_DEV_ADDR(_x)		((_x) & 0x1f)
+#define	MDIO_DATA			0x00038
+#define	MDIO_ADDR			0x0003c
+
+static uint32_t
+memac_read_4(struct memac_mdio_softc_common *sc, uint32_t reg)
+{
+	uint32_t v, r;
+
+	v = bus_read_4(sc->mem_res, reg);
+	if (sc->is_little_endian)
+		r = le32toh(v);
+	else
+		r = be32toh(v);
+
+	return (r);
+}
+
+static void
+memac_write_4(struct memac_mdio_softc_common *sc, uint32_t reg, uint32_t val)
+{
+	uint32_t v;
+
+	if (sc->is_little_endian)
+		v = htole32(val);
+	else
+		v = htobe32(val);
+	bus_write_4(sc->mem_res, reg, v);
+}
+
+static uint32_t
+memac_miibus_wait_no_busy(struct memac_mdio_softc_common *sc)
+{
+	uint32_t count, val;
+
+	for (count = 1000; count > 0; count--) {
+		val = memac_read_4(sc, MDIO_CFG);
+		if ((val & MDIO_CFG_BUSY) == 0)
+			break;
+		DELAY(1);
+	}
+
+	if (count == 0)
+		return (0xffff);
+
+	return (0);
+}
+
+int
+memac_miibus_readreg(struct memac_mdio_softc_common *sc, int phy, int reg)
+{
+	uint32_t cfg, ctl, val;
+
+	/* Set proper Clause 45 mode. */
+	cfg = memac_read_4(sc, MDIO_CFG);
+	/* XXX 45 support? */
+	cfg &= ~MDIO_CFG_ENC45;	/* Use Clause 22 */
+	memac_write_4(sc, MDIO_CFG, cfg);
+
+	val = memac_miibus_wait_no_busy(sc);
+	if (val != 0)
+		return (0xffff);
+
+	/* To whom do we want to talk to.. */
+	ctl = MDIO_CTL_PORT_ADDR(phy) | MDIO_CTL_DEV_ADDR(reg);
+	/* XXX do we need two writes for this to work reliably? */
+	memac_write_4(sc, MDIO_CTL, ctl | MDIO_CTL_READ);
+
+	val = memac_miibus_wait_no_busy(sc);
+	if (val != 0)
+		return (0xffff);
+
+	cfg = memac_read_4(sc, MDIO_CFG);
+	if (cfg & MDIO_CFG_MDIO_RD_ER)
+		return (0xffff);
+
+	val = memac_read_4(sc, MDIO_DATA);
+	val &= 0xffff;
+
+#if defined(MEMAC_MDIO_DEBUG)
+	device_printf(sc->dev, "phy read %d:%d = %#06x\n", phy, reg, val);
+#endif
+
+        return (val);
+}
+
+int
+memac_miibus_writereg(struct memac_mdio_softc_common *sc, int phy, int reg, int data)
+{
+	uint32_t cfg, ctl, val;
+
+#if defined(MEMAC_MDIO_DEBUG)
+	device_printf(sc->dev, "phy write %d:%d\n", phy, reg);
+#endif
+
+	/* Set proper Clause 45 mode. */
+	cfg = memac_read_4(sc, MDIO_CFG);
+	/* XXX 45 support? */
+	cfg &= ~MDIO_CFG_ENC45;	/* Use Clause 22 */
+	memac_write_4(sc, MDIO_CFG, cfg);
+
+	val = memac_miibus_wait_no_busy(sc);
+	if (val != 0)
+		return (0xffff);
+
+	/* To whom do we want to talk to.. */
+	ctl = MDIO_CTL_PORT_ADDR(phy) | MDIO_CTL_DEV_ADDR(reg);
+	memac_write_4(sc, MDIO_CTL, ctl);
+
+	memac_write_4(sc, MDIO_DATA, data & 0xffff);
+
+	val = memac_miibus_wait_no_busy(sc);
+	if (val != 0)
+		return (0xffff);
+
+	return (0);
+}
+
+ssize_t
+memac_mdio_get_property(device_t dev, device_t child, const char *propname,
+    void *propvalue, size_t size, device_property_type_t type)
+{
+
+	return (bus_generic_get_property(dev, child, propname, propvalue, size, type));
+}
+
+int
+memac_mdio_read_ivar(device_t dev, device_t child, int index, uintptr_t *result)
+{
+
+	return (BUS_READ_IVAR(device_get_parent(dev), dev, index, result));
+}
+
+
+int
+memac_mdio_generic_attach(struct memac_mdio_softc_common *sc)
+{
+	int rid;
+
+	rid = 0;
+	sc->mem_res = bus_alloc_resource_any(sc->dev, SYS_RES_MEMORY,
+	    &rid, RF_ACTIVE | RF_SHAREABLE);
+	if (sc->mem_res == NULL) {
+		device_printf(sc->dev, "%s: cannot allocate mem resource\n",
+		    __func__);
+		return (ENXIO);
+	}
+
+	sc->is_little_endian = device_has_property(sc->dev, "little-endian");
+
+	return (0);
+}
+
+int
+memac_mdio_generic_detach(struct memac_mdio_softc_common *sc)
+{
+
+	if (sc->mem_res != NULL)
+		bus_release_resource(sc->dev, SYS_RES_MEMORY,
+		    rman_get_rid(sc->mem_res), sc->mem_res);
+
+	return (0);
+}
