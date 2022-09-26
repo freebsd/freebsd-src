f771c03297b0b187c63365aac061346748388e6d
John Baldwin <jhb@FreeBSD.org>
2022-09-26 14:58:06 -0700

    cxgbe: Use secq(9) to manage the timestamp generations.
    This is mostly cosmetic, but it also doesn't leave a gap of time where
    no structures are valid.  Instead, we permit the ISR to continue to
    use the previous structure if the write to update cal_current isn't
    yet visible.
    
    Reviewed by:	gallatin
    Sponsored by:	Chelsio Communications
    Differential Revision:	https://reviews.freebsd.org/D36669
    
    (cherry picked from commit cee4fc7cada8244f375a6542f03d1f255c719bf1)

--- sys/dev/cxgbe/adapter.h
+++ sys/dev/cxgbe/adapter.h
@@ -41,6 +41,7 @@
 #include <sys/lock.h>
 #include <sys/malloc.h>
 #include <sys/rwlock.h>
+#include <sys/seqc.h>
 #include <sys/sx.h>
 #include <sys/vmem.h>
 #include <vm/uma.h>
@@ -867,7 +868,7 @@ struct clock_sync {
 	uint64_t hw_prev;
 	sbintime_t sbt_cur;
 	sbintime_t sbt_prev;
-	uint32_t gen;
+	seqc_t gen;
 };
 
 struct adapter {
