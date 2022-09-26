39be4f7d1f8b9bebe46c0fc47c4f925632dd49e9
Xin LI <delphij@FreeBSD.org>
2022-09-25 19:01:46 -0700

    scandir(3): Rename alphasort_thunk to scandir_thunk_cmp to reflect that it is not alphasort-specific.
    Reported by:	emaste
    Reviewed by:	emaste
    Differential Revision: https://reviews.freebsd.org/D36708
    
    (cherry picked from commit 45ff071ddccac0a158cfeb53285e6bb2df6bc835)

--- lib/libc/gen/scandir-compat11.c
+++ lib/libc/gen/scandir-compat11.c
@@ -58,7 +58,7 @@ __FBSDID("$FreeBSD$");
 
 #define	SELECT(x)	select(x)
 
-static int freebsd11_alphasort_thunk(void *thunk, const void *p1,
+static int freebsd11_scandir_thunk_cmp(void *thunk, const void *p1,
     const void *p2);
 
 int
@@ -116,7 +116,7 @@ freebsd11_scandir(const char *dirname, struct freebsd11_dirent ***namelist,
 	closedir(dirp);
 	if (numitems && dcomp != NULL)
 		qsort_r(names, numitems, sizeof(struct freebsd11_dirent *),
-		    &dcomp, freebsd11_alphasort_thunk);
+		    &dcomp, freebsd11_scandir_thunk_cmp);
 	*namelist = names;
 	return (numitems);
 
@@ -141,7 +141,7 @@ freebsd11_alphasort(const struct freebsd11_dirent **d1,
 }
 
 static int
-freebsd11_alphasort_thunk(void *thunk, const void *p1, const void *p2)
+freebsd11_scandir_thunk_cmp(void *thunk, const void *p1, const void *p2)
 {
 	int (*dc)(const struct freebsd11_dirent **, const struct
 	    freebsd11_dirent **);
