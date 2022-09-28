5ceec6c1054e6bde3b72672f33705a044fc0eb86
John Baldwin <jhb@FreeBSD.org>
2022-09-28 14:02:59 -0700

    pam_unix: Fix mismatch in array bounds for make_salt().
    Reviewed by:	imp, emaste
    Reported by:	GCC -Warray-parameter
    Differential Revision:	https://reviews.freebsd.org/D36759

--- lib/libpam/modules/pam_unix/pam_unix.c
+++ lib/libpam/modules/pam_unix/pam_unix.c
@@ -76,7 +76,7 @@ __FBSDID("$FreeBSD$");
 #define	LOCKED_PREFIX		"*LOCKED*"
 #define	LOCKED_PREFIX_LEN	(sizeof(LOCKED_PREFIX) - 1)
 
-static void makesalt(char []);
+static void makesalt(char [SALTSIZE + 1]);
 
 static char password_hash[] =		PASSWORD_HASH;
 
