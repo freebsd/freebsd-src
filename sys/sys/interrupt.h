b9c03ff6a253557ad4e99c3cbbab9b9a4c96e1b1
Elliott Mitchell <ehem+freebsd@m5p.com>
2022-09-30 13:22:50 -0700

    kern/intr: switch intr_event_handle() to return stray count
    Comparable to the current situation.  Minor savings, one caller simply
    passes the count directly through.
    
    Differential Revision: https://reviews.freebsd.org/D38449

--- sys/sys/interrupt.h
+++ sys/sys/interrupt.h
@@ -180,7 +180,7 @@ int	intr_event_describe_handler(struct intr_event *ie, void *cookie,
 	    const char *descr);
 int	intr_event_destroy(struct intr_event *ie);
 void	intr_event_incr(struct intr_event *ie);
-int	intr_event_handle(struct intr_event *ie, struct trapframe *frame);
+u_long	intr_event_handle(struct intr_event *ie, struct trapframe *frame);
 int	intr_event_remove_handler(void *cookie);
 int	intr_event_suspend_handler(void *cookie);
 int	intr_event_resume_handler(void *cookie);
