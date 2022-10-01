f341e9bad3eb7438fe3ac5cff2e58df50f1a28e4
Warner Losh <imp@FreeBSD.org>
2022-09-30 18:26:45 -0600

    uart: Remove sbbc reference
    sbbc support is for sparc64 only, which was removed in 58aa35d42975.
    
    Sponsored by:		Netflix

--- sys/dev/uart/uart.h
+++ sys/dev/uart/uart.h
@@ -103,7 +103,6 @@ struct uart_class;
 
 extern struct uart_class uart_ns8250_class __attribute__((weak));
 extern struct uart_class uart_quicc_class __attribute__((weak));
-extern struct uart_class uart_sbbc_class __attribute__((weak));
 extern struct uart_class uart_z8530_class __attribute__((weak));
 
 /*
