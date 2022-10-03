107824ffcb9b2ea0986001a960557b30569d0649
John Baldwin <jhb@FreeBSD.org>
2022-10-03 16:10:42 -0700

    tail: Fix misleading indentation in ARG() macro.
    Reviewed by:	imp, emaste
    Reported by:	GCC
    Differential Revision:	https://reviews.freebsd.org/D36810

--- usr.bin/tail/tail.c
+++ usr.bin/tail/tail.c
@@ -118,7 +118,7 @@ main(int argc, char *argv[])
 	case '+':							\
 		if (off)						\
 			off -= (units);					\
-			style = (forward);				\
+		style = (forward);					\
 		break;							\
 	case '-':							\
 		off = -off;						\
