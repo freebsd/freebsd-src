0924ae8f47ae7095a048843e588a81a12787a819
Richard Scheffenegger <rscheff@FreeBSD.org>
2022-10-03 19:18:58 +0200

    tcp: allow window scale and timestamps to be toggled individually
    Simple change to allow for the individual toggling of
    RFC7323 window scaling and timestamp option.
    
    Reviewed By:    	rrs, tuexen, glebius, guest-ccui, #transport
    Sponsored by:   	NetApp, Inc.
    Differential Revision:	https://reviews.freebsd.org/D36863

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -2280,8 +2280,20 @@ tcp_newtcpcb(struct inpcb *inp)
 	callout_init(&tp->t_timers->tt_2msl, 1);
 	callout_init(&tp->t_timers->tt_delack, 1);
 
-	if (V_tcp_do_rfc1323)
-		tp->t_flags = (TF_REQ_SCALE|TF_REQ_TSTMP);
+	switch (V_tcp_do_rfc1323) {
+		case 0:
+			break;
+		default:
+		case 1:
+			tp->t_flags = (TF_REQ_SCALE|TF_REQ_TSTMP);
+			break;
+		case 2:
+			tp->t_flags = TF_REQ_SCALE;
+			break;
+		case 3:
+			tp->t_flags = TF_REQ_TSTMP;
+			break;
+	}
 	if (V_tcp_do_sack)
 		tp->t_flags |= TF_SACK_PERMIT;
 	TAILQ_INIT(&tp->snd_holes);
