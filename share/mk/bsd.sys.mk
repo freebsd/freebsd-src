9c8849713a1775cb5400376cfec0557e02df16d9
John Baldwin <jhb@FreeBSD.org>
2022-10-03 16:10:42 -0700

    bsd.sys.mk: Add NO_WUSE_AFTER_FREE helper variable.
    This variable expands to -Wno-use-after-free on GCC 12+.
    
    Reviewed by:	imp, emaste
    Differential Revision:	https://reviews.freebsd.org/D36817

--- share/mk/bsd.sys.mk
+++ share/mk/bsd.sys.mk
@@ -117,6 +117,9 @@ NO_WBITWISE_INSTEAD_OF_LOGICAL=	-Wno-bitwise-instead-of-logical
 .if ${COMPILER_TYPE} == "gcc" && ${COMPILER_VERSION} >= 110100
 NO_WARRAY_PARAMETER=	-Wno-array-parameter
 .endif
+.if ${COMPILER_TYPE} == "gcc" && ${COMPILER_VERSION} >= 120100
+NO_WUSE_AFTER_FREE=	-Wno-use-after-free
+.endif
 .endif # WARNS
 
 .if defined(FORMAT_AUDIT)
