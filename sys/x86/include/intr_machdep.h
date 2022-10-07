2220730dd5830c85d4a62b47eb5aeaa5866ba443
Elliott Mitchell <ehem+freebsd@m5p.com>
2022-10-06 20:23:34 -0700

    intr/x86: remove intr_bind() from x86
    `intr_bind(u_int vector, u_char cpu);` looked suspicious since
    everywhere "cpu" is a u_int and >256 processors isn't unreasonable now.
    Upon checking, `intr_bind()` is not used anywhere in FreeBSD.  Time to
    remove.
    
    Reviewed by: mjg
    Differential Revision: https://reviews.freebsd.org/D36901

--- sys/x86/include/intr_machdep.h
+++ sys/x86/include/intr_machdep.h
@@ -143,9 +143,6 @@ void	intr_add_cpu(u_int cpu);
 int	intr_add_handler(const char *name, int vector, driver_filter_t filter,
     driver_intr_t handler, void *arg, enum intr_type flags, void **cookiep,
     int domain);
-#ifdef SMP
-int	intr_bind(u_int vector, u_char cpu);
-#endif
 int	intr_config_intr(int vector, enum intr_trigger trig,
     enum intr_polarity pol);
 int	intr_describe(u_int vector, void *ih, const char *descr);
