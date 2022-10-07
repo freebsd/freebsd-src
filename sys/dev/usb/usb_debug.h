98a62a83b072d11984d96b18d8bcdb6596124204
Hans Petter Selasky <hselasky@FreeBSD.org>
2022-10-07 13:28:38 +0200

    usb(4): Make the enumeration thread nice time a variable.
    Depends on "options USB_DEBUG".
    
    Suggested by:	koobs@
    MFC after:	1 week
    Sponsored by:	NVIDIA Networking

--- sys/dev/usb/usb_debug.h
+++ sys/dev/usb/usb_debug.h
@@ -74,6 +74,7 @@ extern unsigned int usb_resume_delay;
 extern unsigned int usb_resume_wait;
 extern unsigned int usb_resume_recovery;
 extern unsigned int usb_extra_power_up_time;
+extern unsigned int usb_enum_nice_time;
 #else
 #define usb_port_reset_delay		USB_PORT_RESET_DELAY
 #define usb_port_root_reset_delay	USB_PORT_ROOT_RESET_DELAY
@@ -85,6 +86,7 @@ extern unsigned int usb_extra_power_up_time;
 #define usb_resume_wait			USB_RESUME_WAIT
 #define usb_resume_recovery		USB_RESUME_RECOVERY
 #define usb_extra_power_up_time		USB_EXTRA_POWER_UP_TIME
+#define usb_enum_nice_time		USB_ENUM_NICE_TIME
 #endif
 
 #endif					/* _USB_DEBUG_H_ */
