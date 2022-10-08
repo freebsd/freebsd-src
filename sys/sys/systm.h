143942f9929a36ec7fe56556d0afe0b2d23e08c4
Mateusz Guzik <mjg@FreeBSD.org>
2022-10-08 09:38:41 +0000

    unr: remove UNR64_LOCKED
    All platforms support 64-bit atomics now.

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -510,10 +510,6 @@ int alloc_unr_specific(struct unrhdr *uh, u_int item);
 int alloc_unrl(struct unrhdr *uh);
 void free_unr(struct unrhdr *uh, u_int item);
 
-#ifndef __LP64__
-#define UNR64_LOCKED
-#endif
-
 struct unrhdr64 {
         uint64_t	counter;
 };
@@ -525,16 +521,12 @@ new_unrhdr64(struct unrhdr64 *unr64, uint64_t low)
 	unr64->counter = low;
 }
 
-#ifdef UNR64_LOCKED
-uint64_t alloc_unr64(struct unrhdr64 *);
-#else
 static __inline uint64_t
 alloc_unr64(struct unrhdr64 *unr64)
 {
 
 	return (atomic_fetchadd_64(&unr64->counter, 1));
 }
-#endif
 
 void	intr_prof_stack_use(struct thread *td, struct trapframe *frame);
 
