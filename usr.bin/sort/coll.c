555ff54887f34e3132c91999cdf6a3078bdb4995
Baptiste Daroussin <bapt@FreeBSD.org>
2022-10-12 15:57:11 +0200

    sort: replace malloc+memset with calloc

--- usr.bin/sort/coll.c
+++ usr.bin/sort/coll.c
@@ -73,8 +73,7 @@ keys_array_alloc(void)
 	size_t sz;
 
 	sz = keys_array_size();
-	ka = sort_malloc(sz);
-	memset(ka, 0, sz);
+	ka = sort_calloc(1, sz);
 
 	return (ka);
 }
@@ -157,8 +156,7 @@ sort_list_item_alloc(void)
 	size_t sz;
 
 	sz = sizeof(struct sort_list_item) + keys_array_size();
-	si = sort_malloc(sz);
-	memset(si, 0, sz);
+	si = sort_calloc(1, sz);
 
 	return (si);
 }
