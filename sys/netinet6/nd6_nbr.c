94502e922379b9c465e3c80ca31d515f767be74d
Gleb Smirnoff <glebius@FreeBSD.org>
2022-10-17 11:39:40 -0700

    carp: fix regression panic from 8caac243b04
    Reported & tested by:	Oleg Ginzburg <olevole olevole.ru>
    Fixes:			8caac243b04e4c4d79ffccf6591ced1824f3bba1

--- sys/netinet6/nd6_nbr.c
+++ sys/netinet6/nd6_nbr.c
@@ -514,7 +514,8 @@ nd6_ns_output_fib(struct ifnet *ifp, const struct in6_addr *saddr6,
 			 * Do not send NS for CARP address if we are not
 			 * the CARP master.
 			 */
-			if (ifa != NULL && !(*carp_master_p)(ifa)) {
+			if (ifa != NULL && ifa->ifa_carp != NULL &&
+			    !(*carp_master_p)(ifa)) {
 				log(LOG_DEBUG,
 				    "nd6_ns_output: NS from BACKUP CARP address %s\n",
 				    ip6_sprintf(ip6buf, &ip6->ip6_src));
