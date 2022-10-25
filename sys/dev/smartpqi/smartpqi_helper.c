6e36a48f1f50818225c339a2dafb3ee167d9462f
Gordon Bergling <gbe@FreeBSD.org>
2022-10-25 13:17:20 +0200

    smartpqi(4): Fix two typos in kernel error messages
    - s/runnning/running/
    
    MFC after:	2 weeks

--- sys/dev/smartpqi/smartpqi_helper.c
+++ sys/dev/smartpqi/smartpqi_helper.c
@@ -150,7 +150,7 @@ pqisrc_take_ctrl_offline(pqisrc_softstate_t *softs)
 
 	if (SIS_IS_KERNEL_PANIC(softs)) {
                 lockupcode = PCI_MEM_GET32(softs, &softs->ioa_reg->mb[7], LEGACY_SIS_SRCV_OFFSET_MAILBOX_7);
-                DBG_ERR("Controller FW is not runnning, Lockup code = %x\n", lockupcode);
+                DBG_ERR("Controller FW is not running, Lockup code = %x\n", lockupcode);
         }
         else {
                 pqisrc_trigger_nmi_sis(softs);
