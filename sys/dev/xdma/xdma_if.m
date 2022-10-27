91e2614a710520d023fd59108caa971d6e57e0d1
Andrew Turner <andrew@FreeBSD.org>
2022-10-27 17:17:20 +0100

    Fix xdma_if.m in an ACPI only kernel
     - We depend on header polution to include sys/malloc.h. Include it
       directly.
     - Only define FDT-specific fuctions when building a FDT kernel.
    
    Sponsored by:	Innovate UK

--- sys/dev/xdma/xdma_if.m
+++ sys/dev/xdma/xdma_if.m
@@ -32,6 +32,8 @@
 
 #include "opt_platform.h"
 
+#include <sys/malloc.h>
+
 #include <machine/bus.h>
 
 #ifdef FDT
@@ -81,6 +83,7 @@ METHOD int channel_submit_sg {
 	uint32_t			sg_n;
 };
 
+#ifdef FDT
 #
 # Notify driver we have machine-dependend data.
 #
@@ -90,6 +93,7 @@ METHOD int ofw_md_data {
 	int ncells;
 	void **data;
 };
+#endif
 
 #
 # Allocate both virtual and harware channels.
