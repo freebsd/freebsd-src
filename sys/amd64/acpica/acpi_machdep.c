19c6e3f92827c7698c51f7ccb7974d1c86b0f41b
Jung-uk Kim <jkim@FreeBSD.org>
2022-10-27 22:04:32 -0400

    acpica: Merge ACPICA 20221020

--- sys/amd64/acpica/acpi_machdep.c
+++ sys/amd64/acpica/acpi_machdep.c
@@ -109,7 +109,7 @@ map_table(vm_paddr_t pa, const char *sig)
 	length = header->Length;
 	pmap_unmapbios(header, sizeof(ACPI_TABLE_HEADER));
 	table = pmap_mapbios(pa, length);
-	if (ACPI_FAILURE(AcpiTbChecksum(table, length))) {
+	if (ACPI_FAILURE(AcpiUtChecksum(table, length))) {
 		if (bootverbose)
 			printf("ACPI: Failed checksum for table %s\n", sig);
 #if (ACPI_CHECKSUM_ABORT)
@@ -199,7 +199,7 @@ acpi_find_table(const char *sig)
 		 * the version 1.0 portion of the RSDP.  Version 2.0 has
 		 * an additional checksum that we verify first.
 		 */
-		if (AcpiTbChecksum((UINT8 *)rsdp, ACPI_RSDP_XCHECKSUM_LENGTH)) {
+		if (AcpiUtChecksum((UINT8 *)rsdp, ACPI_RSDP_XCHECKSUM_LENGTH)) {
 			printf("ACPI: RSDP failed extended checksum\n");
 			pmap_unmapbios(rsdp, sizeof(ACPI_TABLE_RSDP));
 			return (0);
