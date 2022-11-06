004bb636ca65f3239da284c20abb7f9d1d953dee
Richard Scheffenegger <rscheff@FreeBSD.org>
2022-11-06 12:36:55 +0100

    tcp: Move sysctl OIDs related to ECN to tcp_ecn.c
    Keep all ECN related code in (mostly) one place.
    
    No functional change.
    
    Event:			IETF 115 Hackathon
    Reviewed By:		tuexen, #transport
    Sponsored by:		NetApp, Inc.
    Differential Revision:	https://reviews.freebsd.org/D37285

--- sys/netinet/tcp_ecn.c
+++ sys/netinet/tcp_ecn.c
@@ -101,6 +101,19 @@ __FBSDID("$FreeBSD$");
 #include <netinet/tcpip.h>
 #include <netinet/tcp_ecn.h>
 
+static SYSCTL_NODE(_net_inet_tcp, OID_AUTO, ecn,
+    CTLFLAG_RW | CTLFLAG_MPSAFE, 0,
+    "TCP ECN");
+
+VNET_DEFINE(int, tcp_do_ecn) = 2;
+SYSCTL_INT(_net_inet_tcp_ecn, OID_AUTO, enable,
+    CTLFLAG_VNET | CTLFLAG_RW, &VNET_NAME(tcp_do_ecn), 0,
+    "TCP ECN support");
+
+VNET_DEFINE(int, tcp_ecn_maxretries) = 1;
+SYSCTL_INT(_net_inet_tcp_ecn, OID_AUTO, maxretries,
+    CTLFLAG_VNET | CTLFLAG_RW, &VNET_NAME(tcp_ecn_maxretries), 0,
+    "Max retries before giving up on ECN");
 
 /*
  * Process incoming SYN,ACK packet
