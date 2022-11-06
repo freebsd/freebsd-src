22c81cc51636cfebe94e0979eb31556d87775938
Richard Scheffenegger <rscheff@FreeBSD.org>
2022-11-06 11:55:52 +0100

    tcp: add AccECN CE packet counters to tcpinfo
    Provide diagnostics information around AccECN into
    the tcpinfo struct.
    
    Event:			IETF 115 Hackathon
    Reviewed By:		tuexen, #transport
    Sponsored by:		NetApp, Inc.
    Differential Revision:	https://reviews.freebsd.org/D37280

--- sys/netinet/tcp_ecn.c
+++ sys/netinet/tcp_ecn.c
@@ -325,8 +325,10 @@ tcp_ecn_input_segment(struct tcpcb *tp, uint16_t thflags, int iptos)
 			}
 		} else {
 			/* RFC3168 ECN handling */
-			if ((thflags & (TH_SYN | TH_ECE)) == TH_ECE)
+			if ((thflags & (TH_SYN | TH_ECE)) == TH_ECE) {
 				delta_ace = 1;
+				tp->t_scep++;
+			}
 			if (thflags & TH_CWR) {
 				tp->t_flags2 &= ~TF2_ECN_SND_ECE;
 				tp->t_flags |= TF_ACKNOW;
