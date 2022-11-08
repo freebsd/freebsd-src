685f0d228395bc1074ebef998832176d5186bd15
Ed Maste <emaste@FreeBSD.org>
2022-11-08 13:14:55 -0500

    tests: also remove sparc64 case
    (cherry picked from commit 185efcc376b9dcb3b920d1053a250ac754ad163a)

--- tests/sys/kern/ptrace_test.c
+++ tests/sys/kern/ptrace_test.c
@@ -59,8 +59,7 @@ __FBSDID("$FreeBSD$");
  * Architectures with a user-visible breakpoint().
  */
 #if defined(__aarch64__) || defined(__amd64__) || defined(__arm__) ||	\
-    defined(__i386__) || defined(__mips__) || defined(__riscv) ||	\
-    defined(__sparc64__)
+    defined(__i386__) || defined(__mips__) || defined(__riscv)
 #define	HAVE_BREAKPOINT
 #endif
 
@@ -78,11 +77,6 @@ __FBSDID("$FreeBSD$");
 #define	SKIP_BREAK(reg)	((reg)->r_regs[PC] += 4)
 #elif defined(__riscv)
 #define	SKIP_BREAK(reg)	((reg)->sepc += 4)
-#elif defined(__sparc64__)
-#define	SKIP_BREAK(reg)	do {						\
-	(reg)->r_tpc = (reg)->r_tnpc + 4;				\
-	(reg)->r_tnpc += 8;						\
-} while (0)
 #endif
 #endif
 
