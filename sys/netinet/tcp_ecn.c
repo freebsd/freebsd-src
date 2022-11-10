1623de0db1a674ff9eb4ee20f9f6730df0b8816c
Richard Scheffenegger <rscheff@FreeBSD.org>
2022-11-10 11:35:02 +0100

    tcp: account sent/received IP ECN markings independently
    Have tcpstats (netstat -s) differentiate between received and sent
    ECN-marked packets. Also account for IP ECN bits (on TCP packets)
    even when the tcp session has not negotiated ECN support.
    
    Event:			IETF 115 Hackathon
    Reviewed By:		glebius, tuexen, #transport
    Sponsored by:		NetApp, Inc.
    Differential Revision:	https://reviews.freebsd.org/D37314

--- sys/netinet/tcp_ecn.c
+++ sys/netinet/tcp_ecn.c
@@ -288,19 +288,19 @@ tcp_ecn_input_segment(struct tcpcb *tp, uint16_t thflags, int tlen, int pkts, in
 {
 	int delta_cep = 0;
 
-	if (tp->t_flags2 & (TF2_ECN_PERMIT | TF2_ACE_PERMIT)) {
-		switch (iptos & IPTOS_ECN_MASK) {
-		case IPTOS_ECN_CE:
-			TCPSTAT_INC(tcps_ecn_ce);
-			break;
-		case IPTOS_ECN_ECT0:
-			TCPSTAT_INC(tcps_ecn_ect0);
-			break;
-		case IPTOS_ECN_ECT1:
-			TCPSTAT_INC(tcps_ecn_ect1);
-			break;
-		}
+	switch (iptos & IPTOS_ECN_MASK) {
+	case IPTOS_ECN_CE:
+		TCPSTAT_INC(tcps_ecn_rcvce);
+		break;
+	case IPTOS_ECN_ECT0:
+		TCPSTAT_INC(tcps_ecn_rcvect0);
+		break;
+	case IPTOS_ECN_ECT1:
+		TCPSTAT_INC(tcps_ecn_rcvect1);
+		break;
+	}
 
+	if (tp->t_flags2 & (TF2_ECN_PERMIT | TF2_ACE_PERMIT)) {
 		if (tp->t_flags2 & TF2_ACE_PERMIT) {
 			if ((iptos & IPTOS_ECN_MASK) == IPTOS_ECN_CE)
 				tp->t_rcep += 1;
@@ -413,10 +413,10 @@ tcp_ecn_output_established(struct tcpcb *tp, uint16_t *thflags, int len, bool rx
 	if (newdata) {
 		if (tp->t_flags2 & TF2_ECN_USE_ECT1) {
 			ipecn = IPTOS_ECN_ECT1;
-			TCPSTAT_INC(tcps_ecn_ect1);
+			TCPSTAT_INC(tcps_ecn_sndect1);
 		} else {
 			ipecn = IPTOS_ECN_ECT0;
-			TCPSTAT_INC(tcps_ecn_ect0);
+			TCPSTAT_INC(tcps_ecn_sndect0);
 		}
 	}
 	/*
@@ -496,6 +496,18 @@ int
 tcp_ecn_syncache_add(uint16_t thflags, int iptos)
 {
 	int scflags = 0;
+
+	switch (iptos & IPTOS_ECN_MASK) {
+	case IPTOS_ECN_CE:
+		TCPSTAT_INC(tcps_ecn_rcvce);
+		break;
+	case IPTOS_ECN_ECT0:
+		TCPSTAT_INC(tcps_ecn_rcvect0);
+		break;
+	case IPTOS_ECN_ECT1:
+		TCPSTAT_INC(tcps_ecn_rcvect1);
+		break;
+	}
 
 	switch (thflags & (TH_AE|TH_CWR|TH_ECE)) {
 	/* no ECN */
