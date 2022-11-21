240e9b6759201196c0cf1d8faad2e7ea55a3cb06
Justin Hibbits <jhibbits@FreeBSD.org>
2022-11-20 19:47:19 -0500

    powerpc/dtrace: Guard fuword64 usage for powerpc64 only
    32-bit powerpc doesn't have a fuword64, so only use it on powerpc64.
    This could also be done elsewhere that splits 32-bit and 64-bit
    accesses, but adding ifdefs everywhere would just clutter up the source
    for little benefit.  This is the only usage of fuword64 directly; other
    archs have a dtrace_fuword64_nocheck(), but powerpc does not, and I
    don't see a need to add it currently.
    
    MFC after:	1 week

--- sys/cddl/contrib/opensolaris/uts/powerpc/dtrace/fasttrap_isa.c
+++ sys/cddl/contrib/opensolaris/uts/powerpc/dtrace/fasttrap_isa.c
@@ -246,12 +246,16 @@ fasttrap_usdt_args(fasttrap_probe_t *probe, struct reg *rp, int argc,
 		if (x < 8)
 			argv[i] = rp->fixreg[x];
 		else
+#ifdef __powerpc64__
 			if (SV_PROC_FLAG(curproc, SV_ILP32))
+#endif
 				argv[i] = fuword32((void *)(rp->fixreg[1] + 8 +
 				    (x * sizeof(uint32_t))));
+#ifdef __powerpc64__
 			else
 				argv[i] = fuword64((void *)(rp->fixreg[1] + 48 +
 				    (x * sizeof(uint64_t))));
+#endif
 	}
 
 	for (; i < argc; i++) {
