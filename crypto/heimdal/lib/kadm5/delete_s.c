671f55828d038e17fb29fd115b88e7cdf53500c3
Cy Schubert <cy@FreeBSD.org>
2022-11-26 12:18:03 -0800

    heimdal: Check return code from _hdb_fetch_kvno()
    A malformed principal will cause kadmind to segfault. The PR
    addresses only when chpass is requested but delete is also affected.
    Fix both.
    
    PR:		268002
    Reported by:	Robert Morris <rtm@lcs.mit.edu>
    MFC after:	3 days

--- crypto/heimdal/lib/kadm5/delete_s.c
+++ crypto/heimdal/lib/kadm5/delete_s.c
@@ -50,7 +50,7 @@ kadm5_s_delete_principal(void *server_handle, krb5_principal princ)
     }
     ret = context->db->hdb_fetch_kvno(context->context, context->db, princ,
 				      HDB_F_DECRYPT|HDB_F_GET_ANY|HDB_F_ADMIN_DATA, 0, &ent);
-    if(ret == HDB_ERR_NOENTRY)
+    if (ret)
 	goto out;
     if(ent.entry.flags.immutable) {
 	ret = KADM5_PROTECT_PRINCIPAL;
