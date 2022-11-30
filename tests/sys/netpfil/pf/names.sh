16603f60156e13ea47e66b2fdcc391bb13db7dee
Kristof Provost <kp@FreeBSD.org>
2022-11-30 15:20:41 +0100

    pf tests: check that we clean up unused kifs
    The previous commit fixed a memory leak, where we'd fail to clean up
    removed groups (and interfaces).
    Check that we now clean those up as expected.
    
    MFC after:	2 weeks
    Sponsored by:	Rubicon Communications, LLC ("Netgate")
    Differential Revision:	https://reviews.freebsd.org/D37570
    
    (cherry picked from commit 654e8d84ec8bcb3d94cd8de69edecbe1bbd73eb8)

--- tests/sys/netpfil/pf/names.sh
+++ tests/sys/netpfil/pf/names.sh
@@ -53,7 +53,51 @@ names_cleanup()
 	pft_cleanup
 }
 
+atf_test_case "group" "cleanup"
+group_head()
+{
+	atf_set descr 'Test group cleanup, PR257218'
+	atf_set require.user root
+}
+
+group_body()
+{
+	pft_init
+
+	vnet_mkjail alcatraz
+
+	if [ -n "$(jexec alcatraz pfctl -sI | grep '^epair$')" ];
+	then
+		atf_fail "Unexpected epair group"
+	fi
+
+	epair=$(vnet_mkepair)
+	if [ -n "$(jexec alcatraz pfctl -sI | grep '^epair$')" ];
+	then
+		atf_fail "Unexpected epair group"
+	fi
+
+	ifconfig ${epair}b vnet alcatraz
+	if [ -z "$(jexec alcatraz pfctl -sI | grep '^epair$')" ];
+	then
+		atf_fail "Failed to find epair group"
+	fi
+
+	ifconfig ${epair}a destroy
+
+	if [ -n "$(jexec alcatraz pfctl -sI | grep '^epair$')" ];
+	then
+		atf_fail "Unexpected epair group"
+	fi
+}
+
+group_cleanup()
+{
+	pft_cleanup
+}
+
 atf_init_test_cases()
 {
 	atf_add_test_case "names"
+	atf_add_test_case "group"
 }
