aaf570313289aad6f96eb49d3b19663d85512c82
Konstantin Belousov <kib@FreeBSD.org>
2022-11-30 10:45:52 +0200

    ptrace(2): add PT_SC_REMOTE remote syscall request
    Tested by:	pho
    
    (cherry picked from commit 140ceb5d956bb8795a77c23d3fd5ef047b0f3c68)

--- sys/sys/ptrace.h
+++ sys/sys/ptrace.h
@@ -87,6 +87,7 @@
 #define	PT_VM_ENTRY	41	/* Get VM map (entry) */
 #define	PT_GETREGSET	42	/* Get a target register set */
 #define	PT_SETREGSET	43	/* Set a target register set */
+#define	PT_SC_REMOTE	44	/* Execute a syscall */
 
 #define PT_FIRSTMACH    64	/* for machine-specific requests */
 #include <machine/ptrace.h>	/* machine-specific requests, if any */
@@ -192,13 +193,28 @@ struct ptrace_coredump {
 #define	PC_COMPRESS	0x00000001	/* Allow compression */
 #define	PC_ALL		0x00000002	/* Include non-dumpable entries */
 
+struct ptrace_sc_remote {
+	struct ptrace_sc_ret pscr_ret;
+	u_int	pscr_syscall;
+	u_int	pscr_nargs;
+	register_t	*pscr_args;
+};
+
 #ifdef _KERNEL
+
+#include <sys/proc.h>
 
 struct thr_coredump_req {
 	struct vnode	*tc_vp;		/* vnode to write coredump to. */
 	off_t		tc_limit;	/* max coredump file size. */
 	int		tc_flags;	/* user flags */
 	int		tc_error;	/* request result */
+};
+
+struct thr_syscall_req {
+	struct ptrace_sc_ret ts_ret;
+	u_int	ts_nargs;
+	struct syscall_args ts_sa;
 };
 
 int	ptrace_set_pc(struct thread *_td, unsigned long _addr);
