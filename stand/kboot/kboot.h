7e1a2e46aa9a2f834c33b5c50de0437ac19d7b41
Warner Losh <imp@FreeBSD.org>
2022-12-02 11:05:58 -0700

    kboot: Create routines to read Linux tiny files
    Most of the files in /sys/ and /proc/ are small with one value. Create
    two routines to help us read the file and decode that value.
    
    Sponsored by:		Netflix

--- stand/kboot/kboot.h
+++ stand/kboot/kboot.h
@@ -19,4 +19,8 @@ void fdt_arch_fixups(void *fdtp);
 uint64_t kboot_get_phys_load_segment(void);
 uint8_t kboot_get_kernel_machine_bits(void);
 
+/* util.c */
+bool file2str(const char *fn, char *buffer, size_t buflen);
+bool file2u64(const char *fn, uint64_t *val);
+
 #endif /* KBOOT_H */
