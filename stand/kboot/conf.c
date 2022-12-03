fe2083b65ec6fb22dc872aecf745c2fd7c89fb39
Warner Losh <imp@FreeBSD.org>
2022-12-03 12:48:19 -0700

    kboot: Make dosfs support conditional
    Sponsored by:		Netflix

--- stand/kboot/conf.c
+++ stand/kboot/conf.c
@@ -82,7 +82,9 @@ struct fs_ops *file_system[] = {
 #if defined(LOADER_BZIP2_SUPPORT)
     &bzipfs_fsops,
 #endif
+#if defined(LOADER_MSDOS_SUPPORT)
     &dosfs_fsops,
+#endif
     &hostfs_fsops,
     NULL
 };
