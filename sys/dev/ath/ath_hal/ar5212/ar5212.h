6d9ad9414caf506b5643efeb1e94b272c426fbc6
John Baldwin <jhb@FreeBSD.org>
2022-12-07 12:30:42 -0800

    ath: Fix mismatches in array bounds.
    Reported by:	GCC -Warray-parameter
    Reviewed by:	imp, emaste
    Differential Revision:	https://reviews.freebsd.org/D37542

--- sys/dev/ath/ath_hal/ar5212/ar5212.h
+++ sys/dev/ath/ath_hal/ar5212/ar5212.h
@@ -574,7 +574,7 @@ extern	HAL_BOOL ar5212ResetCalValid(struct ath_hal *ah,
 		const struct ieee80211_channel *);
 extern	int16_t ar5212GetNoiseFloor(struct ath_hal *ah);
 extern	void ar5212InitNfCalHistBuffer(struct ath_hal *);
-extern	int16_t ar5212GetNfHistMid(const int16_t calData[]);
+extern	int16_t ar5212GetNfHistMid(const int16_t calData[AR512_NF_CAL_HIST_MAX]);
 extern	void ar5212SetSpurMitigation(struct ath_hal *,
 		 const struct ieee80211_channel *);
 extern	HAL_BOOL ar5212SetAntennaSwitchInternal(struct ath_hal *ah,
