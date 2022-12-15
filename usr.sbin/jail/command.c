561dcc7f3722315c55176be5611a010155724a89
Jamie Gritton <jamie@FreeBSD.org>
2022-12-14 16:47:55 -0800

    jail: fix a NULL pointer derefence in parsing ip6.addr settings.
    This is the counterpart to bd24e861b4ef, which did the same for ip4.
    PR:		268377
    Reported by:	ahkithaama at proton.me
    
    (cherry picked from commit e8d7ae918a105f252cf06249787d82f43e023ba8)

--- usr.sbin/jail/command.c
+++ usr.sbin/jail/command.c
@@ -424,7 +424,7 @@ run_command(struct cfjail *j)
 		} else
 			argc = 4;
 
-		if (!down) {
+		if (!down && extrap != NULL) {
 			for (cs = strtok(extrap, " "); cs;
 			     cs = strtok(NULL, " ")) {
 				size_t len = strlen(cs) + 1;
