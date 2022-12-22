6e9ab1e63192130c4e2fdd16ac8c190e14a886d1
Rick Macklem <rmacklem@FreeBSD.org>
2022-12-22 09:10:27 -0800

    rpc.tlsservd: Check for a tls syscall failure.
    Although the tls syscall to set up the upcall should
    not normally fail, the daemon should check for such
    a failure.  This patch adds a check for that failure.
    
    (cherry picked from commit ab3c59a107c56d4304b6ba2c36116b87a0122b4f)

--- usr.sbin/rpc.tlsservd/rpc.tlsservd.c
+++ usr.sbin/rpc.tlsservd/rpc.tlsservd.c
@@ -343,7 +343,14 @@ main(int argc, char **argv)
 	rpctls_gothup = false;
 	LIST_INIT(&rpctls_ssllist);
 
-	rpctls_syscall(RPCTLS_SYSC_SRVSETPATH, _PATH_RPCTLSSDSOCK);
+	if (rpctls_syscall(RPCTLS_SYSC_SRVSETPATH, _PATH_RPCTLSSDSOCK) < 0) {
+		if (rpctls_debug_level == 0) {
+			syslog(LOG_ERR,
+			    "Can't set upcall socket errno=%d", errno);
+			exit(1);
+		}
+		err(1, "Can't set upcall socket");
+	}
 
 	rpctls_svc_run();
 
