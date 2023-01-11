29345e72e1420d4e38e96e26fb5c5ab56e6fbd50
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2023-01-11 23:23:35 +0100

    linuxkpi: Add `pm_suspend_via_firmware()`
    Reviewed by:	manu
    Approved by:	manu
    Differential Revision:	https://reviews.freebsd.org/D38084

---
+++ sys/compat/linuxkpi/common/include/linux/suspend.h
@@ -0,0 +1,12 @@
+/* Public domain. */
+
+#ifndef _LINUXKPI_LINUX_SUSPEND_H_
+#define _LINUXKPI_LINUX_SUSPEND_H_
+
+static inline int
+pm_suspend_via_firmware()
+{
+	return 0;
+}
+
+#endif	/* _LINUXKPI_LINUX_SUSPEND_H_ */
