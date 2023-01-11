10fdb005202236c791d7e52fd22dfcf027785648
Gordon Bergling <gbe@FreeBSD.org>
2023-01-11 11:29:23 +0100

    i386: Fix a typo in a source code comment
    - s/betwen/between/
    
    (cherry picked from commit 77a863b6c217f3c143e1a8514a0fe409d5936c0f)

--- sys/i386/i386/in_cksum_machdep.c
+++ sys/i386/i386/in_cksum_machdep.c
@@ -472,7 +472,7 @@ skip_start:
 		} else if (mlen == -1)
 			/*
 			 * This mbuf has odd number of bytes.
-			 * There could be a word split betwen
+			 * There could be a word split between
 			 * this mbuf and the next mbuf.
 			 * Save the last byte (to prepend to next mbuf).
 			 */
