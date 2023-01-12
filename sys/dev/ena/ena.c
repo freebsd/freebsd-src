freebsd/freebsd-src

commit 74dba3ad78510ecdfdef8c793635d11533ccec6d

Split function checking for missing TX completion in ENA driver
Pure cosmetic change for better readability of the driver.

Submitted by: Michal Krawczyk <mk@semihalf.com>
Reviewed by: byenduri_gmail.com
Obtained from: Semihalf
Sponsored by: Amazon, Inc.
Differential Revision: https://reviews.freebsd.org/D12857

sys/dev/ena/ena.c
@@ -3349,6 +3349,56 @@ static void check_for_admin_com_state(struct ena_adapter *adapter)
 	}
 }
 
+static int
+check_missing_comp_in_queue(struct ena_adapter *adapter,
+    struct ena_ring *tx_ring)
+{
+	struct bintime curtime, time;
+	struct ena_tx_buffer *tx_buf;
+	uint32_t missed_tx = 0;
+	int i;
+
+	getbinuptime(&curtime);
+
+	for (i = 0; i < tx_ring->ring_size; i++) {
+		tx_buf = &tx_ring->tx_buffer_info[i];
+
+		if (!bintime_isset(&tx_buf->timestamp))
+			continue;
+
+		time = curtime;
+		bintime_sub(&time, &tx_buf->timestamp);
+
+		/* Check again if packet is still waiting */
+		if (unlikely(bttosbt(time) > adapter->missing_tx_timeout)) {
+
+			if (!tx_buf->print_once)
+				ena_trace(ENA_WARNING, "Found a Tx that wasn't "
+				    "completed on time, qid %d, index %d.\n",
+				    tx_ring->qid, i);
+
+			tx_buf->print_once = true;
+			missed_tx++;
+
+			if (unlikely(missed_tx >
+			    adapter->missing_tx_threshold)) {
+				device_printf(adapter->pdev,
+					      "The number of lost tx completion "
+					      "is above the threshold (%d > %d). "
+					      "Reset the device\n",
+					      missed_tx,
+					      adapter->missing_tx_threshold);
+				adapter->reset_reason =
+				    ENA_REGS_RESET_MISS_TX_CMPL;
+				adapter->trigger_reset = true;
+				return (EIO);
+			}
+		}
+	}
+
+	return (0);
+}
+
 /*
  * Check for TX which were not completed on time.
  * Timeout is defined by "missing_tx_timeout".
@@ -3358,9 +3408,7 @@ static void check_for_admin_com_state(struct ena_adapter *adapter)
 static void check_for_missing_tx_completions(struct ena_adapter *adapter)
 {
 	struct ena_ring *tx_ring;
-	struct ena_tx_buffer *tx_info;
-	struct bintime curtime, time;
-	int i, j, budget, missed_tx;
+	int i, budget, rc;
 
 	/* Make sure the driver doesn't turn the device in other process */
 	rmb();
@@ -3375,48 +3423,13 @@ static void check_for_missing_tx_completions(struct ena_adapter *adapter)
 		return;
 
 	budget = adapter->missing_tx_max_queues;
-	getbinuptime(&curtime);
 
 	for (i = adapter->next_monitored_tx_qid; i < adapter->num_queues; i++) {
 		tx_ring = &adapter->tx_ring[i];
 
-		missed_tx = 0;
-
-		for (j = 0; j < tx_ring->ring_size; j++) {
-			tx_info = &tx_ring->tx_buffer_info[j];
-
-			if (!bintime_isset(&tx_info->timestamp))
-				continue;
-
-			time = curtime;
-			bintime_sub(&time, &tx_info->timestamp);
-
-			/* Check again if packet is still waiting */
-			if (bintime_isset(&tx_info->timestamp) && unlikely(
-			    bttosbt(time) > adapter->missing_tx_timeout)) {
-				if (tx_info->print_once)
-					device_printf(adapter->pdev,
-					    "Found a Tx that wasn't completed "
-					    "on time, qid %d, index %d.\n",
-					    tx_ring->qid, j);
-
-				tx_info->print_once = false;
-				missed_tx++;
-
-				if (unlikely(missed_tx >
-				    adapter->missing_tx_threshold)) {
-					device_printf(adapter->pdev,
-					    "The number of lost tx completion "
-					    "is above the threshold (%d > %d). "
-					    "Reset the device\n", missed_tx,
-					    adapter->missing_tx_threshold);
-					adapter->reset_reason =
-					    ENA_REGS_RESET_MISS_TX_CMPL;
-					adapter->trigger_reset = true;
-					return;
-				}
-			}
-		}
+		rc = check_missing_comp_in_queue(adapter, tx_ring);
+		if (unlikely(rc))
+			return;
 
 		budget--;
 		if (budget == 0) {

commit 46280bd60974634e9c13de5df470805701414bd0

ena: Re-Enable per-packet missing tx completion print
Commit [1] first added the ena_tx_buffer.print_once member,
so that a message about a missing tx completion is printed only
once per packet (and not every second when the watchdog runs).
In this commit print_once is initialized to true, and is set back
to false after detecting a missing tx completion and printing
a warning about it to dmesg.

Commit [2] incorrectly reverses the values assigned to print_once.
The variable is initialized to be true but is checked to be false
when a missing tx completion is detected. This is never true, and
therefore the warning print for each missing tx completion is never
printed since this commit.

Commit [3] added time passed since last TX cleanup to the missing
tx completions per-packet print. However, due to the issue in commit
[2], this time is never printed.

This commit reverses back the values assigned to ena_tx_buffer.print_once
erroneously by commit [2], bringing back to life the missing tx
completion per-packet print.

Also add a space after "." in the missing tx completion print.

[1] - 9b8d05b8ac78 ("Add support for Amazon Elastic Network Adapter (ENA) NIC")
[2] - 74dba3ad7851 ("Split function checking for missing TX completion in ENA driver")
[3] - d8aba82b5ca7 ("ena: Store ticks of last Tx cleanup")

Fixes: 74dba3ad7851 ("Split function checking for missing TX completion in ENA driver")
Fixes: d8aba82b5ca7 ("ena: Store ticks of last Tx cleanup")
MFC after: 2 weeks
Sponsored by: Amazon, Inc.

sys/dev/ena/ena.c
@@ -3019,19 +3019,19 @@ check_missing_comp_in_tx_queue(struct ena_adapter *adapter,
 		/* Check again if packet is still waiting */
 		if (unlikely(time_offset > adapter->missing_tx_timeout)) {
 
-			if (!tx_buf->print_once) {
+			if (tx_buf->print_once) {
 				time_since_last_cleanup = TICKS_2_USEC(ticks -
 				    tx_ring->tx_last_cleanup_ticks);
 				missing_tx_comp_to = sbttoms(
 				    adapter->missing_tx_timeout);
 				ena_log(pdev, WARN,
-				    "Found a Tx that wasn't completed on time, qid %d, index %d."
+				    "Found a Tx that wasn't completed on time, qid %d, index %d. "
 				    "%d usecs have passed since last cleanup. Missing Tx timeout value %d msecs.\n",
 				    tx_ring->qid, i, time_since_last_cleanup,
 				    missing_tx_comp_to);
 			}
 
-			tx_buf->print_once = true;
+			tx_buf->print_once = false;
 			missed_tx++;
 		}
 	}