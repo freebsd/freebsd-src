c91bf3cb34f0c1238a70e674bf23f89cccddfaec
Brooks Davis <brooks@FreeBSD.org>
2023-01-20 18:03:39 +0000

    epoch: replace hand coded assertion
    The assertion is equivalent to kstack_contains() so use that rather
    than spelling it out.
    
    Suggested by:	jhb
    Reviewed by:	jhb
    MFC after:	1 week
    Sponsored by:	DARPA, AFRL
    Differential Revision:	https://reviews.freebsd.org/D38107
    
    (cherry picked from commit fa1d803c0f652d72840a3c59139baf9d30792860)

--- sys/kern/subr_epoch.c
+++ sys/kern/subr_epoch.c
@@ -443,9 +443,7 @@ _epoch_enter_preempt(epoch_t epoch, epoch_tracker_t et EPOCH_FILE_LINE)
 
 	MPASS(cold || epoch != NULL);
 	td = curthread;
-	MPASS((vm_offset_t)et >= td->td_kstack &&
-	    (vm_offset_t)et + sizeof(struct epoch_tracker) <=
-	    td->td_kstack + td->td_kstack_pages * PAGE_SIZE);
+	MPASS(kstack_contains(td, (vm_offset_t)et, sizeof(*et)));
 
 	INIT_CHECK(epoch);
 	MPASS(epoch->e_flags & EPOCH_PREEMPT);
