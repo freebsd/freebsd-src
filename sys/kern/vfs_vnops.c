432e7797d5dd2a15267db85a3beaef7a03620015
Konstantin Belousov <kib@FreeBSD.org>
2023-01-20 08:45:35 +0200

    Handle int rank issues in in vn_getsize_locked() and vn_seek()
    In vn_getsize_locked(), when storing vattr.va_size of type u_quad_t into
    off_t size, we must avoid overflow.
    
    Then, the check for fsize < 0, introduced in the commit
    d7b7284d07d4363bad0e289b3afac32cff11ca07 'vfs: add vn_getsize', is nop [1].
    
    Reported and reviewed by:	jhb
    Coverity CID:	1502346
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D38133

--- sys/kern/vfs_vnops.c
+++ sys/kern/vfs_vnops.c
@@ -2656,9 +2656,7 @@ vn_seek(struct file *fp, off_t offset, int whence, struct thread *td)
 		if (fsize == 0 && vp->v_type == VCHR &&
 		    fo_ioctl(fp, DIOCGMEDIASIZE, &size, cred, td) == 0)
 			fsize = size;
-		if (noneg &&
-		    (fsize > OFF_MAX ||
-		    (offset > 0 && fsize > OFF_MAX - offset))) {
+		if (noneg && offset > 0 && fsize > OFF_MAX - offset) {
 			error = EOVERFLOW;
 			break;
 		}
