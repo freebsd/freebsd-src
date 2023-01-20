ad7aa28c1d1a522e423627f05188e432149490eb
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2023-01-20 20:23:59 +0100

    linuxkpi: Add `msix_cap` to `struct pci_dev`
    At the same time, define `PCI_MSIX_FLAGS`and `PCI_MSIX_FLAGS_ENABLE`.
    
    Reviewed by:	manu
    Approved by:	manu
    Differential Revision:	https://reviews.freebsd.org/D38160
    
    (cherry picked from commit 4152ce2139548e3e3c28c1e224747fa5d10ae656)

--- sys/compat/linuxkpi/common/include/linux/pci.h
+++ sys/compat/linuxkpi/common/include/linux/pci.h
@@ -172,6 +172,8 @@ MODULE_PNP_INFO("U32:vendor;U32:device;V32:subvendor;V32:subdevice",	\
 #define	PCI_MSI_ADDRESS_HI	PCIR_MSI_ADDR_HIGH
 #define	PCI_MSI_FLAGS		PCIR_MSI_CTRL
 #define	PCI_MSI_FLAGS_ENABLE	PCIM_MSICTRL_MSI_ENABLE
+#define	PCI_MSIX_FLAGS		PCIR_MSIX_CTRL
+#define	PCI_MSIX_FLAGS_ENABLE	PCIM_MSIXCTRL_MSIX_ENABLE
 
 #define PCI_EXP_LNKCAP_CLKPM	0x00040000
 #define PCI_EXP_DEVSTA_TRPND	0x0020
@@ -327,6 +329,7 @@ struct pci_dev {
 	unsigned int		devfn;
 	uint32_t		class;
 	uint8_t			revision;
+	uint8_t			msix_cap;
 	bool			msi_enabled;
 
 	TAILQ_HEAD(, pci_mmio_region)	mmio;
