a893b07fbb0cacb1f237e77a59ccc4d3e5881658
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2023-01-20 21:13:32 +0100

    linuxkpi: Define `pm_suspend_target_state` in <linux/suspend.h>
    It is set to `PM_SUSPEND_ON`.
    
    Reviewed by:	manu
    Approved by:	manu
    Differential Revision:	https://reviews.freebsd.org/D38161

--- sys/compat/linuxkpi/common/include/linux/suspend.h
+++ sys/compat/linuxkpi/common/include/linux/suspend.h
@@ -3,6 +3,13 @@
 #ifndef _LINUXKPI_LINUX_SUSPEND_H_
 #define _LINUXKPI_LINUX_SUSPEND_H_
 
+typedef int suspend_state_t;
+
+extern suspend_state_t pm_suspend_target_state;
+
+#define	PM_SUSPEND_ON		0
+#define	PM_SUSPEND_TO_IDLE	1
+
 static inline int
 pm_suspend_via_firmware()
 {
