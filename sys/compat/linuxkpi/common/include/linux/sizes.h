ea42177215c3e48978d1ef5c95617b4a587e33cf
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2023-01-20 18:17:37 +0100

    linuxkpi: Adds more `SZ_*` macros
    `SZ_2K` and `SZ_1G` were defined in irdma. They are removed from this
    driver.
    
    Reviewed by:	manu
    Approved by:	manu
    Differential Revision:	https://reviews.freebsd.org/D38150

--- sys/compat/linuxkpi/common/include/linux/sizes.h
+++ sys/compat/linuxkpi/common/include/linux/sizes.h
@@ -32,6 +32,7 @@
 #define	_LINUXKPI_LINUX_SIZES_H_
 
 #define SZ_1K	(1024 * 1)
+#define SZ_2K	(1024 * 2)
 #define SZ_4K	(1024 * 4)
 #define SZ_8K	(1024 * 8)
 #define SZ_16K	(1024 * 16)
@@ -43,9 +44,22 @@
 
 #define SZ_1M	(1024 * 1024 * 1)
 #define SZ_2M	(1024 * 1024 * 2)
+#define SZ_4M	(1024 * 1024 * 4)
 #define SZ_8M	(1024 * 1024 * 8)
 #define SZ_16M	(1024 * 1024 * 16)
 #define SZ_32M	(1024 * 1024 * 32)
 #define SZ_64M	(1024 * 1024 * 64)
+#define SZ_128M	(1024 * 1024 * 128)
+#define SZ_256M	(1024 * 1024 * 256)
+#define SZ_512M	(1024 * 1024 * 512)
+
+#define SZ_1G	(1024 * 1024 * 1024 * 1)
+#define SZ_2G	(1024 * 1024 * 1024 * 2)
+#define SZ_4G	(1024 * 1024 * 1024 * 4)
+#define SZ_8G	(1024 * 1024 * 1024 * 8)
+#define SZ_16G	(1024 * 1024 * 1024 * 16)
+#define SZ_32G	(1024 * 1024 * 1024 * 32)
+
+#define SZ_64T	(1024 * 1024 * 1024 * 1024 * 64)
 
 #endif
