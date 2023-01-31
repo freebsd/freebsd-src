83a49712afa276b7f320e0dd669c8dfd33f9a5ac
Konstantin Belousov <kib@FreeBSD.org>
2023-02-01 01:49:54 +0200

    kstack_contains(): account for struct pcb on stack
    for arm64, arm, powerpc, and riscv
    
    Reviewed by:	jhb
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D38320

--- sys/riscv/include/stack.h
+++ sys/riscv/include/stack.h
@@ -63,7 +63,8 @@ static __inline bool
 kstack_contains(struct thread *td, vm_offset_t va, size_t len)
 {
 	return (va >= td->td_kstack && va + len >= va &&
-	    va + len <= td->td_kstack + td->td_kstack_pages * PAGE_SIZE);
+	    va + len <= td->td_kstack + td->td_kstack_pages * PAGE_SIZE -
+	    sizeof(struct pcb));
 }
 #endif	/* _SYS_PROC_H_ */
 
