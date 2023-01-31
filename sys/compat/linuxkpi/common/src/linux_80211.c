8e45120aceeb5a1457b9406c9b1543e5a1d1d314
Bjoern A. Zeeb <bz@FreeBSD.org>
2023-01-31 23:12:30 +0000

    LinuxKPI: 802.11: fix indent in lkpi_wake_tx_queues()
    Fix indentation in lkpi_wake_tx_queues().
    No functional changes.
    
    Sponsored by:	The FreeBSD Foundation
    Approved by:	re (cperciva)
    
    (cherry picked from commit b0ddb44fd427d44b8153781ce9c461e393771aff)
    (cherry picked from commit 9e782bfab9042aed12c36746534b1e4e4ec6d597)

--- sys/compat/linuxkpi/common/src/linux_80211.c
+++ sys/compat/linuxkpi/common/src/linux_80211.c
@@ -861,24 +861,24 @@ lkpi_wake_tx_queues(struct ieee80211_hw *hw, struct ieee80211_sta *sta,
 	/* Wake up all queues to know they are allocated in the driver. */
 	for (tid = 0; tid < nitems(sta->txq); tid++) {
 
-			if (tid == IEEE80211_NUM_TIDS) {
-				IMPROVE("station specific?");
-				if (!ieee80211_hw_check(hw, STA_MMPDU_TXQ))
-					continue;
-			} else if (tid >= hw->queues)
+		if (tid == IEEE80211_NUM_TIDS) {
+			IMPROVE("station specific?");
+			if (!ieee80211_hw_check(hw, STA_MMPDU_TXQ))
 				continue;
+		} else if (tid >= hw->queues)
+			continue;
 
-			if (sta->txq[tid] == NULL)
-				continue;
+		if (sta->txq[tid] == NULL)
+			continue;
 
-			ltxq = TXQ_TO_LTXQ(sta->txq[tid]);
-			if (dequeue_seen && !ltxq->seen_dequeue)
-				continue;
+		ltxq = TXQ_TO_LTXQ(sta->txq[tid]);
+		if (dequeue_seen && !ltxq->seen_dequeue)
+			continue;
 
-			if (no_emptyq && skb_queue_empty(&ltxq->skbq))
-				continue;
+		if (no_emptyq && skb_queue_empty(&ltxq->skbq))
+			continue;
 
-			lkpi_80211_mo_wake_tx_queue(hw, sta->txq[tid]);
+		lkpi_80211_mo_wake_tx_queue(hw, sta->txq[tid]);
 	}
 }
 
