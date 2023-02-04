db5655124ca4047ac397b9421ca4a08868a49ae9
Rick Macklem <rmacklem@FreeBSD.org>
2023-02-04 14:45:23 -0800

    vfs_mount.c: Free exports structures in vfs_destroy_mount()
    During testing of exporting file systems in jails, I
    noticed that the export structures on a mount
    were not being free'd when the mount is dismounted.
    
    This bug appears to have been in the system for a
    very long time.  It would have resulted in a slow memory
    leak when exported file systems were dismounted.
    
    Prior to r362158, freeing the structures during dismount
    would not have been safe, since VFS_CHECKEXP() returned
    a pointer into an export structure, which might still have been
    used by the NFS server for an in-progress RPC when the file system
    is dismounted.  r362158 fixed this, so it should now be safe
    to free the structures in vfs_mount_destroy(), which is what
    this patch does.
    
    Reviewed by:	kib
    MFC after:	1 month
    Differential Revision:	https://reviews.freebsd.org/D38385

--- sys/sys/mount.h
+++ sys/sys/mount.h
@@ -1017,6 +1017,7 @@ void	vfs_periodic(struct mount *, int);
 int	vfs_busy(struct mount *, int);
 int	vfs_export			 /* process mount export info */
 	    (struct mount *, struct export_args *);
+void	vfs_free_addrlist(struct netexport *);
 void	vfs_allocate_syncvnode(struct mount *);
 void	vfs_deallocate_syncvnode(struct mount *);
 int	vfs_donmount(struct thread *td, uint64_t fsflags,
