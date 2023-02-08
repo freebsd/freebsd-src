0dc69e5cc6a16374f5b118e4ce6c694797d1d31a
Dag-Erling Smørgrav <des@FreeBSD.org>
2023-02-08 23:58:51 +0100

    depend-cleanup.sh: Simplify the logic, and clean bootstrap tools.
    * There's no need to check if the file exists before grepping it; if it does not exist, grep will fail, which is what we want.  Just redirect the error message to /dev/null.
    * There's no need to split the .o and .pico cases; the bodies are identical, so combine the conditions.  Use a glob to avoid a false negative if one exists but not the other.
    * Also run depend-cleanup.sh on the bootstrap build tree.  This unbreaks the build after 0c9829e4884e.
    
    Sponsored by:	Klara, Inc.
    Sponsored by:	NetApp, Inc.
    Reviewed by:	imp, emaste
    Differential Revision:	https://reviews.freebsd.org/D38447

--- tools/build/depend-cleanup.sh
+++ tools/build/depend-cleanup.sh
@@ -28,18 +28,8 @@ fi
 # $3 source extension
 clean_dep()
 {
-	if [ -e "$OBJTOP"/$1/.depend.$2.pico ] && \
-	    egrep -qw "$2\.$3" "$OBJTOP"/$1/.depend.$2.pico; then \
-		echo "Removing stale dependencies and objects for $2.$3"; \
-		rm -f \
-		    "$OBJTOP"/$1/.depend.$2.* \
-		    "$OBJTOP"/$1/$2.*o \
-		    "$OBJTOP"/obj-lib32/$1/.depend.$2.* \
-		    "$OBJTOP"/obj-lib32/$1/$2.*o
-	fi
-	if [ -e "$OBJTOP"/$1/.depend.$2.o ] && \
-	    egrep -qw "$2\.$3" "$OBJTOP"/$1/.depend.$2.o; then \
-		echo "Removing stale dependencies and objects for $2.$3"; \
+	if egrep -qw "$2\.$3" "$OBJTOP"/$1/.depend.$2.*o 2>/dev/null; then
+		echo "Removing stale dependencies and objects for $2.$3"
 		rm -f \
 		    "$OBJTOP"/$1/.depend.$2.* \
 		    "$OBJTOP"/$1/$2.*o \
