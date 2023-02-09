8852c03cb606a1dcee4f4da1ded2d51b61cbafeb
Alexander V. Chernikov <melifaro@FreeBSD.org>
2023-02-09 14:31:34 +0000

    testing: handling non-root users with VNETs in pytest-based tests.
    Currently isolation and resource requirements are handled directly
     by the kyua runner, based on the requirements specified by the test.
    It works well for simple tests, but may cause discrepancy with tests
     doing complex pre-setups. For example, all tests that perform
     VNET setups require root access to properly function.
    
    This change adds additional handling of the "require_user" property
     within the python testing framework. Specifically, it requests
     root access if the test class signals its root requirements and
     drops privileges to the desired user after performing the pre-setup.
    
    Differential Revision: https://reviews.freebsd.org/D37923
    MFC after:	2 weeks

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,10 +7,14 @@ PLUGIN_ENABLED = False
 DEFAULT_HANDLER = None
 
 
+def set_handler(config):
+    global DEFAULT_HANDLER, PLUGIN_ENABLED
+    DEFAULT_HANDLER = ATFHandler(report_file_name=config.option.atf_file)
+    PLUGIN_ENABLED = True
+    return DEFAULT_HANDLER
+
+
 def get_handler():
-    global DEFAULT_HANDLER
-    if DEFAULT_HANDLER is None:
-        DEFAULT_HANDLER = ATFHandler()
     return DEFAULT_HANDLER
 
 
@@ -81,11 +85,9 @@ def pytest_configure(config):
         "markers", "timeout(dur): int/float with max duration in sec"
     )
 
-    global PLUGIN_ENABLED
-    PLUGIN_ENABLED = config.option.atf
-    if not PLUGIN_ENABLED:
+    if not config.option.atf:
         return
-    get_handler()
+    handler = set_handler(config)
 
     if config.option.collectonly:
         # Need to output list of tests to stdout, hence override
@@ -93,6 +95,8 @@ def pytest_configure(config):
         reporter = config.pluginmanager.getplugin("terminalreporter")
         if reporter:
             config.pluginmanager.unregister(reporter)
+    else:
+        handler.setup_configure()
 
 
 def pytest_collection_modifyitems(session, config, items):
@@ -114,6 +118,12 @@ def pytest_collection_finish(session):
         handler.list_tests(session.items)
 
 
+def pytest_runtest_setup(item):
+    if PLUGIN_ENABLED:
+        handler = get_handler()
+        handler.setup_method_pre(item)
+
+
 def pytest_runtest_logreport(report):
     if PLUGIN_ENABLED:
         handler = get_handler()
@@ -123,4 +133,4 @@ def pytest_runtest_logreport(report):
 def pytest_unconfigure(config):
     if PLUGIN_ENABLED and config.option.atf_file:
         handler = get_handler()
-        handler.write_report(config.option.atf_file)
+        handler.write_report()
