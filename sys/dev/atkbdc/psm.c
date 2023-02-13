849a381488c0fb86ffceb72abec5ba273391e053
Mateusz Guzik <mjg@FreeBSD.org>
2023-02-13 18:38:49 +0000

    psm: ansify
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

--- sys/dev/atkbdc/psm.c
+++ sys/dev/atkbdc/psm.c
@@ -7395,9 +7395,8 @@ found:
  * All values should be numbers derived from getmicrouptime().
  */
 static int
-timeelapsed(start, secs, usecs, now)
-	const struct timeval *start, *now;
-	int secs, usecs;
+timeelapsed(const struct timeval *start, int secs, int usecs,
+    const struct timeval *now)
 {
 	struct timeval snow, tv;
 
