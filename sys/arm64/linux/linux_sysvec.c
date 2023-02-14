7194ef08e3c371b0d2957a19f02cd80ce8bf93a1
Dmitry Chagin <dchagin@FreeBSD.org>
2023-02-14 17:46:33 +0300

    linux(4): Cleanup includes under arm64/linux
    Cleanup unneeded includes, sort the rest according to style(9).
    No functional changes.
    
    MFC after:		2 weeks

--- sys/arm64/linux/linux_sysvec.c
+++ sys/arm64/linux/linux_sysvec.c
@@ -32,8 +32,6 @@ __FBSDID("$FreeBSD$");
 #define	__ELF_WORD_SIZE	64
 
 #include <sys/param.h>
-#include <sys/systm.h>
-#include <sys/cdefs.h>
 #include <sys/elf.h>
 #include <sys/exec.h>
 #include <sys/imgact.h>
@@ -45,18 +43,14 @@ __FBSDID("$FreeBSD$");
 #include <sys/mutex.h>
 #include <sys/proc.h>
 #include <sys/stddef.h>
-#include <sys/signalvar.h>
 #include <sys/syscallsubr.h>
 #include <sys/sysctl.h>
 #include <sys/sysent.h>
 
-#include <vm/vm.h>
 #include <vm/pmap.h>
+#include <vm/vm.h>
 #include <vm/vm_map.h>
-#include <vm/vm_extern.h>
-#include <vm/vm_object.h>
 #include <vm/vm_page.h>
-#include <vm/vm_param.h>
 
 #include <arm64/linux/linux.h>
 #include <arm64/linux/linux_proto.h>
