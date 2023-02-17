a0fdaf9a7b34b6f8fdab747f7a198158e66b57ab
Emmanuel Vadot <manu@FreeBSD.org>
2023-02-17 14:49:52 +0100

    linuxkpi: Add more suspend defines
    Needed by drm 5.15-lts
    
    Sponsored by:	Beckhoff Automation GmbH & Co. KG

--- sys/compat/linuxkpi/common/include/linux/suspend.h
+++ sys/compat/linuxkpi/common/include/linux/suspend.h
@@ -9,6 +9,10 @@ extern suspend_state_t pm_suspend_target_state;
 
 #define	PM_SUSPEND_ON		0
 #define	PM_SUSPEND_TO_IDLE	1
+#define	PM_SUSPEND_STANDBY	2
+#define	PM_SUSPEND_MEM		3
+#define	PM_SUSPEND_MIN		PM_SUSPEND_TO_IDLE
+#define	PM_SUSPEND_MAX		4
 
 static inline int
 pm_suspend_via_firmware(void)
