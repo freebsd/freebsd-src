b73664e30bd6092e71bb7e21143067d9c374715e
Konstantin Belousov <kib@FreeBSD.org>
2023-02-21 16:00:22 +0200

    kern/sysv_ipc.c: use ANSI C function definition
    Also remove pointless return's.
    
    Sponsored by:	The FreeBSD Foundation
    MFC after:	3 days

--- sys/kern/sysv_ipc.c
+++ sys/kern/sysv_ipc.c
@@ -56,23 +56,18 @@ void (*shmexit_hook)(struct vmspace *) = NULL;
 
 /* called from kern_fork.c */
 void
-shmfork(p1, p2)
-	struct proc *p1, *p2;
+shmfork(struct proc *p1, struct proc *p2)
 {
-
 	if (shmfork_hook != NULL)
 		shmfork_hook(p1, p2);
-	return;
 }
 
 /* called from kern_exit.c */
 void
 shmexit(struct vmspace *vm)
 {
-
 	if (shmexit_hook != NULL)
 		shmexit_hook(vm);
-	return;
 }
 #endif
 
