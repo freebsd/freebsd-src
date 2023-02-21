97e55d1a6efb3e7e21c19be1ff67db58dd0e9125
Gleb Smirnoff <glebius@FreeBSD.org>
2023-02-21 08:50:07 -0800

    Revert "unix/dgram: return EAGAIN instead of ENOBUFS when O_NONBLOCK set"
    This API change led to unexpected consequences with Go runtime. The
    Go runtime emulates blocking sockets over non-blocking sockets and
    for that uses available event dispatcher on the target OS, which is
    kevent(2) if availabe, with OS independent layer on top.  It expects
    that if whatever O_NONBLOCK socket returned ever EAGAIN, then it is
    supposed to be reported as writable by the event dispatcher. kevent(2)
    would never report a unix/dgram socket, since they never change their
    state, they always are writeable.  The expectations of Go are not
    literally specified by SUS, however they are in its spirit.  The SUS
    specifies EAGAIN for send(2) as "The socket's file descriptor is marked
    O_NONBLOCK and the requested operation would block" [1].  This doesn't
    apply to FreeBSD unix/dgram socket, it never blocks on send(2).
    
    Thus, changing API trying to mimic Linux was a mistake.  But what about
    the problem we tried to fix? Discussed that with Max Dounin of nginx,
    and we agreed that the log bomb described shall be fixed on nginx side,
    and it actually isn't specific to FreeBSD, may happen with nginx on any
    non-Linux system with a certain configuration.
    
    [1] https://pubs.opengroup.org/onlinepubs/9699919799/functions/send.html
    
    This reverts commit c16c011c38a285554d6dd46472c6f47c5135b0cd.

--- sys/kern/uipc_usrreq.c
+++ sys/kern/uipc_usrreq.c
@@ -1338,7 +1338,7 @@ uipc_sosend_dgram(struct socket *so, struct sockaddr *addr, struct uio *uio,
 		f = NULL;
 	} else {
 		soroverflow_locked(so2);
-		error = (so->so_state & SS_NBIO) ? EAGAIN : ENOBUFS;
+		error = ENOBUFS;
 		if (f->m_next->m_type == MT_CONTROL)
 			unp_scan(f->m_next, unp_freerights);
 	}
