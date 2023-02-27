40f41200e05d1d765e20f8912c5ec3ff0250bdc8
Mark Johnston <markj@FreeBSD.org>
2023-02-27 13:41:58 -0500

    netmap: Fix compiler warnings in tools
    - Remove write-only variables, or hide them in cases where their use is
      conditional or commented out.
    - Check for errors from cmd_apply() in nmreplay.
    - Use ANSI C definitions.
    
    Reviewed by:	vmaffione
    MFC after:	2 weeks
    Sponsored by:	Klara, Inc.
    Differential Revision:	https://reviews.freebsd.org/D38752

--- tools/tools/netmap/nmreplay.c
+++ tools/tools/netmap/nmreplay.c
@@ -1249,6 +1249,8 @@ main(int argc, char **argv)
 		err += cmd_apply(delay_cfg, d[i], qs, &qs->c_delay);
 		err += cmd_apply(bw_cfg, b[i], qs, &qs->c_bw);
 		err += cmd_apply(loss_cfg, l[i], qs, &qs->c_loss);
+		if (err != 0)
+			exit(1);
 	}
 
 	pthread_create(&bp[0].cons_tid, NULL, nmreplay_main, (void*)&bp[0]);
