3a9147772270c07e26648b3ff649a688edd8c521
Kyle Evans <kevans@FreeBSD.org>
2023-02-27 19:59:43 -0600

    usb: dwc3: implement hw.usb.xhci.use_polling
    Polling is currently only implemented in the xhci pci attachment.
    Adding it to dwc3 doesn't make it much uglier, and supporting it can be
    useful for confirming that hardware's otherwise functional when
    interrupts are apparently not firing.
    
    Reviewed by:	manu
    Differential Revision:	https://reviews.freebsd.org/D38816

--- sys/dev/usb/controller/dwc3.c
+++ sys/dev/usb/controller/dwc3.c
@@ -103,6 +103,17 @@ struct snps_dwc3_softc {
 
 #define	IS_DMA_32B	1
 
+static void
+xhci_interrupt_poll(void *_sc)
+{
+	struct xhci_softc *sc = _sc;
+
+	USB_BUS_UNLOCK(&sc->sc_bus);
+	xhci_interrupt(sc);
+	USB_BUS_LOCK(&sc->sc_bus);
+	usb_callout_reset(&sc->sc_callout, 1, (void *)&xhci_interrupt_poll, sc);
+}
+
 static int
 snps_dwc3_attach_xhci(device_t dev)
 {
@@ -133,18 +144,29 @@ snps_dwc3_attach_xhci(device_t dev)
 	sprintf(sc->sc_vendor, "Synopsys");
 	device_set_desc(sc->sc_bus.bdev, "Synopsys");
 
-	err = bus_setup_intr(dev, sc->sc_irq_res, INTR_TYPE_BIO | INTR_MPSAFE,
-	    NULL, (driver_intr_t *)xhci_interrupt, sc, &sc->sc_intr_hdl);
-	if (err != 0) {
-		device_printf(dev, "Failed to setup IRQ, %d\n", err);
-		sc->sc_intr_hdl = NULL;
-		return (err);
+	if (xhci_use_polling() == 0) {
+		err = bus_setup_intr(dev, sc->sc_irq_res, INTR_TYPE_BIO | INTR_MPSAFE,
+		    NULL, (driver_intr_t *)xhci_interrupt, sc, &sc->sc_intr_hdl);
+		if (err != 0) {
+			device_printf(dev, "Failed to setup IRQ, %d\n", err);
+			sc->sc_intr_hdl = NULL;
+			return (err);
+		}
 	}
 
 	err = xhci_init(sc, dev, IS_DMA_32B);
 	if (err != 0) {
 		device_printf(dev, "Failed to init XHCI, with error %d\n", err);
 		return (ENXIO);
+	}
+
+	usb_callout_init_mtx(&sc->sc_callout, &sc->sc_bus.bus_mtx, 0);
+
+	if (xhci_use_polling() != 0) {
+		device_printf(dev, "Interrupt polling at %dHz\n", hz);
+		USB_BUS_LOCK(&sc->sc_bus);
+		xhci_interrupt_poll(sc);
+		USB_BUS_UNLOCK(&sc->sc_bus);
 	}
 
 	err = xhci_start_controller(sc);
