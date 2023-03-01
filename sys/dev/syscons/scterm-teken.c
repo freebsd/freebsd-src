76a0183e361e7845cd59915166174400dbcead2a
Mateusz Guzik <mjg@FreeBSD.org>
2023-03-01 11:11:46 +0000

    syscons: whack __mips__ leftovers
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

--- sys/dev/syscons/scterm-teken.c
+++ sys/dev/syscons/scterm-teken.c
@@ -42,7 +42,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/consio.h>
 #include <sys/kbio.h>
 
-#if defined(__arm__) || defined(__mips__) || defined(__powerpc__)
+#if defined(__arm__) || defined(__powerpc__)
 #include <machine/sc_machdep.h>
 #else
 #include <machine/pc/display.h>
