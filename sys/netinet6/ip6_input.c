bb55bb1740d262ea5f4e083a9d116339932ee39a
Justin Hibbits <jhibbits@FreeBSD.org>
2023-03-06 16:29:24 -0500

    inet6: Include if_private.h in one more netstack file
    ip6_input() and ip6_destroy() both directly reference ifnet members.
    This file was missed in 3d0d5b21
    
    Fixes:		3d0d5b21 ("IfAPI: Explicitly include <net/if_private.h>...")
    Sponsored by:	Juniper Networks, Inc.

--- sys/netinet6/ip6_input.c
+++ sys/netinet6/ip6_input.c
@@ -95,6 +95,7 @@ __FBSDID("$FreeBSD$");
 #include <net/if.h>
 #include <net/if_var.h>
 #include <net/if_types.h>
+#include <net/if_private.h>
 #include <net/if_dl.h>
 #include <net/route.h>
 #include <net/netisr.h>
