96a0db365f5d315fd97b480bb3ecc84cba296d8d
Alexander V. Chernikov <melifaro@FreeBSD.org>
2023-03-09 14:36:52 +0000

    netlink: add rtsock-compatible header to use with netlink snl(3).
    Some routing socket defines (`RTM_` and `RTA_` ones) clash with the ones
     used by the the Netlink.
    As some rtsock definitions like interface flags or route flags are used in
     both netlink and rtsock, provide a convenient way to include those without
     running into the define collision.
    
    Differential Revision: https://reviews.freebsd.org/D38982
    MFC after:	2 weeks
    
    (cherry picked from commit 90d625122886a2db5b66d248807b89f4a0270466)

--- sys/net/route.h
+++ sys/net/route.h
@@ -268,6 +268,7 @@ struct rt_msghdr {
 
 #define RTM_VERSION	5	/* Up the ante and ignore older versions */
 
+#ifndef NETLINK_COMPAT
 /*
  * Message types.
  *
@@ -300,6 +301,8 @@ struct rt_msghdr {
 #define	RTM_IFANNOUNCE	0x11	/* (5) iface arrival/departure */
 #define	RTM_IEEE80211	0x12	/* (5) IEEE80211 wireless event */
 
+#endif /* NETLINK_COMPAT*/
+
 /*
  * Bitmask values for rtm_inits and rmx_locks.
  */
@@ -313,6 +316,8 @@ struct rt_msghdr {
 #define RTV_RTTVAR	0x80	/* init or lock _rttvar */
 #define RTV_WEIGHT	0x100	/* init or lock _weight */
 
+#ifndef NETLINK_COMPAT
+
 /*
  * Bitmask values for rtm_addrs.
  */
@@ -324,6 +329,8 @@ struct rt_msghdr {
 #define RTA_IFA		0x20	/* interface addr sockaddr present */
 #define RTA_AUTHOR	0x40	/* sockaddr for author of redirect */
 #define RTA_BRD		0x80	/* for NEWADDR, broadcast or p-p dest addr */
+
+#endif /* NETLINK_COMPAT*/
 
 /*
  * Index offsets for sockaddr array for alternate internal encoding.
