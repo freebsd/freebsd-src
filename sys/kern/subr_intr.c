39eef4e7849d4c128c82474b2f9386b954788a6e
Elliott Mitchell <ehem+freebsd@m5p.com>
2023-03-10 08:21:53 -0800

    intrng: change isrc_free_irq()'s return to void
    isrc_free_irq() is called from error and release paths which do not want
    to know about issues with the table.  They may panic() due to inability
    to recover from double-errors.  They may leak the isrc due to
    consequences of EBUSY from intr_event_destroy().  They may provide a
    less precise printf().  Since isrc_free_irq() can very easily recover
    from these conditions, do so and there is no longer any need for a
    return.
    
    Report as this should not occur.
    
    Differential Revision: https://reviews.freebsd.org/D38599

--- sys/kern/subr_intr.c
+++ sys/kern/subr_intr.c
@@ -467,18 +467,22 @@ isrc_set_irq(struct intr_irqsrc *isrc, u_int irq)
 /*
  *  Free unique interrupt number (resource handle) from interrupt source.
  */
-static inline int
+static inline void
 isrc_free_irq(struct intr_irqsrc *isrc)
 {
 
 	mtx_assert(&isrc_table_lock, MA_OWNED);
 
-	if (isrc->isrc_event.ie_irq >= intr_nirq)
-		return (EINVAL);
-	if (irq_sources[isrc->isrc_event.ie_irq] != isrc)
-		return (EINVAL);
+	if (isrc->isrc_event.ie_irq >= intr_nirq) {
+		printf("ERROR: reached %s() with no irq?\n", __func__);
+		return;
+	}
 
-	irq_sources[isrc->isrc_event.ie_irq] = NULL;
+	if (irq_sources[isrc->isrc_event.ie_irq] == isrc)
+		irq_sources[isrc->isrc_event.ie_irq] = NULL;
+	else
+		printf("ERROR: INTRNG: %s(): corrupt table entry?\n",
+		    __func__);
 	isrc->isrc_event.ie_irq = INTR_IRQ_INVALID;	/* just to be safe */
 
 	/*
@@ -489,8 +493,6 @@ isrc_free_irq(struct intr_irqsrc *isrc)
 	 */
 	if (irq_next_free >= intr_nirq)
 		irq_next_free = 0;
-
-	return (0);
 }
 
 device_t
@@ -555,7 +557,7 @@ intr_isrc_deregister(struct intr_irqsrc *isrc)
 	if (error == 0) {
 		if ((isrc->isrc_flags & INTR_ISRCF_IPI) == 0)
 			isrc_release_counters(isrc);
-		error = isrc_free_irq(isrc);
+		isrc_free_irq(isrc);
 	}
 	mtx_unlock(&isrc_table_lock);
 	return (error);
