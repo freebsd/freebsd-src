c2caf3b3313fe73bfc9b4b984c21da8d571f8798
Kyle Evans <kevans@FreeBSD.org>
2023-03-11 00:03:02 -0600

    flua: lposix: add more useful functions for general purpose scripts
    unistd:
    - _exit
    - close
    - fork
    - getpid
    - pipe
    - read
    - write
    
    libgen:
    - basename, dirname
    
    stdlib:
    - realpath
    
    These are sufficient for a number of real world scenarios.  In our first
    application of them, we use the libgen+stdlib additions to grab the
    script dir based on argv[0].  The unistd assortment is then used to
    outsource a bunch of work to forks and report back to the main process.
    
    Reviewed by:	emaste, imp
    Differential Revision:	https://reviews.freebsd.org/D39083

--- libexec/flua/modules/lposix.h
+++ libexec/flua/modules/lposix.h
@@ -7,6 +7,9 @@
 
 #include <lua.h>
 
+int luaopen_posix_libgen(lua_State *L);
+int luaopen_posix_stdlib(lua_State *L);
 int luaopen_posix_sys_stat(lua_State *L);
 int luaopen_posix_sys_utsname(lua_State *L);
+int luaopen_posix_sys_wait(lua_State *L);
 int luaopen_posix_unistd(lua_State *L);
