461ccb55d50ccf1b5bcfe1310fe32d72f8b0ecdd
Rob Norris <rob.norris@klarasystems.com>
2023-03-15 09:07:18 +1100

    dhclient: add ability to ignore options in offers
    A machine might exist on multiple networks, all of which offer, say, default
    routes or name servers. There's no easy way to indicate in the config
    that those options are only valid for a single interface.
    
    Now, we can write:
    
        interface "lan0" {
          request routers;
          require routers;
        }
        interface "lan1" {
          ignore routers;
        }
    
    And only take action on default routes offered on lan0.
    
    Tested by:	Jose Luis Duran <jlduran at gmail dot com>
    MFC after:	2 months
    Reviewed by:	allanjude, imp
    Sponsored by:	Zenith Electronics LLC
    Sponsored by:	Klara, Inc.
    Pull Request:	#693

--- sbin/dhclient/clparse.c
+++ sbin/dhclient/clparse.c
@@ -186,6 +186,7 @@ read_client_leases(void)
  *	hardware-declaration |
  *	REQUEST option-list |
  *	REQUIRE option-list |
+ *	IGNORE option-list |
  *	TIMEOUT number |
  *	RETRY number |
  *	REBOOT number |
@@ -248,6 +249,9 @@ parse_client_statement(FILE *cfile, struct interface_info *ip,
 		memset(config->required_options, 0,
 		    sizeof(config->required_options));
 		parse_option_list(cfile, config->required_options);
+		return;
+	case IGNORE:
+		parse_option_list(cfile, config->ignored_options);
 		return;
 	case TIMEOUT:
 		parse_lease_time(cfile, &config->timeout);
