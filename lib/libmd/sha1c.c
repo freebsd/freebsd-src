f8c3964334306954bd1912d01ee85043c5df7faf
Mateusz Guzik <mjg@FreeBSD.org>
2023-03-16 19:02:56 +0000

    libmd: ansify
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

--- lib/libmd/sha1c.c
+++ lib/libmd/sha1c.c
@@ -129,8 +129,7 @@ char *SHA1_version="SHA1 part of SSLeay 0.9.0b 11-Oct-1998";
 #  define	M_nl2c		nl2c
 #endif
 
-void SHA1_Init(c)
-SHA_CTX *c;
+void SHA1_Init(SHA_CTX *c)
 	{
 	c->h0=INIT_DATA_h0;
 	c->h1=INIT_DATA_h1;
@@ -143,10 +142,7 @@ SHA_CTX *c;
 	}
 
 void
-SHA1_Update(c, in, len)
-	SHA_CTX *c;
-	const void *in;
-	size_t len;
+SHA1_Update(SHA_CTX *c, const void *in, size_t len)
 {
 	u_int32_t *p;
 	int ew,ec,sw,sc;
@@ -277,9 +273,7 @@ SHA1_Update(c, in, len)
 	p[sw]=l;
 	}
 
-void SHA1_Transform(c,b)
-SHA_CTX *c;
-unsigned char *b;
+void SHA1_Transform(SHA_CTX *c, unsigned char *b)
 	{
 	u_int32_t p[16];
 #if BYTE_ORDER != BIG_ENDIAN
@@ -317,10 +311,7 @@ unsigned char *b;
 #ifndef SHA1_ASM
 
 void 
-sha1_block(c, W, num)
-	SHA_CTX *c;
-	const u_int32_t *W;
-	int num;
+sha1_block(SHA_CTX *c, const u_int32_t *W, int num)
 {
 	u_int32_t A,B,C,D,E,T;
 	u_int32_t X[16];
@@ -437,9 +428,7 @@ sha1_block(c, W, num)
 	}
 #endif
 
-void SHA1_Final(md, c)
-unsigned char *md;
-SHA_CTX *c;
+void SHA1_Final(unsigned char *md, SHA_CTX *c)
 	{
 	int i,j;
 	u_int32_t l;
