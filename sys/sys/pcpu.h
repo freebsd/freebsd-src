7881be671841e54924c49869fc4cdd59a5cff8fc
Mateusz Guzik <mjg@FreeBSD.org>
2023-03-21 07:27:25 +0000

    vfs: decouple freevnodes from vnode batching
    In principle one cpu can keep vholding vnodes, while another vdrops
    them. In this case it may be the local count will keep growing in an
    unbounded manner. Roll it up after a threshold instead.
    
    While here move it out of dpcpu into struct pcpu.
    
    Reviewed by:	kib (previous version)
    Differential Revision:	https://reviews.freebsd.org/D39195

--- sys/sys/pcpu.h
+++ sys/sys/pcpu.h
@@ -189,7 +189,8 @@ struct pcpu {
 	long		pc_cp_time[CPUSTATES];	/* statclock ticks */
 	struct _device	*pc_device;		/* CPU device handle */
 	void		*pc_netisr;		/* netisr SWI cookie */
-	int		pc_unused1;		/* unused field */
+	int8_t		pc_vfs_freevnodes;	/* freevnodes counter */
+	char		pc_unused1[3];		/* unused pad */
 	int		pc_domain;		/* Memory domain. */
 	struct rm_queue	pc_rm_queue;		/* rmlock list of trackers */
 	uintptr_t	pc_dynamic;		/* Dynamic per-cpu data area */
