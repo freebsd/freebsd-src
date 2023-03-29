1d907da955e9a17c3f329b1fcb93a9a2e0248d55
Corvin Köhne <corvink@FreeBSD.org>
2023-03-29 15:13:03 +0200

    bhyve: remove unnecessary const qualifier in acpi_device.h
    Those const qualifier declare that the function doesn't change the
    values internally. It makes no sense to add them in the header file.
    
    Reviewed by:		markj
    MFC after:		1 week
    Sponsored by:		Beckhoff Automation GmbH & Co. KG
    Differential Revision:	https://reviews.freebsd.org/D39318

--- usr.sbin/bhyve/acpi_device.h
+++ usr.sbin/bhyve/acpi_device.h
@@ -26,13 +26,13 @@ struct acpi_device;
  * @param[in]  hid     Hardware ID of the ACPI device. Should always be a NULL
  *                     terminated string.
  */
-int acpi_device_create(struct acpi_device **const new_dev,
-    struct vmctx *const vm_ctx, const char *const name, const char *const hid);
-void acpi_device_destroy(struct acpi_device *const dev);
+int acpi_device_create(struct acpi_device **new_dev, struct vmctx *vm_ctx,
+    const char *name, const char *hid);
+void acpi_device_destroy(struct acpi_device *dev);
 
-int acpi_device_add_res_fixed_ioport(struct acpi_device *const dev,
-    const UINT16 port, UINT8 length);
-int acpi_device_add_res_fixed_memory32(struct acpi_device *const dev,
-    const UINT8 write_protected, const UINT32 address, const UINT32 length);
+int acpi_device_add_res_fixed_ioport(struct acpi_device *dev, UINT16 port,
+    UINT8 length);
+int acpi_device_add_res_fixed_memory32(struct acpi_device *dev,
+    UINT8 write_protected, UINT32 address, UINT32 length);
 
-void acpi_device_write_dsdt(const struct acpi_device *const dev);
+void acpi_device_write_dsdt(const struct acpi_device *dev);
