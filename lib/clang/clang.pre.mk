d8ed7fa3d55d57f3bb3c2eeb8826e3e0a63ccdc3
Dimitry Andric <dim@FreeBSD.org>
2023-03-29 20:29:48 +0200

    Revert "Rework {clang,lldb,llvm}-tblgen invocations to add --write-if-changed"
    This reverts commit 8f391d9098083044793ff036dff8c96930fb1553. There are
    still a bunch of problems, and apparently ${.ALLSRC} does not work as I
    expected.
    
    Fixes:          8f391d909808

--- lib/clang/clang.pre.mk
+++ lib/clang/clang.pre.mk
@@ -2,8 +2,7 @@
 
 .include "llvm.pre.mk"
 
-CLANG_SRCS=		${LLVM_BASE}/clang
+CLANG_SRCS=	${LLVM_BASE}/clang
 
-CLANG_TBLGEN?=		clang-tblgen
-CLANG_TBLGEN_BIN!=	which ${CLANG_TBLGEN} || echo __nonexistent_clang_tblgen__
-CLANG_TBLGEN_OPTS?=	--write-if-changed
+CLANG_TBLGEN?=	clang-tblgen
+CLANG_TBLGEN_BIN!= which ${CLANG_TBLGEN}
