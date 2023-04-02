25b86f8559c2e7076daff56933217e95cd4398d4
Alexander V. Chernikov <melifaro@FreeBSD.org>
2023-04-02 14:10:16 +0000

    ping: fix failing test_pinger[_3_1_verbose_false] test.

--- sbin/ping/tests/test_ping.py
+++ sbin/ping/tests/test_ping.py
@@ -1231,7 +1231,7 @@ ping: quoted data too short (28 bytes) from 192.0.2.2
 PING 192.0.2.2 (192.0.2.2): 56 data bytes
 92 bytes from 192.0.2.2: Destination Host Unreachable
 Vr HL TOS  Len   ID Flg  off TTL Pro  cks      Src      Dst
- 4  5  00 0054 0001   0 0000  40  01 f6a4 192.0.2.1  192.0.2.2
+ 4  5  00 0054 0001   0 0000  40  01 f6a4 192.0.2.1  192.0.2.2 
 
 
 --- 192.0.2.2 ping statistics ---
