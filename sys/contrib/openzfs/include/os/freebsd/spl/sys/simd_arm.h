d6e24901349dc34a2f8040d67730eb2d510073ab
Mateusz Guzik <mjg@FreeBSD.org>
2023-04-07 21:44:49 +0000

    zfs: disable kernel fpu usage on arm and aarc64
    It is not implemented and causes panics on boot.
    
    This is a temporary measure until someone(tm) sorts it out.
    
    Reported by:	many
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

--- sys/contrib/openzfs/include/os/freebsd/spl/sys/simd_arm.h
+++ sys/contrib/openzfs/include/os/freebsd/spl/sys/simd_arm.h
@@ -46,7 +46,7 @@
 #include <machine/elf.h>
 #include <machine/md_var.h>
 
-#define	kfpu_allowed()		1
+#define	kfpu_allowed()		0
 #define	kfpu_initialize(tsk)	do {} while (0)
 #define	kfpu_begin()		do {} while (0)
 #define	kfpu_end()		do {} while (0)
