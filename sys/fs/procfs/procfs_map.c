0e9e9048ae08b38336ed3186b748bfd44a728f31
Joseph Koshy <jkoshy@FreeBSD.org>
2023-04-10 17:11:16 +0000

    procfs: Sync a documentation comment with the code.
    Approved by:	gnn (mentor)
    Differential Revision: https://reviews.freebsd.org/D39488

--- sys/fs/procfs/procfs_map.c
+++ sys/fs/procfs/procfs_map.c
@@ -202,8 +202,9 @@ procfs_doprocmap(PFS_FILL_ARGS)
 
 		/*
 		 * format:
-		 *  start, end, resident, private resident, cow, access, type,
-		 *         charged, charged uid.
+		 *  start, end, resident, private-resident, obj, access,
+		 *  ref_count, shadow_count, flags, cow, needs_copy, type,
+		 *  fullpath, charged, charged uid.
 		 */
 		error = sbuf_printf(sb,
 		    "0x%lx 0x%lx %d %d %p %s%s%s %d %d 0x%x %s %s %s %s %s %d\n",
