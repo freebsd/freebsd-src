f83ffe1d5da69acec9ace38b9626ae1667ea1edb
Zhenlei Huang <zlei@FreeBSD.org>
2023-04-12 12:20:38 +0800

    ice(4): Use the existing CTLFLAG_RWTUN flag definition
    Use it when possible, instead of separated flags.
    
    No functional change intended.
    
    Reviewed by:	hselasky, erj
    MFC after:	3 days
    Differential Revision:	https://reviews.freebsd.org/D39466

--- sys/dev/ice/ice_lib.c
+++ sys/dev/ice/ice_lib.c
@@ -5852,7 +5852,7 @@ ice_add_debug_tunables(struct ice_softc *sc)
 	debug_list = SYSCTL_CHILDREN(sc->debug_sysctls);
 
 	SYSCTL_ADD_U64(ctx, debug_list, OID_AUTO, "debug_mask",
-		       ICE_CTLFLAG_DEBUG | CTLFLAG_RW | CTLFLAG_TUN,
+		       ICE_CTLFLAG_DEBUG | CTLFLAG_RWTUN,
 		       &sc->hw.debug_mask, 0,
 		       ICE_SYSCTL_DEBUG_MASK_HELP);
 
