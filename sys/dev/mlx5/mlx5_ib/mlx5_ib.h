9162793e44f0d219750647fbbe90d50628b9cca5
Hans Petter Selasky <hselasky@FreeBSD.org>
2023-04-18 13:40:22 +0200

    mlx5: Remove unused debugfs node pointers.
    No functional change intended.
    
    Sponsored by:	NVIDIA Networking
    
    (cherry picked from commit 80b4ef6d10544d30b6cfeddca12ae28d81816201)

--- sys/dev/mlx5/mlx5_ib/mlx5_ib.h
+++ sys/dev/mlx5/mlx5_ib/mlx5_ib.h
@@ -576,18 +576,12 @@ struct mlx5_cache_ent {
 	spinlock_t		lock;
 
 
-	struct dentry	       *dir;
 	char                    name[4];
 	u32                     order;
 	u32			size;
 	u32                     cur;
 	u32                     miss;
 	u32			limit;
-
-	struct dentry          *fsize;
-	struct dentry          *fcur;
-	struct dentry          *fmiss;
-	struct dentry          *flimit;
 
 	struct mlx5_ib_dev     *dev;
 	struct work_struct	work;
@@ -599,7 +593,6 @@ struct mlx5_mr_cache {
 	struct workqueue_struct *wq;
 	struct mlx5_cache_ent	ent[MAX_MR_CACHE_ENTRIES];
 	int			stopped;
-	struct dentry		*root;
 	unsigned long		last_add;
 };
 
