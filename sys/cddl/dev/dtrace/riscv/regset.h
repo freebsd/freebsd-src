1fef7abdc76bd6f0c93775bd0c1b62b523abd20d
Christos Margiolis <christos@FreeBSD.org>
2023-04-20 16:25:37 +0000

    dtrace: add register bindings for RISC-V
    Reviewed by:	mhorne, markj
    MFC after:	1 week
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D39611

--- sys/cddl/dev/dtrace/riscv/regset.h
+++ sys/cddl/dev/dtrace/riscv/regset.h
@@ -42,7 +42,40 @@
 extern "C" {
 #endif
 
-/* Place here */
+#define REG_ZERO	0
+#define REG_RA		1
+#define REG_SP		2
+#define REG_GP		3
+#define REG_TP		4
+#define REG_T0		5
+#define REG_T1		6
+#define REG_T2		7
+#define REG_S0		8
+#define REG_FP		8
+#define REG_S1		9
+#define REG_A0		10
+#define REG_A1		11
+#define REG_A2		12
+#define REG_A3		13
+#define REG_A4		14
+#define REG_A5		15
+#define REG_A6		16
+#define REG_A7		17
+#define REG_S2		18
+#define REG_S3		19
+#define REG_S4		20
+#define REG_S5		21
+#define REG_S6		22
+#define REG_S7		23
+#define REG_S8		24
+#define REG_S9		25
+#define REG_S10		26
+#define REG_S11		27
+#define REG_T3		28
+#define REG_T4		29
+#define REG_T5		30
+#define REG_T6		31
+#define REG_PC		32
 
 #ifdef	__cplusplus
 }
