ff80f64a6553dc52306096e30101487f61af0bb6
Dimitry Andric <dim@FreeBSD.org>
2023-04-23 15:22:44 +0200

    powerpc: fix a few pmap related functions to return correct types
    While experimenting with changing boolean_t to another type, I noticed
    that several powerpc pmap related functions returned the wrong type:
    boolean_t instead of int.
    
    Fix several declarations and definitions to match the actual pmap
    function types: pmap_dev_direct_mapped_t and pmap_ts_referenced_t.
    
    MFC after:	3 days

--- sys/powerpc/aim/mmu_oea.c
+++ sys/powerpc/aim/mmu_oea.c
@@ -320,7 +320,7 @@ vm_paddr_t moea_kextract(vm_offset_t);
 void moea_kenter_attr(vm_offset_t, vm_paddr_t, vm_memattr_t);
 void moea_kenter(vm_offset_t, vm_paddr_t);
 void moea_page_set_memattr(vm_page_t m, vm_memattr_t ma);
-boolean_t moea_dev_direct_mapped(vm_paddr_t, vm_size_t);
+int moea_dev_direct_mapped(vm_paddr_t, vm_size_t);
 static void moea_sync_icache(pmap_t, vm_offset_t, vm_size_t);
 void moea_dumpsys_map(vm_paddr_t pa, size_t sz, void **va);
 void moea_scan_init(void);
@@ -2658,7 +2658,7 @@ moea_bat_mapped(int idx, vm_paddr_t pa, vm_size_t size)
 	return (0);
 }
 
-boolean_t
+int
 moea_dev_direct_mapped(vm_paddr_t pa, vm_size_t size)
 {
 	int i;
