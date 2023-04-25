f7288e9ecf0873d583f0e858d213171cf41724c6
Alexander V. Chernikov <melifaro@FreeBSD.org>
2023-04-25 11:24:42 +0000

    arp: convert arp(8) to netlink.
    The change is intended to be fully transparent to the users.
    Similarly to route(8) and netstat(8), arp can be build without
     netlink by defining WITHOUT_NETLINK in make.conf.
    
    Differential Revision:	https://reviews.freebsd.org/D39720

---
+++ usr.sbin/arp/arp.h
@@ -0,0 +1,21 @@
+#ifndef _USR_SBIN_ARP_ARP_H_
+#define _USR_SBIN_ARP_ARP_H_
+
+int valid_type(int type);
+struct sockaddr_in *getaddr(char *host);
+int print_entries_nl(uint32_t ifindex, struct in_addr addr);
+
+struct arp_opts {
+	bool aflag;
+	bool nflag;
+	time_t expire_time;
+	int flags;
+};
+extern struct arp_opts opts;
+
+int print_entries_nl(uint32_t ifindex, struct in_addr addr);
+int delete_nl(uint32_t ifindex, char *host);
+int set_nl(uint32_t ifindex, struct sockaddr_in *dst, struct sockaddr_dl *sdl,
+    char *host);
+
+#endif
