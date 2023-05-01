4a781d2807057915ea4de602ddaf97d435068b42
Warner Losh <imp@FreeBSD.org>
2023-05-01 09:45:41 -0600

    stand: remove ZFS warning about computered but unused variable
    Toss a __unused on the unexpected_errors variable. We compute it, but
    never use it.
    
    Sponsored by:		Netflix

--- sys/cddl/boot/zfs/zfssubr.c
+++ sys/cddl/boot/zfs/zfssubr.c
@@ -1574,7 +1574,7 @@ vdev_raidz_read(vdev_t *vd, const blkptr_t *bp, void *data,
 	raidz_map_t *rm;
 	raidz_col_t *rc;
 	int c, error;
-	int unexpected_errors;
+	int unexpected_errors __unused;
 	int parity_errors;
 	int parity_untried;
 	int data_errors;
