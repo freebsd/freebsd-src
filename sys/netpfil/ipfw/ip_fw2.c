2c5f534845e62b7a21114c98ccd9d537d436430d
Igor Ostapenko <igor.ostapenko@pm.me>
2023-05-05 01:01:33 +0100

    sys/netpfil/ipfw/ip_fw2.c: typo (Compatiblity)
    https://bugs.freebsd.org/269753
    
    PR:                      269753
    Reported by:             Igor Ostapenko
    Approved by:             doc, src (delphij, imp, zlei)
    Differential revision:   https://reviews.freebsd.org/D38741

--- sys/netpfil/ipfw/ip_fw2.c
+++ sys/netpfil/ipfw/ip_fw2.c
@@ -2002,7 +2002,7 @@ do {								\
 					    >> 8) | (offset != 0));
 				} else {
 					/*
-					 * Compatiblity: historically bare
+					 * Compatibility: historically bare
 					 * "frag" would match IPv6 fragments.
 					 */
 					match = (cmd->arg1 == 0x1 &&
