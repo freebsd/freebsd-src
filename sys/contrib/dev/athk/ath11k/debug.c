5b81cc48ddd073d4c25acebf26d374e9bd408e94
Bjoern A. Zeeb <bz@FreeBSD.org>
2023-05-11 22:23:00 +0000

    ath11k: import ath11k driver
    Import BSD-3-Clause-Clear ath11k driver assumed to be
    based on Linux kvalo/ath.git master at
    6bae9de622d3ef4805aba40e763eb4b0975c4f6d.
    
    Complement the driver to make compile on FreeBSD
    using LinuxKPI with changes covered by #ifdef (__FreeBSD__).
    Further select updates were applied since the initial import
    in order to keep compiling along with other LinuxKPI based
    drivers.
    
    Add the module build framework but keep disconnected from the
    build for now.
    The current driver (or rather LinuxKPI) lacks support for some
    "qcom" bits needed in order to get things working.
    There was interest by various people to enhance support further.
    
    We initially only plan to support PCI parts but it would be great
    to further enhance qcom SoC support to run on several (cheap) APs.
    
    The firmware is provided by port net/wifi-firmware-ath11k-kmod.
    
    Given the lack of full license texts on most files this is
    imported under the draft policy for handling SPDX files (D29226)
    and with approval for BSD-3-Clause-Clear. [1]
    
    Approved by:    core (jhb, 2023-05-11) [1]
    MFC after:      2 months

---
+++ sys/contrib/dev/athk/ath11k/debug.c
@@ -0,0 +1,169 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+/*
+ * Copyright (c) 2018-2019 The Linux Foundation. All rights reserved.
+ */
+
+#include <linux/vmalloc.h>
+#include "core.h"
+#include "debug.h"
+
+void ath11k_info(struct ath11k_base *ab, const char *fmt, ...)
+{
+	struct va_format vaf = {
+		.fmt = fmt,
+	};
+	va_list args;
+
+	va_start(args, fmt);
+	vaf.va = &args;
+#if defined(__linux__)
+	dev_info(ab->dev, "%pV", &vaf);
+#elif defined(__FreeBSD__)
+	{
+		char *str;
+		vasprintf(&str, M_KMALLOC, fmt, args);
+		dev_printk(KERN_INFO, ab->dev, "%s", str);
+		free(str, M_KMALLOC);
+	}
+#endif
+	trace_ath11k_log_info(ab, &vaf);
+	va_end(args);
+}
+EXPORT_SYMBOL(ath11k_info);
+
+void ath11k_err(struct ath11k_base *ab, const char *fmt, ...)
+{
+	struct va_format vaf = {
+		.fmt = fmt,
+	};
+	va_list args;
+
+	va_start(args, fmt);
+	vaf.va = &args;
+#if defined(__linux__)
+	dev_err(ab->dev, "%pV", &vaf);
+#elif defined(__FreeBSD__)
+	{
+		char *str;
+		vasprintf(&str, M_KMALLOC, fmt, args);
+		dev_printk(KERN_ERR, ab->dev, "%s", str);
+		free(str, M_KMALLOC);
+	}
+#endif
+	trace_ath11k_log_err(ab, &vaf);
+	va_end(args);
+}
+EXPORT_SYMBOL(ath11k_err);
+
+void ath11k_warn(struct ath11k_base *ab, const char *fmt, ...)
+{
+	struct va_format vaf = {
+		.fmt = fmt,
+	};
+	va_list args;
+
+	va_start(args, fmt);
+	vaf.va = &args;
+#if defined(__linux__)
+	dev_warn_ratelimited(ab->dev, "%pV", &vaf);
+#elif defined(__FreeBSD__)
+	{
+		static linux_ratelimit_t __ratelimited;
+
+		if (linux_ratelimited(&__ratelimited)) {
+			char *str;
+			vasprintf(&str, M_KMALLOC, fmt, args);
+			dev_printk(KERN_WARN, ab->dev, "%s", str);
+			free(str, M_KMALLOC);
+		}
+	}
+#endif
+	trace_ath11k_log_warn(ab, &vaf);
+	va_end(args);
+}
+EXPORT_SYMBOL(ath11k_warn);
+
+#ifdef CONFIG_ATH11K_DEBUG
+
+void __ath11k_dbg(struct ath11k_base *ab, enum ath11k_debug_mask mask,
+		  const char *fmt, ...)
+{
+	struct va_format vaf;
+	va_list args;
+
+	va_start(args, fmt);
+
+	vaf.fmt = fmt;
+	vaf.va = &args;
+
+	if (ath11k_debug_mask & mask)
+#if defined(__linux__)
+		dev_printk(KERN_DEBUG, ab->dev, "%pV", &vaf);
+#elif defined(__FreeBSD__)
+	{
+		char *str;
+		vasprintf(&str, M_KMALLOC, fmt, args);
+		dev_printk(KERN_DEBUG, ab->dev, "%s", str);
+		free(str, M_KMALLOC);
+	}
+#endif
+
+	trace_ath11k_log_dbg(ab, mask, &vaf);
+
+	va_end(args);
+}
+EXPORT_SYMBOL(__ath11k_dbg);
+
+void ath11k_dbg_dump(struct ath11k_base *ab,
+		     enum ath11k_debug_mask mask,
+		     const char *msg, const char *prefix,
+		     const void *buf, size_t len)
+{
+#if defined(__linux__)
+	char linebuf[256];
+	size_t linebuflen;
+	const void *ptr;
+#elif defined(__FreeBSD__)
+        struct sbuf *sb;
+        int rc;
+#endif
+
+	if (ath11k_debug_mask & mask) {
+		if (msg)
+			__ath11k_dbg(ab, mask, "%s\n", msg);
+
+#if defined(__linux__)
+		for (ptr = buf; (ptr - buf) < len; ptr += 16) {
+			linebuflen = 0;
+			linebuflen += scnprintf(linebuf + linebuflen,
+						sizeof(linebuf) - linebuflen,
+						"%s%08x: ",
+						(prefix ? prefix : ""),
+						(unsigned int)(ptr - buf));
+			hex_dump_to_buffer(ptr, len - (ptr - buf), 16, 1,
+					   linebuf + linebuflen,
+					   sizeof(linebuf) - linebuflen, true);
+			dev_printk(KERN_DEBUG, ab->dev, "%s\n", linebuf);
+		}
+#elif defined(__FreeBSD__)
+		sb = sbuf_new_auto();
+		if (sb == NULL)
+			goto trace;
+
+		sbuf_hexdump(sb, buf, len, prefix, 0);
+		sbuf_trim(sb);
+		rc = sbuf_finish(sb);
+		if (rc == 0)
+			dev_printk(KERN_DEBUG, ab->dev, "%s\n", sbuf_data(sb));
+		sbuf_delete(sb);
+trace: ;
+#endif
+	}
+
+	/* tracing code doesn't like null strings */
+	trace_ath11k_log_dbg_dump(ab, msg ? msg : "", prefix ? prefix : "",
+				  buf, len);
+}
+EXPORT_SYMBOL(ath11k_dbg_dump);
+
+#endif /* CONFIG_ATH11K_DEBUG */
