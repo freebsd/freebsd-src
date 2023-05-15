d5edf13d013f3298e7f703c406223feb84810a73
Corvin Köhne <corvink@FreeBSD.org>
2023-05-15 13:00:06 +0200

    bhyve: add basic TPM device
    Add an empty TPM device struct which will be used for TPM emulation in
    subsequent commits.
    
    Reviewed by:		markj
    MFC after:		1 week
    Sponsored by:		Beckhoff Automation GmbH & Co. KG
    Differential Revision:	https://reviews.freebsd.org/D40452

---
+++ usr.sbin/bhyve/tpm_device.h
@@ -0,0 +1,18 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2023 Beckhoff Automation GmbH & Co. KG
+ * Author: Corvin Köhne <corvink@FreeBSD.org>
+ */
+
+#pragma once
+
+#include <vmmapi.h>
+
+#include "config.h"
+
+struct tpm_device;
+
+int tpm_device_create(struct tpm_device **new_dev, struct vmctx *vm_ctx,
+    nvlist_t *nvl);
+void tpm_device_destroy(struct tpm_device *dev);
