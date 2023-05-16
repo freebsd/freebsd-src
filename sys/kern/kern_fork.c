0282f87530b33ebba890794b07f75a2874357e3a
Dmitry Chagin <dchagin@FreeBSD.org>
2023-05-16 22:46:26 +0300

    ktrace: Simplify ae6ac587, drop the sa var declaration
    Suggested by:		kib
    MFC after:		5 days

--- sys/kern/kern_fork.c
+++ sys/kern/kern_fork.c
@@ -1125,9 +1125,6 @@ fork_exit(void (*callout)(void *, struct trapframe *), void *arg,
 void
 fork_return(struct thread *td, struct trapframe *frame)
 {
-#ifdef KTRACE
-	struct syscall_args *sa;
-#endif
 	struct proc *p;
 
 	p = td->td_proc;
@@ -1170,10 +1167,8 @@ fork_return(struct thread *td, struct trapframe *frame)
 	userret(td, frame);
 
 #ifdef KTRACE
-	if (KTRPOINT(td, KTR_SYSRET)) {
-		sa = &td->td_sa;
-		ktrsysret(sa->code, 0, 0);
-	}
+	if (KTRPOINT(td, KTR_SYSRET))
+		ktrsysret(td->td_sa.code, 0, 0);
 #endif
 }
 
