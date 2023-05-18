3058c5b51afec92401c28ecd1db3a5ca1cdb90b0
Peter Wemm <peter@FreeBSD.org>
2023-05-18 11:30:10 -0700

    libc: Update copyright text to match current templates.

--- lib/libc/gen/__getosreldate.c
+++ lib/libc/gen/__getosreldate.c
@@ -2,7 +2,6 @@
  * SPDX-License-Identifier: BSD-2-Clause
  *
  * Copyright (c) 2007 Peter Wemm
- * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
