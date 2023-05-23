5b82288767d3546a8d4537d42c211760cf2d1b80
Mateusz Guzik <mjg@FreeBSD.org>
2023-05-23 13:20:46 +0000

    amd64: zero-pad register dumps on panic
    de gustibus and so on
    
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

--- sys/amd64/amd64/trap.c
+++ sys/amd64/amd64/trap.c
@@ -930,15 +930,15 @@ trap_fatal(struct trapframe *frame, vm_offset_t eva)
 	printf("current process		= %d (%s)\n",
 	    curproc->p_pid, curthread->td_name);
 
-	printf("rdi: %16lx rsi: %16lx rdx: %16lx\n", frame->tf_rdi,
+	printf("rdi: %016lx rsi: %016lx rdx: %016lx\n", frame->tf_rdi,
 	    frame->tf_rsi, frame->tf_rdx);
-	printf("rcx: %16lx  r8: %16lx  r9: %16lx\n", frame->tf_rcx,
+	printf("rcx: %016lx  r8: %016lx  r9: %016lx\n", frame->tf_rcx,
 	    frame->tf_r8, frame->tf_r9);
-	printf("rax: %16lx rbx: %16lx rbp: %16lx\n", frame->tf_rax,
+	printf("rax: %016lx rbx: %016lx rbp: %016lx\n", frame->tf_rax,
 	    frame->tf_rbx, frame->tf_rbp);
-	printf("r10: %16lx r11: %16lx r12: %16lx\n", frame->tf_r10,
+	printf("r10: %016lx r11: %016lx r12: %016lx\n", frame->tf_r10,
 	    frame->tf_r11, frame->tf_r12);
-	printf("r13: %16lx r14: %16lx r15: %16lx\n", frame->tf_r13,
+	printf("r13: %016lx r14: %016lx r15: %016lx\n", frame->tf_r13,
 	    frame->tf_r14, frame->tf_r15);
 
 #ifdef KDB
