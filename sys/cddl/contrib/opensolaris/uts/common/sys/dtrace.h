27ff920e6a0e0a918b3addb8cb0704949e41b374
Christos Margiolis <christos@FreeBSD.org>
2023-05-23 17:19:04 +0300

    dtrace: remove ifdef around dtrace_instr_size()
    Architectures that are not included in the #ifdef won't be able to
    compile libdtrace. This was tested on an ARM64 build. If the ifdef is
    removed, libdtrace can be compiled with no problems, otherwise it fails
    at libdtrace.
    
    Reviewed by:	markj
    Approved by:	markj (mentor)
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D39948

--- sys/cddl/contrib/opensolaris/uts/common/sys/dtrace.h
+++ sys/cddl/contrib/opensolaris/uts/common/sys/dtrace.h
@@ -2427,9 +2427,7 @@ extern void dtrace_helpers_destroy(proc_t *);
 
 #endif /* _KERNEL */
 
-#if defined(__i386) || defined(__amd64) || defined (__riscv)
 extern int dtrace_instr_size(uint8_t *instr);
-#endif
 
 #if defined(__i386) || defined(__amd64)
 extern int dtrace_dis_get_byte(void *p);
