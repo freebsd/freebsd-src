270d20ce91c898c532ebaff76bb5b71c8544e928
Joseph Mingrone <jrm@FreeBSD.org>
2023-05-26 22:45:43 -0300

    libpcap: Update PACKAGE_STRING to 1.10.4
    Reviewed by:	emaste
    Fixes:		6f9cba8f8b5e (libpcap: Update to 1.10.3)
    Fixes:		dd744a896be3 (libpcap: Update to 1.10.4)
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D40295

--- lib/libpcap/config.h
+++ lib/libpcap/config.h
@@ -184,7 +184,7 @@
 #define PACKAGE_NAME "pcap"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "pcap 1.9.1"
+#define PACKAGE_STRING "pcap 1.10.4"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "pcap"
