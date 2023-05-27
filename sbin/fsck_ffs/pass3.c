67a1f80f71ca46021ab845016b9a0529405c0dc9
Kirk McKusick <mckusick@FreeBSD.org>
2023-05-26 22:55:11 -0700

    Do not try to adjust a directory depth when its reconnection is declined.
    MFC after:    1 week
    Sponsored by: The FreeBSD Foundation

--- sbin/fsck_ffs/pass3.c
+++ sbin/fsck_ffs/pass3.c
@@ -100,10 +100,10 @@ pass3(void)
 			if (linkup(orphan, inp->i_dotdot, NULL)) {
 				inp->i_parent = inp->i_dotdot = lfdir;
 				inoinfo(lfdir)->ino_linkcnt--;
+				inoinfo(orphan)->ino_state = DFOUND;
+				check_dirdepth(inp);
+				propagate();
 			}
-			inoinfo(orphan)->ino_state = DFOUND;
-			check_dirdepth(inp);
-			propagate();
 			continue;
 		}
 		pfatal("ORPHANED DIRECTORY LOOP DETECTED I=%lu",
