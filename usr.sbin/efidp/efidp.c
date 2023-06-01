c9f01b519843dac0802bb9d3c6775d86cbe9a52e
Elyes Haouas <ehaouas@noos.fr>
2023-06-01 17:41:34 -0600

    efidp: Fix typos
    Signed-off-by: Elyes Haouas <ehaouas@noos.fr>
    Reviewed by: imp
    Pull Request: https://github.com/freebsd/freebsd-src/pull/653

--- usr.sbin/efidp/efidp.c
+++ usr.sbin/efidp/efidp.c
@@ -38,7 +38,7 @@ __FBSDID("$FreeBSD$");
 #include <string.h>
 #include <unistd.h>
 
-#define MAXSIZE 65536	/* Everyting will be smaller than this, most 1000x smaller */
+#define MAXSIZE 65536	/* Everything will be smaller than this, most 1000x smaller */
 
 /* options descriptor */
 static struct option longopts[] = {
