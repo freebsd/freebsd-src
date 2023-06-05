e374f1ec937f70ab2c89e0b392b45a67752c4172
Ed Maste <emaste@FreeBSD.org>
2023-06-05 10:49:53 -0400

    openssh: include destination constraints for smartcard keys
    From openssh-portable 54ac4ab2b53ce9fcb66b8250dee91c070e4167ed,
    OpenBSD-Commit-ID: add879fac6903a1cb1d1e42c4309e5359c3d870f
    
    PR:		271839
    Sponsored by:	The FreeBSD Foundation
    Approved by:	so
    Security:	FreeBSD-SA-23:05.openssh
    Security:	CVE-2023-28531
    
    (cherry picked from commit 2b4fb1350ceea0f2a0f04b1b8f5bfd3c32329ae4)

--- crypto/openssh/authfd.c
+++ crypto/openssh/authfd.c
@@ -666,7 +666,7 @@ ssh_update_card(int sock, int add, const char *reader_id, const char *pin,
     struct dest_constraint **dest_constraints, size_t ndest_constraints)
 {
 	struct sshbuf *msg;
-	int r, constrained = (life || confirm);
+	int r, constrained = (life || confirm || dest_constraints);
 	u_char type;
 
 	if (add) {
