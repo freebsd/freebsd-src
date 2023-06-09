d9b466296bdd1aa3b25b0cb6f0f546af21e55c62
Warner Losh <imp@FreeBSD.org>
2023-06-09 17:18:54 -0600

    mvs: Eliminate unused variable.
    ccim is unused. Just remove it.
    
    PR: 271920
    Sponsored by:		Netflix
    
    (cherry picked from commit 17dce737e305891c88e2410469cd23d8cfad1ca3)

--- sys/dev/mvs/mvs_soc.c
+++ sys/dev/mvs/mvs_soc.c
@@ -205,7 +205,7 @@ static int
 mvs_ctlr_setup(device_t dev)
 {
 	struct mvs_controller *ctlr = device_get_softc(dev);
-	int ccc = ctlr->ccc, cccc = ctlr->cccc, ccim = 0;
+	int ccc = ctlr->ccc, cccc = ctlr->cccc;
 
 	/* Mask chip interrupts */
 	ATA_OUTL(ctlr->r_mem, CHIP_SOC_MIM, 0x00000000);
@@ -222,8 +222,6 @@ mvs_ctlr_setup(device_t dev)
 	ccc *= 150;
 	ATA_OUTL(ctlr->r_mem, HC_ICT, cccc);
 	ATA_OUTL(ctlr->r_mem, HC_ITT, ccc);
-	if (ccc)
-		ccim |= IC_HC0_COAL_DONE;
 	/* Enable chip interrupts */
 	ctlr->gmim = ((ccc ? IC_HC0_COAL_DONE :
 	    (IC_DONE_HC0 & CHIP_SOC_HC0_MASK(ctlr->channels))) |
