7ea8d02798055068682e80d8791556d3f81bda82
Richard Scheffenegger <rscheff@FreeBSD.org>
2023-06-20 23:23:01 +0200

    Update various sys/netinet source files to conform with the style(9) guide on how to label FALLTHOUGH in switch statements.
    No functional chance.
    
    Reviewed By:		tuexen, cc, #transport
    Sponsored by:		NetApp, Inc.
    Differential Revision:	https://reviews.freebsd.org/D40622

--- sys/netinet/tcp_log_buf.c
+++ sys/netinet/tcp_log_buf.c
@@ -1928,7 +1928,7 @@ tcp_log_state_change(struct tcpcb *tp, int state)
 	case TCP_LOG_STATE_CLEAR:
 		while ((log_entry = STAILQ_FIRST(&tp->t_logs)) != NULL)
 			tcp_log_remove_log_head(tp, log_entry);
-		/* Fall through */
+		/* FALLTHROUGH */
 
 	case TCP_LOG_STATE_OFF:
 		tp->_t_logstate = TCP_LOG_STATE_OFF;
