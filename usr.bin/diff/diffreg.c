e8f740fbc261b953ea4f22b80846f55c54225710
John Baldwin <jhb@FreeBSD.org>
2023-06-20 09:29:00 -0700

    diff: Fully comment out the jackpot variable.
    This fixes a set but unused warning.

--- usr.bin/diff/diffreg.c
+++ usr.bin/diff/diffreg.c
@@ -682,14 +682,14 @@ unravel(int p)
 static void
 check(FILE *f1, FILE *f2, int flags)
 {
-	int i, j, jackpot, c, d;
+	int i, j, /* jackpot, */ c, d;
 	long ctold, ctnew;
 
 	rewind(f1);
 	rewind(f2);
 	j = 1;
 	ixold[0] = ixnew[0] = 0;
-	jackpot = 0;
+	/* jackpot = 0; */
 	ctold = ctnew = 0;
 	for (i = 1; i <= len[0]; i++) {
 		if (J[i] == 0) {
@@ -759,7 +759,7 @@ check(FILE *f1, FILE *f2, int flags)
 					}
 				}
 				if (chrtran(c) != chrtran(d)) {
-					jackpot++;
+					/* jackpot++; */
 					J[i] = 0;
 					if (c != '\n' && c != EOF)
 						ctold += skipline(f1);
