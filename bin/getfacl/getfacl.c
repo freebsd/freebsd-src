781624ca2d054430052c828ba8d2c2eaf2d733e7
Kyle Evans <kevans@FreeBSD.org>
2023-06-21 23:58:33 -0500

    getfacl: free `acl` in print_acl error path
    Sponsored by:	Klara, Inc.

--- bin/getfacl/getfacl.c
+++ bin/getfacl/getfacl.c
@@ -157,6 +157,7 @@ print_acl(char *path, acl_type_t type, int hflag, int iflag, int nflag,
 	acl_text = acl_to_text_np(acl, 0, flags);
 	if (!acl_text) {
 		warn("%s: acl_to_text_np() failed", path);
+		(void)acl_free(acl);
 		return(-1);
 	}
 
