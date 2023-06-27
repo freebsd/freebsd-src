0b17f32f0456e0cdd41f21480d9d58326d91e839
John Baldwin <jhb@FreeBSD.org>
2023-06-27 10:19:31 -0700

    libipf: Remove set but unused variable from printfraginfo().
    Reported by:	GCC
    Differential Revision:	https://reviews.freebsd.org/D40652

--- sbin/ipf/libipf/printfraginfo.c
+++ sbin/ipf/libipf/printfraginfo.c
@@ -14,7 +14,6 @@
 void
 printfraginfo(char *prefix, struct ipfr *ifr)
 {
-	frentry_t fr;
 	int family;
 
 	PRINTF("%s", prefix);
@@ -25,14 +24,8 @@ printfraginfo(char *prefix, struct ipfr *ifr)
 		PRINTF("inet");
 		family = AF_INET;
 	}
-	fr.fr_flags = 0xffffffff;
 
 	PRINTF(" %s -> ", hostname(family, &ifr->ipfr_src));
-/*
-	if (kmemcpy((char *)&fr, (u_long)ifr->ipfr_rule,
-		    sizeof(fr)) == -1)
-		return;
- */
 	PRINTF("%s id %x ttl %lu pr %d pkts %u bytes %u seen0 %d ref %d\n",
 		hostname(family, &ifr->ipfr_dst), ifr->ipfr_id,
 		ifr->ipfr_ttl, ifr->ipfr_p, ifr->ipfr_pkts, ifr->ipfr_bytes,
