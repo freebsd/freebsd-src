70f7bf34e3b4d0934c8c38ff49852eb6be343ac5
John Baldwin <jhb@FreeBSD.org>
2023-06-28 11:11:00 -0700

    bsdinstall: Handle errors from geom_gettree.
    geom_gettree probably never fails, and if it does there isn't much of
    a fallback other than aborting partitioning.  However, a few places
    were checking the return value and not doing anything with it
    triggering a unused-but-set-variable warning.  Checking the errors
    resolves the warning.
    
    While here, check for errors in other places that weren't checking for
    them at all, remove a spurious double call (the second call overwrote
    the mesh structure leaking all the pointers from the first), and close
    a few resource leaks on error paths.
    
    Reviewed by:	imp, emaste
    Differential Revision:	https://reviews.freebsd.org/D40779
    
    (cherry picked from commit 23099099196548550461ba427dcf09dcfb01878d)

--- usr.sbin/bsdinstall/partedit/part_wizard.c
+++ usr.sbin/bsdinstall/partedit/part_wizard.c
@@ -58,31 +58,37 @@ part_wizard(const char *fsreq)
 
 startwizard:
 	error = geom_gettree(&mesh);
+	if (error != 0)
+		return (1);
 
 	dlg_put_backtitle();
-	error = geom_gettree(&mesh);
 	disk = boot_disk_select(&mesh);
-	if (disk == NULL)
+	if (disk == NULL) {
+		geom_deletetree(&mesh);
 		return (1);
+	}
 
 	dlg_clear();
 	dlg_put_backtitle();
 	schemeroot = wizard_partition(&mesh, disk);
 	free(disk);
+	geom_deletetree(&mesh);
 	if (schemeroot == NULL)
 		return (1);
 
-	geom_deletetree(&mesh);
 	dlg_clear();
 	dlg_put_backtitle();
 	error = geom_gettree(&mesh);
+	if (error != 0) {
+		free(schemeroot);
+		return (1);
+	}
 
 	error = wizard_makeparts(&mesh, schemeroot, fstype, 1);
+	free(schemeroot);
+	geom_deletetree(&mesh);
 	if (error)
 		goto startwizard;
-	free(schemeroot);
-
-	geom_deletetree(&mesh);
 
 	return (0);
 }
@@ -280,11 +286,13 @@ query:
 
 	if (strcmp(scheme, "MBR") == 0) {
 		struct gmesh submesh;
-		geom_gettree(&submesh);
-		gpart_create(provider_for_name(&submesh, disk),
-		    "freebsd", NULL, NULL, &retval,
-		    choice /* Non-interactive for "Entire Disk" */);
-		geom_deletetree(&submesh);
+
+		if (geom_gettree(&submesh) == 0) {
+			gpart_create(provider_for_name(&submesh, disk),
+			    "freebsd", NULL, NULL, &retval,
+			    choice /* Non-interactive for "Entire Disk" */);
+			geom_deletetree(&submesh);
+		}
 	} else {
 		retval = strdup(disk);
 	}
@@ -304,7 +312,7 @@ wizard_makeparts(struct gmesh *mesh, const char *disk, const char *fstype,
 	struct gmesh submesh;
 	char swapsizestr[10], rootsizestr[10];
 	intmax_t swapsize, available;
-	int retval;
+	int error, retval;
 
 	if (strcmp(fstype, "zfs") == 0) {
 		fsname = fsnames[1];
@@ -350,12 +358,16 @@ wizard_makeparts(struct gmesh *mesh, const char *disk, const char *fstype,
 	humanize_number(rootsizestr, 7, available - swapsize - 1024*1024,
 	    "B", HN_AUTOSCALE, HN_NOSPACE | HN_DECIMAL);
 
-	geom_gettree(&submesh);
+	error = geom_gettree(&submesh);
+	if (error != 0)
+		return (error);
 	pp = provider_for_name(&submesh, disk);
 	gpart_create(pp, fsname, rootsizestr, "/", NULL, 0);
 	geom_deletetree(&submesh);
 
-	geom_gettree(&submesh);
+	error = geom_gettree(&submesh);
+	if (error != 0)
+		return (error);
 	pp = provider_for_name(&submesh, disk);
 	gpart_create(pp, "freebsd-swap", swapsizestr, NULL, NULL, 0);
 	geom_deletetree(&submesh);
