318052bcc8678ff014600151f9b321a5ce223330
Dmitry Chagin <dchagin@FreeBSD.org>
2023-06-29 19:53:07 +0300

    libexecinfo: Enable backtrace_test.backtrace_fmt_basic on amd64 again
    Due to unwind tables generation enabled after e98b78dc for csu.
    
    PR:			241562, 246322, 246537
    Reviewed by:		kib, ngie
    Differential Revision:	https://reviews.freebsd.org/D40758

--- contrib/netbsd-tests/lib/libexecinfo/t_backtrace.c
+++ contrib/netbsd-tests/lib/libexecinfo/t_backtrace.c
@@ -151,11 +151,6 @@ ATF_TC_HEAD(backtrace_fmt_basic, tc)
 
 ATF_TC_BODY(backtrace_fmt_basic, tc)
 {
-#if defined(__amd64__)
-	if (atf_tc_get_config_var_as_bool_wd(tc, "ci", false))
-		atf_tc_skip("https://bugs.freebsd.org/246537");
-#endif
-
 	myfunc(12);
 
 	if (prevent_inline)
