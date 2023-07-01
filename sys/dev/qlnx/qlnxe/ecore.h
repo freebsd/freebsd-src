5e7be7eb251dd430f8179208a6fcb83afebb03b6
Mark O'Donovan <shiftee@posteo.net>
2023-07-01 11:06:51 -0600

    qlnxe: Fix building as in-kernel component
    Add some extra files for building the driver as part of the kernel.
    Change some #defines to match those used when building as a module.
    
    PR: 268354
    
    Signed-off-by: Mark O'Donovan <shiftee@posteo.net>
    Reviewed by: imp
    Pull Request: https://github.com/freebsd/freebsd-src/pull/779

--- sys/dev/qlnx/qlnxe/ecore.h
+++ sys/dev/qlnx/qlnxe/ecore.h
@@ -94,9 +94,10 @@ enum ecore_nvm_cmd {
 #define CONFIG_ECORE_SRIOV
 #define CONFIG_ECORE_ROCE
 #define CONFIG_ECORE_IWARP
-#define CONFIG_ECORE_FCOE
-#define CONFIG_ECORE_ISCSI
 #define CONFIG_ECORE_LL2
+#define CONFIG_ECORE_RDMA
+#define ECORE_CONFIG_DIRECT_HWFN
+#define QLNX_ENABLE_IWARP
 #endif
 #endif
 
