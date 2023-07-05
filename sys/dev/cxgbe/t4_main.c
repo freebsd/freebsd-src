9bc18f99f570884d33861a3887eb19711071cf4c
John Baldwin <jhb@FreeBSD.org>
2023-07-05 16:00:47 -0700

    ddb: Rework macros to make it easier to add new command tables.
    - Add new DB_DEFINE_TABLE and DB_DECLARE_TABLE macros to define new
      command tables.  DB_DECLARE_TABLE is intended for use in headers
      similar to MALLOC_DECLARE and SYSCTL_DECL.
    
      DB_DEFINE_TABLE takes three arguments, the name of the parent table,
      the command name, and the name of the table itself, e.g.
      DB_DEFINE_TABLE(show, foo, show_foo) defines a new "show foo" table.
    
    - DB_TABLE_COMMAND, DB_TABLE_COMMAND_FLAGS, DB_TABLE_ALIAS, and
      DB_ALIAS_FLAGS allow new commands and aliases to be defined.  These
      are similar to the existing DB_COMMAND, etc. except that they take
      an initial argument giving the name of the parent table, e.g.:
    
      DB_TABLE_COMMAND(show_foo, bar, db_show_foo_bar)
    
      defines a new "show foo bar" command.
    
    This provides a cleaner interface than the ad-hoc use of internal
    macros like _DB_SET that was required previously (e.g. in cxgbe(4)).
    
    This retires DB_FUNC macro as well as the internal _DB_FUNC macro.
    
    Reviewed by:	melifaro, kib, markj
    Differential Revision:	https://reviews.freebsd.org/D40819

--- sys/dev/cxgbe/t4_main.c
+++ sys/dev/cxgbe/t4_main.c
@@ -12921,10 +12921,9 @@ t4_dump_devlog(struct adapter *sc)
 	} while (i != first && !db_pager_quit);
 }
 
-static struct db_command_table db_t4_table = LIST_HEAD_INITIALIZER(db_t4_table);
-_DB_SET(_show, t4, NULL, db_show_table, 0, &db_t4_table);
+static DB_DEFINE_TABLE(show, t4, show_t4);
 
-DB_FUNC(devlog, db_show_devlog, db_t4_table, CS_OWN, NULL)
+DB_TABLE_COMMAND_FLAGS(show_t4, devlog, db_show_devlog, CS_OWN)
 {
 	device_t dev;
 	int t;
@@ -12950,7 +12949,7 @@ DB_FUNC(devlog, db_show_devlog, db_t4_table, CS_OWN, NULL)
 	t4_dump_devlog(device_get_softc(dev));
 }
 
-DB_FUNC(tcb, db_show_t4tcb, db_t4_table, CS_OWN, NULL)
+DB_TABLE_COMMAND_FLAGS(show_t4, tcb, db_show_t4tcb, CS_OWN)
 {
 	device_t dev;
 	int radix, tid, t;
