c81495a621c461b3d3395a7c5b0e73458201c443
Emmanuel Vadot <manu@FreeBSD.org>
2023-07-07 09:16:05 +0200

    fwget: Fix quoting
    This kind of quoting doesn't work, no idea why shellcheck wanted this.
    If there is a "safer" way to "fix" this feel free to do it just test
    that it's working after.
    
    Reported by:	Kenneth Raplee <kenrap@kennethraplee.com>
    Sponsored by:	Beckhoff Automation GmbH & Co. KG
    Fixes:	7ad4d94d5bf2 ("Fix some shell issues by adding quotes and replace backticks with $()")

--- usr.sbin/fwget/fwget.sh
+++ usr.sbin/fwget/fwget.sh
@@ -27,7 +27,7 @@
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-: "${LIBEXEC_PATH:='/usr/libexec/fwget'}"
+: ${LIBEXEC_PATH:="/usr/libexec/fwget"}
 
 usage()
 {
