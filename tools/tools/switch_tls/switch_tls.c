5c3b5af6a2f6ebcccb73d1472f0aac63cacdee7f
Alfonso Gregory <gfunni234@gmail.com>
2023-07-07 10:39:23 -0600

    Mark usage function as __dead2 in programs where it does not return
    In most cases, usage does not return, so mark them as __dead2. For the
    cases where they do return, they have not been marked __dead2.
    
    Reviewed by: imp
    Pull Request: https://github.com/freebsd/freebsd-src/pull/735

--- tools/tools/switch_tls/switch_tls.c
+++ tools/tools/switch_tls/switch_tls.c
@@ -61,7 +61,7 @@ static bool tcpswitchall(const char *, int);
 static bool tcpswitchbyname(const char *, const char *, const char *,
     const char *, int);
 static bool tcpswitchconn(const struct in_conninfo *, int);
-static void usage(void);
+static void usage(void) __dead2;
 
 /*
  * Switch a tcp connection.
