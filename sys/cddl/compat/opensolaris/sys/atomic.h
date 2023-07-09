c63c4e694cfb0a2cba46b91869f7630e2afd80ae
Jessica Clarke <jrtc27@FreeBSD.org>
2023-07-09 18:45:32 +0100

    Don't bother to check COMPAT_32BIT when __LP64__ is defined
    Under COMPAT_32BIT we are compiling 32-bit code and so __LP64__ is not
    defined, __ILP32__ is, and thus the check is completely redundant.
    
    Reviewed by:	brooks, jhb, imp
    Differential Revision:	https://reviews.freebsd.org/D40917

--- sys/cddl/compat/opensolaris/sys/atomic.h
+++ sys/cddl/compat/opensolaris/sys/atomic.h
@@ -157,7 +157,7 @@ atomic_dec_64_nv(volatile uint64_t *target)
 	return (atomic_add_64_nv(target, -1));
 }
 
-#if !defined(COMPAT_32BIT) && defined(__LP64__)
+#ifdef __LP64__
 static __inline void *
 atomic_cas_ptr(volatile void *target, void *cmp,  void *newval)
 {
@@ -171,6 +171,6 @@ atomic_cas_ptr(volatile void *target, void *cmp,  void *newval)
 	return ((void *)atomic_cas_32((volatile uint32_t *)target,
 	    (uint32_t)cmp, (uint32_t)newval));
 }
-#endif	/* !defined(COMPAT_32BIT) && defined(__LP64__) */
+#endif	/* __LP64__ */
 
 #endif	/* !_OPENSOLARIS_SYS_ATOMIC_H_ */
