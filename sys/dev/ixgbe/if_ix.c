6ad2a9a64f34ea3b376c3064fcba30b3c8e654dd
Piotr Kubaj <pkubaj@FreeBSD.org>
2023-07-11 18:45:04 +0200

    ix: remove stale comment
    Reported by:	Navdeep Parhar

--- sys/dev/ixgbe/if_ix.c
+++ sys/dev/ixgbe/if_ix.c
@@ -3678,7 +3678,6 @@ ixgbe_if_update_admin_status(if_ctx_t ctx)
 			ixgbe_fc_enable(&sc->hw);
 			/* Update DMA coalescing config */
 			ixgbe_config_dmac(sc);
-			/* should actually be negotiated value */
 			iflib_link_state_change(ctx, LINK_STATE_UP,
 			    ixgbe_link_speed_to_baudrate(adapter->link_speed));
 
