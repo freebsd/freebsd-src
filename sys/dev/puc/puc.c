2671288e39708d0744108ec434a20a54636a1a26
Mitchell Horne <mhorne@FreeBSD.org>
2023-07-24 09:52:19 -0300

    puc: fix man page reference
    The reference to puc(9) is wrong; the page does not exist, and drivers
    belong to section 4. Change the reference to puc(4), which does exist.
    
    MFC after:	3 days
    Sponsored by:	The FreeBSD Foundation

--- sys/dev/puc/puc.c
+++ sys/dev/puc/puc.c
@@ -74,7 +74,7 @@ const char puc_driver_name[] = "puc";
 static MALLOC_DEFINE(M_PUC, "PUC", "PUC driver");
 
 SYSCTL_NODE(_hw, OID_AUTO, puc, CTLFLAG_RD | CTLFLAG_MPSAFE, 0,
-    "puc(9) driver configuration");
+    "puc(4) driver configuration");
 
 struct puc_bar *
 puc_get_bar(struct puc_softc *sc, int rid)
