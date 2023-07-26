fa2299d723cce44206d864a1c4855eb4d7bc639f
Richard Scheffenegger <rscheff@FreeBSD.org>
2023-07-27 00:41:25 +0200

    tcp: Handle <RST,ACK> in SYN-RCVD
    Patch base stack to correctly handle the RST bit independently
    of other header flags per TCP RFC.
    
    MFC after: 1 week
    Reviewed By: tuexen, #transport
    Sponsored by: NetApp, Inc.
    Differential Revision: https://reviews.freebsd.org/D40982

--- sys/netinet/tcp_syncache.c
+++ sys/netinet/tcp_syncache.c
@@ -624,18 +624,6 @@ syncache_chkrst(struct in_conninfo *inc, struct tcphdr *th, struct mbuf *m,
 	SCH_LOCK_ASSERT(sch);
 
 	/*
-	 * Any RST to our SYN|ACK must not carry ACK, SYN or FIN flags.
-	 * See RFC 793 page 65, section SEGMENT ARRIVES.
-	 */
-	if (tcp_get_flags(th) & (TH_ACK|TH_SYN|TH_FIN)) {
-		if ((s = tcp_log_addrs(inc, th, NULL, NULL)))
-			log(LOG_DEBUG, "%s; %s: Spurious RST with ACK, SYN or "
-			    "FIN flag set, segment ignored\n", s, __func__);
-		TCPSTAT_INC(tcps_badrst);
-		goto done;
-	}
-
-	/*
 	 * No corresponding connection was found in syncache.
 	 * If syncookies are enabled and possibly exclusively
 	 * used, or we are under memory pressure, a valid RST
