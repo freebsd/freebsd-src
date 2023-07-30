b4d08c5273ca150c851f7eabdb426bfb3a77362d
Konstantin Belousov <kib@FreeBSD.org>
2023-07-30 04:51:52 +0300

    rtld: switch from malloc_aligned() to __crt_aligned_alloc()
    Use regular free(), since it works now.
    
    Reviewed by:	markj
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D41150

--- libexec/rtld-elf/xmalloc.c
+++ libexec/rtld-elf/xmalloc.c
@@ -75,34 +75,18 @@ xstrdup(const char *str)
 }
 
 void *
-malloc_aligned(size_t size, size_t align, size_t offset)
+xmalloc_aligned(size_t size, size_t align, size_t offset)
 {
-	char *mem, *res;
-	uintptr_t x;
+	void *res;
 
 	offset &= align - 1;
 	if (align < sizeof(void *))
 		align = sizeof(void *);
 
-	mem = xmalloc(size + 3 * align + offset);
-	x = roundup((uintptr_t)mem + sizeof(void *), align);
-	x += offset;
-	res = (void *)x;
-	x -= sizeof(void *);
-	memcpy((void *)x, &mem, sizeof(mem));
+	res = __crt_aligned_alloc_offset(align, size, offset);
+	if (res == NULL) {
+		rtld_fdputstr(STDERR_FILENO, "Out of memory\n");
+		_exit(1);
+	}
 	return (res);
-}
-
-void
-free_aligned(void *ptr)
-{
-	void *mem;
-	uintptr_t x;
-
-	if (ptr == NULL)
-		return;
-	x = (uintptr_t)ptr;
-	x -= sizeof(void *);
-	memcpy(&mem, (void *)x, sizeof(mem));
-	free(mem);
 }
