50f60b50bfeeca1242257e3ba034d8f8f91a2ca0
Vladimir Kondratyev <wulf@FreeBSD.org>
2023-07-31 17:33:14 +0300

    nvme(4): detect S3X NVMe controller in 2016-2017 MacBooks
    Adds support for detection of the S3X NVMe controller found in the
    13" MacBook Pro 2017 without Touch Bar (MacBook14,1)
    It is known to be used in following MacBooks:
    - Retina MacBook 2016 (MacBook9,1)
    - 13" MacBook Pro 2016 without Touch Bar (MacBook13,1)
    - 13" MacBook Pro 2016 with Touch Bar (MacBook13,2)

--- sys/dev/nvme/nvme_pci.c
+++ sys/dev/nvme/nvme_pci.c
@@ -95,6 +95,7 @@ static struct _pcsid
 	{ 0xa821144d,		0, 0, "Samsung PM1725", QUIRK_DELAY_B4_CHK_RDY },
 	{ 0xa822144d,		0, 0, "Samsung PM1725a", QUIRK_DELAY_B4_CHK_RDY },
 	{ 0x07f015ad,		0, 0, "VMware NVMe Controller" },
+	{ 0x2003106b,		0, 0, "Apple S3X NVMe Controller" },
 	{ 0x00000000,		0, 0, NULL  }
 };
 
