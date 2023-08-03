db2cc817faf924bd898046547dde984162f806ec
Martin Matuska <mm@FreeBSD.org>
2023-08-04 01:04:14 +0200

    libarchive: merge from vendor branch
    Changes to not yet connected unzip only.
    
    (cherry picked from commit 058ab969fd2f7a8d04240d1e9bc9d63918480226)

--- contrib/libarchive/unzip/test/test_x.c
+++ contrib/libarchive/unzip/test/test_x.c
@@ -25,8 +25,8 @@
  */
 #include "test.h"
 
-/* Test x arg - Exclude paths */
-DEFINE_TEST(test_x)
+/* Test x arg with single exclude path */
+DEFINE_TEST(test_x_single)
 {
 	const char *reffile = "test_basic.zip";
 	int r;
@@ -41,4 +41,40 @@ DEFINE_TEST(test_x)
 	assertTextFileContents("contents b\n", "test_basic/b");
 	assertFileNotExists("test_basic/c");
 	assertTextFileContents("contents CAPS\n", "test_basic/CAPS");
+}
+
+/* Test x arg with multiple exclude paths */
+DEFINE_TEST(test_x_multiple)
+{
+	const char *reffile = "test_basic.zip";
+	int r;
+
+	extract_reference_file(reffile);
+	r = systemf("%s %s -x test_basic/c test_basic/b >test.out 2>test.err", testprog, reffile);
+	assertEqualInt(0, r);
+	assertNonEmptyFile("test.out");
+	assertEmptyFile("test.err");
+
+	assertTextFileContents("contents a\n", "test_basic/a");
+	assertFileNotExists("test_basic/b");
+	assertFileNotExists("test_basic/c");
+	assertTextFileContents("contents CAPS\n", "test_basic/CAPS");
+}
+
+/* Test x arg with multiple exclude paths and a d arg afterwards */
+DEFINE_TEST(test_x_multiple_with_d)
+{
+	const char *reffile = "test_basic.zip";
+	int r;
+
+	extract_reference_file(reffile);
+	r = systemf("%s %s -x test_basic/c test_basic/b -d foobar >test.out 2>test.err", testprog, reffile);
+	assertEqualInt(0, r);
+	assertNonEmptyFile("test.out");
+	assertEmptyFile("test.err");
+
+	assertTextFileContents("contents a\n", "foobar/test_basic/a");
+	assertFileNotExists("foobar/test_basic/b");
+	assertFileNotExists("foobar/test_basic/c");
+	assertTextFileContents("contents CAPS\n", "foobar/test_basic/CAPS");
 }
