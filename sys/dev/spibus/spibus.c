c6889cbf664ecddf7fdce7c6d3c90a282c1b5d98
Vladimir Kondratyev <wulf@FreeBSD.org>
2023-08-03 19:10:50 +0300

    spibus(4): Allow IRQ resource to be released
    IRQ Resource is allocated on spibus(4). We must release it here too
    rather than propagate request down the tree.
    
    Fixes:	4dd8db62e9d3 ("Add IRQ resource to SPIBUS")
    
    MFC after:	1 week
    Reviewed by:	manu
    Differential Revision:	https://reviews.freebsd.org/D41243

--- sys/dev/spibus/spibus.c
+++ sys/dev/spibus/spibus.c
@@ -247,11 +247,11 @@ static device_method_t spibus_methods[] = {
 	/* Bus interface */
 	DEVMETHOD(bus_setup_intr,	bus_generic_setup_intr),
 	DEVMETHOD(bus_teardown_intr,	bus_generic_teardown_intr),
-	DEVMETHOD(bus_release_resource,	bus_generic_release_resource),
 	DEVMETHOD(bus_activate_resource, bus_generic_activate_resource),
 	DEVMETHOD(bus_deactivate_resource, bus_generic_deactivate_resource),
 	DEVMETHOD(bus_adjust_resource,	bus_generic_adjust_resource),
 	DEVMETHOD(bus_alloc_resource,	bus_generic_rl_alloc_resource),
+	DEVMETHOD(bus_release_resource,	bus_generic_rl_release_resource),
 	DEVMETHOD(bus_get_resource,	bus_generic_rl_get_resource),
 	DEVMETHOD(bus_set_resource,	bus_generic_rl_set_resource),
 	DEVMETHOD(bus_get_resource_list, spibus_get_resource_list),
