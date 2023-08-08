e4bd6f0e92152d31310a306c5ac8d0f2eae4ba83
Elliott Mitchell <ehem+freebsd@m5p.com>
2023-08-08 16:23:32 -0700

    kern: move clk_intr_event to clock.[ch]
    clk_intr_event is a clock interrupt, not a generalized interrupt.  As
    such everything should be in clock.h/kern_clock.c instead of
    interrupt.h/kern_intr.c.

--- sys/sys/clock.h
+++ sys/sys/clock.h
@@ -195,6 +195,11 @@ void clock_dbgprint_ct(device_t dev, int rw, const struct clocktime *ct);
 void clock_dbgprint_err(device_t dev, int rw, int err);
 void clock_dbgprint_ts(device_t dev, int rw, const struct timespec *ts);
 
+/*
+ * SWI for clock events
+ */
+extern struct	intr_event *clk_intr_event;
+
 #endif /* _KERNEL */
 
 #endif /* !_SYS_CLOCK_H_ */
