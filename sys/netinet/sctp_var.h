5e393834c59135d63a2c949957a4023fe51941b2
Michael Tuexen <tuexen@FreeBSD.org>
2023-08-08 13:05:39 +0200

    sctp: remove duplicate code
    No functional change intended.
    
    MFC after:	1 week

--- sys/netinet/sctp_var.h
+++ sys/netinet/sctp_var.h
@@ -321,6 +321,7 @@ struct sctp_tcb;
 struct sctphdr;
 
 void sctp_close(struct socket *so);
+void sctp_abort(struct socket *so);
 int sctp_disconnect(struct socket *so);
 ipproto_ctlinput_t sctp_ctlinput;
 int sctp_ctloutput(struct socket *, struct sockopt *);
