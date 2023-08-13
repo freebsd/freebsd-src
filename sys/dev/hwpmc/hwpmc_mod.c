3c872a70b883e4e2b38135bd6bd114ac9d2ea579
Konstantin Belousov <kib@FreeBSD.org>
2023-08-13 16:42:49 +0300

    hwpmc: on process exit, ensure that the owned log is closed
    Tested by:	pho (previous version)
    Reviewed by:	markj
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D41521

--- sys/dev/hwpmc/hwpmc_mod.c
+++ sys/dev/hwpmc/hwpmc_mod.c
@@ -4989,12 +4989,12 @@ pmc_process_exit(void *arg __unused, struct proc *p)
 	}
 	PMC_EPOCH_EXIT();
 
-	if (!is_using_hwpmcs)
-		return;
-
 	PMC_GET_SX_XLOCK();
 	PMCDBG3(PRC,EXT,1,"process-exit proc=%p (%d, %s)", p, p->p_pid,
 	    p->p_comm);
+
+	if (!is_using_hwpmcs)
+		goto out;
 
 	/*
 	 * Since this code is invoked by the last thread in an exiting process,
@@ -5116,6 +5116,8 @@ out:
 	 * If the process owned PMCs, free them up and free up memory.
 	 */
 	if ((po = pmc_find_owner_descriptor(p)) != NULL) {
+		if ((po->po_flags & PMC_PO_OWNS_LOGFILE) != 0)
+			pmclog_close(po);
 		pmc_remove_owner(po);
 		pmc_destroy_owner_descriptor(po);
 	}
