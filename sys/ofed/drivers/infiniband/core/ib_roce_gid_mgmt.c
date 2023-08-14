freebsd/freebsd-src

commit f1ec47f918f1829da1be19b50a11d4ac64785c04

ofed: Mechanically convert to IfAPI
Summary:
Because of the intricacies of this code it wasn't purely scripted, but
instead hand-mechanical.

Reviewed by:	hselasky
Sponsored by:	Juniper Networks, Inc.
Differential Revision: https://reviews.freebsd.org/D38560

sys/ofed/drivers/infiniband/core/ib_roce_gid_mgmt.c
@@ -56,7 +56,7 @@ enum gid_op_type {
 
 struct roce_netdev_event_work {
 	struct work_struct work;
-	struct ifnet *ndev;
+	if_t ndev;
 };
 
 struct roce_rescan_work {
@@ -91,7 +91,7 @@ unsigned long roce_gid_type_mask_support(struct ib_device *ib_dev, u8 port)
 EXPORT_SYMBOL(roce_gid_type_mask_support);
 
 static void update_gid(enum gid_op_type gid_op, struct ib_device *ib_dev,
-    u8 port, union ib_gid *gid, struct ifnet *ndev)
+    u8 port, union ib_gid *gid, if_t ndev)
 {
 	int i;
 	unsigned long gid_type_mask = roce_gid_type_mask_support(ib_dev, port);
@@ -119,9 +119,9 @@ static void update_gid(enum gid_op_type gid_op, struct ib_device *ib_dev,
 
 static int
 roce_gid_match_netdev(struct ib_device *ib_dev, u8 port,
-    struct ifnet *idev, void *cookie)
+    if_t idev, void *cookie)
 {
-	struct ifnet *ndev = (struct ifnet *)cookie;
+	if_t ndev = (if_t )cookie;
 	if (idev == NULL)
 		return (0);
 	return (ndev == idev);
@@ -129,7 +129,7 @@ roce_gid_match_netdev(struct ib_device *ib_dev, u8 port,
 
 static int
 roce_gid_match_all(struct ib_device *ib_dev, u8 port,
-    struct ifnet *idev, void *cookie)
+    if_t idev, void *cookie)
 {
 	if (idev == NULL)
 		return (0);
@@ -138,7 +138,7 @@ roce_gid_match_all(struct ib_device *ib_dev, u8 port,
 
 static int
 roce_gid_enum_netdev_default(struct ib_device *ib_dev,
-    u8 port, struct ifnet *idev)
+    u8 port, if_t idev)
 {
 	unsigned long gid_type_mask;
 
@@ -150,32 +150,81 @@ roce_gid_enum_netdev_default(struct ib_device *ib_dev,
 	return (hweight_long(gid_type_mask));
 }
 
+struct ipx_entry {
+	STAILQ_ENTRY(ipx_entry)	entry;
+	union ipx_addr {
+		struct sockaddr sa[0];
+		struct sockaddr_in v4;
+		struct sockaddr_in6 v6;
+	} ipx_addr;
+	if_t ndev;
+};
+
+STAILQ_HEAD(ipx_queue, ipx_entry);
+
+#ifdef INET
+static u_int
+roce_gid_update_addr_ifa4_cb(void *arg, struct ifaddr *ifa, u_int count)
+{
+	struct ipx_queue *ipx_head = arg;
+	struct ipx_entry *entry;
+
+	entry = kzalloc(sizeof(*entry), GFP_ATOMIC);
+	if (entry == NULL) {
+		pr_warn("roce_gid_update_addr_callback: "
+		    "couldn't allocate entry for IPv4 update\n");
+		return (0);
+	}
+	entry->ipx_addr.v4 = *((struct sockaddr_in *)ifa->ifa_addr);
+	entry->ndev = ifa->ifa_ifp;
+	STAILQ_INSERT_TAIL(ipx_head, entry, entry);
+
+	return (1);
+}
+#endif
+
+#ifdef INET6
+static u_int
+roce_gid_update_addr_ifa6_cb(void *arg, struct ifaddr *ifa, u_int count)
+{
+	struct ipx_queue *ipx_head = arg;
+	struct ipx_entry *entry;
+
+	entry = kzalloc(sizeof(*entry), GFP_ATOMIC);
+	if (entry == NULL) {
+		pr_warn("roce_gid_update_addr_callback: "
+		    "couldn't allocate entry for IPv6 update\n");
+		return (0);
+	}
+	entry->ipx_addr.v6 = *((struct sockaddr_in6 *)ifa->ifa_addr);
+	entry->ndev = ifa->ifa_ifp;
+
+	/* trash IPv6 scope ID */
+	sa6_recoverscope(&entry->ipx_addr.v6);
+	entry->ipx_addr.v6.sin6_scope_id = 0;
+
+	STAILQ_INSERT_TAIL(ipx_head, entry, entry);
+
+	return (1);
+}
+#endif
+
 static void
 roce_gid_update_addr_callback(struct ib_device *device, u8 port,
-    struct ifnet *ndev, void *cookie)
+    if_t ndev, void *cookie)
 {
-	struct ipx_entry {
-		STAILQ_ENTRY(ipx_entry)	entry;
-		union ipx_addr {
-			struct sockaddr sa[0];
-			struct sockaddr_in v4;
-			struct sockaddr_in6 v6;
-		} ipx_addr;
-		struct ifnet *ndev;
-	};
+	struct epoch_tracker et;
+	struct if_iter iter;
 	struct ipx_entry *entry;
-	struct ifnet *idev;
-#if defined(INET) || defined(INET6)
-	struct ifaddr *ifa;
-#endif
 	VNET_ITERATOR_DECL(vnet_iter);
 	struct ib_gid_attr gid_attr;
 	union ib_gid gid;
+	if_t ifp;
 	int default_gids;
 	u16 index_num;
 	int i;
 
-	STAILQ_HEAD(, ipx_entry) ipx_head;
+	struct ipx_queue ipx_head;
 
 	STAILQ_INIT(&ipx_head);
 
@@ -185,59 +234,24 @@ roce_gid_update_addr_callback(struct ib_device *device, u8 port,
 	VNET_LIST_RLOCK();
 	VNET_FOREACH(vnet_iter) {
 	    CURVNET_SET(vnet_iter);
-	    IFNET_RLOCK();
-	    CK_STAILQ_FOREACH(idev, &V_ifnet, if_link) {
-		struct epoch_tracker et;
-
-		if (idev != ndev) {
-			if (idev->if_type != IFT_L2VLAN)
+	    NET_EPOCH_ENTER(et);
+	    for (ifp = if_iter_start(&iter); ifp != NULL; ifp = if_iter_next(&iter)) {
+		if (ifp != ndev) {
+			if (if_gettype(ifp) != IFT_L2VLAN)
 				continue;
-			if (ndev != rdma_vlan_dev_real_dev(idev))
+			if (ifp != rdma_vlan_dev_real_dev(ifp))
 				continue;
 		}
 
 		/* clone address information for IPv4 and IPv6 */
-		NET_EPOCH_ENTER(et);
 #if defined(INET)
-		CK_STAILQ_FOREACH(ifa, &idev->if_addrhead, ifa_link) {
-			if (ifa->ifa_addr == NULL ||
-			    ifa->ifa_addr->sa_family != AF_INET)
-				continue;
-			entry = kzalloc(sizeof(*entry), GFP_ATOMIC);
-			if (entry == NULL) {
-				pr_warn("roce_gid_update_addr_callback: "
-				    "couldn't allocate entry for IPv4 update\n");
-				continue;
-			}
-			entry->ipx_addr.v4 = *((struct sockaddr_in *)ifa->ifa_addr);
-			entry->ndev = idev;
-			STAILQ_INSERT_TAIL(&ipx_head, entry, entry);
-		}
+		if_foreach_addr_type(ifp, AF_INET, roce_gid_update_addr_ifa4_cb, &ipx_head);
 #endif
 #if defined(INET6)
-		CK_STAILQ_FOREACH(ifa, &idev->if_addrhead, ifa_link) {
-			if (ifa->ifa_addr == NULL ||
-			    ifa->ifa_addr->sa_family != AF_INET6)
-				continue;
-			entry = kzalloc(sizeof(*entry), GFP_ATOMIC);
-			if (entry == NULL) {
-				pr_warn("roce_gid_update_addr_callback: "
-				    "couldn't allocate entry for IPv6 update\n");
-				continue;
-			}
-			entry->ipx_addr.v6 = *((struct sockaddr_in6 *)ifa->ifa_addr);
-			entry->ndev = idev;
-
-			/* trash IPv6 scope ID */
-			sa6_recoverscope(&entry->ipx_addr.v6);
-			entry->ipx_addr.v6.sin6_scope_id = 0;
-
-			STAILQ_INSERT_TAIL(&ipx_head, entry, entry);
-		}
+		if_foreach_addr_type(ifp, AF_INET6, roce_gid_update_addr_ifa6_cb, &ipx_head);
 #endif
-		NET_EPOCH_EXIT(et);
 	    }
-	    IFNET_RUNLOCK();
+	    NET_EPOCH_EXIT(et);
 	    CURVNET_RESTORE();
 	}
 	VNET_LIST_RUNLOCK();
@@ -315,12 +329,12 @@ roce_gid_queue_scan_event_handler(struct work_struct *_work)
 }
 
 static void
-roce_gid_queue_scan_event(struct ifnet *ndev)
+roce_gid_queue_scan_event(if_t ndev)
 {
 	struct roce_netdev_event_work *work;
 
 retry:
-	switch (ndev->if_type) {
+	switch (if_gettype(ndev)) {
 	case IFT_ETHER:
 		break;
 	case IFT_L2VLAN:
@@ -358,7 +372,7 @@ roce_gid_delete_all_event_handler(struct work_struct *_work)
 }
 
 static void
-roce_gid_delete_all_event(struct ifnet *ndev)
+roce_gid_delete_all_event(if_t ndev)
 {
 	struct roce_netdev_event_work *work;
 
@@ -380,7 +394,7 @@ roce_gid_delete_all_event(struct ifnet *ndev)
 static int
 inetaddr_event(struct notifier_block *this, unsigned long event, void *ptr)
 {
-	struct ifnet *ndev = netdev_notifier_info_to_ifp(ptr);
+	if_t ndev = netdev_notifier_info_to_ifp(ptr);
 
 	switch (event) {
 	case NETDEV_UNREGISTER:
@@ -404,7 +418,7 @@ static struct notifier_block nb_inetaddr = {
 static eventhandler_tag eh_ifnet_event;
 
 static void
-roce_ifnet_event(void *arg, struct ifnet *ifp, int event)
+roce_ifnet_event(void *arg, if_t ifp, int event)
 {
 	if (event != IFNET_EVENT_PCP || is_vlan_dev(ifp))
 		return;

sys/ofed/drivers/infiniband/core/core_priv.h
@@ -82,10 +82,10 @@ void ib_cache_setup(void);
 void ib_cache_cleanup(void);
 
 typedef void (*roce_netdev_callback)(struct ib_device *device, u8 port,
-	      struct ifnet *idev, void *cookie);
+	      if_t idev, void *cookie);
 
 typedef int (*roce_netdev_filter)(struct ib_device *device, u8 port,
-	     struct ifnet *idev, void *cookie);
+	     if_t idev, void *cookie);
 
 void ib_enum_roce_netdev(struct ib_device *ib_dev,
 			 roce_netdev_filter filter,
@@ -107,7 +107,7 @@ int ib_cache_gid_parse_type_str(const char *buf);
 const char *ib_cache_gid_type_str(enum ib_gid_type gid_type);
 
 void ib_cache_gid_set_default_gid(struct ib_device *ib_dev, u8 port,
-				  struct ifnet *ndev,
+				  if_t ndev,
 				  unsigned long gid_type_mask,
 				  enum ib_cache_gid_default_mode mode);
 
@@ -118,8 +118,8 @@ int ib_cache_gid_del(struct ib_device *ib_dev, u8 port,
 		     union ib_gid *gid, struct ib_gid_attr *attr);
 
 int ib_cache_gid_del_all_netdev_gids(struct ib_device *ib_dev, u8 port,
-				     struct ifnet *ndev);
-void ib_cache_gid_del_all_by_netdev(struct ifnet *ndev);
+				     if_t ndev);
+void ib_cache_gid_del_all_by_netdev(if_t ndev);
 
 int roce_gid_mgmt_init(void);
 void roce_gid_mgmt_cleanup(void);

sys/ofed/drivers/infiniband/core/ib_addr.c
@@ -140,32 +140,34 @@ rdma_copy_addr_sub(u8 *dst, const u8 *src, unsigned min, unsigned max)
 	memset(dst + min, 0, max - min);
 }
 
-int rdma_copy_addr(struct rdma_dev_addr *dev_addr, struct ifnet *dev,
+int rdma_copy_addr(struct rdma_dev_addr *dev_addr, if_t dev,
 		     const unsigned char *dst_dev_addr)
 {
+	int dev_type = if_gettype(dev);
+
 	/* check for loopback device */
-	if (dev->if_flags & IFF_LOOPBACK) {
+	if (if_getflags(dev) & IFF_LOOPBACK) {
 		dev_addr->dev_type = ARPHRD_ETHER;
 		memset(dev_addr->src_dev_addr, 0, MAX_ADDR_LEN);
 		memset(dev_addr->broadcast, 0, MAX_ADDR_LEN);
 		memset(dev_addr->dst_dev_addr, 0, MAX_ADDR_LEN);
-		dev_addr->bound_dev_if = dev->if_index;
+		dev_addr->bound_dev_if = if_getindex(dev);
 		return (0);
-	} else if (dev->if_type == IFT_INFINIBAND)
+	} else if (dev_type == IFT_INFINIBAND)
 		dev_addr->dev_type = ARPHRD_INFINIBAND;
-	else if (dev->if_type == IFT_ETHER || dev->if_type == IFT_L2VLAN)
+	else if (dev_type == IFT_ETHER || dev_type == IFT_L2VLAN)
 		dev_addr->dev_type = ARPHRD_ETHER;
 	else
 		dev_addr->dev_type = 0;
-	rdma_copy_addr_sub(dev_addr->src_dev_addr, IF_LLADDR(dev),
-			   dev->if_addrlen, MAX_ADDR_LEN);
-	rdma_copy_addr_sub(dev_addr->broadcast, dev->if_broadcastaddr,
-			   dev->if_addrlen, MAX_ADDR_LEN);
+	rdma_copy_addr_sub(dev_addr->src_dev_addr, if_getlladdr(dev),
+			   if_getaddrlen(dev), MAX_ADDR_LEN);
+	rdma_copy_addr_sub(dev_addr->broadcast, if_getbroadcastaddr(dev),
+			   if_getaddrlen(dev), MAX_ADDR_LEN);
 	if (dst_dev_addr != NULL) {
 		rdma_copy_addr_sub(dev_addr->dst_dev_addr, dst_dev_addr,
-				   dev->if_addrlen, MAX_ADDR_LEN);
+				   if_getaddrlen(dev), MAX_ADDR_LEN);
 	}
-	dev_addr->bound_dev_if = dev->if_index;
+	dev_addr->bound_dev_if = if_getindex(dev);
 	return 0;
 }
 EXPORT_SYMBOL(rdma_copy_addr);
@@ -173,7 +175,7 @@ EXPORT_SYMBOL(rdma_copy_addr);
 int rdma_translate_ip(const struct sockaddr *addr,
 		      struct rdma_dev_addr *dev_addr)
 {
-	struct ifnet *dev;
+	if_t dev;
 	int ret;
 
 	if (dev_addr->bound_dev_if) {
@@ -198,7 +200,7 @@ int rdma_translate_ip(const struct sockaddr *addr,
 
 	if (dev != NULL) {
 		/* disallow connections through 127.0.0.1 itself */
-		if (dev->if_flags & IFF_LOOPBACK)
+		if (if_getflags(dev) & IFF_LOOPBACK)
 			ret = -EINVAL;
 		else
 			ret = rdma_copy_addr(dev_addr, dev, NULL);
@@ -241,7 +243,7 @@ static void queue_req(struct addr_req *req)
 }
 
 #if defined(INET) || defined(INET6)
-static int addr_resolve_multi(u8 *edst, struct ifnet *ifp, struct sockaddr *dst_in)
+static int addr_resolve_multi(u8 *edst, if_t ifp, struct sockaddr *dst_in)
 {
 	struct sockaddr *llsa;
 	struct sockaddr_dl sdl;
@@ -250,14 +252,10 @@ static int addr_resolve_multi(u8 *edst, struct ifnet *ifp, struct sockaddr *dst_
 	sdl.sdl_len = sizeof(sdl);
 	llsa = (struct sockaddr *)&sdl;
 
-	if (ifp->if_resolvemulti == NULL) {
-		error = EOPNOTSUPP;
-	} else {
-		error = ifp->if_resolvemulti(ifp, &llsa, dst_in);
-		if (error == 0) {
-			rdma_copy_addr_sub(edst, LLADDR((struct sockaddr_dl *)llsa),
-			    ifp->if_addrlen, MAX_ADDR_LEN);
-		}
+	error = if_resolvemulti(ifp, &llsa, dst_in);
+	if (error == 0) {
+		rdma_copy_addr_sub(edst, LLADDR((struct sockaddr_dl *)llsa),
+		    if_getaddrlen(ifp), MAX_ADDR_LEN);
 	}
 	return (error);
 }
@@ -268,7 +266,7 @@ static int addr4_resolve(struct sockaddr_in *src_in,
 			 const struct sockaddr_in *dst_in,
 			 struct rdma_dev_addr *addr,
 			 u8 *edst,
-			 struct ifnet **ifpp)
+			 if_t *ifpp)
 {
 	enum {
 		ADDR_VALID = 0,
@@ -279,7 +277,7 @@ static int addr4_resolve(struct sockaddr_in *src_in,
 	in_port_t src_port;
 	struct sockaddr *saddr = NULL;
 	struct nhop_object *nh;
-	struct ifnet *ifp;
+	if_t ifp;
 	int error;
 	int type;
 
@@ -332,13 +330,13 @@ static int addr4_resolve(struct sockaddr_in *src_in,
 		if (ifp == NULL) {
 			error = ENETUNREACH;
 			goto done;
-		} else if (ifp->if_flags & IFF_LOOPBACK) {
+		} else if (if_getflags(ifp) & IFF_LOOPBACK) {
 			/*
 			 * Source address cannot be a loopback device.
 			 */
 			error = EHOSTUNREACH;
 			goto error_put_ifp;
-		} else if (nh->nh_ifp->if_flags & IFF_LOOPBACK) {
+		} else if (if_getflags(nh->nh_ifp) & IFF_LOOPBACK) {
 			if (memcmp(&src_in->sin_addr, &dst_in->sin_addr,
 			    sizeof(src_in->sin_addr))) {
 				/*
@@ -364,7 +362,7 @@ static int addr4_resolve(struct sockaddr_in *src_in,
 		break;
 	case ADDR_SRC_ANY:
 		/* check for loopback device */
-		if (nh->nh_ifp->if_flags & IFF_LOOPBACK)
+		if (if_getflags(nh->nh_ifp) & IFF_LOOPBACK)
 			saddr = (struct sockaddr *)&dst_tmp;
 		else
 			saddr = nh->nh_ifa->ifa_addr;
@@ -381,8 +379,8 @@ static int addr4_resolve(struct sockaddr_in *src_in,
 	 * Step 3 - resolve destination MAC address
 	 */
 	if (dst_tmp.sin_addr.s_addr == INADDR_BROADCAST) {
-		rdma_copy_addr_sub(edst, ifp->if_broadcastaddr,
-		    ifp->if_addrlen, MAX_ADDR_LEN);
+		rdma_copy_addr_sub(edst, if_getbroadcastaddr(ifp),
+		    if_getaddrlen(ifp), MAX_ADDR_LEN);
 		error = 0;
 	} else if (IN_MULTICAST(ntohl(dst_tmp.sin_addr.s_addr))) {
 		bool is_gw = (nh->nh_flags & NHF_GATEWAY) != 0;
@@ -391,7 +389,7 @@ static int addr4_resolve(struct sockaddr_in *src_in,
 			goto error_put_ifp;
 		else if (is_gw)
 			addr->network = RDMA_NETWORK_IPV4;
-	} else if (ifp->if_flags & IFF_LOOPBACK) {
+	} else if (if_getflags(ifp) & IFF_LOOPBACK) {
 		memset(edst, 0, MAX_ADDR_LEN);
 		error = 0;
 	} else {
@@ -440,7 +438,7 @@ static int addr4_resolve(struct sockaddr_in *src_in,
 			 const struct sockaddr_in *dst_in,
 			 struct rdma_dev_addr *addr,
 			 u8 *edst,
-			 struct ifnet **ifpp)
+			 if_t *ifpp)
 {
 	return -EADDRNOTAVAIL;
 }
@@ -451,7 +449,7 @@ static int addr6_resolve(struct sockaddr_in6 *src_in,
 			 const struct sockaddr_in6 *dst_in,
 			 struct rdma_dev_addr *addr,
 			 u8 *edst,
-			 struct ifnet **ifpp)
+			 if_t *ifpp)
 {
 	enum {
 		ADDR_VALID = 0,
@@ -462,7 +460,7 @@ static int addr6_resolve(struct sockaddr_in6 *src_in,
 	in_port_t src_port;
 	struct sockaddr *saddr = NULL;
 	struct nhop_object *nh;
-	struct ifnet *ifp;
+	if_t ifp;
 	int error;
 	int type;
 
@@ -530,13 +528,13 @@ static int addr6_resolve(struct sockaddr_in6 *src_in,
 		if (ifp == NULL) {
 			error = ENETUNREACH;
 			goto done;
-		} else if (ifp->if_flags & IFF_LOOPBACK) {
+		} else if (if_getflags(ifp) & IFF_LOOPBACK) {
 			/*
 			 * Source address cannot be a loopback device.
 			 */
 			error = EHOSTUNREACH;
 			goto error_put_ifp;
-		} else if (nh->nh_ifp->if_flags & IFF_LOOPBACK) {
+		} else if (if_getflags(nh->nh_ifp) & IFF_LOOPBACK) {
 			if (memcmp(&src_in->sin6_addr, &dst_in->sin6_addr,
 			    sizeof(src_in->sin6_addr))) {
 				/*
@@ -562,7 +560,7 @@ static int addr6_resolve(struct sockaddr_in6 *src_in,
 		break;
 	case ADDR_SRC_ANY:
 		/* check for loopback device */
-		if (nh->nh_ifp->if_flags & IFF_LOOPBACK)
+		if (if_getflags(nh->nh_ifp) & IFF_LOOPBACK)
 			saddr = (struct sockaddr *)&dst_tmp;
 		else
 			saddr = nh->nh_ifa->ifa_addr;
@@ -586,7 +584,7 @@ static int addr6_resolve(struct sockaddr_in6 *src_in,
 			goto error_put_ifp;
 		else if (is_gw)
 			addr->network = RDMA_NETWORK_IPV6;
-	} else if (nh->nh_ifp->if_flags & IFF_LOOPBACK) {
+	} else if (if_getflags(nh->nh_ifp) & IFF_LOOPBACK) {
 		memset(edst, 0, MAX_ADDR_LEN);
 		error = 0;
 	} else {
@@ -628,18 +626,18 @@ static int addr6_resolve(struct sockaddr_in6 *src_in,
 			 const struct sockaddr_in6 *dst_in,
 			 struct rdma_dev_addr *addr,
 			 u8 *edst,
-			 struct ifnet **ifpp)
+			 if_t *ifpp)
 {
 	return -EADDRNOTAVAIL;
 }
 #endif
 
-static int addr_resolve_neigh(struct ifnet *dev,
+static int addr_resolve_neigh(if_t dev,
 			      const struct sockaddr *dst_in,
 			      u8 *edst,
 			      struct rdma_dev_addr *addr)
 {
-	if (dev->if_flags & IFF_LOOPBACK) {
+	if (if_getflags(dev) & IFF_LOOPBACK) {
 		int ret;
 
 		/*
@@ -647,7 +645,7 @@ static int addr_resolve_neigh(struct ifnet *dev,
 		 * sure the destination device address is global by
 		 * clearing the bound device interface:
 		 */
-		if (addr->bound_dev_if == dev->if_index)
+		if (addr->bound_dev_if == if_getindex(dev))
 			addr->bound_dev_if = 0;
 
 		ret = rdma_translate_ip(dst_in, addr);
@@ -659,7 +657,7 @@ static int addr_resolve_neigh(struct ifnet *dev,
 	}
 
 	/* If the device doesn't do ARP internally */
-	if (!(dev->if_flags & IFF_NOARP))
+	if (!(if_getflags(dev) & IFF_NOARP))
 		return rdma_copy_addr(addr, dev, edst);
 
 	return rdma_copy_addr(addr, dev, NULL);
@@ -670,7 +668,7 @@ static int addr_resolve(struct sockaddr *src_in,
 			struct rdma_dev_addr *addr)
 {
 	struct epoch_tracker et;
-	struct ifnet *ndev = NULL;
+	if_t ndev = NULL;
 	u8 edst[MAX_ADDR_LEN];
 	int ret;
 
@@ -860,7 +858,7 @@ static void resolve_cb(int status, struct sockaddr *src_addr,
 
 int rdma_addr_find_l2_eth_by_grh(const union ib_gid *sgid,
 				 const union ib_gid *dgid,
-				 u8 *dmac, struct ifnet *dev,
+				 u8 *dmac, if_t dev,
 				 int *hoplimit)
 {
 	int ret = 0;
@@ -874,7 +872,7 @@ int rdma_addr_find_l2_eth_by_grh(const union ib_gid *sgid,
 
 	memset(&dev_addr, 0, sizeof(dev_addr));
 
-	dev_addr.bound_dev_if = dev->if_index;
+	dev_addr.bound_dev_if = if_getindex(dev);
 	dev_addr.net = dev_net(dev);
 
 	ctx.addr = &dev_addr;

sys/ofed/drivers/infiniband/core/ib_cache.c
@@ -183,7 +183,7 @@ static int write_gid(struct ib_device *ib_dev, u8 port,
 	__releases(&table->rwlock) __acquires(&table->rwlock)
 {
 	int ret = 0;
-	struct ifnet *old_net_dev;
+	if_t old_net_dev;
 	enum ib_gid_type old_gid_type;
 
 	/* in rdma_cap_roce_gid_table, this funciton should be protected by a
@@ -311,12 +311,12 @@ static int find_gid(struct ib_gid_table *table, const union ib_gid *gid,
 	return found;
 }
 
-static void addrconf_ifid_eui48(u8 *eui, struct ifnet *dev)
+static void addrconf_ifid_eui48(u8 *eui, if_t dev)
 {
-	if (dev->if_addrlen != ETH_ALEN)
+	if (if_getaddrlen(dev) != ETH_ALEN)
 		return;
-	memcpy(eui, IF_LLADDR(dev), 3);
-	memcpy(eui + 5, IF_LLADDR(dev) + 3, 3);
+	memcpy(eui, if_getlladdr(dev), 3);
+	memcpy(eui + 5, if_getlladdr(dev) + 3, 3);
 
 	/* NOTE: The scope ID is added by the GID to IP conversion */
 
@@ -325,7 +325,7 @@ static void addrconf_ifid_eui48(u8 *eui, struct ifnet *dev)
 	eui[0] ^= 2;
 }
 
-static void make_default_gid(struct ifnet *dev, union ib_gid *gid)
+static void make_default_gid(if_t dev, union ib_gid *gid)
 {
 	gid->global.subnet_prefix = cpu_to_be64(0xfe80000000000000LL);
 	addrconf_ifid_eui48(&gid->raw[8], dev);
@@ -400,7 +400,7 @@ out_unlock:
 }
 
 int ib_cache_gid_del_all_netdev_gids(struct ib_device *ib_dev, u8 port,
-				     struct ifnet *ndev)
+				     if_t ndev)
 {
 	struct ib_gid_table **ports_table = ib_dev->cache.gid_cache;
 	struct ib_gid_table *table;
@@ -447,8 +447,8 @@ static int __ib_cache_gid_get(struct ib_device *ib_dev, u8 port, int index,
 		memcpy(attr, &table->data_vec[index].attr, sizeof(*attr));
 		/* make sure network device is valid and attached */
 		if (attr->ndev != NULL &&
-		    (attr->ndev->if_flags & IFF_DYING) == 0 &&
-		    attr->ndev->if_addr != NULL)
+		    (if_getflags(attr->ndev) & IFF_DYING) == 0 &&
+		    if_getifaddr(attr->ndev) != NULL)
 			dev_hold(attr->ndev);
 		else
 			attr->ndev = NULL;
@@ -490,7 +490,7 @@ static int _ib_cache_gid_table_find(struct ib_device *ib_dev,
 static int ib_cache_gid_find(struct ib_device *ib_dev,
 			     const union ib_gid *gid,
 			     enum ib_gid_type gid_type,
-			     struct ifnet *ndev, u8 *port,
+			     if_t ndev, u8 *port,
 			     u16 *index)
 {
 	unsigned long mask = GID_ATTR_FIND_MASK_GID |
@@ -507,7 +507,7 @@ static int ib_cache_gid_find(struct ib_device *ib_dev,
 int ib_find_cached_gid_by_port(struct ib_device *ib_dev,
 			       const union ib_gid *gid,
 			       enum ib_gid_type gid_type,
-			       u8 port, struct ifnet *ndev,
+			       u8 port, if_t ndev,
 			       u16 *index)
 {
 	int local_index;
@@ -672,7 +672,7 @@ static void cleanup_gid_table_port(struct ib_device *ib_dev, u8 port,
 }
 
 void ib_cache_gid_set_default_gid(struct ib_device *ib_dev, u8 port,
-				  struct ifnet *ndev,
+				  if_t ndev,
 				  unsigned long gid_type_mask,
 				  enum ib_cache_gid_default_mode mode)
 {
@@ -899,7 +899,7 @@ EXPORT_SYMBOL(ib_get_cached_gid);
 int ib_find_cached_gid(struct ib_device *device,
 		       const union ib_gid *gid,
 		       enum ib_gid_type gid_type,
-		       struct ifnet *ndev,
+		       if_t ndev,
 		       u8               *port_num,
 		       u16              *index)
 {

sys/ofed/drivers/infiniband/core/ib_cm.c
@@ -503,7 +503,7 @@ static int cm_init_av_by_path(struct ib_sa_path_rec *path, struct cm_av *av,
 	unsigned long flags;
 	int ret;
 	u8 p;
-	struct ifnet *ndev = ib_get_ndev_from_path(path);
+	if_t ndev = ib_get_ndev_from_path(path);
 
 	read_lock_irqsave(&cm.device_lock, flags);
 	list_for_each_entry(cm_dev, &cm.device_list, list) {
@@ -1819,7 +1819,7 @@ static int cm_req_handler(struct cm_work *work)
 				&gid, &gid_attr);
 	if (!ret) {
 		if (gid_attr.ndev) {
-			work->path[0].ifindex = gid_attr.ndev->if_index;
+			work->path[0].ifindex = if_getindex(gid_attr.ndev);
 			work->path[0].net = dev_net(gid_attr.ndev);
 			dev_put(gid_attr.ndev);
 		}
@@ -1833,7 +1833,7 @@ static int cm_req_handler(struct cm_work *work)
 					    &work->path[0].sgid,
 					    &gid_attr);
 		if (!err && gid_attr.ndev) {
-			work->path[0].ifindex = gid_attr.ndev->if_index;
+			work->path[0].ifindex = if_getindex(gid_attr.ndev);
 			work->path[0].net = dev_net(gid_attr.ndev);
 			dev_put(gid_attr.ndev);
 		}

commit 6bca7c1359385cbe5309569519b513e8f55b20df

ofed: fix roce gid insertion for vlan interfaces
When attempting to use vlan interface the correct GID
wasn't created due to incorrect ifp validation.

The problem was introduced in
f1ec47f918f1 ('ofed: Mechanically convert to IfAPI)

Signed-off-by: Bartosz Sobczak bartosz.sobczak@intel.com
Signed-off-by: Eric Joyner <erj@FreeBSD.org>

PR:		273043
Sponsored by:	Intel Corporation
Reviewed by:	jhibbits@, erj@
MFC after:	3 days
Differential Revision:	https://reviews.freebsd.org/D41426

sys/ofed/drivers/infiniband/core/ib_roce_gid_mgmt.c
@@ -239,7 +239,7 @@ roce_gid_update_addr_callback(struct ib_device *device, u8 port,
 		if (ifp != ndev) {
 			if (if_gettype(ifp) != IFT_L2VLAN)
 				continue;
-			if (ifp != rdma_vlan_dev_real_dev(ifp))
+			if (ndev != rdma_vlan_dev_real_dev(ifp))
 				continue;
 		}
 