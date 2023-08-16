freebsd/freebsd-src

commit 1c0ac635d9f001c420638148747f9486b98f9f52

Remove $FreeBSD$: one-line .c comment pattern
Remove /^/[*/]\s*\$FreeBSD\$.*\n/

libexec/bootpd/bptypes.h
@@ -1,4 +1,3 @@
-/* $FreeBSD$
  */
 
 #ifndef	BPTYPES_H

commit 7baec2544032598d8ac21fa83f65dd2b3c11844c

Fix partially removed comment by removing the rest
Fixes: 1c0ac635d9f0

libexec/bootpd/bptypes.h
@@ -1,5 +1,3 @@
- */
-
 #ifndef	BPTYPES_H
 #define	BPTYPES_H
 