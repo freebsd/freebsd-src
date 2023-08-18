bd8e35ef312cb47da78555508e388a8754b41916
Enji Cooper <ngie@FreeBSD.org>
2023-08-18 04:28:13 -0700

    Fix GoogleTest 1.14.0 import
    - Prune headers and tests no longer installed after the upgrade.
    - Remove GoogleTest-related files when MK_GOOGLETEST == no.
    - Disable `-Werror` with gcc to unbreak the gcc12 CI run with
      `lib/googletest`. Any issues found by g++ will be filed
      upstream and hopefully resolved in a future version.
    - Remove clang -Werror issues which are resolved in version 1.14.0 to
      avoid masking valid issues.
    
    MFC after:	1 week
    MFC with:	2eea0af6ef83c7e1f0c613028ffdf1938073c00b

--- share/mk/googletest.test.inc.mk
+++ share/mk/googletest.test.inc.mk
@@ -1,20 +1,9 @@
-
 GTESTS_CXXFLAGS+= -DGTEST_HAS_POSIX_RE=1
 GTESTS_CXXFLAGS+= -DGTEST_HAS_PTHREAD=1
 GTESTS_CXXFLAGS+= -DGTEST_HAS_STREAM_REDIRECTION=1
 GTESTS_CXXFLAGS+= -frtti
 
 .include <bsd.compiler.mk>
-
-.if ${COMPILER_TYPE} == "clang" && ${COMPILER_VERSION} >= 100000
-# Required until googletest is upgraded to a more recent version (after
-# upstream commit efecb0bfa687cf87836494f5d62868485c00fb66).
-GTESTS_CXXFLAGS+= -Wno-deprecated-copy
-
-# Required until googletest is upgraded to a more recent version (after
-# upstream commit d44b137fd104dfffdcdea103f7de11b9eccc45c2).
-GTESTS_CXXFLAGS+= -Wno-signed-unsigned-wchar
-.endif
 
 # XXX: src.libnames.mk should handle adding this directory for libgtest's,
 # libgmock's, etc, headers.
