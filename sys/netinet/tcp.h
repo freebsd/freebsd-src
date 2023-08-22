1d15c4548576763843ea7268e494bf6015758ef8
Marius Strobl <marius@FreeBSD.org>
2023-08-22 20:12:59 +0200

    tcp_info: Add and export more FreeBSD-specific fields
    This change adds struct tcp_info fields corresponding to the following
    struct tcpcb ones:
    - snd_una
    - snd_max
    - rcv_numsacks
    - rcv_adv
    - dupacks
    
    Note that while both tcp_fill_info() and fill_tcp_info_from_tcb() are
    extended accordingly, no counterpart of rcv_numsacks is available in
    the cxgbe(4) TOE PCB, though.
    
    Sponsored by:	NetApp, Inc. (originally)
    
    (cherry picked from commit dc485b968ddeb070d23354f55164a8c336acf081)
    
    Conflicts (due tue missing 945f9a7c):
    	sys/netinet/tcp.h

--- sys/netinet/tcp.h
+++ sys/netinet/tcp.h
@@ -398,8 +398,17 @@ struct tcp_info {
 	u_int32_t	__tcpi_received_e0_bytes;
 	u_int32_t	__tcpi_received_ce_bytes;
 
+	u_int32_t	__tcpi_total_tlp;	/* tail loss probes sent */
+	u_int64_t	__tcpi_total_tlp_bytes;	/* tail loss probe bytes sent */
+
+	u_int32_t	tcpi_snd_una;		/* Unacked seqno sent */
+	u_int32_t	tcpi_snd_max;		/* Highest seqno sent */
+	u_int32_t	tcpi_rcv_numsacks;	/* Distinct SACK blks present */
+	u_int32_t	tcpi_rcv_adv;		/* Peer advertised window */
+	u_int32_t	tcpi_dupacks;		/* Consecutive dup ACKs recvd */
+
 	/* Padding to grow without breaking ABI. */
-	u_int32_t	__tcpi_pad[18];		/* Padding. */
+	u_int32_t	__tcpi_pad[10];		/* Padding. */
 };
 
 /*
