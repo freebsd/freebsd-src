27096e41f03f81fd6622e45ec3a91268f0eacdc8
Warner Losh <imp@FreeBSD.org>
2023-08-21 19:32:14 -0600

    sys: Remove $FreeBSD$: one-line bare tag
    Remove /^\s*\$FreeBSD\$$\n/
    
    Similar commit in current:
    (cherry picked from commit 78d146160dc5)

--- sys/dev/cxgb/cxgb_ioctl.h
+++ sys/dev/cxgb/cxgb_ioctl.h
@@ -26,8 +26,6 @@ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 
-$FreeBSD$
-
 ***************************************************************************/
 #ifndef __CHIOCTL_H__
 #define __CHIOCTL_H__
