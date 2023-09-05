4d17239617439b9a500f4c7a59f5f4d323d252b7
Dmitry Chagin <dchagin@FreeBSD.org>
2023-09-05 21:15:52 +0300

    linux(4): Deduplicate mprotect, madvise
    MFC after:		1 week
    
    (cherry picked from commit 553b1a4e4eb426d2c29a033b284c13bfbab30334)

--- sys/i386/linux/linux_machdep.c
+++ sys/i386/linux/linux_machdep.c
@@ -333,20 +333,6 @@ linux_mmap(struct thread *td, struct linux_mmap_args *args)
 }
 
 int
-linux_mprotect(struct thread *td, struct linux_mprotect_args *uap)
-{
-
-	return (linux_mprotect_common(td, PTROUT(uap->addr), uap->len, uap->prot));
-}
-
-int
-linux_madvise(struct thread *td, struct linux_madvise_args *uap)
-{
-
-	return (linux_madvise_common(td, PTROUT(uap->addr), uap->len, uap->behav));
-}
-
-int
 linux_ioperm(struct thread *td, struct linux_ioperm_args *args)
 {
 	int error;
