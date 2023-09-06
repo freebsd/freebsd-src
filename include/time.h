a3f8c956fc462319d5f5b63294563c7240196be0
Dag-Erling Smørgrav <des@FreeBSD.org>
2023-09-06 03:37:43 +0200

    time.h: Fix visibility check for C11 and C23 features.
    __BSD_VISIBLE is always defined; we need to check if it's true.
    
    Fixes:		d02489d11aed a7b84a68ae41
    MFC after:	3 days
    Reviewed by:	brooks, imp
    Differential Revision:	https://reviews.freebsd.org/D41733

--- include/time.h
+++ include/time.h
@@ -179,13 +179,13 @@ time_t posix2time(time_t t);
 #include <xlocale/_time.h>
 #endif
 
-#if defined(__BSD_VISIBLE) || __ISO_C_VISIBLE >= 2011 || \
+#if __BSD_VISIBLE || __ISO_C_VISIBLE >= 2011 || \
     (defined(__cplusplus) && __cplusplus >= 201703)
 #include <sys/_timespec.h>
 /* ISO/IEC 9899:2011 7.27.2.5 The timespec_get function */
 #define TIME_UTC	1	/* time elapsed since epoch */
 int timespec_get(struct timespec *ts, int base);
-#if defined (__BSD_VISIBLE) || __ISO_C_VISIBLE >= 2023
+#if __BSD_VISIBLE || __ISO_C_VISIBLE >= 2023
 /* ISO/IEC 9899:2024 7.29.1 Components of time */
 #define TIME_MONOTONIC	2	/* monotonic time */
 /* ISO/IEC 9899:2024 7.29.2.7 The timespec_getres function */
