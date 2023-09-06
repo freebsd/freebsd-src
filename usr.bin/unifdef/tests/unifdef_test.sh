aacbe7384221d2eafa326864bbbe2f22a10063ce
Dag-Erling Smørgrav <des@FreeBSD.org>
2023-09-06 17:11:04 +0000

    unifdef: Handle redefined symbols correctly.
    MFC after:	1 week
    Sponsored by:	Klara, Inc.
    Reviewed by:	kevans
    Differential Revision:	https://reviews.freebsd.org/D41758

--- usr.bin/unifdef/tests/unifdef_test.sh
+++ usr.bin/unifdef/tests/unifdef_test.sh
@@ -17,6 +17,27 @@ EOF
 	atf_check -o file:f unifdef <f
 }
 
+atf_test_case redefine
+redefine_head() {
+	atf_set descr "redefine the same symbol"
+}
+redefine_body() {
+	cat >file <<EOF
+#if FOO
+a
+#else
+b
+#endif
+EOF
+	atf_check -s exit:1 -o inline:"a\n" unifdef -DFOO <file
+	atf_check -s exit:1 -o inline:"a\n" unifdef -UFOO -DFOO <file
+	atf_check -s exit:1 -o inline:"a\n" unifdef -DFOO=0 -DFOO <file
+	atf_check -s exit:1 -o inline:"b\n" unifdef -UFOO <file
+	atf_check -s exit:1 -o inline:"b\n" unifdef -DFOO -UFOO <file
+	atf_check -s exit:1 -o inline:"b\n" unifdef -DFOO -DFOO=0 <file
+}
+
 atf_init_test_cases() {
 	atf_add_test_case hash_comment
+	atf_add_test_case redefine
 }
