d9985d455223d65cc68a2d5e530a0bec79efe545
Zhenlei Huang <zlei@FreeBSD.org>
2023-09-09 16:06:23 +0800

    sysctl(9): Enable vnet sysctl variables to be loader tunable
    Complete phase two of 3da1cf1e88f8.
    
    In 3da1cf1e88f8, the meaning of the flag CTLFLAG_TUN is extended to
    automatically check if there is a kernel environment variable which
    shall initialize the SYSCTL during early boot. It works for all SYSCTL
    types both statically and dynamically created ones, except for the
    SYSCTLs which belong to VNETs.
    
    This change extends the meaning further, to allow it also works for
    the SYSCTLs which belong to VNETs. A typical usage is
    ```
    VNET_DEFINE_STATIC(int, foo) = 0;
    SYSCTL_INT(_net, OID_AUTO, foo, CTLFLAG_RWTUN | CTLFLAG_VNET,
        &VNET_NAME(foo), 0, "Description of the foo loader tunable");
    ```
    
    Note that the implementation has a limitation. It behaves the same way
    as that of non-vnet loader tunables. That is, after the kernel or modules
    being initialized, any changes (e.g. via kenv) to kernel environment
    variable will not affect the corresponding vnet variable of subsequently
    created VNETs. To overcome it, we can use TUNABLE_XXX_FETCH to fetch
    the kernel environment variable into those vnet variables during vnet
    constructing.
    
    This change will fix the following SYSCTLs those belong to VNETs and
    have CTLFLAG_TUN flag:
    ```
    net.add_addr_allfibs
    net.bpf.optimize_writers
    net.inet.tcp.fastopen.ccache_buckets
    net.link.bridge.inherit_mac
    net.link.bridge.ipfw_arp
    net.link.bridge.log_stp
    net.link.bridge.pfil_bridge
    net.link.bridge.pfil_local_phys
    net.link.bridge.pfil_member
    net.link.bridge.pfil_onlyip
    net.link.lagg.default_use_flowid
    net.link.lagg.default_use_numa
    net.link.lagg.default_flowid_shift
    net.link.lagg.lacp.debug
    net.link.lagg.lacp.default_strict_mode
    ```
    
    Although the following vnet SYSCTLs have CTLFLAG_TUN flag, theirs
    values are re-fetched via TUNABLE_XXX_FETCH, thus are not affected
    by this change.
    ```
    net.inet.ip.reass_hashsize
    net.inet.tcp.hostcache.cachelimit
    net.inet.tcp.hostcache.hashsize
    net.inet.tcp.hostcache.bucketlimit
    net.inet.tcp.syncache.bucketlimit
    net.inet.tcp.syncache.cachelimit
    net.inet.tcp.syncache.hashsize
    net.key.spdcache.maxentries
    net.key.spdcache.threshold
    ```
    
    In memoriam:	hselasky
    Discussed with:	hselasky, glebius
    Fixes:		3da1cf1e88f8 Extend the meaning of the CTLFLAG_TUN flag ...
    MFC after:	2 weeks
    Relnotes:	yes
    Differential Revision:	https://reviews.freebsd.org/D39638

--- sys/kern/linker_if.m
+++ sys/kern/linker_if.m
@@ -154,3 +154,12 @@ STATICMETHOD int link_preload {
 METHOD int link_preload_finish {
     linker_file_t	file;
 };
+
+#ifdef VIMAGE
+#
+# Propagate system tunable values to all vnets.
+#
+METHOD void propagate_vnets {
+	linker_file_t	file;
+};
+#endif
