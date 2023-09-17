2dd9ccab41fb795df9b098aa97397b542b563378
Shawn Anastasio <sanastasio@raptorengineering.com>
2023-09-17 10:40:48 -0400

    powerpc: Implement fpu_kern_enter/fpu_kern_leave
    Summary:
    Provide an implementation of fpu_kern_enter/fpu_kern_leave for PPC to
    enable FPU, VSX, and Altivec usage in-kernel. The functions currently
    only support FPU_KERN_NOCTX, but this is sufficient for ossl(1) and many
    other users of the API.
    
    This patchset has been tested on powerpc64le using a modified version of
    the in-tree tools/tools/crypto/cryptocheck.c tool to check for FPU/Vec
    register clobbering along with a follow-up patch to enable ossl(4) on
    powerpc64*.
    
    Reviewed by:	jhibbits
    MFC after:	2 weeks
    Differential Revision: https://reviews.freebsd.org/D41540

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -75,7 +75,7 @@
  * cannot include sys/param.h and should only be updated here.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1500000
+#define __FreeBSD_version 1500001
 
 /*
  * __FreeBSD_kernel__ indicates that this system uses the kernel of FreeBSD,
