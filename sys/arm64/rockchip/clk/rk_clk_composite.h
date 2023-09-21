89484ef6831a2705c0bb2d85be128fa211f42772
Emmanuel Vadot <manu@FreeBSD.org>
2023-09-21 16:56:16 +0200

    arm64: rockchip: rk_clk_composite: Remove gate code
    It was a bad idea to have composite clock directly managing gates.
    All clocks drivers have been rewrite to not use this functionality
    and directly export the gate. We can now remove this code.

--- sys/arm64/rockchip/clk/rk_clk_composite.h
+++ sys/arm64/rockchip/clk/rk_clk_composite.h
@@ -41,17 +41,13 @@ struct rk_clk_composite_def {
 	uint32_t	div_shift;
 	uint32_t	div_width;
 
-	uint32_t	gate_offset;
-	uint32_t	gate_shift;
-
 	uint32_t	flags;
 };
 
 #define	RK_CLK_COMPOSITE_HAVE_MUX	0x0001
-#define	RK_CLK_COMPOSITE_HAVE_GATE	0x0002
-#define	RK_CLK_COMPOSITE_DIV_EXP	0x0004	/* Register   0, 1, 2, 2, ... */
+#define	RK_CLK_COMPOSITE_DIV_EXP	0x0002	/* Register   0, 1, 2, 2, ... */
 						/* Divider    1, 2, 4, 8, ... */
-#define	RK_CLK_COMPOSITE_GRF		0x0008 /* Use syscon registers instead of CRU's */
+#define	RK_CLK_COMPOSITE_GRF		0x0004 /* Use syscon registers instead of CRU's */
 int rk_clk_composite_register(struct clkdom *clkdom,
     struct rk_clk_composite_def *clkdef);
 
