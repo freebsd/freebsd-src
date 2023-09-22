freebsd/freebsd-src

commit 013ab21c7c08949eb7e75c5adefa6d599c69aeeb

netlink: add netlink support
Netlinks is a communication protocol currently used in Linux kernel to modify,
 read and subscribe for nearly all networking state. Interfaces, addresses, routes,
 firewall, fibs, vnets, etc are controlled via netlink.
It is async, TLV-based protocol, providing 1-1 and 1-many communications.

The current implementation supports the subset of NETLINK_ROUTE
family. To be more specific, the following is supported:
* Dumps:
 - routes
 - nexthops / nexthop groups
 - interfaces
 - interface addresses
 - neighbors (arp/ndp)
* Notifications:
 - interface arrival/departure
 - interface address arrival/departure
 - route addition/deletion
* Modifications:
 - adding/deleting routes
 - adding/deleting nexthops/nexthops groups
 - adding/deleting neghbors
 - adding/deleting interfaces (basic support only)
* Rtsock interaction
 - route events are bridged both ways

The implementation also supports the NETLINK_GENERIC family framework.

Implementation notes:
Netlink is implemented via loadable/unloadable kernel module,
 not touching many kernel parts.
Each netlink socket uses dedicated taskqueue to support async operations
 that can sleep, such as interface creation. All message processing is
 performed within these taskqueues.

Compatibility:
Most of the Netlink data models specified above maps to FreeBSD concepts
 nicely. Unmodified ip(8) binary correctly works with
interfaces, addresses, routes, nexthops and nexthop groups. Some
software such as net/bird require header-only modifications to compile
and work with FreeBSD netlink.

Reviewed by:	imp
Differential Revision: https://reviews.freebsd.org/D36002
MFC after:	2 months

sys/netlink/netlink_generic.c
@@ -1,0 +1,472 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2022 Alexander V. Chernikov <melifaro@FreeBSD.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+#include <sys/types.h>
+#include <sys/malloc.h>
+#include <sys/priv.h>
+#include <sys/socket.h>
+#include <sys/ck.h>
+
+#include <netlink/netlink.h>
+#include <netlink/netlink_ctl.h>
+#include <netlink/netlink_var.h>
+#include <netlink/netlink_generic.h>
+
+#define	DEBUG_MOD_NAME	nl_generic
+#define	DEBUG_MAX_LEVEL	LOG_DEBUG3
+#include <netlink/netlink_debug.h>
+_DECLARE_DEBUG(LOG_DEBUG3);
+
+#define	MAX_FAMILIES	20
+#define	MAX_GROUPS	20
+
+#define	MIN_GROUP_NUM	48
+
+static struct sx sx_lock;
+
+#define	GENL_LOCK_INIT()	sx_init(&sx_lock, "genetlink lock")
+#define	GENL_LOCK_DESTROY()	sx_destroy(&sx_lock)
+#define	GENL_LOCK()		sx_xlock(&sx_lock)
+#define	GENL_UNLOCK()		sx_xunlock(&sx_lock)
+
+struct genl_family {
+	const char	*family_name;
+	uint16_t	family_hdrsize;
+	uint16_t	family_id;
+	uint16_t	family_version;
+	uint16_t	family_attr_max;
+	uint16_t	family_cmd_size;
+	uint16_t	family_num_groups;
+	struct genl_cmd	*family_cmds;
+};
+
+static struct genl_family	families[MAX_FAMILIES];
+
+
+struct genl_group {
+	struct genl_family	*group_family;
+	const char		*group_name;
+};
+static struct genl_group	groups[MAX_GROUPS];
+
+
+static int dump_family(struct nlmsghdr *hdr, struct genlmsghdr *ghdr,
+    const struct genl_family *gf, struct nl_writer *nw);
+static void nlctrl_notify(const struct genl_family *gf, int action);
+
+static struct genl_family *
+find_family(const char *family_name)
+{
+	for (int i = 0; i < MAX_FAMILIES; i++) {
+		struct genl_family *gf = &families[i];
+		if (gf->family_name != NULL && !strcmp(gf->family_name, family_name))
+			return (gf);
+	}
+
+	return (NULL);
+}
+
+uint32_t
+genl_register_family(const char *family_name, size_t hdrsize, int family_version,
+    int max_attr_idx)
+{
+	uint32_t family_id = 0;
+
+	MPASS(family_name != NULL);
+	if (find_family(family_name) != NULL)
+		return (0);
+
+	GENL_LOCK();
+	for (int i = 0; i < MAX_FAMILIES; i++) {
+		struct genl_family *gf = &families[i];
+		if (gf->family_name == NULL) {
+			gf->family_name = family_name;
+			gf->family_version = family_version;
+			gf->family_hdrsize = hdrsize;
+			gf->family_attr_max = max_attr_idx;
+			gf->family_id = i + GENL_MIN_ID;
+			NL_LOG(LOG_DEBUG2, "Registered family %s id %d",
+			    gf->family_name, gf->family_id);
+			family_id = gf->family_id;
+			nlctrl_notify(gf, CTRL_CMD_NEWFAMILY);
+			break;
+		}
+	}
+	GENL_UNLOCK();
+
+	return (family_id);
+}
+
+static void
+free_family(struct genl_family *gf)
+{
+	if (gf->family_cmds != NULL)
+		free(gf->family_cmds, M_NETLINK);
+}
+
+/*
+ * Can sleep, I guess
+ */
+bool
+genl_unregister_family(const char *family_name)
+{
+	bool found = false;
+
+	GENL_LOCK();
+	struct genl_family *gf = find_family(family_name);
+
+	nlctrl_notify(gf, CTRL_CMD_DELFAMILY);
+
+	if (gf != NULL) {
+		found = true;
+		/* TODO: zero pointer first */
+		free_family(gf);
+		bzero(gf, sizeof(*gf));
+	}
+	GENL_UNLOCK();
+
+	return (found);
+}
+
+bool
+genl_register_cmds(const char *family_name, const struct genl_cmd *cmds, int count)
+{
+	GENL_LOCK();
+	struct genl_family *gf = find_family(family_name);
+	if (gf == NULL) {
+		GENL_UNLOCK();
+		return (false);
+	}
+
+	int cmd_size = gf->family_cmd_size;
+
+	for (int i = 0; i < count; i++) {
+		MPASS(cmds[i].cmd_cb != NULL);
+		if (cmds[i].cmd_num >= cmd_size)
+			cmd_size = cmds[i].cmd_num + 1;
+	}
+
+	if (cmd_size > gf->family_cmd_size) {
+		/* need to realloc */
+		size_t sz = cmd_size * sizeof(struct genl_cmd);
+		void *data = malloc(sz, M_NETLINK, M_WAITOK | M_ZERO);
+
+		memcpy(data, gf->family_cmds, gf->family_cmd_size * sizeof(struct genl_cmd));
+		void *old_data = gf->family_cmds;
+		gf->family_cmds = data;
+		gf->family_cmd_size = cmd_size;
+		free(old_data, M_NETLINK);
+	}
+
+	for (int i = 0; i < count; i++) {
+		const struct genl_cmd *cmd = &cmds[i];
+		MPASS(gf->family_cmds[cmd->cmd_num].cmd_cb == NULL);
+		gf->family_cmds[cmd->cmd_num] = cmds[i];
+		NL_LOG(LOG_DEBUG2, "Adding cmd %s(%d) to family %s",
+		    cmd->cmd_name, cmd->cmd_num, gf->family_name);
+	}
+	GENL_UNLOCK();
+	return (true);
+}
+
+static struct genl_group *
+find_group(const struct genl_family *gf, const char *group_name)
+{
+	for (int i = 0; i < MAX_GROUPS; i++) {
+		struct genl_group *gg = &groups[i];
+		if (gg->group_family == gf && !strcmp(gg->group_name, group_name))
+			return (gg);
+	}
+	return (NULL);
+}
+
+uint32_t
+genl_register_group(const char *family_name, const char *group_name)
+{
+	uint32_t group_id = 0;
+
+	MPASS(family_name != NULL);
+	MPASS(group_name != NULL);
+
+	GENL_LOCK();
+	struct genl_family *gf = find_family(family_name);
+
+	if (gf == NULL || find_group(gf, group_name) != NULL) {
+		GENL_UNLOCK();
+		return (0);
+	}
+
+	for (int i = 0; i < MAX_GROUPS; i++) {
+		struct genl_group *gg = &groups[i];
+		if (gg->group_family == NULL) {
+			gf->family_num_groups++;
+			gg->group_family = gf;
+			gg->group_name = group_name;
+			group_id = i + MIN_GROUP_NUM;
+			break;
+		}
+	}
+	GENL_UNLOCK();
+
+	return (group_id);
+}
+
+/*
+ * Handler called by netlink subsystem when matching netlink message is received
+ */
+static int
+genl_handle_message(struct nlmsghdr *hdr, struct nl_pstate *npt)
+{
+	struct nlpcb *nlp = npt->nlp;
+	int error = 0;
+
+	int family_id = (int)hdr->nlmsg_type - GENL_MIN_ID;
+
+	if (__predict_false(family_id < 0 || family_id > MAX_FAMILIES)) {
+		NLP_LOG(LOG_DEBUG, nlp, "invalid message type: %d", hdr->nlmsg_type);
+		return (ENOTSUP);
+	}
+
+	if (__predict_false(hdr->nlmsg_len < sizeof(hdr) + GENL_HDRLEN)) {
+		NLP_LOG(LOG_DEBUG, nlp, "invalid message size: %d", hdr->nlmsg_len);
+		return (EINVAL);
+	}
+
+	struct genl_family *gf = &families[family_id];
+
+	struct genlmsghdr *ghdr = (struct genlmsghdr *)(hdr + 1);
+
+	if (ghdr->cmd >= gf->family_cmd_size || gf->family_cmds[ghdr->cmd].cmd_cb == NULL) {
+		NLP_LOG(LOG_DEBUG, nlp, "family %s: invalid cmd %d",
+		    gf->family_name, ghdr->cmd);
+		return (ENOTSUP);
+	}
+
+	struct genl_cmd *cmd = &gf->family_cmds[ghdr->cmd];
+
+	if (cmd->cmd_priv != 0 && !nlp_has_priv(nlp, cmd->cmd_priv)) {
+		NLP_LOG(LOG_DEBUG, nlp, "family %s: cmd %d priv_check() failed",
+		    gf->family_name, ghdr->cmd);
+		return (EPERM);
+	}
+
+	NLP_LOG(LOG_DEBUG2, nlp, "received family %s cmd %s(%d) len %d",
+	    gf->family_name, cmd->cmd_name, ghdr->cmd, hdr->nlmsg_len);
+
+	error = cmd->cmd_cb(hdr, npt);
+
+	return (error);
+}
+
+static uint32_t
+get_cmd_flags(const struct genl_cmd *cmd)
+{
+	uint32_t flags = cmd->cmd_flags;
+	if (cmd->cmd_priv != 0)
+		flags |= GENL_ADMIN_PERM;
+	return (flags);
+}
+
+static int
+dump_family(struct nlmsghdr *hdr, struct genlmsghdr *ghdr,
+    const struct genl_family *gf, struct nl_writer *nw)
+{
+	if (!nlmsg_reply(nw, hdr, sizeof(struct genlmsghdr)))
+		goto enomem;
+
+	struct genlmsghdr *ghdr_new = nlmsg_reserve_object(nw, struct genlmsghdr);
+	ghdr_new->cmd = ghdr->cmd;
+	ghdr_new->version = gf->family_version;
+	ghdr_new->reserved = 0;
+
+        nlattr_add_string(nw, CTRL_ATTR_FAMILY_NAME, gf->family_name);
+        nlattr_add_u16(nw, CTRL_ATTR_FAMILY_ID, gf->family_id);
+        nlattr_add_u32(nw, CTRL_ATTR_VERSION, gf->family_version);
+        nlattr_add_u32(nw, CTRL_ATTR_HDRSIZE, gf->family_hdrsize);
+        nlattr_add_u32(nw, CTRL_ATTR_MAXATTR, gf->family_attr_max);
+
+	if (gf->family_cmd_size > 0) {
+		int off = nlattr_add_nested(nw, CTRL_ATTR_OPS);
+		if (off == 0)
+			goto enomem;
+		for (int i = 0, cnt=0; i < gf->family_cmd_size; i++) {
+			struct genl_cmd *cmd = &gf->family_cmds[i];
+			if (cmd->cmd_cb == NULL)
+				continue;
+			int cmd_off = nlattr_add_nested(nw, ++cnt);
+			if (cmd_off == 0)
+				goto enomem;
+
+			nlattr_add_u32(nw, CTRL_ATTR_OP_ID, cmd->cmd_num);
+			nlattr_add_u32(nw, CTRL_ATTR_OP_FLAGS, get_cmd_flags(cmd));
+			nlattr_set_len(nw, cmd_off);
+		}
+		nlattr_set_len(nw, off);
+	}
+	if (gf->family_num_groups > 0) {
+		int off = nlattr_add_nested(nw, CTRL_ATTR_MCAST_GROUPS);
+		if (off == 0)
+			goto enomem;
+		for (int i = 0, cnt = 0; i < MAX_GROUPS; i++) {
+			struct genl_group *gg = &groups[i];
+			if (gg->group_family != gf)
+				continue;
+
+			int cmd_off = nlattr_add_nested(nw, ++cnt);
+			if (cmd_off == 0)
+				goto enomem;
+			nlattr_add_u32(nw, CTRL_ATTR_MCAST_GRP_ID, i + MIN_GROUP_NUM);
+			nlattr_add_string(nw, CTRL_ATTR_MCAST_GRP_NAME, gg->group_name);
+			nlattr_set_len(nw, cmd_off);
+		}
+		nlattr_set_len(nw, off);
+	}
+	if (nlmsg_end(nw))
+		return (0);
+enomem:
+        NL_LOG(LOG_DEBUG, "unable to dump family %s state (ENOMEM)", gf->family_name);
+        nlmsg_abort(nw);
+	return (ENOMEM);
+}
+
+
+/* Declare ourself as a user */
+#define	CTRL_FAMILY_NAME	"nlctrl"
+
+static uint32_t ctrl_family_id;
+static uint32_t ctrl_group_id;
+
+struct nl_parsed_family {
+	uint32_t	family_id;
+	char		*family_name;
+	uint8_t		version;
+};
+
+#define	_IN(_field)	offsetof(struct genlmsghdr, _field)
+#define	_OUT(_field)	offsetof(struct nl_parsed_family, _field)
+static const struct nlfield_parser nlf_p_generic[] = {
+	{ .off_in = _IN(version), .off_out = _OUT(version), .cb = nlf_get_u8 },
+};
+
+static struct nlattr_parser nla_p_generic[] = {
+	{ .type = CTRL_ATTR_FAMILY_ID , .off = _OUT(family_id), .cb = nlattr_get_uint32 },
+	{ .type = CTRL_ATTR_FAMILY_NAME , .off = _OUT(family_id), .cb = nlattr_get_string },
+};
+#undef _IN
+#undef _OUT
+NL_DECLARE_PARSER(genl_parser, struct genlmsghdr, nlf_p_generic, nla_p_generic);
+
+static int
+nlctrl_handle_getfamily(struct nlmsghdr *hdr, struct nl_pstate *npt)
+{
+	int error = 0;
+
+	struct nl_parsed_family attrs = {};
+	error = nl_parse_nlmsg(hdr, &genl_parser, npt, &attrs);
+	if (error != 0)
+		return (error);
+
+	struct genlmsghdr ghdr = {
+		.cmd = CTRL_CMD_NEWFAMILY,
+	};
+
+	for (int i = 0; i < MAX_FAMILIES; i++) {
+		struct genl_family *gf = &families[i];
+		if (gf->family_name == NULL)
+			continue;
+		if (attrs.family_id != 0 && attrs.family_id != gf->family_id)
+			continue;
+		if (attrs.family_name != NULL && strcmp(attrs.family_name, gf->family_name))
+			continue;
+		error = dump_family(hdr, &ghdr, &families[i], npt->nw);
+		if (error != 0)
+			break;
+	}
+
+	return (error);
+}
+
+static void
+nlctrl_notify(const struct genl_family *gf, int cmd)
+{
+	struct nlmsghdr hdr = {.nlmsg_type = NETLINK_GENERIC };
+	struct genlmsghdr ghdr = { .cmd = cmd };
+	struct nl_writer nw = {};
+
+	if (nlmsg_get_group_writer(&nw, NLMSG_SMALL, NETLINK_GENERIC, ctrl_group_id)) {
+		dump_family(&hdr, &ghdr, gf, &nw);
+		nlmsg_flush(&nw);
+		return;
+	}
+	NL_LOG(LOG_DEBUG, "error allocating group writer");
+}
+
+static const struct genl_cmd nlctrl_cmds[] = {
+	{
+		.cmd_num = CTRL_CMD_GETFAMILY,
+		.cmd_name = "GETFAMILY",
+		.cmd_cb = nlctrl_handle_getfamily,
+		.cmd_flags = GENL_CMD_CAP_DO | GENL_CMD_CAP_DUMP, GENL_CMD_CAP_HASPOL,
+	},
+};
+
+static void
+genl_nlctrl_init()
+{
+	ctrl_family_id = genl_register_family(CTRL_FAMILY_NAME, 0, 2, CTRL_ATTR_MAX);
+	genl_register_cmds(CTRL_FAMILY_NAME, nlctrl_cmds, NL_ARRAY_LEN(nlctrl_cmds));
+	ctrl_group_id = genl_register_group(CTRL_FAMILY_NAME, "notify");
+}
+
+static void
+genl_nlctrl_destroy()
+{
+	genl_unregister_family(CTRL_FAMILY_NAME);
+}
+
+static const struct nlhdr_parser *all_parsers[] = { &genl_parser };
+
+static void
+genl_load(void *u __unused)
+{
+	GENL_LOCK_INIT();
+	NL_VERIFY_PARSERS(all_parsers);
+	netlink_register_proto(NETLINK_GENERIC, "NETLINK_GENERIC", genl_handle_message);
+	genl_nlctrl_init();
+}
+SYSINIT(genl_load, SI_SUB_PROTO_DOMAIN, SI_ORDER_THIRD, genl_load, NULL);
+
+static void
+genl_unload(void *u __unused)
+{
+	genl_nlctrl_destroy();
+	GENL_LOCK_DESTROY();
+	epoch_wait_preempt(net_epoch_preempt);
+}
+SYSUNINIT(genl_unload, SI_SUB_PROTO_DOMAIN, SI_ORDER_THIRD, genl_unload, NULL);

sys/netlink/netlink_route.c
@@ -1,0 +1,135 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2022 Alexander V. Chernikov <melifaro@FreeBSD.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+#include <sys/types.h>
+#include <sys/malloc.h>
+#include <sys/socket.h>
+#include <sys/ck.h>
+
+#include <net/route.h>
+#include <net/route/route_ctl.h>
+#include <netlink/netlink.h>
+#include <netlink/netlink_ctl.h>
+#include <netlink/netlink_var.h>
+#include <netlink/netlink_route.h>
+#include <netlink/route/route_var.h>
+
+#define	DEBUG_MOD_NAME	nl_route_core
+#define	DEBUG_MAX_LEVEL	LOG_DEBUG3
+#include <netlink/netlink_debug.h>
+_DECLARE_DEBUG(LOG_DEBUG);
+
+#define	HANDLER_MAX_NUM	(NL_RTM_MAX + 10)
+static const struct rtnl_cmd_handler *rtnl_handler[HANDLER_MAX_NUM] = {};
+
+bool
+rtnl_register_messages(const struct rtnl_cmd_handler *handlers, int count)
+{
+	for (int i = 0; i < count; i++) {
+		if (handlers[i].cmd >= HANDLER_MAX_NUM)
+			return (false);
+		MPASS(rtnl_handler[handlers[i].cmd] == NULL);
+	}
+	for (int i = 0; i < count; i++)
+		rtnl_handler[handlers[i].cmd] = &handlers[i];
+	return (true);
+}
+
+/*
+ * Handler called by netlink subsystem when matching netlink message is received
+ */
+static int
+rtnl_handle_message(struct nlmsghdr *hdr, struct nl_pstate *npt)
+{
+	const struct rtnl_cmd_handler *cmd;
+	struct epoch_tracker et;
+	struct nlpcb *nlp = npt->nlp;
+	int error = 0;
+
+	if (__predict_false(hdr->nlmsg_type >= HANDLER_MAX_NUM)) {
+		NLMSG_REPORT_ERR_MSG(npt, "unknown message type: %d", hdr->nlmsg_type);
+		return (ENOTSUP);
+	}
+
+	cmd = rtnl_handler[hdr->nlmsg_type];
+	if (__predict_false(cmd == NULL)) {
+		NLMSG_REPORT_ERR_MSG(npt, "unknown message type: %d", hdr->nlmsg_type);
+		return (ENOTSUP);
+	}
+
+	NLP_LOG(LOG_DEBUG2, nlp, "received msg %s(%d) len %d", cmd->name,
+	    hdr->nlmsg_type, hdr->nlmsg_len);
+
+	if (cmd->priv != 0 && !nlp_has_priv(nlp, cmd->priv)) {
+		NLP_LOG(LOG_DEBUG2, nlp, "priv %d check failed for msg %s", cmd->priv, cmd->name);
+		return (EPERM);
+	} else if (cmd->priv != 0)
+		NLP_LOG(LOG_DEBUG3, nlp, "priv %d check passed for msg %s", cmd->priv, cmd->name);
+
+	bool need_epoch = !(cmd->flags & RTNL_F_NOEPOCH);
+
+	if (need_epoch)
+		NET_EPOCH_ENTER(et);
+	error = cmd->cb(hdr, nlp, npt);
+	if (need_epoch)
+		NET_EPOCH_EXIT(et);
+
+	NLP_LOG(LOG_DEBUG3, nlp, "message %s -> error %d", cmd->name, error);
+
+	return (error);
+}
+
+static struct rtbridge nlbridge = { .route_f = rtnl_handle_route_event };
+static struct rtbridge *nlbridge_orig_p;
+
+static void
+rtnl_load(void *u __unused)
+{
+	NL_LOG(LOG_NOTICE, "rtnl loading");
+	nlbridge_orig_p = netlink_callback_p;
+	netlink_callback_p = &nlbridge;
+	rtnl_neighs_init();
+	rtnl_ifaces_init();
+	rtnl_nexthops_init();
+	rtnl_routes_init();
+	netlink_register_proto(NETLINK_ROUTE, "NETLINK_ROUTE", rtnl_handle_message);
+}
+SYSINIT(rtnl_load, SI_SUB_PROTO_DOMAIN, SI_ORDER_THIRD, rtnl_load, NULL);
+
+static void
+rtnl_unload(void *u __unused)
+{
+	netlink_callback_p = nlbridge_orig_p;
+	rtnl_ifaces_destroy();
+	rtnl_neighs_destroy();
+
+	/* Wait till all consumers read nlbridge data */
+	epoch_wait_preempt(net_epoch_preempt);
+}
+SYSUNINIT(rtnl_unload, SI_SUB_PROTO_DOMAIN, SI_ORDER_THIRD, rtnl_unload, NULL);

etc/mtree/BSD.include.dist
@@ -269,6 +269,10 @@
     ..
     netinet6
     ..
+    netlink
+        route
+        ..
+    ..
     netipsec
     ..
     netnatm

sys/modules/Makefile
@@ -266,6 +266,7 @@ SUBDIR=	\
 	my \
 	${_nctgpio} \
 	${_neta} \
+	netlink \
 	${_netgraph} \
 	${_nfe} \
 	nfscl \

sys/modules/netlink/Makefile
@@ -1,0 +1,17 @@
+.PATH:	${SRCTOP}/sys/netlink
+KMOD=	netlink
+
+SRCS =	netlink_module.c netlink_domain.c netlink_io.c \
+	netlink_message_parser.c netlink_message_writer.c  netlink_generic.c \
+	netlink_route.c route/iface.c route/iface_drivers.c route/neigh.c \
+	route/nexthop.c route/route.c
+
+EXPORT_SYMS=
+EXPORT_SYMS+=	nlmsg_get_chain_writer
+EXPORT_SYMS+=	nlmsg_refill_buffer
+EXPORT_SYMS+=	nlmsg_end
+EXPORT_SYMS+=	nlmsg_flush
+
+EXPORT_SYMS= YES
+
+.include <bsd.kmod.mk>

sys/net/route.c
@@ -694,3 +694,14 @@ rt_routemsg_info(int cmd, struct rt_addrinfo *info, int fibnum)
 
 	return (rtsock_routemsg_info(cmd, info, fibnum));
 }
+
+/* Netlink-related callbacks needed to glue rtsock, netlink and linuxolator */
+static void
+ignore_route_event(uint32_t fibnum, const struct rib_cmd_info *rc)
+{
+}
+static struct rtbridge ignore_cb = { .route_f = ignore_route_event };
+
+void *linux_netlink_p = NULL; /* Callback pointer for Linux translator functions */
+struct rtbridge *rtsock_callback_p = &ignore_cb;
+struct rtbridge *netlink_callback_p = &ignore_cb;

sys/net/route/route_ctl.h
@@ -189,4 +189,11 @@ void rib_unsubscribe_locked(struct rib_subscription *rs);
 void rib_notify(struct rib_head *rnh, enum rib_subscription_type type,
     struct rib_cmd_info *rc);
 
+/* Event bridge */
+typedef void route_event_f(uint32_t fibnum, const struct rib_cmd_info *rc);
+struct rtbridge{
+	route_event_f	*route_f;
+};
+extern struct rtbridge *rtsock_callback_p;
+extern struct rtbridge *netlink_callback_p;
 #endif

sys/net/rtsock.c
@@ -219,6 +219,7 @@ static void	send_rtm_reply(struct socket *so, struct rt_msghdr *rtm,
 			int rtm_errno);
 static bool	can_export_rte(struct ucred *td_ucred, bool rt_is_host,
 			const struct sockaddr *rt_dst);
+static void	rtsock_notify_event(uint32_t fibnum, const struct rib_cmd_info *rc);
 
 static struct netisr_handler rtsock_nh = {
 	.nh_name = "rtsock",
@@ -273,6 +274,45 @@ vnet_rts_uninit(void)
 VNET_SYSUNINIT(vnet_rts_uninit, SI_SUB_PROTO_DOMAIN, SI_ORDER_THIRD,
     vnet_rts_uninit, 0);
 #endif
+
+static void
+report_route_event(const struct rib_cmd_info *rc, void *_cbdata)
+{
+	uint32_t fibnum = (uint32_t)(uintptr_t)_cbdata;
+	struct nhop_object *nh;
+
+	nh = rc->rc_cmd == RTM_DELETE ? rc->rc_nh_old : rc->rc_nh_new;
+	rt_routemsg(rc->rc_cmd, rc->rc_rt, nh, fibnum);
+}
+
+static void
+rts_handle_route_event(uint32_t fibnum, const struct rib_cmd_info *rc)
+{
+#ifdef ROUTE_MPATH
+	if ((rc->rc_nh_new && NH_IS_NHGRP(rc->rc_nh_new)) ||
+	    (rc->rc_nh_old && NH_IS_NHGRP(rc->rc_nh_old))) {
+		rib_decompose_notification(rc, report_route_event,
+		    (void *)(uintptr_t)fibnum);
+	} else
+#endif
+		report_route_event(rc, (void *)(uintptr_t)fibnum);
+}
+static struct rtbridge rtsbridge = { .route_f = rts_handle_route_event };
+static struct rtbridge *rtsbridge_orig_p;
+
+static void
+rtsock_notify_event(uint32_t fibnum, const struct rib_cmd_info *rc)
+{
+	netlink_callback_p->route_f(fibnum, rc);
+}
+
+static void
+rtsock_init(void)
+{
+	rtsbridge_orig_p = rtsock_callback_p;
+	rtsock_callback_p = &rtsbridge;
+}
+SYSINIT(rtsock_init, SI_SUB_PROTO_DOMAIN, SI_ORDER_THIRD, rtsock_init, NULL);
 
 static void
 rts_handle_ifnet_arrival(void *arg __unused, struct ifnet *ifp)
@@ -1074,6 +1114,7 @@ rts_send(struct socket *so, int flags, struct mbuf *m,
 		}
 		error = rib_action(fibnum, rtm->rtm_type, &info, &rc);
 		if (error == 0) {
+			rtsock_notify_event(fibnum, &rc);
 #ifdef ROUTE_MPATH
 			if (NH_IS_NHGRP(rc.rc_nh_new) ||
 			    (rc.rc_nh_old && NH_IS_NHGRP(rc.rc_nh_old))) {
@@ -1095,6 +1136,7 @@ rts_send(struct socket *so, int flags, struct mbuf *m,
 	case RTM_DELETE:
 		error = rib_action(fibnum, RTM_DELETE, &info, &rc);
 		if (error == 0) {
+			rtsock_notify_event(fibnum, &rc);
 #ifdef ROUTE_MPATH
 			if (NH_IS_NHGRP(rc.rc_nh_old) ||
 			    (rc.rc_nh_new && NH_IS_NHGRP(rc.rc_nh_new))) {

commit 2529f886581d4b002391fc8314d74757af2263a9

netlink: add unregister call in cleanup
For protocols that use netlink (generic and route for now), the unint
handler seems to have forgotten to call unregister, which will cause
the assertion the next time the module is loaded.

This patch adds unregister call to netlink_unregister_proto() for those
handlers to avoid bad things happen.

Reviewed-by: melifaro
Fixes: 013ab21c7c08 ("netlink: add netlink support")
Pull-request: https://github.com/freebsd/freebsd-src/pull/781
Signed-off-by: Lin Ma <linma@zju.edu.cn>

sys/netlink/netlink_generic.c
@@ -296,6 +296,7 @@ SYSINIT(genl_load_all, SI_SUB_PROTO_DOMAIN, SI_ORDER_THIRD, genl_load_all, NULL)
 static void
 genl_unload(void *u __unused)
 {
+	netlink_unregister_proto(NETLINK_GENERIC);
 	EVENTHANDLER_DEREGISTER(genl_family_event, family_event_tag);
 	genl_unregister_family(CTRL_FAMILY_NAME);
 	NET_EPOCH_WAIT();

sys/netlink/netlink_route.c
@@ -134,6 +134,7 @@ static void
 rtnl_unload(void *u __unused)
 {
 	netlink_callback_p = nlbridge_orig_p;
+	netlink_unregister_proto(NETLINK_ROUTE);
 	rtnl_ifaces_destroy();
 	rtnl_neighs_destroy();
 