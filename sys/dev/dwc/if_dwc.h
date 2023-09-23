b69c49d106325a9ceb06ca0d5b827d1dbcc8a54c
Emmanuel Vadot <manu@FreeBSD.org>
2023-09-23 18:56:26 +0200

    dwc: Add support for aal
    snps,aal is used to indicate that the DMA engine should
    use Address-Aligned Beats.

--- sys/dev/dwc/if_dwc.h
+++ sys/dev/dwc/if_dwc.h
@@ -220,6 +220,7 @@
 /* DMA */
 #define	BUS_MODE		0x1000
 #define	 BUS_MODE_MIXEDBURST	(1 << 26)
+#define	 BUS_MODE_AAL		(1 << 25)
 #define	 BUS_MODE_EIGHTXPBL	(1 << 24) /* Multiplies PBL by 8 */
 #define	 BUS_MODE_USP		(1 << 23)
 #define	 BUS_MODE_RPBL_SHIFT	17 /* Single block transfer size */
