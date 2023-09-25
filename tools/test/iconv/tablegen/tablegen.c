c0b3cc01c6914e52c57b5b3afb712f72eff320e1
John Baldwin <jhb@FreeBSD.org>
2023-09-25 07:55:43 -0700

    Update a few tools to not embed $FreeBSD$ in generated files
    Reviewed by:	imp, emaste
    Differential Revision:	https://reviews.freebsd.org/D41959

--- tools/test/iconv/tablegen/tablegen.c
+++ tools/test/iconv/tablegen/tablegen.c
@@ -254,7 +254,6 @@ main (int argc, char *argv[])
 		if (dflag && iconvctl(cd, ICONV_SET_FALLBACKS, &fbs) != 0)
 			err(1, NULL);
 		if (cflag) {
-			printf("# $FreeBSD$\n\n");
 			printf("TYPE\t\tROWCOL\n");
 			printf("NAME\t\tUCS/%s\n", argv[0]);
 			printf("%s", citrus_common);
@@ -266,7 +265,6 @@ main (int argc, char *argv[])
 		if (dflag && (iconvctl(cd, ICONV_SET_FALLBACKS, &fbs) != 0))
 			err(1, NULL);
 		if (cflag) {
-			printf("# $FreeBSD$\n\n");
 			printf("TYPE\t\tROWCOL\n");
 			printf("NAME\t\t%s/UCS\n", argv[0]);
 			printf("%s", citrus_common);
