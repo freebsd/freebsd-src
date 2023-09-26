14a06e98a2dd6984984c328eaaaaaf02470c8822
Olivier Certner <olce.freebsd@certner.fr>
2023-09-26 12:26:46 +0200

    pdinit(): Fix comment
    Reviewed by:            markj, kib
    MFC after:              1 week
    Sponsored by:           The FreeBSD Foundation
    Differential Revision:  https://reviews.freebsd.org/D42256

--- sys/kern/kern_descrip.c
+++ sys/kern/kern_descrip.c
@@ -2238,7 +2238,7 @@ fdinit(void)
  * Build a pwddesc structure from another.
  * Copy the current, root, and jail root vnode references.
  *
- * If pdp is not NULL, return with it shared locked.
+ * If pdp is not NULL and keeplock is true, return with it (exclusively) locked.
  */
 struct pwddesc *
 pdinit(struct pwddesc *pdp, bool keeplock)
