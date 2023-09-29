618f598880612da07467344f497ebf7230a51518
Zhenlei Huang <zlei@FreeBSD.org>
2023-09-29 08:01:43 +0800

    ipfilter: Add sysctl flag CTLFLAG_TUN to loader tunable
    The sysctl variable `net.inet.ipf.large_nat` is actually a loader
    tunable. Add sysctl flag CTLFLAG_TUN to it so that `sysctl -T` will
    report it correctly.
    
    No functional change intended.
    
    Reviewed by:	cy (for #network)
    Fixes:		0cc4343432a7 ipfilter: Make LARGE_NAT a tunable
    MFC after:	3 days
    Differential Revision:	https://reviews.freebsd.org/D42005

--- sys/netpfil/ipfilter/netinet/mlfk_ipl.c
+++ sys/netpfil/ipfilter/netinet/mlfk_ipl.c
@@ -134,7 +134,7 @@ SYSCTL_IPF(_net_inet_ipf, OID_AUTO, fr_running, CTLFLAG_RD,
 	   &VNET_NAME(ipfmain.ipf_running), 0, "IPF is running");
 SYSCTL_IPF(_net_inet_ipf, OID_AUTO, fr_chksrc, CTLFLAG_RW, &VNET_NAME(ipfmain.ipf_chksrc), 0, "");
 SYSCTL_IPF(_net_inet_ipf, OID_AUTO, fr_minttl, CTLFLAG_RW, &VNET_NAME(ipfmain.ipf_minttl), 0, "");
-SYSCTL_IPF(_net_inet_ipf, OID_AUTO, large_nat, CTLFLAG_RD, &VNET_NAME(ipfmain.ipf_large_nat), 0, "large_nat");
+SYSCTL_IPF(_net_inet_ipf, OID_AUTO, large_nat, CTLFLAG_RDTUN | CTLFLAG_NOFETCH, &VNET_NAME(ipfmain.ipf_large_nat), 0, "large_nat");
 
 #define CDEV_MAJOR 79
 #include <sys/poll.h>
