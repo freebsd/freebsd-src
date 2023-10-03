freebsd/freebsd-src

commit 980746e5cb264a70ad3dcbbf38ba919ad3436aa1

fbt: simplify arm64 function-prologue parsing
Reviewed by:	markj
Approved by:	markj (mentor)
Sponsored by:	The FreeBSD Foundation
Differential Revision:	https://reviews.freebsd.org/D40364

sys/cddl/dev/fbt/aarch64/fbt_isa.c
@@ -130,35 +130,25 @@ fbt_provide_module_function(linker_file_t lf, int symindx,
 	 */
 	if (*instr == NOP_INSTR)
 		found = true;
-	if (!found) {
-		for (; instr < limit; instr++) {
+	for (; !found && instr < limit; instr++) {
+		/*
+		 * Functions start with "stp xt1, xt2, [xn, <const>]!" or
+		 * "sub sp, sp, <const>".
+		 *
+		 * Sometimes the compiler will have a sub instruction that is
+		 * not of the above type so don't stop if we see one.
+		 */
+		if ((*instr & LDP_STP_MASK) == STP_64) {
 			/*
-			 * Some functions start with
-			 * "stp xt1, xt2, [xn, <const>]!"
+			 * Assume any other store of this type means we are
+			 * past the function prologue.
 			 */
-			if ((*instr & LDP_STP_MASK) == STP_64) {
-				/*
-				 * Assume any other store of this type means we
-				 * are past the function prolog.
-				 */
-				if (((*instr >> ADDR_SHIFT) & ADDR_MASK) == 31)
-					found = true;
-				break;
-			}
-
-			/*
-			 * Some functions start with a "sub sp, sp, <const>"
-			 * Sometimes the compiler will have a sub instruction
-			 * that is not of the above type so don't stop if we
-			 * see one.
-			 */
-			if ((*instr & SUB_MASK) == SUB_INSTR &&
-			    ((*instr >> SUB_RD_SHIFT) & SUB_R_MASK) == 31 &&
-			    ((*instr >> SUB_RN_SHIFT) & SUB_R_MASK) == 31) {
+			if (((*instr >> ADDR_SHIFT) & ADDR_MASK) == 31)
 				found = true;
-				break;
-			}
-		}
+		} else if ((*instr & SUB_MASK) == SUB_INSTR &&
+		    ((*instr >> SUB_RD_SHIFT) & SUB_R_MASK) == 31 &&
+		    ((*instr >> SUB_RN_SHIFT) & SUB_R_MASK) == 31)
+			found = true;
 	}
 
 	if (!found)

commit 4c06a3d4851aa48a161cc20fdc5bb0f0d6eae49d

dtrace: fix fbt regression for aarch64
fbt computes incorrect instruction position for AArch64 kernel module symbol.

The issue is with the for loop, it does an extra increment of instr pointer
after the required instruction is found. Hence, a wrong instruction is
targeted for patching.

Fixes:          980746e5cb26 ("fbt: simplify arm64 function-prologue parsing")
Pull-request:   https://github.com/freebsd/freebsd-src/pull/855
Signed-off-by:  Igor Ostapenko <pm@igoro.pro>

sys/cddl/dev/fbt/aarch64/fbt_isa.c
@@ -90,7 +90,6 @@ fbt_provide_module_function(linker_file_t lf, int symindx,
 	uint32_t *instr, *limit;
 	const char *name;
 	char *modname;
-	bool found;
 	int offs;
 
 	modname = opaque;
@@ -119,16 +118,16 @@ fbt_provide_module_function(linker_file_t lf, int symindx,
 	if ((*instr & BTI_MASK) == BTI_INSTR)
 		instr++;
 
-	/* Look for stp (pre-indexed) operation */
-	found = false;
 	/*
 	 * If the first instruction is a nop it's a specially marked
 	 * asm function. We only support a nop first as it's not a normal
 	 * part of the function prologue.
 	 */
 	if (*instr == NOP_INSTR)
-		found = true;
-	for (; !found && instr < limit; instr++) {
+		goto found;
+
+	/* Look for stp (pre-indexed) or sub operation */
+	for (; instr < limit; instr++) {
 		/*
 		 * Functions start with "stp xt1, xt2, [xn, <const>]!" or
 		 * "sub sp, sp, <const>".
@@ -142,14 +141,14 @@ fbt_provide_module_function(linker_file_t lf, int symindx,
 			 * past the function prologue.
 			 */
 			if (((*instr >> ADDR_SHIFT) & ADDR_MASK) == 31)
-				found = true;
+				break;
 		} else if ((*instr & SUB_MASK) == SUB_INSTR &&
 		    ((*instr >> SUB_RD_SHIFT) & SUB_R_MASK) == 31 &&
 		    ((*instr >> SUB_RN_SHIFT) & SUB_R_MASK) == 31)
-			found = true;
+			break;
 	}
-
-	if (!found)
+found:
+	if (instr >= limit)
 		return (0);
 
 	fbt = malloc(sizeof (fbt_probe_t), M_FBT, M_WAITOK | M_ZERO);