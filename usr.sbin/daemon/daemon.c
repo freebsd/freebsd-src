321326b13ed01d950de4b35cbc9c85233a432a67
Dag-Erling Smørgrav <des@FreeBSD.org>
2023-10-06 18:44:52 +0200

    daemon: Disable stdio buffering.
    The daemon utility already does its own buffering and retransmits its
    child's output line by line.  There's no need for stdio to add its own
    buffering on top of this.
    
    MFC after:	1 week
    Sponsored by:	Modirum MDPay
    Reviewed by:	allanjude
    Differential Revision:	https://reviews.freebsd.org/D42111

--- usr.sbin/daemon/daemon.c
+++ usr.sbin/daemon/daemon.c
@@ -420,6 +420,7 @@ daemon_eventloop(struct daemon_state *state)
 	close(state->pipe_fd[1]);
 	state->pipe_fd[1] = -1;
 	setproctitle("%s[%d]", state->title, (int)state->pid);
+	setbuf(stdout, NULL);
 
 	while (state->mode != MODE_NOCHILD) {
 		ret = kevent(kq, NULL, 0, &event, 1, NULL);
