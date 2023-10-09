5a2767d20325d5065ae4bc04d954530150551016
Alan Somers <asomers@FreeBSD.org>
2023-10-09 12:26:25 -0600

    Fix multiple bugs with ctld's UCL parsing
    * Don't segfault when parsing a misformatted auth-group section
    * If the config file specifies a chap section within a target but no
      auth-group, create a new anonymous auth-group.  That matches the
      behavior with non-UCL config files.
    * Protect some potential segfaults with assertions
    
    PR:		274380
    Sponsored by:	Axcient
    Reviewed by:	jhb
    Differential Revision: https://reviews.freebsd.org/D43198
    
    (cherry picked from commit 2391e536c01cb51d2a2c11a0f5820481314e9ec2)

--- usr.sbin/ctld/ctld.c
+++ usr.sbin/ctld/ctld.c
@@ -533,6 +533,7 @@ auth_group_find(const struct conf *conf, const char *name)
 {
 	struct auth_group *ag;
 
+	assert(name != NULL);
 	TAILQ_FOREACH(ag, &conf->conf_auth_groups, ag_next) {
 		if (ag->ag_name != NULL && strcmp(ag->ag_name, name) == 0)
 			return (ag);
