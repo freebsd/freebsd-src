4e00940e9f1f7dc1210a094d916ef930ff447e4a
Olivier Certner <olce.freebsd@certner.fr>
2023-10-13 14:13:30 +0200

    uma: UMA_ALIGN_CACHE: Resolve the proper value at use point
    Having a special value of -1 that is resolved internally to
    'uma_align_cache' provides no significant advantages and prevents
    changing that variable to an unsigned type, which is natural for an
    alignment mask.  So suppress it and replace its use with a call to
    uma_get_align_mask().  The small overhead of the added function call is
    irrelevant since UMA_ALIGN_CACHE is only used when creating new zones,
    which is not performance critical.
    
    Reviewed by:            markj, kib
    Sponsored by:           The FreeBSD Foundation
    Differential Revision:  https://reviews.freebsd.org/D42259
    
    (cherry picked from commit e557eafe7233f8231c1f5f5b098e4bab8e818645)
    
    Approved by:    markj (mentor)

--- sys/vm/uma_core.c
+++ sys/vm/uma_core.c
@@ -3182,7 +3182,7 @@ uma_kcreate(uma_zone_t zone, size_t size, uma_init uminit, uma_fini fini,
 	args.size = size;
 	args.uminit = uminit;
 	args.fini = fini;
-	args.align = (align == UMA_ALIGN_CACHE) ? uma_cache_align_mask : align;
+	args.align = align;
 	args.flags = flags;
 	args.zone = zone;
 	return (zone_alloc_item(kegs, &args, UMA_ANYDOMAIN, M_WAITOK));
@@ -3195,7 +3195,6 @@ uma_set_cache_align_mask(int mask)
 {
 
 	if (mask >= 0)
-		/* UMA_ALIGN_CACHE is also not permitted here. */
 		uma_cache_align_mask = mask;
 }
 
