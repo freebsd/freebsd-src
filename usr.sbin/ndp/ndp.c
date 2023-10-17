169746a877f6f90214fec17bc0861eaf94cde855
R. Christian McDonald <rcm@rcm.sh>
2023-10-17 18:57:22 +0200

    ndp: fix timestamp display output
    The current xo_format string is incorrect. This restores the display
    format prior to libxo-ification work while also explicitly marking
    tv_sec and tv_usec as encoded output only.
    
    MFC after:	1 week
    Reviewed by:	kp
    Sponsored by:	Rubicon Communications, LLC ("Netgate")
    Differential Revision:	https://reviews.freebsd.org/D42269
    
    (cherry picked from commit 2bb78b46e02413483409fe73244995524b838b6e)

--- usr.sbin/ndp/ndp.c
+++ usr.sbin/ndp/ndp.c
@@ -1544,7 +1544,7 @@ ts_print(const struct timeval *tvp)
 
 	/* Default */
 	sec = (tvp->tv_sec + thiszone) % 86400;
-	xo_emit("{:tv_sec/%lld}{:tv_usec/%lld}%02d:%02d:%02d.%06u ",
+	xo_emit("{e:tv_sec/%lld}{e:tv_usec/%lld}{d:/%02d:%02d:%02d.%06u} ",
 	    tvp->tv_sec, tvp->tv_usec,
 	    sec / 3600, (sec % 3600) / 60, sec % 60, (u_int32_t)tvp->tv_usec);
 }
