39eee4471f2f1ae1d1fc47efe9d2cc5276c93349
Dag-Erling Smørgrav <des@FreeBSD.org>
2023-10-18 16:05:37 +0200

    hesiod: Modernize.
    Reviewed by:	zlei
    Differential Revision:	https://reviews.freebsd.org/D42190

--- lib/libc/net/hesiod.c
+++ lib/libc/net/hesiod.c
@@ -81,8 +81,7 @@ static void	  translate_errors(void);
  *	initialize a hesiod_p.
  */
 int 
-hesiod_init(context)
-	void	**context;
+hesiod_init(void **context)
 {
 	struct hesiod_p	*ctx;
 	const char	*p, *configname;
@@ -130,8 +129,7 @@ hesiod_init(context)
  *	Deallocates the hesiod_p.
  */
 void 
-hesiod_end(context)
-	void	*context;
+hesiod_end(void *context)
 {
 	struct hesiod_p *ctx = (struct hesiod_p *) context;
 
@@ -221,10 +219,7 @@ hesiod_to_bind(void *context, const char *name, const char *type)
  *	by the resolver.
  */
 char **
-hesiod_resolve(context, name, type)
-	void		*context;
-	const char	*name;
-	const char	*type;
+hesiod_resolve(void *context, const char *name, const char *type)
 {
 	struct hesiod_p	*ctx = (struct hesiod_p *) context;
 	char		*bindname, **retvec;
@@ -243,9 +238,7 @@ hesiod_resolve(context, name, type)
 
 /*ARGSUSED*/
 void 
-hesiod_free_list(context, list)
-	void	 *context;
-	char	**list;
+hesiod_free_list(void *context, char **list)
 {
 	char  **p;
 
@@ -263,9 +256,7 @@ hesiod_free_list(context, list)
  *	or ctx->rhs which need to be freed by the caller.
  */
 static int 
-read_config_file(ctx, filename)
-	struct hesiod_p	*ctx;
-	const char	*filename;
+read_config_file(struct hesiod_p *ctx, const char *filename)
 {
 	char	*key, *data, *p, **which;
 	char	 buf[MAXDNAME + 7];
@@ -357,9 +348,7 @@ read_config_file(ctx, filename)
  *	return a list of them.
  */
 static char **
-get_txt_records(qclass, name)
-	int		 qclass;
-	const char	*name;
+get_txt_records(int qclass, const char *name)
 {
 	HEADER		*hp;
 	unsigned char	 qbuf[PACKETSZ], abuf[MAX_HESRESP], *p, *eom, *eor;
@@ -483,16 +472,14 @@ static void	 *context;
 static int	  errval = HES_ER_UNINIT;
 
 int
-hes_init()
+hes_init(void)
 {
 	init_context();
 	return errval;
 }
 
 char *
-hes_to_bind(name, type)
-	const char	*name;
-	const char	*type;
+hes_to_bind(const char *name, const char *type)
 {
 	static	char	*bindname;
 	if (init_context() < 0)
@@ -506,9 +493,7 @@ hes_to_bind(name, type)
 }
 
 char **
-hes_resolve(name, type)
-	const char	*name;
-	const char	*type;
+hes_resolve(const char *name, const char *type)
 {
 	static char	**list;
 
@@ -529,20 +514,19 @@ hes_resolve(name, type)
 }
 
 int
-hes_error()
+hes_error(void)
 {
 	return errval;
 }
 
 void
-hes_free(hp)
-	char **hp;
+hes_free(char **hp)
 {
 	hesiod_free_list(context, hp);
 }
 
 static int
-init_context()
+init_context(void)
 {
 	if (!inited) {
 		inited = 1;
@@ -556,7 +540,7 @@ init_context()
 }
 
 static void
-translate_errors()
+translate_errors(void)
 {
 	switch (errno) {
 	case ENOENT:
