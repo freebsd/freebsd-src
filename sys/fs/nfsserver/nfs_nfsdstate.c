f300335d9aebf2e99862bf783978bd44ede23550
Rick Macklem <rmacklem@FreeBSD.org>
2023-10-19 12:35:35 -0700

    nfsd: Fix NFSv4.1/4.2 Claim_Deleg_Cur_FH
    When I implemented a test patch using Open Claim_Deleg_Cur_FH
    I discovered that the NFSv4.1/4.2 server was broken for this
    Open option.  Fortunately it is never used by the FreeBSD
    client and never used by other clients unless delegations
    are enabled. (The FreeBSD NFSv4 server does not have delegations
    enabled by default.)
    
    Claim_Deleg_Cur_FH was broken because the code mistakenly
    assumed a stateID argument, which is not the case.
    This patch fixes the bug by changing the XDR parser to not
    expect a stateID and to fill most of the stateID in from the
    clientID. The clientID is the first two elements of the "other"
    array for the stateID and is sufficient to identify which
    client the delegation is issued to.  Since there is only one
    delegation issued to a client per file, this is sufficient to
    locate the correct delegation.
    
    If you are running non-FreeBSD NFSv4.1/4.2 mounts against the
    FreeBSD server, you need this patch if you have delegations enabled.
    
    PR:	274574
    MFC after:	2 weeks

--- sys/fs/nfsserver/nfs_nfsdstate.c
+++ sys/fs/nfsserver/nfs_nfsdstate.c
@@ -2568,6 +2568,10 @@ tryagain:
 	    /*
 	     * For Delegate_Cur, search for the matching Delegation,
 	     * which indicates no conflict.
+	     * For NFSv4.1/4.2 Claim_Deleg_Cur_FH only provides
+	     * the clientid, which is the first two "other" elements
+	     * for the stateid.  This should be sufficient, since there
+	     * is only one delegation per client and file.
 	     * An old delegation should have been recovered by the
 	     * client doing a Claim_DELEGATE_Prev, so I won't let
 	     * it match and return NFSERR_EXPIRED. Should I let it
@@ -2578,8 +2582,8 @@ tryagain:
 		    (((nd->nd_flag & ND_NFSV41) != 0 &&
 		    stateidp->seqid == 0) ||
 		    stateidp->seqid == stp->ls_stateid.seqid) &&
-		    !NFSBCMP(stateidp->other, stp->ls_stateid.other,
-			  NFSX_STATEIDOTHER))
+		    stateidp->other[0] == stp->ls_stateid.other[0] &&
+		    stateidp->other[1] == stp->ls_stateid.other[1])
 			break;
 	    }
 	    if (stp == LIST_END(&lfp->lf_deleg) ||
@@ -2830,6 +2834,10 @@ tryagain:
 	    /*
 	     * For Delegate_Cur, search for the matching Delegation,
 	     * which indicates no conflict.
+	     * For NFSv4.1/4.2 Claim_Deleg_Cur_FH only provides
+	     * the clientid, which is the first two "other" elements
+	     * for the stateid.  This should be sufficient, since there
+	     * is only one delegation per client and file.
 	     * An old delegation should have been recovered by the
 	     * client doing a Claim_DELEGATE_Prev, so I won't let
 	     * it match and return NFSERR_EXPIRED. Should I let it
@@ -2840,8 +2848,8 @@ tryagain:
 		    (((nd->nd_flag & ND_NFSV41) != 0 &&
 		    stateidp->seqid == 0) ||
 		    stateidp->seqid == stp->ls_stateid.seqid) &&
-		    !NFSBCMP(stateidp->other, stp->ls_stateid.other,
-			NFSX_STATEIDOTHER))
+		    stateidp->other[0] == stp->ls_stateid.other[0] &&
+		    stateidp->other[1] == stp->ls_stateid.other[1])
 			break;
 	    }
 	    if (stp == LIST_END(&lfp->lf_deleg) ||
