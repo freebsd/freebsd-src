f9ac06af3b2dec6ac75f5639cb1396f9d943fc06
Bjoern A. Zeeb <bz@FreeBSD.org>
2023-10-29 14:25:23 +0000

    net80211: move net_epoch into net80211
    Move the net_epoch into net80211 around the if_input calls and out of
    the driver (in this first case LinuxKPI).  This reduces coverage but
    also allows us to alloc in calls like (*ampdu_rx_start) which do not
    actually pass data up the stack.
    
    The follow-up commits (squashed) reverts:
    b65f813c1ab99448278961c5ca80dc422b1eae29
      Revert "Widen EPOCH(9) usage in PCI WLAN drivers.",
    21c4082de9e2cf9a0fd81a9a981ab06022956847
      Revert "Widen EPOCH(9) usage in USB WLAN drivers.",
    17c328b6aebfa03cd1c2cbfbbc617e3b341bf1e4
      Revert "Enter the network epoch in USB WiFi drivers when processing input",
    af2441fbc7fa9e522e7f8697e5a181bdd4ff9e00
      Revert "[ath] Attempt to fix epoch handling.",
    and 6c3e93cb5a4aa4b8a2d8d4d326f2a7c34d3a4458 for if_ath.c only
      ath: Revert "Use NET_TASK_INIT() and NET_GROUPTASK_INIT() for drivers that process".
    
    Sponsored by:	The FreeBSD Foundation
    Differential Revision: https://reviews.freebsd.org/D42427
    
    (cherry picked from commit de607e3c230be88f6232b9c9fd6c37199648dc8c)
    (cherry picked from commit 1c6dd33d26eb02c6145383a49150965eeca61120)
    (cherry picked from commit 39b702797994fbd62a21dd3af6d4cd7045fded23)
    (cherry picked from commit 75f298492a12d53df97c26963ec9f2dc0b052878)
    (cherry picked from commit eb3821e6d92fa45ece7ec08efde45e35be1bdc95)
    (cherry picked from commit 82506f26c03aa312b91e01a797f31e061749a76d)

--- sys/net80211/ieee80211_input.c
+++ sys/net80211/ieee80211_input.c
@@ -273,6 +273,7 @@ void
 ieee80211_deliver_data(struct ieee80211vap *vap,
 	struct ieee80211_node *ni, struct mbuf *m)
 {
+	struct epoch_tracker et;
 	struct ether_header *eh = mtod(m, struct ether_header *);
 	struct ifnet *ifp = vap->iv_ifp;
 
@@ -303,7 +304,9 @@ ieee80211_deliver_data(struct ieee80211vap *vap,
 		m->m_pkthdr.ether_vtag = ni->ni_vlan;
 		m->m_flags |= M_VLANTAG;
 	}
+	NET_EPOCH_ENTER(et);
 	ifp->if_input(ifp, m);
+	NET_EPOCH_EXIT(et);
 }
 
 struct mbuf *
