057da7fcb14b4facf58eeaa4dd681315f79200e2
Mark Johnston <markj@FreeBSD.org>
2023-11-02 14:33:37 -0400

    riscv: Port improvements from arm64/amd64 pmaps, part 1
    - When promoting, do not require that all PTEs all have PTE_A set.
      Instead, record whether they did and store this information in the
      PTP's valid bits.
    - Synchronize some comments in pmap_promote_l2().
    - Make pmap_promote_l2() scan starting from the end of the 2MB range
      instead of the beginning.  See the commit log for 9d1b7fa31f510 for
      justification of this, which I believe applies here as well.
    
    Reviewed by:	kib
    MFC after:	1 month
    Differential Revision:	https://reviews.freebsd.org/D42288

--- sys/riscv/include/pte.h
+++ sys/riscv/include/pte.h
@@ -80,7 +80,7 @@ typedef	uint64_t	pn_t;			/* page number */
 #define	PTE_RWX		(PTE_R | PTE_W | PTE_X)
 #define	PTE_RX		(PTE_R | PTE_X)
 #define	PTE_KERN	(PTE_V | PTE_R | PTE_W | PTE_A | PTE_D)
-#define	PTE_PROMOTE	(PTE_V | PTE_RWX | PTE_D | PTE_A | PTE_G | PTE_U | \
+#define	PTE_PROMOTE	(PTE_V | PTE_RWX | PTE_D | PTE_G | PTE_U | \
 			 PTE_SW_MANAGED | PTE_SW_WIRED)
 
 /* Bits 63 - 54 are reserved for future use. */
