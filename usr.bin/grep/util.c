fb7b8d6f13f233dccce0db7d5d670ad3e5900d79
Kyle Evans <kevans@FreeBSD.org>
2023-11-04 21:08:36 -0500

    grep: don't rely on implementation-defined malloc(0) behavior
    The very few places that rely on malloc/calloc of a zero-size region
    won't attempt to dereference it, so just return NULL rather than rolling
    the dice with the underlying malloc implementation.
    
    Reported by:	brooks, Shawn Webb

--- usr.bin/grep/util.c
+++ usr.bin/grep/util.c
@@ -650,6 +650,8 @@ grep_malloc(size_t size)
 {
 	void *ptr;
 
+	if (size == 0)
+		return (NULL);
 	if ((ptr = malloc(size)) == NULL)
 		err(2, "malloc");
 	return (ptr);
@@ -663,6 +665,8 @@ grep_calloc(size_t nmemb, size_t size)
 {
 	void *ptr;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	if ((ptr = calloc(nmemb, size)) == NULL)
 		err(2, "calloc");
 	return (ptr);
