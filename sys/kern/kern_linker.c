e7fd435d3d4e888d9894d8c212c29ae6e2768f74
Zhenlei Huang <zlei@FreeBSD.org>
2023-11-07 12:45:25 +0800

    kern linker: Do not retry loading modules on EEXIST
    LINKER_LOAD_FILE() calls linker_load_dependencies() which will return
    EEXIST in case the module to be loaded has already been compiled into
    the kernel. Since the format of the module is now recognized then there
    is no need to retry loading with a different linker, otherwise the
    userland will get misleading error number ENOEXEC.
    
    PR:		274936
    Reviewed by:	dfr
    MFC after:	2 weeks
    Differential Revision:	https://reviews.freebsd.org/D42474
    
    (cherry picked from commit ecf710f0e04e3314a492747124166ccb7cf4019e)

--- sys/kern/kern_linker.c
+++ sys/kern/kern_linker.c
@@ -461,8 +461,11 @@ linker_load_file(const char *filename, linker_file_t *result)
 		 * If we got something other than ENOENT, then it exists but
 		 * we cannot load it for some other reason.
 		 */
-		if (error != ENOENT)
+		if (error != ENOENT) {
 			foundfile = 1;
+			if (error == EEXIST)
+				break;
+		}
 		if (lf) {
 			error = linker_file_register_modules(lf);
 			if (error == EEXIST) {
