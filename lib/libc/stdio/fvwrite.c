59ec3ffdd7ce85f32ea833e8024f7bacd36d4e97
Ed Maste <emaste@FreeBSD.org>
2023-11-07 10:00:35 -0500

    libc: remove unused errno.h include
    errno.h was added in 44cf1e5eb470, which has been reverted.
    
    Fixes: d09a3bf72c0b ("fflush: correct buffer handling in __sflush")
    Sponsored by: The FreeBSD Foundation
    
    (cherry picked from commit 418f026bd5a5084c1c4e2e91ad38051f6caa928c)
    (cherry picked from commit abe12d2f4ce31c3da0961b1b0a58df11f5a41e19)
    
    Approved by:	so

--- lib/libc/stdio/fvwrite.c
+++ lib/libc/stdio/fvwrite.c
@@ -36,7 +36,6 @@
 static char sccsid[] = "@(#)fvwrite.c	8.1 (Berkeley) 6/4/93";
 #endif /* LIBC_SCCS and not lint */
 #include <sys/cdefs.h>
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
