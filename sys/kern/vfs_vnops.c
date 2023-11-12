1703a823c5f827f9690a6baa0f7293a9fe0585ad
Konstantin Belousov <kib@FreeBSD.org>
2023-11-12 20:29:14 +0200

    vn_copy_file_range(): use local variables for invp/outvp vnodes v_mounts
    (cherry picked from commit 89188bd6ba8d8332c65498f2b71c90e5ed4b9dae)

--- sys/kern/vfs_vnops.c
+++ sys/kern/vfs_vnops.c
@@ -3046,6 +3046,7 @@ vn_copy_file_range(struct vnode *invp, off_t *inoffp, struct vnode *outvp,
     off_t *outoffp, size_t *lenp, unsigned int flags, struct ucred *incred,
     struct ucred *outcred, struct thread *fsize_td)
 {
+	struct mount *inmp, *outmp;
 	int error;
 	size_t len;
 	uint64_t uval;
@@ -3075,15 +3076,17 @@ vn_copy_file_range(struct vnode *invp, off_t *inoffp, struct vnode *outvp,
 	if (len == 0)
 		goto out;
 
+	inmp = invp->v_mount;
+	outmp = outvp->v_mount;
+
 	/*
 	 * If the two vnodes are for the same file system type, call
 	 * VOP_COPY_FILE_RANGE(), otherwise call vn_generic_copy_file_range()
 	 * which can handle copies across multiple file system types.
 	 */
 	*lenp = len;
-	if (invp->v_mount == outvp->v_mount ||
-	    strcmp(invp->v_mount->mnt_vfc->vfc_name,
-	    outvp->v_mount->mnt_vfc->vfc_name) == 0)
+	if (inmp == outmp || strcmp(inmp->mnt_vfc->vfc_name,
+	    outmp->mnt_vfc->vfc_name) == 0)
 		error = VOP_COPY_FILE_RANGE(invp, inoffp, outvp, outoffp,
 		    lenp, flags, incred, outcred, fsize_td);
 	else
