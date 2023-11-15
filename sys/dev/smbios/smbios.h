ba0e4d7971e05ee64281a4fc49a2fb408c8ad816
Andrew Gallatin <gallatin@FreeBSD.org>
2023-11-15 11:11:53 -0500

    smbios: handle smbios3 for arm64
    Get smbios working on arm64 where it seems to be
    exclusively smbios version 3.x
    
    The "interesting" thing here is that the smbios table seems to be
    RAM in the EFI runtime services table. This makes it owned by "ram0",
    and not io memory. That prevents bus_alloc_resource() from being able
    to claim it, since ram0 already owns it. According to jhb, this is
    how things are supposed to work.  Eg, bus_alloc_resource() is meant
    to be used with IO memory, not physical memory.  Following his
    suggestion, I converted the driver to simply use pmap_mapbios().
    
    This is a prerequisite for getting IPMI to attach via the SSIF
    attachment on arm64 servers, where all IPMI that I've seen
    uses SSIF.
    
    Note that this change is based on initial work by Allan Jude in
    https://reviews.freebsd.org/D28739.
    
    Reviewed by: imp
    Sponsored by: Netflix, Ampere Computing LLC (D28739)
    Differential Revision: https://reviews.freebsd.org/D42592

--- sys/dev/smbios/smbios.h
+++ sys/dev/smbios/smbios.h
@@ -38,6 +38,8 @@
 #define	SMBIOS_OFF	0
 #define	SMBIOS_LEN	4
 #define	SMBIOS_SIG	"_SM_"
+#define	SMBIOS3_LEN	5
+#define	SMBIOS3_SIG	"_SM3_"
 
 struct smbios_eps {
 	uint8_t		anchor_string[4];		/* '_SM_' */
@@ -55,6 +57,19 @@ struct smbios_eps {
 	uint16_t	number_structures;
 	uint8_t		BCD_revision;
 } __packed;
+
+struct smbios3_eps {
+        uint8_t		anchor_string[5];                /* '_SM3_' */
+        uint8_t		checksum;
+        uint8_t		length;
+        uint8_t		major_version;
+        uint8_t		minor_version;
+        uint8_t		docrev;
+        uint8_t		entry_point_revision;
+        uint8_t		reserved;
+        uint32_t	structure_table_max_size;
+        uint64_t	structure_table_address;
+};
 
 struct smbios_structure_header {
 	uint8_t		type;
