147f278d7d4b0e9550d6a8e47082251c6ce7d136
Kristof Provost <kp@FreeBSD.org>
2023-11-16 17:06:29 +0100

    pf: always create multihomed states as floating
    When we create a new state for multihomed sctp connections (i.e.
    based on INIT/INIT_ACK or ASCONF parameters) we cannot know what
    interfaces we'll be seeing that traffic on. Make those states floating,
    irrespective of state policy.
    
    MFC after:	1 week
    Sponsored by:	Orange Business Services
    
    (cherry picked from commit 0fe663b2a815dcb41431543940ec51408f418392)

--- sys/netpfil/pf/pf.c
+++ sys/netpfil/pf/pf.c
@@ -5540,8 +5540,12 @@ again:
 			j->pd.sctp_flags |= PFDESC_SCTP_ADD_IP;
 			PF_RULES_RLOCK();
 			sm = NULL;
-			/* XXX: May generated unwanted abort if we try to insert a duplicate state. */
-			ret = pf_test_rule(&r, &sm, pd->dir, kif,
+			/*
+			 * New connections need to be floating, because
+			 * we cannot know what interfaces it will use.
+			 * That's why we pass V_pfi_all rather than kif.
+			 */
+			ret = pf_test_rule(&r, &sm, pd->dir, V_pfi_all,
 			    j->m, off, &j->pd, &ra, &rs, NULL);
 			PF_RULES_RUNLOCK();
 			SDT_PROBE4(pf, sctp, multihome, test, kif, r, j->m, ret);
