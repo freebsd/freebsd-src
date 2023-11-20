92ee2a73bc67d8da158a4342a79f090f096bcd26
Warner Losh <imp@FreeBSD.org>
2023-11-20 15:40:55 -0700

    cdefs: Remove vararg related defines, they are unused.
    Remove __GNUCLIKE_BUILTIN_VARARGS, __GNUCLIKE_BUILTIN_STDARG,
    __GNUCLIKE_BUILTIN_VAALIST, __GNUC_VA_LIST_COMPATIBILITY: they are
    unused. Also remove them from the generated cryptodevh.py script.
    
    PR: 275221 (exp-run)
    Sponsored by:		Netflix

--- sys/sys/cdefs.h
+++ sys/sys/cdefs.h
@@ -74,14 +74,6 @@
 
 #if defined(__GNUC__)
 
-#if (__GNUC_MINOR__ > 95 || __GNUC__ >= 3)
-#define	__GNUCLIKE_BUILTIN_VARARGS 1
-#define	__GNUCLIKE_BUILTIN_STDARG 1
-#define	__GNUCLIKE_BUILTIN_VAALIST 1
-#endif
-
-#define	__GNUC_VA_LIST_COMPATIBILITY 1
-
 /*
  * Compiler memory barriers, specific to gcc and clang.
  */
