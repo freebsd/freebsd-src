80d7e8884ceac5e4b3333815042a9723210376bc
Alexander Motin <mav@FreeBSD.org>
2023-11-22 15:10:57 -0500

    CAM: Remove return value from xpt_path_sbuf()
    It is wrong to call sbuf_len() on third-party sbuf.  If that sbuf
    has a drain function, it ends up in assertion.  But even would it
    work, it would return not newly written length, but the full one.
    Searching through the sources I don't see this value used.

--- sys/cam/cam_xpt.h
+++ sys/cam/cam_xpt.h
@@ -102,9 +102,9 @@ int			xpt_path_comp(struct cam_path *path1,
 				      struct cam_path *path2);
 int			xpt_path_comp_dev(struct cam_path *path,
 					  struct cam_ed *dev);
-int			xpt_path_string(struct cam_path *path, char *str,
+char *			xpt_path_string(struct cam_path *path, char *str,
 					size_t str_len);
-int			xpt_path_sbuf(struct cam_path *path, struct sbuf *sb);
+void			xpt_path_sbuf(struct cam_path *path, struct sbuf *sb);
 path_id_t		xpt_path_path_id(struct cam_path *path);
 target_id_t		xpt_path_target_id(struct cam_path *path);
 lun_id_t		xpt_path_lun_id(struct cam_path *path);
