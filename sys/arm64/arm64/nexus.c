ecf2106c0701e522b8c643f8ea37550b3e58ba57
John Baldwin <jhb@FreeBSD.org>
2023-11-22 09:06:33 -0800

    arm64/amd64/riscv nexus: Use bus_generic_rl_*
    Reviewed by:	mhorne, imp
    Differential Revision:	https://reviews.freebsd.org/D42716

--- sys/arm64/arm64/nexus.c
+++ sys/arm64/arm64/nexus.c
@@ -112,7 +112,6 @@ static bus_deactivate_resource_t nexus_deactivate_resource;
 static bus_get_resource_list_t	nexus_get_reslist;
 static bus_map_resource_t	nexus_map_resource;
 static bus_release_resource_t	nexus_release_resource;
-static bus_set_resource_t	nexus_set_resource;
 
 #ifdef SMP
 static bus_bind_intr_t		nexus_bind_intr;
@@ -141,7 +140,7 @@ static device_method_t nexus_methods[] = {
 	DEVMETHOD(bus_get_resource_list, nexus_get_reslist),
 	DEVMETHOD(bus_map_resource,	nexus_map_resource),
 	DEVMETHOD(bus_release_resource,	nexus_release_resource),
-	DEVMETHOD(bus_set_resource,	nexus_set_resource),
+	DEVMETHOD(bus_set_resource,	bus_generic_rl_set_resource),
 #ifdef SMP
 	DEVMETHOD(bus_bind_intr,	nexus_bind_intr),
 #endif
@@ -433,19 +432,6 @@ nexus_get_reslist(device_t dev, device_t child)
 	struct nexus_device *ndev = DEVTONX(child);
 
 	return (&ndev->nx_resources);
-}
-
-static int
-nexus_set_resource(device_t dev, device_t child, int type, int rid,
-    rman_res_t start, rman_res_t count)
-{
-	struct nexus_device	*ndev = DEVTONX(child);
-	struct resource_list	*rl = &ndev->nx_resources;
-
-	/* XXX this should return a success/failure indicator */
-	resource_list_add(rl, type, rid, start, start + count - 1, count);
-
-	return (0);
 }
 
 static int
