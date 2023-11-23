49b7b73f47feec61c1d86ddf2153077e11f40880
Warner Losh <imp@FreeBSD.org>
2023-11-23 10:21:37 -0700

    stand: Remove ancient SCCS tags.
    Remove ancient SCCS tags from the tree, automated scripting, with two
    minor fixup to keep things compiling. All the common forms in the tree
    were removed with a perl script.
    
    Sponsored by:		Netflix

--- stand/libsa/getopt.c
+++ stand/libsa/getopt.c
@@ -28,9 +28,6 @@
  */
 
 #include <sys/cdefs.h>
-#if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)getopt.c	8.3 (Berkeley) 4/27/95";
-#endif /* LIBC_SCCS and not lint */
 
 #include "stand.h"
 #include <string.h>
--- stand/libsa/strdup.c
+++ stand/libsa/strdup.c
@@ -28,9 +28,6 @@
  */
 
 #include <sys/cdefs.h>
-#if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strdup.c	8.1 (Berkeley) 6/4/93";
-#endif /* LIBC_SCCS and not lint */
 
 #include "stand.h"
 #include <stddef.h>
