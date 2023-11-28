freebsd/freebsd-src

commit 8a962f091bcc416edd466bb5868995660279c737

pst: improve shutdown_post_sync handler
It is desirable to shut down the raid controller even in the face of a
panic. In the SCHEDULER_STOPPED() case, set the interrupt mask bits so
that we request a polled wait, rather than sleep(), from
iop_queue_wait_msg().

Tweak the function name and signature.

Reviewed by:	markj
MFC after:	2 weeks
Sponsored by:	The FreeBSD Foundation
Differential Revision:	https://reviews.freebsd.org/D42337

sys/dev/pst/pst-raid.c
@@ -73,7 +73,7 @@ struct pst_request {
 static disk_strategy_t pststrategy;
 static int pst_probe(device_t);
 static int pst_attach(device_t);
-static int pst_shutdown(device_t);
+static void pst_shutdown_post_sync(device_t, int);
 static void pst_start(struct pst_softc *);
 static void pst_done(struct iop_softc *, u_int32_t, struct i2o_single_reply *);
 static int pst_rw(struct pst_request *);
@@ -170,17 +170,22 @@ pst_attach(device_t dev)
 	   name, psc->info->capacity/(512*255*63), 255, 63,
 	   device_get_nameunit(psc->iop->dev));
 
-    EVENTHANDLER_REGISTER(shutdown_post_sync, pst_shutdown,
+    EVENTHANDLER_REGISTER(shutdown_post_sync, pst_shutdown_post_sync,
 			  dev, SHUTDOWN_PRI_FIRST);
     return 0;
 }
 
-static int
-pst_shutdown(device_t dev)
+static void
+pst_shutdown_post_sync(device_t dev, int howto __unused)
 {
     struct pst_softc *psc = device_get_softc(dev);
     struct i2o_bsa_cache_flush_message *msg;
     int mfa;
+
+    if (SCHEDULER_STOPPED()) {
+	/* Request polled shutdown. */
+	psc->iop->reg->oqueue_intr_mask = 0xffffffff;
+    }
 
     mfa = iop_get_mfa(psc->iop);
     msg = (struct i2o_bsa_cache_flush_message *)(psc->iop->ibase + mfa);
@@ -194,7 +199,6 @@ pst_shutdown(device_t dev)
     msg->control_flags = 0x0; /* 0x80 = post progress reports */
     if (iop_queue_wait_msg(psc->iop, mfa, (struct i2o_basic_message *)msg))
 	printf("pst: shutdown failed!\n");
-    return 0;
 }
 
 static void

sys/dev/pst/pst-iop.c
@@ -432,7 +432,7 @@ iop_queue_wait_msg(struct iop_softc *sc, int mfa, struct i2o_basic_message *msg)
     int status, timeout = 10000;
 
     mtx_lock(&sc->mtx);
-    if (!(sc->reg->oqueue_intr_mask & 0x08)) {
+    if ((sc->reg->oqueue_intr_mask & I2O_OUT_INTR_QUEUE) == 0) {
         msg->transaction_context = (u_int32_t)&request;
         msg->initiator_context = (u_int32_t)iop_done;
         sc->reg->iqueue = mfa;

commit c2ef4a87498da9e10050789ff0dec6546cdbaa8d

pst-raid: De-pessimize the building of i386 kernels
Add include of sys/proc.h

Fixes: 8a962f091bcc

sys/dev/pst/pst-raid.c
@@ -39,6 +39,7 @@
 #include <sys/lock.h>
 #include <sys/malloc.h>
 #include <sys/mutex.h>
+#include <sys/proc.h>
 #include <sys/rman.h>
 
 #include <vm/vm.h>