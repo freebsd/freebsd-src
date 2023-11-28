bb06104dce0b2d471772fefe2ce5b7e651287479
Konstantin Belousov <kib@FreeBSD.org>
2023-11-28 14:51:54 +0200

    EVFILT_SIGNAL: do not use target process pointer on detach
    PR:	275286
    Approved by:	so
    Security:	FreeBSD-EN-24:03.kqueue
    
    (cherry picked from commit ed410b78edc53e17b5a3e93ace2adbeb3a734ae9)
    (cherry picked from commit 24346a2f777598272caffbd7e4fb6d85cafe64ed)

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -4291,9 +4291,7 @@ filt_sigattach(struct knote *kn)
 static void
 filt_sigdetach(struct knote *kn)
 {
-	struct proc *p = kn->kn_ptr.p_proc;
-
-	knlist_remove(p->p_klist, kn, 0);
+	knlist_remove(kn->kn_knlist, kn, 0);
 }
 
 /*
