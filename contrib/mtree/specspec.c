aaafba014a78a9bd24679815aa7f918260bfd406
Jose Luis Duran <jlduran@gmail.com>
2023-11-29 22:21:46 +0000

    mtree: st_nlink on Linux is not int, so add an explicit cast.
    Merging remarks:
    
    We had this problem fixed in 1c324569538099af34419e6818fbfde4779efd33.
    This commit exists purely for metadata changes.
    
    Obtained from:	https://github.com/NetBSD/src/commit/24a21ee034b74f1e01d84d3cebd5ae670db7a844

--- contrib/mtree/specspec.c
+++ contrib/mtree/specspec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: specspec.c,v 1.2 2012/10/05 01:27:29 christos Exp $	*/
+/*	$NetBSD: specspec.c,v 1.3 2015/01/07 20:50:36 joerg Exp $	*/
 
 /*-
  * Copyright (c) 2003 Poul-Henning Kamp
@@ -31,7 +31,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: specspec.c,v 1.2 2012/10/05 01:27:29 christos Exp $");
+__RCSID("$NetBSD: specspec.c,v 1.3 2015/01/07 20:50:36 joerg Exp $");
 
 #include <err.h>
 #include <grp.h>
