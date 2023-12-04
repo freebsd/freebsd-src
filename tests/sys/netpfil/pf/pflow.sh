6bd36d1cf4e98f11661642331fddb5d30dced68d
Kristof Provost <kp@FreeBSD.org>
2023-12-04 16:11:35 +0100

    pf tests: pflow functionality test
    Test that we actually send netflow messages when configured to do so.
    We do not yet inspect the generated netflow messages.
    
    Sponsored by:	Rubicon Communications, LLC ("Netgate")
    Differential Revision:	https://reviews.freebsd.org/D43111

--- tests/sys/netpfil/pf/pflow.sh
+++ tests/sys/netpfil/pf/pflow.sh
@@ -74,7 +74,69 @@ basic_cleanup()
 	pft_cleanup
 }
 
+atf_test_case "state_defaults" "cleanup"
+state_defaults_head()
+{
+	atf_set descr 'Test set state-defaults pflow'
+	atf_set require.user root
+	atf_set require.progs scapy
+}
+
+state_defaults_body()
+{
+	pflow_init
+
+	epair=$(vnet_mkepair)
+	ifconfig ${epair}a 192.0.2.2/24 up
+
+	vnet_mkjail alcatraz ${epair}b
+	jexec alcatraz ifconfig ${epair}b 192.0.2.1/24 up
+
+	# Sanity check
+	atf_check -s exit:0 -o ignore ping -c 1 192.0.2.1
+
+	jexec alcatraz pfctl -e
+	pft_set_rules alcatraz \
+		"pass"
+
+	pflow=$(jexec alcatraz pflowctl -c)
+	jexec alcatraz pflowctl -s ${pflow} dst 192.0.2.2:2055
+
+	# No flow data is generated because no states are marked for it.
+	ping -c 1 192.0.2.1
+	# Flush states to force pflow creation
+	jexec alcatraz pfctl -Fstates
+
+	atf_check -o match:"No data" \
+	    $(atf_get_srcdir)/pft_read_ipfix.py --recvif ${epair}a --port 2055
+
+	# Expect pflow output with state-defaults pflow
+	pft_set_rules alcatraz \
+		"set state-defaults pflow" \
+		"pass"
+
+	ping -c 1 192.0.2.1
+
+	# We default to version 5
+	atf_check -o match:"^v=5.*" \
+	    $(atf_get_srcdir)/pft_read_ipfix.py --recvif ${epair}a --port 2055
+
+	# Switch to version 10
+	jexec alcatraz pflowctl -s ${pflow} proto 10
+
+	ping -c 1 192.0.2.1
+
+	atf_check -o match:"^v=10.*" \
+	    $(atf_get_srcdir)/pft_read_ipfix.py --recvif ${epair}a --port 2055
+}
+
+state_defaults_cleanup()
+{
+	pft_cleanup
+}
+
 atf_init_test_cases()
 {
 	atf_add_test_case "basic"
+	atf_add_test_case "state_defaults"
 }
