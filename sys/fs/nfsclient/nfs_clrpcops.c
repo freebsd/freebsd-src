08143226d007322fe8b463851df07a3e9d80282c
Rick Macklem <rmacklem@FreeBSD.org>
2023-12-03 16:12:14 -0800

    nfscl: Fix comment for commit 6aded1e6b2e5
    Commit 6aded1e6b2e5 fixed a rare case when handling an NFSv4
    Rename reply when delegations are in use.  This patch fixes the
    associated comment.
    
    (cherry picked from commit 0a958aa16fed1978879d64e3b225f1d232cc5a98)

--- sys/fs/nfsclient/nfs_clrpcops.c
+++ sys/fs/nfsclient/nfs_clrpcops.c
@@ -2555,7 +2555,7 @@ tryagain:
 				     * If the Delegreturn failed, try again
 				     * without it. The server will Recall, as
 				     * required.
-				     * If ret > 1, the first iteration of this
+				     * If ret > 1, the second iteration of this
 				     * loop is the second DelegReturn result.
 				     */
 				    mbuf_freem(nd->nd_mrep);
