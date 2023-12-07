c71373ef9247171e82275c5fb20bc53bf48da92e
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2023-12-07 19:45:25 +0100

    Revert "linuxkpi: `GFP_KERNEL` equals `M_NOWAIT` now"
    This change seems to break some drivers such as the mlx5*(4) drivers.
    
    As kib@ says:
    > According to the 'official' Linux kernel documentation, the GFP_KERNEL
    > flag implies sleepable context.
    
    It was introduced while working on the new vt(4)/DRM integration [1].
    During this work, doing sleepable allocations broke vt(4) and the DRM
    drivers. However, I made further improvements and some locking-related
    fixed to the new integration without revisiting the need for it.
    
    After more testing, the improvements to the integration mentionned above
    seems to make the change to `GFP_KERNEL` unneeded now. I can thus
    revert it to restore expectations of other drivers.
    
    This reverts commit 5281f8a9aa2485f955a98b211d79b56000beb22b.
    
    [1] https://github.com/freebsd/drm-kmod/pull/243
    
    Reviewed by:	kib
    Approved by:	kib
    Differential Revision:	https://reviews.freebsd.org/D42962

--- sys/compat/linuxkpi/common/src/linux_shmemfs.c
+++ sys/compat/linuxkpi/common/src/linux_shmemfs.c
@@ -47,15 +47,8 @@ linux_shmem_read_mapping_page_gfp(vm_object_t obj, int pindex, gfp_t gfp)
 	struct page *page;
 	int rv;
 
-	/*
-	 * Historically, GFP_KERNEL was the equivalent of M_WAITOK. But it was
-	 * changed to a synonym of M_NOWAIT to allow allocations in
-	 * non-sleepable code.
-	 *
-	 * However, there was an assertion here to make sure that `gfp` was
-	 * never set to GFP_NOWAIT/M_NOWAIT. Do we need a specific handling of
-	 * M_NOWAIT here?
-	 */
+	if ((gfp & GFP_NOWAIT) != 0)
+		panic("GFP_NOWAIT is unimplemented");
 
 	VM_OBJECT_WLOCK(obj);
 	rv = vm_page_grab_valid(&page, obj, pindex, VM_ALLOC_NORMAL |
