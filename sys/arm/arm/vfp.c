ce2f34ade8b787b068085fa8a8ddd295b06c2737
Mark Johnston <markj@FreeBSD.org>
2023-12-11 09:08:49 -0500

    arm: Disable the VFP during boot
    The VFP code expects the kernel to boot with VFP disabled, but some
    boards will boot with it enabled.  Make sure that vfp_init() disables
    the VFP on each CPU during boot.
    
    PR:		273752
    Reviewed by:	andrew
    Diagnosed by:	Thomas Skibo <thomas-bsd@skibo.net>
    MFC after:	1 week

--- sys/arm/arm/vfp.c
+++ sys/arm/arm/vfp.c
@@ -181,6 +181,8 @@ vfp_init(void)
 				elf_hwcap |= HWCAP_VFPv4;
 		}
 
+		vfp_disable();
+
 		/* initialize the coprocess 10 and 11 calls
 		 * These are called to restore the registers and enable
 		 * the VFP hardware.
