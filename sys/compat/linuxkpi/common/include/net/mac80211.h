d8e197d434cb02cef0b84d4efce981fe52e98198
Bjoern A. Zeeb <bz@FreeBSD.org>
2023-12-12 01:59:17 +0000

    LinuxKPI: 802.11: more TXQ implementation and locking
    Implement ieee80211_handle_wake_tx_queue() and ieee80211_tx_dequeue_ni()
    while looking at the code.  They are needed by various wireless drivers.
    
    Introduce an ltxq lock and protect the skbq by that.
    This prevents panics due to a race between a driver upcall and
    the net80211 tx downcall.  While the former should be rcu protected we
    cannot rely on that.
    It remains questionable if we need to protect further fields there
    (with a different lock?).
    
    Also introduce a txq_mtx on the lhw which needs to be further deployed
    but we need to come up with a good strategy to not end up with 7 different
    locks.
    
    Sponsored by:	The FreeBSD Foundation
    PR:		274178, 275710
    Tested by:	cc
    MFC after:	3 days

--- sys/compat/linuxkpi/common/include/net/mac80211.h
+++ sys/compat/linuxkpi/common/include/net/mac80211.h
@@ -1117,6 +1117,8 @@ void linuxkpi_ieee80211_txq_schedule_start(struct ieee80211_hw *, uint8_t);
 struct ieee80211_txq *linuxkpi_ieee80211_next_txq(struct ieee80211_hw *, uint8_t);
 void linuxkpi_ieee80211_schedule_txq(struct ieee80211_hw *,
     struct ieee80211_txq *, bool);
+void linuxkpi_ieee80211_handle_wake_tx_queue(struct ieee80211_hw *,
+	struct ieee80211_txq *);
 
 /* -------------------------------------------------------------------------- */
 
@@ -1681,7 +1683,7 @@ static inline void
 ieee80211_return_txq(struct ieee80211_hw *hw, struct ieee80211_txq *txq,
     bool withoutpkts)
 {
-	linuxkpi_ieee80211_schedule_txq(hw, txq, true);
+	linuxkpi_ieee80211_schedule_txq(hw, txq, withoutpkts);
 }
 
 static inline void
@@ -1706,7 +1708,7 @@ static inline void
 ieee80211_handle_wake_tx_queue(struct ieee80211_hw *hw,
     struct ieee80211_txq *txq)
 {
-	TODO();
+	linuxkpi_ieee80211_handle_wake_tx_queue(hw, txq);
 }
 
 /* -------------------------------------------------------------------------- */
@@ -2197,11 +2199,23 @@ ieee80211_tkip_add_iv(u8 *crypto_hdr, struct ieee80211_key_conf *keyconf,
 	TODO();
 }
 
-static __inline struct sk_buff *
+static inline struct sk_buff *
 ieee80211_tx_dequeue(struct ieee80211_hw *hw, struct ieee80211_txq *txq)
 {
 
 	return (linuxkpi_ieee80211_tx_dequeue(hw, txq));
+}
+
+static inline struct sk_buff *
+ieee80211_tx_dequeue_ni(struct ieee80211_hw *hw, struct ieee80211_txq *txq)
+{
+	struct sk_buff *skb;
+
+	local_bh_disable();
+	skb = linuxkpi_ieee80211_tx_dequeue(hw, txq);
+	local_bh_enable();
+
+	return (skb);
 }
 
 static __inline void
@@ -2454,13 +2468,6 @@ ieee80211_stop_rx_ba_session_offl(struct ieee80211_vif *vif, uint8_t *addr,
     uint8_t tid)
 {
 	TODO();
-}
-
-static __inline struct sk_buff *
-ieee80211_tx_dequeue_ni(struct ieee80211_hw *hw, struct ieee80211_txq *txq)
-{
-	TODO();
-	return (NULL);
 }
 
 static __inline void
