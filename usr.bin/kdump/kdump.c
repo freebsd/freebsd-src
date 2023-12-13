ee0e6ac14776ca4c131d2178e790a3c4d85c682f
Kyle Evans <kevans@FreeBSD.org>
2023-12-13 10:49:03 -0600

    kdump: use print_mask_arg0 appropriately
    Some callers are using print_mask_arg() when they should be using
    print_mask_arg0(); the latter should be used when all flags are optional
    and there's not a flag to be decoded with a 0-mask.  This turns:
    
    nmount(0x6991e009000,0x8,0<><invalid>0)
    
    into:
    
    nmount(0x6991e009000,0x8,0)
    
    Reviewed by:	jhb
    Differential Revision:	https://reviews.freebsd.org/D43007
    
    (cherry picked from commit a206524709bdfbd577bcfe76676da80b8250f84c)

--- usr.bin/kdump/kdump.c
+++ usr.bin/kdump/kdump.c
@@ -880,7 +880,8 @@ ktrsyscall_freebsd(struct ktr_syscall *ktr, register_t **resip,
 				print_number(ip, narg, c);
 				print_number(ip, narg, c);
 				putchar(',');
-				print_mask_arg(sysdecode_close_range_flags, *ip);
+				print_mask_arg0(sysdecode_close_range_flags,
+				    *ip);
 				ip += 3;
 				narg -= 3;
 				break;
@@ -946,14 +947,14 @@ ktrsyscall_freebsd(struct ktr_syscall *ktr, register_t **resip,
 				print_number(ip, narg, c);
 				print_number(ip, narg, c);
 				putchar(',');
-				print_mask_arg(sysdecode_mount_flags, *ip);
+				print_mask_arg0(sysdecode_mount_flags, *ip);
 				ip++;
 				narg--;
 				break;
 			case SYS_unmount:
 				print_number(ip, narg, c);
 				putchar(',');
-				print_mask_arg(sysdecode_mount_flags, *ip);
+				print_mask_arg0(sysdecode_mount_flags, *ip);
 				ip++;
 				narg--;
 				break;
@@ -1411,7 +1412,7 @@ ktrsyscall_freebsd(struct ktr_syscall *ktr, register_t **resip,
 				print_number(ip, narg, c);
 				print_number(ip, narg, c);
 				putchar(',');
-				print_mask_arg(sysdecode_mount_flags, *ip);
+				print_mask_arg0(sysdecode_mount_flags, *ip);
 				ip++;
 				narg--;
 				break;
