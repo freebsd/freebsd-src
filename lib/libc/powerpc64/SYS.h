3112a7a09d31ce2ab78eacd13ee42ea66a993d21
Brooks Davis <brooks@FreeBSD.org>
2023-12-21 21:36:12 +0000

    Revert "power*/SYS.h: implement _SYSCALL_BODY() macro"
    bapt reports build errors on powerpc/powerpc so this is the most likely
    culprit.
    
    This reverts commit b34367116e280c816e3e5d6b2ee10f701547db2e.

--- lib/libc/powerpc64/SYS.h
+++ lib/libc/powerpc64/SYS.h
@@ -40,7 +40,11 @@
 	li	0,(SYS_##name);					\
 	sc
 
-#define _SYSCALL_BODY(name)					\
+#define	PSEUDO(name)						\
+	.text;							\
+	.align 2;						\
+ENTRY(__sys_##name);						\
+	WEAK_REFERENCE(__sys_##name, _##name);			\
 	_SYSCALL(name);						\
 	bnslr;							\
 	mflr	%r0;						\
@@ -51,14 +55,7 @@
 	addi	%r1,%r1,48;					\
 	ld	%r0,16(%r1);					\
 	mtlr	%r0;						\
-	blr
-
-#define	PSEUDO(name)						\
-	.text;							\
-	.align 2;						\
-ENTRY(__sys_##name);						\
-	WEAK_REFERENCE(__sys_##name, _##name);			\
-	_SYSCALL_BODY(name);					\
+	blr;							\
 END(__sys_##name)
 
 #define	RSYSCALL(name)						\
@@ -67,5 +64,16 @@ END(__sys_##name)
 ENTRY(__sys_##name);						\
 	WEAK_REFERENCE(__sys_##name, name);			\
 	WEAK_REFERENCE(__sys_##name, _##name);			\
-	_SYSCALL_BODY(name);					\
+	_SYSCALL(name);						\
+	bnslr;							\
+								\
+	mflr	%r0;						\
+	std	%r0,16(%r1);					\
+	stdu	%r1,-48(%r1);					\
+	bl	CNAME(HIDENAME(cerror));			\
+	nop;							\
+	addi	%r1,%r1,48;					\
+	ld	%r0,16(%r1);					\
+	mtlr	%r0;						\
+	blr;							\
 END(__sys_##name)
