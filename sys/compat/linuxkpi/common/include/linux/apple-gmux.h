730387b64e10b617f6f0b6f3a64501dc35dd7861
Vladimir Kondratyev <wulf@FreeBSD.org>
2023-12-24 11:20:00 +0300

    LinuxKPI: Add linux/apple-gmux.h
    Sponsored by:	Serenity Cyber Security, LLC
    Obtained from:	OpenBSD
    Reviewed by:	manu, bz
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D42819

---
+++ sys/compat/linuxkpi/common/include/linux/apple-gmux.h
@@ -0,0 +1,12 @@
+/* Public domain. */
+
+#ifndef _LINUXKPI_LINUX_APPLE_GMUX_H
+#define _LINUXKPI_LINUX_APPLE_GMUX_H
+
+static inline bool
+apple_gmux_detect(void *a, void *b)
+{
+	return false;
+}
+
+#endif
