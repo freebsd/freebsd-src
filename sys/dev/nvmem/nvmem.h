0e150bb9b38335600c0c24c877e922c99d79b107
Emmanuel Vadot <manu@FreeBSD.org>
2023-12-26 18:51:22 +0100

    nvmem: Move nvmem code in dev/nvmem
    We've removed kernel option EXT_RESOURCES almost two years ago.
    While it was ok to have some code under a common 'extres' subdirectory
    at first, we now have a lot of consumer of it and we made it mandatory
    so no need to have it under a cryptic name.
    
    Reviewed by:	dab, imp
    Sponsored by:   Beckhoff Automation GmbH & Co. KG
    Differential Revision:	https://reviews.freebsd.org/D43193

--- sys/dev/nvmem/nvmem.h
+++ sys/dev/nvmem/nvmem.h
@@ -23,8 +23,8 @@
  * SUCH DAMAGE.
  */
 
-#ifndef _DEV_EXTRES_NVMEM_H_
-#define _DEV_EXTRES_NVMEM_H_
+#ifndef _DEV_NVMEM_H_
+#define	_DEV_NVMEM_H_
 
 int nvmem_get_cell_len(phandle_t node, const char *name);
 int nvmem_read_cell_by_name(phandle_t node, const char *name, void *cell, size_t buflen);
@@ -32,4 +32,4 @@ int nvmem_read_cell_by_idx(phandle_t node, int idx, void *cell, size_t buflen);
 int nvmem_write_cell_by_name(phandle_t node, const char *name, void *cell, size_t buflen);
 int nvmem_write_cell_by_idx(phandle_t node, int idx, void *cell, size_t buflen);
 
-#endif /* _DEV_EXTRES_NVMEM_H_ */
+#endif /* _DEV_NVMEM_H_ */
