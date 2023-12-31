dc024acd51740f794d09fc478be6a4f046766ba4
Konstantin Belousov <kib@FreeBSD.org>
2023-12-31 22:46:04 +0200

    vtruncbuf: add a comment explaining the purpose of the loop
    (cherry picked from commit 4c41d10f1538c232174d5200d245e5fcd9936958)

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -2558,6 +2558,9 @@ restart_unlocked:
 		;
 
 	if (length > 0) {
+		/*
+		 * Write out vnode metadata, e.g. indirect blocks.
+		 */
 restartsync:
 		TAILQ_FOREACH_SAFE(bp, &bo->bo_dirty.bv_hd, b_bobufs, nbp) {
 			if (bp->b_lblkno > 0)
