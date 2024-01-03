a8cd14ae762012001ad24197c1939c87b617e221
Ricardo Branco <rbranco@suse.de>
2024-01-03 21:17:58 +0100

    hexdump: Do not trust st_size if it equals zero

--- usr.bin/hexdump/display.c
+++ usr.bin/hexdump/display.c
@@ -397,13 +397,14 @@ doskip(const char *fname, int statok)
 	if (statok) {
 		if (fstat(fileno(stdin), &sb))
 			err(1, "%s", fname);
-		if (S_ISREG(sb.st_mode) && skip > sb.st_size) {
+		if (S_ISREG(sb.st_mode) && skip > sb.st_size && sb.st_size > 0) {
 			address += sb.st_size;
 			skip -= sb.st_size;
 			return;
 		}
 	}
-	if (!statok || S_ISFIFO(sb.st_mode) || S_ISSOCK(sb.st_mode)) {
+	if (!statok || S_ISFIFO(sb.st_mode) || S_ISSOCK(sb.st_mode) || \
+	    (S_ISREG(sb.st_mode) && sb.st_size == 0)) {
 		noseek();
 		return;
 	}
