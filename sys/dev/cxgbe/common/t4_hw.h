729b9f63a9283536f7cd4f65deb3b45902cc032e
Navdeep Parhar <np@FreeBSD.org>
2024-01-09 13:22:16 -0800

    cxgbe(4): Use the correct size for the CIM LA on the T6.
    The logic analyzer in the T6 CIM block has a different capture size than
    previous chips.
    
    MFC after:	1 week
    Sponsored by:	Chelsio Communications

--- sys/dev/cxgbe/common/t4_hw.h
+++ sys/dev/cxgbe/common/t4_hw.h
@@ -66,7 +66,8 @@ enum {
 	CIM_NUM_IBQ    = 6,     /* # of CIM IBQs */
 	CIM_NUM_OBQ    = 6,     /* # of CIM OBQs */
 	CIM_NUM_OBQ_T5 = 8,     /* # of CIM OBQs for T5 adapter */
-	CIMLA_SIZE     = 2048,  /* # of 32-bit words in CIM LA */
+	CIMLA_SIZE     = 256 * 8,  /* 256 rows * ceil(235/32) 32-bit words */
+	CIMLA_SIZE_T6  = 256 * 10, /* 256 rows * ceil(311/32) 32-bit words */
 	CIM_PIFLA_SIZE = 64,    /* # of 192-bit words in CIM PIF LA */
 	CIM_MALA_SIZE  = 64,    /* # of 160-bit words in CIM MA LA */
 	CIM_IBQ_SIZE   = 128,   /* # of 128-bit words in a CIM IBQ */
