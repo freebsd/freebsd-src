7474b39bc7717ee33a3a384db96674b9cb497d87
Val Packett <val@packett.cool>
2024-01-16 15:36:41 -0300

    cross-build: define __intX_t aliases on Linux/musl
    They are completely absent there.
    
    Sponsored by:		https://www.patreon.com/valpackett

--- tools/build/cross-build/include/linux/sys/types.h
+++ tools/build/cross-build/include/linux/sys/types.h
@@ -40,6 +40,18 @@
  */
 #include <sys/cdefs.h>
 
+#ifndef __GLIBC__
+#include <stdint.h>
+typedef	int8_t	__int8_t;
+typedef	int16_t	__int16_t;
+typedef	int32_t	__int32_t;
+typedef	int64_t	__int64_t;
+typedef	uint8_t	__uint8_t;
+typedef	uint16_t	__uint16_t;
+typedef	uint32_t	__uint32_t;
+typedef	uint64_t	__uint64_t;
+#endif
+
 #include_next <sys/types.h>
 
 #ifndef __size_t
