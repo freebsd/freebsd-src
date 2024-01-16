freebsd/freebsd-src

commit a087c7a082a8c85121804ba03957803102ae8083

xen: remove xen_domain_type enum/variable
The vm_guest variable readily covers all uses of xen_domain_type, so
merge them together.  Since support for PV domains has been removed
hard-core xen_pv_domain() to return false.

Reviewed by: royger

sys/x86/xen/hvm.c
@@ -345,15 +345,13 @@ xen_hvm_init(enum xen_hvm_init_type init_type)
 			return;
 
 		/*
-		 * If xen_domain_type is not set at this point
+		 * If the Xen domain type is not set at this point
 		 * it means we are inside a (PV)HVM guest, because
 		 * for PVH the guest type is set much earlier
 		 * (see hammer_time_xen).
 		 */
-		if (!xen_domain()) {
-			xen_domain_type = XEN_HVM_DOMAIN;
+		if (!xen_domain())
 			vm_guest = VM_GUEST_XEN;
-		}
 
 		setup_xen_features();
 #ifdef SMP

sys/x86/xen/pv.c
@@ -165,7 +165,6 @@ hammer_time_xen(vm_paddr_t start_info_paddr)
 	int rc;
 
 	if (isxen()) {
-		xen_domain_type = XEN_HVM_DOMAIN;
 		vm_guest = VM_GUEST_XEN;
 		rc = xen_hvm_init_hypercall_stubs(XEN_HVM_INIT_EARLY);
 		if (rc) {

sys/xen/xen-os.h
@@ -79,30 +79,22 @@ extern shared_info_t *HYPERVISOR_shared_info;
 
 extern bool xen_suspend_cancelled;
 
-enum xen_domain_type {
-	XEN_NATIVE,             /* running on bare hardware    */
-	XEN_PV_DOMAIN,          /* running in a PV domain      */
-	XEN_HVM_DOMAIN,         /* running in a Xen hvm domain */
-};
-
-extern enum xen_domain_type xen_domain_type;
-
-static inline int
+static inline bool
 xen_domain(void)
 {
-	return (xen_domain_type != XEN_NATIVE);
+	return (vm_guest == VM_GUEST_XEN);
 }
 
-static inline int
+static inline bool
 xen_pv_domain(void)
 {
-	return (xen_domain_type == XEN_PV_DOMAIN);
+	return (false);
 }
 
-static inline int
+static inline bool
 xen_hvm_domain(void)
 {
-	return (xen_domain_type == XEN_HVM_DOMAIN);
+	return (vm_guest == VM_GUEST_XEN);
 }
 
 static inline bool

sys/xen/xen_common.c
@@ -41,8 +41,6 @@
 #include <contrib/xen/vcpu.h>
 
 /*-------------------------------- Global Data -------------------------------*/
-enum xen_domain_type xen_domain_type = XEN_NATIVE;
-
 /**
  * Start info flags. ATM this only used to store the initial domain flag for
  * PVHv2, and it's always empty for HVM guests.

commit 1afbb3edba8a935d9171f320fb9e39fb27e2a504

x86/xen: fix HVM guest hypercall page setup
a087c7a082a8 didn't take into account that vm_guest will also get setup by
generic identify CPU code, and hence by the time xen_hvm_init() gets called
vm_guest will always be set if running as a Xen guest, either by the PVH entry
point code, or by generic CPU identification.

xen_hvm_init() and xen_hvm_init_hypercall_stubs() were relying on xen_domain()
returning false when running as an HVM guest, and used that into order to
figure out whether hypercall page needed to be populated.

Get rid of such assumptions and simplify the code since legacy PVH is no
longer supported.

This fixes booting FreeBSD as a Xen HVM guest.

Fixes: a087c7a082a8 ('xen: remove xen_domain_type enum/variable')
Sponsored by: Cloud Software Group

sys/x86/xen/hvm.c
@@ -155,24 +155,13 @@ xen_hvm_init_hypercall_stubs(enum xen_hvm_init_type init_type)
 {
 	uint32_t regs[4];
 
-	/* Legacy PVH will get here without the cpuid leaf being set. */
-	if (xen_cpuid_base == 0)
-		xen_cpuid_base = xen_hvm_cpuid_base();
+	if (xen_cpuid_base != 0)
+		/* Already setup. */
+		goto out;
+
+	xen_cpuid_base = xen_hvm_cpuid_base();
 	if (xen_cpuid_base == 0)
 		return (ENXIO);
-
-	if (xen_domain() && init_type == XEN_HVM_INIT_LATE) {
-		/*
-		 * If the domain type is already set we can assume that the
-		 * hypercall page has been populated too, so just print the
-		 * version (and apply any quirks) and exit.
-		 */
-		hypervisor_version();
-		return 0;
-	}
-
-	if (init_type == XEN_HVM_INIT_LATE)
-		hypervisor_version();
 
 	/*
 	 * Find the hypercall pages.
@@ -185,6 +174,8 @@ xen_hvm_init_hypercall_stubs(enum xen_hvm_init_type init_type)
 	    ? (vm_paddr_t)((uintptr_t)&hypercall_page - KERNBASE)
 	    : vtophys(&hypercall_page));
 
+out:
+	hypervisor_version();
 	return (0);
 }
 
@@ -334,7 +325,8 @@ xen_hvm_init(enum xen_hvm_init_type init_type)
 	int error;
 	int i;
 
-	if (init_type == XEN_HVM_INIT_CANCELLED_SUSPEND)
+	if (!xen_domain() ||
+	    init_type == XEN_HVM_INIT_CANCELLED_SUSPEND)
 		return;
 
 	error = xen_hvm_init_hypercall_stubs(init_type);
@@ -343,15 +335,6 @@ xen_hvm_init(enum xen_hvm_init_type init_type)
 	case XEN_HVM_INIT_LATE:
 		if (error != 0)
 			return;
-
-		/*
-		 * If the Xen domain type is not set at this point
-		 * it means we are inside a (PV)HVM guest, because
-		 * for PVH the guest type is set much earlier
-		 * (see hammer_time_xen).
-		 */
-		if (!xen_domain())
-			vm_guest = VM_GUEST_XEN;
 
 		setup_xen_features();
 #ifdef SMP