228d8c9efe1e9a2946a5e64ff33362a54b9c3b5d
Brooks Davis <brooks@FreeBSD.org>
2024-01-16 22:51:40 +0000

    libc: split libc and syscall interposing (2/2)
    Move the __libc_interposing implementation to libc/gen so it doesn't end
    up in libsys.
    
    Reviewed by:	kib, emaste, imp
    Pull Request:	https://github.com/freebsd/freebsd-src/pull/908

--- lib/libc/gen/libc_interposing_table.c
+++ lib/libc/gen/libc_interposing_table.c
@@ -30,53 +30,27 @@
  */
 
 #include <sys/types.h>
+#include <stddef.h>
 #include "libc_private.h"
 
 #define	SLOT(a, b) \
 	[INTERPOS_##a] = (interpos_func_t)b
-interpos_func_t __libsys_interposing[INTERPOS_MAX] = {
-	SLOT(accept, __sys_accept),
-	SLOT(accept4, __sys_accept4),
-	SLOT(aio_suspend, __sys_aio_suspend),
-	SLOT(close, __sys_close),
-	SLOT(connect, __sys_connect),
-	SLOT(fcntl, __sys_fcntl),
-	SLOT(fsync, __sys_fsync),
-	SLOT(fork, __sys_fork),
-	SLOT(msync, __sys_msync),
-	SLOT(nanosleep, __sys_nanosleep),
-	SLOT(openat, __sys_openat),
-	SLOT(poll, __sys_poll),
-	SLOT(pselect, __sys_pselect),
-	SLOT(read, __sys_read),
-	SLOT(readv, __sys_readv),
-	SLOT(recvfrom, __sys_recvfrom),
-	SLOT(recvmsg, __sys_recvmsg),
-	SLOT(select, __sys_select),
-	SLOT(sendmsg, __sys_sendmsg),
-	SLOT(sendto, __sys_sendto),
-	SLOT(setcontext, __sys_setcontext),
-	SLOT(sigaction, __sys_sigaction),
-	SLOT(sigprocmask, __sys_sigprocmask),
-	SLOT(sigsuspend, __sys_sigsuspend),
-	SLOT(sigwait, __libc_sigwait),
-	SLOT(sigtimedwait, __sys_sigtimedwait),
-	SLOT(sigwaitinfo, __sys_sigwaitinfo),
-	SLOT(swapcontext, __sys_swapcontext),
-	SLOT(wait4, __sys_wait4),
-	SLOT(write, __sys_write),
-	SLOT(writev, __sys_writev),
-	SLOT(kevent, __sys_kevent),
-	SLOT(wait6, __sys_wait6),
-	SLOT(ppoll, __sys_ppoll),
-	SLOT(fdatasync, __sys_fdatasync),
-	SLOT(clock_nanosleep, __sys_clock_nanosleep),
-	SLOT(pdfork, __sys_pdfork),
+interpos_func_t __libc_interposing[INTERPOS_MAX] = {
+	SLOT(system, __libc_system),
+	SLOT(tcdrain, __libc_tcdrain),
+	SLOT(_pthread_mutex_init_calloc_cb, _pthread_mutex_init_calloc_cb_stub),
+	SLOT(spinlock, __libc_spinlock_stub),
+	SLOT(spinunlock, __libc_spinunlock_stub),
+	SLOT(map_stacks_exec, __libc_map_stacks_exec),
+	SLOT(distribute_static_tls, __libc_distribute_static_tls),
 };
 #undef SLOT
 
 interpos_func_t *
-__libsys_interposing_slot(int interposno)
+__libc_interposing_slot(int interposno)
 {
-	return (&__libsys_interposing[interposno]);
+	/* XXX: forward compat. Remove after 15.0-RELEASE. */
+	if (__libc_interposing[interposno] == NULL)
+		return (__libsys_interposing_slot(interposno));
+	return (&__libc_interposing[interposno]);
 }
