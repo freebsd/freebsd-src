12d6a032dff10fad72f7d3f1bb5cb4c53b9c4284
Olivier Certner <olce@FreeBSD.org>
2024-01-18 11:22:28 +0100

    Annotate 'rebooting' with __read_mostly
    While here, put such annotation after the variable for 'dumping', since
    it concerns the variable and not the type.
    
    Reviewed by:            markj
    Approved by:            markj (mentor)
    MFC after:              2 weeks
    Sponsored by:           The FreeBSD Foundation
    Differential Revision:  https://reviews.freebsd.org/D43570

--- sys/kern/kern_shutdown.c
+++ sys/kern/kern_shutdown.c
@@ -226,8 +226,8 @@ SYSCTL_INT(_kern, OID_AUTO, kerneldump_gzlevel, CTLFLAG_RWTUN,
  */
 const char *panicstr __read_mostly;
 
-int __read_mostly dumping;		/* system is dumping */
-int rebooting;				/* system is rebooting */
+int dumping __read_mostly;		/* system is dumping */
+int rebooting __read_mostly;		/* system is rebooting */
 /*
  * Used to serialize between sysctl kern.shutdown.dumpdevname and list
  * modifications via ioctl.
