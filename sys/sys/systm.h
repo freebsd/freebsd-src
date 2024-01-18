a6d066d0120f0b83efca4e618050557a91259a30
Olivier Certner <olce@FreeBSD.org>
2024-01-18 11:31:59 +0100

    SCHEDULER_STOPPED(): Move it (back) to 'systm.h'
    It's not an assertion, so doesn't logically belong to 'kassert.h'.
    Moreover, a subsequent commit will make it rely on a variable whose
    declaration also belongs to 'systm.h'.
    
    Approved by:            markj (mentor)
    MFC after:              2 weeks
    Sponsored by:           The FreeBSD Foundation
    Differential Revision:  https://reviews.freebsd.org/D43571
    
    (cherry picked from commit cd0c52e50bc17f4c3fab998f9f33b1d3ada6c90e)
    
    Approved by:            markj (mentor)

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -101,6 +101,18 @@ struct ucred;
 #include <sys/pcpu.h>		/* curthread */
 #include <sys/kpilite.h>
 
+/*
+ * If we have already panic'd and this is the thread that called
+ * panic(), then don't block on any mutexes but silently succeed.
+ * Otherwise, the kernel will deadlock since the scheduler isn't
+ * going to run the thread that holds any lock we need.
+ */
+#define	SCHEDULER_STOPPED_TD(td)  ({					\
+	MPASS((td) == curthread);					\
+	__predict_false((td)->td_stopsched);				\
+})
+#define	SCHEDULER_STOPPED() SCHEDULER_STOPPED_TD(curthread)
+
 extern int osreldate;
 
 extern const void *zero_region;	/* address space maps to a zeroed page	*/
