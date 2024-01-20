b146a9c34084787ab16a12ab6d2b4c10df25a6d9
Gordon Bergling <gbe@FreeBSD.org>
2024-01-20 20:10:21 +0100

    bce(4): Fix a typo in a kernel message
    - s/adddress/address/
    
    (cherry picked from commit e1c0d368d630cce01691e1967ada9f36d6b8c751)

--- sys/dev/bce/if_bce.c
+++ sys/dev/bce/if_bce.c
@@ -10663,7 +10663,7 @@ bce_dump_driver_state(struct bce_softc *sc)
 	val_hi = BCE_ADDR_HI(sc->tx_bd_chain);
 	val_lo = BCE_ADDR_LO(sc->tx_bd_chain);
 	BCE_PRINTF("0x%08X:%08X - (sc->tx_bd_chain) tx_bd chain "
-	    "virtual adddress\n", val_hi, val_lo);
+	    "virtual address\n", val_hi, val_lo);
 
 	val_hi = BCE_ADDR_HI(sc->rx_bd_chain);
 	val_lo = BCE_ADDR_LO(sc->rx_bd_chain);
