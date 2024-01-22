58d3171698341c664d7c676541b86385a924ae93
Konstantin Belousov <kib@FreeBSD.org>
2024-01-23 00:31:55 +0200

    Add fget_remote()
    The function holds and returns struct file for a file descriptor index
    in the given process.
    
    Reviewed by:	brooks, markj
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D43518

--- sys/sys/file.h
+++ sys/sys/file.h
@@ -261,6 +261,7 @@ int fget_write(struct thread *td, int fd, cap_rights_t *rightsp,
 int fget_fcntl(struct thread *td, int fd, cap_rights_t *rightsp,
     int needfcntl, struct file **fpp);
 int _fdrop(struct file *fp, struct thread *td);
+int fget_remote(struct thread *td, struct proc *p, int fd, struct file **fpp);
 
 fo_rdwr_t	invfo_rdwr;
 fo_truncate_t	invfo_truncate;
