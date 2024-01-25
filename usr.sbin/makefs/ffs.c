0459337491cdfdd752507c0d2722999a6a9781ef
Ed Maste <emaste@FreeBSD.org>
2024-01-24 20:47:36 -0500

    makefs: warn that ffs sectorsize other than 512 may not work
    newfs always sets sectorsize to DEV_BSIZE (512) and derives some other
    values based on the number of 512-byte sectors per real sector.  Similar
    logic is required in makefs.  Until that happens, emit a warning that
    the image may be incorrect.
    
    PR:		276571
    (cherry picked from commit 176b9e0d724b4c1a24d2c46a1256381784f04edc)
    (cherry picked from commit 8e402178193f7b273a956b864d8d8b288ce62ee7)

--- usr.sbin/makefs/ffs.c
+++ usr.sbin/makefs/ffs.c
@@ -340,6 +340,9 @@ ffs_validate(const char *dir, fsnode *root, fsinfo_t *fsopts)
 		/* set FFS defaults */
 	if (fsopts->sectorsize == -1)
 		fsopts->sectorsize = DFL_SECSIZE;
+	if (fsopts->sectorsize != DFL_SECSIZE)
+		warnx("sectorsize %d may produce nonfunctional image",
+		    fsopts->sectorsize);
 	if (ffs_opts->fsize == -1)
 		ffs_opts->fsize = MAX(DFL_FRAGSIZE, fsopts->sectorsize);
 	if (ffs_opts->bsize == -1)
