b0df33b1022077dbf60c367e159f38c03073a983
Richard Scheffenegger <rscheff@FreeBSD.org>
2024-01-25 23:21:07 +0100

    tcp: AccECN access ACE field by shifting bits
    Shifting bits is quicker than checking header flag bits
    one by one. Also improve readability by the use of switch
    statements.
    
    No change in behaviour.
    
    Reviewed By:           glebius, tuexen, #transport
    Sponsored by:          NetApp, Inc.
    Differential Revision: https://reviews.freebsd.org/D43560

--- sys/netinet/tcp_ecn.h
+++ sys/netinet/tcp_ecn.h
@@ -38,6 +38,8 @@
 #include <netinet/tcp_var.h>
 #include <netinet/tcp_syncache.h>
 
+#define TH_ACE_SHIFT 6
+
 void	 tcp_ecn_input_syn_sent(struct tcpcb *, uint16_t, int);
 void	 tcp_ecn_input_parallel_syn(struct tcpcb *, uint16_t, int);
 int	 tcp_ecn_input_segment(struct tcpcb *, uint16_t, int, int, int);
@@ -46,7 +48,6 @@ int	 tcp_ecn_output_established(struct tcpcb *, uint16_t *, int, bool);
 void	 tcp_ecn_syncache_socket(struct tcpcb *, struct syncache *);
 int	 tcp_ecn_syncache_add(uint16_t, int);
 uint16_t tcp_ecn_syncache_respond(uint16_t, struct syncache *);
-int	 tcp_ecn_get_ace(uint16_t);
 
 #endif /* _KERNEL */
 
