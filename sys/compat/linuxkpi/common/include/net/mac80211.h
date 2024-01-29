b42f8328250ad257cb43a9407245f22b1e2936cc
Bjoern A. Zeeb <bz@FreeBSD.org>
2024-01-29 14:21:17 +0000

    LinuxKPI: 802.11: extend ieee80211_key_seq and WLAN_KEY_LEN_* defines
    For iwlwifi(4) d3.c we need another struct in struct ieee80211_key_seq
    and related WLAN_KEY_LEN_* defines in.
    
    Reviewed by:	cc, emaste
    Differential Revision: https://reviews.freebsd.org/D43646
    
    (cherry picked from commit 2f029bf911e29dc46c32ea142daba4f26a2078f4)

--- sys/compat/linuxkpi/common/include/net/mac80211.h
+++ sys/compat/linuxkpi/common/include/net/mac80211.h
@@ -529,6 +529,9 @@ struct ieee80211_key_seq {
 			uint8_t		pn[IEEE80211_CCMP_PN_LEN];
 		} aes_cmac;
 		struct {
+			uint8_t		pn[IEEE80211_CCMP_PN_LEN];
+		} aes_gmac;
+		struct {
 			uint32_t	iv32;
 			uint16_t	iv16;
 		} tkip;
