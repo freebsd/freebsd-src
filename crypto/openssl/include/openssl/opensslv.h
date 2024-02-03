74fe298c8299fdb8c8f761728ddd245b0c3fe04a
Cy Schubert <cy@FreeBSD.org>
2024-02-02 16:34:36 -0800

    OpenSSL: Update version strings
    Reported by:	"Herbert J. Skuhra" <herbert@gojira.at>
    Fixes:		9eb4e0b42d7c
    MFC after:	3 days

--- crypto/openssl/include/openssl/opensslv.h
+++ crypto/openssl/include/openssl/opensslv.h
@@ -29,7 +29,7 @@ extern "C" {
  */
 # define OPENSSL_VERSION_MAJOR  3
 # define OPENSSL_VERSION_MINOR  0
-# define OPENSSL_VERSION_PATCH  12
+# define OPENSSL_VERSION_PATCH  13
 
 /*
  * Additional version information
@@ -74,8 +74,8 @@ extern "C" {
  * longer variant with OPENSSL_VERSION_PRE_RELEASE_STR and
  * OPENSSL_VERSION_BUILD_METADATA_STR appended.
  */
-# define OPENSSL_VERSION_STR "3.0.12"
-# define OPENSSL_FULL_VERSION_STR "3.0.12"
+# define OPENSSL_VERSION_STR "3.0.13"
+# define OPENSSL_FULL_VERSION_STR "3.0.13"
 
 /*
  * SECTION 3: ADDITIONAL METADATA
@@ -88,7 +88,7 @@ extern "C" {
  * SECTION 4: BACKWARD COMPATIBILITY
  */
 
-# define OPENSSL_VERSION_TEXT "OpenSSL 3.0.12 24 Oct 2023"
+# define OPENSSL_VERSION_TEXT "OpenSSL 3.0.13 24 Oct 2023"
 
 /* Synthesize OPENSSL_VERSION_NUMBER with the layout 0xMNN00PPSL */
 # ifdef OPENSSL_VERSION_PRE_RELEASE
