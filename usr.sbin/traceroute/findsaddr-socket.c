6c7f2a61dcadf92c35fea2091da5e81230a42e42
Lexi Winter <lexi@le-Fay.ORG>
2024-02-03 13:19:03 +0000

    traceroute: remove configuration #defines
    traceroute used a series of #defines to specify what features are
    available on the host platform.  As traceroute is now in source, these
    are unnecessary and complicate the code, so remove them.
    
    Reviewed by: imp
    Pull Request: https://github.com/freebsd/freebsd-src/pull/1100

--- usr.sbin/traceroute/findsaddr-socket.c
+++ usr.sbin/traceroute/findsaddr-socket.c
@@ -42,9 +42,7 @@ static const char rcsid[] =
 #include <sys/file.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
-#ifdef HAVE_SYS_SOCKIO_H
 #include <sys/sockio.h>
-#endif
 #include <sys/time.h>				/* concession to AIX */
 
 #if __STDC__
@@ -64,17 +62,11 @@ struct rtentry;
 #include <unistd.h>
 
 #include "gnuc.h"
-#ifdef HAVE_OS_PROTO_H
 #include "os-proto.h"
-#endif
 
 #include "findsaddr.h"
 
-#ifdef HAVE_SOCKADDR_SA_LEN
 #define SALEN(sa) ((sa)->sa_len)
-#else
-#define SALEN(sa) salen(sa)
-#endif
 
 #ifndef roundup
 #define roundup(x, y)   ((((x)+((y)-1))/(y))*(y))  /* to any y */
@@ -91,10 +83,6 @@ static struct rtmsg rtmsg = {
 	RTA_DST | RTA_IFA, 0, 0, 0, 0, 0, { 0 } },
 	{ 0 }
 };
-
-#ifndef HAVE_SOCKADDR_SA_LEN
-static int salen(struct sockaddr *);
-#endif
 
 /*
  * Return the source address for the given destination address
@@ -199,21 +187,3 @@ findsaddr(register const struct sockaddr_in *to,
 
 	return ("failed!");
 }
-
-#ifndef HAVE_SOCKADDR_SA_LEN
-static int
-salen(struct sockaddr *sa)
-{
-	switch (sa->sa_family) {
-
-	case AF_INET:
-		return (sizeof(struct sockaddr_in));
-
-	case AF_LINK:
-		return (sizeof(struct sockaddr_dl));
-
-	default:
-		return (sizeof(struct sockaddr));
-	}
-}
-#endif
