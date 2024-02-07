327edc36f0c08688bcb8e07104aeff2429ad88fc
Brooks Davis <brooks@FreeBSD.org>
2024-02-07 19:37:55 +0000

    libc: don't directly link libsys
    It is sufficent to add it as a filter.
    
    Reported by:	kib
    Reviewed by:	kib
    Differential Revision:	https://reviews.freebsd.org/D43781

--- share/mk/src.libnames.mk
+++ share/mk/src.libnames.mk
@@ -393,7 +393,7 @@ _DP_xo=		util
 _DP_ztest=	geom m nvpair umem zpool pthread avl zfs_core spl zutil zfs uutil icp
 # The libc dependencies are not strictly needed but are defined to make the
 # assert happy.
-_DP_c=		sys compiler_rt
+_DP_c=		compiler_rt
 # Use libssp_nonshared only on i386 and power*.  Other archs emit direct calls
 # to __stack_chk_fail, not __stack_chk_fail_local provided by libssp_nonshared.
 .if ${MK_SSP} != "no" && \
