52bf6257c2aac19788636e525b139ec57a38fb1c
Mark Johnston <markj@FreeBSD.org>
2024-02-08 10:56:58 -0500

    arm64: Define shadow maps for KMSAN
    Both are the same size as the kernel map.
    
    Reviewed by:	imp
    MFC after:	2 weeks
    Sponsored by:	Klara, Inc.
    Sponsored by:	Juniper Networks, Inc.
    Differential Revision:	https://reviews.freebsd.org/D43154

--- sys/arm64/include/vmparam.h
+++ sys/arm64/include/vmparam.h
@@ -131,6 +131,12 @@
  *                  0xfffffeffffffffff  End of DMAP
  *                  0xffffa00000000000  Start of DMAP
  *
+ *                  0xffff027fffffffff  End of KMSAN origin map
+ *                  0xffff020000000000  Start of KMSAN origin map
+ *
+ *                  0xffff017fffffffff  End of KMSAN shadow map
+ *                  0xffff010000000000  Start of KMSAN shadow map
+ *
  *                  0xffff009fffffffff  End of KASAN shadow map
  *                  0xffff008000000000  Start of KASAN shadow map
  *
@@ -166,6 +172,14 @@
 /* 128 GiB KASAN shadow map */
 #define	KASAN_MIN_ADDRESS	(0xffff008000000000UL)
 #define	KASAN_MAX_ADDRESS	(0xffff00a000000000UL)
+
+/* 512GiB KMSAN shadow map */
+#define	KMSAN_SHAD_MIN_ADDRESS	(0xffff010000000000UL)
+#define	KMSAN_SHAD_MAX_ADDRESS	(0xffff018000000000UL)
+
+/* 512GiB KMSAN origin map */
+#define	KMSAN_ORIG_MIN_ADDRESS	(0xffff020000000000UL)
+#define	KMSAN_ORIG_MAX_ADDRESS	(0xffff028000000000UL)
 
 /* The address bits that hold a pointer authentication code */
 #define	PAC_ADDR_MASK		(0xff7f000000000000UL)
