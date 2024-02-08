45eae45d91cb308396e15d6dabc764fc4c701f9f
Gleb Smirnoff <glebius@FreeBSD.org>
2024-02-08 09:00:23 -0800

    tests/unix_passfd: fix typos - assignment instead of comparison
    This typos were actually hiding a kernel bug, see 541e6a989cae.
    
    Reviewed by:		markj
    Differential Revision:	https://reviews.freebsd.org/D43734

--- tests/sys/kern/unix_passfd_test.c
+++ tests/sys/kern/unix_passfd_test.c
@@ -912,7 +912,7 @@ ATF_TC_BODY(empty_rights_message, tc)
 	/* Only the non-empty message should be received. */
 	len = recvmsg(fd[1], &msghdr, 0);
 	ATF_REQUIRE_MSG(len == 0, "recvmsg failed: %s", strerror(errno));
-	ATF_REQUIRE(msghdr.msg_controllen = CMSG_SPACE(sizeof(int)));
+	ATF_REQUIRE(msghdr.msg_controllen == CMSG_SPACE(sizeof(int)));
 	error = close(*(int *)CMSG_DATA(msghdr.msg_control));
 	ATF_REQUIRE_MSG(error == 0, "close failed: %s", strerror(errno));
 
@@ -938,7 +938,7 @@ ATF_TC_BODY(empty_rights_message, tc)
 	/* Only the non-empty message should be received. */
 	len = recvmsg(fd[1], &msghdr, 0);
 	ATF_REQUIRE_MSG(len == 0, "recvmsg failed: %s", strerror(errno));
-	ATF_REQUIRE(msghdr.msg_controllen = CMSG_SPACE(sizeof(int)));
+	ATF_REQUIRE(msghdr.msg_controllen == CMSG_SPACE(sizeof(int)));
 	error = close(*(int *)CMSG_DATA(msghdr.msg_control));
 	ATF_REQUIRE_MSG(error == 0, "close failed: %s", strerror(errno));
 
