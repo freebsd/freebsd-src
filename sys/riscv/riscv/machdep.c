95541913c39c1804117b9d7754c00683060ae9cc
John Baldwin <jhb@FreeBSD.org>
2024-02-15 12:20:30 -0800

    riscv: Add missing includes for DDB
    The #ifdef DDB code in parse_metadata was dead code without opt_ddb.h.
    While here, update the call to db_fetch_ksymtab for changes in commit
    02bc014a200a.
    
    Reviewed by:	mhorne
    Obtained from:	CheriBSD
    Differential Revision:	https://reviews.freebsd.org/D43919

--- sys/riscv/riscv/machdep.c
+++ sys/riscv/riscv/machdep.c
@@ -33,6 +33,7 @@
  * SUCH DAMAGE.
  */
 
+#include "opt_ddb.h"
 #include "opt_kstack_pages.h"
 #include "opt_platform.h"
 
@@ -90,6 +91,10 @@
 #include <machine/sbi.h>
 #include <machine/trap.h>
 #include <machine/vmparam.h>
+
+#ifdef DDB
+#include <ddb/ddb.h>
+#endif
 
 #ifdef FDT
 #include <contrib/libfdt/libfdt.h>
@@ -455,7 +460,7 @@ parse_metadata(void)
 #ifdef DDB
 	ksym_start = MD_FETCH(kmdp, MODINFOMD_SSYM, uintptr_t);
 	ksym_end = MD_FETCH(kmdp, MODINFOMD_ESYM, uintptr_t);
-	db_fetch_ksymtab(ksym_start, ksym_end);
+	db_fetch_ksymtab(ksym_start, ksym_end, 0);
 #endif
 #ifdef FDT
 	try_load_dtb(kmdp);
