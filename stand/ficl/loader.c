2a018f814a38121edb4e34517d721ada7d782af0
Warner Losh <imp@FreeBSD.org>
2024-02-15 20:53:28 -0700

    loader: Only create gfx 4th bindings when gfx is available
    Only create the gfx bindings for 4th when it's compiled into the
    loader. We do this with a linker set that only gets brought in to those
    loaders that call gfx_framework_init. This calls gfx_interp_md() will
    will drag in gfx_loader.c which will add to the linker set that
    registers these bindings.
    
    Sponsored by:		Netflix
    Reviewed by:		kevans, jhb
    Differential Revision:	https://reviews.freebsd.org/D43904

--- stand/ficl/loader.c
+++ stand/ficl/loader.c
@@ -44,8 +44,6 @@
 #include "bootstrap.h"
 #include <string.h>
 #include <uuid.h>
-#include <gfx_fb.h>
-#include <pnglite.h>
 #include "ficl.h"
 
 /*		FreeBSD's loader interaction words and extras
@@ -64,182 +62,6 @@
  *		uuid-to-string ( addr' -- addr n )
  * 		.#	    ( value -- )
  */
-
-#ifndef TESTMAIN
-/* ( flags x1 y1 x2 y2 -- flag ) */
-void
-ficl_term_putimage(FICL_VM *pVM)
-{
-        char *namep, *name;
-        int names;
-        unsigned long ret = FICL_FALSE;
-        uint32_t x1, y1, x2, y2, f;
-        png_t png;
-	int error;
-
-#if FICL_ROBUST > 1
-	vmCheckStack(pVM, 7, 1);
-#endif
-        names = stackPopINT(pVM->pStack);
-        namep = (char *) stackPopPtr(pVM->pStack);
-        y2 = stackPopINT(pVM->pStack);
-        x2 = stackPopINT(pVM->pStack);
-        y1 = stackPopINT(pVM->pStack);
-        x1 = stackPopINT(pVM->pStack);
-        f = stackPopINT(pVM->pStack);
-
-	x1 = gfx_state.tg_origin.tp_col + x1 * gfx_state.tg_font.vf_width;
-	y1 = gfx_state.tg_origin.tp_row + y1 * gfx_state.tg_font.vf_height;
-	if (x2 != 0) {
-		x2 = gfx_state.tg_origin.tp_col +
-		    x2 * gfx_state.tg_font.vf_width;
-	}
-	if (y2 != 0) {
-		y2 = gfx_state.tg_origin.tp_row +
-		    y2 * gfx_state.tg_font.vf_height;
-	}
-
-        name = ficlMalloc(names + 1);
-        if (!name)
-		vmThrowErr(pVM, "Error: out of memory");
-        (void) strncpy(name, namep, names);
-        name[names] = '\0';
-
-        if ((error = png_open(&png, name)) != PNG_NO_ERROR) {
-		if (f & FL_PUTIMAGE_DEBUG)
-			printf("%s\n", png_error_string(error));
-	} else {
-                if (gfx_fb_putimage(&png, x1, y1, x2, y2, f) == 0)
-                        ret = FICL_TRUE;        /* success */
-                (void) png_close(&png);
-	}
-        ficlFree(name);
-	stackPushUNS(pVM->pStack, ret);
-}
-
-/* ( flags x1 y1 x2 y2 -- flag ) */
-void
-ficl_fb_putimage(FICL_VM *pVM)
-{
-        char *namep, *name;
-        int names;
-        unsigned long ret = FICL_FALSE;
-        uint32_t x1, y1, x2, y2, f;
-        png_t png;
-	int error;
-
-#if FICL_ROBUST > 1
-	vmCheckStack(pVM, 7, 1);
-#endif
-        names = stackPopINT(pVM->pStack);
-        namep = (char *) stackPopPtr(pVM->pStack);
-        y2 = stackPopINT(pVM->pStack);
-        x2 = stackPopINT(pVM->pStack);
-        y1 = stackPopINT(pVM->pStack);
-        x1 = stackPopINT(pVM->pStack);
-        f = stackPopINT(pVM->pStack);
-
-        name = ficlMalloc(names + 1);
-        if (!name)
-		vmThrowErr(pVM, "Error: out of memory");
-        (void) strncpy(name, namep, names);
-        name[names] = '\0';
-
-        if ((error = png_open(&png, name)) != PNG_NO_ERROR) {
-		if (f & FL_PUTIMAGE_DEBUG)
-			printf("%s\n", png_error_string(error));
-	} else {
-                if (gfx_fb_putimage(&png, x1, y1, x2, y2, f) == 0)
-                        ret = FICL_TRUE;        /* success */
-                (void) png_close(&png);
-	}
-        ficlFree(name);
-	stackPushUNS(pVM->pStack, ret);
-}
-
-void
-ficl_fb_setpixel(FICL_VM *pVM)
-{
-        FICL_UNS x, y;
-
-#if FICL_ROBUST > 1
-	vmCheckStack(pVM, 2, 0);
-#endif
-
-        y = stackPopUNS(pVM->pStack);
-        x = stackPopUNS(pVM->pStack);
-        gfx_fb_setpixel(x, y);
-}
-
-void
-ficl_fb_line(FICL_VM *pVM)
-{
-	FICL_UNS x0, y0, x1, y1, wd;
-
-#if FICL_ROBUST > 1
-	vmCheckStack(pVM, 5, 0);
-#endif
-
-	wd = stackPopUNS(pVM->pStack);
-	y1 = stackPopUNS(pVM->pStack);
-	x1 = stackPopUNS(pVM->pStack);
-	y0 = stackPopUNS(pVM->pStack);
-	x0 = stackPopUNS(pVM->pStack);
-	gfx_fb_line(x0, y0, x1, y1, wd);
-}
-
-void
-ficl_fb_bezier(FICL_VM *pVM)
-{
-	FICL_UNS x0, y0, x1, y1, x2, y2, width;
-
-#if FICL_ROBUST > 1
-	vmCheckStack(pVM, 7, 0);
-#endif
-
-	width = stackPopUNS(pVM->pStack);
-	y2 = stackPopUNS(pVM->pStack);
-	x2 = stackPopUNS(pVM->pStack);
-	y1 = stackPopUNS(pVM->pStack);
-	x1 = stackPopUNS(pVM->pStack);
-	y0 = stackPopUNS(pVM->pStack);
-	x0 = stackPopUNS(pVM->pStack);
-	gfx_fb_bezier(x0, y0, x1, y1, x2, y2, width);
-}
-
-void
-ficl_fb_drawrect(FICL_VM *pVM)
-{
-	FICL_UNS x1, x2, y1, y2, fill;
-
-#if FICL_ROBUST > 1
-	vmCheckStack(pVM, 5, 0);
-#endif
-
-	fill = stackPopUNS(pVM->pStack);
-	y2 = stackPopUNS(pVM->pStack);
-	x2 = stackPopUNS(pVM->pStack);
-	y1 = stackPopUNS(pVM->pStack);
-	x1 = stackPopUNS(pVM->pStack);
-	gfx_fb_drawrect(x1, y1, x2, y2, fill);
-}
-
-void
-ficl_term_drawrect(FICL_VM *pVM)
-{
-	FICL_UNS x1, x2, y1, y2;
-
-#if FICL_ROBUST > 1
-	vmCheckStack(pVM, 4, 0);
-#endif
-
-	y2 = stackPopUNS(pVM->pStack);
-	x2 = stackPopUNS(pVM->pStack);
-	y1 = stackPopUNS(pVM->pStack);
-	x1 = stackPopUNS(pVM->pStack);
-	gfx_term_drawrect(x1, y1, x2, y2);
-}
-#endif	/* TESTMAIN */
 
 void
 ficlSetenv(FICL_VM *pVM)
@@ -1042,16 +864,6 @@ void ficlCompilePlatform(FICL_SYSTEM *pSys)
     dictAppendWord(dp, "ccall",	    ficlCcall,	    FW_DEFAULT);
     dictAppendWord(dp, "uuid-from-string", ficlUuidFromString, FW_DEFAULT);
     dictAppendWord(dp, "uuid-to-string", ficlUuidToString, FW_DEFAULT);
-#ifndef TESTMAIN
-    dictAppendWord(dp, "fb-setpixel", ficl_fb_setpixel, FW_DEFAULT);
-    dictAppendWord(dp, "fb-line", ficl_fb_line, FW_DEFAULT);
-    dictAppendWord(dp, "fb-bezier", ficl_fb_bezier, FW_DEFAULT);
-    dictAppendWord(dp, "fb-drawrect", ficl_fb_drawrect, FW_DEFAULT);
-    dictAppendWord(dp, "fb-putimage", ficl_fb_putimage, FW_DEFAULT);
-    dictAppendWord(dp, "term-drawrect", ficl_term_drawrect, FW_DEFAULT);
-    dictAppendWord(dp, "term-putimage", ficl_term_putimage, FW_DEFAULT);
-    dictAppendWord(dp, "isvirtualized?",ficlIsvirtualizedQ, FW_DEFAULT);
-#endif
     
     SET_FOREACH(fnpp, Xficl_compile_set)
 	(*fnpp)(pSys);
