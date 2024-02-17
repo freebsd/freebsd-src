2d120981e26dfef5c9cb9eb9936bb46cb6918136
Vladimir Kondratyev <wulf@FreeBSD.org>
2024-02-18 00:01:03 +0300

    Bump __FreeBSD_version after linuxkpi changes

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -75,7 +75,7 @@
  * cannot include sys/param.h and should only be updated here.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1400507
+#define __FreeBSD_version 1400508
 
 /*
  * __FreeBSD_kernel__ indicates that this system uses the kernel of FreeBSD,
