07ece5936ab1129003231af9c593a9cb77f763b3
Xin LI <delphij@FreeBSD.org>
2024-02-17 13:56:56 -0800

    MFC: MFV: expat 2.6.0.
    Approved by:	re (cperciva)
    
    (cherry picked from commit 4543ef516683042d46f3bd3bb8a4f3f746e00499)
    (cherry picked from commit 60cfaec917dad12ceab7450a518616668d9f1ed0)

--- contrib/expat/tests/structdata.c
+++ contrib/expat/tests/structdata.c
@@ -7,7 +7,8 @@
                                  |_| XML parser
 
    Copyright (c) 2017      Rhodri James <rhodri@wildebeest.org.uk>
-   Copyright (c) 2017-2021 Sebastian Pipping <sebastian@pipping.org>
+   Copyright (c) 2017-2023 Sebastian Pipping <sebastian@pipping.org>
+   Copyright (c) 2022      Sean McBride <sean@rogue-research.com>
    Licensed under the MIT license:
 
    Permission is  hereby granted,  free of charge,  to any  person obtaining
@@ -29,6 +30,10 @@
    OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
    USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+
+#if defined(NDEBUG)
+#  undef NDEBUG /* because test suite relies on assert(...) at the moment */
+#endif
 
 #include "expat_config.h"
 
@@ -56,7 +61,7 @@
 static XML_Char *
 xmlstrdup(const XML_Char *s) {
   size_t byte_count = (xcstrlen(s) + 1) * sizeof(XML_Char);
-  XML_Char *dup = malloc(byte_count);
+  XML_Char *const dup = (XML_Char *)malloc(byte_count);
 
   assert(dup != NULL);
   memcpy(dup, s, byte_count);
@@ -79,13 +84,13 @@ StructData_AddItem(StructData *storage, const XML_Char *s, int data0, int data1,
   assert(storage != NULL);
   assert(s != NULL);
   if (storage->count == storage->max_count) {
-    StructDataEntry *new;
+    StructDataEntry *new_entries;
 
     storage->max_count += STRUCT_EXTENSION_COUNT;
-    new = realloc(storage->entries,
-                  storage->max_count * sizeof(StructDataEntry));
-    assert(new != NULL);
-    storage->entries = new;
+    new_entries = (StructDataEntry *)realloc(
+        storage->entries, storage->max_count * sizeof(StructDataEntry));
+    assert(new_entries != NULL);
+    storage->entries = new_entries;
   }
 
   entry = &storage->entries[storage->count];
@@ -103,17 +108,17 @@ void
 StructData_CheckItems(StructData *storage, const StructDataEntry *expected,
                       int count) {
   char buffer[1024];
-  int i;
 
   assert(storage != NULL);
   assert(expected != NULL);
   if (count != storage->count) {
-    sprintf(buffer, "wrong number of entries: got %d, expected %d",
-            storage->count, count);
+    snprintf(buffer, sizeof(buffer),
+             "wrong number of entries: got %d, expected %d", storage->count,
+             count);
     StructData_Dispose(storage);
     fail(buffer);
   } else {
-    for (i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++) {
       const StructDataEntry *got = &storage->entries[i];
       const StructDataEntry *want = &expected[i];
 
@@ -126,11 +131,11 @@ StructData_CheckItems(StructData *storage, const StructDataEntry *expected,
       } else {
         if (got->data0 != want->data0 || got->data1 != want->data1
             || got->data2 != want->data2) {
-          sprintf(buffer,
-                  "struct '%" XML_FMT_STR
-                  "' expected (%d,%d,%d), got (%d,%d,%d)",
-                  got->str, want->data0, want->data1, want->data2, got->data0,
-                  got->data1, got->data2);
+          snprintf(buffer, sizeof(buffer),
+                   "struct '%" XML_FMT_STR
+                   "' expected (%d,%d,%d), got (%d,%d,%d)",
+                   got->str, want->data0, want->data1, want->data2, got->data0,
+                   got->data1, got->data2);
           StructData_Dispose(storage);
           fail(buffer);
         }
