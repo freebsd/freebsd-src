freebsd/freebsd-src

commit 278bbb187769c04159078003fcaaa549990c2bef

ls: versort incompatible w/ timesort and sizesort
ls.1: versort incompatible w/ timesort and sizesort

Reviewed by: imp
Pull Request: https://github.com/freebsd/freebsd-src/pull/825

bin/ls/ls.c
@@ -313,14 +313,21 @@ main(int argc, char *argv[])
 		case 'A':
 			f_listdot = 1;
 			break;
-		/* The -t and -S options override each other. */
+		/* The -S, -t and -v options override each other. */
 		case 'S':
 			f_sizesort = 1;
 			f_timesort = 0;
+			f_verssort = 0;
 			break;
 		case 't':
 			f_timesort = 1;
 			f_sizesort = 0;
+			f_verssort = 0;
+			break;
+		case 'v':
+			f_verssort = 1;
+			f_sizesort = 0;
+			f_verssort = 0;
 			break;
 		/* Other flags.  Please keep alphabetic. */
 		case ',':
@@ -433,9 +440,6 @@ main(int argc, char *argv[])
 			break;
 		case 's':
 			f_size = 1;
-			break;
-		case 'v':
-			f_verssort = 1;
 			break;
 		case 'w':
 			f_nonprint = 0;

bin/ls/ls.1
@@ -29,7 +29,7 @@
 .\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 .\" SUCH DAMAGE.
 .\"
-.Dd July 18, 2023
+.Dd January 12, 2024
 .Dt LS 1
 .Os
 .Sh NAME
@@ -457,9 +457,9 @@ options all override each other; the last one specified determines
 the file time used.
 .Pp
 The
-.Fl S
+.Fl S , t
 and
-.Fl t
+.Fl v
 options override each other; the last one specified determines
 the sort order used.
 .Pp

commit 450fd70c80e51aa6cdf4d75e27306f5a137ffce0

ls: Fix -v and associated test
The cleanup of 278bbb187769c had a cut and paste error (so f_verssort
was set to 1 and then to 0 rather thame f_timesort being set to 0).

Fixes: 278bbb187769c
Sponsored by: Netflix

bin/ls/ls.c
@@ -327,7 +327,7 @@ main(int argc, char *argv[])
 		case 'v':
 			f_verssort = 1;
 			f_sizesort = 0;
-			f_verssort = 0;
+			f_timesort = 0;
 			break;
 		/* Other flags.  Please keep alphabetic. */
 		case ',':
@@ -566,6 +566,7 @@ main(int argc, char *argv[])
 			blocksize /= 512;
 		}
 	}
+
 	/* Select a sort function. */
 	if (f_reversesort) {
 		if (f_sizesort)