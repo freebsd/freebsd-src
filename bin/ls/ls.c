ef75877fc2d93199aab2b509089136c433af1f20
Warner Losh <imp@FreeBSD.org>
2024-02-19 09:48:05 -0700

    ls: Fix -v and associated test
    The cleanup of d854370fa86b7 had a cut and paste error (so f_verssort
    was set to 1 and then to 0 rather thame f_timesort being set to 0).
    
    Fixes: d854370fa86b7
    Sponsored by: Netflix

--- bin/ls/ls.c
+++ bin/ls/ls.c
@@ -327,7 +327,7 @@ main(int argc, char *argv[])
 		case 'v':
 			f_verssort = 1;
 			f_sizesort = 0;
-			f_verssort = 0;
+			f_timesort = 0;
 			break;
 		/* Other flags.  Please keep alphabetic. */
 		case ',':
@@ -566,6 +566,7 @@ main(int argc, char *argv[])
 			blocksize /= 512;
 		}
 	}
+
 	/* Select a sort function. */
 	if (f_reversesort) {
 		if (f_sizesort)
