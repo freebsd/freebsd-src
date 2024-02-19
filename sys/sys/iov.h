f24c816daa00852f4a1d37649d00905a0d67f1b3
Gordon Bergling <gbe@FreeBSD.org>
2024-02-19 12:02:57 +0100

    iov.h: Fix a typo in a source code comment
    - s/subystem/subsystem/
    
    MFC after:	3 days

--- sys/sys/iov.h
+++ sys/sys/iov.h
@@ -209,7 +209,7 @@ struct pci_iov_schema
  *    (PF or individual VF).
  *         Note: We will refer to the section of the configuration schema that
  *               specifies the parameters for this subsystem and device
- *               configuration as the device/subystem schema.
+ *               configuration as the device/subsystem schema.
  * 2) The subsystem node must contain only keys that correspond to parameters
  *    that are specified in the device/subsystem schema.
  * 3) Every parameter specified as required in the device/subsystem schema is
