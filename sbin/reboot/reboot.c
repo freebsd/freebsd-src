0c3ade2cf13df1ed5cd9db4081137ec90fcd19d0
Gleb Smirnoff <glebius@FreeBSD.org>
2024-02-19 19:51:22 -0800

    nextboot: fix nextboot -k on ZFS
    zfsbootcfg(1) expects pool name to operate on, not currently mounted
    filesystem name.
    
    Fixes:	fd6d47375a78fbf0737012b7cc11180291781e8b

--- sbin/reboot/reboot.c
+++ sbin/reboot/reboot.c
@@ -130,6 +130,11 @@ write_nextboot(const char *fn, const char *env, bool force)
 	}
 
 	if (zfs) {
+		char *slash;
+
+		if ((slash = strchr(sfs.f_mntfromname, '/')) == NULL)
+			E("Can't find ZFS pool name in %s", sfs.f_mntfromname);
+		*slash = '\0';
 		zfsbootcfg(sfs.f_mntfromname, force);
 	}
 
