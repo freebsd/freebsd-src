32568e5f2435a2539b0c4177a6fefe7a9e0a0c89
Warner Losh <imp@FreeBSD.org>
2024-02-21 08:48:32 -0700

    loader: Retire CTASSERT
    The project is moving away from CTASSERT in favor of
    _Static_assert. Cleanup the few instances in the loader proactively.
    
    Sponsored by:		Netflix
    Reviewed by:		manu, tsoome
    Differential Revision:	https://reviews.freebsd.org/D44006

--- stand/common/bootstrap.h
+++ stand/common/bootstrap.h
@@ -382,8 +382,4 @@ int gen_setcurrdev(struct env_var *ev, int flags, const void *value);
 int mount_currdev(struct env_var *, int, const void *);
 void set_currdev(const char *devname);
 
-#ifndef CTASSERT
-#define	CTASSERT(x)	_Static_assert(x, "compile-time assertion failed")
-#endif
-
 #endif /* !_BOOTSTRAP_H_ */
