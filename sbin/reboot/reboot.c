freebsd/freebsd-src

commit 0ca7dada0309e730ff81423fbb6117094bba7635

reboot: Implement -D from nextboot
Implement -D from nextboot.sh which deletes the nextboot.conf file and
exists.

Sponsored by:		Netflix
Differential Revision:	https://reviews.freebsd.org/D43822

sbin/reboot/reboot.c
@@ -95,7 +95,7 @@ main(int argc, char *argv[])
 	struct utmpx utx;
 	const struct passwd *pw;
 	int ch, howto, i, sverrno;
-	bool fflag, lflag, nflag, qflag, Nflag;
+	bool Dflag, fflag, lflag, Nflag, nflag, qflag;
 	uint64_t pageins;
 	const char *user, *kernel = NULL;
 
@@ -104,11 +104,14 @@ main(int argc, char *argv[])
 		howto = RB_HALT;
 	} else
 		howto = 0;
-	fflag = lflag = nflag = qflag = Nflag = false;
-	while ((ch = getopt(argc, argv, "cdk:lNnpqr")) != -1)
+	Dflag = fflag = lflag = Nflag = nflag = qflag = false;
+	while ((ch = getopt(argc, argv, "cDdk:lNnpqr")) != -1)
 		switch(ch) {
 		case 'c':
 			howto |= RB_POWERCYCLE;
+			break;
+		case 'D':
+			Dflag = true;
 			break;
 		case 'd':
 			howto |= RB_DUMP;
@@ -148,6 +151,8 @@ main(int argc, char *argv[])
 	if (argc != 0)
 		usage();
 
+	if (Dflag && ((howto & ~RB_HALT) != 0  || kernel != NULL))
+		errx(1, "cannot delete existing nextboot config and do anything else");
 	if ((howto & (RB_DUMP | RB_HALT)) == (RB_DUMP | RB_HALT))
 		errx(1, "cannot dump (-d) when halting; must reboot instead");
 	if (Nflag && (howto & RB_NOSYNC) != 0)
@@ -161,6 +166,12 @@ main(int argc, char *argv[])
 	if (geteuid()) {
 		errno = EPERM;
 		err(1, NULL);
+	}
+
+	if (Dflag) {
+		if (unlink(PATH_NEXTBOOT) != 0)
+			err(1, "unlink %s", PATH_NEXTBOOT);
+		exit(0);
 	}
 
 	if (qflag) {

sbin/reboot/reboot.8
@@ -36,16 +36,16 @@
 .Nd stopping and restarting the system
 .Sh SYNOPSIS
 .Nm halt
-.Op Fl flNnpq
+.Op Fl DflNnpq
 .Op Fl k Ar kernel
 .Nm
-.Op Fl cdflNnpqr
+.Op Fl cDdflNnpqr
 .Op Fl k Ar kernel
 .Nm fasthalt
-.Op Fl flNnpq
+.Op Fl DflNnpq
 .Op Fl k Ar kernel
 .Nm fastboot
-.Op Fl dflNnpq
+.Op Fl dDflNnpq
 .Op Fl k Ar kernel
 .Sh DESCRIPTION
 The
@@ -77,6 +77,10 @@ driver implements the power cycle functionality and only on hardware
 with a BMC that supports power cycling.
 Unlike power off, the amount of hardware that supports power cycling
 is small.
+.It Fl D
+Delete existing
+.Nm nextboot
+configuration and exit.
 .It Fl d
 The system is requested to create a crash dump.
 This option is

commit bb6c02126e6ef841b4fedecbfb7ad334d84238d4

reboot: Emulate nextboot -D better
It used to produce no output when the file couldn't be removed. Emulate
that better by unlinking and ignoring errors. It's used at the end of
reboot always, even when the file isn't going to be there.

Sponsored by: Netflix
Fixes: 0ca7dada0309

sbin/reboot/reboot.c
@@ -288,8 +288,7 @@ main(int argc, char *argv[])
 		errx(1, "-r and -k cannot be used together, there is no next kernel");
 
 	if (Dflag) {
-		if (unlink(PATH_NEXTBOOT) != 0)
-			err(1, "unlink %s", PATH_NEXTBOOT);
+		(void)unlink(PATH_NEXTBOOT);	/* Say nothing if it's not there */
 		exit(0);
 	}
 