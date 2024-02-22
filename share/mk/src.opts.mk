freebsd/freebsd-src

commit f2c2803904307b58a21bb74cc975dfcfcf6260cf

loader: Mark BEARSSL broken on powerpc
When BEARSSL is enabled, we pull in libsecureboot, which has EFI
dependencies which don't exist on powerpc. This needs to be detangled,
but until then mark it as broken.

Sponsored by:		Netflix

share/mk/src.opts.mk
@@ -313,6 +313,9 @@ BROKEN_OPTIONS+=LIB32
 .if ${__T:Mpowerpc*} || ${__T} == "i386"
 BROKEN_OPTIONS+=EFI
 .endif
+# Bad coupling for libsecure stuff with bearssl and efi, so broken on EFI
+.if ${__T:Mpowerpc*}
+BROKEN_OPTIONS+=BEARSSL		# bearssl brings in secure efi stuff xxx
 # OFW is only for powerpc, exclude others
 .if ${__T:Mpowerpc*} == ""
 BROKEN_OPTIONS+=LOADER_OFW

commit bf2f4c7da05524c4c3afa712d12083cbb8917adb

Add missing .endif
Last second move from inside the prior block to outside bites me.

Noticed by: jrtc23
Fixes: f2c280390430
Sponsored by:		Netflix

share/mk/src.opts.mk
@@ -316,6 +316,7 @@ BROKEN_OPTIONS+=EFI
 # Bad coupling for libsecure stuff with bearssl and efi, so broken on EFI
 .if ${__T:Mpowerpc*}
 BROKEN_OPTIONS+=BEARSSL		# bearssl brings in secure efi stuff xxx
+.endif
 # OFW is only for powerpc, exclude others
 .if ${__T:Mpowerpc*} == ""
 BROKEN_OPTIONS+=LOADER_OFW