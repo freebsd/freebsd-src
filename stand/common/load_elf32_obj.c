bcb0e17d35fb475128d9ad1840c85b188ab0ed5e
Warner Losh <imp@FreeBSD.org>
2024-02-22 09:30:59 -0700

    loader: These files have no copyrightable material
    These files have no copyrightable material, and so are in the public
    domain.
    
    Sponsored by:		Netflix
    
    (cherry picked from commit 27c0f2a544989760c4b3b16a3615839c8ab44b32)

--- stand/common/load_elf32_obj.c
+++ stand/common/load_elf32_obj.c
@@ -1,3 +1,5 @@
+/* This file is in the public domain */
+
 #define __ELF_WORD_SIZE 32
 #define	_MACHINE_ELF_WANT_32BIT
 #include "load_elf_obj.c"
--- stand/common/load_elf64.c
+++ stand/common/load_elf64.c
@@ -1,2 +1,4 @@
+/* This file is in the public domain */
+
 #define __ELF_WORD_SIZE 64
 #include "load_elf.c"
