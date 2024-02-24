2ba9bcd97608923c084edd6d7a36bf5b5a73f6be
Gordon Bergling <gbe@FreeBSD.org>
2024-02-24 10:13:44 +0100

    if_hn: Fix two typos in source code comments
    - s/managment/management/
    - s/transacion/transaction/
    
    Obtained from:	NetBSD
    
    (cherry picked from commit 61b88a230bac766f55984d8d33b98845d2b0d1a9)

--- sys/dev/hyperv/netvsc/if_hn.c
+++ sys/dev/hyperv/netvsc/if_hn.c
@@ -2532,7 +2532,7 @@ hn_detach(device_t dev)
 				hn_stop(sc, true);
 			/*
 			 * NOTE:
-			 * hn_stop() only suspends data, so managment
+			 * hn_stop() only suspends data, so management
 			 * stuffs have to be suspended manually here.
 			 */
 			hn_suspend_mgmt(sc);
