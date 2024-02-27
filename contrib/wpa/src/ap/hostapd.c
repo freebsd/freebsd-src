8e139c369364c1247133b55163289d67780bb976
Cy Schubert <cy@FreeBSD.org>
2024-02-26 19:34:09 -0800

    hostapd: Work around lack of MLME support
    hostap MLME uses Linux data structures and definitions not available
    in FreeBSD. The ability for hostapd to select the frequency (channel)
    depends Linux MLME, though strictly it's not required. Work around the
    Linux MLME requirement to configure device frequency.
    
    The detailed description is: hostapd will only set the channel (frequency)
    when Linux MLME is configured. Enabling NEED_AP_MLME will result in
    numerous build errors due do Linux data structures and definitions not
    available under FreeBSD. The code to set the frequency from the selected
    channel is only within the NEED_AP_MLME code path because without MLME,
    hostapd_get_hw_features() is an inline that always returns -1 whereas with
    MLME hostapd_get_hw_features() will obtain hardware features from the
    kernel. Until such time we simply set the frequency as configured.
    
    PR:		276375
    MFC after:	1 month

--- contrib/wpa/src/ap/hostapd.c
+++ contrib/wpa/src/ap/hostapd.c
@@ -1698,6 +1698,20 @@ static int setup_interface2(struct hostapd_iface *iface)
 {
 	iface->wait_channel_update = 0;
 
+#ifdef __FreeBSD
+	/* XXX hostapd_get_hw_features() is an inline that always returns -1
+	 * because MLME will not build under FreeBSD due to its use of
+	 * Linux definitions. Normally FreeBSD would uncondionally execute the
+	 * "Not all drivers support..." block. Instead we #ifdef out the entire
+	 * block of code instead of maintaining the fallacy that
+	 * hostapd_get_hw_features() returns anything meaninful.
+	 *
+	 * Ideally WANT_AP_MLME should be taught about FreeBSD data structures
+	 * and defintions. Instead we do this to enable channel selection in
+	 * hostapd.conf.
+	 */
+	iface->freq = iface->conf->channel;
+#else
 	if (hostapd_get_hw_features(iface)) {
 		/* Not all drivers support this yet, so continue without hw
 		 * feature data. */
@@ -1744,6 +1758,7 @@ static int setup_interface2(struct hostapd_iface *iface)
 		if (iface->conf->ieee80211h)
 			wpa_printf(MSG_DEBUG, "DFS support is enabled");
 	}
+#endif
 	return hostapd_setup_interface_complete(iface, 0);
 
 fail:
