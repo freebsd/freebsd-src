04d5a8ae04013fd34e3bde5e401b6eaff46ac9c2
Gordon Bergling <gbe@FreeBSD.org>
2024-02-27 13:42:10 +0100

    vge(4): Fix a typo in a source code comment
    - s/opration/operation/
    
    MFC after:	3 days

--- sys/dev/vge/if_vge.c
+++ sys/dev/vge/if_vge.c
@@ -1218,7 +1218,7 @@ vge_discard_rxbuf(struct vge_softc *sc, int prod)
 
 	/*
 	 * Note: the manual fails to document the fact that for
-	 * proper opration, the driver needs to replentish the RX
+	 * proper operation, the driver needs to replentish the RX
 	 * DMA ring 4 descriptors at a time (rather than one at a
 	 * time, like most chips). We can allocate the new buffers
 	 * but we should not set the OWN bits until we're ready
