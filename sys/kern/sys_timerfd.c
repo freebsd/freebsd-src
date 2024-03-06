104328e63034f34a7961c0ebfb2dfdf78717982d
Baptiste Daroussin <bapt@FreeBSD.org>
2024-03-06 15:11:32 +0100

    timerfd_create: accept CLOCK_UPTIME/CLOCK_BOOTTIME
    This is a common use case when using timerfd_create to actually use
    it with CLOCK_BOOTTIME on linux which is CLOCK_UPTIME for us.
    
    Note that currently on freebsd CLOCK_BOOTTIME is CLOCK_UPTIME, but the
    semantic is supposed to be different, this has to be fixed later.
    
    Tested with the fnott notification software
    
    Reviewed by:	des, imp
    Differential Revision:	https://reviews.freebsd.org/D44253
    
    (cherry picked from commit cf742faa39a58a9b43b671c66097e6880459d4ae)
    (cherry picked from commit 0ecf0b26a750582b804e238e6446db55188d7fdc)

--- sys/kern/sys_timerfd.c
+++ sys/kern/sys_timerfd.c
@@ -431,8 +431,20 @@ kern_timerfd_create(struct thread *td, int clockid, int flags)
 	AUDIT_ARG_VALUE(clockid);
 	AUDIT_ARG_FFLAGS(flags);
 
-	if (clockid != CLOCK_REALTIME && clockid != CLOCK_MONOTONIC)
+	switch (clockid) {
+	case CLOCK_REALTIME:
+		/* FALLTHROUGH */
+	case CLOCK_MONOTONIC:
+		/* FALLTHROUGH */
+	case CLOCK_UPTIME:
+		/*
+		 * CLOCK_BOOTTIME should be added once different from
+		 * CLOCK_UPTIME
+		 */
+		break;
+	default:
 		return (EINVAL);
+	}
 	if ((flags & ~(TFD_CLOEXEC | TFD_NONBLOCK)) != 0)
 		return (EINVAL);
 
