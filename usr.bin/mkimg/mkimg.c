c7f1512739c729cd82042cf229a3ab7c56920b70
Eugene Grosbein <eugen@FreeBSD.org>
2024-03-12 22:55:42 +0700

    MFC: mkimg(1): process non-seekable output gracefully
    mkimg may make severe load only to fail in the end
    if output is non-seekable pipe, socket or FIFO
    unless output format is raw disk image.
    
    Check it out and fail early. Make it clear in the manual.
    
    (cherry picked from commit 7f0dc6e2cdfa0317c9917dd46e9da9d3897a8fbb)

--- usr.bin/mkimg/mkimg.c
+++ usr.bin/mkimg/mkimg.c
@@ -556,6 +556,7 @@ mkimg(void)
 int
 main(int argc, char *argv[])
 {
+	const char *format_name;
 	int bcfd, outfd;
 	int c, error;
 
@@ -700,6 +701,7 @@ main(int argc, char *argv[])
 			errc(EX_DATAERR, error, "boot code");
 	}
 
+	format_name = format_selected()->name;
 	if (verbose) {
 		fprintf(stderr, "Logical sector size: %u\n", secsz);
 		fprintf(stderr, "Physical block size: %u\n", blksz);
@@ -710,9 +712,19 @@ main(int argc, char *argv[])
 			fprintf(stderr, "Partitioning scheme: %s\n",
 			    scheme_selected()->name);
 		fprintf(stderr, "Output file format:  %s\n",
-		    format_selected()->name);
+		    format_name);
 		fputc('\n', stderr);
 	}
+
+#if defined(SPARSE_WRITE)
+	/*
+	 * sparse_write() fails if output is not seekable so fail early
+	 * not wasting some load unless output format is raw
+	 */
+	if (strcmp("raw", format_name) &&
+	    lseek(outfd, (off_t)0, SEEK_CUR) == -1 && errno == ESPIPE)
+		errx(EX_USAGE, "%s: output must be seekable", format_name);
+#endif
 
 	error = image_init();
 	if (error)
