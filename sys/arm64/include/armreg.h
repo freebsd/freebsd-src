27b444b1137691a2acf325d2b1e4ab2a7424cb44
Andrew Turner <andrew@FreeBSD.org>
2024-03-12 16:50:04 +0000

    arm64: Add EL1 hardware breakpoint exceptions
    Reviewed by:	jhb
    Sponsored by:	Arm Ltd
    Differential Revision:	https://reviews.freebsd.org/D44353

--- sys/arm64/include/armreg.h
+++ sys/arm64/include/armreg.h
@@ -439,6 +439,7 @@
 #define	 EXCP_TRAP_FP		0x2c	/* Trapped FP exception */
 #define	 EXCP_SERROR		0x2f	/* SError interrupt */
 #define	 EXCP_BRKPT_EL0		0x30	/* Hardware breakpoint, from same EL */
+#define	 EXCP_BRKPT_EL1		0x31	/* Hardware breakpoint, from same EL */
 #define	 EXCP_SOFTSTP_EL0	0x32	/* Software Step, from lower EL */
 #define	 EXCP_SOFTSTP_EL1	0x33	/* Software Step, from same EL */
 #define	 EXCP_WATCHPT_EL0	0x34	/* Watchpoint, from lower EL */
