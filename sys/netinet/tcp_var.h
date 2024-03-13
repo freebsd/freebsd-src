87c7f74ff4dac9b3bb4138216292e68871acb0c5
Konstantin Belousov <kib@FreeBSD.org>
2024-03-13 13:54:50 +0200

    netinet/tcp_var.h: always define IS_FASTOPEN() for kernel compilation env
    (cherry picked from commit 220ee18f196482c534a659d1eb50db26c54ca7d0)

--- sys/netinet/tcp_var.h
+++ sys/netinet/tcp_var.h
@@ -816,10 +816,12 @@ tcp_packets_this_ack(struct tcpcb *tp, tcp_seq ack)
 #define	ENTER_RECOVERY(t_flags) t_flags |= (TF_CONGRECOVERY | TF_FASTRECOVERY)
 #define	EXIT_RECOVERY(t_flags) t_flags &= ~(TF_CONGRECOVERY | TF_FASTRECOVERY)
 
-#if defined(_KERNEL) && !defined(TCP_RFC7413)
+#if defined(_KERNEL)
+#if !defined(TCP_RFC7413)
 #define	IS_FASTOPEN(t_flags)		(false)
 #else
 #define	IS_FASTOPEN(t_flags)		(t_flags & TF_FASTOPEN)
+#endif
 #endif
 
 #define	BYTES_THIS_ACK(tp, th)	(th->th_ack - tp->snd_una)
