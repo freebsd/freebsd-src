64e4760ee2bafa6779a55f97667f56865a7daa4e
Konstantin Belousov <kib@FreeBSD.org>
2024-03-20 05:49:01 +0200

    sys/syscallsubr.h: align definition of kern_fcntl_freebsd() on 32bit
    Fixes:	4f804500e65291c646378ab2109a5c8e427a2958

--- sys/sys/syscallsubr.h
+++ sys/sys/syscallsubr.h
@@ -164,7 +164,7 @@ int	kern_fchmodat(struct thread *td, int fd, const char *path,
 int	kern_fchownat(struct thread *td, int fd, const char *path,
 	    enum uio_seg pathseg, int uid, int gid, int flag);
 int	kern_fcntl(struct thread *td, int fd, int cmd, intptr_t arg);
-int	kern_fcntl_freebsd(struct thread *td, int fd, int cmd, long arg);
+int	kern_fcntl_freebsd(struct thread *td, int fd, int cmd, intptr_t arg);
 int	kern_fhopen(struct thread *td, const struct fhandle *u_fhp, int flags);
 int	kern_fhstat(struct thread *td, fhandle_t fh, struct stat *buf);
 int	kern_fhstatfs(struct thread *td, fhandle_t fh, struct statfs *buf);
