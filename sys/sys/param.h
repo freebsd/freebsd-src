69da6e0879831f1ccc5c60135ed43fcea10a933b
Dimitry Andric <dim@FreeBSD.org>
2024-03-20 23:39:06 +0100

    Merge commit bbb8a0df7367 from llvm-project (by Shafik Yaghmour):
      [Clang] Fix ResolveConstructorOverload to not select a conversion function if we are going use copy elision
    
      ResolveConstructorOverload needs to check properly if we are going to use copy
      elision we can't use a conversion function.
    
      This fixes:
    
      https://github.com/llvm/llvm-project/issues/39319
      https://github.com/llvm/llvm-project/issues/60182
      https://github.com/llvm/llvm-project/issues/62157
      https://github.com/llvm/llvm-project/issues/64885
      https://github.com/llvm/llvm-project/issues/65568
    
      Differential Revision: https://reviews.llvm.org/D148474
    
    This should fix 'Assertion failed: (isa<To>(Val) && "cast<Ty>() argument
    of incompatible type!")' errors when building devel/boost-libs,
    specifically libs/url/src/segments_view.cpp.
    
    Bump __FreeBSD_version so this fix can easily be detected from
    devel/boost-all/compiled.mk.
    
    PR:		273335
    (cherry picked from commit bcd401b5a39c6186bb1fe450286299f2a481244c)

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -75,7 +75,7 @@
  * cannot include sys/param.h and should only be updated here.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1400509
+#define __FreeBSD_version 1400510
 
 /*
  * __FreeBSD_kernel__ indicates that this system uses the kernel of FreeBSD,
