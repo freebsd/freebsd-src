312a05c39e5fd79b37ee6f922462232797f56a2a
Brooks Davis <brooks@FreeBSD.org>
2024-03-21 23:46:28 +0000

    riscv: remove more riscv64sf support
    Remove a few more bits of riscv64sf support in libc and libm.
    
    Reduce floating point ABI checks to requiring double hard float.
    
    Reviewed by:	imp, jhb
    Fixes:		1ca12bd927d7 Remove the riscv64sf architecture.
    Differential Revision:	https://reviews.freebsd.org/D44334

--- lib/msun/riscv/fenv.c
+++ lib/msun/riscv/fenv.c
@@ -37,14 +37,6 @@
  */
 const fenv_t __fe_dfl_env = 0;
 
-#ifdef __riscv_float_abi_soft
-#define __set_env(env, flags, mask, rnd) env = ((flags) | (rnd) << 5)
-#define __env_flags(env)                ((env) & FE_ALL_EXCEPT)
-#define __env_mask(env)                 (0) /* No exception traps. */
-#define __env_round(env)                (((env) >> 5) & _ROUND_MASK)
-#include "fenv-softfloat.h"
-#endif
-
 extern inline int feclearexcept(int __excepts);
 extern inline int fegetexceptflag(fexcept_t *__flagp, int __excepts);
 extern inline int fesetexceptflag(const fexcept_t *__flagp, int __excepts);
