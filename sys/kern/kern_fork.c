b0db3496f9ce0c761fec5aef36f58dcbeb08d929
stelkasouridis <stelkasouridis@gmail.com>
2024-03-27 05:37:18 +0200

    commented (did not remove, for potential future testing) the functionality of fork adding processes to the database

--- sys/kern/kern_fork.c
+++ sys/kern/kern_fork.c
@@ -71,7 +71,7 @@
 #include <sys/sx.h>
 #include <sys/sysent.h>
 #include <sys/signalvar.h>
-#include <sys/sqlite_lkm.h>
+// #include <sys/sqlite_lkm.h>
 
 #include <security/audit/audit.h>
 #include <security/mac/mac_framework.h>
@@ -299,9 +299,11 @@ retry:
 	mtx_unlock(&procid_lock);
 
 	//Call dbos_add_pid(result) here
+	/*
 	if (dbos_add_pid_ptr != NULL) {
 		(*dbos_add_pid_ptr)(result);
 	}
+	*/
 
 	return (result);
 }
