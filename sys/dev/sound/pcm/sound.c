4dfb88f3dea5708ab3c514736e1c8bab1a4a225c
Christos Margiolis <christos@FreeBSD.org>
2024-03-29 23:29:43 +0800

    sound: Get rid of pcm/sndstat.h and turn macros into regular code
    There is no reason to have macros for this. Putting the code in
    sndstat_prepare_pcm() directly makes it easier to work with it.
    
    No functional change intended.
    
    Sponsored by:	The FreeBSD Foundation
    MFC after:	2 weeks
    Reviewed by:	markj
    Differential Revision:	https://reviews.freebsd.org/D44545
    
    (cherry picked from commit 6d1cee1640e1c360917e483ece5ec1bde423b788)

--- sys/dev/sound/pcm/sound.c
+++ sys/dev/sound/pcm/sound.c
@@ -37,7 +37,6 @@
 #include <dev/sound/pcm/ac97.h>
 #include <dev/sound/pcm/vchan.h>
 #include <dev/sound/pcm/dsp.h>
-#include <dev/sound/pcm/sndstat.h>
 #include <dev/sound/version.h>
 #include <sys/limits.h>
 #include <sys/sysctl.h>
@@ -76,10 +75,139 @@ SYSCTL_STRING(_hw_snd, OID_AUTO, version, CTLFLAG_RD, &snd_driver_version,
 struct unrhdr *pcmsg_unrhdr = NULL;
 
 static int
-sndstat_prepare_pcm(SNDSTAT_PREPARE_PCM_ARGS)
+sndstat_prepare_pcm(struct sbuf *s, device_t dev, int verbose)
 {
-	SNDSTAT_PREPARE_PCM_BEGIN();
-	SNDSTAT_PREPARE_PCM_END();
+	struct snddev_info *d;
+	struct pcm_channel *c;
+	struct pcm_feeder *f;
+
+	d = device_get_softc(dev);
+	PCM_BUSYASSERT(d);
+
+	if (CHN_EMPTY(d, channels.pcm)) {
+		sbuf_printf(s, " (mixer only)");
+		return (0);
+	}
+
+	if (verbose < 1) {
+		sbuf_printf(s, " (%s%s%s",
+		    d->playcount ? "play" : "",
+		    (d->playcount && d->reccount) ? "/" : "",
+		    d->reccount ? "rec" : "");
+	} else {
+		sbuf_printf(s, " (%dp:%dv/%dr:%dv",
+		    d->playcount, d->pvchancount,
+		    d->reccount, d->rvchancount);
+	}
+	sbuf_printf(s, "%s)%s",
+	    ((d->playcount != 0 && d->reccount != 0) &&
+	    (d->flags & SD_F_SIMPLEX)) ? " simplex" : "",
+	    (device_get_unit(dev) == snd_unit) ? " default" : "");
+
+	if (verbose <= 1)
+		return (0);
+
+	sbuf_printf(s, "\n\t");
+	sbuf_printf(s, "snddev flags=0x%b", d->flags, SD_F_BITS);
+
+	CHN_FOREACH(c, d, channels.pcm) {
+		KASSERT(c->bufhard != NULL && c->bufsoft != NULL,
+		    ("hosed pcm channel setup"));
+
+		sbuf_printf(s, "\n\t");
+
+		sbuf_printf(s, "%s[%s]: ",
+		    (c->parentchannel != NULL) ?
+		    c->parentchannel->name : "", c->name);
+		sbuf_printf(s, "spd %d", c->speed);
+		if (c->speed != sndbuf_getspd(c->bufhard)) {
+			sbuf_printf(s, "/%d",
+			    sndbuf_getspd(c->bufhard));
+		}
+		sbuf_printf(s, ", fmt 0x%08x", c->format);
+		if (c->format != sndbuf_getfmt(c->bufhard)) {
+			sbuf_printf(s, "/0x%08x",
+			    sndbuf_getfmt(c->bufhard));
+		}
+		sbuf_printf(s, ", flags 0x%08x, 0x%08x",
+		    c->flags, c->feederflags);
+		if (c->pid != -1) {
+			sbuf_printf(s, ", pid %d (%s)",
+			    c->pid, c->comm);
+		}
+		sbuf_printf(s, "\n\t");
+
+		sbuf_printf(s, "interrupts %d, ", c->interrupts);
+
+		if (c->direction == PCMDIR_REC)	{
+			sbuf_printf(s,
+			    "overruns %d, feed %u, hfree %d, "
+			    "sfree %d [b:%d/%d/%d|bs:%d/%d/%d]",
+				c->xruns, c->feedcount,
+				sndbuf_getfree(c->bufhard),
+				sndbuf_getfree(c->bufsoft),
+				sndbuf_getsize(c->bufhard),
+				sndbuf_getblksz(c->bufhard),
+				sndbuf_getblkcnt(c->bufhard),
+				sndbuf_getsize(c->bufsoft),
+				sndbuf_getblksz(c->bufsoft),
+				sndbuf_getblkcnt(c->bufsoft));
+		} else {
+			sbuf_printf(s,
+			    "underruns %d, feed %u, ready %d "
+			    "[b:%d/%d/%d|bs:%d/%d/%d]",
+				c->xruns, c->feedcount,
+				sndbuf_getready(c->bufsoft),
+				sndbuf_getsize(c->bufhard),
+				sndbuf_getblksz(c->bufhard),
+				sndbuf_getblkcnt(c->bufhard),
+				sndbuf_getsize(c->bufsoft),
+				sndbuf_getblksz(c->bufsoft),
+				sndbuf_getblkcnt(c->bufsoft));
+		}
+		sbuf_printf(s, "\n\t");
+
+		sbuf_printf(s, "channel flags=0x%b", c->flags, CHN_F_BITS);
+		sbuf_printf(s, "\n\t");
+
+		sbuf_printf(s, "{%s}",
+		    (c->direction == PCMDIR_REC) ? "hardware" : "userland");
+		sbuf_printf(s, " -> ");
+		f = c->feeder;
+		while (f->source != NULL)
+			f = f->source;
+		while (f != NULL) {
+			sbuf_printf(s, "%s", f->class->name);
+			if (f->desc->type == FEEDER_FORMAT) {
+				sbuf_printf(s, "(0x%08x -> 0x%08x)",
+				    f->desc->in, f->desc->out);
+			} else if (f->desc->type == FEEDER_MATRIX) {
+				sbuf_printf(s, "(%d.%d -> %d.%d)",
+				    AFMT_CHANNEL(f->desc->in) -
+				    AFMT_EXTCHANNEL(f->desc->in),
+				    AFMT_EXTCHANNEL(f->desc->in),
+				    AFMT_CHANNEL(f->desc->out) -
+				    AFMT_EXTCHANNEL(f->desc->out),
+				    AFMT_EXTCHANNEL(f->desc->out));
+			} else if (f->desc->type == FEEDER_RATE) {
+				sbuf_printf(s,
+				    "(0x%08x q:%d %d -> %d)",
+				    f->desc->out,
+				    FEEDER_GET(f, FEEDRATE_QUALITY),
+				    FEEDER_GET(f, FEEDRATE_SRC),
+				    FEEDER_GET(f, FEEDRATE_DST));
+			} else {
+				sbuf_printf(s, "(0x%08x)",
+				    f->desc->out);
+			}
+			sbuf_printf(s, " -> ");
+			f = f->parent;
+		}
+		sbuf_printf(s, "{%s}",
+		    (c->direction == PCMDIR_REC) ? "userland" : "hardware");
+	}
+
+	return (0);
 }
 
 void *
