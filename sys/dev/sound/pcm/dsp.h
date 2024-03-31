b8ede1ba5b7ffa6f944dfe1bc9cf232b7d377576
Christos Margiolis <christos@FreeBSD.org>
2024-03-31 13:20:17 +0800

    sound: Get rid of snd_clone and use DEVFS_CDEVPRIV(9)
    Currently the snd_clone framework creates device nodes on-demand for
    every channel, through the dsp_clone() callback, and is responsible for
    routing audio to the appropriate channel(s). This patch gets rid of the
    whole snd_clone framework (including any related sysctls) and instead
    uses DEVFS_CDEVPRIV(9) to handle device opening, channel allocation and
    audio routing. This results in a significant reduction in code size as
    well as complexity.
    
    Behavior that is preserved:
    
    - hw.snd.basename_clone.
    - Exclusive access of an audio device (i.e VCHANs disabled).
    - Multiple processes can read from/write to the device.
    - A device can only be opened as many times as the maximum allowed
      channel number (see SND_MAXHWCHAN in pcm/sound.h).
    - OSSv4 compatibility aliases are preserved.
    
    Behavior changes:
    
    Only one /dev/dspX device node is created (on attach) for each audio
    device, as opposed to the current /dev/dspX.Y devices created by
    snd_clone. According to the sound(4) man page, devices are not meant to
    be opened through /dev/dspX.Y anyway, so it is best if we do not create
    device nodes for them in the first place. As a result of this, modify
    dsp_oss_audioinfo() to print /dev/dspX in the "ai->devnode", instead of
    /dev/dspX.Y.
    
    Sponsored by:	The FreeBSD Foundation
    MFC after:	2 months
    Reviewed by:	dev_submerge.ch, markj
    Differential Revision:	https://reviews.freebsd.org/D44411

--- sys/dev/sound/pcm/dsp.h
+++ sys/dev/sound/pcm/dsp.h
@@ -33,12 +33,9 @@
 
 extern struct cdevsw dsp_cdevsw;
 
-struct dsp_cdevinfo;
-
+int dsp_make_dev(device_t);
+void dsp_destroy_dev(device_t);
 char *dsp_unit2name(char *, size_t, int);
 int dsp_oss_audioinfo(struct cdev *, oss_audioinfo *);
-
-void dsp_cdevinfo_init(struct snddev_info *);
-void dsp_cdevinfo_flush(struct snddev_info *);
 
 #endif /* !_PCMDSP_H_ */
