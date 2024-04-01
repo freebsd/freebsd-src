4cbe37a29fe1b3f07673d11c26e62c3e729a0744
Eugene Grosbein <eugen@FreeBSD.org>
2024-04-01 10:31:51 +0700

    if_bridge: use IF_MINMTU
    Replace incorrect constant 576 with IF_MINMTU to check for minumum MTU.
    This unbreaks bridging tap interfaces with small mtu.
    
    MFC after:	1 week

--- sys/net/if_bridge.c
+++ sys/net/if_bridge.c
@@ -960,7 +960,7 @@ bridge_ioctl(struct ifnet *ifp, u_long cmd, caddr_t data)
 	case SIOCSIFMTU:
 		oldmtu = sc->sc_ifp->if_mtu;
 
-		if (ifr->ifr_mtu < 576) {
+		if (ifr->ifr_mtu < IF_MINMTU) {
 			error = EINVAL;
 			break;
 		}
