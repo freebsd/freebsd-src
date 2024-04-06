a307629d537ac20d3d5bdd529fb9fd3c9f50fd1b
Dimitry Andric <dim@FreeBSD.org>
2024-04-06 22:15:21 +0200

    Bump __FreeBSD_version for llvm 18.1.3 merge
    PR:		276104
    MFC after:	1 month

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -73,7 +73,7 @@
  * cannot include sys/param.h and should only be updated here.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1500017
+#define __FreeBSD_version 1500018
 
 /*
  * __FreeBSD_kernel__ indicates that this system uses the kernel of FreeBSD,
