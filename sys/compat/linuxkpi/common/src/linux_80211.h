af034dcbb24248f940595d56fab332d008376a99
Bjoern A. Zeeb <bz@FreeBSD.org>
2024-04-06 21:17:58 +0000

    LinuxKPI: 802.11 compile in debugging based on IEEE80211 debugging
    If IEEE80211 debugging is turned on in kernel configs also turn on
    LinuxKPI 802.11 and SKB debugging (sysctls) to have them available.
    
    Sponsored by:	The FreeBSD Foundation
    Differential Revision: https://reviews.freebsd.org/D45820
    
    (cherry picked from commit 59d262fe969af5a8a83f0ccd7586c9bd2c8fb1f3)

--- sys/compat/linuxkpi/common/src/linux_80211.h
+++ sys/compat/linuxkpi/common/src/linux_80211.h
@@ -42,6 +42,12 @@
 #ifndef _LKPI_SRC_LINUX_80211_H
 #define _LKPI_SRC_LINUX_80211_H
 
+#include "opt_wlan.h"
+
+#if defined(IEEE80211_DEBUG) && !defined(LINUXKPI_DEBUG_80211)
+#define	LINUXKPI_DEBUG_80211
+#endif
+
 /* #define	LINUXKPI_DEBUG_80211 */
 
 #ifndef	D80211_TODO
