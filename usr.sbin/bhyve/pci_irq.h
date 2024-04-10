CTSRD-CHERI/cheribsd

bhyve: Support legacy PCI interrupts on arm64
This allows us to remove various #ifdef hacks and enable building more
PCI devices.

Note that a hole is left in the interrupt mapping for the RTC rather
than having the two core devices straddle the PCIe interrupts. QEMU's
virt machine also takes this approach.

usr.sbin/bhyve/pci_irq.h
@@ -32,14 +32,16 @@
 
 struct pci_devinst;
 
+#if defined(__amd64__)
+#include "amd64/pci_irq_md.h"
+#elif defined(__aarch64__)
+#include "aarch64/pci_irq_md.h"
+#else
+#error Unsupported platform
+#endif
+
 void	pci_irq_assert(struct pci_devinst *pi);
 void	pci_irq_deassert(struct pci_devinst *pi);
-void	pci_irq_init(struct vmctx *ctx);
-void	pci_irq_reserve(int irq);
-void	pci_irq_use(int irq);
-int	pirq_alloc_pin(struct pci_devinst *pi);
-int	pirq_irq(int pin);
-uint8_t	pirq_read(int pin);
-void	pirq_write(struct vmctx *ctx, int pin, uint8_t val);
+void	pci_irq_route(struct pci_devinst *pi, struct pci_irq *irq);
 
 #endif

freebsd/freebsd-src

Reviewed by:	jhb
MFC after:	2 weeks
Obtained from:	CheriBSD

usr.sbin/bhyve/pci_irq.h
@@ -32,14 +32,16 @@
 
 struct pci_devinst;
 
+#if defined(__amd64__)
+#include "amd64/pci_irq_machdep.h"
+#elif defined(__aarch64__)
+#include "aarch64/pci_irq_machdep.h"
+#else
+#error Unsupported platform
+#endif
+
 void	pci_irq_assert(struct pci_devinst *pi);
 void	pci_irq_deassert(struct pci_devinst *pi);
-void	pci_irq_init(struct vmctx *ctx);
-void	pci_irq_reserve(int irq);
-void	pci_irq_use(int irq);
-int	pirq_alloc_pin(struct pci_devinst *pi);
-int	pirq_irq(int pin);
-uint8_t	pirq_read(int pin);
-void	pirq_write(struct vmctx *ctx, int pin, uint8_t val);
+void	pci_irq_route(struct pci_devinst *pi, struct pci_irq *irq);
 
 #endif