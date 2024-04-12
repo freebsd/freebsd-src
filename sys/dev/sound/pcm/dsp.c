f91a50ce428cf817fe341bf757782444fb60d909
Christos Margiolis <christos@FreeBSD.org>
2024-04-12 20:34:27 +0200

    sound: Update some LICENSE headers
    The following commits introduced substantial changes to pcm/dsp.c,
    pcm/sndstat.c and pcm/sound.c.
    
    61f1f74171d719adbd613c731134825f199b873e ("sound: Move
    sndstat_prepare_pcm() to pcm/sndstat.c and remove
    sndstat_entry->handler")
    19d63a05182bfc7d857541d479112dfae75aa1d6 ("sound: Get rid of snd_clone
    and use DEVFS_CDEVPRIV(9)")
    
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Reviewed by:	markj
    Differential Revision:	https://reviews.freebsd.org/D44757

--- sys/dev/sound/pcm/dsp.c
+++ sys/dev/sound/pcm/dsp.c
@@ -5,6 +5,10 @@
  * Portions Copyright (c) Ryan Beasley <ryan.beasley@gmail.com> - GSoC 2006
  * Copyright (c) 1999 Cameron Grant <cg@FreeBSD.org>
  * All rights reserved.
+ * Copyright (c) 2024 The FreeBSD Foundation
+ *
+ * Portions of this software were developed by Christos Margiolis
+ * <christos@FreeBSD.org> under sponsorship from the FreeBSD Foundation.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
--- sys/dev/sound/pcm/sound.c
+++ sys/dev/sound/pcm/sound.c
@@ -6,6 +6,10 @@
  * Copyright (c) 1999 Cameron Grant <cg@FreeBSD.org>
  * Copyright (c) 1997 Luigi Rizzo
  * All rights reserved.
+ * Copyright (c) 2024 The FreeBSD Foundation
+ *
+ * Portions of this software were developed by Christos Margiolis
+ * <christos@FreeBSD.org> under sponsorship from the FreeBSD Foundation.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
