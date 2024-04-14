bf7e13e690c32a86fa2cb4aa31343c935dc6cdcb
Dimitry Andric <dim@FreeBSD.org>
2024-04-14 19:38:42 +0200

    sanitizers: mark __elf_aux_vector as weak, to allow linking without libsys
    After libsys was introduced, linking sanitized programs started failing
    with:
    
      # c++ -fsanitize=address main.cc
      ld: error: undefined symbol: __elf_aux_vector
      >>> referenced by sanitizer_linux_libcdep.cpp:950 (/usr/src/contrib/llvm-project/compiler-rt/lib/sanitizer_common/sanitizer_linux_libcdep.cpp:950)
      >>>		    sanitizer_linux_libcdep.o:(__sanitizer::ReExec()) in archive /usr/lib/clang/17/lib/freebsd/libclang_rt.asan-x86_64.a
      c++: error: linker command failed with exit code 1 (use -v to see invocation)
    
    Mark __elf_aux_vector as weak in the internal sanitizer declaration, so
    the linker will accept it at link time. The dynamic linker will then
    take care of the symbol at run time.
    
    Suggestion by:	brooks
    PR:		276104, 277393
    MFC after:	1 month
    
    (cherry picked from commit 8b181c2fbaa8c6b32e5510ef6e3a6d364ff4710e)

--- contrib/llvm-project/compiler-rt/lib/sanitizer_common/sanitizer_linux_libcdep.cpp
+++ contrib/llvm-project/compiler-rt/lib/sanitizer_common/sanitizer_linux_libcdep.cpp
@@ -53,7 +53,7 @@
 // that, it was never implemented. So just define it to zero.
 #    undef MAP_NORESERVE
 #    define MAP_NORESERVE 0
-extern const Elf_Auxinfo *__elf_aux_vector;
+extern const Elf_Auxinfo *__elf_aux_vector __attribute__ ((weak));
 #  endif
 
 #  if SANITIZER_NETBSD
