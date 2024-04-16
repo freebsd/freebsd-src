b10f0061a3cff5e5a8eb3d828ed26f6826629fa4
Brooks Davis <brooks@FreeBSD.org>
2024-04-16 23:26:29 +0100

    lib{c,sys}: normalize export of openat, setcontext, and swapcontext
    List them in the symbol map rather than using the __sym_default to
    expose them.  This will allow later improvements in the stub
    implementations in libc.so.
    
    Reviewed by:	kib
    Differential Revision:	https://reviews.freebsd.org/D44113

--- lib/libc/sys/openat.c
+++ lib/libc/sys/openat.c
@@ -37,7 +37,6 @@
 __weak_reference(__sys_openat, __openat);
 __sym_compat(openat, __impl_openat, FBSD_1.1);
 __weak_reference(openat, __impl_openat);
-__sym_default(openat, openat, FBSD_1.2);
 
 #pragma weak openat
 int
