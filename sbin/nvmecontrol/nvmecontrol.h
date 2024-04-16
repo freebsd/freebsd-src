c3bc471339983669ebee1d2dc7ce2a91adb8b129
Warner Losh <imp@FreeBSD.org>
2024-04-16 16:35:53 -0600

    nvmecontrol: Have to truncate on all 32-bit architectures
    armv7, powerpc, powerpcspe and i386 all lack 128-bit integer
    types. Adjust the comment and #ifdef. I don't think we support nvme on
    any of these other architectures at the moment, but it won't hurt to be
    more precise.
    
    Sponsored by:		Netflix
    Reviewed by:		chuck
    Differential Revision:	https://reviews.freebsd.org/D44650

--- sbin/nvmecontrol/nvmecontrol.h
+++ sbin/nvmecontrol/nvmecontrol.h
@@ -92,12 +92,12 @@ void print_intel_add_smart(const struct nvme_controller_data *cdata __unused, vo
 /* Utility Routines */
 /*
  * 128-bit integer augments to standard values. On i386 this
- * doesn't exist, so we use 64-bit values. So, on 32-bit i386,
+ * doesn't exist, so we use 64-bit values. So, on 32-bit systems,
  * you'll get truncated values until someone implement 128bit
  * ints in software.
  */
 #define UINT128_DIG	39
-#ifdef __i386__
+#ifdef __ILP32__
 typedef uint64_t uint128_t;
 #else
 typedef __uint128_t uint128_t;
