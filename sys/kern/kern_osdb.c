a6861ab068e9030734a5c43d9101a16e66fccb3f
Robert Soule <robert.soule@yale.edu>
2024-04-23 21:42:15 -0400

    fix

--- sys/kern/kern_osdb.c
+++ sys/kern/kern_osdb.c
@@ -23,7 +23,7 @@ int sys_osdb_column_bytes(struct thread *td, struct osdb_column_bytes_args *args
 int sys_osdb_column_bytes16(struct thread *td, struct osdb_column_bytes16_args *args) { return 1; }
 int sys_osdb_column_type(struct thread *td, struct osdb_column_type_args *args) { return 1; }
 
-int freebsd32_osdb_column_int64(struct thread *td, struct osdb_column_int64_args *args) { return 1; }
+int freebsd32_osdb_column_int64(struct thread *td, struct freebsd32_osdb_column_int64_args *args) { return 1; }
 
   
 
