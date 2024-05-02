e002d0a1089d023467342180faeb764064c1c880
Navdeep Parhar <np@FreeBSD.org>
2024-05-02 16:47:18 -0700

    cxgbe(4): sc->port is indexed by port_id and not tx_chan.
    MFC after:	1 week
    Sponsored by:	Chelsio Communications

--- sys/dev/cxgbe/t4_sched.c
+++ sys/dev/cxgbe/t4_sched.c
@@ -334,7 +334,7 @@ bind_txq_to_traffic_class(struct adapter *sc, struct sge_txq *txq, int idx)
 		goto done;
 	}
 
-	tc0 = &sc->port[txq->eq.tx_chan]->sched_params->cl_rl[0];
+	tc0 = &sc->port[txq->eq.port_id]->sched_params->cl_rl[0];
 	if (idx != -1) {
 		/*
 		 * Bind to a different class at index idx.
