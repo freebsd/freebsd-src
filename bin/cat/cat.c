0af77bfb47f11de3d93bda3245ae9938b84044c7
Dag-Erling Smørgrav <des@FreeBSD.org>
2024-05-10 12:59:18 +0200

    cat: Check for lack of success rather than a specific failure.
    MFC after:	3 days
    Sponsored by:	Klara, Inc.
    Reviewed by:	oshogbo
    Differential Revision:	https://reviews.freebsd.org/D45149

--- bin/cat/cat.c
+++ bin/cat/cat.c
@@ -198,7 +198,7 @@ main(int argc, char *argv[])
 		stdout_lock.l_start = 0;
 		stdout_lock.l_type = F_WRLCK;
 		stdout_lock.l_whence = SEEK_SET;
-		if (fcntl(STDOUT_FILENO, F_SETLKW, &stdout_lock) == -1)
+		if (fcntl(STDOUT_FILENO, F_SETLKW, &stdout_lock) != 0)
 			err(EXIT_FAILURE, "stdout");
 	}
 
@@ -266,7 +266,7 @@ scanfiles(char *argv[], int cooked __unused)
 #endif
 		} else {
 #ifndef BOOTSTRAP_CAT
-			if (in_kernel_copy(fd) == -1) {
+			if (in_kernel_copy(fd) != 0) {
 				if (errno == EINVAL || errno == EBADF ||
 				    errno == EISDIR)
 					raw_cat(fd);
@@ -500,12 +500,12 @@ udom_open(const char *path, int flags)
 	switch (flags & O_ACCMODE) {
 	case O_RDONLY:
 		cap_rights_clear(&rights, CAP_WRITE);
-		if (shutdown(fd, SHUT_WR) == -1)
+		if (shutdown(fd, SHUT_WR) != 0)
 			warn(NULL);
 		break;
 	case O_WRONLY:
 		cap_rights_clear(&rights, CAP_READ);
-		if (shutdown(fd, SHUT_RD) == -1)
+		if (shutdown(fd, SHUT_RD) != 0)
 			warn(NULL);
 		break;
 	default:
@@ -513,7 +513,7 @@ udom_open(const char *path, int flags)
 	}
 
 	cap_rights_clear(&rights, CAP_CONNECT, CAP_SHUTDOWN);
-	if (caph_rights_limit(fd, &rights) < 0) {
+	if (caph_rights_limit(fd, &rights) != 0) {
 		serrno = errno;
 		close(fd);
 		errno = serrno;
