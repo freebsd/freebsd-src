4186ba1d9ed314f91f2a080f1295056dac0294db
Christos Margiolis <christos@FreeBSD.org>
2024-05-14 15:42:04 +0200

    snd_hda: Add patch for Lenovo Ideapad 3
    Reported by:	bsd-lists@bsdforge.com
    Sponsored by:	The FreeBSD Foundation
    MFC after:	3 days
    Reviewed by:	emaste
    Differential Revision:	https://reviews.freebsd.org/D45100

--- sys/dev/sound/pci/hda/hdaa_patches.c
+++ sys/dev/sound/pci/hda/hdaa_patches.c
@@ -318,7 +318,8 @@ hdac_pin_patch(struct hdaa_widget *w)
 		}
 	} else if (id == HDA_CODEC_ALC257 &&
 	    (subid == LENOVO_L5AMD_SUBVENDOR ||
-	    subid == LENOVO_L5INTEL_SUBVENDOR)) {
+	    subid == LENOVO_L5INTEL_SUBVENDOR ||
+	    subid == LENOVO_I3_SUBVENDOR)) {
 		switch (nid) {
 		case 20:
 			patch_str = "as=1 seq=0";
