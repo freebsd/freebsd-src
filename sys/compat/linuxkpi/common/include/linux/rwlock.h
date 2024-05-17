freebsd/freebsd-src

commit 5fe0ffa7c3120554a8c82f15af4bec024a5d15c1

linuxkpi: rwlock: Simplify code
Just use a typedef for rwlock_t, no need to create a useless
structure.

Reviewed by:		bz
Sponsored by:		Beckhoff Automation GmbH & Co. KG
Differential Revision:	https://reviews.freebsd.org/D45206

sys/compat/linuxkpi/common/include/linux/rwlock.h
@@ -34,14 +34,12 @@
 #include <sys/rwlock.h>
 #include <sys/libkern.h>
 
-typedef struct {
-	struct rwlock rw;
-} rwlock_t;
+typedef struct rwlock rwlock_t;
 
-#define	read_lock(_l)		rw_rlock(&(_l)->rw)
-#define	write_lock(_l)		rw_wlock(&(_l)->rw)
-#define	read_unlock(_l)		rw_runlock(&(_l)->rw)
-#define	write_unlock(_l)	rw_wunlock(&(_l)->rw)
+#define	read_lock(_l)		rw_rlock(_l)
+#define	write_lock(_l)		rw_wlock(_l)
+#define	read_unlock(_l)		rw_runlock(_l)
+#define	write_unlock(_l)	rw_wunlock(_l)
 #define	read_lock_irq(lock)	read_lock((lock))
 #define	read_unlock_irq(lock)	read_unlock((lock))
 #define	write_lock_irq(lock)	write_lock((lock))
@@ -54,13 +52,6 @@ typedef struct {
     do { read_unlock(lock); } while (0)
 #define	write_unlock_irqrestore(lock, flags)				\
     do { write_unlock(lock); } while (0)
-
-static inline void
-rwlock_init(rwlock_t *lock)
-{
-
-	memset(&lock->rw, 0, sizeof(lock->rw));
-	rw_init_flags(&lock->rw, "lnxrw", RW_NOWITNESS);
-}
+#define	rwlock_init(_l)	rw_init_flags(_l, "lnxrw", RW_NOWITNESS)
 
 #endif	/* _LINUXKPI_LINUX_RWLOCK_H_ */

commit 1e8c7116533750906bd9fd3cc79b7dd984cda0f0

linuxkpi: rwlock: Fix rwlock_init
Some linux code re-init some spinlock so add MTX_NEW to mtx_init.

Reported by:    rlibby
Fixes:          5fe0ffa7c312 ("linuxkpi: rwlock: Simplify code")

sys/compat/linuxkpi/common/include/linux/rwlock.h
@@ -52,6 +52,6 @@ typedef struct rwlock rwlock_t;
     do { read_unlock(lock); } while (0)
 #define	write_unlock_irqrestore(lock, flags)				\
     do { write_unlock(lock); } while (0)
-#define	rwlock_init(_l)	rw_init_flags(_l, "lnxrw", RW_NOWITNESS)
+#define	rwlock_init(_l)	rw_init_flags(_l, "lnxrw", RW_NOWITNESS | RW_NEW)
 
 #endif	/* _LINUXKPI_LINUX_RWLOCK_H_ */