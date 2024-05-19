41af15744be3f5cbedf4ba60a11c85ced76baa9e
Ricardo Branco <rbranco@suse.de>
2024-05-19 11:37:12 +0200

    linux: Update linux manpage to mention mqueuefs
    Reviewed by: imp, kib
    Pull Request: https://github.com/freebsd/freebsd-src/pull/1248

--- sys/sys/syscallsubr.h
+++ sys/sys/syscallsubr.h
@@ -213,6 +213,15 @@ int	kern_kqueue(struct thread *td, int flags, struct filecaps *fcaps);
 int	kern_kldload(struct thread *td, const char *file, int *fileid);
 int	kern_kldstat(struct thread *td, int fileid, struct kld_file_stat *stat);
 int	kern_kldunload(struct thread *td, int fileid, int flags);
+int	kern_kmq_notify(struct thread *, int, struct sigevent *);
+int	kern_kmq_open(struct thread *, const char *, int, mode_t,
+		const struct mq_attr *);
+int	kern_kmq_setattr(struct thread *, int, const struct mq_attr *,
+		struct mq_attr *);
+int	kern_kmq_timedreceive(struct thread *, int, char *,
+		size_t, unsigned int *, const struct timespec *);
+int	kern_kmq_timedsend(struct thread *td, int, const char *,
+		size_t, unsigned int, const struct timespec *);
 int	kern_linkat(struct thread *td, int fd1, int fd2, const char *path1,
 	    const char *path2, enum uio_seg segflg, int flag);
 int	kern_listen(struct thread *td, int s, int backlog);
@@ -386,15 +395,6 @@ int	kern_writev(struct thread *td, int fd, struct uio *auio);
 int	kern_socketpair(struct thread *td, int domain, int type, int protocol,
 	    int *rsv);
 int	kern_unmount(struct thread *td, const char *path, int flags);
-int	kern_kmq_notify(struct thread *, int, struct sigevent *);
-int	kern_kmq_open(struct thread *, const char *, int, mode_t,
-		const struct mq_attr *);
-int	kern_kmq_setattr(struct thread *, int, const struct mq_attr *,
-		struct mq_attr *);
-int	kern_kmq_timedreceive(struct thread *, int, char *,
-		size_t, unsigned int *, const struct timespec *);
-int	kern_kmq_timedsend(struct thread *td, int, const char *,
-		size_t, unsigned int, const struct timespec *);
 
 /* flags for kern_sigaction */
 #define	KSA_OSIGSET	0x0001	/* uses osigact_t */
