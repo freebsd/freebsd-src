a926bf9254b2a5a9c3b60cf1760fe7746584308b
Colin Percival <cperciva@FreeBSD.org>
2024-05-20 10:42:49 -0700

    Modernize DVD package set in preparation for 14.1
    [MFC note: We're making the same changes for 13.4.]
    
    Remove archivers/unzip (now in base) and emulators/linux_base-c7 (old
    and unlikely to be useful without other linux packages being installed),
    ports-mgmt/portmaster (now largely obsolete and discouraged in favour
    of using pkg and binary packages) and x11-drivers/xf86-video-vmware
    (questionably useful).
    
    Replace devel/git with devel/git@lite (sufficient for most purposes),
    and adjust the "ensure the ports exist to sanitize the list" code to
    ignore the @lite part when checking that /usr/ports/devel/git exists.
    
    Add sysutils/seatd and x11-wm/sway for wayland support.
    
    MFC after:	1 minute
    Differential Revision:	https://reviews.freebsd.org/D45278

--- release/scripts/pkg-stage.sh
+++ release/scripts/pkg-stage.sh
@@ -12,28 +12,26 @@ export ROOTDIR="$PWD/dvd"
 export PKGCMD="/usr/sbin/pkg -d --rootdir ${ROOTDIR}"
 export PORTSDIR="${PORTSDIR:-/usr/ports}"
 
-_DVD_PACKAGES="archivers/unzip
-devel/git
-emulators/linux_base-c7
+_DVD_PACKAGES="devel/git@lite
 graphics/drm-kmod
 graphics/drm-510-kmod
 misc/freebsd-doc-all
 net/mpd5
 net/rsync
 ports-mgmt/pkg
-ports-mgmt/portmaster
 shells/bash
 shells/zsh
 security/sudo
 sysutils/screen
+sysutils/seatd
 sysutils/tmux
 www/firefox
 www/links
-x11-drivers/xf86-video-vmware
 x11/gnome
 x11/kde5
 x11/sddm
-x11/xorg"
+x11/xorg
+x11-wm/sway"
 
 # If NOPORTS is set for the release, do not attempt to build pkg(8).
 if [ ! -f ${PORTSDIR}/Makefile ]; then
@@ -60,10 +58,10 @@ fi
 # Ensure the ports listed in _DVD_PACKAGES exist to sanitize the
 # final list.
 for _P in ${_DVD_PACKAGES}; do
-	if [ -d "${PORTSDIR}/${_P}" ]; then
+	if [ -d "${PORTSDIR}/${_P%%@*}" ]; then
 		DVD_PACKAGES="${DVD_PACKAGES} ${_P}"
 	else
-		echo "*** Skipping nonexistent port: ${_P}"
+		echo "*** Skipping nonexistent port: ${_P%%@*}"
 	fi
 done
 
