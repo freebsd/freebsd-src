5a3f11076674d9e6cb79b6e5bce8ca3f58367867
Cy Schubert <cy@FreeBSD.org>
2024-05-23 14:02:06 -0700

    sys/netinet/icmp6.h: Fix build
    Fix stdint.h file not found.
    
    Fixes: 		fbf26747ba66

--- sys/netinet/icmp6.h
+++ sys/netinet/icmp6.h
@@ -63,7 +63,7 @@
 #ifndef _NETINET_ICMP6_H_
 #define _NETINET_ICMP6_H_
 
-#include <stdint.h>
+#include <sys/stdint.h>
 
 #define ICMPV6_PLD_MAXLEN	1232	/* IPV6_MMTU - sizeof(struct ip6_hdr)
 					   - sizeof(struct icmp6_hdr) */
