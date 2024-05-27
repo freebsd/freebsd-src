d28bbfa2715a45c841e0eeec38d7f7b73513c66e
Warner Losh <imp@FreeBSD.org>
2024-05-27 11:07:26 -0600

    localedef: Bootstrap better
    Declare these functions in a macro-safe way instead of relying on
    ifdefs.
    
    Sponsored by:		Netflix

--- usr.bin/localedef/bootstrap/ctype.h
+++ usr.bin/localedef/bootstrap/ctype.h
@@ -40,17 +40,14 @@
 /*
  * On macOS isxdigit(), etc are defined in _ctype.h, but we have to include the
  * target _ctype.h so that localedef uses the correct values.
- * To fix macOS bootstrap, provide a declaration of isxdigit(), etc. here.
- * However, only do so if they are not already define as a macro (as is the
- * case on Linux).
+ * To fix macOS bootstrap, provide a declaration of isxdigit(), etc. here in
+ * a macro-safe way.
  */
-#ifndef isxdigit
 __BEGIN_DECLS
-int	isalpha(int);
-int	isascii(int);
-int	isdigit(int);
-int	isgraph(int);
-int	islower(int);
-int	isxdigit(int);
+int	(isalpha)(int);
+int	(isascii)(int);
+int	(isdigit)(int);
+int	(isgraph)(int);
+int	(islower)(int);
+int	(isxdigit)(int);
 __END_DECLS
-#endif
