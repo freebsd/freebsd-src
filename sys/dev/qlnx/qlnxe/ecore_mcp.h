0649f8f679b9aade17a85e83b53e82b64123b39d
Keith Reynolds <keith.reynolds@hpe.com>
2024-05-27 23:13:10 -0700

    qlnxe: Fix multiple locking issues
    Multiple issues are reported with WITNESS and code inspection of the
    locking and lock initialization.
    
    PR:		278084
    MFC after:	1 week

--- sys/dev/qlnx/qlnxe/ecore_mcp.h
+++ sys/dev/qlnx/qlnxe/ecore_mcp.h
@@ -51,10 +51,10 @@ struct ecore_mcp_info {
 	/* List for mailbox commands which were sent and wait for a response */
 	osal_list_t cmd_list;
 
-	/* Spinlock used for protecting the access to the mailbox commands list
+	/* Lock used for protecting the access to the mailbox commands list
 	 * and the sending of the commands.
 	 */
-	osal_spinlock_t cmd_lock;
+	osal_mutex_t cmd_lock;
 
 	/* Flag to indicate whether sending a MFW mailbox command is blocked */
 	bool b_block_cmd;
@@ -62,7 +62,7 @@ struct ecore_mcp_info {
 	/* Spinlock used for syncing SW link-changes and link-changes
 	 * originating from attention context.
 	 */
-	osal_spinlock_t link_lock;
+	osal_mutex_t link_lock;
 
 	/* Address of the MCP public area */
 	u32 public_base;
