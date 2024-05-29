freebsd/freebsd-src

commit 660ec326999b330d8d089dca842a3144b39cf213

linux: implement PR_CHILD_SET_SUBREAPER
Reviewed by: imp, dchagin
Pull Request: https://github.com/freebsd/freebsd-src/pull/1260

sys/compat/linux/linux_misc.c
@@ -1819,6 +1819,14 @@ linux_prctl(struct thread *td, struct linux_prctl_args *args)
 #endif
 		error = EINVAL;
 		break;
+	case LINUX_PR_SET_CHILD_SUBREAPER:
+		if (args->arg2 == 0){
+			return (kern_procctl(td, P_PID, 0, PROC_REAP_RELEASE,
+			    NULL));
+		}
+
+		return (kern_procctl(td, P_PID, 0, PROC_REAP_ACQUIRE,
+		    NULL));
 	case LINUX_PR_SET_NO_NEW_PRIVS:
 		arg = args->arg2 == 1 ?
 		    PROC_NO_NEW_PRIVS_ENABLE : PROC_NO_NEW_PRIVS_DISABLE;

sys/compat/linux/linux_misc.h
@@ -57,6 +57,7 @@
 #define	LINUX_PR_GET_SECCOMP	21
 #define	LINUX_PR_SET_SECCOMP	22
 #define	LINUX_PR_CAPBSET_READ	23
+#define LINUX_PR_SET_CHILD_SUBREAPER 36
 #define	LINUX_PR_SET_NO_NEW_PRIVS	38
 #define	LINUX_PR_SET_PTRACER	1499557217
 

commit ed5bd81356a77ad0281ddf272f4d0451954958d1

minor style tweak.
checkstyle9 doesn't check for this construct...

Fixes: 660ec326999b

sys/compat/linux/linux_misc.c
@@ -1820,7 +1820,7 @@ linux_prctl(struct thread *td, struct linux_prctl_args *args)
 		error = EINVAL;
 		break;
 	case LINUX_PR_SET_CHILD_SUBREAPER:
-		if (args->arg2 == 0){
+		if (args->arg2 == 0) {
 			return (kern_procctl(td, P_PID, 0, PROC_REAP_RELEASE,
 			    NULL));
 		}