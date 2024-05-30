57f268ccfc662d274d82cfc4e0e0ecb2db5d616e
Zhenlei Huang <zlei.huang@gmail.com>
2024-05-31 00:58:20 +0800

    zfs: cherry-pick change from openzfs
    Vendor PR:
      #16210 FreeBSD: Add const qualifier to members of struct
             opensolaris_utsname
    
    Obtained from:	OpenZFS
    OpenZFS commit:	e2357561b9499296bff758afe4868dbc39735675
    
    Note: this is required by the following up MFC [1]. No functional change.
    
    1. 356be1348dac kernel: Make some compile time constant variables const

--- sys/contrib/openzfs/include/os/freebsd/spl/sys/misc.h
+++ sys/contrib/openzfs/include/os/freebsd/spl/sys/misc.h
@@ -45,11 +45,11 @@
 #define	F_SEEK_HOLE	FIOSEEKHOLE
 
 struct opensolaris_utsname {
-	char	*sysname;
-	char	*nodename;
-	char	*release;
-	char	version[32];
-	char	*machine;
+	const char	*sysname;
+	const char	*nodename;
+	const char	*release;
+	char		version[32];
+	const char	*machine;
 };
 
 extern char hw_serial[11];
