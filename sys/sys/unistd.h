freebsd/freebsd-src

commit 99470353249ecfc36c96a1a89b2bc2ddf75236fb

FreeBSD bump POSIX_VERISON to 200809L
FreeBSD now provides the overwhelming majority of the new interfaces for
POSIX_VERSION 200809 (aka POSIX.1-2017 aka IEEE Std 1003.1™-2017, The
Open Group Technical Standard Base Specifications, Issue 7 and IEEE Std
1003.1™-2017 (Revision of IEEE Std 1003.1-2008))

Exp Run PR: 249444
Sponsored by: Netflix

sys/sys/unistd.h
@@ -93,7 +93,7 @@
 #endif
 
 /* Define the POSIX.1 version we target for compliance. */
-#define	_POSIX_VERSION		200112L
+#define	_POSIX_VERSION		200808L
 
 /* access function */
 #define	F_OK		0	/* test for existence of file */

commit ac02a3d4ffd55e732aa13a84c596898d514d9725

sys/unistd.h: Use correct POSIX_VERION of 200809L
200809L was tested, but I recreated the patch rather than using the
actual patch and typo'd to 200808 by mistake. Also, the PR in original
was the patch ID not the bug ID.

Pr Exp Run: 277915
Fixes: 99470353249e
Sponsored by: Netflix

sys/sys/unistd.h
@@ -93,7 +93,7 @@
 #endif
 
 /* Define the POSIX.1 version we target for compliance. */
-#define	_POSIX_VERSION		200808L
+#define	_POSIX_VERSION		200809L
 
 /* access function */
 #define	F_OK		0	/* test for existence of file */