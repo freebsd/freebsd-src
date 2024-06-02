f1d73aacdc47529310e2302094685295c032e28f
Alan Cox <alc@FreeBSD.org>
2024-06-02 03:56:47 -0500

    pmap: Skip some superpage promotion attempts that will fail
    Implement a simple heuristic to skip pointless promotion attempts by
    pmap_enter_quick_locked() and moea64_enter().  Specifically, when
    vm_fault() calls pmap_enter_quick() to map neighboring pages at the end
    of a copy-on-write fault, there is no point in attempting promotion in
    pmap_enter_quick_locked() and moea64_enter().  Promotion will fail
    because the base pages have differing protection.
    
    Reviewed by:	kib
    Differential Revision:	https://reviews.freebsd.org/D45431
    MFC after:	1 week

--- sys/vm/vm.h
+++ sys/vm/vm.h
@@ -76,6 +76,7 @@ typedef u_char vm_prot_t;	/* protection codes */
 #define	VM_PROT_COPY		((vm_prot_t) 0x08)	/* copy-on-read */
 #define	VM_PROT_PRIV_FLAG	((vm_prot_t) 0x10)
 #define	VM_PROT_FAULT_LOOKUP	VM_PROT_PRIV_FLAG
+#define	VM_PROT_NO_PROMOTE	VM_PROT_PRIV_FLAG
 #define	VM_PROT_QUICK_NOFAULT	VM_PROT_PRIV_FLAG	/* same to save bits */
 
 #define	VM_PROT_ALL		(VM_PROT_READ|VM_PROT_WRITE|VM_PROT_EXECUTE)
