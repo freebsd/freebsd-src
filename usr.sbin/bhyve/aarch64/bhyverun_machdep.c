ee2adeb70faa40ac0c3988569adbcd8ee0f692d9
Mark Johnston <markj@FreeBSD.org>
2024-06-04 15:03:17 -0400

    bhyve: Add arm64 support to the gdb stub
    - Add -G to the arm64 getopt handler.
    - Add static register definitions and extensible XML definitions.
    - Provide definitions for MD bits such as breakpoint encoding and
      length.
    - Ensure that bhyve re-injects breakpoint exceptions that it is not
      responsible for.
    
    Reviewed by:	andrew
    Sponsored by:	Innovate UK
    Differential Revision:	https://reviews.freebsd.org/D44740

--- usr.sbin/bhyve/aarch64/bhyverun_machdep.c
+++ usr.sbin/bhyve/aarch64/bhyverun_machdep.c
@@ -99,6 +99,7 @@ bhyve_usage(int code)
 	    "       -C: include guest memory in core file\n"
 	    "       -c: number of CPUs and/or topology specification\n"
 	    "       -D: destroy on power-off\n"
+	    "       -G: start a debug server\n"
 	    "       -h: help\n"
 	    "       -k: key=value flat config file\n"
 	    "       -m: memory size\n"
@@ -119,7 +120,7 @@ bhyve_optparse(int argc, char **argv)
 	const char *optstr;
 	int c;
 
-	optstr = "hCDSWk:f:o:p:c:s:m:U:";
+	optstr = "hCDSWk:f:o:p:G:c:s:m:U:";
 	while ((c = getopt(argc, argv, optstr)) != -1) {
 		switch (c) {
 		case 'c':
@@ -133,6 +134,9 @@ bhyve_optparse(int argc, char **argv)
 			break;
 		case 'D':
 			set_config_bool("destroy_on_poweroff", true);
+			break;
+		case 'G':
+			bhyve_parse_gdb_options(optarg);
 			break;
 		case 'k':
 			bhyve_parse_simple_config_file(optarg);
