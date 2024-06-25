freebsd/freebsd-src

commit 35114f24606f5081cd7d48c652ddeda0058aef39

p9fs: move NULL check immediately after allocation
Reported by:	Shawn Webb (HardenedBSD)
Reviewed by:	dfr
Sponsored by:	The FreeBSD Foundation
Differential Revision: https://reviews.freebsd.org/D45719

sys/dev/virtio/p9fs/virtio_p9fs.c
@@ -332,6 +332,11 @@ vt9p_attach(device_t dev)
 	cv_init(&chan->submit_cv, "Conditional variable for submit queue" );
 	chan->max_nsegs = MAX_SUPPORTED_SGS;
 	chan->vt9p_sglist = sglist_alloc(chan->max_nsegs, M_NOWAIT);
+	if (chan->vt9p_sglist == NULL) {
+		error = ENOMEM;
+		P9_DEBUG(ERROR, "%s: Cannot allocate sglist\n", __func__);
+		goto out;
+	}
 
 	/* Negotiate the features from the host */
 	virtio_set_feature_desc(dev, virtio_9p_feature_desc);
@@ -366,12 +371,6 @@ vt9p_attach(device_t dev)
 	tree = device_get_sysctl_tree(dev);
 	SYSCTL_ADD_STRING(ctx, SYSCTL_CHILDREN(tree), OID_AUTO, "p9fs_mount_tag",
 	    CTLFLAG_RD, chan->mount_tag, 0, "Mount tag");
-
-	if (chan->vt9p_sglist == NULL) {
-		error = ENOMEM;
-		P9_DEBUG(ERROR, "%s: Cannot allocate sglist\n", __func__);
-		goto out;
-	}
 
 	/* We expect one virtqueue, for requests. */
 	error = vt9p_alloc_virtqueue(chan);

commit 72dc4e54546e3dfeaf91666477a9a8e5ff364232

p9fs: use M_WAITOK where appropriate
device_attach routines are allowed to sleep, and this routine already
has other M_WAITOK allocations.

Reported by:	markj
Reviewed by:	markj
Fixes: 35114f24606f ("p9fs: move NULL check immediately after alloc...")
Sponsored by:	The FreeBSD Foundation
Differential Revision: https://reviews.freebsd.org/D45721

sys/dev/virtio/p9fs/virtio_p9fs.c
@@ -331,12 +331,7 @@ vt9p_attach(device_t dev)
 	/* Initialize the condition variable */
 	cv_init(&chan->submit_cv, "Conditional variable for submit queue" );
 	chan->max_nsegs = MAX_SUPPORTED_SGS;
-	chan->vt9p_sglist = sglist_alloc(chan->max_nsegs, M_NOWAIT);
-	if (chan->vt9p_sglist == NULL) {
-		error = ENOMEM;
-		P9_DEBUG(ERROR, "%s: Cannot allocate sglist\n", __func__);
-		goto out;
-	}
+	chan->vt9p_sglist = sglist_alloc(chan->max_nsegs, M_WAITOK);
 
 	/* Negotiate the features from the host */
 	virtio_set_feature_desc(dev, virtio_9p_feature_desc);