freebsd/freebsd-src

commit 9b33b154b531606eccfc091faf50467eca0573f1

- Add the interrupt vector number to intr_event_create so MI code can    lookup hard interrupt events by number.  Ignore the irq# for soft intrs.  - Add support to cpuset for binding hardware interrupts.  This has the    side effect of binding any ithread associated with the hard interrupt.    As per restrictions imposed by MD code we can only bind interrupts to    a single cpu presently.  Interrupts can be 'unbound' by binding them    to all cpus.
Reviewed by:	jhb
Sponsored by:	Nokia

sys/powerpc/powerpc/intr_machdep.c
@@ -226,7 +226,7 @@ powerpc_setup_intr(const char *name, u_int irq, driver_filter_t filter,
 		return (ENOMEM);
 
 	if (i->event == NULL) {
-		error = intr_event_create(&i->event, (void *)irq, 0,
+		error = intr_event_create(&i->event, (void *)irq, 0, irq,
 		    powerpc_intr_mask, powerpc_intr_unmask, powerpc_intr_eoi,
 		    NULL, "irq%u:", irq);
 		if (error)

sys/amd64/amd64/intr_machdep.c
@@ -139,7 +139,7 @@ intr_register_source(struct intsrc *isrc)
 	vector = isrc->is_pic->pic_vector(isrc);
 	if (interrupt_sources[vector] != NULL)
 		return (EEXIST);
-	error = intr_event_create(&isrc->is_event, isrc, 0,
+	error = intr_event_create(&isrc->is_event, isrc, 0, vector,
 	    intr_disable_src, (mask_fn)isrc->is_pic->pic_enable_source,
 	    (mask_fn)isrc->is_pic->pic_eoi_source, intr_assign_cpu, "irq%d:",
 	    vector);

sys/i386/i386/intr_machdep.c
@@ -130,7 +130,7 @@ intr_register_source(struct intsrc *isrc)
 	vector = isrc->is_pic->pic_vector(isrc);
 	if (interrupt_sources[vector] != NULL)
 		return (EEXIST);
-	error = intr_event_create(&isrc->is_event, isrc, 0,
+	error = intr_event_create(&isrc->is_event, isrc, 0, vector,
 	    intr_disable_src, (mask_fn)isrc->is_pic->pic_enable_source,
 	    (mask_fn)isrc->is_pic->pic_eoi_source, intr_assign_cpu, "irq%d:",
 	    vector);

sys/sparc64/sparc64/intr_machdep.c
@@ -282,7 +282,7 @@ intr_controller_register(int vec, const struct intr_controller *ic,
 	 * CPU as long as the source of a level sensitive interrupt is
 	 * not cleared.
 	 */
-	error = intr_event_create(&ie, iv, 0, NULL, intr_enable_eoi,
+	error = intr_event_create(&ie, iv, 0, vec, NULL, intr_enable_eoi,
 	    intr_enable_eoi, NULL, "vec%d:", vec);
 	if (error != 0)
 		return (error);

sys/sun4v/sun4v/intr_machdep.c
@@ -307,8 +307,8 @@ inthand_add(const char *name, int vec, driver_filter_t *filt,
 	ie = iv->iv_event;
 	mtx_unlock_spin(&intr_table_lock);
 	if (ie == NULL) {
-		errcode = intr_event_create(&ie, (void *)(intptr_t)vec, 0, NULL,
-		    intr_enable, intr_enable, NULL, "vec%d:", vec);
+		errcode = intr_event_create(&ie, (void *)(intptr_t)vec, 0, vec,
+		    NULL, intr_enable, intr_enable, NULL, "vec%d:", vec);
 		if (errcode)
 			return (errcode);
 		mtx_lock_spin(&intr_table_lock);

sys/arm/arm/intr.c
@@ -70,7 +70,7 @@ arm_setup_irqhandler(const char *name, driver_filter_t *filt,
 		return;
 	event = intr_events[irq];
 	if (event == NULL) {
-		error = intr_event_create(&event, (void *)irq, 0,
+		error = intr_event_create(&event, (void *)irq, 0, irq,
 		    (mask_fn)arm_mask_irq, (mask_fn)arm_unmask_irq,
 		    NULL, NULL, "intr%d:", irq);
 		if (error)

sys/ia64/ia64/interrupt.c
@@ -340,7 +340,7 @@ ia64_setup_intr(const char *name, int irq, driver_filter_t filter,
 			return (ENOMEM);
 
 		error = intr_event_create(&i->event, (void *)(uintptr_t)vector,
-		    0, ia64_intr_mask, ia64_intr_unmask, ia64_intr_eoi,
+		    0, vector, ia64_intr_mask, ia64_intr_unmask, ia64_intr_eoi,
 		    NULL, "irq%u:", irq);
 		if (error) {
 			free(i, M_DEVBUF);

sys/kern/kern_cpuset.c
@@ -412,6 +412,8 @@ cpuset_which(cpuwhich_t which, id_t id, struct proc **pp, struct thread **tdp,
 			return (0);
 		}
 		return (ESRCH);
+	case CPU_WHICH_IRQ:
+		return (0);
 	default:
 		return (EINVAL);
 	}
@@ -760,6 +762,8 @@ cpuset_getid(struct thread *td, struct cpuset_getid_args *uap)
 		break;
 	case CPU_WHICH_CPUSET:
 		break;
+	case CPU_WHICH_IRQ:
+		return (EINVAL);
 	}
 	switch (uap->level) {
 	case CPU_LEVEL_ROOT:
@@ -820,6 +824,9 @@ cpuset_getaffinity(struct thread *td, struct cpuset_getaffinity_args *uap)
 			break;
 		case CPU_WHICH_CPUSET:
 			break;
+		case CPU_WHICH_IRQ:
+			error = EINVAL;
+			goto out;
 		}
 		if (uap->level == CPU_LEVEL_ROOT)
 			nset = cpuset_refroot(set);
@@ -844,6 +851,9 @@ cpuset_getaffinity(struct thread *td, struct cpuset_getaffinity_args *uap)
 			break;
 		case CPU_WHICH_CPUSET:
 			CPU_COPY(&set->cs_mask, mask);
+			break;
+		case CPU_WHICH_IRQ:
+			error = intr_getaffinity(uap->id, mask);
 			break;
 		}
 		break;
@@ -921,6 +931,9 @@ cpuset_setaffinity(struct thread *td, struct cpuset_setaffinity_args *uap)
 			break;
 		case CPU_WHICH_CPUSET:
 			break;
+		case CPU_WHICH_IRQ:
+			error = EINVAL;
+			goto out;
 		}
 		if (uap->level == CPU_LEVEL_ROOT)
 			nset = cpuset_refroot(set);
@@ -945,6 +958,9 @@ cpuset_setaffinity(struct thread *td, struct cpuset_setaffinity_args *uap)
 				error = cpuset_modify(set, mask);
 				cpuset_rel(set);
 			}
+			break;
+		case CPU_WHICH_IRQ:
+			error = intr_setaffinity(uap->id, mask);
 			break;
 		default:
 			error = EINVAL;

commit 5bde9b4d44ea64a39f33c862cea962d47a0640a3

intr/powerpc: switch to vector value for interrupt core "irq"
The interrupt core "irq" is meant to be an index into a dense table of
interrupts.  This better maps to the PowerPC interrupt vector, than the
PowerPC interrupt identifier.  As such switch to ->vector for the
intr_event_create() call.

Fixes: 9b33b154b53 ("Add the interrupt vector number to intr_event_create")

sys/powerpc/powerpc/intr_machdep.c
@@ -522,7 +522,7 @@ powerpc_setup_intr(const char *name, u_int irq, driver_filter_t filter,
 		return (ENOMEM);
 
 	if (i->event == NULL) {
-		error = intr_event_create(&i->event, (void *)i, 0, irq,
+		error = intr_event_create(&i->event, (void *)i, 0, i->vector,
 		    powerpc_intr_pre_ithread, powerpc_intr_post_ithread,
 		    powerpc_intr_eoi, powerpc_assign_intr_cpu, "irq%u:", irq);
 		if (error)