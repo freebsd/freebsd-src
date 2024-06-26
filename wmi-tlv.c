e05a7642395e7a6cbf1876350e96bd80e6292cd4
Bjoern A. Zeeb <bz@FreeBSD.org>
2024-06-26 22:03:19 +0000

    ath10k: update Atheros/QCA's ath10k driver
    This version is based on
    https://git.kernel.org/pub/scm/linux/kernel/git/wireless/wireless-testing.git
    2a220a15be657a24868368892e3e2caba2115283 (wt-2023-08-06)
    and was committed to FreeBSD main as
    07724ba62b4c432ea04dce9465a5ab6e2c3f5a0d.

--- wmi-tlv.c
+++ wmi-tlv.c
@@ -205,7 +205,7 @@ static int ath10k_wmi_tlv_event_bcn_tx_status(struct ath10k *ar,
 	}
 
 	arvif = ath10k_get_arvif(ar, vdev_id);
-	if (arvif && arvif->is_up && arvif->vif->csa_active)
+	if (arvif && arvif->is_up && arvif->vif->bss_conf.csa_active)
 		ieee80211_queue_work(ar->hw, &arvif->ap_csa_work);
 
 	kfree(tb);
@@ -584,7 +584,14 @@ static void ath10k_wmi_event_tdls_peer(struct ath10k *ar, struct sk_buff *skb)
 			ath10k_warn(ar, "did not find station from tdls peer event");
 			goto exit;
 		}
+
 		arvif = ath10k_get_arvif(ar, __le32_to_cpu(ev->vdev_id));
+		if (!arvif) {
+			ath10k_warn(ar, "no vif for vdev_id %d found",
+				    __le32_to_cpu(ev->vdev_id));
+			goto exit;
+		}
+
 		ieee80211_tdls_oper_request(
 					arvif->vif, station->addr,
 					NL80211_TDLS_TEARDOWN,
