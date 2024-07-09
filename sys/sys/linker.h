109585704978df5fef8377dc458cb18b18e15c02
Emmanuel Vadot <manu@FreeBSD.org>
2024-07-09 14:37:08 +0200

    linker: Add MODINFOMD_SPLASH type
    This mod type and structure holds the splash screen data and metadata.
    
    Differential Revision:	https://reviews.freebsd.org/D45930
    Reviewed by:		tsoome
    Sponsored by:		Beckhoff Automation GmbH & Co. KG

--- sys/sys/linker.h
+++ sys/sys/linker.h
@@ -248,6 +248,7 @@ void linker_kldload_unbusy(int flags);
 #define MODINFOMD_FW_HANDLE	0x000c		/* Firmware dependent handle */
 #define MODINFOMD_KEYBUF	0x000d		/* Crypto key intake buffer */
 #define MODINFOMD_FONT		0x000e		/* Console font */
+#define MODINFOMD_SPLASH	0x000f		/* Console splash screen */
 #define MODINFOMD_NOCOPY	0x8000		/* don't copy this metadata to the kernel */
 
 #define MODINFOMD_DEPLIST	(0x4001 | MODINFOMD_NOCOPY)	/* depends on */
