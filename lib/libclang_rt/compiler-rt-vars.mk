97dbe3e7ace2c222b18f4eb787de91ee44192e76
Andrew Turner <andrew@FreeBSD.org>
2024-07-12 11:28:50 +0000

    libclang_rt: Simplify the arm check
    We just need to check we are building for arm.
    
    Reviewed by:	manu, emaste
    Sponsored by:	Arm Ltd
    Differential Revision:	https://reviews.freebsd.org/D45644

--- lib/libclang_rt/compiler-rt-vars.mk
+++ lib/libclang_rt/compiler-rt-vars.mk
@@ -5,7 +5,7 @@ SANITIZER_SHAREDIR=		${CLANGDIR}/share
 
 # armv[67] is a bit special since we allow a soft-floating version via
 # CPUTYPE matching *soft*. This variant may not actually work though.
-.if ${MACHINE_ARCH:Marmv[67]*} != "" && \
+.if ${MACHINE_CPUARCH} == "arm" && \
     (!defined(CPUTYPE) || ${CPUTYPE:M*soft*} == "")
 CRTARCH?=	armhf
 .else
