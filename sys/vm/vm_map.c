040600428d8286647115342a98227d8572a4645d
Alan Cox <alc@FreeBSD.org>
2024-07-13 14:40:43 -0500

    vm ASLR: Handle VM_NRESERV_LEVEL == 0
    Some flavors of powerpc don't enable superpage reservations.
    
    Fixes: c101ca7dc10d ("arm64: Support the L3 ATTR_CONTIGUOUS page ...")

--- sys/vm/vm_map.c
+++ sys/vm/vm_map.c
@@ -1993,7 +1993,7 @@ out:
 	return (result);
 }
 
-#if VM_NRESERVLEVEL == 1
+#if VM_NRESERVLEVEL <= 1
 static const int aslr_pages_rnd_64[2] = {0x1000, 0x10};
 static const int aslr_pages_rnd_32[2] = {0x100, 0x4};
 #elif VM_NRESERVLEVEL == 2
