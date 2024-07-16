664b9fcb1c051c17ba11d1e5e8a1db9938d76bd5
Olivier Certner <olce@FreeBSD.org>
2024-07-16 22:37:44 +0200

    cred: Separate constant for the number of inlined groups
    CRED_SMALLGROUPS_NB now holds the number of inlined groups in field
    'cr_smallgroups'.  XU_NGROUPS stays the number of groups allowed in
    'struct xucred'.  The first is an implementation detail, while the
    second is part of a public interface.  All mentions of XU_NGROUPS in the
    tree have been reviewed and only those concerning the implementation
    detail have been changed to use CRED_SMALLGROUPS_NB (they were all in
    'kern_prot.c').
    
    No functional change, as CRED_SMALLGROUPS_NB is set to 16, the same
    value as XU_NGROUPS.
    
    Reviewed by:    mhorne (slightly different version)
    Approved by:    markj (mentor)
    MFC after:      3 days
    Differential Revision:  https://reviews.freebsd.org/D46911

--- sys/sys/ucred.h
+++ sys/sys/ucred.h
@@ -42,6 +42,14 @@ struct loginclass;
 
 #define	XU_NGROUPS	16
 
+#if defined(_KERNEL) || defined(_WANT_UCRED)
+/*
+ * Number of groups inlined in 'struct ucred'.  It must stay reasonably low as
+ * it is also used by some functions to allocate an array of this size on the
+ * stack.
+ */
+#define	CRED_SMALLGROUPS_NB	16
+
 /*
  * Credentials.
  *
@@ -55,7 +63,6 @@ struct loginclass;
  *
  * See "Credential management" comment in kern_prot.c for more information.
  */
-#if defined(_KERNEL) || defined(_WANT_UCRED)
 struct ucred {
 	struct mtx cr_mtx;
 	long	cr_ref;			/* (c) reference count */
@@ -78,7 +85,8 @@ struct ucred {
 	struct label	*cr_label;	/* MAC label */
 	gid_t	*cr_groups;		/* groups */
 	int	cr_agroups;		/* Available groups */
-	gid_t   cr_smallgroups[XU_NGROUPS];	/* storage for small groups */
+	/* storage for small groups */
+	gid_t   cr_smallgroups[CRED_SMALLGROUPS_NB];
 };
 #define	NOCRED	((struct ucred *)0)	/* no credential available */
 #define	FSCRED	((struct ucred *)-1)	/* filesystem credential */
