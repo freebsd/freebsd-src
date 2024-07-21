a9a364864ebbb3f92cf55583affbea311f29afa1
Vladimir Kondratyev <wulf@FreeBSD.org>
2024-07-21 16:10:44 +0300

    LinuxKPI: Fix resource leak on pci_iounmap-ing of PCI BAR
    If the resource was allocated with want_iomap_res flag set.
    
    Sponsored by:	Serenity CyberSecurity, LLC
    MFC after:	1 week
    Reviewed by:	manu, bz
    Differential Revision:	https://reviews.freebsd.org/D45905
    
    (cherry picked from commit 14fc33ea3a0571ca70e609b6f9e67db39f7c6140)

--- sys/compat/linuxkpi/common/src/linux_pci.c
+++ sys/compat/linuxkpi/common/src/linux_pci.c
@@ -781,12 +781,18 @@ void
 linuxkpi_pci_iounmap(struct pci_dev *pdev, void *res)
 {
 	struct pci_mmio_region *mmio, *p;
+	bus_space_handle_t bh = (bus_space_handle_t)res;
 
 	TAILQ_FOREACH_SAFE(mmio, &pdev->mmio, next, p) {
-		if ((bus_space_handle_t)res < rman_get_bushandle(mmio->res) ||
-		    (bus_space_handle_t)res >= rman_get_bushandle(mmio->res) +
-					       rman_get_size(mmio->res))
-			continue;
+		if (pdev->want_iomap_res) {
+			if (res != mmio->res)
+				continue;
+		} else {
+			if (bh <  rman_get_bushandle(mmio->res) ||
+			    bh >= rman_get_bushandle(mmio->res) +
+				  rman_get_size(mmio->res))
+				continue;
+		}
 		bus_release_resource(pdev->dev.bsddev,
 		    mmio->type, mmio->rid, mmio->res);
 		TAILQ_REMOVE(&pdev->mmio, mmio, next);
