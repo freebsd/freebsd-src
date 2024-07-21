47933aa23d4dbd714ed13c5f8dbd6202dd472cc7
Vladimir Kondratyev <wulf@FreeBSD.org>
2024-07-21 16:10:14 +0300

    LinuxKPI: Remove vmas argument from get_user_pages on KBI layer
    To chase Linux kernel 6.5
    
    Sponsored by:	Serenity Cyber Security, LLC
    Reviewed by:	manu
    Differential Revision:	https://reviews.freebsd.org/D45850

--- sys/compat/linuxkpi/common/src/linux_page.c
+++ sys/compat/linuxkpi/common/src/linux_page.c
@@ -277,8 +277,8 @@ get_user_pages_remote(struct task_struct *task, struct mm_struct *mm,
 }
 
 long
-get_user_pages(unsigned long start, unsigned long nr_pages,
-    unsigned int gup_flags, struct page **pages, struct vm_area_struct **vmas)
+lkpi_get_user_pages(unsigned long start, unsigned long nr_pages,
+    unsigned int gup_flags, struct page **pages)
 {
 	vm_map_t map;
 
