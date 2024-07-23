cade8f6c118f304eb7c91a1d423b4a97ee466284
Brooks Davis <brooks@FreeBSD.org>
2024-07-23 22:31:44 +0100

    ELF auxargs: reserve a slot for CheriABI use
    Reserve slot 37 to avoid flag days downstream.
    
    We currently use this slot in CheriBSD to maintain counters in userspace
    while providing access to them vis sysctl.  CHERI support isn't landing
    in FreeBSD quite yet (probably for 16 unless hardware access accelerates
    radically), but it seems highly likely at this point so reserving slots
    will limit future diffs in both directions.
    
    Reviewed by:	jhb
    Sponsored by:	DARPA, AFRL
    Differential Revision:	https://reviews.freebsd.org/D46060

--- sys/sys/elf_common.h
+++ sys/sys/elf_common.h
@@ -990,8 +990,9 @@ typedef struct {
 #define	AT_KPRELOAD	34	/* Base of vdso, preloaded by rtld */
 #define	AT_USRSTACKBASE	35	/* Top of user stack */
 #define	AT_USRSTACKLIM	36	/* Grow limit of user stack */
+#define	AT_CHERI_STATS	37	/* Reserved */
 
-#define	AT_COUNT	37	/* Count of defined aux entry types. */
+#define	AT_COUNT	38	/* Count of defined aux entry types. */
 
 /*
  * Relocation types.
