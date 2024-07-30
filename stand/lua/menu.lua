freebsd/freebsd-src

commit 170dbe8c77de17fac20b6a2d17f68487e1a4c604

stand: Add "Loader needs updating" to the first menu item"
When the boot loader version is too old, add a warning to the boot menu
to maybe catch people's attention.

Sponsored by:		Netflix
Reviewed by:		jhb
Differential Revision:	https://reviews.freebsd.org/D45890

stand/lua/menu.lua
@@ -245,6 +245,7 @@ menu.welcome = {
 			boot_entry_1, boot_entry_2 = multi_user, single_user
 		end
 		return {
+			loader_needs_upgrade,
 			boot_entry_1,
 			boot_entry_2,
 			menu_entries.prompt,
@@ -410,6 +411,15 @@ menu.welcome = {
 				return loader.getenv('chain_disk') ~= nil
 			end,
 			alias = {"l", "L"},
+		},
+		loader_needs_upgrade = {
+			entry_type = core.MENU_SEPARATOR,
+			name = function()
+				return "Loader requires updating"
+			end
+			visible = function()
+				return core.loaderTooOld()
+			end
 		},
 		vendor = {
 			entry_type = core.MENU_ENTRY,

commit 3a3b44dc5c87a5d8f2c7141dfa1ba5f7c0a89a2d

stand: Use correct loader needs to be updated code
Use the correct loader code that adds an inactive highlighted menu item
indicating that an update is needed.

My laptop is the only machine that I have a boot menu. I'd debugged the
menu part there, but had all the other changes, including my original
menu code, on my server and hadn't copied it back before pushing.

Fixes: 170dbe8c77de
Sponsored by: Netflix

stand/lua/menu.lua
@@ -245,7 +245,6 @@ menu.welcome = {
 			boot_entry_1, boot_entry_2 = multi_user, single_user
 		end
 		return {
-			loader_needs_upgrade,
 			boot_entry_1,
 			boot_entry_2,
 			menu_entries.prompt,
@@ -264,6 +263,10 @@ menu.welcome = {
 			menu_entries.boot_envs,
 			menu_entries.chainload,
 			menu_entries.vendor,
+			{
+				entry_type = core.MENU_SEPARATOR,
+			},
+			menu_entries.loader_needs_upgrade,
 		}
 	end,
 	all_entries = {
@@ -415,8 +418,8 @@ menu.welcome = {
 		loader_needs_upgrade = {
 			entry_type = core.MENU_SEPARATOR,
 			name = function()
-				return "Loader requires updating"
-			end
+				return color.highlight("Loader needs to be updated")
+			end,
 			visible = function()
 				return core.loaderTooOld()
 			end