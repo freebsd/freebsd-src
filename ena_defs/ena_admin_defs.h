0e025f8caad67c5ec6aeb03dcc36abb054e3295d
Osama Abboud <osamaabb@amazon.com>
2024-08-04 11:19:17 +0000

    ena: Add configuration notifications interface support
    This commit introduces a new asynchronous event notification group which
    will be used in favor of the configuration notification channel.
    
    Approved by: cperciva (mentor)
    MFC after: 2 weeks
    Sponsored by: Amazon, Inc.

--- ena_defs/ena_admin_defs.h
+++ ena_defs/ena_admin_defs.h
@@ -1243,7 +1243,8 @@ enum ena_admin_aenq_group {
 	ENA_ADMIN_NOTIFICATION                      = 3,
 	ENA_ADMIN_KEEP_ALIVE                        = 4,
 	ENA_ADMIN_REFRESH_CAPABILITIES              = 5,
-	ENA_ADMIN_AENQ_GROUPS_NUM                   = 6,
+	ENA_ADMIN_CONF_NOTIFICATIONS		    = 6,
+	ENA_ADMIN_AENQ_GROUPS_NUM                   = 7,
 };
 
 enum ena_admin_aenq_notification_syndrome {
@@ -1278,6 +1279,14 @@ struct ena_admin_aenq_keep_alive_desc {
 	uint32_t rx_overruns_low;
 
 	uint32_t rx_overruns_high;
+};
+
+struct ena_admin_aenq_conf_notifications_desc {
+	struct ena_admin_aenq_common_desc aenq_common_desc;
+
+	uint64_t notifications_bitmap;
+
+	uint64_t reserved;
 };
 
 struct ena_admin_ena_mmio_req_read_less_resp {
