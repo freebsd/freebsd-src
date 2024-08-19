9ed8b29ca6afaded3b5cdcd4fc898f977b3bdd6a
Andrew Turner <andrew@FreeBSD.org>
2024-08-19 13:53:27 +0100

    sys/conf: Introduce NOSAN_CFLAGS and NOSAN_C
    To simplify disabling the kernel sanitizers in some files add
    NOSAN_CFLAGS and NOSAN_C variables. These are CFLAGS and NORMAL_C with
    the sanitizer flags removed.
    
    While here add MSAN_CFLAGS to simplify keeping KMSAN in kern_kcov.c
    
    Reviewed by:	khng, brooks, imp, markj
    Sponsored by:	Arm Ltd
    Differential Revision:	https://reviews.freebsd.org/D45498

--- sys/conf/kern.pre.mk
+++ sys/conf/kern.pre.mk
@@ -129,11 +129,12 @@ KMSAN_ENABLED!= grep KMSAN opt_global.h || true ; echo
 .if !empty(KMSAN_ENABLED)
 # Disable -fno-sanitize-memory-param-retval until interceptors have been
 # updated to work properly with it.
-SAN_CFLAGS+=	-DSAN_NEEDS_INTERCEPTORS -DSAN_INTERCEPTOR_PREFIX=kmsan \
+MSAN_CFLAGS+=	-DSAN_NEEDS_INTERCEPTORS -DSAN_INTERCEPTOR_PREFIX=kmsan \
 		-fsanitize=kernel-memory
 .if ${COMPILER_TYPE} == "clang" && ${COMPILER_VERSION} >= 160000
-SAN_CFLAGS+=	-fno-sanitize-memory-param-retval
+MSAN_CFLAGS+=	-fno-sanitize-memory-param-retval
 .endif
+SAN_CFLAGS+=	${MSAN_CFLAGS}
 .endif
 
 KUBSAN_ENABLED!=	grep KUBSAN opt_global.h || true ; echo
@@ -211,6 +212,10 @@ NORMAL_FW= uudecode -o ${.TARGET} ${.ALLSRC}
 NORMAL_FWO= ${CC:N${CCACHE_BIN}} -c ${ASM_CFLAGS} ${WERROR} -o ${.TARGET} \
 	$S/kern/firmw.S -DFIRMW_FILE=\""${.ALLSRC:M*.fw}"\" \
 	-DFIRMW_SYMBOL="${.ALLSRC:M*.fw:C/[-.\/]/_/g}"
+
+# Remove sanitizer arguments. Some -fno-sanitize* and -fasan-shadow-offset*
+# arguments become an error if the appropriate sanitizer is not enabled.
+NOSAN_C= ${NORMAL_C:N-fsanitize*:N-fno-sanitize*:N-fasan-shadow-offset*}
 
 # for ZSTD in the kernel (include zstd/lib/freebsd before other CFLAGS)
 ZSTD_C= ${CC} -c -DZSTD_HEAPMODE=1 -I$S/contrib/zstd/lib/freebsd ${CFLAGS} \
