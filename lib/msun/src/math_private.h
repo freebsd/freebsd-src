851b781251bb7ef96c0f753d5ab9a667471d399b
Steve Kargl <kargl@FreeBSD.org>
2024-08-21 14:59:07 +0800

    msun: Fix typo in comment
    PR:		280965
    MFC after:	3 days
    
    (cherry picked from commit 213eb102aeec50b8c236aac1d8f0e0a3f9a99449)

--- lib/msun/src/math_private.h
+++ lib/msun/src/math_private.h
@@ -406,7 +406,7 @@ do {								\
  * any extra precision into the type of 'a' -- 'a' should have type float_t,
  * double_t or long double.  b's type should be no larger than 'a's type.
  * Callers should use these types with scopes as large as possible, to
- * reduce their own extra-precision and efficiciency problems.  In
+ * reduce their own extra-precision and efficiency problems.  In
  * particular, they shouldn't convert back and forth just to call here.
  */
 #ifdef DEBUG
