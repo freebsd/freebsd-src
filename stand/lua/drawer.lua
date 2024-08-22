freebsd/freebsd-src

commit 7cb65be96d47cbe0b740266bc633d272fc4c5e6b

stand: Use correct loader needs to be updated code
Use the correct loader code that adds an inactive highlighted menu item
indicating that an update is needed.

My laptop is the only machine that I have a boot menu. I'd debugged the
menu part there, but had all the other changes, including my original
menu code, on my server and hadn't copied it back before pushing.

Fixes: 0eac99f76ec3
Sponsored by: Netflix

stand/lua/menu.lua
@@ -245,7 +245,6 @@ menu.welcome = {
 			boot_entry_1, boot_entry_2 = multi_user, single_user
 		end
 		return {
-			loader_needs_upgrade,
 			boot_entry_1,
 			boot_entry_2,
 			menu_entries.prompt,
@@ -264,6 +263,10 @@ menu.welcome = {
 			menu_entries.boot_envs,
 			menu_entries.chainload,
 			menu_entries.vendor,
+			{
+				entry_type = core.MENU_SEPARATOR,
+			},
+			menu_entries.loader_needs_upgrade,
 		}
 	end,
 	all_entries = {
@@ -415,8 +418,8 @@ menu.welcome = {
 		loader_needs_upgrade = {
 			entry_type = core.MENU_SEPARATOR,
 			name = function()
-				return "Loader requires updating"
-			end
+				return color.highlight("Loader needs to be updated")
+			end,
 			visible = function()
 				return core.loaderTooOld()
 			end

commit 101afbc6ee2f06f77e6886f1f3ffe115c579967c

loader/menu: tweak for added line
I added a line to the menu, but didn't adjust so things were a
line off. Make the necessary adjustments.

Fixes: 7cb65be96d47
Sponsored by: Netflix
MFC After: 3 days

stand/lua/drawer.lua
@@ -472,7 +472,7 @@ logodefs = {
 brand_position = {x = 2, y = 1}
 logo_position = {x = 46, y = 4}
 menu_position = {x = 5, y = 10}
-frame_size = {w = 42, h = 13}
+frame_size = {w = 42, h = 14}
 default_shift = {x = 0, y = 0}
 shift = default_shift
 

stand/lua/menu.lua
@@ -535,7 +535,7 @@ end
 
 function menu.autoboot(delay)
 	local x = loader.getenv("loader_menu_timeout_x") or 4
-	local y = loader.getenv("loader_menu_timeout_y") or 23
+	local y = loader.getenv("loader_menu_timeout_y") or 24
 	local endtime = loader.time() + delay
 	local time
 	local last