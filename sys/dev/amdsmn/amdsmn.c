7235dc428e2c4920100229fd92359d837aba9393
Oliver Fromme <olli@FreeBSD.org>
2024-08-22 21:46:19 -0700

    amdsmn(4), amdtemp(4): add support for AMD Ryzen 7 "Phoenix" processors
    Adds support for AMD Ryzen 7 "Phoenix" processors (family 0x19,
    model 0x70-0x7f) to the amdsmn(4) and amdtemp(4) drivers. This
    enables temperature readings of these CPUs via sysctl.
    
    The sensors function identically to those for the "Raphael" processors
    (model 0x60-0x6f); only the PCI device ID differs.
    
    PR:		kern/280942
    Relnotes:	yes
    MFC after:	3 days

--- sys/dev/amdsmn/amdsmn.c
+++ sys/dev/amdsmn/amdsmn.c
@@ -60,6 +60,7 @@
 #define	PCI_DEVICE_ID_AMD_17H_M60H_ROOT		0x1630
 #define	PCI_DEVICE_ID_AMD_19H_M10H_ROOT		0x14a4
 #define	PCI_DEVICE_ID_AMD_19H_M60H_ROOT		0x14d8
+#define	PCI_DEVICE_ID_AMD_19H_M70H_ROOT		0x14e8
 
 struct pciid;
 struct amdsmn_softc {
@@ -112,6 +113,12 @@ static const struct pciid {
 	{
 		.amdsmn_vendorid = CPU_VENDOR_AMD,
 		.amdsmn_deviceid = PCI_DEVICE_ID_AMD_19H_M60H_ROOT,
+		.amdsmn_addr_reg = F17H_SMN_ADDR_REG,
+		.amdsmn_data_reg = F17H_SMN_DATA_REG,
+	},
+	{
+		.amdsmn_vendorid = CPU_VENDOR_AMD,
+		.amdsmn_deviceid = PCI_DEVICE_ID_AMD_19H_M70H_ROOT,
 		.amdsmn_addr_reg = F17H_SMN_ADDR_REG,
 		.amdsmn_data_reg = F17H_SMN_DATA_REG,
 	},
