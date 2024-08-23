431f8665a501089eae631a3f21419cecc48e3156
Brooks Davis <brooks@FreeBSD.org>
2024-08-23 18:57:59 +0100

    proccontrol: make -s require a target
    Require a command to exec or a pid to target and update usage and the
    manpage to make this more clear.
    
    It makes no sense to invoke a procctl(2) command on the current process
    only to exit.  Users are sometimes confused about how proccontrol works
    and think it effects their shell environment when invoked without a
    target.  Disallowing this nonsensical behavior and clarifiying usage
    will hopefully reduce confusion.
    
    Reviewed by:	kib
    Differential Revision:	https://reviews.freebsd.org/D46422

--- usr.bin/proccontrol/proccontrol.c
+++ usr.bin/proccontrol/proccontrol.c
@@ -81,10 +81,14 @@ str2pid(const char *str)
 static void __dead2
 usage(void)
 {
-
-	fprintf(stderr, "Usage: proccontrol -m (aslr|protmax|trace|trapcap|"
-	    "stackgap|nonewprivs|wxmap"KPTI_USAGE LA_USAGE") [-q] "
-	    "[-s (enable|disable)] [-p pid | command]\n");
+	fprintf(stderr, "Usage:\n");
+	fprintf(stderr, "    proccontrol -m mode -s (enable|disable) "
+	    "(-p pid | command)\n");
+	fprintf(stderr, "    proccontrol -m mode -q [-p pid]\n");
+	fprintf(stderr, "Modes: "
+	    "aslr|protmax|trace|trapcap|stackgap|nonewprivs|wxmap"
+	    KPTI_USAGE LA_USAGE
+	    "\n");
 	exit(1);
 }
 
@@ -157,6 +161,8 @@ main(int argc, char *argv[])
 			usage();
 		pid = getpid();
 	} else if (pid == -1) {
+		if (!query)
+			usage();
 		pid = getpid();
 	}
 
