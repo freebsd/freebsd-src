d5e31d74bf30b0f4accad98ae1bf045adb8f3bcb
Elliott Mitchell <ehem+freebsd@m5p.com>
2024-08-24 13:39:21 -0700

    intr/x86: revert remains of f9acc6410e5e
    I'm sure this seemed to make sense when done, but now appears a very
    low-value optimization.  This isn't very maintainable now and is in the
    way of future changes.

--- sys/x86/isa/clock.c
+++ sys/x86/isa/clock.c
@@ -89,7 +89,6 @@ static	struct mtx clock_lock;
 static	struct intsrc *i8254_intsrc;
 static	uint16_t i8254_lastcount;
 static	uint16_t i8254_offset;
-static	int	(*i8254_pending)(struct intsrc *);
 static	int	i8254_ticked;
 
 struct attimer_softc {
@@ -491,7 +490,7 @@ i8254_get_timecount(struct timecounter *tc)
 	    (!i8254_ticked && (clkintr_pending ||
 	    ((count < 20 || (!(flags & PSL_I) &&
 	    count < i8254_max_count / 2u)) &&
-	    i8254_pending != NULL && i8254_pending(i8254_intsrc))))) {
+	    i8254_intsrc != NULL && i8254_intsrc->is_pic->pic_source_pending(i8254_intsrc))))) {
 		i8254_ticked = 1;
 		i8254_offset += i8254_max_count;
 	}
@@ -570,8 +569,6 @@ attimer_attach(device_t dev)
 	    &sc->port_rid, IO_TIMER1, IO_TIMER1 + 3, 4, RF_ACTIVE)))
 		device_printf(dev,"Warning: Couldn't map I/O.\n");
 	i8254_intsrc = intr_lookup_source(0);
-	if (i8254_intsrc != NULL)
-		i8254_pending = i8254_intsrc->is_pic->pic_source_pending;
 	resource_int_value(device_get_name(dev), device_get_unit(dev),
 	    "timecounter", &i8254_timecounter);
 	set_i8254_freq(MODE_STOP, 0);
