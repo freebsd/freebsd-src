0f2b2276abc305905e7d88619a7abca26b0dd7eb
Alan Somers <asomers@FreeBSD.org>
2024-08-26 14:44:24 -0600

    ctl: fix uninitialized data used by PREVENT ALLOW MEDIUM REMOVAL
    Zero-initialize the bitmap of preventers.  Otherwise, the START STOP
    UNIT command may not have the intended effect.
    
    MFC after:	2 weeks
    Sponsored by:	Axcient
    Reviewed by:    emaste, markj
    Pull Request:   https://github.com/freebsd/freebsd-src/pull/1409

--- sys/cam/ctl/ctl.c
+++ sys/cam/ctl/ctl.c
@@ -4630,7 +4630,7 @@ fail:
 	ctl_tpc_lun_init(lun);
 	if (lun->flags & CTL_LUN_REMOVABLE) {
 		lun->prevent = malloc((CTL_MAX_INITIATORS + 31) / 32 * 4,
-		    M_CTL, M_WAITOK);
+		    M_CTL, M_WAITOK | M_ZERO);
 	}
 
 	/*
