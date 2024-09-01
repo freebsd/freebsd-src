4f9e373d9c1c7908f51b82ef1407d93c691f1dff
Mark Johnston <markj@FreeBSD.org>
2024-09-01 14:00:20 +0000

    arm64: Fix the VM_MAX_NAMELEN definition
    The maximum VM name length has changed since the arm64 vmm code was
    forked.  For now, just sync with the amd64 definitions, since they
    permit longer VM names.  As arm64/vmm is implemented only in main, I
    won't bother dealing with backward compat.
    
    Reviewed by:	jhb
    Differential Revision:	https://reviews.freebsd.org/D46484

--- sys/arm64/include/vmm.h
+++ sys/arm64/include/vmm.h
@@ -102,14 +102,30 @@ enum vm_reg_name {
 #define	VM_INTINFO_HWEXCEPTION	(3 << 8)
 #define	VM_INTINFO_SWINTR	(4 << 8)
 
-#define VM_MAX_SUFFIXLEN 15
-
 #define VM_GUEST_BASE_IPA	0x80000000UL	/* Guest kernel start ipa */
 
+/*
+ * The VM name has to fit into the pathname length constraints of devfs,
+ * governed primarily by SPECNAMELEN.  The length is the total number of
+ * characters in the full path, relative to the mount point and not 
+ * including any leading '/' characters.
+ * A prefix and a suffix are added to the name specified by the user.
+ * The prefix is usually "vmm/" or "vmm.io/", but can be a few characters
+ * longer for future use.
+ * The suffix is a string that identifies a bootrom image or some similar
+ * image that is attached to the VM. A separator character gets added to
+ * the suffix automatically when generating the full path, so it must be
+ * accounted for, reducing the effective length by 1.
+ * The effective length of a VM name is 229 bytes for FreeBSD 13 and 37
+ * bytes for FreeBSD 12.  A minimum length is set for safety and supports
+ * a SPECNAMELEN as small as 32 on old systems.
+ */
+#define VM_MAX_PREFIXLEN 10
+#define VM_MAX_SUFFIXLEN 15
+#define VM_MAX_NAMELEN \
+    (SPECNAMELEN - VM_MAX_PREFIXLEN - VM_MAX_SUFFIXLEN - 1)
+
 #ifdef _KERNEL
-
-#define	VM_MAX_NAMELEN	32
-
 struct vm;
 struct vm_exception;
 struct vm_exit;
