73e399c157a82dbe7d1ffd7e0346ead7e2167952
Zhenlei Huang <zlei@FreeBSD.org>
2024-09-03 18:25:26 +0800

    drm2: Stop checking for failures from malloc(M_WAITOK)
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D45852
    
    (cherry picked from commit 6dbf3aca4f5c4f7287f186360c2391f2adfea8d6)
    (cherry picked from commit 95532d15a12c9cbf7c5e62e912de90919af7db24)

--- sys/dev/drm2/drm_buffer.c
+++ sys/dev/drm2/drm_buffer.c
@@ -50,45 +50,15 @@ int drm_buffer_alloc(struct drm_buffer **buf, int size)
 	 * variable sized */
 	*buf = malloc(sizeof(struct drm_buffer) + nr_pages*sizeof(char *),
 			DRM_MEM_DRIVER, M_ZERO | M_WAITOK);
-
-	if (*buf == NULL) {
-		DRM_ERROR("Failed to allocate drm buffer object to hold"
-				" %d bytes in %d pages.\n",
-				size, nr_pages);
-		return -ENOMEM;
-	}
-
 	(*buf)->size = size;
 
 	for (idx = 0; idx < nr_pages; ++idx) {
-
 		(*buf)->data[idx] =
 			malloc(min(PAGE_SIZE, size - idx * PAGE_SIZE),
 				DRM_MEM_DRIVER, M_WAITOK);
-
-
-		if ((*buf)->data[idx] == NULL) {
-			DRM_ERROR("Failed to allocate %dth page for drm"
-					" buffer with %d bytes and %d pages.\n",
-					idx + 1, size, nr_pages);
-			goto error_out;
-		}
-
 	}
 
 	return 0;
-
-error_out:
-
-	/* Only last element can be null pointer so check for it first. */
-	if ((*buf)->data[idx])
-		free((*buf)->data[idx], DRM_MEM_DRIVER);
-
-	for (--idx; idx >= 0; --idx)
-		free((*buf)->data[idx], DRM_MEM_DRIVER);
-
-	free(*buf, DRM_MEM_DRIVER);
-	return -ENOMEM;
 }
 EXPORT_SYMBOL(drm_buffer_alloc);
 
