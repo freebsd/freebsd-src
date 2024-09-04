8f251937850142748cdf67a46630342934ff9f91
Kristof Provost <kp@FreeBSD.org>
2024-09-04 14:54:23 +0200

    if_ovpn: ensure it's safe to modify the mbuf
    PR:		280036
    Reviewed by:	ae
    MFC after:	1 week
    Sponsored by:	Rubicon Communications, LLC ("Netgate")
    Differential Revision:	https://reviews.freebsd.org/D46529
    
    (cherry picked from commit 5644e2c6d47c6113a61ab7fc0776b7227677656a)

--- sys/net/if_ovpn.c
+++ sys/net/if_ovpn.c
@@ -2119,6 +2119,12 @@ ovpn_output(struct ifnet *ifp, struct mbuf *m, const struct sockaddr *dst,
 
 	sc = ifp->if_softc;
 
+	m = m_unshare(m, M_NOWAIT);
+	if (m == NULL) {
+		OVPN_COUNTER_ADD(sc, lost_data_pkts_out, 1);
+		return (ENOBUFS);
+	}
+
 	OVPN_RLOCK(sc);
 
 	SDT_PROBE1(if_ovpn, tx, transmit, start, m);
@@ -2236,6 +2242,12 @@ ovpn_udp_input(struct mbuf *m, int off, struct inpcb *inp,
 	OVPN_RLOCK_TRACKER;
 
 	M_ASSERTPKTHDR(m);
+
+	m = m_unshare(m, M_NOWAIT);
+	if (m == NULL) {
+		OVPN_COUNTER_ADD(sc, nomem_data_pkts_in, 1);
+		return (true);
+	}
 
 	OVPN_COUNTER_ADD(sc, transport_bytes_received, m->m_pkthdr.len - off);
 
