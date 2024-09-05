c87c8fda44ba507a4ac373bf2998e755ab2dd8c9
Michael Tuexen <tuexen@FreeBSD.org>
2024-09-05 17:31:31 +0200

    neta: improve TCP LRO
    Use the appropriate function to flush correctly all entries. The old
    code does not remove the element from the hash table, only from the
    active queue.
    
    Reviewed by:		Peter Lei, rscheff
    Sponsored by:		Netflix, Inc.
    Differential Revision:	https://reviews.freebsd.org/D46433
    
    (cherry picked from commit 5203dcce2527fd235648e2b855a13f62247eb2de)

--- sys/dev/neta/if_mvneta.c
+++ sys/dev/neta/if_mvneta.c
@@ -3000,8 +3000,6 @@ mvneta_rx_queue(struct mvneta_softc *sc, int q, int npkt)
 	struct mvneta_rx_desc *r;
 	struct mvneta_buf *rxbuf;
 	struct mbuf *m;
-	struct lro_ctrl *lro;
-	struct lro_entry *queued;
 	void *pktbuf;
 	int i, pktlen, processed, ndma;
 
@@ -3115,11 +3113,7 @@ rx_lro:
 	/*
 	 * Flush any outstanding LRO work
 	 */
-	lro = &rx->lro;
-	while (__predict_false((queued = LIST_FIRST(&lro->lro_active)) != NULL)) {
-		LIST_REMOVE(LIST_FIRST((&lro->lro_active)), next);
-		tcp_lro_flush(lro, queued);
-	}
+	tcp_lro_flush_all(&rx->lro);
 }
 
 STATIC void
