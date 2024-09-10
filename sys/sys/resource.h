63b1caad419c994c65e1d679e1f6f4c71109b9f2
Konstantin Belousov <kib@FreeBSD.org>
2024-09-10 06:57:58 +0300

    kernel: add RLIMIT_PIPEBUF
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D46619

--- sys/sys/resource.h
+++ sys/sys/resource.h
@@ -114,8 +114,9 @@ struct __wrusage {
 #define	RLIMIT_SWAP	12		/* swap used */
 #define	RLIMIT_KQUEUES	13		/* kqueues allocated */
 #define	RLIMIT_UMTXP	14		/* process-shared umtx */
+#define	RLIMIT_PIPEBUF	15		/* pipes/fifos buffers */
 
-#define	RLIM_NLIMITS	15		/* number of resource limits */
+#define	RLIM_NLIMITS	16		/* number of resource limits */
 
 #define	RLIM_INFINITY	((rlim_t)(((__uint64_t)1 << 63) - 1))
 #define	RLIM_SAVED_MAX	RLIM_INFINITY
