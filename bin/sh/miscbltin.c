d5ed8778bf3b808d637c7913da66dc31b892e5f7
Konstantin Belousov <kib@FreeBSD.org>
2024-09-10 07:12:27 +0300

    bin/sh: support RLIMIT_PIPEBUF
    (cherry picked from commit 5d92f20c7d316aca521d4db4c1dab0f2d0e92c36)

--- bin/sh/miscbltin.c
+++ bin/sh/miscbltin.c
@@ -490,6 +490,9 @@ static const struct limits limits[] = {
 #ifdef RLIMIT_UMTXP
 	{ "umtx shared locks",	(char *)0,	RLIMIT_UMTXP,	   1, 'o' },
 #endif
+#ifdef RLIMIT_PIPEBUF
+	{ "pipebuf",		(char *)0,	RLIMIT_PIPEBUF, 1024, 'y' },
+#endif
 	{ (char *) 0,		(char *)0,	0,		   0, '\0' }
 };
 
@@ -525,7 +528,7 @@ ulimitcmd(int argc __unused, char **argv __unused)
 	struct rlimit	limit;
 
 	what = 'f';
-	while ((optc = nextopt("HSatfdsmcnuvlbpwko")) != '\0')
+	while ((optc = nextopt("HSatfdsmcnuvlbpwkoy")) != '\0')
 		switch (optc) {
 		case 'H':
 			how = HARD;
