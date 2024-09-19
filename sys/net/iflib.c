7ebda9886eda8956df5a24423396a52ffce5e4e6
Mark Johnston <markj@FreeBSD.org>
2024-09-19 09:21:05 +0000

    iflib: Use if_alloc_dev() to allocate the ifnet
    This ensures that the ifnet's NUMA affinity is accurate.
    
    Reviewed by:	kbowling
    MFC after:	2 weeks
    Sponsored by:	Klara, Inc.
    Differential Revision:	https://reviews.freebsd.org/D46667
    
    (cherry picked from commit 767723ddebe9c76a2d4a45a50d9b0efc9f2f91d7)

--- sys/net/iflib.c
+++ sys/net/iflib.c
@@ -5750,7 +5750,7 @@ iflib_register(if_ctx_t ctx)
 
 	CTX_LOCK_INIT(ctx);
 	STATE_LOCK_INIT(ctx, device_get_nameunit(ctx->ifc_dev));
-	ifp = ctx->ifc_ifp = if_alloc(IFT_ETHER);
+	ifp = ctx->ifc_ifp = if_alloc_dev(IFT_ETHER, dev);
 
 	/*
 	 * Initialize our context's device specific methods
