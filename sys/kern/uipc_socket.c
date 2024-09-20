283bf3b4b105ad24e919444ab0e21f3296891283
Mark Johnston <markj@FreeBSD.org>
2024-09-20 11:40:31 +0000

    socket: Only log splice structs to ktrace if KTR_STRUCT is configured
    Fixes:	a1da7dc1cdad ("socket: Implement SO_SPLICE")

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -3955,7 +3955,8 @@ sosetopt(struct socket *so, struct sockopt *sopt)
 			if (error)
 				goto bad;
 #ifdef KTRACE
-			ktrsplice(&splice);
+			if (KTRPOINT(curthread, KTR_STRUCT))
+				ktrsplice(&splice);
 #endif
 
 			error = splice_init();
