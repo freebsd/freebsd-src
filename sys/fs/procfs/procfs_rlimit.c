eb182e05be50689c7c50bee3bcec9874d690dc83
Konstantin Belousov <kib@FreeBSD.org>
2024-09-20 18:04:32 +0300

    procfs: ensure that RLIMIT_IDENT is properly updated when a limit is added
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week

--- sys/fs/procfs/procfs_rlimit.c
+++ sys/fs/procfs/procfs_rlimit.c
@@ -57,6 +57,9 @@
 #include <fs/pseudofs/pseudofs.h>
 #include <fs/procfs/procfs.h>
 
+_Static_assert(nitems(rlimit_ident) == RLIM_NLIMITS,
+    "resource.h RLIMIT_IDENT needs update");
+
 int
 procfs_doprocrlimit(PFS_FILL_ARGS)
 {
