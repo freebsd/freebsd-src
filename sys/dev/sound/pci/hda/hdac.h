freebsd/freebsd-src

commit 35e492f3bd4160991994617f4354f9e0b256f9af

Update list of Conexant and Realtek codecs.
MFC after:	2 weeks

sys/dev/sound/pci/hda/hdac.h
@@ -339,7 +339,13 @@
 /* Realtek */
 #define REALTEK_VENDORID	0x10ec
 #define HDA_CODEC_ALC221	HDA_CODEC_CONSTRUCT(REALTEK, 0x0221)
+#define HDA_CODEC_ALC225	HDA_CODEC_CONSTRUCT(REALTEK, 0x0225)
+#define HDA_CODEC_ALC231	HDA_CODEC_CONSTRUCT(REALTEK, 0x0231)
 #define HDA_CODEC_ALC233	HDA_CODEC_CONSTRUCT(REALTEK, 0x0233)
+#define HDA_CODEC_ALC234	HDA_CODEC_CONSTRUCT(REALTEK, 0x0234)
+#define HDA_CODEC_ALC235	HDA_CODEC_CONSTRUCT(REALTEK, 0x0235)
+#define HDA_CODEC_ALC255	HDA_CODEC_CONSTRUCT(REALTEK, 0x0255)
+#define HDA_CODEC_ALC256	HDA_CODEC_CONSTRUCT(REALTEK, 0x0256)
 #define HDA_CODEC_ALC260	HDA_CODEC_CONSTRUCT(REALTEK, 0x0260)
 #define HDA_CODEC_ALC262	HDA_CODEC_CONSTRUCT(REALTEK, 0x0262)
 #define HDA_CODEC_ALC267	HDA_CODEC_CONSTRUCT(REALTEK, 0x0267)
@@ -348,8 +354,23 @@
 #define HDA_CODEC_ALC270	HDA_CODEC_CONSTRUCT(REALTEK, 0x0270)
 #define HDA_CODEC_ALC272	HDA_CODEC_CONSTRUCT(REALTEK, 0x0272)
 #define HDA_CODEC_ALC273	HDA_CODEC_CONSTRUCT(REALTEK, 0x0273)
+#define HDA_CODEC_ALC274	HDA_CODEC_CONSTRUCT(REALTEK, 0x0274)
 #define HDA_CODEC_ALC275	HDA_CODEC_CONSTRUCT(REALTEK, 0x0275)
 #define HDA_CODEC_ALC276	HDA_CODEC_CONSTRUCT(REALTEK, 0x0276)
+#define HDA_CODEC_ALC280	HDA_CODEC_CONSTRUCT(REALTEK, 0x0280)
+#define HDA_CODEC_ALC282	HDA_CODEC_CONSTRUCT(REALTEK, 0x0282)
+#define HDA_CODEC_ALC283	HDA_CODEC_CONSTRUCT(REALTEK, 0x0283)
+#define HDA_CODEC_ALC284	HDA_CODEC_CONSTRUCT(REALTEK, 0x0284)
+#define HDA_CODEC_ALC285	HDA_CODEC_CONSTRUCT(REALTEK, 0x0285)
+#define HDA_CODEC_ALC286	HDA_CODEC_CONSTRUCT(REALTEK, 0x0286)
+#define HDA_CODEC_ALC288	HDA_CODEC_CONSTRUCT(REALTEK, 0x0288)
+#define HDA_CODEC_ALC290	HDA_CODEC_CONSTRUCT(REALTEK, 0x0290)
+#define HDA_CODEC_ALC292	HDA_CODEC_CONSTRUCT(REALTEK, 0x0292)
+#define HDA_CODEC_ALC293	HDA_CODEC_CONSTRUCT(REALTEK, 0x0293)
+#define HDA_CODEC_ALC294	HDA_CODEC_CONSTRUCT(REALTEK, 0x0294)
+#define HDA_CODEC_ALC295	HDA_CODEC_CONSTRUCT(REALTEK, 0x0295)
+#define HDA_CODEC_ALC298	HDA_CODEC_CONSTRUCT(REALTEK, 0x0298)
+#define HDA_CODEC_ALC299	HDA_CODEC_CONSTRUCT(REALTEK, 0x0299)
 #define HDA_CODEC_ALC292	HDA_CODEC_CONSTRUCT(REALTEK, 0x0292)
 #define HDA_CODEC_ALC295	HDA_CODEC_CONSTRUCT(REALTEK, 0x0295)
 #define HDA_CODEC_ALC660	HDA_CODEC_CONSTRUCT(REALTEK, 0x0660)
@@ -357,7 +378,11 @@
 #define HDA_CODEC_ALC663	HDA_CODEC_CONSTRUCT(REALTEK, 0x0663)
 #define HDA_CODEC_ALC665	HDA_CODEC_CONSTRUCT(REALTEK, 0x0665)
 #define HDA_CODEC_ALC670	HDA_CODEC_CONSTRUCT(REALTEK, 0x0670)
+#define HDA_CODEC_ALC671	HDA_CODEC_CONSTRUCT(REALTEK, 0x0671)
 #define HDA_CODEC_ALC680	HDA_CODEC_CONSTRUCT(REALTEK, 0x0680)
+#define HDA_CODEC_ALC700	HDA_CODEC_CONSTRUCT(REALTEK, 0x0700)
+#define HDA_CODEC_ALC701	HDA_CODEC_CONSTRUCT(REALTEK, 0x0701)
+#define HDA_CODEC_ALC703	HDA_CODEC_CONSTRUCT(REALTEK, 0x0703)
 #define HDA_CODEC_ALC861	HDA_CODEC_CONSTRUCT(REALTEK, 0x0861)
 #define HDA_CODEC_ALC861VD	HDA_CODEC_CONSTRUCT(REALTEK, 0x0862)
 #define HDA_CODEC_ALC880	HDA_CODEC_CONSTRUCT(REALTEK, 0x0880)
@@ -370,6 +395,7 @@
 #define HDA_CODEC_ALC892	HDA_CODEC_CONSTRUCT(REALTEK, 0x0892)
 #define HDA_CODEC_ALC899	HDA_CODEC_CONSTRUCT(REALTEK, 0x0899)
 #define HDA_CODEC_ALC1150	HDA_CODEC_CONSTRUCT(REALTEK, 0x0900)
+#define HDA_CODEC_ALC1220	HDA_CODEC_CONSTRUCT(REALTEK, 0x1220)
 #define HDA_CODEC_ALCXXXX	HDA_CODEC_CONSTRUCT(REALTEK, 0xffff)
 
 /* Motorola */
@@ -546,6 +572,17 @@
 #define HDA_CODEC_CX20652	HDA_CODEC_CONSTRUCT(CONEXANT, 0x50ac)
 #define HDA_CODEC_CX20664	HDA_CODEC_CONSTRUCT(CONEXANT, 0x50b8)
 #define HDA_CODEC_CX20665	HDA_CODEC_CONSTRUCT(CONEXANT, 0x50b9)
+#define HDA_CODEC_CX21722	HDA_CODEC_CONSTRUCT(CONEXANT, 0x50f1)
+#define HDA_CODEC_CX20722	HDA_CODEC_CONSTRUCT(CONEXANT, 0x50f2)
+#define HDA_CODEC_CX21724	HDA_CODEC_CONSTRUCT(CONEXANT, 0x50f3)
+#define HDA_CODEC_CX20724	HDA_CODEC_CONSTRUCT(CONEXANT, 0x50f4)
+#define HDA_CODEC_CX20751	HDA_CODEC_CONSTRUCT(CONEXANT, 0x510f)
+#define HDA_CODEC_CX20751_2	HDA_CODEC_CONSTRUCT(CONEXANT, 0x5110)
+#define HDA_CODEC_CX20753	HDA_CODEC_CONSTRUCT(CONEXANT, 0x5111)
+#define HDA_CODEC_CX20755	HDA_CODEC_CONSTRUCT(CONEXANT, 0x5113)
+#define HDA_CODEC_CX20756	HDA_CODEC_CONSTRUCT(CONEXANT, 0x5114)
+#define HDA_CODEC_CX20757	HDA_CODEC_CONSTRUCT(CONEXANT, 0x5115)
+#define HDA_CODEC_CX20952	HDA_CODEC_CONSTRUCT(CONEXANT, 0x51d7)
 #define HDA_CODEC_CXXXXX	HDA_CODEC_CONSTRUCT(CONEXANT, 0xffff)
 
 /* VIA */

sys/dev/sound/pci/hda/hdacc.c
@@ -77,7 +77,13 @@ static const struct {
 	{ HDA_CODEC_CS4207, 0,		"Cirrus Logic CS4207" },
 	{ HDA_CODEC_CS4210, 0,		"Cirrus Logic CS4210" },
 	{ HDA_CODEC_ALC221, 0,		"Realtek ALC221" },
+	{ HDA_CODEC_ALC225, 0,		"Realtek ALC225" },
+	{ HDA_CODEC_ALC231, 0,		"Realtek ALC231" },
 	{ HDA_CODEC_ALC233, 0,		"Realtek ALC233" },
+	{ HDA_CODEC_ALC234, 0,		"Realtek ALC234" },
+	{ HDA_CODEC_ALC235, 0,		"Realtek ALC235" },
+	{ HDA_CODEC_ALC255, 0,		"Realtek ALC255" },
+	{ HDA_CODEC_ALC256, 0,		"Realtek ALC256" },
 	{ HDA_CODEC_ALC260, 0,		"Realtek ALC260" },
 	{ HDA_CODEC_ALC262, 0,		"Realtek ALC262" },
 	{ HDA_CODEC_ALC267, 0,		"Realtek ALC267" },
@@ -86,17 +92,36 @@ static const struct {
 	{ HDA_CODEC_ALC270, 0,		"Realtek ALC270" },
 	{ HDA_CODEC_ALC272, 0,		"Realtek ALC272" },
 	{ HDA_CODEC_ALC273, 0,		"Realtek ALC273" },
+	{ HDA_CODEC_ALC274, 0,		"Realtek ALC274" },
 	{ HDA_CODEC_ALC275, 0,		"Realtek ALC275" },
 	{ HDA_CODEC_ALC276, 0,		"Realtek ALC276" },
 	{ HDA_CODEC_ALC292, 0,		"Realtek ALC292" },
 	{ HDA_CODEC_ALC295, 0,		"Realtek ALC295" },
+	{ HDA_CODEC_ALC280, 0,		"Realtek ALC280" },
+	{ HDA_CODEC_ALC282, 0,		"Realtek ALC282" },
+	{ HDA_CODEC_ALC283, 0,		"Realtek ALC283" },
+	{ HDA_CODEC_ALC284, 0,		"Realtek ALC284" },
+	{ HDA_CODEC_ALC285, 0,		"Realtek ALC285" },
+	{ HDA_CODEC_ALC286, 0,		"Realtek ALC286" },
+	{ HDA_CODEC_ALC288, 0,		"Realtek ALC288" },
+	{ HDA_CODEC_ALC290, 0,		"Realtek ALC290" },
+	{ HDA_CODEC_ALC292, 0,		"Realtek ALC292" },
+	{ HDA_CODEC_ALC293, 0,		"Realtek ALC293" },
+	{ HDA_CODEC_ALC294, 0,		"Realtek ALC294" },
+	{ HDA_CODEC_ALC295, 0,		"Realtek ALC295" },
+	{ HDA_CODEC_ALC298, 0,		"Realtek ALC298" },
+	{ HDA_CODEC_ALC299, 0,		"Realtek ALC299" },
 	{ HDA_CODEC_ALC660, 0,		"Realtek ALC660-VD" },
 	{ HDA_CODEC_ALC662, 0x0002,	"Realtek ALC662 rev2" },
 	{ HDA_CODEC_ALC662, 0,		"Realtek ALC662" },
 	{ HDA_CODEC_ALC663, 0,		"Realtek ALC663" },
 	{ HDA_CODEC_ALC665, 0,		"Realtek ALC665" },
 	{ HDA_CODEC_ALC670, 0,		"Realtek ALC670" },
+	{ HDA_CODEC_ALC671, 0,		"Realtek ALC671" },
 	{ HDA_CODEC_ALC680, 0,		"Realtek ALC680" },
+	{ HDA_CODEC_ALC700, 0,		"Realtek ALC700" },
+	{ HDA_CODEC_ALC701, 0,		"Realtek ALC701" },
+	{ HDA_CODEC_ALC703, 0,		"Realtek ALC703" },
 	{ HDA_CODEC_ALC861, 0x0340,	"Realtek ALC660" },
 	{ HDA_CODEC_ALC861, 0,		"Realtek ALC861" },
 	{ HDA_CODEC_ALC861VD, 0,	"Realtek ALC861-VD" },
@@ -113,6 +138,7 @@ static const struct {
 	{ HDA_CODEC_ALC892, 0,		"Realtek ALC892" },
 	{ HDA_CODEC_ALC899, 0,		"Realtek ALC899" },
 	{ HDA_CODEC_ALC1150, 0,		"Realtek ALC1150" },
+	{ HDA_CODEC_ALC1220, 0,		"Realtek ALC1220" },
 	{ HDA_CODEC_AD1882, 0,		"Analog Devices AD1882" },
 	{ HDA_CODEC_AD1882A, 0,		"Analog Devices AD1882A" },
 	{ HDA_CODEC_AD1883, 0,		"Analog Devices AD1883" },
@@ -248,6 +274,17 @@ static const struct {
 	{ HDA_CODEC_CX20652, 0,		"Conexant CX20652" },
 	{ HDA_CODEC_CX20664, 0,		"Conexant CX20664" },
 	{ HDA_CODEC_CX20665, 0,		"Conexant CX20665" },
+	{ HDA_CODEC_CX21722, 0,		"Conexant CX21722" },
+	{ HDA_CODEC_CX20722, 0,		"Conexant CX20722" },
+	{ HDA_CODEC_CX21724, 0,		"Conexant CX21724" },
+	{ HDA_CODEC_CX20724, 0,		"Conexant CX20724" },
+	{ HDA_CODEC_CX20751, 0,		"Conexant CX20751/2" },
+	{ HDA_CODEC_CX20751_2, 0,		"Conexant CX20751/2" },
+	{ HDA_CODEC_CX20753, 0,		"Conexant CX20753/4" },
+	{ HDA_CODEC_CX20755, 0,		"Conexant CX20755" },
+	{ HDA_CODEC_CX20756, 0,		"Conexant CX20756" },
+	{ HDA_CODEC_CX20757, 0,		"Conexant CX20757" },
+	{ HDA_CODEC_CX20952, 0,		"Conexant CX20952" },
 	{ HDA_CODEC_VT1708_8, 0,	"VIA VT1708_8" },
 	{ HDA_CODEC_VT1708_9, 0,	"VIA VT1708_9" },
 	{ HDA_CODEC_VT1708_A, 0,	"VIA VT1708_A" },

commit 4004e1752ec7992eeace7f0318d2ca3fc7ba9e03

hda: remove duplicate codec defines
Reviewed by:	emaste
Fixes: 35e492f3bd41 ("Update list of Conexant and Realtek codecs.")
Differential Revision: https://reviews.freebsd.org/D45965

sys/dev/sound/pci/hda/hdac.h
@@ -586,8 +586,6 @@
 #define HDA_CODEC_ALC295	HDA_CODEC_CONSTRUCT(REALTEK, 0x0295)
 #define HDA_CODEC_ALC298	HDA_CODEC_CONSTRUCT(REALTEK, 0x0298)
 #define HDA_CODEC_ALC299	HDA_CODEC_CONSTRUCT(REALTEK, 0x0299)
-#define HDA_CODEC_ALC292	HDA_CODEC_CONSTRUCT(REALTEK, 0x0292)
-#define HDA_CODEC_ALC295	HDA_CODEC_CONSTRUCT(REALTEK, 0x0295)
 #define HDA_CODEC_ALC300	HDA_CODEC_CONSTRUCT(REALTEK, 0x0300)
 #define HDA_CODEC_ALC623	HDA_CODEC_CONSTRUCT(REALTEK, 0x0623)
 #define HDA_CODEC_ALC660	HDA_CODEC_CONSTRUCT(REALTEK, 0x0660)