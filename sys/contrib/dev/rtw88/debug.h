3de0d1929d19d519d8c14b4b15186ad3f9e0d2b7
Bjoern A. Zeeb <bz@FreeBSD.org>
2024-09-24 23:50:22 +0000

    rtw88: update Realtek's rtw88 driver.
    This version is based on
    git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    98f7e32f20d28ec452afb208f9cffc08448a2652 ( tag: v6.11 ).
    
    Sponsored by:   The FreeBSD Foundation
    
    (cherry picked from commit 11c53278a8a3e86e14377f09bbaa7bad193d3713)

--- sys/contrib/dev/rtw88/debug.h
+++ sys/contrib/dev/rtw88/debug.h
@@ -27,8 +27,9 @@ enum rtw_debug_mask {
 	RTW_DBG_SDIO		= 0x00040000,
 
 #if defined(__FreeBSD__)
-	RTW_DBG_IO_RW		= 0x80000000,
+	RTW_DBG_IO_RW		= 0x10000000,
 #endif
+	RTW_DBG_UNEXP		= 0x80000000,
 	RTW_DBG_ALL		= 0xffffffff
 };
 
@@ -46,19 +47,25 @@ static inline void rtw_debugfs_init(struct rtw_dev *rtwdev) {}
 #ifdef CONFIG_RTW88_DEBUG
 
 __printf(3, 4)
-void __rtw_dbg(struct rtw_dev *rtwdev, enum rtw_debug_mask mask,
-	       const char *fmt, ...);
+void rtw_dbg(struct rtw_dev *rtwdev, enum rtw_debug_mask mask,
+	     const char *fmt, ...);
 
-#if defined(__linux__)
-#define rtw_dbg(rtwdev, a...) __rtw_dbg(rtwdev, ##a)
-#elif defined(__FreeBSD__)
-#define	rtw_dbg(rtwdev, ...)	__rtw_dbg(rtwdev, __VA_ARGS__)
-#endif
+static inline bool rtw_dbg_is_enabled(struct rtw_dev *rtwdev,
+				      enum rtw_debug_mask mask)
+{
+	return !!(rtw_debug_mask & mask);
+}
 
 #else
 
 static inline void rtw_dbg(struct rtw_dev *rtwdev, enum rtw_debug_mask mask,
 			   const char *fmt, ...) {}
+
+static inline bool rtw_dbg_is_enabled(struct rtw_dev *rtwdev,
+				      enum rtw_debug_mask mask)
+{
+	return false;
+}
 
 #endif /* CONFIG_RTW88_DEBUG */
 
