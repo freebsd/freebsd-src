66c7d5365a7812f530d15962fd3640db5a2d4b15
Michael Tuexen <tuexen@FreeBSD.org>
2024-09-26 08:06:24 +0200

    MAC: improve handling of listening sockets
    so_peerlabel can only be used when the socket is not listening.
    
    Reviewed by:		markj
    Sponsored by:		Netflix, Inc.
    Differential Revision:	https://reviews.freebsd.org/D46755
    
    (cherry picked from commit 2fb778fab893b4a8a86ecfa20acf2e23bb2cdae8)

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -155,6 +155,7 @@
 #include <net/vnet.h>
 
 #include <security/mac/mac_framework.h>
+#include <security/mac/mac_internal.h>
 
 #include <vm/uma.h>
 
@@ -1465,6 +1466,10 @@ solisten_proto(struct socket *so, int backlog)
 	sbsnd_flags = so->so_snd.sb_flags;
 	sbrcv_timeo = so->so_rcv.sb_timeo;
 	sbsnd_timeo = so->so_snd.sb_timeo;
+
+#ifdef MAC
+	mac_socketpeer_label_free(so->so_peerlabel);
+#endif
 
 	sbdestroy(so, SO_SND);
 	sbdestroy(so, SO_RCV);
