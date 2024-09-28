9a45681088db1ae4463b36343e227ebefad05458
Elliott Mitchell <ehem+freebsd@m5p.com>
2024-09-28 13:28:48 -0700

    intrng: convert interrupt infrastructure to inline intr_event structures
    The intr_event structure is a baseline for what all architectures
    support.  This isn't truly separate from the infrastructures and is
    always allocated.  As such merging seems best.

--- sys/arm/mv/gpio.c
+++ sys/arm/mv/gpio.c
@@ -564,8 +564,7 @@ mv_gpio_intr_handler(device_t dev, int pin)
 #error "No known invoked functions modify ->gpio_events, therefore NOP."
 	isrc.isrc_event = sc->gpio_events[pin];
 
-	if (isrc.isrc_event == NULL ||
-	    CK_SLIST_EMPTY(&isrc.isrc_event->ie_handlers))
+	if (CK_SLIST_EMPTY(&isrc.isrc_event.ie_handlers))
 		return;
 
 	intr_isrc_dispatch(&isrc, NULL);
