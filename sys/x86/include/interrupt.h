33d02de6697bf9a20a5367ccb5b1de856836df72
Elliott Mitchell <ehem+freebsd@m5p.com>
2024-10-01 21:02:09 -0700

    intr/x86: introduce PIC base softc
    Due to having per-PIC structures, adopt these into the AT PIC and
    IO-APIC implementations.  Change the zero-length array in `struct ioapic`
    to a flexible array as FreeBSD now allows C99.

--- sys/x86/include/interrupt.h
+++ sys/x86/include/interrupt.h
@@ -43,6 +43,8 @@ typedef struct intsrc interrupt_t;
 
 DECLARE_CLASS(pic_base_class);
 
+typedef struct { } pic_base_softc_t;
+
 /*
  * Values used in determining the allocation of IRQ values among
  * different types of I/O interrupts.  These values are used as
