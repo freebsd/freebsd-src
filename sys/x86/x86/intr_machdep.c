d2a531b521ef38502b848795af5f69448d0a5d54
Elliott Mitchell <ehem+freebsd@m5p.com>
2024-10-01 21:02:09 -0700

    intr/x86: introduce PIC base class
    This allows the interrupt framework to introduce hooks into device
    function tables.  Some functionality may be common and the framework
    may provide standard functionality.  Certain PIC tasks may also go
    through the function tables.

--- sys/x86/x86/intr_machdep.c
+++ sys/x86/x86/intr_machdep.c
@@ -55,9 +55,11 @@
 #include <sys/systm.h>
 #include <sys/taskqueue.h>
 #include <sys/vmmeter.h>
+
 #include <machine/clock.h>
 #include <machine/intr_machdep.h>
 #include <machine/smp.h>
+
 #ifdef DDB
 #include <ddb/ddb.h>
 #endif
@@ -437,6 +439,12 @@ intr_assign_cpu(void *arg, int cpu)
 	return (EOPNOTSUPP);
 #endif
 }
+
+static device_method_t pic_base_methods[] = {
+	DEVMETHOD_END
+};
+
+PUBLIC_DEFINE_CLASSN(pic_base, pic_base_class, pic_base_methods, 0);
 
 static void
 intrcnt_setname(const char *name, int index)
