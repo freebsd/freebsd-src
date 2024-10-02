999dfe4483983d702175e64dc959475a0e41733a
Elliott Mitchell <ehem+freebsd@m5p.com>
2024-10-01 21:02:09 -0700

    intr/x86: introduce PIC base class
    This allows the interrupt framework to introduce hooks into device
    function tables.  Some functionality may be common and the framework
    may provide standard functionality.  Certain PIC tasks may also go
    through the function tables.

--- sys/x86/include/intr_machdep.h
+++ sys/x86/include/intr_machdep.h
@@ -32,7 +32,10 @@
 
 #include <sys/_cpuset.h>
 #include <sys/_interrupt.h>
+#include <sys/kobj.h>
 #include <sys/types.h>
+
+DECLARE_CLASS(pic_base_class);
 
 /*
  * Values used in determining the allocation of IRQ values among
