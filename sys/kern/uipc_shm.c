27990c4ec016cbeaa5513d949e892e41c625b6a2
Mark Johnston <markj@FreeBSD.org>
2024-10-04 14:54:44 +0000

    shm: Respect PROT_MAX when creating private mappings
    We were previously unconditionally adding PROT_WRITE to the maxprot of
    private mapping (because a private mapping can be written even if the
    fd is read-only), but this might violate the user's PROT_MAX request.
    
    While here, rename cap_maxprot to max_maxprot.  This is the intersection
    of the maximum protections imposed by capsicum rights on the fd (not
    really relevant for private mappings) and the user-required maximum
    protections (which were not being obeyed).  In particular, cap_maxprot
    is a misnomer after the introduction of PROT_MAX.
    
    Add some regression test cases.  mmap__maxprot_shm fails without this
    patch.
    
    Note: Capsicum's CAP_MMAP_W is a bit ambiguous.  Should it be required
    in order to create writeable private mappings?  Currently it is, even
    though such mappings don't permit writes to the object referenced by the
    fd.
    
    Reported by:	brooks
    Reviewed by:	brooks
    MFC after:	1 month
    Fixes:		c7841c6b8e41 ("Relax restrictions on private mappings of POSIX shm objects.")
    Differential Revision:	https://reviews.freebsd.org/D46741

--- sys/kern/uipc_shm.c
+++ sys/kern/uipc_shm.c
@@ -1658,7 +1658,7 @@ fail:
 
 static int
 shm_mmap(struct file *fp, vm_map_t map, vm_offset_t *addr, vm_size_t objsize,
-    vm_prot_t prot, vm_prot_t cap_maxprot, int flags,
+    vm_prot_t prot, vm_prot_t max_maxprot, int flags,
     vm_ooffset_t foff, struct thread *td)
 {
 	struct shmfd *shmfd;
@@ -1681,8 +1681,8 @@ shm_mmap(struct file *fp, vm_map_t map, vm_offset_t *addr, vm_size_t objsize,
 	 * writeable.
 	 */
 	if ((flags & MAP_SHARED) == 0) {
-		cap_maxprot |= VM_PROT_WRITE;
-		maxprot |= VM_PROT_WRITE;
+		if ((max_maxprot & VM_PROT_WRITE) != 0)
+			maxprot |= VM_PROT_WRITE;
 		writecnt = false;
 	} else {
 		if ((fp->f_flag & FWRITE) != 0 &&
@@ -1702,7 +1702,7 @@ shm_mmap(struct file *fp, vm_map_t map, vm_offset_t *addr, vm_size_t objsize,
 			goto out;
 		}
 	}
-	maxprot &= cap_maxprot;
+	maxprot &= max_maxprot;
 
 	/* See comment in vn_mmap(). */
 	if (
