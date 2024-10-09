ec41c67a771c7d2b239c25527e7bc042e259a873
Elliott Mitchell <ehem+freebsd@m5p.com>
2024-10-08 19:51:23 -0700

    Revert "sys/intr.h: formally depend on machine/intr.h"
    This reverts commit 4f12b529f40422589404fc939ed4a92710412a61.
    
    Unfortunately having the unusual #include direction serves a high-value
    purpose.  The <machine/intr.h> headers almost serve as a standard point
    to retrieve architecture interrupt headers.  This is far more valuable.
    
    Leave the move of #include <sys/bus.h> in riscv/trap.c intact.  That is
    a reasonable move unrelated to the purpose of this revert.
    
    Move the #include of sys/intr.h earlier in arm/include and
    riscv/include.  This is a historical artifact from d6233babae3 and is
    overdue for cleanup.

--- sys/riscv/riscv/timer.c
+++ sys/riscv/riscv/timer.c
@@ -41,7 +41,6 @@
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/bus.h>
-#include <sys/intr.h>
 #include <sys/kernel.h>
 #include <sys/module.h>
 #include <sys/rman.h>
@@ -51,6 +50,7 @@
 #include <sys/watchdog.h>
 
 #include <machine/cpufunc.h>
+#include <machine/intr.h>
 #include <machine/md_var.h>
 #include <machine/sbi.h>
 
