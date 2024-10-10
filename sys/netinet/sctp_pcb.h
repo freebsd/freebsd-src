e1a09d1e9df30347c279604191a04ce2ef20bf0c
Michael Tuexen <tuexen@FreeBSD.org>
2024-10-10 10:42:35 +0200

    sctp: make sctp_free_ifn() static
    It is not used outside of the file.
    No functional change intended.
    
    MFC after:	3 days

--- sys/netinet/sctp_pcb.h
+++ sys/netinet/sctp_pcb.h
@@ -508,7 +508,6 @@ sctp_add_addr_to_vrf(uint32_t vrfid,
 
 void sctp_update_ifn_mtu(uint32_t ifn_index, uint32_t mtu);
 
-void sctp_free_ifn(struct sctp_ifn *sctp_ifnp);
 void sctp_free_ifa(struct sctp_ifa *sctp_ifap);
 
 void
