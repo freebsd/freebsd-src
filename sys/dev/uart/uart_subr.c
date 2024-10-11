freebsd/freebsd-src

commit 031dc6e4e202dc38073efe748b6d0e8d2e4e4827

dev/uart: Support setting the register io width
Some uarts require a specific register width. Support setting this in
the kernel environment.

Reviewed by:	imp (earlier version)
Sponsored by:	Arm Ltd
Differential Revision:	https://reviews.freebsd.org/D43362

sys/dev/uart/uart_subr.c
@@ -47,6 +47,7 @@
 #define	UART_TAG_SB	8
 #define	UART_TAG_XO	9
 #define	UART_TAG_BD	10
+#define	UART_TAG_RW	11
 
 static bus_addr_t
 uart_parse_addr(const char **p)
@@ -148,6 +149,10 @@ uart_parse_tag(const char **p)
 		tag = UART_TAG_RS;
 		goto out;
 	}
+	if ((*p)[0] == 'r' && (*p)[1] == 'w') {
+		tag = UART_TAG_RW;
+		goto out;
+	}
 	if ((*p)[0] == 's' && (*p)[1] == 'b') {
 		tag = UART_TAG_SB;
 		goto out;
@@ -225,6 +230,7 @@ uart_getenv(int devtype, struct uart_devinfo *di, struct uart_class *class)
 	/* Set defaults. */
 	di->bas.chan = 0;
 	di->bas.regshft = 0;
+	di->bas.regiowidth = 1;
 	di->bas.rclk = 0;
 	di->baudrate = 0;
 	di->databits = 8;
@@ -263,6 +269,9 @@ uart_getenv(int devtype, struct uart_devinfo *di, struct uart_class *class)
 			break;
 		case UART_TAG_RS:
 			di->bas.regshft = uart_parse_long(&spec);
+			break;
+		case UART_TAG_RW:
+			di->bas.regiowidth = uart_parse_long(&spec);
 			break;
 		case UART_TAG_SB:
 			di->stopbits = uart_parse_long(&spec);

commit 9299a4ac5cb4b789f17dcbaff9ece545b2e1ca8c

uart: Document rw:XXX field of hw.uart.console
Add a one-liner description of rw - Register Width added in 031dc6e4e202.

Fixes: 031dc6e4e202
Sponsored by:		Netflix

sys/dev/uart/uart_subr.c
@@ -185,6 +185,7 @@ out:
  *	mm = Memory mapped I/O address
  *	pa = Parity
  *	rs = Register shift
+ *	rw = Register width
  *	sb = Stopbits
  *	xo = Device clock (xtal oscillator)
  *