3b94fd6bdd15a3fc4808d1ab73f8d724ca50559d
Elliott Mitchell <ehem+freebsd@m5p.com>
2024-10-12 10:22:46 -0700

    intr/x86: use softc as PIC list
    Device structures aren't quite as handy for the PIC list as raw C
    structures.  By using a standard base softc, the softc area can be used
    for the PIC list.  Adjust things to match.
    
    The TAILQ functionality simply isn't well suited for handling this sort
    of situation.

--- sys/x86/include/interrupt.h
+++ sys/x86/include/interrupt.h
@@ -39,15 +39,11 @@ typedef struct intsrc interrupt_t;
 #include <sys/_cpuset.h>
 #include <sys/_interrupt.h>
 #include <sys/kobj.h>
-#include <sys/queue.h>
 #include <sys/types.h>
 
 DECLARE_CLASS(pic_base_class);
 
-typedef struct pic_entr {
-	TAILQ_ENTRY(pic_entr) pics_next;
-	device_t pic;
-} pic_base_softc_t;
+typedef struct { device_t next, prev; } pic_base_softc_t;
 
 /*
  * Values used in determining the allocation of IRQ values among
