01ac81e81cf1769489431198c02b92555f7f9ced
Peter Eriksson <pen@lysator.liu.se>
2024-10-13 22:01:33 -0600

    ciss: Expose tunable hw.ciss.nop_message_heartbeat as sysctl
    Expose the hw.ciss.nop_message_heartbeat tuneable as a sysctl and make
    it writeable at runtime.
    
    PR: 246279
    Reviewed by: imp
    Tested by: Marek Zarychta
    Differential Revision: https://reviews.freebsd.org/D25155

--- sys/dev/ciss/ciss.c
+++ sys/dev/ciss/ciss.c
@@ -271,6 +271,9 @@ SYSCTL_INT(_hw_ciss, OID_AUTO, expose_hidden_physical, CTLFLAG_RWTUN,
 
 static unsigned int ciss_nop_message_heartbeat = 0;
 TUNABLE_INT("hw.ciss.nop_message_heartbeat", &ciss_nop_message_heartbeat);
+SYSCTL_INT(_hw_ciss, OID_AUTO, nop_message_heartbeat, CTLFLAG_RWTUN,
+	   &ciss_nop_message_heartbeat, 0,
+	   "nop heartbeat messages");
 
 /*
  * This tunable can force a particular transport to be used:
