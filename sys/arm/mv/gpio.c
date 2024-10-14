a63dd3ffb5bf98d5b84470cbb95a0e4d1a457c55
Elliott Mitchell <ehem+freebsd@m5p.com>
2024-10-14 11:37:40 -0700

    arm64/mv_gpio: disconnect Marvell GPIO driver from kernel build
    This driver was never fully adapted to work with INTRNG (current ARM
    interrupt core).  Since the driver needs a full rework at this point,
    disconnect the driver from the FreeBSD kernel build.  Hopefully this
    might be updated by someone, but it has become too problematic.

--- sys/arm/mv/gpio.c
+++ sys/arm/mv/gpio.c
@@ -448,6 +448,7 @@ mv_gpio_setup_intrhandler(device_t dev, const char *name, driver_filter_t *filt,
 		} else if (sc->gpio_setup[pin].gp_flags & MV_GPIO_IN_IRQ_DOUBLE_EDGE)
 			mv_gpio_double_edge_init(dev, pin);
 		MV_GPIO_UNLOCK();
+#error "This is very suspicious."
 		error = intr_event_create(&event, (void *)s, 0, pin,
 		    (void (*)(void *))mv_gpio_intr_mask,
 		    (void (*)(void *))mv_gpio_intr_unmask,
@@ -459,6 +460,7 @@ mv_gpio_setup_intrhandler(device_t dev, const char *name, driver_filter_t *filt,
 		sc->gpio_events[pin] = event;
 	}
 
+#error "This fails to update interrupt names, as INTRNG isn't informed."
 	intr_event_add_handler(event, name, filt, hand, arg,
 	    intr_priority(flags), flags, cookiep);
 	return (0);
