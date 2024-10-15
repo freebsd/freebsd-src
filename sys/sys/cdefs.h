6327bb4f1c8f4a3714f344f16afbcef7ec79a92f
Warner Losh <imp@FreeBSD.org>
2024-10-15 13:49:53 -0600

    cdefs: Add __deprecated
    Add __deprecated decorator. This is for a deprecated interface. copystr
    is tagged with this today in copy(9), but don't actually provide it or
    use it. copystr is a #define so adding it will have to wait.
    
    LinuxKPI was defining this away completely in compiler.h. Since this is
    shared between Linux KPI consumers and OpenZFS, if it's already defined,
    use the FreeBSD sys/cdefs.h version, otherwise define it away. For
    OpenZFS this will retain it, while for Linux KPI it will tend to drop it
    (I think always, but I didn't look at everything).
    
    Sponsored by:		Netflix
    Reviewed by:		jhb, emaste
    Differential Revision:	https://reviews.freebsd.org/D46137

--- sys/sys/cdefs.h
+++ sys/sys/cdefs.h
@@ -154,6 +154,7 @@
 #define	__pure2		__attribute__((__const__))
 #define	__unused	__attribute__((__unused__))
 #define	__used		__attribute__((__used__))
+#define __deprecated	__attribute__((__deprecated__))
 #define	__packed	__attribute__((__packed__))
 #define	__aligned(x)	__attribute__((__aligned__(x)))
 #define	__section(x)	__attribute__((__section__(x)))
