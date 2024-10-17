00fa80275f34586775e744d83ff94f8f42eaa696
Zhenlei Huang <zlei@FreeBSD.org>
2024-10-17 21:05:29 +0800

    ndp: Remove a stray semicolon
    MFC after:	1 week
    
    (cherry picked from commit 17120e4bc3e61557816ca0558a60ab969b3df404)

--- usr.sbin/ndp/ndp.c
+++ usr.sbin/ndp/ndp.c
@@ -639,7 +639,7 @@ dump_rtsock(struct sockaddr_in6 *addr, int cflag)
 		xo_emit(xobuf, "Neighbor", "Linklayer Address", "Netif", "Expire", "S", "Flags");
 	}
 	xo_open_list("neighbor-cache");
-again:;
+again:
 	mib[0] = CTL_NET;
 	mib[1] = PF_ROUTE;
 	mib[2] = 0;
