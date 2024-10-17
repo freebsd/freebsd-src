953f9be276ad79759c5f6372d707e6a1858f2a01
ktullavik <ktullavik@gmail.com>
2024-10-17 23:24:27 +0200

    Implement support for libxo

--- usr.sbin/devinfo/devinfo.c
+++ usr.sbin/devinfo/devinfo.c
@@ -39,12 +39,17 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <libxo/xo.h>
 #include "devinfo.h"
 
 static bool	rflag;
 static bool	vflag;
+static int open_tag_count;
+static char *last_res;
 
 static void	print_indent(int);
+static void	print_kvlist(char *);
+static void	xml_safe_string(char *);
 static void	print_resource(struct devinfo_res *);
 static int	print_device_matching_resource(struct devinfo_res *, void *);
 static int	print_device_rman_resources(struct devinfo_rman *, void *);
@@ -70,8 +75,38 @@ print_indent(int n)
 
 	if (n < 1)
 		return;
-	asprintf(&s, "%*s", n, " ");
-	printf("%s", s);
+	asprintf(&s, "{P:%*s}", n, " ");
+	xo_emit(s);
+}
+
+/*
+ * Takes a list of key-value pairs in the form
+ * "key1=val1 key2=val2 ..." and prints them according
+ * to xo formatting.
+ */
+static void
+print_kvlist(char *s)
+{
+	char *kv;
+	char *copy = strdup(s);
+
+	while ((kv = strsep(&copy, " ")) != NULL) {
+		char* k = strsep(&kv, "=");
+		xo_emit("{ea:%s/%s} {d:%s}={d:%s}", k, kv, k, kv);
+	}
+	free(copy);
+}
+
+static void
+xml_safe_string(char *s)
+{
+	int i;
+
+	for (i=0; s[i] != '\0'; i++) {
+		if (s[i] == ' ' || s[i] == '/') {
+			s[i] = '-';
+		}
+	}
 }
 
 /*
@@ -83,21 +118,31 @@ print_resource(struct devinfo_res *res)
 	struct devinfo_rman	*rman;
 	bool			hexmode;
 	rman_res_t		end;
+	char			*safe_desc;
 
 	rman = devinfo_handle_to_rman(res->dr_rman);
 	hexmode =  (rman->dm_size > 1000) || (rman->dm_size == 0);
 	end = res->dr_start + res->dr_size - 1;
 
+	safe_desc = (char*) malloc(strlen(rman->dm_desc));
+	strcpy(safe_desc, rman->dm_desc);
+	xml_safe_string(safe_desc);
+
+	xo_open_instance(safe_desc);
+
 	if (hexmode) {
-		printf("0x%jx", res->dr_start);
+		xo_emit("{:start/0x%llx}", res->dr_start);
 		if (res->dr_size > 1)
-			printf("-0x%jx", end);
+			xo_emit("{D:-}{d:end/0x%llx}", end);
+		xo_emit("{e:end/0x%llx}", end);
 	}
 	else {
-		printf("%ju", res->dr_start);
+		xo_emit("{:start/%u}", res->dr_start);
 		if (res->dr_size > 1)
-			printf("-%ju", end);
+			xo_emit("{D:-}{d:end/%u}", end);
+		xo_emit("{e:end/%u}", end);
 	}
+	xo_close_instance(safe_desc);
 }
 
 /*
@@ -119,7 +164,7 @@ print_device_matching_resource(struct devinfo_res *res, void *arg)
 			return(1);
 		print_indent(ia->indent);
 		print_resource(res);
-		printf("\n");
+		xo_emit("\n");
 	}
 	return(0);
 }
@@ -132,6 +177,7 @@ print_device_rman_resources(struct devinfo_rman *rman, void *arg)
 {
 	struct indent_arg	*ia = (struct indent_arg *)arg;
 	int			indent;
+	char		*safe_desc;
 
 	indent = ia->indent;
 
@@ -141,13 +187,21 @@ print_device_rman_resources(struct devinfo_rman *rman, void *arg)
 	    print_device_matching_resource, ia) != 0) {
 
 		/* there are, print header */
+
+		safe_desc = (char*) malloc(strlen(rman->dm_desc));
+		strcpy(safe_desc, rman->dm_desc);
+		xml_safe_string(safe_desc);
+
 		print_indent(indent);
-		printf("%s:\n", rman->dm_desc);
+		xo_emit("{d:%s}:\n", rman->dm_desc);
+		xo_open_list(safe_desc);
 
 		/* print resources */
 		ia->indent = indent + 4;
 		devinfo_foreach_rman_resource(rman,
 		    print_device_matching_resource, ia);
+
+		xo_close_list(safe_desc);
 	}
 	ia->indent = indent;
 	return(0);
@@ -158,20 +212,40 @@ print_device_props(struct devinfo_dev *dev)
 {
 	if (vflag) {
 		if (*dev->dd_desc) {
-			printf(" <%s>", dev->dd_desc);
+			xo_emit(" <{d:%s}>", dev->dd_desc);
+			xo_emit("{e:description/%s}", dev->dd_desc);
 		}
 		if (*dev->dd_pnpinfo) {
-			printf(" pnpinfo %s", dev->dd_pnpinfo);
+			xo_open_container("pnpinfo");
+			xo_emit("{D: pnpinfo}");
+
+			if ((strcmp(dev->dd_pnpinfo, "unknown") == 0))
+				xo_emit("{D: unknown}");
+			else
+				print_kvlist(dev->dd_pnpinfo);
+
+			xo_close_container("pnpinfo");
 		}
 		if (*dev->dd_location) {
-			printf(" at %s", dev->dd_location);
+			xo_open_container("location");
+			xo_emit("{D: at}");
+			print_kvlist(dev->dd_location);
+			xo_close_container("location");
 		}
+
+		// If verbose, then always print state for json/xml.
+		if (!(dev->dd_flags & DF_ENABLED))
+			xo_emit("{e:state/disabled}");
+		else if (dev->dd_flags & DF_SUSPENDED)
+			xo_emit("{e:state/suspended}");
+		else
+			xo_emit("{e:state/enabled}");
 	}
 
 	if (!(dev->dd_flags & DF_ENABLED))
-		printf(" (disabled)");
+		xo_emit("{D: (disabled)}");
 	else if (dev->dd_flags & DF_SUSPENDED)
-		printf(" (suspended)");
+		xo_emit("{D: (suspended)}");
 }
 
 /*
@@ -181,16 +255,20 @@ int
 print_device(struct devinfo_dev *dev, void *arg)
 {
 	struct indent_arg	ia;
-	int			indent;
+	int			indent, ret;
+	const char*	devname = dev->dd_name[0] ? dev->dd_name : "unknown";
 	bool		printit = vflag || (dev->dd_name[0] != 0
-				   && dev->dd_state >= DS_ATTACHED);
+				    && dev->dd_state >= DS_ATTACHED);
 
 	if (printit) {
 		indent = (int)(intptr_t)arg;
 		print_indent(indent);
-		printf("%s", dev->dd_name[0] ? dev->dd_name : "unknown");
+
+		xo_open_container(devname);
+		xo_emit("{d:%s}", devname);
+
 		print_device_props(dev);
-		printf("\n");
+		xo_emit("\n");
 		if (rflag) {
 			ia.indent = indent + 4;
 			ia.arg = dev;
@@ -199,8 +277,13 @@ print_device(struct devinfo_dev *dev, void *arg)
 		}
 	}
 
-	return(devinfo_foreach_device_child(dev, print_device,
+	ret = (devinfo_foreach_device_child(dev, print_device,
 	    (void *)((char *)arg + 2)));
+
+	if (printit) {
+		xo_close_container(devname);
+	}
+	return(ret);
 }
 
 /*
@@ -212,6 +295,7 @@ print_rman_resource(struct devinfo_res *res, void *arg __unused)
 	struct devinfo_dev	*dev;
 	struct devinfo_rman *rman;
 	rman_res_t		end;
+	char			*res_str, *entry;
 	bool			hexmode;
 
 	dev = devinfo_handle_to_device(res->dr_device);
@@ -219,27 +303,43 @@ print_rman_resource(struct devinfo_res *res, void *arg __unused)
 	hexmode =  (rman->dm_size > 1000) || (rman->dm_size == 0);
 	end = res->dr_start + res->dr_size - 1;
 
-	printf("    ");
-
 	if (hexmode) {
 		if (res->dr_size > 1)
-			printf("0x%jx-0x%jx", res->dr_start, end);
+			asprintf(&res_str, "0x%lx-0x%lx", res->dr_start, end);
 		else
-			printf("0x%jx", res->dr_start);
+			asprintf(&res_str, "0x%lx", res->dr_start);
 	}
 	else {
 		if (res->dr_size > 1)
-			printf("%ju-%ju", res->dr_start, end);
+			asprintf(&res_str, "%u-%u", (unsigned int) res->dr_start,
+			    (unsigned int) end);
 		else
-			printf("%ju", res->dr_start);
+			asprintf(&res_str, "%u", (unsigned int) res->dr_start);
+	}
+
+	xo_emit("{P:    }");
+
+	if (last_res == NULL) {
+		// First resource
+		xo_open_list(res_str);
+	}
+	else if (strcmp(res_str, last_res) != 0) {
+		// We can't repeat json keys. So we keep an
+		// open list from the last iteration and only
+		// create a new list when see a new resource.
+		xo_close_list(last_res);
+		xo_open_list(res_str);
 	}
 
 	if ((dev != NULL) && (dev->dd_name[0] != 0)) {
-		printf(" (%s)", dev->dd_name);
-	} else {
-		printf(" ----");
+		asprintf(&entry, "{el:%s}{D:%s} {D:(%s)}\n", res_str, res_str, dev->dd_name);
+		xo_emit(entry, dev->dd_name);
 	}
-	printf("\n");
+	else {
+		asprintf(&entry, "{el:%s}{D:%s} {D:----}\n", res_str, res_str);
+		xo_emit(entry, "----");
+	}
+	last_res = res_str;
 	return(0);
 }
 
@@ -249,8 +349,19 @@ print_rman_resource(struct devinfo_res *res, void *arg __unused)
 int
 print_rman(struct devinfo_rman *rman, void *arg __unused)
 {
-	printf("%s:\n", rman->dm_desc);
+	char* safe_desc;
+
+	safe_desc = (char*) malloc(strlen(rman->dm_desc));
+	strcpy(safe_desc, rman->dm_desc);
+	xml_safe_string(safe_desc);
+
+	xo_emit("{d:%s}:\n", rman->dm_desc);
+	xo_open_container(safe_desc);
+
 	devinfo_foreach_rman_resource(rman, print_rman_resource, 0);
+
+	xo_close_list(last_res);
+	xo_close_container(safe_desc);
 	return(0);
 }
 
@@ -259,12 +370,17 @@ print_device_path_entry(struct devinfo_dev *dev)
 {
 	const char *devname = dev->dd_name[0] ? dev->dd_name : "unknown";
 
-	printf("%s", devname);
+	xo_open_container(devname);
+	open_tag_count++;
+	xo_emit("{d:%s }", devname);
 	print_device_props(dev);
 	if (vflag)
-		printf("\n");
+		xo_emit("\n");
 }
 
+/*
+ * Recurse until we find the right dev. On the way up we print path.
+ */
 static int
 print_device_path(struct devinfo_dev *dev, void *xname)
 {
@@ -278,7 +394,7 @@ print_device_path(struct devinfo_dev *dev, void *xname)
 
 	rv = devinfo_foreach_device_child(dev, print_device_path, xname);
 	if (rv == 1) {
-		printf(" ");
+		xo_emit("{P: }");
 		print_device_path_entry(dev);
 	}
 	return (rv);
@@ -287,19 +403,26 @@ print_device_path(struct devinfo_dev *dev, void *xname)
 static void
 print_path(struct devinfo_dev *root, char *path)
 {
-	if (devinfo_foreach_device_child(root, print_device_path, (void *)path) == 0)
-		errx(1, "%s: Not found", path);
+	open_tag_count = 0;
+	if (devinfo_foreach_device_child(root, print_device_path,
+	    (void *)path) == 0)
+		xo_errx(1, "%s: Not found", path);
 	if (!vflag)
-		printf("\n");
+		xo_emit("\n");
+
+	while (open_tag_count > 0) {
+		xo_close_container_d();
+		open_tag_count--;
+	}
 }
 
 static void __dead2
 usage(void)
 {
-	fprintf(stderr, "%s\n%s\n%s\n",
-	    "usage: devinfo [-rv]",
-	    "       devinfo -u",
-	    "       devinfo -p dev [-v]");
+	xo_error(
+	    "usage: devinfo [-rv]\n"
+	    "       devinfo -u\n"
+	    "       devinfo -p dev [-v]\n");
 	exit(1);
 }
 
@@ -310,6 +433,11 @@ main(int argc, char *argv[])
 	int			c, rv;
 	bool			uflag;
 	char			*path = NULL;
+
+	argc = xo_parse_args(argc, argv);
+	if (argc < 0) {
+		exit(1);
+	}
 
 	uflag = false;
 	while ((c = getopt(argc, argv, "p:ruv")) != -1) {
@@ -336,20 +464,32 @@ main(int argc, char *argv[])
 
 	if ((rv = devinfo_init()) != 0) {
 		errno = rv;
-		err(1, "devinfo_init");
+		xo_err(1, "devinfo_init");
 	}
 
 	if ((root = devinfo_handle_to_device(DEVINFO_ROOT_DEVICE)) == NULL)
-		errx(1, "can't find root device");
+		xo_errx(1, "can't find root device");
 
 	if (path) {
+		xo_set_flags(NULL, XOF_DTRT);
+		xo_open_container("device-path");
 		print_path(root, path);
+		xo_close_container("device-path");
 	} else if (uflag) {
 		/* print resource usage? */
+		xo_set_flags(NULL, XOF_DTRT);
+		xo_open_container("device-resources");
 		devinfo_foreach_rman(print_rman, NULL);
+		xo_close_container("device-resources");
 	} else {
 		/* print device hierarchy */
+		xo_open_container("device-information");
 		devinfo_foreach_device_child(root, print_device, (void *)0);
+		xo_close_container("device-information");
+	}
+
+	if (xo_finish() < 0) {
+		exit(1);
 	}
 	return(0);
 }
