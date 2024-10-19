f4e35c044c8988b7452cefbdcc417f5fd723e021
Mark Johnston <markj@FreeBSD.org>
2024-10-19 13:03:56 +0000

    bus: Set the current VNET in device_attach()
    Some drivers, in particular anything which creates an ifnet during
    attach, need to have the current VNET set, as if_attach_internal() and
    its callees access VNET-global variables.
    
    device_probe_and_attach() handles this, but this is not the only way to
    arrive in DEVICE_ATTACH.  In particular, bus drivers may invoke
    device_attach() directly, as does devctl2's DEV_ENABLE ioctl handler.
    So, set the current VNET in device_attach() instead.
    
    I believe it is always safe to use vnet0, as devctl2 ioctls are not
    permitted within a jail.
    
    PR:		282168
    Reviewed by:	zlei, kevans, bz, imp, glebius
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D47174

--- sys/kern/subr_bus.c
+++ sys/kern/subr_bus.c
@@ -35,6 +35,7 @@
 #include <sys/conf.h>
 #include <sys/domainset.h>
 #include <sys/eventhandler.h>
+#include <sys/jail.h>
 #include <sys/lock.h>
 #include <sys/kernel.h>
 #include <sys/limits.h>
@@ -2538,10 +2539,7 @@ device_probe_and_attach(device_t dev)
 	else if (error != 0)
 		return (error);
 
-	CURVNET_SET_QUIET(vnet0);
-	error = device_attach(dev);
-	CURVNET_RESTORE();
-	return error;
+	return (device_attach(dev));
 }
 
 /**
@@ -2577,6 +2575,10 @@ device_attach(device_t dev)
 		return (ENXIO);
 	}
 
+	KASSERT(IS_DEFAULT_VNET(TD_TO_VNET(curthread)),
+	    ("device_attach: curthread is not in default vnet"));
+	CURVNET_SET_QUIET(TD_TO_VNET(curthread));
+
 	device_sysctl_init(dev);
 	if (!device_is_quiet(dev))
 		device_print_child(dev->parent, dev);
@@ -2609,8 +2611,10 @@ device_attach(device_t dev)
 			KASSERT(dev->busy == 0, ("attach failed but busy"));
 			dev->state = DS_NOTPRESENT;
 		}
+		CURVNET_RESTORE();
 		return (error);
 	}
+	CURVNET_RESTORE();
 	dev->flags |= DF_ATTACHED_ONCE;
 	/*
 	 * We only need the low bits of this time, but ranges from tens to thousands
