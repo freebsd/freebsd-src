eaab882600d3cf1562a4030e7c91ad3e0861937b
Mark Johnston <markj@FreeBSD.org>
2024-10-20 14:27:25 +0000

    Revert "Reapply "sbin/ping: allow normal users to specify larger packets""
    The value of MAXPAYLOAD in the re-applied commit is not right.  Back
    this out until it's had a proper review.
    
    This reverts commit b88df1e893c455731c7915f72a3b97b078ab04e2.
    
    Requested by:	maxim, glebius

--- sbin/ping/ping.c
+++ sbin/ping/ping.c
@@ -96,8 +96,8 @@
 #define	DEFDATALEN	56		/* default data length */
 #define	FLOOD_BACKOFF	20000		/* usecs to back off if F_FLOOD mode */
 					/* runs out of buffer space */
-#define	MAXIPLEN	((int)sizeof(struct ip) + MAX_IPOPTLEN)
-#define	MAXPAYLOAD	(IP_MAXPACKET - MAXIPLEN - ICMP_MINLEN)
+#define	MAXIPLEN	(sizeof(struct ip) + MAX_IPOPTLEN)
+#define	MAXICMPLEN	(ICMP_ADVLENMIN + MAX_IPOPTLEN)
 #define	MAXWAIT		10000		/* max ms to wait for response */
 #define	MAXALARM	(60 * 60)	/* max seconds for alarm timeout */
 #define	MAXTOS		255
@@ -458,10 +458,11 @@ ping(int argc, char *const *argv)
 				errx(EX_USAGE, "invalid packet size: `%s'",
 				    optarg);
 			datalen = (int)ltmp;
-			if (datalen > MAXPAYLOAD) {
-				errx(EX_USAGE,
+			if (uid != 0 && datalen > DEFDATALEN) {
+				errno = EPERM;
+				err(EX_NOPERM,
 				    "packet size too large: %d > %u",
-				    datalen, MAXPAYLOAD);
+				    datalen, DEFDATALEN);
 			}
 			break;
 		case 'T':		/* multicast TTL */
