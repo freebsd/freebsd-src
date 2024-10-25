539ad1d2b14dc4007c2e4fa6454dda2f28bf3095
Christos Margiolis <christos@FreeBSD.org>
2024-10-25 13:36:51 +0200

    sound: Remove redundant refcount checks in vchan_setnew()
    When adding a new vchan, we are looking for a parent channel which
    either already has vchans (i.e CHN_F_HAS_VCHAN), or does not, but is
    also not being used (i.e !CHN_F_BUSY). Since CHN_F_BUSY essentially
    tells us if the channel is currently being used or not, there is no need
    to check if the channel's refcount is 0 as well.
    
    When removing a vchan, we first check if we have only 1 vchan allocated
    that is also being used (so we cannot remove it at the moment), and then
    we check if the vchan is not busy and remove it. Again, checking
    CHN_F_BUSY is enough.
    
    Sponsored by:	The FreeBSD Foundation
    MFC after:	2 days
    Reviewed by:	dev_submerge.ch
    Differential Revision:	https://reviews.freebsd.org/D47268

--- sys/dev/sound/pcm/vchan.c
+++ sys/dev/sound/pcm/vchan.c
@@ -928,7 +928,6 @@ vchan_setnew(struct snddev_info *d, int direction, int newcnt)
 			CHN_LOCK(c);
 			if (c->direction == direction &&
 			    ((c->flags & CHN_F_HAS_VCHAN) || (vcnt == 0 &&
-			    c->refcount < 1 &&
 			    !(c->flags & (CHN_F_BUSY | CHN_F_VIRTUAL))))) {
 				/*
 				 * Reuse hw channel with vchans already
@@ -987,12 +986,11 @@ vchan_setnew(struct snddev_info *d, int direction, int newcnt)
 			}
 			CHN_FOREACH_SAFE(ch, c, nch, children) {
 				CHN_LOCK(ch);
-				if (vcnt == 1 && c->refcount > 0) {
+				if (vcnt == 1 && ch->flags & CHN_F_BUSY) {
 					CHN_UNLOCK(ch);
 					break;
 				}
-				if (!(ch->flags & CHN_F_BUSY) &&
-				    ch->refcount < 1) {
+				if (!(ch->flags & CHN_F_BUSY)) {
 					err = vchan_destroy(ch);
 					if (err == 0)
 						vcnt--;
