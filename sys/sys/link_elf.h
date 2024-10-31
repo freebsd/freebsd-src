7cd3e2696ca357630ff591959956295da9abbe2d
Konstantin Belousov <kib@FreeBSD.org>
2024-10-31 07:55:43 +0200

    sys/link_elf.h: remove extern linkage specifier from dl_iterate_phdr() prototype
    Reviewed by:	brooks
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D47351

--- sys/sys/link_elf.h
+++ sys/sys/link_elf.h
@@ -93,7 +93,7 @@ struct dl_phdr_info
 __BEGIN_DECLS
 
 typedef int (*__dl_iterate_hdr_callback)(struct dl_phdr_info *, size_t, void *);
-extern int dl_iterate_phdr(__dl_iterate_hdr_callback, void *);
+int dl_iterate_phdr(__dl_iterate_hdr_callback, void *);
 int _rtld_addr_phdr(const void *, struct dl_phdr_info *);
 int _rtld_get_stack_prot(void);
 int _rtld_is_dlopened(void *);
