6c4b2446c6d873d2e902d1233db774fd2560bf96
John Baldwin <jhb@FreeBSD.org>
2024-11-01 10:10:30 -0400

    x86: Remove invalid DEVMETHOD methods for leaf devices
    None of these drivers are for bus devices, so bus_generic_* is not
    appropriate.  Most of these were nops except that detach would
    actually "succeed" (but not do any cleanup).
    
    Reviewed by:	imp
    Differential Revision:	https://reviews.freebsd.org/D47374

--- sys/x86/isa/atpic.c
+++ sys/x86/isa/atpic.c
@@ -599,10 +599,6 @@ static device_method_t atpic_methods[] = {
 	/* Device interface */
 	DEVMETHOD(device_probe,		atpic_probe),
 	DEVMETHOD(device_attach,	atpic_attach),
-	DEVMETHOD(device_detach,	bus_generic_detach),
-	DEVMETHOD(device_shutdown,	bus_generic_shutdown),
-	DEVMETHOD(device_suspend,	bus_generic_suspend),
-	DEVMETHOD(device_resume,	bus_generic_resume),
 	{ 0, 0 }
 };
 
