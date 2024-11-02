7fe14d4bb8c01c9d5f25cd476a3354053569a221
Michael Tuexen <tuexen@FreeBSD.org>
2024-11-02 16:33:02 +0100

    sctp: don't consider the interface name when removing an address
    Checking the interface name can not be done consistently, so
    don't do it.
    
    (cherry picked from commit bf11fdaf0d095fecca61fa8b457d06e27fae5946)

--- sys/netinet/sctp_bsd_addr.c
+++ sys/netinet/sctp_bsd_addr.c
@@ -338,8 +338,7 @@ sctp_addr_change(struct ifaddr *ifa, int cmd)
 		    (void *)ifa, ifa->ifa_addr, ifa_flags, 1);
 	} else {
 		sctp_del_addr_from_vrf(SCTP_DEFAULT_VRFID, ifa->ifa_addr,
-		    ifa->ifa_ifp->if_index,
-		    ifa->ifa_ifp->if_xname);
+		    ifa->ifa_ifp->if_index);
 
 		/*
 		 * We don't bump refcount here so when it completes the
