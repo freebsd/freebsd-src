2e55cdfe32db090d6b8c4699949f3d5f5d808437
Elliott Mitchell <ehem+freebsd@m5p.com>
2024-11-02 17:38:40 -0700

    intr/x86: move setting of ->is_pic inside intr_register_source()
    As all x86 PICs must set this value, it seems more consistent to do so
    inside intr_register_source().  Hopefully a trivial reduction in work.

--- sys/x86/include/interrupt.h
+++ sys/x86/include/interrupt.h
@@ -138,7 +138,7 @@ struct intsrc *intr_lookup_source(int vector);
 device_t	intr_create_pic(const char *name, u_int unit,
 		    struct kobj_class *driver);
 void	intr_register_pic(x86pic_t pic);
-int	intr_register_source(u_int vector, struct intsrc *isrc);
+int	intr_register_source(u_int vector, struct intsrc *isrc, device_t pic);
 int	intr_remove_handler(void *cookie);
 void	intr_resume(bool suspend_cancelled);
 void	intr_suspend(void);
