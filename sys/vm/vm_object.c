92a9501b6be3d37a4eefcfbdda799b656ae146e8
Konstantin Belousov <kib@FreeBSD.org>
2024-11-04 23:54:30 +0200

    vm_object: do not assume that un_pager.devp.dev is cdev
    PR:	282533
    
    (cherry picked from commit 580340dbdaaf372867e9ed3dd257430982753e5e)

--- sys/vm/vm_object.c
+++ sys/vm/vm_object.c
@@ -2610,7 +2610,8 @@ vm_object_list_handler(struct sysctl_req *req, bool swap_only)
 			sp = swap_pager_swapped_pages(obj);
 			kvo->kvo_swapped = sp > UINT32_MAX ? UINT32_MAX : sp;
 		}
-		if (obj->type == OBJT_DEVICE || obj->type == OBJT_MGTDEVICE) {
+		if ((obj->type == OBJT_DEVICE || obj->type == OBJT_MGTDEVICE) &&
+		    (obj->flags & OBJ_CDEVH) != 0) {
 			cdev = obj->un_pager.devp.handle;
 			if (cdev != NULL) {
 				csw = dev_refthread(cdev, &ref);
