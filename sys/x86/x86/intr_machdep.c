169afec97b3f460dc4bfcd4ec3c047cbaee4dd47
Elliott Mitchell <ehem+freebsd@m5p.com>
2024-11-05 09:26:42 -0800

    intr/x86: merge pic_disable_source() call into pic_disable_intr()
    When pic_disable_intr() is called, it is paired with
    pic_disable_source().  Let the PIC implementations handle
    calling of the second function.
    
    Significantly, the pic_disable_source() calls paired with the
    pic_disable_intr() calls are the only time PIC_NO_EOI was used.  This
    means the enum can be removed and pic_disable_source()'s prototype can
    be simplified.  Crucially, this means pic_disable_source_t matches
    intr_event_pre_ithread_t.
    
    Differential Revision: https://reviews.freebsd.org/D47745

--- sys/x86/x86/intr_machdep.c
+++ sys/x86/x86/intr_machdep.c
@@ -326,10 +326,8 @@ intr_remove_handler(struct intsrc *isrc, struct intr_handler *handler)
 	if (error == 0) {
 		sx_xlock(&intrsrc_lock);
 		isrc->is_handlers--;
-		if (isrc->is_handlers == 0) {
-			PIC_DISABLE_SOURCE(isrc->is_pic, isrc, PIC_NO_EOI);
+		if (isrc->is_handlers == 0)
 			PIC_DISABLE_INTR(isrc->is_pic, isrc);
-		}
 		intrcnt_updatename(isrc);
 		sx_xunlock(&intrsrc_lock);
 	}
@@ -350,7 +348,7 @@ intr_disable_src(void *arg)
 	struct intsrc *isrc;
 
 	isrc = arg;
-	PIC_DISABLE_SOURCE(isrc->is_pic, isrc, PIC_EOI);
+	PIC_DISABLE_SOURCE(isrc->is_pic, isrc);
 }
 
 static void
@@ -403,7 +401,7 @@ intr_execute_handlers(struct intsrc *isrc, struct trapframe *frame)
 	 * stray count, and log the condition.
 	 */
 	if (intr_event_handle_(ie, frame) != 0) {
-		PIC_DISABLE_SOURCE(isrc->is_pic, isrc, PIC_EOI);
+		PIC_DISABLE_SOURCE(isrc->is_pic, isrc);
 		(*isrc->is_straycount)++;
 		if (*isrc->is_straycount < INTR_STRAY_LOG_MAX)
 			log(LOG_ERR, "stray irq%d\n", vector);
