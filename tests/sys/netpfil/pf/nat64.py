1df5f9cf33b048324c1564e5feece8aeec1f0e32
Kristof Provost <kp@FreeBSD.org>
2024-11-08 15:45:57 +0100

    pf tests: verify that TCP RST makes it through NAT64
    Sponsored by:	Rubicon Communications, LLC ("Netgate")
    Differential Revision:	https://reviews.freebsd.org/D47796

---
+++ tests/sys/netpfil/pf/nat64.py
@@ -0,0 +1,84 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright (c) 2024 Rubicon Communications, LLC (Netgate)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+import pytest
+from atf_python.sys.net.tools import ToolsHelper
+from atf_python.sys.net.vnet import VnetTestTemplate
+
+class TestNAT64(VnetTestTemplate):
+    REQUIRED_MODULES = [ "pf" ]
+    TOPOLOGY = {
+        "vnet1": {"ifaces": ["if1"]},
+        "vnet2": {"ifaces": ["if1", "if2"]},
+        "vnet3": {"ifaces": ["if2"]},
+        "if1": {"prefixes6": [("2001:db8::2/64", "2001:db8::1/64")]},
+        "if2": {"prefixes4": [("192.0.2.1/24", "192.0.2.2/24")]},
+    }
+
+    def vnet3_handler(self, vnet):
+        ToolsHelper.print_output("echo foo | nc -l 1234")
+
+    def vnet2_handler(self, vnet):
+        ifname = vnet.iface_alias_map["if1"].name
+
+        ToolsHelper.print_output("/sbin/pfctl -e")
+        ToolsHelper.pf_rules([
+            "pass inet6 proto icmp6",
+            "pass in on %s inet6 af-to inet from 192.0.2.1" % ifname])
+
+    @pytest.mark.require_user("root")
+    def test_tcp_rst(self):
+        ToolsHelper.print_output("/sbin/route -6 add default 2001:db8::1")
+
+        import scapy.all as sp
+
+        # Send a SYN
+        packet = sp.IPv6(dst="64:ff9b::192.0.2.2") \
+            / sp.TCP(dport=1222, flags="S")
+
+        # Get a reply
+        reply = sp.sr1(packet)
+
+        # We expect to get a RST here.
+        tcp = reply.getlayer(sp.TCP)
+        assert tcp
+        assert "R" in tcp.flags
+
+        # Now try to SYN to an open port
+        packet = sp.IPv6(dst="64:ff9b::192.0.2.2") \
+            / sp.TCP(dport=1234, flags="S")
+        reply = sp.sr1(packet)
+
+        tcp = reply.getlayer(sp.TCP)
+        assert tcp
+
+        # We don't get RST
+        assert "R" not in tcp.flags
+
+        # We do get SYN|ACK
+        assert "S" in tcp.flags
+        assert "A" in tcp.flags
+
