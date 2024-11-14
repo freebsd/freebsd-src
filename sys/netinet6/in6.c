9e67981e7855bcaf50717491287b0ba8ea23495e
Mark Johnston <markj@FreeBSD.org>
2024-11-14 19:20:21 +0000

    in6: Constify some sockaddr conversion functions
    No functional change intended.
    
    MFC after:	1 week
    Sponsored by:	Klara, Inc.
    Sponsored by:	Stormshield
    
    (cherry picked from commit 3bb15ffc4ab4bfcd4c1bd700a64b3b5ed95389c3)

--- sys/netinet6/in6.c
+++ sys/netinet6/in6.c
@@ -2655,7 +2655,7 @@ in6_domifdetach(struct ifnet *ifp, void *aux)
  * v4 mapped addr or v4 compat addr
  */
 void
-in6_sin6_2_sin(struct sockaddr_in *sin, struct sockaddr_in6 *sin6)
+in6_sin6_2_sin(struct sockaddr_in *sin, const struct sockaddr_in6 *sin6)
 {
 
 	bzero(sin, sizeof(*sin));
@@ -2667,7 +2667,7 @@ in6_sin6_2_sin(struct sockaddr_in *sin, struct sockaddr_in6 *sin6)
 
 /* Convert sockaddr_in to sockaddr_in6 in v4 mapped addr format. */
 void
-in6_sin_2_v4mapsin6(struct sockaddr_in *sin, struct sockaddr_in6 *sin6)
+in6_sin_2_v4mapsin6(const struct sockaddr_in *sin, struct sockaddr_in6 *sin6)
 {
 	bzero(sin6, sizeof(*sin6));
 	sin6->sin6_len = sizeof(struct sockaddr_in6);
