c71b427bfa9aca65d64cbc95231df5883fc582c9
Zhihao Yuan <lichray@gmail.com>
2024-11-27 18:30:46 +0000

    nvi: Replace Clang-only __builtin_is_aligned with C code (#124)
    We should use alignof in the future.
    
    Obtained from: https://github.com/lichray/nvi2/commit/25c4d7db4ea638a31ac458b733a3b67b0a0ff634
    
    (cherry picked from commit 06a98fefd3d3ff42b7e7832af6c0736b98f167ac)

--- contrib/nvi/common/mem.h
+++ contrib/nvi/common/mem.h
@@ -212,6 +212,18 @@ p2roundup(size_t n)
 	return (n);
 }
 
+/*
+ * is_aligned --
+ *      Determine whether the program can safely read an object with an
+ *      alignment requirement from ptr.
+ *
+ * See also: https://clang.llvm.org/docs/LanguageExtensions.html#alignment-builtins
+ */
+static __inline int
+is_aligned(void *ptr, size_t alignment) {
+	return ((uintptr_t)ptr % alignment) == 0;
+}
+
 /* Additional TAILQ helper. */
 #define TAILQ_ENTRY_ISVALID(elm, field)					\
 	((elm)->field.tqe_prev != NULL)
