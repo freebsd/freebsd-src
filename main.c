45af733dbd17548ea0051a665f50143aadd3b2a6
Warner Losh <imp@FreeBSD.org>
2024-11-29 21:48:35 -0700

    awk: Update to 20240728 bsd-feature 3319c34a8713 (add mktime)
    Jul 28, 2024
    	Fixed readcsvrec resize segfault when reading csv records longer
    	than 8k. Thanks to Ozan Yigit.
    	mktime() added to bsd-features branch. Thanks to Todd Miller.

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20240623";
+const char	*version = "version 20240728";
 
 #define DEBUG
 #include <stdio.h>
