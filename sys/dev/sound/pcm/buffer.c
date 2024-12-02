00731aaaed76785af8befe13e9a4e85b3554b3f5
Christos Margiolis <christos@FreeBSD.org>
2024-12-02 17:26:27 +0100

    sound: Remove dead code from pcm/
    No functional change intended.
    
    Sponsored by:	The FreeBSD Foundation
    MFC after:	2 days
    Reviewed by:	dev_submerge.ch, markj
    Differential Revision:	https://reviews.freebsd.org/D47733

--- sys/dev/sound/pcm/buffer.c
+++ sys/dev/sound/pcm/buffer.c
@@ -356,15 +356,6 @@ sndbuf_setfmt(struct snd_dbuf *b, u_int32_t fmt)
 	b->fmt = fmt;
 	b->bps = AFMT_BPS(b->fmt);
 	b->align = AFMT_ALIGN(b->fmt);
-#if 0
-	b->bps = AFMT_CHANNEL(b->fmt);
-	if (b->fmt & AFMT_16BIT)
-		b->bps <<= 1;
-	else if (b->fmt & AFMT_24BIT)
-		b->bps *= 3;
-	else if (b->fmt & AFMT_32BIT)
-		b->bps <<= 2;
-#endif
 	return 0;
 }
 
