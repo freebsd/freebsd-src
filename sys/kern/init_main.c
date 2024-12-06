f6e0690dc9f2e8ba78ba74041eba4ff77091448a
Zhenlei Huang <zlei@FreeBSD.org>
2024-12-06 21:46:55 +0800

    init_main: Use TUNABLE_INT_FETCH to initialize verbose_sysinit
    It is too late to initialize verbose_sysinit via TUNABLE_INT, as
    TUNABLE_INT runs at the order of SI_SUB_TUNABLES, thus any subsystems
    those have order prior or equal to SI_SUB_TUNABLES are not logged.
    
    Reviewed by:	kevans
    Fixes:	c7962400c9a7 Add debug.verbose_sysinit tunable for VERBOSE_SYSINIT
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D47907
    
    (cherry picked from commit a7cfcb26451219d6f4e60c486c855adeea288397)
    (cherry picked from commit 9c6454fe7048ade86faa1a864b04c7b250a660ec)

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -138,7 +138,6 @@ SYSCTL_INT(_debug, OID_AUTO, bootverbose, CTLFLAG_RW, &bootverbose, 0,
  * - 1, 'compiled in but verbose by default' (default)
  */
 int	verbose_sysinit = VERBOSE_SYSINIT;
-TUNABLE_INT("debug.verbose_sysinit", &verbose_sysinit);
 #endif
 
 #ifdef INVARIANTS
@@ -265,7 +264,8 @@ restart:
 	}
 
 #if defined(VERBOSE_SYSINIT)
-	last = SI_SUB_COPYRIGHT;
+	last = SI_SUB_DUMMY;
+	TUNABLE_INT_FETCH("debug.verbose_sysinit", &verbose_sysinit);
 	verbose = 0;
 #if !defined(DDB)
 	printf("VERBOSE_SYSINIT: DDB not enabled, symbol lookups disabled.\n");
