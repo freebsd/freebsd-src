71c92e6b94f0757ba698352888e98363495b5c44
Xin LI <delphij@FreeBSD.org>
2024-12-08 11:07:01 -0800

    MFC: MFV: file 5.46.
    (cherry picked from commit ae316d1d1cffd71ab7751f94e10118777a88e027)

--- contrib/file/src/cdf.h
+++ contrib/file/src/cdf.h
@@ -283,70 +283,79 @@ typedef struct {
 } cdf_catalog_t;
 
 struct timespec;
-int cdf_timestamp_to_timespec(struct timespec *, cdf_timestamp_t);
-int cdf_timespec_to_timestamp(cdf_timestamp_t *, const struct timespec *);
-int cdf_read_header(const cdf_info_t *, cdf_header_t *);
-void cdf_swap_header(cdf_header_t *);
-void cdf_unpack_header(cdf_header_t *, char *);
-void cdf_swap_dir(cdf_directory_t *);
-void cdf_unpack_dir(cdf_directory_t *, char *);
-void cdf_swap_class(cdf_classid_t *);
-ssize_t cdf_read_sector(const cdf_info_t *, void *, size_t, size_t,
-    const cdf_header_t *, cdf_secid_t);
-ssize_t cdf_read_short_sector(const cdf_stream_t *, void *, size_t, size_t,
-    const cdf_header_t *, cdf_secid_t);
-int cdf_read_sat(const cdf_info_t *, cdf_header_t *, cdf_sat_t *);
-size_t cdf_count_chain(const cdf_sat_t *, cdf_secid_t, size_t);
-int cdf_read_long_sector_chain(const cdf_info_t *, const cdf_header_t *,
-    const cdf_sat_t *, cdf_secid_t, size_t, cdf_stream_t *);
-int cdf_read_short_sector_chain(const cdf_header_t *, const cdf_sat_t *,
-    const cdf_stream_t *, cdf_secid_t, size_t, cdf_stream_t *);
-int cdf_read_sector_chain(const cdf_info_t *, const cdf_header_t *,
-    const cdf_sat_t *, const cdf_sat_t *, const cdf_stream_t *, cdf_secid_t,
-    size_t, cdf_stream_t *);
-int cdf_read_dir(const cdf_info_t *, const cdf_header_t *, const cdf_sat_t *,
-    cdf_dir_t *);
-int cdf_read_ssat(const cdf_info_t *, const cdf_header_t *, const cdf_sat_t *,
+file_protected int cdf_timestamp_to_timespec(struct timespec *,
+    cdf_timestamp_t);
+file_protected int cdf_timespec_to_timestamp(cdf_timestamp_t *,
+    const struct timespec *);
+file_protected int cdf_read_header(const cdf_info_t *, cdf_header_t *);
+file_protected void cdf_swap_header(cdf_header_t *);
+file_protected void cdf_unpack_header(cdf_header_t *, char *);
+file_protected void cdf_swap_dir(cdf_directory_t *);
+file_protected void cdf_unpack_dir(cdf_directory_t *, char *);
+file_protected void cdf_swap_class(cdf_classid_t *);
+file_protected ssize_t cdf_read_sector(const cdf_info_t *, void *, size_t,
+    size_t, const cdf_header_t *, cdf_secid_t);
+file_protected ssize_t cdf_read_short_sector(const cdf_stream_t *, void *,
+    size_t, size_t, const cdf_header_t *, cdf_secid_t);
+file_protected int cdf_read_sat(const cdf_info_t *, cdf_header_t *,
     cdf_sat_t *);
-int cdf_read_short_stream(const cdf_info_t *, const cdf_header_t *,
-    const cdf_sat_t *, const cdf_dir_t *, cdf_stream_t *,
-    const cdf_directory_t **);
-int cdf_read_property_info(const cdf_stream_t *, const cdf_header_t *, uint32_t,
-    cdf_property_info_t **, size_t *, size_t *);
-int cdf_read_user_stream(const cdf_info_t *, const cdf_header_t *,
-    const cdf_sat_t *, const cdf_sat_t *, const cdf_stream_t *,
-    const cdf_dir_t *, const char *, cdf_stream_t *);
-int cdf_find_stream(const cdf_dir_t *, const char *, int);
-int cdf_zero_stream(cdf_stream_t *);
-int cdf_read_doc_summary_info(const cdf_info_t *, const cdf_header_t *,
-    const cdf_sat_t *, const cdf_sat_t *, const cdf_stream_t *,
-    const cdf_dir_t *, cdf_stream_t *);
-int cdf_read_summary_info(const cdf_info_t *, const cdf_header_t *,
-    const cdf_sat_t *, const cdf_sat_t *, const cdf_stream_t *,
-    const cdf_dir_t *, cdf_stream_t *);
-int cdf_unpack_summary_info(const cdf_stream_t *, const cdf_header_t *,
-    cdf_summary_info_header_t *, cdf_property_info_t **, size_t *);
-int cdf_unpack_catalog(const cdf_header_t *, const cdf_stream_t *,
-    cdf_catalog_t **);
-int cdf_print_classid(char *, size_t, const cdf_classid_t *);
-int cdf_print_property_name(char *, size_t, uint32_t);
-int cdf_print_elapsed_time(char *, size_t, cdf_timestamp_t);
-uint16_t cdf_tole2(uint16_t);
-uint32_t cdf_tole4(uint32_t);
-uint64_t cdf_tole8(uint64_t);
-char *cdf_ctime(const time_t *, char *);
-char *cdf_u16tos8(char *, size_t, const uint16_t *);
+file_protected size_t cdf_count_chain(const cdf_sat_t *, cdf_secid_t, size_t);
+file_protected int cdf_read_long_sector_chain(const cdf_info_t *,
+    const cdf_header_t *, const cdf_sat_t *, cdf_secid_t, size_t,
+    cdf_stream_t *);
+file_protected int cdf_read_short_sector_chain(const cdf_header_t *,
+    const cdf_sat_t *, const cdf_stream_t *, cdf_secid_t, size_t,
+    cdf_stream_t *);
+file_protected int cdf_read_sector_chain(const cdf_info_t *,
+    const cdf_header_t *, const cdf_sat_t *, const cdf_sat_t *,
+    const cdf_stream_t *, cdf_secid_t, size_t, cdf_stream_t *);
+file_protected int cdf_read_dir(const cdf_info_t *, const cdf_header_t *,
+    const cdf_sat_t *, cdf_dir_t *);
+file_protected int cdf_read_ssat(const cdf_info_t *, const cdf_header_t *,
+    const cdf_sat_t *, cdf_sat_t *);
+file_protected int cdf_read_short_stream(const cdf_info_t *,
+    const cdf_header_t *, const cdf_sat_t *, const cdf_dir_t *,
+    cdf_stream_t *, const cdf_directory_t **);
+file_protected int cdf_read_property_info(const cdf_stream_t *,
+    const cdf_header_t *, uint32_t, cdf_property_info_t **, size_t *, size_t *);
+file_protected int cdf_read_user_stream(const cdf_info_t *,
+    const cdf_header_t *, const cdf_sat_t *, const cdf_sat_t *,
+    const cdf_stream_t *, const cdf_dir_t *, const char *, cdf_stream_t *);
+file_protected int cdf_find_stream(const cdf_dir_t *, const char *, int);
+file_protected int cdf_zero_stream(cdf_stream_t *);
+file_protected int cdf_read_doc_summary_info(const cdf_info_t *,
+    const cdf_header_t *, const cdf_sat_t *, const cdf_sat_t *,
+    const cdf_stream_t *, const cdf_dir_t *, cdf_stream_t *);
+file_protected int cdf_read_summary_info(const cdf_info_t *,
+    const cdf_header_t *, const cdf_sat_t *, const cdf_sat_t *,
+    const cdf_stream_t *, const cdf_dir_t *, cdf_stream_t *);
+file_protected int cdf_unpack_summary_info(const cdf_stream_t *,
+    const cdf_header_t *, cdf_summary_info_header_t *, cdf_property_info_t **,
+    size_t *);
+file_protected int cdf_unpack_catalog(const cdf_header_t *,
+    const cdf_stream_t *, cdf_catalog_t **);
+file_protected int cdf_print_classid(char *, size_t, const cdf_classid_t *);
+file_protected int cdf_print_property_name(char *, size_t, uint32_t);
+file_protected int cdf_print_elapsed_time(char *, size_t, cdf_timestamp_t);
+file_protected uint16_t cdf_tole2(uint16_t);
+file_protected uint32_t cdf_tole4(uint32_t);
+file_protected uint64_t cdf_tole8(uint64_t);
+file_protected char *cdf_ctime(const time_t *, char *);
+file_protected char *cdf_u16tos8(char *, size_t, const uint16_t *);
 
 #ifdef CDF_DEBUG
-void cdf_dump_header(const cdf_header_t *);
-void cdf_dump_sat(const char *, const cdf_sat_t *, size_t);
-void cdf_dump(const void *, size_t);
-void cdf_dump_stream(const cdf_stream_t *);
-void cdf_dump_dir(const cdf_info_t *, const cdf_header_t *, const cdf_sat_t *,
-    const cdf_sat_t *, const cdf_stream_t *, const cdf_dir_t *);
-void cdf_dump_property_info(const cdf_property_info_t *, size_t);
-void cdf_dump_summary_info(const cdf_header_t *, const cdf_stream_t *);
-void cdf_dump_catalog(const cdf_header_t *, const cdf_stream_t *);
+file_protected void cdf_dump_header(const cdf_header_t *);
+file_protected void cdf_dump_sat(const char *, const cdf_sat_t *, size_t);
+file_protected void cdf_dump(const void *, size_t);
+file_protected void cdf_dump_stream(const cdf_stream_t *);
+file_protected void cdf_dump_dir(const cdf_info_t *, const cdf_header_t *,
+    const cdf_sat_t *, const cdf_sat_t *, const cdf_stream_t *,
+    const cdf_dir_t *);
+file_protected void cdf_dump_property_info(const cdf_property_info_t *, size_t);
+file_protected void cdf_dump_summary_info(const cdf_header_t *,
+    const cdf_stream_t *);
+file_protected void cdf_dump_catalog(const cdf_header_t *,
+    const cdf_stream_t *);
 #endif
 
 
