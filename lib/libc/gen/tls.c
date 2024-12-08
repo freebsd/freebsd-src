05fe8c88e1ce7904a14997a5e5e253d92fbe3e42
Maxim Sobolev <sobomax@FreeBSD.org>
2024-12-08 11:08:43 -0800

    libc: lib_malloc_aligned(): add a missing NULL check
    For some reason return value of the __je_bootstrap_malloc()
    is not checked and then de-referenced few lines below, causing
    a SEGV if an early allocation fails.
    
    MFC after:      1 month
    
    (cherry picked from commit 1e99be5dcda222d47a77715e190a381a14f46ece)

--- lib/libc/gen/tls.c
+++ lib/libc/gen/tls.c
@@ -113,6 +113,8 @@ libc_malloc_aligned(size_t size, size_t align)
 		align = sizeof(void *);
 
 	mem = __je_bootstrap_malloc(size + sizeof(void *) + align - 1);
+	if (mem == NULL)
+		return (NULL);
 	res = (void *)roundup2((uintptr_t)mem + sizeof(void *), align);
 	*(void **)((uintptr_t)res - sizeof(void *)) = mem;
 	return (res);
