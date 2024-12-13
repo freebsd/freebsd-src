76c246851f024628ec50d9c2e4082ebcd669366d
Warner Losh <imp@FreeBSD.org>
2024-12-13 15:49:42 -0700

    jemalloc: Retire SWAP_RESERVE_* compat shims
    We no longer need these compat shims. They were needed for FreeBSD 12
    and earlier.
    
    Sponsored by:		Netflix

--- contrib/jemalloc/src/pages.c
+++ contrib/jemalloc/src/pages.c
@@ -461,10 +461,6 @@ os_overcommits_sysctl(void) {
 	}
 #endif
 
-#ifndef SWAP_RESERVE_FORCE_ON
-#define	SWAP_RESERVE_FORCE_ON		(1 << 0)
-#define	SWAP_RESERVE_RLIMIT_ON		(1 << 1)
-#endif
 	return ((vm_overcommit & (SWAP_RESERVE_FORCE_ON |
 	    SWAP_RESERVE_RLIMIT_ON)) == 0);
 }
