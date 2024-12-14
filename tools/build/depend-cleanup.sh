freebsd/freebsd-src

commit 42ee30f19cc7ad85187cbf97155dbd5207a04845

depend-cleanup.sh: Need to delete jemalloc.3 in objtree
With the jemalloc.3 move into the tree (it's a generated file that we
don't/can't generate as part of the build), we need to remove it from
the .OBJDIR or make gets confused about how to build it (honestly for
reasons unknown).

Sponsored by:		Netflix

tools/build/depend-cleanup.sh
@@ -231,3 +231,11 @@ clean_dep   lib/libcrypt crypt-md5    c
 clean_dep   lib/libcrypt crypt-nthash c
 clean_dep   lib/libcrypt crypt-sha256 c
 clean_dep   lib/libcrypt crypt-sha512 c
+
+# 20241213  b55f5e1c4ae3  jemalloc: Move generated jemalloc.3 into lib/libc tree
+if [ -e "$OBJTOP"/lib/libc/jemalloc.3 ]; then
+	# Have to cleanup the jemalloc.3 in the obj tree since make gets
+	# confused and won't use the one in lib/libc/malloc/jemalloc/jemalloc.3
+	echo "Removing stale jemalloc.3 object"
+	run rm -f "$OBJTOP"/lib/libc/jemalloc.3
+fi

commit 1d69498623c0eced2de0f97cd553aff81a936911

depend-cleanup: Fix test arg
-e follows the symlink to see if the file exists. I tested on the wrong
tree, so it appeared to work for me. Change to -h which see if the link
is there or not. The file will never not be a link if present.

Fixes: 42ee30f19cc7
Noticed by: Herbert Skuhra
Sponsored by: Netflix

tools/build/depend-cleanup.sh
@@ -233,7 +233,7 @@ clean_dep   lib/libcrypt crypt-sha256 c
 clean_dep   lib/libcrypt crypt-sha512 c
 
 # 20241213  b55f5e1c4ae3  jemalloc: Move generated jemalloc.3 into lib/libc tree
-if [ -e "$OBJTOP"/lib/libc/jemalloc.3 ]; then
+if [ -h "$OBJTOP"/lib/libc/jemalloc.3 ]; then
 	# Have to cleanup the jemalloc.3 in the obj tree since make gets
 	# confused and won't use the one in lib/libc/malloc/jemalloc/jemalloc.3
 	echo "Removing stale jemalloc.3 object"