2bc951c18b68e6c2752ab2a188987f2e6eb5d5ae
Elliott Mitchell <ehem+freebsd@m5p.com>
2024-12-13 20:40:00 -0800

    intr/x86: change intr_register_pic()'s return to void
    The one error detected by intr_register_pic() isn't really crucial.  As
    such most callers are ignoring the integer return.  In case of a debug
    kernel we really do want this noticed, but for a release kernel this
    may not cause harm.
    
    panic() is a bit overkill, but but this is extremely improbable and we
    do want it to be noticed if it somehow occurs.  For non-debug kernels
    simply leave it at a printf().

--- sys/x86/x86/intr_machdep.c
+++ sys/x86/x86/intr_machdep.c
@@ -140,20 +140,22 @@ intr_pic_registered(struct pic *pic)
  * sources.  This also allows controllers with no active sources (such as
  * 8259As in a system using the APICs) to participate in suspend and resume.
  */
-int
+void
 intr_register_pic(struct pic *pic)
 {
-	int error;
 
 	mtx_lock(&intrpic_lock);
-	if (intr_pic_registered(pic))
-		error = EBUSY;
-	else {
+	if (__predict_false(intr_pic_registered(pic)))
+#if defined(WITNESS) || defined(INVARIANTS)
+		panic
+#else
+		printf
+#endif
+		    ("ERROR: %s: called with already registered PIC",
+		    __func__);
+	else
 		TAILQ_INSERT_TAIL(&pics, pic, pics);
-		error = 0;
-	}
 	mtx_unlock(&intrpic_lock);
-	return (error);
 }
 
 /*
