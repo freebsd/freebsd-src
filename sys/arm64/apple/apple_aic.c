325b953fbe444dd49b125bf650d9d09f05cc6399
Kyle Evans <kevans@FreeBSD.org>
2024-12-17 09:37:27 -0600

    arm64: apple: re-align to recent standards
    In the time these have lingered in my tree, we've started to drop the
    __FBSDID tags.  Make sure we're using SPDX tags (and the correct one)
    as well.
    
    Reported by:	mhorne

--- sys/arm64/apple/apple_aic.c
+++ sys/arm64/apple/apple_aic.c
@@ -1,4 +1,6 @@
 /*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
  * Copyright (c) 2021 Andrew Turner
  * Copyright (c) 2022 Michael J. Karels <karels@freebsd.org>
  * Copyright (c) 2022 Kyle Evans <kevans@FreeBSD.org>
