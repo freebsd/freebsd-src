830e08ad1b57a34c3b7aef9b8de987cae0b87bb1
Cy Schubert <cy@FreeBSD.org>
2024-12-23 14:30:58 -0800

    ntp: Undo upstream (ntp.org) fix for upstream Bug 3851
    The patch for upstream (ntp.org) fix for upstream Bug 3851 may have
    fixed a Linux bug but it caused a regression when ntpd is run on
    FreeBSD.
    
    Suggested that so@ publish an errata and merge this to releng/14.2.
    
    PR:		283116
    MFH:		3 days

--- contrib/ntp/ntpd/ntp_proto.c
+++ contrib/ntp/ntpd/ntp_proto.c
@@ -471,6 +471,7 @@ transmit(
 		return;
 	}
 
+#if 0
 	/* [Bug 3851] drop pool servers which can no longer be reached. */
 	if (MDF_PCLNT & peer->cast_flags) {
 		if (   (IS_IPV6(&peer->srcadr) && !nonlocal_v6_addr_up)
@@ -479,6 +480,7 @@ transmit(
 			return;
 		}
 	}
+#endif
 
 	 /*
 	 * In unicast modes the dance is much more intricate. It is
