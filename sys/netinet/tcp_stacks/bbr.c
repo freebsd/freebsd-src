0d93b08b494225969358c1af8ed65e99b137aed8
Michael Tuexen <tuexen@FreeBSD.org>
2025-01-03 14:22:25 +0100

    TCP BBR: simplify expression
    rsm cannot be NULL, when calling bbr_update_bbr_info().
    So no need to check partially for it. No functional change intended.
    
    Reviewed by:		rrs
    CID:			1523803
    Sponsored by:		Netflix, Inc.
    Differential Revision:	https://reviews.freebsd.org/D48293
    
    (cherry picked from commit 4173a3a009a42c47f1ec2d1cbfb99347aaf195da)

--- sys/netinet/tcp_stacks/bbr.c
+++ sys/netinet/tcp_stacks/bbr.c
@@ -6745,7 +6745,7 @@ bbr_update_bbr_info(struct tcp_bbr *bbr, struct bbr_sendmap *rsm, uint32_t rtt, 
 		bbr_log_rtt_shrinks(bbr, cts, 0, rtt, __LINE__, BBR_RTTS_NEWRTT, 0);
 		bbr_set_reduced_rtt(bbr, cts, __LINE__);
 	}
-	bbr_log_type_bbrrttprop(bbr, rtt, (rsm ? rsm->r_end : 0), uts, cts,
+	bbr_log_type_bbrrttprop(bbr, rtt, rsm->r_end, uts, cts,
 	    match, rsm->r_start, rsm->r_flags);
 	apply_filter_min_small(&bbr->r_ctl.rc_rttprop, rtt, cts);
 	if (old_rttprop != bbr_get_rtt(bbr, BBR_RTT_PROP)) {
