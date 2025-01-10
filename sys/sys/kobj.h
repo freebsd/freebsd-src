de169ceb662d939afb4dfb0cabc067ca756c21dc
Elliott Mitchell <ehem+freebsd@m5p.com>
2025-01-09 16:45:05 -0800

    kobj: convert KOBJOPLOOKUP() to returning pointer to function
    This makes KOBJOPLOOKUP() rather more suitable for purposes outside
    the macros generated by `makeobjops.awk`.  The magic variable name "_m"
    isn't suitable for anything besides the auto-generated files.  Whereas
    returning a function pointer is suitable for explicit lookup on a known
    parent class, or caching the function pointer in a location outside the
    lookup cache.
    
    Differential Revision: https://reviews.freebsd.org/D48408

--- sys/sys/kobj.h
+++ sys/sys/kobj.h
@@ -247,7 +247,7 @@ extern unsigned int kobj_lookup_misses;
  * slow way.
  */
 #ifdef KOBJ_STATS
-#define KOBJOPLOOKUP(OPS,OP) do {				\
+#define KOBJOPLOOKUP(OPS, OP) __extension__ ({			\
 	kobjop_desc_t _desc = &OP##_##desc;			\
 	kobj_method_t **_cep =					\
 	    &OPS->cache[_desc->id & (KOBJ_CACHE_SIZE-1)];	\
@@ -258,10 +258,10 @@ extern unsigned int kobj_lookup_misses;
 		kobj_lookup_misses++;				\
 	} else							\
 		kobj_lookup_hits++;				\
-	_m = _ce->func;						\
-} while (0)
+	(OP##_t *)(_ce->func);					\
+})
 #else
-#define KOBJOPLOOKUP(OPS,OP) do {				\
+#define KOBJOPLOOKUP(OPS, OP) __extension__ ({			\
 	kobjop_desc_t _desc = &OP##_##desc;			\
 	kobj_method_t **_cep =					\
 	    &OPS->cache[_desc->id & (KOBJ_CACHE_SIZE-1)];	\
@@ -269,8 +269,8 @@ extern unsigned int kobj_lookup_misses;
 	if (_ce->desc != _desc)					\
 		_ce = kobj_lookup_method(OPS->cls,		\
 					 _cep, _desc);		\
-	_m = _ce->func;						\
-} while (0)
+	(OP##_t *)(_ce->func);					\
+})
 #endif
 
 kobj_method_t* kobj_lookup_method(kobj_class_t cls,
