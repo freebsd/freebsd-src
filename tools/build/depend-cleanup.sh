freebsd/freebsd-src

commit d57842ed2dad1c9bb8ae7271a8b5df772ac5213b

depend-cleanup.sh: clean up after AArch64 libc SIMD merge
This should cover all the changed functions.

Event:		GSoC 2024
Reported by:	andrew
Approved by:	emaste
Differential Revision: https://reviews.freebsd.org/D48443

tools/build/depend-cleanup.sh
@@ -239,3 +239,48 @@ if [ -h "$OBJTOP"/lib/libc/jemalloc.3 ]; then
 	echo "Removing stale jemalloc.3 object"
 	run rm -f "$OBJTOP"/lib/libc/jemalloc.3
 fi
+
+if [ $MACHINE_ARCH = aarch64 ]; then
+	# 20250110  5e7d93a60440  add strcmp SIMD implementation
+	clean_dep   lib/libc strcmp S
+
+	# 20250110  b91003acffe7  add strspn optimized implementation
+	clean_dep   lib/libc strspn c
+
+	# 20250110  f2bd390a54f1  add strcspn optimized implementation
+	clean_dep   lib/libc strcspn c
+
+	# 20250110  89b3872376cb  add optimized strpbrk & strsep implementations
+	clean_dep   lib/libc strpbrk c
+
+	# 20250110  79287d783c72  strcat enable use of SIMD
+	clean_dep   lib/libc strcat c
+
+	# 20250110  756b7fc80837  add strlcpy SIMD implementation
+	clean_dep   lib/libc strlcpy c
+
+	# 20250110  25c485e14769  add strncmp SIMD implementation
+	clean_dep   lib/libc strncmp S
+
+	# 20250110  bad17991c06d  add memccpy SIMD implementation
+	clean_dep   lib/libc memccpy c
+
+	# 20250110  3dc5429158cf  add strncat SIMD implementation
+	clean_dep   lib/libc strncat c
+
+	# 20250110  bea89d038ac5  add strlcat SIMD implementation
+	clean_dep   lib/libc strlcat c
+
+	# 20250110  3863fec1ce2d  add strlen SIMD implementation
+	clean_dep   lib/libc strlen S
+
+	# 20250110  79e01e7e643c  add bcopy & bzero wrapper
+	clean_dep   lib/libc bcopy c
+	clean_dep   lib/libc bzero c
+
+	# 20250110  f2c98669fc1b  add ASIMD-enhanced timingsafe_bcmp implementation
+	clean_dep   lib/libc timingsafe_bcmp c
+
+	# 20250110  3f224333af16  add timingsafe_memcmp() assembly implementation
+	clean_dep   lib/libc timingsafe_memcmp c
+fi

commit c0661bbbd0d917f602d42fe4bb9ee3edc5970c8a

depend-cleanup: Fix match expression for AArch64 asm files
Some routines (strcmp, strncmp, strlen) switched from an asm
implementation from arm-optimized-routines to a new asm implementation.
The default clean_dep rule matches both the old and new dependency
string, resulting in those routines always being rebuilt.  Instead use
"arm-optimized-routines" (i.e., contrib subdir for the previous
implementation) as the matching regex.

Also delete the old generated src files.

Reviewed by:	fuz
Sponsored by:	The FreeBSD Foundation
Fixes: d57842ed2dad ("depend-cleanup.sh: clean up after AArch64 libc SIMD merge")
Differential Revision: https://reviews.freebsd.org/D48459

tools/build/depend-cleanup.sh
@@ -242,7 +242,8 @@ fi
 
 if [ $MACHINE_ARCH = aarch64 ]; then
 	# 20250110  5e7d93a60440  add strcmp SIMD implementation
-	clean_dep   lib/libc strcmp S
+	clean_dep   lib/libc strcmp S arm-optimized-routines
+	run rm -f "$OBJTOP"/lib/libc/strcmp.S
 
 	# 20250110  b91003acffe7  add strspn optimized implementation
 	clean_dep   lib/libc strspn c
@@ -260,7 +261,8 @@ if [ $MACHINE_ARCH = aarch64 ]; then
 	clean_dep   lib/libc strlcpy c
 
 	# 20250110  25c485e14769  add strncmp SIMD implementation
-	clean_dep   lib/libc strncmp S
+	clean_dep   lib/libc strncmp S arm-optimized-routines
+	run rm -f "$OBJTOP"/lib/libc/strncmp.S
 
 	# 20250110  bad17991c06d  add memccpy SIMD implementation
 	clean_dep   lib/libc memccpy c
@@ -272,7 +274,8 @@ if [ $MACHINE_ARCH = aarch64 ]; then
 	clean_dep   lib/libc strlcat c
 
 	# 20250110  3863fec1ce2d  add strlen SIMD implementation
-	clean_dep   lib/libc strlen S
+	clean_dep   lib/libc strlen S arm-optimized-routines
+	run rm -f "$OBJTOP"/lib/libc/strlen.S
 
 	# 20250110  79e01e7e643c  add bcopy & bzero wrapper
 	clean_dep   lib/libc bcopy c