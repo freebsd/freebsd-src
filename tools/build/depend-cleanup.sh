freebsd/freebsd-src

commit 7da3772a721519f2cfea3d79340ecca19979d7ab

lib/libc/aarch64/string: add strlcat SIMD implementation
This patch requires D46243 as it depends on strlcpy being labeled
__strlcpy.

It's a direct copy from the amd64 string functions using memchr and
strlcpy to implement strlcat.

Tested by:	fuz (exprun)
Reviewed by:	fuz, emaste
Sponsored by:	Google LLC (GSoC 2024)
PR:		281175
Differential Revision: https://reviews.freebsd.org/D46272

lib/libc/aarch64/string/Makefile.inc
@@ -29,7 +29,8 @@ MDSRCS+= \
 	strlcpy.S \
 	strncmp.S \
 	memccpy.S \
-	strncat.c
+	strncat.c \
+	strlcat.c
 
 #
 # Add the above functions. Generate an asm file that includes the needed

lib/libc/aarch64/string/memchr.S
@@ -1,0 +1,4 @@
+	.weak memchr
+	.set memchr, __memchr_aarch64
+
+#include "aarch64/memchr.S"

lib/libc/aarch64/string/strlcat.c
@@ -1,0 +1,25 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2023 Robert Clausecker
+ */
+
+#include <sys/cdefs.h>
+
+#include <string.h>
+
+void *__memchr_aarch64(const void *, int, size_t);
+size_t __strlcpy(char *restrict, const char *restrict, size_t);
+
+size_t
+strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+{
+	char *loc = __memchr_aarch64(dst, '\0', dstsize);
+
+	if (loc != NULL) {
+		size_t dstlen = (size_t)(loc - dst);
+
+		return (dstlen + __strlcpy(loc, src, dstsize - dstlen));
+	} else
+		return (dstsize + strlen(src));
+}

commit 4c7f3626c213584eecdea8ec6e1344139cfbb748

depend-cleanup: 7da3772a7215 also moved memchr
Commit 7da3772a7215 added strlcat but also introduced a memchr wrapper
in the source tree rather than using the autogenerated one used for
AARCH64_STRING_FUNCS.  Move memchr.S to MDSRS and add a cleanup rule for
the old wrapper.

Reviewed by:	fuz
Sponsored by:	The FreeBSD Foundation
Fixes: 7da3772a7215 ("lib/libc/aarch64/string: add strlcat SIMD implementation")
Differential Revision: https://reviews.freebsd.org/D48502

lib/libc/aarch64/string/Makefile.inc
@@ -4,7 +4,6 @@
 #
 
 AARCH64_STRING_FUNCS= \
-	memchr \
 	memcmp \
 	memcpy \
 	memmove \
@@ -19,6 +18,7 @@ AARCH64_STRING_FUNCS= \
 
 # SIMD-enhanced routines not derived from Arm's code
 MDSRCS+= \
+	memchr.S \
 	strcmp.S \
 	strspn.S \
 	strcspn.S \

tools/build/depend-cleanup.sh
@@ -270,8 +270,9 @@ if [ $MACHINE_ARCH = aarch64 ]; then
 	# 20250110  3dc5429158cf  add strncat SIMD implementation
 	clean_dep   lib/libc strncat c
 
-	# 20250110  bea89d038ac5  add strlcat SIMD implementation
+	# 20250110  bea89d038ac5  add strlcat SIMD implementation, and move memchr
 	clean_dep   lib/libc strlcat c
+	run rm -f "$OBJTOP"/lib/libc/memchr.S
 
 	# 20250110  3863fec1ce2d  add strlen SIMD implementation
 	clean_dep   lib/libc strlen S arm-optimized-routines