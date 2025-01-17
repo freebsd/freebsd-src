b06104ed8e8f13bf9f04433f616aa2e723ba65ba
Alan Somers <asomers@FreeBSD.org>
2025-01-17 10:36:08 -0700

    ctld: correctly parse LUN size on 32-bit arches
    Disk offset values must always be 64-bit, not size_t.
    
    PR:		214874
    Submitted by:	pprocacci@gmail.com
    MFC after:	2 weeks
    Event:		January 2025 Bug-busting session

--- usr.sbin/ctld/ctld.c
+++ usr.sbin/ctld/ctld.c
@@ -1568,7 +1568,7 @@ lun_set_serial(struct lun *lun, const char *value)
 }
 
 void
-lun_set_size(struct lun *lun, size_t value)
+lun_set_size(struct lun *lun, int64_t value)
 {
 
 	lun->l_size = value;
