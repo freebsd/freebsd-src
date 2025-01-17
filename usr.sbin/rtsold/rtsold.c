3ba946aebf747e12da4fb22ee0e45ee0e3a233ee
Mark Johnston <markj@FreeBSD.org>
2025-01-17 15:14:22 +0000

    rtsold: Add -i to the usage message
    PR:		266634
    Fixes:		231bac4ccc43 ("rtsol/rtsold: Add option to skip random delay")
    MFC after:	1 week
    
    (cherry picked from commit 9e9e64b693ab1e08cca46e26e468160542c33f3a)
    (cherry picked from commit c4380d5b9383e2a062840573b36ab643b65b2610)

--- usr.sbin/rtsold/rtsold.c
+++ usr.sbin/rtsold/rtsold.c
@@ -792,17 +792,17 @@ usage(const char *progname)
 {
 
 	if (strcmp(progname, "rtsold") == 0) {
-		fprintf(stderr, "usage: rtsold [-dDfFm1] [-O script-name] "
+		fprintf(stderr, "usage: rtsold [-dDfFim1] [-O script-name] "
 		    "[-M script-name ] [-A script-name ] "
 		    "[-p pidfile] [-R script-name] interface ...\n");
-		fprintf(stderr, "usage: rtsold [-dDfFm1] [-O script-name] "
+		fprintf(stderr, "usage: rtsold [-dDfFim1] [-O script-name] "
 		    "[-M script-name ] [-A script-name ] "
 		    "[-p pidfile] [-R script-name] -a\n");
 	} else {
-		fprintf(stderr, "usage: rtsol [-dDF] [-O script-name] "
+		fprintf(stderr, "usage: rtsol [-dDFi] [-O script-name] "
 		    "[-M script-name ] [-A script-name ] "
 		    "[-p pidfile] [-R script-name] interface ...\n");
-		fprintf(stderr, "usage: rtsol [-dDF] [-O script-name] "
+		fprintf(stderr, "usage: rtsol [-dDFi] [-O script-name] "
 		    "[-M script-name ] [-A script-name ] "
 		    "[-p pidfile] [-R script-name] -a\n");
 	}
