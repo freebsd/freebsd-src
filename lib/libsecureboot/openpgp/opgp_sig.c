24601af72bfce0a2106453072288b0d58515f1b1
Simon J. Gerraty <sjg@FreeBSD.org>
2025-01-20 12:56:44 -0800

    libsecureboot add sha384 and sha512 for OpenPGP
    gpg supports SHA384, SHA512 as well as SHA256 so allow for them.
    
    Tweak Makefile.inc so we can build libsecureboot with only OpenPGP
    trust anchors.
    
    Reviewed by: imp
    Differential Revision:	https://reviews.freebsd.org/D48546

--- lib/libsecureboot/openpgp/opgp_sig.c
+++ lib/libsecureboot/openpgp/opgp_sig.c
@@ -339,6 +339,16 @@ openpgp_verify(const char *filename,
 				mlen = br_sha256_SIZE;
 				hash_oid = BR_HASH_OID_SHA256;
 				break;
+			case 9:			/* sha384 */
+				md = &br_sha384_vtable;
+				mlen = br_sha384_SIZE;
+				hash_oid = BR_HASH_OID_SHA384;
+				break;
+			case 10:		/* sha512 */
+				md = &br_sha512_vtable;
+				mlen = br_sha512_SIZE;
+				hash_oid = BR_HASH_OID_SHA512;
+				break;
 			default:
 				warnx("unsupported hash algorithm: %s", hname);
 				rc = -1;
