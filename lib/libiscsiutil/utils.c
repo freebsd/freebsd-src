dd3603749cb7f20a628f04d595b105962b21a3d2
John Baldwin <jhb@FreeBSD.org>
2025-01-24 09:52:10 -0500

    iscsi: Move valid_iscsi_name to libiscsiutil
    While here, use isxdigit(3) instead of a home-rolled version.
    
    Reviewed by:	mav, asomers
    Sponsored by:	Chelsio Communications
    Differential Revision:	https://reviews.freebsd.org/D48593

--- lib/libiscsiutil/utils.c
+++ lib/libiscsiutil/utils.c
@@ -28,9 +28,12 @@
  * SUCH DAMAGE.
  */
 
+#include <ctype.h>
 #include <string.h>
 
 #include "libiscsiutil.h"
+
+#define	MAX_NAME_LEN			223
 
 char *
 checked_strdup(const char *s)
@@ -41,4 +44,72 @@ checked_strdup(const char *s)
 	if (c == NULL)
 		log_err(1, "strdup");
 	return (c);
+}
+
+bool
+valid_iscsi_name(const char *name, void (*warn_fn)(const char *, ...))
+{
+	int i;
+
+	if (strlen(name) >= MAX_NAME_LEN) {
+		warn_fn("overlong name for target \"%s\"; max length allowed "
+		    "by iSCSI specification is %d characters",
+		    name, MAX_NAME_LEN);
+		return (false);
+	}
+
+	/*
+	 * In the cases below, we don't return an error, just in case the admin
+	 * was right, and we're wrong.
+	 */
+	if (strncasecmp(name, "iqn.", strlen("iqn.")) == 0) {
+		for (i = strlen("iqn."); name[i] != '\0'; i++) {
+			/*
+			 * XXX: We should verify UTF-8 normalisation, as defined
+			 *      by 3.2.6.2: iSCSI Name Encoding.
+			 */
+			if (isalnum(name[i]))
+				continue;
+			if (name[i] == '-' || name[i] == '.' || name[i] == ':')
+				continue;
+			warn_fn("invalid character \"%c\" in target name "
+			    "\"%s\"; allowed characters are letters, digits, "
+			    "'-', '.', and ':'", name[i], name);
+			break;
+		}
+		/*
+		 * XXX: Check more stuff: valid date and a valid reversed domain.
+		 */
+	} else if (strncasecmp(name, "eui.", strlen("eui.")) == 0) {
+		if (strlen(name) != strlen("eui.") + 16)
+			warn_fn("invalid target name \"%s\"; the \"eui.\" "
+			    "should be followed by exactly 16 hexadecimal "
+			    "digits", name);
+		for (i = strlen("eui."); name[i] != '\0'; i++) {
+			if (!isxdigit(name[i])) {
+				warn_fn("invalid character \"%c\" in target "
+				    "name \"%s\"; allowed characters are 1-9 "
+				    "and A-F", name[i], name);
+				break;
+			}
+		}
+	} else if (strncasecmp(name, "naa.", strlen("naa.")) == 0) {
+		if (strlen(name) > strlen("naa.") + 32)
+			warn_fn("invalid target name \"%s\"; the \"naa.\" "
+			    "should be followed by at most 32 hexadecimal "
+			    "digits", name);
+		for (i = strlen("naa."); name[i] != '\0'; i++) {
+			if (!isxdigit(name[i])) {
+				warn_fn("invalid character \"%c\" in target "
+				    "name \"%s\"; allowed characters are 1-9 "
+				    "and A-F", name[i], name);
+				break;
+			}
+		}
+	} else {
+		warn_fn("invalid target name \"%s\"; should start with "
+		    "either \"iqn.\", \"eui.\", or \"naa.\"",
+		    name);
+	}
+	return (true);
 }
