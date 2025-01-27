58e82ad8f0b5d4a9d4cd4b9170896a595e1b1fb3
Konstantin Belousov <kib@FreeBSD.org>
2025-01-27 22:38:27 +0200

    crtend: accurately check for the start of .ctors
    For the hypothetic situation where crtbegin.o is not linked into the
    binary, but crtend.o is, which results in the missing starting sentinel
    in the ctors array, be careful to not iterate past the start of the
    section.
    
    Reviewed by:	andrew, dim
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D48700

--- lib/csu/common/crtend.c
+++ lib/csu/common/crtend.c
@@ -21,7 +21,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <sys/cdefs.h>
+#include <sys/types.h>
 #include "crt.h"
 
 typedef void (*crt_func)(void);
@@ -45,15 +45,22 @@ static crt_func __DTOR_END__[] __section(".dtors") __used = {
 	(crt_func)0
 };
 
+extern const char startof_ctors[] __asm(".startof..ctors")
+    __weak_symbol __hidden;
+
 static void
 __do_global_ctors_aux(void)
 {
 	crt_func fn;
+	uintptr_t ctors_start;
 	int n;
 
+	ctors_start = (uintptr_t)&startof_ctors;
 	for (n = 1;; n++) {
 		fn = __CTOR_END__[-n];
-		if (fn == (crt_func)0 || fn == (crt_func)-1)
+		if (fn == (crt_func)0 || fn == (crt_func)-1 ||
+		    (ctors_start > 0 &&
+		    (uintptr_t)&__CTOR_END__[-n] < ctors_start))
 			break;
 		fn();
 	}
