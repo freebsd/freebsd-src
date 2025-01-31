freebsd/freebsd-src

commit 4cc5d081d8c23020130c5f30ed31f79fa105de74

mlx5en: only enable to toggle offload caps if they are supported
Reviewed by:	Ariel Ehrenberg <aehrenberg@nvidia.com>
Sponsored by:	NVidia networking
MFC after:	1 week

sys/dev/mlx5/mlx5_en/mlx5_en_main.c
@@ -3552,6 +3552,24 @@ mlx5e_ioctl(if_t ifp, u_long command, caddr_t data)
 		drv_ioctl_data = (struct siocsifcapnv_driver_data *)data;
 		PRIV_LOCK(priv);
 siocsifcap_driver:
+		if (!mlx5e_is_tlstx_capable(priv->mdev)) {
+			drv_ioctl_data->reqcap &= ~(IFCAP_TXTLS4 |
+			    IFCAP_TXTLS6);
+		}
+		if (!mlx5e_is_tlsrx_capable(priv->mdev)) {
+		        drv_ioctl_data->reqcap &= ~(
+			    IFCAP2_BIT(IFCAP2_RXTLS4) |
+			    IFCAP2_BIT(IFCAP2_RXTLS6));
+		}
+		if (!mlx5e_is_ipsec_capable(priv->mdev)) {
+			drv_ioctl_data->reqcap &=
+			    ~IFCAP2_BIT(IFCAP2_IPSEC_OFFLOAD);
+		}
+		if (!mlx5e_is_ratelimit_capable(priv->mdev)) {
+			drv_ioctl_data->reqcap &= ~(IFCAP_TXTLS_RTLMT |
+			    IFCAP_TXRTLMT);
+		}
+
 		mask = drv_ioctl_data->reqcap ^ if_getcapenable(ifp);
 
 		if (mask & IFCAP_TXCSUM) {

commit 36fdc42c6a4c828d334471438c4f852e4b5a25e2

mlx5en: Fix SIOCSIFCAPNV
In 4cc5d081d8c23, a change was introduced that manipulated
drv_ioctl_data->reqcap using IFCAP2 bits.  This was noticed
when creating a mixed lagg with mce0 and ixl0 caused the
interfaces' txcsum caps to be disabled.

Fixes: 4cc5d081d8c23
Reviewed by: glebius
Sponsored by: Netflix
MFC After: 7 days

sys/dev/mlx5/mlx5_en/mlx5_en_main.c
@@ -3557,12 +3557,12 @@ siocsifcap_driver:
 			    IFCAP_TXTLS6);
 		}
 		if (!mlx5e_is_tlsrx_capable(priv->mdev)) {
-		        drv_ioctl_data->reqcap &= ~(
+		        drv_ioctl_data->reqcap2 &= ~(
 			    IFCAP2_BIT(IFCAP2_RXTLS4) |
 			    IFCAP2_BIT(IFCAP2_RXTLS6));
 		}
 		if (!mlx5e_is_ipsec_capable(priv->mdev)) {
-			drv_ioctl_data->reqcap &=
+			drv_ioctl_data->reqcap2 &=
 			    ~IFCAP2_BIT(IFCAP2_IPSEC_OFFLOAD);
 		}
 		if (!mlx5e_is_ratelimit_capable(priv->mdev)) {