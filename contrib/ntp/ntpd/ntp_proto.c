61e5e864ca19a1a6cfef859cbaf1ae6795ca0bed
Cy Schubert <cy@FreeBSD.org>
2025-02-03 15:16:08 -0800

    ntp: Another patch to address IPv6 pool regression
    98e34e8e2557 circumvented an upstream patch which caused an IPv6
    pool regression. This patch, discussed in
    https://bugs.ntp.org/show_bug.cgi?id=3958, addresses another
    unworkable combination of link-local local address with
    non-link-local server.
    
    (cherry picked from commit c1767cf87cb64c25426fd7fe119be283b134509a)

--- contrib/ntp/ntpd/ntp_proto.c
+++ contrib/ntp/ntpd/ntp_proto.c
@@ -475,7 +475,7 @@ transmit(
 	/* [Bug 3851] drop pool servers which can no longer be reached. */
 	if (MDF_PCLNT & peer->cast_flags) {
 		if (   (IS_IPV6(&peer->srcadr) && !nonlocal_v6_addr_up)
-		    || !nonlocal_v4_addr_up) {
+		    || (IS_IPV4(&peer->srcadr) && !nonlocal_v4_addr_up)) {
 			unpeer(peer);
 			return;
 		}
