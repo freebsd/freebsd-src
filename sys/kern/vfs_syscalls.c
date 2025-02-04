1a0cb938f7b461edc40316cb44a80438df674174
Konstantin Belousov <kib@FreeBSD.org>
2025-02-04 02:23:05 +0200

    unlink, rmdir: call notify_upper from VOP pre method instead of syscall
    Suppose that there are two or more nullfs mounts over some fs, and
    suppose the we unlink a file on one of the nullfs mount.
    This way notify_upper get called from the lower vnode as well, allowing
    the other nullfs mounts to note that and drop their caches for the
    unlinked vnode.
    
    PR:	254210
    Reviewed by:	olce
    Tested by:	pho
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D48825

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -2034,7 +2034,6 @@ restart:
 		if (error != 0)
 			goto out;
 #endif
-		vfs_notify_upper(vp, VFS_NOTIFY_UPPER_UNLINK);
 		error = VOP_REMOVE(nd.ni_dvp, vp, &nd.ni_cnd);
 #ifdef MAC
 out:
@@ -4002,7 +4001,6 @@ restart:
 			goto fdout;
 		goto restart;
 	}
-	vfs_notify_upper(vp, VFS_NOTIFY_UPPER_UNLINK);
 	error = VOP_RMDIR(nd.ni_dvp, nd.ni_vp, &nd.ni_cnd);
 	vn_finished_write(mp);
 out:
