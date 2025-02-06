d8413a1c3ba235a79ae6b8cc35767a861855c7e2
Zhenlei Huang <zlei@FreeBSD.org>
2025-02-07 02:32:05 +0800

    ifnet: Fix build without BPF
    The newly introduced function bpf_ifdetach() is only available when
    device bpf is enabled.
    
    Fixes:	1ed9b381d470 ifnet: Detach BPF descriptors on interface vmove event

--- sys/net/if.c
+++ sys/net/if.c
@@ -1262,10 +1262,12 @@ finish_vnet_shutdown:
 static void
 if_vmove(struct ifnet *ifp, struct vnet *new_vnet)
 {
+#ifdef DEV_BPF
 	/*
 	 * Detach BPF file descriptors from its interface.
 	 */
 	bpf_ifdetach(ifp);
+#endif
 
 	/*
 	 * Detach from current vnet, but preserve LLADDR info, do not
