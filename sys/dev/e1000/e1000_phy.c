253a2989cd5a0047963196e94ff727005bf3f405
Amir Avivi <amir.avivi@intel.com>
2025-02-06 16:08:39 +0000

    e1000: fix iterator type
    DPDK commit message
    
    net/e1000/base: fix iterator type
    
    Fix static analysis warning about comparison between types of
    incompatible width, which might lead to an infinite loop due to
    overflow.
    
    Fixes: af75078fece3 ("first public release")
    Cc: stable@dpdk.org
    
    Signed-off-by: Amir Avivi <amir.avivi@intel.com>
    Signed-off-by: Anatoly Burakov <anatoly.burakov@intel.com>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>
    
    Obtained from:	DPDK (3d36053991)
    
    (cherry picked from commit 15a0db2101cd67eccd0be2ddf1ad18be1ea5afd0)

--- sys/dev/e1000/e1000_phy.c
+++ sys/dev/e1000/e1000_phy.c
@@ -2360,7 +2360,8 @@ s32 e1000_phy_has_link_generic(struct e1000_hw *hw, u32 iterations,
 			       u32 usec_interval, bool *success)
 {
 	s32 ret_val = E1000_SUCCESS;
-	u16 i, phy_status;
+	u16 phy_status;
+	u32 i;
 
 	DEBUGFUNC("e1000_phy_has_link_generic");
 
