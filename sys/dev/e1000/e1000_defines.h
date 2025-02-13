4b29599fbbe33b75b7b58cfc5deea7a881e9a10e
Aurelien Cazuc <aurelien.cazuc.external@stormshield.eu>
2025-02-13 11:08:42 -0700

    e1000: Fix vlan PCP/DEI on lem(4)
    The vlan PCP and CFI/DEI were discarded when receiving vlan tagged
    packets on lem(4) interfaces with vlanhwtag.
    
    According to the 82540 SDM[1] (pg. 24), vlan tag is in the standard
    format, so there's no reason to discard PCP/DEI.
    
    [1]: http://iommu.com/datasheets/ethernet/controllers-nics/intel/e1000/pci-pci-x-family-gbe-controllers-software-dev-manual.pdf
    
    MFC after:	3 days
    Sponsored by:	Stormshield (author)
    Differential Revision:	https://reviews.freebsd.org/D48987

--- sys/dev/e1000/e1000_defines.h
+++ sys/dev/e1000/e1000_defines.h
@@ -130,7 +130,6 @@
 #define E1000_RXD_ERR_TCPE	0x20    /* TCP/UDP Checksum Error */
 #define E1000_RXD_ERR_IPE	0x40    /* IP Checksum Error */
 #define E1000_RXD_ERR_RXE	0x80    /* Rx Data Error */
-#define E1000_RXD_SPC_VLAN_MASK	0x0FFF  /* VLAN ID is in lower 12 bits */
 
 #define E1000_RXDEXT_STATERR_TST	0x00000100 /* Time Stamp taken */
 #define E1000_RXDEXT_STATERR_LB		0x00040000
