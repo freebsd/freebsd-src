3b1bff00f166341d20a8410af5b003e322210ebe
Konstantin Belousov <kib@FreeBSD.org>
2025-03-18 04:10:03 +0200

    descriptors: add fget_remote_foreach()
    Suggested by:	markj
    Reviewed by:	markj
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D49372

--- sys/sys/file.h
+++ sys/sys/file.h
@@ -267,6 +267,8 @@ int fget_fcntl(struct thread *td, int fd, cap_rights_t *rightsp,
     int needfcntl, struct file **fpp);
 int _fdrop(struct file *fp, struct thread *td);
 int fget_remote(struct thread *td, struct proc *p, int fd, struct file **fpp);
+int fget_remote_foreach(struct thread *td, struct proc *p,
+    int (*fn)(struct proc *, int, struct file *, void *), void *arg);
 
 fo_rdwr_t	invfo_rdwr;
 fo_truncate_t	invfo_truncate;
