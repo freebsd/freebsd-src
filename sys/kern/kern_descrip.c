4b3d44f3b43b8c8339d7f3a8a21cd6da47bd732c
Konstantin Belousov <kib@FreeBSD.org>
2025-03-18 03:50:01 +0200

    kern/kern_descrip.c:  remove unneeded headers
    (cherry picked from commit 6eb7cba469d7cf1beccfb18679d1e0bd4a468341)

--- sys/kern/kern_descrip.c
+++ sys/kern/kern_descrip.c
@@ -36,14 +36,11 @@
  *	@(#)kern_descrip.c	8.6 (Berkeley) 4/19/94
  */
 
-#include <sys/cdefs.h>
 #include "opt_capsicum.h"
 #include "opt_ddb.h"
 #include "opt_ktrace.h"
 
-#include <sys/param.h>
 #include <sys/systm.h>
-
 #include <sys/capsicum.h>
 #include <sys/conf.h>
 #include <sys/fcntl.h>
