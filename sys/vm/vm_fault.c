freebsd/freebsd-src

commit 4be25c5e2cec196240439115a3faf5f394f515d2

vm_fault: update pred lock acq in copy_entry
In vm_fault_copy_entry, variable 'mpred' is invalided as the
predecessor of the next page to be inserted when an allocation failure
leads to lock release and reacquisition.  Recompute it in that case.

Reported by:    markj
Reviewed by:    markj
Fixes:  vm_page: expose page_alloc_after 74ff78304d431dea81817e2ddc3
Differential Revision:  https://reviews.freebsd.org/D49521

sys/vm/vm_fault.c
@@ -2199,14 +2199,16 @@ again:
 			/*
 			 * Allocate a page in the destination object.
 			 */
-			dst_m = vm_page_alloc_after(dst_object, (src_object ==
-			    dst_object ? src_pindex : 0) + dst_pindex,
+			pindex = (src_object == dst_object ? src_pindex : 0) +
+			    dst_pindex;
+			dst_m = vm_page_alloc_after(dst_object, pindex,
 			    VM_ALLOC_NORMAL, mpred);
 			if (dst_m == NULL) {
 				VM_OBJECT_WUNLOCK(dst_object);
 				VM_OBJECT_RUNLOCK(object);
 				vm_wait(dst_object);
 				VM_OBJECT_WLOCK(dst_object);
+				mpred = vm_page_mpred(src_object, pindex);
 				goto again;
 			}
 

commit 6ab435de6f4e5bc7ce694e914bdada22c3623e6b

vm_fault: correct mpred update after alloc fail
Find the predecessor in dst_object, not src_object.

Fixes: 4be25c5e2cec <https://reviews.freebsd.org/rG6d6c97fb72a7dce85008cef891d093b24dcbb380> ("vm_fault: update pred lock acq in copy_entry")

Reported by:	markj
Reviewed by:	markj
Differential Revision:	https://reviews.freebsd.org/D49547

sys/vm/vm_fault.c
@@ -2208,7 +2208,7 @@ again:
 				VM_OBJECT_RUNLOCK(object);
 				vm_wait(dst_object);
 				VM_OBJECT_WLOCK(dst_object);
-				mpred = vm_page_mpred(src_object, pindex);
+				mpred = vm_page_mpred(dst_object, pindex);
 				goto again;
 			}
 