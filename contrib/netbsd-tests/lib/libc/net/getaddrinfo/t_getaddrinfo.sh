9b37d84c87e69dabc69d818aa4d2fea718bd8b74
Enji Cooper <ngie@FreeBSD.org>
2025-03-31 21:51:50 -0700

    Mark several getaddrinfo tests as XFAIL
    These tests were recently enabled on main and have failed consistently
    since they were enabled.
    
    - lib.libc.net.getaddrinfo.getaddrinfo.basic
    - lib.libc.net.getaddrinfo.getaddrinfo.nofamily
    - lib.libc.net.getaddrinfo.getaddrinfo_test.basic
    - lib.libc.net.getaddrinfo.getaddrinfo_test.empty_servname
    - lib.libc.net.getaddrinfo.getaddrinfo_test.sock_raw
    
    Mark them as expected failures so they no longer count as failures in
    Jenkins CI.
    
    PR:		285826
    MFC with:	5313457780, 0b773a94ab

--- contrib/netbsd-tests/lib/libc/net/getaddrinfo/t_getaddrinfo.sh
+++ contrib/netbsd-tests/lib/libc/net/getaddrinfo/t_getaddrinfo.sh
@@ -68,6 +68,12 @@ basic_body()
 {
 	TEST=$(atf_get_srcdir)/h_gai
 
+	# Begin FreeBSD
+	if [ "$(atf_config_get ci false)" = "true" ]; then
+		atf_expect_fail "https://bugs.freebsd.org/285826"
+	fi
+	# End FreeBSD
+
 	( $TEST ::1 http
 	  $TEST 127.0.0.1 http
 	  $TEST localhost http
@@ -128,6 +134,12 @@ empty_servname_body()
 {
 	TEST=$(atf_get_srcdir)/h_gai
 
+	# Begin FreeBSD
+	if [ "$(atf_config_get ci false)" = "true" ]; then
+		atf_expect_fail "https://bugs.freebsd.org/285826"
+	fi
+	# End FreeBSD
+
 	( $TEST ::1 ''
 	  $TEST 127.0.0.1 ''
 	  $TEST localhost ''
@@ -144,6 +156,12 @@ sock_raw_head()
 sock_raw_body()
 {
 	TEST=$(atf_get_srcdir)/h_gai
+
+	# Begin FreeBSD
+	if [ "$(atf_config_get ci false)" = "true" ]; then
+		atf_expect_fail "https://bugs.freebsd.org/285826"
+	fi
+	# End FreeBSD
 
 	( $TEST -R -p 0 localhost ''
 	  $TEST -R -p 59 localhost ''
