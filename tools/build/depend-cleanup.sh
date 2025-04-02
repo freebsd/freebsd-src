7feee915ace1adcbbeca6bdbaf68ac27dee13ce5
John Baldwin <jhb@FreeBSD.org>
2025-04-02 14:42:08 -0400

    depend-cleanup: Handle ctld moving from C to C++
    Reviewed by:	asomers
    Sponsored by:	Chelsio Communications
    Differential Revision:	https://reviews.freebsd.org/D49140

--- tools/build/depend-cleanup.sh
+++ tools/build/depend-cleanup.sh
@@ -337,3 +337,12 @@ if [ $MACHINE_ARCH = aarch64 ]; then
 	# 20250110  3f224333af16  add timingsafe_memcmp() assembly implementation
 	ALL_libcompats= clean_dep   lib/libc timingsafe_memcmp c
 fi
+
+# 20250402  839d0755fea8    ctld converted to C++
+clean_dep   usr.sbin/ctld   ctld c
+clean_dep   usr.sbin/ctld   conf c
+clean_dep   usr.sbin/ctld   discovery c
+clean_dep   usr.sbin/ctld   isns c
+clean_dep   usr.sbin/ctld   kernel c
+clean_dep   usr.sbin/ctld   login c
+clean_dep   usr.sbin/ctld   uclparse c
