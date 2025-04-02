e94d3f65b90107204d477a7fab059d8cf3b1b0c3
John Baldwin <jhb@FreeBSD.org>
2025-04-02 14:40:32 -0400

    libiscsiutil: Fix header to work with C++
    Reviewed by:	asomers
    Sponsored by:	Chelsio Communications
    Differential Revision:	https://reviews.freebsd.org/D49137

--- lib/libiscsiutil/libiscsiutil.h
+++ lib/libiscsiutil/libiscsiutil.h
@@ -95,6 +95,8 @@ struct rchap {
 	size_t		rchap_challenge_len;
 };
 
+__BEGIN_DECLS
+
 struct chap		*chap_new(void);
 char			*chap_get_id(const struct chap *chap);
 char			*chap_get_challenge(const struct chap *chap);
@@ -173,5 +175,7 @@ void			log_warnx(const char *, ...) __printflike(1, 2);
 void			log_debugx(const char *, ...) __printflike(1, 2);
 
 char			*checked_strdup(const char *);
+
+__END_DECLS
 
 #endif /* !__LIBISCSIUTIL_H__ */
