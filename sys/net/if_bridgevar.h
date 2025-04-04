2e4014e81f6c755b5977662917c2defbb697ff53
Lexi Winter <lexi@hemlock.eden.le-fay.org>
2025-04-04 11:59:09 +0100

    net/if_bridgevar.h: add include guard
    Reviewed by:	kp
    Approved by:	des (mentor)
    
    (cherry picked from commit f6aedb956ef154828c4bfaddaa5d5eb2dda5225c)

--- sys/net/if_bridgevar.h
+++ sys/net/if_bridgevar.h
@@ -74,6 +74,9 @@
  * Data structure and control definitions for bridge interfaces.
  */
 
+#ifndef	_NET_IF_BRIDGEVAR_H_
+#define	_NET_IF_BRIDGEVAR_H_
+
 #include <sys/callout.h>
 #include <sys/queue.h>
 #include <sys/condvar.h>
@@ -316,3 +319,5 @@ struct ifbpstpconf {
 extern	void (*bridge_dn_p)(struct mbuf *, struct ifnet *);
 
 #endif /* _KERNEL */
+
+#endif /* _NET_IF_BRIDGEVAR_H_ */
