0d1d8c8cfa57069edc7e049772a9732929177fb0
Warner Losh <imp@FreeBSD.org>
2025-04-11 15:45:27 -0600

    loader/efi: Fix trivial whitespace error
    Sponsored by:		Netflix

--- stand/efi/loader/main.c
+++ stand/efi/loader/main.c
@@ -4,7 +4,7 @@
  * All rights reserved.
  *
  * Copyright (c) 2016-2019 Netflix, Inc. written by M. Warner Losh
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
