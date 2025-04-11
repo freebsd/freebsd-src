7e791e2de6d5182b102ec46f48f750ac54ebac80
Warner Losh <imp@FreeBSD.org>
2025-04-11 15:48:52 -0600

    loader/libefi: Fix trivial trailing whitespace
    Sponsored by:		Netflix

--- stand/efi/libefi/efinet.c
+++ stand/efi/libefi/efinet.c
@@ -48,7 +48,7 @@ static int efinet_match(struct netif *, void *);
 static int efinet_probe(struct netif *, void *);
 static ssize_t efinet_put(struct iodesc *, void *, size_t);
 
-struct netif_driver efinetif = {   
+struct netif_driver efinetif = {
 	.netif_bname = "efinet",
 	.netif_match = efinet_match,
 	.netif_probe = efinet_probe,
@@ -150,7 +150,7 @@ efinet_put(struct iodesc *desc, void *pkt, size_t len)
 		buf = NULL;	/* XXX Is this needed? */
 		status = net->GetStatus(net, NULL, &buf);
 		/*
-		 * XXX EFI1.1 and the E1000 card returns a different 
+		 * XXX EFI1.1 and the E1000 card returns a different
 		 * address than we gave.  Sigh.
 		 */
 	} while (status == EFI_SUCCESS && buf == NULL);
@@ -328,7 +328,7 @@ efinet_init(struct iodesc *desc, void *machdep_hint)
 static void
 efinet_end(struct netif *nif)
 {
-	EFI_SIMPLE_NETWORK *net = nif->nif_devdata; 
+	EFI_SIMPLE_NETWORK *net = nif->nif_devdata;
 
 	if (net == NULL)
 		return;
