bsdjhb/freebsd

ctld: Convert struct portal to a C++ class
- Convert portal_init_socket and portal_reuse_socket into class methods.
  Move the logic to handle proxy portal setup into the init_socket method
  so that some fields can remain private.

- Add accessors for a few other members (most are const) so that all the
  fields can be private.

- Use std::string, freebsd::addrinfo_up, and freebsd::fd_up classes to
  manage fields owned exclusively.

- Add a vector of proxy portal pointers to struct conf and use the index
  into the vector as the portal ID.  This replaces an O(n^2) loop to
  find the portal for a portal ID returned by kernel_accept with a direct
  lookup.

usr.sbin/ctld/ctld.h
@@ -46,6 +46,7 @@
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <libutil++>
 
 #define	DEFAULT_CONFIG_PATH		"/etc/ctl.conf"
 #define	DEFAULT_PIDFILE			"/var/run/ctld.pid"
@@ -124,17 +125,26 @@ private:
 typedef std::shared_ptr<auth_group> auth_group_sp;
 
 struct portal {
-	TAILQ_ENTRY(portal)		p_next;
-	struct portal_group		*p_portal_group;
-	bool				p_iser;
-	char				*p_listen;
-	struct addrinfo			*p_ai;
-#ifdef ICL_KERNEL_PROXY
-	int				p_id;
-#endif
+	portal(struct portal_group *pg, const char *listen, bool iser,
+	    struct addrinfo *ai) : p_portal_group(pg), p_listen(listen),
+				   p_ai(ai), p_iser(iser) {}
 
-	TAILQ_HEAD(, target)		p_targets;
-	int				p_socket;
+	bool reuse_socket(portal &oldp);
+	bool init_socket();
+
+	portal_group *portal_group() { return p_portal_group; }
+	const char *listen() const { return p_listen.c_str(); }
+	const addrinfo *ai() const { return p_ai.get(); }
+	int socket() const { return p_socket; }
+	void close() { p_socket.reset(); }
+
+private:
+	struct portal_group		*p_portal_group;
+	std::string			p_listen;
+	freebsd::addrinfo_up		p_ai;
+	bool				p_iser;
+
+	freebsd::fd_up			p_socket;
 };
 
 #define	PG_FILTER_UNKNOWN		0
@@ -152,7 +162,7 @@ struct portal_group {
 	int				pg_discovery_filter = PG_FILTER_UNKNOWN;
 	bool				pg_foreign = false;
 	bool				pg_unassigned = false;
-	TAILQ_HEAD(, portal)		pg_portals;
+	std::list<portal>		pg_portals;
 	TAILQ_HEAD(, port)		pg_ports;
 	char				*pg_offload = nullptr;
 	char				*pg_redirection = nullptr;
@@ -242,14 +252,18 @@ struct conf {
 	int				conf_timeout;
 	int				conf_maxproc;
 
-#ifdef ICL_KERNEL_PROXY
-	int				conf_portal_id = 0;
-#endif
 	struct pidfh			*conf_pidfh = nullptr;
 
 	bool				conf_default_pg_defined = false;
 	bool				conf_default_ag_defined = false;
 	bool				conf_kernel_port_on = false;
+
+#ifdef ICL_KERNEL_PROXY
+	int add_proxy_portal(portal *);
+	portal *proxy_portal(int);
+private:
+	std::vector<portal *>		conf_proxy_portals;
+#endif
 };
 
 /* Physical ports exposed by the kernel */

freebsd/freebsd-src

usr.sbin/ctld/ctld.h
@@ -46,6 +46,7 @@
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <libutil++>
 
 #define	DEFAULT_CONFIG_PATH		"/etc/ctl.conf"
 #define	DEFAULT_PIDFILE			"/var/run/ctld.pid"
@@ -124,18 +125,29 @@ private:
 typedef std::shared_ptr<auth_group> auth_group_sp;
 
 struct portal {
-	TAILQ_ENTRY(portal)		p_next;
-	struct portal_group		*p_portal_group;
-	bool				p_iser;
-	char				*p_listen;
-	struct addrinfo			*p_ai;
-#ifdef ICL_KERNEL_PROXY
-	int				p_id;
-#endif
+	portal(struct portal_group *pg, const char *listen, bool iser,
+	    struct addrinfo *ai) : p_portal_group(pg), p_listen(listen),
+				   p_ai(ai), p_iser(iser) {}
 
-	TAILQ_HEAD(, target)		p_targets;
-	int				p_socket;
+	bool reuse_socket(portal &oldp);
+	bool init_socket();
+
+	portal_group *portal_group() { return p_portal_group; }
+	const char *listen() const { return p_listen.c_str(); }
+	const addrinfo *ai() const { return p_ai.get(); }
+	int socket() const { return p_socket; }
+	void close() { p_socket.reset(); }
+
+private:
+	struct portal_group		*p_portal_group;
+	std::string			p_listen;
+	freebsd::addrinfo_up		p_ai;
+	bool				p_iser;
+
+	freebsd::fd_up			p_socket;
 };
+
+typedef std::unique_ptr<portal> portal_up;
 
 #define	PG_FILTER_UNKNOWN		0
 #define	PG_FILTER_NONE			1
@@ -152,7 +164,7 @@ struct portal_group {
 	int				pg_discovery_filter = PG_FILTER_UNKNOWN;
 	bool				pg_foreign = false;
 	bool				pg_unassigned = false;
-	TAILQ_HEAD(, portal)		pg_portals;
+	std::list<portal_up>	        pg_portals;
 	TAILQ_HEAD(, port)		pg_ports;
 	char				*pg_offload = nullptr;
 	char				*pg_redirection = nullptr;
@@ -242,14 +254,18 @@ struct conf {
 	int				conf_timeout;
 	int				conf_maxproc;
 
-#ifdef ICL_KERNEL_PROXY
-	int				conf_portal_id = 0;
-#endif
 	struct pidfh			*conf_pidfh = nullptr;
 
 	bool				conf_default_pg_defined = false;
 	bool				conf_default_ag_defined = false;
 	bool				conf_kernel_port_on = false;
+
+#ifdef ICL_KERNEL_PROXY
+	int add_proxy_portal(portal *);
+	portal *proxy_portal(int);
+private:
+	std::vector<portal *>		conf_proxy_portals;
+#endif
 };
 
 /* Physical ports exposed by the kernel */