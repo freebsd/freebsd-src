d943d0f721bc8d65927a5a846bcd6847e297959b
Warner Losh <imp@FreeBSD.org>
2025-04-16 22:05:16 -0600

    kboot: Implement data_from_kernel to read kernel variables
    Since the linux kernel doesn't expose enough of the EFI details to
    userland via /sys/firmware/efi, write a routine to extract data from the
    kernel directly by parsing /proc/kproc and /proc/kallsyms.
    
    Sponsored by:		Netflix
    Reviewed by:		kevans, jhibbits
    Differential Revision:	https://reviews.freebsd.org/D49868

--- stand/kboot/include/util.h
+++ stand/kboot/include/util.h
@@ -6,6 +6,10 @@
 
 #pragma once
 
+/* dfk.c: */
+bool data_from_kernel(const char *sym, void *buf, size_t len);
+
+/* util.c: */
 bool file2str(const char *fn, char *buffer, size_t buflen);
 bool file2u32(const char *fn, uint32_t *val);
 bool file2u64(const char *fn, uint64_t *val);
