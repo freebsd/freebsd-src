b6cd84ca2d08b39e6a51a782ddf2b58293be6cba
Doug Moore <dougm@FreeBSD.org>
2025-04-20 23:04:43 -0500

    powerpc: replace tailq pointers with iterators
    Change architecture-specific code to use iterators rather than tailq
    pointers.
    
    Reviewed by:	kib
    Differential Revision:	https://reviews.freebsd.org/D49927

--- sys/powerpc/aim/mmu_oea64.c
+++ sys/powerpc/aim/mmu_oea64.c
@@ -81,6 +81,7 @@
 #include <vm/vm_extern.h>
 #include <vm/vm_pageout.h>
 #include <vm/vm_dumpset.h>
+#include <vm/vm_radix.h>
 #include <vm/vm_reserv.h>
 #include <vm/uma.h>
 
@@ -1826,17 +1827,18 @@ void
 moea64_enter_object(pmap_t pm, vm_offset_t start, vm_offset_t end,
     vm_page_t m_start, vm_prot_t prot)
 {
+	struct pctrie_iter pages;
 	vm_page_t m;
-	vm_pindex_t diff, psize;
 	vm_offset_t va;
 	int8_t psind;
 
 	VM_OBJECT_ASSERT_LOCKED(m_start->object);
 
-	psize = atop(end - start);
-	m = m_start;
-	while (m != NULL && (diff = m->pindex - m_start->pindex) < psize) {
-		va = start + ptoa(diff);
+	vm_page_iter_limit_init(&pages, m_start->object,
+	    m_start->pindex + atop(end - start));
+	m = vm_radix_iter_lookup(&pages, m_start->pindex);
+	while (m != NULL) {
+		va = start + ptoa(m->pindex - m_start->pindex);
 		if ((va & HPT_SP_MASK) == 0 && va + HPT_SP_SIZE <= end &&
 		    m->psind == 1 && moea64_ps_enabled(pm))
 			psind = 1;
@@ -1846,8 +1848,9 @@ moea64_enter_object(pmap_t pm, vm_offset_t start, vm_offset_t end,
 		    (VM_PROT_READ | VM_PROT_EXECUTE),
 		    PMAP_ENTER_NOSLEEP | PMAP_ENTER_QUICK_LOCKED, psind);
 		if (psind == 1)
-			m = &m[HPT_SP_SIZE / PAGE_SIZE - 1];
-		m = TAILQ_NEXT(m, listq);
+			m = vm_radix_iter_jump(&pages, HPT_SP_SIZE / PAGE_SIZE);
+		else
+			m = vm_radix_iter_step(&pages);
 	}
 }
 
