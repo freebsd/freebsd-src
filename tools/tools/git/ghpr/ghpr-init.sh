a1a06bc686615bc4ff32b553ad1193fb30406607
Warner Losh <imp@FreeBSD.org>
2025-04-22 10:25:17 -0600

    ghpr: Make init a little more robust
    Use newer git config subcomands and set the values so we don't get repetition.
    
    Sponsored by:		Netflix

--- tools/tools/git/ghpr/ghpr-init.sh
+++ tools/tools/git/ghpr/ghpr-init.sh
@@ -16,12 +16,14 @@ if [ "$(git config branch.${BRANCH}.opabinia)" = "true" ]; then
     # Bail if the branch already exists
 else
     if git rev-parse --verify ${BRANCH} > /dev/null 2>&1; then
-	echo "Branch ${BRANCH} already exists, skipping creation"
+	echo "Branch ${BRANCH} already exists, skipping creation, but rebasing to ${base}"
+	git rebase ${base} ${BRANCH}
     else
 	# Create the branch and tag it as the one we're using for opabinia merging.
+	echo "Creating ${BRANCH} from ${base} to land changes"
 	git checkout -b ${BRANCH} ${base} || die "Can't create ${BRANCH}"
     fi
 fi
 
-git config --add --type bool branch.${BRANCH}.opabinia true || die "Can't annotate"
-git config --add branch.${BRANCH}.opabinia.base ${base} || die "Can't annotate"
+git config set --type bool --all branch.${BRANCH}.opabinia true || die "Can't annotate"
+git config set --all branch.${BRANCH}.opabinia.base "${base}" || die "Can't annotate"
