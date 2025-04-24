0640f7c9dc16a60daeb39fe89c182ae034048bc0
Adrian Chadd <adrian@FreeBSD.org>
2025-04-23 22:58:40 -0700

    e6000sw: correctly depend upon etherswitch
    This fixes the driver to load correctly as a module when etherswitch
    is also a module.
    
    Differential Revision:	https://reviews.freebsd.org/D50027
    Reviewed by:	imp

--- sys/dev/etherswitch/e6000sw/e6000sw.c
+++ sys/dev/etherswitch/e6000sw/e6000sw.c
@@ -198,7 +198,7 @@ DRIVER_MODULE(e6000sw, mdio, e6000sw_driver, 0, 0);
 DRIVER_MODULE(etherswitch, e6000sw, etherswitch_driver, 0, 0);
 DRIVER_MODULE(miibus, e6000sw, miibus_driver, 0, 0);
 MODULE_DEPEND(e6000sw, mdio, 1, 1, 1);
-
+MODULE_DEPEND(e6000sw, etherswitch, 1, 1, 1);
 
 static void
 e6000sw_identify(driver_t *driver, device_t parent)
