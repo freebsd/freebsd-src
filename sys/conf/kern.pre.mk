0947920999dcf405faa7554c8b0bc9d2bc4e5c0a
Siva Mahadevan <me@svmhdvn.name>
2025-04-24 12:08:47 +0000

    files: add new IPFILTER_C rule for netpfil/ipfilter sources
    This re-enables all compiler warnings for these sources.
    
    Pull Request:	https://github.com/freebsd/freebsd-src/pull/1677
    MFC after:	1 month
    
    (cherry picked from commit 45730dd3ed334d265fc77cf6df1f82013fd5bf69)

--- sys/conf/kern.pre.mk
+++ sys/conf/kern.pre.mk
@@ -341,6 +341,10 @@ BNXT_CFLAGS=	-I$S/dev/bnxt/bnxt_en ${OFEDCFLAGS}
 BNXT_C_NOIMP=	${CC} -c -o ${.TARGET} ${BNXT_CFLAGS} ${WERROR}
 BNXT_C=		${BNXT_C_NOIMP} ${.IMPSRC}
 
+# IP Filter
+IPFILTER_CFLAGS=	-I$S/netpfil/ipfilter
+IPFILTER_C=		${NORMAL_C} ${IPFILTER_CFLAGS}
+
 GEN_CFILES= $S/$M/$M/genassym.c ${MFILES:T:S/.m$/.c/}
 SYSTEM_CFILES= config.c env.c hints.c vnode_if.c
 SYSTEM_DEP= Makefile ${SYSTEM_OBJS}
