54dd70dfb6b341984a9ddba03e4f459a254b00a0
Olivier Certner <olce@FreeBSD.org>
2025-04-28 14:55:00 +0200

    ps(1): Add copyright
    Where appropriate.
    
    MFC after:      3 days
    Sponsored by:   The FreeBSD Foundation

--- bin/ps/ps.c
+++ bin/ps/ps.c
@@ -3,6 +3,11 @@
  *
  * Copyright (c) 1990, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
+ * Copyright (c) 2025 The FreeBSD Foundation
+ *
+ * Portions of this software were developed by Olivier Certner
+ * <olce@FreeBSD.org> at Kumacom SARL under sponsorship from the FreeBSD
+ * Foundation.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
