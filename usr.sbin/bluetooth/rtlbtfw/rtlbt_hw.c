freebsd/freebsd-src

commit 221d969bd1b95dc959f1f71806ac0e7c7ff258fe

rtlbtfw: Firmware loader for Realtek 87XX/88XX bluetooth USB adaptors
Firmware files are available in the comms/rtlbt-firmware port.

Sponsored by:	Future Crew LLC
MFC after:	1 month
Differential Revision:	https://reviews.freebsd.org/D46739

usr.sbin/bluetooth/rtlbtfw/rtlbt_hw.c
@@ -1,0 +1,236 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2019 Vladimir Kondratyev <wulf@FreeBSD.org>
+ * Copyright (c) 2023 Future Crew LLC.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+#include <sys/endian.h>
+#include <sys/stat.h>
+
+#include <netgraph/bluetooth/include/ng_hci.h>
+
+#include <err.h>
+#include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <libusb.h>
+
+#include "rtlbt_fw.h"
+#include "rtlbt_hw.h"
+#include "rtlbt_dbg.h"
+
+static int
+rtlbt_hci_command(struct libusb_device_handle *hdl, struct rtlbt_hci_cmd *cmd,
+    void *event, int size, int *transferred, int timeout)
+{
+	struct timespec to, now, remains;
+	int ret;
+
+	ret = libusb_control_transfer(hdl,
+	    LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_DEVICE,
+	    0,
+	    0,
+	    0,
+	    (uint8_t *)cmd,
+	    RTLBT_HCI_CMD_SIZE(cmd),
+	    timeout);
+
+	if (ret < 0) {
+		rtlbt_err("libusb_control_transfer() failed: err=%s",
+		    libusb_strerror(ret));
+		return (ret);
+	}
+
+	clock_gettime(CLOCK_MONOTONIC, &now);
+	to = RTLBT_MSEC2TS(timeout);
+	timespecadd(&to, &now, &to);
+
+	do {
+		timespecsub(&to, &now, &remains);
+		ret = libusb_interrupt_transfer(hdl,
+		    RTLBT_INTERRUPT_ENDPOINT_ADDR,
+		    event,
+		    size,
+		    transferred,
+		    RTLBT_TS2MSEC(remains) + 1);
+
+		if (ret < 0) {
+			rtlbt_err("libusb_interrupt_transfer() failed: err=%s",
+			    libusb_strerror(ret));
+			return (ret);
+		}
+
+		switch (((struct rtlbt_hci_event *)event)->header.event) {
+		case NG_HCI_EVENT_COMMAND_COMPL:
+			if (*transferred <
+			    (int)offsetof(struct rtlbt_hci_event_cmd_compl, data))
+				break;
+			if (cmd->opcode !=
+			    ((struct rtlbt_hci_event_cmd_compl *)event)->opcode)
+				break;
+			return (0);
+		default:
+			break;
+		}
+		rtlbt_debug("Stray HCI event: %x",
+		    ((struct rtlbt_hci_event *)event)->header.event);
+	} while (timespeccmp(&to, &now, >));
+
+	rtlbt_err("libusb_interrupt_transfer() failed: err=%s",
+	    libusb_strerror(LIBUSB_ERROR_TIMEOUT));
+
+	return (LIBUSB_ERROR_TIMEOUT);
+}
+
+int
+rtlbt_read_local_ver(struct libusb_device_handle *hdl,
+    ng_hci_read_local_ver_rp *ver)
+{
+	int ret, transferred;
+	struct rtlbt_hci_event_cmd_compl *event;
+	struct rtlbt_hci_cmd cmd = {
+		.opcode = htole16(NG_HCI_OPCODE(NG_HCI_OGF_INFO,
+		    NG_HCI_OCF_READ_LOCAL_VER)),
+		.length = 0,
+	};
+	uint8_t buf[RTLBT_HCI_EVT_COMPL_SIZE(ng_hci_read_local_ver_rp)];
+
+	memset(buf, 0, sizeof(buf));
+
+	ret = rtlbt_hci_command(hdl,
+	    &cmd,
+	    buf,
+	    sizeof(buf),
+	    &transferred,
+	    RTLBT_HCI_CMD_TIMEOUT);
+
+	if (ret < 0 || transferred != sizeof(buf)) {
+		 rtlbt_debug("Can't read local version: code=%d, size=%d",
+		     ret,
+		     transferred);
+		 return (-1);
+	}
+
+	event = (struct rtlbt_hci_event_cmd_compl *)buf;
+	memcpy(ver, event->data, sizeof(ng_hci_read_local_ver_rp));
+
+	return (0);
+}
+
+int
+rtlbt_read_rom_ver(struct libusb_device_handle *hdl, uint8_t *ver)
+{
+	int ret, transferred;
+	struct rtlbt_hci_event_cmd_compl *event;
+	struct rtlbt_hci_cmd cmd = {
+		.opcode = htole16(0xfc6d),
+		.length = 0,
+	};
+	uint8_t buf[RTLBT_HCI_EVT_COMPL_SIZE(struct rtlbt_rom_ver_rp)];
+
+	memset(buf, 0, sizeof(buf));
+
+	ret = rtlbt_hci_command(hdl,
+	    &cmd,
+	    buf,
+	    sizeof(buf),
+	    &transferred,
+	    RTLBT_HCI_CMD_TIMEOUT);
+
+	if (ret < 0 || transferred != sizeof(buf)) {
+		 rtlbt_debug("Can't read ROM version: code=%d, size=%d",
+		     ret,
+		     transferred);
+		 return (-1);
+	}
+
+	event = (struct rtlbt_hci_event_cmd_compl *)buf;
+	*ver = ((struct rtlbt_rom_ver_rp *)event->data)->version;
+
+	return (0);
+}
+
+int
+rtlbt_load_fwfile(struct libusb_device_handle *hdl,
+    const struct rtlbt_firmware *fw)
+{
+	uint8_t cmd_buf[RTLBT_HCI_MAX_CMD_SIZE];
+	struct rtlbt_hci_cmd *cmd = (struct rtlbt_hci_cmd *)cmd_buf;
+	struct rtlbt_hci_dl_cmd *dl_cmd = (struct rtlbt_hci_dl_cmd *)cmd->data;
+	uint8_t evt_buf[RTLBT_HCI_EVT_COMPL_SIZE(struct rtlbt_hci_dl_rp)];
+	uint8_t *data = fw->buf;
+	int frag_num = fw->len / RTLBT_MAX_CMD_DATA_LEN + 1;
+	int frag_len = RTLBT_MAX_CMD_DATA_LEN;
+	int i;
+	int ret, transferred;
+
+	for (i = 0; i < frag_num; i++) {
+
+		rtlbt_debug("download fw (%d/%d)", i + 1, frag_num);
+
+		memset(cmd_buf, 0, sizeof(cmd_buf));
+		cmd->opcode = htole16(0xfc20);
+		if (i > 0x7f)
+			dl_cmd->index = (i & 0x7f) + 1;
+		else
+			dl_cmd->index = i;
+
+		if (i == (frag_num - 1)) {
+			dl_cmd->index |= 0x80; /* data end */
+			frag_len = fw->len % RTLBT_MAX_CMD_DATA_LEN;
+		}
+		cmd->length = frag_len + 1;
+		memcpy(dl_cmd->data, data, frag_len);
+
+		/* Send download command */
+		ret = rtlbt_hci_command(hdl,
+		    cmd,
+		    evt_buf,
+		    sizeof(evt_buf),
+		    &transferred,
+		    RTLBT_HCI_CMD_TIMEOUT);
+		if (ret < 0) {
+			rtlbt_err("download fw command failed (%d)", errno);
+			goto out;
+		}
+		if (transferred != sizeof(evt_buf)) {
+			rtlbt_err("download fw event length mismatch");
+			errno = EIO;
+			ret = -1;
+			goto out;
+		}
+
+		data += RTLBT_MAX_CMD_DATA_LEN;
+	}
+
+out:
+	return (ret);
+}

targets/pseudo/userland/Makefile.depend
@@ -430,6 +430,7 @@ DIRDEPS+= \
 	usr.sbin/bluetooth/l2control \
 	usr.sbin/bluetooth/l2ping \
 	usr.sbin/bluetooth/rfcomm_pppd \
+	usr.sbin/bluetooth/rtlbtfw \
 	usr.sbin/bluetooth/sdpcontrol \
 	usr.sbin/bluetooth/sdpd \
 	usr.sbin/bootparamd/bootparamd \

tools/build/mk/OptionalObsoleteFiles.inc
@@ -193,6 +193,7 @@ OLD_FILES+=etc/bluetooth/hosts
 OLD_FILES+=etc/bluetooth/protocols
 OLD_FILES+=etc/defaults/bluetooth.device.conf
 OLD_FILES+=etc/devd/iwmbtfw.conf
+OLD_FILES+=etc/devd/rtlbtfw.conf
 OLD_DIRS+=etc/bluetooth
 OLD_FILES+=etc/rc.d/bluetooth
 OLD_FILES+=etc/rc.d/bthidd
@@ -240,6 +241,7 @@ OLD_FILES+=usr/sbin/iwmbtfw
 OLD_FILES+=usr/sbin/l2control
 OLD_FILES+=usr/sbin/l2ping
 OLD_FILES+=usr/sbin/rfcomm_pppd
+OLD_FILES+=usr/sbin/rtlbtfw
 OLD_FILES+=usr/sbin/sdpcontrol
 OLD_FILES+=usr/sbin/sdpd
 OLD_FILES+=usr/share/examples/etc/defaults/bluetooth.device.conf
@@ -318,6 +320,7 @@ OLD_FILES+=usr/share/man/man8/iwmbtfw.8.gz
 OLD_FILES+=usr/share/man/man8/l2control.8.gz
 OLD_FILES+=usr/share/man/man8/l2ping.8.gz
 OLD_FILES+=usr/share/man/man8/rfcomm_pppd.8.gz
+OLD_FILES+=usr/share/man/man8/rtlbtfw.8.gz
 OLD_FILES+=usr/share/man/man8/sdpcontrol.8.gz
 OLD_FILES+=usr/share/man/man8/sdpd.8.gz
 .endif

usr.sbin/bluetooth/Makefile
@@ -19,6 +19,7 @@ SUBDIR+=	bcmfw
 SUBDIR+=	bthidcontrol
 SUBDIR+=	bthidd
 SUBDIR+=	iwmbtfw
+SUBDIR+=	rtlbtfw
 .endif
 
 .include <bsd.subdir.mk>

usr.sbin/bluetooth/rtlbtfw/Makefile
@@ -1,0 +1,9 @@
+PACKAGE=	bluetooth
+CONFS=		rtlbtfw.conf
+CONFSDIR=       /etc/devd
+PROG=		rtlbtfw
+MAN=		rtlbtfw.8
+LIBADD+=	usb
+SRCS=		main.c rtlbt_fw.c rtlbt_hw.c
+
+.include <bsd.prog.mk>

usr.sbin/bluetooth/rtlbtfw/main.c
@@ -1,0 +1,525 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2013 Adrian Chadd <adrian@freebsd.org>
+ * Copyright (c) 2019 Vladimir Kondratyev <wulf@FreeBSD.org>
+ * Copyright (c) 2023 Future Crew LLC.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+#include <sys/stat.h>
+#include <sys/endian.h>
+
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <libusb.h>
+
+#include "rtlbt_fw.h"
+#include "rtlbt_hw.h"
+#include "rtlbt_dbg.h"
+
+#define	_DEFAULT_RTLBT_FIRMWARE_PATH	"/usr/share/firmware/rtlbt"
+
+int	rtlbt_do_debug = 0;
+int	rtlbt_do_info = 0;
+
+struct rtlbt_devid {
+	uint16_t product_id;
+	uint16_t vendor_id;
+};
+
+static struct rtlbt_devid rtlbt_list[] = {
+	/* Realtek 8821CE Bluetooth devices */
+	{ .vendor_id = 0x13d3, .product_id = 0x3529 },
+
+	/* Realtek 8822CE Bluetooth devices */
+	{ .vendor_id = 0x0bda, .product_id = 0xb00c },
+	{ .vendor_id = 0x0bda, .product_id = 0xc822 },
+
+	/* Realtek 8822CU Bluetooth devices */
+	{ .vendor_id = 0x13d3, .product_id = 0x3549 },
+
+	/* Realtek 8852AE Bluetooth devices */
+	{ .vendor_id = 0x0bda, .product_id = 0x2852 },
+	{ .vendor_id = 0x0bda, .product_id = 0xc852 },
+	{ .vendor_id = 0x0bda, .product_id = 0x385a },
+	{ .vendor_id = 0x0bda, .product_id = 0x4852 },
+	{ .vendor_id = 0x04c5, .product_id = 0x165c },
+	{ .vendor_id = 0x04ca, .product_id = 0x4006 },
+	{ .vendor_id = 0x0cb8, .product_id = 0xc549 },
+
+#ifdef RTLBTFW_SUPPORTS_FW_V2
+	/* Realtek 8852CE Bluetooth devices */
+	{ .vendor_id = 0x04ca, .product_id = 0x4007 },
+	{ .vendor_id = 0x04c5, .product_id = 0x1675 },
+	{ .vendor_id = 0x0cb8, .product_id = 0xc558 },
+	{ .vendor_id = 0x13d3, .product_id = 0x3587 },
+	{ .vendor_id = 0x13d3, .product_id = 0x3586 },
+	{ .vendor_id = 0x13d3, .product_id = 0x3592 },
+#endif
+
+	/* Realtek 8852BE Bluetooth devices */
+	{ .vendor_id = 0x0cb8, .product_id = 0xc559 },
+	{ .vendor_id = 0x0bda, .product_id = 0x887b },
+	{ .vendor_id = 0x13d3, .product_id = 0x3571 },
+
+	/* Realtek 8723AE Bluetooth devices */
+	{ .vendor_id = 0x0930, .product_id = 0x021d },
+	{ .vendor_id = 0x13d3, .product_id = 0x3394 },
+
+	/* Realtek 8723BE Bluetooth devices */
+	{ .vendor_id = 0x0489, .product_id = 0xe085 },
+	{ .vendor_id = 0x0489, .product_id = 0xe08b },
+	{ .vendor_id = 0x04f2, .product_id = 0xb49f },
+	{ .vendor_id = 0x13d3, .product_id = 0x3410 },
+	{ .vendor_id = 0x13d3, .product_id = 0x3416 },
+	{ .vendor_id = 0x13d3, .product_id = 0x3459 },
+	{ .vendor_id = 0x13d3, .product_id = 0x3494 },
+
+	/* Realtek 8723BU Bluetooth devices */
+	{ .vendor_id = 0x7392, .product_id = 0xa611 },
+
+	/* Realtek 8723DE Bluetooth devices */
+	{ .vendor_id = 0x0bda, .product_id = 0xb009 },
+	{ .vendor_id = 0x2ff8, .product_id = 0xb011 },
+
+	/* Realtek 8761BUV Bluetooth devices */
+	{ .vendor_id = 0x2357, .product_id = 0x0604 },
+	{ .vendor_id = 0x0b05, .product_id = 0x190e },
+	{ .vendor_id = 0x2550, .product_id = 0x8761 },
+	{ .vendor_id = 0x0bda, .product_id = 0x8771 },
+	{ .vendor_id = 0x6655, .product_id = 0x8771 },
+	{ .vendor_id = 0x7392, .product_id = 0xc611 },
+	{ .vendor_id = 0x2b89, .product_id = 0x8761 },
+
+	/* Realtek 8821AE Bluetooth devices */
+	{ .vendor_id = 0x0b05, .product_id = 0x17dc },
+	{ .vendor_id = 0x13d3, .product_id = 0x3414 },
+	{ .vendor_id = 0x13d3, .product_id = 0x3458 },
+	{ .vendor_id = 0x13d3, .product_id = 0x3461 },
+	{ .vendor_id = 0x13d3, .product_id = 0x3462 },
+
+	/* Realtek 8822BE Bluetooth devices */
+	{ .vendor_id = 0x13d3, .product_id = 0x3526 },
+	{ .vendor_id = 0x0b05, .product_id = 0x185c },
+
+	/* Realtek 8822CE Bluetooth devices */
+	{ .vendor_id = 0x04ca, .product_id = 0x4005 },
+	{ .vendor_id = 0x04c5, .product_id = 0x161f },
+	{ .vendor_id = 0x0b05, .product_id = 0x18ef },
+	{ .vendor_id = 0x13d3, .product_id = 0x3548 },
+	{ .vendor_id = 0x13d3, .product_id = 0x3549 },
+	{ .vendor_id = 0x13d3, .product_id = 0x3553 },
+	{ .vendor_id = 0x13d3, .product_id = 0x3555 },
+	{ .vendor_id = 0x2ff8, .product_id = 0x3051 },
+	{ .vendor_id = 0x1358, .product_id = 0xc123 },
+	{ .vendor_id = 0x0bda, .product_id = 0xc123 },
+	{ .vendor_id = 0x0cb5, .product_id = 0xc547 },
+};
+
+static int
+rtlbt_is_realtek(struct libusb_device_descriptor *d)
+{
+	int i;
+
+	/* Search looking for whether it's a Realtek-based device */
+	for (i = 0; i < (int) nitems(rtlbt_list); i++) {
+		if ((rtlbt_list[i].product_id == d->idProduct) &&
+		    (rtlbt_list[i].vendor_id == d->idVendor)) {
+			rtlbt_info("found USB Realtek");
+			return (1);
+		}
+	}
+
+	/* Not found */
+	return (0);
+}
+
+static int
+rtlbt_is_bluetooth(struct libusb_device *dev)
+{
+	struct libusb_config_descriptor *cfg;
+	const struct libusb_interface *ifc;
+	const struct libusb_interface_descriptor *d;
+	int r;
+
+	r = libusb_get_active_config_descriptor(dev, &cfg);
+	if (r < 0) {
+		rtlbt_err("Cannot retrieve config descriptor: %s",
+		    libusb_error_name(r));
+		return (0);
+	}
+
+	if (cfg->bNumInterfaces != 0) {
+		/* Only 0-th HCI/ACL interface is supported by downloader */
+		ifc = &cfg->interface[0];
+		if (ifc->num_altsetting != 0) {
+			/* BT HCI/ACL interface has no altsettings */
+			d = &ifc->altsetting[0];
+			/* Check if interface is a bluetooth */
+			if (d->bInterfaceClass == LIBUSB_CLASS_WIRELESS &&
+			    d->bInterfaceSubClass == 0x01 &&
+			    d->bInterfaceProtocol == 0x01) {
+				rtlbt_info("found USB Realtek");
+				libusb_free_config_descriptor(cfg);
+				return (1);
+			}
+		}
+	}
+	libusb_free_config_descriptor(cfg);
+
+	/* Not found */
+	return (0);
+}
+
+static libusb_device *
+rtlbt_find_device(libusb_context *ctx, int bus_id, int dev_id)
+{
+	libusb_device **list, *dev = NULL, *found = NULL;
+	struct libusb_device_descriptor d;
+	ssize_t cnt, i;
+	int r;
+
+	cnt = libusb_get_device_list(ctx, &list);
+	if (cnt < 0) {
+		rtlbt_err("libusb_get_device_list() failed: code %lld",
+		    (long long int) cnt);
+		return (NULL);
+	}
+
+	/*
+	 * Scan through USB device list.
+	 */
+	for (i = 0; i < cnt; i++) {
+		dev = list[i];
+		if (bus_id == libusb_get_bus_number(dev) &&
+		    dev_id == libusb_get_device_address(dev)) {
+			/* Get the device descriptor for this device entry */
+			r = libusb_get_device_descriptor(dev, &d);
+			if (r != 0) {
+				rtlbt_err("libusb_get_device_descriptor: %s",
+				    libusb_strerror(r));
+				break;
+			}
+
+			/* For non-Realtek match on the vendor/product id */
+			if (rtlbt_is_realtek(&d)) {
+				/*
+				 * Take a reference so it's not freed later on.
+				 */
+				found = libusb_ref_device(dev);
+				break;
+			}
+			/* For Realtek vendor match on the interface class */
+			if (d.idVendor == 0x0bda && rtlbt_is_bluetooth(dev)) {
+				/*
+				 * Take a reference so it's not freed later on.
+				 */
+				found = libusb_ref_device(dev);
+				break;
+			}
+		}
+	}
+
+	libusb_free_device_list(list, 1);
+	return (found);
+}
+
+static void
+rtlbt_dump_version(ng_hci_read_local_ver_rp *ver)
+{
+	rtlbt_info("hci_version    0x%02x", ver->hci_version);
+	rtlbt_info("hci_revision   0x%04x", le16toh(ver->hci_revision));
+	rtlbt_info("lmp_version    0x%02x", ver->lmp_version);
+	rtlbt_info("lmp_subversion 0x%04x", le16toh(ver->lmp_subversion));
+}
+
+/*
+ * Parse ugen name and extract device's bus and address
+ */
+
+static int
+parse_ugen_name(char const *ugen, uint8_t *bus, uint8_t *addr)
+{
+	char *ep;
+
+	if (strncmp(ugen, "ugen", 4) != 0)
+		return (-1);
+
+	*bus = (uint8_t) strtoul(ugen + 4, &ep, 10);
+	if (*ep != '.')
+		return (-1);
+
+	*addr = (uint8_t) strtoul(ep + 1, &ep, 10);
+	if (*ep != '\0')
+		return (-1);
+
+	return (0);
+}
+
+static void
+usage(void)
+{
+	fprintf(stderr,
+	    "Usage: rtlbtfw (-D) -d ugenX.Y (-f firmware path) (-I)\n");
+	fprintf(stderr, "    -D: enable debugging\n");
+	fprintf(stderr, "    -d: device to operate upon\n");
+	fprintf(stderr, "    -f: firmware path, if not default\n");
+	fprintf(stderr, "    -I: enable informational output\n");
+	exit(127);
+}
+
+int
+main(int argc, char *argv[])
+{
+	libusb_context *ctx = NULL;
+	libusb_device *dev = NULL;
+	libusb_device_handle *hdl = NULL;
+	ng_hci_read_local_ver_rp ver;
+	int r;
+	uint8_t bus_id = 0, dev_id = 0;
+	int devid_set = 0;
+	int n;
+	char *firmware_dir = NULL;
+	char *firmware_path = NULL;
+	char *config_path = NULL;
+	int retcode = 1;
+	const struct rtlbt_id_table *ic;
+	uint8_t rom_version;
+	struct rtlbt_firmware fw, cfg;
+	enum rtlbt_fw_type fw_type;
+	uint16_t fw_lmp_subversion;
+
+	/* Parse command line arguments */
+	while ((n = getopt(argc, argv, "Dd:f:hIm:p:v:")) != -1) {
+		switch (n) {
+		case 'd': /* ugen device name */
+			devid_set = 1;
+			if (parse_ugen_name(optarg, &bus_id, &dev_id) < 0)
+				usage();
+			break;
+		case 'D':
+			rtlbt_do_debug = 1;
+			break;
+		case 'f': /* firmware dir */
+			if (firmware_dir)
+				free(firmware_dir);
+			firmware_dir = strdup(optarg);
+			break;
+		case 'I':
+			rtlbt_do_info = 1;
+			break;
+		case 'h':
+		default:
+			usage();
+			break;
+			/* NOT REACHED */
+		}
+	}
+
+	/* Ensure the devid was given! */
+	if (devid_set == 0) {
+		usage();
+		/* NOTREACHED */
+	}
+
+	/* libusb setup */
+	r = libusb_init(&ctx);
+	if (r != 0) {
+		rtlbt_err("libusb_init failed: code %d", r);
+		exit(127);
+	}
+
+	rtlbt_debug("opening dev %d.%d", (int) bus_id, (int) dev_id);
+
+	/* Find a device based on the bus/dev id */
+	dev = rtlbt_find_device(ctx, bus_id, dev_id);
+	if (dev == NULL) {
+		rtlbt_err("device not found");
+		goto shutdown;
+	}
+
+	/* XXX enforce that bInterfaceNumber is 0 */
+
+	/* XXX enforce the device/product id if they're non-zero */
+
+	/* Grab device handle */
+	r = libusb_open(dev, &hdl);
+	if (r != 0) {
+		rtlbt_err("libusb_open() failed: code %d", r);
+		goto shutdown;
+	}
+
+	/* Check if ng_ubt is attached */
+	r = libusb_kernel_driver_active(hdl, 0);
+	if (r < 0) {
+		rtlbt_err("libusb_kernel_driver_active() failed: code %d", r);
+		goto shutdown;
+	}
+	if (r > 0) {
+		rtlbt_info("Firmware has already been downloaded");
+		retcode = 0;
+		goto shutdown;
+	}
+
+	/* Get local version */
+	r = rtlbt_read_local_ver(hdl, &ver);
+	if (r < 0) {
+		rtlbt_err("rtlbt_read_local_ver() failed code %d", r);
+		goto shutdown;
+	}
+	rtlbt_dump_version(&ver);
+
+	ic = rtlbt_get_ic(ver.lmp_subversion, ver.hci_revision,
+	    ver.hci_version);
+	if (ic == NULL) {
+		rtlbt_err("rtlbt_get_ic() failed: Unknown IC");
+		goto shutdown;
+	}
+
+	/* Default the firmware path */
+	if (firmware_dir == NULL)
+		firmware_dir = strdup(_DEFAULT_RTLBT_FIRMWARE_PATH);
+
+	firmware_path = rtlbt_get_fwname(ic->fw_name, firmware_dir, "_fw.bin");
+	if (firmware_path == NULL)
+		goto shutdown;
+
+	rtlbt_debug("firmware_path = %s", firmware_path);
+
+	rtlbt_info("loading firmware %s", firmware_path);
+
+	/* Read in the firmware */
+	if (rtlbt_fw_read(&fw, firmware_path) <= 0) {
+		rtlbt_debug("rtlbt_fw_read() failed");
+		return (-1);
+	}
+
+	fw_type = rtlbt_get_fw_type(&fw, &fw_lmp_subversion);
+	if (fw_type == RTLBT_FW_TYPE_UNKNOWN &&
+	    (ic->flags & RTLBT_IC_FLAG_SIMPLE) == 0) {
+		rtlbt_debug("Unknown firmware type");
+		goto shutdown;
+	}
+
+	if (fw_type != RTLBT_FW_TYPE_UNKNOWN) {
+
+		/* Match hardware and firmware lmp_subversion */
+		if (fw_lmp_subversion != ver.lmp_subversion) {
+			rtlbt_err("firmware is for %x but this is a %x",
+			    fw_lmp_subversion, ver.lmp_subversion);
+			goto shutdown;
+		}
+
+		/* Query a ROM version */
+		r = rtlbt_read_rom_ver(hdl, &rom_version);
+		if (r < 0) {
+			rtlbt_err("rtlbt_read_rom_ver() failed code %d", r);
+			goto shutdown;
+		}
+		rtlbt_debug("rom_version = %d", rom_version);
+
+		/* Load in the firmware */
+		r = rtlbt_parse_fwfile_v1(&fw, rom_version);
+		if (r < 0) {
+			rtlbt_err("Parseing firmware file failed");
+			goto shutdown;
+		}
+
+		config_path = rtlbt_get_fwname(ic->fw_name, firmware_dir,
+		    "_config.bin");
+		if (config_path == NULL)
+			goto shutdown;
+
+		rtlbt_info("loading config %s", config_path);
+
+		/* Read in the config file */
+		if (rtlbt_fw_read(&cfg, config_path) <= 0) {
+			rtlbt_err("rtlbt_fw_read() failed");
+			if ((ic->flags & RTLBT_IC_FLAG_CONFIG) != 0)
+				goto shutdown;
+		} else {
+			r = rtlbt_append_fwfile(&fw, &cfg);
+			rtlbt_fw_free(&cfg);
+			if (r < 0) {
+				rtlbt_err("Appending config file failed");
+				goto shutdown;
+			}
+		}
+	}
+
+	r = rtlbt_load_fwfile(hdl, &fw);
+	if (r < 0) {
+		rtlbt_debug("Loading firmware file failed");
+		goto shutdown;
+	}
+
+	/* free it */
+	rtlbt_fw_free(&fw);
+
+	rtlbt_info("Firmware download complete");
+
+	/* Execute Read Local Version one more time */
+	r = rtlbt_read_local_ver(hdl, &ver);
+	if (r < 0) {
+		rtlbt_err("rtlbt_read_local_ver() failed code %d", r);
+		goto shutdown;
+	}
+	rtlbt_dump_version(&ver);
+
+	retcode = 0;
+
+	/* Ask kernel driver to probe and attach device again */
+	r = libusb_reset_device(hdl);
+	if (r != 0)
+		rtlbt_err("libusb_reset_device() failed: %s",
+		    libusb_strerror(r));
+
+shutdown:
+
+	/* Shutdown */
+
+	if (hdl != NULL)
+		libusb_close(hdl);
+
+	if (dev != NULL)
+		libusb_unref_device(dev);
+
+	if (ctx != NULL)
+		libusb_exit(ctx);
+
+	if (retcode == 0)
+		rtlbt_info("Firmware download is successful!");
+	else
+		rtlbt_err("Firmware download failed!");
+
+	return (retcode);
+}

usr.sbin/bluetooth/rtlbtfw/rtlbt_dbg.h
@@ -1,0 +1,46 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2013 Adrian Chadd <adrian@freebsd.org>
+ * Copyright (c) 2019 Vladimir Kondratyev <wulf@FreeBSD.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef	__RTLBT_DEBUG_H__
+#define	__RTLBT_DEBUG_H__
+
+extern	int rtlbt_do_debug;
+extern	int rtlbt_do_info;
+
+#define	rtlbt_err(fmt, ...)						\
+	fprintf(stderr, "rtlbtfw: %s: "fmt"\n", __func__, ##__VA_ARGS__)
+#define	rtlbt_info(fmt, ...)	do {					\
+	if (rtlbt_do_info)						\
+		fprintf(stderr, "%s: "fmt"\n", __func__, ##__VA_ARGS__);\
+} while (0)
+#define	rtlbt_debug(fmt, ...)	do {					\
+	if (rtlbt_do_debug)						\
+		fprintf(stderr, "%s: "fmt"\n", __func__, ##__VA_ARGS__);\
+} while (0)
+
+#endif

commit 87431a4387be434f8ef62448b87c8a10b27934f0

rtlbtfw(8): Fix incorrect chunk index overwrap in rtlbt_load_fwfile()
It prevented large (>256 chunks) firmwares from loading.

Fixes: 221d969bd1b9 ("rtlbtfw: Firmware loader for Realtek 87XX/88XX")

Sponsored by:	Future Crew, LLC
MFC after:	1 week

usr.sbin/bluetooth/rtlbtfw/rtlbt_hw.c
@@ -189,19 +189,18 @@ rtlbt_load_fwfile(struct libusb_device_handle *hdl,
 	uint8_t *data = fw->buf;
 	int frag_num = fw->len / RTLBT_MAX_CMD_DATA_LEN + 1;
 	int frag_len = RTLBT_MAX_CMD_DATA_LEN;
-	int i;
+	int i, j;
 	int ret, transferred;
 
-	for (i = 0; i < frag_num; i++) {
+	for (i = 0, j = 0; i < frag_num; i++, j++) {
 
 		rtlbt_debug("download fw (%d/%d)", i + 1, frag_num);
 
 		memset(cmd_buf, 0, sizeof(cmd_buf));
 		cmd->opcode = htole16(0xfc20);
-		if (i > 0x7f)
-			dl_cmd->index = (i & 0x7f) + 1;
-		else
-			dl_cmd->index = i;
+		if (j > 0x7f)
+			j = 1;
+		dl_cmd->index = j;
 
 		if (i == (frag_num - 1)) {
 			dl_cmd->index |= 0x80; /* data end */