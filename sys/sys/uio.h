a1424b75453fe0bcf24a9794378287b675ee4517
Gleb Smirnoff <glebius@FreeBSD.org>
2025-04-30 14:23:25 -0700

    uio: provide uioadvance()
    The function advances data pointers forward in given uio by offset.  It
    is planned to be used for interaction between aio(4) and unix(4).
    
    Differential Revision:	https://reviews.freebsd.org/D48920

--- sys/sys/uio.h
+++ sys/sys/uio.h
@@ -90,6 +90,7 @@ int	physcopyin_vlist(struct bus_dma_segment *src, off_t offset,
 	    vm_paddr_t dst, size_t len);
 int	physcopyout_vlist(vm_paddr_t src, struct bus_dma_segment *dst,
 	    off_t offset, size_t len);
+void	uioadvance(struct uio *, size_t);
 int	uiomove(void *cp, int n, struct uio *uio);
 int	uiomove_frombuf(void *buf, int buflen, struct uio *uio);
 int	uiomove_fromphys(struct vm_page *ma[], vm_offset_t offset, int n,
