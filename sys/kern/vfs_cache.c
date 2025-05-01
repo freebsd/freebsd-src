455203ec88850dfc776b932666ffe34e6f6e5619
Olivier Certner <olce@FreeBSD.org>
2025-05-01 12:30:49 +0200

    VFS cache: Fix initial sizing for non-default 'ncsizefactor'

--- sys/kern/vfs_cache.c
+++ sys/kern/vfs_cache.c
@@ -2686,7 +2686,7 @@ nchinit(void *dummy __unused)
 
 	ncsize = desiredvnodes * ncsizefactor;
 	cache_recalc_neg_min();
-	nchashtbl = nchinittbl(desiredvnodes * 2, &nchash);
+	nchashtbl = nchinittbl(ncsize, &nchash);
 	ncbuckethash = cache_roundup_2(mp_ncpus * mp_ncpus) - 1;
 	if (ncbuckethash < 7) /* arbitrarily chosen to avoid having one lock */
 		ncbuckethash = 7;
