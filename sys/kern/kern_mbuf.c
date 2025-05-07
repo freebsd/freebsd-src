271128b0698653294acf0ed3457d5871af5b3ef1
Dag-Erling Smørgrav <des@FreeBSD.org>
2025-05-07 20:58:48 +0200

    mbuf: Allow clusters to fill an entire jumbo page.
    The assumption that MCLBYTES == MJUMPAGESIZE can only happen if pages
    are small is incorrect: it can also happen if MCLSHIFT is adjusted to
    increase the size of clusters.  Restore the ability to have clusters
    fill a jumbo page, while still disallowing them from exceeding them.
    
    MFC after:	1 week
    Fixes:		840327e5ddf3, 9c3ad5ba932b
    Sponsored by:	NetApp, Inc.
    Sponsored by:	Klara, Inc.
    Reviewed by:	siderop1_netapp.com, kevans, brooks
    Differential Revision:	https://reviews.freebsd.org/D50242

--- sys/kern/kern_mbuf.c
+++ sys/kern/kern_mbuf.c
@@ -61,8 +61,8 @@
 #include <vm/uma.h>
 #include <vm/uma_dbg.h>
 
-_Static_assert(MJUMPAGESIZE > MCLBYTES,
-    "Cluster must be smaller than a jumbo page");
+_Static_assert(MCLBYTES <= MJUMPAGESIZE,
+    "Cluster must not be larger than a jumbo page");
 
 /*
  * In FreeBSD, Mbufs and Mbuf Clusters are allocated from UMA
