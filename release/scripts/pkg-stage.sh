1ecfdc009172491a133d596332a9aaf6851a49c1
Colin Percival <cperciva@FreeBSD.org>
2025-05-11 15:10:48 -0700

    release: Don't put drm-kmod package onto DVD
    The X.Y-RELEASE DVDs use a quarterly package set which was built on
    X.(Y-1)-RELEASE, and those kernel modules are never going to be
    useful.
    
    MFC after:	30 seconds
    Sponsored by:	Amazon
    
    (cherry picked from commit 94e44a074e5212cc3459e360a9de55500f7c41d0)

--- release/scripts/pkg-stage.sh
+++ release/scripts/pkg-stage.sh
@@ -13,9 +13,6 @@ export PKGCMD="/usr/sbin/pkg -d --rootdir ${ROOTDIR}"
 export PORTSDIR="${PORTSDIR:-/usr/ports}"
 
 _DVD_PACKAGES="devel/git@lite
-graphics/drm-kmod
-graphics/drm-510-kmod
-graphics/drm-515-kmod
 misc/freebsd-doc-all
 net/mpd5
 net/rsync
