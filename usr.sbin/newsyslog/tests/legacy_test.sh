2b8ccf2a09a88040333a35913ca3b138ac8c2630
Benjamin Esham <benjamin@esham.io>
2025-05-18 12:52:38 -0400

    newsyslog tests: Move "force" flag into individual test cases
    Previously, the run_newsyslog function always called newsyslog with the
    "-F" flag, which tells it to trim log files even if the criteria haven't
    been met. This was fine for the existing tests, but it meant that it
    would be impossible to test the trimming process itself. Now, the -F
    flag is used within individual tests as needed.
    
    Signed-off-by: Benjamin Esham <benjamin@esham.io>

--- usr.sbin/newsyslog/tests/legacy_test.sh
+++ usr.sbin/newsyslog/tests/legacy_test.sh
@@ -205,8 +205,7 @@ tmpdir_clean()
 
 run_newsyslog()
 {
-
-	newsyslog -f ../newsyslog.conf -F -r "$@"
+	newsyslog -f ../newsyslog.conf -r "$@"
 }
 
 tests_normal_rotate() {
@@ -216,10 +215,10 @@ tests_normal_rotate() {
 	dir="$2"
 
 	if [ -n "$dir" ]; then
-		newsyslog_args=" -a ${dir}"
+		newsyslog_args="-F -a ${dir}"
 		name_postfix="${ext} archive dir"
 	else
-		newsyslog_args=""
+		newsyslog_args="-F"
 		name_postfix="${ext}"
 	fi
 
@@ -294,10 +293,10 @@ tests_normal_rotate_keepn() {
 	dir="$3"
 
 	if [ -n "$dir" ]; then
-		newsyslog_args=" -a ${dir}"
+		newsyslog_args="-F -a ${dir}"
 		name_postfix="${ext} archive dir"
 	else
-		newsyslog_args=""
+		newsyslog_args="-F"
 		name_postfix="${ext}"
 	fi
 
@@ -363,10 +362,10 @@ tests_time_rotate() {
 	dir="$2"
 
 	if [ -n "$dir" ]; then
-		newsyslog_args="-t DEFAULT -a ${dir}"
+		newsyslog_args="-F -t DEFAULT -a ${dir}"
 		name_postfix="${ext} archive dir"
 	else
-		newsyslog_args="-t DEFAULT"
+		newsyslog_args="-F -t DEFAULT"
 		name_postfix="${ext}"
 	fi
 
@@ -424,10 +423,10 @@ tests_rfc5424() {
 	dir="$2"
 
 	if [ -n "$dir" ]; then
-		newsyslog_args=" -a ${dir}"
+		newsyslog_args="-F -a ${dir}"
 		name_postfix="${ext} archive dir"
 	else
-		newsyslog_args=""
+		newsyslog_args="-F"
 		name_postfix="${ext}"
 	fi
 
@@ -473,16 +472,16 @@ tests_p_flag_rotate() {
 	end
 
 	begin "rotate p flag 1 ${ext}"
-	run_newsyslog
+	run_newsyslog -F
 	ckfe $LOGFNAME
 	ckfe ${LOGFNAME}.0
 	cknt ${LOGFNAME}.0${ext}
-	run_newsyslog
+	run_newsyslog -F
 	ckfe $LOGFNAME
 	ckfe ${LOGFNAME}.0
 	cknt ${LOGFNAME}.0${ext}
 	ckfe ${LOGFNAME}.1${ext}
-	run_newsyslog
+	run_newsyslog -F
 	ckfe $LOGFNAME
 	ckfe ${LOGFNAME}.0
 	cknt ${LOGFNAME}.0${ext}
@@ -507,7 +506,7 @@ tests_normal_rotate_recompress() {
 	end
 
 	begin "rotate normal 1"
-	run_newsyslog
+	run_newsyslog -F
 	ckfe $LOGFNAME
 	ckfe ${LOGFNAME}.0${ext}
 	cknt ${LOGFNAME}.1${ext}
@@ -517,7 +516,7 @@ tests_normal_rotate_recompress() {
 	gunzip ${LOGFNAME}.0${ext}
 	ckfe ${LOGFNAME}.0
 	cknt ${LOGFNAME}.0${ext}
-	run_newsyslog
+	run_newsyslog -F
 	ckfe $LOGFNAME
 	ckfe ${LOGFNAME}.0${ext}
 	ckfe ${LOGFNAME}.1${ext}
