d31625bd9aea199a81c3c4ea5c675b2441c9be49
Kyle Evans <kevans@FreeBSD.org>
2025-05-20 20:31:03 -0500

    wireguard-tools: update to v1.0.20250521
    This includes the following notable changes:
     - Fix for "wg show all endpoints"
     - Fix for INET6-only FreeBSD kernels rolled in
     - Some stubs for incremental updates of AllowedIPs
     - Handling of strdup(3) failure in config bits

--- containers.h
+++ containers.h
@@ -28,6 +28,10 @@ struct timespec64 {
 	int64_t tv_nsec;
 };
 
+enum {
+	WGALLOWEDIP_REMOVE_ME = 1U << 0,
+};
+
 struct wgallowedip {
 	uint16_t family;
 	union {
@@ -35,6 +39,7 @@ struct wgallowedip {
 		struct in6_addr ip6;
 	};
 	uint8_t cidr;
+	uint32_t flags;
 	struct wgallowedip *next_allowedip;
 };
 
