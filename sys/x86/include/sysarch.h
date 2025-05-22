3b4558e5bbdf4981ab7c001dbfdcc30a2a9a5d15
Konstantin Belousov <kib@FreeBSD.org>
2025-05-22 07:32:03 +0300

    amd64: add amd64_set_tlsbase(3)
    Tested by:	pho
    Reviewed by:	olce
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D50482

--- sys/x86/include/sysarch.h
+++ sys/x86/include/sysarch.h
@@ -142,6 +142,7 @@ int amd64_get_fsbase(void **);
 int amd64_get_gsbase(void **);
 int amd64_set_fsbase(void *);
 int amd64_set_gsbase(void *);
+int amd64_set_tlsbase(void *);
 int x86_pkru_get_perm(unsigned int keyidx, int *access, int *modify);
 int x86_pkru_set_perm(unsigned int keyidx, int access, int modify);
 int x86_pkru_protect_range(void *addr, unsigned long len, unsigned int keyidx,
