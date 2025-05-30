f89a24766cb811dba1aa74a65f70df72bc1fe00a
Konstantin Belousov <kib@FreeBSD.org>
2025-05-30 14:24:12 +0300

    thr_new: pass params.flags to cpu_set_user_tls()
    (cherry picked from commit 2b8b37c8f516a5955f81a6fc35c7aac01ddf4a3a)

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -1252,7 +1252,7 @@ int	cpu_procctl(struct thread *td, int idtype, id_t id, int com,
 void	cpu_set_syscall_retval(struct thread *, int);
 int	cpu_set_upcall(struct thread *, void (*)(void *), void *,
 	    stack_t *);
-int	cpu_set_user_tls(struct thread *, void *tls_base);
+int	cpu_set_user_tls(struct thread *, void *tls_base, int flags);
 void	cpu_thread_alloc(struct thread *);
 void	cpu_thread_clean(struct thread *);
 void	cpu_thread_exit(struct thread *);
