f2a587b8b7779ac3adce38c5f34e08ddaef39cfb
John Baldwin <jhb@FreeBSD.org>
2025-06-02 11:05:26 -0400

    cam nvme: Move opcode and status code tables from CTL to base CAM
    This adds support for decoding admin opcodes to the existing opcode
    decoding and also removes one of the duplicate NVMe opcode string
    tables.
    
    - Add nvme_opcode_sbuf and nvme_cpl_sbuf which are CCB
      type-independent and handle the guts of the CTL-specific routines.
    
    - Use nvme_opcode_sbuf to decode the opcode in nvme_command_sbuf.
    
    - Add nvme_status_sbuf to decode the CPL/CQE in a ccb_nvmeio.
    
    Reviewed by:	imp
    Sponsored by:	Chelsio Communications
    Differential Revision:	https://reviews.freebsd.org/D50631

--- sys/cam/nvme/nvme_all.h
+++ sys/cam/nvme/nvme_all.h
@@ -44,8 +44,11 @@ void	nvme_print_ident_short(const struct nvme_controller_data *,
     const struct nvme_namespace_data *, struct sbuf *);
 const char *nvme_op_string(const struct nvme_command *, int admin);
 const char *nvme_cmd_string(const struct nvme_command *, char *, size_t);
+void nvme_opcode_sbuf(bool admin, uint8_t opc, struct sbuf *sb);
 void nvme_cmd_sbuf(const struct nvme_command *, struct sbuf *sb);
 int nvme_command_sbuf(struct ccb_nvmeio *nvmeio, struct sbuf *sb);
+void nvme_cpl_sbuf(const struct nvme_completion *cpl, struct sbuf *sbuf);
+int nvme_status_sbuf(struct ccb_nvmeio *nvmeio, struct sbuf *sb);
 const void *nvme_get_identify_cntrl(struct cam_periph *);
 const void *nvme_get_identify_ns(struct cam_periph *);
 
