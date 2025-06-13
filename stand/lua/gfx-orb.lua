11d6b40c04123f506d8f8032fbd68bbc4af68d62
Kyle Evans <kevans@FreeBSD.org>
2025-06-12 21:23:39 -0500

    stand: lua: convert orb to a new format that allows per-graphic shift
    Previously, we had one shift that had to work for both the ASCII version
    and the image that we may draw if we have a framebuffer to play with.
    That makes it more difficult to get the positioning just right, so split
    them out into their own tables for convenience.
    
    While we're here, stop clipping off the bottom of the orb when the
    autoboot sequence is interrupted.  There's plenty of space for it to
    move up a hair, so let's do that..
    
    Reviewed by:	imp, tsoome
    Differential Revision:	https://reviews.freebsd.org/D50801

--- stand/lua/gfx-orb.lua
+++ stand/lua/gfx-orb.lua
@@ -27,7 +27,8 @@
 
 return {
 	logo = {
-		graphic = {
+		ascii = {
+			image = {
 		    "  \027[31m```                        \027[31;1m`\027[31m",
 		    " s` `.....---...\027[31;1m....--.```   -/\027[31m",
 		    " +o   .--`         \027[31;1m/y:`      +.\027[31m",
@@ -43,10 +44,14 @@ return {
 		    "    `:`                  \027[31;1m`:`",
 		    "      \027[31;1m.--             `--.",
 		    "         .---.....----.\027[m",
+			},
+			requires_color = true,
+			shift = {x = 5, y = -1},
 		},
-		requires_color = true,
-		shift = {x = 2, y = -1},
-		image = "/boot/images/freebsd-logo-rev.png",
-		image_rl = 15
+		fb = {
+			image = "/boot/images/freebsd-logo-rev.png",
+			width = 15,
+			shift = {x = 2, y = -2},
+		},
 	}
 }
