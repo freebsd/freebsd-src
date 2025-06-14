e4e44f69f963dad9e25d589c18b3e9a279e49249
Aymeric Wibo <obiwac@FreeBSD.org>
2025-06-14 17:33:05 +0200

    amdsmu: Expose sysctls for metrics about last sleep
    Get "log" address with the `SMU_MSG_LOG_GETDRAM_ADDR_HI/LO` SMU
    commands. From this, we dump & read metrics about residency in various
    sleep states (none, S0i2, and S0i3), as well as active durations for
    the IP blocks on the CPU during the last sleep. All these metrics are
    exposed under the `dev.amdsmu.0.metrics` node.
    
    This is useful for debugging sleep as the residency registers in the
    `_LPI` object are not populated on some systems (e.g. AMD Phoenix).
    
    A `dev.amdsmu.0.ip_blocks.XXX` node is also created for each IP block,
    with children for the current active state and time spent active during
    the last sleep.
    
    Differential Revision: https://reviews.freebsd.org/D48714
    
    Reviewed by:	cem, kib, mckusick
    Approved by:	kib, mckusick
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D48714

--- sys/dev/amdsmu/amdsmu_reg.h
+++ sys/dev/amdsmu/amdsmu_reg.h
@@ -9,6 +9,8 @@
 #ifndef _AMDSMU_REG_H_
 #define	_AMDSMU_REG_H_
 
+#include <sys/types.h>
+
 /*
  * TODO These are in common with amdtemp; should we find a way to factor these
  * out?  Also, there are way more of these.  I couldn't find a centralized place
@@ -49,5 +51,33 @@ enum amdsmu_msg {
 	SMU_MSG_LOG_DUMP_DATA		= 0x08,
 	SMU_MSG_GET_SUP_CONSTRAINTS	= 0x09,
 };
+
+/* XXX Copied from Linux struct smu_metrics. */
+struct amdsmu_metrics {
+	uint32_t table_version;
+	uint32_t hint_count;
+	uint32_t s0i3_last_entry_status;
+	uint32_t time_last_in_s0i2;
+	uint64_t time_last_entering_s0i3;
+	uint64_t total_time_entering_s0i3;
+	uint64_t time_last_resuming;
+	uint64_t total_time_resuming;
+	uint64_t time_last_in_s0i3;
+	uint64_t total_time_in_s0i3;
+	uint64_t time_last_in_sw_drips;
+	uint64_t total_time_in_sw_drips;
+	/*
+	 * This is how long each IP block was active for (us), i.e., blocking
+	 * entry to S0i3.  In Linux, these are called "timecondition_notmet_*".
+	 *
+	 * XXX Total active time for IP blocks seems to be buggy and reporting
+	 * garbage (at least on Phoenix), so it's disabled for now.  The last
+	 * active time for the USB4_0 IP block also seems to be buggy.
+	 */
+	uint64_t ip_block_last_active_time[32];
+#ifdef IP_BLOCK_TOTAL_ACTIVE_TIME
+	uint64_t ip_block_total_active_time[32];
+#endif
+} __attribute__((packed));
 
 #endif /* _AMDSMU_REG_H_ */
