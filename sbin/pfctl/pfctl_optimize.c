04df3b0967623f517fc8bc9c7ec64018e9ac053e
Kristof Provost <kp@FreeBSD.org>
2025-06-17 13:58:59 +0200

    pfctl: rule optimizer: anchor name vs. anchor path mix up
    OK bluhm@
    
    Obtained from:	OpenBSD, sashan <sashan@openbsd.org>, a2fcc04574
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

--- sbin/pfctl/pfctl_optimize.c
+++ sbin/pfctl/pfctl_optimize.c
@@ -1337,7 +1337,7 @@ again:
 
 
 	if (pfctl_define_table(tbl->pt_name, PFR_TFLAG_CONST, 1,
-	    pf->astack[0]->name, tbl->pt_buf, pf->astack[0]->ruleset.tticket)) {
+	    pf->astack[0]->path, tbl->pt_buf, pf->astack[0]->ruleset.tticket)) {
 		warn("failed to create table %s in %s",
 		    tbl->pt_name, pf->astack[0]->name);
 		return (1);
