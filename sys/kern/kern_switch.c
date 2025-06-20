freebsd/freebsd-src

commit d2ac231616f04bd63795021f23d324351a24788b

Make the run queue parameters machine dependent.  Optimize 64 bit architectures by using a 64 bit word for the bit array which keeps track of non-empty queues.
Reviewed by:	peter

sys/kern/kern_switch.c
@@ -36,6 +36,8 @@
 #include <sys/queue.h>
 #include <machine/critical.h>
 
+CTASSERT((RQB_BPW * RQB_LEN) == RQ_NQS);
+
 /*
  * Global run queue.
  */

sys/alpha/include/runq.h
@@ -26,46 +26,33 @@
  * $FreeBSD$
  */
 
-#ifndef	_RUNQ_H_
-#define	_RUNQ_H_
+#ifndef	_MACHINE_RUNQ_H_
+#define	_MACHINE_RUNQ_H_
 
-#include <machine/runq.h>
+#define	RQB_LEN		(1UL)		/* Number of priority status words. */
+#define	RQB_L2BPW	(6UL)		/* Log2(sizeof(rqb_word_t) * NBBY)). */
+#define	RQB_BPW		(1UL<<RQB_L2BPW)	/* Bits in an rqb_word_t. */
 
-struct kse;
+#define	RQB_BIT(pri)	(1UL << ((pri) & (RQB_BPW - 1)))
+#define	RQB_WORD(pri)	((pri) >> RQB_L2BPW)
 
-/*
- * Run queue parameters.
- */
-
-#define	RQ_NQS		(64)		/* Number of run queues. */
-#define	RQ_PPQ		(4)		/* Priorities per queue. */
+#define	RQB_FFS(word)	(ffs64(word))
 
 /*
- * Head of run queues.
+ * Type of run queue status word.
  */
-TAILQ_HEAD(rqhead, kse);
+typedef	u_int64_t	rqb_word_t;
 
-/*
- * Bit array which maintains the status of a run queue.  When a queue is
- * non-empty the bit corresponding to the queue number will be set.
- */
-struct rqbits {
-	rqb_word_t rqb_bits[RQB_LEN];
-};
+static __inline u_long
+ffs64(u_long mask)
+{
+	u_long bit;
 
-/*
- * Run queue structure.  Contains an array of run queues on which processes
- * are placed, and a structure to maintain the status of each queue.
- */
-struct runq {
-	struct	rqbits rq_status;
-	struct	rqhead rq_queues[RQ_NQS];
-};
-
-void	runq_add(struct runq *, struct kse *);
-int	runq_check(struct runq *);
-struct	kse *runq_choose(struct runq *);
-void	runq_init(struct runq *);
-void	runq_remove(struct runq *, struct kse *);
+	if (mask == 0)
+		return (0);
+	for (bit = 1; (mask & 1UL) == 0; bit++)
+		mask >>= 1UL;
+	return (bit);
+}
 
 #endif

sys/amd64/include/runq.h
@@ -26,46 +26,21 @@
  * $FreeBSD$
  */
 
-#ifndef	_RUNQ_H_
-#define	_RUNQ_H_
+#ifndef	_MACHINE_RUNQ_H_
+#define	_MACHINE_RUNQ_H_
 
-#include <machine/runq.h>
+#define	RQB_LEN		(2)		/* Number of priority status words. */
+#define	RQB_L2BPW	(5)		/* Log2(sizeof(rqb_word_t) * NBBY)). */
+#define	RQB_BPW		(1<<RQB_L2BPW)	/* Bits in an rqb_word_t. */
 
-struct kse;
+#define	RQB_BIT(pri)	(1 << ((pri) & (RQB_BPW - 1)))
+#define	RQB_WORD(pri)	((pri) >> RQB_L2BPW)
 
-/*
- * Run queue parameters.
- */
-
-#define	RQ_NQS		(64)		/* Number of run queues. */
-#define	RQ_PPQ		(4)		/* Priorities per queue. */
+#define	RQB_FFS(word)	(ffs(word))
 
 /*
- * Head of run queues.
+ * Type of run queue status word.
  */
-TAILQ_HEAD(rqhead, kse);
-
-/*
- * Bit array which maintains the status of a run queue.  When a queue is
- * non-empty the bit corresponding to the queue number will be set.
- */
-struct rqbits {
-	rqb_word_t rqb_bits[RQB_LEN];
-};
-
-/*
- * Run queue structure.  Contains an array of run queues on which processes
- * are placed, and a structure to maintain the status of each queue.
- */
-struct runq {
-	struct	rqbits rq_status;
-	struct	rqhead rq_queues[RQ_NQS];
-};
-
-void	runq_add(struct runq *, struct kse *);
-int	runq_check(struct runq *);
-struct	kse *runq_choose(struct runq *);
-void	runq_init(struct runq *);
-void	runq_remove(struct runq *, struct kse *);
+typedef	u_int32_t	rqb_word_t;
 
 #endif

sys/i386/include/runq.h
@@ -26,46 +26,21 @@
  * $FreeBSD$
  */
 
-#ifndef	_RUNQ_H_
-#define	_RUNQ_H_
+#ifndef	_MACHINE_RUNQ_H_
+#define	_MACHINE_RUNQ_H_
 
-#include <machine/runq.h>
+#define	RQB_LEN		(2)		/* Number of priority status words. */
+#define	RQB_L2BPW	(5)		/* Log2(sizeof(rqb_word_t) * NBBY)). */
+#define	RQB_BPW		(1<<RQB_L2BPW)	/* Bits in an rqb_word_t. */
 
-struct kse;
+#define	RQB_BIT(pri)	(1 << ((pri) & (RQB_BPW - 1)))
+#define	RQB_WORD(pri)	((pri) >> RQB_L2BPW)
 
-/*
- * Run queue parameters.
- */
-
-#define	RQ_NQS		(64)		/* Number of run queues. */
-#define	RQ_PPQ		(4)		/* Priorities per queue. */
+#define	RQB_FFS(word)	(ffs(word))
 
 /*
- * Head of run queues.
+ * Type of run queue status word.
  */
-TAILQ_HEAD(rqhead, kse);
-
-/*
- * Bit array which maintains the status of a run queue.  When a queue is
- * non-empty the bit corresponding to the queue number will be set.
- */
-struct rqbits {
-	rqb_word_t rqb_bits[RQB_LEN];
-};
-
-/*
- * Run queue structure.  Contains an array of run queues on which processes
- * are placed, and a structure to maintain the status of each queue.
- */
-struct runq {
-	struct	rqbits rq_status;
-	struct	rqhead rq_queues[RQ_NQS];
-};
-
-void	runq_add(struct runq *, struct kse *);
-int	runq_check(struct runq *);
-struct	kse *runq_choose(struct runq *);
-void	runq_init(struct runq *);
-void	runq_remove(struct runq *, struct kse *);
+typedef	u_int32_t	rqb_word_t;
 
 #endif

sys/ia64/include/runq.h
@@ -26,46 +26,33 @@
  * $FreeBSD$
  */
 
-#ifndef	_RUNQ_H_
-#define	_RUNQ_H_
+#ifndef	_MACHINE_RUNQ_H_
+#define	_MACHINE_RUNQ_H_
 
-#include <machine/runq.h>
+#define	RQB_LEN		(1UL)		/* Number of priority status words. */
+#define	RQB_L2BPW	(6UL)		/* Log2(sizeof(rqb_word_t) * NBBY)). */
+#define	RQB_BPW		(1UL<<RQB_L2BPW)	/* Bits in an rqb_word_t. */
 
-struct kse;
+#define	RQB_BIT(pri)	(1UL << ((pri) & (RQB_BPW - 1)))
+#define	RQB_WORD(pri)	((pri) >> RQB_L2BPW)
 
-/*
- * Run queue parameters.
- */
-
-#define	RQ_NQS		(64)		/* Number of run queues. */
-#define	RQ_PPQ		(4)		/* Priorities per queue. */
+#define	RQB_FFS(word)	(ffs64(word))
 
 /*
- * Head of run queues.
+ * Type of run queue status word.
  */
-TAILQ_HEAD(rqhead, kse);
+typedef	u_int64_t	rqb_word_t;
 
-/*
- * Bit array which maintains the status of a run queue.  When a queue is
- * non-empty the bit corresponding to the queue number will be set.
- */
-struct rqbits {
-	rqb_word_t rqb_bits[RQB_LEN];
-};
+static __inline u_long
+ffs64(u_long mask)
+{
+	u_long bit;
 
-/*
- * Run queue structure.  Contains an array of run queues on which processes
- * are placed, and a structure to maintain the status of each queue.
- */
-struct runq {
-	struct	rqbits rq_status;
-	struct	rqhead rq_queues[RQ_NQS];
-};
-
-void	runq_add(struct runq *, struct kse *);
-int	runq_check(struct runq *);
-struct	kse *runq_choose(struct runq *);
-void	runq_init(struct runq *);
-void	runq_remove(struct runq *, struct kse *);
+	if (mask == 0)
+		return (0);
+	for (bit = 1; (mask & 1UL) == 0; bit++)
+		mask >>= 1UL;
+	return (bit);
+}
 
 #endif

sys/powerpc/include/runq.h
@@ -26,46 +26,21 @@
  * $FreeBSD$
  */
 
-#ifndef	_RUNQ_H_
-#define	_RUNQ_H_
+#ifndef	_MACHINE_RUNQ_H_
+#define	_MACHINE_RUNQ_H_
 
-#include <machine/runq.h>
+#define	RQB_LEN		(2)		/* Number of priority status words. */
+#define	RQB_L2BPW	(5)		/* Log2(sizeof(rqb_word_t) * NBBY)). */
+#define	RQB_BPW		(1<<RQB_L2BPW)	/* Bits in an rqb_word_t. */
 
-struct kse;
+#define	RQB_BIT(pri)	(1 << ((pri) & (RQB_BPW - 1)))
+#define	RQB_WORD(pri)	((pri) >> RQB_L2BPW)
 
-/*
- * Run queue parameters.
- */
-
-#define	RQ_NQS		(64)		/* Number of run queues. */
-#define	RQ_PPQ		(4)		/* Priorities per queue. */
+#define	RQB_FFS(word)	(ffs(word))
 
 /*
- * Head of run queues.
+ * Type of run queue status word.
  */
-TAILQ_HEAD(rqhead, kse);
-
-/*
- * Bit array which maintains the status of a run queue.  When a queue is
- * non-empty the bit corresponding to the queue number will be set.
- */
-struct rqbits {
-	rqb_word_t rqb_bits[RQB_LEN];
-};
-
-/*
- * Run queue structure.  Contains an array of run queues on which processes
- * are placed, and a structure to maintain the status of each queue.
- */
-struct runq {
-	struct	rqbits rq_status;
-	struct	rqhead rq_queues[RQ_NQS];
-};
-
-void	runq_add(struct runq *, struct kse *);
-int	runq_check(struct runq *);
-struct	kse *runq_choose(struct runq *);
-void	runq_init(struct runq *);
-void	runq_remove(struct runq *, struct kse *);
+typedef	u_int32_t	rqb_word_t;
 
 #endif

sys/sparc64/include/runq.h
@@ -26,46 +26,33 @@
  * $FreeBSD$
  */
 
-#ifndef	_RUNQ_H_
-#define	_RUNQ_H_
+#ifndef	_MACHINE_RUNQ_H_
+#define	_MACHINE_RUNQ_H_
 
-#include <machine/runq.h>
+#define	RQB_LEN		(1UL)		/* Number of priority status words. */
+#define	RQB_L2BPW	(6UL)		/* Log2(sizeof(rqb_word_t) * NBBY)). */
+#define	RQB_BPW		(1UL<<RQB_L2BPW)	/* Bits in an rqb_word_t. */
 
-struct kse;
+#define	RQB_BIT(pri)	(1UL << ((pri) & (RQB_BPW - 1)))
+#define	RQB_WORD(pri)	((pri) >> RQB_L2BPW)
 
-/*
- * Run queue parameters.
- */
-
-#define	RQ_NQS		(64)		/* Number of run queues. */
-#define	RQ_PPQ		(4)		/* Priorities per queue. */
+#define	RQB_FFS(word)	(ffs64(word))
 
 /*
- * Head of run queues.
+ * Type of run queue status word.
  */
-TAILQ_HEAD(rqhead, kse);
+typedef	u_int64_t	rqb_word_t;
 
-/*
- * Bit array which maintains the status of a run queue.  When a queue is
- * non-empty the bit corresponding to the queue number will be set.
- */
-struct rqbits {
-	rqb_word_t rqb_bits[RQB_LEN];
-};
+static __inline u_long
+ffs64(u_long mask)
+{
+	u_long bit;
 
-/*
- * Run queue structure.  Contains an array of run queues on which processes
- * are placed, and a structure to maintain the status of each queue.
- */
-struct runq {
-	struct	rqbits rq_status;
-	struct	rqhead rq_queues[RQ_NQS];
-};
-
-void	runq_add(struct runq *, struct kse *);
-int	runq_check(struct runq *);
-struct	kse *runq_choose(struct runq *);
-void	runq_init(struct runq *);
-void	runq_remove(struct runq *, struct kse *);
+	if (mask == 0)
+		return (0);
+	for (bit = 1; (mask & 1UL) == 0; bit++)
+		mask >>= 1UL;
+	return (bit);
+}
 
 #endif

commit 3e5aeb20645fa0a67ce50d96c5136de14067a944

runq: Fix printing status word bits
'rqsw_t' is 'unsigned long', strictly longer than integers on 64-bit
platforms.

While here, factor out the print format for a status word.

Fixes:          d2ac231616f0 ("Make the run queue parameters machine dependent...")
MFC after:      1 month
Event:          Kitchener-Waterloo Hackathon 202506
Sponsored by:   The FreeBSD Foundation

sys/kern/kern_switch.c
@@ -326,7 +326,9 @@ runq_sw_set_not_empty_op(int idx, int sw_idx, rqsw_t sw_bit, rqsw_t *swp)
 	rqsw_t old_sw __unused = *swp;
 
 	*swp |= sw_bit;
-	CTR4(KTR_RUNQ, "runq_sw_set_not_empty: idx=%d sw_idx=%d bits=%#x->%#x",
+	CTR4(KTR_RUNQ,
+	    "runq_sw_set_not_empty: idx=%d sw_idx=%d "
+	    "bits=" RQSW_PRI "->" RQSW_PRI,
 	    idx, sw_idx, old_sw, *swp);
 	return (0);
 }
@@ -349,7 +351,9 @@ runq_sw_set_empty_op(int idx, int sw_idx, rqsw_t sw_bit, rqsw_t *swp)
 	rqsw_t old_sw __unused = *swp;
 
 	*swp &= ~sw_bit;
-	CTR4(KTR_RUNQ, "runq_sw_set_empty: idx=%d sw_idx=%d bits=%#x->%#x",
+	CTR4(KTR_RUNQ,
+	    "runq_sw_set_empty: idx=%d sw_idx=%d "
+	    "bits=" RQSW_PRI "->" RQSW_PRI,
 	    idx, sw_idx, old_sw, *swp);
 	return (0);
 }
@@ -525,7 +529,8 @@ last_mask:
 		goto return_idx;
 	return (-1);
 return_idx:
-	CTR4(KTR_RUNQ, "runq_findq: bits=%#x->%#x i=%d idx=%d",
+	CTR4(KTR_RUNQ,
+	    "runq_findq: bits=" RQSW_PRI "->" RQSW_PRI " i=%d idx=%d",
 	    (*rqsw)[i], w, i, idx);
 	return (idx);
 }

sys/sys/runq.h
@@ -60,6 +60,7 @@ struct thread;
 
 typedef unsigned long	rqsw_t;		/* runq's status words type. */
 #define	RQSW_BPW	(sizeof(rqsw_t) * NBBY) /* Bits per runq word. */
+#define RQSW_PRI	"%#lx"		/* printf() directive. */
 
 /* Number of status words to cover RQ_NQS queues. */
 #define	RQSW_NB			(howmany(RQ_NQS, RQSW_BPW))