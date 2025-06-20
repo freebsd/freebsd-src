cce16908f71c92b76b7d629a7c4b6713802aaf7e
Gleb Smirnoff <glebius@FreeBSD.org>
2025-06-20 15:37:47 -0700

    libc/rpc/svc_nl: plg a memory leak
    CID:	1591126

--- lib/libc/rpc/svc_nl.c
+++ lib/libc/rpc/svc_nl.c
@@ -92,7 +92,7 @@ svc_nl_create(const char *service)
 		.nl_family = PF_NETLINK,
 	};
 	struct nl_softc *sc;
-	SVCXPRT *xprt;
+	SVCXPRT *xprt = NULL;
 	void *buf = NULL;
 	uint16_t family;
 	ssize_t len = 1024;
@@ -133,6 +133,7 @@ svc_nl_create(const char *service)
 
 	return (xprt);
 fail:
+	free(xprt);
 	free(buf);
 	snl_free(&sc->snl);
 	free(sc);
