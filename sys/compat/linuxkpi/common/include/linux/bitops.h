freebsd/freebsd-src

commit 94cfe4c3c1b2ef9e8d436f5a5ecbb93db0f17743

LinuxKPI: add HWEIGHT32()
Add HWEIGHT32() macro needed by iwlwifi and while here add the 8/16/64
variants likewise.

Sponsored by:	The FreeBSD Foundation
MFC after:	12 days
Reviewed by:	hselasky
Differential Revision: https://reviews.freebsd.org/D30501

sys/compat/linuxkpi/common/include/linux/bitops.h
@@ -63,6 +63,11 @@
 #define	hweight64(x)	bitcount64(x)
 #define	hweight_long(x)	bitcountl(x)
 
+#define	HWEIGHT8(x)	(bitcount8((uint8_t)(x)) + 1)
+#define	HWEIGHT16(x)	(bitcount16(x) + 1)
+#define	HWEIGHT32(x)	(bitcount32(x) + 1)
+#define	HWEIGHT64(x)	(bitcount64(x) + 1)
+
 static inline int
 __ffs(int mask)
 {

commit 45d02ea5412cb7d5f48d00ccb08eafc829269e83

linuxkpi: Remove incorrect + 1 from HWEIGHT* macros
Fixes: 94cfe4c3c1b2 ("LinuxKPI: add HWEIGHT32()")
Reviewed by:	bz
Sponsored by:	The FreeBSD Foundation
Differential Revision: https://reviews.freebsd.org/D51010

sys/compat/linuxkpi/common/include/linux/bitops.h
@@ -62,10 +62,10 @@
 #define	hweight64(x)	bitcount64(x)
 #define	hweight_long(x)	bitcountl(x)
 
-#define	HWEIGHT8(x)	(bitcount8((uint8_t)(x)) + 1)
-#define	HWEIGHT16(x)	(bitcount16(x) + 1)
-#define	HWEIGHT32(x)	(bitcount32(x) + 1)
-#define	HWEIGHT64(x)	(bitcount64(x) + 1)
+#define	HWEIGHT8(x)	(bitcount8((uint8_t)(x)))
+#define	HWEIGHT16(x)	(bitcount16(x))
+#define	HWEIGHT32(x)	(bitcount32(x))
+#define	HWEIGHT64(x)	(bitcount64(x))
 
 static inline int
 __ffs(int mask)