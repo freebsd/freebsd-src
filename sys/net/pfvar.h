c9beb64eecb5d8d855a5e3acee10d9d62b531569
Kristof Provost <kp@FreeBSD.org>
2025-06-30 18:35:31 +0200

    pf: pf_create_kanchor() can be static
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

--- sys/net/pfvar.h
+++ sys/net/pfvar.h
@@ -2678,7 +2678,6 @@ void			 pf_kanchor_remove(struct pf_krule *);
 void			 pf_remove_if_empty_kruleset(struct pf_kruleset *);
 struct pf_kruleset	*pf_find_kruleset(const char *);
 struct pf_kruleset	*pf_get_leaf_kruleset(char *, char **);
-struct pf_kanchor	*pf_create_kanchor(struct pf_kanchor *, const char *);
 struct pf_kruleset	*pf_find_or_create_kruleset(const char *);
 void			 pf_rs_initialize(void);
 
