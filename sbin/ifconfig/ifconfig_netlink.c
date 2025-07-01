8f43b39b1c38bde166314d4682daae7bf15a5445
Alexander V. Chernikov <melifaro@FreeBSD.org>
2025-07-01 20:16:57 +0700

    ifconfig_netlink.c: MFC: optimise non-listing case
    This change produced by melifaro and the commit is concerted with him
    as he is pretty busy IRL these days.
    
    The change restores performance of /sbin/ifconfig utility
    for non-listing case after it switched from rtsock to netlink(4) API
    in FreeBSD 14+.
    
    PR:		287872
    
    (cherry picked from commit b1b17432aa1be670564161232d110461a5dde4ce)

--- sbin/ifconfig/ifconfig_netlink.c
+++ sbin/ifconfig/ifconfig_netlink.c
@@ -162,7 +162,7 @@ struct ifmap {
  * Memory is allocated using snl temporary buffers
  */
 static struct ifmap *
-prepare_ifmap(struct snl_state *ss)
+prepare_ifmap(struct snl_state *ss, const char *ifname)
 {
 	struct snl_writer nw = {};
 
@@ -170,6 +170,8 @@ prepare_ifmap(struct snl_state *ss)
 	struct nlmsghdr *hdr = snl_create_msg_request(&nw, RTM_GETLINK);
 	hdr->nlmsg_flags |= NLM_F_DUMP;
 	snl_reserve_msg_object(&nw, struct ifinfomsg);
+       if (ifname != NULL)
+               snl_add_msg_attr_string(&nw, IFLA_IFNAME, ifname);
 
 	if (! (hdr = snl_finalize_msg(&nw)) || !snl_send_message(ss, hdr))
 		return (NULL);
@@ -457,7 +459,7 @@ list_interfaces_nl(struct ifconfig_args *args)
 
 	nl_init_socket(&ss);
 
-	struct ifmap *ifmap = prepare_ifmap(&ss);
+       struct ifmap *ifmap = prepare_ifmap(&ss, args->ifname);
 	struct iface **sorted_ifaces = snl_allocz(&ss, ifmap->count * sizeof(void *));
 	for (uint32_t i = 0, num = 0; i < ifmap->size; i++) {
 		if (ifmap->ifaces[i] != NULL) {
