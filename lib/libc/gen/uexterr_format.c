ab3598bc3bc91fc603be19487b1a88ad39cf5bc6
Konstantin Belousov <kib@FreeBSD.org>
2025-07-03 06:04:23 +0300

    uexterr_format(): simplify output when ext error string is available
    If the extended error string is provided by kernel, return only the
    string, which is supposedly enough to identify exact cause of the error.
    If the string is not provided, print the technically looking gibberish
    which still allows to identify location with kernel sources.
    
    Reviewed by:	asomers, brooks
    Sponsored by:	The FreeBSD Foundation
    Differential revision:	https://reviews.freebsd.org/D51141

--- lib/libc/gen/uexterr_format.c
+++ lib/libc/gen/uexterr_format.c
@@ -23,9 +23,13 @@ __uexterr_format(const struct uexterror *ue, char *buf, size_t bufsz)
 		strlcpy(buf, "No error", bufsz);
 		return (0);
 	}
-	snprintf(buf, bufsz,
-	    "errno %d category %u (src line %u) p1 %#jx p2 %#jx %s",
-	    ue->error, ue->cat, ue->src_line,
-	    (uintmax_t)ue->p1, (uintmax_t)ue->p2, ue->msg);
+	if (ue->msg[0] == '\0') {
+		snprintf(buf, bufsz,
+		    "errno %d category %u (src line %u) p1 %#jx p2 %#jx",
+		    ue->error, ue->cat, ue->src_line,
+		    (uintmax_t)ue->p1, (uintmax_t)ue->p2);
+	} else {
+		strlcpy(buf, ue->msg, bufsz);
+	}
 	return (0);
 }
