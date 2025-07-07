4c346407241eea1169c6b187b3dca9522bd1f463
Lexi Winter <ivy@FreeBSD.org>
2025-07-07 14:30:13 +0100

    packages: move ucl files to a subdirectory
    We want to have a UCL file for every package, to set fields like
    comment, desc, license, etc.; to avoid cluttering release/packages with
    ~200 new files, move these to the ucl/ directory.
    
    Add a better description for the yp package to demonstrate how this will
    work in practice.
    
    Clean up the handling of the package comment suffix for generated
    packages (dev, lib32, etc.) and do the UCL modifications in
    generate-ucl.lua, so we don't overwrite the ucl's comment.
    
    Add a long-form suffix to the description for generated packages to
    explain what they do.
    
    Eventually, we should require the ucl file exists (try=false) to prevent
    people accidentally creating new packages with a typo, but for now, keep
    the ucl optional.
    
    Reviewed by:	kevans, des, bapt, emaste
    Approved by:	kevans (mentor), des (mentor)
    Differential Revision:	https://reviews.freebsd.org/D50160

--- release/packages/generate-ucl.lua
+++ release/packages/generate-ucl.lua
@@ -3,33 +3,66 @@
 --[[ usage:
 generare-ucl.lua [<variablename> <variablevalue>]... <sourceucl> <destucl>
 
-In the <destucl> files the variable <variablename> (in the form ${variablename}
-in the <sourceucl>) will be expanded to <variablevalue>.
+Build a package's UCL configuration by loading the template UCL file
+<sourceucl>, replacing any $VARIABLES in the UCL based on the provided
+variables, then writing the result to <destucl>.
 
-The undefined variables will reamin unmofifier "${variablename}"
+If COMMENT_SUFFIX or DESC_SUFFIX are set, append these to the generated comment
+and desc fields.  We do this here because there's no way to do it in
+template.ucl.
 ]]--
 
 local ucl = require("ucl")
 
+local comment_suffix = nil
+local desc_suffix = nil
+
+-- This parser is the output UCL we want to build.
+local parser = ucl.parser()
+
+-- Set any $VARIABLES from the command line in the parser.  This causes ucl to
+-- automatically replace them when we load the source ucl.
 if #arg < 2 or #arg % 2 ~= 0 then
 	io.stderr:write(arg[0] .. ": expected an even number of arguments, got " .. #arg)
 	os.exit(1)
 end
 
-local parser = ucl.parser()
 for i = 2, #arg - 2, 2 do
-	parser:register_variable(arg[i - 1], arg[i])
+	local varname = arg[i - 1]
+	local varvalue = arg[i]
+
+	if varname == "COMMENT_SUFFIX" and #varvalue > 0 then
+		comment_suffix = varvalue
+	elseif varname == "DESC_SUFFIX" and #varvalue > 0 then
+		desc_suffix = varvalue
+	end
+
+	parser:register_variable(varname, varvalue)
 end
+
+-- Load the source ucl file.
 local res,err = parser:parse_file(arg[#arg - 1])
 if not res then
 	io.stderr:write(arg[0] .. ": fail to parse("..arg[#arg - 1].."): "..err)
 	os.exit(1)
 end
+
+local obj = parser:get_object()
+
+-- Add comment and desc suffix.
+if comment_suffix ~= nil then
+	obj["comment"] = obj["comment"] .. comment_suffix
+end
+if desc_suffix ~= nil then
+	obj["desc"] = obj["desc"] .. "\n\n" .. desc_suffix
+end
+
+-- Write the output file.
 local f,err = io.open(arg[#arg], "w")
 if not f then
 	io.stderr:write(arg[0] .. ": fail to open("..arg[#arg].."): ".. err)
 	os.exit(1)
 end
-local obj = parser:get_object()
+
 f:write(ucl.to_format(obj, 'ucl', true))
 f:close()
