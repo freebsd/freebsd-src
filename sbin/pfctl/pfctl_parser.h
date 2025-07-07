0993870f71eeffecd9ac1ac22f5e170385694f85
Kristof Provost <kp@FreeBSD.org>
2025-07-07 11:39:07 +0200

    pf: rename PF_OPT_TABLE_PREFIX to PF_OPTIMIZER_TABLE_PFX
    Move it to pf.h.
    OPT is misleading and usually refers to command line arguments to pfctl
    
    ok sashan kn
    
    Obtained from:	OpenBSD, henning <henning@openbsd.org>, 9c6ad19ba4
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

--- sbin/pfctl/pfctl_parser.h
+++ sbin/pfctl/pfctl_parser.h
@@ -263,7 +263,6 @@ struct pf_opt_tbl {
 	struct node_tinithead	 pt_nodes;
 	struct pfr_buffer	*pt_buf;
 };
-#define PF_OPT_TABLE_PREFIX	"__automatic_"
 
 /* optimizer pf_rule container */
 struct pf_opt_rule {
