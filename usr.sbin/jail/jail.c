6d24bb6eb000c2ee72a41fa4d918effa36e213dc
Jim Long <github@umpquanet.com>
2025-07-10 14:45:26 -0700

    Implement NUL-terminated output strings for "jail -e".
    When the user specifies an empty string as the separator for "jail -e",
    such as:
    
    	# jail -e ''
    
    this patch causes jail(8) to print the jail parameters defined in
    /etc/jail.conf (or other file specified by -f) as nul-terminated strings.
    After the last variable for a given jail, one additional nul appears,
    followed by the output for the next jail, if any.  No quoting or character
    escaping is done on the values.
    
    This permits a script to conveniently and accurately read the output from
    jail(8) and take actions based on the variables defined.
    
    The jail(8) man page is also updated to reflect this functionality.

--- usr.sbin/jail/jail.c
+++ usr.sbin/jail/jail.c
@@ -969,6 +969,7 @@ print_jail(FILE *fp, struct cfjail *j, int oldcl, int running)
 #endif
 		fputs(separator, fp);
 		print_param(fp, j->intparams[IP_COMMAND], ' ', 0);
+		putc('\n', fp);
 	} else {
 		printsep = 0;
 		if (running) {
@@ -978,13 +979,18 @@ print_jail(FILE *fp, struct cfjail *j, int oldcl, int running)
 		TAILQ_FOREACH(p, &j->params, tq)
 			if (strcmp(p->name, "jid")) {
 				if (printsep)
-					fputs(separator, fp);
+					*separator ? fputs(separator, fp) : putc(0, fp);
 				else
 					printsep = 1;
 				print_param(fp, p, ',', 1);
 			}
+		if (*separator)
+			putc('\n', fp);
+		else {
+			putc(0, fp);
+			putc(0, fp);
+		}
 	}
-	putc('\n', fp);
 }
 
 /*
@@ -1029,7 +1035,8 @@ quoted_print(FILE *fp, char *str)
 	int c, qc;
 	char *p = str;
 
-	qc = !*p ? '"'
+	qc = !*separator ? 0
+	    : !*p ? '"'
 	    : strchr(p, '\'') ? '"'
 	    : strchr(p, '"') ? '\''
 	    : strchr(p, ' ') || strchr(p, '\t') ? '"'
