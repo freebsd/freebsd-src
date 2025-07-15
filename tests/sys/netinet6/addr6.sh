33b92e604eb5c88e7ca9716f511a72c09f4853de
Lexi Winter <ivy@FreeBSD.org>
2025-07-15 06:11:40 +0100

    netinet6: allow binding a raw socket to an anycast address
    Raw sockets have a separate check for this in rip6_bind() that was
    missed in the previous change.  This fixes e.g. 'ping -S' using an
    anycast address.
    
    Fixes: a82811ddbf50 ("netinet6: allow binding to anycast addresses")
    Reviewed by:	tuexen, kevans, des (previous version)
    Approved by:	kevans (mentor)
    Differential Revision:	https://reviews.freebsd.org/D50438

--- tests/sys/netinet6/addr6.sh
+++ tests/sys/netinet6/addr6.sh
@@ -39,7 +39,32 @@ addr6_invalid_addr_cleanup()
 	vnet_cleanup
 }
 
+atf_test_case "anycast_raw_addr" "cleanup"
+anycast_raw_addr_head()
+{
+	atf_set descr "a raw socket can bind to an anycast address"
+	atf_set require.user root
+}
+
+anycast_raw_addr_body()
+{
+	# lo0 needs to be up in the test jail for this test to work
+	ifconfig lo0 up
+
+	netif=$(ifconfig lo create)
+	echo $netif >netif
+	atf_check -s exit:0 ifconfig $netif inet6 2001:db8::1/128 up
+	atf_check -s exit:0 ifconfig $netif inet6 2001:db8::2/128 anycast
+	atf_check -s exit:0 -o ignore ping -c1 -S 2001:db8::2 2001:db8::1
+}
+
+anycast_raw_addr_cleanup()
+{
+	ifconfig $(cat netif) destroy
+}
+
 atf_init_test_cases()
 {
 	atf_add_test_case "addr6_invalid_addr"
+	atf_add_test_case "anycast_raw_addr"
 }
