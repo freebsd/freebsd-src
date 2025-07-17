0096fe40d60da5d7d336fa3ff99f983bb3d73451
Nazarenko Nikita Vladimirovich <nazarenko@tuta.com>
2025-07-18 01:34:46 +0300

    kldstat: Memory zeroing using memset()
    Use memset() instead of the outdated bzero() function when zeroing
    memory. bzero() was in the POSIX.1-2001 standard.
    
    Reviewed by: imp
    Pull Request: https://github.com/freebsd/freebsd-src/pull/1774
    Closes: https://github.com/freebsd/freebsd-src/pull/1774

--- sbin/kldstat/kldstat.c
+++ sbin/kldstat/kldstat.c
@@ -35,7 +35,7 @@
 #include <libutil.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include <unistd.h>
 
 #define	PTR_WIDTH ((int)(sizeof(void *) * 2 + 2))
@@ -51,7 +51,7 @@ printmod(int modid)
 {
 	struct module_stat stat;
 
-	bzero(&stat, sizeof(stat));
+	memset(&stat, 0, sizeof(stat));
 	stat.version = sizeof(struct module_stat);
 	if (modstat(modid, &stat) < 0) {
 		warn("can't stat module id %d", modid);
