1b5eb96898d78f6a83855b7793e8ac3e16b5438b
Cy Schubert <cy@FreeBSD.org>
2025-07-19 07:11:18 -0700

    KRB5: Enable MIT KRB5 by default
    Set WITH_MITKRB5=yes as the default.
    
    Rebuild all USES=gssapi ports is recommended.
    
    A clean buildworld is required.
    
    Relnotes:	yes

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -74,7 +74,7 @@
  * cannot include sys/param.h and should only be updated here.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1500053
+#define __FreeBSD_version 1500054
 
 /*
  * __FreeBSD_kernel__ indicates that this system uses the kernel of FreeBSD,
