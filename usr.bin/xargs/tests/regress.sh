a7fa987abe1fb0b25a7dd83dc81f14f64a82efc9
Dag-Erling Smørgrav <des@FreeBSD.org>
2025-07-26 18:44:35 +0200

    xargs: Limit -n to {ARG_MAX}
    Since it's not possible to pass more than {ARG_MAX} bytes on the command
    line, it's also not possible to pass more than {ARG_MAX} individual
    arguments.  Therefore, {ARG_MAX} is a reasonable upper bound for the -n
    option.  This resolves both the arithmetic overflow issue and the CI OOM
    issue, so we can safely re-enable the test.
    
    Fixes:		eab91d008165
    Fixes:		2682a1552724
    MFC after:	1 week
    Reviewed by:	jlduran, emaste
    Differential Revision:	https://reviews.freebsd.org/D51536

--- usr.bin/xargs/tests/regress.sh
+++ usr.bin/xargs/tests/regress.sh
@@ -1,5 +1,5 @@
 
-echo 1..21
+echo 1..23
 
 REGRESSION_START($1)
 
@@ -12,9 +12,8 @@ REGRESSION_TEST(`R', `xargs -I% -R1 echo The % % % %% % % <${SRCDIR}/regress.in'
 REGRESSION_TEST(`R-1', `xargs -I% -R-1 echo The % % % %% % % <${SRCDIR}/regress.in')
 REGRESSION_TEST(`n1', `xargs -n1 echo <${SRCDIR}/regress.in')
 REGRESSION_TEST(`n2', `xargs -n2 echo <${SRCDIR}/regress.in')
-# This test may consume a large amount of memory, making it unsuited to CI
-# environments.  Disable it for now.
-#REGRESSION_TEST(`n2147483647', `xargs -n2147483647 <${SRCDIR}/regress.in')
+argmax=$(sysctl -n kern.argmax)
+REGRESSION_TEST(`nargmax', `xargs -n$argmax <${SRCDIR}/regress.in')
 REGRESSION_TEST(`n2P0',`xargs -n2 -P0 echo <${SRCDIR}/regress.in | sort')
 REGRESSION_TEST(`n3', `xargs -n3 echo <${SRCDIR}/regress.in')
 REGRESSION_TEST(`0', `xargs -0 -n1 echo <${SRCDIR}/regress.0.in')
@@ -28,5 +27,6 @@ REGRESSION_TEST_FREEFORM(`parallel1', `echo /var/empty       /var/empty       | 
 REGRESSION_TEST_FREEFORM(`parallel2', `echo /var/empty       /var/empty/nodir | xargs -n1 -P2 test -d; [ $? = 1 ]')
 REGRESSION_TEST_FREEFORM(`parallel3', `echo /var/empty/nodir /var/empty       | xargs -n1 -P2 test -d; [ $? = 1 ]')
 REGRESSION_TEST_FREEFORM(`parallel4', `echo /var/empty/nodir /var/empty/nodir | xargs -n1 -P2 test -d; [ $? = 1 ]')
+REGRESSION_TEST_FREEFORM(`ntoobig', `seq 42 | xargs -n$((argmax+1)); [ $? = 1 ]')
 
 REGRESSION_END()
