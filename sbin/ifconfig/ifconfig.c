878d3bdb59d70b2ce077f4bbdefea0ec875d6e82
Zhenlei Huang <zlei@FreeBSD.org>
2025-08-02 00:33:09 +0800

    ifconfig: Garbage collect unused parameter swabips
    and the macro EN_SWABIPS.
    
    The macro EN_SWABIPS is identical to IFF_LINK0 (also historically
    IFF_LLC0) and we already have the parameter link0 to toggle IFF_LINK0.
    
    These were inherited from 386BSD 0.1 and have never been used since
    the very first FreeBSD release.
    
    Reviewed by:	adrian, #network
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D51368
    
    (cherry picked from commit 8632e4e73a6934f3f9996a18932e36b04e6a3faf)

--- sbin/ifconfig/ifconfig.c
+++ sbin/ifconfig/ifconfig.c
@@ -2028,11 +2028,6 @@ static struct cmd basic_cmds[] = {
 	DEF_CMD("-alias",	-IFF_UP,	notealias),
 	DEF_CMD("delete",	-IFF_UP,	notealias),
 	DEF_CMD("remove",	-IFF_UP,	notealias),
-#ifdef notdef
-#define	EN_SWABIPS	0x1000
-	DEF_CMD("swabips",	EN_SWABIPS,	setifflags),
-	DEF_CMD("-swabips",	EN_SWABIPS,	clearifflags),
-#endif
 	DEF_CMD_ARG("netmask",			setifnetmask),
 	DEF_CMD_ARG("metric",			setifmetric),
 	DEF_CMD_ARG("broadcast",		setifbroadaddr),
