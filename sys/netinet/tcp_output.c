aef089775acaa85fb0ae67b68407070fb65c3e29
Richard Scheffenegger <rscheff@FreeBSD.org>
2025-08-06 22:25:30 +0200

    tcp: ensure SACK rxmit never ends up left of its hole
    When a RTO happens during SACK loss recovery, snd_recover can possibly pulled left.
    With Lost Retransmission Detection (LRD) this can lead to rxmit of a hole to end up
    pointing to the left of the hole, which is unexpected and leads to complications.
    
    Reviewed By: tuexen, #transport
    Sponsored by: NetApp, Inc.
    MFC after: 1 week
    Differential Revision: https://reviews.freebsd.org/D51725

--- sys/netinet/tcp_output.c
+++ sys/netinet/tcp_output.c
@@ -1250,7 +1250,7 @@ send:
 		 * fack acks recoverypoint.
 		 */
 		if ((tp->t_flags & TF_LRD) && SEQ_GEQ(p->rxmit, p->end))
-			p->rxmit = tp->snd_recover;
+			p->rxmit = SEQ_MAX(p->rxmit, tp->snd_recover);
 		tp->sackhint.sack_bytes_rexmit += len;
 	}
 	if (IN_RECOVERY(tp->t_flags)) {
