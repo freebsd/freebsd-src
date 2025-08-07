fd3ebfd235fa863ef6ca4d3ad0b613ac060ded4b
Simon J. Gerraty <sjg@FreeBSD.org>
2025-08-07 15:15:37 -0700

    Replace license with just SPDX BSD-2-Clause

--- libexec/rc/safe_eval.sh
+++ libexec/rc/safe_eval.sh
@@ -1,16 +1,10 @@
-# SPDX-License-Identifier: BSD-2-Clause
-
+:
 # RCSid:
-#	$Id: safe_eval.sh,v 1.24 2025/05/23 21:34:54 sjg Exp $
+#	$Id: safe_eval.sh,v 1.25 2025/08/07 22:13:03 sjg Exp $
 #
 #	@(#) Copyright (c) 2023-2024 Simon J. Gerraty
 #
-#	This file is provided in the hope that it will
-#	be of use.  There is absolutely NO WARRANTY.
-#	Permission to copy, redistribute or otherwise
-#	use this file is hereby granted provided that
-#	the above copyright notice and this notice are
-#	left intact.
+#	SPDX-License-Identifier: BSD-2-Clause
 #
 #	Please send copies of changes and bug-fixes to:
 #	sjg@crufty.net
