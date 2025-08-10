af22612804d6022b661e8a217003b06b30c916f9
Lexi Winter <ivy@FreeBSD.org>
2025-08-10 13:57:36 +0100

    Remove Secure RPC DES authentication
    Following the earlier removal of keyserv, none of this functionality
    works since it requires keyserv.
    
    Remove the relevant symbols from libc's Symbol.map.  Leave compatibility
    symbols for existing applications, but since the functions don't work
    without keyserv, stub them out to return an error.
    
    Remove some private symbols that were only used by keyserv; these don't
    get compatibility symbols.
    
    Remove the documentation for the old functions.
    
    Remove rpc.ypupdated since it requires DES authentication.
    
    Reviewed by:		manu, des, emaste
    Differential Revision:	https://reviews.freebsd.org/D50442

--- include/rpc/auth_des.h
+++ include/rpc/auth_des.h
@@ -33,91 +33,14 @@
  * Copyright (c) 1986 - 1991 by Sun Microsystems, Inc.
  */
 
-/*
- * auth_des.h, Protocol for DES style authentication for RPC
- */
+/* Note, RPC DES authentication was removed in FreeBSD 15.0. */
 
 #ifndef _AUTH_DES_
 #define _AUTH_DES_
 
-/*
- * There are two kinds of "names": fullnames and nicknames
- */
-enum authdes_namekind {
-	ADN_FULLNAME, 
-	ADN_NICKNAME
-};
-
-/*
- * A fullname contains the network name of the client, 
- * a conversation key and the window
- */
-struct authdes_fullname {
-	char *name;		/* network name of client, up to MAXNETNAMELEN */
-	des_block key;		/* conversation key */
-	u_long window;		/* associated window */
-};
-
-
-/*
- * A credential 
- */
-struct authdes_cred {
-	enum authdes_namekind adc_namekind;
-	struct authdes_fullname adc_fullname;
-	u_long adc_nickname;
-};
-
-
-
-/*
- * A des authentication verifier 
- */
-struct authdes_verf {
-	union {
-		struct timeval adv_ctime;	/* clear time */
-		des_block adv_xtime;		/* crypt time */
-	} adv_time_u;
-	u_long adv_int_u;
-};
-
-/*
- * des authentication verifier: client variety
- *
- * adv_timestamp is the current time.
- * adv_winverf is the credential window + 1.
- * Both are encrypted using the conversation key.
- */
-#define adv_timestamp	adv_time_u.adv_ctime
-#define adv_xtimestamp	adv_time_u.adv_xtime
-#define adv_winverf	adv_int_u
-
-/*
- * des authentication verifier: server variety
- *
- * adv_timeverf is the client's timestamp + client's window
- * adv_nickname is the server's nickname for the client.
- * adv_timeverf is encrypted using the conversation key.
- */
-#define adv_timeverf	adv_time_u.adv_ctime
-#define adv_xtimeverf	adv_time_u.adv_xtime
-#define adv_nickname	adv_int_u
-
-/*
- * Map a des credential into a unix cred.
- *
- */
 __BEGIN_DECLS
-extern int authdes_getucred( struct authdes_cred *, uid_t *, gid_t *, int *, gid_t * );
-__END_DECLS
-
-__BEGIN_DECLS
-extern bool_t	xdr_authdes_cred(XDR *, struct authdes_cred *);
-extern bool_t	xdr_authdes_verf(XDR *, struct authdes_verf *);
 extern int	rtime(dev_t, struct netbuf *, int, struct timeval *,
 		    struct timeval *);
-extern void	kgetnetname(char *);
-extern enum auth_stat _svcauth_des(struct svc_req *, struct rpc_msg *);
 __END_DECLS
 
 #endif /* ndef _AUTH_DES_ */
