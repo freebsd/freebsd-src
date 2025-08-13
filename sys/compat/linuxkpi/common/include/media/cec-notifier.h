bdf2e34e1541492476a830ae95ce8bad7ab61307
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2025-08-13 21:13:10 +0200

    linuxkpi: Replicate the chain of #include in the `cec*.h` headers
    The i915 DRM driver depends on this namespace pollution to access
    `debugfs_*` functions, after several explicit #include of
    <linux/debugfs.h> were removed in Linux 6.10.
    
    Reviewed by:	bz, christos
    Sponsored by:	The FreeBSD Foundation
    Differential Revision: https://reviews.freebsd.org/D54494
    
    (cherry picked from commit 10c6f1f627cd4267f9b95f60a9a30e4609566f8a)

---
+++ sys/compat/linuxkpi/common/include/media/cec-notifier.h
@@ -0,0 +1,17 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2025-2026 The FreeBSD Foundation
+ * Copyright (c) 2025-2026 Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
+ *
+ * This software was developed by Jean-Sébastien Pédron under sponsorship
+ * from the FreeBSD Foundation.
+ */
+
+#ifndef _LINUXKPI_MEDIA_CEC_NOTIFIER_H_
+#define	_LINUXKPI_MEDIA_CEC_NOTIFIER_H_
+
+#include <linux/err.h>
+#include <media/cec.h>
+
+#endif
