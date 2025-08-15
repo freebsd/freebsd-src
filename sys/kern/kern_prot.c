9a7c512a614993333beec4e2d509290b0c00dde6
Eric van Gyzen <vangyzen@FreeBSD.org>
2025-08-15 09:16:58 -0400

    ucred groups: restore a useful comment
    The most useful comments are those that explain _why_.
    Restore such an explanation dropped by commit 6d2efbb34fd.
    
    Reviewed by:		kevans
    Sponsored by:		Dell Inc.
    Differential Revision:	https://reviews.freebsd.org/D51918

--- sys/kern/kern_prot.c
+++ sys/kern/kern_prot.c
@@ -2921,7 +2921,8 @@ crextend(struct ucred *cr, int n)
  * Normalizes a set of groups to be applied to a 'struct ucred'.
  *
  * Normalization ensures that the supplementary groups are sorted in ascending
- * order and do not contain duplicates.
+ * order and do not contain duplicates.  This allows group_is_supplementary
+ * to do a binary search.
  */
 static void
 groups_normalize(int *ngrp, gid_t *groups)
