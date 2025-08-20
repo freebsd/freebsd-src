79a0959affecd2b055b9048002c0635018576103
Kristof Provost <kp@FreeBSD.org>
2025-08-20 16:28:24 +0200

    pfctl: add af-to and other missing action types in print_rule()
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

--- sbin/pfctl/pfctl_parser.c
+++ sbin/pfctl/pfctl_parser.c
@@ -844,7 +844,7 @@ print_rule(struct pfctl_rule *r, const char *anchor_call, int verbose, int numer
 {
 	static const char *actiontypes[] = { "pass", "block", "scrub",
 	    "no scrub", "nat", "no nat", "binat", "no binat", "rdr", "no rdr",
-	    "", "", "match"};
+	    "synproxy drop", "defer", "match", "af-rt", "route-to" };
 	static const char *anchortypes[] = { "anchor", "anchor", "anchor",
 	    "anchor", "nat-anchor", "nat-anchor", "binat-anchor",
 	    "binat-anchor", "rdr-anchor", "rdr-anchor" };
