f89f82e4042a24e451b5ba349119cc3446d55601
Muhammad Saheed <saheed@FreeBSD.org>
2025-08-25 21:50:46 +0200

    netlink: Fix IFF_UP flag handling in RTM_NEWLINK's modify_link handler
    IFF_UP could previously only be unset via RTM_NEWLINK. Requests to set
    IFF_UP, though they succeeded, did not actually set the flag.
    
    Reviewed by:	obiwac, kp, mckusick (mentor)
    Approved by:	obiwac, kp, mckusick (mentor)
    Sponsored by:	Google LLC (GSoC)
    Differential Revision:	https://reviews.freebsd.org/D51871

--- sys/netlink/route/iface_drivers.c
+++ sys/netlink/route/iface_drivers.c
@@ -82,9 +82,12 @@ _nl_modify_ifp_generic(struct ifnet *ifp, struct nl_parsed_link *lattrs,
 		}
 	}
 
-	if ((lattrs->ifi_change & IFF_UP) && (lattrs->ifi_flags & IFF_UP) == 0) {
-		/* Request to down the interface */
-		if_down(ifp);
+	if ((lattrs->ifi_change & IFF_UP) != 0 || lattrs->ifi_change == 0) {
+		/* Request to up or down the interface */
+		if (lattrs->ifi_flags & IFF_UP)
+			if_up(ifp);
+		else
+			if_down(ifp);
 	}
 
 	if (lattrs->ifla_mtu > 0) {
