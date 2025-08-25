dbf17ec890795af131fd9274d38a94be74b7f092
Dag-Erling Smørgrav <des@FreeBSD.org>
2025-08-25 23:41:52 +0200

    caroot: Rename script and normalize license
    MFC after:	1 week
    Reviewed by:	mandree, markj
    Differential Revision:	https://reviews.freebsd.org/D51775
    
    (cherry picked from commit 0886019bf853bd30b70aa4b8cdb059830ca6aaad)

--- secure/caroot/ca-extract.pl
+++ secure/caroot/ca-extract.pl
@@ -1,38 +1,38 @@
 #!/usr/bin/env perl
-##
-##  MAca-bundle.pl -- Regenerate ca-root-nss.crt from the Mozilla certdata.txt
-##
-##  Rewritten in September 2011 by Matthias Andree to heed untrust
-##
-
-##  Copyright (c) 2011, 2013 Matthias Andree <mandree@FreeBSD.org>
-##  All rights reserved.
-##  Copyright (c) 2018, Allan Jude <allanjude@FreeBSD.org>
-##  Copyright (c) 2025 Dag-Erling Smørgrav <des@FreeBSD.org>
-##
-##  Redistribution and use in source and binary forms, with or without
-##  modification, are permitted provided that the following conditions are
-##  met:
-##
-##  * Redistributions of source code must retain the above copyright
-##  notice, this list of conditions and the following disclaimer.
-##
-##  * Redistributions in binary form must reproduce the above copyright
-##  notice, this list of conditions and the following disclaimer in the
-##  documentation and/or other materials provided with the distribution.
-##
-##  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-##  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-##  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-##  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-##  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-##  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-##  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-##  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-##  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-##  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-##  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-##  POSSIBILITY OF SUCH DAMAGE.
+#-
+# SPDX-License-Identifier: BSD-2-Clause
+#
+#  Copyright (c) 2011, 2013 Matthias Andree <mandree@FreeBSD.org>
+#  Copyright (c) 2018 Allan Jude <allanjude@FreeBSD.org>
+#  Copyright (c) 2025 Dag-Erling Smørgrav <des@FreeBSD.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+#
+# ca-extract.pl -- Extract trusted and untrusted certificates from
+# Mozilla's certdata.txt.
+#
+# Rewritten in September 2011 by Matthias Andree to heed untrust
+#
 
 use strict;
 use warnings;
