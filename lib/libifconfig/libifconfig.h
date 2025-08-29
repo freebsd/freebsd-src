df78122f616aef977a09a8f8408ac80e95174306
Muhammad Saheed <saheed@FreeBSD.org>
2025-08-29 17:36:10 +0200

    libifconfig: Add netlink based helper to bring the interface up/down
    Adds `ifconfig_set_up()` to set and unset the `IFF_UP`
    on a network interface using `RTM_NEWLINK`.
    
    Sponsored by:	Google LLC (GSoC)
    Reviewed by:	obiwac, mckusick (mentor), kp
    Approved by:	obiwac, mckusick (mentor), kp
    Differential Revision:	https://reviews.freebsd.org/D52128

--- lib/libifconfig/libifconfig.h
+++ lib/libifconfig/libifconfig.h
@@ -35,6 +35,8 @@
 #include <netinet/ip_carp.h>
 #include <netinet6/in6_var.h>
 
+#include <stdbool.h>
+
 #define ND6_IFF_DEFAULTIF    0x8000
 
 typedef enum {
@@ -381,3 +383,12 @@ int ifconfig_set_vlantag(ifconfig_handle_t *h, const char *name,
  * 		length of *lenp * IFNAMSIZ bytes.
  */
 int ifconfig_list_cloners(ifconfig_handle_t *h, char **bufp, size_t *lenp);
+
+/** Brings the interface up/down
+ * @param h	    An open ifconfig state object
+ * @param ifname    The interface name
+ * @param up	    true to bring the interface up, false to bring it down
+ * @return	    0 on success, nonzero on failure.
+ *		    On failure, the error info on the handle is set.
+ */
+int ifconfig_set_up(ifconfig_handle_t *h, const char *ifname, bool up);
