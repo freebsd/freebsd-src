3753913c9e1c3375be29b8f4ab925b054fe1ac4b
Bjoern A. Zeeb <bz@FreeBSD.org>
2025-09-04 20:27:16 +0000

    LinuxKPI: kunit: add static_stub.h and more dummy headers
    These came up during a wifi driver update.
    
    Sponsored by:	The FreeBSD Foundation (initially)
    Reviewed by:	dumbbell
    Differential Revision:	https://reviews.freebsd.org/D52079
    
    (cherry picked from commit 7c473933624f701263a83da526cac680b1ca69ed)

---
+++ sys/compat/linuxkpi/common/include/kunit/static_stub.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025 The FreeBSD Foundation
+ *
+ * This software was developed by Björn Zeeb under sponsorship from
+ * the FreeBSD Foundation.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef	_LINUXKPI_KUNIT_STATIC_STUB_H
+#define	_LINUXKPI_KUNIT_STATIC_STUB_H
+
+#define	KUNIT_STATIC_STUB_REDIRECT(_fn, ...)	do { } while(0)
+
+#endif	/* _LINUXKPI_KUNIT_STATIC_STUB_H */
