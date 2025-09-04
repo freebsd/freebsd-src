36224ed42708e5d917698261e6dfe4f128069da8
Mark Johnston <markj@FreeBSD.org>
2025-09-04 19:34:17 +0000

    libc: Reimplement the *rand48 family of functions
    Rather than implementing the recurrence using 3 16-bit integers, as was
    done in _dorand48() before this patch, provide an equivalent
    implementation using 64-bit integers.
    
    For drand48() and erand48(), replace the use of ldexp() with
    bit-twiddling assuming IEEE 754 double-precision float layout.
    
    This implementation is significantly faster and requires less code,
    while producing identical outputs on supported platforms.
    
    While here, add a STANDARDS section to rand48.3.
    
    Obtained from:	https://github.com/apple-oss-distributions/libc
    MFC after:	3 weeks
    Sponsored by:	Klara, Inc.
    Differential Revision:	https://reviews.freebsd.org/D52429
    
    (cherry picked from commit 2ba20004ef7649db7654520e8376927c4410d9c3)

--- lib/libc/gen/seed48.c
+++ lib/libc/gen/seed48.c
@@ -13,24 +13,14 @@
 
 #include "rand48.h"
 
-extern unsigned short _rand48_seed[3];
-extern unsigned short _rand48_mult[3];
-extern unsigned short _rand48_add;
-
 unsigned short *
 seed48(unsigned short xseed[3])
 {
 	static unsigned short sseed[3];
 
-	sseed[0] = _rand48_seed[0];
-	sseed[1] = _rand48_seed[1];
-	sseed[2] = _rand48_seed[2];
-	_rand48_seed[0] = xseed[0];
-	_rand48_seed[1] = xseed[1];
-	_rand48_seed[2] = xseed[2];
-	_rand48_mult[0] = RAND48_MULT_0;
-	_rand48_mult[1] = RAND48_MULT_1;
-	_rand48_mult[2] = RAND48_MULT_2;
+	STORERAND48(_rand48_seed, sseed);
+	LOADRAND48(_rand48_seed, xseed);
+	_rand48_mult = RAND48_MULT;
 	_rand48_add = RAND48_ADD;
-	return sseed;
+	return (sseed);
 }
