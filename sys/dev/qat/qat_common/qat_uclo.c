88c0ce38bc18acaf50188eda402384a0cb93890d
Hareshx Sankar Raj <hareshx.sankar.raj@intel.com>
2025-09-04 14:21:36 -0400

    qat: enable qat driver for 402xx device
    Enabled qat driver support for 402xx device with
    ID 0x4944/0x4945. This is an additional device supported
    under the existing qat_4xxx driver. Firmwares specific to
    the 402xx device are added.
    
    Signed-off-by: Hareshx Sankar Raj <hareshx.sankar.raj@intel.com>
    
    Relnotes:	yes
    Sponsored by:	Intel Corporation
    MFC after:	1 month
    Differential Revision:	https://reviews.freebsd.org/D52488
    
    (cherry picked from commit 7fbd362c091b2b384d14c3ed5af9234bb2eff9c2)

--- sys/dev/qat/qat_common/qat_uclo.c
+++ sys/dev/qat/qat_common/qat_uclo.c
@@ -892,6 +892,7 @@ qat_uclo_get_dev_type(struct icp_qat_fw_loader_handle *handle)
 		return ICP_QAT_AC_C4XXX_DEV_TYPE;
 	case ADF_4XXX_PCI_DEVICE_ID:
 	case ADF_401XX_PCI_DEVICE_ID:
+	case ADF_402XX_PCI_DEVICE_ID:
 		return ICP_QAT_AC_4XXX_A_DEV_TYPE;
 	default:
 		pr_err("QAT: unsupported device 0x%x\n",
