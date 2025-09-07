ac3444a9528eca7beb8a6856e225951a2db46cbf
Konstantin Belousov <kib@FreeBSD.org>
2025-09-07 13:59:45 +0300

    kern: replace several EBADF with EINVAL
    (cherry picked from commit fd9e09cb2ab07993e8dc783c802f273329e70bb8)

--- sys/kern/kern_event.c
+++ sys/kern/kern_event.c
@@ -1864,7 +1864,7 @@ kqueue_acquire(struct file *fp, struct kqueue **kqp)
 
 	kq = fp->f_data;
 	if (fp->f_type != DTYPE_KQUEUE || kq == NULL)
-		return (EBADF);
+		return (EINVAL);
 	*kqp = kq;
 	KQ_LOCK(kq);
 	if ((kq->kq_state & KQ_CLOSING) == KQ_CLOSING) {
