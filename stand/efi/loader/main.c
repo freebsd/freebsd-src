fa4f76eecbf21b440a0afaf3360065c619b95258
Ahmad Khalifa <vexeduxr@FreeBSD.org>
2025-09-09 20:19:24 +0300

    stand: remove unused variables
    (cherry picked from commit 7d48a56205224cfa969c32b47496dc8ac6a6a637)

--- stand/efi/loader/main.c
+++ stand/efi/loader/main.c
@@ -990,7 +990,7 @@ main(int argc, CHAR16 *argv[])
 	EFI_DEVICE_PATH *imgpath;
 	CHAR16 *text;
 	EFI_STATUS rv;
-	size_t sz, bosz = 0, bisz = 0;
+	size_t sz, bisz = 0;
 	UINT16 boot_order[100];
 	char boot_info[4096];
 	char buf[32];
@@ -1196,14 +1196,12 @@ main(int argc, CHAR16 *argv[])
 				    boot_order[i] == boot_current ? "[*]" : "");
 			printf("\n");
 			is_last = boot_order[(sz / sizeof(boot_order[0])) - 1] == boot_current;
-			bosz = sz;
 		} else if (uefi_boot_mgr) {
 			/*
 			 * u-boot doesn't set BootOrder, but otherwise participates in the
 			 * boot manager protocol. So we fake it here and don't consider it
 			 * a failure.
 			 */
-			bosz = sizeof(boot_order[0]);
 			boot_order[0] = boot_current;
 			is_last = true;
 		}
