2b84c4c39148001b1f2455b0b047212d9e54d833
Mark Johnston <markj@FreeBSD.org>
2025-09-10 14:33:58 +0000

    dtrace: Use a size_t to represent a buffer size in the printm action
    printm is specific to the FreeBSD dtrace port.  I believe it's
    effectively the same as tracemem(), though printm apparently predates
    it.  It stores the size of the buffer of traced data inline.  Currently
    it represents that size using a uintptr_t, which isn't really right and
    poses challenges when porting to CHERI because
    `DTRACE_STORE(uintptr_t, ...`  requires the destination to be suitably
    aligned, but this isn't necessary since we're just storing a size.
    
    Convert to using a size_t.  This should be a no-op since
    sizeof(uintptr_t) == sizeof(size_t) on non-CHERI platforms (and besides
    that I don't see a reason to use printm() when tracemem() is available
    and is simpler to use.)
    
    Reviewed by:	Domagoj Stolfa, avg
    MFC after:	2 weeks
    Sponsored by:	Innovate UK
    Differential Revision:	https://reviews.freebsd.org/D52055
    
    (cherry picked from commit 3877025f52ee205fe99ad4ff68229933d57e4bcb)

--- sys/cddl/contrib/opensolaris/uts/common/dtrace/dtrace.c
+++ sys/cddl/contrib/opensolaris/uts/common/dtrace/dtrace.c
@@ -7761,7 +7761,8 @@ dtrace_probe(dtrace_id_t id, uintptr_t arg0, uintptr_t arg1,
 				uintptr_t *memref = (uintptr_t *)(uintptr_t) val;
 
 				if (!DTRACE_INSCRATCHPTR(&mstate,
-				    (uintptr_t)memref, 2 * sizeof(uintptr_t))) {
+				    (uintptr_t) memref,
+				    sizeof (uintptr_t) + sizeof (size_t))) {
 					*flags |= CPU_DTRACE_BADADDR;
 					continue;
 				}
@@ -7773,21 +7774,21 @@ dtrace_probe(dtrace_id_t id, uintptr_t arg0, uintptr_t arg1,
 				 * Check if the size exceeds the allocated
 				 * buffer size.
 				 */
-				if (size + sizeof(uintptr_t) > dp->dtdo_rtype.dtdt_size) {
+				if (size + sizeof (size_t) >
+				    dp->dtdo_rtype.dtdt_size) {
 					/* Flag a drop! */
 					*flags |= CPU_DTRACE_DROP;
 					continue;
 				}
 
 				/* Store the size in the buffer first. */
-				DTRACE_STORE(uintptr_t, tomax,
-				    valoffs, size);
+				DTRACE_STORE(size_t, tomax, valoffs, size);
 
 				/*
 				 * Offset the buffer address to the start
 				 * of the data.
 				 */
-				valoffs += sizeof(uintptr_t);
+				valoffs += sizeof(size_t);
 
 				/*
 				 * Reset to the memory address rather than
