66f36c3686762d9a6e48b0453fc7221be36a0eb8
Lexi Winter <ivy@FreeBSD.org>
2025-09-12 21:59:25 +0100

    packages: Add meta-package sets
    Add several metapackages which contain no files themselves, but depend
    on other packages.  This allows the user to install a system by using
    a set like "FreeBSD-set-minimal" instead of having to know which
    specific packages are required, and it means if we add more package
    in the future, existing installations will get them automatically as
    long as they were installed using a set.
    
    The defined sets are significantly less granular than the actual
    packages; the assumption is that users who want a very specific set of
    packages installed can do that manually, while sets are intended more
    for typical users who just want to install FreeBSD.
    
    The following sets are defined:
    
    * minimal: the set of packages required to boot the system and bring
      up a multi-user UNIX system.  This includes hardware support,
      networking (wireless, DHCP), basic functionality like syslogd, cron
      and periodic.  It does not include a kernel, because the kernels are
      quite large and the user might want to use a custom kernel.
    
      sendmail and DMA are not included in minimal.  Since we provide two
      MTAs, the user should select which one they want, or install one from
      ports.  sshd is not included because it's not required for the base
      system, and the user might want to install it from ports.
    
      Some other significant subsystems (e.g. NFS, Kerberos) are also not
      included.
    
      The minimal set does not include any libraries itself, since we rely
      on dependencies to pull these in.  This implies it doesn't include
      library manpages, which is fine, since users won't want manpages for
      libraries unless they've also installed the development packages.
    
    * devel: the compiler toolchain for building software, along with all
      "-dev" packages which include header files and static libraries.
    
    * lib32: 32-bit compatibility packages.  This includes both runtime
      and development packages.
    
    * kernels: the kernel packages.  For freebsd.org builds, this will be
      GENERIC, GENERIC-DEBUG, etc., for custom builds it will be whatever
      was set in $KERNCONF.
    
    * base: everything else; this includes the entire base system.
    
    For each set, also generate a <setname>-dbg set containing the debug
    packages for the set.
    
    The sets are built along with the rest of the packages using a new
    script called create-sets.sh, which examines the "set" annotation in
    each package and puts it in the appropriate set.  This is in
    anticipation of the later "groups" feature appearing in pkg itself, at
    which point we can simply replace the set annotation with the group.
    
    MFC after:	3 days
    Reviewed by:	imp, bapt
    Differential Revision:	https://reviews.freebsd.org/D52412

---
+++ release/packages/generate-set-ucl.lua
@@ -0,0 +1,96 @@
+#!/usr/libexec/flua
+
+--[[ usage:
+generare-set-ucl.lua <template> [<variablename> <variablevalue>]
+
+Generate the UCL for a set metapackage.  The variables provided will be
+substituted as UCL variables.
+]]--
+
+local ucl = require("ucl")
+
+-- This parser is the output UCL we want to build.
+local parser = ucl.parser()
+
+if #arg < 1 then
+	io.stderr:write(arg[0] .. ": missing template filename\n")
+	os.exit(1)
+end
+
+local template = table.remove(arg, 1)
+
+-- Set any $VARIABLES from the command line in the parser.  This causes ucl to
+-- automatically replace them when we load the source ucl.
+if #arg % 2 ~= 0 then
+	io.stderr:write(arg[0] .. ": expected an even number of arguments, "
+	    .. "got " .. #arg .. "\n")
+	os.exit(1)
+end
+
+local pkgprefix = nil
+local pkgversion = nil
+local pkgdeps = ""
+
+for i = 2, #arg, 2 do
+	local varname = arg[i - 1]
+	local varvalue = arg[i]
+
+	if varname == "PKG_NAME_PREFIX" and #varvalue > 0 then
+		pkgprefix = varvalue
+	elseif varname == "VERSION" and #varvalue > 0 then
+		pkgversion = varvalue
+	elseif varname == "SET_DEPENDS" and #varvalue > 0 then
+		pkgdeps = varvalue
+	end
+
+	parser:register_variable(varname, varvalue)
+end
+
+-- Load the source template.
+local res,err = parser:parse_file(template)
+if not res then
+	io.stderr:write(arg[0] .. ": fail to parse(" .. template .. "): "
+	    .. err .. "\n")
+	os.exit(1)
+end
+
+local obj = parser:get_object()
+
+-- Dependency handling.
+obj["deps"] = obj["deps"] or {}
+
+-- If PKG_NAME_PREFIX is provided, rewrite the names of dependency packages.
+-- We can't do this in UCL since variable substitution doesn't work in array
+-- keys.  Note that this only applies to dependencies from the set UCL files,
+-- because SET_DEPENDS already have the correct prefix.
+if pkgprefix ~= nil then
+	newdeps = {}
+	for dep, opts in pairs(obj["deps"]) do
+		local newdep = pkgprefix .. "-" .. dep
+		newdeps[newdep] = opts
+	end
+	obj["deps"] = newdeps
+end
+
+-- Add dependencies from SET_DEPENDS.
+for dep in string.gmatch(pkgdeps, "[^%s]+") do
+	obj["deps"][dep] = {
+		["origin"] = "base"
+	}
+end
+
+-- Add a version key to all dependencies, otherwise pkg doesn't like it.
+for dep, opts in pairs(obj["deps"]) do
+	if obj["deps"][dep]["version"] == nil then
+		obj["deps"][dep]["version"] = pkgversion
+	end
+end
+
+-- If there are no dependencies, remove the deps key, otherwise pkg raises an
+-- error trying to read the manifest.
+if next(obj["deps"]) == nil then
+	obj["deps"] = nil
+end
+
+-- Write the output.
+io.stdout:write(ucl.to_format(obj, 'ucl', true))
