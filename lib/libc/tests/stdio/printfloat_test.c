freebsd/freebsd-src

commit 76303a9735ee760da4d7a15177d8595c656b9c7b

Make several changes to the way printf handles hex floating point (%a):
1. Previously, printing the number 1.0 could produce 0x1p+0, 0x2p-1,
   0x4p-2, or 0x8p-3, depending on what happened to be convenient. This
   meant that printing a value as a double and printing the same value
   as a long double could produce different (but equivalent) results.
   The change is to always make the leading digit a 1, unless the
   number is 0. This solves the aforementioned problem and has
   several other advantages.

2. Use the FPU to do rounding. This is far simpler and more portable
   than manipulating the bits, and it fixes an obsure round-to-even
   bug. It also raises the exceptions now required by IEEE 754R.
   The drawbacks are that it is usually slightly slower, and it makes
   printf less effective as a debugging tool when the FPU is hosed
   (e.g., due to a buggy softfloat implementation).

3. On i386, twiddle the rounding precision so that (2) works properly
   for long doubles.

4. Make several simplifications that are now possible due to (2).

5. Split __hldtoa() into a separate file.

Thanks to remko for access to a sparc64 box for testing.

lib/libc/gdtoa/_hldtoa.c
@@ -30,64 +30,79 @@ __FBSDID("$FreeBSD$");
 #include <float.h>
 #include <limits.h>
 #include <math.h>
+#include <stdint.h>
+
+#ifdef __i386__
+#include <ieeefp.h>
+#endif
+
 #include "fpmath.h"
 #include "gdtoaimp.h"
+
+#if (LDBL_MANT_DIG > DBL_MANT_DIG)
 
 /* Strings values used by dtoa() */
 #define	INFSTR	"Infinity"
 #define	NANSTR	"NaN"
 
-#define	DBL_ADJ	(DBL_MAX_EXP - 2)
-#define	SIGFIGS	((DBL_MANT_DIG + 3) / 4 + 1)
+#ifdef LDBL_IMPLICIT_NBIT
+#define	MANH_SIZE	LDBL_MANH_SIZE
+#else
+#define	MANH_SIZE	(LDBL_MANH_SIZE - 1)
+#endif
+
+#if MANH_SIZE > 32
+typedef uint64_t manh_t;
+#else
+typedef uint32_t manh_t;
+#endif
+
+#if LDBL_MANL_SIZE > 32
+typedef uint64_t manl_t;
+#else
+typedef uint32_t manl_t;
+#endif
+
+#define	LDBL_ADJ	(LDBL_MAX_EXP - 2)
+#define	SIGFIGS		((LDBL_MANT_DIG + 3) / 4 + 1)
 
 static const float one[] = { 1.0f, -1.0f };
 
 /*
- * This procedure converts a double-precision number in IEEE format
- * into a string of hexadecimal digits and an exponent of 2.  Its
- * behavior is bug-for-bug compatible with dtoa() in mode 2, with the
- * following exceptions:
- *
- * - An ndigits < 0 causes it to use as many digits as necessary to
- *   represent the number exactly.
- * - The additional xdigs argument should point to either the string
- *   "0123456789ABCDEF" or the string "0123456789abcdef", depending on
- *   which case is desired.
- * - This routine does not repeat dtoa's mistake of setting decpt
- *   to 9999 in the case of an infinity or NaN.  INT_MAX is used
- *   for this purpose instead.
- *
- * Note that the C99 standard does not specify what the leading digit
- * should be for non-zero numbers.  For instance, 0x1.3p3 is the same
- * as 0x2.6p2 is the same as 0x4.cp3.  This implementation always makes
- * the leading digit a 1. This ensures that the exponent printed is the
- * actual base-2 exponent, i.e., ilogb(d).
- *
- * Inputs:	d, xdigs, ndigits
- * Outputs:	decpt, sign, rve
+ * This is the long double version of __hdtoa().
  */
 char *
-__hdtoa(double d, const char *xdigs, int ndigits, int *decpt, int *sign,
+__hldtoa(long double e, const char *xdigs, int ndigits, int *decpt, int *sign,
     char **rve)
 {
-	union IEEEd2bits u;
+	union IEEEl2bits u;
 	char *s, *s0;
+	manh_t manh;
+	manl_t manl;
 	int bufsize;
-	uint32_t manh, manl;
+#ifdef __i386__
+	fp_prec_t oldprec;
+#endif
 
-	u.d = d;
+	u.e = e;
 	*sign = u.bits.sign;
 
-	switch (fpclassify(d)) {
+	switch (fpclassify(e)) {
 	case FP_NORMAL:
-		*decpt = u.bits.exp - DBL_ADJ;
+		*decpt = u.bits.exp - LDBL_ADJ;
 		break;
 	case FP_ZERO:
 		*decpt = 1;
 		return (nrv_alloc("0", rve, 1));
 	case FP_SUBNORMAL:
-		u.d *= 0x1p514;
-		*decpt = u.bits.exp - (514 + DBL_ADJ);
+#ifdef __i386__
+		oldprec = fpsetprec(FP_PE);
+#endif
+		u.e *= 0x1p514L;
+		*decpt = u.bits.exp - (514 + LDBL_ADJ);
+#ifdef __i386__
+		fpsetprec(oldprec);
+#endif
 		break;
 	case FP_INFINITE:
 		*decpt = INT_MAX;
@@ -112,19 +127,26 @@ __hdtoa(double d, const char *xdigs, int ndigits, int *decpt, int *sign,
 	/* Round to the desired number of digits. */
 	if (SIGFIGS > ndigits && ndigits > 0) {
 		float redux = one[u.bits.sign];
-		int offset = 4 * ndigits + DBL_MAX_EXP - 4 - DBL_MANT_DIG;
+		int offset = 4 * ndigits + LDBL_MAX_EXP - 4 - LDBL_MANT_DIG;
+#ifdef __i386__
+		oldprec = fpsetprec(FP_PE);
+#endif
 		u.bits.exp = offset;
-		u.d += redux;
-		u.d -= redux;
+		u.e += redux;
+		u.e -= redux;
 		*decpt += u.bits.exp - offset;
+#ifdef __i386__
+		fpsetprec(oldprec);
+#endif
 	}
 
+	mask_nbit_l(u);
 	manh = u.bits.manh;
-	manl = u.bits.manl;
+	manl = u.bits.manl;	
 	*s0 = '1';
 	for (s = s0 + 1; s < s0 + bufsize; s++) {
-		*s = xdigs[(manh >> (DBL_MANH_SIZE - 4)) & 0xf];
-		manh = (manh << 4) | (manl >> (DBL_MANL_SIZE - 4));
+		*s = xdigs[(manh >> (MANH_SIZE - 4)) & 0xf];
+		manh = (manh << 4) | (manl >> (LDBL_MANL_SIZE - 4));
 		manl <<= 4;
 	}
 
@@ -140,3 +162,15 @@ __hdtoa(double d, const char *xdigs, int ndigits, int *decpt, int *sign,
 		*rve = s;
 	return (s0);
 }
+
+#else	/* (LDBL_MANT_DIG == DBL_MANT_DIG) */
+
+char *
+__hldtoa(long double e, const char *xdigs, int ndigits, int *decpt, int *sign,
+    char **rve)
+{
+
+	return (__hdtoa((double)e, xdigs, ndigits, decpt, sign, rve));
+}
+
+#endif	/* (LDBL_MANT_DIG == DBL_MANT_DIG) */

lib/libc/gdtoa/Makefile.inc
@@ -3,7 +3,7 @@
 # netlib gdtoa sources
 .PATH: ${.CURDIR}/gdtoa
 
-MISRCS+=_hdtoa.c _ldtoa.c glue.c
+MISRCS+=_hdtoa.c _hldtoa.c _ldtoa.c glue.c
 GDTOASRCS+=dmisc.c dtoa.c gdtoa.c gethex.c gmisc.c \
 	hd_init.c hexnan.c misc.c smisc.c \
 	strtod.c strtodg.c strtof.c strtord.c sum.c ulp.c

commit 9dd78db9c30a220ac3e8e65d89548ff99c14dd90

libc: prevent incorrect %a/%La rounding at full precision
In __hdtoa() and __hldtoa(), rounding is incorrectly applied when
the requested precision exactly matches the number of significant
hexadecimal digits. In this case, the redux adjustment can trigger
an unintended exponent increment and shift the rounding position
left by one bit. This causes the least significant digit to be
rounded incorrectly.

The fix adds a new condition based on MAX_HEX_DIGITS (derived from
MANT_DIG) so that rounding is performed only when precision is
strictly less than the number of significant digits. This avoids
the unintended shift while preserving correct rounding for other
cases.

A new regression test
(printfloat_test:hexadecimal_rounding_offset_eq_exp) covers both
the binary64 (%.13a) and binary128 (%.28La on arm64) cases that
previously fail, ensuring the bug does not regress.

Note: MAX_HEX_DIGITS represents the maximum number of hexadecimal
digits needed to express the mantissa. It is computed by subtracting
the implicit integer bit from [L]DBL_MANT_DIG, dividing the remaining
mantissa bits by 4 (with +3 to round up any remainder), and finally
adding +1 for the leading integer digit. This makes its meaning
explicit and distinct from SIGFIGS, which serves a different purpose.

Fixes: 76303a9735ee ("Make several changes to the way printf handles hex floating point (%a):")
Signed-off-by: Osamu Sho <osamusho@gmail.com>
Reviewed by: imp,jlduran
Pull Request: https://github.com/freebsd/freebsd-src/pull/1837

lib/libc/gdtoa/_hdtoa.c
@@ -40,6 +40,7 @@
 
 #define	DBL_ADJ	(DBL_MAX_EXP - 2)
 #define	SIGFIGS	((DBL_MANT_DIG + 3) / 4 + 1)
+#define	MAX_HEX_DIGITS	((DBL_MANT_DIG + 3 - 1) / 4 + 1)
 
 static const float one[] = { 1.0f, -1.0f };
 
@@ -111,7 +112,7 @@ __hdtoa(double d, const char *xdigs, int ndigits, int *decpt, int *sign,
 	s0 = rv_alloc(bufsize);
 
 	/* Round to the desired number of digits. */
-	if (SIGFIGS > ndigits && ndigits > 0) {
+	if (MAX_HEX_DIGITS > ndigits && ndigits > 0) {
 		float redux = one[u.bits.sign];
 		int offset = 4 * ndigits + DBL_MAX_EXP - 4 - DBL_MANT_DIG;
 		u.bits.exp = offset;

lib/libc/gdtoa/_hldtoa.c
@@ -65,6 +65,7 @@ typedef uint32_t manl_t;
 
 #define	LDBL_ADJ	(LDBL_MAX_EXP - 2)
 #define	SIGFIGS		((LDBL_MANT_DIG + 3) / 4 + 1)
+#define	MAX_HEX_DIGITS	((LDBL_MANT_DIG + 3 - 1) / 4 + 1)
 
 static const float one[] = { 1.0f, -1.0f };
 
@@ -125,7 +126,7 @@ __hldtoa(long double e, const char *xdigs, int ndigits, int *decpt, int *sign,
 	s0 = rv_alloc(bufsize);
 
 	/* Round to the desired number of digits. */
-	if (SIGFIGS > ndigits && ndigits > 0) {
+	if (MAX_HEX_DIGITS > ndigits && ndigits > 0) {
 		float redux = one[u.bits.sign];
 		int offset = 4 * ndigits + LDBL_MAX_EXP - 4 - LDBL_MANT_DIG;
 #ifdef __i386__

lib/libc/tests/stdio/printfloat_test.c
@@ -398,6 +398,18 @@ ATF_TC_BODY(subnormal_float, tc)
 	testfmt("-0X1P-149", "%A", negative);
 }
 
+ATF_TC_WITHOUT_HEAD(hexadecimal_rounding_fullprec);
+ATF_TC_BODY(hexadecimal_rounding_fullprec, tc)
+{
+	/* Double: %.13a with binary64 mantissa=53 */
+	testfmt("0x1.1234567890bbbp+0", "%.13a", 0x1.1234567890bbbp + 0);
+
+#if defined(__aarch64__)
+	/* On arm64, long double is IEEE binary128 (mantissa=113) */
+	testfmt("0x1.3c0ca428c59fbbbbbbbbbbbbbbbbp+0", "%.28La", 0x1.3c0ca428c59fbbbbbbbbbbbbbbbbp + 0L);
+#endif
+}
+
 ATF_TP_ADD_TCS(tp)
 {
 
@@ -414,6 +426,7 @@ ATF_TP_ADD_TCS(tp)
 	ATF_TP_ADD_TC(tp, hexadecimal_rounding);
 	ATF_TP_ADD_TC(tp, subnormal_double);
 	ATF_TP_ADD_TC(tp, subnormal_float);
+	ATF_TP_ADD_TC(tp, hexadecimal_rounding_fullprec);
 
 	return (atf_no_error());
 }