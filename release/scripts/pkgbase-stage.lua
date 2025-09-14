340e68fd7c0d62cac1f0876b6475a89df5065db5
Colin Percival <cperciva@FreeBSD.org>
2025-09-13 19:01:26 -0700

    release: Pass ABI to pkgbase-stage.lua
    We then pass ABI from pkgbase-stage.lua to pkg(8); without this,
    cross-building releases with PKGBASE enabled fails with
    
    pkg: wrong architecture: ...
    pkg: repository FreeBSD-base contains packages with wrong ABI: ...
    
    MFC after:	1 minute
    Discussed with:	emaste, jrtc27
    Sponsored by:	https://www.patreon.com/c/cperciva

--- release/scripts/pkgbase-stage.lua
+++ release/scripts/pkgbase-stage.lua
@@ -115,6 +115,8 @@ local function main()
 	local target = assert(arg[3])
 	-- =hitespace separated list of all libcompat names (e.g. "32")
 	local all_libcompats = assert(arg[4])
+	-- ABI of repository
+	local ABI = assert(arg[5])
 
 	assert(os.execute("mkdir -p pkgbase-repo-conf"))
 	local f <close> = assert(io.open("pkgbase-repo-conf/FreeBSD-base.conf", "w"))
@@ -127,6 +129,7 @@ local function main()
 	assert(f:close())
 
 	local pkg = "pkg -o ASSUME_ALWAYS_YES=yes -o IGNORE_OSVERSION=yes " ..
+	    "-o ABI=" .. ABI .. " " ..
 	    "-o INSTALL_AS_USER=1 -o PKG_DBDIR=./pkgdb -R ./pkgbase-repo-conf "
 
 	assert(os.execute(pkg .. "update"))
