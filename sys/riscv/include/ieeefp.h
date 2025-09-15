e4dd763b24a1fb9f97d1fd47100b08558c15bb57
Alex Richardson <arichardson@FreeBSD.org>
2025-09-15 15:07:32 -0700

    riscv: Provide prototypes for fpgetmask/fpsetmask
    These functions are stubs that do nothing but are called by some software
    and not providing them results in implicit function declaration errors.
    This was missed in D25740.
    
    Reviewed by:	#riscv, mhorne
    MFC after:	1 week
    Differential Revision: https://reviews.freebsd.org/D52035

--- sys/riscv/include/ieeefp.h
+++ sys/riscv/include/ieeefp.h
@@ -5,4 +5,9 @@
 /* TODO */
 typedef int fp_except_t;
 
+__BEGIN_DECLS
+extern fp_except_t fpgetmask(void);
+extern fp_except_t fpsetmask(fp_except_t);
+__END_DECLS
+
 #endif /* _MACHINE_IEEEFP_H_ */
