e1401a283d101acd94d14d769a0a2d80c34a65dc
Dag-Erling Smørgrav <des@FreeBSD.org>
2025-09-15 19:56:33 +0200

    ifconfig: Enter jail as soon as possible
    Some options (in particular, -g) are processed immediately upon being
    parsed.  This will produce the wrong result in combination with -j since
    we only attach to the jail after we're done parsing arguments.  Solve
    this by attaching to the jail immediately when -j is encountered.  The
    downside is that e.g. `ifconfig -j foo -j bar` would previously attach
    to jail “bar”, whereas now it will attempt to attach to jail “foo”, and
    if successful, attempt to attach to jail “bar” within jail “foo”.  This
    may be considered a feature.
    
    PR:		289134
    MFC after:	1 week
    Reviewed by:	zlei
    Differential Revision:	https://reviews.freebsd.org/D52501
    
    (cherry picked from commit 18fd1443d205aed6be22966125a4820f77571948)

--- sbin/ifconfig/ifconfig.c
+++ sbin/ifconfig/ifconfig.c
@@ -472,6 +472,9 @@ args_parse(struct ifconfig_args *args, int argc, char *argv[])
 {
 	char options[1024];
 	struct option *p;
+#ifdef JAIL
+	int jid;
+#endif
 	int c;
 
 	/* Parse leading line options */
@@ -503,7 +506,11 @@ args_parse(struct ifconfig_args *args, int argc, char *argv[])
 #ifdef JAIL
 			if (optarg == NULL)
 				usage();
-			args->jail_name = optarg;
+			jid = jail_getid(optarg);
+			if (jid == -1)
+				Perror("jail not found");
+			if (jail_attach(jid) != 0)
+				Perror("cannot attach to jail");
 #else
 			Perror("not built with jail support");
 #endif
@@ -620,9 +627,6 @@ main(int ac, char *av[])
 {
 	char *envformat;
 	int flags;
-#ifdef JAIL
-	int jid;
-#endif
 	struct ifconfig_args _args = {};
 	struct ifconfig_args *args = &_args;
 
@@ -646,16 +650,6 @@ main(int ac, char *av[])
 	atexit(printifnamemaybe);
 
 	args_parse(args, ac, av);
-
-#ifdef JAIL
-	if (args->jail_name) {
-		jid = jail_getid(args->jail_name);
-		if (jid == -1)
-			Perror("jail not found");
-		if (jail_attach(jid) != 0)
-			Perror("cannot attach to jail");
-	}
-#endif
 
 	if (!args->all && !args->namesonly) {
 		/* not listing, need an argument */
