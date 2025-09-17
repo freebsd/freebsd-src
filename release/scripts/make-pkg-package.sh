b04c6eb37e151fb2dec08fb31c34bb6626c25fb8
Colin Percival <cperciva@FreeBSD.org>
2025-09-17 15:34:25 -0700

    Include a pkg package in the pkgbase repo
    It is essential that users be able to install the FreeBSD base system
    from release media and have all the bits needed to update the FreeBSD
    base system without touching the ports tree or pkg.freebsd.org.  To
    that end, resurrect (and heavily rewrite) the make-pkg-package.sh
    script and hook it into the create-packages target; if /usr/ports
    exists when building pkgbase packages, we'll also (cross)build pkg
    and include it in the repository.
    
    Scripting for actually installing this package as part of the FreeBSD
    installation process to come later, but I wanted this in the tree in
    time to test it in this week's snapshots.
    
    MFC after:	1 day
    Sponsored by:	https://www.patreon.com/cperciva

--- release/scripts/make-pkg-package.sh
+++ release/scripts/make-pkg-package.sh
@@ -1,26 +1,22 @@
 #!/bin/sh
-#
-#
 
-# Simulate the build environment.
-eval $(make -C ${SRCDIR} TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH} buildenvvars)
+# Simulate the build environment.  Note that we need to unset some variables
+# which are set in the src tree since they have different (unwanted) effects
+# in the ports tree.
+SRC_PKG_VERSION=${PKG_VERSION}
+PKG_ABI=$(${PKG_CMD} -o ABI_FILE=${WSTAGEDIR}/bin/sh config ABI)
+unset PKG_VERSION
+unset MAKEFLAGS
+unset PKGBASE
 export WRKDIRPREFIX=/tmp/ports.${TARGET}
-export WSTAGEDIR=${WSTAGEDIR}
-export REPODIR=${REPODIR}
-export PKG_CMD=${PKG_CMD}
-export PKG_VERSION=${PKG_VERSION}
-export OSVERSION=${OSVERSION}
 export WRKDIR=$(make -C ${PORTSDIR}/ports-mgmt/pkg -V WRKDIR)
 
 make -C ${PORTSDIR}/ports-mgmt/pkg TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH} \
 	CONFIGURE_ARGS="--host=$(uname -m)-portbld-freebsd${REVISION}" \
-	stage create-manifest
+	BATCH=YES stage create-manifest
 
-${PKG_CMD} -o ABI_FILE=${WSTAGEDIR}/bin/sh \
-	create -v -m ${WRKDIR}/.metadir/ \
+${PKG_CMD} -o ABI=${PKG_ABI} \
+	create -v -m ${WRKDIR}/.metadir.pkg/ \
 	-r ${WRKDIR}/stage \
 	-p ${WRKDIR}/.PLIST.mktmp \
-	-o ${REPODIR}/$(${PKG_CMD} -o ABI_FILE=${WSTAGEDIR}/bin/sh config ABI)/${PKG_VERSION}
-mkdir -p ${REPODIR}/$(${PKG_CMD} -o ABI_FILE=${WSTAGEDIR}/bin/sh config ABI)/${PKG_VERSION}/Latest/
-cd ${REPODIR}/$(${PKG_CMD} -o ABI_FILE=${WSTAGEDIR}/bin/sh config ABI)/${PKG_VERSION}/Latest/ && \
-	ln -s ../pkg-*.txz
+	-o ${REPODIR}/${PKG_ABI}/${SRC_PKG_VERSION}
