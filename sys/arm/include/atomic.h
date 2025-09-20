1cf34a9ead7b3851369fe5fbb62663acb7b2aeba
Konstantin Belousov <kib@FreeBSD.org>
2025-09-20 22:10:02 +0300

    arm/atomic.h: remove misleading comment
    (cherry picked from commit 1c0465a3d32e59cd260067e87469abd013571470)

--- sys/arm/include/atomic.h
+++ sys/arm/include/atomic.h
@@ -1109,11 +1109,6 @@ atomic_thread_fence_seq_cst(void)
 #define atomic_store_rel_int		atomic_store_rel_32
 #define atomic_swap_int			atomic_swap_32
 
-/*
- * For:
- *  - atomic_load_acq_8
- *  - atomic_load_acq_16
- */
 #include <sys/_atomic_subword.h>
 
 #define	atomic_set_short		atomic_set_16
