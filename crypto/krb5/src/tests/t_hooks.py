3e13aae0d1f1a28352d52e41d6ef8f02efd9150e
Cy Schubert <cy@FreeBSD.org>
2025-09-22 08:06:41 -0700

    krb5: Remove tests
    Tests contains a file that contains spaces in it. This causes the
    freebsd-update build scripts some issues. As we don't need the tests
    directory let's simply remove it.
    
    MFC after:	3 days or as determined by re@
    MFC to:		stable/15

--- crypto/krb5/src/tests/t_hooks.py
+++
@@ -1,8 +0,0 @@
-from k5test import *
-
-# Test that KDC send and recv hooks work correctly.
-realm = K5Realm(create_host=False, get_creds=False)
-realm.run(['./hooks', realm.user_princ, password('user')])
-realm.stop()
-
-success('send and recv hook tests')
