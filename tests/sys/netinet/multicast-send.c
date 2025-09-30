6d780af1cdc907f41e723332c30f3e318e4cfd95
Gleb Smirnoff <glebius@FreeBSD.org>
2025-09-29 19:34:13 -0700

    tests/netinet: rename and extend sendto-IP_MULTICAST_IF.c
    Rename this test helper app to multicast-send.c.  Extend it to send
    from/to arbitrary addresses and arbitrary payload so that it can be used
    in more test scenarios.

--- tests/sys/netinet/multicast-send.c
+++ tests/sys/netinet/multicast-send.c
@@ -28,35 +28,69 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <net/if.h>
 #include <assert.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 #include <err.h>
+
+static in_port_t
+atop(const char *c)
+{
+	unsigned long ul;
+
+	errno = 0;
+	if ((ul = strtol(c, NULL, 10)) < 1 || ul > IPPORT_MAX || errno != 0)
+		err(1, "can't parse %s", c);
+
+	return ((in_port_t)ul);
+}
 
 int
 main(int argc, char *argv[])
 {
-	struct sockaddr_in sin = {
+	struct sockaddr_in src = {
+		.sin_family = AF_INET,
+		.sin_len = sizeof(struct sockaddr_in),
+	}, dst = {
 		.sin_family = AF_INET,
 		.sin_len = sizeof(struct sockaddr_in),
 	};
+	struct ip_mreqn mreqn;
 	struct in_addr in;
-	int s, rv;
+	int s;
+	bool index;
 
-	if (argc < 2)
-		errx(1, "Usage: %s IPv4-address", argv[0]);
+	if (argc < 7)
+		errx(1, "Usage: %s src-IPv4 src-port dst-IPv4 dst-port "
+		    "interface payload", argv[0]);
 
-	if (inet_pton(AF_INET, argv[1], &in) != 1)
+	if (inet_pton(AF_INET, argv[1], &src.sin_addr) != 1)
 		err(1, "inet_pton(%s) failed", argv[1]);
+	src.sin_port = htons(atop(argv[2]));
+	if (inet_pton(AF_INET, argv[3], &dst.sin_addr) != 1)
+		err(1, "inet_pton(%s) failed", argv[3]);
+	dst.sin_port = htons(atop(argv[4]));
+	if (inet_pton(AF_INET, argv[5], &in) == 1)
+		index = false;
+	else if ((mreqn.imr_ifindex = if_nametoindex(argv[5])) > 0)
+		index = true;
+	else
+		err(1, "if_nametoindex(%s) failed", argv[5]);
 
 	assert((s = socket(PF_INET, SOCK_DGRAM, 0)) > 0);
-	assert(bind(s, (struct sockaddr *)&sin, sizeof(sin)) == 0);
-	assert(setsockopt(s, IPPROTO_IP, IP_MULTICAST_IF, &in, sizeof(in))
-	  == 0);
-	/* RFC 6676 */
-	assert(inet_pton(AF_INET, "233.252.0.1", &sin.sin_addr) == 1);
-	sin.sin_port = htons(6676);
-	rv = sendto(s, &sin, sizeof(sin), 0,
-	    (struct sockaddr *)&sin, sizeof(sin));
-	if (rv != sizeof(sin))
+	assert(bind(s, (struct sockaddr *)&src, sizeof(src)) == 0);
+	if (index)
+		assert(setsockopt(s, IPPROTO_IP, IP_MULTICAST_IF, &mreqn,
+		    sizeof(mreqn)) == 0);
+	else
+		assert(setsockopt(s, IPPROTO_IP, IP_MULTICAST_IF, &in,
+		    sizeof(in)) == 0);
+	if (sendto(s, argv[6], strlen(argv[6]) + 1, 0, (struct sockaddr *)&dst,
+	    sizeof(dst)) != (ssize_t)strlen(argv[6]) + 1)
 		err(1, "sendto failed");
 
 	return (0);
