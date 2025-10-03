80ada959004c4386880e47b11618f8abfc2d80e1
Kyle Evans <kevans@FreeBSD.org>
2025-10-03 13:09:04 -0500

    flua: kick out the remaining builtin modules
    Bootstrap flua has some magic now to handle modules by building them in
    and discovering them via linker sets.  This is slightly cleaner than
    always building them in and baking them into loadedlibs for both
    bootstrap and system flua.
    
    Adjust the stand build now that these three libs have their own new
    homes.
    
    Reviewed by:	bapt, emaste
    Differential Revision:	https://reviews.freebsd.org/D51891

--- stand/loader.mk
+++ stand/loader.mk
@@ -89,7 +89,7 @@ SRCS+=	interp_lua.c
 .include "${BOOTSRC}/lua.mk"
 LDR_INTERP=	${LIBLUA}
 LDR_INTERP32=	${LIBLUA32}
-CFLAGS.interp_lua.c= -DLUA_PATH=\"${LUAPATH}\" -I${FLUASRC}/modules
+CFLAGS.interp_lua.c= -DLUA_PATH=\"${LUAPATH}\" -I${FLUASRC}/lfs
 .elif ${LOADER_INTERP} == "4th"
 SRCS+=	interp_forth.c
 .include "${BOOTSRC}/ficl.mk"
