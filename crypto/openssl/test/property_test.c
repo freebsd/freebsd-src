c0366f908ff44e1af39c40135dc3804bdcdd505e
Enji Cooper <ngie@FreeBSD.org>
2025-10-03 20:26:18 -0700

    crypto/openssl: update to 3.5.4
    This change includes all necessary changes required to update to OpenSSL
    3.5.4.
    
    More information about the 3.5.4 release can be found in the relevant
    release notes (see 8e12a5c4eb3507846b5 for more details).
    
    Merge commit '8e12a5c4eb3507846b507d0afe87d115af41df40'
    
    Approved by:	re (cperciva)
    
    (cherry picked from commit 046c625e9382e17da953767b881aaa782fa73af8)
    (cherry picked from commit 57d1d2c49707d1d74cdae2946d89bbd427661dca)

--- crypto/openssl/test/property_test.c
+++ crypto/openssl/test/property_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2023 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2019-2025 The OpenSSL Project Authors. All Rights Reserved.
  * Copyright (c) 2019, Oracle and/or its affiliates.  All rights reserved.
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
@@ -687,6 +687,22 @@ static int test_property_list_to_string(int i)
     return ret;
 }
 
+static int test_property_list_to_string_bounds(void)
+{
+    OSSL_PROPERTY_LIST *pl = NULL;
+    char buf[16];
+    int ret = 0;
+
+    if (!TEST_ptr(pl = ossl_parse_query(NULL, "provider='$1'", 1)))
+        goto err;
+    if (!TEST_size_t_eq(ossl_property_list_to_string(NULL, pl, buf, 10), 14))
+        goto err;
+    ret = 1;
+ err:
+    ossl_property_free(pl);
+    return ret;
+}
+
 int setup_tests(void)
 {
     ADD_TEST(test_property_string);
@@ -701,5 +717,6 @@ int setup_tests(void)
     ADD_TEST(test_query_cache_stochastic);
     ADD_TEST(test_fips_mode);
     ADD_ALL_TESTS(test_property_list_to_string, OSSL_NELEM(to_string_tests));
+    ADD_TEST(test_property_list_to_string_bounds);
     return 1;
 }
