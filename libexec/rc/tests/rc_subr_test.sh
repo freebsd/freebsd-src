2f744885ea09b7d44c7955aaaf740cb024e4312e
Mark Johnston <markj@FreeBSD.org>
2025-10-08 12:51:16 +0000

    rc tests: Add a test to check for warnings from rcorder
    Reviewed by:	des
    MFC after:	2 weeks
    Differential Revision:	https://reviews.freebsd.org/D52954
    
    (cherry picked from commit 04d47c7cde54cc325cca3f47113976f5500dc58f)

--- libexec/rc/tests/rc_subr_test.sh
+++ libexec/rc/tests/rc_subr_test.sh
@@ -28,6 +28,17 @@
 # SUCH DAMAGE.
 #
 
+atf_test_case no_cycles
+no_cycles_head()
+{
+	atf_set "descr" "Verify that /etc/rc.d/* contains no cycles"
+}
+
+no_cycles_body()
+{
+	atf_check -e empty -o ignore rcorder /etc/rc.d/*
+}
+
 atf_test_case oomprotect_all
 oomprotect_all_head()
 {
@@ -132,6 +143,7 @@ EOF
 
 atf_init_test_cases()
 {
+	atf_add_test_case no_cycles
 	atf_add_test_case oomprotect_all
 	atf_add_test_case oomprotect_yes
 	atf_add_test_case wait_for_pids_progress
