e474fdfde604372000fc6bc4f7197b1e7c904597
Ryan Libby <rlibby@FreeBSD.org>
2025-10-09 12:29:37 -0700

    acpidump: quiet gcc -Wstrict-aliasing
    Appease GCC.
    
    Reviewed by:	kib
    Differential Revision:	https://reviews.freebsd.org/D52941

--- usr.sbin/acpi/acpidump/acpi.c
+++ usr.sbin/acpi/acpidump/acpi.c
@@ -1913,16 +1913,19 @@ acpi_handle_ivrs_ivmd_type(ACPI_IVRS_MEMORY *addr)
 static void
 acpi_handle_ivrs_ivmd(ACPI_IVRS_MEMORY *addr)
 {
+	UINT16 x16;
+
 	printf("\tMem Type=%#x(%s) ",
 	    addr->Header.Type, acpi_handle_ivrs_ivmd_type(addr));
 	switch (addr->Header.Type) {
 	case ACPI_IVRS_TYPE_MEMORY2:
-		printf("Id=%#06x PCISeg=%#x ", addr->Header.DeviceId,
-		    *(UINT16 *)&addr->Reserved);
+		memcpy(&x16, &addr->Reserved, sizeof(x16));
+		printf("Id=%#06x PCISeg=%#x ", addr->Header.DeviceId, x16);
 		break;
 	case ACPI_IVRS_TYPE_MEMORY3:
+		memcpy(&x16, &addr->Reserved, sizeof(x16));
 		printf("Id=%#06x-%#06x PCISeg=%#x", addr->Header.DeviceId,
-		    addr->AuxData, *(UINT16 *)&addr->Reserved);
+		    addr->AuxData, x16);
 		break;
 	}
 	printf("Start=%#18jx Length=%#jx Flags=",
