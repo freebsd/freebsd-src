freebsd/freebsd-src

commit a96abd73fbadc230a46f180789d0d09076247474

nvme: use nitems() rather than hard coded value
Suggested by: jhb@
Sponsored by: Netflix

sys/dev/nvme/nvme.h
@@ -2173,7 +2173,7 @@ void	nvme_namespace_data_swapbytes(struct nvme_namespace_data *s __unused)
 	s->anagrpid = le32toh(s->anagrpid);
 	s->nvmsetid = le16toh(s->nvmsetid);
 	s->endgid = le16toh(s->endgid);
-	for (i = 0; i < 64; i++)
+	for (i = 0; i < nitems(s->lbaf); i++)
 		s->lbaf[i] = le32toh(s->lbaf[i]);
 #endif
 }

commit 55cb1328724514ea1181c4d7a8bc40c6e15c23ad

nvme: Use unsigned
CI complains about signed/unsigned comparisons, but normal build does
not. Fix this by transitioning to unsigned and using a loop-scoped
variable.

Fixes: a96abd73fbad
Sponsored by: Netflix

sys/dev/nvme/nvme.h
@@ -2153,8 +2153,6 @@ static inline
 void	nvme_namespace_data_swapbytes(struct nvme_namespace_data *s __unused)
 {
 #if _BYTE_ORDER != _LITTLE_ENDIAN
-	int i;
-
 	s->nsze = le64toh(s->nsze);
 	s->ncap = le64toh(s->ncap);
 	s->nuse = le64toh(s->nuse);
@@ -2173,7 +2171,7 @@ void	nvme_namespace_data_swapbytes(struct nvme_namespace_data *s __unused)
 	s->anagrpid = le32toh(s->anagrpid);
 	s->nvmsetid = le16toh(s->nvmsetid);
 	s->endgid = le16toh(s->endgid);
-	for (i = 0; i < nitems(s->lbaf); i++)
+	for (unsigned i = 0; i < nitems(s->lbaf); i++)
 		s->lbaf[i] = le32toh(s->lbaf[i]);
 #endif
 }