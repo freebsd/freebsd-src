3aaaba20786966062d1355d4809142b623ffc4b7
Bjoern A. Zeeb <bz@FreeBSD.org>
2025-10-17 19:34:20 +0000

    LinuxKPI: add fips.h
    We do not currently aim to support crypto FIPS compliance in LinuxKPI
    so keep this turned off.
    
    Reviewed by:	emaste, dumbbell
    Differential Revision: https://reviews.freebsd.org/D53207
    
    (cherry picked from commit 98609f4fecffd0e70293ddd57c2b785e498b1131)

---
+++ sys/compat/linuxkpi/common/include/linux/fips.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025 Bjoern A. Zeeb
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef	_LINUXKPI_LINUX_FIPS_H
+#define	_LINUXKPI_LINUX_FIPS_H
+
+#define	fips_enabled	0
+
+#endif /* _LINUXKPI_LINUX_FIPS_H */
