224ac9d2a030f85924e44a507ddc412797c3fe87
Bjoern A. Zeeb <bz@FreeBSD.org>
2025-10-19 15:28:30 +0000

    LinuxKPI: soc/mediatek add pr_debug calls
    We do not support Wireless Ethernet Dispatch (wed) on the mt76 (yet).
    Add pr_debug calls with TODO in case we run into the calls anyway so
    we could deal with them.
    
    MFC after:	3 days

--- sys/compat/linuxkpi/common/include/linux/soc/mediatek/mtk_wed.h
+++ sys/compat/linuxkpi/common/include/linux/soc/mediatek/mtk_wed.h
@@ -1,54 +1,36 @@
 /*-
+ * Copyright (c) 2022-2025 Bjoern A. Zeeb
+ *
  * SPDX-License-Identifier: BSD-2-Clause
- *
- * Copyright (c) 2022-2023 Bjoern A. Zeeb
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
  */
 
 #ifndef	_LINUXKPI_LINUX_SOC_MEDIATEK_MTK_WED_H
 #define	_LINUXKPI_LINUX_SOC_MEDIATEK_MTK_WED_H
+
+#include <linux/kernel.h>	/* pr_debug */
 
 struct mtk_wed_device {
 };
 
 #define	WED_WO_STA_REC	0x6
 
-#define	mtk_wed_device_start(_dev, _mask)		do { } while(0)
-#define	mtk_wed_device_detach(_dev)			do { } while(0)
+#define	mtk_wed_device_start(_dev, _mask)		do { pr_debug("%s: TODO\n", __func__); } while(0)
+#define	mtk_wed_device_detach(_dev)			do { pr_debug("%s: TODO\n", __func__); } while(0)
 #define	mtk_wed_device_irq_get(_dev, _mask)		0
-#define	mtk_wed_device_irq_set_mask(_dev, _mask)	do { } while(0)
-#define	mtk_wed_device_update_msg(_dev, _id, _msg, _len)	(-ENODEV)
-#define	mtk_wed_device_dma_reset(_dev)			do {} while (0)
+#define	mtk_wed_device_irq_set_mask(_dev, _mask)	do { pr_debug("%s: TODO\n", __func__); } while(0)
+#define	mtk_wed_device_update_msg(_dev, _id, _msg, _len)	({ pr_debug("%s: TODO\n", __func__); -ENODEV; })
+#define	mtk_wed_device_dma_reset(_dev)			do { pr_debug("%s: TODO\n", __func__); } while (0)
 #define	mtk_wed_device_ppe_check(_dev, _skb, _reason, _entry) \
-    do {} while (0)
-#define	mtk_wed_device_stop(_dev)			do { } while(0)
-#define	mtk_wed_device_start_hw_rro(_dev, _mask, _b)	do { } while(0)
-#define	mtk_wed_device_setup_tc(_dev, _ndev, _type, _tdata)	(-EOPNOTSUPP)
+    do { pr_debug("%s: TODO\n", __func__); } while (0)
+#define	mtk_wed_device_stop(_dev)			do { pr_debug("%s: TODO\n", __func__); } while(0)
+#define	mtk_wed_device_start_hw_rro(_dev, _mask, _b)	do { pr_debug("%s: TODO\n", __func__); } while(0)
+#define	mtk_wed_device_setup_tc(_dev, _ndev, _type, _tdata)	({ pr_debug("%s: TODO\n", __func__); -EOPNOTSUPP; })
 
 static inline bool
 mtk_wed_device_active(struct mtk_wed_device *dev __unused)
 {
 
+	pr_debug("%s: TODO\n", __func__);
 	return (false);
 }
 
@@ -56,6 +38,7 @@ static inline bool
 mtk_wed_get_rx_capa(struct mtk_wed_device *dev __unused)
 {
 
+	pr_debug("%s: TODO\n", __func__);
 	return (false);
 }
 
