a6369fb9e4b40396ea6ef1987436019612c387cd
Elliott Mitchell <ehem+freebsd@m5p.com>
2025-10-19 13:29:08 -0700

    nfscommon: Update the nfs_bigreply array
    The variable is simply a long series of flags.  Mark as boolean to allow
    the compiler to use smaller types.
    
    Convert to designated initializer to better indicate which operations it
    is set for.  Without this the array is kind of long and sparse.
    
    nfsv4_setsequence() is no longer used outside nfs_commonsubs.c, so now
    mark it as static.  The fourth argument is true/false, so convert to
    boolean.
    
    No semantics change is made by this commit.
    
    Discussed with:	jrtc27
    MFC after:	2 weeks

--- sys/fs/nfs/nfs_var.h
+++ sys/fs/nfs/nfs_var.h
@@ -361,8 +361,6 @@ int nfsv4_getipaddr(struct nfsrv_descript *, struct sockaddr_in *,
 int nfsv4_seqsession(uint32_t, uint32_t, uint32_t, struct nfsslot *,
     struct mbuf **, uint16_t);
 void nfsv4_seqsess_cacherep(uint32_t, struct nfsslot *, int, struct mbuf **);
-void nfsv4_setsequence(struct nfsmount *, struct nfsrv_descript *,
-    struct nfsclsession *, int, struct ucred *);
 int nfsv4_sequencelookup(struct nfsmount *, struct nfsclsession *, int *,
     int *, uint32_t *, uint8_t *, bool);
 void nfsv4_freeslot(struct nfsclsession *, int, bool);
