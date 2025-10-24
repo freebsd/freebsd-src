6a6a1f927fed89d801ad32adfbc95ec0bd0849db
Konstantin Belousov <kib@FreeBSD.org>
2025-10-24 12:39:13 +0300

    amd64: print 'EFI RT fault' line before fault CPU state
    Suggested by:	arrowd
    Sponsored by:	The FreeBSD Foundation
    MFC after:	3 days

--- sys/amd64/amd64/trap.c
+++ sys/amd64/amd64/trap.c
@@ -435,9 +435,9 @@ trap(struct trapframe *frame)
 
 			if ((print_efirt_faults == 1 && cnt == 0) ||
 			    print_efirt_faults == 2) {
-				trap_diag(frame, 0);
 				printf("EFI RT fault %s\n",
 				    traptype_to_msg(type));
+				trap_diag(frame, 0);
 			}
 			frame->tf_rip = (long)curpcb->pcb_onfault;
 			return;
@@ -870,8 +870,8 @@ after_vmfault:
 
 			if ((print_efirt_faults == 1 && cnt == 0) ||
 			    print_efirt_faults == 2) {
-				trap_diag(frame, eva);
 				printf("EFI RT page fault\n");
+				trap_diag(frame, eva);
 			}
 		}
 		frame->tf_rip = (long)curpcb->pcb_onfault;
