87b73c2684d52091ee9d43d1207ca3e980f0903a
Ed Maste <emaste@FreeBSD.org>
2025-10-25 18:32:05 -0400

    ipfw: Remove more unused IP_FW_* cases
    All of the do_cmd() calls are in dummynet.c and specify the socket
    option at compile time; none of these removed cases are used in ipfw
    after the v3 work.
    
    Approved by:	re (cperciva)
    Reviewed by:	markj
    Sponsored by:	The FreeBSD Foundation
    Differential Revision: https://reviews.freebsd.org/D53378
    
    (cherry picked from commit 0e2e0fb955adf15a217949bc4cc337d53d2c7259)
    (cherry picked from commit 6b1e5d4d20a94b5bebd726eb6d1df8dca2738f8e)

--- sbin/ipfw/ipfw2.c
+++ sbin/ipfw/ipfw2.c
@@ -648,11 +648,7 @@ do_cmd(int optname, void *optval, uintptr_t optlen)
 	if (ipfw_socket < 0)
 		err(EX_UNAVAILABLE, "socket");
 
-	if (optname == IP_FW_GET ||
-	    optname == IP_FW_ADD || optname == IP_FW3 ||
-	    optname == IP_FW_NAT_GET_CONFIG ||
-	    optname < 0 ||
-	    optname == IP_FW_NAT_GET_LOG) {
+	if (optname == IP_FW3 || optname < 0) {
 		if (optname < 0)
 			optname = -optname;
 		i = getsockopt(ipfw_socket, IPPROTO_IP, optname, optval,
