e2f8e26309b0e132aefb77ddf879ad22cc127571
Elliott Mitchell <ehem+freebsd@m5p.com>
2025-10-27 16:08:16 -0700

    intr/x86: merge disable_source calls into disable_intr, unconditionally EOI
    Due to interfacing with the interrupt core it is extremely valuable for
    pic_disable_source handlers not to require an argument.  Avoiding the
    EOI when removing handlers isn't too valuable.  This causes
    pic_disable_source to match behavior of other architectures.  Together
    these are very high-value.

--- sys/x86/x86/msi.c
+++ sys/x86/x86/msi.c
@@ -131,7 +131,7 @@ static void	msi_create_source(void);
 static void	msi_enable_source(x86pic_t pic, struct intsrc *isrc);
 static void	msi_eoi_source(x86pic_t pic, struct intsrc *isrc);
 static void	msi_enable_intr(x86pic_t pic, struct intsrc *isrc);
-static pic_disable_intr_t		msi_disable_intr;
+static void	msi_disable_intr(x86pic_t pic, struct intsrc *isrc);
 static int	msi_source_pending(x86pic_t pic, struct intsrc *isrc);
 static int	msi_assign_cpu(x86pic_t pic, struct intsrc *isrc,
 		    u_int apic_id);
@@ -208,7 +208,7 @@ msi_enable_intr(x86pic_t pic, struct intsrc *isrc)
 }
 
 static void
-msi_disable_intr(x86pic_t pic, struct intsrc *isrc, enum eoi_flag eoi)
+msi_disable_intr(x86pic_t pic, struct intsrc *isrc)
 {
 	struct msi_intsrc *msi = (struct msi_intsrc *)isrc;
 
@@ -221,8 +221,7 @@ msi_disable_intr(x86pic_t pic, struct intsrc *isrc, enum eoi_flag eoi)
 	if (msi == NULL)
 		return;
 
-	if (eoi == PIC_EOI)
-		lapic_eoi();
+	lapic_eoi();
 
 	msi->msi_enabled--;
 	if (msi->msi_enabled == 0) {
