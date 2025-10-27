5dbc62d55708d044b1d858eb0c3699c38c508b39
Mark Johnston <markj@FreeBSD.org>
2025-10-27 16:33:34 +0000

    gdb: Make development a bit easier
    Provide a command which can be used to reload gdb modules.
    
    MFC after:	1 week

--- sys/tools/kernel-gdb.py
+++ sys/tools/kernel-gdb.py
@@ -4,12 +4,40 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
+import importlib
 import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "gdb"))
 
-# Import FreeBSD kernel debugging commands and modules below.
-import acttrace
-import pcpu
-import vnet
+modules = [
+    "acttrace",
+    "freebsd",
+    "pcpu",
+    "vnet"
+]
+
+
+def reload_modules(modules):
+    for mod in modules:
+        if mod in sys.modules:
+            importlib.reload(sys.modules[mod])
+        else:
+            importlib.import_module(mod)
+
+reload_modules(modules)
+
+
+class reload(gdb.Command):
+    """
+    Reload the FreeBSD kernel GDB helper scripts.
+    """
+    def __init__(self):
+        super(reload, self).__init__("kgdb-reload", gdb.COMMAND_USER)
+
+    def invoke(self, arg, from_tty):
+        reload_modules(modules)
+
+
+# Register the reload command with gdb.
+reload()
