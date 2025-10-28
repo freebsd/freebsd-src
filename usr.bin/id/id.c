eff786f23611582e518f038e4086a1306c019d23
Dag-Erling Smørgrav <des@FreeBSD.org>
2025-10-28 17:51:56 +0100

    id: Add -d and -s options
    These options may not be combined with any other options and print the
    current or specified user's home directory and shell respectively.
    
    Reviewed by:	imp, bcr
    Differential Revision:	https://reviews.freebsd.org/D53301
    
    (cherry picked from commit f41b1eb637f576634be0df9d657f46aa57afea59)

--- usr.bin/id/id.c
+++ usr.bin/id/id.c
@@ -55,6 +55,8 @@ static void	auditid(void);
 #endif
 static void	group(struct passwd *, bool);
 static void	maclabel(void);
+static void	dir(struct passwd *);
+static void	shell(struct passwd *);
 static void	usage(void);
 static struct passwd *who(char *);
 
@@ -69,7 +71,7 @@ main(int argc, char *argv[])
 	bool Aflag;
 #endif
 	bool Gflag, Mflag, Pflag;
-	bool cflag, gflag, nflag, pflag, rflag, uflag;
+	bool cflag, dflag, gflag, nflag, pflag, rflag, sflag, uflag;
 	int ch, combo, error, id;
 	const char *myname, *optstr;
 	char loginclass[MAXLOGNAME];
@@ -78,10 +80,10 @@ main(int argc, char *argv[])
 	Aflag = false;
 #endif
 	Gflag = Mflag = Pflag = false;
-	cflag = gflag = nflag = pflag = rflag = uflag = false;
+	cflag = dflag = gflag = nflag = pflag = rflag = sflag = uflag = false;
 
 	myname = getprogname();
-	optstr = "AGMPacgnpru";
+	optstr = "AGMPacdgnprsu";
 	if (strcmp(myname, "groups") == 0) {
 		isgroups = true;
 		optstr = "";
@@ -114,6 +116,9 @@ main(int argc, char *argv[])
 		case 'c':
 			cflag = true;
 			break;
+		case 'd':
+			dflag = true;
+			break;
 		case 'g':
 			gflag = true;
 			break;
@@ -125,6 +130,9 @@ main(int argc, char *argv[])
 			break;
 		case 'r':
 			rflag = true;
+			break;
+		case 's':
+			sflag = true;
 			break;
 		case 'u':
 			uflag = true;
@@ -142,6 +150,8 @@ main(int argc, char *argv[])
 		usage();
 
 	combo = Aflag + Gflag + Mflag + Pflag + gflag + pflag + uflag;
+	if (combo + dflag + sflag > 1)
+		usage();
 	if (combo > 1)
 		usage();
 	if (combo == 0 && (nflag || rflag))
@@ -185,6 +195,11 @@ main(int argc, char *argv[])
 		exit(0);
 	}
 
+	if (dflag) {
+		dir(pw);
+		exit(0);
+	}
+
 	if (Gflag) {
 		group(pw, nflag);
 		exit(0);
@@ -202,6 +217,11 @@ main(int argc, char *argv[])
 
 	if (pflag) {
 		pretty(pw);
+		exit(0);
+	}
+
+	if (sflag) {
+		shell(pw);
 		exit(0);
 	}
 
@@ -465,6 +485,26 @@ pline(struct passwd *pw)
 }
 
 static void
+dir(struct passwd *pw)
+{
+	if (pw == NULL) {
+		if ((pw = getpwuid(getuid())) == NULL)
+			err(1, "getpwuid");
+	}
+	printf("%s\n", pw->pw_dir);
+}
+
+static void
+shell(struct passwd *pw)
+{
+	if (pw == NULL) {
+		if ((pw = getpwuid(getuid())) == NULL)
+			err(1, "getpwuid");
+	}
+	printf("%s\n", pw->pw_shell);
+}
+
+static void
 usage(void)
 {
 	if (isgroups)
@@ -481,8 +521,10 @@ usage(void)
 		    "       id -M\n"
 		    "       id -P [user]\n"
 		    "       id -c\n"
+		    "       id -d [user]\n"
 		    "       id -g [-nr] [user]\n"
 		    "       id -p [user]\n"
+		    "       id -s [user]\n"
 		    "       id -u [-nr] [user]\n");
 	exit(1);
 }
