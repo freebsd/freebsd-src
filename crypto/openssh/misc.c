ce03706ab26c5770150f1ef96aca36b69baa535f
Ed Maste <emaste@FreeBSD.org>
2025-10-31 13:04:51 -0400

    openssh: Handle localtime_r() failure by return "UNKNOWN-TIME"
    Apply openssh-portable commit 8b6c1f402feb by deraadt@openbsd.org
    
    Obtained from:	openssh-portable
    PR:		288773
    Reported by:	wosch
    Sponsored by:	The FreeBSD Foundation

--- crypto/openssh/misc.c
+++ crypto/openssh/misc.c
@@ -2535,8 +2535,10 @@ format_absolute_time(uint64_t t, char *buf, size_t len)
 	time_t tt = t > SSH_TIME_T_MAX ? SSH_TIME_T_MAX : t;
 	struct tm tm;
 
-	localtime_r(&tt, &tm);
-	strftime(buf, len, "%Y-%m-%dT%H:%M:%S", &tm);
+	if (localtime_r(&tt, &tm) == NULL)
+		strlcpy(buf, "UNKNOWN-TIME", len);
+	else
+		strftime(buf, len, "%Y-%m-%dT%H:%M:%S", &tm);
 }
 
 /*
