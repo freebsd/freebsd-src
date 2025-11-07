freebsd/freebsd-src

commit 18556b667ca9a6db91e8d70777eda30f01a7508c

safe(4): add gone_in 16.0
Reviewed by:	emaste
Differential Revision:	https://reviews.freebsd.org/D53426

sys/dev/safe/safe.c
@@ -424,6 +424,8 @@ safe_attach(device_t dev)
 #ifdef SAFE_DEBUG
 	safec = sc;			/* for use by hw.safe.dump */
 #endif
+	gone_in(16, "%s(4) is deprecated in 15.0 and removed in 16.0\n",
+	    safe_driver->name);
 	return (0);
 bad4:
 	crypto_unregister_all(sc->sc_cid);

share/man/man4/safe.4
@@ -25,7 +25,7 @@
 .\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 .\" SUCH DAMAGE.
 .\"
-.Dd November 22, 2024
+.Dd October 31, 2025
 .Dt SAFE 4
 .Os
 .Sh NAME
@@ -57,6 +57,11 @@ In
 .Cd hw.safe.rngbufsize
 .Cd hw.safe.rngmaxalarm
 .Ed
+.Sh DEPRECATION NOTICE
+The
+.Nm
+driver is deprecated and is scheduled for removal in
+.Fx 16.0 .
 .Sh DESCRIPTION
 The
 .Nm
@@ -128,5 +133,14 @@ packet processing.
 .Xr random 4 ,
 .Xr crypto 7 ,
 .Xr crypto 9
+.Sh HISTORY
+The
+.Nm
+driver first appeared in
+.Fx 5.2 .
+It is deprecated in
+.Fx 15.0
+and removed in
+.Fx 16.0 .
 .Sh BUGS
 Public key support is not implemented.

commit b460b002db993a10c69f85b62a4cf92865672925

safe(4): fix api used
Fixes: 18556b66

sys/dev/safe/safe.c
@@ -425,7 +425,7 @@ safe_attach(device_t dev)
 	safec = sc;			/* for use by hw.safe.dump */
 #endif
 	gone_in(16, "%s(4) is deprecated in 15.0 and removed in 16.0\n",
-	    safe_driver->name);
+	    safe_driver.name);
 	return (0);
 bad4:
 	crypto_unregister_all(sc->sc_cid);