0339b24f8d30f92979c4ec5ba2556cd83cce9e98
David E. O'Brien <obrien@FreeBSD.org>
2025-11-07 18:18:13 -0800

    random: Have RANDOM_PURE_START be a cross-platform source
    and one that will be with us in the long-term future.
    (this helps reduce diffs in the future and for down-stream users
    that trim entropy sources).  Also, move deprecated (removed in 16.0)
    sources to the bottom of the list to reduce changes to 15.x.
    
    Approved by:	re (cperciva)
    Reviewed by:	glebius
    Obtained from:	Juniper Networks
    Differential Revision:	https://reviews.freebsd.org/D53311
    (cherry picked from commit 9f3886347c1750cf80a82314470fc7186088eb9a)
    (cherry picked from commit e224f2c1b98ba065942c83d322cdb8e3987d0c81)

--- sys/dev/random/fenestrasX/fx_pool.c
+++ sys/dev/random/fenestrasX/fx_pool.c
@@ -167,13 +167,7 @@ static const struct fxrng_ent_char {
 	[RANDOM_RANDOMDEV] = {
 		.entc_cls = &fxrng_lo_push,
 	},
-	[RANDOM_PURE_SAFE] = {
-		.entc_cls = &fxrng_hi_push,
-	},
-	[RANDOM_PURE_GLXSB] = {
-		.entc_cls = &fxrng_hi_push,
-	},
-	[RANDOM_PURE_HIFN] = {
+	[RANDOM_PURE_TPM] = {
 		.entc_cls = &fxrng_hi_push,
 	},
 	[RANDOM_PURE_RDRAND] = {
@@ -200,9 +194,6 @@ static const struct fxrng_ent_char {
 	[RANDOM_PURE_DARN] = {
 		.entc_cls = &fxrng_hi_pull,
 	},
-	[RANDOM_PURE_TPM] = {
-		.entc_cls = &fxrng_hi_push,
-	},
 	[RANDOM_PURE_VMGENID] = {
 		.entc_cls = &fxrng_hi_push,
 	},
@@ -214,6 +205,15 @@ static const struct fxrng_ent_char {
 	},
 	[RANDOM_PURE_ARM_TRNG] = {
 		.entc_cls = &fxrng_hi_pull,
+	},
+	[RANDOM_PURE_SAFE] = {
+		.entc_cls = &fxrng_hi_push,
+	},
+	[RANDOM_PURE_GLXSB] = {
+		.entc_cls = &fxrng_hi_push,
+	},
+	[RANDOM_PURE_HIFN] = {
+		.entc_cls = &fxrng_hi_push,
 	},
 };
 CTASSERT(nitems(fxrng_ent_char) == ENTROPYSOURCE);
