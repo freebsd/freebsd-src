eb95b990f8ebfb85ce6e4d7d16305746b5eddcde
Christos Margiolis <christos@FreeBSD.org>
2025-11-11 14:21:22 +0100

    sound tests: Fix format specified for kevent.data
    Reported by:	CI
    Fixes:		dd81b19ef236 ("sound tests: Test polling")
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week

--- tests/sys/sound/polling.c
+++ tests/sys/sound/polling.c
@@ -93,7 +93,7 @@ ATF_TC_BODY(poll_kqueue, tc)
 	ATF_REQUIRE_MSG(kevent(kq, NULL, 0, &ev, 1, NULL) == 1,
 	    FMT_ERR("kevent"));
 	ATF_REQUIRE_MSG((ev.flags & EV_ERROR) == 0, "EV_ERROR is set");
-	ATF_REQUIRE_MSG(ev.data != 0, "data is %ld", ev.data);
+	ATF_REQUIRE_MSG(ev.data != 0, "data is %lld", ev.data);
 	ATF_REQUIRE_MSG(read(fd, buf, sizeof(buf)) > 0, FMT_ERR("read"));
 
 	EV_SET(&ev, fd, EVFILT_READ, EV_DELETE, 0, 0, 0);
@@ -108,7 +108,7 @@ ATF_TC_BODY(poll_kqueue, tc)
 	ATF_REQUIRE_MSG(kevent(kq, NULL, 0, &ev, 1, NULL) == 1,
 	    FMT_ERR("kevent"));
 	ATF_REQUIRE_MSG((ev.flags & EV_ERROR) == 0, "EV_ERROR is set");
-	ATF_REQUIRE_MSG(ev.data != 0, "data is %ld", ev.data);
+	ATF_REQUIRE_MSG(ev.data != 0, "data is %lld", ev.data);
 	ATF_REQUIRE_MSG(write(fd, buf, sizeof(buf)) > 0, FMT_ERR("write"));
 
 	EV_SET(&ev, fd, EVFILT_WRITE, EV_DELETE, 0, 0, 0);
