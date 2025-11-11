55540ebc33ad9a52bc4de121cb598243153981a8
Christos Margiolis <christos@FreeBSD.org>
2025-11-11 13:05:03 +0100

    sound: Retire snd_dbuf->dev
    Redundant, and if we at some point really need this, we can fetch it
    from snd_dbuf->channel->dev.
    
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Reviewed by:	markj
    Differential Revision:	https://reviews.freebsd.org/D53519

--- sys/dev/sound/pcm/buffer.c
+++ sys/dev/sound/pcm/buffer.c
@@ -41,13 +41,12 @@
 #include "snd_fxdiv_gen.h"
 
 struct snd_dbuf *
-sndbuf_create(device_t dev, char *drv, char *desc, struct pcm_channel *channel)
+sndbuf_create(char *drv, char *desc, struct pcm_channel *channel)
 {
 	struct snd_dbuf *b;
 
 	b = malloc(sizeof(*b), M_DEVBUF, M_WAITOK | M_ZERO);
 	snprintf(b->name, SNDBUF_NAMELEN, "%s:%s", drv, desc);
-	b->dev = dev;
 	b->channel = channel;
 
 	return b;
@@ -72,7 +71,7 @@ sndbuf_setmap(void *arg, bus_dma_segment_t *segs, int nseg, int error)
 	struct snd_dbuf *b = (struct snd_dbuf *)arg;
 
 	if (snd_verbose > 3) {
-		device_printf(b->dev, "sndbuf_setmap %lx, %lx; ",
+		printf("sndbuf_setmap %lx, %lx; ",
 		    (u_long)segs[0].ds_addr, (u_long)segs[0].ds_len);
 		printf("%p -> %lx\n", b->buf, (u_long)segs[0].ds_addr);
 	}
