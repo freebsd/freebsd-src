046f6ed928ef2ca5742ea8faab6040580d15ff14
John Baldwin <jhb@FreeBSD.org>
2025-11-12 22:23:27 -0500

    aq(4): Remove unused DRIVER_MODULE devclass

--- sys/dev/aq/aq_main.c
+++ sys/dev/aq/aq_main.c
@@ -196,8 +196,12 @@ static driver_t aq_driver = {
 	"aq", aq_methods, sizeof(struct aq_dev),
 };
 
+#if __FreeBSD_version >= 1400058
+DRIVER_MODULE(atlantic, pci, aq_driver, 0, 0);
+#else
 static devclass_t aq_devclass;
 DRIVER_MODULE(atlantic, pci, aq_driver, aq_devclass, 0, 0);
+#endif
 
 MODULE_DEPEND(atlantic, pci, 1, 1, 1);
 MODULE_DEPEND(atlantic, ether, 1, 1, 1);
