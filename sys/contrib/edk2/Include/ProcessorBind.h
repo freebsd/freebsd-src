ed04129430efaa9dcfa91b5d6a62dc475eceb426
Warner Losh <imp@FreeBSD.org>
2025-11-16 10:34:33 -0700

    edk2: Move ProcessorBind.h to contrib/edk2
    Have our own ProcessorBind.h. It just includes sys/efi-edk2.h, but
    undefines TRUE, FALSE and MAC. The first two are from acpica and are
    redefined to something almost the same. MAC is a global option that,
    well, interferes with using EDK2, dangit. I suppose I should redefine it
    after, but I don't think you can save the value of a pre-processor
    variable.
    
    This breaks a little with the tradition of having this in a seprate
    directory and using build magic. However, the build is already magical
    enough and having this here makes things less magical. Also, EDK2 puts
    this in a processor specific directory, so we won't have conflicts (they
    need it there since they run on more processors than we do: we can just
    include sys/efi-edk2.h which covers the smallar variance we have in
    processors).
    
    Sponsored by:		Netflix

---
+++ sys/contrib/edk2/Include/ProcessorBind.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2025 Netflix, Inc
+ *
+ * SPDX-License-Idnetifier: BSD-2-Clause
+ */
+/* These three will be redefined -- well MAC is an option that collides */
+#undef TRUE
+#undef FALSE
+#undef MAC
+#include <sys/efi-edk2.h>
+
