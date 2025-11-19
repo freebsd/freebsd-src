freebsd/freebsd-src

commit c4539460e3a4a2b637d55fd0a9ac59514864483f

sh: Improve error handling in read builtin:
* If read -t times out, return status as if interrupted by SIGALRM
  (formerly 1).
* If a trapped signal interrupts read, return status 128+sig (formerly 1).
* If [EINTR] occurs but there is no trap, retry the read (for example
  because of a SIGWINCH in interactive mode).
* If a read error occurs, write an error message and return status 2.

As before, a variable assignment error returns 2 and discards the remaining
data read.

bin/sh/miscbltin.c
@@ -60,6 +60,7 @@ __FBSDID("$FreeBSD$");
 #include "error.h"
 #include "mystring.h"
 #include "syntax.h"
+#include "trap.h"
 
 #undef eflag
 
@@ -102,6 +103,8 @@ readcmd(int argc __unused, char **argv __unused)
 	struct timeval tv;
 	char *tvptr;
 	fd_set ifds;
+	ssize_t nread;
+	int sig;
 
 	rflag = 0;
 	prompt = NULL;
@@ -156,8 +159,10 @@ readcmd(int argc __unused, char **argv __unused)
 		/*
 		 * If there's nothing ready, return an error.
 		 */
-		if (status <= 0)
-			return(1);
+		if (status <= 0) {
+			sig = pendingsig;
+			return (128 + (sig != 0 ? sig : SIGALRM));
+		}
 	}
 
 	status = 0;
@@ -165,7 +170,19 @@ readcmd(int argc __unused, char **argv __unused)
 	backslash = 0;
 	STARTSTACKSTR(p);
 	for (;;) {
-		if (read(STDIN_FILENO, &c, 1) != 1) {
+		nread = read(STDIN_FILENO, &c, 1);
+		if (nread == -1) {
+			if (errno == EINTR) {
+				sig = pendingsig;
+				if (sig == 0)
+					continue;
+				status = 128 + sig;
+				break;
+			}
+			warning("read error: %s", strerror(errno));
+			status = 2;
+			break;
+		} else if (nread != 1) {
 			status = 1;
 			break;
 		}

bin/sh/sh.1
@@ -32,7 +32,7 @@
 .\"	from: @(#)sh.1	8.6 (Berkeley) 5/4/95
 .\" $FreeBSD$
 .\"
-.Dd April 21, 2013
+.Dd May 3, 2013
 .Dt SH 1
 .Os
 .Sh NAME
@@ -2372,7 +2372,9 @@ option is specified and the
 elapses before a complete line of input is supplied,
 the
 .Ic read
-command will return an exit status of 1 without assigning any values.
+command will return an exit status as if terminated by
+.Dv SIGALRM
+without assigning any values.
 The
 .Ar timeout
 value may optionally be followed by one of
@@ -2388,6 +2390,11 @@ is assumed.
 The
 .Fl e
 option exists only for backward compatibility with older scripts.
+.Pp
+The exit status is 0 on success, 1 on end of file,
+between 2 and 128 if an error occurs
+and greater than 128 if a trapped signal interrupts
+.Ic read .
 .It Ic readonly Oo Fl p Oc Op Ar name ...
 Each specified
 .Ar name

tools/regression/bin/sh/builtins/read7.0
@@ -1,0 +1,5 @@
+# $FreeBSD$
+
+{ errmsg=`read x <&- 2>&1 >&3`; } 3>&1
+r=$?
+[ "$r" -ge 2 ] && [ "$r" -le 128 ] && [ -n "$errmsg" ]

tools/regression/bin/sh/builtins/read8.0
@@ -1,0 +1,13 @@
+# $FreeBSD$
+
+T=`mktemp -d ${TMPDIR:-/tmp}/sh-test.XXXXXX`
+trap 'rm -rf $T' 0
+cd $T || exit 3
+mkfifo fifo1
+trapped=
+trap trapped=1 QUIT
+{ kill -QUIT $$; sleep 1; exit 4; } >fifo1 &
+read dummy <fifo1
+r=$?
+kill $!
+[ "$r" -gt 128 ] && [ -n "$trapped" ]

commit 1c5eed3a5b6db9363c1bf3e1ab5e2d94d72497d1

sh: Don't assume EINTR means SIGALRM
While waiting for input in the read builtin, if select() is interrupted
but there is no pending signal, we act like we timed out, and return the
same status as if we had been interrupted by SIGALRM, instead of looping
until we actually do time out.

* Replace the single select() call with a ppoll() loop.

* Improve validation of the timeout value.  We now accept things like
  "1h30m15s", which we used to silently truncate to "1h".  The flip side
  is that we no longer accept things like "1hour" or "5sec".

* Modify the existing `read -t 0` test case to verify that read returns
  immediately when there is input and fails immediately when there isn't.

* Add a second test case which performs the same tests with a non-zero
  timeout value.

PR:		290844
MFC after:	1 week
Fixes:          c4539460e3a4 ("sh: Improve error handling in read builtin:")
Reviewed by:	jilles, bdrewery
Differential Revision:	https://reviews.freebsd.org/D53761

bin/sh/miscbltin.c
@@ -40,11 +40,14 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/resource.h>
-#include <unistd.h>
+
 #include <errno.h>
+#include <poll.h>
+#include <signal.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "shell.h"
 #include "options.h"
@@ -162,17 +165,18 @@ readcmd(int argc __unused, char **argv __unused)
 	int is_ifs;
 	int saveall = 0;
 	ptrdiff_t lastnonifs, lastnonifsws;
-	struct timeval tv;
-	char *tvptr;
-	fd_set ifds;
+	sigset_t set, oset;
+	intmax_t number, timeout;
+	struct timespec tnow, tend, tresid;
+	struct pollfd pfd;
+	char *endptr;
 	ssize_t nread;
 	int sig;
 	struct fdctx fdctx;
 
 	rflag = 0;
 	prompt = NULL;
-	tv.tv_sec = -1;
-	tv.tv_usec = 0;
+	timeout = -1;
 	while ((i = nextopt("erp:t:")) != '\0') {
 		switch(i) {
 		case 'p':
@@ -184,22 +188,29 @@ readcmd(int argc __unused, char **argv __unused)
 			rflag = 1;
 			break;
 		case 't':
-			tv.tv_sec = strtol(shoptarg, &tvptr, 0);
-			if (tvptr == shoptarg)
-				error("timeout value");
-			switch(*tvptr) {
-			case 0:
-			case 's':
-				break;
-			case 'h':
-				tv.tv_sec *= 60;
-				/* FALLTHROUGH */
-			case 'm':
-				tv.tv_sec *= 60;
-				break;
-			default:
-				error("timeout unit");
-			}
+			timeout = 0;
+			do {
+				number = strtol(shoptarg, &endptr, 0);
+				if (number < 0 || endptr == shoptarg)
+					error("timeout value");
+				switch (*endptr) {
+				case 's':
+					endptr++;
+					break;
+				case 'h':
+					number *= 60;
+					/* FALLTHROUGH */
+				case 'm':
+					number *= 60;
+					endptr++;
+					break;
+				}
+				if (*endptr != '\0' &&
+				    !(*endptr >= '0' && *endptr <= '9'))
+					error("timeout unit");
+				timeout += number;
+				shoptarg = endptr;
+			} while (*shoptarg != '\0');
 			break;
 		}
 	}
@@ -212,13 +223,33 @@ readcmd(int argc __unused, char **argv __unused)
 	if ((ifs = bltinlookup("IFS", 1)) == NULL)
 		ifs = " \t\n";
 
-	if (tv.tv_sec >= 0) {
+	if (timeout >= 0) {
 		/*
 		 * Wait for something to become available.
 		 */
-		FD_ZERO(&ifds);
-		FD_SET(0, &ifds);
-		status = select(1, &ifds, NULL, NULL, &tv);
+		pfd.fd = STDIN_FILENO;
+		pfd.events = POLLIN;
+		status = sig = 0;
+		sigfillset(&set);
+		sigprocmask(SIG_SETMASK, &set, &oset);
+		if (pendingsig) {
+			/* caught a signal already */
+			status = -1;
+		} else if (timeout == 0) {
+			status = poll(&pfd, 1, 0);
+		} else {
+			clock_gettime(CLOCK_UPTIME, &tnow);
+			tend = tnow;
+			tend.tv_sec += timeout;
+			do {
+				timespecsub(&tend, &tnow, &tresid);
+				status = ppoll(&pfd, 1, &tresid, &oset);
+				if (status >= 0 || pendingsig != 0)
+					break;
+				clock_gettime(CLOCK_UPTIME, &tnow);
+			} while (timespeccmp(&tnow, &tend, <));
+		}
+		sigprocmask(SIG_SETMASK, &oset, NULL);
 		/*
 		 * If there's nothing ready, return an error.
 		 */

bin/sh/sh.1
@@ -31,7 +31,7 @@
 .\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 .\" SUCH DAMAGE.
 .\"
-.Dd May 1, 2025
+.Dd November 17, 2025
 .Dt SH 1
 .Os
 .Sh NAME
@@ -2544,6 +2544,10 @@ to explicitly specify seconds, minutes or hours.
 If none is supplied,
 .Ql s
 is assumed.
+Multiple value-unit groups may be stringed together, in which case
+they are added up, e.g.\&
+.Ql 1h30m15s
+which adds up to 5,415 seconds.
 .Pp
 The
 .Fl e

bin/sh/tests/builtins/Makefile
@@ -143,6 +143,7 @@ ${PACKAGE}FILES+=		read8.0
 ${PACKAGE}FILES+=		read9.0
 ${PACKAGE}FILES+=		read10.0
 ${PACKAGE}FILES+=		read11.0
+${PACKAGE}FILES+=		read12.0
 ${PACKAGE}FILES+=		return1.0
 ${PACKAGE}FILES+=		return2.1
 ${PACKAGE}FILES+=		return3.1

bin/sh/tests/builtins/read12.0
@@ -1,5 +1,5 @@
-# Verify that `read -t 0 v` succeeds immediately if input is available
-# and fails immediately if not
+# Verify that `read -t 3 v` succeeds immediately if input is available
+# and times out after 3 s if not
 
 set -e
 
@@ -15,7 +15,7 @@ exec 3<fifo1
 v=original_value
 r=0
 ts=$(date +%s%3N)
-read -t 0 v <&3 || r=$?
+read -t 3 v <&3 || r=$?
 te=$(date +%s%3N)
 [ "$r" -eq 0 ]
 [ $((te-ts)) -lt 250 ]
@@ -24,9 +24,9 @@ te=$(date +%s%3N)
 v=original_value
 r=0
 ts=$(date +%s%3N)
-read -t 0 v <&3 || r=$?
+read -t 3 v <&3 || r=$?
 te=$(date +%s%3N)
 kill -TERM "$!" || :
 [ "$r" -gt 128 ] && [ "$(kill -l "$r")" = ALRM ]
-[ $((te-ts)) -lt 250 ]
+[ $((te-ts)) -gt 3000 ] && [ $((te-ts)) -lt 3250 ]
 [ -z "$v" ]