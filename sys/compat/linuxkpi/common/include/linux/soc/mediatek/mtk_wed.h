7ed3a99f3563fce1f1088a376151e7ad68b738d6
Bjoern A. Zeeb <bz@FreeBSD.org>
2025-11-25 17:20:50 +0000

    LinuxKPI: mt76: depend on CONFIG_NET_MEDIATEK_SOC_WED
    If CONFIG_NET_MEDIATEK_SOC_WED is not set then be silent;  if
    CONFIG_NET_MEDIATEK_SOC_WED is set we will enable logic or rather te
    pr_debug("TODO") calls for now.
    
    Spsonsored by:	The FreeBSD Foundation
    
    (cherry picked from commit 51c73fc5f5dd95c6a0e3d232091a732a14c7ecc9)

--- sys/compat/linuxkpi/common/include/linux/soc/mediatek/mtk_wed.h
+++ sys/compat/linuxkpi/common/include/linux/soc/mediatek/mtk_wed.h
@@ -14,6 +14,7 @@ struct mtk_wed_device {
 
 #define	WED_WO_STA_REC	0x6
 
+#if defined(CONFIG_NET_MEDIATEK_SOC_WED)
 #define	mtk_wed_device_start(_dev, _mask)		do { pr_debug("%s: TODO\n", __func__); } while(0)
 #define	mtk_wed_device_detach(_dev)			do { pr_debug("%s: TODO\n", __func__); } while(0)
 #define	mtk_wed_device_irq_get(_dev, _mask)		0
@@ -29,7 +30,6 @@ struct mtk_wed_device {
 static inline bool
 mtk_wed_device_active(struct mtk_wed_device *dev __unused)
 {
-
 	pr_debug("%s: TODO\n", __func__);
 	return (false);
 }
@@ -41,5 +41,31 @@ mtk_wed_get_rx_capa(struct mtk_wed_device *dev __unused)
 	pr_debug("%s: TODO\n", __func__);
 	return (false);
 }
+
+#else	/* ! CONFIG_NET_MEDIATEK_SOC_WED */
+
+#define	mtk_wed_device_start(_dev, _mask)		do { } while(0)
+#define	mtk_wed_device_detach(_dev)			do { } while(0)
+#define	mtk_wed_device_irq_get(_dev, _mask)		0
+#define	mtk_wed_device_irq_set_mask(_dev, _mask)	do { } while(0)
+#define	mtk_wed_device_update_msg(_dev, _id, _msg, _len)	-ENODEV
+#define	mtk_wed_device_dma_reset(_dev)			do { } while (0)
+#define	mtk_wed_device_ppe_check(_dev, _skb, _reason, _entry) do { } while (0)
+#define	mtk_wed_device_stop(_dev)			do { } while(0)
+#define	mtk_wed_device_start_hw_rro(_dev, _mask, _b)	do { } while(0)
+#define	mtk_wed_device_setup_tc(_dev, _ndev, _type, _tdata)	-EOPNOTSUPP
+
+static inline bool
+mtk_wed_device_active(struct mtk_wed_device *dev __unused)
+{
+	return (false);
+}
+
+static inline bool
+mtk_wed_get_rx_capa(struct mtk_wed_device *dev __unused)
+{
+	return (false);
+}
+#endif	/* CONFIG_NET_MEDIATEK_SOC_WED */
 
 #endif	/* _LINUXKPI_LINUX_SOC_MEDIATEK_MTK_WED_H */
