freebsd/freebsd-src

commit ad704a34bc2c77a0d8a132b96aa0134363a0a5c5

Use syscall_helper_register(9) rather than syscall_register().
The usage is simpler, documented, and more common.

Reviewed by:	cem
Sponsored by:	DARPA, AFRL
Differential Revision:	https://reviews.freebsd.org/D14227

sys/kgssapi/gss_impl.c
@@ -56,39 +56,33 @@ MALLOC_DEFINE(M_GSSAPI, "GSS-API", "GSS-API");
 /*
  * Syscall hooks
  */
-static int gssd_syscall_offset = SYS_gssd_syscall;
-static struct sysent gssd_syscall_prev_sysent;
-MAKE_SYSENT(gssd_syscall);
-static bool_t gssd_syscall_registered = FALSE;
+static struct syscall_helper_data gssd_syscalls[] = {
+	SYSCALL_INIT_HELPER(gssd_syscall),
+	SYSCALL_INIT_LAST
+};
 
 struct kgss_mech_list kgss_mechs;
 CLIENT *kgss_gssd_handle;
 struct mtx kgss_gssd_lock;
 
-static void
-kgss_init(void *dummy)
+static int
+kgss_load(void)
 {
 	int error;
 
 	LIST_INIT(&kgss_mechs);
-	error = syscall_register(&gssd_syscall_offset, &gssd_syscall_sysent,
-	    &gssd_syscall_prev_sysent, SY_THR_STATIC_KLD);
-	if (error)
-		printf("Can't register GSSD syscall\n");
-	else
-		gssd_syscall_registered = TRUE;
+	error = syscall_helper_register(gssd_syscalls, SY_THR_STATIC_KLD);
+	if (error != 0)
+		return (error);
+	return (0);
 }
-SYSINIT(kgss_init, SI_SUB_LOCK, SI_ORDER_FIRST, kgss_init, NULL);
 
 static void
-kgss_uninit(void *dummy)
+kgss_unload(void)
 {
 
-	if (gssd_syscall_registered)
-		syscall_deregister(&gssd_syscall_offset,
-		    &gssd_syscall_prev_sysent);
+	syscall_helper_unregister(gssd_syscalls);
 }
-SYSUNINIT(kgss_uninit, SI_SUB_LOCK, SI_ORDER_FIRST, kgss_uninit, NULL);
 
 int
 sys_gssd_syscall(struct thread *td, struct gssd_syscall_args *uap)
@@ -293,6 +287,8 @@ kgssapi_modevent(module_t mod, int type, void *data)
 
 	switch (type) {
 	case MOD_LOAD:
+		if (error != 0)
+			return (error);
 		rpc_gss_entries.rpc_gss_refresh_auth = rpc_gss_refresh_auth;
 		rpc_gss_entries.rpc_gss_secfind = rpc_gss_secfind;
 		rpc_gss_entries.rpc_gss_secpurge = rpc_gss_secpurge;
@@ -317,8 +313,11 @@ kgssapi_modevent(module_t mod, int type, void *data)
 		rpc_gss_entries.rpc_gss_svc_max_data_length =
 		    rpc_gss_svc_max_data_length;
 		mtx_init(&kgss_gssd_lock, "kgss_gssd_lock", NULL, MTX_DEF);
+		error = kgss_load();
 		break;
 	case MOD_UNLOAD:
+		kgss_unload();
+		mtx_destroy(&kgss_gssd_lock);
 		/*
 		 * Unloading of the kgssapi module is not currently supported.
 		 * If somebody wants this, we would need to keep track of

commit 6901376f59b870ddbb0d922e3455a388333cd460

kgssapi: Remove broken MOD_UNLOAD code
The module panicked at unload with "recursing but non-recursive rw".
There is a comment that "Unloading of the kgssapi module is not
currently supported" and the MOD_UNLOAD case falls through to returning
EOPNOTSUPP anyway.  Just #if 0 the code in the unload path, leaving it
as a hint in case someone implements unload support later on.

PR:		291249
Reviewed by:	rmacklem
Fixes: ad704a34bc2c ("Use syscall_helper_register(9) rather than syscall_register().")
Sponsored by:	The FreeBSD Foundation
Differential Revision: https://reviews.freebsd.org/D53949

sys/kgssapi/gss_impl.c
@@ -88,6 +88,7 @@ kgss_load(void)
 	return (0);
 }
 
+#if 0
 static void
 kgss_unload(void)
 {
@@ -96,6 +97,7 @@ kgss_unload(void)
 	clnt_destroy(KGSS_VNET(kgss_gssd_handle));
 	KGSS_CURVNET_RESTORE();
 }
+#endif
 
 int
 kgss_oid_equal(const gss_OID oid1, const gss_OID oid2)
@@ -297,8 +299,10 @@ kgssapi_modevent(module_t mod, int type, void *data)
 		error = kgss_load();
 		break;
 	case MOD_UNLOAD:
+#if 0
 		kgss_unload();
 		mtx_destroy(&kgss_gssd_lock);
+#endif
 		/*
 		 * Unloading of the kgssapi module is not currently supported.
 		 * If somebody wants this, we would need to keep track of