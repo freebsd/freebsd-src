5c025978fc3649730329994eecc56ada119e6717
Jason A. Harmening <jah@FreeBSD.org>
2025-11-28 01:36:24 -0600

    unionfs: Implement VOP_GETLOWVNODE
    This function returns the vnode that will be used to resolve the
    access type specified in the 'flags' argument, and is useful for
    optimal behavior of vn_copy_file_range(). While most filesystems
    can simply use the default implementation which returns the passed-
    in vnode, unionfs (like nullfs) ideally should resolve the access
    request to whichever base layer vnode will be used for the I/O.
    
    For unionfs, write accesses must be resolved through the upper vnode,
    while read accesses will be resolved through the upper vnode if
    present or the lower vnode otherwise.  Provide a simple
    unionfs_getlowvnode() implementation that reflects this policy.
    
    Reviewed by:	kib, olce
    Tested by:	pho
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D53988

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -906,6 +906,7 @@ int	vop_stdunp_bind(struct vop_unp_bind_args *ap);
 int	vop_stdunp_connect(struct vop_unp_connect_args *ap);
 int	vop_stdunp_detach(struct vop_unp_detach_args *ap);
 int	vop_stdadd_writecount_nomsync(struct vop_add_writecount_args *ap);
+int	vop_stdcopy_file_range(struct vop_copy_file_range_args *ap);
 int	vop_eopnotsupp(struct vop_generic_args *ap);
 int	vop_ebadf(struct vop_generic_args *ap);
 int	vop_einval(struct vop_generic_args *ap);
