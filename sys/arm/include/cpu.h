b033e3684fd14c798405420449eb55e1d4f9df1e
Warner Losh <imp@FreeBSD.org>
2025-12-02 22:29:42 -0700

    armv7: Tweak some armv6 removals
    Fixes: 42421307b115 (sys: remove armv6/6.1 support from debug monitor)
    Suggested by:	jhb
    Sponsored by:	Netflix

--- sys/arm/include/cpu.h
+++ sys/arm/include/cpu.h
@@ -186,7 +186,6 @@ _RF0(cp14_dbgoslsr_get, CP14_DBGOSLSR(%0))
 _RF0(cp14_dbgosdlr_get, CP14_DBGOSDLR(%0))
 _RF0(cp14_dbgdscrint_get, CP14_DBGDSCRint(%0))
 
-_WF1(cp14_dbgdscr_v6_set, CP14_DBGDSCRext_V6(%0))
 _WF1(cp14_dbgdscr_v7_set, CP14_DBGDSCRext_V7(%0))
 _WF1(cp14_dbgvcr_set, CP14_DBGVCR(%0))
 _WF1(cp14_dbgoslar_set, CP14_DBGOSLAR(%0))
