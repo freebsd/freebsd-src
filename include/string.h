aa285c42ed8927aba92344bb00a9a1a14dcf24cc
Konstantin Belousov <kib@FreeBSD.org>
2025-12-04 01:56:26 +0200

    libc/string: add strdupa(3) and strndupa(3)
    Reviewed by:	markj
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D54066

--- include/string.h
+++ include/string.h
@@ -154,6 +154,34 @@ void	 swab(const void * __restrict, void * __restrict, ssize_t);
 
 int	 timingsafe_bcmp(const void *, const void *, size_t);
 int	 timingsafe_memcmp(const void *, const void *, size_t);
+
+#if __has_builtin(__builtin_alloca)
+#define	strdupa(_Str) (__extension__({				\
+	const char *_Str1;					\
+	size_t _Len;						\
+	char *_Copy;						\
+								\
+	_Str1 = (_Str);						\
+	_Len = strlen(_Str1) + 1;				\
+	_Copy = (char *)__builtin_alloca(_Len);			\
+	memcpy(_Copy, _Str1, _Len);				\
+	_Copy;							\
+}))
+
+#define	strndupa(_Str, _Maxlen) (__extension__({		\
+	const char *_Str1;					\
+	char *_Copy;						\
+	size_t _Len;						\
+								\
+	_Str1 = (_Str);						\
+	_Len = strnlen((_Str1), (_Maxlen));			\
+	_Copy = __builtin_alloca(_Len + 1);			\
+	(void)memcpy(_Copy, _Str1, _Len);			\
+	_Copy[_Len] = '\0';					\
+	_Copy;							\
+}))
+#endif
+
 #endif /* __BSD_VISIBLE */
 
 #if __POSIX_VISIBLE >= 200112 || defined(_XLOCALE_H_)
