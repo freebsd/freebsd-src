d080b30da6d6d2256414822e7751b029bfc03b0e
Warner Losh <imp@FreeBSD.org>
2025-12-03 21:05:06 -0700

    nvme: Fix devd logging of SMART_ERROR
    Remove extra arg so that we have type=SMART_ERROR rather than an invalid
    message. Also log all aens.
    
    Sponsored by:		Netflix

--- sys/dev/nvme/nvme_ctrlr.c
+++ sys/dev/nvme/nvme_ctrlr.c
@@ -677,7 +677,7 @@ nvme_ctrlr_log_critical_warnings(struct nvme_controller *ctrlr,
 		nvme_printf(ctrlr, "SMART WARNING: unknown critical warning(s): state = 0x%02x\n",
 		    state & NVME_CRIT_WARN_ST_RESERVED_MASK);
 
-	nvme_ctrlr_devctl(ctrlr, "critical", "SMART_ERROR", "state=0x%02x", state);
+	nvme_ctrlr_devctl(ctrlr, "SMART_ERROR", "state=0x%02x", state);
 }
 
 static void
@@ -1142,6 +1142,10 @@ nvme_ctrlr_aer_task(void *arg, int pending)
 		nvme_ctrlr_construct_and_submit_aer(ctrlr, aer);
 		goto out;
 	}
+
+	nvme_ctrlr_devctl(ctrlr, "aen", "type=0x%x info=0x%x page=0x%x",
+	    NVMEV(NVME_ASYNC_EVENT_TYPE, aer->cpl.cdw0),
+	    NVMEV(NVME_ASYNC_EVENT_INFO, aer->cpl.cdw0), aer->log_page_id);
 
 	aer->log_page_size = 0;
 	len = nvme_ctrlr_get_log_page_size(aer->ctrlr, aer->log_page_id);
