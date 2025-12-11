95d8576d8cc725ce2536d85335523c350b0bf69c
Oskar Holmlund <oh@FreeBSD.org>
2025-12-11 18:19:44 +0100

    make_dtb.sh: add include path
    The device tree include file for TI TPS65* is in a
    relative path to the source for example:
    device-tree/src/arm/ti/omap/am335x-bone-common.dtsi#n305
    device-tree/src/arm/rockchip/rk3066a-marsboard.dts#n183
    
    This patch gets the dts path and adds that as an include path
    for the device tree compiler.
    
    Approved by: manu (mentor)
    Differential revision: https://reviews.freebsd.org/D53887

--- sys/tools/fdt/make_dtb.sh
+++ sys/tools/fdt/make_dtb.sh
@@ -21,6 +21,7 @@ fi
 
 for d in ${dts}; do
     dtb="${dtb_path}/$(basename "$d" .dts).dtb"
+    dts_path=`dirname $d`
     ${CPP} -P -x assembler-with-cpp -I "$S/dts/include" -I "$S/contrib/device-tree/include" -I "$S/dts/${MACHINE}" -I "$S/contrib/device-tree/src/${MACHINE}" -I "$S/contrib/device-tree/src/" -include "$d" -include "$S/dts/freebsd-compatible.dts" /dev/null |
-	${DTC} -@ -O dtb -o "$dtb" -b 0 -p 1024 -i "$S/dts/${MACHINE}" -i "$S/contrib/device-tree/src/${MACHINE}" -i "$S/contrib/device-tree/src/"
+	${DTC} -@ -O dtb -o "$dtb" -b 0 -p 1024 -i "$S/dts/${MACHINE}" -i "$S/contrib/device-tree/src/${MACHINE}" -i "$S/contrib/device-tree/src/" -i "$dts_path"
 done
