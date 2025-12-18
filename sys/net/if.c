6bb78e7791fc2597c1f900984f8e42a4f9710cf2
Gleb Smirnoff <glebius@FreeBSD.org>
2025-12-18 08:47:31 -0800

    net: split ifnet_arrival_event into two events
    Run the original ifnet_arrival_event before linking the interface.
    Otherwise there is a race window when interface is already visible, but
    not all of the protocols have completed their attach. Provide a new event
    handler ifnet_attached_event, that is executed when the inteface is fully
    visible.  Use it in route(4) socket and netlink(4) to announce new
    interface to the userland. Properly document the ifnet events in if_var.h.
    
    Reviewed by:		zlei, melifaro
    Differential Revision:	https://reviews.freebsd.org/D54085

--- sys/net/if.c
+++ sys/net/if.c
@@ -932,10 +932,9 @@ if_attach_internal(struct ifnet *ifp, bool vmove)
 
 	if (domain_init_status >= 2)
 		if_attachdomain1(ifp);
-
-	if_link_ifnet(ifp);
-
 	EVENTHANDLER_INVOKE(ifnet_arrival_event, ifp);
+	if_link_ifnet(ifp);
+	EVENTHANDLER_INVOKE(ifnet_attached_event, ifp);
 	if (IS_DEFAULT_VNET(curvnet))
 		devctl_notify("IFNET", ifp->if_xname, "ATTACH", NULL);
 }
