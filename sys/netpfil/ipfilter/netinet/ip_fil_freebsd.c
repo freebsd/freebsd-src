f60a41543430c764a276af5dc30b9bcf2b8c25d5
Cy Schubert <cy@FreeBSD.org>
2025-12-18 18:37:32 -0800

    Revert "ipfilter: Add missing kenv fetch"
    The commit depends on d9788eabffa4b67fc534685fc3d9b8e3334af196 which
    has not been merged yet.
    
    This reverts commit 5ece89c74efd8ab836459af5bc6720af904ad1d4.

--- sys/netpfil/ipfilter/netinet/ip_fil_freebsd.c
+++ sys/netpfil/ipfilter/netinet/ip_fil_freebsd.c
@@ -1457,6 +1457,4 @@ ipf_fbsd_kenv_get(ipf_main_softc_t *softc)
 {
 	TUNABLE_INT_FETCH("net.inet.ipf.large_nat",
 		&softc->ipf_large_nat);
-	TUNABLE_INT_FETCH("net.inet.ipf.jail_allowed",
-		&softc->ipf_jail_allowed);
 }
