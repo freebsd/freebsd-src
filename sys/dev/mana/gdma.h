d0a2bd2765b365c4be9b17c29306f848953e55a5
Wei Hu <whu@FreeBSD.org>
2025-12-23 07:00:38 +0000

    mana: support jumbo packet size
    Remove the restriction of up to 4k packet size. Now the driver
    supports up to MJUM16BYTES size packets as long as hardware
    supporting it.
    
    Tested on VMs in Azure.
    
    Tested by:	whu
    MFC after:	1 week
    Sponsored by:	Microsoft

--- sys/dev/mana/gdma.h
+++ sys/dev/mana/gdma.h
@@ -193,6 +193,9 @@ struct gdma_general_req {
 }; /* HW DATA */
 
 #define GDMA_MESSAGE_V1 1
+#define GDMA_MESSAGE_V2 2
+#define GDMA_MESSAGE_V3 3
+#define GDMA_MESSAGE_V4 4
 
 struct gdma_general_resp {
 	struct gdma_resp_hdr	hdr;
@@ -408,6 +411,9 @@ struct gdma_context {
 	unsigned int		num_msix_usable;
 	struct gdma_resource	msix_resource;
 	struct gdma_irq_context	*irq_contexts;
+
+	/* L2 MTU */
+	uint16_t		adapter_mtu;
 
 	/* This maps a CQ index to the queue structure. */
 	unsigned int		max_num_cqs;
