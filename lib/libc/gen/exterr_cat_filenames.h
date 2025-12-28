3254e030ecd82f84cc24b4ad479a2ddcfa165cb9
Konstantin Belousov <kib@FreeBSD.org>
2025-12-28 16:23:43 +0200

    Add automatically generated file libc/gen/exterr_cat_filenames.h
    MFC after:	1 week

---
+++ lib/libc/gen/exterr_cat_filenames.h
@@ -0,0 +1,17 @@
+/*
+ * Automatically @generated, use
+ * tools/build/make_libc_exterr_cat_filenames.sh
+ */
+	[EXTERR_CAT_FUSE_DEVICE] = "fs/fuse/fuse_device.c",
+	[EXTERR_CAT_FUSE_VFS] = "fs/fuse/fuse_vfsops.c",
+	[EXTERR_CAT_FUSE_VNOPS] = "fs/fuse/fuse_vnops.c",
+	[EXTERR_CAT_GEOM] = "geom/geom_subr.c",
+	[EXTERR_CAT_GEOMVFS] = "geom/geom_vfs.c",
+	[EXTERR_CAT_FILEDESC] = "kern/kern_descrip.c",
+	[EXTERR_CAT_INOTIFY] = "kern/vfs_inotify.c",
+	[EXTERR_CAT_GENIO] = "kern/sys_generic.c",
+	[EXTERR_CAT_VFSBIO] = "kern/vfs_bio.c",
+	[EXTERR_CAT_VFSSYSCALL] = "kern/vfs_syscalls.c",
+	[EXTERR_CAT_BRIDGE] = "net/if_bridge.c",
+	[EXTERR_CAT_SWAP] = "vm/swap_pager.c",
+	[EXTERR_CAT_MMAP] = "vm/vm_mmap.c",
