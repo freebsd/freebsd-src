5ae5aab15f80570acdda4c64975d2d6fec59ad98
Konstantin Belousov <kib@FreeBSD.org>
2026-01-01 00:26:38 +0200

    vm/vm_object.h: clarify the OBJ_ONEMAPPING semantic
    Reviewed by:	markj
    Sponsored by:	The FreeBSD Foundation
    MFC after:	3 days
    Differential revision:	https://reviews.freebsd.org/D54459

--- sys/vm/vm_object.h
+++ sys/vm/vm_object.h
@@ -197,8 +197,9 @@ struct vm_object {
 #define	OBJ_SPLIT	0x00000400	/* object is being split */
 #define	OBJ_COLLAPSING	0x00000800	/* Parent of collapse. */
 #define	OBJ_COLORED	0x00001000	/* pg_color is defined */
-#define	OBJ_ONEMAPPING	0x00002000	/* One USE (a single, non-forked)
-					   mapping flag */
+#define	OBJ_ONEMAPPING	0x00002000	/* Each page is allowed to have at most
+					   one managed mapping, all in the same
+					   vm_map */
 #define	OBJ_PAGERPRIV1	0x00004000	/* Pager private */
 #define	OBJ_PAGERPRIV2	0x00008000	/* Pager private */
 #define	OBJ_SYSVSHM	0x00010000	/* SysV SHM */
