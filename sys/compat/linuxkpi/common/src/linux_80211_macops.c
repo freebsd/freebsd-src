9592f563c36bd207d98f1b3a13839b88d5760d97
Bjoern A. Zeeb <bz@FreeBSD.org>
2026-01-03 20:10:00 +0000

    LinuxKPI: 802.11: split (*bss_info_changed) up for more modern drivers
    With the advent of MLO some of the updates (*bss_info_changed) would
    have done are not per-link.  This had (*vif_cfg_changed) and
    (*link_conf_changed) introduced which are used by iwlwifi, rtw89,
    select mt76 drivers, and ath12k currently it seems.
    A driver normally only supports on or the other set.
    
    Factor out the call to (*bss_info_changed) into an internal function.
    There split the options up depending on whether they are for the
    vif or a link and leave a fallback to (*bss_info_changed) for older
    drivers.
    
    Add the mac80211 ops implementations for the two new calls along with
    a currently unused backup option for (*bss_info_changed) for each
    as I assume we will eventually call the directly rather than from the
    internal wrapper function.
    
    Sponsored by:	The FreeBSD Foundation
    MFC after:	3 days

--- sys/compat/linuxkpi/common/src/linux_80211_macops.c
+++ sys/compat/linuxkpi/common/src/linux_80211_macops.c
@@ -546,24 +546,80 @@ lkpi_80211_mo_remove_chanctx(struct ieee80211_hw *hw,
 }
 
 void
-lkpi_80211_mo_bss_info_changed(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
-    struct ieee80211_bss_conf *conf, uint64_t changed)
+lkpi_80211_mo_vif_cfg_changed(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
+    uint64_t vif_cfg_bits, bool fallback)
 {
 	struct lkpi_hw *lhw;
+
+	might_sleep();
+	/* XXX-FINISH all callers for lockdep_assert_wiphy(hw->wiphy); */
+
+	lhw = HW_TO_LHW(hw);
+	if (lhw->ops->vif_cfg_changed == NULL &&
+	    lhw->ops->bss_info_changed == NULL)
+		return;
+
+	if (vif_cfg_bits == 0)
+		return;
+
+	LKPI_80211_TRACE_MO("hw %p vif %p vif_cfg_bits %#jx", hw, vif, (uintmax_t)vif_cfg_bits);
+	if (lhw->ops->link_info_changed != NULL)
+		lhw->ops->vif_cfg_changed(hw, vif, vif_cfg_bits);
+	else if (fallback)
+		lhw->ops->bss_info_changed(hw, vif, &vif->bss_conf, vif_cfg_bits);
+}
+
+void
+lkpi_80211_mo_link_info_changed(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
+    struct ieee80211_bss_conf *conf, uint64_t link_info_bits, uint8_t link_id,
+    bool fallback)
+{
+	struct lkpi_hw *lhw;
+
+	might_sleep();
+	/* XXX-FINISH all callers for lockdep_assert_wiphy(hw->wiphy); */
 
 	lhw = HW_TO_LHW(hw);
 	if (lhw->ops->link_info_changed == NULL &&
 	    lhw->ops->bss_info_changed == NULL)
 		return;
 
-	if (changed == 0)
+	if (link_info_bits == 0)
 		return;
 
-	LKPI_80211_TRACE_MO("hw %p vif %p conf %p changed %#jx", hw, vif, conf, (uintmax_t)changed);
+	if (!ieee80211_vif_link_active(vif, link_id))
+		return;
+
+	LKPI_80211_TRACE_MO("hw %p vif %p conf %p link_info_bits %#jx", hw, vif, conf, (uintmax_t)link_info_bits);
 	if (lhw->ops->link_info_changed != NULL)
-		lhw->ops->link_info_changed(hw, vif, conf, changed);
-	else
-		lhw->ops->bss_info_changed(hw, vif, conf, changed);
+		lhw->ops->link_info_changed(hw, vif, conf, link_info_bits);
+	else if (fallback)
+		lhw->ops->bss_info_changed(hw, vif, conf, link_info_bits);
+}
+
+/*
+ * This is basically obsolete but one caller.
+ * The functionality is now split between lkpi_80211_mo_link_info_changed() and
+ * lkpi_80211_mo_vif_cfg_changed().  Those functions have a flag whether to call
+ * the (*bss_info_changed) fallback or not.  See lkpi_bss_info_change().
+ */
+void
+lkpi_80211_mo_bss_info_changed(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
+    struct ieee80211_bss_conf *conf, uint64_t bss_changed)
+{
+	struct lkpi_hw *lhw;
+
+	/* XXX-FINISH all callers for lockdep_assert_wiphy(hw->wiphy); */
+
+	lhw = HW_TO_LHW(hw);
+	if (lhw->ops->bss_info_changed == NULL)
+		return;
+
+	if (bss_changed == 0)
+		return;
+
+	LKPI_80211_TRACE_MO("hw %p vif %p conf %p changed %#jx", hw, vif, conf, (uintmax_t)bss_changed);
+	lhw->ops->bss_info_changed(hw, vif, conf, bss_changed);
 }
 
 int
