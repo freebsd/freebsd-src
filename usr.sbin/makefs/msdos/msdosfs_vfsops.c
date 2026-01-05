8f9e07cc23817e3e9dd715c67e7178c5154ccea4
Jose Luis Duran <jlduran@FreeBSD.org>
2026-01-05 20:35:37 +0000

    makefs: Fix typo 's/mirrorring/mirroring/'
    MFC after:	1 week

--- usr.sbin/makefs/msdos/msdosfs_vfsops.c
+++ usr.sbin/makefs/msdos/msdosfs_vfsops.c
@@ -163,7 +163,7 @@ m_msdosfs_mount(struct m_vnode *devvp)
 		pmp->pm_fatdiv = 1;
 		pmp->pm_FATsecs = getulong(b710->bpbBigFATsecs);
 
-		/* mirrorring is enabled if the FATMIRROR bit is not set */
+		/* mirroring is enabled if the FATMIRROR bit is not set */
 		if ((getushort(b710->bpbExtFlags) & FATMIRROR) == 0)
 			pmp->pm_flags |= MSDOSFS_FATMIRROR;
 		else
