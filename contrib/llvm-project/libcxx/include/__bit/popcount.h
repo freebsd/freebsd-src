58970241b77b52c391f93d341a101425dc0875fd
Dimitry Andric <dimitry@andric.com>
2026-01-05 21:14:13 +0100

    libcxx-compat: revert llvmorg-21-init-8400-g703cfe745b96:
      [libc++] Replace __libcpp_popcount by __builtin_popcountg (#133937)
    
      `__libcpp_popcount` was previously used as a fallback for `__builtin_popcountg` to ensure compatibility with older compilers (Clang 18 and earlier), as `__builtin_popcountg` became available in Clang 19. Now that support for Clang 18 has been officially dropped in #130142, we can now safely  replace all instances of `__libcpp_popcount` with `__builtin_popcountg` and eliminate the fallback logic.
    
    This is part of making libc++ 21 build with clang 18.
    
    PR:		292067
    MFC after:	1 month

--- contrib/llvm-project/libcxx/include/__bit/popcount.h
+++ contrib/llvm-project/libcxx/include/__bit/popcount.h
@@ -11,6 +11,7 @@
 
 #include <__config>
 #include <__type_traits/integer_traits.h>
+#include <limits>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #  pragma GCC system_header
@@ -22,9 +23,37 @@ _LIBCPP_PUSH_MACROS
 _LIBCPP_BEGIN_NAMESPACE_STD
 
 template <class _Tp>
+[[__nodiscard__]] _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR int __popcount_impl(_Tp __t) _NOEXCEPT {
+  if _LIBCPP_CONSTEXPR (sizeof(_Tp) <= sizeof(unsigned int)) {
+    return __builtin_popcount(static_cast<unsigned int>(__t));
+  } else if _LIBCPP_CONSTEXPR (sizeof(_Tp) <= sizeof(unsigned long)) {
+    return __builtin_popcountl(static_cast<unsigned long>(__t));
+  } else if _LIBCPP_CONSTEXPR (sizeof(_Tp) <= sizeof(unsigned long long)) {
+    return __builtin_popcountll(static_cast<unsigned long long>(__t));
+  } else {
+#if _LIBCPP_STD_VER == 11
+    return __t != 0 ? __builtin_popcountll(static_cast<unsigned long long>(__t)) +
+                          std::__popcount_impl<_Tp>(__t >> numeric_limits<unsigned long long>::digits)
+                    : 0;
+#else
+    int __ret = 0;
+    while (__t != 0) {
+      __ret += __builtin_popcountll(static_cast<unsigned long long>(__t));
+      __t >>= std::numeric_limits<unsigned long long>::digits;
+    }
+    return __ret;
+#endif
+  }
+}
+
+template <class _Tp>
 [[__nodiscard__]] _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR int __popcount(_Tp __t) _NOEXCEPT {
   static_assert(__is_unsigned_integer_v<_Tp>, "__popcount only works with unsigned types");
+#if __has_builtin(__builtin_popcountg) // TODO (LLVM 21): This can be dropped once we only support Clang >= 19.
   return __builtin_popcountg(__t);
+#else
+  return std::__popcount_impl(__t);
+#endif
 }
 
 #if _LIBCPP_STD_VER >= 20
