fb4f7b4d46464629f79a8cfeaad5f3e89a2b42a4
Kristof Provost <kp@FreeBSD.org>
2026-01-06 15:13:07 +0100

    pf tests: basic state limiters test case
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

---
+++ tests/sys/netpfil/pf/limiters.sh
@@ -0,0 +1,81 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright (c) 2026 Rubicon Communications, LLC (Netgate)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+. $(atf_get_srcdir)/utils.subr
+
+atf_test_case "state_basic" "cleanup"
+state_basic_head()
+{
+	atf_set descr 'Basic state limiter test'
+	atf_set require.user root
+}
+
+state_basic_body()
+{
+	pft_init
+
+	epair=$(vnet_mkepair)
+
+	ifconfig ${epair}a 192.0.2.2/24 up
+
+	vnet_mkjail alcatraz ${epair}b
+	jexec alcatraz ifconfig ${epair}b 192.0.2.1/24 up
+
+	# Sanity check
+	atf_check -s exit:0 -o ignore \
+	    ping -c 1 192.0.2.1
+
+	jexec alcatraz pfctl -e
+	# Allow up to one ICMP state.
+	pft_set_rules alcatraz \
+	    "set timeout icmp.error 120" \
+	    "state limiter \"server\" id 1 limit 1" \
+	    "block in proto icmp" \
+	    "pass in proto icmp state limiter \"server\""
+
+	atf_check -s exit:0 -o ignore \
+	    ping -c 2 192.0.2.1
+
+	# This should now fail
+	atf_check -s exit:2 -o ignore \
+	    ping -c 2 192.0.2.1
+
+	jexec alcatraz pfctl -sLimiterStates
+	hardlim=$(jexec alcatraz pfctl -sLimiterStates | awk 'NR>1 { print $5; }')
+	if [ $hardlim -eq 0 ]; then
+		atf_fail "Hard limit not incremented"
+	fi
+}
+
+state_basic_cleanup()
+{
+	pft_cleanup
+}
+
+atf_init_test_cases()
+{
+	atf_add_test_case "state_basic"
+}
