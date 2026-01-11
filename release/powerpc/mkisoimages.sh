f0a7f9573193fcf92bbda9b643ad7fe21d51e566
Adrian Chadd <adrian@FreeBSD.org>
2026-01-10 19:59:41 -0800

    powerpc: fix release image building for Apple partitions
    awk changed somewhere between 14 and 15 and it stopped accepting
    a hexadecimal number as its input - it will always return 0.
    This results in a very badly written apple boot block.
    
    So just remove it; do the math in shell.
    
    PR:		kern/292341
    Differential Revision:	https://reviews.freebsd.org/D54639
    Reviewed by:	imp
    MFC after:	1 week

--- release/powerpc/mkisoimages.sh
+++ release/powerpc/mkisoimages.sh
@@ -75,7 +75,7 @@ if [ -n "$bootable" ]; then
 	# Apple boot code
 	uudecode -p "`dirname "$0"`/hfs-boot.bz2.uu" | bunzip2 > $BOOTBLOCK
 	OFFSET=$(hd $BOOTBLOCK | grep 'Loader START' | cut -f 1 -d ' ')
-	OFFSET=0x$(echo 0x$OFFSET | awk '{printf("%x\n",$1/512);}')
+	OFFSET=$((0x${OFFSET} / 512))
 	dd if="$BASEBITSDIR/boot/loader" of=$BOOTBLOCK seek=$OFFSET conv=notrunc
 
 	bootable="-o bootimage=macppc;$BOOTBLOCK -o no-emul-boot"
