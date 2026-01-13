0fa7b3bee72e7cfedabe371c7082fbf44508ec05
Bjoern A. Zeeb <bz@FreeBSD.org>
2026-01-13 20:33:27 +0000

    riscv: add (a dummy) acpica_machdep.h
    We have reports that iwlwifi(4) works on RISC-V.  While we can turn off
    full ACPI-specific files easily, intermittent code still relies on the
    header files to be present.  In order to not need to completely #ifdef
    everything out we want to include acpi.h from LinuxKPI and as a result
    need this file.  With this the iwlwifi(4) code compiles just fine and
    will do the right thing (given the functional ACPI parts are disabled/
    unavailable).
    
    Sponsored by:	The FreeBSD Foundation
    MFC after:	3 days
    Reviewed by:	mhorne, emaste
    Differential Revision: https://reviews.freebsd.org/D54691

---
+++ sys/riscv/include/acpica_machdep.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2026 The FreeBSD Foundation
+ *
+ * This software was developed by Björn Zeeb
+ * under sponsorship from the FreeBSD Foundation.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef __ACPICA_MACHDEP_H__
+#define	__ACPICA_MACHDEP_H__
+
+/*
+ * This is a placeholder until full ACPI support for RISC-V emerges.
+ * With is we can include acpi.h from LinuxKPI and avoid (major) local changes
+ * to compile drivers otherwise fine on RISC-V.
+ */
+
+#endif /* __ACPICA_MACHDEP_H__ */
