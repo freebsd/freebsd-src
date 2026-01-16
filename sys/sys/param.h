66aa9176a742da72d572867cee1049492ff178ab
Kyle Evans <kevans@FreeBSD.org>
2026-01-16 03:06:38 -0600

    Bump __FreeBSD_version for MAC changes

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -74,7 +74,7 @@
  * cannot include sys/param.h and should only be updated here.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1600008
+#define __FreeBSD_version 1600009
 
 /*
  * __FreeBSD_kernel__ indicates that this system uses the kernel of FreeBSD,
