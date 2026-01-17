freebsd/freebsd-src

commit 3daae1ac1d82ecdcd855101bab5206e914b12350

ipfw: create a bpf tap point for every log rule
Dynamically allocate bpf tap points for every rule that has "log".
The name is "ipfw%u", where %u is substituted to the rule number.
The default catch all "ipfw0" tap still exists for compatibility
and it will catch packets in case if there are no bpf listeners
on a per-rule tap.

Reviewed by:		ae
Differential Revision:	https://reviews.freebsd.org/D53877

sys/netpfil/ipfw/ip_fw_bpf.c
@@ -32,13 +32,14 @@
 #include <sys/lock.h>
 #include <sys/rwlock.h>
 #include <sys/socket.h>
+#include <sys/tree.h>
 #include <net/ethernet.h>
 #include <net/if.h>
 #include <net/if_pflog.h>
 #include <net/vnet.h>
 #include <net/bpf.h>
 
-#include <netinet/in.h>
+#include <netinet/ip.h>
 #include <netinet/ip_fw.h>
 #include <netinet/ip_var.h>
 #include <netpfil/ipfw/ip_fw_private.h>
@@ -54,48 +55,114 @@ static const struct bif_methods bpf_ipfw_methods = {
 	.bif_chkdir = bpf_ipfw_chkdir,
 };
 
-static const char ipfwname[] = "ipfw0";
-static const char ipfwlogname[] = "ipfwlog0";
+struct ipfw_tap {
+	RB_ENTRY(ipfw_tap)	entry;
+	uint32_t		rule;
+	u_int			refs;
+	struct bpf_if		*bpf;
+	char 			name[sizeof("ipfw4294967295")];
+};
 
-VNET_DEFINE_STATIC(struct bpf_if *, bpf_en10mb);
-VNET_DEFINE_STATIC(struct bpf_if *, bpf_pflog);
-#define	V_bpf_en10mb	VNET(bpf_en10mb)
-#define	V_bpf_pflog	VNET(bpf_pflog)
-
-void
-ipfw_bpf_tap(u_char *pkt, u_int pktlen)
+static int32_t
+tap_compare(const struct ipfw_tap *a, const struct ipfw_tap *b)
 {
-	bpf_tap(V_bpf_en10mb, pkt, pktlen);
+	return ((int32_t)(a->rule/2 - b->rule/2));
 }
+RB_HEAD(tap_tree, ipfw_tap);
+VNET_DEFINE_STATIC(struct tap_tree, tap_tree);
+#define	V_tap_tree	VNET(tap_tree)
+RB_GENERATE_STATIC(tap_tree, ipfw_tap, entry, tap_compare);
+VNET_DEFINE_STATIC(struct ipfw_tap *, default_tap);
+#define	V_default_tap	VNET(default_tap)
 
 void
-ipfw_bpf_mtap(struct mbuf *m)
+ipfw_tap_alloc(uint32_t rule)
 {
-	bpf_mtap(V_bpf_en10mb, m);
-}
+	struct ipfw_tap	*tap, key = { .rule = rule };
+	int n __diagused;
 
-void
-ipfw_bpf_mtap2(void *data, u_int dlen, struct mbuf *m)
-{
-	switch (dlen) {
-	case (ETHER_HDR_LEN):
-		bpf_mtap2(V_bpf_en10mb, data, dlen, m);
-		break;
-	case (PFLOG_HDRLEN):
-		bpf_mtap2(V_bpf_pflog, data, dlen, m);
-		break;
-	default:
-		MPASS(0);
+	tap = RB_FIND(tap_tree, &V_tap_tree, &key);
+	if (tap != NULL) {
+		MPASS(tap->rule == rule);
+		tap->refs++;
+		return;
 	}
+	tap = malloc(sizeof(*tap), M_IPFW, M_WAITOK);
+	tap->rule = rule;
+	tap->refs = 1;
+	/* Note: the default rule logs to "ipfw0". */
+	if (__predict_false(rule == IPFW_DEFAULT_RULE)) {
+		V_default_tap = tap;
+		rule = 0;
+	}
+	n = snprintf(tap->name, sizeof(tap->name), "ipfw%u", rule);
+	MPASS(n > 4 && n < sizeof("ipfw4294967295"));
+	tap->bpf = bpf_attach(tap->name, DLT_EN10MB, PFLOG_HDRLEN,
+	    &bpf_ipfw_methods, NULL);
+	tap = RB_INSERT(tap_tree, &V_tap_tree, tap);
+	MPASS(tap == NULL);
+}
+
+void
+ipfw_tap_free(uint32_t rule)
+{
+
+	struct ipfw_tap	*tap, key = { .rule = rule };
+
+	tap = RB_FIND(tap_tree, &V_tap_tree, &key);
+	MPASS(tap != NULL);
+	if (--tap->refs == 0) {
+		bpf_detach(tap->bpf);
+		RB_REMOVE(tap_tree, &V_tap_tree, tap);
+		free(tap, M_IPFW);
+	}
+}
+
+void
+ipfw_bpf_tap(struct ip_fw_args *args, struct ip *ip, uint32_t rulenum)
+{
+	struct ipfw_tap *tap, key = { .rule = rulenum };
+
+	tap = RB_FIND(tap_tree, &V_tap_tree, &key);
+	MPASS(tap != NULL);
+	if (!bpf_peers_present(tap->bpf))
+		tap = V_default_tap;
+	if (args->flags & IPFW_ARGS_LENMASK) {
+		bpf_tap(tap->bpf, args->mem, IPFW_ARGS_LENGTH(args->flags));
+	} else if (args->flags & IPFW_ARGS_ETHER) {
+		/* layer2, use orig hdr */
+		bpf_mtap(tap->bpf, args->m);
+	} else {
+		char *fakehdr;
+
+		/* Add fake header. Later we will store
+		 * more info in the header.
+		 */
+		if (ip->ip_v == 4)
+			fakehdr = "DDDDDDSSSSSS\x08\x00";
+		else if (ip->ip_v == 6)
+			fakehdr = "DDDDDDSSSSSS\x86\xdd";
+		else
+			/* Obviously bogus EtherType. */
+			fakehdr = "DDDDDDSSSSSS\xff\xff";
+
+		bpf_mtap2(tap->bpf, fakehdr, ETHER_HDR_LEN, args->m);
+	}
+}
+
+VNET_DEFINE_STATIC(struct bpf_if *, bpf_pflog);
+#define	V_bpf_pflog	VNET(bpf_pflog)
+void
+ipfw_pflog_tap(void *data, struct mbuf *m)
+{
+	bpf_mtap2(V_bpf_pflog, data, PFLOG_HDRLEN, m);
 }
 
 void
 ipfw_bpf_init(int first __unused)
 {
-
-	V_bpf_en10mb = bpf_attach(ipfwname, DLT_EN10MB, ETHER_HDR_LEN,
-	    &bpf_ipfw_methods, NULL);
-	V_bpf_pflog = bpf_attach(ipfwlogname, DLT_PFLOG, PFLOG_HDRLEN,
+	ipfw_tap_alloc(IPFW_DEFAULT_RULE);
+	V_bpf_pflog = bpf_attach("ipfwlog0", DLT_PFLOG, PFLOG_HDRLEN,
 	    &bpf_ipfw_methods, NULL);
 }
 
@@ -103,6 +170,6 @@ void
 ipfw_bpf_uninit(int last __unused)
 {
 
-	bpf_detach(V_bpf_en10mb);
+	ipfw_tap_free(IPFW_DEFAULT_RULE);
 	bpf_detach(V_bpf_pflog);
 }

sys/netpfil/ipfw/ip_fw_log.c
@@ -96,31 +96,6 @@
 
 #define	TARG(k, f)	IP_FW_ARG_TABLEARG(chain, k, f)
 
-static void
-ipfw_log_ipfw0(struct ip_fw_args *args, struct ip *ip)
-{
-	if (args->flags & IPFW_ARGS_LENMASK)
-		ipfw_bpf_tap(args->mem, IPFW_ARGS_LENGTH(args->flags));
-	else if (args->flags & IPFW_ARGS_ETHER)
-		/* layer2, use orig hdr */
-		ipfw_bpf_mtap(args->m);
-	else {
-		/* Add fake header. Later we will store
-		 * more info in the header.
-		 */
-		if (ip->ip_v == 4)
-			ipfw_bpf_mtap2("DDDDDDSSSSSS\x08\x00",
-			    ETHER_HDR_LEN, args->m);
-		else if (ip->ip_v == 6)
-			ipfw_bpf_mtap2("DDDDDDSSSSSS\x86\xdd",
-			    ETHER_HDR_LEN, args->m);
-		else
-			/* Obviously bogus EtherType. */
-			ipfw_bpf_mtap2("DDDDDDSSSSSS\xff\xff",
-			    ETHER_HDR_LEN, args->m);
-	}
-}
-
 /*
  * XXX this function alone takes about 2Kbytes of code!
  */
@@ -747,7 +722,8 @@ ipfw_log(struct ip_fw_chain *chain, struct ip_fw *f, u_int hlen,
 	    /* O_LOG is the first action */
 	    ((cmd = ACTION_PTR(f)) && cmd->arg1 == IPFW_LOG_DEFAULT)) {
 		if (V_fw_verbose == 0) {
-			ipfw_log_ipfw0(args, ip);
+			ipfw_bpf_tap(args, ip,
+			    f != NULL ? f->rulenum : IPFW_DEFAULT_RULE);
 			return;
 		}
 		ipfw_log_syslog(chain, f, hlen, args, offset, tablearg, ip);
@@ -761,6 +737,6 @@ ipfw_log(struct ip_fw_chain *chain, struct ip_fw *f, u_int hlen,
 		ipfw_log_rtsock(chain, f, hlen, args, offset, tablearg, eh);
 
 	if (cmd->arg1 & IPFW_LOG_IPFW0)
-		ipfw_log_ipfw0(args, ip);
+		ipfw_bpf_tap(args, ip, f->rulenum);
 }
 /* end of file */

sys/netpfil/ipfw/ip_fw_private.h
@@ -161,9 +161,10 @@ struct ip_fw_chain;
 
 void ipfw_bpf_init(int);
 void ipfw_bpf_uninit(int);
-void ipfw_bpf_tap(u_char *, u_int);
-void ipfw_bpf_mtap(struct mbuf *);
-void ipfw_bpf_mtap2(void *, u_int, struct mbuf *);
+void ipfw_tap_alloc(uint32_t);
+void ipfw_tap_free(uint32_t);
+void ipfw_bpf_tap(struct ip_fw_args *, struct ip *, uint32_t);
+void ipfw_pflog_tap(void *, struct mbuf *);
 void ipfw_log(struct ip_fw_chain *chain, struct ip_fw *f, u_int hlen,
     struct ip_fw_args *args, u_short offset, uint32_t tablearg, struct ip *ip,
     void *eh);

sys/netpfil/ipfw/ip_fw_sockopt.c
@@ -210,6 +210,8 @@ ipfw_free_rule(struct ip_fw *rule)
 	 */
 	if (rule->refcnt > 1)
 		return;
+	if (ACTION_PTR(rule)->opcode == O_LOG)
+		ipfw_tap_free(rule->rulenum);
 	uma_zfree_pcpu(V_ipfw_cntr_zone, rule->cntr);
 	free(rule, M_IPFW);
 }
@@ -2511,6 +2513,9 @@ import_rule_v1(struct ip_fw_chain *chain, struct rule_check_info *ci)
 
 	/* Copy opcodes */
 	memcpy(krule->cmd, urule->cmd, krule->cmd_len * sizeof(uint32_t));
+
+	if (ACTION_PTR(krule)->opcode == O_LOG)
+		ipfw_tap_alloc(krule->rulenum);
 }
 
 /*

sbin/ipfw/ipfw.8
@@ -1,5 +1,5 @@
 .\"
-.Dd December 4, 2025
+.Dd December 10, 2025
 .Dt IPFW 8
 .Os
 .Sh NAME
@@ -704,7 +704,18 @@ option (see below), packets are logged in two ways: if the sysctl variable
 is set to 0 (default), one can use the
 .Xr bpf 4
 tap named
-.Li ipfw0 .
+.Li ipfwXXXXX ,
+where XXXXX is the number of the rule that has the
+.Cm log
+keyword.
+The compatibility
+.Xr bpf 4
+tap named
+.Li ipfw0
+still exists.
+It will catch packets in case if there are no
+.Xr bpf 4
+listener(s) on a per-rule tap.
 There is zero overhead when no
 .Xr bpf 4
 listener is attached to the tap.
@@ -746,10 +757,12 @@ Logs a packet to
 with a
 .Dv LOG_SECURITY
 facility.
-.It Ar ipfw0
+.It Ar bpf
 Logs a packet to the
-.Li ipfw0
-pseudo interface.
+.Xr bpf 4
+tap named
+.Li ipfwXXXXX ,
+where XXXXX is the rule number.
 .It Ar rtsock
 Logs a packet to the
 .Xr route 4
@@ -769,7 +782,7 @@ I.e. A packet matching a rule with
 .Cm log logamount
 100
 .Cm logdst
-syslog,ipfw0 ...
+syslog,bpf ...
 .Ed
 .Pp
 will log upto 50 packets.
@@ -3663,9 +3676,9 @@ Default value is
 .It Cm log
 Turn on logging of all handled packets via BPF tap named
 .Ar ipfwlog0 .
-Note that it has different purpose than
-.Ar ipfw0
-tap.
+Note that it has different purpose than per-rule
+.Xr bpf 4
+taps.
 Translators sends to BPF an additional information with each packet.
 With
 .Cm tcpdump

sbin/ipfw/ipfw2.c
@@ -2013,7 +2013,7 @@ print_logdst(struct buf_pr *bp, uint16_t arg1)
 		comma = ",";
 	}
 	if (arg1 & IPFW_LOG_IPFW0) {
-		bprintf(bp, "%sipfw0", comma);
+		bprintf(bp, "%sbpf", comma);
 		comma = ",";
 	}
 	if (arg1 & IPFW_LOG_RTSOCK) {
@@ -4066,8 +4066,9 @@ parse_logdst(char *logdst_iter)
 			ret |= IPFW_LOG_SYSLOG;
 			continue;
 		}
-		if (_substrcmp(token, "ipfw0") == 0) {
-			/* XXX add multiple ipfw* */
+		/* ipfw0 is compatibility keyword. */
+		if (_substrcmp(token, "bpf") == 0 ||
+		    _substrcmp(token, "ipfw0") == 0) {
 			ret |= IPFW_LOG_IPFW0;
 			continue;
 		}

sys/netpfil/ipfw/nat64/nat64_translate.c
@@ -151,7 +151,7 @@ nat64_log(struct pfloghdr *logdata, struct mbuf *m, sa_family_t family)
 
 	logdata->dir = PF_OUT;
 	logdata->af = family;
-	ipfw_bpf_mtap2(logdata, PFLOG_HDRLEN, m);
+	ipfw_pflog_tap(logdata, m);
 }
 
 static int

sys/netpfil/ipfw/nat64/nat64clat.c
@@ -77,7 +77,7 @@ nat64clat_log(struct pfloghdr *plog, struct mbuf *m, sa_family_t family,
 	plog->subrulenr = htonl(pktid);
 	plog->ruleset[0] = '\0';
 	strlcpy(plog->ifname, "NAT64CLAT", sizeof(plog->ifname));
-	ipfw_bpf_mtap2(plog, PFLOG_HDRLEN, m);
+	ipfw_pflog_tap(plog, m);
 }
 
 static int

commit 65b4bf7a6e0d415492889434a72085debdacdf82

ipfw: refactor how we store bpf tap points
Make the tap database belong to ip_fw_chain, but leave the default "ipfw0"
tap per-vnet.  This is only slightly better than keeping the database per-
vnet, as the bpf name space is per-vnet.  However, we yet have only single
ipfw chain.  Whenever multiple chains will coexist, this needs to be
addressed.

Require the chain lock to make modifications to the database.

Move tap allocation to a later ruleset build stage, when all rule numbers
are known already.  This fixes a panic introduced by 3daae1ac1d82.

Fixes:	3daae1ac1d82ecdcd855101bab5206e914b12350

sys/netpfil/ipfw/ip_fw2.c
@@ -3667,6 +3667,7 @@ vnet_ipfw_init(const void *unused)
 #ifdef IPFIREWALL_NAT
 	LIST_INIT(&chain->nat);
 #endif
+	RB_INIT(&chain->taps);
 
 	/* Init shared services hash table */
 	ipfw_init_srv(chain);

sys/netpfil/ipfw/ip_fw_bpf.c
@@ -1,7 +1,7 @@
 /*-
  * Copyright (c) 2016 Yandex LLC
  * Copyright (c) 2016 Andrey V. Elsukov <ae@FreeBSD.org>
- * Copyright (c) 2025 Gleb Smirnoff <glebius@FreeBSD.org>
+ * Copyright (c) 2025-2026 Gleb Smirnoff <glebius@FreeBSD.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -68,20 +68,20 @@ tap_compare(const struct ipfw_tap *a, const struct ipfw_tap *b)
 {
 	return (a->rule != b->rule ? (a->rule < b->rule ? -1 : 1) : 0);
 }
-RB_HEAD(tap_tree, ipfw_tap);
-VNET_DEFINE_STATIC(struct tap_tree, tap_tree);
-#define	V_tap_tree	VNET(tap_tree)
 RB_GENERATE_STATIC(tap_tree, ipfw_tap, entry, tap_compare);
-VNET_DEFINE_STATIC(struct ipfw_tap *, default_tap);
+VNET_DEFINE_STATIC(struct ipfw_tap, default_tap) = { .name = "ipfw0" };
 #define	V_default_tap	VNET(default_tap)
 
 void
-ipfw_tap_alloc(uint32_t rule)
+ipfw_tap_alloc(struct ip_fw_chain *ch, uint32_t rule)
 {
 	struct ipfw_tap	*tap, key = { .rule = rule };
 	int n __diagused;
 
-	tap = RB_FIND(tap_tree, &V_tap_tree, &key);
+	MPASS(rule > 0 && rule < IPFW_DEFAULT_RULE);
+	IPFW_UH_WLOCK_ASSERT(ch);
+
+	tap = RB_FIND(tap_tree, &ch->taps, &key);
 	if (tap != NULL) {
 		MPASS(tap->rule == rule);
 		tap->refs++;
@@ -90,43 +90,51 @@ ipfw_tap_alloc(uint32_t rule)
 	tap = malloc(sizeof(*tap), M_IPFW, M_WAITOK);
 	tap->rule = rule;
 	tap->refs = 1;
-	/* Note: the default rule logs to "ipfw0". */
-	if (__predict_false(rule == IPFW_DEFAULT_RULE)) {
-		V_default_tap = tap;
-		rule = 0;
-	}
 	n = snprintf(tap->name, sizeof(tap->name), "ipfw%u", rule);
 	MPASS(n > 4 && n < sizeof("ipfw4294967295"));
 	tap->bpf = bpf_attach(tap->name, DLT_EN10MB, PFLOG_HDRLEN,
 	    &bpf_ipfw_methods, NULL);
-	tap = RB_INSERT(tap_tree, &V_tap_tree, tap);
+	tap = RB_INSERT(tap_tree, &ch->taps, tap);
 	MPASS(tap == NULL);
 }
 
 void
-ipfw_tap_free(uint32_t rule)
+ipfw_tap_free(struct ip_fw_chain *ch, uint32_t rule)
 {
-
 	struct ipfw_tap	*tap, key = { .rule = rule };
 
-	tap = RB_FIND(tap_tree, &V_tap_tree, &key);
+	MPASS(rule > 0 && rule < IPFW_DEFAULT_RULE);
+	IPFW_UH_WLOCK_ASSERT(ch);
+
+	tap = RB_FIND(tap_tree, &ch->taps, &key);
 	MPASS(tap != NULL);
 	if (--tap->refs == 0) {
 		bpf_detach(tap->bpf);
-		RB_REMOVE(tap_tree, &V_tap_tree, tap);
+		RB_REMOVE(tap_tree, &ch->taps, tap);
 		free(tap, M_IPFW);
 	}
 }
 
 void
-ipfw_bpf_tap(struct ip_fw_args *args, struct ip *ip, uint32_t rulenum)
+ipfw_bpf_tap(struct ip_fw_chain *ch, struct ip_fw_args *args,
+    struct ip *ip, uint32_t rulenum)
 {
-	struct ipfw_tap *tap, key = { .rule = rulenum };
+	struct ipfw_tap *tap;
 
-	tap = RB_FIND(tap_tree, &V_tap_tree, &key);
-	MPASS(tap != NULL);
-	if (!bpf_peers_present(tap->bpf))
-		tap = V_default_tap;
+	if (rulenum == IPFW_DEFAULT_RULE) {
+		tap = &V_default_tap;
+	} else {
+		struct ipfw_tap key = { .rule = rulenum };
+
+		tap = RB_FIND(tap_tree, &ch->taps, &key);
+		MPASS(tap != NULL);
+		/*
+		 * Compatibility: if user is not using per-rule taps, fallback
+		 * to the default tap.
+		 */
+		if (!bpf_peers_present(tap->bpf))
+			tap = &V_default_tap;
+	}
 	if (args->flags & IPFW_ARGS_LENMASK) {
 		bpf_tap(tap->bpf, args->mem, IPFW_ARGS_LENGTH(args->flags));
 	} else if (args->flags & IPFW_ARGS_ETHER) {
@@ -161,7 +169,8 @@ ipfw_pflog_tap(void *data, struct mbuf *m)
 void
 ipfw_bpf_init(int first __unused)
 {
-	ipfw_tap_alloc(IPFW_DEFAULT_RULE);
+	V_default_tap.bpf = bpf_attach(V_default_tap.name, DLT_EN10MB,
+	    PFLOG_HDRLEN, &bpf_ipfw_methods, NULL);
 	V_bpf_pflog = bpf_attach("ipfwlog0", DLT_PFLOG, PFLOG_HDRLEN,
 	    &bpf_ipfw_methods, NULL);
 }
@@ -169,7 +178,6 @@ ipfw_bpf_init(int first __unused)
 void
 ipfw_bpf_uninit(int last __unused)
 {
-
-	ipfw_tap_free(IPFW_DEFAULT_RULE);
+	bpf_detach(V_default_tap.bpf);
 	bpf_detach(V_bpf_pflog);
 }

sys/netpfil/ipfw/ip_fw_log.c
@@ -722,7 +722,7 @@ ipfw_log(struct ip_fw_chain *chain, struct ip_fw *f, u_int hlen,
 	    /* O_LOG is the first action */
 	    ((cmd = ACTION_PTR(f)) && cmd->arg1 == IPFW_LOG_DEFAULT)) {
 		if (V_fw_verbose == 0) {
-			ipfw_bpf_tap(args, ip,
+			ipfw_bpf_tap(chain, args, ip,
 			    f != NULL ? f->rulenum : IPFW_DEFAULT_RULE);
 			return;
 		}
@@ -737,6 +737,6 @@ ipfw_log(struct ip_fw_chain *chain, struct ip_fw *f, u_int hlen,
 		ipfw_log_rtsock(chain, f, hlen, args, offset, tablearg, eh);
 
 	if (cmd->arg1 & IPFW_LOG_IPFW0)
-		ipfw_bpf_tap(args, ip, f->rulenum);
+		ipfw_bpf_tap(chain, args, ip, f->rulenum);
 }
 /* end of file */

sys/netpfil/ipfw/ip_fw_private.h
@@ -28,6 +28,9 @@
 #ifndef _IPFW2_PRIVATE_H
 #define _IPFW2_PRIVATE_H
 
+#include <sys/queue.h>
+#include <sys/tree.h>
+
 /*
  * Internal constants and data structures used by ipfw components
  * and not meant to be exported outside the kernel.
@@ -161,9 +164,10 @@ struct ip_fw_chain;
 
 void ipfw_bpf_init(int);
 void ipfw_bpf_uninit(int);
-void ipfw_tap_alloc(uint32_t);
-void ipfw_tap_free(uint32_t);
-void ipfw_bpf_tap(struct ip_fw_args *, struct ip *, uint32_t);
+void ipfw_tap_alloc(struct ip_fw_chain *, uint32_t);
+void ipfw_tap_free(struct ip_fw_chain *, uint32_t);
+void ipfw_bpf_tap(struct ip_fw_chain *, struct ip_fw_args *, struct ip *,
+    uint32_t);
 void ipfw_pflog_tap(void *, struct mbuf *);
 void ipfw_log(struct ip_fw_chain *chain, struct ip_fw *f, u_int hlen,
     struct ip_fw_args *args, u_short offset, uint32_t tablearg, struct ip *ip,
@@ -320,6 +324,7 @@ struct ip_fw_chain {
 	void		*ifcfg;		/* interface module data */
 	int		*idxmap_back;	/* standby skipto array of rules */
 	struct namedobj_instance	*srvmap; /* cfg name->number mappings */
+	RB_HEAD(tap_tree, ipfw_tap) taps;	/* see ip_fw_bpf.c */
 #if defined( __linux__ ) || defined( _WIN32 )
 	spinlock_t uh_lock;
 #else

sys/netpfil/ipfw/ip_fw_sockopt.c
@@ -210,8 +210,6 @@ ipfw_free_rule(struct ip_fw *rule)
 	 */
 	if (rule->refcnt > 1)
 		return;
-	if (ACTION_PTR(rule)->opcode == O_LOG)
-		ipfw_tap_free(rule->rulenum);
 	uma_zfree_pcpu(V_ipfw_cntr_zone, rule->cntr);
 	free(rule, M_IPFW);
 }
@@ -505,6 +503,8 @@ ipfw_commit_rules(struct ip_fw_chain *chain, struct rule_check_info *rci,
 			memcpy((char *)ci->urule + ci->urule_numoff, &rulenum,
 			    sizeof(rulenum));
 		}
+		if (ACTION_PTR(krule)->opcode == O_LOG)
+			ipfw_tap_alloc(chain, krule->rulenum);
 	}
 
 	/* duplicate the remaining part, we always have the default rule */
@@ -2102,6 +2102,8 @@ unref_rule_objects(struct ip_fw_chain *ch, struct ip_fw *rule)
 		else
 			no->refcnt--;
 	}
+	if (ACTION_PTR(rule)->opcode == O_LOG)
+		ipfw_tap_free(ch, rule->rulenum);
 }
 
 /*
@@ -2459,9 +2461,6 @@ import_rule_v1(struct ip_fw_chain *chain, struct rule_check_info *ci)
 
 	/* Copy opcodes */
 	memcpy(krule->cmd, urule->cmd, krule->cmd_len * sizeof(uint32_t));
-
-	if (ACTION_PTR(krule)->opcode == O_LOG)
-		ipfw_tap_alloc(krule->rulenum);
 }
 
 /*