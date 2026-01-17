69b496e2e0d17f28407f5233013003e1606fbfe0
Jose Luis Duran <jlduran@FreeBSD.org>
2026-01-17 18:10:48 +0000

    nanobsd: Add a NO_ROOT build option
    Add a -U option to build NanoBSD images without root privileges.  It
    relies on makefs/mkimg and metalog (mtree) files, similar to what
    release engineering uses to build images.
    
    Keep the current way to build NanoBSD images untouched.  Once this
    method gets battle tested, it may be used to build images as root as
    well.
    
    Reviewed by:	imp, emaste
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D48793

--- tools/tools/nanobsd/nanobsd.sh
+++ tools/tools/nanobsd/nanobsd.sh
@@ -36,6 +36,7 @@ topdir=`dirname ${nanobsd_sh}`
 # Parse arguments
 
 do_clean=true
+do_root=true
 do_kernel=true
 do_installkernel=true
 do_world=true
@@ -49,7 +50,7 @@ do_prep_image=true
 . "${topdir}/legacy.sh"
 
 set +e
-args=`getopt BKXWbc:fhiIknpqvw $*`
+args=`getopt BKXWbc:fhiIknpqUvw $*`
 if [ $? -ne 0 ] ; then
 	usage
 	exit 2
@@ -131,6 +132,11 @@ do
 		;;
 	-v)
 		PPLEVEL=$(($PPLEVEL + 1))
+		shift
+		;;
+	-U)
+		do_root=false
+		NANO_NOPRIV_BUILD=true
 		shift
 		;;
 	-w)
@@ -221,9 +227,17 @@ else
 fi
 if $do_code ; then
 	calculate_partitioning
-	create_code_slice
+	if [ -z "${NANO_NOPRIV_BUILD}" ]; then
+		create_code_slice
+	else
+		_create_code_slice
+	fi
 	if $do_image ; then
-		create_diskimage
+		if [ -z "${NANO_NOPRIV_BUILD}" ]; then
+			create_diskimage
+		else
+			_create_diskimage
+		fi
 	else
 		pprint 2 "Skipping image build (as instructed)"
 	fi
