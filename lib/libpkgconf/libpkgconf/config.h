4fc6c943554d53b8d91fa1d488a382a10946fc45
Pierre Pronchery <khorben@FreeBSD.org>
2026-01-21 22:56:55 +0100

    Import pkgconf 2.5.1

--- lib/libpkgconf/libpkgconf/config.h
+++ lib/libpkgconf/libpkgconf/config.h
@@ -1,54 +1,34 @@
-/* libpkgconf/config.h.  Generated from config.h.in by configure.  */
 /* libpkgconf/config.h.in.  Generated from configure.ac by autoheader.  */
 
-/* Define to 1 if you have the declaration of 'reallocarray', and to 0 if you
-   don't. */
-#define HAVE_DECL_REALLOCARRAY 1
+/* Define to 1 if you have the `strlcat' function. */
+#define HAVE_STRLCAT 1
 
-/* Define to 1 if you have the declaration of 'strlcat', and to 0 if you
-   don't. */
+/* Define to 1 if you have the `strlcpy' function. */
+#define HAVE_STRLCPY 1
+
+/* Define to 1 if you have the `strndup' function. */
+#define HAVE_STRNDUP 1
+
+/* Define to 1 if you have the `reallocarray' function. */
+#define HAVE_REALLOCARRAY 1
+
+/* Define to 1 if you have the `strlcat' function. */
 #define HAVE_DECL_STRLCAT 1
 
-/* Define to 1 if you have the declaration of 'strlcpy', and to 0 if you
-   don't. */
+/* Define to 1 if you have the `strlcpy' function. */
 #define HAVE_DECL_STRLCPY 1
 
-/* Define to 1 if you have the declaration of 'strndup', and to 0 if you
-   don't. */
+/* Define to 1 if you have the `strndup' function. */
 #define HAVE_DECL_STRNDUP 1
 
-/* Define to 1 if you have the <dlfcn.h> header file. */
-#define HAVE_DLFCN_H 1
+/* Define to 1 if you have the `reallocarray' function. */
+#define HAVE_DECL_REALLOCARRAY 1
 
-/* Define to 1 if you have the <inttypes.h> header file. */
-#define HAVE_INTTYPES_H 1
+/* Define to 1 if you have the `pledge' function. */
+/* #undef HAVE_DECL_PLEDGE */
 
-/* Define to 1 if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H 1
-
-/* Define to 1 if you have the <stdio.h> header file. */
-#define HAVE_STDIO_H 1
-
-/* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
-
-/* Define to 1 if you have the <strings.h> header file. */
-#define HAVE_STRINGS_H 1
-
-/* Define to 1 if you have the <string.h> header file. */
-#define HAVE_STRING_H 1
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H 1
-
-/* Define to 1 if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H 1
-
-/* Define to 1 if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H 1
-
-/* Define to the sub-directory where libtool stores uninstalled libraries. */
-#define LT_OBJDIR ".libs/"
+/* Define to 1 if you have the `unveil' function. */
+/* #undef HAVE_DECL_UNVEIL */
 
 /* Name of package */
 #define PACKAGE "pkgconf"
@@ -60,7 +40,7 @@
 #define PACKAGE_NAME "pkgconf"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "pkgconf 2.4.3"
+#define PACKAGE_STRING "pkgconf 2.5.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "pkgconf"
@@ -69,24 +49,29 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.4.3"
+#define PACKAGE_VERSION "2.5.1"
 
-/* Define to 1 if all of the C89 standard headers exist (not just the ones
-   required in a freestanding environment). This macro is provided for
-   backward compatibility; new code need not use it. */
+/* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "2.4.3"
+#define VERSION "2.5.1"
+
+/* Enable large inode numbers on Mac OS X 10.5.  */
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
 
 /* Number of bits in a file offset, on hosts where this is settable. */
 /* #undef _FILE_OFFSET_BITS */
 
-/* Define to 1 on platforms where this makes off_t a 64-bit type. */
+/* Define for large files, on AIX-style hosts. */
 /* #undef _LARGE_FILES */
 
 /* Number of bits in time_t, on hosts where this is settable. */
 /* #undef _TIME_BITS */
 
-/* Define to 1 on platforms where this makes time_t a 64-bit type. */
-/* #undef __MINGW_USE_VC2005_COMPAT */
+/* Enable Solaris extensions. */
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
