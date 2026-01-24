2ddb7378a7a7582910b993edbf8932ff1edfa922
Alan Somers <asomers@FreeBSD.org>
2026-01-24 09:02:44 -0700

    fusefs: style
    Reorder macro definitions.

--- sys/fs/fuse/fuse_ipc.h
+++ sys/fs/fuse/fuse_ipc.h
@@ -240,11 +240,11 @@ struct fuse_data {
 #define FSESS_WARN_READLINK_EMBEDDED_NUL 0x1000000 /* corrupt READLINK output */
 #define FSESS_WARN_DOT_LOOKUP	  0x2000000 /* Inconsistent . LOOKUP response */
 #define FSESS_WARN_INODE_MISMATCH 0x4000000 /* ino != nodeid */
+#define	FSESS_SETXATTR_EXT	  0x8000000 /* extended fuse_setxattr_in */
+#define FSESS_AUTO_UNMOUNT	  0x10000000 /* perform unmount when server dies */
 #define FSESS_MNTOPTS_MASK	( \
 	FSESS_DAEMON_CAN_SPY | FSESS_PUSH_SYMLINKS_IN | \
 	FSESS_DEFAULT_PERMISSIONS | FSESS_INTR | FSESS_AUTO_UNMOUNT)
-#define	FSESS_SETXATTR_EXT	  0x8000000 /* extended fuse_setxattr_in */
-#define FSESS_AUTO_UNMOUNT	  0x10000000 /* perform unmount when server dies */
 
 extern int fuse_data_cache_mode;
 
