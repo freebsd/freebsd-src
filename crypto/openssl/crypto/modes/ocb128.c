65c1295c6bb083f2d3f82f19e773b841faad8147
Gordon Tetlow <gordon@FreeBSD.org>
2026-01-26 10:42:20 -0800

    openssl: Fix multiple vulnerabilities
    This is a rollup commit from upstream to fix:
      Stack buffer overflow in CMS AuthEnvelopedData parsing (CVE-2025-15467)
      Heap out-of-bounds write in BIO_f_linebuffer on short writes (CVE-2025-68160)
      Unauthenticated/unencrypted trailing bytes with low-level OCB function calls (CVE-2025-69418)
      Out of bounds write in PKCS12_get_friendlyname() UTF-8 conversion (CVE-2025-69419)
      Missing ASN1_TYPE validation in TS_RESP_verify_response() function (CVE-2025-69420)
      NULL Pointer Dereference in PKCS12_item_decrypt_d2i_ex function (CVE-2025-69421)
      Missing ASN1_TYPE validation in PKCS#12 parsing (CVE-2026-22795)
      ASN1_TYPE Type Confusion in the PKCS7_digest_from_attributes() function (CVE-2026-22796)
    
    See https://openssl-library.org/news/secadv/ for additional details.
    
    Approved by:	so
    Obtained from:	OpenSSL
    Security:	FreeBSD-SA-26:01.openssl
    Security:	CVE-2025-15467
    Security:	CVE-2025-68160
    Security:	CVE-2025-69418
    Security:	CVE-2025-69419
    Security:	CVE-2025-69420
    Security:	CVE-2025-69421
    Security:	CVE-2026-22795
    Security:	CVE-2026-22796

--- crypto/openssl/crypto/modes/ocb128.c
+++ crypto/openssl/crypto/modes/ocb128.c
@@ -342,7 +342,7 @@ int CRYPTO_ocb128_encrypt(OCB128_CONTEXT *ctx,
 
     if (num_blocks && all_num_blocks == (size_t)all_num_blocks
         && ctx->stream != NULL) {
-        size_t max_idx = 0, top = (size_t)all_num_blocks;
+        size_t max_idx = 0, top = (size_t)all_num_blocks, processed_bytes = 0;
 
         /*
          * See how many L_{i} entries we need to process data at hand
@@ -356,6 +356,9 @@ int CRYPTO_ocb128_encrypt(OCB128_CONTEXT *ctx,
         ctx->stream(in, out, num_blocks, ctx->keyenc,
                     (size_t)ctx->sess.blocks_processed + 1, ctx->sess.offset.c,
                     (const unsigned char (*)[16])ctx->l, ctx->sess.checksum.c);
+        processed_bytes = num_blocks * 16;
+        in += processed_bytes;
+        out += processed_bytes;
     } else {
         /* Loop through all full blocks to be encrypted */
         for (i = ctx->sess.blocks_processed + 1; i <= all_num_blocks; i++) {
@@ -434,7 +437,7 @@ int CRYPTO_ocb128_decrypt(OCB128_CONTEXT *ctx,
 
     if (num_blocks && all_num_blocks == (size_t)all_num_blocks
         && ctx->stream != NULL) {
-        size_t max_idx = 0, top = (size_t)all_num_blocks;
+        size_t max_idx = 0, top = (size_t)all_num_blocks, processed_bytes = 0;
 
         /*
          * See how many L_{i} entries we need to process data at hand
@@ -448,6 +451,9 @@ int CRYPTO_ocb128_decrypt(OCB128_CONTEXT *ctx,
         ctx->stream(in, out, num_blocks, ctx->keydec,
                     (size_t)ctx->sess.blocks_processed + 1, ctx->sess.offset.c,
                     (const unsigned char (*)[16])ctx->l, ctx->sess.checksum.c);
+        processed_bytes = num_blocks * 16;
+        in += processed_bytes;
+        out += processed_bytes;
     } else {
         OCB_BLOCK tmp;
 
