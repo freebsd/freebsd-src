d100ff6c09c97babda963d2c295601650faeeebf
John Baldwin <jhb@FreeBSD.org>
2026-01-31 12:20:50 -0500

    libkern: Add libcalls for ffs and __ffsdi2
    These are needed when compiling a RISC-V kernel with GCC which does
    not inline __builtin_ffs*.
    
    The __ffsdi2 is adapated from the previous ffsl.c.
    
    This partially reverts commit f4db342d44198973c1c7b9005d0c5683a582707e.
    
    Reviewed by:	mhorne
    Differential Revision:	https://reviews.freebsd.org/D54967
    
    (cherry picked from commit 4c822dd4a15c533c08271b2286da949fffafc89e)

--- sys/libkern/quad.h
+++ sys/libkern/quad.h
@@ -102,6 +102,7 @@ quad_t		__ashrdi3(quad_t, qshift_t);
 int		__cmpdi2(quad_t a, quad_t b);
 quad_t		__divdi3(quad_t a, quad_t b);
 quad_t		__divmoddi4(quad_t a, quad_t b, quad_t *rem);
+int		__ffsdi2(quad_t a);
 quad_t		__lshrdi3(quad_t, qshift_t);
 quad_t		__moddi3(quad_t a, quad_t b);
 u_quad_t	__qdivrem(u_quad_t u, u_quad_t v, u_quad_t *rem);
