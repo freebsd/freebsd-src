f6745503ded7cc938a52d6916277da12c5b31d4f
Mark Johnston <markj@FreeBSD.org>
2026-02-02 01:55:04 +0000

    Make zvol_set_common() block until the operation has completed
    This is motivated by a FreeBSD AIO test case which create a zvol with -o
    volmode=dev, then immediately tries to open the zvol device file.  The
    open occasionally fails with ENOENT.
    
    When a zvol is created without the volmode setting, zvol_create_minors()
    blocks until the task is finished, at which point OS-dependent code will
    have created a device file.  However, zvol_set_common() may cause the
    device file to be destroyed and re-created, at least on FreeBSD, if the
    voltype switches from GEOM to DEV.  In this case, we do not block
    waiting for the operation to finish, causing the test failure.
    
    Fix the problem by making zvol_set_common() block until the operation
    has finished.  In FreeBSD zvol code, use g_waitidle() to block until
    asynchronous GEOM operations are done.  This fixes a secondary race
    where zvol_os_remove_minor() does not block until the zvol device file
    is removed, and the subsequent zvol_os_create_minor() fails because the
    (to-be-destroyed) device file already exists.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Alexander Motin <alexander.motin@TrueNAS.com>
    Signed-off-by: Mark Johnston <markj@FreeBSD.org>
    Closes #18191
    (cherry picked from commit d736868672cb3df197d2ddcfef29ac14edf4ee20)

--- sys/contrib/openzfs/module/os/freebsd/zfs/zvol_os.c
+++ sys/contrib/openzfs/module/os/freebsd/zfs/zvol_os.c
@@ -919,7 +919,7 @@ retry:
 		return (SET_ERROR(ENXIO));
 
 	mutex_enter(&zv->zv_state_lock);
-	if (zv->zv_zso->zso_dying || zv->zv_flags & ZVOL_REMOVING) {
+	if (zv->zv_flags & ZVOL_REMOVING || zv->zv_zso->zso_dying) {
 		err = SET_ERROR(ENXIO);
 		goto out_locked;
 	}
@@ -1426,6 +1426,7 @@ zvol_os_remove_minor(zvol_state_t *zv)
 		pp->private = NULL;
 		g_wither_geom(pp->geom, ENXIO);
 		g_topology_unlock();
+		g_waitidle(curthread);
 	} else if (zv->zv_volmode == ZFS_VOLMODE_DEV) {
 		struct zvol_state_dev *zsd = &zso->zso_dev;
 		struct cdev *dev = zsd->zsd_cdev;
@@ -1555,6 +1556,7 @@ out_dmu_objset_disown:
 		g_error_provider(zv->zv_zso->zso_geom.zsg_provider, 0);
 		/* geom was locked inside zvol_alloc() function */
 		g_topology_unlock();
+		g_waitidle(curthread);
 	}
 out_doi:
 	kmem_free(doi, sizeof (dmu_object_info_t));
