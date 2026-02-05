4ebb7c8048aef03d1ab154f1c5ccc30b56376604
Konstantin Belousov <kib@FreeBSD.org>
2026-02-05 21:10:00 +0200

    sys/abi_compat.h: add Foundation copyright for uint64_t stuff
    Reviewed by:	brooks
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D55135
    
    (cherry picked from commit 9ce0c190d8b3570c0a537f84952dca8c6e462ec3)

--- sys/sys/abi_compat.h
+++ sys/sys/abi_compat.h
@@ -4,6 +4,11 @@
  * Copyright (c) 2001 Doug Rabson
  * All rights reserved.
  *
+ * Copyright 2026 The FreeBSD Foundation
+ *
+ * Portions of this software were developed by Konstantin Belousov
+ * under sponsorship from the FreeBSD Foundation.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
