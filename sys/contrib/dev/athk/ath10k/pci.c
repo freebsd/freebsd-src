019cff5337bba4cf4934f2ddf686a287ab646712
Bjoern A. Zeeb <bz@FreeBSD.org>
2026-02-07 22:29:44 +0000

    ath10k: harmonize all MODULE_DEPEND to ath10k
    With theoretically multiple bus attachements (unclear if we will ever
    support anything but PCI) rename the module name to "ath10k" and use
    it consistently.  Move the common depends to core.c and lindebugfs
    and usb to their respective files.
    
    MFC after:	3 days

--- sys/contrib/dev/athk/ath10k/pci.c
+++ sys/contrib/dev/athk/ath10k/pci.c
@@ -3933,15 +3933,6 @@ module_exit(ath10k_pci_exit);
 MODULE_AUTHOR("Qualcomm Atheros");
 MODULE_DESCRIPTION("Driver support for Qualcomm Atheros PCIe/AHB 802.11ac WLAN devices");
 MODULE_LICENSE("Dual BSD/GPL");
-#if defined(__FreeBSD__)
-MODULE_VERSION(ath10k_pci, 1);
-MODULE_DEPEND(ath10k_pci, linuxkpi, 1, 1, 1);
-MODULE_DEPEND(ath10k_pci, linuxkpi_wlan, 1, 1, 1);
-MODULE_DEPEND(ath10k_pci, athk_common, 1, 1, 1);
-#ifdef CONFIG_ATH10K_DEBUGFS
-MODULE_DEPEND(ath10k_pci, debugfs, 1, 1, 1);
-#endif
-#endif
 
 /* QCA988x 2.0 firmware files */
 MODULE_FIRMWARE(QCA988X_HW_2_0_FW_DIR "/" ATH10K_FW_API2_FILE);
