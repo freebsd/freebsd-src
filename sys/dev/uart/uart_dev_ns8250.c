fce4758b7126adfa047e774241302a7e763b19c7
Colin Percival <cperciva@FreeBSD.org>
2026-02-07 16:42:03 -0800

    uart: Add ns8250 ACPI entry for SPCR rev 2
    This is an MFC "in spirit" of a685a263b803; the code in this area has
    been significantly restructured between 14.x and 15.x, but the general
    concept of adding ACPI_DBG2_16550_WITH_GAS (aka 0x12) as a recognized
    flavour of ns8250 UART which can be configured via SPCR remains.
    
    Approved by:	re (cperciva)
    Reviewed by:	imp
    Tested by:	David Woodhouse
    Sponsored by:	Amazon
    Differential Revision:	https://reviews.freebsd.org/D55173
    
    (cherry picked from commit 1dd5b197b72357dfe50d6da2c9cb5b34bf4bf64b)

--- sys/dev/uart/uart_dev_ns8250.c
+++ sys/dev/uart/uart_dev_ns8250.c
@@ -449,6 +449,7 @@ static struct acpi_uart_compat_data acpi_compat_data[] = {
 	{"AMDI0020", &uart_ns8250_class, 0, 2, 0, 48000000, UART_F_BUSY_DETECT, "AMD / Synopsys Designware UART"},
 	{"APMC0D08", &uart_ns8250_class, ACPI_DBG2_16550_COMPATIBLE, 2, 4, 0, 0, "APM compatible UART"},
 	{"MRVL0001", &uart_ns8250_class, ACPI_DBG2_16550_SUBSET, 2, 0, 200000000, UART_F_BUSY_DETECT, "Marvell / Synopsys Designware UART"},
+	{"", &uart_ns8250_class, ACPI_DBG2_16550_WITH_GAS, 0, 0, 0, 0, "ACPI 16550 WITH GAS"},
 	{"SCX0006",  &uart_ns8250_class, 0, 2, 0, 62500000, UART_F_BUSY_DETECT, "SynQuacer / Synopsys Designware UART"},
 	{"HISI0031", &uart_ns8250_class, 0, 2, 0, 200000000, UART_F_BUSY_DETECT, "HiSilicon / Synopsys Designware UART"},
 	{"NXP0018", &uart_ns8250_class, 0, 0, 0, 350000000, UART_F_BUSY_DETECT, "NXP / Synopsys Designware UART"},
