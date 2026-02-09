5aca8a88b8d8ef88852099cd58fc761837aa73f2
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2026-02-09 10:26:19 +0200

    diff: Add SPDX-License-Identifier tag
    Reviewed by: emaste
    Sponsored by: The FreeBSD Foundation

--- usr.bin/diff/diff.h
+++ usr.bin/diff/diff.h
@@ -1,6 +1,8 @@
 /*	$OpenBSD: diff.h,v 1.34 2020/11/01 18:16:08 jcs Exp $	*/
 
 /*-
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
  * Copyright (c) 1991, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
