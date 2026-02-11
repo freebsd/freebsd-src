be37b02754be794b07dc328a09b9acc48e90c3c0
Siva Mahadevan <siva@FreeBSD.org>
2026-02-11 16:05:25 -0500

    libnv/tests: unskip nvlist_send_recv__send_many_fds__dgram
    The test passes consistently for 1000+ consecutive iterations.
    
    PR:		260891
    Reviewed by:	oshogbo
    Approved by:	lwhsu (mentor)
    Sponsored by:	The FreeBSD Foundation
    MFC after:	3 days
    Differential Revision:	https://reviews.freebsd.org/D55223

--- lib/libnv/tests/nvlist_send_recv_test.c
+++ lib/libnv/tests/nvlist_send_recv_test.c
@@ -482,8 +482,6 @@ ATF_TC_BODY(nvlist_send_recv__send_many_fds__dgram, tc)
 	size_t len;
 	int error;
 
-	atf_tc_skip("https://bugs.freebsd.org/260891");
-
 	/* size of the largest datagram to send */
 	temp_maxdgram = 16772;
 	len = sizeof(maxdgram);
