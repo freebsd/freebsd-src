239bc29f176f00299aae83bb8ec554c74540398e
Bjoern A. Zeeb <bz@FreeBSD.org>
2026-02-19 23:10:34 +0000

    qoriq: fix spelling in device_set_desc()
    The product series is called NXP QorIQ Layerscape.  Remove the extra 'e.
    
    MFC after:	3 days
    Reviewed by:	mmel, emaste
    Differential Revision: https://reviews.freebsd.org/D55388

--- sys/arm64/qoriq/qoriq_dw_pci.c
+++ sys/arm64/qoriq/qoriq_dw_pci.c
@@ -176,7 +176,7 @@ qorif_dw_pci_probe(device_t dev)
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
-	device_set_desc(dev, "NPX Layaerscape PCI-E Controller");
+	device_set_desc(dev, "NPX Layerscape PCI-E Controller");
 	return (BUS_PROBE_DEFAULT);
 }
 
