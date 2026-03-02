7ba86edecbb09f378a0fdd424599a9bc383dc511
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2026-03-03 00:06:54 +0100

    linuxkpi: Move `_RET_IP_` to <linux/instruction_pointer.h>
    This matches the declaration on Linux.
    
    Reviewed by:	bz, emaste
    Sponsored by:	The FreeBSD Foundation
    Differential Revision: https://reviews.freebsd.org/D55730
    
    (cherry picked from commit 8af854ea479a140a6e284d74e481d0b547408df2)

---
+++ sys/compat/linuxkpi/common/include/linux/instruction_pointer.h
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2021 Neel Chauhan
+ * Copyright (c) 2026 The FreeBSD Foundation
+ */
+
+#ifndef	_LINUXKPI_LINUX_INSTRUCTION_POINTER_H_
+#define	_LINUXKPI_LINUX_INSTRUCTION_POINTER_H_
+
+#define	_RET_IP_		__builtin_return_address(0)
+
+#endif /* _LINUXKPI_LINUX_INSTRUCTION_POINTER_H_ */
