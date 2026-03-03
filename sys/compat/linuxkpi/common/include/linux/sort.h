dea5dccc381aeffff119affdb5e03e2cd59c3447
Siva Mahadevan <siva@FreeBSD.org>
2026-03-03 14:09:35 -0500

    LinuxKPI: avoid -Werror=unused-value in sort() from BUILD_BUG_ON_ZERO()
    The BUILD_BUG_ON_ZERO() macro returns an (int)0 if it does not fail
    at build time. LinuxKPI sort() has it as a guard for an unsupported
    argument but ignores the return value.
    
    This leads to gcc complaining:
    
    /usr/src/sys/compat/linuxkpi/common/include/linux/build_bug.h:60:33: error: statement with no effect [-Werror=unused-value]
       60 | #define BUILD_BUG_ON_ZERO(x)    ((int)sizeof(struct { int:-((x) != 0); }))
          |                                 ^
    /usr/src/sys/compat/linuxkpi/common/include/linux/sort.h:37:9: note: in expansion of macro 'BUILD_BUG_ON_ZERO'
       37 |         BUILD_BUG_ON_ZERO(swap);                        \
          |         ^~~~~~~~~~~~~~~~~
    /usr/src/sys/contrib/dev/rtw89/core.c:2575:9: note: in expansion of macro 'sort'
     2575 |         sort(drift, RTW89_BCN_TRACK_STAT_NR, sizeof(*drift), cmp_u16, NULL);
    
    Change to BUILD_BUG_ON() for the statement version.
    
    Reported by:	CI
    Co-authored-by:	bz
    Approved by:	emaste (mentor)
    MFC after:	3 days
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D55634

--- sys/compat/linuxkpi/common/include/linux/sort.h
+++ sys/compat/linuxkpi/common/include/linux/sort.h
@@ -34,7 +34,7 @@
 #include <sys/libkern.h>
 
 #define	sort(base, num, size, cmp, swap)	do {	\
-	BUILD_BUG_ON_ZERO(swap);			\
+	BUILD_BUG_ON(swap);				\
 	qsort(base, num, size, cmp);			\
 } while (0)
 
