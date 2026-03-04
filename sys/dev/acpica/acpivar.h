6c911c7ba0737d544b9cf8e00cdc61550eec9e4b
John Baldwin <jhb@FreeBSD.org>
2026-03-04 15:19:48 -0500

    acpi: Treat ACPI_IVAR_FLAGS as a global IVAR
    The ACPI bus uses the flags IVAR for any device with a _PRW method
    including devices such as PCI bridges/devices that are not direct
    children of acpi0.
    
    Reported by:	ngie
    Reviewed by:	ngie
    Fixes:		ec27fb8d4682 ("acpi: Split ACPI IVARs into global and private sets")
    Differential Revision:	https://reviews.freebsd.org/D55561

--- sys/dev/acpica/acpivar.h
+++ sys/dev/acpica/acpivar.h
@@ -279,9 +279,9 @@ extern int	acpi_override_isa_irq_polarity;
  */
 enum {
 	ACPI_IVAR_PRIVATE = 20,
-	ACPI_IVAR_FLAGS,
 	ACPI_IVAR_DOMAIN,
-	ACPI_IVAR_HANDLE = BUS_IVARS_ACPI
+	ACPI_IVAR_HANDLE = BUS_IVARS_ACPI,
+	ACPI_IVAR_FLAGS
 };
 
 /*
