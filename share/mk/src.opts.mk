freebsd/freebsd-src

commit 65d9a1cee4cdf6b4545bdb3c94e63411dc07d67b

build: Boostrap LLVM_BINUTILS for cross-tools
Reported by:	vexeduxr, jrtc27
Reviewed by:	imp
Sponsored by:	The FreeBSD Foundation
Pull request:	https://github.com/freebsd/freebsd-src/pull/2084
Differential Revision: https://reviews.freebsd.org/D55923

share/mk/src.opts.mk
@@ -124,6 +124,7 @@ __DEFAULT_YES_OPTIONS = \
     LLD_BOOTSTRAP \
     LLVM_ASSERTIONS \
     LLVM_BINUTILS \
+    LLVM_BINUTILS_BOOTSTRAP \
     LLVM_COV \
     LOADER_BIOS_TEXTONLY \
     LOADER_GELI \

Makefile.inc1
@@ -239,6 +239,12 @@ X${BINUTIL}?=	${${BINUTIL}}
 MK_LLD_BOOTSTRAP=	no
 .endif
 
+# If full paths to all standard bintuils are given, don't build LLVM binutils.
+.if ${XAR:M/*} && ${XNM:M/*} && ${XOBJCOPY:M/*} && ${XRANLIB:M/*} && \
+    ${XSIZE:M/*} && ${XSTRINGS:M/*} && ${XSTRIPBIN:M/*}
+MK_LLVM_BINUTILS_BOOTSTRAP=	no
+.endif
+
 # We also want the X_LINKER* variables if we are using an external toolchain.
 _WANT_TOOLCHAIN_CROSS_VARS=	t
 .include "share/mk/bsd.linker.mk"
@@ -2701,7 +2707,8 @@ _gensnmptree=	usr.sbin/bsnmpd/gensnmptree
 # llvm-tblgen is also needed for various llvm binutils (e.g. objcopy).
 .if ${MK_CLANG_BOOTSTRAP} != "no" || ${MK_CLANG} != "no" || \
     ${MK_LLD_BOOTSTRAP} != "no" || ${MK_LLD} != "no" || \
-    ${MK_LLDB} != "no" || ${MK_LLVM_BINUTILS} != "no"
+    ${MK_LLDB} != "no" || \
+    ${MK_LLVM_BINUTILS_BOOTSTRAP} != "no" || ${MK_LLVM_BINUTILS} != "no"
 _clang_tblgen= \
 	lib/clang/libllvmminimal \
 	usr.bin/clang/llvm-min-tblgen \
@@ -3061,8 +3068,15 @@ _clang=		usr.bin/clang/clang
 .if ${MK_LLD_BOOTSTRAP} != "no"
 _lld=		usr.bin/clang/lld
 .endif
-.if ${MK_CLANG_BOOTSTRAP} != "no" || ${MK_LLD_BOOTSTRAP} != "no"
+.if ${MK_CLANG_BOOTSTRAP} != "no" || ${MK_LLD_BOOTSTRAP} != "no" || \
+    ${MK_LLVM_BINUTILS_BOOTSTRAP} != "no"
 _clang_libs=	lib/clang
+.endif
+.if ${MK_LLVM_BINUTILS_BOOTSTRAP}} != "no"
+_llvm_binutils=	usr.bin/clang/llvm-ar \
+		usr.bin/clang/llvm-nm \
+		usr.bin/clang/llvm-objcopy \
+		usr.bin/clang/llvm-size
 .endif
 .if ${MK_USB} != "no"
 _usb_tools=	stand/usb/tools
@@ -3079,6 +3093,7 @@ cross-tools: .MAKE .PHONY
     ${_clang_libs} \
     ${_clang} \
     ${_lld} \
+    ${_llvm_binutils} \
     ${_elftctools} \
     ${_dtrace_tools} \
     ${_btxld} \

share/man/man5/src.conf.5
@@ -989,6 +989,19 @@ and
 .Xr objdump 1 .
 .Xr strings 1
 is always provided by ELF Tool Chain.
+.It Va WITHOUT_LLVM_BINUTILS_BOOTSTRAP
+Do not build LLVM binary utilities during the bootstrap phase of
+the build.
+To be able to build the system alternate binary utilities must be provided via
+.Ev XAR ,
+.Ev XNM ,
+.Ev XOBJCOPY ,
+.Ev XRANLIB ,
+.Ev XSIZE ,
+.Ev XSTRINGS ,
+and
+.Ev XSTRIPBIN .
+
 .It Va WITHOUT_LLVM_COV
 Do not build the
 .Xr llvm-cov 1

tools/build/options/WITHOUT_LLVM_BINUTILS_BOOTSTRAP
@@ -1,0 +1,12 @@
+Do not build LLVM binary utilities during the bootstrap phase of
+the build.
+To be able to build the system alternate binary utilities must be provided via
+.Ev XAR ,
+.Ev XNM ,
+.Ev XOBJCOPY ,
+.Ev XRANLIB ,
+.Ev XSIZE ,
+.Ev XSTRINGS ,
+and
+.Ev XSTRIPBIN .
+

commit 7fc6ff2b2216d0876d64c46705b1d3868b46e39d

src.opts.mk: !CROSS_COMPILER implies !LLVM_BINUTILS_BOOTSTRAP
This fixes the build when WITHOUT_CROSS_COMPILER is set.

Reported by: fuz, vishwin
Fixes: 65d9a1cee4cd ("build: Boostrap LLVM_BINUTILS for cross-tools")
Sponsored by: The FreeBSD Foundation
Differential Revision: https://reviews.freebsd.org/D56219

share/mk/src.opts.mk
@@ -495,6 +495,7 @@ MK_ZONEINFO_LEAPSECONDS_SUPPORT:= no
 MK_CLANG_BOOTSTRAP:= no
 MK_ELFTOOLCHAIN_BOOTSTRAP:= no
 MK_LLD_BOOTSTRAP:= no
+MK_LLVM_BINUTILS_BOOTSTRAP:= no
 .endif
 
 .if ${MK_TOOLCHAIN} == "no"