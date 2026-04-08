b8352da33f3434f0ba6093df361261d1b1f973a1
Pierre Pronchery <khorben@FreeBSD.org>
2026-04-08 18:52:29 +0200

    pkgconf: import into the base system
    This introduces the following option:
    
    * MK_PKGCONF: determines if pkgconf and bomtool should be built
    
    The objective is to allow the creation of SBOM information while
    building FreeBSD's src tree. The build system cannot rely on the
    presence of bomtool (and eventually also spdxtool) in the build
    environment, except for having it as part of the src tree directly.
    
    The framework implementing the generation of SBOM files is under review
    in D56474.
    
    This will also help simplifying the build, with the introduction of
    another framework relying on the availability of pkgconf.
    
    Sponsored by:		Alpha-Omega, The FreeBSD Foundation
    Reviewed by:		bapt, philip
    Approved by:		philip (mentor)
    Differential Revision:	https://reviews.freebsd.org/D56404

---
+++ lib/libpkgconf/libpkgconf/config.h
@@ -0,0 +1,77 @@
+/* libpkgconf/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `strlcat' function. */
+#define HAVE_STRLCAT 1
+
+/* Define to 1 if you have the `strlcpy' function. */
+#define HAVE_STRLCPY 1
+
+/* Define to 1 if you have the `strndup' function. */
+#define HAVE_STRNDUP 1
+
+/* Define to 1 if you have the `reallocarray' function. */
+#define HAVE_REALLOCARRAY 1
+
+/* Define to 1 if you have the `strlcat' function. */
+#define HAVE_DECL_STRLCAT 1
+
+/* Define to 1 if you have the `strlcpy' function. */
+#define HAVE_DECL_STRLCPY 1
+
+/* Define to 1 if you have the `strndup' function. */
+#define HAVE_DECL_STRNDUP 1
+
+/* Define to 1 if you have the `reallocarray' function. */
+#define HAVE_DECL_REALLOCARRAY 1
+
+/* Define to 1 if you have the `pledge' function. */
+/* #undef HAVE_DECL_PLEDGE */
+
+/* Define to 1 if you have the `unveil' function. */
+/* #undef HAVE_DECL_UNVEIL */
+
+/* Name of package */
+#define PACKAGE "pkgconf"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "https://github.com/pkgconf/pkgconf/issues/new"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "pkgconf"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "pkgconf 2.5.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "pkgconf"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.5.1"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "2.5.1"
+
+/* Enable large inode numbers on Mac OS X 10.5.  */
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+
+/* Number of bits in a file offset, on hosts where this is settable. */
+/* #undef _FILE_OFFSET_BITS */
+
+/* Define for large files, on AIX-style hosts. */
+/* #undef _LARGE_FILES */
+
+/* Number of bits in time_t, on hosts where this is settable. */
+/* #undef _TIME_BITS */
+
+/* Enable Solaris extensions. */
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
