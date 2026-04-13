afbda58063048e2ddc47f7fc6fcc34718ccd7dbf
Kristof Provost <kp@FreeBSD.org>
2026-04-13 15:48:39 +0200

    pf: do not allow flags to be changed with securelevel set
    With securelevel set (for pf that means >= 3) we're expected to reject
    rule changes. However, we allowed interface flags to be changed, which
    would allow 'set skip on X' to be changed.
    
    Remove DIOCSETIFFLAG and DIOCCLRIFFLAG from the securelevel whitelist.
    
    MFC after:	1 week
    Reported by:	cyberkittens
    Sponsored by:	Rubicon Communications, LLC ("Netgate")
    
    (cherry picked from commit d5ca00f2d8743f0885c17f50c8c011cae285fbdb)

--- sys/netpfil/pf/pf_ioctl.c
+++ sys/netpfil/pf/pf_ioctl.c
@@ -2598,8 +2598,6 @@ pfioctl(struct cdev *dev, u_long cmd, caddr_t addr, int flags, struct thread *td
 		case DIOCIGETIFACES:
 		case DIOCGIFSPEEDV0:
 		case DIOCGIFSPEEDV1:
-		case DIOCSETIFFLAG:
-		case DIOCCLRIFFLAG:
 		case DIOCGETETHRULES:
 		case DIOCGETETHRULE:
 		case DIOCGETETHRULESETS:
