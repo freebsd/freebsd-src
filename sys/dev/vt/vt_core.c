addb9fa7fced43a858a690b12b9274559c47815f
Ahmad Khalifa <vexeduxr@FreeBSD.org>
2026-04-20 22:11:27 +0200

    vt_core: delay the shutdown splash event handler
    Even though the functions are currently executed in the correct order,
    there's no garuntee that this will always be the case.
    
    Reported by:	kevans
    Discussed with:	kevans
    Fixes:		4b862c713ac5556ab4bd1828b47c5eb9cb28e067
    MFC after:	3 days
    
    (cherry picked from commit e72c59315e7a2bedd654ac7c6e82dd3ceba30ed2)

--- sys/dev/vt/vt_core.c
+++ sys/dev/vt/vt_core.c
@@ -3191,7 +3191,7 @@ vt_upgrade(struct vt_device *vd)
 				    vt_window_switch, vw, SHUTDOWN_PRI_DEFAULT);
 #ifdef DEV_SPLASH
 				EVENTHANDLER_REGISTER(shutdown_pre_sync,
-				    vt_shutdown_splash, vw, SHUTDOWN_PRI_DEFAULT);
+				    vt_shutdown_splash, vw, SHUTDOWN_PRI_DEFAULT + 1);
 #endif
 			}
 		}
