20aa68698defecebbc6eddc39d2fc2773b6167b9
Jessica Clarke <jrtc27@FreeBSD.org>
2026-04-23 16:40:33 +0100

    arm64/vmm: Enable 16-bit VMIDs when in use by pmap
    pmap_init always uses 16-bit VMIDs when supported, but we never enable
    them in VTCR_EL2 (for ASIDs, locore enables them in TCR_EL1 and
    pmap_init keys off whether they've been enabled, but the order in which
    pmap_init and vmmops_modinit run is reversed). As a result, although the
    full 16-bit value can be stored to VTTBR_EL2 and read back, the upper 8
    bits are treated as 0, and so VMIDs that our VMID allocation believes
    are distinct end up aliasing.
    
    In future this interface may change such that vmm decides on the VMID
    width and tells the pmap to use that, with appropriate support for
    unloading and reloading vmm, but that can come as a follow-up change, as
    this is a more minimal bug fix.
    
    Reviewed by:	markj
    Obtained from:	CheriBSD
    Fixes:		570cd4a2079c ("Import the kernel parts of bhyve/arm64")
    MFC after:	1 week
    Differential Revision:	https://reviews.freebsd.org/D55860

--- sys/arm64/include/pmap.h
+++ sys/arm64/include/pmap.h
@@ -156,6 +156,7 @@ void	*pmap_mapdev_attr(vm_paddr_t pa, vm_size_t size, vm_memattr_t ma);
 bool	pmap_page_is_mapped(vm_page_t m);
 int	pmap_pinit_stage(pmap_t, enum pmap_stage, int);
 bool	pmap_ps_enabled(pmap_t pmap);
+bool	pmap_vs_enabled(void);
 uint64_t pmap_to_ttbr0(pmap_t pmap);
 void	pmap_disable_promotion(vm_offset_t sva, vm_size_t size);
 void	pmap_map_delete(pmap_t, vm_offset_t, vm_offset_t);
