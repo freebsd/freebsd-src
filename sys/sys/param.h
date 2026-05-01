4702f6ab1514335088a849f9f47a3e95e3724a2d
Colin Percival <cperciva@FreeBSD.org>
2026-05-01 00:00:00 +0000

    Post-15.1-branch updates
    Rename stable/15 to -STABLE
    
    Bump __FreeBSD_version
    
    Approved by:	re (implicit)
    Sponsored by:	OpenSats Initiative

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -74,7 +74,7 @@
  * cannot include sys/param.h and should only be updated here.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1500509
+#define __FreeBSD_version 1501500
 
 /*
  * __FreeBSD_kernel__ indicates that this system uses the kernel of FreeBSD,
