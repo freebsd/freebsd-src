eac33929d580444b00b6ac2e351a3c9e7b831a87
Konstantin Belousov <kib@FreeBSD.org>
2026-05-08 17:13:35 +0300

    sys/cdefs.h: move __BEGIN_DECLS/__END_DECLS into a helper sys/_decls.h
    There are situations where nothing from sys/cdefs.h is needed except for
    the declaration braces.  More, the other facilities from sys/cdefs.h
    might unnecessarly pollute the namespace.
    
    Reviewed by:	markj, imp
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D56889

--- sys/sys/cdefs.h
+++ sys/sys/cdefs.h
@@ -61,13 +61,7 @@
 #define	__has_builtin(x)	0
 #endif
 
-#if defined(__cplusplus)
-#define	__BEGIN_DECLS	extern "C" {
-#define	__END_DECLS	}
-#else
-#define	__BEGIN_DECLS
-#define	__END_DECLS
-#endif
+#include <sys/_decls.h>
 
 /*
  * This code has been put in place to help reduce the addition of
