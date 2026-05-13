e5d8b775bbf6f378a591211e373859ec44f74149
Slava Shwartsman <slavash@nvidia.com>
2026-05-13 17:54:51 +0300

    RDMA: Fix link active_speed size
    According to the IB spec active_speed size should be u16 and not u8 as
    before. Changing it to allow further extensions in offered speeds.
    
    Linux commit:
    376ceb31ff87 RDMA: Fix link active_speed size
    
    Reviewed by:    kib
    Tested by:      Wafa Hamzah <wafah@nvidia.com>
    MFC after:      1 week
    Sponsored by:   NVIDIA Networking
    Differential revision: https://reviews.freebsd.org/D57084

--- sys/ofed/include/rdma/ib_verbs.h
+++ sys/ofed/include/rdma/ib_verbs.h
@@ -609,7 +609,7 @@ struct ib_port_attr {
 	u8			subnet_timeout;
 	u8			init_type_reply;
 	u8			active_width;
-	u8			active_speed;
+	u16			active_speed;
 	u8                      phys_state;
 	bool			grh_required;
 };
