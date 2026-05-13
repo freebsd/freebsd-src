29575f2f25465122b40ea87407a0a58617b459c0
Pierre Pronchery <khorben@FreeBSD.org>
2026-05-13 15:30:16 +0200

    contrib/pkgconf: hook spdxtool(1) into the build
    This also integrates spdxtool to the SBOM generation framework.
    As mentioned earlier, this adds the following options:
    
    - JSONLDDIR: Destination for SPDX version 3 files (/usr/share/sbom/jsonld)
    - SPDXTOOL:  Path to spdxtool(1) (for SPDX version 3 files)
    
    Sponsored by:	Alpha Omega, The FreeBSD Foundation

--- share/mk/bsd.sbom.mk
+++ share/mk/bsd.sbom.mk
@@ -3,14 +3,18 @@
 # +++ variables +++
 #
 # BOMTOOL	Tool converting pkg-config files into SPDX version 2 files
+# JSONLDDIR	Destination directory for the SPDX version 3 files
 # SBOMDIR	Source directory for the pkg-config files
 # SPDXDIR	Destination directory for the SPDX version 2 files
+# SPDXTOOL	Tool converting pkg-config files into SPDX version 3 files
 
 .if	${MK_SBOM} != "no"
 
 BOMTOOL?=	bomtool
+JSONLDDIR?=	/usr/share/sbom/jsonld
 SBOMDIR?=	${SRCTOP}/release/sbom/pkgconfig
 SPDXDIR?=	/usr/share/sbom/spdx
+SPDXTOOL?=	spdxtool
 
 .if defined(LIB)
 PCFILE?=	lib${LIB}.pc
@@ -24,12 +28,26 @@ SBOM_TAG_ARGS=	${TAG_ARGS}
 
 .if !empty(PCFILE) && exists(${SBOMDIR}/${PCFILE})
 
+${PCFILE:R}.jsonld: ${SBOMDIR}/${PCFILE}
+	${SPDXTOOL} ${SBOMDIR}/${PCFILE} > ${.TARGET}
+
+jsonldinstall: .PHONY ${PCFILE:R}.jsonld
+	${INSTALL} ${SBOM_TAG_ARGS} -m 0644 ${PCFILE:R}.jsonld \
+		${DESTDIR}${JSONLDDIR}/${PCFILE:R}.jsonld
+
 ${PCFILE:R}.spdx: ${SBOMDIR}/${PCFILE}
 	${BOMTOOL} ${SBOMDIR}/${PCFILE} > ${.TARGET}
 
 spdxinstall: .PHONY ${PCFILE:R}.spdx
 	${INSTALL} ${SBOM_TAG_ARGS} -m 0644 ${PCFILE:R}.spdx \
 		${DESTDIR}${SPDXDIR}/${PCFILE:R}.spdx
+
+.if !defined(NO_JSONLD_SBOM)
+all: ${PCFILE:R}.jsonld
+
+realinstall: jsonldinstall
+.ORDER: beforeinstall jsonldinstall
+.endif
 
 .if !defined(NO_SPDX_SBOM)
 all: ${PCFILE:R}.spdx
