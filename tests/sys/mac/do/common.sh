472d977c2fde4df13e871c54ae9db03f146a72a0
Olivier Certner <olce@FreeBSD.org>
2026-05-26 19:06:55 +0200

    MAC/do: Tests: Fix copyrights
    No comma needed after a single year.  Add SPDX.
    
    Reviewed by:    bapt
    MFC after:      3 days
    Sponsored by:   The FreeBSD Foundation
    Pull Request:   https://ron-dev.freebsd.org/FreeBSD/src/pulls/38
    
    (cherry picked from commit b0c948fe92acc8bd295cc53584e25c082c749cd1)

--- tests/sys/mac/do/common.sh
+++ tests/sys/mac/do/common.sh
@@ -1,5 +1,6 @@
+# Copyright (c) 2026 The FreeBSD Foundation
 #
-# Copyright (c) 2026, The FreeBSD Foundation
+# SPDX-License-Identifier: BSD-2-Clause
 #
 # This software was developed by Olivier Certner <olce@FreeBSD.org> at
 # Kumacom SARL under sponsorship from the FreeBSD Foundation.
