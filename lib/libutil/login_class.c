b5dce0ae4f78251f56ffcb6c6a58b9e6c20380e0
Dag-Erling Smørgrav <des@FreeBSD.org>
2026-05-30 00:06:44 +0200

    login_class: Fix kqueues, pipebuf resource types
    * kqueues is a count but is listed as a size
    
    * pipebuf is a size but is listed as a count
    
    PR:		295623
    MFC after:	1 week
    Fixes:          a4c04958f526 ("libutil: support RLIMIT_PIPEBUF")
    Fixes:          85a0ddfd0b26 ("Add a resource limit for the total...")
    Reviewed by:	kib
    Differential Revision:	https://reviews.freebsd.org/D57333

--- lib/libutil/login_class.c
+++ lib/libutil/login_class.c
@@ -63,9 +63,9 @@ static struct login_res {
     { "vmemoryuse",      login_getcapsize, RLIMIT_VMEM    },
     { "pseudoterminals", login_getcapnum,  RLIMIT_NPTS    },
     { "swapuse",         login_getcapsize, RLIMIT_SWAP    },
-    { "kqueues",         login_getcapsize, RLIMIT_KQUEUES },
+    { "kqueues",         login_getcapnum,  RLIMIT_KQUEUES },
     { "umtxp",           login_getcapnum,  RLIMIT_UMTXP   },
-    { "pipebuf",         login_getcapnum,  RLIMIT_PIPEBUF },
+    { "pipebuf",         login_getcapsize, RLIMIT_PIPEBUF },
     { "vms",             login_getcapnum,  RLIMIT_VMM     },
     { NULL,              0,                0              }
 };
