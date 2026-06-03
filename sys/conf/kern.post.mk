freebsd/freebsd-src

commit 72ab129799a231e322b119de3f9c1263e76527b8

x86: remove sys/mount.h from genassym.c
The header is not needed, and causes some issues with build because it
requires vnode_if.h generated due to transient dependency from vnode.h.

While there, remove unneeded explicit sys/cdefs.h and sys/param.h usage.

Reported and tested by:		thierry
Fixes:	1d5e4020e36e ("vnode: add VIRF_KNOTE flag")
Sponsored by:	The FreeBSD Foundation
MFC after:	1 week

sys/i386/i386/genassym.c
@@ -32,12 +32,10 @@
  * SUCH DAMAGE.
  */
 
-#include <sys/cdefs.h>
 #include "opt_apic.h"
 #include "opt_hwpmc_hooks.h"
 #include "opt_kstack_pages.h"
 
-#include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/assym.h>
 #include <sys/bio.h>
@@ -47,7 +45,6 @@
 #endif
 #include <sys/proc.h>
 #include <sys/errno.h>
-#include <sys/mount.h>
 #include <sys/mutex.h>
 #include <sys/socket.h>
 #include <sys/resourcevar.h>

sys/amd64/amd64/genassym.c
@@ -32,11 +32,9 @@
  * SUCH DAMAGE.
  */
 
-#include <sys/cdefs.h>
 #include "opt_hwpmc_hooks.h"
 #include "opt_kstack_pages.h"
 
-#include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/assym.h>
 #include <sys/bio.h>
@@ -46,7 +44,6 @@
 #include <sys/pmckern.h>
 #endif
 #include <sys/errno.h>
-#include <sys/mount.h>
 #include <sys/mutex.h>
 #include <sys/socket.h>
 #include <sys/resourcevar.h>

commit ce243df20aa8ba94918cafdca2537c8cd18887c8

i386: Fix build (of 'genassym.o')
i386's genassym.c needs to define some assembly symbols holding the size
of NFS structures to support NFS_ROOT while booting with a nfs_diskless
structure.  For this, it needs to include a few NFS headers, which
require definitions from <sys/mount.h> (fhandle_t, vfs_init_t), which
was removed by commit 72ab129799a2 ("x86: remove sys/mount.h from
genassym.c").

Since recently, <sys/mount.h> has been including <sys/vnode.h>, so needs
"vnode_if.h" to have been generated for the compilation of 'genassym.o'
not to fail.  Make sure this is the case (for all architectures for
simplicity) by tweaking the rule for 'genassym.o' in
'sys/conf/kern.post.mk', leaving a comment there so that it can be
removed when i386 is dropped (or if the above-mentioned dependency is
broken).

Fixes:          72ab129799a2 ("x86: remove sys/mount.h from genassym.c")
Sponsored by:   The FreeBSD Foundation

sys/conf/kern.post.mk
@@ -260,7 +260,10 @@ genoffset_test.o: $S/kern/genoffset.c offset.inc
 assym.inc: $S/kern/genassym.sh genassym.o genoffset_test.o
 	NM='${NM}' NMFLAGS='${NMFLAGS}' sh $S/kern/genassym.sh genassym.o > ${.TARGET}
 
-genassym.o: $S/$M/$M/genassym.c  offset.inc
+# vnode_if.h added for the sake of i386 only to fix commit 72ab129799a2 ("x86:
+# remove sys/mount.h from genassym.c"). Can be removed as soon as i386 kernels
+# are dropped, or if the <sys/mount.h> -> <sys/vnode.h> dependency vanishes.
+genassym.o: $S/$M/$M/genassym.c offset.inc vnode_if.h
 	${CC} -c ${NOSAN_CFLAGS:N-flto*:N-fno-common} \
 	    -fcommon $S/$M/$M/genassym.c
 

sys/i386/i386/genassym.c
@@ -45,6 +45,7 @@
 #endif
 #include <sys/proc.h>
 #include <sys/errno.h>
+#include <sys/mount.h>
 #include <sys/mutex.h>
 #include <sys/socket.h>
 #include <sys/resourcevar.h>