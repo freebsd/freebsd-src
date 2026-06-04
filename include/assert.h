freebsd/freebsd-src

commit 439710cf003b7849d2119e9a7694d7b27ab6d70f

assert.h: Revert "Remove leading tabs for whitespace consistency"
This reverts commit 157c184689ea3d7b8b6bd89aff849e94f004aa0e.

As per style(9), a tab goes after #define.  This should not have been
removed.

Reported by:	kib
Fixes:		157c184689ea3d7b8b6bd89aff849e94f004aa0e.
Pull Request:	https://github.com/freebsd/freebsd-src/pull/2203

include/assert.h
@@ -46,11 +46,11 @@
 #undef __assert_unreachable
 
 #ifdef NDEBUG
-#define assert(...)	((void)0)
-#define _assert(...)	((void)0)
+#define	assert(e)	((void)0)
+#define	_assert(e)	((void)0)
 #if __BSD_VISIBLE
-#define __assert_unreachable()	__unreachable()
-#endif /* __BSD_VISIBLE */
+#define	__assert_unreachable()	__unreachable()
+#endif	/* __BSD_VISIBLE */
 #else
 #ifdef __cplusplus
 #if __cplusplus < 202002L
@@ -71,25 +71,25 @@
  * of the remaining guarantees.
  *
  */
-#define __assert_sanitize(...)	((void)0)
+#define	__assert_sanitize(...)	((void)0)
 #else
-#define __assert_sanitize(...)	(void)sizeof(((bool(*)(bool))0)(__VA_ARGS__))
+#define	__assert_sanitize(...)	(void)sizeof(((bool(*)(bool))0)(__VA_ARGS__))
 #endif /* __cplusplus < 202002L */
 #else
-#define __assert_sanitize(...)	(void)sizeof(((_Bool(*)(_Bool))0)(__VA_ARGS__))
+#define	__assert_sanitize(...)	(void)sizeof(((_Bool(*)(_Bool))0)(__VA_ARGS__))
 #endif /* __cplusplus */
-#define assert(...)	(__assert_sanitize(__VA_ARGS__),       \
+#define	assert(...)	(__assert_sanitize(__VA_ARGS__),       \
 			    (__VA_ARGS__) ? (void)0 :          \
 			    __assert(__func__, __FILE__,       \
 			    __LINE__, #__VA_ARGS__))
-#define _assert(...)	assert(__VA_ARGS__)
+#define	_assert(...)	assert(__VA_ARGS__)
 #if __BSD_VISIBLE
-#define __assert_unreachable()	assert(0 && "unreachable segment reached")
-#endif /* __BSD_VISIBLE */
+#define	__assert_unreachable()	assert(0 && "unreachable segment reached")
+#endif	/* __BSD_VISIBLE */
 #endif /* NDEBUG */
 
 #ifndef __STDC_VERSION_ASSERT_H__
-#define __STDC_VERSION_ASSERT_H__ 202311L
+#define	__STDC_VERSION_ASSERT_H__ 202311L
 
 /*
  * Static assertions.  In principle we could define static_assert for
@@ -105,7 +105,7 @@
  */
 #if __ISO_C_VISIBLE >= 2011 && !defined(__cplusplus) && \
     __STDC_VERSION__ < 202311L
-#define static_assert	_Static_assert
+#define	static_assert	_Static_assert
 #endif
 
 __BEGIN_DECLS

commit 7487932f4fbc5a71231d3b1fc93d160253f38c83

assert.h: style(9): Space after #define, between #endif and comment
style(9) still allows TAB after #define but this is a historical
artifact and by far the minority of uses cases.  Going forward, we would
like to promote the use of a single space, as it allows alignment to
survive line prefixing (such as in diffs).

style(9) also has prescribed a single space between '#else' or '#endif'
and a comment recalling the guard since 2002.

So, commit 157c184689ea ("assert.h: Remove leading tabs for whitespace
consistency") was good, and in line with rules about whitespace changes
(since the file was heavily modified by surrounding commits).

This commit is thus basically a revert of 439710cf003b ("assert.h:
Revert "Remove leading tabs for whitespace consistency"), which extended
replacing spaces with TABs in the code introduced in the meantime (after
commit 157c184689ea).

Reviewed by:    fuz, imp
Fixes:          439710cf003b ("assert.h: Revert "Remove leading tabs for whitespace consistency")
MAC after:      3 days
Differential Revision:  https://reviews.freebsd.org/D57391

include/assert.h
@@ -46,11 +46,11 @@
 #undef __assert_unreachable
 
 #ifdef NDEBUG
-#define	assert(e)	((void)0)
-#define	_assert(e)	((void)0)
+#define assert(e)	((void)0)
+#define _assert(e)	((void)0)
 #if __BSD_VISIBLE
-#define	__assert_unreachable()	__unreachable()
-#endif	/* __BSD_VISIBLE */
+#define __assert_unreachable()	__unreachable()
+#endif /* __BSD_VISIBLE */
 #else
 #ifdef __cplusplus
 #if __cplusplus < 202002L
@@ -71,25 +71,25 @@
  * of the remaining guarantees.
  *
  */
-#define	__assert_sanitize(...)	((void)0)
+#define __assert_sanitize(...)	((void)0)
 #else
-#define	__assert_sanitize(...)	(void)sizeof(((bool(*)(bool))0)(__VA_ARGS__))
+#define __assert_sanitize(...)	(void)sizeof(((bool(*)(bool))0)(__VA_ARGS__))
 #endif /* __cplusplus < 202002L */
 #else
-#define	__assert_sanitize(...)	(void)sizeof(((_Bool(*)(_Bool))0)(__VA_ARGS__))
+#define __assert_sanitize(...)	(void)sizeof(((_Bool(*)(_Bool))0)(__VA_ARGS__))
 #endif /* __cplusplus */
-#define	assert(...)	(__assert_sanitize(__VA_ARGS__),       \
+#define assert(...)	(__assert_sanitize(__VA_ARGS__),       \
 			    (__VA_ARGS__) ? (void)0 :          \
 			    __assert(__func__, __FILE__,       \
 			    __LINE__, #__VA_ARGS__))
-#define	_assert(...)	assert(__VA_ARGS__)
+#define _assert(...)	assert(__VA_ARGS__)
 #if __BSD_VISIBLE
-#define	__assert_unreachable()	assert(0 && "unreachable segment reached")
-#endif	/* __BSD_VISIBLE */
+#define __assert_unreachable()	assert(0 && "unreachable segment reached")
+#endif /* __BSD_VISIBLE */
 #endif /* NDEBUG */
 
 #ifndef __STDC_VERSION_ASSERT_H__
-#define	__STDC_VERSION_ASSERT_H__ 202311L
+#define __STDC_VERSION_ASSERT_H__ 202311L
 
 /*
  * Static assertions.  In principle we could define static_assert for
@@ -105,7 +105,7 @@
  */
 #if __ISO_C_VISIBLE >= 2011 && !defined(__cplusplus) && \
     __STDC_VERSION__ < 202311L
-#define	static_assert	_Static_assert
+#define static_assert	_Static_assert
 #endif
 
 __BEGIN_DECLS