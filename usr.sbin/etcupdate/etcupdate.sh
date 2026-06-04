freebsd/freebsd-src

commit 6d65c91b9a4743bbffc3508bf754e68b65bbd494

etcupdate: fix arguments order of diff command
Due to misplacement of the second -L argument of diff command, it is
treated by getopt_long as an error.

Also add -l option for a diff command that alters the way it shows
differences. Instead of printing full diff, it reports changed file
the same way as added/removed files are reported.

Reviewed by:	imp
Obtained from:	Yandex LLC
MFC after:	1 week
Differential Revision:	https://reviews.freebsd.org/D56708

usr.sbin/etcupdate/etcupdate.sh
@@ -527,9 +527,15 @@ diffnode()
 			echo
 			;;
 		$COMPARE_DIFFFILES)
+			if [ -n "$difflistonly" ]; then
+				echo
+				echo "Changed: $3"
+				echo
+				break;
+			fi
 			echo "Index: $3"
 			rule "="
-			diff -u $diffargs -L "$3 ($4)" $1/$3 -L "$3 ($5)" $2/$3
+			diff -u $diffargs -L "$3 ($4)" -L "$3 ($5)" $1/$3 $2/$3
 			;;
 	esac
 }
@@ -1787,10 +1793,14 @@ ignore=
 nobuild=
 preworld=
 noroot=
-while getopts "d:m:nprs:t:A:BD:FI:L:M:N" option; do
+difflistonly=
+while getopts "d:lm:nprs:t:A:BD:FI:L:M:N" option; do
 	case "$option" in
 		d)
 			WORKDIR=$OPTARG
+			;;
+		l)
+			difflistonly=YES
 			;;
 		m)
 			MAKE_CMD=$OPTARG

usr.sbin/etcupdate/etcupdate.8
@@ -23,7 +23,7 @@
 .\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 .\" SUCH DAMAGE.
 .\"
-.Dd July 15, 2024
+.Dd April 30, 2026
 .Dt ETCUPDATE 8
 .Os
 .Sh NAME
@@ -51,6 +51,7 @@
 .Ar tarball
 .Nm
 .Cm diff
+.Op Fl l
 .Op Fl d Ar workdir
 .Op Fl D Ar destdir
 .Op Fl I Ar patterns
@@ -468,6 +469,10 @@ shell pattern.
 This option may be specified multiple times to specify multiple patterns.
 Multiple space-separated patterns may also be specified in a single
 option.
+.It Fl l
+In
+.Cm diff
+mode list files that are changed locally, omit printing content difference.
 .It Fl L Ar logfile
 Specify an alternate path for the log file.
 The

commit a85e39030f8c7faa3d5a33373389440de6f0fff7

etcupdate: Make diff -l actually work
While here, remove unnecessary blank lines.

MFC after:	1 week
Fixes:          6d65c91b9a47 ("etcupdate: fix arguments order of diff command")
Reviewed by:	Boris Lytochkin <lytboris@gmail.com>
Differential Revision:	https://reviews.freebsd.org/D57330

usr.sbin/etcupdate/etcupdate.sh
@@ -500,42 +500,32 @@ diffnode()
 		$COMPARE_EQUAL)
 			;;
 		$COMPARE_ONLYFIRST)
-			echo
 			echo "Removed: $3"
-			echo
 			;;
 		$COMPARE_ONLYSECOND)
-			echo
 			echo "Added: $3"
-			echo
 			;;
 		$COMPARE_DIFFTYPE)
 			first=`file_type $1/$3`
 			second=`file_type $2/$3`
-			echo
 			echo "Node changed from a $first to a $second: $3"
-			echo
 			;;
 		$COMPARE_DIFFLINKS)
 			first=`readlink $1/$file`
 			second=`readlink $2/$file`
-			echo
 			echo "Link changed: $file"
 			rule "="
 			echo "-$first"
 			echo "+$second"
-			echo
 			;;
 		$COMPARE_DIFFFILES)
 			if [ -n "$difflistonly" ]; then
-				echo
 				echo "Changed: $3"
-				echo
-				break;
+			else
+				echo "Index: $3"
+				rule "="
+				diff -u $diffargs -L "$3 ($4)" -L "$3 ($5)" $1/$3 $2/$3
 			fi
-			echo "Index: $3"
-			rule "="
-			diff -u $diffargs -L "$3 ($4)" -L "$3 ($5)" $1/$3 $2/$3
 			;;
 	esac
 }