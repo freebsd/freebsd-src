1e25cda7f06923d05e28dac8eb1c1c428a5c92dc
Siva Mahadevan <siva@FreeBSD.org>
2026-06-10 18:29:02 -0400

    libc: fix strtold NaN representation on riscv
    Regenerate gd_qnan.h on riscv using the qnan.c
    config tool found in contrib/gdtoa.
    
    This fixes the following tests in CI:
    lib/libc/stdio/scanfloat_test:infinities_and_nans
    lib/libc/stdlib/strtod_test:strtold_nan
    
    Reviewed by:	jrtc27
    MFC after:	3 days
    Differential Revision:	https://reviews.freebsd.org/D57405

--- lib/libc/riscv/gd_qnan.h
+++ lib/libc/riscv/gd_qnan.h
@@ -9,11 +9,11 @@
 #define d_QNAN0 0x0
 #define d_QNAN1 0x7ff80000
 #define ld_QNAN0 0x0
-#define ld_QNAN1 0x7ff80000
+#define ld_QNAN1 0x0
 #define ld_QNAN2 0x0
-#define ld_QNAN3 0x0
+#define ld_QNAN3 0x7fff8000
 #define ldus_QNAN0 0x0
 #define ldus_QNAN1 0x0
 #define ldus_QNAN2 0x0
-#define ldus_QNAN3 0x7ff8
+#define ldus_QNAN3 0x0
 #define ldus_QNAN4 0x0
