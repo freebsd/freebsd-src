652f4d4c1e400aac30ed4c0017540d932936c16f
Gordon Bergling <gbe@FreeBSD.org>
2026-06-14 10:52:48 +0200

    acpi(4): Fix a typo in a source code comment
    - s/freqency/frequency/
    
    (cherry picked from commit f2574978567e63a1eb518c6d325ddf424a22a5e0)

--- sys/dev/acpica/acpi_timer.c
+++ sys/dev/acpica/acpi_timer.c
@@ -344,7 +344,7 @@ acpi_timer_get_timecount_safe(struct timecounter *tc)
 }
 
 /*
- * Timecounter freqency adjustment interface.
+ * Timecounter frequency adjustment interface.
  */ 
 static int
 acpi_timer_sysctl_freq(SYSCTL_HANDLER_ARGS)
