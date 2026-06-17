72a6197df998bb6aa36cf33aa44f6d5b50247537
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2026-06-17 20:43:01 +0200

    linuxkpi: Define `ULL()`
    It simply appends "ULL" to its argument.
    
    The amdgpu DRM driver used it at some point in the development cycle of
    Linux 6.14 but the use case was dropped later. Let's still add it to
    linuxkpi because it will help if we need to do a git bisect in drm-kmod.
    
    Reviewed by:	emaste
    Sponsored by:	The FreeBSD Foundation
    Differential Revision: https://reviews.freebsd.org/D57703

---
+++ sys/compat/linuxkpi/common/include/linux/const.h
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2026 The FreeBSD Foundation
+ */
+
+#ifndef _LINUXKPI_LINUX_CONST_H
+#define	_LINUXKPI_LINUX_CONST_H
+
+#include <vdso/const.h>
+
+#endif /* _LINUXKPI_LINUX_CONST_H */
