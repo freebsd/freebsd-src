c31042a7a944fb7a39404fa9f1b68b2befaa919c
Dag-Erling Smørgrav <des@FreeBSD.org>
2026-06-19 09:26:20 +0200

    ldns: Update to 1.9.2
    Merge commit '3750aed65c1f5a610b44d29c92236ca119b62780'
    
    (cherry picked from commit 63442ceae5ed31ad45e401f5fe0010b824bc2b38)

--- contrib/ldns/ldns/util.h
+++ contrib/ldns/ldns/util.h
@@ -27,8 +27,8 @@ extern "C" {
 #define dprintf(X,Y) fprintf(stderr, (X), (Y))
 /* #define	dprintf(X, Y)  */
 
-#define LDNS_VERSION "1.9.1"
-#define LDNS_REVISION ((1<<16)|(9<<8)|(1))
+#define LDNS_VERSION "1.9.2"
+#define LDNS_REVISION ((1<<16)|(9<<8)|(2))
 
 /**
  * splint static inline workaround
