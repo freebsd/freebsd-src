8163944f2739733a0bb9265416a3687ec33691ff
Christos Margiolis <christos@FreeBSD.org>
2026-06-24 16:47:05 +0300

    sound: Retire unused SD_F_EQ_PC
    This flag is neither set anywhere, nor is there a way to set it from
    userland, so it is effectively useless, and currently the effect is that
    EQ can only be enabled for primary playback channels.
    
    Retire the flag and keep this behavior, and think later whether we want
    to allow virtual channels to get their own EQ feeder as well.
    
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    
    (cherry picked from commit ede0236d3b34a89bae7795f0f45696d4d94ce138)

--- sys/dev/sound/pcm/feeder_chain.c
+++ sys/dev/sound/pcm/feeder_chain.c
@@ -725,10 +725,8 @@ feeder_chain(struct pcm_channel *c)
 
 		/* Soft EQ only applicable for PLAY. */
 		if (cdesc.dummy == 0 &&
-		    c->direction == PCMDIR_PLAY && (d->flags & SD_F_EQ_ENABLED) &&
-		    (((d->flags & SD_F_EQ_PC) &&
-		    !(c->flags & CHN_F_HAS_VCHAN)) ||
-		    (!(d->flags & SD_F_EQ_PC) && !(c->flags & CHN_F_VIRTUAL))))
+		    c->direction == PCMDIR_PLAY &&
+		    (d->flags & SD_F_EQ_ENABLED) && !(c->flags & CHN_F_VIRTUAL))
 			cdesc.use_eq = 1;
 
 		if (FEEDFORMAT_NE_REQUIRED(&cdesc)) {
