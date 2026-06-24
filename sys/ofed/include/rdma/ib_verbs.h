b6354f04081823cc0ccf2af573a59a826d7763c8
John Baldwin <jhb@FreeBSD.org>
2026-06-24 15:30:56 -0400

    OFED: Calculate UDP source port based on flow label or lqpn/rqpn
    Calculate and set UDP source port based on the flow label. If flow label
    is not defined in GRH then calculate it based on lqpn/rqpn.
    
    Tested by:	Wafa Hamzah <wafah@nvidia.com> (mlx5_ib)
    Tested by:	John Baldwin <jhb@FreeBSD.org> (iw_cxgbe)
    Obtained from:	Linux commit 18451db82ef7f943c60a7fce685f16172bda5106
    Sponsored by:	Chelsio Communications

--- sys/ofed/include/rdma/ib_verbs.h
+++ sys/ofed/include/rdma/ib_verbs.h
@@ -4223,4 +4223,21 @@ static inline u32 rdma_calc_flow_label(u32 lqpn, u32 rqpn)
 
 	return (u32)(v & IB_GRH_FLOWLABEL_MASK);
 }
+
+/**
+ * rdma_get_udp_sport - Calculate and set UDP source port based on the flow
+ *                      label. If flow label is not defined in GRH then
+ *                      calculate it based on lqpn/rqpn.
+ *
+ * @fl:                 flow label from GRH
+ * @lqpn:               local qp number
+ * @rqpn:               remote qp number
+ */
+static inline u16 rdma_get_udp_sport(u32 fl, u32 lqpn, u32 rqpn)
+{
+	if (!fl)
+		fl = rdma_calc_flow_label(lqpn, rqpn);
+
+	return rdma_flow_label_to_udp_sport(fl);
+}
 #endif /* IB_VERBS_H */
