markjdb/freebsd

commit c6a0eb7ada62c7d6e7d4686b2fe32e009311c1aa

hwpstate_amd(4): Rename '*set_autonomous_hwp*()' => 'enable_cppc*()'
This is to better reflect that we are really enabling CPPC in these
functions and because we are likely to stop activating CPPC autonomous
mode by default in the near future.

No functional change (intended).

Sponsored by:   The FreeBSD Foundation

sys/x86/cpufreq/hwpstate_amd.c
@@ -698,7 +698,7 @@ struct set_autonomous_hwp_data {
 };
 
 static void
-amd_set_autonomous_hwp_cb(void *args)
+enable_cppc_cb(void *args)
 {
 	struct set_autonomous_hwp_data *const data = args;
 	struct hwpstate_softc *const sc = data->sc;
@@ -765,7 +765,7 @@ amd_set_autonomous_hwp_cb(void *args)
 }
 
 static int
-amd_set_autonomous_hwp(struct hwpstate_softc *sc)
+enable_cppc(struct hwpstate_softc *sc)
 {
 	const device_t dev = sc->dev;
 	const u_int cpuid = cpu_get_pcpu(dev)->pc_cpuid;
@@ -775,7 +775,7 @@ amd_set_autonomous_hwp(struct hwpstate_softc *sc)
 
 	data.sc = sc;
 	smp_rendezvous_cpu(cpuid, smp_no_rendezvous_barrier,
-	    amd_set_autonomous_hwp_cb, smp_no_rendezvous_barrier, &data);
+	    enable_cppc_cb, smp_no_rendezvous_barrier, &data);
 
 	if (hwp_has_error(data.res, HWP_ERROR_CPPC_ENABLE)) {
 		device_printf(dev, "CPU%u: Failed to enable CPPC!\n", cpuid);
@@ -908,7 +908,7 @@ hwpstate_attach(device_t dev)
 
 	sc = device_get_softc(dev);
 	if ((sc->flags & HWPFL_USE_CPPC) != 0) {
-		if ((res = amd_set_autonomous_hwp(sc)))
+		if ((res = enable_cppc(sc)) != 0)
 			return (res);
 		SYSCTL_ADD_PROC(device_get_sysctl_ctx(dev),
 		    SYSCTL_STATIC_CHILDREN(_debug), OID_AUTO,

freebsd/freebsd-src

commit 7b107346448a7df2446bc67bf0bc2a6acaef5cbf

hwpstate_amd(4): Rename remaining 'struct set_autonomous_hwp_data'
For consistency.

No functional change (intended).

Fixes:          c6a0eb7ada62 ("hwpstate_amd(4): Rename '*set_autonomous_hwp*()' => 'enable_cppc*()'")
Event:          Halifax Hackathon 202606
Location:       jrm@'s living room
Sponsored by:   The FreeBSD Foundation

sys/x86/cpufreq/hwpstate_amd.c
@@ -793,7 +793,7 @@ hwpstate_identify(driver_t *driver, device_t parent)
 		device_printf(parent, "hwpstate: add child failed\n");
 }
 
-struct set_autonomous_hwp_data {
+struct enable_cppc_data {
 	/* Inputs */
 	struct hwpstate_softc *sc;
 	/* Outputs */
@@ -808,7 +808,7 @@ struct set_autonomous_hwp_data {
 static void
 enable_cppc_cb(void *args)
 {
-	struct set_autonomous_hwp_data *const data = args;
+	struct enable_cppc_data *const data = args;
 	struct hwpstate_softc *const sc = data->sc;
 	uint64_t lowest_perf, highest_perf;
 	int error;
@@ -890,7 +890,7 @@ enable_cppc(struct hwpstate_softc *sc)
 {
 	const device_t dev = sc->dev;
 	const u_int cpuid = sc->cpuid;
-	struct set_autonomous_hwp_data data;
+	struct enable_cppc_data data;
 	struct sbuf sbs;
 	struct sbuf *sb;
 