freebsd/freebsd-src

commit ac3ede5371af34b0a89fa72b7a5bb706457b99ad

x86/xen: remove PVHv1 code
PVHv1 was officially removed from Xen in 4.9, so just axe the related
code from FreeBSD.

Note FreeBSD supports PVHv2, which is the replacement for PVHv1.

Sponsored by: Citrix Systems R&D
Reviewed by: kib, Elliott Mitchell
Differential Revision: https://reviews.freebsd.org/D30228

sys/x86/include/init.h
@@ -41,9 +41,6 @@ struct init_ops {
 	void	(*early_clock_source_init)(void);
 	void	(*early_delay)(int);
 	void	(*parse_memmap)(caddr_t, vm_paddr_t *, int *);
-	void	(*mp_bootaddress)(vm_paddr_t *, unsigned int *);
-	int	(*start_all_aps)(void);
-	void	(*msi_init)(void);
 };
 
 extern struct init_ops init_ops;

sys/amd64/amd64/machdep.c
@@ -187,13 +187,6 @@ struct init_ops init_ops = {
 	.early_clock_source_init =	i8254_init,
 	.early_delay =			i8254_delay,
 	.parse_memmap =			native_parse_memmap,
-#ifdef SMP
-	.mp_bootaddress =		mp_bootaddress,
-	.start_all_aps =		native_start_all_aps,
-#endif
-#ifdef DEV_PCI
-	.msi_init =			msi_init,
-#endif
 };
 
 /*
@@ -1283,8 +1276,9 @@ getmemsize(caddr_t kmdp, u_int64_t first)
 	 * is configured to support APs and APs for the system start
 	 * in real mode mode (e.g. SMP bare metal).
 	 */
-	if (init_ops.mp_bootaddress)
-		init_ops.mp_bootaddress(physmap, &physmap_idx);
+#ifdef SMP
+	mp_bootaddress(physmap, &physmap_idx);
+#endif
 
 	/* call pmap initialization to make new kernel address space */
 	pmap_bootstrap(&first);

sys/amd64/amd64/mp_machdep.c
@@ -245,7 +245,7 @@ cpu_mp_start(void)
 	mptramp_la57 = la57;
 
 	/* Start each Application Processor */
-	init_ops.start_all_aps();
+	start_all_aps();
 
 	set_interrupt_apic_ids();
 
@@ -397,7 +397,7 @@ mp_realloc_pcpu(int cpuid, int domain)
  * start each AP in our list
  */
 int
-native_start_all_aps(void)
+start_all_aps(void)
 {
 	u_int64_t *pt5, *pt4, *pt3, *pt2;
 	u_int32_t mpbioswarmvec;

sys/amd64/include/smp.h
@@ -38,7 +38,7 @@ inthand_t
 	IDTVEC(rendezvous_pti);
 
 void	invlop_handler(void);
-int	native_start_all_aps(void);
+int	start_all_aps(void);
 void	mp_bootaddress(vm_paddr_t *, unsigned int *);
 
 #endif /* !LOCORE */

sys/conf/files
@@ -3545,7 +3545,6 @@ dev/xen/netback/netback.c	optional xenhvm
 dev/xen/netfront/netfront.c	optional xenhvm
 dev/xen/xenpci/xenpci.c		optional xenpci
 dev/xen/timer/timer.c		optional xenhvm
-dev/xen/pvcpu/pvcpu.c		optional xenhvm
 dev/xen/xenstore/xenstore.c	optional xenhvm
 dev/xen/xenstore/xenstore_dev.c	optional xenhvm
 dev/xen/xenstore/xenstored_dev.c	optional xenhvm

sys/conf/files.amd64
@@ -387,8 +387,6 @@ dev/viawd/viawd.c		optional	viawd
 dev/vmd/vmd.c			optional	vmd
 dev/vmd/vmd_bus.c		optional	vmd_bus
 dev/wbwd/wbwd.c			optional	wbwd
-dev/xen/pci/xen_acpi_pci.c	optional	xenhvm
-dev/xen/pci/xen_pci.c		optional	xenhvm
 isa/syscons_isa.c		optional	sc
 isa/vga_isa.c			optional	vga
 kern/imgact_aout.c		optional compat_aout
@@ -461,8 +459,6 @@ x86/x86/mptable.c		optional	mptable
 x86/x86/mptable_pci.c		optional	mptable pci
 x86/x86/msi.c			optional	pci
 x86/xen/pv.c			optional	xenhvm
-x86/xen/pvcpu_enum.c		optional	xenhvm
-x86/xen/xen_pci_bus.c		optional	xenhvm
 
 compat/linuxkpi/common/src/linux_fpu.c		optional compat_linuxkpi \
 	compile-with "${LINUXKPI_C}"

sys/conf/files.x86
@@ -328,5 +328,3 @@ x86/xen/hvm.c			optional	xenhvm
 x86/xen/xen_intr.c		optional	xenhvm
 x86/xen/xen_apic.c		optional	xenhvm
 x86/xen/xenpv.c			optional	xenhvm
-x86/xen/xen_msi.c		optional	xenhvm
-x86/xen/xen_nexus.c		optional	xenhvm

sys/dev/xen/pci/xen_acpi_pci.c
@@ -1,97 +1,0 @@
-/*
- * Copyright (c) 2014 Roger Pau Monné <roger.pau@citrix.com>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
-#include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
-
-#include <sys/param.h>
-#include <sys/systm.h>
-#include <sys/bus.h>
-#include <sys/kernel.h>
-#include <sys/malloc.h>
-#include <sys/module.h>
-
-#include <contrib/dev/acpica/include/acpi.h>
-#include <contrib/dev/acpica/include/accommon.h>
-
-#include <dev/acpica/acpivar.h>
-#include <dev/acpica/acpi_pcivar.h>
-
-#include <sys/pciio.h>
-#include <dev/pci/pcireg.h>
-#include <dev/pci/pcivar.h>
-#include <dev/pci/pci_private.h>
-
-#include <xen/xen-os.h>
-#include <xen/hypervisor.h>
-#include <xen/xen_pci.h>
-
-#include "pcib_if.h"
-#include "pci_if.h"
-
-static int
-xen_acpi_pci_probe(device_t dev)
-{
-
-	if (!xen_pv_domain())
-		return (ENXIO);
-	if (acpi_get_handle(dev) == NULL)
-		return (ENXIO);
-
-	device_set_desc(dev, "Xen ACPI PCI bus");
-
-	return (BUS_PROBE_SPECIFIC);
-}
-
-static void
-xen_acpi_pci_child_added(device_t dev, device_t child)
-{
-
-	acpi_pci_child_added(dev, child);
-	xen_pci_child_added_method(dev, child);
-}
-
-static device_method_t xen_acpi_pci_methods[] = {
-	/* Device interface */
-	DEVMETHOD(device_probe,		xen_acpi_pci_probe),
-
-	/* PCI interface overwrites */
-	DEVMETHOD(pci_enable_msi,	xen_pci_enable_msi_method),
-	DEVMETHOD(pci_disable_msi,	xen_pci_disable_msi_method),
-	DEVMETHOD(pci_child_added,	xen_acpi_pci_child_added),
-
-	DEVMETHOD_END
-};
-
-static devclass_t pci_devclass;
-
-DECLARE_CLASS(acpi_pci_driver);
-DEFINE_CLASS_1(pci, xen_acpi_pci_driver, xen_acpi_pci_methods,
-    sizeof(struct pci_softc), acpi_pci_driver);
-DRIVER_MODULE(xen_acpi_pci, pcib, xen_acpi_pci_driver, pci_devclass, 0, 0);
-MODULE_DEPEND(xen_acpi_pci, pci, 1, 1, 1);
-MODULE_DEPEND(xen_acpi_pci, acpi, 1, 1, 1);
-MODULE_VERSION(xen_acpi_pci, 1);

commit ad26a56cf2f55967cc73d04a6ea17c27892d3141

acpi_timer(4): Remove unused 'acpi_timer_disabled' boolean
Same reason as for the previous commit to acpi_cpu(4).  This boolean is
not used anywhere.  Disabling acpi_timer(4) can be done through the
regular ACPI disable mechanism (using the 'debug.acpi.disabled' tunable,
see acpi_disabled()).

Reviewed by:    emaste (implicit)
Fixes:          ac3ede5371af ("x86/xen: remove PVHv1 code")
MFC after:      3 days
Event:          Halifax Hackathon 202606
Location:       Dalhousie CS Faculty building
Sponsored by:   The FreeBSD Foundation

sys/dev/acpica/acpi_timer.c
@@ -64,9 +64,6 @@ static eventhandler_tag		acpi_timer_eh;
 
 static u_int	acpi_timer_frequency = 14318182 / 4;
 
-/* Knob to disable acpi_timer device */
-bool acpi_timer_disabled = false;
-
 static void	acpi_timer_identify(driver_t *driver, device_t parent);
 static int	acpi_timer_probe(device_t dev);
 static int	acpi_timer_attach(device_t dev);
@@ -125,8 +122,7 @@ acpi_timer_identify(driver_t *driver, device_t parent)
     ACPI_FUNCTION_TRACE((char *)(uintptr_t)__func__);
 
     if (acpi_disabled("timer") || (acpi_quirks & ACPI_Q_TIMER) ||
-	acpi_timer_dev || acpi_timer_disabled ||
-	AcpiGbl_FADT.PmTimerLength == 0)
+	acpi_timer_dev || AcpiGbl_FADT.PmTimerLength == 0)
 	return_VOID;
 
     if ((dev = BUS_ADD_CHILD(parent, 2, "acpi_timer", 0)) == NULL) {

sys/x86/include/init.h
@@ -43,7 +43,4 @@ struct init_ops {
 
 extern struct init_ops init_ops;
 
-/* Knob to disable acpi_timer device */
-extern bool acpi_timer_disabled;
-
 #endif /* __X86_INIT_H__ */