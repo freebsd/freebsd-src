d6c4cea7740d5c5c673a06ba37e4f1bdcddb2ece
Timo Völker <timo.voelker@fh-muenster.de>
2026-06-28 12:50:15 +0200

    loopback: improve checksum offloading
    * Allow disabling IFCAP_RXCSUM_IPV6 or IFCAP_TXCSUM_IPV6.
    * Do not pretend the checksum is correct by setting the LO_CSUM_SET
      flags if IFCAP_RXCSUM_IPV6 or IFCAP_RXCSUM is enabled. Instead,
      remove the LO_CSUM_SET flags (in case they have been set somehow)
      if IFCAP_RXCSUM_IPV6 or IFCAP_RXCSUM is disabled.
    * Do not unset the transmit checksum offload flags LO_CSUM_FEATURES or
      LO_CSUM_FEATURES6 since they now have a meaning for the receive path.
    
    Reviewed by:		glebius, pouria, tuexen
    Okayed by:		bz
    MFC after:		1 week
    Differential Revision:	https://reviews.freebsd.org/D57518

--- sys/net/if_loop.c
+++ sys/net/if_loop.c
@@ -229,38 +229,22 @@ looutput(struct ifnet *ifp, struct mbuf *m, const struct sockaddr *dst,
 	else
 		af = RO_GET_FAMILY(ro, dst);
 
-#if 1	/* XXX */
 	switch (af) {
 	case AF_INET:
-		if (ifp->if_capenable & IFCAP_RXCSUM) {
-			m->m_pkthdr.csum_data = 0xffff;
-			m->m_pkthdr.csum_flags = LO_CSUM_SET;
+		if ((ifp->if_capenable & IFCAP_RXCSUM) == 0) {
+			m->m_pkthdr.csum_flags &= ~LO_CSUM_SET;
 		}
-		m->m_pkthdr.csum_flags &= ~LO_CSUM_FEATURES;
 		break;
 	case AF_INET6:
-#if 0
-		/*
-		 * XXX-BZ for now always claim the checksum is good despite
-		 * any interface flags.   This is a workaround for 9.1-R and
-		 * a proper solution ought to be sought later.
-		 */
-		if (ifp->if_capenable & IFCAP_RXCSUM_IPV6) {
-			m->m_pkthdr.csum_data = 0xffff;
-			m->m_pkthdr.csum_flags = LO_CSUM_SET;
+		if ((ifp->if_capenable & IFCAP_RXCSUM_IPV6) == 0) {
+			m->m_pkthdr.csum_flags &= ~LO_CSUM_SET;
 		}
-#else
-		m->m_pkthdr.csum_data = 0xffff;
-		m->m_pkthdr.csum_flags = LO_CSUM_SET;
-#endif
-		m->m_pkthdr.csum_flags &= ~LO_CSUM_FEATURES6;
 		break;
 	default:
 		printf("looutput: af=%d unexpected\n", af);
 		m_freem(m);
 		return (EAFNOSUPPORT);
 	}
-#endif
 	return (if_simloop(ifp, m, af, 0));
 }
 
@@ -420,29 +404,15 @@ loioctl(struct ifnet *ifp, u_long cmd, caddr_t data)
 			ifp->if_capenable ^= IFCAP_RXCSUM;
 		if ((mask & IFCAP_TXCSUM) != 0)
 			ifp->if_capenable ^= IFCAP_TXCSUM;
-		if ((mask & IFCAP_RXCSUM_IPV6) != 0) {
-#if 0
+		if ((mask & IFCAP_RXCSUM_IPV6) != 0)
 			ifp->if_capenable ^= IFCAP_RXCSUM_IPV6;
-#else
-			error = EOPNOTSUPP;
-			break;
-#endif
-		}
-		if ((mask & IFCAP_TXCSUM_IPV6) != 0) {
-#if 0
+		if ((mask & IFCAP_TXCSUM_IPV6) != 0)
 			ifp->if_capenable ^= IFCAP_TXCSUM_IPV6;
-#else
-			error = EOPNOTSUPP;
-			break;
-#endif
-		}
 		ifp->if_hwassist = 0;
 		if (ifp->if_capenable & IFCAP_TXCSUM)
 			ifp->if_hwassist = LO_CSUM_FEATURES;
-#if 0
 		if (ifp->if_capenable & IFCAP_TXCSUM_IPV6)
 			ifp->if_hwassist |= LO_CSUM_FEATURES6;
-#endif
 		break;
 
 	default:
