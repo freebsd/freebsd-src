3de9dc5bf4b438e0d98222dc028982380d5a25d2
Kyle Evans <kevans@FreeBSD.org>
2026-06-30 08:12:51 -0500

    libc: gen: add a test for rtld underflowing our posix_spawn thread
    This is a distillation of the environment described in the PR, using
    a dummy shlib and mapping it repeatedly.  This takes advantage of the
    guard page added in 2767a1f3686e5b16 to reliably crash if rtld tries to
    scale its stack usage excessively with the # DSOs loaded.
    
    PR:		295991
    Reviewed by:	kib
    Differential Revision:	https://reviews.freebsd.org/D57954

---
+++ lib/libc/tests/gen/libdummy/libdummy.c
@@ -0,0 +1,14 @@
+/*
+ *
+ * Copyright (C) 2026 Kyle Evans <kevans@FreeBSD.org>
+ *
+ * SPDX-license-Identifier: BSD-2-Clause
+ */
+
+int dummy_random(void);
+
+int
+dummy_random(void)
+{
+	return (42);
+}
