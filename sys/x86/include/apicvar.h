7b675fe7306db65581b3da800fc07c4cc7def134
Koine Yuusuke <koinec@yahoo.co.jp>
2026-07-01 17:28:21 +0200

    x86/local_apic.c: Add support for installing a thermal interrupt handler
    The thermal interrupt is initially masked.
    
    Thermal interrupt handling is enabled by calling lapic_enable_thermal(),
    which installs a (single) handler.
    
    [olce: Wrote the commit message.]
    
    Reviewed by:    kib, olce
    MFC after:      2 weeks
    Differential Revision:  https://reviews.freebsd.org/D44454
    
    (cherry picked from commit 87ba088fa3108dd180008a04f25760ec71476c87)

--- sys/x86/include/apicvar.h
+++ sys/x86/include/apicvar.h
@@ -184,12 +184,12 @@ struct apic_enumerator {
 inthand_t
 	IDTVEC(apic_isr1), IDTVEC(apic_isr2), IDTVEC(apic_isr3),
 	IDTVEC(apic_isr4), IDTVEC(apic_isr5), IDTVEC(apic_isr6),
-	IDTVEC(apic_isr7), IDTVEC(cmcint), IDTVEC(errorint),
-	IDTVEC(spuriousint), IDTVEC(timerint),
+	IDTVEC(apic_isr7), IDTVEC(cmcint), IDTVEC(thermalint),
+	IDTVEC(errorint), IDTVEC(spuriousint), IDTVEC(timerint),
 	IDTVEC(apic_isr1_pti), IDTVEC(apic_isr2_pti), IDTVEC(apic_isr3_pti),
 	IDTVEC(apic_isr4_pti), IDTVEC(apic_isr5_pti), IDTVEC(apic_isr6_pti),
-	IDTVEC(apic_isr7_pti), IDTVEC(cmcint_pti), IDTVEC(errorint_pti),
-	IDTVEC(spuriousint_pti), IDTVEC(timerint_pti);
+	IDTVEC(apic_isr7_pti), IDTVEC(cmcint_pti), IDTVEC(thermalint_pti),
+	IDTVEC(errorint_pti), IDTVEC(spuriousint_pti), IDTVEC(timerint_pti);
 
 extern vm_paddr_t lapic_paddr;
 extern int *apic_cpuids;
@@ -198,6 +198,7 @@ extern int *apic_cpuids;
 extern void (*ipi_vectored)(u_int, int);
 
 typedef struct ioapic *ioapic_drv_t;
+typedef void (*lapic_thermal_handle_function)(int, void *);
 
 void	apic_register_enumerator(struct apic_enumerator *enumerator);
 ioapic_drv_t	ioapic_create(vm_paddr_t addr, int32_t apic_id, int intbase);
@@ -232,6 +233,8 @@ void	apic_enable_vector(u_int apic_id, u_int vector);
 void	apic_disable_vector(u_int apic_id, u_int vector);
 void	apic_free_vector(u_int apic_id, u_int vector, u_int irq);
 void	lapic_calibrate_timer(void);
+void	lapic_enable_thermal(lapic_thermal_handle_function thermfunc, void *value);
+void	lapic_disable_thermal(void);
 int	lapic_enable_pmc(void);
 void	lapic_disable_pmc(void);
 void	lapic_reenable_pmc(void);
@@ -256,6 +259,7 @@ int	lapic_set_lvt_polarity(u_int apic_id, u_int lvt,
 int	lapic_set_lvt_triggermode(u_int apic_id, u_int lvt,
 	    enum intr_trigger trigger);
 void	lapic_handle_cmc(void);
+void	lapic_handle_thermal(void);
 void	lapic_handle_error(void);
 void	lapic_handle_intr(int vector, struct trapframe *frame);
 void	lapic_handle_timer(struct trapframe *frame);
