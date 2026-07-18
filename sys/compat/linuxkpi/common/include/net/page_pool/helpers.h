e591a76621430d6d29f2580e2dec8991e450ed5e
Bjoern A. Zeeb <bz@FreeBSD.org>
2026-07-18 11:54:42 +0000

    LinuxKPI: page pool updates and add to the build
    Split implementation out from the header files.
    
    This "page pool" is the very minimalistic version we need in order to
    support packets on mt76.
    We allocate the page pool in order to have the meta data available of
    which we only make limited use.
    This implementation does no pooling, it does no page fragments for now,
    it always hands out a full page and frees it upon return.
    
    It is written in a way that it can be in the tree before the 'struct page'
    work it depends on has landed in order to reduce friction for people who
    want to try mt7921 (or others later) upfront.  We use the same #ifdef
    as in the struct page work for that reason so one knob will turn everything
    on or off.
    
    Once the struct page work has landed and settled we can start filling this
    with more complexity.
    
    In the unlikely event that in the mean time any other consumer would start
    showing up they will have to be aware that the current code as-is
    essentially is a NOP without the 'struct page' work.  A WARN_ONCE() will
    notify them.
    
    Sponsored by:	The FreeBSD Foundation
    MFC after:	3 days

--- sys/compat/linuxkpi/common/include/net/page_pool/helpers.h
+++ sys/compat/linuxkpi/common/include/net/page_pool/helpers.h
@@ -1,5 +1,9 @@
-/*-
+/*
  * Copyright (c) 2023-2025 Bjoern A. Zeeb
+ * Copyright (c) 2025-2026 The FreeBSD Foundation
+ *
+ * Portions of this software were developed by Björn Zeeb
+ * under sponsorship from the FreeBSD Foundation.
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -12,58 +16,57 @@
 #include <linux/dma-mapping.h>
 #include <net/page_pool/types.h>
 
+struct page_pool *linuxkpi_page_pool_create(const struct page_pool_params *);
+void linuxkpi_page_pool_destroy(struct page_pool *);
+
+struct page *linuxkpi_page_pool_dev_alloc_frag(struct page_pool *,
+    uint32_t *, size_t);
+void linuxkpi_page_pool_put_full_page(struct page_pool *, struct page *, bool);
+dma_addr_t linuxkpi_page_pool_get_dma_addr(const struct page *);
+
+
 static inline struct page_pool *
 page_pool_create(const struct page_pool_params *ppparams)
 {
-
-	pr_debug("%s: TODO\n", __func__);
-	return (NULL);
+	return (linuxkpi_page_pool_create(ppparams));
 }
 
 static inline void
-page_pool_destroy(struct page_pool *ppool)
+page_pool_destroy(struct page_pool *pp)
 {
-
-	pr_debug("%s: TODO\n", __func__);
+	linuxkpi_page_pool_destroy(pp);
 }
 
+
 static inline struct page *
-page_pool_dev_alloc_frag(struct page_pool *ppool, uint32_t *offset,
+page_pool_dev_alloc_frag(struct page_pool *pp, uint32_t *offset,
     size_t size)
 {
+	return (linuxkpi_page_pool_dev_alloc_frag(pp, offset, size));
+}
 
-	pr_debug("%s: TODO\n", __func__);
-	return (NULL);
+static inline void
+page_pool_put_full_page(struct page_pool *pp, struct page *page,
+    bool allow_direct)
+{
+	linuxkpi_page_pool_put_full_page(pp, page, allow_direct);
 }
 
 static inline dma_addr_t
-page_pool_get_dma_addr(struct page *page)
+page_pool_get_dma_addr(const struct page *page)
 {
-
-	pr_debug("%s: TODO\n", __func__);
-	return (0);
+	return (linuxkpi_page_pool_get_dma_addr(page));
 }
 
 static inline enum dma_data_direction
-page_pool_get_dma_dir(const struct page_pool *ppool)
+page_pool_get_dma_dir(const struct page_pool *pp)
 {
-
-	pr_debug("%s: TODO\n", __func__);
-	return (DMA_BIDIRECTIONAL);
-}
-
-static inline void
-page_pool_put_full_page(struct page_pool *ppool, struct page *page,
-    bool allow_direct)
-{
-
-	pr_debug("%s: TODO\n", __func__);
+	return (pp->params.dma_dir);
 }
 
 static inline int
 page_pool_ethtool_stats_get_count(void)
 {
-
 	pr_debug("%s: TODO\n", __func__);
 	return (0);
 }
@@ -71,7 +74,6 @@ page_pool_ethtool_stats_get_count(void)
 static inline uint8_t *
 page_pool_ethtool_stats_get_strings(uint8_t *x)
 {
-
 	pr_debug("%s: TODO\n", __func__);
 	return (x);
 }
