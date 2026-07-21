049699e1b46b3745d8462fbefffaa1eefa2584c8
Pierre Pronchery <khorben@FreeBSD.org>
2026-07-21 12:41:46 +0200

    SBOM: generate the .pc files
    As suggested by kaniini on GitHub.
    
    This is still work in progress.
    
    Sponsored by:   The FreeBSD Foundation

--- share/mk/bsd.sbom.mk
+++ share/mk/bsd.sbom.mk
@@ -4,36 +4,55 @@
 #
 # BOMTOOL	Tool converting pkg-config files into SPDX version 2 files
 # JSONLDDIR	Destination directory for the SPDX version 3 files
-# SBOMDIR	Source directory for the pkg-config files
+# SBOMDIR	Destination directory for the SBOM files
 # SPDXDIR	Destination directory for the SPDX version 2 files
 # SPDXTOOL	Tool converting pkg-config files into SPDX version 3 files
 
 .if	${MK_SBOM} != "no"
 
 BOMTOOL?=	bomtool
-JSONLDDIR?=	/usr/share/sbom/spdx-3.0.1
-SBOMDIR?=	${SRCTOP}/share/sbom/pkgconfig
-SPDXDIR?=	/usr/share/sbom/spdx-2.2
+JSONLDDIR?=	${SBOMDIR}/spdx-3.0.1
+SBOMDIR?=	/usr/share/sbom
+SPDXDIR?=	${SBOMDIR}/spdx-2.2
 SPDXTOOL?=	spdxtool
 TOOLARGS=	--define-variable=FREEBSD_VERSION=${OS_REVISION}
+_UCLFILE=	${SRCTOP}/packages/${PACKAGE:Uutilities}/${PACKAGE:Ucommon}.ucl
+
+# meta-data
+.if exists(_UCLFILE)
+SBOM_COPYRIGHT!=sed -n '/^ \* Copyright /{ s/^ \* //; p; }' ${_UCLFILE}
+SBOM_DESC!=	sed -n '/^comment *= *"/{ s/^comment *= *"\(.*\)"$$/\1/; p; }' ${_UCLFILE}
+SBOM_LICENSE!=	sed -n '/^ \* SPDX-License-Identifier: */{ s/^ \* SPDX-License-Identifier: //; p; }' ${_UCLFILE}
+.endif # exists(_UCLFILE)
+SBOM_COPYRIGHT?=Copyright (c) 2026 The FreeBSD Project
+SBOM_DESC?=	${SBOM_NAME}
+SBOM_LICENSE?=	BSD-2-Clause
+SBOM_VERSION?=	${FREEBSD_RELEASE}
+SBOM_SOURCE?=	https://cgit.FreeBSD.org/src/tree
+.if !empty(MAN)
+SBOM_URL?=	https://man.FreeBSD.org/cgi/man.cgi?query=${MAN:S,\.\([0-9a-z]\)\+,(\1),}
+.endif
 
 . if defined(LIB)
-SBOMNAME?=	lib${LIB}
+SBOM_NAME?=	lib${LIB}
+SBOM_KIND?=
 SBOMTAGS=	package=${PACKAGE:Uutilities},lib
 . elif defined(PROG)
-SBOMNAME?=	${PROG}
+SBOM_NAME?=	${PROG}
+SBOM_KIND?=	tool
 SBOMTAGS=	package=${PACKAGE:Uutilities}
 . else
-SBOMNAME=
+SBOM_NAME=
+SBOM_KIND?=
 SBOMTAGS=
 . endif # defined(PROG) || defined(LIB)
 
-. if !empty(SBOMNAME)
-SBOMFILE?=	${SBOMNAME}.pc
-. endif # !empty(SBOMNAME)
+. if !empty(SBOM_NAME)
+SBOMFILE?=	${SBOM_NAME}.pc
+. endif # !empty(SBOM_NAME)
 SBOM_TAG_ARGS=	-T ${SBOMTAGS:ts,:[*]}
 
-. if !empty(SBOMFILE) && exists(${SBOMDIR}/${SBOMFILE})
+. if !empty(SBOMFILE)
 
 _SBOMREQUIRES=
 .  for l in ${LIBADD}
@@ -42,34 +61,53 @@ _SBOMREQUIRES+=	lib${l}
 SBOMREQUIRES=	${_SBOMREQUIRES:S/ /,/gW}
 TOOLARGS+=	--define-variable=SBOM_REQUIRES=${SBOMREQUIRES:Q}
 
+sbom/${SBOMFILE}:
+	mkdir -p ${.OBJDIR}/sbom
+	(echo "# Copyright (c) 2026 The FreeBSD Project"; echo "#";	\
+	echo "# ${SBOM_LICENSE}"; echo "#";				\
+	echo "Copyright: ${SBOM_COPYRIGHT}";				\
+	echo "Name: ${SBOM_NAME}";					\
+	echo "Description: ${SBOM_DESC}";				\
+	echo "Version: ${SBOM_VERSION}";				\
+	echo "License: ${SBOM_LICENSE}";				\
+	[ -z "${SBOM_SOURCE}" ] || echo "Source: ${SBOM_SOURCE}";	\
+	[ -z "${SBOM_URL}" ] || echo "URL: ${SBOM_URL}";		\
+	[ -z "${SBOM_KIND}" ] || echo "Kind: ${SBOM_KIND}";		\
+	[ -z "${SBOM_REQUIRES}" ] || echo "Requires: ${SBOM_REQUIRES}") \
+	> ${.TARGET}
+
 .  if !defined(NO_JSONLD_SBOM)
-all: ${SBOMNAME}.jsonld
+JSONLDFILE?= ${SBOMFILE:R}.jsonld
 
-${SBOMNAME}.jsonld: ${SBOMDIR}/${SBOMFILE}
-	${SPDXTOOL} ${TOOLARGS} ${SBOMDIR}/${SBOMFILE} > ${.TARGET}
+all: sbom/${JSONLDFILE}
 
-jsonldinstall: .PHONY ${SBOMNAME}.jsonld
-	${INSTALL} ${SBOM_TAG_ARGS} -m 0644 ${SBOMNAME}.jsonld \
-		${DESTDIR}${JSONLDDIR}/${SBOMNAME}.jsonld
+sbom/${JSONLDFILE}: sbom/${SBOMFILE}
+	${SPDXTOOL} ${TOOLARGS} sbom/${SBOMFILE} > ${.TARGET}
+
+jsonldinstall: .PHONY sbom/${SBOM_NAME}.jsonld
+	${INSTALL} ${SBOM_TAG_ARGS} -m 0644 sbom/${SBOMFILE:R}.jsonld \
+		${DESTDIR}${JSONLDDIR}/${SBOMFILE:R}.jsonld
 
 realinstall: jsonldinstall
 .ORDER: beforeinstall jsonldinstall
 .  endif # !defined(NO_JSONLD_SBOM)
 
 .  if !defined(NO_SPDX_SBOM)
-all: ${SBOMNAME}.spdx
+SPDXFILE?= ${SBOMFILE:R}.spdx
 
-${SBOMNAME}.spdx: ${SBOMDIR}/${SBOMFILE}
-	${BOMTOOL} ${TOOLARGS} ${SBOMDIR}/${SBOMFILE} > ${.TARGET}
+all: sbom/${SPDXFILE}
 
-spdxinstall: .PHONY ${SBOMNAME}.spdx
-	${INSTALL} ${SBOM_TAG_ARGS} -m 0644 ${SBOMNAME}.spdx \
-		${DESTDIR}${SPDXDIR}/${SBOMFILE:R}.spdx
+sbom/${SPDXFILE}: sbom/${SBOMFILE}
+	${BOMTOOL} ${TOOLARGS} sbom/${SBOMFILE} > ${.TARGET}
+
+spdxinstall: .PHONY sbom/${SPDXFILE}
+	${INSTALL} ${SBOM_TAG_ARGS} -m 0644 sbom/${SPDXFILE} \
+		${DESTDIR}${SPDXDIR}/${SPDXFILE}
 
 realinstall: spdxinstall
 .ORDER: beforeinstall spdxinstall
 .  endif # !defined(NO_SPDX_SBOM)
 
-. endif	# exists(${SBOMDIR}/${SBOMFILE})
+. endif	# !empty(SBOMFILE)
 
 .endif	# ${MK_SBOM}
