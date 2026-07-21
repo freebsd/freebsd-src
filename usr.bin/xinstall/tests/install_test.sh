d34870708db9fa1eb8e29b5e085b755de1189b1f
Dag-Erling Smørgrav <des@FreeBSD.org>
2026-07-21 08:03:44 +0000

    install: Allow installing stdin
    If from_name is "/dev/stdin" or "-" and the target is not a directory,
    skip the comparison and copy data from standard input to the target.
    
    MFC after:	1 week
    Reviewed by:	imp
    Differential Revision:	https://reviews.freebsd.org/D58348

--- usr.bin/xinstall/tests/install_test.sh
+++ usr.bin/xinstall/tests/install_test.sh
@@ -557,6 +557,36 @@ digest_body() {
 	done
 }
 
+atf_test_case null
+null_head() {
+	atf_set "descr" "Install empty file"
+}
+null_body() {
+	atf_check mkdir dst
+	atf_check -s exit:71 -e not-empty install /dev/null dst
+	atf_check install /dev/null dst/file
+	atf_check test -f dst/file
+	atf_check test ! -s dst/file
+}
+
+atf_test_case stdin
+stdin_head() {
+	atf_set "descr" "Install stdin"
+}
+stdin_body() {
+	atf_check mkdir dst
+	echo "The Magic Words are Squeamish Ossifrage" >file
+	atf_check -s exit:71 -e not-empty install - dst <file
+	atf_check test ! -e dst/file
+	atf_check install - dst/file <file
+	atf_check cmp -s file dst/file
+	atf_check rm dst/file
+	atf_check -s exit:71 -e not-empty install /dev/stdin dst <file
+	atf_check test ! -e dst/file
+	atf_check install /dev/stdin dst/file <file
+	atf_check cmp -s file dst/file
+}
+
 atf_init_test_cases() {
 	atf_add_test_case incompatible_opts
 	atf_add_test_case copy_to_empty
@@ -605,4 +635,6 @@ atf_init_test_cases() {
 	atf_add_test_case set_optional_exec
 	atf_add_test_case metalog
 	atf_add_test_case digest
+	atf_add_test_case null
+	atf_add_test_case stdin
 }
