c4d8b894dd5ef27e19926bcb7f4e937e15f443c5
Dag-Erling Smørgrav <des@FreeBSD.org>
2026-07-21 08:03:05 +0000

    nvme: Explicitly cast caddr_t values
    Sponsored by:	Klara, Inc.
    Sponsored by:	NetApp, Inc.
    MFC after:	1 week
    Fixes:		6d0001d44490 ("nvme: add support for DIOCGIDENT")
    Reviewed by:	bnovkov, imp
    Differential Revision:	https://reviews.freebsd.org/D58357
    
    (cherry picked from commit bd30d1ad78e152d0a963bc873643a911321cca6e)

--- sys/dev/nvme/nvme_ns.c
+++ sys/dev/nvme/nvme_ns.c
@@ -90,7 +90,7 @@ nvme_ns_ioctl(struct cdev *cdev, u_long cmd, caddr_t arg, int flag,
 		break;
 	}
 	case DIOCGIDENT: {
-		uint8_t *sn = arg;
+		uint8_t *sn = (uint8_t *)arg;
 		nvme_ctrlr_get_ident(ctrlr, sn);
 		break;
 	}
