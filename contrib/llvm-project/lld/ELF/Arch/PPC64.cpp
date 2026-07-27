aa3b76871e9644c86b5dc16c3a42215447a29853
Jessica Clarke <jrtc27@FreeBSD.org>
2026-07-27 17:54:04 +0100

    Merge commit 26bf39cdba0b from llvm-project (by Jessica Clarke):
      [ELF][PowerPC] Don't assume TOC pointer is valid in IPLT entries (#207555)
    
      Unlike normal PLT entries, IPLT entries can be called indirectly even
      when in PIEs/DSOs, and so there's no guarantee on what's in the TOC
      pointer register at that time. Therefore we must emit variants of the
      existing code that work without it, whether r12-relative (playing the
      same role as MIPS's $25) in the same number of instructions, or first
      retrieving PC in an i386-like manner, being careful not to clobber LR.
      On 32-bit PowerPC even direct calls to IPLT entries face the same issue,
      since we'd use the TOC base of the resolver, which may not be the same
      as the caller, even within the same object.
    
      Normal canonical PLTs still look broken on 64-bit PowerPC as they use
      the TOC pointer register too, and similarly on 32-bit PowerPC for PIEs.
      We should probably treat these cases the same as PIE on i386 (except
      including PDEs for 64-bit PowerPC), where it's an error due to the use
      of %ebx in PLT entries.
    
    Bump LLD_FREEBSD_VERSION for this fix as otherwise an existing system
    linker will be deemed new enough to use and produce broken kernels for
    TARGET=powerpc (regardless of TARGET_ARCH/MACHINE/MACHINE_ARCH) builds.
    
    PR:		294369
    MFC after:	1 week
    
    (cherry picked from commit b68f2fe1932cbc9809245e2c5a2db2bc0944cc5d)

--- contrib/llvm-project/lld/ELF/Arch/PPC64.cpp
+++ contrib/llvm-project/lld/ELF/Arch/PPC64.cpp
@@ -1167,8 +1167,9 @@ void PPC64::writePlt(uint8_t *buf, const Symbol &sym,
 }
 
 void PPC64::writeIplt(uint8_t *buf, const Symbol &sym,
-                      uint64_t /*pltEntryAddr*/) const {
-  writePPC64LoadAndBranch(ctx, buf, sym.getGotPltVA(ctx));
+                      uint64_t pltEntryAddr) const {
+  writePPC64LoadAndBranch(ctx, buf, pltEntryAddr, sym.getGotPltVA(ctx),
+                          /*toc=*/false);
 }
 
 static std::pair<RelType, uint64_t> toAddr16Rel(RelType type, uint64_t val) {
