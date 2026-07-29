9facb187c9aa02ced20338bf69c6f6e59ac1030a
Jessica Clarke <jrtc27@FreeBSD.org>
2026-07-29 15:09:42 +0100

    link_elf: Make phdrs first page check actually fatal
    Otherwise we'll print an error but carry on regardless, presumably
    destined to walk off the end of the mapping.
    
    Reported by:	thebugfixers@pm.me
    MFC after:	1 week
    
    (cherry picked from commit 1e39a314d870e312f623199e146eda6bdbc293a3)

--- sys/kern/link_elf.c
+++ sys/kern/link_elf.c
@@ -1086,8 +1086,11 @@ link_elf_load_file(linker_class_t cls, const char* filename,
 	 */
 	if (!((hdr->e_phentsize == sizeof(Elf_Phdr)) &&
 	      (hdr->e_phoff + hdr->e_phnum*sizeof(Elf_Phdr) <= PAGE_SIZE) &&
-	      (hdr->e_phoff + hdr->e_phnum*sizeof(Elf_Phdr) <= nbytes)))
+	      (hdr->e_phoff + hdr->e_phnum*sizeof(Elf_Phdr) <= nbytes))) {
 		link_elf_error(filename, "Unreadable program headers");
+		error = ENOEXEC;
+		goto out;
+	}
 
 	/*
 	 * Scan the program header entries, and save key information.
