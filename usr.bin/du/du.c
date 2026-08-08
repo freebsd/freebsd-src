freebsd/freebsd-src

commit d1588599c02456200eae5f7fcf7f9d309f7bac8e

Report the next directory being scanned when a ^T is pressed (or any SIGINFO).  Provides some progress report for the impatient.  This won't report that we're blocking in our walk due to disk/network problems, however.  There's no really good way to report that condition that I'm aware of...

usr.bin/du/du.c
@@ -77,10 +77,12 @@ static void	prthumanval(int64_t);
 static void	ignoreadd(const char *);
 static void	ignoreclean(void);
 static int	ignorep(FTSENT *);
+static void	siginfo(int __unused);
 
 static int	nodumpflag = 0;
 static int	Aflag;
 static long	blocksize, cblocksize;
+static volatile sig_atomic_t info;
 
 int
 main(int argc, char *argv[])
@@ -248,6 +250,8 @@ main(int argc, char *argv[])
 
 	rval = 0;
 
+	(void)signal(SIGINFO, siginfo);
+
 	if ((fts = fts_open(argv, ftsoptions, NULL)) == NULL)
 		err(1, "fts_open");
 
@@ -277,6 +281,10 @@ main(int argc, char *argv[])
 					    cblocksize, blocksize),
 					    p->fts_path);
 				}
+			}
+			if (info) {
+				info = 0;
+				(void)printf("\t%s\n", p->fts_path);
 			}
 			break;
 		case FTS_DC:			/* Ignore. */
@@ -530,4 +538,11 @@ ignorep(FTSENT *ent)
 		if (fnmatch(ign->mask, ent->fts_name, 0) != FNM_NOMATCH)
 			return 1;
 	return 0;
+}
+
+static void
+siginfo(int sig __unused)
+{
+
+	info = 1;
 }

commit fd79bf63442eefd2c3bfb695a377dbd705f5cc6d

du: Print progress information to stderr
* On SIGINFO, print the current path to stderr rather than stdout.

* Do so immediately, instead of the next time we finish a directory.

* Document this behavior in the manual page.

PR:		296861
MFC after:	1 week
Fixes:          d1588599c024 ("Report the next directory being scanned ...")
Reviewed by:	wollman
Differential Revision:	https://reviews.freebsd.org/D58702

usr.bin/du/du.1
@@ -25,7 +25,7 @@
 .\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 .\" SUCH DAMAGE.
 .\"
-.Dd June 9, 2026
+.Dd August 7, 2026
 .Dt DU 1
 .Os
 .Sh NAME
@@ -195,6 +195,16 @@ and
 .Fl Fl si
 options all override each other; the last one specified determines
 the block counts used.
+.Pp
+If
+.Nm
+receives a
+.Dv SIGINFO
+(see the
+.Cm status
+argument for
+.Xr stty 1 )
+signal, the current file will be written to the standard error output.
 .Sh ENVIRONMENT
 .Bl -tag -width BLOCKSIZE
 .It Ev BLOCKSIZE

usr.bin/du/du.c
@@ -270,6 +270,10 @@ main(int argc, char *argv[])
 	xo_open_container("disk-usage-information");
 	xo_open_list("paths");
 	for (errno = 0; (p = fts_read(fts)) != NULL; errno = 0) {
+		if (info) {
+			info = 0;
+			(void)fprintf(stderr, "\t%s\n", p->fts_path);
+		}
 		switch (p->fts_info) {
 		case FTS_D:			/* Ignore. */
 			if (ignorep(p))
@@ -284,10 +288,6 @@ main(int argc, char *argv[])
 			if (p->fts_level <= depth && check_threshold(p))
 				print_file_size(p);
 
-			if (info) {
-				info = 0;
-				(void)printf("\t%s\n", p->fts_path);
-			}
 			break;
 		case FTS_DC:			/* Ignore. */
 			break;