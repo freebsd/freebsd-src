0b59bbcd52dacdc7c4aeb0f29ef400f8e854b8a3
Kevin Bowling <kbowling@FreeBSD.org>
2026-08-11 12:00:51 -0700

    e1000: Avoid a signed multicast bitmap shift
    The multicast hash bit can be 31.  Use an unsigned value so setting the
    bit cannot shift a signed integer into its sign bit.
    
    (cherry picked from commit a86d65b2c99bab3fe46389b3b51ad27956dccfe9)

--- sys/dev/e1000/e1000_mac.c
+++ sys/dev/e1000/e1000_mac.c
@@ -611,7 +611,7 @@ void e1000_update_mc_addr_list_generic(struct e1000_hw *hw,
 		hash_reg = (hash_value >> 5) & (hw->mac.mta_reg_count - 1);
 		hash_bit = hash_value & 0x1F;
 
-		hw->mac.mta_shadow[hash_reg] |= (1 << hash_bit);
+		hw->mac.mta_shadow[hash_reg] |= 1U << hash_bit;
 		mc_addr_list += (ETHER_ADDR_LEN);
 	}
 
