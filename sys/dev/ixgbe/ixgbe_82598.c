43c32e31aa58c57ab50e7cde187c320d90a45a3e
Kevin Bowling <kbowling@FreeBSD.org>
2026-08-11 12:36:08 -0700

    ixgbe: Use unsigned register bitmap shifts
    VLAN, VMDq, and VF reset bit indices can reach 31.  Use unsigned
    values when constructing their 32-bit register masks so the shifts do
    not operate on signed integers.
    
    (cherry picked from commit bf6feffef6c16559333048859b24547acea3dba5)

--- sys/dev/ixgbe/ixgbe_82598.c
+++ sys/dev/ixgbe/ixgbe_82598.c
@@ -1025,7 +1025,7 @@ s32 ixgbe_set_vfta_82598(struct ixgbe_hw *hw, u32 vlan, u32 vind,
 
 	/* Set the nibble for VMD queue index */
 	bits = IXGBE_READ_REG(hw, IXGBE_VFTAVIND(vftabyte, regindex));
-	bits &= (~(0x0F << bitindex));
+	bits &= ~(0x0FU << bitindex);
 	bits |= (vind << bitindex);
 	IXGBE_WRITE_REG(hw, IXGBE_VFTAVIND(vftabyte, regindex), bits);
 
@@ -1035,10 +1035,10 @@ s32 ixgbe_set_vfta_82598(struct ixgbe_hw *hw, u32 vlan, u32 vind,
 	bits = IXGBE_READ_REG(hw, IXGBE_VFTA(regindex));
 	if (vlan_on)
 		/* Turn on this VLAN id */
-		bits |= (1 << bitindex);
+		bits |= 1U << bitindex;
 	else
 		/* Turn off this VLAN id */
-		bits &= ~(1 << bitindex);
+		bits &= ~(1U << bitindex);
 	IXGBE_WRITE_REG(hw, IXGBE_VFTA(regindex), bits);
 
 	return IXGBE_SUCCESS;
