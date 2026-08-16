28ca279559d3ff80af69cb3e1e32cb3e6246be47
Bjoern A. Zeeb <bz@FreeBSD.org>
2026-08-16 22:05:42 +0000

    LinuxKPI: add can_wakeup option and accessor functions
    We can implement device_set_wakeup_capable() in the !CONFIG_PM_SLEEP
    case;  we do not have the infrastructure in place for the CONFIG_PM_SLEEP
    case so leave a pr_debug TODO.
    
    Needed by an upcoming wireless driver.
    
    Reviewed by:	dumbbell
    Differential Revision: https://reviews.freebsd.org/D58883
    
    (cherry picked from commit a424fb3519ebf8fb972e7ba45ba19df322992b00)

--- sys/compat/linuxkpi/common/src/linux_compat.c
+++ sys/compat/linuxkpi/common/src/linux_compat.c
@@ -2818,6 +2818,12 @@ device_can_wakeup(struct device *dev)
 	return (false);
 }
 
+void
+linuxkpi_device_set_wakeup_capable(struct device *dev, bool capable)
+{
+	dev->power.can_wakeup = capable;
+}
+
 static void
 devm_device_group_remove(struct device *dev, void *p)
 {
