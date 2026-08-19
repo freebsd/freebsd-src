0670e41433afd804df1b7333d72ae299ae50642f
MrNasTech <mrnascara@gmail.com>
2026-08-18 23:07:52 -0400

    Create nspire_drivers.h

---
+++ sys/arm/Nspire-cx-2/nspire_drivers.h
@@ -0,0 +1,7 @@
+// define stuff for nspire cx, like where everything is in memory space 
+#define NSP_RAM 0xA4000000
+#define NSP_SD 0x10000000
+#define NSP_IO 0xB8000000 // memory-mapped i/o ports....
+
+// todo: add more useless variables whenever they are needed..
+// also i LOVE netbsd "run everywhere" should be the slogan..
