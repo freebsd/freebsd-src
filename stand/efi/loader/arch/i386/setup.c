ad9b4cc631aba4396db9e9087918f77cafd88f35
Ahmad Khalifa <vexeduxr@FreeBSD.org>
2026-08-28 17:30:18 +0300

    update email address in my copyright notices

--- stand/efi/loader/arch/i386/setup.c
+++ stand/efi/loader/arch/i386/setup.c
@@ -1,7 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause
  *
- * Copyright (c) 2024 Ahmad Khalifa <ahmadkhalifa570@gmail.com>
+ * Copyright (c) 2024 Ahmad Khalifa <vexeduxr@FreeBSD.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
