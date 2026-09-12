9315ab37fdc1ea7ffb0aeb7835ba1de937c047e4
Baptiste Daroussin <bapt@FreeBSD.org>
2026-09-12 09:01:56 +0200

    video(4): wake the kqueue knote when a buffer completes
    selwakeup() only wakes select/poll waiters; it does not notify the kqueue
    knote registered on the device, so EVFILT_READ never fired when a frame
    became available.

--- sys/dev/video/video.c
+++ sys/dev/video/video.c
@@ -303,6 +303,7 @@ video_buf_done(struct video_buf *vb, size_t bytesused, uint32_t sequence)
 	vb->state = VB_DONE;
 	STAILQ_INSERT_TAIL(&vd->done, vb, entry);
 	selwakeup(&vd->sel);
+	KNOTE_LOCKED(&vd->sel.si_note, 0);
 	wakeup(&vd->done);
 	mtx_unlock(&vd->mtx);
 }
@@ -323,6 +324,7 @@ video_buf_error(struct video_buf *vb)
 	vb->state = VB_ERROR;
 	STAILQ_INSERT_TAIL(&vd->done, vb, entry);
 	selwakeup(&vd->sel);
+	KNOTE_LOCKED(&vd->sel.si_note, 0);
 	wakeup(&vd->done);
 	mtx_unlock(&vd->mtx);
 }
