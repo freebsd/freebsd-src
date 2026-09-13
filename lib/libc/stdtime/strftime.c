6b26fcc32f1577c6dd62c5851e06c11ff33c0073
Dag-Erling Smørgrav <des@FreeBSD.org>
2026-09-13 15:29:59 +0200

    tzcode: Update to 2026d
    MFC after:	1 week
    
    (cherry picked from commit 212e3524943222650688bd1f49d5eb4c9326de7f)
    
    libc/stdtime: Catch up with tzcode 2026d
    
    Fixes:		212e35249432 ("tzcode: Update to 2026c")
    (cherry picked from commit 1689b875b4b80c152cf7d3652c72335a2149173e)

--- lib/libc/stdtime/strftime.c
+++ lib/libc/stdtime/strftime.c
@@ -386,9 +386,7 @@ label:
 						int	bot;
 						int	top;
 
-						len = isleap_sum(year, base) ?
-							DAYSPERLYEAR :
-							DAYSPERNYEAR;
+						len = year_sum_days(year, base);
 						/*
 						 * What yday (-3 ... 3) does
 						 * the ISO year begin on?
@@ -415,9 +413,7 @@ label:
 							break;
 						}
 						--base;
-						yday += isleap_sum(year, base) ?
-							DAYSPERLYEAR :
-							DAYSPERNYEAR;
+						yday += year_sum_days(year, base);
 					}
 #ifdef XPG4_1994_04_09
 					if ((w == 52 &&
