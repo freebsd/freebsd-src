0b7215cfa262a1c21026e5dc791ec8235c83324a
Dag-Erling Smørgrav <des@FreeBSD.org>
2026-09-13 14:27:18 +0200

    graid3: Restore lock acquisition
    The lock acquisition in g_raid3_ctl_insert() was improperly dropped a
    while ago, making it impossible to add or replace a device in an
    existing graid3.  This went unnoticed because the tests are broken.
    
    MFC after:	1 week
    Fixes:		fcf69f3dbce6 ("Consistently use gctl_get_provider instead of home-grown variants.")
    Event:		EuroBSDcon 2026 DevSummit
    Reviewed by:	delphij
    Differential Revision:	https://reviews.freebsd.org/D59563
    
    (cherry picked from commit 9094f86b57a7e4b356cae83f35c8d41c842b42a2)

--- sys/geom/raid3/g_raid3_ctl.c
+++ sys/geom/raid3/g_raid3_ctl.c
@@ -426,6 +426,7 @@ g_raid3_ctl_insert(struct gctl_req *req, struct g_class *mp)
 		no = gctl_get_paraml(req, "number", sizeof(*no));
 	else
 		no = NULL;
+	g_topology_lock();
 	gp = g_new_geomf(mp, "raid3:insert");
 	gp->orphan = g_raid3_ctl_insert_orphan;
 	cp = g_new_consumer(gp);
