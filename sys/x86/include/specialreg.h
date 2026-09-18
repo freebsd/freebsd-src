9aa031eb927720e26c8c11ce697f1c691ca0ef75
Konstantin Belousov <kib@FreeBSD.org>
2026-09-18 17:33:54 +0300

    x86: add definitions for the CORE_CAP and MEMORY_CTL MSRs
    Reviewed by:	markj
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Differential revision:	https://reviews.freebsd.org/D59815

--- sys/x86/include/specialreg.h
+++ sys/x86/include/specialreg.h
@@ -575,9 +575,15 @@
 #define	IA32_ARCH_CAP_TSX_CTRL	0x00000080
 #define	IA32_ARCH_CAP_TAA_NO	0x00000100
 
+/* MSR IA32_CORE_CAP bits (platform) */
+#define	IA32_CORE_CAP_SPLITLOCK	0x00000020
+
 /* MSR IA32_TSX_CTRL bits */
 #define	IA32_TSX_CTRL_RTM_DISABLE	0x00000001
 #define	IA32_TSX_CTRL_TSX_CPUID_CLEAR	0x00000002
+
+/* MSR MEMORY_CTL platform bits */
+#define	MSR_MEMORY_CTL_SPLITLOCK	0x20000000
 
 /*
  * CPUID manufacturers identifiers
@@ -607,6 +613,7 @@
 #define	MSR_APICBASE		0x01b
 #define	MSR_EBL_CR_POWERON	0x02a
 #define	MSR_TEST_CTL		0x033
+#define	MSR_MEMORY_CTL		0x033	/* Platform, same index as TEST */
 #define	MSR_IA32_FEATURE_CONTROL 0x03a
 #define	MSR_IA32_SPEC_CTRL	0x048
 #define	MSR_IA32_PRED_CMD	0x049
@@ -618,6 +625,7 @@
 #define	MSR_PERFCTR0		0x0c1
 #define	MSR_PERFCTR1		0x0c2
 #define	MSR_PLATFORM_INFO	0x0ce
+#define	MSR_IA32_CORE_CAP	0x0cf
 #define	MSR_MPERF		0x0e7
 #define	MSR_APERF		0x0e8
 #define	MSR_IA32_EXT_CONFIG	0x0ee	/* Undocumented. Core Solo/Duo only */
