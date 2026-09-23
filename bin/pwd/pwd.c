markjdb/freebsd

commit 2df923c5d2d02350abc29f61b603c5b9615b225c

pwd: Clean up and adopt POSIX semantics
According to POSIX, the default should be -L.  Based on code history,
whoever first wrote BSD pwd(1) could not figure out how to implement
-L and therefore made -P the default (and only) option. Support for -L
was later added, but the default was never changed.

Clean up the code, make -L the default, and rewrite getcwd_logical() to
reject paths that contain dot or dot-dot, as required by POSIX.

MFC after:	1 week
Reviewed by:	olce
Differential Revision:	https://reviews.freebsd.org/D55146

bin/pwd/pwd.c
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 1991, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
+ * Copyright (c) 2026 Dag-Erling Smørgrav
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,40 +32,74 @@
 
 #include <sys/param.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 
 #include <err.h>
-#include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-static char *getcwd_logical(void);
-void usage(void);
+static char *
+getcwd_logical(void)
+{
+	struct stat log, phy;
+	char *pwd, *p, *q;
+
+	/* $PWD is set and absolute */
+	if ((pwd = getenv("PWD")) == NULL || *pwd != '/')
+		return (NULL);
+	/* $PWD does not contain /./ or /../ */
+	for (p = pwd; *p; p = q) {
+		for (q = ++p; *q && *q != '/'; q++)
+			/* nothing */;
+		if ((*p == '.' && q == ++p) ||
+		    (*p == '.' && q == ++p))
+			return (NULL);
+	}
+	/* $PWD refers to the current directory */
+	if (stat(pwd, &log) != 0 || stat(".", &phy) != 0 ||
+	    log.st_dev != phy.st_dev || log.st_ino != phy.st_ino)
+		return (NULL);
+	return (pwd);
+}
+
+static char *
+getcwd_physical(void)
+{
+	static char pwd[MAXPATHLEN];
+
+	return (getcwd(pwd, sizeof(pwd)));
+}
+
+static void
+usage(void)
+{
+	(void)fprintf(stderr, "usage: pwd [-L | -P]\n");
+	exit(1);
+}
 
 int
 main(int argc, char *argv[])
 {
-	int physical;
-	int ch;
-	char *p;
+	char *pwd;
+	int opt;
+	bool logical;
 
-	physical = 1;
-	while ((ch = getopt(argc, argv, "LP")) != -1)
-		switch (ch) {
+	logical = true;
+	while ((opt = getopt(argc, argv, "LP")) != -1) {
+		switch (opt) {
 		case 'L':
-			physical = 0;
+			logical = true;
 			break;
 		case 'P':
-			physical = 1;
+			logical = false;
 			break;
-		case '?':
 		default:
 			usage();
 		}
+	}
 	argc -= optind;
 	argv += optind;
-
 	if (argc != 0)
 		usage();
 
@@ -72,40 +107,10 @@ main(int argc, char *argv[])
 	 * If we're trying to find the logical current directory and that
 	 * fails, behave as if -P was specified.
 	 */
-	if ((!physical && (p = getcwd_logical()) != NULL) ||
-	    (p = getcwd(NULL, 0)) != NULL)
-		printf("%s\n", p);
+	if ((logical && (pwd = getcwd_logical()) != NULL) ||
+	    (pwd = getcwd_physical()) != NULL)
+		printf("%s\n", pwd);
 	else
 		err(1, ".");
-
 	exit(0);
-}
-
-void __dead2
-usage(void)
-{
-
-	(void)fprintf(stderr, "usage: pwd [-L | -P]\n");
-  	exit(1);
-}
-
-static char *
-getcwd_logical(void)
-{
-	struct stat lg, phy;
-	char *pwd;
-
-	/*
-	 * Check that $PWD is an absolute logical pathname referring to
-	 * the current working directory.
-	 */
-	if ((pwd = getenv("PWD")) != NULL && *pwd == '/') {
-		if (stat(pwd, &lg) == -1 || stat(".", &phy) == -1)
-			return (NULL);
-		if (lg.st_dev == phy.st_dev && lg.st_ino == phy.st_ino)
-			return (pwd);
-	}
-
-	errno = ENOENT;
-	return (NULL);
 }

bin/pwd/pwd.1
@@ -29,7 +29,7 @@
 .\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 .\" SUCH DAMAGE.
 .\"
-.Dd May 22, 2023
+.Dd Februrary 6, 2026
 .Dt PWD 1
 .Os
 .Sh NAME
@@ -60,7 +60,7 @@ Display the physical current working directory (all symbolic links resolved).
 .El
 .Pp
 If no options are specified, the
-.Fl P
+.Fl L
 option is assumed.
 .Sh ENVIRONMENT
 Environment variables used by

freebsd/freebsd-src

commit 72bb9eb7b6decc34f343bdcbf0a8df0c73f9edd4

pwd(1): De-obfuscate, style(9)
In getcwd_logical(), test for a '.' or '..' component in one of the most
straightforward and intelligible ways possible.

In particular, this removes a superfluous re-test of the the component's
first character being '.' when the first one did not pass and, more
importantly, prevents the second test from relying on a side-effect in
the first.

While here, for better clarity, replace the loop that searches for '/'
with a simple call to strchrnul().

Add high-level comments about what is going on.

While here, test explicitly that pointed 'char' values are not 0 ('\0')
(style(9)).

While here, separate the successive steps of getcwd_logical() with blank
lines.

No functional change (intended).

Discussed with: emaste
Fixes:          2df923c5d2d0 ("pwd: Clean up and adopt POSIX semantics")
MFC after:      3 days
Sponsored by:   The FreeBSD Foundation
Differential Revision:  https://reviews.freebsd.org/D59709

bin/pwd/pwd.c
@@ -33,33 +33,40 @@
 #include <sys/param.h>
 #include <sys/stat.h>
 
+#include <assert.h>
 #include <err.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 static char *
 getcwd_logical(void)
 {
 	struct stat log, phy;
-	char *pwd, *p, *q;
+	char *pwd, *s, *e;
 
 	/* $PWD is set and absolute */
 	if ((pwd = getenv("PWD")) == NULL || *pwd != '/')
 		return (NULL);
+
 	/* $PWD does not contain /./ or /../ */
-	for (p = pwd; *p; p = q) {
-		for (q = ++p; *q && *q != '/'; q++)
-			/* nothing */;
-		if ((*p == '.' && q == ++p) ||
-		    (*p == '.' && q == ++p))
+	e = pwd;
+	do {
+		assert(*e == '/');
+		s = e + 1; /* Beginning of next component (may point to NUL). */
+		/* Search for the component's end. */
+		e = strchrnul(s, '/');
+		if (s[0] == '.' && (s + 1 == e || (s[1] == '.' && s + 2 == e)))
 			return (NULL);
-	}
+	} while (*e != '\0');
+
 	/* $PWD refers to the current directory */
 	if (stat(pwd, &log) != 0 || stat(".", &phy) != 0 ||
 	    log.st_dev != phy.st_dev || log.st_ino != phy.st_ino)
 		return (NULL);
+
 	return (pwd);
 }
 